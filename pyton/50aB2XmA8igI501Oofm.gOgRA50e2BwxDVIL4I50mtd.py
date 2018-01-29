99c7d94059f1a8d68ddc23dd62f470ec3537c46d
SUNsung <weatherius@gmail.com>
2018-01-29 09:20:09 +0200

    Я опять приумножил,  теперь все стало гораздо красивее!

--- pyton/50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py
+++ pyton/50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py
@@ -1,5 +1,97 @@
 
         
-          # Only apply checks if we got all parts of the boolean expression
-  if not (lhs and operator and rhs):
-    return
+        
+class HTTPieHelpFormatter(RawDescriptionHelpFormatter):
+    '''A nicer help formatter.
+    
+        '''
+    
+    
+def _lookup_app_object(name):
+    top = _app_ctx_stack.top
+    if top is None:
+        raise RuntimeError(_app_ctx_err_msg)
+    return getattr(top, name)
+    
+        class Index2(flask.views.View):
+        methods = ['OPTIONS']
+        provide_automatic_options = True
+    
+    '''
+    
+    Base = declarative_base()
+    
+    '''
+    
+        try:
+        fk_columns = generic_find_constraint_name(
+            table='columns', columns={'datasource_name'},
+            referenced='datasources', db=db)
+        with op.batch_alter_table('columns') as batch_op:
+            batch_op.drop_constraint(fk_columns, type_='foreignkey')
+    except Exception as e:
+        logging.warning(str(e))
+    
+    from alembic import op
+import sqlalchemy as sa
+    
+    from alembic import op
+import sqlalchemy as sa
+    
+    def downgrade():
+    op.drop_column('slices', 'description')
+
+    
+            d = deque('abc', maxlen=5)
+        self.assertEqual(d * -5, deque())
+        self.assertEqual(d * 0, deque())
+        self.assertEqual(d * 1, deque('abc'))
+        self.assertEqual(d * 2, deque('bcabc'))
+        self.assertEqual(d * 30, deque('bcabc'))
+    
+            'replace':  a[i1:i2] should be replaced by b[j1:j2]
+        'delete':   a[i1:i2] should be deleted.
+                    Note that j1==j2 in this case.
+        'insert':   b[j1:j2] should be inserted at a[i1:i1].
+                    Note that i1==i2 in this case.
+        'equal':    a[i1:i2] == b[j1:j2]
+    
+            '''
+        self.object_hook = object_hook
+        self.parse_float = parse_float or float
+        self.parse_int = parse_int or int
+        self.parse_constant = parse_constant or _CONSTANTS.__getitem__
+        self.strict = strict
+        self.object_pairs_hook = object_pairs_hook
+        self.parse_object = JSONObject
+        self.parse_array = JSONArray
+        self.parse_string = scanstring
+        self.memo = {}
+        self.scan_once = scanner.make_scanner(self)
+    
+            if nextchar == ''':
+            return parse_string(string, idx + 1, strict)
+        elif nextchar == '{':
+            return parse_object((string, idx + 1), strict,
+                _scan_once, object_hook, object_pairs_hook, memo)
+        elif nextchar == '[':
+            return parse_array((string, idx + 1), _scan_once)
+        elif nextchar == 'n' and string[idx:idx + 4] == 'null':
+            return None, idx + 4
+        elif nextchar == 't' and string[idx:idx + 4] == 'true':
+            return True, idx + 4
+        elif nextchar == 'f' and string[idx:idx + 5] == 'false':
+            return False, idx + 5
+    }
+    
+    >>> from nntplib import NNTP
+>>> s = NNTP('news')
+>>> resp, count, first, last, name = s.group('comp.lang.python')
+>>> print('Group', name, 'has', count, 'articles, range', first, 'to', last)
+Group comp.lang.python has 51 articles, range 5770 to 5821
+>>> resp, subs = s.xhdr('subject', '{0}-{1}'.format(first, last))
+>>> resp = s.quit()
+>>>
+    
+        def test_oldargs0_2_kw(self):
+        self.assertRaises(TypeError, {}.keys, x=2, y=2)
