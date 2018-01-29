d8d2ed940c52fcb7491f4706efc6518f16193f51
SUNsung <weatherius@gmail.com>
2018-01-29 17:25:52 +0200

    Смотри что я исправил,  теперь ничего не работает! =(

--- ruby/435kO3PoUjRHk430wZQF0OUujw43slTJBTU0i5E43L2P.rb
+++ ruby/435kO3PoUjRHk430wZQF0OUujw43slTJBTU0i5E43L2P.rb
@@ -1,108 +1,62 @@
 
         
-          describe '.all' do
-    it 'uses the client to fetch all keys' do
-      mock_client_response(:list_keys, with: no_args) do
-        [
-          {
-            canDownload: false,
-            canRevoke: true,
-            keyId: 'some-key-id',
-            keyName: 'Test Key via fastlane',
-            servicesCount: 2
-          },
-          {
-            canDownload: true,
-            canRevoke: true,
-            keyId: 'B92NE4F7RG',
-            keyName: 'Test Key via browser',
-            servicesCount: 2
-          }
-        ]
-      end
-    
-          {
-          author: last_git_commit_formatted_with('%an'),
-          message: last_git_commit_formatted_with('%B'),
-          commit_hash: last_git_commit_formatted_with('%H'),
-          abbreviated_commit_hash: last_git_commit_formatted_with('%h')
-      }
-    end
-    
-          def load_all_devices
-        self.devices = []
-    
-          # Read-only path to the shell-escaped gradle script, suitable for use in shell commands
-      attr_reader :escaped_gradle_path
-    
-      def collection_presenter
-    ActivityPub::CollectionPresenter.new(
-      id: tag_url(@tag),
-      type: :ordered,
-      size: @tag.statuses.count,
-      items: @statuses.map { |s| ActivityPub::TagManager.instance.uri_for(s) }
-    )
-  end
-    
-          format.json do
-        render json: collection_presenter,
-               serializer: ActivityPub::CollectionSerializer,
-               adapter: ActivityPub::Adapter,
-               content_type: 'application/activity+json'
+              def convert_key(key)
+        key.is_a?(Symbol) ? key.to_s : key
       end
     end
   end
-    
-          expect(response).to redirect_to(settings_preferences_path)
-      user.reload
-      expect(user.locale).to eq 'en'
-      expect(user.filtered_languages).to eq ['es', 'fr']
-    end
-    
-          it 'creates mention for target account' do
-        expect(account.mentions.count).to eq 1
-      end
-    end
-    
-    desc 'Initial setup for Octopress: copies the default theme into the path of Jekyll's generator. Rake install defaults to rake install[classic] to install a different theme run rake install[some_theme_name]'
-task :install, :theme do |t, args|
-  if File.directory?(source_dir) || File.directory?('sass')
-    abort('rake aborted!') if ask('A theme is already installed, proceeding will overwrite existing files. Are you sure?', ['y', 'n']) == 'n'
-  end
-  # copy theme into working Jekyll directories
-  theme = args.theme || 'classic'
-  puts '## Copying '+theme+' theme into ./#{source_dir} and ./sass'
-  mkdir_p source_dir
-  cp_r '#{themes_dir}/#{theme}/source/.', source_dir
-  mkdir_p 'sass'
-  cp_r '#{themes_dir}/#{theme}/sass/.', 'sass'
-  mkdir_p '#{source_dir}/#{posts_dir}'
-  mkdir_p public_dir
 end
+
     
-      get(/.+/) do
-    send_sinatra_file(request.path) {404}
-  end
-    
-        def handle_gist_redirecting(data)
-      redirected_url = data.header['Location']
-      if redirected_url.nil? || redirected_url.empty?
-        raise ArgumentError, 'GitHub replied with a 302 but didn't provide a location in the response headers.'
-      end
-    
-          Dir.chdir(code_path) do
-        code = file.read
-        @filetype = file.extname.sub('.','') if @filetype.nil?
-        title = @title ? '#{@title} (#{file.basename})' : file.basename
-        url = '/#{code_dir}/#{@file}'
-        source = '<figure class='code'><figcaption><span>#{title}</span> <a href='#{url}'>download</a></figcaption>\n'
-        source += '#{HighlightCode::highlight(code, @filetype)}</figure>'
-        TemplateWrapper::safe_wrap(source)
+            raise t('error.invalid_stage_name', name: invalid, path: stage_config_path.join('#{invalid}.rb'))
       end
     end
   end
+end
+
     
-      class PostFilters < Octopress::Hooks::Post
-    def pre_render(post)
-      OctopressFilters::pre_filter(post)
+      # Convenience to access the current SSHKit backend outside of an `on` block.
+  def backend
+    SSHKit::Backend.current
+  end
+end
+
+    
+        # @abstract
+    #
+    # Identify the SHA of the commit that will be deployed.  This will most likely involve SshKit's capture method.
+    #
+    # @return void
+    #
+    def fetch_revision
+      raise NotImplementedError, 'Your SCM strategy module should provide a #fetch_revision method'
+    end
+  end
+end
+
+    
+          def encrypt(value)
+        options[:encryptor].hexdigest value.to_s
+      end
+    
+        it 'leaves normal params untouched' do
+      mock_app do |env|
+        request = Rack::Request.new(env)
+        [200, {'Content-Type' => 'text/plain'}, [request.params['foo']]]
+      end
+      get '/', :foo => 'bar'
+      expect(body).to eq('bar')
+    end
+    
+      %w(GET HEAD).each do |method|
+    it 'accepts #{method} requests with non-whitelisted Origin' do
+      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://malicious.com')).to be_ok
+    end
+  end
+    
+    
+  it 'should allow changing the nosniff-mode off' do
+    mock_app do
+      use Rack::Protection::XSSHeader, :nosniff => false
+      run DummyApp
     end
