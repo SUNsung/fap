787ff4e9cc993915d099295a2cceb904bfa284ae
SUNsung <weatherius@gmail.com>
2018-01-30 04:08:08 +0200

    Тут я немного нагомнокодил,  теперь ничего не работает! =(

--- pyton/41BMdst10M35o4130VOw9GlEvs41Z3BDfqfDaUk41z58.py
+++ pyton/41BMdst10M35o4130VOw9GlEvs41Z3BDfqfDaUk41z58.py
@@ -1,65 +1,89 @@
 
         
-            old_layer = keras.layers.Deconvolution2D(5, 3, 3,
-                                             output_shape=(6, 7, 5),
-                                             init='normal',
-                                             subsample=(2, 2),
-                                             border_mode='valid',
-                                             dim_ordering='th',
-                                             W_regularizer='l1',
-                                             b_regularizer='l2',
-                                             W_constraint='maxnorm',
-                                             b_constraint='unitnorm',
-                                             name='conv')
-    new_layer = keras.layers.Conv2DTranspose(
-        5, (3, 3),
-        kernel_initializer='normal',
-        strides=(2, 2),
-        padding='valid',
-        kernel_regularizer='l1',
-        bias_regularizer='l2',
-        kernel_constraint='max_norm',
-        bias_constraint='unit_norm',
-        data_format='channels_first',
-        name='conv')
-    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer.get_config())
+        # TODO: response is the only one
+    
+    import pytest
+from requests.compat import urljoin
+    
+        @pytest.fixture(autouse=True)
+    def setup(self):
+        '''CaseInsensitiveDict instance with 'Accept' header.'''
+        self.case_insensitive_dict = CaseInsensitiveDict()
+        self.case_insensitive_dict['Accept'] = 'application/json'
+    
+        return {'name': implementation, 'version': implementation_version}
+    
+        def __init__(self, operators, supervisors, directors):
+        self.operators = operators
+        self.supervisors = supervisors
+        self.directors = directors
+        self.queued_calls = deque()
+    
+        def __init__(self, size):
+        self.size = size
+        self.table = [[] for _ in range(self.size)]
+    
+            (2016-01, shopping), 125
+        (2016-01, gas), 50
+        '''
+        total = sum(values)
+        self.handle_budget_notifications(key, total)
+        yield key, sum(values)
+    
+        def categorize(self, transaction):
+        if transaction.seller in self.seller_category_map:
+            return self.seller_category_map[transaction.seller]
+        if transaction.seller in self.seller_category_overrides_map:
+            seller_category_map[transaction.seller] = \
+                self.manual_overrides[transaction.seller].peek_min()
+            return self.seller_category_map[transaction.seller]
+        return None
+    
+        def steps(self):
+        '''Run the map and reduce steps.'''
+        return [
+            self.mr(mapper=self.mapper,
+                    reducer=self.reducer),
+            self.mr(mapper=self.mapper_sort,
+                    reducer=self.reducer_identity),
+        ]
     
     
-@pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
-def test_he_normal(tensor_shape):
-    fan_in, _ = initializers._compute_fans(tensor_shape)
-    scale = np.sqrt(2. / fan_in)
-    _runner(initializers.he_normal(), tensor_shape,
-            target_mean=0., target_std=None, target_max=2 * scale)
-    
-            # Test equivalence of convert_dense_weights_data_format
-        out1 = model1.predict(x)
-        layer_utils.convert_dense_weights_data_format(model1.layers[2], prev_shape, target_data_format)
-        for (src, dst) in zip(model1.layers, model2.layers):
-            dst.set_weights(src.get_weights())
-        out2 = model2.predict(transpose(x))
+class LookupService(object):
     
     
-def test_cifar():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-        assert len(x_train) == len(y_train) == 50000
-        assert len(x_test) == len(y_test) == 10000
-        (x_train, y_train), (x_test, y_test) = cifar100.load_data('fine')
-        assert len(x_train) == len(y_train) == 50000
-        assert len(x_test) == len(y_test) == 10000
-        (x_train, y_train), (x_test, y_test) = cifar100.load_data('coarse')
-        assert len(x_train) == len(y_train) == 50000
-        assert len(x_test) == len(y_test) == 10000
+if __name__ == '__main__':
+    RemoveDuplicateUrls.run()
+
     
-            if data_format == 'channels_first':
-            inputs = np.random.rand(num_samples, sequence_len,
-                                    input_channel,
-                                    input_num_row, input_num_col)
-        else:
-            inputs = np.random.rand(num_samples, sequence_len,
-                                    input_num_row, input_num_col,
-                                    input_channel)
+    # revision identifiers, used by Alembic.
+revision = '1a48a5411020'
+down_revision = '289ce07647b'
+    
+    def upgrade():
+    op.add_column('table_columns', sa.Column('expression', sa.Text(), nullable=True))
+    
+    Revision ID: 7e3ddad2a00b
+Revises: b46fa1b0b39e
+Create Date: 2016-10-14 11:17:54.995156
+    
+        if not opts and not args:
+        # Display help.
+        print(_help)
+        # Enter GUI mode.
+        #from .gui import gui_main
+        #gui_main()
+    else:
+        conf = {}
+        for opt, arg in opts:
+            if opt in ('-h', '--help'):
+                # Display help.
+                print(_help)
+    
+    from ..common import *
+    
+    
+site_info = 'kugou.com'
+download = kugou_download
+# download_playlist = playlist_not_supported('kugou')
+download_playlist=kugou_download_playlist
