fa0f304b0ee1ba038f471b130adcc66571e4fad5
SUNsung <weatherius@gmail.com>
2018-01-31 00:50:29 +0200

    rewrite [50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py] File in: 2018-01-31 00:50:28

--- pyton/50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py
+++ pyton/50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py
@@ -1,196 +1,55 @@
 
         
-            # second blueprint instance
-    r = client.get('/pages/hello')
-    assert r.status_code == 200
+            app.add_url_rule('/', view_func=Index.as_view('index'))
+    rv = client.get('/')
+    assert rv.data == b'Blub'
+    assert rv.headers['X-Method'] == 'GET'
+    rv = client.head('/')
+    assert rv.data == b''
+    assert rv.headers['X-Method'] == 'HEAD'
     
-        def _get_source_fast(self, environment, template):
-        for srcobj, loader in self._iter_loaders(template):
-            try:
-                return loader.get_source(environment, template)
-            except TemplateNotFound:
-                continue
-        raise TemplateNotFound(template)
+    from sklearn.utils import check_random_state
+from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.metrics.pairwise import pairwise_kernels
     
-            def __init__(self, name, doc=None):
-            self.name = name
-            self.__doc__ = doc
-        def _fail(self, *args, **kwargs):
-            raise RuntimeError('signalling support is unavailable '
-                               'because the blinker library is '
-                               'not installed.')
-        send = lambda *a, **kw: None
-        connect = disconnect = has_receivers_for = receivers_for = \
-            temporarily_connected_to = connected_to = _fail
-        del _fail
+    ratio = scikits_time / scipy_time
     
+            # split the paragraph into fake smaller paragraphs to make the
+        # problem harder e.g. more similar to tweets
+        if lang in ('zh', 'ja'):
+        # FIXME: whitespace tokenizing does not work on chinese and japanese
+            continue
+        words = content.split()
+        n_groups = len(words) / n_words_per_short_text
+        if n_groups < 1:
+            continue
+        groups = np.array_split(words, n_groups)
     
-class FormDataRoutingRedirect(AssertionError):
-    '''This exception is raised by Flask in debug mode if it detects a
-    redirect caused by the routing system when the request method is not
-    GET, HEAD or OPTIONS.  Reasoning: form data will be dropped.
-    '''
+        input_file = open(os.path.join(exercise_dir, f))
+    output_file = open(os.path.join(skeleton_dir, f), 'w')
     
-        All keys are expected to be strings. The structure remembers the
-    case of the last key to be set, and ``iter(instance)``,
-    ``keys()``, ``items()``, ``iterkeys()``, and ``iteritems()``
-    will contain case-sensitive keys. However, querying and contains
-    testing is case insensitive::
+    xx = np.linspace(3, 9, 100)
+yy = np.linspace(1, 5, 100).T
+xx, yy = np.meshgrid(xx, yy)
+Xfull = np.c_[xx.ravel(), yy.ravel()]
     
-    try:
-    from .packages.urllib3.contrib import pyopenssl
-except ImportError:
-    pyopenssl = None
-    OpenSSL = None
-    cryptography = None
-else:
-    import OpenSSL
-    import cryptography
-    
-        # Check chardet for compatibility.
-    major, minor, patch = chardet_version.split('.')[:3]
-    major, minor, patch = int(major), int(minor), int(patch)
-    # chardet >= 3.0.2, < 3.1.0
-    assert major == 3
-    assert minor < 1
-    assert patch >= 2
+            sock, port = bind_unused_port()
+        server = namespace['TestServer']()
+        server.add_socket(sock)
+        client = IOStream(socket.socket())
+        yield client.connect(('localhost', port))
+        result = yield client.read_until_close()
+        self.assertEqual(result, b'data')
+        server.stop()
+        client.close()
     
     
-def iter_slices(string, slice_length):
-    '''Iterate over slices of a string.'''
-    pos = 0
-    if slice_length is None or slice_length <= 0:
-        slice_length = len(string)
-    while pos < len(string):
-        yield string[pos:pos + slice_length]
-        pos += slice_length
+class ExceptionBenchmark(Benchmark):
+    def make_context(self):
+        return stack_context.ExceptionStackContext(self.__handle_exception)
     
-        old_layer = keras.layers.Convolution2D(5, nb_row=3, nb_col=3, name='conv')
-    new_layer = keras.layers.Conv2D(5, (3, 3), name='conv')
-    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer.get_config())
-    
-    from keras.preprocessing import sequence
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
-from keras.layers import Embedding
-from keras.layers import LSTM
-from keras.layers import Conv1D, MaxPooling1D
-from keras.datasets import imdb
-    
-    # input image dimensions
-img_rows, img_cols = 28, 28
-    
-    num_classes = np.max(y_train) + 1
-print(num_classes, 'classes')
-    
-    
-def test_sparse_metrics():
-    for metric in all_sparse_metrics:
-        y_a = K.variable(np.random.randint(0, 7, (6,)), dtype=K.floatx())
-        y_b = K.variable(np.random.random((6, 7)), dtype=K.floatx())
-        assert K.eval(metric(y_a, y_b)).shape == (6,)
-    
-    from keras.utils.test_utils import get_test_data, keras_test
-from keras.utils.np_utils import to_categorical
-from keras.models import Sequential
-from keras import layers, optimizers
-import keras.backend as K
-import keras
-    
-        # test serialization
-    config = outer_model.get_config()
-    outer_model = models.Model.from_config(config)
-    outer_model.compile(optimizer='rmsprop',
-                        loss='categorical_crossentropy',
-                        metrics=['accuracy'])
-    outer_model.fit([x, y, z], labels, epochs=1)
-    
-    
-def find_constraint_name(upgrade=True):
-    cols = {'column_name'} if upgrade else {'datasource_name'}
-    return generic_find_constraint_name(
-        table='columns', columns=cols, referenced='datasources', db=db)
-    
-    # revision identifiers, used by Alembic.
-revision = '1a48a5411020'
-down_revision = '289ce07647b'
-    
-    def upgrade():
-    op.add_column('table_columns', sa.Column('expression', sa.Text(), nullable=True))
-    
-    Revision ID: 289ce07647b
-Revises: 2929af7925ed
-Create Date: 2015-11-21 11:18:00.650587
-    
-    '''
-    
-    from alembic import op
-import sqlalchemy as sa
-    
-    
-def downgrade():
-    try:
-        op.drop_column('dbs', 'allow_run_sync')
-        op.drop_column('dbs', 'allow_run_async')
-    except Exception:
-        pass
-    
-    '''
-    
-        title = match1(html, r'&title=([^&]+)')
-    
-    
-def extract_board_data(url):
-    json_data = extract_json_data(url, limit=LIMIT)
-    pin_list = json_data['pins']
-    title = json_data['title']
-    pin_count = json_data['pin_count']
-    pin_count -= len(pin_list)
-    
-    def kugou_download_playlist(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    html=get_html(url)
-    pattern=re.compile('title='(.*?)'.* data='(\w*)\|.*?'')
-    pairs=pattern.findall(html)
-    for title,hash_val in pairs:
-        kugou_download_by_hash(title,hash_val,output_dir,merge,info_only)
-    
-    def kuwo_download_by_rid(rid, output_dir = '.', merge = True, info_only = False):
-    html=get_content('http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_%s'%rid)
-    title=match1(html,r'<name>(.*)</name>')
-    #to get title
-    #format =aac|mp3 ->to get aac format=mp3 ->to get mp3
-    url=get_content('http://antiserver.kuwo.cn/anti.s?format=mp3&rid=MUSIC_%s&type=convert_url&response=url'%rid)
-    songtype, ext, size = url_info(url)
-    print_info(site_info, title, songtype, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir)
-    
-        def test_sequential_execution(self):
-        self.command_stack[0].execute()
-        output_after_first_execution = os.listdir(self.test_dir)
-        self.assertEqual(output_after_first_execution[0], 'bar.txt')
-        self.command_stack[1].execute()
-        output_after_second_execution = os.listdir(self.test_dir)
-        self.assertEqual(output_after_second_execution[0], 'baz.txt')
-    
-        def get_current_time_as_html_fragment(self, time_provider):
-        current_time = time_provider.now()
-        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
-        return current_time_as_html_fragment
-    
-        # commands are just pushed into the command stack
-    command_stack.append(MoveFileCommand('foo.txt', 'bar.txt'))
-    command_stack.append(MoveFileCommand('bar.txt', 'baz.txt'))
-    
-    
-class Reporter:
-    
-            for j in range(w):
-            pixel = im_pixel[j, i]
-            # 修掉脑袋比下一个小格子还高的情况的 bug
-            if abs(j - piece_x) < piece_body_width:
-                continue
-    
-    
-c = wda.Client()
-s = c.session()
+        @classmethod
+    def update_cache(cls, chat):
+        cls.cache.append(chat)
+        if len(cls.cache) > cls.cache_size:
+            cls.cache = cls.cache[-cls.cache_size:]
