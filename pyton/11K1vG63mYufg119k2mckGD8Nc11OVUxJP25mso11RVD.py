61cb8ee47e9b60bf219845d602740de704cd18ac
SUNsung <weatherius@gmail.com>
2018-01-31 13:22:59 +0200

    Rewrite [11K1vG63mYufg119k2mckGD8Nc11OVUxJP25mso11RVD.py] File in: 2018-01-31 13:22:59

--- pyton/11K1vG63mYufg119k2mckGD8Nc11OVUxJP25mso11RVD.py
+++ pyton/11K1vG63mYufg119k2mckGD8Nc11OVUxJP25mso11RVD.py
@@ -1,12 +1,146 @@
-    failures = []
-    while point is not None:
-        if point.name:
-            if re.search('h[1-2]', point.name):
-                if point.name == 'h1':
-                    h1_directory = os.path.join(output_directory, clean_text(point.text))
-                    current_directory = h1_directory
-                elif point.name == 'h2':
-                    current_directory = os.path.join(h1_directory, clean_text(point.text))  
-                if not os.path.exists(current_directory):
-                    os.makedirs(current_directory)
-                print_title(point.text)
+
+        
+        # TODO: response is the only one
+    
+    
+def check_compatibility(urllib3_version, chardet_version):
+    urllib3_version = urllib3_version.split('.')
+    assert urllib3_version != ['dev']  # Verify urllib3 isn't installed from git.
+    
+            Currently, this closes the PoolManager and any active ProxyManager,
+        which closes any pooled connections.
+        '''
+        self.poolmanager.clear()
+        for proxy in self.proxy_manager.values():
+            proxy.clear()
+    
+    
+class ConnectTimeout(ConnectionError, Timeout):
+    '''The request timed out while trying to connect to the remote server.
+    
+    >>> requests.codes['temporary_redirect']
+307
+>>> requests.codes.teapot
+418
+>>> requests.codes['\o/']
+200
+    
+    ESTIMATORS = {
+    'dummy': DummyClassifier(),
+    'random_forest': RandomForestClassifier(n_estimators=100,
+                                            max_features='sqrt',
+                                            min_samples_split=10),
+    'extra_trees': ExtraTreesClassifier(n_estimators=100,
+                                        max_features='sqrt',
+                                        min_samples_split=10),
+    'logistic_regression': LogisticRegression(),
+    'naive_bayes': MultinomialNB(),
+    'adaboost': AdaBoostClassifier(n_estimators=10),
+}
+    
+        alpha = 0.01  # regularization parameter
+    
+    
+def _plot(results, metrics, formats, title, x_ticks, x_label,
+          format_markers=('x', '|', 'o', '+'),
+          metric_colors=('c', 'm', 'y', 'k', 'g', 'r', 'b')):
+    '''
+    Plot the results by metric, format and some other variable given by
+    x_label
+    '''
+    fig = plt.figure('scikit-learn multilabel metrics benchmarks')
+    plt.title(title)
+    ax = fig.add_subplot(111)
+    for i, metric in enumerate(metrics):
+        for j, format in enumerate(formats):
+            ax.plot(x_ticks, results[i, j].flat,
+                    label='{}, {}'.format(metric, format),
+                    marker=format_markers[j],
+                    color=metric_colors[i % len(metric_colors)])
+    ax.set_xlabel(x_label)
+    ax.set_ylabel('Time (s)')
+    ax.legend()
+    plt.show()
+    
+    plt.figure('scikit-learn Ward's method benchmark results')
+plt.imshow(np.log(ratio), aspect='auto', origin='lower')
+plt.colorbar()
+plt.contour(ratio, levels=[1, ], colors='k')
+plt.yticks(range(len(n_features)), n_features.astype(np.int))
+plt.ylabel('N features')
+plt.xticks(range(len(n_samples)), n_samples.astype(np.int))
+plt.xlabel('N samples')
+plt.title('Scikit's time, in units of scipy time (log)')
+plt.show()
+
+    
+        ###########################################################################
+    # List sampling algorithm
+    ###########################################################################
+    # We assume that sampling algorithm has the following signature:
+    #   sample(n_population, n_sample)
+    #
+    sampling_algorithm = {}
+    
+    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
+DATA_FOLDER = 'txt_sentoken'
+    
+        if not os.path.exists(ARCHIVE_NAME):
+        print('Downloading dataset from %s (14 MB)' % URL)
+        opener = urlopen(URL)
+        with open(ARCHIVE_NAME, 'wb') as archive:
+            archive.write(opener.read())
+    
+        # TASK: Build a grid search to find out whether unigrams or bigrams are
+    # more useful.
+    # Fit the pipeline on the training set using grid search for the parameters
+    parameters = {
+        'vect__ngram_range': [(1, 1), (1, 2)],
+    }
+    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1)
+    grid_search.fit(docs_train, y_train)
+    
+    The outer product of the row and column label vectors shows a
+representation of the checkerboard structure.
+    
+    
+    def rewind(self, marker=None):
+        '''
+        Reset the stream so that next call to index would return marker.
+        The marker will usually be index() but it doesn't have to be.  It's
+        just a marker to indicate what state the stream was in.  This is
+        essentially calling release() and seek().  If there are markers
+        created after this marker argument, this routine must unroll them
+        like a stack.  Assume the state the stream was in when this marker
+        was created.
+    
+        @classmethod
+    def setUpClass(cls):
+        requires('gui')
+        cls.root = Tk()
+    
+            m = match_number(string, idx)
+        if m is not None:
+            integer, frac, exp = m.groups()
+            if frac or exp:
+                res = parse_float(integer + (frac or '') + (exp or ''))
+            else:
+                res = parse_int(integer)
+            return res, m.end()
+        elif nextchar == 'N' and string[idx:idx + 3] == 'NaN':
+            return parse_constant('NaN'), idx + 3
+        elif nextchar == 'I' and string[idx:idx + 8] == 'Infinity':
+            return parse_constant('Infinity'), idx + 8
+        elif nextchar == '-' and string[idx:idx + 9] == '-Infinity':
+            return parse_constant('-Infinity'), idx + 9
+        else:
+            raise StopIteration(idx)
+    
+    
+def triplet_to_brightness(rgbtuple):
+    # return the brightness (grey level) along the scale 0.0==black to
+    # 1.0==white
+    r = 0.299
+    g = 0.587
+    b = 0.114
+    return r*rgbtuple[0] + g*rgbtuple[1] + b*rgbtuple[2]
