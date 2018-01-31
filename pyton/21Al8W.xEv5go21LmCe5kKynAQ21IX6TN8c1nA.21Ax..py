6f5f8317d4039934420d668bd4672ecd09ac02df
SUNsung <weatherius@gmail.com>
2018-01-31 16:16:31 +0200

    Rewrite [21Al8W.xEv5go21LmCe5kKynAQ21IX6TN8c1nA.21Ax..py] File in: 2018-01-31 16:16:31

--- pyton/21Al8W.xEv5go21LmCe5kKynAQ21IX6TN8c1nA.21Ax..py
+++ pyton/21Al8W.xEv5go21LmCe5kKynAQ21IX6TN8c1nA.21Ax..py
@@ -1,58 +1,81 @@
 
-class CallState(Enum):
+        
+                if any([s_line.startswith(s) for s in ['* [', '- [']]):
+            if indent == last_indent:
+                blocks[-1].append(line)
+            else:
+                blocks.append([line])
+            last_indent = indent
+        else:
+            blocks.append([line])
+            last_indent = None
     
-        def set(self, key, value):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                item.value = value
-                return
-        self.table[hash_index].append(Item(key, value))
+        def __init__(self, operators, supervisors, directors):
+        self.operators = operators
+        self.supervisors = supervisors
+        self.directors = directors
+        self.queued_calls = deque()
     
-        def __init__(self, template_categories_to_budget_map):
-        self.categories_to_budget_map = template_categories_to_budget_map
+    print 'POTFILE := ' + os.path.join(I18N_PATH, 'r2.pot')
     
-        def mapper(self, _, line):
-        yield line, 1
+        paths = {
+        'root': root_path,
+        'controllers': os.path.join(root_path, 'controllers'),
+        'templates': [os.path.join(root_path, 'templates')],
+    }
+    
+    def set_extension(environ, ext):
+    environ['extension'] = ext
+    environ['render_style'], environ['content_type'] = extension_mapping[ext]
+
+    
+    # patch out SSRFable/XSSable endpoints in older versions of weberror
+import weberror.evalexception
+    
+        @csrf_exempt
+    @json_validate(
+        VRatelimit(rate_ip=True, prefix='rate_register_'),
+        signature=VSigned(),
+        name=VUname(['user']),
+        email=ValidEmail('email'),
+        password=VPasswordChange(['passwd', 'passwd2']),
+    )
+    def POST_register(self, responder, name, email, password, **kwargs):
+        kwargs.update(dict(
+            controller=self,
+            form=responder('noop'),
+            responder=responder,
+            name=name,
+            email=email,
+            password=password,
+        ))
+        return handle_register(**kwargs)
+    
+            To request a new CAPTCHA,
+        use [/api/new_captcha](#POST_api_new_captcha).
+        '''
+        image = captcha.get_image(iden)
+        f = StringIO.StringIO()
+        image.save(f, 'PNG')
+        response.content_type = 'image/png;'
+        return f.getvalue()
+    
+
+    
+        @validate(
+        container_id=VGTMContainerId('id')
+    )
+    def GET_gtm(self, container_id):
+        return GoogleTagManager(container_id=container_id).render()
+
     
     
-class ExceptionBenchmark(Benchmark):
-    def make_context(self):
-        return stack_context.ExceptionStackContext(self.__handle_exception)
-    
-        @classmethod
-    def send_updates(cls, chat):
-        logging.info('sending message to %d waiters', len(cls.waiters))
-        for waiter in cls.waiters:
-            try:
-                waiter.write_message(chat)
-            except:
-                logging.error('Error sending message', exc_info=True)
-    
-    api('wikipagelisting', WikiPageListingJsonTemplate)
-api('wikipagediscussions', WikiJsonTemplate)
-api('wikipagesettings', WikiSettingsJsonTemplate)
-    
-                note = None
-            buyer = c.user
-            if c.user.name.lower() in g.live_config['proxy_gilding_accounts']:
-                note = 'proxy-%s' % c.user.name
-                if proxying_for:
-                    try:
-                        buyer = Account._by_name(proxying_for)
-                    except NotFound:
-                        pass
-    
-    from reddit_base import RedditController, UnloggedUser
-from r2.lib.pages import (ButtonLite, ButtonDemoPanel, WidgetDemoPanel,
-                          BoringPage)
-from r2.lib.pages.things import wrap_links
-from r2.models import *
-from r2.lib.validator import *
-from pylons import request, response
-from pylons import tmpl_context as c
-from pylons.i18n import _
-    
-    
-class ErrorController(RedditController):
-    '''Generates error documents as and when they are required.
+# Magic Number，不设置可能无法正常执行，请根据具体截图从上到下按需设置
+under_game_score_y = config['under_game_score_y']
+# 长按的时间系数，请自己根据实际情况调节
+press_coefficient = config['press_coefficient']
+# 二分之一的棋子底座高度，可能要调节
+piece_base_height_1_2 = config['piece_base_height_1_2']
+# 棋子的宽度，比截图中量到的稍微大一点比较安全，可能要调节
+piece_body_width = config['piece_body_width']
+time_coefficient = config['press_coefficient']
