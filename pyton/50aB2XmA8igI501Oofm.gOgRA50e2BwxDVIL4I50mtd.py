386d1b01a435681ee550301146a6ea1b9f427963
SUNsung <weatherius@gmail.com>
2018-01-31 09:42:59 +0200

    rewrite [50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py] File in: 2018-01-31 09:42:59

--- pyton/50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py
+++ pyton/50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py
@@ -1,55 +1,92 @@
 
         
-            app.add_url_rule('/', view_func=Index.as_view('index'))
-    rv = client.get('/')
-    assert rv.data == b'Blub'
-    assert rv.headers['X-Method'] == 'GET'
-    rv = client.head('/')
-    assert rv.data == b''
-    assert rv.headers['X-Method'] == 'HEAD'
+        import pytest
+from requests.compat import urljoin
     
-    from sklearn.utils import check_random_state
-from sklearn.metrics.pairwise import pairwise_distances
-from sklearn.metrics.pairwise import pairwise_kernels
+        :param str u_string: unicode string to check. Must be unicode
+        and not Python 2 `str`.
+    :rtype: bool
+    '''
+    assert isinstance(u_string, str)
+    try:
+        u_string.encode('ascii')
+        return True
+    except UnicodeEncodeError:
+        return False
+
     
-    ratio = scikits_time / scipy_time
+        def __repr__(self):
+        return '<lookup \'%s\'>' % (self.name)
     
-            # split the paragraph into fake smaller paragraphs to make the
-        # problem harder e.g. more similar to tweets
-        if lang in ('zh', 'ja'):
-        # FIXME: whitespace tokenizing does not work on chinese and japanese
-            continue
-        words = content.split()
-        n_groups = len(words) / n_words_per_short_text
-        if n_groups < 1:
-            continue
-        groups = np.array_split(words, n_groups)
+        def test_lower_items(self):
+        assert list(self.case_insensitive_dict.lower_items()) == [('accept', 'application/json')]
     
-        input_file = open(os.path.join(exercise_dir, f))
-    output_file = open(os.path.join(skeleton_dir, f), 'w')
+            if not qop:
+            respdig = KD(HA1, '%s:%s' % (nonce, HA2))
+        elif qop == 'auth' or 'auth' in qop.split(','):
+            noncebit = '%s:%s:%s:%s:%s' % (
+                nonce, ncvalue, cnonce, 'auth', HA2
+            )
+            respdig = KD(HA1, noncebit)
+        else:
+            # XXX handle auth-int.
+            return None
     
-    xx = np.linspace(3, 9, 100)
-yy = np.linspace(1, 5, 100).T
-xx, yy = np.meshgrid(xx, yy)
-Xfull = np.c_[xx.ravel(), yy.ravel()]
+    import pytest
     
-            sock, port = bind_unused_port()
-        server = namespace['TestServer']()
-        server.add_socket(sock)
-        client = IOStream(socket.socket())
-        yield client.connect(('localhost', port))
-        result = yield client.read_until_close()
-        self.assertEqual(result, b'data')
-        server.stop()
-        client.close()
+        def __getstate__(self):
+        return dict((attr, getattr(self, attr, None)) for attr in
+                    self.__attrs__)
     
     
-class ExceptionBenchmark(Benchmark):
-    def make_context(self):
-        return stack_context.ExceptionStackContext(self.__handle_exception)
+class ConnectionError(RequestException):
+    '''A Connection error occurred.'''
     
-        @classmethod
-    def update_cache(cls, chat):
-        cls.cache.append(chat)
-        if len(cls.cache) > cls.cache_size:
-            cls.cache = cls.cache[-cls.cache_size:]
+            '''
+        try:
+            return getattr(stacked_proxy, key)
+        except TypeError:
+            return default
+    
+    from r2.lib.manager import tp_manager
+from r2.lib.jsontemplates import *
+    
+        @require_oauth2_scope('account')
+    @validate(
+        VUser(),
+        validated_prefs=PREFS_JSON_VALIDATOR,
+    )
+    @api_doc(api_section.account, json_model=PREFS_JSON_VALIDATOR,
+             uri='/api/v1/me/prefs')
+    def PATCH_prefs(self, validated_prefs):
+        user_prefs = c.user.preferences()
+        for short_name, new_value in validated_prefs.iteritems():
+            pref_name = 'pref_' + short_name
+            user_prefs[pref_name] = new_value
+        vprefs.filter_prefs(user_prefs, c.user)
+        vprefs.set_prefs(c.user, user_prefs)
+        c.user._commit()
+        return self.api_wrapper(PrefsJsonTemplate().data(c.user))
+    
+    from pylons import request
+from pylons import app_globals as g
+from reddit_base import RedditController
+from r2.lib.pages import AdminPage, AdminAwards
+from r2.lib.pages import AdminAwardGive, AdminAwardWinners
+from r2.lib.validator import *
+    
+    from r2.controllers.reddit_base import RedditController
+from r2.lib.base import proxyurl
+from r2.lib.csrf import csrf_exempt
+from r2.lib.template_helpers import get_domain
+from r2.lib.pages import Embed, BoringPage, HelpPage
+from r2.lib.filters import websafe, SC_OFF, SC_ON
+from r2.lib.memoize import memoize
+    
+        def send429(self):
+        retry_after = request.environ.get('retry_after')
+        if retry_after:
+            response.headers['Retry-After'] = str(retry_after)
+            template_name = '/ratelimit_toofast.html'
+        else:
+            template_name = '/ratelimit_throttled.html'
