0a07d95fbca2fe592466daa9cbb537defdc9cbbe
SUNsung <weatherius@gmail.com>
2018-01-31 16:50:14 +0200

    Rewrite [36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py] File in: 2018-01-31 16:50:13

--- pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
+++ pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
@@ -1,153 +1,112 @@
 
         
-        # TODO: run all these tests in session mode as well
+                request_line = '{method} {path}{query} HTTP/1.1'.format(
+            method=self._orig.method,
+            path=url.path or '/',
+            query='?' + url.query if url.query else ''
+        )
     
-    https://github.com/Homebrew/homebrew-core/blob/master/Formula/httpie.rb
-    
-        @property
-    def config(self):
-        if not hasattr(self, '_config'):
-            self._config = Config(directory=self.config_dir)
-            if self._config.is_new():
-                self._config.save()
-            else:
-                self._config.load()
-        return self._config
+        exc = ConnectionError('Connection aborted')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
+    assert ret == ExitStatus.ERROR
+    assert error_msg == (
+        'ConnectionError: '
+        'Connection aborted while doing GET request to URL: '
+        'http://www.google.com')
     
     
-def test_unicode_url_query_arg_item_verbose(httpbin):
-    r = http('--verbose', httpbin.url + '/get', u'test==%s' % UNICODE)
+def test_follow_redirect_output_options(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=h',
+             '--history-print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 2
+    assert 'HTTP/1.1 302 FOUND' not in r
     assert HTTP_OK in r
-    assert UNICODE in r
     
-                    self._status_line = PROGRESS.format(
-                    percentage=percentage,
-                    downloaded=humanize_bytes(downloaded),
-                    speed=humanize_bytes(speed),
-                    eta=eta,
-                )
+        msg = Message('Content-Disposition: %s' % content_disposition)
+    filename = msg.get_filename()
+    if filename:
+        # Basic sanitation.
+        filename = os.path.basename(filename).lstrip('.').strip()
+        if filename:
+            return filename
     
-        This processor that applies syntax highlighting to the headers,
-    and also to the body if its content type is recognized.
+    from httpie.plugins.base import AuthPlugin
     
-        def test_verify_custom_ca_bundle_invalid_path(self, httpbin_secure):
-        # since 2.14.0 requests raises IOError
-        with pytest.raises((SSLError, IOError)):
-            http(httpbin_secure.url + '/get', '--verify', '/__not_found__')
+        def test_self_signed_server_cert_by_default_raises_ssl_error(
+            self,
+            httpbin_secure_untrusted):
+        with pytest.raises(SSLError):
+            http(httpbin_secure_untrusted.url + '/get')
     
-        def set(self, results, query):
-        '''Set the result for the given query key in the cache.
-        
-        When updating an entry, updates its position to the front of the LRU list.
-        If the entry is new and the cache is at capacity, removes the oldest entry
-        before the new entry is added.
-        '''
-        node = self.lookup[query]
-        if node is not None:
-            # Key exists in cache, update the value
-            node.results = results
-            self.linked_list.move_to_front(node)
-        else:
-            # Key does not exist in cache
-            if self.size == self.MAX_SIZE:
-                # Remove the oldest entry from the linked list and lookup
-                self.lookup.pop(self.linked_list.tail.query, None)
-                self.linked_list.remove_from_tail()
-            else:
-                self.size += 1
-            # Add the new key and value
-            new_node = Node(results)
-            self.linked_list.append_to_front(new_node)
-            self.lookup[query] = new_node
+        Assumes `from __future__ import division`.
     
-        def categorize(self, transaction):
-        if transaction.seller in self.seller_category_map:
-            return self.seller_category_map[transaction.seller]
-        if transaction.seller in self.seller_category_overrides_map:
-            seller_category_map[transaction.seller] = \
-                self.manual_overrides[transaction.seller].peek_min()
-            return self.seller_category_map[transaction.seller]
-        return None
+        def inner(*suffix):
+        return urljoin(httpbin_url, '/'.join(suffix))
     
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer)
-        ]
+        def __setitem__(self, key, value):
+        # Use the lowercased key for lookups, but store the actual
+        # key alongside the value.
+        self._store[key.lower()] = (key, value)
     
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer),
-            self.mr(mapper=self.mapper_sort,
-                    reducer=self.reducer_identity),
-        ]
-    
-    from alembic import op
-import sqlalchemy as sa
-    
+        ...what? Basically, expose the parsed HTTP headers from the server response
+    the way `cookielib` expects to see them.
     '''
     
-    
-def downgrade():
-    op.drop_table('logs')
-
-    
-        # set up the templating system
-    config['pylons.app_globals'].mako_lookup = TemplateLookup(
-        directories=paths['templates'],
-        error_handler=handle_mako_error,
-        module_directory=module_directory,
-        input_encoding='utf-8',
-        default_filters=['conditional_websafe'],
-        filesystem_checks=getattr(g, 'reload_templates', False),
-        imports=[
-            'from r2.lib.filters import websafe, unsafe, conditional_websafe',
-            'from pylons import request',
-            'from pylons import tmpl_context as c',
-            'from pylons import app_globals as g',
-            'from pylons.i18n import _, ungettext',
-        ],
-        modulename_callable=mako_module_path,
-    )
-    
-                    if is_api:
-                    return _wsgi_json(start_response, 413, error_msg)
-                else:
-                    start_response('413 Too Big', [])
-                    return ['<html>'
-                            '<head>'
-                            '<script type='text/javascript'>'
-                            'parent.completedUploadImage('failed','
-                            ''','
-                            ''','
-                            '[['BAD_CSS_NAME', ''], ['IMAGE_ERROR', '', error_msg,'']],'
-                            ''');'
-                            '</script></head><body>you shouldn\'t be here</body></html>']
-    
-    class EmbedController(RedditController):
-    allow_stylesheets = True
-    
-        def send429(self):
-        retry_after = request.environ.get('retry_after')
-        if retry_after:
-            response.headers['Retry-After'] = str(retry_after)
-            template_name = '/ratelimit_toofast.html'
-        else:
-            template_name = '/ratelimit_throttled.html'
+                    low_conn = conn._get_conn(timeout=DEFAULT_POOL_TIMEOUT)
     
     
-class GoogleTagManagerController(MinimalController):
-    def pre(self):
-        if request.host != g.media_domain:
-            # don't serve up untrusted content except on our
-            # specifically untrusted domain
-            self.abort404()
+def _parse_content_type_header(header):
+    '''Returns content type and parameters from given header
     
-        click_count += 1
-    if click_count > 1:
-        click_count = 0
-        cor1 = cor.pop()
-        cor2 = cor.pop()
+    # run scipy-based optimization (L-BFGS) over the pixels of the generated image
+# so as to minimize the neural style loss
+x = preprocess_image(base_image_path)
+    
+        assert success_result == 1
+    partial_result = K.eval(
+        metrics.sparse_top_k_categorical_accuracy(y_true, y_pred, k=2))
+    
+        # test lambda with output_mask lambda
+    input_a = layers.Input(shape=input_shapes[0][1:])
+    input_b = layers.Input(shape=input_shapes[1][1:])
+    a = layers.Masking()(input_a)
+    b = layers.Masking()(input_b)
+    merged = legacy_layers.merge(
+        [a, b], mode=lambda tup: K.concatenate([tup[0], tup[1]], axis=1),
+        output_shape=lambda tup: (tup[0][0], tup[0][1] + tup[1][1]) + tup[0][2:],
+        output_mask=lambda tup: K.concatenate([tup[0], tup[1]]))
+    model = models.Model([input_a, input_b], merged)
+    expected_output_shape = model.compute_output_shape(input_shapes)
+    actual_output_shape = model.predict(inputs).shape
+    assert expected_output_shape == actual_output_shape
+    
+    scale = 0.25
+    
+    
+def backup_screenshot(ts):
+    '''
+    为了方便失败的时候 debug
+    '''
+    if not os.path.isdir(screenshot_backup_dir):
+        os.mkdir(screenshot_backup_dir)
+    shutil.copy('1.png', '{}{}.png'.format(screenshot_backup_dir, ts))
+    
+    
+def main():
+    '''
+    主函数
+    '''
+    op = yes_or_no('请确保手机打开了 ADB 并连接了电脑，'
+                   '然后打开跳一跳并【开始游戏】后再用本程序，确定开始？')
+    if not op:
+        print('bye')
+        return
+    print('程序版本号：{}'.format(VERSION))
+    debug.dump_device_info()
+    screenshot.check_screenshot()
