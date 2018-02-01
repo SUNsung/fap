3ced4bdc6326837d89820087711e466abf700097
SUNsung <weatherius@gmail.com>
2018-02-01 23:01:05 +0200

    Rewrite [37ee9WDclnXGs37gjZAgT2rmxE373j7KDepwWwE37ND9.cpp] File in: 2018-02-01 23:01:04

--- cccccc/37ee9WDclnXGs37gjZAgT2rmxE373j7KDepwWwE37ND9.cpp
+++ cccccc/37ee9WDclnXGs37gjZAgT2rmxE373j7KDepwWwE37ND9.cpp
@@ -1,160 +1,348 @@
 
         
-          brightray::ContentClient content_client_;
-  std::unique_ptr<content::ContentBrowserClient> browser_client_;
-  std::unique_ptr<content::ContentRendererClient> renderer_client_;
-  std::unique_ptr<content::ContentUtilityClient> utility_client_;
+            // Create the meta-variable, let the typechecker name it.
+    Identifier name = TC.getNextResponseVariableName(SF.getParentModule());
+    VarDecl *vd = new (Context) VarDecl(/*IsStatic*/false,
+                                        VarDecl::Specifier::Let,
+                                        /*IsCaptureList*/false,
+                                        PBD->getStartLoc(), name,
+                                        pattern->getType(), &SF);
+    vd->setInterfaceType(pattern->getType());
+    SF.Decls.push_back(vd);
     
-    #endif  // ATOM_BROWSER_API_ATOM_API_DEBUGGER_H_
-
+        auto bucketA = getResultBucket(a, hasExpectedTypes);
+    auto bucketB = getResultBucket(b, hasExpectedTypes);
+    if (bucketA < bucketB)
+      return false;
+    else if (bucketB < bucketA)
+      return true;
     
-    void Initialize(v8::Local<v8::Object> exports, v8::Local<v8::Value> unused,
-                v8::Local<v8::Context> context, void* priv) {
-  v8::Isolate* isolate = context->GetIsolate();
-  mate::Dictionary dict(isolate, exports);
-  dict.Set('globalShortcut', atom::api::GlobalShortcut::Create(isolate));
+    public:
+  DeadEndBlocks(const SILFunction *F) : F(F) {}
+    
+      // Add the current module's source files to the dependencies.
+  llvm::SetVector<const clang::FileEntry *> fileDependencies;
+  // FIXME: This is not desirable because:
+  // 1. It picks shim header files as file dependencies
+  // 2. Having all the other swift files of the module as file dependencies ends
+  //   up making all of them associated with all the other files as main files.
+  //   It's better to associate each swift file with the unit that recorded it
+  //   as the main one.
+  // Keeping the code in case we want to revisit.
+#if 0
+  collectFileDependencies(fileDependencies, dependencyTracker, module, fileMgr);
+#endif
+    
+    /// Attempt to get a doc comment from the declaration, or other inherited
+/// sources, like from base classes or protocols.
+Optional<DocComment *> getCascadingDocComment(swift::markup::MarkupContext &MC,
+                                             const Decl *D);
+    
+    
+    {  /// Return a hash code of any components from these options that should
+  /// contribute to a Swift Bridging PCH hash.
+  llvm::hash_code getPCHHashComponents() const {
+    // Nothing here that contributes anything significant when emitting the PCH.
+    return llvm::hash_value(0);
+  }
+};
+    
+      /// Minimum signed int value that can be stored in a Json::Value.
+  static const Int minInt;
+  /// Maximum signed int value that can be stored in a Json::Value.
+  static const Int maxInt;
+  /// Maximum unsigned int value that can be stored in a Json::Value.
+  static const UInt maxUInt;
+    
+    // Find the file which defines an extension extending the given message type
+// with the given field number.
+// Python DescriptorDatabases are not required to implement this method.
+bool PyDescriptorDatabase::FindFileContainingExtension(
+    const string& containing_type, int field_number,
+    FileDescriptorProto* output) {
+  ScopedPyObjectPtr py_method(
+      PyObject_GetAttrString(py_database_, 'FindFileContainingExtension'));
+  if (py_method == NULL) {
+    // This method is not implemented, returns without error.
+    PyErr_Clear();
+    return false;
+  }
+  ScopedPyObjectPtr py_descriptor(
+      PyObject_CallFunction(py_method.get(), 's#i', containing_type.c_str(),
+                            containing_type.size(), field_number));
+  return GetFileDescriptorProto(py_descriptor.get(), output);
 }
     
-    class RenderProcessPreferences
-    : public mate::Wrappable<RenderProcessPreferences> {
- public:
-  static mate::Handle<RenderProcessPreferences>
-      ForAllWebContents(v8::Isolate* isolate);
+      // This is the top-level C++ Message object that owns the whole
+  // proto tree.  Every Python RepeatedScalarContainer holds a
+  // reference to it in order to keep it alive as long as there's a
+  // Python object that references any part of the tree.
+  shared_ptr<Message> owner;
+    
+    #include <google/protobuf/compiler/code_generator.h>
+#include <google/protobuf/compiler/csharp/csharp_field_base.h>
+    
+    namespace google {
+namespace protobuf {
+namespace compiler {
+namespace csharp {
+    }
+    }
+    }
     }
     
+    #endif  // GOOGLE_PROTOBUF_COMPILER_CSHARP_REPEATED_PRIMITIVE_FIELD_H__
     
-    {  return v8::Local<v8::ObjectTemplate>::New(
-      isolate, event_template)->NewInstance();
+    struct Options;
+    
+    // Author: kenton@google.com (Kenton Varda)
+//  Based on original Protocol Buffers design by
+//  Sanjay Ghemawat, Jeff Dean, and others.
+    
+    
+    {}  // namespace google
+#endif  // GOOGLE_PROTOBUF_COMPILER_JAVA_EXTENSION_LITE_H__
+
+    
+        declare.in(frame).out(ptvec);
+    
+        if( !*state )
+        return;
+    
+    OCL_TEST_P(StereoBMFixture, StereoBM)
+{
+    Ptr<StereoBM> bm = StereoBM::create( n_disp, winSize);
+    bm->setPreFilterType(bm->PREFILTER_XSOBEL);
+    bm->setTextureThreshold(0);
+    }
+    
+            cv::Affine3<T>::Vec3 vo;
+        //s = (double)cv::getTickCount();
+        cv::Rodrigues(R, vo);
+        //std::cout << 'O:' <<(cv::getTickCount() - s)*1000/cv::getTickFrequency() << std::endl;
+    
+            gray = cvCreateImage( cvSize( img.width, img.height ), IPL_DEPTH_8U, 1 );
+        thresh = cvCreateImage( cvSize( img.width, img.height ), IPL_DEPTH_8U, 1 );
+        cvCvtColor( &img, gray, CV_BGR2GRAY );
+    
+    TEST(Calib3d_DecomposeProjectionMatrix, accuracy)
+{
+    CV_DecomposeProjectionMatrixTest test;
+    test.safe_run();
+}
+
+    
+    
+    {    bool calc_jacobians;
+    bool test_cpp;
+};
+    
+    void HHVM_FUNCTION(xhprof_frame_begin, const String& name) {
+  Profiler *prof = ThreadInfo::s_threadInfo->m_profiler;
+  if (prof) {
+    s_profiler_factory->cacheString(name);
+    prof->beginFrame(name.data());
+  }
 }
     
+    #include <folly/portability/SysResource.h>
+#include <folly/portability/SysTime.h>
     
-    {}  // namespace atom
+      static auto constexpr kTombstone = kInvalidDataType;
+  static auto constexpr kEmpty = KindOfUninit;
     
-    void AutoUpdater::QuitAndInstall() {
+        size_t GetNumParallelSequencesForFixingBPTTMode()
+    {
+        return 1;
+    }
+    void SetNumParallelSequences(const size_t){};
+    void CopyMBLayoutTo(MBLayoutPtr pMBLayout)
+    {
+        pMBLayout->CopyFrom(m_pMBLayout);
+        NOT_IMPLEMENTED;
+    }
+    virtual const std::map<LabelIdType, LabelType>& GetLabelMapping(const std::wstring& sectionName);
+    virtual void SetLabelMapping(const std::wstring& sectionName, const std::map<typename BinaryReader<ElemType>::LabelIdType, typename BinaryReader<ElemType>::LabelType>& labelMapping);
+    virtual bool GetData(const std::wstring& sectionName, size_t numRecords, void* data, size_t& dataBufferSize, size_t recordStart = 0);
+    
+    
+    {
+    {
+    {
+    {
+    {
+    {
+    {
+    {                    if (EqualInsensitive(functionName, 'CreateModel'))
+                    {
+                        params.insert(params.begin(), key);
+                        CallFunction('CreateModelWithName', params);
+                    }
+                    else if (EqualInsensitive(functionName, 'LoadModel'))
+                    {
+                        params.insert(params.begin(), key);
+                        CallFunction('LoadModelWithName', params);
+                    }
+                    else
+                    { // not a MEL command, so pass it on to NDL
+                        if (!m_netNdlDefault)
+                            RuntimeError('NDL Command cannot be executed until default model is established, cannot set '%s' without a default mode\n Try calling SetDefaultModel(model) before any NDL statement are embedded\n', key.c_str());
+                        HandleNDLInline(stringParse, tokenStart, tokenEnd);
+                    }
+                }
+            }
+            return tokenEnd;
+        }
+        // Function(x,y,z) - function with no return
+        else
+        {
+            std::string value = stringParse.substr(tokenStart, tokenEnd - tokenStart);
+            if (keyEnd > tokenEnd)
+                RuntimeError('Invalid line, expecting function call, %s', value.c_str());
+            std::string functionName;
+            std::string paramList;
+            // Function(x,y,z) - function with no return
+            CallStringParse(value, functionName, paramList);
+            ConfigParamList params(paramList);
+            CallFunction(functionName, params);
+        }
+        return tokenEnd;
+    }
+};
+} } }
+
+    
+    // ---------------------------------------------------------------------------
+// latticesource -- manages loading of lattices for MMI (in pairs for numer and denom)
+// ---------------------------------------------------------------------------
+    
+    
+    {
+    {
+    {    // please add anything else you might need HERE
+};
+};
+};
+
+    
+    bool js_cocos2dx_studio_ComAudio_constructor(JSContext *cx, uint32_t argc, jsval *vp);
+void js_cocos2dx_studio_ComAudio_finalize(JSContext *cx, JSObject *obj);
+void js_register_cocos2dx_studio_ComAudio(JSContext *cx, JS::HandleObject global);
+void register_all_cocos2dx_studio(JSContext* cx, JS::HandleObject obj);
+bool js_cocos2dx_studio_ComAudio_stopAllEffects(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_getEffectsVolume(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_stopEffect(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_getBackgroundMusicVolume(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_willPlayBackgroundMusic(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_setBackgroundMusicVolume(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_end(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_start(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_stopBackgroundMusic(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_pauseBackgroundMusic(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_isBackgroundMusicPlaying(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_isLoop(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_resumeAllEffects(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_pauseAllEffects(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_preloadBackgroundMusic(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_playBackgroundMusic(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_stop(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_playEffect(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_preloadEffect(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_setLoop(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_unloadEffect(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_rewindBackgroundMusic(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_pauseEffect(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_resumeBackgroundMusic(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_setFile(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_setEffectsVolume(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_getFile(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_resumeEffect(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_create(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ComAudio_ComAudio(JSContext *cx, uint32_t argc, jsval *vp);
+    
+    
+    
+    #ifdef __cplusplus
+extern 'C' {
+#endif
+#include 'tolua++.h'
+#ifdef __cplusplus
 }
 #endif
     
-      // net::URLRequestJobFactory::ProtocolHandler:
-  net::URLRequestJob* MaybeCreateJob(
-      net::URLRequest* request,
-      net::NetworkDelegate* network_delegate) const override;
-  bool IsSafeRedirectTarget(const GURL& location) const override;
+    USING_NS_CC;
     
-    #include 'atom/browser/net/js_asker.h'
-#include 'base/memory/ref_counted_memory.h'
-#include 'net/http/http_status_code.h'
-#include 'net/url_request/url_request_simple_job.h'
+    			m_body = m_world->CreateBody(&bd);
+			m_body->CreateFixture(&box, 1.0f);
     
-    /** \brief Lightweight wrapper to tag static string.
- *
- * Value constructor and objectValue member assignement takes advantage of the
- * StaticString and avoid the cost of string duplication when storing the
- * string or the member name.
- *
- * Example of usage:
- * \code
- * Json::Value aValue( StaticString('some text') );
- * Json::Value object;
- * static const StaticString code('code');
- * object[code] = 1234;
- * \endcode
- */
-class JSON_API StaticString {
-public:
-  explicit StaticString(const char* czstring) : c_str_(czstring) {}
-    }
+    JNIEXPORT void JNICALL Java_com_tencent_mars_xlog_Xlog_appenderClose(JNIEnv *env, jobject) {
+	appender_close();
+}
     
-    class Descriptor;
-class FileDescriptor;
-class EnumDescriptor;
-class OneofDescriptor;
-class ServiceDescriptor;
-    
-    struct CMessage;
-struct CMessageClass;
-    
-    class EnumGenerator : public SourceGeneratorBase {
- public:
-  EnumGenerator(const EnumDescriptor* descriptor, const Options* options);
-  ~EnumGenerator();
-    }
-    
-    #include <google/protobuf/compiler/command_line_interface.h>
-#include <google/protobuf/compiler/csharp/csharp_helpers.h>
-#include <google/protobuf/io/zero_copy_stream.h>
-#include <google/protobuf/io/printer.h>
-    
-    class ReflectionClassGenerator : public SourceGeneratorBase {
- public:
-  ReflectionClassGenerator(const FileDescriptor* file, const Options* options);
-  ~ReflectionClassGenerator();
-    }
+    const std::vector<std::string>& DumpCrashStack::StackList() const {
+    return vecdump_;
+}
+
     
     
-    { private:
-  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(RepeatedEnumFieldGenerator);
-};
+    {}
     
-    #endif  // GOOGLE_PROTOBUF_COMPILER_CSHARP_SOURCE_GENERATOR_BASE_H__
+        void* This() const {return m_this;}
     
-    // Generates code for a lite extension, which may be within the scope of some
-// message or may be at file scope.  This is much simpler than FieldGenerator
-// since extensions are just simple identifiers with interesting types.
-class ImmutableExtensionLiteGenerator : public ExtensionGenerator {
- public:
-  explicit ImmutableExtensionLiteGenerator(const FieldDescriptor* descriptor,
-                                           Context* context);
-  virtual ~ImmutableExtensionLiteGenerator();
-    }
-    
-    extern const std::string NIL;
-    
-    void AbstractAuthResolver::setDefaultCred(std::string user,
-                                          std::string password)
+    int TSpy::__TestFun1(int i)
 {
-  defaultUser_ = std::move(user);
-  defaultPassword_ = std::move(password);
+    return reinterpret_cast<Test_Spy_Sample*>(This())->__TestFun1(i);
+}
+    
+    jvalue JNU_CallMethodByMethodInfo(JNIEnv* _env, jobject _obj, JniMethodInfo _method_info, ...);
+    
+    extern pthread_key_t g_env_key;
+    
+    void AbstractBtMessage::setBtMessageDispatcher(BtMessageDispatcher* dispatcher)
+{
+  dispatcher_ = dispatcher;
+}
+    
+      const std::shared_ptr<SocketRecvBuffer>& getSocketRecvBuffer() const
+  {
+    return socketRecvBuffer_;
+  }
+    
+    #include 'AbstractCommand.h'
+    
+    namespace aria2 {
+    }
+    
+    void AnnounceTier::nextEvent()
+{
+  switch (event) {
+  case STARTED:
+    event = DOWNLOADING;
+    break;
+  case STARTED_AFTER_COMPLETION:
+    event = SEEDING;
+    break;
+  case STOPPED:
+    event = HALTED;
+    break;
+  case COMPLETED:
+    event = SEEDING;
+    break;
+  default:
+    break;
+  }
 }
     
     namespace aria2 {
     }
     
-    AbstractBtMessage::~AbstractBtMessage() = default;
-    
-    bool AbstractProxyRequestCommand::executeInternal()
+    std::unique_ptr<AuthConfig> AuthConfig::create(std::string user,
+                                               std::string password)
 {
-  // socket->setBlockingMode();
-  if (httpConnection_->sendBufferIsEmpty()) {
-    auto httpRequest = make_unique<HttpRequest>();
-    httpRequest->setUserAgent(getOption()->get(PREF_USER_AGENT));
-    httpRequest->setRequest(getRequest());
-    httpRequest->setProxyRequest(proxyRequest_);
-    }
-    }
-    
-      virtual ~AbstractProxyRequestCommand();
-    
-    
-    {  void setBtAnnounce(const std::shared_ptr<BtAnnounce>& btAnnounce);
-};
-    
-    #include <memory>
-    
-      AnnounceEvent event;
-  std::deque<std::string> urls;
-    
-    #include 'DiskWriterFactory.h'
-#include 'a2functional.h'
-    
-    class ApiCallbackDownloadEventListener : public DownloadEventListener {
-public:
-  ApiCallbackDownloadEventListener(Session* session,
-                                   DownloadEventCallback callback,
-                                   void* userData);
-  virtual ~ApiCallbackDownloadEventListener();
-  virtual void onEvent(DownloadEvent event,
-                       const RequestGroup* group) CXX11_OVERRIDE;
-    }
-    
-    
-    {} // namespace aria2
+  if (user.empty()) {
+    return nullptr;
+  }
+  else {
+    return make_unique<AuthConfig>(std::move(user), std::move(password));
+  }
+}
