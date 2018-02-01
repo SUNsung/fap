733cd805b387aad9322df3ad3e801997fc569741
SUNsung <weatherius@gmail.com>
2018-02-01 16:16:34 +0200

    Rewrite [057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp] File in: 2018-02-01 16:16:33

--- cccccc/057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp
+++ cccccc/057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp
@@ -1,121 +1,164 @@
-namespace tesseract {
-BOOL8 Tesseract::word_adaptable(  //should we adapt?
-                                WERD_RES *word,
-                                uinT16 mode) {
-  if (tessedit_adaption_debug) {
-    tprintf('Running word_adaptable() for %s rating %.4f certainty %.4f\n',
-          word->best_choice == NULL ? '' :
-          word->best_choice->unichar_string().string(),
-          word->best_choice->rating(), word->best_choice->certainty());
+
+        
+          // Check for input iterator errors
+  if (!iter->status().ok()) {
+    s = iter->status();
   }
-    }
+    
+    #ifndef STORAGE_LEVELDB_DB_BUILDER_H_
+#define STORAGE_LEVELDB_DB_BUILDER_H_
+    
+    // A comparator for internal keys that uses a specified comparator for
+// the user key portion and breaks ties by decreasing sequence number.
+class InternalKeyComparator : public Comparator {
+ private:
+  const Comparator* user_comparator_;
+ public:
+  explicit InternalKeyComparator(const Comparator* c) : user_comparator_(c) { }
+  virtual const char* Name() const;
+  virtual int Compare(const Slice& a, const Slice& b) const;
+  virtual void FindShortestSeparator(
+      std::string* start,
+      const Slice& limit) const;
+  virtual void FindShortSuccessor(std::string* key) const;
     }
     
-    /**
- * @name tess_acceptable_word
- *
- * @return true if the word is regarded as 'good enough'.
- * @param word_choice after context
- * @param raw_choice before context
- */
-bool Tesseract::tess_acceptable_word(WERD_RES* word) {
-  return getDict().AcceptableResult(word);
+    int main(int argc, char** argv) {
+  return leveldb::test::RunAllTests();
+}
+
+    
+      // Evict any entry for the specified file number
+  void Evict(uint64_t file_number);
+    
+    
+    {  edit.SetComparatorName('foo');
+  edit.SetLogNumber(kBig + 100);
+  edit.SetNextFile(kBig + 200);
+  edit.SetLastSequence(kBig + 1000);
+  TestEncodeDecode(edit);
 }
     
-    // The number of points to consider at each end.
-const int kNumEndPoints = 3;
-// The minimum number of points at which to switch to number of points
-// for badly fitted lines.
-// To ensure a sensible error metric, kMinPointsForErrorCount should be at
-// least kMaxRealDistance / (1 - %ile) where %ile is the fractile used in
-// ComputeUpperQuartileError.
-const int kMinPointsForErrorCount = 16;
-// The maximum real distance to use before switching to number of
-// mis-fitted points, which will get square-rooted for true distance.
-const int kMaxRealDistance = 2.0;
-    
-    
-    {  // Best available image.
-  Pix* pix_;
-  // True if the source image is white-on-black.
-  bool inverse_;
-  // Block the word came from. If not null, block->re_rotation() takes the
-  // 'untransformed' coordinates even further back to the original image.
-  // Used only on the first DENORM in a chain.
-  const BLOCK* block_;
-  // Rotation to apply between translation to the origin and scaling.
-  const FCOORD* rotation_;
-  // Previous transformation in a chain.
-  const DENORM* predecessor_;
-  // Non-linear transformation maps directly from each integer offset from the
-  // origin to the corresponding x-coord. Owned by the DENORM.
-  GenericVector<float>* x_map_;
-  // Non-linear transformation maps directly from each integer offset from the
-  // origin to the corresponding y-coord. Owned by the DENORM.
-  GenericVector<float>* y_map_;
-  // x-coordinate to be mapped to final_xshift_ in the result.
-  float x_origin_;
-  // y-coordinate to be mapped to final_yshift_ in the result.
-  float y_origin_;
-  // Scale factors for x and y coords. Applied to pre-rotation system.
-  float x_scale_;
-  float y_scale_;
-  // Destination coords of the x_origin_ and y_origin_.
-  float final_xshift_;
-  float final_yshift_;
-};
-#endif
-
-    
-    #include 'leveldb/env.h'
-    
-     private:
-  Env* const env_;
-  const std::string dbname_;
-  const Options* options_;
-  Cache* cache_;
-    
-          case kNextFileNumber:
-        if (GetVarint64(&input, &next_file_number_)) {
-          has_next_file_number_ = true;
-        } else {
-          msg = 'next file number';
-        }
+    static std::string PrintContents(WriteBatch* b) {
+  InternalKeyComparator cmp(BytewiseComparator());
+  MemTable* mem = new MemTable(cmp);
+  mem->Ref();
+  std::string state;
+  Status s = WriteBatchInternal::InsertInto(b, mem);
+  int count = 0;
+  Iterator* iter = mem->NewIterator();
+  for (iter->SeekToFirst(); iter->Valid(); iter->Next()) {
+    ParsedInternalKey ikey;
+    ASSERT_TRUE(ParseInternalKey(iter->key(), &ikey));
+    switch (ikey.type) {
+      case kTypeValue:
+        state.append('Put(');
+        state.append(ikey.user_key.ToString());
+        state.append(', ');
+        state.append(iter->value().ToString());
+        state.append(')');
+        count++;
         break;
-    
-      void Write(bool sync, Order order, DBState state,
-             int num_entries, int value_size, int entries_per_batch) {
-    // Create new database if state == FRESH
-    if (state == FRESH) {
-      if (FLAGS_use_existing_db) {
-        message_ = 'skipping (--use_existing_db is true)';
-        return;
-      }
-      delete db_;
-      db_ = NULL;
-      Open(sync);
-      Start();  // Do not count time taken to destroy/open
+      case kTypeDeletion:
+        state.append('Delete(');
+        state.append(ikey.user_key.ToString());
+        state.append(')');
+        count++;
+        break;
     }
+    state.append('@');
+    state.append(NumberToString(ikey.sequence));
+  }
+  delete iter;
+  if (!s.ok()) {
+    state.append('ParseError()');
+  } else if (count != WriteBatchInternal::Count(b)) {
+    state.append('CountMismatch()');
+  }
+  mem->Unref();
+  return state;
+}
+    
+    // Returns a new environment that stores its data in memory and delegates
+// all non-file-storage tasks to base_env. The caller must delete the result
+// when it is no longer needed.
+// *base_env must remain live while the result is in use.
+Env* NewMemEnv(Env* base_env);
+    
+    // Dump the contents of the file named by fname in text format to
+// *dst.  Makes a sequence of dst->Append() calls; each call is passed
+// the newline-terminated text corresponding to a single item found
+// in the file.
+//
+// Returns a non-OK result if fname does not name a leveldb storage
+// file, or if the file cannot be read.
+Status DumpFile(Env* env, const std::string& fname, WritableFile* dst);
+    
+    #include 'test_precomp.hpp'
+#include 'opencv2/core/affine.hpp'
+#include 'opencv2/calib3d.hpp'
+#include <iostream>
+    
+        mutable std::vector<Point3f> corners3d;
+private:
+    void generateEdge(const Point3f& p1, const Point3f& p2, std::vector<Point3f>& out) const;
+    Mat generateChessBoard(const Mat& bg, const Mat& camMat, const Mat& distCoeffs,
+        const Point3f& zero, const Point3f& pb1, const Point3f& pb2,
+        float sqWidth, float sqHeight, const std::vector<Point3f>& whole, std::vector<Point2f>& corners) const;
+    void generateBasis(Point3f& pb1, Point3f& pb2) const;
+    
+        void dRv1(mat_t& dr3_dr1, mat_t& dt3_dr1)
+    {
+        dr3_dr1.create(3, 3);     dt3_dr1.create(3, 3);
     }
     
-      DB *db;
-  Options options;
-  options.create_if_missing = true;
-  ASSERT_OK(DB::Open(options, dbpath, &db));
     
     
-    {}  // namespace leveldb
-
+    #if COCOS2D_DEBUG >= 1
+    if (!cobj) 
+    {
+        tolua_error(tolua_S,'invalid 'cobj' in function 'lua_cocos2dx_cocosdenshion_SimpleAudioEngine_playBackgroundMusic'', nullptr);
+        return 0;
+    }
+#endif
     
     
-    { private:
-  const FilterPolicy* policy_;
-  const char* data_;    // Pointer to filter data (at block-start)
-  const char* offset_;  // Pointer to beginning of offset array (at block-end)
-  size_t num_;          // Number of entries in offset array
-  size_t base_lg_;      // Encoding parameter (see kFilterBaseLg in .cc file)
-};
     
-      // The block handle for the metaindex block of the table
-  const BlockHandle& metaindex_handle() const { return metaindex_handle_; }
-  void set_metaindex_handle(const BlockHandle& h) { metaindex_handle_ = h; }
+    
+    {        ok &= luaval_to_vec2(tolua_S, 2, &arg0, 'cc.PhysicsWorld:setGravity');
+        if(!ok)
+        {
+            tolua_error(tolua_S,'invalid arguments in function 'lua_cocos2dx_physics_PhysicsWorld_setGravity'', nullptr);
+            return 0;
+        }
+        cobj->setGravity(arg0);
+        lua_settop(tolua_S, 1);
+        return 1;
+    }
+    luaL_error(tolua_S, '%s has wrong number of arguments: %d, was expecting %d \n', 'cc.PhysicsWorld:setGravity',argc, 1);
+    return 0;
+    
+    			if (settings->drawContactNormals == 1)
+			{
+				b2Vec2 p1 = point->position;
+				b2Vec2 p2 = p1 + k_axisScale * point->normal;
+				m_debugDraw.DrawSegment(p1, p2, b2Color(0.9f, 0.9f, 0.9f));
+			}
+			else if (settings->drawContactImpulse == 1)
+			{
+				b2Vec2 p1 = point->position;
+				b2Vec2 p2 = p1 + k_impulseScale * point->normalImpulse * point->normal;
+				m_debugDraw.DrawSegment(p1, p2, b2Color(0.9f, 0.9f, 0.3f));
+			}
+    
+    
+    {
+    {			body->CreateFixture(&fd);
+		}
+	}
+    
+    // This is used to test sensor shapes.
+class Breakable : public Test
+{
+public:
+    }
