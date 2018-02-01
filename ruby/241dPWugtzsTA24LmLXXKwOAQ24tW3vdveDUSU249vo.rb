a2ac75054066bcae6494904fc855790f41c70544
SUNsung <weatherius@gmail.com>
2018-02-01 18:41:59 +0200

    Rewrite [241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb] File in: 2018-02-01 18:41:58

--- ruby/241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb
+++ ruby/241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb
@@ -1,25 +1,217 @@
-  def render_response
-    render json: {
-      html: view_to_html_string('shared/notifications/_button', notification_setting: @notification_setting),
-      saved: @saved
-    }
-  end
+
+        
+        module Jekyll
+  binding.pry
+end
+
     
-      describe '#message' do
-    msg = /osxfuse is already installed from the binary distribution/
-    its(:message) { is_expected.to match(msg) }
+          theme.create!
+      Jekyll.logger.info 'Your new Jekyll theme, #{theme.name.cyan},' \
+        ' is ready for you in #{theme.path.to_s.cyan}!'
+      Jekyll.logger.info 'For help getting started, read #{theme.path}/README.md.'
+    end
+    # rubocop:enable Metrics/AbcSize
   end
 end
 
     
-            def_node_matcher :redundant_regex?, <<-PATTERN
-          {(send $!nil? {:match :=~} (regexp (str $#literal_at_end?) (regopt)))
-           (send (regexp (str $#literal_at_end?) (regopt)) {:match :=~} $_)}
-        PATTERN
+        # --
+    # Check if a file is a symlink.
+    # NOTE: This can be converted to allowing even in safe,
+    #   since we use Pathutil#in_path? now.
+    # --
+    def symlink?(entry)
+      site.safe && File.symlink?(entry) && symlink_outside_site_source?(entry)
+    end
     
-            def autocorrect(node)
-          # Regexp#match can take a second argument, but this cop doesn't
-          # register an offense in that case
-          return unless node.first_argument.regexp_type?
+        # Gets the name of this layout.
+    attr_reader :name
     
-            private
+        # Returns the current git branch - can be replaced using the environment variable `GIT_BRANCH`
+    def self.git_branch
+      return ENV['GIT_BRANCH'] if ENV['GIT_BRANCH'].to_s.length > 0 # set by Jenkins
+      s = Actions.sh('git rev-parse --abbrev-ref HEAD', log: false).chomp
+      return s.to_s.strip if s.to_s.length > 0
+      nil
+    rescue
+      nil
+    end
+    
+            command = [escaped_gradle_path, 'tasks', '--console=plain'].join(' ')
+        output = Action.sh(command, print_command: false, print_command_output: false)
+        output.split('\n').each do |line|
+          if (result = line.match(/(\w+)\s\-\s([\w\s]+)/))
+            self.tasks << GradleTask.new(title: result[1], description: result[2])
+          end
+        end
+    
+            expect(result).to eq('svn info | grep Revision | egrep -o '[0-9]+'')
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('svn info | grep Revision | egrep -o '[0-9]+'')
+      end
+    end
+    
+          it 'gets the correct version number for 'TargetA'' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          get_version_number(xcodeproj: '.xcproject', target: 'TargetA')
+        end').runner.execute(:test)
+        expect(result).to eq('4.3.2')
+      end
+    
+          it 'properly removes new lines of the build number' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          increment_build_number(build_number: '24\n', xcodeproj: '.xcproject')
+        end').runner.execute(:test)
+    
+      def test_enable
+    @opt.parse!(%w'--enable-foo')
+    assert_equal(true, @foo)
+    @opt.parse!(%w'--enable-bar')
+    assert_equal(true, @bar)
+  end
+    
+      def test_permute
+    assert_equal(%w'', no_error {@opt.permute!(%w'')})
+    assert_equal(self.class, @flag)
+    assert_equal(%w'foo bar', no_error {@opt.permute!(%w'foo bar')})
+    assert_equal(self.class, @flag)
+    assert_equal(%w'- foo bar', no_error {@opt.permute!(%w'- foo bar')})
+    assert_equal(self.class, @flag)
+    assert_equal(%w'foo bar', no_error {@opt.permute!(%w'-- foo bar')})
+    assert_equal(self.class, @flag)
+    assert_equal(%w'foo - bar', no_error {@opt.permute!(%w'foo - bar')})
+    assert_equal(self.class, @flag)
+    assert_equal(%w'foo bar', no_error {@opt.permute!(%w'foo -- bar')})
+    assert_equal(self.class, @flag)
+    assert_equal(%w'foo --help bar', no_error {@opt.permute!(%w'foo -- --help bar')})
+    assert_equal(self.class, @flag)
+  end
+    
+      #
+  # Returns a string which represents the time as date-time defined by RFC 2822:
+  #
+  #   day-of-week, DD month-name CCYY hh:mm:ss zone
+  #
+  # where zone is [+-]hhmm.
+  #
+  # If +self+ is a UTC time, -0000 is used as zone.
+  #
+  # You must require 'time' to use this method.
+  #
+  def rfc2822
+    sprintf('%s, %02d %s %0*d %02d:%02d:%02d ',
+      RFC2822_DAY_NAME[wday],
+      day, RFC2822_MONTH_NAME[mon-1], year < 0 ? 5 : 4, year,
+      hour, min, sec) <<
+    if utc?
+      '-0000'
+    else
+      off = utc_offset
+      sign = off < 0 ? '-' : '+'
+      sprintf('%s%02d%02d', sign, *(off.abs / 60).divmod(60))
+    end
+  end
+  alias rfc822 rfc2822
+    
+        def split_arguments(arguments, sep=',')
+      return [] if arguments.strip == 'void'
+      arguments.scan(/([\w\*\s]+\(\*\w*\)\(.*?\)|[\w\*\s\[\]]+)(?:#{sep}\s*|$)/).collect {|m| m[0]}
+    end
+    
+      self.each_test do |test, i|
+    define_method('test_#{i}') do ||
+      Tempfile.create('iotest.dat') do |fh|
+        fh.print test[1]
+        fh.rewind
+        assert_equal(test[2], fh.scanf(test[0]))
+      end
+    end
+  end
+end
+
+    
+          def def_csv_accessor(klass, full_name)
+        klass.def_csv_element(full_name)
+      end
+    
+      def test_font_helper_without_suffix
+    assert_match %r(url\(['']?/assets/.*eot['']?\)), @css
+  end
+    
+          File.open('bower.json', 'w') do |f|
+        f.puts JSON.pretty_generate(spec)
+      end
+    end
+  end
+end
+
+    
+      def collection_presenter
+    page = ActivityPub::CollectionPresenter.new(
+      id: account_following_index_url(@account, page: params.fetch(:page, 1)),
+      type: :ordered,
+      size: @account.following_count,
+      items: @follows.map { |f| ActivityPub::TagManager.instance.uri_for(f.target_account) },
+      part_of: account_following_index_url(@account),
+      next: page_url(@follows.next_page),
+      prev: page_url(@follows.prev_page)
+    )
+    if params[:page].present?
+      page
+    else
+      ActivityPub::CollectionPresenter.new(
+        id: account_following_index_url(@account),
+        type: :ordered,
+        size: @account.following_count,
+        first: page
+      )
+    end
+  end
+end
+
+    
+      class MediaAttachmentSerializer < ActiveModel::Serializer
+    include RoutingHelper
+    
+          out =
+        Sass::Util.silence_sass_warnings do
+          if @options[:from] == :css
+            require 'sass/css'
+            Sass::CSS.new(read(input), @options[:for_tree]).render(@options[:to])
+          else
+            if input_path
+              Sass::Engine.for_file(input_path, @options[:for_engine])
+            else
+              Sass::Engine.new(read(input), @options[:for_engine])
+            end.to_tree.send('to_#{@options[:to]}', @options[:for_tree])
+          end
+        end
+    
+          # This is optional for backwards-compatibility with Sass 3.3, which didn't
+      # enable sourcemaps by default and instead used '--sourcemap' to do so.
+      opts.on(:OPTIONAL, '--sourcemap=TYPE',
+          'How to link generated output to the source files.',
+          '  auto (default): relative paths where possible, file URIs elsewhere',
+          '  file: always absolute file URIs',
+          '  inline: include the source text in the sourcemap',
+          '  none: no sourcemaps') do |type|
+        if type && !%w(auto file inline none).include?(type)
+          $stderr.puts 'Unknown sourcemap type #{type}.\n\n'
+          $stderr.puts opts
+          exit
+        elsif type.nil?
+          Sass::Util.sass_warn <<MESSAGE.rstrip
+DEPRECATION WARNING: Passing --sourcemap without a value is deprecated.
+Sourcemaps are now generated by default, so this flag has no effect.
+MESSAGE
+        end
+    
+        # Starts the read-eval-print loop.
+    def run
+      environment = Environment.new
+      @line = 0
+      loop do
+        @line += 1
+        unless (text = Readline.readline('>> '))
+          puts
+          return
+        end
