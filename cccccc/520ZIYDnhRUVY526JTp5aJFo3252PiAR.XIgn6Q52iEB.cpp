2736b427baf9b12887dd9d292114e1628bcb09e9
SUNsung <weatherius@gmail.com>
2018-02-02 07:41:20 +0200

    Rewrite [520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp] File in: 2018-02-02 07:41:20

--- cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
+++ cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
@@ -1,7 +1,207 @@
 
         
-        namespace leveldb {
+        class DebugGraphDecorator : public DebugGraphDecoratorInterface {
+ public:
+  DebugGraphDecorator(const DebugOptions& debug_options)
+      : debug_options_(debug_options) {}
+  virtual ~DebugGraphDecorator() {}
     }
+    
+    class TestRandomAccessFile : public RandomAccessFile {
+  // The file contents is 10 bytes of all A's
+  Status Read(uint64 offset, size_t n, StringPiece* result,
+              char* scratch) const override {
+    Status s;
+    for (int i = 0; i < n; ++i) {
+      if (offset + i >= 10) {
+        n = i;
+        s = errors::OutOfRange('EOF');
+        break;
+      }
+      scratch[i] = 'A';
+    }
+    *result = StringPiece(scratch, n);
+    return s;
+  }
+};
+    
+    
+    {
+    {  PersistentTensor handle_ GUARDED_BY(mu_);
+};
+}  // namespace tensorflow
+    
+    #include 'tensorflow/core/grappler/optimizers/graph_optimizer.h'
+    
+      // Sets the appropriate library kind to that passed in.
+  PluginConfig& SetBlas(PluginId blas);
+  PluginConfig& SetDnn(PluginId dnn);
+  PluginConfig& SetFft(PluginId fft);
+  PluginConfig& SetRng(PluginId rng);
+    
+      ExpectFailure(
+      Builder().Input(FakeInput(2, DT_INT32)).Input(FakeInput(2, DT_STRING)),
+      'Inconsistent values for attr 'T' DT_INT32 vs. DT_STRING while');
+    
+    
+    {
+    {
+    {      // Process the node 'out'.
+      if (IsEnter(out)) {
+        if (is_visited) {
+          const string& parent_frame = (*info)[out_parent->id()].frame_name;
+          if (parent_frame != frame_name) {
+            return errors::InvalidArgument(
+                'The node '', out->name(),
+                '' has inputs from different '
+                'frames. The input '',
+                curr_node->name(), '' is in frame '', frame_name,
+                ''. The input '', parent_nodes[out->id()]->name(),
+                '' is in frame '', parent_frame, ''.');
+          }
+        } else {
+          out_info->frame = out;
+          out_info->parent_frame = frame;
+          TF_RETURN_IF_ERROR(
+              GetNodeAttr(out->attrs(), 'frame_name', &out_info->frame_name));
+          if (out_info->frame_name.empty()) {
+            return errors::InvalidArgument('The Enter node ', out->name(),
+                                           ' must have a frame name.');
+          }
+        }
+      } else {
+        if (is_visited) {
+          if (out_info->frame_name != frame_name) {
+            return errors::InvalidArgument(
+                'The node '', out->name(),
+                '' has inputs from different '
+                'frames. The input '',
+                curr_node->name(), '' is in frame '', frame_name,
+                ''. The input '', parent_nodes[out->id()]->name(),
+                '' is in frame '', out_info->frame_name, ''.');
+          }
+        } else {
+          out_info->frame = frame;
+          out_info->parent_frame = parent;
+          out_info->frame_name = frame_name;
+        }
+      }
+    }
+  }
+  return Status::OK();
+}
+    
+    #include 'tensorflow/core/lib/core/status.h'
+#include 'tensorflow/core/lib/core/stringpiece.h'
+#if !defined(IS_SLIM_BUILD)
+#include 'tensorflow/core/lib/io/zlib_compression_options.h'
+#include 'tensorflow/core/lib/io/zlib_outputbuffer.h'
+#endif  // IS_SLIM_BUILD
+#include 'tensorflow/core/platform/macros.h'
+#include 'tensorflow/core/platform/types.h'
+    
+    namespace internal {
+template <typename Scalar>
+struct functor_traits<scalar_clip_op<Scalar> > {
+  enum {
+    Cost = NumTraits<Scalar>::AddCost * 3,
+    PacketAccess = packet_traits<Scalar>::HasMax &&
+                   packet_traits<Scalar>::HasMin &&
+                   packet_traits<Scalar>::HasNegate
+  };
+};
+}  // namespace internal
+    
+    REGISTER_KERNEL_BUILDER(Name('TextLineReader').Device(DEVICE_CPU),
+                        TextLineReaderOp);
+REGISTER_KERNEL_BUILDER(Name('TextLineReaderV2').Device(DEVICE_CPU),
+                        TextLineReaderOp);
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+      // If instruction is part of inputs, don't reset the bit_vector.
+  if (std::find(inputs.begin(), inputs.end(), instruction) == inputs.end()) {
+    bit_vector.SetToZero();
+  }
+  bit_vector.Set(GetIndex(instruction));
+  for (const HloInstruction* input : inputs) {
+    bit_vector.OrWith(GetBitVector(input));
+  }
+    
+      /// DebuggerClient is asked to emit SIL references to locals,
+  /// permitting SILGen to access them like any other variables.
+  /// This avoids generation of properties.
+  virtual SILValue emitLValueForVariable(VarDecl *var,
+                                         SILBuilder &builder) = 0;
+    
+    void
+SyntaxASTMap::recordSyntaxMapping(RC<syntax::SyntaxData> FromNode,
+                                  ASTNode ToNode) {
+  if (FromNode->getKind() == SyntaxKind::Unknown) {
+    return;
+  }
+    }
+    
+    static inline void _swift_dispatch_after(
+    dispatch_time_t when,
+    dispatch_queue_t queue,
+    __swift_shims_dispatch_block_t block) {
+  dispatch_after(when, queue, block);
+}
+    
+        // Finish and check for builder errors
+    if (s.ok()) {
+      s = builder->Finish();
+      if (s.ok()) {
+        meta->file_size = builder->FileSize();
+        assert(meta->file_size > 0);
+      }
+    } else {
+      builder->Abandon();
+    }
+    delete builder;
+    
+    class Env;
+class Iterator;
+class TableCache;
+class VersionEdit;
+    
+    namespace leveldb {
+    }
+    
+    // Return a new iterator that converts internal keys (yielded by
+// '*internal_iter') that were live at the specified 'sequence' number
+// into appropriate user keys.
+extern Iterator* NewDBIterator(
+    DBImpl* db,
+    const Comparator* user_key_comparator,
+    Iterator* internal_iter,
+    SequenceNumber sequence,
+    uint32_t seed);
+    
+    void InternalKeyComparator::FindShortestSeparator(
+      std::string* start,
+      const Slice& limit) const {
+  // Attempt to shorten the user portion of the key
+  Slice user_start = ExtractUserKey(*start);
+  Slice user_limit = ExtractUserKey(limit);
+  std::string tmp(user_start.data(), user_start.size());
+  user_comparator_->FindShortestSeparator(&tmp, user_limit);
+  if (tmp.size() < user_start.size() &&
+      user_comparator_->Compare(user_start, tmp) < 0) {
+    // User key has become shorter physically, but larger logically.
+    // Tack on the earliest possible number to the shortened user key.
+    PutFixed64(&tmp, PackSequenceAndType(kMaxSequenceNumber,kValueTypeForSeek));
+    assert(this->Compare(*start, tmp) < 0);
+    assert(this->Compare(tmp, limit) < 0);
+    start->swap(tmp);
+  }
+}
     
     // Called on every log record (each one of which is a WriteBatch)
 // found in a kDescriptorFile.
@@ -20,109 +220,98 @@ static void VersionEditPrinter(uint64_t pos, Slice record, WritableFile* dst) {
   dst->Append(r);
 }
     
-    Status SetCurrentFile(Env* env, const std::string& dbname,
-                      uint64_t descriptor_number) {
-  // Remove leading 'dbname/' and add newline to manifest file name
-  std::string manifest = DescriptorFileName(dbname, descriptor_number);
-  Slice contents = manifest;
-  assert(contents.starts_with(dbname + '/'));
-  contents.remove_prefix(dbname.size() + 1);
-  std::string tmp = TempFileName(dbname, descriptor_number);
-  Status s = WriteStringToFileSync(env, contents.ToString() + '\n', tmp);
-  if (s.ok()) {
-    s = env->RenameFile(tmp, CurrentFileName(dbname));
-  }
-  if (!s.ok()) {
-    env->DeleteFile(tmp);
-  }
-  return s;
+    // Return the legacy file name for an sstable with the specified number
+// in the db named by 'dbname'. The result will be prefixed with
+// 'dbname'.
+extern std::string SSTTableFileName(const std::string& dbname, uint64_t number);
+    
+            // Bind KV values into replace_stmt
+        status = sqlite3_bind_blob(replace_stmt, 1, key, 16, SQLITE_STATIC);
+        ErrorCheck(status);
+        status = sqlite3_bind_blob(replace_stmt, 2, value,
+                                   value_size, SQLITE_STATIC);
+        ErrorCheck(status);
+    
+    
+// //////////////////////////////////////////////////////////////////////
+// Beginning of content of file: include/json/value.h
+// //////////////////////////////////////////////////////////////////////
+    
+    namespace enum_descriptor {
+PyObject* NewEnumValuesByName(const EnumDescriptor* descriptor);
+PyObject* NewEnumValuesByNumber(const EnumDescriptor* descriptor);
+PyObject* NewEnumValuesSeq(const EnumDescriptor* descriptor);
+}  // namespace enum_descriptor
+    
+    #include <google/protobuf/stubs/logging.h>
+#include <google/protobuf/stubs/common.h>
+#include <google/protobuf/descriptor.pb.h>
+#include <google/protobuf/pyext/message.h>
+#include <google/protobuf/pyext/scoped_pyobject_ptr.h>
+    
+    
+    {
+    {
+    {}  // namespace cpp
+}  // namespace compiler
+}  // namespace protobuf
+    
+    void RepeatedMessageFieldGenerator::GenerateMergingCode(io::Printer* printer) {
+  printer->Print(
+    variables_,
+    '$name$_.Add(other.$name$_);\n');
 }
     
-    WriteBatch::WriteBatch() {
-  Clear();
-}
+      virtual void WriteHash(io::Printer* printer);
+  virtual void WriteEquals(io::Printer* printer);
+  virtual void WriteToString(io::Printer* printer);
     
-    inline
-static void WalCheckpoint(sqlite3* db_) {
-  // Flush all writes to disk
-  if (FLAGS_WAL_enabled) {
-    sqlite3_wal_checkpoint_v2(db_, NULL, SQLITE_CHECKPOINT_FULL, NULL, NULL);
+    void WriteMessageDocComment(io::Printer* printer, const Descriptor* message);
+void WriteFieldDocComment(io::Printer* printer, const FieldDescriptor* field);
+void WriteEnumDocComment(io::Printer* printer, const EnumDescriptor* enum_);
+void WriteEnumValueDocComment(io::Printer* printer,
+                              const EnumValueDescriptor* value);
+void WriteServiceDocComment(io::Printer* printer,
+                            const ServiceDescriptor* service);
+void WriteMethodDocComment(io::Printer* printer,
+                           const MethodDescriptor* method);
+    
+    
+    {  name += UNLV_EXT;              //add extension
+  if ((pdfp = fopen (name.string (), 'rb')) == NULL) {
+    return false;                //didn't read one
+  } else {
+    while (tfscanf(pdfp, '%d %d %d %d %*s', &x, &y, &width, &height) >= 4) {
+                                 //make rect block
+      block = new BLOCK (name.string (), TRUE, 0, 0,
+                         (inT16) x, (inT16) (ysize - y - height),
+                         (inT16) (x + width), (inT16) (ysize - y));
+                                 //on end of list
+      block_it.add_to_end (block);
+    }
+    fclose(pdfp);
   }
+  return true;
 }
     
-    
-    {}  // namespace leveldb
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    		// Define platform
-		{
-			b2BodyDef bd;
-			bd.type = b2_dynamicBody;
-			bd.position.Set(-4.0f, 5.0f);
-			m_platform = m_world->CreateBody(&bd);
+    inline ICOORD
+operator- (                      //subtract vectors
+const ICOORD & op1,              //operands
+const ICOORD & op2) {
+  ICOORD sum;                    //result
     }
     
-    	static Test* Create()
-	{
-		return new Breakable;
-	}
     
-    bool ReadJpeg(const std::string& data, JpegReadMode mode,
-              JPEGData* jpg) {
-  return ReadJpeg(reinterpret_cast<const uint8_t*>(data.data()),
-                  static_cast<const size_t>(data.size()),
-                  mode, jpg);
-}
+// ============== Private Code ======================
     
-    size_t HistogramHeaderCost(const JpegHistogram& histo) {
-  size_t header_bits = 17 * 8;
-  for (int i = 0; i + 1 < JpegHistogram::kSize; ++i) {
-    if (histo.counts[i] > 0) {
-      header_bits += 8;
-    }
-  }
-  return header_bits;
-}
-    
-    
-    {}  // namespace guetzli
-    
-    // Entropy encoding (Huffman) utilities.
-    
-    inline int Log2FloorNonZero(uint32_t n) {
-#ifdef __GNUC__
-  return 31 ^ __builtin_clz(n);
-#else
-  unsigned int result = 0;
-  while (n >>= 1) result++;
-  return result;
-#endif
-}
-    
-    // Computes the DCT (Discrete Cosine Transform) of the 8x8 array in 'block',
-// scaled up by a factor of 16. The values in 'block' are laid out row-by-row
-// and the result is written to the same memory area.
-void ComputeBlockDCT(coeff_t* block);
-    
-      tmp0 = in[2 * stride];
-  tmp1 = kIDCTMatrix[ 2] * tmp0;
-  tmp2 = kIDCTMatrix[10] * tmp0;
-  out[0] += tmp1;
-  out[1] += tmp2;
-  out[2] -= tmp2;
-  out[3] -= tmp1;
-  out[4] -= tmp1;
-  out[5] -= tmp2;
-  out[6] += tmp2;
-  out[7] += tmp1;
-    
-    #include 'guetzli/jpeg_data.h'
+    // Refreshes the words in the block_list by using blobs in the
+// new_blobs list.
+// Block list must have word segmentation in it.
+// It consumes the blobs provided in the new_blobs list. The blobs leftover in
+// the new_blobs list after the call weren't matched to any blobs of the words
+// in block list.
+// The output not_found_blobs is a list of blobs from the original segmentation
+// in the block_list for which no corresponding new blobs were found.
+void RefreshWordBlobsFromNewBlobs(BLOCK_LIST* block_list,
+                                  C_BLOB_LIST* new_blobs,
+                                  C_BLOB_LIST* not_found_blobs);
