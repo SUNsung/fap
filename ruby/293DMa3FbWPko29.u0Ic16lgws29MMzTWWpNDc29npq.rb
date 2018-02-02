77ef9c5fac9c65370f3b040f7480f9730634f229
SUNsung <weatherius@gmail.com>
2018-02-02 12:14:06 +0200

    Rewrite [293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb] File in: 2018-02-02 12:14:06

--- ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
+++ ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
@@ -1,44 +1,69 @@
 
         
-            def initialize
-      clear
-    end
+        # Create an array of vendored mimetype => [extensions]
+mimes = {}
+json = open('https://raw.githubusercontent.com/jshttp/mime-db/master/db.json').read
+data = JSON.parse(json)
+data.reject! { |mime, meta| meta['extensions'].nil? || meta['extensions'].empty? }
+data.each do |mime, meta|
+  # Normalize extensions and mime-types
+  mime = mime.downcase.strip
+  extensions = meta['extensions'].map { |e| e.downcase.strip }.compact
     
-        def calculated_type_matches
-      possible_types.select do |content_type|
-        content_type == type_from_file_contents
-      end
-    end
+          # Group an array of items by an expression
+      #
+      # input - the object array
+      # variable - the variable to assign each item to in the expression
+      # expression -a Liquid comparison expression passed in as a string
+      #
+      # Returns the filtered array of objects
+      def group_by_exp(input, variable, expression)
+        return input unless groupable?(input)
     
-        # Swaps the height and width if necessary
-    def auto_orient
-      if EXIF_ROTATED_ORIENTATION_VALUES.include?(@orientation)
-        @height, @width = @width, @height
-        @orientation -= 4
-      end
-    end
+    profile = Profile.new
+# puts profile.generate
+command = ['/usr/bin/sandbox-exec', '-p', profile.generate, profile.pod_bin, *ARGV]
+exec(*command)
+
     
-        # Returns an integer timestamp that is time zone-neutral, so that paths
-    # remain valid even if a server's time zone changes.
-    def updated_at attachment, style_name
-      attachment.updated_at
-    end
-    
-            def failure_message_when_negated
-          'Should not have an attachment named #{@attachment_name}'
+            def self.options
+          [[
+            '--all', 'Remove all the cached pods without asking'
+          ]].concat(super)
         end
-        alias negative_failure_message failure_message_when_negated
     
-            protected
+            def signal_end_of_output
+          output_pipe.puts(END_OF_OUTPUT_SIGNAL)
+          STDOUT.flush
+        end
     
+        def self.register(klass, attachment_name, attachment_options)
+      instance.register(klass, attachment_name, attachment_options)
+    end
     
-    {  # Returns hash with styles missing from recent run of rake paperclip:refresh:missing_styles
-  #   {
-  #     :User => {:avatar => [:big]},
-  #     :Book => {
-  #       :cover => [:croppable]},
-  #     }
-  #   }
-  def self.missing_attachments_styles
-    current_styles = current_attachments_styles
-    registered_styles = get_registered_attachments_styles
+          @klass.send :define_method, @name do |*args|
+        ivar = '@attachment_#{name}'
+        attachment = instance_variable_get(ivar)
+    
+        # Returns the id of the instance.
+    def id attachment, style_name
+      attachment.instance.id
+    end
+    
+    module Paperclip
+  class << self
+    attr_writer :registered_attachments_styles_path
+    def registered_attachments_styles_path
+      @registered_attachments_styles_path ||= Rails.root.join('public/system/paperclip_attachments.yml').to_s
+    end
+  end
+    
+        logger.info('Installing pear package', :package => input_package,
+                  :directory => staging_path)
+    ::Dir.chdir(staging_path) do
+      safesystem('pear', '-c', config, 'install', '-n', '-f', input_package)
+    end
+    
+      option '--user', 'USER',
+    'Set the user to USER in the prototype files.',
+    :default => 'root'
