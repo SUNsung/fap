fd165d67a059a852d8119549f9eacf7ae89f07ce
SUNsung <weatherius@gmail.com>
2018-02-02 04:58:53 +0200

    Rewrite [45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb] File in: 2018-02-02 04:58:53

--- ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
+++ ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
@@ -1,17 +1,71 @@
 
         
-                    sequence_base := (
-              'x' ||
-              -- Take the first two bytes (four hex characters)
-              substr(
-                -- Of the MD5 hash of the data we documented
-                md5(table_name ||
-                  '#{SecureRandom.hex(16)}' ||
-                  time_part::text
-                ),
-                1, 4
-              )
-            -- And turn it into a bigint
-            )::bit(16)::bigint;
+          def present(payload)
+    if payload.is_a?(Hash)
+      payload = ActiveSupport::HashWithIndifferentAccess.new(payload)
+      MAIN_KEYS.each do |key|
+        return { :title => payload[key].to_s, :entries => present_hash(payload, key) } if payload.has_key?(key)
+      end
     
-      attributes :id, :type, :name, :updated
+      def evernote_oauth_token
+    service && service.token
+  end
+end
+
+    
+        respond_to do |format|
+      format.html { redirect_back events_path, notice: 'Event deleted.' }
+      format.json { head :no_content }
+    end
+  end
+    
+        respond_to do |format|
+      if !running? && @job.destroy
+        format.html { redirect_to jobs_path, notice: 'Job deleted.' }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to jobs_path, alert: 'Can not delete a running job.' }
+        format.json { render json: @job.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+    
+            # Defines a capability for the given provider. The block should return
+        # a class/module that has a method with the capability name, ready
+        # to be executed. This means that if it is an instance method,
+        # the block should return an instance of the class.
+        #
+        # @param [String] provider The name of the provider
+        # @param [String] cap The name of the capability
+        def self.provider_capability(provider, cap, &block)
+          components.provider_capabilities[provider.to_sym].register(cap.to_sym, &block)
+          nil
+        end
+    
+          def markdown_podfile
+        UI::ErrorReport.markdown_podfile
+      end
+    
+            # Checks if a template URL is given else returns the TEMPLATE_REPO URL
+        #
+        # @return String
+        #
+        def template_repo_url
+          @template_url || TEMPLATE_REPO
+        end
+      end
+    end
+  end
+end
+
+    
+    namespace :gem do
+  def version
+    require 'spree/core/version'
+    Spree.version
+  end
+    
+            def show
+          @stock_movement = scope.find(params[:id])
+          respond_with(@stock_movement)
+        end
