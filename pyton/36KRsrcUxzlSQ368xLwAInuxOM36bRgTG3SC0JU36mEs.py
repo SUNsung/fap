f5fcbaee044508ee56de9608015d43f92611eea0
SUNsung <weatherius@gmail.com>
2018-02-02 14:16:45 +0200

    Rewrite [36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py] File in: 2018-02-02 14:16:45

--- pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
+++ pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
@@ -1,112 +1,148 @@
 
         
-                request_line = '{method} {path}{query} HTTP/1.1'.format(
-            method=self._orig.method,
-            path=url.path or '/',
-            query='?' + url.query if url.query else ''
+                def get_auth(self, username=None, password=None):
+            assert self.raw_auth is None
+            assert username is None
+            assert password is None
+            return basic_auth()
+    
+            '''
+        available_plugins = plugin_manager.get_formatters_grouped()
+        self.enabled_plugins = []
+        for group in groups:
+            for cls in available_plugins[group]:
+                p = cls(env=env, **kwargs)
+                if p.enabled:
+                    self.enabled_plugins.append(p)
+    
+    
+@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
+@pytest.mark.parametrize('filename', filenames)
+def test_rst_file_syntax(filename):
+    p = subprocess.Popen(
+        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
+    err = p.communicate()[1]
+    assert p.returncode == 0, err.decode('utf8')
+
+    
+            self.status.started(
+            resumed_from=self._resumed_from,
+            total_size=total_size
         )
     
-        exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR
-    assert error_msg == (
-        'ConnectionError: '
-        'Connection aborted while doing GET request to URL: '
-        'http://www.google.com')
+    from httpie import ExitStatus
+from httpie.input import SSL_VERSION_ARG_MAPPING
+from utils import http, HTTP_OK, TESTS_ROOT
+    
+        >>> humanize_bytes(1)
+    '1 B'
+    >>> humanize_bytes(1024, precision=1)
+    '1.0 kB'
+    >>> humanize_bytes(1024 * 123, precision=1)
+    '123.0 kB'
+    >>> humanize_bytes(1024 * 12342, precision=1)
+    '12.1 MB'
+    >>> humanize_bytes(1024 * 12342, precision=2)
+    '12.05 MB'
+    >>> humanize_bytes(1024 * 1234, precision=2)
+    '1.21 MB'
+    >>> humanize_bytes(1024 * 1234 * 1111, precision=2)
+    '1.31 GB'
+    >>> humanize_bytes(1024 * 1234 * 1111, precision=1)
+    '1.3 GB'
+    
+            # FIXME: False positive, e.g., 'localhost' matches but is a valid URL.
+        elif not re.match('^[a-zA-Z]+$', self.args.method):
+            # Invoked as `http URL item+'. The URL is now in `args.method`
+            # and the first ITEM is now incorrectly in `args.url`.
+            try:
+                # Parse the URL as an ITEM and store it as the first ITEM arg.
+                self.args.items.insert(0, KeyValueArgType(
+                    *SEP_GROUP_ALL_ITEMS).__call__(self.args.url))
+    
+        parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--estimators', nargs='+', required=True,
+                        choices=ESTIMATORS)
+    args = vars(parser.parse_args())
     
     
-def test_follow_redirect_output_options(httpbin):
-    r = http('--check-status',
-             '--follow',
-             '--all',
-             '--print=h',
-             '--history-print=H',
-             httpbin.url + '/redirect/2')
-    assert r.count('GET /') == 2
-    assert 'HTTP/1.1 302 FOUND' not in r
-    assert HTTP_OK in r
+def plot_feature_errors(all_errors, batch_size, all_components, data):
+    plt.figure()
+    plot_results(all_components, all_errors['pca'], label='PCA')
+    plot_results(all_components, all_errors['ipca'],
+                 label='IncrementalPCA, bsize=%i' % batch_size)
+    plot_results(all_components, all_errors['rpca'], label='RandomizedPCA')
+    plt.legend(loc='lower left')
+    plt.suptitle('Algorithm error vs. n_components\n'
+                 'LFW, size %i x %i' % data.shape)
+    plt.xlabel('Number of components (out of max %i)' % data.shape[1])
+    plt.ylabel('Mean absolute error')
     
-        msg = Message('Content-Disposition: %s' % content_disposition)
-    filename = msg.get_filename()
-    if filename:
-        # Basic sanitation.
-        filename = os.path.basename(filename).lstrip('.').strip()
-        if filename:
-            return filename
+        for i, DD in enumerate(Drange):
+        print('D = %i (%i out of %i)' % (DD, i + 1, len(Drange)))
+        X = get_data(N, DD, dataset)
+        for algorithm in algorithms:
+            nbrs = neighbors.NearestNeighbors(n_neighbors=k,
+                                              algorithm=algorithm,
+                                              leaf_size=leaf_size)
+            t0 = time()
+            nbrs.fit(X)
+            t1 = time()
+            nbrs.kneighbors(X)
+            t2 = time()
     
-    from httpie.plugins.base import AuthPlugin
-    
-        def test_self_signed_server_cert_by_default_raises_ssl_error(
-            self,
-            httpbin_secure_untrusted):
-        with pytest.raises(SSLError):
-            http(httpbin_secure_untrusted.url + '/get')
-    
-        Assumes `from __future__ import division`.
-    
-        def inner(*suffix):
-        return urljoin(httpbin_url, '/'.join(suffix))
-    
-        def __setitem__(self, key, value):
-        # Use the lowercased key for lookups, but store the actual
-        # key alongside the value.
-        self._store[key.lower()] = (key, value)
-    
-        ...what? Basically, expose the parsed HTTP headers from the server response
-    the way `cookielib` expects to see them.
-    '''
-    
-                    low_conn = conn._get_conn(timeout=DEFAULT_POOL_TIMEOUT)
+        clf = clone(transfomer)
     
     
-def _parse_content_type_header(header):
-    '''Returns content type and parameters from given header
+if __name__ == '__main__':
+    # NOTE: we put the following in a 'if __name__ == '__main__'' protected
+    # block to be able to use a multi-core grid search that also works under
+    # Windows, see: http://docs.python.org/library/multiprocessing.html#windows
+    # The multiprocessing module is used as the backend of joblib.Parallel
+    # that is used when n_jobs != 1 in GridSearchCV
     
-    # run scipy-based optimization (L-BFGS) over the pixels of the generated image
-# so as to minimize the neural style loss
-x = preprocess_image(base_image_path)
+    # Learn a frontier for outlier detection with several classifiers
+xx1, yy1 = np.meshgrid(np.linspace(-8, 28, 500), np.linspace(3, 40, 500))
+xx2, yy2 = np.meshgrid(np.linspace(3, 10, 500), np.linspace(-5, 45, 500))
+for i, (clf_name, clf) in enumerate(classifiers.items()):
+    plt.figure(1)
+    clf.fit(X1)
+    Z1 = clf.decision_function(np.c_[xx1.ravel(), yy1.ravel()])
+    Z1 = Z1.reshape(xx1.shape)
+    legend1[clf_name] = plt.contour(
+        xx1, yy1, Z1, levels=[0], linewidths=2, colors=colors[i])
+    plt.figure(2)
+    clf.fit(X2)
+    Z2 = clf.decision_function(np.c_[xx2.ravel(), yy2.ravel()])
+    Z2 = Z2.reshape(xx2.shape)
+    legend2[clf_name] = plt.contour(
+        xx2, yy2, Z2, levels=[0], linewidths=2, colors=colors[i])
     
-        assert success_result == 1
-    partial_result = K.eval(
-        metrics.sparse_top_k_categorical_accuracy(y_true, y_pred, k=2))
+    from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn import datasets
     
-        # test lambda with output_mask lambda
-    input_a = layers.Input(shape=input_shapes[0][1:])
-    input_b = layers.Input(shape=input_shapes[1][1:])
-    a = layers.Masking()(input_a)
-    b = layers.Masking()(input_b)
-    merged = legacy_layers.merge(
-        [a, b], mode=lambda tup: K.concatenate([tup[0], tup[1]], axis=1),
-        output_shape=lambda tup: (tup[0][0], tup[0][1] + tup[1][1]) + tup[0][2:],
-        output_mask=lambda tup: K.concatenate([tup[0], tup[1]]))
-    model = models.Model([input_a, input_b], merged)
-    expected_output_shape = model.compute_output_shape(input_shapes)
-    actual_output_shape = model.predict(inputs).shape
-    assert expected_output_shape == actual_output_shape
-    
-    scale = 0.25
-    
-    
-def backup_screenshot(ts):
-    '''
-    为了方便失败的时候 debug
-    '''
-    if not os.path.isdir(screenshot_backup_dir):
-        os.mkdir(screenshot_backup_dir)
-    shutil.copy('1.png', '{}{}.png'.format(screenshot_backup_dir, ts))
+        def __init__(self):
+        self.reset_appid()
     
     
-def main():
-    '''
-    主函数
-    '''
-    op = yes_or_no('请确保手机打开了 ADB 并连接了电脑，'
-                   '然后打开跳一跳并【开始游戏】后再用本程序，确定开始？')
-    if not op:
-        print('bye')
-        return
-    print('程序版本号：{}'.format(VERSION))
-    debug.dump_device_info()
-    screenshot.check_screenshot()
+from connect_manager import https_manager
+    
+        version_string = 'Version:%d-%d; Build:%d; Platform:%d; CSD:%s; ServicePack:%d-%d; Suite:%d; ProductType:%d' %  (
+        os_version.dwMajorVersion, os_version.dwMinorVersion,
+        os_version.dwBuildNumber,
+        os_version.dwPlatformId,
+        os_version.szCSDVersion,
+        os_version.wServicePackMajor, os_version.wServicePackMinor,
+        os_version.wSuiteMask,
+        os_version.wReserved
+    )
+    
+            self.log_fd = open(self.log_path, 'a')
+    
+            Using setter/getter methods is deprecated. Use o.index instead.'''
