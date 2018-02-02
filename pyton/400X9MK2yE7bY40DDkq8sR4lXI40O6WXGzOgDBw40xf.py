9fa1f3c1e5f2033174a0fb51b2f0f9b3a10d214a
SUNsung <weatherius@gmail.com>
2018-02-02 06:07:08 +0200

    Rewrite [400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py] File in: 2018-02-02 06:07:07

--- pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
+++ pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
@@ -1,72 +1,51 @@
 
         
-            >>> mu = mean(data)
-    >>> pvariance(data, mu)
-    1.25
+        # This was the only thing that Flask used to export at one point and it had
+# a more generic name.
+jsonify = json.jsonify
     
-                self.assertIn('Project-Id-Version', header)
-            self.assertIn('POT-Creation-Date', header)
-            self.assertIn('PO-Revision-Date', header)
-            self.assertIn('Last-Translator', header)
-            self.assertIn('Language-Team', header)
-            self.assertIn('MIME-Version', header)
-            self.assertIn('Content-Type', header)
-            self.assertIn('Content-Transfer-Encoding', header)
-            self.assertIn('Generated-By', header)
+            def post(self):
+            1 // 0
     
-    The interface is file-like.  The implemented methods are:
-read, close, seek, tell, isatty.
-Extra methods are: skip() (called by close, skips to the end of the chunk),
-getname() (returns the name (ID) of the chunk)
+    # Can't import these from paths.py because that uses `future` imports
+DIR_OF_CURRENT_SCRIPT = os.path.dirname( os.path.abspath( __file__ ) )
+DIR_OF_YCMD = os.path.join( DIR_OF_CURRENT_SCRIPT, '..', '..', 'third_party',
+                            'ycmd' )
     
-    Class SequenceMatcher:
-    A flexible class for comparing pairs of sequences of any type.
     
-    def py_scanstring(s, end, strict=True,
-        _b=BACKSLASH, _m=STRINGCHUNK.match):
-    '''Scan the string s for a JSON string. End is the index of the
-    character in s after the quote that started the JSON string.
-    Unescapes all valid JSON string escape sequences and raises ValueError
-    on attempt to decode an invalid string. If strict is False then literal
-    control characters are allowed in the string.
+  # This returns a future! Use JsonFromFuture to get the value.
+  # |timeout| is num seconds to tolerate no response from server before giving
+  # up; see Requests docs for details (we just pass the param along).
+  @staticmethod
+  def PostDataToHandlerAsync( data, handler, timeout = _READ_TIMEOUT_SEC ):
+    return BaseRequest._TalkToHandlerAsync( data, handler, 'POST', timeout )
     
-        return _scan_once
     
-    def escape(m):
-    all, tail = m.group(0, 1)
-    assert all.startswith('\\')
-    esc = simple_escapes.get(tail)
-    if esc is not None:
-        return esc
-    if tail.startswith('x'):
-        hexes = tail[1:]
-        if len(hexes) < 2:
-            raise ValueError('invalid hex string escape ('\\%s')' % tail)
-        try:
-            i = int(hexes, 16)
-        except ValueError:
-            raise ValueError('invalid hex string escape ('\\%s')' % tail) from None
+def ExpectedFailure( reason, *exception_matchers ):
+  '''Defines a decorator to be attached to tests. This decorator
+  marks the test as being known to fail, e.g. where documenting or exercising
+  known incorrect behaviour.
+    
+        vimsupport.JumpToLocation( target_name, 2, 5 )
+    
+    
+  def CurrentFiletypeCompletionEnabled( self ):
+    filetypes = vimsupport.CurrentFiletypes()
+    filetype_to_disable = self._user_options[
+      'filetype_specific_completion_to_disable' ]
+    if '*' in filetype_to_disable:
+      return False
     else:
-        try:
-            i = int(tail, 8)
-        except ValueError:
-            raise ValueError('invalid octal string escape ('\\%s')' % tail) from None
-    return chr(i)
+      return not any([ x in filetype_to_disable for x in filetypes ])
     
-            Returns a (response, lines) tuple where `response` is a unicode
-        string and `lines` is a list of bytes objects.
-        If `file` is a file-like object, it must be open in binary mode.
-        '''
+      if not p.isfile( build_file ):
+    sys.exit(
+      'File {0} does not exist; you probably forgot to run:\n'
+      '\tgit submodule update --init --recursive\n'.format( build_file ) )
     
-    subsystem_details = {
-    # -s flag        : (C entry point template), (is it __main__?), (is it a DLL?)
-    'console'        : (None,                    1,                 0),
-    'windows'        : (WINMAINTEMPLATE,         1,                 0),
-    'service'        : (SERVICETEMPLATE,         0,                 0),
-    'com_dll'        : ('',                      0,                 1),
-}
-    
-            del x; del y; gc.collect(); gc.collect(); gc.collect()
-        a.append(100)
-        a.pop()
-        x = (c_int * 16).from_buffer(a)
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
