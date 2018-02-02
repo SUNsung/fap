e2110a27b4e5a329f0a68a4b6b496bc684c5d9f4
SUNsung <weatherius@gmail.com>
2018-02-02 19:40:05 +0200

    Rewrite [44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp] File in: 2018-02-02 19:40:05

--- cccccc/44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp
+++ cccccc/44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp
@@ -1,169 +1,158 @@
 
         
-          void Compute(OpKernelContext* context) override LOCKS_EXCLUDED(mu_) {
-    mutex_lock l(mu_);
-    if (resource_ == nullptr) {
-      ResourceMgr* mgr = context->resource_manager();
-      OP_REQUIRES_OK(context, cinfo_.Init(mgr, def()));
-    }
-    }
+          // ...Code to store on disk or send over a network goes here...
     
-    #ifndef TENSORFLOW_COMPILER_XLA_SERVICE_HLO_CONSTANT_FOLDING_H_
-#define TENSORFLOW_COMPILER_XLA_SERVICE_HLO_CONSTANT_FOLDING_H_
+    //go generator is used to generate GRPC code for serialization system, such as flatbuffers
+#include <memory>
+#include <vector>
     
-      // Converts binary example labels from 0.0 or 1.0 to -1.0 or 1.0 respectively
-  // as expected by smooth hinge loss.
-  Status ConvertLabel(float* const example_label) const final {
-    if (*example_label == 0.0) {
-      *example_label = -1;
-      return Status::OK();
-    }
-    if (*example_label == 1.0) {
-      return Status::OK();
-    }
-    return errors::InvalidArgument(
-        'Only labels of 0.0 or 1.0 are supported right now. '
-        'Found example with label: ',
-        *example_label);
-  }
-    
-      // Polls the CUDA platform for the event's current status.
-  Event::Status PollForStatus();
-    
-    Status RecordWriter::Close() {
-#if !defined(IS_SLIM_BUILD)
-  if (IsZlibCompressed(options_)) {
-    Status s = dest_->Close();
-    delete dest_;
-    dest_ = nullptr;
-    return s;
-  }
-#endif  // IS_SLIM_BUILD
-  return Status::OK();
-}
-    
-    #include 'tensorflow/core/common_runtime/device.h'
-#include 'tensorflow/core/framework/device_base.h'
-    
-    // See docs in ../ops/io_ops.cc.
-    
-      // Compute the number of unichars in the label.
-  GenericVector<UNICHAR_ID> encoding;
-  if (!unicharset.encode_string(label, true, &encoding, NULL, NULL)) {
-    tprintf('Not outputting illegal unichar %s\n', label);
-    return;
-  }
-    
-    void FullPageBlock(int width, int height, BLOCK_LIST *blocks) {
-  BLOCK_IT block_it(blocks);
-  BLOCK* block = new BLOCK('', TRUE, 0, 0, 0, 0, width, height);
-  block_it.add_to_end(block);
+    int main(int argc, const char *argv[]) {
+  RunServer();
+  return 0;
 }
 
     
-    // A CostFunc that takes the variance of step into account in the cost.
-inT64 DPPoint::CostWithVariance(const DPPoint* prev) {
-  if (prev == NULL || prev == this) {
-    UpdateIfBetter(0, 1, NULL, 0, 0, 0);
-    return 0;
-  }
-    }
-    
-    // Because it involves 2𝜃 , Eq 2 has 2 solutions 90 degrees apart, but which
-// is the min and which is the max? From Eq1:
-// E/N = Var(xi)sin²𝜃  - 2Covar(xi, yi)sin𝜃 cos𝜃  + Var(yi)cos²𝜃
-// and 90 degrees away, using sin/cos equivalences:
-// E'/N = Var(xi)cos²𝜃  + 2Covar(xi, yi)sin𝜃 cos𝜃  + Var(yi)sin²𝜃
-// The second error is smaller (making it the minimum) iff
-// E'/N < E/N ie:
-// (Var(xi) - Var(yi))(cos²𝜃 - sin²𝜃) < -4Covar(xi, yi)sin𝜃 cos𝜃
-// Using double angles:
-// (Var(xi) - Var(yi))cos2𝜃  < -2Covar(xi, yi)sin2𝜃  (InEq 1)
-// But atan2(2Covar(xi, yi), Var(xi) - Var(yi)) picks 2𝜃  such that:
-// sgn(cos2𝜃) = sgn(Var(xi) - Var(yi)) and sgn(sin2𝜃) = sgn(Covar(xi, yi))
-// so InEq1 can *never* be true, making the atan2 result *always* the min!
-// In the degenerate case, where Covar(xi, yi) = 0 AND Var(xi) = Var(yi),
-// the 2 solutions have equal error and the inequality is still false.
-// Therefore the solution really is as trivial as Eq 3.
-    
-      // Calculate y-shift
-  int bln_yshift = 0, bottom_shift = 0, top_shift = 0;
-  if (bottom < min_bottom - tolerance) {
-    bottom_shift = bottom - min_bottom;
-  } else if (bottom > max_bottom + tolerance) {
-    bottom_shift = bottom - max_bottom;
-  }
-  if (top < min_top - tolerance) {
-    top_shift = top - min_top;
-  } else if (top > max_top + tolerance) {
-    top_shift = top - max_top;
-  }
-  if ((top_shift >= 0 && bottom_shift > 0) ||
-      (top_shift < 0 && bottom_shift < 0)) {
-    bln_yshift = (top_shift + bottom_shift) / 2;
-  }
-  *yshift = bln_yshift * yscale;
-    
-    // Refreshes the words in the block_list by using blobs in the
-// new_blobs list.
-// Block list must have word segmentation in it.
-// It consumes the blobs provided in the new_blobs list. The blobs leftover in
-// the new_blobs list after the call weren't matched to any blobs of the words
-// in block list.
-// The output not_found_blobs is a list of blobs from the original segmentation
-// in the block_list for which no corresponding new blobs were found.
-void RefreshWordBlobsFromNewBlobs(BLOCK_LIST* block_list,
-                                  C_BLOB_LIST* new_blobs,
-                                  C_BLOB_LIST* not_found_blobs);
-    
-      tesseract::ParagraphJustification justification() const {
-    return justification_;
-  }
-  int margin() const { return margin_; }
-  int first_indent() const { return first_indent_; }
-  int body_indent() const { return body_indent_; }
-  int tolerance() const { return tolerance_; }
-  bool is_flush() const {
-    return (justification_ == tesseract::JUSTIFICATION_LEFT ||
-            justification_ == tesseract::JUSTIFICATION_RIGHT) &&
-        abs(first_indent_ - body_indent_) <= tolerance_;
-  }
-    
-    
-    {
-    {
-    {    /// return the value of an iterator
-    reference value() const
-    {
-        auto it = --this->base();
-        return it.operator * ();
-    }
-};
+    inline flatbuffers::Offset<Type> CreateType(
+    flatbuffers::FlatBufferBuilder &_fbb,
+    BaseType base_type = None,
+    BaseType element = None,
+    int32_t index = -1) {
+  TypeBuilder builder_(_fbb);
+  builder_.add_index(index);
+  builder_.add_element(element);
+  builder_.add_base_type(base_type);
+  return builder_.Finish();
 }
+    
+    ::grpc::ClientAsyncReader< flatbuffers::grpc::Message<Monster>>* MonsterStorage::Stub::PrepareAsyncRetrieveRaw(::grpc::ClientContext* context, const flatbuffers::grpc::Message<Stat>& request, ::grpc::CompletionQueue* cq) {
+  return ::grpc::internal::ClientAsyncReaderFactory< flatbuffers::grpc::Message<Monster>>::Create(channel_.get(), cq, rpcmethod_Retrieve_, context, request, false, nullptr);
 }
-
     
-    namespace nlohmann
-{
-namespace detail
-{
-///////////////////////////
-// JSON type enumeration //
-///////////////////////////
-    }
+    namespace grpc {
     }
     
-    Unit FileToVector(const std::string &Path, size_t MaxSize = 0,
-                  bool ExitOnError = true);
-    
-    void ListFilesInDirRecursive(const std::string &Dir, long *Epoch,
-                             std::vector<std::string> *V, bool TopDir) {
-  auto E = GetEpoch(Dir);
-  if (Epoch)
-    if (E && *Epoch >= E) return;
+      flatbuffers::grpc::MessageBuilder fbb;
+  {
+    grpc::ClientContext context;
+    // Build a request with the name set.
+    auto monster_offset = CreateMonster(fbb, 0, 0, 0, fbb.CreateString('Fred'));
+    fbb.Finish(monster_offset);
+    auto request = fbb.ReleaseMessage<Monster>();
+    flatbuffers::grpc::Message<Stat> response;
     }
+    
+      static std::string FullNamespace(const char *separator, const Namespace &ns);
+    
+    
+    {    GenerateFn generate;
+    const char *generator_opt_short;
+    const char *generator_opt_long;
+    const char *lang_name;
+    bool schema_only;
+    GenerateFn generateGRPC;
+    flatbuffers::IDLOptions::Language lang;
+    const char *generator_help;
+    MakeRuleFn make_rule;
+  };
+    
+    // 'smart' pointer for use with resizing vectors: turns a pointer inside
+// a vector into a relative offset, such that it is not affected by resizes.
+template<typename T, typename U> class pointer_inside_vector {
+ public:
+  pointer_inside_vector(T *ptr, std::vector<U> &vec)
+      : offset_(reinterpret_cast<uint8_t *>(ptr) -
+                reinterpret_cast<uint8_t *>(flatbuffers::vector_data(vec))),
+        vec_(vec) {}
+    }
+    
+      // Use the `CreateWeapon` shortcut to create Weapons with all fields set.
+  auto sword = CreateWeapon(builder, weapon_one_name, weapon_one_damage);
+  auto axe = CreateWeapon(builder, weapon_two_name, weapon_two_damage);
+    
+    // Platform specific functions:
+bool IsFile(const std::string &Path);
+    
+      if (LocationLen + DirLen + FileLen != FileName.size()) {
+    Printf('DirName() failed for \'%s\', invalid path.\n', FileName.c_str());
+    exit(1);
+  }
     
       bool InFuzzingThread() const { return IsMyThread; }
   size_t GetCurrentUnitInFuzzingThead(const uint8_t **Data) const;
   void TryDetectingAMemoryLeak(const uint8_t *Data, size_t Size,
                                bool DuringInitialCorpusExecution);
+    
+    void Fuzzer::MinimizeCrashLoop(const Unit &U) {
+  if (U.size() <= 2) return;
+  while (!TimedOut() && TotalNumberOfRuns < Options.MaxNumberOfRuns) {
+    MD.StartMutationSequence();
+    memcpy(CurrentUnitData, U.data(), U.size());
+    for (int i = 0; i < Options.MutateDepth; i++) {
+      size_t NewSize = MD.Mutate(CurrentUnitData, U.size(), MaxMutationLen);
+      assert(NewSize > 0 && NewSize <= MaxMutationLen);
+      RunOne(CurrentUnitData, NewSize);
+      TryDetectingAMemoryLeak(CurrentUnitData, NewSize,
+                              /*DuringInitialCorpusExecution*/ false);
+    }
+  }
+}
+    
+      size_t AddWordFromDictionary(Dictionary &D, uint8_t *Data, size_t Size,
+                               size_t MaxSize);
+  size_t MutateImpl(uint8_t *Data, size_t Size, size_t MaxSize,
+                    const std::vector<Mutator> &Mutators);
+    
+    #ifndef LLVM_FUZZER_SHA1_H
+#define LLVM_FUZZER_SHA1_H
+    
+    __attribute__((visibility('default')))
+void __sanitizer_cov_trace_switch(uint64_t Val, uint64_t *Cases) {
+  // Updates the value profile based on the relative position of Val and Cases.
+  // We want to handle one random case at every call (handling all is slow).
+  // Since none of the arguments contain any random bits we use a thread-local
+  // counter to choose the random case to handle.
+  static thread_local size_t Counter;
+  Counter++;
+  uint64_t N = Cases[0];
+  uint64_t *Vals = Cases + 2;
+  char *PC = (char*)__builtin_return_address(0);
+  // We need a random number < N using Counter as a seed. But w/o DIV.
+  // * find a power of two >= N
+  // * mask Counter with this power of two.
+  // * maybe subtract N.
+  size_t Nlog = sizeof(long) * 8 - __builtin_clzl((long)N);
+  size_t PowerOfTwoGeN = 1U << Nlog;
+  assert(PowerOfTwoGeN >= N);
+  size_t Idx = Counter & (PowerOfTwoGeN - 1);
+  if (Idx >= N)
+    Idx -= N;
+  assert(Idx < N);
+  uint64_t TwoIn32 = 1ULL << 32;
+  if ((Val | Vals[Idx]) < TwoIn32)
+    fuzzer::TPC.HandleCmp(PC + Idx, static_cast<uint32_t>(Val),
+                          static_cast<uint32_t>(Vals[Idx]));
+  else
+    fuzzer::TPC.HandleCmp(PC + Idx, Val, Vals[Idx]);
+}
+    
+    int TraceState::TryToAddDesiredData(const uint8_t *PresentData,
+                                    const uint8_t *DesiredData,
+                                    size_t DataSize) {
+  if (NumMutations >= kMaxMutations || !WantToHandleOneMoreMutation()) return 0;
+  ScopedDoingMyOwnMemmem scoped_doing_my_own_memmem;
+  const uint8_t *UnitData;
+  auto UnitSize = F->GetCurrentUnitInFuzzingThead(&UnitData);
+  int Res = 0;
+  const uint8_t *Beg = UnitData;
+  const uint8_t *End = Beg + UnitSize;
+  for (const uint8_t *Cur = Beg; Cur < End; Cur++) {
+    Cur = (uint8_t *)SearchMemory(Cur, End - Cur, PresentData, DataSize);
+    if (!Cur)
+      break;
+    size_t Pos = Cur - Beg;
+    assert(Pos < UnitSize);
+    AddMutation(Pos, DataSize, DesiredData);
+    Res++;
+  }
+  return Res;
+}
