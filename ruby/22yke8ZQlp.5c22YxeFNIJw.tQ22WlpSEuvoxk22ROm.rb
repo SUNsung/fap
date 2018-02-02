cd90e50756bd18feb42227f37916d279e12a3cd6
SUNsung <weatherius@gmail.com>
2018-02-02 17:41:02 +0200

    Rewrite [22yke8ZQlp.5c22YxeFNIJw.tQ22WlpSEuvoxk22ROm.rb] File in: 2018-02-02 17:41:01

--- ruby/22yke8ZQlp.5c22YxeFNIJw.tQ22WlpSEuvoxk22ROm.rb
+++ ruby/22yke8ZQlp.5c22YxeFNIJw.tQ22WlpSEuvoxk22ROm.rb
@@ -1,127 +1,172 @@
 
         
-            alias_method :blank?, :empty?
+          </body>
+</html>
+HTML
+CONTENT_NOT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
     
-    module Docs
-  class PageDb
-    attr_reader :pages
+    def dest_dir(*subdirs)
+  test_dir('dest', *subdirs)
+end
     
-          def inherited(subclass)
-        super
-        subclass.base_url = base_url
-        subclass.dir = dir
-      end
+      # If a given extension is listed multiple times, prefer the first one listed
+  extensions.reject! { |extension| mimes.values.flatten.include?(extension) }
+    
+      # reset variable to default state on Windows
+  ENV['TZ'] = nil
+  dst
+end
+
+    
+          https://pip.readthedocs.org/en/stable/installing/#install-pip
+    EOS
+  when 'pil' then <<-EOS.undent
+    Instead of PIL, consider `pip install pillow` or `brew install Homebrew/python/pillow`.
+    EOS
+  when 'macruby' then <<-EOS.undent
+    MacRuby works better when you install their package:
+      http://www.macruby.org/
+    EOS
+  when /(lib)?lzma/
+    'lzma is now part of the xz formula.'
+  when 'xcode'
+    if MacOS.version >= :lion
+      <<-EOS.undent
+      Xcode can be installed from the App Store.
+      EOS
+    else
+      <<-EOS.undent
+      Xcode can be installed from https://developer.apple.com/xcode/downloads/
+      EOS
     end
+  when 'gtest', 'googletest', 'google-test' then <<-EOS.undent
+    Installing gtest system-wide is not recommended; it should be vendored
+    in your projects that use it.
+    EOS
+  when 'gmock', 'googlemock', 'google-mock' then <<-EOS.undent
+    Installing gmock system-wide is not recommended; it should be vendored
+    in your projects that use it.
+    EOS
+  when 'sshpass' then <<-EOS.undent
+    We won't add sshpass because it makes it too easy for novice SSH users to
+    ruin SSH's security.
+    EOS
+  when 'gsutil' then <<-EOS.undent
+    Install gsutil with `pip install gsutil`
+    EOS
+  when 'clojure' then <<-EOS.undent
+    Clojure isn't really a program but a library managed as part of a
+    project and Leiningen is the user interface to that library.
     
-              # Built-in events
-          if node['id'] == 'Events-catalog'
-            node.next_element.css('li').each do |li|
-              name = '#{li.at_css('b').content.delete(''').strip} event'
-              id = name.parameterize
-              li['id'] = id
-              entries << [name, id, type] unless name == entries.last[0]
-            end
-            next
-          end
+      def fixopt(f)
+    path = if f.linked_keg.directory? && f.linked_keg.symlink?
+      f.linked_keg.resolved_path
+    elsif f.prefix.directory?
+      f.prefix
+    elsif (kids = f.rack.children).size == 1 && kids.first.directory?
+      kids.first
+    else
+      raise
+    end
+    Keg.new(path).optlink
+  rescue StandardError
+    raise '#{f.opt_prefix} not present or broken\nPlease reinstall #{f.full_name}. Sorry :('
+  end
+end
     
-              id = link['href'].remove('#')
-          entries << [name, id]
-          next if name =~ /Sass|Less|Glyphicons/
-    
-            # Helper method to get access to the class variable. This is mostly
-        # exposed for tests. This shouldn't be mucked with directly, since it's
-        # structure may change at any time.
-        def registered; @@registered; end
+        keys.each do |key|
+      value = env[key]
+      s = '#{key}: #{value}'
+      case key
+      when 'CC', 'CXX', 'LD'
+        s << ' => #{Pathname.new(value).realpath}' if File.symlink?(value)
       end
+      f.puts s
     end
   end
 end
 
     
-          # Renders a template, handling the template as a string, but otherwise
-      # acting the same way as {#render}.
+      def describe_python
+    python = which 'python'
+    return 'N/A' if python.nil?
+    python_binary = Utils.popen_read python, '-c', 'import sys; sys.stdout.write(sys.executable)'
+    python_binary = Pathname.new(python_binary).realpath
+    if python == python_binary
+      python
+    else
+      '#{python} => #{python_binary}'
+    end
+  end
+    
+      def migrate_tap_migration
+    report[:D].each do |full_name|
+      name = full_name.split('/').last
+      next unless (dir = HOMEBREW_CELLAR/name).exist? # skip if formula is not installed.
+      next unless new_tap_name = tap.tap_migrations[name] # skip if formula is not in tap_migrations list.
+      tabs = dir.subdirs.map { |d| Tab.for_keg(Keg.new(d)) }
+      next unless tabs.first.tap == tap # skip if installed formula is not from this tap.
+      new_tap = Tap.fetch(new_tap_name)
+      new_tap.install unless new_tap.installed?
+      # update tap for each Tab
+      tabs.each { |tab| tab.tap = new_tap }
+      tabs.each(&:write)
+    end
+  end
+    
+      def self.all
+    opoo 'Formula.all is deprecated, use Formula.map instead'
+    map
+  end
+    
+            # Defines a capability for the given host. The block should return
+        # a class/module that has a method with the capability name, ready
+        # to be executed. This means that if it is an instance method,
+        # the block should return an instance of the class.
+        #
+        # @param [String] host The name of the host
+        # @param [String] cap The name of the capability
+        def self.host_capability(host, cap, &block)
+          components.host_capabilities[host.to_sym].register(cap.to_sym, &block)
+          nil
+        end
+    
+          def delete(key)
+        super(convert_key(key))
+      end
+    
+          # This returns the keys (or ids) that are in the string.
       #
-      # @return [String]
-      def render_string
-        Erubis::Eruby.new(template, trim: true).result(binding)
-      end
-    
-        def assets_path
-      @assets_path ||= File.join gem_path, 'assets'
-    end
-    
-    
-    {    # @include transition(#{border-color ease-in-out .15s, box-shadow ease-in-out .15s})
-    # to
-    # @include transition(border-color ease-in-out .15s, box-shadow ease-in-out .15s)
-    def deinterpolate_vararg_mixins(scss)
-      scss = scss.dup
-      VARARG_MIXINS.each do |mixin|
-        if scss.gsub! /(@include\s*#{Regexp.quote(mixin)})\(\s*\#\{([^}]+)\}\s*\)/, '\1(\2)'
-          log_transform mixin
+      # @return [<Array<String>]
+      def keys
+        regexp = /^#\s*VAGRANT-BEGIN:\s*(.+?)$\r?\n?(.*)$\r?\n?^#\s*VAGRANT-END:\s(\1)$/m
+        @value.scan(regexp).map do |match|
+          match[0]
         end
       end
-      scss
+    
+        def configure_sass
+      require 'sass'
+    
+        def replace_vars(less)
+      less = less.dup
+      # skip header comment
+      less =~ %r(\A/\*(.*?)\*/)m
+      from           = $~ ? $~.to_s.length : 0
+      less[from..-1] = less[from..-1].
+          gsub(/(?!@mixin|@media|@page|@keyframes|@font-face|@-\w)@/, '$').
+          # variables that would be ignored by gsub above: e.g. @page-header-border-color
+          gsub(/@(page[\w-]+)/, '$\1')
+      less
     end
-    
-        def silence_log
-      @silence = true
-      yield
-    ensure
-      @silence = false
-    end
-  end
-end
-
-    
-      def test_image_helper
-    assert_match %r(url\(['']?/assets/apple-touch-icon-144-precomposed.*png['']?\)), @css
-  end
-    
-    desc 'Start a dummy (test) Rails app server'
-task :dummy_rails do
-  require 'rack'
-  require 'term/ansicolor'
-  port = ENV['PORT'] || 9292
-  puts %Q(Starting on #{Term::ANSIColor.cyan 'http://localhost:#{port}'})
-  Rack::Server.start(
-    config: 'test/dummy_rails/config.ru',
-    Port: port)
-end
-    
-    ###
-### methods
-###
-    
-              @node = node
-          @variable = variable
-          @referenced = false
-          @references = []
-        end
-    
-            def nested_comparison?(node)
-          if node.or_type?
-            node.node_parts.all? { |node_part| comparison? node_part }
-          else
-            false
-          end
-        end
-    
-              add_offense(node)
-        end
-    
-      def generate_error_hash(error)
-    {
-      :path => request.path,
-      :status => error.status_code,
-      :error => error.to_hash
-    }
-  end
-    
-              def plugins
-            @plugins ||= find_plugins_gem_specs.map do |spec|
-              { :name => spec.name, :version => spec.version.to_s }
-            end.sort_by do |spec|
-              spec[:name]
-            end
-          end
