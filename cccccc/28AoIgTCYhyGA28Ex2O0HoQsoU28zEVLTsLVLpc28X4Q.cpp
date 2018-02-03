f2d75d0286a642fc47b1e2de0645de1dc491139b
SUNsung <weatherius@gmail.com>
2018-02-03 07:09:46 +0200

    Rewrite [28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp] File in: 2018-02-03 07:09:45

--- cccccc/28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp
+++ cccccc/28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp
@@ -1,134 +1,403 @@
 
         
-        namespace tesseract {
+        #endif  // TENSORFLOW_KERNELS_SPARSE_TENSOR_DENSE_ADD_OP_H_
+
+    
+    namespace perftools {
+namespace gputools {
+namespace cuda {
+    }
+    }
     }
     
-    #ifndef           PGEDIT_H
-#define           PGEDIT_H
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    // Helper recursively prints all paths through the ratings matrix, starting
-// at column col.
-static void PrintMatrixPaths(int col, int dim,
-                             const MATRIX& ratings,
-                             int length, const BLOB_CHOICE** blob_choices,
-                             const UNICHARSET& unicharset,
-                             const char *label, FILE *output_file) {
-  for (int row = col; row < dim && row - col < ratings.bandwidth(); ++row) {
-    if (ratings.get(col, row) != NOT_CLASSIFIED) {
-      BLOB_CHOICE_IT bc_it(ratings.get(col, row));
-      for (bc_it.mark_cycle_pt(); !bc_it.cycled_list(); bc_it.forward()) {
-        blob_choices[length] = bc_it.data();
-        if (row + 1 < dim) {
-          PrintMatrixPaths(row + 1, dim, ratings, length + 1, blob_choices,
-                           unicharset, label, output_file);
-        } else {
-          PrintPath(length + 1, blob_choices, unicharset, label, output_file);
-        }
-      }
+    namespace xla {
     }
+    
+    class TFRecordReader : public ReaderBase {
+ public:
+  TFRecordReader(const string& node_name, const string& compression_type,
+                 Env* env)
+      : ReaderBase(strings::StrCat('TFRecordReader '', node_name, ''')),
+        env_(env),
+        offset_(0),
+        compression_type_(compression_type) {}
+    }
+    
+        // pass address (value interface)
+    iterator find(const K& key)                     { return m.find(&key); }
+    const_iterator find(const K& key) const         { return m.find(&key); }
+    iterator lower_bound(const K& key)              { return m.lower_bound(&key); }
+    const_iterator lower_bound(const K& key) const  { return m.lower_bound(&key); }
+    size_type erase(const K& key)                   { return m.erase(&key); }
+    size_type count(const K& key) const             { return m.count(&key); }
+    
+    // Maximum level to which a new compacted memtable is pushed if it
+// does not create overlap.  We try to push to level 2 to avoid the
+// relatively expensive level 0=>1 compactions and to avoid some
+// expensive manifest file operations.  We do not push all the way to
+// the largest level since that can generate a lot of wasted disk
+// space if the same key space is being repeatedly overwritten.
+static const int kMaxMemCompactLevel = 2;
+    
+      // Successful parses
+  static struct {
+    const char* fname;
+    uint64_t number;
+    FileType type;
+  } cases[] = {
+    { '100.log',            100,   kLogFile },
+    { '0.log',              0,     kLogFile },
+    { '0.sst',              0,     kTableFile },
+    { '0.ldb',              0,     kTableFile },
+    { 'CURRENT',            0,     kCurrentFile },
+    { 'LOCK',               0,     kDBLockFile },
+    { 'MANIFEST-2',         2,     kDescriptorFile },
+    { 'MANIFEST-7',         7,     kDescriptorFile },
+    { 'LOG',                0,     kInfoLogFile },
+    { 'LOG.old',            0,     kInfoLogFile },
+    { '18446744073709551615.log', 18446744073709551615ull, kLogFile },
+  };
+  for (int i = 0; i < sizeof(cases) / sizeof(cases[0]); i++) {
+    std::string f = cases[i].fname;
+    ASSERT_TRUE(ParseFileName(f, &number, &type)) << f;
+    ASSERT_EQ(cases[i].type, type) << f;
+    ASSERT_EQ(cases[i].number, number) << f;
+  }
+    
+      void Start() {
+    start_ = Env::Default()->NowMicros() * 1e-6;
+    bytes_ = 0;
+    message_.clear();
+    last_op_finish_ = start_;
+    hist_.Clear();
+    done_ = 0;
+    next_report_ = 100;
+  }
+    
+      // Three-way comparison.  Returns value:
+  //   < 0 iff 'a' < 'b',
+  //   == 0 iff 'a' == 'b',
+  //   > 0 iff 'a' > 'b'
+  virtual int Compare(const Slice& a, const Slice& b) const = 0;
+    
+      /* Convert from BlobProto to Blob<float> */
+  Blob<float> mean_blob;
+  mean_blob.FromProto(blob_proto);
+  CHECK_EQ(mean_blob.channels(), num_channels_)
+    << 'Number of channels of mean file doesn't match input layer.';
+    
+    int main(int argc, char** argv) {
+  if (argc != 4) {
+    printf('This script converts the MNIST dataset to the leveldb format used\n'
+           'by caffe to train a siamese network.\n'
+           'Usage:\n'
+           '    convert_mnist_data input_image_file input_label_file '
+           'output_db_file\n'
+           'The MNIST dataset could be downloaded at\n'
+           '    http://yann.lecun.com/exdb/mnist/\n'
+           'You should gunzip them after downloading.\n');
+  } else {
+    google::InitGoogleLogging(argv[0]);
+    convert_dataset(argv[1], argv[2], argv[3]);
+  }
+  return 0;
+}
+#else
+int main(int argc, char** argv) {
+  LOG(FATAL) << 'This example requires LevelDB; compile with USE_LEVELDB.';
+}
+#endif  // USE_LEVELDB
+
+    
+    
+    { protected:
+  /**
+   * @param bottom input Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$
+   * @param top output Blob vector (length 1)
+   *   -# @f$ (N \times 1 \times K) @f$ or, if out_max_val
+   *      @f$ (N \times 2 \times K) @f$ unless axis set than e.g.
+   *      @f$ (N \times K \times H \times W) @f$ if axis == 1
+   *      the computed outputs @f$
+   *       y_n = \arg\max\limits_i x_{ni}
+   *      @f$ (for @f$ K = 1 @f$).
+   */
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  /// @brief Not implemented (non-differentiable function)
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+    NOT_IMPLEMENTED;
+  }
+  bool out_max_val_;
+  size_t top_k_;
+  bool has_axis_;
+  int axis_;
+};
+    
+      /// @brief The spatial dimensions of the input.
+  inline int input_shape(int i) {
+    return (*bottom_shape_)[channel_axis_ + i];
+  }
+  // reverse_dimensions should return true iff we are implementing deconv, so
+  // that conv helpers know which dimensions are which.
+  virtual bool reverse_dimensions() = 0;
+  // Compute height_out_ and width_out_ from other parameters.
+  virtual void compute_output_shape() = 0;
+    
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #ifdef USE_CUDNN
+/**
+ * @brief CuDNN acceleration of TanHLayer.
+ */
+template <typename Dtype>
+class CuDNNTanHLayer : public TanHLayer<Dtype> {
+ public:
+  explicit CuDNNTanHLayer(const LayerParameter& param)
+      : TanHLayer<Dtype>(param), handles_setup_(false) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNTanHLayer();
+    }
+    
+    /**
+ * @brief A layer for learning 'embeddings' of one-hot vector input.
+ *        Equivalent to an InnerProductLayer with one-hot vectors as input, but
+ *        for efficiency the input is the 'hot' index of each column itself.
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+template <typename Dtype>
+class EmbedLayer : public Layer<Dtype> {
+ public:
+  explicit EmbedLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    }
+    
+    //////////////////////////////////////////////////////////////////////
+    
+    String TimeStamp::CurrentMicroTime() {
+  struct timeval tp;
+  gettimeofday(&tp, nullptr);
+  char ret[100];
+  snprintf(ret, 100, '%.8F %ld', (double)tp.tv_usec / 1000000, tp.tv_sec);
+  return String(ret, CopyString);
+}
+    
+    /*
+ * Timing execution of block of codes.
+ */
+struct Timer {
+  enum Type {
+    WallTime,
+    SystemCPU,
+    UserCPU,
+    TotalCPU,
+  };
+  enum ReportType {
+    Log,
+    Stderr,
+    Trace,
+  };
+  enum Who {
+    Self = RUSAGE_SELF,
+    Children = RUSAGE_CHILDREN,
+#ifdef RUSAGE_THREAD
+    Thread = RUSAGE_THREAD,
+#endif
+  };
+    }
+    
+    
+    {///////////////////////////////////////////////////////////////////////////////
+}
+    
+    /*
+ * This function is like a request-agnostic version of
+ * server_warmup_status().
+ * Three conditions necessary for the jit to qualify as 'warmed-up':
+ * 1. Has HHVM evaluated enough requests?
+ * 2. Has retranslateAll happened yet?
+ * 3. Has code size plateaued? Is the rate of new code emission flat?
+ * If the jit is warmed up, this function returns the empty string.
+ */
+std::string warmupStatusString();
+    
+    #include 'hphp/util/assertions.h'
+#include 'hphp/util/type-scan.h'
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    
+        const char *rdata = ret.data();
+    int rsize = ret.size();
+    
+    namespace boost {
+    }
+    
+      /// Clear the buffer.
+  void clear()
+  {
+    begin_offset_ = 0;
+    end_offset_ = 0;
+  }
+    
+    #if defined(BOOST_ASIO_HAS_STD_FUNCTION)
+# include <functional>
+#else // defined(BOOST_ASIO_HAS_STD_FUNCTION)
+# include <boost/function.hpp>
+#endif // defined(BOOST_ASIO_HAS_STD_FUNCTION)
+    
+    #ifndef BOOST_ASIO_DETAIL_HANDLER_ALLOC_HELPERS_HPP
+#define BOOST_ASIO_DETAIL_HANDLER_ALLOC_HELPERS_HPP
+    
+    int poll_read(int d, state_type state, boost::system::error_code& ec)
+{
+  if (d == -1)
+  {
+    ec = boost::asio::error::bad_descriptor;
+    return -1;
+  }
+    }
+    
+    template <typename Time_Traits>
+void dev_poll_reactor::schedule_timer(timer_queue<Time_Traits>& queue,
+    const typename Time_Traits::time_type& time,
+    typename timer_queue<Time_Traits>::per_timer_data& timer, wait_op* op)
+{
+  boost::asio::detail::mutex::scoped_lock lock(mutex_);
+    }
+    
+    template <typename Time_Traits>
+void epoll_reactor::add_timer_queue(timer_queue<Time_Traits>& queue)
+{
+  do_add_timer_queue(queue);
+}
+    
+    extern JSClass  *jsb_cocosbuilder_CCBReader_class;
+extern JSObject *jsb_cocosbuilder_CCBReader_prototype;
+    
+    
+    
+    #ifdef __cplusplus
+extern 'C' {
+#endif
+#include 'tolua++.h'
+#ifdef __cplusplus
+}
+#endif
+    
+    
+    
+    
+    
+    
+    {	for (int32 i = 0; i < manifold->pointCount && m_pointCount < k_maxContactPoints; ++i)
+	{
+		ContactPoint* cp = m_points + m_pointCount;
+		cp->fixtureA = fixtureA;
+		cp->fixtureB = fixtureB;
+		cp->position = worldManifold.points[i];
+		cp->normal = worldManifold.normal;
+		cp->state = state2[i];
+		cp->normalImpulse = manifold->points[i].normalImpulse;
+		cp->tangentImpulse = manifold->points[i].tangentImpulse;
+		cp->separation = worldManifold.separations[i];
+		++m_pointCount;
+	}
+}
+    
+    class Bridge : public Test
+{
+public:
+    }
+    
+    // Platform detection.
+#ifdef __linux__
+#define LIBFUZZER_APPLE 0
+#define LIBFUZZER_LINUX 1
+#define LIBFUZZER_WINDOWS 0
+#elif __APPLE__
+#define LIBFUZZER_APPLE 1
+#define LIBFUZZER_LINUX 0
+#define LIBFUZZER_WINDOWS 0
+#elif _WIN32
+#define LIBFUZZER_APPLE 0
+#define LIBFUZZER_LINUX 0
+#define LIBFUZZER_WINDOWS 1
+#else
+#error 'Support for your platform has not been implemented'
+#endif
+    
+    static void CheckFnPtr(void *FnPtr, const char *FnName, bool WarnIfMissing) {
+  if (FnPtr == nullptr && WarnIfMissing) {
+    Printf('WARNING: Failed to find function \'%s\'.\n', FnName);
   }
 }
     
-        #ifndef GRAPHICS_DISABLED
-    void plot(                 //draw one
-              ScrollView* window,   //window to draw in
-              ScrollView::Color colour);  //uniform colour
-    void plot(                 //draw one
-              ScrollView* window);  //in rainbow colours
+    
+    {  // Mutate the integer value.
+  switch(Rand(5)) {
+    case 0: Val++; break;
+    case 1: Val--; break;
+    case 2: Val /= 2; break;
+    case 3: Val *= 2; break;
+    case 4: Val = Rand(Val * Val); break;
+    default: assert(0);
+  }
+  // Just replace the bytes with the new ones, don't bother moving bytes.
+  for (size_t i = B; i < E; i++) {
+    size_t Idx = E + B - i - 1;
+    assert(Idx >= B && Idx < E);
+    Data[Idx] = (Val % 10) + '0';
+    Val /= 10;
+  }
+  return Size;
+}
+    
+    // Private copy of SHA1 implementation.
+static const int kSHA1NumBytes = 20;
+    
+      void AddValueForMemcmp(void *caller_pc, const void *s1, const void *s2,
+                         size_t n);
+  void AddValueForStrcmp(void *caller_pc, const char *s1, const char *s2,
+                         size_t n);
     
     
     {
-    {    std::ostringstream oss;
-    std::copy(line.begin() + options_index,
-              line.end(),
-              std::ostream_iterator<std::string>(oss, ' '));
-    r['options'] = oss.str();
-    results.push_back(r);
-  }
-}
-    
-      /// Calling startExtension should declare the registry external.
-  /// This will cause extension-internal events to forward to osquery core.
-  bool external_{false};
-    
-    
-    {error_exit:
-  // Unlock and return a status to the daemon.
-  lck_mtx_unlock(osquery.mtx);
-  return err;
-}
-    
-    namespace osquery {
-    }
-    
-        pt::ptree multi_pack;
-    for (const auto& path : paths) {
-      std::string content;
-      if (!readFile(path, content)) {
-        LOG(WARNING) << 'Cannot read multi-pack file: ' << path;
-        continue;
+    {      (void)sigemptyset(&BlockedSignalsSet);
+      (void)sigaddset(&BlockedSignalsSet, SIGCHLD);
+      if (sigprocmask(SIG_BLOCK, &BlockedSignalsSet, &OldBlockedSignalsSet) ==
+          -1) {
+        Printf('Failed to block SIGCHLD\n');
+        // Try our best to restore the signal handlers.
+        (void)sigaction(SIGQUIT, &OldSigQuitAction, NULL);
+        (void)sigaction(SIGINT, &OldSigIntAction, NULL);
+        (void)posix_spawnattr_destroy(&SpawnAttributes);
+        return -1;
       }
     }
-    
-      auto s = TLSRequestHelper::go<JSONSerializer>(
-      uri_, params, json, FLAGS_config_tls_max_attempts);
-  if (s.ok()) {
-    if (FLAGS_tls_node_api) {
-      // The node API embeds configuration data (JSON escaped).
-      pt::ptree tree;
-      try {
-        std::stringstream input;
-        input << json;
-        pt::read_json(input, tree);
-      } catch (const pt::json_parser::json_parser_error& /* e */) {
-        VLOG(1) << 'Could not parse JSON from TLS node API';
-      }
-    }
-    }
-    
-    #include 'osquery/core/conversions.h'
-    
-    #include 'mars/log/appender.h'
-    
-    #ifndef __PublicComponent__testspy__
-#define __PublicComponent__testspy__
-    
-    // bool JNU_Jstring2Wstring( JNIEnv* _env, const jstring jstr, std::wstring& wstr); //in linux sizeof(wchar_t)==4 but sizeof(jchar)==2
-wchar_t* JNU_Jstring2Wchar(JNIEnv* _env, const jstring jstr);
-void JNU_FreeWchar(JNIEnv* _env, jstring str, wchar_t* wchar);
-jstring JNU_Wstring2Jstring(JNIEnv* _env, const std::wstring& wstr);
-jstring JNU_Wchar2JString(JNIEnv* _env, wchar_t* wchar);
-    
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
-    
-    
-    {}  // namespace grpc
-    
-      grpc::ServerBuilder builder;
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  builder.RegisterService(&service);
-  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
-  std::cerr << 'Server listening on ' << server_address << std::endl;
-    
-    grpc::string GetHeaderEpilogue(grpc_generator::File *file,
-                               const Parameters & /*params*/) {
-  grpc::string output;
-  {
-    // Scope the output stream so it closes and finalizes output to the string.
-    auto printer = file->CreatePrinter(&output);
-    std::map<grpc::string, grpc::string> vars;
-    }
-    }
-    
-    template<typename T> const char *EnumName(T tval, const TypeTable *type_table) {
-  if (!type_table || !type_table->names) return nullptr;
-  auto i = LookupEnum(static_cast<int32_t>(tval), type_table->values,
-                      type_table->num_elems);
-  if (i >= 0 && i < static_cast<int32_t>(type_table->num_elems)) {
-    return type_table->names[i];
+    ++ActiveThreadCount;
   }
-  return nullptr;
-}
