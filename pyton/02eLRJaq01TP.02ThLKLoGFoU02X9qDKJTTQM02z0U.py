4403b2a2cf2a8145f0a121fb19698f7866f14fc7
SUNsung <weatherius@gmail.com>
2018-02-03 09:35:27 +0200

    Rewrite [02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py] File in: 2018-02-03 09:35:26

--- pyton/02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py
+++ pyton/02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py
@@ -1,21 +1,126 @@
 
         
-        from ..common import *
-from ..extractor import VideoExtractor
+        
+collect_ignore = [
+    # deprecated or moved modules
+    'scrapy/conf.py',
+    'scrapy/stats.py',
+    'scrapy/project.py',
+    'scrapy/utils/decorator.py',
+    'scrapy/statscol.py',
+    'scrapy/squeue.py',
+    'scrapy/log.py',
+    'scrapy/dupefilter.py',
+    'scrapy/command.py',
+    'scrapy/linkextractor.py',
+    'scrapy/spider.py',
     
-        for i in range(10, 30):
-        url = 'https://stream{i}.mixcloud.com/c/m4a/64{p}.m4a'.format(
-            i = i,
-            p = preview
+    
+class EventNotification( BaseRequest ):
+  def __init__( self, event_name, buffer_number = None, extra_data = None ):
+    super( EventNotification, self ).__init__()
+    self._event_name = event_name
+    self._buffer_number = buffer_number
+    self._extra_data = extra_data
+    self._response_future = None
+    self._cached_response = None
+    
+    
+class CommandRequest( BaseRequest ):
+  def __init__( self, arguments, completer_target = None, extra_data = None ):
+    super( CommandRequest, self ).__init__()
+    self._arguments = _EnsureBackwardsCompatibility( arguments )
+    self._completer_target = ( completer_target if completer_target
+                               else 'filetype_default' )
+    self._extra_data = extra_data
+    self._response = None
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+    
+        @gen_test
+    def test_http10_no_content_length(self):
+        # Regression test for a bug in which can_keep_alive would crash
+        # for an HTTP/1.0 (not 1.1) response with no content-length.
+        conn = HTTP1Connection(self.client_stream, True)
+        self.server_stream.write(b'HTTP/1.0 200 Not Modified\r\n\r\nhello')
+        self.server_stream.close()
+    
+    # Increasing --n without --keepalive will eventually run into problems
+# due to TIME_WAIT sockets
+define('n', type=int, default=15000)
+define('c', type=int, default=25)
+define('keepalive', type=bool, default=False)
+define('quiet', type=bool, default=False)
+    
+    
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r'/', HomeHandler),
+            (r'/archive', ArchiveHandler),
+            (r'/feed', FeedHandler),
+            (r'/entry/([^/]+)', EntryHandler),
+            (r'/compose', ComposeHandler),
+            (r'/auth/create', AuthCreateHandler),
+            (r'/auth/login', AuthLoginHandler),
+            (r'/auth/logout', AuthLogoutHandler),
+        ]
+        settings = dict(
+            blog_title=u'Tornado Blog',
+            template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+            static_path=os.path.join(os.path.dirname(__file__), 'static'),
+            ui_modules={'Entry': EntryModule},
+            xsrf_cookies=True,
+            cookie_secret='__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__',
+            login_url='/auth/login',
+            debug=True,
         )
-        try:
-            mime, ext, size = url_info(url)
-            break
-        except: continue
+        super(Application, self).__init__(handlers, **settings)
+        # Have one global connection to the blog DB across all handlers
+        self.db = torndb.Connection(
+            host=options.mysql_host, database=options.mysql_database,
+            user=options.mysql_user, password=options.mysql_password)
     
+    import logging
+import tornado.escape
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.websocket
+import os.path
+import uuid
     
-def jump(distance):
-    press_time = distance * time_coefficient
-    press_time = press_time
-    print('press_time = ',press_time)
-    s.tap_hold(200, 200, press_time)
+        @classmethod
+    def setUpClass(cls):
+        cls.s = Subject()
+        cls.dec_obs = DecimalViewer()
+        cls.hex_obs = HexViewer()
+    
+        def test_sales_manager_shall_not_talk_through_proxy_with_delay(cls):
+        cls.ntp.busy = 'No'
+        start_time = time()
+        cls.ntp.talk()
+        end_time = time()
+        execution_time = end_time - start_time
+        print_output = cls.output.getvalue()
+        expected_print_output = 'Proxy checking for Sales Manager availability\n\
+This Sales Manager will not talk to you whether he/she is busy or not\n'
+        cls.assertEqual(print_output, expected_print_output)
+        expected_execution_time = 1
+        cls.assertEqual(int(execution_time*10), expected_execution_time)
+    
+        def test_am_station_overflow_after_scan(self):
+        self.radio.scan()
+        station = self.radio.state.stations[self.radio.state.pos]
+        expected_station = '1250'
+        self.assertEqual(station, expected_station)
