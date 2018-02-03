c785533eb4c4f137f692da54ebf43c45a2a1f6e5
SUNsung <weatherius@gmail.com>
2018-02-03 10:50:32 +0200

    Rewrite [27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py] File in: 2018-02-03 10:50:31

--- pyton/27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py
+++ pyton/27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py
@@ -1,93 +1,176 @@
 
         
-            def short_desc(self):
-        '''
-        A short description of the command
-        '''
-        return ''
+            def test_youporn(self):
+        self._assert_restricted(
+            'http://www.youporn.com/watch/505835/sex-ed-is-it-safe-to-masturbate-daily/',
+            '505835.mp4', 2, old_age=25)
     
+        def test_facebook_matching(self):
+        self.assertTrue(FacebookIE.suitable('https://www.facebook.com/Shiniknoh#!/photo.php?v=10153317450565268'))
+        self.assertTrue(FacebookIE.suitable('https://www.facebook.com/cindyweather?fref=ts#!/photo.php?v=10152183998945793'))
     
+    IGNORED_FILES = [
+    'setup.py',  # http://bugs.python.org/issue13943
+    'conf.py',
+    'buildserver.py',
+]
     
-        @property
-    def templates_dir(self):
-        _templates_base_dir = self.settings['TEMPLATES_DIR'] or \
-            join(scrapy.__path__[0], 'templates')
-        return join(_templates_base_dir, 'spiders')
+    from .nuevo import NuevoBaseIE
+    
+        _TESTS = [{
+        'url': 'http://camwithher.tv/view_video.php?viewkey=6e9a24e2c0e842e1f177&page=&viewtype=&category=',
+        'info_dict': {
+            'id': '5644',
+            'ext': 'flv',
+            'title': 'Periscope Tease',
+            'description': 'In the clouds teasing on periscope to my favorite song',
+            'duration': 240,
+            'view_count': int,
+            'comment_count': int,
+            'uploader': 'MileenaK',
+            'upload_date': '20160322',
+        },
+        'params': {
+            'skip_download': True,
+        }
+    }, {
+        'url': 'http://camwithher.tv/view_video.php?viewkey=6dfd8b7c97531a459937',
+        'only_matching': True,
+    }, {
+        'url': 'http://camwithher.tv/view_video.php?page=&viewkey=6e9a24e2c0e842e1f177&viewtype=&category=',
+        'only_matching': True,
+    }, {
+        'url': 'http://camwithher.tv/view_video.php?viewkey=b6c3b5bea9515d1a1fc4&page=&viewtype=&category=mv',
+        'only_matching': True,
+    }]
+    
+    containers = (('thefuck/python3-tcsh',
+               u'''FROM python:3
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'),
+              ('thefuck/python2-tcsh',
+               u'''FROM python:2
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'))
+    
+        i = tf.train.range_input_producer(epoch_size, shuffle=False).dequeue()
+    x = tf.strided_slice(data, [0, i * num_steps],
+                         [batch_size, (i + 1) * num_steps])
+    x.set_shape([batch_size, num_steps])
+    y = tf.strided_slice(data, [0, i * num_steps + 1],
+                         [batch_size, (i + 1) * num_steps + 1])
+    y.set_shape([batch_size, num_steps])
+    return x, y
 
     
-            cb = request.callback
+    import tensorflow as tf
     
     
-class ScrapesContract(Contract):
-    ''' Contract to check presence of fields in scraped items
-        @scrapes page_name page_body
-    '''
+def train_input_fn(features, labels, batch_size):
+    '''An input function for training'''
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
     
-        # For pickling to work, the __module__ variable needs to be set to the frame
-    # where the named tuple is created.  Bypass this step in enviroments where
-    # sys._getframe is not defined (Jython for example).
-    if hasattr(_sys, '_getframe'):
-        result.__module__ = _sys._getframe(1).f_globals.get('__name__', '__main__')
+    py_library(
+    name = 'rnn',
+    srcs = [
+        'rnn.py',
+    ],
+    srcs_version = 'PY2AND3',
+    deps = [
+        ':rnn_cell',
+        '//tensorflow:tensorflow_py',
+    ],
+)
     
-            result_item = result_queue.get(block=True)
-        if result_item is not None:
-            work_item = pending_work_items[result_item.work_id]
-            del pending_work_items[result_item.work_id]
+      if tokenizer:
+    words = tokenizer(sentence)
+  else:
+    words = basic_tokenizer(sentence)
+  if not normalize_digits:
+    return [vocabulary.get(w, UNK_ID) for w in words]
+  # Normalize digits by 0 before looking words up in the vocabulary.
+  return [vocabulary.get(_DIGIT_RE.sub(b'0', w), UNK_ID) for w in words]
     
-            self.assertEqual(set([SUCCESSFUL_FUTURE,
-                              CANCELLED_AND_NOTIFIED_FUTURE,
-                              future1]), finished)
-        self.assertEqual(set([CANCELLED_FUTURE, future2]), pending)
+        for n_components in [i.astype(int) for i in
+                         np.linspace(data.shape[1] // 10,
+                                     data.shape[1], num=4)]:
+        all_times = defaultdict(list)
+        all_errors = defaultdict(list)
+        pca = PCA(n_components=n_components)
+        rpca = RandomizedPCA(n_components=n_components, random_state=1999)
+        results_dict = {k: benchmark(est, data) for k, est in [('pca', pca),
+                                                               ('rpca', rpca)]}
     
+        print('')
+    print('')
+
     
-  # These two methods exist to avoid importing the requests module at startup;
-  # reducing loading time since this module is slow to import.
-  @classmethod
-  def Requests( cls ):
-    try:
-      return cls.requests
-    except AttributeError:
-      import requests
-      cls.requests = requests
-      return requests
+        from sklearn.tree import DecisionTreeRegressor
     
-            present_dialog.assert_has_exact_calls( [
-          PresentDialog_Confirm_Call( MESSAGE ),
-        ] )
-        load_extra_conf.assert_has_exact_calls( [
-          call( FILE_NAME ),
-        ] )
+    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
+DATA_FOLDER = 'txt_sentoken'
     
-    
-class WSGIApplicationTest(AsyncHTTPTestCase):
-    def get_app(self):
-        class HelloHandler(RequestHandler):
-            def get(self):
-                self.write('Hello world!')
-    
-        This is a non-blocking and non-threaded resolver.  It may not produce
-    the same results as the system resolver, but can be used for non-blocking
-    resolution when threads cannot be used.
-    
-    # This import will fail if path is not set up correctly
-import testapp
-    
-            'ExceptionBenchmark().enter_exit(50)',
-        'ExceptionBenchmark().call_wrapped(50)',
-        'ExceptionBenchmark().enter_exit(500)',
-        'ExceptionBenchmark().call_wrapped(500)',
-    ]
-    for cmd in cmds:
-        print(cmd)
-        subprocess.check_call(base_cmd + [cmd])
+    plt.show()
+
     
     
-class FeedHandler(BaseHandler):
-    def get(self):
-        entries = self.db.query('SELECT * FROM entries ORDER BY published '
-                                'DESC LIMIT 10')
-        self.set_header('Content-Type', 'application/atom+xml')
-        self.render('feed.xml', entries=entries)
+# Create dataset of classification task with many redundant and few
+# informative features
+X, y = datasets.make_classification(n_samples=100000, n_features=20,
+                                    n_informative=2, n_redundant=10,
+                                    random_state=42)
     
-    project = 'Tornado'
-copyright = '2009-%s, The Tornado Authors' % time.strftime('%Y')
+    plots = []
+names = []
+for score_func in score_funcs:
+    print('Computing %s for %d values of n_clusters and n_samples=%d'
+          % (score_func.__name__, len(n_clusters_range), n_samples))
+    
+        from pprint import pprint
+    pprint(contributions)
+    
+        def __init__(self, name, action):
+        self.name = name
+        self.action = action
+    
+        def test_frozen_pool(self):
+        with ObjectPool(self.sample_queue) as pool:
+            self.assertEqual(pool, 'first')
+            self.assertEqual(pool, 'first')
+        self.assertTrue(self.sample_queue.get() == 'second')
+        self.assertFalse(self.sample_queue.empty())
+        self.assertTrue(self.sample_queue.get() == 'first')
+        self.assertTrue(self.sample_queue.empty())
+    
+        def setUp(self):
+        self.tc1 = TC1()
+        self.tc2 = TC2()
+        self.tc3 = TC3()
+        self.average_result_tc1 = '###### In Test 1 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc2 = '###### In Test 2 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc3 = '###### In Test 3 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.runner = TestRunner()
+        self.out = StringIO()
+        self.saved_stdout = sys.stdout
+        sys.stdout = self.out
+    
+            # and can also be undone at will
+        for cmd in reversed(command_stack):
+            cmd.undo()
+    finally:
+        os.unlink('foo.txt')
