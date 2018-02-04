da43f7b8a06e26efd043be1350574d39def6c101
SUNsung <weatherius@gmail.com>
2018-02-04 20:17:53 +0200

    Rewrite [486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb] File in: 2018-02-04 20:17:53

--- ruby/486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb
+++ ruby/486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb
@@ -1,193 +1,164 @@
 
         
-              https://pip.readthedocs.org/en/stable/installing/#install-pip
-    EOS
-  when 'pil' then <<-EOS.undent
-    Instead of PIL, consider `pip install pillow` or `brew install Homebrew/python/pillow`.
-    EOS
-  when 'macruby' then <<-EOS.undent
-    MacRuby works better when you install their package:
-      http://www.macruby.org/
-    EOS
-  when /(lib)?lzma/
-    'lzma is now part of the xz formula.'
-  when 'xcode'
-    if MacOS.version >= :lion
-      <<-EOS.undent
-      Xcode can be installed from the App Store.
-      EOS
+            # Assign additional cores to the guest OS.
+    v.customize ['modifyvm', :id, '--cpus', cpu_count]
+    v.customize ['modifyvm', :id, '--ioapic', 'on']
+    
+          topic.feature_topic_users(args)
+    end
+    
+        self.base_url = 'http://localhost/'
+    
+          if options && options[:ignore_case]
+        base = base.downcase
+        dest = dest.downcase
+      end
+    
+            css('> .section', '#preamble', 'a[href*='dict.html']', 'code var', 'code strong').each do |node|
+          node.before(node.children).remove
+        end
+    
+      class DummyOutput < String
+    alias write concat
+  end
+  def assert_no_error(*args)
+    $stderr, stderr = DummyOutput.new, $stderr
+    assert_nothing_raised(*args) {return yield}
+  ensure
+    stderr, $stderr = $stderr, stderr
+    $!.backtrace.delete_if {|e| /\A#{Regexp.quote(__FILE__)}:#{__LINE__-2}/o =~ e} if $!
+    assert_empty(stderr)
+  end
+  alias no_error assert_no_error
+    
+        def make_time(date, year, mon, day, hour, min, sec, sec_fraction, zone, now)
+      if !year && !mon && !day && !hour && !min && !sec && !sec_fraction
+        raise ArgumentError, 'no time information in #{date.inspect}'
+      end
+    
+      # Open3.pipeline_start starts a list of commands as a pipeline.
+  # No pipes are created for stdin of the first command and
+  # stdout of the last command.
+  #
+  #   Open3.pipeline_start(cmd1, cmd2, ... [, opts]) {|wait_threads|
+  #     ...
+  #   }
+  #
+  #   wait_threads = Open3.pipeline_start(cmd1, cmd2, ... [, opts])
+  #   ...
+  #
+  # Each cmd is a string or an array.
+  # If it is an array, the elements are passed to Process.spawn.
+  #
+  #   cmd:
+  #     commandline                              command line string which is passed to a shell
+  #     [env, commandline, opts]                 command line string which is passed to a shell
+  #     [env, cmdname, arg1, ..., opts]          command name and one or more arguments (no shell)
+  #     [env, [cmdname, argv0], arg1, ..., opts] command name and arguments including argv[0] (no shell)
+  #
+  #   Note that env and opts are optional, as for Process.spawn.
+  #
+  # Example:
+  #
+  #   # Run xeyes in 10 seconds.
+  #   Open3.pipeline_start('xeyes') {|ts|
+  #     sleep 10
+  #     t = ts[0]
+  #     Process.kill('TERM', t.pid)
+  #     p t.value #=> #<Process::Status: pid 911 SIGTERM (signal 15)>
+  #   }
+  #
+  #   # Convert pdf to ps and send it to a printer.
+  #   # Collect error message of pdftops and lpr.
+  #   pdf_file = 'paper.pdf'
+  #   printer = 'printer-name'
+  #   err_r, err_w = IO.pipe
+  #   Open3.pipeline_start(['pdftops', pdf_file, '-'],
+  #                        ['lpr', '-P#{printer}'],
+  #                        :err=>err_w) {|ts|
+  #     err_w.close
+  #     p err_r.read # error messages of pdftops and lpr.
+  #   }
+  #
+  def pipeline_start(*cmds, **opts, &block)
+    if block
+      pipeline_run(cmds, opts, [], [], &block)
     else
-      <<-EOS.undent
-      Xcode can be installed from https://developer.apple.com/xcode/downloads/
-      EOS
+      ts, = pipeline_run(cmds, opts, [], [])
+      ts
     end
-  when 'gtest', 'googletest', 'google-test' then <<-EOS.undent
-    Installing gtest system-wide is not recommended; it should be vendored
-    in your projects that use it.
-    EOS
-  when 'gmock', 'googlemock', 'google-mock' then <<-EOS.undent
-    Installing gmock system-wide is not recommended; it should be vendored
-    in your projects that use it.
-    EOS
-  when 'sshpass' then <<-EOS.undent
-    We won't add sshpass because it makes it too easy for novice SSH users to
-    ruin SSH's security.
-    EOS
-  when 'gsutil' then <<-EOS.undent
-    Install gsutil with `pip install gsutil`
-    EOS
-  when 'clojure' then <<-EOS.undent
-    Clojure isn't really a program but a library managed as part of a
-    project and Leiningen is the user interface to that library.
+  end
+  module_function :pipeline_start
     
-    def bottle_resolve_formula_names(bottle_file)
-  receipt_file_path = bottle_receipt_path bottle_file
-  receipt_file = Utils.popen_read('tar', '-xOzf', bottle_file, receipt_file_path)
-  name = receipt_file_path.split('/').first
-  tap = Tab.from_file_content(receipt_file, '#{bottle_file}/#{receipt_file_path}').tap
-    
-          # Find commands in Homebrew/dev-cmd
-      if ARGV.homebrew_developer?
-        puts
-        puts 'Built-in development commands'
-        puts_columns internal_development_commands
-      end
-    
-        args = dirs + %w[-type f (]
-    args.concat UNBREWED_EXCLUDE_FILES.flat_map { |f| %W[! -name #{f}] }
-    args.concat UNBREWED_EXCLUDE_PATHS.flat_map { |d| %W[! -path #{d}] }
-    args.concat %w[)]
-    
-          To export the needed variables, add them to your dotfiles.
-       * On Bash, add them to `~/.bash_profile`.
-       * On Zsh, add them to `~/.zprofile` instead.
-    
-      def render_response
-    render json: {
-      html: view_to_html_string('shared/notifications/_button', notification_setting: @notification_setting),
-      saved: @saved
+      def test_block
+    r = Open3.popen3(RUBY, '-e', 'STDOUT.print STDIN.read') {|i,o,e,t|
+      i.print 'baz'
+      i.close
+      assert_equal('baz', o.read)
+      'qux'
     }
+    assert_equal('qux', r)
   end
     
-        if options['recipients'].present?
-      emails = options['recipients']
-      emails = [emails] if emails.is_a?(String)
-      unless emails.all? { |email| email =~ Devise.email_regexp || email =~ /\{/ }
-        errors.add(:base, ''when provided, 'recipients' should be an email address or an array of email addresses')
-      end
-    end
-  end
+        def test_crc
+      Tempfile.create('test_zlib_gzip_file_crc') {|t|
+        t.close
+        Zlib::GzipWriter.open(t.path) {|gz| gz.print('foo') }
     }
     
-      module ClassMethods
-    def form_configurable(name, *args)
-      options = args.extract_options!.reverse_merge(roles: [], type: :string)
+            def to_feed(feed, current)
+          if current.respond_to?(:itunes_owner=)
+            _not_set_required_variables = not_set_required_variables
+            if (required_variable_names - _not_set_required_variables).empty?
+              return
+            end
     
-                sequence_base := (
-              'x' ||
-              -- Take the first two bytes (four hex characters)
-              substr(
-                -- Of the MD5 hash of the data we documented
-                md5(table_name ||
-                  '#{SecureRandom.hex(16)}' ||
-                  time_part::text
-                ),
-                1, 4
-              )
-            -- And turn it into a bigint
-            )::bit(16)::bigint;
-    
-      def collection_presenter
-    ActivityPub::CollectionPresenter.new(
-      id: tag_url(@tag),
-      type: :ordered,
-      size: @tag.statuses.count,
-      items: @statuses.map { |s| ActivityPub::TagManager.instance.uri_for(s) }
-    )
-  end
-    
-            expect(response).to have_http_status(:missing)
-      end
-    end
+    def process_args
+  if ARGV.first =~ %r{^-+h(?:elp)?$}
+    puts usage
+    exit 0
+  elsif ARGV.length == 1
+    $app_path = ARGV.first
+  else
+    puts usage
+    exit 1
   end
 end
-
     
-      let(:user) { Fabricate(:user, filtered_languages: []) }
-    
-      def verify_payload?
-    payload.present? && VerifySalmonService.new.call(payload)
-  end
-    
-      # Flushes all queued logs to the wrapped logger.
-  def flush
-    @messages.each {|(l, m)| @inner.log(l, m)}
-  end
-    
-      Sass::Plugin.options.merge!(config)
-    
-            unless guarded && environment.var(name)
-          environment.set_var(name, val.perform(environment))
+          def call
+        title('Gems')
+        table(all_gem_names) do |gem, row|
+          row.yellow if update_available?(gem)
+          row << gem
+          row << installed_gem_version(gem)
+          row << '(update available)' if update_available?(gem)
         end
-    
-      # POST /books
-  # POST /books.json
-  def create
-    @book = Book.new(book_params)
-    
-            def operator
-          assignment_node = meta_assignment_node || @node
-          assignment_node.loc.operator.source
-        end
-    
-            # This is a convenient way to check whether the variable is used
-        # in its entire variable lifetime.
-        # For more precise usage check, refer Assignment#used?.
-        #
-        # Once the variable is captured by a block, we have no idea
-        # when, where and how many times the block would be invoked
-        # and it means we cannot track the usage of the variable.
-        # So we consider it's used to suppress false positive offenses.
-        def used?
-          @captured_by_block || referenced?
-        end
-    
-            def autocorrect(node)
-          # Regexp#match can take a second argument, but this cop doesn't
-          # register an offense in that case
-          return unless node.first_argument.regexp_type?
-    
-                add_offense(void_next) if void_next
-          end
-        end
-    
-    module RuboCop
-  module Cop
-    module Style
-      # This cop checks for optional arguments to methods
-      # that do not come at the end of the argument list
-      #
-      # @example
-      #   # bad
-      #   def foo(a = 1, b, c)
-      #   end
-      #
-      #   # good
-      #   def baz(a, b, c = 1)
-      #   end
-      #
-      #   def foobar(a = 1, b = 2, c = 3)
-      #   end
-      class OptionalArguments < Cop
-        MSG = 'Optional arguments should appear at the end ' \
-              'of the argument list.'.freeze
-    
-          def initialize(agent)
-        @agent = agent
-        logger.debug('[api-service] start') if logger.debug?
       end
     
-      public
-  def close; end;
+          def calculate_column_widths(rows)
+        num_columns = rows.map { |row| row.values.length }.max
+        Array.new(num_columns) do |col|
+          rows.map { |row| row.values[col].to_s.length }.max
+        end
+      end
     
-      extend self
+            on roles(target_roles) do
+          unless test '[ -f #{file.to_s.shellescape} ]'
+            info 'Uploading #{prerequisite_file} to #{file}'
+            upload! File.open(prerequisite_file), file
+          end
+        end
+      end
+    end
+    
+    desc 'Deploy a new release.'
+task :deploy do
+  set(:deploying, true)
+  %w{ starting started
+      updating updated
+      publishing published
+      finishing finished }.each do |task|
+    invoke 'deploy:#{task}'
+  end
+end
+task default: :deploy
