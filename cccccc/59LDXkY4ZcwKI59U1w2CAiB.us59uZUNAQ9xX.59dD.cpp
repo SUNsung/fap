da0c8856bade73212ac5bc7ad71c5c2aff6f1554
SUNsung <weatherius@gmail.com>
2018-02-04 13:09:57 +0200

    Rewrite [59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp] File in: 2018-02-04 13:09:57

--- cccccc/59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp
+++ cccccc/59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp
@@ -1,86 +1,106 @@
 
         
-        #endif  // GTEST_INCLUDE_GTEST_GTEST_TEST_PART_H_
-
-    
-      // Waits for the death test to finish and returns its status.
-  virtual int Wait() = 0;
-    
-    // A handy wrapper for AddRef.
-#define GTEST_ADD_REF_(T) typename ::std::tr1::gtest_internal::AddRef<T>::type
-    
-    $range i 1..n-1
-$for i [[
-$range j 1..i
-$range k i+1..n
-template <$for j, [[typename T$j]]>
-struct Types<$for j, [[T$j]]$for k[[, internal::None]]> {
-  typedef internal::Types$i<$for j, [[T$j]]> type;
-};
-    
-    #include <boost/algorithm/string/join.hpp>
-#include <boost/algorithm/string/predicate.hpp>
+        void read_image(std::ifstream* image_file, std::ifstream* label_file,
+        uint32_t index, uint32_t rows, uint32_t cols,
+        char* pixels, char* label) {
+  image_file->seekg(index * rows * cols + 16);
+  image_file->read(pixels, rows * cols);
+  label_file->seekg(index + 8);
+  label_file->read(label, 1);
+}
     
       /**
-   * @brief A convenience method to check if the return code is 0
+   * @brief Computes the Contrastive error gradient w.r.t. the inputs.
    *
-   * @code{.cpp}
-   *   auto s = doSomething();
-   *   if (s.ok()) {
-   *     LOG(INFO) << 'doing work';
-   *   } else {
-   *     LOG(ERROR) << s.toString();
-   *   }
-   * @endcode
+   * Computes the gradients with respect to the two input vectors (bottom[0] and
+   * bottom[1]), but not the similarity label (bottom[2]).
    *
-   * @return a boolean which is true if the status code is 0, false otherwise.
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *      respect to the outputs
+   *   -# @f$ (1 \times 1 \times 1 \times 1) @f$
+   *      This Blob's diff will simply contain the loss_weight* @f$ \lambda @f$,
+   *      as @f$ \lambda @f$ is the coefficient of this layer's output
+   *      @f$\ell_i@f$ in the overall Net loss
+   *      @f$ E = \lambda_i \ell_i + \mbox{other loss terms}@f$; hence
+   *      @f$ \frac{\partial E}{\partial \ell_i} = \lambda_i @f$.
+   *      (*Assuming that this top Blob is not used as a bottom (input) by any
+   *      other layer of the Net.)
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length 2)
+   *   -# @f$ (N \times C \times 1 \times 1) @f$
+   *      the features @f$a@f$; Backward fills their diff with
+   *      gradients if propagate_down[0]
+   *   -# @f$ (N \times C \times 1 \times 1) @f$
+   *      the features @f$b@f$; Backward fills their diff with gradients if
+   *      propagate_down[1]
    */
-  bool ok() const { return getCode() == 0; }
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
     
-      // All control should be from a single daemon.
-  // Wrap all IOCTL API handling in locks to guarantee proper use.
-  lck_mtx_lock(osquery.mtx);
-  switch (cmd) {
-  // Daemon is requesting a new subscription (e.g., monitored path).
-  case OSQUERY_IOCTL_SUBSCRIPTION:
-    sub = (osquery_subscription_args_t *)data;
-    if ((err = subscribe_to_event(sub->event, sub->subscribe))) {
-      goto error_exit;
+    #endif  // CAFFE_CUDNN_SIGMOID_LAYER_HPP_
+
+    
+    
+    { protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual inline bool reverse_dimensions() { return true; }
+  virtual void compute_output_shape();
+};
+    
+      /**
+   * @brief Computes the error gradient w.r.t. the exp inputs.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *      respect to the outputs
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
+   *      with respect to computed outputs @f$ y @f$
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$; Backward fills their diff with
+   *      gradients @f$
+   *        \frac{\partial E}{\partial x} =
+   *            \frac{\partial E}{\partial y} y \alpha \log_e(gamma)
+   *      @f$ if propagate_down[0]
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+                progress = update_progress(progress, r, repeat_num, 0);
+            ts->printf( cvtest::TS::LOG, '\n');
+            prepareForTest(bg, camMat, distCoeffs, brds_num, cbg);
+    
+            bad_caller = caller;
+        bad_caller.dpdr = &bad_dpdr_c3;
+        errors += run_test_case( CV_StsBadArg, 'Bad dpdr format', bad_caller );
+    
+    
+    {
+    {        std::vector<cv::Point2d>::const_iterator it0 = pix0.begin();
+        std::vector<cv::Point2d>::const_iterator it1 = pix1.begin();
+        int row = 0;
+        for (;it0 != pix0.end(); ++it0, ++it1)
+        {
+            cv::Point2d d = invEps*(*it0 - *it1);
+            jac.at<double>(row, col) = d.x;
+            ++row;
+            jac.at<double>(row, col) = d.y;
+            ++row;
+        }
     }
-    break;
-    }
+}
     
-    #include <osquery/config.h>
-#include <osquery/database.h>
-    
-    /**
- * @brief Split a given string based on an delimiter.
- *
- * @param s the string that you'd like to split.
- * @param delim the delimiter which you'd like to split the string by.
- * @param occurrences the number of times to split by delim.
- *
- * @return a vector of strings split by delim for occurrences.
- */
-std::vector<std::string> split(const std::string& s,
-                               const std::string& delim,
-                               size_t occurences);
-    
-      virtual bool isHidden() const CXX11_OVERRIDE;
-    
-    
-    {} // namespace aria2
-    
-    class ApiCallbackDownloadEventListener : public DownloadEventListener {
-public:
-  ApiCallbackDownloadEventListener(Session* session,
-                                   DownloadEventCallback callback,
-                                   void* userData);
-  virtual ~ApiCallbackDownloadEventListener();
-  virtual void onEvent(DownloadEvent event,
-                       const RequestGroup* group) CXX11_OVERRIDE;
-    }
-    
-      virtual TLSSessionSide getSide() const CXX11_OVERRIDE { return side_; }
-    
-    #endif // D_ASYNC_NAME_RESOLVER_MAN_H
+            Mat_<double> dr3_dt2, dt3_dt2;
+           diff.dTv2(dr3_dt2, dt3_dt2);
