2e3824c7d2ab4cf50208b8b9652a0363a934c6c2
SUNsung <weatherius@gmail.com>
2018-02-04 12:46:54 +0200

    Rewrite [42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py] File in: 2018-02-04 12:46:54

--- pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
+++ pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
@@ -1,75 +1,242 @@
 
         
-                # put all lines in the file into a Python list
-        strings = f.readlines()
-        
-        # above line leaves trailing newline characters; strip them out
-        strings = [x.strip(u'\n') for x in strings]
-        
-        # remove empty-lines and comments
-        strings = [x for x in strings if x and not x.startswith(u'#')]
-        
-        # insert empty string since all are being removed
-        strings.insert(0, u'')
+                Operator:                  '#582800',   # class: 'o'
+        Operator.Word:             'bold #004461',   # class: 'ow' - like keywords
     
-    # Aliases for types that are spelled differently in different Python
-# versions. bytes_type is deprecated and no longer used in Tornado
-# itself but is left in case anyone outside Tornado is using it.
-bytes_type = bytes
-if PY3:
-    unicode_type = str
-    basestring_type = str
-else:
-    # The names unicode and basestring don't exist in py3 so silence flake8.
-    unicode_type = unicode  # noqa
-    basestring_type = basestring  # noqa
+        :param method: method for the new :class:`Request` object.
+    :param url: URL for the new :class:`Request` object.
+    :param params: (optional) Dictionary or bytes to be sent in the query string for the :class:`Request`.
+    :param data: (optional) Dictionary or list of tuples ``[(key, value)]`` (will be form-encoded), bytes, or file-like object to send in the body of the :class:`Request`.
+    :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param headers: (optional) Dictionary of HTTP Headers to send with the :class:`Request`.
+    :param cookies: (optional) Dict or CookieJar object to send with the :class:`Request`.
+    :param files: (optional) Dictionary of ``'name': file-like-objects`` (or ``{'name': file-tuple}``) for multipart encoding upload.
+        ``file-tuple`` can be a 2-tuple ``('filename', fileobj)``, 3-tuple ``('filename', fileobj, 'content_type')``
+        or a 4-tuple ``('filename', fileobj, 'content_type', custom_headers)``, where ``'content-type'`` is a string
+        defining the content type of the given file and ``custom_headers`` a dict-like object containing additional headers
+        to add for the file.
+    :param auth: (optional) Auth tuple to enable Basic/Digest/Custom HTTP Auth.
+    :param timeout: (optional) How many seconds to wait for the server to send data
+        before giving up, as a float, or a :ref:`(connect timeout, read
+        timeout) <timeouts>` tuple.
+    :type timeout: float or tuple
+    :param allow_redirects: (optional) Boolean. Enable/disable GET/OPTIONS/POST/PUT/PATCH/DELETE/HEAD redirection. Defaults to ``True``.
+    :type allow_redirects: bool
+    :param proxies: (optional) Dictionary mapping protocol to the URL of the proxy.
+    :param verify: (optional) Either a boolean, in which case it controls whether we verify
+            the server's TLS certificate, or a string, in which case it must be a path
+            to a CA bundle to use. Defaults to ``True``.
+    :param stream: (optional) if ``False``, the response content will be immediately downloaded.
+    :param cert: (optional) if String, path to ssl client cert file (.pem). If Tuple, ('cert', 'key') pair.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
     
+        while True:
+        more_to_read = select.select([sock], [], [], timeout)[0]
+        if not more_to_read:
+            break
     
-class WSGIContainerTest(AsyncHTTPTestCase):
-    def wsgi_app(self, environ, start_response):
-        status = '200 OK'
-        response_headers = [('Content-Type', 'text/plain')]
-        start_response(status, response_headers)
-        return [b'Hello world!']
-    
-        def _sock_state_cb(self, fd, readable, writable):
-        state = ((IOLoop.READ if readable else 0) |
-                 (IOLoop.WRITE if writable else 0))
-        if not state:
-            self.io_loop.remove_handler(fd)
-            del self.fds[fd]
-        elif fd in self.fds:
-            self.io_loop.update_handler(fd, state)
-            self.fds[fd] = state
-        else:
-            self.io_loop.add_handler(fd, self._handle_events, state)
-            self.fds[fd] = state
+    #: Python 2.x?
+is_py2 = (_ver[0] == 2)
     
     
-class EntryHandler(BaseHandler):
-    def get(self, slug):
-        entry = db.Query(Entry).filter('slug =', slug).get()
-        if not entry:
-            raise tornado.web.HTTPError(404)
-        self.render('entry.html', entry=entry)
+@pytest.mark.skipif(sys.version_info[:2] != (2,6), reason='Only run on Python 2.6')
+def test_system_ssl_py26():
+    '''OPENSSL_VERSION_NUMBER isn't provided in Python 2.6, verify we don't
+    blow up in this case.
+    '''
+    assert info()['system_ssl'] == {'version': ''}
+    
+    from .compat import StringIO, cStringIO
     
     
-@gen.coroutine
-def c2():
-    pass
+@ignore_warnings
+def benchmark(metrics=tuple(v for k, v in sorted(METRICS.items())),
+              formats=tuple(v for k, v in sorted(FORMATS.items())),
+              samples=1000, classes=4, density=.2,
+              n_times=5):
+    '''Times metric calculations for a number of inputs
+    
+    fixed_batch_size_comparison(X)
+variable_batch_size_comparison(X)
+plt.show()
+
     
     
-# Using HTTP POST, upload one or more files in a single multipart-form-encoded
-# request.
-@gen.coroutine
-def multipart_producer(boundary, filenames, write):
-    boundary_bytes = boundary.encode()
+def make_linkcode_resolve(package, url_fmt):
+    '''Returns a linkcode_resolve function for the given URL format
     
-    Useful to test features that will eventually run on S3, or if you want to
-run something locally that was once running on S3.
+            # split the paragraph into fake smaller paragraphs to make the
+        # problem harder e.g. more similar to tweets
+        if lang in ('zh', 'ja'):
+        # FIXME: whitespace tokenizing does not work on chinese and japanese
+            continue
+        words = content.split()
+        n_groups = len(words) / n_words_per_short_text
+        if n_groups < 1:
+            continue
+        groups = np.array_split(words, n_groups)
     
-        # Do a little work. Alternately, could leave this script running and
-    # poke at it with a browser.
-    client = httpclient.AsyncHTTPClient()
-    yield client.fetch('http://127.0.0.1:8888/dummy/')
-    yield client.fetch('http://127.0.0.1:8888/dummyasync/', raise_error=False)
+    
+if not os.path.exists(TRAIN_FOLDER) or not os.path.exists(TEST_FOLDER):
+    
+        # TASK: Predict the outcome on the testing set and store it in a variable
+    # named y_predicted
+    
+    # Author: Virgile Fritsch <virgile.fritsch@inria.fr>
+# License: BSD 3 clause
+    
+    The dataset is generated using the ``make_biclusters`` function, which
+creates a matrix of small values and implants bicluster with large
+values. The rows and columns are then shuffled and passed to the
+Spectral Co-Clustering algorithm. Rearranging the shuffled matrix to
+make biclusters contiguous shows how accurately the algorithm found
+the biclusters.
+    
+            clf_score = brier_score_loss(y_test, prob_pos, pos_label=y.max())
+        print('%s:' % name)
+        print('\tBrier: %1.3f' % (clf_score))
+        print('\tPrecision: %1.3f' % precision_score(y_test, y_pred))
+        print('\tRecall: %1.3f' % recall_score(y_test, y_pred))
+        print('\tF1: %1.3f\n' % f1_score(y_test, y_pred))
+    
+    from alembic import op
+import sqlalchemy as sa
+    
+    
+def downgrade():
+    op.drop_column('tables', 'description')
+
+    
+    
+def upgrade():
+    op.add_column('metrics', sa.Column('changed_by_fk', sa.Integer(), nullable=True))
+    op.add_column('metrics', sa.Column('changed_on', sa.DateTime(), nullable=True))
+    op.add_column('metrics', sa.Column('created_by_fk', sa.Integer(), nullable=True))
+    op.add_column('metrics', sa.Column('created_on', sa.DateTime(), nullable=True))
+    try:
+        op.alter_column('columns', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('columns', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('css_templates', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('css_templates', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('dashboards', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('dashboards', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('datasources', 'changed_by_fk',
+                   existing_type=sa.INTEGER(),
+                   nullable=True)
+        op.alter_column('datasources', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('datasources', 'created_by_fk',
+                   existing_type=sa.INTEGER(),
+                   nullable=True)
+        op.alter_column('datasources', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('dbs', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('dbs', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('slices', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('slices', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('sql_metrics', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('sql_metrics', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('table_columns', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('table_columns', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('tables', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('tables', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('url', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('url', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.create_foreign_key(None, 'metrics', 'ab_user', ['changed_by_fk'], ['id'])
+        op.create_foreign_key(None, 'metrics', 'ab_user', ['created_by_fk'], ['id'])
+    except:
+        pass
+    
+    
+from __future__ import with_statement
+import threading
+import weakref
+import sys
+    
+            Schedules the callable to be executed as fn(*args, **kwargs) and returns
+        a Future instance representing the execution of the callable.
+    
+            Args:
+            max_workers: The maximum number of processes that can be used to
+                execute the given calls. If None or not given then as many
+                worker processes will be created as the machine has processors.
+        '''
+        _check_system_limits()
+    
+    
+  def Start( self ):
+    request_data = BuildRequestData( self._buffer_number )
+    if self._extra_data:
+      request_data.update( self._extra_data )
+    request_data[ 'event_name' ] = self._event_name
+    
+      match = GETBUFVAR_REGEX.search( value )
+  if match:
+    buffer_number = int( match.group( 'buffer_number' ) )
+    option = match.group( 'option' )
+    return _MockGetBufferVariable( buffer_number, option )
+    
+      vim_current.buffer.__setitem__.assert_called_with(
+      slice( None, None, None ), [ 'test' ] )
+    
+    
+def _FormatYcmdDebugInfo( ycmd ):
+  python = ycmd[ 'python' ]
+  clang = ycmd[ 'clang' ]
+  message = ( 'Server Python interpreter: {0}\n'
+              'Server Python version: {1}\n'
+              'Server has Clang support compiled in: {2}\n'
+              'Clang version: {3}\n'.format( python[ 'executable' ],
+                                             python[ 'version' ],
+                                             clang[ 'has_support' ],
+                                             clang[ 'version' ] ) )
+  extra_conf = ycmd[ 'extra_conf' ]
+  extra_conf_path = extra_conf[ 'path' ]
+  if not extra_conf_path:
+    message += 'No extra configuration file found\n'
+  elif not extra_conf[ 'is_loaded' ]:
+    message += ( 'Extra configuration file found but not loaded\n'
+                 'Extra configuration path: {0}\n'.format( extra_conf_path ) )
+  else:
+    message += ( 'Extra configuration file found and loaded\n'
+                 'Extra configuration path: {0}\n'.format( extra_conf_path ) )
+  return message
