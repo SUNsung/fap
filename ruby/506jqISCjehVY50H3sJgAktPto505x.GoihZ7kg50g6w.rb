536beeb39352dd782df2bb4b89344a63e684982d
SUNsung <weatherius@gmail.com>
2018-02-05 11:34:31 +0200

    Rewrite [506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb] File in: 2018-02-05 11:34:30

--- ruby/506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb
+++ ruby/506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb
@@ -1,86 +1,35 @@
 
         
-          def initialize(blob, variation_or_variation_key)
-    @blob, @variation = blob, ActiveStorage::Variation.wrap(variation_or_variation_key)
-  end
+            projects << @user.contributed_projects.visible_to_user(current_user) if current_user
+    projects << @user.contributed_projects.public_to_user(current_user)
     
-    class TestResponseTest < ActiveSupport::TestCase
-  def assert_response_code_range(range, predicate)
-    response = ActionDispatch::TestResponse.new
-    (0..599).each do |status|
-      response.status = status
-      assert_equal range.include?(status), response.send(predicate),
-                   'ActionDispatch::TestResponse.new(#{status}).#{predicate}'
+        def register_compass_extension
+      ::Compass::Frameworks.register(
+          'bootstrap',
+          :version               => Bootstrap::VERSION,
+          :path                  => gem_path,
+          :stylesheets_directory => stylesheets_path,
+          :templates_directory   => File.join(gem_path, 'templates')
+      )
     end
+    
+    require 'open-uri'
+require 'json'
+require 'strscan'
+require 'forwardable'
+require 'term/ansicolor'
+require 'fileutils'
+    
+      def test_font_helper_with_suffix_sharp
+    assert_match %r(url\(['']?/assets/.*svg#.+['']?\)), @css
   end
     
-    module ActionMailer
-  # The <tt>ActionMailer::DeliveryJob</tt> class is used when you
-  # want to send emails outside of the request-response cycle.
-  #
-  # Exceptions are rescued and handled by the mailer class.
-  class DeliveryJob < ActiveJob::Base # :nodoc:
-    queue_as { ActionMailer::Base.deliver_later_queue_name }
-    
-      # If a given extension is listed multiple times, prefer the first one listed
-  extensions.reject! { |extension| mimes.values.flatten.include?(extension) }
-    
-    #
-    
-              if e.end_with?('/')
-            entry.in_path?(
-              item
-            )
-    
-    If you run into trouble, you can find helpful resources at https://jekyllrb.com/help/!
-            MSG
-            raise Jekyll::Errors::MissingDependencyException, name
-          end
-        end
-      end
+      describe 'environment' do
+    it 'adds the fuse directories to the appropriate paths' do
+      expect(ENV).to receive(:append_path).with('PKG_CONFIG_PATH', any_args)
+      expect(ENV).to receive(:append_path).with('HOMEBREW_LIBRARY_PATHS', any_args)
+      expect(ENV).to receive(:append_path).with('HOMEBREW_INCLUDE_PATHS', any_args)
+      subject.modify_build_environment
     end
   end
 end
-
-    
-      class FeatureTopicUsers < Jobs::Base
-    
-        find_union(segments, Project).includes(:namespace).order_id_desc
-  end
-    
-          case Rails.env
-      when 'development'
-        config.eager_load = false
-      when 'test'
-        config.eager_load = false
-      when 'production'
-        config.eager_load = true
-      end
-    end
-  end
-end
-    
-      GEMFILE_EXTENSIONS.each do |extension|
-    extension_pathname = root.join('Gemfile#{extension}')
-    
-          when :banner
-        # Because some ftp server send multiple banner we take only the first one and ignore the rest
-        if not (s[:info])
-          s[:info] = matches
-          report_service(s)
-        end
-    
-          case matched
-        when :ok
-          # Last command was successful, in addition most servers transmit a banner with the first +OK
-          case s[:last]
-            when nil
-              # Its the first +OK must include the banner, worst case its just +OK
-              s[:info]  = matches
-              s[:proto] = 'tcp'
-              s[:name]  = 'pop3'
-              report_service(s)
-    
-          # There is only one pattern per run to test
-      matched = nil
-      matches = nil
