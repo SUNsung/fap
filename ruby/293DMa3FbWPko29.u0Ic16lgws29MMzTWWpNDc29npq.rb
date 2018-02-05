5a3ea146f3b495a6a491c370336b65940cfa6ac0
SUNsung <weatherius@gmail.com>
2018-02-05 20:13:08 +0200

    Rewrite [293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb] File in: 2018-02-05 20:13:08

--- ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
+++ ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
@@ -1,69 +1,18 @@
 
         
-        # Create an array of vendored mimetype => [extensions]
-mimes = {}
-json = open('https://raw.githubusercontent.com/jshttp/mime-db/master/db.json').read
-data = JSON.parse(json)
-data.reject! { |mime, meta| meta['extensions'].nil? || meta['extensions'].empty? }
-data.each do |mime, meta|
-  # Normalize extensions and mime-types
-  mime = mime.downcase.strip
-  extensions = meta['extensions'].map { |e| e.downcase.strip }.compact
-    
-          # Group an array of items by an expression
-      #
-      # input - the object array
-      # variable - the variable to assign each item to in the expression
-      # expression -a Liquid comparison expression passed in as a string
-      #
-      # Returns the filtered array of objects
-      def group_by_exp(input, variable, expression)
-        return input unless groupable?(input)
-    
-    profile = Profile.new
-# puts profile.generate
-command = ['/usr/bin/sandbox-exec', '-p', profile.generate, profile.pod_bin, *ARGV]
-exec(*command)
-
-    
-            def self.options
-          [[
-            '--all', 'Remove all the cached pods without asking'
-          ]].concat(super)
+                # Tags are declared in the server but computed in the connection. This allows us per-connection tailored tags.
+        def new_tagged_logger
+          TaggedLoggerProxy.new server.logger,
+            tags: server.config.log_tags.map { |tag| tag.respond_to?(:call) ? tag.call(request) : tag.to_s.camelize }
         end
     
-            def signal_end_of_output
-          output_pipe.puts(END_OF_OUTPUT_SIGNAL)
-          STDOUT.flush
+          private
+        def generated_attribute_methods
+          @generated_attribute_methods ||= Module.new.tap { |mod| include mod }
         end
     
-        def self.register(klass, attachment_name, attachment_options)
-      instance.register(klass, attachment_name, attachment_options)
+    module MiddlewareTest
+  class MyMiddleware
+    def initialize(app)
+      @app = app
     end
-    
-          @klass.send :define_method, @name do |*args|
-        ivar = '@attachment_#{name}'
-        attachment = instance_variable_get(ivar)
-    
-        # Returns the id of the instance.
-    def id attachment, style_name
-      attachment.instance.id
-    end
-    
-    module Paperclip
-  class << self
-    attr_writer :registered_attachments_styles_path
-    def registered_attachments_styles_path
-      @registered_attachments_styles_path ||= Rails.root.join('public/system/paperclip_attachments.yml').to_s
-    end
-  end
-    
-        logger.info('Installing pear package', :package => input_package,
-                  :directory => staging_path)
-    ::Dir.chdir(staging_path) do
-      safesystem('pear', '-c', config, 'install', '-n', '-f', input_package)
-    end
-    
-      option '--user', 'USER',
-    'Set the user to USER in the prototype files.',
-    :default => 'root'
