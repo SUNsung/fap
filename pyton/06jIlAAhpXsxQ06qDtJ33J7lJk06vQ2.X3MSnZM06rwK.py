345ac516f781df41dd0e87b57cbac5100a959404
SUNsung <weatherius@gmail.com>
2018-02-05 14:05:37 +0200

    Rewrite [06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py] File in: 2018-02-05 14:05:37

--- pyton/06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py
+++ pyton/06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py
@@ -1,46 +1,144 @@
 
-def JsonFromFuture( future ):
-  response = future.result()
-  _ValidateResponseObject( response )
-  if response.status_code == BaseRequest.Requests().codes.server_error:
-    raise MakeServerException( response.json() )
+        
+        import io
+import optparse
     
-    
-  def Done( self ):
-    return bool( self._response_future ) and self._response_future.done()
-    
-      with MockArbitraryBuffer( 'cpp' ):
-    with MockEventNotification( DiagnosticResponse ):
-      ycm.OnFileReadyToParse()
-      ok_( ycm.FileParseRequestReady() )
-      ycm.HandleFileParseRequest()
-      vim_command.assert_has_calls( [
-        PlaceSign_Call( 2, 2, 1, False ),
-        UnplaceSign_Call( 1, 1 )
-      ] )
-      eq_( ycm.GetErrorCount(), 0 )
-      eq_( ycm.GetWarningCount(), 1 )
-    
-    
-@patch( 'ycm.vimsupport.VariableExists', return_value = False )
-@patch( 'ycm.vimsupport.SearchInCurrentBuffer', return_value = 0 )
-@patch( 'vim.current' )
-def InsertNamespace_insert_test( vim_current, *args ):
-  contents = [ '',
-               'namespace Taqueria {',
-               '',
-               '  int taco = Math' ]
-  vim_current.buffer = VimBuffer( '', contents = contents )
-    }
-    
-    
-def SendDebugInfoRequest( extra_data = None ):
-  request = DebugInfoRequest( extra_data )
-  # This is a blocking call.
-  request.Start()
-  return request.Response()
+    with io.open(README_FILE, 'w', encoding='utf-8') as f:
+    f.write(header)
+    f.write(options)
+    f.write(footer)
 
     
     
-  def Response( self ):
-    return self._response
+def list_extractors(age_limit):
+    '''
+    Return a list of extractors that are suitable for the given age,
+    sorted by extractor ID.
+    '''
+    
+    
+class AcademicEarthCourseIE(InfoExtractor):
+    _VALID_URL = r'^https?://(?:www\.)?academicearth\.org/playlists/(?P<id>[^?#/]+)'
+    IE_NAME = 'AcademicEarth:Course'
+    _TEST = {
+        'url': 'http://academicearth.org/playlists/laws-of-nature/',
+        'info_dict': {
+            'id': 'laws-of-nature',
+            'title': 'Laws of Nature',
+            'description': 'Introduce yourself to the laws of nature with these free online college lectures from Yale, Harvard, and MIT.',
+        },
+        'playlist_count': 3,
+    }
+    
+        _TESTS = [{
+        'url': 'http://camwithher.tv/view_video.php?viewkey=6e9a24e2c0e842e1f177&page=&viewtype=&category=',
+        'info_dict': {
+            'id': '5644',
+            'ext': 'flv',
+            'title': 'Periscope Tease',
+            'description': 'In the clouds teasing on periscope to my favorite song',
+            'duration': 240,
+            'view_count': int,
+            'comment_count': int,
+            'uploader': 'MileenaK',
+            'upload_date': '20160322',
+        },
+        'params': {
+            'skip_download': True,
+        }
+    }, {
+        'url': 'http://camwithher.tv/view_video.php?viewkey=6dfd8b7c97531a459937',
+        'only_matching': True,
+    }, {
+        'url': 'http://camwithher.tv/view_video.php?page=&viewkey=6e9a24e2c0e842e1f177&viewtype=&category=',
+        'only_matching': True,
+    }, {
+        'url': 'http://camwithher.tv/view_video.php?viewkey=b6c3b5bea9515d1a1fc4&page=&viewtype=&category=mv',
+        'only_matching': True,
+    }]
+    
+        def short_desc(self):
+        '''
+        A short description of the command
+        '''
+        return ''
+    
+        def syntax(self):
+        return '[options] <spider>'
+    
+        def run(self, args, opts):
+        if opts.list:
+            self._list_templates()
+            return
+        if opts.dump:
+            template_file = self._find_template(opts.dump)
+            if template_file:
+                with open(template_file, 'r') as f:
+                    print(f.read())
+            return
+        if len(args) != 2:
+            raise UsageError()
+    
+            return request
+    
+    # The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+version = '0'
+# The full version, including alpha/beta/rc tags.
+release = '0'
+    
+        @classmethod
+    def _call(cls, enhancement):
+        from certbot.display.enhancements import ask
+        return ask(enhancement)
+    
+    def downgrade():
+    constraint = find_constraint_name(False) or 'fk_columns_datasource_name_datasources'
+    with op.batch_alter_table('columns',
+        naming_convention=naming_convention) as batch_op:
+        batch_op.drop_constraint(constraint, type_='foreignkey')
+        batch_op.create_foreign_key(
+            'fk_columns_column_name_datasources',
+            'datasources',
+            ['column_name'], ['datasource_name'])
+
+    
+    def upgrade():
+    op.add_column('dashboards', sa.Column('slug', sa.String(length=255), nullable=True))
+    try:
+        op.create_unique_constraint('idx_unique_slug', 'dashboards', ['slug'])
+    except:
+        pass
+    
+    from alembic import op
+import sqlalchemy as sa
+    
+    Revision ID: 43df8de3a5f4
+Revises: 7dbf98566af7
+Create Date: 2016-01-18 23:43:16.073483
+    
+    # A list of ignored prefixes for module index sorting.
+#modindex_common_prefix = []
+    
+      The parameters are:
+    - |reason| a textual description of the reason for the known issue. This
+               is used for the skip reason
+    - |exception_matchers| additional arguments are hamcrest matchers to apply
+                 to the exception thrown. If the matchers don't match, then the
+                 test is marked as error, with the original exception.
+    
+    
+  def Start( self ):
+    request_data = BuildRequestData()
+    if self._extra_data:
+      request_data.update( self._extra_data )
+    request_data.update( {
+      'completer_target': self._completer_target,
+      'command_arguments': self._arguments
+    } )
+    with HandleServerException():
+      self._response = self.PostDataToHandler( request_data,
+                                               'run_completer_command' )
