c84f284dcd734156a246135deca476d9ea20008c
SUNsung <weatherius@gmail.com>
2018-02-05 04:54:34 +0200

    Rewrite [58KGovPY3zzV.58zkNU58UaOUs58wCzcfeKRKEU58url.rb] File in: 2018-02-05 04:54:33

--- ruby/58KGovPY3zzV.58zkNU58UaOUs58wCzcfeKRKEU58url.rb
+++ ruby/58KGovPY3zzV.58zkNU58UaOUs58wCzcfeKRKEU58url.rb
@@ -1,97 +1,34 @@
 
         
-          def validate_evernote_options
-    unless evernote_consumer_key.present? &&
-      evernote_consumer_secret.present? &&
-      evernote_oauth_token.present?
-      errors.add(:base, 'Evernote ENV variables and a Service are required')
-    end
-  end
+        puts '\nUnable to find an RSS feed for the following blogs:'
+puts '==================================================='
+unavailable.each do |b|
+  puts '#{b.name} | #{b.web_url}'
+end
+puts '==================================================='
+
     
-          if respond_to?(:stop)
-        stop
-      else
-        terminate_thread!
-      end
-    end
-    
-      included do
-    include Oauthable
-    
-      module SortableTableHelper
-    # :call-seq:
-    #   sortable_column(attribute, default_direction = 'desc', name: attribute.humanize)
-    def sortable_column(attribute, default_direction = nil, options = nil)
-      if options.nil? && (options = Hash.try_convert(default_direction))
-        default_direction = nil
-      end
-      default_direction ||= 'desc'
-      options ||= {}
-      name = options[:name] || attribute.humanize
-      selected = @table_sort_info[:attribute].to_s == attribute
-      if selected
-        direction = @table_sort_info[:direction]
-        new_direction = direction.to_s == 'desc' ? 'asc' : 'desc'
-        classes = 'selected #{direction}'
-      else
-        classes = ''
-        new_direction = default_direction
-      end
-      link_to(name, url_for(sort: '#{attribute}.#{new_direction}'), class: classes)
+          expect('.border-style-false-third').to have_ruleset(ruleset)
+      expect('.border-style-false-third').to_not have_rule(bad_rule)
     end
   end
 end
 
     
-        process_font_assets
-    process_stylesheet_assets
-    process_javascript_assets
-    store_version
-  end
+      context 'called with one color' do
+    it 'applies same width to all sides' do
+      rule = 'border-width: 1px'
     
-      def setup
-    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
-    success = Dir.chdir DUMMY_PATH do
-      silence_stdout_if !ENV['VERBOSE'] do
-        system 'node', 'manifest.js', tmp_dir
-      end
+      context 'called with arguments (1, $value: 2em)' do
+    it 'outputs double the first value from the default scale' do
+      expect('.one-base-two').to have_rule('font-size: 2.5em')
     end
-    assert success, 'Node.js Mincer compilation failed'
-    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
-    css_name = manifest['assets']['application.css']
-    @css = File.read('#{tmp_dir}/#{css_name}')
   end
-end
-
     
-    desc 'Dumps output to a CSS file for testing'
-task :debug do
-  require 'sass'
-  path = Bootstrap.stylesheets_path
-  %w(bootstrap).each do |file|
-    engine = Sass::Engine.for_file('#{path}/#{file}.scss', syntax: :scss, load_paths: [path])
-    File.open('./#{file}.css', 'w') { |f| f.write(engine.render) }
-  end
-end
+      context 'called with no prefixes' do
+    it 'outputs the spec' do
+      rule = 'appearance: none;'
     
-    
-    
-                  # Remove it form the session objects so freeup
-              sessions.delete(s[:session])
-    
-    		temp[1].strip!
-		temp[1] = temp[1].scan(/\w+/)
-    
-            def log_level?(level, min_level)
-          log_levels[level] >= log_levels[min_level]
-        end
-    
-      config = Merb::Plugins.config[:sass] || Merb::Plugins.config['sass'] || {}
-    
-          private
-    
-        it 'sets the output verbosity' do
-      expect(config.output_verbosity).to eq 0
+          expect('.size-both').to have_ruleset(rule)
     end
-    
-              private
+  end
