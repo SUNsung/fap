da57a56edfb62d1fd0fcdf7ee352cf783944d9f8
SUNsung <weatherius@gmail.com>
2018-02-05 15:55:34 +0200

    Rewrite [45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb] File in: 2018-02-05 15:55:34

--- ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
+++ ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
@@ -1,71 +1,35 @@
 
         
-          def present(payload)
-    if payload.is_a?(Hash)
-      payload = ActiveSupport::HashWithIndifferentAccess.new(payload)
-      MAIN_KEYS.each do |key|
-        return { :title => payload[key].to_s, :entries => present_hash(payload, key) } if payload.has_key?(key)
-      end
+        class Jekyll::Commands::NewTheme < Jekyll::Command
+  class << self
+    def init_with_program(prog)
+      prog.command(:'new-theme') do |c|
+        c.syntax 'new-theme NAME'
+        c.description 'Creates a new Jekyll theme scaffold'
+        c.option 'code_of_conduct', \
+          '-c', '--code-of-conduct', \
+          'Include a Code of Conduct. (defaults to false)'
     
-      def evernote_oauth_token
-    service && service.token
-  end
-end
-
-    
-        respond_to do |format|
-      format.html { redirect_back events_path, notice: 'Event deleted.' }
-      format.json { head :no_content }
+          message = TestMailer.send_test(args[:to_address])
+      Email::Sender.new(message, :test_message).send
     end
-  end
     
-        respond_to do |format|
-      if !running? && @job.destroy
-        format.html { redirect_to jobs_path, notice: 'Job deleted.' }
-        format.json { head :no_content }
-      else
-        format.html { redirect_to jobs_path, alert: 'Can not delete a running job.' }
-        format.json { render json: @job.errors, status: :unprocessable_entity }
-      end
-    end
-  end
     
-            # Defines a capability for the given provider. The block should return
-        # a class/module that has a method with the capability name, ready
-        # to be executed. This means that if it is an instance method,
-        # the block should return an instance of the class.
-        #
-        # @param [String] provider The name of the provider
-        # @param [String] cap The name of the capability
-        def self.provider_capability(provider, cap, &block)
-          components.provider_capabilities[provider.to_sym].register(cap.to_sym, &block)
-          nil
-        end
-    
-          def markdown_podfile
-        UI::ErrorReport.markdown_podfile
-      end
-    
-            # Checks if a template URL is given else returns the TEMPLATE_REPO URL
-        #
-        # @return String
-        #
-        def template_repo_url
-          @template_url || TEMPLATE_REPO
+    {    # @include transition(#{border-color ease-in-out .15s, box-shadow ease-in-out .15s})
+    # to
+    # @include transition(border-color ease-in-out .15s, box-shadow ease-in-out .15s)
+    def deinterpolate_vararg_mixins(scss)
+      scss = scss.dup
+      VARARG_MIXINS.each do |mixin|
+        if scss.gsub! /(@include\s*#{Regexp.quote(mixin)})\(\s*\#\{([^}]+)\}\s*\)/, '\1(\2)'
+          log_transform mixin
         end
       end
+      scss
     end
-  end
-end
-
     
-    namespace :gem do
-  def version
-    require 'spree/core/version'
-    Spree.version
-  end
-    
-            def show
-          @stock_movement = scope.find(params[:id])
-          respond_with(@stock_movement)
-        end
+      # The test environment is used exclusively to run your application's
+  # test suite. You never need to work with it otherwise. Remember that
+  # your test database is 'scratch space' for the test suite and is wiped
+  # and recreated between test runs. Don't rely on the data there!
+  config.cache_classes = true
