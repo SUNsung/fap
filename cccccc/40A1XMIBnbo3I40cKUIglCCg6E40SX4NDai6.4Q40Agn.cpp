d5ec44023f30bfc8abbea06946cb8297e80408f1
SUNsung <weatherius@gmail.com>
2018-02-06 07:07:55 +0200

    Rewrite [40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp] File in: 2018-02-06 07:07:54

--- cccccc/40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp
+++ cccccc/40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp
@@ -1,298 +1,154 @@
 
         
-        // Generate destructors.
-#include 'ipc/struct_destructor_macros.h'
-#include 'content/nw/src/common/common_message_generator.h'
+        
+    {}  // namespace atom
     
-    class Dispatcher : public content::RenderViewObserver {
- public:
-  explicit Dispatcher(content::RenderView* render_view);
-  ~Dispatcher() final;
+      void Attach(mate::Arguments* args);
+  bool IsAttached();
+  void Detach();
+  void SendCommand(mate::Arguments* args);
+    
+    namespace api {
     }
     
-      bool handled = true;
-  base::ThreadRestrictions::ScopedAllowIO allow_io;
-  base::ThreadRestrictions::ScopedAllowWait allow_wait;
-  IPC_BEGIN_MESSAGE_MAP(DispatcherHost, message)
-    IPC_MESSAGE_HANDLER(ShellViewHostMsg_Allocate_Object, OnAllocateObject)
-    IPC_MESSAGE_HANDLER(ShellViewHostMsg_Deallocate_Object, OnDeallocateObject)
-    IPC_MESSAGE_HANDLER(ShellViewHostMsg_Call_Object_Method, OnCallObjectMethod)
-    IPC_MESSAGE_HANDLER(ShellViewHostMsg_Call_Object_Method_Sync,
-                        OnCallObjectMethodSync)
-    IPC_MESSAGE_HANDLER(ShellViewHostMsg_Call_Static_Method, OnCallStaticMethod)
-    IPC_MESSAGE_HANDLER(ShellViewHostMsg_Call_Static_Method_Sync,
-                        OnCallStaticMethodSync)
-    IPC_MESSAGE_HANDLER(ShellViewHostMsg_UncaughtException,
-                        OnUncaughtException);
-    IPC_MESSAGE_HANDLER(ShellViewHostMsg_GetShellId, OnGetShellId);
-    IPC_MESSAGE_HANDLER(ShellViewHostMsg_CreateShell, OnCreateShell);
-    IPC_MESSAGE_HANDLER(ShellViewHostMsg_AllocateId, OnAllocateId);
-    IPC_MESSAGE_HANDLER(ShellViewHostMsg_SetForceClose, OnSetForceClose);
-    IPC_MESSAGE_UNHANDLED(handled = false)
-  IPC_END_MESSAGE_MAP()
+      static void BuildPrototype(v8::Isolate* isolate,
+                             v8::Local<v8::FunctionTemplate> prototype);
     
-      static void ClearObjectRegistry();
+    // The OSX fork() implementation can crash in the child process before
+// fork() returns.  In that case, the shutdown pipe will still be
+// shared with the parent process.  To prevent child crashes from
+// causing parent shutdowns, |g_pipe_pid| is the pid for the process
+// which registered |g_shutdown_pipe_write_fd|.
+// See <http://crbug.com/175341>.
+pid_t g_pipe_pid = -1;
+int g_shutdown_pipe_write_fd = -1;
+int g_shutdown_pipe_read_fd = -1;
     
     
-namespace nwapi {
-    }
-    
-    Menu::~Menu() {
-  Destroy();
-}
-    
-    
-    {}  // namespace nw
+    {}  // namespace atom
 
     
-    void MenuItem::SetChecked(bool checked) {
-  // Set active will cause 'activate' to be emitted, so block here
-  block_active_ = true;
-  gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(menu_item_), checked);
-  block_active_ = false;
+    
+    { private:
+  DISALLOW_COPY_AND_ASSIGN(AtomQuotaPermissionContext);
+};
+    
+    void AutoUpdater::SetFeedURL(const std::string& url,
+                             const HeaderMap& requestHeaders) {
 }
     
-      base::ThreadRestrictions::ScopedAllowWait allow_wait;
+      // URLRequestJob:
+  void GetResponseInfo(net::HttpResponseInfo* info) override;
     
-      for(auto& data : params->data) {
-    if (!reader->Read(data)) {
-      *error = reader->error();
-      return false;
+      /**
+   * @brief Applies the transformation defined in the data layer's
+   * transform_param block to a cv::Mat
+   *
+   * @param cv_img
+   *    cv::Mat containing the data to be transformed.
+   * @param transformed_blob
+   *    This is destination blob. It can be part of top blob's data if
+   *    set_cpu_data() is used. See image_data_layer.cpp for an example.
+   */
+  void Transform(const cv::Mat& cv_img, Blob<Dtype>* transformed_blob);
+#endif  // USE_OPENCV
+    
+      static vector<string> LayerTypeList() {
+    CreatorRegistry& registry = Registry();
+    vector<string> layer_types;
+    for (typename CreatorRegistry::iterator iter = registry.begin();
+         iter != registry.end(); ++iter) {
+      layer_types.push_back(iter->first);
     }
-    response->Append(data.ToValue());
+    return layer_types;
   }
     
-        // Finish and check for builder errors
-    if (s.ok()) {
-      s = builder->Finish();
-      if (s.ok()) {
-        meta->file_size = builder->FileSize();
-        assert(meta->file_size > 0);
-      }
-    } else {
-      builder->Abandon();
-    }
-    delete builder;
     
+    {  Blob<Dtype> col_buffer_;
+  Blob<Dtype> bias_multiplier_;
+};
     
-    {}  // namespace leveldb
+      bool handles_setup_;
+  cudnnHandle_t             handle_;
+  cudnnLRNDescriptor_t norm_desc_;
+  cudnnTensorDescriptor_t bottom_desc_, top_desc_;
     
-    struct leveldb_filterpolicy_t : public FilterPolicy {
-  void* state_;
-  void (*destructor_)(void*);
-  const char* (*name_)(void*);
-  char* (*create_)(
-      void*,
-      const char* const* key_array, const size_t* key_length_array,
-      int num_keys,
-      size_t* filter_length);
-  unsigned char (*key_match_)(
-      void*,
-      const char* key, size_t length,
-      const char* filter, size_t filter_length);
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
+    
+    namespace caffe {
     }
     
-    #ifndef STORAGE_LEVELDB_DB_DB_ITER_H_
-#define STORAGE_LEVELDB_DB_DB_ITER_H_
-    
-    // A helper class useful for DBImpl::Get()
-class LookupKey {
- public:
-  // Initialize *this for looking up user_key at a snapshot with
-  // the specified sequence number.
-  LookupKey(const Slice& user_key, SequenceNumber sequence);
-    }
-    
-    std::string TempFileName(const std::string& dbname, uint64_t number) {
-  assert(number > 0);
-  return MakeFileName(dbname, number, 'dbtmp');
-}
+      virtual inline const char* type() const { return 'Exp'; }
     
     
-    {  ASSERT_TRUE(! Overlaps(NULL, 'j'));
-  ASSERT_TRUE(! Overlaps('r', NULL));
-  ASSERT_TRUE(Overlaps(NULL, 'p'));
-  ASSERT_TRUE(Overlaps(NULL, 'p1'));
-  ASSERT_TRUE(Overlaps('q', NULL));
-  ASSERT_TRUE(Overlaps(NULL, NULL));
-}
     
-    void WriteBatch::Put(const Slice& key, const Slice& value) {
-  WriteBatchInternal::SetCount(this, WriteBatchInternal::Count(this) + 1);
-  rep_.push_back(static_cast<char>(kTypeValue));
-  PutLengthPrefixedSlice(&rep_, key);
-  PutLengthPrefixedSlice(&rep_, value);
-}
     
-            // Create values for key-value pair
-        const int k = (order == SEQUENTIAL) ? i + j :
-                      (rand_.Next() % num_entries);
-        char key[100];
-        snprintf(key, sizeof(key), '%016d', k);
     
-      /// Construct a buffer to represent a given memory range.
-  const_buffer(const void* data, std::size_t size)
-    : data_(data),
-      size_(size)
-  {
+    #if COCOS2D_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+    
+    
+    {
+    {			body->CreateFixture(&fd);
+		}
+	}
+    
+      std::vector<std::string> line_exports;
+  unsigned int readonly = 0;
+  int options_index = -1;
+    
+    using SubscriptionRef = std::shared_ptr<Subscription>;
+using BaseEventPublisher = EventPublisher<SubscriptionContext, EventContext>;
+using EventPublisherRef = std::shared_ptr<BaseEventPublisher>;
+using SubscriptionContextRef = std::shared_ptr<SubscriptionContext>;
+using EventContextRef = std::shared_ptr<EventContext>;
+using BaseEventSubscriber = EventSubscriber<BaseEventPublisher>;
+using EventSubscriberRef = std::shared_ptr<EventSubscriber<BaseEventPublisher>>;
+    
+    /**
+ * @brief Perform an action while waiting for an the extension timeout.
+ *
+ * We define a 'global' extension timeout using CLI flags.
+ * There are several locations where code may act assuming an extension has
+ * loaded or broadcasted a registry.
+ *
+ * @param predicate return true or set stop to end the timeout loop.
+ * @return the last status from the predicate.
+ */
+Status applyExtensionDelay(std::function<Status(bool& stop)> predicate);
+    
+    /**
+ * @brief Create an alias to a command line flag.
+ *
+ * Like OSQUERY_FLAG, do not use this in the osquery codebase. Use the derived
+ * macros that abstract the tail of boolean arguments.
+ */
+#define OSQUERY_FLAG_ALIAS(t, a, n, s, e)                                      \
+  FlagAlias<t> FLAGS_##a(#a, #t, #n, &FLAGS_##n);                              \
+  namespace flags {                                                            \
+  static GFLAGS_NAMESPACE::FlagRegisterer oflag_##a(                           \
+      #a, #a, #a, &FLAGS_##n, &FLAGS_##n);                                     \
+  const int flag_alias_##a = Flag::createAlias(#a, {#n, s, e, 0, 1});          \
   }
     
-    #include <boost/asio/detail/pop_options.hpp>
+    kern_return_t OsqueryStart(kmod_info_t *ki, void *d) {
+  dbg_printf('Kernel module starting!\n');
+    }
     
-    #if !defined(BOOST_ASIO_HAS_THREADS)
-typedef long atomic_count;
-inline void increment(atomic_count& a, long b) { a += b; }
-#elif defined(BOOST_ASIO_HAS_STD_ATOMIC)
-typedef std::atomic<long> atomic_count;
-inline void increment(atomic_count& a, long b) { a += b; }
-#else // defined(BOOST_ASIO_HAS_STD_ATOMIC)
-typedef boost::detail::atomic_count atomic_count;
-inline void increment(atomic_count& a, long b) { while (b > 0) ++a, --b; }
-#endif // defined(BOOST_ASIO_HAS_STD_ATOMIC)
+    #include <osquery/config.h>
+#include <osquery/dispatcher.h>
     
-    #endif // BOOST_ASIO_DETAIL_CALL_STACK_HPP
-
-    
-    #if !defined(BOOST_ASIO_WINDOWS_RUNTIME)
-    
-    #endif // BOOST_ASIO_DETAIL_HANDLER_INVOKE_HELPERS_HPP
-
-    
-    void buffer_sequence_adapter_base::init_native_buffer(
-    buffer_sequence_adapter_base::native_buffer_type& buf,
-    const boost::asio::mutable_buffer& buffer)
-{
-  std::memset(&buf, 0, sizeof(native_buffer_type));
-  Microsoft::WRL::ComPtr<IInspectable> insp
-    = Microsoft::WRL::Make<winrt_buffer_impl>(buffer);
-  buf = reinterpret_cast<Windows::Storage::Streams::IBuffer^>(insp.Get());
-}
-    
-      sorted_vector_map<int, Movable> vmap(
-      std::make_move_iterator(s.begin()), std::make_move_iterator(s.end()));
-    
-      __m128i arr1;
-  if (HAYSTACK_ALIGNED) {
-    arr1 = _mm_load_si128(
-        reinterpret_cast<const __m128i*>(haystack.data() + blockStartIdx));
-  } else {
-    arr1 = _mm_loadu_si128(
-        reinterpret_cast<const __m128i*>(haystack.data() + blockStartIdx));
+    /// Safely convert a string representation of an integer base.
+inline Status safeStrtoll(const std::string& rep, size_t base, long long& out) {
+  char* end{nullptr};
+  out = strtoll(rep.c_str(), &end, static_cast<int>(base));
+  if (end == nullptr || end == rep.c_str() || *end != '\0' ||
+      ((out == LLONG_MIN || out == LLONG_MAX) && errno == ERANGE)) {
+    out = 0;
+    return Status(1);
   }
-    
-    #pragma once
-    
-    TEST_F(MockEnvTest, Corrupt) {
-  const std::string kGood = 'this is a good string, synced to disk';
-  const std::string kCorrupted = 'this part may be corrupted';
-  const std::string kFileName = '/dir/f';
-  unique_ptr<WritableFile> writable_file;
-  ASSERT_OK(env_->NewWritableFile(kFileName, &writable_file, soptions_));
-  ASSERT_OK(writable_file->Append(kGood));
-  ASSERT_TRUE(writable_file->GetFileSize() == kGood.size());
-    }
-    
-      virtual bool Merge(const Slice& key,
-                     const Slice* existing_value,
-                     const Slice& value,
-                     std::string* new_value,
-                     Logger* logger) const override;
-    
-      // size amplification = percentage of additional size
-  if (candidate_size * 100 < ratio * earliest_file_size) {
-    ROCKS_LOG_BUFFER(
-        log_buffer,
-        '[%s] Universal: size amp not needed. newer-files-total-size %' PRIu64
-        ' earliest-file-size %' PRIu64,
-        cf_name.c_str(), candidate_size, earliest_file_size);
-    return nullptr;
-  } else {
-    ROCKS_LOG_BUFFER(
-        log_buffer,
-        '[%s] Universal: size amp needed. newer-files-total-size %' PRIu64
-        ' earliest-file-size %' PRIu64,
-        cf_name.c_str(), candidate_size, earliest_file_size);
-  }
-  assert(start_index < sorted_runs.size() - 1);
-    
-      // Write 8 files in L0
-  for (int i = 0; i < 8; i++) {
-    GenerateNewRandomFile(&rnd);
-  }
-  dbfull()->TEST_WaitForCompact();
-    
-    inline size_t Roundup(size_t x, size_t y) {
-  return ((x + y - 1) / y) * y;
+  return Status(0);
 }
-    
-      // Helper functions for DumpTable()
-  Status DumpIndexBlock(WritableFile* out_file);
-  Status DumpDataBlocks(WritableFile* out_file);
-  void DumpKeyValue(const Slice& key, const Slice& value,
-                    WritableFile* out_file);
-    
-      // REQUIRES: The client must provide a merge operator if Merge operation
-  // needs to be accessed. Calling Merge on a DB without a merge operator
-  // would result in Status::NotSupported. The client must ensure that the
-  // merge operator supplied here has the same name and *exactly* the same
-  // semantics as the merge operator provided to previous open calls on
-  // the same DB. The only exception is reserved for upgrade, where a DB
-  // previously without a merge operator is introduced to Merge operation
-  // for the first time. It's necessary to specify a merge operator when
-  // opening the DB in this case.
-  // Default: nullptr
-  std::shared_ptr<MergeOperator> merge_operator = nullptr;
-    
-    bool nextConnection(int tid)
-{
-    m.lock();
-    int socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (socketfd == -1) {
-        cout << 'FD error, connections: ' << connections << endl;
-        return false;
-    }
-    }
-    
-    int getKb(int pid) {
-    std::string line;
-    std::ifstream self((std::string('/proc/') + std::to_string(pid) + std::string('/status')).c_str());
-    int vmRSS;
-    while(!self.eof()) {
-        std::getline(self, line, ':');
-        if (line == 'VmRSS') {
-            self >> vmRSS;
-        }
-        std::getline(self, line);
-    }
-    return vmRSS;
-}
-    
-        // Format message frame(s)
-    unsigned char *framePackBufferOffset = framePackBuffer;
-    for (int i = 0; i < framesPerSend; i++) {
-        framePackBufferOffset[0] = 130;
-        if (byteSize < 126) {
-            framePackBufferLength += byteSize + 6;
-            framePackBufferOffset[1] = 128 | byteSize;
-            framePackBufferOffset += byteSize + 6;
-        } else if (byteSize <= UINT16_MAX) {
-            framePackBufferLength += byteSize + 8;
-            framePackBufferOffset[1] = 128 | 126;
-            *((uint16_t *) &framePackBufferOffset[2]) = htons(byteSize);
-            framePackBufferOffset += byteSize + 8;
-        } else {
-            framePackBufferLength += byteSize + 14;
-            framePackBufferOffset[1] = 128 | 127;
-            *((uint64_t *) &framePackBufferOffset[2]) = htobe64(byteSize);
-            framePackBufferOffset += byteSize + 14;
-        }
-    }
-    
-    #endif // EXTENSIONS_UWS_H
-
-    
-        Hub(int extensionOptions = 0, bool useDefaultLoop = false, unsigned int maxPayload = 16777216) : uS::Node(LARGE_BUFFER_SIZE, WebSocketProtocol<SERVER, WebSocket<SERVER>>::CONSUME_PRE_PADDING, WebSocketProtocol<SERVER, WebSocket<SERVER>>::CONSUME_POST_PADDING, useDefaultLoop),
-                                             Group<SERVER>(extensionOptions, maxPayload, this, nodeData), Group<CLIENT>(0, maxPayload, this, nodeData) {
-        inflateInit2(&inflationStream, -15);
-        inflationBuffer = new char[LARGE_BUFFER_SIZE];
-    }
