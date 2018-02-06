1a6b0d37b23e4c776a1872a4726d6592943685ee
SUNsung <weatherius@gmail.com>
2018-02-06 07:41:06 +0200

    Rewrite [index.cpp] File in: 2018-02-06 07:41:05

--- cccccc/index.cpp
+++ cccccc/index.cpp
@@ -1,93 +1,169 @@
 
         
-        #if !defined(BOOST_ASIO_HAS_THREADS)
-typedef long atomic_count;
-inline void increment(atomic_count& a, long b) { a += b; }
-#elif defined(BOOST_ASIO_HAS_STD_ATOMIC)
-typedef std::atomic<long> atomic_count;
-inline void increment(atomic_count& a, long b) { a += b; }
-#else // defined(BOOST_ASIO_HAS_STD_ATOMIC)
-typedef boost::detail::atomic_count atomic_count;
-inline void increment(atomic_count& a, long b) { while (b > 0) ++a, --b; }
-#endif // defined(BOOST_ASIO_HAS_STD_ATOMIC)
+        namespace nw {
+    }
     
-      // Return a pointer to the beginning of the unread data.
-  const_buffer data() const
-  {
-    return boost::asio::buffer(buffer_) + begin_offset_;
+    
+    {  DISALLOW_COPY_AND_ASSIGN(Base);
+};
+    
+    #include 'base/strings/string_piece.h'
+#include 'v8/include/v8.h'
+    
+    void Clipboard::SetText(std::string& text) {
+  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
+  ui::Clipboard::ObjectMap map;
+  map[ui::Clipboard::CBF_TEXT].push_back(
+      std::vector<char>(text.begin(), text.end()));
+  clipboard->WriteObjects(ui::CLIPBOARD_TYPE_COPY_PASTE, map);
+}
+    
+    
+    {}  // namespace nwapi
+    
+      RenderView* render_view = GetCurrentRenderView();
+  if (!render_view) {
+    args.GetReturnValue().Set(isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate,
+                                     'Unable to get render view in CreateShell'))));
+    return;
   }
     
-    #ifndef BOOST_ASIO_DETAIL_FUNCTION_HPP
-#define BOOST_ASIO_DETAIL_FUNCTION_HPP
+      if (type == 'App') {
+    content::Shell* shell =
+        content::Shell::FromRenderViewHost(render_view_host());
+    nwapi::App::Call(shell, method, arguments, result, this);
+    return;
+  } else if (type == 'Screen') {
+    nwapi::Screen::Call(this, method, arguments, result);
+    return;
+  }
     
-    #ifndef BOOST_ASIO_DETAIL_HANDLER_INVOKE_HELPERS_HPP
-#define BOOST_ASIO_DETAIL_HANDLER_INVOKE_HELPERS_HPP
-    
-        // Update number of buckets and initialise all buckets to empty.
-    bucket_type* tmp = new bucket_type[num_buckets_];
-    delete[] buckets_;
-    buckets_ = tmp;
-    for (std::size_t i = 0; i < num_buckets_; ++i)
-      buckets_[i].first = buckets_[i].last = end_iter;
-    
-    namespace boost {
-namespace asio {
-namespace detail {
-namespace descriptor_ops {
-    }
-    }
-    }
+    bool MenuDelegate::IsItemForCommandIdDynamic(int command_id) const {
+  if (command_id < 0)
+    return false;
     }
     
-    			b2BodyDef bd;
-			bd.type = b2_dynamicBody;
-			bd.angularDamping = 2.0f;
-			bd.linearDamping = 0.5f;
+    void Menu::Popup(int x, int y, content::Shell* shell) {
+  GdkEventButton* event = NULL; //FIXME: shell->web_contents()->GetRenderWidgetHostView()->GetLastMouseDown();
+  uint32_t triggering_event_time = event ? event->time : GDK_CURRENT_TIME;
+  gfx::Point point;
+  if (!event) {
+    // gfx::Rect bounds = shell->web_contents()->GetRenderWidgetHostView()->GetViewBounds();
+    // point = gfx::Point(x + bounds.x(), y + bounds.y());
+    DVLOG(1) << 'no last mouse down event';
+    point = gfx::Point(x, y);
+  }else
+    point = gfx::Point(event->x_root, event->y_root);
+    }
+    
+    bool MenuItem::GetChecked() {
+  return is_checked_;
+}
+    
+      private:
+    bool ReadText(ClipboardData& data) {
+      DCHECK(data.type == TYPE_TEXT);
+      base::string16 text;
+      clipboard_->ReadText(ui::CLIPBOARD_TYPE_COPY_PASTE, &text);
+      data.data.reset(new std::string(base::UTF16ToUTF8(text)));
+      return true;
+    }
+    
+    
+    {  DECLARE_EXTENSION_FUNCTION('nw.Clipboard.clearSync', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwClipboardClearSyncFunction);
+};
     
     
     {
-    {			body->CreateFixture(&fd);
-		}
-	}
-    
-    
-    {			b2EdgeShape shape;
-			shape.Set(b2Vec2(-40.0f, 0.0f), b2Vec2(40.0f, 0.0f));
-			ground->CreateFixture(&shape, 0.0f);
-		}
-    
-    				b2Vec2 anchor(-15.0f + 1.0f * i, 5.0f);
-				jd.Initialize(prevBody, body, anchor);
-				m_world->CreateJoint(&jd);
-    
-    			float32 hs[10] = {0.25f, 1.0f, 4.0f, 0.0f, 0.0f, -1.0f, -2.0f, -2.0f, -1.25f, 0.0f};
-    
-      std::unique_ptr<AuthConfig> getUserDefinedAuthConfig() const;
-    
-      void setBtRuntime(const std::shared_ptr<BtRuntime>& btRuntime);
-    
-    #endif // D_ADAPTIVE_FILE_ALLOCATION_ITERATOR_H
+    {        ASSERT_LT(cvtest::norm(va, vo, cv::NORM_L2), 1e-9);
+    }
+}
 
     
-    void AnnounceTier::nextEvent()
-{
-  switch (event) {
-  case STARTED:
-    event = DOWNLOADING;
-    break;
-  case STARTED_AFTER_COMPLETION:
-    event = SEEDING;
-    break;
-  case STOPPED:
-    event = HALTED;
-    break;
-  case COMPLETED:
-    event = SEEDING;
-    break;
-  default:
-    break;
-  }
-}
+                ts->printf( cvtest::TS::LOG, 'artificial corners\n');
+            runTest(bg.size(), camMat, distCoeffs, brds_num, cbg.cornersSize(), ARTIFICIAL_CORNERS);
+            progress = update_progress(progress, r, repeat_num, 0);
     
-    void configureAsyncNameResolverMan(AsyncNameResolverMan* asyncNameResolverMan,
-                                   Option* option);
+            bad_caller = caller;
+        bad_caller.jacobian = (CvMat*)zeros.ptr();
+        errors += run_test_case( CV_StsBadArg, 'Bad jacobian ', bad_caller );
+    
+            gray = cvCreateImage( cvSize( img.width, img.height ), IPL_DEPTH_8U, 1 );
+        thresh = cvCreateImage( cvSize( img.width, img.height ), IPL_DEPTH_8U, 1 );
+        cvCvtColor( &img, gray, CV_BGR2GRAY );
+    
+            if (norm(dr3_dr1, dr3dr1) > thres || norm(dt3_dr1, dt3dr1) > thres)
+        {
+            ts->printf( cvtest::TS::LOG, 'Invalid derivates by r1\n' );
+            ts->set_failed_test_info(cvtest::TS::FAIL_BAD_ACCURACY);
+        }
+    
+    struct RepoAuthType;
+struct StringData;
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    
+      explicit ValIter(ArrayData* arr)
+    : m_arr(arr)
+    , m_kind(arr->kind())
+  {
+    assert(isMixed(m_kind) || m_kind == kPackedKind || m_kind == kVecKind);
+    if (isMixed(m_kind)) {
+      m_iterMixed = asMixed(arr)->data();
+      m_stopMixed = m_iterMixed + asMixed(arr)->m_used;
+    } else {
+      m_iterPacked = reinterpret_cast<TypedValue*>(arr + 1);
+      m_stopPacked = m_iterPacked + arr->m_size;
+    }
+  }
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you use this binding you'll need to call 5 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXX_CreateFontsTexture(), ImGui_ImplXXXX_NewFrame(), ImGui_ImplXXXX_Render() and ImGui_ImplXXXX_Shutdown().
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+    struct GLFWwindow;
+    
+    static const char* ImGui_Marmalade_GetClipboardText(void* /*user_data*/)
+{
+    if (!s3eClipboardAvailable())
+        return NULL;
+    }
+    
+            static float f = 0.0f;
+        ImGui::Text('Hello, world!');
+        ImGui::SliderFloat('float', &f, 0.0f, 1.0f);
+        ImGui::Text('Application average %.3f ms/frame (%.1f FPS)', 1000.0f / io.Framerate, io.Framerate);
+        ImGui::ShowDemoWindow(NULL);
+    
+    
+    {        // Rendering
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui::Render();
+        glfwSwapBuffers(window);
+    }
+    
+        // Create The Image Views
+    {
+        VkImageViewCreateInfo info = {};
+        info.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+        info.viewType = VK_IMAGE_VIEW_TYPE_2D;
+        info.format = g_SurfaceFormat.format;
+        info.components.r = VK_COMPONENT_SWIZZLE_R;
+        info.components.g = VK_COMPONENT_SWIZZLE_G;
+        info.components.b = VK_COMPONENT_SWIZZLE_B;
+        info.components.a = VK_COMPONENT_SWIZZLE_A;
+        info.subresourceRange = g_ImageRange;
+        for (uint32_t i = 0; i < g_BackBufferCount; i++)
+        {
+            info.image = g_BackBuffer[i];
+            err = vkCreateImageView(g_Device, &info, g_Allocator, &g_BackBufferView[i]);
+            check_vk_result(err);
+        }
+    }
