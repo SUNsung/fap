023465f97d1d7c8d232c431711c6206c782bd218
SUNsung <weatherius@gmail.com>
2018-02-06 23:34:11 +0200

    Rewrite [11K1vG63mYufg119k2mckGD8Nc11OVUxJP25mso11RVD.py] File in: 2018-02-06 23:34:10

--- pyton/11K1vG63mYufg119k2mckGD8Nc11OVUxJP25mso11RVD.py
+++ pyton/11K1vG63mYufg119k2mckGD8Nc11OVUxJP25mso11RVD.py
@@ -1,20 +1,90 @@
 
         
-                headers = [
-            '%s: %s' % (
-                name,
-                value if isinstance(value, str) else value.decode('utf8')
-            )
-            for name, value in headers.items()
-        ]
+        import word2vec
+    
+        optimizer = tf.train.AdagradOptimizer(learning_rate=0.1)
+    train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
+    return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
+    
+    py_library(
+    name = 'linear',
+    srcs = [
+        'linear.py',
+    ],
+    srcs_version = 'PY2AND3',
+    deps = [
+        '//tensorflow:tensorflow_py',
+    ],
+)
+    
+          ps_device_spec.merge_from(current_device)
+      return ps_device_spec.to_string()
+    else:
+      worker_device_spec = pydev.DeviceSpec.from_string(worker_device or '')
+      worker_device_spec.merge_from(current_device)
+      return worker_device_spec.to_string()
+  return _local_device_chooser
+
     
     
-class Formatting(object):
-    '''A delegate class that invokes the actual processors.'''
+if __name__ == '__main__':
+    from glmnet.elastic_net import Lasso as GlmnetLasso
+    from sklearn.linear_model import Lasso as ScikitLasso
+    # Delayed import of matplotlib.pyplot
+    import matplotlib.pyplot as plt
     
-    import requests
-from requests import __version__ as requests_version
-from pygments import __version__ as pygments_version
+    In the second benchmark, we increase the number of dimensions of the
+training set. Then we plot the computation time as function of
+the number of dimensions.
     
-            if ext:
-            fn += ext
+                gc.collect()
+            print('benchmarking lars_path (with Gram):', end='')
+            sys.stdout.flush()
+            tstart = time()
+            G = np.dot(X.T, X)  # precomputed Gram matrix
+            Xy = np.dot(X.T, y)
+            lars_path(X, y, Xy=Xy, Gram=G, method='lasso')
+            delta = time() - tstart
+            print('%0.3fs' % delta)
+            results['lars_path (with Gram)'].append(delta)
+    
+                D_results_build[algorithm][i] = (t1 - t0)
+            D_results_query[algorithm][i] = (t2 - t1)
+    
+    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
+TRAIN_FOLDER = '20news-bydate-train'
+TEST_FOLDER = '20news-bydate-test'
+    
+    
+if __name__ == '__main__':
+    # NOTE: we put the following in a 'if __name__ == '__main__'' protected
+    # block to be able to use a multi-core grid search that also works under
+    # Windows, see: http://docs.python.org/library/multiprocessing.html#windows
+    # The multiprocessing module is used as the backend of joblib.Parallel
+    # that is used when n_jobs != 1 in GridSearchCV
+    
+        def add_card(self, card):
+        self.cards.append(card)
+    
+        def get(self, query)
+        '''Get the stored query result from the cache.
+        
+        Accessing a node updates its position to the front of the LRU list.
+        '''
+        node = self.lookup[query]
+        if node is None:
+            return None
+        self.linked_list.move_to_front(node)
+        return node.results
+    
+    
+class Transaction(object):
+    
+        def mapper_sort(self, key, value):
+        '''Construct key to ensure proper sorting.
+    
+    
+class LookupService(object):
+    
+    
+class RemoveDuplicateUrls(MRJob):
