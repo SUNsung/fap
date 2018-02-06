8c70560f01d9416c65bf74ab95a61164ba1bc771
SUNsung <weatherius@gmail.com>
2018-02-06 18:46:38 +0200

    Rewrite [index.cpp] File in: 2018-02-06 18:46:38

--- cccccc/index.cpp
+++ cccccc/index.cpp
@@ -1,169 +1,68 @@
 
         
-        namespace nw {
-    }
+          v8::Handle<v8::String> source = v8::String::NewExternal(isolate,
+                                                          new extensions::StaticV8ExternalOneByteStringResource(
+          GetStringResource(resource_id)));
+  v8::Handle<v8::String> wrapped_source = WrapSource(source);
     
-    
-    {  DISALLOW_COPY_AND_ASSIGN(Base);
-};
-    
-    #include 'base/strings/string_piece.h'
-#include 'v8/include/v8.h'
-    
-    void Clipboard::SetText(std::string& text) {
-  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
-  ui::Clipboard::ObjectMap map;
-  map[ui::Clipboard::CBF_TEXT].push_back(
-      std::vector<char>(text.begin(), text.end()));
-  clipboard->WriteObjects(ui::CLIPBOARD_TYPE_COPY_PASTE, map);
+    // static
+int DispatcherHost::AllocateId() {
+  return next_object_id_++;
 }
     
+    typedef std::map<std::string,std::string> KeyMap;
     
-    {}  // namespace nwapi
-    
-      RenderView* render_view = GetCurrentRenderView();
-  if (!render_view) {
-    args.GetReturnValue().Set(isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate,
-                                     'Unable to get render view in CreateShell'))));
-    return;
-  }
-    
-      if (type == 'App') {
-    content::Shell* shell =
-        content::Shell::FromRenderViewHost(render_view_host());
-    nwapi::App::Call(shell, method, arguments, result, this);
-    return;
-  } else if (type == 'Screen') {
-    nwapi::Screen::Call(this, method, arguments, result);
-    return;
-  }
-    
-    bool MenuDelegate::IsItemForCommandIdDynamic(int command_id) const {
-  if (command_id < 0)
-    return false;
+    bool NwAppGetArgvSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
     }
     
-    void Menu::Popup(int x, int y, content::Shell* shell) {
-  GdkEventButton* event = NULL; //FIXME: shell->web_contents()->GetRenderWidgetHostView()->GetLastMouseDown();
-  uint32_t triggering_event_time = event ? event->time : GDK_CURRENT_TIME;
-  gfx::Point point;
-  if (!event) {
-    // gfx::Rect bounds = shell->web_contents()->GetRenderWidgetHostView()->GetViewBounds();
-    // point = gfx::Point(x + bounds.x(), y + bounds.y());
-    DVLOG(1) << 'no last mouse down event';
-    point = gfx::Point(x, y);
-  }else
-    point = gfx::Point(event->x_root, event->y_root);
-    }
-    
-    bool MenuItem::GetChecked() {
-  return is_checked_;
-}
-    
-      private:
-    bool ReadText(ClipboardData& data) {
-      DCHECK(data.type == TYPE_TEXT);
-      base::string16 text;
-      clipboard_->ReadText(ui::CLIPBOARD_TYPE_COPY_PASTE, &text);
-      data.data.reset(new std::string(base::UTF16ToUTF8(text)));
-      return true;
-    }
-    
-    
-    {  DECLARE_EXTENSION_FUNCTION('nw.Clipboard.clearSync', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwClipboardClearSyncFunction);
-};
-    
-    
-    {
-    {        ASSERT_LT(cvtest::norm(va, vo, cv::NORM_L2), 1e-9);
-    }
-}
+    #endif // BOOST_ASIO_DETAIL_DATE_TIME_FWD_HPP
 
     
-                ts->printf( cvtest::TS::LOG, 'artificial corners\n');
-            runTest(bg.size(), camMat, distCoeffs, brds_num, cbg.cornersSize(), ARTIFICIAL_CORNERS);
-            progress = update_progress(progress, r, repeat_num, 0);
-    
-            bad_caller = caller;
-        bad_caller.jacobian = (CvMat*)zeros.ptr();
-        errors += run_test_case( CV_StsBadArg, 'Bad jacobian ', bad_caller );
-    
-            gray = cvCreateImage( cvSize( img.width, img.height ), IPL_DEPTH_8U, 1 );
-        thresh = cvCreateImage( cvSize( img.width, img.height ), IPL_DEPTH_8U, 1 );
-        cvCvtColor( &img, gray, CV_BGR2GRAY );
-    
-            if (norm(dr3_dr1, dr3dr1) > thres || norm(dt3_dr1, dt3dr1) > thres)
-        {
-            ts->printf( cvtest::TS::LOG, 'Invalid derivates by r1\n' );
-            ts->set_failed_test_info(cvtest::TS::FAIL_BAD_ACCURACY);
-        }
-    
-    struct RepoAuthType;
-struct StringData;
-    
-    ///////////////////////////////////////////////////////////////////////////////
-    
-      explicit ValIter(ArrayData* arr)
-    : m_arr(arr)
-    , m_kind(arr->kind())
-  {
-    assert(isMixed(m_kind) || m_kind == kPackedKind || m_kind == kVecKind);
-    if (isMixed(m_kind)) {
-      m_iterMixed = asMixed(arr)->data();
-      m_stopMixed = m_iterMixed + asMixed(arr)->m_used;
-    } else {
-      m_iterPacked = reinterpret_cast<TypedValue*>(arr + 1);
-      m_stopPacked = m_iterPacked + arr->m_size;
-    }
-  }
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you use this binding you'll need to call 5 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXX_CreateFontsTexture(), ImGui_ImplXXXX_NewFrame(), ImGui_ImplXXXX_Render() and ImGui_ImplXXXX_Shutdown().
-// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-    struct GLFWwindow;
-    
-    static const char* ImGui_Marmalade_GetClipboardText(void* /*user_data*/)
+    template <typename Time_Traits>
+void dev_poll_reactor::schedule_timer(timer_queue<Time_Traits>& queue,
+    const typename Time_Traits::time_type& time,
+    typename timer_queue<Time_Traits>::per_timer_data& timer, wait_op* op)
 {
-    if (!s3eClipboardAvailable())
-        return NULL;
+  boost::asio::detail::mutex::scoped_lock lock(mutex_);
     }
     
-            static float f = 0.0f;
-        ImGui::Text('Hello, world!');
-        ImGui::SliderFloat('float', &f, 0.0f, 1.0f);
-        ImGui::Text('Application average %.3f ms/frame (%.1f FPS)', 1000.0f / io.Framerate, io.Framerate);
-        ImGui::ShowDemoWindow(NULL);
+      grpc::ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  std::cerr << 'Server listening on ' << server_address << std::endl;
     
-    
-    {        // Rendering
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui::Render();
-        glfwSwapBuffers(window);
+    namespace NamespaceC {
     }
     
-        // Create The Image Views
-    {
-        VkImageViewCreateInfo info = {};
-        info.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-        info.viewType = VK_IMAGE_VIEW_TYPE_2D;
-        info.format = g_SurfaceFormat.format;
-        info.components.r = VK_COMPONENT_SWIZZLE_R;
-        info.components.g = VK_COMPONENT_SWIZZLE_G;
-        info.components.b = VK_COMPONENT_SWIZZLE_B;
-        info.components.a = VK_COMPONENT_SWIZZLE_A;
-        info.subresourceRange = g_ImageRange;
-        for (uint32_t i = 0; i < g_BackBufferCount; i++)
-        {
-            info.image = g_BackBuffer[i];
-            err = vkCreateImageView(g_Device, &info, g_Allocator, &g_BackBufferView[i]);
-            check_vk_result(err);
-        }
-    }
+    MonsterStorage::Service::Service() {
+  AddMethod(new ::grpc::internal::RpcServiceMethod(
+      MonsterStorage_method_names[0],
+      ::grpc::internal::RpcMethod::NORMAL_RPC,
+      new ::grpc::internal::RpcMethodHandler< MonsterStorage::Service, flatbuffers::grpc::Message<Monster>, flatbuffers::grpc::Message<Stat>>(
+          std::mem_fn(&MonsterStorage::Service::Store), this)));
+  AddMethod(new ::grpc::internal::RpcServiceMethod(
+      MonsterStorage_method_names[1],
+      ::grpc::internal::RpcMethod::SERVER_STREAMING,
+      new ::grpc::internal::ServerStreamingHandler< MonsterStorage::Service, flatbuffers::grpc::Message<Stat>, flatbuffers::grpc::Message<Monster>>(
+          std::mem_fn(&MonsterStorage::Service::Retrieve), this)));
+}
+    
+    // Computes how many bytes you'd have to pad to be able to write an
+// 'scalar_size' scalar if the buffer had grown to 'buf_size' (downwards in
+// memory).
+inline size_t PaddingBytes(size_t buf_size, size_t scalar_size) {
+  return ((~buf_size) + 1) & (scalar_size - 1);
+}
+    
+    template<typename T> T HashFnv1(const char *input) {
+  T hash = FnvTraits<T>::kOffsetBasis;
+  for (const char *c = input; *c; ++c) {
+    hash *= FnvTraits<T>::kFnvPrime;
+    hash ^= static_cast<unsigned char>(*c);
+  }
+  return hash;
+}
+    
+    
+    {}  // namespace flatbuffers
