fe1580c495da5f3dc0706d55478b87ca903a80bb
SUNsung <weatherius@gmail.com>
2018-02-06 08:38:03 +0200

    Rewrite [42mRgUVQTpNMc42tEMzKsZJFYY428WJhEx8oXfc42Vg0.py] File in: 2018-02-06 08:38:02

--- pyton/42mRgUVQTpNMc42tEMzKsZJFYY428WJhEx8oXfc42Vg0.py
+++ pyton/42mRgUVQTpNMc42tEMzKsZJFYY428WJhEx8oXfc42Vg0.py
@@ -1,44 +1,81 @@
 
         
-                def fetch(field):
-            try:
-                c = subprocess.Popen([intrptr, 'setup.py',
-                                      '--' + field], cwd=folder,
-                                      stdout=subprocess.PIPE)
-                return c.communicate()[0].strip()
-            except OSError:
-                return '?'
-        self.name = name
-        self.license = fetch('license')
-        self.author = fetch('author')
-        self.version = fetch('version')
+            def _accept_connection(self):
+        try:
+            ready, _, _ = select.select([self.server_sock], [], [], self.WAIT_EVENT_TIMEOUT)
+            if not ready:
+                return None
     
-    # the signals
-from .signals import signals_available, template_rendered, request_started, \
-     request_finished, got_request_exception, request_tearing_down, \
-     appcontext_tearing_down, appcontext_pushed, \
-     appcontext_popped, message_flashed, before_render_template
+            This attribute checks if the status code of the response is between
+        400 and 600 to see if there was a client error or a server error. If
+        the status code, is between 200 and 400, this will return True. This
+        is **not** a check to see if the response code is ``200 OK``.
+        '''
+        return self.ok
     
-        app.logger.info('\n'.join(info))
     
-    # Apply monkey patches to fix issues in external libraries
-from . import _monkeypatches
-del _monkeypatches
+class ConnectionError(RequestException):
+    '''A Connection error occurred.'''
     
-        def run(self, args, opts):
-        # load contracts
-        contracts = build_component_list(self.settings.getwithbase('SPIDER_CONTRACTS'))
-        conman = ContractsManager(load_object(c) for c in contracts)
-        runner = TextTestRunner(verbosity=2 if opts.verbose else 1)
-        result = TextTestResult(runner.stream, runner.descriptions, runner.verbosity)
+    '''
+The ``codes`` object defines a mapping from common names for HTTP statuses
+to their numerical codes, accessible either as attributes or as dictionary
+items.
     
-    import scrapy
-from scrapy.commands import ScrapyCommand
-from scrapy.utils.template import render_templatefile, string_camelcase
-from scrapy.exceptions import UsageError
+    if hasattr(ctypes, 'windll'):
+    WSAStringToAddressA = ctypes.windll.ws2_32.WSAStringToAddressA
+    WSAAddressToStringA = ctypes.windll.ws2_32.WSAAddressToStringA
+else:
+    def not_windows():
+        raise SystemError(
+            'Invalid platform. ctypes.windll must be available.'
+        )
+    WSAStringToAddressA = not_windows
+    WSAAddressToStringA = not_windows
     
-    def mixcloud_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html(url, faker=True)
-    title = r1(r'<meta property='og:title' content='([^']*)'', html)
-    preview_url = r1(r'm-preview=\'([^\']+)\'', html)
-    preview = r1(r'previews(.*)\.mp3$', preview_url)
+    def os_detail():
+    if sys.platform == 'win32':
+        return win32_version_string()
+    elif sys.platform.startswith('linux'):
+        distname,version,id = platform.linux_distribution()
+        return 'Dist:%s; Version:%s; ID:%s' % (distname,version,id)
+    elif sys.platform == 'darwin':
+        release, versioninfo, machine = platform.mac_ver()
+        return 'Release:%s; Version:%s Machine:%s' % (release, versioninfo, machine)
+    else:
+        return 'None'
+
+    
+            raise NotImplementedError
+    
+    
+    Revision ID: 430039611635
+Revises: d827694c7555
+Create Date: 2016-02-10 08:47:28.950891
+    
+    Revision ID: 43df8de3a5f4
+Revises: 7dbf98566af7
+Create Date: 2016-01-18 23:43:16.073483
+    
+    
+def downgrade():
+    op.drop_column('tables', 'description')
+
+    
+    # revision identifiers, used by Alembic.
+revision = '5a7bad26f2a7'
+down_revision = '4e6a06bad7a8'
+    
+    from r2.lib.translation import I18N_PATH
+from r2.lib.plugin import PluginLoader
+from r2.lib import js
+    
+        def __call__(self, environ, start_response):
+        environ['FULLPATH'] = environ.get('PATH_INFO')
+        qs = environ.get('QUERY_STRING')
+        if qs:
+            environ['FULLPATH'] += '?' + qs
+        return self.app(environ, start_response)
+    
+            Responds with a 120x50 `image/png` which should be displayed
+        to the user.
