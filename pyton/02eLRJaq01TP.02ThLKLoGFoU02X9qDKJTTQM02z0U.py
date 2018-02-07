b267ccd48b941cc591a9313c70df5483a2e56683
SUNsung <weatherius@gmail.com>
2018-02-07 03:38:15 +0200

    Rewrite [02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py] File in: 2018-02-07 03:38:14

--- pyton/02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py
+++ pyton/02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py
@@ -1,126 +1,137 @@
 
         
         
-collect_ignore = [
-    # deprecated or moved modules
-    'scrapy/conf.py',
-    'scrapy/stats.py',
-    'scrapy/project.py',
-    'scrapy/utils/decorator.py',
-    'scrapy/statscol.py',
-    'scrapy/squeue.py',
-    'scrapy/log.py',
-    'scrapy/dupefilter.py',
-    'scrapy/command.py',
-    'scrapy/linkextractor.py',
-    'scrapy/spider.py',
+def _py_files(folder):
+    return glob.glob(folder + '/*.py') + glob.glob(folder + '/*/*.py')
+    
+    from alembic import op
+import sqlalchemy as sa
+    
+    # revision identifiers, used by Alembic.
+revision = '1d2ddd543133'
+down_revision = 'd2424a248d63'
     
     
-class EventNotification( BaseRequest ):
-  def __init__( self, event_name, buffer_number = None, extra_data = None ):
-    super( EventNotification, self ).__init__()
-    self._event_name = event_name
-    self._buffer_number = buffer_number
-    self._extra_data = extra_data
-    self._response_future = None
-    self._cached_response = None
+def downgrade():
+    op.drop_table('logs')
+
     
     
-class CommandRequest( BaseRequest ):
-  def __init__( self, arguments, completer_target = None, extra_data = None ):
-    super( CommandRequest, self ).__init__()
-    self._arguments = _EnsureBackwardsCompatibility( arguments )
-    self._completer_target = ( completer_target if completer_target
-                               else 'filetype_default' )
-    self._extra_data = extra_data
-    self._response = None
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    import tornado.httpserver
-import tornado.ioloop
-import tornado.options
-import tornado.web
-    
-        @gen_test
-    def test_http10_no_content_length(self):
-        # Regression test for a bug in which can_keep_alive would crash
-        # for an HTTP/1.0 (not 1.1) response with no content-length.
-        conn = HTTP1Connection(self.client_stream, True)
-        self.server_stream.write(b'HTTP/1.0 200 Not Modified\r\n\r\nhello')
-        self.server_stream.close()
-    
-    # Increasing --n without --keepalive will eventually run into problems
-# due to TIME_WAIT sockets
-define('n', type=int, default=15000)
-define('c', type=int, default=25)
-define('keepalive', type=bool, default=False)
-define('quiet', type=bool, default=False)
+def upgrade():
+    # cleanup after: https://github.com/airbnb/superset/pull/1078
+    try:
+        slices_ibfk_1 = generic_find_constraint_name(
+            table='slices', columns={'druid_datasource_id'},
+            referenced='datasources', db=db) or 'slices_ibfk_1'
+        slices_ibfk_2 = generic_find_constraint_name(
+            table='slices', columns={'table_id'},
+            referenced='tables', db=db) or 'slices_ibfk_2'
     
     
-class Application(tornado.web.Application):
-    def __init__(self):
-        handlers = [
-            (r'/', HomeHandler),
-            (r'/archive', ArchiveHandler),
-            (r'/feed', FeedHandler),
-            (r'/entry/([^/]+)', EntryHandler),
-            (r'/compose', ComposeHandler),
-            (r'/auth/create', AuthCreateHandler),
-            (r'/auth/login', AuthLoginHandler),
-            (r'/auth/logout', AuthLogoutHandler),
-        ]
-        settings = dict(
-            blog_title=u'Tornado Blog',
-            template_path=os.path.join(os.path.dirname(__file__), 'templates'),
-            static_path=os.path.join(os.path.dirname(__file__), 'static'),
-            ui_modules={'Entry': EntryModule},
-            xsrf_cookies=True,
-            cookie_secret='__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__',
-            login_url='/auth/login',
-            debug=True,
-        )
-        super(Application, self).__init__(handlers, **settings)
-        # Have one global connection to the blog DB across all handlers
-        self.db = torndb.Connection(
-            host=options.mysql_host, database=options.mysql_database,
-            user=options.mysql_user, password=options.mysql_password)
+def upgrade():
+    op.add_column('dashboards', sa.Column('json_metadata', sa.Text(), nullable=True))
+    
+    def downgrade():
+    op.drop_column('slices', 'description')
+
+    
+            add_accept_handler(listener, accept_callback)
+        self.client_stream = IOStream(socket.socket())
+        self.addCleanup(self.client_stream.close)
+        yield [self.client_stream.connect(('127.0.0.1', port)),
+               event.wait()]
+        self.io_loop.remove_handler(listener)
+        listener.close()
+    
+    
+class QueueBasicTest(AsyncTestCase):
+    def test_repr_and_str(self):
+        q = queues.Queue(maxsize=1)
+        self.assertIn(hex(id(q)), repr(q))
+        self.assertNotIn(hex(id(q)), str(q))
+        q.get()
+    
+        def fileno(self):
+        return self.reader.fileno()
+    
+    
+@skipIfNonUnix
+class TestMultiprocess(unittest.TestCase):
+    # These tests verify that the two multiprocess examples from the
+    # TCPServer docs work. Both tests start a server with three worker
+    # processes, each of which prints its task id to stdout (a single
+    # byte, so we don't have to worry about atomicity of the shared
+    # stdout stream) and then exits.
+    def run_subproc(self, code):
+        proc = subprocess.Popen(sys.executable,
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE)
+        proc.stdin.write(utf8(code))
+        proc.stdin.close()
+        proc.wait()
+        stdout = proc.stdout.read()
+        proc.stdout.close()
+        if proc.returncode != 0:
+            raise RuntimeError('Process returned %d. stdout=%r' % (
+                proc.returncode, stdout))
+        return to_unicode(stdout)
     
     import logging
-import tornado.escape
-import tornado.ioloop
-import tornado.options
-import tornado.web
-import tornado.websocket
-import os.path
-import uuid
+from tornado.curl_httpclient import CurlAsyncHTTPClient
+from tornado.simple_httpclient import SimpleAsyncHTTPClient
+from tornado.ioloop import IOLoop
+from tornado.options import define, options, parse_command_line
+from tornado.web import RequestHandler, Application
+    
+        def __handle_exception(self, typ, value, tb):
+        pass
+    
+        def _on_auth(self, user):
+        if not user:
+            raise tornado.web.HTTPError(500, 'Facebook auth failed')
+        self.set_secure_cookie('fbdemo_user', tornado.escape.json_encode(user))
+        self.redirect(self.get_argument('next', '/'))
     
         @classmethod
-    def setUpClass(cls):
-        cls.s = Subject()
-        cls.dec_obs = DecimalViewer()
-        cls.hex_obs = HexViewer()
+    def send_updates(cls, chat):
+        logging.info('sending message to %d waiters', len(cls.waiters))
+        for waiter in cls.waiters:
+            try:
+                waiter.write_message(chat)
+            except:
+                logging.error('Error sending message', exc_info=True)
     
-        def test_sales_manager_shall_not_talk_through_proxy_with_delay(cls):
-        cls.ntp.busy = 'No'
-        start_time = time()
-        cls.ntp.talk()
-        end_time = time()
-        execution_time = end_time - start_time
-        print_output = cls.output.getvalue()
-        expected_print_output = 'Proxy checking for Sales Manager availability\n\
-This Sales Manager will not talk to you whether he/she is busy or not\n'
-        cls.assertEqual(print_output, expected_print_output)
-        expected_execution_time = 1
-        cls.assertEqual(int(execution_time*10), expected_execution_time)
+        def test_sequential_undo(self):
+        self.command_stack = list(reversed(self.command_stack))
+        self.command_stack[0].undo()
+        output_after_first_undo = os.listdir(self.test_dir)
+        self.assertEqual(output_after_first_undo[0], 'bar.txt')
+        self.command_stack[1].undo()
+        output_after_second_undo = os.listdir(self.test_dir)
+        self.assertEqual(output_after_second_undo[0], 'foo.txt')
     
-        def test_am_station_overflow_after_scan(self):
-        self.radio.scan()
-        station = self.radio.state.stations[self.radio.state.pos]
-        expected_station = '1250'
-        self.assertEqual(station, expected_station)
+    
+def main():
+    message_center = Provider()
+    
+        def test_items_recoil(self):
+        with ObjectPool(self.sample_queue, True) as pool:
+            self.assertEqual(pool, 'first')
+        self.assertTrue(self.sample_queue.get() == 'second')
+        self.assertFalse(self.sample_queue.empty())
+        self.assertTrue(self.sample_queue.get() == 'first')
+        self.assertTrue(self.sample_queue.empty())
+    
+    
+@coroutine
+def coroutine2(target):
+    while True:
+        request = yield
+        if 10 < request <= 20:
+            print('request {} handled in coroutine 2'.format(request))
+        else:
+            target.send(request)
+    
+    print('Counting to two...')
+for number in count_to_two():
+    print(number, end=' ')
