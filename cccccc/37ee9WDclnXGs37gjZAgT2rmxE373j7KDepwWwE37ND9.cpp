d9b6f2c7e3df315df3c3d2474978747db36c1f3e
SUNsung <weatherius@gmail.com>
2018-02-07 18:35:00 +0200

    Rewrite [37ee9WDclnXGs37gjZAgT2rmxE373j7KDepwWwE37ND9.cpp] File in: 2018-02-07 18:35:00

--- cccccc/37ee9WDclnXGs37gjZAgT2rmxE373j7KDepwWwE37ND9.cpp
+++ cccccc/37ee9WDclnXGs37gjZAgT2rmxE373j7KDepwWwE37ND9.cpp
@@ -1,348 +1,209 @@
 
         
-            // Create the meta-variable, let the typechecker name it.
-    Identifier name = TC.getNextResponseVariableName(SF.getParentModule());
-    VarDecl *vd = new (Context) VarDecl(/*IsStatic*/false,
-                                        VarDecl::Specifier::Let,
-                                        /*IsCaptureList*/false,
-                                        PBD->getStartLoc(), name,
-                                        pattern->getType(), &SF);
-    vd->setInterfaceType(pattern->getType());
-    SF.Decls.push_back(vd);
+        #ifndef ATOM_BROWSER_API_ATOM_API_GLOBAL_SHORTCUT_H_
+#define ATOM_BROWSER_API_ATOM_API_GLOBAL_SHORTCUT_H_
     
-        auto bucketA = getResultBucket(a, hasExpectedTypes);
-    auto bucketB = getResultBucket(b, hasExpectedTypes);
-    if (bucketA < bucketB)
-      return false;
-    else if (bucketB < bucketA)
-      return true;
-    
-    public:
-  DeadEndBlocks(const SILFunction *F) : F(F) {}
-    
-      // Add the current module's source files to the dependencies.
-  llvm::SetVector<const clang::FileEntry *> fileDependencies;
-  // FIXME: This is not desirable because:
-  // 1. It picks shim header files as file dependencies
-  // 2. Having all the other swift files of the module as file dependencies ends
-  //   up making all of them associated with all the other files as main files.
-  //   It's better to associate each swift file with the unit that recorded it
-  //   as the main one.
-  // Keeping the code in case we want to revisit.
-#if 0
-  collectFileDependencies(fileDependencies, dependencyTracker, module, fileMgr);
-#endif
-    
-    /// Attempt to get a doc comment from the declaration, or other inherited
-/// sources, like from base classes or protocols.
-Optional<DocComment *> getCascadingDocComment(swift::markup::MarkupContext &MC,
-                                             const Decl *D);
-    
-    
-    {  /// Return a hash code of any components from these options that should
-  /// contribute to a Swift Bridging PCH hash.
-  llvm::hash_code getPCHHashComponents() const {
-    // Nothing here that contributes anything significant when emitting the PCH.
-    return llvm::hash_value(0);
-  }
-};
-    
-      /// Minimum signed int value that can be stored in a Json::Value.
-  static const Int minInt;
-  /// Maximum signed int value that can be stored in a Json::Value.
-  static const Int maxInt;
-  /// Maximum unsigned int value that can be stored in a Json::Value.
-  static const UInt maxUInt;
-    
-    // Find the file which defines an extension extending the given message type
-// with the given field number.
-// Python DescriptorDatabases are not required to implement this method.
-bool PyDescriptorDatabase::FindFileContainingExtension(
-    const string& containing_type, int field_number,
-    FileDescriptorProto* output) {
-  ScopedPyObjectPtr py_method(
-      PyObject_GetAttrString(py_database_, 'FindFileContainingExtension'));
-  if (py_method == NULL) {
-    // This method is not implemented, returns without error.
-    PyErr_Clear();
-    return false;
-  }
-  ScopedPyObjectPtr py_descriptor(
-      PyObject_CallFunction(py_method.get(), 's#i', containing_type.c_str(),
-                            containing_type.size(), field_number));
-  return GetFileDescriptorProto(py_descriptor.get(), output);
+    int RenderProcessPreferences::AddEntry(const base::DictionaryValue& entry) {
+  return preferences_.AddEntry(entry);
 }
     
-      // This is the top-level C++ Message object that owns the whole
-  // proto tree.  Every Python RepeatedScalarContainer holds a
-  // reference to it in order to keep it alive as long as there's a
-  // Python object that references any part of the tree.
-  shared_ptr<Message> owner;
-    
-    #include <google/protobuf/compiler/code_generator.h>
-#include <google/protobuf/compiler/csharp/csharp_field_base.h>
-    
-    namespace google {
-namespace protobuf {
-namespace compiler {
-namespace csharp {
-    }
-    }
-    }
+    class Event : public Wrappable<Event>,
+              public content::WebContentsObserver {
+ public:
+  static Handle<Event> Create(v8::Isolate* isolate);
     }
     
-    #endif  // GOOGLE_PROTOBUF_COMPILER_CSHARP_REPEATED_PRIMITIVE_FIELD_H__
-    
-    struct Options;
-    
-    // Author: kenton@google.com (Kenton Varda)
-//  Based on original Protocol Buffers design by
-//  Sanjay Ghemawat, Jeff Dean, and others.
+    #include 'content/public/browser/download_item.h'
+#include 'content/public/browser/download_manager.h'
+#include 'content/public/browser/save_page_type.h'
+#include 'v8/include/v8.h'
     
     
-    {}  // namespace google
-#endif  // GOOGLE_PROTOBUF_COMPILER_JAVA_EXTENSION_LITE_H__
-
+    {}  // namespace atom
     
-        declare.in(frame).out(ptvec);
+    #ifndef ATOM_BROWSER_NET_URL_REQUEST_STRING_JOB_H_
+#define ATOM_BROWSER_NET_URL_REQUEST_STRING_JOB_H_
     
-        if( !*state )
-        return;
-    
-    OCL_TEST_P(StereoBMFixture, StereoBM)
-{
-    Ptr<StereoBM> bm = StereoBM::create( n_disp, winSize);
-    bm->setPreFilterType(bm->PREFILTER_XSOBEL);
-    bm->setTextureThreshold(0);
+    namespace internal {
     }
     
-            cv::Affine3<T>::Vec3 vo;
-        //s = (double)cv::getTickCount();
-        cv::Rodrigues(R, vo);
-        //std::cout << 'O:' <<(cv::getTickCount() - s)*1000/cv::getTickFrequency() << std::endl;
-    
-            gray = cvCreateImage( cvSize( img.width, img.height ), IPL_DEPTH_8U, 1 );
-        thresh = cvCreateImage( cvSize( img.width, img.height ), IPL_DEPTH_8U, 1 );
-        cvCvtColor( &img, gray, CV_BGR2GRAY );
-    
-    TEST(Calib3d_DecomposeProjectionMatrix, accuracy)
-{
-    CV_DecomposeProjectionMatrixTest test;
-    test.safe_run();
-}
-
-    
-    
-    {    bool calc_jacobians;
-    bool test_cpp;
-};
-    
-    void HHVM_FUNCTION(xhprof_frame_begin, const String& name) {
-  Profiler *prof = ThreadInfo::s_threadInfo->m_profiler;
-  if (prof) {
-    s_profiler_factory->cacheString(name);
-    prof->beginFrame(name.data());
+      void StartServers(size_t num_servers,
+                    std::vector<int> ports = std::vector<int>()) {
+    for (size_t i = 0; i < num_servers; ++i) {
+      int port = 0;
+      if (ports.size() == num_servers) port = ports[i];
+      servers_.emplace_back(new ServerData(server_host_, port));
+    }
   }
-}
     
-    #include <folly/portability/SysResource.h>
-#include <folly/portability/SysTime.h>
+      // Ignores any errors. This method does nothing except potentially suppress
+  // complaints from any tools that are checking that errors are not dropped on
+  // the floor.
+  void IgnoreError() const {}
     
-      static auto constexpr kTombstone = kInvalidDataType;
-  static auto constexpr kEmpty = KindOfUninit;
+    DEFINE_string(test_name, '', 'Name of the test being executed');
     
-        size_t GetNumParallelSequencesForFixingBPTTMode()
-    {
+      grpc_channel_args* b =
+      (grpc_channel_args*)gpr_malloc(sizeof(grpc_channel_args));
+  b->num_args = a->num_args;
+  b->args = (grpc_arg*)gpr_malloc(sizeof(grpc_arg) * b->num_args);
+  for (size_t i = 0; i < a->num_args; i++) {
+    b->args[i] = copy_arg(args[i]);
+  }
+    
+      return 1;
+    
+    int grpc_byte_buffer_reader_next(grpc_byte_buffer_reader* reader,
+                                 grpc_slice* slice) {
+  switch (reader->buffer_in->type) {
+    case GRPC_BB_RAW: {
+      grpc_slice_buffer* slice_buffer;
+      slice_buffer = &reader->buffer_out->data.raw.slice_buffer;
+      if (reader->current.index < slice_buffer->count) {
+        *slice = grpc_slice_ref_internal(
+            slice_buffer->slices[reader->current.index]);
+        reader->current.index += 1;
         return 1;
+      }
+      break;
     }
-    void SetNumParallelSequences(const size_t){};
-    void CopyMBLayoutTo(MBLayoutPtr pMBLayout)
-    {
-        pMBLayout->CopyFrom(m_pMBLayout);
-        NOT_IMPLEMENTED;
-    }
-    virtual const std::map<LabelIdType, LabelType>& GetLabelMapping(const std::wstring& sectionName);
-    virtual void SetLabelMapping(const std::wstring& sectionName, const std::map<typename BinaryReader<ElemType>::LabelIdType, typename BinaryReader<ElemType>::LabelType>& labelMapping);
-    virtual bool GetData(const std::wstring& sectionName, size_t numRecords, void* data, size_t& dataBufferSize, size_t recordStart = 0);
-    
-    
-    {
-    {
-    {
-    {
-    {
-    {
-    {
-    {                    if (EqualInsensitive(functionName, 'CreateModel'))
-                    {
-                        params.insert(params.begin(), key);
-                        CallFunction('CreateModelWithName', params);
-                    }
-                    else if (EqualInsensitive(functionName, 'LoadModel'))
-                    {
-                        params.insert(params.begin(), key);
-                        CallFunction('LoadModelWithName', params);
-                    }
-                    else
-                    { // not a MEL command, so pass it on to NDL
-                        if (!m_netNdlDefault)
-                            RuntimeError('NDL Command cannot be executed until default model is established, cannot set '%s' without a default mode\n Try calling SetDefaultModel(model) before any NDL statement are embedded\n', key.c_str());
-                        HandleNDLInline(stringParse, tokenStart, tokenEnd);
-                    }
-                }
-            }
-            return tokenEnd;
-        }
-        // Function(x,y,z) - function with no return
-        else
-        {
-            std::string value = stringParse.substr(tokenStart, tokenEnd - tokenStart);
-            if (keyEnd > tokenEnd)
-                RuntimeError('Invalid line, expecting function call, %s', value.c_str());
-            std::string functionName;
-            std::string paramList;
-            // Function(x,y,z) - function with no return
-            CallStringParse(value, functionName, paramList);
-            ConfigParamList params(paramList);
-            CallFunction(functionName, params);
-        }
-        return tokenEnd;
-    }
-};
-} } }
-
-    
-    // ---------------------------------------------------------------------------
-// latticesource -- manages loading of lattices for MMI (in pairs for numer and denom)
-// ---------------------------------------------------------------------------
-    
-    
-    {
-    {
-    {    // please add anything else you might need HERE
-};
-};
-};
-
-    
-    bool js_cocos2dx_studio_ComAudio_constructor(JSContext *cx, uint32_t argc, jsval *vp);
-void js_cocos2dx_studio_ComAudio_finalize(JSContext *cx, JSObject *obj);
-void js_register_cocos2dx_studio_ComAudio(JSContext *cx, JS::HandleObject global);
-void register_all_cocos2dx_studio(JSContext* cx, JS::HandleObject obj);
-bool js_cocos2dx_studio_ComAudio_stopAllEffects(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_getEffectsVolume(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_stopEffect(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_getBackgroundMusicVolume(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_willPlayBackgroundMusic(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_setBackgroundMusicVolume(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_end(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_start(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_stopBackgroundMusic(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_pauseBackgroundMusic(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_isBackgroundMusicPlaying(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_isLoop(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_resumeAllEffects(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_pauseAllEffects(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_preloadBackgroundMusic(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_playBackgroundMusic(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_stop(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_playEffect(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_preloadEffect(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_setLoop(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_unloadEffect(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_rewindBackgroundMusic(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_pauseEffect(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_resumeBackgroundMusic(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_setFile(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_setEffectsVolume(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_getFile(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_resumeEffect(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_create(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComAudio_ComAudio(JSContext *cx, uint32_t argc, jsval *vp);
-    
-    
-    
-    #ifdef __cplusplus
-extern 'C' {
-#endif
-#include 'tolua++.h'
-#ifdef __cplusplus
-}
-#endif
-    
-    USING_NS_CC;
-    
-    			m_body = m_world->CreateBody(&bd);
-			m_body->CreateFixture(&box, 1.0f);
-    
-    JNIEXPORT void JNICALL Java_com_tencent_mars_xlog_Xlog_appenderClose(JNIEnv *env, jobject) {
-	appender_close();
+  }
+  return 0;
 }
     
-    const std::vector<std::string>& DumpCrashStack::StackList() const {
-    return vecdump_;
+    #include <grpc++/channel.h>
+#include <grpc++/client_context.h>
+#include <grpc++/create_channel.h>
+#include <grpc++/security/credentials.h>
+#include <grpc++/security/server_credentials.h>
+#include <grpc++/server.h>
+#include <grpc++/server_builder.h>
+#include <grpc++/server_context.h>
+#include <grpc/grpc.h>
+#include <grpc/support/alloc.h>
+#include <grpc/support/log.h>
+#include <grpc/support/thd.h>
+#include <grpc/support/time.h>
+    
+    
+    {    if (subject_alt_name_count != 0) {
+      result = add_subject_alt_names_properties_to_peer(
+          peer, subject_alt_names, (size_t)subject_alt_name_count);
+      if (result != TSI_OK) break;
+    }
+  } while (0);
+    
+    static void* fake_realloc(void* addr, size_t size) { return (void*)size; }
+    
+    // Attempt to snappy uncompress input[0,input_length-1] into *output.
+// Returns true if successful, false if the input is invalid lightweight
+// compressed data.
+//
+// REQUIRES: at least the first 'n' bytes of output[] must be writable
+// where 'n' is the result of a successful call to
+// Snappy_GetUncompressedLength.
+extern bool Snappy_Uncompress(const char* input_data, size_t input_length,
+                              char* output);
+    
+      // If a seek to internal key 'k' in specified file finds an entry,
+  // call (*handle_result)(arg, found_key, found_value).
+  Status Get(const ReadOptions& options,
+             uint64_t file_number,
+             uint64_t file_size,
+             const Slice& k,
+             void* arg,
+             void (*handle_result)(void*, const Slice&, const Slice&));
+    
+    class Env;
+    
+    int main(int argc, char** argv) {
+  return leveldb::test::RunAllTests();
 }
 
     
+    class Issue178 { };
     
-    {}
+      DB *db;
+  Options options;
+  options.create_if_missing = true;
+  ASSERT_OK(DB::Open(options, dbpath, &db));
     
-        void* This() const {return m_this;}
-    
-    int TSpy::__TestFun1(int i)
-{
-    return reinterpret_cast<Test_Spy_Sample*>(This())->__TestFun1(i);
-}
-    
-    jvalue JNU_CallMethodByMethodInfo(JNIEnv* _env, jobject _obj, JniMethodInfo _method_info, ...);
-    
-    extern pthread_key_t g_env_key;
-    
-    void AbstractBtMessage::setBtMessageDispatcher(BtMessageDispatcher* dispatcher)
-{
-  dispatcher_ = dispatcher;
-}
-    
-      const std::shared_ptr<SocketRecvBuffer>& getSocketRecvBuffer() const
-  {
-    return socketRecvBuffer_;
+     public:
+  Iter(const Comparator* comparator,
+       const char* data,
+       uint32_t restarts,
+       uint32_t num_restarts)
+      : comparator_(comparator),
+        data_(data),
+        restarts_(restarts),
+        num_restarts_(num_restarts),
+        current_(restarts_),
+        restart_index_(num_restarts_) {
+    assert(num_restarts_ > 0);
   }
     
-    #include 'AbstractCommand.h'
-    
-    namespace aria2 {
+      // Takes ownership of 'iter' and will delete it when destroyed, or
+  // when Set() is invoked again.
+  void Set(Iterator* iter) {
+    delete iter_;
+    iter_ = iter;
+    if (iter_ == NULL) {
+      valid_ = false;
+    } else {
+      Update();
     }
-    
-    void AnnounceTier::nextEvent()
-{
-  switch (event) {
-  case STARTED:
-    event = DOWNLOADING;
-    break;
-  case STARTED_AFTER_COMPLETION:
-    event = SEEDING;
-    break;
-  case STOPPED:
-    event = HALTED;
-    break;
-  case COMPLETED:
-    event = SEEDING;
-    break;
-  default:
-    break;
   }
-}
     
-    namespace aria2 {
+      for (const auto& iter : line) {
+    options_index++;
+    if (iter[0] == '/') {
+      line_exports.push_back(iter);
+    } else {
+      break;
     }
+  }
     
-    std::unique_ptr<AuthConfig> AuthConfig::create(std::string user,
-                                               std::string password)
-{
-  if (user.empty()) {
-    return nullptr;
-  }
-  else {
-    return make_unique<AuthConfig>(std::move(user), std::move(password));
-  }
+    /// Internal callExtension implementation using a UNIX domain socket path.
+Status callExtension(const std::string& extension_path,
+                     const std::string& registry,
+                     const std::string& item,
+                     const PluginRequest& request,
+                     PluginResponse& response);
+    
+    /**
+ * @brief Logger plugin feature bits for complicated loggers.
+ *
+ * Logger plugins may opt-in to additional features like explicitly handling
+ * Glog status events or requesting event subscribers to forward each event
+ * directly to the logger. This enumeration tracks, and corresponds to, each
+ * of the feature methods defined in a logger plugin.
+ *
+ * A specific registry call action can be used to retrieve an overloaded Status
+ * object containing all of the opt-in features.
+ */
+enum LoggerFeatures {
+  LOGGER_FEATURE_BLANK = 0,
+  LOGGER_FEATURE_LOGSTATUS = 1,
+  LOGGER_FEATURE_LOGEVENT = 2,
+};
+    
+      /// The user this instance dropped privileges to.
+  uid_t to_user_{0};
+    
+      /*
+   * @brief a variable tracking all of the paths we attempt to carve
+   *
+   * This is a globbed set of file paths that we're expecting will be
+   * carved.
+   */
+  std::set<boost::filesystem::path> carvePaths_;
+    
+    
+    {
+    {  c.reset();
 }
+}
+
+    
+      status = request.getResponse(response_tree);
+  ASSERT_TRUE(status.ok());
