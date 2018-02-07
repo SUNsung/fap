c25ce89917c7d1c875100437fe4a2a1f607f1de5
SUNsung <weatherius@gmail.com>
2018-02-07 06:06:59 +0200

    Rewrite [499.uIM2vfqN.49Mb7ia7CsFt249aOpuwkuLsQc49WzG.py] File in: 2018-02-07 06:06:58

--- pyton/499.uIM2vfqN.49Mb7ia7CsFt249aOpuwkuLsQc49WzG.py
+++ pyton/499.uIM2vfqN.49Mb7ia7CsFt249aOpuwkuLsQc49WzG.py
@@ -1,248 +1,99 @@
 
         
-            plugin_manager.register(Plugin)
-    try:
-        r = http(
-            httpbin + BASIC_AUTH_URL,
-            '--auth-type',
-            Plugin.auth_type,
-            '--auth',
-            BASIC_AUTH_HEADER_VALUE,
-        )
-        assert HTTP_OK in r
-        assert r.json == AUTH_OK
-    finally:
-        plugin_manager.unregister(Plugin)
+          def setUp(self):
+    FLAGS.train_data = os.path.join(self.get_temp_dir(), 'test-text.txt')
+    FLAGS.eval_data = os.path.join(self.get_temp_dir(), 'eval-text.txt')
+    FLAGS.save_path = self.get_temp_dir()
+    with open(FLAGS.train_data, 'w') as f:
+      f.write(
+          '''alice was beginning to get very tired of sitting by her sister on
+          the bank, and of having nothing to do: once or twice she had peeped
+          into the book her sister was reading, but it had no pictures or
+          conversations in it, 'and what is the use of a book,' thought alice
+          'without pictures or conversations?' So she was considering in her own
+          mind (as well as she could, for the hot day made her feel very sleepy
+          and stupid), whether the pleasure of making a daisy-chain would be
+          worth the trouble of getting up and picking the daisies, when suddenly
+          a White rabbit with pink eyes ran close by her.\n''')
+      with open(FLAGS.eval_data, 'w') as f:
+        f.write('alice she rabbit once\n')
     
-                yield line.decode(self.msg.encoding) \
-                      .encode(self.output_encoding, 'replace') + lf
+      @tf.test.mock.patch.dict(premade_estimator.__dict__,
+                           {'load_data': four_lines_data})
+  def test_premade_estimator(self):
+    premade_estimator.main([None, '--train_steps=1'])
     
-        try:
-        parsed_args = parser.parse_args(args=args, env=env)
-    except KeyboardInterrupt:
-        env.stderr.write('\n')
-        if include_traceback:
-            raise
-        exit_status = ExitStatus.ERROR_CTRL_C
-    except SystemExit as e:
-        if e.code != ExitStatus.OK:
-            env.stderr.write('\n')
-            if include_traceback:
-                raise
-            exit_status = ExitStatus.ERROR
-    else:
-        try:
-            exit_status = program(
-                args=parsed_args,
-                env=env,
-                log_error=log_error,
-            )
-        except KeyboardInterrupt:
-            env.stderr.write('\n')
-            if include_traceback:
-                raise
-            exit_status = ExitStatus.ERROR_CTRL_C
-        except SystemExit as e:
-            if e.code != ExitStatus.OK:
-                env.stderr.write('\n')
-                if include_traceback:
-                    raise
-                exit_status = ExitStatus.ERROR
-        except requests.Timeout:
-            exit_status = ExitStatus.ERROR_TIMEOUT
-            log_error('Request timed out (%ss).', parsed_args.timeout)
-        except requests.TooManyRedirects:
-            exit_status = ExitStatus.ERROR_TOO_MANY_REDIRECTS
-            log_error('Too many redirects (--max-redirects=%s).',
-                      parsed_args.max_redirects)
-        except Exception as e:
-            # TODO: Further distinction between expected and unexpected errors.
-            msg = str(e)
-            if hasattr(e, 'request'):
-                request = e.request
-                if hasattr(request, 'url'):
-                    msg += ' while doing %s request to URL: %s' % (
-                        request.method, request.url)
-            log_error('%s: %s', type(e).__name__, msg)
-            if include_traceback:
-                raise
-            exit_status = ExitStatus.ERROR
+    py_binary(
+    name = 'translate',
+    srcs = [
+        'translate.py',
+    ],
+    srcs_version = 'PY2AND3',
+    deps = [
+        ':data_utils',
+        ':seq2seq_model',
+        '//tensorflow:tensorflow_py',
+    ],
+)
     
-        Uses threading to periodically update the status (speed, ETA, etc.).
+        # Embedding dimension.
+    self.emb_dim = FLAGS.embedding_size
     
     
-@pytest.mark.parametrize('ssl_version', SSL_VERSION_ARG_MAPPING.keys())
-def test_ssl_version(httpbin_secure, ssl_version):
-    try:
-        r = http(
-            '--ssl', ssl_version,
-            httpbin_secure + '/get'
-        )
-        assert HTTP_OK in r
-    except SSLError as e:
-        if ssl_version == 'ssl3':
-            # pytest-httpbin doesn't support ssl3
-            assert 'SSLV3_ALERT_HANDSHAKE_FAILURE' in str(e)
-        else:
-            raise
+def fake_data(num_images):
+  '''Generate a fake dataset that matches the dimensions of MNIST.'''
+  data = numpy.ndarray(
+      shape=(num_images, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS),
+      dtype=numpy.float32)
+  labels = numpy.zeros(shape=(num_images,), dtype=numpy.int64)
+  for image in xrange(num_images):
+    label = image % 2
+    data[image, :, :, 0] = label - 0.5
+    labels[image] = label
+  return data, labels
     
-        >>> humanize_bytes(1)
-    '1 B'
-    >>> humanize_bytes(1024, precision=1)
-    '1.0 kB'
-    >>> humanize_bytes(1024 * 123, precision=1)
-    '123.0 kB'
-    >>> humanize_bytes(1024 * 12342, precision=1)
-    '12.1 MB'
-    >>> humanize_bytes(1024 * 12342, precision=2)
-    '12.05 MB'
-    >>> humanize_bytes(1024 * 1234, precision=2)
-    '1.21 MB'
-    >>> humanize_bytes(1024 * 1234 * 1111, precision=2)
-    '1.31 GB'
-    >>> humanize_bytes(1024 * 1234 * 1111, precision=1)
-    '1.3 GB'
+          # Encoder inputs are padded and then reversed.
+      encoder_pad = [data_utils.PAD_ID] * (encoder_size - len(encoder_input))
+      encoder_inputs.append(list(reversed(encoder_input + encoder_pad)))
     
-        def get_auth_plugin_mapping(self):
-        return {plugin.auth_type: plugin for plugin in self.get_auth_plugins()}
+        ordered_state = collections.OrderedDict(
+        sorted(state.items(), key=lambda t: t[0]))
+    # For class instance without __repr__, some special cares are required.
+    # Otherwise, the object address will be used.
+    if '_cluster_spec' in ordered_state:
+      ordered_state['_cluster_spec'] = collections.OrderedDict(
+         sorted(ordered_state['_cluster_spec'].as_dict().items(),
+                key=lambda t: t[0])
+      )
+    return ', '.join(
+        '%s=%r' % (k, v) for (k, v) in six.iteritems(ordered_state)) 
     
-    filenames = {
-    'bin': 'youtube-dl',
-    'exe': 'youtube-dl.exe',
-    'tar': 'youtube-dl-%s.tar.gz' % version}
-build_dir = os.path.join('..', '..', 'build', version)
-for key, filename in filenames.items():
-    url = 'https://yt-dl.org/downloads/%s/%s' % (version, filename)
-    fn = os.path.join(build_dir, filename)
-    with open(fn, 'rb') as f:
-        data = f.read()
-    if not data:
-        raise ValueError('File %s is empty!' % fn)
-    sha256sum = hashlib.sha256(data).hexdigest()
-    new_version[key] = (url, sha256sum)
+    from alembic import op
+import sqlalchemy as sa
     
-        with io.open(infile, encoding='utf-8') as inf:
-        issue_template_tmpl = inf.read()
-    
-    header = oldreadme[:oldreadme.index('# OPTIONS')]
-footer = oldreadme[oldreadme.index('# CONFIGURATION'):]
-    
-    compat_print('total downloads traffic: %s' % format_size(total_bytes))
-
-    
-        def test_yahoo_https(self):
-        # https://github.com/rg3/youtube-dl/issues/2701
-        self.assertMatch(
-            'https://screen.yahoo.com/smartwatches-latest-wearable-gadgets-163745379-cbs.html',
-            ['Yahoo'])
-    
-    import shutil
-    
-        def test_secondary_proxy_https(self):
-        params = self._check_params(['secondary_proxy', 'secondary_server_ip'])
-        if params is None:
-            return
-        ydl = FakeYDL()
-        req = compat_urllib_request.Request('https://yt-dl.org/ip')
-        req.add_header('Ytdl-request-proxy', params['secondary_proxy'])
-        self.assertEqual(
-            ydl.urlopen(req).read().decode('utf-8'),
-            params['secondary_server_ip'])
-    
-    
-def list_extractors(age_limit):
-    '''
-    Return a list of extractors that are suitable for the given age,
-    sorted by extractor ID.
     '''
     
-            runtime = self._search_regex(
-            r'Runtime\s*:\s*(.+?) \|', webpage, 'duration', default=None)
-        if runtime:
-            runtime = re.sub(r'[\s-]', '', runtime)
-        duration = parse_duration(runtime)
-        view_count = int_or_none(self._search_regex(
-            r'Views\s*:\s*(\d+)', webpage, 'view count', default=None))
-        comment_count = int_or_none(self._search_regex(
-            r'Comments\s*:\s*(\d+)', webpage, 'comment count', default=None))
-    
-    from keras.models import Sequential
-from keras.engine.training import _weighted_masked_objective
-from keras.layers import TimeDistributed, Masking, Dense
-from keras.utils.test_utils import keras_test
-from keras import losses
-from keras import backend as K
-    
-            layer_test(local.LocallyConnected2D,
-                   kwargs={'filters': filters,
-                           'kernel_size': (3, 3),
-                           'padding': padding,
-                           'kernel_regularizer': 'l2',
-                           'bias_regularizer': 'l2',
-                           'activity_regularizer': 'l2',
-                           'strides': strides,
-                           'data_format': 'channels_first'},
-                   input_shape=(num_samples, stack_size, num_row, num_col))
-    
-    x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+    # revision identifiers, used by Alembic.
+revision = '2591d77e9831'
+down_revision = '12d55656cbca'
     
     
-def handle_module(mod):
-    for name, mem in inspect.getmembers(mod):
-        if inspect.isclass(mem):
-            handle_class(name, mem)
-        elif inspect.isfunction(mem):
-            handle_function(name, mem)
-        elif 'keras' in name and inspect.ismodule(mem):
-            # Only test keras' modules
-            handle_module(mem)
+class Slice(Base):
+    '''Declarative class to do query in upgrade'''
+    __tablename__ = 'slices'
+    id = Column(Integer, primary_key=True)
+    datasource_id = Column(Integer)
+    druid_datasource_id = Column(Integer)
+    table_id = Column(Integer)
+    datasource_type = Column(String(200))
+    
+    from alembic import op
+import sqlalchemy as sa
     
     
-@pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
-def test_uniform(tensor_shape):
-    _runner(initializers.RandomUniform(minval=-1, maxval=1), tensor_shape,
-            target_mean=0., target_max=1, target_min=-1)
+def upgrade():
+    op.add_column('dashboards', sa.Column('json_metadata', sa.Text(), nullable=True))
     
-        outer_model = models.Model([a, b, c], o)
-    outer_model.compile(optimizer='rmsprop',
-                        loss='categorical_crossentropy',
-                        metrics=['accuracy'])
-    outer_model.fit([x, y, z], labels, epochs=1)
-    
-    
-def test_get_fn():
-    '''Activations has a convenience 'get' function. All paths of this
-    function are tested here, although the behaviour in some instances
-    seems potentially surprising (e.g. situation 3)
-    '''
-    
-    x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-    
-        def setUp(self):
-        from acme.errors import BadNonce
-        self.error = BadNonce(nonce='xxx', error='error')
-    
-        def test_repr(self):
-        self.assertEqual(repr(self.addr2), 'certbot_apache.obj.Addr(('127.0.0.1', '443'))')
-    
-    # If true, an OpenSearch description file will be output, and all pages will
-# contain a <link> tag referring to it.  The value of this option must be the
-# base URL from which the finished HTML is served.
-#html_use_opensearch = ''
-    
-    
-def pull_screenshot():
-    c.screenshot('autojump.png')
+    Revision ID: 7dbf98566af7
+Revises: 8e80a26a31db
+Create Date: 2016-01-17 22:00:23.640788
