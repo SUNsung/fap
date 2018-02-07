2c38fa798f38ffe7dd81a42a23a5a8d2659a33cd
SUNsung <weatherius@gmail.com>
2018-02-07 06:50:27 +0200

    Rewrite [index.cpp] File in: 2018-02-07 06:50:26

--- cccccc/index.cpp
+++ cccccc/index.cpp
@@ -1,68 +1,198 @@
 
         
-          v8::Handle<v8::String> source = v8::String::NewExternal(isolate,
-                                                          new extensions::StaticV8ExternalOneByteStringResource(
-          GetStringResource(resource_id)));
-  v8::Handle<v8::String> wrapped_source = WrapSource(source);
-    
-    // static
-int DispatcherHost::AllocateId() {
-  return next_object_id_++;
+        void SetProxyConfigCallback(
+    base::WaitableEvent* done,
+    net::URLRequestContextGetter* url_request_context_getter,
+    const net::ProxyConfig& proxy_config) {
+  net::ProxyService* proxy_service =
+      url_request_context_getter->GetURLRequestContext()->proxy_service();
+  proxy_service->ResetConfigService(
+      new net::ProxyConfigServiceFixed(proxy_config));
+  done->Signal();
 }
     
-    typedef std::map<std::string,std::string> KeyMap;
+    #include 'base/basictypes.h'
+#include '../dispatcher_host.h'
     
-    bool NwAppGetArgvSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
+    ui::KeyboardCode GetKeycodeFromText(std::string text){
+  ui::KeyboardCode retval = ui::VKEY_UNKNOWN;
+  if (text.size() != 0){
+    std::string upperText = base::ToUpperASCII(text);
+    std::string keyName = text;
+    bool found = false;
+    if (upperText.size() == 1){
+      char key = upperText[0];
+      if (key>='0' && key<='9'){//handle digital
+        keyName = 'Digit' + upperText;
+        found = true;
+      } else if (key>='A'&&key<='Z'){//handle alphabet
+        keyName = 'Key' + upperText;
+        found = true;
+      }
+    }
+    }
     }
     
-    #endif // BOOST_ASIO_DETAIL_DATE_TIME_FWD_HPP
+    
+    {
+} // namespace extensions
+#endif
 
     
-    template <typename Time_Traits>
-void dev_poll_reactor::schedule_timer(timer_queue<Time_Traits>& queue,
-    const typename Time_Traits::time_type& time,
-    typename timer_queue<Time_Traits>::per_timer_data& timer, wait_op* op)
+    void JRodriguesMatlab(const Mat& src, Mat& dst);
+    
+            if (lane < 16)
+        {
+            T partial = ptr[tid];
+    }
+    
+            //here everything goes. check that everything is setted properly
+        dprintf(('proxy_x\n'));print_matrix(proxy_x);
+        dprintf(('d first time\n'));print_matrix(d);
+        dprintf(('r\n'));print_matrix(r);
+    
+    cv::cuda::GpuMat::GpuMat(const GpuMat& m, Rect roi) :
+    flags(m.flags), rows(roi.height), cols(roi.width),
+    step(m.step), data(m.data + roi.y*step), refcount(m.refcount),
+    datastart(m.datastart), dataend(m.dataend),
+    allocator(m.allocator)
 {
-  boost::asio::detail::mutex::scoped_lock lock(mutex_);
+    flags &= roi.width < m.cols ? ~Mat::CONTINUOUS_FLAG : -1;
+    data += roi.x * elemSize();
     }
     
-      grpc::ServerBuilder builder;
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  builder.RegisterService(&service);
-  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
-  std::cerr << 'Server listening on ' << server_address << std::endl;
-    
-    namespace NamespaceC {
+        UMatData* allocate(int dims, const int* sizes, int type,
+                       void* data0, size_t* step,
+                       int /*flags*/, UMatUsageFlags /*usageFlags*/) const
+    {
+        size_t total = CV_ELEM_SIZE(type);
+        for (int i = dims-1; i >= 0; i--)
+        {
+            if (step)
+            {
+                if (data0 && step[i] != CV_AUTOSTEP)
+                {
+                    CV_Assert(total <= step[i]);
+                    total = step[i];
+                }
+                else
+                {
+                    step[i] = total;
+                }
+            }
+    }
     }
     
-    MonsterStorage::Service::Service() {
-  AddMethod(new ::grpc::internal::RpcServiceMethod(
-      MonsterStorage_method_names[0],
-      ::grpc::internal::RpcMethod::NORMAL_RPC,
-      new ::grpc::internal::RpcMethodHandler< MonsterStorage::Service, flatbuffers::grpc::Message<Monster>, flatbuffers::grpc::Message<Stat>>(
-          std::mem_fn(&MonsterStorage::Service::Store), this)));
-  AddMethod(new ::grpc::internal::RpcServiceMethod(
-      MonsterStorage_method_names[1],
-      ::grpc::internal::RpcMethod::SERVER_STREAMING,
-      new ::grpc::internal::ServerStreamingHandler< MonsterStorage::Service, flatbuffers::grpc::Message<Stat>, flatbuffers::grpc::Message<Monster>>(
-          std::mem_fn(&MonsterStorage::Service::Retrieve), this)));
+    TEST_F(HybridEnd2endTest, AsyncEcho) {
+  typedef EchoTestService::WithAsyncMethod_Echo<TestServiceImpl> SType;
+  SType service;
+  SetUpServer(&service, nullptr, nullptr);
+  ResetStub();
+  std::thread echo_handler_thread(HandleEcho<SType>, &service, cqs_[0].get(),
+                                  false);
+  TestAllMethods();
+  echo_handler_thread.join();
 }
     
-    // Computes how many bytes you'd have to pad to be able to write an
-// 'scalar_size' scalar if the buffer had grown to 'buf_size' (downwards in
-// memory).
-inline size_t PaddingBytes(size_t buf_size, size_t scalar_size) {
-  return ((~buf_size) + 1) & (scalar_size - 1);
-}
-    
-    template<typename T> T HashFnv1(const char *input) {
-  T hash = FnvTraits<T>::kOffsetBasis;
-  for (const char *c = input; *c; ++c) {
-    hash *= FnvTraits<T>::kFnvPrime;
-    hash ^= static_cast<unsigned char>(*c);
+    void grpc_tsi_alts_shutdown() {
+  gpr_mu_destroy(&g_alts_resource.mu);
+  if (g_alts_resource.cq == nullptr) {
+    return;
   }
-  return hash;
+  grpc_completion_queue_destroy(g_alts_resource.cq);
+  grpc_channel_destroy(g_alts_resource.channel);
+  gpr_thd_join(g_alts_resource.thread_id);
+}
+
+    
+    static gpr_timespec five_seconds_from_now(void) {
+  return n_seconds_from_now(5);
 }
     
+        memset(ops, 0, sizeof(ops));
+    op = ops;
+    op->op = GRPC_OP_SEND_MESSAGE;
+    op->data.send_message.send_message = request_payload;
+    op->flags = 0;
+    op->reserved = nullptr;
+    op++;
+    op->op = GRPC_OP_RECV_MESSAGE;
+    op->data.recv_message.recv_message = &response_payload_recv;
+    op->flags = 0;
+    op->reserved = nullptr;
+    op++;
+    error = grpc_call_start_batch(c, ops, (size_t)(op - ops), tag(2), nullptr);
+    GPR_ASSERT(GRPC_CALL_OK == error);
     
-    {}  // namespace flatbuffers
+      // if num_clients is set to <=0, do dynamic sizing: all workers
+  // except for servers are clients
+  if (num_clients <= 0) {
+    num_clients = workers.size() - num_servers;
+  }
+    
+      std::thread* server_try_cancel_thd = nullptr;
+  if (server_try_cancel == CANCEL_DURING_PROCESSING) {
+    server_try_cancel_thd =
+        new std::thread(&TestServiceImpl::ServerTryCancel, this, context);
+  }
+    
+      Status RequestStream(ServerContext* context,
+                       ServerReader<EchoRequest>* reader,
+                       EchoResponse* response) override;
+    
+    //---- Don't implement demo windows functionality (ShowDemoWindow()/ShowStyleEditor()/ShowUserGuide() methods will be empty)
+//---- It is very strongly recommended to NOT disable the demo windows. Please read the comment at the top of imgui_demo.cpp.
+//#define IMGUI_DISABLE_DEMO_WINDOWS
+    
+            // Create the input layout
+        D3D10_INPUT_ELEMENT_DESC local_layout[] = 
+        {
+            { 'POSITION', 0, DXGI_FORMAT_R32G32_FLOAT,   0, (size_t)(&((ImDrawVert*)0)->pos), D3D10_INPUT_PER_VERTEX_DATA, 0 },
+            { 'TEXCOORD', 0, DXGI_FORMAT_R32G32_FLOAT,   0, (size_t)(&((ImDrawVert*)0)->uv),  D3D10_INPUT_PER_VERTEX_DATA, 0 },
+            { 'COLOR',    0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, (size_t)(&((ImDrawVert*)0)->col), D3D10_INPUT_PER_VERTEX_DATA, 0 },
+        };
+        if (g_pd3dDevice->CreateInputLayout(local_layout, 3, g_pVertexShaderBlob->GetBufferPointer(), g_pVertexShaderBlob->GetBufferSize(), &g_pInputLayout) != S_OK)
+            return false;
+    
+    // Use if you want to reset your rendering device without losing ImGui state.
+IMGUI_API void        ImGui_ImplDX10_InvalidateDeviceObjects();
+IMGUI_API bool        ImGui_ImplDX10_CreateDeviceObjects();
+    
+    void ImGui_ImplDX9_InvalidateDeviceObjects()
+{
+    if (!g_pd3dDevice)
+        return;
+    if (g_pVB)
+    {
+        g_pVB->Release();
+        g_pVB = NULL;
+    }
+    if (g_pIB)
+    {
+        g_pIB->Release();
+        g_pIB = NULL;
+    }
+    }
+    
+    #pragma once
+    
+    #include <nbind/api.h>
+#include <nbind/BindDefiner.h>
+    
+    TEST(YogaTest, end_overrides) {
+  const YGNodeRef root = YGNodeNew();
+  YGNodeStyleSetFlexDirection(root, YGFlexDirectionRow);
+  YGNodeStyleSetWidth(root, 100);
+  YGNodeStyleSetHeight(root, 100);
+    }
+    
+    
+    {  YGNodeFreeRecursive(root);
+}
+
+    
+    namespace facebook {
+    }
+    
+    namespace facebook {
+    }
