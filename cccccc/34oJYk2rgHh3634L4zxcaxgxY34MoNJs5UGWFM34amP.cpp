0be0a80cbeb5d350e2ccf7528a1049e22eaf06e6
SUNsung <weatherius@gmail.com>
2018-02-09 01:25:52 +0200

    Rewrite [34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp] File in: 2018-02-09 01:25:52

--- cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
+++ cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
@@ -1,290 +1,272 @@
 
         
-        #include 'swift/AST/SyntaxASTMap.h'
-#include 'swift/AST/Expr.h'
-#include 'swift/AST/Decl.h'
-#include 'swift/AST/Stmt.h'
-#include 'swift/Syntax/Syntax.h'
+          testReads(
+      {{0, 5 * 1024 * 1024}, {kAlign, 5 * 1024 * 1024}}, AsyncIO::NOT_POLLABLE);
     
-    namespace swift {
+    std::shared_ptr<LogWriter> FileWriterFactory::createWriter(File file) {
+  // Determine whether we should use ImmediateFileWriter or AsyncFileWriter
+  if (async_) {
+    auto asyncWriter = make_shared<AsyncFileWriter>(std::move(file));
+    if (maxBufferSize_.hasValue()) {
+      asyncWriter->setMaxBufferSize(maxBufferSize_.value());
     }
-    
-      bool isEmpty() const {
-    return Parts.isEmpty();
-  }
-    
-      /// Retrieve a reference to the declaration this one overrides.
-  ConcreteDeclRef
-  getOverriddenDecl(ASTContext &ctx) const;
-    
-      bool haveLastDiag() {
-    return LastDiagBufferID >= 0;
-  }
-  void clearLastDiag() {
-    LastDiagBufferID = -1;
-  }
-  DiagnosticEntryInfo &getLastDiag() {
-    assert(haveLastDiag());
-    return BufferDiagnostics[LastDiagBufferID][LastDiagIndex];
-  }
-    
-    
-    {  /**
-   * @brief Computes the error gradient w.r.t. the absolute value inputs.
-   *
-   * @param top output Blob vector (length 1), providing the error gradient with
-   *      respect to the outputs
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
-   *      with respect to computed outputs @f$ y @f$
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$; Backward fills their diff with
-   *      gradients @f$
-   *        \frac{\partial E}{\partial x} =
-   *            \mathrm{sign}(x) \frac{\partial E}{\partial y}
-   *      @f$ if propagate_down[0]
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-};
-    
-    #endif  // CAFFE_CONTRASTIVE_LOSS_LAYER_HPP_
-
-    
-    #ifdef USE_CUDNN
-/**
- * @brief CuDNN acceleration of TanHLayer.
- */
-template <typename Dtype>
-class CuDNNTanHLayer : public TanHLayer<Dtype> {
- public:
-  explicit CuDNNTanHLayer(const LayerParameter& param)
-      : TanHLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNTanHLayer();
-    }
-    
-     protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-     protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    
-    {
-    {} // namespace asio
-} // namespace boost
-    
-    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
-# pragma once
-#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
-    
-      static void validate(const boost::array<Elem, 2>& buffer_sequence)
-  {
-    boost::asio::buffer_cast<const void*>(buffer_sequence[0]);
-    boost::asio::buffer_cast<const void*>(buffer_sequence[1]);
-  }
-    
-    
-    {
-    {
-    {} // namespace detail
-} // namespace asio
-} // namespace boost
-    
-      STDMETHODIMP get_Capacity(UINT32* value)
-  {
-    *value = capacity_;
-    return S_OK;
-  }
-    
-    #include 'port/port.h'
-#include 'util/testharness.h'
-#include 'util/env_posix_test_helper.h'
-    
-      void SetComparatorName(const Slice& name) {
-    has_comparator_ = true;
-    comparator_ = name.ToString();
-  }
-  void SetLogNumber(uint64_t num) {
-    has_log_number_ = true;
-    log_number_ = num;
-  }
-  void SetPrevLogNumber(uint64_t num) {
-    has_prev_log_number_ = true;
-    prev_log_number_ = num;
-  }
-  void SetNextFile(uint64_t num) {
-    has_next_file_number_ = true;
-    next_file_number_ = num;
-  }
-  void SetLastSequence(SequenceNumber seq) {
-    has_last_sequence_ = true;
-    last_sequence_ = seq;
-  }
-  void SetCompactPointer(int level, const InternalKey& key) {
-    compact_pointers_.push_back(std::make_pair(level, key));
-  }
-    
-      // Store the specified number as the sequence number for the start of
-  // this batch.
-  static void SetSequence(WriteBatch* batch, SequenceNumber seq);
-    
-    #include 'leveldb/env.h'
-#include 'leveldb/status.h'
-#include 'port/port.h'
-#include 'util/mutexlock.h'
-#include <map>
-#include <string.h>
-#include <string>
-#include <vector>
-    
-      // count the keys
-  leveldb::Iterator* iter = db->NewIterator(leveldb::ReadOptions());
-  size_t num_keys = 0;
-  for (iter->SeekToFirst(); iter->Valid(); iter->Next()) {
-    num_keys++;
-  }
-  delete iter;
-  ASSERT_EQ(kNumKeys, num_keys) << 'Bad number of keys';
-    
-    void CondVar::Wait() {
-  PthreadCall('wait', pthread_cond_wait(&cv_, &mu_->mu_));
-}
-    
-    class FilterBlockReader {
- public:
- // REQUIRES: 'contents' and *policy must stay live while *this is live.
-  FilterBlockReader(const FilterPolicy* policy, const Slice& contents);
-  bool KeyMayMatch(uint64_t block_offset, const Slice& key);
-    }
-    
-    Status BlockHandle::DecodeFrom(Slice* input) {
-  if (GetVarint64(input, &offset_) &&
-      GetVarint64(input, &size_)) {
-    return Status::OK();
+    return asyncWriter;
   } else {
-    return Status::Corruption('bad block handle');
+    if (maxBufferSize_.hasValue()) {
+      throw std::invalid_argument(to<string>(
+          'the \'max_buffer_size\' option is only valid for async file '
+          'handlers'));
+    }
+    return make_shared<ImmediateFileWriter>(std::move(file));
   }
 }
     
-      // Takes ownership of 'iter' and will delete it when destroyed, or
-  // when Set() is invoked again.
-  void Set(Iterator* iter) {
-    delete iter_;
-    iter_ = iter;
-    if (iter_ == NULL) {
-      valid_ = false;
-    } else {
-      Update();
-    }
-  }
+      // The fixed portion of the header takes up 31 bytes.
+  //
+  // The variable portions that we can't account for here include the line
+  // number and the thread ID (just in case it is larger than 6 digits long).
+  // Here we guess that 40 bytes will be long enough to include room for this.
+  //
+  // If this still isn't long enough the string will grow as necessary, so the
+  // code will still be correct, but just slightly less efficient than if we
+  // had allocated a large enough buffer the first time around.
+  size_t headerLengthGuess = 40 + basename.size();
     
-        Size(double width, double height)
-    : width(width)
-    , height(height)
+    
     {
-    }
-    
-    TEST(YogaTest, computed_layout_padding) {
-  const YGNodeRef root = YGNodeNew();
-  YGNodeStyleSetWidth(root, 100);
-  YGNodeStyleSetHeight(root, 100);
-  YGNodeStyleSetPaddingPercent(root, YGEdgeStart, 10);
-    }
-    
-      ASSERT_FLOAT_EQ(30, YGNodeLayoutGetLeft(root_child1));
-  ASSERT_FLOAT_EQ(0, YGNodeLayoutGetTop(root_child1));
-  ASSERT_FLOAT_EQ(50, YGNodeLayoutGetWidth(root_child1));
-  ASSERT_FLOAT_EQ(10, YGNodeLayoutGetHeight(root_child1));
-    
-    
-    {  YGConfigFree(config);
-}
+    {  folly::File file_;
+};
+} // namespace folly
 
     
-      const YGNodeRef root_child2 = YGNodeNewWithConfig(config);
-  YGNodeStyleSetFlexGrow(root_child2, 1);
-  YGNodeStyleSetFlexShrink(root_child2, 1);
-  YGNodeStyleSetFlexBasisPercent(root_child2, 0);
-  YGNodeInsertChild(root, root_child2, 2);
-  YGNodeCalculateLayout(root, YGUndefined, YGUndefined, YGDirectionLTR);
+    /*
+ * This file contains utility functions for parsing and serializing
+ * LogConfig strings.
+ *
+ * This is separate from the LogConfig class itself, to reduce the dependencies
+ * of the core logging library.  Other code that wants to use the logging
+ * library to log messages but does not need to parse log config strings
+ * therefore does not need to depend on the folly JSON library.
+ */
     
-      ASSERT_FLOAT_EQ(0, YGNodeLayoutGetLeft(root));
-  ASSERT_FLOAT_EQ(0, YGNodeLayoutGetTop(root));
-  ASSERT_FLOAT_EQ(100, YGNodeLayoutGetWidth(root));
-  ASSERT_FLOAT_EQ(100, YGNodeLayoutGetHeight(root));
     
-    namespace facebook {
-namespace alog {
+    {        // see if our best match qualifies
+        if (best < 3) { // fast path literals
+            ++q;
+        } else if (best > 2  &&  best <= 0x80    &&  dist <= 0x100) {
+            outliterals(lit_start, q-lit_start); lit_start = (q += best);
+            stb_out(0x80 + best-1);
+            stb_out(dist-1);
+        } else if (best > 5  &&  best <= 0x100   &&  dist <= 0x4000) {
+            outliterals(lit_start, q-lit_start); lit_start = (q += best);
+            stb_out2(0x4000 + dist-1);       
+            stb_out(best-1);
+        } else if (best > 7  &&  best <= 0x100   &&  dist <= 0x80000) {
+            outliterals(lit_start, q-lit_start); lit_start = (q += best);
+            stb_out3(0x180000 + dist-1);     
+            stb_out(best-1);
+        } else if (best > 8  &&  best <= 0x10000 &&  dist <= 0x80000) {
+            outliterals(lit_start, q-lit_start); lit_start = (q += best);
+            stb_out3(0x100000 + dist-1);     
+            stb_out2(best-1);
+        } else if (best > 9                      &&  dist <= 0x1000000) {
+            if (best > 65536) best = 65536;
+            outliterals(lit_start, q-lit_start); lit_start = (q += best);
+            if (best <= 0x100) {
+                stb_out(0x06);
+                stb_out3(dist-1);
+                stb_out(best-1);
+            } else {
+                stb_out(0x04);
+                stb_out3(dist-1);
+                stb_out2(best-1);
+            }
+        } else {  // fallback literals if no match was a balanced tradeoff
+            ++q;
+        }
     }
+    
+    void DebugHUD_InitDefaults( DebugHUD *hud )
+{
+    hud->show_demo_window = true;
+    hud->show_another_window = true;
+    hud->rotation_speed = 15.0f;
+    
+    hud->cubeColor1[0] = 0.4f;
+    hud->cubeColor1[1] = 0.4f;
+    hud->cubeColor1[2] = 1.0f;
+    hud->cubeColor1[3] = 1.0f;
+    
+    hud->cubeColor2[0] = 1.0f;
+    hud->cubeColor2[1] = 0.4f;
+    hud->cubeColor2[2] = 0.4f;
+    hud->cubeColor2[3] = 1.0f;
+    
+    hud->clearColor[0] = 0.45f;
+    hud->clearColor[1] = 0.55f;
+    hud->clearColor[2] = 0.60f;
+    hud->clearColor[3] = 1.00f;
+}
+    
+    // Implemented features:
+//  [X] User texture binding. Use 'ID3D10ShaderResourceView*' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
+    
+    int32 ImGui_Marmalade_PointerButtonEventCallback(void* SystemData, void* pUserData)
+{
+    // pEvent->m_Button is of type s3ePointerButton and indicates which mouse
+    // button was pressed.  For touchscreen this should always have the value
+    // S3E_POINTER_BUTTON_SELECT
+    s3ePointerEvent* pEvent = (s3ePointerEvent*)SystemData;
     }
     
+        // Cleanup
+    ImGui_Marmalade_Shutdown();
+    ImGui::DestroyContext();
+    IwGxTerminate();
     
-    {private:
-  const char* m_functionName;
-  const char* m_fileName;
-  int m_lineNumber;
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you use this binding you'll need to call 4 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXXX_NewFrame(), ImGui::Render() and ImGui_ImplXXXX_Shutdown().
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+      grpc::ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  std::cerr << 'Server listening on ' << server_address << std::endl;
+    
+    inline const char **EnumNamesEnumInNestedNS() {
+  static const char *names[] = {
+    'A',
+    'B',
+    'C',
+    nullptr
+  };
+  return names;
+}
+    
+    
+    {  WeaponT *AsWeapon() {
+    return type == Equipment_Weapon ?
+      reinterpret_cast<WeaponT *>(value) : nullptr;
+  }
+  const WeaponT *AsWeapon() const {
+    return type == Equipment_Weapon ?
+      reinterpret_cast<const WeaponT *>(value) : nullptr;
+  }
 };
     
-    template <typename T>
-inline bool operator!=(const RefPtr<T>& ref, std::nullptr_t ptr) {
-  return ref.get() != ptr;
-}
+    #include <grpc++/impl/codegen/async_stream.h>
+#include <grpc++/impl/codegen/async_unary_call.h>
+#include <grpc++/impl/codegen/method_handler_impl.h>
+#include <grpc++/impl/codegen/proto_utils.h>
+#include <grpc++/impl/codegen/rpc_method.h>
+#include <grpc++/impl/codegen/service_type.h>
+#include <grpc++/impl/codegen/status.h>
+#include <grpc++/impl/codegen/stub_options.h>
+#include <grpc++/impl/codegen/sync_stream.h>
     
-    bool nextConnection(int tid)
-{
-    m.lock();
-    int socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (socketfd == -1) {
-        cout << 'FD error, connections: ' << connections << endl;
-        return false;
+    grpc::string GetHeaderEpilogue(grpc_generator::File *file,
+                               const Parameters & /*params*/) {
+  grpc::string output;
+  {
+    // Scope the output stream so it closes and finalizes output to the string.
+    auto printer = file->CreatePrinter(&output);
+    std::map<grpc::string, grpc::string> vars;
     }
     }
     
-    Context::Context(const Context &other)
-{
-    if (other.context) {
-        context = other.context;
-        SSL_CTX_up_ref(context);
-    }
-}
+    // Return the source of the generated service file.
+grpc::string GenerateServiceSource(grpc_generator::File* file,
+                                   const grpc_generator::Service* service,
+                                   grpc_java_generator::Parameters* parameters);
     
-    #ifdef UWS_THREADSAFE
-    std::lock_guard<std::recursive_mutex> lockGuard(*nodeData->asyncMutex);
-    if (isClosed()) {
-        if (callback) {
-            callback(this, callbackData, true, nullptr);
+    namespace grpc {
+    }
+    
+    class FixedTypedVector : public Object {
+ public:
+  FixedTypedVector(const uint8_t *data, uint8_t byte_width, Type element_type,
+                   uint8_t len)
+      : Object(data, byte_width), type_(element_type), len_(len) {}
+    }
+    
+     private:
+  bool LoadSchema(const std::string &ident, Parser *parser) {
+    // Find the schema, if not, exit.
+    auto it = schemas_.find(ident);
+    if (it == schemas_.end()) {
+      // Don't attach the identifier, since it may not be human readable.
+      lasterror_ = 'identifier for this buffer not in the registry';
+      return false;
+    }
+    auto &schema = it->second;
+    // Load the schema from disk. If not, exit.
+    std::string schematext;
+    if (!LoadFile(schema.path_.c_str(), false, &schematext)) {
+      lasterror_ = 'could not load schema: ' + schema.path_;
+      return false;
+    }
+    // Parse schema.
+    parser->opts = opts_;
+    if (!parser->Parse(schematext.c_str(), vector_data(include_paths_),
+                       schema.path_.c_str())) {
+      lasterror_ = parser->error_;
+      return false;
+    }
+    return true;
+  }
+    
+    using namespace MyGame::Sample;
+    
+    // pull out the type of messages sent by our config
+typedef server::message_ptr message_ptr;
+    
+            if (!ext->messages.empty()) {
+            lws_callback_on_writable(wsi);
         }
-        return;
-    }
-#endif
     
-        void destroy() {
-        delete this;
+        auto startPoint = high_resolution_clock::now();
+    vector<thread *> threads;
+    for (int i = 0; i < THREADS; i++) {
+        threads.push_back(new thread([i] {
+            while(nextConnection(i));
+        }));
     }
     
-        Async(Loop *loop) : Poll(loop, ::eventfd(0, EFD_CLOEXEC)) {
-        this->loop = loop;
+    template <bool isServer>
+void Group<isServer>::startAutoPing(int intervalMs, std::string userMessage) {
+    timer = new uS::Timer(loop);
+    timer->setData(this);
+    timer->start(timerCallback, intervalMs, intervalMs);
+    userPingMessage = userMessage;
+}
+    
+    // todo: move this out to examples folder, it is not a test but a stragiht up example of EventSource support
+void serveEventSource() {
+    uWS::Hub h;
+    }
+    
+    public:
+    void addAsync() {
+        async = new Async(loop);
+        async->setData(this);
+        async->start(NodeData::asyncCallback);
+    }
+    
+        static inline size_t formatMessage(char *dst, const char *src, size_t length, OpCode opCode, size_t reportedLength, bool compressed) {
+        size_t messageLength;
+        size_t headerLength;
+        if (reportedLength < 126) {
+            headerLength = 2;
+            dst[1] = reportedLength;
+        } else if (reportedLength <= UINT16_MAX) {
+            headerLength = 4;
+            dst[1] = 126;
+            *((uint16_t *) &dst[2]) = htons(reportedLength);
+        } else {
+            headerLength = 10;
+            dst[1] = 127;
+            *((uint64_t *) &dst[2]) = htobe64(reportedLength);
+        }
     }
