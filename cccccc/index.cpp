411e9c6b8722a3da5c659b0ebffb37fe17c557e4
SUNsung <weatherius@gmail.com>
2018-02-08 13:47:31 +0200

    Rewrite [index.cpp] File in: 2018-02-08 13:47:30

--- cccccc/index.cpp
+++ cccccc/index.cpp
@@ -1,198 +1,152 @@
 
         
-        void SetProxyConfigCallback(
-    base::WaitableEvent* done,
-    net::URLRequestContextGetter* url_request_context_getter,
-    const net::ProxyConfig& proxy_config) {
-  net::ProxyService* proxy_service =
-      url_request_context_getter->GetURLRequestContext()->proxy_service();
-  proxy_service->ResetConfigService(
-      new net::ProxyConfigServiceFixed(proxy_config));
-  done->Signal();
-}
-    
-    #include 'base/basictypes.h'
-#include '../dispatcher_host.h'
-    
-    ui::KeyboardCode GetKeycodeFromText(std::string text){
-  ui::KeyboardCode retval = ui::VKEY_UNKNOWN;
-  if (text.size() != 0){
-    std::string upperText = base::ToUpperASCII(text);
-    std::string keyName = text;
-    bool found = false;
-    if (upperText.size() == 1){
-      char key = upperText[0];
-      if (key>='0' && key<='9'){//handle digital
-        keyName = 'Digit' + upperText;
-        found = true;
-      } else if (key>='A'&&key<='Z'){//handle alphabet
-        keyName = 'Key' + upperText;
-        found = true;
-      }
+        namespace google {
+namespace protobuf {
+namespace {
     }
     }
     }
     
+    class MapFieldGenerator : public FieldGeneratorBase {
+ public:
+  MapFieldGenerator(const FieldDescriptor* descriptor,
+                    int fieldOrdinal,
+                    const Options* options);
+  ~MapFieldGenerator();
+    }
     
-    {
-} // namespace extensions
+    
+    {}
+    
+    void WriteMessageDocComment(io::Printer* printer, const Descriptor* message);
+void WriteFieldDocComment(io::Printer* printer, const FieldDescriptor* field);
+void WriteEnumDocComment(io::Printer* printer, const EnumDescriptor* enum_);
+void WriteEnumValueDocComment(io::Printer* printer,
+                              const EnumValueDescriptor* value);
+void WriteServiceDocComment(io::Printer* printer,
+                            const ServiceDescriptor* service);
+void WriteMethodDocComment(io::Printer* printer,
+                           const MethodDescriptor* method);
+    
+     private:
+  template <
+      template <typename> class Atom,
+      typename Deadline,
+      typename IdleTime>
+  static bool futexWaitPreIdle(
+      FutexResult& _ret,
+      Futex<Atom>& fut,
+      uint32_t expected,
+      Deadline const& deadline,
+      uint32_t waitMask,
+      IdleTime idleTimeout,
+      size_t stackToRetain,
+      float timeoutVariationFrac) {
+    // idleTimeout < 0 means no flush behavior
+    if (idleTimeout < IdleTime::zero()) {
+      return false;
+    }
+    }
+    
+      auto seq2 = folly::index_sequence<0, 4, 3>();
+  EXPECT_EQ(3, seq2.size());
+    
+      T* address(T& x) const {
+    return std::addressof(x);
+  }
+    
+    #if !__clang__ || __CLANG_PREREQ(3, 7) // Clang 3.6 crashes on this line
+  /* implicit */ Range(std::nullptr_t) = delete;
 #endif
+    
+      /// Always claims to be on CPU zero, node zero
+  static int degenerateGetcpu(unsigned* cpu, unsigned* node, void*) {
+    if (cpu != nullptr) {
+      *cpu = 0;
+    }
+    if (node != nullptr) {
+      *node = 0;
+    }
+    return 0;
+  }
+    
+    template <typename F>
+using ScopeGuardImplDecay = ScopeGuardImpl<typename std::decay<F>::type>;
+    
+        int getOverflow(void) const;
+    int getDisplay(void) const;
+    
+    
+    {    void toJS(nbind::cbOutput expose) const
+    {
+        expose(left, right, top, bottom, width, height);
+    }
+};
 
-    
-    void JRodriguesMatlab(const Mat& src, Mat& dst);
-    
-            if (lane < 16)
-        {
-            T partial = ptr[tid];
-    }
-    
-            //here everything goes. check that everything is setted properly
-        dprintf(('proxy_x\n'));print_matrix(proxy_x);
-        dprintf(('d first time\n'));print_matrix(d);
-        dprintf(('r\n'));print_matrix(r);
-    
-    cv::cuda::GpuMat::GpuMat(const GpuMat& m, Rect roi) :
-    flags(m.flags), rows(roi.height), cols(roi.width),
-    step(m.step), data(m.data + roi.y*step), refcount(m.refcount),
-    datastart(m.datastart), dataend(m.dataend),
-    allocator(m.allocator)
-{
-    flags &= roi.width < m.cols ? ~Mat::CONTINUOUS_FLAG : -1;
-    data += roi.x * elemSize();
-    }
-    
-        UMatData* allocate(int dims, const int* sizes, int type,
-                       void* data0, size_t* step,
-                       int /*flags*/, UMatUsageFlags /*usageFlags*/) const
-    {
-        size_t total = CV_ELEM_SIZE(type);
-        for (int i = dims-1; i >= 0; i--)
-        {
-            if (step)
-            {
-                if (data0 && step[i] != CV_AUTOSTEP)
-                {
-                    CV_Assert(total <= step[i]);
-                    total = step[i];
-                }
-                else
-                {
-                    step[i] = total;
-                }
-            }
-    }
-    }
-    
-    TEST_F(HybridEnd2endTest, AsyncEcho) {
-  typedef EchoTestService::WithAsyncMethod_Echo<TestServiceImpl> SType;
-  SType service;
-  SetUpServer(&service, nullptr, nullptr);
-  ResetStub();
-  std::thread echo_handler_thread(HandleEcho<SType>, &service, cqs_[0].get(),
-                                  false);
-  TestAllMethods();
-  echo_handler_thread.join();
-}
-    
-    void grpc_tsi_alts_shutdown() {
-  gpr_mu_destroy(&g_alts_resource.mu);
-  if (g_alts_resource.cq == nullptr) {
-    return;
-  }
-  grpc_completion_queue_destroy(g_alts_resource.cq);
-  grpc_channel_destroy(g_alts_resource.channel);
-  gpr_thd_join(g_alts_resource.thread_id);
-}
-
-    
-    static gpr_timespec five_seconds_from_now(void) {
-  return n_seconds_from_now(5);
-}
-    
-        memset(ops, 0, sizeof(ops));
-    op = ops;
-    op->op = GRPC_OP_SEND_MESSAGE;
-    op->data.send_message.send_message = request_payload;
-    op->flags = 0;
-    op->reserved = nullptr;
-    op++;
-    op->op = GRPC_OP_RECV_MESSAGE;
-    op->data.recv_message.recv_message = &response_payload_recv;
-    op->flags = 0;
-    op->reserved = nullptr;
-    op++;
-    error = grpc_call_start_batch(c, ops, (size_t)(op - ops), tag(2), nullptr);
-    GPR_ASSERT(GRPC_CALL_OK == error);
-    
-      // if num_clients is set to <=0, do dynamic sizing: all workers
-  // except for servers are clients
-  if (num_clients <= 0) {
-    num_clients = workers.size() - num_servers;
-  }
-    
-      std::thread* server_try_cancel_thd = nullptr;
-  if (server_try_cancel == CANCEL_DURING_PROCESSING) {
-    server_try_cancel_thd =
-        new std::thread(&TestServiceImpl::ServerTryCancel, this, context);
-  }
-    
-      Status RequestStream(ServerContext* context,
-                       ServerReader<EchoRequest>* reader,
-                       EchoResponse* response) override;
-    
-    //---- Don't implement demo windows functionality (ShowDemoWindow()/ShowStyleEditor()/ShowUserGuide() methods will be empty)
-//---- It is very strongly recommended to NOT disable the demo windows. Please read the comment at the top of imgui_demo.cpp.
-//#define IMGUI_DISABLE_DEMO_WINDOWS
-    
-            // Create the input layout
-        D3D10_INPUT_ELEMENT_DESC local_layout[] = 
-        {
-            { 'POSITION', 0, DXGI_FORMAT_R32G32_FLOAT,   0, (size_t)(&((ImDrawVert*)0)->pos), D3D10_INPUT_PER_VERTEX_DATA, 0 },
-            { 'TEXCOORD', 0, DXGI_FORMAT_R32G32_FLOAT,   0, (size_t)(&((ImDrawVert*)0)->uv),  D3D10_INPUT_PER_VERTEX_DATA, 0 },
-            { 'COLOR',    0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, (size_t)(&((ImDrawVert*)0)->col), D3D10_INPUT_PER_VERTEX_DATA, 0 },
-        };
-        if (g_pd3dDevice->CreateInputLayout(local_layout, 3, g_pVertexShaderBlob->GetBufferPointer(), g_pVertexShaderBlob->GetBufferSize(), &g_pInputLayout) != S_OK)
-            return false;
-    
-    // Use if you want to reset your rendering device without losing ImGui state.
-IMGUI_API void        ImGui_ImplDX10_InvalidateDeviceObjects();
-IMGUI_API bool        ImGui_ImplDX10_CreateDeviceObjects();
-    
-    void ImGui_ImplDX9_InvalidateDeviceObjects()
-{
-    if (!g_pd3dDevice)
-        return;
-    if (g_pVB)
-    {
-        g_pVB->Release();
-        g_pVB = NULL;
-    }
-    if (g_pIB)
-    {
-        g_pIB->Release();
-        g_pIB = NULL;
-    }
-    }
     
     #pragma once
     
-    #include <nbind/api.h>
-#include <nbind/BindDefiner.h>
+      const YGNodeRef root_child0 = YGNodeNew();
+  YGNodeStyleSetFlexGrow(root_child0, 1);
+  YGNodeStyleSetMargin(root_child0, YGEdgeHorizontal, 10);
+  YGNodeStyleSetMargin(root_child0, YGEdgeAll, 20);
+  YGNodeInsertChild(root, root_child0, 0);
     
-    TEST(YogaTest, end_overrides) {
-  const YGNodeRef root = YGNodeNew();
-  YGNodeStyleSetFlexDirection(root, YGFlexDirectionRow);
-  YGNodeStyleSetWidth(root, 100);
-  YGNodeStyleSetHeight(root, 100);
+      ASSERT_FLOAT_EQ(0, YGNodeLayoutGetLeft(root_child2));
+  ASSERT_FLOAT_EQ(0, YGNodeLayoutGetTop(root_child2));
+  ASSERT_FLOAT_EQ(50, YGNodeLayoutGetWidth(root_child2));
+  ASSERT_FLOAT_EQ(50, YGNodeLayoutGetHeight(root_child2));
+    
+      const YGNodeRef root_child0 = YGNodeNewWithConfig(config);
+  YGNodeStyleSetWidth(root_child0, 10);
+  YGNodeStyleSetHeight(root_child0, 10);
+  YGNodeInsertChild(root, root_child0, 0);
+  YGNodeCalculateLayout(root, YGUndefined, YGUndefined, YGDirectionLTR);
+    
+      const YGNodeRef root = YGNodeNewWithConfig(config);
+  YGNodeStyleSetHeightPercent(root, 100);
+  YGNodeStyleSetWidthPercent(root, 100);
+    
+      if (InitialCorpus.empty()) {
+    InitialCorpus.push_back(Unit({'\n'}));  // Valid ASCII input.
+    if (Options.Verbosity)
+      Printf('INFO: A corpus is not provided, starting from an empty corpus\n');
+  }
+  F->ShuffleAndMinimize(&InitialCorpus);
+  InitialCorpus.clear();  // Don't need this memory any more.
+  F->Loop();
+    
+    namespace fuzzer {
     }
     
-    
-    {  YGNodeFreeRecursive(root);
-}
-
-    
-    namespace facebook {
+    namespace fuzzer {
     }
     
-    namespace facebook {
+    namespace fuzzer {
+    }
+    
+    #ifndef LLVM_FUZZER_TRACE_PC
+#define LLVM_FUZZER_TRACE_PC
+    
+    // This is a reimplementation of Libc's `system()`. On Darwin the Libc
+// implementation contains a mutex which prevents it from being used
+// concurrently. This implementation **can** be used concurrently. It sets the
+// signal handlers when the first thread enters and restores them when the last
+// thread finishes execution of the function and ensures this is not racey by
+// using a mutex.
+int ExecuteCommand(const std::string &Command) {
+  posix_spawnattr_t SpawnAttributes;
+  if (posix_spawnattr_init(&SpawnAttributes))
+    return -1;
+  // Block and ignore signals of the current process when the first thread
+  // enters.
+  {
+    std::lock_guard<std::mutex> Lock(SignalMutex);
+    if (ActiveThreadCount == 0) {
+      static struct sigaction IgnoreSignalAction;
+      sigset_t BlockedSignalsSet;
+      memset(&IgnoreSignalAction, 0, sizeof(IgnoreSignalAction));
+      IgnoreSignalAction.sa_handler = SIG_IGN;
+    }
+    }
     }
