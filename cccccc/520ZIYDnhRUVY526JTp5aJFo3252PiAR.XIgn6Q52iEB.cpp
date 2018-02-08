e34b261185a4d12e8d3c41e2fcc496397d9589bb
SUNsung <weatherius@gmail.com>
2018-02-08 20:07:56 +0200

    Rewrite [520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp] File in: 2018-02-08 20:07:56

--- cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
+++ cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
@@ -1,263 +1,167 @@
 
         
-        int main(int argc, char** argv) {
-  FLAGS_alsologtostderr = 1;
-    }
-    
-    #ifdef USE_OPENCV
-  /**
-   * @brief Applies the transformation defined in the data layer's
-   * transform_param block to a vector of Mat.
-   *
-   * @param mat_vector
-   *    A vector of Mat containing the data to be transformed.
-   * @param transformed_blob
-   *    This is destination blob. It can be part of top blob's data if
-   *    set_cpu_data() is used. See memory_layer.cpp for an example.
-   */
-  void Transform(const vector<cv::Mat> & mat_vector,
-                Blob<Dtype>* transformed_blob);
-    
-    #endif  // CAFFE_ARGMAX_LAYER_HPP_
-
-    
-     protected:
-  // Helper functions that abstract away the column buffer and gemm arguments.
-  // The last argument in forward_cpu_gemm is so that we can skip the im2col if
-  // we just called weight_cpu_gemm with the same input.
-  void forward_cpu_gemm(const Dtype* input, const Dtype* weights,
-      Dtype* output, bool skip_im2col = false);
-  void forward_cpu_bias(Dtype* output, const Dtype* bias);
-  void backward_cpu_gemm(const Dtype* input, const Dtype* weights,
-      Dtype* output);
-  void weight_cpu_gemm(const Dtype* input, const Dtype* output, Dtype*
-      weights);
-  void backward_cpu_bias(Dtype* bias, const Dtype* input);
-    
-    /**
- * @brief Index into the input blob along its first axis.
- *
- * This layer can be used to select, reorder, and even replicate examples in a
- * batch.  The second blob is cast to int and treated as an index into the
- * first axis of the first blob.
- */
-template <typename Dtype>
-class BatchReindexLayer : public Layer<Dtype> {
- public:
-  explicit BatchReindexLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    
-    {}  // namespace caffe
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    
-    {}  // namespace caffe
-    
-    #ifdef USE_CUDNN
-/**
- * @brief cuDNN implementation of SoftmaxLayer.
- *        Fallback to SoftmaxLayer for CPU mode.
- */
-template <typename Dtype>
-class CuDNNSoftmaxLayer : public SoftmaxLayer<Dtype> {
- public:
-  explicit CuDNNSoftmaxLayer(const LayerParameter& param)
-      : SoftmaxLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNSoftmaxLayer();
-    }
-    
-    
-    {}  // namespace caffe
-    
-    
-    {  bool stable_prod_grad_;
-};
-    
-     protected:
-  /**
-   * @param bottom input Blob vector (length 2+)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs to be filtered @f$ x_1 @f$
-   *   -# ...
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs to be filtered @f$ x_K @f$
-   *   -# @f$ (N \times 1 \times 1 \times 1) @f$
-   *      the selector blob
-   * @param top output Blob vector (length 1+)
-   *   -# @f$ (S \times C \times H \times W) @f$ ()
-   *        the filtered output @f$ x_1 @f$
-   *        where S is the number of items
-   *        that haven't been filtered
-   *      @f$ (S \times C \times H \times W) @f$
-   *        the filtered output @f$ x_K @f$
-   *        where S is the number of items
-   *        that haven't been filtered
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-    const vector<Blob<Dtype>*>& top);
-    
-    
-    {    return S_OK;
+        void GlobalShortcut::OnKeyPressed(const ui::Accelerator& accelerator) {
+  if (accelerator_callback_map_.find(accelerator) ==
+      accelerator_callback_map_.end()) {
+    // This should never occur, because if it does, GlobalGlobalShortcutListener
+    // notifes us with wrong accelerator.
+    NOTREACHED();
+    return;
+  }
+  accelerator_callback_map_[accelerator].Run();
 }
     
-                    // cost(i) = CbarVec' * D * CbarVec;
-                cv::Mat cost_mat =  Cbarvec.t() * D * Cbarvec;
-                cost.push_back( cost_mat.at<double>(0) );
+    int RenderProcessPreferences::AddEntry(const base::DictionaryValue& entry) {
+  return preferences_.AddEntry(entry);
+}
     
-    CV_ModelEstimator2_Test::CV_ModelEstimator2_Test()
-{
-    generalPositionsCount = get_test_case_count() / 2;
-    maxPointsCount = 100;
+    #endif  // ATOM_BROWSER_API_ATOM_API_WEB_REQUEST_H_
+
+    
+    
+    {}  // namespace mate
+    
+    v8::Local<v8::Object> CreateEventFromFlags(v8::Isolate* isolate, int flags) {
+  mate::Dictionary obj = mate::Dictionary::CreateEmpty(isolate);
+  obj.Set('shiftKey', static_cast<bool>(flags & ui::EF_SHIFT_DOWN));
+  obj.Set('ctrlKey', static_cast<bool>(flags & ui::EF_CONTROL_DOWN));
+  obj.Set('altKey', static_cast<bool>(flags & ui::EF_ALT_DOWN));
+  obj.Set('metaKey', static_cast<bool>(flags & ui::EF_COMMAND_DOWN));
+  return obj.GetHandle();
+}
+    
+    // A self-destroyed class for handling save page request.
+class SavePageHandler : public content::DownloadManager::Observer,
+                        public content::DownloadItem::Observer {
+ public:
+  using SavePageCallback = base::Callback<void(v8::Local<v8::Value>)>;
     }
     
-    CV_EXPORTS_W void absdiff(InputArray src1, Scalar src2, OutputArray dst);
+    #include 'atom/browser/atom_quota_permission_context.h'
     
     
-    {} // namespace
+    {}  // namespace atom
+    
+    Delegate* AutoUpdater::delegate_ = nullptr;
+    
+    AsarProtocolHandler::AsarProtocolHandler(
+    const scoped_refptr<base::TaskRunner>& file_task_runner)
+    : file_task_runner_(file_task_runner) {}
+    
+    namespace atom {
+    }
+    
+      // URLRequestJob:
+  void GetResponseInfo(net::HttpResponseInfo* info) override;
     
     
-    {            for (auto& x : m_netStatefulNodes)
-            {
-                const wstring& name = x.first;
-                auto& pNode         = x.second;
-                if (m_netStates[name][iSubminibatch])
-                    pNode->ImportState(std::move(m_netStates[name][iSubminibatch]));
-            }
-        }
+    {  base::LaunchOptions options;
+  options.allow_new_privs = true;
+  options.new_process_group = true;  // detach
+  options.fds_to_remap = &no_stdout;
+  base::Process process = base::LaunchProcess(argv, options);
+  return process.IsValid() ? 0 : 1;
+}
     
-    template function<ComputationNetworkPtr(DEVICEID_TYPE)> GetNetworkFactory<ScriptableObjects::IConfigRecord, float>(const ScriptableObjects::IConfigRecord& config);
-template function<ComputationNetworkPtr(DEVICEID_TYPE)> GetNetworkFactory<ScriptableObjects::IConfigRecord, double>(const ScriptableObjects::IConfigRecord& config);
-template function<ComputationNetworkPtr(DEVICEID_TYPE)> GetNetworkFactory<ConfigParameters, float>(const ConfigParameters& config);
-template function<ComputationNetworkPtr(DEVICEID_TYPE)> GetNetworkFactory<ConfigParameters, double>(const ConfigParameters& config);
-template ComputationNetworkPtr GetModelFromConfig<ConfigParameters, float> (const ConfigParameters& config, const wstring&, vector<wstring>& outputNodeNamesVector);
-template ComputationNetworkPtr GetModelFromConfig<ConfigParameters, double>(const ConfigParameters& config, const wstring&, vector<wstring>& outputNodeNamesVector);
-
     
-    template void DoConvertFromDbn<float>(const ConfigParameters& config);
-template void DoConvertFromDbn<double>(const ConfigParameters& config);
-template void DoExportToDbn<float>(const ConfigParameters& config);
-template void DoExportToDbn<double>(const ConfigParameters& config);
-
+    {  cached_entries_.Clear();
+  for (const auto& iter : entries_)
+    cached_entries_.Append(iter.second->CreateDeepCopy());
+  cache_needs_update_ = false;
+}
     
-    // LoadConfigFiles - load multiple configuration file, and adds to config parameters
-// filePaths - A '+' delimited list of file paths, corresponding to config files to load
-// configStringToAppend - A config string which should be processed together with the config files
-void ConfigParser::LoadConfigFiles(const std::wstring& filePaths, const std::string* configStringToAppend)
-{
-    std::string configString = ReadConfigFiles(filePaths);
-    if (configStringToAppend != nullptr)
+    // static
+void App::Call(Shell* shell,
+               const std::string& method,
+               const base::ListValue& arguments,
+               base::ListValue* result,
+               DispatcherHost* dispatcher_host) {
+  if (method == 'GetDataPath') {
+    ShellBrowserContext* browser_context =
+      static_cast<ShellBrowserContext*>(shell->web_contents()->GetBrowserContext());
+    result->AppendString(browser_context->GetPath().value());
+    return;
+  }else if (method == 'GetArgv') {
+    nw::Package* package = shell->GetPackage();
+    CommandLine* command_line = CommandLine::ForCurrentProcess();
+    CommandLine::StringVector args = command_line->GetArgs();
+    CommandLine::StringVector argv = command_line->original_argv();
+    }
+    }
+    
+    
+    {}  // namespace nwapi
+    
+    
+    {  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
+  if (!item)
+    return;
+  item->OnClick();
+}
+    
+    void Menu::Insert(MenuItem* menu_item, int pos) {
+  std::vector<MenuItem*>::iterator begin = menu_items.begin();
+  menu_items.insert(begin+pos,menu_item);
+  if (GTK_IS_ACCEL_GROUP(gtk_accel_group)){
+    menu_item->UpdateKeys(gtk_accel_group);
+  }
+  gtk_menu_shell_insert(GTK_MENU_SHELL(menu_), menu_item->menu_item_, pos);
+}
+    
+    class NwClipboardClearSyncFunction : public NWSyncExtensionFunction {
+ public:
+  NwClipboardClearSyncFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+    using namespace std;
+    
+            bool inWildcard = !(firstStart + firstCount == secondStart && secondCount == 0);
+    
+    
+    {    return std::equal(s1.begin(), s1.end(), s2.begin(), [](const TElement& a, const TElement& b)
     {
-        configString += *configStringToAppend;
-    }
-    }
+        return std::tolower(a) == std::tolower(b);
+    });
+}
     
-    
-    {  // For fragments
-  kFirstType = 2,
-  kMiddleType = 3,
-  kLastType = 4
-};
-static const int kMaxRecordType = kLastType;
-    
-      // Store the specified number as the sequence number for the start of
-  // this batch.
-  static void SetSequence(WriteBatch* batch, SequenceNumber seq);
-    
-            // Create values for key-value pair
-        const int k = (order == SEQUENTIAL) ? i + j :
-                      (rand_.Next() % num_entries);
-        char key[100];
-        snprintf(key, sizeof(key), '%016d', k);
-    
-    
-    {}  // namespace leveldb
-
-    
-    class FilterPolicy;
-    
-    class FilterBlockTest {
+    // Add a second service with one sync streamed unary method.
+class StreamedUnaryDupPkg
+    : public duplicate::EchoTestService::WithStreamedUnaryMethod_Echo<
+          TestServiceImplDupPkg> {
  public:
-  TestHashFilter policy_;
+  Status StreamedEcho(
+      ServerContext* context,
+      ServerUnaryStreamer<EchoRequest, EchoResponse>* stream) override {
+    EchoRequest req;
+    EchoResponse resp;
+    uint32_t next_msg_sz;
+    stream->NextMessageSize(&next_msg_sz);
+    gpr_log(GPR_INFO, 'Streamed Unary Next Message Size is %u', next_msg_sz);
+    GPR_ASSERT(stream->Read(&req));
+    resp.set_message(req.message() + '_dup');
+    GPR_ASSERT(stream->Write(resp));
+    return Status::OK;
+  }
 };
     
-      // Must be called before any CompareBlock() calls can be called.
-  virtual void StartBlockComparisons() = 0;
-  // No more CompareBlock() calls can be called after this.
-  virtual void FinishBlockComparisons() = 0;
+    DEFINE_string(hashed_id, '', 'Hash of the user id');
     
-    void OutputImage::ToLinearRGB(int xmin, int ymin, int xsize, int ysize,
-                              std::vector<std::vector<float> >* rgb) const {
-  const double* lut = Srgb8ToLinearTable();
-  std::vector<uint8_t> rgb_pixels = ToSRGB(xmin, ymin, xsize, ysize);
-  for (int p = 0; p < xsize * ysize; ++p) {
-    for (int i = 0; i < 3; ++i) {
-      (*rgb)[i][p] = static_cast<float>(lut[rgb_pixels[3 * p + i]]);
-    }
+    /* decompress 'input' to 'output' using 'algorithm'.
+   On success, appends slices to output and returns 1.
+   On failure, output is unchanged, and returns 0. */
+int grpc_msg_decompress(grpc_message_compression_algorithm algorithm,
+                        grpc_slice_buffer* input, grpc_slice_buffer* output);
+    
+    static int is_compressed(grpc_byte_buffer* buffer) {
+  switch (buffer->type) {
+    case GRPC_BB_RAW:
+      if (buffer->data.raw.compression == GRPC_COMPRESS_NONE) {
+        return 0 /* GPR_FALSE */;
+      }
+      break;
   }
+  return 1 /* GPR_TRUE */;
 }
     
-    void UpdateGuess(const std::vector<float>& target,
-                 const std::vector<float>& reconstructed,
-                 std::vector<float>* guess) {
-  assert(reconstructed.size() == guess->size());
-  assert(target.size() == guess->size());
-  for (size_t i = 0; i < guess->size(); ++i) {
-    // TODO(user): Evaluate using a decaying constant here.
-    (*guess)[i] = Clip((*guess)[i] - (reconstructed[i] - target[i]));
-  }
-}
-    
-    
-    {}  // namespace guetzli
-    
-    #include <stdint.h>
-#include <string.h>
-#include <vector>
-    
-    #endif  // GUETZLI_JPEG_HUFFMAN_DECODE_H_
-
-    
-    #include <vector>
-#include <string>
-    
-    
-    {    pclose(stream);
-}
-    
-    // Licensed under the MIT License (the 'License'); you may not use this file except in 
-// compliance with the License. You may obtain a copy of the License at
-// http://opensource.org/licenses/MIT
-    
-    // Licensed under the MIT License (the 'License'); you may not use this file except in 
-// compliance with the License. You may obtain a copy of the License at
-// http://opensource.org/licenses/MIT
-    
-        xassert2(now > touch_times_.front());
-    
-    
-#endif /* defined(__PublicComponent__testspy__) */
-
-    
-    void TSpy::TestFun0()
-{
-    return reinterpret_cast<Test_Spy_Sample*>(This())->TestFun0();
-}
-    
-            JavaVMAttachArgs args;
-        args.group = NULL;
-        args.name = 'default';
-        args.version = JNI_VERSION_1_6;
-        status_ = vm_->AttachCurrentThread(&env_, &args);
+    static void proxy_mapper_destroy(grpc_proxy_mapper* mapper) {}
