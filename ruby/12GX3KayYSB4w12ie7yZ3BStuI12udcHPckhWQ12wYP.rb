02c2ece96c95aa317370948b63c2b07f48b81c8f
SUNsung <weatherius@gmail.com>
2018-02-08 13:15:51 +0200

    Rewrite [12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb] File in: 2018-02-08 13:15:51

--- ruby/12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb
+++ ruby/12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb
@@ -1,68 +1,154 @@
 
         
-        #
-def run_in_shell(*args)
-  p, output = Jekyll::Utils::Exec.run(*args)
-    
-        def no_subcommand(args)
-      unless args.empty? ||
-          args.first !~ %r(!/^--/!) || %w(--help --version).include?(args.first)
-        deprecation_message 'Jekyll now uses subcommands instead of just switches. \
-                          Run `jekyll help` to find out more.'
-        abort
-      end
+            def variant
+      ActiveStorage::Variant.new(image, variation).processed
     end
     
-        # Gets the name of this layout.
-    attr_reader :name
+          include ActiveSupport::Testing::ConstantLookup
     
-            # This should return an action callable for the given name.
-        #
-        # @param [Symbol] name Name of the action.
-        # @return [Object] A callable action sequence object, whether it
-        #   is a proc, object, etc.
-        def action(name)
-          nil
-        end
+            # Require the adapter itself and give useful feedback about
+        #   1. Missing adapter gems and
+        #   2. Adapter gems' missing dependencies.
+        path_to_adapter = 'action_cable/subscription_adapter/#{adapter}'
+        begin
+          require path_to_adapter
+        rescue LoadError => e
+          # We couldn't require the adapter itself. Raise an exception that
+          # points out config typos and missing gems.
+          if e.path == path_to_adapter
+            # We can assume that a non-builtin adapter was specified, so it's
+            # either misspelled or missing from Gemfile.
+            raise e.class, 'Could not load the '#{adapter}' Action Cable pubsub adapter. Ensure that the adapter is spelled correctly in config/cable.yml and that you've added the necessary adapter gem to your Gemfile.', e.backtrace
     
-          # Renders the template using the class intance as the binding. Because the
-      # renderer inherits from `OpenStruct`, additional view variables can be
-      # added like normal accessors.
-      #
-      # @return [String]
-      def render
-        old_template = template
-        result = nil
-        File.open(full_template_path, 'r') do |f|
-          self.template = f.read
-          result = render_string
-        end
+          def redis_connection_for_subscriptions
+        redis_connection
+      end
     
-    EOS
+          def reflections
+        join_root.drop(1).map!(&:reflection)
+      end
+    
+      s.files        = Dir['CHANGELOG.md', 'README.rdoc', 'MIT-LICENSE', 'lib/**/*']
+  s.require_path = 'lib'
+  s.requirements << 'none'
+    
+    module ActionMailer
+  # Implements the ActiveSupport::LogSubscriber for logging notifications when
+  # email is delivered or received.
+  class LogSubscriber < ActiveSupport::LogSubscriber
+    # An email was delivered.
+    def deliver(event)
+      info do
+        recipients = Array(event.payload[:to]).join(', ')
+        'Sent mail to #{recipients} (#{event.duration.round(1)}ms)'
+      end
+    
+    def bottle_native_regex
+  /(\.#{bottle_tag}\.bottle\.(\d+\.)?tar\.gz)$/o
 end
     
-      Sass::Plugin.options.merge!(config)
+        formula.extend(Debrew::Formula) if ARGV.debug?
     
-        def paragraphize(input)
-      '<p>#{input.lstrip.rstrip.gsub(/\n\n/, '</p><p>').gsub(/\n/, '<br/>')}</p>'
+      def merge(*args)
+    @settings.merge(*args)
+    self
+  end
+    
+      def internal_development_commands
+    find_internal_commands HOMEBREW_LIBRARY_PATH/'dev-cmd'
+  end
+    
+            command = [adb_path, 'devices'].join(' ')
+        output = Actions.sh(command, log: false)
+        output.split('\n').each do |line|
+          if (result = line.match(/(.*)\tdevice$/))
+            self.devices << AdbDevice.new(serial: result[1])
+          end
+        end
+        self.devices
+      end
     end
   end
 end
+
     
-          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
-        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
-        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
-          @img['title']  = title
-          @img['alt']    = alt
-        else
-          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
-        end
-        @img['class'].gsub!(/'/, '') if @img['class']
+          # Read-only path to the shell-escaped gradle script, suitable for use in shell commands
+      attr_reader :escaped_gradle_path
+    
+        attr_accessor :crashlytics_path
+    attr_accessor :api_key
+    attr_accessor :build_secret
+    attr_accessor :emails
+    attr_accessor :groups
+    attr_accessor :schemes
+    attr_accessor :export_method
+    
+        private
+    
+        class << self
+      attr_accessor :dir
+    
+              # Built-in events
+          if node['id'] == 'Events-catalog'
+            node.next_element.css('li').each do |li|
+              name = '#{li.at_css('b').content.delete(''').strip} event'
+              id = name.parameterize
+              li['id'] = id
+              entries << [name, id, type] unless name == entries.last[0]
+            end
+            next
+          end
+    
+          def additional_entries
+        entries = []
+    
+    require_relative 'converter/fonts_conversion'
+require_relative 'converter/less_conversion'
+require_relative 'converter/js_conversion'
+require_relative 'converter/logger'
+require_relative 'converter/network'
+    
+        def log_http_get_files(files, from, cached = false)
+      return if files.empty?
+      s = '  #{'CACHED ' if cached}GET #{files.length} files from #{from} #{files * ' '}...'
+      if cached
+        puts dark green s
+      else
+        puts dark cyan s
       end
-      super
     end
     
-        def render(context)
-      file_dir = (context.registers[:site].source || 'source')
-      file_path = Pathname.new(file_dir).expand_path
-      file = file_path + @file
+      def participation_answer(user)
+    poll_participations.find_by(author_id: user.person.id)
+  end
+    
+        def index
+      pods_json = PodPresenter.as_collection(Pod.all)
+    
+        def raise_because_imagemagick_missing
+      raise Errors::CommandNotFoundError.new('Could not run the `identify` command. Please install ImageMagick.')
+    end
+  end
+end
+
+    
+        def add_paperclip_callbacks
+      @klass.send(
+        :define_paperclip_callbacks,
+        :post_process, :'#{@name}_post_process')
+    end
+    
+        # You can add your own processor via the Paperclip configuration. Normally
+    # Paperclip will load all processors from the
+    # Rails.root/lib/paperclip_processors directory, but here you can add any
+    # existing class using this mechanism.
+    #
+    #   Paperclip.configure do |c|
+    #     c.register_processor :watermarker, WatermarkingProcessor.new
+    #   end
+    def register_processor(name, processor)
+      @known_processors ||= {}
+      @known_processors[name.to_s] = processor
+    end
+  end
+end
