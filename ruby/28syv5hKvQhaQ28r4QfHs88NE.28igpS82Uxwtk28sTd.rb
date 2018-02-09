ee1a0f6d2580ac501009be1dfc5b3444bca6157b
SUNsung <weatherius@gmail.com>
2018-02-09 14:02:35 +0200

    Rewrite [28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb] File in: 2018-02-09 14:02:34

--- ruby/28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb
+++ ruby/28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb
@@ -1,64 +1,93 @@
 
         
-          url = File.dirname(url)
-  url == FORWARD_SLASH ? url : '#{url}/'
-end
+          def create
+    return render_404 unless can_read?(resource)
     
-    def source_dir(*subdirs)
-  test_dir('source', *subdirs)
-end
-    
-          new_theme_name = args.join('_')
-      theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
-      if theme.path.exist?
-        Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.'
-      end
-    
-      def find_matching_tag(tag)
-    if key?(tag)
-      tag
+      entries.each do |entry|
+    if File.exist?(entry[:file])
+      warn '[skip] #{entry[:file]} already exists'
     else
-      find_altivec_tag(tag) || find_or_later_tag(tag)
-    end
-  end
-    
-          plist_filename = if f.plist
-        f.plist_path.basename
-      else
-        File.basename Dir['#{keg}/*.plist'].first
+      File.open(entry[:file], 'w+') do |f|
+        f.write(ERB.new(File.read(entry[:template])).result(binding))
+        puts I18n.t(:written_file, scope: :capistrano, file: entry[:file])
       end
-      plist_link = '#{destination}/#{plist_filename}'
-      plist_domain = f.plist_path.basename('.plist')
-      destination_path = Pathname.new File.expand_path destination
-      plist_path = destination_path/plist_filename
-    
-      def snow_leopard_64?
-    MacOS.prefer_64_bit?
-  end
-end
-    
-        process_font_assets
-    process_stylesheet_assets
-    process_javascript_assets
-    store_version
+    end
   end
     
-        # unindent by n spaces
-    def unindent(txt, n = 2)
-      txt.gsub /^[ ]{#{n}}/, ''
+      it 'overrides the rake method, and sets the sshkit_backend to SSHKit::Backend::Printer' do
+    capture_io do
+      flags '--dry-run', '-n'
+    end
+    sshkit_backend = Capistrano::Configuration.fetch(:sshkit_backend)
+    expect(sshkit_backend).to eq(SSHKit::Backend::Printer)
+  end
+    
+      context 'with default reaction' do
+    before(:each) do
+      mock_app do
+        use Rack::Protection::CookieTossing
+        run DummyApp
+      end
     end
     
-    desc 'Dumps output to a CSS file for testing'
-task :debug do
-  require 'sass'
-  path = Bootstrap.stylesheets_path
-  %w(bootstrap).each do |file|
-    engine = Sass::Engine.for_file('#{path}/#{file}.scss', syntax: :scss, load_paths: [path])
-    File.open('./#{file}.css', 'w') { |f| f.write(engine.render) }
+      it 'should not set the X-Frame-Options for other content types' do
+    expect(get('/', {}, 'wants' => 'text/foo').headers['X-Frame-Options']).to be_nil
   end
-end
     
-            def show
-          @stock_movement = scope.find(params[:id])
-          respond_with(@stock_movement)
+      if ''.respond_to?(:encoding)  # Ruby 1.9+ M17N
+    context 'PATH_INFO's encoding' do
+      before do
+        @app = Rack::Protection::PathTraversal.new(proc { |e| [200, {'Content-Type' => 'text/plain'}, [e['PATH_INFO'].encoding.to_s]] })
+      end
+    
+      it 'passes errors through if :reaction => :report is used' do
+    mock_app do
+      use Rack::Protection, :reaction => :report
+      run proc { |e| [200, {'Content-Type' => 'text/plain'}, [e['protection.failed'].to_s]] }
+    end
+    
+      it 'accepts requests with the same Accept-Language header' do
+    session = {:foo => :bar}
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
+    expect(session).not_to be_empty
+  end
+    
+            # This is a convenient way to check whether the variable is used
+        # in its entire variable lifetime.
+        # For more precise usage check, refer Assignment#used?.
+        #
+        # Once the variable is captured by a block, we have no idea
+        # when, where and how many times the block would be invoked
+        # and it means we cannot track the usage of the variable.
+        # So we consider it's used to suppress false positive offenses.
+        def used?
+          @captured_by_block || referenced?
         end
+    
+            def autocorrect(node)
+          redundant_regex?(node) do |receiver, regex_str|
+            receiver, regex_str = regex_str, receiver if receiver.is_a?(String)
+            regex_str = regex_str[2..-1] # drop \A anchor
+            regex_str = interpret_string_escapes(regex_str)
+    
+                  add_offense(condition)
+            end
+    
+            def on_if(node)
+          return unless nested_variable_comparison?(node.condition)
+          add_offense(node)
+        end
+    
+              arguments.each_with_index do |argument, index|
+            optarg_positions << index if argument.optarg_type?
+            arg_positions << index if argument.arg_type?
+          end
+    
+      context 'called with three styles' do
+    it 'applies second style to left and right' do
+      rule = 'border-style: dashed double solid'
+    
+    describe 'buttons' do
+  before(:all) do
+    ParserSupport.parse_file('library/buttons')
