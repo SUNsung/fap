8ccc3b1ad41789d3401d9dea5d459edbc8eba644
SUNsung <weatherius@gmail.com>
2018-02-09 13:32:18 +0200

    Rewrite [28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb] File in: 2018-02-09 13:32:18

--- ruby/28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb
+++ ruby/28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb
@@ -1,56 +1,64 @@
-  def regular?
-    !staff?
-  end
-    
-      protected
-    
-    platforms :jruby do
-  gem 'activerecord-jdbc-adapter'
-  gem 'activerecord-jdbcsqlite3-adapter'
-  gem 'jruby-openssl'
+
+        
+          url = File.dirname(url)
+  url == FORWARD_SLASH ? url : '#{url}/'
 end
     
-    # grab name/url pairings from README.md
-contents = File.read INPUT_FILENAME
-matches = contents.scan(/\* (.*) (http.*)/)
-# All blogs that do not respond
-unavailable = []
-temp_ignores = [
-  'AdRoll',
-  'Buzzfeed',
-  'SourceClear',
-  'TaskRabbit',
-  'theScore',
-  'Trivago',
-  'Xmartlabs',
-  'WyeWorks',
-  'Zoosk',
-  'Rudolf Olah'
-]
+    def source_dir(*subdirs)
+  test_dir('source', *subdirs)
+end
     
-            on roles(target_roles) do
-          unless test '[ -f #{file.to_s.shellescape} ]'
-            info 'Uploading #{prerequisite_file} to #{file}'
-            upload! File.open(prerequisite_file), file
-          end
-        end
+          new_theme_name = args.join('_')
+      theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
+      if theme.path.exist?
+        Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.'
       end
-    end
     
-            def create
-          authorize! :create, StockLocation
-          @stock_location = StockLocation.new(stock_location_params)
-          if @stock_location.save
-            respond_with(@stock_location, status: 201, default_template: :show)
-          else
-            invalid_resource!(@stock_location)
-          end
-        end
-    
-          expect('.all-buttons-focus').to have_ruleset(ruleset)
+      def find_matching_tag(tag)
+    if key?(tag)
+      tag
+    else
+      find_altivec_tag(tag) || find_or_later_tag(tag)
     end
   end
     
-      context 'called with no prefixes' do
-    it 'outputs the spec' do
-      rule = 'appearance: none;'
+          plist_filename = if f.plist
+        f.plist_path.basename
+      else
+        File.basename Dir['#{keg}/*.plist'].first
+      end
+      plist_link = '#{destination}/#{plist_filename}'
+      plist_domain = f.plist_path.basename('.plist')
+      destination_path = Pathname.new File.expand_path destination
+      plist_path = destination_path/plist_filename
+    
+      def snow_leopard_64?
+    MacOS.prefer_64_bit?
+  end
+end
+    
+        process_font_assets
+    process_stylesheet_assets
+    process_javascript_assets
+    store_version
+  end
+    
+        # unindent by n spaces
+    def unindent(txt, n = 2)
+      txt.gsub /^[ ]{#{n}}/, ''
+    end
+    
+    desc 'Dumps output to a CSS file for testing'
+task :debug do
+  require 'sass'
+  path = Bootstrap.stylesheets_path
+  %w(bootstrap).each do |file|
+    engine = Sass::Engine.for_file('#{path}/#{file}.scss', syntax: :scss, load_paths: [path])
+    File.open('./#{file}.css', 'w') { |f| f.write(engine.render) }
+  end
+end
+    
+            def show
+          @stock_movement = scope.find(params[:id])
+          respond_with(@stock_movement)
+        end
