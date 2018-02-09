6542bb695e2cf454f5f02e95fe8334ad7643748f
SUNsung <weatherius@gmail.com>
2018-02-09 19:21:56 +0200

    Rewrite [index.rb] File in: 2018-02-09 19:21:55

--- ruby/index.rb
+++ ruby/index.rb
@@ -1,69 +1,178 @@
 
         
-        desc 'Dumps output to a CSS file for testing'
-task :debug do
-  require 'sass'
-  path = Bootstrap.stylesheets_path
-  %w(bootstrap).each do |file|
-    engine = Sass::Engine.for_file('#{path}/#{file}.scss', syntax: :scss, load_paths: [path])
-    File.open('./#{file}.css', 'w') { |f| f.write(engine.render) }
-  end
-end
+        # For this pull request, which changes Page#dir
+# https://github.com/jekyll/jekyll/pull/4403
     
-    def list_login_items_for_app(app_path)
-  out, err, status = Open3.capture3(
-    '/usr/bin/osascript', '-e',
-    'tell application \'System Events\' to get the name of every login item ' \
-    'whose path contains \'#{File.basename(app_path)}\''
-  )
-  if status.exitstatus > 0
-    $stderr.puts err
-    exit status.exitstatus
-  end
-  puts out.gsub(', ', '\n')
-end
+    #
     
-    EOS
-end
-    
-    def config_tag(config, key, tag=nil, classname=nil)
-  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
-  tag       ||= 'div'
-  classname ||= key.sub(/_/, '-').sub(/\./, '-')
-  output      = '<#{tag} class='#{classname}''
-    
-          Dir.chdir(code_path) do
-        code = file.read
-        @filetype = file.extname.sub('.','') if @filetype.nil?
-        title = @title ? '#{@title} (#{file.basename})' : file.basename
-        url = '/#{code_dir}/#{@file}'
-        source = '<figure class='code'><figcaption><span>#{title}</span> <a href='#{url}'>download</a></figcaption>\n'
-        source += '#{HighlightCode::highlight(code, @filetype)}</figure>'
-        TemplateWrapper::safe_wrap(source)
+    If you run into trouble, you can find helpful resources at https://jekyllrb.com/help/!
+            MSG
+            raise Jekyll::Errors::MissingDependencyException, name
+          end
+        end
       end
     end
   end
+end
+
     
-      class VideoTag < Liquid::Tag
-    @video = nil
-    @poster = ''
-    @height = ''
-    @width = ''
-    
-    module LogStash
-  module Api
-    module Commands
-      module System
-        class Plugins < Commands::Base
-          def run
-            { :total => plugins.count, :plugins => plugins }
-          end
-    
-        on_event do |event, data|
-      Thread.current[:logstash_output_codec_batch] << [event, data]
+        if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
     end
   end
     
-      def fifty_percent
-    [1, (maximum * 0.5)].max.floor
+        # The path used after unlocking the resource
+    def after_unlock_path_for(resource)
+      new_session_path(resource)  if is_navigational_format?
+    end
+    
+      def app
+    Rails.application
   end
+end
+
+    
+          # Remembers the given resource by setting up a cookie
+      def remember_me(resource)
+        return if request.env['devise.skip_storage']
+        scope = Devise::Mapping.find_scope!(resource)
+        resource.remember_me!
+        cookies.signed[remember_key(resource, scope)] = remember_cookie_values(resource)
+      end
+    
+        def form_configurable_fields
+      self._form_configurable_fields
+    end
+    
+        def stop!
+      @scheduler.jobs(tag: id).each(&:unschedule)
+    
+      def index
+    set_table_sort sorts: %w[provider name global], default: { provider: :asc }
+    
+      def import
+    if params[:file]
+      file = params[:file]
+      content = JSON.parse(file.read)
+      new_credentials = content.map do |hash|
+        current_user.user_credentials.build(hash.slice('credential_name', 'credential_value', 'mode'))
+      end
+    
+            off = zone_offset(zone)
+        year, mon, day, hour, min, sec =
+          apply_offset(year, mon, day, hour, min, sec, off)
+        t = self.utc(year, mon, day, hour, min, sec)
+        force_zone!(t, zone, off)
+        t
+      else
+        raise ArgumentError.new('not RFC 2822 compliant date: #{date.inspect}')
+      end
+    end
+    alias rfc822 rfc2822
+    
+        private
+    
+      def test_ip
+    a = ACL::ACLEntry.new('192.0.0.0/8')
+    assert_not_operator(a, :match, @hosts['localhost'])
+    assert_operator(a, :match, @hosts['yum'])
+    
+        ctx_proc = proc { |ctx| ctx.ssl_version = :TLSv1_2 }
+    start_server(ctx_proc: ctx_proc) do |port|
+      called = {}
+      ctx = OpenSSL::SSL::SSLContext.new
+      ctx.session_cache_mode = OpenSSL::SSL::SSLContext::SESSION_CACHE_CLIENT
+      ctx.session_new_cb = lambda { |ary|
+        sock, sess = ary
+        called[:new] = [sock, sess]
+      }
+      if TEST_SESSION_REMOVE_CB
+        ctx.session_remove_cb = lambda { |ary|
+          ctx, sess = ary
+          called[:remove] = [ctx, sess]
+          # any resulting value is OK (ignored)
+        }
+      end
+    
+      ##
+  # The current version of ACL
+    
+          when :login_pass
+        if(s[:user] and s[:pass])
+          report_auth_info(s)
+          print_status('Successful FTP Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]}')
+          # Remove it form the session objects so freeup memory
+          sessions.delete(s[:session])
+          return
+        end
+    
+    classNames.each { |name|
+	codez << %Q^
+public class #{name} {
+	public static void main(String args[]) {
+		System.out.println('This is from #{name}.');
+	}
+}^}
+    
+      @src.__NR_execve      = __NR_execve
+  @src.__NR_getpeername = __NR_getpeername
+  @src.__NR_accept      = __NR_accept
+  @src.__NR_listen      = __NR_listen
+  @src.__NR_bind        = __NR_bind
+  @src.__NR_socket      = __NR_socket
+  @src.__NR_connect     = __NR_connect
+  @src.__NR_close       = __NR_close
+  @src.__NR_kfcntl      = __NR_kfcntl
+    
+    	def parse_line(line)
+		if line =~ /\w+ <[\.\w]+>:/
+			# End a previous block
+			unless block_size == 0
+				block_end
+			end
+			block_begin(line)
+    
+            def used?
+          @variable.captured_by_block? || @referenced
+        end
+    
+                lambda do |corrector|
+              new_source = receiver.source + '.end_with?(' +
+                           to_string_literal(regex_str) + ')'
+              corrector.replace(node.source_range, new_source)
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+    
+                previous.push(when_node.conditions)
+          end
+        end
+    
+            private
+    
+    module LogStash
+  module Api
+    class Service
+      include LogStash::Util::Loggable
+    
+      public
+  def close; end;
+    
+      def _nt_comment
+    start_index = index
+    if node_cache[:comment].has_key?(index)
+      cached = node_cache[:comment][index]
+      if cached
+        cached = SyntaxNode.new(input, index...(index + 1)) if cached == true
+        @index = cached.interval.end
+      end
+      return cached
+    end
