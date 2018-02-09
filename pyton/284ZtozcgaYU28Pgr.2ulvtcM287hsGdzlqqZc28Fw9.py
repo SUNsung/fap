b5884a64943ed7deb6f5d4b5c27e024002f3bddc
SUNsung <weatherius@gmail.com>
2018-02-09 09:59:43 +0200

    Rewrite [284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py] File in: 2018-02-09 09:59:43

--- pyton/284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py
+++ pyton/284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py
@@ -1,51 +1,149 @@
 
         
-        with io.open(README_FILE, encoding='utf-8') as f:
-    oldreadme = f.read()
+        
+@pytest.fixture()
+def chdir(tmpdir):
+    '''Change to pytest-provided temporary directory'''
+    tmpdir.chdir()
+
     
-        params = {
-        'age_limit': age,
-        'skip_download': True,
-        'writeinfojson': True,
-        'outtmpl': '%(id)s.%(ext)s',
+        def process_options(self, args, opts):
+        try:
+            self.settings.setdict(arglist_to_dict(opts.set),
+                                  priority='cmdline')
+        except ValueError:
+            raise UsageError('Invalid -s value, use -s NAME=VALUE', print_help=False)
+    
+    
+class _BenchSpider(scrapy.Spider):
+    '''A spider that follows all links'''
+    name = 'follow'
+    total = 10000
+    show = 20
+    baseurl = 'http://localhost:8998'
+    link_extractor = LinkExtractor()
+    
+        def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
+                          help='set spider argument (may be repeated)')
+        parser.add_option('-o', '--output', metavar='FILE',
+                          help='dump scraped items into FILE (use - for stdout)')
+        parser.add_option('-t', '--output-format', metavar='FORMAT',
+                          help='format to use for dumping items with -o')
+    
+        def set(self, results, query):
+        '''Set the result for the given query key in the cache.
+        
+        When updating an entry, updates its position to the front of the LRU list.
+        If the entry is new and the cache is at capacity, removes the oldest entry
+        before the new entry is added.
+        '''
+        node = self.lookup[query]
+        if node is not None:
+            # Key exists in cache, update the value
+            node.results = results
+            self.linked_list.move_to_front(node)
+        else:
+            # Key does not exist in cache
+            if self.size == self.MAX_SIZE:
+                # Remove the oldest entry from the linked list and lookup
+                self.lookup.pop(self.linked_list.tail.query, None)
+                self.linked_list.remove_from_tail()
+            else:
+                self.size += 1
+            # Add the new key and value
+            new_node = Node(results)
+            self.linked_list.append_to_front(new_node)
+            self.lookup[query] = new_node
+    
+        HOUSING = 0
+    FOOD = 1
+    GAS = 2
+    SHOPPING = 3
+    ...
+    
+        def extract_max_priority_page(self):
+        '''Return the highest priority link in `links_to_crawl`.'''
+        ...
+    
+            if first > last or first < 0 or last < 0 or last >= len(self.tokens):
+            raise ValueError(
+                'replace: range invalid: '+first+'..'+last+
+                '(size='+len(self.tokens)+')')
+    
+        entry_points = {'console_scripts': proj_info['console_scripts']}
+)
+
+    
+        def extract(self, **kwargs):
+        for i in self.streams:
+            s = self.streams[i]
+            _, s['container'], s['size'] = url_info(s['url'])
+            s['src'] = [s['url']]
+    
+    
+def construct_url(url, **params):
+    param_str = urlparse.urlencode(params)
+    return url + '?' + param_str
+    
+            mp3 = match1(content, r'name='filename'\s*value='([^']+\.mp3)'')
+        if mp3: mp3 = 'http://res.infoq.com/downloads/mp3downloads/%s' % mp3
+    
+    #----------------------------------------------------------------------
+def showroom_get_roomid_by_room_url_key(room_url_key):
+    '''str->str'''
+    fake_headers_mobile = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'UTF-8,*;q=0.5',
+        'Accept-Encoding': 'gzip,deflate,sdch',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'
     }
-    ydl = YoutubeDL(params)
-    ydl.add_default_info_extractors()
-    json_filename = os.path.splitext(filename)[0] + '.info.json'
-    try_rm(json_filename)
-    ydl.download([url])
-    res = os.path.exists(json_filename)
-    try_rm(json_filename)
-    return res
+    webpage_url = 'https://www.showroom-live.com/' + room_url_key
+    html = get_content(webpage_url, headers = fake_headers_mobile)
+    roomid = match1(html, r'room\?room_id\=(\d+)')
+    assert roomid
+    return roomid
     
-    # Allow direct execution
-import os
-import sys
-import unittest
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    def get_api_subtype():
+    if is_api() and c.render_style.startswith('api-'):
+        return c.render_style[4:]
     
+        @require_oauth2_scope('mysubreddits')
+    @validate(
+        VUser(),
+    )
+    @api_doc(
+        section=api_section.account,
+        uri='/api/v1/me/karma',
+    )
+    def GET_karma(self):
+        '''Return a breakdown of subreddit karma.'''
+        karmas = c.oauth_user.all_karmas(include_old=False)
+        resp = KarmaListJsonTemplate().render(karmas)
+        return self.api_wrapper(resp.finalize())
     
-class RtspFD(FileDownloader):
-    def real_download(self, filename, info_dict):
-        url = info_dict['url']
-        self.report_destination(filename)
-        tmpfilename = self.temp_name(filename)
-    
-        _TEST = {
-        'url': 'http://www.anitube.se/video/36621',
-        'md5': '59d0eeae28ea0bc8c05e7af429998d43',
-        'info_dict': {
-            'id': '36621',
-            'ext': 'mp4',
-            'title': 'Recorder to Randoseru 01',
-            'duration': 180.19,
-        },
-        'skip': 'Blocked in the US',
-    }
-    
-    from .onet import OnetBaseIE
-    
-    
-@pytest.mark.functional
-def test_select_command_with_arrows(proc, TIMEOUT):
-    select_command_with_arrows(proc, TIMEOUT)
+        def GET_cachehealth(self):
+        results = {}
+        behaviors = {
+            # Passed on to poll(2) in milliseconds
+            'connect_timeout': 1000,
+            # Passed on to setsockopt(2) in microseconds
+            'receive_timeout': int(1e6),
+            'send_timeout': int(1e6),
+        }
+        for server in cache._CACHE_SERVERS:
+            try:
+                if server.startswith('udp:'):
+                    # libmemcached doesn't support UDP get/fetch operations
+                    continue
+                mc = pylibmc.Client([server], behaviors=behaviors)
+                # it's ok that not all caches are mcrouter, we'll just ignore
+                # the miss either way
+                mc.get('__mcrouter__.version')
+                results[server] = 'OK'
+            except pylibmc.Error as e:
+                g.log.warning('Health check for %s FAILED: %s', server, e)
+                results[server] = 'FAILED %s' % e
+        return json.dumps(results)
