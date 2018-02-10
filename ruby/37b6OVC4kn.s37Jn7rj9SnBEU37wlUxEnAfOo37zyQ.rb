eb1aee5cfc48768630237cd4fbaa9445e659ff93
SUNsung <weatherius@gmail.com>
2018-02-10 07:33:09 +0200

    Rewrite [37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb] File in: 2018-02-10 07:33:09

--- ruby/37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb
+++ ruby/37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb
@@ -1,181 +1,191 @@
 
         
-                subscription_klass = id_options[:channel].safe_constantize
-    
-    require 'abstract_unit'
-    
-                  when_connected do
-                send_command('unsubscribe')
-                @raw_client = nil
-              end
-            end
-    
-          def on_error(message) # :nodoc:
-        # log errors to make diagnosing socket errors easier
-        logger.error 'WebSocket error occurred: #{message}'
-      end
-    
-      def call_controller(klass, action)
-    klass.action(action).call(@request.env)
+              key = Spaceship::Portal::Key.create(name: 'New Key', apns: true, device_check: true, music_id: 'some-music-id')
+      expect(key).to be_instance_of(Spaceship::Portal::Key)
+      expect(key.id).to eq('a-new-key-id')
+    end
   end
     
-      s.author   = 'David Heinemeier Hansson'
-  s.email    = 'david@loudthinking.com'
-  s.homepage = 'http://rubyonrails.org'
+        def self.git_log_between(pretty_format, from, to, merge_commit_filtering, date_format = nil, ancestry_path)
+      command = ['git log']
+      command << '--pretty=\'#{pretty_format}\''
+      command << '--date=\'#{date_format}\'' if date_format
+      command << '--ancestry-path' if ancestry_path
+      command << '#{from.shellescape}...#{to.shellescape}'
+      command << git_log_merge_commit_filtering_option(merge_commit_filtering)
+      Actions.sh(command.compact.join(' '), log: false).chomp
+    rescue
+      nil
+    end
     
-        private
-      # 'Deserialize' the mailer class name by hand in case another argument
-      # (like a Global ID reference) raised DeserializationError.
-      def mailer_class
-        if mailer = Array(@serialized_arguments).first || Array(arguments).first
-          mailer.constantize
+            commands << Fastlane::Actions.sh(command, log: false)
+    
+          def merge!(other)
+        other.each do |key, value|
+          self[convert_key(key)] = value
         end
+        self
       end
     
-        included do
-      # Do not make this inheritable, because we always want it to propagate
-      cattr_accessor :raise_delivery_errors, default: true
-      cattr_accessor :perform_deliveries, default: true
-      cattr_accessor :deliver_later_queue_name, default: :mailers
-    
-    CONTENT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
-    
-    def test_dir(*subdirs)
-  File.join(TEST_DIR, *subdirs)
-end
-    
-      File.write(Paths.status_file, p.exitstatus)
-  File.open(Paths.output_file, 'wb') do |f|
-    f.print '$ '
-    f.puts args.join(' ')
-    f.puts output
-    f.puts 'EXIT STATUS: #{p.exitstatus}'
-  end
-    
-              # If it's a directory they want to exclude, AKA
-          # ends with a '/' then we will go on to check and
-          # see if the entry falls within that path and
-          # exclude it if that's the case.
-    
-        def place_in_layout?
-      false
-    end
-    
-      def external_commands
-    paths.reduce([]) do |cmds, path|
-      Dir['#{path}/brew-*'].each do |file|
-        next unless File.executable?(file)
-        cmd = File.basename(file, '.rb')[5..-1]
-        cmds << cmd unless cmd.include?('.')
-      end
-      cmds
-    end.sort
-  end
-    
-      def describe_perl
-    describe_path(which 'perl')
-  end
-    
-      UNBREWED_EXCLUDE_FILES = %w[.DS_Store]
-  UNBREWED_EXCLUDE_PATHS = %w[
-    .github/*
-    bin/brew
-    lib/gdk-pixbuf-2.0/*
-    lib/gio/*
-    lib/node_modules/*
-    lib/python[23].[0-9]/*
-    lib/pypy/*
-    lib/pypy3/*
-    lib/ruby/gems/[12].*
-    lib/ruby/site_ruby/[12].*
-    lib/ruby/vendor_ruby/[12].*
-    share/pypy/*
-    share/pypy3/*
-    share/doc/homebrew/*
-    share/info/dir
-    share/man/man1/brew.1
-    share/man/whatis
-  ]
-    
-        updated_taps = []
-    Tap.each do |tap|
-      next unless tap.git?
-      begin
-        reporter = Reporter.new(tap)
-      rescue Reporter::ReporterRevisionUnsetError => e
-        onoe e if ARGV.homebrew_developer?
-        next
-      end
-      if reporter.updated?
-        updated_taps << tap.name
-        hub.add(reporter)
-      end
-    end
-    
-      DATA = :DATA
-    
-      def dry_run!(event = nil)
-    @dry_run = true
-    
-        gem_dependency_check { defined?(EvernoteOAuth) && Devise.omniauth_providers.include?(:evernote) }
-  end
-    
-        def every(*args, &blk)
-      schedule(:every, args, &blk)
-    end
-    
-        if params[:sort].present?
-      attribute, direction = params[:sort].downcase.split('.')
-      unless valid_sorts.include?(attribute)
-        attribute, direction = default.to_a.first
-      end
-    else
-      attribute, direction = default.to_a.first
-    end
-    
-    map = {}
-dups = []
-    
-    require 'cocoapods'
-    
-        # From asking people, it seems MacPorts does not have a `prefix` command, like
-    # Homebrew does, so make an educated guess:
-    if port_prefix = prefix_from_bin('port')
-      prefixes << port_prefix
-    end
-    
-            # Prints the list of specs & pod cache dirs for a single pod name.
-        #
-        # This output is valid YAML so it can be parsed with 3rd party tools
-        #
-        # @param [Array<Hash>] cache_descriptors
-        #        The various infos about a pod cache. Keys are
-        #        :spec_file, :version, :release and :slug
-        #
-        def print_pod_cache_infos(pod_name, cache_descriptors)
-          UI.puts '#{pod_name}:'
-          cache_descriptors.each do |desc|
-            if @short_output
-              [:spec_file, :slug].each { |k| desc[k] = desc[k].relative_path_from(@cache.root) }
+                  # An IO::WaitReadable means there may be more IO but this
+              # IO object is not ready to be read from yet. No problem,
+              # we read as much as we can, so we break.
+              breakable = true
+            elsif e.is_a?(Errno::EAGAIN)
+              # Otherwise, we just look for the EAGAIN error which should be
+              # all that IO::WaitReadable does in Ruby 1.9.
+              breakable = true
             end
-            UI.puts('  - Version: #{desc[:version]}')
-            UI.puts('    Type:    #{pod_type(desc)}')
-            UI.puts('    Spec:    #{desc[:spec_file]}')
-            UI.puts('    Pod:     #{desc[:slug]}')
+    
+        if rbenv_prefix = prefix_from_bin('rbenv')
+      prefixes << rbenv_prefix
+    end
+    
+          def markdown_podfile
+        UI::ErrorReport.markdown_podfile
+      end
+    
+            tests.each do |test|
+          target_module << '\n  target '#{test.name.gsub(/'/, '\\\\\'')}' do\n'
+          target_module << '    inherit! :search_paths\n'
+          target_module << template_contents(config.default_test_podfile_path, '    ', 'Pods for testing')
+          target_module << '\n  end\n'
+        end
+    
+            def execute_repl_command(repl_command)
+          unless repl_command == '\n'
+            repl_commands = repl_command.split
+            subcommand = repl_commands.shift.capitalize
+            arguments = repl_commands
+            subcommand_class = Pod::Command::IPC.const_get(subcommand)
+            subcommand_class.new(CLAide::ARGV.new(arguments)).run
+            signal_end_of_output
           end
         end
       end
     end
   end
 end
+
+    
+    @@ layout
+<html>
+  <head>
+    <title>Super Simple Chat with Sinatra</title>
+    <meta charset='utf-8' />
+    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
+  </head>
+  <body><%= yield %></body>
+</html>
+    
+          default_options :escape => :html,
+        :escaper => defined?(EscapeUtils) ? EscapeUtils : self
+    
+        it 'Reads referrer from Host header when Referer header is relative' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
+      expect(subject.referrer(env)).to eq('foo.com')
+    end
+    
+      it 'comparison of Accept-Language header is not case sensitive' do
+    session = {:foo => :bar}
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'A'
+    expect(session).not_to be_empty
+  end
+    
+        def render(context)
+      quote = paragraphize(super)
+      author = '<strong>#{@by.strip}</strong>' if @by
+      if @source
+        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
+        parts = []
+        url.each do |part|
+          if (parts + [part]).join('/').length < 32
+            parts << part
+          end
+        end
+        source = parts.join('/')
+        source << '/&hellip;' unless source == @source
+      end
+      if !@source.nil?
+        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
+      elsif !@title.nil?
+        cite = ' <cite>#{@title}</cite>'
+      end
+      blockquote = if @by.nil?
+        quote
+      elsif cite
+        '#{quote}<footer>#{author + cite}</footer>'
+      else
+        '#{quote}<footer>#{author}</footer>'
+      end
+      '<blockquote>#{blockquote}</blockquote>'
+    end
+    
+      if options.respond_to? 'keys'
+    options.each do |k,v|
+      unless v.nil?
+        v = v.join ',' if v.respond_to? 'join'
+        v = v.to_json if v.respond_to? 'keys'
+        output += ' data-#{k.sub'_','-'}='#{v}''
+      end
+    end
+  elsif options.respond_to? 'join'
+    output += ' data-value='#{config[key].join(',')}''
+  else
+    output += ' data-value='#{config[key]}''
+  end
+  output += '></#{tag}>'
+end
+    
+          rtn = ''
+      (context.environments.first['site'][@array_name] || []).each do |file|
+        if file !~ /^[a-zA-Z0-9_\/\.-]+$/ || file =~ /\.\// || file =~ /\/\./
+          rtn = rtn + 'Include file '#{file}' contains invalid characters or sequences'
+        end
+    
+    require 'pathname'
+require './plugins/octopress_filters'
+    
+        def render(context)
+      output = super
+      types = {
+        '.mp4' => 'type='video/mp4; codecs=\'avc1.42E01E, mp4a.40.2\''',
+        '.ogv' => 'type='video/ogg; codecs=theora, vorbis'',
+        '.webm' => 'type='video/webm; codecs=vp8, vorbis''
+      }
+      if @videos.size > 0
+        video =  '<video #{sizes} preload='metadata' controls #{poster}>'
+        @videos.each do |v|
+          video << '<source src='#{v}' #{types[File.extname(v)]}>'
+        end
+        video += '</video>'
+      else
+        'Error processing input, expected syntax: {% video url/to/video [url/to/video] [url/to/video] [width height] [url/to/poster] %}'
+      end
+    end
+    
+        def raise_if_blank_file
+      if path.blank?
+        raise Errors::NotIdentifiedByImageMagickError.new('Cannot find the geometry of a file with a blank name')
+      end
+    end
+    
+            def allowing *types
+          @allowed_types = types.flatten
+          self
+        end
+    
+        begin
+      create_scaffold(source, @target_path)
+    rescue Errno::EACCES => exception
+      report_exception('Permission denied when executing the plugin manager', exception)
+    rescue => exception
+      report_exception('Plugin creation Aborted', exception)
+    end
+  end
+    
+          def initialize(agent)
+        @agent = agent
+        logger.debug('[api-service] start') if logger.debug?
+      end
