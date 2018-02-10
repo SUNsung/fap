e992639f38fa318ec13d208b93b369927a8b5ffd
SUNsung <weatherius@gmail.com>
2018-02-10 03:40:28 +0200

    Rewrite [28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp] File in: 2018-02-10 03:40:27

--- cccccc/28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp
+++ cccccc/28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp
@@ -1,344 +1,137 @@
 
         
-        int main(int argc, char** argv) {
-  if (argc != 4) {
-    printf('This script converts the MNIST dataset to the leveldb format used\n'
-           'by caffe to train a siamese network.\n'
-           'Usage:\n'
-           '    convert_mnist_data input_image_file input_label_file '
-           'output_db_file\n'
-           'The MNIST dataset could be downloaded at\n'
-           '    http://yann.lecun.com/exdb/mnist/\n'
-           'You should gunzip them after downloading.\n');
-  } else {
-    google::InitGoogleLogging(argv[0]);
-    convert_dataset(argv[1], argv[2], argv[3]);
-  }
-  return 0;
+        bool js_cocos2dx_studio_ContourData_constructor(JSContext *cx, uint32_t argc, jsval *vp);
+void js_cocos2dx_studio_ContourData_finalize(JSContext *cx, JSObject *obj);
+void js_register_cocos2dx_studio_ContourData(JSContext *cx, JS::HandleObject global);
+void register_all_cocos2dx_studio(JSContext* cx, JS::HandleObject obj);
+bool js_cocos2dx_studio_ContourData_init(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ContourData_addVertex(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ContourData_create(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ContourData_ContourData(JSContext *cx, uint32_t argc, jsval *vp);
+    
+    
+    
+        return 0;
 }
-#else
-int main(int argc, char** argv) {
-  LOG(FATAL) << 'This example requires LevelDB; compile with USE_LEVELDB.';
-}
-#endif  // USE_LEVELDB
-
-    
-    #endif  // CAFFE_BASE_CONVOLUTION_LAYER_HPP_
-
-    
-    namespace caffe {
-    }
-    
-    
-    {  Blob<Dtype> diff_;  // cached for backward pass
-  Blob<Dtype> dist_sq_;  // cached for backward pass
-  Blob<Dtype> diff_sq_;  // tmp storage for gpu forward pass
-  Blob<Dtype> summer_vec_;  // tmp storage for gpu forward pass
-};
-    
-    #include 'caffe/layers/lrn_layer.hpp'
-    
-    #ifdef USE_CUDNN
-/**
- * @brief CuDNN acceleration of SigmoidLayer.
- */
-template <typename Dtype>
-class CuDNNSigmoidLayer : public SigmoidLayer<Dtype> {
- public:
-  explicit CuDNNSigmoidLayer(const LayerParameter& param)
-      : SigmoidLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNSigmoidLayer();
-    }
-    
-     protected:
-  /**
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the computed outputs @f$
-   *        y = \left\{
-   *        \begin{array}{lr}
-   *            x                  & \mathrm{if} \; x > 0 \\
-   *            \alpha (\exp(x)-1) & \mathrm{if} \; x \le 0
-   *        \end{array} \right.
-   *      @f$.  
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-        cv::Ptr<cv::StereoBM> sm = cv::StereoBM::create(state->numberOfDisparities,
-                                                       state->SADWindowSize);
-    sm->setPreFilterType(state->preFilterType);
-    sm->setPreFilterSize(state->preFilterSize);
-    sm->setPreFilterCap(state->preFilterCap);
-    sm->setBlockSize(state->SADWindowSize);
-    sm->setNumDisparities(state->numberOfDisparities > 0 ? state->numberOfDisparities : 64);
-    sm->setTextureThreshold(state->textureThreshold);
-    sm->setUniquenessRatio(state->uniquenessRatio);
-    sm->setSpeckleRange(state->speckleRange);
-    sm->setSpeckleWindowSize(state->speckleWindowSize);
-    sm->setDisp12MaxDiff(state->disp12MaxDiff);
-    
-    # if defined(CLZ64)
-/**
- * OC_ILOGNZ_64 - Integer binary logarithm of a non-zero 64-bit value.
- * @_v: A non-zero 64-bit value.
- * Returns floor(log2(_v))+1.
- * This is the number of bits that would be required to represent _v in two's
- *  complement notation with all of the leading zeros stripped.
- * If _v is zero, the return value is undefined; use OC_ILOG_64() instead.
- */
-#  define OC_ILOGNZ_64(_v) (CLZ64_OFFS-CLZ64(_v))
-/**
- * OC_ILOG_64 - Integer binary logarithm of a 64-bit value.
- * @_v: A 64-bit value.
- * Returns floor(log2(_v))+1, or 0 if _v==0.
- * This is the number of bits that would be required to represent _v in two's
- *  complement notation with all of the leading zeros stripped.
- */
-#  define OC_ILOG_64(_v)   (OC_ILOGNZ_64(_v)&-!!(_v))
-# else
-#  define OC_ILOGNZ_64(_v) (oc_ilog64(_v))
-#  define OC_ILOG_64(_v)   (oc_ilog64(_v))
-# endif
-    
-        60,30,500,    3,18.,  1024
-  },
-  /* 8: 2048 x 27 */
-  {
-    8,{0,1,2,2,3,3,4,4},{3,4,3,4,3},{0,1,1,2,2},{-1,0,1,2,3},
-    {{4},{5,6},{7,8},{-1,9,10,11},{-1,12,13,14}},
-    2,{0,2048,   186,46,744, 12,92,372,1500,  28,66,130, 260,520,1112,
-       6,20,36,56,  78,110,158,222,  316,440,624,  928,1300,1700},
-    
-    static const vorbis_residue_template _res_44p51_n1[]={
-  {2,0,30,  &_residue_44p_lo,
-   &_huff_book__44pn1_short,&_huff_book__44pn1_short,
-   &_resbook_44p_n1,&_resbook_44p_n1},
-    }
-    
-    static const static_bookblock _resbook_44u_n1={
-  {
-    {0},
-    {0,0,&_44un1__p1_0},
-    {0,0,&_44un1__p2_0},
-    {0,0,&_44un1__p3_0},
-    {0,0,&_44un1__p4_0},
-    {0,0,&_44un1__p5_0},
-    {&_44un1__p6_0,&_44un1__p6_1},
-    {&_44un1__p7_0,&_44un1__p7_1,&_44un1__p7_2}
-   }
-};
-static const static_bookblock _resbook_44u_0={
-  {
-    {0},
-    {0,0,&_44u0__p1_0},
-    {0,0,&_44u0__p2_0},
-    {0,0,&_44u0__p3_0},
-    {0,0,&_44u0__p4_0},
-    {0,0,&_44u0__p5_0},
-    {&_44u0__p6_0,&_44u0__p6_1},
-    {&_44u0__p7_0,&_44u0__p7_1,&_44u0__p7_2}
-   }
-};
-static const static_bookblock _resbook_44u_1={
-  {
-    {0},
-    {0,0,&_44u1__p1_0},
-    {0,0,&_44u1__p2_0},
-    {0,0,&_44u1__p3_0},
-    {0,0,&_44u1__p4_0},
-    {0,0,&_44u1__p5_0},
-    {&_44u1__p6_0,&_44u1__p6_1},
-    {&_44u1__p7_0,&_44u1__p7_1,&_44u1__p7_2}
-   }
-};
-static const static_bookblock _resbook_44u_2={
-  {
-    {0},
-    {0,0,&_44u2__p1_0},
-    {0,0,&_44u2__p2_0},
-    {0,0,&_44u2__p3_0},
-    {0,0,&_44u2__p4_0},
-    {0,0,&_44u2__p5_0},
-    {&_44u2__p6_0,&_44u2__p6_1},
-    {&_44u2__p7_0,&_44u2__p7_1,&_44u2__p7_2}
-   }
-};
-static const static_bookblock _resbook_44u_3={
-  {
-    {0},
-    {0,0,&_44u3__p1_0},
-    {0,0,&_44u3__p2_0},
-    {0,0,&_44u3__p3_0},
-    {0,0,&_44u3__p4_0},
-    {0,0,&_44u3__p5_0},
-    {&_44u3__p6_0,&_44u3__p6_1},
-    {&_44u3__p7_0,&_44u3__p7_1,&_44u3__p7_2}
-   }
-};
-static const static_bookblock _resbook_44u_4={
-  {
-    {0},
-    {0,0,&_44u4__p1_0},
-    {0,0,&_44u4__p2_0},
-    {0,0,&_44u4__p3_0},
-    {0,0,&_44u4__p4_0},
-    {0,0,&_44u4__p5_0},
-    {&_44u4__p6_0,&_44u4__p6_1},
-    {&_44u4__p7_0,&_44u4__p7_1,&_44u4__p7_2}
-   }
-};
-static const static_bookblock _resbook_44u_5={
-  {
-    {0},
-    {0,0,&_44u5__p1_0},
-    {0,0,&_44u5__p2_0},
-    {0,0,&_44u5__p3_0},
-    {0,0,&_44u5__p4_0},
-    {0,0,&_44u5__p5_0},
-    {0,0,&_44u5__p6_0},
-    {&_44u5__p7_0,&_44u5__p7_1},
-    {&_44u5__p8_0,&_44u5__p8_1},
-    {&_44u5__p9_0,&_44u5__p9_1,&_44u5__p9_2}
-   }
-};
-static const static_bookblock _resbook_44u_6={
-  {
-    {0},
-    {0,0,&_44u6__p1_0},
-    {0,0,&_44u6__p2_0},
-    {0,0,&_44u6__p3_0},
-    {0,0,&_44u6__p4_0},
-    {0,0,&_44u6__p5_0},
-    {0,0,&_44u6__p6_0},
-    {&_44u6__p7_0,&_44u6__p7_1},
-    {&_44u6__p8_0,&_44u6__p8_1},
-    {&_44u6__p9_0,&_44u6__p9_1,&_44u6__p9_2}
-   }
-};
-static const static_bookblock _resbook_44u_7={
-  {
-    {0},
-    {0,0,&_44u7__p1_0},
-    {0,0,&_44u7__p2_0},
-    {0,0,&_44u7__p3_0},
-    {0,0,&_44u7__p4_0},
-    {0,0,&_44u7__p5_0},
-    {0,0,&_44u7__p6_0},
-    {&_44u7__p7_0,&_44u7__p7_1},
-    {&_44u7__p8_0,&_44u7__p8_1},
-    {&_44u7__p9_0,&_44u7__p9_1,&_44u7__p9_2}
-   }
-};
-static const static_bookblock _resbook_44u_8={
-  {
-    {0},
-    {0,0,&_44u8_p1_0},
-    {0,0,&_44u8_p2_0},
-    {0,0,&_44u8_p3_0},
-    {0,0,&_44u8_p4_0},
-    {&_44u8_p5_0,&_44u8_p5_1},
-    {&_44u8_p6_0,&_44u8_p6_1},
-    {&_44u8_p7_0,&_44u8_p7_1},
-    {&_44u8_p8_0,&_44u8_p8_1},
-    {&_44u8_p9_0,&_44u8_p9_1,&_44u8_p9_2}
-   }
-};
-static const static_bookblock _resbook_44u_9={
-  {
-    {0},
-    {0,0,&_44u9_p1_0},
-    {0,0,&_44u9_p2_0},
-    {0,0,&_44u9_p3_0},
-    {0,0,&_44u9_p4_0},
-    {&_44u9_p5_0,&_44u9_p5_1},
-    {&_44u9_p6_0,&_44u9_p6_1},
-    {&_44u9_p7_0,&_44u9_p7_1},
-    {&_44u9_p8_0,&_44u9_p8_1},
-    {&_44u9_p9_0,&_44u9_p9_1,&_44u9_p9_2}
-   }
-};
-    
-    # undef c2l
-# define c2l(c,l)        (l =((unsigned long)(*((c)++)))    , \
-                         l|=((unsigned long)(*((c)++)))<< 8L, \
-                         l|=((unsigned long)(*((c)++)))<<16L, \
-                         l|=((unsigned long)(*((c)++)))<<24L)
-    
-    #if defined(__WATCOMC__)
-void GetTSC(unsigned long&);
-#pragma aux GetTSC = 0x0f 0x31 'mov [edi], eax' parm [edi] modify [edx eax];
-#elif defined(__GNUC__)
-inline
-void GetTSC(unsigned long& tsc)
+int lua_cocos2dx_physics_PhysicsBody_resetForces(lua_State* tolua_S)
 {
-  asm volatile('.byte 15, 49\n\t'
-	       : '=eax' (tsc)
-	       :
-	       : '%edx', '%eax');
-}
-#elif defined(_MSC_VER)
-inline
-void GetTSC(unsigned long& tsc)
-{
-  unsigned long a;
-  __asm _emit 0fh
-  __asm _emit 31h
-  __asm mov a, eax;
-  tsc=a;
-}
-#endif      
+    int argc = 0;
+    cocos2d::PhysicsBody* cobj = nullptr;
+    bool ok  = true;
     
-    	for (j=0; j<6; j++)
+    
+    {    CHECK_GL_ERROR_DEBUG();
+}
+    
+    	b2FixtureDef fd;
+	fd.shape = &circle;
+	fd.density = 20.0f;
+	fd.restitution = 0.0f;
+	
+	b2Vec2 minV = position - b2Vec2(0.3f,0.3f);
+	b2Vec2 maxV = position + b2Vec2(0.3f,0.3f);
+	
+	b2AABB aabb;
+	aabb.lowerBound = minV;
+	aabb.upperBound = maxV;
+    
+    		for (int32 i = 0; i < 3; ++i)
 		{
-		for (i=0; i<1000; i++) /**/
-			{
-			des_encrypt3(&data[0],key1,key2,key3);
-			GetTSC(s1);
-			des_encrypt3(&data[0],key1,key2,key3);
-			des_encrypt3(&data[0],key1,key2,key3);
-			des_encrypt3(&data[0],key1,key2,key3);
-			GetTSC(e1);
-			GetTSC(s2);
-			des_encrypt3(&data[0],key1,key2,key3);
-			des_encrypt3(&data[0],key1,key2,key3);
-			des_encrypt3(&data[0],key1,key2,key3);
-			des_encrypt3(&data[0],key1,key2,key3);
-			GetTSC(e2);
-			des_encrypt3(&data[0],key1,key2,key3);
-			}
+			b2CircleShape shape;
+			shape.m_radius = 0.5f;
     }
     
-    #if defined(__WATCOMC__)
-void GetTSC(unsigned long&);
-#pragma aux GetTSC = 0x0f 0x31 'mov [edi], eax' parm [edi] modify [edx eax];
-#elif defined(__GNUC__)
-inline
-void GetTSC(unsigned long& tsc)
-{
-  asm volatile('.byte 15, 49\n\t'
-	       : '=eax' (tsc)
-	       :
-	       : '%edx', '%eax');
+    
+    {  private:
+    void* object_;
+};
+    
+    #include <vector>
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+    
+    {  private:
+    JavaVM* vm_;
+    JNIEnv* env_;
+    bool we_attach_;
+    int status_;
+};
+    
+    
+    {  // Check that deleting works.
+  ASSERT_TRUE(!env_->DeleteFile(test_dir_ + '/non_existent').ok());
+  ASSERT_OK(env_->DeleteFile(test_dir_ + '/g'));
+  ASSERT_EQ(Status::NotFound(), env_->FileExists(test_dir_ + '/g'));
+  ASSERT_OK(env_->GetChildren(test_dir_, &children));
+  ASSERT_EQ(0U, children.size());
+  ASSERT_TRUE(
+      env_->GetChildren(test_dir_ + '/non_existent', &children).IsNotFound());
 }
-#elif defined(_MSC_VER)
-inline
-void GetTSC(unsigned long& tsc)
-{
-  unsigned long a;
-  __asm _emit 0fh
-  __asm _emit 31h
-  __asm mov a, eax;
-  tsc=a;
+    
+    
+/// Accessors
+    
+      // size amplification = percentage of additional size
+  if (candidate_size * 100 < ratio * earliest_file_size) {
+    ROCKS_LOG_BUFFER(
+        log_buffer,
+        '[%s] Universal: size amp not needed. newer-files-total-size %' PRIu64
+        ' earliest-file-size %' PRIu64,
+        cf_name.c_str(), candidate_size, earliest_file_size);
+    return nullptr;
+  } else {
+    ROCKS_LOG_BUFFER(
+        log_buffer,
+        '[%s] Universal: size amp needed. newer-files-total-size %' PRIu64
+        ' earliest-file-size %' PRIu64,
+        cf_name.c_str(), candidate_size, earliest_file_size);
+  }
+  assert(start_index < sorted_runs.size() - 1);
+    
+      ASSERT_GT(listener->compaction_reasons_.size(), 0);
+  for (auto compaction_reason : listener->compaction_reasons_) {
+    ASSERT_EQ(compaction_reason, CompactionReason::kUniversalSizeAmplification);
+  }
+    
+      // Link WAL files. Copy exact size of last one because it is the only one
+  // that has changes after the last flush.
+  for (size_t i = 0; s.ok() && i < wal_size; ++i) {
+    if ((live_wal_files[i]->Type() == kAliveLogFile) &&
+        (!flush_memtable ||
+         live_wal_files[i]->StartSequence() >= *sequence_number ||
+         live_wal_files[i]->LogNumber() >= min_log_num)) {
+      if (i + 1 == wal_size) {
+        s = copy_file_cb(db_options.wal_dir, live_wal_files[i]->PathName(),
+                         live_wal_files[i]->SizeFileBytes(), kLogFile);
+        break;
+      }
+      if (same_fs) {
+        // we only care about live log files
+        s = link_file_cb(db_options.wal_dir, live_wal_files[i]->PathName(),
+                         kLogFile);
+        if (s.IsNotSupported()) {
+          same_fs = false;
+          s = Status::OK();
+        }
+      }
+      if (!same_fs) {
+        s = copy_file_cb(db_options.wal_dir, live_wal_files[i]->PathName(), 0,
+                         kLogFile);
+      }
+    }
+  }
+    
+    
+    {
+    {  releaseJniEnv(attached_thread);
 }
-#endif      
+// @lint-ignore TXT4 T25377293 Grandfathered in
+}  // namespace rocksdb
+    
+      StatisticsJni::StatisticsJni(std::shared_ptr<Statistics> stats)
+      : StatisticsImpl(stats, false), m_ignore_histograms() {
+  }
