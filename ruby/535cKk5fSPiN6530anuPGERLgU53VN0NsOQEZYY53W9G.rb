ddcdbacf564c106837648ca6b46b69ccccbfaae2
SUNsung <weatherius@gmail.com>
2018-02-10 19:55:55 +0200

    Rewrite [535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb] File in: 2018-02-10 19:55:55

--- ruby/535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb
+++ ruby/535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb
@@ -1,102 +1,53 @@
 
         
-                # Helper method to get access to the class variable. This is mostly
-        # exposed for tests. This shouldn't be mucked with directly, since it's
-        # structure may change at any time.
-        def registered; @@registered; end
-      end
-    end
-  end
-end
+        File.write(config, output)
 
     
-    RSpec.describe TagsController, type: :controller do
-  render_views
-    
-      def type
-    'Emoji'
-  end
-    
-      def cc
-    ActivityPub::TagManager.instance.cc(object)
-  end
-    
-          def validate!
-        super
-        raise Informative, 'Existing Podfile found in directory' unless config.podfile_path_in_dir(Pathname.pwd).nil?
-        if @project_path
-          help! 'Xcode project at #{@project_path} does not exist' unless File.exist? @project_path
-          project_path = @project_path
-        else
-          raise Informative, 'No Xcode project found, please specify one' unless @project_paths.length > 0
-          raise Informative, 'Multiple Xcode projects found, please specify one' unless @project_paths.length == 1
-          project_path = @project_paths.first
-        end
-        @xcode_project = Xcodeproj::Project.open(project_path)
-      end
-    
-            def print_version
-          output_pipe.puts 'version: '#{Pod::VERSION}''
-        end
-    
-            # Runs the template configuration utilities.
-        #
-        # @return [void]
-        #
-        def configure_template
-          UI.section('Configuring #{@name} template.') do
-            Dir.chdir(@name) do
-              if File.exist?('configure')
-                system({ 'COCOAPODS_VERSION' => Pod::VERSION }, './configure', @name, *@additional_args)
-              else
-                UI.warn 'Template does not have a configure file.'
-              end
-            end
-          end
-        end
-    
-        def ensure_required_accessors! #:nodoc:
-      %w(file_name).each do |field|
-        unless @instance.respond_to?('#{@name_string}_#{field}') && @instance.respond_to?('#{@name_string}_#{field}=')
-          raise Paperclip::Error.new('#{@instance.class} model missing required attr_accessor for '#{@name_string}_#{field}'')
-        end
+        def no_subcommand(args)
+      unless args.empty? ||
+          args.first !~ %r(!/^--/!) || %w(--help --version).include?(args.first)
+        deprecation_message 'Jekyll now uses subcommands instead of just switches. \
+                          Run `jekyll help` to find out more.'
+        abort
       end
     end
     
-      # Defines the geometry of an image.
-  class Geometry
-    attr_accessor :height, :width, :modifier
+    The full error message from Ruby is: '#{e.message}'
     
-        def geometry_string
-      begin
-        orientation = Paperclip.options[:use_exif_orientation] ?
-          '%[exif:orientation]' : '1'
-        Paperclip.run(
-          'identify',
-          '-format '%wx%h,#{orientation}' :file', {
-            :file => '#{path}[0]'
-          }, {
-            :swallow_stderr => true
-          }
-        )
-      rescue Cocaine::ExitStatusError
-        ''
-      rescue Cocaine::CommandNotFoundError => e
-        raise_because_imagemagick_missing
-      end
-    end
-    
-            def no_error_when_valid?
-          @file = StringIO.new('.')
-          @subject.send(@attachment_name).assign(@file)
-          @subject.valid?
-          expected_message = [
-            @attachment_name.to_s.titleize,
-            I18n.t(:blank, scope: [:errors, :messages])
-          ].join(' ')
-          @subject.errors.full_messages.exclude?(expected_message)
-        end
-      end
-    end
+    desc 'Dumps output to a CSS file for testing'
+task :debug do
+  require 'sass'
+  path = Bootstrap.stylesheets_path
+  %w(bootstrap).each do |file|
+    engine = Sass::Engine.for_file('#{path}/#{file}.scss', syntax: :scss, load_paths: [path])
+    File.open('./#{file}.css', 'w') { |f| f.write(engine.render) }
   end
 end
+    
+            It is possible to specify a list of dependencies which will be used by
+        the template in the `Podfile.default` (normal targets) `Podfile.test`
+        (test targets) files which should be stored in the
+        `~/.cocoapods/templates` folder.
+      DESC
+      self.arguments = [
+        CLAide::Argument.new('XCODEPROJ', :false),
+      ]
+    
+    get '/' do
+  halt erb(:login) unless params[:user]
+  erb :chat, :locals => { :user => params[:user].gsub(/\W/, '') }
+end
+    
+          def report(env)
+        warn env, 'attack reported by #{self.class}'
+        env[options[:report_key]] = true
+      end
+    
+        it 'leaves normal params untouched' do
+      mock_app do |env|
+        request = Rack::Request.new(env)
+        [200, {'Content-Type' => 'text/plain'}, [request.params['foo']]]
+      end
+      get '/', :foo => 'bar'
+      expect(body).to eq('bar')
+    end
