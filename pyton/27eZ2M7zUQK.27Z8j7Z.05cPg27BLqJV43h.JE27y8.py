f9462dff4414ce70f3327f6b50dd2425f046b4c2
SUNsung <weatherius@gmail.com>
2018-02-10 11:29:28 +0200

    Rewrite [27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py] File in: 2018-02-10 11:29:28

--- pyton/27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py
+++ pyton/27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py
@@ -1,121 +1,282 @@
 
         
-        with io.open('update/releases.atom', 'w', encoding='utf-8') as atom_file:
-    atom_file.write(atom_template)
-
+            Uses the last ([-1]) value of other fields
+    '''
+    column_width = max(max(len(k) for k in formats) + 1, 8)
+    first_width = max(len(k) for k in metrics)
+    head_fmt = ('{:<{fw}s}' + '{:>{cw}s}' * len(formats))
+    row_fmt = ('{:<{fw}s}' + '{:>{cw}.3f}' * len(formats))
+    print(head_fmt.format('Metric', *formats,
+                          cw=column_width, fw=first_width))
+    for metric, row in zip(metrics, results[:, :, -1, -1, -1]):
+        print(row_fmt.format(metric, *row,
+                             cw=column_width, fw=first_width))
+    
+        max_it = len(samples_range) * len(features_range)
+    for n_samples in samples_range:
+        for n_features in features_range:
+            it += 1
+            print('====================')
+            print('Iteration %03d of %03d' % (it, max_it))
+            print('====================')
+            dataset_kwargs = {
+                'n_samples': n_samples,
+                'n_features': n_features,
+                'n_informative': n_features / 10,
+                'effective_rank': min(n_samples, n_features) / 10,
+                #'effective_rank': None,
+                'bias': 0.0,
+            }
+            print('n_samples: %d' % n_samples)
+            print('n_features: %d' % n_features)
+            X, y = make_regression(**dataset_kwargs)
+    
+    for i, n in enumerate(n_samples):
+    for j, p in enumerate(n_features):
+        X = np.random.normal(size=(n, p))
+        t0 = time.time()
+        ward.fit(X)
+        scikits_time[j, i] = time.time() - t0
+        t0 = time.time()
+        hierarchy.ward(X)
+        scipy_time[j, i] = time.time() - t0
+    
+    from sklearn import clone
+from sklearn.externals.six.moves import xrange
+from sklearn.random_projection import (SparseRandomProjection,
+                                       GaussianRandomProjection,
+                                       johnson_lindenstrauss_min_dim)
+    
+    import os
+import tarfile
+from contextlib import closing
+try:
+    from urllib import urlopen
+except ImportError:
+    from urllib.request import urlopen
+    
+    plt.matshow(data, cmap=plt.cm.Blues)
+plt.title('Original dataset')
+    
+        fig = plt.figure(fig_index, figsize=(10, 10))
+    ax1 = plt.subplot2grid((3, 1), (0, 0), rowspan=2)
+    ax2 = plt.subplot2grid((3, 1), (2, 0))
+    
+    Only adjusted measures can hence safely be used as a consensus index
+to evaluate the average stability of clustering algorithms for a given
+value of k on various overlapping sub-samples of the dataset.
     
     
-class TestCache(unittest.TestCase):
-    def setUp(self):
-        TEST_DIR = os.path.dirname(os.path.abspath(__file__))
-        TESTDATA_DIR = os.path.join(TEST_DIR, 'testdata')
-        _mkdir(TESTDATA_DIR)
-        self.test_dir = os.path.join(TESTDATA_DIR, 'cache_test')
-        self.tearDown()
+def upgrade():
+    try:
+        op.alter_column(
+            'clusters', 'changed_on',
+            existing_type=sa.DATETIME(),
+            nullable=True)
+        op.alter_column(
+            'clusters', 'created_on',
+            existing_type=sa.DATETIME(), nullable=True)
+        op.drop_constraint(None, 'columns', type_='foreignkey')
+        op.drop_constraint(None, 'columns', type_='foreignkey')
+        op.drop_column('columns', 'created_on')
+        op.drop_column('columns', 'created_by_fk')
+        op.drop_column('columns', 'changed_on')
+        op.drop_column('columns', 'changed_by_fk')
+        op.alter_column('css_templates', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('css_templates', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('dashboards', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('dashboards', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.create_unique_constraint(None, 'dashboards', ['slug'])
+        op.alter_column('datasources', 'changed_by_fk',
+                   existing_type=sa.INTEGER(),
+                   nullable=True)
+        op.alter_column('datasources', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('datasources', 'created_by_fk',
+                   existing_type=sa.INTEGER(),
+                   nullable=True)
+        op.alter_column('datasources', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('dbs', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('dbs', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('slices', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('slices', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('sql_metrics', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('sql_metrics', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('table_columns', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('table_columns', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('tables', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('tables', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('url', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('url', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        ### end Alembic commands ###
+    except:
+        pass
     
-        def test_proxy_http(self):
-        params = self._check_params(['primary_proxy', 'primary_server_ip'])
-        if params is None:
-            return
-        ydl = FakeYDL({
-            'proxy': params['primary_proxy']
-        })
-        self.assertEqual(
-            ydl.urlopen('http://yt-dl.org/ip').read().decode('utf-8'),
-            params['primary_server_ip'])
+    def upgrade():
+    op.add_column('dashboards', sa.Column('slug', sa.String(length=255), nullable=True))
+    try:
+        op.create_unique_constraint('idx_unique_slug', 'dashboards', ['slug'])
+    except:
+        pass
     
-            with io.open(as_file, 'r', encoding='utf-8') as as_f:
-            as_content = as_f.read()
+        try:
+        fk_columns = generic_find_constraint_name(
+            table='columns', columns={'datasource_name'},
+            referenced='datasources', db=db)
+        with op.batch_alter_table('columns') as batch_op:
+            batch_op.drop_constraint(fk_columns, type_='foreignkey')
+    except Exception as e:
+        logging.warning(str(e))
     
-                # Total failure, only occurs when url is totally wrong
-            # Won't happen in middle of valid playlist (next case)
-            if 'url' not in api_response or 'error' in api_response:
-                raise ExtractorError('Invalid url for track %d of album url %s' % (track_no, url))
-            # URL is good but song id doesn't exist - usually means end of playlist
-            elif not api_response['url']:
-                break
-            else:
-                # Pull out the album metadata and add to result (if it exists)
-                for resultkey, apikey in [('id', 'album_id'), ('title', 'album_title')]:
-                    if apikey in api_response and resultkey not in result:
-                        result[resultkey] = api_response[apikey]
-                song_id = url_basename(api_response['url']).rpartition('.')[0]
-                result['entries'].append({
-                    'id': compat_str(api_response.get('id', song_id)),
-                    'uploader': api_response.get('artist'),
-                    'title': api_response.get('title', song_id),
-                    'url': api_response['url'],
-                })
-        return result
-
+    from alembic import op
+import sqlalchemy as sa
     
-            page = self._download_json(
-            'http://vxml.56.com/json/%s/' % text_id, text_id, 'Downloading video info')
+    Revision ID: 430039611635
+Revises: d827694c7555
+Create Date: 2016-02-10 08:47:28.950891
     
+    from alembic import op
+import sqlalchemy as sa
     
-def best_server():
-    # TODO: find and use the best server
-    # teredo.remlab.net / teredo - debian.remlab.net(Germany)
-    # teredo.ngix.ne.kr(South Korea)
-    # teredo.managemydedi.com(USA, Chicago)
-    # teredo.trex.fi(Finland)
-    # win8.ipv6.microsoft.com(The Teredo server hidden in Windows RT 8.1) of which Windows 7 has no knowledge.
-    # win10.ipv6.microsoft.com
-    return 'teredo.remlab.net'
+    '''
     
-        win_version = env_info.win32_version()
-    if win_version == 10:
-        xlog.info('detected Win10, enable connect concurrency control, interval:%d', config.connect_interval)
-        return config.connect_interval
-    
-        version_string = 'Version:%d-%d; Build:%d; Platform:%d; CSD:%s; ServicePack:%d-%d; Suite:%d; ProductType:%d' %  (
-        os_version.dwMajorVersion, os_version.dwMinorVersion,
-        os_version.dwBuildNumber,
-        os_version.dwPlatformId,
-        os_version.szCSDVersion,
-        os_version.wServicePackMajor, os_version.wServicePackMinor,
-        os_version.wSuiteMask,
-        os_version.wReserved
-    )
+        def test_from_bytes(self):
+        def check(tests, byteorder, signed=False):
+            for test, expected in tests.items():
+                try:
+                    self.assertEqual(
+                        int.from_bytes(test, byteorder, signed=signed),
+                        expected)
+                except Exception as err:
+                    raise AssertionError(
+                        'failed to convert {0} with byteorder={1!r} and signed={2}'
+                        .format(test, byteorder, signed)) from err
     
     
-class MismatchedNotSetException(MismatchedSetException):
-    '''@brief Used for remote debugger deserialization'''
+print('\n# ======================================================================')
+print('#                               Factorial')
+print('# ======================================================================\n')
     
-    def __str__(self):
-        return 'MismatchedNotSetException(%r!=%r)' % (
-            self.getUnexpectedType(), self.expecting
-            )
-    __repr__ = __str__
+    class TZInfo:
+    def __init__(self, transitions, type_indices, ttis, abbrs):
+        self.transitions = transitions
+        self.type_indices = type_indices
+        self.ttis = ttis
+        self.abbrs = abbrs
     
-    This should be interpreted in this way: you have two data points in the class
-interval 1.5-2.5, three data points in the class interval 2.5-3.5, and one in
-the class interval 3.5-4.5. The median of these data points is 2.8333...
+        def seek(self, pos, whence=0):
+        '''Seek to specified position into the chunk.
+        Default position is 0 (start of chunk).
+        If the file is not seekable, this will result in an error.
+        '''
     
-        def test_secure_httponly_true_if_have_value(self):
-        # This isn't really valid, but demonstrates what the current code
-        # is expected to do in this case.
-        C = cookies.SimpleCookie()
-        C.load('eggs=scrambled; httponly=foo; secure=bar; Path=/bacon')
-        self.assertTrue(C['eggs']['httponly'])
-        self.assertTrue(C['eggs']['secure'])
-        # Here is what it actually does; don't depend on this behavior.  These
-        # checks are testing backward compatibility for issue 16611.
-        self.assertEqual(C['eggs']['httponly'], 'foo')
-        self.assertEqual(C['eggs']['secure'], 'bar')
     
-    from heapq import nlargest as _nlargest
-from collections import namedtuple as _namedtuple
+    {        if nextchar == '}':
+            break
+        elif nextchar != ',':
+            raise JSONDecodeError('Expecting ',' delimiter', s, end - 1)
+        end = _w(s, end).end()
+        nextchar = s[end:end + 1]
+        end += 1
+        if nextchar != ''':
+            raise JSONDecodeError(
+                'Expecting property name enclosed in double quotes', s, end - 1)
+    if object_pairs_hook is not None:
+        result = object_pairs_hook(pairs)
+        return result, end
+    pairs = dict(pairs)
+    if object_hook is not None:
+        pairs = object_hook(pairs)
+    return pairs, end
     
-        # override in derived classes
-    def _extractrgb(self, mo):
-        return [int(x) for x in mo.group('red', 'green', 'blue')]
+    The get_colordb() function will try to examine the file to figure out what the
+format of the file is.  If it can't figure out the file format, or it has
+trouble reading the file, None is returned.  You can pass get_colordb() an
+optional filetype argument.
     
-        def test_oldargs1_0(self):
-        msg = r'count\(\) takes exactly one argument \(0 given\)'
-        self.assertRaisesRegex(TypeError, msg, [].count)
+        if allow_dotted_names:
+        attrs = attr.split('.')
+    else:
+        attrs = [attr]
     
-    This module can be used to create simple XML-RPC servers
-by creating a server and either installing functions, a
-class instance, or by extending the SimpleXMLRPCServer
-class.
+        There will be a subclass for every refined PyObject type that we care
+    about.
+    
+                if srname:
+                c.site = Subreddit._by_name(srname)
+    
+    import json
+import os
+    
+        def tearDown(self):
+        self.out.close()
+        sys.stdout = self.saved_stdout
+    
+        def test_display_current_time_at_current_time(self):
+        '''
+        Just as justification for working example. (Will always pass.)
+        '''
+        production_code_time_provider = ProductionCodeTimeProvider()
+        class_under_test = TimeDisplay(production_code_time_provider)
+        current_time = datetime.datetime.now()
+        expected_time = '<span class=\'tinyBoldText\'>{}:{}</span>'.format(current_time.hour, current_time.minute)
+        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
+    
+        def test_display_current_time_at_midnight(self):
+        class_under_test = TimeDisplay()
+        expected_time = '24:01'
+        result = class_under_test.get_current_time_as_as_html_fragment()
+        self.assertEqual(result, expected_time)
+'''
+    
+        if update:
+        time.sleep(1)
+        pull_screenshot()
+        im.set_array(update_data())
+        update = False
+    return im,
+    
+    
+def main():
+    while True:
+        pull_screenshot()
+        im = Image.open('./1.png')
+    
+    
+c = wda.Client()
+s = c.session()
