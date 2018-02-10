7ed0a30691797499beb28e9acbc20cb559e8b57f
SUNsung <weatherius@gmail.com>
2018-02-10 12:32:22 +0200

    Rewrite [06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py] File in: 2018-02-10 12:32:21

--- pyton/06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py
+++ pyton/06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py
@@ -1,105 +1,225 @@
 
         
-        
-@pytest.mark.functional
-def test_without_confirmation(proc, TIMEOUT):
-    without_confirmation(proc, TIMEOUT)
-    
-    
-@pytest.fixture
-def client(app):
-    return app.test_client()
-    
-        A simple application that shows how Flask and jQuery get along.
-    
-    
-def import_state_tuples(state_tuples, name, num_replicas):
-  restored = []
-  for i in range(len(state_tuples) * num_replicas):
-    c = tf.get_collection_ref(name)[2 * i + 0]
-    h = tf.get_collection_ref(name)[2 * i + 1]
-    restored.append(tf.contrib.rnn.LSTMStateTuple(c, h))
-  return tuple(restored)
-    
-    FLAGS = flags.FLAGS
-    
-    See the following papers for more information on neural translation models.
- * http://arxiv.org/abs/1409.3215
- * http://arxiv.org/abs/1409.0473
- * http://arxiv.org/abs/1412.2007
-'''
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-    
-      def make_batch(self, batch_size):
-    '''Read the images and labels from 'filenames'.'''
-    filenames = self.get_filenames()
-    # Repeat infinitely.
-    dataset = tf.contrib.data.TFRecordDataset(filenames).repeat()
-    
-    if os.path.isfile(log_file):
-    os.remove(log_file)
-    
-        def log(self, level, fmt, *args, **kwargs):
-        string = '%s - [%s] %s\n' % (time.ctime()[4:-5], level, fmt % args)
-        #print string
-        #sys.stderr.write(string)
-        self.log_fd.write(string)
-        try:
-            self.log_fd.flush()
-        except:
-            pass
-    
-    \note Please be warned that the line numbers in the API documentation do not
-match the real locations in the source code of the package. This is an
-unintended artifact of doxygen, which I could only convince to use the
-correct module names by concatenating all files from the package into a single
-module file...
-    
-    
-    def alreadyParsedRule(self, input, ruleIndex):
         '''
-        Has this rule already parsed input at the current index in the
-        input stream?  Return the stop token index or MEMO_RULE_UNKNOWN.
-        If we attempted but failed to parse properly before, return
-        MEMO_RULE_FAILED.
+    The approach taken is explained below. I decided to do it simply.
+    Initially I was considering parsing the data into some sort of
+    structure and then generating an appropriate README. I am still
+    considering doing it - but for now this should work. The only issue
+    I see is that it only sorts the entries at the lowest level, and that
+    the order of the top-level contents do not match the order of the actual
+    entries.
     
-            raise NotImplementedError
+    filenames = {
+    'bin': 'youtube-dl',
+    'exe': 'youtube-dl.exe',
+    'tar': 'youtube-dl-%s.tar.gz' % version}
+build_dir = os.path.join('..', '..', 'build', version)
+for key, filename in filenames.items():
+    url = 'https://yt-dl.org/downloads/%s/%s' % (version, filename)
+    fn = os.path.join(build_dir, filename)
+    with open(fn, 'rb') as f:
+        data = f.read()
+    if not data:
+        raise ValueError('File %s is empty!' % fn)
+    sha256sum = hashlib.sha256(data).hexdigest()
+    new_version[key] = (url, sha256sum)
     
-    print 'PLUGIN_I18N_PATHS := ' + ','.join(os.path.relpath(plugin.path)
-                                         for plugin in plugins
-                                         if plugin.needs_translation)
+    import rsa
+import json
+from binascii import hexlify
     
-        @require_oauth2_scope('creddits')
-    @validate(
-        VUser(),
-        target=VByName('fullname'),
-    )
-    @api_doc(
-        api_section.gold,
-        uri='/api/v1/gold/gild/{fullname}',
-    )
-    def POST_gild(self, target):
-        if not isinstance(target, (Comment, Link)):
-            err = RedditError('NO_THING_ID')
-            self.on_validation_error(err)
+        entry = entry_template.replace('@TIMESTAMP@', timestamp)
+    entry = entry.replace('@VERSION@', v)
+    entries.append(entry)
     
-    from r2.controllers.reddit_base import RedditController
-from r2.lib.base import proxyurl
-from r2.lib.csrf import csrf_exempt
-from r2.lib.template_helpers import get_domain
-from r2.lib.pages import Embed, BoringPage, HelpPage
-from r2.lib.filters import websafe, SC_OFF, SC_ON
-from r2.lib.memoize import memoize
+    if isinstance(helptext, bytes):
+    helptext = helptext.decode('utf-8')
     
     
-def updatefig(*args):
-    global update
+def _mkdir(d):
+    if not os.path.exists(d):
+        os.mkdir(d)
     
-        for i in range(int(h / 3), int(h * 2 / 3)):
-        last_pixel = im_pixel[0, i]
-        if board_x or board_y:
-            break
-        board_x_sum = 0
-        board_x_c = 0
+            self.server_process.terminate()
+        self.server_process.communicate()
+    
+    # Allow direct execution
+import os
+import sys
+import unittest
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    
+    
+class AudiMediaIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)?audi-mediacenter\.com/(?:en|de)/audimediatv/(?P<id>[^/?#]+)'
+    _TEST = {
+        'url': 'https://www.audi-mediacenter.com/en/audimediatv/60-seconds-of-audi-sport-104-2015-wec-bahrain-rookie-test-1467',
+        'md5': '79a8b71c46d49042609795ab59779b66',
+        'info_dict': {
+            'id': '1565',
+            'ext': 'mp4',
+            'title': '60 Seconds of Audi Sport 104/2015 - WEC Bahrain, Rookie Test',
+            'description': 'md5:60e5d30a78ced725f7b8d34370762941',
+            'upload_date': '20151124',
+            'timestamp': 1448354940,
+            'duration': 74022,
+            'view_count': int,
+        }
+    }
+    # extracted from https://audimedia.tv/assets/embed/embedded-player.js (dataSourceAuthToken)
+    _AUTH_TOKEN = 'e25b42847dba18c6c8816d5d8ce94c326e06823ebf0859ed164b3ba169be97f2'
+    
+    '''Tests for word2vec module.'''
+    
+    filegroup(
+    name = 'all_files',
+    srcs = glob(
+        ['**/*'],
+        exclude = [
+            '**/METADATA',
+            '**/OWNERS',
+        ],
+    ),
+    visibility = ['//tensorflow:__subpackages__'],
+)
+
+    
+    filegroup(
+    name = 'all_files',
+    srcs = glob(
+        ['**/*'],
+        exclude = [
+            '**/METADATA',
+            '**/OWNERS',
+        ],
+    ),
+    visibility = ['//tensorflow:__subpackages__'],
+)
+
+    
+    py_library(
+    name = 'seq2seq_model',
+    srcs = [
+        'seq2seq_model.py',
+    ],
+    srcs_version = 'PY2AND3',
+    deps = [
+        ':data_utils',
+        '//tensorflow:tensorflow_py',
+    ],
+)
+    
+      # We will replicate the model structure for the training subgraph, as well
+  # as the evaluation subgraphs, while sharing the trainable parameters.
+  def model(data, train=False):
+    '''The Model definition.'''
+    # 2D convolution, with 'SAME' padding (i.e. the output feature map has
+    # the same size as the input). Note that {strides} is a 4D array whose
+    # shape matches the data layout: [image index, y, x, depth].
+    conv = tf.nn.conv2d(data,
+                        conv1_weights,
+                        strides=[1, 1, 1, 1],
+                        padding='SAME')
+    # Bias and rectified linear non-linearity.
+    relu = tf.nn.relu(tf.nn.bias_add(conv, conv1_biases))
+    # Max pooling. The kernel size spec {ksize} also follows the layout of
+    # the data. Here we have a pooling window of 2, and a stride of 2.
+    pool = tf.nn.max_pool(relu,
+                          ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1],
+                          padding='SAME')
+    conv = tf.nn.conv2d(pool,
+                        conv2_weights,
+                        strides=[1, 1, 1, 1],
+                        padding='SAME')
+    relu = tf.nn.relu(tf.nn.bias_add(conv, conv2_biases))
+    pool = tf.nn.max_pool(relu,
+                          ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1],
+                          padding='SAME')
+    # Reshape the feature map cuboid into a 2D matrix to feed it to the
+    # fully connected layers.
+    pool_shape = pool.get_shape().as_list()
+    reshape = tf.reshape(
+        pool,
+        [pool_shape[0], pool_shape[1] * pool_shape[2] * pool_shape[3]])
+    # Fully connected layer. Note that the '+' operation automatically
+    # broadcasts the biases.
+    hidden = tf.nn.relu(tf.matmul(reshape, fc1_weights) + fc1_biases)
+    # Add a 50% dropout during training only. Dropout also scales
+    # activations such that no rescaling is needed at evaluation time.
+    if train:
+      hidden = tf.nn.dropout(hidden, 0.5, seed=SEED)
+    return tf.matmul(hidden, fc2_weights) + fc2_biases
+    
+        return image, label
+    
+            start = datetime.now()
+        ols = linear_model.LinearRegression()
+        ols.fit(X, Y)
+        time_ols[i] = total_seconds(datetime.now() - start)
+    
+        ###########################################################################
+    # Perform benchmark
+    ###########################################################################
+    time = {}
+    n_samples = np.linspace(start=0, stop=opts.n_population,
+        num=opts.n_steps).astype(np.int)
+    
+    html_folder = u'html'
+text_folder = u'paragraphs'
+short_text_folder = u'short_paragraphs'
+n_words_per_short_text = 5
+    
+    try:
+    from urllib import urlopen
+except ImportError:
+    from urllib.request import urlopen
+    
+        for line in input_file:
+        linestrip = line.strip()
+        if len(linestrip) == 0:
+            in_exercise_region = False
+        elif linestrip.startswith('# TASK:'):
+            in_exercise_region = True
+    
+    The base classifier is a random forest classifier with 25 base estimators
+(trees). If this classifier is trained on all 800 training datapoints, it is
+overly confident in its predictions and thus incurs a large log-loss.
+Calibrating an identical classifier, which was trained on 600 datapoints, with
+method='sigmoid' on the remaining 200 datapoints reduces the confidence of the
+predictions, i.e., moves the probability vectors from the edges of the simplex
+towards the center. This calibration results in a lower log-loss. Note that an
+alternative would have been to increase the number of base estimators which
+would have resulted in a similar decrease in log-loss.
+'''
+print(__doc__)
+    
+    
+def main():
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([
+        (r'/', MainHandler),
+    ])
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+    
+    try:
+    xrange
+except NameError:
+    xrange = range
+    
+    
+class PostModule(tornado.web.UIModule):
+    def render(self, post):
+        return self.render_string('modules/post.html', post=post)
+    
+            The user's response to the CAPTCHA should be sent as `captcha`
+        along with your request.
+    
+        def send503(self):
+        retry_after = request.environ.get('retry_after')
+        if retry_after:
+            response.headers['Retry-After'] = str(retry_after)
+        return request.environ['usable_error_content']
