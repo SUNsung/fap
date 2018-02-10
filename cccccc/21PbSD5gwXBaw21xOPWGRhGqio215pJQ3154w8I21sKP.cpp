8b55cf3df26d8a94265ae89b4f82e2dc6ecc8ed6
SUNsung <weatherius@gmail.com>
2018-02-10 05:25:03 +0200

    Rewrite [21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp] File in: 2018-02-10 05:25:02

--- cccccc/21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp
+++ cccccc/21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp
@@ -1,119 +1,128 @@
 
         
-        class AtomMainDelegate : public brightray::MainDelegate {
- public:
-  AtomMainDelegate();
-  ~AtomMainDelegate();
+          // The resource is deleted from the resource manager only when it is private
+  // to kernel. Ideally the resource should be deleted when it is no longer held
+  // by anyone, but it would break backward compatibility.
+  ~ResourceOpKernel() override {
+    if (resource_ != nullptr) {
+      resource_->Unref();
+      if (cinfo_.resource_is_private_to_kernel()) {
+        if (!cinfo_.resource_manager()
+                 ->template Delete<T>(cinfo_.container(), cinfo_.name())
+                 .ok()) {
+          // Do nothing; the resource can have been deleted by session resets.
+        }
+      }
     }
-    
-      // DesktopMediaListObserver overrides.
-  void OnSourceAdded(int index) override;
-  void OnSourceRemoved(int index) override;
-  void OnSourceMoved(int old_index, int new_index) override;
-  void OnSourceNameChanged(int index) override;
-  void OnSourceThumbnailChanged(int index) override;
-  bool OnRefreshFinished() override;
-    
-    void GlobalShortcut::Unregister(const ui::Accelerator& accelerator) {
-  if (!ContainsKey(accelerator_callback_map_, accelerator))
-    return;
-    }
-    
-    class GlobalShortcut : public extensions::GlobalShortcutListener::Observer,
-                       public mate::TrackableObject<GlobalShortcut> {
- public:
-  static mate::Handle<GlobalShortcut> Create(v8::Isolate* isolate);
-    }
-    
-    class Event : public Wrappable<Event>,
-              public content::WebContentsObserver {
- public:
-  static Handle<Event> Create(v8::Isolate* isolate);
-    }
-    
-    SavePageHandler::SavePageHandler(content::WebContents* web_contents,
-                                 const SavePageCallback& callback)
-    : web_contents_(web_contents),
-      callback_(callback) {
-}
-    
-      AtomQuotaPermissionContext();
-  virtual ~AtomQuotaPermissionContext();
-    
-    #include 'atom/browser/net/js_asker.h'
-#include 'net/url_request/url_request_simple_job.h'
-    
-    #include <vector>
-#include <string>
-    
-    void WakeUpLock::Unlock() {
-    ::wakeupLock_Unlock(object_);
-}
-    
-    void Packer_Pack(const char* _url, unsigned int _sequence, const void* _data, size_t _datalen, AutoBuffer& _outbuf, bool _dohash) {
-    ASSERT(_url);
-    size_t url_size = strnlen(_url, 128);
-    ASSERT(0xFF >= sizeof(LongLinkPack) + url_size);
-    }
-    
-    //
-//  testspy.h
-//  PublicComponent
-//
-//  Created by yerungui on 14-5-13.
-//
-    
-    #include <jni.h>
-#include <string>
-struct JniMethodInfo;
-class AutoBuffer;
-    
-    #include <folly/synchronization/ParkingLot.h>
-    
-      T exchange(T v, std::memory_order mo = std::memory_order_seq_cst) noexcept {
-    DeterministicSchedule::beforeSharedAccess();
-    T rv = data.exchange(v, mo);
-    FOLLY_TEST_DSCHED_VLOG(this << '.exchange(' << std::hex << v << ') -> '
-                                << std::hex << rv);
-    DeterministicSchedule::afterSharedAccess(true);
-    return rv;
   }
     
-    /**
- * For all other iterators, we need to use the decrement operator.
- */
-template <class Iter>
-typename std::enable_if<
-    !std::is_same<
-        typename std::iterator_traits<Iter>::iterator_category,
-        std::random_access_iterator_tag>::value,
-    typename std::iterator_traits<Iter>::reference>::type
-value_before(Iter i) {
-  return *--i;
-}
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    TEST(CoreAllocator, Basic) {
-  CoreAllocator<32> alloc;
-  auto a = alloc.get(0);
-  auto res = a->allocate(8);
-  memset(res, 0, 8);
-  a->deallocate(res);
-  res = a->allocate(8);
-  EXPECT_TRUE((intptr_t)res % 8 == 0); // check alignment
-  memset(res, 0, 8);
-  a->deallocate(res);
-  res = a->allocate(12);
-  EXPECT_TRUE((intptr_t)res % 16 == 0); // check alignment
-  memset(res, 0, 12);
-  a->deallocate(res);
-  res = a->allocate(257);
-  memset(res, 0, 257);
-  a->deallocate(res);
+    // Estimate the cost of running a Grappler item by actually running the
+// corresponding TensorFlow graph on the specified cluster and measuring the
+// runtimes.
+class MeasuringCostEstimator : public CostEstimator {
+ public:
+  // Run the model for measurement_steps to measure its average cost.
+  // When measurement_threads is greater than 0, use a threadpool of as many
+  // threads to run the measurements; otherwise, run them serially. Does not
+  // take ownership of cluster.
+  explicit MeasuringCostEstimator(Cluster* cluster, int measurement_steps,
+                                  int measurement_threads);
+  ~MeasuringCostEstimator() override {}
     }
     
-    #include <folly/container/SparseByteSet.h>
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-      if (!kIsSanitize) { // asan allocator raises SIGABRT instead
-    EXPECT_EQ(EINVAL, (trial(2), errno)) << 'too small';
-    EXPECT_EQ(EINVAL, (trial(513), errno)) << 'not power of two';
-  }
+    #endif  // TENSORFLOW_KERNELS_SPARSE_TENSOR_DENSE_ADD_OP_H_
+
+    
+    #include 'tensorflow/stream_executor/lib/statusor.h'
+#include 'tensorflow/stream_executor/platform/port.h'
+    
+    // A PluginConfig describes the set of plugins to be used by a StreamExecutor
+// instance. Each plugin is defined by an arbitrary identifier, usually best set
+// to the address static member in the implementation (to avoid conflicts).
+//
+// A PluginConfig may be passed to the StreamExecutor constructor - the plugins
+// described therein will be used to provide BLAS, DNN, FFT, and RNG
+// functionality. Platform-appropriate defaults will be used for any un-set
+// libraries. If a platform does not support a specified plugin (ex. cuBLAS on
+// an OpenCL executor), then an error will be logged and no plugin operations
+// will succeed.
+//
+// The StreamExecutor BUILD target does not link ANY plugin libraries - even
+// common host fallbacks! Any plugins must be explicitly linked by dependent
+// targets. See the cuda, opencl and host BUILD files for implemented plugin
+// support (search for 'plugin').
+class PluginConfig {
+ public:
+  // Value specifying the platform's default option for that plugin.
+  static const PluginId kDefault;
+    }
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+     protected:
+  /// @copydoc AbsValLayer
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    
+    #endif  // CAFFE_ARGMAX_LAYER_HPP_
+
+    
+    
+    {}  // namespace caffe
+    
+    #endif  // CAFFE_BNLL_LAYER_HPP_
+
+    
+    
+    { protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual inline bool reverse_dimensions() { return false; }
+  virtual void compute_output_shape();
+};
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    
+    {  int M_;
+  int K_;
+  int N_;
+  bool bias_term_;
+  Blob<Dtype> bias_multiplier_;
+};
