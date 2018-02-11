b65004044aebc5b9baf2f5c61798fff364600d6f
SUNsung <weatherius@gmail.com>
2018-02-11 11:55:50 +0200

    Rewrite [31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py] File in: 2018-02-11 11:55:50

--- pyton/31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py
+++ pyton/31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py
@@ -1,33 +1,73 @@
 
         
-        
-with open(BIN_FILE_PATH, 'rb') as f:
-    BIN_FILE_CONTENT = f.read()
+          Raises:
+    tf.errors.InvalidArgumentError: if batch_size or num_steps are too high.
+  '''
+  with tf.name_scope(name, 'PTBProducer', [raw_data, batch_size, num_steps]):
+    raw_data = tf.convert_to_tensor(raw_data, name='raw_data', dtype=tf.int32)
     
     
-def test_unicode_digest_auth(httpbin):
-    # it doesn't really authenticate us because httpbin
-    # doesn't interpret the utf8-encoded auth
-    http('--auth-type=digest',
-         '--auth', u'test:%s' % UNICODE,
-         httpbin.url + u'/digest-auth/auth/test/' + UNICODE)
+class CIFAR10InputTest(tf.test.TestCase):
+    
+    import iris_data
+    
+      # Small utility function to evaluate a dataset by feeding batches of data to
+  # {eval_data} and pulling the results from {eval_predictions}.
+  # Saves memory and enables this to run on smaller GPUs.
+  def eval_in_batches(data, sess):
+    '''Get all predictions for a dataset by running it in small batches.'''
+    size = data.shape[0]
+    if size < EVAL_BATCH_SIZE:
+      raise ValueError('batch size for evals larger than dataset: %d' % size)
+    predictions = numpy.ndarray(shape=(size, NUM_LABELS), dtype=numpy.float32)
+    for begin in xrange(0, size, EVAL_BATCH_SIZE):
+      end = begin + EVAL_BATCH_SIZE
+      if end <= size:
+        predictions[begin:end, :] = sess.run(
+            eval_prediction,
+            feed_dict={eval_data: data[begin:end, ...]})
+      else:
+        batch_predictions = sess.run(
+            eval_prediction,
+            feed_dict={eval_data: data[-EVAL_BATCH_SIZE:, ...]})
+        predictions[begin:, :] = batch_predictions[begin - size:, :]
+    return predictions
+    
+    Forward-backward pass:
+Run on Tesla K40c: 480 +/- 48 ms / batch
+Run on Titan X:    244 +/- 30 ms / batch
+'''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+    
+                # is there a channel override for token type?
+            try:
+                overrideChannel = self.channelOverrideMap[t.type]
+                
+            except KeyError:
+                # no override for this type
+                pass
+            
+            else:
+                if overrideChannel == self.channel:
+                    t.channel = overrideChannel
+                else:
+                    discard = True
+            
+            if not discard:
+                t.index = index
+                self.tokens.append(t)
+                index += 1
+    
+    from alembic import op
+import sqlalchemy as sa
+    
+    
+def downgrade():
+    op.drop_table('logs')
 
     
-    
-def parse_items(items,
-                headers_class=CaseInsensitiveDict,
-                data_class=OrderedDict,
-                files_class=DataDict,
-                params_class=ParamsDict):
-    '''Parse `KeyValue` `items` into `data`, `headers`, `files`,
-    and `params`.
-    
-        check_format(sys.argv[1])
-    if len(errors) > 0:
-        for err in errors:
-            print(err)
-        sys.exit(1)
-    
-    
-if __name__ == '__main__':
-    cli()
+    Revision ID: 3c3ffe173e4f
+Revises: ad82a75afd82
+Create Date: 2016-08-18 14:06:28.784699
