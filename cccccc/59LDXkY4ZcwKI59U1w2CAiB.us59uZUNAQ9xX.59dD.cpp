ec1d988e485017a7f7b2701a87f82bee340fbbf9
SUNsung <weatherius@gmail.com>
2018-02-11 16:17:21 +0200

    Rewrite [59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp] File in: 2018-02-11 16:17:21

--- cccccc/59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp
+++ cccccc/59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp
@@ -1,287 +1,155 @@
 
         
-        #include 'brightray/common/content_client.h'
-#include 'brightray/common/main_delegate.h'
+        #include <map>
+#include <string>
     
-    #endif  // ATOM_BROWSER_API_ATOM_API_RENDER_PROCESS_PREFERENCES_H_
+    namespace api {
+    }
+    
+    
+    {}  // namespace api
+    
+    
+    {}  // namespace atom
+    
+    AsarProtocolHandler::~AsarProtocolHandler() {
+}
+    
+    #include 'net/url_request/url_request_job_factory.h'
+    
+    #include 'atom/browser/net/asar/url_request_asar_job.h'
+#include 'atom/browser/net/js_asker.h'
+    
+    namespace content {
+struct MainFunctionParams;
+}
+    
+        // enable tracing. Nodes listed here get their m_traceNodeValueXXX flags set
+    std::vector<std::wstring> m_traceNodeNamesReal;
+    std::vector<std::wstring> m_traceNodeNamesCategory;
+    std::vector<std::wstring> m_traceNodeNamesSparse;
+    
+    void GPUDataTransferer::CopyCPUToGPUAsync(void* cpuBuffer, size_t totalSize, void* gpuBuffer)
+{
+    m_inner->CopyCPUToGPUAsync(cpuBuffer, 1, totalSize, gpuBuffer);
+    m_inner->RecordCPUToGPUCopy();
+}
+    
+        // first try if a NetworkBuilder is present
+    function<ComputationNetworkPtr(DEVICEID_TYPE)> createNetworkFn;
+    bool gotIt = TryGetNetworkFactory<ConfigRecordType, ElemType>(config, createNetworkFn);
+    if (gotIt)
+    {
+        // We have several ways to create a network.
+        net = createNetworkFn(deviceId);
+        if (outputNodeNames.size() > 0)
+        {
+            net->InvalidateCompiledNetwork();
+            PatchOutputNodes(net, outputNodeNames, outputNodeNamesVector);
+            net->CompileNetwork();
+            // BUGBUG: This will generate double Validation output in the log
+        }
+    }
+    else // no NetworkBuilder given: load from 'modelPath'
+    {
+        wstring modelPath = config(L'modelPath');
+    }
+    
+    #include <memory> // for shared_ptr
+    
+    // ---------------------------------------------------------------------------
+// ConfigException -- all errors from processing the config files are reported as ConfigException
+// ---------------------------------------------------------------------------
+    
+    // Compares two ASCII strings ignoring the case.
+// TODO: Should switch to boost, boost::iequal should be used instead.
+// TODO: we already have EqualCI() in Basics.h which does the same thing.
+template<class TElement>
+inline bool AreEqualIgnoreCase(
+    const std::basic_string<TElement, char_traits<TElement>, allocator<TElement>>& s1,
+    const std::basic_string<TElement, char_traits<TElement>, allocator<TElement> >& s2)
+{
+    if (s1.size() != s2.size())
+    {
+        return false;
+    }
+    }
+    
+    #endif
 
     
-    // static
-Handle<Event> Event::Create(v8::Isolate* isolate) {
-  return mate::CreateHandle(isolate, new Event(isolate));
-}
+      {2,0,32,  &_residue_44_low,
+   &_huff_book__44c0_s_long,&_huff_book__44c0_sm_long,
+   &_resbook_44s_0,&_resbook_44sm_0}
+};
+static const vorbis_residue_template _res_44s_1[]={
+  {2,0,16,  &_residue_44_low,
+   &_huff_book__44c1_s_short,&_huff_book__44c1_sm_short,
+   &_resbook_44s_1,&_resbook_44sm_1},
     
-    bool SavePageHandler::Handle(const base::FilePath& full_path,
-                             const content::SavePageType& save_type) {
-  auto download_manager = content::BrowserContext::GetDownloadManager(
-      web_contents_->GetBrowserContext());
-  download_manager->AddObserver(this);
-  // Chromium will create a 'foo_files' directory under the directory of saving
-  // page 'foo.html' for holding other resource files of 'foo.html'.
-  base::FilePath saved_main_directory_path = full_path.DirName().Append(
-      full_path.RemoveExtension().BaseName().value() +
-      FILE_PATH_LITERAL('_files'));
-  bool result = web_contents_->SavePage(full_path,
-                                        saved_main_directory_path,
-                                        save_type);
-  download_manager->RemoveObserver(this);
-  // If initialization fails which means fail to create |DownloadItem|, we need
-  // to delete the |SavePageHandler| instance to avoid memory-leak.
-  if (!result)
-    delete this;
-  return result;
-}
+    #include <stdio.h>
+#include <stdlib.h>
+#include <openssl/cast.h>
     
-    void AtomQuotaPermissionContext::RequestQuotaPermission(
-    const content::StorageQuotaParams& params,
-    int render_process_id,
-    const PermissionCallback& callback) {
-  callback.Run(response::QUOTA_PERMISSION_RESPONSE_ALLOW);
-}
     
-    Delegate* AutoUpdater::GetDelegate() {
-  return delegate_;
-}
-    
-      // net::URLRequestJobFactory::ProtocolHandler:
-  net::URLRequestJob* MaybeCreateJob(
-      net::URLRequest* request,
-      net::NetworkDelegate* network_delegate) const override;
-  bool IsSafeRedirectTarget(const GURL& location) const override;
-    
-      // URLRequestSimpleJob:
-  int GetRefCountedData(std::string* mime_type,
-                        std::string* charset,
-                        scoped_refptr<base::RefCountedMemory>* data,
-                        const net::CompletionCallback& callback) const override;
-    
-    namespace swift {
+    {        vecdump_.push_back(strstack);
+        strstack.clear();
     }
     
-        llvm::errs() << '\n=====================================================\n';
-    SyntaxNode->dump(llvm::errs());
-    llvm::errs() << '\n\n---- Maps to semantic node: ----\n\n';
     
-    
-    {  bool checkValue(SILValue Value);
+    {  private:
+    std::vector<std::string> vecdump_;
 };
     
-      // Only allow allocation using the allocator in MarkupContext or by
-  // placement new.
-  void *operator new(size_t Bytes, swift::markup::MarkupContext &MC,
-                     unsigned Alignment = alignof(DocComment));
-  void *operator new(size_t Bytes, void *Mem) {
-    assert(Mem);
-    return Mem;
-  }
+    #endif /* WAKEUPLOCK_H_ */
+
     
-    namespace swift {
-namespace syntax {
-    }
-    }
     
-    namespace llvm {
-  template<> class PointerLikeTypeTraits<swift::ConcreteDeclRef> {
-    typedef llvm::PointerUnion<swift::ValueDecl *,
-                               swift::ConcreteDeclRef::SpecializedDeclRef *>
-      DataPointer;
-    typedef PointerLikeTypeTraits<DataPointer> DataTraits;
-    }
-    }
-    
-    namespace cv { namespace cuda { namespace device
-{
-    struct Emulation
-    {
-    }
-    }
-    }
-    }
-    
-    //! @endcond
-    
-    namespace cv {
-    }
-    
-    #define SEC_METHOD_ITERATIONS 4
-#define INITIAL_SEC_METHOD_SIGMA 0.1
-    class ConjGradSolverImpl : public ConjGradSolver
-    {
-    public:
-        Ptr<Function> getFunction() const;
-        void setFunction(const Ptr<Function>& f);
-        TermCriteria getTermCriteria() const;
-        ConjGradSolverImpl();
-        void setTermCriteria(const TermCriteria& termcrit);
-        double minimize(InputOutputArray x);
-    protected:
-        Ptr<MinProblemSolver::Function> _Function;
-        TermCriteria _termcrit;
-        Mat_<double> d,r,buf_x,r_old;
-        Mat_<double> minimizeOnTheLine_buf1,minimizeOnTheLine_buf2;
-    private:
-        static void minimizeOnTheLine(Ptr<MinProblemSolver::Function> _f,Mat_<double>& x,const Mat_<double>& d,Mat_<double>& buf1,Mat_<double>& buf2);
-    };
-    
-    Vec3i cv::cuda::DeviceInfo::maxTexture3D() const
-{
-#ifndef HAVE_CUDA
-    throw_no_cuda();
-    return Vec3i();
-#else
-    return Vec3i(deviceProps().get(device_id_)->maxTexture3D);
-#endif
+    {    __DelOlderTouchTime(now);
+    touch_times_.push_back(now);
+    return true;
 }
     
-        static void CODEGEN_FUNCPTR Switch_UniformMatrix4x3fv(GLint location, GLsizei count, GLboolean transpose, const GLfloat *value)
-    {
-        UniformMatrix4x3fv = (PFNUNIFORMMATRIX4X3FVPROC)IntGetProcAddress('glUniformMatrix4x3fv');
-        UniformMatrix4x3fv(location, count, transpose, value);
+    class ServiceBase {
+  public:
+    virtual ~ServiceBase() {}
+    void DependServices(const TServicesMap& _dependservices) { m_dependservices = _dependservices;}
+    const char* ServiceName() const { return m_servicename.c_str();}
     }
     
-            void Init(ComputationNetworkPtr& net,
-                  const std::list<ComputationNodeBasePtr>& learnableNodes,
-                  const std::vector<ComputationNodeBasePtr>& criterionNodes,
-                  const std::vector<ComputationNodeBasePtr>& evaluationNodes)
-        {
-            m_MBLayoutCache = make_shared<MBLayout>();
-            m_netCriterionAccumulator = make_shared<Matrix<ElemType>>(1, 1, net->GetDeviceId());
-            m_netEvaluationAccumulator = make_shared<Matrix<ElemType>>(1, evaluationNodes.size(), net->GetDeviceId());
-            // remember ptrs to learnable nodes
-            for (auto x : learnableNodes)
-            {
-                shared_ptr<ComputationNode<ElemType>> pLearnableNode = dynamic_pointer_cast<ComputationNode<ElemType>>(x);
-                wstring nodename = x->NodeName();
-                m_LearnableNodePtr[nodename] = pLearnableNode;
-            }
-            for (auto& x : criterionNodes)
-            {
-                m_netCriterionNodes.push_back(dynamic_pointer_cast<ComputationNode<ElemType>>(x));
-            }
-            for (auto& x : evaluationNodes)
-            {
-                m_netEvaluationNodes.push_back(dynamic_pointer_cast<ComputationNode<ElemType>>(x));
-            }
-            m_netCriterionAccumulator->SetValue((ElemType) 0);
-            m_netEvaluationAccumulator->SetValue((ElemType) 0);
-    }
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
     
-        wstring nodeName = featureNode->NodeName();
-    if (!NodeNameExists(nodeName))
-        RuntimeError('RemoveFeatureNode: feature node does not exist.');
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
     
-        // ProcessPassNDLScript - Process a pass of the NDL script
-    // script - NDL Script to process
-    // ndlPass - complete processing for this pass, all passes if ndlPassAll
-    // skipThrough - for iterative processing, skip through this node in the script (used for in-line MEL processing)
-    // fullValidate - validate as a complete network? (false if this might be a snippet of a full network)
-    // returns: last NDL node processed
-    NDLNode<ElemType>* ProcessPassNDLScript(NDLScript<ElemType>* script, NDLPass ndlPass, NDLNode<ElemType>* skipThrough = nullptr, bool fullValidate = false, const std::wstring& dumpFileName = L'')
-    {
-        if (ndlPass == ndlPassFinal)
-        {
-            // make sure to clear the caches so we pick up the new nodes
-            m_net->InvalidateCompiledNetwork();
-            // if requested then dump the nodes
-            // Note: This happens on the invalidated network.
-            if (dumpFileName != L'')
-                m_net->DumpAllNodesToFile(false, true, dumpFileName);
+    int getKb(int pid) {
+    std::string line;
+    std::ifstream self((std::string('/proc/') + std::to_string(pid) + std::string('/status')).c_str());
+    int vmRSS;
+    while(!self.eof()) {
+        std::getline(self, line, ':');
+        if (line == 'VmRSS') {
+            self >> vmRSS;
         }
-        NDLNodeEvaluatorImpl<ElemType> ndlEvaluator(m_net);
-        NDLNode<ElemType>* lastNode = script->Evaluate(ndlEvaluator, L'', ndlPass, skipThrough);
-        if (ndlPass == ndlPassResolve)
-            SetOutputNodes(script);
-        return lastNode;
+        std::getline(self, line);
     }
-    
-    template <typename ElemType>
-void DoExportToDbn(const ConfigParameters& config)
-{
-    DEVICEID_TYPE deviceID = DeviceFromConfig(config);
-    }
-    
-    // type of data in this section
-enum SectionType
-{
-    sectionTypeNull = 0,
-    sectionTypeFile = 1,          // file header
-    sectionTypeData = 2,          // data section
-    sectionTypeLabel = 3,         // label data
-    sectionTypeLabelMapping = 4,  // label mapping table (array of strings)
-    sectionTypeStats = 5,         // data statistics
-    sectionTypeCategoryLabel = 6, // labels in category format (float type, all zeros with a single 1.0 per column)
-    sectionTypeMax
-};
-    
-    template <class _T, int _N>
-class hardcoded_array
-{
-    _T data[_N];
-    inline void check_index(size_t i) const
-    {
-        i;
-        assert(i < _N);
-    }
-    inline void check_size(size_t n) const
-    {
-        n;
-        assert(n == _N);
-    }
-    }
-    
-    using namespace folly;
-    
-      testReads(
-      {{0, 5 * 1024 * 1024}, {kAlign, 5 * 1024 * 1024}}, AsyncIO::NOT_POLLABLE);
-    
-    #include <folly/portability/GTest.h>
-    
-    namespace folly {
-    }
-    
-    LogCategoryConfig::LogCategoryConfig(
-    LogLevel l,
-    bool inherit,
-    std::vector<std::string> h)
-    : level{l}, inheritParentLevel{inherit}, handlers{h} {}
-    
-      /**
-   * Block until all messages that have already been sent to this LogHandler
-   * have been processed.
-   *
-   * For LogHandlers that perform asynchronous processing of log messages,
-   * this ensures that messages already sent to this handler have finished
-   * being processed.
-   *
-   * Other threads may still call handleMessage() while flush() is running.
-   * handleMessage() calls that did not complete before the flush() call
-   * started will not necessarily be processed by the flush call.
-   */
-  virtual void flush() = 0;
-    
-      YGNodeCalculateLayout(root, 100, 100, YGDirectionLTR);
-    
-    
-    {  YGConfigFree(config);
+    return vmRSS;
 }
     
     
-    {  // There are subtle issues with calling the next functions directly. It is
-  // much better to always use a ThreadScope to manage attaching/detaching for
-  // you.
-  FBEXPORT static JNIEnv* ensureCurrentThreadIsAttached();
-  FBEXPORT static void detachCurrentThread();
-};
+    {}
+
     
-    class ProgramLocation {
-public:
-  ProgramLocation() : m_functionName('Unspecified'), m_fileName('Unspecified'), m_lineNumber(0) {}
-    }
+    struct HttpResponse;
     
-    template <typename T>
-inline bool operator==(const RefPtr<T>& ref, std::nullptr_t ptr) {
-  return ref.get() == ptr;
-}
-    
-    namespace facebook {
-    }
+    #ifdef __APPLE__
+        if (createdFd != INVALID_SOCKET) {
+            int noSigpipe = 1;
+            setsockopt(createdFd, SOL_SOCKET, SO_NOSIGPIPE, &noSigpipe, sizeof(int));
+        }
+#endif
