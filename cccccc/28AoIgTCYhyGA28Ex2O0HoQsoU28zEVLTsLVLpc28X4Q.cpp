53e6119448f18140a783ae0bcf1643bafdf250f1
SUNsung <weatherius@gmail.com>
2018-02-11 09:11:49 +0200

    Rewrite [28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp] File in: 2018-02-11 09:11:49

--- cccccc/28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp
+++ cccccc/28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp
@@ -1,137 +1,319 @@
 
         
-        bool js_cocos2dx_studio_ContourData_constructor(JSContext *cx, uint32_t argc, jsval *vp);
-void js_cocos2dx_studio_ContourData_finalize(JSContext *cx, JSObject *obj);
-void js_register_cocos2dx_studio_ContourData(JSContext *cx, JS::HandleObject global);
-void register_all_cocos2dx_studio(JSContext* cx, JS::HandleObject obj);
-bool js_cocos2dx_studio_ContourData_init(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ContourData_addVertex(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ContourData_create(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ContourData_ContourData(JSContext *cx, uint32_t argc, jsval *vp);
-    
-    
-    
-        return 0;
+        void Event::PreventDefault(v8::Isolate* isolate) {
+  GetWrapper()->Set(StringToV8(isolate, 'defaultPrevented'),
+                           v8::True(isolate));
 }
-int lua_cocos2dx_physics_PhysicsBody_resetForces(lua_State* tolua_S)
+    
+      // Pass the sender and message to be replied.
+  void SetSenderAndMessage(content::WebContents* sender, IPC::Message* message);
+    
+    // Create a pure JavaScript Event object.
+v8::Local<v8::Object> CreateEventObject(v8::Isolate* isolate) {
+  if (event_template.IsEmpty()) {
+    event_template.Reset(isolate, ObjectTemplateBuilder(isolate)
+        .SetMethod('preventDefault', &PreventDefault)
+        .Build());
+  }
+    }
+    
+    
+    {}  // namespace atom
+
+    
+    #include 'base/files/file_path.h'
+#include 'base/files/file_util.h'
+#include 'base/logging.h'
+#include 'base/values.h'
+#include 'base/command_line.h'
+#include 'content/nw/src/breakpad_linux.h'
+#include 'content/nw/src/api/api_messages.h'
+#include 'content/nw/src/api/bindings_common.h'
+#include 'content/public/renderer/render_view.h'
+#include 'content/public/renderer/render_thread.h'
+#include 'content/public/renderer/v8_value_converter.h'
+#include 'extensions/renderer/static_v8_external_one_byte_string_resource.h'
+#include 'grit/nw_resources.h'
+#include 'third_party/node/src/node.h'
+#undef CHECK
+#include 'third_party/node/src/node_internals.h'
+#include 'third_party/node/src/req_wrap.h'
+#include 'third_party/WebKit/public/web/WebSecurityPolicy.h'
+#include 'url/gurl.h'
+    
+    #include 'base/basictypes.h'
+#include 'base/id_map.h'
+#include 'base/memory/weak_ptr.h'
+#include 'content/public/browser/web_contents_observer.h'
+    
+    bool MenuDelegate::IsCommandIdChecked(int command_id) const {
+  if (command_id < 0)
+    return false;
+    }
+    
+    
+    {  // Send event.
+  base::ListValue args;
+  object_manager()->SendEvent(this, 'click', args);
+}
+    
+    bool read_unlv_file(                    //print list of sides
+                     STRING name,        //basename of file
+                     inT32 xsize,        //image size
+                     inT32 ysize,        //image size
+                     BLOCK_LIST *blocks  //output list
+                    ) {
+  FILE *pdfp;                    //file pointer
+  BLOCK *block;                  //current block
+  int x;                         //current top-down coords
+  int y;
+  int width;                     //of current block
+  int height;
+  BLOCK_IT block_it = blocks;    //block iterator
+    }
+    
+    #include 'dppoint.h'
+#include 'tprintf.h'
+    
+      // get paragraphs
+  PARA_LIST *para_list() {
+    return &paras_;
+  }
+  /// get blobs
+  C_BLOB_LIST *blob_list() {
+    return &c_blobs;
+  }
+  C_BLOB_LIST *reject_blobs() {
+    return &rej_blobs;
+  }
+  FCOORD re_rotation() const {
+    return re_rotation_;         // How to transform coords back to image.
+  }
+  void set_re_rotation(const FCOORD& rotation) {
+    re_rotation_ = rotation;
+  }
+  FCOORD classify_rotation() const {
+    return classify_rotation_;   // Apply this before classifying.
+  }
+  void set_classify_rotation(const FCOORD& rotation) {
+    classify_rotation_ = rotation;
+  }
+  FCOORD skew() const {
+    return skew_;                // Direction of true horizontal.
+  }
+  void set_skew(const FCOORD& skew) {
+    skew_ = skew;
+  }
+  const ICOORD& median_size() const {
+    return median_size_;
+  }
+  void set_median_size(int x, int y) {
+    median_size_.set_x(x);
+    median_size_.set_y(y);
+  }
+    
+    #include 'quspline.h'
+#include 'werd.h'
+    
+      /// Get the native acceptor representation.
+  /**
+   * This function may be used to obtain the underlying representation of the
+   * acceptor. This is intended to allow access to native acceptor functionality
+   * that is not otherwise provided.
+   */
+  native_handle_type native_handle()
+  {
+    return this->get_service().native_handle(this->get_implementation());
+  }
+    
+    /// Provides waitable timer functionality.
+/**
+ * The basic_waitable_timer class template provides the ability to perform a
+ * blocking or asynchronous wait for a timer to expire.
+ *
+ * A waitable timer is always in one of two states: 'expired' or 'not expired'.
+ * If the wait() or async_wait() function is called on an expired timer, the
+ * wait operation will complete immediately.
+ *
+ * Most applications will use one of the boost::asio::steady_timer,
+ * boost::asio::system_timer or boost::asio::high_resolution_timer typedefs.
+ *
+ * @note This waitable timer functionality is for use with the C++11 standard
+ * library's @c &lt;chrono&gt; facility, or with the Boost.Chrono library.
+ *
+ * @par Thread Safety
+ * @e Distinct @e objects: Safe.@n
+ * @e Shared @e objects: Unsafe.
+ *
+ * @par Examples
+ * Performing a blocking wait (C++11):
+ * @code
+ * // Construct a timer without setting an expiry time.
+ * boost::asio::steady_timer timer(io_service);
+ *
+ * // Set an expiry time relative to now.
+ * timer.expires_from_now(std::chrono::seconds(5));
+ *
+ * // Wait for the timer to expire.
+ * timer.wait();
+ * @endcode
+ *
+ * @par 
+ * Performing an asynchronous wait (C++11):
+ * @code
+ * void handler(const boost::system::error_code& error)
+ * {
+ *   if (!error)
+ *   {
+ *     // Timer expired.
+ *   }
+ * }
+ *
+ * ...
+ *
+ * // Construct a timer with an absolute expiry time.
+ * boost::asio::steady_timer timer(io_service,
+ *     std::chrono::steady_clock::now() + std::chrono::seconds(60));
+ *
+ * // Start an asynchronous wait.
+ * timer.async_wait(handler);
+ * @endcode
+ *
+ * @par Changing an active waitable timer's expiry time
+ *
+ * Changing the expiry time of a timer while there are pending asynchronous
+ * waits causes those wait operations to be cancelled. To ensure that the action
+ * associated with the timer is performed only once, use something like this:
+ * used:
+ *
+ * @code
+ * void on_some_event()
+ * {
+ *   if (my_timer.expires_from_now(seconds(5)) > 0)
+ *   {
+ *     // We managed to cancel the timer. Start new asynchronous wait.
+ *     my_timer.async_wait(on_timeout);
+ *   }
+ *   else
+ *   {
+ *     // Too late, timer has already expired!
+ *   }
+ * }
+ *
+ * void on_timeout(const boost::system::error_code& e)
+ * {
+ *   if (e != boost::asio::error::operation_aborted)
+ *   {
+ *     // Timer was not cancelled, take necessary action.
+ *   }
+ * }
+ * @endcode
+ *
+ * @li The boost::asio::basic_waitable_timer::expires_from_now() function
+ * cancels any pending asynchronous waits, and returns the number of
+ * asynchronous waits that were cancelled. If it returns 0 then you were too
+ * late and the wait handler has already been executed, or will soon be
+ * executed. If it returns 1 then the wait handler was successfully cancelled.
+ *
+ * @li If a wait handler is cancelled, the boost::system::error_code passed to
+ * it contains the value boost::asio::error::operation_aborted.
+ */
+template <typename Clock,
+    typename WaitTraits = boost::asio::wait_traits<Clock>,
+    typename WaitableTimerService = waitable_timer_service<Clock, WaitTraits> >
+class basic_waitable_timer
+  : public basic_io_object<WaitableTimerService>
 {
-    int argc = 0;
-    cocos2d::PhysicsBody* cobj = nullptr;
-    bool ok  = true;
+public:
+  /// The clock type.
+  typedef Clock clock_type;
+    }
     
+    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
     
-    {    CHECK_GL_ERROR_DEBUG();
-}
-    
-    	b2FixtureDef fd;
-	fd.shape = &circle;
-	fd.density = 20.0f;
-	fd.restitution = 0.0f;
-	
-	b2Vec2 minV = position - b2Vec2(0.3f,0.3f);
-	b2Vec2 maxV = position + b2Vec2(0.3f,0.3f);
-	
-	b2AABB aabb;
-	aabb.lowerBound = minV;
-	aabb.upperBound = maxV;
-    
-    		for (int32 i = 0; i < 3; ++i)
-		{
-			b2CircleShape shape;
-			shape.m_radius = 0.5f;
+    namespace boost {
     }
     
     
-    {  private:
-    void* object_;
+    {  static std::size_t check_for_completion(
+      const boost::system::error_code& ec,
+      std::size_t total_transferred)
+  {
+    return transfer_all_t()(ec, total_transferred);
+  }
 };
     
-    #include <vector>
+    #ifndef BOOST_ASIO_DETAIL_IMPL_DEV_POLL_REACTOR_HPP
+#define BOOST_ASIO_DETAIL_IMPL_DEV_POLL_REACTOR_HPP
     
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
-    
-    
-    {  private:
-    JavaVM* vm_;
-    JNIEnv* env_;
-    bool we_attach_;
-    int status_;
-};
-    
-    
-    {  // Check that deleting works.
-  ASSERT_TRUE(!env_->DeleteFile(test_dir_ + '/non_existent').ok());
-  ASSERT_OK(env_->DeleteFile(test_dir_ + '/g'));
-  ASSERT_EQ(Status::NotFound(), env_->FileExists(test_dir_ + '/g'));
-  ASSERT_OK(env_->GetChildren(test_dir_, &children));
-  ASSERT_EQ(0U, children.size());
-  ASSERT_TRUE(
-      env_->GetChildren(test_dir_ + '/non_existent', &children).IsNotFound());
-}
-    
-    
-/// Accessors
-    
-      // size amplification = percentage of additional size
-  if (candidate_size * 100 < ratio * earliest_file_size) {
-    ROCKS_LOG_BUFFER(
-        log_buffer,
-        '[%s] Universal: size amp not needed. newer-files-total-size %' PRIu64
-        ' earliest-file-size %' PRIu64,
-        cf_name.c_str(), candidate_size, earliest_file_size);
-    return nullptr;
-  } else {
-    ROCKS_LOG_BUFFER(
-        log_buffer,
-        '[%s] Universal: size amp needed. newer-files-total-size %' PRIu64
-        ' earliest-file-size %' PRIu64,
-        cf_name.c_str(), candidate_size, earliest_file_size);
-  }
-  assert(start_index < sorted_runs.size() - 1);
-    
-      ASSERT_GT(listener->compaction_reasons_.size(), 0);
-  for (auto compaction_reason : listener->compaction_reasons_) {
-    ASSERT_EQ(compaction_reason, CompactionReason::kUniversalSizeAmplification);
-  }
-    
-      // Link WAL files. Copy exact size of last one because it is the only one
-  // that has changes after the last flush.
-  for (size_t i = 0; s.ok() && i < wal_size; ++i) {
-    if ((live_wal_files[i]->Type() == kAliveLogFile) &&
-        (!flush_memtable ||
-         live_wal_files[i]->StartSequence() >= *sequence_number ||
-         live_wal_files[i]->LogNumber() >= min_log_num)) {
-      if (i + 1 == wal_size) {
-        s = copy_file_cb(db_options.wal_dir, live_wal_files[i]->PathName(),
-                         live_wal_files[i]->SizeFileBytes(), kLogFile);
-        break;
-      }
-      if (same_fs) {
-        // we only care about live log files
-        s = link_file_cb(db_options.wal_dir, live_wal_files[i]->PathName(),
-                         kLogFile);
-        if (s.IsNotSupported()) {
-          same_fs = false;
-          s = Status::OK();
-        }
-      }
-      if (!same_fs) {
-        s = copy_file_cb(db_options.wal_dir, live_wal_files[i]->PathName(), 0,
-                         kLogFile);
-      }
+    namespace rocksdb {
+namespace {
+// Used in universal compaction when trivial move is enabled.
+// This structure is used for the construction of min heap
+// that contains the file meta data, the level of the file
+// and the index of the file in that level
     }
-  }
+    }
     
+        if (s.ok() && db_options.allow_2pc) {
+      // If 2PC is enabled, we need to get minimum log number after the flush.
+      // Need to refetch the live files to recapture the snapshot.
+      if (!db_->GetIntProperty(DB::Properties::kMinLogNumberToKeep,
+                               &min_log_num)) {
+        return Status::InvalidArgument(
+            '2PC enabled but cannot fine the min log number to keep.');
+      }
+      // We need to refetch live files with flush to handle this case:
+      // A previous 000001.log contains the prepare record of transaction tnx1.
+      // The current log file is 000002.log, and sequence_number points to this
+      // file.
+      // After calling GetLiveFiles(), 000003.log is created.
+      // Then tnx1 is committed. The commit record is written to 000003.log.
+      // Now we fetch min_log_num, which will be 3.
+      // Then only 000002.log and 000003.log will be copied, and 000001.log will
+      // be skipped. 000003.log contains commit message of tnx1, but we don't
+      // have respective prepare record for it.
+      // In order to avoid this situation, we need to force flush to make sure
+      // all transactions committed before getting min_log_num will be flushed
+      // to SST files.
+      // We cannot get min_log_num before calling the GetLiveFiles() for the
+      // first time, because if we do that, all the logs files will be included,
+      // far more than needed.
+      s = db_->GetLiveFiles(live_files, &manifest_file_size, flush_memtable);
+    }
     
-    {
-    {  releaseJniEnv(attached_thread);
+      // when we know more data has been written to the file. we can use this
+  // function to force the reader to look again in the file.
+  // Also aligns the file position indicator to the start of the next block
+  // by reading the rest of the data from the EOF position to the end of the
+  // block that was partially read.
+  void UnmarkEOF();
+    
+    /*
+ * Class:     org_rocksdb_IngestExternalFileOptions
+ * Method:    snapshotConsistency
+ * Signature: (J)Z
+ */
+jboolean Java_org_rocksdb_IngestExternalFileOptions_snapshotConsistency(
+    JNIEnv* env, jobject jobj, jlong jhandle) {
+  auto* options =
+      reinterpret_cast<rocksdb::IngestExternalFileOptions*>(jhandle);
+  return static_cast<jboolean>(options->snapshot_consistency);
 }
-// @lint-ignore TXT4 T25377293 Grandfathered in
-}  // namespace rocksdb
     
-      StatisticsJni::StatisticsJni(std::shared_ptr<Statistics> stats)
-      : StatisticsImpl(stats, false), m_ignore_histograms() {
-  }
+    /**
+ * @brief create new dir
+ * @details [long description]
+ *
+ * @param dirname [description]
+ * @return [description]
+ */
+Status EnvLibrados::CreateDir(const std::string& dirname)
+{
+  LOG_DEBUG('[IN]%s\n', dirname.c_str());
+  std::string fid, dir, file;
+  split(dirname + '/temp', &dir, &file);
+  Status s = _GetFid(dir + '/' + file, fid);
+    }
