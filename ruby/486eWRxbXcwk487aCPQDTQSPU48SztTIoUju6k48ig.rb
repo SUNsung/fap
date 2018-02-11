4c8bde226e5a80cca7005999dbb6b7424d7973b5
SUNsung <weatherius@gmail.com>
2018-02-11 11:39:16 +0200

    Rewrite [486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb] File in: 2018-02-11 11:39:15

--- ruby/486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb
+++ ruby/486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb
@@ -1,166 +1,148 @@
 
         
-              included do
-        setup :setup_with_controller
-        ActiveSupport.run_load_hooks(:action_view_test_case, self)
-      end
-    
-              # Require the adapter itself and give useful feedback about
-          #   1. Missing adapter gems and
-          #   2. Adapter gems' missing dependencies.
-          path_to_adapter = 'active_record/connection_adapters/#{spec[:adapter]}_adapter'
-          begin
-            require path_to_adapter
-          rescue LoadError => e
-            # We couldn't require the adapter itself. Raise an exception that
-            # points out config typos and missing gems.
-            if e.path == path_to_adapter
-              # We can assume that a non-builtin adapter was specified, so it's
-              # either misspelled or missing from Gemfile.
-              raise e.class, 'Could not load the '#{spec[:adapter]}' Active Record adapter. Ensure that the adapter is spelled correctly in config/database.yml and that you've added the necessary adapter gem to your Gemfile.', e.backtrace
-    
-      def popen_run(cmd, opts, child_io, parent_io) # :nodoc:
-    if last = Hash.try_convert(cmd.last)
-      opts = opts.merge(last)
-      cmd.pop
+            # Returns the unwrapped subject and body of the last commit
+    # <b>DEPRECATED:</b> Use <tt>last_git_commit_message</tt> instead.
+    def self.last_git_commit
+      UI.important('`last_git_commit` is deprecated. Please use `last_git_commit_message` instead.')
+      last_git_commit_message
     end
-    pid = spawn(*cmd, opts)
-    wait_thr = Process.detach(pid)
-    child_io.each(&:close)
-    result = [*parent_io, wait_thr]
-    if defined? yield
-      begin
-        return yield(*result)
-      ensure
-        parent_io.each(&:close)
-        wait_thr.join
+    
+          it 'renders HTML' do
+        expect(render(options, screenshots)).to match(/<html>/)
       end
     end
-    result
-  end
-  module_function :popen_run
-  class << self
-    private :popen_run
-  end
     
-      def test_popen2
-    with_pipe {|r, w|
-      with_reopen(STDERR, w) {|old|
-        w.close
-        Open3.popen2(RUBY, '-e', 's=STDIN.read; STDOUT.print s+'o'; STDERR.print s+'e'') {|i,o,t|
-          assert_kind_of(Thread, t)
-          i.print 'z'
-          i.close
-          STDERR.reopen(old)
-          assert_equal('zo', o.read)
-          assert_equal('ze', r.read)
-        }
-      }
-    }
-  end
+          xcode_project = Xcodeproj::Project.open(@project_file_path)
+      target = xcode_project.targets.find { |t| t.name == @target_name }
     
-    # Testing 'c'
-      [ '%3c', 'abc', [ 'abc' ] ],
-      [ '%3c', 'a\nb', [ 'a\nb' ] ],
-      [ '%3c', 'a\nbcd', [ 'a\nb' ] ],
-      [ '%c\n\n', 'x\n\n', [ 'x' ] ],
-      [ '%c', '\n', [ '\n' ] ],
-      [ '%c', 'x\n', [ 'x' ] ],
-      [ '%2c', ' 123', [' 1'] ],
-      [ ' %c', ' x', ['x'] ],
-      [ '%c', ' x', [' '] ],
-      [ '%c', '123', ['1'] ],
-      [ '%2c', '123', ['12'] ],
-      [ '%5c', 'a\nb\n\n', [ 'a\nb\n\n' ] ],
-      [ '%6c', 'a\nb\n\nx', [ 'a\nb\n\nx' ] ],
-      [ '%5c', 'ab\ncd', [ 'ab\ncd' ] ],
-    
-      def test_session_timeout
-    sess = OpenSSL::SSL::Session.new(DUMMY_SESSION_NO_EXT)
-    assert_raise(TypeError) do
-      sess.timeout = Time.now
-    end
-    sess.timeout = 1
-    assert_equal(1, sess.timeout.to_i)
-    sess.timeout = 1.2345
-    assert_equal(1, sess.timeout.to_i)
-    sess.timeout = 2**31 - 1
-    assert_equal(2**31 - 1, sess.timeout.to_i)
-  end
-    
-        ##
-    # Matches +addr+ against this entry.
-    
-              fd.write(res)
-        end
+            expect(result).to eq('hg parent --template '{node|short}'')
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('hg parent --template '{node|short}'')
       end
-      break
-    rescue ::Timeout::Error
-      $stderr.puts '#{prefix}#{site} timed out'
-    rescue ::Interrupt
-      raise $!
-    rescue ::Exception => e
-      $stderr.puts '#{prefix}#{site} #{e.class} #{e}'
+    
+          it 'gets the correct version number for 'TargetATests'' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          get_version_number(xcodeproj: '.xcproject', target: 'TargetATests')
+        end').runner.execute(:test)
+        expect(result).to eq('4.3.2')
+      end
+    
+      class Worker
+    attr_reader :thread, :id, :agent, :config, :mutex, :scheduler, :restarting
+    
+      included do
+    include Oauthable
+    
+        # replace CSS rule blocks matching rule_prefix with yield(rule_block, rule_pos)
+    # will also include immediately preceding comments in rule_block
+    #
+    # option :comments -- include immediately preceding comments in rule_block
+    #
+    # replace_rules('.a{ \n .b{} }', '.b') { |rule, pos| '>#{rule}<'  } #=> '.a{ \n >.b{}< }'
+    def replace_rules(less, selector = SELECTOR_RE, options = {}, &block)
+      options       = {prefix: true, comments: true}.merge(options || {})
+      less          = less.dup
+      s             = CharStringScanner.new(less)
+      rule_re       = if options[:prefix]
+                        /(?:#{selector}[#{SELECTOR_CHAR})=(\s]*?#{RULE_OPEN_BRACE_RE})/
+                      else
+                        /#{selector}[\s]*#{RULE_OPEN_BRACE_RE}/
+                      end
+      rule_start_re = if options[:comments]
+                        /(?:#{COMMENT_RE}*)^#{rule_re}/
+                      else
+                        /^#{rule_re}/
+                      end
+    
+      def test_font_helper_without_suffix
+    assert_match %r(url\(['']?/assets/.*eot['']?\)), @css
+  end
+    
+          metadata_master_container_path.join(cask_version)
+    end
+    
+    module Hbc
+  class DSL
+    class Container
+      VALID_KEYS = Set.new [
+        :type,
+        :nested,
+      ]
+    
+        export LANG=en_US.UTF-8
+    DOC
+  else
+    STDERR.puts <<-DOC
+    \e[33mWARNING: CocoaPods requires your terminal to be using UTF-8 encoding.
+    Consider adding the following to ~/.profile:
+    
+              If there is multiple cache for various versions of the requested pod,
+          you will be asked which one to clean. Use `--all` to clean them all.
+    
+            # Split out the targets into app and test targets
+        test_targets, app_targets = project.native_targets.
+                              sort_by { |t| t.name.downcase }.
+                              partition(&:test_target_type?)
+    
+            TEMPLATE_REPO = 'https://github.com/CocoaPods/pod-template.git'.freeze
+        TEMPLATE_INFO_URL = 'https://github.com/CocoaPods/pod-template'.freeze
+        CREATE_NEW_POD_INFO_URL = 'http://guides.cocoapods.org/making/making-a-cocoapod'.freeze
+    
+          def self.options
+        [
+          ['--update', 'Run `pod repo update` before listing'],
+          ['--stats',  'Show additional stats (like GitHub watchers and forks)'],
+        ].concat(super)
+      end
+    
+    module Rack
+  module Protection
+    ##
+    # Prevented attack::   XSS
+    # Supported browsers:: all
+    # More infos::         http://en.wikipedia.org/wiki/Cross-site_scripting
+    #
+    # Automatically escapes Rack::Request#params so they can be embedded in HTML
+    # or JavaScript without any further issues. Calls +html_safe+ on the escaped
+    # strings if defined, to avoid double-escaping in Rails.
+    #
+    # Options:
+    # escape:: What escaping modes to use, should be Symbol or Array of Symbols.
+    #          Available: :html (default), :javascript, :url
+    class EscapedParams < Base
+      extend Rack::Utils
+    
+        it 'accepts requests with a single session cookie' do
+      get '/', {}, 'HTTP_COOKIE' => 'rack.session=SESSION_TOKEN'
+      expect(last_response).to be_ok
+    end
+    
+      %w(GET HEAD).each do |method|
+    it 'accepts #{method} requests with non-whitelisted Origin' do
+      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://malicious.com')).to be_ok
     end
   end
     
-          when :login_pass
-    
-            # Prints the list of specs & pod cache dirs for a single pod name.
-        #
-        # This output is valid YAML so it can be parsed with 3rd party tools
-        #
-        # @param [Array<Hash>] cache_descriptors
-        #        The various infos about a pod cache. Keys are
-        #        :spec_file, :version, :release and :slug
-        #
-        def print_pod_cache_infos(pod_name, cache_descriptors)
-          UI.puts '#{pod_name}:'
-          cache_descriptors.each do |desc|
-            if @short_output
-              [:spec_file, :slug].each { |k| desc[k] = desc[k].relative_path_from(@cache.root) }
-            end
-            UI.puts('  - Version: #{desc[:version]}')
-            UI.puts('    Type:    #{pod_type(desc)}')
-            UI.puts('    Spec:    #{desc[:spec_file]}')
-            UI.puts('    Pod:     #{desc[:slug]}')
-          end
-        end
+          it 'should remain unchanged as ASCII-8BIT' do
+        body = @app.call({ 'PATH_INFO' => '/'.encode('ASCII-8BIT') })[2][0]
+        expect(body).to eq('ASCII-8BIT')
       end
     end
   end
 end
 
     
-            self.description = <<-DESC
-          Creates a scaffold for the development of a new Pod named `NAME`
-          according to the CocoaPods best practices.
-          If a `TEMPLATE_URL`, pointing to a git repo containing a compatible
-          template, is specified, it will be used in place of the default one.
-        DESC
+      it 'accepts requests with the same Accept-Language header' do
+    session = {:foo => :bar}
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
+    expect(session).not_to be_empty
+  end
     
-        def initialize(tag_name, markup, tokens)
-      @by = nil
-      @source = nil
-      @title = nil
-      if markup =~ FullCiteWithTitle
-        @by = $1
-        @source = $2 + $3
-        @title = $4.titlecase.strip
-      elsif markup =~ FullCite
-        @by = $1
-        @source = $2 + $3
-      elsif markup =~ AuthorTitle
-        @by = $1
-        @title = $2.titlecase.strip
-      elsif markup =~ Author
-        @by = $1
-      end
-      super
-    end
-    
-            def destroy
-          authorize! :destroy, stock_location
-          stock_location.destroy
-          respond_with(stock_location, status: 204)
+            def update
+          authorize! :update, stock_location
+          if stock_location.update_attributes(stock_location_params)
+            respond_with(stock_location, status: 200, default_template: :show)
+          else
+            invalid_resource!(stock_location)
+          end
         end
