e4d0cf074257acf3be5f6acb3004e276927bcbd2
SUNsung <weatherius@gmail.com>
2018-02-11 17:53:08 +0200

    Rewrite [588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py] File in: 2018-02-11 17:53:07

--- pyton/588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py
+++ pyton/588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py
@@ -1,47 +1,89 @@
 
         
-            cmd = cmd.split()
+        
+def patch(url, data=None, **kwargs):
+    r'''Sends a PATCH request.
     
-        def tell(self):
-        if self.closed:
-            raise ValueError('I/O operation on closed file')
-        return self.size_read
+        return out
     
-            '''
-        obj, end = self.raw_decode(s, idx=_w(s, 0).end())
-        end = _w(s, end).end()
-        if end != len(s):
-            raise JSONDecodeError('Extra data', s, end)
-        return obj
+        if isinstance(username, str):
+        username = username.encode('latin1')
     
-    import copy
-import warnings
-import xml.dom
     
-        print('$(target)$(debug_suffix)%s: $(temp_dir) $(OBJS)' % (target_ext))
-    print('\tlink -out:$(target)$(debug_suffix)%s %s' %
-          (target_ext, target_link_flags), '@<<')
-    print('\t$(OBJS)')
-    print('\t$(LIBS)')
-    print('\t$(ADDN_LINK_FILES)')
-    print('\t$(pythonlib) $(lcustom) $(l_debug)')
-    print('\t$(resources)')
-    print('<<')
-    print()
-    print('clean:')
-    print('\t-del /f *.obj')
-    print('\t-del /f $(target).exe')
-
+def extract_cookies_to_jar(jar, request, response):
+    '''Extract the cookies from the response into a CookieJar.
     
-        def test_truediv(self):
-        simple_real = [float(i) for i in range(-5, 6)]
-        simple_complex = [complex(x, y) for x in simple_real for y in simple_real]
-        for x in simple_complex:
-            for y in simple_complex:
-                self.check_div(x, y)
+        def __init__(self, license_plate):
+        super(Car, self).__init__(VehicleSize.COMPACT, license_plate, spot_size=1)
     
-            [['blorpie'],[ 'whoops' ] , [
-                                 ],\t'd-shtaeou',\r'd-nthiouh',
-        'i-vhbjkhnth', {'nifty':87}, {'morefield' :\tfalse,'field'
-            :'yes'}  ]
-           '''
+    
+class Categorizer(object):
+    
+        def bfs(self, source, dest):
+        if source is None:
+            return False
+        queue = deque()
+        queue.append(source)
+        source.visit_state = State.visited
+        while queue:
+            node = queue.popleft()
+            print(node)
+            if dest is node:
+                return True
+            for adjacent_node in node.adj_nodes.values():
+                if adjacent_node.visit_state == State.unvisited:
+                    queue.append(adjacent_node)
+                    adjacent_node.visit_state = State.visited
+        return False
+    
+            # Self test
+        self.assertTrue(self.addr.conflicts(self.addr))
+        self.assertTrue(self.addr1.conflicts(self.addr1))
+        # This is a tricky one...
+        self.assertTrue(self.addr1.conflicts(self.addr2))
+    
+            with open(self.log_path, 'r') as fd:
+            content = fd.read()
+            return content
+    
+    
+    def toOriginalString(self, start=None, end=None):
+        if start is None:
+            start = self.MIN_TOKEN_INDEX
+        if end is None:
+            end = self.size() - 1
+        
+        buf = StringIO()
+        i = start
+        while i >= self.MIN_TOKEN_INDEX and i <= end and i < len(self.tokens):
+            buf.write(self.get(i).text)
+            i += 1
+    
+            channelStr = ''
+        if self.channel > 0:
+            channelStr = ',channel=' + str(self.channel)
+    
+        def test_empty_objects(self):
+        self.assertEqual(self.loads('{}'), {})
+        self.assertEqual(self.loads('[]'), [])
+        self.assertEqual(self.loads(''''), '')
+    
+    
+print('\n# ======================================================================')
+print('#                               Factorial')
+print('# ======================================================================\n')
+    
+    def evalString(s):
+    assert s.startswith(''') or s.startswith('''), repr(s[:1])
+    q = s[0]
+    if s[:3] == q*3:
+        q = q*3
+    assert s.endswith(q), repr(s[-len(q):])
+    assert len(s) >= 2*len(q)
+    s = s[len(q):-len(q)]
+    return re.sub(r'\\(\'|\'|\\|[abfnrtv]|x.{0,2}|[0-7]{1,3})', escape, s)
+    
+            If the registered instance has a _dispatch method then that
+        method will be called with the name of the XML-RPC method and
+        its parameters as a tuple
+        e.g. instance._dispatch('add',(2,3))
