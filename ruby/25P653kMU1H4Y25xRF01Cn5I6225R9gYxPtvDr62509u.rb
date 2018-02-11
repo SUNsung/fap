afbbf0231c7d24af3c334b0105028f84a90b52e3
SUNsung <weatherius@gmail.com>
2018-02-11 19:43:01 +0200

    Rewrite [25P653kMU1H4Y25xRF01Cn5I6225R9gYxPtvDr62509u.rb] File in: 2018-02-11 19:43:00

--- ruby/25P653kMU1H4Y25xRF01Cn5I6225R9gYxPtvDr62509u.rb
+++ ruby/25P653kMU1H4Y25xRF01Cn5I6225R9gYxPtvDr62509u.rb
@@ -1,18 +1,3 @@
 
         
-            def register(klass, attachment_name, attachment_options)
-      @attachments ||= {}
-      @attachments[klass] ||= {}
-      @attachments[klass][attachment_name] = attachment_options
-    end
-    
-            def allowed_types_allowed?
-          @missing_allowed_types ||= @allowed_types.reject { |type| type_allowed?(type) }
-          @missing_allowed_types.none?
-        end
-    
-            def error_when_not_valid?
-          @subject.send(@attachment_name).assign(nil)
-          @subject.valid?
-          @subject.errors[:'#{@attachment_name}'].present?
-        end
+        
