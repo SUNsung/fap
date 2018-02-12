2d841def554b1480d302e892e4463b485125d184
SUNsung <weatherius@gmail.com>
2018-02-12 20:05:56 +0200

    Rewrite [435kO3PoUjRHk430wZQF0OUujw43slTJBTU0i5E43L2P.rb] File in: 2018-02-12 20:05:56

--- ruby/435kO3PoUjRHk430wZQF0OUujw43slTJBTU0i5E43L2P.rb
+++ ruby/435kO3PoUjRHk430wZQF0OUujw43slTJBTU0i5E43L2P.rb
@@ -1,92 +1,111 @@
 
         
-        def site_configuration(overrides = {})
-  build_configs({
-    'source'      => source_dir,
-    'destination' => dest_dir
-  }, build_configs(overrides))
-end
+                  table = reflection.aliased_table
+          value = transform_value(owner[foreign_key])
+          scope = apply_scope(scope, table, key, value)
     
-    unless File.exist?(PROF_OUTPUT_FILE)
-  StackProf.run(
-    mode: MODE.to_sym,
-    interval: 100,
-    raw: true,
-    out: PROF_OUTPUT_FILE
-  ) do
-    puts 'GC Stats:', JSON.pretty_generate(GC.stat)
-    GC.disable
+                def when_connected(&block)
+              if @raw_client
+                block.call
+              else
+                @when_connected << block
+              end
+            end
     
-      Jekyll::External.require_if_present(Jekyll::External.blessed_gems) do |g, ver_constraint|
-    cmd = g.split('-').last
-    p.command(cmd.to_sym) do |c|
-      c.syntax cmd
-      c.action do
-        Jekyll.logger.abort_with 'You must install the '#{g}' gem' \
-          ' version #{ver_constraint} to use the 'jekyll #{cmd}' command.'
-      end
-    end
-  end
+      def test_helper_proxy
+    methods = AllHelpersController.helpers.methods
     
-        def defaults_deprecate_type(old, current)
-      Jekyll.logger.warn 'Defaults:', 'The '#{old}' type has become '#{current}'.'
-      Jekyll.logger.warn 'Defaults:', 'Please update your front-matter defaults to use \
-                        'type: #{current}'.'
-    end
-  end
-end
-
+      s.metadata = {
+    'source_code_uri' => 'https://github.com/rails/rails/tree/v#{version}/actionmailer',
+    'changelog_uri'   => 'https://github.com/rails/rails/blob/v#{version}/actionmailer/CHANGELOG.md'
+  }
     
-    module Jekyll
-  class EntryFilter
-    attr_reader :site
-    SPECIAL_LEADING_CHARACTERS = [
-      '.', '_', '#', '~',
-    ].freeze
-    
-        # Gets the name of this layout.
-    attr_reader :name
-    
-        # Assign additional cores to the guest OS.
-    v.customize ['modifyvm', :id, '--cpus', cpu_count]
-    v.customize ['modifyvm', :id, '--ioapic', 'on']
-    
-      def revoke_moderation!
-    set_permission('moderator', false)
-  end
-    
-          # Returns the full path to the template, taking into accoun the gem directory
-      # and adding the `.erb` extension to the end.
-      #
-      # @return [String]
-      def full_template_path
-        @template_root.join('#{template}.erb').to_s.squeeze('/')
-      end
-    end
-  end
-end
-
-    
-    namespace :db do
-  namespace :migrate do
-    desc 'Setup the db or migrate depending on state of db'
-    task setup: :environment do
-      begin
-        if ActiveRecord::Migrator.current_version.zero?
-          Rake::Task['db:migrate'].invoke
-          Rake::Task['db:seed'].invoke
+        private
+      # 'Deserialize' the mailer class name by hand in case another argument
+      # (like a Global ID reference) raised DeserializationError.
+      def mailer_class
+        if mailer = Array(@serialized_arguments).first || Array(arguments).first
+          mailer.constantize
         end
-      rescue ActiveRecord::NoDatabaseError
-        Rake::Task['db:setup'].invoke
-      else
-        Rake::Task['db:migrate'].invoke
+      end
+    
+        # An email was generated.
+    def process(event)
+      debug do
+        mailer = event.payload[:mailer]
+        action = event.payload[:action]
+        '#{mailer}##{action}: processed outbound mail in #{event.duration.round(1)}ms'
       end
     end
+    
+    class Jekyll::Commands::NewTheme < Jekyll::Command
+  class << self
+    def init_with_program(prog)
+      prog.command(:'new-theme') do |c|
+        c.syntax 'new-theme NAME'
+        c.description 'Creates a new Jekyll theme scaffold'
+        c.option 'code_of_conduct', \
+          '-c', '--code-of-conduct', \
+          'Include a Code of Conduct. (defaults to false)'
+    
+              # If it's a directory they want to exclude, AKA
+          # ends with a '/' then we will go on to check and
+          # see if the entry falls within that path and
+          # exclude it if that's the case.
+    
+        # Check if excerpt includes a string
+    #
+    # Returns true if the string passed in
+    def include?(something)
+      (output && output.include?(something)) || content.include?(something)
+    end
+    
+        # Gets/Sets the extension of this layout.
+    attr_accessor :ext
+    
+      # Make it so that network access from the vagrant guest is able to
+  # use SSH private keys that are present on the host without copying
+  # them into the VM.
+  config.ssh.forward_agent = true
+    
+            css('.toplang', '#quickview', '.top').remove
+    
+        # apply general less to scss conversion
+    def convert_to_scss(file)
+      # get local mixin names before converting the definitions
+      mixins = shared_mixins + read_mixins(file)
+      file   = replace_vars(file)
+      file   = replace_mixin_definitions(file)
+      file   = replace_mixins(file, mixins)
+      file   = extract_mixins_from_selectors(file, CLASSES_TO_MIXINS.inject({}) { |h, cl| h.update('.#{cl}' => cl) })
+      file   = replace_spin(file)
+      file   = replace_fadein(file)
+      file   = replace_image_urls(file)
+      file   = replace_escaping(file)
+      file   = convert_less_ampersand(file)
+      file   = deinterpolate_vararg_mixins(file)
+      file   = replace_calculation_semantics(file)
+      file   = replace_file_imports(file)
+      file   = wrap_at_groups_with_at_root(file)
+      file
+    end
+    
+    class NodeMincerTest < Minitest::Test
+  DUMMY_PATH = 'test/dummy_node_mincer'
+    
+      # Read and eval a .rake file in such a way that `self` within the .rake file
+  # refers to this plugin instance. This gives the tasks in the file access to
+  # helper methods defined by the plugin.
+  def eval_rakefile(path)
+    contents = IO.read(path)
+    instance_eval(contents, path, 1)
   end
     
-        if object.conversation.uri?
-      object.conversation.uri
-    else
-      OStatus::TagManager.instance.unique_tag(object.conversation.created_at, object.conversation.id, 'Conversation')
+        it 'yields the properties for a single role' do
+      recipient = mock('recipient')
+      recipient.expects(:doit).with('example1.com', :redis, port: 6379, type: :slave)
+      recipient.expects(:doit).with('example2.com', :redis, port: 6379, type: :master)
+      dsl.role_properties(:redis) do |host, role, props|
+        recipient.doit(host, role, props)
+      end
     end
-  end
