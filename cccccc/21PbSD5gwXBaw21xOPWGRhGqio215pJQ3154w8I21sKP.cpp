8b3961c7b1fe01ea45e72f53bb152e3b9d8f9faf
SUNsung <weatherius@gmail.com>
2018-02-12 16:33:19 +0200

    Rewrite [21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp] File in: 2018-02-12 16:33:18

--- cccccc/21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp
+++ cccccc/21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp
@@ -1,165 +1,264 @@
 
         
-        void leveldb_put(
-    leveldb_t* db,
-    const leveldb_writeoptions_t* options,
-    const char* key, size_t keylen,
-    const char* val, size_t vallen,
-    char** errptr) {
-  SaveError(errptr,
-            db->rep->Put(options->rep, Slice(key, keylen), Slice(val, vallen)));
-}
+          // Solve the cubic equation
+  double Q = (3 * c_a - b_a2) / 9;
+  double R = (9 * b_a * c_a - 27 * d_a - 2 * b_a * b_a2) / 54;
+  double Q3 = Q * Q * Q;
+  double D = Q3 + R * R;
+  double b_a_3 = (1. / 3.) * b_a;
     
-    struct FileMetaData {
-  int refs;
-  int allowed_seeks;          // Seeks allowed until compaction
-  uint64_t number;
-  uint64_t file_size;         // File size in bytes
-  InternalKey smallest;       // Smallest internal key served by table
-  InternalKey largest;        // Largest internal key served by table
+            template<int CTA_SIZE>
+        static __forceinline__ __device__ int Ballot(int predicate)
+        {
+#if defined (__CUDA_ARCH__) && (__CUDA_ARCH__ >= 200)
+            return __ballot(predicate);
+#else
+            __shared__ volatile int cta_buffer[CTA_SIZE];
     }
     
-    #if 0
-#include '_modelest.h'
     
+    {} // namespace
     
+    namespace
+{
+    template <class ObjType>
+    void ensureSizeIsEnoughImpl(int rows, int cols, int type, ObjType& obj)
     {
+        if (obj.empty() || obj.type() != type || obj.data != obj.datastart)
+        {
+            obj.create(rows, cols, type);
+        }
+        else
+        {
+            const size_t esz = obj.elemSize();
+            const ptrdiff_t delta2 = obj.dataend - obj.datastart;
+    }
+    }
+    }
+    
+        // Extension: 3.1
+    extern void (CODEGEN_FUNCPTR *DrawArraysInstanced)(GLenum mode, GLint first, GLsizei count, GLsizei instancecount);
+    extern void (CODEGEN_FUNCPTR *DrawElementsInstanced)(GLenum mode, GLsizei count, GLenum type, const GLvoid *indices, GLsizei instancecount);
+    extern void (CODEGEN_FUNCPTR *TexBuffer)(GLenum target, GLenum internalformat, GLuint buffer);
+    extern void (CODEGEN_FUNCPTR *PrimitiveRestartIndex)(GLuint index);
+    
+    // To write value-parameterized tests, first you should define a fixture
+// class. It is usually derived from testing::TestWithParam<T> (see below for
+// another inheritance scheme that's sometimes useful in more complicated
+// class hierarchies), where the type of your parameter values.
+// TestWithParam<T> is itself derived from testing::Test. T can be any
+// copyable type. If it's a raw pointer, you are responsible for managing the
+// lifespan of the pointed values.
+    
+        void ComputeCurrentValue() {
+      if (!AtEnd())
+        current_value_ = ParamType($for j, [[*current$(j)_]]);
+    }
+    bool AtEnd() const {
+      // We must report iterator past the end of the range when either of the
+      // component iterators has reached the end of its range.
+      return
+$for j  || [[
+    }
+    
+    template <GTEST_8_TYPENAMES_(T)>
+inline GTEST_8_TUPLE_(T) make_tuple(const T0& f0, const T1& f1, const T2& f2,
+    const T3& f3, const T4& f4, const T5& f5, const T6& f6, const T7& f7) {
+  return GTEST_8_TUPLE_(T)(f0, f1, f2, f3, f4, f5, f6, f7);
+}
+    
+        int traceLevel = config(L'traceLevel', 0);
+    if (config.Exists(L'createNetwork'))
     {
-    {            static __device__ __forceinline__ int atomicMax(int* address, int val)
-            {
-                return ::atomicMax(address, val);
-            }
-            static __device__ __forceinline__ float atomicMax(float* address, float val)
-            {
-            #if __CUDA_ARCH__ >= 120
-                int* address_as_i = (int*) address;
-                int old = *address_as_i, assumed;
-                do {
-                    assumed = old;
-                    old = ::atomicCAS(address_as_i, assumed,
-                        __float_as_int(::fmaxf(val, __int_as_float(assumed))));
-                } while (assumed != old);
-                return __int_as_float(old);
-            #else
-                (void) address;
-                (void) val;
-                return 0.0f;
-            #endif
-            }
-            static __device__ __forceinline__ double atomicMax(double* address, double val)
-            {
-            #if __CUDA_ARCH__ >= 130
-                unsigned long long int* address_as_ull = (unsigned long long int*) address;
-                unsigned long long int old = *address_as_ull, assumed;
-                do {
-                    assumed = old;
-                    old = ::atomicCAS(address_as_ull, assumed,
-                        __double_as_longlong(::fmax(val, __longlong_as_double(assumed))));
-                } while (assumed != old);
-                return __longlong_as_double(old);
-            #else
-                (void) address;
-                (void) val;
-                return 0.0;
-            #endif
-            }
+        createNetworkFn = GetCreateNetworkFn(config); // (we need a separate function needed due to template code)
+        return true;
+    }
+    else if (config.Exists(L'SimpleNetworkBuilder'))
+    {
+        const ConfigRecordType& simpleNetworkBuilderConfig(config(L'SimpleNetworkBuilder'));
+        auto netBuilder = make_shared<SimpleNetworkBuilder<ElemType>>(simpleNetworkBuilderConfig); // parses the configuration and stores it in the SimpleNetworkBuilder object
+        createNetworkFn = [netBuilder, traceLevel](DEVICEID_TYPE deviceId)
+        {
+            auto net = shared_ptr<ComputationNetwork>(netBuilder->BuildNetworkFromDescription()); // this operates based on the configuration saved above
+            net->SetTraceLevel(traceLevel);
+            return net;
         };
-    }; //struct Emulation
-}}} // namespace cv { namespace cuda { namespace cudev
+        return true;
+    }
+    // legacy NDL
+    else if (config.Exists(L'NDLNetworkBuilder'))
+    {
+        const ConfigRecordType& ndlNetworkBuilderConfig(config(L'NDLNetworkBuilder'));
+        shared_ptr<NDLBuilder<ElemType>> netBuilder = make_shared<NDLBuilder<ElemType>>(ndlNetworkBuilderConfig);
+        createNetworkFn = [netBuilder, traceLevel](DEVICEID_TYPE deviceId)
+        {
+            auto net = shared_ptr<ComputationNetwork>(netBuilder->BuildNetworkFromDescription());
+            net->SetTraceLevel(traceLevel);
+            return net;
+        };
+        return true;
+    }
+    // legacy test mode for BrainScript. Will go away once we fully integrate with BS.
+    else if (config.Exists(L'BrainScriptNetworkBuilder') || config.Exists(L'ExperimentalNetworkBuilder' /*legacy name*/))
+    {
+        // We interface with outer old CNTK config by taking the inner part, which we get as a string, as BrainScript.
+        // We prepend a few standard definitions, and also definition of deviceId and precision, which all objects will pull out again when they are being constructed.
+        // BUGBUG: We are not getting TextLocations right in this way! Do we need to inject location markers into the source? Moot once we fully switch to BS
+        wstring sourceOfNetwork = config.Exists(L'BrainScriptNetworkBuilder') ? config(L'BrainScriptNetworkBuilder') : config(L'ExperimentalNetworkBuilder');
+        if (sourceOfNetwork.find_first_of(L'([{') != 0)
+            InvalidArgument('BrainScript network description must be either a BS expression in ( ) or a config record in { }');
+    }
+    }
     
-    CV_EXPORTS_W void setErrorVerbosity(bool verbose);
+    template <class _T>
+class const_array_ref
+{
+    const _T* data;
+    size_t n;
+    inline void check_index(size_t i) const
+    {
+        i;
+        assert(i < n);
+    }
+    inline void check_ptr() const
+    {
+        n;
+        data;
+        assert(n == 0 || data != NULL);
+    }
+    }
     
-    #ifndef __OPENCV_CORE_BUFFER_POOL_IMPL_HPP__
-#define __OPENCV_CORE_BUFFER_POOL_IMPL_HPP__
-    
-            if (total_width * new_rows != total_size)
-            CV_Error(cv::Error::StsBadArg, 'The total number of matrix elements is not divisible by the new number of rows');
-    
-            datastart = data =  (uchar*)ptr;
-        dataend = data + nettosize;
-    
-    #define __gl_h_
-#define __GL_H__
-#define __glext_h_
-#define __GLEXT_H_
-#define __gl_ATI_h_
-    
-    
-    {  // Statically allocating this lets us use this in allocation-sensitive
-  // contexts. This relies on the assumption that std::mutex won't dynamically
-  // allocate memory, which we assume to be the case on Linux and iOS.
-  static Indestructible<std::array<Bucket, kNumBuckets>> gBuckets;
-  return (*gBuckets)[key % kNumBuckets];
+    TEST(LogTest, ManyBlocks) {
+  for (int i = 0; i < 100000; i++) {
+    Write(NumberString(i));
+  }
+  for (int i = 0; i < 100000; i++) {
+    ASSERT_EQ(NumberString(i), Read());
+  }
+  ASSERT_EQ('EOF', Read());
 }
     
-    TEST(FollyIntegerSequence, core) {
-  constexpr auto seq = folly::integer_sequence<int, 0, 3, 2>();
-  static_assert(seq.size() == 3, '');
-  EXPECT_EQ(3, seq.size());
-    }
-    
-      // Allow implicit conversion from Range<From> to Range<To> if From is
-  // implicitly convertible to To.
-  template <
-      class OtherIter,
-      typename std::enable_if<
-          (!std::is_same<Iter, OtherIter>::value &&
-           std::is_convertible<OtherIter, Iter>::value),
-          int>::type = 0>
-  constexpr /* implicit */ Range(const Range<OtherIter>& other)
-      : b_(other.begin()), e_(other.end()) {}
-    
-    BENCHMARK_DRAW_LINE()
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 1_stripe_0_work, 1, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 2_stripe_0_work, 2, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 4_stripe_0_work, 4, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 8_stripe_0_work, 8, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 16_stripe_0_work, 16, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 32_stripe_0_work, 32, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 64_stripe_0_work, 64, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthThreadLocal, 2_stripe_0_work, 2, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthThreadLocal, 4_stripe_0_work, 4, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthThreadLocal, 8_stripe_0_work, 8, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthThreadLocal, 16_stripe_0_work, 16, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthThreadLocal, 32_stripe_0_work, 32, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthThreadLocal, 64_stripe_0_work, 64, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthPthreadSelf, 2_stripe_0_work, 2, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthPthreadSelf, 4_stripe_0_work, 4, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthPthreadSelf, 8_stripe_0_work, 8, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthPthreadSelf, 16_stripe_0_work, 16, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthPthreadSelf, 32_stripe_0_work, 32, 0)
-BENCHMARK_NAMED_PARAM(contentionAtWidthPthreadSelf, 64_stripe_0_work, 64, 0)
-BENCHMARK_NAMED_PARAM(atomicIncrBaseline, local_incr_0_work, 0)
-BENCHMARK_DRAW_LINE()
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 1_stripe_500_work, 1, 500)
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 2_stripe_500_work, 2, 500)
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 4_stripe_500_work, 4, 500)
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 8_stripe_500_work, 8, 500)
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 16_stripe_500_work, 16, 500)
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 32_stripe_500_work, 32, 500)
-BENCHMARK_NAMED_PARAM(atomicIncrBaseline, local_incr_500_work, 500)
-BENCHMARK_DRAW_LINE()
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 1_stripe_1000_work, 1, 1000)
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 2_stripe_1000_work, 2, 1000)
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 4_stripe_1000_work, 4, 1000)
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 8_stripe_1000_work, 8, 1000)
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 16_stripe_1000_work, 16, 1000)
-BENCHMARK_NAMED_PARAM(contentionAtWidthGetcpu, 32_stripe_1000_work, 32, 1000)
-BENCHMARK_NAMED_PARAM(atomicIncrBaseline, local_incr_1000_work, 1000)
-    
-    #include <memory>
-#include <string>
-    
-      if (!ok) {
-    return;
+      VersionEdit edit;
+  for (int i = 0; i < 4; i++) {
+    TestEncodeDecode(edit);
+    edit.AddFile(3, kBig + 300 + i, kBig + 400 + i,
+                 InternalKey('foo', kBig + 500 + i, kTypeValue),
+                 InternalKey('zoo', kBig + 600 + i, kTypeDeletion));
+    edit.DeleteFile(4, kBig + 700 + i);
+    edit.SetCompactPointer(i, InternalKey('x', kBig + 900 + i, kTypeValue));
   }
-  SeekToRestartPoint(index);
-  // Linear search (within restart block) for first key >= target
     
-      void SeekToRestartPoint(uint32_t index) {
-    key_.Clear();
-    restart_index_ = index;
-    // current_ will be fixed by ParseNextKey();
+    class Env;
+    
+      DBImpl* dbi = reinterpret_cast<DBImpl*>(db);
+  ASSERT_OK(dbi->TEST_CompactMemTable());
+    
+    TEST(Issue200, Test) {
+  // Get rid of any state from an old run.
+  std::string dbpath = test::TmpDir() + '/leveldb_issue200_test';
+  DestroyDB(dbpath, Options());
     }
     
-    #include 'rocksjni/statisticsjni.h'
+    static void PthreadCall(const char* label, int result) {
+  if (result != 0) {
+    fprintf(stderr, 'pthread %s: %s\n', label, strerror(result));
+    abort();
+  }
+}
+    
+    class FilterBlockReader {
+ public:
+ // REQUIRES: 'contents' and *policy must stay live while *this is live.
+  FilterBlockReader(const FilterPolicy* policy, const Slice& contents);
+  bool KeyMayMatch(uint64_t block_offset, const Slice& key);
+    }
+    
+    Status ReadBlock(RandomAccessFile* file,
+                 const ReadOptions& options,
+                 const BlockHandle& handle,
+                 BlockContents* result) {
+  result->data = Slice();
+  result->cachable = false;
+  result->heap_allocated = false;
+    }
+    
+    
+    
+    
+    
+        virtual void DrawString(int x, int y, const char* string, ...); 
+    
+    			b2BodyDef bd;
+			bd.type = b2_dynamicBody;
+			bd.angularDamping = 2.0f;
+			bd.linearDamping = 0.5f;
+    
+    
+    {		body2->SetAngularVelocity(m_angularVelocity);
+		body2->SetLinearVelocity(velocity2);
+	}
+    
+    			float32 x = 20.0f, y1 = 0.0f, dx = 5.0f;
+    
+    
+    {        // Store mount point
+        fs::path path(parts[1].begin(), parts[1].end());
+        struct stat st;
+        const int ret = stat(path.string().c_str(), &st);
+        if (ret == -1 && errno == ENOENT) {
+          return;
+        }
+        checkUnixError(ret, 'stat hugepage mountpoint failed');
+        pos->mountPoint = fs::canonical(path);
+        pos->device = st.st_dev;
+      };
+    
+    /**
+ * FileHandlerFactory is a LogHandlerFactory that constructs log handlers
+ * that write to a file.
+ *
+ * Note that FileHandlerFactory allows opening and appending to arbitrary files
+ * based on the handler options.  This may make it unsafe to use
+ * FileHandlerFactory in some contexts: for instance, a setuid binary should
+ * generally avoid registering the FileHandlerFactory if they allow log
+ * handlers to be configured via command line parameters, since otherwise this
+ * may allow non-root users to append to files that they otherwise would not
+ * have write permissions for.
+ */
+class FileHandlerFactory : public LogHandlerFactory {
+ public:
+  StringPiece getType() const override {
+    return 'file';
+  }
+    }
+    
+    /**
+ * A helper class for creating an AsyncFileWriter or ImmediateFileWriter based
+ * on log handler options settings.
+ *
+ * This is used by StreamHandlerFactory and FileHandlerFactory.
+ */
+class FileWriterFactory {
+ public:
+  bool processOption(StringPiece name, StringPiece value);
+  std::shared_ptr<LogWriter> createWriter(File file);
+    }
+    
+      /**
+   * Whether this category should inherit its effective log level from its
+   * parent category, if the parent category has a more verbose log level.
+   */
+  bool inheritParentLevel{true};
+    
+        // Parse the levelString into a LogLevel
+    levelString = trimWhitespace(levelString);
+    try {
+      categoryConfig.level = stringToLogLevel(levelString);
+    } catch (const std::exception&) {
+      throw LogConfigParseError{to<string>(
+          'invalid log level \'',
+          levelString,
+          '\' for category \'',
+          categoryName,
+          '\'')};
+    }
