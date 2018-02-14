cde8d26db51802d74cc3a13e80580398c8b18b4c
SUNsung <weatherius@gmail.com>
2018-02-14 16:20:40 +0200

    Rewrite [522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py] File in: 2018-02-14 16:20:39

--- pyton/522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py
+++ pyton/522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py
@@ -1,110 +1,180 @@
+
+        
+        versions_info = json.load(open('update/versions.json'))
+versions = list(versions_info['versions'].keys())
+versions.sort()
+    
+    options = helptext[helptext.index('  General Options:') + 19:]
+options = re.sub(r'(?m)^  (\w.+)$', r'## \1', options)
+options = '# OPTIONS\n' + options + '\n'
+    
+    
 if __name__ == '__main__':
     unittest.main()
 
     
-        return sorted(
-        filter(lambda ie: ie.is_suitable(age_limit), gen_extractors()),
-        key=lambda ie: ie.IE_NAME.lower())
+    from __future__ import unicode_literals
     
-                stream_url_hls = json_data.get('stream_url_hls')
-            if stream_url_hls:
-                formats.extend(self._extract_m3u8_formats(
-                    stream_url_hls, video_id, 'mp4',
-                    entry_protocol='m3u8_native', m3u8_id='hls', fatal=False))
+    # Allow direct execution
+import os
+import sys
+import unittest
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     
-            # Audiomack wraps a lot of soundcloud tracks in their branded wrapper
-        # if so, pass the work off to the soundcloud extractor
-        if SoundcloudIE.suitable(api_response['url']):
-            return {'_type': 'url', 'url': api_response['url'], 'ie_key': 'Soundcloud'}
     
-    from .common import InfoExtractor
-from ..utils import (
-    int_or_none,
-    parse_duration,
-    unified_strdate,
-)
+class RtspFD(FileDownloader):
+    def real_download(self, filename, info_dict):
+        url = info_dict['url']
+        self.report_destination(filename)
+        tmpfilename = self.temp_name(filename)
     
-            return info_dict
+    
+def get_info_extractor(ie_name):
+    '''Returns the info extractor class with the given ie_name'''
+    return globals()[ie_name + 'IE']
 
     
-        for node in doctree.traverse(settingslist_node):
-        settings_list = nodes.bullet_list()
-        settings_list.extend([make_setting_element(d, app, fromdocname)
-                              for d in sorted(env.scrapy_all_settings,
-                                              key=itemgetter('setting_name'))
-                              if fromdocname != d['docname']])
-        node.replace_self(settings_list)
+    import re
     
-                tested_methods = conman.tested_methods_from_spidercls(spidercls)
-            if opts.list:
-                for method in tested_methods:
-                    contract_reqs[spidercls.name].append(method)
-            elif tested_methods:
-                self.crawler_process.crawl(spidercls)
     
-            try:
-            self.max_bound = int(self.args[2])
-        except IndexError:
-            self.max_bound = float('inf')
+class AnySexIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)?anysex\.com/(?P<id>\d+)'
+    _TEST = {
+        'url': 'http://anysex.com/156592/',
+        'md5': '023e9fbb7f7987f5529a394c34ad3d3d',
+        'info_dict': {
+            'id': '156592',
+            'ext': 'mp4',
+            'title': 'Busty and sexy blondie in her bikini strips for you',
+            'description': 'md5:de9e418178e2931c10b62966474e1383',
+            'categories': ['Erotic'],
+            'duration': 270,
+            'age_limit': 18,
+        }
+    }
     
-    '''
+                for video_version in json_data.get('video_versions'):
+                video_version_url = video_version.get('download_url') or video_version.get('stream_url')
+                if not video_version_url:
+                    continue
+                f = {
+                    'url': video_version_url,
+                    'width': int_or_none(video_version.get('width')),
+                    'height': int_or_none(video_version.get('height')),
+                    'abr': int_or_none(video_version.get('audio_bitrate')),
+                    'vbr': int_or_none(video_version.get('video_bitrate')),
+                }
+                bitrate = self._search_regex(r'(\d+)k', video_version_url, 'bitrate', default=None)
+                if bitrate:
+                    f.update({
+                        'format_id': 'http-%s' % bitrate,
+                    })
+                formats.append(f)
+            self._sort_formats(formats)
+    
+    
+class ClipRsIE(OnetBaseIE):
+    _VALID_URL = r'https?://(?:www\.)?clip\.rs/(?P<id>[^/]+)/\d+'
+    _TEST = {
+        'url': 'http://www.clip.rs/premijera-frajle-predstavljaju-novi-spot-za-pesmu-moli-me-moli/3732',
+        'md5': 'c412d57815ba07b56f9edc7b5d6a14e5',
+        'info_dict': {
+            'id': '1488842.1399140381',
+            'ext': 'mp4',
+            'title': 'PREMIJERA Frajle predstavljaju novi spot za pesmu Moli me, moli',
+            'description': 'md5:56ce2c3b4ab31c5a2e0b17cb9a453026',
+            'duration': 229,
+            'timestamp': 1459850243,
+            'upload_date': '20160405',
+        }
+    }
+    
+        try:
+        s1 = os.lstat(path)
+    except OSError:
+        # the OSError should be handled with more care
+        # it could be a 'permission denied' but path is still a mount
+        return False
+    else:
+        # A symlink can never be a mount point
+        if os.path.stat.S_ISLNK(s1.st_mode):
+            return False
+    
+    from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+    
+            url = '%s/roles/?owner__username=%s&name=%s' % (self.baseurl, user_name, role_name)
+        data = self.__call_galaxy(url)
+        if len(data['results']) != 0:
+            return data['results'][0]
+        return None
+    
+    # Set parameters:
+# ngram_range = 2 will add bi-grams features
+ngram_range = 1
+max_features = 20000
+maxlen = 400
+batch_size = 32
+embedding_dims = 50
+epochs = 5
+    
+    x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+    
+    from __future__ import print_function
+    
+        # test lambda with output_shape lambda
+    input_a = layers.Input(shape=input_shapes[0][1:])
+    input_b = layers.Input(shape=input_shapes[1][1:])
+    merged = legacy_layers.merge(
+        [input_a, input_b],
+        mode=lambda tup: K.concatenate([tup[0], tup[1]]),
+        output_shape=lambda tup: tup[0][:-1] + (tup[0][-1] + tup[1][-1],))
+    model = models.Model([input_a, input_b], merged)
+    expected_output_shape = model.compute_output_shape(input_shapes)
+    actual_output_shape = model.predict(inputs).shape
+    assert expected_output_shape == actual_output_shape
+    
+                # check dropout
+            layer_test(convolutional_recurrent.ConvLSTM2D,
+                       kwargs={'data_format': data_format,
+                               'return_sequences': return_sequences,
+                               'filters': filters,
+                               'kernel_size': (num_row, num_col),
+                               'padding': 'same',
+                               'dropout': 0.1,
+                               'recurrent_dropout': 0.1},
+                       input_shape=inputs.shape)
+    
+    Because our custom layer is written with primitives from the Keras
+backend (`K`), our code can run both on TensorFlow and Theano.
+'''
+    
+    
+def downgrade():
+  with op.batch_alter_table('tables') as batch_op:
+    batch_op.drop_constraint('user_id', type_='foreignkey')
+    batch_op.drop_column('user_id')
+
     
     # revision identifiers, used by Alembic.
-revision = '1e2841a4128'
-down_revision = '5a7bad26f2a7'
+revision = '315b3f4da9b0'
+down_revision = '1a48a5411020'
     
-    '''
+    Revision ID: 4500485bde7d
+Revises: 41f6a59a61f2
+Create Date: 2016-09-12 23:33:14.789632
     
-    from alembic import op
-import sqlalchemy as sa
-from sqlalchemy_utils.types.encrypted import EncryptedType
-    
-    from alembic import op
-import sqlalchemy as sa
-    
-    reload(sys)  
-sys.setdefaultencoding('utf8')
-    
-        def __init__(self):
-        '''Initializes the future. Should not be called by clients.'''
-        self._condition = threading.Condition()
-        self._state = PENDING
-        self._result = None
-        self._exception = None
-        self._waiters = []
-        self._done_callbacks = []
-    
-    def main():
-    for name, fn in [('sequential',
-                      functools.partial(download_urls_sequential, URLS)),
-                     ('processes',
-                      functools.partial(download_urls_with_executor,
-                                        URLS,
-                                        ProcessPoolExecutor(10))),
-                     ('threads',
-                      functools.partial(download_urls_with_executor,
-                                        URLS,
-                                        ThreadPoolExecutor(10)))]:
-        sys.stdout.write('%s: ' % name.ljust(12))
-        start = time.time()
-        url_map = fn()
-        sys.stdout.write('%.2f seconds (%d of %d downloaded)\n' %
-                         (time.time() - start, len(url_map), len(URLS)))
-    
-    # The name of an image file (relative to this directory) to place at the top of
-# the title page.
-#latex_logo = None
+            mp3 = match1(content, r'name='filename'\s*value='([^']+\.mp3)'')
+        if mp3: mp3 = 'http://res.infoq.com/downloads/mp3downloads/%s' % mp3
     
     
-def _ValidateResponseObject( response ):
-  our_hmac = CreateHmac( response.content, BaseRequest.hmac_secret )
-  their_hmac = ToBytes( b64decode( response.headers[ _HMAC_HEADER ] ) )
-  if not SecureBytesEqual( our_hmac, their_hmac ):
-    raise RuntimeError( 'Received invalid HMAC for response!' )
-  return True
-    
-    
-class DiagnosticFilter( object ):
-  def __init__( self, config_or_filters ):
-    if isinstance( config_or_filters, list ):
-      self._filters = config_or_filters
+def main():
+    while True:
+        pull_screenshot()
+        im = Image.open('./1.png')
