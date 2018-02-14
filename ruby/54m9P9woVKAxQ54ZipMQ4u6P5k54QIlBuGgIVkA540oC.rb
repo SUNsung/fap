b47a42e90492323243675bbdaefa030b10e910fc
SUNsung <weatherius@gmail.com>
2018-02-15 01:04:05 +0200

    Rewrite [54m9P9woVKAxQ54ZipMQ4u6P5k54QIlBuGgIVkA540oC.rb] File in: 2018-02-15 01:04:05

--- ruby/54m9P9woVKAxQ54ZipMQ4u6P5k54QIlBuGgIVkA540oC.rb
+++ ruby/54m9P9woVKAxQ54ZipMQ4u6P5k54QIlBuGgIVkA540oC.rb
@@ -1,45 +1,27 @@
 
         
-                      s[:proto]='pop3'
-              s[:extra]='Failed Login. Banner: #{s[:banner]}'
-              report_auth_info(s)
-              print_status('Invalid POP3 Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]} (#{s[:banner].strip})')
-              s[:pass]=''
-          end
-        when nil
-          # No matches, no saved state
-        else
-          s[:last]=matched
-          sessions[s[:session]].merge!({k => matches})
-      end # end case matched
-    end # end of each_key
-  end # end of parse
-end
-    
-    sock = TCPSocket.new(ip, port)
-    
-    clsJavaCompile 	= Rjb::import('javaCompile.CompileSourceInMemory')
-clsCreateJar	= Rjb::import('javaCompile.CreateJarFile')
-clsFile			= Rjb::import('java.io.File')
-system			= Rjb::import('java.lang.System')
-#clsString	= Rjb::import('java.lang.String')
-    
-        _cal[ver].each_pair do |key, value|
-      cal[ver][key] = Array.new
-      cal[ver][key] << String.new
-      cal[ver][key][-1] << '#ifdef AIX%s' % ver.delete('.')
-      cal[ver][key][-1] << '\n'
-      cal[ver][key][-1] << '''.rjust(5)
-      value.each_byte do |c|
-        cal[ver][key][-1] << '\x%02x' % c
+              new_theme_name = args.join('_')
+      theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
+      if theme.path.exist?
+        Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.'
       end
-      cal[ver][key][-1] << '''.ljust(7)
-      cal[ver][key][-1] << '/*  cal     r2,-%d(r29)' %
-          (65536 - value.unpack('nn')[1])
-      cal[ver][key][-1] << '*/'.rjust(15)
-      cal[ver][key][-1] << '\n'
-      cal[ver][key][-1] << '#endif'
-      cal[ver][key][-1] << '\n'
+    
+          process(name)
+      read_yaml(base, name)
     end
     
-    	attr_accessor :file, :block, :block_size
+        if f.keg_only?
+      keg_site_packages = f.opt_prefix/'lib/python2.7/site-packages'
+      unless Language::Python.in_sys_path?('python', keg_site_packages)
+        s = <<-EOS.undent
+          If you need Python to find bindings for this keg-only formula, run:
+            echo #{keg_site_packages} >> #{homebrew_site_packages/f.name}.pth
+        EOS
+        s += instructions unless Language::Python.reads_brewed_pth_files?('python')
+      end
+      return s
+    end
+    
+      def origin
+    Homebrew.git_origin || '(none)'
+  end
