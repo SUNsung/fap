74d91051ee3b75a1cce6e7adac7b93f6202eefa0
SUNsung <weatherius@gmail.com>
2018-02-14 08:11:19 +0200

    Rewrite [34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp] File in: 2018-02-14 08:11:19

--- cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
+++ cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
@@ -1,173 +1,110 @@
 
         
-          void Compute(OpKernelContext* context) override LOCKS_EXCLUDED(mu_) {
-    mutex_lock l(mu_);
-    if (resource_ == nullptr) {
-      ResourceMgr* mgr = context->resource_manager();
-      OP_REQUIRES_OK(context, cinfo_.Init(mgr, def()));
-    }
-    }
-    
-    #endif  // TENSORFLOW_STREAM_EXECUTOR_CUDA_CUDA_EVENT_H_
+        #endif // BOOST_ASIO_DETAIL_ATOMIC_COUNT_HPP
 
     
-    // Prefetching support
-//
-// Defined behavior on some of the uarchs:
-// PREFETCH_HINT_T0:
-//   prefetch to all levels of the hierarchy (except on p4: prefetch to L2)
-// PREFETCH_HINT_NTA:
-//   p4: fetch to L2, but limit to 1 way (out of the 8 ways)
-//   core: skip L2, go directly to L1
-//   k8 rev E and later: skip L2, can go to either of the 2-ways in L1
-enum PrefetchHint {
-  PREFETCH_HINT_T0 = 3,  // More temporal locality
-  PREFETCH_HINT_T1 = 2,
-  PREFETCH_HINT_T2 = 1,  // Less temporal locality
-  PREFETCH_HINT_NTA = 0  // No temporal locality
-};
-template <PrefetchHint hint>
-void prefetch(const void* x);
+      // Constructor.
+  explicit buffered_stream_storage(std::size_t buffer_capacity)
+    : begin_offset_(0),
+      end_offset_(0),
+      buffer_(buffer_capacity)
+  {
+  }
     
-    class TFRecordReader : public ReaderBase {
- public:
-  TFRecordReader(const string& node_name, const string& compression_type,
-                 Env* env)
-      : ReaderBase(strings::StrCat('TFRecordReader '', node_name, ''')),
-        env_(env),
-        offset_(0),
-        compression_type_(compression_type) {}
-    }
-    
-      // If a blob with the same bounding box as one of the truth character
-  // bounding boxes is not classified as the corresponding truth character
-  // blames character classifier for incorrect answer.
-  void BlameClassifier(const UNICHARSET& unicharset,
-                       const TBOX& blob_box,
-                       const BLOB_CHOICE_LIST& choices,
-                       bool debug);
-    
-    // This class fits a line to a set of ICOORD points.
-// There is no restriction on the direction of the line, as it
-// uses a vector method, ie no concern over infinite gradients.
-// The fitted line has the least upper quartile of squares of perpendicular
-// distances of all source points from the line, subject to the constraint
-// that the line is made from one of the pairs of [{p1,p2,p3},{pn-2, pn-1, pn}]
-// i.e. the 9 combinations of one of the first 3 and last 3 points.
-// A fundamental assumption of this algorithm is that one of the first 3 and
-// one of the last 3 points are near the best line fit.
-// The points must be Added in line order for the algorithm to work properly.
-// No floating point calculations are needed* to make an accurate fit,
-// and no random numbers are needed** so the algorithm is deterministic,
-// architecture-stable, and compiler-stable as well as stable to minor
-// changes in the input.
-// *A single floating point division is used to compute each line's distance.
-// This is unlikely to result in choice of a different line, but if it does,
-// it would be easy to replace with a 64 bit integer calculation.
-// **Random numbers are used in the nth_item function, but the worst
-// non-determinism that can result is picking a different result among equals,
-// and that wouldn't make any difference to the end-result distance, so the
-// randomness does not affect the determinism of the algorithm. The random
-// numbers are only there to guarantee average linear time.
-// Fitting time is linear, but with a high constant, as it tries 9 different
-// lines and computes the distance of all points each time.
-// This class is aimed at replacing the LLSQ (linear least squares) and
-// LMS (least median of squares) classes that are currently used for most
-// of the line fitting in Tesseract.
-class DetLineFit {
- public:
-  DetLineFit();
-  ~DetLineFit();
-    }
+      STDMETHODIMP get_Capacity(UINT32* value)
+  {
+    *value = capacity_;
+    return S_OK;
+  }
     
     
-    {  result.xcoord = -src.ycoord;
-  result.ycoord = src.xcoord;
-  return result;
+    {  bool first = op_queue_[op_type].enqueue_operation(descriptor, op);
+  io_service_.work_started();
+  if (first)
+  {
+    ::pollfd& ev = add_pending_event_change(descriptor);
+    ev.events = POLLERR | POLLHUP;
+    if (op_type == read_op
+        || op_queue_[read_op].has_operation(descriptor))
+      ev.events |= POLLIN;
+    if (op_type == write_op
+        || op_queue_[write_op].has_operation(descriptor))
+      ev.events |= POLLOUT;
+    if (op_type == except_op
+        || op_queue_[except_op].has_operation(descriptor))
+      ev.events |= POLLPRI;
+    interrupter_.interrupt();
+  }
 }
     
-    #include 'points.h'
-#include 'params.h'
     
-    // Fills in the x-height range accepted by the given unichar_id, given its
-// bounding box in the usual baseline-normalized coordinates, with some
-// initial crude x-height estimate (such as word size) and this denoting the
-// transformation that was used.
-void DENORM::XHeightRange(int unichar_id, const UNICHARSET& unicharset,
-                          const TBOX& bbox,
-                          float* min_xht, float* max_xht, float* yshift) const {
-  // Default return -- accept anything.
-  *yshift = 0.0f;
-  *min_xht = 0.0f;
-  *max_xht = MAX_FLOAT32;
+    
+    
+    
+    
+    
+    
+    
+    
+    {    CHECK_GL_ERROR_DEBUG();
+}
+    
+    	void Step(Settings* settings)
+	{
+		// Drive the kinematic body.
+		if (m_platform->GetType() == b2_kinematicBody)
+		{
+			b2Vec2 p = m_platform->GetTransform().p;
+			b2Vec2 v = m_platform->GetLinearVelocity();
+    }
     }
     
-     private:
-  friend class VersionSet;
-    
-      ASSERT_TRUE(! Overlaps('a', 'b'));
-  ASSERT_TRUE(! Overlaps('z1', 'z2'));
-  ASSERT_TRUE(Overlaps('a', 'p'));
-  ASSERT_TRUE(Overlaps('a', 'q'));
-  ASSERT_TRUE(Overlaps('a', 'z'));
-  ASSERT_TRUE(Overlaps('p', 'p1'));
-  ASSERT_TRUE(Overlaps('p', 'q'));
-  ASSERT_TRUE(Overlaps('p', 'z'));
-  ASSERT_TRUE(Overlaps('p1', 'p2'));
-  ASSERT_TRUE(Overlaps('p1', 'z'));
-  ASSERT_TRUE(Overlaps('q', 'q'));
-  ASSERT_TRUE(Overlaps('q', 'q1'));
-    
-    // WriteBatchInternal provides static methods for manipulating a
-// WriteBatch that we don't want in the public WriteBatch interface.
-class WriteBatchInternal {
- public:
-  // Return the number of entries in the batch.
-  static int Count(const WriteBatch* batch);
-    }
-    
-        // Change journal mode to WAL if WAL enabled flag is on
-    if (FLAGS_WAL_enabled) {
-      std::string WAL_stmt = 'PRAGMA journal_mode = WAL';
-    }
-    
-        if (bytes_ > 0) {
-      char rate[100];
-      snprintf(rate, sizeof(rate), '%6.1f MB/s',
-               (bytes_ / 1048576.0) / (finish - start_));
-      if (!message_.empty()) {
-        message_  = std::string(rate) + ' ' + message_;
-      } else {
-        message_ = rate;
-      }
-    }
-    
-    // Returns a new environment that stores its data in memory and delegates
-// all non-file-storage tasks to base_env. The caller must delete the result
-// when it is no longer needed.
-// *base_env must remain live while the result is in use.
-Env* NewMemEnv(Env* base_env);
-    
-      // Check that the directory is empty.
-  ASSERT_TRUE(!env_->FileExists('/dir/non_existent'));
-  ASSERT_TRUE(!env_->GetFileSize('/dir/non_existent', &file_size).ok());
-  ASSERT_OK(env_->GetChildren('/dir', &children));
-  ASSERT_EQ(0, children.size());
-    
-    Mutex::~Mutex() { PthreadCall('destroy mutex', pthread_mutex_destroy(&mu_)); }
-    
-    namespace {
-class MergingIterator : public Iterator {
- public:
-  MergingIterator(const Comparator* comparator, Iterator** children, int n)
-      : comparator_(comparator),
-        children_(new IteratorWrapper[n]),
-        n_(n),
-        current_(NULL),
-        direction_(kForward) {
-    for (int i = 0; i < n; i++) {
-      children_[i].Set(children[i]);
-    }
+      // Returns an index of random unit from the corpus to mutate.
+  // Hypothesis: units added to the corpus last are more likely to be
+  // interesting. This function gives more weight to the more recent units.
+  size_t ChooseUnitIdxToMutate(Random &Rand) {
+    size_t Idx = static_cast<size_t>(CorpusDistribution(Rand.Get_mt19937()));
+    assert(Idx < Inputs.size());
+    return Idx;
   }
+    
+    
+    {private:
+  DictionaryEntry DE[kMaxDictSize];
+  size_t Size = 0;
+};
+    
+    
+    {  // One greedy pass: add the file's features to AllFeatures.
+  // If new features were added, add this file to NewFiles.
+  for (size_t i = NumFilesInFirstCorpus; i < Files.size(); i++) {
+    auto &Cur = Files[i].Features;
+    // Printf('%s -> sz %zd ft %zd\n', Files[i].Name.c_str(),
+    //       Files[i].Size, Cur.size());
+    size_t OldSize = AllFeatures.size();
+    AllFeatures.insert(Cur.begin(), Cur.end());
+    if (AllFeatures.size() > OldSize)
+      NewFiles->push_back(Files[i].Name);
+  }
+  return AllFeatures.size() - InitialNumFeatures;
+}
+    
+      /// Mutates data by adding a word from the manual dictionary.
+  size_t Mutate_AddWordFromManualDictionary(uint8_t *Data, size_t Size,
+                                            size_t MaxSize);
+    
+    TracePC TPC;
+    
+    
+    {}  // namespace fuzzer
+
+    
+    namespace aria2 {
     }
-    }
+    
+      virtual bool getInitialOption() const CXX11_OVERRIDE;
+    
+    
+    {} // namespace aria2
+    
+    #endif // D_ANON_DISK_WRITER_FACTORY_H
