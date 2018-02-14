3d6f71e93a5e4f0bd27a7df3c0511cf81fd4dbf4
SUNsung <weatherius@gmail.com>
2018-02-14 13:22:29 +0200

    Rewrite [34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp] File in: 2018-02-14 13:22:29

--- cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
+++ cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
@@ -1,110 +1,136 @@
 
         
-        #endif // BOOST_ASIO_DETAIL_ATOMIC_COUNT_HPP
-
-    
-      // Constructor.
-  explicit buffered_stream_storage(std::size_t buffer_capacity)
-    : begin_offset_(0),
-      end_offset_(0),
-      buffer_(buffer_capacity)
-  {
-  }
-    
-      STDMETHODIMP get_Capacity(UINT32* value)
-  {
-    *value = capacity_;
-    return S_OK;
-  }
-    
-    
-    {  bool first = op_queue_[op_type].enqueue_operation(descriptor, op);
-  io_service_.work_started();
-  if (first)
-  {
-    ::pollfd& ev = add_pending_event_change(descriptor);
-    ev.events = POLLERR | POLLHUP;
-    if (op_type == read_op
-        || op_queue_[read_op].has_operation(descriptor))
-      ev.events |= POLLIN;
-    if (op_type == write_op
-        || op_queue_[write_op].has_operation(descriptor))
-      ev.events |= POLLOUT;
-    if (op_type == except_op
-        || op_queue_[except_op].has_operation(descriptor))
-      ev.events |= POLLPRI;
-    interrupter_.interrupt();
-  }
-}
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    {    CHECK_GL_ERROR_DEBUG();
-}
-    
-    	void Step(Settings* settings)
-	{
-		// Drive the kinematic body.
-		if (m_platform->GetType() == b2_kinematicBody)
-		{
-			b2Vec2 p = m_platform->GetTransform().p;
-			b2Vec2 v = m_platform->GetLinearVelocity();
-    }
-    }
-    
-      // Returns an index of random unit from the corpus to mutate.
-  // Hypothesis: units added to the corpus last are more likely to be
-  // interesting. This function gives more weight to the more recent units.
-  size_t ChooseUnitIdxToMutate(Random &Rand) {
-    size_t Idx = static_cast<size_t>(CorpusDistribution(Rand.Get_mt19937()));
-    assert(Idx < Inputs.size());
-    return Idx;
-  }
-    
-    
-    {private:
-  DictionaryEntry DE[kMaxDictSize];
-  size_t Size = 0;
+        enum CommentPlacement {
+  commentBefore = 0,      ///< a comment placed on the line before a value
+  commentAfterOnSameLine, ///< a comment just after a value on the same line
+  commentAfter, ///< a comment on the line after a value (only make sense for
+  /// root value)
+  numberOfCommentPlacement
 };
     
+    bool Value::isNull() const { return type_ == nullValue; }
     
-    {  // One greedy pass: add the file's features to AllFeatures.
-  // If new features were added, add this file to NewFiles.
-  for (size_t i = NumFilesInFirstCorpus; i < Files.size(); i++) {
-    auto &Cur = Files[i].Features;
-    // Printf('%s -> sz %zd ft %zd\n', Files[i].Name.c_str(),
-    //       Files[i].Size, Cur.size());
-    size_t OldSize = AllFeatures.size();
-    AllFeatures.insert(Cur.begin(), Cur.end());
-    if (AllFeatures.size() > OldSize)
-      NewFiles->push_back(Files[i].Name);
-  }
-  return AllFeatures.size() - InitialNumFeatures;
+    #include <google/protobuf/compiler/code_generator.h>
+#include <google/protobuf/compiler/plugin.h>
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/descriptor.pb.h>
+#include <google/protobuf/io/printer.h>
+#include <google/protobuf/io/zero_copy_stream.h>
+#include <google/protobuf/stubs/strutil.h>
+    
+    
+    { private:
+  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(MapFieldGenerator);
+};
+    
+    const Options* SourceGeneratorBase::options() {
+  return this->options_;
 }
     
-      /// Mutates data by adding a word from the manual dictionary.
-  size_t Mutate_AddWordFromManualDictionary(uint8_t *Data, size_t Size,
-                                            size_t MaxSize);
-    
-    TracePC TPC;
-    
-    
-    {}  // namespace fuzzer
-
-    
-    namespace aria2 {
+    namespace google {
+namespace protobuf {
+  namespace io {
+    class Printer;             // printer.h
+  }
+}
     }
     
-      virtual bool getInitialOption() const CXX11_OVERRIDE;
+     private:
+  const FieldDescriptor* descriptor_;
+  Context* context_;
+  ClassNameResolver* name_resolver_;
+  string scope_;
     
     
-    {} // namespace aria2
+    {  // Calculate num_trailing and trailing_certainty.
+  for (*trailing_certainty = 0.0f, *num_rebuilt_trailing = 0;
+       *num_rebuilt_trailing < trailing_outliers;
+       (*num_rebuilt_trailing)++) {
+    int blob_idx = num_blobs - 1 - *num_rebuilt_trailing;
+    float char_certainty = word->best_choice->certainty(blob_idx);
+    if (char_certainty > *unlikely_threshold) {
+      break;
+    }
+    if (char_certainty < *trailing_certainty) {
+      *trailing_certainty = char_certainty;
+    }
+  }
+}
     
-    #endif // D_ANON_DISK_WRITER_FACTORY_H
+      // Sets up the norm_truth_word from truth_word using the given DENORM.
+  void SetupNormTruthWord(const DENORM& denorm);
+    
+    
+    {  name += UNLV_EXT;              //add extension
+  if ((pdfp = fopen (name.string (), 'rb')) == NULL) {
+    return false;                //didn't read one
+  } else {
+    while (tfscanf(pdfp, '%d %d %d %d %*s', &x, &y, &width, &height) >= 4) {
+                                 //make rect block
+      block = new BLOCK (name.string (), TRUE, 0, 0,
+                         (inT16) x, (inT16) (ysize - y - height),
+                         (inT16) (x + width), (inT16) (ysize - y));
+                                 //on end of list
+      block_it.add_to_end (block);
+    }
+    fclose(pdfp);
+  }
+  return true;
+}
+    
+    
+    {    ICOORD pt;
+    int halfwidth;
+  };
+  // Type holds the distance of each point from the fitted line and the point
+  // itself. Use of double allows integer distances from ICOORDs to be stored
+  // exactly, and also the floating point results from ConstrainedFit.
+  typedef KDPairInc<double, ICOORD> DistPointPair;
+    
+    double LLSQ::m() const {  // get gradient
+  double covar = covariance();
+  double x_var = x_variance();
+  if (x_var != 0.0)
+    return covar / x_var;
+  else
+    return 0.0;                    // too little
+}
+    
+    
+/**********************************************************************
+ * ROW::ROW
+ *
+ * Constructor to build a ROW. Only the stats stuff are given here.
+ * The words are added directly.
+ **********************************************************************/
+    
+        void close() {
+        loop->post([this]() {
+            delete this;
+        });
+    }
+    
+    
+    {        delete transferData;
+        transferCb(s);
+    }
+    
+            uv_os_sock_t listenFd = SOCKET_ERROR;
+        addrinfo *listenAddr;
+        if ((options & uS::ONLY_IPV4) == 0) {
+            for (addrinfo *a = result; a && listenFd == SOCKET_ERROR; a = a->ai_next) {
+                if (a->ai_family == AF_INET6) {
+                    listenFd = netContext->createSocket(a->ai_family, a->ai_socktype, a->ai_protocol);
+                    listenAddr = a;
+                }
+            }
+        }
+    
+    template <bool isServer>
+struct WIN32_EXPORT Group : protected uS::NodeData {
+protected:
+    friend struct Hub;
+    friend struct WebSocket<isServer>;
+    friend struct HttpSocket<false>;
+    friend struct HttpSocket<true>;
+    }
