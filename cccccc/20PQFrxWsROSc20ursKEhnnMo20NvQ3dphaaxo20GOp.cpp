67e5d6383ed37b3869d90d55feb2ef00ca961e18
SUNsung <weatherius@gmail.com>
2018-02-15 08:46:06 +0200

    Rewrite [20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp] File in: 2018-02-15 08:46:05

--- cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
+++ cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
@@ -1,238 +1,66 @@
 
         
-        #endif  // TENSORFLOW_DEBUGGER_STATE_IMPL_H_
-
+          /// DebuggerClient is consulted at two times during name
+  /// lookup.  This is the first time: after all names in a
+  /// source file have been checked but before external
+  /// Modules are checked.  The results in the ResultVector will
+  /// be consulted first.  Return true if results have been added
+  /// to RV.
+  /// FIXME: I don't think this ever does anything useful.
+  virtual bool lookupOverrides(DeclBaseName Name, DeclContext *DC,
+                               SourceLoc Loc, bool IsTypeLookup,
+                               ResultVector &RV) = 0;
     
-      // During the first Compute(), resource is either created or looked up using
-  // shared_name. In the latter case, the resource found should be verified if
-  // it is compatible with this op's configuration. The verification may fail in
-  // cases such as two graphs asking queues of the same shared name to have
-  // inconsistent capacities.
-  virtual Status VerifyResource(T* resource) { return Status::OK(); }
+      /// The list of regular users from the last run of the checker.
+  llvm::SmallVector<SILInstruction *, 16> LifetimeEndingUsers;
     
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-      // Create parent/child pipes for the specified channels and make the
-  // parent-side of the pipes non-blocking.
-  for (int i = 0; i < kNFds; i++) {
-    if (action_[i] == ACTION_PIPE) {
-      int pipe_fds[2];
-      if (pipe(pipe_fds) < 0) {
-        LOG(ERROR) << 'Start cannot create pipe: ' << strerror(errno);
-        ClosePipes();
-        return false;
-      }
-      // Handle the direction of the pipe (stdin vs stdout/err).
-      if (i == 0) {
-        parent_pipe_[i] = pipe_fds[1];
-        child_pipe_[i] = pipe_fds[0];
-      } else {
-        parent_pipe_[i] = pipe_fds[0];
-        child_pipe_[i] = pipe_fds[1];
-      }
-    }
+        // Extra copy values that we should visit recursively.
+    llvm::SmallVector<CopyValueInst *, 8> NewCopyInsts;
+    llvm::SmallVector<SILInstruction *, 8> NewBorrowInsts;
+    if (!all_of(Checker.RegularUsers, [&](SILInstruction *I) -> bool {
+          if (auto *CVI = dyn_cast<CopyValueInst>(I)) {
+            NewCopyInsts.push_back(CVI);
+            return true;
+          }
     }
     
-        // Validate output_index
-    if (n->num_outputs() == 0) {
-      return errors::InvalidArgument(
-          'Tried to fetch data for '', t,
-          '', which produces no output.  To run to a node but not fetch any '
-          'data, pass '',
-          t,
-          '' as an argument to the 'target_node_names' argument of the '
-          'Session::Run API.');
-    } else if (id.second >= n->num_outputs()) {
-      return errors::InvalidArgument('FetchOutputs ', t,
-                                     ': output index too large, must be < ',
-                                     n->num_outputs());
-    }
-    
-    #ifndef TENSORFLOW_PLATFORM_PREFETCH_H_
-#define TENSORFLOW_PLATFORM_PREFETCH_H_
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    class TFRecordReaderOp : public ReaderOpKernel {
- public:
-  explicit TFRecordReaderOp(OpKernelConstruction* context)
-      : ReaderOpKernel(context) {
-    Env* env = context->env();
-    }
-    }
-    
-    #include <map>
-#include <string>
-    
-    namespace IPC {
-class Message;
-}
-    
-    #ifndef ATOM_BROWSER_NET_HTTP_PROTOCOL_HANDLER_H_
-#define ATOM_BROWSER_NET_HTTP_PROTOCOL_HANDLER_H_
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(URLRequestStringJob);
-};
-    
-    
-    {}  // namespace relauncher
-
-    
-    static void TestEncodeDecode(const VersionEdit& edit) {
-  std::string encoded, encoded2;
-  edit.EncodeTo(&encoded);
-  VersionEdit parsed;
-  Status s = parsed.DecodeFrom(encoded);
-  ASSERT_TRUE(s.ok()) << s.ToString();
-  parsed.EncodeTo(&encoded2);
-  ASSERT_EQ(encoded, encoded2);
-}
-    
-    TEST(FindFileTest, Empty) {
-  ASSERT_EQ(0, Find('foo'));
-  ASSERT_TRUE(! Overlaps('a', 'z'));
-  ASSERT_TRUE(! Overlaps(NULL, 'z'));
-  ASSERT_TRUE(! Overlaps('a', NULL));
-  ASSERT_TRUE(! Overlaps(NULL, NULL));
-}
-    
-    #include <vector>
-    
-    namespace caffe {
-    }
-    
-    
-    {}  // namespace caffe
-    
-    
-    {}  // namespace caffe
-    
-      /**
-   * @brief Computes the error gradient w.r.t. the forwarded inputs.
-   *
-   * @param top output Blob vector (length 1+), providing the error gradient with
-   *        respect to the outputs
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2+), into which the top error
-   *        gradient is copied
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-      DBWrapper* db_wrapper = ObjectWrap::Unwrap<DBWrapper>(args.This());
-  rocksdb::Slice begin     = *v8::String::Utf8Value(args[0]->ToString());
-  rocksdb::Slice end       = *v8::String::Utf8Value(args[1]->ToString());
-  db_wrapper->status_    = db_wrapper->db_->CompactRange(&end, &begin);
-    
-     public: // Insert/Update
-  /// Insert value before/after pivot in (list: key). Return the length.
-  /// May throw RedisListException
-  int InsertBefore(const std::string& key, const std::string& pivot,
-                   const std::string& value);
-  int InsertAfter(const std::string& key, const std::string& pivot,
-                  const std::string& value);
-    
-        // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them. 
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple. 
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Read 'misc/fonts/README.txt' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Roboto-Medium.ttf', 16.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Cousine-Regular.ttf', 15.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/DroidSans.ttf', 16.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/ProggyTiny.ttf', 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF('c:\\Windows\\Fonts\\ArialUni.ttf', 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
-    
-        // Main loop
-    MSG msg;
-    ZeroMemory(&msg, sizeof(msg));
-    while (msg.message != WM_QUIT)
-    {
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-        if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-            continue;
-        }
-        ImGui_ImplDX10_NewFrame();
-    }
-    
-    // Use if you want to reset your rendering device without losing ImGui state.
-IMGUI_API void        ImGui_Marmalade_InvalidateDeviceObjects();
-IMGUI_API bool        ImGui_Marmalade_CreateDeviceObjects();
-    
-    // Use if you want to reset your rendering device without losing ImGui state.
-IMGUI_API void        ImGui_ImplSdlGL2_InvalidateDeviceObjects();
-IMGUI_API bool        ImGui_ImplSdlGL2_CreateDeviceObjects();
-
-    
-        g_AttribLocationTex = glGetUniformLocation(g_ShaderHandle, 'Texture');
-    g_AttribLocationProjMtx = glGetUniformLocation(g_ShaderHandle, 'ProjMtx');
-    g_AttribLocationPosition = glGetAttribLocation(g_ShaderHandle, 'Position');
-    g_AttribLocationUV = glGetAttribLocation(g_ShaderHandle, 'UV');
-    g_AttribLocationColor = glGetAttribLocation(g_ShaderHandle, 'Color');
-    
-    void DupAndCloseStderr() {
-  int OutputFd = DuplicateFile(2);
-  if (OutputFd > 0) {
-    FILE *NewOutputFile = OpenFile(OutputFd, 'w');
-    if (NewOutputFile) {
-      OutputFile = NewOutputFile;
-      if (EF->__sanitizer_set_report_fd)
-        EF->__sanitizer_set_report_fd(reinterpret_cast<void *>(OutputFd));
-      CloseFile(2);
-    }
+      ArrayRef<const MarkupASTNode *> getChildren() const {
+    return {getTrailingObjects<MarkupASTNode *>(), NumChildren};
   }
-}
     
-    // The control file example:
-//
-// 3 # The number of inputs
-// 1 # The number of inputs in the first corpus, <= the previous number
-// file0
-// file1
-// file2  # One file name per line.
-// STARTED 0 123  # FileID, file size
-// DONE 0 1 4 6 8  # FileID COV1 COV2 ...
-// STARTED 1 456  # If DONE is missing, the input crashed while processing.
-// STARTED 2 567
-// DONE 2 8 9
-bool Merger::Parse(std::istream &IS, bool ParseCoverage) {
-  LastFailure.clear();
-  std::string Line;
+    /// Options for controlling diagnostics.
+class DiagnosticOptions {
+public:
+  /// Indicates whether textual diagnostics should use color.
+  bool UseColor = false;
     }
     
+    /// \brief Diagnostic consumer that displays diagnostics to standard error.
+class PrintingDiagnosticConsumer : public DiagnosticConsumer {
+  llvm::raw_ostream &Stream;
+  bool ForceColors = false;
+  bool DidErrorOccur = false;
+public:
+  PrintingDiagnosticConsumer(llvm::raw_ostream &stream = llvm::errs()) :
+    Stream(stream) { }
+    }
     
-    {}  // namespace fuzzer
-    
-      void HandleTrace(uint32_t *guard, uintptr_t PC);
-  void HandleInit(uint32_t *start, uint32_t *stop);
-  void HandleCallerCallee(uintptr_t Caller, uintptr_t Callee);
-  void HandleValueProfile(size_t Value) { ValueProfileMap.AddValue(Value); }
-  template <class T> void HandleCmp(void *PC, T Arg1, T Arg2);
-  size_t GetTotalPCCoverage();
-  void SetUseCounters(bool UC) { UseCounters = UC; }
-  void SetUseValueProfile(bool VP) { UseValueProfile = VP; }
-  void SetPrintNewPCs(bool P) { DoPrintNewPCs = P; }
-  template <class Callback> size_t CollectFeatures(Callback CB);
-  bool UpdateValueProfileMap(ValueBitMap *MaxValueProfileMap) {
-    return UseValueProfile && MaxValueProfileMap->MergeFrom(ValueProfileMap);
+    public:
+  void setInputBufferIDs(ArrayRef<unsigned> BufferIDs) {
+    InputBufIDs.append(BufferIDs.begin(), BufferIDs.end());
+    std::sort(InputBufIDs.begin(), InputBufIDs.end());
   }
+    
+        HHVM_FE(fb_setprofile);
+    HHVM_FE(xhprof_frame_begin);
+    HHVM_FE(xhprof_frame_end);
+    HHVM_FE(xhprof_enable);
+    HHVM_FE(xhprof_disable);
+    HHVM_FE(xhprof_network_enable);
+    HHVM_FE(xhprof_network_disable);
+    HHVM_FE(xhprof_sample_enable);
+    HHVM_FE(xhprof_sample_disable);
+    
+    public:
+  explicit Timer(Type type, const char *name = nullptr, ReportType r = Log);
+  ~Timer();
