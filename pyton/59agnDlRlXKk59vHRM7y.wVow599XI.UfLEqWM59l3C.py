1bb0b5913212cc8083be6838805801d005ffd36f
SUNsung <weatherius@gmail.com>
2018-02-16 17:55:14 +0200

    Rewrite [59agnDlRlXKk59vHRM7y.wVow599XI.UfLEqWM59l3C.py] File in: 2018-02-16 17:55:14

--- pyton/59agnDlRlXKk59vHRM7y.wVow599XI.UfLEqWM59l3C.py
+++ pyton/59agnDlRlXKk59vHRM7y.wVow599XI.UfLEqWM59l3C.py
@@ -1,164 +1,143 @@
 
         
-        DEFAULT_REDIRECT_LIMIT = 30
-CONTENT_CHUNK_SIZE = 10 * 1024
-ITER_CHUNK_SIZE = 512
-    
-            assert server.handler_results[0] == data
-    
-    import sys
+        
+def get(url, params=None, **kwargs):
+    r'''Sends a GET request.
     
     
-class InvalidHeader(RequestException, ValueError):
-    '''The header value provided was somehow invalid.'''
+class CaseInsensitiveDict(collections.MutableMapping):
+    '''A case-insensitive ``dict``-like object.
     
-        scikit_results = []
-    glmnet_results = []
-    n = 20
-    step = 500
-    n_features = 1000
-    n_informative = n_features / 10
-    n_test_samples = 1000
-    for i in range(1, n + 1):
-        print('==================')
-        print('Iteration %s of %s' % (i, n))
-        print('==================')
+            with server as address:
+            sock = socket.socket()
+            sock.connect(address)
+            time.sleep(1.5)
+            sock.sendall(b'hehehe, not received')
+            sock.close()
     
-        for ns in n_samples:
-        for nf in n_features:
-            it += 1
-            print('==================')
-            print('Iteration %s of %s' % (it, max(len(n_samples),
-                                          len(n_features))))
-            print('==================')
-            n_informative = nf // 10
-            X, Y, coef_ = make_regression(n_samples=ns, n_features=nf,
-                                          n_informative=n_informative,
-                                          noise=0.1, coef=True)
+        def update(self, other):
+        '''Updates this jar with cookies from another CookieJar or dict-like'''
+        if isinstance(other, cookielib.CookieJar):
+            for cookie in other:
+                self.set_cookie(copy.copy(cookie))
+        else:
+            super(RequestsCookieJar, self).update(other)
     
-                short_text_filename = os.path.join(short_text_lang_folder,
-                                               '%s_%04d.txt' % (lang, j))
-            print('Writing %s' % short_text_filename)
-            open(short_text_filename, 'wb').write(
-                small_content.encode('utf-8', 'ignore'))
-            j += 1
-            if j >= 1000:
-                break
+        # Check urllib3 for compatibility.
+    major, minor, patch = urllib3_version  # noqa: F811
+    major, minor, patch = int(major), int(minor), int(patch)
+    # urllib3 >= 1.21.1, <= 1.22
+    assert major == 1
+    assert minor >= 21
+    assert minor <= 22
     
-        print('Decompressing %s' % ARCHIVE_NAME)
-    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
-        archive.extractall(path='.')
-    os.remove(ARCHIVE_NAME)
+    print('Pad sequences (samples x time)')
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+    
+        for data_format in ['channels_first', 'channels_last']:
+        if data_format == 'channels_first':
+            shape = (3, 5, 5)
+            target_shape = (5, 5, 3)
+            prev_shape = (2, 3, 2)
+            flip = lambda x: np.flip(np.flip(x, axis=2), axis=3)
+            transpose = lambda x: np.transpose(x, (0, 2, 3, 1))
+            target_data_format = 'channels_last'
+        elif data_format == 'channels_last':
+            shape = (5, 5, 3)
+            target_shape = (3, 5, 5)
+            prev_shape = (2, 2, 3)
+            flip = lambda x: np.flip(np.flip(x, axis=1), axis=2)
+            transpose = lambda x: np.transpose(x, (0, 3, 1, 2))
+            target_data_format = 'channels_first'
+    
+            if transform_matrix is not None:
+            h, w = x.shape[img_row_axis], x.shape[img_col_axis]
+            transform_matrix = transform_matrix_offset_center(transform_matrix, h, w)
+            x = apply_transform(x, transform_matrix, img_channel_axis,
+                                fill_mode=self.fill_mode, cval=self.cval)
+    
+    
+@keras_test
+def test_min_max_norm():
+    array = get_example_array()
+    for m in get_test_values():
+        norm_instance = constraints.min_max_norm(min_value=m, max_value=m * 2)
+        normed = norm_instance(K.variable(array))
+        value = K.eval(normed)
+        l2 = np.sqrt(np.sum(np.square(value), axis=0))
+        assert not l2[l2 < m]
+        assert not l2[l2 > m * 2 + 1e-5]
+    
+    
+def test_boston_housing():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = boston_housing.load_data()
+        assert len(x_train) == len(y_train)
+        assert len(x_test) == len(y_test)
+    
+    
+@keras_test
+def test_temporal_classification_functional():
+    '''
+    Classify temporal sequences of float numbers
+    of length 3 into 2 classes using
+    single layer of GRU units and softmax applied
+    to the last activations of the units
+    '''
+    np.random.seed(1337)
+    (x_train, y_train), (x_test, y_test) = get_test_data(num_train=200,
+                                                         num_test=20,
+                                                         input_shape=(3, 4),
+                                                         classification=True,
+                                                         num_classes=2)
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+    
+                # cntk doesn't support eval convolution with static
+            # variable, will enable it later
+            if K.backend() != 'cntk':
+                # check regularizers
+                kwargs = {'data_format': data_format,
+                          'return_sequences': return_sequences,
+                          'kernel_size': (num_row, num_col),
+                          'stateful': True,
+                          'filters': filters,
+                          'batch_input_shape': inputs.shape,
+                          'kernel_regularizer': regularizers.L1L2(l1=0.01),
+                          'recurrent_regularizer': regularizers.L1L2(l1=0.01),
+                          'bias_regularizer': 'l2',
+                          'activity_regularizer': 'l2',
+                          'kernel_constraint': 'max_norm',
+                          'recurrent_constraint': 'max_norm',
+                          'bias_constraint': 'max_norm',
+                          'padding': 'same'}
+    
+    
+if __name__ == '__main__':
+    main()
 
     
-            C = float(self.complexity.get())
-        gamma = float(self.gamma.get())
-        coef0 = float(self.coef0.get())
-        degree = int(self.degree.get())
-        kernel_map = {0: 'linear', 1: 'rbf', 2: 'poly'}
-        if len(np.unique(y)) == 1:
-            clf = svm.OneClassSVM(kernel=kernel_map[self.kernel.get()],
-                                  gamma=gamma, coef0=coef0, degree=degree)
-            clf.fit(X)
-        else:
-            clf = svm.SVC(kernel=kernel_map[self.kernel.get()], C=C,
-                          gamma=gamma, coef0=coef0, degree=degree)
-            clf.fit(X, y)
-        if hasattr(clf, 'score'):
-            print('Accuracy:', clf.score(X, y) * 100)
-        X1, X2, Z = self.decision_surface(clf)
-        self.model.clf = clf
-        self.model.set_surface((X1, X2, Z))
-        self.model.surface_type = self.surface_type.get()
-        self.fitted = True
-        self.model.changed('surface')
     
+def yes_or_no(prompt, true_value='y', false_value='n', default=True):
     '''
-print(__doc__)
-    
-    # Plot changes in predicted probabilities via arrows
-plt.figure(0)
-colors = ['r', 'g', 'b']
-for i in range(clf_probs.shape[0]):
-    plt.arrow(clf_probs[i, 0], clf_probs[i, 1],
-              sig_clf_probs[i, 0] - clf_probs[i, 0],
-              sig_clf_probs[i, 1] - clf_probs[i, 1],
-              color=colors[y_test[i]], head_width=1e-2)
-    
-    import numpy as np
-import matplotlib.pyplot as plt
-    
-    # Max number of base BASE digits to use in test cases.  Doubling
-# this will more than double the runtime.
-MAXDIGITS = 15
-    
-            d = deque('abc', maxlen=5)
-        self.assertEqual(d * -5, deque())
-        self.assertEqual(d * 0, deque())
-        self.assertEqual(d * 1, deque('abc'))
-        self.assertEqual(d * 2, deque('bcabc'))
-        self.assertEqual(d * 30, deque('bcabc'))
-    
-        @classmethod
-    def zonelist(cls, zonedir='/usr/share/zoneinfo'):
-        zones = []
-        for root, _, files in os.walk(zonedir):
-            for f in files:
-                p = os.path.join(root, f)
-                with open(p, 'rb') as o:
-                    magic =  o.read(4)
-                if magic == b'TZif':
-                    zones.append(p[len(zonedir) + 1:])
-        return zones
-    
-    class Chunk:
-    def __init__(self, file, align=True, bigendian=True, inclheader=False):
-        import struct
-        self.closed = False
-        self.align = align      # whether to align to word (2-byte) boundaries
-        if bigendian:
-            strflag = '>'
-        else:
-            strflag = '<'
-        self.file = file
-        self.chunkname = file.read(4)
-        if len(self.chunkname) < 4:
-            raise EOFError
-        try:
-            self.chunksize = struct.unpack_from(strflag+'L', file.read(4))[0]
-        except struct.error:
-            raise EOFError from None
-        if inclheader:
-            self.chunksize = self.chunksize - 8 # subtract header
-        self.size_read = 0
-        try:
-            self.offset = self.file.tell()
-        except (AttributeError, OSError):
-            self.seekable = False
-        else:
-            self.seekable = True
-    
-    import re
-    
-    
-class RangeTest(unittest.TestCase):
-    def assert_iterators_equal(self, xs, ys, test_id, limit=None):
-        # check that an iterator xs matches the expected results ys,
-        # up to a given limit.
-        if limit is not None:
-            xs = itertools.islice(xs, limit)
-            ys = itertools.islice(ys, limit)
-        sentinel = object()
-        pairs = itertools.zip_longest(xs, ys, fillvalue=sentinel)
-        for i, (x, y) in enumerate(pairs):
-            if x == y:
-                continue
-            elif x == sentinel:
-                self.fail('{}: iterator ended unexpectedly '
-                          'at position {}; expected {}'.format(test_id, i, y))
-            elif y == sentinel:
-                self.fail('{}: unexpected excess element {} at '
-                          'position {}'.format(test_id, x, i))
-            else:
-                self.fail('{}: wrong element at position {};'
-                          'expected {}, got {}'.format(test_id, i, y, x))
+    检查是否已经为启动程序做好了准备
+    '''
+    default_value = true_value if default else false_value
+    prompt = '{} {}/{} [{}]: '.format(prompt, true_value,
+        false_value, default_value)
+    i = input(prompt)
+    if not i:
+        return default
+    while True:
+        if i == true_value:
+            return True
+        elif i == false_value:
+            return False
+        prompt = 'Please input {} or {}: '.format(true_value, false_value)
+        i = input(prompt)
