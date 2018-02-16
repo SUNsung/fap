e1356cd7d2443166647dbf97700a7ac6e426cd35
SUNsung <weatherius@gmail.com>
2018-02-17 01:02:51 +0200

    Rewrite [21W2opHb8j9nc21BW1kdzP3EsU21DPttMJECOBk21iml.rb] File in: 2018-02-17 01:02:50

--- ruby/21W2opHb8j9nc21BW1kdzP3EsU21DPttMJECOBk21iml.rb
+++ ruby/21W2opHb8j9nc21BW1kdzP3EsU21DPttMJECOBk21iml.rb
@@ -1,111 +1,126 @@
-        if exp.is_a?(Regexp)
-          entry =~ exp
-    
-    module Jekyll
-  module Filters
-    module GroupingFilters
-      # Group an array of items by a property
-      #
-      # input - the inputted Enumerable
-      # property - the property
-      #
-      # Returns an array of Hashes, each looking something like this:
-      #  {'name'  => 'larry'
-      #   'items' => [...] } # all the items where `property` == 'larry'
-      def group_by(input, property)
-        if groupable?(input)
-          groups = input.group_by { |item| item_property(item, property).to_s }
-          grouped_array(groups)
-        else
-          input
-        end
+
+        
+              if options[:roles].is_a?(Symbol)
+        options[:roles] = [options[:roles]]
       end
     
-        context 'only newlines' do
-      let(:keywords) { 'One\nTwo\r\nThree\nFour Token\n' }
+      def destroy
+    @event.destroy
     
-          def initialize(adb_path: nil)
-        self.adb_path = adb_path
+      def destroy
+    @services = current_user.services.find(params[:id])
+    @services.destroy
+    
+    module Docs
+  class PageDb
+    attr_reader :pages
+    
+      def test_without
+    @opt.parse!(%w'--without-zot')
+    assert_nil(@zot)
+    assert_raise(OptionParser::NeedlessArgument) {@opt.parse!(%w'--without-zot=foobar')}
+  end
+    
+          if off
+        year, mon, day, hour, min, sec =
+          apply_offset(year, mon, day, hour, min, sec, off)
+        t = self.utc(year, mon, day, hour, min, sec, usec)
+        force_zone!(t, zone, off)
+        t
+      else
+        self.local(year, mon, day, hour, min, sec, usec)
       end
+    end
+    private :make_time
     
-          it 'get HG revision number' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          get_build_number_repository(
-            use_hg_revision_number: true
-          )
-        end').runner.execute(:test)
+      def subtest_xmlschema_alias(method)
+    t = Time.utc(1985, 4, 12, 23, 20, 50, 520000)
+    s = '1985-04-12T23:20:50.52Z'
+    assert_equal(t, Time.__send__(method, s))
+    assert_equal(s, t.__send__(method, 2))
     
-          it 'raises an exception when use passes workspace' do
-        expect do
-          Fastlane::FastFile.new.parse('lane :test do
-            get_version_number(xcodeproj: 'project.xcworkspace')
-          end').runner.execute(:test)
-        end.to raise_error('Please pass the path to the project, not the workspace')
+      self.each_test do |test, i|
+    define_method('test_#{i}') do ||
+      Tempfile.create('iotest.dat') do |fh|
+        fh.print test[1]
+        fh.rewind
+        assert_equal(test[2], fh.scanf(test[0]))
       end
     end
   end
 end
 
     
-          it 'it increments all targets minor version number' do
-        Fastlane::FastFile.new.parse('lane :test do
-          increment_version_number(bump_type: 'minor')
-        end').runner.execute(:test)
-    
-        # Returns true if a file has been assigned.
-    def file?
-      original_filename.present?
     end
-    
-        # Returns the width and height in a format suitable to be passed to Geometry.parse
-    def to_s
-      s = ''
-      s << width.to_i.to_s if width > 0
-      s << 'x#{height.to_i}' if height > 0
-      s << modifier.to_s
-      s
-    end
-    
-        def add_required_validations
-      options = Paperclip::Attachment.default_options.deep_merge(@options)
-      if options[:validate_media_type] != false
-        name = @name
-        @klass.validates_media_type_spoof_detection name,
-          :if => ->(instance){ instance.send(name).dirty? }
-      end
-    end
-    
-            def no_error_when_valid?
-          @file = StringIO.new('.')
-          @subject.send(@attachment_name).assign(@file)
-          @subject.valid?
-          expected_message = [
-            @attachment_name.to_s.titleize,
-            I18n.t(:blank, scope: [:errors, :messages])
-          ].join(' ')
-          @subject.errors.full_messages.exclude?(expected_message)
-        end
-      end
-    end
-  end
-end
 
     
-      context 'called with three styles' do
-    it 'applies second style to left and right' do
-      rule = 'border-style: dashed double solid'
     
-      context 'called with argument (2)' do
-    it 'outputs the second value from the default scale' do
-      expect('.two-base-one').to have_rule('font-size: 1.5625em')
-    end
+    {    r1, w1 = IO.pipe
+    r2, w2 = IO.pipe
+    t1 = Thread.new { w1 << megacontent; w1.close }
+    t2 = Thread.new { r2.read; r2.close }
+    IO.copy_stream(r1, w2) rescue nil
+    w2.close
+    r1.close
+    t1.join
+    t2.join
+  }, 'megacontent-copy_stream', ['INT'], :timeout => 10 or break
+end
+    
+        ##
+    # Creates a Regexp to match an address.
+    
+      def adjusted_results name, results
+    s = nil
+    results.each_with_index{|e, i|
+      r = e.min
+      case name
+      when /^vm1_/
+        if @loop_wl1
+          r -= @loop_wl1[i]
+          r = 0 if r < 0
+          s = '*'
+        end
+      when /^vm2_/
+        if @loop_wl2
+          r -= @loop_wl2[i]
+          r = 0 if r < 0
+          s = '*'
+        end
+      end
+      yield r
+    }
+    s
   end
     
-      context 'called with three sizes' do
-    it 'applies second width to left and right' do
-      rule = 'padding: 4px 5px 6px'
+          def http_error_page_as_json(e)
+        render json: {error: :invalid_request, error_description: e.message}, status: 400
+      end
     
-      context 'called with one prefix' do
-    it 'applies the prefix to the property' do
-      rule = '-webkit-appearance: none; ' +
-             'appearance: none;'
+          def create
+        req = Rack::Request.new(request.env)
+        if req['client_assertion_type'] == 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
+          handle_jwt_bearer(req)
+        end
+        self.status, headers, self.response_body = Api::OpenidConnect::TokenEndpoint.new.call(request.env)
+        headers.each {|name, value| response.headers[name] = value }
+        nil
+      end
+    
+      out = File.join(output, site + '.txt')
+  File.unlink(out) if File.exist?(out)
+    
+        # We want to return immediatly if we do not have a packet which is handled by us
+    return unless pkt.is_tcp?
+    return if (pkt.tcp_sport != 143 and pkt.tcp_dport != 143)
+    s = find_session((pkt.tcp_sport == 143) ? get_session_src(pkt) : get_session_dst(pkt))
+    s[:sname] ||= 'imap4'
+    
+    
+    
+            def_node_matcher :redundant_regex?, <<-PATTERN
+          {(send $!nil? {:match :=~} (regexp (str $#literal_at_end?) (regopt)))
+           (send (regexp (str $#literal_at_end?) (regopt)) {:match :=~} $_)}
+        PATTERN
+    
+            private
