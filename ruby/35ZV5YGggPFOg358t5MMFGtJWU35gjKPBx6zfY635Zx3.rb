e6c26a64bdbc31092d47266d561f30ab7af31668
SUNsung <weatherius@gmail.com>
2018-02-16 19:13:35 +0200

    Create [35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb] File in: 2018-02-16 19:13:35

---
+++ ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
@@ -0,0 +1,30 @@
+    def puts(*args)
+      STDERR.puts *args unless @silence
+    end
+    
+      # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
+    
+        if test_conf['database']&.present?
+      ActiveRecord::Base.establish_connection(:test)
+      yield
+      ActiveRecord::Base.establish_connection(Rails.env.to_sym)
+    end
+  end
+    
+      def id
+    object.id.to_s
+  end
+    
+      private
+    
+            def capture_with_block!
+          @captured_by_block = true
+        end
+    
+            def on_case(case_node)
+          case_node.when_branches.each_with_object([]) do |when_node, previous|
+            when_node.each_condition do |condition|
+              next unless repeated_condition?(previous, condition)
