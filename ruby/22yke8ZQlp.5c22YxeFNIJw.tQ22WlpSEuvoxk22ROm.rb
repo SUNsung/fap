aecbc72b003e3b8ef8f11f4cfe1b3a27af801dae
SUNsung <weatherius@gmail.com>
2018-02-17 22:04:29 +0200

    Rewrite [22yke8ZQlp.5c22YxeFNIJw.tQ22WlpSEuvoxk22ROm.rb] File in: 2018-02-17 22:04:29

--- ruby/22yke8ZQlp.5c22YxeFNIJw.tQ22WlpSEuvoxk22ROm.rb
+++ ruby/22yke8ZQlp.5c22YxeFNIJw.tQ22WlpSEuvoxk22ROm.rb
@@ -1,53 +1,103 @@
 
         
-            Or via the Cask:
-      brew cask install ngrok
-    EOS
-  end
-end
-
+              # Returns constant of subscription adapter specified in config/cable.yml.
+      # If the adapter cannot be found, this will default to the Redis adapter.
+      # Also makes sure proper dependencies are required.
+      def pubsub_adapter
+        adapter = (cable.fetch('adapter') { 'redis' })
     
-      # True if a {Formula} is being built with a specific option.
-  # <pre>args << '--i-want-spam' if build.with? 'spam'
+        def env_for(url)
+      Rack::MockRequest.env_for(url)
+    end
+  end
+    
+      def test_dasherized_keys_as_json
+    with_test_route_set do
+      post '/?full=1',
+        params: '{'first-key':{'sub-key':'...'}}',
+        headers: { 'CONTENT_TYPE' => 'application/json' }
+      assert_equal 'action, controller, first-key(sub-key), full', @controller.response.body
+      assert_equal '...', @controller.params['first-key']['sub-key']
+    end
+  end
+    
+    require 'active_job'
+    
+        # Returns +text+ wrapped at +len+ columns and indented +indent+ spaces.
+    # By default column length +len+ equals 72 characters and indent
+    # +indent+ equal two spaces.
+    #
+    #   my_text = 'Here is a sample text with more than 40 characters'
+    #
+    #   format_paragraph(my_text, 25, 4)
+    #   # => '    Here is a sample text with\n    more than 40 characters'
+    def format_paragraph(text, len = 72, indent = 2)
+      sentences = [[]]
+    
+    module ActionMailer
+  class NonInferrableMailerError < ::StandardError
+    def initialize(name)
+      super 'Unable to determine the mailer to test from #{name}. ' \
+        'You'll need to specify it using tests YourMailer in your ' \
+        'test case definition'
+    end
+  end
+    
+      # GET /resource/sign_in
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    yield resource if block_given?
+    respond_with(resource, serialize_options(resource))
+  end
+    
+      # Sets the flash message with :key, using I18n. By default you are able
+  # to set up your messages using specific resource scope, and if no message is
+  # found we look to the default scope. Set the 'now' options key to a true
+  # value to populate the flash.now hash in lieu of the default flash hash (so
+  # the flash message will be available to the current action instead of the
+  # next action).
+  # Example (i18n locale file):
   #
-  # args << '--qt-gui' if build.with? 'qt' # '--with-qt' ==> build.with? 'qt'
+  #   en:
+  #     devise:
+  #       passwords:
+  #         #default_scope_messages - only if resource_scope is not found
+  #         user:
+  #           #resource_scope_messages
   #
-  # # If a formula presents a user with a choice, but the choice must be fulfilled:
-  # if build.with? 'example2'
-  #   args << '--with-example2'
-  # else
-  #   args << '--with-example1'
-  # end</pre>
-  def with?(val)
-    option_names = val.respond_to?(:option_names) ? val.option_names : [val]
-    
-          # Find commands in Homebrew/dev-cmd
-      if ARGV.homebrew_developer?
-        puts
-        puts 'Built-in development commands'
-        puts_columns internal_development_commands
-      end
-    
-      def last_commit
-    Homebrew.git_last_commit || 'never'
+  # Please refer to README or en.yml locale file to check what messages are
+  # available.
+  def set_flash_message(key, kind, options = {})
+    message = find_message(kind, options)
+    if options[:now]
+      flash.now[key] = message if message.present?
+    else
+      flash[key] = message if message.present?
+    end
   end
     
-        results.map do |name|
-      begin
-        formula = Formulary.factory(name)
-        canonical_name = formula.name
-        canonical_full_name = formula.full_name
-      rescue
-        canonical_name = canonical_full_name = name
+        def reset_password_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :reset_password_instructions, opts)
+    end
+    
+          generate_helpers!(Devise::URL_HELPERS)
+    
+          def stages
+        names = Dir[stage_definitions].map { |f| File.basename(f, '.rb') }
+        assert_valid_stage_names(names)
+        names
       end
-      # Ignore aliases from results when the full name was also found
-      if aliases.include?(name) && results.include?(canonical_full_name)
-        next
-      elsif (HOMEBREW_CELLAR/canonical_name).directory?
-        pretty_installed(name)
-      else
-        name
+    
+        def after(task, post_task, *args, &block)
+      Rake::Task.define_task(post_task, *args, &block) if block_given?
+      task = Rake::Task[task]
+      task.enhance do
+        post = Rake.application.lookup(post_task, task.scope)
+        raise ArgumentError, 'Task #{post_task.inspect} not found' unless post
+        post.invoke
       end
-    end.compact
-  end
-end
+    end
+    
+      mkdir_p tasks_dir
