65fb03b8b0ccac8388f7cada2b6db6dafd6c5e6d
SUNsung <weatherius@gmail.com>
2018-02-17 08:07:32 +0200

    Rewrite [486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb] File in: 2018-02-17 08:07:32

--- ruby/486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb
+++ ruby/486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb
@@ -1,96 +1,74 @@
 
         
-              def load_all_devices
-        self.devices = []
+          yield
+end
     
-        attr_accessor :crashlytics_path
-    attr_accessor :api_key
-    attr_accessor :build_secret
-    attr_accessor :emails
-    attr_accessor :groups
-    attr_accessor :schemes
-    attr_accessor :export_method
-    
-            expect(result).to eq('svn info | grep Revision | egrep -o '[0-9]+'')
-        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('svn info | grep Revision | egrep -o '[0-9]+'')
-      end
-    end
-    
-          included do
-        before_create :generate_confirmation_token, if: :confirmation_required?
-        after_create :skip_reconfirmation_in_callback!, if: :send_confirmation_notification?
-        if defined?(ActiveRecord) && self < ActiveRecord::Base # ActiveRecord
-          after_commit :send_on_create_confirmation_instructions, on: :create, if: :send_confirmation_notification?
-          after_commit :send_reconfirmation_instructions, on: :update, if: :reconfirmation_required?
-        else # Mongoid
-          after_create :send_on_create_confirmation_instructions, if: :send_confirmation_notification?
-          after_update :send_reconfirmation_instructions, if: :reconfirmation_required?
-        end
-        before_update :postpone_email_change_until_confirmation_and_regenerate_confirmation_token, if: :postpone_email_change?
-      end
-    
-      test 'should send email instructions for the user confirm its email' do
-    user = create_user
-    assert_email_sent user.email do
-      User.send_confirmation_instructions(email: user.email)
-    end
+      def collection_presenter
+    ActivityPub::CollectionPresenter.new(
+      id: tag_url(@tag),
+      type: :ordered,
+      size: @tag.statuses.count,
+      items: @statuses.map { |s| ActivityPub::TagManager.instance.uri_for(s) }
+    )
   end
     
-      # POST /resource/unlock
-  def create
-    self.resource = resource_class.send_unlock_instructions(resource_params)
-    yield resource if block_given?
+      describe 'GET #show' do
+    let!(:tag)     { Fabricate(:tag, name: 'test') }
+    let!(:local)   { Fabricate(:status, tags: [tag], text: 'local #test') }
+    let!(:remote)  { Fabricate(:status, tags: [tag], text: 'remote #test', account: Fabricate(:account, domain: 'remote')) }
+    let!(:late)    { Fabricate(:status, tags: [tag], text: 'late #test') }
     
-          def remember_me_is_active?(resource)
-        return false unless resource.respond_to?(:remember_me)
-        scope = Devise::Mapping.find_scope!(resource)
-        _, token, generated_at = cookies.signed[remember_key(resource, scope)]
-        resource.remember_me?(token, generated_at)
+    class FollowerAccountsController < ApplicationController
+  include AccountControllerConcern
+    
+      def type
+    'Emoji'
+  end
+    
+      def content
+    Formatter.instance.format(object)
+  end
+    
+          put :update, params: {
+        user: {
+          setting_boost_modal: '1',
+          setting_delete_modal: '0',
+        }
+      }
+    
+        puts 'Downloading emojos from source... (#{source})'
+    
+    @@ layout
+<html>
+  <head>
+    <title>Super Simple Chat with Sinatra</title>
+    <meta charset='utf-8' />
+    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
+  </head>
+  <body><%= yield %></body>
+</html>
+    
+          def deny(env)
+        warn env, 'attack prevented by #{self.class}'
+        [options[:status], {'Content-Type' => 'text/plain'}, [options[:message]]]
       end
     
-            routes.each do |module_name, actions|
-          [:path, :url].each do |path_or_url|
-            actions.each do |action|
-              action = action ? '#{action}_' : ''
-              method = :'#{action}#{module_name}_#{path_or_url}'
+          def call(env)
+        request  = Request.new(env)
+        get_was  = handle(request.GET)
+        post_was = handle(request.POST) rescue nil
+        app.call env
+      ensure
+        request.GET.replace  get_was  if get_was
+        request.POST.replace post_was if post_was
+      end
     
-            if b_length > a_length
-          (b_length - a_length).times { a_split.insert(-2, 0) }
-        elsif a_length > b_length
-          (a_length - b_length).times { b_split.insert(-2, 0) }
-        end
-    
-        def assert_index(index)
-      i = index.is_a?(Integer) ? index : @filters.index(filter_const(index))
-      raise 'No such filter to insert: #{index}' unless i
-      i
+        after do
+      app.instrument(env)
     end
+    
+      it 'should not override the header if already set X-Content-Type-Options' do
+    mock_app with_headers('X-Content-Type-Options' => 'sniff')
+    expect(get('/', {}, 'wants' => 'text/html').headers['X-Content-Type-Options']).to eq('sniff')
   end
 end
-
-    
-        delegate :empty?, :blank?, to: :pages
-    
-        def read_file(path)
-      File.read(path) rescue nil
-    end
-  end
-end
-
-    
-      public
-  # Relies on the codec being synchronous (which they all are!)
-  # We need a better long term design here, but this is an improvement
-  # over the current API for shared plugins
-  # It is best if the codec implements this directly
-  def multi_encode(events)
-    if @has_encode_sync              
-      events.map {|event| [event, self.encode_sync(event)]}
-    else
-      batch = Thread.current[:logstash_output_codec_batch] ||= []
-      batch.clear
-      
-      events.each {|event| self.encode(event) }
-      batch
-    end
-  end
