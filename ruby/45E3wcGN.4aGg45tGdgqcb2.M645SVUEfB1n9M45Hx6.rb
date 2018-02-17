820a23808ef8f44b3f3d6d3d3838480b7257c3c8
SUNsung <weatherius@gmail.com>
2018-02-17 14:58:55 +0200

    Rewrite [45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb] File in: 2018-02-17 14:58:55

--- ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
+++ ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
@@ -1,35 +1,98 @@
 
         
-        class Jekyll::Commands::NewTheme < Jekyll::Command
-  class << self
-    def init_with_program(prog)
-      prog.command(:'new-theme') do |c|
-        c.syntax 'new-theme NAME'
-        c.description 'Creates a new Jekyll theme scaffold'
-        c.option 'code_of_conduct', \
-          '-c', '--code-of-conduct', \
-          'Include a Code of Conduct. (defaults to false)'
+            assert_dom_equal '<script>\n//<![CDATA[\nalert('hello')\n//]]>\n</script>',
+      javascript_tag('alert('hello')')
     
-          message = TestMailer.send_test(args[:to_address])
-      Email::Sender.new(message, :test_message).send
-    end
+          def initialize
+        super
+        self.class.controller_path = ''
+        @request = ActionController::TestRequest.create(self.class)
+        @response = ActionDispatch::TestResponse.new
     
+      test 'token_and_options returns empty string with empty token' do
+    token = ''.dup
+    actual = ActionController::HttpAuthentication::Token.token_and_options(sample_request(token)).first
+    expected = token
+    assert_equal(expected, actual)
+  end
     
-    {    # @include transition(#{border-color ease-in-out .15s, box-shadow ease-in-out .15s})
-    # to
-    # @include transition(border-color ease-in-out .15s, box-shadow ease-in-out .15s)
-    def deinterpolate_vararg_mixins(scss)
-      scss = scss.dup
-      VARARG_MIXINS.each do |mixin|
-        if scss.gsub! /(@include\s*#{Regexp.quote(mixin)})\(\s*\#\{([^}]+)\}\s*\)/, '\1(\2)'
-          log_transform mixin
-        end
+      def test_parsing_json_doesnot_rescue_exception
+    req = Class.new(ActionDispatch::Request) do
+      def params_parsers
+        { json: Proc.new { |data| raise Interrupt } }
       end
-      scss
-    end
     
-      # The test environment is used exclusively to run your application's
-  # test suite. You never need to work with it otherwise. Remember that
-  # your test database is 'scratch space' for the test suite and is wiped
-  # and recreated between test runs. Don't rely on the data there!
-  config.cache_classes = true
+        class MessageDelivery < ActionMailer::MessageDelivery # :nodoc:
+      def initialize(mailer_class, action, params, *args)
+        super(mailer_class, action, *args)
+        @params = params
+      end
+    
+      class Callbacks
+    def self.around_create(record)
+      now = Time.now.utc
+    
+      yield
+end
+    
+      def collection_presenter
+    ActivityPub::CollectionPresenter.new(
+      id: tag_url(@tag),
+      type: :ordered,
+      size: @tag.statuses.count,
+      items: @statuses.map { |s| ActivityPub::TagManager.instance.uri_for(s) }
+    )
+  end
+    
+        # If {#limit_to_network} is disabled, this will always return `true`.
+    # Otherwise, return `true` only if all of the given IPs are within the
+    # project {#boundary boundaries}.
+    
+        end
+    
+    require 'rubygems'  # install rubygems
+require 'hpricot'   # gem install hpricot
+require 'uri'
+require 'timeout'
+    
+      def get_result
+    @src.get_result
+  end
+end
+    
+          # Override `Kernel#puts` to prepend four spaces to each line.
+      def puts(string=nil)
+        $stdout.puts(string.to_s.gsub(/^/, '    '))
+      end
+    
+        def deploying?
+      fetch(:deploying, false)
+    end
+  end
+end
+
+    
+      desc 'Rollback to previous release.'
+  task :rollback do
+    %w{ starting started
+        reverting reverted
+        publishing published
+        finishing_rollback finished }.each do |task|
+      invoke 'deploy:#{task}'
+    end
+  end
+end
+    
+      if File.exist?('Capfile')
+    warn '[skip] Capfile already exists'
+  else
+    FileUtils.cp(capfile, 'Capfile')
+    puts I18n.t(:written_file, scope: :capistrano, file: 'Capfile')
+  end
+    
+      let(:help_output) do
+    out, _err = capture_io do
+      flags '--help', '-h'
+    end
+    out
+  end
