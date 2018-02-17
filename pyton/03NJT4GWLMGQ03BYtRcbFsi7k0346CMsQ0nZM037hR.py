fde6870eeb94bffac84b4f4d0a04c4109a537f1a
SUNsung <weatherius@gmail.com>
2018-02-18 00:49:04 +0200

    Create [03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py] File in: 2018-02-18 00:49:03

---
+++ pyton/03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py
@@ -0,0 +1,18 @@
+if __name__ == '__main__':
+    
+        def test_initial_am_station(self):
+        station = self.radio.state.stations[self.radio.state.pos]
+        expected_station = '1250'
+        self.assertEqual(station, expected_station)
+    
+    *TL;DR80
+Traverses a container and accesses the container's elements.
+'''
+    
+        def tearDown(self):
+        if not self._bProblem:
+            print('Tearing down')
+            time.sleep(0.1)
+            self._tm.publishReport()
+        else:
+            print('Test not executed. No tear down required.')
