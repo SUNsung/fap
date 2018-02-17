e88964e10f58561d0451c3926b9a0d2d9ace2fb0
SUNsung <weatherius@gmail.com>
2018-02-17 11:45:08 +0200

    Rewrite [41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb] File in: 2018-02-17 11:45:08

--- ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
+++ ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
@@ -1,99 +1,137 @@
-  private
-    
-    See CONTRIBUTING.md for more information.
-    
-      def as_json(options={})
-    {
-      poll_id:             id,
-      post_id:             status_message.id,
-      question:            question,
-      poll_answers:        poll_answers,
-      participation_count: participation_count
-    }
-  end
-    
-                  if(inp.attributes[ikey] =~ /^http/i)
-                inp[ikey] = ''
-                next
-              end
-    
-          when :login_pass
-        if(s[:user] and s[:pass])
-          report_auth_info(s)
-          print_status('Successful FTP Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]}')
-          # Remove it form the session objects so freeup memory
-          sessions.delete(s[:session])
-          return
-        end
-    
-    
-# extract label addresses
-addrs = {}
-dtrans.each_line { |ln|
-	if ln =~ /;[^ ].*:/
-		parts = ln.split(' ')
-		label = parts[1]
-		label = label.slice(1,label.index(':')-1)
-		addr = parts[0].split(':')[1].to_i(16)
-		#puts '%s => %x' % [label, addr]
-		one = { label => addr }
-		addrs.merge!(one)
-	end
-}
-#puts addrs.inspect
-    
-    
-jarsignerOpts	= ['-keystore', 'msfkeystore', '-storepass', 'msfstorepass',
-		   '-keypass', 'msfkeypass', '-signedJar', 's#{outputJar}',
-		   outputJar, 'signFiles']
-    
-    		# For file format aixcoff-rs6000
-		if temp.length == 4
-			temp[2] << ' '
-			temp[2] << temp[3]
-			temp.pop
-		end
-    
-          ext = @options[:from]
-      Sass::Util.glob('#{@options[:input]}/**/*.#{ext}') do |f|
-        output =
-          if @options[:in_place]
-            f
-          elsif @options[:output]
-            output_name = f.gsub(/\.(c|sa|sc|le)ss$/, '.#{@options[:to]}')
-            output_name[0...@options[:input].size] = @options[:output]
-            output_name
-          else
-            f.gsub(/\.(c|sa|sc|le)ss$/, '.#{@options[:to]}')
-          end
-    
-            def define_logger(name, options = {})
-          class_eval <<-RUBY, __FILE__, __LINE__ + 1
-            def #{name}(message)
-              #{options.fetch(:to, :log)}(#{name.inspect}, message)
-            end
-          RUBY
-        end
-      end
-    end
-  end
-end
 
-    
-    module Sass
-  # Runs a SassScript read-eval-print loop.
-  # It presents a prompt on the terminal,
-  # reads in SassScript expressions,
-  # evaluates them,
-  # and prints the result.
-  class Repl
-    # @param options [{Symbol => Object}] An options hash.
-    def initialize(options = {})
-      @options = options
+        
+            # Get rid of any info 'dir' files, so they don't conflict at the link stage
+    info_dir_file = @f.info + 'dir'
+    if info_dir_file.file? && !@f.skip_clean?(info_dir_file)
+      observe_file_removal info_dir_file
     end
+    
+          path_modified_time = path.mtime
+      days_default = options[:days_default]
+    
+        first_warning = true
+    methods.each do |method|
+      unless checks.respond_to?(method)
+        Homebrew.failed = true
+        puts 'No check available by the name: #{method}'
+        next
+      end
+    
+        # Exclude cache, logs, and repository, if they are located under the prefix.
+    [HOMEBREW_CACHE, HOMEBREW_LOGS, HOMEBREW_REPOSITORY].each do |dir|
+      dirs.delete dir.relative_path_from(HOMEBREW_PREFIX).to_s
+    end
+    dirs.delete 'etc'
+    dirs.delete 'var'
+    
+          if $stdout.tty?
+        count = local_results.length + tap_results.length
+    
+        @report
+  end
+    
+    class Devise::UnlocksController < DeviseController
+  prepend_before_action :require_no_authentication
+    
+      def respond_with_navigational(*args, &block)
+    respond_with(*args) do |format|
+      format.any(*navigational_formats, &block)
+    end
+  end
+    
+      def test_returns_success
+    Warden.test_mode!
+    
+          def self.generate_helpers!(routes=nil)
+        routes ||= begin
+          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
+          Devise::URL_HELPERS.slice(*mappings)
+        end
+    
+      def user_search
+    if params[:admins_controller_user_search]
+      search_params = params.require(:admins_controller_user_search)
+                            .permit(:username, :email, :guid, :under13)
+      @search = UserSearch.new(search_params)
+      @users = @search.perform
+    end
+    
+          def create
+        req = Rack::Request.new(request.env)
+        if req['client_assertion_type'] == 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
+          handle_jwt_bearer(req)
+        end
+        self.status, headers, self.response_body = Api::OpenidConnect::TokenEndpoint.new.call(request.env)
+        headers.each {|name, value| response.headers[name] = value }
+        nil
+      end
     
           private
     
-      desc 'Publish the release.'
-  task :publishing do
+    desc 'Deploy a new release.'
+task :deploy do
+  set(:deploying, true)
+  %w{ starting started
+      updating updated
+      publishing published
+      finishing finished }.each do |task|
+    invoke 'deploy:#{task}'
   end
+end
+task default: :deploy
+
+    
+      deploy_rb = File.expand_path('../../templates/deploy.rb.erb', __FILE__)
+  stage_rb = File.expand_path('../../templates/stage.rb.erb', __FILE__)
+  capfile = File.expand_path('../../templates/Capfile', __FILE__)
+    
+        # Hash access of interpolations. Included only for compatibility,
+    # and is not intended for normal use.
+    def self.[] name
+      method(name)
+    end
+    
+            def autocorrect(node)
+          redundant_regex?(node) do |receiver, regex_str|
+            receiver, regex_str = regex_str, receiver if receiver.is_a?(String)
+            regex_str = regex_str[0..-3] # drop \Z anchor
+            regex_str = interpret_string_escapes(regex_str)
+    
+    module RuboCop
+  module Cop
+    module Performance
+      # This cop identifies the use of `Regexp#match` or `String#match`, which
+      # returns `#<MatchData>`/`nil`. The return value of `=~` is an integral
+      # index/`nil` and is more performant.
+      #
+      # @example
+      #   # bad
+      #   do_something if str.match(/regex/)
+      #   while regex.match('str')
+      #     do_something
+      #   end
+      #
+      #   # good
+      #   method(str =~ /regex/)
+      #   return value unless regex =~ 'str'
+      class RedundantMatch < Cop
+        MSG = 'Use `=~` in places where the `MatchData` returned by ' \
+              '`#match` will not be used.'.freeze
+    
+    module RuboCop
+  module Cop
+    module Style
+      # This cop checks against comparing a variable with multiple items, where
+      # `Array#include?` could be used instead to avoid code repetition.
+      #
+      # @example
+      #   # bad
+      #   a = 'a'
+      #   foo if a == 'a' || a == 'b' || a == 'c'
+      #
+      #   # good
+      #   a = 'a'
+      #   foo if ['a', 'b', 'c'].include?(a)
+      class MultipleComparison < Cop
+        MSG = 'Avoid comparing a variable with multiple items ' \
+          'in a conditional, use `Array#include?` instead.'.freeze
