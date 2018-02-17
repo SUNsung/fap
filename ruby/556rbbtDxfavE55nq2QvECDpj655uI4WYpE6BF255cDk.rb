ab58e6ccf9ed77e9a53f23fd98ba395a2f5f1bf0
SUNsung <weatherius@gmail.com>
2018-02-17 14:36:56 +0200

    Create [556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb] File in: 2018-02-17 14:36:55

---
+++ ruby/556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb
@@ -0,0 +1,27 @@
+gem 'rails-controller-testing'
+    
+    class Devise::PasswordsController < DeviseController
+  prepend_before_action :require_no_authentication
+  # Render the #edit only if coming from a reset password email link
+  append_before_action :assert_reset_token_passed, only: :edit
+    
+          # Forgets the given resource by deleting a cookie
+      def forget_me(resource)
+        scope = Devise::Mapping.find_scope!(resource)
+        resource.forget_me!
+        cookies.delete(remember_key(resource, scope), forget_cookie_values(resource))
+      end
+    
+        def stop!
+      @scheduler.jobs(tag: id).each(&:unschedule)
+    
+        valid_oauth_providers :tumblr
+  end
+    
+      def set_table_sort(sort_options)
+    valid_sorts = sort_options[:sorts] or raise ArgumentError.new('You must specify :sorts as an array of valid sort attributes.')
+    default = sort_options[:default] || { valid_sorts.first.to_sym => :desc }
+    
+      def destroy
+    @services = current_user.services.find(params[:id])
+    @services.destroy
