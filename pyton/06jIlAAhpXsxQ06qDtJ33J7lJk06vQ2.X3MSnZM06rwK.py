2167db6986b56807971cecfebcc1d5224a5e4c0a
SUNsung <weatherius@gmail.com>
2018-02-17 20:56:47 +0200

    Rewrite [06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py] File in: 2018-02-17 20:56:46

--- pyton/06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py
+++ pyton/06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py
@@ -1,31 +1,221 @@
 
         
-            def run(self):
-        try:
-            self.server_sock = self._create_socket_and_bind()
-            # in case self.port = 0
-            self.port = self.server_sock.getsockname()[1]
-            self.ready_event.set()
-            self._handle_requests()
+        py_binary(
+    name = 'cifar10_multi_gpu_train',
+    srcs = [
+        'cifar10_multi_gpu_train.py',
+    ],
+    srcs_version = 'PY2AND3',
+    visibility = ['//tensorflow:__subpackages__'],
+    deps = [
+        ':cifar10',
+    ],
+)
+    
+    py_library(
+    name = 'package',
+    srcs = [
+        '__init__.py',
+    ],
+    srcs_version = 'PY2AND3',
+    deps = [
+        ':rnn',
+        ':rnn_cell',
+        ':seq2seq',
+    ],
+)
     
     
-@pytest.mark.skipif(sys.version_info < (2,7), reason='Only run on Python 2.7+')
-def test_system_ssl():
-    '''Verify we're actually setting system_ssl when it should be available.'''
-    assert info()['system_ssl']['version'] != ''
+# TODO(b/64848083) Remove once uid bug is fixed
+class RunConfig(tf.contrib.learn.RunConfig): 
+  def uid(self, whitelist=None):
+    '''Generates a 'Unique Identifier' based on all internal fields.
+    Caller should use the uid string to check `RunConfig` instance integrity
+    in one session use, but should not rely on the implementation details, which
+    is subject to change.
+    Args:
+      whitelist: A list of the string names of the properties uid should not
+        include. If `None`, defaults to `_DEFAULT_UID_WHITE_LIST`, which
+        includes most properties user allowes to change.
+    Returns:
+      A uid string.
+    '''
+    if whitelist is None:
+      whitelist = run_config._DEFAULT_UID_WHITE_LIST
     
-    This module contains the set of Requests' exceptions.
-'''
-from urllib3.exceptions import HTTPError as BaseHTTPError
+      def preprocess(self, image):
+    '''Preprocess a single image in [height, width, depth] layout.'''
+    if self.subset == 'train' and self.use_distortion:
+      # Pad 4 pixels on each dimension of feature map, done in mini-batch
+      image = tf.image.resize_image_with_crop_or_pad(image, 40, 40)
+      image = tf.random_crop(image, [HEIGHT, WIDTH, DEPTH])
+      image = tf.image.random_flip_left_right(image)
+    return image
     
-        @pytest.mark.parametrize(
-        'path', (
-            '/',
-            __file__,
-            pytest.__file__,
-            '/etc/invalid/location',
-        ))
-    def test_unzipped_paths_unchanged(self, path):
-        assert path == extract_zipped_paths(path)
+            for s in 'NDk':
+            if s == quantity:
+                pass
+            else:
+                descr_string += '%s = %i, ' % (s, fiducial_values[s])
     
-        By default this will get the strings from the blns.txt file
+        if not os.path.exists(ARCHIVE_NAME):
+        print('Downloading dataset from %s (14 MB)' % URL)
+        opener = urlopen(URL)
+        with open(ARCHIVE_NAME, 'wb') as archive:
+            archive.write(opener.read())
+    
+    plt.figure(2)  # 'banana' shape
+plt.title('Outlier detection on a real data set (boston housing)')
+plt.scatter(X2[:, 0], X2[:, 1], color='black')
+plt.xlim((xx2.min(), xx2.max()))
+plt.ylim((yy2.min(), yy2.max()))
+plt.legend((legend2_values_list[0].collections[0],
+            legend2_values_list[1].collections[0],
+            legend2_values_list[2].collections[0]),
+           (legend2_keys_list[0], legend2_keys_list[1], legend2_keys_list[2]),
+           loc='upper center',
+           prop=matplotlib.font_manager.FontProperties(size=12))
+plt.ylabel('% lower status of the population')
+plt.xlabel('average number of rooms per dwelling')
+    
+    The data is generated with the ``make_checkerboard`` function, then
+shuffled and passed to the Spectral Biclustering algorithm. The rows
+and columns of the shuffled matrix are rearranged to show the
+biclusters found by the algorithm.
+    
+        @mock.patch('certbot_compatibility_test.validator.requests.get')
+    def test_hsts_bad_max_age(self, mock_get_request):
+        mock_get_request.return_value = create_response(
+            headers={'strict-transport-security': 'max-age=not-an-int'})
+        self.assertFalse(self.validator.hsts('test.com'))
+    
+    # List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = ['_build']
+    
+        @mock.patch('certbot.display.enhancements.util')
+    def test_redirect(self, mock_util):
+        mock_util().menu.return_value = (display_util.OK, 1)
+        self.assertTrue(self._call('redirect'))
+    
+    from certbot import interfaces
+from certbot.plugins import common
+    
+    slice_user = Table('slice_user', Base.metadata,
+    Column('id', Integer, primary_key=True),
+    Column('user_id', Integer, ForeignKey('ab_user.id')),
+    Column('slice_id', Integer, ForeignKey('slices.id'))
+)
+    
+    from alembic import op
+import sqlalchemy as sa
+    
+    '''
+    
+    
+def downgrade():
+    op.drop_column('metrics', 'created_on')
+    op.drop_column('metrics', 'created_by_fk')
+    op.drop_column('metrics', 'changed_on')
+    op.drop_column('metrics', 'changed_by_fk')
+    try:
+        op.alter_column('url', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('url', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('tables', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('tables', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('table_columns', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('table_columns', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('sql_metrics', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('sql_metrics', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('slices', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('slices', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.drop_constraint(None, 'metrics', type_='foreignkey')
+        op.drop_constraint(None, 'metrics', type_='foreignkey')
+        op.alter_column('dbs', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('dbs', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('datasources', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('datasources', 'created_by_fk',
+                   existing_type=sa.INTEGER(),
+                   nullable=False)
+        op.alter_column('datasources', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('datasources', 'changed_by_fk',
+                   existing_type=sa.INTEGER(),
+                   nullable=False)
+        op.alter_column('dashboards', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('dashboards', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('css_templates', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('css_templates', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('columns', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+        op.alter_column('columns', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=False)
+    except:
+        pass
+
+    
+    Revision ID: 7dbf98566af7
+Revises: 8e80a26a31db
+Create Date: 2016-01-17 22:00:23.640788
+    
+        fftv.publish('cartoon')
+    fftv.publish('music')
+    fftv.publish('ads')
+    fftv.publish('movie')
+    fftv.publish('cartoon')
+    fftv.publish('cartoon')
+    fftv.publish('movie')
+    fftv.publish('blank')
+    
+    Test code which will almost always fail (if not exactly 12:01) when untestable
+production code (production code time provider is datetime) is used:
+    
+    class ProductionCodeTimeProvider(object):
+    '''
+    Production code version of the time provider (just a wrapper for formatting
+    datetime for this example).
+    '''
+    
+    
+class Handler(object):
+    __metaclass__ = abc.ABCMeta
+    
+    
+class UserSpecification(CompositeSpecification):
