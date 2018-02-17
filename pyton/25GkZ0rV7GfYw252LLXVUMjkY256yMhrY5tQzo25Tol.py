50718b734212dd3fb58b74236468e210043669d8
SUNsung <weatherius@gmail.com>
2018-02-17 10:56:19 +0200

    Rewrite [25GkZ0rV7GfYw252LLXVUMjkY256yMhrY5tQzo25Tol.py] File in: 2018-02-17 10:56:19

--- pyton/25GkZ0rV7GfYw252LLXVUMjkY256yMhrY5tQzo25Tol.py
+++ pyton/25GkZ0rV7GfYw252LLXVUMjkY256yMhrY5tQzo25Tol.py
@@ -1,68 +1,151 @@
 
         
-            def setUp(self):
-        from acme.errors import PollError
-        self.timeout = PollError(
-            exhausted=set([mock.sentinel.AR]),
-            updated={})
-        self.invalid = PollError(exhausted=set(), updated={
-            mock.sentinel.AR: mock.sentinel.AR2})
+        
+def side_effect(old_cmd, command):
+    with tarfile.TarFile(_tar_file(old_cmd.script_parts)[0]) as archive:
+        for file in archive.getnames():
+            try:
+                os.remove(file)
+            except OSError:
+                # does not try to remove directories as we cannot know if they
+                # already existed before
+                pass
+
     
-    # The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+        # To avoid having to always use the '|safe' filter in flatpage templates,
+    # mark the title and content as already safe (since they are raw HTML
+    # content in the first place).
+    f.title = mark_safe(f.title)
+    f.content = mark_safe(f.content)
     
-    from alembic import op
-import sqlalchemy as sa
+    print('Build model...')
     
-    '''
+    It is preferable to run this script on GPU, for speed.
     
-    '''
+    Can easily be extended to include new transformations,
+new preprocessing methods, etc...
+'''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
     
-    '''
     
-    from alembic import op
-from superset import db
-from superset.utils import generic_find_constraint_name, table_has_constraint
-import logging
-import sqlalchemy as sa
-from sqlalchemy.dialects import mysql
+def upgrade():
+    try:
+        op.alter_column(
+            'clusters', 'changed_on',
+            existing_type=sa.DATETIME(),
+            nullable=True)
+        op.alter_column(
+            'clusters', 'created_on',
+            existing_type=sa.DATETIME(), nullable=True)
+        op.drop_constraint(None, 'columns', type_='foreignkey')
+        op.drop_constraint(None, 'columns', type_='foreignkey')
+        op.drop_column('columns', 'created_on')
+        op.drop_column('columns', 'created_by_fk')
+        op.drop_column('columns', 'changed_on')
+        op.drop_column('columns', 'changed_by_fk')
+        op.alter_column('css_templates', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('css_templates', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('dashboards', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('dashboards', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.create_unique_constraint(None, 'dashboards', ['slug'])
+        op.alter_column('datasources', 'changed_by_fk',
+                   existing_type=sa.INTEGER(),
+                   nullable=True)
+        op.alter_column('datasources', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('datasources', 'created_by_fk',
+                   existing_type=sa.INTEGER(),
+                   nullable=True)
+        op.alter_column('datasources', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('dbs', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('dbs', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('slices', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('slices', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('sql_metrics', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('sql_metrics', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('table_columns', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('table_columns', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('tables', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('tables', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('url', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('url', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        ### end Alembic commands ###
+    except:
+        pass
+    
+    def upgrade():
+    op.add_column('dashboards', sa.Column('slug', sa.String(length=255), nullable=True))
+    try:
+        op.create_unique_constraint('idx_unique_slug', 'dashboards', ['slug'])
+    except:
+        pass
+    
+    class User(Base):
+    '''Declarative class to do query in upgrade'''
+    __tablename__ = 'ab_user'
+    id = Column(Integer, primary_key=True)
     
     
 def downgrade():
-    op.drop_table('slice_user')
-    op.drop_table('dashboard_user')
+    bind = op.get_bind()
+    session = db.Session(bind=bind)
+    for slc in session.query(Slice).all():
+        if slc.datasource_type == 'druid':
+            slc.druid_datasource_id = slc.datasource_id
+        if slc.datasource_type == 'table':
+            slc.table_id = slc.datasource_id
+        session.merge(slc)
+        session.commit()
+    session.close()
+    op.drop_column('slices', 'datasource_id')
 
     
-    # revision identifiers, used by Alembic.
-revision = '55179c7f25c7'
-down_revision = '315b3f4da9b0'
+    '''
     
-    # revision identifiers, used by Alembic.
-revision = '7e3ddad2a00b'
-down_revision = 'b46fa1b0b39e'
+    '''
     
-        def wake(self):
-        try:
-            self.writer.send(b'x')
-        except (IOError, socket.error, ValueError):
-            pass
+        entry_points = {'console_scripts': proj_info['console_scripts']}
+)
+
     
-            yield write(b'\r\n')
+        video_url = match1(html, r'filepath=(.+)&sec')
+    video_url = video_url.replace('&mid', '?mid')
     
-        logging.info('Listening on http://localhost:%d' % options.port)
-    IOLoop.current().start()
-    
-    plugins = PluginLoader()
-print 'PLUGINS := ' + ' '.join(plugin.name for plugin in plugins
-                               if plugin.needs_static_build)
-    
-                if not c.user.employee:
-                c.user.gold_creddits -= months
-                c.user._commit()
-    
-            res = AdminPage(content = AdminAwardGive(award, recipient, desc,
-                                                 url, hours),
-                        title='give an award').render()
-        return res
+    __all__ = ['mixcloud_download']
