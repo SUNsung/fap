9bbf18165df7b1a2a92c00a80f8e35101662cd28
SUNsung <weatherius@gmail.com>
2018-02-17 05:01:56 +0200

    Rewrite [400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py] File in: 2018-02-17 05:01:55

--- pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
+++ pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
@@ -1,53 +1,82 @@
 
         
-            @property
-    def body(self):
-        body = self._orig.body
-        if isinstance(body, str):
-            # Happens with JSON/form request data parsed from the command line.
-            body = body.encode('utf8')
-        return body or b''
+            infile, outfile = args
+    
+    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    
+        def assertMatch(self, url, ie_list):
+        self.assertEqual(self.matching_ies(url), ie_list)
+    
+    
+class ClipRsIE(OnetBaseIE):
+    _VALID_URL = r'https?://(?:www\.)?clip\.rs/(?P<id>[^/]+)/\d+'
+    _TEST = {
+        'url': 'http://www.clip.rs/premijera-frajle-predstavljaju-novi-spot-za-pesmu-moli-me-moli/3732',
+        'md5': 'c412d57815ba07b56f9edc7b5d6a14e5',
+        'info_dict': {
+            'id': '1488842.1399140381',
+            'ext': 'mp4',
+            'title': 'PREMIJERA Frajle predstavljaju novi spot za pesmu Moli me, moli',
+            'description': 'md5:56ce2c3b4ab31c5a2e0b17cb9a453026',
+            'duration': 229,
+            'timestamp': 1459850243,
+            'upload_date': '20160405',
+        }
+    }
+    
+    # The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = '_static/flask-favicon.ico'
+    
+        r = client.get('/world')
+    assert r.status_code == 200
+    
+    
+def test_messages(client, app):
+    '''Test that messages work'''
+    login(client, app.config['USERNAME'],
+          app.config['PASSWORD'])
+    rv = client.post('/add', data=dict(
+        title='<Hello>',
+        text='<strong>HTML</strong> allowed here'
+    ), follow_redirects=True)
+    assert b'No entries here so far' not in rv.data
+    assert b'&lt;Hello&gt;' in rv.data
+    assert b'<strong>HTML</strong> allowed here' in rv.data
 
     
+    # Make coding more python3-ish
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
     
-@mock.patch('httpie.core.get_response')
-def test_timeout(get_response):
-    def error(msg, *args, **kwargs):
-        global error_msg
-        error_msg = msg % args
     
-                    if not speed:
-                    eta = '-:--:--'
-                else:
-                    s = int((self.status.total_size - downloaded) / speed)
-                    h, s = divmod(s, 60 * 60)
-                    m, s = divmod(s, 60)
-                    eta = '{0}:{1:0>2}:{2:0>2}'.format(h, m, s)
+class Galaxy(object):
+    ''' Keeps global galaxy info '''
     
-        @staticmethod
-    def make_header(username, password):
-        credentials = u'%s:%s' % (username, password)
-        token = b64encode(credentials.encode('utf8')).strip().decode('latin1')
-        return 'Basic %s' % token
+    # Documents to append as an appendix to all manuals.
+#latex_appendices = []
     
-        '''
-)
-troubleshooting.add_argument(
-    '--default-scheme',
-    default='http',
-    help='''
-    The default scheme to use if not specified in the URL.
+    def sequential():
+    return list(map(is_prime, PRIMES))
     
-    import pytest
-import pytest_httpbin.certs
-from requests.exceptions import SSLError
     
-        def live_config_iteritems(self):
-        live = self.stacked_proxy_safe_get(g, 'live_config', {})
-        return live.iteritems()
+  # This is the blocking version of the method. See below for async.
+  # |timeout| is num seconds to tolerate no response from server before giving
+  # up; see Requests docs for details (we just pass the param along).
+  @staticmethod
+  def PostDataToHandler( data, handler, timeout = _READ_TIMEOUT_SEC ):
+    return JsonFromFuture( BaseRequest.PostDataToHandlerAsync( data,
+                                                               handler,
+                                                               timeout ) )
     
-    api('trophy', TrophyJsonTemplate)
-api('rules', RulesJsonTemplate)
+          with HandleServerException( display = False ):
+        BaseRequest.GetDataFromHandler( 'healthy' )
+
     
-    import json
-import os
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
