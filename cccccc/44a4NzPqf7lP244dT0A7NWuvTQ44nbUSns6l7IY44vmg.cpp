e91e5e02a2f395f1ddf002b089242514c3cd9fcb
SUNsung <weatherius@gmail.com>
2018-02-18 20:47:21 +0200

    Rewrite [44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp] File in: 2018-02-18 20:47:20

--- cccccc/44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp
+++ cccccc/44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp
@@ -1,239 +1,230 @@
 
         
-        #include <unordered_set>
-#include <vector>
-    
-    #include 'tensorflow/compiler/xla/service/hlo_module.h'
-#include 'tensorflow/compiler/xla/service/hlo_pass_interface.h'
-    
-      if (pid_ > 0) {
-    // Parent process: close the child-side pipes and return.
-    running_ = true;
-    for (int i = 0; i < kNFds; i++) {
-      if (child_pipe_[i] >= 0) {
-        close(child_pipe_[i]);
-        child_pipe_[i] = -1;
-      }
-    }
-    return true;
-  }
-    
-      ExpectFailure(Builder().Attr('T', DT_STRING).Input(FakeInput(DT_BOOL)),
-                'Inconsistent values for attr 'T' DT_STRING vs. DT_BOOL while');
-    
-    static uint32 MaskedCrc(const char* data, size_t n) {
-  return crc32c::Mask(crc32c::Value(data, n));
+        
+    {  ret += ')';
+  return ret;
 }
     
-    /**
- * \ingroup CXX11_NeuralNetworks_Module
- * \brief Template functor to clip the magnitude of the first scalar.
- *
- * \sa class CwiseBinaryOp, MatrixBase::Clip
- */
-template <typename Scalar>
-struct scalar_clip_op {
-  EIGEN_EMPTY_STRUCT_CTOR(scalar_clip_op)
-  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE const Scalar
-  operator()(const Scalar& a, const Scalar& b) const {
-    return numext::mini(numext::maxi(a, -b), b);
+    int64_t TimeStamp::Current() {
+  return time(0);
+}
+    
+    #include 'hphp/runtime/vm/jit/abi.h'
+#include 'hphp/runtime/vm/jit/arg-group.h'
+#include 'hphp/runtime/vm/jit/fixup.h'
+#include 'hphp/runtime/vm/jit/phys-reg-saver.h'
+#include 'hphp/runtime/vm/jit/vasm-gen.h'
+#include 'hphp/runtime/vm/jit/vasm-instr.h'
+#include 'hphp/runtime/vm/jit/vasm-reg.h'
+    
+    // Check if we should stop due to flow control, breakpoints, and signals.
+bool DebuggerProxy::checkFlowBreak(CmdInterrupt &cmd) {
+  TRACE(2, 'DebuggerProxy::checkFlowBreak\n');
+  // If there is an outstanding Ctrl-C from the client, go ahead and break now.
+  // Note: this stops any flow control command we might have in-flight.
+  if (m_signum == CmdSignal::SignalBreak) {
+    Lock lock(m_signalMutex);
+    if (m_signum == CmdSignal::SignalBreak) {
+      m_signum = CmdSignal::SignalNone;
+      m_flow.reset();
+      return true;
+    }
   }
-  template <typename Packet>
-  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE const Packet
-  packetOp(const Packet& a, const Packet& b) const {
-    return internal::pmin(internal::pmax(a, internal::pnegate(b)), b);
-  }
+    }
+    
+      /**
+   * Query if a task is finished. This is non-blocking and can be called as
+   * many times as desired.
+   */
+  static int64_t TaskStatus(const Resource& task);
+    
+    struct IRGS;
+    
+    Vlabel Vunit::makeBlock(AreaIndex area, uint64_t weight) {
+  auto i = blocks.size();
+  blocks.emplace_back(area, weight);
+  return Vlabel{i};
+}
+    
+    inline void initNuma() {}
+inline constexpr int next_numa_node(std::atomic_int& curr_node) { return 0; }
+inline constexpr int num_numa_nodes() { return 1; }
+inline void numa_interleave(void* start, size_t size) {}
+inline void numa_bind_to(void* start, size_t size, int node) {}
+inline constexpr bool numa_node_allowed(int node) { return true; }
+    
+    void ThriftBuffer::read(char *data, int len) {
+  auto const avail = m_pEnd - m_p;
+  if (avail < 0) not_reached();
+    }
+    
+    #include 'hphp/runtime/vm/resumable.h'
+    
+    #include 'src/core/lib/compression/compression_internal.h'
+    
+    std::unique_ptr<ScenarioResult> RunScenario(
+    const grpc::testing::ClientConfig& client_config, size_t num_clients,
+    const grpc::testing::ServerConfig& server_config, size_t num_servers,
+    int warmup_seconds, int benchmark_seconds, int spawn_local_worker_count,
+    const grpc::string& qps_server_target_override,
+    const grpc::string& credential_type, bool run_inproc);
+    
+    class WorkerServiceImpl;
+    
+    // Get leading or trailing comments in a string. Comment lines start with '// '.
+// Leading detached comments are put in in front of leading comments.
+template <typename DescriptorType>
+inline grpc::string GetNodeComments(const DescriptorType* desc, bool leading) {
+  return grpc_generator::GetPrefixedComments(desc, leading, '//');
+}
+    
+    
+    {  // The data in the buffer.
+  detail::buffered_stream_storage storage_;
 };
     
+    #if !defined(BOOST_ASIO_HAS_THREADS)
+// Nothing to include.
+#elif defined(BOOST_ASIO_HAS_STD_ATOMIC)
+# include <atomic>
+#else // defined(BOOST_ASIO_HAS_STD_ATOMIC)
+# include <boost/detail/atomic_count.hpp>
+#endif // defined(BOOST_ASIO_HAS_STD_ATOMIC)
     
-    { private:
-  enum { kBufferSize = 256 << 10 /* 256 kB */ };
-  const int skip_header_lines_;
-  Env* const env_;
-  int64 line_number_;
-  std::unique_ptr<RandomAccessFile> file_;  // must outlive input_buffer_
-  std::unique_ptr<io::InputBuffer> input_buffer_;
-};
-    
-        // Validate that data[i].shape = indices[i].shape + constant
-    OP_REQUIRES_OK(c, c->input_list('data', data_inputs));
-    const Tensor& data0 = (*data_inputs)[0];
-    const Tensor& indices0 = (*indices_inputs)[0];
-    for (int input_num = 0; input_num < indices_inputs->size(); input_num++) {
-      const Tensor& indices = (*indices_inputs)[input_num];
-      const Tensor& data = (*data_inputs)[input_num];
-      OP_REQUIRES(
-          c, TensorShapeUtils::StartsWith(data.shape(), indices.shape()),
-          errors::InvalidArgument('data[', input_num,
-                                  '].shape = ', data.shape().DebugString(),
-                                  ' does not start with indices[', input_num,
-                                  '].shape = ', indices.shape().DebugString()));
-      OP_REQUIRES(
-          c, input_num == 0 || SameExtraShape(data0, indices0, data, indices),
-          errors::InvalidArgument(
-              'Need data[0].shape[', indices0.dims(), ':] = data[', input_num,
-              '].shape[', indices.dims(),
-              ':], got data[0].shape = ', data0.shape().DebugString(),
-              ', data[', input_num, '].shape = ', data.shape().DebugString(),
-              ', indices[0].shape = ', indices0.shape().DebugString(),
-              ', indices[', input_num,
-              '].shape = ', indices.shape().DebugString()));
-    }
-    
-      // Okay, we need to print this expression.  We generally do this by creating a
-  // REPL metavariable (e.g. r4) to hold the result, so it can be referred to
-  // in the future.  However, if this is a direct reference to a decl (e.g. 'x')
-  // then don't create a repl metavariable.
-  if (VarDecl *d = getObviousDeclFromExpr(E)) {
-    generatePrintOfExpression(d->getName().str(), E);
-    return;
+      std::size_t check_for_completion(
+      const boost::system::error_code& ec,
+      std::size_t total_transferred)
+  {
+    return detail::adapt_completion_condition_result(
+        completion_condition_(ec, total_transferred));
   }
     
-    class SILFunction;
-class SILBasicBlock;
+    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
     
-    #include 'swift/Index/IndexRecord.h'
-#include 'swift/AST/ASTContext.h'
-#include 'swift/AST/Decl.h'
-#include 'swift/AST/Expr.h'
-#include 'swift/AST/Module.h'
-#include 'swift/AST/ParameterList.h'
-#include 'swift/AST/Pattern.h'
-#include 'swift/AST/Stmt.h'
-#include 'swift/AST/Types.h'
-#include 'swift/AST/DiagnosticsFrontend.h'
-#include 'swift/AST/ModuleLoader.h'
-#include 'swift/ClangImporter/ClangModule.h'
-#include 'swift/Index/Index.h'
-#include 'swift/Strings.h'
-#include 'clang/Basic/FileManager.h'
-#include 'clang/Frontend/CompilerInstance.h'
-#include 'clang/Index/IndexingAction.h'
-#include 'clang/Index/IndexRecordWriter.h'
-#include 'clang/Index/IndexUnitWriter.h'
-#include 'clang/Lex/Preprocessor.h'
-#include 'llvm/Support/Path.h'
-    
-    /// \brief Diagnostic consumer that displays diagnostics to standard error.
-class PrintingDiagnosticConsumer : public DiagnosticConsumer {
-  llvm::raw_ostream &Stream;
-  bool ForceColors = false;
-  bool DidErrorOccur = false;
-public:
-  PrintingDiagnosticConsumer(llvm::raw_ostream &stream = llvm::errs()) :
-    Stream(stream) { }
+    // Calls to asio_handler_allocate and asio_handler_deallocate must be made from
+// a namespace that does not contain any overloads of these functions. The
+// boost_asio_handler_alloc_helpers namespace is defined here for that purpose.
+namespace boost_asio_handler_alloc_helpers {
     }
     
-    #endif
-
+    #define BOOST_ASIO_BUFFERED_HANDSHAKE_HANDLER_CHECK( \
+    handler_type, handler) \
+  \
+  typedef BOOST_ASIO_HANDLER_TYPE(handler_type, \
+      void(boost::system::error_code, std::size_t)) \
+    asio_true_handler_type; \
+  \
+  BOOST_ASIO_HANDLER_TYPE_REQUIREMENTS_ASSERT( \
+      sizeof(boost::asio::detail::two_arg_handler_test( \
+          boost::asio::detail::clvref< \
+            asio_true_handler_type>(), \
+          static_cast<const boost::system::error_code*>(0), \
+          static_cast<const std::size_t*>(0))) == 1, \
+      'BufferedHandshakeHandler type requirements not met') \
+  \
+  typedef boost::asio::detail::handler_type_requirements< \
+      sizeof( \
+        boost::asio::detail::argbyv( \
+          boost::asio::detail::clvref< \
+            asio_true_handler_type>())) + \
+      sizeof( \
+        boost::asio::detail::lvref< \
+          asio_true_handler_type>()( \
+          boost::asio::detail::lvref<const boost::system::error_code>(), \
+          boost::asio::detail::lvref<const std::size_t>()), \
+        char(0))> BOOST_ASIO_UNUSED_TYPEDEF
     
-    class Env;
-class Iterator;
-class TableCache;
-class VersionEdit;
-    
-      // Return an internal key (suitable for passing to an internal iterator)
-  Slice internal_key() const { return Slice(kstart_, end_ - kstart_); }
-    
-    Status DumpFile(Env* env, const std::string& fname, WritableFile* dst) {
-  FileType ftype;
-  if (!GuessType(fname, &ftype)) {
-    return Status::InvalidArgument(fname + ': unknown file type');
-  }
-  switch (ftype) {
-    case kLogFile:         return DumpLog(env, fname, dst);
-    case kDescriptorFile:  return DumpDescriptor(env, fname, dst);
-    case kTableFile:       return DumpTable(env, fname, dst);
-    default:
-      break;
-  }
-  return Status::InvalidArgument(fname + ': not a dump-able file type');
-}
-    
-    #include 'db/filename.h'
-    
-    void WriteBatch::Delete(const Slice& key) {
-  WriteBatchInternal::SetCount(this, WriteBatchInternal::Count(this) + 1);
-  rep_.push_back(static_cast<char>(kTypeDeletion));
-  PutLengthPrefixedSlice(&rep_, key);
-}
-    
-      void FinishedSingleOp() {
-    if (FLAGS_histogram) {
-      double now = Env::Default()->NowMicros() * 1e-6;
-      double micros = (now - last_op_finish_) * 1e6;
-      hist_.Add(micros);
-      if (micros > 20000) {
-        fprintf(stderr, 'long op: %.1f micros%30s\r', micros, '');
-        fflush(stderr);
-      }
-      last_op_finish_ = now;
-    }
-    }
-    
-    #endif  // STORAGE_LEVELDB_INCLUDE_FILTER_POLICY_H_
-
-    
-        state->preFilterType = CV_STEREO_BM_XSOBEL; //CV_STEREO_BM_NORMALIZED_RESPONSE;
-    state->preFilterSize = 9;
-    state->preFilterCap = 31;
-    state->SADWindowSize = 15;
-    state->minDisparity = 0;
-    state->numberOfDisparities = numberOfDisparities > 0 ? numberOfDisparities : 64;
-    state->textureThreshold = 10;
-    state->uniquenessRatio = 15;
-    state->speckleRange = state->speckleWindowSize = 0;
-    state->trySmallerWindows = 0;
-    state->roi1 = state->roi2 = cvRect(0,0,0,0);
-    state->disp12MaxDiff = -1;
-    
-    struct CV_EXPORTS IntrinsicParams
+    void buffer_sequence_adapter_base::init_native_buffer(
+    buffer_sequence_adapter_base::native_buffer_type& buf,
+    const boost::asio::mutable_buffer& buffer)
 {
-    Vec2d f;
-    Vec2d c;
-    Vec4d k;
-    double alpha;
-    std::vector<uchar> isEstimate;
+  std::memset(&buf, 0, sizeof(native_buffer_type));
+  Microsoft::WRL::ComPtr<IInspectable> insp
+    = Microsoft::WRL::Make<winrt_buffer_impl>(buffer);
+  buf = reinterpret_cast<Windows::Storage::Streams::IBuffer^>(insp.Get());
+}
+    
+    #include <boost/asio/detail/push_options.hpp>
+    
+      // Returns an aggregate distance or similarity value between the baseline
+  // image and the image in the last Compare() call (or the baseline image, if
+  // Compare() was not called yet).
+  // The interpretation of this aggregate value depends on the comparator used.
+  virtual float distmap_aggregate() const = 0;
+    
+    std::vector<float> YUV420ToRGB(const std::vector<std::vector<float> >& yuv420,
+                               const int width, const int height) {
+  std::vector<std::vector<float> > yuv;
+  yuv.push_back(yuv420[0]);
+  std::vector<float> u = Upsample2x2(yuv420[1], width, height);
+  std::vector<float> v = Upsample2x2(yuv420[2], width, height);
+  yuv.push_back(Blur(u, width, height));
+  yuv.push_back(Blur(v, width, height));
+  return YUVToRGB(yuv);
+}
+    
+    #include 'guetzli/output_image.h'
+    
+    // Decodes the parsed jpeg coefficients into an RGB image.
+// There can be only either 1 or 3 image components, in either case, an RGB
+// output image will be generated.
+// Only YUV420 and YUV444 sampling factors are supported.
+// Vector will be empty if a decoding error occurred.
+std::vector<uint8_t> DecodeJpegToRGB(const JPEGData& jpg);
+    
+    bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
+                     JPEGData* jpg) {
+  static const int quant[3 * kDCTBlockSize] = {
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  };
+  return EncodeRGBToJpeg(rgb, w, h, quant, jpg);
+}
+    
+        printer->Print(vars, '// Generated by the gRPC C++ plugin.\n');
+    printer->Print(vars,
+                   '// If you make any local change, they will be lost.\n');
+    printer->Print(vars, '// source: $filename$\n\n');
+    
+      // Appends the given text to the generated code as well as a newline
+  // character.  Any text within {{ and }} delimeters is replaced by values
+  // previously stored in the CodeWriter by calling SetValue above.  The newline
+  // will be suppressed if the text ends with the \\ character.
+  void operator+=(std::string text);
+    
+    int main(int /*argc*/, const char * /*argv*/ []) {
+  // Build up a serialized buffer algorithmically:
+  flatbuffers::FlatBufferBuilder builder;
     }
     
-    cv::cuda::GpuMat::GpuMat(const GpuMat& m, Range rowRange_, Range colRange_)
-{
-    flags = m.flags;
-    step = m.step; refcount = m.refcount;
-    data = m.data; datastart = m.datastart; dataend = m.dataend;
-    allocator = m.allocator;
-    }
+    std::string BaseGenerator::LastNamespacePart(const Namespace &ns) {
+  if (!ns.components.empty())
+    return ns.components.back();
+  else
+    return std::string('');
+}
     
-    // A small event handler class to process incoming events to
-// this window.
-class PGEventHandler : public SVEventHandler {
-  public:
-   PGEventHandler(tesseract::Tesseract* tess) : tess_(tess) {
-   }
-   void Notify(const SVEvent* sve);
-  private:
-    tesseract::Tesseract* tess_;
-};
-    
-    	if (NULL != _funcname) {
-		env->ReleaseStringUTFChars(_funcname, funcname_cstr);
-	}
-    
-    #include 'boost/function.hpp'
-    
-        while (-1 != __ParseStack(stream, state, strcache, strstack)) {
-        if (!_processname.empty() && std::string::npos == strstack.find(processname, 0)) {
-            strstack.clear();
-            continue;
-        }
-    }
-    
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
-    
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
+      bool any_generator = false;
+  bool print_make_rules = false;
+  bool raw_binary = false;
+  bool schema_binary = false;
+  bool grpc_enabled = false;
+  std::vector<std::string> filenames;
+  std::list<std::string> include_directories_storage;
+  std::vector<const char *> include_directories;
+  std::vector<const char *> conform_include_directories;
+  std::vector<bool> generator_enabled(params_.num_generators, false);
+  size_t binary_files_from = std::numeric_limits<size_t>::max();
+  std::string conform_to_schema;
