32b2c6034297729596b910d81bc75dda02029f8d
SUNsung <weatherius@gmail.com>
2018-02-18 19:34:23 +0200

    Rewrite [56LzyGKFpsjHM56BDZmQcfbR4k56T8TbIRVGt865639q.py] File in: 2018-02-18 19:34:23

--- pyton/56LzyGKFpsjHM56BDZmQcfbR4k56T8TbIRVGt865639q.py
+++ pyton/56LzyGKFpsjHM56BDZmQcfbR4k56T8TbIRVGt865639q.py
@@ -1,37 +1,14 @@
 
         
-            READY = 0
-    IN_PROGRESS = 1
-    COMPLETE = 2
-    
-        def get(self, query)
-        '''Get the stored query result from the cache.
         
-        Accessing a node updates its position to the front of the LRU list.
-        '''
-        node = self.lookup[query]
-        if node is None:
-            return None
-        self.linked_list.move_to_front(node)
-        return node.results
+class BadNonceTest(unittest.TestCase):
+    '''Tests for acme.errors.BadNonce.'''
     
-        def __init__(self, memory_cache, reverse_index_cluster):
-        self.memory_cache = memory_cache
-        self.reverse_index_cluster = reverse_index_cluster
+            self.addr1 = Addr.fromstring('127.0.0.1')
+        self.addr2 = Addr.fromstring('127.0.0.1:*')
     
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer),
-            self.mr(mapper=self.mapper_sort,
-                    reducer=self.reducer_identity),
-        ]
+        # Implement all methods from IInstaller, remembering to add
+    # 'self' as first argument, e.g. def get_all_names(self)...
+
     
-        def crawl_page(self, page):
-        for url in page.child_urls:
-            self.data_store.add_link_to_crawl(url)
-        self.reverse_index_queue.generate(page)
-        self.doc_index_queue.generate(page)
-        self.data_store.remove_link_to_crawl(page.url)
-        self.data_store.insert_crawled_link(page.url, page.signature)
+    scan_ip_log = Scan_ip_log()
