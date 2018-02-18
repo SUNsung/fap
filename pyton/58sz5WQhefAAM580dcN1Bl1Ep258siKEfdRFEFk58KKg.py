78f8f7714f758e347114e98417f63ab1a711bc96
SUNsung <weatherius@gmail.com>
2018-02-18 22:05:01 +0200

    Rewrite [58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py] File in: 2018-02-18 22:05:00

--- pyton/58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py
+++ pyton/58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py
@@ -1,99 +1,107 @@
-# revision identifiers, used by Alembic.
-revision = '12d55656cbca'
-down_revision = '55179c7f25c7'
-    
-    def upgrade():
-    op.add_column('dashboards', sa.Column('slug', sa.String(length=255), nullable=True))
+
+        
+        print('Enter the PKCS1 private key, followed by a blank line:')
+privkey = b''
+while True:
     try:
-        op.create_unique_constraint('idx_unique_slug', 'dashboards', ['slug'])
-    except:
-        pass
+        line = input()
+    except EOFError:
+        break
+    if line == '':
+        break
+    privkey += line.encode('ascii') + b'\n'
+privkey = rsa.PrivateKey.load_pkcs1(privkey)
     
-    '''
+        # Get the version from youtube_dl/version.py without importing the package
+    exec(compile(open('youtube_dl/version.py').read(),
+                 'youtube_dl/version.py', 'exec'))
+    
+    README_FILE = 'README.md'
+helptext = sys.stdin.read()
     
     
-def downgrade():
-    op.drop_column('dbs', 'password')
-
+def _download_restricted(url, filename, age):
+    ''' Returns true if the file has been downloaded '''
     
-    from alembic import op
-import sqlalchemy as sa
     
-    '''
+def _is_empty(d):
+    return not bool(os.listdir(d))
     
+        def test_func(self):
+        as_file = os.path.join(TEST_DIR, testfile)
+        swf_file = os.path.join(TEST_DIR, test_id + '.swf')
+        if ((not os.path.exists(swf_file)) or
+                os.path.getmtime(swf_file) < os.path.getmtime(as_file)):
+            # Recompile
             try:
-            result = self.fn(*self.args, **self.kwargs)
-        except BaseException:
-            e = sys.exc_info()[1]
-            self.future.set_exception(e)
-        else:
-            self.future.set_result(result)
+                subprocess.check_call([
+                    'mxmlc', '-output', swf_file,
+                    '-static-link-runtime-shared-libraries', as_file])
+            except OSError as ose:
+                if ose.errno == errno.ENOENT:
+                    print('mxmlc not found! Skipping test.')
+                    return
+                raise
     
-            Args:
-            wait: If True then shutdown will not return until all running
-                futures have finished executing and the resources used by the
-                executor have been reclaimed.
-        '''
-        pass
+            # TODO: handle s and e stage_mode (live streams and ended live streams)
+        if stage_mode not in ('s', 'e'):
+            request = sanitized_Request(
+                'https://audimedia.tv/api/video/v1/videos/%s?embed[]=video_versions&embed[]=thumbnail_image&where[content_language_iso]=%s' % (video_id, lang),
+                headers={'X-Auth-Token': self._AUTH_TOKEN})
+            json_data = self._download_json(request, video_id)['results']
+            formats = []
     
-        # For pickling to work, the __module__ variable needs to be set to the frame
-    # where the named tuple is created.  Bypass this step in enviroments where
-    # sys._getframe is not defined (Jython for example).
-    if hasattr(_sys, '_getframe'):
-        result.__module__ = _sys._getframe(1).f_globals.get('__name__', '__main__')
+        def _real_extract(self, url):
+        # URLs end with [uploader name]/[uploader title]
+        # this title is whatever the user types in, and is rarely
+        # the proper song title.  Real metadata is in the api response
+        album_url_tag = self._match_id(url)
     
-    class ThreadPoolExecutor(_base.Executor):
-    def __init__(self, max_workers):
-        '''Initializes a new ThreadPoolExecutor instance.
+    from .common import InfoExtractor
+from ..utils import (
+    int_or_none,
+    parse_duration,
+    unified_strdate,
+)
     
-      # This method blocks
-  # |timeout| is num seconds to tolerate no response from server before giving
-  # up; see Requests docs for details (we just pass the param along).
-  @staticmethod
-  def GetDataFromHandler( handler, timeout = _READ_TIMEOUT_SEC ):
-    return JsonFromFuture( BaseRequest._TalkToHandlerAsync( '',
-                                                            handler,
-                                                            'GET',
-                                                            timeout ) )
+            self.host = host
+        self.port = port
+        self.requests_to_handle = requests_to_handle
+    
+        # Copy is required
+    def copy(self):
+        return CaseInsensitiveDict(self._store.values())
+    
+        @possible_keys
+    def test_delitem(self, key):
+        del self.case_insensitive_dict[key]
+        assert key not in self.case_insensitive_dict
+    
+    import pytest
+import requests
+from tests.testserver.server import Server
+    
+    def kugou_download_playlist(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+    html=get_html(url)
+    pattern=re.compile('title='(.*?)'.* data='(\w*)\|.*?'')
+    pairs=pattern.findall(html)
+    for title,hash_val in pairs:
+        kugou_download_by_hash(title,hash_val,output_dir,merge,info_only)
+    
+    def kuwo_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+    if 'www.kuwo.cn/yinyue' in url:
+        rid=match1(url,'yinyue/(\d+)')
+        kuwo_download_by_rid(rid,output_dir, merge, info_only)
+    else:
+        kuwo_playlist_download(url,output_dir,merge,info_only)
+    
+    __all__ = ['miomio_download']
     
     
-def RegexSingleList_test():
-  opts = _JavaFilter( { 'regex' : [ 'taco' ] }  )
-  f = _CreateFilterForTypes( opts, [ 'java' ] )
+def mtv81_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_content(url)
+    title = HTMLParser().unescape(
+        '|'.join(match1(html, r'<title>(.*?)</title>').split('|')[:-2]))
     
-        return config
-
-    
-            Always succeeds, even if the proxy has not yet been initialized.
-        Normally, if the proxy hasn't been initialized, a `TypeError` is
-        raised to indicate a programming error. To avoid crashing on feature
-        checks that are done too early (e.g., during initial DB set-up of
-        the pylons environment), this function will instead return `default`
-        for an uninitialized proxy.
-    
-    
-class ExtensionMiddleware(object):
-    ext_pattern = re.compile(r'\.([^/]+)\Z')
-    
-    
-def register_api_templates(template_name, template_class):
-    for style in ('api', 'api-html', 'api-compact'):
-        tpm.add_handler(
-            name=template_name,
-            style=style,
-            handler=template_class,
-        )
-    
-        def _login(self, responder, user, rem=None):
-        '''Login the user.
-    
-    from webob.exc import HTTPFound, HTTPMovedPermanently
-from pylons.i18n import _
-from pylons import request, response
-from pylons import tmpl_context as c
-from pylons import app_globals as g
-    
-    import pylibmc
-from pylons import request, response
-from pylons import app_globals as g
-from pylons.controllers.util import abort
+            self.title = match1(content,
+                            r'setup\[\'title\'\] = '([^']+)';')
