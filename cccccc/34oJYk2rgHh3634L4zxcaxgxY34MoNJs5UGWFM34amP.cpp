39eaf8680325a5344c338ef250d5dad507f7da81
SUNsung <weatherius@gmail.com>
2018-02-18 02:38:45 +0200

    Rewrite [34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp] File in: 2018-02-18 02:38:45

--- cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
+++ cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
@@ -1,136 +1,204 @@
 
         
-        enum CommentPlacement {
-  commentBefore = 0,      ///< a comment placed on the line before a value
-  commentAfterOnSameLine, ///< a comment just after a value on the same line
-  commentAfter, ///< a comment on the line after a value (only make sense for
-  /// root value)
-  numberOfCommentPlacement
-};
+        bool js_cocos2dx_physics3d_Physics3D6DofConstraint_constructor(JSContext *cx, uint32_t argc, jsval *vp);
+void js_cocos2dx_physics3d_Physics3D6DofConstraint_finalize(JSContext *cx, JSObject *obj);
+void js_register_cocos2dx_physics3d_Physics3D6DofConstraint(JSContext *cx, JS::HandleObject global);
+void register_all_cocos2dx_physics3d(JSContext* cx, JS::HandleObject obj);
+bool js_cocos2dx_physics3d_Physics3D6DofConstraint_setLinearLowerLimit(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3D6DofConstraint_getLinearLowerLimit(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3D6DofConstraint_getAngularUpperLimit(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3D6DofConstraint_getUseFrameOffset(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3D6DofConstraint_getLinearUpperLimit(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3D6DofConstraint_setAngularLowerLimit(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3D6DofConstraint_isLimited(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3D6DofConstraint_setUseFrameOffset(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3D6DofConstraint_setLinearUpperLimit(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3D6DofConstraint_getAngularLowerLimit(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3D6DofConstraint_setAngularUpperLimit(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3D6DofConstraint_create(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3D6DofConstraint_Physics3D6DofConstraint(JSContext *cx, uint32_t argc, jsval *vp);
     
-    bool Value::isNull() const { return type_ == nullValue; }
     
-    #include <google/protobuf/compiler/code_generator.h>
-#include <google/protobuf/compiler/plugin.h>
-#include <google/protobuf/descriptor.h>
-#include <google/protobuf/descriptor.pb.h>
-#include <google/protobuf/io/printer.h>
-#include <google/protobuf/io/zero_copy_stream.h>
-#include <google/protobuf/stubs/strutil.h>
+    
+    
+    
+    
+    
+    extern TestEntry g_testEntries[];
+// This is called when a joint in the world is implicitly destroyed
+// because an attached body is destroyed. This gives us a chance to
+// nullify the mouse joint.
+class DestructionListener : public b2DestructionListener
+{
+public:
+	void SayGoodbye(b2Fixture* fixture) { B2_NOT_USED(fixture); }
+	void SayGoodbye(b2Joint* joint);
+    }
+    
+    			m_body = m_world->CreateBody(&bd);
+			m_body->CreateFixture(&box, 1.0f);
+    
+    			b2PolygonShape shape;
+			shape.Set(vertices, 3);
+    
+    #include <xgboost/logging.h>
+#include <cctype>
+#include <cstdio>
+#include <string>
+#include './io.h'
     
     
     { private:
-  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(MapFieldGenerator);
+  std::istream &fin;
 };
     
-    const Options* SourceGeneratorBase::options() {
-  return this->options_;
-}
+    /*!
+ * \brief global singleton of a random engine.
+ *  This random engine is thread-local and
+ *  only visible to current thread.
+ */
+GlobalRandomEngine& GlobalRandom(); // NOLINT(*)
     
-    namespace google {
-namespace protobuf {
-  namespace io {
-    class Printer;             // printer.h
-  }
-}
+        // want to compute storage boundary for each feature
+    // using variants of prefix sum scan
+    boundary_.resize(nfeature);
+    size_t accum_index_ = 0;
+    size_t accum_row_ind_ = 0;
+    for (bst_uint fid = 0; fid < nfeature; ++fid) {
+      boundary_[fid].index_begin = accum_index_;
+      boundary_[fid].row_ind_begin = accum_row_ind_;
+      if (type_[fid] == kDenseColumn) {
+        accum_index_ += static_cast<size_t>(nrow);
+      } else {
+        accum_index_ += feature_counts_[fid];
+        accum_row_ind_ += feature_counts_[fid];
+      }
+      boundary_[fid].index_end = accum_index_;
+      boundary_[fid].row_ind_end = accum_row_ind_;
     }
     
-     private:
-  const FieldDescriptor* descriptor_;
-  Context* context_;
-  ClassNameResolver* name_resolver_;
-  string scope_;
-    
-    
-    {  // Calculate num_trailing and trailing_certainty.
-  for (*trailing_certainty = 0.0f, *num_rebuilt_trailing = 0;
-       *num_rebuilt_trailing < trailing_outliers;
-       (*num_rebuilt_trailing)++) {
-    int blob_idx = num_blobs - 1 - *num_rebuilt_trailing;
-    float char_certainty = word->best_choice->certainty(blob_idx);
-    if (char_certainty > *unlikely_threshold) {
-      break;
-    }
-    if (char_certainty < *trailing_certainty) {
-      *trailing_certainty = char_certainty;
-    }
-  }
-}
-    
-      // Sets up the norm_truth_word from truth_word using the given DENORM.
-  void SetupNormTruthWord(const DENORM& denorm);
-    
-    
-    {  name += UNLV_EXT;              //add extension
-  if ((pdfp = fopen (name.string (), 'rb')) == NULL) {
-    return false;                //didn't read one
-  } else {
-    while (tfscanf(pdfp, '%d %d %d %d %*s', &x, &y, &width, &height) >= 4) {
-                                 //make rect block
-      block = new BLOCK (name.string (), TRUE, 0, 0,
-                         (inT16) x, (inT16) (ysize - y - height),
-                         (inT16) (x + width), (inT16) (ysize - y));
-                                 //on end of list
-      block_it.add_to_end (block);
-    }
-    fclose(pdfp);
-  }
-  return true;
-}
-    
-    
-    {    ICOORD pt;
-    int halfwidth;
-  };
-  // Type holds the distance of each point from the fitted line and the point
-  // itself. Use of double allows integer distances from ICOORDs to be stored
-  // exactly, and also the floating point results from ConstrainedFit.
-  typedef KDPairInc<double, ICOORD> DistPointPair;
-    
-    double LLSQ::m() const {  // get gradient
-  double covar = covariance();
-  double x_var = x_variance();
-  if (x_var != 0.0)
-    return covar / x_var;
-  else
-    return 0.0;                    // too little
-}
-    
-    
-/**********************************************************************
- * ROW::ROW
- *
- * Constructor to build a ROW. Only the stats stuff are given here.
- * The words are added directly.
- **********************************************************************/
-    
-        void close() {
-        loop->post([this]() {
-            delete this;
-        });
+        if (EXPECTS_CRASH_DUMP_HEADER == _state) {
+        _state = EXPECTS_CRASH_DUMP_CONTENT;
+        strout += _strcache;
     }
     
+    #include 'mars/comm/autobuffer.h'
     
-    {        delete transferData;
-        transferCb(s);
+    namespace mars_boost {} namespace boost = mars_boost; namespace mars_boost {
     }
     
-            uv_os_sock_t listenFd = SOCKET_ERROR;
-        addrinfo *listenAddr;
-        if ((options & uS::ONLY_IPV4) == 0) {
-            for (addrinfo *a = result; a && listenFd == SOCKET_ERROR; a = a->ai_next) {
-                if (a->ai_family == AF_INET6) {
-                    listenFd = netContext->createSocket(a->ai_family, a->ai_socktype, a->ai_protocol);
-                    listenAddr = a;
-                }
-            }
+    // Licensed under the MIT License (the 'License'); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+// http://opensource.org/licenses/MIT
+    
+    #define DEFINE_HAS_MEMBER(member_name) \
+    template <typename T>\
+    class has_##member_name {\
+      private:\
+        struct yes_type { char x[1]; };\
+        struct no_type { char x[2]; };\
+        template <int> struct tester;\
+        template <typename U> static yes_type test(tester<sizeof(&U::member_name)>*);\
+        template <typename U> static no_type test(...);\
+      public:\
+        static const bool value = (sizeof(test<T>(0)) == sizeof(yes_type));\
+    };
+    
+    
+    {
+    {        if (JNI_OK == status_) {
+            we_attach_ = true;
+            pthread_setspecific(g_env_key, env_);
+        } else {
+            ASSERT2(false, 'vm:%p, env:%p, status:%d', vm_, env_, status_);
+            env_ = NULL;
+            return;
         }
+    } while (false);
     
-    template <bool isServer>
-struct WIN32_EXPORT Group : protected uS::NodeData {
-protected:
-    friend struct Hub;
-    friend struct WebSocket<isServer>;
-    friend struct HttpSocket<false>;
-    friend struct HttpSocket<true>;
+    jint ret = env_->PushLocalFrame(_capacity);
+    ASSERT2(0 == ret, 'ret:%d', ret);
+}
+    
+    std::function<size_t(size_t)> DeterministicSchedule::uniform(uint64_t seed) {
+  auto rand = std::make_shared<std::ranlux48>(seed);
+  return [rand](size_t numActive) {
+    auto dist = std::uniform_int_distribution<size_t>(0, numActive - 1);
+    return dist(*rand);
+  };
+}
+    
+      T fetch_or(T v,
+             std::memory_order /* mo */ = std::memory_order_seq_cst) noexcept {
+    DeterministicSchedule::beforeSharedAccess();
+    T rv = data;
+    data |= v;
+    FOLLY_TEST_DSCHED_VLOG(this << '.fetch_or(' << std::hex << v << ') -> '
+                                << std::hex << rv);
+    DeterministicSchedule::afterSharedAccess(true);
+    return rv;
+  }
+    
+      // Calculation of threshold to move credits in bulk from consumers
+  // to producers
+  constexpr Weight threshold(Weight capacity) const noexcept {
+    return capacity / 10;
+  }
+    
+    
+    {
+    {    return allocateHard();
+  }
+  void deallocate(void* mem) {
+    std::lock_guard<std::mutex> g(m_);
+    *static_cast<void**>(mem) = freelist_;
+    freelist_ = mem;
+  }
+};
+    
+    TEST(CacheLocality, FakeSysfs) {
+  auto parsed = CacheLocality::readFromSysfsTree([](std::string name) {
+    auto iter = fakeSysfsTree.find(name);
+    return iter == fakeSysfsTree.end() ? std::string() : iter->second;
+  });
     }
+    
+    #endif // D_ABSTRACT_AUTH_RESOLVER_H
+
+    
+      virtual void addTag(uint32_t tag) CXX11_OVERRIDE;
+    
+    namespace aria2 {
+    }
+    
+    class BinaryStream;
+    
+    
+    {  return false;
+}
+    
+    void AnnounceTier::nextEvent()
+{
+  switch (event) {
+  case STARTED:
+    event = DOWNLOADING;
+    break;
+  case STARTED_AFTER_COMPLETION:
+    event = SEEDING;
+    break;
+  case STOPPED:
+    event = HALTED;
+    break;
+  case COMPLETED:
+    event = SEEDING;
+    break;
+  default:
+    break;
+  }
+}
+    
+    #else // defined(__MAC_10_6)
+  return false;
+    
+    void configureAsyncNameResolverMan(AsyncNameResolverMan* asyncNameResolverMan,
+                                   Option* option);
