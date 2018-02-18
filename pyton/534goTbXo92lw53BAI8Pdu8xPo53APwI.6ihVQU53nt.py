c60d4cee1c7001bc9d89ada88ba50e20c2ee74c1
SUNsung <weatherius@gmail.com>
2018-02-18 04:33:39 +0200

    Rewrite [534goTbXo92lw53BAI8Pdu8xPo53APwI.6ihVQU53nt.py] File in: 2018-02-18 04:33:39

--- pyton/534goTbXo92lw53BAI8Pdu8xPo53APwI.6ihVQU53nt.py
+++ pyton/534goTbXo92lw53BAI8Pdu8xPo53APwI.6ihVQU53nt.py
@@ -1,83 +1,232 @@
 
-def main():
-    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
-    options, args = parser.parse_args()
-    if len(args) != 2:
-        parser.error('Expected an input and an output filename')
+        
+        filenames = {
+    'bin': 'youtube-dl',
+    'exe': 'youtube-dl.exe',
+    'tar': 'youtube-dl-%s.tar.gz' % version}
+build_dir = os.path.join('..', '..', 'build', version)
+for key, filename in filenames.items():
+    url = 'https://yt-dl.org/downloads/%s/%s' % (version, filename)
+    fn = os.path.join(build_dir, filename)
+    with open(fn, 'rb') as f:
+        data = f.read()
+    if not data:
+        raise ValueError('File %s is empty!' % fn)
+    sha256sum = hashlib.sha256(data).hexdigest()
+    new_version[key] = (url, sha256sum)
     
-    from youtube_dl.extractor import (
-    FacebookIE,
-    gen_extractors,
-    YoutubeIE,
+    entries = []
+for v in versions:
+    fields = v.split('.')
+    year, month, day = map(int, fields[:3])
+    faked = 0
+    patchlevel = 0
+    while True:
+        try:
+            datetime.date(year, month, day)
+        except ValueError:
+            day -= 1
+            faked += 1
+            assert day > 0
+            continue
+        break
+    if len(fields) >= 4:
+        try:
+            patchlevel = int(fields[3])
+        except ValueError:
+            patchlevel = 1
+    timestamp = '%04d-%02d-%02dT00:%02d:%02dZ' % (year, month, day, faked, patchlevel)
+    
+    README_FILE = 'README.md'
+helptext = sys.stdin.read()
+    
+    py_binary(
+    name = 'cifar10_train',
+    srcs = [
+        'cifar10_train.py',
+    ],
+    srcs_version = 'PY2AND3',
+    visibility = ['//tensorflow:__subpackages__'],
+    deps = [
+        ':cifar10',
+    ],
 )
     
-    
-class TestSocks(unittest.TestCase):
-    _SKIP_SOCKS_TEST = True
-    
-            video_url = self._html_search_regex(r'video_url\s*:\s*'([^']+)'', webpage, 'video URL')
-    
-    from .onet import OnetBaseIE
-    
-    
-def test_messages(client, app):
-    '''Test that messages work'''
-    login(client, app.config['USERNAME'],
-          app.config['PASSWORD'])
-    rv = client.post('/add', data=dict(
-        title='<Hello>',
-        text='<strong>HTML</strong> allowed here'
-    ), follow_redirects=True)
-    assert b'No entries here so far' not in rv.data
-    assert b'&lt;Hello&gt;' in rv.data
-    assert b'<strong>HTML</strong> allowed here' in rv.data
+    filegroup(
+    name = 'all_files',
+    srcs = glob(
+        ['**/*'],
+        exclude = [
+            '**/METADATA',
+            '**/OWNERS',
+        ],
+    ),
+    visibility = ['//tensorflow:__subpackages__'],
+)
 
     
+        # Global step: scalar, i.e., shape [].
+    w_out = tf.Variable(tf.zeros([opts.vocab_size, opts.emb_dim]), name='w_out')
     
-def gravatar_url(email, size=80):
-    '''Return the gravatar image for the given email address.'''
-    return 'https://www.gravatar.com/avatar/%s?d=identicon&s=%d' % \
-        (md5(email.strip().lower().encode('utf-8')).hexdigest(), size)
+                gc.collect()
+            print('- benchmarking LassoLars')
+            clf = LassoLars(alpha=alpha, fit_intercept=False,
+                            normalize=False, precompute=precompute)
+            tstart = time()
+            clf.fit(X, Y)
+            lars_lasso_results.append(time() - tstart)
     
-        :copyright: © 2010 by the Pallets team.
-    :license: BSD, see LICENSE for more details.
+        if args.plot is not None:
+        print('Displaying plot', file=sys.stderr)
+        title = ('Multilabel metrics with %s' %
+                 ', '.join('{0}={1}'.format(field, getattr(args, field))
+                           for field in ['samples', 'classes', 'density']
+                           if args.plot != field))
+        _plot(results, args.metrics, args.formats, title, steps, args.plot)
+
+    
+        # decode the payload explicitly as UTF-8 since lxml is confused for some
+    # reason
+    with codecs.open(html_filename,'r','utf-8') as html_file:
+        html_content = html_file.read()
+    tree = ElementTree(lxml.html.document_fromstring(html_content))
+    i = 0
+    j = 0
+    for p in tree.findall('//p'):
+        content = p.text_content()
+        if len(content) < 100:
+            # skip paragraphs that are too short - probably too noisy and not
+            # representative of the actual language
+            continue
+    
+    import os
+import tarfile
+from contextlib import closing
+try:
+    from urllib import urlopen
+except ImportError:
+    from urllib.request import urlopen
+    
+        # TASK: print the cross-validated scores for the each parameters set
+    # explored by the grid search
+    
+    Both kinds of calibration can fix this issue and yield nearly identical
+results. This shows that sigmoid calibration can deal with situations where
+the calibration curve of the base classifier is sigmoid (e.g., for LinearSVC)
+but not where it is transposed-sigmoid (e.g., Gaussian naive Bayes).
 '''
+print(__doc__)
     
-            def validate(sig):
-            return (b'value' ==
-                    decode_signed_value(SignedValueTest.SECRET, 'key',
-                                        prefix + sig, clock=self.present))
-        self.assertTrue(validate(
-            '3d4e60b996ff9c5d5788e333a0cba6f238a22c6c0f94788870e1a9ecd482e152'))
-        # All zeros
-        self.assertFalse(validate('0' * 32))
-        # Change one character
-        self.assertFalse(validate(
-            '4d4e60b996ff9c5d5788e333a0cba6f238a22c6c0f94788870e1a9ecd482e152'))
-        # Change another character
-        self.assertFalse(validate(
-            '3d4e60b996ff9c5d5788e333a0cba6f238a22c6c0f94788870e1a9ecd482e153'))
-        # Truncate
-        self.assertFalse(validate(
-            '3d4e60b996ff9c5d5788e333a0cba6f238a22c6c0f94788870e1a9ecd482e15'))
-        # Lengthen
-        self.assertFalse(validate(
-            '3d4e60b996ff9c5d5788e333a0cba6f238a22c6c0f94788870e1a9ecd482e1538'))
     
-    from tornado import autoreload
+class BadNonceTest(unittest.TestCase):
+    '''Tests for acme.errors.BadNonce.'''
     
-    from tornado.escape import utf8
-from tornado.httpclient import HTTPRequest
-from tornado.locks import Event
-from tornado.stack_context import ExceptionStackContext
-from tornado.testing import AsyncHTTPTestCase, gen_test
-from tornado.test import httpclient_test
-from tornado.test.util import unittest
-from tornado.web import Application, RequestHandler
+        # Implement all methods from IAuthenticator, remembering to add
+    # 'self' as first argument, e.g. def prepare(self)...
     
-        @gen_test
-    def test_acquire_timeout(self):
-        lock = locks.Lock()
-        lock.acquire()
-        with self.assertRaises(gen.TimeoutError):
-            yield lock.acquire(timeout=timedelta(seconds=0.01))
+    PROJ_NAME = 'you-get'
+PACKAGE_NAME = 'you_get'
+    
+                    log.println('    platform: {}'.format(platform.platform()))
+                log.println('    python:   {}'.format(sys.version.split('\n')[0]))
+    
+        def extract(self, **kwargs):
+        for i in self.streams:
+            s = self.streams[i]
+            _, s['container'], s['size'] = url_info(s['url'])
+            s['src'] = [s['url']]
+    
+    def kuwo_download_by_rid(rid, output_dir = '.', merge = True, info_only = False):
+    html=get_content('http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_%s'%rid)
+    title=match1(html,r'<name>(.*)</name>')
+    #to get title
+    #format =aac|mp3 ->to get aac format=mp3 ->to get mp3
+    url=get_content('http://antiserver.kuwo.cn/anti.s?format=mp3&rid=MUSIC_%s&type=convert_url&response=url'%rid)
+    songtype, ext, size = url_info(url)
+    print_info(site_info, title, songtype, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir)
+    
+    from ..common import *
+import urllib.error
+from json import loads
+from time import time, sleep
+    
+    >>> def roundrobin(*iterables):
+...     pending = deque(iter(i) for i in iterables)
+...     while pending:
+...         task = pending.popleft()
+...         try:
+...             yield next(task)
+...         except StopIteration:
+...             continue
+...         pending.append(task)
+...
+    
+    def pi_cdecimal():
+    '''cdecimal'''
+    D = C.Decimal
+    lasts, t, s, n, na, d, da = D(0), D(3), D(3), D(1), D(0), D(0), D(24)
+    while s != lasts:
+        lasts = s
+        n, na = n+na, na+8
+        d, da = d+da, da+32
+        t = (t * n) / d
+        s += t
+    return s
+    
+        @classmethod
+    def zonelist(cls, zonedir='/usr/share/zoneinfo'):
+        zones = []
+        for root, _, files in os.walk(zonedir):
+            for f in files:
+                p = os.path.join(root, f)
+                with open(p, 'rb') as o:
+                    magic =  o.read(4)
+                if magic == b'TZif':
+                    zones.append(p[len(zonedir) + 1:])
+        return zones
+    
+                self.assertIn('Project-Id-Version', header)
+            self.assertIn('POT-Creation-Date', header)
+            self.assertIn('PO-Revision-Date', header)
+            self.assertIn('Last-Translator', header)
+            self.assertIn('Language-Team', header)
+            self.assertIn('MIME-Version', header)
+            self.assertIn('Content-Type', header)
+            self.assertIn('Content-Transfer-Encoding', header)
+            self.assertIn('Generated-By', header)
+    
+            if self.closed:
+            raise ValueError('I/O operation on closed file')
+        if self.seekable:
+            try:
+                n = self.chunksize - self.size_read
+                # maybe fix alignment
+                if self.align and (self.chunksize & 1):
+                    n = n + 1
+                self.file.seek(n, 1)
+                self.size_read = self.size_read + n
+                return
+            except OSError:
+                pass
+        while self.size_read < self.chunksize:
+            n = min(8192, self.chunksize - self.size_read)
+            dummy = self.read(n)
+            if not dummy:
+                raise EOFError
+
+    
+    def py_scanstring(s, end, strict=True,
+        _b=BACKSLASH, _m=STRINGCHUNK.match):
+    '''Scan the string s for a JSON string. End is the index of the
+    character in s after the quote that started the JSON string.
+    Unescapes all valid JSON string escape sequences and raises ValueError
+    on attempt to decode an invalid string. If strict is False then literal
+    control characters are allowed in the string.
+    
+    
+class DOMBuilder:
+    entityResolver = None
+    errorHandler = None
+    filter = None
