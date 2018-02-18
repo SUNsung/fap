e830007680b301d7a4c52be8f61449f9f956ab87
SUNsung <weatherius@gmail.com>
2018-02-18 20:00:14 +0200

    Create [14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py] File in: 2018-02-18 20:00:14

---
+++ pyton/14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py
@@ -0,0 +1,6 @@
+    # Sorting the libraries
+    inner_blocks = sorted(blocks[0].split('##'))
+    for i in range(1 , len(inner_blocks)):
+        if inner_blocks[i][0] != '#':
+            inner_blocks[i] = '##' + inner_blocks[i]
+    inner_blocks=''.join(inner_blocks)
