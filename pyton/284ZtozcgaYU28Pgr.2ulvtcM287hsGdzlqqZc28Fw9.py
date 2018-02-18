c3658b07292c55b3c01671090cf432d2938ae422
SUNsung <weatherius@gmail.com>
2018-02-18 13:27:13 +0200

    Rewrite [284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py] File in: 2018-02-18 13:27:13

--- pyton/284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py
+++ pyton/284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py
@@ -1,141 +1,155 @@
 
         
-            def long_desc(self):
-        '''A long description of the command. Return short description when not
-        available. It cannot contain newlines, since contents will be formatted
-        by optparser which removes newlines and wraps text.
+            # noinspection PyProtectedMember
+    @property
+    def headers(self):
+        original = self._orig.raw._original_response
+    
+        def __init__(self, groups, env=Environment(), **kwargs):
         '''
-        return self.short_desc()
+        :param groups: names of processor groups to be applied
+        :param env: Environment
+        :param kwargs: additional keyword arguments for processors
     
-    from six.moves.urllib.parse import urlencode
+        # Will be set to the raw value of `-a` (if provided) before
+    # `get_auth()` gets called.
+    raw_auth = None
     
-            infos = []
-        if not self.wasSuccessful():
-            write('FAILED')
-            failed, errored = map(len, (self.failures, self.errors))
-            if failed:
-                infos.append('failures=%d' % failed)
-            if errored:
-                infos.append('errors=%d' % errored)
-        else:
-            write('OK')
+        def test_verify_custom_ca_bundle_invalid_path(self, httpbin_secure):
+        # since 2.14.0 requests raises IOError
+        with pytest.raises((SSLError, IOError)):
+            http(httpbin_secure.url + '/get', '--verify', '/__not_found__')
     
-            try:
-            spidercls = self.crawler_process.spider_loader.load(name)
-        except KeyError:
-            pass
-        else:
-            # if spider already exists and not --force then halt
-            if not opts.force:
-                print('Spider %r already exists in module:' % name)
-                print('  %s' % spidercls.__module__)
-                return
-        template_file = self._find_template(opts.template)
-        if template_file:
-            self._genspider(module, name, domain, opts.template, template_file)
-            if opts.edit:
-                self.exitcode = os.system('scrapy edit '%s'' % name)
+            if response.status_code != 200:
+            path += '.error'
     
+        target.add_argument('--run',
+                        metavar='ID',
+                        help='Shippable run ID')
     
-class CallCenter(object):
+      def setUp(self):
+    self._string_data = '\n'.join(
+        [' hello there i am',
+         ' rain as day',
+         ' want some cheesy puffs ?'])
     
-        def take_spot(self, spot):
-        self.spots_taken.append(spot)
-    
-        def __init__(self, seller_category_map, seller_category_overrides_map):
-        self.seller_category_map = seller_category_map
-        self.seller_category_overrides_map = seller_category_overrides_map
+    py_library(
+    name = 'cifar10_input',
+    srcs = ['cifar10_input.py'],
+    srcs_version = 'PY2AND3',
+    visibility = ['//tensorflow:internal'],
+    deps = [
+        '//tensorflow:tensorflow_py',
+    ],
+)
     
     
-class HitCounts(MRJob):
+def self_test():
+  '''Test the translation model.'''
+  with tf.Session() as sess:
+    print('Self-test for neural translation model.')
+    # Create model with vocabularies of 10, 2 small buckets, 2 layers of 32.
+    model = seq2seq_model.Seq2SeqModel(10, 10, [(3, 3), (6, 6)], 32, 2,
+                                       5.0, 32, 0.3, 0.99, num_samples=8)
+    sess.run(tf.global_variables_initializer())
     
-    - RecognitionException
-  - MismatchedRangeException
-  - MismatchedSetException
-    - MismatchedNotSetException
-    .
-  - MismatchedTokenException
-  - MismatchedTreeNodeException
-  - NoViableAltException
-  - EarlyExitException
-  - FailedPredicateException
-  .
-.
+      Returns:
+    A tuple of 6 elements:
+      (1) path to the token-ids for English training data-set,
+      (2) path to the token-ids for French training data-set,
+      (3) path to the token-ids for English development data-set,
+      (4) path to the token-ids for French development data-set,
+      (5) path to the English vocabulary file,
+      (6) path to the French vocabulary file.
+  '''
+  # Get wmt data to the specified directory.
+  train_path = get_wmt_enfr_train_set(data_dir)
+  dev_path = get_wmt_enfr_dev_set(data_dir)
     
-                    if c >= self.min[s] and c <= self.max[s]:
-                    # move to next state
-                    snext = self.transition[s][c-self.min[s]]
-                    #print 'in range, next state = %d' % snext
-                    
-                    if snext < 0:
-                        #print 'not a normal transition'
-                        # was in range but not a normal transition
-                        # must check EOT, which is like the else clause.
-                        # eot[s]>=0 indicates that an EOT edge goes to another
-                        # state.
-                        if self.eot[s] >= 0: # EOT Transition to accept state?
-                            #print 'EOT trans to accept state %d' % self.eot[s]
-                            
-                            s = self.eot[s]
-                            input.consume()
-                            # TODO: I had this as return accept[eot[s]]
-                            # which assumed here that the EOT edge always
-                            # went to an accept...faster to do this, but
-                            # what about predicated edges coming from EOT
-                            # target?
-                            continue
+    # Check minimum required Python version
+import sys
+if sys.version_info < (2, 7):
+    print('Scrapy %s requires Python 2.7' % __version__)
+    sys.exit(1)
     
-    
-class MismatchedRangeException(RecognitionException):
-    '''@brief The next token does not match a range of expected types.'''
-    
-    
-    def getCharIndex(self):
-        '''What is the index of the current character of lookahead?'''
-        
-        return self.input.index()
-    
-    
-class UnsafeThreadPoolExecutor(_base.Executor):
-    def __init__(self, max_workers):
-        '''Initializes a new ThreadPoolExecutor instance.
-    
-    if __name__ == '__main__':
-    main()
+        def run(self, args, opts):
+        '''
+        Entry point for running commands
+        '''
+        raise NotImplementedError
 
     
-        if not self._response_future or self._event_name != 'FileReadyToParse':
-      return []
+            if infos:
+            writeln(' (%s)' % (', '.join(infos),))
+        else:
+            write('\n')
     
+        if C is not None:
+        # C version of decimal
+        start_calc = time.time()
+        x = factorial(C.Decimal(n), 0)
+        end_calc = time.time()
+        start_conv = time.time()
+        sx = str(x)
+        end_conv = time.time()
+        print('cdecimal:')
+        print('calculation time: %fs' % (end_calc-start_calc))
+        print('conversion time: %fs\n' % (end_conv-start_conv))
     
-  def Start( self ):
-    request_data = BuildRequestData()
-    request_data.update( { 'filetypes': self.filetypes } )
-    with HandleServerException():
-      self._response = self.PostDataToHandler( request_data,
-                                               'semantic_completion_available' )
+        def get_message(self, key):
+        '''Return a Message representation or raise a KeyError.'''
+        subpath = self._lookup(key)
+        with open(os.path.join(self._path, subpath), 'rb') as f:
+            if self._factory:
+                msg = self._factory(f)
+            else:
+                msg = MaildirMessage(f)
+        subdir, name = os.path.split(subpath)
+        msg.set_subdir(subdir)
+        if self.colon in name:
+            msg.set_info(name.split(self.colon)[-1])
+        msg.set_date(os.path.getmtime(os.path.join(self._path, subpath)))
+        return msg
     
+        def parseURI(self, uri):
+        if self.entityResolver:
+            input = self.entityResolver.resolveEntity(None, uri)
+        else:
+            input = DOMEntityResolver().resolveEntity(None, uri)
+        return self.parse(input)
     
-  def IsAllowed( self, diagnostic ):
-    # NOTE: a diagnostic IsAllowed() ONLY if NO filters match it
-    for filterMatches in self._filters:
-      if filterMatches( diagnostic ):
-        return False
+        # We only ever write one 'entry point' symbol - either
+    # 'main' or 'WinMain'.  Therefore, there is no need to
+    # pass a subsystem switch to the linker as it works it
+    # out all by itself.  However, the subsystem _does_ determine
+    # the file extension and additional linker flags.
+    target_link_flags = ''
+    target_ext = '.exe'
+    if subsystem_details[vars['subsystem']][2]:
+        target_link_flags = '-dll'
+        target_ext = '.dll'
     
+        def test_range_iterators_invocation(self):
+        # verify range iterators instances cannot be created by
+        # calling their type
+        rangeiter_type = type(iter(range(0)))
+        self.assertRaises(TypeError, rangeiter_type, 1, 3, 1)
+        long_rangeiter_type = type(iter(range(1 << 1000)))
+        self.assertRaises(TypeError, long_rangeiter_type, 1, 3, 1)
     
-def FormatDebugInfoResponse_Completer_ServerRunningWithHost_test():
-  response = deepcopy( GENERIC_RESPONSE )
-  assert_that(
-    FormatDebugInfoResponse( response ),
-    contains_string(
-      'Completer name completer debug information:\n'
-      '  Server name running at: http://127.0.0.1:1234\n'
-      '  Server name process ID: 12345\n'
-      '  Server name executable: /path/to/executable\n'
-      '  Server name logfiles:\n'
-      '    /path/to/stdout/logfile\n'
-      '    /path/to/stderr/logfile\n'
-      '  Server name key: value\n'
-      '  Key: value\n'
-    )
-  )
+    api('trophy', TrophyJsonTemplate)
+api('rules', RulesJsonTemplate)
+    
+        allowed_render_styles = ('html', 'xml', 'js', 'embed', '', 'compact', 'api')
+    # List of admins to blame (skip the first admin, 'reddit')
+    # If list is empty, just blame 'an admin'
+    admins = g.admins[1:] or ['an admin']
+    def __before__(self):
+        try:
+            c.error_page = True
+            RedditController.__before__(self)
+        except (HTTPMovedPermanently, HTTPFound):
+            # ignore an attempt to redirect from an error page
+            pass
+        except Exception as e:
+            handle_awful_failure('ErrorController.__before__: %r' % e)
