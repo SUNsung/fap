579e1450d5cc8886a7339f0a833f1a372eb13de0
SUNsung <weatherius@gmail.com>
2018-02-20 01:12:38 +0200

    Rewrite [430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb] File in: 2018-02-20 01:12:37

--- ruby/430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb
+++ ruby/430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb
@@ -1,93 +1,156 @@
-        def add_constraints(scope, owner, chain)
-          scope = last_chain_scope(scope, chain.last, owner)
+
+        
+        config = File.expand_path '../lib/jekyll/mime.types', __dir__
     
-            def helper_method(*methods)
-          # Almost a duplicate from ActionController::Helpers
-          methods.flatten.each do |method|
-            _helpers.module_eval <<-end_eval, __FILE__, __LINE__ + 1
-              def #{method}(*args, &block)                    # def current_user(*args, &block)
-                _test_case.send(%(#{method}), *args, &block)  #   _test_case.send(%(current_user), *args, &block)
-              end                                             # end
-            end_eval
-          end
+        def base_directory
+      @base_directory.to_s
+    end
+    
+          # Group an array of items by an expression
+      #
+      # input - the object array
+      # variable - the variable to assign each item to in the expression
+      # expression -a Liquid comparison expression passed in as a string
+      #
+      # Returns the filtered array of objects
+      def group_by_exp(input, variable, expression)
+        return input unless groupable?(input)
+    
+          topic.feature_topic_users(args)
+    end
+    
+      def grant_admin!
+    set_permission('admin', true)
+  end
+    
+          when :login_fail
+    
+            when :err
+          case s[:last]
+            when :pass
+              # Oops got a -ERR after a pass so its crap ignore the pass
+              # But report it, might be helpfull for guessing :-)
+    
+    		# Reset the block size
+		self.block_size = 0
+    
+        set :run, Proc.new { File.expand_path($0) == File.expand_path(app_file) }
+    
+          def remove_bad_cookies(request, response)
+        return if bad_cookies.empty?
+        paths = cookie_paths(request.path)
+        bad_cookies.each do |name|
+          paths.each { |path| response.set_cookie name, empty_cookie(request.host, path) }
+        end
+      end
+    
+        it 'copes with nested arrays' do
+      mock_app do |env|
+        request = Rack::Request.new(env)
+        [200, {'Content-Type' => 'text/plain'}, [request.params['foo']['bar']]]
+      end
+      get '/', :foo => {:bar => '<bar>'}
+      expect(body).to eq('&lt;bar&gt;')
+    end
+    
+      %w(GET HEAD).each do |method|
+    it 'accepts #{method} requests with non-whitelisted Origin' do
+      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://malicious.com')).to be_ok
+    end
+  end
+    
+      context 'escaping' do
+    before do
+      mock_app { |e| [200, {'Content-Type' => 'text/plain'}, [e['PATH_INFO']]] }
+    end
+    
+        def define_class_getter
+      @klass.extend(ClassMethods)
+    end
+    
+            def failure_message
+          'Should have an attachment named #{@attachment_name}'
         end
     
-                  if very_raw_connection && very_raw_connection.respond_to?(:flush)
-                very_raw_connection.flush
-              end
-            end
+            def multiple_assignment_node
+          grandparent_node = node.parent ? node.parent.parent : nil
+          return nil unless grandparent_node
+          return nil unless grandparent_node.type == MULTIPLE_ASSIGNMENT_TYPE
+          return nil unless node.parent.type == MULTIPLE_LEFT_HAND_SIDE_TYPE
+          grandparent_node
         end
+      end
     end
   end
 end
 
     
-    class TestResponseTest < ActiveSupport::TestCase
-  def assert_response_code_range(range, predicate)
-    response = ActionDispatch::TestResponse.new
-    (0..599).each do |status|
-      response.status = status
-      assert_equal range.include?(status), response.send(predicate),
-                   'ActionDispatch::TestResponse.new(#{status}).#{predicate}'
+              @assignments.reverse_each do |assignment|
+            next if consumed_branches.include?(assignment.branch)
+    
+            def autocorrect(node)
+          redundant_regex?(node) do |receiver, regex_str|
+            receiver, regex_str = regex_str, receiver if receiver.is_a?(String)
+            regex_str = regex_str[2..-1] # drop \A anchor
+            regex_str = interpret_string_escapes(regex_str)
+    
+    module RuboCop
+  module Cop
+    module Style
+      # This cop checks for optional arguments to methods
+      # that do not come at the end of the argument list
+      #
+      # @example
+      #   # bad
+      #   def foo(a = 1, b, c)
+      #   end
+      #
+      #   # good
+      #   def baz(a, b, c = 1)
+      #   end
+      #
+      #   def foobar(a = 1, b = 2, c = 3)
+      #   end
+      class OptionalArguments < Cop
+        MSG = 'Optional arguments should appear at the end ' \
+              'of the argument list.'.freeze
+    
+      def human?
+    params.has_key?('human') && (params['human'].nil? || as_boolean(params['human']) == true)
+  end
+end
+
+    
+              def plugins
+            @plugins ||= find_plugins_gem_specs.map do |spec|
+              { :name => spec.name, :version => spec.version.to_s }
+            end.sort_by do |spec|
+              spec[:name]
+            end
+          end
+    
+          def extract_metrics(path, *keys)
+        snapshot.metric_store.extract_metrics(path, *keys)
+      end
+    end
+  end
+end
+
+    
+          expect('.all-buttons-active').to have_ruleset(ruleset)
     end
   end
     
-          def on_error(message) # :nodoc:
-        # log errors to make diagnosing socket errors easier
-        logger.error 'WebSocket error occurred: #{message}'
-      end
+      context 'called with one size' do
+    it 'applies same width to all sides' do
+      rule = 'margin: 1px'
     
-        class << self
-      # Returns singleton instance for this class in this thread. If none exists, one is created.
-      def instance
-        current_instances[name] ||= new
-      end
-    
-          def first
-        @text = 'Hello world'
-      end
-    
-      def test_default_helpers_only
-    assert_equal [JustMeHelper], JustMeController._helpers.ancestors.reject(&:anonymous?)
-    assert_equal [MeTooHelper, JustMeHelper], MeTooController._helpers.ancestors.reject(&:anonymous?)
-  end
-    
-        test 'head :switching_protocols (101) does not return any content' do
-      content = body(HeadController.action(:switching_protocols).call(Rack::MockRequest.env_for('/')))
-      assert_empty content
-    end
-    
-    Gem::Specification.new do |s|
-  s.platform    = Gem::Platform::RUBY
-  s.name        = 'actionmailer'
-  s.version     = version
-  s.summary     = 'Email composition, delivery, and receiving framework (part of Rails).'
-  s.description = 'Email on Rails. Compose, deliver, receive, and test emails using the familiar controller/view pattern. First-class support for multipart email and attachments.'
-    
-          text.split.each do |word|
-        if sentences.first.present? && (sentences.last + [word]).join(' ').length > len
-          sentences << [word]
-        else
-          sentences.last << word
-        end
-      end
-    
-        def build_for_type(type, user, attributes = {})
-      attributes.delete(:type)
-    
-          if respond_to?(:stop)
-        stop
-      else
-        terminate_thread!
-      end
-    end
-    
-      def tumblr_consumer_key
-    ENV['TUMBLR_OAUTH_KEY']
-  end
-    
-        respond_to do |format|
-      format.html { render layout: !request.xhr? }
-      format.json { render json: @jobs }
+          expect('.prefix--moz-ms').to have_ruleset(rule)
     end
   end
+end
+
+    
+      context 'called with two sizes' do
+    it 'applies to height and width' do
+      rule = 'height: 2em; width: 1em;'
