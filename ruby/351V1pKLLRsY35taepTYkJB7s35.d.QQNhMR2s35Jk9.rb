1b1028c6172f1f5beb8315f0b1198eb71df77a89
SUNsung <weatherius@gmail.com>
2018-02-20 08:13:26 +0200

    Rewrite [351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb] File in: 2018-02-20 08:13:25

--- ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
+++ ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
@@ -1,7 +1,33 @@
-    def metadata_master_container_path
-      @metadata_master_container_path ||= caskroom_path.join(METADATA_SUBDIR)
-    end
+
+        
+        class ActivityPub::EmojiSerializer < ActiveModel::Serializer
+  include RoutingHelper
     
-      # Clean the keg of formula @f
-  def clean
-    ObserverPathnameExtension.reset_counts!
+      let(:user) { Fabricate(:user) }
+    
+            def regexp_named_capture?
+          @node.type == REGEXP_NAMED_CAPTURE_TYPE
+        end
+    
+    module RuboCop
+  module Cop
+    class VariableForce
+      # A Variable represents existence of a local variable.
+      # This holds a variable declaration node,
+      # and some states of the variable.
+      class Variable
+        VARIABLE_DECLARATION_TYPES =
+          (VARIABLE_ASSIGNMENT_TYPES + ARGUMENT_DECLARATION_TYPES).freeze
+    
+            def autocorrect(node)
+          lambda do |corrector|
+            each_unnecessary_space_match(node) do |range|
+              corrector.replace(range, ' ')
+            end
+          end
+        end
+    
+            def_node_matcher :redundant_regex?, <<-PATTERN
+          {(send $!nil? {:match :=~} (regexp (str $#literal_at_end?) (regopt)))
+           (send (regexp (str $#literal_at_end?) (regopt)) {:match :=~} $_)}
+        PATTERN
