3ffb9446936fbfb7dcaaf40e719d6c849ccb3bb0
SUNsung <weatherius@gmail.com>
2018-02-20 04:49:48 +0200

    Rewrite [14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py] File in: 2018-02-20 04:49:48

--- pyton/14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py
+++ pyton/14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py
@@ -1,6 +1,144 @@
-    # Sorting the libraries
-    inner_blocks = sorted(blocks[0].split('##'))
-    for i in range(1 , len(inner_blocks)):
-        if inner_blocks[i][0] != '#':
-            inner_blocks[i] = '##' + inner_blocks[i]
-    inner_blocks=''.join(inner_blocks)
+
+        
+        
+def test_urls(client):
+    r = client.get('/')
+    assert r.status_code == 200
+    
+        app.config.update(dict(
+        DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+        DEBUG=True,
+        SECRET_KEY=b'_5#y2L'F4Q8z\n\xec]/',
+        USERNAME='admin',
+        PASSWORD='default'
+    ))
+    app.config.update(config or {})
+    app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+    
+        A simple application that shows how Flask and jQuery get along.
+    
+    import word2vec
+    
+    def four_lines_data():
+  text = StringIO(FOUR_LINES)
+    
+    py_test(
+    name = 'translate_test',
+    size = 'medium',
+    srcs = [
+        'translate.py',
+    ],
+    args = [
+        '--self_test=True',
+    ],
+    main = 'translate.py',
+    srcs_version = 'PY2AND3',
+    deps = [
+        ':data_utils',
+        ':seq2seq_model',
+        '//tensorflow:tensorflow_py',
+    ],
+)
+    
+        # Linear learning rate decay.
+    words_to_train = float(opts.words_per_epoch * opts.epochs_to_train)
+    lr = opts.learning_rate * tf.maximum(
+        0.0001,
+        1.0 - tf.cast(total_words_processed, tf.float32) / words_to_train)
+    
+    
+def win32_version_string():
+    import ctypes
+    class OSVERSIONINFOEXW(ctypes.Structure):
+        _fields_ = [('dwOSVersionInfoSize', ctypes.c_ulong),
+                    ('dwMajorVersion', ctypes.c_ulong),
+                    ('dwMinorVersion', ctypes.c_ulong),
+                    ('dwBuildNumber', ctypes.c_ulong),
+                    ('dwPlatformId', ctypes.c_ulong),
+                    ('szCSDVersion', ctypes.c_wchar*128),
+                    ('wServicePackMajor', ctypes.c_ushort),
+                    ('wServicePackMinor', ctypes.c_ushort),
+                    ('wSuiteMask', ctypes.c_ushort),
+                    ('wProductType', ctypes.c_byte),
+                    ('wReserved', ctypes.c_byte)]
+    '''
+    Get's the OS major and minor versions.  Returns a tuple of
+    (OS_MAJOR, OS_MINOR).
+    '''
+    os_version = OSVERSIONINFOEXW()
+    os_version.dwOSVersionInfoSize = ctypes.sizeof(os_version)
+    retcode = ctypes.windll.Ntdll.RtlGetVersion(ctypes.byref(os_version))
+    if retcode != 0:
+        raise Exception('Failed to get OS version')
+    
+    
+from xlog import getLogger
+xlog = getLogger('gae_proxy')
+from config import config
+    
+    # begin[licence]
+#
+# [The 'BSD licence']
+# Copyright (c) 2005-2008 Terence Parr
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# end[licence]
+    
+    
+def linear_model_main(_distances, _press_times, target_distance):
+    regr = LinearRegression()
+    regr.fit(_distances, _press_times)
+    predict_press_time = regr.predict(target_distance)
+    result = {}
+    # 截距 b
+    result['intercept'] = regr.intercept_
+    # 斜率值 k
+    result['coefficient'] = regr.coef_
+    # 预估的按压时间
+    result['value'] = predict_press_time
+    return result
+    
+    
+def open_accordant_config():
+    '''
+    调用配置文件
+    '''
+    screen_size = _get_screen_size()
+    config_file = '{path}/config/{screen_size}/config.json'.format(
+        path=sys.path[0],
+        screen_size=screen_size
+    )
+    
+        print(round(target_distance), round(jump_error_value), round(actual_distance), round(last_press_time))
+    ''''# 将结果采集进学习字典
+    if last_piece_x > 0 and last_press_time > 0:
+        ai.add_data(round(actual_distance, 2), round(last_press_time))
+        # print(round(actual_distance), round(last_press_time))'''
+    
+            for j in range(int(board_x_start), int(board_x_end)):
+            pixel = im_pixel[j, i]
+            # 修掉脑袋比下一个小格子还高的情况的 bug
+            if abs(j - piece_x) < piece_body_width:
+                continue
