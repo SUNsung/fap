b009ad73bd7f87e8271bdff3c1b60bc35ddb284c
SUNsung <weatherius@gmail.com>
2018-02-20 21:08:15 +0200

    Rewrite [12DT0BT..tu5Q12L4hECP9xYeU12GO4u19dJvM612IIr.py] File in: 2018-02-20 21:08:14

--- pyton/12DT0BT..tu5Q12L4hECP9xYeU12GO4u19dJvM612IIr.py
+++ pyton/12DT0BT..tu5Q12L4hECP9xYeU12GO4u19dJvM612IIr.py
@@ -1,125 +1,206 @@
 
         
-        
-def check_entry(line_num, segments):
-    # START Title
-    title = segments[index_title].upper()
-    if title.endswith(' API'):
-        add_error(line_num, 'Title should not contain 'API'')
-    # END Title
-    # START Description
-    # first character should be capitalized
-    char = segments[index_desc][0]
-    if char.upper() != char:
-        add_error(line_num, 'first character of description is not capitalized')
-    # last character should not punctuation
-    char = segments[index_desc][-1]
-    if char in punctuation:
-        add_error(line_num, 'description should not end with {}'.format(char))
-    # END Description
-    # START Auth
-    # values should conform to valid options only
-    auth = segments[index_auth].replace('`', '')
-    if auth not in auth_keys:
-        add_error(line_num, '{} is not a valid Auth option'.format(auth))
-    # END Auth
-    # START HTTPS
-    # values should conform to valid options only
-    https = segments[index_https]
-    if https not in https_keys:
-        add_error(line_num, '{} is not a valid HTTPS option'.format(https))
-    # END HTTPS
-    # START CORS
-    # values should conform to valid options only
-    cors = segments[index_cors]
-    if cors not in cors_keys:
-        add_error(line_num, '{} is not a valid CORS option'.format(cors))
-    # END CORS
-    # START Link
-    # url should be wrapped in '[Go!]()' Markdown syntax
-    link = segments[index_link]
-    if not link.startswith('[Go!](http') or not link.endswith(')'):
-        add_error(line_num, 'link syntax should be '[Go!](LINK)'')
-    # END Link
-    
-          with self.assertRaises(tf.errors.OutOfRangeError):
-        sess.run([result.key, result.uint8image])
+            cmd = cmd.split()
     
     
-def main(argv):
-    args = parser.parse_args(argv[1:])
+def get_args(args_list):
+    parser = argparse.ArgumentParser(
+        description='ansible inventory script reading from landscape cluster')
+    mutex_group = parser.add_mutually_exclusive_group(required=True)
+    help_list = 'list all hosts from landscape cluster'
+    mutex_group.add_argument('--list', action='store_true', help=help_list)
+    help_host = 'display variables for a host'
+    mutex_group.add_argument('--host', help=help_host)
+    return parser.parse_args(args_list)
     
-        # Convert the inputs to a Dataset.
-    dataset = tf.data.Dataset.from_tensor_slices(inputs)
-    
-    filegroup(
-    name = 'all_files',
-    srcs = glob(
-        ['**/*'],
-        exclude = [
-            '**/METADATA',
-            '**/OWNERS',
-        ],
-    ),
-    visibility = ['//tensorflow:__subpackages__'],
-)
-
-    
-        # Declare all variables we need.
-    # Input words embedding: [vocab_size, emb_dim]
-    w_in = tf.Variable(
-        tf.random_uniform(
-            [opts.vocab_size,
-             opts.emb_dim], -0.5 / opts.emb_dim, 0.5 / opts.emb_dim),
-        name='w_in')
-    
-    import numpy
-from six.moves import urllib
-from six.moves import xrange  # pylint: disable=redefined-builtin
-import tensorflow as tf
+        args = parser.parse_args()
     
     
-def data_to_token_ids(data_path, target_path, vocabulary_path,
-                      tokenizer=None, normalize_digits=True):
-  '''Tokenize data file and turn into token-ids using given vocabulary file.
+class TerminalModule(TerminalBase):
     
-    # Create a custom estimator using my_model_fn to define the model
-tf.logging.info('Before classifier construction')
-classifier = tf.estimator.Estimator(
-    model_fn=my_model_fn,
-    model_dir=PATH)  # Path to where checkpoints etc are stored
-tf.logging.info('...done constructing classifier')
+            start = time.time()
+        func(X, n_jobs=1)
+        one_core.append(time.time() - start)
     
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer)
-        ]
+        print('Benchmarks')
+    print('===========================')
+    print('Generate dataset benchmarks... ', end='')
+    X_dense, X_sparse = make_sparse_random_data(opts.n_samples,
+                                                opts.n_features,
+                                                n_nonzeros,
+                                                random_state=opts.random_seed)
+    X = X_dense if opts.dense else X_sparse
+    print('done')
     
-            Accessing a node updates its position to the front of the LRU list.
-        '''
-        node = self.lookup[query]
-        if node is None:
-            return None
-        self.linked_list.move_to_front(node)
-        return node.results
+    # to store the results
+scikit_classifier_results = []
+scikit_regressor_results = []
     
-    from babel.messages.pofile import read_po
+        try:
+        fn = inspect.getsourcefile(obj)
+    except Exception:
+        fn = None
+    if not fn:
+        try:
+            fn = inspect.getsourcefile(sys.modules[obj.__module__])
+        except Exception:
+            fn = None
+    if not fn:
+        return
+    
+    import codecs
+    
+    try:
+    from urllib import urlopen
+except ImportError:
+    from urllib.request import urlopen
+    
+    #import matplotlib.pyplot as plt
+#plt.matshow(cm, cmap=plt.cm.jet)
+#plt.show()
+    
+        out = K.eval(weighted_loss(K.variable(x),
+                               K.variable(y),
+                               K.variable(weights),
+                               K.variable(mask)))
+    
+    model = Sequential()
+model.add(Embedding(max_features, embedding_size, input_length=maxlen))
+model.add(Dropout(0.25))
+model.add(Conv1D(filters,
+                 kernel_size,
+                 padding='valid',
+                 activation='relu',
+                 strides=1))
+model.add(MaxPooling1D(pool_size=pool_size))
+model.add(LSTM(lstm_output_size))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+    
+    x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+    
+    learning_rate = 1e-6
+clip_norm = 1.0
     
     
-register_api_templates('comment', CommentJsonTemplate)
-register_api_templates('morerecursion', MoreCommentJsonTemplate)
-register_api_templates('morechildren', MoreCommentJsonTemplate)
-
+@keras_test
+def test_convert_weights():
+    def get_model(shape, data_format):
+        model = Sequential()
+        model.add(Conv2D(filters=2,
+                         kernel_size=(4, 3),
+                         input_shape=shape,
+                         data_format=data_format))
+        model.add(Flatten())
+        model.add(Dense(5))
+        return model
     
-                send_gift(
-                buyer=buyer,
-                recipient=recipient,
-                months=months,
-                days=months * 31,
-                signed=False,
-                giftmessage=None,
-                thing_fullname=thing_fullname,
-                note=note,
-            )
+        # Arguments
+        x: Numpy array of input data.
+        y: Numpy array of targets data.
+        image_data_generator: Instance of `ImageDataGenerator`
+            to use for random transformations and normalization.
+        batch_size: Integer, size of a batch.
+        shuffle: Boolean, whether to shuffle the data between epochs.
+        seed: Random seed for data shuffling.
+        data_format: String, one of `channels_first`, `channels_last`.
+        save_to_dir: Optional directory where to save the pictures
+            being yielded, in a viewable format. This is useful
+            for visualizing the random transformations being
+            applied, for debugging purposes.
+        save_prefix: String prefix to use for saving sample
+            images (if `save_to_dir` is set).
+        save_format: Format to use for saving sample images
+            (if `save_to_dir` is set).
+    '''
+    
+    
+def test_reuters():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = reuters.load_data()
+        assert len(x_train) == len(y_train)
+        assert len(x_test) == len(y_test)
+        assert len(x_train) + len(x_test) == 11228
+        (x_train, y_train), (x_test, y_test) = reuters.load_data(maxlen=10)
+        assert len(x_train) == len(y_train)
+        assert len(x_test) == len(y_test)
+        word_index = reuters.get_word_index()
+        assert isinstance(word_index, dict)
+    
+    
+@keras_test
+def test_maxout_dense():
+    layer_test(legacy_layers.MaxoutDense,
+               kwargs={'output_dim': 3},
+               input_shape=(3, 2))
+    
+        # 4. Anything else is not a valid argument
+    with pytest.raises(ValueError):
+        a = activations.get(6)
+    
+                # check that output changes after states are reset
+            # (even though the model itself didn't change)
+            layer.reset_states()
+            out3 = model.predict(np.ones_like(inputs))
+            assert(out2.max() != out3.max())
+    
+                if point.name == 'p':
+                link = point.find('a')
+                if link is not None:
+                    link = clean_pdf_link(link.attrs['href'])
+                    ext = get_extension(link)
+                    print(ext)
+                    if not ext in forbidden_extensions:
+                        print(shorten_title(point.text) + ' (' + link + ')')
+                        try:
+                            name = clean_text(point.text.split('[' + ext + ']')[0])
+                            fullname = '.'.join((name, ext))
+                            if not os.path.exists('/'.join((current_directory, fullname)) ):
+                                download_pdf(link, current_directory, '.'.join((name, ext)))
+                        except KeyboardInterrupt:
+                            try:
+                                print('Press Ctrl-C in 1 second to quit')
+                                time.sleep(1)
+                            except KeyboardInterrupt:
+                                print('Cancelling..')
+                                break
+                        except:
+                            failures.append(point.text)
+                        
+        point = point.next_sibling          
+    
+    
+def update_data():
+    global src_x, src_y
+    
+            # 在跳跃落下的瞬间 摄像机移动前截图 这个参数要自己校调
+        time.sleep(0.2)
+        pull_screenshot_temp()
+        im_temp = Image.open('./autojump_temp.png')
+        temp_piece_x, temp_piece_y = find_piece(im_temp)
+        debug.computing_error(press_time, board_x, board_y, piece_x, piece_y, temp_piece_x, temp_piece_y)
+    
+        # 粗查棋子位置
+    piece_found, piece_fx, piece_fy = 0, 0, 0
+    scan_piece_unit = w // 40  # 间隔单位
+    ny = (h + w) // 2  # 寻找下限 从画面中央的正方形的下缘开始
+    while ny > (h - w) // 2 and not piece_found:
+        ny -= scan_piece_unit
+        for nx in range(0, w, scan_piece_unit):
+            pixel = im_pixel[nx, ny]
+            if find_piece(pixel):
+                piece_fx, piece_fy = nx, ny
+                piece_found = True
+                break
+    print('%-12s %s,%s' % ('piece_fuzzy:', piece_fx, piece_fy))
+    if not piece_fx:
+        return 0, 0  # 没找到棋子
