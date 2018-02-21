585c9bca65bb8686e15d32941d68d0ccaf0d87ea
SUNsung <weatherius@gmail.com>
2018-02-22 01:59:32 +0200

    Rewrite [36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py] File in: 2018-02-22 01:59:31

--- pyton/36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py
+++ pyton/36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py
@@ -1,137 +1,166 @@
 
         
-                Comment:                   'italic #8f5902', # class: 'c'
-        Comment.Preproc:           'noitalic',       # class: 'cp'
+            data_train = fetch_20newsgroups_vectorized(subset='train')
+    data_test = fetch_20newsgroups_vectorized(subset='test')
+    X_train = check_array(data_train.data, dtype=np.float32,
+                          accept_sparse='csc')
+    X_test = check_array(data_test.data, dtype=np.float32, accept_sparse='csr')
+    y_train = data_train.target
+    y_test = data_test.target
     
-                return self.server_sock.accept()[0]
-        except (select.error, socket.error):
-            return None
+        dimensions = 500 * np.arange(1, n_iter + 1)
     
-                if self.status_code == 0 or self.raw is None:
-                self._content = None
-            else:
-                self._content = bytes().join(self.iter_content(CONTENT_CHUNK_SIZE)) or bytes()
+            # Create flat baselines to compare the variation over batch size
+        all_times['pca'].extend([results_dict['pca']['time']] *
+                                len(batch_sizes))
+        all_errors['pca'].extend([results_dict['pca']['error']] *
+                                 len(batch_sizes))
+        all_times['rpca'].extend([results_dict['rpca']['time']] *
+                                 len(batch_sizes))
+        all_errors['rpca'].extend([results_dict['rpca']['error']] *
+                                  len(batch_sizes))
+        for batch_size in batch_sizes:
+            ipca = IncrementalPCA(n_components=n_components,
+                                  batch_size=batch_size)
+            results_dict = {k: benchmark(est, data) for k, est in [('ipca',
+                                                                   ipca)]}
+            all_times['ipca'].append(results_dict['ipca']['time'])
+            all_errors['ipca'].append(results_dict['ipca']['error'])
     
-    import pytest
+    n_samples = np.logspace(.5, 3, 9)
+n_features = np.logspace(1, 3.5, 7)
+N_samples, N_features = np.meshgrid(n_samples,
+                                    n_features)
+scikits_time = np.zeros(N_samples.shape)
+scipy_time = np.zeros(N_samples.shape)
     
-                return _r
+        opener = build_opener()
+    html_filename = os.path.join(html_folder, lang + '.html')
+    if not os.path.exists(html_filename):
+        print('Downloading %s' % page)
+        request = Request(page)
+        # change the User Agent to avoid being blocked by Wikipedia
+        # downloading a couple of articles should not be considered abusive
+        request.add_header('User-Agent', 'OpenAnything/1.0')
+        html_content = opener.open(request).read()
+        open(html_filename, 'wb').write(html_content)
+    
+    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
+DATA_FOLDER = 'txt_sentoken'
+    
+        print('Decompressing %s' % ARCHIVE_NAME)
+    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
+        archive.extractall(path='.')
+    os.remove(ARCHIVE_NAME)
+
+    
+    Sentiment analysis can be casted as a binary text classification problem,
+that is fitting a linear classifier on features extracted from the text
+of the user messages so as to guess wether the opinion of the author is
+positive or negative.
     
     '''
-requests.cookies
-~~~~~~~~~~~~~~~~
+# Author: Olivier Grisel <olivier.grisel@ensta.org>
+# License: Simplified BSD
+    
+    This example demonstrates how to generate a checkerboard dataset and
+bicluster it using the Spectral Biclustering algorithm.
+    
+    Plot the classification probability for different classifiers. We use a 3
+class dataset, and we classify it with a Support Vector classifier, L1
+and L2 penalized logistic regression with either a One-Vs-Rest or multinomial
+setting, and Gaussian process classification.
+    
+            X, y = generate_data(n_test, n_features)
+        score_clf1 += clf1.score(X, y)
+        score_clf2 += clf2.score(X, y)
     
     
-def _check_cryptography(cryptography_version):
-    # cryptography < 1.3.4
-    try:
-        cryptography_version = list(map(int, cryptography_version.split('.')))
-    except ValueError:
-        return
+class UserService(object):
     
-    # Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'Requests.tex', u'Requests Documentation',
-     u'Kenneth Reitz', 'manual'),
-]
+        def can_fit_in_spot(self, spot):
+        return True if (spot.size == LARGE or spot.size == COMPACT) else False
     
-        @pytest.mark.parametrize(
-        'url', (
-            'http://192.168.1.1:5000/',
-            'http://192.168.1.1/',
-            'http://www.requests.com/',
-        ))
-    def test_bypass_no_proxy_keyword(self, url):
-        no_proxy = '192.168.1.1,requests.com'
-        assert get_environ_proxies(url, no_proxy=no_proxy) == {}
+        def categorize(self, transaction):
+        if transaction.seller in self.seller_category_map:
+            return self.seller_category_map[transaction.seller]
+        if transaction.seller in self.seller_category_overrides_map:
+            seller_category_map[transaction.seller] = \
+                self.manual_overrides[transaction.seller].peek_min()
+            return self.seller_category_map[transaction.seller]
+        return None
     
-    Py3 = sys.version_info[0] == 3
+    from alembic import op
+import sqlalchemy as sa
     
-    '''Tests for models.tutorials.rnn.ptb.reader.'''
-    
-    FOUR_LINES = '\n'.join([
-    '1,52.40, 2823,152,2',
-    '164, 99.80,176.60,66.20,1',
-    '176,2824, 136,3.19,0',
-    '2,177.30,66.30, 53.10,1',])
-    
-    py_test(
-    name = 'translate_test',
-    size = 'medium',
-    srcs = [
-        'translate.py',
-    ],
-    args = [
-        '--self_test=True',
-    ],
-    main = 'translate.py',
-    srcs_version = 'PY2AND3',
-    deps = [
-        ':data_utils',
-        ':seq2seq_model',
-        '//tensorflow:tensorflow_py',
-    ],
-)
-    
-      Args:
-    source_path: path to the files with token-ids for the source language.
-    target_path: path to the file with token-ids for the target language;
-      it must be aligned with the source file: n-th line contains the desired
-      output for n-th line from the source_path.
-    max_size: maximum number of lines to read, all other will be ignored;
-      if 0 or None, data files will be read completely (no limit).
+            with op.batch_alter_table('slices') as batch_op:
+            batch_op.drop_constraint(slices_ibfk_1, type_='foreignkey')
+            batch_op.drop_constraint(slices_ibfk_2, type_='foreignkey')
+            batch_op.drop_column('druid_datasource_id')
+            batch_op.drop_column('table_id')
+    except Exception as e:
+        logging.warning(str(e))
     
     
-def get_wmt_enfr_train_set(directory):
-  '''Download the WMT en-fr training corpus to directory unless it's there.'''
-  train_path = os.path.join(directory, 'giga-fren.release2.fixed')
-  if not (gfile.Exists(train_path +'.fr') and gfile.Exists(train_path +'.en')):
-    corpus_file = maybe_download(directory, 'training-giga-fren.tar',
-                                 _WMT_ENFR_TRAIN_URL)
-    print('Extracting tar file %s' % corpus_file)
-    with tarfile.open(corpus_file, 'r') as corpus_tar:
-      corpus_tar.extractall(directory)
-    gunzip_file(train_path + '.fr.gz', train_path + '.fr')
-    gunzip_file(train_path + '.en.gz', train_path + '.en')
-  return train_path
-    
-          Args:
-      batch_size: Total batch size used to calculate examples/second from
-      global time.
-      every_n_steps: Log stats every n steps.
-      every_n_secs: Log stats every n seconds.
-    '''
-    if (every_n_steps is None) == (every_n_secs is None):
-      raise ValueError('exactly one of every_n_steps'
-                       ' and every_n_secs should be provided.')
-    self._timer = basic_session_run_hooks.SecondOrStepTimer(
-        every_steps=every_n_steps, every_secs=every_n_secs)
+if __name__ == '__main__':
+    cli()
+
     
     
-@keras_test
-def test_sequential_sample_weights():
-    model = create_sequential_model()
-    model.compile(loss=loss, optimizer='rmsprop')
+def select_files(files):
+    for f in files:
+        if re.match(PYFILE, f):
+            yield f
+        else:
+            try:
+                with open(f) as f_:
+                    first = f_.readline()
+                    if first.startswith('#!') and 'python' in first:
+                        yield f
+            except (IOError, OSError):
+                logging.exception('Unable to check-code against %s', f)
     
+        def has_gold(self, user):
+        if not user:
+            return False
     
-@keras_test
-def test_gaussiandropout_legacy_interface():
-    old_layer = keras.layers.GaussianDropout(p=0.6, name='drop')
-    new_layer_1 = keras.layers.GaussianDropout(rate=0.6, name='drop')
-    new_layer_2 = keras.layers.GaussianDropout(0.6, name='drop')
-    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer_1.get_config())
-    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer_2.get_config())
+        @require_oauth2_scope('creddits')
+    @validate(
+        VUser(),
+        user=VAccountByName('username'),
+        months=VInt('months', min=1, max=36),
+        timeout=VNotInTimeout(),
+    )
+    @api_doc(
+        api_section.gold,
+        uri='/api/v1/gold/give/{username}',
+    )
+    def POST_give(self, user, months, timeout):
+        self._gift_using_creddits(
+            recipient=user,
+            months=months,
+            proxying_for=request.POST.get('proxying_for'),
+        )
+
     
+        @require_oauth2_scope('identity')
+    @validate(
+        VUser(),
+        fields=VList(
+            'fields',
+            choices=PREFS_JSON_SPEC.spec.keys(),
+            error=errors.errors.NON_PREFERENCE,
+        ),
+    )
+    @api_doc(api_section.account, uri='/api/v1/me/prefs')
+    def GET_prefs(self, fields):
+        '''Return the preference settings of the logged in user'''
+        resp = PrefsJsonTemplate(fields).data(c.oauth_user)
+        return self.api_wrapper(resp)
     
-def handle_function(name, member):
-    if is_accepted(name, member):
-        return
-    doc = member.__doc__
-    if doc is None and not member_too_small(member):
-        raise ValueError('{} function doesn't have any documentation'.format(name),
-                         member.__module__, inspect.getmodule(member).__file__)
-    args = list(inspect.signature(member).parameters.keys())
-    assert_args_presence(args, doc, member, name)
-    assert_function_style(name, member, doc, args)
-    assert_doc_style(name, member, doc)
+    class CaptchaController(RedditController):
+    @allow_oauth2_access
+    @api_doc(api_section.captcha, uri='/captcha/{iden}')
+    def GET_captchaimg(self, iden):
+        '''
+        Request a CAPTCHA image given an `iden`.
