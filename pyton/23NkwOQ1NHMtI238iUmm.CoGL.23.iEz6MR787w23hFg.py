5b0ba988414da72efdd459c6dfe3911d394bcc52
SUNsung <weatherius@gmail.com>
2018-02-21 20:30:33 +0200

    Rewrite [23NkwOQ1NHMtI238iUmm.CoGL.23.iEz6MR787w23hFg.py] File in: 2018-02-21 20:30:31

--- pyton/23NkwOQ1NHMtI238iUmm.CoGL.23.iEz6MR787w23hFg.py
+++ pyton/23NkwOQ1NHMtI238iUmm.CoGL.23.iEz6MR787w23hFg.py
@@ -1,35 +1,68 @@
 
         
-          @tf.test.mock.patch.dict(premade_estimator.__dict__,
-                           {'load_data': four_lines_data})
-  def test_premade_estimator(self):
-    premade_estimator.main([None, '--train_steps=1'])
+        
+def add_error(line_num, message):
+    '''adds an error to the dynamic error list'''
+    err = '(L{:03d}) {}'.format(line_num + 1, message)
+    errors.append(err)
     
-        for pred_dict, expec in zip(predictions, expected):
-        template = ('\nPrediction is '{}' ({:.1f}%), expected '{}'')
+            X_test = X[-n_test_samples:]
+        Y_test = Y[-n_test_samples:]
+        X = X[:(i * step)]
+        Y = Y[:(i * step)]
     
-    py_test(
-    name = 'cifar10_input_test',
-    size = 'small',
-    srcs = ['cifar10_input_test.py'],
-    srcs_version = 'PY2AND3',
-    deps = [
-        ':cifar10_input',
-        '//tensorflow:tensorflow_py',
-        '//tensorflow/python:framework_test_lib',
-        '//tensorflow/python:platform_test',
-    ],
-)
     
-        # class_ids will be the model prediction for the class (Iris flower type)
-    # The output node with the highest value is our prediction
-    predictions = { 'class_ids': tf.argmax(input=logits, axis=1) }
+if __name__ == '__main__':
+    from mpl_toolkits.mplot3d import axes3d  # register the 3d projection
+    import matplotlib.pyplot as plt
     
-        global_step = run_values.results
-    if self._timer.should_trigger_for_step(global_step):
-      elapsed_time, elapsed_steps = self._timer.update_last_triggered_step(
-          global_step)
-      if elapsed_time is not None:
-        steps_per_sec = elapsed_steps / elapsed_time
-        self._step_train_time += elapsed_time
-        self._total_steps += elapsed_steps
+        #------------------------------------------------------------
+    # varying k
+    k_results_build = dict([(alg, np.zeros(len(krange)))
+                            for alg in algorithms])
+    k_results_query = dict([(alg, np.zeros(len(krange)))
+                            for alg in algorithms])
+    
+        op.add_option('--eps',
+                  dest='eps', default=0.5, type=float,
+                  help='See the documentation of the underlying transformers.')
+    
+       ###########################################################################
+    # Set custom automatic method selection
+    sampling_algorithm['custom-auto'] = \
+        lambda n_population, n_samples, random_state=None: \
+            sample_without_replacement(n_population,
+                                       n_samples,
+                                       method='auto',
+                                       random_state=random_state)
+    
+    
+if not os.path.exists(DATA_FOLDER):
+    
+        # Print and plot the confusion matrix
+    cm = metrics.confusion_matrix(y_test, y_predicted)
+    print(cm)
+    
+    plt.matshow(data, cmap=plt.cm.Blues)
+plt.title('Original dataset')
+    
+    ax = plt.axes([0.15, 0.04, 0.7, 0.05])
+plt.title('Probability')
+plt.colorbar(imshow_handle, cax=ax, orientation='horizontal')
+    
+    
+class ShutdownRequest( BaseRequest ):
+  def __init__( self ):
+    super( BaseRequest, self ).__init__()
+    
+    
+  def _ThreadMain( self ):
+    while True:
+      time.sleep( self._ping_interval_seconds )
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
