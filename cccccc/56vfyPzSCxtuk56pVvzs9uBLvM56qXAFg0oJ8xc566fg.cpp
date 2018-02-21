329dc0faa49ccf3a8b70a0fecc899af75b0e131e
SUNsung <weatherius@gmail.com>
2018-02-21 14:33:05 +0200

    Rewrite [56vfyPzSCxtuk56pVvzs9uBLvM56qXAFg0oJ8xc566fg.cpp] File in: 2018-02-21 14:33:04

--- cccccc/56vfyPzSCxtuk56pVvzs9uBLvM56qXAFg0oJ8xc566fg.cpp
+++ cccccc/56vfyPzSCxtuk56pVvzs9uBLvM56qXAFg0oJ8xc566fg.cpp
@@ -1,108 +1,269 @@
 
         
-        bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_constructor(JSContext *cx, uint32_t argc, jsval *vp);
-void js_cocos2dx_physics3d_Physics3DConeTwistConstraint_finalize(JSContext *cx, JSObject *obj);
-void js_register_cocos2dx_physics3d_Physics3DConeTwistConstraint(JSContext *cx, JS::HandleObject global);
-void register_all_cocos2dx_physics3d(JSContext* cx, JS::HandleObject obj);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getBFrame(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_setFixThresh(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getFrameOffsetB(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getFrameOffsetA(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getFixThresh(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getSwingSpan2(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getSwingSpan1(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_setMaxMotorImpulse(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_setFrames(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getTwistAngle(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_GetPointForAngle(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_setMaxMotorImpulseNormalized(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getTwistSpan(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_setDamping(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_setLimit(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getAFrame(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_enableMotor(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_create(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_Physics3DConeTwistConstraint(JSContext *cx, uint32_t argc, jsval *vp);
-    
-    bool js_cocos2dx_studio_AnimationData_constructor(JSContext *cx, uint32_t argc, jsval *vp);
-void js_cocos2dx_studio_AnimationData_finalize(JSContext *cx, JSObject *obj);
-void js_register_cocos2dx_studio_AnimationData(JSContext *cx, JS::HandleObject global);
-void register_all_cocos2dx_studio(JSContext* cx, JS::HandleObject obj);
-bool js_cocos2dx_studio_AnimationData_getMovement(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_AnimationData_getMovementCount(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_AnimationData_addMovement(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_AnimationData_create(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_AnimationData_AnimationData(JSContext *cx, uint32_t argc, jsval *vp);
-    
-    
-    
-    
-    {
-    {			float minX = -6.0f;
-			float maxX = 0.0f;
-			float minY = 4.0f;
-			float maxY = 6.0f;
-			
-			for (int32 i = 0; i < 400; ++i)
-			{
-				b2BodyDef bd;
-				bd.type = b2_dynamicBody;
-				bd.position = b2Vec2(RandomFloat(minX,maxX),RandomFloat(minY,maxY));
-				b2Body* body = m_world->CreateBody(&bd);
-				body->CreateFixture(&shape, 0.01f);
-			}
-		}
-		
-		{
-			b2PolygonShape shape;
-			shape.SetAsBox(1.5f, 1.5f);
-			b2BodyDef bd;
-			bd.type = b2_dynamicBody;
-			bd.position.Set(-40.0f,5.0f);
-			bd.bullet = true;
-			b2Body* body = m_world->CreateBody(&bd);
-			body->CreateFixture(&shape, 1.0f);
-			body->SetLinearVelocity(b2Vec2(150.0f, 0.0f));
-		}
-	}
-    
-    
-    {	bool m_broke;
-	bool m_break;
+        
+    {  DISALLOW_COPY_AND_ASSIGN(DesktopCapturer);
 };
     
-    		for (int32 i = 0; i < 2; ++i)
-		{
-			b2Vec2 vertices[3];
-			vertices[0].Set(-0.5f, 0.0f);
-			vertices[1].Set(0.5f, 0.0f);
-			vertices[2].Set(0.0f, 1.5f);
+    
+    {}  // namespace api
+    
+    #include 'atom/common/node_includes.h'
+    
+    void Event::SetSenderAndMessage(content::WebContents* sender,
+                                IPC::Message* message) {
+  DCHECK(!sender_);
+  DCHECK(!message_);
+  sender_ = sender;
+  message_ = message;
     }
     
     
-    {			body->ApplyAngularImpulse(100.0f, true);
-		}
+    {}  // namespace api
     
-    template<typename DType>
-inline void CompressArray<DType>::Decompress(int chunk_id) {
-  int chunk_size = static_cast<int>(
-      raw_chunks_[chunk_id + 1] - raw_chunks_[chunk_id]) * sizeof(DType);
-  int encoded_size = static_cast<int>(
-      encoded_chunks_[chunk_id + 1] - encoded_chunks_[chunk_id]);
-  // decompress data
-  int src_size = LZ4_decompress_fast(
-      dmlc::BeginPtr(in_buffer_) + encoded_chunks_[chunk_id],
-      reinterpret_cast<char*>(dmlc::BeginPtr(data) + raw_chunks_[chunk_id]),
-      chunk_size);
-  CHECK_EQ(encoded_size, src_size);
+    namespace atom {
+    }
+    
+      // Don't execute signal handlers of SIGUSR2.
+  sigset_t mask;
+  sigemptyset(&mask);
+  sigaddset(&mask, SIGUSR2);
+  if (sigprocmask(SIG_BLOCK, &mask, NULL) < 0) {
+    PLOG(ERROR) << 'sigprocmask';
+    return;
+  }
+    
+    
+    {  if (event.filter != EVFILT_PROC ||
+      event.fflags != NOTE_EXIT ||
+      event.ident != static_cast<uintptr_t>(parent_pid)) {
+    LOG(ERROR) << 'kevent (monitor): unexpected event, filter ' << event.filter
+               << ', fflags ' << event.fflags << ', ident ' << event.ident;
+    return;
+  }
+}
+    
+      int AddEntry(const base::DictionaryValue& entry);
+  void RemoveEntry(int id);
+    
+    static const char* kModuleName = '_api_implementation';
+static const char kModuleDocstring[] =
+'_api_implementation is a module that exposes compile-time constants that\n'
+'determine the default API implementation to use for Python proto2.\n'
+'\n'
+'It complements api_implementation.py by setting defaults using compile-time\n'
+'constants defined in C, such that one can set defaults at compilation\n'
+'(e.g. with blaze flag --copt=-DPYTHON_PROTO2_CPP_IMPL_V2).';
+    
+    // Mappings and Sequences of descriptors.
+// They implement containers like fields_by_name, EnumDescriptor.values...
+// See descriptor_containers.cc for more description.
+#include <Python.h>
+    
+    // Appends the scalar 'item' to the end of the container 'self'.
+//
+// Returns None if successful; returns NULL and sets an exception if
+// unsuccessful.
+PyObject* Append(RepeatedScalarContainer* self, PyObject* item);
+    
+    #endif  // GOOGLE_PROTOBUF_COMPILER_CSHARP_ENUM_H__
+    
+    
+    {
+    {
+    {
+    {
+    {}  // namespace
+}  // namespace csharp
+}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
+
+    
+    
+    { private:
+  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(MapFieldGenerator);
+};
+    
+      std::string namespace_;
+  std::string reflectionClassname_;
+    
+    #endif  // GOOGLE_PROTOBUF_COMPILER_CSHARP_SOURCE_GENERATOR_BASE_H__
+    
+    namespace google {
+namespace protobuf {
+namespace compiler {
+namespace java {
+namespace {
+    }
+    }
+    }
+    }
+    }
+    
+    #ifndef incl_HPHP_WORKLOAD_STATS_H_
+#define incl_HPHP_WORKLOAD_STATS_H_
+    
+    std::string show(const RepoAuthType::Array& ar) {
+  auto ret = std::string{};
+    }
+    
+    namespace HPHP {
+///////////////////////////////////////////////////////////////////////////////
+    }
+    
+    const StaticString s_pagelet('pagelet');
+    
+    void Vunit::freeScratchBlock(Vlabel l) {
+  // This will leak blocks if anything's been added since the corresponding
+  // call to makeScratchBlock(), but it's harmless.
+  if (l == blocks.size() - 1) blocks.pop_back();
+}
+    
+      for (mapping = enc_name_map; mapping->names != nullptr; mapping++) {
+    for (p = mapping->names; *p != '\0'; p += (strlen(p) + 1)) {
+      if (strcasecmp(p, pname) == 0) {
+        return mapping->code;
+      }
+    }
+  }
+    
+    #ifndef incl_HPHP_SWEEPABLE_H_
+#define incl_HPHP_SWEEPABLE_H_
+    
+    inline void PackedArray::scan(const ArrayData* a, type_scan::Scanner& scanner) {
+  assert(checkInvariants(a));
+  auto data = packedData(a);
+  scanner.scan(*data, a->getSize() * sizeof(*data));
+}
+    
+    TransID Scale::findProfTransID(Vlabel blk) const {
+  for (auto& i : m_unit.blocks[blk].code) {
+    if (!i.origin) continue;
+    auto profTransID = i.origin->marker().profTransID();
+    if (profTransID == kInvalidTransID) continue;
+    return profTransID;
+  }
+  return kInvalidTransID;
+}
+    
+      // set interleave for early code. we'll then force interleave for a few
+  // regions, and switch to local for the threads
+  numa_set_interleave_mask(numa_all_nodes_ptr);
+    
+    namespace HPHP { namespace jit {
+    }
+    }
+    
+    
+    // -------------------------------------------------------------------
+    // DecimateMinibatch - decimate minibatch for parallelization
+    // -------------------------------------------------------------------
+    // non-inplace decimation , to be used in subminibatch implementation
+    // returns a subset of parallel sequences
+    template <class ElemType>
+    static pair<size_t, size_t> DecimateMinibatch(const StreamMinibatchInputs& MB,    // input matrices
+                                                  StreamMinibatchInputs& decimatedMB, // output decimated matrices.
+                                                  MBLayoutPtr pMBLayout,              // input MBLayout
+                                                  MBLayoutPtr& pDecimateMBLayout,     // output decimated MBLayout (note: cannot work in-place)
+                                                  size_t numProcs, size_t rank)
+    {
+        size_t numParallelSequences = pMBLayout->GetNumParallelSequences();
+        size_t nT = pMBLayout->GetNumTimeSteps();
+    }
+    
+            // M             * V
+        // (8192 x 9304) * (9304 x 1024) -> (8192 x 1024)   // 78047.609 MFlops, 81.773 total MB
+        // 7.86 ms / frame
+        // We need to store: 4 cols of V and 1 row of M, that is 9304 x 4 x 5 = 186 KB. Too much for the cache!
+        // (8192 x 1024) * (1024 x 9304) -> (8192 x 9304)   // 78047.609 MFlops, 17.086 total MB
+        // 1.78 ms / frame
+    
+    // magic numbers for headers
+const WORD magicFile = 0xACE9;       // file header, only expected at beginning of file
+const WORD magicSection = 0x4ACE;    // section headers, all other header types
+const WORD magicIncomplete = 0xBAD1; // use a section header for the file that isn't valid until we close it properly
+    
+    BOOST_FIXTURE_TEST_CASE(MatrixSparsePlusSparse, RandomSeedFixture)
+{
+    std::mt19937 rng(0);
+    Matrix<float> mAdense(c_deviceIdZero);
+    mAdense.AssignTruncateBottomOf(Matrix<float>::RandomUniform(dim1, dim2, c_deviceIdZero, -3.0f, 0.1f, IncrementCounter()), 0);
+    Matrix<float> mAsparse(mAdense.DeepClone());
+    }
+    
+            // set the Computational network in the script, so we can do name lookup in the model
+        script->SetComputationNetwork(m_net);
+    
+    
+    {
+    {        return newNumItersSinceLastPrintOfProgress;
+    }
+};
+    
+    vector<wstring> /*IConfigRecord::*/ ComputationNodeBase::GetMemberIds() const
+{
+    return vector<wstring>{ L'name', L'operation', L'dim', L'dims', /*L'tag', */L'inputs', OperationName() + L'Args' };
 }
     
     
+    
+    
+    
+    
+    
+    
+    
+    void GLESDebugDraw::DrawString(int x, int y, const char *string, ...)
+{
+//    NSLog(@'DrawString: unsupported: %s', string);
+    //printf(string);
+    /* Unsupported as yet. Could replace with bitmap font renderer at a later date */
+}
+    
+    class AddPair : public Test
+{
+public:
+    }
+    
+    
     {
+    {		case 'k':
+			m_platform->SetType(b2_kinematicBody);
+			m_platform->SetLinearVelocity(b2Vec2(-m_speed, 0.0f));
+			m_platform->SetAngularVelocity(0.0f);
+			break;
+		}
+	}
+    
+    #endif
+
+    
+    
     {
-    { private:
-  std::ifstream fi;
-};
-}  // namespace common
-}  // namespace xgboost
-#endif  // XGBOOST_COMMON_CONFIG_H_
+    {				prevBody = body;
+			}
+		}
+    
+    /**
+ * Symbolicates a stack trace into a new vector
+ *
+ * @param stackTrace The input stack trace
+ */
+FBEXPORT inline std::vector<StackTraceElement> getStackTraceSymbols(
+    const std::vector<InstructionPointer>& trace) {
+  auto symbols = std::vector<StackTraceElement>{};
+  getStackTraceSymbols(symbols, trace);
+  return symbols;
+}
+    
+        static Config * create(void);
+    
+        method(getComputedWidth);
+    method(getComputedHeight);
+    
+    #include <cstdarg>
+#include <stdio.h>
+    
+    #pragma once
+#include <fb/assert.h>
+#include <utility>
