9c95b6463b4f445e5f977df58acebaf52f16601a
SUNsung <weatherius@gmail.com>
2018-02-21 16:28:36 +0200

    Rewrite [27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py] File in: 2018-02-21 16:28:35

--- pyton/27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py
+++ pyton/27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py
@@ -1,282 +1,91 @@
 
         
-            Uses the last ([-1]) value of other fields
-    '''
-    column_width = max(max(len(k) for k in formats) + 1, 8)
-    first_width = max(len(k) for k in metrics)
-    head_fmt = ('{:<{fw}s}' + '{:>{cw}s}' * len(formats))
-    row_fmt = ('{:<{fw}s}' + '{:>{cw}.3f}' * len(formats))
-    print(head_fmt.format('Metric', *formats,
-                          cw=column_width, fw=first_width))
-    for metric, row in zip(metrics, results[:, :, -1, -1, -1]):
-        print(row_fmt.format(metric, *row,
-                             cw=column_width, fw=first_width))
+          def replicate_states(self, state_coll_name):
+    state_list = self._metagraph.collection_def[state_coll_name]
+    num_states = len(state_list.node_list.value)
+    for replica_id in range(1, FLAGS.num_gpus):
+      for i in range(num_states):
+        state_list.node_list.value.append(state_list.node_list.value[i])
+    for replica_id in range(FLAGS.num_gpus):
+      for i in range(num_states):
+        index = replica_id * num_states + i
+        state_list.node_list.value[index] = with_autoparallel_prefix(
+            replica_id, state_list.node_list.value[index])
     
-        max_it = len(samples_range) * len(features_range)
-    for n_samples in samples_range:
-        for n_features in features_range:
-            it += 1
-            print('====================')
-            print('Iteration %03d of %03d' % (it, max_it))
-            print('====================')
-            dataset_kwargs = {
-                'n_samples': n_samples,
-                'n_features': n_features,
-                'n_informative': n_features / 10,
-                'effective_rank': min(n_samples, n_features) / 10,
-                #'effective_rank': None,
-                'bias': 0.0,
-            }
-            print('n_samples: %d' % n_samples)
-            print('n_features: %d' % n_features)
-            X, y = make_regression(**dataset_kwargs)
-    
-    for i, n in enumerate(n_samples):
-    for j, p in enumerate(n_features):
-        X = np.random.normal(size=(n, p))
-        t0 = time.time()
-        ward.fit(X)
-        scikits_time[j, i] = time.time() - t0
-        t0 = time.time()
-        hierarchy.ward(X)
-        scipy_time[j, i] = time.time() - t0
-    
-    from sklearn import clone
-from sklearn.externals.six.moves import xrange
-from sklearn.random_projection import (SparseRandomProjection,
-                                       GaussianRandomProjection,
-                                       johnson_lindenstrauss_min_dim)
-    
-    import os
-import tarfile
-from contextlib import closing
-try:
-    from urllib import urlopen
-except ImportError:
-    from urllib.request import urlopen
-    
-    plt.matshow(data, cmap=plt.cm.Blues)
-plt.title('Original dataset')
-    
-        fig = plt.figure(fig_index, figsize=(10, 10))
-    ax1 = plt.subplot2grid((3, 1), (0, 0), rowspan=2)
-    ax2 = plt.subplot2grid((3, 1), (2, 0))
-    
-    Only adjusted measures can hence safely be used as a consensus index
-to evaluate the average stability of clustering algorithms for a given
-value of k on various overlapping sub-samples of the dataset.
+    py_binary(
+    name = 'train_classifier',
+    srcs = ['train_classifier.py'],
+    deps = [
+        ':graphs',
+        ':train_utils',
+        # google3 file dep,
+        # tensorflow internal dep,
+    ],
+)
     
     
-def upgrade():
-    try:
-        op.alter_column(
-            'clusters', 'changed_on',
-            existing_type=sa.DATETIME(),
-            nullable=True)
-        op.alter_column(
-            'clusters', 'created_on',
-            existing_type=sa.DATETIME(), nullable=True)
-        op.drop_constraint(None, 'columns', type_='foreignkey')
-        op.drop_constraint(None, 'columns', type_='foreignkey')
-        op.drop_column('columns', 'created_on')
-        op.drop_column('columns', 'created_by_fk')
-        op.drop_column('columns', 'changed_on')
-        op.drop_column('columns', 'changed_by_fk')
-        op.alter_column('css_templates', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        op.alter_column('css_templates', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        op.alter_column('dashboards', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        op.alter_column('dashboards', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        op.create_unique_constraint(None, 'dashboards', ['slug'])
-        op.alter_column('datasources', 'changed_by_fk',
-                   existing_type=sa.INTEGER(),
-                   nullable=True)
-        op.alter_column('datasources', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        op.alter_column('datasources', 'created_by_fk',
-                   existing_type=sa.INTEGER(),
-                   nullable=True)
-        op.alter_column('datasources', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        op.alter_column('dbs', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        op.alter_column('dbs', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        op.alter_column('slices', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        op.alter_column('slices', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        op.alter_column('sql_metrics', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        op.alter_column('sql_metrics', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        op.alter_column('table_columns', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        op.alter_column('table_columns', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        op.alter_column('tables', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        op.alter_column('tables', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        op.alter_column('url', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        op.alter_column('url', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=True)
-        ### end Alembic commands ###
-    except:
-        pass
+def virtual_adversarial_loss_bidir(logits, embedded, inputs,
+                                   logits_from_embedding_fn):
+  '''Virtual adversarial loss for bidirectional models.'''
+  logits = tf.stop_gradient(logits)
+  f_inputs, _ = inputs
+  weights = f_inputs.eos_weights
+  if FLAGS.single_label:
+    indices = tf.stack([tf.range(FLAGS.batch_size), f_inputs.length - 1], 1)
+    weights = tf.expand_dims(tf.gather_nd(f_inputs.eos_weights, indices), 1)
+  assert weights is not None
     
-    def upgrade():
-    op.add_column('dashboards', sa.Column('slug', sa.String(length=255), nullable=True))
-    try:
-        op.create_unique_constraint('idx_unique_slug', 'dashboards', ['slug'])
-    except:
-        pass
+    Computational time:
+  1.8 hours to train 10000 steps without adversarial or virtual adversarial
+    training, on 1 layer 1024 hidden units LSTM, 256 embeddings, 400 truncated
+    BP, 64 minibatch and on single GPU (Pascal Titan X, cuDNNv5).
     
-        try:
-        fk_columns = generic_find_constraint_name(
-            table='columns', columns={'datasource_name'},
-            referenced='datasources', db=db)
-        with op.batch_alter_table('columns') as batch_op:
-            batch_op.drop_constraint(fk_columns, type_='foreignkey')
-    except Exception as e:
-        logging.warning(str(e))
-    
-    from alembic import op
-import sqlalchemy as sa
-    
-    Revision ID: 430039611635
-Revises: d827694c7555
-Create Date: 2016-02-10 08:47:28.950891
-    
-    from alembic import op
-import sqlalchemy as sa
-    
-    '''
-    
-        def test_from_bytes(self):
-        def check(tests, byteorder, signed=False):
-            for test, expected in tests.items():
-                try:
-                    self.assertEqual(
-                        int.from_bytes(test, byteorder, signed=signed),
-                        expected)
-                except Exception as err:
-                    raise AssertionError(
-                        'failed to convert {0} with byteorder={1!r} and signed={2}'
-                        .format(test, byteorder, signed)) from err
-    
-    
-print('\n# ======================================================================')
-print('#                               Factorial')
-print('# ======================================================================\n')
-    
-    class TZInfo:
-    def __init__(self, transitions, type_indices, ttis, abbrs):
-        self.transitions = transitions
-        self.type_indices = type_indices
-        self.ttis = ttis
-        self.abbrs = abbrs
-    
-        def seek(self, pos, whence=0):
-        '''Seek to specified position into the chunk.
-        Default position is 0 (start of chunk).
-        If the file is not seekable, this will result in an error.
-        '''
-    
-    
-    {        if nextchar == '}':
-            break
-        elif nextchar != ',':
-            raise JSONDecodeError('Expecting ',' delimiter', s, end - 1)
-        end = _w(s, end).end()
-        nextchar = s[end:end + 1]
-        end += 1
-        if nextchar != ''':
-            raise JSONDecodeError(
-                'Expecting property name enclosed in double quotes', s, end - 1)
-    if object_pairs_hook is not None:
-        result = object_pairs_hook(pairs)
-        return result, end
-    pairs = dict(pairs)
-    if object_hook is not None:
-        pairs = object_hook(pairs)
-    return pairs, end
-    
-    The get_colordb() function will try to examine the file to figure out what the
-format of the file is.  If it can't figure out the file format, or it has
-trouble reading the file, None is returned.  You can pass get_colordb() an
-optional filetype argument.
-    
-        if allow_dotted_names:
-        attrs = attr.split('.')
-    else:
-        attrs = [attr]
-    
-        There will be a subclass for every refined PyObject type that we care
-    about.
-    
-                if srname:
-                c.site = Subreddit._by_name(srname)
-    
-    import json
-import os
-    
-        def tearDown(self):
-        self.out.close()
-        sys.stdout = self.saved_stdout
-    
-        def test_display_current_time_at_current_time(self):
-        '''
-        Just as justification for working example. (Will always pass.)
-        '''
-        production_code_time_provider = ProductionCodeTimeProvider()
-        class_under_test = TimeDisplay(production_code_time_provider)
-        current_time = datetime.datetime.now()
-        expected_time = '<span class=\'tinyBoldText\'>{}:{}</span>'.format(current_time.hour, current_time.minute)
-        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
-    
-        def test_display_current_time_at_midnight(self):
-        class_under_test = TimeDisplay()
-        expected_time = '24:01'
-        result = class_under_test.get_current_time_as_as_html_fragment()
-        self.assertEqual(result, expected_time)
+    If you are not interested in eager execution, you should ignore this file.
 '''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
     
-        if update:
-        time.sleep(1)
-        pull_screenshot()
-        im.set_array(update_data())
-        update = False
-    return im,
+    import tensorflow as tf
     
-    
-def main():
-    while True:
-        pull_screenshot()
-        im = Image.open('./1.png')
+          with self.assertRaises(tf.errors.OutOfRangeError):
+        sess.run([result.key, result.uint8image])
     
     
-c = wda.Client()
-s = c.session()
+def maybe_download_and_extract():
+  '''Download and extract model tar file.'''
+  dest_directory = FLAGS.model_dir
+  if not os.path.exists(dest_directory):
+    os.makedirs(dest_directory)
+  filename = DATA_URL.split('/')[-1]
+  filepath = os.path.join(dest_directory, filename)
+  if not os.path.exists(filepath):
+    def _progress(count, block_size, total_size):
+      sys.stdout.write('\r>> Downloading %s %.1f%%' % (
+          filename, float(count * block_size) / float(total_size) * 100.0))
+      sys.stdout.flush()
+    filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
+    print()
+    statinfo = os.stat(filepath)
+    print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
+  tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+    
+            with ExceptionStackContext(error_handler):
+            request = HTTPRequest(self.get_url('/custom_reason'),
+                                  prepare_curl_callback=lambda curl: 1 / 0)
+        yield [error_event.wait(), self.http_client.fetch(request)]
+        self.assertEqual(1, len(exc_info))
+        self.assertIs(exc_info[0][0], ZeroDivisionError)
+    
+        @gen_test
+    def test_notify_all_with_timeout(self):
+        c = locks.Condition()
+        self.record_done(c.wait(), 0)
+        self.record_done(c.wait(timedelta(seconds=0.01)), 1)
+        self.record_done(c.wait(), 2)
+    
+        def get_current_user(self):
+        user_id = self.get_secure_cookie('blogdemo_user')
+        if not user_id:
+            return None
+        return self.db.get('SELECT * FROM authors WHERE id = %s', int(user_id))
