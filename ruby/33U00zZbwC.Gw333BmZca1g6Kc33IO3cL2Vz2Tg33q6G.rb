3fa3b7dd33e91cc6e0ffe80699fe0f7b07fbfaf2
SUNsung <weatherius@gmail.com>
2018-02-21 23:19:58 +0200

    Rewrite [33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb] File in: 2018-02-21 23:19:58

--- ruby/33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb
+++ ruby/33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb
@@ -1,66 +1,42 @@
 
         
-            t = Time.utc(1996, 12, 20, 0, 39, 57)
-    s = '1996-12-19T16:39:57-08:00'
-    assert_equal(t, Time.__send__(method, s))
-    # There is no way to generate time string with arbitrary timezone.
-    s = '1996-12-20T00:39:57Z'
-    assert_equal(t, Time.__send__(method, s))
-    assert_equal(s, t.iso8601)
-    
-            def to_feed(feed, current)
-          if @content and current.respond_to?(:itunes_duration=)
-            current.itunes_duration ||= current.class::ITunesDuration.new
-            current.itunes_duration.content = @content
-          end
-        end
-    
-          # Get the publicly-visible URL for an imported file. This URL is used by
-      # source maps to link to the source stylesheet. This may return `nil` to
-      # indicate that no public URL is available; however, this will cause
-      # sourcemap generation to fail if any CSS is generated from files imported
-      # from this importer.
-      #
-      # If an absolute 'file:' URI can be produced for an imported file, that
-      # should be preferred to returning `nil`. However, a URL relative to
-      # `sourcemap_directory` should be preferred over an absolute 'file:' URI.
-      #
-      # @param uri [String] A URI known to be valid for this importer.
-      # @param sourcemap_directory [String, NilClass] The absolute path to a
-      #   directory on disk where the sourcemap will be saved. If uri refers to
-      #   a file on disk that's accessible relative to sourcemap_directory, this
-      #   may return a relative URL. This may be `nil` if the sourcemap's
-      #   eventual location is unknown.
-      # @return [String?] The publicly-visible URL for this file, or `nil`
-      #   indicating that no publicly-visible URL exists. This should be
-      #   appropriately URL-escaped.
-      def public_url(uri, sourcemap_directory)
-        return if @public_url_warning_issued
-        @public_url_warning_issued = true
-        Sass::Util.sass_warn <<WARNING
-WARNING: #{self.class.name} should define the #public_url method.
-WARNING
-        nil
-      end
-    
-      # Creates a delayed logger wrapping `inner`.
-  #
-  # @param inner [Sass::Logger::Base] The wrapped logger.
-  def initialize(inner)
-    self.log_level = inner.log_level
-    @inner = inner
-    @messages = []
+          def grant_admin!
+    set_permission('admin', true)
   end
     
-            {
-          :always_update     => false,
-          :template_location => root + '/public/stylesheets/sass',
-          :css_location      => root + '/public/stylesheets',
-          :cache_location    => root + '/tmp/sass-cache',
-          :always_check      => env != 'production',
-          :quiet             => env != 'production',
-          :full_exception    => env != 'production'
-        }.freeze
+    def load_apps
+  out, err, status = Open3.capture3('/usr/bin/osascript', '-e', 'tell application 'System Events' to get (name, bundle identifier, unix id) of every process')
+  if status.exitstatus > 0
+    puts err
+    exit status.exitstatus
+  end
+  out = out.split(', ')
+  one_third   = out.length / 3
+  @app_names  = out.shift(one_third)
+  @bundle_ids = out.shift(one_third)
+  @unix_ids   = out.shift(one_third)
+end
+    
+      #forward some requests to status message, because a poll is just attached to a status message and is not sharable itself
+  delegate :author_id, :diaspora_handle, :public?, :subscribers, to: :status_message
+    
+          if File.directory?(source_entry)
+        FileUtils.mkdir(target_entry) unless File.exists?(target_entry)
+        transform_r(source_entry, target_entry)
+      else
+        # copy the new file, in case of being an .erb file should render first
+        if source_entry.end_with?('erb')
+          target_entry = target_entry.gsub(/.erb$/,'').gsub('example', name)
+          File.open(target_entry, 'w') { |f| f.write(render(source_entry)) }
+        else
+          FileUtils.cp(source_entry, target_entry)
+        end
+        puts '\t create #{File.join(full_plugin_name, Pathname.new(target_entry).relative_path_from(Pathname.new(@target_path)))}'
       end
     end
   end
+    
+    module LogStash
+  module Api
+    class Service
+      include LogStash::Util::Loggable
