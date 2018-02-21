fdc95fac5893750f7592f08beedcdfd4afc7cca1
SUNsung <weatherius@gmail.com>
2018-02-21 22:04:10 +0200

    Rewrite [04yHUL5UdTEwA04YAK7VC2XmuY04Iy3dgE1vcc04xn..py] File in: 2018-02-21 22:04:10

--- pyton/04yHUL5UdTEwA04YAK7VC2XmuY04Iy3dgE1vcc04xn..py
+++ pyton/04yHUL5UdTEwA04YAK7VC2XmuY04Iy3dgE1vcc04xn..py
@@ -1,24 +1,67 @@
 
-def get_meta(data):
-    meta = {'hostvars': {}}
-    for node in data:
-        meta['hostvars'][node['Name']] = node['Tags']
-    return meta
+        
+            import matplotlib.pyplot as plt
     
+        ratio = n_samples / opts.n_population
     
-def get_api_key():
-    '''
-    rtype: str
-    '''
-    key = os.environ.get('SHIPPABLE_KEY', None)
+        # TASK: print the mean and std for each candidate along with the parameter
+    # settings for all the candidates explored by grid search.
+    n_candidates = len(grid_search.cv_results_['params'])
+    for i in range(n_candidates):
+        print(i, 'params - %s; mean - %0.2f; std - %0.2f'
+                 % (grid_search.cv_results_['params'][i],
+                    grid_search.cv_results_['mean_test_score'][i],
+                    grid_search.cv_results_['std_test_score'][i]))
     
-        'bracketed_hostport': re.compile(
-        r'''^
-            \[(.+)\]                    # [host identifier]
-            :([0-9]+)                   # :port number
-            $
-        ''', re.X
-    ),
+    First example
+-------------
+The first example illustrates how robust covariance estimation can help
+concentrating on a relevant cluster when another one exists. Here, many
+observations are confounded into one and break down the empirical covariance
+estimation.
+Of course, some screening tools would have pointed out the presence of two
+clusters (Support Vector Machines, Gaussian Mixture Models, univariate
+outlier detection, ...). But had it been a high-dimensional example, none
+of these could be applied that easily.
     
-    from ansible.compat.tests import mock, unittest
-from ansible.module_utils.gcp import check_min_pkg_version, GCPUtils, GCPInvalidURLError
+            controller.coef0 = Tk.StringVar()
+        controller.coef0.set('0')
+        r = Tk.Frame(valbox)
+        Tk.Label(r, text='coef0:', anchor='e', width=7).pack(side=Tk.LEFT)
+        Tk.Entry(r, width=6, textvariable=controller.coef0).pack(side=Tk.LEFT)
+        r.pack()
+        valbox.pack(side=Tk.LEFT)
+    
+            fraction_of_positives, mean_predicted_value = \
+            calibration_curve(y_test, prob_pos, n_bins=10)
+    
+        def on_operator_inservice(self):
+        self._hsm.on_switchover()  # message ignored
+        self._hsm.send_operator_inservice_response()
+        self._hsm.next_state('suspect')
+    
+        def test_cat_adapter_shall_make_noise(self):
+        cat = Cat()
+        cat_adapter = Adapter(cat, make_noise=cat.meow)
+        noise = cat_adapter.make_noise()
+        expected_noise = 'meow!'
+        self.assertEqual(noise, expected_noise)
+    
+    Test code which will almost always fail (if not exactly 12:01) when untestable
+production code (have a look into constructor_injection.py) is used:
+    
+    class TimeDisplay(object):
+    
+        def main_method(self):
+        '''will execute either _instance_method_1 or _instance_method_2
+    
+        def tearDown(self):
+        if not self._bProblem:
+            print('Tearing down')
+            time.sleep(0.1)
+            self._tm.publishReport()
+        else:
+            print('Test not executed. No tear down required.')
+    
+        def or_specification(self, candidate):
+        raise NotImplementedError()
