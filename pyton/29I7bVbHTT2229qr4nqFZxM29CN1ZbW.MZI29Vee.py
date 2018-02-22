6e78befff8bc82bd62591540d091d07446218b74
SUNsung <weatherius@gmail.com>
2018-02-22 10:38:46 +0200

    Rewrite [29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py] File in: 2018-02-22 10:38:45

--- pyton/29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py
+++ pyton/29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py
@@ -1,105 +1,250 @@
 
         
-            def iter_lines(self, chunk_size):
-        yield self.body, b''
+        
+def gen_extractors():
+    ''' Return a list of an instance of every supported extractor.
+    The order does matter; the first extractor matched is the one handling the URL.
+    '''
+    return [klass() for klass in gen_extractor_classes()]
     
-            '''
-        available_plugins = plugin_manager.get_formatters_grouped()
-        self.enabled_plugins = []
-        for group in groups:
-            for cls in available_plugins[group]:
-                p = cls(env=env, **kwargs)
-                if p.enabled:
-                    self.enabled_plugins.append(p)
+        _TEST = {
+        'url': 'http://www.anitube.se/video/36621',
+        'md5': '59d0eeae28ea0bc8c05e7af429998d43',
+        'info_dict': {
+            'id': '36621',
+            'ext': 'mp4',
+            'title': 'Recorder to Randoseru 01',
+            'duration': 180.19,
+        },
+        'skip': 'Blocked in the US',
+    }
+    
+    from .common import InfoExtractor
+from ..utils import js_to_json
     
     
-def test_unicode_raw_json_item(httpbin):
-    r = http('--json', 'POST', httpbin.url + '/post',
-             u'test:={ '%s' : [ '%s' ] }' % (UNICODE, UNICODE))
-    assert HTTP_OK in r
-    assert r.json['json'] == {'test': {UNICODE: [UNICODE]}}
+class EnvironmentConfig(object):
+    uri = os.getenv('LANDSCAPE_API_URI')
+    access_key = os.getenv('LANDSCAPE_API_KEY')
+    secret_key = os.getenv('LANDSCAPE_API_SECRET')
+    ssl_ca_file = os.getenv('LANDSCAPE_API_SSL_CA_FILE')
     
+    try:
+    import json
+except ImportError:
+    import simplejson as json
+import os
+import os.path
+import sys
+import ConfigParser
+import StringIO
+    
+    # Note, sha1 is the only hash algorithm compatible with python2.4 and with
+# FIPS-140 mode (as of 11-2014)
+try:
+    from hashlib import sha1 as sha1
+except ImportError:
+    from sha import sha as sha1
+    
+        try:
+        with open(path, 'r') as key_fd:
+            return key_fd.read().strip()
+    except IOError:
+        return None
+    
+        @g_connect
+    def get_import_task(self, task_id=None, github_user=None, github_repo=None):
         '''
-)
-positional.add_argument(
-    'items',
-    metavar='REQUEST_ITEM',
-    nargs=ZERO_OR_MORE,
-    default=None,
-    type=KeyValueArgType(*SEP_GROUP_ALL_ITEMS),
-    help=r'''
-    Optional key-value pairs to be included in the request. The separator used
-    determines the type:
-    
-        ERROR_TIMEOUT = 2
-    ERROR_TOO_MANY_REDIRECTS = 6
-    
-        @classmethod
-    def __get_test_directory(self):
+        Check the status of an import task.
         '''
-        Get the temporary directory for the tests.
-        '''
-        self.test_dir = os.path.join(os.path.dirname(
-            os.path.realpath(__file__)), 'test_command')
+        url = '%s/imports/' % self.baseurl
+        if task_id is not None:
+            url = '%s?id=%d' % (url, task_id)
+        elif github_user is not None and github_repo is not None:
+            url = '%s?github_user=%s&github_repo=%s' % (url, github_user, github_repo)
+        else:
+            raise AnsibleError('Expected task_id or github_user and github_repo')
     
-        def test_b_observers_shall_be_attachable(cls):
-        cls.s.attach(cls.dec_obs)
-        cls.assertEqual(isinstance(cls.s._observers[0], DecimalViewer), True)
-        cls.assertEqual(len(cls.s._observers), 1)
-        cls.s.attach(cls.hex_obs)
-        cls.assertEqual(isinstance(cls.s._observers[1], HexViewer), True)
-        cls.assertEqual(len(cls.s._observers), 2)
+            params1 = {
+            'creationTimestamp': '2017-04-21T11:19:20.718-07:00',
+            'defaultService': 'https://www.googleapis.com/compute/v1/projects/myproject/global/backendServices/default-backend-service',
+            'description': '',
+            'fingerprint': 'ickr_pwlZPU=',
+            'hostRules': [
+                {
+                    'description': '',
+                    'hosts': [
+                        '*.'
+                    ],
+                    'pathMatcher': 'path-matcher-one'
+                }
+            ],
+            'id': '8566395781175047111',
+            'kind': 'compute#urlMap',
+            'name': 'newtesturlmap-foo',
+            'pathMatchers': [
+                {
+                    'defaultService': 'https://www.googleapis.com/compute/v1/projects/myproject/global/backendServices/bes-pathmatcher-one-default',
+                    'description': 'path matcher one',
+                    'name': 'path-matcher-one',
+                    'pathRules': [
+                        {
+                            'paths': [
+                                '/data',
+                                '/aboutus'
+                            ],
+                            'service': 'https://www.googleapis.com/compute/v1/projects/myproject/global/backendServices/my-one-bes'
+                        }
+                    ]
+                }
+            ],
+            'selfLink': 'https://www.googleapis.com/compute/v1/projects/myproject/global/urlMaps/newtesturlmap-foo'
+        }
+        params2 = {
+            'defaultService': 'https://www.googleapis.com/compute/v1/projects/myproject/global/backendServices/default-backend-service',
+            'hostRules': [
+                {
+                    'description': '',
+                    'hosts': [
+                        '*.'
+                    ],
+                    'pathMatcher': 'path-matcher-one'
+                }
+            ],
+            'name': 'newtesturlmap-foo',
+            'pathMatchers': [
+                {
+                    'defaultService': 'https://www.googleapis.com/compute/v1/projects/myproject/global/backendServices/bes-pathmatcher-one-default',
+                    'description': 'path matcher one',
+                    'name': 'path-matcher-one',
+                    'pathRules': [
+                        {
+                            'paths': [
+                                '/data',
+                                '/aboutus'
+                            ],
+                            'service': 'https://www.googleapis.com/compute/v1/projects/myproject/global/backendServices/my-one-bes'
+                        }
+                    ]
+                }
+            ],
+        }
     
-        def __init__(self, name, action):
-        self.name = name
-        self.action = action
-    
-        def setUp(self):
-        self.tc1 = TC1()
-        self.tc2 = TC2()
-        self.tc3 = TC3()
-        self.average_result_tc1 = '###### In Test 1 ######\n' + \
-                                  'Setting up\n' + \
-                                  'Running test\n' + \
-                                  'Tearing down\n' + \
-                                  'Test Finished'
-        self.average_result_tc2 = '###### In Test 2 ######\n' + \
-                                  'Setting up\n' + \
-                                  'Running test\n' + \
-                                  'Tearing down\n' + \
-                                  'Test Finished'
-        self.average_result_tc3 = '###### In Test 3 ######\n' + \
-                                  'Setting up\n' + \
-                                  'Running test\n' + \
-                                  'Tearing down\n' + \
-                                  'Test Finished'
-        self.runner = TestRunner()
-        self.out = StringIO()
-        self.saved_stdout = sys.stdout
-        sys.stdout = self.out
-    
-    Test code which will almost always fail (if not exactly 12:01) when untestable
-production code (have a look into constructor_injection.py) is used:
+        model.train_on_batch(x_train[:32], y_train[:32],
+                         class_weight=class_weight)
+    score = model.evaluate(x_test[test_ids, :], y_test[test_ids, :], verbose=0)
+    assert(score < standard_score_sequential)
     
     
-def count_to(count):
-    '''Counts by word numbers, up to a maximum of five'''
-    numbers = ['one', 'two', 'three', 'four', 'five']
-    for number in numbers[:count]:
-        yield number
+@keras_test
+def test_atrousconv2d_legacy_interface():
+    old_layer = keras.layers.AtrousConvolution2D(
+        5, 3, 3,
+        atrous_rate=(2, 2),
+        init='normal',
+        subsample=(2, 2),
+        border_mode='valid',
+        dim_ordering='th',
+        W_regularizer='l1',
+        b_regularizer='l2',
+        W_constraint='maxnorm',
+        b_constraint='unitnorm',
+        name='conv')
+    new_layer = keras.layers.Conv2D(5, (3, 3),
+                                    kernel_initializer='normal',
+                                    strides=(2, 2),
+                                    padding='valid',
+                                    kernel_regularizer='l1',
+                                    bias_regularizer='l2',
+                                    kernel_constraint='max_norm',
+                                    bias_constraint='unit_norm',
+                                    data_format='channels_first',
+                                    dilation_rate=(2, 2),
+                                    name='conv')
+    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer.get_config())
     
-        print(u'Setting Data 1 = 10')
-    data1.data = 10
-    print(u'Setting Data 2 = 15')
-    data2.data = 15
-    print(u'Setting Data 1 = 3')
-    data1.data = 3
-    print(u'Setting Data 2 = 5')
-    data2.data = 5
-    print(u'Detach HexViewer from data1 and data2.')
-    data1.detach(view2)
-    data2.detach(view2)
-    print(u'Setting Data 1 = 10')
-    data1.data = 10
-    print(u'Setting Data 2 = 15')
-    data2.data = 15
+    # the data, shuffled and split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+    
+    
+@pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
+def test_lecun_uniform(tensor_shape):
+    fan_in, _ = initializers._compute_fans(tensor_shape)
+    scale = np.sqrt(3. / fan_in)
+    _runner(initializers.lecun_uniform(), tensor_shape,
+            target_mean=0., target_max=scale, target_min=-scale)
+    
+    model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(),
+              metrics=['accuracy'])
+    
+        # Arguments
+        config: dict of the form {'class_name': str, 'config': dict}
+        custom_objects: dict mapping class names (or function names)
+            of custom (non-Keras) objects to class/functions
+    
+        return os_version.dwMajorVersion
+    
+        def warn(self, fmt, *args, **kwargs):
+        self.log('WARNING', fmt, *args, **kwargs)
+    
+    # begin[licence]
+#
+# [The 'BSD licence']
+# Copyright (c) 2005-2008 Terence Parr
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# end[licensc]
+    
+        stream_types = [  #this is just a sample. Will make it in prepare()
+        # {'id': '1080'},
+        # {'id': '720'},
+        # {'id': '360'},
+        # {'id': '288'},
+        # {'id': '190'},
+        # {'id': '180'},
+        
+    ]
+    
+        title = r1(r'<meta name='description' content='([^']*)'', html)
+    flashvars = r1(r'flashvars='(type=[^']*)'', html)
+    
+    #----------------------------------------------------------------------
+def showroom_get_roomid_by_room_url_key(room_url_key):
+    '''str->str'''
+    fake_headers_mobile = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'UTF-8,*;q=0.5',
+        'Accept-Encoding': 'gzip,deflate,sdch',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'
+    }
+    webpage_url = 'https://www.showroom-live.com/' + room_url_key
+    html = get_content(webpage_url, headers = fake_headers_mobile)
+    roomid = match1(html, r'room\?room_id\=(\d+)')
+    assert roomid
+    return roomid
+    
+    from babel.messages.pofile import read_po
