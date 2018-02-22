c2e4abd123a35d1f60cad196121abefcfbc0b18c
SUNsung <weatherius@gmail.com>
2018-02-22 15:21:33 +0200

    Rewrite [28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb] File in: 2018-02-22 15:21:32

--- ruby/28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb
+++ ruby/28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb
@@ -1,3 +1,152 @@
 
         
-        
+              # Overwrite this factory method for Redis connections if you want to use a different Redis library than the redis gem.
+      # This is needed, for example, when using Makara proxies for distributed Redis.
+      cattr_accessor :redis_connector, default: ->(config) do
+        ::Redis.new(config.slice(:url, :host, :port, :db, :password))
+      end
+    
+            # Returns a hash with database connection information.
+        #
+        # == Examples
+        #
+        # Full hash Configuration.
+        #
+        #   configurations = { 'production' => { 'host' => 'localhost', 'database' => 'foo', 'adapter' => 'sqlite3' } }
+        #   Resolver.new(configurations).resolve(:production)
+        #   # => { 'host' => 'localhost', 'database' => 'foo', 'adapter' => 'sqlite3'}
+        #
+        # Initialized with URL configuration strings.
+        #
+        #   configurations = { 'production' => 'postgresql://localhost/foo' }
+        #   Resolver.new(configurations).resolve(:production)
+        #   # => { 'host' => 'localhost', 'database' => 'foo', 'adapter' => 'postgresql' }
+        #
+        def resolve(config)
+          if config
+            resolve_connection config
+          elsif env = ActiveRecord::ConnectionHandling::RAILS_ENV.call
+            resolve_symbol_connection env.to_sym
+          else
+            raise AdapterNotSpecified
+          end
+        end
+    
+      test 'response status aliases deprecated' do
+    response = ActionDispatch::TestResponse.create
+    assert_deprecated { response.success? }
+    assert_deprecated { response.missing? }
+    assert_deprecated { response.error? }
+  end
+end
+
+    
+          def clear_all # :nodoc:
+        reset_all
+        current_instances.clear
+      end
+    
+        class SetsResponseBody < ControllerWithCallbacks
+      before_action :set_body
+    
+      class BareEmptyTest < ActiveSupport::TestCase
+    test 'response body is nil' do
+      controller = BareEmptyController.new
+      controller.set_request!(ActionDispatch::Request.empty)
+      controller.set_response!(BareController.make_response!(controller.request))
+      controller.index
+      assert_nil controller.response_body
+    end
+  end
+    
+      class BlockMiddleware
+    attr_accessor :configurable_message
+    def initialize(app, &block)
+      @app = app
+      yield(self) if block_given?
+    end
+    
+        s = 'This formula is keg-only, which means it was not symlinked into #{HOMEBREW_PREFIX}.'
+    s << '\n\n#{f.keg_only_reason}'
+    if f.lib.directory? || f.include.directory?
+      s <<
+        <<-EOS.undent_________________________________________________________72
+    
+        # Get rid of any info 'dir' files, so they don't conflict at the link stage
+    info_dir_file = @f.info + 'dir'
+    if info_dir_file.file? && !@f.skip_clean?(info_dir_file)
+      observe_file_removal info_dir_file
+    end
+    
+          return true if prune == 'all'
+    
+      def participation_answer(user)
+    poll_participations.find_by(author_id: user.person.id)
+  end
+    
+          def fetch_public_key(o_auth_app, jwt)
+        public_key = fetch_public_key_from_json(o_auth_app.jwks, jwt)
+        if public_key.empty? && o_auth_app.jwks_uri
+          response = Faraday.get(o_auth_app.jwks_uri)
+          public_key = fetch_public_key_from_json(response.body, jwt)
+        end
+        raise Rack::OAuth2::Server::Authorize::BadRequest(:unauthorized_client) if public_key.empty?
+        public_key
+      end
+    
+        def split_colon_path(path)
+      one, two = path.split(':', 2)
+      if one && two && Sass::Util.windows? &&
+          one =~ /\A[A-Za-z]\Z/ && two =~ %r{\A[/\\]}
+        # If we're on Windows and we were passed a drive letter path,
+        # don't split on that colon.
+        one2, two = two.split(':', 2)
+        one = one + ':' + one2
+      end
+      return one, two
+    end
+    
+    # A logger that delays messages until they're explicitly flushed to an inner
+# logger.
+#
+# This can be installed around the current logger by calling \{#install!}, and
+# the original logger can be replaced by calling \{#uninstall!}. The log
+# messages can be flushed by calling \{#flush}.
+class Sass::Logger::Delayed < Sass::Logger::Base
+  # Installs a new delayed logger as the current Sass logger, wrapping the
+  # original logger.
+  #
+  # This can be undone by calling \{#uninstall!}.
+  #
+  # @return [Sass::Logger::Delayed] The newly-created logger.
+  def self.install!
+    logger = Sass::Logger::Delayed.new(Sass.logger)
+    Sass.logger = logger
+    logger
+  end
+    
+          temp_file = File.open('_escaped_params_tmp_file', 'w')
+      begin
+        temp_file.write('hello world')
+        temp_file.close
+    
+      it 'accepts requests with the same Accept-Language header' do
+    session = {:foo => :bar}
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
+    expect(session).not_to be_empty
+  end
+    
+      context 'called with one color' do
+    it 'applies same width to all sides' do
+      rule = 'border-width: 1px'
+    
+          expect('.padding-explicit').to have_rule(rule)
+    end
+  end
+    
+      context 'expands hover text inputs' do
+    it 'finds selectors' do
+      list = @inputs_list.map { |input| '#{input}:hover' }
+      list = list.join(', ')
+      ruleset = 'content: #{list};'
