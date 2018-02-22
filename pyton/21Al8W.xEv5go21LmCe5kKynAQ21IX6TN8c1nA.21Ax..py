981761fdb1612f47f093222cd05c22705bc98e52
SUNsung <weatherius@gmail.com>
2018-02-22 16:08:04 +0200

    Rewrite [21Al8W.xEv5go21LmCe5kKynAQ21IX6TN8c1nA.21Ax..py] File in: 2018-02-22 16:08:03

--- pyton/21Al8W.xEv5go21LmCe5kKynAQ21IX6TN8c1nA.21Ax..py
+++ pyton/21Al8W.xEv5go21LmCe5kKynAQ21IX6TN8c1nA.21Ax..py
@@ -1,24 +1,101 @@
 
         
-            def test_repr(self):
-        self.assertEqual(repr(self.addr2), 'certbot_apache.obj.Addr(('127.0.0.1', '443'))')
+        _BATCH_NORM_DECAY = 0.997
+_BATCH_NORM_EPSILON = 1e-5
     
-        @mock.patch('certbot_compatibility_test.validator.requests.get')
-    def test_hsts_include_subdomains(self, mock_get_request):
-        mock_get_request.return_value = create_response(
-            headers={'strict-transport-security':
-                     'max-age=31536000;includeSubDomains'})
-        self.assertTrue(self.validator.hsts('test.com'))
+      def test_tensor_shapes_resnet_50(self):
+    self.tensor_shapes_helper(50)
     
-    # If true, show URL addresses after external links.
-#latex_show_urls = False
+      def Conversion2dTestWithType(self, dtype):
+    original_data = np.arange(9).reshape(3, 3).astype(dtype)
+    serialized = datum_io.SerializeToString(original_data)
+    retrieved_data = datum_io.ParseFromString(serialized)
+    self.assertTrue(np.array_equal(original_data, retrieved_data))
     
-    # General information about the project.
-project = u'certbot-nginx'
-copyright = u'2014-2015, Let\'s Encrypt Project'
-author = u'Let\'s Encrypt Project'
+        Args:
+      data_format: Either 'channels_first' or 'channels_last'.
+        'channels_first' is typically faster on GPUs while 'channels_last' is
+        typically faster on CPUs. See
+        https://www.tensorflow.org/performance/performance_guide#data_formats
+    '''
+    super(Model, self).__init__()
+    if data_format == 'channels_first':
+      self._input_shape = [-1, 1, 28, 28]
+    else:
+      assert data_format == 'channels_last'
+      self._input_shape = [-1, 28, 28, 1]
     
-        @mock.patch('certbot.display.enhancements.util')
-    def test_redirect(self, mock_util):
-        mock_util().menu.return_value = (display_util.OK, 1)
-        self.assertTrue(self._call('redirect'))
+    
+def train(model, optimizer, dataset, log_interval=None):
+  '''Trains model on `dataset` using `optimizer`.'''
+    
+    
+def random_dataset():
+  batch_size = 64
+  images = tf.random_normal([batch_size, 784])
+  labels = tf.random_uniform([batch_size], minval=0, maxval=10, dtype=tf.int32)
+  return tf.data.Dataset.from_tensors((images, labels))
+    
+    
+if __name__ == '__main__':
+  tf.test.main()
+
+    
+    
+class Word2VecTest(tf.test.TestCase):
+    
+      def testPtbRawData(self):
+    tmpdir = tf.test.get_temp_dir()
+    for suffix in 'train', 'valid', 'test':
+      filename = os.path.join(tmpdir, 'ptb.%s.txt' % suffix)
+      with tf.gfile.GFile(filename, 'w') as fh:
+        fh.write(self._string_data)
+    # Smoke test
+    output = reader.ptb_raw_data(tmpdir)
+    self.assertEqual(len(output), 4)
+    
+    def my_model(features, labels, mode, params):
+    '''DNN with three hidden layers, and dropout of 0.1 probability.'''
+    # Create three fully connected layers each layer having a dropout
+    # probability of 0.1.
+    net = tf.feature_column.input_layer(features, params['feature_columns'])
+    for units in params['hidden_units']:
+        net = tf.layers.dense(net, units=units, activation=tf.nn.relu)
+    
+        model = create_temporal_sequential_model()
+    model.compile(loss=loss, optimizer='rmsprop',
+                  sample_weight_mode='temporal')
+    
+    print('Loading data...')
+(x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=max_words,
+                                                         test_split=0.2)
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
+    
+    
+@pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
+def test_orthogonal(tensor_shape):
+    _runner(initializers.orthogonal(), tensor_shape,
+            target_mean=0.)
+    
+    
+for seq_index in range(100):
+    # Take one sequence (part of the training set)
+    # for trying out decoding.
+    input_seq = encoder_input_data[seq_index: seq_index + 1]
+    decoded_sentence = decode_sequence(input_seq)
+    print('-')
+    print('Input sentence:', input_texts[seq_index])
+    print('Decoded sentence:', decoded_sentence)
+
+    
+    chars = sorted(list(set(text)))
+print('total chars:', len(chars))
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+    
+    # Set backend based on KERAS_BACKEND flag, if applicable.
+if 'KERAS_BACKEND' in os.environ:
+    _backend = os.environ['KERAS_BACKEND']
+    assert _backend in {'theano', 'tensorflow', 'cntk'}
+    _BACKEND = _backend
