2106907f61300503ba7bf4f51d3a3c1fddf552ee
SUNsung <weatherius@gmail.com>
2018-02-22 11:37:43 +0200

    Rewrite [01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py] File in: 2018-02-22 11:37:43

--- pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
+++ pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
@@ -1,142 +1,139 @@
-        Keyword:                   'bold #004461',   # class: 'k'
-        Keyword.Constant:          'bold #004461',   # class: 'kc'
-        Keyword.Declaration:       'bold #004461',   # class: 'kd'
-        Keyword.Namespace:         'bold #004461',   # class: 'kn'
-        Keyword.Pseudo:            'bold #004461',   # class: 'kp'
-        Keyword.Reserved:          'bold #004461',   # class: 'kr'
-        Keyword.Type:              'bold #004461',   # class: 'kt'
+
+        
+          Raises:
+    tf.errors.InvalidArgumentError: if batch_size or num_steps are too high.
+  '''
+  with tf.name_scope(name, 'PTBProducer', [raw_data, batch_size, num_steps]):
+    raw_data = tf.convert_to_tensor(raw_data, name='raw_data', dtype=tf.int32)
     
-    Provides utility functions that are consumed internally by Requests
-which depend on extremely few external helpers (such as compat)
-'''
+        Args:
+      dataset: the labeled dataset to evaluate, {'train', 'test', 'valid'}.
     
-        def lower_items(self):
-        '''Like iteritems(), but with all lowercase keys.'''
-        return (
-            (lowerkey, keyval[1])
-            for (lowerkey, keyval)
-            in self._store.items()
-        )
+          tf.summary.histogram(tag('gradient'), grad_values)
+      tf.summary.scalar(tag('gradient_norm'), tf.global_norm([grad_values]))
+    else:
+      tf.logging.info('Var %s has no gradient', var.op.name)
+
     
-        def test_server_finishes_when_no_connections(self):
-        '''the server thread exits even if there are no connections'''
-        server = Server.basic_response_server()
-        with server:
-            pass
     
-            if not qop:
-            respdig = KD(HA1, '%s:%s' % (nonce, HA2))
-        elif qop == 'auth' or 'auth' in qop.split(','):
-            noncebit = '%s:%s:%s:%s:%s' % (
-                nonce, ncvalue, cnonce, 'auth', HA2
-            )
-            respdig = KD(HA1, noncebit)
+def random_dataset():
+  batch_size = 64
+  images = tf.random_normal([batch_size, 784])
+  labels = tf.random_uniform([batch_size], minval=0, maxval=10, dtype=tf.int32)
+  return tf.data.Dataset.from_tensors((images, labels))
+    
+    
+if __name__ == '__main__':
+  tf.test.main()
+
+    
+            print(template.format(iris_data.SPECIES[class_id],
+                              100 * probability, expec))
+    
+      xy = (df, df.pop('Species'))
+  return xy, xy
+    
+    filegroup(
+    name = 'all_files',
+    srcs = glob(
+        ['**/*'],
+        exclude = [
+            '**/METADATA',
+            '**/OWNERS',
+        ],
+    ),
+    visibility = ['//tensorflow:__subpackages__'],
+)
+
+    
+        @value.setter
+    def value(self, new_value):
+        if 1 <= new_value <= 13:
+            self._value = new_value
         else:
-            # XXX handle auth-int.
-            return None
+            raise ValueError('Invalid card value: {}'.format(new_value))
     
+        def __init__(self, template_categories_to_budget_map):
+        self.categories_to_budget_map = template_categories_to_budget_map
     
-class RequestsWarning(Warning):
-    '''Base warning for Requests.'''
-    pass
+            (category1, 1), product4
+        (category1, 2), product1
+        (category1, 3), product2
+        (category2, 3), product1
+        (category2, 7), product3
+        '''
+        category, product_id = key
+        quantity = value
+        yield (category, quantity), product_id
     
-        # Informational.
-    100: ('continue',),
-    101: ('switching_protocols',),
-    102: ('processing',),
-    103: ('checkpoint',),
-    122: ('uri_too_long', 'request_uri_too_long'),
-    200: ('ok', 'okay', 'all_ok', 'all_okay', 'all_good', '\\o/', '✓'),
-    201: ('created',),
-    202: ('accepted',),
-    203: ('non_authoritative_info', 'non_authoritative_information'),
-    204: ('no_content',),
-    205: ('reset_content', 'reset'),
-    206: ('partial_content', 'partial'),
-    207: ('multi_status', 'multiple_status', 'multi_stati', 'multiple_stati'),
-    208: ('already_reported',),
-    226: ('im_used',),
+                        if 'G' not in server_type and 'g' not in server_type and server_type not in google_server_types:
+                        xlog.warn('IP:%s host:%s not support GAE, server type:%s status:%d', response.ssl_sock.ip, host, server_type, response.status)
+                        google_ip.report_connect_fail(response.ssl_sock.ip)
+                        response.close()
+                        continue
+                break
+        except OpenSSL.SSL.SysCallError as e:
+            errors.append(e)
+            xlog.warn('direct_handler.handler err:%r %s/%s', e, host, url)
+        except Exception as e:
+            errors.append(e)
+            xlog.exception('direct_handler.handler %r %s %s , retry...', e, host, url)
     
+        version_string = 'Version:%d-%d; Build:%d; Platform:%d; CSD:%s; ServicePack:%d-%d; Suite:%d; ProductType:%d' %  (
+        os_version.dwMajorVersion, os_version.dwMinorVersion,
+        os_version.dwBuildNumber,
+        os_version.dwPlatformId,
+        os_version.szCSDVersion,
+        os_version.wServicePackMajor, os_version.wServicePackMinor,
+        os_version.wSuiteMask,
+        os_version.wReserved
+    )
     
-@pytest.mark.parametrize(
-    'value, expected', (
-        ('foo='is a fish', bar='as well'', {'foo': 'is a fish', 'bar': 'as well'}),
-        ('key_without_value', {'key_without_value': None})
-    ))
-def test_parse_dict_header(value, expected):
-    assert parse_dict_header(value) == expected
+            I.i.u = insert u before op @ index i
+        R.x-y.u = replace x-y indexed tokens with u
     
+    class ClassicToken(Token):
+    '''@brief Alternative token implementation.
     
-class Command(ScrapyCommand):
+    A Token object like we'd use in ANTLR 2.x; has an actual string created
+    and associated with this object.  These objects are needed for imaginary
+    tree nodes that have payload objects.  We need to create a Token object
+    that has a string; the tree node will point at this token.  CommonToken
+    has indexes into a char stream and hence cannot be used to introduce
+    new strings.
+    '''
     
-        @property
-    def templates_dir(self):
-        _templates_base_dir = self.settings['TEMPLATES_DIR'] or \
-            join(scrapy.__path__[0], 'templates')
-        return join(_templates_base_dir, 'spiders')
-
+    from ..common import *
+from ..extractor import VideoExtractor
     
-        def _clean_req(self, request, method, results):
-        ''' stop the request from returning objects and records any errors '''
-    
-    _options = [
-    'help',
-    'version',
-    'gui',
-    'force',
-    'playlists',
-]
-_short_options = 'hVgfl'
-    
-        video_url = match1(html, r'filepath=(.+)&sec')
-    video_url = video_url.replace('&mid', '?mid')
-    
-    site_info = 'MioMio.tv'
-download = miomio_download
-download_playlist = playlist_not_supported('miomio')
-
-    
-    def mixcloud_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html(url, faker=True)
-    title = r1(r'<meta property='og:title' content='([^']*)'', html)
-    preview_url = r1(r'm-preview=\'([^\']+)\'', html)
-    preview = r1(r'previews(.*)\.mp3$', preview_url)
-    
-        def _adjust_thread_count(self):
-        # When the executor gets lost, the weakref callback will wake up
-        # the worker threads.
-        def weakref_cb(_, q=self._work_queue):
-            q.put(None)
-        # TODO(bquinlan): Should avoid creating new threads if there are more
-        # idle threads than items in the work queue.
-        if len(self._threads) < self._max_workers:
-            t = threading.Thread(target=_worker,
-                                 args=(weakref.ref(self, weakref_cb),
-                                       self._work_queue))
-            t.daemon = True
-            t.start()
-            self._threads.add(t)
-    
-        # Execute the template string in a temporary namespace and
-    # support tracing utilities by setting a value for frame.f_globals['__name__']
-    namespace = dict(_itemgetter=_itemgetter, __name__='namedtuple_%s' % typename,
-                     _property=property, _tuple=tuple)
-    try:
-        exec(template, namespace)
-    except SyntaxError:
-        e = _sys.exc_info()[1]
-        raise SyntaxError(e.message + ':\n' + template)
-    result = namespace[typename]
-    
-    def download_urls_sequential(urls, timeout=60):
-    url_to_content = {}
-    for url in urls:
+        for i in range(10, 30):
+        url = 'https://stream{i}.mixcloud.com/c/m4a/64{p}.m4a'.format(
+            i = i,
+            p = preview
+        )
         try:
-            url_to_content[url] = load_url(url, timeout=timeout)
-        except:
-            pass
-    return url_to_content
+            mime, ext, size = url_info(url)
+            break
+        except: continue
     
-    def with_process_pool_executor():
-    with ProcessPoolExecutor(10) as executor:
-        return list(executor.map(is_prime, PRIMES))
+        def prepare(self, **kwargs):
+        content = get_content(self.url)
+    
+        def extract(self, **kwargs):
+        for i in self.streams:
+            # for each available stream
+            s = self.streams[i]
+            # fill in 'container' field and 'size' field (optional)
+            _, s['container'], s['size'] = url_info(s['url'])
+            # 'src' field is a list of processed urls for direct downloading
+            # usually derived from 'url'
+            s['src'] = [s['url']]
+    
+        #title
+    title = ''
+    profile_api = 'https://www.showroom-live.com/api/room/profile?room_id={room_id}'.format(room_id = room_id)
+    html = loads(get_content(profile_api))
+    try:
+        title = html['main_name']
+    except KeyError:
+        title = 'Showroom_{room_id}'.format(room_id = room_id)
