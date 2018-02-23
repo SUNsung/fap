1face648de0135b7d29dc5a2213f08d59e047f37
SUNsung <weatherius@gmail.com>
2018-02-23 22:25:02 +0200

    Rewrite [57LVBZVtgmn257WndSwRWaCnE57BTvJomQu9Mw572GS.py] File in: 2018-02-23 22:25:02

--- pyton/57LVBZVtgmn257WndSwRWaCnE57BTvJomQu9Mw572GS.py
+++ pyton/57LVBZVtgmn257WndSwRWaCnE57BTvJomQu9Mw572GS.py
@@ -1,185 +1,177 @@
 
         
-        from youtube_dl.extractor import (
-    FacebookIE,
-    gen_extractors,
-    YoutubeIE,
-)
-    
-        def test_socks4a(self):
-        self.assertTrue(isinstance(self._get_ip('socks4a'), compat_str))
-    
-    
-for testfile in os.listdir(TEST_DIR):
-    _make_testfunc(testfile)
-    
+        
+def upgrade():
     try:
-    from .lazy_extractors import *
-    from .lazy_extractors import _ALL_CLASSES
-    _LAZY_LOADER = True
-except ImportError:
-    _LAZY_LOADER = False
-    from .extractors import *
+        constraint = find_constraint_name() or 'fk_columns_column_name_datasources'
+        with op.batch_alter_table('columns',
+                naming_convention=naming_convention) as batch_op:
+            batch_op.drop_constraint(constraint, type_='foreignkey')
+            batch_op.create_foreign_key(
+                'fk_columns_datasource_name_datasources',
+                'datasources',
+                ['datasource_name'], ['datasource_name'])
+    except:
+        logging.warning(
+            'Could not find or drop constraint on `columns`')
     
-        def _real_extract(self, url):
-        # URLs end with [uploader name]/[uploader title]
-        # this title is whatever the user types in, and is rarely
-        # the proper song title.  Real metadata is in the api response
-        album_url_tag = self._match_id(url)
-        result = {'_type': 'playlist', 'entries': []}
-        # There is no one endpoint for album metadata - instead it is included/repeated in each song's metadata
-        # Therefore we don't know how many songs the album has and must infi-loop until failure
-        for track_no in itertools.count():
-            # Get song's metadata
-            api_response = self._download_json(
-                'http://www.audiomack.com/api/music/url/album/%s/%d?extended=1&_=%d'
-                % (album_url_tag, track_no, time.time()), album_url_tag,
-                note='Querying song information (%d)' % (track_no + 1))
-    
-        def take_call(self, call):
-        '''Assume the employee will always successfully take the call.'''
-        self.call = call
-        self.call.employee = self
-        self.call.state = CallState.IN_PROGRESS
-    
-        def __init__(self, MAX_SIZE):
-        self.MAX_SIZE = MAX_SIZE
-        self.size = 0
-        self.lookup = {}  # key: query, value: node
-        self.linked_list = LinkedList()
-    
-    
-class Message(object):
-    
-        def __init__(self, license_plate):
-        super(Motorcycle, self).__init__(VehicleSize.MOTORCYCLE, license_plate, spot_size=1)
-    
-            (2016-01, shopping), 25
-        (2016-01, shopping), 100
-        (2016-01, gas), 50
-        '''
-        timestamp, seller, amount = line.split('\t')
-        period = self. extract_year_month(timestamp)
-        if period == self.current_year_month():
-            yield (period, category), amount
-    
-    
-if __name__ == '__main__':
-    HitCounts.run()
-
-    
-    # revision identifiers, used by Alembic.
-revision = '12d55656cbca'
-down_revision = '55179c7f25c7'
-    
-    # revision identifiers, used by Alembic.
-revision = '1a48a5411020'
-down_revision = '289ce07647b'
-    
-    
-def downgrade():
-    op.drop_column('logs', 'dt')
-
-    
-    slice_user = Table('slice_user', Base.metadata,
-    Column('id', Integer, primary_key=True),
-    Column('user_id', Integer, ForeignKey('ab_user.id')),
-    Column('slice_id', Integer, ForeignKey('slices.id'))
-)
+    Revision ID: 430039611635
+Revises: d827694c7555
+Create Date: 2016-02-10 08:47:28.950891
     
     
 def upgrade():
-    op.add_column(
-        'dbs',
-        sa.Column(
-            'password',
-            EncryptedType(sa.String(1024)),
-        nullable=True))
-    
-    def upgrade():
-    op.add_column('datasources', sa.Column('offset', sa.Integer(), nullable=True))
-    op.add_column('tables', sa.Column('offset', sa.Integer(), nullable=True))
-    
-    Revision ID: 315b3f4da9b0
-Revises: 1a48a5411020
-Create Date: 2015-12-04 11:16:58.226984
-    
-    Base = declarative_base()
-    
-    
-def downgrade():
-    op.drop_column('metrics', 'created_on')
-    op.drop_column('metrics', 'created_by_fk')
-    op.drop_column('metrics', 'changed_on')
-    op.drop_column('metrics', 'changed_by_fk')
+    op.add_column('metrics', sa.Column('changed_by_fk', sa.Integer(), nullable=True))
+    op.add_column('metrics', sa.Column('changed_on', sa.DateTime(), nullable=True))
+    op.add_column('metrics', sa.Column('created_by_fk', sa.Integer(), nullable=True))
+    op.add_column('metrics', sa.Column('created_on', sa.DateTime(), nullable=True))
     try:
-        op.alter_column('url', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.alter_column('url', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.alter_column('tables', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.alter_column('tables', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.alter_column('table_columns', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.alter_column('table_columns', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.alter_column('sql_metrics', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.alter_column('sql_metrics', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.alter_column('slices', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.alter_column('slices', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.drop_constraint(None, 'metrics', type_='foreignkey')
-        op.drop_constraint(None, 'metrics', type_='foreignkey')
-        op.alter_column('dbs', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.alter_column('dbs', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.alter_column('datasources', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.alter_column('datasources', 'created_by_fk',
-                   existing_type=sa.INTEGER(),
-                   nullable=False)
-        op.alter_column('datasources', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.alter_column('datasources', 'changed_by_fk',
-                   existing_type=sa.INTEGER(),
-                   nullable=False)
-        op.alter_column('dashboards', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.alter_column('dashboards', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.alter_column('css_templates', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.alter_column('css_templates', 'changed_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
-        op.alter_column('columns', 'created_on',
-                   existing_type=sa.DATETIME(),
-                   nullable=False)
         op.alter_column('columns', 'changed_on',
                    existing_type=sa.DATETIME(),
-                   nullable=False)
+                   nullable=True)
+        op.alter_column('columns', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('css_templates', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('css_templates', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('dashboards', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('dashboards', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('datasources', 'changed_by_fk',
+                   existing_type=sa.INTEGER(),
+                   nullable=True)
+        op.alter_column('datasources', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('datasources', 'created_by_fk',
+                   existing_type=sa.INTEGER(),
+                   nullable=True)
+        op.alter_column('datasources', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('dbs', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('dbs', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('slices', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('slices', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('sql_metrics', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('sql_metrics', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('table_columns', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('table_columns', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('tables', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('tables', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('url', 'changed_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.alter_column('url', 'created_on',
+                   existing_type=sa.DATETIME(),
+                   nullable=True)
+        op.create_foreign_key(None, 'metrics', 'ab_user', ['changed_by_fk'], ['id'])
+        op.create_foreign_key(None, 'metrics', 'ab_user', ['created_by_fk'], ['id'])
     except:
         pass
+    
+            class SetCookieFalsyFlags(RequestHandler):
+            def get(self):
+                self.set_cookie('a', '1', secure=True)
+                self.set_cookie('b', '1', secure=False)
+                self.set_cookie('c', '1', httponly=True)
+                self.set_cookie('d', '1', httponly=False)
+    
+            The name of the argument is provided if known, but may be None
+        (e.g. for unnamed groups in the url regex).
+        '''
+        try:
+            return _unicode(value)
+        except UnicodeDecodeError:
+            raise HTTPError(400, 'Invalid unicode in %s: %r' %
+                            (name or 'url', value[:40]))
+    
+                def data_received(self, data):
+                body.append(data)
+    
+            # It would be better to run the wsgiref server implementation in
+        # another thread instead of using our own WSGIContainer, but this
+        # fits better in our async testing framework and the wsgiref
+        # validator should keep us honest
+        return WSGIContainer(validator(WSGIApplication([
+            ('/', HelloHandler),
+            ('/path/(.*)', PathQuotingHandler),
+            ('/typecheck', TypeCheckHandler),
+        ])))
+    
+        def _sock_state_cb(self, fd, readable, writable):
+        state = ((IOLoop.READ if readable else 0) |
+                 (IOLoop.WRITE if writable else 0))
+        if not state:
+            self.io_loop.remove_handler(fd)
+            del self.fds[fd]
+        elif fd in self.fds:
+            self.io_loop.update_handler(fd, state)
+            self.fds[fd] = state
+        else:
+            self.io_loop.add_handler(fd, self._handle_events, state)
+            self.fds[fd] = state
+    
+    
+def try_close(f):
+    # Avoid issue #875 (race condition when using the file in another
+    # thread).
+    for i in range(10):
+        try:
+            f.close()
+        except IOError:
+            # Yield to another thread
+            time.sleep(1e-3)
+        else:
+            break
+    # Try a last time and let raise
+    f.close()
+    
+    define('port', default=8888)
+define('num_chunks', default=1000)
+define('chunk_size', default=2048)
+    
+        def wait_for_messages(self, cursor=None):
+        # Construct a Future to return to our caller.  This allows
+        # wait_for_messages to be yielded from a coroutine even though
+        # it is not a coroutine itself.  We will set the result of the
+        # Future when results are available.
+        result_future = Future()
+        if cursor:
+            new_count = 0
+            for msg in reversed(self.cache):
+                if msg['id'] == cursor:
+                    break
+                new_count += 1
+            if new_count:
+                result_future.set_result(self.cache[-new_count:])
+                return result_future
+        self.waiters.add(result_future)
+        return result_future
