09df18a0f22e727bbf228c6ddf3423dea2dc9c50
SUNsung <weatherius@gmail.com>
2018-02-24 01:57:57 +0200

    Rewrite [575urXwEo.Ecw57tvsrTSuiLkw57IESfWuAmlSg57b5D.cpp] File in: 2018-02-24 01:57:57

--- cccccc/575urXwEo.Ecw57tvsrTSuiLkw57IESfWuAmlSg57b5D.cpp
+++ cccccc/575urXwEo.Ecw57tvsrTSuiLkw57IESfWuAmlSg57b5D.cpp
@@ -1,160 +1,128 @@
 
         
-        class DesktopCapturer: public mate::EventEmitter<DesktopCapturer>,
-                       public DesktopMediaListObserver {
- public:
-  static mate::Handle<DesktopCapturer> Create(v8::Isolate* isolate);
-    }
+        #include <osquery/registry.h>
+#include <osquery/status.h>
     
-    void GlobalShortcut::UnregisterAll() {
-  accelerator_callback_map_.clear();
-  GlobalShortcutListener::GetInstance()->UnregisterAccelerators(this);
-}
+      /// Protector for broadcast lookups and external registry mutations.
+  mutable Mutex mutex_;
     
-    RenderProcessPreferences::RenderProcessPreferences(
-    v8::Isolate* isolate,
-    const atom::RenderProcessPreferences::Predicate& predicate)
-    : preferences_(predicate) {
-  Init(isolate);
-}
+    /**
+ * @brief Mimic the REGISTER macro, extensions should use this helper.
+ *
+ * The SDK does not provide a REGISTER macro for modules or extensions.
+ * Tools built with the osquery SDK should use REGISTER_EXTERNAL to add to
+ * their own 'external' registry. This registry will broadcast to the osquery
+ * extension manager (core) in an extension.
+ *
+ * osquery 'modules' should not construct their plugin registrations in
+ * global scope (global construction time). Instead they should use the
+ * module call-in well defined symbol, declare their SDK constraints, then
+ * use the REGISTER_MODULE call within `initModule`.
+ */
+#define REGISTER_EXTERNAL(t, r, n)                                             \
+  namespace registries {                                                       \
+  const ::osquery::registries::PI<t> k##ExtensionRegistryItem##t(r, n, false); \
+  }
     
-    class RenderProcessPreferences
-    : public mate::Wrappable<RenderProcessPreferences> {
- public:
-  static mate::Handle<RenderProcessPreferences>
-      ForAllWebContents(v8::Isolate* isolate);
-    }
+    /**
+ * @brief Converts a struct tm to ASCII time UTC by converting the tm_time to
+ * epoch and then running gmtime() on the new epoch
+ *
+ * @param tm_time the local time/date to covert to UTC ASCII time
+ *
+ * @return the data/time of tm_time in the format: 'Wed Sep 21 10:27:52 2011'
+ */
+std::string toAsciiTimeUTC(const struct tm* tm_time);
     
-      // The auth is cancelled, must be called on UI thread.
-  void CancelAuth();
-    
-    
-    {  // Wait the SIGUSR2 signal to happen.
-  struct signalfd_siginfo si;
-  HANDLE_EINTR(read(usr2_fd, &si, sizeof(si)));
-}
-    
-    
-    {}  // namespace accelerator_util
-    
-    #include <jni.h>
-    
-    // Licensed under the MIT License (the 'License'); you may not use this file except in 
-// compliance with the License. You may obtain a copy of the License at
-// http://opensource.org/licenses/MIT
-    
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
+    #pragma once
     
     
     {
-    {        if (JNI_OK == status_) {
-            we_attach_ = true;
-            pthread_setspecific(g_env_key, env_);
-        } else {
-            ASSERT2(false, 'vm:%p, env:%p, status:%d', vm_, env_, status_);
-            env_ = NULL;
-            return;
-        }
-    } while (false);
-    
-    jint ret = env_->PushLocalFrame(_capacity);
-    ASSERT2(0 == ret, 'ret:%d', ret);
+    {  return readFile(value, pack);
 }
+}
+
     
-    namespace rocksdb {
+    #include 'osquery/config/plugins/tls_config.h'
+#include 'osquery/core/conversions.h'
+#include 'osquery/core/json.h'
+#include 'osquery/dispatcher/scheduler.h'
+#include 'osquery/remote/requests.h'
+#include 'osquery/remote/serializers/json.h'
+#include 'osquery/remote/transports/tls.h'
+#include 'osquery/remote/utility.h'
+    
+    Status TLSConfigPlugin::setUp() {
+  if (FLAGS_enroll_always && !FLAGS_disable_enrollment) {
+    // clear any cached node key
+    clearNodeKey();
+    auto node_key = getNodeKey('tls');
+    if (node_key.size() == 0) {
+      // Could not generate a node key, continue logging to stderr.
+      return Status(1, 'No node key, TLS config failed.');
+    }
+  }
     }
     
-      std::string res;
-  bool status = slists.Get('k1', &res);
-  ASSERT_TRUE(status);
-    
-    /*
- * Class:     org_rocksdb_IngestExternalFileOptions
- * Method:    newIngestExternalFileOptions
- * Signature: ()J
- */
-jlong Java_org_rocksdb_IngestExternalFileOptions_newIngestExternalFileOptions__(
-    JNIEnv* env, jclass jclazz) {
-  auto* options = new rocksdb::IngestExternalFileOptions();
-  return reinterpret_cast<jlong>(options);
-}
-    
-    
-// --------------------
-// --- EnvLibrados ----
-// --------------------
-/**
- * @brief EnvLibrados ctor
- * @details [long description]
- *
- * @param db_name unique database name
- * @param config_path the configure file path for rados
- */
-EnvLibrados::EnvLibrados(const std::string& db_name,
-                         const std::string& config_path,
-                         const std::string& db_pool)
-  : EnvLibrados('client.admin',
-                'ceph',
-                0,
-                db_name,
-                config_path,
-                db_pool,
-                '/wal',
-                db_pool,
-                1 << 20) {}
-    
-        // `flatbuffers::grpc::MessageBuilder` is a `FlatBufferBuilder` with a
-    // special allocator for efficient gRPC buffer transfer, but otherwise
-    // usage is the same as usual.
-    auto msg_offset = mb_.CreateString('Hello, ' + name);
-    auto hello_offset = CreateHelloReply(mb_, msg_offset);
-    mb_.Finish(hello_offset);
-    
-    struct TypeBuilder {
-  flatbuffers::FlatBufferBuilder &fbb_;
-  flatbuffers::uoffset_t start_;
-  void add_base_type(BaseType base_type) {
-    fbb_.AddElement<int8_t>(Type::VT_BASE_TYPE, static_cast<int8_t>(base_type), 0);
-  }
-  void add_element(BaseType element) {
-    fbb_.AddElement<int8_t>(Type::VT_ELEMENT, static_cast<int8_t>(element), 0);
-  }
-  void add_index(int32_t index) {
-    fbb_.AddElement<int32_t>(Type::VT_INDEX, index, -1);
-  }
-  TypeBuilder(flatbuffers::FlatBufferBuilder &_fbb)
-        : fbb_(_fbb) {
-    start_ = fbb_.StartTable();
-  }
-  TypeBuilder &operator=(const TypeBuilder &);
-  flatbuffers::Offset<Type> Finish() {
-    const auto end = fbb_.EndTable(start_);
-    auto o = flatbuffers::Offset<Type>(end);
-    return o;
-  }
-};
-    
-    
-    {}  // namespace NamespaceC
-    
-    ::grpc::ClientAsyncResponseReader< flatbuffers::grpc::Message<Stat>>* MonsterStorage::Stub::PrepareAsyncStoreRaw(::grpc::ClientContext* context, const flatbuffers::grpc::Message<Monster>& request, ::grpc::CompletionQueue* cq) {
-  return ::grpc::internal::ClientAsyncResponseReaderFactory< flatbuffers::grpc::Message<Stat>>::Create(channel_.get(), cq, rpcmethod_Store_, context, request, false);
-}
-    
-    grpc::string GetHeaderPrologue(grpc_generator::File *file,
-                               const Parameters & /*params*/) {
-  grpc::string output;
-  {
-    // Scope the output stream so it closes and finalizes output to the string.
-    auto printer = file->CreatePrinter(&output);
-    std::map<grpc::string, grpc::string> vars;
+      std::stringstream result;
+  CFDataGetBytes(cf_data, range, (UInt8*)buffer);
+  for (CFIndex i = 0; i < range.length; ++i) {
+    uint8_t byte = buffer[i];
+    if (isprint(byte)) {
+      result << byte;
+    } else if (buffer[i] == 0) {
+      result << ' ';
+    } else {
+      result << '%' << std::setfill('0') << std::setw(2) << std::hex
+             << (int)byte;
     }
+  }
+    
+                ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our windows open/close state
+            ImGui::Checkbox('Another Window', &show_another_window);
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you use this binding you'll need to call 4 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXXX_NewFrame(), ImGui::Render() and ImGui_ImplXXXX_Shutdown().
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+        if (pEvent->m_Pressed == 1)
+    {
+        if (pEvent->m_Button == S3E_POINTER_BUTTON_LEFTMOUSE)
+            g_MousePressed[0] = true;
+        if (pEvent->m_Button == S3E_POINTER_BUTTON_RIGHTMOUSE)
+            g_MousePressed[1] = true;
+        if (pEvent->m_Button == S3E_POINTER_BUTTON_MIDDLEMOUSE)
+            g_MousePressed[2] = true;
+        if (pEvent->m_Button == S3E_POINTER_BUTTON_MOUSEWHEELUP)
+            io.MouseWheel += pEvent->m_y;
+        if (pEvent->m_Button == S3E_POINTER_BUTTON_MOUSEWHEELDOWN)
+            io.MouseWheel += pEvent->m_y;
     }
     
-      virtual grpc::string name() const = 0;
+    // Use if you want to reset your rendering device without losing ImGui state.
+IMGUI_API void        ImGui_Marmalade_InvalidateDeviceObjects();
+IMGUI_API bool        ImGui_Marmalade_CreateDeviceObjects();
     
-      typedef void (*ErrorFn)(const FlatCompiler *flatc, const std::string &err,
-                          bool usage, bool show_exe_name);
+    
+    {    // At this point note that we set ImGui::GetIO().Fonts->TexID to be == g_FontTexture, so clear both.
+    ImGuiIO& io = ImGui::GetIO();
+    IM_ASSERT(g_FontTexture == io.Fonts->TexID);
+    if (g_FontTexture)
+        g_FontTexture->Release();
+    g_FontTexture = NULL;
+    io.Fonts->TexID = NULL;
+}
+    
+    struct GLFWwindow;
+    
+    // GLFW callbacks (installed by default if you enable 'install_callbacks' during initialization)
+// Provided here if you want to chain callbacks.
+// You can also handle inputs yourself and use those as a reference.
+IMGUI_API void        ImGui_ImplGlfw_MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+IMGUI_API void        ImGui_ImplGlfw_ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+IMGUI_API void        ImGui_ImplGlfw_KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+IMGUI_API void        ImGui_ImplGlfw_CharCallback(GLFWwindow* window, unsigned int c);
+
+    
+        g_Window = window;
