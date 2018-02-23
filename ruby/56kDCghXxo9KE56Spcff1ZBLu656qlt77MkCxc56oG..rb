6ab75d2382ae067552951fd66d1e79974f0ee7d1
SUNsung <weatherius@gmail.com>
2018-02-23 07:38:29 +0200

    Rewrite [56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb] File in: 2018-02-23 07:38:28

--- ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
+++ ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
@@ -1,76 +1,102 @@
-    respond_to do |format|
-      if !running? && @job.destroy
-        format.html { redirect_to jobs_path, notice: 'Job deleted.' }
-        format.json { head :no_content }
-      else
-        format.html { redirect_to jobs_path, alert: 'Can not delete a running job.' }
-        format.json { render json: @job.errors, status: :unprocessable_entity }
-      end
-    end
+
+        
+        describe 'String#swapcase' do
+  it 'returns a new string with all uppercase chars from self converted to lowercase and vice versa' do
+   'Hello'.swapcase.should == 'hELLO'
+   'cYbEr_PuNk11'.swapcase.should == 'CyBeR_pUnK11'
+   '+++---111222???'.swapcase.should == '+++---111222???'
   end
     
-      def user_credential_params
-    params.require(:user_credential).permit(:credential_name, :credential_value, :mode)
+      it 'understands integers' do
+    '20'.to_c.should == Complex(20)
+  end
+    
+      it 'raises a #{frozen_error_class} if self is frozen' do
+    s = 'abcdefghijklmnopqR'.freeze
+    lambda { s.tr!('cdefg', '12') }.should raise_error(frozen_error_class)
+    lambda { s.tr!('R', 'S')      }.should raise_error(frozen_error_class)
+    lambda { s.tr!('', '')        }.should raise_error(frozen_error_class)
   end
 end
 
     
-        def add(path, content)
-      @pages[path] = content
+      it 'sends #to_str to the argument and returns the result if it's a String' do
+    x = String.new
+    obj = mock('to_str')
+    obj.should_receive(:to_str).and_return(x)
+    String.try_convert(obj).should equal(x)
+  end
+    
+        ruby_version_is '2.5' do
+      it 'returns the same object for equal unfrozen strings' do
+        origin = 'this is a string'
+        dynamic = %w(this is a string).join(' ')
+    
+        private
+    
+        # apply general less to scss conversion
+    def convert_to_scss(file)
+      # get local mixin names before converting the definitions
+      mixins = shared_mixins + read_mixins(file)
+      file   = replace_vars(file)
+      file   = replace_mixin_definitions(file)
+      file   = replace_mixins(file, mixins)
+      file   = extract_mixins_from_selectors(file, CLASSES_TO_MIXINS.inject({}) { |h, cl| h.update('.#{cl}' => cl) })
+      file   = replace_spin(file)
+      file   = replace_fadein(file)
+      file   = replace_image_urls(file)
+      file   = replace_escaping(file)
+      file   = convert_less_ampersand(file)
+      file   = deinterpolate_vararg_mixins(file)
+      file   = replace_calculation_semantics(file)
+      file   = replace_file_imports(file)
+      file   = wrap_at_groups_with_at_root(file)
+      file
     end
     
-          dest = url.normalized_path
-      dest_dir = Pathname.new(dest)
+    desc 'Start a dummy (test) Rails app server'
+task :dummy_rails do
+  require 'rack'
+  require 'term/ansicolor'
+  port = ENV['PORT'] || 9292
+  puts %Q(Starting on #{Term::ANSIColor.cyan 'http://localhost:#{port}'})
+  Rack::Server.start(
+    config: 'test/dummy_rails/config.ru',
+    Port: port)
+end
     
-        public
+        def index
+      pods_json = PodPresenter.as_collection(Pod.all)
     
-      class DummyOutput < String
-    alias write concat
+        def any_searchfield_present?
+      if %w(username email guid under13).all? { |attr| public_send(attr).blank? }
+        errors.add :base, 'no fields for search set'
+      end
+    end
+    
+    get '/stream', :provides => 'text/event-stream' do
+  stream :keep_open do |out|
+    connections << out
+    out.callback { connections.delete(out) }
   end
-  def no_error(*args)
-    $stderr, stderr = DummyOutput.new, $stderr
-    assert_nothing_raised(*args) {return yield}
-  ensure
-    stderr, $stderr = $stderr, stderr
-    $!.backtrace.delete_if {|e| /\A#{Regexp.quote(__FILE__)}:#{__LINE__-2}/o =~ e} if $!
-    assert_empty(stderr)
-  end
+end
     
+    # include would include the module in Object
+# extend only extends the `main` object
+extend Sinatra::Delegator
     
-    {        Zlib::GzipReader.open(t.path) do |f|
-          assert_equal(false, f.closed?)
-          f.read
-          assert_equal(false, f.closed?)
-          f.close
-          assert_equal(true, f.closed?)
+          def remove_bad_cookies(request, response)
+        return if bad_cookies.empty?
+        paths = cookie_paths(request.path)
+        bad_cookies.each do |name|
+          paths.each { |path| response.set_cookie name, empty_cookie(request.host, path) }
         end
-      }
-    end
-    
-            expect_any_instance_of(ActivityPub::LinkedDataSignature).to receive(:verify_account!).and_return(actor)
-        expect(ActivityPub::Activity).to receive(:factory).with(instance_of(Hash), actor, instance_of(Hash))
-    
-        respond_to do |format|
-      format.html do
-        serializable_resource = ActiveModelSerializers::SerializableResource.new(InitialStatePresenter.new(initial_state_params), serializer: InitialStateSerializer)
-        @initial_state_json   = serializable_resource.to_json
       end
     
-      private
+          session = {:foo => :bar}
+      get '/', {}, 'rack.session' => session, 'HTTP_USER_AGENT' => 'a'
+      get '/', {}, 'rack.session' => session, 'HTTP_USER_AGENT' => 'b'
+      expect(session[:foo]).to eq :bar
+    end
     
-            def on_send(node)
-          return unless match_call?(node) &&
-                        (!node.value_used? || only_truthiness_matters?(node)) &&
-                        !(node.parent && node.parent.block_type?)
-    
-            private
-    
-            MSG = 'Do not freeze immutable objects, as freezing them has no ' \
-              'effect.'.freeze
-    
-      context 'called with null values' do
-    it 'writes rules for other three' do
-      ruleset = 'padding-top: 11px; ' +
-                'padding-right: 12px; ' +
-                'padding-left: 13px;'
-      bad_rule = 'padding-bottom: null;'
+    class LogStash::PluginManager::Generate < LogStash::PluginManager::Command
