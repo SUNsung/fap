70660e51490fa31097b7674eeff8af7c9dea3c89
SUNsung <weatherius@gmail.com>
2018-02-23 15:57:38 +0200

    Rewrite [20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py] File in: 2018-02-23 15:57:38

--- pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
+++ pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
@@ -1,84 +1,72 @@
 
         
-        
-class CallState(Enum):
+            def entry_ok(self):
+        'Return sensible ConfigParser section name or None.'
+        self.entry_error['text'] = ''
+        name = self.entry.get().strip()
+        if not name:
+            self.showerror('no name specified.')
+            return None
+        elif len(name)>30:
+            self.showerror('name is longer than 30 characters.')
+            return None
+        elif name in self.used_names:
+            self.showerror('name is already in use.')
+            return None
+        return name
     
-        def __init__(self, vehicle_size, license_plate, spot_size):
-        self.vehicle_size = vehicle_size
-        self.license_plate = license_plate
-        self.spot_size
-        self.spots_taken = []
-    
-        def reducer_identity(self, key, value):
-        yield key, value
-    
-    
-if __name__ == '__main__':
-    RemoveDuplicateUrls.run()
-
-    
-    
-class Page(object):
-    
-      Args:
-    filename_cc: is the path for the source (e.g. .cc) file
-    filename_h: is the path for the header path
-    
-                self._work_queue.put(w)
-            self._adjust_thread_count()
-            return f
-    submit.__doc__ = _base.Executor.submit.__doc__
-    
-    from ycm.client.base_request import ( BaseRequest, BuildRequestData,
-                                      HandleServerException )
+        >>> from fractions import Fraction as F
+    >>> variance([F(1, 6), F(1, 2), F(5, 3)])
+    Fraction(67, 108)
     
     
-def FormatDebugInfoResponse_ExtraConfFoundButNotLoaded_test():
-  response = deepcopy( GENERIC_RESPONSE )
-  response[ 'extra_conf' ].update( {
-    'is_loaded': False,
-    'path': '/path/to/extra/conf'
-  } )
-  assert_that(
-    FormatDebugInfoResponse( response ),
-    contains_string(
-      'Extra configuration file found but not loaded\n'
-      'Extra configuration path: /path/to/extra/conf\n'
-    )
-  )
+# Use speedup if available
+scanstring = c_scanstring or py_scanstring
     
+    >>> from nntplib import NNTP
+>>> s = NNTP('news')
+>>> resp, count, first, last, name = s.group('comp.lang.python')
+>>> print('Group', name, 'has', count, 'articles, range', first, 'to', last)
+Group comp.lang.python has 51 articles, range 5770 to 5821
+>>> resp, subs = s.xhdr('subject', '{0}-{1}'.format(first, last))
+>>> resp = s.quit()
+>>>
     
-def exec_in(code, glob, loc=None):
-    # type: (Any, Dict[str, Any], Optional[Mapping[str, Any]]) -> Any
-    if isinstance(code, basestring_type):
-        # exec(string) inherits the caller's future imports; compile
-        # the string first to prevent that.
-        code = compile(code, '<string>', 'exec', dont_inherit=True)
-    exec(code, glob, loc)
+        def aliases_of(self, red, green, blue):
+        try:
+            name, aliases = self.__byrgb[(red, green, blue)]
+        except KeyError:
+            raise BadColor((red, green, blue)) from None
+        return [name] + aliases
     
-    from tornado.test import httpserver_test
-from tornado.test import web_test
+            If no XML data is given then it is read from stdin. The resulting
+        XML-RPC response is printed to stdout along with the correct HTTP
+        headers.
+        '''
     
+        @support.requires_IEEE_754
+    def test_negative_zero_repr_str(self):
+        def test(v, expected, test_fn=self.assertEqual):
+            test_fn(repr(v), expected)
+            test_fn(str(v), expected)
     
-class HomeHandler(BaseHandler):
-    def get(self):
-        entries = self.db.query('SELECT * FROM entries ORDER BY published '
-                                'DESC LIMIT 5')
-        if not entries:
-            self.redirect('/compose')
-            return
-        self.render('home.html', entries=entries)
+            a = 0
+        b = sys.maxsize**10
+        c = 2*sys.maxsize
+        expected_len = 1 + (b - a) // c
+        x = range(a, b, c)
+        self.assertIn(a, x)
+        self.assertNotIn(b, x)
+        self.assertRaises(OverflowError, len, x)
+        self.assertTrue(x)
+        self.assertEqual(_range_len(x), expected_len)
+        self.assertEqual(x[0], a)
+        idx = sys.maxsize+1
+        self.assertEqual(x[idx], a+(idx*c))
+        self.assertEqual(x[idx:idx+1][0], a+(idx*c))
+        with self.assertRaises(IndexError):
+            x[-expected_len-1]
+        with self.assertRaises(IndexError):
+            x[expected_len]
     
-    
-class CommandTest(unittest.TestCase):
-    
-        @classmethod
-    def setUpClass(cls):
-        ''' Class scope setup. '''
-        cls.p = Proxy()
-    
-    ### OUTPUT ###
-# Counting to two...
-# one two
-# Counting to five...
-# one two three four five
+            self.assertEqual(x[:], a.tolist())
