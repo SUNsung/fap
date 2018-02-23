872b9b08203a09ab5768985bc001f29388899e39
SUNsung <weatherius@gmail.com>
2018-02-23 12:05:08 +0200

    Rewrite [23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp] File in: 2018-02-23 12:05:07

--- cccccc/23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp
+++ cccccc/23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp
@@ -1,299 +1,187 @@
-bool PartialRunMgr::FindOrCreate(int step_id,
-                                 CancellationManager** cancellation_manager) {
-  mutex_lock l(mu_);
-  auto it = step_id_to_partial_run_.find(step_id);
-  if (it != step_id_to_partial_run_.end()) {
-    *cancellation_manager = it->second->cancellation_manager.get();
-    return false;
-  }
-    }
-    
-      void Compute(OpKernelContext* context) override LOCKS_EXCLUDED(mu_) {
-    mutex_lock l(mu_);
-    if (resource_ == nullptr) {
-      ResourceMgr* mgr = context->resource_manager();
-      OP_REQUIRES_OK(context, cinfo_.Init(mgr, def()));
-    }
-    }
-    
-    // Estimate the cost of running a Grappler item by actually running the
-// corresponding TensorFlow graph on the specified cluster and measuring the
-// runtimes.
-class MeasuringCostEstimator : public CostEstimator {
- public:
-  // Run the model for measurement_steps to measure its average cost.
-  // When measurement_threads is greater than 0, use a threadpool of as many
-  // threads to run the measurements; otherwise, run them serially. Does not
-  // take ownership of cluster.
-  explicit MeasuringCostEstimator(Cluster* cluster, int measurement_steps,
-                                  int measurement_threads);
-  ~MeasuringCostEstimator() override {}
-    }
-    
-    Status FetchOutputs(Graph* g, const DeviceAttributes& device_info,
-                    const gtl::ArraySlice<string>& fetch_outputs,
-                    bool use_function_convention, NameIndex* name_index,
-                    std::vector<Node*>* out_fetch_nodes,
-                    DataTypeVector* out_fetch_types) {
-  out_fetch_nodes->clear();
-  out_fetch_nodes->reserve(fetch_outputs.size());
-  for (size_t i = 0; i < fetch_outputs.size(); ++i) {
-    const string& t = fetch_outputs[i];
-    }
-    }
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-      scoped_ptr<base::Value> value_args(
-      converter->FromV8Value(args, isolate->GetCurrentContext()));
-  if (!value_args.get() ||
-      !value_args->IsType(base::Value::TYPE_LIST))
-    return isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate,
-        'Unable to convert 'args' passed to CallObjectMethodSync')));
-    
-    typedef std::map<std::string,std::string> KeyMap;
-    
-    void MenuItem::SetSubmenu(Menu* sub_menu) {
-  submenu_ = sub_menu;
-  if (GTK_IS_ACCEL_GROUP(gtk_accel_group)){
-    sub_menu->UpdateKeys(gtk_accel_group);
-  }
-  if (sub_menu == NULL)
-    gtk_menu_item_remove_submenu(GTK_MENU_ITEM(menu_item_));
-  else
-    gtk_menu_item_set_submenu(GTK_MENU_ITEM(menu_item_), sub_menu->menu_);
-}
-    
-    
-    {
-} // namespace extensions
 
+        
+        
+    {  SnapshotList* list_;                 // just for sanity checks
+};
     
-      auto fn = [&](const uint32_t id) { return table.lookup(id); };
+      // Evict any entry for the specified file number
+  void Evict(uint64_t file_number);
     
-      switch (arr->tag()) {
-    case RepoAuthType::Array::Tag::Packed: {
-      for (uint32_t idx = 0; idx < arr->size(); ++idx) {
-        auto rat = arr->packedElem(idx);
-        assert(rat.resolved());
-        if (rat.mayHaveArrData()) check(rat.array());
-      }
-      break;
-    }
-    case RepoAuthType::Array::Tag::PackedN: {
-      auto rat = arr->elemType();
-      assert(rat.resolved());
-      if (rat.mayHaveArrData()) check(rat.array());
-      break;
-    }
-  }
-    
-    #include <chrono>
-    
-    void HHVM_FUNCTION(xhprof_network_enable) {
-  ServerStats::StartNetworkProfile();
-}
-    
-    //////////////////////////////////////////////////////////////////////
-    
-    namespace HPHP { namespace jit {
-    }
-    }
-    
-    // Sends a copy of the given command to the associated client
-// using the buffer in m_thrift. Returns false if an exception
-// occurs during the send (typically a socket error).
-bool DebuggerProxy::sendToClient(DebuggerCommand *cmd) {
-  TRACE(2, 'DebuggerProxy::sendToClient\n');
-  return cmd->send(m_thrift);
-}
-    
-    //////////////////////////////////////////////////////////////////////
-    
-    namespace HPHP {
-///////////////////////////////////////////////////////////////////////////////
-    }
-    
-    #endif  // STORAGE_LEVELDB_DB_TABLE_CACHE_H_
-
-    
-    
-    {
-    {      default:
-        msg = 'unknown tag';
+          case kNewFile:
+        if (GetLevel(&input, &level) &&
+            GetVarint64(&input, &f.number) &&
+            GetVarint64(&input, &f.file_size) &&
+            GetInternalKey(&input, &f.smallest) &&
+            GetInternalKey(&input, &f.largest)) {
+          new_files_.push_back(std::make_pair(level, f));
+        } else {
+          msg = 'new-file entry';
+        }
         break;
-    }
-  }
     
-    
-    {  FileMetaData() : refs(0), allowed_seeks(1 << 30), file_size(0) { }
-};
-    
-      ~FindFileTest() {
-    for (int i = 0; i < files_.size(); i++) {
-      delete files_[i];
-    }
-  }
-    
-      // The following fields are not protected by any mutex. They are only mutable
-  // while the file is being written, and concurrent access is not allowed
-  // to writable files.
-  std::vector<char*> blocks_;
-  uint64_t size_;
-    
-      ASSERT_OK(DB::Open(options, '/dir/db', &db));
-  for (size_t i = 0; i < 3; ++i) {
-    ASSERT_OK(db->Put(WriteOptions(), keys[i], vals[i]));
-  }
-    
-    #endif  // STORAGE_LEVELDB_TABLE_FILTER_BLOCK_H_
-
-    
-      // Check second filter
-  ASSERT_TRUE(reader.KeyMayMatch(3100, 'box'));
-  ASSERT_TRUE(! reader.KeyMayMatch(3100, 'foo'));
-  ASSERT_TRUE(! reader.KeyMayMatch(3100, 'bar'));
-  ASSERT_TRUE(! reader.KeyMayMatch(3100, 'hello'));
-    
-      // The block handle for the index block of the table
-  const BlockHandle& index_handle() const {
-    return index_handle_;
-  }
-  void set_index_handle(const BlockHandle& h) {
-    index_handle_ = h;
-  }
-    
-    
-    {}  // namespace leveldb
-
-    
-    
-    {void MergingIterator::FindLargest() {
-  IteratorWrapper* largest = NULL;
-  for (int i = n_-1; i >= 0; i--) {
-    IteratorWrapper* child = &children_[i];
-    if (child->Valid()) {
-      if (largest == NULL) {
-        largest = child;
-      } else if (comparator_->Compare(child->key(), largest->key()) > 0) {
-        largest = child;
-      }
-    }
-  }
-  current_ = largest;
+    TEST(FindFileTest, MultipleNullBoundaries) {
+  Add('150', '200');
+  Add('200', '250');
+  Add('300', '350');
+  Add('400', '450');
+  ASSERT_TRUE(! Overlaps(NULL, '149'));
+  ASSERT_TRUE(! Overlaps('451', NULL));
+  ASSERT_TRUE(Overlaps(NULL, NULL));
+  ASSERT_TRUE(Overlaps(NULL, '150'));
+  ASSERT_TRUE(Overlaps(NULL, '199'));
+  ASSERT_TRUE(Overlaps(NULL, '200'));
+  ASSERT_TRUE(Overlaps(NULL, '201'));
+  ASSERT_TRUE(Overlaps(NULL, '400'));
+  ASSERT_TRUE(Overlaps(NULL, '800'));
+  ASSERT_TRUE(Overlaps('100', NULL));
+  ASSERT_TRUE(Overlaps('200', NULL));
+  ASSERT_TRUE(Overlaps('449', NULL));
+  ASSERT_TRUE(Overlaps('450', NULL));
 }
-}  // namespace
-    
-        CALL_TYPE(jobject, Object)
-    CALL_TYPE(jboolean, Boolean)
-    CALL_TYPE(jbyte, Byte)
-    CALL_TYPE(jchar, Char)
-    CALL_TYPE(jshort, Short)
-    CALL_TYPE(jint, Int)
-    CALL_TYPE(jlong, Long)
-    CALL_TYPE(jfloat, Float)
-    CALL_TYPE(jdouble, Double)
     
     
-    {    void toJS(nbind::cbOutput expose) const
-    {
-        expose(unit, value);
-    }
-};
-
-    
-    // convert to alias_ref<T> from T
-template <typename T>
-struct Convert<alias_ref<T>> {
-  typedef JniType<T> jniType;
-  static alias_ref<jniType> fromJni(jniType t) {
-    return wrap_alias(t);
-  }
-  static jniType toJniRet(alias_ref<jniType> t) {
-    return t.get();
-  }
-  static jniType toCall(alias_ref<jniType> t) {
-    return t.get();
-  }
-};
-    
-    namespace facebook {
-namespace lyra {
-    }
+    {      // LevelDB's default cache size is a combined 4 MB
+      std::string WAL_checkpoint = 'PRAGMA wal_autocheckpoint = 4096';
+      status = sqlite3_exec(db_, WAL_stmt.c_str(), NULL, NULL, &err_msg);
+      ExecErrorCheck(status, err_msg);
+      status = sqlite3_exec(db_, WAL_checkpoint.c_str(), NULL, NULL, &err_msg);
+      ExecErrorCheck(status, err_msg);
     }
     
-    static YGSize YGJNIMeasureFunc(
-    YGNodeRef node,
-    float width,
-    YGMeasureMode widthMode,
-    float height,
-    YGMeasureMode heightMode) {
-  if (auto obj = YGNodeJobject(node)->lockLocal()) {
-    static auto measureFunc = findClassStatic('com/facebook/yoga/YogaNode')
-                                  ->getMethod<jlong(jfloat, jint, jfloat, jint)>('measure');
-    }
+        if (num_entries != num_) {
+      char msg[100];
+      snprintf(msg, sizeof(msg), '(%d ops)', num_entries);
+      message_ = msg;
     }
     
-    bool Config::isExperimentalFeatureEnabled(int feature) const
+    class FilterBlockReader {
+ public:
+ // REQUIRES: 'contents' and *policy must stay live while *this is live.
+  FilterBlockReader(const FilterPolicy* policy, const Slice& contents);
+  bool KeyMayMatch(uint64_t block_offset, const Slice& key);
+    }
+    
+    inline BlockHandle::BlockHandle()
+    : offset_(~static_cast<uint64_t>(0)),
+      size_(~static_cast<uint64_t>(0)) {
+}
+    
+      // We might want to use a heap in case there are lots of children.
+  // For now we use a simple array since we expect a very small number
+  // of children in leveldb.
+  const Comparator* comparator_;
+  IteratorWrapper* children_;
+  int n_;
+  IteratorWrapper* current_;
+    
+    #include <string>
+#include <set>
+#include <map>
+    
+    void TSpy::TestFun0()
 {
-    return YGConfigIsExperimentalFeatureEnabled(m_config, static_cast<YGExperimentalFeature>(feature));
+    return reinterpret_cast<Test_Spy_Sample*>(This())->TestFun0();
 }
+    
+    //
+//  testspy_spy.cpp
+//  PublicComponent
+//
+//  Created by yerungui on 14-5-13.
+//
+    
+    // Licensed under the MIT License (the 'License'); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+// http://opensource.org/licenses/MIT
+    
+    #include 'rocksdb/db.h'
+#include 'rocksdb/slice.h'
+#include 'rocksdb/options.h'
+    
+           * Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+       * Redistributions in binary form must reproduce the above
+   copyright notice, this list of conditions and the following disclaimer
+   in the documentation and/or other materials provided with the
+   distribution.
+    
+    
+    {} // namespace rocksdb
 
     
-    class Config {
-    }
+     public: // Insert/Update
+  /// Insert value before/after pivot in (list: key). Return the length.
+  /// May throw RedisListException
+  int InsertBefore(const std::string& key, const std::string& pivot,
+                   const std::string& value);
+  int InsertAfter(const std::string& key, const std::string& pivot,
+                  const std::string& value);
     
-        method(getMinWidth);
-    method(getMinHeight);
-    
-    template<typename... ARGS>
-inline void logd(const char* tag, const char* msg, ARGS... args) noexcept {
-  log(ANDROID_LOG_DEBUG, tag, msg, args...);
+    inline size_t Roundup(size_t x, size_t y) {
+  return ((x + y - 1) / y) * y;
 }
     
-    #pragma once
-#include <cstring>
-#include <string>
-#include <sstream>
-    
-    /**
- * A thread-local object is a 'global' object within a thread. This is useful
- * for writing apartment-threaded code, where nothing is actullay shared
- * between different threads (hence no locking) but those variables are not
- * on stack in local scope. To use it, just do something like this,
- *
- *   ThreadLocal<MyClass> static_object;
- *     static_object->data_ = ...;
- *     static_object->doSomething();
- *
- *   ThreadLocal<int> static_number;
- *     int value = *static_number;
- *
- * So, syntax-wise it's similar to pointers. T can be primitive types, and if
- * it's a class, there has to be a default constructor.
- */
-template<typename T>
-class ThreadLocal {
-public:
-  /**
-   * Constructor that has to be called from a thread-neutral place.
-   */
-  ThreadLocal() :
-    m_key(0),
-    m_cleanup(OnThreadExit) {
-    initialize();
+      if(m_jcallback_obj != nullptr) {    
+    env->DeleteGlobalRef(m_jcallback_obj);
   }
+    
+      StatisticsJni::StatisticsJni(std::shared_ptr<Statistics> stats,
+      const std::set<uint32_t> ignore_histograms) : StatisticsImpl(stats, false),
+      m_ignore_histograms(ignore_histograms) {
+  }
+    
+        Status status;
+    EnvOptions env_opts;
+    std::unique_ptr<WritableFile> log_file;
+    
+      virtual Slice value() const override {
+    assert(Valid());
+    return current_->value();
+  }
+    
+    
+    {private:
+  uint8_t Size = 0;
+  uint8_t Data[kMaxSize];
+};
+    
+    #endif
+
+    
+    
+    {}  // namespace fuzzer
+    
+    
+    {  bool Parse(std::istream &IS, bool ParseCoverage);
+  bool Parse(const std::string &Str, bool ParseCoverage);
+  void ParseOrExit(std::istream &IS, bool ParseCoverage);
+  size_t Merge(std::vector<std::string> *NewFiles);
+};
+    
+    #ifndef LLVM_FUZZER_SHA1_H
+#define LLVM_FUZZER_SHA1_H
+    
+    unsigned NumberOfCpuCores();
+    
+    // This is a reimplementation of Libc's `system()`. On Darwin the Libc
+// implementation contains a mutex which prevents it from being used
+// concurrently. This implementation **can** be used concurrently. It sets the
+// signal handlers when the first thread enters and restores them when the last
+// thread finishes execution of the function and ensures this is not racey by
+// using a mutex.
+int ExecuteCommand(const std::string &Command) {
+  posix_spawnattr_t SpawnAttributes;
+  if (posix_spawnattr_init(&SpawnAttributes))
+    return -1;
+  // Block and ignore signals of the current process when the first thread
+  // enters.
+  {
+    std::lock_guard<std::mutex> Lock(SignalMutex);
+    if (ActiveThreadCount == 0) {
+      static struct sigaction IgnoreSignalAction;
+      sigset_t BlockedSignalsSet;
+      memset(&IgnoreSignalAction, 0, sizeof(IgnoreSignalAction));
+      IgnoreSignalAction.sa_handler = SIG_IGN;
     }
-    
-    
-#define DEFINE_BOXED_PRIMITIVE(LITTLE, BIG)                          \
-  struct J ## BIG : detail::JPrimitive<J ## BIG, j ## LITTLE> {      \
-    static auto constexpr kJavaDescriptor = 'Ljava/lang/' #BIG ';';  \
-    static auto constexpr kValueMethod = #LITTLE 'Value';            \
-    j ## LITTLE LITTLE ## Value() const {                            \
-      return value();                                                \
-    }                                                                \
-  };                                                                 \
-  inline local_ref<jobject> autobox(j ## LITTLE val) {               \
-    return J ## BIG::valueOf(val);                                   \
-  }
+    }
+    }
