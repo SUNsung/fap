39209e163b15d4ea90e342fe7029ce586c4c4407
SUNsung <weatherius@gmail.com>
2018-02-23 09:25:52 +0200

    Rewrite [57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb] File in: 2018-02-23 09:25:52

--- ruby/57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb
+++ ruby/57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb
@@ -1,19 +1,88 @@
-      subdir = parent.join(leaf)
+
+        
+            groups << @user.authorized_groups.visible_to_user(current_user) if current_user
+    groups << @user.authorized_groups.public_to_user(current_user)
     
-            def matches? subject
-          @subject = subject
-          @subject = @subject.new if @subject.class == Class
-          @allowed_types && @rejected_types &&
-          allowed_types_allowed? && rejected_types_rejected?
+          if record.created_at.nil? || record.created_at >= now || record.created_at == record.updated_at
+        yield
+      else
+        record.id = Mastodon::Snowflake.id_at(record.created_at)
+        tries     = 0
+    
+      # Before we load the schema, define the timestamp_id function.
+  # Idiomatically, we might do this in a migration, but then it
+  # wouldn't end up in schema.rb, so we'd need to figure out a way to
+  # get it in before doing db:setup as well. This is simpler, and
+  # ensures it's always in place.
+  Rake::Task['db:schema:load'].enhance ['db:define_timestamp_id']
+    
+      def url
+    if object.needs_redownload?
+      media_proxy_url(object.id, :original)
+    else
+      full_asset_url(object.file.url(:original))
+    end
+  end
+    
+          it 'renders application layout' do
+        get :show, params: { id: 'test', max_id: late.id }
+        expect(response).to render_template layout: 'application'
+      end
+    end
+    
+      def id
+    ActivityPub::TagManager.instance.uri_for(object)
+  end
+    
+    
+  class UserSearch
+    include ActiveModel::Model
+    include ActiveModel::Conversion
+    include ActiveModel::Validations
+    
+          rescue_from OpenIDConnect::ValidationFailed,
+                  ActiveRecord::RecordInvalid, Api::OpenidConnect::Error::InvalidSectorIdentifierUri do |e|
+        validation_fail_as_json(e)
+      end
+    
+          def create
+        req = Rack::Request.new(request.env)
+        if req['client_assertion_type'] == 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
+          handle_jwt_bearer(req)
+        end
+        self.status, headers, self.response_body = Api::OpenidConnect::TokenEndpoint.new.call(request.env)
+        headers.each {|name, value| response.headers[name] = value }
+        nil
+      end
+    
+            def has_column?
+          @subject.column_names.include?('#{@attachment_name}_file_name')
+        end
+      end
+    end
+  end
+end
+
+    
+          class ValidateAttachmentPresenceMatcher
+        def initialize attachment_name
+          @attachment_name = attachment_name
         end
     
-            def matches? subject
-          @subject = subject
-          @subject = @subject.new if @subject.class == Class
-          lower_than_low? && higher_than_low? && lower_than_high? && higher_than_high?
+        Hash.new.tap do |missing_styles|
+      current_styles.each do |klass, attachment_definitions|
+        attachment_definitions.each do |attachment_name, styles|
+          registered = registered_styles[klass][attachment_name] || [] rescue []
+          missed = styles - registered
+          if missed.present?
+            klass_sym = klass.to_s.to_sym
+            missing_styles[klass_sym] ||= Hash.new
+            missing_styles[klass_sym][attachment_name.to_sym] ||= Array.new
+            missing_styles[klass_sym][attachment_name.to_sym].concat(missed.to_a)
+            missing_styles[klass_sym][attachment_name.to_sym].map!(&:to_s).sort!.map!(&:to_sym).uniq!
+          end
         end
-    
-        def load_processor(name)
-      if defined?(Rails.root) && Rails.root
-        filename = '#{name.to_s.underscore}.rb'
-        directories = %w(lib/paperclip lib/paperclip_processors)
+      end
+    end
+  end
+end
