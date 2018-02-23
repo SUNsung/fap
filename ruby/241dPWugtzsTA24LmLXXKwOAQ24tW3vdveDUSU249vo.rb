8c051dd0a7bb2a5f3a2478e12072e9626bfc1773
SUNsung <weatherius@gmail.com>
2018-02-23 17:04:00 +0200

    Rewrite [241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb] File in: 2018-02-23 17:03:59

--- ruby/241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb
+++ ruby/241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb
@@ -1,172 +1,43 @@
 
         
-                stdlibs = detect_stdlibs(ENV.compiler)
-        Tab.create(formula, ENV.compiler, stdlibs.first, formula.build, formula.source_modified_time).write
+              config.paths['log']             = '#{Msf::Config.log_directory}/#{Rails.env}.log'
+      config.paths['config/database'] = [Metasploit::Framework::Database.configurations_pathname.try(:to_path)]
     
-        keys.each do |key|
-      value = env[key]
-      s = '#{key}: #{value}'
-      case key
-      when 'CC', 'CXX', 'LD'
-        s << ' => #{Pathname.new(value).realpath}' if File.symlink?(value)
-      end
-      f.puts s
+    # Copyright (C) 2008 Rapid7, Inc.
+    
+          when :login_fail
+        if(s[:user] and s[:pass])
+          report_auth_info(s.merge({:active => false}))
+          print_status('Failed FTP Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]}')
+    
+          # There is only one pattern per run to test
+      matched = nil
+      matches = nil
+    
+    
+    
+      @src.__NR_execve      = __NR_execve
+  @src.__NR_getpeername = __NR_getpeername
+  @src.__NR_accept      = __NR_accept
+  @src.__NR_listen      = __NR_listen
+  @src.__NR_bind        = __NR_bind
+  @src.__NR_socket      = __NR_socket
+  @src.__NR_connect     = __NR_connect
+  @src.__NR_close       = __NR_close
+  @src.__NR_kfcntl      = __NR_kfcntl
+    
+        # Extracts the Geometry from a 'WxH,O' string
+    # Where W is the width, H is the height,
+    # and O is the EXIF orientation
+    def self.parse(string)
+      GeometryParser.new(string).make
     end
-  end
-end
-
     
-      # True if a {Formula} is being built in 32-bit/x86 mode.
-  # This is needed for some use-cases though we prefer to build Universal
-  # when a 32-bit version is needed.
-  def build_32_bit?
-    include?('32-bit') && option_defined?('32-bit')
-  end
-    
-          plist_filename = if f.plist
-        f.plist_path.basename
-      else
-        File.basename Dir['#{keg}/*.plist'].first
-      end
-      plist_link = '#{destination}/#{plist_filename}'
-      plist_domain = f.plist_path.basename('.plist')
-      destination_path = Pathname.new File.expand_path destination
-      plist_path = destination_path/plist_filename
-    
-          if path.symlink? || path.directory?
-        next
-      elsif path.extname == '.la'
-        path.unlink
-      else
-        # Set permissions for executables and non-executables
-        perms = if path.mach_o_executable? || path.text_executable?
-          0555
-        else
-          0444
+            def allowing *types
+          @allowed_types = types.flatten
+          self
         end
-        if ARGV.debug?
-          old_perms = path.stat.mode & 0777
-          if perms != old_perms
-            puts 'Fixing #{path} permissions from #{old_perms.to_s(8)} to #{perms.to_s(8)}'
-          end
+    
+            def failure_message
+          'Attachment #{@attachment_name} should be required'
         end
-        path.chmod perms
-      end
-    end
-  end
-end
-
-    
-      def llvm
-    @llvm ||= MacOS.llvm_build_version if MacOS.has_apple_developer_tools?
-  end
-    
-        current_revision_var = 'HOMEBREW_UPDATE_AFTER#{repo_var}'
-    @current_revision = ENV[current_revision_var].to_s
-    raise ReporterRevisionUnsetError, current_revision_var if @current_revision.empty?
-  end
-    
-      describe 'instance methods' do
-    let(:key_attributes) do # these keys are intentionally strings.
-      {
-        'canDownload' => false,
-        'canRevoke' => true,
-        'keyId' => 'some-key-id',
-        'keyName' => 'fastlane',
-        'servicesCount' => 3,
-        'services' => [
-          {
-            'name' => 'APNS',
-            'id' => 'U27F4V844T',
-            'configurations' => []
-          },
-          {
-            'name' => 'MusicKit',
-            'id' => '6A7HVUVQ3M',
-            'configurations' => [
-              {
-                'name' => 'music id test',
-                'identifier' => 'music.com.snatchev.test',
-                'type' => 'music',
-                'prefix' => 'some-prefix-id',
-                'id' => 'some-music-kit-id'
-              }
-            ]
-          },
-          {
-            'name' => 'DeviceCheck',
-            'id' => 'DQ8HTZ7739',
-            'configurations' => []
-          }
-        ]
-      }
-    end
-    
-          it 'does set the source directory' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            cloc(source_directory: 'MyCoolApp')
-          end').runner.execute(:test)
-    
-        context 'Mercurial repository' do
-      before do
-        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_svn?).and_return(false)
-        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_git_svn?).and_return(false)
-        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_git?).and_return(false)
-        expect(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_hg?).and_return(true)
-      end
-    
-    map = {}
-dups = []
-    
-        def lotus?
-      defined?(::Lotus)
-    end
-    
-      # Configure static asset server for tests with Cache-Control for performance.
-  if config.respond_to?(:serve_static_files)
-    # rails >= 4.2
-    config.serve_static_files = true
-  elsif config.respond_to?(:serve_static_assets)
-    # rails < 4.2
-    config.serve_static_assets = true
-  end
-  config.static_cache_control = 'public, max-age=3600'
-    
-    Bundler.require(
-    *Rails.groups(
-        coverage: [:test],
-        db: all_environments,
-        pcap: all_environments
-    )
-)
-    
-    msfenv_real_pathname = Pathname.new(__FILE__).realpath
-root = msfenv_real_pathname.parent.parent
-    
-    $stdout.puts res
-
-    
-        end # end of each_key
-  end # end of parse
-end
-    
-                  s[:proto] = 'tcp'
-              s[:name]  = 'pop3'
-              s[:extra] = 'Successful Login. Banner: #{s[:banner]}'
-              report_auth_info(s)
-              print_status('Successful POP3 Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]} (#{s[:banner].strip})')
-    
-      def parse(pkt)
-    # We want to return immediantly if	we do not have a packet which is handled by us
-    return unless pkt.is_tcp?
-    return if (pkt.tcp_sport != 80 and pkt.tcp_dport != 80)
-    s = find_session((pkt.tcp_sport == 80) ? get_session_src(pkt) : get_session_dst(pkt))
-    
-    puts '* System info:'
-    
-    require 'rubygems'
-require 'rjb'
-    
-    keytoolOpts 	= ['-genkey', '-alias', 'signFiles', '-keystore', 'msfkeystore',
-		   '-storepass', 'msfstorepass', '-dname', 'cn=#{certCN}',
-		   '-keypass', 'msfkeypass']
