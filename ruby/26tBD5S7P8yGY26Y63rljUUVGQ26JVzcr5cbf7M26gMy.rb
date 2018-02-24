b9676ee7e597311c0c09d5f874382e382ea18b1d
SUNsung <weatherius@gmail.com>
2018-02-24 07:16:33 +0200

    Rewrite [26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb] File in: 2018-02-24 07:16:33

--- ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
+++ ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
@@ -1,24 +1,69 @@
 
         
-          # any user that is either a moderator or an admin
-  def staff?
-    admin || moderator
-  end
+            it 'should have all of the services' do
+      expect(key).to have_apns
+      expect(key).to have_music_kit
+      expect(key).to have_device_check
+    end
     
-            attr_writer :log_levels
-    
-    (import 'mDNSResponder.sb')
-    
-            private
-    
-            def initialize(argv)
-          @name = argv.shift_argument
-          @template_url = argv.option('template-url', TEMPLATE_REPO)
-          super
-          @additional_args = argv.remainder!
-        end
-    
-            sets = config.sources_manager.aggregate.all_sets
-        sets.each { |set| UI.pod(set, :name_and_version) }
-        UI.puts '\n#{sets.count} pods were found'
+          def self.description
+        'Create a new Keychain'
       end
+    
+        class GradleHelper
+      # Path to the gradle script
+      attr_accessor :gradle_path
+    
+        def has_all_detectable_values?
+      api_key && build_secret && crashlytics_path && emails && schemes
+    end
+  end
+end
+
+    
+          it 'does switch to plain text when xml is toggled off' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            cloc(xml: false)
+          end').runner.execute(:test)
+    
+    module Metasploit
+  module Framework
+    class Application < Rails::Application
+      include Metasploit::Framework::CommonEngine
+    
+    unless ENV['BUNDLE_GEMFILE']
+  require 'pathname'
+    
+    input = ARGV.shift() || usage()
+    
+    #
+# This script extracts the forms from the main page of each
+# web site in a list. The output of this can be used with
+# Metasploit (and other tools) to obtain the saved form data
+# of these domains.
+#
+    
+      def parse(pkt)
+    # We want to return immediatly if we do not have a packet which is handled by us
+    return unless pkt.is_tcp?
+    return if (pkt.tcp_sport != 110 and pkt.tcp_dport != 110)
+    s = find_session((pkt.tcp_sport == 110) ? get_session_src(pkt) : get_session_dst(pkt))
+    
+    
+# extract label addresses
+addrs = {}
+dtrans.each_line { |ln|
+	if ln =~ /;[^ ].*:/
+		parts = ln.split(' ')
+		label = parts[1]
+		label = label.slice(1,label.index(':')-1)
+		addr = parts[0].split(':')[1].to_i(16)
+		#puts '%s => %x' % [label, addr]
+		one = { label => addr }
+		addrs.merge!(one)
+	end
+}
+#puts addrs.inspect
+    
+          def run
+        update_if_necessary!
