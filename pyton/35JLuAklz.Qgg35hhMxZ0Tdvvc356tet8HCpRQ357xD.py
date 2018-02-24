6a5cdd6cf978d63935e71d51ae6294fe97da31a0
SUNsung <weatherius@gmail.com>
2018-02-25 00:14:46 +0200

    Rewrite [35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py] File in: 2018-02-25 00:14:45

--- pyton/35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py
+++ pyton/35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py
@@ -1,101 +1,135 @@
-    def test_create_option_buttons(self):
-        e = self.engine
-        for state in (0, 1):
-            for var in (e.revar, e.casevar, e.wordvar, e.wrapvar):
-                var.set(state)
-            frame, options = self.btn_test_setup(
-                    self.dialog.create_option_buttons)
-            for spec, button in zip (options, frame.pack_slaves()):
-                var, label = spec
-                self.assertEqual(button['text'], label)
-                self.assertEqual(var.get(), state)
+
+        
+        # If true, links to the reST sources are added to the pages.
+#html_show_sourcelink = True
     
-        For this base class, accept any non-blank string.
+    
+def register_cli(app):
+    @app.cli.command('initdb')
+    def initdb_command():
+        '''Creates the database tables.'''
+        init_db()
+        print('Initialized the database.')
+    
+    if __name__ == '__main__':
+    app.run()
+
+    
+    
+def head(url, **kwargs):
+    r'''Sends a HEAD request.
+    
+        def __repr__(self):
+        return str(dict(self.items()))
+    
+    
+class CookieConflictError(RuntimeError):
+    '''There are two cookies that meet the criteria specified in the cookie jar.
+    Use .get and .set and include domain and path args in order to be more specific.
     '''
-    def __init__(self, parent, title, message, *, text0='', used_names={},
-                 _htest=False, _utest=False):
-        '''Create popup, do not return until tk widget destroyed.
     
-        def test_setter(self):
-        M = cookies.Morsel()
-        # tests the .set method to set keys and their values
-        for i in M._reserved:
-            # Makes sure that all reserved keys can't be set this way
-            self.assertRaises(cookies.CookieError,
-                              M.set, i, '%s_value' % i, '%s_value' % i)
-        for i in 'thou cast _the- !holy! ^hand| +*grenade~'.split():
-            # Try typical use case. Setting decent values.
-            # Check output and js_output.
-            M['path'] = '/foo' # Try a reserved key as well
-            M.set(i, '%s_val' % i, '%s_coded_val' % i)
-            self.assertEqual(M.key, i)
-            self.assertEqual(M.value, '%s_val' % i)
-            self.assertEqual(M.coded_value, '%s_coded_val' % i)
-            self.assertEqual(
-                M.output(),
-                'Set-Cookie: %s=%s; Path=/foo' % (i, '%s_coded_val' % i))
-            expected_js_output = '''
-        <script type='text/javascript'>
-        <!-- begin hiding
-        document.cookie = '%s=%s; Path=/foo';
-        // end hiding -->
-        </script>
-        ''' % (i, '%s_coded_val' % i)
-            self.assertEqual(M.js_output(), expected_js_output)
-        for i in ['foo bar', 'foo@bar']:
-            # Try some illegal characters
-            self.assertRaises(cookies.CookieError,
-                              M.set, i, '%s_value' % i, '%s_value' % i)
+    '''
+requests.exceptions
+~~~~~~~~~~~~~~~~~~~
     
-    #
-# NOTE: This is the pi function from the decimal documentation, modified
-# for benchmarking purposes. Since floats do not have a context, the higher
-# intermediate precision from the original is NOT used, so the modified
-# algorithm only gives an approximation to the correctly rounded result.
-# For serious use, refer to the documentation or the appropriate literature.
-#
-def pi_float():
-    '''native float'''
-    lasts, t, s, n, na, d, da = 0, 3.0, 3, 1, 0, 0, 24
-    while s != lasts:
-        lasts = s
-        n, na = n+na, na+8
-        d, da = d+da, da+32
-        t = (t * n) / d
-        s += t
-    return s
+        with server as (host, port):
+        url = 'http://{0}:{1}/'.format(host, port)
+        r = requests.get(url, auth=auth)
+        # Verify server succeeded in authenticating.
+        assert r.status_code == 200
+        # Verify Authorization was sent in final request.
+        assert 'Authorization' in r.request.headers
+        assert r.request.headers['Authorization'].startswith('Digest ')
+        # Verify redirect happened as we expected.
+        assert r.history[0].status_code == 302
+        close_server.set()
     
-        @classmethod
-    def zonelist(cls, zonedir='/usr/share/zoneinfo'):
-        zones = []
-        for root, _, files in os.walk(zonedir):
-            for f in files:
-                p = os.path.join(root, f)
-                with open(p, 'rb') as o:
-                    magic =  o.read(4)
-                if magic == b'TZif':
-                    zones.append(p[len(zonedir) + 1:])
-        return zones
+        scikit_classifier_results = []
+    scikit_regressor_results = []
+    n = 10
+    step = 500
+    start_dim = 500
+    n_classes = 10
     
-        def parse(self, input):
-        options = copy.copy(self._options)
-        options.filter = self.filter
-        options.errorHandler = self.errorHandler
-        fp = input.byteStream
-        if fp is None and options.systemId:
-            import urllib.request
-            fp = urllib.request.urlopen(input.systemId)
-        return self._parse_bytestream(fp, options)
+        url_fmt is along the lines of ('https://github.com/USER/PROJECT/'
+                                   'blob/{revision}/{package}/'
+                                   '{path}#L{lineno}')
+    '''
+    revision = _get_git_revision()
+    return partial(_linkcode_resolve, revision=revision, package=package,
+                   url_fmt=url_fmt)
+
     
-        print('# The following line assumes you have built Python using the standard instructions')
-    print('# Otherwise fix the following line to point to the library.')
-    print('pythonlib = '$(pythonhome)/pcbuild/python%s$(debug_suffix).lib'' % version_suffix)
-    print()
+    import os
+import tarfile
+from contextlib import closing
     
-        def handle_xmlrpc(self, request_text):
-        '''Handle a single XML-RPC request'''
+    # Print the classification report
+print(metrics.classification_report(y_test, y_predicted,
+                                    target_names=dataset.target_names))
     
-            seq = list(range(a, b, c))
-        self.assertIn(a, seq)
-        self.assertNotIn(b, seq)
-        self.assertEqual(len(seq), 2)
+            C = float(self.complexity.get())
+        gamma = float(self.gamma.get())
+        coef0 = float(self.coef0.get())
+        degree = int(self.degree.get())
+        kernel_map = {0: 'linear', 1: 'rbf', 2: 'poly'}
+        if len(np.unique(y)) == 1:
+            clf = svm.OneClassSVM(kernel=kernel_map[self.kernel.get()],
+                                  gamma=gamma, coef0=coef0, degree=degree)
+            clf.fit(X)
+        else:
+            clf = svm.SVC(kernel=kernel_map[self.kernel.get()], C=C,
+                          gamma=gamma, coef0=coef0, degree=degree)
+            clf.fit(X, y)
+        if hasattr(clf, 'score'):
+            print('Accuracy:', clf.score(X, y) * 100)
+        X1, X2, Z = self.decision_surface(clf)
+        self.model.clf = clf
+        self.model.set_surface((X1, X2, Z))
+        self.model.surface_type = self.surface_type.get()
+        self.fitted = True
+        self.model.changed('surface')
+    
+    from sklearn import datasets
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (brier_score_loss, precision_score, recall_score,
+                             f1_score)
+from sklearn.calibration import CalibratedClassifierCV, calibration_curve
+from sklearn.model_selection import train_test_split
+    
+    The base classifier is a random forest classifier with 25 base estimators
+(trees). If this classifier is trained on all 800 training datapoints, it is
+overly confident in its predictions and thus incurs a large log-loss.
+Calibrating an identical classifier, which was trained on 600 datapoints, with
+method='sigmoid' on the remaining 200 datapoints reduces the confidence of the
+predictions, i.e., moves the probability vectors from the edges of the simplex
+towards the center. This calibration results in a lower log-loss. Note that an
+alternative would have been to increase the number of base estimators which
+would have resulted in a similar decrease in log-loss.
+'''
+print(__doc__)
+    
+    plt.title('Clustering measures for 2 random uniform labelings\n'
+          'with equal number of clusters')
+plt.xlabel('Number of clusters (Number of samples is fixed to %d)' % n_samples)
+plt.ylabel('Score value')
+plt.legend(plots, names)
+plt.ylim(ymin=-0.05, ymax=1.05)
+    
+        strings = []
+    with open(filepath, 'r') as f:
+    
+            with open(self.log_path, 'r') as fd:
+            content = fd.read()
+            return content
+    
+    ## Anything on different channel than DEFAULT_CHANNEL is not parsed
+# by parser.
+HIDDEN_CHANNEL = 99
+    
+            Terence implemented packed table initializers, because Java has a
+        size restriction on .class files and the lookup tables can grow
+        pretty large. The generated JavaLexer.java of the Java.g example
+        would be about 15MB with uncompressed array initializers.
