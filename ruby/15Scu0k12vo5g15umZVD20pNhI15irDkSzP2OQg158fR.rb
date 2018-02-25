dbdf54d27493763281ee590c46f9cdd7d614dacd
SUNsung <weatherius@gmail.com>
2018-02-25 19:56:53 +0200

    Rewrite [15Scu0k12vo5g15umZVD20pNhI15irDkSzP2OQg158fR.rb] File in: 2018-02-25 19:56:52

--- ruby/15Scu0k12vo5g15umZVD20pNhI15irDkSzP2OQg158fR.rb
+++ ruby/15Scu0k12vo5g15umZVD20pNhI15irDkSzP2OQg158fR.rb
@@ -1,86 +1,91 @@
 
         
-          module ClassMethods
+            sidekiq_options queue: 'critical'
+    
+      module ClassMethods
     def load_types_in(module_name, my_name = module_name.singularize)
       const_set(:MODULE_NAME, module_name)
       const_set(:BASE_CLASS_NAME, my_name)
       const_set(:TYPES, Dir[Rails.root.join('app', 'models', module_name.underscore, '*.rb')].map { |path| module_name + '::' + File.basename(path, '.rb').camelize })
     end
     
-      def present(payload)
-    if payload.is_a?(Hash)
-      payload = ActiveSupport::HashWithIndifferentAccess.new(payload)
-      MAIN_KEYS.each do |key|
-        return { :title => payload[key].to_s, :entries => present_hash(payload, key) } if payload.has_key?(key)
+          if options[:type] == :array && (options[:values].blank? || !options[:values].is_a?(Array))
+        raise ArgumentError.new('When using :array as :type you need to provide the :values as an Array')
       end
     
-      def complete_option(method)
-    if self.respond_to? 'complete_#{method}'.to_sym
-      self.send('complete_#{method}'.to_sym)
+      def table_sort
+    raise('You must call set_table_sort in any action using table_sort.') unless @table_sort_info.present?
+    @table_sort_info[:order]
+  end
+    
+        respond_to do |format|
+      format.html { render layout: !request.xhr? }
+      format.json { render json: @jobs }
     end
   end
     
-        @user_credentials = current_user.user_credentials.reorder(table_sort).page(params[:page])
+    invalids = []
+Parallel.each(links, in_threads: 4) do |link|
+  href = link.attribute('href').to_s
+  begin
+    case check_link(URI.join(BASE_URI, href))
+    when (200...300)
+      putc('.')
+    when (300..302)
+      putc('w')
+    end
+  rescue => e
+    putc('F')
+    invalids << '#{href} (reason: #{e.message})'
+  end
+end
     
-            # Defines a capability for the given guest. The block should return
-        # a class/module that has a method with the capability name, ready
-        # to be executed. This means that if it is an instance method,
-        # the block should return an instance of the class.
-        #
-        # @param [String] guest The name of the guest
-        # @param [String] cap The name of the capability
-        def self.guest_capability(guest, cap, &block)
-          components.guest_capabilities[guest.to_sym].register(cap.to_sym, &block)
-          nil
-        end
+      it 'ignores underscores in between the digits' do
+    '1_2_3asdf'.to_i.should == 123
+  end
     
-                  # An IO::WaitReadable means there may be more IO but this
-              # IO object is not ready to be read from yet. No problem,
-              # we read as much as we can, so we break.
-              breakable = true
-            elsif e.is_a?(Errno::EAGAIN)
-              # Otherwise, we just look for the EAGAIN error which should be
-              # all that IO::WaitReadable does in Ruby 1.9.
-              breakable = true
-            end
+      it 'translates chars not in from_string when it starts with a ^' do
+    'hello'.tr_s('^aeiou', '*').should == '*e*o'
+    '123456789'.tr_s('^345', 'abc').should == 'c345c'
+    'abcdefghijk'.tr_s('^d-g', '9131').should == '1defg1'
     
-          # This inserts a block with the given key and value.
-      #
-      # @param [String] key
-      # @param [String] value
-      def insert(key, value)
-        # Insert the new block into the value
-        new_block = <<BLOCK
-# VAGRANT-BEGIN: #{key}
-#{value.strip}
-# VAGRANT-END: #{key}
-BLOCK
+      it 'translates chars not in from_string when it starts with a ^' do
+    'hello'.tr('^aeiou', '*').should == '*e**o'
+    '123456789'.tr('^345', 'abc').should == 'cc345cccc'
+    'abcdefghijk'.tr('^d-g', '9131').should == '111defg1111'
     
-            @template_root = data.delete(:template_root)
-        @template_root ||= Vagrant.source_root.join('templates')
-        @template_root = Pathname.new(@template_root)
-    
-          respond_with do |format|
-        format.html do
-          gon.preloads[:pods] = pods_json
-          gon.unchecked_count = Pod.unchecked.count
-          gon.version_failed_count = Pod.version_failed.count
-          gon.error_count = Pod.check_failed.count
-    
-          def http_error_page_as_json(e)
-        render json: {error: :invalid_request, error_description: e.message}, status: 400
+            origin.should_not equal(dynamic)
+        (-origin).should equal(-dynamic)
       end
     
-        it 'redirects requests with duplicate session cookies' do
-      get '/', {}, 'HTTP_COOKIE' => 'rack.session=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
-      expect(last_response).to be_redirect
-      expect(last_response.location).to eq('/')
-    end
+        def process_directory
+      @options[:input] = @args.shift
+      unless @options[:input]
+        raise 'Error: directory required when using --recursive.'
+      end
     
-        { # yes, this is ugly, feel free to change that
-      '/..' => '/', '/a/../b' => '/b', '/a/../b/' => '/b/', '/a/.' => '/a/',
-      '/%2e.' => '/', '/a/%2E%2e/b' => '/b', '/a%2f%2E%2e%2Fb/' => '/b/',
-      '//' => '/', '/%2fetc%2Fpasswd' => '/etc/passwd'
-    }.each do |a, b|
-      it('replaces #{a.inspect} with #{b.inspect}') { expect(get(a).body).to eq(b) }
+          opts.on('--cache-location PATH',
+              'The path to save parsed Sass files. Defaults to .sass-cache.') do |loc|
+        @options[:for_engine][:cache_location] = loc
+      end
+    
+            def define_logger(name, options = {})
+          class_eval <<-RUBY, __FILE__, __LINE__ + 1
+            def #{name}(message)
+              #{options.fetch(:to, :log)}(#{name.inspect}, message)
+            end
+          RUBY
+        end
+      end
+    end
+  end
+end
+
+    
+        # Returns the Sass/SCSS code for the media query list.
+    #
+    # @param options [{Symbol => Object}] An options hash (see {Sass::CSS#initialize}).
+    # @return [String]
+    def to_src(options)
+      queries.map {|q| q.to_src(options)}.join(', ')
     end
