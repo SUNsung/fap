bc9f6d304030899d0bdfb91d7809c125767ba58b
SUNsung <weatherius@gmail.com>
2018-02-25 21:02:45 +0200

    Rewrite [520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp] File in: 2018-02-25 21:02:45

--- cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
+++ cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
@@ -1,138 +1,286 @@
 
         
-            size_t i = 0;      // Points to the next leaf node.
-    size_t j = n + 1;  // Points to the next non-leaf node.
-    for (size_t k = n - 1; k != 0; --k) {
-      size_t left, right;
-      if (tree[i].total_count_ <= tree[j].total_count_) {
-        left = i;
-        ++i;
-      } else {
-        left = j;
-        ++j;
-      }
-      if (tree[i].total_count_ <= tree[j].total_count_) {
-        right = i;
-        ++i;
-      } else {
-        right = j;
-        ++j;
-      }
+        template <typename T1, typename T2, typename T3, typename T4>
+internal::ValueArray4<T1, T2, T3, T4> Values(T1 v1, T2 v2, T3 v3, T4 v4) {
+  return internal::ValueArray4<T1, T2, T3, T4>(v1, v2, v3, v4);
+}
+    
+    #if !GTEST_OS_SYMBIAN
+# include <utility>
+#endif
+    
+    // Prints the given number of elements in an array, without printing
+// the curly braces.
+template <typename T>
+void PrintRawArrayTo(const T a[], size_t count, ::std::ostream* os) {
+  UniversalPrint(a[0], os);
+  for (size_t i = 1; i != count; i++) {
+    *os << ', ';
+    UniversalPrint(a[i], os);
+  }
+}
+    
+    #endif  // GTEST_INCLUDE_GTEST_GTEST_SPI_H_
+
+    
+    // Helper function for implementing {EXPECT|ASSERT}_PRED2.  Don't use
+// this in your code.
+template <typename Pred,
+          typename T1,
+          typename T2>
+AssertionResult AssertPred2Helper(const char* pred_text,
+                                  const char* e1,
+                                  const char* e2,
+                                  Pred pred,
+                                  const T1& v1,
+                                  const T2& v2) {
+  if (pred(v1, v2)) return AssertionSuccess();
     }
     
-    namespace guetzli {
+      // We use a const char* instead of an std::string, as Google Test used to be
+  // used where std::string is not available.  TODO(wan@google.com): change to
+  // std::string.
+  const char* pattern_;
+  bool is_valid_;
+    
+    template <>
+class Get<2> {
+ public:
+  template <class Tuple>
+  static GTEST_ADD_REF_(GTEST_TUPLE_ELEMENT_(2, Tuple))
+  Field(Tuple& t) { return t.f2_; }  // NOLINT
     }
     
-    // Fills in 'result' with the inverse DCT of 'block'.
-// The arguments 'block' and 'result' point to 8x8 arrays that are arranged in
-// a row-by-row memory layout.
-void ComputeBlockIDCT(const coeff_t* block, uint8_t* result);
-    
-      // Sets the 8x8 coefficient block with block coordinates (block_x, block_y)
-  // to block[].
-  // NOTE: If the component is 2x2 subsampled, this corresponds to the 16x16
-  // pixel area with upper-left corner (16 * block_x, 16 * block_y).
-  // REQUIRES: block[k] % quant()[k] == 0 for each coefficient index k.
-  void SetCoeffBlock(int block_x, int block_y,
-                     const coeff_t block[kDCTBlockSize]);
+      // Returns true iff n is a prime number.
+  virtual bool IsPrime(int n) const = 0;
     
     
-    {    tls_stackSize = 1;
+    {  EXPECT_FALSE(IsPrime(-1));
+  EXPECT_FALSE(IsPrime(-2));
+  EXPECT_FALSE(IsPrime(INT_MIN));
+}
+    
+    // If the max-min top of a unicharset char is bigger than kMaxCharTopRange
+// then the char top cannot be used to judge misfits or suggest a new top.
+const int kMaxCharTopRange = 48;
+    
+    extern BLOCK_LIST *current_block_list;
+extern STRING_VAR_H (editor_image_win_name, 'EditorImage',
+'Editor image window name');
+extern INT_VAR_H (editor_image_xpos, 590, 'Editor image X Pos');
+extern INT_VAR_H (editor_image_ypos, 10, 'Editor image Y Pos');
+extern INT_VAR_H (editor_image_height, 680, 'Editor image height');
+extern INT_VAR_H (editor_image_width, 655, 'Editor image width');
+extern INT_VAR_H (editor_image_word_bb_color, BLUE,
+'Word bounding box colour');
+extern INT_VAR_H (editor_image_blob_bb_color, YELLOW,
+'Blob bounding box colour');
+extern INT_VAR_H (editor_image_text_color, WHITE, 'Correct text colour');
+extern STRING_VAR_H (editor_dbwin_name, 'EditorDBWin',
+'Editor debug window name');
+extern INT_VAR_H (editor_dbwin_xpos, 50, 'Editor debug window X Pos');
+extern INT_VAR_H (editor_dbwin_ypos, 500, 'Editor debug window Y Pos');
+extern INT_VAR_H (editor_dbwin_height, 24, 'Editor debug window height');
+extern INT_VAR_H (editor_dbwin_width, 80, 'Editor debug window width');
+extern STRING_VAR_H (editor_word_name, 'BlnWords',
+'BL normalised word window');
+extern INT_VAR_H (editor_word_xpos, 60, 'Word window X Pos');
+extern INT_VAR_H (editor_word_ypos, 510, 'Word window Y Pos');
+extern INT_VAR_H (editor_word_height, 240, 'Word window height');
+extern INT_VAR_H (editor_word_width, 655, 'Word window width');
+extern double_VAR_H (editor_smd_scale_factor, 1.0, 'Scaling for smd image');
+    
+      // Compute the number of unichars in the label.
+  GenericVector<UNICHAR_ID> encoding;
+  if (!unicharset.encode_string(label, true, &encoding, NULL, NULL)) {
+    tprintf('Not outputting illegal unichar %s\n', label);
     return;
   }
-  SCOPE_EXIT { pthread_attr_destroy(&attr); };
     
-    void DeterministicSchedule::afterSharedAccess() {
-  auto sched = tls_sched;
-  if (!sched) {
-    return;
-  }
-  sem_post(sched->sems_[sched->scheduler_(sched->sems_.size())]);
-}
-    
-    namespace folly {
-namespace parking_lot_detail {
+      // Step two: Try to split off bits of the word that are both outliers
+  //           and have much lower certainty than average
+  // Calculate num_leading and leading_certainty.
+  for (*leading_certainty = 0.0f, *num_rebuilt_leading = 0;
+       *num_rebuilt_leading < leading_outliers;
+       (*num_rebuilt_leading)++) {
+    float char_certainty = word->best_choice->certainty(*num_rebuilt_leading);
+    if (char_certainty > *unlikely_threshold) {
+      break;
     }
+    if (char_certainty < *leading_certainty) {
+      *leading_certainty = char_certainty;
     }
-    
-      FOLLY_ALWAYS_INLINE Weight getCapacity() const noexcept {
-    return capacity_.load(std::memory_order_acquire);
   }
     
-    TEST_F(UtilityTest, copy) {
-  struct MyData {};
-  struct Worker {
-    size_t rrefs = 0, crefs = 0;
-    void something(MyData&&) {
-      ++rrefs;
-    }
-    void something(const MyData&) {
-      ++crefs;
-    }
-  };
-    }
+    #include 'ccstruct.h'
     
-    #if _POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600 || \
-    (defined(__ANDROID__) && (__ANDROID_API__ > 15)) ||   \
-    (defined(__APPLE__) &&                                \
-     (__MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_6 ||    \
-      __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_3_0))
+      // Transforms the given coords one step forward to normalized space, without
+  // using any block rotation or predecessor.
+  void LocalNormTransform(const TPOINT& pt, TPOINT* transformed) const;
+  void LocalNormTransform(const FCOORD& pt, FCOORD* transformed) const;
+  // Transforms the given coords forward to normalized space using the
+  // full transformation sequence defined by the block rotation, the
+  // predecessors, deepest first, and finally this. If first_norm is not NULL,
+  // then the first and deepest transformation used is first_norm, ending
+  // with this, and the block rotation will not be applied.
+  void NormTransform(const DENORM* first_norm, const TPOINT& pt,
+                     TPOINT* transformed) const;
+  void NormTransform(const DENORM* first_norm, const FCOORD& pt,
+                     FCOORD* transformed) const;
+  // Transforms the given coords one step back to source space, without
+  // using to any block rotation or predecessor.
+  void LocalDenormTransform(const TPOINT& pt, TPOINT* original) const;
+  void LocalDenormTransform(const FCOORD& pt, FCOORD* original) const;
+  // Transforms the given coords all the way back to source image space using
+  // the full transformation sequence defined by this and its predecessors
+  // recursively, shallowest first, and finally any block re_rotation.
+  // If last_denorm is not NULL, then the last transformation used will
+  // be last_denorm, and the block re_rotation will never be executed.
+  void DenormTransform(const DENORM* last_denorm, const TPOINT& pt,
+                       TPOINT* original) const;
+  void DenormTransform(const DENORM* last_denorm, const FCOORD& pt,
+                       FCOORD* original) const;
     
-    CacheLocality CacheLocality::readFromSysfs() {
-  return readFromSysfsTree([](std::string name) {
-    std::ifstream xi(name.c_str());
-    std::string rv;
-    std::getline(xi, rv);
-    return rv;
-  });
+    TEST(LogTest, ReadFourthStart) {
+  CheckInitialOffsetRecord(
+      2 * (kHeaderSize + 1000) + (2 * log::kBlockSize - 1000) + 3 * kHeaderSize,
+      3);
 }
     
-    
-    {      ready++;
-      while (!go.load()) {
-        std::this_thread::yield();
-      }
-      std::atomic<int> localWork(0);
-      for (size_t i = iters; i > 0; --i) {
-        ++*(counters[AccessSpreader<Tag>::current(stripes)]);
-        for (size_t j = work; j > 0; --j) {
-          localWork.load();
-        }
-      }
-    }));
-    
-    inline const folly::dynamic& DynamicParser::ParserStack::value() const{
-  if (!value_) {
-    throw DynamicParserLogicError(
-      'Parsing nullptr, or parsing after releaseErrors()'
-    );
-  }
-  return *value_;
+    TEST(FindFileTest, MultipleNullBoundaries) {
+  Add('150', '200');
+  Add('200', '250');
+  Add('300', '350');
+  Add('400', '450');
+  ASSERT_TRUE(! Overlaps(NULL, '149'));
+  ASSERT_TRUE(! Overlaps('451', NULL));
+  ASSERT_TRUE(Overlaps(NULL, NULL));
+  ASSERT_TRUE(Overlaps(NULL, '150'));
+  ASSERT_TRUE(Overlaps(NULL, '199'));
+  ASSERT_TRUE(Overlaps(NULL, '200'));
+  ASSERT_TRUE(Overlaps(NULL, '201'));
+  ASSERT_TRUE(Overlaps(NULL, '400'));
+  ASSERT_TRUE(Overlaps(NULL, '800'));
+  ASSERT_TRUE(Overlaps('100', NULL));
+  ASSERT_TRUE(Overlaps('200', NULL));
+  ASSERT_TRUE(Overlaps('449', NULL));
+  ASSERT_TRUE(Overlaps('450', NULL));
 }
     
-    std::unique_ptr<AuthConfig>
-AbstractAuthResolver::getUserDefinedAuthConfig() const
-{
-  return AuthConfig::create(userDefinedUser_, userDefinedPassword_);
-}
+    // WriteBatchInternal provides static methods for manipulating a
+// WriteBatch that we don't want in the public WriteBatch interface.
+class WriteBatchInternal {
+ public:
+  // Return the number of entries in the batch.
+  static int Count(const WriteBatch* batch);
+    }
     
-    public:
-  AbstractProxyResponseCommand(
-      cuid_t cuid, const std::shared_ptr<Request>& req,
-      const std::shared_ptr<FileEntry>& fileEntry, RequestGroup* requestGroup,
-      const std::shared_ptr<HttpConnection>& httpConnection, DownloadEngine* e,
-      const std::shared_ptr<SocketCore>& s);
+    class Issue178 { };
     
-      void setBtRuntime(const std::shared_ptr<BtRuntime>& btRuntime);
+      iter->Seek('5');
+  ASSERT_EQ(iter->key().ToString(), '5');
+  iter->Prev();
+  ASSERT_EQ(iter->key().ToString(), '4');
+  iter->Prev();
+  ASSERT_EQ(iter->key().ToString(), '3');
+  iter->Next();
+  ASSERT_EQ(iter->key().ToString(), '4');
+  iter->Next();
+  ASSERT_EQ(iter->key().ToString(), '5');
     
     
-    {  virtual std::unique_ptr<DiskWriter>
-  newDiskWriter(const std::string& filename) CXX11_OVERRIDE
-  {
-    return make_unique<DiskWriterType>();
+    { private:
+  /*! \brief the underlying stream */
+  dmlc::Stream *stream_;
+  /*! \brief buffer to hold data */
+  std::string buffer_;
+  /*! \brief length of valid data in buffer */
+  size_t read_len_;
+  /*! \brief pointer in the buffer */
+  size_t read_ptr_;
+};
+    
+    // This is a helpful data structure to define parameters
+// You do not have to use it.
+// see http://dmlc-core.readthedocs.org/en/latest/parameter.html
+// for introduction of this module.
+struct MyLogisticParam : public dmlc::Parameter<MyLogisticParam> {
+  float scale_neg_weight;
+  // declare parameters
+  DMLC_DECLARE_PARAMETER(MyLogisticParam) {
+    DMLC_DECLARE_FIELD(scale_neg_weight).set_default(1.0f).set_lower_bound(0.0f)
+        .describe('Scale the weight of negative examples by this factor');
   }
 };
     
-    #include <vector>
-#include <string>
-#include <memory>
+        FILE* stream = popen('logcat -d -v time -s 'DEBUG:I'', 'r');    // 将“ls －l”命令的输出 通过管道读取（“r”参数）到FILE* stream
+    
+    #ifdef ANDROID
+#include 'comm/android/callstack.h'
+#endif
+    
+            __FirstGetCreater(T::ServiceName());
+    
+      private:
+    virtual void __OnAttach(const char* _key) = 0;
+    virtual void __OnDetach(const char* _key) = 0;
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+    jbyteArray JNU_Buffer2JbyteArray(JNIEnv* _env, const AutoBuffer& ab);
+jbyteArray JNU_Buffer2JbyteArray(JNIEnv* _env, const void* _buffer, size_t _length);
+void JNU_FreeJbyteArray(JNIEnv* _env, jbyteArray bytes);
+    
+    ScopeJEnv::ScopeJEnv(JavaVM* jvm, jint _capacity)
+    : vm_(jvm), env_(NULL), we_attach_(false), status_(0) {
+    ASSERT(jvm);
+    do {
+        env_ = (JNIEnv*)pthread_getspecific(g_env_key);
+        
+        if (NULL != env_) {
+            break;
+        }
+        
+        status_ = vm_->GetEnv((void**) &env_, JNI_VERSION_1_6);
+    }
+    }
+    
+      std::unique_ptr<AuthConfig> getUserDefinedAuthConfig() const;
+    
+      virtual bool getChangeOption() const CXX11_OVERRIDE;
+    
+    
+    {} // namespace aria2
+
+    
+    bool AdaptiveFileAllocationIterator::finished()
+{
+  if (!allocator_) {
+    return offset_ == totalLength_;
+  }
+  else {
+    return allocator_->finished();
+  }
+}
+    
+      /**
+   * The internal announce URL pointer points to next URL.
+   * If the current URL is the last element of its group, then the first
+   * element of the next group is pointed.
+   */
+  void announceFailure();
+    
+    AnnounceTier::~AnnounceTier() = default;
+    
+    #endif // D_ANNOUNCE_TIER_H
+
+    
+    std::ostream& operator<<(std::ostream& o,
+                         const std::shared_ptr<AuthConfig>& authConfig)
+{
+  o << authConfig->getAuthText();
+  return o;
+}
