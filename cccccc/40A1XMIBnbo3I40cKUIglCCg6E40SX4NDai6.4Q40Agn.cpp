6ca0540c278dca32eaa2e44c72c46cb508b3be68
SUNsung <weatherius@gmail.com>
2018-02-25 10:58:32 +0200

    Rewrite [40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp] File in: 2018-02-25 10:58:31

--- cccccc/40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp
+++ cccccc/40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp
@@ -1,316 +1,108 @@
 
         
-        namespace swift {
+          /// Emit a partial application of an Objective-C method to its 'self'
+  /// argument.
+  void emitObjCPartialApplication(IRGenFunction &IGF,
+                                  ObjCMethod method,
+                                  CanSILFunctionType origType,
+                                  CanSILFunctionType partialAppliedType,
+                                  llvm::Value *self,
+                                  SILType selfType,
+                                  Explosion &out);
+    
+    
+    {  static bool classof(const MarkupASTNode *N) {
+    return N->getKind() == ASTNodeKind::CodeBlock;
+  }
+};
+    
+    struct IndexRelation {
+  const Decl *decl;
+  SymbolInfo symInfo;
+  SymbolRoleSet roles = SymbolRoleSet(0);
     }
     
-    #pragma mark - NSNumber verification
+    namespace testing {
+    }
     
-    void JRodriguesMatlab(const Mat& src, Mat& dst);
+    // This helper class is used by {ASSERT|EXPECT}_NO_FATAL_FAILURE to check if a
+// statement generates new fatal failures. To do so it registers itself as the
+// current test part result reporter. Besides checking if fatal failures were
+// reported, it only delegates the reporting to the former result reporter.
+// The original result reporter is restored in the destructor.
+// INTERNAL IMPLEMENTATION - DO NOT USE IN A USER PROGRAM.
+class GTEST_API_ HasNewFatalFailureHelper
+    : public TestPartResultReporterInterface {
+ public:
+  HasNewFatalFailureHelper();
+  virtual ~HasNewFatalFailureHelper();
+  virtual void ReportTestPartResult(const TestPartResult& result);
+  bool has_new_fatal_failure() const { return has_new_fatal_failure_; }
+ private:
+  bool has_new_fatal_failure_;
+  TestPartResultReporterInterface* original_reporter_;
+    }
     
-            if(black_contours.size() < 5 || white_contours.size() < 5) continue;
+     private:
+  class Iterator : public ParamIteratorInterface<ParamType> {
+   public:
+    Iterator(const ParamGeneratorInterface<ParamType>* base,
+      const ParamGenerator<T1>& g1,
+      const typename ParamGenerator<T1>::iterator& current1,
+      const ParamGenerator<T2>& g2,
+      const typename ParamGenerator<T2>::iterator& current2,
+      const ParamGenerator<T3>& g3,
+      const typename ParamGenerator<T3>::iterator& current3,
+      const ParamGenerator<T4>& g4,
+      const typename ParamGenerator<T4>::iterator& current4,
+      const ParamGenerator<T5>& g5,
+      const typename ParamGenerator<T5>::iterator& current5)
+        : base_(base),
+          begin1_(g1.begin()), end1_(g1.end()), current1_(current1),
+          begin2_(g2.begin()), end2_(g2.end()), current2_(current2),
+          begin3_(g3.begin()), end3_(g3.end()), current3_(current3),
+          begin4_(g4.begin()), end4_(g4.end()), current4_(current4),
+          begin5_(g5.begin()), end5_(g5.end()), current5_(current5)    {
+      ComputeCurrentValue();
+    }
+    virtual ~Iterator() {}
+    }
     
-            static __device__ __forceinline__ int syncthreadsOr(int pred)
-        {
-#if defined (__CUDA_ARCH__) && (__CUDA_ARCH__ < 200)
-                // just campilation stab
-                return 0;
-#else
-                return __syncthreads_or(pred);
-#endif
-        }
+    // Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
+int Factorial(int n) {
+  int result = 1;
+  for (int i = 1; i <= n; i++) {
+    result *= i;
+  }
+    }
     
-        static void CODEGEN_FUNCPTR Switch_BindRenderbuffer(GLenum target, GLuint renderbuffer)
+    
+    {    // You can generate a failure in any event handler except
+    // OnTestPartResult. Just use an appropriate Google Test assertion to do
+    // it.
+    EXPECT_LE(difference, 0) << 'Leaked ' << difference << ' unit(s) of Water!';
+  }
+    
+    #if COCOS2D_DEBUG >= 1
+    if (!cobj) 
     {
-        BindRenderbuffer = (PFNBINDRENDERBUFFERPROC)IntGetProcAddress('glBindRenderbuffer');
-        BindRenderbuffer(target, renderbuffer);
+        tolua_error(tolua_S,'invalid 'cobj' in function 'lua_cocos2dx_physics_PhysicsShapeEdgePolygon_getPointsCount'', nullptr);
+        return 0;
     }
-    
-    #ifndef GL_ARB_shader_objects
-    // GL types for program/shader text and shader object handles
-    typedef char GLcharARB;
-    typedef unsigned int GLhandleARB;
 #endif
     
-    #ifdef HAVE_LAPACK
     
-    static const int kBlockSize = 32768;
-    
-    void FilterBlockBuilder::StartBlock(uint64_t block_offset) {
-  uint64_t filter_index = (block_offset / kFilterBase);
-  assert(filter_index >= filter_offsets_.size());
-  while (filter_index > filter_offsets_.size()) {
-    GenerateFilter();
-  }
+    {    delete[] glVertices;
 }
     
-    Status TableCache::Get(const ReadOptions& options,
-                       uint64_t file_number,
-                       uint64_t file_size,
-                       const Slice& k,
-                       void* arg,
-                       void (*saver)(void*, const Slice&, const Slice&)) {
-  Cache::Handle* handle = NULL;
-  Status s = FindTable(file_number, file_size, &handle);
-  if (s.ok()) {
-    Table* t = reinterpret_cast<TableAndFile*>(cache_->Value(handle))->table;
-    s = t->InternalGet(options, k, arg, saver);
-    cache_->Release(handle);
-  }
-  return s;
-}
-    
-    #include <set>
-#include <utility>
-#include <vector>
-#include 'db/dbformat.h'
-    
-      // The following fields are not protected by any mutex. They are only mutable
-  // while the file is being written, and concurrent access is not allowed
-  // to writable files.
-  std::vector<char*> blocks_;
-  uint64_t size_;
-    
-    Block::Block(const BlockContents& contents)
-    : data_(contents.data.data()),
-      size_(contents.data.size()),
-      owned_(contents.heap_allocated) {
-  if (size_ < sizeof(uint32_t)) {
-    size_ = 0;  // Error marker
-  } else {
-    size_t max_restarts_allowed = (size_-sizeof(uint32_t)) / sizeof(uint32_t);
-    if (NumRestarts() > max_restarts_allowed) {
-      // The size is too small for NumRestarts()
-      size_ = 0;
-    } else {
-      restart_offset_ = size_ - (1 + NumRestarts()) * sizeof(uint32_t);
-    }
-  }
-}
+        virtual void DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color);
     
     
-    {  virtual bool KeyMayMatch(const Slice& key, const Slice& filter) const {
-    uint32_t h = Hash(key.data(), key.size(), 1);
-    for (size_t i = 0; i + 4 <= filter.size(); i += 4) {
-      if (h == DecodeFixed32(filter.data() + i)) {
-        return true;
-      }
-    }
-    return false;
-  }
-};
+    {
+    {				prevBody = body;
+			}
+		}
     
-    void Iterator::RegisterCleanup(CleanupFunction func, void* arg1, void* arg2) {
-  assert(func != NULL);
-  Cleanup* c;
-  if (cleanup_.function == NULL) {
-    c = &cleanup_;
-  } else {
-    c = new Cleanup;
-    c->next = cleanup_.next;
-    cleanup_.next = c;
-  }
-  c->function = func;
-  c->arg1 = arg1;
-  c->arg2 = arg2;
-}
-    
-        jobject     (*AllocObject)(JNIEnv*, jclass);
-    jobject     (*NewObject)(JNIEnv*, jclass, jmethodID, ...);
-    jobject     (*NewObjectV)(JNIEnv*, jclass, jmethodID, va_list);
-    jobject     (*NewObjectA)(JNIEnv*, jclass, jmethodID, jvalue*);
-    
-      auto i = 0;
-  out << 'Backtrace:\n';
-  for (auto& elm : trace) {
-    out << '    #' << dec << setfill('0') << setw(2) << i++ << ' ' << elm << '\n';
-  }
-    
-    TEST_F(YogaTest_HadOverflowTests, spacing_overflow_no_wrap_and_no_flex_children) {
-  const YGNodeRef child0 = YGNodeNewWithConfig(config);
-  YGNodeStyleSetWidth(child0, 80);
-  YGNodeStyleSetHeight(child0, 40);
-  YGNodeStyleSetMargin(child0, YGEdgeTop, 10);
-  YGNodeStyleSetMargin(child0, YGEdgeBottom, 10);
-  YGNodeInsertChild(root, child0, 0);
-  const YGNodeRef child1 = YGNodeNewWithConfig(config);
-  YGNodeStyleSetWidth(child1, 80);
-  YGNodeStyleSetHeight(child1, 40);
-  YGNodeStyleSetMargin(child1, YGEdgeBottom, 5);
-  YGNodeInsertChild(root, child1, 1);
-    }
-    
-        Config(Config const &) = delete;
-    
-    void assertInternal(const char* formatstr ...) {
-    va_list va_args;
-    va_start(va_args, formatstr);
-    vsnprintf(sAssertBuf, sizeof(sAssertBuf), formatstr, va_args);
-    va_end(va_args);
-    if (gAssertHandler != NULL) {
-        gAssertHandler(sAssertBuf);
-    }
-    FBLOG(LOG_FATAL, 'fbassert', '%s', sAssertBuf);
-    // crash at this specific address so that we can find our crashes easier
-    *(int*)0xdeadb00c = 0;
-    // let the compiler know we won't reach the end of the function
-     __builtin_unreachable();
-}
-    
-    // Keeps a thread-local reference to the current thread's JNIEnv.
-struct Environment {
-  // May be null if this thread isn't attached to the JVM
-  FBEXPORT static JNIEnv* current();
-  static void initialize(JavaVM* vm);
-    }
-    
-      const char* functionName() const { return m_functionName; }
-  const char* fileName() const { return m_fileName; }
-  int lineNumber() const { return m_lineNumber; }
-    
-      void reset(T* other = NULL) {
-    T* old = (T*)pthread_getspecific(m_key);
-    if (old != other) {
-      FBASSERT(m_cleanup);
-      m_cleanup(old);
-      pthread_setspecific(m_key, other);
-    }
-  }
-    
-    #if ENABLE_FBASSERT
-#define FBASSERTMSGF(expr, msg, ...) !(expr) ? facebook::assertInternal('Assert (%s:%d): ' msg, __FILE__, __LINE__, ##__VA_ARGS__) : (void) 0
-#else
-#define FBASSERTMSGF(expr, msg, ...)
-#endif // ENABLE_FBASSERT
-    
-      assert(animal->name()->str() == 'Dog');
-  assert(animal->sound()->str() == 'Bark');
-  (void)animal; // To silence 'Unused Variable' warnings.
-    
-    
-    {	printer->Print(vars, 'x := &$StreamType${stream}\n');
-	if (ServerOnlyStreaming(method)) {
-		printer->Print('if err := x.ClientStream.SendMsg(in); err != nil { return nil, err }\n');
-		printer->Print('if err := x.ClientStream.CloseSend(); err != nil { return nil, err }\n');
-	}
-	printer->Print('return x,nil\n');
-	printer->Outdent();
-	printer->Print('}\n\n');
-    
-    ::grpc::Status MonsterStorage::Service::Store(::grpc::ServerContext* context, const flatbuffers::grpc::Message<Monster>* request, flatbuffers::grpc::Message<Stat>* response) {
-  (void) context;
-  (void) request;
-  (void) response;
-  return ::grpc::Status(::grpc::StatusCode::UNIMPLEMENTED, '');
-}
-    
-        vars['filename'] = file->filename();
-    vars['filename_identifier'] = FilenameIdentifier(file->filename());
-    vars['filename_base'] = file->filename_without_ext();
-    vars['message_header_ext'] = message_header_ext();
-    
-    class LogHelper {
-  std::ostream* os_;
-    }
-    
-    // The callback implementation of our server, that derives from the generated
-// code. It implements all rpcs specified in the FlatBuffers schema.
-class ServiceImpl final : public MyGame::Example::MonsterStorage::Service {
-  virtual ::grpc::Status Store(
-      ::grpc::ServerContext *context,
-      const flatbuffers::grpc::Message<Monster> *request,
-      flatbuffers::grpc::Message<Stat> *response) override {
-    // Create a response from the incoming request name.
-    fbb_.Clear();
-    auto stat_offset = CreateStat(
-        fbb_, fbb_.CreateString('Hello, ' + request->GetRoot()->name()->str()));
-    fbb_.Finish(stat_offset);
-    // Transfer ownership of the message to gRPC
-    *response = fbb_.ReleaseMessage<Stat>();
-    return grpc::Status::OK;
-  }
-  virtual ::grpc::Status Retrieve(
-      ::grpc::ServerContext *context,
-      const flatbuffers::grpc::Message<Stat> *request,
-      ::grpc::ServerWriter<flatbuffers::grpc::Message<Monster>> *writer)
-      override {
-    for (int i = 0; i < 10; i++) {
-      fbb_.Clear();
-      // Create 10 monsters for resposne.
-      auto monster_offset =
-          CreateMonster(fbb_, 0, 0, 0,
-                        fbb_.CreateString(request->GetRoot()->id()->str() +
-                                          ' No.' + std::to_string(i)));
-      fbb_.Finish(monster_offset);
-    }
-    }
-    }
-    
-      // Appends the given text to the generated code as well as a newline
-  // character.  Any text within {{ and }} delimeters is replaced by values
-  // previously stored in the CodeWriter by calling SetValue above.  The newline
-  // will be suppressed if the text ends with the \\ character.
-  void operator+=(std::string text);
-    
-    // Flags that configure how the Builder behaves.
-// The 'Share' flags determine if the Builder automatically tries to pool
-// this type. Pooling can reduce the size of serialized data if there are
-// multiple maps of the same kind, at the expense of slightly slower
-// serialization (the cost of lookups) and more memory use (std::set).
-// By default this is on for keys, but off for strings.
-// Turn keys off if you have e.g. only one map.
-// Turn strings on if you expect many non-unique string values.
-// Additionally, sharing key vectors can save space if you have maps with
-// identical field populations.
-enum BuilderFlag {
-  BUILDER_FLAG_NONE = 0,
-  BUILDER_FLAG_SHARE_KEYS = 1,
-  BUILDER_FLAG_SHARE_STRINGS = 2,
-  BUILDER_FLAG_SHARE_KEYS_AND_STRINGS = 3,
-  BUILDER_FLAG_SHARE_KEY_VECTORS = 4,
-  BUILDER_FLAG_SHARE_ALL = 7,
-};
-    
-    inline NamedHashFunction<uint32_t>::HashFunction FindHashFunction32(
-    const char *name) {
-  std::size_t size = sizeof(kHashFunctions32) / sizeof(kHashFunctions32[0]);
-  for (std::size_t i = 0; i < size; ++i) {
-    if (std::strcmp(name, kHashFunctions32[i].name) == 0) {
-      return kHashFunctions32[i].function;
-    }
-  }
-  return nullptr;
-}
-    
-    inline size_t InlineSize(ElementaryType type, const TypeTable *type_table) {
-  switch (type) {
-    case ET_UTYPE:
-    case ET_BOOL:
-    case ET_CHAR:
-    case ET_UCHAR: return 1;
-    case ET_SHORT:
-    case ET_USHORT: return 2;
-    case ET_INT:
-    case ET_UINT:
-    case ET_FLOAT:
-    case ET_STRING: return 4;
-    case ET_LONG:
-    case ET_ULONG:
-    case ET_DOUBLE: return 8;
-    case ET_SEQUENCE:
-      switch (type_table->st) {
-        case ST_TABLE:
-        case ST_UNION: return 4;
-        case ST_STRUCT: return type_table->values[type_table->num_elems];
-        default: assert(false); return 1;
-      }
-    default: assert(false); return 1;
-  }
-}
-    
-    // Get a field's default, if you know it's floating point and its exact type.
-template<typename T> T GetFieldDefaultF(const reflection::Field &field) {
-  assert(sizeof(T) == GetTypeSize(field.type()->base_type()));
-  return static_cast<T>(field.default_real());
-}
-    
-    #include 'monster_generated.h'  // Already includes 'flatbuffers/flatbuffers.h'.
+    			bd.position.Set(230.0f, 0.5f);
+			body = m_world->CreateBody(&bd);
+			body->CreateFixture(&box, 0.5f);
