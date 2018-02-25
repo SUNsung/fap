b4cdef52f96460babebef76cc287a008c4fa7b00
SUNsung <weatherius@gmail.com>
2018-02-25 09:17:48 +0200

    Rewrite [344qHfiIvMzFw34qrRyNfdSdbM34Vdj6Oc0ULXk34wZT.cpp] File in: 2018-02-25 09:17:47

--- cccccc/344qHfiIvMzFw34qrRyNfdSdbM34Vdj6Oc0ULXk34wZT.cpp
+++ cccccc/344qHfiIvMzFw34qrRyNfdSdbM34Vdj6Oc0ULXk34wZT.cpp
@@ -1,132 +1,296 @@
 
         
-        // Generate constructors.
-#include 'ipc/struct_constructor_macros.h'
-#include 'content/nw/src/common/common_message_generator.h'
-    
-    
-    {}  // namespace remote
-    
-    EventListener::EventListener(int id,
-  const base::WeakPtr<DispatcherHost>& dispatcher_host,
-  const base::DictionaryValue& option) : Base(id, dispatcher_host, option) {
-    }
-    
-    
+        
     {
-    {    if (zoom_controller) {
-      double zoom_factor = content::ZoomLevelToZoomFactor(zoom_controller->GetZoomLevel());
-      if (zoom_factor > content::kMaximumZoomFactor) {
-        zoom_factor = content::kMaximumZoomFactor;
-      }
-      if (zoom_factor < content::kMinimumZoomFactor) {
-        zoom_factor = content::kMinimumZoomFactor;
-      }
-      x *= zoom_factor;
-      y *= zoom_factor;
+    {    if (SemanticNode.is<Expr *>()) {
+      SemanticNode.get<Expr *>()->dump(llvm::errs());
+    } else if (SemanticNode.is<Decl *>()) {
+      SemanticNode.get<Decl *>()->dump(llvm::errs());
+    } else if (SemanticNode.is<Expr *>()) {
+      SemanticNode.get<Expr *>()->dump(llvm::errs());
+    } else {
+      llvm_unreachable('ASTNode has pointer to unknown thing!');
     }
-    
-    Popup(x, y, rvh);
-  } else if (method == 'EnableShowEvent') {
-    arguments.GetBoolean(0, &enable_show_event_);
-  } else {
-    NOTREACHED() << 'Invalid call to Menu method:' << method
-                 << ' arguments:' << arguments;
+    llvm::errs() << '\n=====================================================\n';
   }
 }
+
+    
+    StringRef swift::unicode::extractFirstExtendedGraphemeCluster(StringRef S) {
+  // Extended grapheme cluster segmentation algorithm as described in Unicode
+  // Standard Annex #29.
+  if (S.empty())
+    return StringRef();
+    }
+    
+      /// Suppress all warnings
+  bool SuppressWarnings = false;
+    
+      /// Returns the SourceLoc for the beginning of the specified buffer
+  /// (at offset zero).
+  ///
+  /// Note that the resulting location might not point at the first token: it
+  /// might point at whitespace or a comment.
+  SourceLoc getLocForBufferStart(unsigned BufferID) const {
+    return getRangeForBuffer(BufferID).getStart();
+  }
+    
+      static void ClearCache(content::RenderProcessHost* render_view_host);
+  static void SetProxyConfig(content::RenderProcessHost* render_process_host,
+                             const std::string& proxy_config);
+    
+    #endif  // CONTENT_NW_SRC_API_BASE_BASE_H_
+
     
     
-    {  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
-  if (!item)
+    {  RenderThread::Get()->Send(new ShellViewHostMsg_Call_Object_Method(
+      routing_id,
+      object_id,
+      type,
+      method,
+      *static_cast<base::ListValue*>(value_args.get())));
+  return v8::Undefined(isolate);
+}
+    
+        content::WebContents* embedder_web_contents =
+        GetFirstAppWindowWebContents();
+    if (!embedder_web_contents) {
+      LOG(ERROR) << 'UNABLE TO FIND EMBEDDER WEB CONTENTS.';
+      return;
+    }
+    
+      static v8::Handle<v8::Object> GetObjectRegistry();
+  static v8::Handle<v8::Value> GetWindowId(blink::WebFrame* frame);
+  static void ZoomLevelChanged(blink::WebView* web_view);
+  static void willHandleNavigationPolicy(
+    content::RenderView* rv,
+    blink::WebFrame* frame,
+    const blink::WebURLRequest& request,
+    blink::WebNavigationPolicy* policy,
+    blink::WebString* manifest);
+    
+    
+namespace nwapi {
+    }
+    
+    namespace nwapi {
+    }
+    
+    #include 'content/nw/src/api/menuitem/menuitem.h'
+    
+    void genNFSShare(const std::string& share_line, QueryData& results) {
+  auto line = osquery::split(share_line);
+  if (line.size() == 0 || boost::starts_with(line[0], '#')) {
     return;
-  item->OnClick();
-}
-    
-    void Menu::UpdateStates() {
-}
-    
-    
-    {
-    {    // build keyboard code
-    ui::DomCode domCode = ui::KeycodeConverter::CodeStringToDomCode(keyName.c_str());
-    retval = ui::DomCodeToUsLayoutKeyboardCode(domCode);
   }
-  return retval;
-}
+    }
     
-    #include <vector>
-    
-    /* Starts the stream by sending request to |url| using |method| and |headers|.
- * If |end_of_stream| is true, then no data is expected to be written. The
- * |method| is HTTP verb, with PUT having a special meaning to mark idempotent
- * request, which could use QUIC 0-RTT.
- */
-GRPC_SUPPORT_EXPORT
-int bidirectional_stream_start(bidirectional_stream* stream,
-                               const char* url,
-                               int priority,
-                               const char* method,
-                               const bidirectional_stream_header_array* headers,
-                               bool end_of_stream);
-    
-    #include 'src/core/lib/compression/compression_internal.h'
-    
-      Status BidiStream(
-      ServerContext* context,
-      ServerReaderWriter<EchoResponse, EchoRequest>* stream) override;
-    
-    TEST(InlinedVectorTest, ClearAndRepopulate) {
-  const int kNumElements = 10;
-  InlinedVector<int, 5> v;
-  EXPECT_EQ(0UL, v.size());
-  for (int i = 0; i < kNumElements; ++i) {
-    v.push_back(i);
-    EXPECT_EQ(i + 1UL, v.size());
+    static int osquery_open(dev_t dev, int oflags, int devtype, struct proc *p) {
+  // Close is not working so leave these out for now.
+  int err = 0;
+  lck_mtx_lock(osquery.mtx);
+  if (osquery.open_count == 0) {
+    osquery.open_count++;
   }
-  for (int i = 0; i < kNumElements; ++i) {
-    EXPECT_EQ(i, v[i]);
+#ifndef KERNEL_TEST
+  else {
+    err = -EACCES;
   }
-  v.clear();
-  EXPECT_EQ(0UL, v.size());
-  for (int i = 0; i < kNumElements; ++i) {
-    v.push_back(kNumElements + i);
-    EXPECT_EQ(i + 1UL, v.size());
-  }
-  for (int i = 0; i < kNumElements; ++i) {
-    EXPECT_EQ(kNumElements + i, v[i]);
-  }
-}
-    
-    int main(int argc, char** argv) {
-  RunServer();
+#endif // !KERNEL_TEST
     }
     
     
-    {}  // namespace grpc_node_generator
+    {TEST_F(CarverTests, test_decompression) {
+  auto s = osquery::decompress(
+      fs::temp_directory_path() / fs::path('test.config.zst'),
+      fs::temp_directory_path() / fs::path('test.config'));
+  EXPECT_TRUE(s.ok());
+  EXPECT_TRUE(
+      hashFromFile(
+          HashType::HASH_TYPE_SHA256,
+          (fs::temp_directory_path() / fs::path('test.config')).string()) ==
+      hashFromFile(HashType::HASH_TYPE_SHA256, kTestDataPath + 'test.config'));
+}
+}
+
+    
+    #include <sstream>
+#include <vector>
+    
+    #include 'guetzli/jpeg_data_writer.h'
+    
+      yuv = PreProcessChannel(width_, height_, 2, 1.3f, 0.5f,
+                          cfg.u_sharpen, cfg.u_blur, yuv);
+  yuv = PreProcessChannel(width_, height_, 1, 1.3f, 0.5f,
+                          cfg.v_sharpen, cfg.v_blur, yuv);
+    
+    std::vector<float> Blur(const std::vector<float>& image, int w, int h) {
+    // This is only made for small sigma, e.g. 1.3.
+    static const double kSigma = 1.3;
+    std::vector<double> kernel(5);
+    for (size_t i = 0; i < kernel.size(); i++) {
+      kernel[i] = Normal(1.0 * i - kernel.size() / 2, kSigma);
+    }
+    }
+    
+          // The sentinel node becomes the parent node.
+      size_t j_end = 2 * n - k;
+      tree[j_end].total_count_ =
+          tree[left].total_count_ + tree[right].total_count_;
+      tree[j_end].index_left_ = static_cast<int16_t>(left);
+      tree[j_end].index_right_or_value_ = static_cast<int16_t>(right);
+    
+    // Entropy encoding (Huffman) utilities.
+    
+    #ifndef GUETZLI_FAST_LOG_H_
+#define GUETZLI_FAST_LOG_H_
+    
+    // kIDCTMatrix[8*x+u] = alpha(u)*cos((2*x+1)*u*M_PI/16)*sqrt(2), with fixed 13
+// bit precision, where alpha(0) = 1/sqrt(2) and alpha(u) = 1 for u > 0.
+// Some coefficients are off by +-1 to mimick libjpeg's behaviour.
+static const int kIDCTMatrix[kDCTBlockSize] = {
+  8192,  11363,  10703,   9633,   8192,   6437,   4433,   2260,
+  8192,   9633,   4433,  -2259,  -8192, -11362, -10704,  -6436,
+  8192,   6437,  -4433, -11362,  -8192,   2261,  10704,   9633,
+  8192,   2260, -10703,  -6436,   8192,   9633,  -4433, -11363,
+  8192,  -2260, -10703,   6436,   8192,  -9633,  -4433,  11363,
+  8192,  -6437,  -4433,  11362,  -8192,  -2261,  10704,  -9633,
+  8192,  -9633,   4433,   2259,  -8192,  11362, -10704,   6436,
+  8192, -11363,  10703,  -9633,   8192,  -6437,   4433,  -2260,
+};
+    
+    // Fills in 'result' with the inverse DCT of 'block'.
+// The arguments 'block' and 'result' point to 8x8 arrays that are arranged in
+// a row-by-row memory layout.
+void ComputeBlockIDCT(const coeff_t* block, uint8_t* result);
+    
+    #endif  // GUETZLI_JPEG_DATA_DECODER_H_
+
+    
+    
+    {}  // namespace guetzli
+    
+    #include <inttypes.h>
+    
+      // Fills in out[] array with the floating-point precision pixel view of the
+  // component.
+  // REQUIRES: factor_x() == 1 and factor_y() == 1.
+  void ToFloatPixels(float* out, int stride) const;
+    
+    namespace folly {
+namespace hazptr {
+    }
+    }
+    
+    void AsyncIOQueue::submit(AsyncIOOp* op) {
+  submit([op]() { return op; });
+}
+    
+      /**
+   * Reset the operation for reuse.  It is an error to call reset() on
+   * an Op that is still pending.
+   */
+  void reset(NotificationCallback cb = NotificationCallback());
+    
+    void list() {
+  for (const auto& p : getHugePageSizes()) {
+    std::cout << p.size << ' ' << p.mountPoint << '\n';
+  }
+}
+    
+    namespace folly {
+    }
+    
+    #include <folly/Optional.h>
+#include <folly/Range.h>
+#include <memory>
+    
+      // Format the data into a buffer.
+  std::string buffer;
+  StringPiece msgData{message.getMessage()};
+  if (message.containsNewlines()) {
+    // If there are multiple lines in the log message, add a header
+    // before each one.
+    std::string header;
+    header.reserve(headerLengthGuess);
+    headerFormatter.appendTo(header);
+    }
+    
+    
+    {
+    {  folly::File file_;
+};
+} // namespace folly
+
+    
+    void LogCategory::setLevel(LogLevel level, bool inherit) {
+  // We have to set the level through LoggerDB, since we require holding
+  // the LoggerDB lock to iterate through our children in case our effective
+  // level changes.
+  db_->setLevel(this, level, inherit);
+}
+    
+    bool LogCategoryConfig::operator==(const LogCategoryConfig& other) const {
+  return level == other.level &&
+      inheritParentLevel == other.inheritParentLevel &&
+      handlers == other.handlers;
+}
+    
+    
+    {} // namespace folly
+
     
     /**
- * Symbolicates a stack trace into a given vector
+ * Parse a JSON configuration string.
  *
- * @param symbols The vector to receive the output. The vector is cleared and
- * enough room to keep the frames are reserved.
+ * See the documentation in logging/docs/Config.md for a description of the
+ * JSON configuration object format.
  *
- * @param stackTrace The input stack trace
+ * This function uses relaxed JSON parsing, allowing C and C++ style
+ * comments, as well as trailing commas.
  */
-FBEXPORT void getStackTraceSymbols(std::vector<StackTraceElement>& symbols,
-                                   const std::vector<InstructionPointer>& trace);
+LogConfig parseLogConfigJson(StringPiece value);
     
-          obj->setFieldValue<jboolean>(hasNewLayoutField, true);
-      YGTransferLayoutDirection(root, obj);
-      root->setHasNewLayout(false);
+    // Abort the program after logging the mesage if the given condition is not
+// true. Otherwise, do nothing.
+#define GRPC_CODEGEN_CHECK(x)                                            \
+  (x) ? (void)0                                                          \
+      : LogMessageVoidify() & LogHelper(&std::cerr).get_os()             \
+                                  << 'CHECK FAILED: ' << __FILE__ << ':' \
+                                  << __LINE__ << ': '
     
-    #include <yoga/Yoga.h>
     
-        Config(void);
+template<typename T> T EndianScalar(T t) {
+  #if FLATBUFFERS_LITTLEENDIAN
+    return t;
+  #else
+    return EndianSwap(t);
+  #endif
+}
     
-    #pragma once
-    
-    namespace facebook {
+      // Converts a binary buffer to text using one of the schemas in the registry,
+  // use the file_identifier to indicate which.
+  // If DetachedBuffer::data() is null then parsing failed.
+  DetachedBuffer TextToFlatBuffer(const char *text,
+                                  const char *file_identifier) {
+    // Load and parse the schema.
+    Parser parser;
+    if (!LoadSchema(file_identifier, &parser)) return DetachedBuffer();
+    // Parse the text.
+    if (!parser.Parse(text)) {
+      lasterror_ = parser.error_;
+      return DetachedBuffer();
     }
+    // We have a valid FlatBuffer. Detach it from the builder and return.
+    return parser.builder_.ReleaseBufferPointer();
+  }
     
+      auto weapon_two_name = builder.CreateString('Axe');
+  short weapon_two_damage = 5;
     
-    {}
+      if (opts.proto_mode) {
+    if (any_generator)
+      Error('cannot generate code directly from .proto files', true);
+  } else if (!any_generator && conform_to_schema.empty()) {
+    Error('no options: specify at least one generator.', true);
+  }
