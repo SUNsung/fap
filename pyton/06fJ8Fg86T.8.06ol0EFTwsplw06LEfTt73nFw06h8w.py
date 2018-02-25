048e8b7d5b031dcaeb79c7228d4cbc5757de5dff
SUNsung <weatherius@gmail.com>
2018-02-26 01:58:26 +0200

    Rewrite [06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py] File in: 2018-02-26 01:58:26

--- pyton/06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py
+++ pyton/06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py
@@ -1,92 +1,122 @@
 
         
-            # Train the Model.
-    classifier.train(
-        input_fn=lambda:iris_data.train_input_fn(train_x, train_y,
-                                                 args.batch_size),
-        steps=args.train_steps)
-    
-        # The text file for eval.
-    self.eval_data = FLAGS.eval_data
+            def get_testable_domain_names(self):
+        '''Returns the set of domain names that can be tested against'''
+        if self._test_names:
+            return self._test_names
+        else:
+            return {'example.com'}
     
     
-tf.app.flags.DEFINE_float('learning_rate', 0.5, 'Learning rate.')
-tf.app.flags.DEFINE_float('learning_rate_decay_factor', 0.99,
-                          'Learning rate decays by this much.')
-tf.app.flags.DEFINE_float('max_gradient_norm', 5.0,
-                          'Clip gradients to this norm.')
-tf.app.flags.DEFINE_integer('batch_size', 64,
-                            'Batch size to use during training.')
-tf.app.flags.DEFINE_integer('size', 1024, 'Size of each model layer.')
-tf.app.flags.DEFINE_integer('num_layers', 3, 'Number of layers in the model.')
-tf.app.flags.DEFINE_integer('from_vocab_size', 40000, 'English vocabulary size.')
-tf.app.flags.DEFINE_integer('to_vocab_size', 40000, 'French vocabulary size.')
-tf.app.flags.DEFINE_string('data_dir', '/tmp', 'Data directory')
-tf.app.flags.DEFINE_string('train_dir', '/tmp', 'Training directory.')
-tf.app.flags.DEFINE_string('from_train_data', None, 'Training data.')
-tf.app.flags.DEFINE_string('to_train_data', None, 'Training data.')
-tf.app.flags.DEFINE_string('from_dev_data', None, 'Training data.')
-tf.app.flags.DEFINE_string('to_dev_data', None, 'Training data.')
-tf.app.flags.DEFINE_integer('max_train_data_size', 0,
-                            'Limit on the size of training data (0: no limit).')
-tf.app.flags.DEFINE_integer('steps_per_checkpoint', 200,
-                            'How many training steps to do per checkpoint.')
-tf.app.flags.DEFINE_boolean('decode', False,
-                            'Set to True for interactive decoding.')
-tf.app.flags.DEFINE_boolean('self_test', False,
-                            'Run a self-test if this is set to True.')
-tf.app.flags.DEFINE_boolean('use_fp16', False,
-                            'Train using fp16 instead of fp32.')
+class AskTest(unittest.TestCase):
+    '''Test the ask method.'''
+    def setUp(self):
+        logging.disable(logging.CRITICAL)
     
-      if ps_strategy is None:
-    ps_strategy = device_setter._RoundRobinStrategy(num_devices)
-  if not six.callable(ps_strategy):
-    raise TypeError('ps_strategy must be callable')
+    if __name__ == '__main__':
+    unittest.main()  # pragma: no cover
+
     
-        def syntax(self):
-        '''
-        Command syntax (preferably one-line). Do not include command name.
-        '''
-        return ''
+        # Parse and validate the field names.  Validation serves two purposes,
+    # generating informative error messages and preventing template injection attacks.
+    if isinstance(field_names, basestring):
+        field_names = field_names.replace(',', ' ').split() # names separated by whitespace and/or commas
+    field_names = tuple(map(str, field_names))
+    for name in (typename,) + field_names:
+        if not all(c.isalnum() or c=='_' for c in name):
+            raise ValueError('Type names and field names can only contain alphanumeric characters and underscores: %r' % name)
+        if _iskeyword(name):
+            raise ValueError('Type names and field names cannot be a keyword: %r' % name)
+        if name[0].isdigit():
+            raise ValueError('Type names and field names cannot start with a number: %r' % name)
+    seen_names = set()
+    for name in field_names:
+        if name.startswith('_'):
+            raise ValueError('Field names cannot start with an underscore: %r' % name)
+        if name in seen_names:
+            raise ValueError('Encountered duplicate field name: %r' % name)
+        seen_names.add(name)
     
-            for spidername in args or spider_loader.list():
-            spidercls = spider_loader.load(spidername)
-            spidercls.start_requests = lambda s: conman.from_spider(s, result)
+    def download_urls_sequential(urls, timeout=60):
+    url_to_content = {}
+    for url in urls:
+        try:
+            url_to_content[url] = load_url(url, timeout=timeout)
+        except:
+            pass
+    return url_to_content
     
-    
-def sanitize_module_name(module_name):
-    '''Sanitize the given module name, by replacing dashes and points
-    with underscores and prefixing it with a letter if it doesn't start
-    with one
+        This will typically be run on the result of the communicate() method
+    of a subprocess.Popen object.
     '''
-    module_name = module_name.replace('-', '_').replace('.', '_')
-    if module_name[0] not in string.ascii_letters:
-        module_name = 'a' + module_name
-    return module_name
-    
-    '''
-    
-    Revision ID: 2591d77e9831
-Revises: 12d55656cbca
-Create Date: 2015-12-15 17:02:45.128709
+    stderr = re.sub(r'\[\d+ refs\]\r?\n?$'.encode(), ''.encode(), stderr).strip()
+    return stderr
     
     
-class Slice(Base):
-    '''Declarative class to do query in upgrade'''
-    __tablename__ = 'slices'
-    id = Column(Integer, primary_key=True)
-    datasource_id = Column(Integer)
-    druid_datasource_id = Column(Integer)
-    table_id = Column(Integer)
-    datasource_type = Column(String(200))
+def _FormatYcmdDebugInfo( ycmd ):
+  python = ycmd[ 'python' ]
+  clang = ycmd[ 'clang' ]
+  message = ( 'Server Python interpreter: {0}\n'
+              'Server Python version: {1}\n'
+              'Server has Clang support compiled in: {2}\n'
+              'Clang version: {3}\n'.format( python[ 'executable' ],
+                                             python[ 'version' ],
+                                             clang[ 'has_support' ],
+                                             clang[ 'version' ] ) )
+  extra_conf = ycmd[ 'extra_conf' ]
+  extra_conf_path = extra_conf[ 'path' ]
+  if not extra_conf_path:
+    message += 'No extra configuration file found\n'
+  elif not extra_conf[ 'is_loaded' ]:
+    message += ( 'Extra configuration file found but not loaded\n'
+                 'Extra configuration path: {0}\n'.format( extra_conf_path ) )
+  else:
+    message += ( 'Extra configuration file found and loaded\n'
+                 'Extra configuration path: {0}\n'.format( extra_conf_path ) )
+  return message
     
-    '''
     
-    '''
+  def Start( self ):
+    request_data = BuildRequestData()
+    request_data.update( { 'filetypes': self.filetypes } )
+    with HandleServerException():
+      self._response = self.PostDataToHandler( request_data,
+                                               'semantic_completion_available' )
     
-    # revision identifiers, used by Alembic.
-revision = '763d4b211ec9'
-down_revision = 'd2424a248d63'
+      def FilterLevel( diagnostic ):
+    return diagnostic[ 'kind' ] == expected_kind
     
-    def downgrade():
-    op.drop_column('slices', 'description')
+    
+def FormatDebugInfoResponse_NoExtraConf_test():
+  response = deepcopy( GENERIC_RESPONSE )
+  response[ 'extra_conf' ].update( {
+    'is_loaded': False,
+    'path': None
+  } )
+  assert_that(
+    FormatDebugInfoResponse( response ),
+    contains_string(
+      'No extra configuration file found\n'
+    )
+  )
+    
+            total_count = 0
+        translated_count = 0
+        with open(fn) as f:
+            catalog = read_po(f)
+            for msg in catalog:
+                total_count += 1
+                if is_translated(msg):
+                    translated_count += 1
+        pct = translated_count / float(total_count) * 100
+        click.echo('% -7s % 2d%%' % (
+            locale,
+            pct,
+        ), err=True)
+        if pct >= MINIMUM and locale not in rv:
+            rv.append(locale)
+    with open(catalog_file, 'w') as f:
+        json.dump({
+            'supported_locales': sorted(rv)
+        }, f, indent=2)
+        f.write('\n')
