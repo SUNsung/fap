bab32ac317d5a754e0f0027e3dfa69649ec5224d
SUNsung <weatherius@gmail.com>
2018-02-25 02:20:50 +0200

    Rewrite [275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb] File in: 2018-02-25 02:20:49

--- ruby/275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb
+++ ruby/275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb
@@ -1,21 +1,98 @@
-  # Asynchronously send an email
-  class TestEmail < Jobs::Base
-    
-          def device_avalaible?(serial)
-        load_all_devices
-        return devices.map(&:serial).include?(serial)
-      end
-    
-        def emails=(emails)
-      @emails = emails ? emails.compact : nil
+
+        
+          # Allows a bottle tag to specify a specific OS or later,
+  # so the same bottle can target multiple OSs.
+  # Not used in core, used in taps.
+  def find_or_later_tag(tag)
+    begin
+      tag_version = MacOS::Version.from_symbol(tag)
+    rescue ArgumentError
+      return
     end
     
-          it 'does switch to plain text when xml is toggled off' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            cloc(xml: false)
-          end').runner.execute(:test)
+        if ARGV.named.empty?
+      slow_checks = %w[
+        check_for_broken_symlinks
+        check_missing_deps
+        check_for_outdated_homebrew
+        check_for_linked_keg_only_brews
+      ]
+      methods = (checks.all.sort - slow_checks) + slow_checks
+    else
+      methods = ARGV.named
+    end
     
-          expect('.size-auto').to have_ruleset(rule)
+          if $stdout.tty?
+        count = local_results.length + tap_results.length
+    
+      # POST /resource/password
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    yield resource if block_given?
+    
+            routes.each do |module_name, actions|
+          [:path, :url].each do |path_or_url|
+            actions.each do |action|
+              action = action ? '#{action}_' : ''
+              method = :'#{action}#{module_name}_#{path_or_url}'
+    
+      protected
+    
+        unless user
+      EmailInviter.new(email, inviter).send!
+      flash[:notice] = 'invitation sent to #{email}'
+    else
+      flash[:notice]= 'error sending invite to #{email}'
+    end
+    redirect_to user_search_path, :notice => flash[:notice]
+  end
+    
+    class Rack::Builder
+  include Sinatra::Delegator
+end
+
+    
+          def redirect(env)
+        request = Request.new(env)
+        warn env, 'attack prevented by #{self.class}'
+        [302, {'Content-Type' => 'text/html', 'Location' => request.path}, []]
+      end
+    
+      # Many RSpec users commonly either run the entire suite or an individual
+  # file, and it's useful to allow more verbose output when running an
+  # individual spec file.
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured
+    # (e.g. via a command-line flag).
+    config.default_formatter = 'doc'
+  end
+    
+      def generate_error_hash(error)
+    {
+      :path => request.path,
+      :status => error.status_code,
+      :error => error.to_hash
+    }
+  end
+    
+      context 'called with four colors' do
+    it 'applies different colors to all sides' do
+      rule = 'border-color: #00f #0f0 #f00 #ff0'
+    
+    describe 'margin' do
+  before(:all) do
+    ParserSupport.parse_file('library/margin')
+  end
+    
+      context 'called with arguments (2, $value: 4em 6em)' do
+    it 'outputs sextuple the second value from the default scale' do
+      expect('.two-double-value').to have_rule('font-size: 3.125em')
     end
   end
 end
+
+    
+          expect('.size-both').to have_ruleset(rule)
+    end
+  end
