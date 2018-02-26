fd1811e51041e7348df79b91e585f124489d3624
SUNsung <weatherius@gmail.com>
2018-02-26 12:17:18 +0200

    Rewrite [india.rb] File in: 2018-02-26 12:17:17

--- ruby/india.rb
+++ ruby/india.rb
@@ -1,139 +1,102 @@
 
         
-              out = checks.send(method)
-      unless out.nil? || out.empty?
-        if first_warning
-          $stderr.puts <<-EOS.undent
-            #{Tty.white}Please note that these warnings are just used to help the Homebrew maintainers
-            with debugging if you file an issue. If everything you use Homebrew for is
-            working fine: please don't worry and just ignore them. Thanks!#{Tty.reset}
-          EOS
+              https://pip.readthedocs.org/en/stable/installing/#install-pip
+    EOS
+  when 'pil' then <<-EOS.undent
+    Instead of PIL, consider `pip install pillow` or `brew install Homebrew/python/pillow`.
+    EOS
+  when 'macruby' then <<-EOS.undent
+    MacRuby works better when you install their package:
+      http://www.macruby.org/
+    EOS
+  when /(lib)?lzma/
+    'lzma is now part of the xz formula.'
+  when 'xcode'
+    if MacOS.version >= :lion
+      <<-EOS.undent
+      Xcode can be installed from the App Store.
+      EOS
+    else
+      <<-EOS.undent
+      Xcode can be installed from https://developer.apple.com/xcode/downloads/
+      EOS
+    end
+  when 'gtest', 'googletest', 'google-test' then <<-EOS.undent
+    Installing gtest system-wide is not recommended; it should be vendored
+    in your projects that use it.
+    EOS
+  when 'gmock', 'googlemock', 'google-mock' then <<-EOS.undent
+    Installing gmock system-wide is not recommended; it should be vendored
+    in your projects that use it.
+    EOS
+  when 'sshpass' then <<-EOS.undent
+    We won't add sshpass because it makes it too easy for novice SSH users to
+    ruin SSH's security.
+    EOS
+  when 'gsutil' then <<-EOS.undent
+    Install gsutil with `pip install gsutil`
+    EOS
+  when 'clojure' then <<-EOS.undent
+    Clojure isn't really a program but a library managed as part of a
+    project and Leiningen is the user interface to that library.
+    
+      def clang
+    @clang ||= MacOS.clang_version if MacOS.has_apple_developer_tools?
+  end
+    
+    class PrettyListing
+  def initialize(path)
+    Pathname.new(path).children.sort_by { |p| p.to_s.downcase }.each do |pn|
+      case pn.basename.to_s
+      when 'bin', 'sbin'
+        pn.find { |pnn| puts pnn unless pnn.directory? }
+      when 'lib'
+        print_dir pn do |pnn|
+          # dylibs have multiple symlinks and we don't care about them
+          (pnn.extname == '.dylib' || pnn.extname == '.pc') && !pnn.symlink?
         end
-    
-        # Unbrewed uses the PREFIX, which will exist
-    # Things below use the CELLAR, which doesn't until the first formula is installed.
-    unless HOMEBREW_CELLAR.exist?
-      raise NoSuchKegError.new(ARGV.named.first) if ARGV.named.any?
-      return
-    end
-    
-          def self.available_options
-        [
-          FastlaneCore::ConfigItem.new(key: :name,
-                                       env_name: 'KEYCHAIN_NAME',
-                                       description: 'Keychain name',
-                                       conflicting_options: [:path],
-                                       is_string: true,
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :path,
-                                       env_name: 'KEYCHAIN_PATH',
-                                       description: 'Path to keychain',
-                                       is_string: true,
-                                       conflicting_options: [:name],
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :password,
-                                       env_name: 'KEYCHAIN_PASSWORD',
-                                       description: 'Password for the keychain',
-                                       sensitive: true,
-                                       optional: false),
-          FastlaneCore::ConfigItem.new(key: :default_keychain,
-                                       description: 'Should the newly created Keychain be the new system default keychain',
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :unlock,
-                                       description: 'Unlock keychain after create',
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :timeout,
-                                       description: 'timeout interval in seconds. Set `false` if you want to specify 'no time-out'',
-                                       is_string: false,
-                                       default_value: 300),
-          FastlaneCore::ConfigItem.new(key: :lock_when_sleeps,
-                                       description: 'Lock keychain when the system sleeps',
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :lock_after_timeout,
-                                       description: 'Lock keychain after timeout interval',
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :add_to_search_list,
-                                       description: 'Add keychain to search list',
-                                       is_string: false,
-                                       default_value: true)
-        ]
-      end
-    
-          def load_all_tasks
-        self.tasks = []
-    
-        def crashlytics_path_valid?
-      !crashlytics_path.nil? && File.exist?(crashlytics_path) && File.exist?(File.join(crashlytics_path, 'submit'))
-    end
-    
-          it 'does switch to plain text when xml is toggled off' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            cloc(xml: false)
-          end').runner.execute(:test)
-    
-          it 'gets the correct version number for 'TargetATests'' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          get_version_number(xcodeproj: '.xcproject', target: 'TargetATests')
-        end').runner.execute(:test)
-        expect(result).to eq('4.3.2')
-      end
-    
-          it 'pass a custom build number to the tool' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          increment_build_number(build_number: 24, xcodeproj: '.xcproject')
-        end').runner.execute(:test)
-    
-          it 'it increments all targets minor version major' do
-        Fastlane::FastFile.new.parse('lane :test do
-          increment_version_number(bump_type: 'major')
-        end').runner.execute(:test)
-    
-    gem 'rails', '~> 5.1'
-gem 'omniauth', '~> 1.3'
-gem 'oauth2'
-gem 'omniauth-oauth2'
-gem 'rdoc'
-    
-        def reset_password_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :reset_password_instructions, opts)
-    end
-    
-          # Forgets the given resource by deleting a cookie
-      def forget_me(resource)
-        scope = Devise::Mapping.find_scope!(resource)
-        resource.forget_me!
-        cookies.delete(remember_key(resource, scope), forget_cookie_values(resource))
-      end
-    
-              tries     += 1
-          record.id += rand(100)
-    
-      let(:payload) do
-    {
-      '@context': 'https://www.w3.org/ns/activitystreams',
-      id: 'foo',
-      type: 'Create',
-      actor: ActivityPub::TagManager.instance.uri_for(actor),
-      object: {
-        id: 'bar',
-        type: 'Note',
-        content: 'Lorem ipsum',
-      },
-    }
-  end
-    
-      private
-    
-          format.json do
-        render json: collection_presenter,
-               serializer: ActivityPub::CollectionSerializer,
-               adapter: ActivityPub::Adapter,
-               content_type: 'application/activity+json'
+      else
+        if pn.directory?
+          if pn.symlink?
+            puts '#{pn} -> #{pn.readlink}'
+          else
+            print_dir pn
+          end
+        elsif Metafiles.list?(pn.basename.to_s)
+          puts pn
+        end
       end
     end
   end
+    
+      def self.require_universal_deps
+    define_method(:require_universal_deps?) { true }
+  end
+    
+        sidekiq_options queue: 'critical'
+    
+        ret = route.slice(:location, :label)
+    ret[:full_location] = 'adminPlugins.#{ret[:location]}'
+    ret
+  end
+    
+          unless root?
+        raise Invalid, 'missing name' if !name || name.empty?
+        raise Invalid, 'missing path' if !path || path.empty?
+        raise Invalid, 'missing type' if !type || type.empty?
+      end
+    end
+    
+    module Docs
+  class PageDb
+    attr_reader :pages
+    
+              node.css('> ul > li > a').each do |link|
+            n = link.content
+            next if n.start_with?('Ex: ') || n.start_with?('Default ') || n =~ /example/i || IGNORE_ENTRIES.include?(n)
+            id = link['href'].remove('#')
+            n.downcase!
+            n.prepend '#{name}: '
+            entries << [n, id]
+          end
+        end
