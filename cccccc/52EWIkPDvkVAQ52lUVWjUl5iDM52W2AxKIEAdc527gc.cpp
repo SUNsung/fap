797aa20215cbbb3415ed746f637fadb951c1caa9
SUNsung <weatherius@gmail.com>
2018-02-26 05:16:48 +0200

    Rewrite [52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp] File in: 2018-02-26 05:16:47

--- cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
+++ cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
@@ -1,256 +1,240 @@
 
         
-        static void frame_begin()
-{
-    VkResult err;
-    for (;;)
-    {
-        err = vkWaitForFences(g_Device, 1, &g_Fence[g_FrameIndex], VK_TRUE, 100);
-        if (err == VK_SUCCESS) break;
-        if (err == VK_TIMEOUT) continue;
-        check_vk_result(err);
+         private:
+  using PendingRequestMap = std::map<int, SendCommandCallback>;
+    
+    namespace mate {
     }
-    {
-        err = vkAcquireNextImageKHR(g_Device, g_Swapchain, UINT64_MAX, g_PresentCompleteSemaphore[g_FrameIndex], VK_NULL_HANDLE, &g_BackbufferIndices[g_FrameIndex]);
-        check_vk_result(err);
-    }
-    {
-        err = vkResetCommandPool(g_Device, g_CommandPool[g_FrameIndex], 0);
-        check_vk_result(err);
-        VkCommandBufferBeginInfo info = {};
-        info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-        info.flags |= VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
-        err = vkBeginCommandBuffer(g_CommandBuffer[g_FrameIndex], &info);
-        check_vk_result(err);
-    }
-    {
-        VkRenderPassBeginInfo info = {};
-        info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-        info.renderPass = g_RenderPass;
-        info.framebuffer = g_Framebuffer[g_BackbufferIndices[g_FrameIndex]];
-        info.renderArea.extent.width = fb_width;
-        info.renderArea.extent.height = fb_height;
-        info.clearValueCount = 1;
-        info.pClearValues = &g_ClearValue;
-        vkCmdBeginRenderPass(g_CommandBuffer[g_FrameIndex], &info, VK_SUBPASS_CONTENTS_INLINE);
-    }
+    
+    SavePageHandler::SavePageHandler(content::WebContents* web_contents,
+                                 const SavePageCallback& callback)
+    : web_contents_(web_contents),
+      callback_(callback) {
 }
     
-        // Cleanup
-    ImGui_ImplA5_Shutdown();
-    ImGui::DestroyContext();
-    al_destroy_event_queue(queue);
-    al_destroy_display(display);
+      AtomQuotaPermissionContext();
+  virtual ~AtomQuotaPermissionContext();
     
-    
-    {    return 0;
+    net::URLRequestJob* AsarProtocolHandler::MaybeCreateJob(
+    net::URLRequest* request,
+    net::NetworkDelegate* network_delegate) const {
+  base::FilePath full_path;
+  net::FileURLToFilePath(request->url(), &full_path);
+  auto* job = new URLRequestAsarJob(request, network_delegate);
+  job->Initialize(file_task_runner_, full_path);
+  return job;
 }
-
     
-                    // Copy rasterized pixels to main texture
-                uint8_t* blit_dst = atlas->TexPixelsAlpha8 + rect.y * atlas->TexWidth + rect.x;
-                font_face.BlitGlyph(ft_glyph_bitmap, blit_dst, atlas->TexWidth, multiply_enabled ? multiply_table : NULL);
-                FT_Done_Glyph(ft_glyph);
-    
-    // DirectX data
-static ID3D11Device*            g_pd3dDevice = NULL;
-static ID3D11DeviceContext*     g_pd3dDeviceContext = NULL;
-static ID3D11Buffer*            g_pVB = NULL;
-static ID3D11Buffer*            g_pIB = NULL;
-static ID3D10Blob *             g_pVertexShaderBlob = NULL;
-static ID3D11VertexShader*      g_pVertexShader = NULL;
-static ID3D11InputLayout*       g_pInputLayout = NULL;
-static ID3D11Buffer*            g_pVertexConstantBuffer = NULL;
-static ID3D10Blob *             g_pPixelShaderBlob = NULL;
-static ID3D11PixelShader*       g_pPixelShader = NULL;
-static ID3D11SamplerState*      g_pFontSampler = NULL;
-static ID3D11ShaderResourceView*g_pFontTextureView = NULL;
-static ID3D11RasterizerState*   g_pRasterizerState = NULL;
-static ID3D11BlendState*        g_pBlendState = NULL;
-static ID3D11DepthStencilState* g_pDepthStencilState = NULL;
-static int                      g_VertexBufferSize = 5000, g_IndexBufferSize = 10000;
-    
-    // GLFW callbacks (registered by default to GLFW if you enable 'install_callbacks' during initialization)
-// Provided here if you want to chain callbacks yourself. You may also handle inputs yourself and use those as a reference.
-IMGUI_API void        ImGui_ImplGlfw_MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
-IMGUI_API void        ImGui_ImplGlfw_ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
-IMGUI_API void        ImGui_ImplGlfw_KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-IMGUI_API void        ImGui_ImplGlfw_CharCallback(GLFWwindow* window, unsigned int c);
-
-    
-    // Use if you want to reset your rendering device without losing ImGui state.
-IMGUI_API void        ImGui_ImplGlfwGL3_InvalidateDeviceObjects();
-IMGUI_API bool        ImGui_ImplGlfwGL3_CreateDeviceObjects();
-    
-                if (ImGui::Button('Button'))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text('counter = %d', counter);
-    
-        if (connections % 1000 == 0 || connections < 1000) {
-        cout << 'Connections: ' << connections << endl;
-    }
-    
-    template <bool isServer>
-void ExtensionsNegotiator<isServer>::readOffer(std::string offer) {
-    if (isServer) {
-        ExtensionsParser extensionsParser(offer.data(), offer.length());
-        if ((options & PERMESSAGE_DEFLATE) && extensionsParser.perMessageDeflate) {
-            if (extensionsParser.clientNoContextTakeover || (options & CLIENT_NO_CONTEXT_TAKEOVER)) {
-                options |= CLIENT_NO_CONTEXT_TAKEOVER;
-            }
-    }
-    }
-    }
-    
-    template <bool isServer>
-void Group<isServer>::timerCallback(uS::Timer *timer) {
-    Group<isServer> *group = (Group<isServer> *) timer->getData();
-    }
-    
-        while (httpSocket->outstandingResponsesHead) {
-        Group<isServer>::from(httpSocket)->httpCancelledRequestHandler(httpSocket->outstandingResponsesHead);
-        HttpResponse *next = httpSocket->outstandingResponsesHead->next;
-        delete httpSocket->outstandingResponsesHead;
-        httpSocket->outstandingResponsesHead = next;
-    }
+    #include 'atom/browser/net/http_protocol_handler.h'
     
     #include <string>
+#include <vector>
     
-        // same as listen(TRANSFERS), backwards compatible API for now
-    void addAsync() {
-        if (!async) {
-            NodeData::addAsync();
-        }
-    }
-    
-        // todo: asio is thread safe, use it!
-    bool threadSafeChange(Loop *loop, Poll *self, int events) {
-        return false;
-    }
-    
-        void start(Loop *loop, Poll *self, int events) {
-        epoll_event event;
-        event.events = events;
-        event.data.ptr = self;
-        epoll_ctl(loop->epfd, EPOLL_CTL_ADD, state.fd, &event);
-    }
-    
-    namespace fuzzer {
-    }
-    
-    namespace fuzzer {
-// A simple POD sized array of bytes.
-template <size_t kMaxSize> class FixedWord {
-public:
-  FixedWord() {}
-  FixedWord(const uint8_t *B, uint8_t S) { Set(B, S); }
-    }
-    }
-    
-    #endif // LIBFUZZER_WINDOWS
-
-    
-      Fuzzer(UserCallback CB, InputCorpus &Corpus, MutationDispatcher &MD,
-         FuzzingOptions Options);
-  ~Fuzzer();
-  void Loop();
-  void MinimizeCrashLoop(const Unit &U);
-  void ShuffleAndMinimize(UnitVector *V);
-  void InitializeTraceState();
-  void RereadOutputCorpus(size_t MaxSize);
+    // Saves the COM marker segment as a string to *jpg.
+bool ProcessCOM(const uint8_t* data, const size_t len, size_t* pos,
+                JPEGData* jpg) {
+  VERIFY_LEN(2);
+  size_t marker_len = ReadUint16(data, pos);
+  VERIFY_INPUT(marker_len, 2, 65535, MARKER_LEN);
+  VERIFY_LEN(marker_len - 2);
+  std::string com_str(reinterpret_cast<const char*>(
+      &data[*pos - 2]), marker_len);
+  *pos += marker_len - 2;
+  jpg->com_data.push_back(com_str);
+  return true;
+}
     
     
-    {  std::vector<Mutator> Mutators;
-  std::vector<Mutator> DefaultMutators;
+    {  // Returns a heuristic cutoff on block errors in the sense that we won't
+  // consider distortions where a block error is greater than this.
+  virtual float BlockErrorLimit() const = 0;
+  // Given the search direction (+1 for upwards and -1 for downwards) and the
+  // current distance map, fills in *block_weight image with the relative block
+  // error adjustment weights.
+  // The target_mul param has the same semantics as in DistanceOK().
+  // Note that this is essentially a static function in the sense that it does
+  // not depend on the last Compare() call.
+  virtual void ComputeBlockErrorAdjustmentWeights(
+      int direction, int max_block_dist, double target_mul, int factor_x,
+      int factor_y, const std::vector<float>& distmap,
+      std::vector<float>* block_weight) = 0;
 };
     
-      void StopTraceRecording() {
-    if (!RecordingMemcmp)
-      return;
-    RecordingMemcmp = false;
-    for (size_t i = 0; i < NumMutations; i++) {
-      auto &M = Mutations[i];
-      if (Options.Verbosity >= 2) {
-        AutoDictUnitCounts[M.W]++;
-        AutoDictAdds++;
-        if ((AutoDictAdds & (AutoDictAdds - 1)) == 0) {
-          typedef std::pair<size_t, Word> CU;
-          std::vector<CU> CountedUnits;
-          for (auto &I : AutoDictUnitCounts)
-            CountedUnits.push_back(std::make_pair(I.second, I.first));
-          std::sort(CountedUnits.begin(), CountedUnits.end(),
-                    [](const CU &a, const CU &b) { return a.first > b.first; });
-          Printf('AutoDict:\n');
-          for (auto &I : CountedUnits) {
-            Printf('   %zd ', I.first);
-            PrintASCII(I.second.data(), I.second.size());
-            Printf('\n');
-          }
-        }
-      }
-      MD.AddWordToAutoDictionary({M.W, M.Pos});
+    
+    {}  // namespace
+    
+    static const int kCrToRedTable[256] = {
+  -179, -178, -177, -175, -174, -172, -171, -170, -168, -167, -165, -164,
+  -163, -161, -160, -158, -157, -156, -154, -153, -151, -150, -149, -147,
+  -146, -144, -143, -142, -140, -139, -137, -136, -135, -133, -132, -130,
+  -129, -128, -126, -125, -123, -122, -121, -119, -118, -116, -115, -114,
+  -112, -111, -109, -108, -107, -105, -104, -102, -101, -100,  -98,  -97,
+   -95,  -94,  -93,  -91,  -90,  -88,  -87,  -86,  -84,  -83,  -81,  -80,
+   -79,  -77,  -76,  -74,  -73,  -72,  -70,  -69,  -67,  -66,  -64,  -63,
+   -62,  -60,  -59,  -57,  -56,  -55,  -53,  -52,  -50,  -49,  -48,  -46,
+   -45,  -43,  -42,  -41,  -39,  -38,  -36,  -35,  -34,  -32,  -31,  -29,
+   -28,  -27,  -25,  -24,  -22,  -21,  -20,  -18,  -17,  -15,  -14,  -13,
+   -11,  -10,   -8,   -7,   -6,   -4,   -3,   -1,    0,    1,    3,    4,
+     6,    7,    8,   10,   11,   13,   14,   15,   17,   18,   20,   21,
+    22,   24,   25,   27,   28,   29,   31,   32,   34,   35,   36,   38,
+    39,   41,   42,   43,   45,   46,   48,   49,   50,   52,   53,   55,
+    56,   57,   59,   60,   62,   63,   64,   66,   67,   69,   70,   72,
+    73,   74,   76,   77,   79,   80,   81,   83,   84,   86,   87,   88,
+    90,   91,   93,   94,   95,   97,   98,  100,  101,  102,  104,  105,
+   107,  108,  109,  111,  112,  114,  115,  116,  118,  119,  121,  122,
+   123,  125,  126,  128,  129,  130,  132,  133,  135,  136,  137,  139,
+   140,  142,  143,  144,  146,  147,  149,  150,  151,  153,  154,  156,
+   157,  158,  160,  161,  163,  164,  165,  167,  168,  170,  171,  172,
+   174,  175,  177,  178
+};
+    
+    
+    {}  // namespace guetzli
+
+    
+    // Butteraugli scores that correspond to JPEG quality levels, starting at
+// kLowestQuality. They were computed by taking median BA scores of JPEGs
+// generated using libjpeg-turbo at given quality from a set of PNGs.
+// The scores above quality level 100 are just linearly decreased so that score
+// for 110 is 90% of the score for 100.
+const double kScoreForQuality[] = {
+  2.810761,  // 70
+  2.729300,
+  2.689687,
+  2.636811,
+  2.547863,
+  2.525400,
+  2.473416,
+  2.366133,
+  2.338078,
+  2.318654,
+  2.201674,  // 80
+  2.145517,
+  2.087322,
+  2.009328,
+  1.945456,
+  1.900112,
+  1.805701,
+  1.750194,
+  1.644175,
+  1.562165,
+  1.473608,  // 90
+  1.382021,
+  1.294298,
+  1.185402,
+  1.066781,
+  0.971769,  // 95
+  0.852901,
+  0.724544,
+  0.611302,
+  0.443185,
+  0.211578,  // 100
+  0.209462,
+  0.207346,
+  0.205230,
+  0.203114,
+  0.200999,  // 105
+  0.198883,
+  0.196767,
+  0.194651,
+  0.192535,
+  0.190420,  // 110
+  0.190420,
+};
+    
+    void DebugHUD_DoInterface(DebugHUD *hud)
+{
+    // 1. Show a simple window.
+    // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called 'Debug'.
+    {
+        static float f = 0.0f;
+        static int counter = 0;
+        ImGui::Text('Hello, world!');                           // Display some text (you can use a format string too)
+        ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+        ImGui::ColorEdit3('clear color', hud->clearColor);      // Edit 3 floats representing a color
     }
-    for (auto &W : InterestingWords)
-      MD.AddWordToAutoDictionary({W});
+    }
+    
+        for (int n = 0; n < 50; n++)
+    {
+        printf('NewFrame() %d\n', n);
+        io.DisplaySize = ImVec2(1920, 1080);
+        io.DeltaTime = 1.0f / 60.0f;
+        ImGui::NewFrame();
+    }
+    
+    #include 'imgui.h'
+#include 'imgui_impl_dx9.h'
+    
+    static VkCommandBuffer        g_CommandBuffer = VK_NULL_HANDLE;
+static VkDeviceSize           g_BufferMemoryAlignment = 256;
+static VkPipelineCreateFlags  g_PipelineCreateFlags = 0;
+static int                    g_FrameIndex = 0;
+    
+    void ListFilesInDirRecursive(const std::string &Dir, long *Epoch,
+                             std::vector<std::string> *V, bool TopDir) {
+  auto E = GetEpoch(Dir);
+  if (Epoch)
+    if (E && *Epoch >= E) return;
+    }
+    
+    size_t MutationDispatcher::Mutate_ChangeBinaryInteger(uint8_t *Data,
+                                                      size_t Size,
+                                                      size_t MaxSize) {
+  if (Size > MaxSize) return 0;
+  switch (Rand(4)) {
+    case 3: return ChangeBinaryInteger<uint64_t>(Data, Size, Rand);
+    case 2: return ChangeBinaryInteger<uint32_t>(Data, Size, Rand);
+    case 1: return ChangeBinaryInteger<uint16_t>(Data, Size, Rand);
+    case 0: return ChangeBinaryInteger<uint8_t>(Data, Size, Rand);
+    default: assert(0);
   }
-    
-    namespace aria2 {
-    }
-    
-      void setUserDefinedCred(std::string user, std::string password);
-    
-      virtual void openFile(int64_t totalLength = 0) CXX11_OVERRIDE;
-    
-    std::string AbstractOptionHandler::toTagString() const
-{
-  std::string s;
-  for (int i = 0; i < MAX_HELP_TAG; ++i) {
-    if (tags_ & (1 << i)) {
-      s += strHelpTag(i);
-      s += ', ';
-    }
-  }
-  if (!s.empty()) {
-    s.resize(s.size() - 2);
-  }
-  return s;
+  return 0;
 }
     
-    AbstractProxyRequestCommand::AbstractProxyRequestCommand(
-    cuid_t cuid, const std::shared_ptr<Request>& req,
-    const std::shared_ptr<FileEntry>& fileEntry, RequestGroup* requestGroup,
-    DownloadEngine* e, const std::shared_ptr<Request>& proxyRequest,
-    const std::shared_ptr<SocketCore>& s)
-    : AbstractCommand(cuid, req, fileEntry, requestGroup, e, s),
-      proxyRequest_(proxyRequest),
-      httpConnection_(std::make_shared<HttpConnection>(
-          cuid, s, std::make_shared<SocketRecvBuffer>(s)))
-{
-  setTimeout(std::chrono::seconds(getOption()->getAsInt(PREF_CONNECT_TIMEOUT)));
-  disableReadCheckSocket();
-  setWriteCheckSocket(getSocket());
-}
-    
-    void AnnounceList::moveToStoppedAllowedTier()
-{
-  auto itr = find_wrap_if(std::begin(tiers_), std::end(tiers_), currentTier_,
-                          FindStoppedAllowedTier());
-  setCurrentTier(std::move(itr));
-}
-    
-      void nextEvent();
-    
-    // DiskwriterFactory class template to create DiskWriter derived
-// object, ignoring filename.
-template <class DiskWriterType>
-class AnonDiskWriterFactory : public DiskWriterFactory {
-public:
-  AnonDiskWriterFactory() = default;
-  virtual ~AnonDiskWriterFactory() = default;
-    }
-    
-    AuthConfig::AuthConfig(std::string user, std::string password)
-    : user_(std::move(user)), password_(std::move(password))
-{
-}
+    struct FuzzingOptions {
+  int Verbosity = 1;
+  size_t MaxLen = 0;
+  int UnitTimeoutSec = 300;
+  int TimeoutExitCode = 77;
+  int ErrorExitCode = 77;
+  int MaxTotalTimeSec = 0;
+  int RssLimitMb = 0;
+  bool DoCrossOver = true;
+  int MutateDepth = 5;
+  bool UseCounters = false;
+  bool UseIndirCalls = true;
+  bool UseMemcmp = true;
+  bool UseMemmem = true;
+  bool UseCmp = false;
+  bool UseValueProfile = false;
+  bool Shrink = false;
+  int ReloadIntervalSec = 1;
+  bool ShuffleAtStartUp = true;
+  bool PreferSmall = true;
+  size_t MaxNumberOfRuns = -1L;
+  int ReportSlowUnits = 10;
+  bool OnlyASCII = false;
+  std::string OutputCorpus;
+  std::string ArtifactPrefix = './';
+  std::string ExactArtifactPath;
+  std::string ExitOnSrcPos;
+  std::string ExitOnItem;
+  bool SaveArtifacts = true;
+  bool PrintNEW = true; // Print a status line when new units are found;
+  bool OutputCSV = false;
+  bool PrintNewCovPcs = false;
+  bool PrintFinalStats = false;
+  bool PrintCorpusStats = false;
+  bool PrintCoverage = false;
+  bool DumpCoverage = false;
+  bool DetectLeaks = true;
+  int  TraceMalloc = 0;
+  bool HandleAbrt = false;
+  bool HandleBus = false;
+  bool HandleFpe = false;
+  bool HandleIll = false;
+  bool HandleInt = false;
+  bool HandleSegv = false;
+  bool HandleTerm = false;
+};
