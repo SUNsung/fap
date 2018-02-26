b48c0780e29ca8332c5c69c5569fde8eccbbcc0c
SUNsung <weatherius@gmail.com>
2018-02-26 11:43:17 +0200

    Rewrite [284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py] File in: 2018-02-26 11:43:17

--- pyton/284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py
+++ pyton/284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py
@@ -1,155 +1,86 @@
 
         
-            # noinspection PyProtectedMember
-    @property
-    def headers(self):
-        original = self._orig.raw._original_response
+        error_msg = None
     
-        def __init__(self, groups, env=Environment(), **kwargs):
-        '''
-        :param groups: names of processor groups to be applied
-        :param env: Environment
-        :param kwargs: additional keyword arguments for processors
     
-        # Will be set to the raw value of `-a` (if provided) before
-    # `get_auth()` gets called.
-    raw_auth = None
+class Downloader(object):
     
-        def test_verify_custom_ca_bundle_invalid_path(self, httpbin_secure):
-        # since 2.14.0 requests raises IOError
-        with pytest.raises((SSLError, IOError)):
-            http(httpbin_secure.url + '/get', '--verify', '/__not_found__')
+        def get_formatters_grouped(self):
+        groups = {}
+        for group_name, group in groupby(
+                self.get_formatters(),
+                key=lambda p: getattr(p, 'group_name', 'format')):
+            groups[group_name] = list(group)
+        return groups
     
-            if response.status_code != 200:
-            path += '.error'
+    CIFAR_FILENAME = 'cifar-10-python.tar.gz'
+CIFAR_DOWNLOAD_URL = 'https://www.cs.toronto.edu/~kriz/' + CIFAR_FILENAME
+CIFAR_LOCAL_FOLDER = 'cifar-10-batches-py'
     
-        target.add_argument('--run',
-                        metavar='ID',
-                        help='Shippable run ID')
+        model = cifar10_main.Cifar10Model(
+        32, data_format='channels_last', num_classes=num_classes)
+    fake_input = tf.random_uniform([batch_size, _HEIGHT, _WIDTH, _NUM_CHANNELS])
+    output = model(fake_input, training=True)
+    
+            # Extract and save features.
+        (locations_out, descriptors_out, feature_scales_out,
+         attention_out) = sess.run(
+             [locations, descriptors, feature_scales, attention],
+             feed_dict={
+                 input_image:
+                     im,
+                 input_score_threshold:
+                     config.delf_local_config.score_threshold,
+                 input_image_scales:
+                     list(config.image_scales),
+                 input_max_feature_num:
+                     config.delf_local_config.max_feature_num
+             })
+    
+      Note that this should eventually be handled by replicate_model_fn
+  directly. Multi-GPU support is currently experimental, however,
+  so doing the work here until that feature is in place.
+  '''
+  from tensorflow.python.client import device_lib
     
       def setUp(self):
-    self._string_data = '\n'.join(
-        [' hello there i am',
-         ' rain as day',
-         ' want some cheesy puffs ?'])
+    FLAGS.train_data = os.path.join(self.get_temp_dir(), 'test-text.txt')
+    FLAGS.eval_data = os.path.join(self.get_temp_dir(), 'eval-text.txt')
+    FLAGS.save_path = self.get_temp_dir()
+    with open(FLAGS.train_data, 'w') as f:
+      f.write(
+          '''alice was beginning to get very tired of sitting by her sister on
+          the bank, and of having nothing to do: once or twice she had peeped
+          into the book her sister was reading, but it had no pictures or
+          conversations in it, 'and what is the use of a book,' thought alice
+          'without pictures or conversations?' So she was considering in her own
+          mind (as well as she could, for the hot day made her feel very sleepy
+          and stupid), whether the pleasure of making a daisy-chain would be
+          worth the trouble of getting up and picking the daisies, when suddenly
+          a White rabbit with pink eyes ran close by her.\n''')
+      with open(FLAGS.eval_data, 'w') as f:
+        f.write('alice she rabbit once\n')
     
-    py_library(
-    name = 'cifar10_input',
-    srcs = ['cifar10_input.py'],
-    srcs_version = 'PY2AND3',
-    visibility = ['//tensorflow:internal'],
-    deps = [
-        '//tensorflow:tensorflow_py',
-    ],
-)
+          for i in range(3):
+        key, label, uint8image = sess.run([
+            result.key, result.label, result.uint8image])
+        self.assertEqual('%s:%d' % (filename, i), tf.compat.as_text(key))
+        self.assertEqual(labels[i], label)
+        self.assertAllEqual(expected[i], uint8image)
+    
+        if not all((board_x, board_y)):
+        return 0, 0, 0, 0
     
     
-def self_test():
-  '''Test the translation model.'''
-  with tf.Session() as sess:
-    print('Self-test for neural translation model.')
-    # Create model with vocabularies of 10, 2 small buckets, 2 layers of 32.
-    model = seq2seq_model.Seq2SeqModel(10, 10, [(3, 3), (6, 6)], 32, 2,
-                                       5.0, 32, 0.3, 0.99, num_samples=8)
-    sess.run(tf.global_variables_initializer())
-    
-      Returns:
-    A tuple of 6 elements:
-      (1) path to the token-ids for English training data-set,
-      (2) path to the token-ids for French training data-set,
-      (3) path to the token-ids for English development data-set,
-      (4) path to the token-ids for French development data-set,
-      (5) path to the English vocabulary file,
-      (6) path to the French vocabulary file.
-  '''
-  # Get wmt data to the specified directory.
-  train_path = get_wmt_enfr_train_set(data_dir)
-  dev_path = get_wmt_enfr_dev_set(data_dir)
-    
-    # Check minimum required Python version
-import sys
-if sys.version_info < (2, 7):
-    print('Scrapy %s requires Python 2.7' % __version__)
-    sys.exit(1)
-    
-        def run(self, args, opts):
-        '''
-        Entry point for running commands
-        '''
-        raise NotImplementedError
-
-    
-            if infos:
-            writeln(' (%s)' % (', '.join(infos),))
-        else:
-            write('\n')
-    
-        if C is not None:
-        # C version of decimal
-        start_calc = time.time()
-        x = factorial(C.Decimal(n), 0)
-        end_calc = time.time()
-        start_conv = time.time()
-        sx = str(x)
-        end_conv = time.time()
-        print('cdecimal:')
-        print('calculation time: %fs' % (end_calc-start_calc))
-        print('conversion time: %fs\n' % (end_conv-start_conv))
-    
-        def get_message(self, key):
-        '''Return a Message representation or raise a KeyError.'''
-        subpath = self._lookup(key)
-        with open(os.path.join(self._path, subpath), 'rb') as f:
-            if self._factory:
-                msg = self._factory(f)
-            else:
-                msg = MaildirMessage(f)
-        subdir, name = os.path.split(subpath)
-        msg.set_subdir(subdir)
-        if self.colon in name:
-            msg.set_info(name.split(self.colon)[-1])
-        msg.set_date(os.path.getmtime(os.path.join(self._path, subpath)))
-        return msg
-    
-        def parseURI(self, uri):
-        if self.entityResolver:
-            input = self.entityResolver.resolveEntity(None, uri)
-        else:
-            input = DOMEntityResolver().resolveEntity(None, uri)
-        return self.parse(input)
-    
-        # We only ever write one 'entry point' symbol - either
-    # 'main' or 'WinMain'.  Therefore, there is no need to
-    # pass a subsystem switch to the linker as it works it
-    # out all by itself.  However, the subsystem _does_ determine
-    # the file extension and additional linker flags.
-    target_link_flags = ''
-    target_ext = '.exe'
-    if subsystem_details[vars['subsystem']][2]:
-        target_link_flags = '-dll'
-        target_ext = '.dll'
-    
-        def test_range_iterators_invocation(self):
-        # verify range iterators instances cannot be created by
-        # calling their type
-        rangeiter_type = type(iter(range(0)))
-        self.assertRaises(TypeError, rangeiter_type, 1, 3, 1)
-        long_rangeiter_type = type(iter(range(1 << 1000)))
-        self.assertRaises(TypeError, long_rangeiter_type, 1, 3, 1)
-    
-    api('trophy', TrophyJsonTemplate)
-api('rules', RulesJsonTemplate)
-    
-        allowed_render_styles = ('html', 'xml', 'js', 'embed', '', 'compact', 'api')
-    # List of admins to blame (skip the first admin, 'reddit')
-    # If list is empty, just blame 'an admin'
-    admins = g.admins[1:] or ['an admin']
-    def __before__(self):
-        try:
-            c.error_page = True
-            RedditController.__before__(self)
-        except (HTTPMovedPermanently, HTTPFound):
-            # ignore an attempt to redirect from an error page
-            pass
-        except Exception as e:
-            handle_awful_failure('ErrorController.__before__: %r' % e)
+def check_screenshot():  # 检查获取截图的方式
+    global screenshot_way
+    if (screenshot_way < 0):
+        print('暂不支持当前设备')
+        sys.exit()
+    binary_screenshot = pull_screenshot()
+    try:
+        Image.open(BytesIO(binary_screenshot)).load()  # 直接使用内存IO
+        print('Capture Method: {}'.format(screenshot_way))
+    except Exception:
+        screenshot_way -= 1
+        check_screenshot()
