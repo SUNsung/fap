905b649db7be5e886c4b28d3ec6892b3143fea7e
SUNsung <weatherius@gmail.com>
2018-02-26 22:17:58 +0200

    Rewrite [45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb] File in: 2018-02-26 22:17:58

--- ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
+++ ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
@@ -1,109 +1,140 @@
 
         
-        module Homebrew
-  def build_env_keys(env)
-    %w[
-      CC CXX LD OBJC OBJCXX
-      HOMEBREW_CC HOMEBREW_CXX
-      CFLAGS CXXFLAGS CPPFLAGS LDFLAGS SDKROOT MAKEFLAGS
-      CMAKE_PREFIX_PATH CMAKE_INCLUDE_PATH CMAKE_LIBRARY_PATH CMAKE_FRAMEWORK_PATH
-      MACOSX_DEPLOYMENT_TARGET PKG_CONFIG_PATH PKG_CONFIG_LIBDIR
-      HOMEBREW_DEBUG HOMEBREW_MAKE_JOBS HOMEBREW_VERBOSE
-      HOMEBREW_SVN HOMEBREW_GIT
-      HOMEBREW_SDKROOT HOMEBREW_BUILD_FROM_SOURCE
-      MAKE GIT CPP
-      ACLOCAL_PATH PATH CPATH].select { |key| env.key?(key) }
+            attr_accessor :crashlytics_path
+    attr_accessor :api_key
+    attr_accessor :build_secret
+    attr_accessor :emails
+    attr_accessor :groups
+    attr_accessor :schemes
+    attr_accessor :export_method
+    
+      def failure
+    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
+    redirect_to after_omniauth_failure_path_for(resource_name)
   end
     
-          raise Discourse::InvalidParameters.new(:to_address) unless args[:to_address].present?
+    source 'https://rubygems.org'
     
-      describe 'instance methods' do
-    let(:key_attributes) do # these keys are intentionally strings.
-      {
-        'canDownload' => false,
-        'canRevoke' => true,
-        'keyId' => 'some-key-id',
-        'keyName' => 'fastlane',
-        'servicesCount' => 3,
-        'services' => [
-          {
-            'name' => 'APNS',
-            'id' => 'U27F4V844T',
-            'configurations' => []
-          },
-          {
-            'name' => 'MusicKit',
-            'id' => '6A7HVUVQ3M',
-            'configurations' => [
-              {
-                'name' => 'music id test',
-                'identifier' => 'music.com.snatchev.test',
-                'type' => 'music',
-                'prefix' => 'some-prefix-id',
-                'id' => 'some-music-kit-id'
-              }
-            ]
-          },
-          {
-            'name' => 'DeviceCheck',
-            'id' => 'DQ8HTZ7739',
-            'configurations' => []
-          }
-        ]
-      }
-    end
+      # GET /resource/password/new
+  def new
+    self.resource = resource_class.new
+  end
     
-          # Run a certain action
-      def trigger(command: nil, serial: nil)
-        android_serial = serial != '' ? 'ANDROID_SERIAL=#{serial}' : nil
-        command = [android_serial, adb_path, command].join(' ')
-        Action.sh(command)
+      # GET /resource/unlock?unlock_token=abcdef
+  def show
+    self.resource = resource_class.unlock_access_by_token(params[:unlock_token])
+    yield resource if block_given?
+    
+          # Remembers the given resource by setting up a cookie
+      def remember_me(resource)
+        return if request.env['devise.skip_storage']
+        scope = Devise::Mapping.find_scope!(resource)
+        resource.remember_me!
+        cookies.signed[remember_key(resource, scope)] = remember_cookie_values(resource)
       end
     
-            command = [escaped_gradle_path, 'tasks', '--console=plain'].join(' ')
-        output = Action.sh(command, print_command: false, print_command_output: false)
-        output.split('\n').each do |line|
-          if (result = line.match(/(\w+)\s\-\s([\w\s]+)/))
-            self.tasks << GradleTask.new(title: result[1], description: result[2])
-          end
+            # This should return the state of the machine within this provider.
+        # The state must be an instance of {MachineState}. Please read the
+        # documentation of that class for more information.
+        #
+        # @return [MachineState]
+        def state
+          nil
         end
     
-          it 'does set the source directory' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            cloc(source_directory: 'MyCoolApp')
-          end').runner.execute(:test)
+          class << self
+        # Mark a given block of code as a 'busy' block of code, which will
+        # register a SIGINT handler for the duration of the block. When a
+        # SIGINT occurs, the `sig_callback` proc will be called. It is up
+        # to the callback to behave properly and exit the application.
+        def busy(sig_callback)
+          register(sig_callback)
+          return yield
+        ensure
+          unregister(sig_callback)
+        end
     
-          it 'gets the correct version number for 'TargetBTests'' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          get_version_number(xcodeproj: '.xcproject', target: 'TargetBTests')
-        end').runner.execute(:test)
-        expect(result).to eq('5.4.3')
+          # Renders a template, handling the template as a string, but otherwise
+      # acting the same way as {#render}.
+      #
+      # @return [String]
+      def render_string
+        Erubis::Eruby.new(template, trim: true).result(binding)
       end
     
-    desc 'Generate jekyll site'
-task :generate do
-  raise '### You haven't set anything up yet. First run `rake install` to set up an Octopress theme.' unless File.directory?(source_dir)
-  puts '## Generating Site with Jekyll'
-  system 'compass compile --css-dir #{source_dir}/stylesheets'
-  system 'jekyll build'
-end
+    Print a list of currently running Applications and associated
+Bundle IDs, which may be useful in a Cask uninstall stanza, eg
     
-    module Jekyll
+      // writing
+  $('form').on('submit',function(e) {
+    $.post('/', {msg: '<%= user %>: ' + $('#msg').val()});
+    $('#msg').val(''); $('#msg').focus();
+    e.preventDefault();
+  });
+</script>
     
-          if File.symlink?(includes_dir)
-        return 'Includes directory '#{includes_dir}' cannot be a symlink'
+          def random_string(secure = defined? SecureRandom)
+        secure ? SecureRandom.hex(16) : '%032x' % rand(2**128-1)
+      rescue NotImplementedError
+        random_string false
       end
     
-    require 'pathname'
-require './plugins/octopress_filters'
-    
-        def initialize(tag_name, markup, tokens)
-      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
-      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
-      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
-      super
+      describe '#referrer' do
+    it 'Reads referrer from Referer header' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
+      expect(subject.referrer(env)).to eq('bar.com')
     end
     
-            def update
-          authorize! :update, @order, order_token
-          @address = find_address
+        it 'leaves normal params untouched' do
+      mock_app do |env|
+        request = Rack::Request.new(env)
+        [200, {'Content-Type' => 'text/plain'}, [request.params['foo']]]
+      end
+      get '/', :foo => 'bar'
+      expect(body).to eq('bar')
+    end
+    
+    
+  it 'should set the X-Content-Type-Options for other content types' do
+    expect(get('/', {}, 'wants' => 'application/foo').header['X-Content-Type-Options']).to eq('nosniff')
+  end
+    
+    When /^(?:|I )follow '([^']*)'$/ do |link|
+  click_link(link)
+end
+    
+        # Extracts the Geometry from a file (or path to a file)
+    def self.from_file(file)
+      GeometryDetector.new(file).make
+    end
+    
+        def define_getters
+      define_instance_getter
+      define_class_getter
+    end
+    
+          original_extension = extension(attachment, style_name)
+      style = attachment.styles[style_name.to_s.to_sym]
+      if style && style[:format]
+        style[:format].to_s
+      elsif extensions_for_mime_type.include? original_extension
+        original_extension
+      elsif !extensions_for_mime_type.empty?
+        extensions_for_mime_type.first
+      else
+        # It's possible, though unlikely, that the mime type is not in the
+        # database, so just use the part after the '/' in the mime type as the
+        # extension.
+        %r{/([^/]*)\z}.match(attachment.content_type)[1]
+      end
+    end
+    
+            def error_when_not_valid?
+          @subject.send(@attachment_name).assign(nil)
+          @subject.valid?
+          @subject.errors[:'#{@attachment_name}'].present?
+        end
+    
+            raise LoadError, 'Could not find the '#{name}' processor in any of these paths: #{directories.join(', ')}' unless required.any?
+      end
+    end
