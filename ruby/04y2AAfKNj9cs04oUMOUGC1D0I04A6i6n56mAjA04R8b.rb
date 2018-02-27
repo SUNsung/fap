f95f7ace975b683ba020f5d8a7c38f28b148d205
SUNsung <weatherius@gmail.com>
2018-02-28 00:52:28 +0200

    Rewrite [04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb] File in: 2018-02-28 00:52:27

--- ruby/04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb
+++ ruby/04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb
@@ -1,25 +1,56 @@
-  def evernote_consumer_key
-    (config = Devise.omniauth_configs[:evernote]) && config.strategy.consumer_key
+
+        
+          def app_caveats
+    if keg && keg.app_installed?
+      <<-EOS.undent
+        .app bundles were installed.
+        Run `brew linkapps #{keg.name}` to symlink these to /Applications.
+      EOS
+    end
   end
     
-          if respond_to?(:stop)
-        stop
-      else
-        terminate_thread!
+            next unless HOMEBREW_CELLAR.directory?
+    
+      def query_regexp(query)
+    case query
+    when %r{^/(.*)/$} then Regexp.new($1)
+    else /.*#{Regexp.escape(query)}.*/i
+    end
+  rescue RegexpError
+    odie '#{query} is not a valid regex'
+  end
+    
+    # This formula serves as the base class for several very similar
+# formulae for Amazon Web Services related tools.
+class AmazonWebServicesFormula < Formula
+  # Use this method to peform a standard install for Java-based tools,
+  # keeping the .jars out of HOMEBREW_PREFIX/lib
+  def install
+    rm Dir['bin/*.cmd'] # Remove Windows versions
+    libexec.install Dir['*']
+    bin.install_symlink Dir['#{libexec}/bin/*'] - ['#{libexec}/bin/service']
+  end
+  alias_method :standard_install, :install
+    
+            commands
+      end
+    
+        class AdbHelper
+      # Path to the adb binary
+      attr_accessor :adb_path
+    
+          scripts = target.build_phases.select { |t| t.class == Xcodeproj::Project::Object::PBXShellScriptBuildPhase }
+      crash_script = scripts.find { |s| includes_run_script?(s.shell_script) }
+    
+            expect(result).to eq('svn info | grep Revision | egrep -o '[0-9]+'')
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('svn info | grep Revision | egrep -o '[0-9]+'')
       end
     end
     
-      def index
-    @jobs = Delayed::Job.order('coalesce(failed_at,'1000-01-01'), run_at asc').page(params[:page])
+          it 'pass a custom build number to the tool' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          increment_build_number(build_number: 24, xcodeproj: '.xcproject')
+        end').runner.execute(:test)
     
-          respond_to do |format|
-        if new_credentials.map(&:save).all?
-          format.html { redirect_to user_credentials_path, notice: 'The file was successfully uploaded.'}
-        else
-          format.html { redirect_to user_credentials_path, notice: 'One or more of the uploaded credentials was not imported due to an error. Perhaps an existing credential had the same name?'}
-        end
-      end
-    else
-      redirect_to user_credentials_path, notice: 'No file was chosen to be uploaded.' 
-    end
+        find_union(segments, Project).includes(:namespace).order_id_desc
   end
