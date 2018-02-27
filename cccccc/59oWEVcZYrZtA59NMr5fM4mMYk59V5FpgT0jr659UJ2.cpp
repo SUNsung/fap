8ef1fe9c5070153a60f265f2c7a05a31f72e96ba
SUNsung <weatherius@gmail.com>
2018-02-27 21:33:45 +0200

    Rewrite [59oWEVcZYrZtA59NMr5fM4mMYk59V5FpgT0jr659UJ2.cpp] File in: 2018-02-27 21:33:44

--- cccccc/59oWEVcZYrZtA59NMr5fM4mMYk59V5FpgT0jr659UJ2.cpp
+++ cccccc/59oWEVcZYrZtA59NMr5fM4mMYk59V5FpgT0jr659UJ2.cpp
@@ -1,174 +1,312 @@
 
         
-        void HHVM_FUNCTION(fb_setprofile,
-  const Variant& callback,
-  int64_t flags = EventHook::ProfileDefault
-) {
-  if (ThreadInfo::s_threadInfo->m_profiler != nullptr) {
-    // phpprof is enabled, don't let PHP code override it
-    return;
+        void PartialRunMgr::ExecutorDone(int step_id, const Status& executor_status) {
+  StatusCallback done;
+  Status callback_status;
+  {
+    mutex_lock l(mu_);
+    auto run_it = step_id_to_partial_run_.find(step_id);
+    if (run_it == step_id_to_partial_run_.end()) {
+      return;
+    }
+    // If we found the partial_run, we call the final callback, if it
+    // exists.
+    // It is guaranteed that run_it->second->final_callback is left empty
+    // after the std::move call.
+    done = std::move(run_it->second->final_callback);
+    if (!executor_status.ok()) {
+      run_it->second->final_status = executor_status;
+    }
+    callback_status = run_it->second->final_status;
+    run_it->second->executor_done = true;
   }
-  g_context->m_setprofileCallback = callback;
-  g_context->m_setprofileFlags = flags;
-  if (callback.isNull()) {
-    HPHP::EventHook::Disable();
-  } else {
-    HPHP::EventHook::Enable();
+  if (done != nullptr) {
+    done(callback_status);
+    mutex_lock l(mu_);
+    step_id_to_partial_run_.erase(step_id);
   }
 }
     
-    __thread int64_t s_extra_request_nanoseconds;
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    #ifdef USE_GCC_FAST_TLS
+    #ifndef TENSORFLOW_GRAPPLER_OPTIMIZERS_MODEL_PRUNER_H_
+#define TENSORFLOW_GRAPPLER_OPTIMIZERS_MODEL_PRUNER_H_
     
-    #endif
+    namespace tensorflow {
+    }
+    
+    void SubProcess::SetProgram(const string& file,
+                            const std::vector<string>& argv) {
+  mutex_lock procLock(proc_mu_);
+  mutex_lock dataLock(data_mu_);
+  if (running_) {
+    LOG(FATAL) << 'SetProgram called after the process was started.';
+    return;
+  }
+    }
+    
+    // Rewrite graph by replacing the output tensors specified in
+// 'fed_outputs' with special feed nodes for each specified output
+// tensor, and removing any nodes that are now disconnected from the
+// part of the graph that reaches the sink node.  The set of special
+// feed nodes added to the graph are returned in '*feed_nodes'.
+//
+// Return true on success.  On error, return false and sets *error to
+// an appropriate error message (and *g is left in an indeterminate
+// state).
+static Status FeedInputs(Graph* g, const DeviceAttributes& device_info,
+                         const gtl::ArraySlice<string>& fed_outputs,
+                         bool use_function_convention,
+                         subgraph::NameIndex* name_index,
+                         DataTypeVector* out_feed_types) {
+  out_feed_types->clear();
+  out_feed_types->reserve(fed_outputs.size());
+  for (size_t i = 0; i < fed_outputs.size(); ++i) {
+    const string& t = fed_outputs[i];
+    TensorId id(ParseTensorName(t));
+    }
+    }
+    
+    #ifndef CONTENT_NW_SRC_API_APP_APP_H_
+#define CONTENT_NW_SRC_API_APP_APP_H_
+    
+    #endif  // CONTENT_NW_SRC_API_BASE_BASE_H_
+
+    
+      v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  v8::HandleScope handleScope(isolate);
+    
+      template<typename T> T* AddListener() {
+    std::map<int, BaseEvent*>::iterator i = listerners_.find(T::id);
+    if (i==listerners_.end()) {
+      T* listener_object = new T(this);
+      listerners_[T::id] = listener_object;
+      return listener_object;
+    }
+    return NULL;
+  }
+    
+    void MenuItem::OnClick(GtkWidget* widget) {
+  if (block_active_)
+    return;
+    }
+    
+      // Bring servers back up on the same port (we aren't recreating the channel).
+  StartServers(kNumServers, ports);
+    
+      ServerConfig server_config;
+  server_config.set_server_type(ASYNC_SERVER);
+  server_config.set_async_server_threads(8);
+    
+            // As part of the initial CREATE state, we *request* that the system
+        // start processing SayHello requests. In this request, 'this' acts are
+        // the tag uniquely identifying the request (so that different CallData
+        // instances can serve different requests concurrently), in this case
+        // the memory address of this CallData instance.
+        service_->RequestSayHello(&ctx_, &request_, &responder_, cq_, cq_,
+                                  this);
+      } else if (status_ == PROCESS) {
+        // Spawn a new CallData instance to serve new clients while we process
+        // the one for this CallData. The instance will deallocate itself as
+        // part of its FINISH state.
+        new CallData(service_, cq_);
+    
+    
+    {  return 0;
+}
+
+    
+    #ifdef BAZEL_BUILD
+#include 'examples/protos/helloworld.grpc.pb.h'
+#else
+#include 'helloworld.grpc.pb.h'
+#endif
+    
+    std::string GetDbFileContent(int argc, char** argv);
+    
+    // Data pertaining to configuration of the generator with respect to anything
+// that may be used internally at Google.
+struct GeneratorConfiguration {
+  GeneratorConfiguration();
+  grpc::string grpc_package_root;
+  // TODO(https://github.com/grpc/grpc/issues/8622): Drop this.
+  grpc::string beta_package_root;
+  // TODO(https://github.com/google/protobuf/issues/888): Drop this.
+  grpc::string import_prefix;
+};
+    
+      NSDictionary *flagNames = @{
+#define GRPC_XMACRO_ITEM(methodName, FlagName) \
+    @(kSCNetworkReachabilityFlags ## FlagName): @#methodName,
+#include 'GRXReachabilityFlagNames.xmacro.h'
+#undef GRPC_XMACRO_ITEM
+  };
+    
+    void BuildDCHistograms(const JPEGData& jpg, JpegHistogram* histo) {
+  for (size_t i = 0; i < jpg.components.size(); ++i) {
+    const JPEGComponent& c = jpg.components[i];
+    JpegHistogram* dc_histogram = &histo[i];
+    coeff_t last_dc_coeff = 0;
+    for (int mcu_y = 0; mcu_y < jpg.MCU_rows; ++mcu_y) {
+      for (int mcu_x = 0; mcu_x < jpg.MCU_cols; ++mcu_x) {
+        for (int iy = 0; iy < c.v_samp_factor; ++iy) {
+          for (int ix = 0; ix < c.h_samp_factor; ++ix) {
+            int block_y = mcu_y * c.v_samp_factor + iy;
+            int block_x = mcu_x * c.h_samp_factor + ix;
+            int block_idx = block_y * c.width_in_blocks + block_x;
+            coeff_t dc_coeff = c.coeffs[block_idx << 6];
+            int diff = std::abs(dc_coeff - last_dc_coeff);
+            int nbits = Log2Floor(diff) + 1;
+            dc_histogram->Add(nbits);
+            last_dc_coeff = dc_coeff;
+          }
+        }
+      }
+    }
+  }
+}
+    
+    static const int kCbToBlueTable[256] = {
+  -227, -225, -223, -222, -220, -218, -216, -214, -213, -211, -209, -207,
+  -206, -204, -202, -200, -198, -197, -195, -193, -191, -190, -188, -186,
+  -184, -183, -181, -179, -177, -175, -174, -172, -170, -168, -167, -165,
+  -163, -161, -159, -158, -156, -154, -152, -151, -149, -147, -145, -144,
+  -142, -140, -138, -136, -135, -133, -131, -129, -128, -126, -124, -122,
+  -120, -119, -117, -115, -113, -112, -110, -108, -106, -105, -103, -101,
+   -99,  -97,  -96,  -94,  -92,  -90,  -89,  -87,  -85,  -83,  -82,  -80,
+   -78,  -76,  -74,  -73,  -71,  -69,  -67,  -66,  -64,  -62,  -60,  -58,
+   -57,  -55,  -53,  -51,  -50,  -48,  -46,  -44,  -43,  -41,  -39,  -37,
+   -35,  -34,  -32,  -30,  -28,  -27,  -25,  -23,  -21,  -19,  -18,  -16,
+   -14,  -12,  -11,   -9,   -7,   -5,   -4,   -2,    0,    2,    4,    5,
+     7,    9,   11,   12,   14,   16,   18,   19,   21,   23,   25,   27,
+    28,   30,   32,   34,   35,   37,   39,   41,   43,   44,   46,   48,
+    50,   51,   53,   55,   57,   58,   60,   62,   64,   66,   67,   69,
+    71,   73,   74,   76,   78,   80,   82,   83,   85,   87,   89,   90,
+    92,   94,   96,   97,   99,  101,  103,  105,  106,  108,  110,  112,
+   113,  115,  117,  119,  120,  122,  124,  126,  128,  129,  131,  133,
+   135,  136,  138,  140,  142,  144,  145,  147,  149,  151,  152,  154,
+   156,  158,  159,  161,  163,  165,  167,  168,  170,  172,  174,  175,
+   177,  179,  181,  183,  184,  186,  188,  190,  191,  193,  195,  197,
+   198,  200,  202,  204,  206,  207,  209,  211,  213,  214,  216,  218,
+   220,  222,  223,  225,
+};
+    
+    
+    {}  // namespace guetzli
+    
+    // Parses the jpeg stream contained in data[*pos ... len) and fills in *jpg with
+// the parsed information.
+// If mode is JPEG_READ_HEADER, it fills in only the image dimensions in *jpg.
+// Returns false if the data is not valid jpeg, or if it contains an unsupported
+// jpeg feature.
+bool ReadJpeg(const uint8_t* data, const size_t len, JpegReadMode mode,
+              JPEGData* jpg);
+// string variant
+bool ReadJpeg(const std::string& data, JpegReadMode mode,
+              JPEGData* jpg);
+    
+      // Fills in out[] array with the floating-point precision pixel view of the
+  // component.
+  // REQUIRES: factor_x() == 1 and factor_y() == 1.
+  void ToFloatPixels(float* out, int stride) const;
+    
+    template<typename DType>
+inline void CompressArray<DType>::InitCompressChunks(
+    const std::vector<bst_uint>& chunk_ptr) {
+  raw_chunks_ = chunk_ptr;
+  CHECK_GE(raw_chunks_.size(), 2);
+  out_buffer_.resize(raw_chunks_.size() - 1);
+  for (size_t i = 0; i < out_buffer_.size(); ++i) {
+    out_buffer_[i].resize(raw_chunks_[i + 1] - raw_chunks_[i]);
+  }
+}
+    
+    
+    {
+    {
+    { private:
+  /*! \brief input stream */
+  dmlc::Stream *strm_;
+  /*! \brief current buffer pointer */
+  size_t buffer_ptr_;
+  /*! \brief internal buffer */
+  std::string buffer_;
+};
+}  // namespace common
+}  // namespace xgboost
+#endif  // XGBOOST_COMMON_IO_H_
 
     
     
-    {  return ret;
-}
+    {
+    {XGBOOST_REGISTER_TREE_UPDATER(TreeRefresher, 'refresh')
+.describe('Refresher that refreshes the weight and statistics according to data.')
+.set_body([]() {
+    return new TreeRefresher<GradStats>();
+  });
+}  // namespace tree
+}  // namespace xgboost
+
     
-    static_assert(
-  kSizeIndex2PackedArrayCapacity[PackedArray::SmallSizeIndex]
-    == PackedArray::SmallSize,
-  'SmallSizeIndex must be MM size class index for array of SmallSize'
-);
-    
-    ///////////////////////////////////////////////////////////////////////////////
-    
-    
-    {private:
-  int m_timeout;
-};
-    
-    void initNuma();
-    
-      String softStr(soft, CopyString);
-  String hardStr(hard, CopyString);
-    
-      Offset defaultOff = bcOff(env) + iv.vec32()[iv.size() - 1];
-  Offset zeroOff = 0;
-  if (base <= 0 && (base + nTargets) > 0) {
-    zeroOff = bcOff(env) + iv.vec32()[0 - base];
-  } else {
-    zeroOff = defaultOff;
-  }
-    
-    FilterBlockBuilder::FilterBlockBuilder(const FilterPolicy* policy)
-    : policy_(policy) {
-}
-    
-    Iterator* TableCache::NewIterator(const ReadOptions& options,
-                                  uint64_t file_number,
-                                  uint64_t file_size,
-                                  Table** tableptr) {
-  if (tableptr != NULL) {
-    *tableptr = NULL;
-  }
+    bool DBWrapper::AddToBatch(rocksdb::WriteBatch& batch, bool del,
+                           Handle<Array> array) {
+  Handle<Array> put_pair;
+  for (uint i = 0; i < array->Length(); i++) {
+    if (del) {
+      if (!array->Get(i)->IsString()) {
+        return false;
+      }
+    }
+    }
     }
     
-      // Delete the specified 'file' from the specified 'level'.
-  void DeleteFile(int level, uint64_t file) {
-    deleted_files_.insert(std::make_pair(level, file));
+      EnvBasicTestWithParam() : env_(GetParam()) {
+    test_dir_ = test::TmpDir(env_) + '/env_basic_test';
   }
+    
+      std::string scratch;
+  scratch.resize(kGood.size() + kCorrupted.size() + 16);
+  Slice result;
+  unique_ptr<RandomAccessFile> rand_file;
+  ASSERT_OK(env_->NewRandomAccessFile(kFileName, &rand_file, soptions_));
+  ASSERT_OK(rand_file->Read(0, kGood.size(), &result, &(scratch[0])));
+  ASSERT_EQ(result.compare(kGood), 0);
+    
+    #include <map>
+#include <node.h>
     
     namespace {
-class EmptyIterator : public Iterator {
- public:
-  EmptyIterator(const Status& s) : status_(s) { }
-  virtual bool Valid() const { return false; }
-  virtual void Seek(const Slice& target) { }
-  virtual void SeekToFirst() { }
-  virtual void SeekToLast() { }
-  virtual void Next() { assert(false); }
-  virtual void Prev() { assert(false); }
-  Slice key() const { assert(false); return Slice(); }
-  Slice value() const { assert(false); return Slice(); }
-  virtual Status status() const { return status_; }
- private:
-  Status status_;
-};
-}  // namespace
-    
-    namespace folly {
-namespace fs {
-    }
+// OpenDb opens a (possibly new) rocksdb database with a StringAppendOperator
+std::shared_ptr<DB> OpenNormalDb(char delim_char) {
+  DB* db;
+  Options options;
+  options.create_if_missing = true;
+  options.merge_operator.reset(new StringAppendOperator(delim_char));
+  EXPECT_OK(DB::Open(options, kDbName, &db));
+  return std::shared_ptr<DB>(db);
+}
     }
     
-    DEFINE_bool(cp, false, 'Copy file');
-    
-          buffer.append(header);
-      auto line = msgData.subpiece(idx, end - idx);
-      buffer.append(line.data(), line.size());
-      buffer.push_back('\n');
-    
-    /**
- * A LogWriter implementation that immediately writes to a file descriptor when
- * it is invoked.
- *
- * The downside of this class is that logging I/O occurs directly in your
- * normal program threads, so that logging I/O may block or slow down normal
- * processing.
- *
- * However, one benefit of this class is that log messages are written out
- * immediately, so if your program crashes, all log messages generated before
- * the crash will have already been written, and no messages will be lost.
- */
-class ImmediateFileWriter : public LogWriter {
- public:
-  /**
-   * Construct an ImmediateFileWriter that appends to the file at the specified
-   * path.
-   */
-  explicit ImmediateFileWriter(folly::StringPiece path);
+    // Print the (list: key) out to stdout. For debugging mostly. Public for now.
+void RedisLists::Print(const std::string& key) {
+  // Extract the string data representing the list.
+  std::string data;
+  db_->Get(get_option_, key, &data);
     }
     
-    
-    {  // If this is a fatal message, flush the handlers to make sure the log
-  // message was written out, then crash.
-  if (isLogLevelFatal(message.getLevel())) {
-    auto numHandlers = db_->flushAllHandlers();
-    if (numHandlers == 0) {
-      // No log handlers were configured.
-      // Print the message to stderr, to make sure we always print the reason
-      // we are crashing somewhere.
-      auto msg = folly::to<std::string>(
-          'FATAL:',
-          message.getFileName(),
-          ':',
-          message.getLineNumber(),
-          ': ',
-          message.getMessage(),
-          '\n');
-      folly::writeFull(STDERR_FILENO, msg.data(), msg.size());
-    }
-    std::abort();
-  }
+    void JniCallback::releaseJniEnv(jboolean& attached) const {
+  JniUtil::releaseJniEnv(m_jvm, attached);
 }
     
-    namespace folly {
-    }
+      StatisticsJni::StatisticsJni(std::shared_ptr<Statistics> stats,
+      const std::set<uint32_t> ignore_histograms) : StatisticsImpl(stats, false),
+      m_ignore_histograms(ignore_histograms) {
+  }
     
-      // Parse the handler options
-  auto* options = value.get_ptr('options');
-  if (options) {
-    if (!options->isObject()) {
-      throw LogConfigParseError{to<string>(
-          'unexpected data type for \'options\' field of handler \'',
-          handlerName,
-          '\': got ',
-          dynamicTypename(*options),
-          ', expected an object')};
-    }
-    }
-    
-    /**
- * Parse a JSON configuration string.
- *
- * See the documentation in logging/docs/Config.md for a description of the
- * JSON configuration object format.
- *
- * This function uses relaxed JSON parsing, allowing C and C++ style
- * comments, as well as trailing commas.
- */
-LogConfig parseLogConfigJson(StringPiece value);
+    // @lint-ignore TXT4 T25377293 Grandfathered in
+#endif  // JAVA_ROCKSJNI_STATISTICSJNI_H_
