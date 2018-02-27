98b140f47170ec42024b371036afd9a895b2e798
SUNsung <weatherius@gmail.com>
2018-02-27 11:29:52 +0200

    Rewrite [44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp] File in: 2018-02-27 11:29:51

--- cccccc/44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp
+++ cccccc/44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp
@@ -1,230 +1,284 @@
 
         
-        
-    {  ret += ')';
-  return ret;
-}
-    
-    int64_t TimeStamp::Current() {
-  return time(0);
-}
-    
-    #include 'hphp/runtime/vm/jit/abi.h'
-#include 'hphp/runtime/vm/jit/arg-group.h'
-#include 'hphp/runtime/vm/jit/fixup.h'
-#include 'hphp/runtime/vm/jit/phys-reg-saver.h'
-#include 'hphp/runtime/vm/jit/vasm-gen.h'
-#include 'hphp/runtime/vm/jit/vasm-instr.h'
-#include 'hphp/runtime/vm/jit/vasm-reg.h'
-    
-    // Check if we should stop due to flow control, breakpoints, and signals.
-bool DebuggerProxy::checkFlowBreak(CmdInterrupt &cmd) {
-  TRACE(2, 'DebuggerProxy::checkFlowBreak\n');
-  // If there is an outstanding Ctrl-C from the client, go ahead and break now.
-  // Note: this stops any flow control command we might have in-flight.
-  if (m_signum == CmdSignal::SignalBreak) {
-    Lock lock(m_signalMutex);
-    if (m_signum == CmdSignal::SignalBreak) {
-      m_signum = CmdSignal::SignalNone;
-      m_flow.reset();
-      return true;
-    }
+          if (flags.bit (CHECK_DAWGS) &&
+    (word->best_choice->permuter () != SYSTEM_DAWG_PERM) &&
+    (word->best_choice->permuter () != FREQ_DAWG_PERM) &&
+    (word->best_choice->permuter () != USER_DAWG_PERM) &&
+    (word->best_choice->permuter () != NUMBER_PERM)) {
+    if (tessedit_adaption_debug) tprintf('word not in dawgs\n');
+    return FALSE;
   }
+    
+    // Blamer-related information to determine the source of errors.
+struct BlamerBundle {
+  static const char *IncorrectReasonName(IncorrectResultReason irr);
+  BlamerBundle() : truth_has_char_boxes_(false),
+      incorrect_result_reason_(IRR_CORRECT),
+      lattice_data_(NULL) { ClearResults(); }
+  BlamerBundle(const BlamerBundle &other) {
+    this->CopyTruth(other);
+    this->CopyResults(other);
+  }
+  ~BlamerBundle() { delete[] lattice_data_; }
     }
     
-      /**
-   * Query if a task is finished. This is non-blocking and can be called as
-   * many times as desired.
-   */
-  static int64_t TaskStatus(const Resource& task);
     
-    struct IRGS;
+    {}  // namespace tesseract.
+
     
-    Vlabel Vunit::makeBlock(AreaIndex area, uint64_t weight) {
-  auto i = blocks.size();
-  blocks.emplace_back(area, weight);
-  return Vlabel{i};
-}
-    
-    inline void initNuma() {}
-inline constexpr int next_numa_node(std::atomic_int& curr_node) { return 0; }
-inline constexpr int num_numa_nodes() { return 1; }
-inline void numa_interleave(void* start, size_t size) {}
-inline void numa_bind_to(void* start, size_t size, int node) {}
-inline constexpr bool numa_node_allowed(int node) { return true; }
-    
-    void ThriftBuffer::read(char *data, int len) {
-  auto const avail = m_pEnd - m_p;
-  if (avail < 0) not_reached();
+    // Fills in the x-height range accepted by the given unichar_id, given its
+// bounding box in the usual baseline-normalized coordinates, with some
+// initial crude x-height estimate (such as word size) and this denoting the
+// transformation that was used.
+void DENORM::XHeightRange(int unichar_id, const UNICHARSET& unicharset,
+                          const TBOX& bbox,
+                          float* min_xht, float* max_xht, float* yshift) const {
+  // Default return -- accept anything.
+  *yshift = 0.0f;
+  *min_xht = 0.0f;
+  *max_xht = MAX_FLOAT32;
     }
     
-    #include 'hphp/runtime/vm/resumable.h'
     
-    #include 'src/core/lib/compression/compression_internal.h'
-    
-    std::unique_ptr<ScenarioResult> RunScenario(
-    const grpc::testing::ClientConfig& client_config, size_t num_clients,
-    const grpc::testing::ServerConfig& server_config, size_t num_servers,
-    int warmup_seconds, int benchmark_seconds, int spawn_local_worker_count,
-    const grpc::string& qps_server_target_override,
-    const grpc::string& credential_type, bool run_inproc);
-    
-    class WorkerServiceImpl;
-    
-    // Get leading or trailing comments in a string. Comment lines start with '// '.
-// Leading detached comments are put in in front of leading comments.
-template <typename DescriptorType>
-inline grpc::string GetNodeComments(const DescriptorType* desc, bool leading) {
-  return grpc_generator::GetPrefixedComments(desc, leading, '//');
-}
-    
-    
-    {  // The data in the buffer.
-  detail::buffered_stream_storage storage_;
+    {  private:
+    float xcoord;                //2 floating coords
+    float ycoord;
 };
     
-    #if !defined(BOOST_ASIO_HAS_THREADS)
-// Nothing to include.
-#elif defined(BOOST_ASIO_HAS_STD_ATOMIC)
-# include <atomic>
-#else // defined(BOOST_ASIO_HAS_STD_ATOMIC)
-# include <boost/detail/atomic_count.hpp>
-#endif // defined(BOOST_ASIO_HAS_STD_ATOMIC)
+    namespace jit {
+    }
     
-      std::size_t check_for_completion(
-      const boost::system::error_code& ec,
-      std::size_t total_transferred)
-  {
-    return detail::adapt_completion_condition_result(
-        completion_condition_(ec, total_transferred));
+    Vreg Vunit::makeConst(Vconst vconst) {
+  auto it = constToReg.find(vconst);
+  if (it != constToReg.end()) return it->second;
+    }
+    
+      mpz_clear(gmpDataA);
+  mpz_clear(gmpDataB);
+  mpz_clear(gmpReturn);
+    
+      /**
+   * Set the URLChecker function which determines which paths this server is
+   * allowed to server.
+   *
+   * Defaults to SatelliteServerInfo::checkURL()
+   */
+  void setUrlChecker(const URLChecker& checker) {
+    m_urlChecker = checker;
   }
     
-    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
-# pragma once
-#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
-    
-    // Calls to asio_handler_allocate and asio_handler_deallocate must be made from
-// a namespace that does not contain any overloads of these functions. The
-// boost_asio_handler_alloc_helpers namespace is defined here for that purpose.
-namespace boost_asio_handler_alloc_helpers {
+    namespace HPHP {
     }
     
-    #define BOOST_ASIO_BUFFERED_HANDSHAKE_HANDLER_CHECK( \
-    handler_type, handler) \
-  \
-  typedef BOOST_ASIO_HANDLER_TYPE(handler_type, \
-      void(boost::system::error_code, std::size_t)) \
-    asio_true_handler_type; \
-  \
-  BOOST_ASIO_HANDLER_TYPE_REQUIREMENTS_ASSERT( \
-      sizeof(boost::asio::detail::two_arg_handler_test( \
-          boost::asio::detail::clvref< \
-            asio_true_handler_type>(), \
-          static_cast<const boost::system::error_code*>(0), \
-          static_cast<const std::size_t*>(0))) == 1, \
-      'BufferedHandshakeHandler type requirements not met') \
-  \
-  typedef boost::asio::detail::handler_type_requirements< \
-      sizeof( \
-        boost::asio::detail::argbyv( \
-          boost::asio::detail::clvref< \
-            asio_true_handler_type>())) + \
-      sizeof( \
-        boost::asio::detail::lvref< \
-          asio_true_handler_type>()( \
-          boost::asio::detail::lvref<const boost::system::error_code>(), \
-          boost::asio::detail::lvref<const std::size_t>()), \
-        char(0))> BOOST_ASIO_UNUSED_TYPEDEF
+    void ThriftBuffer::read(Array &data) {
+  String sdata;
+  read(sdata);
+  data = unserialize_with_no_notice(sdata).toArray();
+}
     
-    void buffer_sequence_adapter_base::init_native_buffer(
-    buffer_sequence_adapter_base::native_buffer_type& buf,
-    const boost::asio::mutable_buffer& buffer)
+    #include <limits.h>  /* for PIPE_BUF */
+    
+    
+    {  return make_map_array(
+    s_name,   String(pw.pw_name,   CopyString),
+    s_passwd, String(pw.pw_passwd, CopyString),
+    s_uid,    (int)pw.pw_uid,
+    s_gid,    (int)pw.pw_gid,
+    s_gecos,  String(pw.pw_gecos,  CopyString),
+    s_dir,    String(pw.pw_dir,    CopyString),
+    s_shell,  String(pw.pw_shell,  CopyString)
+  );
+}
+    
+    //////////////////////////////////////////////////////////////////////
+    
+    
+    {
+    {
+    {                // add to the desired node group
+                m_net->AddToNodeGroup(groupTag, cnNode);
+            }
+        }
+    }
+    
+    namespace Microsoft { namespace MSR { namespace BS {
+    }
+    }
+    }
+    
+        // FileParse - parse at the file level, can be overridden for 'section of file' behavior
+    // stringParse - file concatentated as a single string
+    void FileParse(const std::string& stringParse)
+    {
+        ConfigParameters sections(stringParse);
+        bool loadOrEditFound = false;
+    }
+    
+    // Data Writer class
+// interface for clients of the Data Writer
+// mirrors the IDataWriter interface, except the Init method is private (use the constructor)
+class DataWriter : public IDataWriter, protected Plugin
 {
-  std::memset(&buf, 0, sizeof(native_buffer_type));
-  Microsoft::WRL::ComPtr<IInspectable> insp
-    = Microsoft::WRL::Make<winrt_buffer_impl>(buffer);
-  buf = reinterpret_cast<Windows::Storage::Streams::IBuffer^>(insp.Get());
-}
-    
-    #include <boost/asio/detail/push_options.hpp>
-    
-      // Returns an aggregate distance or similarity value between the baseline
-  // image and the image in the last Compare() call (or the baseline image, if
-  // Compare() was not called yet).
-  // The interpretation of this aggregate value depends on the comparator used.
-  virtual float distmap_aggregate() const = 0;
-    
-    std::vector<float> YUV420ToRGB(const std::vector<std::vector<float> >& yuv420,
-                               const int width, const int height) {
-  std::vector<std::vector<float> > yuv;
-  yuv.push_back(yuv420[0]);
-  std::vector<float> u = Upsample2x2(yuv420[1], width, height);
-  std::vector<float> v = Upsample2x2(yuv420[2], width, height);
-  yuv.push_back(Blur(u, width, height));
-  yuv.push_back(Blur(v, width, height));
-  return YUVToRGB(yuv);
-}
-    
-    #include 'guetzli/output_image.h'
-    
-    // Decodes the parsed jpeg coefficients into an RGB image.
-// There can be only either 1 or 3 image components, in either case, an RGB
-// output image will be generated.
-// Only YUV420 and YUV444 sampling factors are supported.
-// Vector will be empty if a decoding error occurred.
-std::vector<uint8_t> DecodeJpegToRGB(const JPEGData& jpg);
-    
-    bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
-                     JPEGData* jpg) {
-  static const int quant[3 * kDCTBlockSize] = {
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-  };
-  return EncodeRGBToJpeg(rgb, w, h, quant, jpg);
-}
-    
-        printer->Print(vars, '// Generated by the gRPC C++ plugin.\n');
-    printer->Print(vars,
-                   '// If you make any local change, they will be lost.\n');
-    printer->Print(vars, '// source: $filename$\n\n');
-    
-      // Appends the given text to the generated code as well as a newline
-  // character.  Any text within {{ and }} delimeters is replaced by values
-  // previously stored in the CodeWriter by calling SetValue above.  The newline
-  // will be suppressed if the text ends with the \\ character.
-  void operator+=(std::string text);
-    
-    int main(int /*argc*/, const char * /*argv*/ []) {
-  // Build up a serialized buffer algorithmically:
-  flatbuffers::FlatBufferBuilder builder;
+    IDataWriter* m_dataWriter; // writer
     }
     
-    std::string BaseGenerator::LastNamespacePart(const Namespace &ns) {
-  if (!ns.components.empty())
-    return ns.components.back();
-  else
-    return std::string('');
+    namespace Microsoft { namespace MSR { namespace CNTK {
+    }
+    }
+    }
+    
+        bool empty() const
+    {
+#ifndef NONUMLATTICEMMI // TODO:set NUM lattice to null so as to save memory
+        if (numlattices.empty() ^ denlattices.empty())
+            RuntimeError('latticesource: numerator and denominator lattices must be either both empty or both not empty');
+#endif
+        return denlattices.empty();
+    }
+    
+    inline void ColorTransformYCbCrToRGB(uint8_t* pixel) {
+  int y  = pixel[0];
+  int cb = pixel[1];
+  int cr = pixel[2];
+  pixel[0] = kRangeLimit[y + kCrToRedTable[cr]];
+  pixel[1] = kRangeLimit[y +
+                         ((kCrToGreenTable[cr] + kCbToGreenTable[cb]) >> 16)];
+  pixel[2] = kRangeLimit[y + kCbToBlueTable[cb]];
 }
     
-      bool any_generator = false;
-  bool print_make_rules = false;
-  bool raw_binary = false;
-  bool schema_binary = false;
-  bool grpc_enabled = false;
-  std::vector<std::string> filenames;
-  std::list<std::string> include_directories_storage;
-  std::vector<const char *> include_directories;
-  std::vector<const char *> conform_include_directories;
-  std::vector<bool> generator_enabled(params_.num_generators, false);
-  size_t binary_files_from = std::numeric_limits<size_t>::max();
-  std::string conform_to_schema;
+    #include 'guetzli/entropy_encode.h'
+    
+    #include <math.h>
+    
+    // kIDCTMatrix[8*x+u] = alpha(u)*cos((2*x+1)*u*M_PI/16)*sqrt(2), with fixed 13
+// bit precision, where alpha(0) = 1/sqrt(2) and alpha(u) = 1 for u > 0.
+// Some coefficients are off by +-1 to mimick libjpeg's behaviour.
+static const int kIDCTMatrix[kDCTBlockSize] = {
+  8192,  11363,  10703,   9633,   8192,   6437,   4433,   2260,
+  8192,   9633,   4433,  -2259,  -8192, -11362, -10704,  -6436,
+  8192,   6437,  -4433, -11362,  -8192,   2261,  10704,   9633,
+  8192,   2260, -10703,  -6436,   8192,   9633,  -4433, -11363,
+  8192,  -2260, -10703,   6436,   8192,  -9633,  -4433,  11363,
+  8192,  -6437,  -4433,  11362,  -8192,  -2261,  10704,  -9633,
+  8192,  -9633,   4433,   2259,  -8192,  11362, -10704,   6436,
+  8192, -11363,  10703,  -9633,   8192,  -6437,   4433,  -2260,
+};
+    
+    // Mimic libjpeg's heuristics to guess jpeg color space.
+// Requires that the jpg has 3 components.
+bool HasYCbCrColorSpace(const JPEGData& jpg);
+    
+      // Special case code with only one value.
+  if (total_count == 1) {
+    code.bits = 0;
+    code.value = symbols[0];
+    for (key = 0; key < total_size; ++key) {
+      table[key] = code;
+    }
+    return total_size;
+  }
+    
+    
+    { private:
+  const int width_;
+  const int height_;
+  std::vector<OutputImageComponent> components_;
+};
+    
+    // Butteraugli scores that correspond to JPEG quality levels, starting at
+// kLowestQuality. They were computed by taking median BA scores of JPEGs
+// generated using libjpeg-turbo at given quality from a set of PNGs.
+// The scores above quality level 100 are just linearly decreased so that score
+// for 110 is 90% of the score for 100.
+const double kScoreForQuality[] = {
+  2.810761,  // 70
+  2.729300,
+  2.689687,
+  2.636811,
+  2.547863,
+  2.525400,
+  2.473416,
+  2.366133,
+  2.338078,
+  2.318654,
+  2.201674,  // 80
+  2.145517,
+  2.087322,
+  2.009328,
+  1.945456,
+  1.900112,
+  1.805701,
+  1.750194,
+  1.644175,
+  1.562165,
+  1.473608,  // 90
+  1.382021,
+  1.294298,
+  1.185402,
+  1.066781,
+  0.971769,  // 95
+  0.852901,
+  0.724544,
+  0.611302,
+  0.443185,
+  0.211578,  // 100
+  0.209462,
+  0.207346,
+  0.205230,
+  0.203114,
+  0.200999,  // 105
+  0.198883,
+  0.196767,
+  0.194651,
+  0.192535,
+  0.190420,  // 110
+  0.190420,
+};
+    
+      assert(animal->name()->str() == 'Dog');
+  assert(animal->sound()->str() == 'Bark');
+  (void)animal; // To silence 'Unused Variable' warnings.
+    
+    
+#endif  // GRPC_monster_5ftest__INCLUDED
+
+    
+     private:
+  void ParseFile(flatbuffers::Parser &parser, const std::string &filename,
+                 const std::string &contents,
+                 std::vector<const char *> &include_directories) const;
+    
+      // Converts a binary buffer to text using one of the schemas in the registry,
+  // use the file_identifier to indicate which.
+  // If DetachedBuffer::data() is null then parsing failed.
+  DetachedBuffer TextToFlatBuffer(const char *text,
+                                  const char *file_identifier) {
+    // Load and parse the schema.
+    Parser parser;
+    if (!LoadSchema(file_identifier, &parser)) return DetachedBuffer();
+    // Parse the text.
+    if (!parser.Parse(text)) {
+      lasterror_ = parser.error_;
+      return DetachedBuffer();
+    }
+    // We have a valid FlatBuffer. Detach it from the builder and return.
+    return parser.builder_.ReleaseBufferPointer();
+  }
+    
+      // Use the `CreateWeapon` shortcut to create Weapons with all fields set.
+  auto sword = CreateWeapon(builder, weapon_one_name, weapon_one_damage);
+  auto axe = CreateWeapon(builder, weapon_two_name, weapon_two_damage);
+    
+      flatbuffers::FlatCompiler::InitParams params;
+  params.generators = generators;
+  params.num_generators = sizeof(generators) / sizeof(generators[0]);
+  params.warn_fn = Warn;
+  params.error_fn = Error;
+    
+    // Create a struct with a builder and the struct's arguments.
+static void GenStructBuilder(const StructDef &struct_def,
+                             std::string *code_ptr) {
+  BeginBuilderArgs(struct_def, code_ptr);
+  StructBuilderArgs(struct_def, '', code_ptr);
+  EndBuilderArgs(code_ptr);
+    }
