15b029d50f2b495b26a397c00d586a77f88e60c0
SUNsung <weatherius@gmail.com>
2018-02-27 04:40:34 +0200

    Rewrite [520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp] File in: 2018-02-27 04:40:34

--- cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
+++ cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
@@ -1,286 +1,147 @@
 
         
-        template <typename T1, typename T2, typename T3, typename T4>
-internal::ValueArray4<T1, T2, T3, T4> Values(T1 v1, T2 v2, T3 v3, T4 v4) {
-  return internal::ValueArray4<T1, T2, T3, T4>(v1, v2, v3, v4);
-}
-    
-    #if !GTEST_OS_SYMBIAN
-# include <utility>
-#endif
-    
-    // Prints the given number of elements in an array, without printing
-// the curly braces.
-template <typename T>
-void PrintRawArrayTo(const T a[], size_t count, ::std::ostream* os) {
-  UniversalPrint(a[0], os);
-  for (size_t i = 1; i != count; i++) {
-    *os << ', ';
-    UniversalPrint(a[i], os);
-  }
-}
-    
-    #endif  // GTEST_INCLUDE_GTEST_GTEST_SPI_H_
-
-    
-    // Helper function for implementing {EXPECT|ASSERT}_PRED2.  Don't use
-// this in your code.
-template <typename Pred,
-          typename T1,
-          typename T2>
-AssertionResult AssertPred2Helper(const char* pred_text,
-                                  const char* e1,
-                                  const char* e2,
-                                  Pred pred,
-                                  const T1& v1,
-                                  const T2& v2) {
-  if (pred(v1, v2)) return AssertionSuccess();
-    }
-    
-      // We use a const char* instead of an std::string, as Google Test used to be
-  // used where std::string is not available.  TODO(wan@google.com): change to
-  // std::string.
-  const char* pattern_;
-  bool is_valid_;
-    
-    template <>
-class Get<2> {
- public:
-  template <class Tuple>
-  static GTEST_ADD_REF_(GTEST_TUPLE_ELEMENT_(2, Tuple))
-  Field(Tuple& t) { return t.f2_; }  // NOLINT
-    }
-    
-      // Returns true iff n is a prime number.
-  virtual bool IsPrime(int n) const = 0;
-    
-    
-    {  EXPECT_FALSE(IsPrime(-1));
-  EXPECT_FALSE(IsPrime(-2));
-  EXPECT_FALSE(IsPrime(INT_MIN));
-}
-    
-    // If the max-min top of a unicharset char is bigger than kMaxCharTopRange
-// then the char top cannot be used to judge misfits or suggest a new top.
-const int kMaxCharTopRange = 48;
-    
-    extern BLOCK_LIST *current_block_list;
-extern STRING_VAR_H (editor_image_win_name, 'EditorImage',
-'Editor image window name');
-extern INT_VAR_H (editor_image_xpos, 590, 'Editor image X Pos');
-extern INT_VAR_H (editor_image_ypos, 10, 'Editor image Y Pos');
-extern INT_VAR_H (editor_image_height, 680, 'Editor image height');
-extern INT_VAR_H (editor_image_width, 655, 'Editor image width');
-extern INT_VAR_H (editor_image_word_bb_color, BLUE,
-'Word bounding box colour');
-extern INT_VAR_H (editor_image_blob_bb_color, YELLOW,
-'Blob bounding box colour');
-extern INT_VAR_H (editor_image_text_color, WHITE, 'Correct text colour');
-extern STRING_VAR_H (editor_dbwin_name, 'EditorDBWin',
-'Editor debug window name');
-extern INT_VAR_H (editor_dbwin_xpos, 50, 'Editor debug window X Pos');
-extern INT_VAR_H (editor_dbwin_ypos, 500, 'Editor debug window Y Pos');
-extern INT_VAR_H (editor_dbwin_height, 24, 'Editor debug window height');
-extern INT_VAR_H (editor_dbwin_width, 80, 'Editor debug window width');
-extern STRING_VAR_H (editor_word_name, 'BlnWords',
-'BL normalised word window');
-extern INT_VAR_H (editor_word_xpos, 60, 'Word window X Pos');
-extern INT_VAR_H (editor_word_ypos, 510, 'Word window Y Pos');
-extern INT_VAR_H (editor_word_height, 240, 'Word window height');
-extern INT_VAR_H (editor_word_width, 655, 'Word window width');
-extern double_VAR_H (editor_smd_scale_factor, 1.0, 'Scaling for smd image');
-    
-      // Compute the number of unichars in the label.
-  GenericVector<UNICHAR_ID> encoding;
-  if (!unicharset.encode_string(label, true, &encoding, NULL, NULL)) {
-    tprintf('Not outputting illegal unichar %s\n', label);
-    return;
-  }
-    
-      // Step two: Try to split off bits of the word that are both outliers
-  //           and have much lower certainty than average
-  // Calculate num_leading and leading_certainty.
-  for (*leading_certainty = 0.0f, *num_rebuilt_leading = 0;
-       *num_rebuilt_leading < leading_outliers;
-       (*num_rebuilt_leading)++) {
-    float char_certainty = word->best_choice->certainty(*num_rebuilt_leading);
-    if (char_certainty > *unlikely_threshold) {
-      break;
-    }
-    if (char_certainty < *leading_certainty) {
-      *leading_certainty = char_certainty;
-    }
-  }
-    
-    #include 'ccstruct.h'
-    
-      // Transforms the given coords one step forward to normalized space, without
-  // using any block rotation or predecessor.
-  void LocalNormTransform(const TPOINT& pt, TPOINT* transformed) const;
-  void LocalNormTransform(const FCOORD& pt, FCOORD* transformed) const;
-  // Transforms the given coords forward to normalized space using the
-  // full transformation sequence defined by the block rotation, the
-  // predecessors, deepest first, and finally this. If first_norm is not NULL,
-  // then the first and deepest transformation used is first_norm, ending
-  // with this, and the block rotation will not be applied.
-  void NormTransform(const DENORM* first_norm, const TPOINT& pt,
-                     TPOINT* transformed) const;
-  void NormTransform(const DENORM* first_norm, const FCOORD& pt,
-                     FCOORD* transformed) const;
-  // Transforms the given coords one step back to source space, without
-  // using to any block rotation or predecessor.
-  void LocalDenormTransform(const TPOINT& pt, TPOINT* original) const;
-  void LocalDenormTransform(const FCOORD& pt, FCOORD* original) const;
-  // Transforms the given coords all the way back to source image space using
-  // the full transformation sequence defined by this and its predecessors
-  // recursively, shallowest first, and finally any block re_rotation.
-  // If last_denorm is not NULL, then the last transformation used will
-  // be last_denorm, and the block re_rotation will never be executed.
-  void DenormTransform(const DENORM* last_denorm, const TPOINT& pt,
-                       TPOINT* original) const;
-  void DenormTransform(const DENORM* last_denorm, const FCOORD& pt,
-                       FCOORD* original) const;
-    
-    TEST(LogTest, ReadFourthStart) {
-  CheckInitialOffsetRecord(
-      2 * (kHeaderSize + 1000) + (2 * log::kBlockSize - 1000) + 3 * kHeaderSize,
-      3);
-}
-    
-    TEST(FindFileTest, MultipleNullBoundaries) {
-  Add('150', '200');
-  Add('200', '250');
-  Add('300', '350');
-  Add('400', '450');
-  ASSERT_TRUE(! Overlaps(NULL, '149'));
-  ASSERT_TRUE(! Overlaps('451', NULL));
-  ASSERT_TRUE(Overlaps(NULL, NULL));
-  ASSERT_TRUE(Overlaps(NULL, '150'));
-  ASSERT_TRUE(Overlaps(NULL, '199'));
-  ASSERT_TRUE(Overlaps(NULL, '200'));
-  ASSERT_TRUE(Overlaps(NULL, '201'));
-  ASSERT_TRUE(Overlaps(NULL, '400'));
-  ASSERT_TRUE(Overlaps(NULL, '800'));
-  ASSERT_TRUE(Overlaps('100', NULL));
-  ASSERT_TRUE(Overlaps('200', NULL));
-  ASSERT_TRUE(Overlaps('449', NULL));
-  ASSERT_TRUE(Overlaps('450', NULL));
-}
-    
-    // WriteBatchInternal provides static methods for manipulating a
-// WriteBatch that we don't want in the public WriteBatch interface.
-class WriteBatchInternal {
- public:
-  // Return the number of entries in the batch.
-  static int Count(const WriteBatch* batch);
-    }
-    
-    class Issue178 { };
-    
-      iter->Seek('5');
-  ASSERT_EQ(iter->key().ToString(), '5');
-  iter->Prev();
-  ASSERT_EQ(iter->key().ToString(), '4');
-  iter->Prev();
-  ASSERT_EQ(iter->key().ToString(), '3');
-  iter->Next();
-  ASSERT_EQ(iter->key().ToString(), '4');
-  iter->Next();
-  ASSERT_EQ(iter->key().ToString(), '5');
+          // During the first Compute(), resource is either created or looked up using
+  // shared_name. In the latter case, the resource found should be verified if
+  // it is compatible with this op's configuration. The verification may fail in
+  // cases such as two graphs asking queues of the same shared name to have
+  // inconsistent capacities.
+  virtual Status VerifyResource(T* resource) { return Status::OK(); }
     
     
     { private:
-  /*! \brief the underlying stream */
-  dmlc::Stream *stream_;
-  /*! \brief buffer to hold data */
-  std::string buffer_;
-  /*! \brief length of valid data in buffer */
-  size_t read_len_;
-  /*! \brief pointer in the buffer */
-  size_t read_ptr_;
+  Cluster* cluster_;  // Not owned.
+  int measurement_steps_;
+  int measurement_threads_;
+  std::vector<std::pair<string, Tensor>> feed_;
+  std::vector<string> fetch_;
+  std::unique_ptr<thread::ThreadPool> thread_pool_;
 };
     
-    // This is a helpful data structure to define parameters
-// You do not have to use it.
-// see http://dmlc-core.readthedocs.org/en/latest/parameter.html
-// for introduction of this module.
-struct MyLogisticParam : public dmlc::Parameter<MyLogisticParam> {
-  float scale_neg_weight;
-  // declare parameters
-  DMLC_DECLARE_PARAMETER(MyLogisticParam) {
-    DMLC_DECLARE_FIELD(scale_neg_weight).set_default(1.0f).set_lower_bound(0.0f)
-        .describe('Scale the weight of negative examples by this factor');
-  }
-};
+      Status OnWorkStartedLocked() override {
+    line_number_ = 0;
+    TF_RETURN_IF_ERROR(env_->NewRandomAccessFile(current_work(), &file_));
+    }
     
-        FILE* stream = popen('logcat -d -v time -s 'DEBUG:I'', 'r');    // 将“ls －l”命令的输出 通过管道读取（“r”参数）到FILE* stream
+    template <class T>
+class DynamicStitchOpGPU : public DynamicStitchOpImplBase<T> {
+ public:
+  explicit DynamicStitchOpGPU(OpKernelConstruction* c)
+      : DynamicStitchOpImplBase<T>(c, 'DynamicStitchOp') {}
+    }
     
-    #ifdef ANDROID
-#include 'comm/android/callstack.h'
+    extern JSClass  *jsb_cocosbuilder_CCBReader_class;
+extern JSObject *jsb_cocosbuilder_CCBReader_prototype;
+    
+    bool js_cocos2dx_navmesh_NavMeshObstacle_constructor(JSContext *cx, uint32_t argc, jsval *vp);
+void js_cocos2dx_navmesh_NavMeshObstacle_finalize(JSContext *cx, JSObject *obj);
+void js_register_cocos2dx_navmesh_NavMeshObstacle(JSContext *cx, JS::HandleObject global);
+void register_all_cocos2dx_navmesh(JSContext* cx, JS::HandleObject obj);
+bool js_cocos2dx_navmesh_NavMeshObstacle_getSyncFlag(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_initWith(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_syncToObstacle(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_syncToNode(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_getHeight(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_setSyncFlag(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_getRadius(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_create(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_getNavMeshObstacleComponentName(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_NavMeshObstacle(JSContext *cx, uint32_t argc, jsval *vp);
+    
+    bool js_cocos2dx_studio_RotationFrame_constructor(JSContext *cx, uint32_t argc, jsval *vp);
+void js_cocos2dx_studio_RotationFrame_finalize(JSContext *cx, JSObject *obj);
+void js_register_cocos2dx_studio_RotationFrame(JSContext *cx, JS::HandleObject global);
+void register_all_cocos2dx_studio(JSContext* cx, JS::HandleObject obj);
+bool js_cocos2dx_studio_RotationFrame_setRotation(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_RotationFrame_getRotation(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_RotationFrame_create(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_RotationFrame_RotationFrame(JSContext *cx, uint32_t argc, jsval *vp);
+    
+    
+    
+    #ifdef __cplusplus
+extern 'C' {
+#endif
+#include 'tolua++.h'
+#ifdef __cplusplus
+}
 #endif
     
-            __FirstGetCreater(T::ServiceName());
     
-      private:
-    virtual void __OnAttach(const char* _key) = 0;
-    virtual void __OnDetach(const char* _key) = 0;
     
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
+    			b2Vec2 vertices[3];
+			vertices[0] = b2Mul(xf1, b2Vec2(-1.0f, 0.0f));
+			vertices[1] = b2Mul(xf1, b2Vec2(1.0f, 0.0f));
+			vertices[2] = b2Mul(xf1, b2Vec2(0.0f, 0.5f));
+			
+			b2PolygonShape poly1;
+			poly1.Set(vertices, 3);
     
-    jbyteArray JNU_Buffer2JbyteArray(JNIEnv* _env, const AutoBuffer& ab);
-jbyteArray JNU_Buffer2JbyteArray(JNIEnv* _env, const void* _buffer, size_t _length);
-void JNU_FreeJbyteArray(JNIEnv* _env, jbyteArray bytes);
+    		for (int32 i = 0; i < 3; ++i)
+		{
+			b2CircleShape shape;
+			shape.m_radius = 0.5f;
+    }
     
-    ScopeJEnv::ScopeJEnv(JavaVM* jvm, jint _capacity)
-    : vm_(jvm), env_(NULL), we_attach_(false), status_(0) {
-    ASSERT(jvm);
-    do {
-        env_ = (JNIEnv*)pthread_getspecific(g_env_key);
-        
-        if (NULL != env_) {
-            break;
+    				if (i > 0)
+				{
+					b2Vec2 anchor(-5.0f + 1.0f * i, 5.0f);
+					jd.Initialize(prevBody, body, anchor);
+					m_world->CreateJoint(&jd);
+				}
+    
+        VkSwapchainKHR swapchains[1] = {g_Swapchain};
+    uint32_t indices[1] = {g_BackbufferIndices[PresentIndex]};
+    VkPresentInfoKHR info = {};
+    info.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+    info.waitSemaphoreCount = 1;
+    info.pWaitSemaphores = &g_RenderCompleteSemaphore[PresentIndex];
+    info.swapchainCount = 1;
+    info.pSwapchains = swapchains;
+    info.pImageIndices = indices;
+    err = vkQueuePresentKHR(g_Queue, &info);
+    check_vk_result(err);
+    
+    // Implemented features:
+//  [X] User texture binding. Use 'ID3D10ShaderResourceView*' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
+    
+    // Handler for Win32 messages, update mouse/keyboard data.
+// You may or not need this for your implementation, but it can serve as reference for handling inputs.
+// Commented out to avoid dragging dependencies on <windows.h> types. You can copy the extern declaration in your code.
+/*
+IMGUI_API LRESULT   ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+*/
+
+    
+            // 2. Show another simple window. In most cases you will use an explicit Begin/End pair to name your windows.
+        if (show_another_window)
+        {
+            ImGui::Begin('Another Window', &show_another_window);
+            ImGui::Text('Hello from another window!');
+            if (ImGui::Button('Close Me'))
+                show_another_window = false;
+            ImGui::End();
         }
-        
-        status_ = vm_->GetEnv((void**) &env_, JNI_VERSION_1_6);
+    
+            // 1. Show a simple window.
+        // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called 'Debug'.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+            ImGui::Text('Hello, world!');                           // Display some text (you can use a format string too)
+            ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
     }
-    }
     
-      std::unique_ptr<AuthConfig> getUserDefinedAuthConfig() const;
+            // 3. Show the ImGui demo window. Most of the sample code is in ImGui::ShowDemoWindow(). Read its code to learn more about Dear ImGui!
+        if (show_demo_window)
+        {
+            ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver); // Normally user code doesn't need/want to call this because positions are saved in .ini file anyway. Here we just want to make the demo initial state a bit more friendly!
+            ImGui::ShowDemoWindow(&show_demo_window);
+        }
     
-      virtual bool getChangeOption() const CXX11_OVERRIDE;
-    
-    
-    {} // namespace aria2
-
-    
-    bool AdaptiveFileAllocationIterator::finished()
-{
-  if (!allocator_) {
-    return offset_ == totalLength_;
-  }
-  else {
-    return allocator_->finished();
-  }
-}
-    
-      /**
-   * The internal announce URL pointer points to next URL.
-   * If the current URL is the last element of its group, then the first
-   * element of the next group is pointed.
-   */
-  void announceFailure();
-    
-    AnnounceTier::~AnnounceTier() = default;
-    
-    #endif // D_ANNOUNCE_TIER_H
-
-    
-    std::ostream& operator<<(std::ostream& o,
-                         const std::shared_ptr<AuthConfig>& authConfig)
-{
-  o << authConfig->getAuthText();
-  return o;
-}
+    #include 'imgui.h'
+#include 'imgui_impl_dx9.h'
+#include <d3d9.h>
+#define DIRECTINPUT_VERSION 0x0800
+#include <dinput.h>
+#include <tchar.h>
