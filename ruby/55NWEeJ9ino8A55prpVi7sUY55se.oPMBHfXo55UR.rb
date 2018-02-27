5a267e658e6a59cc114d1ba593c051615063262b
SUNsung <weatherius@gmail.com>
2018-02-27 02:42:59 +0200

    Rewrite [55NWEeJ9ino8A55prpVi7sUY55se.oPMBHfXo55UR.rb] File in: 2018-02-27 02:42:58

--- ruby/55NWEeJ9ino8A55prpVi7sUY55se.oPMBHfXo55UR.rb
+++ ruby/55NWEeJ9ino8A55prpVi7sUY55se.oPMBHfXo55UR.rb
@@ -1,88 +1,48 @@
-  def recipients(payload = {})
-    emails = interpolated(payload)['recipients']
-    if emails.present?
-      if emails.is_a?(String)
-        [emails]
-      else
-        emails
-      end
-    else
-      [user.email]
-    end
-  end
-    
-      module SortableTableHelper
-    # :call-seq:
-    #   sortable_column(attribute, default_direction = 'desc', name: attribute.humanize)
-    def sortable_column(attribute, default_direction = nil, options = nil)
-      if options.nil? && (options = Hash.try_convert(default_direction))
-        default_direction = nil
-      end
-      default_direction ||= 'desc'
-      options ||= {}
-      name = options[:name] || attribute.humanize
-      selected = @table_sort_info[:attribute].to_s == attribute
-      if selected
-        direction = @table_sort_info[:direction]
-        new_direction = direction.to_s == 'desc' ? 'asc' : 'desc'
-        classes = 'selected #{direction}'
-      else
-        classes = ''
-        new_direction = default_direction
-      end
-      link_to(name, url_for(sort: '#{attribute}.#{new_direction}'), class: classes)
-    end
-  end
-end
 
+        
+                        on.unsubscribe do |chan, count|
+                  if count == 0
+                    @subscription_lock.synchronize do
+                      @raw_client = nil
+                    end
+                  end
+                end
+              end
+            end
+          end
     
-      def participation_answer(user)
-    poll_participations.find_by(author_id: user.person.id)
+              if node.reflection.collection?
+            other.target.push(model)
+          else
+            other.target = model
+          end
+    
+        def call(env)
+      result = @app.call(env)
+      result[1]['Middleware-Test'] = 'Success'
+      result[1]['Middleware-Order'] = 'First'
+      result
+    end
   end
     
-              render 'admins/pods'
+        private
+      # 'Deserialize' the mailer class name by hand in case another argument
+      # (like a Global ID reference) raised DeserializationError.
+      def mailer_class
+        if mailer = Array(@serialized_arguments).first || Array(arguments).first
+          mailer.constantize
         end
-        format.mobile { render 'admins/pods' }
-        format.json { render json: pods_json }
+      end
+    
+        # An email was generated.
+    def process(event)
+      debug do
+        mailer = event.payload[:mailer]
+        action = event.payload[:action]
+        '#{mailer}##{action}: processed outbound mail in #{event.duration.round(1)}ms'
       end
     end
     
-          def to_boolean(str)
-        str.downcase == 'true'
-      end
-    
-          def get_key_from_kid(keys, kid)
-        keys.each do |key|
-          return key if key.has_value?(kid)
-        end
-      end
-    
-          # Prints a title in blue with surrounding newlines.
-      def title(text)
-        # Use $stdout directly to bypass the indentation that our `puts` does.
-        $stdout.puts(color.colorize('\n#{text}\n', :blue))
-      end
-    
-        def exit_deploy_because_of_exception(ex)
-      warn t(:deploy_failed, ex: ex.message)
-      invoke 'deploy:failed'
-      exit(false)
-    end
-    
-          def redirect(env)
-        request = Request.new(env)
-        warn env, 'attack prevented by #{self.class}'
-        [302, {'Content-Type' => 'text/html', 'Location' => request.path}, []]
-      end
-    
-      subject { described_class.new(lambda {}) }
-    
-      it 'should set the X-Frame-Options' do
-    expect(get('/', {}, 'wants' => 'text/html').headers['X-Frame-Options']).to eq('SAMEORIGIN')
-  end
-    
-      %w(GET HEAD POST PUT DELETE).each do |method|
-    it 'accepts #{method} requests with no Origin' do
-      expect(send(method.downcase, '/')).to be_ok
-    end
-  end
+      option '--type', 'TYPE', 'Type of the plugin {input, filter, codec, output}s', :required => true
+  option '--name', 'PLUGIN', 'Name of the new plugin', :required => true
+  option '--path', 'PATH', 'Location where the plugin skeleton will be created', :default => Dir.pwd
