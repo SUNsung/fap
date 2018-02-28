de45c0ec7da64feafa9d729af5ec78316472eed8
SUNsung <weatherius@gmail.com>
2018-02-28 05:37:13 +0200

    Rewrite [583iMTkDnj7Ng58YkjFmvCMVU58pkRuxJF.pr658BEy.py] File in: 2018-02-28 05:37:13

--- pyton/583iMTkDnj7Ng58YkjFmvCMVU58pkRuxJF.pr658BEy.py
+++ pyton/583iMTkDnj7Ng58YkjFmvCMVU58pkRuxJF.pr658BEy.py
@@ -1,175 +1,78 @@
 
         
-            params = {
-        'age_limit': age,
-        'skip_download': True,
-        'writeinfojson': True,
-        'outtmpl': '%(id)s.%(ext)s',
-    }
-    ydl = YoutubeDL(params)
-    ydl.add_default_info_extractors()
-    json_filename = os.path.splitext(filename)[0] + '.info.json'
-    try_rm(json_filename)
-    ydl.download([url])
-    res = os.path.exists(json_filename)
-    try_rm(json_filename)
-    return res
+        print('Build model...')
     
-            if check_executable('mplayer', ['-h']):
-            args = [
-                'mplayer', '-really-quiet', '-vo', 'null', '-vc', 'dummy',
-                '-dumpstream', '-dumpfile', tmpfilename, url]
-        elif check_executable('mpv', ['-h']):
-            args = [
-                'mpv', '-really-quiet', '--vo=null', '--stream-dump=' + tmpfilename, url]
-        else:
-            self.report_error('MMS or RTSP download detected but neither 'mplayer' nor 'mpv' could be run. Please install any.')
-            return False
+        if member.__doc__ is None and not member_too_small(member):
+        raise ValueError('{} class doesn't have any documentation'.format(name),
+                         member.__module__, inspect.getmodule(member).__file__)
+    for n, met in inspect.getmembers(member):
+        if inspect.ismethod(met):
+            handle_method(n, met)
     
-            raw_payload = self._search_regex([
-            r'class='amtv-embed'[^>]+id='([^']+)'',
-            r'class=\\'amtv-embed\\'[^>]+id=\\'([^']+)\\'',
-        ], webpage, 'raw payload')
-        _, stage_mode, video_id, lang = raw_payload.split('-')
     
-            return {
-            'id': flv_id,
-            'url': rtmp_url,
-            'ext': 'flv',
-            'no_resume': True,
-            'title': title,
-            'description': description,
-            'duration': duration,
-            'view_count': view_count,
-            'comment_count': comment_count,
-            'uploader': uploader,
-            'upload_date': upload_date,
-        }
+if __name__ == '__main__':
+    pytest.main([__file__])
 
     
-            return info_dict
-
+        input_a = layers.Input(shape=input_shapes[0][1:])
+    input_b = layers.Input(shape=input_shapes[1][1:])
+    a = layers.Masking()(input_a)
+    b = layers.Masking()(input_b)
+    merged = legacy_layers.merge([a, b], mode=fn_mode, output_shape=fn_output_shape, output_mask=fn_output_mask)
+    model = models.Model([input_a, input_b], merged)
+    expected_output_shape = model.compute_output_shape(input_shapes)
+    actual_output_shape = model.predict(inputs).shape
+    assert expected_output_shape == actual_output_shape
     
-    site_info = 'MioMio.tv'
-download = miomio_download
-download_playlist = playlist_not_supported('miomio')
-
+    
+@keras_test
+def test_merge_overlap(in_tmpdir):
+    (x_train, y_train), (x_test, y_test) = _get_test_data()
+    left = Sequential()
+    left.add(Dense(num_hidden, input_shape=(input_dim,)))
+    left.add(Activation('relu'))
+    
+        def __init__(self):
+        self.head = None
+        self.tail = None
+    
+        HOUSING = 0
+    FOOD = 1
+    GAS = 2
+    SHOPPING = 3
+    ...
+    
+        def set(self, results, query):
+        '''Set the result for the given query key in the cache.
+    
+        def get_people(self, ids):
+        results = []
+        for id in ids:
+            if id in self.people:
+                results.append(self.people[id])
+        return results
     
     #----------------------------------------------------------------------
-def showroom_get_roomid_by_room_url_key(room_url_key):
-    '''str->str'''
-    fake_headers_mobile = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Charset': 'UTF-8,*;q=0.5',
-        'Accept-Encoding': 'gzip,deflate,sdch',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'
+def fc2video_download_by_upid(upid, output_dir = '.', merge = True, info_only = False, **kwargs):
+    ''''''
+    fake_headers = {
+        'DNT': '1',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'en-CA,en;q=0.8,en-US;q=0.6,zh-CN;q=0.4,zh;q=0.2',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.58 Safari/537.36',
+        'Accept': '*/*',
+        'X-Requested-With': 'ShockwaveFlash/19.0.0.245',
+        'Connection': 'keep-alive',
     }
-    webpage_url = 'https://www.showroom-live.com/' + room_url_key
-    html = get_content(webpage_url, headers = fake_headers_mobile)
-    roomid = match1(html, r'room\?room_id\=(\d+)')
-    assert roomid
-    return roomid
+    api_base = 'http://video.fc2.com/ginfo.php?upid={upid}&mimi={mimi}'.format(upid = upid, mimi = makeMimi(upid))
+    html = get_content(api_base, headers=fake_headers)
     
-    
-class TestNaitivePool(unittest.TestCase):
-    
-        def test_parrot_eng_localization(self):
-        self.assertEqual(self.e.get('parrot'), 'parrot')
-    
-        def test_am_station_overflow_after_scan(self):
-        self.radio.scan()
-        station = self.radio.state.stations[self.radio.state.pos]
-        expected_station = '1250'
-        self.assertEqual(station, expected_station)
-    
-        def main_method(self):
-        '''will execute either _instance_method_1 or _instance_method_2
-    
-        def publishReport(self):
-        self._db.update()
-        self._reporter.report()
-    
-    
-class CompositeSpecification(Specification):
-    
-    print 'PLUGIN_I18N_PATHS := ' + ','.join(os.path.relpath(plugin.path)
-                                         for plugin in plugins
-                                         if plugin.needs_translation)
-    
-        def live_config_iteritems(self):
-        live = self.stacked_proxy_safe_get(g, 'live_config', {})
-        return live.iteritems()
-    
-    api('organiclisting',       OrganicListingJsonTemplate)
-api('subreddittraffic', TrafficJsonTemplate)
-api('takedownpane', TakedownJsonTemplate)
-api('policyview', PolicyViewJsonTemplate)
-    
-    from r2.config.extensions import set_extension
-from r2.controllers.reddit_base import RedditController, generate_modhash
-from r2.controllers.login import handle_login, handle_register
-from r2.lib.csrf import csrf_exempt
-from r2.lib.validator import (
-    json_validate,
-    ValidEmail,
-    VPasswordChange,
-    VRatelimit,
-    VSigned,
-    VThrottledLogin,
-    VUname,
-)
-    
-            # See if the target is already an existing friend.
-        # If not, create the friend relationship.
-        friend_rel = Account.get_friend(c.user, friend)
-        rel_exists = bool(friend_rel)
-        if not friend_rel:
-            friend_rel = c.user.add_friend(friend)
-            response.status = 201
-    
-            def builder_wrapper(thing = None):
-            kw = {}
-            if not thing:
-                kw['url'] = url
-                kw['title'] = title
-            return ButtonLite(thing,
-                              image = 1 if buttonimage is None else buttonimage,
-                              target = '_new' if newwindow else '_parent',
-                              styled = styled, **kw)
-    
-            To request a new CAPTCHA,
-        use [/api/new_captcha](#POST_api_new_captcha).
-        '''
-        image = captcha.get_image(iden)
-        f = StringIO.StringIO()
-        image.save(f, 'PNG')
-        response.content_type = 'image/png;'
-        return f.getvalue()
-    
-
-    
-    
-    def send400(self):
-        if 'usable_error_content' in request.environ:
-            return request.environ['usable_error_content']
-        else:
-            res = pages.RedditError(
-                title=_('bad request (%(domain)s)') % dict(domain=g.domain),
-                message=_('you sent an invalid request'),
-                explanation=request.GET.get('explanation'))
-            return res.render()
-    
-    from r2.controllers.reddit_base import MinimalController
-from r2.lib.pages import (
-    GoogleTagManagerJail,
-    GoogleTagManager,
-)
-from r2.lib.validator import (
-    validate,
-    VGTMContainerId,
-)
-    
-        def GET_promohealth(self):
-        response.content_type = 'application/json'
-        return json.dumps(promote.health_check())
+        for i in range(10, 30):
+        url = 'https://stream{i}.mixcloud.com/c/m4a/64{p}.m4a'.format(
+            i = i,
+            p = preview
+        )
+        try:
+            mime, ext, size = url_info(url)
+            break
+        except: continue
