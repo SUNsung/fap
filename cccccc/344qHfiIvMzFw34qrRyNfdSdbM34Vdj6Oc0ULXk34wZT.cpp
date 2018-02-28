de0e5337c93b0afa9fe6db5e7e761e56db5479b5
SUNsung <weatherius@gmail.com>
2018-02-28 21:38:00 +0200

    Rewrite [344qHfiIvMzFw34qrRyNfdSdbM34Vdj6Oc0ULXk34wZT.cpp] File in: 2018-02-28 21:38:00

--- cccccc/344qHfiIvMzFw34qrRyNfdSdbM34Vdj6Oc0ULXk34wZT.cpp
+++ cccccc/344qHfiIvMzFw34qrRyNfdSdbM34Vdj6Oc0ULXk34wZT.cpp
@@ -1,296 +1,244 @@
 
         
-        
-    {
-    {    if (SemanticNode.is<Expr *>()) {
-      SemanticNode.get<Expr *>()->dump(llvm::errs());
-    } else if (SemanticNode.is<Decl *>()) {
-      SemanticNode.get<Decl *>()->dump(llvm::errs());
-    } else if (SemanticNode.is<Expr *>()) {
-      SemanticNode.get<Expr *>()->dump(llvm::errs());
-    } else {
-      llvm_unreachable('ASTNode has pointer to unknown thing!');
-    }
-    llvm::errs() << '\n=====================================================\n';
-  }
-}
-
+              T* resource;
+      OP_REQUIRES_OK(
+          context,
+          mgr->LookupOrCreate<T>(cinfo_.container(), cinfo_.name(), &resource,
+                                 [this](T** ret) EXCLUSIVE_LOCKS_REQUIRED(mu_) {
+                                   Status s = CreateResource(ret);
+                                   if (!s.ok() && *ret != nullptr) {
+                                     CHECK((*ret)->Unref());
+                                   }
+                                   return s;
+                                 }));
     
-    StringRef swift::unicode::extractFirstExtendedGraphemeCluster(StringRef S) {
-  // Extended grapheme cluster segmentation algorithm as described in Unicode
-  // Standard Annex #29.
-  if (S.empty())
-    return StringRef();
-    }
-    
-      /// Suppress all warnings
-  bool SuppressWarnings = false;
-    
-      /// Returns the SourceLoc for the beginning of the specified buffer
-  /// (at offset zero).
-  ///
-  /// Note that the resulting location might not point at the first token: it
-  /// might point at whitespace or a comment.
-  SourceLoc getLocForBufferStart(unsigned BufferID) const {
-    return getRangeForBuffer(BufferID).getStart();
-  }
-    
-      static void ClearCache(content::RenderProcessHost* render_view_host);
-  static void SetProxyConfig(content::RenderProcessHost* render_process_host,
-                             const std::string& proxy_config);
-    
-    #endif  // CONTENT_NW_SRC_API_BASE_BASE_H_
-
-    
-    
-    {  RenderThread::Get()->Send(new ShellViewHostMsg_Call_Object_Method(
-      routing_id,
-      object_id,
-      type,
-      method,
-      *static_cast<base::ListValue*>(value_args.get())));
-  return v8::Undefined(isolate);
-}
-    
-        content::WebContents* embedder_web_contents =
-        GetFirstAppWindowWebContents();
-    if (!embedder_web_contents) {
-      LOG(ERROR) << 'UNABLE TO FIND EMBEDDER WEB CONTENTS.';
-      return;
-    }
-    
-      static v8::Handle<v8::Object> GetObjectRegistry();
-  static v8::Handle<v8::Value> GetWindowId(blink::WebFrame* frame);
-  static void ZoomLevelChanged(blink::WebView* web_view);
-  static void willHandleNavigationPolicy(
-    content::RenderView* rv,
-    blink::WebFrame* frame,
-    const blink::WebURLRequest& request,
-    blink::WebNavigationPolicy* policy,
-    blink::WebString* manifest);
-    
-    
-namespace nwapi {
-    }
-    
-    namespace nwapi {
-    }
-    
-    #include 'content/nw/src/api/menuitem/menuitem.h'
-    
-    void genNFSShare(const std::string& share_line, QueryData& results) {
-  auto line = osquery::split(share_line);
-  if (line.size() == 0 || boost::starts_with(line[0], '#')) {
+    void SubProcess::SetProgram(const string& file,
+                            const std::vector<string>& argv) {
+  mutex_lock procLock(proc_mu_);
+  mutex_lock dataLock(data_mu_);
+  if (running_) {
+    LOG(FATAL) << 'SetProgram called after the process was started.';
     return;
   }
     }
     
-    static int osquery_open(dev_t dev, int oflags, int devtype, struct proc *p) {
-  // Close is not working so leave these out for now.
-  int err = 0;
-  lck_mtx_lock(osquery.mtx);
-  if (osquery.open_count == 0) {
-    osquery.open_count++;
+      // Given the driver version file contents, finds the kernel module version and
+  // returns it as a string.
+  //
+  // This is solely used for more informative log messages when the user is
+  // running on a machine that happens to have a libcuda/kernel driver mismatch.
+  static port::StatusOr<DriverVersion> FindKernelModuleVersion(
+      const string &driver_version_file_contents);
+    
+    
+    { private:
+  PluginId blas_, dnn_, fft_, rng_;
+};
+    
+    // ---------------------------------------------------------------------------
+// Inline implementation
+// ---------------------------------------------------------------------------
+template <PrefetchHint hint>
+inline void prefetch(const void* x) {
+// Check of COMPILER_GCC macro below is kept only for backward-compatibility
+// reasons. COMPILER_GCC3 is the macro that actually enables prefetch.
+#if defined(__llvm__) || defined(COMPILER_GCC) || defined(COMPILER_GCC3)
+  __builtin_prefetch(x, 0, hint);
+#else
+// You get no effect.  Feel free to add more sections above.
+#endif
+}
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    HloReachabilityMap::HloReachabilityMap(
+    const std::list<HloInstruction*>& instructions)
+    : size_(instructions.size()) {
+  bit_vectors_.reserve(size_);
+  for (const HloInstruction* hlo : instructions) {
+    indices_[hlo] = bit_vectors_.size();
+    bit_vectors_.emplace_back(size_);
   }
-#ifndef KERNEL_TEST
-  else {
-    err = -EACCES;
-  }
-#endif // !KERNEL_TEST
+  CHECK_EQ(size_, indices_.size());  // instructions should be unique
+}
+    
+    sampled_audio: A rank-2 float tensor containing all tracks of the audio.
+    Dimension 0 is time and dimension 1 is the channel.
+file_format: A string or rank-0 string tensor describing the audio file
+    format. This value must be `'wav'`.
+samples_per_second: The number of samples per second that the audio should
+    have, as an int or rank-0 `int32` tensor. This value must be
+    positive.
+bits_per_second: The approximate bitrate of the encoded audio file, as
+    an int or rank-0 `int32` tensor. This is ignored by the 'wav' file
+    format.
+contents: The binary audio file contents, as a rank-0 string tensor.
+)doc');
+    
+    // WorkloadStats is used to track per request timing for different states
+// of the VM.  At the entrypoint to a change of vm state a WorkloadStats object
+// should be made to guard the state change with appropriate timers and
+// counters.
+//
+// The states tracked are:
+//  - In a request (this is a superset of the interpreter state)
+//  - In the interpreter through Dispatch, or DispatchBB (interpOne disregarded)
+//  - In the JIT (currently tracks time inside the translate routine)
+//
+// Note the time in the TC is not tracked.  This is roughly:
+//   Time in request - Time in interp
+//
+// This gives us the relative interp time formula of:
+//   Relative interp time = Time in interp / Time in request
+struct WorkloadStats final {
+  enum State {
+    InRequest,
+    // -> InInterp   Okay (entering Dispatch loop)
+    // -> InTrans    Okay (entering translate)
+    InInterp,
+    // -> InRequest  Okay (leaving the dispatch loop)
+    // -> InTrans    Okay (entering translate)
+    InTrans,
+    // -> InRequest  Okay (leaving translate)
+    // -> InInterp   Okay (leaving translate)
+  };
     }
     
     
-    {TEST_F(CarverTests, test_decompression) {
-  auto s = osquery::decompress(
-      fs::temp_directory_path() / fs::path('test.config.zst'),
-      fs::temp_directory_path() / fs::path('test.config'));
-  EXPECT_TRUE(s.ok());
-  EXPECT_TRUE(
-      hashFromFile(
-          HashType::HASH_TYPE_SHA256,
-          (fs::temp_directory_path() / fs::path('test.config')).string()) ==
-      hashFromFile(HashType::HASH_TYPE_SHA256, kTestDataPath + 'test.config'));
+    {  v << vcall{
+    CallSpec::direct(pthread_getspecific),
+    v.makeVcallArgs({{v.cns(datum.tls->m_key)}}),
+    v.makeTuple({d}),
+    Fixup{},
+    DestType::SSA
+  };
 }
-}
-
     
-    #include <sstream>
+    
+    {  jit::array<Section, kNumAreas> sections;
+};
+    
+      OnigEncoding default_mbctype;
+  OnigEncoding current_mbctype;
+  RegexCache ht_rc;
+  std::string search_str;
+  unsigned int search_pos;
+  php_mb_regex_t *search_re;
+  OnigRegion *search_regs;
+  OnigOptionType regex_default_options;
+  OnigSyntaxType *regex_default_syntax;
+    
+    namespace HPHP {
+///////////////////////////////////////////////////////////////////////////////
+    }
+    
+    void PageletServer::Stop() {
+  if (s_dispatcher) {
+    auto monitor = getSingleton<HostHealthMonitor>();
+    monitor->unsubscribe(s_dispatcher);
+    s_dispatcher->stop();
+    Lock l(s_dispatchMutex);
+    delete s_dispatcher;
+    s_dispatcher = nullptr;
+  }
+}
+    
+    void numa_bind_to(void* start, size_t size, int node) {
+  if (!use_numa) return;
+  numa_tonode_memory(start, size, node);
+}
+    
+    extern uint32_t numa_node_set;
+extern uint32_t numa_node_mask;
+extern uint32_t numa_num_nodes;
+extern std::vector<bitmask*> node_to_cpu_mask;
+extern bool use_numa;
+    
+    void ThriftBuffer::read(std::string &data) {
+  String sdata;
+  read(sdata);
+  data = std::string(sdata.data(), sdata.size());
+}
+    
+    
+    {  return geteuid();
+}
+    
+      std::vector<LdSSwitchData::Elm> cases(numCases);
+  for (int i = 0; i < numCases; ++i) {
+    auto const& kv = iv.strvec()[i];
+    cases[i].str  = curUnit(env)->lookupLitstrId(kv.str);
+    cases[i].dest = SrcKey{curSrcKey(env), bcOff(env) + kv.dest};
+  }
+    
+    #ifndef STORAGE_LEVELDB_DB_LOG_FORMAT_H_
+#define STORAGE_LEVELDB_DB_LOG_FORMAT_H_
+    
+    #include <string>
+#include <stdint.h>
+#include 'db/dbformat.h'
+#include 'leveldb/cache.h'
+#include 'leveldb/table.h'
+#include 'port/port.h'
+    
+    // Tag numbers for serialized VersionEdit.  These numbers are written to
+// disk and should not be changed.
+enum Tag {
+  kComparator           = 1,
+  kLogNumber            = 2,
+  kNextFileNumber       = 3,
+  kLastSequence         = 4,
+  kCompactPointer       = 5,
+  kDeletedFile          = 6,
+  kNewFile              = 7,
+  // 8 was used for large value refs
+  kPrevLogNumber        = 9
+};
+    
+    #include <set>
+#include <utility>
 #include <vector>
+#include 'db/dbformat.h'
     
-    #include 'guetzli/jpeg_data_writer.h'
-    
-      yuv = PreProcessChannel(width_, height_, 2, 1.3f, 0.5f,
-                          cfg.u_sharpen, cfg.u_blur, yuv);
-  yuv = PreProcessChannel(width_, height_, 1, 1.3f, 0.5f,
-                          cfg.v_sharpen, cfg.v_blur, yuv);
-    
-    std::vector<float> Blur(const std::vector<float>& image, int w, int h) {
-    // This is only made for small sigma, e.g. 1.3.
-    static const double kSigma = 1.3;
-    std::vector<double> kernel(5);
-    for (size_t i = 0; i < kernel.size(); i++) {
-      kernel[i] = Normal(1.0 * i - kernel.size() / 2, kSigma);
-    }
+      void Stop(const Slice& name) {
+    double finish = Env::Default()->NowMicros() * 1e-6;
     }
     
-          // The sentinel node becomes the parent node.
-      size_t j_end = 2 * n - k;
-      tree[j_end].total_count_ =
-          tree[left].total_count_ + tree[right].total_count_;
-      tree[j_end].index_left_ = static_cast<int16_t>(left);
-      tree[j_end].index_right_or_value_ = static_cast<int16_t>(right);
-    
-    // Entropy encoding (Huffman) utilities.
-    
-    #ifndef GUETZLI_FAST_LOG_H_
-#define GUETZLI_FAST_LOG_H_
-    
-    // kIDCTMatrix[8*x+u] = alpha(u)*cos((2*x+1)*u*M_PI/16)*sqrt(2), with fixed 13
-// bit precision, where alpha(0) = 1/sqrt(2) and alpha(u) = 1 for u > 0.
-// Some coefficients are off by +-1 to mimick libjpeg's behaviour.
-static const int kIDCTMatrix[kDCTBlockSize] = {
-  8192,  11363,  10703,   9633,   8192,   6437,   4433,   2260,
-  8192,   9633,   4433,  -2259,  -8192, -11362, -10704,  -6436,
-  8192,   6437,  -4433, -11362,  -8192,   2261,  10704,   9633,
-  8192,   2260, -10703,  -6436,   8192,   9633,  -4433, -11363,
-  8192,  -2260, -10703,   6436,   8192,  -9633,  -4433,  11363,
-  8192,  -6437,  -4433,  11362,  -8192,  -2261,  10704,  -9633,
-  8192,  -9633,   4433,   2259,  -8192,  11362, -10704,   6436,
-  8192, -11363,  10703,  -9633,   8192,  -6437,   4433,  -2260,
-};
-    
-    // Fills in 'result' with the inverse DCT of 'block'.
-// The arguments 'block' and 'result' point to 8x8 arrays that are arranged in
-// a row-by-row memory layout.
-void ComputeBlockIDCT(const coeff_t* block, uint8_t* result);
-    
-    #endif  // GUETZLI_JPEG_DATA_DECODER_H_
-
-    
-    
-    {}  // namespace guetzli
-    
-    #include <inttypes.h>
-    
-      // Fills in out[] array with the floating-point precision pixel view of the
-  // component.
-  // REQUIRES: factor_x() == 1 and factor_y() == 1.
-  void ToFloatPixels(float* out, int stride) const;
-    
-    namespace folly {
-namespace hazptr {
-    }
+    namespace leveldb {
     }
     
-    void AsyncIOQueue::submit(AsyncIOOp* op) {
-  submit([op]() { return op; });
-}
-    
-      /**
-   * Reset the operation for reuse.  It is an error to call reset() on
-   * an Op that is still pending.
-   */
-  void reset(NotificationCallback cb = NotificationCallback());
-    
-    void list() {
-  for (const auto& p : getHugePageSizes()) {
-    std::cout << p.size << ' ' << p.mountPoint << '\n';
+      // create first key range
+  leveldb::WriteBatch batch;
+  for (size_t i = 0; i < kNumKeys; i++) {
+    batch.Put(Key1(i), 'value for range 1 key');
   }
-}
+  ASSERT_OK(db->Write(leveldb::WriteOptions(), &batch));
     
-    namespace folly {
-    }
+    void Mutex::Lock() { PthreadCall('lock', pthread_mutex_lock(&mu_)); }
     
-    #include <folly/Optional.h>
-#include <folly/Range.h>
-#include <memory>
+      // Check third filter (empty)
+  ASSERT_TRUE(! reader.KeyMayMatch(4100, 'foo'));
+  ASSERT_TRUE(! reader.KeyMayMatch(4100, 'bar'));
+  ASSERT_TRUE(! reader.KeyMayMatch(4100, 'box'));
+  ASSERT_TRUE(! reader.KeyMayMatch(4100, 'hello'));
     
-      // Format the data into a buffer.
-  std::string buffer;
-  StringPiece msgData{message.getMessage()};
-  if (message.containsNewlines()) {
-    // If there are multiple lines in the log message, add a header
-    // before each one.
-    std::string header;
-    header.reserve(headerLengthGuess);
-    headerFormatter.appendTo(header);
-    }
-    
-    
-    {
-    {  folly::File file_;
-};
-} // namespace folly
-
-    
-    void LogCategory::setLevel(LogLevel level, bool inherit) {
-  // We have to set the level through LoggerDB, since we require holding
-  // the LoggerDB lock to iterate through our children in case our effective
-  // level changes.
-  db_->setLevel(this, level, inherit);
-}
-    
-    bool LogCategoryConfig::operator==(const LogCategoryConfig& other) const {
-  return level == other.level &&
-      inheritParentLevel == other.inheritParentLevel &&
-      handlers == other.handlers;
-}
-    
-    
-    {} // namespace folly
-
-    
-    /**
- * Parse a JSON configuration string.
- *
- * See the documentation in logging/docs/Config.md for a description of the
- * JSON configuration object format.
- *
- * This function uses relaxed JSON parsing, allowing C and C++ style
- * comments, as well as trailing commas.
- */
-LogConfig parseLogConfigJson(StringPiece value);
-    
-    // Abort the program after logging the mesage if the given condition is not
-// true. Otherwise, do nothing.
-#define GRPC_CODEGEN_CHECK(x)                                            \
-  (x) ? (void)0                                                          \
-      : LogMessageVoidify() & LogHelper(&std::cerr).get_os()             \
-                                  << 'CHECK FAILED: ' << __FILE__ << ':' \
-                                  << __LINE__ << ': '
-    
-    
-template<typename T> T EndianScalar(T t) {
-  #if FLATBUFFERS_LITTLEENDIAN
-    return t;
-  #else
-    return EndianSwap(t);
-  #endif
-}
-    
-      // Converts a binary buffer to text using one of the schemas in the registry,
-  // use the file_identifier to indicate which.
-  // If DetachedBuffer::data() is null then parsing failed.
-  DetachedBuffer TextToFlatBuffer(const char *text,
-                                  const char *file_identifier) {
-    // Load and parse the schema.
-    Parser parser;
-    if (!LoadSchema(file_identifier, &parser)) return DetachedBuffer();
-    // Parse the text.
-    if (!parser.Parse(text)) {
-      lasterror_ = parser.error_;
-      return DetachedBuffer();
-    }
-    // We have a valid FlatBuffer. Detach it from the builder and return.
-    return parser.builder_.ReleaseBufferPointer();
+      virtual Slice value() const {
+    assert(Valid());
+    return current_->value();
   }
     
-      auto weapon_two_name = builder.CreateString('Axe');
-  short weapon_two_damage = 5;
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
     
-      if (opts.proto_mode) {
-    if (any_generator)
-      Error('cannot generate code directly from .proto files', true);
-  } else if (!any_generator && conform_to_schema.empty()) {
-    Error('no options: specify at least one generator.', true);
-  }
+        LongLinkPack st = {0};
+    memcpy(&st, _rawbuf, sizeof(LongLinkPack));
+    
+    
+class ServiceBase;
+typedef std::map<std::string, ServiceBase*> TServicesMap;
+    
+    int ScopeJEnv::Status() {
+    return status_;
+}
