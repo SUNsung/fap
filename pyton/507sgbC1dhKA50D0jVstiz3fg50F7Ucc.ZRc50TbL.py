51be98c77929602d4e38da6c67507e389c63eedb
SUNsung <weatherius@gmail.com>
2018-03-01 01:04:01 +0200

    Rewrite [507sgbC1dhKA50D0jVstiz3fg50F7Ucc.ZRc50TbL.py] File in: 2018-03-01 01:04:00

--- pyton/507sgbC1dhKA50D0jVstiz3fg50F7Ucc.ZRc50TbL.py
+++ pyton/507sgbC1dhKA50D0jVstiz3fg50F7Ucc.ZRc50TbL.py
@@ -1,56 +1,75 @@
-js.load_plugin_modules(plugins)
-modules = dict((k, m) for k, m in js.module.iteritems())
-print 'JS_MODULES := ' + ' '.join(modules.iterkeys())
-outputs = []
-for name, module in modules.iteritems():
-    outputs.extend(module.outputs)
-    print 'JS_MODULE_OUTPUTS_%s := %s' % (name, ' '.join(module.outputs))
-    print 'JS_MODULE_DEPS_%s := %s' % (name, ' '.join(module.dependencies))
+
+        
+        
+def get_meta(data):
+    meta = {'hostvars': {}}
+    for node in data:
+        meta['hostvars'][node['hostname']] = {'tags': node['tags']}
+    return meta
     
-    from r2.controllers.api_docs import api_doc, api_section
-from r2.controllers.oauth2 import require_oauth2_scope
-from r2.controllers.reddit_base import OAuth2OnlyController
-from r2.controllers.ipn import send_gift
-from r2.lib.errors import RedditError
-from r2.lib.validator import (
-    validate,
-    VAccountByName,
-    VByName,
-    VInt,
-    VNotInTimeout,
-)
-from r2.models import Account, Comment, Link, NotFound
-from r2.models.gold import creddits_lock
-from r2.lib.validator import VUser
-    
-    from r2.config.extensions import set_extension
-from r2.controllers.reddit_base import RedditController, generate_modhash
-from r2.controllers.login import handle_login, handle_register
-from r2.lib.csrf import csrf_exempt
-from r2.lib.validator import (
-    json_validate,
-    ValidEmail,
-    VPasswordChange,
-    VRatelimit,
-    VSigned,
-    VThrottledLogin,
-    VUname,
-)
-    
-    ###  OUTPUT ###
-# Before subclassing:
-# BaseRegisteredClass
-# After subclassing:
-# BaseRegisteredClass
-# ClassRegistree
+        def remove_role(self, role_name):
+        del self.roles[role_name]
 
     
-        def get_current_time_as_html_fragment(self):
-        current_time = self.time_provider.now()
-        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
-        return current_time_as_html_fragment
-'''
+            try:
+            parts = role_name.split('.')
+            user_name = '.'.join(parts[0:-1])
+            role_name = parts[-1]
+            if notify:
+                display.display('- downloading role '%s', owned by %s' % (role_name, user_name))
+        except:
+            raise AnsibleError('Invalid role name (%s). Specify role as format: username.rolename' % role_name)
+    
+            # zone, resource, entity, method
+        input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/zones/us-east1-c/instanceGroupManagers/my-mig/recreateInstances'
+        actual = GCPUtils.parse_gcp_url(input_url)
+        self.assertEquals('compute', actual['service'])
+        self.assertEquals('v1', actual['api_version'])
+        self.assertEquals('myproject', actual['project'])
+        self.assertEquals('us-east1-c', actual['zone'])
+        self.assertEquals('instanceGroupManagers', actual['resource_name'])
+        self.assertEquals('my-mig', actual['entity_name'])
+        self.assertEquals('recreateInstances', actual['method_name'])
     
     
-class Catalog(object):
-    '''catalog of multiple static methods that are executed depending on an init
+class TerminalModule(TerminalBase):
+    
+        for pvm_list in duplicates:
+        first_prm = pvm_list[0]
+        roles = set(first_prm.role)
+        for pvm in pvm_list[1:]:
+            roles = roles.union(pvm.role)
+            sm.get_session.delete(pvm)
+        first_prm.roles = list(roles)
+    sm.get_session.commit()
+    
+    
+@manager.option(
+    '-v', '--verbose', action='store_true',
+    help='Show extra information')
+def version(verbose):
+    '''Prints the current version number'''
+    print(Fore.BLUE + '-=' * 15)
+    print(Fore.YELLOW + 'Superset ' + Fore.CYAN + '{version}'.format(
+        version=config.get('VERSION_STRING')))
+    print(Fore.BLUE + '-=' * 15)
+    if verbose:
+        print('[DB] : ' + '{}'.format(db.engine))
+    print(Style.RESET_ALL)
+    
+    
+class TableModelView(DatasourceModelView, DeleteMixin, YamlExportMixin):  # noqa
+    datamodel = SQLAInterface(models.SqlaTable)
+    
+        @property
+    def data(self):
+        # work around for https://github.com/pandas-dev/pandas/issues/18372
+        return [dict((k, _maybe_box_datetimelike(v))
+                     for k, v in zip(self.__df.columns, np.atleast_1d(row)))
+                for row in self.__df.values]
+    
+    FORM_DATA_KEY_WHITELIST = list(frontend_config.get('controls').keys()) + ['slice_id']
+    
+    
+def upgrade():
+    op.add_column('datasources', sa.Column('params', sa.String(length=1000), nullable=True))
