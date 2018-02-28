19a361374e0ef629b398a82b8ee5d35c0e4ebd1d
SUNsung <weatherius@gmail.com>
2018-02-28 07:59:43 +0200

    Rewrite [index.cpp] File in: 2018-02-28 07:59:43

--- cccccc/index.cpp
+++ cccccc/index.cpp
@@ -1,174 +1,255 @@
 
         
-        #include 'atom/common/api/api_messages.h'
-#include 'atom/common/native_mate_converters/string16_converter.h'
-#include 'content/public/browser/web_contents.h'
+          // Initializes all members to the default options.
+  PluginConfig();
+    
+    bool HloReachabilityMap::IsConnected(const HloInstruction* a,
+                                     const HloInstruction* b) const {
+  return IsReachable(a, b) || IsReachable(b, a);
+}
+    
+    #if GOOGLE_CUDA
+#define REGISTER_DYNAMIC_STITCH_GPU(type)                \
+  REGISTER_KERNEL_BUILDER(Name('DynamicStitch')          \
+                              .Device(DEVICE_GPU)        \
+                              .TypeConstraint<type>('T') \
+                              .HostMemory('indices'),    \
+                          DynamicStitchOpGPU<type>)      \
+  REGISTER_KERNEL_BUILDER(Name('ParallelDynamicStitch')  \
+                              .Device(DEVICE_GPU)        \
+                              .TypeConstraint<type>('T') \
+                              .HostMemory('indices')     \
+                              .HostMemory('data')        \
+                              .HostMemory('merged'),     \
+                          ParallelDynamicStitchOpCPU<type>)
+    
+    #include 'atom/browser/api/event.h'
+    
+    #include 'atom/browser/api/event.h'
+#include 'native_mate/arguments.h'
+#include 'native_mate/dictionary.h'
 #include 'native_mate/object_template_builder.h'
+#include 'ui/events/event_constants.h'
     
-    #include 'atom/common/node_includes.h'
-    
-    
-    { private:
-  DISALLOW_COPY_AND_ASSIGN(AtomQuotaPermissionContext);
-};
-    
-    #include 'base/strings/string16.h'
-#include 'base/synchronization/lock.h'
-#include 'content/public/browser/resource_dispatcher_host_login_delegate.h'
+      const net::AuthChallengeInfo* auth_info() const { return auth_info_.get(); }
     
     
-    {}  // namespace atom
+    {}  // namespace accelerator_util
+    
+    int main(int argc, char** argv) {
+  if (argc != 4) {
+    printf('This script converts the MNIST dataset to the leveldb format used\n'
+           'by caffe to train a siamese network.\n'
+           'Usage:\n'
+           '    convert_mnist_data input_image_file input_label_file '
+           'output_db_file\n'
+           'The MNIST dataset could be downloaded at\n'
+           '    http://yann.lecun.com/exdb/mnist/\n'
+           'You should gunzip them after downloading.\n');
+  } else {
+    google::InitGoogleLogging(argv[0]);
+    convert_dataset(argv[1], argv[2], argv[3]);
+  }
+  return 0;
+}
+#else
+int main(int argc, char** argv) {
+  LOG(FATAL) << 'This example requires LevelDB; compile with USE_LEVELDB.';
+}
+#endif  // USE_LEVELDB
 
     
-    struct CV_EXPORTS IntrinsicParams
-{
-    Vec2d f;
-    Vec2d c;
-    Vec4d k;
-    double alpha;
-    std::vector<uchar> isEstimate;
+      /**
+   * @brief Applies the transformation defined in the data layer's
+   * transform_param block to a cv::Mat
+   *
+   * @param cv_img
+   *    cv::Mat containing the data to be transformed.
+   * @param transformed_blob
+   *    This is destination blob. It can be part of top blob's data if
+   *    set_cpu_data() is used. See image_data_layer.cpp for an example.
+   */
+  void Transform(const cv::Mat& cv_img, Blob<Dtype>* transformed_blob);
+#endif  // USE_OPENCV
+    
+    #include <utility>
+#include <vector>
+    
+    /**
+ * @brief Takes at least two Blob%s and concatenates them along either the num
+ *        or channel dimension, outputting the result.
+ */
+template <typename Dtype>
+class ConcatLayer : public Layer<Dtype> {
+ public:
+  explicit ConcatLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
     }
     
-    static void orderContours(const std::vector<std::vector<Point> >& contours, Point2f point, std::vector<std::pair<int, float> >& order)
+    #endif  // CAFFE_CONV_LAYER_HPP_
+
+    
+    
+    {  /// when divided by UINT_MAX, the randomly generated values @f$u\sim U(0,1)@f$
+  Blob<unsigned int> rand_vec_;
+  /// the probability @f$ p @f$ of dropping any input
+  Dtype threshold_;
+  /// the scale for undropped inputs at train time @f$ 1 / (1 - p) @f$
+  Dtype scale_;
+  unsigned int uint_thres_;
+};
+    
+    /**
+ * @brief Compute elementwise operations, such as product and sum,
+ *        along multiple input Blobs.
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+template <typename Dtype>
+class EltwiseLayer : public Layer<Dtype> {
+ public:
+  explicit EltwiseLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    }
+    
+    /** @defgroup buffer_cast boost::asio::buffer_cast
+ *
+ * @brief The boost::asio::buffer_cast function is used to obtain a pointer to
+ * the underlying memory region associated with a buffer.
+ *
+ * @par Examples:
+ *
+ * To access the memory of a non-modifiable buffer, use:
+ * @code boost::asio::const_buffer b1 = ...;
+ * const unsigned char* p1 = boost::asio::buffer_cast<const unsigned char*>(b1);
+ * @endcode
+ *
+ * To access the memory of a modifiable buffer, use:
+ * @code boost::asio::mutable_buffer b2 = ...;
+ * unsigned char* p2 = boost::asio::buffer_cast<unsigned char*>(b2);
+ * @endcode
+ *
+ * The boost::asio::buffer_cast function permits violations of type safety, so
+ * uses of it in application code should be carefully considered.
+ */
+/*@{*/
+    }
+    
+    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+    
+    #ifndef BOOST_ASIO_DETAIL_BASE_FROM_COMPLETION_COND_HPP
+#define BOOST_ASIO_DETAIL_BASE_FROM_COMPLETION_COND_HPP
+    
+    #if !defined(BOOST_ASIO_WINDOWS) && !defined(__CYGWIN__)
+    
+    #if !defined(BOOST_ASIO_HAS_THREADS)
+typedef null_event event;
+#elif defined(BOOST_ASIO_WINDOWS)
+typedef win_event event;
+#elif defined(BOOST_ASIO_HAS_PTHREADS)
+typedef posix_event event;
+#elif defined(BOOST_ASIO_HAS_STD_MUTEX_AND_CONDVAR)
+typedef std_event event;
+#endif
+    
+    template <typename Function, typename Context>
+inline void invoke(const Function& function, Context& context)
 {
-    order.clear();
-    size_t i, j, n = contours.size();
-    for(i = 0; i < n; i++)
-    {
-        size_t ni = contours[i].size();
-        double min_dist = std::numeric_limits<double>::max();
-        for(j = 0; j < ni; j++)
+#if !defined(BOOST_ASIO_HAS_HANDLER_HOOKS)
+  Function tmp(function);
+  tmp();
+#else
+  using boost::asio::asio_handler_invoke;
+  asio_handler_invoke(function, boost::asio::detail::addressof(context));
+#endif
+}
+    
+    class winrt_buffer_impl :
+  public Microsoft::WRL::RuntimeClass<
+    Microsoft::WRL::RuntimeClassFlags<
+      Microsoft::WRL::RuntimeClassType::WinRtClassicComMix>,
+    ABI::Windows::Storage::Streams::IBuffer,
+    Windows::Storage::Streams::IBufferByteAccess>
+{
+public:
+  explicit winrt_buffer_impl(const boost::asio::const_buffer& b)
+  {
+    bytes_ = const_cast<byte*>(boost::asio::buffer_cast<const byte*>(b));
+    length_ = boost::asio::buffer_size(b);
+    capacity_ = boost::asio::buffer_size(b);
+  }
+    }
+    
+      op_queue<operation> ops;
+    
+    
+    {        std::string arg0_tmp; ok &= luaval_to_std_string(tolua_S, 2, &arg0_tmp, 'cc.SimpleAudioEngine:preloadBackgroundMusic'); arg0 = arg0_tmp.c_str();
+        if(!ok)
         {
-            double dist = norm(Point2f((float)contours[i][j].x, (float)contours[i][j].y) - point);
-            min_dist = MIN(min_dist, dist);
+            tolua_error(tolua_S,'invalid arguments in function 'lua_cocos2dx_cocosdenshion_SimpleAudioEngine_preloadBackgroundMusic'', nullptr);
+            return 0;
         }
-        order.push_back(std::pair<int, float>((int)i, (float)min_dist));
+        cobj->preloadBackgroundMusic(arg0);
+        lua_settop(tolua_S, 1);
+        return 1;
     }
-    }
+    luaL_error(tolua_S, '%s has wrong number of arguments: %d, was expecting %d \n', 'cc.SimpleAudioEngine:preloadBackgroundMusic',argc, 1);
+    return 0;
     
-    #ifndef __OPENCV_CORE_BUFFER_POOL_IMPL_HPP__
-#define __OPENCV_CORE_BUFFER_POOL_IMPL_HPP__
     
-        struct InitializeVariables
-    {
-        InitializeVariables()
-        {
-            // Extension: 1.1
-            CullFace = Switch_CullFace;
-            FrontFace = Switch_FrontFace;
-            Hint = Switch_Hint;
-            LineWidth = Switch_LineWidth;
-            PointSize = Switch_PointSize;
-            PolygonMode = Switch_PolygonMode;
-            Scissor = Switch_Scissor;
-            TexParameterf = Switch_TexParameterf;
-            TexParameterfv = Switch_TexParameterfv;
-            TexParameteri = Switch_TexParameteri;
-            TexParameteriv = Switch_TexParameteriv;
-            TexImage1D = Switch_TexImage1D;
-            TexImage2D = Switch_TexImage2D;
-            DrawBuffer = Switch_DrawBuffer;
-            Clear = Switch_Clear;
-            ClearColor = Switch_ClearColor;
-            ClearStencil = Switch_ClearStencil;
-            ClearDepth = Switch_ClearDepth;
-            StencilMask = Switch_StencilMask;
-            ColorMask = Switch_ColorMask;
-            DepthMask = Switch_DepthMask;
-            Disable = Switch_Disable;
-            Enable = Switch_Enable;
-            Finish = Switch_Finish;
-            Flush = Switch_Flush;
-            BlendFunc = Switch_BlendFunc;
-            LogicOp = Switch_LogicOp;
-            StencilFunc = Switch_StencilFunc;
-            StencilOp = Switch_StencilOp;
-            DepthFunc = Switch_DepthFunc;
-            PixelStoref = Switch_PixelStoref;
-            PixelStorei = Switch_PixelStorei;
-            ReadBuffer = Switch_ReadBuffer;
-            ReadPixels = Switch_ReadPixels;
-            GetBooleanv = Switch_GetBooleanv;
-            GetDoublev = Switch_GetDoublev;
-            GetError = Switch_GetError;
-            GetFloatv = Switch_GetFloatv;
-            GetIntegerv = Switch_GetIntegerv;
-            GetString = Switch_GetString;
-            GetTexImage = Switch_GetTexImage;
-            GetTexParameterfv = Switch_GetTexParameterfv;
-            GetTexParameteriv = Switch_GetTexParameteriv;
-            GetTexLevelParameterfv = Switch_GetTexLevelParameterfv;
-            GetTexLevelParameteriv = Switch_GetTexLevelParameteriv;
-            IsEnabled = Switch_IsEnabled;
-            DepthRange = Switch_DepthRange;
-            Viewport = Switch_Viewport;
-            DrawArrays = Switch_DrawArrays;
-            DrawElements = Switch_DrawElements;
-            GetPointerv = Switch_GetPointerv;
-            PolygonOffset = Switch_PolygonOffset;
-            CopyTexImage1D = Switch_CopyTexImage1D;
-            CopyTexImage2D = Switch_CopyTexImage2D;
-            CopyTexSubImage1D = Switch_CopyTexSubImage1D;
-            CopyTexSubImage2D = Switch_CopyTexSubImage2D;
-            TexSubImage1D = Switch_TexSubImage1D;
-            TexSubImage2D = Switch_TexSubImage2D;
-            BindTexture = Switch_BindTexture;
-            DeleteTextures = Switch_DeleteTextures;
-            GenTextures = Switch_GenTextures;
-            IsTexture = Switch_IsTexture;
-            Indexub = Switch_Indexub;
-            Indexubv = Switch_Indexubv;
+#endif // __cocos2dx_experimental_h__
+
+    
+    
+    
+    #endif
+
+    
+    
+    {		Test::Step(settings);
+	}
+    
+    			b2RevoluteJointDef jd;
+    
+    
+    {    int hashedToken = 0;
+    while (isalnum(*in) || *in == '-' || *in == '_') {
+        if (isdigit(*in)) {
+            hashedToken = hashedToken * 10 - (*in - '0');
+        } else {
+            hashedToken += *in;
+        }
+        in++;
     }
+    return hashedToken;
+}
+    
+    namespace uS {
     }
     
-    #ifndef GL_LOAD_GEN_BASIC_OPENGL_TYPEDEFS
-#define GL_LOAD_GEN_BASIC_OPENGL_TYPEDEFS
-    typedef unsigned int GLenum;
-    typedef unsigned char GLboolean;
-    typedef unsigned int GLbitfield;
-    typedef signed char GLbyte;
-    typedef short GLshort;
-    typedef int GLint;
-    typedef int GLsizei;
-    typedef unsigned char GLubyte;
-    typedef unsigned short GLushort;
-    typedef unsigned int GLuint;
-    typedef float GLfloat;
-    typedef float GLclampf;
-    typedef double GLdouble;
-    typedef double GLclampd;
-    #define GLvoid void
-#endif // GL_LOAD_GEN_BASIC_OPENGL_TYPEDEFS
+        static void forceClose(WebSocketState<isServer> *webSocketState) {
+        WebSocket<isServer> *webSocket = static_cast<WebSocket<isServer> *>(webSocketState);
+        webSocket->terminate();
+    }
     
-    bool js_cocos2dx_physics3d_PhysicsSprite3D_constructor(JSContext *cx, uint32_t argc, jsval *vp);
-void js_cocos2dx_physics3d_PhysicsSprite3D_finalize(JSContext *cx, JSObject *obj);
-void js_register_cocos2dx_physics3d_PhysicsSprite3D(JSContext *cx, JS::HandleObject global);
-void register_all_cocos2dx_physics3d(JSContext* cx, JS::HandleObject obj);
-bool js_cocos2dx_physics3d_PhysicsSprite3D_syncNodeToPhysics(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_PhysicsSprite3D_syncPhysicsToNode(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_PhysicsSprite3D_getPhysicsObj(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_PhysicsSprite3D_setSyncFlag(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_PhysicsSprite3D_PhysicsSprite3D(JSContext *cx, uint32_t argc, jsval *vp);
+        void run();
     
-    
-    
-    
-    
-        //    glPointSize(size);
-    
-    			b2FixtureDef sd;
-			sd.shape = &shape;
-			sd.density = 0.0f;
-			sd.restitution = k_restitution;
-    
-    	b2Body* m_body1;
-	b2Vec2 m_velocity;
-	float32 m_angularVelocity;
-	b2PolygonShape m_shape1;
-	b2PolygonShape m_shape2;
-	b2Fixture* m_piece1;
-	b2Fixture* m_piece2;
-    
-    			b2FixtureDef fd;
-			fd.shape = &shape;
-			fd.density = 1.0f;
+        uv_os_sock_t getFd() {
+#ifdef _WIN32
+        uv_os_sock_t fd;
+        uv_fileno((uv_handle_t *) uv_poll, (uv_os_fd_t *) &fd);
+        return fd;
+#else
+        return uv_poll->io_watcher.fd;
+#endif
+    }
