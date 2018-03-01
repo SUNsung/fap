4421c0b2c3951a3ee09b35c6c21a994267909d34
SUNsung <weatherius@gmail.com>
2018-03-01 06:27:13 +0200

    Rewrite [28aJTUE77Ne5k28QgiEnm5S.yg28mwO2fPZYocA28xyK.cpp] File in: 2018-03-01 06:27:12

--- cccccc/28aJTUE77Ne5k28QgiEnm5S.yg28mwO2fPZYocA28xyK.cpp
+++ cccccc/28aJTUE77Ne5k28QgiEnm5S.yg28mwO2fPZYocA28xyK.cpp
@@ -1,184 +1,234 @@
-std::shared_ptr<Channel> CreateTestChannel(
-    const grpc::string& server, const grpc::string& override_hostname,
-    bool enable_ssl, bool use_prod_roots,
-    const std::shared_ptr<CallCredentials>& creds,
-    const ChannelArguments& args);
-    
-    
-    {  return 0;
-}
 
+        
+          /// DebuggerClient is asked to emit SIL references to locals,
+  /// permitting SILGen to access them like any other variables.
+  /// This avoids generation of properties.
+  virtual SILValue emitLValueForVariable(VarDecl *var,
+                                         SILBuilder &builder) = 0;
     
-    #include 'test/cpp/qps/server.h'
+    #include 'swift/AST/SyntaxASTMap.h'
+#include 'swift/AST/Expr.h'
+#include 'swift/AST/Decl.h'
+#include 'swift/AST/Stmt.h'
+#include 'swift/Syntax/Syntax.h'
     
+    namespace llvm {
+class FoldingSetNodeID;
+} // end namespace llvm
     
-    { private:
-  std::unique_ptr<Greeter::Stub> stub_;
-};
+      /// Treat all warnings as errors
+  bool WarningsAsErrors = false;
     
-    // Logic and data behind the server's behavior.
-class GreeterServiceImpl final : public Greeter::Service {
-  Status SayHello(ServerContext* context, const HelloRequest* request,
-                  HelloReply* reply) override {
-    std::string prefix('Hello ');
-    reply->set_message(prefix + request->name());
-    return Status::OK;
-  }
-};
-    
-    class PythonGrpcGenerator : public grpc::protobuf::compiler::CodeGenerator {
- public:
-  PythonGrpcGenerator(const GeneratorConfiguration& config);
-  ~PythonGrpcGenerator();
+                template<typename T>
+            static __device__ __forceinline__ T atomicAdd(T* address, T val)
+            {
+#if defined (__CUDA_ARCH__) && (__CUDA_ARCH__ < 120)
+                T count;
+                unsigned int tag = threadIdx.x << ( (sizeof(unsigned int) << 3) - 5U);
+                do
+                {
+                    count = *address & TAG_MASK;
+                    count = tag | (count + val);
+                    *address = count;
+                } while (*address != count);
     }
     
-    #include 'android_native_app_glue.h'
-#include 'animal_generated.h' // Includes 'flatbuffers/flatbuffers.h'.
+            if (!isContinuous())
+            CV_Error(cv::Error::BadStep, 'The matrix is not continuous, thus its number of rows can not be changed');
     
-    
-	//Service Descriptor
-	printer->Print(vars, 'var $ServiceDesc$ = $grpc$.ServiceDesc{\n');
-	printer->Indent();
-	printer->Print(vars, 'ServiceName: \'$Package$.$Service$\',\n');
-	printer->Print(vars, 'HandlerType: (*$Service$Server)(nil),\n');
-	printer->Print(vars, 'Methods: []$grpc$.MethodDesc{\n');
-	printer->Indent();
-	for (int i = 0; i < service->method_count(); i++) {
-		auto method = service->method(i);
-		vars['Method'] = method->name();
-		vars['Handler'] = '_' + vars['Service'] + '_' + vars['Method'] + '_Handler';
-		if (method->NoStreaming()) {
-			printer->Print('{\n');
-			printer->Indent();
-			printer->Print(vars, 'MethodName: \'$Method$\',\n');
-			printer->Print(vars, 'Handler: $Handler$, \n');
-			printer->Outdent();
-			printer->Print('},\n');
-		}
-	}
-	printer->Outdent();
-	printer->Print('},\n');
-	printer->Print(vars, 'Streams: []$grpc$.StreamDesc{\n');
-	printer->Indent();
-	for (int i = 0; i < service->method_count(); i++) {
-		auto method = service->method(i);
-		vars['Method'] = method->name();
-		vars['Handler'] = '_' + vars['Service'] + '_' + vars['Method'] + '_Handler';
-		if (!method->NoStreaming()) {
-			printer->Print('{\n');
-			printer->Indent();
-			printer->Print(vars, 'StreamName: \'$Method$\',\n');
-			printer->Print(vars, 'Handler: $Handler$, \n');
-			if (ClientOnlyStreaming(method.get())) {
-				printer->Print('ClientStreams: true,\n');
-			} else if (ServerOnlyStreaming(method.get())) {
-				printer->Print('ServerStreams: true,\n');
-			} else {
-				printer->Print('ServerStreams: true,\n');
-				printer->Print('ClientStreams: true,\n');
-			}
-			printer->Outdent();
-			printer->Print('},\n');
-		}
-	}
-	printer->Outdent();
-	printer->Print('},\n');
-	printer->Outdent();
-	printer->Print('}\n\n');
-    
-    #include 'src/compiler/schema_interface.h'
-    
-    inline flatbuffers::Offset<KeyValue> CreateKeyValue(
-    flatbuffers::FlatBufferBuilder &_fbb,
-    flatbuffers::Offset<flatbuffers::String> key = 0,
-    flatbuffers::Offset<flatbuffers::String> value = 0) {
-  KeyValueBuilder builder_(_fbb);
-  builder_.add_value(value);
-  builder_.add_key(key);
-  return builder_.Finish();
+    // Returns the number of misfit blob tops in this word.
+int Tesseract::CountMisfitTops(WERD_RES *word_res) {
+  int bad_blobs = 0;
+  int num_blobs = word_res->rebuild_word->NumBlobs();
+  for (int blob_id = 0; blob_id < num_blobs; ++blob_id) {
+    TBLOB* blob = word_res->rebuild_word->blobs[blob_id];
+    UNICHAR_ID class_id = word_res->best_choice->unichar_id(blob_id);
+    if (unicharset.get_isalpha(class_id) || unicharset.get_isdigit(class_id)) {
+      int top = blob->bounding_box().top();
+      if (top >= INT_FEAT_RANGE)
+        top = INT_FEAT_RANGE - 1;
+      int min_bottom, max_bottom, min_top, max_top;
+      unicharset.get_top_bottom(class_id, &min_bottom, &max_bottom,
+                                &min_top, &max_top);
+      if (max_top - min_top > kMaxCharTopRange)
+        continue;
+      bool bad =  top < min_top - x_ht_acceptance_tolerance ||
+                  top > max_top + x_ht_acceptance_tolerance;
+      if (bad)
+        ++bad_blobs;
+      if (debug_x_ht_level >= 1) {
+        tprintf('Class %s is %s with top %d vs limits of %d->%d, +/-%d\n',
+                unicharset.id_to_unichar(class_id),
+                bad ? 'Misfit' : 'OK', top, min_top, max_top,
+                static_cast<int>(x_ht_acceptance_tolerance));
+      }
+    }
+  }
+  return bad_blobs;
 }
     
-      TypedVector Keys() const {
-    const size_t num_prefixed_fields = 3;
-    auto keys_offset = data_ - byte_width_ * num_prefixed_fields;
-    return TypedVector(Indirect(keys_offset, byte_width_),
-                       static_cast<uint8_t>(
-                           ReadUInt64(keys_offset + byte_width_, byte_width_)),
-                       TYPE_KEY);
-  }
     
-    struct IterationVisitor {
-  // These mark the scope of a table or struct.
-  virtual void StartSequence() {}
-  virtual void EndSequence() {}
-  // Called for each field regardless of wether it is present or not.
-  // If not present, val == nullptr. set_idx is the index of all set fields.
-  virtual void Field(size_t /*field_idx*/, size_t /*set_idx*/,
-                     ElementaryType /*type*/, bool /*is_vector*/,
-                     const TypeTable * /*type_table*/, const char * /*name*/,
-                     const uint8_t * /*val*/) {}
-  // Called for a value that is actually present, after a field, or as part
-  // of a vector.
-  virtual void UType(uint8_t, const char *) {}
-  virtual void Bool(bool) {}
-  virtual void Char(int8_t, const char *) {}
-  virtual void UChar(uint8_t, const char *) {}
-  virtual void Short(int16_t, const char *) {}
-  virtual void UShort(uint16_t, const char *) {}
-  virtual void Int(int32_t, const char *) {}
-  virtual void UInt(uint32_t, const char *) {}
-  virtual void Long(int64_t) {}
-  virtual void ULong(uint64_t) {}
-  virtual void Float(float) {}
-  virtual void Double(double) {}
-  virtual void String(const String *) {}
-  virtual void Unknown(const uint8_t *) {}  // From a future version.
-  // These mark the scope of a vector.
-  virtual void StartVector() {}
-  virtual void EndVector() {}
-  virtual void Element(size_t /*i*/, ElementaryType /*type*/,
-                       const TypeTable * /*type_table*/,
-                       const uint8_t * /*val*/) {}
-  virtual ~IterationVisitor() {}
+    {  name += UNLV_EXT;              //add extension
+  if ((pdfp = fopen (name.string (), 'rb')) == NULL) {
+    return false;                //didn't read one
+  } else {
+    while (tfscanf(pdfp, '%d %d %d %d %*s', &x, &y, &width, &height) >= 4) {
+                                 //make rect block
+      block = new BLOCK (name.string (), TRUE, 0, 0,
+                         (inT16) x, (inT16) (ysize - y - height),
+                         (inT16) (x + width), (inT16) (ysize - y));
+                                 //on end of list
+      block_it.add_to_end (block);
+    }
+    fclose(pdfp);
+  }
+  return true;
+}
+    
+    #include 'ccstruct.h'
+    
+    // Solve the dynamic programming problem for the given array of points, with
+// the given size and cost function.
+// Steps backwards are limited to being between min_step and max_step
+// inclusive.
+// The return value is the tail of the best path.
+DPPoint* DPPoint::Solve(int min_step, int max_step, bool debug,
+                        CostFunc cost_func, int size, DPPoint* points) {
+  if (size <= 0 || max_step < min_step || min_step >= size)
+    return NULL;  // Degenerate, but not necessarily an error.
+  ASSERT_HOST(min_step > 0);  // Infinite loop possible if this is not true.
+  if (debug)
+    tprintf('min = %d, max=%d\n',
+            min_step, max_step);
+  // Evaluate the total cost at each point.
+  for (int i = 0; i < size; ++i) {
+    for (int offset = min_step; offset <= max_step; ++offset) {
+      DPPoint* prev = offset <= i ? points + i - offset : NULL;
+      inT64 new_cost = (points[i].*cost_func)(prev);
+      if (points[i].best_prev_ != NULL && offset > min_step * 2 &&
+          new_cost > points[i].total_cost_)
+        break;  // Find only the first minimum if going over twice the min.
+    }
+    points[i].total_cost_ += points[i].local_cost_;
+    if (debug) {
+      tprintf('At point %d, local cost=%d, total_cost=%d, steps=%d\n',
+              i, points[i].local_cost_, points[i].total_cost_,
+              points[i].total_steps_);
+    }
+  }
+  // Now find the end of the best path and return it.
+  int best_cost = points[size - 1].total_cost_;
+  int best_end = size - 1;
+  for (int end = best_end - 1; end >= size - min_step; --end) {
+    int cost = points[end].total_cost_;
+    if (cost < best_cost) {
+      best_cost = cost;
+      best_end = end;
+    }
+  }
+  return points + best_end;
+}
+    
+    inline FCOORD operator *(                    //scalar multiply
+                         const FCOORD &op1,  //operands
+                         float scale) {
+  FCOORD result;                 //output
+    }
+    
+    
+    {  protected:
+    inT16 xcoord;                //< x value
+    inT16 ycoord;                //< y value
 };
     
-    static std::string GenType(const Type &type, bool underlying = false) {
-  switch (type.base_type) {
-    case BASE_TYPE_STRUCT:
-      return type.struct_def->defined_namespace->GetFullyQualifiedName(
-          type.struct_def->name);
-    case BASE_TYPE_VECTOR: return '[' + GenType(type.VectorType()) + ']';
-    default:
-      if (type.enum_def && !underlying) {
-        return type.enum_def->defined_namespace->GetFullyQualifiedName(
-            type.enum_def->name);
-      } else {
-        return kTypeNames[type.base_type];
-      }
+      auto elems = arr->types();
+  for (auto& t : types) {
+    *elems++ = t;
   }
+    
+    void Timer::GetRealtimeTime(timespec &ts) {
+  gettime(CLOCK_REALTIME, &ts);
 }
     
-            if (ssl) {
-            if (!h.listen(3000,
-                          uS::TLS::createContext('misc/ssl/cert.pem',
-                          'misc/ssl/key.pem', '1234'))) {
-                std::cerr << 'FAILURE: Cannot listen!' << std::endl;
-                exit(-1);
-            }
-            h.connect('wss://localhost:3000', nullptr);
-        } else {
-            if (!h.listen(3000)) {
-                std::cerr << 'FAILURE: Cannot listen!' << std::endl;
-                exit(-1);
-            }
-            h.connect('ws://localhost:3000', nullptr);
-        }
+    #ifndef incl_HPHP_PAGELET_SERVER_H_
+#define incl_HPHP_PAGELET_SERVER_H_
     
-            char mask[4];
-        if (!isServer) {
-            dst[1] |= 0x80;
-            uint32_t random = rand();
-            memcpy(mask, &random, 4);
-            memcpy(dst + headerLength, &random, 4);
-            headerLength += 4;
-        }
+    
+    {
+    {
+    {}}}
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    
+    static int _php_mbstr_parse_mail_headers(Array &ht, const char *str,
+                                         size_t str_len) {
+  const char *ps;
+  size_t icnt;
+  int state = 0;
+  int crlf_state = -1;
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    
+      // We lower Switch to a series of comparisons if any of the successors are in
+  // included in the region.
+  auto const shouldLower =
+    std::any_of(offsets.begin(), offsets.end(), [&](Offset o) {
+      SrcKey sk(curSrcKey(env), bcOff(env) + o);
+      return env.irb->hasBlock(sk);
+    });
+  if (shouldLower && profile.optimizing()) {
+    auto const values = sortedSwitchProfile(profile, iv.size());
+    FTRACE(2, 'Switch profile data for Switch @ {}\n', bcOff(env));
+    for (UNUSED auto const& val : values) {
+      FTRACE(2, '  case {} hit {} times\n', val.caseIdx, val.count);
+    }
+    }
+    
+    extern const std::string NIL;
+    
+    
+    {} // namespace aria2
+
+    
+    protected:
+  virtual bool prepareForRetry(time_t wait);
+    
+    
+    {  virtual void dropCache(int64_t len, int64_t offset) CXX11_OVERRIDE;
+};
+    
+    class AbstractProxyResponseCommand : public AbstractCommand {
+private:
+  std::shared_ptr<HttpConnection> httpConnection_;
+    }
+    
+    void AnnounceList::reconfigure(
+    const std::vector<std::vector<std::string>>& announceList)
+{
+  for (const auto& vec : announceList) {
+    if (vec.empty()) {
+      continue;
+    }
+    }
+    }
+    
+    void AnnounceTier::nextEvent()
+{
+  switch (event) {
+  case STARTED:
+    event = DOWNLOADING;
+    break;
+  case STARTED_AFTER_COMPLETION:
+    event = SEEDING;
+    break;
+  case STOPPED:
+    event = HALTED;
+    break;
+  case COMPLETED:
+    event = SEEDING;
+    break;
+  default:
+    break;
+  }
+}
