f0a9174d2b00c2d656144407c173937f6a189eb1
SUNsung <weatherius@gmail.com>
2018-03-01 14:20:48 +0200

    Rewrite [29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py] File in: 2018-03-01 14:20:48

--- pyton/29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py
+++ pyton/29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py
@@ -1,250 +1,89 @@
 
         
         
-def gen_extractors():
-    ''' Return a list of an instance of every supported extractor.
-    The order does matter; the first extractor matched is the one handling the URL.
+def get_db():
+    '''Opens a new database connection if there is none yet for the
+    current application context.
     '''
-    return [klass() for klass in gen_extractor_classes()]
-    
-        _TEST = {
-        'url': 'http://www.anitube.se/video/36621',
-        'md5': '59d0eeae28ea0bc8c05e7af429998d43',
-        'info_dict': {
-            'id': '36621',
-            'ext': 'mp4',
-            'title': 'Recorder to Randoseru 01',
-            'duration': 180.19,
-        },
-        'skip': 'Blocked in the US',
-    }
-    
-    from .common import InfoExtractor
-from ..utils import js_to_json
+    top = _app_ctx_stack.top
+    if not hasattr(top, 'sqlite_db'):
+        top.sqlite_db = sqlite3.connect(app.config['DATABASE'])
+        top.sqlite_db.row_factory = sqlite3.Row
+    return top.sqlite_db
     
     
-class EnvironmentConfig(object):
-    uri = os.getenv('LANDSCAPE_API_URI')
-    access_key = os.getenv('LANDSCAPE_API_KEY')
-    secret_key = os.getenv('LANDSCAPE_API_SECRET')
-    ssl_ca_file = os.getenv('LANDSCAPE_API_SSL_CA_FILE')
-    
-    try:
-    import json
-except ImportError:
-    import simplejson as json
-import os
-import os.path
-import sys
-import ConfigParser
-import StringIO
-    
-    # Note, sha1 is the only hash algorithm compatible with python2.4 and with
-# FIPS-140 mode (as of 11-2014)
-try:
-    from hashlib import sha1 as sha1
-except ImportError:
-    from sha import sha as sha1
-    
-        try:
-        with open(path, 'r') as key_fd:
-            return key_fd.read().strip()
-    except IOError:
-        return None
-    
-        @g_connect
-    def get_import_task(self, task_id=None, github_user=None, github_repo=None):
-        '''
-        Check the status of an import task.
-        '''
-        url = '%s/imports/' % self.baseurl
-        if task_id is not None:
-            url = '%s?id=%d' % (url, task_id)
-        elif github_user is not None and github_repo is not None:
-            url = '%s?github_user=%s&github_repo=%s' % (url, github_user, github_repo)
-        else:
-            raise AnsibleError('Expected task_id or github_user and github_repo')
-    
-            params1 = {
-            'creationTimestamp': '2017-04-21T11:19:20.718-07:00',
-            'defaultService': 'https://www.googleapis.com/compute/v1/projects/myproject/global/backendServices/default-backend-service',
-            'description': '',
-            'fingerprint': 'ickr_pwlZPU=',
-            'hostRules': [
-                {
-                    'description': '',
-                    'hosts': [
-                        '*.'
-                    ],
-                    'pathMatcher': 'path-matcher-one'
-                }
-            ],
-            'id': '8566395781175047111',
-            'kind': 'compute#urlMap',
-            'name': 'newtesturlmap-foo',
-            'pathMatchers': [
-                {
-                    'defaultService': 'https://www.googleapis.com/compute/v1/projects/myproject/global/backendServices/bes-pathmatcher-one-default',
-                    'description': 'path matcher one',
-                    'name': 'path-matcher-one',
-                    'pathRules': [
-                        {
-                            'paths': [
-                                '/data',
-                                '/aboutus'
-                            ],
-                            'service': 'https://www.googleapis.com/compute/v1/projects/myproject/global/backendServices/my-one-bes'
-                        }
-                    ]
-                }
-            ],
-            'selfLink': 'https://www.googleapis.com/compute/v1/projects/myproject/global/urlMaps/newtesturlmap-foo'
-        }
-        params2 = {
-            'defaultService': 'https://www.googleapis.com/compute/v1/projects/myproject/global/backendServices/default-backend-service',
-            'hostRules': [
-                {
-                    'description': '',
-                    'hosts': [
-                        '*.'
-                    ],
-                    'pathMatcher': 'path-matcher-one'
-                }
-            ],
-            'name': 'newtesturlmap-foo',
-            'pathMatchers': [
-                {
-                    'defaultService': 'https://www.googleapis.com/compute/v1/projects/myproject/global/backendServices/bes-pathmatcher-one-default',
-                    'description': 'path matcher one',
-                    'name': 'path-matcher-one',
-                    'pathRules': [
-                        {
-                            'paths': [
-                                '/data',
-                                '/aboutus'
-                            ],
-                            'service': 'https://www.googleapis.com/compute/v1/projects/myproject/global/backendServices/my-one-bes'
-                        }
-                    ]
-                }
-            ],
-        }
-    
-        model.train_on_batch(x_train[:32], y_train[:32],
-                         class_weight=class_weight)
-    score = model.evaluate(x_test[test_ids, :], y_test[test_ids, :], verbose=0)
-    assert(score < standard_score_sequential)
+def ptb_producer(raw_data, batch_size, num_steps, name=None):
+  '''Iterate on the raw PTB data.
     
     
-@keras_test
-def test_atrousconv2d_legacy_interface():
-    old_layer = keras.layers.AtrousConvolution2D(
-        5, 3, 3,
-        atrous_rate=(2, 2),
-        init='normal',
-        subsample=(2, 2),
-        border_mode='valid',
-        dim_ordering='th',
-        W_regularizer='l1',
-        b_regularizer='l2',
-        W_constraint='maxnorm',
-        b_constraint='unitnorm',
-        name='conv')
-    new_layer = keras.layers.Conv2D(5, (3, 3),
-                                    kernel_initializer='normal',
-                                    strides=(2, 2),
-                                    padding='valid',
-                                    kernel_regularizer='l1',
-                                    bias_regularizer='l2',
-                                    kernel_constraint='max_norm',
-                                    bias_constraint='unit_norm',
-                                    data_format='channels_first',
-                                    dilation_rate=(2, 2),
-                                    name='conv')
-    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer.get_config())
+def evaluate(defun=False):
+  model = mnist.Model(data_format())
+  dataset = random_dataset()
+  if defun:
+    model.call = tfe.defun(model.call)
+  with tf.device(device()):
+    mnist_eager.test(model, dataset)
     
-    # the data, shuffled and split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+      def _record(self, label, red, green, blue):
+    image_size = 32 * 32
+    record = bytes(bytearray([label] + [red] * image_size +
+                             [green] * image_size + [blue] * image_size))
+    expected = [[[red, green, blue]] * 32] * 32
+    return record, expected
     
     
-@pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
-def test_lecun_uniform(tensor_shape):
-    fan_in, _ = initializers._compute_fans(tensor_shape)
-    scale = np.sqrt(3. / fan_in)
-    _runner(initializers.lecun_uniform(), tensor_shape,
-            target_mean=0., target_max=scale, target_min=-scale)
-    
-    model.compile(loss='categorical_crossentropy',
-              optimizer=RMSprop(),
-              metrics=['accuracy'])
-    
-        # Arguments
-        config: dict of the form {'class_name': str, 'config': dict}
-        custom_objects: dict mapping class names (or function names)
-            of custom (non-Keras) objects to class/functions
-    
-        return os_version.dwMajorVersion
-    
-        def warn(self, fmt, *args, **kwargs):
-        self.log('WARNING', fmt, *args, **kwargs)
-    
-    # begin[licence]
 #
-# [The 'BSD licence']
-# Copyright (c) 2005-2008 Terence Parr
-# All rights reserved.
+# Backwards compat functions.  Some modules include md5s in their return values
+# Continue to support that for now.  As of ansible-1.8, all of those modules
+# should also return 'checksum' (sha1 for now)
+# Do not use md5 unless it is needed for:
+# 1) Optional backwards compatibility
+# 2) Compliance with a third party protocol
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
+# MD5 will not work on systems which are FIPS-140-2 compliant.
 #
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# end[licensc]
     
-        stream_types = [  #this is just a sample. Will make it in prepare()
-        # {'id': '1080'},
-        # {'id': '720'},
-        # {'id': '360'},
-        # {'id': '288'},
-        # {'id': '190'},
-        # {'id': '180'},
-        
-    ]
+        url = 'https://api.shippable.com/projects'
+    response = requests.get(url, data, headers=headers)
     
-        title = r1(r'<meta name='description' content='([^']*)'', html)
-    flashvars = r1(r'flashvars='(type=[^']*)'', html)
+        # This matches a hostname or host pattern including [x:y(:z)] ranges.
+    #
+    # We roughly follow DNS rules here, but also allow ranges (and underscores).
+    # In the past, no systematic rules were enforced about inventory hostnames,
+    # but the parsing context (e.g. shlex.split(), fnmatch.fnmatch()) excluded
+    # various metacharacters anyway.
+    #
+    # We don't enforce DNS length restrictions here (63 characters per label,
+    # 253 characters total) or make any attempt to process IDNs.
     
-    #----------------------------------------------------------------------
-def showroom_get_roomid_by_room_url_key(room_url_key):
-    '''str->str'''
-    fake_headers_mobile = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Charset': 'UTF-8,*;q=0.5',
-        'Accept-Encoding': 'gzip,deflate,sdch',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'
-    }
-    webpage_url = 'https://www.showroom-live.com/' + room_url_key
-    html = get_content(webpage_url, headers = fake_headers_mobile)
-    roomid = match1(html, r'room\?room_id\=(\d+)')
-    assert roomid
-    return roomid
+    #      default_readme_template
+#      default_meta_template
     
-    from babel.messages.pofile import read_po
+            return '[@%d,%d:%d=%r,<%d>%s,%d:%d]' % (
+            self.index,
+            self.start, self.stop,
+            txt,
+            self.type, channelStr,
+            self.line, self.charPositionInLine
+            )
+    
+    
+    
+c = wda.Client()
+s = c.session()
+    
+        ix, iy = event.xdata, event.ydata
+    coords = [(ix, iy)]
+    print('now = ', coords)
+    cor.append(coords)
+    
+    
+# Magic Number，不设置可能无法正常执行，请根据具体截图从上到下按需设置
+under_game_score_y = config['under_game_score_y']
+# 长按的时间系数，请自己根据实际情况调节
+press_coefficient = config['press_coefficient']
+# 二分之一的棋子底座高度，可能要调节
+piece_base_height_1_2 = config['piece_base_height_1_2']
+# 棋子的宽度，比截图中量到的稍微大一点比较安全，可能要调节
+piece_body_width = config['piece_body_width']
+time_coefficient = config['press_coefficient']
