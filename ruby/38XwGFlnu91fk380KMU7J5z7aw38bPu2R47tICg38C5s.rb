37728b32b3a667e405e6769ddadb3ef5d14539b5
SUNsung <weatherius@gmail.com>
2018-03-01 04:33:06 +0200

    Rewrite [38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb] File in: 2018-03-01 04:33:05

--- ruby/38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb
+++ ruby/38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb
@@ -1,103 +1,96 @@
 
         
-            # Remove directories opposite from traversal, so that a subtree with no
-    # actual files gets removed correctly.
-    dirs.reverse_each do |d|
-      if d.children.empty?
-        puts 'rmdir: #{d} (empty)' if ARGV.verbose?
-        d.rmdir
-      end
-    end
+                command = 'security set-keychain-settings'
+        command << ' -t #{params[:timeout]}' if params[:timeout]
+        command << ' -l' if params[:lock_when_sleeps]
+        command << ' -u' if params[:lock_after_timeout]
+        command << ' #{keychain_path}'
     
-        def boolify(value)
-      agent.send(:boolify, value)
-    end
+                # Register the handler if this is our first callback.
+            Signal.trap('INT') { fire_callbacks } if registered.length == 1
+          end
+        end
     
-        if params[:sort].present?
-      attribute, direction = params[:sort].downcase.split('.')
-      unless valid_sorts.include?(attribute)
-        attribute, direction = default.to_a.first
-      end
-    else
-      attribute, direction = default.to_a.first
-    end
-    
-        respond_to do |format|
-      format.html { redirect_to jobs_path, notice: 'All jobs removed.' }
-      format.json { head :no_content }
-    end
+      def test_next_month__2
+    d = Date.new(2000,1,31).next_month(-1)
+    assert_equal([1999, 12, 31], [d.year, d.mon, d.mday])
+    d = Date.new(2000,1,31).next_month
+    assert_equal([2000, 2, 29], [d.year, d.mon, d.mday])
+    d = Date.new(2000,1,31).next_month(12)
+    assert_equal([2001, 1, 31], [d.year, d.mon, d.mday])
+    d = Date.new(2000,1,31).next_month(13)
+    assert_equal([2001, 2, 28], [d.year, d.mon, d.mday])
   end
     
-        respond_to do |format|
-      format.html { redirect_to services_path }
-      format.json { render json: @service }
-    end
-  end
-end
+    undef printf
+alias printf printf_orig
 
     
-        def tasks_without_stage_dependency
-      stages + default_tasks
-    end
+    FIRST     = -> l { LEFT[RIGHT[l]] }
+IF        = -> b { b }
+LEFT      = -> p { p[-> x { -> y { x } } ] }
+RIGHT     = -> p { p[-> x { -> y { y } } ] }
+IS_EMPTY  = LEFT
+REST      = -> l { RIGHT[RIGHT[l]] }
     
-    # IMPORTANT: The Capistrano::Plugin system is not yet considered a stable,
-# public API, and is subject to change without notice. Eventually it will be
-# officially documented and supported, but for now, use it at your own risk.
-#
-# Base class for Capistrano plugins. Makes building a Capistrano plugin as easy
-# as writing a `Capistrano::Plugin` subclass and overriding any or all of its
-# three template methods:
-#
-# * set_defaults
-# * register_hooks
-# * define_tasks
-#
-# Within the plugin you can use any methods of the Rake or Capistrano DSLs, like
-# `fetch`, `invoke`, etc. In cases when you need to use SSHKit's backend outside
-# of an `on` block, use the `backend` convenience method. E.g. `backend.test`,
-# `backend.execute`, or `backend.capture`.
-#
-# Package up and distribute your plugin class as a gem and you're good to go!
-#
-# To use a plugin, all a user has to do is install it in the Capfile, like this:
-#
-#   # Capfile
-#   require 'capistrano/superfancy'
-#   install_plugin Capistrano::Superfancy
-#
-# Or, to install the plugin without its hooks:
-#
-#   # Capfile
-#   require 'capistrano/superfancy'
-#   install_plugin Capistrano::Superfancy, load_hooks: false
-#
-class Capistrano::Plugin < Rake::TaskLib
-  include Capistrano::DSL
+    mkpieces
+mkboard
+$p[4] = [$p[4][0]]
+$pnum = (0...$p.length).to_a
+setpiece([],0)
+
     
-        # Provide a wrapper for the SCM that loads a strategy for the user.
-    #
-    # @param [Rake] context     The context in which the strategy should run
-    # @param [Module] strategy  A module to include into the SCM instance. The
-    #    module should provide the abstract methods of Capistrano::SCM
-    #
-    def initialize(context, strategy)
-      @context = context
-      singleton = class << self; self; end
-      singleton.send(:include, strategy)
-    end
+    module Capistrano
+  module Doctor
+    # Prints table of all Capistrano-related gems and their version numbers. If
+    # there is a newer version of a gem available, call attention to it.
+    class GemsDoctor
+      include Capistrano::Doctor::OutputHelpers
     
-      tasks_dir = Pathname.new('lib/capistrano/tasks')
-  config_dir = Pathname.new('config')
-  deploy_dir = config_dir.join('deploy')
-    
-    Liquid::Template.register_tag('config_tag', ConfigTag)
-    
-        def render(context)
-      if @img
-        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
-      else
-        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
+          def stage_set?
+        !!fetch(:stage, false)
       end
+    
+    module Capistrano
+  module TaskEnhancements
+    def before(task, prerequisite, *args, &block)
+      prerequisite = Rake::Task.define_task(prerequisite, *args, &block) if block_given?
+      Rake::Task[task].enhance [prerequisite]
+    end
+    
+      # Implemented by subclasses to hook into Capistrano's deployment flow using
+  # using the `before` and `after` DSL methods. Note that `register_hooks` will
+  # not be called if the user has opted-out of hooks when installing the plugin.
+  #
+  # Example:
+  #
+  #   def register_hooks
+  #     after 'deploy:updated', 'my_plugin:do_something'
+  #   end
+  #
+  def register_hooks; end
+    
+      desc 'Started'
+  task :started do
+  end
+    
+      describe 'asking for a variable' do
+    before do
+      dsl.ask(:scm, :svn)
+      $stdout.stubs(:print)
+    end
+    
+        @buttons_list = %w(
+      button
+      [type='button']
+      [type='reset']
+      [type='submit']
+    )
+  end
+    
+      context 'called with arguments (2, $value: 4em 6em)' do
+    it 'outputs sextuple the second value from the default scale' do
+      expect('.two-double-value').to have_rule('font-size: 3.125em')
     end
   end
 end
