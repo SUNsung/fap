95ef8476c5690b46a6702a701b81cefc54fdcb0f
SUNsung <weatherius@gmail.com>
2018-03-01 07:52:00 +0200

    Rewrite [32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py] File in: 2018-03-01 07:51:59

--- pyton/32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py
+++ pyton/32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py
@@ -1,139 +1,44 @@
 
         
         
-def random_perturbation_loss(embedded, length, loss_fn):
-  '''Adds noise to embeddings and recomputes classification loss.'''
-  noise = tf.random_normal(shape=tf.shape(embedded))
-  perturb = _scale_l2(_mask_by_length(noise, length), FLAGS.perturb_norm_length)
-  return loss_fn(embedded + perturb)
+def get_json_from_api(url, username, password):
+    headers = {'Accept': 'application/json; version=1.5'}
+    response = open_url(url, headers=headers, url_username=username, url_password=password)
+    return json.loads(response.read())['data']
     
-      def setUp(self):
-    FLAGS.train_data = os.path.join(self.get_temp_dir(), 'test-text.txt')
-    FLAGS.eval_data = os.path.join(self.get_temp_dir(), 'eval-text.txt')
-    FLAGS.save_path = self.get_temp_dir()
-    with open(FLAGS.train_data, 'w') as f:
-      f.write(
-          '''alice was beginning to get very tired of sitting by her sister on
-          the bank, and of having nothing to do: once or twice she had peeped
-          into the book her sister was reading, but it had no pictures or
-          conversations in it, 'and what is the use of a book,' thought alice
-          'without pictures or conversations?' So she was considering in her own
-          mind (as well as she could, for the hot day made her feel very sleepy
-          and stupid), whether the pleasure of making a daisy-chain would be
-          worth the trouble of getting up and picking the daisies, when suddenly
-          a White rabbit with pink eyes ran close by her.\n''')
-      with open(FLAGS.eval_data, 'w') as f:
-        f.write('alice she rabbit once\n')
+        parent = os.path.join(path, os.path.pardir)
+    parent = os.path.realpath(parent)
     
-        # 2. Evaluation mode
-    # Return our loss (which is used to evaluate our model)
-    # Set the TensorBoard scalar my_accurace to the accuracy
-    # Obs: This function only sets value during mode == ModeKeys.EVAL
-    # To set values during training, see tf.summary.scalar
-    if mode == tf.estimator.ModeKeys.EVAL:
-        return tf.estimator.EstimatorSpec(
-            mode,
-            loss=loss,
-            eval_metric_ops={'my_accuracy': accuracy})
+    from __future__ import print_function
     
-      if summary_writer is not None:
-    global_step_val = sess.run(tf.train.get_global_step())
-    tf.logging.info('Finished eval for step ' + str(global_step_val))
-    summary_writer.add_summary(summary, global_step_val)
+            # load data path for resource usage
+        this_dir, this_filename = os.path.split(__file__)
+        type_path = 'container_enabled' if getattr(self.options, 'container_enabled', False) else 'default'
+        self.DATA_PATH = os.path.join(this_dir, 'data', type_path)
     
-    import numpy as np
-from six.moves import urllib
-import tensorflow as tf
+        terminal_stderr_re = [
+        re.compile(r'% ?Error: '),
+        re.compile(r'^% \w+', re.M),
+        re.compile(r'% ?Bad secret'),
+        re.compile(r'invalid input', re.I),
+        re.compile(r'(?:incomplete|ambiguous) command', re.I),
+        re.compile(r'connection timed out', re.I),
+        re.compile(r'[^\r\n]+ not found', re.I),
+        re.compile(r''[^']' +returned error code: ?\d+'),
+        re.compile(r'syntax error'),
+        re.compile(r'unknown command'),
+        re.compile(r'Error\[\d+\]: ', re.I),
+        re.compile(r'Error:', re.I)
+    ]
     
-        def set(self, results, query):
-        '''Set the result for the given query key in the cache.
-        
-        When updating an entry, updates its position to the front of the LRU list.
-        If the entry is new and the cache is at capacity, removes the oldest entry
-        before the new entry is added.
-        '''
-        node = self.lookup[query]
-        if node is not None:
-            # Key exists in cache, update the value
-            node.results = results
-            self.linked_list.move_to_front(node)
+    import os
+import re
+    
+            # simple test to validate param value
+        if param in self._static_method_choices.keys():
+            self.param = param
         else:
-            # Key does not exist in cache
-            if self.size == self.MAX_SIZE:
-                # Remove the oldest entry from the linked list and lookup
-                self.lookup.pop(self.linked_list.tail.query, None)
-                self.linked_list.remove_from_tail()
-            else:
-                self.size += 1
-            # Add the new key and value
-            new_node = Node(results)
-            self.linked_list.append_to_front(new_node)
-            self.lookup[query] = new_node
+            raise ValueError('Invalid Value for Param: {0}'.format(param))
     
-        def __init__(self, query, results):
-        self.query = query
-        self.results = results
     
-        def reducer(self, key, values):
-        total = sum(values)
-        if total == 1:
-            yield key, total
-    
-        def test_where_subquery(self):
-        query = '''
-          SELECT name
-            FROM t1
-            WHERE regionkey = (SELECT max(regionkey) FROM t2)
-        '''
-        self.assertEquals({'t1', 't2'}, self.extract_tables(query))
-    
-        id = Column(Integer, primary_key=True)
-    column_name = Column(String(255))
-    verbose_name = Column(String(1024))
-    is_active = Column(Boolean, default=True)
-    type = Column(String(32))
-    groupby = Column(Boolean, default=False)
-    count_distinct = Column(Boolean, default=False)
-    sum = Column(Boolean, default=False)
-    avg = Column(Boolean, default=False)
-    max = Column(Boolean, default=False)
-    min = Column(Boolean, default=False)
-    filterable = Column(Boolean, default=False)
-    description = Column(Text)
-    is_dttm = None
-    
-        @classmethod
-    def register_sources(cls, datasource_config):
-        for module_name, class_names in datasource_config.items():
-            class_names = [str(s) for s in class_names]
-            module_obj = __import__(module_name, fromlist=class_names)
-            for class_name in class_names:
-                source_class = getattr(module_obj, class_name)
-                cls.sources[source_class.type] = source_class
-    
-        @staticmethod
-    def _dimensions_to_values(dimensions):
-        '''
-        Replace dimensions specs with their `dimension`
-        values, and ignore those without
-        '''
-        values = []
-        for dimension in dimensions:
-            if isinstance(dimension, dict):
-                if 'dimension' in dimension:
-                    values.append(dimension['dimension'])
-            else:
-                values.append(dimension)
-    
-    from superset import appbuilder, db, security, sm, utils
-from superset.connectors.base.views import DatasourceModelView
-from superset.connectors.connector_registry import ConnectorRegistry
-from superset.utils import has_access
-from superset.views.base import (
-    BaseSupersetView, DatasourceFilter, DeleteMixin,
-    get_datasource_exist_error_mgs, ListWidgetWithCheckboxes, SupersetModelView,
-    validate_json, YamlExportMixin,
-)
-from . import models
-    
-        '''ORM object for table columns, each table can have multiple columns'''
+class TC:
