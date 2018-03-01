523f840eace931fdfd4fc66f05f91a3259b18774
SUNsung <weatherius@gmail.com>
2018-03-01 09:14:42 +0200

    Rewrite [33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb] File in: 2018-03-01 09:14:41

--- ruby/33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb
+++ ruby/33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb
@@ -1,13 +1,182 @@
-  included do
-    scope :admins, -> { where(admin: true) }
-    scope :moderators, -> { where(moderator: true) }
-    scope :staff, -> { where('moderator or admin ') }
+
+        
+        def site_configuration(overrides = {})
+  build_configs({
+    'source'      => source_dir,
+    'destination' => dest_dir
+  }, build_configs(overrides))
+end
+    
+    options = {
+  sort: true,
+  limit: 30,
+  format: :text,
+}
+    
+        def process(args)
+      arg_is_present? args, '--server', 'The --server command has been replaced by the \
+                          'serve' subcommand.'
+      arg_is_present? args, '--serve', 'The --serve command has been replaced by the \
+                          'serve' subcommand.'
+      arg_is_present? args, '--no-server', 'To build Jekyll without launching a server, \
+                          use the 'build' subcommand.'
+      arg_is_present? args, '--auto', 'The switch '--auto' has been replaced with \
+                          '--watch'.'
+      arg_is_present? args, '--no-auto', 'To disable auto-replication, simply leave off \
+                          the '--watch' switch.'
+      arg_is_present? args, '--pygments', 'The 'pygments'settings has been removed in \
+                          favour of 'highlighter'.'
+      arg_is_present? args, '--paginate', 'The 'paginate' setting can only be set in \
+                          your config files.'
+      arg_is_present? args, '--url', 'The 'url' setting can only be set in your \
+                          config files.'
+      no_subcommand(args)
+    end
+    
+          private
+      def parse_expression(str)
+        Liquid::Variable.new(str, Liquid::ParseContext.new)
+      end
+    
+          def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :name,
+                                       env_name: 'KEYCHAIN_NAME',
+                                       description: 'Keychain name',
+                                       conflicting_options: [:path],
+                                       is_string: true,
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :path,
+                                       env_name: 'KEYCHAIN_PATH',
+                                       description: 'Path to keychain',
+                                       is_string: true,
+                                       conflicting_options: [:name],
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :password,
+                                       env_name: 'KEYCHAIN_PASSWORD',
+                                       description: 'Password for the keychain',
+                                       sensitive: true,
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :default_keychain,
+                                       description: 'Should the newly created Keychain be the new system default keychain',
+                                       is_string: false,
+                                       default_value: false),
+          FastlaneCore::ConfigItem.new(key: :unlock,
+                                       description: 'Unlock keychain after create',
+                                       is_string: false,
+                                       default_value: false),
+          FastlaneCore::ConfigItem.new(key: :timeout,
+                                       description: 'timeout interval in seconds. Set `false` if you want to specify 'no time-out'',
+                                       is_string: false,
+                                       default_value: 300),
+          FastlaneCore::ConfigItem.new(key: :lock_when_sleeps,
+                                       description: 'Lock keychain when the system sleeps',
+                                       is_string: false,
+                                       default_value: false),
+          FastlaneCore::ConfigItem.new(key: :lock_after_timeout,
+                                       description: 'Lock keychain after timeout interval',
+                                       is_string: false,
+                                       default_value: false),
+          FastlaneCore::ConfigItem.new(key: :add_to_search_list,
+                                       description: 'Add keychain to search list',
+                                       is_string: false,
+                                       default_value: true)
+        ]
+      end
+    
+          it 'splits correctly' do
+        expected = [
+          'One',
+          'Two',
+          'Three',
+          'Four Token',
+          'Or',
+          'Newlines',
+          'Everywhere'
+        ]
+        expect(generator.split_keywords(keywords)).to eq(expected)
+      end
+    end
+  end
+end
+
+    
+        def schemes_valid?
+      !schemes.nil? && schemes.size == 1 && !schemes.first.empty?
+    end
+    
+            expect(result).to eq('/usr/local/bin/cloc  --by-file  --out=build/cloc.txt')
+      end
+    end
+  end
+end
+
+    
+          it 'gets the correct version number for 'SampleProject_tests'' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          get_version_number(xcodeproj: '.xcproject', target: 'SampleProject_tests')
+        end').runner.execute(:test)
+        expect(result).to eq('1.0')
+      end
+    
+        def log_file_info(s)
+      puts '    #{magenta s}'
+    end
+    
+      find_files = ->(path) {
+    Find.find(Pathname.new(path).relative_path_from(Pathname.new Dir.pwd).to_s).map do |path|
+      path if File.file?(path)
+    end.compact
+  }
+    
+          opts.on('-q', '--quiet', 'Silence warnings and status messages during compilation.') do
+        @options[:for_engine][:quiet] = true
+      end
+    end
+    
+    # A logger that delays messages until they're explicitly flushed to an inner
+# logger.
+#
+# This can be installed around the current logger by calling \{#install!}, and
+# the original logger can be replaced by calling \{#uninstall!}. The log
+# messages can be flushed by calling \{#flush}.
+class Sass::Logger::Delayed < Sass::Logger::Base
+  # Installs a new delayed logger as the current Sass logger, wrapping the
+  # original logger.
+  #
+  # This can be undone by calling \{#uninstall!}.
+  #
+  # @return [Sass::Logger::Delayed] The newly-created logger.
+  def self.install!
+    logger = Sass::Logger::Delayed.new(Sass.logger)
+    Sass.logger = logger
+    logger
   end
     
-        def entries_as_json
-      @entries.sort! { |a, b| sort_fn(a.name, b.name) }.map(&:as_json)
+            {
+          :always_update     => false,
+          :template_location => root + '/public/stylesheets/sass',
+          :css_location      => root + '/public/stylesheets',
+          :cache_location    => root + '/tmp/sass-cache',
+          :always_check      => env != 'production',
+          :quiet             => env != 'production',
+          :full_exception    => env != 'production'
+        }.freeze
+      end
     end
+  end
     
-        def as_json
-      @pages
-    end
+            def update
+          authorize! :update, stock_location
+          if stock_location.update_attributes(stock_location_params)
+            respond_with(stock_location, status: 200, default_template: :show)
+          else
+            invalid_resource!(stock_location)
+          end
+        end
+    
+            def index
+          authorize! :read, StockMovement
+          @stock_movements = scope.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
+          respond_with(@stock_movements)
+        end
