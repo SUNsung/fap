3ee588f320e3df6fc257aa8aca9e89465e779d88
SUNsung <weatherius@gmail.com>
2018-03-01 20:34:43 +0200

    Rewrite [41BMdst10M35o4130VOw9GlEvs41Z3BDfqfDaUk41z58.py] File in: 2018-03-01 20:34:42

--- pyton/41BMdst10M35o4130VOw9GlEvs41Z3BDfqfDaUk41z58.py
+++ pyton/41BMdst10M35o4130VOw9GlEvs41Z3BDfqfDaUk41z58.py
@@ -1,40 +1,186 @@
 
         
+        
+@mock.patch('httpie.input.AuthCredentials._getpass',
+            new=lambda self, prompt: 'UNEXPECTED_PROMPT_RESPONSE')
+def test_auth_plugin_prompt_password_false(httpbin):
+    
+            request_line = '{method} {path}{query} HTTP/1.1'.format(
+            method=self._orig.method,
+            path=url.path or '/',
+            query='?' + url.query if url.query else ''
+        )
+    
+    
+def is_valid_mime(mime):
+    return mime and MIME_RE.match(mime)
+    
+    
+FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
+FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
+JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
+BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
+    
+        exc = ConnectionError('Connection aborted')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
+    assert ret == ExitStatus.ERROR
+    assert error_msg == (
+        'ConnectionError: '
+        'Connection aborted while doing GET request to URL: '
+        'http://www.google.com')
+    
         '''
-    The approach taken is explained below. I decided to do it simply.
-    Initially I was considering parsing the data into some sort of
-    structure and then generating an appropriate README. I am still
-    considering doing it - but for now this should work. The only issue
-    I see is that it only sorts the entries at the lowest level, and that
-    the order of the top-level contents do not match the order of the actual
-    entries.
+)
     
-    if __name__ == '__main__':
+        # 128+2 SIGINT <http://www.tldp.org/LDP/abs/html/exitcodes.html>
+    ERROR_CTRL_C = 130
     
-                gc.collect()
-            print('- benchmarking LassoLars')
-            clf = LassoLars(alpha=alpha, fit_intercept=False,
-                            normalize=False, precompute=precompute)
-            tstart = time()
-            clf.fit(X, Y)
-            lars_lasso_results.append(time() - tstart)
+            '''
+        if self.args.method is None:
+            # Invoked as `http URL'.
+            assert not self.args.items
+            if not self.args.ignore_stdin and not self.env.stdin_isatty:
+                self.args.method = HTTP_POST
+            else:
+                self.args.method = HTTP_GET
     
-    The input data is mostly low rank but is a fat infinite tail.
-'''
+            # put all lines in the file into a Python list
+        strings = f.readlines()
+        
+        # above line leaves trailing newline characters; strip them out
+        strings = [x.strip(u'\n') for x in strings]
+        
+        # remove empty-lines and comments
+        strings = [x for x in strings if x and not x.startswith(u'#')]
+        
+        # insert empty string since all are being removed
+        strings.insert(0, u'')
+    
+    # Can't import these from paths.py because that uses `future` imports
+DIR_OF_CURRENT_SCRIPT = os.path.dirname( os.path.abspath( __file__ ) )
+DIR_OF_YCMD = os.path.join( DIR_OF_CURRENT_SCRIPT, '..', '..', 'third_party',
+                            'ycmd' )
+    
+    The follow diagram and text describe the data-flow through the system:
+    
+            finished, pending = futures.wait(
+                [SUCCESSFUL_FUTURE,
+                 CANCELLED_AND_NOTIFIED_FUTURE,
+                 EXCEPTION_FUTURE,
+                 future1,
+                 future2],
+                return_when=futures.ALL_COMPLETED)
+    
+    
+def _FormatCompleterDebugInfo( completer ):
+  message = '{0} completer debug information:\n'.format( completer[ 'name' ] )
+  for server in completer[ 'servers' ]:
+    name = server[ 'name' ]
+    if server[ 'is_running' ]:
+      address = server[ 'address' ]
+      port = server[ 'port' ]
+      if address and port:
+        message += '  {0} running at: http://{1}:{2}\n'.format( name,
+                                                                address,
+                                                                port )
+      else:
+        message += '  {0} running\n'.format( name )
+      message += '  {0} process ID: {1}\n'.format( name, server[ 'pid' ] )
+    else:
+      message += '  {0} not running\n'.format( name )
+    message += '  {0} executable: {1}\n'.format( name, server[ 'executable'] )
+    logfiles = server[ 'logfiles' ]
+    if logfiles:
+      message += '  {0} logfiles:\n'.format( name )
+      for logfile in logfiles:
+        message += '    {0}\n'.format( logfile )
+    else:
+      message += '  No logfiles available\n'
+    if 'extras' in server:
+      for extra in server[ 'extras' ]:
+        message += '  {0} {1}: {2}\n'.format( name,
+                                              extra[ 'key' ],
+                                              extra[ 'value' ] )
+  for item in completer[ 'items' ]:
+    message += '  {0}: {1}\n'.format( item[ 'key' ].capitalize(),
+                                      item[ 'value' ] )
+  return message
+    
+    DIR_OF_THIS_SCRIPT = p.dirname( p.abspath( __file__ ) )
+DIR_OF_OLD_LIBS = p.join( DIR_OF_THIS_SCRIPT, 'python' )
+    
+      return [ config_entry ]
+    
+    from __future__ import unicode_literals
 from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
     
-        class_name = info['fullname'].split('.')[0]
-    if type(class_name) != str:
-        # Python 2 only
-        class_name = class_name.encode('utf-8')
-    module = __import__(info['module'], fromlist=[class_name])
-    obj = attrgetter(info['fullname'])(module)
     
-        def decision_surface(self, cls):
-        delta = 1
-        x = np.arange(x_min, x_max + delta, delta)
-        y = np.arange(y_min, y_max + delta, delta)
-        X1, X2 = np.meshgrid(x, y)
-        Z = cls.decision_function(np.c_[X1.ravel(), X2.ravel()])
-        Z = Z.reshape(X1.shape)
-        return X1, X2, Z
+def MakeUserOptions( custom_options = {} ):
+  options = dict( user_options_store.DefaultOptions() )
+  options.update( DEFAULT_CLIENT_OPTIONS )
+  options.update( custom_options )
+  return options
+    
+        Configurable subclasses must define the class methods
+    `configurable_base` and `configurable_default`, and use the instance
+    method `initialize` instead of ``__init__``.
+    
+            auth_header = self.request.headers.get('Authorization', None)
+        if auth_header is not None:
+            auth_mode, params = auth_header.split(' ', 1)
+            assert auth_mode == 'Digest'
+            param_dict = {}
+            for pair in params.split(','):
+                k, v = pair.strip().split('=', 1)
+                if v[0] == ''' and v[-1] == ''':
+                    v = v[1:-1]
+                param_dict[k] = v
+            assert param_dict['realm'] == realm
+            assert param_dict['opaque'] == opaque
+            assert param_dict['nonce'] == nonce
+            assert param_dict['username'] == username
+            assert param_dict['uri'] == self.request.path
+            h1 = md5(utf8('%s:%s:%s' % (username, realm, password))).hexdigest()
+            h2 = md5(utf8('%s:%s' % (self.request.method,
+                                     self.request.path))).hexdigest()
+            digest = md5(utf8('%s:%s:%s' % (h1, nonce, h2))).hexdigest()
+            if digest == param_dict['response']:
+                self.write('ok')
+            else:
+                self.write('fail')
+        else:
+            self.set_status(401)
+            self.set_header('WWW-Authenticate',
+                            'Digest realm='%s', nonce='%s', opaque='%s'' %
+                            (realm, nonce, opaque))
+    
+       Unlike the standard library's `queue` module, the classes defined here
+   are *not* thread-safe. To use these queues from another thread,
+   use `.IOLoop.add_callback` to transfer control to the `.IOLoop` thread
+   before calling any queue methods.
+'''
+    
+    
+class AuthLoginHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
+    @tornado.web.asynchronous
+    def get(self):
+        my_url = (self.request.protocol + '://' + self.request.host +
+                  '/auth/login?next=' +
+                  tornado.escape.url_escape(self.get_argument('next', '/')))
+        if self.get_argument('code', False):
+            self.get_authenticated_user(
+                redirect_uri=my_url,
+                client_id=self.settings['facebook_api_key'],
+                client_secret=self.settings['facebook_secret'],
+                code=self.get_argument('code'),
+                callback=self._on_auth)
+            return
+        self.authorize_redirect(redirect_uri=my_url,
+                                client_id=self.settings['facebook_api_key'],
+                                extra_params={'scope': 'user_posts'})
