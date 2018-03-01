9be473f276fbba788f98487eab37aaa3733bb189
SUNsung <weatherius@gmail.com>
2018-03-01 21:40:45 +0200

    Rewrite [45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py] File in: 2018-03-01 21:40:45

--- pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
+++ pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
@@ -1,47 +1,116 @@
-        cases = [0, 0.001, 0.99, 1.0, 1.5, 1e20, 1e200]
-        # 2**48 is an important boundary in the internals.  2**53 is an
-        # important boundary for IEEE double precision.
-        for t in 2.0**48, 2.0**50, 2.0**53:
-            cases.extend([t - 1.0, t - 0.3, t, t + 0.3, t + 1.0,
-                          int(t-1), int(t), int(t+1)])
-        cases.extend([0, 1, 2, sys.maxsize, float(sys.maxsize)])
-        # 1 << 20000 should exceed all double formats.  int(1e200) is to
-        # check that we get equality with 1e200 above.
-        t = int(1e200)
-        cases.extend([0, 1, 2, 1 << 20000, t-1, t, t+1])
-        cases.extend([-x for x in cases])
-        for x in cases:
-            Rx = Rat(x)
-            for y in cases:
-                Ry = Rat(y)
-                Rcmp = (Rx > Ry) - (Rx < Ry)
-                with self.subTest(x=x, y=y, Rcmp=Rcmp):
-                    xycmp = (x > y) - (x < y)
-                    eq(Rcmp, xycmp)
-                    eq(x == y, Rcmp == 0)
-                    eq(x != y, Rcmp != 0)
-                    eq(x < y, Rcmp < 0)
-                    eq(x <= y, Rcmp <= 0)
-                    eq(x > y, Rcmp > 0)
-                    eq(x >= y, Rcmp >= 0)
+
+        
+        
+def get_args(args_list):
+    parser = argparse.ArgumentParser(
+        description='ansible inventory script reading from serf cluster')
+    mutex_group = parser.add_mutually_exclusive_group(required=True)
+    help_list = 'list all hosts from serf cluster'
+    mutex_group.add_argument('--list', action='store_true', help=help_list)
+    help_host = 'display variables for a host'
+    mutex_group.add_argument('--host', help=help_host)
+    return parser.parse_args(args_list)
     
-    The __init__ method has one required argument, a file-like object
-(including a chunk instance), and one optional argument, a flag which
-specifies whether or not chunks are aligned on 2-byte boundaries.  The
-default is 1, i.e. aligned.
-'''
+            with self.assertRaises(GCPInvalidURLError) as cm:
+            GCPUtils.parse_gcp_url(no_resource_no_loc_input_url)
+        self.assertTrue(cm.exception, GCPInvalidURLError)
     
-            digits = [long(ob_digit[i]) * 2**(SHIFT*i)
-                  for i in safe_range(abs(ob_size))]
-        result = sum(digits)
-        if ob_size < 0:
-            result = -result
-        return result
+        @support.requires_IEEE_754
+    def test_float_conversion(self):
     
-        def test_sort_keys_flag(self):
-        infile = self._create_infile()
-        rc, out, err = assert_python_ok('-m', 'json.tool', '--sort-keys', infile)
-        self.assertEqual(rc, 0)
-        self.assertEqual(out.splitlines(),
-                         self.expect_without_sort_keys.encode().splitlines())
-        self.assertEqual(err, b'')
+    __all__ = ['JSONDecoder', 'JSONDecodeError']
+    
+        def _explain_to(self, message):
+        '''Copy mbox- or MMDF-specific state to message insofar as possible.'''
+        if isinstance(message, MaildirMessage):
+            flags = set(self.get_flags())
+            if 'O' in flags:
+                message.set_subdir('cur')
+            if 'F' in flags:
+                message.add_flag('F')
+            if 'A' in flags:
+                message.add_flag('R')
+            if 'R' in flags:
+                message.add_flag('S')
+            if 'D' in flags:
+                message.add_flag('T')
+            del message['status']
+            del message['x-status']
+            maybe_date = ' '.join(self.get_from().split()[-5:])
+            try:
+                message.set_date(calendar.timegm(time.strptime(maybe_date,
+                                                      '%a %b %d %H:%M:%S %Y')))
+            except (ValueError, OverflowError):
+                pass
+        elif isinstance(message, _mboxMMDFMessage):
+            message.set_flags(self.get_flags())
+            message.set_from(self.get_from())
+        elif isinstance(message, MHMessage):
+            flags = set(self.get_flags())
+            if 'R' not in flags:
+                message.add_sequence('unseen')
+            if 'A' in flags:
+                message.add_sequence('replied')
+            if 'F' in flags:
+                message.add_sequence('flagged')
+            del message['status']
+            del message['x-status']
+        elif isinstance(message, BabylMessage):
+            flags = set(self.get_flags())
+            if 'R' not in flags:
+                message.add_label('unseen')
+            if 'D' in flags:
+                message.add_label('deleted')
+            if 'A' in flags:
+                message.add_label('answered')
+            del message['status']
+            del message['x-status']
+        elif isinstance(message, Message):
+            pass
+        else:
+            raise TypeError('Cannot convert to specified type: %s' %
+                            type(message))
+    
+        objects = []
+    libs = ['shell32.lib', 'comdlg32.lib', 'wsock32.lib', 'user32.lib', 'oleaut32.lib']
+    for moddefn in moddefns:
+        print('# Module', moddefn.name)
+        for file in moddefn.sourceFiles:
+            base = os.path.basename(file)
+            base, ext = os.path.splitext(base)
+            objects.append(base + '.obj')
+            print(r'$(temp_dir)\%s.obj: '%s'' % (base, file))
+            print('\t@$(CC) -c -nologo /Fo$* $(cdl) $(c_debug) /D BUILD_FREEZE', end=' ')
+            print(''-I$(pythonhome)/Include'  '-I$(pythonhome)/PC' \\')
+            print('\t\t$(cflags) $(cdebug) $(cinclude) \\')
+            extra = moddefn.GetCompilerOptions()
+            if extra:
+                print('\t\t%s \\' % (' '.join(extra),))
+            print('\t\t'%s'' % file)
+            print()
+    
+            # make sure everything works in ''.format()
+        self.assertEqual('*{0:.3f}*'.format(3.14159+2.71828j), '*3.142+2.718j*')
+    
+        def _create_infile(self):
+        infile = support.TESTFN
+        with open(infile, 'w') as fp:
+            self.addCleanup(os.remove, infile)
+            fp.write(self.data)
+        return infile
+    
+        def test_large_exhausted_iterator_pickling(self):
+        for proto in range(pickle.HIGHEST_PROTOCOL + 1):
+            r = range(20)
+            i = iter(r)
+            while True:
+                r = next(i)
+                if r == 19:
+                    break
+            d = pickle.dumps(i, proto)
+            i2 = pickle.loads(d)
+            self.assertEqual(list(i), [])
+            self.assertEqual(list(i2), [])
+    
+            del wndclass
+        del wndproc
