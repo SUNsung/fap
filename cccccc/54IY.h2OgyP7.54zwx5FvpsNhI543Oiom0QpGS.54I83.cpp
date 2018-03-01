2f360c7a6be72e8013159b13e4bc8698fa859ec7
SUNsung <weatherius@gmail.com>
2018-03-02 01:52:59 +0200

    Rewrite [54IY.h2OgyP7.54zwx5FvpsNhI543Oiom0QpGS.54I83.cpp] File in: 2018-03-02 01:52:58

--- cccccc/54IY.h2OgyP7.54zwx5FvpsNhI543Oiom0QpGS.54I83.cpp
+++ cccccc/54IY.h2OgyP7.54zwx5FvpsNhI543Oiom0QpGS.54I83.cpp
@@ -1,100 +1,254 @@
 
         
-        namespace python {
+        class PyDescriptorDatabase : public DescriptorDatabase {
+ public:
+  explicit PyDescriptorDatabase(PyObject* py_database);
+  ~PyDescriptorDatabase();
     }
     
+    #ifndef GOOGLE_PROTOBUF_PYTHON_CPP_REPEATED_SCALAR_CONTAINER_H__
+#define GOOGLE_PROTOBUF_PYTHON_CPP_REPEATED_SCALAR_CONTAINER_H__
+    
+    #include <string>
+    
     
     {
     {
-    {
-    {
-    {}  // namespace
-}  // namespace csharp
+    {}  // namespace csharp
 }  // namespace compiler
 }  // namespace protobuf
-}  // namespace google
-
     
-      virtual void WriteHash(io::Printer* printer);
-  virtual void WriteEquals(io::Printer* printer);
-  virtual void WriteToString(io::Printer* printer);
-    
-    void RepeatedMessageFieldGenerator::GenerateSerializationCode(io::Printer* printer) {
-  printer->Print(
-    variables_,
-    '$name$_.WriteTo(output, _repeated_$name$_codec);\n');
+    void RepeatedEnumFieldGenerator::GenerateFreezingCode(io::Printer* printer) {
 }
     
+    #include <string>
     
-    { private:
-  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(RepeatedMessageFieldGenerator);
+    #include <google/protobuf/compiler/code_generator.h>
+#include <google/protobuf/compiler/plugin.h>
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/descriptor.pb.h>
+#include <google/protobuf/io/printer.h>
+#include <google/protobuf/io/zero_copy_stream.h>
+#include <google/protobuf/wire_format.h>
+    
+    #include <google/protobuf/compiler/csharp/csharp_source_generator_base.h>
+#include <google/protobuf/compiler/csharp/csharp_helpers.h>
+#include <google/protobuf/compiler/csharp/csharp_names.h>
+#include <google/protobuf/compiler/csharp/csharp_options.h>
+    
+    int getifaddrs(struct ifaddrs** result) {
+	int fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
+	if (fd < 0) {
+		return -1;
+	}
+	netlinkrequest ifaddr_request;
+	memset(&ifaddr_request, 0, sizeof(ifaddr_request));
+	ifaddr_request.header.nlmsg_flags = NLM_F_ROOT | NLM_F_REQUEST;
+	ifaddr_request.header.nlmsg_type = RTM_GETADDR;
+	ifaddr_request.header.nlmsg_len = NLMSG_LENGTH(sizeof(ifaddrmsg));
+	ssize_t count = send(fd, &ifaddr_request, ifaddr_request.header.nlmsg_len, 0);
+	if (static_cast<size_t>(count) != ifaddr_request.header.nlmsg_len) {
+		close(fd);
+		return -1;
+	}
+	struct ifaddrs* start = NULL;
+	struct ifaddrs* current = NULL;
+	char buf[kMaxReadSize];
+	ssize_t amount_read = recv(fd, &buf, kMaxReadSize, 0);
+	while (amount_read > 0) {
+		nlmsghdr* header = reinterpret_cast<nlmsghdr*>(&buf[0]);
+		size_t header_size = static_cast<size_t>(amount_read);
+		for ( ; NLMSG_OK(header, header_size);
+		      header = NLMSG_NEXT(header, header_size)) {
+			switch (header->nlmsg_type) {
+			case NLMSG_DONE:
+				// Success. Return.
+				*result = start;
+				close(fd);
+				return 0;
+			case NLMSG_ERROR:
+				close(fd);
+				freeifaddrs(start);
+				return -1;
+			case RTM_NEWADDR: {
+				ifaddrmsg* address_msg =
+						reinterpret_cast<ifaddrmsg*>(NLMSG_DATA(header));
+				rtattr* rta = IFA_RTA(address_msg);
+				ssize_t payload_len = IFA_PAYLOAD(header);
+				while (RTA_OK(rta, payload_len)) {
+					if (rta->rta_type == IFA_ADDRESS) {
+						int family = address_msg->ifa_family;
+						if (family == AF_INET || family == AF_INET6) {
+							ifaddrs* newest = new ifaddrs;
+							memset(newest, 0, sizeof(ifaddrs));
+							if (current) {
+								current->ifa_next = newest;
+							} else {
+								start = newest;
+							}
+							if (populate_ifaddrs(newest, address_msg, RTA_DATA(rta),
+									     RTA_PAYLOAD(rta)) != 0) {
+								freeifaddrs(start);
+								*result = NULL;
+								return -1;
+							}
+							current = newest;
+						}
+					}
+					rta = RTA_NEXT(rta, payload_len);
+				}
+				break;
+			}
+			}
+		}
+		amount_read = recv(fd, &buf, kMaxReadSize, 0);
+	}
+	close(fd);
+	freeifaddrs(start);
+	return -1;
+}
+    
+      {1,2,6,  &_residue_44p_lfe,
+   &_huff_book__44p3_lfe,&_huff_book__44p3_lfe,
+   &_resbook_44p_l3,&_resbook_44p_l3}
 };
+static const vorbis_residue_template _res_44p51_4[]={
+  {2,0,15,  &_residue_44p,
+   &_huff_book__44p4_short,&_huff_book__44p4_short,
+   &_resbook_44p_4,&_resbook_44p_4},
     
-    #include <google/protobuf/descriptor.h>
+    #define todB_nn(x) todB(x)
     
     
-    {  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(ImmutableExtensionLiteGenerator);
+    {                return intgr ;
+        }
+    
+    #undef MULT16_32_Q15
+static inline int MULT16_32_Q15(int a, int b)
+{
+    int c;
+    asm volatile('MULT $ac1, %0, %1' : : 'r' (a), 'r' (b));
+    asm volatile('EXTR.W %0,$ac1, %1' : '=r' (c): 'i' (15));
+    return c;
+}
+    
+     protected:
+  /// The database was opened in a ReadOnly mode.
+  bool read_only_{false};
+    
+      /// Remove all subscriptions from a named subscriber.
+  virtual void removeSubscriptions(const std::string& subscriber);
+    
+    /**
+ * @brief Helper struct for managing extenion metadata.
+ *
+ * This structure should match the members of Thrift's InternalExtensionInfo.
+ */
+struct ExtensionInfo {
+  std::string name;
+  std::string version;
+  std::string min_sdk_version;
+  std::string sdk_version;
 };
     
     /**
- * @brief An osquery backing storage (database) type that persists executions.
+ * @brief A small tracking wrapper for options, binary flags.
  *
- * The osquery tools need a high-performance storage and indexing mechanism for
- * storing intermediate results from EventPublisher%s, persisting one-time
- * generated values, and performing non-memory backed differentials.
- *
- * Practically, osquery is built around RocksDB's performance guarantees and
- * all of the internal APIs expect RocksDB's indexing and read performance.
- * However, access to this representation of a backing-store is still abstracted
- * to removing RocksDB as a dependency for the osquery SDK.
+ * The osquery-specific gflags-like options define macro `FLAG` uses a Flag
+ * instance to track the options data.
  */
-class DatabasePlugin : public Plugin {
+class Flag : private boost::noncopyable {
  public:
-  /**
-   * @brief Perform a domain and key lookup from the backing store.
+  /*
+   * @brief Create a new flag.
    *
-   * Database value access indexing is abstracted into domains and keys.
-   * Both are string values but exist separately for simple indexing without
-   * API-enforcing tokenization. In some cases we do add a component-specific
-   * tokeninzation to keys.
+   * @param name The 'name' or the options switch data.
+   * @param flag Flag information filled in using the helper macro.
    *
-   * @param domain A string value representing abstract storage indexing.
-   * @param key A string value representing the lookup/retrieval key.
-   * @param value The output parameter, left empty if the key does not exist.
-   * @return Failure if the data could not be accessed. It is up to the plugin
-   * to determine if a missing key means a non-success status.
+   * @return A mostly needless flag instance.
    */
-  virtual Status get(const std::string& domain,
-                     const std::string& key,
-                     std::string& value) const = 0;
+  static int create(const std::string& name, const FlagDetail& flag);
     }
     
-    #include <sstream>
-#include <string>
-#include <utility>
-    
-      /**
-   * @brief Initialize any platform dependent libraries or objects
-   *
-   * On windows, we require the COM libraries be initialized just once
-   */
-  static void platformSetup();
-    
-      /*
-   * @brief A helper function to perform a start to finish carve
-   *
-   * This function walks through the carve, compress, and exfil functions
-   * in one fell swoop. Use of this class should largely happen through
-   * this function.
-   */
-  void start();
-    
-    /// Config retrieval TLS endpoint (path) using TLS hostname.
-CLI_FLAG(string,
-         config_tls_endpoint,
-         '',
-         'TLS/HTTPS endpoint for config retrieval');
-    
      protected:
-  /// Calculate the URL once and cache the result.
-  std::string uri_;
+  /**
+   * @brief Initialize the logger with the name of the binary and any status
+   * logs generated between program launch and logger start.
+   *
+   * The logger initialization is called once CLI flags have been parsed, the
+   * registry items are constructed, extension routes broadcasted and extension
+   * plugins discovered (as a logger may be an extension plugin) and the config
+   * has been loaded (which may include additional CLI flag-options).
+   *
+   * All of these actions may have generated VERBOSE, INFO, WARNING, or ERROR
+   * logs. The internal logging facility, Glog, collects these intermediate
+   * status logs and a customized log sink buffers them until the active
+   * osquery logger's `init` method is called.
+   *
+   * @param binary_name The string name of the process (argv[0]).
+   * @param log The set of status (INFO, WARNING, ERROR) logs generated before
+   * the logger's `init` method was called.
+   */
+  virtual void init(const std::string& binary_name,
+                    const std::vector<StatusLogLine>& log) = 0;
+    
+    
+    {  if (osquery.buffer) {
+    IOFreeAligned(osquery.buffer, osquery.buf_size);
+    osquery.buffer = NULL;
+  }
+}
+    
+    namespace osquery {
+    }
+    
+    
+    {      multi_pack.put_child(fs::path(path).stem().string(), single_pack);
+    }
+    
+    namespace osquery {
+    }
+    
+    bool PlatformProcess::operator!=(const PlatformProcess& process) const {
+  return (nativeHandle() != process.nativeHandle());
+}
+    
+        // This is the 20 byte message (JSON) according to Socket.IO
+    unsigned char socketio[] = 'xxxx42[\'message\',\'yolo\']';
+    for (int i = 4; i < 24; i++) {
+        socketio[i] ^= 'x';
+	cout << (int) socketio[i] << endl;
+    }
+    
+    
+    {
+    {
+    {                if ((getPoll() & UV_WRITABLE) == 0) {
+                    setPoll(getPoll() | UV_WRITABLE);
+                    changePoll(this);
+                }
+            }
+        }
+        messageQueue.push(message);
+        wasTransferred = true;
+        return true;
+    }
+    
+        void change(Loop *loop, Poll *self, int events) {
+        epoll_event event;
+        event.events = events;
+        event.data.ptr = self;
+        epoll_ctl(loop->epfd, EPOLL_CTL_MOD, state.fd, &event);
+    }
+    
+        Hub(int extensionOptions = 0, bool useDefaultLoop = false, unsigned int maxPayload = 16777216) : uS::Node(LARGE_BUFFER_SIZE, WebSocketProtocol<SERVER, WebSocket<SERVER>>::CONSUME_PRE_PADDING, WebSocketProtocol<SERVER, WebSocket<SERVER>>::CONSUME_POST_PADDING, useDefaultLoop),
+                                             Group<SERVER>(extensionOptions, maxPayload, this, nodeData), Group<CLIENT>(0, maxPayload, this, nodeData) {
+        inflateInit2(&inflationStream, -15);
+        inflationBuffer = new char[LARGE_BUFFER_SIZE];
+    }
+    
+        int indices = NodeData::getMemoryBlockIndex(NodeData::preAllocMaxSize) + 1;
+    nodeData->preAlloc = new char*[indices];
+    for (int i = 0; i < indices; i++) {
+        nodeData->preAlloc[i] = nullptr;
+    }
