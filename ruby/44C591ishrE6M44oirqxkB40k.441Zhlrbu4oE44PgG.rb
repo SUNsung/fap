fa24083b60faa30d9eee4aca8a79b10777eca75e
SUNsung <weatherius@gmail.com>
2018-03-02 22:17:29 +0200

    Rewrite [44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb] File in: 2018-03-02 22:17:28

--- ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
+++ ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
@@ -1,105 +1,86 @@
-      key = Spaceship::Portal::Key.find('some-key-id')
-      expect(key).to be_instance_of(Spaceship::Portal::Key)
-      expect(key.id).to eq('some-key-id')
-    end
-  end
-    
-        def self.last_git_tag_name(match_lightweight = true, tag_match_pattern = nil)
-      tag_pattern_param = tag_match_pattern ? '=#{tag_match_pattern.shellescape}' : ''
-    
-          if valid_type?(type)
-        type.constantize.new(attributes).tap do |instance|
-          instance.user = user if instance.respond_to?(:user=)
-        end
-      else
-        const_get(:BASE_CLASS_NAME).constantize.new(attributes).tap do |instance|
-          instance.type = type
-          instance.user = user if instance.respond_to?(:user=)
-        end
-      end
-    end
-  end
-end
-    
-        def logger
-      return super unless dry_run?
-      @dry_run_logger
-    end
-    
-      def tumblr_consumer_key
-    ENV['TUMBLR_OAUTH_KEY']
-  end
-    
-              result
-        end
-    
-    module Vagrant
-  module Util
-    # This class is used to render the ERB templates in the
-    # `GEM_ROOT/templates` directory.
-    class TemplateRenderer < OpenStruct
-      class << self
-        # Render a given template and return the result. This method optionally
-        # takes a block which will be passed the renderer prior to rendering, which
-        # allows the caller to set any view variables within the renderer itself.
-        #
-        # @return [String] Rendered template
-        def render(*args)
-          render_with(:render, *args)
-        end
-    
-          format.json do
-        @statuses = Status.as_tag_timeline(@tag, current_account, params[:local]).paginate_by_max_id(20, params[:max_id])
-        @statuses = cache_collection(@statuses, Status)
-    
-          it 'renders application layout' do
-        get :show, params: { id: 'test', max_id: late.id }
-        expect(response).to render_template layout: 'application'
-      end
-    end
-    
-      def name
-    ':#{object.shortcode}:'
-  end
-end
 
-    
-      before do
-    sign_in user, scope: :user
-  end
-    
-        # Returns the scaling and cropping geometries (in string-based ImageMagick format)
-    # neccessary to transform this Geometry into the Geometry given. If crop is true,
-    # then it is assumed the destination Geometry will be the exact final resolution.
-    # In this case, the source Geometry is scaled so that an image containing the
-    # destination Geometry would be completely filled by the source image, and any
-    # overhanging image would be cropped. Useful for square thumbnail images. The cropping
-    # is weighted at the center of the Geometry.
-    def transformation_to dst, crop = false
-      if crop
-        ratio = Geometry.new( dst.width / self.width, dst.height / self.height )
-        scale_geometry, scale = scaling(dst, ratio)
-        crop_geometry         = cropping(dst, ratio, scale)
-      else
-        scale_geometry        = dst.to_s
-      end
-    
-        def self.plural_cache
-      @plural_cache ||= PluralCache.new
+        
+              {
+          author: last_git_commit_formatted_with('%an'),
+          message: last_git_commit_formatted_with('%B'),
+          commit_hash: last_git_commit_formatted_with('%H'),
+          abbreviated_commit_hash: last_git_commit_formatted_with('%h')
+      }
     end
     
-            def rejected_types_rejected?
-          @missing_rejected_types ||= @rejected_types.select { |type| type_allowed?(type) }
-          @missing_rejected_types.none?
+          context('when the tag exists') do
+        before do
+          allow(Fastlane::Actions).to receive(:sh).with('git rev-parse -q --verify refs/tags/1.2.0 || true', { log: nil }).and_return('41215512353215321')
         end
+    
+          it 'prefers a custom version number over a boring version bump' do
+        Fastlane::FastFile.new.parse('lane :test do
+          increment_version_number(version_number: '1.77.3', bump_type: 'major')
+        end').runner.execute(:test)
+    
+    
+    {      delta = 1
+      stat = File.stat(path)
+      assert_in_delta tb,   stat.birthtime.to_f, delta
+      assert_in_delta t0+2, stat.mtime.to_f, delta
+      if stat.birthtime != stat.ctime
+        assert_in_delta t0+4, stat.ctime.to_f, delta
+      end
+      if /mswin|mingw/ !~ RUBY_PLATFORM && !Bug::File::Fs.noatime?(path)
+        # Windows delays updating atime
+        assert_in_delta t0+6, stat.atime.to_f, delta
+      end
+    }
+  rescue NotImplementedError
+  end
+    
+      before :each do
+    @object = { 1 => 2, 3 => 4, 5 => 6 }
+  end
+  it_should_behave_like :enumeratorized_with_origin_size
+end
+    
+      def test_each_pair
+    ENV.each_pair do |k, v|
+      assert_kind_of(String, k)
+      assert_kind_of(String, v)
+    end
+  end
+    
+        ('z' * 24).to_i(36).should == 22452257707354557240087211123792674815
+    
+    @@ chat
+<pre id='chat'></pre>
+<form>
+  <input id='msg' placeholder='type message here...' />
+</form>
+    
+          def html?(headers)
+        return false unless header = headers.detect { |k,v| k.downcase == 'content-type' }
+        options[:html_types].include? header.last[/^\w+\/\w+/]
       end
     end
   end
 end
 
     
-            def matches? subject
-          @subject = subject
-          @subject = subject.new if subject.class == Class
-          error_when_not_valid? && no_error_when_valid?
-        end
+    
+  it 'should allow changing the protection mode to a string' do
+    # I have no clue what other modes are available
+    mock_app do
+      use Rack::Protection::FrameOptions, :frame_options => 'ALLOW-FROM foo'
+      run DummyApp
+    end
+    
+      def as_boolean(string)
+    return true   if string == true   || string =~ (/(true|t|yes|y|1)$/i)
+    return false  if string == false  || string.blank? || string =~ (/(false|f|no|n|0)$/i)
+    raise ArgumentError.new('invalid value for Boolean: \'#{string}\'')
+  end
+    
+      public
+  def close; end;
+    
+      def fifty_percent
+    [1, (maximum * 0.5)].max.floor
+  end
