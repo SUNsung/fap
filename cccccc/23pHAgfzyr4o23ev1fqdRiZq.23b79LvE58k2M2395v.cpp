2723cbbc8e5966dbd9a6ca1c5ac3435cda9ca817
SUNsung <weatherius@gmail.com>
2018-03-02 03:31:30 +0200

    Rewrite [23pHAgfzyr4o23ev1fqdRiZq.23b79LvE58k2M2395v.cpp] File in: 2018-03-02 03:31:29

--- cccccc/23pHAgfzyr4o23ev1fqdRiZq.23b79LvE58k2M2395v.cpp
+++ cccccc/23pHAgfzyr4o23ev1fqdRiZq.23b79LvE58k2M2395v.cpp
@@ -1,237 +1,225 @@
 
         
-        
-    {} // namespace swift
-    
-      /// The list of regular users from the last run of the checker.
-  llvm::SmallVector<SILInstruction *, 16> RegularUsers;
-    
-    #include 'swift/Markup/Markup.h'
-#include 'llvm/ADT/Optional.h'
-    
-    SILDebugScope::SILDebugScope(SILLocation Loc, SILFunction *SILFn,
-                             const SILDebugScope *ParentScope ,
-                             const SILDebugScope *InlinedCallSite)
-    : Loc(Loc), InlinedCallSite(InlinedCallSite) {
-  if (ParentScope)
-    Parent = ParentScope;
-  else {
-    assert(SILFn && 'no parent provided');
-    Parent = SILFn;
-  }
-}
-    
-    /// Create a canonicalized substitution list from subs.
-/// subs is the substitution list to be canonicalized.
-/// canSubs is an out-parameter, which is used to store the results in case
-/// the list of substitutions was not canonical.
-/// The function returns a list of canonicalized substitutions.
-/// If the substitution list subs was canonical already, it will be returned and
-/// canSubs out-parameter will be empty.
-/// If something had to be canonicalized, then the canSubs out-parameter will be
-/// populated and the returned SubstitutionList would refer to canSubs storage.
-SubstitutionList
-getCanonicalSubstitutionList(SubstitutionList subs,
-                             SmallVectorImpl<Substitution> &canSubs);
-    
-      void setCodeCompletionPoint(unsigned BufferID, unsigned Offset) {
-    assert(BufferID != 0U && 'Buffer should be valid');
-    }
-    
-    
-    {}  // namespace leveldb
-    
-    
-    {  // When limit user key is prefix of start user key
-  ASSERT_EQ(IKey('foobar', 100, kTypeValue),
-            Shorten(IKey('foobar', 100, kTypeValue),
-                    IKey('foo', 200, kTypeValue)));
-}
-    
-    namespace leveldb {
-    }
-    
-      // If a seek to internal key 'k' in specified file finds an entry,
-  // call (*handle_result)(arg, found_key, found_value).
-  Status Get(const ReadOptions& options,
-             uint64_t file_number,
-             uint64_t file_size,
-             const Slice& k,
-             void* arg,
-             void (*handle_result)(void*, const Slice&, const Slice&));
-    
-    
-    {}  // namespace leveldb
-    
-      // Return a string that contains the copy of the referenced data.
-  std::string ToString() const { return std::string(data_, size_); }
-    
-      caffe::Datum datum;
-  datum.set_channels(2);  // one channel for each image in the pair
-  datum.set_height(rows);
-  datum.set_width(cols);
-  LOG(INFO) << 'A total of ' << num_items << ' items.';
-  LOG(INFO) << 'Rows: ' << rows << ' Cols: ' << cols;
-  for (int itemid = 0; itemid < num_items; ++itemid) {
-    int i = caffe::caffe_rng_rand() % num_items;  // pick a random  pair
-    int j = caffe::caffe_rng_rand() % num_items;
-    read_image(&image_file, &label_file, i, rows, cols,
-        pixels, &label_i);
-    read_image(&image_file, &label_file, j, rows, cols,
-        pixels + (rows * cols), &label_j);
-    datum.set_data(pixels, 2*rows*cols);
-    if (label_i  == label_j) {
-      datum.set_label(1);
-    } else {
-      datum.set_label(0);
-    }
-    datum.SerializeToString(&value);
-    std::string key_str = caffe::format_int(itemid, 8);
-    db->Put(leveldb::WriteOptions(), key_str, value);
-  }
-    
-      virtual inline const char* type() const { return 'AbsVal'; }
-  virtual inline int ExactNumBottomBlobs() const { return 1; }
-  virtual inline int ExactNumTopBlobs() const { return 1; }
-    
-    
-    {}  // namespace caffe
-    
-      int num_spatial_axes_;
-  int bottom_dim_;
-  int top_dim_;
-    
-     protected:
-  /**
-   * @param bottom input Blob vector (length 2+)
-   *   -# @f$ (N \times ...) @f$
-   *      the inputs @f$ x_1 @f$
-   *   -# @f$ (M) @f$
-   *      the inputs @f$ x_2 @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (M \times ...) @f$:
-   *      the reindexed array @f$
-   *        y = x_1[x_2]
-   *      @f$
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-      virtual inline int ExactNumBottomBlobs() const { return 3; }
-  virtual inline const char* type() const { return 'ContrastiveLoss'; }
-  /**
-   * Unlike most loss layers, in the ContrastiveLossLayer we can backpropagate
-   * to the first two inputs.
-   */
-  virtual inline bool AllowForceBackward(const int bottom_index) const {
-    return bottom_index != 2;
-  }
-    
-    namespace caffe {
-    }
-    
-    #ifdef USE_CUDNN
-/**
- * @brief CuDNN acceleration of TanHLayer.
- */
-template <typename Dtype>
-class CuDNNTanHLayer : public TanHLayer<Dtype> {
+        class TestFileSystem : public NullFileSystem {
  public:
-  explicit CuDNNTanHLayer(const LayerParameter& param)
-      : TanHLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNTanHLayer();
+  Status NewRandomAccessFile(
+      const string& fname, std::unique_ptr<RandomAccessFile>* result) override {
+    result->reset(new TestRandomAccessFile);
+    return Status::OK();
+  }
+  // Always return size of 10
+  Status GetFileSize(const string& fname, uint64* file_size) override {
+    *file_size = 10;
+    return Status::OK();
+  }
+};
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    namespace tensorflow {
     }
     
-    #endif  // CAFFE_DECONV_LAYER_HPP_
-
+      // Populates the CUDA-platform-specific elements of this object.
+  port::Status Init();
     
-    /**
- * @brief Compute elementwise operations, such as product and sum,
- *        along multiple input Blobs.
- *
- * TODO(dox): thorough documentation for Forward, Backward, and proto params.
- */
-template <typename Dtype>
-class EltwiseLayer : public Layer<Dtype> {
+    // A PluginConfig describes the set of plugins to be used by a StreamExecutor
+// instance. Each plugin is defined by an arbitrary identifier, usually best set
+// to the address static member in the implementation (to avoid conflicts).
+//
+// A PluginConfig may be passed to the StreamExecutor constructor - the plugins
+// described therein will be used to provide BLAS, DNN, FFT, and RNG
+// functionality. Platform-appropriate defaults will be used for any un-set
+// libraries. If a platform does not support a specified plugin (ex. cuBLAS on
+// an OpenCL executor), then an error will be logged and no plugin operations
+// will succeed.
+//
+// The StreamExecutor BUILD target does not link ANY plugin libraries - even
+// common host fallbacks! Any plugins must be explicitly linked by dependent
+// targets. See the cuda, opencl and host BUILD files for implemented plugin
+// support (search for 'plugin').
+class PluginConfig {
  public:
-  explicit EltwiseLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+  // Value specifying the platform's default option for that plugin.
+  static const PluginId kDefault;
     }
     
-    #endif  // CAFFE_EXP_LAYER_HPP_
-
-    
-    #endif  // CAFFE_FILTER_LAYER_HPP_
-
-    
-    #include <vector>
-#include <string>
-    
-    static int __ParseStack(FILE* file,  NDK_CRASH_PARSER_STATE& _state, char _strcache[2048], std::string& strout) {
-    ASSERT(file);
+    TEST_F(NodeDefBuilderTest, NInPolymorphicTwice) {
+  Op(OpDefBuilder('NInPolymorphicTwice')
+         .Input('a: N*T')
+         .Input('b: N*T')
+         .Attr('T: type')
+         .Attr('N: int >= 0'));
     }
     
-    void WakeUpLock::Unlock() {
-    ::wakeupLock_Unlock(object_);
-}
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
     
-    void SimpleIntPack(const void* _data, size_t _datalen, AutoBuffer& _outbuf) {
-    SimplePack<unsigned int>(_data, _datalen, _outbuf);
-}
     
-    //
-//  testspy.h
-//  PublicComponent
-//
-//  Created by yerungui on 14-5-13.
-//
+    {  void CopyDeviceTensorToCPU(const Tensor *device_tensor, StringPiece edge_name,
+                             Device *device, Tensor *cpu_tensor,
+                             StatusCallback done) override;
+};
     
-    #ifndef _COMM_FUNCTION_H_
-#define _COMM_FUNCTION_H_
+        http://www.apache.org/licenses/LICENSE-2.0
     
-    void AbstractHttpServerResponseCommand::updateReadWriteCheck()
-{
-  if (httpServer_->wantRead()) {
-    if (!readCheck_) {
-      readCheck_ = true;
-      e_->addSocketForReadCheck(socket_, this);
+      void Compute(OpKernelContext* c) override {
+    OpInputList indices_inputs;
+    OpInputList data_inputs;
+    int first_dim_size;
+    int data_elements_size;
+    Tensor* merged = nullptr;
+    this->CheckArgsAndAllocateResult(c, &indices_inputs, &data_inputs,
+                                     &first_dim_size, &data_elements_size,
+                                     &merged);
+    if (!c->status().ok()) {
+      // Avoid segmentation faults if merged cannot be allocated and an error is
+      // passed back in the context.
+      return;
     }
-  }
-  else if (readCheck_) {
-    readCheck_ = false;
-    e_->deleteSocketForReadCheck(socket_, this);
-  }
-  if (httpServer_->wantWrite()) {
-    if (!writeCheck_) {
-      writeCheck_ = true;
-      e_->addSocketForWriteCheck(socket_, this);
     }
-  }
-  else if (writeCheck_) {
-    writeCheck_ = false;
-    e_->deleteSocketForWriteCheck(socket_, this);
-  }
-}
     
-    #endif // D_ABSTRACT_PROXY_REQUEST_COMMAND_H
-
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-      CFRef<CFArrayRef> items;
-  CFArrayRef raw_items = nullptr;
-  OSStatus rv = SecPKCS12Import(data, options.get(), &raw_items);
-  if (rv != errSecSuccess) {
-    A2_LOG_DEBUG(
-        fmt('Failed to parse PKCS12 data: %s', errToString(rv).c_str()));
+    /// Index the given module and store the results to \p indexStorePath.
+///
+/// \param module The module to index.
+///
+/// \param indexUnitTokens A list of unique identifiers for the index units to
+/// be written. This may either be one unit per source file of \p module, or it
+/// may be a single unit, in which case all the index information will be
+/// combined into a single unit.
+///
+/// \param moduleUnitToken A unique identifier for this module unit in the form
+/// of a file path. Only used if \p indexUnitTokens are specified for each
+/// source file, otherwise the single \p indexUnitTokens value is used instead.
+///
+/// \param indexStorePath The location to write the indexing data to.
+///
+/// \param indexSystemModules If true, emit index data for imported serialized
+/// swift system modules.
+///
+/// \param isDebugCompilation true for non-optimized compiler invocation.
+///
+/// \param targetTriple The target for this compilation.
+///
+/// \param dependencyTracker The set of dependencies seen while building.
+bool indexAndRecord(ModuleDecl *module, ArrayRef<std::string> indexUnitTokens,
+                    StringRef moduleUnitToken, StringRef indexStorePath,
+                    bool indexSystemModules, bool isDebugCompilation,
+                    StringRef targetTriple,
+                    const DependencyTracker &dependencyTracker);
+// FIXME: indexUnitTokens could be StringRef, but that creates an impedance
+// mismatch in the caller.
+    
+    static bool writeRecord(SymbolTracker &record, std::string Filename,
+                        std::string indexStorePath, DiagnosticEngine *diags,
+                        std::string &outRecordFile) {
+  if (record.getOccurrences().empty()) {
+    outRecordFile = std::string();
     return false;
   }
-  items.reset(raw_items);
+    }
+    
+    class Text final : public InlineContent {
+  StringRef LiteralContent;
+  Text(StringRef LiteralContent)
+    : InlineContent(ASTNodeKind::Text),
+      LiteralContent(LiteralContent) {}
+public:
+  static Text *create(MarkupContext &MC, StringRef LiteralContent);
+  StringRef getLiteralContent() const { return LiteralContent; };
+  void setLiteralContent(StringRef LC) {
+    LiteralContent = LC;
+  }
+    }
+    
+    /// A SyntaxRewriter for applying a set of formatting rules to a Syntax tree.
+struct FormatSyntaxRewriter : public SyntaxRewriter {
+  virtual StructDeclSyntax
+  rewriteStructDecl(StructDeclSyntax Struct) override;
+};
+    
+      /// Indicates whether the diagnostics produced during compilation should be
+  /// checked against expected diagnostics, indicated by markers in the
+  /// input source file.
+  enum {
+    NoVerify,
+    Verify,
+    VerifyAndApplyFixes
+  } VerifyMode = NoVerify;
+    
+    class EditorDiagConsumer : public swift::DiagnosticConsumer {
+  typedef std::vector<DiagnosticEntryInfo> DiagnosticsTy;
+  /// Maps from a BufferID to the diagnostics that were emitted inside that
+  /// buffer.
+  llvm::DenseMap<unsigned, DiagnosticsTy> BufferDiagnostics;
+    }
+    
+      /// Creates and adds a memory buffer to the \c SourceManager, taking
+  /// ownership of the newly created copy.
+  ///
+  /// \p InputData and \p BufIdentifier are copied, so that this memory can go
+  /// away as soon as this function returns.
+  unsigned addMemBufferCopy(StringRef InputData, StringRef BufIdentifier = '');
+    
+      /**
+   * @brief Plan the best set of indexes for event record access.
+   *
+   * @param start an inclusive time to begin searching.
+   * @param stop an inclusive time to end searching.
+   * @param sort if true the indexes will be sorted.
+   *
+   * @return List of 'index.step' index strings.
+   */
+  std::vector<std::string> getIndexes(EventTime start,
+                                      EventTime stop,
+                                      bool sort = true);
+    
+    /// External (extensions) SQL implementation of the osquery getQueryColumns API.
+Status getQueryColumnsExternal(const std::string& query, TableColumns& columns);
+    
+    struct FlagInfo {
+  std::string type;
+  std::string description;
+  std::string default_value;
+  std::string value;
+  FlagDetail detail;
+};
+    
+    /**
+ * @brief Prepend a reference number to the log line.
+ *
+ * A reference number is an external-search helper for somewhat confusing or
+ * seeminly-critical log lines.
+ */
+#define RLOG(n) '[Ref #' #n '] '
+    
+    #include <gtest/gtest.h>
+    
+    /**
+ * @brief Access the internal storage of the Decorator parser.
+ *
+ * The decoration set is a map of column name to value. It contains the opaque
+ * set of decoration point results.
+ *
+ * Decorations are applied to log items before they are sent to the downstream
+ * logging APIs: logString, logSnapshot, etc.
+ *
+ * @param results the output parameter to write decorations.
+ */
+void getDecorations(std::map<std::string, std::string>& results);
+    
+        std::string arg_timeout('--timeout');
+    arguments.push_back(arg_timeout.c_str());
+    arguments.push_back(extensions_timeout.c_str());
