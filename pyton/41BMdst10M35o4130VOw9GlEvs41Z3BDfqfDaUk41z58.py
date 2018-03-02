af5e51b1f8d5bb59c876b650b28c729b159866b3
SUNsung <weatherius@gmail.com>
2018-03-02 12:46:13 +0200

    Rewrite [41BMdst10M35o4130VOw9GlEvs41Z3BDfqfDaUk41z58.py] File in: 2018-03-02 12:46:13

--- pyton/41BMdst10M35o4130VOw9GlEvs41Z3BDfqfDaUk41z58.py
+++ pyton/41BMdst10M35o4130VOw9GlEvs41Z3BDfqfDaUk41z58.py
@@ -1,186 +1,50 @@
 
         
-        
-@mock.patch('httpie.input.AuthCredentials._getpass',
-            new=lambda self, prompt: 'UNEXPECTED_PROMPT_RESPONSE')
-def test_auth_plugin_prompt_password_false(httpbin):
+        # If true, SmartyPants will be used to convert quotes and dashes to
+# typographically correct entities.
+#html_use_smartypants = True
     
-            request_line = '{method} {path}{query} HTTP/1.1'.format(
-            method=self._orig.method,
-            path=url.path or '/',
-            query='?' + url.query if url.query else ''
-        )
+        with app.app_context():
+        init_db()
+        yield app
     
     
-def is_valid_mime(mime):
-    return mime and MIME_RE.match(mime)
+@app.route('/<username>/unfollow')
+def unfollow_user(username):
+    '''Removes the current user as follower of the given user.'''
+    if not g.user:
+        abort(401)
+    whom_id = get_user_id(username)
+    if whom_id is None:
+        abort(404)
+    db = get_db()
+    db.execute('delete from follower where who_id=? and whom_id=?',
+              [session['user_id'], whom_id])
+    db.commit()
+    flash('You are no longer following '%s'' % username)
+    return redirect(url_for('user_timeline', username=username))
     
     
-FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
-FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
-JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
-BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
-    
-        exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR
-    assert error_msg == (
-        'ConnectionError: '
-        'Connection aborted while doing GET request to URL: '
-        'http://www.google.com')
-    
-        '''
-)
-    
-        # 128+2 SIGINT <http://www.tldp.org/LDP/abs/html/exitcodes.html>
-    ERROR_CTRL_C = 130
-    
-            '''
-        if self.args.method is None:
-            # Invoked as `http URL'.
-            assert not self.args.items
-            if not self.args.ignore_stdin and not self.env.stdin_isatty:
-                self.args.method = HTTP_POST
-            else:
-                self.args.method = HTTP_GET
-    
-            # put all lines in the file into a Python list
-        strings = f.readlines()
-        
-        # above line leaves trailing newline characters; strip them out
-        strings = [x.strip(u'\n') for x in strings]
-        
-        # remove empty-lines and comments
-        strings = [x for x in strings if x and not x.startswith(u'#')]
-        
-        # insert empty string since all are being removed
-        strings.insert(0, u'')
-    
-    # Can't import these from paths.py because that uses `future` imports
-DIR_OF_CURRENT_SCRIPT = os.path.dirname( os.path.abspath( __file__ ) )
-DIR_OF_YCMD = os.path.join( DIR_OF_CURRENT_SCRIPT, '..', '..', 'third_party',
-                            'ycmd' )
-    
-    The follow diagram and text describe the data-flow through the system:
-    
-            finished, pending = futures.wait(
-                [SUCCESSFUL_FUTURE,
-                 CANCELLED_AND_NOTIFIED_FUTURE,
-                 EXCEPTION_FUTURE,
-                 future1,
-                 future2],
-                return_when=futures.ALL_COMPLETED)
+import argparse
+import collections
+import os
+import sys
     
     
-def _FormatCompleterDebugInfo( completer ):
-  message = '{0} completer debug information:\n'.format( completer[ 'name' ] )
-  for server in completer[ 'servers' ]:
-    name = server[ 'name' ]
-    if server[ 'is_running' ]:
-      address = server[ 'address' ]
-      port = server[ 'port' ]
-      if address and port:
-        message += '  {0} running at: http://{1}:{2}\n'.format( name,
-                                                                address,
-                                                                port )
-      else:
-        message += '  {0} running\n'.format( name )
-      message += '  {0} process ID: {1}\n'.format( name, server[ 'pid' ] )
-    else:
-      message += '  {0} not running\n'.format( name )
-    message += '  {0} executable: {1}\n'.format( name, server[ 'executable'] )
-    logfiles = server[ 'logfiles' ]
-    if logfiles:
-      message += '  {0} logfiles:\n'.format( name )
-      for logfile in logfiles:
-        message += '    {0}\n'.format( logfile )
-    else:
-      message += '  No logfiles available\n'
-    if 'extras' in server:
-      for extra in server[ 'extras' ]:
-        message += '  {0} {1}: {2}\n'.format( name,
-                                              extra[ 'key' ],
-                                              extra[ 'value' ] )
-  for item in completer[ 'items' ]:
-    message += '  {0}: {1}\n'.format( item[ 'key' ].capitalize(),
-                                      item[ 'value' ] )
-  return message
+def secure_hash_s(data, hash_func=sha1):
+    ''' Return a secure hash hex digest of data. '''
     
-    DIR_OF_THIS_SCRIPT = p.dirname( p.abspath( __file__ ) )
-DIR_OF_OLD_LIBS = p.join( DIR_OF_THIS_SCRIPT, 'python' )
+        if key:
+        return key
     
-      return [ config_entry ]
+        # new build
     
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    
-def MakeUserOptions( custom_options = {} ):
-  options = dict( user_options_store.DefaultOptions() )
-  options.update( DEFAULT_CLIENT_OPTIONS )
-  options.update( custom_options )
-  return options
-    
-        Configurable subclasses must define the class methods
-    `configurable_base` and `configurable_default`, and use the instance
-    method `initialize` instead of ``__init__``.
-    
-            auth_header = self.request.headers.get('Authorization', None)
-        if auth_header is not None:
-            auth_mode, params = auth_header.split(' ', 1)
-            assert auth_mode == 'Digest'
-            param_dict = {}
-            for pair in params.split(','):
-                k, v = pair.strip().split('=', 1)
-                if v[0] == ''' and v[-1] == ''':
-                    v = v[1:-1]
-                param_dict[k] = v
-            assert param_dict['realm'] == realm
-            assert param_dict['opaque'] == opaque
-            assert param_dict['nonce'] == nonce
-            assert param_dict['username'] == username
-            assert param_dict['uri'] == self.request.path
-            h1 = md5(utf8('%s:%s:%s' % (username, realm, password))).hexdigest()
-            h2 = md5(utf8('%s:%s' % (self.request.method,
-                                     self.request.path))).hexdigest()
-            digest = md5(utf8('%s:%s:%s' % (h1, nonce, h2))).hexdigest()
-            if digest == param_dict['response']:
-                self.write('ok')
-            else:
-                self.write('fail')
-        else:
-            self.set_status(401)
-            self.set_header('WWW-Authenticate',
-                            'Digest realm='%s', nonce='%s', opaque='%s'' %
-                            (realm, nonce, opaque))
-    
-       Unlike the standard library's `queue` module, the classes defined here
-   are *not* thread-safe. To use these queues from another thread,
-   use `.IOLoop.add_callback` to transfer control to the `.IOLoop` thread
-   before calling any queue methods.
-'''
-    
-    
-class AuthLoginHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
-    @tornado.web.asynchronous
-    def get(self):
-        my_url = (self.request.protocol + '://' + self.request.host +
-                  '/auth/login?next=' +
-                  tornado.escape.url_escape(self.get_argument('next', '/')))
-        if self.get_argument('code', False):
-            self.get_authenticated_user(
-                redirect_uri=my_url,
-                client_id=self.settings['facebook_api_key'],
-                client_secret=self.settings['facebook_secret'],
-                code=self.get_argument('code'),
-                callback=self._on_auth)
-            return
-        self.authorize_redirect(redirect_uri=my_url,
-                                client_id=self.settings['facebook_api_key'],
-                                extra_params={'scope': 'user_posts'})
+            # global, resource
+        input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/global/urlMaps'
+        actual = GCPUtils.parse_gcp_url(input_url)
+        self.assertEquals('compute', actual['service'])
+        self.assertEquals('v1', actual['api_version'])
+        self.assertEquals('myproject', actual['project'])
+        self.assertTrue('global' in actual)
+        self.assertTrue(actual['global'])
+        self.assertEquals('urlMaps', actual['resource_name'])
