3a70cebbe2a4e05611a407ad08597aaa1c72f24e
SUNsung <weatherius@gmail.com>
2018-03-02 10:37:36 +0200

    Rewrite [241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb] File in: 2018-03-02 10:37:36

--- ruby/241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb
+++ ruby/241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb
@@ -1,43 +1,93 @@
 
         
-              config.paths['log']             = '#{Msf::Config.log_directory}/#{Rails.env}.log'
-      config.paths['config/database'] = [Metasploit::Framework::Database.configurations_pathname.try(:to_path)]
+              # Returns the time the given Sass file was last modified.
+      #
+      # If the given file has been deleted or the time can't be accessed
+      # for some other reason, this should return nil.
+      #
+      # @param uri [String] The URI of the file to check.
+      #   Comes from a `:filename` option set on an engine returned by this importer.
+      # @param options [{Symbol => Object}] Options for the Sass file
+      #   containing the `@import` currently being checked.
+      # @return [Time, nil]
+      def mtime(uri, options)
+        Sass::Util.abstract(self)
+      end
     
-    # Copyright (C) 2008 Rapid7, Inc.
-    
-          when :login_fail
-        if(s[:user] and s[:pass])
-          report_auth_info(s.merge({:active => false}))
-          print_status('Failed FTP Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]}')
-    
-          # There is only one pattern per run to test
-      matched = nil
-      matches = nil
-    
-    
-    
-      @src.__NR_execve      = __NR_execve
-  @src.__NR_getpeername = __NR_getpeername
-  @src.__NR_accept      = __NR_accept
-  @src.__NR_listen      = __NR_listen
-  @src.__NR_bind        = __NR_bind
-  @src.__NR_socket      = __NR_socket
-  @src.__NR_connect     = __NR_connect
-  @src.__NR_close       = __NR_close
-  @src.__NR_kfcntl      = __NR_kfcntl
-    
-        # Extracts the Geometry from a 'WxH,O' string
-    # Where W is the width, H is the height,
-    # and O is the EXIF orientation
-    def self.parse(string)
-      GeometryParser.new(string).make
+      # Uninstalls this logger from \{Sass.logger\}. This should only be called if
+  # the logger was installed using \{#install!}
+  def uninstall!
+    if Sass.logger != self
+      throw Exception.new('Can't uninstall a logger that's not currently installed.')
     end
     
-            def allowing *types
-          @allowed_types = types.flatten
-          self
-        end
+      // writing
+  $('form').on('submit',function(e) {
+    $.post('/', {msg: '<%= user %>: ' + $('#msg').val()});
+    $('#msg').val(''); $('#msg').focus();
+    e.preventDefault();
+  });
+</script>
     
-            def failure_message
-          'Attachment #{@attachment_name} should be required'
+        set :run, Proc.new { File.expand_path($0) == File.expand_path(app_file) }
+    
+          def accepts?(env)
+        cookie_header = env['HTTP_COOKIE']
+        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
+        cookies.each do |k, v|
+          if k == session_key && Array(v).size > 1
+            bad_cookies << k
+          elsif k != session_key && Rack::Utils.unescape(k) == session_key
+            bad_cookies << k
+          end
         end
+        bad_cookies.empty?
+      end
+    
+      context 'with custom session key' do
+    it 'denies requests with duplicate session cookies' do
+      mock_app do
+        use Rack::Protection::CookieTossing, :session_key => '_session'
+        run DummyApp
+      end
+    
+        it 'accepts #{method} requests with whitelisted Origin' do
+      mock_app do
+        use Rack::Protection::HttpOrigin, :origin_whitelist => ['http://www.friend.com']
+        run DummyApp
+      end
+      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://www.friend.com')).to be_ok
+    end
+  end
+end
+
+    
+          it { expect(instrumenter).not_to receive(:instrument) }
+    end
+  end
+    
+      it 'denies requests with a changing Accept-Language header' do
+    session = {:foo => :bar}
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'b'
+    expect(session).to be_empty
+  end
+    
+        # The time zone to use for timestamp interpolation.  Using the default
+    # time zone ensures that results are consistent across all threads.
+    def time_zone
+      @options[:use_default_time_zone] ? Time.zone_default : Time.zone
+    end
+    
+        # Perform the actual interpolation. Takes the pattern to interpolate
+    # and the arguments to pass, which are the attachment and style name.
+    # You can pass a method name on your record as a symbol, which should turn
+    # an interpolation pattern for Paperclip to use.
+    def self.interpolate pattern, *args
+      pattern = args.first.instance.send(pattern) if pattern.kind_of? Symbol
+      result = pattern.dup
+      interpolators_cache.each do |method, token|
+        result.gsub!(token) { send(method, *args) } if result.include?(token)
+      end
+      result
+    end
