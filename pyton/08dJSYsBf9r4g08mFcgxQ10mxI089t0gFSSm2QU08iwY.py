cebf075044fd41bed7873cf2f9b7114d1ef590e1
SUNsung <weatherius@gmail.com>
2018-03-02 23:56:13 +0200

    Rewrite [08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py] File in: 2018-03-02 23:56:13

--- pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
+++ pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
@@ -1,66 +1,117 @@
-    # not a test, but looks like a test
-    'scrapy/utils/testsite.py',
-    
-    
-class _BenchSpider(scrapy.Spider):
-    '''A spider that follows all links'''
-    name = 'follow'
-    total = 10000
-    show = 20
-    baseurl = 'http://localhost:8998'
-    link_extractor = LinkExtractor()
-    
-        def short_desc(self):
-        return 'Run a spider'
-    
-    
-class Rank(Enum):
-    
-        def set(self, results, query):
-        '''Set the result for the given query key in the cache.
+
         
-        When updating an entry, updates its position to the front of the LRU list.
-        If the entry is new and the cache is at capacity, removes the oldest entry
-        before the new entry is added.
-        '''
-        node = self.lookup[query]
-        if node is not None:
-            # Key exists in cache, update the value
-            node.results = results
-            self.linked_list.move_to_front(node)
-        else:
-            # Key does not exist in cache
-            if self.size == self.MAX_SIZE:
-                # Remove the oldest entry from the linked list and lookup
-                self.lookup.pop(self.linked_list.tail.query, None)
-                self.linked_list.remove_from_tail()
-            else:
-                self.size += 1
-            # Add the new key and value
-            new_node = Node(results)
-            self.linked_list.append_to_front(new_node)
-            self.lookup[query] = new_node
+                # Keyword arguments > stream.encoding > default utf8
+        if self.stdin_encoding is None:
+            self.stdin_encoding = getattr(
+                self.stdin, 'encoding', None) or 'utf8'
+        if self.stdout_encoding is None:
+            actual_stdout = self.stdout
+            if is_windows:
+                # noinspection PyUnresolvedReferences
+                from colorama import AnsiToWin32
+                if isinstance(self.stdout, AnsiToWin32):
+                    actual_stdout = self.stdout.wrapped
+            self.stdout_encoding = getattr(
+                actual_stdout, 'encoding', None) or 'utf8'
     
-        def parse_query(self, query):
-        '''Remove markup, break text into terms, deal with typos,
-        normalize capitalization, convert to use boolean operations.
-        '''
-        ...
     
-            pdf = match1(content, r'name='filename'\s*value='([^']+\.pdf)'')
-        if pdf: pdf = 'http://res.infoq.com/downloads/pdfdownloads/%s' % pdf
+def test_follow_all_redirects_shown(httpbin):
+    r = http('--follow', '--all', httpbin.url + '/redirect/2')
+    assert r.count('HTTP/1.1') == 3
+    assert r.count('HTTP/1.1 302 FOUND', 2)
+    assert HTTP_OK in r
     
-    __all__ = ['kuwo_download']
+        def unregister(self, plugin):
+        self._plugins.remove(plugin)
     
-        # mgid%3Auma%3Avideo%3Amtv81.com%3A897974
-    vid = match1(html, r'getTheVideo\('(.*?)'')
-    xml = parseString(
-        get_content('http://intl.esperanto.mtvi.com/www/xml/media/mediaGen.jhtml?uri={}&flashPlayer=LNX%2013,0,0,206&geo=CN&sid=123456'.format(vid)))
+        parent = os.path.join(path, os.path.pardir)
+    parent = os.path.realpath(parent)
     
-    from mako.lookup import TemplateLookup
-from pylons.error import handle_mako_error
-from pylons.configuration import PylonsConfig
+            # global, resource, entity
+        input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/global/urlMaps/my-url-map'
+        actual = GCPUtils.parse_gcp_url(input_url)
+        self.assertEquals('myproject', actual['project'])
+        self.assertTrue('global' in actual)
+        self.assertTrue(actual['global'])
+        self.assertEquals('v1', actual['api_version'])
+        self.assertEquals('compute', actual['service'])
     
-        def GET_promohealth(self):
-        response.content_type = 'application/json'
-        return json.dumps(promote.health_check())
+    from ansible.plugins.terminal import TerminalBase
+from ansible.errors import AnsibleConnectionFailure
+    
+    import os
+import re
+    
+    def plot(func):
+    random_state = check_random_state(0)
+    one_core = []
+    multi_core = []
+    sample_sizes = range(1000, 6000, 1000)
+    
+        ###########################################################################
+    # Set transformer input
+    ###########################################################################
+    transformers = {}
+    
+       ###########################################################################
+    # Set custom automatic method selection
+    sampling_algorithm['custom-auto'] = \
+        lambda n_population, n_samples, random_state=None: \
+            sample_without_replacement(n_population,
+                                       n_samples,
+                                       method='auto',
+                                       random_state=random_state)
+    
+    First, we fix a training set, increase the number of
+samples to classify and plot number of classified samples as a
+function of time.
+    
+        url_fmt is along the lines of ('https://github.com/USER/PROJECT/'
+                                   'blob/{revision}/{package}/'
+                                   '{path}#L{lineno}')
+    '''
+    revision = _get_git_revision()
+    return partial(_linkcode_resolve, revision=revision, package=package,
+                   url_fmt=url_fmt)
+
+    
+    import lxml.html
+from lxml.etree import ElementTree
+import numpy as np
+    
+            controller.coef0 = Tk.StringVar()
+        controller.coef0.set('0')
+        r = Tk.Frame(valbox)
+        Tk.Label(r, text='coef0:', anchor='e', width=7).pack(side=Tk.LEFT)
+        Tk.Entry(r, width=6, textvariable=controller.coef0).pack(side=Tk.LEFT)
+        r.pack()
+        valbox.pack(side=Tk.LEFT)
+    
+    Plot the classification probability for different classifiers. We use a 3
+class dataset, and we classify it with a Support Vector classifier, L1
+and L2 penalized logistic regression with either a One-Vs-Rest or multinomial
+setting, and Gaussian process classification.
+    
+    # Author: Olivier Grisel <olivier.grisel@ensta.org>
+# License: BSD 3 clause
+    
+        if update:
+        time.sleep(1)
+        pull_screenshot()
+        im.set_array(update_data())
+        update = False
+    return im,
+    
+    if sys.version_info.major != 3:
+    print('请使用Python3')
+    exit(1)
+try:
+    from common import debug, config, screenshot, UnicodeStreamFilter
+    from common.auto_adb import auto_adb
+except Exception as ex:
+    print(ex)
+    print('请将脚本放在项目根目录中运行')
+    print('请检查项目根目录中的 common 文件夹是否存在')
+    exit(1)
+adb = auto_adb()
+VERSION = '1.1.4'
