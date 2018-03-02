d088ac616f704ad5dc4458ef1832ebd16a639f90
SUNsung <weatherius@gmail.com>
2018-03-02 12:04:09 +0200

    Rewrite [25a3.kxPhUsE25a6lVJEmo3BQ25kZhSMSFcSbs25IS.rb] File in: 2018-03-02 12:04:09

--- ruby/25a3.kxPhUsE25a6lVJEmo3BQ25kZhSMSFcSbs25IS.rb
+++ ruby/25a3.kxPhUsE25a6lVJEmo3BQ25kZhSMSFcSbs25IS.rb
@@ -1,140 +1,57 @@
 
         
-        CONTENT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
-    
-    require 'pry'
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-require 'jekyll'
-    
-    config = File.expand_path '../lib/jekyll/mime.types', __dir__
-    
-    module Jekyll
-  module Deprecator
-    extend self
-    
-    puts 'Deduping #{links.size} links...'
-    
-    html_readme = '<html>#{Kramdown::Document.new(open('README.md').read).to_html}</html>'
-readme_doctree = REXML::Document.new(html_readme)
-links = REXML::XPath.match(readme_doctree, '//a')
-    
-        # .visible-sm { @include responsive-visibility() }
-    # to:
-    # @include responsive-visibility('.visible-sm')
-    def apply_mixin_parent_selector(file, rule_sel)
-      log_transform rule_sel
-      replace_rules file, '\s*' + rule_sel, comments: false do |rule, rule_pos, css|
-        body = unwrap_rule_block(rule.dup).strip
-        next rule unless body =~ /^@include \w+/m || body =~ /^@media/ && body =~ /\{\s*@include/
-        rule =~ /(#{COMMENT_RE}*)([#{SELECTOR_CHAR}\s*]+?)#{RULE_OPEN_BRACE_RE}/
-        cmt, sel = $1, $2.strip
-        # take one up selector chain if this is an &. selector
-        if sel.start_with?('&')
-          parent_sel = selector_for_pos(css, rule_pos.begin)
-          sel        = parent_sel + sel[1..-1]
-        end
-        # unwrap, and replace @include
-        unindent unwrap_rule_block(rule).gsub(/(@include [\w-]+)\(?([\$\w\-,\s]*)\)?/) {
-          name, args = $1, $2
-          sel.gsub(/\s+/, ' ').split(/,\s*/ ).map { |sel_part|
-            '#{cmt}#{name}('#{sel_part}'#{', ' if args && !args.empty?}#{args})'
-          }.join(';\n')
-        }
-      end
-    end
-    }
-    
-        def log_http_get_files(files, from, cached = false)
-      return if files.empty?
-      s = '  #{'CACHED ' if cached}GET #{files.length} files from #{from} #{files * ' '}...'
-      if cached
-        puts dark green s
-      else
-        puts dark cyan s
-      end
+            def register_lotus
+      Lotus::Assets.sources << assets_path
     end
     
-      path = 'assets/stylesheets'
-  css_path = args.with_defaults(css_path: 'tmp')[:css_path]
-  puts Term::ANSIColor.bold 'Compiling SCSS in #{path}'
-  Dir.mkdir(css_path) unless File.directory?(css_path)
-  %w(_bootstrap bootstrap/_theme).each do |file|
-    save_path = '#{css_path}/#{file.sub(/(^|\/)?_+/, '\1').sub('/', '-')}.css'
-    puts Term::ANSIColor.cyan('  #{save_path}') + '...'
-    engine    = Sass::Engine.for_file('#{path}/#{file}.scss', syntax: :scss, load_paths: [path])
-    css       = engine.render
-    File.open(save_path, 'w') { |f| f.write css }
-  end
-end
+      # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
     
-        def metadata_subdir(leaf, version: self.version, timestamp: :latest, create: false)
-      if create && timestamp == :latest
-        raise CaskError, 'Cannot create metadata subdir when timestamp is :latest.'
+      def test_font_helper_without_suffix
+    assert_match %r(url\(['']?/assets/.*eot['']?\)), @css
+  end
+    
+          attr_reader :app, :options
+    
+          def bad_cookies
+        @bad_cookies ||= []
       end
     
-        # @abstract
-    #
-    # Identify the SHA of the commit that will be deployed.  This will most likely involve SshKit's capture method.
-    #
-    # @return void
-    #
-    def fetch_revision
-      raise NotImplementedError, 'Your SCM strategy module should provide a #fetch_revision method'
+          expected_header = <<-END.chomp
+rack.%2573ession=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.%2573ession=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.%2573ession=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.session=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.session=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.session=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
+END
+      expect(last_response.headers['Set-Cookie']).to eq(expected_header)
     end
   end
-end
-
     
-      desc 'Published'
-  task :published do
+        it 'copes with nested arrays' do
+      mock_app do |env|
+        request = Rack::Request.new(env)
+        [200, {'Content-Type' => 'text/plain'}, [request.params['foo']['bar']]]
+      end
+      get '/', :foo => {:bar => '<bar>'}
+      expect(body).to eq('&lt;bar&gt;')
+    end
+    
+        %w[/foo/bar /foo/bar/ / /.f /a.x].each do |path|
+      it('does not touch #{path.inspect}') { expect(get(path).body).to eq(path) }
+    end
+    
+          session = {:foo => :bar}
+      get '/', {}, 'rack.session' => session, 'HTTP_USER_AGENT' => 'a'
+      get '/', {}, 'rack.session' => session, 'HTTP_USER_AGENT' => 'b'
+      expect(session[:foo]).to eq :bar
+    end
+    
+      it 'should set the X-XSS-Protection for XHTML' do
+    expect(get('/', {}, 'wants' => 'application/xhtml+xml').headers['X-XSS-Protection']).to eq('1; mode=block')
   end
     
-      deploy_rb = File.expand_path('../../templates/deploy.rb.erb', __FILE__)
-  stage_rb = File.expand_path('../../templates/stage.rb.erb', __FILE__)
-  capfile = File.expand_path('../../templates/Capfile', __FILE__)
-    
-    module RuboCop
-  module Cop
-    module Layout
-      # Checks for unnecessary additional spaces inside array percent literals
-      # (i.e. %i/%w).
-      #
-      # @example
-      #
-      #   # bad
-      #   %w(foo  bar  baz)
-      #   # good
-      #   %i(foo bar baz)
-      class SpaceInsideArrayPercentLiteral < Cop
-        include MatchRange
-        include PercentLiteral
-    
-            def on_case(case_node)
-          case_node.when_branches.each_with_object([]) do |when_node, previous|
-            when_node.each_condition do |condition|
-              next unless repeated_condition?(previous, condition)
-    
-            def variables_in_simple_node(node)
-          simple_double_comparison?(node) do |var1, var2|
-            return [variable_name(var1), variable_name(var2)]
-          end
-          simple_comparison?(node) do |var|
-            return [variable_name(var)]
-          end
-          []
-        end
-    
-    # This is the base class for logstash codecs.
-module LogStash::Codecs; class Base < LogStash::Plugin
-  include LogStash::Config::Mixin
+    Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
