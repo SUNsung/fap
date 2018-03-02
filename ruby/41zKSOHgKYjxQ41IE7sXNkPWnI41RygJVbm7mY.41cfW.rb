12a147649700ceeeb9246b00a4f367ef470d1e36
SUNsung <weatherius@gmail.com>
2018-03-03 01:05:24 +0200

    Rewrite [41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb] File in: 2018-03-03 01:05:23

--- ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
+++ ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
@@ -1,88 +1,93 @@
 
         
-              # Topic may be hard deleted due to spam, no point complaining
-      # we would have to look at the topics table id sequence to find cases
-      # where this was called with an invalid id, no point really
-      return unless topic.present?
-    
-      private
-    
-      def retry_queued
-    @jobs = Delayed::Job.awaiting_retry.update_all(run_at: Time.zone.now)
-    
-    respond_to do |format|
-      format.html { redirect_to jobs_path, notice: 'Queued jobs getting retried.' }
-      format.json { head :no_content }
+            if test_conf['database']&.present?
+      ActiveRecord::Base.establish_connection(:test)
+      yield
+      ActiveRecord::Base.establish_connection(Rails.env.to_sym)
     end
   end
     
-        # replace in the top-level selector
-    # replace_in_selector('a {a: {a: a} } a {}', /a/, 'b') => 'b {a: {a: a} } b {}'
-    def replace_in_selector(css, pattern, sub)
-      # scan for selector positions in css
-      s        = CharStringScanner.new(css)
-      prev_pos = 0
-      sel_pos  = []
-      while (brace = s.scan_next(RULE_OPEN_BRACE_RE))
-        pos = s.pos
-        sel_pos << (prev_pos .. pos - 1)
-        s.pos    = close_brace_pos(css, s.pos - 1) + 1
-        prev_pos = pos
+      def virtual_tags
+    object.mentions + object.tags + object.emojis
+  end
+    
+      describe 'PUT #update' do
+    it 'updates notifications settings' do
+      user.settings['notification_emails'] = user.settings['notification_emails'].merge('follow' => false)
+      user.settings['interactions'] = user.settings['interactions'].merge('must_be_follower' => true)
+    
+          put :update, params: {
+        user: {
+          setting_boost_modal: '1',
+          setting_delete_modal: '0',
+        }
+      }
+    
+      describe 'environment' do
+    it 'adds the fuse directories to the appropriate paths' do
+      expect(ENV).to receive(:append_path).with('PKG_CONFIG_PATH', any_args)
+      expect(ENV).to receive(:append_path).with('HOMEBREW_LIBRARY_PATHS', any_args)
+      expect(ENV).to receive(:append_path).with('HOMEBREW_INCLUDE_PATHS', any_args)
+      subject.modify_build_environment
+    end
+  end
+end
+    
+          attr_accessor(*VALID_KEYS)
+      attr_accessor :pairs
+    
+          # A string representation of the importer.
+      # Should be overridden by subclasses.
+      #
+      # This is used to help debugging,
+      # and should usually just show the load path encapsulated by this importer.
+      #
+      # @return [String]
+      def to_s
+        Sass::Util.abstract(self)
       end
-      replace_substrings_at(css, sel_pos) { |s| s.gsub(pattern, sub) }
+    
+            def log_level?(level, min_level)
+          log_levels[level] >= log_levels[min_level]
+        end
+    
+            def initialize
+          @values = []
+        end
+    
+        # @abstract
+    #
+    # Update the clone on the deployment target
+    #
+    # @return void
+    #
+    def update
+      raise NotImplementedError, 'Your SCM strategy module should provide a #update method'
     end
     
-      # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+        yield
     
-    desc 'Start a dummy (test) Rails app server'
-task :dummy_rails do
-  require 'rack'
-  require 'term/ansicolor'
-  port = ENV['PORT'] || 9292
-  puts %Q(Starting on #{Term::ANSIColor.cyan 'http://localhost:#{port}'})
-  Rack::Server.start(
-    config: 'test/dummy_rails/config.ru',
-    Port: port)
-end
+        private
     
-            def_node_matcher :redundant_regex?, <<-PATTERN
-          {(send $!nil? {:match :=~} (regexp (str $#literal_at_start?) (regopt)))
-           (send (regexp (str $#literal_at_start?) (regopt)) {:match :=~} $_)}
-        PATTERN
+        # Returns an extension based on the content type. e.g. 'jpeg' for
+    # 'image/jpeg'. If the style has a specified format, it will override the
+    # content-type detection.
+    #
+    # Each mime type generally has multiple extensions associated with it, so
+    # if the extension from the original filename is one of these extensions,
+    # that extension is used, otherwise, the first in the list is used.
+    def content_type_extension attachment, style_name
+      mime_type = MIME::Types[attachment.content_type]
+      extensions_for_mime_type = unless mime_type.empty?
+        mime_type.first.extensions
+      else
+        []
+      end
     
-    module RuboCop
-  module Cop
-    module Lint
-      # Don't omit the accumulator when calling `next` in a `reduce` block.
-      #
-      # @example
-      #
-      #   # bad
-      #
-      #   result = (1..4).reduce(0) do |acc, i|
-      #     next if i.odd?
-      #     acc + i
-      #   end
-      #
-      # @example
-      #
-      #   # good
-      #
-      #   result = (1..4).reduce(0) do |acc, i|
-      #     next acc if i.odd?
-      #     acc + i
-      #   end
-      class NextWithoutAccumulator < Cop
-        MSG = 'Use `next` with an accumulator argument in a `reduce`.'.freeze
-    
-            def each_misplaced_optional_arg(arguments)
-          optarg_positions, arg_positions = argument_positions(arguments)
-          return if optarg_positions.empty? || arg_positions.empty?
-    
-          expect('.border-width-false-third').to have_ruleset(ruleset)
-      expect('.border-width-false-third').to_not have_rule(bad_rule)
+    module Paperclip
+  class << self
+    attr_writer :registered_attachments_styles_path
+    def registered_attachments_styles_path
+      @registered_attachments_styles_path ||= Rails.root.join('public/system/paperclip_attachments.yml').to_s
     end
   end
-end
