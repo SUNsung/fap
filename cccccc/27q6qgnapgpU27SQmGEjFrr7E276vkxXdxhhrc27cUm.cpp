54724cd6cf966d5d12b5d17416f0e26e3a434006
SUNsung <weatherius@gmail.com>
2018-03-03 18:15:55 +0200

    Rewrite [27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp] File in: 2018-03-03 18:15:55

--- cccccc/27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp
+++ cccccc/27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp
@@ -1,126 +1,280 @@
-#endif  // ATOM_BROWSER_API_ATOM_API_DEBUGGER_H_
+
+        
+          // Constructs a Message from a C-string.
+  explicit Message(const char* str) : ss_(new ::std::stringstream) {
+    *ss_ << str;
+  }
+    
+    namespace internal {
+    }
+    
+    
+    {  return AssertionFailure() << pred_text << '('
+                            << e1 << ', '
+                            << e2 << ') evaluates to false, where'
+                            << '\n' << e1 << ' evaluates to ' << v1
+                            << '\n' << e2 << ' evaluates to ' << v2;
+}
+    
+      // Returns true if pathname describes an absolute path.
+  bool IsAbsolutePath() const;
+    
+      const string test_case_name_;
+  TestInfoContainer tests_;
+  InstantiationContainer instantiations_;
+    
+    
+    {# endif  // GTEST_HAS_RTTI
+}
+    
+    #include          <string.h>
+#include          <ctype.h>
+#include          'params.h'
+#include          'float2int.h'
+#include          'tesseractclass.h'
+    
+    namespace tesseract {
+void Tesseract::tess_segment_pass_n(int pass_n, WERD_RES *word) {
+  int saved_enable_assoc = 0;
+  int saved_chop_enable = 0;
+    }
+    }
+    
+    namespace tesseract {
+    }
+    
+    // Update the other members if the cost is lower.
+void DPPoint::UpdateIfBetter(inT64 cost, inT32 steps, const DPPoint* prev,
+                             inT32 n, inT32 sig_x, inT64 sig_xsq) {
+  if (cost < total_cost_) {
+    total_cost_ = cost;
+    total_steps_ = steps;
+    best_prev_ = prev;
+    n_ = n;
+    sig_x_ = sig_x;
+    sig_xsq_ = sig_xsq;
+  }
+}
+    
+      // Calculate the scale factor we'll use to get to image y-pixels
+  double midx = (bbox.left() + bbox.right()) / 2.0;
+  double ydiff = (bbox.top() - bbox.bottom()) + 2.0;
+  FCOORD mid_bot(midx, bbox.bottom()), tmid_bot;
+  FCOORD mid_high(midx, bbox.bottom() + ydiff), tmid_high;
+  DenormTransform(NULL, mid_bot, &tmid_bot);
+  DenormTransform(NULL, mid_high, &tmid_high);
+    
+    #include          <stdio.h>
+#include          <math.h>
+#include          'elst.h'
+    
+    static int set_flags(struct ifaddrs* ifaddr) {
+	int fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (fd == -1) {
+		return -1;
+	}
+	ifreq ifr;
+	memset(&ifr, 0, sizeof(ifr));
+	strncpy(ifr.ifr_name, ifaddr->ifa_name, IFNAMSIZ - 1);
+	int rc = ioctl(fd, SIOCGIFFLAGS, &ifr);
+	close(fd);
+	if (rc == -1) {
+		return -1;
+	}
+	ifaddr->ifa_flags = ifr.ifr_flags;
+	return 0;
+}
+    
+    # define OC_STATIC_ILOG0(_v) (!!(_v))
+# define OC_STATIC_ILOG1(_v) (((_v)&0x2)?2:OC_STATIC_ILOG0(_v))
+# define OC_STATIC_ILOG2(_v) \
+ (((_v)&0xC)?2+OC_STATIC_ILOG1((_v)>>2):OC_STATIC_ILOG1(_v))
+# define OC_STATIC_ILOG3(_v) \
+ (((_v)&0xF0)?4+OC_STATIC_ILOG2((_v)>>4):OC_STATIC_ILOG2(_v))
+# define OC_STATIC_ILOG4(_v) \
+ (((_v)&0xFF00)?8+OC_STATIC_ILOG3((_v)>>8):OC_STATIC_ILOG3(_v))
+# define OC_STATIC_ILOG5(_v) \
+ (((_v)&0xFFFF0000)?16+OC_STATIC_ILOG4((_v)>>16):OC_STATIC_ILOG4(_v))
+# define OC_STATIC_ILOG6(_v) \
+ (((_v)&0xFFFFFFFF00000000ULL)?32+OC_STATIC_ILOG5((_v)>>32):OC_STATIC_ILOG5(_v))
+/**
+ * OC_STATIC_ILOG_32 - The integer logarithm of an (unsigned, 32-bit) constant.
+ * @_v: A non-negative 32-bit constant.
+ * Returns floor(log2(_v))+1, or 0 if _v==0.
+ * This is the number of bits that would be required to represent _v in two's
+ *  complement notation with all of the leading zeros stripped.
+ * This macro is suitable for evaluation at compile time, but it should not be
+ *  used on values that can change at runtime, as it operates via exhaustive
+ *  search.
+ */
+# define OC_STATIC_ILOG_32(_v) (OC_STATIC_ILOG5((ogg_uint32_t)(_v)))
+/**
+ * OC_STATIC_ILOG_64 - The integer logarithm of an (unsigned, 64-bit) constant.
+ * @_v: A non-negative 64-bit constant.
+ * Returns floor(log2(_v))+1, or 0 if _v==0.
+ * This is the number of bits that would be required to represent _v in two's
+ *  complement notation with all of the leading zeros stripped.
+ * This macro is suitable for evaluation at compile time, but it should not be
+ *  used on values that can change at runtime, as it operates via exhaustive
+ *  search.
+ */
+# define OC_STATIC_ILOG_64(_v) (OC_STATIC_ILOG6((ogg_int64_t)(_v)))
+    
+    static const static_codebook*const _floor_128x4_books[]={
+  &_huff_book_line_128x4_class0,
+  &_huff_book_line_128x4_0sub0,
+  &_huff_book_line_128x4_0sub1,
+  &_huff_book_line_128x4_0sub2,
+  &_huff_book_line_128x4_0sub3,
+};
+static const static_codebook*const _floor_256x4_books[]={
+  &_huff_book_line_256x4_class0,
+  &_huff_book_line_256x4_0sub0,
+  &_huff_book_line_256x4_0sub1,
+  &_huff_book_line_256x4_0sub2,
+  &_huff_book_line_256x4_0sub3,
+};
+static const static_codebook*const _floor_128x7_books[]={
+  &_huff_book_line_128x7_class0,
+  &_huff_book_line_128x7_class1,
+    }
+    
+        {&_44p3_p3_0,&_44p3_p3_1,0},
+    {&_44p3_p4_0,&_44p3_p4_1,0},
+    {&_44p3_p5_0,&_44p3_p5_1,0},
+    
+    #define opus_fft_free_arch(_st, arch) \
+   ((void)(arch), opus_fft_free_arm_neon(_st))
+    
+      /// Cancel all asynchronous operations associated with the acceptor.
+  /**
+   * This function causes all outstanding asynchronous connect, send and receive
+   * operations to finish immediately, and the handlers for cancelled operations
+   * will be passed the boost::asio::error::operation_aborted error.
+   *
+   * @param ec Set to indicate what error occurred, if any.
+   */
+  boost::system::error_code cancel(boost::system::error_code& ec)
+  {
+    return this->get_service().cancel(this->get_implementation(), ec);
+  }
+    
+        // Push the key/value pair on to the stack.
+    context(Key* k, Value& v)
+      : key_(k),
+        value_(&v),
+        next_(call_stack<Key, Value>::top_)
+    {
+      call_stack<Key, Value>::top_ = this;
+    }
+    
+        BOOST_ASIO_HANDLER_COMPLETION((o));
+    
+        // Check if we need to run the operation again.
+    if (ec == boost::asio::error::would_block
+        || ec == boost::asio::error::try_again)
+      return false;
+    
+    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+    
+      // Put the result back to the database
+  db_->Put(put_option_, key, it.WriteResult());
+    
+     private: // Private Functions
+  /// Calls InsertBefore or InsertAfter
+  int Insert(const std::string& key, const std::string& pivot,
+             const std::string& value, bool insert_after);
+ private:
+  std::string db_name_;       // The actual database name/path
+  WriteOptions put_option_;
+  ReadOptions get_option_;
+    
+    /**
+ * @brief get file modification time
+ * @details [long description]
+ *
+ * @param fname [description]
+ * @param file_mtime [description]
+ *
+ * @return [description]
+ */
+Status EnvLibrados::GetFileModificationTime(const std::string& fname,
+    uint64_t* file_mtime)
+{
+  LOG_DEBUG('[IN]%s\n', fname.c_str());
+  std::string fid, dir, file;
+  split(fname, &dir, &file);
+  time_t mtime;
+  uint64_t file_size;
+  Status s = Status::OK();
+  do {
+    std::string fpath = dir + '/' + file;
+    s = _GetFid(dir + '/' + file, fid);
+    }
+    }
+    
+      virtual void SeekToFirst() override {
+    ClearHeaps();
+    for (auto& child : children_) {
+      child.SeekToFirst();
+      if (child.Valid()) {
+        minHeap_.push(&child);
+      }
+    }
+    direction_ = kForward;
+    current_ = CurrentForward();
+  }
+    
+    #endif  // GRPC_INTERNAL_COMPILER_GO_GENERATOR_H
 
     
-    #include 'atom/browser/api/atom_api_web_contents.h'
-#include 'atom/browser/atom_browser_client.h'
-#include 'atom/common/native_mate_converters/value_converter.h'
-#include 'content/public/browser/render_process_host.h'
-#include 'native_mate/dictionary.h'
-#include 'native_mate/object_template_builder.h'
-    
-    void Event::SetSenderAndMessage(content::WebContents* sender,
-                                IPC::Message* message) {
-  DCHECK(!sender_);
-  DCHECK(!message_);
-  sender_ = sender;
-  message_ = message;
+    namespace grpc_java_generator {
+struct Parameters {
+  //        //Defines the custom parameter types for methods
+  //        //eg: flatbuffers uses flatbuffers.Builder as input for the client
+  //        and output for the server grpc::string custom_method_io_type;
+    }
     }
     
-      // content::WebContentsObserver implementations:
-  void WebContentsDestroyed() override;
+    // An abstract interface representing a method.
+struct Method : public CommentHolder {
+  virtual ~Method() {}
+    }
     
-    #include 'atom/browser/api/event.h'
-#include 'native_mate/arguments.h'
-#include 'native_mate/dictionary.h'
-#include 'native_mate/object_template_builder.h'
-#include 'ui/events/event_constants.h'
+    
+    {  return 0;
+}
+
+    
+      std::string GetNameSpace(const Definition &def) const;
     
      private:
-  void Destroy(content::DownloadItem* item);
+  void ParseFile(flatbuffers::Parser &parser, const std::string &filename,
+                 const std::string &contents,
+                 std::vector<const char *> &include_directories) const;
     
-      // Fill request details on IO thread.
-  std::unique_ptr<base::DictionaryValue> request_details(
-      new base::DictionaryValue);
-  FillRequestDetails(request_details.get(), request_);
+    // Raw helper functions used below: get any value in memory as a 64bit int, a
+// double or a string.
+// All scalars get static_cast to an int64_t, strings use strtoull, every other
+// data type returns 0.
+int64_t GetAnyValueI(reflection::BaseType type, const uint8_t *data);
+// All scalars static cast to double, strings use strtod, every other data
+// type is 0.0.
+double GetAnyValueF(reflection::BaseType type, const uint8_t *data);
+// All scalars converted using stringstream, strings as-is, and all other
+// data types provide some level of debug-pretty-printing.
+std::string GetAnyValueS(reflection::BaseType type, const uint8_t *data,
+                         const reflection::Schema *schema, int type_index);
     
-     protected:
-  ~LoginHandler() override;
-    
-    void cvReleaseStereoBMState( CvStereoBMState** state )
+    void AbstractBtMessage::setBtMessageDispatcher(BtMessageDispatcher* dispatcher)
 {
-    if( !state )
-        CV_Error( CV_StsNullPtr, '' );
-    }
+  dispatcher_ = dispatcher;
+}
     
-        cost__ = 9999;
+      void setBtMessageFactory(BtMessageFactory* factory);
     
+      virtual bool getChangeOption() const CXX11_OVERRIDE;
     
-    {} // namespace
-    
-            // Version: 2.1
-        PIXEL_PACK_BUFFER                = 0x88EB,
-        PIXEL_UNPACK_BUFFER              = 0x88EC,
-        PIXEL_PACK_BUFFER_BINDING        = 0x88ED,
-        PIXEL_UNPACK_BUFFER_BINDING      = 0x88EF,
-        FLOAT_MAT2x3                     = 0x8B65,
-        FLOAT_MAT2x4                     = 0x8B66,
-        FLOAT_MAT3x2                     = 0x8B67,
-        FLOAT_MAT3x4                     = 0x8B68,
-        FLOAT_MAT4x2                     = 0x8B69,
-        FLOAT_MAT4x3                     = 0x8B6A,
-        SRGB                             = 0x8C40,
-        SRGB8                            = 0x8C41,
-        SRGB_ALPHA                       = 0x8C42,
-        SRGB8_ALPHA8                     = 0x8C43,
-        COMPRESSED_SRGB                  = 0x8C48,
-        COMPRESSED_SRGB_ALPHA            = 0x8C49,
-    
-    /* Creates a new stream object that uses |engine| and |callback|. All stream
- * tasks are performed asynchronously on the |engine| network thread. |callback|
- * methods are invoked synchronously on the |engine| network thread, but must
- * not run tasks on the current thread to prevent blocking networking operations
- * and causing exceptions during shutdown. The |annotation| is stored in
- * bidirectional stream for arbitrary use by application.
- *
- * Returned |bidirectional_stream*| is owned by the caller, and must be
- * destroyed using |bidirectional_stream_destroy|.
- *
- * Both |calback| and |engine| must remain valid until stream is destroyed.
- */
-GRPC_SUPPORT_EXPORT
-bidirectional_stream* bidirectional_stream_create(
-    stream_engine* engine,
-    void* annotation,
-    bidirectional_stream_callback* callback);
-    
-      if (server_try_cancel_thd != nullptr) {
-    server_try_cancel_thd->join();
-    delete server_try_cancel_thd;
-    return Status::CANCELLED;
-  }
-    
-    #include <grpc/support/log.h>
-    
-    class QpsWorker {
- public:
-  explicit QpsWorker(int driver_port, int server_port,
-                     const grpc::string& credential_type);
-  ~QpsWorker();
-    }
-    
-      ClientConfig client_config;
-  client_config.set_client_type(SYNC_CLIENT);
-  client_config.set_outstanding_rpcs_per_channel(1);
-  client_config.set_client_channels(1);
-  client_config.set_rpc_type(UNARY);
-  client_config.mutable_load_params()->mutable_closed_loop();
-    
-    using grpc::Server;
-using grpc::ServerAsyncResponseWriter;
-using grpc::ServerBuilder;
-using grpc::ServerContext;
-using grpc::ServerCompletionQueue;
-using grpc::Status;
-using helloworld::HelloRequest;
-using helloworld::HelloReply;
-using helloworld::Greeter;
-    
-    
-    {  void ReadLong(long* l) {
-    size_t start = current_;
-    while (current_ != db_.size() && db_[current_] != ',' && db_[current_] != '}') {
-      current_++;
-    }
-    // It will throw an exception if fails.
-    *l = std::stol(db_.substr(start, current_ - start));
-  }
+      void setPeerStorage(const std::shared_ptr<PeerStorage>& peerStorage);
