29261856dfddeca0aa75f88754d26db77549ec9f
SUNsung <weatherius@gmail.com>
2018-03-03 07:09:08 +0200

    Rewrite [39SBMq24Fd4rw39buLQ54lHMqw394ZgHwZE7YVw39B7p.cpp] File in: 2018-03-03 07:09:01

--- cccccc/39SBMq24Fd4rw39buLQ54lHMqw394ZgHwZE7YVw39B7p.cpp
+++ cccccc/39SBMq24Fd4rw39buLQ54lHMqw394ZgHwZE7YVw39B7p.cpp
@@ -1,279 +1,237 @@
-const int kCIFARSize = 32;
-const int kCIFARImageNBytes = 3072;
-const int kCIFARBatchSize = 10000;
-const int kCIFARTrainBatches = 5;
-    
-    void convert_dataset(const char* image_filename, const char* label_filename,
-        const char* db_path, const string& db_backend) {
-  // Open files
-  std::ifstream image_file(image_filename, std::ios::in | std::ios::binary);
-  std::ifstream label_file(label_filename, std::ios::in | std::ios::binary);
-  CHECK(image_file) << 'Unable to open file ' << image_filename;
-  CHECK(label_file) << 'Unable to open file ' << label_filename;
-  // Read the magic and the meta data
-  uint32_t magic;
-  uint32_t num_items;
-  uint32_t num_labels;
-  uint32_t rows;
-  uint32_t cols;
-    }
-    
-    
-    {  static string LayerTypeListString() {
-    vector<string> layer_types = LayerTypeList();
-    string layer_types_str;
-    for (vector<string>::iterator iter = layer_types.begin();
-         iter != layer_types.end(); ++iter) {
-      if (iter != layer_types.begin()) {
-        layer_types_str += ', ';
-      }
-      layer_types_str += *iter;
-    }
-    return layer_types_str;
+
+        
+        // Create a pure JavaScript Event object.
+v8::Local<v8::Object> CreateEventObject(v8::Isolate* isolate) {
+  if (event_template.IsEmpty()) {
+    event_template.Reset(isolate, ObjectTemplateBuilder(isolate)
+        .SetMethod('preventDefault', &PreventDefault)
+        .Build());
   }
-};
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    #ifdef USE_CUDNN
-template <typename Dtype>
-class CuDNNLRNLayer : public LRNLayer<Dtype> {
- public:
-  explicit CuDNNLRNLayer(const LayerParameter& param)
-      : LRNLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNLRNLayer();
     }
     
     
-    {  bool handles_setup_;
-  cudnnHandle_t             handle_;
-  cudnnTensorDescriptor_t bottom_desc_, top_desc_;
-  cudnnPoolingDescriptor_t  pooling_desc_;
-  cudnnPoolingMode_t        mode_;
-};
+    {  BrowserThread::PostTask(
+      BrowserThread::UI, FROM_HERE,
+      base::Bind(&Browser::RequestLogin,
+                 base::Unretained(Browser::Get()),
+                 base::RetainedRef(make_scoped_refptr(this)),
+                 base::Passed(&request_details)));
+}
+    
+    #include 'net/url_request/url_request_job_factory.h'
+    
+    #if defined(OS_POSIX)
+// The 'magic' file descriptor that the relauncher process' write side of the
+// pipe shows up on. Chosen to avoid conflicting with stdin, stdout, and
+// stderr.
+extern const int kRelauncherSyncFD;
 #endif
     
-                // emulate all the nodes, find nodes that have state
-            m_netStatefulNodes = EnumerateStatefulNode(*net, criterionNodes, evaluationNodes);
-            for (auto x : m_netStatefulNodes)
-            {
-                wstring name = x.first;
-                m_netStates[name] = vector<shared_ptr<INodeState>>();
-            }
-    
-    #pragma warning(disable : 4267) // conversion from 'size_t' to 'unsigned int'; happens in CUDA <<<a,b>>> syntax if a and b are size_t
-#pragma warning(disable : 4127) // conditional expression is constant; 'if (sizeof(ElemType)==sizeof(float))' triggers this
-#pragma warning(disable : 4702) // unreachable code; triggered for unknown reasons
-    
-    using namespace std;
-using namespace Microsoft::MSR::ScriptableObjects;
-    
-    // ---------------------------------------------------------------------------
-// Expression -- the entire config is a tree of Expression types
-// We don't use polymorphism here because C++ is so verbose...
-// ---------------------------------------------------------------------------
-    
-    
-    {
-    {
-    {            std::vector<std::string> filePathVec = msra::strfun::split(filePaths, '+');
-            for (auto filePath : filePathVec)
-            {
-                // if file hasn't already been resolved (the resolvedPaths vector doesn't contain it), resolve it.
-                if (std::find(resolvedConfigFiles.begin(), resolvedConfigFiles.end(), filePath) == resolvedConfigFiles.end())
-                {
-                    // Recursively resolve the include statements in the included config files.
-                    // Ensure that the same config file isn't included twice, by keeping track of the config
-                    // files that have already been resolved in the resolvedPaths vector.
-                    resolvedConfigFiles.push_back(filePath);
-                    newConfigString += ResolveIncludeStatements(ReadConfigFile(filePath), resolvedConfigFiles);
-                }
-                else
-                {
-                    // We already resolved this path.  Write a warning so that user is aware of this.
-                    // TODO: This message is written to stderr before stderr gets redirected to the specified file.  Fix this.
-                    fprintf(stderr, 'Warning: Config file included multiple times.  Not including config file again: %s', filePath.c_str());
-                }
-            }
-        }
-        else
-        {
-            newConfigString += (line + '\n');
-        }
-    }
-    return newConfigString;
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9, typename T10,
+    typename T11, typename T12, typename T13, typename T14, typename T15,
+    typename T16, typename T17, typename T18, typename T19, typename T20,
+    typename T21, typename T22, typename T23, typename T24, typename T25,
+    typename T26, typename T27, typename T28, typename T29, typename T30,
+    typename T31, typename T32, typename T33, typename T34, typename T35>
+internal::ValueArray35<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
+    T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
+    T29, T30, T31, T32, T33, T34, T35> Values(T1 v1, T2 v2, T3 v3, T4 v4,
+    T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12, T13 v13,
+    T14 v14, T15 v15, T16 v16, T17 v17, T18 v18, T19 v19, T20 v20, T21 v21,
+    T22 v22, T23 v23, T24 v24, T25 v25, T26 v26, T27 v27, T28 v28, T29 v29,
+    T30 v30, T31 v31, T32 v32, T33 v33, T34 v34, T35 v35) {
+  return internal::ValueArray35<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
+      T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25,
+      T26, T27, T28, T29, T30, T31, T32, T33, T34, T35>(v1, v2, v3, v4, v5, v6,
+      v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21,
+      v22, v23, v24, v25, v26, v27, v28, v29, v30, v31, v32, v33, v34, v35);
 }
     
-    // GetWriter - get a reader type from the DLL
-// The F version gets the 'float' version, and D gets 'double'.
-extern 'C' DATAWRITER_API void GetWriterF(IDataWriter** pwriter);
-extern 'C' DATAWRITER_API void GetWriterD(IDataWriter** pwriter);
+    // A set of macros for testing Google Test assertions or code that's expected
+// to generate Google Test fatal failures.  It verifies that the given
+// statement will cause exactly one fatal Google Test failure with 'substr'
+// being part of the failure message.
+//
+// There are two different versions of this macro. EXPECT_FATAL_FAILURE only
+// affects and considers failures generated in the current thread and
+// EXPECT_FATAL_FAILURE_ON_ALL_THREADS does the same but for all threads.
+//
+// The verification of the assertion is done correctly even when the statement
+// throws an exception or aborts the current function.
+//
+// Known restrictions:
+//   - 'statement' cannot reference local non-static variables or
+//     non-static members of the current object.
+//   - 'statement' cannot return a value.
+//   - You cannot stream a failure message to this macro.
+//
+// Note that even though the implementations of the following two
+// macros are much alike, we cannot refactor them to use a common
+// helper macro, due to some peculiarity in how the preprocessor
+// works.  The AcceptsMacroThatExpandsToUnprotectedComma test in
+// gtest_unittest.cc will fail to compile if we do that.
+#define EXPECT_FATAL_FAILURE(statement, substr) \
+  do { \
+    class GTestExpectFatalFailureHelper {\
+     public:\
+      static void Execute() { statement; }\
+    };\
+    ::testing::TestPartResultArray gtest_failures;\
+    ::testing::internal::SingleFailureChecker gtest_checker(\
+        &gtest_failures, ::testing::TestPartResult::kFatalFailure, (substr));\
+    {\
+      ::testing::ScopedFakeTestPartResultReporter gtest_reporter(\
+          ::testing::ScopedFakeTestPartResultReporter:: \
+          INTERCEPT_ONLY_CURRENT_THREAD, &gtest_failures);\
+      GTestExpectFatalFailureHelper::Execute();\
+    }\
+  } while (::testing::internal::AlwaysFalse())
+    
+    #ifndef GTEST_INCLUDE_GTEST_GTEST_TEST_PART_H_
+#define GTEST_INCLUDE_GTEST_GTEST_TEST_PART_H_
+    
+    // Gets the content of the stringstream's buffer as an std::string.  Each '\0'
+// character in the buffer is replaced with '\\0'.
+GTEST_API_ std::string StringStreamToString(::std::stringstream* stream);
+    
+    // This provides interface PrimeTable that determines whether a number is a
+// prime and determines a next prime number. This interface is used
+// in Google Test samples demonstrating use of parameterized tests.
     
     
+    {    // also update node groups
+    for (auto nodeTag : newNodeTags)
     {
-    {        return newNumItersSinceLastPrintOfProgress;
+        AddToNodeGroup(nodeTag, newNode);
     }
-};
-    
-        // other goodies I came across (intrin.h):
-    //  - _mm_prefetch
-    //  - _mm_stream_ps --store without polluting cache
-    //  - unknown: _mm_addsub_ps, _mm_hsub_ps, _mm_movehdup_ps, _mm_moveldup_ps, _mm_blend_ps, _mm_blendv_ps, _mm_insert_ps, _mm_extract_ps, _mm_round_ps
-    //  - _mm_dp_ps dot product! http://msdn.microsoft.com/en-us/library/bb514054.aspx
-    //    Not so interesting for long vectors, we get better numerical precision with parallel adds and hadd at the end
-    
-    static const int kDelayMicros = 100000;
-static const int kReadOnlyFileLimit = 4;
-static const int kMMapLimit = 4;
-    
-    TEST(LogTest, ReadFourthMiddleBlock) {
-  CheckInitialOffsetRecord(log::kBlockSize + 1, 3);
 }
     
-    // Snapshots are kept in a doubly-linked list in the DB.
-// Each SnapshotImpl corresponds to a particular sequence number.
-class SnapshotImpl : public Snapshot {
- public:
-  SequenceNumber number_;  // const after creation
-    }
+        // mBdense.AssignElementPowerOf(mAdense, 3.2f);
+    // mBsparse.AssignElementPowerOf(mAsparse, 3.2f);
     
-    namespace leveldb {
-    }
+    template void FindConfigNames<ConfigParameters>(const ConfigParameters&, std::string key, std::vector<std::wstring>& names);
+template void FindConfigNames<ScriptableObjects::IConfigRecord>(const ScriptableObjects::IConfigRecord&, std::string key, std::vector<std::wstring>& names);
     
-    Status VersionEdit::DecodeFrom(const Slice& src) {
-  Clear();
-  Slice input = src;
-  const char* msg = NULL;
-  uint32_t tag;
-    }
+    #endif // __cocos2dx_csloader_h__
+
     
     
-  // Iterator interface methods
-  bool Valid() const        { return valid_; }
-  Slice key() const         { assert(Valid()); return key_; }
-  Slice value() const       { assert(Valid()); return iter_->value(); }
-  // Methods below require iter() != NULL
-  Status status() const     { assert(iter_); return iter_->status(); }
-  void Next()               { assert(iter_); iter_->Next();        Update(); }
-  void Prev()               { assert(iter_); iter_->Prev();        Update(); }
-  void Seek(const Slice& k) { assert(iter_); iter_->Seek(k);       Update(); }
-  void SeekToFirst()        { assert(iter_); iter_->SeekToFirst(); Update(); }
-  void SeekToLast()         { assert(iter_); iter_->SeekToLast();  Update(); }
     
-        return 0;
-}
-int lua_cocos2dx_physics_PhysicsJointRatchet_getPhase(lua_State* tolua_S)
+    
+    
+    #if COCOS2D_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,'#ferror in function 'lua_cocos2dx_physics_PhysicsShapeEdgePolygon_getPointsCount'.',&tolua_err);
+#endif
+    
+    /// Test settings. Some can be controlled in the GUI.
+struct Settings
 {
-    int argc = 0;
-    cocos2d::PhysicsJointRatchet* cobj = nullptr;
-    bool ok  = true;
-    
-    		extern int32 b2_gjkCalls, b2_gjkIters, b2_gjkMaxIters;
-		extern int32 b2_toiCalls, b2_toiIters;
-		extern int32 b2_toiRootIters, b2_toiMaxRootIters;
-    
-    			b2FixtureDef fd;
-			fd.shape = &shape;
-			fd.density = 20.0f;
-    
-    		// Car
-		{
-			b2PolygonShape chassis;
-			b2Vec2 vertices[8];
-			vertices[0].Set(-1.5f, -0.5f);
-			vertices[1].Set(1.5f, -0.5f);
-			vertices[2].Set(1.5f, 0.0f);
-			vertices[3].Set(0.0f, 0.9f);
-			vertices[4].Set(-1.15f, 0.9f);
-			vertices[5].Set(-1.5f, 0.2f);
-			chassis.Set(vertices, 6);
+	Settings()
+	{
+		viewCenter.Set(0.0f, 20.0f);
+		hz = 60.0f;
+		velocityIterations = 8;
+		positionIterations = 3;
+		drawShapes = 1;
+		drawJoints = 1;
+		drawAABBs = 0;
+		drawContactPoints = 0;
+		drawContactNormals = 0;
+		drawContactImpulse = 0;
+		drawFrictionImpulse = 0;
+		drawCOMs = 0;
+		drawStats = 0;
+		drawProfile = 0;
+		enableWarmStarting = 1;
+		enableContinuous = 1;
+		enableSubStepping = 0;
+		enableSleep = 1;
+		pause = 0;
+		singleStep = 0;
+	}
     }
     
-    #define EXPECT_MARKER() \
-  if (pos + 2 > len || data[pos] != 0xff) {                             \
-    fprintf(stderr, 'Marker byte (0xff) expected, found: %d '           \
-            'pos=%d len=%d\n',                                          \
-            (pos < len ? data[pos] : 0), static_cast<int>(pos),         \
-            static_cast<int>(len));                                     \
-    jpg->error = JPEG_MARKER_BYTE_NOT_FOUND;                            \
-    return false;                                                       \
-  }
+    #ifndef DUMPCRASHSTACK_H_
+#define DUMPCRASHSTACK_H_
     
-      // Returns an aggregate distance or similarity value between the baseline
-  // image and the image in the last Compare() call (or the baseline image, if
-  // Compare() was not called yet).
-  // The interpretation of this aggregate value depends on the comparator used.
-  virtual float distmap_aggregate() const = 0;
+    #endif
+
     
-    std::vector<float> Sharpen(const std::vector<float>& image, int w, int h,
-                           float sigma, float amount) {
-  // This is only made for small sigma, e.g. 1.3.
-  std::vector<double> kernel(5);
-  for (size_t i = 0; i < kernel.size(); i++) {
-    kernel[i] = Normal(1.0 * i - kernel.size() / 2, sigma);
-  }
-    }
-    
-    namespace guetzli {
-    }
-    
-    const double* NewSrgb8ToLinearTable() {
-  double* table = new double[256];
-  int i = 0;
-  for (; i < 11; ++i) {
-    table[i] = i / 12.92;
-  }
-  for (; i < 256; ++i) {
-    table[i] = 255.0 * std::pow(((i / 255.0) + 0.055) / 1.055, 2.4);
-  }
-  return table;
+    Test_Spy_Sample::~Test_Spy_Sample()
+{
+    SPY_DETACH_CLASS();
 }
     
-      tmp0 = in[6 * stride];
-  tmp1 = kIDCTMatrix[ 6] * tmp0;
-  tmp2 = kIDCTMatrix[14] * tmp0;
-  out[0] += tmp1;
-  out[1] += tmp2;
-  out[2] -= tmp2;
-  out[3] -= tmp1;
-  out[4] -= tmp1;
-  out[5] -= tmp2;
-  out[6] += tmp2;
-  out[7] += tmp1;
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
     
-    int BuildJpegHuffmanTable(const int* count_in, const int* symbols,
-                          HuffmanTableEntry* lut) {
-  HuffmanTableEntry code;    // current table entry
-  HuffmanTableEntry* table;  // next available space in table
-  int len;         // current code length
-  int idx;         // symbol index
-  int key;         // prefix code
-  int reps;        // number of replicate key values in current table
-  int low;         // low bits for current root entry
-  int table_bits;  // key length of current table
-  int table_size;  // size of current table
-  int total_size;  // sum of root table size and 2nd level table sizes
+    JNIEnv* ScopeJEnv::GetEnv() {
+    return env_;
+}
+    
+    namespace grpc_cpp_generator {
     }
     
-    // Preprocesses the u (1) or v (2) channel of the given YUV image (range 0-255).
-std::vector<std::vector<float>> PreProcessChannel(
-    int w, int h, int channel, float sigma, float amount, bool blur,
-    bool sharpen, const std::vector<std::vector<float>>& image);
     
-    
-    {} // namespace aria2
-    
-    private:
-  std::string userDefinedUser_;
-  std::string userDefinedPassword_;
-    
-    AdaptiveFileAllocationIterator::~AdaptiveFileAllocationIterator() = default;
-    
-    namespace aria2 {
+    {    auto stream = stub_->SayManyHellos(&context, request_msg);
+    while (stream->Read(&response_msg)) {
+      const HelloReply *response = response_msg.GetRoot();
+      callback(response->message()->str());
     }
+    auto status = stream->Finish();
+    if (!status.ok()) {
+      std::cerr << status.error_code() << ': ' << status.error_message()
+                << std::endl;
+      callback('RPC failed');
+    }
+  }
+    
+    ::grpc::ClientAsyncResponseReader< flatbuffers::grpc::Message<Stat>>* MonsterStorage::Stub::AsyncStoreRaw(::grpc::ClientContext* context, const flatbuffers::grpc::Message<Monster>& request, ::grpc::CompletionQueue* cq) {
+  return ::grpc::internal::ClientAsyncResponseReaderFactory< flatbuffers::grpc::Message<Stat>>::Create(channel_.get(), cq, rpcmethod_Store_, context, request, true);
+}
+    
+    
+    {    GenerateFn generate;
+    const char *generator_opt_short;
+    const char *generator_opt_long;
+    const char *lang_name;
+    bool schema_only;
+    GenerateFn generateGRPC;
+    flatbuffers::IDLOptions::Language lang;
+    const char *generator_help;
+    MakeRuleFn make_rule;
+  };
+    
+    static void GenNameSpace(const Namespace &name_space, std::string *_schema,
+                         const Namespace **last_namespace) {
+  if (*last_namespace == &name_space) return;
+  *last_namespace = &name_space;
+  auto &schema = *_schema;
+  schema += 'namespace ';
+  for (auto it = name_space.components.begin();
+       it != name_space.components.end(); ++it) {
+    if (it != name_space.components.begin()) schema += '.';
+    schema += *it;
+  }
+  schema += ';\n\n';
+}
+    
+    bool GenerateGo(const Parser &parser, const std::string &path,
+                const std::string &file_name) {
+  go::GoGenerator generator(parser, path, file_name, parser.opts.go_namespace);
+  return generator.generate();
+}
+    
+    template<class T> std::string GenFullName(const T *enum_def) {
+  std::string full_name;
+  const auto &name_spaces = enum_def->defined_namespace->components;
+  for (auto ns = name_spaces.cbegin(); ns != name_spaces.cend(); ++ns) {
+    full_name.append(*ns + '_');
+  }
+  full_name.append(enum_def->name);
+  return full_name;
+}
