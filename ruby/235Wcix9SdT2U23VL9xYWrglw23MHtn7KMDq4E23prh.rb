69ad97b99d213647f7a4944e9e5702b8c7fcb464
SUNsung <weatherius@gmail.com>
2018-03-03 13:33:42 +0200

    Rewrite [235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb] File in: 2018-03-03 13:33:41

--- ruby/235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb
+++ ruby/235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb
@@ -1,40 +1,187 @@
 
         
-        puts 'Deduping #{links.size} links...'
+              command = ['git describe']
+      command << '--tags' if match_lightweight
+      command << '`git rev-list --tags#{tag_pattern_param} --max-count=1`'
+      Actions.sh(command.compact.join(' '), log: false).chomp
+    rescue
+      nil
+    end
     
-    With optional '-t <bundle-id>', silently test if a given app
-is running, exiting with an error code if not.
-    
-            # Removes the specified cache
-        #
-        # @param [Array<Hash>] cache_descriptors
-        #        An array of caches to remove, each specified with the same
-        #        hash as cache_descriptors_per_pod especially :spec_file and :slug
-        #
-        def remove_caches(cache_descriptors)
-          cache_descriptors.each do |desc|
-            UI.message('Removing spec #{desc[:spec_file]} (v#{desc[:version]})') do
-              FileUtils.rm(desc[:spec_file])
-            end
-            UI.message('Removing cache #{desc[:slug]}') do
-              FileUtils.rm_rf(desc[:slug])
-            end
+            command = [escaped_gradle_path, 'tasks', '--console=plain'].join(' ')
+        output = Action.sh(command, print_command: false, print_command_output: false)
+        output.split('\n').each do |line|
+          if (result = line.match(/(\w+)\s\-\s([\w\s]+)/))
+            self.tasks << GradleTask.new(title: result[1], description: result[2])
           end
         end
     
-          # @param  [[Xcodeproj::PBXTarget]] targets
-      #         An array which always has a target as its first item
-      #         and may optionally contain a second target as its test target
-      #
-      # @return [String] the text for the target module
-      #
-      def template_contents(path, prefix, fallback)
-        if path.exist?
-          path.read.chomp.lines.map { |line| '#{prefix}#{line}' }.join('\n')
-        else
-          '#{prefix}# #{fallback}'
-        end
+            expect(result).to eq('git rev-parse --short HEAD')
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('git rev-parse --short HEAD')
       end
     end
+    
+          it 'automatically removes new lines from the version number' do
+        Fastlane::FastFile.new.parse('lane :test do
+          increment_version_number(version_number: '1.77.3\n', bump_type: 'major')
+        end').runner.execute(:test)
+    
+            # With reconfirmable, notify the original email when the user first
+        # requests the email change, instead of when the change is confirmed.
+        def send_email_changed_notification?
+          if self.class.reconfirmable
+            self.class.send_email_changed_notification && reconfirmation_required?
+          else
+            super
+          end
+        end
+    
+      test 'should require reconfirmation after creating a record and updating the email' do
+    admin = create_admin
+    assert !admin.instance_variable_get(:@bypass_confirmation_postpone)
+    admin.email = 'new_test@email.com'
+    admin.save
+    assert admin.pending_reconfirmation?
+  end
+    
+    group :test do
+  gem 'omniauth-facebook'
+  gem 'omniauth-openid'
+  gem 'webrat', '0.7.3', require: false
+  gem 'mocha', '~> 1.1', require: false
+end
+    
+    module Devise
+  module Controllers
+    # Create url helpers to be used with resource/scope configuration. Acts as
+    # proxies to the generated routes created by devise.
+    # Resource param can be a string or symbol, a class, or an instance object.
+    # Example using a :user resource:
+    #
+    #   new_session_path(:user)      => new_user_session_path
+    #   session_path(:user)          => user_session_path
+    #   destroy_session_path(:user)  => destroy_user_session_path
+    #
+    #   new_password_path(:user)     => new_user_password_path
+    #   password_path(:user)         => user_password_path
+    #   edit_password_path(:user)    => edit_user_password_path
+    #
+    #   new_confirmation_path(:user) => new_user_confirmation_path
+    #   confirmation_path(:user)     => user_confirmation_path
+    #
+    # Those helpers are included by default to ActionController::Base.
+    #
+    # In case you want to add such helpers to another class, you can do
+    # that as long as this new class includes both url_helpers and
+    # mounted_helpers. Example:
+    #
+    #     include Rails.application.routes.url_helpers
+    #     include Rails.application.routes.mounted_helpers
+    #
+    module UrlHelpers
+      def self.remove_helpers!
+        self.instance_methods.map(&:to_s).grep(/_(url|path)$/).each do |method|
+          remove_method method
+        end
+      end
+    
+      def test_each_byte_extended_file
+    [nil, {:textmode=>true}, {:binmode=>true}].each do |mode|
+      Tempfile.create('test-extended-file', mode) {|f|
+        assert_nil(f.getc)
+        f.print 'a'
+        f.rewind
+        result = []
+        f.each_byte {|b| result << b.chr }
+        assert_equal([?a], result, 'mode = <#{mode}>')
+      }
+    end
+  end
+    
+    describe :sorted_set_select_bang, shared: true do
+  before :each do
+    @set = SortedSet['one', 'two', 'three']
+  end
+    
+      def test_with_object
+    obj = [0, 1]
+    ret = (1..10).each.with_object(obj) {|i, memo|
+      memo[0] += i
+      memo[1] *= i
+    }
+    assert_same(obj, ret)
+    assert_equal([55, 3628800], ret)
+    
+      ruby_version_is ''...'2.4' do
+    it 'is locale insensitive (only upcases a-z and only downcases A-Z)' do
+      'ÄÖÜ'.swapcase.should == 'ÄÖÜ'
+      'ärger'.swapcase.should == 'äRGER'
+      'BÄR'.swapcase.should == 'bÄr'
+    end
+  end
+    
+      it 'understands 'a+bi' to mean a complex number with 'a' as the real part, 'b' as the imaginary' do
+    '79+4i'.to_c.should == Complex(79,4)
+  end
+    
+      it 'returns a Fixnum for long strings with trailing spaces' do
+    '0                             '.to_i.should == 0
+    '0                             '.to_i.should be_an_instance_of(Fixnum)
+    
+    desc 'Dumps output to a CSS file for testing'
+task :debug do
+  require 'sass'
+  path = Bootstrap.stylesheets_path
+  %w(bootstrap).each do |file|
+    engine = Sass::Engine.for_file('#{path}/#{file}.scss', syntax: :scss, load_paths: [path])
+    File.open('./#{file}.css', 'w') { |f| f.write(engine.render) }
   end
 end
+    
+    @@ login
+<form action='/'>
+  <label for='user'>User Name:</label>
+  <input name='user' value='' />
+  <input type='submit' value='GO!' />
+</form>
+    
+          def escape(object)
+        case object
+        when Hash   then escape_hash(object)
+        when Array  then object.map { |o| escape(o) }
+        when String then escape_string(object)
+        when Tempfile then object
+        else nil
+        end
+      end
+    
+      describe '#referrer' do
+    it 'Reads referrer from Referer header' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
+      expect(subject.referrer(env)).to eq('bar.com')
+    end
+    
+        it 'denies requests with duplicate session cookies' do
+      get '/', {}, 'HTTP_COOKIE' => 'rack.session=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
+      expect(last_response).not_to be_ok
+    end
+    
+      # Many RSpec users commonly either run the entire suite or an individual
+  # file, and it's useful to allow more verbose output when running an
+  # individual spec file.
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured
+    # (e.g. via a command-line flag).
+    config.default_formatter = 'doc'
+  end
+    
+    # The project root directory
+$root = ::File.dirname(__FILE__)
+    
+    Liquid::Template.register_tag('img', Jekyll::ImageTag)
+
+    
+          expect('.all-text-inputs').to have_ruleset(ruleset)
+    end
+  end
