ba0068f5707a6ca80cefc58ebfcf75ec2ae2eccc
SUNsung <weatherius@gmail.com>
2018-03-04 11:14:29 +0200

    Rewrite [40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp] File in: 2018-03-04 11:14:29

--- cccccc/40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp
+++ cccccc/40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp
@@ -1,244 +1,224 @@
 
         
-        // Build a Table file from the contents of *iter.  The generated file
-// will be named according to meta->number.  On success, the rest of
-// *meta will be filled with metadata about the generated table.
-// If no data is present in *iter, meta->file_size will be set to
-// zero, and no Table file will be produced.
-extern Status BuildTable(const std::string& dbname,
-                         Env* env,
-                         const Options& options,
-                         TableCache* table_cache,
-                         Iterator* iter,
-                         FileMetaData* meta);
+        # define INSTANTIATE_TEST_CASE_P(prefix, test_case_name, generator) \
+  ::testing::internal::ParamGenerator<test_case_name::ParamType> \
+      gtest_##prefix##test_case_name##_EvalGenerator_() { return generator; } \
+  int gtest_##prefix##test_case_name##_dummy_ = \
+      ::testing::UnitTest::GetInstance()->parameterized_test_registry(). \
+          GetTestCasePatternHolder<test_case_name>(\
+              #test_case_name, __FILE__, __LINE__)->AddTestCaseInstantiation(\
+                  #prefix, \
+                  &gtest_##prefix##test_case_name##_EvalGenerator_, \
+                  __FILE__, __LINE__)
     
-    void leveldb_options_set_paranoid_checks(
-    leveldb_options_t* opt, unsigned char v) {
-  opt->rep.paranoid_checks = v;
-}
-    
-    
-// Owned filenames have the form:
-//    dbname/CURRENT
-//    dbname/LOCK
-//    dbname/LOG
-//    dbname/LOG.old
-//    dbname/MANIFEST-[0-9]+
-//    dbname/[0-9]+.(log|sst|ldb)
-bool ParseFileName(const std::string& fname,
-                   uint64_t* number,
-                   FileType* type) {
-  Slice rest(fname);
-  if (rest == 'CURRENT') {
-    *number = 0;
-    *type = kCurrentFile;
-  } else if (rest == 'LOCK') {
-    *number = 0;
-    *type = kDBLockFile;
-  } else if (rest == 'LOG' || rest == 'LOG.old') {
-    *number = 0;
-    *type = kInfoLogFile;
-  } else if (rest.starts_with('MANIFEST-')) {
-    rest.remove_prefix(strlen('MANIFEST-'));
-    uint64_t num;
-    if (!ConsumeDecimalNumber(&rest, &num)) {
-      return false;
-    }
-    if (!rest.empty()) {
-      return false;
-    }
-    *type = kDescriptorFile;
-    *number = num;
-  } else {
-    // Avoid strtoull() to keep filename format independent of the
-    // current locale
-    uint64_t num;
-    if (!ConsumeDecimalNumber(&rest, &num)) {
-      return false;
-    }
-    Slice suffix = rest;
-    if (suffix == Slice('.log')) {
-      *type = kLogFile;
-    } else if (suffix == Slice('.sst') || suffix == Slice('.ldb')) {
-      *type = kTableFile;
-    } else if (suffix == Slice('.dbtmp')) {
-      *type = kTempFile;
-    } else {
-      return false;
-    }
-    *number = num;
-  }
-  return true;
-}
-    
-      fname = LogFileName('foo', 192);
-  ASSERT_EQ('foo/', std::string(fname.data(), 4));
-  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
-  ASSERT_EQ(192, number);
-  ASSERT_EQ(kLogFile, type);
-    
-      Cache::Handle* handle = NULL;
-  Status s = FindTable(file_number, file_size, &handle);
-  if (!s.ok()) {
-    return NewErrorIterator(s);
-  }
-    
-      std::string comparator_;
-  uint64_t log_number_;
-  uint64_t prev_log_number_;
-  uint64_t next_file_number_;
-  SequenceNumber last_sequence_;
-  bool has_comparator_;
-  bool has_log_number_;
-  bool has_prev_log_number_;
-  bool has_next_file_number_;
-  bool has_last_sequence_;
-    
-      void Write(bool sync, Order order, DBState state,
-             int num_entries, int value_size, int entries_per_batch) {
-    // Create new database if state == FRESH
-    if (state == FRESH) {
-      if (FLAGS_use_existing_db) {
-        message_ = 'skipping (--use_existing_db is true)';
-        return;
-      }
-      delete db_;
-      db_ = NULL;
-      Open(sync);
-      Start();  // Do not count time taken to destroy/open
-    }
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9, typename T10,
+    typename T11, typename T12>
+class ValueArray12 {
+ public:
+  ValueArray12(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9,
+      T10 v10, T11 v11, T12 v12) : v1_(v1), v2_(v2), v3_(v3), v4_(v4), v5_(v5),
+      v6_(v6), v7_(v7), v8_(v8), v9_(v9), v10_(v10), v11_(v11), v12_(v12) {}
     }
     
-    // This function takes tif/box pair of files and runs recognition on the image,
-// while making sure that the word bounds that tesseract identified roughly
-// match to those specified by the input box file. For each word (ngram in a
-// single bounding box from the input box file) it outputs the ocred result,
-// the correct label, rating and certainty.
-void Tesseract::recog_training_segmented(const STRING &fname,
-                                         PAGE_RES *page_res,
-                                         volatile ETEXT_DESC *monitor,
-                                         FILE *output_file) {
-  STRING box_fname = fname;
-  const char *lastdot = strrchr(box_fname.string(), '.');
-  if (lastdot != NULL) box_fname[lastdot - box_fname.string()] = '\0';
-  box_fname += '.box';
-  // ReadNextBox() will close box_file
-  FILE *box_file = open_file(box_fname.string(), 'r');
-    }
-    
-    #include 'mfoutline.h'
-#include 'tessbox.h'
-#include 'tesseractclass.h'
-    
-    // Returns true if there were enough points at the last call to Fit or
-// ConstrainedFit for the fitted points to be used on a badly fitted line.
-bool DetLineFit::SufficientPointsForIndependentFit() const {
-  return distances_.size() >= kMinPointsForErrorCount;
-}
-    
-      // Constrained fit with a supplied direction vector. Finds the best line_pt,
-  // that is one of the supplied points having the median cross product with
-  // direction, ignoring points that have a cross product outside of the range
-  // [min_dist, max_dist]. Returns the resulting error metric using the same
-  // reduced set of points.
-  // *Makes use of floating point arithmetic*
-  double ConstrainedFit(const FCOORD& direction,
-                        double min_dist, double max_dist,
-                        bool debug, ICOORD* line_pt);
-    
-    
-    {  if (total_weight > 0) {
-    error = sigyy + m * (m * sigxx + 2 * (c * sigx - sigxy)) + c *
-            (total_weight * c - 2 * sigy);
-    if (error >= 0)
-      error = sqrt(error / total_weight);  // sqrt of mean
-    else
-      error = 0;
-  } else {
-    error = 0;                   // too little
-  }
-  return error;
-}
-    
-      // Transforms the given coords one step forward to normalized space, without
-  // using any block rotation or predecessor.
-  void LocalNormTransform(const TPOINT& pt, TPOINT* transformed) const;
-  void LocalNormTransform(const FCOORD& pt, FCOORD* transformed) const;
-  // Transforms the given coords forward to normalized space using the
-  // full transformation sequence defined by the block rotation, the
-  // predecessors, deepest first, and finally this. If first_norm is not NULL,
-  // then the first and deepest transformation used is first_norm, ending
-  // with this, and the block rotation will not be applied.
-  void NormTransform(const DENORM* first_norm, const TPOINT& pt,
-                     TPOINT* transformed) const;
-  void NormTransform(const DENORM* first_norm, const FCOORD& pt,
-                     FCOORD* transformed) const;
-  // Transforms the given coords one step back to source space, without
-  // using to any block rotation or predecessor.
-  void LocalDenormTransform(const TPOINT& pt, TPOINT* original) const;
-  void LocalDenormTransform(const FCOORD& pt, FCOORD* original) const;
-  // Transforms the given coords all the way back to source image space using
-  // the full transformation sequence defined by this and its predecessors
-  // recursively, shallowest first, and finally any block re_rotation.
-  // If last_denorm is not NULL, then the last transformation used will
-  // be last_denorm, and the block re_rotation will never be executed.
-  void DenormTransform(const DENORM* last_denorm, const TPOINT& pt,
-                       TPOINT* original) const;
-  void DenormTransform(const DENORM* last_denorm, const FCOORD& pt,
-                       FCOORD* original) const;
-    
-    
-    { private:
-  BOOL8 proportional;          //< proportional
-  bool right_to_left_;         //< major script is right to left.
-  inT8 kerning;                //< inter blob gap
-  inT16 spacing;               //< inter word gap
-  inT16 pitch;                 //< pitch of non-props
-  inT16 font_class;            //< correct font class
-  inT32 xheight;               //< height of chars
-  float cell_over_xheight_;    //< Ratio of cell height to xheight.
-  STRING filename;             //< name of block
-  ROW_LIST rows;               //< rows in block
-  PARA_LIST paras_;            //< paragraphs of block
-  C_BLOB_LIST c_blobs;         //< before textord
-  C_BLOB_LIST rej_blobs;       //< duff stuff
-  FCOORD re_rotation_;         //< How to transform coords back to image.
-  FCOORD classify_rotation_;   //< Apply this before classifying.
-  FCOORD skew_;                //< Direction of true horizontal.
-  ICOORD median_size_;         //< Median size of blobs.
-};
+      // Trivial case 2: even numbers
+  if (n % 2 == 0) return n == 2;
     
     
     {
-    {#undef EXT_FUNC
-};
-} // namespace fuzzer
-    
-    namespace fuzzer {
-    }
-    
-    namespace fuzzer {
-    }
-    
-    // Decides which files need to be merged (add thost to NewFiles).
-// Returns the number of new features added.
-size_t Merger::Merge(std::vector<std::string> *NewFiles) {
-  NewFiles->clear();
-  assert(NumFilesInFirstCorpus <= Files.size());
-  std::set<uint32_t> AllFeatures;
-    }
-    
-    // Overwrites part of To[0,ToSize) with a part of From[0,FromSize).
-// Returns ToSize.
-size_t MutationDispatcher::CopyPartOf(const uint8_t *From, size_t FromSize,
-                                      uint8_t *To, size_t ToSize) {
-  // Copy From[FromBeg, FromBeg + CopySize) into To[ToBeg, ToBeg + CopySize).
-  size_t ToBeg = Rand(ToSize);
-  size_t CopySize = Rand(ToSize - ToBeg) + 1;
-  assert(ToBeg + CopySize <= ToSize);
-  CopySize = std::min(CopySize, FromSize);
-  size_t FromBeg = Rand(FromSize - CopySize + 1);
-  assert(FromBeg + CopySize <= FromSize);
-  memmove(To + ToBeg, From + FromBeg, CopySize);
-  return ToSize;
+    {
+    {  for (const auto& share_line : osquery::split(content, '\n')) {
+    genNFSShare(share_line, results);
+  }
+  return results;
 }
+}
+}
+
+    
+      /**
+   * @brief The privilege/permissions dropper deconstructor will restore
+   * effective permissions.
+   *
+   * There should only be a single drop of privilege/permission active.
+   */
+  virtual ~DropPrivileges();
+    
+      /// Get a list of the registry item names for a given registry.
+  std::vector<std::string> names(const std::string& registry_name) const;
+    
+        size_t length = 0;
+    void *e = NULL;
+    switch (*(int *)data) {
+    case 0:
+      e = osquery_cqueue_reserve(
+          &osquery.cqueue, OSQUERY_TEST_EVENT_0, sizeof(test_event_0_data_t));
+      length = 4096;
+      break;
+    case 1:
+      e = osquery_cqueue_reserve(
+          &osquery.cqueue, OSQUERY_TEST_EVENT_1, sizeof(test_event_1_data_t));
+      length = 33;
+      break;
+    default:
+      return -ENOTTY;
+    }
+    if (!e) {
+      return -EINVAL;
+    }
+    
+     private:
+  /*
+   * @brief A helper function to 'carve' files from disk
+   *
+   * This function performs a 'forensic carve' of a specified path to the
+   * users tmp directory
+   */
+  Status carve(const boost::filesystem::path& path);
+    
+        pt::ptree multi_pack;
+    for (const auto& path : paths) {
+      std::string content;
+      if (!readFile(path, content)) {
+        LOG(WARNING) << 'Cannot read multi-pack file: ' << path;
+        continue;
+      }
+    }
+    
+    #include <osquery/config.h>
+#include <osquery/dispatcher.h>
+    
+    void BuildDCHistograms(const JPEGData& jpg, JpegHistogram* histo) {
+  for (size_t i = 0; i < jpg.components.size(); ++i) {
+    const JPEGComponent& c = jpg.components[i];
+    JpegHistogram* dc_histogram = &histo[i];
+    coeff_t last_dc_coeff = 0;
+    for (int mcu_y = 0; mcu_y < jpg.MCU_rows; ++mcu_y) {
+      for (int mcu_x = 0; mcu_x < jpg.MCU_cols; ++mcu_x) {
+        for (int iy = 0; iy < c.v_samp_factor; ++iy) {
+          for (int ix = 0; ix < c.h_samp_factor; ++ix) {
+            int block_y = mcu_y * c.v_samp_factor + iy;
+            int block_x = mcu_x * c.h_samp_factor + ix;
+            int block_idx = block_y * c.width_in_blocks + block_x;
+            coeff_t dc_coeff = c.coeffs[block_idx << 6];
+            int diff = std::abs(dc_coeff - last_dc_coeff);
+            int nbits = Log2Floor(diff) + 1;
+            dc_histogram->Add(nbits);
+            last_dc_coeff = dc_coeff;
+          }
+        }
+      }
+    }
+  }
+}
+    
+        // The nodes are:
+    // [0, n): the sorted leaf nodes that we start with.
+    // [n]: we add a sentinel here.
+    // [n + 1, 2n): new parent nodes are added here, starting from
+    //              (n+1). These are naturally in ascending order.
+    // [2n]: we add a sentinel at the end as well.
+    // There will be (2n+1) elements at the end.
+    const HuffmanTree sentinel(~static_cast<uint32_t>(0), -1, -1);
+    tree[n] = sentinel;
+    tree[n + 1] = sentinel;
+    
+    // Mimic libjpeg's heuristics to guess jpeg color space.
+// Requires that the jpg has 3 components.
+bool HasYCbCrColorSpace(const JPEGData& jpg);
+    
+    #endif  // GUETZLI_JPEG_ERROR_H_
+
+    
+      // Make a local copy of the input bit length histogram.
+  int count[kJpegHuffmanMaxBitLength + 1] = { 0 };
+  int total_count = 0;
+  for (len = 1; len <= kJpegHuffmanMaxBitLength; ++len) {
+    count[len] = count_in[len];
+    total_count += count[len];
+  }
+    
+    // Builds jpeg-style Huffman lookup table from the given symbols.
+// The symbols are in order of increasing bit lengths. The number of symbols
+// with bit length n is given in counts[n] for each n >= 1.
+// Returns the size of the lookup table.
+int BuildJpegHuffmanTable(const int* counts, const int* symbols,
+                          HuffmanTableEntry* lut);
+    
+      int width() const { return width_; }
+  int height() const { return height_; }
+  int factor_x() const { return factor_x_; }
+  int factor_y() const { return factor_y_; }
+  int width_in_blocks() const { return width_in_blocks_; }
+  int height_in_blocks() const { return height_in_blocks_; }
+  const coeff_t* coeffs() const { return &coeffs_[0]; }
+  const int* quant() const { return &quant_[0]; }
+  bool IsAllZero() const;
+    
+    namespace facebook {
+namespace jni {
+    }
+    }
+    
+      /**
+   * The offset within the current function
+   */
+  int functionOffset() const noexcept {
+    auto absoluteSymbol = static_cast<const char*>(functionAddress_);
+    auto absoluteabsoluteProgramCounter =
+        static_cast<const char*>(absoluteProgramCounter_);
+    return absoluteabsoluteProgramCounter - absoluteSymbol;
+  }
+    
+      YGNodeCalculateLayout(root, 200, 100, YGDirectionLTR);
+    
+     public: // Setters
+    
+    namespace facebook {
+    }
+    
+    
+    {
+    {}}
+    
+    #pragma once
+#include <atomic>
+#include <fb/assert.h>
+#include <fb/noncopyable.h>
+#include <fb/nonmovable.h>
+#include <fb/RefPtr.h>
+    
+    #include <fb/visibility.h>
+    
+    
+    {
+    {        // Start the ASIO io_service run loop
+        echo_server.run();
+    } catch (websocketpp::exception const & e) {
+        std::cout << e.what() << std::endl;
+    } catch (...) {
+        std::cout << 'other exception' << std::endl;
+    }
+}
+
+    
+    #define CONNECTIONS_PER_ADDRESS 28000
+#define THREADS 10
+    
+    #endif
+    
+        sockaddr_storage addr;
+    socklen_t addrLength = sizeof(addr);
+    if (getpeername(fd, (sockaddr *) &addr, &addrLength) == -1) {
+        return {0, '', ''};
+    }
+    
+    
+    {    static const int CONSUME_POST_PADDING = 4;
+    static const int CONSUME_PRE_PADDING = LONG_MESSAGE_HEADER - 1;
+};
+    
+    struct Timer {
+    Loop *loop;
+    void *data;
+    }
