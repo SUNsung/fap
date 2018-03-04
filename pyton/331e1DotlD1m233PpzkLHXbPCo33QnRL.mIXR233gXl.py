df5a1585b8404f58d66118f5432cfc97e8853d84
SUNsung <weatherius@gmail.com>
2018-03-04 20:58:54 +0200

    Rewrite [331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py] File in: 2018-03-04 20:58:53

--- pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
+++ pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
@@ -1,117 +1,139 @@
 
-@keras_test
-def test_model_trainability_switch():
-    # a non-trainable model has no trainable weights
-    x = Input(shape=(1,))
-    y = Dense(2)(x)
-    model = Model(x, y)
-    model.trainable = False
-    assert model.trainable_weights == []
+        
+            if member.__doc__ is None and not member_too_small(member):
+        raise ValueError('{} class doesn't have any documentation'.format(name),
+                         member.__module__, inspect.getmodule(member).__file__)
+    for n, met in inspect.getmembers(member):
+        if inspect.ismethod(met):
+            handle_method(n, met)
     
-    if ngram_range > 1:
-    print('Adding {}-gram features'.format(ngram_range))
-    # Create set of unique n-gram from the training set.
-    ngram_set = set()
-    for input_list in x_train:
-        for i in range(2, ngram_range + 1):
-            set_of_ngram = create_ngram_set(input_list, ngram_value=i)
-            ngram_set.update(set_of_ngram)
+        history = model.fit(x, ys, validation_split=0.05, batch_size=10,
+                        verbose=0, epochs=3)
+    ground_truth = -np.log(0.5)
+    assert(np.abs(history.history['loss'][-1] - ground_truth) < 0.06)
     
-    from keras.models import Sequential
-from keras.engine.training import _weighted_masked_objective
-from keras.layers import TimeDistributed, Masking, Dense
-from keras.utils.test_utils import keras_test
-from keras import losses
-from keras import backend as K
+        assert np.all(model.predict(inputs) == output)
     
-        # prime the model with 'ab' sequence and let it generate the learned alphabet
-    sentence = alphabet[:sequence_length]
-    generated = sentence
-    for iteration in range(number_of_chars - sequence_length):
-        x = np.zeros((1, sequence_length, number_of_chars))
-        for t, char in enumerate(sentence):
-            x[0, t, ord(char) - ord('a')] = 1.
-        preds = model.predict(x, verbose=0)[0]
-        next_char = chr(np.argmax(preds) + ord('a'))
-        generated += next_char
-        sentence = sentence[1:] + next_char
+        @classmethod
+    def get_datasource(cls, datasource_type, datasource_id, session):
+        return (
+            session.query(cls.sources[datasource_type])
+            .filter_by(id=datasource_id)
+            .first()
+        )
     
-    
-def test_elu():
-    x = K.placeholder(ndim=2)
-    f = K.function([x], [activations.elu(x, 0.5)])
-    
-    
-@keras_test
-def test_vector_regression():
-    '''
-    Perform float data prediction (regression) using 2 layer MLP
-    with tanh and sigmoid activations.
-    '''
-    (x_train, y_train), (x_test, y_test) = get_test_data(num_train=500,
-                                                         num_test=200,
-                                                         input_shape=(20,),
-                                                         output_shape=(num_classes,),
-                                                         classification=False)
-    
-        model.summary()
-    json_str = model.to_json()
-    model_from_json(json_str)
-    
-            return contracts
-    
-        def test_from_bytes(self):
-        def check(tests, byteorder, signed=False):
-            for test, expected in tests.items():
-                try:
-                    self.assertEqual(
-                        int.from_bytes(test, byteorder, signed=signed),
-                        expected)
-                except Exception as err:
-                    raise AssertionError(
-                        'failed to convert {0} with byteorder={1!r} and signed={2}'
-                        .format(test, byteorder, signed)) from err
-    
-            if self.closed:
-            raise ValueError('I/O operation on closed file')
-        if self.seekable:
-            try:
-                n = self.chunksize - self.size_read
-                # maybe fix alignment
-                if self.align and (self.chunksize & 1):
-                    n = n + 1
-                self.file.seek(n, 1)
-                self.size_read = self.size_read + n
-                return
-            except OSError:
-                pass
-        while self.size_read < self.chunksize:
-            n = min(8192, self.chunksize - self.size_read)
-            dummy = self.read(n)
-            if not dummy:
-                raise EOFError
+            logging.info('Importing %d %s',
+                     len(data.get(DRUID_CLUSTERS_KEY, [])),
+                     DRUID_CLUSTERS_KEY)
+        for datasource in data.get(DRUID_CLUSTERS_KEY, []):
+            DruidCluster.import_from_dict(session, datasource, sync=sync)
+        session.commit()
+    else:
+        logging.info('Supplied object is not a dictionary.')
 
     
-        def scan_once(string, idx):
+        def __init__(self, database=None, query=None, table=None, **kwargs):
+        self.database = database
+        self.query = query
+        self.schema = None
+        if query and query.schema:
+            self.schema = query.schema
+        elif table:
+            self.schema = table.schema
+        self.context = {
+            'url_param': url_param,
+            'current_user_id': current_user_id,
+            'current_username': current_username,
+            'form_data': {},
+        }
+        self.context.update(kwargs)
+        self.context.update(BASE_CONTEXT)
+        if self.engine:
+            self.context[self.engine] = self
+        self.env = SandboxedEnvironment()
+    
+    
+def cast_filter_data(form_data):
+    '''Used by cast_form_data to parse the filters'''
+    flts = []
+    having_flts = []
+    fd = form_data
+    filter_pattern = re.compile(r'''((?:[^,'']|'[^']*'|'[^']*')+)''')
+    for i in range(0, 10):
+        for prefix in ['flt', 'having']:
+            col_str = '{}_col_{}'.format(prefix, i)
+            op_str = '{}_op_{}'.format(prefix, i)
+            val_str = '{}_eq_{}'.format(prefix, i)
+            if col_str in fd and op_str in fd and val_str in fd \
+               and len(fd[val_str]) > 0:
+                f = {}
+                f['col'] = fd[col_str]
+                f['op'] = fd[op_str]
+                if prefix == 'flt':
+                    # transfer old strings in filter value to list
+                    splitted = filter_pattern.split(fd[val_str])[1::2]
+                    values = [types.replace(''', '').strip() for types in splitted]
+                    f['val'] = values
+                    flts.append(f)
+                if prefix == 'having':
+                    f['val'] = fd[val_str]
+                    having_flts.append(f)
+            if col_str in fd:
+                del fd[col_str]
+            if op_str in fd:
+                del fd[op_str]
+            if val_str in fd:
+                del fd[val_str]
+    fd['filters'] = flts
+    fd['having_filters'] = having_flts
+    return fd
+    
+    
+def find_constraint_name(upgrade=True):
+    cols = {'column_name'} if upgrade else {'datasource_name'}
+    return generic_find_constraint_name(
+        table='columns', columns=cols, referenced='datasources', db=db)
+    
+        def __init__(self, parent, title, message, used_names,
+                 *, _htest=False, _utest=False):
+        super().__init__(parent, title, message, used_names=used_names,
+                         _htest=_htest, _utest=_utest)
+    
+        >>> mode(['red', 'blue', 'blue', 'red', 'green', 'red', 'red'])
+    'red'
+    
+            for proto in range(pickle.HIGHEST_PROTOCOL + 1):
+            s = pickle.dumps(d, proto)
+            e = pickle.loads(s)
+            self.assertNotEqual(id(d), id(e))
+            self.assertEqual(type(d), type(e))
+            self.assertEqual(list(d), list(e))
+    
+    def test():
+    for i in range(256):
+        c = chr(i)
+        s = repr(c)
+        e = evalString(s)
+        if e != c:
+            print(i, c, s, e)
+    
+        print('LIBS=', end=' ')
+    for lib in libs: print(''%s'' % (lib), end=' ')
+    print() ; print()
+    
+        def find_byname(self, name):
+        '''Return (red, green, blue) for name'''
+        name = name.lower()
         try:
-            return _scan_once(string, idx)
-        finally:
-            memo.clear()
+            return self.__byname[name]
+        except KeyError:
+            raise BadColor(name) from None
     
-        def _artcmd(self, line, file=None):
-        '''Internal: process a HEAD, BODY or ARTICLE command.'''
-        resp, lines = self._longcmd(line, file)
-        resp, art_num, message_id = self._statparse(resp)
-        return resp, ArticleInfo(art_num, message_id, lines)
-    
-        # override in derived classes
-    def _extractrgb(self, mo):
-        return [int(x) for x in mo.group('red', 'green', 'blue')]
-    
-        def test_iterator_pickling(self):
-        testcases = [(13,), (0, 11), (-22, 10), (20, 3, -1),
-                     (13, 21, 3), (-2, 2, 2), (2**65, 2**65+2)]
-        for proto in range(pickle.HIGHEST_PROTOCOL + 1):
-            for t in testcases:
-                it = itorg = iter(range(*t))
-                data = list(range(*t))
+            # Misc. other alternate tests
+        self.assertEqual(format((-1.5+0.5j), '#f'), '-1.500000+0.500000j')
+        self.assertEqual(format((-1.5+0.5j), '#.0f'), '-2.+0.j')
+        self.assertEqual(format((-1.5+0.5j), '#e'), '-1.500000e+00+5.000000e-01j')
+        self.assertEqual(format((-1.5+0.5j), '#.0e'), '-2.e+00+5.e-01j')
+        self.assertEqual(format((-1.5+0.5j), '#g'), '-1.50000+0.500000j')
+        self.assertEqual(format((-1.5+0.5j), '.0g'), '-2+0.5j')
+        self.assertEqual(format((-1.5+0.5j), '#.0g'), '-2.+0.5j')
