479ad6b84e14dfd8673e0257a235f80292388127
SUNsung <weatherius@gmail.com>
2018-03-04 17:39:13 +0200

    Rewrite [52S5RObwiVrkk52avGEptlTUIw52896Hbba4orU52s3B.rb] File in: 2018-03-04 17:39:12

--- ruby/52S5RObwiVrkk52avGEptlTUIw52896Hbba4orU52s3B.rb
+++ ruby/52S5RObwiVrkk52avGEptlTUIw52896Hbba4orU52s3B.rb
@@ -1,108 +1,115 @@
 
         
-              module ClassMethods
-        def tests(helper_class)
-          case helper_class
-          when String, Symbol
-            self.helper_class = '#{helper_class.to_s.underscore}_helper'.camelize.safe_constantize
-          when Module
-            self.helper_class = helper_class
-          end
-        end
-    
-          # Returns constant of subscription adapter specified in config/cable.yml.
-      # If the adapter cannot be found, this will default to the Redis adapter.
-      # Also makes sure proper dependencies are required.
-      def pubsub_adapter
-        adapter = (cable.fetch('adapter') { 'redis' })
-    
-    module ActiveRecord
-  module ConnectionAdapters
-    class ConnectionSpecification
-      class ResolverTest < ActiveRecord::TestCase
-        def resolve(spec, config = {})
-          Resolver.new(config).resolve(spec)
-        end
-    
-        def master_helper_methods
-      @controller_class._helpers.instance_methods
+              it 'renders HTML' do
+        capture = render(options, screenshots)
+        expect(capture).to match(/<html>/)
+        expect(capture).to include('<li>Some</li>')
+        expect(capture).to include('<li>key</li>')
+        expect(capture).to include('<li>words</li>')
+      end
     end
     
-    require 'abstract_unit'
+          scripts = target.build_phases.select { |t| t.class == Xcodeproj::Project::Object::PBXShellScriptBuildPhase }
+      crash_script = scripts.find { |s| includes_run_script?(s.shell_script) }
     
-    module ActionMailer
-  # The <tt>ActionMailer::DeliveryJob</tt> class is used when you
-  # want to send emails outside of the request-response cycle.
-  #
-  # Exceptions are rescued and handled by the mailer class.
-  class DeliveryJob < ActiveJob::Base # :nodoc:
-    queue_as { ActionMailer::Base.deliver_later_queue_name }
+      protected
     
-          indentation = ' ' * indent
-      sentences.map! { |sentence|
-        '#{indentation}#{sentence.join(' ')}'
-      }.join '\n'
+      before_action :upgrade_warning, only: :index
+    
+    module Vagrant
+  module Plugin
+    module V2
+      # This is the base class for a provider for the V2 API. A provider
+      # is responsible for creating compute resources to match the needs
+      # of a Vagrant-configured system.
+      class Provider
+        include CapabilityHost
+    
+          # Renders the template using the class intance as the binding. Because the
+      # renderer inherits from `OpenStruct`, additional view variables can be
+      # added like normal accessors.
+      #
+      # @return [String]
+      def render
+        old_template = template
+        result = nil
+        File.open(full_template_path, 'r') do |f|
+          self.template = f.read
+          result = render_string
+        end
+    
+        def log_processed(name)
+      puts green '    #{name}'
+    end
+    
+    desc 'Start a dummy (test) Rails app server'
+task :dummy_rails do
+  require 'rack'
+  require 'term/ansicolor'
+  port = ENV['PORT'] || 9292
+  puts %Q(Starting on #{Term::ANSIColor.cyan 'http://localhost:#{port}'})
+  Rack::Server.start(
+    config: 'test/dummy_rails/config.ru',
+    Port: port)
+end
+    
+                -- Return the time part and the sequence part. OR appears
+            -- faster here than addition, but they're equivalent:
+            -- time_part has no trailing two bytes, and tail is only
+            -- the last two bytes.
+            RETURN time_part | tail;
+          END
+        $$ LANGUAGE plpgsql VOLATILE;
+      SQL
+    end
+    
+        it 'leaves the domains status and attachements, but clears media' do
+      expect { bad_status1.reload }.not_to raise_error
+      expect { bad_status2.reload }.not_to raise_error
+      expect { bad_attachment.reload }.not_to raise_error
+      expect(bad_attachment.file.exists?).to be false
     end
   end
 end
 
     
-      class AssertMultipartSelectMailer < ActionMailer::Base
-    def test(options)
-      mail subject: 'Test e-mail', from: 'test@test.host', to: 'test <test@test.host>' do |format|
-        format.text { render plain: options[:text] }
-        format.html { render plain: options[:html] }
+            def listen
+          while repl_command = STDIN.gets
+            execute_repl_command(repl_command)
+          end
+        end
+    
+          def html?(headers)
+        return false unless header = headers.detect { |k,v| k.downcase == 'content-type' }
+        options[:html_types].include? header.last[/^\w+\/\w+/]
       end
     end
   end
-    
-          def remember_cookie_values(resource)
-        options = { httponly: true }
-        options.merge!(forget_cookie_values(resource))
-        options.merge!(
-          value: resource.class.serialize_into_cookie(resource),
-          expires: resource.remember_expires_at
-        )
-      end
-    
-          header_info.each do | var, value|
-        if request.respond_to?(:set_header)
-          request.set_header(var, value)
-        else
-          request.env[var]  = value
-        end
-      end
-    
-    module Metasploit
-  module Framework
-    class Application < Rails::Application
-      include Metasploit::Framework::CommonEngine
-    
-      def parse(pkt)
-    # We want to return immediantly if	we do not have a packet which is handled by us
-    return unless pkt.is_tcp?
-    return if (pkt.tcp_sport != 80 and pkt.tcp_dport != 80)
-    s = find_session((pkt.tcp_sport == 80) ? get_session_src(pkt) : get_session_dst(pkt))
-    
-    
-# extract label addresses
-addrs = {}
-dtrans.each_line { |ln|
-	if ln =~ /;[^ ].*:/
-		parts = ln.split(' ')
-		label = parts[1]
-		label = label.slice(1,label.index(':')-1)
-		addr = parts[0].split(':')[1].to_i(16)
-		#puts '%s => %x' % [label, addr]
-		one = { label => addr }
-		addrs.merge!(one)
-	end
-}
-#puts addrs.inspect
-    
-    p meterp.sys.config.sysinfo
-    
-      def get_result
-    result
-  end
 end
+
+    
+        it 'leaves TempFiles untouched' do
+      mock_app do |env|
+        request = Rack::Request.new(env)
+        [200, {'Content-Type' => 'text/plain'}, [request.params['file'][:filename] + '\n' + \
+                                                 request.params['file'][:tempfile].read + '\n' + \
+                                                 request.params['other']]]
+      end
+    
+    
+  it 'should allow changing the protection mode to a string' do
+    # I have no clue what other modes are available
+    mock_app do
+      use Rack::Protection::FrameOptions, :frame_options => 'ALLOW-FROM foo'
+      run DummyApp
+    end
+    
+      %w(GET HEAD POST PUT DELETE).each do |method|
+    it 'accepts #{method} requests when allow_if is true' do
+      mock_app do
+        use Rack::Protection::HttpOrigin, :allow_if => lambda{|env| env.has_key?('HTTP_ORIGIN') }
+        run DummyApp
+      end
+      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://any.domain.com')).to be_ok
+    end
+  end
