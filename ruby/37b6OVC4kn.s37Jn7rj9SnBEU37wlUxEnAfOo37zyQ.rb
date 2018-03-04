b9e03f24c1d67b3d27fb7eff2f9b66b1591afbf7
SUNsung <weatherius@gmail.com>
2018-03-05 01:49:00 +0200

    Rewrite [37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb] File in: 2018-03-05 01:48:59

--- ruby/37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb
+++ ruby/37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb
@@ -1,35 +1,77 @@
 
         
-            def terminate_thread!
-      if thread
-        thread.instance_eval { ActiveRecord::Base.connection_pool.release_connection }
-        thread.wakeup if thread.status == 'sleep'
-        thread.terminate
+              def restore_request_parameters
+        req = build_rack_request
+        req.update_param('client_id', session[:client_id])
+        req.update_param('redirect_uri', session[:redirect_uri])
+        req.update_param('response_type', response_type_as_space_seperated_values)
+        req.update_param('scope', session[:scopes])
+        req.update_param('state', session[:state])
+        req.update_param('nonce', session[:nonce])
+      end
+    
+          def create
+        req = Rack::Request.new(request.env)
+        if req['client_assertion_type'] == 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
+          handle_jwt_bearer(req)
+        end
+        self.status, headers, self.response_body = Api::OpenidConnect::TokenEndpoint.new.call(request.env)
+        headers.each {|name, value| response.headers[name] = value }
+        nil
+      end
+    
+          out =
+        Sass::Util.silence_sass_warnings do
+          if @options[:from] == :css
+            require 'sass/css'
+            Sass::CSS.new(read(input), @options[:for_tree]).render(@options[:to])
+          else
+            if input_path
+              Sass::Engine.for_file(input_path, @options[:for_engine])
+            else
+              Sass::Engine.new(read(input), @options[:for_engine])
+            end.to_tree.send('to_#{@options[:to]}', @options[:for_tree])
+          end
+        end
+    
+          # If this importer is based on files on the local filesystem This method
+      # should return true if the file, when changed, should trigger a
+      # recompile.
+      #
+      # It is acceptable for non-sass files to be watched and trigger a recompile.
+      #
+      # @param filename [String] The absolute filename for a file that has changed.
+      # @return [Boolean] When the file changed should cause a recompile.
+      def watched_file?(filename)
+        false
       end
     end
+  end
+end
+
     
-        valid_oauth_providers :tumblr
+      # Creates a delayed logger wrapping `inner`.
+  #
+  # @param inner [Sass::Logger::Base] The wrapped logger.
+  def initialize(inner)
+    self.log_level = inner.log_level
+    @inner = inner
+    @messages = []
   end
     
-        direction = direction.to_s == 'desc' ? 'desc' : 'asc'
-    
-      def retry_queued
-    @jobs = Delayed::Job.awaiting_retry.update_all(run_at: Time.zone.now)
-    
-    respond_to do |format|
-      format.html { redirect_to jobs_path, notice: 'Queued jobs getting retried.' }
-      format.json { head :no_content }
+            def define_logger(name, options = {})
+          class_eval <<-RUBY, __FILE__, __LINE__ + 1
+            def #{name}(message)
+              #{options.fetch(:to, :log)}(#{name.inspect}, message)
+            end
+          RUBY
+        end
+      end
     end
   end
+end
+
     
-      context 'called with three sizes' do
-    it 'applies second width to left and right' do
-      rule = 'padding: 4px 5px 6px'
-    
-      context 'called with three sizes' do
-    it 'applies second width to left and right' do
-      ruleset = 'position: relative; ' +
-                'top: 4px; ' +
-                'right: 5px; ' +
-                'bottom: 6px; ' +
-                'left: 5px;'
+      require 'sass/plugin/rack'
+  class Sass::Plugin::MerbBootLoader < Merb::BootLoader
+    after Merb::BootLoader::RackUpApplication
