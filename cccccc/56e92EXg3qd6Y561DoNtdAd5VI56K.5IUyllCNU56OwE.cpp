07ddd42e71f00787b7c59700968a6b2850c5f172
SUNsung <weatherius@gmail.com>
2018-03-06 00:36:37 +0200

    Rewrite [56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp] File in: 2018-03-06 00:36:36

--- cccccc/56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp
+++ cccccc/56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp
@@ -1,120 +1,418 @@
 
         
-        #if defined(_MSC_VER) && (_MSC_VER >= 1200)
+        int main(int argc, char** argv) {
+  return leveldb::test::RunAllTests();
+}
+
+    
+    #include 'leveldb/status.h'
+    
+    #ifndef STORAGE_LEVELDB_DB_DB_ITER_H_
+#define STORAGE_LEVELDB_DB_DB_ITER_H_
+    
+    
+    {}  // namespace leveldb
+
+    
+    std::string LockFileName(const std::string& dbname) {
+  return dbname + '/LOCK';
+}
+    
+    
+    {  input.remove_prefix(kHeader);
+  Slice key, value;
+  int found = 0;
+  while (!input.empty()) {
+    found++;
+    char tag = input[0];
+    input.remove_prefix(1);
+    switch (tag) {
+      case kTypeValue:
+        if (GetLengthPrefixedSlice(&input, &key) &&
+            GetLengthPrefixedSlice(&input, &value)) {
+          handler->Put(key, value);
+        } else {
+          return Status::Corruption('bad WriteBatch Put');
+        }
+        break;
+      case kTypeDeletion:
+        if (GetLengthPrefixedSlice(&input, &key)) {
+          handler->Delete(key);
+        } else {
+          return Status::Corruption('bad WriteBatch Delete');
+        }
+        break;
+      default:
+        return Status::Corruption('unknown WriteBatch tag');
+    }
+  }
+  if (found != WriteBatchInternal::Count(this)) {
+    return Status::Corruption('WriteBatch has wrong count');
+  } else {
+    return Status::OK();
+  }
+}
+    
+    
+    {}  // namespace leveldb
+    
+    #include <string>
+#include 'leveldb/env.h'
+#include 'leveldb/status.h'
+    
+    bool Value::isUInt() const {
+  switch (type_) {
+  case intValue:
+    return value_.int_ >= 0 && LargestUInt(value_.int_) <= LargestUInt(maxUInt);
+  case uintValue:
+    return value_.uint_ <= maxUInt;
+  case realValue:
+    return value_.real_ >= 0 && value_.real_ <= maxUInt &&
+           IsIntegral(value_.real_);
+  default:
+    break;
+  }
+  return false;
+}
+    
+    class Descriptor;
+class FileDescriptor;
+class EnumDescriptor;
+class OneofDescriptor;
+class ServiceDescriptor;
+    
+    // Find a file by file name.
+bool PyDescriptorDatabase::FindFileByName(const string& filename,
+                                          FileDescriptorProto* output) {
+  ScopedPyObjectPtr py_descriptor(PyObject_CallMethod(
+      py_database_, 'FindFileByName', 's#', filename.c_str(), filename.size()));
+  return GetFileDescriptorProto(py_descriptor.get(), output);
+}
+    
+    
+    { private:
+  // The python object that implements the database. The reference is owned.
+  PyObject* py_database_;
+};
+    
+    // Appends all the CMessages in the input iterator to the container.
+//
+// Returns None if successful; returns NULL and sets an exception if
+// unsuccessful.
+PyObject* Extend(RepeatedCompositeContainer* self, PyObject* value);
+    
+    
+    {  // Pointer to the parent's descriptor that describes this
+  // field.  Used together with the parent's message when making a
+  // default message instance mutable.
+  // The pointer is owned by the global DescriptorPool.
+  const FieldDescriptor* parent_field_descriptor;
+} RepeatedScalarContainer;
+    
+     private:
+  const FieldDescriptor* descriptor_;
+  Context* context_;
+  ClassNameResolver* name_resolver_;
+  string scope_;
+    
+    struct FBExtension : Extension {
+  FBExtension(): Extension('fb', '1.0.0') {}
+    }
+    
+    void HHVM_FUNCTION(fb_setprofile,
+  const Variant& callback,
+  int64_t flags = EventHook::ProfileDefault
+) {
+  if (ThreadInfo::s_threadInfo->m_profiler != nullptr) {
+    // phpprof is enabled, don't let PHP code override it
+    return;
+  }
+  g_context->m_setprofileCallback = callback;
+  g_context->m_setprofileFlags = flags;
+  if (callback.isNull()) {
+    HPHP::EventHook::Disable();
+  } else {
+    HPHP::EventHook::Enable();
+  }
+}
+    
+    #include 'hphp/runtime/base/execution-context.h'
+#include 'hphp/util/struct-log.h'
+#include 'hphp/util/timer.h'
+#include 'hphp/util/trace.h'
+    
+      /* convert */
+  if (convd != nullptr) {
+    vars.assignIfRef(php_mbfl_convert(vars, convd, &string, &result));
+    for (int n = 0; n < args.size(); n++) {
+      const_cast<Array&>(args).set(n, php_mbfl_convert(args[n], convd,
+                                                        &string, &result));
+    }
+    MBSTRG(illegalchars) += mbfl_buffer_illegalchars(convd);
+    mbfl_buffer_converter_delete(convd);
+  }
+    
+      if (!variantToGMPData('gmp_sub', gmpDataA, dataA)) {
+    return false;
+  }
+  if (!variantToGMPData('gmp_sub', gmpDataB, dataB)) {
+    mpz_clear(gmpDataA);
+    return false;
+  }
+    
+    #ifndef incl_HPHP_JIT_VASM_H_
+#define incl_HPHP_JIT_VASM_H_
+    
+      Server(const std::string &address, int port);
+    
+    public int
+magic_setflags(struct magic_set *ms, int flags)
+{
+  if (ms == NULL)
+    return -1;
+#if !defined(HAVE_UTIME) && !defined(HAVE_UTIMES)
+  if (flags & MAGIC_PRESERVE_ATIME)
+    return -1;
+#endif
+  ms->flags = flags;
+  return 0;
+}
+    
+    
+    {    // also update node groups
+    for (auto nodeTag : newNodeTags)
+    {
+        AddToNodeGroup(nodeTag, newNode);
+    }
+}
+    
+        bool haslattice(std::wstring key) const
+    {
+#ifdef NONUMLATTICEMMI
+        return denlattices.haslattice(key);
+#else
+        return numlattices.haslattice(key) && denlattices.haslattice(key);
+#endif
+    }
+    
+    template <class _T>
+class array_ref
+{
+    _T* data;
+    size_t n;
+    inline void check_index(size_t i) const
+    {
+        i;
+        assert(i < n);
+    }
+    inline void check_ptr() const
+    {
+        n;
+        data;
+        assert(n == 0 || data != NULL);
+    }
+    }
+    
+    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
 # pragma once
 #endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
     
-    template<class T, std::size_t N>
-class array;
+    namespace boost {
+namespace asio {
+namespace detail {
+    }
+    }
+    }
     
-      // Resize the buffer to the specified length.
-  void resize(size_type length)
+    namespace boost {
+namespace asio {
+namespace detail {
+    }
+    }
+    }
+    
+    
+    {  // The offset to the end of the unread data.
+  size_type end_offset_;
+  
+  // The data in the buffer.
+  std::vector<byte_type> buffer_;
+};
+    
+    namespace boost {
+namespace asio {
+namespace detail {
+    }
+    }
+    }
+    
+      descriptor_write_op(int descriptor,
+      const ConstBufferSequence& buffers, Handler& handler)
+    : descriptor_write_op_base<ConstBufferSequence>(
+        descriptor, buffers, &descriptor_write_op::do_complete),
+      handler_(BOOST_ASIO_MOVE_CAST(Handler)(handler))
   {
-    BOOST_ASIO_ASSERT(length <= capacity());
-    if (begin_offset_ + length <= capacity())
-    {
-      end_offset_ = begin_offset_ + length;
-    }
-    else
-    {
-      using namespace std; // For memmove.
-      memmove(&buffer_[0], &buffer_[0] + begin_offset_, size());
-      end_offset_ = length;
-      begin_offset_ = 0;
-    }
   }
     
     #if !defined(BOOST_ASIO_HAS_THREADS) \
   || defined(BOOST_ASIO_DISABLE_FENCED_BLOCK)
-# include <boost/asio/detail/null_fenced_block.hpp>
+typedef null_fenced_block fenced_block;
 #elif defined(__MACH__) && defined(__APPLE__)
-# include <boost/asio/detail/macos_fenced_block.hpp>
+typedef macos_fenced_block fenced_block;
 #elif defined(__sun)
-# include <boost/asio/detail/solaris_fenced_block.hpp>
+typedef solaris_fenced_block fenced_block;
 #elif defined(__GNUC__) && defined(__arm__) \
   && !defined(__GCC_HAVE_SYNC_COMPARE_AND_SWAP_4)
-# include <boost/asio/detail/gcc_arm_fenced_block.hpp>
+typedef gcc_arm_fenced_block fenced_block;
 #elif defined(__GNUC__) && (defined(__hppa) || defined(__hppa__))
-# include <boost/asio/detail/gcc_hppa_fenced_block.hpp>
+typedef gcc_hppa_fenced_block fenced_block;
 #elif defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
-# include <boost/asio/detail/gcc_x86_fenced_block.hpp>
+typedef gcc_x86_fenced_block fenced_block;
 #elif defined(__GNUC__) \
   && ((__GNUC__ == 4 && __GNUC_MINOR__ >= 1) || (__GNUC__ > 4)) \
   && !defined(__INTEL_COMPILER) && !defined(__ICL) \
   && !defined(__ICC) && !defined(__ECC) && !defined(__PATHSCALE__)
-# include <boost/asio/detail/gcc_sync_fenced_block.hpp>
+typedef gcc_sync_fenced_block fenced_block;
 #elif defined(BOOST_ASIO_WINDOWS) && !defined(UNDER_CE)
-# include <boost/asio/detail/win_fenced_block.hpp>
+typedef win_fenced_block fenced_block;
 #else
-# include <boost/asio/detail/null_fenced_block.hpp>
+typedef null_fenced_block fenced_block;
 #endif
     
-    #define BOOST_ASIO_ACCEPT_HANDLER_CHECK( \
-    handler_type, handler) \
-  \
-  typedef BOOST_ASIO_HANDLER_TYPE(handler_type, \
-      void(boost::system::error_code)) \
-    asio_true_handler_type; \
-  \
-  BOOST_ASIO_HANDLER_TYPE_REQUIREMENTS_ASSERT( \
-      sizeof(boost::asio::detail::one_arg_handler_test( \
-          boost::asio::detail::clvref< \
-            asio_true_handler_type>(), \
-          static_cast<const boost::system::error_code*>(0))) == 1, \
-      'AcceptHandler type requirements not met') \
-  \
-  typedef boost::asio::detail::handler_type_requirements< \
-      sizeof( \
-        boost::asio::detail::argbyv( \
-          boost::asio::detail::clvref< \
-            asio_true_handler_type>())) + \
-      sizeof( \
-        boost::asio::detail::lvref< \
-          asio_true_handler_type>()( \
-            boost::asio::detail::lvref<const boost::system::error_code>()), \
-        char(0))> BOOST_ASIO_UNUSED_TYPEDEF
+    #ifndef BOOST_ASIO_DETAIL_FUNCTION_HPP
+#define BOOST_ASIO_DETAIL_FUNCTION_HPP
     
-    template <typename Time_Traits>
-void dev_poll_reactor::add_timer_queue(timer_queue<Time_Traits>& queue)
-{
-  do_add_timer_queue(queue);
-}
-    
-    
-    {    void toJS(nbind::cbOutput expose) const
-    {
-        expose(unit, value);
-    }
-};
+    #endif // BOOST_ASIO_DETAIL_GCC_ARM_FENCED_BLOCK_HPP
 
     
-    
-    { private:
-  const InstructionPointer absoluteProgramCounter_;
-  const InstructionPointer libraryBase_;
-  const InstructionPointer functionAddress_;
-  const std::string libraryName_;
-  const std::string functionName_;
-};
-    
-    class YogaTest_HadOverflowTests : public Test {
-protected:
-  YogaTest_HadOverflowTests() {
-    config = YGConfigNew();
-    root = YGNodeNewWithConfig(config);
-    YGNodeStyleSetWidth(root, 200);
-    YGNodeStyleSetHeight(root, 100);
-    YGNodeStyleSetFlexDirection(root, YGFlexDirectionColumn);
-    YGNodeStyleSetFlexWrap(root, YGWrapNoWrap);
+      if (shutdown_)
+  {
+    io_service_.post_immediate_completion(op, false);
+    return;
   }
+    
+    void dev_poll_reactor::deregister_internal_descriptor(
+    socket_type descriptor, dev_poll_reactor::per_descriptor_data&)
+{
+  boost::asio::detail::mutex::scoped_lock lock(mutex_);
     }
     
-        double top;
-    double bottom;
     
-        method(setMeasureFunc);
-    method(unsetMeasureFunc);
+    {    stb__running_adler = stb_adler32(stb__running_adler, start, q - start);
+    return q - start;
+}
     
-    // This allows storing the assert message before the current process terminates due to a crash
-typedef void (*AssertHandler)(const char* message);
-void setAssertHandler(AssertHandler assertHandler);
+            ImGui::Checkbox('Demo Window', &hud->show_demo_window);      // Edit bools storing our windows open/close state
+        ImGui::Checkbox('Another Window', &hud->show_another_window);
+    
+    // Use if you want to reset your rendering device without losing ImGui state.
+IMGUI_API void        ImGui_ImplDX10_InvalidateDeviceObjects();
+IMGUI_API bool        ImGui_ImplDX10_CreateDeviceObjects();
+    
+        if (int size = s3eClipboardGetText(NULL, 0))
+    {
+        if (g_ClipboardText)
+            delete[] g_ClipboardText;
+        g_ClipboardText = new char[size];
+        g_ClipboardText[0] = '\0';
+        s3eClipboardGetText(g_ClipboardText, size);
+    }
+    
+    IMGUI_API bool        ImGui_ImplGlfwGL2_Init(GLFWwindow* window, bool install_callbacks);
+IMGUI_API void        ImGui_ImplGlfwGL2_Shutdown();
+IMGUI_API void        ImGui_ImplGlfwGL2_NewFrame();
+IMGUI_API void        ImGui_ImplGlfwGL2_RenderDrawData(ImDrawData* draw_data);
+    
+                if (ImGui::Button('Button'))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text('counter = %d', counter);
+    
+                ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our windows open/close state
+            ImGui::Checkbox('Another Window', &show_another_window);
+    
+        // Setup ImGui binding
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+    ImGui_ImplSdlGL2_Init(window);
+    
+        try {
+        // Set logging settings
+        echo_server.set_access_channels(websocketpp::log::alevel::none);
+        echo_server.clear_access_channels(websocketpp::log::alevel::frame_payload);
+    }
+    
+    
+    {    void push(Message *message)
+    {
+        if (tail) {
+            tail->nextMessage = message;
+            tail = message;
+        } else {
+            head = message;
+            tail = message;
+        }
+    }
+};
+    
+    
+    
+    template <bool isServer>
+std::string ExtensionsNegotiator<isServer>::generateOffer() {
+    std::string extensionsOffer;
+    if (options & Options::PERMESSAGE_DEFLATE) {
+        extensionsOffer += 'permessage-deflate';
+    }
+    }
+    
+    Socket::Address Socket::getAddress()
+{
+    uv_os_sock_t fd = getFd();
+    }
+    
+    
+    {    if (async) {
+        async->close();
+    }
+}
+    
+        void setNoDelay(int enable) {
+        setsockopt(getFd(), IPPROTO_TCP, TCP_NODELAY, &enable, sizeof(int));
+    }
+    
+    
+    {        if (!numPolls) {
+            closing.clear();
+            return;
+        }
+    }
+    closing.clear();
+    
+        void start(void (*cb)(Timer *), int timeout, int repeat) {
+        loop->timepoint = std::chrono::system_clock::now();
+        std::chrono::system_clock::time_point timepoint = loop->timepoint + std::chrono::milliseconds(timeout);
+    }
+    
+    public:
+    template <bool isServer>
+    Group<isServer> *createGroup(int extensionOptions = 0, unsigned int maxPayload = 16777216) {
+        return new Group<isServer>(extensionOptions, maxPayload, this, nodeData);
+    }
+    
+        // duplicated code for now!
+    template <class F>
+    void forEachHttpSocket(const F &cb) {
+        uS::Poll *iterator = httpSocketHead;
+        iterators.push(iterator);
+        while (iterator) {
+            uS::Poll *lastIterator = iterator;
+            cb((HttpSocket<isServer> *) iterator);
+            iterator = iterators.top();
+            if (lastIterator == iterator) {
+                iterator = ((uS::Socket *) iterator)->next;
+                iterators.top() = iterator;
+            }
+        }
+        iterators.pop();
+    }
