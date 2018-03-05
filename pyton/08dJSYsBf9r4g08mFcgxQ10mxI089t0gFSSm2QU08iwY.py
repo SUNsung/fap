60cfc1ab506da7ffb6ae82d208eaea90bd32678e
SUNsung <weatherius@gmail.com>
2018-03-05 07:04:03 +0200

    Rewrite [08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py] File in: 2018-03-05 07:04:03

--- pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
+++ pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
@@ -1,117 +1,91 @@
 
         
-                # Keyword arguments > stream.encoding > default utf8
-        if self.stdin_encoding is None:
-            self.stdin_encoding = getattr(
-                self.stdin, 'encoding', None) or 'utf8'
-        if self.stdout_encoding is None:
-            actual_stdout = self.stdout
-            if is_windows:
-                # noinspection PyUnresolvedReferences
-                from colorama import AnsiToWin32
-                if isinstance(self.stdout, AnsiToWin32):
-                    actual_stdout = self.stdout.wrapped
-            self.stdout_encoding = getattr(
-                actual_stdout, 'encoding', None) or 'utf8'
+                        fn = os.path.join(dirpath, basename)
+                with io.open(fn, encoding='utf-8') as inf:
+                    code = inf.read()
+    
+        return sorted(
+        filter(lambda ie: ie.is_suitable(age_limit), gen_extractors()),
+        key=lambda ie: ie.IE_NAME.lower())
+    
+    import re
+    
+    from .onet import OnetBaseIE
     
     
-def test_follow_all_redirects_shown(httpbin):
-    r = http('--follow', '--all', httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 3
-    assert r.count('HTTP/1.1 302 FOUND', 2)
-    assert HTTP_OK in r
+def check_entry(line_num, segments):
+    # START Title
+    title = segments[index_title].upper()
+    if title.endswith(' API'):
+        add_error(line_num, 'Title should not contain 'API'')
+    # END Title
+    # START Description
+    # first character should be capitalized
+    char = segments[index_desc][0]
+    if char.upper() != char:
+        add_error(line_num, 'first character of description is not capitalized')
+    # last character should not punctuation
+    char = segments[index_desc][-1]
+    if char in punctuation:
+        add_error(line_num, 'description should not end with {}'.format(char))
+    desc_length = len(segments[index_desc])
+    if desc_length > 100:
+        add_error(line_num, 'description should not exceed 100 characters (currently {})'.format(desc_length))
+    # END Description
+    # START Auth
+    # values should conform to valid options only
+    auth = segments[index_auth]
+    if auth != 'No' and (not auth.startswith('`') or not auth.endswith('`')):
+        add_error(line_num, 'auth value is not enclosed with `backticks`')
+    if auth.replace('`', '') not in auth_keys:
+        add_error(line_num, '{} is not a valid Auth option'.format(auth))
+    # END Auth
+    # START HTTPS
+    # values should conform to valid options only
+    https = segments[index_https]
+    if https not in https_keys:
+        add_error(line_num, '{} is not a valid HTTPS option'.format(https))
+    # END HTTPS
+    # START CORS
+    # values should conform to valid options only
+    cors = segments[index_cors]
+    if cors not in cors_keys:
+        add_error(line_num, '{} is not a valid CORS option'.format(cors))
+    # END CORS
+    # START Link
+    # url should be wrapped in '[Go!]()' Markdown syntax
+    link = segments[index_link]
+    if not link.startswith('[Go!](http') or not link.endswith(')'):
+        add_error(line_num, 'link syntax should be '[Go!](LINK)'')
+    # END Link
     
-        def unregister(self, plugin):
-        self._plugins.remove(plugin)
+        def contribute(self):
+        self.blackboard.common_state['problems'] += random.randint(10, 20)
+        self.blackboard.common_state['suggestions'] += random.randint(10, 20)
+        self.blackboard.common_state['contributions'] += [self.__class__.__name__]
+        self.blackboard.common_state['progress'] += random.randint(10, 30)
     
-        parent = os.path.join(path, os.path.pardir)
-    parent = os.path.realpath(parent)
+    from __future__ import print_function
     
-            # global, resource, entity
-        input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/global/urlMaps/my-url-map'
-        actual = GCPUtils.parse_gcp_url(input_url)
-        self.assertEquals('myproject', actual['project'])
-        self.assertTrue('global' in actual)
-        self.assertTrue(actual['global'])
-        self.assertEquals('v1', actual['api_version'])
-        self.assertEquals('compute', actual['service'])
-    
-    from ansible.plugins.terminal import TerminalBase
-from ansible.errors import AnsibleConnectionFailure
-    
-    import os
-import re
-    
-    def plot(func):
-    random_state = check_random_state(0)
-    one_core = []
-    multi_core = []
-    sample_sizes = range(1000, 6000, 1000)
-    
-        ###########################################################################
-    # Set transformer input
-    ###########################################################################
-    transformers = {}
-    
-       ###########################################################################
-    # Set custom automatic method selection
-    sampling_algorithm['custom-auto'] = \
-        lambda n_population, n_samples, random_state=None: \
-            sample_without_replacement(n_population,
-                                       n_samples,
-                                       method='auto',
-                                       random_state=random_state)
-    
-    First, we fix a training set, increase the number of
-samples to classify and plot number of classified samples as a
-function of time.
-    
-        url_fmt is along the lines of ('https://github.com/USER/PROJECT/'
-                                   'blob/{revision}/{package}/'
-                                   '{path}#L{lineno}')
-    '''
-    revision = _get_git_revision()
-    return partial(_linkcode_resolve, revision=revision, package=package,
-                   url_fmt=url_fmt)
+    ###  OUTPUT ###
+# Before subclassing:
+# BaseRegisteredClass
+# After subclassing:
+# BaseRegisteredClass
+# ClassRegistree
 
     
-    import lxml.html
-from lxml.etree import ElementTree
-import numpy as np
+        def __init__(self, src, dest):
+        self.src = src
+        self.dest = dest
     
-            controller.coef0 = Tk.StringVar()
-        controller.coef0.set('0')
-        r = Tk.Frame(valbox)
-        Tk.Label(r, text='coef0:', anchor='e', width=7).pack(side=Tk.LEFT)
-        Tk.Entry(r, width=6, textvariable=controller.coef0).pack(side=Tk.LEFT)
-        r.pack()
-        valbox.pack(side=Tk.LEFT)
     
-    Plot the classification probability for different classifiers. We use a 3
-class dataset, and we classify it with a Support Vector classifier, L1
-and L2 penalized logistic regression with either a One-Vs-Rest or multinomial
-setting, and Gaussian process classification.
+class TestManager:
     
-    # Author: Olivier Grisel <olivier.grisel@ensta.org>
-# License: BSD 3 clause
-    
-        if update:
-        time.sleep(1)
-        pull_screenshot()
-        im.set_array(update_data())
-        update = False
-    return im,
-    
-    if sys.version_info.major != 3:
-    print('请使用Python3')
-    exit(1)
-try:
-    from common import debug, config, screenshot, UnicodeStreamFilter
-    from common.auto_adb import auto_adb
-except Exception as ex:
-    print(ex)
-    print('请将脚本放在项目根目录中运行')
-    print('请检查项目根目录中的 common 文件夹是否存在')
-    exit(1)
-adb = auto_adb()
-VERSION = '1.1.4'
+        a_transaction = Transaction(True, num_obj)
+    try:
+        for i in range(3):
+            num_obj.increment()
+            print(num_obj)
+        a_transaction.commit()
+        print('-- committed')
