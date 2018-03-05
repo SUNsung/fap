3bdeca15ec5d683fb7955bd7b3ba163fa92e1697
SUNsung <weatherius@gmail.com>
2018-03-05 02:19:26 +0200

    Rewrite [40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp] File in: 2018-03-05 02:19:26

--- cccccc/40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp
+++ cccccc/40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp
@@ -1,224 +1,128 @@
 
         
-        # define INSTANTIATE_TEST_CASE_P(prefix, test_case_name, generator) \
-  ::testing::internal::ParamGenerator<test_case_name::ParamType> \
-      gtest_##prefix##test_case_name##_EvalGenerator_() { return generator; } \
-  int gtest_##prefix##test_case_name##_dummy_ = \
-      ::testing::UnitTest::GetInstance()->parameterized_test_registry(). \
-          GetTestCasePatternHolder<test_case_name>(\
-              #test_case_name, __FILE__, __LINE__)->AddTestCaseInstantiation(\
-                  #prefix, \
-                  &gtest_##prefix##test_case_name##_EvalGenerator_, \
-                  __FILE__, __LINE__)
-    
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12>
-class ValueArray12 {
- public:
-  ValueArray12(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9,
-      T10 v10, T11 v11, T12 v12) : v1_(v1), v2_(v2), v3_(v3), v4_(v4), v5_(v5),
-      v6_(v6), v7_(v7), v8_(v8), v9_(v9), v10_(v10), v11_(v11), v12_(v12) {}
-    }
-    
-      // Trivial case 2: even numbers
-  if (n % 2 == 0) return n == 2;
-    
-    
+            static void* GetProcAddress (const char* name)
     {
-    {
-    {  for (const auto& share_line : osquery::split(content, '\n')) {
-    genNFSShare(share_line, results);
-  }
-  return results;
-}
-}
-}
-
-    
-      /**
-   * @brief The privilege/permissions dropper deconstructor will restore
-   * effective permissions.
-   *
-   * There should only be a single drop of privilege/permission active.
-   */
-  virtual ~DropPrivileges();
-    
-      /// Get a list of the registry item names for a given registry.
-  std::vector<std::string> names(const std::string& registry_name) const;
-    
-        size_t length = 0;
-    void *e = NULL;
-    switch (*(int *)data) {
-    case 0:
-      e = osquery_cqueue_reserve(
-          &osquery.cqueue, OSQUERY_TEST_EVENT_0, sizeof(test_event_0_data_t));
-      length = 4096;
-      break;
-    case 1:
-      e = osquery_cqueue_reserve(
-          &osquery.cqueue, OSQUERY_TEST_EVENT_1, sizeof(test_event_1_data_t));
-      length = 33;
-      break;
-    default:
-      return -ENOTTY;
-    }
-    if (!e) {
-      return -EINVAL;
-    }
-    
-     private:
-  /*
-   * @brief A helper function to 'carve' files from disk
-   *
-   * This function performs a 'forensic carve' of a specified path to the
-   * users tmp directory
-   */
-  Status carve(const boost::filesystem::path& path);
-    
-        pt::ptree multi_pack;
-    for (const auto& path : paths) {
-      std::string content;
-      if (!readFile(path, content)) {
-        LOG(WARNING) << 'Cannot read multi-pack file: ' << path;
-        continue;
-      }
-    }
-    
-    #include <osquery/config.h>
-#include <osquery/dispatcher.h>
-    
-    void BuildDCHistograms(const JPEGData& jpg, JpegHistogram* histo) {
-  for (size_t i = 0; i < jpg.components.size(); ++i) {
-    const JPEGComponent& c = jpg.components[i];
-    JpegHistogram* dc_histogram = &histo[i];
-    coeff_t last_dc_coeff = 0;
-    for (int mcu_y = 0; mcu_y < jpg.MCU_rows; ++mcu_y) {
-      for (int mcu_x = 0; mcu_x < jpg.MCU_cols; ++mcu_x) {
-        for (int iy = 0; iy < c.v_samp_factor; ++iy) {
-          for (int ix = 0; ix < c.h_samp_factor; ++ix) {
-            int block_y = mcu_y * c.v_samp_factor + iy;
-            int block_x = mcu_x * c.h_samp_factor + ix;
-            int block_idx = block_y * c.width_in_blocks + block_x;
-            coeff_t dc_coeff = c.coeffs[block_idx << 6];
-            int diff = std::abs(dc_coeff - last_dc_coeff);
-            int nbits = Log2Floor(diff) + 1;
-            dc_histogram->Add(nbits);
-            last_dc_coeff = dc_coeff;
-          }
+        static void* h = NULL;
+        if (!h)
+        {
+            h = dlopen('libclAmdFft.Runtime.so', RTLD_LAZY | RTLD_GLOBAL);
+            if (!h)
+                return NULL;
         }
-      }
+    }
+    
+       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    
+      // Extract the original list data
+  std::string data;
+  db_->Get(get_option_, key, &data);
+    
+    namespace rocksdb {
+    }
+    
+    // Open the db inside DateTieredDBImpl because options needs pointer to its ttl
+DateTieredDBImpl::DateTieredDBImpl(
+    DB* db, Options options,
+    const std::vector<ColumnFamilyDescriptor>& descriptors,
+    const std::vector<ColumnFamilyHandle*>& handles, int64_t ttl,
+    int64_t column_family_interval)
+    : db_(db),
+      cf_options_(ColumnFamilyOptions(options)),
+      ioptions_(ImmutableCFOptions(options)),
+      icomp_(cf_options_.comparator),
+      ttl_(ttl),
+      column_family_interval_(column_family_interval),
+      mutex_(options.statistics.get(), db->GetEnv(), DB_MUTEX_WAIT_MICROS,
+             options.use_adaptive_mutex) {
+  latest_timebound_ = std::numeric_limits<int64_t>::min();
+  for (size_t i = 0; i < handles.size(); ++i) {
+    const auto& name = descriptors[i].name;
+    int64_t timestamp = 0;
+    try {
+      timestamp = ParseUint64(name);
+    } catch (const std::invalid_argument&) {
+      // Bypass unrelated column family, e.g. default
+      db_->DestroyColumnFamilyHandle(handles[i]);
+      continue;
+    }
+    if (timestamp > latest_timebound_) {
+      latest_timebound_ = timestamp;
+    }
+    handle_map_.insert(std::make_pair(timestamp, handles[i]));
+  }
+}
+    
+    #include 'BtMessage.h'
+#include 'Command.h'
+    
+    bool AbstractHttpServerResponseCommand::execute()
+{
+  if (e_->getRequestGroupMan()->downloadFinished() || e_->isHaltRequested()) {
+    return true;
+  }
+  try {
+    ssize_t len = httpServer_->sendResponse();
+    if (len > 0) {
+      timeoutTimer_ = global::wallclock();
+    }
+  }
+  catch (RecoverableException& e) {
+    A2_LOG_INFO_EX(fmt('CUID#%' PRId64
+                       ' - Error occurred while transmitting response body.',
+                       getCuid()),
+                   e);
+    return true;
+  }
+  if (httpServer_->sendBufferIsEmpty()) {
+    A2_LOG_INFO(fmt('CUID#%' PRId64 ' - HttpServer: all response transmitted.',
+                    getCuid()));
+    afterSend(httpServer_, e_);
+    return true;
+  }
+  else {
+    if (timeoutTimer_.difference(global::wallclock()) >= 30_s) {
+      A2_LOG_INFO(fmt('CUID#%' PRId64
+                      ' - HttpServer: Timeout while trasmitting response.',
+                      getCuid()));
+      return true;
+    }
+    else {
+      updateReadWriteCheck();
+      e_->addCommand(std::unique_ptr<Command>(this));
+      return false;
     }
   }
 }
     
-        // The nodes are:
-    // [0, n): the sorted leaf nodes that we start with.
-    // [n]: we add a sentinel here.
-    // [n + 1, 2n): new parent nodes are added here, starting from
-    //              (n+1). These are naturally in ascending order.
-    // [2n]: we add a sentinel at the end as well.
-    // There will be (2n+1) elements at the end.
-    const HuffmanTree sentinel(~static_cast<uint32_t>(0), -1, -1);
-    tree[n] = sentinel;
-    tree[n + 1] = sentinel;
-    
-    // Mimic libjpeg's heuristics to guess jpeg color space.
-// Requires that the jpg has 3 components.
-bool HasYCbCrColorSpace(const JPEGData& jpg);
-    
-    #endif  // GUETZLI_JPEG_ERROR_H_
-
-    
-      // Make a local copy of the input bit length histogram.
-  int count[kJpegHuffmanMaxBitLength + 1] = { 0 };
-  int total_count = 0;
-  for (len = 1; len <= kJpegHuffmanMaxBitLength; ++len) {
-    count[len] = count_in[len];
-    total_count += count[len];
-  }
-    
-    // Builds jpeg-style Huffman lookup table from the given symbols.
-// The symbols are in order of increasing bit lengths. The number of symbols
-// with bit length n is given in counts[n] for each n >= 1.
-// Returns the size of the lookup table.
-int BuildJpegHuffmanTable(const int* counts, const int* symbols,
-                          HuffmanTableEntry* lut);
-    
-      int width() const { return width_; }
-  int height() const { return height_; }
-  int factor_x() const { return factor_x_; }
-  int factor_y() const { return factor_y_; }
-  int width_in_blocks() const { return width_in_blocks_; }
-  int height_in_blocks() const { return height_in_blocks_; }
-  const coeff_t* coeffs() const { return &coeffs_[0]; }
-  const int* quant() const { return &quant_[0]; }
-  bool IsAllZero() const;
-    
-    namespace facebook {
-namespace jni {
-    }
-    }
-    
-      /**
-   * The offset within the current function
-   */
-  int functionOffset() const noexcept {
-    auto absoluteSymbol = static_cast<const char*>(functionAddress_);
-    auto absoluteabsoluteProgramCounter =
-        static_cast<const char*>(absoluteProgramCounter_);
-    return absoluteabsoluteProgramCounter - absoluteSymbol;
-  }
-    
-      YGNodeCalculateLayout(root, 200, 100, YGDirectionLTR);
-    
-     public: // Setters
-    
-    namespace facebook {
-    }
-    
     
     {
-    {}}
-    
-    #pragma once
-#include <atomic>
-#include <fb/assert.h>
-#include <fb/noncopyable.h>
-#include <fb/nonmovable.h>
-#include <fb/RefPtr.h>
-    
-    #include <fb/visibility.h>
-    
-    
-    {
-    {        // Start the ASIO io_service run loop
-        echo_server.run();
-    } catch (websocketpp::exception const & e) {
-        std::cout << e.what() << std::endl;
-    } catch (...) {
-        std::cout << 'other exception' << std::endl;
-    }
+    {    httpConnection_->sendProxyRequest(std::move(httpRequest));
+  }
+  else {
+    httpConnection_->sendPendingData();
+  }
+  if (httpConnection_->sendBufferIsEmpty()) {
+    getDownloadEngine()->addCommand(getNextCommand());
+    return true;
+  }
+  else {
+    setWriteCheckSocket(getSocket());
+    addCommandSelf();
+    return false;
+  }
 }
-
     
-    #define CONNECTIONS_PER_ADDRESS 28000
-#define THREADS 10
-    
-    #endif
-    
-        sockaddr_storage addr;
-    socklen_t addrLength = sizeof(addr);
-    if (getpeername(fd, (sockaddr *) &addr, &addrLength) == -1) {
-        return {0, '', ''};
-    }
-    
-    
-    {    static const int CONSUME_POST_PADDING = 4;
-    static const int CONSUME_PRE_PADDING = LONG_MESSAGE_HEADER - 1;
-};
-    
-    struct Timer {
-    Loop *loop;
-    void *data;
-    }
+    AnnounceTier::AnnounceTier(std::deque<std::string> urls)
+    : event(STARTED), urls(std::move(urls))
+{
+}
