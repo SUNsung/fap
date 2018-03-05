c1b0f5b7d7e1b3482ee3f6e10cd0e40976902215
SUNsung <weatherius@gmail.com>
2018-03-05 16:18:52 +0200

    Rewrite [20prHYUcKLOmw20aKy6Jnqkcxg20PHiyDt2VOYE20O.D.rb] File in: 2018-03-05 16:18:52

--- ruby/20prHYUcKLOmw20aKy6Jnqkcxg20PHiyDt2VOYE20O.D.rb
+++ ruby/20prHYUcKLOmw20aKy6Jnqkcxg20PHiyDt2VOYE20O.D.rb
@@ -1,72 +1,84 @@
 
         
-              # Renders the template using the class intance as the binding. Because the
-      # renderer inherits from `OpenStruct`, additional view variables can be
-      # added like normal accessors.
-      #
-      # @return [String]
-      def render
-        old_template = template
-        result = nil
-        File.open(full_template_path, 'r') do |f|
-          self.template = f.read
-          result = render_string
+        def fixture_site(overrides = {})
+  Jekyll::Site.new(site_configuration(overrides))
+end
+    
+    unless File.exist?(PROF_OUTPUT_FILE)
+  StackProf.run(
+    mode: MODE.to_sym,
+    interval: 100,
+    raw: true,
+    out: PROF_OUTPUT_FILE
+  ) do
+    puts 'GC Stats:', JSON.pretty_generate(GC.stat)
+    GC.disable
+    
+            c.action do |args, opts|
+          Jekyll::Commands::NewTheme.process(args, opts)
         end
-    
-    module Metasploit
-  module Framework
-    class Application < Rails::Application
-      include Metasploit::Framework::CommonEngine
-    
-        inp.attributes.keys.each do |ikey|
-      if (ikey.downcase == 'value')
-        inp[ikey] = ''
-        next
-      end
-    
-          when :banner
-        # Because some ftp server send multiple banner we take only the first one and ignore the rest
-        if not (s[:info])
-          s[:info] = matches
-          report_service(s)
-        end
-    
-      def parse(pkt)
-    # We want to return immediantly if	we do not have a packet which is handled by us
-    return unless pkt.is_tcp?
-    return if (pkt.tcp_sport != 80 and pkt.tcp_dport != 80)
-    s = find_session((pkt.tcp_sport == 80) ? get_session_src(pkt) : get_session_dst(pkt))
-    
-    puts '* Initializing Meterpreter'
-    
-    classNames.each { |name|
-	filesIn << clsFile.new_with_sig('Ljava.lang.String;', '#{outputDir}/#{name}.class')
-}
-    
-    outputJar = 'output.jar'
-    
-        @execve      = ''
-    @getpeername = ''
-    @accept      = ''
-    @listen      = ''
-    @bind        = ''
-    @socket      = ''
-    @connect     = ''
-    @close       = ''
-    @kfcntl      = ''
-    
-    	def initialize(filename)
-		unless filename.empty?
-			self.file = File.new(filename)
-		else
-			self.file = STDIN
-		end
-    
-        # Remove directories opposite from traversal, so that a subtree with no
-    # actual files gets removed correctly.
-    dirs.reverse_each do |d|
-      if d.children.empty?
-        puts 'rmdir: #{d} (empty)' if ARGV.verbose?
-        d.rmdir
       end
     end
+    
+          # Group an array of items by an expression
+      #
+      # input - the object array
+      # variable - the variable to assign each item to in the expression
+      # expression -a Liquid comparison expression passed in as a string
+      #
+      # Returns the filtered array of objects
+      def group_by_exp(input, variable, expression)
+        return input unless groupable?(input)
+    
+      # Update version.rb file with BOOTSTRAP_SHA
+  def store_version
+    path    = 'lib/bootstrap-sass/version.rb'
+    content = File.read(path).sub(/BOOTSTRAP_SHA\s*=\s*[''][\w]+['']/, 'BOOTSTRAP_SHA = '#@branch_sha'')
+    File.open(path, 'w') { |f| f.write(content) }
+  end
+end
+
+    
+        def log_processed(name)
+      puts green '    #{name}'
+    end
+    
+      # The test environment is used exclusively to run your application's
+  # test suite. You never need to work with it otherwise. Remember that
+  # your test database is 'scratch space' for the test suite and is wiped
+  # and recreated between test runs. Don't rely on the data there!
+  config.cache_classes = true
+    
+    def report_apps
+  running = Set.new
+  @app_names.zip(@bundle_ids, @unix_ids).each do |app_name, bundle_id, _unix_id|
+    next if excluded_bundle_id bundle_id
+    next if excluded_app_name app_name
+    bundle_id.gsub!(%r{^(missing value)$}, '<\1>')
+    running.add '#{bundle_id}\t#{app_name}'
+  end
+    
+        # @abstract
+    #
+    # Create a (new) clone of the remote-repository on the deployment target
+    #
+    # @return void
+    #
+    def clone
+      raise NotImplementedError, 'Your SCM strategy module should provide a #clone method'
+    end
+    
+      desc 'Finished'
+  task :finished do
+  end
+    
+              add_offense(node)
+        end
+    
+      def default_metadata
+    @factory.build(:default_metadata).all
+  end
+    
+      def self.plugin_type
+    'codec'
+  end
