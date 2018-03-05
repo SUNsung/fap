6f1695e666132da558e3070df687d145ad1d65c0
SUNsung <weatherius@gmail.com>
2018-03-05 14:33:49 +0200

    Rewrite [23NkwOQ1NHMtI238iUmm.CoGL.23.iEz6MR787w23hFg.py] File in: 2018-03-05 14:33:48

--- pyton/23NkwOQ1NHMtI238iUmm.CoGL.23.iEz6MR787w23hFg.py
+++ pyton/23NkwOQ1NHMtI238iUmm.CoGL.23.iEz6MR787w23hFg.py
@@ -1,255 +1,81 @@
 
         
-            infile, outfile = args
+        
+class PollErrorTest(unittest.TestCase):
+    '''Tests for acme.errors.PollError.'''
     
-    options = helptext[helptext.index('  General Options:') + 19:]
-options = re.sub(r'(?m)^  (\w.+)$', r'## \1', options)
-options = '# OPTIONS\n' + options + '\n'
+    # If true, links to the reST sources are added to the pages.
+#html_show_sourcelink = True
+    
+        return response
+    
+    # Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+]
     
     
-class TestCache(unittest.TestCase):
-    def setUp(self):
-        TEST_DIR = os.path.dirname(os.path.abspath(__file__))
-        TESTDATA_DIR = os.path.join(TEST_DIR, 'testdata')
-        _mkdir(TESTDATA_DIR)
-        self.test_dir = os.path.join(TESTDATA_DIR, 'cache_test')
-        self.tearDown()
-    
-        def test_secondary_proxy_https(self):
-        params = self._check_params(['secondary_proxy', 'secondary_server_ip'])
-        if params is None:
-            return
-        ydl = FakeYDL()
-        req = compat_urllib_request.Request('https://yt-dl.org/ip')
-        req.add_header('Ytdl-request-proxy', params['secondary_proxy'])
-        self.assertEqual(
-            ydl.urlopen(req).read().decode('utf-8'),
-            params['secondary_server_ip'])
-    
-            retval = subprocess.call(args)
-        if retval == 0:
-            fsize = os.path.getsize(encodeFilename(tmpfilename))
-            self.to_screen('\r[%s] %s bytes' % (args[0], fsize))
-            self.try_rename(tmpfilename, filename)
-            self._hook_progress({
-                'downloaded_bytes': fsize,
-                'total_bytes': fsize,
-                'filename': filename,
-                'status': 'finished',
-            })
-            return True
-        else:
-            self.to_stderr('\n')
-            self.report_error('%s exited with code %d' % (args[0], retval))
-            return False
+if __name__ == '__main__':
+    unittest.main()  # pragma: no cover
 
     
-        _ALL_CLASSES = [
-        klass
-        for name, klass in globals().items()
-        if name.endswith('IE') and name != 'GenericIE'
-    ]
-    _ALL_CLASSES.append(GenericIE)
+        @mock.patch('certbot.notify.smtplib.LMTP')
+    @mock.patch('certbot.notify.subprocess.Popen')
+    def test_everything_fails(self, mock_popen, mock_lmtp):
+        from certbot.notify import notify
+        lmtp_obj = mock.MagicMock()
+        mock_lmtp.return_value = lmtp_obj
+        lmtp_obj.sendmail.side_effect = socket.error(17)
+        proc = mock.MagicMock()
+        mock_popen.return_value = proc
+        proc.communicate.side_effect = OSError('What we have here is a '
+                                               'failure to communicate.')
+        self.assertFalse(notify('Goose', 'auntrhody@example.com',
+                                'The old grey goose is dead.'))
+        self.assertEqual(lmtp_obj.sendmail.call_count, 1)
+        self.assertEqual(proc.communicate.call_count, 1)
     
-            # Request the extended version of the api for extra fields like artist and title
-        api_response = self._download_json(
-            'http://www.audiomack.com/api/music/url/song/%s?extended=1&_=%d' % (
-                album_url_tag, time.time()),
-            album_url_tag)
     
-            String:                    '#4e9a06',        # class: 's'
-        String.Backtick:           '#4e9a06',        # class: 'sb'
-        String.Char:               '#4e9a06',        # class: 'sc'
-        String.Doc:                'italic #8f5902', # class: 'sd' - like a comment
-        String.Double:             '#4e9a06',        # class: 's2'
-        String.Escape:             '#4e9a06',        # class: 'se'
-        String.Heredoc:            '#4e9a06',        # class: 'sh'
-        String.Interpol:           '#4e9a06',        # class: 'si'
-        String.Other:              '#4e9a06',        # class: 'sx'
-        String.Regex:              '#4e9a06',        # class: 'sr'
-        String.Single:             '#4e9a06',        # class: 's1'
-        String.Symbol:             '#4e9a06',        # class: 'ss'
-    
-        return inner
-    
-        :param url: URL for the new :class:`Request` object.
-    :param params: (optional) Dictionary or bytes to be sent in the query string for the :class:`Request`.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
+def _get_screen_size():
     '''
-    
-        def __call__(self, r):
-        r.headers['Proxy-Authorization'] = _basic_auth_str(self.username, self.password)
-        return r
-    
-    #: Python 3.x?
-is_py3 = (_ver[0] == 3)
-    
-        # Check cryptography version
-    from cryptography import __version__ as cryptography_version
-    _check_cryptography(cryptography_version)
-except ImportError:
-    pass
-    
-    num_classes = 10
-batch_size = 128
-epochs = 15
-weighted_class = 5
-high_weight = 10
-train_samples = 5000
-test_samples = 1000
-timesteps = 3
-input_dim = 10
-loss = 'mse'
-loss_full_name = 'mean_squared_error'
-standard_weight = 1
-standard_score_sequential = 0.5
-    
-    
-@keras_test
-def test_loss_masking():
-    weighted_loss = _weighted_masked_objective(losses.get('mae'))
-    shape = (3, 4, 2)
-    x = np.arange(24).reshape(shape)
-    y = 2 * x
-    
-        if len(args) > 0 and '# Arguments' not in doc:
-        raise ValueError('{} needs a '# Arguments' section'.format(name),
-                         member.__module__)
-    
-        history = model.fit(x, ys, validation_split=0.05, batch_size=10,
-                        verbose=0, epochs=3)
-    ground_truth = -np.log(0.5)
-    assert(np.abs(history.history['loss'][-1] - ground_truth) < 0.06)
-    
-    
-@keras_test
-def test_merge_sum(in_tmpdir):
-    (x_train, y_train), (x_test, y_test) = _get_test_data()
-    left = Sequential()
-    left.add(Dense(num_hidden, input_shape=(input_dim,)))
-    left.add(Activation('relu'))
-    
-        n_samples = 2000
-    list_n_features = np.linspace(500, 3000, 5).astype(np.int)
-    lasso_results, lars_lasso_results = compute_bench(alpha, [n_samples],
-                                           list_n_features, precompute=False)
-    plt.subplot(212)
-    plt.plot(list_n_features, lasso_results, 'b-', label='Lasso')
-    plt.plot(list_n_features, lars_lasso_results, 'r-', label='LassoLars')
-    plt.title('%d samples, alpha=%s' % (n_samples, alpha))
-    plt.legend(loc='upper left')
-    plt.xlabel('number of features')
-    plt.ylabel('Time (s)')
-    plt.axis('tight')
-    plt.show()
+    获取手机屏幕大小
+    '''
+    size_str = adb.get_screen()
+    m = re.search(r'(\d+)x(\d+)', size_str)
+    if m:
+        return '{height}x{width}'.format(height=m.group(2), width=m.group(1))
+    return '1920x1080'
 
     
-        # decode the payload explicitly as UTF-8 since lxml is confused for some
-    # reason
-    with codecs.open(html_filename,'r','utf-8') as html_file:
-        html_content = html_file.read()
-    tree = ElementTree(lxml.html.document_fromstring(html_content))
-    i = 0
-    j = 0
-    for p in tree.findall('//p'):
-        content = p.text_content()
-        if len(content) < 100:
-            # skip paragraphs that are too short - probably too noisy and not
-            # representative of the actual language
-            continue
     
-        # the training data folder must be passed as first argument
-    movie_reviews_data_folder = sys.argv[1]
-    dataset = load_files(movie_reviews_data_folder, shuffle=False)
-    print('n_samples: %d' % len(dataset.data))
-    
-    from sklearn.datasets import make_checkerboard
-from sklearn.datasets import samples_generator as sg
-from sklearn.cluster.bicluster import SpectralBiclustering
-from sklearn.metrics import consensus_score
-    
-    plt.grid('off')
-for x in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    plt.plot([0, x], [x, 0], 'k', alpha=0.2)
-    plt.plot([0, 0 + (1-x)/2], [x, x + (1-x)/2], 'k', alpha=0.2)
-    plt.plot([x, x + (1-x)/2], [0, 0 + (1-x)/2], 'k', alpha=0.2)
-    
-    
-def open_accordant_config():
+def backup_screenshot(ts):
     '''
-    调用配置文件
+    为了方便失败的时候 debug
     '''
-    screen_size = _get_screen_size()
-    config_file = '{path}/config/{screen_size}/config.json'.format(
-        path=sys.path[0],
-        screen_size=screen_size
-    )
+    make_debug_dir(screenshot_backup_dir)
+    shutil.copy('{}{}autojump.png'.format(os.getcwd(), path_split),
+                os.path.join(os.getcwd(), screenshot_backup_dir,
+                             str(ts) + '.png'))
     
-    
-def computing_error(last_press_time, target_board_x, target_board_y, last_piece_x, last_piece_y, temp_piece_x,
-                    temp_piece_y):
-    '''
-    计算跳跃实际误差
-    '''
-    target_distance = math.sqrt(
-        (target_board_x - last_piece_x) ** 2 + (target_board_y - last_piece_y) ** 2)  # 上一轮目标跳跃距离
-    actual_distance = math.sqrt((temp_piece_x - last_piece_x) ** 2 + (temp_piece_y - last_piece_y) ** 2)  # 上一轮实际跳跃距离
-    jump_error_value = math.sqrt((target_board_x - temp_piece_x) ** 2 + (target_board_y - temp_piece_y) ** 2)  # 跳跃误差
-    
-    try:
-    from common import ai, debug, config, UnicodeStreamFilter
-    from common.auto_adb import auto_adb
-except Exception as ex:
-    print(ex)
-    print('请将脚本放在项目根目录中运行')
-    print('请检查项目根目录中的 common 文件夹是否存在')
-    exit(1)
-adb = auto_adb()
-VERSION = '1.1.3'
-    
-            graph = tf.get_default_graph()
-        x = graph.get_tensor_by_name('x:0')
-        logits = graph.get_tensor_by_name('logits_eval:0')
-    #####################识别分数
-        while True:
-            screenshot.pull_screenshot()
-            im = Image.open('./autojump.png')
-            ##比例系数
-            pix_w=im.size[0]*1.0/1080
-            pix_h=im.size[1]
-            region=im.crop((0,pix_h*0.1,460*pix_w,pix_h*0.2))
-            region=region.convert('L')
-            start_h,end_h,pixels_Widh=pixel_division(region,int(460*pix_w),int(pix_h*0.1))
-            if start_h==end_h:
-                continue
-            data = []
-            for i in range(len(pixels_Widh)-1):
-                region1=region.crop((pixels_Widh[i],start_h,pixels_Widh[i+1],end_h))
-                region1.putdata(pross_data(region1))
-                str1='./region'+str(i)+'.png'
-                region1.save(str1)
-                data1 = read_one_image(str1)
-                data.append(data1)
-            feed_dict = {x:data}
-            classification_result = sess.run(logits,feed_dict)
-            output = []
-            output = tf.argmax(classification_result,1).eval()
-            m_score=''
-            for i in range(len(output)):
-                m_score+=strint(output[i])
-            if m_score=='':
-                continue
-            m_score=int(m_score)
-            print('score:{}'.format(m_score))
-            ####################################
-            # 获取棋子和 board 的位置
-            print(j)
-            piece_x, piece_y, board_x, board_y = find_piece_and_board(im)
-            ts = int(time.time())
-            print(ts, piece_x, piece_y, board_x, board_y)
-            set_button_position(im)
+        # 从 scan_start_y 开始往下扫描，棋子应位于屏幕上半部分，这里暂定不超过 2/3
+    for i in range(scan_start_y, int(h * 2 / 3)):
+        # 横坐标方面也减少了一部分扫描开销
+        for j in range(scan_x_border, w - scan_x_border):
+            pixel = im_pixel[j, i]
+            # 根据棋子的最低行的颜色判断，找最后一行那些点的平均值，这个颜
+            # 色这样应该 OK，暂时不提出来
+            if (50 < pixel[0] < 60) \
+                    and (53 < pixel[1] < 63) \
+                    and (95 < pixel[2] < 110):
+                piece_x_sum += j
+                piece_x_c += 1
+                piece_y_max = max(i, piece_y_max)
     
     
 # Magic Number，不设置可能无法正常执行，请根据具体截图从上到下按需设置
@@ -261,3 +87,11 @@ piece_base_height_1_2 = config['piece_base_height_1_2']
 # 棋子的宽度，比截图中量到的稍微大一点比较安全，可能要调节
 piece_body_width = config['piece_body_width']
 time_coefficient = config['press_coefficient']
+    
+            # 标注截图并显示
+        # draw = ImageDraw.Draw(im)
+        # draw.line([piece_x, 0, piece_x, h], fill='blue', width=1)  # start
+        # draw.line([board_x, 0, board_x, h], fill='red', width=1)  # end
+        # draw.ellipse([swipe_x1 - 16, swipe_y1 - 16,
+        #               swipe_x1 + 16, swipe_y1 + 16], fill='red')  # click
+        # im.show()
