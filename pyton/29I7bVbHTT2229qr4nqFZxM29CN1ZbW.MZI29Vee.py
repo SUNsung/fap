02740cfa4097221fae983f1bec2f7f0e1e4564cc
SUNsung <weatherius@gmail.com>
2018-03-06 15:07:42 +0200

    Rewrite [29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py] File in: 2018-03-06 15:07:42

--- pyton/29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py
+++ pyton/29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py
@@ -1,89 +1,71 @@
 
         
-        
-def get_db():
-    '''Opens a new database connection if there is none yet for the
-    current application context.
-    '''
-    top = _app_ctx_stack.top
-    if not hasattr(top, 'sqlite_db'):
-        top.sqlite_db = sqlite3.connect(app.config['DATABASE'])
-        top.sqlite_db.row_factory = sqlite3.Row
-    return top.sqlite_db
+                self.port = random.randint(20000, 30000)
+        self.server_process = subprocess.Popen([
+            'srelay', '-f', '-i', '127.0.0.1:%d' % self.port],
+            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+            webpage = self._download_webpage(url, display_id)
+    
+    def _read_words(filename):
+  with tf.gfile.GFile(filename, 'r') as f:
+    if Py3:
+      return f.read().replace('\n', '<eos>').split()
+    else:
+      return f.read().decode('utf-8').replace('\n', '<eos>').split()
+    
+      return seq_ae_seq
+    
+      if np.isnan(loss_val):
+    raise OverflowError('Loss is nan')
+    
+        Args:
+      inputs: A Tensor representing a batch of input images.
+      training: A boolean. Set to True to add operations required only when
+        training the classifier.
+    
+    from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+    
+    flags.DEFINE_string('checkpoint_dir', '/tmp/text_train',
+                    'Directory where to read model checkpoints.')
+flags.DEFINE_integer('eval_interval_secs', 60, 'How often to run the eval.')
+flags.DEFINE_integer('num_examples', 32, 'Number of examples to run.')
+flags.DEFINE_bool('run_once', False, 'Whether to run eval only once.')
+    
+    data = data_utils
+flags = tf.app.flags
+FLAGS = flags.FLAGS
     
     
-def ptb_producer(raw_data, batch_size, num_steps, name=None):
-  '''Iterate on the raw PTB data.
+def main(_):
+  '''Trains LSTM classification model.'''
+  tf.logging.set_verbosity(tf.logging.INFO)
+  with tf.device(tf.train.replica_device_setter(FLAGS.ps_tasks)):
+    model = graphs.get_model()
+    train_op, loss, global_step = model.classifier_training()
+    train_utils.run_training(
+        train_op,
+        loss,
+        global_step,
+        variables_to_restore=model.pretrained_variables,
+        pretrained_model_dir=FLAGS.pretrained_model_dir)
     
     
-def evaluate(defun=False):
-  model = mnist.Model(data_format())
-  dataset = random_dataset()
-  if defun:
-    model.call = tfe.defun(model.call)
-  with tf.device(device()):
-    mnist_eager.test(model, dataset)
-    
-      def _record(self, label, red, green, blue):
-    image_size = 32 * 32
-    record = bytes(bytearray([label] + [red] * image_size +
-                             [green] * image_size + [blue] * image_size))
-    expected = [[[red, green, blue]] * 32] * 32
-    return record, expected
-    
-    
-#
-# Backwards compat functions.  Some modules include md5s in their return values
-# Continue to support that for now.  As of ansible-1.8, all of those modules
-# should also return 'checksum' (sha1 for now)
-# Do not use md5 unless it is needed for:
-# 1) Optional backwards compatibility
-# 2) Compliance with a third party protocol
-#
-# MD5 will not work on systems which are FIPS-140-2 compliant.
-#
-    
-        url = 'https://api.shippable.com/projects'
-    response = requests.get(url, data, headers=headers)
-    
-        # This matches a hostname or host pattern including [x:y(:z)] ranges.
-    #
-    # We roughly follow DNS rules here, but also allow ranges (and underscores).
-    # In the past, no systematic rules were enforced about inventory hostnames,
-    # but the parsing context (e.g. shlex.split(), fnmatch.fnmatch()) excluded
-    # various metacharacters anyway.
-    #
-    # We don't enforce DNS length restrictions here (63 characters per label,
-    # 253 characters total) or make any attempt to process IDNs.
-    
-    #      default_readme_template
-#      default_meta_template
-    
-            return '[@%d,%d:%d=%r,<%d>%s,%d:%d]' % (
-            self.index,
-            self.start, self.stop,
-            txt,
-            self.type, channelStr,
-            self.line, self.charPositionInLine
-            )
-    
-    
-    
-c = wda.Client()
-s = c.session()
-    
-        ix, iy = event.xdata, event.ydata
-    coords = [(ix, iy)]
-    print('now = ', coords)
-    cor.append(coords)
-    
-    
-# Magic Number，不设置可能无法正常执行，请根据具体截图从上到下按需设置
-under_game_score_y = config['under_game_score_y']
-# 长按的时间系数，请自己根据实际情况调节
-press_coefficient = config['press_coefficient']
-# 二分之一的棋子底座高度，可能要调节
-piece_base_height_1_2 = config['piece_base_height_1_2']
-# 棋子的宽度，比截图中量到的稍微大一点比较安全，可能要调节
-piece_body_width = config['piece_body_width']
-time_coefficient = config['press_coefficient']
+def download(directory, filename):
+  '''Download (and unzip) a file from the MNIST dataset if not already done.'''
+  filepath = os.path.join(directory, filename)
+  if tf.gfile.Exists(filepath):
+    return filepath
+  if not tf.gfile.Exists(directory):
+    tf.gfile.MakeDirs(directory)
+  # CVDF mirror of http://yann.lecun.com/exdb/mnist/
+  url = 'https://storage.googleapis.com/cvdf-datasets/mnist/' + filename + '.gz'
+  zipped_filepath = filepath + '.gz'
+  print('Downloading %s to %s' % (url, zipped_filepath))
+  urllib.request.urlretrieve(url, zipped_filepath)
+  with gzip.open(zipped_filepath, 'rb') as f_in, open(filepath, 'wb') as f_out:
+    shutil.copyfileobj(f_in, f_out)
+  os.remove(zipped_filepath)
+  return filepath
