f39084645511bb2fce20d0b35bfa91b036451fab
SUNsung <weatherius@gmail.com>
2018-03-06 20:25:41 +0200

    Rewrite [23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp] File in: 2018-03-06 20:25:41

--- cccccc/23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp
+++ cccccc/23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp
@@ -1,402 +1,81 @@
 
         
-        
-    {
-    {
-    {}  // namespace cuda
-}  // namespace gputools
-}  // namespace perftools
+          static void ClearCache(content::RenderProcessHost* render_view_host);
+  static void SetProxyConfig(content::RenderProcessHost* render_process_host,
+                             const std::string& proxy_config);
     
-      // Initializes all members to the default options.
-  PluginConfig();
+    #include 'base/logging.h'
+#include 'base/values.h'
+#include 'content/nw/src/api/api_messages.h'
+#include 'content/public/renderer/render_view.h'
+#include 'content/public/renderer/render_thread.h'
+#include 'content/public/renderer/v8_value_converter.h'
+#include 'third_party/WebKit/public/web/WebView.h'
+#include 'third_party/WebKit/public/web/WebLocalFrame.h'
+#include 'ui/base/resource/resource_bundle.h'
     
-    #ifndef TENSORFLOW_COMMON_RUNTIME_SYCL_SYCL_DEVICE_CONTEXT_H_
-#define TENSORFLOW_COMMON_RUNTIME_SYCL_SYCL_DEVICE_CONTEXT_H_
+    #include 'base/files/file_path.h'
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+       void Call(const std::string& method,
+                    const base::ListValue& arguments) override;
+   void CallSync(const std::string& method,
+                        const base::ListValue& arguments,
+                        base::ListValue* result) override;
     
+    #define V8_USE_UNSAFE_HANDLES
     
-    {  return bit_vector != tmp_bit_vector_;
+    namespace base {
+class ListValue;
 }
     
-    The input audio has one row of the tensor for each channel in the audio file.
-Each channel contains audio samples starting at the beginning of the audio and
-having `1/samples_per_second` time between them. The output file will contain
-all of the audio channels contained in the tensor.
-    
-    
-    {  int width = input_layer->width();
-  int height = input_layer->height();
-  float* input_data = input_layer->mutable_cpu_data();
-  for (int i = 0; i < input_layer->channels(); ++i) {
-    cv::Mat channel(height, width, CV_32FC1, input_data);
-    input_channels->push_back(channel);
-    input_data += width * height;
+      RenderView* render_view = GetCurrentRenderView();
+  if (!render_view) {
+    args.GetReturnValue().Set(isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate,
+                                     'Unable to get render view in AllocateObject'))));
+    return;
   }
+    
+    template <typename T1, typename T2, typename T3, typename T4, typename T5>
+internal::ValueArray5<T1, T2, T3, T4, T5> Values(T1 v1, T2 v2, T3 v3, T4 v4,
+    T5 v5) {
+  return internal::ValueArray5<T1, T2, T3, T4, T5>(v1, v2, v3, v4, v5);
 }
     
-    void read_image(std::ifstream* image_file, std::ifstream* label_file,
-        uint32_t index, uint32_t rows, uint32_t cols,
-        char* pixels, char* label) {
-  image_file->seekg(index * rows * cols + 16);
-  image_file->read(pixels, rows * cols);
-  label_file->seekg(index + 8);
-  label_file->read(label, 1);
-}
+    #endif  // GTEST_INCLUDE_GTEST_INTERNAL_GTEST_FILEPATH_H_
+
     
-    /**
- * @brief Computes @f$ y = |x| @f$
- *
- * @param bottom input Blob vector (length 1)
- *   -# @f$ (N \times C \times H \times W) @f$
- *      the inputs @f$ x @f$
- * @param top output Blob vector (length 1)
- *   -# @f$ (N \times C \times H \times W) @f$
- *      the computed outputs @f$ y = |x| @f$
- */
-template <typename Dtype>
-class AbsValLayer : public NeuronLayer<Dtype> {
+    // Helpers for suppressing warnings on unreachable code or constant
+// condition.
+    
+    template <>
+class Get<2> {
  public:
-  explicit AbsValLayer(const LayerParameter& param)
-      : NeuronLayer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+  template <class Tuple>
+  static GTEST_ADD_REF_(GTEST_TUPLE_ELEMENT_(2, Tuple))
+  Field(Tuple& t) { return t.f2_; }  // NOLINT
     }
     
+    // We only implement == and !=, as we don't have a need for the rest yet.
     
-    { private:
-  struct pair_sort_first {
-    bool operator()(const std::pair<int, int> &left,
-                    const std::pair<int, int> &right) {
-      return left.first < right.first;
-    }
-  };
-  void check_batch_reindex(int initial_num, int final_num,
-                           const Dtype* ridx_data);
-};
+    #include 'sample1.h'
     
-    #endif  // CAFFE_BNLL_LAYER_HPP_
-
+      void ServerTryCancel(ServerContext* context);
     
+    static const int WARMUP = 5;
+static const int BENCHMARK = 5;
     
-    {  bool handles_setup_;
-  cudnnHandle_t             handle_;
-  cudnnTensorDescriptor_t bottom_desc_, top_desc_;
-  cudnnPoolingDescriptor_t  pooling_desc_;
-  cudnnPoolingMode_t        mode_;
-};
-#endif
+    #include 'test/core/util/test_config.h'
+#include 'test/cpp/qps/benchmark_config.h'
+#include 'test/cpp/qps/driver.h'
+#include 'test/cpp/qps/report.h'
+#include 'test/cpp/qps/server.h'
+#include 'test/cpp/util/test_config.h'
+#include 'test/cpp/util/test_credentials_provider.h'
     
-     protected:
-  /**
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the computed outputs. At training time, we have @f$
-   *      y_{\mbox{train}} = \left\{
-   *         \begin{array}{ll}
-   *            \frac{x}{1 - p} & \mbox{if } u > p \\
-   *            0 & \mbox{otherwise}
-   *         \end{array} \right.
-   *      @f$, where @f$ u \sim U(0, 1)@f$ is generated independently for each
-   *      input at each iteration. At test time, we simply have
-   *      @f$ y_{\mbox{test}} = \mathbb{E}[y_{\mbox{train}}] = x @f$.
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    }
-    
-    namespace caffe {
-    }
+    #include <grpc/support/log.h>
     
     
-    {  WorkloadStats (const WorkloadStats&) = delete;
-  WorkloadStats& operator=(const WorkloadStats&) = delete;
-};
     
-    std::string show(const RepoAuthType::Array& ar) {
-  auto ret = std::string{};
-    }
-    
-    
-    {  return GetRusageMicros(m_type, Timer::Self);
+    inline grpc::string GetJSServiceFilename(const grpc::string& filename) {
+  return grpc_generator::StripProto(filename) + '_grpc_pb.js';
 }
-    
-    ///////////////////////////////////////////////////////////////////////////////
-    
-    #ifndef incl_HPHP_PAGELET_SERVER_H_
-#define incl_HPHP_PAGELET_SERVER_H_
-    
-    
-    {  return false;
-}
-    
-    /*
- * Block of Vinstrs, managed by Vunit.
- *
- * A Vblock, in addition to containing a Vinstr stream, also specifies where it
- * should be emitted to.
- */
-struct Vblock {
-  explicit Vblock(AreaIndex area_idx, uint64_t w)
-    : area_idx(area_idx)
-    , weight(w) {}
-    }
-    
-    
-    {  // Finally, for each arc whose weight is still unknown at this point, we
-  // currently just approximate it as half of the smallest weight of its
-  // incident blocks.
-  for (auto b : m_blocks) {
-    auto succSet = succs(m_unit.blocks[b]);
-    for (auto s : succSet) {
-      auto arcid = arcId(b, s);
-      if (m_arcWgts[arcid] == kUnknownWeight) {
-        m_arcWgts[arcid] = std::min(weight(b), weight(s)) / 2;
-        FTRACE(3, '  - arc({} -> {}) [guessed] => weight = {}\n',
-               b, s, m_arcWgts[arcid]);
-      }
-    }
-  }
-}
-    
-    /*
- * Passes.
- */
-void allocateRegisters(Vunit&, const Abi&);
-void annotateSFUses(Vunit&);
-void fuseBranches(Vunit&);
-void optimizeCopies(Vunit&, const Abi&);
-void optimizeExits(Vunit&);
-void optimizeJmps(Vunit&);
-void optimizePhis(Vunit&);
-void removeDeadCode(Vunit&);
-void removeTrivialNops(Vunit&);
-void reuseImmq(Vunit&);
-template<typename Folder> void foldImms(Vunit&);
-void simplify(Vunit&);
-    
-      PageletTask(const String& url, const Array& headers, const String& post_data,
-              const String& remote_host,
-              const std::set<std::string> &rfc1867UploadedFiles,
-              const Array& files, int timeoutSeconds) {
-    m_job = new PageletTransport(url, headers, remote_host, post_data,
-                                 rfc1867UploadedFiles, files, timeoutSeconds);
-    m_job->incRefCount();
-  }
-    
-    void initNuma();
-    
-      // If we somehow reach this point and both gname and gid were
-  // passed, then the gid values will override the gname values,
-  // but it will otherwise function just fine.
-  // The assert() clause above should prevent that, however.
-  if ((gname.size() > 0) &&
-      (getgrnam_r(gname.data(), &gr, grbuf.get(), grbuflen, &retgrptr) != 0 ||
-      retgrptr == nullptr)) {
-    return false;
-  } else if ((gid >= 0) &&
-      (getgrgid_r(gid, &gr, grbuf.get(), grbuflen, &retgrptr) != 0 ||
-      retgrptr == nullptr)) {
-    return false;
-  }
-    
-      // Unfortunately, Linux AIO doesn't implement io_cancel, so even for
-  // WaitType::CANCEL we have to wait for IO completion.
-  size_t count = 0;
-  do {
-    int ret;
-    do {
-      // GOTCHA: io_getevents() may returns less than min_nr results if
-      // interrupted after some events have been read (if before, -EINTR
-      // is returned).
-      ret = io_getevents(
-          ctx_,
-          minRequests - count,
-          maxRequests - count,
-          events + count,
-          /* timeout */ nullptr); // wait forever
-    } while (ret == -EINTR);
-    // Check as may not be able to recover without leaking events.
-    CHECK_GE(ret, 0) << 'AsyncIO: io_getevents failed with error '
-                     << errnoStr(-ret);
-    count += ret;
-  } while (count < minRequests);
-  DCHECK_LE(count, maxRequests);
-    
-    path remove_prefix(const path& pth, const path& prefix) {
-  path::const_iterator it;
-  if (!skipPrefix(pth, prefix, it)) {
-    throw filesystem_error(
-        'Path does not start with prefix',
-        pth,
-        prefix,
-        bsys::errc::make_error_code(bsys::errc::invalid_argument));
-  }
-    }
-    
-    namespace {
-    }
-    
-      ~AsyncFileWriter();
-    
-    
-    {} // namespace folly
-
-    
-    
-    {
-    {  folly::File file_;
-};
-} // namespace folly
-
-    
-    
-    {  // Update categoryConfigs_ with all of the entries from the other LogConfig.
-  //
-  // Any entries already present in our categoryConfigs_ are merged: if the new
-  // configuration does not include handler settings our entry's settings are
-  // maintained.
-  for (const auto& entry : other.categoryConfigs_) {
-    auto result = categoryConfigs_.insert(entry);
-    if (!result.second) {
-      auto* existingEntry = &result.first->second;
-      auto oldHandlers = std::move(existingEntry->handlers);
-      *existingEntry = entry.second;
-      if (!existingEntry->handlers.hasValue()) {
-        existingEntry->handlers = std::move(oldHandlers);
-      }
-    }
-  }
-}
-    
-      const CategoryConfigMap& getCategoryConfigs() const {
-    return categoryConfigs_;
-  }
-  const HandlerConfigMap& getHandlerConfigs() const {
-    return handlerConfigs_;
-  }
-    
-    /**
- * Parse a folly::dynamic object.
- *
- * The input should be an object data type, and is parsed the same as a JSON
- * object accpted by parseLogConfigJson().
- */
-LogConfig parseLogConfigDynamic(const dynamic& value);
-    
-    
-    {    auto status = stub_->SayHello(&context, request_msg, &response_msg);
-    if (status.ok()) {
-      const HelloReply *response = response_msg.GetRoot();
-      return response->message()->str();
-    } else {
-      std::cerr << status.error_code() << ': ' << status.error_message()
-                << std::endl;
-      return 'RPC failed';
-    }
-  }
-    
-    inline flatbuffers::Offset<EnumVal> CreateEnumValDirect(
-    flatbuffers::FlatBufferBuilder &_fbb,
-    const char *name = nullptr,
-    int64_t value = 0,
-    flatbuffers::Offset<Object> object = 0,
-    flatbuffers::Offset<Type> union_type = 0) {
-  return reflection::CreateEnumVal(
-      _fbb,
-      name ? _fbb.CreateString(name) : 0,
-      value,
-      object,
-      union_type);
-}
-    
-      if (!file->package().empty()) {
-    std::vector<grpc::string> parts = file->package_parts();
-    }
-    
-    // Abort the program after logging the mesage.
-#define GRPC_CODEGEN_FAIL GRPC_CODEGEN_CHECK(false)
-    
-      // here, parser.builder_ contains a binary buffer that is the parsed data.
-    
-    // Get the value of a vector's struct member.
-static void GetMemberOfVectorOfStruct(const StructDef &struct_def,
-                                      const FieldDef &field,
-                                      std::string *code_ptr) {
-  std::string &code = *code_ptr;
-  auto vectortype = field.value.type.VectorType();
-    }
-    
-    
-    {  for (size_t F = 0; F < kNumFlags; F++) {
-    const auto &D = FlagDescriptions[F];
-    if (strstr(D.Description, 'internal flag') == D.Description) continue;
-    Printf(' %s', D.Name);
-    for (size_t i = 0, n = MaxFlagLen - strlen(D.Name); i < n; i++)
-      Printf(' ');
-    Printf('\t');
-    Printf('%d\t%s\n', D.Default, D.Description);
-  }
-  Printf('\nFlags starting with '--' will be ignored and '
-            'will be passed verbatim to subprocesses.\n');
-}
-    
-      static void StaticAlarmCallback();
-  static void StaticCrashSignalCallback();
-  static void StaticInterruptCallback();
-    
-      struct Mutator {
-    size_t (MutationDispatcher::*Fn)(uint8_t *Data, size_t Size, size_t Max);
-    const char *Name;
-  };
-    
-    std::string Sha1ToString(const uint8_t Sha1[kSHA1NumBytes]);
-    
-    void TracePC::HandleCallerCallee(uintptr_t Caller, uintptr_t Callee) {
-  const uintptr_t kBits = 12;
-  const uintptr_t kMask = (1 << kBits) - 1;
-  uintptr_t Idx = (Caller & kMask) | ((Callee & kMask) << kBits);
-  HandleValueProfile(Idx);
-}
-    
-      void AddMutation(uint32_t Pos, uint32_t Size, const uint8_t *Data) {
-    if (NumMutations >= kMaxMutations) return;
-    auto &M = Mutations[NumMutations++];
-    M.Pos = Pos;
-    M.W.Set(Data, Size);
-  }
-    
-    #endif // D_A2_STR_H
-
-    
-    AbstractAuthResolver::AbstractAuthResolver() {}
-    
-      virtual PrefPtr getPref() const CXX11_OVERRIDE { return pref_; }
-    
-    class ActivePeerConnectionCommand : public Command {
-private:
-  RequestGroup* requestGroup_;
-  std::shared_ptr<BtRuntime> btRuntime_;
-  std::shared_ptr<PieceStorage> pieceStorage_;
-  std::shared_ptr<PeerStorage> peerStorage_;
-  std::shared_ptr<BtAnnounce> btAnnounce_;
-    }
-    
-    #include 'Notifier.h'
-    
-    void configureAsyncNameResolverMan(AsyncNameResolverMan* asyncNameResolverMan,
-                                   Option* option);
