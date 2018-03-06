92baeb41fc35de63e0c1f8642a485f7dd8aa457c
SUNsung <weatherius@gmail.com>
2018-03-06 06:13:25 +0200

    Rewrite [534goTbXo92lw53BAI8Pdu8xPo53APwI.6ihVQU53nt.py] File in: 2018-03-06 06:13:24

--- pyton/534goTbXo92lw53BAI8Pdu8xPo53APwI.6ihVQU53nt.py
+++ pyton/534goTbXo92lw53BAI8Pdu8xPo53APwI.6ihVQU53nt.py
@@ -1,91 +1,46 @@
 
         
-        try:
-    import json
-except ImportError:
-    import simplejson as json
-import os
-import os.path
-import sys
-import ConfigParser
-import StringIO
+        
+@pytest.fixture
+def httpbin(httpbin):
+    return prepare_url(httpbin)
     
-    # The checksum algorithm must match with the algorithm in ShellModule.checksum() method
-checksum = secure_hash
-checksum_s = secure_hash_s
-    
-            if not args.test:
-            with open(path, 'w') as metadata_fd:
-                metadata_fd.write(contents)
-    
-        indent = None
-    if format:
-        indent = 4
-    
-        @g_connect
-    def __call_galaxy(self, url, args=None, headers=None, method=None):
-        if args and not headers:
-            headers = self.__auth_header()
-        try:
-            display.vvv(url)
-            resp = open_url(url, data=args, validate_certs=self._validate_certs, headers=headers, method=method,
-                            timeout=20)
-            data = json.loads(to_text(resp.read(), errors='surrogate_or_strict'))
-        except HTTPError as e:
-            res = json.loads(to_text(e.fp.read(), errors='surrogate_or_strict'))
-            raise AnsibleError(res['detail'])
-        return data
-    
-        def on_open_shell(self):
-        try:
-            for cmd in (b'set terminal length 0', b'set terminal width 512'):
-                self._exec_cli_command(cmd)
-            self._exec_cli_command(b'set terminal length %d' % self.terminal_length)
-        except AnsibleConnectionFailure:
-            raise AnsibleConnectionFailure('unable to set terminal parameters')
-
-    
-        def test_display_current_time_at_midnight(self):
-        '''
-        Would almost always fail (despite of right at/after midnight) if
-        untestable production code would have been used.
-        '''
-        time_provider_stub = MidnightTimeProvider()
-        class_under_test = TimeDisplay()
-        class_under_test.set_time_provider(time_provider_stub)
-        expected_time = '<span class=\'tinyBoldText\'>24:01</span>'
-        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
+        @pytest.fixture(autouse=True)
+    def setup(self):
+        '''LookupDict instance with 'bad_gateway' attribute.'''
+        self.lookup_dict = LookupDict('test')
+        self.lookup_dict.bad_gateway = 502
     
     
-def count_to(count):
-    '''Counts by word numbers, up to a maximum of five'''
-    numbers = ['one', 'two', 'three', 'four', 'five']
-    for number in numbers[:count]:
-        yield number
+def test_idna_without_version_attribute(mocker):
+    '''Older versions of IDNA don't provide a __version__ attribute, verify
+    that if we have such a package, we don't blow up.
+    '''
+    mocker.patch('requests.help.idna', new=None)
+    assert info()['idna'] == {'version': ''}
     
-        def setTM(self, tm):
-        self._tm = tm
     
-        # 根据分辨率查找配置文件
-    if os.path.exists(config_file):
-        with open(config_file, 'r') as f:
-            print('正在从 {} 加载配置文件'.format(config_file))
-            return json.load(f)
-    else:
-        with open('{}/config/default.json'.format(sys.path[0]), 'r') as f:
-            print('Load default config')
-            return json.load(f)
+class RequestException(IOError):
+    '''There was an ambiguous exception that occurred while handling your
+    request.
+    '''
     
-        print(round(target_distance), round(jump_error_value), round(actual_distance), round(last_press_time))
-    ''''# 将结果采集进学习字典
-    if last_piece_x > 0 and last_press_time > 0:
-        ai.add_data(round(actual_distance, 2), round(last_press_time))
-        # print(round(actual_distance), round(last_press_time))'''
+    Some codes have multiple names, and both upper- and lower-case versions of
+the names are allowed. For example, ``codes.ok``, ``codes.OK``, and
+``codes.okay`` all correspond to the HTTP status code 200.
+'''
     
-        # 寻找落点 ◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆
-    board_x = 0
-    # 限制棋盘扫描的横坐标 避免音符bug
-    if piece_x < w / 2:
-        board_x_start, board_x_end = w // 2, w  # 起点和终点的中点是画面中心
-    else:
-        board_x_start, board_x_end = 0, w // 2
+            if (not has_actual_code
+            and not line.strip().startswith('//')
+            and not line.strip().startswith('???')
+            and not line.strip() == ''):
+            has_actual_code = True
+    
+    def shorten_title(title):
+    m1 = re.search('[[0-9]*]', title)
+    m2 = re.search(''.*'', title)
+    if m1:
+        title = m1.group(0)
+    if m2:
+        title = ' '.join((title, m2.group(0)))   
+    return title[:50] + ' [...]'    
