63861ba1dc9ba8dd11e6f4921a87efd340517f6c
SUNsung <weatherius@gmail.com>
2018-03-06 09:58:35 +0200

    Rewrite [24rY3zWRXCUFc24czSxMo6XTc241VoGmNhVMOY24Jpj.py] File in: 2018-03-06 09:58:34

--- pyton/24rY3zWRXCUFc24czSxMo6XTc241VoGmNhVMOY24Jpj.py
+++ pyton/24rY3zWRXCUFc24czSxMo6XTc241VoGmNhVMOY24Jpj.py
@@ -1,106 +1,119 @@
 
         
-        
-if __name__ == '__main__':
-    SpendingByCategory.run()
-
+        entries_str = textwrap.indent(''.join(entries), '\t')
+atom_template = atom_template.replace('@ENTRIES@', entries_str)
     
-        Code is a simple port of what is already in the /scripts directory
+    # Allow direct execution
+import os
+import sys
+import unittest
+import collections
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     
-        def test_create_other_buttons(self):
-        for state in (False, True):
-            var = self.engine.backvar
-            var.set(state)
-            frame, others = self.btn_test_setup(
-                self.dialog.create_other_buttons)
-            buttons = frame.pack_slaves()
-            for spec, button in zip(others, buttons):
-                val, label = spec
-                self.assertEqual(button['text'], label)
-                if val == state:
-                    # hit other button, then this one
-                    # indexes depend on button order
-                    self.assertEqual(var.get(), state)
+            webpage = self._download_webpage(url, playlist_id)
+        title = self._html_search_regex(
+            r'<h1 class='playlist-name'[^>]*?>(.*?)</h1>', webpage, 'title')
+        description = self._html_search_regex(
+            r'<p class='excerpt'[^>]*?>(.*?)</p>',
+            webpage, 'description', fatal=False)
+        urls = re.findall(
+            r'<li class='lecture-preview'>\s*?<a target='_blank' href='([^']+)'>',
+            webpage)
+        entries = [self.url_result(u) for u in urls]
     
-    def _ss(data, c=None):
-    '''Return sum of square deviations of sequence data.
+            duration = parse_duration(self._search_regex(
+            r'<b>Duration:</b> (?:<q itemprop='duration'>)?(\d+:\d+)', webpage, 'duration', fatal=False))
+        view_count = int_or_none(self._html_search_regex(
+            r'<b>Views:</b> (\d+)', webpage, 'view count', fatal=False))
     
-    class TestPyDecode(TestDecode, PyTest): pass
-class TestCDecode(TestDecode, CTest): pass
-
+        _TESTS = [{
+        'url': 'http://camwithher.tv/view_video.php?viewkey=6e9a24e2c0e842e1f177&page=&viewtype=&category=',
+        'info_dict': {
+            'id': '5644',
+            'ext': 'flv',
+            'title': 'Periscope Tease',
+            'description': 'In the clouds teasing on periscope to my favorite song',
+            'duration': 240,
+            'view_count': int,
+            'comment_count': int,
+            'uploader': 'MileenaK',
+            'upload_date': '20160322',
+        },
+        'params': {
+            'skip_download': True,
+        }
+    }, {
+        'url': 'http://camwithher.tv/view_video.php?viewkey=6dfd8b7c97531a459937',
+        'only_matching': True,
+    }, {
+        'url': 'http://camwithher.tv/view_video.php?page=&viewkey=6e9a24e2c0e842e1f177&viewtype=&category=',
+        'only_matching': True,
+    }, {
+        'url': 'http://camwithher.tv/view_video.php?viewkey=b6c3b5bea9515d1a1fc4&page=&viewtype=&category=mv',
+        'only_matching': True,
+    }]
     
-        - linejunk: A function that should accept a single string argument and
-      return true iff the string is junk.  The default is None, and is
-      recommended; the underlying SequenceMatcher class has an adaptive
-      notion of 'noise' lines.
+        def _real_extract(self, url):
+        display_id = self._match_id(url)
     
-        '''
-    # Note that this exception is used from _json
-    def __init__(self, msg, doc, pos):
-        lineno = doc.count('\n', 0, pos) + 1
-        colno = pos - doc.rfind('\n', 0, pos)
-        errmsg = '%s: line %d column %d (char %d)' % (msg, lineno, colno, pos)
-        ValueError.__init__(self, errmsg)
-        self.msg = msg
-        self.doc = doc
-        self.pos = pos
-        self.lineno = lineno
-        self.colno = colno
+    import ssl
     
-    __all__ = ['make_scanner']
+    def kugou_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    if url.lower().find('5sing')!=-1:
+        #for 5sing.kugou.com
+        html=get_html(url)
+        ticket=r1(r''ticket':\s*'(.*)'',html)
+        j=loads(str(b64decode(ticket),encoding='utf-8'))
+        url=j['file']
+        title=j['songName']
+        songtype, ext, size = url_info(url)
+        print_info(site_info, title, songtype, size)
+        if not info_only:
+            download_urls([url], title, ext, size, output_dir, merge=merge)
+    else:
+        #for the www.kugou.com/
+        return kugou_download_playlist(url, output_dir=output_dir, merge=merge, info_only=info_only)
+        # raise NotImplementedError(url)       
     
-        def _generate_toc(self):
-        '''Generate key-to-(start, stop) table of contents.'''
-        starts, stops = [], []
-        self._file.seek(0)
-        next_pos = 0
-        label_lists = []
-        while True:
-            line_pos = next_pos
-            line = self._file.readline()
-            next_pos = self._file.tell()
-            if line == b'\037\014' + linesep:
-                if len(stops) < len(starts):
-                    stops.append(line_pos - len(linesep))
-                starts.append(next_pos)
-                labels = [label.strip() for label
-                                        in self._file.readline()[1:].split(b',')
-                                        if label.strip()]
-                label_lists.append(labels)
-            elif line == b'\037' or line == b'\037' + linesep:
-                if len(stops) < len(starts):
-                    stops.append(line_pos - len(linesep))
-            elif not line:
-                stops.append(line_pos - len(linesep))
-                break
-        self._toc = dict(enumerate(zip(starts, stops)))
-        self._labels = dict(enumerate(label_lists))
-        self._next_key = len(self._toc)
-        self._file.seek(0, 2)
-        self._file_length = self._file.tell()
+    from ..common import *
+import re
     
-        def head(self, message_spec=None, *, file=None):
-        '''Process a HEAD command.  Argument:
-        - message_spec: article number or message id
-        - file: filename string or file object to store the headers in
-        Returns:
-        - resp: server response if successful
-        - ArticleInfo: (article number, message id, list of header lines)
-        '''
-        if message_spec is not None:
-            cmd = 'HEAD {0}'.format(message_spec)
-        else:
-            cmd = 'HEAD'
-        return self._artcmd(cmd, file)
+    from .tudou import tudou_download_by_id
+from .youku import youku_download_by_vid
+from xml.dom.minidom import parseString
     
-    subsystem_details = {
-    # -s flag        : (C entry point template), (is it __main__?), (is it a DLL?)
-    'console'        : (None,                    1,                 0),
-    'windows'        : (WINMAINTEMPLATE,         1,                 0),
-    'service'        : (SERVICETEMPLATE,         0,                 0),
-    'com_dll'        : ('',                      0,                 1),
-}
+        # mgid%3Auma%3Avideo%3Amtv81.com%3A897974
+    vid = match1(html, r'getTheVideo\('(.*?)'')
+    xml = parseString(
+        get_content('http://intl.esperanto.mtvi.com/www/xml/media/mediaGen.jhtml?uri={}&flashPlayer=LNX%2013,0,0,206&geo=CN&sid=123456'.format(vid)))
     
-        X_RGB_TXT -- X Consortium rgb.txt format files.  Three columns of numbers
-                 from 0 .. 255 separated by whitespace.  Arbitrary trailing
-                 columns used as the color name.
+    def shorten_title(title):
+    m1 = re.search('[[0-9]*]', title)
+    m2 = re.search(''.*'', title)
+    if m1:
+        title = m1.group(0)
+    if m2:
+        title = ' '.join((title, m2.group(0)))   
+    return title[:50] + ' [...]'    
+    
+        config = PylonsConfig()
+    
+        @require_oauth2_scope('identity')
+    @validate(
+        VUser(),
+        fields=VList(
+            'fields',
+            choices=PREFS_JSON_SPEC.spec.keys(),
+            error=errors.errors.NON_PREFERENCE,
+        ),
+    )
+    @api_doc(api_section.account, uri='/api/v1/me/prefs')
+    def GET_prefs(self, fields):
+        '''Return the preference settings of the logged in user'''
+        resp = PrefsJsonTemplate(fields).data(c.oauth_user)
+        return self.api_wrapper(resp)
+    
+            res = AdminPage(content = AdminAwardGive(award, recipient, desc,
+                                                 url, hours),
+                        title='give an award').render()
+        return res
