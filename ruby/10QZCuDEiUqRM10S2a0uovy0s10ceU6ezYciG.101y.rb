04937e22bfa11ee76ffcc801d24f3f9a8ca6996b
SUNsung <weatherius@gmail.com>
2018-03-06 19:16:06 +0200

    Rewrite [10QZCuDEiUqRM10S2a0uovy0s10ceU6ezYciG.101y.rb] File in: 2018-03-06 19:16:05

--- ruby/10QZCuDEiUqRM10S2a0uovy0s10ceU6ezYciG.101y.rb
+++ ruby/10QZCuDEiUqRM10S2a0uovy0s10ceU6ezYciG.101y.rb
@@ -1,47 +1,28 @@
 
         
-          def teardown
-    ActiveSupport.escape_html_entities_in_json = @old_escape_html_entities_in_json
+                              until @when_connected.empty?
+                        @when_connected.shift.call
+                      end
+                    end
+    
+            def test_url_invalid_adapter
+          error = assert_raises(LoadError) do
+            spec 'ridiculous://foo?encoding=utf8'
+          end
+    
+        response = ActionDispatch::TestResponse.create(200, { 'Content-Type' => 'application/json' }, '{ 'foo': 'fighters' }')
+    assert_equal({ 'foo' => 'fighters' }, response.parsed_body)
   end
     
-                define_method('#{name}=') do |attribute|
-              attributes[name.to_sym] = attribute
-            end
-          end
-        end
-    
-          test 'when :except is specified with an array, an after action is not triggered on that action' do
-        @controller.process(:index)
-        assert !@controller.instance_variable_defined?('@authenticated')
-      end
+          assert_equal 'Hello world', string
     end
     
-      private
-    def with_params_parsers(parsers = {})
-      old_session = @integration_session
-      original_parsers = ActionDispatch::Request.parameter_parsers
-      ActionDispatch::Request.parameter_parsers = original_parsers.merge parsers
-      reset!
-      yield
-    ensure
-      ActionDispatch::Request.parameter_parsers = original_parsers
-      @integration_session = old_session
+        def call(env)
+      result = @app.call(env)
+      result[1]['Configurable-Message'] = configurable_message
+      result
     end
-    
-          private
-        def processed_mailer
-          @processed_mailer ||= @mailer_class.new.tap do |mailer|
-            mailer.params = @params
-            mailer.process @action, *@args
-          end
-        end
-    
-            def initialize_test_deliveries
-          set_delivery_method :test
-          @old_perform_deliveries = ActionMailer::Base.perform_deliveries
-          ActionMailer::Base.perform_deliveries = true
-          ActionMailer::Base.deliveries.clear
-        end
+  end
     
             def application_mailer_file_name
           @_application_mailer_file_name ||= if mountable_engine?
@@ -55,82 +36,26 @@
 end
 
     
-      test 'confirm time should fallback to devise confirm in default configuration' do
-    swap Devise, allow_unconfirmed_access_for: 1.day do
-      user = create_user
-      user.confirmation_sent_at = 2.days.ago
-      refute user.active_for_authentication?
+      def test_filter_bang
+    h1 = {}
+    ENV.each_pair {|k, v| h1[k] = v }
+    ENV['test'] = 'foo'
+    ENV.filter! {|k, v| IGNORE_CASE ? k.upcase != 'TEST' : k != 'test' }
+    h2 = {}
+    ENV.each_pair {|k, v| h2[k] = v }
+    assert_equal(h1, h2)
     
-    module Devise
-  module Controllers
-    # Create url helpers to be used with resource/scope configuration. Acts as
-    # proxies to the generated routes created by devise.
-    # Resource param can be a string or symbol, a class, or an instance object.
-    # Example using a :user resource:
-    #
-    #   new_session_path(:user)      => new_user_session_path
-    #   session_path(:user)          => user_session_path
-    #   destroy_session_path(:user)  => destroy_user_session_path
-    #
-    #   new_password_path(:user)     => new_user_password_path
-    #   password_path(:user)         => user_password_path
-    #   edit_password_path(:user)    => edit_user_password_path
-    #
-    #   new_confirmation_path(:user) => new_user_confirmation_path
-    #   confirmation_path(:user)     => user_confirmation_path
-    #
-    # Those helpers are included by default to ActionController::Base.
-    #
-    # In case you want to add such helpers to another class, you can do
-    # that as long as this new class includes both url_helpers and
-    # mounted_helpers. Example:
-    #
-    #     include Rails.application.routes.url_helpers
-    #     include Rails.application.routes.mounted_helpers
-    #
-    module UrlHelpers
-      def self.remove_helpers!
-        self.instance_methods.map(&:to_s).grep(/_(url|path)$/).each do |method|
-          remove_method method
-        end
-      end
-    
-      def setup
-    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
-    success = Dir.chdir DUMMY_PATH do
-      silence_stdout_if !ENV['VERBOSE'] do
-        system 'node', 'manifest.js', tmp_dir
-      end
-    end
-    assert success, 'Node.js Mincer compilation failed'
-    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
-    css_name = manifest['assets']['application.css']
-    @css = File.read('#{tmp_dir}/#{css_name}')
+      it 'decodes UTF-8 max codepoints' do
+    [ ['\xf0\x90\x80\x80', [0x10000]],
+      ['\xf3\xbf\xbf\xbf', [0xfffff]],
+      ['\xf4\x80\x80\x80', [0x100000]],
+      ['\xf4\x8f\xbf\xbf', [0x10ffff]]
+    ].should be_computed_by(:unpack, 'U')
   end
-end
-
     
-            # Prints the list of specs & pod cache dirs for a single pod name.
-        #
-        # This output is valid YAML so it can be parsed with 3rd party tools
-        #
-        # @param [Array<Hash>] cache_descriptors
-        #        The various infos about a pod cache. Keys are
-        #        :spec_file, :version, :release and :slug
-        #
-        def print_pod_cache_infos(pod_name, cache_descriptors)
-          UI.puts '#{pod_name}:'
-          cache_descriptors.each do |desc|
-            if @short_output
-              [:spec_file, :slug].each { |k| desc[k] = desc[k].relative_path_from(@cache.root) }
-            end
-            UI.puts('  - Version: #{desc[:version]}')
-            UI.puts('    Type:    #{pod_type(desc)}')
-            UI.puts('    Spec:    #{desc[:spec_file]}')
-            UI.puts('    Pod:     #{desc[:slug]}')
-          end
-        end
-      end
-    end
+      def self.wakeup_dying_sleeping_thread(kill_method_name=:kill)
+    t = ThreadSpecs.dying_thread_ensures(kill_method_name) { yield }
+    Thread.pass while t.status and t.status != 'sleep'
+    t.wakeup
+    t.join
   end
-end
