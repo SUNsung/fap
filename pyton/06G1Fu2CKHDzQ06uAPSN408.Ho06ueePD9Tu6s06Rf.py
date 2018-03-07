29690e26b41d32264a4c47fe4d385ec0f20effbb
SUNsung <weatherius@gmail.com>
2018-03-07 02:52:33 +0200

    Rewrite [06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py] File in: 2018-03-07 02:52:32

--- pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
+++ pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
@@ -1,104 +1,51 @@
-    def move_to_front(self, node):
-        ...
+
+        
+        with open('update/LATEST_VERSION', 'w') as f:
+    f.write(version)
+    
+            webpage = self._download_webpage(url, playlist_id)
+        title = self._html_search_regex(
+            r'<h1 class='playlist-name'[^>]*?>(.*?)</h1>', webpage, 'title')
+        description = self._html_search_regex(
+            r'<p class='excerpt'[^>]*?>(.*?)</p>',
+            webpage, 'description', fatal=False)
+        urls = re.findall(
+            r'<li class='lecture-preview'>\s*?<a target='_blank' href='([^']+)'>',
+            webpage)
+        entries = [self.url_result(u) for u in urls]
+    
+            flv_id = self._html_search_regex(
+            r'<a[^>]+href=['\']/download/\?v=(\d+)', webpage, 'video id')
     
     
-class PollErrorTest(unittest.TestCase):
-    '''Tests for acme.errors.PollError.'''
-    
-            self.addr1 = Addr.fromstring('127.0.0.1')
-        self.addr2 = Addr.fromstring('127.0.0.1:443')
-        self.addr_default = Addr.fromstring('_default_:443')
-    
-            cert = os.path.join(cert_and_key_dir, 'cert')
-        shutil.copy(cert_path, cert)
-        key = os.path.join(cert_and_key_dir, 'key')
-        shutil.copy(key_path, key)
-        if chain_path:
-            chain = os.path.join(cert_and_key_dir, 'chain')
-            shutil.copy(chain_path, chain)
-        else:
-            chain = None
-    
-        @mock.patch('certbot.notify.smtplib.LMTP')
-    def test_smtp_success(self, mock_lmtp):
-        from certbot.notify import notify
-        lmtp_obj = mock.MagicMock()
-        mock_lmtp.return_value = lmtp_obj
-        self.assertTrue(notify('Goose', 'auntrhody@example.com',
-                               'The old grey goose is dead.'))
-        self.assertEqual(lmtp_obj.connect.call_count, 1)
-        self.assertEqual(lmtp_obj.sendmail.call_count, 1)
-    
-    # If true, '()' will be appended to :func: etc. cross-reference text.
-#add_function_parentheses = True
-    
-            slc = Slice(
-            slice_name='Calendar Heatmap multiformat ' + str(i),
-            viz_type='cal_heatmap',
-            datasource_type='table',
-            datasource_id=tbl.id,
-            params=get_slice_json(slice_data),
-        )
-        merge_slice(slc)
-    misc_dash_slices.append(slc.slice_name)
-    
-    # static http headers to be served by your Superset server.
-# This header prevents iFrames from other domains and
-# 'clickjacking' as a result
-HTTP_HEADERS = {'X-Frame-Options': 'SAMEORIGIN'}
-# If you need to allow iframes from other domains (and are
-# aware of the risks), you can disable this header:
-# HTTP_HEADERS = {}
-    
-        def post_update(self, datasource):
-        self.post_add(datasource)
-    
-    import os, json, imp
-here = os.path.abspath(os.path.dirname(__file__))
-proj_info = json.loads(open(os.path.join(here, PROJ_METADATA), encoding='utf-8').read())
-try:
-    README = open(os.path.join(here, 'README.rst'), encoding='utf-8').read()
-except:
-    README = ''
-CHANGELOG = open(os.path.join(here, 'CHANGELOG.rst'), encoding='utf-8').read()
-VERSION = imp.load_source('version', os.path.join(here, 'src/%s/version.py' % PACKAGE_NAME)).__version__
-    
-    import json
-    
-            # extract raw urls
-        orig_img = match1(content,
-                         r'<meta itemprop='image' content='([^']+/originals/[^']+)'')
-        twit_img = match1(content,
-                          r'<meta property='twitter:image:src' name='twitter:image:src' content='([^']+)'')
-    
-        def test_human_adapter_shall_make_noise(self):
-        human = Human()
-        human_adapter = Adapter(human, make_noise=human.speak)
-        noise = human_adapter.make_noise()
-        expected_noise = ''hello''
-        self.assertEqual(noise, expected_noise)
-    
-        message_center.update()
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user_id' in session:
+        g.user = query_db('select * from user where user_id = ?',
+                          [session['user_id']], one=True)
     
     
-class ConcreteHandler3(Handler):
+def test_register(client):
+    '''Make sure registering works'''
+    rv = register(client, 'user1', 'default')
+    assert b'You were successfully registered ' \
+           b'and can login now' in rv.data
+    rv = register(client, 'user1', 'default')
+    assert b'The username is already taken' in rv.data
+    rv = register(client, '', 'default')
+    assert b'You have to enter a username' in rv.data
+    rv = register(client, 'meh', '')
+    assert b'You have to enter a password' in rv.data
+    rv = register(client, 'meh', 'x', 'y')
+    assert b'The two passwords do not match' in rv.data
+    rv = register(client, 'meh', 'foo', email='broken')
+    assert b'You have to enter a valid email address' in rv.data
     
-            # they can be executed later on
-        for cmd in command_stack:
-            cmd.execute()
-    
-        print(u'Setting Data 1 = 10')
-    data1.data = 10
-    print(u'Setting Data 2 = 15')
-    data2.data = 15
-    print(u'Setting Data 1 = 3')
-    data1.data = 3
-    print(u'Setting Data 2 = 5')
-    data2.data = 5
-    print(u'Detach HexViewer from data1 and data2.')
-    data1.detach(view2)
-    data2.detach(view2)
-    print(u'Setting Data 1 = 10')
-    data1.data = 10
-    print(u'Setting Data 2 = 15')
-    data2.data = 15
+        def deploy_cert(self, domain, cert_path, key_path, chain_path=None,
+                    fullchain_path=None):
+        '''Installs cert'''
+        cert_path, key_path, chain_path = self.copy_certs_and_keys(
+            cert_path, key_path, chain_path)
+        self._configurator.deploy_cert(
+            domain, cert_path, key_path, chain_path, fullchain_path)
