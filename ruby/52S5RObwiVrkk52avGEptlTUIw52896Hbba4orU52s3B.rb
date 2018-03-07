bfb43572d202554c661c5cc4ceeaf2b1fa8edab9
SUNsung <weatherius@gmail.com>
2018-03-07 23:37:01 +0200

    Rewrite [52S5RObwiVrkk52avGEptlTUIw52896Hbba4orU52s3B.rb] File in: 2018-03-07 23:37:00

--- ruby/52S5RObwiVrkk52avGEptlTUIw52896Hbba4orU52s3B.rb
+++ ruby/52S5RObwiVrkk52avGEptlTUIw52896Hbba4orU52s3B.rb
@@ -1,31 +1,40 @@
 
         
-          # Make this VM reachable on the host network as well, so that other
-  # VM's running other browsers can access our dev server.
-  config.vm.network :private_network, ip: '192.168.10.200'
+        end
+
     
-          it 'renders HTML' do
-        expect(render(options, screenshots)).to match(/<html>/)
-      end
+        def execute(args)
+    
+      def include_admin_route?
+    admin_route.present?
+  end
+end
+
+    
+      def failed_strategy
+    request.respond_to?(:get_header) ? request.get_header('omniauth.error.strategy') : request.env['omniauth.error.strategy']
+  end
+    
+        # The path used after sending unlock password instructions
+    def after_sending_unlock_instructions_path_for(resource)
+      new_session_path(resource) if is_navigational_format?
     end
     
-        def groups=(groups)
-      @groups = groups ? groups.compact : nil
-    end
+      module Controllers
+    autoload :Helpers,        'devise/controllers/helpers'
+    autoload :Rememberable,   'devise/controllers/rememberable'
+    autoload :ScopedViews,    'devise/controllers/scoped_views'
+    autoload :SignInOut,      'devise/controllers/sign_in_out'
+    autoload :StoreLocation,  'devise/controllers/store_location'
+    autoload :UrlHelpers,     'devise/controllers/url_helpers'
+  end
     
-          script_array = crash_script.shell_script.split('\n').find { |line| includes_run_script?(line) }.split(' ')
-      if script_array.count == 3
-        results.merge!({
-          # The run script build phase probably refers to Fabric.framework/run, but the submit binary
-          # only lives in the Crashlytics.framework, so we'll substitute and try to resolve it that way.
-          crashlytics_path: File.dirname(script_array[0].gsub('Fabric.framework', 'Crashlytics.framework')),
-          api_key: script_array[1],
-          build_secret: script_array[2]
-        })
-      end
+            context = router_name ? send(router_name) : self
     
-            expect(result).to eq('/usr/local/bin/cloc  --by-file --xml  --out=build/cloc.xml MyCoolApp')
-      end
-    
-            expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to match(/cd .* && agvtool new-marketing-version 1.1.0/)
+          # Remembers the given resource by setting up a cookie
+      def remember_me(resource)
+        return if request.env['devise.skip_storage']
+        scope = Devise::Mapping.find_scope!(resource)
+        resource.remember_me!
+        cookies.signed[remember_key(resource, scope)] = remember_cookie_values(resource)
       end
