6e208e83fbc54e567701583d8244ab55242de0be
SUNsung <weatherius@gmail.com>
2018-03-07 05:29:59 +0200

    Rewrite [39OGY9ehipqhg39hiazVMr3UU39RaBMcKMi44Y39Uip.cpp] File in: 2018-03-07 05:29:58

--- cccccc/39OGY9ehipqhg39hiazVMr3UU39RaBMcKMi44Y39Uip.cpp
+++ cccccc/39OGY9ehipqhg39hiazVMr3UU39RaBMcKMi44Y39Uip.cpp
@@ -1,195 +1,226 @@
 
         
-            cv::Mat ppi_A(3, 1, CV_64F);
-    for (int i = 0; i < N; ++i)
-    {
-        z.col(i).copyTo(z_i);
-        ppi_A = LeftMultVec(pp.col(i)) + A;
-        D += ppi_A.t() * ( eye - z_i*z_i.t() ) * ppi_A;
-    }
-    A.release();
-    
-    void CalibrateExtrinsics(InputArrayOfArrays objectPoints, InputArrayOfArrays imagePoints,
-                         const IntrinsicParams& param, const int check_cond,
-                         const double thresh_cond, InputOutputArray omc, InputOutputArray Tc);
-    
-    #if 0
-    
-    void cv::cuda::GpuMat::convertTo(OutputArray _dst, int rtype, double alpha, double beta, Stream& _stream) const
-{
-    (void) _dst;
-    (void) rtype;
-    (void) alpha;
-    (void) beta;
-    (void) _stream;
-    throw_no_cuda();
+        
+    {  // Calling ExecutorDone and PartialRunDone on the step_id should still only
+  // result in the callback being called once.
+  // This proves that the original PartialRun has been removed.
+  partial_run_mgr.PartialRunDone(
+      step_id, [&called](Status status) { called++; }, Status::OK());
+  partial_run_mgr.ExecutorDone(step_id, Status::OK());
+  EXPECT_EQ(1, called);
 }
     
-        hdr.cols = new_width;
-    hdr.flags = (hdr.flags & ~CV_MAT_CN_MASK) | ((new_cn - 1) << CV_CN_SHIFT);
-    
-    #undef cv_hal_LU32f
-#define cv_hal_LU32f lapack_LU32f
-#undef cv_hal_LU64f
-#define cv_hal_LU64f lapack_LU64f
-    
-      if (flags.bit (CHECK_DAWGS) &&
-    (word->best_choice->permuter () != SYSTEM_DAWG_PERM) &&
-    (word->best_choice->permuter () != FREQ_DAWG_PERM) &&
-    (word->best_choice->permuter () != USER_DAWG_PERM) &&
-    (word->best_choice->permuter () != NUMBER_PERM)) {
-    if (tessedit_adaption_debug) tprintf('word not in dawgs\n');
-    return FALSE;
-  }
-    
-    // Fixxht overview.
-// Premise: Initial estimate of x-height is adequate most of the time, but
-// occasionally it is incorrect. Most notable causes of failure are:
-// 1. Small caps, where the top of the caps is the same as the body text
-// xheight. For small caps words the xheight needs to be reduced to correctly
-// recognize the caps in the small caps word.
-// 2. All xheight lines, such as summer. Here the initial estimate will have
-// guessed that the blob tops are caps and will have placed the xheight too low.
-// 3. Noise/logos beside words, or changes in font size on a line. Such
-// things can blow the statistics and cause an incorrect estimate.
-// 4. Incorrect baseline. Can happen when 2 columns are incorrectly merged.
-// In this case the x-height is often still correct.
-//
-// Algorithm.
-// Compare the vertical position (top only) of alphnumerics in a word with
-// the range of positions in training data (in the unicharset).
-// See CountMisfitTops. If any characters disagree sufficiently with the
-// initial xheight estimate, then recalculate the xheight, re-run OCR on
-// the word, and if the number of vertical misfits goes down, along with
-// either the word rating or certainty, then keep the new xheight.
-// The new xheight is calculated as follows:ComputeCompatibleXHeight
-// For each alphanumeric character that has a vertically misplaced top
-// (a misfit), yet its bottom is within the acceptable range (ie it is not
-// likely a sub-or super-script) calculate the range of acceptable xheight
-// positions from its range of tops, and give each value in the range a
-// number of votes equal to the distance of its top from its acceptance range.
-// The x-height position with the median of the votes becomes the new
-// x-height. This assumes that most characters will be correctly recognized
-// even if the x-height is incorrect. This is not a terrible assumption, but
-// it is not great. An improvement would be to use a classifier that does
-// not care about vertical position or scaling at all.
-// Separately collect stats on shifted baselines and apply the same logic to
-// computing a best-fit shift to fix the error. If the baseline needs to be
-// shifted, but the x-height is OK, returns the original x-height along with
-// the baseline shift to indicate that recognition needs to re-run.
-    
-    #include 'ccstruct.h'
-    
-    
-    {  // We shouldn't try calculations if the characters are very short (for example
-  // for punctuation).
-  if (min_height > kBlnXHeight / 8 && height > 0) {
-    float result = height * kBlnXHeight * yscale / min_height;
-    *max_xht = result + kFinalPixelTolerance;
-    result = height * kBlnXHeight * yscale / max_height;
-    *min_xht = result - kFinalPixelTolerance;
-  }
-}
-    
-      // Return whether a given text line could be a first paragraph line according
-  // to this paragraph model.
-  bool ValidFirstLine(int lmargin, int lindent, int rindent, int rmargin) const;
-    
-        // Converts an angle in radians (from ICOORD::angle or FCOORD::angle) to a
-    // standard feature direction as an unsigned angle in 256ths of a circle
-    // measured anticlockwise from (-1, 0).
-    static uinT8 binary_angle_plus_pi(double angle);
-    // Inverse of binary_angle_plus_pi returns an angle in radians for the
-    // given standard feature direction.
-    static double angle_from_direction(uinT8 direction);
-    // Returns the point on the given line nearest to this, ie the point such
-    // that the vector point->this is perpendicular to the line.
-    // The line is defined as a line_point and a dir_vector for its direction.
-    // dir_vector need not be a unit vector.
-    FCOORD nearest_pt_on_line(const FCOORD& line_point,
-                              const FCOORD& dir_vector) const;
-    
-        // Setup inputs
-    ALLEGRO_KEYBOARD_STATE keys;
-    al_get_keyboard_state(&keys);
-    io.KeyCtrl = al_key_down(&keys, ALLEGRO_KEY_LCTRL) || al_key_down(&keys, ALLEGRO_KEY_RCTRL);
-    io.KeyShift = al_key_down(&keys, ALLEGRO_KEY_LSHIFT) || al_key_down(&keys, ALLEGRO_KEY_RSHIFT);
-    io.KeyAlt = al_key_down(&keys, ALLEGRO_KEY_ALT) || al_key_down(&keys, ALLEGRO_KEY_ALTGR);
-    io.KeySuper = al_key_down(&keys, ALLEGRO_KEY_LWIN) || al_key_down(&keys, ALLEGRO_KEY_RWIN);
-    
-                if (ImGui::Button('Button'))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text('counter = %d', counter);
-    
-    // Implemented features:
-//  [X] User texture binding. Use 'ID3D10ShaderResourceView*' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
-    
-    
-    {            ImGui::Text('Application average %.3f ms/frame (%.1f FPS)', 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-        }
-    
-    struct GLFWwindow;
-    
-        // We are using the OpenGL fixed pipeline to make the example code simpler to read!
-    // Setup render state: alpha-blending enabled, no face culling, no depth testing, scissor enabled, vertex/texcoord/color pointers, polygon fill.
-    GLint last_texture; glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
-    GLint last_polygon_mode[2]; glGetIntegerv(GL_POLYGON_MODE, last_polygon_mode);
-    GLint last_viewport[4]; glGetIntegerv(GL_VIEWPORT, last_viewport);
-    GLint last_scissor_box[4]; glGetIntegerv(GL_SCISSOR_BOX, last_scissor_box); 
-    glPushAttrib(GL_ENABLE_BIT | GL_COLOR_BUFFER_BIT | GL_TRANSFORM_BIT);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glDisable(GL_CULL_FACE);
-    glDisable(GL_DEPTH_TEST);
-    glEnable(GL_SCISSOR_TEST);
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-    glEnableClientState(GL_COLOR_ARRAY);
-    glEnable(GL_TEXTURE_2D);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    //glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound
-    
-        void        (*SetObjectField)(JNIEnv*, jobject, jfieldID, jobject);
-    void        (*SetBooleanField)(JNIEnv*, jobject, jfieldID, jboolean);
-    void        (*SetByteField)(JNIEnv*, jobject, jfieldID, jbyte);
-    void        (*SetCharField)(JNIEnv*, jobject, jfieldID, jchar);
-    void        (*SetShortField)(JNIEnv*, jobject, jfieldID, jshort);
-    void        (*SetIntField)(JNIEnv*, jobject, jfieldID, jint);
-    void        (*SetLongField)(JNIEnv*, jobject, jfieldID, jlong);
-    void        (*SetFloatField)(JNIEnv*, jobject, jfieldID, jfloat);
-    void        (*SetDoubleField)(JNIEnv*, jobject, jfieldID, jdouble);
-    
-    
-    {    void toJS(nbind::cbOutput expose) const
-    {
-        expose(unit, value);
-    }
-};
+    #endif  // TENSORFLOW_COMMON_RUNTIME_SESSION_FACTORY_H_
 
     
-      if (state->stackTrace.size() == state->stackTrace.capacity()) {
-    return _URC_END_OF_STACK;
+    class Cluster;
+struct GrapplerItem;
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    TEST_F(NodeDefBuilderTest, TypeListNoMin) {
+  Op(OpDefBuilder('TypeListNoMin').Input('a: T').Attr('T: list(type) >= 0'));
+    }
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+      ConvertUTF8toUTF32(&SourceNext, SourceStart + S.size(), &TargetStart, C + 1,
+                     llvm::lenientConversion);
+  if (TargetStart == C) {
+    // The source string contains an ill-formed subsequence at the end.
+    return S;
   }
     
-      ASSERT_TRUE(YGNodeLayoutGetHadOverflow(root));
+      IndexRecordWriter recordWriter(indexStorePath);
+  std::string error;
+  auto result = recordWriter.beginRecord(
+      Filename, record.hashRecord(), error, &outRecordFile);
+  switch (result) {
+  case IndexRecordWriter::Result::Failure:
+    diags->diagnose(SourceLoc(), diag::error_write_index_record, error);
+    return true;
+  case IndexRecordWriter::Result::AlreadyExists:
+    return false;
+  case IndexRecordWriter::Result::Success:
+    break;
+  }
     
-    void jni_YGNodeSetHasMeasureFunc(alias_ref<jobject>, jlong nativePointer, jboolean hasMeasureFunc) {
-  _jlong2YGNodeRef(nativePointer)
-      ->setMeasureFunc(hasMeasureFunc ? YGJNIMeasureFunc : nullptr);
+    class ParamField final : public PrivateExtension,
+    private llvm::TrailingObjects<ParamField, MarkupASTNode *> {
+  friend TrailingObjects;
+    }
+    
+      // The following strings are guaranteed to live at least as long as the
+  // current indexing action.
+  StringRef name;
+  StringRef USR; // USR may be safely compared by pointer.
+  StringRef group;
+    
+    
+    {  int getLineOffset(SourceLoc Loc) const {
+    if (auto VFile = getVirtualFile(Loc))
+      return VFile->LineOffset;
+    else
+      return 0;
+  }
+};
+    
+    #endif  // 0
+    
+      // Returns true if the death test passed; that is, the test process
+  // exited during the test, its exit status matches a user-supplied
+  // predicate, and its stderr output matches a user-supplied regular
+  // expression.
+  // The user-supplied predicate may be a macro expression rather
+  // than a function pointer or functor, or else Wait and Passed could
+  // be combined.
+  virtual bool Passed(bool exit_status_ok) = 0;
+    
+      // Returns a copy of the FilePath with the directory part removed.
+  // Example: FilePath('path/to/file').RemoveDirectoryName() returns
+  // FilePath('file'). If there is no directory part ('just_a_file'), it returns
+  // the FilePath unmodified. If there is no file part ('just_a_dir/') it
+  // returns an empty FilePath ('').
+  // On Windows platform, '\' is the path separator, otherwise it is '/'.
+  FilePath RemoveDirectoryName() const;
+    
+      // Compares two wide C strings, ignoring case.  Returns true iff they
+  // have the same content.
+  //
+  // Unlike wcscasecmp(), this function can handle NULL argument(s).
+  // A NULL C string is considered different to any non-NULL wide C string,
+  // including the empty string.
+  // NB: The implementations on different platforms slightly differ.
+  // On windows, this method uses _wcsicmp which compares according to LC_CTYPE
+  // environment variable. On GNU platform this method uses wcscasecmp
+  // which compares according to LC_CTYPE category of the current locale.
+  // On MacOS X, it uses towlower, which also uses LC_CTYPE category of the
+  // current locale.
+  static bool CaseInsensitiveWideCStringEquals(const wchar_t* lhs,
+                                               const wchar_t* rhs);
+    
+    template <>
+class tuple<> {
+ public:
+  tuple() {}
+  tuple(const tuple& /* t */)  {}
+  tuple& operator=(const tuple& /* t */) { return *this; }
+};
+    
+      // Now, we have that n is odd and n >= 3.
+    
+        FreeType font driver for pcf fonts
+    
+    # if defined(OC_COLLECT_METRICS)
+typedef struct oc_mode_metrics oc_mode_metrics;
+# endif
+typedef struct oc_mode_rd      oc_mode_rd;
+    
+    static const vorbis_info_residue0 _residue_44_high={
+  0,-1, -1, 10,-1,-1,
+  /* 0   1   2   3   4   5   6   7   8  */
+  {0},
+  {-1},
+  {  0,  1,  2,  4,  8, 16, 32, 71,157},
+  {  0,  1,  2,  3,  4,  8, 16, 71,157},
+};
+    
+    /*
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+    
+    /** 32x32 multiplication, followed by a 31-bit shift right. Results fits in 32 bits */
+#undef MULT32_32_Q31
+#define MULT32_32_Q31(a,b) (opus_val32)((((opus_int64)(a)) * ((opus_int64)(b)))>>31)
+    
+    #define _ISOC9X_SOURCE 1
+#define _ISOC99_SOURCE 1
+    
+      /// Allow shutdown before exit.
+  static void shutdown();
+    
+      /**
+   * @brief Create a routes table for this registry.
+   *
+   * This is called by the extensions API to allow an extension process to
+   * broadcast each registry and the set of plugins (and their optional) route
+   * information.
+   *
+   * The 'table' registry and table plugins are the primary user of the route
+   * information. Each plugin will include the SQL statement used to attach
+   * an equivalent virtual table.
+   */
+  RegistryRoutes getRoutes() const;
+    
+    class ViewsConfigParserPluginTests : public testing::Test {};
+    
+    inline void ColorTransformYCbCrToRGB(uint8_t* pixel) {
+  int y  = pixel[0];
+  int cb = pixel[1];
+  int cr = pixel[2];
+  pixel[0] = kRangeLimit[y + kCrToRedTable[cr]];
+  pixel[1] = kRangeLimit[y +
+                         ((kCrToGreenTable[cr] + kCbToGreenTable[cb]) >> 16)];
+  pixel[2] = kRangeLimit[y + kCbToBlueTable[cb]];
 }
     
-     public: // Style setters
+    // kDCTMatrix[8*u+x] = 0.5*alpha(u)*cos((2*x+1)*u*M_PI/16),
+// where alpha(0) = 1/sqrt(2) and alpha(u) = 1 for u > 0.
+static const double kDCTMatrix[64] = {
+  0.3535533906,  0.3535533906,  0.3535533906,  0.3535533906,
+  0.3535533906,  0.3535533906,  0.3535533906,  0.3535533906,
+  0.4903926402,  0.4157348062,  0.2777851165,  0.0975451610,
+ -0.0975451610, -0.2777851165, -0.4157348062, -0.4903926402,
+  0.4619397663,  0.1913417162, -0.1913417162, -0.4619397663,
+ -0.4619397663, -0.1913417162,  0.1913417162,  0.4619397663,
+  0.4157348062, -0.0975451610, -0.4903926402, -0.2777851165,
+  0.2777851165,  0.4903926402,  0.0975451610, -0.4157348062,
+  0.3535533906, -0.3535533906, -0.3535533906,  0.3535533906,
+  0.3535533906, -0.3535533906, -0.3535533906,  0.3535533906,
+  0.2777851165, -0.4903926402,  0.0975451610,  0.4157348062,
+ -0.4157348062, -0.0975451610,  0.4903926402, -0.2777851165,
+  0.1913417162, -0.4619397663,  0.4619397663, -0.1913417162,
+ -0.1913417162,  0.4619397663, -0.4619397663,  0.1913417162,
+  0.0975451610, -0.2777851165,  0.4157348062, -0.4903926402,
+  0.4903926402, -0.4157348062,  0.2777851165, -0.0975451610,
+};
+    
+    // Performs in-place floating point 8x8 DCT on block[0..63].
+// Note that the DCT used here is the DCT-2 with the first term multiplied by
+// 1/sqrt(2) and the result scaled by 1/2.
+void ComputeBlockDCTDouble(double block[64]);
+    
+    const double* Srgb8ToLinearTable() {
+  static const double* const kSrgb8ToLinearTable = NewSrgb8ToLinearTable();
+  return kSrgb8ToLinearTable;
+}
     
     
-#define DEFINE_BOXED_PRIMITIVE(LITTLE, BIG)                          \
-  struct J ## BIG : detail::JPrimitive<J ## BIG, j ## LITTLE> {      \
-    static auto constexpr kJavaDescriptor = 'Ljava/lang/' #BIG ';';  \
-    static auto constexpr kValueMethod = #LITTLE 'Value';            \
-    j ## LITTLE LITTLE ## Value() const {                            \
-      return value();                                                \
-    }                                                                \
-  };                                                                 \
-  inline local_ref<jobject> autobox(j ## LITTLE val) {               \
-    return J ## BIG::valueOf(val);                                   \
+    {}  // namespace guetzli
+    
+    #ifndef GUETZLI_JPEG_ERROR_H_
+#define GUETZLI_JPEG_ERROR_H_
+    
+      // Special case code with only one value.
+  if (total_count == 1) {
+    code.bits = 0;
+    code.value = symbols[0];
+    for (key = 0; key < total_size; ++key) {
+      table[key] = code;
+    }
+    return total_size;
   }
+    
+    // Gamma-compensated chroma subsampling.
+// Returns Y, U, V image planes, each with width x height dimensions, but the
+// U and V planes are composed of 2x2 blocks with the same values.
+std::vector<std::vector<float> > RGBToYUV420(
+    const std::vector<uint8_t>& rgb_in, const int width, const int height);
