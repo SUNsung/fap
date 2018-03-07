14fb8727357d77597950bbf0d35c60098278c382
SUNsung <weatherius@gmail.com>
2018-03-07 17:07:35 +0200

    Rewrite [52S5RObwiVrkk52avGEptlTUIw52896Hbba4orU52s3B.rb] File in: 2018-03-07 17:07:34

--- ruby/52S5RObwiVrkk52avGEptlTUIw52896Hbba4orU52s3B.rb
+++ ruby/52S5RObwiVrkk52avGEptlTUIw52896Hbba4orU52s3B.rb
@@ -1,115 +1,31 @@
 
         
-              it 'renders HTML' do
-        capture = render(options, screenshots)
-        expect(capture).to match(/<html>/)
-        expect(capture).to include('<li>Some</li>')
-        expect(capture).to include('<li>key</li>')
-        expect(capture).to include('<li>words</li>')
+          # Make this VM reachable on the host network as well, so that other
+  # VM's running other browsers can access our dev server.
+  config.vm.network :private_network, ip: '192.168.10.200'
+    
+          it 'renders HTML' do
+        expect(render(options, screenshots)).to match(/<html>/)
       end
     end
     
-          scripts = target.build_phases.select { |t| t.class == Xcodeproj::Project::Object::PBXShellScriptBuildPhase }
-      crash_script = scripts.find { |s| includes_run_script?(s.shell_script) }
-    
-      protected
-    
-      before_action :upgrade_warning, only: :index
-    
-    module Vagrant
-  module Plugin
-    module V2
-      # This is the base class for a provider for the V2 API. A provider
-      # is responsible for creating compute resources to match the needs
-      # of a Vagrant-configured system.
-      class Provider
-        include CapabilityHost
-    
-          # Renders the template using the class intance as the binding. Because the
-      # renderer inherits from `OpenStruct`, additional view variables can be
-      # added like normal accessors.
-      #
-      # @return [String]
-      def render
-        old_template = template
-        result = nil
-        File.open(full_template_path, 'r') do |f|
-          self.template = f.read
-          result = render_string
-        end
-    
-        def log_processed(name)
-      puts green '    #{name}'
+        def groups=(groups)
+      @groups = groups ? groups.compact : nil
     end
     
-    desc 'Start a dummy (test) Rails app server'
-task :dummy_rails do
-  require 'rack'
-  require 'term/ansicolor'
-  port = ENV['PORT'] || 9292
-  puts %Q(Starting on #{Term::ANSIColor.cyan 'http://localhost:#{port}'})
-  Rack::Server.start(
-    config: 'test/dummy_rails/config.ru',
-    Port: port)
-end
-    
-                -- Return the time part and the sequence part. OR appears
-            -- faster here than addition, but they're equivalent:
-            -- time_part has no trailing two bytes, and tail is only
-            -- the last two bytes.
-            RETURN time_part | tail;
-          END
-        $$ LANGUAGE plpgsql VOLATILE;
-      SQL
-    end
-    
-        it 'leaves the domains status and attachements, but clears media' do
-      expect { bad_status1.reload }.not_to raise_error
-      expect { bad_status2.reload }.not_to raise_error
-      expect { bad_attachment.reload }.not_to raise_error
-      expect(bad_attachment.file.exists?).to be false
-    end
-  end
-end
-
-    
-            def listen
-          while repl_command = STDIN.gets
-            execute_repl_command(repl_command)
-          end
-        end
-    
-          def html?(headers)
-        return false unless header = headers.detect { |k,v| k.downcase == 'content-type' }
-        options[:html_types].include? header.last[/^\w+\/\w+/]
-      end
-    end
-  end
-end
-
-    
-        it 'leaves TempFiles untouched' do
-      mock_app do |env|
-        request = Rack::Request.new(env)
-        [200, {'Content-Type' => 'text/plain'}, [request.params['file'][:filename] + '\n' + \
-                                                 request.params['file'][:tempfile].read + '\n' + \
-                                                 request.params['other']]]
+          script_array = crash_script.shell_script.split('\n').find { |line| includes_run_script?(line) }.split(' ')
+      if script_array.count == 3
+        results.merge!({
+          # The run script build phase probably refers to Fabric.framework/run, but the submit binary
+          # only lives in the Crashlytics.framework, so we'll substitute and try to resolve it that way.
+          crashlytics_path: File.dirname(script_array[0].gsub('Fabric.framework', 'Crashlytics.framework')),
+          api_key: script_array[1],
+          build_secret: script_array[2]
+        })
       end
     
-    
-  it 'should allow changing the protection mode to a string' do
-    # I have no clue what other modes are available
-    mock_app do
-      use Rack::Protection::FrameOptions, :frame_options => 'ALLOW-FROM foo'
-      run DummyApp
-    end
-    
-      %w(GET HEAD POST PUT DELETE).each do |method|
-    it 'accepts #{method} requests when allow_if is true' do
-      mock_app do
-        use Rack::Protection::HttpOrigin, :allow_if => lambda{|env| env.has_key?('HTTP_ORIGIN') }
-        run DummyApp
+            expect(result).to eq('/usr/local/bin/cloc  --by-file --xml  --out=build/cloc.xml MyCoolApp')
       end
-      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://any.domain.com')).to be_ok
-    end
-  end
+    
+            expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to match(/cd .* && agvtool new-marketing-version 1.1.0/)
+      end
