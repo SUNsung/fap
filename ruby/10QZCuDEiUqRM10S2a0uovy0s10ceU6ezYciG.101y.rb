d3ce72e673603b7fb2cefae16680590ed5ce52a2
SUNsung <weatherius@gmail.com>
2018-03-07 04:58:26 +0200

    Rewrite [10QZCuDEiUqRM10S2a0uovy0s10ceU6ezYciG.101y.rb] File in: 2018-03-07 04:58:25

--- ruby/10QZCuDEiUqRM10S2a0uovy0s10ceU6ezYciG.101y.rb
+++ ruby/10QZCuDEiUqRM10S2a0uovy0s10ceU6ezYciG.101y.rb
@@ -1,61 +1,37 @@
 
         
-                              until @when_connected.empty?
-                        @when_connected.shift.call
-                      end
-                    end
-    
-            def test_url_invalid_adapter
-          error = assert_raises(LoadError) do
-            spec 'ridiculous://foo?encoding=utf8'
-          end
-    
-        response = ActionDispatch::TestResponse.create(200, { 'Content-Type' => 'application/json' }, '{ 'foo': 'fighters' }')
-    assert_equal({ 'foo' => 'fighters' }, response.parsed_body)
-  end
-    
-          assert_equal 'Hello world', string
+            mkdir_p page_dir
+    file = '#{page_dir}/#{filename}.#{extension}'
+    if File.exist?(file)
+      abort('rake aborted!') if ask('#{file} already exists. Do you want to overwrite?', ['y', 'n']) == 'n'
     end
-    
-        def call(env)
-      result = @app.call(env)
-      result[1]['Configurable-Message'] = configurable_message
-      result
+    puts 'Creating new page: #{file}'
+    open(file, 'w') do |page|
+      page.puts '---'
+      page.puts 'layout: page'
+      page.puts 'title: \'#{title}\''
+      page.puts 'date: #{Time.now.strftime('%Y-%m-%d %H:%M')}'
+      page.puts 'comments: true'
+      page.puts 'sharing: true'
+      page.puts 'footer: true'
+      page.puts '---'
     end
-  end
-    
-            def application_mailer_file_name
-          @_application_mailer_file_name ||= if mountable_engine?
-            'app/mailers/#{namespaced_path}/application_mailer.rb'
-          else
-            'app/mailers/application_mailer.rb'
-          end
-        end
-    end
+  else
+    puts 'Syntax error: #{args.filename} contains unsupported characters'
   end
 end
-
     
-      def test_filter_bang
-    h1 = {}
-    ENV.each_pair {|k, v| h1[k] = v }
-    ENV['test'] = 'foo'
-    ENV.filter! {|k, v| IGNORE_CASE ? k.upcase != 'TEST' : k != 'test' }
-    h2 = {}
-    ENV.each_pair {|k, v| h2[k] = v }
-    assert_equal(h1, h2)
+    module Jekyll
     
-      it 'decodes UTF-8 max codepoints' do
-    [ ['\xf0\x90\x80\x80', [0x10000]],
-      ['\xf3\xbf\xbf\xbf', [0xfffff]],
-      ['\xf4\x80\x80\x80', [0x100000]],
-      ['\xf4\x8f\xbf\xbf', [0x10ffff]]
-    ].should be_computed_by(:unpack, 'U')
-  end
+      context 'called with three colors' do
+    it 'applies second color to left and right' do
+      rule = 'border-color: #f00 #0f0 #00f'
     
-      def self.wakeup_dying_sleeping_thread(kill_method_name=:kill)
-    t = ThreadSpecs.dying_thread_ensures(kill_method_name) { yield }
-    Thread.pass while t.status and t.status != 'sleep'
-    t.wakeup
-    t.join
-  end
+      context 'called with auto' do
+    it 'applies to auto to height' do
+      rule = 'height: auto; width: 100px;'
+    
+      context 'expands plain text inputs' do
+    it 'finds selectors' do
+      list = @inputs_list.join(', ')
+      ruleset = 'content: #{list};'
