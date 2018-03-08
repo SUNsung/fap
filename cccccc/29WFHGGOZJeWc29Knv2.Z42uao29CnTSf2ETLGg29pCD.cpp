59c46f55aa77115ebe0483061509e0c9a9dc0789
SUNsung <weatherius@gmail.com>
2018-03-08 10:56:26 +0200

    Rewrite [29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp] File in: 2018-03-08 10:56:25

--- cccccc/29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp
+++ cccccc/29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp
@@ -1,189 +1,284 @@
-// Returns the `cand' if it was successfully inserted; otherwise it's
-// the callers responsibility to free it.
-const RepoAuthType::Array* Builder::insert(RepoAuthType::Array* cand) {
-  assert(cand->id() == -1u);
-  std::lock_guard<std::mutex> g(m_impl->mutex);
-  auto ins = m_impl->types.insert(cand);
-  if (ins.second) {
-    cand->m_id = m_impl->nextId++;
-    assert(*ins.first == cand);
-    assert((*ins.first)->id() == cand->id());
-    return cand;
-  }
-  return *ins.first;
-}
-    
-    
-    {///////////////////////////////////////////////////////////////////////////////
-}
+
+        
+        #endif  // TENSORFLOW_GRAPPLER_COSTS_MEASURING_COST_ESTIMATOR_H_
 
     
-    Variant HHVM_FUNCTION(mb_convert_encoding,
-                      const String& str,
-                      const String& to_encoding,
-                      const Variant& from_encoding /* = uninit_variant */) {
-  String encoding = from_encoding.toString();
-  if (from_encoding.isArray()) {
-    StringBuffer _from_encodings;
-    Array encs = from_encoding.toArray();
-    for (ArrayIter iter(encs); iter; ++iter) {
-      if (!_from_encodings.empty()) {
-        _from_encodings.append(',');
-      }
-      _from_encodings.append(iter.second().toString());
-    }
-    encoding = _from_encodings.detach();
-  }
+          case ACTION_PIPE:
+        while (dup2(child_pipe_[i], i) < 0) {
+          if (!retry(errno)) {
+            _exit(1);
+          }
+        }
+        close(child_pipe_[i]);
+        child_pipe_[i] = -1;
+        break;
+    
+    #include 'tensorflow/core/lib/core/status.h'
+#include 'tensorflow/core/lib/core/stringpiece.h'
+#if !defined(IS_SLIM_BUILD)
+#include 'tensorflow/core/lib/io/zlib_compression_options.h'
+#include 'tensorflow/core/lib/io/zlib_outputbuffer.h'
+#endif  // IS_SLIM_BUILD
+#include 'tensorflow/core/platform/macros.h'
+#include 'tensorflow/core/platform/types.h'
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    
+    { private:
+  string file_format_;
+  int32 samples_per_second_;
+  int32 bits_per_second_;
+};
+    
+    using GPUDevice = Eigen::GpuDevice;
+    
+      // content::DevToolsAgentHostClient:
+  void AgentHostClosed(content::DevToolsAgentHost* agent_host,
+                       bool replaced_with_another_client) override;
+  void DispatchProtocolMessage(content::DevToolsAgentHost* agent_host,
+                               const std::string& message) override;
+    
+    #ifndef ATOM_BROWSER_API_ATOM_API_RENDER_PROCESS_PREFERENCES_H_
+#define ATOM_BROWSER_API_ATOM_API_RENDER_PROCESS_PREFERENCES_H_
+    
+    void Event::SetSenderAndMessage(content::WebContents* sender,
+                                IPC::Message* message) {
+  DCHECK(!sender_);
+  DCHECK(!message_);
+  sender_ = sender;
+  message_ = message;
     }
     
-    /**
- * (1) For people who want to quickly come up with an HTTP server handling
- *     their specific requests, we really want to minimize writing an HTTP
- *     server to something like this,
- *
- *     struct MyRequestHandler : RequestHandler {
- *       virtual void handleRequest(Transport *transport) {
- *         // ...
- *       }
- *     };
- *
- *     Then, run a server like this,
- *
- *       auto server = std::make_shared<LibEventServer>('127.0.0.1', 80, 20);
- *       server->setRequestHandlerFactory<MyRequestHandler>();
- *       Server::InstallStopSignalHandlers(server);
- *       server->start();
- *
- *     This way, we can easily swap out an implementation like LibEventServer
- *     without any modifications to MyRequestHandler, if LibEventServer model
- *     doesn't perform well with the specific requests.
- *
- * (2) For people who are interested in implementing a high-performance HTTP
- *     server, derive a new class from Server just like LibEventServer
- *     does.
- *
- *     struct MyTransport : Transport {
- *       // implements transport-related functions
- *     };
- *
- *     struct MyServer : Server {
- *       // implements how to start/stop a server
- *     };
- *
- * (3) LibEventServer is pre-implemented with evhttp, and it has one thread
- *     listening on a socket and dispatching jobs to multiple worker threads.
- *
- */
-    
-    /*!
- * \brief an iterator that iterates over a configure file and gets the configures
- */
-class ConfigIterator: public ConfigStreamReader {
- public:
-  /*!
-   * \brief constructor
-   * \param fname name of configure file
-   */
-  explicit ConfigIterator(const char *fname) : ConfigStreamReader(fi) {
-    fi.open(fname);
-    if (fi.fail()) {
-      LOG(FATAL) << 'cannot open file ' << fname;
-    }
-    ConfigReaderBase::Init();
-  }
-  /*! \brief destructor */
-  ~ConfigIterator(void) {
-    fi.close();
-  }
-    }
-    
-    /*!
- * \brief Input stream that support additional PeekRead
- *  operation, besides read.
- */
-class PeekableInStream : public dmlc::Stream {
- public:
-  explicit PeekableInStream(dmlc::Stream* strm)
-      : strm_(strm), buffer_ptr_(0) {}
-    }
-    
-    namespace xgboost {
-namespace tree {
-    }
-    }
-    
-    #define XGBOOST_TYPE_SWITCH(dtype, OP)        \
-switch (dtype) {                \
-  case xgboost::common::uint32 : {           \
-    typedef uint32_t DType;         \
-    OP; break;              \
-  }               \
-  case xgboost::common::uint16 : {           \
-    typedef uint16_t DType;         \
-    OP; break;              \
-  }               \
-  case xgboost::common::uint8 : {            \
-    typedef uint8_t DType;          \
-    OP; break;              \
-    default: LOG(FATAL) << 'don't recognize type flag' << dtype;  \
-  }               \
+    AtomQuotaPermissionContext::~AtomQuotaPermissionContext() {
 }
     
-      std::unique_ptr<AuthConfig> getUserDefinedAuthConfig() const;
+    #include 'atom/browser/browser.h'
+#include 'atom/common/native_mate_converters/net_converter.h'
+#include 'base/values.h'
+#include 'content/public/browser/browser_thread.h'
+#include 'content/public/browser/render_frame_host.h'
+#include 'content/public/browser/resource_dispatcher_host.h'
+#include 'content/public/browser/resource_request_info.h'
+#include 'content/public/browser/web_contents.h'
+#include 'net/base/auth.h'
+#include 'net/url_request/url_request.h'
     
-    void AbstractBtMessage::setBtRequestFactory(BtRequestFactory* factory)
-{
-  requestFactory_ = factory;
+    #ifndef ATOM_BROWSER_NET_URL_REQUEST_ASYNC_ASAR_JOB_H_
+#define ATOM_BROWSER_NET_URL_REQUEST_ASYNC_ASAR_JOB_H_
+    
+    namespace relauncher {
+    }
+    
+    
+    {  DISALLOW_COPY_AND_ASSIGN(TrayIconGtk);
+};
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    {    mColorLocation = glGetUniformLocation( mShaderProgram->getProgram(), 'u_color');
 }
     
-    bool AbstractProxyRequestCommand::executeInternal()
-{
-  // socket->setBlockingMode();
-  if (httpConnection_->sendBufferIsEmpty()) {
-    auto httpRequest = make_unique<HttpRequest>();
-    httpRequest->setUserAgent(getOption()->get(PREF_USER_AGENT));
-    httpRequest->setRequest(getRequest());
-    httpRequest->setProxyRequest(proxyRequest_);
-    }
-    }
     
-    #include 'AbstractCommand.h'
-    
-    
-    {} // namespace aria2
-    
-    #include 'TimerA2.h'
-    
-    bool AdaptiveFileAllocationIterator::finished()
-{
-  if (!allocator_) {
-    return offset_ == totalLength_;
-  }
-  else {
-    return allocator_->finished();
-  }
+    {
+    {
+    {			if (settings->drawFrictionImpulse == 1)
+			{
+				b2Vec2 tangent = b2Cross(point->normal, 1.0f);
+				b2Vec2 p1 = point->position;
+				b2Vec2 p2 = p1 + k_impulseScale * point->tangentImpulse * tangent;
+				m_debugDraw.DrawSegment(p1, p2, b2Color(0.9f, 0.9f, 0.3f));
+			}
+		}
+	}
 }
     
-      /**
-   * The internal announce URL pointer points to next URL.
-   * If the current URL is the last element of its group, then the first
-   * element of the next group is pointed.
-   */
-  void announceFailure();
     
-    #include 'common.h'
+    {				if (i == (e_count >> 1))
+				{
+					m_middle = body;
+				}
+				prevBody = body;
+			}
     
-    // DiskwriterFactory class template to create DiskWriter derived
-// object, ignoring filename.
-template <class DiskWriterType>
-class AnonDiskWriterFactory : public DiskWriterFactory {
-public:
-  AnonDiskWriterFactory() = default;
-  virtual ~AnonDiskWriterFactory() = default;
+    	// Server registration.
+	printer->Print(vars, 'func Register$Service$Server(s *$grpc$.Server, srv $Service$Server) {\n');
+	printer->Indent();
+	printer->Print(vars, 's.RegisterService(&$ServiceDesc$, srv)\n');
+	printer->Outdent();
+	printer->Print('}\n\n');
+    
+    
+    {    auto stream = stub_->SayManyHellos(&context, request_msg);
+    while (stream->Read(&response_msg)) {
+      const HelloReply *response = response_msg.GetRoot();
+      callback(response->message()->str());
+    }
+    auto status = stream->Finish();
+    if (!status.ok()) {
+      std::cerr << status.error_code() << ': ' << status.error_message()
+                << std::endl;
+      callback('RPC failed');
+    }
+  }
+    
+    void PrintHeaderServerMethodGeneric(
+    grpc_generator::Printer *printer, const grpc_generator::Method *method,
+    std::map<grpc::string, grpc::string> *vars) {
+  (*vars)['Method'] = method->name();
+  (*vars)['Request'] = method->input_type_name();
+  (*vars)['Response'] = method->output_type_name();
+  printer->Print(*vars, 'template <class BaseClass>\n');
+  printer->Print(*vars,
+                 'class WithGenericMethod_$Method$ : public BaseClass {\n');
+  printer->Print(
+      ' private:\n'
+      '  void BaseClassMustBeDerivedFromService(const Service *service) {}\n');
+  printer->Print(' public:\n');
+  printer->Indent();
+  printer->Print(*vars,
+                 'WithGenericMethod_$Method$() {\n'
+                 '  ::grpc::Service::MarkMethodGeneric($Idx$);\n'
+                 '}\n');
+  printer->Print(*vars,
+                 '~WithGenericMethod_$Method$() override {\n'
+                 '  BaseClassMustBeDerivedFromService(this);\n'
+                 '}\n');
+  if (method->NoStreaming()) {
+    printer->Print(
+        *vars,
+        '// disable synchronous version of this method\n'
+        '::grpc::Status $Method$('
+        '::grpc::ServerContext* context, const $Request$* request, '
+        '$Response$* response) final override {\n'
+        '  abort();\n'
+        '  return ::grpc::Status(::grpc::StatusCode::UNIMPLEMENTED, \'\');\n'
+        '}\n');
+  } else if (ClientOnlyStreaming(method)) {
+    printer->Print(
+        *vars,
+        '// disable synchronous version of this method\n'
+        '::grpc::Status $Method$('
+        '::grpc::ServerContext* context, '
+        '::grpc::ServerReader< $Request$>* reader, '
+        '$Response$* response) final override {\n'
+        '  abort();\n'
+        '  return ::grpc::Status(::grpc::StatusCode::UNIMPLEMENTED, \'\');\n'
+        '}\n');
+  } else if (ServerOnlyStreaming(method)) {
+    printer->Print(
+        *vars,
+        '// disable synchronous version of this method\n'
+        '::grpc::Status $Method$('
+        '::grpc::ServerContext* context, const $Request$* request, '
+        '::grpc::ServerWriter< $Response$>* writer) final override '
+        '{\n'
+        '  abort();\n'
+        '  return ::grpc::Status(::grpc::StatusCode::UNIMPLEMENTED, \'\');\n'
+        '}\n');
+  } else if (method->BidiStreaming()) {
+    printer->Print(
+        *vars,
+        '// disable synchronous version of this method\n'
+        '::grpc::Status $Method$('
+        '::grpc::ServerContext* context, '
+        '::grpc::ServerReaderWriter< $Response$, $Request$>* stream) '
+        'final override {\n'
+        '  abort();\n'
+        '  return ::grpc::Status(::grpc::StatusCode::UNIMPLEMENTED, \'\');\n'
+        '}\n');
+  }
+  printer->Outdent();
+  printer->Print(*vars, '};\n');
+}
+    
+    // Abort the program after logging the mesage if the given condition is not
+// true. Otherwise, do nothing.
+#define GRPC_CODEGEN_CHECK(x)                                            \
+  (x) ? (void)0                                                          \
+      : LogMessageVoidify() & LogHelper(&std::cerr).get_os()             \
+                                  << 'CHECK FAILED: ' << __FILE__ << ':' \
+                                  << __LINE__ << ': '
+    
+    #ifndef FLATC_H_
+#  define FLATC_H_
+    
+    #include 'flatbuffers/idl.h'
+    
+      // to ensure it is correct, we now generate text back from the binary,
+  // and compare the two:
+  std::string jsongen;
+  if (!GenerateText(parser, parser.builder_.GetBufferPointer(), &jsongen)) {
+    printf('Couldn't serialize parsed data to JSON!\n');
+    return 1;
+  }
+    
+    void CodeWriter::operator+=(std::string text) {
+  while (true) {
+    auto begin = text.find('{{');
+    if (begin == std::string::npos) { break; }
+    }
+    }
+    }
     }
     
-    #endif // DOWNLOAD_EVENT_LISTENER_H
-
+    static void Warn(const flatbuffers::FlatCompiler *flatc,
+                 const std::string &warn, bool show_exe_name) {
+  (void)flatc;
+  if (show_exe_name) { printf('%s: ', g_program_name); }
+  printf('warning: %s\n', warn.c_str());
+}
     
-    void configureAsyncNameResolverMan(AsyncNameResolverMan* asyncNameResolverMan,
-                                   Option* option);
+    // Outer process. Does not call the target code and thus sohuld not fail.
+void Fuzzer::CrashResistantMerge(const std::vector<std::string> &Args,
+                                 const std::vector<std::string> &Corpora) {
+  if (Corpora.size() <= 1) {
+    Printf('Merge requires two or more corpus dirs\n');
+    return;
+  }
+  std::vector<std::string> AllFiles;
+  ListFilesInDirRecursive(Corpora[0], nullptr, &AllFiles, /*TopDir*/true);
+  size_t NumFilesInFirstCorpus = AllFiles.size();
+  for (size_t i = 1; i < Corpora.size(); i++)
+    ListFilesInDirRecursive(Corpora[i], nullptr, &AllFiles, /*TopDir*/true);
+  Printf('MERGE-OUTER: %zd files, %zd in the initial corpus\n',
+         AllFiles.size(), NumFilesInFirstCorpus);
+  std::string CFPath =
+      'libFuzzerTemp.' + std::to_string(GetPid()) + '.txt';
+  // Write the control file.
+  RemoveFile(CFPath);
+  std::ofstream ControlFile(CFPath);
+  ControlFile << AllFiles.size() << '\n';
+  ControlFile << NumFilesInFirstCorpus << '\n';
+  for (auto &Path: AllFiles)
+    ControlFile << Path << '\n';
+  ControlFile.close();
+    }
+    
+    void sha1_addUncounted(sha1nfo *s, uint8_t data) {
+	uint8_t * const b = (uint8_t*) s->buffer;
+#ifdef SHA_BIG_ENDIAN
+	b[s->bufferOffset] = data;
+#else
+	b[s->bufferOffset ^ 3] = data;
+#endif
+	s->bufferOffset++;
+	if (s->bufferOffset == BLOCK_LENGTH) {
+		sha1_hashBlock(s);
+		s->bufferOffset = 0;
+	}
+}
+    
+      void AddValueForMemcmp(void *caller_pc, const void *s1, const void *s2,
+                         size_t n);
+  void AddValueForStrcmp(void *caller_pc, const char *s1, const char *s2,
+                         size_t n);
