3324956b7ec091966cb3a061d3789e628276c98b
SUNsung <weatherius@gmail.com>
2018-03-09 01:25:07 +0200

    Rewrite [35Lzod5DjJSng35q9qepcUwIo354mILYGBAktQ35TXP.py] File in: 2018-03-09 01:25:06

--- pyton/35Lzod5DjJSng35q9qepcUwIo354mILYGBAktQ35TXP.py
+++ pyton/35Lzod5DjJSng35q9qepcUwIo354mILYGBAktQ35TXP.py
@@ -1,177 +1,25 @@
 
         
-            def iter_lines(self, chunk_size):
-        return ((line, b'\n') for line in self._orig.iter_lines(chunk_size))
+        error_msg = None
     
-        Useful for long-lived HTTP responses that stream by lines
-    such as the Twitter streaming API.
+        :param args: parsed args (argparse.Namespace)
+    :type env: Environment
+    :param log_error: error log function
+    :return: status code
     
+        name = 'Basic HTTP auth'
+    auth_type = 'basic'
     
-@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
-@pytest.mark.parametrize('filename', filenames)
-def test_rst_file_syntax(filename):
-    p = subprocess.Popen(
-        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE
-    )
-    err = p.communicate()[1]
-    assert p.returncode == 0, err.decode('utf8')
-
+        long_description = README,
     
-        '''
-)
-output_options.add_argument(
-    '--output', '-o',
-    type=FileType('a+b'),
-    dest='output_file',
-    metavar='FILE',
-    help='''
-    Save output to FILE instead of stdout. If --download is also set, then only
-    the response body is saved to FILE. Other parts of the HTTP exchange are
-    printed to stderr.
+        html = get_content(url)
+    pid = match1(html, r'video\.settings\.pid\s*=\s*\'([^\']+)\'')
+    title = match1(html, r'video\.settings\.title\s*=\s*\'([^\']+)\'')
     
+    	html = get_html(url)
+	contentid = r1(r'<meta name='contentid' scheme='DMINSTR2' content='([^']+)' />', html)
+	vid = r1(r''demand_ehow_videoid':'([^']+)'', html)
+	assert vid
     
-def repr_dict_nice(d):
-    def prepare_dict(d):
-        for k, v in d.items():
-            if isinstance(v, dict):
-                v = dict(prepare_dict(v))
-            elif isinstance(v, bytes):
-                v = v.decode('utf8')
-            elif not isinstance(v, (int, str)):
-                v = repr(v)
-            yield k, v
-    return json.dumps(
-        dict(prepare_dict(d)),
-        indent=4, sort_keys=True,
-    )
-    
-        indent = None
-    if format:
-        indent = 4
-    
-        def remove_role(self, role_name):
-        del self.roles[role_name]
-
-    
-        def process_options(self, args, opts):
-        try:
-            self.settings.setdict(arglist_to_dict(opts.set),
-                                  priority='cmdline')
-        except ValueError:
-            raise UsageError('Invalid -s value, use -s NAME=VALUE', print_help=False)
-    
-        def _list_templates(self):
-        print('Available templates:')
-        for filename in sorted(os.listdir(self.templates_dir)):
-            if filename.endswith('.tmpl'):
-                print('  %s' % splitext(filename)[0])
-    
-        def post_process(self, output):
-        occurrences = 0
-        for x in output:
-            if isinstance(x, self.obj_type):
-                occurrences += 1
-    
-        def deploy_cert(self, domain, cert_path, key_path, chain_path=None,
-                    fullchain_path=None):
-        '''Installs cert'''
-        cert_path, key_path, chain_path = self.copy_certs_and_keys(
-            cert_path, key_path, chain_path)
-        self._configurator.deploy_cert(
-            domain, cert_path, key_path, chain_path, fullchain_path)
-
-    
-    '''
-    
-    # revision identifiers, used by Alembic.
-revision = '1e2841a4128'
-down_revision = '5a7bad26f2a7'
-    
-    slice_user = Table('slice_user', Base.metadata,
-    Column('id', Integer, primary_key=True),
-    Column('user_id', Integer, ForeignKey('ab_user.id')),
-    Column('slice_id', Integer, ForeignKey('slices.id'))
-)
-    
-    def upgrade():
-    op.add_column('datasources', sa.Column('offset', sa.Integer(), nullable=True))
-    op.add_column('tables', sa.Column('offset', sa.Integer(), nullable=True))
-    
-    # revision identifiers, used by Alembic.
-revision = '3c3ffe173e4f'
-down_revision = 'ad82a75afd82'
-    
-    Revision ID: 4fa88fe24e94
-Revises: b4456560d4f3
-Create Date: 2016-04-15 17:58:33.842012
-    
-        platforms = 'any',
-    zip_safe = True,
-    include_package_data = True,
-    
-    #----------------------------------------------------------------------
-def showroom_get_roomid_by_room_url_key(room_url_key):
-    '''str->str'''
-    fake_headers_mobile = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Charset': 'UTF-8,*;q=0.5',
-        'Accept-Encoding': 'gzip,deflate,sdch',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'
-    }
-    webpage_url = 'https://www.showroom-live.com/' + room_url_key
-    html = get_content(webpage_url, headers = fake_headers_mobile)
-    roomid = match1(html, r'room\?room_id\=(\d+)')
-    assert roomid
-    return roomid
-    
-    class _WorkItem(object):
-    def __init__(self, future, fn, args, kwargs):
-        self.future = future
-        self.fn = fn
-        self.args = args
-        self.kwargs = kwargs
-    
-    def download_urls_sequential(urls, timeout=60):
-    url_to_content = {}
-    for url in urls:
-        try:
-            url_to_content[url] = load_url(url, timeout=timeout)
-        except:
-            pass
-    return url_to_content
-    
-    def main():
-    for name, fn in [('sequential', sequential),
-                     ('processes', with_process_pool_executor),
-                     ('threads', with_thread_pool_executor)]:
-        sys.stdout.write('%s: ' % name.ljust(12))
-        start = time.time()
-        if fn() != [True] * len(PRIMES):
-            sys.stdout.write('failed\n')
-        else:
-            sys.stdout.write('%.2f seconds\n' % (time.time() - start))
-    
-    from __future__ import print_function
-from __future__ import division
-from __future__ import unicode_literals
-from __future__ import absolute_import
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    
-def YouCompleteMeInstance( custom_options = {} ):
-  '''Defines a decorator function for tests that passes a unique YouCompleteMe
-  instance as a parameter. This instance is initialized with the default options
-  `DEFAULT_CLIENT_OPTIONS`. Use the optional parameter |custom_options| to give
-  additional options and/or override the already existing ones.
-    
-    
-MINIMUM = 80
+        title = r1(r'<meta name='description' content='([^']*)'', html)
+    flashvars = r1(r'flashvars='(type=[^']*)'', html)
