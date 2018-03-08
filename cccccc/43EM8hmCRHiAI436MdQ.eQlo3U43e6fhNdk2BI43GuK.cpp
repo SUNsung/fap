b648049a3617775a9ab98849e4d76fd2de78e38f
SUNsung <weatherius@gmail.com>
2018-03-08 19:52:59 +0200

    Rewrite [43EM8hmCRHiAI436MdQ.eQlo3U43e6fhNdk2BI43GuK.cpp] File in: 2018-03-08 19:52:59

--- cccccc/43EM8hmCRHiAI436MdQ.eQlo3U43e6fhNdk2BI43GuK.cpp
+++ cccccc/43EM8hmCRHiAI436MdQ.eQlo3U43e6fhNdk2BI43GuK.cpp
@@ -1,240 +1,243 @@
 
         
-        Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    // ResourceOpKernel<T> is a virtual base class for resource op implementing
-// interface type T. The inherited op looks up the resource name (determined by
-// ContainerInfo), and creates a new resource if necessary.
-//
-// Requirements:
-//  - Op must be marked as stateful.
-//  - Op must have `container` and `shared_name` attributes. Empty `container`
-//  means using the default container. Empty `shared_name` means private
-//  resource.
-//  - Subclass must override CreateResource().
-//  - Subclass is encouraged to override VerifyResource().
-template <typename T>
-class ResourceOpKernel : public OpKernel {
- public:
-  explicit ResourceOpKernel(OpKernelConstruction* context) : OpKernel(context) {
-    OP_REQUIRES_OK(context,
-                   context->allocate_persistent(DT_STRING, TensorShape({2}),
-                                                &handle_, nullptr));
-  }
-    }
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-      double ComputeDualLoss(const double current_dual, const double example_label,
-                         const double example_weight) const final {
-    // For binary classification, there are 2 conjugate functions, one per
-    // label value (-1 and 1).
-    const double y_alpha = current_dual * example_label;  // y \alpha
-    if (y_alpha < 0 || y_alpha > 1.0) {
-      return std::numeric_limits<double>::max();
-    }
-    return (-y_alpha + 0.5 * gamma * current_dual * current_dual) *
-           example_weight;
-  }
-    
-    class Diagnostician {
- public:
-  // Logs diagnostic information when CUDA appears to be misconfigured (e.g. is
-  // not initializing).
-  //
-  // Note: if we're running on a machine that has no GPUs, we don't want to
-  // produce very much log spew beyond saying, 'looks like there's no CUDA
-  // kernel
-  // module running'.
-  //
-  // Note: we use non-Google-File:: API here because we may be called before
-  // InitGoogle has completed.
-  static void LogDiagnosticInformation();
-    }
-    
-    #ifndef TENSORFLOW_COMMON_RUNTIME_SYCL_SYCL_DEVICE_CONTEXT_H_
-#define TENSORFLOW_COMMON_RUNTIME_SYCL_SYCL_DEVICE_CONTEXT_H_
+          void compute();
     
     
-    { private:
-  enum { kBufferSize = 256 << 10 /* 256 kB */ };
-  const int skip_header_lines_;
-  Env* const env_;
-  int64 line_number_;
-  std::unique_ptr<RandomAccessFile> file_;  // must outlive input_buffer_
-  std::unique_ptr<io::InputBuffer> input_buffer_;
-};
-    
-    class Clipboard : public Base {
- public:
-  Clipboard(int id,
-            const base::WeakPtr<DispatcherHost>& dispatcher_host,
-            const base::DictionaryValue& option);
-  ~Clipboard() override;
-    }
-    
-    class BaseEvent {
-  friend class EventListener;
-  DISALLOW_COPY_AND_ASSIGN(BaseEvent);
-    }
-    
-    
-    {  // Convert from content coordinates to window coordinates.
-  // This code copied from chrome_web_contents_view_delegate_views.cc
-  aura::Window* target_window = GetActiveNativeView(rfh);
-  aura::Window* root_window = target_window->GetRootWindow();
-  views::Widget* top_level_widget =
-    views::Widget::GetTopLevelWidgetForNativeView(target_window);
-  aura::client::ScreenPositionClient* screen_position_client =
-        aura::client::GetScreenPositionClient(root_window);
-  if (screen_position_client) {
-    screen_position_client->ConvertPointToScreen(target_window,
-             &screen_point);
-  }
-  set_delay_destruction(true);
-  menu_runner_.reset(new views::MenuRunner(menu_model_.get(), views::MenuRunner::CONTEXT_MENU,
-                                           base::Bind(&Menu::OnMenuClosed, base::Unretained(this))));
-  menu_runner_->RunMenuAt(top_level_widget,
-                       nullptr,
-                       gfx::Rect(screen_point, gfx::Size()),
-                       views::MENU_ANCHOR_TOPRIGHT,
-                       ui::MENU_SOURCE_NONE);
-  // It is possible for the same MenuMessageLoopAura to start a nested
-  // message-loop while it is already running a nested loop. So make
-  // sure the quit-closure gets reset to the outer loop's quit-closure
-  // once the innermost loop terminates.
-  {
-    base::AutoReset<base::Closure> reset_quit_closure(&message_loop_quit_,
-                                                      base::Closure());
-  
-    base::MessageLoop* loop = base::MessageLoop::current();
-    base::MessageLoop::ScopedNestableTaskAllower allow(loop);
-    base::RunLoop run_loop;
-    message_loop_quit_ = run_loop.QuitClosure();
-  
-    run_loop.Run();
-  }
-  set_delay_destruction(false);
-  if (pending_destruction())
-    object_manager_->OnDeallocateObject(id_);
+    {  Scalar = C;
+  return size_t(SourceNext - SourceStart) == S.size();
 }
     
+    #pragma mark - NSData verification
     
-    {            size_t stFrame = 0;
-            for (size_t iUtt = 0; iUtt < extraMaps.size(); iUtt++)
-            {
-                size_t numFramesInThisUtterance = lattices[iUtt]->getnumframes();
-                size_t iParallelSeq = extraMaps[iUtt]; // i-th utterance belongs to iParallelSeq-th parallel sequence
-                if (iParallelSeq >= parallelSeqStId && iParallelSeq < parallelSeqEnId)
-                {
-                    // this utterance has been selected
-                    decimatedLattices->push_back(lattices[iUtt]);
-                    decimatedBoundaryPtr->insert(decimatedBoundaryPtr->end(), boundaries.begin() + stFrame, boundaries.begin() + stFrame + numFramesInThisUtterance);
-                    decimatedUidPtr->insert(decimatedUidPtr->end(), uids.begin() + stFrame, uids.begin() + stFrame + numFramesInThisUtterance);
-                    decimatedExtraMapPtr->push_back(extraMaps[iUtt] - parallelSeqStId);
+      ArrayRef<MarkupASTNode *> getChildren() {
+    return {getTrailingObjects<MarkupASTNode *>(), NumChildren};
+  }
+    
+    SILDebugScope::SILDebugScope(SILLocation Loc)
+    : Loc(Loc), InlinedCallSite(nullptr) {}
+    
+    namespace llvm {
+  template<> class PointerLikeTypeTraits<swift::ConcreteDeclRef> {
+    typedef llvm::PointerUnion<swift::ValueDecl *,
+                               swift::ConcreteDeclRef::SpecializedDeclRef *>
+      DataPointer;
+    typedef PointerLikeTypeTraits<DataPointer> DataTraits;
+    }
+    }
+    
+    // Build a Table file from the contents of *iter.  The generated file
+// will be named according to meta->number.  On success, the rest of
+// *meta will be filled with metadata about the generated table.
+// If no data is present in *iter, meta->file_size will be set to
+// zero, and no Table file will be produced.
+extern Status BuildTable(const std::string& dbname,
+                         Env* env,
+                         const Options& options,
+                         TableCache* table_cache,
+                         Iterator* iter,
+                         FileMetaData* meta);
+    
+    #include <stdint.h>
+#include 'leveldb/db.h'
+#include 'db/dbformat.h'
+    
+    std::string ParsedInternalKey::DebugString() const {
+  char buf[50];
+  snprintf(buf, sizeof(buf), '' @ %llu : %d',
+           (unsigned long long) sequence,
+           int(type));
+  std::string result = ''';
+  result += EscapeString(user_key.ToString());
+  result += buf;
+  return result;
+}
+    
+      // Successful parses
+  static struct {
+    const char* fname;
+    uint64_t number;
+    FileType type;
+  } cases[] = {
+    { '100.log',            100,   kLogFile },
+    { '0.log',              0,     kLogFile },
+    { '0.sst',              0,     kTableFile },
+    { '0.ldb',              0,     kTableFile },
+    { 'CURRENT',            0,     kCurrentFile },
+    { 'LOCK',               0,     kDBLockFile },
+    { 'MANIFEST-2',         2,     kDescriptorFile },
+    { 'MANIFEST-7',         7,     kDescriptorFile },
+    { 'LOG',                0,     kInfoLogFile },
+    { 'LOG.old',            0,     kInfoLogFile },
+    { '18446744073709551615.log', 18446744073709551615ull, kLogFile },
+  };
+  for (int i = 0; i < sizeof(cases) / sizeof(cases[0]); i++) {
+    std::string f = cases[i].fname;
+    ASSERT_TRUE(ParseFileName(f, &number, &type)) << f;
+    ASSERT_EQ(cases[i].type, type) << f;
+    ASSERT_EQ(cases[i].number, number) << f;
+  }
+    
+    WriteBatch::Handler::~Handler() { }
+    
+    class Benchmark {
+ private:
+  sqlite3* db_;
+  int db_num_;
+  int num_;
+  int reads_;
+  double start_;
+  double last_op_finish_;
+  int64_t bytes_;
+  std::string message_;
+  Histogram hist_;
+  RandomGenerator gen_;
+  Random rand_;
+    }
+    
+    #ifndef STORAGE_LEVELDB_INCLUDE_COMPARATOR_H_
+#define STORAGE_LEVELDB_INCLUDE_COMPARATOR_H_
+    
+    // Dump the contents of the file named by fname in text format to
+// *dst.  Makes a sequence of dst->Append() calls; each call is passed
+// the newline-terminated text corresponding to a single item found
+// in the file.
+//
+// Returns a non-OK result if fname does not name a leveldb storage
+// file, or if the file cannot be read.
+Status DumpFile(Env* env, const std::string& fname, WritableFile* dst);
+    
+    template<class Function>
+static Variant do_lazy_stat(Function dostat, const String& filename) {
+  struct stat sb;
+  if (dostat(File::TranslatePathWithFileCache(filename).c_str(), &sb)) {
+    Logger::Verbose('%s/%d: %s', __FUNCTION__, __LINE__,
+                    folly::errnoStr(errno).c_str());
+    return false;
+  }
+  return stat_impl(&sb);
+}
+    
+    #include <folly/portability/SysTime.h>
+    
+    struct Vframe {
+  Vframe(const Func* func, int parent, int cost, uint64_t entry_weight)
+    : func(func)
+    , parent(parent)
+    , entry_weight(entry_weight)
+    , inclusive_cost(cost)
+    , exclusive_cost(cost)
+  {}
+    }
+    
+    /* regex search */
+static Variant _php_mb_regex_ereg_search_exec(const String& pattern,
+                                              const String& option,
+                                              int mode) {
+  int n, i, err, pos, len, beg, end;
+  OnigUChar *str;
+  OnigSyntaxType *syntax = MBSTRG(regex_default_syntax);
+  OnigOptionType noption;
+    }
+    
+      void delist() {
+    auto n = m_next, p = m_prev;
+    n->m_prev = p;
+    p->m_next = n;
+  }
+    
+      /**
+   * How many jobs are queued waiting to be handled.
+   */
+  virtual int getQueuedJobs() = 0;
+    
+    void initNuma() {
+  if (getenv('HHVM_DISABLE_NUMA')) {
+    return;
+  }
+  // When linked dynamically numa_init() is called before JEMallocInitializer()
+  // numa_init is not exported by libnuma.so so it will be NULL
+  // however when linked statically numa_init() is not guaranteed to be called
+  // before JEMallocInitializer(), so call it here.
+  if (&numa_init) {
+    numa_init();
+  }
+  if (numa_available() < 0) return;
+    }
+    
+    #include 'hphp/runtime/base/thrift-buffer.h'
+    
+    bool HHVM_FUNCTION(posix_setpgid,
+                   int pid,
+                   int pgid) {
+  return setpgid(pid, pgid) >= 0;
+}
+    
+      Offset defaultOff = bcOff(env) + iv.vec32()[iv.size() - 1];
+  Offset zeroOff = 0;
+  if (base <= 0 && (base + nTargets) > 0) {
+    zeroOff = bcOff(env) + iv.vec32()[0 - base];
+  } else {
+    zeroOff = defaultOff;
+  }
+    
+    // Define a callback to handle incoming messages
+void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
+    try {
+        s->send(hdl, msg->get_payload(), msg->get_opcode());
+    } catch (const websocketpp::lib::error_code& e) {
+    }
+    }
+    
+        totalConnections = atoi(argv[1]);
+    port = atoi(argv[2]);
+    
+    int byteSize, framesPerSend;
+unsigned char *framePackBuffer;
+int framePackBufferLength = 0;
+const char upgradeHeaderBuffer[] = 'GET / HTTP/1.1\r\n'
+                                   'Upgrade: websocket\r\n'
+                                   'Connection: Upgrade\r\n'
+                                   'Sec-WebSocket-Key: x3JJHMbDL1EzLkh9GBhXDw==\r\n'
+                                   'Host: server.example.com\r\n'
+                                   'Sec-WebSocket-Version: 13\r\n\r\n';
+    
+    
+    {
+    {
+    {            // Send upgrade header
+            uv_write(new uv_write_t, connect->handle, &upgradeHeader, 1, [](uv_write_t *write_t, int status) {
+                if (status < 0) {
+                    cout << 'Connection error' << endl;
+                    exit(-1);
                 }
-                stFrame += numFramesInThisUtterance;
-            }
+                delete write_t;
+            });
         }
+    });
+}
     
-    #include 'stdafx.h'
-#include 'Basics.h'
-#include 'Actions.h'
-#include 'ComputationNetwork.h'
-#include 'ComputationNode.h'
-#include 'DataReader.h'
-#include 'DataWriter.h'
-#include 'SimpleNetworkBuilder.h'
-#include 'Config.h'
-#include 'ScriptableObjects.h'
-    
-    #pragma once
-    
-    struct SourceFile // content of one source file  (only in this header because TextLocation's private member uses it)
-{
-    /*const*/ wstring path;                     // where it came from
-    /*const*/ vector<wstring> lines;            // source code lines
-    SourceFile(wstring location, wstring text); // from string, e.g. command line
-    SourceFile(wstring path);                   // from file
-};
-    
-    // type of data in this section
-enum SectionType
-{
-    sectionTypeNull = 0,
-    sectionTypeFile = 1,          // file header
-    sectionTypeData = 2,          // data section
-    sectionTypeLabel = 3,         // label data
-    sectionTypeLabelMapping = 4,  // label mapping table (array of strings)
-    sectionTypeStats = 5,         // data statistics
-    sectionTypeCategoryLabel = 6, // labels in category format (float type, all zeros with a single 1.0 per column)
-    sectionTypeMax
-};
-    
-    
-    {public:
-    inline array_ref(_T* ptr, size_t size) throw()
-        : data(ptr), n(size)
-    {
-    }
-    inline array_ref() throw()
-        : data(NULL), n(0)
-    {
-    } // in case we have a vector of this
-    inline _T& operator[](size_t i) throw()
-    {
-        check_index(i);
-        return data[i];
-    }
-    inline const _T& operator[](size_t i) const throw()
-    {
-        check_index(i);
-        return data[i];
-    }
-    inline size_t size() const throw()
-    {
-        return n;
-    }
-    inline _T* begin()
-    {
-        return data;
-    }
-    inline _T* end()
-    {
-        return data + n;
-    }
-    inline void resize(size_t sz)
-    {
-        sz;
-        assert(n == sz);
-    } // allow compatibility with some functions
-    // construct from other vector types
-    template <class _V>
-    inline array_ref(_V& v)
-        : data(v.size() > 0 ? &v[0] : NULL), n((size_t) v.size())
-    {
-    }
-};
-    
-        float4 operator>=(const float4& other) const
-    {
-        return _mm_cmpge_ps(v, other);
-    }
-    float4 operator<=(const float4& other) const
-    {
-        return _mm_cmple_ps(v, other);
+        if (keyFilePassword.length()) {
+        context.password.reset(new std::string(keyFilePassword));
+        SSL_CTX_set_default_passwd_cb_userdata(context.context, context.password.get());
+        SSL_CTX_set_default_passwd_cb(context.context, Context::passwordCallback);
     }
     
-    static bool got_sigint = false;
+        template <class T, class D>
+    void sendTransformed(const char *message, size_t length, void(*callback)(void *socket, void *data, bool cancelled, void *reserved), void *callbackData, D transformData) {
+        size_t estimatedLength = T::estimate(message, length) + sizeof(Queue::Message);
+    }
     
-            // The actual processing.
-        std::string prefix('Hello ');
-        reply_.set_message(prefix + request_.name());
+    public:
+    bool isClosed() {
+        return state.fd == -1;
+    }
     
-    #include <iostream>
-#include <memory>
-#include <string>
+        template <void A(Socket *s)>
+    static void accept_poll_cb(Poll *p, int status, int events) {
+        ListenSocket *listenData = (ListenSocket *) p;
+        accept_cb<A, false>(listenData);
+    }
+    
+            struct TransformData {
+            bool hasHead;
+        } transformData = {hasHead};
