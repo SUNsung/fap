041f67d40292a554865650225d37004bf95ee55b
SUNsung <weatherius@gmail.com>
2018-03-08 20:50:55 +0200

    Rewrite [43EM8hmCRHiAI436MdQ.eQlo3U43e6fhNdk2BI43GuK.cpp] File in: 2018-03-08 20:50:55

--- cccccc/43EM8hmCRHiAI436MdQ.eQlo3U43e6fhNdk2BI43GuK.cpp
+++ cccccc/43EM8hmCRHiAI436MdQ.eQlo3U43e6fhNdk2BI43GuK.cpp
@@ -1,243 +1,285 @@
 
         
-          void compute();
-    
-    
-    {  Scalar = C;
-  return size_t(SourceNext - SourceStart) == S.size();
+        
+    {  std::unique_ptr<PartialRunState> partial_run = MakeUnique<PartialRunState>();
+  partial_run->cancellation_manager = MakeUnique<CancellationManager>();
+  *cancellation_manager = partial_run->cancellation_manager.get();
+  step_id_to_partial_run_[step_id] = std::move(partial_run);
+  return true;
 }
     
-    #pragma mark - NSData verification
+    #include 'tensorflow/core/lib/core/errors.h'
+#include 'tensorflow/core/lib/core/status.h'
+#include 'tensorflow/core/lib/gtl/array_slice.h'
+#include 'tensorflow/core/platform/types.h'
     
-      ArrayRef<MarkupASTNode *> getChildren() {
-    return {getTrailingObjects<MarkupASTNode *>(), NumChildren};
-  }
+    #endif  // TENSORFLOW_KERNELS_SPARSE_TENSOR_DENSE_ADD_OP_H_
+
     
-    SILDebugScope::SILDebugScope(SILLocation Loc)
-    : Loc(Loc), InlinedCallSite(nullptr) {}
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    namespace llvm {
-  template<> class PointerLikeTypeTraits<swift::ConcreteDeclRef> {
-    typedef llvm::PointerUnion<swift::ValueDecl *,
-                               swift::ConcreteDeclRef::SpecializedDeclRef *>
-      DataPointer;
-    typedef PointerLikeTypeTraits<DataPointer> DataTraits;
-    }
-    }
-    
-    // Build a Table file from the contents of *iter.  The generated file
-// will be named according to meta->number.  On success, the rest of
-// *meta will be filled with metadata about the generated table.
-// If no data is present in *iter, meta->file_size will be set to
-// zero, and no Table file will be produced.
-extern Status BuildTable(const std::string& dbname,
-                         Env* env,
-                         const Options& options,
-                         TableCache* table_cache,
-                         Iterator* iter,
-                         FileMetaData* meta);
-    
-    #include <stdint.h>
-#include 'leveldb/db.h'
-#include 'db/dbformat.h'
-    
-    std::string ParsedInternalKey::DebugString() const {
-  char buf[50];
-  snprintf(buf, sizeof(buf), '' @ %llu : %d',
-           (unsigned long long) sequence,
-           int(type));
-  std::string result = ''';
-  result += EscapeString(user_key.ToString());
-  result += buf;
-  return result;
-}
-    
-      // Successful parses
-  static struct {
-    const char* fname;
-    uint64_t number;
-    FileType type;
-  } cases[] = {
-    { '100.log',            100,   kLogFile },
-    { '0.log',              0,     kLogFile },
-    { '0.sst',              0,     kTableFile },
-    { '0.ldb',              0,     kTableFile },
-    { 'CURRENT',            0,     kCurrentFile },
-    { 'LOCK',               0,     kDBLockFile },
-    { 'MANIFEST-2',         2,     kDescriptorFile },
-    { 'MANIFEST-7',         7,     kDescriptorFile },
-    { 'LOG',                0,     kInfoLogFile },
-    { 'LOG.old',            0,     kInfoLogFile },
-    { '18446744073709551615.log', 18446744073709551615ull, kLogFile },
-  };
-  for (int i = 0; i < sizeof(cases) / sizeof(cases[0]); i++) {
-    std::string f = cases[i].fname;
-    ASSERT_TRUE(ParseFileName(f, &number, &type)) << f;
-    ASSERT_EQ(cases[i].type, type) << f;
-    ASSERT_EQ(cases[i].number, number) << f;
-  }
-    
-    WriteBatch::Handler::~Handler() { }
-    
-    class Benchmark {
- private:
-  sqlite3* db_;
-  int db_num_;
-  int num_;
-  int reads_;
-  double start_;
-  double last_op_finish_;
-  int64_t bytes_;
-  std::string message_;
-  Histogram hist_;
-  RandomGenerator gen_;
-  Random rand_;
-    }
-    
-    #ifndef STORAGE_LEVELDB_INCLUDE_COMPARATOR_H_
-#define STORAGE_LEVELDB_INCLUDE_COMPARATOR_H_
-    
-    // Dump the contents of the file named by fname in text format to
-// *dst.  Makes a sequence of dst->Append() calls; each call is passed
-// the newline-terminated text corresponding to a single item found
-// in the file.
+    // A PluginConfig describes the set of plugins to be used by a StreamExecutor
+// instance. Each plugin is defined by an arbitrary identifier, usually best set
+// to the address static member in the implementation (to avoid conflicts).
 //
-// Returns a non-OK result if fname does not name a leveldb storage
-// file, or if the file cannot be read.
-Status DumpFile(Env* env, const std::string& fname, WritableFile* dst);
-    
-    template<class Function>
-static Variant do_lazy_stat(Function dostat, const String& filename) {
-  struct stat sb;
-  if (dostat(File::TranslatePathWithFileCache(filename).c_str(), &sb)) {
-    Logger::Verbose('%s/%d: %s', __FUNCTION__, __LINE__,
-                    folly::errnoStr(errno).c_str());
-    return false;
-  }
-  return stat_impl(&sb);
-}
-    
-    #include <folly/portability/SysTime.h>
-    
-    struct Vframe {
-  Vframe(const Func* func, int parent, int cost, uint64_t entry_weight)
-    : func(func)
-    , parent(parent)
-    , entry_weight(entry_weight)
-    , inclusive_cost(cost)
-    , exclusive_cost(cost)
-  {}
+// A PluginConfig may be passed to the StreamExecutor constructor - the plugins
+// described therein will be used to provide BLAS, DNN, FFT, and RNG
+// functionality. Platform-appropriate defaults will be used for any un-set
+// libraries. If a platform does not support a specified plugin (ex. cuBLAS on
+// an OpenCL executor), then an error will be logged and no plugin operations
+// will succeed.
+//
+// The StreamExecutor BUILD target does not link ANY plugin libraries - even
+// common host fallbacks! Any plugins must be explicitly linked by dependent
+// targets. See the cuda, opencl and host BUILD files for implemented plugin
+// support (search for 'plugin').
+class PluginConfig {
+ public:
+  // Value specifying the platform's default option for that plugin.
+  static const PluginId kDefault;
     }
     
-    /* regex search */
-static Variant _php_mb_regex_ereg_search_exec(const String& pattern,
-                                              const String& option,
-                                              int mode) {
-  int n, i, err, pos, len, beg, end;
-  OnigUChar *str;
-  OnigSyntaxType *syntax = MBSTRG(regex_default_syntax);
-  OnigOptionType noption;
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    #endif  // TENSORFLOW_PLATFORM_PREFETCH_H_
+
+    
+    namespace tensorflow {
     }
     
-      void delist() {
-    auto n = m_next, p = m_prev;
-    n->m_prev = p;
-    p->m_next = n;
+    
+    {    input_buffer_.reset(new io::InputBuffer(file_.get(), kBufferSize));
+    for (; line_number_ < skip_header_lines_; ++line_number_) {
+      string line_contents;
+      Status status = input_buffer_->ReadLine(&line_contents);
+      if (errors::IsOutOfRange(status)) {
+        // We ignore an end of file error when skipping header lines.
+        // We will end up skipping this file.
+        return Status::OK();
+      }
+      TF_RETURN_IF_ERROR(status);
+    }
+    return Status::OK();
   }
+    
+      // If instruction is part of inputs, don't reset the bit_vector.
+  if (std::find(inputs.begin(), inputs.end(), instruction) == inputs.end()) {
+    bit_vector.SetToZero();
+  }
+  bit_vector.Set(GetIndex(instruction));
+  for (const HloInstruction* input : inputs) {
+    bit_vector.OrWith(GetBitVector(input));
+  }
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    
+    {    bool checkSubsetPublic( const CvMat* ms1, int count, bool checkPartialSubset );
+};
+    
+    /** @file
+ * @deprecated Use @ref cudev instead.
+ */
+    
+    #endif //HAVE_LAPACK
+#endif //OPENCV_CORE_HAL_INTERNAL_HPP
+
+    
+    
+    {        return dlsym(h, name);
+    }
+    #define CV_CL_GET_PROC_ADDRESS(name) GetProcAddress(name)
+#endif
+    
+    /**
+ * @brief A backing storage domain name, used for key/value based storage.
+ *
+ * There are certain 'cached' variables such as a node-unique UUID or negotiated
+ * 'node_key' following enrollment. If a value or setting must persist between
+ * osqueryi or osqueryd runs it should be stored using the kPersistentSetting%s
+ * domain.
+ */
+extern const std::string kPersistentSettings;
+    
+     private:
+  /**
+   * @brief Default constructor.
+   *
+   * Since instances of Dispatcher should only be created via instance(),
+   * Dispatcher's constructor is private.
+   */
+  Dispatcher() = default;
+  virtual ~Dispatcher() = default;
     
       /**
-   * How many jobs are queued waiting to be handled.
+   * @brief Halt the EventPublisher run loop.
+   *
+   * Any EventSubscriber%s with Subscription%s for this EventPublisher will
+   * become useless. osquery callers MUST deregister events.
+   * EventPublisher%s assume they can hook/trampoline, which requires cleanup.
+   * This will tear down and remove the publisher if the run loop did not start.
+   * Otherwise it will call end on the publisher and assume the run loop will
+   * tear down and remove.
+   *
+   * @param pub The string label for the EventPublisher.
+   *
+   * @return Did the EventPublisher deregister cleanly.
    */
-  virtual int getQueuedJobs() = 0;
+  static Status deregisterEventPublisher(const EventPublisherRef& pub);
     
-    void initNuma() {
-  if (getenv('HHVM_DISABLE_NUMA')) {
-    return;
-  }
-  // When linked dynamically numa_init() is called before JEMallocInitializer()
-  // numa_init is not exported by libnuma.so so it will be NULL
-  // however when linked statically numa_init() is not guaranteed to be called
-  // before JEMallocInitializer(), so call it here.
-  if (&numa_init) {
-    numa_init();
-  }
-  if (numa_available() < 0) return;
-    }
+    #pragma once
     
-    #include 'hphp/runtime/base/thrift-buffer.h'
-    
-    bool HHVM_FUNCTION(posix_setpgid,
-                   int pid,
-                   int pgid) {
-  return setpgid(pid, pgid) >= 0;
-}
-    
-      Offset defaultOff = bcOff(env) + iv.vec32()[iv.size() - 1];
-  Offset zeroOff = 0;
-  if (base <= 0 && (base + nTargets) > 0) {
-    zeroOff = bcOff(env) + iv.vec32()[0 - base];
-  } else {
-    zeroOff = defaultOff;
+      /**
+   * @brief implicit conversion to bool
+   *
+   * Allows easy use of Status in an if statement, as below:
+   *
+   * @code{.cpp}
+   *   if (doSomethingThatReturnsStatus()) {
+   *     LOG(INFO) << 'Success!';
+   *   }
+   * @endcode
+   */
+  /* explicit */ explicit operator bool() const {
+    return ok();
   }
     
-    // Define a callback to handle incoming messages
-void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
-    try {
-        s->send(hdl, msg->get_payload(), msg->get_opcode());
-    } catch (const websocketpp::lib::error_code& e) {
+    KMOD_EXPLICIT_DECL(com.facebook.security.osquery,
+                   STR(OSQUERY_KERNEL_COMMUNICATION_VERSION),
+                   _start,
+                   _stop)
+DECLHIDDEN(kmod_start_func_t *) _realmain = OsqueryStart;
+DECLHIDDEN(kmod_stop_func_t *) _antimain = OsqueryStop;
+DECLHIDDEN(int) _kext_apple_cc = __APPLE_CC__;
+
+    
+    class CarverTests : public testing::Test {
+ public:
+  CarverTests() {
+    fs::create_directories(kFakeDirectory + '/files_to_carve/');
+    writeTextFile(kFakeDirectory + '/files_to_carve/secrets.txt',
+                  'This is a message I'd rather no one saw.');
+    writeTextFile(kFakeDirectory + '/files_to_carve/evil.exe',
+                  'MZP\x00\x02\x00\x00\x00\x04\x00\x0f\x00\xff\xff');
     }
     }
     
-        totalConnections = atoi(argv[1]);
-    port = atoi(argv[2]);
+    namespace errc = boost::system::errc;
+namespace fs = boost::filesystem;
+namespace pt = boost::property_tree;
     
-    int byteSize, framesPerSend;
-unsigned char *framePackBuffer;
-int framePackBufferLength = 0;
-const char upgradeHeaderBuffer[] = 'GET / HTTP/1.1\r\n'
-                                   'Upgrade: websocket\r\n'
-                                   'Connection: Upgrade\r\n'
-                                   'Sec-WebSocket-Key: x3JJHMbDL1EzLkh9GBhXDw==\r\n'
-                                   'Host: server.example.com\r\n'
-                                   'Sec-WebSocket-Version: 13\r\n\r\n';
+    Status platformStrncpy(char* dst, size_t nelms, const char* src, size_t count) {
+  if (dst == nullptr || src == nullptr || nelms == 0) {
+    return Status(1, 'Failed to strncpy: invalid arguments');
+  }
+    }
     
     
     {
-    {
-    {            // Send upgrade header
-            uv_write(new uv_write_t, connect->handle, &upgradeHeader, 1, [](uv_write_t *write_t, int status) {
-                if (status < 0) {
-                    cout << 'Connection error' << endl;
-                    exit(-1);
-                }
-                delete write_t;
-            });
-        }
-    });
+    {      if (sharpenmap[index]) {
+        if (sharpen) yuv[channel][index] = sharpened[index];
+      } else if (blurmap[index]) {
+        if (blur) yuv[channel][index] = blurred[index];
+      }
+    }
+  }
+    
+    const double* Srgb8ToLinearTable() {
+  static const double* const kSrgb8ToLinearTable = NewSrgb8ToLinearTable();
+  return kSrgb8ToLinearTable;
 }
     
-        if (keyFilePassword.length()) {
-        context.password.reset(new std::string(keyFilePassword));
-        SSL_CTX_set_default_passwd_cb_userdata(context.context, context.password.get());
-        SSL_CTX_set_default_passwd_cb(context.context, Context::passwordCallback);
+    // Definition of error codes for parsing jpeg files.
+    
+      // Fill in 2nd level tables and add pointers to root table.
+  table += table_size;
+  table_size = 0;
+  low = 0;
+  for (len = kJpegHuffmanRootTableBits + 1;
+       len <= kJpegHuffmanMaxBitLength; ++len) {
+    for (; count[len] > 0; --count[len]) {
+      // Start a new sub-table if the previous one is full.
+      if (low >= table_size) {
+        table += table_size;
+        table_bits = NextTableBitSize(count, len);
+        table_size = 1 << table_bits;
+        total_size += table_size;
+        low = 0;
+        lut[key].bits = table_bits + kJpegHuffmanRootTableBits;
+        lut[key].value = (table - lut) - key;
+        ++key;
+      }
+      code.bits = len - kJpegHuffmanRootTableBits;
+      code.value = symbols[idx++];
+      reps = 1 << (table_bits - code.bits);
+      while (reps--) {
+        table[low++] = code;
+      }
+    }
+  }
+    
+    namespace fuzzer {
     }
     
-        template <class T, class D>
-    void sendTransformed(const char *message, size_t length, void(*callback)(void *socket, void *data, bool cancelled, void *reserved), void *callbackData, D transformData) {
-        size_t estimatedLength = T::estimate(message, length) + sizeof(Queue::Message);
+      void ExecuteCallback(const uint8_t *Data, size_t Size);
+  size_t RunOne(const uint8_t *Data, size_t Size);
+    
+    // The control file example:
+//
+// 3 # The number of inputs
+// 1 # The number of inputs in the first corpus, <= the previous number
+// file0
+// file1
+// file2  # One file name per line.
+// STARTED 0 123  # FileID, file size
+// DONE 0 1 4 6 8  # FileID COV1 COV2 ...
+// STARTED 1 456  # If DONE is missing, the input crashed while processing.
+// STARTED 2 567
+// DONE 2 8 9
+bool Merger::Parse(std::istream &IS, bool ParseCoverage) {
+  LastFailure.clear();
+  std::string Line;
     }
     
-    public:
-    bool isClosed() {
-        return state.fd == -1;
-    }
+    struct MergeFileInfo {
+  std::string Name;
+  size_t Size = 0;
+  std::vector<uint32_t> Features;
+};
     
-        template <void A(Socket *s)>
-    static void accept_poll_cb(Poll *p, int status, int events) {
-        ListenSocket *listenData = (ListenSocket *) p;
-        accept_cb<A, false>(listenData);
-    }
+      /// Creates a cross-over of two pieces of Data, returns its size.
+  size_t CrossOver(const uint8_t *Data1, size_t Size1, const uint8_t *Data2,
+                   size_t Size2, uint8_t *Out, size_t MaxOutSize);
     
-            struct TransformData {
-            bool hasHead;
-        } transformData = {hasHead};
+    #endif  // LLVM_FUZZER_OPTIONS_H
+
+    
+    // TableOfRecentCompares (TORC) remembers the most recently performed
+// comparisons of type T.
+// We record the arguments of CMP instructions in this table unconditionally
+// because it seems cheaper this way than to compute some expensive
+// conditions inside __sanitizer_cov_trace_cmp*.
+// After the unit has been executed we may decide to use the contents of
+// this table to populate a Dictionary.
+template<class T, size_t kSizeT>
+struct TableOfRecentCompares {
+  static const size_t kSize = kSizeT;
+  struct Pair {
+    T A, B;
+  };
+  void Insert(size_t Idx, T Arg1, T Arg2) {
+    Idx = Idx % kSize;
+    Table[Idx].A = Arg1;
+    Table[Idx].B = Arg2;
+  }
+    }
