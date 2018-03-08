1b7f79afe7023d6a9a61f752360a8e8aaacfa096
SUNsung <weatherius@gmail.com>
2018-03-09 00:53:14 +0200

    Rewrite [20prHYUcKLOmw20aKy6Jnqkcxg20PHiyDt2VOYE20O.D.rb] File in: 2018-03-09 00:53:14

--- ruby/20prHYUcKLOmw20aKy6Jnqkcxg20PHiyDt2VOYE20O.D.rb
+++ ruby/20prHYUcKLOmw20aKy6Jnqkcxg20PHiyDt2VOYE20O.D.rb
@@ -1,84 +1,76 @@
 
         
-        def fixture_site(overrides = {})
-  Jekyll::Site.new(site_configuration(overrides))
-end
+        require 'abstract_unit'
     
-    unless File.exist?(PROF_OUTPUT_FILE)
-  StackProf.run(
-    mode: MODE.to_sym,
-    interval: 100,
-    raw: true,
-    out: PROF_OUTPUT_FILE
-  ) do
-    puts 'GC Stats:', JSON.pretty_generate(GC.stat)
-    GC.disable
+    class RedirectTest < ActionController::TestCase
+  tests RedirectController
     
-            c.action do |args, opts|
-          Jekyll::Commands::NewTheme.process(args, opts)
-        end
-      end
+              # Returns name of the database.
+          def database_from_path
+            if @adapter == 'sqlite3'
+              # 'sqlite3:/foo' is absolute, because that makes sense. The
+              # corresponding relative version, 'sqlite3:foo', is handled
+              # elsewhere, as an 'opaque'.
+    
+        test 'head :switching_protocols (101) does not return a content-type header' do
+      headers = HeadController.action(:switching_protocols).call(Rack::MockRequest.env_for('/')).second
+      assert_nil headers['Content-Type']
+      assert_nil headers['Content-Length']
     end
     
-          # Group an array of items by an expression
-      #
-      # input - the object array
-      # variable - the variable to assign each item to in the expression
-      # expression -a Liquid comparison expression passed in as a string
-      #
-      # Returns the filtered array of objects
-      def group_by_exp(input, variable, expression)
-        return input unless groupable?(input)
+        # Returns +text+ wrapped at +len+ columns and indented +indent+ spaces.
+    # By default column length +len+ equals 72 characters and indent
+    # +indent+ equal two spaces.
+    #
+    #   my_text = 'Here is a sample text with more than 40 characters'
+    #
+    #   format_paragraph(my_text, 25, 4)
+    #   # => '    Here is a sample text with\n    more than 40 characters'
+    def format_paragraph(text, len = 72, indent = 2)
+      sentences = [[]]
     
-      # Update version.rb file with BOOTSTRAP_SHA
-  def store_version
-    path    = 'lib/bootstrap-sass/version.rb'
-    content = File.read(path).sub(/BOOTSTRAP_SHA\s*=\s*[''][\w]+['']/, 'BOOTSTRAP_SHA = '#@branch_sha'')
-    File.open(path, 'w') { |f| f.write(content) }
+            def application_mailer_file_name
+          @_application_mailer_file_name ||= if mountable_engine?
+            'app/mailers/#{namespaced_path}/application_mailer.rb'
+          else
+            'app/mailers/application_mailer.rb'
+          end
+        end
+    end
   end
 end
 
     
-        def log_processed(name)
-      puts green '    #{name}'
     end
+
     
-      # The test environment is used exclusively to run your application's
-  # test suite. You never need to work with it otherwise. Remember that
-  # your test database is 'scratch space' for the test suite and is wiped
-  # and recreated between test runs. Don't rely on the data there!
-  config.cache_classes = true
+        def execute(args)
     
-    def report_apps
-  running = Set.new
-  @app_names.zip(@bundle_ids, @unix_ids).each do |app_name, bundle_id, _unix_id|
-    next if excluded_bundle_id bundle_id
-    next if excluded_app_name app_name
-    bundle_id.gsub!(%r{^(missing value)$}, '<\1>')
-    running.add '#{bundle_id}\t#{app_name}'
+      def revoke_moderation!
+    set_permission('moderator', false)
   end
     
-        # @abstract
-    #
-    # Create a (new) clone of the remote-repository on the deployment target
-    #
-    # @return void
-    #
-    def clone
-      raise NotImplementedError, 'Your SCM strategy module should provide a #clone method'
+    describe :string_unpack_16bit_le_unsigned, shared: true do
+  it 'decodes a short with most significant bit set as a positive number' do
+    '\x00\xff'.unpack(unpack_format()).should == [65280]
+  end
+end
+    
+      # This case occurred in JRuby where native threads are used to provide
+  # the same behavior as MRI green threads. Key to this issue was the fact
+  # that the thread which called #exit in its block was also being explicitly
+  # sent #join from outside the thread. The 100.times provides a certain
+  # probability that the deadlock will occur. It was sufficient to reliably
+  # reproduce the deadlock in JRuby.
+  it 'does not deadlock when called from within the thread while being joined from without' do
+    100.times do
+      t = Thread.new { Thread.stop; Thread.current.send(@method) }
+      Thread.pass while t.status and t.status != 'sleep'
+      t.wakeup.should == t
+      t.join.should == t
     end
-    
-      desc 'Finished'
-  task :finished do
   end
+end
+
     
-              add_offense(node)
-        end
-    
-      def default_metadata
-    @factory.build(:default_metadata).all
-  end
-    
-      def self.plugin_type
-    'codec'
-  end
+        exit_loop = true
