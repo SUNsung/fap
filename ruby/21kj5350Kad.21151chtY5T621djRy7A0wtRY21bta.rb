6a64eb044b290c59935ba37b3b067200a16eda45
SUNsung <weatherius@gmail.com>
2018-03-08 21:29:50 +0200

    Rewrite [21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb] File in: 2018-03-08 21:29:49

--- ruby/21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb
+++ ruby/21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb
@@ -1,89 +1,99 @@
-    # This setting makes it so that network access from inside the vagrant guest
-    # is able to resolve DNS using the hosts VPN connection.
-    v.customize ['modifyvm', :id, '--natdnshostresolver1', 'on']
-  end
+
+        
+        class Jekyll::Commands::NewTheme < Jekyll::Command
+  class << self
+    def init_with_program(prog)
+      prog.command(:'new-theme') do |c|
+        c.syntax 'new-theme NAME'
+        c.description 'Creates a new Jekyll theme scaffold'
+        c.option 'code_of_conduct', \
+          '-c', '--code-of-conduct', \
+          'Include a Code of Conduct. (defaults to false)'
     
+          private
+      def grouped_array(groups)
+        groups.each_with_object([]) do |item, array|
+          array << {
+            'name'  => item.first,
+            'items' => item.last,
+            'size'  => item.last.size,
+          }
+        end
+      end
     end
+  end
+end
 
     
-      def version
-    object.metadata.version
+    module Jekyll
+  class Layout
+    include Convertible
+    
+          def warnings
+        @template.warnings
+      end
+    
+          _form_configurable_fields[name] = options
+    end
+    
+      # Optional
+  #   Override this method if you need to group multiple agents based on an API key,
+  #   or server they connect to.
+  #   Have a look at the TwitterStreamAgent for an example.
+  def self.setup_worker; end
+    
+      def tumblr
+    Tumblr.configure do |config|
+      config.consumer_key = tumblr_consumer_key
+      config.consumer_secret = tumblr_consumer_secret
+      config.oauth_token = tumblr_oauth_token
+      config.oauth_token_secret = tumblr_oauth_token_secret
+    end
+    
+    Tumblr::Client.new
+  end
+end
+    
+      def test_realdirpath_junction
+    Dir.mktmpdir('rubytest-realpath') {|tmpdir|
+      Dir.chdir(tmpdir) do
+        Dir.mkdir('foo')
+        skip 'cannot run mklink' unless system('mklink /j bar foo > nul')
+        assert_equal(File.realpath('foo'), File.realpath('bar'))
+      end
+    }
+  end if /mswin|mingw/ =~ RUBY_PLATFORM
+    
+        $, = ':'
+    a = @cls[1, 2, 3]
+    assert_equal('[1, 2, 3]', a.to_s)
+  ensure
+    $, = nil
   end
     
-      describe :split_keywords do
-    context 'only commas' do
-      let(:keywords) { 'One,Two, Three, Four Token,' }
+      def test_transform_keys
+    x = @cls[a: 1, b: 2, c: 3]
+    y = x.transform_keys {|k| :'#{k}!' }
+    assert_equal({a: 1, b: 2, c: 3}, x)
+    assert_equal({a!: 1, b!: 2, c!: 3}, y)
     
-    module Fastlane
-  class CrashlyticsProjectParser
-    # @param project_file_path path to a .xcodeproj file
-    def initialize(config = {})
-      FastlaneCore::Project.detect_projects(config)
-      @project = FastlaneCore::Project.new(config, xcodebuild_list_silent: true, xcodebuild_suppress_stderr: true)
+      def test_compare_by_identity
+    a1, a2 = 'a', 'a'
+    b1, b2 = 'b', 'b'
+    c = 'c'
+    array = [a1, b1, c, a2, b2]
     
-          it 'gets the correct version number for 'SampleProject_tests'' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          get_version_number(xcodeproj: '.xcproject', target: 'SampleProject_tests')
-        end').runner.execute(:test)
-        expect(result).to eq('1.0')
-      end
+      quarantine! do
     
-            it 'returns true' do
-          result = Fastlane::FastFile.new.parse('lane :test do
-            git_tag_exists(tag: '1.2.0')
-          end').runner.execute(:test)
+        prune
+  end
     
-            expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to match(/cd .* && agvtool new-marketing-version 1.1.0/)
-      end
-    
-        def as_json
-      indexed_docs.map do |doc|
-        json = doc.as_json
-        json[:mtime] = doc_mtime(doc)
-        json[:db_size] = doc_db_size(doc)
-        json
-      end
+        def self.clear
+      instance.clear
     end
     
-          unless root?
-        raise Invalid, 'missing name' if !name || name.empty?
-        raise Invalid, 'missing path' if !path || path.empty?
-        raise Invalid, 'missing type' if !type || type.empty?
-      end
-    end
+      private
     
-        def subpath_to(url, options = nil)
-      url = self.class.parse(url)
-      return unless origin == url.origin
-    
-              # Underscore methods
-          if name.start_with?('Underscore')
-            node.at_css('~ ul').css('li').each do |li|
-              name = [type.downcase, li.at_css('a').content.split.first].join('.')
-              id = name.parameterize
-              li['id'] = id
-              entries << [name, id, type]
-            end
-            next
+              def find_plugins_gem_specs
+            @specs ||= ::Gem::Specification.find_all.select{|spec| logstash_plugin_gem_spec?(spec)}
           end
-    
-    File.readlines(sitelist).each do |site|
-  site.strip!
-  next if site.length == 0
-  next if site =~ /^#/
-    
-    asm = nil
-File.open('h2b.com.dbg.in', 'rb') { |fd|
-	asm = fd.read(fd.stat.size)
-}
-    
-    keytoolOpts 	= ['-genkey', '-alias', 'signFiles', '-keystore', 'msfkeystore',
-		   '-storepass', 'msfstorepass', '-dname', 'cn=#{certCN}',
-		   '-keypass', 'msfkeypass']
-    
-    		end
-	end
-    
-      def default_metadata
-    @factory.build(:default_metadata).all
-  end
