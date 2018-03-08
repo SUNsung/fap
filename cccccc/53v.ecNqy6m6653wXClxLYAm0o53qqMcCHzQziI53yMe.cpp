da59477a226a45f18e1657696d6daea29aa9d528
SUNsung <weatherius@gmail.com>
2018-03-08 09:27:55 +0200

    Rewrite [53v.ecNqy6m6653wXClxLYAm0o53qqMcCHzQziI53yMe.cpp] File in: 2018-03-08 09:27:54

--- cccccc/53v.ecNqy6m6653wXClxLYAm0o53qqMcCHzQziI53yMe.cpp
+++ cccccc/53v.ecNqy6m6653wXClxLYAm0o53qqMcCHzQziI53yMe.cpp
@@ -1,166 +1,306 @@
-TEST(FindFileTest, OverlappingFiles) {
-  Add('150', '600');
-  Add('400', '500');
-  disjoint_sorted_files_ = false;
-  ASSERT_TRUE(! Overlaps('100', '149'));
-  ASSERT_TRUE(! Overlaps('601', '700'));
-  ASSERT_TRUE(Overlaps('100', '150'));
-  ASSERT_TRUE(Overlaps('100', '200'));
-  ASSERT_TRUE(Overlaps('100', '300'));
-  ASSERT_TRUE(Overlaps('100', '400'));
-  ASSERT_TRUE(Overlaps('100', '500'));
-  ASSERT_TRUE(Overlaps('375', '400'));
-  ASSERT_TRUE(Overlaps('450', '450'));
-  ASSERT_TRUE(Overlaps('450', '500'));
-  ASSERT_TRUE(Overlaps('450', '700'));
-  ASSERT_TRUE(Overlaps('600', '700'));
-}
+
+        
+         protected:
+  // Helper functions that abstract away the column buffer and gemm arguments.
+  // The last argument in forward_cpu_gemm is so that we can skip the im2col if
+  // we just called weight_cpu_gemm with the same input.
+  void forward_cpu_gemm(const Dtype* input, const Dtype* weights,
+      Dtype* output, bool skip_im2col = false);
+  void forward_cpu_bias(Dtype* output, const Dtype* bias);
+  void backward_cpu_gemm(const Dtype* input, const Dtype* weights,
+      Dtype* output);
+  void weight_cpu_gemm(const Dtype* input, const Dtype* output, Dtype*
+      weights);
+  void backward_cpu_bias(Dtype* bias, const Dtype* input);
     
-        while (bytes_to_copy > 0) {
-      size_t avail = kBlockSize - block_offset;
-      if (avail > bytes_to_copy) {
-        avail = bytes_to_copy;
-      }
-      memcpy(dst, blocks_[block] + block_offset, avail);
-    }
-    
-    Slice BlockBuilder::Finish() {
-  // Append restart array
-  for (size_t i = 0; i < restarts_.size(); i++) {
-    PutFixed32(&buffer_, restarts_[i]);
-  }
-  PutFixed32(&buffer_, restarts_.size());
-  finished_ = true;
-  return Slice(buffer_);
-}
-    
-    class FilterBlockReader {
- public:
- // REQUIRES: 'contents' and *policy must stay live while *this is live.
-  FilterBlockReader(const FilterPolicy* policy, const Slice& contents);
-  bool KeyMayMatch(uint64_t block_offset, const Slice& key);
-    }
+      virtual inline const char* type() const { return 'Concat'; }
+  virtual inline int MinBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
     
     
-    {  Status result = metaindex_handle_.DecodeFrom(input);
-  if (result.ok()) {
-    result = index_handle_.DecodeFrom(input);
-  }
-  if (result.ok()) {
-    // We skip over any leftover data (just padding for now) in 'input'
-    const char* end = magic_ptr + 8;
-    *input = Slice(end, input->data() + input->size() - end);
-  }
-  return result;
-}
+    { protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual inline bool reverse_dimensions() { return false; }
+  virtual void compute_output_shape();
+};
     
-    #include 'table/merger.h'
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
     
-    
-    
-    
-    
-    
-    
-    				// For a circle: I = 0.5 * m * r * r ==> r = sqrt(2 * I / m)
-				float32 radius = b2Sqrt(2.0f * I / mass);
-    
-    	Bridge()
-	{
-		b2Body* ground = NULL;
-		{
-			b2BodyDef bd;
-			ground = m_world->CreateBody(&bd);
-    }
-    }
-    
-    		m_x = RandomFloat(-1.0f, 1.0f);
-		m_bullet->SetTransform(b2Vec2(m_x, 10.0f), 0.0f);
-		m_bullet->SetLinearVelocity(b2Vec2(0.0f, -50.0f));
-		m_bullet->SetAngularVelocity(0.0f);
-    
-    static void stb_out2(stb_uint v) { stb_out(v >> 8); stb_out(v); }
-static void stb_out3(stb_uint v) { stb_out(v >> 16); stb_out(v >> 8); stb_out(v); }
-static void stb_out4(stb_uint v) { stb_out(v >> 24); stb_out(v >> 16); stb_out(v >> 8 ); stb_out(v); }
-    
-    // Use if you want to reset your rendering device without losing ImGui state.
-IMGUI_API void        ImGui_ImplDX10_InvalidateDeviceObjects();
-IMGUI_API bool        ImGui_ImplDX10_CreateDeviceObjects();
-    
-    // Copyright (C) 2015 by Giovanni Zito
-// This file is part of ImGui
-    
-        // Setup style
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
-    
-        bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-    
-    // GLFW callbacks (installed by default if you enable 'install_callbacks' during initialization)
-// Provided here if you want to chain callbacks.
-// You can also handle inputs yourself and use those as a reference.
-IMGUI_API void        ImGui_ImplGlfw_MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
-IMGUI_API void        ImGui_ImplGlfw_ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
-IMGUI_API void        ImGui_ImplGlfw_KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-IMGUI_API void        ImGui_ImplGlfw_CharCallback(GLFWwindow* window, unsigned int c);
+    #endif  // CAFFE_CUDNN_SOFTMAX_LAYER_HPP_
 
     
-    int main(int, char**)
-{
-    // Setup SDL
-    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0)
-    {
-        printf('Error: %s\n', SDL_GetError());
-        return -1;
+      virtual inline const char* type() const { return 'Deconvolution'; }
+    
+     protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+      virtual inline const char* type() const { return 'ELU'; }
+    
+    
+    {  bool first_reshape_;
+  vector<int> indices_to_forward_;
+};
+    
+      // Returns the average sum of squared perpendicular error from a line
+  // through mean_point() in the direction dir.
+  double rms_orth(const FCOORD &dir) const;
+    
+        float x() const {  //get coords
+      return xcoord;
     }
+    float y() const {
+      return ycoord;
+    }
+    ///rewrite function
+    void set_x(float xin) {
+      xcoord = xin;              //write new value
+    }
+    ///rewrite function
+    void set_y(float yin) {  //value to set
+      ycoord = yin;
     }
     
-        // Create the rasterizer state
-    {
-        D3D10_RASTERIZER_DESC desc;
-        ZeroMemory(&desc, sizeof(desc));
-        desc.FillMode = D3D10_FILL_SOLID;
-        desc.CullMode = D3D10_CULL_NONE;
-        desc.ScissorEnable = true;
-        desc.DepthClipEnable = true;
-        g_pd3dDevice->CreateRasterizerState(&desc, &g_pRasterizerState);
-    }
     
-    AbstractBtMessage::~AbstractBtMessage() = default;
+    {}
     
-      // Swaps socket_ with socket. This disables current read and write
-  // check.
-  void swapSocket(std::shared_ptr<SocketCore>& socket);
+    /*
+ * Before we've inserted arrays into either a ArrayTypeTable::Builder
+ * or an actual ArrayTypeTable, they can't be compared just using
+ * their ids, so we have these.
+ */
     
-    void AbstractOptionHandler::setInitialOption(bool f)
-{
-  updateFlags(FLAG_INITIAL_OPTION, f);
+      ts->tv_sec += s_extra_request_nanoseconds / sec_to_ns;
+  auto res = ts->tv_nsec + s_extra_request_nanoseconds % sec_to_ns;
+  if (res > sec_to_ns) {
+    res -= sec_to_ns;
+    ts->tv_sec += 1;
+  }
+  ts->tv_nsec = res;
+    
+    
+    {  v << vcall{
+    CallSpec::direct(pthread_getspecific),
+    v.makeVcallArgs({{v.cns(datum.tls->m_key)}}),
+    v.makeTuple({d}),
+    Fixup{},
+    DestType::SSA
+  };
 }
     
     
-    {
-    {    httpConnection_->sendProxyRequest(std::move(httpRequest));
-  }
-  else {
-    httpConnection_->sendPendingData();
-  }
-  if (httpConnection_->sendBufferIsEmpty()) {
-    getDownloadEngine()->addCommand(getNextCommand());
-    return true;
-  }
-  else {
-    setWriteCheckSocket(getSocket());
-    addCommandSelf();
-    return false;
+    {  if ((m_threadMode == Normal) || (cmd.getInterruptType() == PSPEnded)) {
+    // If the thread mode is Normal we let other threads with
+    // interrupts go ahead and process them. We also do this when the
+    // thread is at PSPEnded because the thread is done.
+    switchThreadMode(Normal);
   }
 }
     
-      std::shared_ptr<HttpConnection> httpConnection_;
     
-      hash_validator(const std::string& hash) : hash_(hash) {}
+    {  AreaIndex area_idx;
+  int frame{-1};
+  uint64_t weight;
+  jit::vector<Vinstr> code;
+};
     
-    #include <vector>
-#include <string>
-#include <memory>
+      mpz_clear(gmpDataA);
+  mpz_clear(gmpDataB);
+  mpz_clear(gmpReturn);
+    
+    
+    {}
+    
+    jit::vector<Vlabel> DFSSortClusters::sort(uint32_t initialCid) {
+  dfs(initialCid);
+  return std::move(m_list);
+}
+    
+    void PageletServer::Restart() {
+  Stop();
+  if (RuntimeOption::PageletServerThreadCount > 0) {
+    {
+      Lock l(s_dispatchMutex);
+      s_dispatcher = new JobQueueDispatcher<PageletWorker>
+        (RuntimeOption::PageletServerThreadCount,
+         RuntimeOption::PageletServerThreadDropCacheTimeoutSeconds,
+         RuntimeOption::PageletServerThreadDropStack,
+         nullptr);
+      s_dispatcher->setHugeThreadCount(
+        RuntimeOption::PageletServerHugeThreadCount);
+      auto monitor = getSingleton<HostHealthMonitor>();
+      monitor->subscribe(s_dispatcher);
+    }
+    s_dispatcher->start();
+    BootStats::mark('pagelet server started');
+  }
+}
+    
+    #include <cstdint>
+#include <vector>
+#include <numa.h>
+    
+    struct IRGS;
+    
+      tmp0 = in[2 * stride];
+  tmp1 = kIDCTMatrix[ 2] * tmp0;
+  tmp2 = kIDCTMatrix[10] * tmp0;
+  out[0] += tmp1;
+  out[1] += tmp2;
+  out[2] -= tmp2;
+  out[3] -= tmp1;
+  out[4] -= tmp1;
+  out[5] -= tmp2;
+  out[6] += tmp2;
+  out[7] += tmp1;
+    
+    #ifndef GUETZLI_JPEG_DATA_DECODER_H_
+#define GUETZLI_JPEG_DATA_DECODER_H_
+    
+    #include <stdint.h>
+#include <vector>
+    
+    #include <array>
+    
+    template <class Member, class R, class D, class... As>
+struct MemberDef<Member, R (D::*)(As...)> {
+  static R value(D& d, As... as) {
+    return folly::invoke(memberValue<Member>(), d, static_cast<As&&>(as)...);
+  }
+};
+    
+    
+    {  // Runtime compatibility.
+  test_enable_shared_from_this(std::make_shared<C_folly>());
+  test_enable_shared_from_this(std::make_shared<C_folly const>());
+}
+
+    
+      template <typename T>
+  static const T& asConst(const T& t) noexcept {
+    return t;
+  }
+    
+      void reportError(const folly::dynamic* lookup_k, const std::exception& ex);
+    
+    namespace folly { namespace test {
+    }
+    }
+    
+    // This is needed for return conversion
+template <>
+struct Convert<void> {
+  typedef void jniType;
+};
+    
+    #pragma once
+    
+    #include './Node.hh'
+#include './Layout.hh'
+#include './Size.hh'
+#include './Config.hh'
+    
+     public: // Layout mutators
+    
+    namespace detail {
+template <typename T, typename jprim>
+struct JPrimitive : JavaClass<T> {
+  using typename JavaClass<T>::javaobject;
+  using JavaClass<T>::javaClassStatic;
+  static local_ref<javaobject> valueOf(jprim val) {
+    static auto cls = javaClassStatic();
+    static auto method =
+      cls->template getStaticMethod<javaobject(jprim)>('valueOf');
+    return method(cls, val);
+  }
+  jprim value() const {
+    static auto method =
+      javaClassStatic()->template getMethod<jprim()>(T::kValueMethod);
+    return method(this->self());
+  }
+};
+    }
+    
+    
+    {  //Package name for the service
+  grpc::string package_name;
+};
+    
+    #endif // D_A2_STR_H
+
+    
+    void AbstractBtMessage::setBtMessageValidator(
+    std::unique_ptr<BtMessageValidator> validator)
+{
+  validator_ = std::move(validator);
+}
+    
+      DownloadEngine* getDownloadEngine() const { return e_; }
+    
+    class AbstractProxyResponseCommand : public AbstractCommand {
+private:
+  std::shared_ptr<HttpConnection> httpConnection_;
+    }
+    
+      /**
+   * Moves current tier pointer to the tier to which the 'stopped' event can
+   * be sent.
+   * 2-stage search operation is made.
+   * The first search operation is performed from current pointer position
+   * to the end. If no such tier is found, the second search is performed from
+   * the first to the current pointer position.
+   */
+  void moveToStoppedAllowedTier();
+  void moveToCompletedAllowedTier();
+    
+      AnnounceEvent event;
+  std::deque<std::string> urls;
+    
+    class AsyncNameResolverMan {
+public:
+  AsyncNameResolverMan();
+  // Destructor does not call disableNameResolverCheck(). Application
+  // must call it before the destruction of this object.
+  ~AsyncNameResolverMan();
+  // Enable IPv4 address lookup. default: true
+  void setIPv4(bool ipv4) { ipv4_ = ipv4; }
+  // Enable IPv6 address lookup. default: true
+  void setIPv6(bool ipv6) { ipv6_ = ipv6; }
+  // Returns true if asynchronous name resolution has been started.
+  bool started() const;
+  // Starts asynchronous name resolution.
+  void startAsync(const std::string& hostname, DownloadEngine* e,
+                  Command* command);
+  // Appends resolved addresses to |res|.
+  void getResolvedAddress(std::vector<std::string>& res) const;
+  // Adds resolvers to DownloadEngine to check event notification.
+  void setNameResolverCheck(DownloadEngine* e, Command* command);
+  // Removes resolvers from DownloadEngine.
+  void disableNameResolverCheck(DownloadEngine* e, Command* command);
+  // Returns true if any of resolvers are added in DownloadEngine.
+  bool resolverChecked() const { return resolverCheck_; }
+  // Returns status value: 0 for inprogress, 1 for success and -1 for
+  // failure.
+  int getStatus() const;
+  // Returns last error string
+  const std::string& getLastError() const;
+  // Resets state. Also removes resolvers from DownloadEngine.
+  void reset(DownloadEngine* e, Command* command);
+    }
