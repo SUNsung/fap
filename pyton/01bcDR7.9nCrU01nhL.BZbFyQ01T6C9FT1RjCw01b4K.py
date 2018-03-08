ebe48502a58a30544cf36b4bb70203c5fc33bb6c
SUNsung <weatherius@gmail.com>
2018-03-08 20:25:58 +0200

    Rewrite [01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py] File in: 2018-03-08 20:25:57

--- pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
+++ pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
@@ -1,142 +1,70 @@
 
         
-        versions_info = json.load(open('update/versions.json'))
-if 'signature' in versions_info:
-    del versions_info['signature']
+        try:
+    input = raw_input
+except NameError:
+    pass
     
-    
-def _mkdir(d):
-    if not os.path.exists(d):
-        os.mkdir(d)
-    
-            if check_executable('mplayer', ['-h']):
-            args = [
-                'mplayer', '-really-quiet', '-vo', 'null', '-vc', 'dummy',
-                '-dumpstream', '-dumpfile', tmpfilename, url]
-        elif check_executable('mpv', ['-h']):
-            args = [
-                'mpv', '-really-quiet', '--vo=null', '--stream-dump=' + tmpfilename, url]
-        else:
-            self.report_error('MMS or RTSP download detected but neither 'mplayer' nor 'mpv' could be run. Please install any.')
-            return False
-    
-    import re
-    
-            title = self._html_search_regex(r'<title>(.*?)</title>', webpage, 'title')
-        description = self._html_search_regex(
-            r'<div class='description'[^>]*>([^<]+)</div>', webpage, 'description', fatal=False)
-        thumbnail = self._html_search_regex(
-            r'preview_url\s*:\s*\'(.*?)\'', webpage, 'thumbnail', fatal=False)
-    
-    from .common import InfoExtractor
-from ..utils import (
-    int_or_none,
-    parse_iso8601,
-    sanitized_Request,
-)
-    
-            uploader = self._search_regex(
-            r'Added by\s*:\s*<a[^>]+>([^<]+)</a>', webpage, 'uploader', default=None)
-        upload_date = unified_strdate(self._search_regex(
-            r'Added on\s*:\s*([\d-]+)', webpage, 'upload date', default=None))
-    
-        parser.add_argument('-v', '--verbose',
-                        dest='verbose',
-                        action='store_true',
-                        help='show what is being downloaded')
-    
-    label = r'''
-    (?:[\w]|{range})                    # Starts with an alphanumeric or a range
-    (?:[\w_-]|{range})*                 # Then zero or more of the same or [_-]
-    (?<![_-])                           # ...as long as it didn't end with [_-]
-'''.format(range=alphanumeric_range)
-    
-        @g_connect
-    def create_import_task(self, github_user, github_repo, reference=None, role_name=None):
-        '''
-        Post an import request
-        '''
-        url = '%s/imports/' % self.baseurl
-        args = {
-            'github_user': github_user,
-            'github_repo': github_repo,
-            'github_reference': reference if reference else ''
-        }
-        if role_name:
-            args['alternate_role_name'] = role_name
-        elif github_repo.startswith('ansible-role'):
-            args['alternate_role_name'] = github_repo[len('ansible-role') + 1:]
-        data = self.__call_galaxy(url, args=urlencode(args))
-        if data.get('results', None):
-            return data['results']
-        return data
-    
-    import os, json, imp
-here = os.path.abspath(os.path.dirname(__file__))
-proj_info = json.loads(open(os.path.join(here, PROJ_METADATA), encoding='utf-8').read())
-try:
-    README = open(os.path.join(here, 'README.rst'), encoding='utf-8').read()
-except:
-    README = ''
-CHANGELOG = open(os.path.join(here, 'CHANGELOG.rst'), encoding='utf-8').read()
-VERSION = imp.load_source('version', os.path.join(here, 'src/%s/version.py' % PACKAGE_NAME)).__version__
-    
-    #----------------------------------------------------------------------
-def fc2video_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    '''wrapper'''
-    #'http://video.fc2.com/en/content/20151021bTVKnbEw'
-    #'http://xiaojiadianvideo.asia/content/20151021bTVKnbEw'
-    #'http://video.fc2.com/ja/content/20151021bTVKnbEw'
-    #'http://video.fc2.com/tw/content/20151021bTVKnbEw'
-    hostname = urlparse(url).hostname
-    if not ('fc2.com' in hostname or 'xiaojiadianvideo.asia' in hostname):
-        return False
-    upid = match1(url, r'.+/content/(\w+)')
-    
-        while pin_count > 0:
-        json_data = extract_json_data(url, max=pin_list[-1]['pin_id'],
-                                      limit=LIMIT)
-        pins = json_data['pins']
-        pin_list += pins
-        pin_count -= len(pins)
-    
-            pdf = match1(content, r'name='filename'\s*value='([^']+\.pdf)'')
-        if pdf: pdf = 'http://res.infoq.com/downloads/pdfdownloads/%s' % pdf
-    
-    
-site_info = 'kugou.com'
-download = kugou_download
-# download_playlist = playlist_not_supported('kugou')
-download_playlist=kugou_download_playlist
+    if __name__ == '__main__':
+    main()
 
     
-        # mgid%3Auma%3Avideo%3Amtv81.com%3A897974
-    vid = match1(html, r'getTheVideo\('(.*?)'')
-    xml = parseString(
-        get_content('http://intl.esperanto.mtvi.com/www/xml/media/mediaGen.jhtml?uri={}&flashPlayer=LNX%2013,0,0,206&geo=CN&sid=123456'.format(vid)))
+    if isinstance(helptext, bytes):
+    helptext = helptext.decode('utf-8')
     
-        def extract(self, **kwargs):
-        for i in self.streams:
-            # for each available stream
-            s = self.streams[i]
-            # fill in 'container' field and 'size' field (optional)
-            _, s['container'], s['size'] = url_info(s['url'])
-            # 'src' field is a list of processed urls for direct downloading
-            # usually derived from 'url'
-            s['src'] = [s['url']]
+    from youtube_dl.swfinterp import SWFInterpreter
     
-    #----------------------------------------------------------------------
-def showroom_get_roomid_by_room_url_key(room_url_key):
-    '''str->str'''
-    fake_headers_mobile = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Charset': 'UTF-8,*;q=0.5',
-        'Accept-Encoding': 'gzip,deflate,sdch',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'
+    from test.helper import assertRegexpMatches
+    
+            webpage = self._download_webpage(url, video_id)
+    
+                stream_url_hls = json_data.get('stream_url_hls')
+            if stream_url_hls:
+                formats.extend(self._extract_m3u8_formats(
+                    stream_url_hls, video_id, 'mp4',
+                    entry_protocol='m3u8_native', m3u8_id='hls', fatal=False))
+    
+    
+class ClipRsIE(OnetBaseIE):
+    _VALID_URL = r'https?://(?:www\.)?clip\.rs/(?P<id>[^/]+)/\d+'
+    _TEST = {
+        'url': 'http://www.clip.rs/premijera-frajle-predstavljaju-novi-spot-za-pesmu-moli-me-moli/3732',
+        'md5': 'c412d57815ba07b56f9edc7b5d6a14e5',
+        'info_dict': {
+            'id': '1488842.1399140381',
+            'ext': 'mp4',
+            'title': 'PREMIJERA Frajle predstavljaju novi spot za pesmu Moli me, moli',
+            'description': 'md5:56ce2c3b4ab31c5a2e0b17cb9a453026',
+            'duration': 229,
+            'timestamp': 1459850243,
+            'upload_date': '20160405',
+        }
     }
-    webpage_url = 'https://www.showroom-live.com/' + room_url_key
-    html = get_content(webpage_url, headers = fake_headers_mobile)
-    roomid = match1(html, r'room\?room_id\=(\d+)')
-    assert roomid
-    return roomid
+    
+    Does two benchmarks
+    
+        return lasso_results, lars_lasso_results
+    
+    n_samples = np.logspace(.5, 3, 9)
+n_features = np.logspace(1, 3.5, 7)
+N_samples, N_features = np.meshgrid(n_samples,
+                                    n_features)
+scikits_time = np.zeros(N_samples.shape)
+scipy_time = np.zeros(N_samples.shape)
+    
+    If all examples are from the same class, it uses a one-class SVM.
+    
+    # Generate data
+X, y = make_blobs(n_samples=1000, n_features=2, random_state=42,
+                  cluster_std=5.0)
+X_train, y_train = X[:600], y[:600]
+X_valid, y_valid = X[600:800], y[600:800]
+X_train_valid, y_train_valid = X[:800], y[:800]
+X_test, y_test = X[800:], y[800:]
+    
+    
+n_train = 20  # samples for training
+n_test = 200  # samples for testing
+n_averages = 50  # how often to repeat classification
+n_features_max = 75  # maximum number of features
+step = 4  # step size for the calculation
