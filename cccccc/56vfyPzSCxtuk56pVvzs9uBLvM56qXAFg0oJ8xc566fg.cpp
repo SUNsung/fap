fd7fdb0bd10721f2f2b3a20aad8df52f0e69703d
SUNsung <weatherius@gmail.com>
2018-03-08 13:10:58 +0200

    Rewrite [56vfyPzSCxtuk56pVvzs9uBLvM56qXAFg0oJ8xc566fg.cpp] File in: 2018-03-08 13:10:57

--- cccccc/56vfyPzSCxtuk56pVvzs9uBLvM56qXAFg0oJ8xc566fg.cpp
+++ cccccc/56vfyPzSCxtuk56pVvzs9uBLvM56qXAFg0oJ8xc566fg.cpp
@@ -1,192 +1,147 @@
 
         
-            return;
-  } else if (method == 'ClearCache') {
-    ClearCache(GetRenderProcessHost());
-    return;
-  } else if (method == 'CreateShortcut') {
-#if defined(OS_WIN)
-    base::string16 path;
-    arguments.GetString(0, &path);
-    
-      void SetUpCommandLine(base::CommandLine* command_line) override {
-    command_line->AppendSwitch(switches::kUseFakeDeviceForMediaStream);
-    command_line->AppendSwitchASCII(switches::kJavaScriptFlags, '--expose-gc');
-    }
-    
-    // Sets up the CHILD_PROCESS_EXE path to properly point to the helper app.
-void OverrideChildProcessPath();
+          // Implement the abstract interface. All these functions fill the output
+  // with a copy of FileDescriptorProto.
     
     
-    {  switch (item_id) {
-    case WebAutofillClient::MenuItemIDAutocompleteEntry:
-    case WebAutofillClient::MenuItemIDPasswordEntry:
-      // User selected an Autocomplete or password entry, so we fill directly.
-      SetNodeText(value, &element_);
-      break;
-    case WebAutofillClient::MenuItemIDDataListEntry:
-      AcceptDataListSuggestion(value);
-      break;
-  }
-}
+    {}  // namespace google
+
     
-    // Call method of an object in browser.
-// function CallObjectMethod(id, type, method, args);
-v8::Handle<v8::Value> CallObjectMethod(int routing_id,
-                                       int object_id,
-                                       const std::string& type,
-                                       const std::string& method,
-                                       v8::Handle<v8::Value> args);
+    #endif  // GOOGLE_PROTOBUF_COMPILER_CSHARP_ENUM_H__
     
-    
-    {}  // namespace nwapi
+    #include <google/protobuf/compiler/code_generator.h>
+#include <google/protobuf/compiler/plugin.h>
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/descriptor.pb.h>
+#include <google/protobuf/io/printer.h>
+#include <google/protobuf/io/zero_copy_stream.h>
     
     #include <string>
-#include <set>
     
-    class BaseEvent {
-  friend class EventListener;
-  DISALLOW_COPY_AND_ASSIGN(BaseEvent);
-    }
+    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
+//   can only really compare the output against a golden value, which is a
+//   fairly tedious and fragile testing strategy.  If we want to go that route,
+//   it probably makes sense to bite the bullet and write a test that compares
+//   the whole generated output for unittest.proto against a golden value, with
+//   a very simple script that can be run to regenerate it with the latest code.
+//   This would mean that updates to the golden file would have to be included
+//   in any change to the code generator, which would actually be fairly useful
+//   as it allows the reviewer to see clearly how the generated code is
+//   changing.
     
-    namespace {
-    }
+    DEFINE_string(sys_info, '', 'System information');
     
-    aura::Window* Menu::GetActiveNativeView(content::RenderFrameHost* rfh) {
-  content::WebContents* web_contents =
-    content::WebContents::FromRenderFrameHost(rfh);
-  if (!web_contents) {
-    LOG(ERROR) << 'Menu: couldn't find WebContents';
-    return NULL;
-  }
-  return web_contents->GetFullscreenRenderWidgetHostView()
-             ? web_contents->GetFullscreenRenderWidgetHostView()
-                   ->GetNativeView()
-             : web_contents->GetNativeView();
-}
+    #include <gflags/gflags.h>
+#include <grpc/grpc.h>
+#include <grpc/support/time.h>
     
-            if (pD->get_bits_no_markers(1))
-          s = p1;
-        else
-          s = m1;
+    
+    {
+    {        // And we are done! Let the gRPC runtime know we've finished, using the
+        // memory address of this instance as the uniquely identifying tag for
+        // the event.
+        status_ = FINISH;
+        responder_.Finish(reply_, Status::OK, this);
+      } else {
+        GPR_ASSERT(status_ == FINISH);
+        // Once in the FINISH state, deallocate ourselves (CallData).
+        delete this;
       }
-      else
-      {
-        if (r != 15)
-        {
-          pD->m_eob_run = 1 << r;
     }
     
-    static const vorbis_residue_template _res_8s_0[]={
-  {2,0,32,  &_residue_44_mid,
-   &_huff_book__8c0_s_single,&_huff_book__8c0_s_single,
-   &_resbook_8s_0,&_resbook_8s_0},
-};
-static const vorbis_residue_template _res_8s_1[]={
-  {2,0,32,  &_residue_44_mid,
-   &_huff_book__8c1_s_single,&_huff_book__8c1_s_single,
-   &_resbook_8s_1,&_resbook_8s_1},
-};
+      Status ListFeatures(ServerContext* context,
+                      const routeguide::Rectangle* rectangle,
+                      ServerWriter<Feature>* writer) override {
+    auto lo = rectangle->lo();
+    auto hi = rectangle->hi();
+    long left = (std::min)(lo.longitude(), hi.longitude());
+    long right = (std::max)(lo.longitude(), hi.longitude());
+    long top = (std::max)(lo.latitude(), hi.latitude());
+    long bottom = (std::min)(lo.latitude(), hi.latitude());
+    for (const Feature& f : feature_list_) {
+      if (f.location().longitude() >= left &&
+          f.location().longitude() <= right &&
+          f.location().latitude() >= bottom &&
+          f.location().latitude() <= top) {
+        writer->Write(f);
+      }
+    }
+    return Status::OK;
+  }
     
-     ********************************************************************/
+      // Loads a JPEG image from a memory buffer or a file.
+  // req_comps can be 1 (grayscale), 3 (RGB), or 4 (RGBA).
+  // On return, width/height will be set to the image's dimensions, and actual_comps will be set to the either 1 (grayscale) or 3 (RGB).
+  // Notes: For more control over where and how the source data is read, see the decompress_jpeg_image_from_stream() function below, or call the jpeg_decoder class directly.
+  // Requesting a 8 or 32bpp image is currently a little faster than 24bpp because the jpeg_decoder class itself currently always unpacks to either 8 or 32bpp.
+  unsigned char *decompress_jpeg_image_from_memory(const unsigned char *pSrc_data, int src_data_size, int *width, int *height, int *actual_comps, int req_comps);
+  unsigned char *decompress_jpeg_image_from_file(const char *pSrc_filename, int *width, int *height, int *actual_comps, int req_comps);
     
-    /* We currently support 4 ARM variants:
- * arch[0] -> ARMv4
- * arch[1] -> ARMv5E
- * arch[2] -> ARMv6
- * arch[3] -> NEON
- */
-#define OPUS_ARCHMASK 3
+    #endif
+
     
-    #undef MULT32_32_Q31
-static inline int MULT32_32_Q31(int a, int b)
+    
+int             oc_has_mode_metrics;
+oc_mode_metrics OC_MODE_METRICS[64][3][2][OC_SAD_BINS];
+# endif
+    
+    
+#undef MULT16_32_Q16
+static inline int MULT16_32_Q16(int a, int b)
 {
-    int r;
-    asm volatile('MULT $ac1, %0, %1' : : 'r' (a), 'r' (b));
-    asm volatile('EXTR.W %0,$ac1, %1' : '=r' (r): 'i' (31));
-    return r;
+    int c;
+    asm volatile('MULT $ac1,%0, %1' : : 'r' (a), 'r' (b));
+    asm volatile('EXTR.W %0,$ac1, %1' : '=r' (c): 'i' (16));
+    return c;
 }
     
-    #undef silk_MLA_uint
-static OPUS_INLINE opus_int32 silk_MLA_uint(opus_uint32 a32, opus_uint32 b32, opus_uint32 c32){
-    opus_uint32 ret;
-    ops_count += 4;
-    ret = a32 + b32 * c32;
-    return ret;
+    bool FilterBlockReader::KeyMayMatch(uint64_t block_offset, const Slice& key) {
+  uint64_t index = block_offset >> base_lg_;
+  if (index < num_) {
+    uint32_t start = DecodeFixed32(offset_ + index*4);
+    uint32_t limit = DecodeFixed32(offset_ + index*4 + 4);
+    if (start <= limit && limit <= static_cast<size_t>(offset_ - data_)) {
+      Slice filter = Slice(data_ + start, limit - start);
+      return policy_->KeyMayMatch(key, filter);
+    } else if (start == limit) {
+      // Empty filters do not match any keys
+      return false;
+    }
+  }
+  return true;  // Errors are treated as potential matches
 }
     
-        if (sub_batch->Has(String::NewSymbol('column_family'))) {
-      if (!has_key_for_array(sub_batch, 'put') &&
-          !has_key_for_array(sub_batch, 'delete')) {
-        return scope.Close(Boolean::New(false));
+    static void TestEncodeDecode(const VersionEdit& edit) {
+  std::string encoded, encoded2;
+  edit.EncodeTo(&encoded);
+  VersionEdit parsed;
+  Status s = parsed.DecodeFrom(encoded);
+  ASSERT_TRUE(s.ok()) << s.ToString();
+  parsed.EncodeTo(&encoded2);
+  ASSERT_EQ(encoded, encoded2);
+}
+    
+          if (offset != 0) {
+        // There is some room in the last block.
+        avail = kBlockSize - offset;
+      } else {
+        // No room in the last block; push new one.
+        blocks_.push_back(new char[kBlockSize]);
+        avail = kBlockSize;
       }
-    }
+    
+    #endif  // STORAGE_LEVELDB_TABLE_FILTER_BLOCK_H_
+
     
     
-    {    } else if (query == GET_OP) {
-      // Assumes that a non-existent key just returns <empty>
-      std::string res;
-      slists.Get(key, &res);
-      ASSERT_EQ(res, parallel_copy[key]);
-    }
-    
-    // Insert element value into (list: key), right before/after
-//  the first occurrence of pivot
-//   : throws RedisListException
-int RedisLists::Insert(const std::string& key, const std::string& pivot,
-                       const std::string& value, bool insert_after) {
-  // Get the original list data
-  std::string data;
-  db_->Get(get_option_, key, &data);
-    }
-    
-      /// Remove the first (or last) num occurrences of value from the list (key)
-  /// Return the number of elements removed.
-  /// May throw RedisListException
-  int Remove(const std::string& key, int32_t num,
-             const std::string& value);
-  int RemoveFirst(const std::string& key, int32_t num,
-                  const std::string& value);
-  int RemoveLast(const std::string& key, int32_t num,
-                 const std::string& value);
-    
-    /*
- * Class:     org_rocksdb_IngestExternalFileOptions
- * Method:    allowGlobalSeqNo
- * Signature: (J)Z
- */
-jboolean Java_org_rocksdb_IngestExternalFileOptions_allowGlobalSeqNo(
-    JNIEnv* env, jobject jobj, jlong jhandle) {
-  auto* options =
-      reinterpret_cast<rocksdb::IngestExternalFileOptions*>(jhandle);
-  return static_cast<jboolean>(options->allow_global_seqno);
-}
-    
-    
-    {
-    {      if (child.Valid()) {
-        PERF_TIMER_GUARD(seek_min_heap_time);
-        minHeap_.push(&child);
-      }
-    }
-    direction_ = kForward;
-    {
-      PERF_TIMER_GUARD(seek_min_heap_time);
-      current_ = CurrentForward();
-    }
-  }
-    
-    class MergingIterator;
-    
-      jstring jname =
-      (jstring)env->CallObjectMethod(m_jcallback_obj, jname_method_id);
-  if(env->ExceptionCheck()) {
-    // exception thrown
-    return;
-  }
-  jboolean has_exception = JNI_FALSE;
-  m_name = JniUtil::copyString(env, jname, &has_exception);  // also releases jname
-  if (has_exception == JNI_TRUE) {
-    // exception thrown
-    return;
-  }
+  // Iterator interface methods
+  bool Valid() const        { return valid_; }
+  Slice key() const         { assert(Valid()); return key_; }
+  Slice value() const       { assert(Valid()); return iter_->value(); }
+  // Methods below require iter() != NULL
+  Status status() const     { assert(iter_); return iter_->status(); }
+  void Next()               { assert(iter_); iter_->Next();        Update(); }
+  void Prev()               { assert(iter_); iter_->Prev();        Update(); }
+  void Seek(const Slice& k) { assert(iter_); iter_->Seek(k);       Update(); }
+  void SeekToFirst()        { assert(iter_); iter_->SeekToFirst(); Update(); }
+  void SeekToLast()         { assert(iter_); iter_->SeekToLast();  Update(); }
