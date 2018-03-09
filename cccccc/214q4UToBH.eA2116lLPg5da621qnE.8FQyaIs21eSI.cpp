30ca3a3156c8580a0d4dab3d0d5c756a17ce2cb2
SUNsung <weatherius@gmail.com>
2018-03-09 14:08:25 +0200

    Rewrite [214q4UToBH.eA2116lLPg5da621qnE.8FQyaIs21eSI.cpp] File in: 2018-03-09 14:08:24

--- cccccc/214q4UToBH.eA2116lLPg5da621qnE.8FQyaIs21eSI.cpp
+++ cccccc/214q4UToBH.eA2116lLPg5da621qnE.8FQyaIs21eSI.cpp
@@ -1,226 +1,235 @@
 
         
-        #endif // defined(BOOST_ASIO_HAS_STD_ARRAY) || defined(GENERATING_DOCUMENTATION)
-    
-      descriptor_write_op(int descriptor,
-      const ConstBufferSequence& buffers, Handler& handler)
-    : descriptor_write_op_base<ConstBufferSequence>(
-        descriptor, buffers, &descriptor_write_op::do_complete),
-      handler_(BOOST_ASIO_MOVE_CAST(Handler)(handler))
-  {
-  }
-    
-      ~winrt_buffer_impl()
-  {
-  }
-    
-      template <typename T>
-  static std::shared_ptr<T> get_shared_ptr_from_counted_base(
-      counted_base* base,
-      bool inc = true) {
-    if (!base) {
-      return nullptr;
-    }
-    std::shared_ptr<const void> newp;
-    if (inc) {
-      inc_shared_count(base, 1);
-    }
-    newp.*fieldPtr(access_shared_ptr_ptr{}) =
-        get_shared_ptr<const void>(base); // _M_ptr
-    (newp.*fieldPtr(access_refcount{})).*fieldPtr(access_base{}) = base;
-    // reinterpret_pointer_cast<T>
-    auto res = reinterpret_cast<std::shared_ptr<T>*>(&newp);
-    return std::move(*res);
-  }
+          /**
+   * @brief Applies the transformation defined in the data layer's
+   * transform_param block to the data.
+   *
+   * @param datum
+   *    Datum containing the data to be transformed.
+   * @param transformed_blob
+   *    This is destination blob. It can be part of top blob's data if
+   *    set_cpu_data() is used. See data_layer.cpp for an example.
+   */
+  void Transform(const Datum& datum, Blob<Dtype>* transformed_blob);
     
     
-    {
-    {} // namespace detail
-} // namespace folly
+    {  Blob<Dtype> col_buffer_;
+  Blob<Dtype> bias_multiplier_;
+};
+    
+     protected:
+  /// @copydoc BNLLLayer
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    
+    #endif  // CAFFE_CONCAT_LAYER_HPP_
 
     
-    TEST(MemoryIdler, futexWaitAwokenLate) {
-  StrictMock<Futex<MockAtom>> fut;
-  auto clock = MockClock::setup();
-  auto begin = MockClock::time_point(std::chrono::seconds(100));
-  auto idleTimeout = MemoryIdler::defaultIdleTimeout.load();
+    namespace caffe {
     }
     
-    #include <bitset>
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
     
-      // This load is safe because needles.size() >= 16
-  auto arr2 = _mm_loadu_si128(reinterpret_cast<const __m128i*>(needles.data()));
-  auto b =
-      _mm_cmpestri(arr2, 16, arr1, int(haystack.size() - blockStartIdx), 0);
-    
-    template <IdentifyCallable::Kind, typename Fn>
-struct ArgumentTypesByKind {};
-template <typename Fn>
-struct ArgumentTypesByKind<IdentifyCallable::Kind::MemberFunction, Fn> {
-  using type = typename boost::mpl::template pop_front<
-    typename boost::function_types::template parameter_types<
-      decltype(&Fn::operator())
-    >::type
-  >::type;
-};
-template <typename Fn>
-struct ArgumentTypesByKind<IdentifyCallable::Kind::Function, Fn> {
-  using type = typename boost::function_types::template parameter_types<Fn>;
-};
-    
-    namespace detail {
-std::string toPseudoJson(const folly::dynamic& d) {
-  std::stringstream ss;
-  ss << d;
-  return ss.str();
-}
-} // namespace detail
-    
-       private:
-    friend struct Pop;
-    
-    // Errors & exceptions are best tested separately, but squeezing all the
-// features into one test is good for exercising nesting.
-TEST(TestDynamicParser, AllParserFeaturesSuccess) {
-  // Input
-  auto d = dynamic::array(
-    dynamic::object('a', 7)('b', 9)('c', 13.3),
-    5,
-    dynamic::array('x', 'y', 1, 'z'),
-    dynamic::object('int', 7)('false', 0)('true', true)('str', 's'),
-    dynamic::object('bools', dynamic::array(false, true, 0, 1))
-  );
-  // Outputs, in the same order as the inputs.
-  std::map<std::string, double> doubles;
-  folly::Optional<int64_t> outer_int;
-  std::vector<std::string> strings;
-  folly::Optional<int64_t> inner_int;
-  folly::Optional<bool> inner_false;
-  folly::Optional<bool> inner_true;
-  folly::Optional<std::string> inner_str;
-  std::vector<bool> bools;
-  // Parse and verify some invariants
-  DynamicParser p(DynamicParser::OnError::RECORD, &d);
-  EXPECT_EQ(d, p.value());
-  p.required(0, [&](const dynamic& v) {
-    EXPECT_EQ(0, p.key().getInt());
-    EXPECT_EQ(v, p.value());
-    p.objectItems([&](const std::string& k, double v) {
-      EXPECT_EQ(k, p.key().getString());
-      EXPECT_EQ(v, p.value().asDouble());
-      doubles.emplace(k, v);
-    });
-  });
-  p.required(1, [&](int64_t k, int64_t v) {
-    EXPECT_EQ(1, k);
-    EXPECT_EQ(1, p.key().getInt());
-    EXPECT_EQ(5, p.value().getInt());
-    outer_int = v;
-  });
-  p.optional(2, [&](const dynamic& v) {
-    EXPECT_EQ(2, p.key().getInt());
-    EXPECT_EQ(v, p.value());
-    p.arrayItems([&](int64_t k, const std::string& v) {
-      EXPECT_EQ(strings.size(), k);
-      EXPECT_EQ(k, p.key().getInt());
-      EXPECT_EQ(v, p.value().asString());
-      strings.emplace_back(v);
-    });
-  });
-  p.required(3, [&](const dynamic& v) {
-    EXPECT_EQ(3, p.key().getInt());
-    EXPECT_EQ(v, p.value());
-    p.optional('int', [&](const std::string& k, int64_t v) {
-      EXPECT_EQ('int', p.key().getString());
-      EXPECT_EQ(k, p.key().getString());
-      EXPECT_EQ(v, p.value().getInt());
-      inner_int = v;
-    });
-    p.required('false', [&](const std::string& k, bool v) {
-      EXPECT_EQ('false', p.key().getString());
-      EXPECT_EQ(k, p.key().getString());
-      EXPECT_EQ(v, p.value().asBool());
-      inner_false = v;
-    });
-    p.required('true', [&](const std::string& k, bool v) {
-      EXPECT_EQ('true', p.key().getString());
-      EXPECT_EQ(k, p.key().getString());
-      EXPECT_EQ(v, p.value().getBool());
-      inner_true = v;
-    });
-    p.required('str', [&](const std::string& k, const std::string& v) {
-      EXPECT_EQ('str', p.key().getString());
-      EXPECT_EQ(k, p.key().getString());
-      EXPECT_EQ(v, p.value().getString());
-      inner_str = v;
-    });
-    p.optional('not set', [&](bool) { FAIL() << 'No key 'not set''; });
-  });
-  p.required(4, [&](const dynamic& v) {
-    EXPECT_EQ(4, p.key().getInt());
-    EXPECT_EQ(v, p.value());
-    p.optional('bools', [&](const std::string& k, const dynamic& v2) {
-      EXPECT_EQ(std::string('bools'), k);
-      EXPECT_EQ(k, p.key().getString());
-      EXPECT_EQ(v2, p.value());
-      p.arrayItems([&](int64_t k, bool v3) {
-        EXPECT_EQ(bools.size(), k);
-        EXPECT_EQ(k, p.key().getInt());
-        EXPECT_EQ(v3, p.value().asBool());
-        bools.push_back(v3);
-      });
-    });
-  });
-  p.optional(5, [&](int64_t) { FAIL() << 'Index 5 does not exist'; });
-  // Confirm the parse
-  EXPECT_EQ(dynamic(dynamic::object()), p.releaseErrors());
-  EXPECT_EQ((decltype(doubles){{'a', 7.}, {'b', 9.}, {'c', 13.3}}), doubles);
-  EXPECT_EQ((int64_t)5, outer_int);
-  EXPECT_EQ((decltype(strings){'x', 'y', '1', 'z'}), strings);
-  EXPECT_EQ((int64_t)7, inner_int);
-  EXPECT_FALSE(inner_false.value());
-  EXPECT_TRUE(inner_true.value());
-  EXPECT_EQ(std::string('s'), inner_str);
-  EXPECT_EQ(std::string('s'), inner_str);
-  EXPECT_EQ((decltype(bools){false, true, false, true}), bools);
-}
-    
-    const std::string& NestedCommandLineApp::resolveAlias(
-    const std::string& name) const {
-  auto dest = &name;
-  for (;;) {
-    auto pos = aliases_.find(*dest);
-    if (pos == aliases_.end()) {
-      break;
+    namespace caffe {
     }
-    dest = &pos->second;
+    
+      /**
+   * @brief Computes the error gradient w.r.t. the forwarded inputs.
+   *
+   * @param top output Blob vector (length 1+), providing the error gradient with
+   *        respect to the outputs
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length 2+), into which the top error
+   *        gradient is copied
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #define _CRT_NONSTDC_NO_DEPRECATE // make VS accept POSIX functions without _
+    
+    // ---------------------------------------------------------------------------
+// ConfigException -- all errors from processing the config files are reported as ConfigException
+// ---------------------------------------------------------------------------
+    
+    #pragma once
+    
+    public:
+    typedef msra::dbn::latticepair latticepair;
+    latticesource(std::pair<std::vector<std::wstring>, std::vector<std::wstring>> latticetocs, const std::unordered_map<std::string, size_t>& modelsymmap, std::wstring RootPathInToc)
+        : numlattices(latticetocs.first, modelsymmap, RootPathInToc), denlattices(latticetocs.second, modelsymmap, RootPathInToc), verbosity(0)
+    {
+    }
+    
+    // ===========================================================================
+// float4 -- wrapper around the rather ugly SSE intrinsics for float[4]
+//
+// Do not use the intrinsics outside anymore; instead add all you need into this class.
+//
+// MSDN links:
+// basic: http://msdn.microsoft.com/en-us/library/x5c07e2a%28v=VS.80%29.aspx
+// load/store: (add this)
+// newer ones: (seems no single list available)
+// ===========================================================================
+    
+    vector<wstring> /*IConfigRecord::*/ ComputationNodeBase::GetMemberIds() const
+{
+    return vector<wstring>{ L'name', L'operation', L'dim', L'dims', /*L'tag', */L'inputs', OperationName() + L'Args' };
+}
+    
+    int main(int, char**)
+{
+    // Setup Allegro
+    al_init();
+    al_install_keyboard();
+    al_install_mouse();
+    al_init_primitives_addon();
+    al_set_new_display_flags(ALLEGRO_RESIZABLE);
+    ALLEGRO_DISPLAY* display = al_create_display(1280, 720);
+    al_set_window_title(display, 'ImGui Allegro 5 example');
+    ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
+    al_register_event_source(queue, al_get_display_event_source(display));
+    al_register_event_source(queue, al_get_keyboard_event_source());
+    al_register_event_source(queue, al_get_mouse_event_source());
+    }
+    
+        // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them. 
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple. 
+    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Read 'misc/fonts/README.txt' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Roboto-Medium.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Cousine-Regular.ttf', 15.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/DroidSans.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/ProggyTiny.ttf', 10.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF('c:\\Windows\\Fonts\\ArialUni.ttf', 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+    //IM_ASSERT(font != NULL);
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you use this binding you'll need to call 5 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXX_CreateFontsTexture(), ImGui_ImplXXXX_NewFrame(), ImGui_ImplXXXX_Render() and ImGui_ImplXXXX_Shutdown().
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+      /// Search the list for the (index)'th item (0-based) in (list:key)
+  /// A negative index indicates: 'from end-of-list'
+  /// If index is within range: return true, and return the value in *result.
+  /// If (index < -length OR index>=length), then index is out of range:
+  ///   return false (and *result is left unchanged)
+  /// May throw RedisListException
+  bool Index(const std::string& key, int32_t index,
+             std::string* result);
+    
+    /*
+ * Class:     org_rocksdb_IngestExternalFileOptions
+ * Method:    newIngestExternalFileOptions
+ * Signature: ()J
+ */
+jlong Java_org_rocksdb_IngestExternalFileOptions_newIngestExternalFileOptions__(
+    JNIEnv* env, jclass jclazz) {
+  auto* options = new rocksdb::IngestExternalFileOptions();
+  return reinterpret_cast<jlong>(options);
+}
+    
+    #include 'rocksjni/statisticsjni.h'
+    
+    Status DateTieredDBImpl::Write(const WriteOptions& opts, WriteBatch* updates) {
+  class Handler : public WriteBatch::Handler {
+   public:
+    explicit Handler() {}
+    WriteBatch updates_ttl;
+    Status batch_rewrite_status;
+    virtual Status PutCF(uint32_t column_family_id, const Slice& key,
+                         const Slice& value) override {
+      WriteBatchInternal::Put(&updates_ttl, column_family_id, key, value);
+      return Status::OK();
+    }
+    virtual Status MergeCF(uint32_t column_family_id, const Slice& key,
+                           const Slice& value) override {
+      WriteBatchInternal::Merge(&updates_ttl, column_family_id, key, value);
+      return Status::OK();
+    }
+    virtual Status DeleteCF(uint32_t column_family_id,
+                            const Slice& key) override {
+      WriteBatchInternal::Delete(&updates_ttl, column_family_id, key);
+      return Status::OK();
+    }
+    virtual void LogData(const Slice& blob) override {
+      updates_ttl.PutLogData(blob);
+    }
+  };
+  Handler handler;
+  updates->Iterate(&handler);
+  if (!handler.batch_rewrite_status.ok()) {
+    return handler.batch_rewrite_status;
+  } else {
+    return db_->Write(opts, &(handler.updates_ttl));
   }
-  return *dest;
 }
     
-    #include <folly/experimental/NestedCommandLineApp.h>
-    
-    #include <string>
-    
-    
-    {} // namespace aria2
-    
-      std::shared_ptr<Peer> peer_;
-    
-    namespace aria2 {
+    void DupAndCloseStderr() {
+  int OutputFd = DuplicateFile(2);
+  if (OutputFd > 0) {
+    FILE *NewOutputFile = OpenFile(OutputFd, 'w');
+    if (NewOutputFile) {
+      OutputFile = NewOutputFile;
+      if (EF->__sanitizer_set_report_fd)
+        EF->__sanitizer_set_report_fd(reinterpret_cast<void *>(OutputFd));
+      CloseFile(2);
     }
+  }
+}
     
-    #include <algorithm>
+    void DupAndCloseStderr();
     
-    namespace aria2 {
-    }
+    std::string DirName(const std::string &FileName) {
+  char *Tmp = new char[FileName.size() + 1];
+  memcpy(Tmp, FileName.c_str(), FileName.size() + 1);
+  std::string Res = dirname(Tmp);
+  delete [] Tmp;
+  return Res;
+}
     
-    private:
-  void startAsyncFamily(const std::string& hostname, int family,
-                        DownloadEngine* e, Command* command);
-  void setNameResolverCheck(size_t resolverIndex, DownloadEngine* e,
-                            Command* command);
-  void disableNameResolverCheck(size_t index, DownloadEngine* e,
-                                Command* command);
+    using namespace std::chrono;
     
-    AuthConfig::AuthConfig() {}
+    #endif  // LLVM_FUZZER_OPTIONS_H
+
+    
+    #ifndef SHA_BIG_ENDIAN
+	// Swap byte order back
+	int i;
+	for (i=0; i<5; i++) {
+		s->state[i]=
+			  (((s->state[i])<<24)& 0xff000000)
+			| (((s->state[i])<<8) & 0x00ff0000)
+			| (((s->state[i])>>8) & 0x0000ff00)
+			| (((s->state[i])>>24)& 0x000000ff);
+	}
+#endif
+    
+    // Computes SHA1 hash of 'Len' bytes in 'Data', writes kSHA1NumBytes to 'Out'.
+void ComputeSHA1(const uint8_t *Data, size_t Len, uint8_t *Out);
+    
+      void TraceSwitchCallback(uintptr_t PC, size_t ValSizeInBits, uint64_t Val,
+                           size_t NumCases, uint64_t *Cases);
+  int TryToAddDesiredData(uint64_t PresentData, uint64_t DesiredData,
+                          size_t DataSize);
+  int TryToAddDesiredData(const uint8_t *PresentData,
+                          const uint8_t *DesiredData, size_t DataSize);
+    
+    bool ExecuteCommandAndReadOutput(const std::string &Command, std::string *Out) {
+  FILE *Pipe = OpenProcessPipe(Command.c_str(), 'r');
+  if (!Pipe) return false;
+  char Buff[1024];
+  size_t N;
+  while ((N = fread(Buff, 1, sizeof(Buff), Pipe)) > 0)
+    Out->append(Buff, N);
+  return true;
+}
