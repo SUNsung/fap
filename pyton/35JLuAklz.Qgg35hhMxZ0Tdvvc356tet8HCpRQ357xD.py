6bfa44f4de07692d22e6f0e949b821b67f6dea13
SUNsung <weatherius@gmail.com>
2018-03-09 10:23:10 +0200

    Rewrite [35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py] File in: 2018-03-09 10:23:09

--- pyton/35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py
+++ pyton/35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py
@@ -1,135 +1,130 @@
 
         
-        # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+        containers = (('thefuck/python3-tcsh',
+               u'''FROM python:3
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'),
+              ('thefuck/python2-tcsh',
+               u'''FROM python:2
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'))
     
     
-def register_cli(app):
-    @app.cli.command('initdb')
-    def initdb_command():
-        '''Creates the database tables.'''
-        init_db()
-        print('Initialized the database.')
+def check_format(filename):
+    '''
+    validates that each line is formatted correctly,
+    appending to error list as needed
+    '''
+    with open(filename) as fp:
+        lines = list(line.rstrip() for line in fp)
+    check_alphabetical(lines)
+    # START Check Entries
+    num_in_category = min_entries_per_section + 1
+    category = ''
+    category_line = 0
+    anchor_re = re.compile('###\s\S+')
+    for line_num, line in enumerate(lines):
+        # check each section for the minimum number of entries
+        if line.startswith(anchor):
+            if not anchor_re.match(line):
+                add_error(line_num, 'section header is not formatted correctly')
+            if num_in_category < min_entries_per_section:
+                add_error(category_line, '{} section does not have the minimum {} entries (only has {})'.format(
+                    category, min_entries_per_section, num_in_category))
+            category = line.split(' ')[1]
+            category_line = line_num
+            num_in_category = 0
+            continue
+        if not line.startswith('|') or line.startswith('|---'):
+            continue
+        num_in_category += 1
+        segments = line.split('|')[1:-1]
+        # START Global
+        for segment in segments:
+            # every line segment should start and end with exactly 1 space
+            if len(segment) - len(segment.lstrip()) != 1 or len(segment) - len(segment.rstrip()) != 1:
+                add_error(line_num, 'each segment must start and end with exactly 1 space')
+        # END Global
+        segments = [seg.strip() for seg in segments]
+        check_entry(line_num, segments)
+    # END Check Entries
     
-    if __name__ == '__main__':
-    app.run()
-
+    
+def jsonify(result, format=False):
+    ''' format JSON output (uncompressed or uncompressed) '''
     
     
-def head(url, **kwargs):
-    r'''Sends a HEAD request.
     
-        def __repr__(self):
-        return str(dict(self.items()))
+        def reducer(self, key, value):
+        '''Sum values for each key.
+    
+        def __init__(self, MAX_SIZE):
+        self.MAX_SIZE = MAX_SIZE
+        self.size = 0
+        self.lookup = {}  # key: query, value: node
+        self.linked_list = LinkedList()
+    
+      # Remove old YCM libs if present so that YCM can start.
+  old_libs = (
+    glob.glob( p.join( DIR_OF_OLD_LIBS, '*ycm_core.*' ) ) +
+    glob.glob( p.join( DIR_OF_OLD_LIBS, '*ycm_client_support.*' ) ) +
+    glob.glob( p.join( DIR_OF_OLD_LIBS, '*clang*.*') ) )
+  for lib in old_libs:
+    os.remove( lib )
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
     
     
-class CookieConflictError(RuntimeError):
-    '''There are two cookies that meet the criteria specified in the cookie jar.
-    Use .get and .set and include domain and path args in order to be more specific.
+# This class can be used to keep the ycmd server alive for the duration of the
+# life of the client. By default, ycmd shuts down if it doesn't see a request in
+# a while.
+class YcmdKeepalive( object ):
+  def __init__( self, ping_interval_seconds = 60 * 10 ):
+    self._keepalive_thread = Thread( target = self._ThreadMain )
+    self._keepalive_thread.daemon = True
+    self._ping_interval_seconds = ping_interval_seconds
+    
+    
+  def IsAllowed( self, diagnostic ):
+    # NOTE: in this class's implementation, we ask vimsupport for
+    #  the current filetypes and delegate automatically; it is probably,
+    #  more efficient, however, to call SubsetForTypes() and reuse
+    #  the returned DiagnosticFilter if it will be checked repeatedly.
+    filetypes = vimsupport.CurrentFiletypes()
+    return self.SubsetForTypes( filetypes ).IsAllowed( diagnostic )
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+        config['pylons.h'] = r2.lib.helpers
+    config['routes.map'] = make_map(config)
+    
+        `app_conf`
+        The application's local configuration. Normally specified in the
+        [app:<name>] section of the Paste ini file (where <name> defaults to
+        main).
     '''
     
-    '''
-requests.exceptions
-~~~~~~~~~~~~~~~~~~~
+        @validate(VAdmin(),
+              award = VAwardByCodename('awardcn'))
+    def GET_winners(self, award):
+        if award is None:
+            abort(404, 'page not found')
     
-        with server as (host, port):
-        url = 'http://{0}:{1}/'.format(host, port)
-        r = requests.get(url, auth=auth)
-        # Verify server succeeded in authenticating.
-        assert r.status_code == 200
-        # Verify Authorization was sent in final request.
-        assert 'Authorization' in r.request.headers
-        assert r.request.headers['Authorization'].startswith('Digest ')
-        # Verify redirect happened as we expected.
-        assert r.history[0].status_code == 302
-        close_server.set()
+            The user's response to the CAPTCHA should be sent as `captcha`
+        along with your request.
     
-        scikit_classifier_results = []
-    scikit_regressor_results = []
-    n = 10
-    step = 500
-    start_dim = 500
-    n_classes = 10
-    
-        url_fmt is along the lines of ('https://github.com/USER/PROJECT/'
-                                   'blob/{revision}/{package}/'
-                                   '{path}#L{lineno}')
-    '''
-    revision = _get_git_revision()
-    return partial(_linkcode_resolve, revision=revision, package=package,
-                   url_fmt=url_fmt)
-
-    
-    import os
-import tarfile
-from contextlib import closing
-    
-    # Print the classification report
-print(metrics.classification_report(y_test, y_predicted,
-                                    target_names=dataset.target_names))
-    
-            C = float(self.complexity.get())
-        gamma = float(self.gamma.get())
-        coef0 = float(self.coef0.get())
-        degree = int(self.degree.get())
-        kernel_map = {0: 'linear', 1: 'rbf', 2: 'poly'}
-        if len(np.unique(y)) == 1:
-            clf = svm.OneClassSVM(kernel=kernel_map[self.kernel.get()],
-                                  gamma=gamma, coef0=coef0, degree=degree)
-            clf.fit(X)
-        else:
-            clf = svm.SVC(kernel=kernel_map[self.kernel.get()], C=C,
-                          gamma=gamma, coef0=coef0, degree=degree)
-            clf.fit(X, y)
-        if hasattr(clf, 'score'):
-            print('Accuracy:', clf.score(X, y) * 100)
-        X1, X2, Z = self.decision_surface(clf)
-        self.model.clf = clf
-        self.model.set_surface((X1, X2, Z))
-        self.model.surface_type = self.surface_type.get()
-        self.fitted = True
-        self.model.changed('surface')
-    
-    from sklearn import datasets
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import LinearSVC
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import (brier_score_loss, precision_score, recall_score,
-                             f1_score)
-from sklearn.calibration import CalibratedClassifierCV, calibration_curve
-from sklearn.model_selection import train_test_split
-    
-    The base classifier is a random forest classifier with 25 base estimators
-(trees). If this classifier is trained on all 800 training datapoints, it is
-overly confident in its predictions and thus incurs a large log-loss.
-Calibrating an identical classifier, which was trained on 600 datapoints, with
-method='sigmoid' on the remaining 200 datapoints reduces the confidence of the
-predictions, i.e., moves the probability vectors from the edges of the simplex
-towards the center. This calibration results in a lower log-loss. Note that an
-alternative would have been to increase the number of base estimators which
-would have resulted in a similar decrease in log-loss.
-'''
-print(__doc__)
-    
-    plt.title('Clustering measures for 2 random uniform labelings\n'
-          'with equal number of clusters')
-plt.xlabel('Number of clusters (Number of samples is fixed to %d)' % n_samples)
-plt.ylabel('Score value')
-plt.legend(plots, names)
-plt.ylim(ymin=-0.05, ymax=1.05)
-    
-        strings = []
-    with open(filepath, 'r') as f:
-    
-            with open(self.log_path, 'r') as fd:
-            content = fd.read()
-            return content
-    
-    ## Anything on different channel than DEFAULT_CHANNEL is not parsed
-# by parser.
-HIDDEN_CHANNEL = 99
-    
-            Terence implemented packed table initializers, because Java has a
-        size restriction on .class files and the lookup tables can grow
-        pretty large. The generated JavaLexer.java of the Java.g example
-        would be about 15MB with uncompressed array initializers.
+        def GET_blog(self):
+        return self.redirect('https://blog.%s/' %
+                             get_domain(subreddit=False, no_www=True))
