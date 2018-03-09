58177f5c18cf50cacb6a65ab0b662dc3a966b40b
SUNsung <weatherius@gmail.com>
2018-03-09 13:35:51 +0200

    Rewrite [56.TkAHW1n1g56eRN4hURhejs562.bR3Jj5Tp.56sFH.rb] File in: 2018-03-09 13:35:50

--- ruby/56.TkAHW1n1g56eRN4hURhejs562.bR3Jj5Tp.56sFH.rb
+++ ruby/56.TkAHW1n1g56eRN4hURhejs562.bR3Jj5Tp.56sFH.rb
@@ -1,134 +1,129 @@
-  bottle_ext = filename[bottle_native_regex, 1]
-  bottle_url_ext = f.bottle.url[bottle_native_regex, 1]
+
+        
+              {
+          author: last_git_commit_formatted_with('%an'),
+          message: last_git_commit_formatted_with('%B'),
+          commit_hash: last_git_commit_formatted_with('%H'),
+          abbreviated_commit_hash: last_git_commit_formatted_with('%h')
+      }
+    end
     
-          # we readlink because this path probably doesn't exist since caveats
-      # occurs before the link step of installation
-      # Yosemite security measures mildly tighter rules:
-      # https://github.com/Homebrew/homebrew/issues/33815
-      if !plist_path.file? || !plist_path.symlink?
-        if f.plist_startup
-          s << 'To have launchd start #{f.full_name} at startup:'
-          s << '  sudo mkdir -p #{destination}' unless destination_path.directory?
-          s << '  sudo cp -fv #{f.opt_prefix}/*.plist #{destination}'
-          s << '  sudo chown root #{plist_link}'
+          it 'splits correctly' do
+        expected = ['One', 'Two', 'Three', 'Four Token']
+        expect(generator.split_keywords(keywords)).to eq(expected)
+      end
+    end
+    
+          def load_all_tasks
+        self.tasks = []
+    
+            expect(result).to eq('/usr/local/bin/cloc  --by-file --xml  --out=build/cloc.xml MyCoolApp')
+      end
+    
+          it 'get GIT build number' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            get_build_number_repository
+        end').runner.execute(:test)
+    
+        case params[:range]
+    when 'week'
+      range = 1.week
+      @segment = t('admins.stats.week')
+    when '2weeks'
+      range = 2.weeks
+      @segment = t('admins.stats.2weeks')
+    when 'month'
+      range = 1.month
+      @segment = t('admins.stats.month')
+    else
+      range = 1.day
+      @segment = t('admins.stats.daily')
+    end
+    
+          def handle_prompt_with_signed_in_user
+        client_id = params[:client_id]
+        if client_id
+          auth = Api::OpenidConnect::Authorization.find_by_client_id_user_and_scopes(client_id,
+                                                                                     current_user, params[:scope])
+          if auth
+            process_authorization_consent('true')
+          else
+            handle_params_error('interaction_required', 'User must already be authorized when `prompt` is `none`')
+          end
         else
-          s << 'To have launchd start #{f.full_name} at login:'
-          s << '  mkdir -p #{destination}' unless destination_path.directory?
-          s << '  ln -sfv #{f.opt_prefix}/*.plist #{destination}'
+          handle_params_error('bad_request', 'Client ID is missing from request')
         end
-        s << 'Then to load #{f.full_name} now:'
-        if f.plist_startup
-          s << '  sudo launchctl load #{plist_link}'
-        else
-          s << '  launchctl load #{plist_link}'
-        end
-      # For startup plists, we cannot tell whether it's running on launchd,
-      # as it requires for `sudo launchctl list` to get real result.
-      elsif f.plist_startup
-        s << 'To reload #{f.full_name} after an upgrade:'
-        s << '  sudo launchctl unload #{plist_link}'
-        s << '  sudo cp -fv #{f.opt_prefix}/*.plist #{destination}'
-        s << '  sudo chown root #{plist_link}'
-        s << '  sudo launchctl load #{plist_link}'
-      elsif Kernel.system '/bin/launchctl list #{plist_domain} &>/dev/null'
-        s << 'To reload #{f.full_name} after an upgrade:'
-        s << '  launchctl unload #{plist_link}'
-        s << '  launchctl load #{plist_link}'
-      else
-        s << 'To load #{f.full_name}:'
-        s << '  launchctl load #{plist_link}'
       end
     
-        def self.cleanup_lockfiles
-      return unless HOMEBREW_CACHE_FORMULA.directory?
-      candidates = HOMEBREW_CACHE_FORMULA.children
-      lockfiles  = candidates.select { |f| f.file? && f.extname == '.brewing' }
-      lockfiles.each do |file|
-        next unless file.readable?
-        file.open.flock(File::LOCK_EX | File::LOCK_NB) && file.unlink
+          opts.on('--dasherize', 'Convert underscores to dashes.') do
+        @options[:for_tree][:dasherize] = true
+      end
+    
+            {
+          :always_update     => false,
+          :template_location => root + '/public/stylesheets/sass',
+          :css_location      => root + '/public/stylesheets',
+          :cache_location    => root + '/tmp/sass-cache',
+          :always_check      => env != 'production',
+          :quiet             => env != 'production',
+          :full_exception    => env != 'production'
+        }.freeze
       end
     end
-    
-      def internal_development_commands
-    find_internal_commands HOMEBREW_LIBRARY_PATH/'dev-cmd'
   end
     
-      def clang
-    @clang ||= MacOS.clang_version if MacOS.has_apple_developer_tools?
-  end
-    
-    # See browser for an example
-class GithubGistFormula < ScriptFileFormula
-  def self.url(val)
-    super
-    version File.basename(File.dirname(val))[0, 6]
-  end
-end
-    
-        def configure_sass
-      require 'sass'
-    
-        def log_http_get_file(url, cached = false)
-      s = '  #{'CACHED ' if cached}GET #{url}...'
-      if cached
-        puts dark green s
-      else
-        puts dark cyan s
+            raise t('error.invalid_stage_name', name: invalid, path: stage_config_path.join('#{invalid}.rb'))
       end
     end
-    
-      def setup
-    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
-    success = Dir.chdir DUMMY_PATH do
-      silence_stdout_if !ENV['VERBOSE'] do
-        system 'node', 'manifest.js', tmp_dir
-      end
-    end
-    assert success, 'Node.js Mincer compilation failed'
-    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
-    css_name = manifest['assets']['application.css']
-    @css = File.read('#{tmp_dir}/#{css_name}')
   end
 end
 
     
-      def initial_state_params
-    {
-      settings: {},
-      token: current_session&.token,
-    }
+      desc 'Updated'
+  task :updated do
+  end
+    
+    class Rack::Builder
+  include Sinatra::Delegator
+end
+
+    
+          def empty_cookie(host, path)
+        {:value => '', :domain => host, :path => path, :expires => Time.at(0)}
+      end
+    
+          def initialize(*)
+        super
+    
+        it 'copes with nested arrays' do
+      mock_app do |env|
+        request = Rack::Request.new(env)
+        [200, {'Content-Type' => 'text/plain'}, [request.params['foo']['bar']]]
+      end
+      get '/', :foo => {:bar => '<bar>'}
+      expect(body).to eq('&lt;bar&gt;')
+    end
+    
+      it 'should allow changing the protection mode' do
+    # I have no clue what other modes are available
+    mock_app do
+      use Rack::Protection::FrameOptions, :frame_options => :deny
+      run DummyApp
+    end
+    
+      it 'should not override the header if already set X-Content-Type-Options' do
+    mock_app with_headers('X-Content-Type-Options' => 'sniff')
+    expect(get('/', {}, 'wants' => 'text/html').headers['X-Content-Type-Options']).to eq('sniff')
   end
 end
 
     
-        context 'when tag exists' do
-      it 'returns http success' do
-        get :show, params: { id: 'test', max_id: late.id }
-        expect(response).to have_http_status(:success)
-      end
+      context 'called with multiple prefixes' do
+    it 'applies the prefixes to the property' do
+      rule = '-moz-appearance: none; ' +
+             '-ms-appearance: none; ' +
+             'appearance: none;'
     
-      describe 'GET #show' do
-    it 'returns http success' do
-      get :show
-      expect(response).to have_http_status(:success)
+          expect('.size-both').to have_ruleset(rule)
     end
   end
-    
-    def codepoints_to_unicode(codepoints)
-  if codepoints.include?(' ')
-    codepoints.split(' ').map(&:hex).pack('U*')
-  else
-    [codepoints.hex].pack('U')
-  end
-end
-    
-      def _nt_in_operator
-    start_index = index
-    if node_cache[:in_operator].has_key?(index)
-      cached = node_cache[:in_operator][index]
-      if cached
-        cached = SyntaxNode.new(input, index...(index + 1)) if cached == true
-        @index = cached.interval.end
-      end
-      return cached
-    end
