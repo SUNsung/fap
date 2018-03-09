72ed3bed96e099124e1ca1811b731a8582305463
SUNsung <weatherius@gmail.com>
2018-03-09 09:55:29 +0200

    Rewrite [540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py] File in: 2018-03-09 09:55:28

--- pyton/540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py
+++ pyton/540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py
@@ -1,82 +1,97 @@
 
         
-        if (twisted_version.major, twisted_version.minor, twisted_version.micro) >= (15, 5, 0):
-    collect_ignore += _py_files('scrapy/xlib/tx')
+        
+def log_mel_spectrogram(data,
+                        audio_sample_rate=8000,
+                        log_offset=0.0,
+                        window_length_secs=0.025,
+                        hop_length_secs=0.010,
+                        **kwargs):
+  '''Convert waveform to a log magnitude mel-frequency spectrogram.
+    
+        Args:
+      data_format: Either 'channels_first' or 'channels_last'.
+        'channels_first' is typically faster on GPUs while 'channels_last' is
+        typically faster on CPUs. See
+        https://www.tensorflow.org/performance/performance_guide#data_formats
+    '''
+    super(Model, self).__init__()
+    if data_format == 'channels_first':
+      self._input_shape = [-1, 1, 28, 28]
+    else:
+      assert data_format == 'channels_last'
+      self._input_shape = [-1, 28, 28, 1]
+    
+    import os
+    
+    # Fetch and store Training and Test dataset files
+PATH_DATASET = PATH + os.sep + 'dataset'
+FILE_TRAIN = PATH_DATASET + os.sep + 'iris_training.csv'
+FILE_TEST = PATH_DATASET + os.sep + 'iris_test.csv'
+URL_TRAIN = 'http://download.tensorflow.org/data/iris_training.csv'
+URL_TEST = 'http://download.tensorflow.org/data/iris_test.csv'
+    
+    # Reuters Corpus (rcv1)
+flags.DEFINE_string('rcv1_input_dir', '',
+                    'Path to rcv1 directory containing train.csv, unlab.csv, '
+                    'and test.csv.')
+    
+      Returns:
+    dict<metric name, value>, with value being the average over all examples.
+  '''
+  sv = tf.train.Supervisor(
+      logdir=FLAGS.eval_dir, saver=None, summary_op=None, summary_writer=None)
+  with sv.managed_session(
+      master=FLAGS.master, start_standard_services=False) as sess:
+    if not restore_from_checkpoint(sess, saver):
+      return
+    sv.start_queue_runners(sess)
+    
+      # Construct training data writers
+  writer_lm = build_shuffling_tf_record_writer(data.TRAIN_LM)
+  writer_seq_ae = build_shuffling_tf_record_writer(data.TRAIN_SA)
+  writer_class = build_shuffling_tf_record_writer(data.TRAIN_CLASS)
+  writer_valid_class = build_tf_record_writer(data.VALID_CLASS)
+  writer_rev_lm = build_shuffling_tf_record_writer(data.TRAIN_REV_LM)
+  writer_bd_class = build_shuffling_tf_record_writer(data.TRAIN_BD_CLASS)
+  writer_bd_valid_class = build_shuffling_tf_record_writer(data.VALID_BD_CLASS)
+    
+    try:
+    import json
+except ImportError:
+    import simplejson as json
+    
+        parser.add_argument('--key',
+                        metavar='KEY',
+                        default=api_key,
+                        required=not api_key,
+                        help='Shippable API key')
+    
+    # Components that match a numeric or alphanumeric begin:end or begin:end:step
+# range expression inside square brackets.
+    
+    # Make coding more python3-ish
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
     
     
-class _BenchSpider(scrapy.Spider):
-    '''A spider that follows all links'''
-    name = 'follow'
-    total = 10000
-    show = 20
-    baseurl = 'http://localhost:8998'
-    link_extractor = LinkExtractor()
+class AskTest(unittest.TestCase):
+    '''Test the ask method.'''
+    def setUp(self):
+        logging.disable(logging.CRITICAL)
     
-            # start checks
-        if opts.list:
-            for spider, methods in sorted(contract_reqs.items()):
-                if not methods and not opts.verbose:
-                    continue
-                print(spider)
-                for method in sorted(methods):
-                    print('  * %s' % method)
-        else:
-            start = time.time()
-            self.crawler_process.start()
-            stop = time.time()
-    
-            def eb_wrapper(failure):
-            case = _create_testcase(method, 'errback')
-            exc_info = failure.value, failure.type, failure.getTracebackObject()
-            results.addError(case, exc_info)
-    
-    from . import Contract
-    
-        def test_sales_manager_shall_not_respond_through_proxy_with_delay(cls):
-        cls.ntp.busy = 'Yes'
-        start_time = time()
-        cls.ntp.talk()
-        end_time = time()
-        execution_time = end_time - start_time
-        print_output = cls.output.getvalue()
-        expected_print_output = 'Proxy checking for Sales Manager availability\n\
-This Sales Manager will not talk to you whether he/she is busy or not\n'
-        cls.assertEqual(print_output, expected_print_output)
-        expected_execution_time = 1
-        cls.assertEqual(int(execution_time*10), expected_execution_time)
-
-    
-        def __init__(self, name, action):
-        self.name = name
-        self.action = action
-    
-        def subscribe(self, msg):
-        self.provider.subscribe(msg, self)
-    
-        def setUp(self):
-        self.sample_queue = queue.Queue()
-        self.sample_queue.put('first')
-        self.sample_queue.put('second')
-    
-        def test_2nd_am_station_after_scan(self):
-        self.radio.scan()
-        station = self.radio.state.stations[self.radio.state.pos]
-        expected_station = '1380'
-        self.assertEqual(station, expected_station)
-    
-        def test_display_current_time_at_current_time(self):
-        '''
-        Just as justification for working example with the time provider used in
-        production. (Will always pass.)
-        '''
-        production_code_time_provider = ProductionCodeTimeProvider()
-        class_under_test = TimeDisplay()
-        class_under_test.set_time_provider(production_code_time_provider)
-        current_time = datetime.datetime.now()
-        expected_time = '<span class=\'tinyBoldText\'>{}:{}</span>'.format(current_time.hour, current_time.minute)
-        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
-
-    
-    print('Counting to five...')
-for number in count_to_five():
-    print(number, end=' ')
+        @mock.patch('certbot.notify.smtplib.LMTP')
+    @mock.patch('certbot.notify.subprocess.Popen')
+    def test_everything_fails(self, mock_popen, mock_lmtp):
+        from certbot.notify import notify
+        lmtp_obj = mock.MagicMock()
+        mock_lmtp.return_value = lmtp_obj
+        lmtp_obj.sendmail.side_effect = socket.error(17)
+        proc = mock.MagicMock()
+        mock_popen.return_value = proc
+        proc.communicate.side_effect = OSError('What we have here is a '
+                                               'failure to communicate.')
+        self.assertFalse(notify('Goose', 'auntrhody@example.com',
+                                'The old grey goose is dead.'))
+        self.assertEqual(lmtp_obj.sendmail.call_count, 1)
+        self.assertEqual(proc.communicate.call_count, 1)
