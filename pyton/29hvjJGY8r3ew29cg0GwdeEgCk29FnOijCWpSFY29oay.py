ffd6ec38ee11d046b254cad48d683306dc58164b
SUNsung <weatherius@gmail.com>
2018-03-10 11:26:24 +0200

    Rewrite [29hvjJGY8r3ew29cg0GwdeEgCk29FnOijCWpSFY29oay.py] File in: 2018-03-10 11:26:23

--- pyton/29hvjJGY8r3ew29cg0GwdeEgCk29FnOijCWpSFY29oay.py
+++ pyton/29hvjJGY8r3ew29cg0GwdeEgCk29FnOijCWpSFY29oay.py
@@ -1,99 +1,137 @@
 
-def dispatch_hook(key, hooks, hook_data, **kwargs):
-    '''Dispatches a hook dictionary on a given piece of data.'''
-    hooks = hooks or dict()
-    hooks = hooks.get(key)
-    if hooks:
-        if hasattr(hooks, '__call__'):
-            hooks = [hooks]
-        for hook in hooks:
-            _hook_data = hook(hook_data, **kwargs)
-            if _hook_data is not None:
-                hook_data = _hook_data
-    return hook_data
+        
+        # TODO: ensure that history changes.
 
     
-    Provides utility functions that are consumed internally by Requests
-which depend on extremely few external helpers (such as compat)
+            String:                    '#4e9a06',        # class: 's'
+        String.Backtick:           '#4e9a06',        # class: 'sb'
+        String.Char:               '#4e9a06',        # class: 'sc'
+        String.Doc:                'italic #8f5902', # class: 'sd' - like a comment
+        String.Double:             '#4e9a06',        # class: 's2'
+        String.Escape:             '#4e9a06',        # class: 'se'
+        String.Heredoc:            '#4e9a06',        # class: 'sh'
+        String.Interpol:           '#4e9a06',        # class: 'si'
+        String.Other:              '#4e9a06',        # class: 'sx'
+        String.Regex:              '#4e9a06',        # class: 'sr'
+        String.Single:             '#4e9a06',        # class: 's1'
+        String.Symbol:             '#4e9a06',        # class: 'ss'
+    
+            HA1 = hash_utf8(A1)
+        HA2 = hash_utf8(A2)
+    
+    
+@pytest.mark.parametrize(
+    'url, expected', (
+            ('http://192.168.0.1:5000/', True),
+            ('http://192.168.0.1/', True),
+            ('http://172.16.1.1/', True),
+            ('http://172.16.1.1:5000/', True),
+            ('http://localhost.localdomain:5000/v1.0/', True),
+            ('http://google.com:6000/', True),
+            ('http://172.16.1.12/', False),
+            ('http://172.16.1.12:5000/', False),
+            ('http://google.com:5000/v1.0/', False),
+    ))
+def test_should_bypass_proxies(url, expected, monkeypatch):
+    '''Tests for function should_bypass_proxies to check if proxy
+    can be bypassed or not
+    '''
+    monkeypatch.setenv('no_proxy', '192.168.0.0/24,127.0.0.1,localhost.localdomain,172.16.1.1, google.com:6000')
+    monkeypatch.setenv('NO_PROXY', '192.168.0.0/24,127.0.0.1,localhost.localdomain,172.16.1.1, google.com:6000')
+    assert should_bypass_proxies(url, no_proxy=None) == expected
+    
+            :rtype: requests.Response
+        '''
+        # Set defaults that the hooks can utilize to ensure they always have
+        # the correct parameters to reproduce the previous request.
+        kwargs.setdefault('stream', self.stream)
+        kwargs.setdefault('verify', self.verify)
+        kwargs.setdefault('cert', self.cert)
+        kwargs.setdefault('proxies', self.proxies)
+    
+      def replicate_states(self, state_coll_name):
+    state_list = self._metagraph.collection_def[state_coll_name]
+    num_states = len(state_list.node_list.value)
+    for replica_id in range(1, FLAGS.num_gpus):
+      for i in range(num_states):
+        state_list.node_list.value.append(state_list.node_list.value[i])
+    for replica_id in range(FLAGS.num_gpus):
+      for i in range(num_states):
+        index = replica_id * num_states + i
+        state_list.node_list.value[index] = with_autoparallel_prefix(
+            replica_id, state_list.node_list.value[index])
+    
+      def __init__(self, multivalent_tokens=False):
+    self._seq = tf.train.SequenceExample()
+    self._flist = self._seq.feature_lists.feature_list
+    self._timesteps = []
+    self._multivalent_tokens = multivalent_tokens
+    
+        self.assertEqual(t1.weight, 0.0)
+    self.assertEqual(t1.label, 0)
+    self.assertEqual(t1.token, 0)
+    
+    py_binary(
+    name = 'cifar10_multi_gpu_train',
+    srcs = [
+        'cifar10_multi_gpu_train.py',
+    ],
+    srcs_version = 'PY2AND3',
+    visibility = ['//tensorflow:__subpackages__'],
+    deps = [
+        ':cifar10',
+    ],
+)
+    
+        An example with a long-untouched module that everyone has
+    >>> _linkcode_resolve('py', {'module': 'tty',
+    ...                          'fullname': 'setraw'},
+    ...                   package='tty',
+    ...                   url_fmt='http://hg.python.org/cpython/file/'
+    ...                           '{revision}/Lib/{package}/{path}#L{lineno}',
+    ...                   revision='xxxx')
+    'http://hg.python.org/cpython/file/xxxx/Lib/tty/tty.py#L18'
+    '''
+    
+    Second example
+--------------
+The second example shows the ability of the Minimum Covariance Determinant
+robust estimator of covariance to concentrate on the main mode of the data
+distribution: the location seems to be well estimated, although the covariance
+is hard to estimate due to the banana-shaped distribution. Anyway, we can
+get rid of some outlying observations.
+The One-Class SVM is able to capture the real data structure, but the
+difficulty is to adjust its kernel bandwidth parameter so as to obtain
+a good compromise between the shape of the data scatter matrix and the
+risk of over-fitting the data.
+    
+    from sklearn.datasets import make_checkerboard
+from sklearn.datasets import samples_generator as sg
+from sklearn.cluster.bicluster import SpectralBiclustering
+from sklearn.metrics import consensus_score
+    
+    The base classifier is a random forest classifier with 25 base estimators
+(trees). If this classifier is trained on all 800 training datapoints, it is
+overly confident in its predictions and thus incurs a large log-loss.
+Calibrating an identical classifier, which was trained on 600 datapoints, with
+method='sigmoid' on the remaining 200 datapoints reduces the confidence of the
+predictions, i.e., moves the probability vectors from the edges of the simplex
+towards the center. This calibration results in a lower log-loss. Note that an
+alternative would have been to increase the number of base estimators which
+would have resulted in a similar decrease in log-loss.
 '''
+print(__doc__)
     
-            def generate():
-            # Special case for urllib3.
-            if hasattr(self.raw, 'stream'):
-                try:
-                    for chunk in self.raw.stream(chunk_size, decode_content=True):
-                        yield chunk
-                except ProtocolError as e:
-                    raise ChunkedEncodingError(e)
-                except DecodeError as e:
-                    raise ContentDecodingError(e)
-                except ReadTimeoutError as e:
-                    raise ConnectionError(e)
-            else:
-                # Standard file-like object.
-                while True:
-                    chunk = self.raw.read(chunk_size)
-                    if not chunk:
-                        break
-                    yield chunk
+            self.assertFalse(self.vhost2.conflicts([self.addr1,
+                                                self.addr_default]))
     
-    def _init():
-    for code, titles in _codes.items():
-        for title in titles:
-            setattr(codes, title, code)
-            if not title.startswith(('\\', '/')):
-                setattr(codes, title.upper(), code)
+    # If true, links to the reST sources are added to the pages.
+#html_show_sourcelink = True
     
+    autodoc_member_order = 'bysource'
+autodoc_default_flags = ['show-inheritance', 'private-members']
     
-def _re_unescape_replacement(match):
-    # type: (Match[str]) -> str
-    group = match.group(1)
-    if group[0] in _alphanum:
-        raise ValueError('cannot unescape '\\\\%s'' % group[0])
-    return group
-    
-        @gen_test
-    def test_http10_no_content_length(self):
-        # Regression test for a bug in which can_keep_alive would crash
-        # for an HTTP/1.0 (not 1.1) response with no content-length.
-        conn = HTTP1Connection(self.client_stream, True)
-        self.server_stream.write(b'HTTP/1.0 200 Not Modified\r\n\r\nhello')
-        self.server_stream.close()
-    
-    
-class QueuePutTest(AsyncTestCase):
-    @gen_test
-    def test_blocking_put(self):
-        q = queues.Queue()
-        q.put(0)
-        self.assertEqual(0, q.get_nowait())
-    
-        c-ares fails to resolve some names when ``family`` is ``AF_UNSPEC``,
-    so it is only recommended for use in ``AF_INET`` (i.e. IPv4).  This is
-    the default for ``tornado.simple_httpclient``, but other libraries
-    may default to ``AF_UNSPEC``.
-    
-    
-class Waker(interface.Waker):
-    '''Create an OS independent asynchronous pipe.
-    
-    from tornado.escape import utf8
-from tornado.httpclient import HTTPRequest
-from tornado.locks import Event
-from tornado.stack_context import ExceptionStackContext
-from tornado.testing import AsyncHTTPTestCase, gen_test
-from tornado.test import httpclient_test
-from tornado.test.util import unittest
-from tornado.web import Application, RequestHandler
-    
-            # This fires before the wait times out.
-        self.io_loop.call_later(0.01, c.notify)
-        wait = c.wait(timedelta(seconds=0.02))
-        yield gen.sleep(0.03)
-        yield wait  # No TimeoutError.
-    
-        @gen_test
-    def test_stop_in_callback(self):
-        # Issue #2069: calling server.stop() in a loop callback should not
-        # raise EBADF when the loop handles other server connection
-        # requests in the same loop iteration
+        @classmethod
+    def _call(cls, enhancement):
+        from certbot.display.enhancements import ask
+        return ask(enhancement)
