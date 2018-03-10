0431048469efcabd6580891d7aa7b0b82dd6aa6f
SUNsung <weatherius@gmail.com>
2018-03-10 04:07:00 +0200

    Rewrite [540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py] File in: 2018-03-10 04:06:59

--- pyton/540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py
+++ pyton/540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py
@@ -1,97 +1,44 @@
 
         
         
-def log_mel_spectrogram(data,
-                        audio_sample_rate=8000,
-                        log_offset=0.0,
-                        window_length_secs=0.025,
-                        hop_length_secs=0.010,
-                        **kwargs):
-  '''Convert waveform to a log magnitude mel-frequency spectrogram.
-    
-        Args:
-      data_format: Either 'channels_first' or 'channels_last'.
-        'channels_first' is typically faster on GPUs while 'channels_last' is
-        typically faster on CPUs. See
-        https://www.tensorflow.org/performance/performance_guide#data_formats
-    '''
-    super(Model, self).__init__()
-    if data_format == 'channels_first':
-      self._input_shape = [-1, 1, 28, 28]
-    else:
-      assert data_format == 'channels_last'
-      self._input_shape = [-1, 28, 28, 1]
-    
-    import os
-    
-    # Fetch and store Training and Test dataset files
-PATH_DATASET = PATH + os.sep + 'dataset'
-FILE_TRAIN = PATH_DATASET + os.sep + 'iris_training.csv'
-FILE_TEST = PATH_DATASET + os.sep + 'iris_test.csv'
-URL_TRAIN = 'http://download.tensorflow.org/data/iris_training.csv'
-URL_TEST = 'http://download.tensorflow.org/data/iris_test.csv'
-    
-    # Reuters Corpus (rcv1)
-flags.DEFINE_string('rcv1_input_dir', '',
-                    'Path to rcv1 directory containing train.csv, unlab.csv, '
-                    'and test.csv.')
-    
-      Returns:
-    dict<metric name, value>, with value being the average over all examples.
-  '''
-  sv = tf.train.Supervisor(
-      logdir=FLAGS.eval_dir, saver=None, summary_op=None, summary_writer=None)
-  with sv.managed_session(
-      master=FLAGS.master, start_standard_services=False) as sess:
-    if not restore_from_checkpoint(sess, saver):
-      return
-    sv.start_queue_runners(sess)
-    
-      # Construct training data writers
-  writer_lm = build_shuffling_tf_record_writer(data.TRAIN_LM)
-  writer_seq_ae = build_shuffling_tf_record_writer(data.TRAIN_SA)
-  writer_class = build_shuffling_tf_record_writer(data.TRAIN_CLASS)
-  writer_valid_class = build_tf_record_writer(data.VALID_CLASS)
-  writer_rev_lm = build_shuffling_tf_record_writer(data.TRAIN_REV_LM)
-  writer_bd_class = build_shuffling_tf_record_writer(data.TRAIN_BD_CLASS)
-  writer_bd_valid_class = build_shuffling_tf_record_writer(data.VALID_BD_CLASS)
-    
-    try:
-    import json
-except ImportError:
-    import simplejson as json
-    
-        parser.add_argument('--key',
-                        metavar='KEY',
-                        default=api_key,
-                        required=not api_key,
-                        help='Shippable API key')
-    
-    # Components that match a numeric or alphanumeric begin:end or begin:end:step
-# range expression inside square brackets.
-    
-    # Make coding more python3-ish
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
+def side_effect(old_cmd, command):
+    with tarfile.TarFile(_tar_file(old_cmd.script_parts)[0]) as archive:
+        for file in archive.getnames():
+            try:
+                os.remove(file)
+            except OSError:
+                # does not try to remove directories as we cannot know if they
+                # already existed before
+                pass
+
     
     
-class AskTest(unittest.TestCase):
-    '''Test the ask method.'''
-    def setUp(self):
-        logging.disable(logging.CRITICAL)
+@pytest.fixture(params=containers)
+def proc(request, spawnu, TIMEOUT):
+    proc = spawnu(*request.param)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(u'thefuck --alias > ~/.config/fish/config.fish')
+    proc.sendline(u'fish')
+    return proc
     
-        @mock.patch('certbot.notify.smtplib.LMTP')
-    @mock.patch('certbot.notify.subprocess.Popen')
-    def test_everything_fails(self, mock_popen, mock_lmtp):
-        from certbot.notify import notify
-        lmtp_obj = mock.MagicMock()
-        mock_lmtp.return_value = lmtp_obj
-        lmtp_obj.sendmail.side_effect = socket.error(17)
-        proc = mock.MagicMock()
-        mock_popen.return_value = proc
-        proc.communicate.side_effect = OSError('What we have here is a '
-                                               'failure to communicate.')
-        self.assertFalse(notify('Goose', 'auntrhody@example.com',
-                                'The old grey goose is dead.'))
-        self.assertEqual(lmtp_obj.sendmail.call_count, 1)
-        self.assertEqual(proc.communicate.call_count, 1)
+    
+@pytest.mark.functional
+def test_with_confirmation(proc, TIMEOUT):
+    with_confirmation(proc, TIMEOUT)
+    
+        return out
+    
+        def test_basic(self):
+        '''messages are sent and received properly'''
+        question = b'success?'
+        answer = b'yeah, success'
+    
+    
+def _basic_auth_str(username, password):
+    '''Returns a Basic Auth string.'''
+    
+        def get(self, name, default=None, domain=None, path=None):
+        '''Dict-like get() that also supports optional domain and path args in
+        order to resolve naming collisions from using one cookie jar over
+        multiple domains.
