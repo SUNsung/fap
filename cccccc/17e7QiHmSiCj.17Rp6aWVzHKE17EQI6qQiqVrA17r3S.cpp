e58846bfad01e084a77cbf39f9eaca7941c4c73e
SUNsung <weatherius@gmail.com>
2018-03-10 18:12:32 +0200

    Rewrite [17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp] File in: 2018-03-10 18:12:32

--- cccccc/17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp
+++ cccccc/17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp
@@ -1,243 +1,45 @@
 
         
-        
-    {} // end swift namespace
-    
-    
-    {} // end namespace swift
-    
-    /// \brief Diagnostic consumer that displays diagnostics to standard error.
-class PrintingDiagnosticConsumer : public DiagnosticConsumer {
-  llvm::raw_ostream &Stream;
-  bool ForceColors = false;
-  bool DidErrorOccur = false;
-public:
-  PrintingDiagnosticConsumer(llvm::raw_ostream &stream = llvm::errs()) :
-    Stream(stream) { }
+        namespace nwapi {
     }
     
-    #endif // SWIFT_INDEX_INDEXSYMBOL_H
-
+    #ifndef CONTENT_NW_SRC_API_BASE_BASE_H_
+#define CONTENT_NW_SRC_API_BASE_BASE_H_
     
+    // Gets the path to the content shell's pak file.
+bool GetResourcesPakFilePath(base::FilePath& output);
     
-    {  DISALLOW_COPY_AND_ASSIGN(Clipboard);
-};
-    
-      v8::Handle<v8::Value> args = converter.ToV8Value(&arguments, context);
-  DCHECK(!args.IsEmpty()) << 'Invalid 'arguments' in Dispatcher::OnEvent';
-  v8::Handle<v8::Value> argv[] = {
-    v8::Integer::New(isolate, object_id), v8_str(event.c_str()), args };
-    
-    base::string16 MenuDelegate::GetLabelForCommandId(int command_id) const {
-  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
-  return item->label_;
+    void Clipboard::SetText(std::string& text) {
+  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
+  ui::Clipboard::ObjectMap map;
+  map[ui::Clipboard::CBF_TEXT].push_back(
+      std::vector<char>(text.begin(), text.end()));
+  clipboard->WriteObjects(ui::CLIPBOARD_TYPE_COPY_PASTE, map);
 }
     
-    NwMenuModel::NwMenuModel(Delegate* delegate) : SimpleMenuModel(delegate) {
+    namespace base {
+class ListValue;
 }
-    
-    
-    
-    void NwAppClearCacheFunction::OnBrowsingDataRemoverDone() {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  run_loop_.Quit();
-}
-    
-    template <typename T1, typename T2>
-void PrintTo(const ::std::tr1::tuple<T1, T2>& t, ::std::ostream* os) {
-  PrintTupleTo(t, os);
-}
-    
-    // This interface knows how to report a test part result.
-class TestPartResultReporterInterface {
- public:
-  virtual ~TestPartResultReporterInterface() {}
-    }
-    
-    // Ternary predicate assertion macros.
-#define EXPECT_PRED_FORMAT3(pred_format, v1, v2, v3) \
-  GTEST_PRED_FORMAT3_(pred_format, v1, v2, v3, GTEST_NONFATAL_FAILURE_)
-#define EXPECT_PRED3(pred, v1, v2, v3) \
-  GTEST_PRED3_(pred, v1, v2, v3, GTEST_NONFATAL_FAILURE_)
-#define ASSERT_PRED_FORMAT3(pred_format, v1, v2, v3) \
-  GTEST_PRED_FORMAT3_(pred_format, v1, v2, v3, GTEST_FATAL_FAILURE_)
-#define ASSERT_PRED3(pred, v1, v2, v3) \
-  GTEST_PRED3_(pred, v1, v2, v3, GTEST_FATAL_FAILURE_)
-    
-      // Returns true if FilePath ends with a path separator, which indicates that
-  // it is intended to represent a directory. Returns false otherwise.
-  // This does NOT check that a directory (or file) actually exists.
-  bool IsDirectory() const;
     
      private:
-  class Iterator : public ParamIteratorInterface<ParamType> {
-   public:
-    Iterator(const ParamGeneratorInterface<ParamType>* base,
-      const ParamGenerator<T1>& g1,
-      const typename ParamGenerator<T1>::iterator& current1,
-      const ParamGenerator<T2>& g2,
-      const typename ParamGenerator<T2>::iterator& current2)
-        : base_(base),
-          begin1_(g1.begin()), end1_(g1.end()), current1_(current1),
-          begin2_(g2.begin()), end2_(g2.end()), current2_(current2)    {
-      ComputeCurrentValue();
-    }
-    virtual ~Iterator() {}
+  // Helper functions for bindings.
+  static void RequireNwGui(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void GetAbsolutePath(const v8::FunctionCallbackInfo<v8::Value>& args);
+    
+    EventListener::EventListener(int id,
+  const base::WeakPtr<DispatcherHost>& dispatcher_host,
+  const base::DictionaryValue& option) : Base(id, dispatcher_host, option) {
     }
     
-    template <class Container>
-internal::ParamGenerator<typename Container::value_type> ValuesIn(
-    const Container& container);
-    
-    
-    {  /**
-   * Invoked if the stream was canceled via
-   * bidirectional_stream_cancel(). Once invoked, no further callback
-   * methods will be invoked.
-   */
-  void (*on_canceled)(bidirectional_stream* stream);
-} bidirectional_stream_callback;
-    
-    int grpc_byte_buffer_reader_init(grpc_byte_buffer_reader* reader,
-                                 grpc_byte_buffer* buffer) {
-  grpc_core::ExecCtx exec_ctx;
-  grpc_slice_buffer decompressed_slices_buffer;
-  reader->buffer_in = buffer;
-  switch (reader->buffer_in->type) {
-    case GRPC_BB_RAW:
-      grpc_slice_buffer_init(&decompressed_slices_buffer);
-      if (is_compressed(reader->buffer_in)) {
-        if (grpc_msg_decompress(
+      template<typename T> T* AddListener() {
+    std::map<int, BaseEvent*>::iterator i = listerners_.find(T::id);
+    if (i==listerners_.end()) {
+      T* listener_object = new T(this);
+      listerners_[T::id] = listener_object;
+      return listener_object;
     }
-    }
-    }
-    
-    TEST(InlinedVectorTest, PushBackWithMove) {
-  InlinedVector<UniquePtr<int>, 1> v;
-  UniquePtr<int> i = MakeUnique<int>(3);
-  v.push_back(std::move(i));
-  EXPECT_EQ(nullptr, i.get());
-  EXPECT_EQ(1UL, v.size());
-  EXPECT_EQ(3, *v[0]);
-}
-    
-    extern std::vector<grpc::testing::Server*>* g_inproc_servers;
-    
-    namespace grpc {
-namespace testing {
-    }
-    }
-    
-    
-    {  bool failed_ = false;
-  std::string db_;
-  size_t current_ = 0;
-  const std::string location_ = '\'location\':';
-  const std::string latitude_ = '\'latitude\':';
-  const std::string longitude_ = '\'longitude\':';
-  const std::string name_ = '\'name\':';
-};
-    
-    namespace routeguide {
-class Feature;
-    }
-    
-      Status RecordRoute(ServerContext* context, ServerReader<Point>* reader,
-                     RouteSummary* summary) override {
-    Point point;
-    int point_count = 0;
-    int feature_count = 0;
-    float distance = 0.0;
-    Point previous;
-    }
-    
-    #endif /* PCFREAD_H_ */
-    
-        inline int get_width() const { return m_image_x_size; }
-    inline int get_height() const { return m_image_y_size; }
-    
-    /*Accesses one of four (signed) bytes given an index.
-  This can be used to avoid small lookup tables.*/
-#define OC_BYTE_TABLE32(_a,_b,_c,_d,_i) \
-  ((signed char) \
-   (((_a)&0xFF|((_b)&0xFF)<<8|((_c)&0xFF)<<16|((_d)&0xFF)<<24)>>(_i)*8))
-/*Accesses one of eight (unsigned) nibbles given an index.
-  This can be used to avoid small lookup tables.*/
-#define OC_UNIBBLE_TABLE32(_a,_b,_c,_d,_e,_f,_g,_h,_i) \
-  ((((_a)&0xF|((_b)&0xF)<<4|((_c)&0xF)<<8|((_d)&0xF)<<12| \
-   ((_e)&0xF)<<16|((_f)&0xF)<<20|((_g)&0xF)<<24|((_h)&0xF)<<28)>>(_i)*4)&0xF)
-    
-     function: toplevel residue templates for 32/44.1/48kHz
- last mod: $Id: residue_44.h 16962 2010-03-11 07:30:34Z xiphmont $
-    
-    /* The bark scale equations are approximations, since the original
-   table was somewhat hand rolled.  The below are chosen to have the
-   best possible fit to the rolled tables, thus their somewhat odd
-   appearance (these are more accurate and over a longer range than
-   the oft-quoted bark equations found in the texts I have).  The
-   approximations are valid from 0 - 30kHz (nyquist) or so.
-    
-    
-    {    /* Convert to Qres domain */
-    lshift = 29 + a_headrm - b_headrm - Qres;
-    if( lshift < 0 ) {
-        return silk_LSHIFT_SAT32(result, -lshift);
-    } else {
-        if( lshift < 32){
-            return silk_RSHIFT(result, lshift);
-        } else {
-            /* Avoid undefined result */
-            return 0;
-        }
-    }
-}
-    
-    
-    {}  // namespace leveldb
-    
-      Cache::Handle* handle = NULL;
-  Status s = FindTable(file_number, file_size, &handle);
-  if (!s.ok()) {
-    return NewErrorIterator(s);
+    return NULL;
   }
     
-    Iterator* NewEmptyIterator() {
-  return new EmptyIterator(Status::OK());
-}
-    
-    #endif  // STORAGE_LEVELDB_TABLE_MERGER_H_
-
-    
-    
-    {        break;
-    }
-    
-    bool nextConnection(int tid)
-{
-    m.lock();
-    int socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (socketfd == -1) {
-        cout << 'FD error, connections: ' << connections << endl;
-        return false;
-    }
-    }
-    
-        char message[1024];
-    
-        if (SSL_CTX_use_certificate_chain_file(context.context, certChainFileName.c_str()) != 1) {
-        return nullptr;
-    } else if (SSL_CTX_use_PrivateKey_file(context.context, keyFileName.c_str(), SSL_FILETYPE_PEM) != 1) {
-        return nullptr;
-    }
-    
-        int getPoll() {
-        return state.poll;
-    }
-    
-    
-    {    length = LARGE_BUFFER_SIZE - inflationStream.avail_out;
-    return inflationBuffer;
-}
-    
-    
-    {}
+     private:
+  ObjectManager* object_manager_;
