902e74af7f42b8262682338118e33e6f4e9c1fd0
SUNsung <weatherius@gmail.com>
2018-03-10 02:52:49 +0200

    Rewrite [42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py] File in: 2018-03-10 02:52:49

--- pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
+++ pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
@@ -1,3 +1,93 @@
 
         
         
+# -- Options for manual page output ---------------------------------------
+    
+    # Language to be used for generating the HTML full-text search index.
+# Sphinx supports the following languages:
+#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
+#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
+#html_search_language = 'en'
+    
+    
+@zope.interface.implementer(interfaces.IInstaller)
+@zope.interface.provider(interfaces.IPluginFactory)
+class Installer(common.Plugin):
+    '''Example Installer.'''
+    
+    #----------------------------------------------------------------------
+def fc2video_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+    '''wrapper'''
+    #'http://video.fc2.com/en/content/20151021bTVKnbEw'
+    #'http://xiaojiadianvideo.asia/content/20151021bTVKnbEw'
+    #'http://video.fc2.com/ja/content/20151021bTVKnbEw'
+    #'http://video.fc2.com/tw/content/20151021bTVKnbEw'
+    hostname = urlparse(url).hostname
+    if not ('fc2.com' in hostname or 'xiaojiadianvideo.asia' in hostname):
+        return False
+    upid = match1(url, r'.+/content/(\w+)')
+    
+    
+def huaban_download_board(url, output_dir, **kwargs):
+    kwargs['merge'] = False
+    board = extract_board_data(url)
+    output_dir = os.path.join(output_dir, board.title)
+    print_info(site_info, board.title, 'jpg', float('Inf'))
+    for pin in board.pins:
+        download_urls([pin.url], pin.id, pin.ext, float('Inf'),
+                      output_dir=output_dir, faker=True, **kwargs)
+    
+        #title
+    title = ''
+    profile_api = 'https://www.showroom-live.com/api/room/profile?room_id={room_id}'.format(room_id = room_id)
+    html = loads(get_content(profile_api))
+    try:
+        title = html['main_name']
+    except KeyError:
+        title = 'Showroom_{room_id}'.format(room_id = room_id)
+    
+    # mode: ['auto', 'manual']
+MODE = 'manual'
+    
+        return piece_x, piece_y
+    
+        # 从上顶点往下 +274 的位置开始向上找颜色与上顶点一样的点，为下顶点
+    # 该方法对所有纯色平面和部分非纯色平面有效，对高尔夫草坪面、木纹桌面、
+    # 药瓶和非菱形的碟机（好像是）会判断错误
+    for k in range(i+274, i, -1):  # 274 取开局时最大的方块的上下顶点距离
+        pixel = im_pixel[board_x, k]
+        if abs(pixel[0] - last_pixel[0]) \
+                + abs(pixel[1] - last_pixel[1]) \
+                + abs(pixel[2] - last_pixel[2]) < 10:
+            break
+    board_y = int((i+k) / 2)
+    
+    
+def pull_screenshot():
+    c.screenshot('autojump.png')
+    
+    
+def on_click(event):
+    global update
+    global ix, iy
+    global click_count
+    global cor
+    
+    
+def save_debug_screenshot(ts, im, piece_x, piece_y, board_x, board_y):
+    '''
+    对 debug 图片加上详细的注释
+    
+    '''
+    make_debug_dir(screenshot_backup_dir)
+    draw = ImageDraw.Draw(im)
+    draw.line((piece_x, piece_y) + (board_x, board_y), fill=2, width=3)
+    draw.line((piece_x, 0, piece_x, im.size[1]), fill=(255, 0, 0))
+    draw.line((0, piece_y, im.size[0], piece_y), fill=(255, 0, 0))
+    draw.line((board_x, 0, board_x, im.size[1]), fill=(0, 0, 255))
+    draw.line((0, board_y, im.size[0], board_y), fill=(0, 0, 255))
+    draw.ellipse((piece_x - 10, piece_y - 10, piece_x + 10, piece_y + 10), fill=(255, 0, 0))
+    draw.ellipse((board_x - 10, board_y - 10, board_x + 10, board_y + 10), fill=(0, 0, 255))
+    del draw
+    im.save(os.path.join(os.getcwd(), screenshot_backup_dir,
+                         '#' + str(ts) + '.png'))
