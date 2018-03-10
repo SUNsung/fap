511914fd11f7e9c8025a463bee797ad323f83ae3
SUNsung <weatherius@gmail.com>
2018-03-10 07:53:55 +0200

    Rewrite [24rY3zWRXCUFc24czSxMo6XTc241VoGmNhVMOY24Jpj.py] File in: 2018-03-10 07:53:54

--- pyton/24rY3zWRXCUFc24czSxMo6XTc241VoGmNhVMOY24Jpj.py
+++ pyton/24rY3zWRXCUFc24czSxMo6XTc241VoGmNhVMOY24Jpj.py
@@ -1,66 +1,132 @@
 
         
-            # To avoid having to always use the '|safe' filter in flatpage templates,
-    # mark the title and content as already safe (since they are raw HTML
-    # content in the first place).
-    f.title = mark_safe(f.title)
-    f.content = mark_safe(f.content)
-    
-        def test_car_adapter_shall_make_very_loud_noise(self):
-        car = Car()
-        car_adapter = Adapter(car, make_noise=car.make_noise)
-        noise = car_adapter.make_noise(10)
-        expected_noise = 'vroom!!!!!!!!!!'
-    
-        def test_tc2_output(self):
-        self.tc2.run()
-        output = self.out.getvalue().strip()
-        self.assertEqual(output, self.average_result_tc2)
-    
-    if __name__ == '__main__':
-    main()
+            # Sorting the libraries
+    inner_blocks = sorted(blocks[0].split('##'))
+    for i in range(1 , len(inner_blocks)):
+        if inner_blocks[i][0] != '#':
+            inner_blocks[i] = '##' + inner_blocks[i]
+    inner_blocks=''.join(inner_blocks)
     
     
-def count_to(count):
-    '''Counts by word numbers, up to a maximum of five'''
-    numbers = ['one', 'two', 'three', 'four', 'five']
-    for number in numbers[:count]:
-        yield number
-    
-        print(u'Setting Data 1 = 10')
-    data1.data = 10
-    print(u'Setting Data 2 = 15')
-    data2.data = 15
-    print(u'Setting Data 1 = 3')
-    data1.data = 3
-    print(u'Setting Data 2 = 5')
-    data2.data = 5
-    print(u'Detach HexViewer from data1 and data2.')
-    data1.detach(view2)
-    data2.detach(view2)
-    print(u'Setting Data 1 = 10')
-    data1.data = 10
-    print(u'Setting Data 2 = 15')
-    data2.data = 15
-    
-        print(round(target_distance), round(jump_error_value), round(actual_distance), round(last_press_time))
-    ''''# 将结果采集进学习字典
-    if last_piece_x > 0 and last_press_time > 0:
-        ai.add_data(round(actual_distance, 2), round(last_press_time))
-        # print(round(actual_distance), round(last_press_time))'''
+@csrf_protect
+def render_flatpage(request, f):
+    '''
+    Internal interface to the flat page view.
+    '''
+    # If registration is required for accessing this page, and the user isn't
+    # logged in, redirect to the login page.
+    if f.registration_required and not request.user.is_authenticated:
+        from django.contrib.auth.views import redirect_to_login
+        return redirect_to_login(request.path)
+    if f.template_name:
+        template = loader.select_template((f.template_name, DEFAULT_TEMPLATE))
+    else:
+        template = loader.get_template(DEFAULT_TEMPLATE)
     
     
-def pull_screenshot():  # 获取截图
-    global screenshot_way
-    if screenshot_way in [1, 2]:
-        process = subprocess.Popen(
-            'adb shell screencap -p', shell=True, stdout=subprocess.PIPE)
-        screenshot = process.stdout.read()
-        if screenshot_way == 2:
-            binary_screenshot = screenshot.replace(b'\r\n', b'\n')
-        else:
-            binary_screenshot = screenshot.replace(b'\r\r\n', b'\n')
-        return binary_screenshot
-    elif screenshot_way == 0:
-        os.system('adb shell screencap -p /sdcard/autojump.png')
-        os.system('adb pull /sdcard/autojump.png .')
+def get_meta(data):
+    meta = {'hostvars': {}}
+    for node in data:
+        meta['hostvars'][node['hostname']] = {'tags': node['tags']}
+    return meta
+    
+    MIN_TOKEN_TYPE = UP+1
+	
+INVALID_TOKEN_TYPE = 0
+    
+                        s = snext
+                    input.consume()
+                    continue
+    
+    
+    def getInputStream(self):
+        '''@brief From what character stream was this token created.
+    
+        def test_static_with_range_full_file(self):
+        response = self.get_and_head('/static/robots.txt', headers={
+            'Range': 'bytes=0-'})
+        # Note: Chrome refuses to play audio if it gets an HTTP 206 in response
+        # to ``Range: bytes=0-`` :(
+        self.assertEqual(response.code, 200)
+        robots_file_path = os.path.join(self.static_dir, 'robots.txt')
+        with open(robots_file_path) as f:
+            self.assertEqual(response.body, utf8(f.read()))
+        self.assertEqual(response.headers.get('Content-Length'), '26')
+        self.assertEqual(response.headers.get('Content-Range'), None)
+    
+        @gen_test
+    def test_http10_no_content_length(self):
+        # Regression test for a bug in which can_keep_alive would crash
+        # for an HTTP/1.0 (not 1.1) response with no content-length.
+        conn = HTTP1Connection(self.client_stream, True)
+        self.server_stream.write(b'HTTP/1.0 200 Not Modified\r\n\r\nhello')
+        self.server_stream.close()
+    
+        def callback(response):
+        response.rethrow()
+        assert len(response.body) == (options.num_chunks * options.chunk_size)
+        logging.warning('fetch completed in %s seconds', response.request_time)
+        IOLoop.current().stop()
+    
+                if links:
+                kw = {}
+                if wrapper:
+                    links = wrap_links(links, wrapper = wrapper)
+                else:
+                    links = wrap_links(links)
+                links = list(links)
+                links = max(links, key = lambda x: x._score) if links else None
+            if not links and wrapper:
+                return wrapper(None)
+            return links
+            # note: even if _by_url successed or a link was passed in,
+            # it is possible link_listing.things is empty if the
+            # link(s) is/are members of a private reddit
+            # return the link with the highest score (if more than 1)
+        except:
+            #we don't want to return 500s in other people's pages.
+            import traceback
+            g.log.debug('FULLPATH: get_link error in buttons code')
+            g.log.debug(traceback.format_exc())
+            if wrapper:
+                return wrapper(None)
+    
+        def send404(self):
+        if 'usable_error_content' in request.environ:
+            return request.environ['usable_error_content']
+        return pages.RedditError(_('page not found'),
+                                 _('the page you requested does not exist')).render()
+    
+    from r2.controllers.reddit_base import MinimalController
+from r2.lib.pages import (
+    GoogleTagManagerJail,
+    GoogleTagManager,
+)
+from r2.lib.validator import (
+    validate,
+    VGTMContainerId,
+)
+    
+        def GET_cachehealth(self):
+        results = {}
+        behaviors = {
+            # Passed on to poll(2) in milliseconds
+            'connect_timeout': 1000,
+            # Passed on to setsockopt(2) in microseconds
+            'receive_timeout': int(1e6),
+            'send_timeout': int(1e6),
+        }
+        for server in cache._CACHE_SERVERS:
+            try:
+                if server.startswith('udp:'):
+                    # libmemcached doesn't support UDP get/fetch operations
+                    continue
+                mc = pylibmc.Client([server], behaviors=behaviors)
+                # it's ok that not all caches are mcrouter, we'll just ignore
+                # the miss either way
+                mc.get('__mcrouter__.version')
+                results[server] = 'OK'
+            except pylibmc.Error as e:
+                g.log.warning('Health check for %s FAILED: %s', server, e)
+                results[server] = 'FAILED %s' % e
+        return json.dumps(results)
