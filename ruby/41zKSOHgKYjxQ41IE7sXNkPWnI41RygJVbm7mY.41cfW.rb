c95c94f8420995f8af6a94ba7864b5419dbbf65d
SUNsung <weatherius@gmail.com>
2018-03-10 02:09:21 +0200

    Rewrite [41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb] File in: 2018-03-10 02:09:20

--- ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
+++ ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
@@ -1,93 +1,124 @@
 
         
-            if test_conf['database']&.present?
-      ActiveRecord::Base.establish_connection(:test)
-      yield
-      ActiveRecord::Base.establish_connection(Rails.env.to_sym)
-    end
+          setup do
+    @old_escape_html_entities_in_json = ActiveSupport.escape_html_entities_in_json
+    ActiveSupport.escape_html_entities_in_json = true
+    @template = self
+    @request = Class.new do
+      def send_early_hints(links) end
+    end.new
   end
     
-      def virtual_tags
-    object.mentions + object.tags + object.emojis
+            VOID_ELEMENTS = %i(area base br col embed hr img input keygen link meta param source track wbr).to_set
+    
+      def test_redirect_to_url
+    get :redirect_to_url
+    assert_response :redirect
+    assert_redirected_to 'http://www.rubyonrails.org/'
   end
     
-      describe 'PUT #update' do
-    it 'updates notifications settings' do
-      user.settings['notification_emails'] = user.settings['notification_emails'].merge('follow' => false)
-      user.settings['interactions'] = user.settings['interactions'].merge('must_be_follower' => true)
+            # Require the adapter itself and give useful feedback about
+        #   1. Missing adapter gems and
+        #   2. Adapter gems' missing dependencies.
+        path_to_adapter = 'action_cable/subscription_adapter/#{adapter}'
+        begin
+          require path_to_adapter
+        rescue LoadError => e
+          # We couldn't require the adapter itself. Raise an exception that
+          # points out config typos and missing gems.
+          if e.path == path_to_adapter
+            # We can assume that a non-builtin adapter was specified, so it's
+            # either misspelled or missing from Gemfile.
+            raise e.class, 'Could not load the '#{adapter}' Action Cable pubsub adapter. Ensure that the adapter is spelled correctly in config/cable.yml and that you've added the necessary adapter gem to your Gemfile.', e.backtrace
     
-          put :update, params: {
-        user: {
-          setting_boost_modal: '1',
-          setting_delete_modal: '0',
-        }
-      }
+      def setup
+    # Increment symbol counter.
+    @symbol = (@@counter ||= 'A0').succ.dup
     
-      describe 'environment' do
-    it 'adds the fuse directories to the appropriate paths' do
-      expect(ENV).to receive(:append_path).with('PKG_CONFIG_PATH', any_args)
-      expect(ENV).to receive(:append_path).with('HOMEBREW_LIBRARY_PATHS', any_args)
-      expect(ENV).to receive(:append_path).with('HOMEBREW_INCLUDE_PATHS', any_args)
-      subject.modify_build_environment
+        test 'head :no_content (204) does not return any content' do
+      content = body(HeadController.action(:no_content).call(Rack::MockRequest.env_for('/')))
+      assert_empty content
     end
+    
+      class ExclaimerMiddleware
+    def initialize(app)
+      @app = app
+    end
+    
+    module ActionMailer
+  # The <tt>ActionMailer::DeliveryJob</tt> class is used when you
+  # want to send emails outside of the request-response cycle.
+  #
+  # Exceptions are rescued and handled by the mailer class.
+  class DeliveryJob < ActiveJob::Base # :nodoc:
+    queue_as { ActionMailer::Base.deliver_later_queue_name }
+    
+      def validate_type
+    errors.add(:type, 'cannot be changed once an instance has been created') if type_changed? && !new_record?
+    errors.add(:type, 'is not a valid type') unless self.class.valid_type?(type)
+  end
+    
+        def stop!
+      @scheduler.jobs(tag: id).each(&:unschedule)
+    
+      private
+    
+    unless dups.empty?
+  puts '\nDuplicate links:'
+  dups.each do |link|
+    puts '- #{link}'
+    puts `grep -nr '#{link}' README.md`
+  end
+  puts '\nDone with errors.'
+  exit(1)
+end
+    
+        HTTP.get(source).to_s.split('\n').each do |line|
+      next if line.start_with? '#'
+      parts = line.split(';').map(&:strip)
+      next if parts.size < 2
+      codes << [parts[0], parts[1].start_with?('fully-qualified')]
+    end
+    
+          def handle_params_error(error, error_description)
+        if params[:client_id] && params[:redirect_uri]
+          handle_params_error_when_client_id_and_redirect_uri_exists(error, error_description)
+        else
+          render_error I18n.t('api.openid_connect.error_page.could_not_authorize'), error_description
+        end
+      end
+    
+          config.paths['log']             = '#{Msf::Config.log_directory}/#{Rails.env}.log'
+      config.paths['config/database'] = [Metasploit::Framework::Database.configurations_pathname.try(:to_path)]
+    
+    GEMFILE_EXTENSIONS = [
+    '.local',
+    ''
+]
+    
+        res << inp.to_html
+  end
+  res << '</form>'
+end
+    
+      def get_result
+    result
   end
 end
     
-          attr_accessor(*VALID_KEYS)
-      attr_accessor :pairs
-    
-          # A string representation of the importer.
-      # Should be overridden by subclasses.
-      #
-      # This is used to help debugging,
-      # and should usually just show the load path encapsulated by this importer.
-      #
-      # @return [String]
-      def to_s
-        Sass::Util.abstract(self)
+          if @options[:update]
+        Sass::Plugin.update_stylesheets(files)
+        exit 1 if had_error
+        return
       end
     
-            def log_level?(level, min_level)
-          log_levels[level] >= log_levels[min_level]
+        # Starts the read-eval-print loop.
+    def run
+      environment = Environment.new
+      @line = 0
+      loop do
+        @line += 1
+        unless (text = Readline.readline('>> '))
+          puts
+          return
         end
-    
-            def initialize
-          @values = []
-        end
-    
-        # @abstract
-    #
-    # Update the clone on the deployment target
-    #
-    # @return void
-    #
-    def update
-      raise NotImplementedError, 'Your SCM strategy module should provide a #update method'
-    end
-    
-        yield
-    
-        private
-    
-        # Returns an extension based on the content type. e.g. 'jpeg' for
-    # 'image/jpeg'. If the style has a specified format, it will override the
-    # content-type detection.
-    #
-    # Each mime type generally has multiple extensions associated with it, so
-    # if the extension from the original filename is one of these extensions,
-    # that extension is used, otherwise, the first in the list is used.
-    def content_type_extension attachment, style_name
-      mime_type = MIME::Types[attachment.content_type]
-      extensions_for_mime_type = unless mime_type.empty?
-        mime_type.first.extensions
-      else
-        []
-      end
-    
-    module Paperclip
-  class << self
-    attr_writer :registered_attachments_styles_path
-    def registered_attachments_styles_path
-      @registered_attachments_styles_path ||= Rails.root.join('public/system/paperclip_attachments.yml').to_s
-    end
-  end
