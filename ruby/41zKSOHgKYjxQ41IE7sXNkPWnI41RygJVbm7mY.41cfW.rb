97c6a3a17840f741ad74c398924e21134694f13c
SUNsung <weatherius@gmail.com>
2018-03-10 02:30:33 +0200

    Rewrite [41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb] File in: 2018-03-10 02:30:32

--- ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
+++ ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
@@ -1,124 +1,39 @@
 
         
-          setup do
-    @old_escape_html_entities_in_json = ActiveSupport.escape_html_entities_in_json
-    ActiveSupport.escape_html_entities_in_json = true
-    @template = self
-    @request = Class.new do
-      def send_early_hints(links) end
-    end.new
-  end
-    
-            VOID_ELEMENTS = %i(area base br col embed hr img input keygen link meta param source track wbr).to_set
-    
-      def test_redirect_to_url
-    get :redirect_to_url
-    assert_response :redirect
-    assert_redirected_to 'http://www.rubyonrails.org/'
-  end
-    
-            # Require the adapter itself and give useful feedback about
-        #   1. Missing adapter gems and
-        #   2. Adapter gems' missing dependencies.
-        path_to_adapter = 'action_cable/subscription_adapter/#{adapter}'
-        begin
-          require path_to_adapter
-        rescue LoadError => e
-          # We couldn't require the adapter itself. Raise an exception that
-          # points out config typos and missing gems.
-          if e.path == path_to_adapter
-            # We can assume that a non-builtin adapter was specified, so it's
-            # either misspelled or missing from Gemfile.
-            raise e.class, 'Could not load the '#{adapter}' Action Cable pubsub adapter. Ensure that the adapter is spelled correctly in config/cable.yml and that you've added the necessary adapter gem to your Gemfile.', e.backtrace
-    
-      def setup
-    # Increment symbol counter.
-    @symbol = (@@counter ||= 'A0').succ.dup
-    
-        test 'head :no_content (204) does not return any content' do
-      content = body(HeadController.action(:no_content).call(Rack::MockRequest.env_for('/')))
-      assert_empty content
+            def self.definitions_for(klass)
+      instance.definitions_for(klass)
     end
     
-      class ExclaimerMiddleware
-    def initialize(app)
-      @app = app
+        # Returns a String describing the file's content type
+    def detect
+      if blank_name?
+        SENSIBLE_DEFAULT
+      elsif empty_file?
+        EMPTY_TYPE
+      elsif calculated_type_matches.any?
+        calculated_type_matches.first
+      else
+        type_from_file_contents || SENSIBLE_DEFAULT
+      end.to_s
     end
     
-    module ActionMailer
-  # The <tt>ActionMailer::DeliveryJob</tt> class is used when you
-  # want to send emails outside of the request-response cycle.
-  #
-  # Exceptions are rescued and handled by the mailer class.
-  class DeliveryJob < ActiveJob::Base # :nodoc:
-    queue_as { ActionMailer::Base.deliver_later_queue_name }
+        EXIF_ROTATED_ORIENTATION_VALUES = [5, 6, 7, 8]
     
-      def validate_type
-    errors.add(:type, 'cannot be changed once an instance has been created') if type_changed? && !new_record?
-    errors.add(:type, 'is not a valid type') unless self.class.valid_type?(type)
-  end
-    
-        def stop!
-      @scheduler.jobs(tag: id).each(&:unschedule)
-    
-      private
-    
-    unless dups.empty?
-  puts '\nDuplicate links:'
-  dups.each do |link|
-    puts '- #{link}'
-    puts `grep -nr '#{link}' README.md`
-  end
-  puts '\nDone with errors.'
-  exit(1)
-end
-    
-        HTTP.get(source).to_s.split('\n').each do |line|
-      next if line.start_with? '#'
-      parts = line.split(';').map(&:strip)
-      next if parts.size < 2
-      codes << [parts[0], parts[1].start_with?('fully-qualified')]
-    end
-    
-          def handle_params_error(error, error_description)
-        if params[:client_id] && params[:redirect_uri]
-          handle_params_error_when_client_id_and_redirect_uri_exists(error, error_description)
-        else
-          render_error I18n.t('api.openid_connect.error_page.could_not_authorize'), error_description
+            def failure_message
+          '#{expected_attachment}\n'.tap do |message|
+            message << accepted_types_and_failures.to_s
+            message << '\n\n' if @allowed_types.present? && @rejected_types.present?
+            message << rejected_types_and_failures.to_s
+          end
         end
+    
+        def load_processor(name)
+      if defined?(Rails.root) && Rails.root
+        filename = '#{name.to_s.underscore}.rb'
+        directories = %w(lib/paperclip lib/paperclip_processors)
+    
+          def drop_attached_file(*args)
+        ActiveSupport::Deprecation.warn 'Method `drop_attached_file` in the migration has been deprecated and will be replaced by `remove_attachment`.'
+        remove_attachment(*args)
       end
-    
-          config.paths['log']             = '#{Msf::Config.log_directory}/#{Rails.env}.log'
-      config.paths['config/database'] = [Metasploit::Framework::Database.configurations_pathname.try(:to_path)]
-    
-    GEMFILE_EXTENSIONS = [
-    '.local',
-    ''
-]
-    
-        res << inp.to_html
-  end
-  res << '</form>'
-end
-    
-      def get_result
-    result
-  end
-end
-    
-          if @options[:update]
-        Sass::Plugin.update_stylesheets(files)
-        exit 1 if had_error
-        return
-      end
-    
-        # Starts the read-eval-print loop.
-    def run
-      environment = Environment.new
-      @line = 0
-      loop do
-        @line += 1
-        unless (text = Readline.readline('>> '))
-          puts
-          return
-        end
+    end
