4618fb7841d0c790b73b67268d317148254b9a86
SUNsung <weatherius@gmail.com>
2018-03-10 07:17:31 +0200

    Rewrite [21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb] File in: 2018-03-10 07:17:30

--- ruby/21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb
+++ ruby/21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb
@@ -1,99 +1,117 @@
 
         
-        class Jekyll::Commands::NewTheme < Jekyll::Command
-  class << self
-    def init_with_program(prog)
-      prog.command(:'new-theme') do |c|
-        c.syntax 'new-theme NAME'
-        c.description 'Creates a new Jekyll theme scaffold'
-        c.option 'code_of_conduct', \
-          '-c', '--code-of-conduct', \
-          'Include a Code of Conduct. (defaults to false)'
-    
-          private
-      def grouped_array(groups)
-        groups.each_with_object([]) do |item, array|
-          array << {
-            'name'  => item.first,
-            'items' => item.last,
-            'size'  => item.last.size,
-          }
+                # This is called early, before a machine is instantiated, to check
+        # if this provider is usable. This should return true or false.
+        #
+        # If raise_error is true, then instead of returning false, this
+        # should raise an error with a helpful message about why this
+        # provider cannot be used.
+        #
+        # @param [Boolean] raise_error If true, raise exception if not usable.
+        # @return [Boolean]
+        def self.usable?(raise_error=false)
+          # Return true by default for backwards compat since this was
+          # introduced long after providers were being written.
+          true
         end
+    
+          # Returns the full path to the template, taking into accoun the gem directory
+      # and adding the `.erb` extension to the end.
+      #
+      # @return [String]
+      def full_template_path
+        @template_root.join('#{template}.erb').to_s.squeeze('/')
       end
     end
   end
 end
 
     
-    module Jekyll
-  class Layout
-    include Convertible
+      def test_plus
+    set = Set[1,2,3]
     
-          def warnings
-        @template.warnings
-      end
-    
-          _form_configurable_fields[name] = options
-    end
-    
-      # Optional
-  #   Override this method if you need to group multiple agents based on an API key,
-  #   or server they connect to.
-  #   Have a look at the TwitterStreamAgent for an example.
-  def self.setup_worker; end
-    
-      def tumblr
-    Tumblr.configure do |config|
-      config.consumer_key = tumblr_consumer_key
-      config.consumer_secret = tumblr_consumer_secret
-      config.oauth_token = tumblr_oauth_token
-      config.oauth_token_secret = tumblr_oauth_token_secret
-    end
-    
-    Tumblr::Client.new
-  end
-end
-    
-      def test_realdirpath_junction
-    Dir.mktmpdir('rubytest-realpath') {|tmpdir|
-      Dir.chdir(tmpdir) do
-        Dir.mkdir('foo')
-        skip 'cannot run mklink' unless system('mklink /j bar foo > nul')
-        assert_equal(File.realpath('foo'), File.realpath('bar'))
-      end
-    }
-  end if /mswin|mingw/ =~ RUBY_PLATFORM
-    
-        $, = ':'
-    a = @cls[1, 2, 3]
-    assert_equal('[1, 2, 3]', a.to_s)
-  ensure
-    $, = nil
+      it 'decodes the remaining longs when passed the '*' modifier after another directive' do
+    array = 'degfhacbacbdegfh'.unpack(unpack_format()+unpack_format('*'))
+    array.should == [7234302065976107874, 7017560827710891624]
   end
     
-      def test_transform_keys
-    x = @cls[a: 1, b: 2, c: 3]
-    y = x.transform_keys {|k| :'#{k}!' }
-    assert_equal({a: 1, b: 2, c: 3}, x)
-    assert_equal({a!: 1, b!: 2, c!: 3}, y)
-    
-      def test_compare_by_identity
-    a1, a2 = 'a', 'a'
-    b1, b2 = 'b', 'b'
-    c = 'c'
-    array = [a1, b1, c, a2, b2]
-    
-      quarantine! do
+      # Hangs on 1.8.6.114 OS X, possibly also on Linux
+  quarantine! do
+    it 'is deferred if ensure clause sleeps' do
+      ThreadSpecs.wakeup_dying_sleeping_thread(@method) { sleep; ScratchPad.record :after_sleep }
+      ScratchPad.recorded.should == :after_sleep
+    end
+  end
     
         prune
   end
     
-        def self.clear
-      instance.clear
+        private
+    
+          # Get the cache key pair for the given Sass URI.
+      # The URI need not be checked for validity.
+      #
+      # The only strict requirement is that the returned pair of strings
+      # uniquely identify the file at the given URI.
+      # However, the first component generally corresponds roughly to the directory,
+      # and the second to the basename, of the URI.
+      #
+      # Note that keys must be unique *across importers*.
+      # Thus it's probably a good idea to include the importer name
+      # at the beginning of the first component.
+      #
+      # @param uri [String] A URI known to be valid for this importer.
+      # @param options [{Symbol => Object}] Options for the Sass file
+      #   containing the `@import` currently being checked.
+      # @return [(String, String)] The key pair which uniquely identifies
+      #   the file at the given URI.
+      def key(uri, options)
+        Sass::Util.abstract(self)
+      end
+    
+        # Never trust parameters from the scary internet, only allow the white list through.
+    def book_params
+      params.require(:book).permit(:name)
     end
+end
+
     
-      private
+            def name
+          @node.children.first
+        end
     
-              def find_plugins_gem_specs
-            @specs ||= ::Gem::Specification.find_all.select{|spec| logstash_plugin_gem_spec?(spec)}
-          end
+    module RuboCop
+  module Cop
+    module Layout
+      # Checks for unnecessary additional spaces inside array percent literals
+      # (i.e. %i/%w).
+      #
+      # @example
+      #
+      #   # bad
+      #   %w(foo  bar  baz)
+      #   # good
+      #   %i(foo bar baz)
+      class SpaceInsideArrayPercentLiteral < Cop
+        include MatchRange
+        include PercentLiteral
+    
+            def autocorrect(node)
+          # Regexp#match can take a second argument, but this cop doesn't
+          # register an offense in that case
+          return unless node.first_argument.regexp_type?
+    
+            private
+    
+            def on_block(node)
+          on_body_of_reduce(node) do |body|
+            void_next = body.each_node(:next).find do |n|
+              n.children.empty? && parent_block_node(n) == node
+            end
+    
+              [optarg_positions, arg_positions]
+        end
+      end
+    end
+  end
+end
