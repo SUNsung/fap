3c6a6e3b78114f0dda4aad94db4891274377989a
SUNsung <weatherius@gmail.com>
2018-03-11 15:17:51 +0200

    Rewrite [12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb] File in: 2018-03-11 15:17:50

--- ruby/12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb
+++ ruby/12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb
@@ -1,106 +1,95 @@
 
         
-              BOOLEAN_ATTRIBUTES.merge(BOOLEAN_ATTRIBUTES.map(&:to_sym))
+        EOS
+end
     
-                @subscribe_callbacks = Hash.new { |h, k| h[k] = [] }
-            @subscription_lock = Mutex.new
-    
-          def set_body
-        self.response_body = 'Success'
-      end
-    end
-    
-      def test_lib_helper_methods_after_clear_helpers
-    assert_nothing_raised do
-      call_controller(JustMeController, 'lib')
-    end
+    def report_apps
+  running = Set.new
+  @app_names.zip(@bundle_ids, @unix_ids).each do |app_name, bundle_id, _unix_id|
+    next if excluded_bundle_id bundle_id
+    next if excluded_app_name app_name
+    bundle_id.gsub!(%r{^(missing value)$}, '<\1>')
+    running.add '#{bundle_id}\t#{app_name}'
   end
     
-        def wrap_delivery_behavior!(*args) # :nodoc:
-      self.class.wrap_delivery_behavior(message, *args)
+        it 'leaves the domains status and attachements, but clears media' do
+      expect { bad_status1.reload }.not_to raise_error
+      expect { bad_status2.reload }.not_to raise_error
+      expect { bad_attachment.reload }.not_to raise_error
+      expect(bad_attachment.file.exists?).to be false
     end
   end
 end
 
     
-          formatted
+        #
+    # @param ips [String] IP range(s)
+    # @return [true] if actions on ips are allowed.
+    # @return [false] if actions are not allowed on ips.
+    def allow_actions_on?(ips)
+      return true unless limit_to_network
+      return true unless boundary
+      return true if boundary.empty?
+      boundaries = Shellwords.split(boundary)
+      return true if boundaries.empty? # It's okay if there is no boundary range after all
+      given_range = Rex::Socket::RangeWalker.new(ips)
+      return false unless given_range # Can't do things to nonexistant IPs
+      allowed = false
+      boundaries.each do |boundary_range|
+        ok_range = Rex::Socket::RangeWalker.new(boundary)
+        allowed  = true if ok_range.include_range? given_range
+      end
+      return allowed
     end
+    
+        inp.attributes.keys.each do |ikey|
+      if (ikey.downcase == 'value')
+        inp[ikey] = ''
+        next
+      end
+    
+            when :err
+          case s[:last]
+            when :pass
+              # Oops got a -ERR after a pass so its crap ignore the pass
+              # But report it, might be helpfull for guessing :-)
+    
+    classNames.each { |name|
+	filesIn << clsFile.new_with_sig('Ljava.lang.String;', '#{outputDir}/#{name}.class')
+}
     
           private
-        def processed_mailer
-          @processed_mailer ||= @mailer_class.new.tap do |mailer|
-            mailer.params = @params
-            mailer.process @action, *@args
+    
+            on roles(target_roles) do
+          unless test '[ -f #{file.to_s.shellescape} ]'
+            info 'Uploading #{prerequisite_file} to #{file}'
+            upload! File.open(prerequisite_file), file
           end
         end
-    
-    class AssertSelectEmailTest < ActionMailer::TestCase
-  class AssertSelectMailer < ActionMailer::Base
-    def test(html)
-      mail body: html, content_type: 'text/html',
-        subject: 'Test e-mail', from: 'test@test.host', to: 'test <test@test.host>'
-    end
-  end
-    
-      def grant_admin!
-    set_permission('admin', true)
-  end
-    
-          if base == dest
-        ''
-      elsif dest.start_with? File.join(base, '')
-        url.path[(path.length)..-1]
       end
     end
     
-          def get_type
-        case slug
-        when 'api'
-          'Reference'
-        when 'configuration'
-          'Reference: Configuration'
-        when 'stpl'
-          'Reference: SimpleTemplate'
-        when 'plugindev'
-          'Reference: Plugin'
-        else
-          'Manual'
+        # Fetch a var from the context
+    # @param [Symbol] variable The variable to fetch
+    # @param [Object] default  The default value if not found
+    #
+    def fetch(*args)
+      context.fetch(*args)
+    end
+    
+          describe 'setting an internal roles filter' do
+        subject { dsl.roles(:app) }
+        it 'ignores it' do
+          dsl.set :filter, roles: :web
+          expect(subject.map(&:hostname)).to eq(['example3.com', 'example4.com'])
         end
       end
     
-            # Returns the internal data associated with this plugin. This
-        # should NOT be called by the general public.
-        #
-        # @return [Hash]
-        def self.data
-          @data ||= {}
-        end
+          expect('.border-style-alternate').to have_rule(rule)
+    end
+  end
     
-    process_args
-list_login_items_for_app $app_path
-
-    
-    if $opt_test
-  exit test_app($opt_test)
-else
-  report_apps
+          expect('.all-text-inputs-invalid').to have_ruleset(ruleset)
+    end
+  end
 end
-
-    
-    end
-    
-            self.description = <<-DESC
-          Remove the cache for a given pod, or clear the cache completely.
-    
-            self.description = <<-DESC
-          Shows the content of the pods cache as a YAML tree output, organized by pod.
-          If `NAME` is given, only the caches for that pod will be included in the output.
-        DESC
-    
-            TEMPLATE_REPO = 'https://github.com/CocoaPods/pod-template.git'.freeze
-        TEMPLATE_INFO_URL = 'https://github.com/CocoaPods/pod-template'.freeze
-        CREATE_NEW_POD_INFO_URL = 'http://guides.cocoapods.org/making/making-a-cocoapod'.freeze
-    
-          def snapshot
-        agent.metric.collector.snapshot_metric
-      end
