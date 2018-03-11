0dc635bbd75c0098be9dd5777c99b3be976d9975
SUNsung <weatherius@gmail.com>
2018-03-11 12:53:53 +0200

    Rewrite [33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb] File in: 2018-03-11 12:53:52

--- ruby/33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb
+++ ruby/33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb
@@ -1,41 +1,131 @@
 
         
-                expect_any_instance_of(ActivityPub::LinkedDataSignature).to receive(:verify_account!).and_return(actor)
-        expect(ActivityPub::Activity).to receive(:factory).with(instance_of(Hash), actor, instance_of(Hash))
+          bottle_ext = filename[bottle_native_regex, 1]
+  bottle_url_ext = f.bottle.url[bottle_native_regex, 1]
     
-            render json: collection_presenter,
-               serializer: ActivityPub::CollectionSerializer,
-               adapter: ActivityPub::Adapter,
-               content_type: 'application/activity+json'
+          if ARGV.git?
+        system 'git', 'init'
+        system 'git', 'add', '-A'
+      end
+      if ARGV.interactive?
+        ohai 'Entering interactive mode'
+        puts 'Type `exit' to return and finalize the installation'
+        puts 'Install to this prefix: #{formula.prefix}'
+    
+      # True if a {Formula} is being built universally.
+  # e.g. on newer Intel Macs this means a combined x86_64/x86 binary/library.
+  # <pre>args << '--universal-binary' if build.universal?</pre>
+  def universal?
+    include?('universal') && option_defined?('universal')
+  end
+    
+      def zsh_completion_caveats
+    if keg && keg.completion_installed?(:zsh) then <<-EOS.undent
+      zsh completion has been installed to:
+        #{HOMEBREW_PREFIX}/share/zsh/site-functions
+      EOS
+    end
+  end
+    
+    module Homebrew
+  def doctor
+    checks = Diagnostic::Checks.new
+    
+        root.children.sort.each do |pn|
+      if pn.directory?
+        dirs << pn
+      elsif block_given? && yield(pn)
+        puts pn
+        other = 'other '
+      else
+        remaining_root_files << pn unless pn.basename.to_s == '.DS_Store'
       end
     end
+    
+      def migrate_formula_rename
+    report[:R].each do |old_full_name, new_full_name|
+      old_name = old_full_name.split('/').last
+      next unless (dir = HOMEBREW_CELLAR/old_name).directory? && !dir.subdirs.empty?
+    
+      def failure_message
+    exception = request.respond_to?(:get_header) ? request.get_header('omniauth.error') : request.env['omniauth.error']
+    error   = exception.error_reason if exception.respond_to?(:error_reason)
+    error ||= exception.error        if exception.respond_to?(:error)
+    error ||= (request.respond_to?(:get_header) ? request.get_header('omniauth.error.type') : request.env['omniauth.error.type']).to_s
+    error.to_s.humanize if error
   end
     
-          expect(response).to redirect_to(settings_notifications_path)
-      user.reload
-      expect(user.settings['notification_emails']['follow']).to be true
-      expect(user.settings['interactions']['must_be_follower']).to be false
-    end
-  end
-end
+    # TODO:
+# group :mongoid do
+#   gem 'mongoid', '~> 4.0.0'
+# end
 
     
-          puts '>>> Sass is watching for changes. Press Ctrl-C to stop.'
+          # Initialize script_name with nil to prevent infinite loops in
+      # authenticated mounted engines in rails 4.2 and 5.0
+      opts[:script_name] = nil
     
-          # If the importer is based on files on the local filesystem
-      # this method should return folders which should be watched
-      # for changes.
-      #
-      # @return [Array<String>] List of absolute paths of directories to watch
-      def directories_to_watch
-        []
-      end
+        alias_method :blank?, :empty?
     
-      # Uninstalls this logger from \{Sass.logger\}. This should only be called if
-  # the logger was installed using \{#install!}
-  def uninstall!
-    if Sass.logger != self
-      throw Exception.new('Can't uninstall a logger that's not currently installed.')
+        def insert(index, *names)
+      @filters.insert assert_index(index), *filter_const(names)
     end
     
-      config_name 'codec'
+            css('> .section', '#preamble', 'a[href*='dict.html']', 'code var', 'code strong').each do |node|
+          node.before(node.children).remove
+        end
+    
+    Bundler.require(
+    *Rails.groups(
+        coverage: [:test],
+        db: all_environments,
+        pcap: all_environments
+    )
+)
+    
+        # We want to return immediatly if we do not have a packet which is handled by us
+    return unless pkt.is_tcp?
+    return if (pkt.tcp_sport != 143 and pkt.tcp_dport != 143)
+    s = find_session((pkt.tcp_sport == 143) ? get_session_src(pkt) : get_session_dst(pkt))
+    s[:sname] ||= 'imap4'
+    
+    # Sniffer class for GET URL's
+class SnifferURL < BaseProtocolParser
+  def register_sigs
+    self.sigs = {
+      :get		=> /^GET\s+([^\n]+)\s+HTTP\/\d\.\d/i,
+      :webhost	=> /^HOST\:\s+([^\n\r]+)/i,
+    }
+  end
+    
+    class Source < Template
+  attr_accessor :__CAL
+  attr_accessor :__NR_execve
+  attr_accessor :__NR_getpeername
+  attr_accessor :__NR_accept
+  attr_accessor :__NR_listen
+  attr_accessor :__NR_bind
+  attr_accessor :__NR_socket
+  attr_accessor :__NR_connect
+  attr_accessor :__NR_close
+  attr_accessor :__NR_kfcntl
+  attr_accessor :__cal
+  attr_accessor :_cal
+  attr_accessor :cal
+  attr_accessor :ver
+    
+            def operator_assignment_node
+          return nil unless node.parent
+          return nil unless OPERATOR_ASSIGNMENT_TYPES.include?(node.parent.type)
+          return nil unless node.sibling_index.zero?
+          node.parent
+        end
+    
+              add_offense(node)
+        end
+    
+            def on_block(node)
+          on_body_of_reduce(node) do |body|
+            void_next = body.each_node(:next).find do |n|
+              n.children.empty? && parent_block_node(n) == node
+            end
