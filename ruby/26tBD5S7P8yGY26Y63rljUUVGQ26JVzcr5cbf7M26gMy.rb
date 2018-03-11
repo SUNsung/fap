b6c601c78d9eab22121e2037367f65f78649ec2e
SUNsung <weatherius@gmail.com>
2018-03-11 07:01:53 +0200

    Rewrite [26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb] File in: 2018-03-11 07:01:53

--- ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
+++ ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
@@ -1,134 +1,68 @@
 
         
-          setup do
-    @old_escape_html_entities_in_json = ActiveSupport.escape_html_entities_in_json
-    ActiveSupport.escape_html_entities_in_json = true
-    @template = self
-    @request = Class.new do
-      def send_early_hints(links) end
-    end.new
-  end
+            Jekyll::Commands::Build.process({'source' => 'docs'})
     
-        # fun/games_helper.rb
-    assert_includes methods, :stratego
-    
-        def with_test_route_set
-      with_routing do |set|
-        set.draw do
-          match '/', to: 'web_service_test/test#assign_parameters', via: :all
-        end
-        yield
+      p.action do |args, _|
+    if args.empty?
+      Jekyll.logger.error 'A subcommand is required.'
+      puts p
+      abort
+    else
+      subcommand = args.first
+      unless p.has_command? subcommand
+        Jekyll.logger.abort_with 'fatal: 'jekyll #{args.first}' could not' \
+          ' be found. You may need to install the jekyll-#{args.first} gem' \
+          ' or a related gem to be able to use this subcommand.'
       end
     end
-end
-
-    
-        private
-      # 'Deserialize' the mailer class name by hand in case another argument
-      # (like a Global ID reference) raised DeserializationError.
-      def mailer_class
-        if mailer = Array(@serialized_arguments).first || Array(arguments).first
-          mailer.constantize
-        end
-      end
-    
-            case method
-        when NilClass
-          raise 'Delivery method cannot be nil'
-        when Symbol
-          if klass = delivery_methods[method]
-            mail.delivery_method(klass, (send(:'#{method}_settings') || {}).merge(options || {}))
-          else
-            raise 'Invalid delivery method #{method.inspect}'
-          end
-        else
-          mail.delivery_method(method)
-        end
-    
-      # Skips the current run on Rubinius using Minitest::Assertions#skip
-  private def rubinius_skip(message = '')
-    skip message if RUBY_ENGINE == 'rbx'
-  end
-  # Skips the current run on JRuby using Minitest::Assertions#skip
-  private def jruby_skip(message = '')
-    skip message if defined?(JRUBY_VERSION)
   end
 end
 
     
-        mod_time_contents = Time.at 1306527039
+          # Group an array of items by an expression
+      #
+      # input - the object array
+      # variable - the variable to assign each item to in the expression
+      # expression -a Liquid comparison expression passed in as a string
+      #
+      # Returns the filtered array of objects
+      def group_by_exp(input, variable, expression)
+        return input unless groupable?(input)
     
-                def delete_if
-              block_given? or return enum_for(__method__) { size }
-              n = @hash.size
-              super
-              @keys = nil if @hash.size != n
-              self
-            end
+        # Gets/Sets the content of this layout.
+    attr_accessor :content
     
-      it 'decodes the remaining doubles when passed the '*' modifier after another directive' do
-    array = '@\x15333333@\x22ffffff'.unpack(unpack_format()+unpack_format('*'))
-    array.should == [5.3, 9.2]
+      def userpaths?
+    @settings.include? :userpaths
+  end
+end
+    
+      def core_tap_head
+    CoreTap.instance.git_head || '(none)'
   end
     
-          opts.on('-s', '--stdin', :NONE,
-              'Read input from standard input instead of an input file.',
-              'This is the default if no input file is specified. Requires --from.') do
-        @options[:input] = $stdin
-      end
+    module Homebrew
+  def doctor
+    checks = Diagnostic::Checks.new
     
-          # Get the cache key pair for the given Sass URI.
-      # The URI need not be checked for validity.
-      #
-      # The only strict requirement is that the returned pair of strings
-      # uniquely identify the file at the given URI.
-      # However, the first component generally corresponds roughly to the directory,
-      # and the second to the basename, of the URI.
-      #
-      # Note that keys must be unique *across importers*.
-      # Thus it's probably a good idea to include the importer name
-      # at the beginning of the first component.
-      #
-      # @param uri [String] A URI known to be valid for this importer.
-      # @param options [{Symbol => Object}] Options for the Sass file
-      #   containing the `@import` currently being checked.
-      # @return [(String, String)] The key pair which uniquely identifies
-      #   the file at the given URI.
-      def key(uri, options)
-        Sass::Util.abstract(self)
-      end
+        find_union(segments, Project).includes(:namespace).order_id_desc
+  end
     
-        def render(context)
-      if parts = @text.match(/([a-zA-Z\d]*) (.*)/)
-        gist, file = parts[1].strip, parts[2].strip
-      else
-        gist, file = @text.strip, ''
-      end
-      if gist.empty?
-        ''
-      else
-        script_url = script_url_for gist, file
-        code       = get_cached_gist(gist, file) || get_gist_from_web(gist, file)
-        html_output_for script_url, code
-      end
-    end
-    
-        def clear
-      @attachments = Hash.new { |h,k| h[k] = {} }
-    end
-    
-        def scaling dst, ratio
-      if ratio.horizontal? || ratio.square?
-        [ '%dx' % dst.width, ratio.width ]
-      else
-        [ 'x%d' % dst.height, ratio.height ]
-      end
-    end
-    
-              @subject.send(@attachment_name).post_processing = false
-          @subject.send(@attachment_name).assign(file)
-          @subject.valid?
-          @subject.errors[:'#{@attachment_name}_file_size'].blank?
-        ensure
-          @subject.send(@attachment_name).post_processing = true
+        # @mixin transition($transition) {
+    # to:
+    # @mixin transition($transition...) {
+    def varargify_mixin_definitions(scss, *mixins)
+      scss = scss.dup
+      replaced = []
+      mixins.each do |mixin|
+        if scss.gsub! /(@mixin\s*#{Regexp.quote(mixin)})\((#{SCSS_MIXIN_DEF_ARGS_RE})\)/, '\1(\2...)'
+          replaced << mixin
         end
+      end
+      log_transform *replaced unless replaced.empty?
+      scss
+    end
+    }
+    }
+    
+      config.active_support.test_order = :random
