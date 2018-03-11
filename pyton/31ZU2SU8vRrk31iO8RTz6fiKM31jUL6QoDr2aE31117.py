a04d3cabafabeb46e59230fc18d14ce540295b34
SUNsung <weatherius@gmail.com>
2018-03-11 05:46:48 +0200

    Rewrite [31ZU2SU8vRrk31iO8RTz6fiKM31jUL6QoDr2aE31117.py] File in: 2018-03-11 05:46:47

--- pyton/31ZU2SU8vRrk31iO8RTz6fiKM31jUL6QoDr2aE31117.py
+++ pyton/31ZU2SU8vRrk31iO8RTz6fiKM31jUL6QoDr2aE31117.py
@@ -1,232 +1,171 @@
 
         
-                String:                    '#4e9a06',        # class: 's'
-        String.Backtick:           '#4e9a06',        # class: 'sb'
-        String.Char:               '#4e9a06',        # class: 'sc'
-        String.Doc:                'italic #8f5902', # class: 'sd' - like a comment
-        String.Double:             '#4e9a06',        # class: 's2'
-        String.Escape:             '#4e9a06',        # class: 'se'
-        String.Heredoc:            '#4e9a06',        # class: 'sh'
-        String.Interpol:           '#4e9a06',        # class: 'si'
-        String.Other:              '#4e9a06',        # class: 'sx'
-        String.Regex:              '#4e9a06',        # class: 'sr'
-        String.Single:             '#4e9a06',        # class: 's1'
-        String.Symbol:             '#4e9a06',        # class: 'ss'
-    
-    import os
-import h5py
-import json
-    
-          # Set activation of conv2 layer of attention model.
-      with tf.variable_scope(
-          'merge', values=[attention_feature_map, attention_score]):
-        if attention_nonlinear not in _SUPPORTED_ATTENTION_NONLINEARITY:
-          raise ValueError('Unknown attention non-linearity.')
-        if attention_nonlinear == 'softplus':
-          with tf.variable_scope(
-              'softplus_attention',
-              values=[attention_feature_map, attention_score]):
-            attention_prob = tf.nn.softplus(attention_score)
-            attention_feat = tf.reduce_mean(
-                tf.multiply(attention_feature_map, attention_prob), [1, 2])
-        attention_feat = tf.expand_dims(tf.expand_dims(attention_feat, 1), 2)
-    return attention_feat, attention_prob, attention_score
-    
-    import tensorflow as tf
-import tensorflow.contrib.eager as tfe
-    
-    import os
+            @staticmethod
+    def make_header(username, password):
+        credentials = u'%s:%s' % (username, password)
+        token = b64encode(credentials.encode('utf8')).strip().decode('latin1')
+        return 'Basic %s' % token
     
     
-parser = argparse.ArgumentParser()
-parser.add_argument('--batch_size', default=100, type=int, help='batch size')
-parser.add_argument('--train_steps', default=1000, type=int,
-                    help='number of training steps')
+_auth_plugins = plugin_manager.get_auth_plugins()
+auth.add_argument(
+    '--auth-type', '-A',
+    choices=_AuthTypeLazyChoices(),
+    default=None,
+    help='''
+    The authentication mechanism to be used. Defaults to '{default}'.
     
     
-def make_vocab_ids(vocab_filename):
-  if FLAGS.output_char:
-    ret = dict([(char, i) for i, char in enumerate(string.printable)])
-    ret[data.EOS_TOKEN] = len(string.printable)
-    return ret
-  else:
-    with open(vocab_filename) as vocab_f:
-      return dict([(line.strip(), i) for i, line in enumerate(vocab_f)])
+class ExitStatus:
+    '''Exit status code constants.'''
+    OK = 0
+    ERROR = 1
+    PLUGIN_ERROR = 7
     
-        # a Sequential inside a Sequential
-    inner_model = Sequential()
-    inner_model.add(Dense(2, input_dim=1))
-    outer_model = Sequential()
-    outer_model.add(inner_model)
-    assert outer_model.trainable_weights == inner_model.trainable_weights
-    inner_model.trainable = False
-    assert outer_model.trainable_weights == []
-    inner_model.trainable = True
-    inner_model.layers[-1].trainable = False
-    assert outer_model.trainable_weights == []
+            # behaviour near extremes of floating-point range
+        int_dbl_max = int(DBL_MAX)
+        top_power = 2**DBL_MAX_EXP
+        halfway = (int_dbl_max + top_power)//2
+        self.assertEqual(float(int_dbl_max), DBL_MAX)
+        self.assertEqual(float(int_dbl_max+1), DBL_MAX)
+        self.assertEqual(float(halfway-1), DBL_MAX)
+        self.assertRaises(OverflowError, float, halfway)
+        self.assertEqual(float(1-halfway), -DBL_MAX)
+        self.assertRaises(OverflowError, float, -halfway)
+        self.assertRaises(OverflowError, float, top_power-1)
+        self.assertRaises(OverflowError, float, top_power)
+        self.assertRaises(OverflowError, float, top_power+1)
+        self.assertRaises(OverflowError, float, 2*top_power-1)
+        self.assertRaises(OverflowError, float, 2*top_power)
+        self.assertRaises(OverflowError, float, top_power*top_power)
     
+        def test_extra_spaces(self):
+        C = cookies.SimpleCookie()
+        C.load('eggs  =  scrambled  ;  secure  ;  path  =  bar   ; foo=foo   ')
+        self.assertEqual(C.output(),
+            'Set-Cookie: eggs=scrambled; Path=bar; Secure\r\nSet-Cookie: foo=foo')
     
-def create_ngram_set(input_list, ngram_value=2):
-    '''
-    Extract a set of n-grams from a list of integers.
+    The __init__ method has one required argument, a file-like object
+(including a chunk instance), and one optional argument, a flag which
+specifies whether or not chunks are aligned on 2-byte boundaries.  The
+default is 1, i.e. aligned.
+'''
     
-    # Embedding
-max_features = 20000
-maxlen = 100
-embedding_size = 128
+    def evalString(s):
+    assert s.startswith(''') or s.startswith('''), repr(s[:1])
+    q = s[0]
+    if s[:3] == q*3:
+        q = q*3
+    assert s.endswith(q), repr(s[-len(q):])
+    assert len(s) >= 2*len(q)
+    s = s[len(q):-len(q)]
+    return re.sub(r'\\(\'|\'|\\|[abfnrtv]|x.{0,2}|[0-7]{1,3})', escape, s)
     
-    # an auxiliary loss function
-# designed to maintain the 'content' of the
-# base image in the generated image
+        def safe_tp_name(self):
+        try:
+            return self.type().field('tp_name').string()
+        except NullPyObjectPtr:
+            # NULL tp_name?
+            return 'unknown'
+        except RuntimeError:
+            # Can't even read the object at all?
+            return 'unknown'
     
+        def test_car_adapter_shall_make_loud_noise(self):
+        car = Car()
+        car_adapter = Adapter(car, make_noise=car.make_noise)
+        noise = car_adapter.make_noise(1)
+        expected_noise = 'vroom!'
+        self.assertEqual(noise, expected_noise)
+    
+        @classmethod
+    def __get_test_directory(self):
         '''
-    model = Sequential()
-    model.add(layers.Embedding(10, 10, mask_zero=True))
-    model.add(layers.Activation('softmax'))
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='adam')
+        Get the temporary directory for the tests.
+        '''
+        self.test_dir = os.path.join(os.path.dirname(
+            os.path.realpath(__file__)), 'test_command')
     
-                # check state initialization
-            layer = convolutional_recurrent.ConvLSTM2D(filters=filters,
-                                                       kernel_size=(num_row, num_col),
-                                                       data_format=data_format,
-                                                       return_sequences=return_sequences)
-            layer.build(inputs.shape)
-            x = Input(batch_shape=inputs.shape)
-            initial_state = layer.get_initial_state(x)
-            y = layer(x, initial_state=initial_state)
-            model = Model(x, y)
-            assert model.predict(inputs).shape == layer.compute_output_shape(inputs.shape)
-    
-        # Test with Sequential API
-    model = Sequential([
-        layers.Dense(16, input_shape=(x_train.shape[-1],), activation='relu'),
-        layers.Dense(8),
-        layers.Activation('relu'),
-        layers.Dense(num_classes, activation='softmax')
-    ])
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='rmsprop',
-                  metrics=['accuracy'])
-    model.summary()
-    history = model.fit(x_train, y_train, epochs=15, batch_size=16,
-                        validation_data=(x_test, y_test),
-                        verbose=0)
-    assert(history.history['val_acc'][-1] > 0.8)
-    config = model.get_config()
-    model = Sequential.from_config(config)
-    
-    
-class GroupChat(Chat):
-    
-        def __init__(self, license_plate):
-        super(Motorcycle, self).__init__(VehicleSize.MOTORCYCLE, license_plate, spot_size=1)
-    
-    
-class Budget(object):
-    
-        def mapper(self, _, line):
-        '''Parse each log line, extract and transform relevant lines.
-    
-        @gen_test
-    def test_get_clears_timed_out_getters(self):
-        q = queues.Queue()
-        getters = [q.get(timedelta(seconds=0.01)) for _ in range(10)]
-        get = q.get()
-        self.assertEqual(11, len(q._getters))
-        yield gen.sleep(0.02)
-        self.assertEqual(11, len(q._getters))
-        self.assertFalse(get.done())  # Final waiter is still active.
-        q.get()  # get() clears the waiters.
-        self.assertEqual(2, len(q._getters))
-        for getter in getters:
-            self.assertRaises(TimeoutError, getter.result)
-    
-        def test_simple(self):
-        response = self.fetch('/')
-        self.assertEqual(response.body, b'Hello world!')
-    
-            self.writer = socket.socket()
-        set_close_exec(self.writer.fileno())
-        # Disable buffering -- pulling the trigger sends 1 byte,
-        # and we want that sent immediately, to wake up ASAP.
-        self.writer.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-    
-            p = Popen(
-            [sys.executable, '-m', 'testapp'], stdout=subprocess.PIPE,
-            cwd=path, env=dict(os.environ, PYTHONPATH=pythonpath),
-            universal_newlines=True)
-        out = p.communicate()[0]
-        self.assertEqual(out, 'Starting\nStarting\n')
+    ### OUTPUT ###
+# Jack move 5m then stop
 
     
-    try:
-    xrange
-except NameError:
-    xrange = range
+    
+class RadioTest(unittest.TestCase):
+    '''
+    Attention: Test case results depend on test case execution. The test cases
+    in this integration test class should be executed in an explicit order:
+    http://stackoverflow.com/questions/5387299/python-unittest-testcase-execution-order
+    '''
+    
+    '''
+Port of the Java example of 'Setter Injection' in
+'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
+(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
+http://xunitpatterns.com/Dependency%20Injection.html.
     
     
-if __name__ == '__main__':
-    main()
-
+def main():
+    command_stack = []
     
-        i, next_rest, next_rest_time = (0, random.randrange(3, 10),
-                                    random.randrange(5, 10))
-    while True:
-        im = screenshot.pull_screenshot()
-        # 获取棋子和 board 的位置
-        piece_x, piece_y, board_x, board_y, delta_piece_y = find_piece_and_board(im)
-        ts = int(time.time())
-        print(ts, piece_x, piece_y, board_x, board_y)
-        set_button_position(im)
-        jump(math.sqrt((board_x - piece_x) ** 2 + (board_y - piece_y) ** 2), delta_piece_y)
-        if DEBUG_SWITCH:
-            debug.save_debug_screenshot(ts, im, piece_x,
-                                        piece_y, board_x, board_y)
-            debug.backup_screenshot(ts)
-        im.close()
-        i += 1
-        if i == next_rest:
-            print('已经连续打了 {} 下，休息 {}秒'.format(i, next_rest_time))
-            for j in range(next_rest_time):
-                sys.stdout.write('\r程序将在 {}秒 后继续'.format(next_rest_time - j))
-                sys.stdout.flush()
-                time.sleep(1)
-            print('\n继续')
-            i, next_rest, next_rest_time = (0, random.randrange(30, 100),
-                                            random.randrange(10, 60))
-        # 为了保证截图的时候应落稳了，多延迟一会儿，随机值防 ban
-        time.sleep(random.uniform(1.2, 1.4))
+            return coord_y_start_scan
     
-        else:
-        print('ParamError: MODE should be ['auto', 'manual'].')
+    '''
+# === 思路 ===
+# 核心：每次落稳之后截图，根据截图算出棋子的坐标和下一个块顶面的中点坐标，
+#      根据两个点的距离乘以一个时间系数获得长按的时间
+# 识别棋子：靠棋子的颜色来识别位置，通过截图发现最下面一行大概是一条
+           直线，就从上往下一行一行遍历，比较颜色（颜色用了一个区间来比较）
+           找到最下面的那一行的所有点，然后求个中点，求好之后再让 Y 轴坐标
+           减小棋子底盘的一半高度从而得到中心点的坐标
+# 识别棋盘：靠底色和方块的色差来做，从分数之下的位置开始，一行一行扫描，
+           由于圆形的块最顶上是一条线，方形的上面大概是一个点，所以就
+           用类似识别棋子的做法多识别了几个点求中点，这时候得到了块中点的 X
+           轴坐标，这时候假设现在棋子在当前块的中心，根据一个通过截图获取的
+           固定的角度来推出中点的 Y 坐标
+# 最后：根据两点的坐标算距离乘以系数来获取长按时间（似乎可以直接用 X 轴距离）
+'''
+import os
+import shutil
+import time
+import math
+import random
+import json
+from PIL import Image, ImageDraw
+import wda
+    
+        click_count += 1
+    if click_count > 1:
+        click_count = 0
+        cor1 = cor.pop()
+        cor2 = cor.pop()
+    
+        def get_screen(self):
+        process = os.popen(self.adb_path + ' shell wm size')
+        output = process.read()
+        return output
     
     
-def on_click(event):
-    global update    
-    global src_x, src_y
+def open_accordant_config():
+    '''
+    调用配置文件
+    '''
+    screen_size = _get_screen_size()
+    config_file = '{path}/config/{screen_size}/config.json'.format(
+        path=sys.path[0],
+        screen_size=screen_size
+    )
     
-    dst_x, dst_y = event.xdata, event.ydata
     
-    def jump(distance):
+def jump(distance, delta_piece_y):
     '''
     跳跃一定的距离
     '''
-    press_time = distance * press_coefficient
-    press_time = max(press_time, 200)   # 设置 200ms 是最小的按压时间
+    # 计算程序长度与截图测得的距离的比例
+    scale = 0.945 * 2 / head_diameter
+    actual_distance = distance * scale * (math.sqrt(6) / 2)
+    press_time = (-945 + math.sqrt(945 ** 2 + 4 * 105 *
+                                   36 * actual_distance)) / (2 * 105) * 1000
+    press_time *= press_coefficient
+    press_time = max(press_time, 200)  # 设置 200ms 是最小的按压时间
     press_time = int(press_time)
-    
-            if board_x_sum:
-            board_x = board_x_sum / board_x_c
-    
-            # 获取棋子和 board 的位置
-        piece_x, board_x = find_piece_and_board(im)
-        gameover = 0 if all((piece_x, board_x)) else 1
-        swipe_x1, swipe_y1 = set_button_position(
-            im, gameover=gameover)  # 随机点击位置
-    
-    
-fig = plt.figure()
-pull_screenshot()
-img = np.array(Image.open('autojump.png'))
-im = plt.imshow(img, animated=True)
