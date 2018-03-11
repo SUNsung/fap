2cdc094235cad2f9ace1c14200a9b239b7acde0e
SUNsung <weatherius@gmail.com>
2018-03-11 17:52:27 +0200

    Rewrite [556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb] File in: 2018-03-11 17:52:26

--- ruby/556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb
+++ ruby/556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb
@@ -1,172 +1,83 @@
 
         
-              should 'be processed and written to destination when passed as '\
-        'an entry in 'site.pages' array' do
-        @page.content = '{{ site.title }}'
-        @page.data['permalink'] = '/virtual-about/'
-    
-        # Check if excerpt includes a string
-    #
-    # Returns true if the string passed in
-    def include?(something)
-      (output && output.include?(something)) || content.include?(something)
-    end
-    
-          def load_all_devices
-        self.devices = []
-    
-          def task_available?(task)
-        load_all_tasks
-        return tasks.collect(&:title).include?(task)
-      end
-    
-            expect(result).to eq('/usr/local/bin/cloc  --by-file --xml  --out=/tmp/cloc.xml')
-      end
-    
-            expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to match(/cd .* && agvtool new-marketing-version 1.77.3/)
-      end
-    
-        validate :validate_evernote_options
-    
-        if params[:sort].present?
-      attribute, direction = params[:sort].downcase.split('.')
-      unless valid_sorts.include?(attribute)
-        attribute, direction = default.to_a.first
-      end
-    else
-      attribute, direction = default.to_a.first
-    end
-    
-        respond_to do |format|
-      format.html
-      format.json {
-        send_data Utils.pretty_jsonify(@user_credentials.limit(nil).as_json), disposition: 'attachment'
-      }
-    end
+          def remote_url
+    object.remote_url.presence
   end
     
-        def as_json
-      { entries: entries_as_json, types: types_as_json }
-    end
-    
-        def initialize(store, docs)
-      @store = store
-      @docs = docs
-    end
-    
-        private
-    
-          base_dir = Pathname.new(normalized_path)
-      base_dir = base_dir.parent unless path.end_with? '/'
-    
-      # Open3.pipeline_rw starts a list of commands as a pipeline with pipes
-  # which connect to stdin of the first command and stdout of the last command.
-  #
-  #   Open3.pipeline_rw(cmd1, cmd2, ... [, opts]) {|first_stdin, last_stdout, wait_threads|
-  #     ...
-  #   }
-  #
-  #   first_stdin, last_stdout, wait_threads = Open3.pipeline_rw(cmd1, cmd2, ... [, opts])
-  #   ...
-  #   first_stdin.close
-  #   last_stdout.close
-  #
-  # Each cmd is a string or an array.
-  # If it is an array, the elements are passed to Process.spawn.
-  #
-  #   cmd:
-  #     commandline                              command line string which is passed to a shell
-  #     [env, commandline, opts]                 command line string which is passed to a shell
-  #     [env, cmdname, arg1, ..., opts]          command name and one or more arguments (no shell)
-  #     [env, [cmdname, argv0], arg1, ..., opts] command name and arguments including argv[0] (no shell)
-  #
-  #   Note that env and opts are optional, as for Process.spawn.
-  #
-  # The options to pass to Process.spawn are constructed by merging
-  # +opts+, the last hash element of the array, and
-  # specifications for the pipes between each of the commands.
-  #
-  # Example:
-  #
-  #   Open3.pipeline_rw('tr -dc A-Za-z', 'wc -c') {|i, o, ts|
-  #     i.puts 'All persons more than a mile high to leave the court.'
-  #     i.close
-  #     p o.gets #=> '42\n'
-  #   }
-  #
-  #   Open3.pipeline_rw('sort', 'cat -n') {|stdin, stdout, wait_thrs|
-  #     stdin.puts 'foo'
-  #     stdin.puts 'bar'
-  #     stdin.puts 'baz'
-  #     stdin.close     # send EOF to sort.
-  #     p stdout.read   #=> '     1\tbar\n     2\tbaz\n     3\tfoo\n'
-  #   }
-  def pipeline_rw(*cmds, **opts, &block)
-    in_r, in_w = IO.pipe
-    opts[:in] = in_r
-    in_w.sync = true
-    
-      def test_pipeline_rw
-    Open3.pipeline_rw([RUBY, '-e', 'print STDIN.read + '1''],
-                      [RUBY, '-e', 'print STDIN.read + '2'']) {|i,o,ts|
-      assert_kind_of(IO, i)
-      assert_kind_of(IO, o)
-      assert_kind_of(Array, ts)
-      assert_equal(2, ts.length)
-      ts.each {|t| assert_kind_of(Thread, t) }
-      i.print '0'
-      i.close
-      assert_equal('012', o.read)
-      ts.each {|t|
-        assert(t.value.success?)
-      }
-    }
-  end
-    
-            private
-        def update_content
-          components = [@hour, @minute, @second]
-          @content =
-            ::RSS::ITunesItemModel::ITunesDuration.construct(*components)
-        end
+          it 'renders application layout' do
+        get :show, params: { id: 'test', max_id: late.id }
+        expect(response).to render_template layout: 'application'
       end
     end
     
-    #
-# Project
+      def type
+    'Emoji'
+  end
+    
+        File.write(dest, Oj.dump(map))
+    puts 'Wrote emojo to destination! (#{dest})'
+  end
+end
+
+    
+      describe 'for a suspension' do
+    before do
+      subject.call(DomainBlock.create!(domain: 'evil.org', severity: :suspend))
+    end
+    
+          output = input_path if @options[:in_place]
+      write_output(out, output)
+    rescue Sass::SyntaxError => e
+      raise e if @options[:trace]
+      file = ' of #{e.sass_filename}' if e.sass_filename
+      raise 'Error on line #{e.sass_line}#{file}: #{e.message}\n  Use --trace for backtrace'
+    rescue LoadError => err
+      handle_load_error(err)
+    end
+    
+    # A logger that delays messages until they're explicitly flushed to an inner
+# logger.
 #
+# This can be installed around the current logger by calling \{#install!}, and
+# the original logger can be replaced by calling \{#uninstall!}. The log
+# messages can be flushed by calling \{#flush}.
+class Sass::Logger::Delayed < Sass::Logger::Base
+  # Installs a new delayed logger as the current Sass logger, wrapping the
+  # original logger.
+  #
+  # This can be undone by calling \{#uninstall!}.
+  #
+  # @return [Sass::Logger::Delayed] The newly-created logger.
+  def self.install!
+    logger = Sass::Logger::Delayed.new(Sass.logger)
+    Sass.logger = logger
+    logger
+  end
     
-    signer._invoke('JarSignerMSF','[Ljava.lang.String;',jarsignerOpts)
-    
-        _cal[ver].each_pair do |key, value|
-      cal[ver][key] = Array.new
-      cal[ver][key] << String.new
-      cal[ver][key][-1] << '#ifdef AIX%s' % ver.delete('.')
-      cal[ver][key][-1] << '\n'
-      cal[ver][key][-1] << '''.rjust(5)
-      value.each_byte do |c|
-        cal[ver][key][-1] << '\x%02x' % c
-      end
-      cal[ver][key][-1] << '''.ljust(7)
-      cal[ver][key][-1] << '/*  cal     r2,-%d(r29)' %
-          (65536 - value.unpack('nn')[1])
-      cal[ver][key][-1] << '*/'.rjust(15)
-      cal[ver][key][-1] << '\n'
-      cal[ver][key][-1] << '#endif'
-      cal[ver][key][-1] << '\n'
+        def self.definitions_for(klass)
+      instance.definitions_for(klass)
     end
     
-    	def parse_file(file)
-		while (line = file.gets)
-			parse_line(line)
-		end
-    
-        def type_from_mime_magic
-      @type_from_mime_magic ||= File.open(@filepath) do |file|
-        MimeMagic.by_magic(file).try(:type)
+        def calculated_type_matches
+      possible_types.select do |content_type|
+        content_type == type_from_file_contents
       end
     end
     
-            def description
-          'require presence of attachment #{@attachment_name}'
+        Hash.new.tap do |missing_styles|
+      current_styles.each do |klass, attachment_definitions|
+        attachment_definitions.each do |attachment_name, styles|
+          registered = registered_styles[klass][attachment_name] || [] rescue []
+          missed = styles - registered
+          if missed.present?
+            klass_sym = klass.to_s.to_sym
+            missing_styles[klass_sym] ||= Hash.new
+            missing_styles[klass_sym][attachment_name.to_sym] ||= Array.new
+            missing_styles[klass_sym][attachment_name.to_sym].concat(missed.to_a)
+            missing_styles[klass_sym][attachment_name.to_sym].map!(&:to_s).sort!.map!(&:to_sym).uniq!
+          end
         end
+      end
+    end
+  end
+end
