d8c5f5c4e921f1d5d46c305217520640e5ee5c29
SUNsung <weatherius@gmail.com>
2018-03-11 02:34:31 +0200

    Rewrite [28oTaC7EIdhAc28zrpmgHET6qM285OupmIO3iXc280Cc.py] File in: 2018-03-11 02:34:31

--- pyton/28oTaC7EIdhAc28zrpmgHET6qM285OupmIO3iXc280Cc.py
+++ pyton/28oTaC7EIdhAc28zrpmgHET6qM285OupmIO3iXc280Cc.py
@@ -1,47 +1,148 @@
 
         
-        
-if __name__ == '__main__':
-    cli()
-
+        # TODO: response is the only one
     
-        # set up the templating system
-    config['pylons.app_globals'].mako_lookup = TemplateLookup(
-        directories=paths['templates'],
-        error_handler=handle_mako_error,
-        module_directory=module_directory,
-        input_encoding='utf-8',
-        default_filters=['conditional_websafe'],
-        filesystem_checks=getattr(g, 'reload_templates', False),
-        imports=[
-            'from r2.lib.filters import websafe, unsafe, conditional_websafe',
-            'from pylons import request',
-            'from pylons import tmpl_context as c',
-            'from pylons import app_globals as g',
-            'from pylons.i18n import _, ungettext',
-        ],
-        modulename_callable=mako_module_path,
+        :param url: URL for the new :class:`Request` object.
+    :param data: (optional) Dictionary (will be form-encoded), bytes, or file-like object to send in the body of the :class:`Request`.
+    :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    '''
+    
+    
+def to_native_string(string, encoding='ascii'):
+    '''Given a string object, regardless of type, returns a representation of
+    that string in the native string type, encoding and decoding where
+    necessary. This assumes ASCII unless told otherwise.
+    '''
+    if isinstance(string, builtin_str):
+        out = string
+    else:
+        if is_py2:
+            out = string.encode(encoding)
+        else:
+            out = string.decode(encoding)
+    
+            for field, val in fields:
+            if isinstance(val, basestring) or not hasattr(val, '__iter__'):
+                val = [val]
+            for v in val:
+                if v is not None:
+                    # Don't call str() on bytestrings: in Py3 it all goes wrong.
+                    if not isinstance(v, bytes):
+                        v = str(v)
+    
+        def __len__(self):
+        return len(self._store)
+    
+        @pytest.fixture(autouse=True)
+    def setup(self):
+        '''LookupDict instance with 'bad_gateway' attribute.'''
+        self.lookup_dict = LookupDict('test')
+        self.lookup_dict.bad_gateway = 502
+    
+    if is_py2:
+    from urllib import (
+        quote, unquote, quote_plus, unquote_plus, urlencode, getproxies,
+        proxy_bypass, proxy_bypass_environment, getproxies_environment)
+    from urlparse import urlparse, urlunparse, urljoin, urlsplit, urldefrag
+    from urllib2 import parse_http_list
+    import cookielib
+    from Cookie import Morsel
+    from StringIO import StringIO
+    
+    with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+with open('HISTORY.rst', 'r', 'utf-8') as f:
+    history = f.read()
+    
+            security.merge_perm(sm, 'datasource_access', gamma_ds.perm)
+        security.merge_perm(sm, 'datasource_access', no_gamma_ds.perm)
+    
+    
+def cleanup_permissions():
+    # 1. Clean up duplicates.
+    pvms = sm.get_session.query(sm.permissionview_model).all()
+    print('# of permission view menues is: {}'.format(len(pvms)))
+    pvms_dict = defaultdict(list)
+    for pvm in pvms:
+        pvms_dict[(pvm.permission, pvm.view_menu)].append(pvm)
+    duplicates = [v for v in pvms_dict.values() if len(v) > 1]
+    len(duplicates)
+    
+        @classmethod
+    def get_datasource(cls, datasource_type, datasource_id, session):
+        return (
+            session.query(cls.sources[datasource_type])
+            .filter_by(id=datasource_id)
+            .first()
+        )
+    
+    try:
+    import queue
+except ImportError:
+    import Queue as queue
+    
+            If the future has been cancelled (cancel() was called and returned
+        True) then any threads waiting on the future completing (though calls
+        to as_completed() or wait()) are notified and False is returned.
+    
+    def main():
+    for name, fn in [('sequential',
+                      functools.partial(download_urls_sequential, URLS)),
+                     ('processes',
+                      functools.partial(download_urls_with_executor,
+                                        URLS,
+                                        ProcessPoolExecutor(10))),
+                     ('threads',
+                      functools.partial(download_urls_with_executor,
+                                        URLS,
+                                        ThreadPoolExecutor(10)))]:
+        sys.stdout.write('%s: ' % name.ljust(12))
+        start = time.time()
+        url_map = fn()
+        sys.stdout.write('%.2f seconds (%d of %d downloaded)\n' %
+                         (time.time() - start, len(url_map), len(URLS)))
+    
+    
+# -- Options for HTML output ---------------------------------------------------
+    
+    
+class ProcessPoolWaitTests(ProcessPoolMixin, WaitTests):
+    pass
+    
+    
+  def Response( self ):
+    return self._response
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    
+  def SubsetForTypes( self, filetypes ):
+    '''Return a sub-filter limited to the given filetypes'''
+    # NOTE: actually, this class is already filtered
+    return self
+    
+    
+def FormatDebugInfoResponse_ExtraConfFoundButNotLoaded_test():
+  response = deepcopy( GENERIC_RESPONSE )
+  response[ 'extra_conf' ].update( {
+    'is_loaded': False,
+    'path': '/path/to/extra/conf'
+  } )
+  assert_that(
+    FormatDebugInfoResponse( response ),
+    contains_string(
+      'Extra configuration file found but not loaded\n'
+      'Extra configuration path: /path/to/extra/conf\n'
     )
-    
-            '''
-        try:
-            return getattr(stacked_proxy, key)
-        except TypeError:
-            return default
-    
-        def register_hooks(self):
-        if self._hooks_registered:
-            return
-    
-        @validate(buttontype = VInt('t', 1, 5))
-    def GET_button_embed(self, buttontype):
-        if not buttontype:
-            abort(404)
+  )
     
     
-class GoogleTagManagerController(MinimalController):
-    def pre(self):
-        if request.host != g.media_domain:
-            # don't serve up untrusted content except on our
-            # specifically untrusted domain
-            self.abort404()
+def CommaSeparatedFiletypes_test():
