36c7673b48cf6588e9c6d1c72025608bb9fef770
SUNsung <weatherius@gmail.com>
2018-03-11 14:10:57 +0200

    Rewrite [36E7R9v1X2FA36BlRkmecpPg36kRThnN2EjkQ36v1S.cpp] File in: 2018-03-11 14:10:56

--- cccccc/36E7R9v1X2FA36BlRkmecpPg36kRThnN2EjkQ36v1S.cpp
+++ cccccc/36E7R9v1X2FA36BlRkmecpPg36kRThnN2EjkQ36v1S.cpp
@@ -1,86 +1,246 @@
-DEFINE_string(tag, '', 'Optional tag for the test');
+
+        
+          int AddEntry(const base::DictionaryValue& entry);
+  void RemoveEntry(int id);
     
-      // grpc server config.
-  std::ostringstream s;
-  int p = grpc_pick_unused_port_or_die();
-  s << '[::1]:' << p;
-  const string address = s.str();
-  testing::EchoTestService::AsyncService service;
-  ServerBuilder builder;
-  builder.AddListeningPort(address, InsecureServerCredentials());
-  auto cq = builder.AddCompletionQueue();
-  builder.RegisterService(&service);
-  auto server = builder.BuildAndStart();
+     private:
+  // Must be called on IO thread.
+  void DoCancelAuth();
+  void DoLogin(const base::string16& username, const base::string16& password);
     
-    #include 'src/proto/grpc/testing/control.pb.h'
-#include 'test/cpp/qps/histogram.h'
+    // Parse a string as an accelerator.
+bool StringToAccelerator(const std::string& description,
+                         ui::Accelerator* accelerator);
     
-    int main(int argc, char** argv) {
-  RunServer();
-    }
-    
-    
-    {  ServerBuilder builder;
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  builder.RegisterService(&service);
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << 'Server listening on ' << server_address << std::endl;
-  server->Wait();
+    static int make_prefixes(struct ifaddrs* ifaddr, int family, int prefixlen) {
+	char* prefix = NULL;
+	if (family == AF_INET) {
+		sockaddr_in* mask = new sockaddr_in;
+		mask->sin_family = AF_INET;
+		memset(&mask->sin_addr, 0, sizeof(in_addr));
+		ifaddr->ifa_netmask = reinterpret_cast<sockaddr*>(mask);
+		if (prefixlen > 32) {
+			prefixlen = 32;
+		}
+		prefix = reinterpret_cast<char*>(&mask->sin_addr);
+	} else if (family == AF_INET6) {
+		sockaddr_in6* mask = new sockaddr_in6;
+		mask->sin6_family = AF_INET6;
+		memset(&mask->sin6_addr, 0, sizeof(in6_addr));
+		ifaddr->ifa_netmask = reinterpret_cast<sockaddr*>(mask);
+		if (prefixlen > 128) {
+			prefixlen = 128;
+		}
+		prefix = reinterpret_cast<char*>(&mask->sin6_addr);
+	} else {
+		return -1;
+	}
+	for (int i = 0; i < (prefixlen / 8); i++) {
+		*prefix++ = 0xFF;
+	}
+	char remainder = 0xff;
+	remainder <<= (8 - prefixlen % 8);
+	*prefix = remainder;
+	return 0;
 }
     
-    #include <iostream>
+         60,30,500,   1,18.,  128
+  },
+  /* 5: 128 x 17 */
+  {
+    6,{0,1,1,2,3,3},{2,3,3,3},{0,1,2,2},{-1,0,1,2},
+    {{3},{4,5},{-1,6,7,8},{-1,9,10,11}},
+    2,{0,128,  12,46,  4,8,16,  23,33,70,  2,6,10,  14,19,28,  39,58,90},
+    
+        {&_44p1_p2_0,&_44p1_p2_1,0},
+    {&_44p1_p3_0,&_44p1_p3_1,0},
+    {&_44p1_p4_0,&_44p1_p4_1,0},
+    
+    #define todB(x)   (*(x)==0?-400.f:log(*(x)**(x))*4.34294480f)
+#define todB_nn(x)   (*(x)==0.f?-400.f:log(*(x))*8.6858896f)
+    
+    /** 16x32 multiplication, followed by a 16-bit shift right. Results fits in 32 bits */
+#undef MULT16_32_Q16
+static OPUS_INLINE opus_val32 MULT16_32_Q16_armv4(opus_val16 a, opus_val32 b)
+{
+  unsigned rd_lo;
+  int rd_hi;
+  __asm__(
+      '#MULT16_32_Q16\n\t'
+      'smull %0, %1, %2, %3\n\t'
+      : '=&r'(rd_lo), '=&r'(rd_hi)
+      : '%r'(b),'r'(a<<16)
+  );
+  return rd_hi;
+}
+#define MULT16_32_Q16(a, b) (MULT16_32_Q16_armv4(a, b))
+    
+    #elif (defined(_MSC_VER) && _MSC_VER >= 1400) && defined (_M_X64)
+        #include <xmmintrin.h>
+    
+      // Make list of keys from flattened key structure
+  start_.push_back(keys_.size());  // Simplify length computation
+  tmp_keys_.resize(num_keys);
+  for (size_t i = 0; i < num_keys; i++) {
+    const char* base = keys_.data() + start_[i];
+    size_t length = start_[i+1] - start_[i];
+    tmp_keys_[i] = Slice(base, length);
+  }
+    
+    // Snapshots are kept in a doubly-linked list in the DB.
+// Each SnapshotImpl corresponds to a particular sequence number.
+class SnapshotImpl : public Snapshot {
+ public:
+  SequenceNumber number_;  // const after creation
+    }
+    
+      while (msg == NULL && GetVarint32(&input, &tag)) {
+    switch (tag) {
+      case kComparator:
+        if (GetLengthPrefixedSlice(&input, &str)) {
+          comparator_ = str.ToString();
+          has_comparator_ = true;
+        } else {
+          msg = 'comparator name';
+        }
+        break;
+    }
+    }
+    
+      void SetComparatorName(const Slice& name) {
+    has_comparator_ = true;
+    comparator_ = name.ToString();
+  }
+  void SetLogNumber(uint64_t num) {
+    has_log_number_ = true;
+    log_number_ = num;
+  }
+  void SetPrevLogNumber(uint64_t num) {
+    has_prev_log_number_ = true;
+    prev_log_number_ = num;
+  }
+  void SetNextFile(uint64_t num) {
+    has_next_file_number_ = true;
+    next_file_number_ = num;
+  }
+  void SetLastSequence(SequenceNumber seq) {
+    has_last_sequence_ = true;
+    last_sequence_ = seq;
+  }
+  void SetCompactPointer(int level, const InternalKey& key) {
+    compact_pointers_.push_back(std::make_pair(level, key));
+  }
+    
+    #include 'db/dbformat.h'
+#include 'leveldb/write_batch.h'
+    
+        const char* benchmarks = FLAGS_benchmarks;
+    while (benchmarks != NULL) {
+      const char* sep = strchr(benchmarks, ',');
+      Slice name;
+      if (sep == NULL) {
+        name = benchmarks;
+        benchmarks = NULL;
+      } else {
+        name = Slice(benchmarks, sep - benchmarks);
+        benchmarks = sep + 1;
+      }
+    }
+    
+      // Check that the directory is empty.
+  ASSERT_TRUE(!env_->FileExists('/dir/non_existent'));
+  ASSERT_TRUE(!env_->GetFileSize('/dir/non_existent', &file_size).ok());
+  ASSERT_OK(env_->GetChildren('/dir', &children));
+  ASSERT_EQ(0, children.size());
+    
+    Iterator* Block::NewIterator(const Comparator* cmp) {
+  if (size_ < sizeof(uint32_t)) {
+    return NewErrorIterator(Status::Corruption('bad block contents'));
+  }
+  const uint32_t num_restarts = NumRestarts();
+  if (num_restarts == 0) {
+    return NewEmptyIterator();
+  } else {
+    return new Iter(cmp, data_, restart_offset_, num_restarts);
+  }
+}
+    
+    
+    {}  // namespace leveldb
+
+    
+     private:
+  void Update() {
+    valid_ = iter_->Valid();
+    if (valid_) {
+      key_ = iter_->key();
+    }
+  }
+    
+    
+    {    current_->Next();
+    FindSmallest();
+  }
+    
+    #include <memory>
 #include <vector>
     
+    #ifndef GRPC_INTERNAL_COMPILER_SCHEMA_INTERFACE_H
+#define GRPC_INTERNAL_COMPILER_SCHEMA_INTERFACE_H
     
+      // Ensure that a type is prefixed with its namespace whenever it is used
+  // outside of its namespace.
+  std::string WrapInNameSpace(const Namespace *ns,
+                              const std::string &name) const;
     
-        CC_INCREMENT_GL_DRAWN_BATCHES_AND_VERTICES(1,vertexCount);
+      explicit FlatCompiler(const InitParams &params) : params_(params) {}
     
-    		{
-			b2PolygonShape shape;
-			shape.SetAsBox(0.5f, 0.5f);
+    // This is an example of parsing text straight into a buffer and then
+// generating flatbuffer (JSON) text from the buffer.
+int main(int /*argc*/, const char * /*argv*/ []) {
+  // load FlatBuffer schema (.fbs) and JSON from disk
+  std::string schemafile;
+  std::string jsonfile;
+  bool ok = flatbuffers::LoadFile('samples/monster.fbs', false, &schemafile) &&
+            flatbuffers::LoadFile('samples/monsterdata.json', false, &jsonfile);
+  if (!ok) {
+    printf('couldn't load files!\n');
+    return 1;
+  }
     }
     
-    		// Car
-		{
-			b2PolygonShape chassis;
-			b2Vec2 vertices[8];
-			vertices[0].Set(-1.5f, -0.5f);
-			vertices[1].Set(1.5f, -0.5f);
-			vertices[2].Set(1.5f, 0.0f);
-			vertices[3].Set(0.0f, 0.9f);
-			vertices[4].Set(-1.15f, 0.9f);
-			vertices[5].Set(-1.5f, 0.2f);
-			chassis.Set(vertices, 6);
-    }
+    #include 'flatbuffers/flatc.h'
     
-        char message[1024];
-    
-    
-    {    return context;
+    // Mutate the value of a struct's scalar.
+static void MutateScalarFieldOfStruct(const StructDef &struct_def,
+                                      const FieldDef &field,
+                                      std::string *code_ptr) {
+  std::string &code = *code_ptr;
+  std::string type = MakeCamel(GenTypeBasic(field.value.type));
+  std::string setter = 'rcv._tab.Mutate' + type;
+  GenReceiver(struct_def, code_ptr);
+  code += ' Mutate' + MakeCamel(field.name);
+  code += '(n ' + TypeName(field) + ') bool {\n\treturn ' + setter;
+  code += '(rcv._tab.Pos+flatbuffers.UOffsetT(';
+  code += NumToString(field.value.offset) + '), n)\n}\n\n';
 }
     
-    
-    {    if (addr.ss_family == AF_INET) {
-        sockaddr_in *ipv4 = (sockaddr_in *) &addr;
-        inet_ntop(AF_INET, &ipv4->sin_addr, buf, sizeof(buf));
-        return {ntohs(ipv4->sin_port), buf, 'IPv4'};
-    } else {
-        sockaddr_in6 *ipv6 = (sockaddr_in6 *) &addr;
-        inet_ntop(AF_INET6, &ipv6->sin6_addr, buf, sizeof(buf));
-        return {ntohs(ipv6->sin6_port), buf, 'IPv6'};
-    }
+    bool LoadFileRaw(const char *name, bool binary, std::string *buf) {
+  if (DirExists(name)) return false;
+  std::ifstream ifs(name, binary ? std::ifstream::binary : std::ifstream::in);
+  if (!ifs.is_open()) return false;
+  if (binary) {
+    // The fastest way to read a file into a string.
+    ifs.seekg(0, std::ios::end);
+    auto size = ifs.tellg();
+    (*buf).resize(static_cast<size_t>(size));
+    ifs.seekg(0, std::ios::beg);
+    ifs.read(&(*buf)[0], (*buf).size());
+  } else {
+    // This is slower, but works correctly on all platforms for text files.
+    std::ostringstream oss;
+    oss << ifs.rdbuf();
+    *buf = oss.str();
+  }
+  return !ifs.bad();
 }
-    
-        bool write(Queue::Message *message, bool &wasTransferred) {
-        ssize_t sent = 0;
-        if (messageQueue.empty()) {
-    }
-    }
-    
-    #include <string>
-    
-    
-    {    static Group<isServer> *from(uS::Socket *s) {
-        return static_cast<Group<isServer> *>(s->getNodeData());
-    }
-};
