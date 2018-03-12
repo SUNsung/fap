2926d33185e14136c2f72e46c3e2f81ad36f29a1
SUNsung <weatherius@gmail.com>
2018-03-12 09:46:29 +0200

    Rewrite [35.7EyvTrymmo35ALpQz4ed5gM35.e2rv4fCm.w35XER.rb] File in: 2018-03-12 09:46:28

--- ruby/35.7EyvTrymmo35ALpQz4ed5gM35.e2rv4fCm.w35XER.rb
+++ ruby/35.7EyvTrymmo35ALpQz4ed5gM35.e2rv4fCm.w35XER.rb
@@ -1,59 +1,144 @@
 
         
-            html_filters.push 'clean_local_urls'
+              # Returns an escaped version of +html+ without affecting existing escaped entities.
+      #
+      #   escape_once('1 < 2 &amp; 3')
+      #   # => '1 &lt; 2 &amp; 3'
+      #
+      #   escape_once('&lt;&lt; Accept & Checkout')
+      #   # => '&lt;&lt; Accept &amp; Checkout'
+      def escape_once(html)
+        ERB::Util.html_escape_once(html)
+      end
     
-        def self.join(*args)
-      PARSER.join(*args)
-    end
-    
-              # Built-in events
-          if node['id'] == 'Events-catalog'
-            node.next_element.css('li').each do |li|
-              name = '#{li.at_css('b').content.delete(''').strip} event'
-              id = name.parameterize
-              li['id'] = id
-              entries << [name, id, type] unless name == entries.last[0]
-            end
-            next
-          end
-    
-    module Mastodon::Snowflake
-  DEFAULT_REGEX = /timestamp_id\('(?<seq_prefix>\w+)'/
-    
-      if Rails.env == 'development'
-    test_conf = ActiveRecord::Base.configurations['test']
-    
-            expect_any_instance_of(ActivityPub::LinkedDataSignature).to receive(:verify_account!).and_return(actor)
-        expect(ActivityPub::Activity).to receive(:factory).with(instance_of(Hash), actor, instance_of(Hash))
-    
-        def type
-      'Hashtag'
-    end
-    
-          expect(response).to redirect_to(settings_notifications_path)
-      user.reload
-      expect(user.settings['notification_emails']['follow']).to be true
-      expect(user.settings['interactions']['must_be_follower']).to be false
-    end
+      test 'response status aliases deprecated' do
+    response = ActionDispatch::TestResponse.create
+    assert_deprecated { response.success? }
+    assert_deprecated { response.missing? }
+    assert_deprecated { response.error? }
   end
 end
 
     
-        existence_maps.each do |group|
-      existing_one = group.key(true)
+    module ActiveSupport
+  # Abstract super class that provides a thread-isolated attributes singleton, which resets automatically
+  # before and after each request. This allows you to keep all the per-request attributes easily
+  # available to the whole system.
+  #
+  # The following full app-like example demonstrates how to use a Current class to
+  # facilitate easy access to the global, per-request attributes without passing them deeply
+  # around everywhere:
+  #
+  #   # app/models/current.rb
+  #   class Current < ActiveSupport::CurrentAttributes
+  #     attribute :account, :user
+  #     attribute :request_id, :user_agent, :ip_address
+  #
+  #     resets { Time.zone = nil }
+  #
+  #     def user=(user)
+  #       super
+  #       self.account = user.account
+  #       Time.zone    = user.time_zone
+  #     end
+  #   end
+  #
+  #   # app/controllers/concerns/authentication.rb
+  #   module Authentication
+  #     extend ActiveSupport::Concern
+  #
+  #     included do
+  #       before_action :authenticate
+  #     end
+  #
+  #     private
+  #       def authenticate
+  #         if authenticated_user = User.find_by(id: cookies.encrypted[:user_id])
+  #           Current.user = authenticated_user
+  #         else
+  #           redirect_to new_session_url
+  #         end
+  #       end
+  #   end
+  #
+  #   # app/controllers/concerns/set_current_request_details.rb
+  #   module SetCurrentRequestDetails
+  #     extend ActiveSupport::Concern
+  #
+  #     included do
+  #       before_action do
+  #         Current.request_id = request.uuid
+  #         Current.user_agent = request.user_agent
+  #         Current.ip_address = request.ip
+  #       end
+  #     end
+  #   end
+  #
+  #   class ApplicationController < ActionController::Base
+  #     include Authentication
+  #     include SetCurrentRequestDetails
+  #   end
+  #
+  #   class MessagesController < ApplicationController
+  #     def create
+  #       Current.account.messages.create(message_params)
+  #     end
+  #   end
+  #
+  #   class Message < ApplicationRecord
+  #     belongs_to :creator, default: -> { Current.user }
+  #     after_create { |message| Event.create(record: message) }
+  #   end
+  #
+  #   class Event < ApplicationRecord
+  #     before_create do
+  #       self.request_id = Current.request_id
+  #       self.user_agent = Current.user_agent
+  #       self.ip_address = Current.ip_address
+  #     end
+  #   end
+  #
+  # A word of caution: It's easy to overdo a global singleton like Current and tangle your model as a result.
+  # Current should only be used for a few, top-level globals, like account, user, and request details.
+  # The attributes stuck in Current should be used by more or less all actions on all requests. If you start
+  # sticking controller-specific attributes in there, you're going to create a mess.
+  class CurrentAttributes
+    include ActiveSupport::Callbacks
+    define_callbacks :reset
     
-        self.sigs.each_key do |k|
-      # There is only one pattern per run to test
-      matched = nil
-      matches = nil
+      AUTH_HEADERS.each do |header|
+    test 'successful authentication with #{header.downcase}' do
+      @request.env[header] = encode_credentials('lifo')
+      get :index
     
-    sock = TCPSocket.new(ip, port)
+          debug { event.payload[:mail] }
+    end
     
+      def evernote_client
+    EvernoteOAuth::Client.new(
+      token:           evernote_oauth_token,
+      consumer_key:    evernote_consumer_key,
+      consumer_secret: evernote_consumer_secret,
+      sandbox:         use_sandbox?
+    )
+  end
     
-
+      def tumblr_consumer_key
+    ENV['TUMBLR_OAUTH_KEY']
+  end
     
-    		# End the last block
-		unless block_size == 0
-			block_end
-		end
-	end
+      included do
+    helper SortableTableHelper
+  end
+    
+          respond_to do |format|
+        if new_credentials.map(&:save).all?
+          format.html { redirect_to user_credentials_path, notice: 'The file was successfully uploaded.'}
+        else
+          format.html { redirect_to user_credentials_path, notice: 'One or more of the uploaded credentials was not imported due to an error. Perhaps an existing credential had the same name?'}
+        end
+      end
+    else
+      redirect_to user_credentials_path, notice: 'No file was chosen to be uploaded.' 
+    end
+  end
