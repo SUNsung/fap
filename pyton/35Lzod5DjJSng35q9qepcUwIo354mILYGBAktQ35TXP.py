a72c252af44dde33313ac05c455675c774b777af
SUNsung <weatherius@gmail.com>
2018-03-12 16:56:18 +0200

    Rewrite [35Lzod5DjJSng35q9qepcUwIo354mILYGBAktQ35TXP.py] File in: 2018-03-12 16:56:17

--- pyton/35Lzod5DjJSng35q9qepcUwIo354mILYGBAktQ35TXP.py
+++ pyton/35Lzod5DjJSng35q9qepcUwIo354mILYGBAktQ35TXP.py
@@ -1,96 +1,84 @@
 
         
-            return len(cmd) > 1 and 'x' in cmd[1]
+            scikit_results = []
+    glmnet_results = []
+    n = 20
+    step = 500
+    n_features = 1000
+    n_informative = n_features / 10
+    n_test_samples = 1000
+    for i in range(1, n + 1):
+        print('==================')
+        print('Iteration %s of %s' % (i, n))
+        print('==================')
+    
+            # split the paragraph into fake smaller paragraphs to make the
+        # problem harder e.g. more similar to tweets
+        if lang in ('zh', 'ja'):
+        # FIXME: whitespace tokenizing does not work on chinese and japanese
+            continue
+        words = content.split()
+        n_groups = len(words) / n_words_per_short_text
+        if n_groups < 1:
+            continue
+        groups = np.array_split(words, n_groups)
     
     
-def variable_batch_size_comparison(data):
-    batch_sizes = [i.astype(int) for i in np.linspace(data.shape[0] // 10,
-                                                      data.shape[0], num=10)]
+if not os.path.exists(DATA_FOLDER):
     
-        class_name = info['fullname'].split('.')[0]
-    if type(class_name) != str:
-        # Python 2 only
-        class_name = class_name.encode('utf-8')
-    module = __import__(info['module'], fromlist=[class_name])
-    obj = attrgetter(info['fullname'])(module)
+    plt.figure(2)  # 'banana' shape
+plt.title('Outlier detection on a real data set (boston housing)')
+plt.scatter(X2[:, 0], X2[:, 1], color='black')
+plt.xlim((xx2.min(), xx2.max()))
+plt.ylim((yy2.min(), yy2.max()))
+plt.legend((legend2_values_list[0].collections[0],
+            legend2_values_list[1].collections[0],
+            legend2_values_list[2].collections[0]),
+           (legend2_keys_list[0], legend2_keys_list[1], legend2_keys_list[2]),
+           loc='upper center',
+           prop=matplotlib.font_manager.FontProperties(size=12))
+plt.ylabel('% lower status of the population')
+plt.xlabel('average number of rooms per dwelling')
     
-        output_file.close()
-
+    plt.title('Change of predicted probabilities after sigmoid calibration')
+plt.xlabel('Probability class 1')
+plt.ylabel('Probability class 2')
+plt.xlim(-0.05, 1.05)
+plt.ylim(-0.05, 1.05)
+plt.legend(loc='best')
     
-        def reducer(self, key, value):
-        '''Sum values for each key.
+    # Create different classifiers. The logistic regression cannot do
+# multiclass out of the box.
+classifiers = {'L1 logistic': LogisticRegression(C=C, penalty='l1'),
+               'L2 logistic (OvR)': LogisticRegression(C=C, penalty='l2'),
+               'Linear SVC': SVC(kernel='linear', C=C, probability=True,
+                                 random_state=0),
+               'L2 logistic (Multinomial)': LogisticRegression(
+                C=C, solver='lbfgs', multi_class='multinomial'),
+               'GPC': GaussianProcessClassifier(kernel)
+               }
     
-        def reducer(self, key, value):
-        '''Sum values for each key.
+    # Additional templates that should be rendered to pages, maps page names to
+# template names.
+#html_additional_pages = {}
     
-        def _find_available_spot(self, vehicle):
-        '''Find an available spot where vehicle can fit, or return None'''
-        pass
+    manager = Manager(app)
+manager.add_command('db', MigrateCommand)
     
-    
-def updatefig(*args):
-    global update
-    
-            if from_left_find_board_y and from_right_find_board_y:
-            break
-    
-    def main():
-    '''
-    主函数
-    '''
-    op = yes_or_no('请确保手机打开了 ADB 并连接了电脑，'
-                   '然后打开跳一跳并【开始游戏】后再用本程序，确定开始？')
-    if not op:
-        print('bye')
-        return
-    print('程序版本号：{}'.format(VERSION))
-    debug.dump_device_info()
-    screenshot.check_screenshot()
-    
-    # 模拟按压的起始点坐标，需要自动重复游戏请设置成“再来一局”的坐标
-swipe = config.get('swipe', {
-    'x1': 320,
-    'y1': 410,
-    'x2': 320,
-    'y2': 410
-    })
-VERSION = '1.1.4'
-c = wda.Client()
-s = c.session()
-    
-        def test_device_detail(self):
-        process = os.popen(self.adb_path + ' shell getprop ro.product.device')
-        output = process.read()
-        return output
-    
-        print(round(target_distance), round(jump_error_value), round(actual_distance), round(last_press_time))
-    ''''# 将结果采集进学习字典
-    if last_piece_x > 0 and last_press_time > 0:
-        ai.add_data(round(actual_distance, 2), round(last_press_time))
-        # print(round(actual_distance), round(last_press_time))'''
+        @property
+    def time_column_grains(self):
+        return {
+            'time_columns': self.dttm_cols,
+            'time_grains': [grain.name for grain in self.database.grains()],
+        }
     
     
-def find_piece_and_board(im):
-    '''
-    寻找关键坐标
-    '''
-    w, h = im.size
-    points = []  # 所有满足色素的点集合
-    piece_y_max = 0
-    board_x = 0
-    board_y = 0
-    scan_x_border = int(w / 8)  # 扫描棋子时的左右边界
-    scan_start_y = 0  # 扫描的起始 y 坐标
-    im_pixel = im.load()
-    # 以 50px 步长，尝试探测 scan_start_y
-    for i in range(int(h / 3), int(h * 2 / 3), 50):
-        last_pixel = im_pixel[0, i]
-        for j in range(1, w):
-            pixel = im_pixel[j, i]
-            # 不是纯色的线，则记录 scan_start_y 的值，准备跳出循环
-            if pixel != last_pixel:
-                scan_start_y = i - 50
-                break
-        if scan_start_y:
-            break
-    print('start scan Y axis: {}'.format(scan_start_y))
+MAIN = '''\
+import os
+import sys
+    
+        logging.warning('Starting fetch with curl client')
+    curl_client = CurlAsyncHTTPClient()
+    curl_client.fetch('http://localhost:%d/' % options.port,
+                      callback=callback)
+    IOLoop.current().start()
