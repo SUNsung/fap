bb59d1496f9175f6dfb1bca25ef3e1265061143e
SUNsung <weatherius@gmail.com>
2018-03-12 13:16:11 +0200

    Rewrite [22.V44YRlrDRo22FeyDZUWjuDk22De9zSbvqdwc22l9a.py] File in: 2018-03-12 13:16:11

--- pyton/22.V44YRlrDRo22FeyDZUWjuDk22De9zSbvqdwc22l9a.py
+++ pyton/22.V44YRlrDRo22FeyDZUWjuDk22De9zSbvqdwc22l9a.py
@@ -1,137 +1,40 @@
 
         
-            def iter_body(self, chunk_size=1):
-        return self._orig.iter_content(chunk_size=chunk_size)
-    
-            '''
-        raise NotImplementedError()
-    
-        name = 'Basic HTTP auth'
-    auth_type = 'basic'
-    
-        def _get_path(self):
-        '''Return the config file path without side-effects.'''
-        raise NotImplementedError()
-    
-        def get_formatters_grouped(self):
-        groups = {}
-        for group_name, group in groupby(
-                self.get_formatters(),
-                key=lambda p: getattr(p, 'group_name', 'format')):
-            groups[group_name] = list(group)
-        return groups
-    
-        # Redirection.
-    300: ('multiple_choices',),
-    301: ('moved_permanently', 'moved', '\\o-'),
-    302: ('found',),
-    303: ('see_other', 'other'),
-    304: ('not_modified',),
-    305: ('use_proxy',),
-    306: ('switch_proxy',),
-    307: ('temporary_redirect', 'temporary_moved', 'temporary'),
-    308: ('permanent_redirect',
-          'resume_incomplete', 'resume',),  # These 2 to be removed in 3.0
+        intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'werkzeug': ('http://werkzeug.pocoo.org/docs/', None),
+    'click': ('http://click.pocoo.org/', None),
+    'jinja': ('http://jinja.pocoo.org/docs/', None),
+    'itsdangerous': ('https://pythonhosted.org/itsdangerous', None),
+    'sqlalchemy': ('https://docs.sqlalchemy.org/en/latest/', None),
+    'wtforms': ('https://wtforms.readthedocs.io/en/latest/', None),
+    'blinker': ('https://pythonhosted.org/blinker/', None),
+}
     
     
-@pytest.mark.parametrize(
-    'uri, expected', (
-        (
-            # Illegal bytes
-            'http://example.com/?a=%--',
-            'http://example.com/?a=%--',
-        ),
-        (
-            # Reserved characters
-            'http://example.com/?a=%300',
-            'http://example.com/?a=00',
-        )
-    ))
-def test_unquote_unreserved(uri, expected):
-    assert unquote_unreserved(uri) == expected
-    
-            # Verify Authorization isn't sent to the redirected host,
-        # then send another challenge.
-        request_content = consume_socket_content(sock, timeout=0.5)
-        assert b'Authorization:' not in request_content
-        sock.send(text_401)
-    
-        def test_HTTP_307_ALLOW_REDIRECT_POST_WITH_SEEKABLE(self, httpbin):
-        byte_str = b'test'
-        r = requests.post(httpbin('redirect-to'), data=io.BytesIO(byte_str), params={'url': 'post', 'status_code': 307})
-        assert r.status_code == 200
-        assert r.history[0].status_code == 307
-        assert r.history[0].is_redirect
-        assert r.json()['data'] == byte_str.decode('utf-8')
-    
-    from codecs import open
+def get_db():
+    '''Opens a new database connection if there is none yet for the
+    current application context.
+    '''
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
     
     
-def SetUpYCM():
-  from ycm import base
-  from ycmd import user_options_store
-  from ycm.youcompleteme import YouCompleteMe
+@app.cli.command('initdb')
+def initdb_command():
+    '''Creates the database tables.'''
+    init_db()
+    print('Initialized the database.')
     
-        def _adjust_thread_count(self):
-        # When the executor gets lost, the weakref callback will wake up
-        # the worker threads.
-        def weakref_cb(_, q=self._work_queue):
-            q.put(None)
-        # TODO(bquinlan): Should avoid creating new threads if there are more
-        # idle threads than items in the work queue.
-        if len(self._threads) < self._max_workers:
-            t = threading.Thread(target=_worker,
-                                 args=(weakref.ref(self, weakref_cb),
-                                       self._work_queue))
-            t.daemon = True
-            t.start()
-            self._threads.add(t)
     
-    try:
-    import queue
-except ImportError:
-    import Queue as queue
+def login(client, username, password):
+    '''Helper function to login'''
+    return client.post('/login', data={
+        'username': username,
+        'password': password
+    }, follow_redirects=True)
     
-      return FilterRegex
     
-      _assert_rejects( f, { 'text' : 'This is an unimportant taco',
-                        'kind' : 'WARNING' } )
-  _assert_rejects( f, { 'text' : 'This taco will NOT be shown',
-                        'kind' : 'ERROR' } )
-  _assert_accepts( f, { 'text' : 'This burrito WILL be shown',
-                        'kind' : 'ERROR' } )
-    
-    plugins = PluginLoader()
-print 'PLUGINS := ' + ' '.join(plugin.name for plugin in plugins
-                               if plugin.needs_static_build)
-    
-    from pylons import tmpl_context as c
-    
-            if target.subreddit_slow.quarantine:
-            err = RedditError('GILDING_NOT_ALLOWED')
-            self.on_validation_error(err)
-        VNotInTimeout().run(target=target, subreddit=target.subreddit_slow)
-    
-    class ButtonsController(RedditController):
-    def get_wrapped_link(self, url, link = None, wrapper = None):
-        try:
-            links = []
-            if link:
-                links = [link]
-            else:
-                sr = None if isinstance(c.site, FakeSubreddit) else c.site
-                try:
-                    links = Link._by_url(url, sr)
-                except NotFound:
-                    pass
-    
-    from r2.controllers.reddit_base import RedditController
-from r2.lib.base import proxyurl
-from r2.lib.csrf import csrf_exempt
-from r2.lib.template_helpers import get_domain
-from r2.lib.pages import Embed, BoringPage, HelpPage
-from r2.lib.filters import websafe, SC_OFF, SC_ON
-from r2.lib.memoize import memoize
-    
-                if request.GET.has_key('allow_framing'):
-                c.allow_framing = bool(request.GET['allow_framing'] == '1')
+def naughty_strings(filepath=FILEPATH):
+    '''Get the list of naughty_strings.
