6cf9456668c3ab6de11b12802f0fd05723cec00f
SUNsung <weatherius@gmail.com>
2018-03-12 20:58:04 +0200

    Rewrite [41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb] File in: 2018-03-12 20:58:03

--- ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
+++ ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
@@ -1,118 +1,45 @@
 
         
-            include Behavior
-  end
-end
-
+          test 'should be active when confirmation sent at is not overpast' do
+    swap Devise, allow_unconfirmed_access_for: 5.days do
+      Devise.allow_unconfirmed_access_for = 5.days
+      user = create_user
     
-            # Expands each key in @configurations hash into fully resolved hash
-        def resolve_all
-          config = configurations.dup
+        # The path used after resending confirmation instructions.
+    def after_resending_confirmation_instructions_path_for(resource_name)
+      is_navigational_format? ? new_session_path(resource_name) : '/'
+    end
     
-          test 'after_action works with procs' do
-        @controller.process(:index)
-        assert_equal 'Goodbye', @controller.instance_variable_get('@second')
+          def expire_data_after_sign_out!
+        Devise.mappings.each { |_,m| instance_variable_set('@current_#{m.name}', nil) }
+        super
       end
     end
+  end
     
-      class BareTest < ActiveSupport::TestCase
-    test 'response body is a Rack-compatible response' do
-      status, headers, body = BareController.action(:index).call(Rack::MockRequest.env_for('/'))
-      assert_equal 200, status
-      string = ''.dup
-    
-    module ActionMailer
-  # The <tt>ActionMailer::DeliveryJob</tt> class is used when you
-  # want to send emails outside of the request-response cycle.
-  #
-  # Exceptions are rescued and handled by the mailer class.
-  class DeliveryJob < ActiveJob::Base # :nodoc:
-    queue_as { ActionMailer::Base.deliver_later_queue_name }
-    
-        keys.each do |key|
-      value = env[key]
-      s = '#{key}: #{value}'
-      case key
-      when 'CC', 'CXX', 'LD'
-        s << ' => #{Pathname.new(value).realpath}' if File.symlink?(value)
+    module Devise
+  module Controllers
+    # A module that may be optionally included in a controller in order
+    # to provide remember me behavior. Useful when signing in is done
+    # through a callback, like in OmniAuth.
+    module Rememberable
+      # Return default cookie values retrieved from session options.
+      def self.cookie_values
+        Rails.configuration.session_options.slice(:path, :domain, :secure)
       end
-      f.puts s
-    end
+    
+        include Rails.application.routes.url_helpers
+    include Rails.application.routes.mounted_helpers
+    
+          it 'processes payload with sender if no signature exists' do
+        expect_any_instance_of(ActivityPub::LinkedDataSignature).not_to receive(:verify_account!)
+        expect(ActivityPub::Activity).to receive(:factory).with(instance_of(Hash), forwarder, instance_of(Hash))
+    
+      def collection_presenter
+    ActivityPub::CollectionPresenter.new(
+      id: tag_url(@tag),
+      type: :ordered,
+      size: @tag.statuses.count,
+      items: @statuses.map { |s| ActivityPub::TagManager.instance.uri_for(s) }
+    )
   end
-end
-
-    
-      # True if a {Formula} is being built in 32-bit/x86 mode.
-  # This is needed for some use-cases though we prefer to build Universal
-  # when a 32-bit version is needed.
-  def build_32_bit?
-    include?('32-bit') && option_defined?('32-bit')
-  end
-    
-    unless invalids.empty?
-  puts '\n\nFailed links:'
-  invalids.each do |link|
-    puts '- #{link}'
-  end
-  puts 'Done with errors.'
-  exit(1)
-end
-    
-                # Register the handler if this is our first callback.
-            Signal.trap('INT') { fire_callbacks } if registered.length == 1
-          end
-        end
-    
-    ###
-### dependencies
-###
-    
-    
-  it 'should allow changing the nosniff-mode off' do
-    mock_app do
-      use Rack::Protection::XSSHeader, :nosniff => false
-      run DummyApp
-    end
-    
-            attr_reader :name, :declaration_node, :scope,
-                    :assignments, :references, :captured_by_block
-        alias captured_by_block? captured_by_block
-    
-    module RuboCop
-  module Cop
-    module Performance
-      # This cop identifies unnecessary use of a regex where `String#end_with?`
-      # would suffice.
-      #
-      # @example
-      #   # bad
-      #   'abc' =~ /bc\Z/
-      #   'abc'.match(/bc\Z/)
-      #
-      #   # good
-      #   'abc'.end_with?('bc')
-      class EndWith < Cop
-        MSG = 'Use `String#end_with?` instead of a regex match anchored to ' \
-              'the end of the string.'.freeze
-        SINGLE_QUOTE = '''.freeze
-    
-              new_source =
-            node.receiver.source + ' =~ ' + node.first_argument.source
-    
-    module RuboCop
-  module Cop
-    module Performance
-      # This cop identifies unnecessary use of a regex where
-      # `String#start_with?` would suffice.
-      #
-      # @example
-      #   # bad
-      #   'abc' =~ /\Aab/
-      #   'abc'.match(/\Aab/)
-      #
-      #   # good
-      #   'abc'.start_with?('ab')
-      class StartWith < Cop
-        MSG = 'Use `String#start_with?` instead of a regex match anchored to ' \
-              'the beginning of the string.'.freeze
-        SINGLE_QUOTE = '''.freeze
