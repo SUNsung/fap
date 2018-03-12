b7f9534f89a58b1b4a60a630b82800f8919e67b2
SUNsung <weatherius@gmail.com>
2018-03-12 08:10:30 +0200

    Rewrite [34PLB0sJVoLVQ34p1GX8l.ao.634ZsW9pDQdSQo34Qkh.cpp] File in: 2018-03-12 08:10:30

--- cccccc/34PLB0sJVoLVQ34p1GX8l.ao.634ZsW9pDQdSQo34Qkh.cpp
+++ cccccc/34PLB0sJVoLVQ34p1GX8l.ao.634ZsW9pDQdSQo34Qkh.cpp
@@ -1,224 +1,229 @@
 
         
-        class StatusPropagationTest : public ::testing::TestWithParam<StatusTestParam> {
- protected:
-  PartialRunMgr partial_run_mgr_;
-    }
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-      double PrimalLossDerivative(const double wx, const double label,
-                              const double example_weight) const final {
-    if (label * wx >= 1) {
-      return 0;
-    }
-    if (label * wx <= 1 - gamma) {
-      return -label;
-    }
-    return (wx - label) / gamma;
+        
+    {  /* Print the top N predictions. */
+  for (size_t i = 0; i < predictions.size(); ++i) {
+    Prediction p = predictions[i];
+    std::cout << std::fixed << std::setprecision(4) << p.second << ' - \''
+              << p.first << '\'' << std::endl;
   }
-    
-    #include 'third_party/eigen3/unsupported/Eigen/CXX11/Tensor'
-#include 'tensorflow/core/framework/tensor_types.h'
-#include 'tensorflow/core/framework/types.h'
-#include 'tensorflow/core/kernels/scatter_functor.h'
-    
-    bool SubProcess::Start() {
-  mutex_lock procLock(proc_mu_);
-  mutex_lock dataLock(data_mu_);
-  if (running_) {
-    LOG(ERROR) << 'Start called after the process was started.';
-    return false;
-  }
-  if ((exec_path_ == nullptr) || (exec_argv_ == nullptr)) {
-    LOG(ERROR) << 'Start called without setting a program.';
-    return false;
-  }
-    }
-    
-    #include 'tensorflow/stream_executor/cuda/cuda_driver.h'
-#include 'tensorflow/stream_executor/cuda/cuda_stream.h'
-#include 'tensorflow/stream_executor/event.h'
-#include 'tensorflow/stream_executor/lib/status.h'
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    
-    {} // namespace swift
-    
-    
-    {  const SILDebugScope *Scope = this;
-  while (Scope->Parent.is<const SILDebugScope *>())
-    Scope = Scope->Parent.get<const SILDebugScope *>();
-  assert(Scope->Parent.is<SILFunction *>() && 'orphaned scope');
-  return Scope->Parent.get<SILFunction *>();
 }
-    
-    
-    {} // end namespace swift
-    
-    
-    {} // end namespace swift
-    
-    
-    {  StringRef getReceiverUSR() const {
-    for (auto Relation: Relations) {
-      if (Relation.roles & (SymbolRoleSet) SymbolRole::RelationReceivedBy)
-        return Relation.USR;
-    }
-    return StringRef();
-  }
-};
-    
-    void cvValidateDisparity( CvArr* _disp, const CvArr* _cost, int minDisparity,
-                         int numberOfDisparities, int disp12MaxDiff )
-{
-    cv::Mat disp = cv::cvarrToMat(_disp), cost = cv::cvarrToMat(_cost);
-    cv::validateDisparity( disp, cost, minDisparity, numberOfDisparities, disp12MaxDiff );
+#else
+int main(int argc, char** argv) {
+  LOG(FATAL) << 'This example requires OpenCV; compile with USE_OPENCV.';
 }
+#endif  // USE_OPENCV
 
     
-    void dls::compute_eigenvec(const cv::Mat& Mtilde, cv::Mat& eigenval_real, cv::Mat& eigenval_imag,
-                                                  cv::Mat& eigenvec_real, cv::Mat& eigenvec_imag)
-{
-#ifdef HAVE_EIGEN
-    Eigen::MatrixXd Mtilde_eig, zeros_eig;
-    cv::cv2eigen(Mtilde, Mtilde_eig);
-    cv::cv2eigen(cv::Mat::zeros(27, 27, CV_64F), zeros_eig);
-    }
+    #include 'caffe/common.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
     
-    // fit second order curve to a set of 2D points
-inline void fitCurve2Order(const std::vector<Point2f>& /*points*/, std::vector<float>& /*curve*/)
-{
-    // TBD
-}
     
-        static void CODEGEN_FUNCPTR Switch_BlendEquationSeparate(GLenum modeRGB, GLenum modeAlpha)
-    {
-        BlendEquationSeparate = (PFNBLENDEQUATIONSEPARATEPROC)IntGetProcAddress('glBlendEquationSeparate');
-        BlendEquationSeparate(modeRGB, modeAlpha);
-    }
-    
-    #include 'precomp.hpp'
-    
-      switch (arr->tag()) {
-    case RepoAuthType::Array::Tag::Packed: {
-      for (uint32_t idx = 0; idx < arr->size(); ++idx) {
-        auto rat = arr->packedElem(idx);
-        assert(rat.resolved());
-        if (rat.mayHaveArrData()) check(rat.array());
-      }
-      break;
-    }
-    case RepoAuthType::Array::Tag::PackedN: {
-      auto rat = arr->elemType();
-      assert(rat.resolved());
-      if (rat.mayHaveArrData()) check(rat.array());
-      break;
-    }
+    { protected:
+  /**
+   * @param bottom input Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$
+   * @param top output Blob vector (length 1)
+   *   -# @f$ (N \times 1 \times K) @f$ or, if out_max_val
+   *      @f$ (N \times 2 \times K) @f$ unless axis set than e.g.
+   *      @f$ (N \times K \times H \times W) @f$ if axis == 1
+   *      the computed outputs @f$
+   *       y_n = \arg\max\limits_i x_{ni}
+   *      @f$ (for @f$ K = 1 @f$).
+   */
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  /// @brief Not implemented (non-differentiable function)
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+    NOT_IMPLEMENTED;
   }
+  bool out_max_val_;
+  size_t top_k_;
+  bool has_axis_;
+  int axis_;
+};
     
-    /////////////////////////////////////////////////////////////////////////////
-    
-      if (m_log_entry) {
-    m_log_entry->setInt(std::string(s_names[(size_t)m_name].str) + '_micros',
-                        elapsed / 1000);
+      /// @brief The spatial dimensions of the input.
+  inline int input_shape(int i) {
+    return (*bottom_shape_)[channel_axis_ + i];
   }
-    
-    
-    {  switch (t) {
-    case SystemCPU: return to_usec(ru.ru_stime);
-    case UserCPU:   return to_usec(ru.ru_utime);
-    case TotalCPU:  return to_usec(ru.ru_stime) + to_usec(ru.ru_utime);
-    default: always_assert(false);
-  }
-}
-    
-    
-    {  v << vcall{
-    CallSpec::direct(pthread_getspecific),
-    v.makeVcallArgs({{v.cns(datum.tls->m_key)}}),
-    v.makeTuple({d}),
-    Fixup{},
-    DestType::SSA
-  };
-}
-    
-    /**
- * Fast check for the most common form of the UTF-8 encoding identifier.
- */
-ALWAYS_INLINE
-static bool isUtf8(const Variant& encoding) {
-  return encoding.getStringDataOrNull() == s_utf_8.get();
-}
-    
-    
-static Variant HHVM_FUNCTION(gmp_strval,
-                             const Variant& data,
-                             const int64_t base /* = 10 */) {
-  mpz_t gmpData;
-    }
+  // reverse_dimensions should return true iff we are implementing deconv, so
+  // that conv helpers know which dimensions are which.
+  virtual bool reverse_dimensions() = 0;
+  // Compute height_out_ and width_out_ from other parameters.
+  virtual void compute_output_shape() = 0;
     
       /**
-   * Gracefully stop this web server. We will stop accepting new connections
-   * and finish ongoing requests without being interrupted in the middle of
-   * them. Note this is a non-blocking call and it will return immediately.
-   * At background, it will eventually make the thread calling start() quit.
+   * @brief Computes the error gradient w.r.t. the concatenate inputs.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *        respect to the outputs
+   *   -# @f$ (KN \times C \times H \times W) @f$ if axis == 0, or
+   *      @f$ (N \times KC \times H \times W) @f$ if axis == 1:
+   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
+   *      with respect to concatenated outputs @f$ y @f$
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length K), into which the top gradient
+   *        @f$ \frac{\partial E}{\partial y} @f$ is deconcatenated back to the
+   *        inputs @f$
+   *        \left[ \begin{array}{cccc}
+   *          \frac{\partial E}{\partial x_1} &
+   *          \frac{\partial E}{\partial x_2} &
+   *          ... &
+   *          \frac{\partial E}{\partial x_K}
+   *        \end{array} \right] =
+   *        \frac{\partial E}{\partial y}
+   *        @f$
    */
-  virtual void stop() = 0;
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
     
-    Variant HHVM_FUNCTION(posix_getlogin) {
-#if !defined(__APPLE__) && !defined(__FreeBSD__)
-  char buf[L_cuserid];
-#else
-  char buf[MAXLOGNAME];
-#endif
-  if (!getlogin_r(buf, sizeof(buf) - 1)) {
-    return String(buf, CopyString);
-  }
-  return false;
-}
     
-    namespace irgen {
+    { protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual inline bool reverse_dimensions() { return false; }
+  virtual void compute_output_shape();
+};
+    
+    
+    {}  // namespace caffe
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #endif  // CAFFE_CUDNN_SIGMOID_LAYER_HPP_
+
+    
+    /**
+ * @brief During training only, sets a random portion of @f$x@f$ to 0, adjusting
+ *        the rest of the vector magnitude accordingly.
+ *
+ * @param bottom input Blob vector (length 1)
+ *   -# @f$ (N \times C \times H \times W) @f$
+ *      the inputs @f$ x @f$
+ * @param top output Blob vector (length 1)
+ *   -# @f$ (N \times C \times H \times W) @f$
+ *      the computed outputs @f$ y = |x| @f$
+ */
+template <typename Dtype>
+class DropoutLayer : public NeuronLayer<Dtype> {
+ public:
+  /**
+   * @param param provides DropoutParameter dropout_param,
+   *     with DropoutLayer options:
+   *   - dropout_ratio (\b optional, default 0.5).
+   *     Sets the probability @f$ p @f$ that any given unit is dropped.
+   */
+  explicit DropoutLayer(const LayerParameter& param)
+      : NeuronLayer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
     }
     
-    namespace A2STR {
-    }
+      virtual inline const char* type() const { return 'Eltwise'; }
+  virtual inline int MinBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
     
-      std::unique_ptr<AuthConfig> getDefaultAuthConfig() const;
-    
-    #ifdef __MINGW32__
-  HANDLE fd_;
-  // The handle for memory mapped file. mmap equivalent in Windows.
-  HANDLE mapView_;
-#else  // !__MINGW32__
-  int fd_;
-#endif // !__MINGW32__
-    
-    class AbstractHttpServerResponseCommand : public Command {
-private:
-  DownloadEngine* e_;
-  std::shared_ptr<SocketCore> socket_;
-  std::shared_ptr<HttpServer> httpServer_;
-  Timer timeoutTimer_;
-  bool readCheck_;
-  bool writeCheck_;
-    }
-    
-    void AnnounceList::announceSuccess()
+    void DebugHUD_InitDefaults( DebugHUD *hud )
 {
-  if (currentTrackerInitialized_) {
-    (*currentTier_)->nextEvent();
-    auto url = *currentTracker_;
-    (*currentTier_)->urls.erase(currentTracker_);
-    (*currentTier_)->urls.push_front(std::move(url));
-    currentTier_ = std::begin(tiers_);
-    currentTracker_ = std::begin((*currentTier_)->urls);
-  }
+    hud->show_demo_window = true;
+    hud->show_another_window = true;
+    hud->rotation_speed = 15.0f;
+    
+    hud->cubeColor1[0] = 0.4f;
+    hud->cubeColor1[1] = 0.4f;
+    hud->cubeColor1[2] = 1.0f;
+    hud->cubeColor1[3] = 1.0f;
+    
+    hud->cubeColor2[0] = 1.0f;
+    hud->cubeColor2[1] = 0.4f;
+    hud->cubeColor2[2] = 0.4f;
+    hud->cubeColor2[3] = 1.0f;
+    
+    hud->clearColor[0] = 0.45f;
+    hud->clearColor[1] = 0.55f;
+    hud->clearColor[2] = 0.60f;
+    hud->clearColor[3] = 1.00f;
 }
     
-      // Don't allow copying
-  AnnounceTier(const AnnounceTier&) = delete;
-  AnnounceTier& operator=(const AnnounceTier&) = delete;
+        // Convert software texture to hardware texture.
+    ALLEGRO_BITMAP* cloned_img = al_clone_bitmap(img);
+    al_destroy_bitmap(img);
+    if (!cloned_img)
+        return false;
+    
+                if (ImGui::Button('Button'))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text('counter = %d', counter);
+    
+        // Create depth-stencil State
+    {
+        D3D10_DEPTH_STENCIL_DESC desc;
+        ZeroMemory(&desc, sizeof(desc));
+        desc.DepthEnable = false;
+        desc.DepthWriteMask = D3D10_DEPTH_WRITE_MASK_ALL;
+        desc.DepthFunc = D3D10_COMPARISON_ALWAYS;
+        desc.StencilEnable = false;
+        desc.FrontFace.StencilFailOp = desc.FrontFace.StencilDepthFailOp = desc.FrontFace.StencilPassOp = D3D10_STENCIL_OP_KEEP;
+        desc.FrontFace.StencilFunc = D3D10_COMPARISON_ALWAYS;
+        desc.BackFace = desc.FrontFace;
+        g_pd3dDevice->CreateDepthStencilState(&desc, &g_pDepthStencilState);
+    }
+    
+        int err = connect(socketfd, (sockaddr *) &addr, sizeof(addr));
+    if (err) {
+        cout << 'Connection error, connections: ' << connections << endl;
+        return false;
+    }
+    send(socketfd, buf, strlen(buf), 0);
+    memset(message, 0, 1024);
+    size_t length;
+    do {
+        length = recv(socketfd, message, sizeof(message), 0);
+    }
+    while (strncmp(&message[length - 4], '\r\n\r\n', 4));
+    
+    template <bool isServer>
+struct Group;
+    
+    struct Timepoint {
+    void (*cb)(Timer *);
+    Timer *timer;
+    std::chrono::system_clock::time_point timepoint;
+    int nextDelay;
+};
+    
+    template <bool isServer>
+class ExtensionsNegotiator {
+protected:
+    int options;
+public:
+    ExtensionsNegotiator(int wantedOptions);
+    std::string generateOffer();
+    void readOffer(std::string offer);
+    int getNegotiatedOptions();
+};
