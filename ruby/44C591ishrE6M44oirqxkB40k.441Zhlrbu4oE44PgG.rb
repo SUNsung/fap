a4be4798915c819f0e1e36f4a597d103500d163e
SUNsung <weatherius@gmail.com>
2018-03-12 04:23:11 +0200

    Rewrite [44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb] File in: 2018-03-12 04:23:11

--- ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
+++ ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
@@ -1,86 +1,84 @@
 
         
-              {
-          author: last_git_commit_formatted_with('%an'),
-          message: last_git_commit_formatted_with('%B'),
-          commit_hash: last_git_commit_formatted_with('%H'),
-          abbreviated_commit_hash: last_git_commit_formatted_with('%h')
-      }
-    end
-    
-          context('when the tag exists') do
-        before do
-          allow(Fastlane::Actions).to receive(:sh).with('git rev-parse -q --verify refs/tags/1.2.0 || true', { log: nil }).and_return('41215512353215321')
-        end
-    
-          it 'prefers a custom version number over a boring version bump' do
-        Fastlane::FastFile.new.parse('lane :test do
-          increment_version_number(version_number: '1.77.3', bump_type: 'major')
-        end').runner.execute(:test)
-    
-    
-    {      delta = 1
-      stat = File.stat(path)
-      assert_in_delta tb,   stat.birthtime.to_f, delta
-      assert_in_delta t0+2, stat.mtime.to_f, delta
-      if stat.birthtime != stat.ctime
-        assert_in_delta t0+4, stat.ctime.to_f, delta
-      end
-      if /mswin|mingw/ !~ RUBY_PLATFORM && !Bug::File::Fs.noatime?(path)
-        # Windows delays updating atime
-        assert_in_delta t0+6, stat.atime.to_f, delta
-      end
-    }
-  rescue NotImplementedError
-  end
-    
-      before :each do
-    @object = { 1 => 2, 3 => 4, 5 => 6 }
-  end
-  it_should_behave_like :enumeratorized_with_origin_size
-end
-    
-      def test_each_pair
-    ENV.each_pair do |k, v|
-      assert_kind_of(String, k)
-      assert_kind_of(String, v)
+          test 'generate a new token after first has expired' do
+    swap Devise, confirm_within: 3.days do
+      user = create_user
+      old = user.confirmation_token
+      user.update_attribute(:confirmation_sent_at, 4.days.ago)
+      user = User.find(user.id)
+      user.resend_confirmation_instructions
+      assert_not_equal user.confirmation_token, old
     end
   end
     
-        ('z' * 24).to_i(36).should == 22452257707354557240087211123792674815
-    
-    @@ chat
-<pre id='chat'></pre>
-<form>
-  <input id='msg' placeholder='type message here...' />
-</form>
-    
-          def html?(headers)
-        return false unless header = headers.detect { |k,v| k.downcase == 'content-type' }
-        options[:html_types].include? header.last[/^\w+\/\w+/]
-      end
-    end
+      def after_omniauth_failure_path_for(scope)
+    new_session_path(scope)
   end
-end
+    
+    class Devise::PasswordsController < DeviseController
+  prepend_before_action :require_no_authentication
+  # Render the #edit only if coming from a reset password email link
+  append_before_action :assert_reset_token_passed, only: :edit
+    
+    2) You are testing a Devise controller bypassing the router.
+   If so, you can explicitly tell Devise which mapping to use:
+    
+    require 'warden'
+require 'devise/mapping'
+require 'devise/models'
+require 'devise/modules'
+require 'devise/rails'
 
     
-    
-  it 'should allow changing the protection mode to a string' do
-    # I have no clue what other modes are available
-    mock_app do
-      use Rack::Protection::FrameOptions, :frame_options => 'ALLOW-FROM foo'
-      run DummyApp
+          unless root?
+        raise Invalid, 'missing name' if !name || name.empty?
+        raise Invalid, 'missing path' if !path || path.empty?
+        raise Invalid, 'missing type' if !type || type.empty?
+      end
     end
     
-      def as_boolean(string)
-    return true   if string == true   || string =~ (/(true|t|yes|y|1)$/i)
-    return false  if string == false  || string.blank? || string =~ (/(false|f|no|n|0)$/i)
-    raise ArgumentError.new('invalid value for Boolean: \'#{string}\'')
+        private
+    
+            css('.note h3', '.warning h3').each do |node|
+          node.before('<p><strong>#{node.inner_html}</strong></p>').remove
+        end
+    
+            css('.function').each do |node|
+          name = '#{node.at_css('.descname').content}()'
+          id = node.at_css('dt[id]')['id']
+          entries << [name, id]
+        end
+    
+        context 'given no content-type header' do
+      subject { Rack::Protection::Base.new(nil).html?({}) }
+      it { is_expected.to be_falsey }
+    end
   end
     
-      public
-  def close; end;
-    
-      def fifty_percent
-    [1, (maximum * 0.5)].max.floor
+      it 'should not override the header if already set' do
+    mock_app with_headers('X-XSS-Protection' => '0')
+    expect(get('/', {}, 'wants' => 'text/html').headers['X-XSS-Protection']).to eq('0')
   end
+    
+    class SinatraStaticServer < Sinatra::Base
+    
+        def paragraphize(input)
+      '<p>#{input.lstrip.rstrip.gsub(/\n\n/, '</p><p>').gsub(/\n/, '<br/>')}</p>'
+    end
+  end
+end
+    
+        def render(context)
+      if parts = @text.match(/([a-zA-Z\d]*) (.*)/)
+        gist, file = parts[1].strip, parts[2].strip
+      else
+        gist, file = @text.strip, ''
+      end
+      if gist.empty?
+        ''
+      else
+        script_url = script_url_for gist, file
+        code       = get_cached_gist(gist, file) || get_gist_from_web(gist, file)
+        html_output_for script_url, code
+      end
+    end
