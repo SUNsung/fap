8d274b0061a56ee1bb1ad65b8f390d0e76872aa2
SUNsung <weatherius@gmail.com>
2018-03-13 15:42:37 +0200

    Rewrite [19x0PypItE6h.19Yd6Q1PYvSOI19qv4GZ.ce.So19DeN.py] File in: 2018-03-13 15:42:37

--- pyton/19x0PypItE6h.19Yd6Q1PYvSOI19qv4GZ.ce.So19DeN.py
+++ pyton/19x0PypItE6h.19Yd6Q1PYvSOI19qv4GZ.ce.So19DeN.py
@@ -1,106 +1,16 @@
 
         
-        
-def register_cli(app):
-    @app.cli.command('initdb')
-    def initdb_command():
-        '''Creates the database tables.'''
-        init_db()
-        print('Initialized the database.')
+        DOCUMENTATION = '''
+---
+inventory: openshift
+short_description: Openshift gears external inventory script
+description:
+  - Generates inventory of Openshift gears using the REST interface
+  - this permit to reuse playbook to setup an Openshift gear
+version_added: None
+author: Michael Scherer
+'''
     
-    
-def test_login_logout(client, app):
-    '''Make sure login and logout works'''
-    rv = login(client, app.config['USERNAME'],
-               app.config['PASSWORD'])
-    assert b'You were logged in' in rv.data
-    rv = logout(client)
-    assert b'You were logged out' in rv.data
-    rv = login(client,app.config['USERNAME'] + 'x',
-               app.config['PASSWORD'])
-    assert b'Invalid username' in rv.data
-    rv = login(client, app.config['USERNAME'],
-               app.config['PASSWORD'] + 'x')
-    assert b'Invalid password' in rv.data
-    
-    import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.spatial import cKDTree
-from skimage.feature import plot_matches
-from skimage.measure import ransac
-from skimage.transform import AffineTransform
-import tensorflow as tf
-    
-      # Figure out descriptor dimensionality by parsing first one.
-  descriptor_dim = len(
-      datum_io.DatumToArray(delf_features.feature[0].descriptor))
-  locations = np.zeros([num_features, 2])
-  scales = np.zeros([num_features])
-  descriptors = np.zeros([num_features, descriptor_dim])
-  attention = np.zeros([num_features])
-  orientations = np.zeros([num_features])
-    
-    import os.path
-    
-    parser = argparse.ArgumentParser()
-parser.add_argument('--batch_size', default=100, type=int, help='batch size')
-parser.add_argument('--train_steps', default=1000, type=int,
-                    help='number of training steps')
-    
-        value_ops_dict = dict(zip(metric_names, value_ops))
-    
-      Returns:
-    None
-  '''
-  doc_seen = set()
-    
-        def __init__(self):
-        self.settings = None  # set in scrapy.cmdline
-    
-        def from_spider(self, spider, results):
-        requests = []
-        for method in self.tested_methods_from_spidercls(type(spider)):
-            bound_method = spider.__getattribute__(method)
-            requests.append(self.from_method(bound_method, results))
-    
-        def mapper_sort(self, key, value):
-        '''Construct key to ensure proper sorting.
-    
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer)
-        ]
-    
-    
-class Chat(metaclass=ABCMeta):
-    
-    
-class Motorcycle(Vehicle):
-    
-    GB_RESULT_SET = [
-    {
-        'version': 'v1',
-        'timestamp': '2012-01-01T00:00:00.000Z',
-        'event': {
-            'dim1': 'Canada',
-            'metric1': 12345678,
-        },
-    },
-    {
-        'version': 'v1',
-        'timestamp': '2012-01-01T00:00:00.000Z',
-        'event': {
-            'dim1': 'USA',
-            'metric1': 12345678 / 2,
-        },
-    },
-]
-    
-    from superset import frontend_config
-    
-    Revision ID: 19a814813610
-Revises: ca69c70ec99b
-Create Date: 2017-09-15 15:09:40.495345
+            if platforms and isinstance(platforms, string_types):
+            platforms = platforms.split(',')
+            search_url += '&platforms_autocomplete=' + '+'.join(platforms)
