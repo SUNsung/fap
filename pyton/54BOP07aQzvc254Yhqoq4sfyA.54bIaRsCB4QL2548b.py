030033a0a89ac071e6f549a660e88a359c3ecd59
SUNsung <weatherius@gmail.com>
2018-03-14 01:37:06 +0200

    Rewrite [54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py] File in: 2018-03-14 01:37:06

--- pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
+++ pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
@@ -1,54 +1,36 @@
 
         
-        '''
-from datetime import datetime
-import numpy as np
-from sklearn import linear_model
-from sklearn.utils.bench import total_seconds
+        def main(loc, colorscheme):
     
-    In both cases, only 10% of the features are informative.
-'''
-import gc
-from time import time
-import numpy as np
+        def test_repr(self):
+        self.assertRegexpMatches(repr(PENDING_FUTURE),
+                                 '<Future at 0x[0-9a-f]+ state=pending>')
+        self.assertRegexpMatches(repr(RUNNING_FUTURE),
+                                 '<Future at 0x[0-9a-f]+ state=running>')
+        self.assertRegexpMatches(repr(CANCELLED_FUTURE),
+                                 '<Future at 0x[0-9a-f]+ state=cancelled>')
+        self.assertRegexpMatches(repr(CANCELLED_AND_NOTIFIED_FUTURE),
+                                 '<Future at 0x[0-9a-f]+ state=cancelled>')
+        self.assertRegexpMatches(
+                repr(EXCEPTION_FUTURE),
+                '<Future at 0x[0-9a-f]+ state=finished raised IOError>')
+        self.assertRegexpMatches(
+                repr(SUCCESSFUL_FUTURE),
+                '<Future at 0x[0-9a-f]+ state=finished returned int>')
     
-        n_times : int
-        Time calling the metric n_times times.
     
-        for n_components in [i.astype(int) for i in
-                         np.linspace(data.shape[1] // 10,
-                                     data.shape[1], num=4)]:
-        all_times = defaultdict(list)
-        all_errors = defaultdict(list)
-        pca = PCA(n_components=n_components)
-        rpca = RandomizedPCA(n_components=n_components, random_state=1999)
-        results_dict = {k: benchmark(est, data) for k, est in [('pca', pca),
-                                                               ('rpca', rpca)]}
+  def Start( self ):
+    request_data = BuildRequestData()
+    if self._extra_data:
+      request_data.update( self._extra_data )
+    with HandleServerException( display = False ):
+      self._response = self.PostDataToHandler( request_data, 'debug_info' )
     
-    n_samples = np.logspace(.5, 3, 9)
-n_features = np.logspace(1, 3.5, 7)
-N_samples, N_features = np.meshgrid(n_samples,
-                                    n_features)
-scikits_time = np.zeros(N_samples.shape)
-scipy_time = np.zeros(N_samples.shape)
     
-    Benchmarks for random projections.
-    
-        print('Averaging results...', end='')
-    for name in sampling_algorithm:
-        time[name] = np.mean(time[name], axis=1)
-    print('done\n')
-    
-        # decode the payload explicitly as UTF-8 since lxml is confused for some
-    # reason
-    with codecs.open(html_filename,'r','utf-8') as html_file:
-        html_content = html_file.read()
-    tree = ElementTree(lxml.html.document_fromstring(html_content))
-    i = 0
-    j = 0
-    for p in tree.findall('//p'):
-        content = p.text_content()
-        if len(content) < 100:
-            # skip paragraphs that are too short - probably too noisy and not
-            # representative of the actual language
-            continue
+  def IsAllowed( self, diagnostic ):
+    # NOTE: in this class's implementation, we ask vimsupport for
+    #  the current filetypes and delegate automatically; it is probably,
+    #  more efficient, however, to call SubsetForTypes() and reuse
+    #  the returned DiagnosticFilter if it will be checked repeatedly.
+    filetypes = vimsupport.CurrentFiletypes()
+    return self.SubsetForTypes( filetypes ).IsAllowed( diagnostic )
