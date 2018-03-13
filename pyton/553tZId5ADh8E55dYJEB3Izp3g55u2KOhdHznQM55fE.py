97127d5d4796fd1e9a98ea58f930c28232b9b0f8
SUNsung <weatherius@gmail.com>
2018-03-13 17:39:36 +0200

    Rewrite [553tZId5ADh8E55dYJEB3Izp3g55u2KOhdHznQM55fE.py] File in: 2018-03-13 17:39:35

--- pyton/553tZId5ADh8E55dYJEB3Izp3g55u2KOhdHznQM55fE.py
+++ pyton/553tZId5ADh8E55dYJEB3Izp3g55u2KOhdHznQM55fE.py
@@ -1,91 +1,21 @@
 
         
-        
-def print_list():
-    data = get_serf_members_data()
-    nodes = get_nodes(data)
-    groups = get_groups(data)
-    meta = get_meta(data)
-    inventory_data = {_key: nodes, '_meta': meta}
-    inventory_data.update(groups)
-    print(json.dumps(inventory_data))
+            platforms = 'any',
+    zip_safe = True,
+    include_package_data = True,
     
-    # Backwards compat only
-try:
-    from hashlib import md5 as _md5
-except ImportError:
-    try:
-        from md5 import md5 as _md5
-    except ImportError:
-        # Assume we're running in FIPS mode here
-        _md5 = None
+    def kugou_download_playlist(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+    html=get_html(url)
+    pattern=re.compile('title='(.*?)'.* data='(\w*)\|.*?'')
+    pairs=pattern.findall(html)
+    for title,hash_val in pairs:
+        kugou_download_by_hash(title,hash_val,output_dir,merge,info_only)
     
-        data = dict(
-        globalEnv=['%s=%s' % (kp[0], kp[1]) for kp in args.env or []]
-    )
+    from html.parser import HTMLParser
     
-            # zone, resource, entity, method
-        input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/zones/us-east1-c/instanceGroupManagers/my-mig/recreateInstances'
-        actual = GCPUtils.parse_gcp_url(input_url)
-        self.assertEquals('compute', actual['service'])
-        self.assertEquals('v1', actual['api_version'])
-        self.assertEquals('myproject', actual['project'])
-        self.assertEquals('us-east1-c', actual['zone'])
-        self.assertEquals('instanceGroupManagers', actual['resource_name'])
-        self.assertEquals('my-mig', actual['entity_name'])
-        self.assertEquals('recreateInstances', actual['method_name'])
-    
-        def test_items_recoil(self):
-        with ObjectPool(self.sample_queue, True) as pool:
-            self.assertEqual(pool, 'first')
-        self.assertTrue(self.sample_queue.get() == 'second')
-        self.assertFalse(self.sample_queue.empty())
-        self.assertTrue(self.sample_queue.get() == 'first')
-        self.assertTrue(self.sample_queue.empty())
-    
-    ###  OUTPUT ###
-# Before subclassing:
-# BaseRegisteredClass
-# After subclassing:
-# BaseRegisteredClass
-# ClassRegistree
-
-    
-        def test_tc3_output(self):
-        self.tc3.run()
-        output = self.out.getvalue().strip()
-        self.assertEqual(output, self.average_result_tc3)
-    
-    
-    def _onclick(self, event):
-        coord = []
-        self.ix, self.iy = event.xdata, event.ydata
-        coord.append((self.ix, self.iy))
-        print('coordinate = ', coord)
-        self.coords.append(coord)
-        self.click_counter += 1
-    
-    
-def dump_device_info():
-    '''
-    显示设备信息
-    '''
-    size_str = adb.get_screen()
-    device_str = adb.test_device_detail()
-    phone_os_str = adb.test_device_os()
-    density_str = adb.test_density()
-    print('''**********
-Screen: {size}
-Density: {dpi}
-Device: {device}
-Phone OS: {phone_os}
-Host OS: {host_os}
-Python: {python}
-**********'''.format(
-        size=size_str.replace('\n', ''),
-        dpi=density_str.replace('\n', ''),
-        device=device_str.replace('\n', ''),
-        phone_os=phone_os_str.replace('\n', ''),
-        host_os=sys.platform,
-        python=sys.version
-    ))
+        # ordered list of supported stream types / qualities on this site
+    # order: high quality -> low quality
+    stream_types = [
+        {'id': 'original'}, # contains an 'id' or 'itag' field at minimum
+        {'id': 'small'},
+    ]
