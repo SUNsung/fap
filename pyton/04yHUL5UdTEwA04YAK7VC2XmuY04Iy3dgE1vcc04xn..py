1c871bddc034482cbb3e27bd46ebef10d45da7df
SUNsung <weatherius@gmail.com>
2018-03-13 06:52:56 +0200

    Rewrite [04yHUL5UdTEwA04YAK7VC2XmuY04Iy3dgE1vcc04xn..py] File in: 2018-03-13 06:52:56

--- pyton/04yHUL5UdTEwA04YAK7VC2XmuY04Iy3dgE1vcc04xn..py
+++ pyton/04yHUL5UdTEwA04YAK7VC2XmuY04Iy3dgE1vcc04xn..py
@@ -1,132 +1,53 @@
 
         
-        
-@pytest.fixture
-def client():
-    return blueprintexample.app.test_client()
-    
-        A microblog example application written as Flask tutorial with
-    Flask and sqlite3.
-    
-        A simple application that shows how Flask and jQuery get along.
-    
-      perturbs = [_scale_l2(d, FLAGS.perturb_norm_length) for d in perturbs]
-  vadv_logits = logits_from_embedding_fn(
-      [emb + d for (emb, d) in zip(embedded, perturbs)])
-  return _kl_divergence_with_logits(logits, vadv_logits, weights)
-    
-      def add_token(self, token):
-    self._token.int64_list.value.append(token)
-    return self
+            def get(self, key, default=None):
+        return self.__dict__.get(key, default)
+
     
     
-def evaluate(defun=False):
-  model = mnist.Model(data_format())
-  dataset = random_dataset()
-  if defun:
-    model.call = tfe.defun(model.call)
-  with tf.device(device()):
-    mnist_eager.test(model, dataset)
+class TooManyRedirects(RequestException):
+    '''Too many redirects.'''
     
-    import tensorflow as tf
+    DOCUMENTATION = '''
+---
+inventory: openshift
+short_description: Openshift gears external inventory script
+description:
+  - Generates inventory of Openshift gears using the REST interface
+  - this permit to reuse playbook to setup an Openshift gear
+version_added: None
+author: Michael Scherer
+'''
     
-      @tf.test.mock.patch.dict(premade_estimator.__dict__,
-                           {'load_data': four_lines_data})
-  def test_premade_estimator(self):
-    premade_estimator.main([None, '--train_steps=1'])
+        try:
+        return json.dumps(result, sort_keys=True, indent=indent, ensure_ascii=False)
+    except UnicodeDecodeError:
+        return json.dumps(result, sort_keys=True, indent=indent)
+
     
-        # Train the Model.
-    classifier.train(
-        input_fn=lambda:iris_data.train_input_fn(train_x, train_y,
-                                                 args.batch_size),
-        steps=args.train_steps)
+            if tags and isinstance(tags, string_types):
+            tags = tags.split(',')
+            search_url += '&tags_autocomplete=' + '+'.join(tags)
     
-    flags.DEFINE_string('checkpoint_dir', '/tmp/text_train',
-                    'Directory where to read model checkpoints.')
-flags.DEFINE_integer('eval_interval_secs', 60, 'How often to run the eval.')
-flags.DEFINE_integer('num_examples', 32, 'Number of examples to run.')
-flags.DEFINE_bool('run_once', False, 'Whether to run eval only once.')
+        for ns in n_samples:
+        for nf in n_features:
+            it += 1
+            print('==================')
+            print('Iteration %s of %s' % (it, max(len(n_samples),
+                                          len(n_features))))
+            print('==================')
+            n_informative = nf // 10
+            X, Y, coef_ = make_regression(n_samples=ns, n_features=nf,
+                                          n_informative=n_informative,
+                                          noise=0.1, coef=True)
     
-        # Build and write sequence files.
-    vocab_ids = _build_random_vocabulary(FLAGS.vocab_size)
-    seqs = [_build_random_sequence(vocab_ids) for _ in range(5)]
-    seqs_label = [
-        data.build_labeled_sequence(seq, random.choice([True, False]))
-        for seq in seqs
-    ]
-    seqs_lm = [data.build_lm_sequence(seq) for seq in seqs]
-    seqs_ae = [data.build_seq_ae_sequence(seq) for seq in seqs]
-    seqs_rev = [data.build_reverse_sequence(seq) for seq in seqs]
-    seqs_bidir = [
-        data.build_bidirectional_seq(seq, rev)
-        for seq, rev in zip(seqs, seqs_rev)
-    ]
-    seqs_bidir_label = [
-        data.build_labeled_sequence(bd_seq, random.choice([True, False]))
-        for bd_seq in seqs_bidir
-    ]
+            ax.xaxis.set_major_locator(ticker.FixedLocator(tick_vals))
+        ax.xaxis.set_major_formatter(ticker.FixedFormatter(tick_labels))
     
-      def save_state(self, value):
-    # LSTM tuple states
-    state_names = _get_tuple_state_names(self._num_states, self._state_name)
-    save_ops = []
-    for (c_state, h_state), (c_name, h_name) in zip(value, state_names):
-      save_ops.append(self._batch.save_state(c_name, c_state))
-      save_ops.append(self._batch.save_state(h_name, h_state))
-    return tf.group(*save_ops)
+        for n_samples in sample_sizes:
+        X = random_state.rand(n_samples, 300)
     
-    
-def _summarize_vars_and_grads(grads_and_vars):
-  tf.logging.info('Trainable variables:')
-  tf.logging.info('-' * 60)
-  for grad, var in grads_and_vars:
-    tf.logging.info(var)
-    
-    FLAGS = tf.app.flags.FLAGS
-    
-    
-class Cache(object):
-    
-        def __init__(self, person_ids, lookup):
-        self.lookup = lookup
-        self.person_ids = person_ids
-        self.visited_ids = set()
-    
-        def extract_max_priority_page(self):
-        '''Return the highest priority link in `links_to_crawl`.'''
-        ...
-    
-    
-class NoTalkProxyTest(unittest.TestCase):
-    
-        jim = Subscriber('jim', message_center)
-    jim.subscribe('cartoon')
-    jack = Subscriber('jack', message_center)
-    jack.subscribe('music')
-    gee = Subscriber('gee', message_center)
-    gee.subscribe('movie')
-    vani = Subscriber('vani', message_center)
-    vani.subscribe('movie')
-    vani.unsubscribe('movie')
-    
-        def test_shall_toggle_from_am_to_fm(self):
-        self.radio.toggle_amfm()
-        state = self.radio.state.name
-        expected_state_name = 'FM'
-        self.assertEqual(state, expected_state_name)
-    
-        def test_display_current_time_at_current_time(self):
-        '''
-        Just as justification for working example. (Will always pass.)
-        '''
-        production_code_time_provider = ProductionCodeTimeProvider()
-        class_under_test = TimeDisplay(production_code_time_provider)
-        current_time = datetime.datetime.now()
-        expected_time = '<span class=\'tinyBoldText\'>{}:{}</span>'.format(current_time.hour, current_time.minute)
-        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
-    
-    
-class ConcreteHandler1(Handler):
-    
-    
-class MoveFileCommand(object):
+        print('Decompressing %s' % ARCHIVE_NAME)
+    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
+        archive.extractall(path='.')
+    os.remove(ARCHIVE_NAME)
