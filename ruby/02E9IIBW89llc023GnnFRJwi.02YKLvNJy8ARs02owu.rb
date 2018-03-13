67769553e6ef4d73f282059d7412b65482e59df4
SUNsung <weatherius@gmail.com>
2018-03-13 13:31:03 +0200

    Create [02E9IIBW89llc023GnnFRJwi.02YKLvNJy8ARs02owu.rb] File in: 2018-03-13 13:31:02

---
+++ ruby/02E9IIBW89llc023GnnFRJwi.02YKLvNJy8ARs02owu.rb
@@ -0,0 +1,9 @@
+      raise Discourse::InvalidParameters.new(:to_address) unless args[:to_address].present?
+    
+          expect('.border-style-all').to have_rule(rule)
+    end
+  end
+    
+      context 'called with three sizes' do
+    it 'applies second width to left and right' do
+      rule = 'padding: 4px 5px 6px'
