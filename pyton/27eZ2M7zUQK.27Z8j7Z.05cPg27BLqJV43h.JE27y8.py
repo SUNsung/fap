d54b915c7b0f689531a783b7ad358163ba38830b
SUNsung <weatherius@gmail.com>
2018-03-13 04:55:46 +0200

    Rewrite [27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py] File in: 2018-03-13 04:55:46

--- pyton/27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py
+++ pyton/27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py
@@ -1,24 +1,115 @@
 
         
-            # noinspection PyMethodOverriding
-    def get_auth(self, username, password):
-        return requests.auth.HTTPDigestAuth(username, password)
-
+        
+def get_info(package_name):
+    api_url = 'https://pypi.python.org/pypi/{}/json'.format(package_name)
+    resp = requests.get(api_url).json()
+    hasher = hashlib.sha256()
+    for release in resp['urls']:
+        download_url = release['url']
+        if download_url.endswith('.tar.gz'):
+            hasher.update(requests.get(download_url).content)
+            return {
+                'name': package_name,
+                'url': download_url,
+                'sha256': hasher.hexdigest(),
+            }
+    else:
+        raise RuntimeError(
+            '{}: download not found: {}'.format(package_name, resp))
     
-        def get_formatters_grouped(self):
-        groups = {}
-        for group_name, group in groupby(
-                self.get_formatters(),
-                key=lambda p: getattr(p, 'group_name', 'format')):
-            groups[group_name] = list(group)
-        return groups
+    
+class HTTPRequest(HTTPMessage):
+    '''A :class:`requests.models.Request` wrapper.'''
+    
+        '''
     
     
-# -- Options for LaTeX output --------------------------------------------------
+FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
+FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
+JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
+BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
     
     
-def get_user_id(username):
-    '''Convenience method to look up the id for a username.'''
-    rv = query_db('select user_id from user where username = ?',
-                  [username], one=True)
-    return rv[0] if rv else None
+def has_docutils():
+    try:
+        # noinspection PyUnresolvedReferences
+        import docutils
+        return True
+    except ImportError:
+        return False
+    
+    
+def test_follow_redirect_output_options(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=h',
+             '--history-print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 2
+    assert 'HTTP/1.1 302 FOUND' not in r
+    assert HTTP_OK in r
+    
+            :param mime: E.g., 'application/atom+xml'.
+        :param content: The body content as text
+    
+    
+def delete(url, **kwargs):
+    r'''Sends a DELETE request.
+    
+                if isinstance(fp, (str, bytes, bytearray)):
+                fdata = fp
+            else:
+                fdata = fp.read()
+    
+            :param name: a string containing name of cookie
+        :param domain: (optional) string containing domain of cookie
+        :param path: (optional) string containing path of cookie
+        :raises KeyError: if cookie is not found
+        :raises CookieConflictError: if there are multiple cookies
+            that match name and optionally domain and path
+        :return: cookie.value
+        '''
+        toReturn = None
+        for cookie in iter(self):
+            if cookie.name == name:
+                if domain is None or cookie.domain == domain:
+                    if path is None or cookie.path == path:
+                        if toReturn is not None:  # if there are multiple cookies that meet passed in criteria
+                            raise CookieConflictError('There are multiple cookies with name, %r' % (name))
+                        toReturn = cookie.value  # we will eventually return this as long as no cookie conflict
+    
+            # Set environment's basic authentication if not explicitly set.
+        auth = request.auth
+        if self.trust_env and not auth and not self.auth:
+            auth = get_netrc_auth(request.url)
+    
+        def test_proxy_auth_empty_pass(self, httpbin):
+        adapter = HTTPAdapter()
+        headers = adapter.proxy_headers('http://user:@httpbin.org')
+        assert headers == {'Proxy-Authorization': 'Basic dXNlcjo='}
+    
+        def remove(self, key):
+        hash_index = self._hash_function(key)
+        for index, item in enumerate(self.table[hash_index]):
+            if item.key == key:
+                del self.table[hash_index][index]
+                return
+        raise KeyError('Key not found')
+    
+            (category1, 1), product4
+        (category1, 2), product1
+        (category1, 3), product2
+        (category2, 3), product1
+        (category2, 7), product3
+        '''
+        category, product_id = key
+        quantity = value
+        yield (category, quantity), product_id
+    
+        def extract_max_priority_page(self):
+        '''Return the highest priority link in `links_to_crawl`.'''
+        pass
+    
+        By default this will get the strings from the blns.txt file
