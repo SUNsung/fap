fdaf5b7980253b776d7b50b276fda8601a48a6a8
SUNsung <weatherius@gmail.com>
2018-03-14 08:12:34 +0200

    Rewrite [02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py] File in: 2018-03-14 08:12:33

--- pyton/02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py
+++ pyton/02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py
@@ -1,86 +1,28 @@
-        # Self test
-        self.assertTrue(self.addr.conflicts(self.addr))
-        self.assertTrue(self.addr1.conflicts(self.addr1))
-        # This is a tricky one...
-        self.assertTrue(self.addr1.conflicts(self.addr2))
+
+        
+        
+if __name__ == '__main__':
+    main()
+
     
-        def __init__(self, args):
-        '''Initializes the plugin with the given command line args'''
-        self._temp_dir = tempfile.mkdtemp()
-        self.le_config = util.create_le_config(self._temp_dir)
-        config_dir = util.extract_configs(args.configs, self._temp_dir)
-        self._configs = [
-            os.path.join(config_dir, config)
-            for config in os.listdir(config_dir)]
+    site_info = '花瓣 (Huaban)'
     
-        def _adjust_thread_count(self):
-        # When the executor gets lost, the weakref callback will wake up
-        # the worker threads.
-        def weakref_cb(_, q=self._work_queue):
-            q.put(None)
-        # TODO(bquinlan): Should avoid creating new threads if there are more
-        # idle threads than items in the work queue.
-        if len(self._threads) < self._max_workers:
-            t = threading.Thread(target=_worker,
-                                 args=(weakref.ref(self, weakref_cb),
-                                       self._work_queue))
-            t.daemon = True
-            t.start()
-            self._threads.add(t)
+    def kuwo_download_by_rid(rid, output_dir = '.', merge = True, info_only = False):
+    html=get_content('http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_%s'%rid)
+    title=match1(html,r'<name>(.*)</name>')
+    #to get title
+    #format =aac|mp3 ->to get aac format=mp3 ->to get mp3
+    url=get_content('http://antiserver.kuwo.cn/anti.s?format=mp3&rid=MUSIC_%s&type=convert_url&response=url'%rid)
+    songtype, ext, size = url_info(url)
+    print_info(site_info, title, songtype, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir)
     
-            Returns:
-            False if the Future was cancelled, True otherwise.
+            print_info(site_info, title, type_, size_full)
+        if not info_only:
+            download_urls(url_list, title, ext, total_size=size_full, output_dir=output_dir, merge=merge, headers=fake_headers)
+    else:
+        raise NotImplementedError(flashvars)
     
-        # Parse and validate the field names.  Validation serves two purposes,
-    # generating informative error messages and preventing template injection attacks.
-    if isinstance(field_names, basestring):
-        field_names = field_names.replace(',', ' ').split() # names separated by whitespace and/or commas
-    field_names = tuple(map(str, field_names))
-    for name in (typename,) + field_names:
-        if not all(c.isalnum() or c=='_' for c in name):
-            raise ValueError('Type names and field names can only contain alphanumeric characters and underscores: %r' % name)
-        if _iskeyword(name):
-            raise ValueError('Type names and field names cannot be a keyword: %r' % name)
-        if name[0].isdigit():
-            raise ValueError('Type names and field names cannot start with a number: %r' % name)
-    seen_names = set()
-    for name in field_names:
-        if name.startswith('_'):
-            raise ValueError('Field names cannot start with an underscore: %r' % name)
-        if name in seen_names:
-            raise ValueError('Encountered duplicate field name: %r' % name)
-        seen_names.add(name)
-    
-                if work_item.future.set_running_or_notify_cancel():
-                call_queue.put(_CallItem(work_id,
-                                         work_item.fn,
-                                         work_item.args,
-                                         work_item.kwargs),
-                               block=True)
-            else:
-                del pending_work_items[work_id]
-                continue
-    
-    def download_urls_with_executor(urls, executor, timeout=60):
-    try:
-        url_to_content = {}
-        future_to_url = dict((executor.submit(load_url, url, timeout), url)
-                             for url in urls)
-    
-    # The reST default role (used for this markup: `text`) to use for all documents.
-#default_role = None
-    
-    
-def WaitUntilReady( timeout = 5 ):
-  expiration = time.time() + timeout
-  while True:
-    try:
-      if time.time() > expiration:
-        raise RuntimeError( 'Waited for the server to be ready '
-                            'for {0} seconds, aborting.'.format( timeout ) )
-      if _IsReady():
-        return
-    except requests.exceptions.ConnectionError:
-      pass
-    finally:
-      time.sleep( 0.1 )
+    class MusicPlayOn(VideoExtractor):
+    name = 'MusicPlayOn'
