3a8399b7777998c45642c0b0be8ad247ea593c69
SUNsung <weatherius@gmail.com>
2018-03-14 22:31:12 +0200

    Rewrite [06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py] File in: 2018-03-14 22:31:12

--- pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
+++ pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
@@ -1,51 +1,83 @@
 
         
-        with open('update/LATEST_VERSION', 'w') as f:
-    f.write(version)
+        :copyright: (c) 2012 by Kenneth Reitz.
+:license: Apache2, see LICENSE for more details.
+'''
     
-            webpage = self._download_webpage(url, playlist_id)
-        title = self._html_search_regex(
-            r'<h1 class='playlist-name'[^>]*?>(.*?)</h1>', webpage, 'title')
-        description = self._html_search_regex(
-            r'<p class='excerpt'[^>]*?>(.*?)</p>',
-            webpage, 'description', fatal=False)
-        urls = re.findall(
-            r'<li class='lecture-preview'>\s*?<a target='_blank' href='([^']+)'>',
-            webpage)
-        entries = [self.url_result(u) for u in urls]
+    '''
+requests._internal_utils
+~~~~~~~~~~~~~~
     
-            flv_id = self._html_search_regex(
-            r'<a[^>]+href=['\']/download/\?v=(\d+)', webpage, 'video id')
+        def __call__(self, r):
+        r.headers['Proxy-Authorization'] = _basic_auth_str(self.username, self.password)
+        return r
     
+        def has_header(self, name):
+        return name in self._r.headers or name in self._new_headers
     
-@app.before_request
-def before_request():
-    g.user = None
-    if 'user_id' in session:
-        g.user = query_db('select * from user where user_id = ?',
-                          [session['user_id']], one=True)
+            errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
     
     
-def test_register(client):
-    '''Make sure registering works'''
-    rv = register(client, 'user1', 'default')
-    assert b'You were successfully registered ' \
-           b'and can login now' in rv.data
-    rv = register(client, 'user1', 'default')
-    assert b'The username is already taken' in rv.data
-    rv = register(client, '', 'default')
-    assert b'You have to enter a username' in rv.data
-    rv = register(client, 'meh', '')
-    assert b'You have to enter a password' in rv.data
-    rv = register(client, 'meh', 'x', 'y')
-    assert b'The two passwords do not match' in rv.data
-    rv = register(client, 'meh', 'foo', email='broken')
-    assert b'You have to enter a valid email address' in rv.data
+@pytest.mark.parametrize(
+    'mask, expected', (
+        (8, '255.0.0.0'),
+        (24, '255.255.255.0'),
+        (25, '255.255.255.128'),
+    ))
+def test_dotted_netmask(mask, expected):
+    assert dotted_netmask(mask) == expected
     
-        def deploy_cert(self, domain, cert_path, key_path, chain_path=None,
-                    fullchain_path=None):
-        '''Installs cert'''
-        cert_path, key_path, chain_path = self.copy_certs_and_keys(
-            cert_path, key_path, chain_path)
-        self._configurator.deploy_cert(
-            domain, cert_path, key_path, chain_path, fullchain_path)
+        close_server = threading.Event()
+    server = Server(response_handler, wait_to_close_event=close_server)
+    
+    import io
+import requests
+import pytest
+from requests.adapters import HTTPAdapter
+from requests.auth import HTTPDigestAuth, _basic_auth_str
+from requests.compat import (
+    Morsel, cookielib, getproxies, str, urlparse,
+    builtin_str, OrderedDict)
+from requests.cookies import (
+    cookiejar_from_dict, morsel_to_cookie)
+from requests.exceptions import (
+    ConnectionError, ConnectTimeout, InvalidSchema, InvalidURL,
+    MissingSchema, ReadTimeout, Timeout, RetryError, TooManyRedirects,
+    ProxyError, InvalidHeader, UnrewindableBodyError, SSLError, InvalidProxyURL)
+from requests.models import PreparedRequest
+from requests.structures import CaseInsensitiveDict
+from requests.sessions import SessionRedirectMixin
+from requests.models import urlencode
+from requests.hooks import default_hooks
+    
+            html = get_content(self.url)
+    
+    #----------------------------------------------------------------------
+def fc2video_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+    '''wrapper'''
+    #'http://video.fc2.com/en/content/20151021bTVKnbEw'
+    #'http://xiaojiadianvideo.asia/content/20151021bTVKnbEw'
+    #'http://video.fc2.com/ja/content/20151021bTVKnbEw'
+    #'http://video.fc2.com/tw/content/20151021bTVKnbEw'
+    hostname = urlparse(url).hostname
+    if not ('fc2.com' in hostname or 'xiaojiadianvideo.asia' in hostname):
+        return False
+    upid = match1(url, r'.+/content/(\w+)')
+    
+    class Pinterest(VideoExtractor):
+    # site name
+    name = 'Pinterest'
+    
+    def showroom_download_by_room_id(room_id, output_dir = '.', merge = False, info_only = False, **kwargs):
+    '''Source: Android mobile'''
+    while True:
+        timestamp = str(int(time() * 1000))
+        api_endpoint = 'https://www.showroom-live.com/api/live/streaming_url?room_id={room_id}&_={timestamp}'.format(room_id = room_id, timestamp = timestamp)
+        html = get_content(api_endpoint)
+        html = json.loads(html)
+        #{'streaming_url_list': [{'url': 'rtmp://52.197.69.198:1935/liveedge', 'id': 1, 'label': 'original spec(low latency)', 'is_default': True, 'type': 'rtmp', 'stream_name': '7656a6d5baa1d77075c971f6d8b6dc61b979fc913dc5fe7cc1318281793436ed'}, {'url': 'http://52.197.69.198:1935/liveedge/7656a6d5baa1d77075c971f6d8b6dc61b979fc913dc5fe7cc1318281793436ed/playlist.m3u8', 'is_default': True, 'id': 2, 'type': 'hls', 'label': 'original spec'}, {'url': 'rtmp://52.197.69.198:1935/liveedge', 'id': 3, 'label': 'low spec(low latency)', 'is_default': False, 'type': 'rtmp', 'stream_name': '7656a6d5baa1d77075c971f6d8b6dc61b979fc913dc5fe7cc1318281793436ed_low'}, {'url': 'http://52.197.69.198:1935/liveedge/7656a6d5baa1d77075c971f6d8b6dc61b979fc913dc5fe7cc1318281793436ed_low/playlist.m3u8', 'is_default': False, 'id': 4, 'type': 'hls', 'label': 'low spec'}]}
+        if len(html) >= 1:
+            break
+        log.w('The live show is currently offline.')
+        sleep(1)
