7e2e0572cfa55baebd4c4ccda7cc699739685ce9
SUNsung <weatherius@gmail.com>
2018-03-15 17:26:56 +0200

    Rewrite [29hvjJGY8r3ew29cg0GwdeEgCk29FnOijCWpSFY29oay.py] File in: 2018-03-15 17:26:55

--- pyton/29hvjJGY8r3ew29cg0GwdeEgCk29FnOijCWpSFY29oay.py
+++ pyton/29hvjJGY8r3ew29cg0GwdeEgCk29FnOijCWpSFY29oay.py
@@ -1,43 +1,68 @@
 
         
-            # Sorting the libraries
-    inner_blocks = sorted(blocks[0].split('##'))
-    for i in range(1 , len(inner_blocks)):
-        if inner_blocks[i][0] != '#':
-            inner_blocks[i] = '##' + inner_blocks[i]
-    inner_blocks=''.join(inner_blocks)
+        
+atom_template = textwrap.dedent('''\
+    <?xml version='1.0' encoding='utf-8'?>
+    <feed xmlns='http://www.w3.org/2005/Atom'>
+        <link rel='self' href='http://rg3.github.io/youtube-dl/update/releases.atom' />
+        <title>youtube-dl releases</title>
+        <id>https://yt-dl.org/feed/youtube-dl-updates-feed</id>
+        <updated>@TIMESTAMP@</updated>
+        @ENTRIES@
+    </feed>''')
     
-        Example: adding tri-gram
-    >>> sequences = [[1, 3, 4, 5], [1, 3, 7, 9, 2]]
-    >>> token_indice = {(1, 3): 1337, (9, 2): 42, (4, 5): 2017, (7, 9, 2): 2018}
-    >>> add_ngram(sequences, token_indice, ngram_range=3)
-    [[1, 3, 4, 5, 1337], [1, 3, 7, 9, 2, 1337, 2018]]
+    import shutil
+    
+            self.port = random.randint(20000, 30000)
+        self.server_process = subprocess.Popen([
+            'srelay', '-f', '-i', '127.0.0.1:%d' % self.port],
+            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    
+class RtspFD(FileDownloader):
+    def real_download(self, filename, info_dict):
+        url = info_dict['url']
+        self.report_destination(filename)
+        tmpfilename = self.temp_name(filename)
+    
+    
+def gen_extractor_classes():
+    ''' Return a list of supported extractors.
+    The order does matter; the first extractor matched is the one handling the URL.
     '''
-    new_sequences = []
-    for input_list in sequences:
-        new_list = input_list[:]
-        for i in range(len(new_list) - ngram_range + 1):
-            for ngram_value in range(2, ngram_range + 1):
-                ngram = tuple(new_list[i:i + ngram_value])
-                if ngram in token_indice:
-                    new_list.append(token_indice[ngram])
-        new_sequences.append(new_list)
+    return _ALL_CLASSES
     
-        for data_format in ['channels_first', 'channels_last']:
-        if data_format == 'channels_first':
-            shape = (3, 5, 5)
-            target_shape = (5, 5, 3)
-            prev_shape = (2, 3, 2)
-            flip = lambda x: np.flip(np.flip(x, axis=2), axis=3)
-            transpose = lambda x: np.transpose(x, (0, 2, 3, 1))
-            target_data_format = 'channels_last'
-        elif data_format == 'channels_last':
-            shape = (5, 5, 3)
-            target_shape = (3, 5, 5)
-            prev_shape = (2, 2, 3)
-            flip = lambda x: np.flip(np.flip(x, axis=1), axis=2)
-            transpose = lambda x: np.transpose(x, (0, 3, 1, 2))
-            target_data_format = 'channels_first'
+            return {
+            '_type': 'playlist',
+            'id': playlist_id,
+            'title': title,
+            'description': description,
+            'entries': entries,
+        }
+
+    
+        h = history.history
+    assert_array_almost_equal(h['val_loss'], h['val_' + loss_full_name], decimal=decimal)
+    assert_array_almost_equal(h['val_loss'], h['val_weighted_' + loss_full_name], decimal=decimal)
+    
+        # by setting the `trainable` argument, in Model
+    x = Input(shape=(1,))
+    layer = Dense(2)
+    y = layer(x)
+    model = Model(x, y)
+    assert model.trainable_weights == layer.trainable_weights
+    layer.trainable = False
+    assert model.trainable_weights == []
+    
+    
+def style_loss(style, combination):
+    assert K.ndim(style) == 3
+    assert K.ndim(combination) == 3
+    S = gram_matrix(style)
+    C = gram_matrix(combination)
+    channels = 3
+    size = img_nrows * img_ncols
+    return K.sum(K.square(S - C)) / (4. * (channels ** 2) * (size ** 2))
     
     
 def get_example_array():
@@ -46,88 +71,152 @@ def get_example_array():
     example_array[0, 0] = 0.  # 0 could possibly cause trouble
     return example_array
     
-        # Transform sequences and labels into 'one-hot' encoding
-    x = np.zeros((len(sentences), sequence_length, number_of_chars), dtype=np.bool)
-    y = np.zeros((len(sentences), number_of_chars), dtype=np.bool)
-    for i, sentence in enumerate(sentences):
-        for t, char in enumerate(sentence):
-            x[i, t, ord(char) - ord('a')] = 1
-        y[i, ord(next_chars[i]) - ord('a')] = 1
     
-            config = model.get_config()
-        model = models.Model.from_config(config)
-        model.compile('rmsprop', 'mse')
+@keras_test
+def test_maxout_dense():
+    layer_test(legacy_layers.MaxoutDense,
+               kwargs={'output_dim': 3},
+               input_shape=(3, 2))
     
-            labels = np.ones([size, self.absolute_max_string_len])
-        input_length = np.zeros([size, 1])
-        label_length = np.zeros([size, 1])
-        source_str = []
-        for i in range(size):
-            # Mix in some blank inputs.  This seems to be important for
-            # achieving translational invariance
-            if train and i > size - 4:
-                if K.image_data_format() == 'channels_first':
-                    X_data[i, 0, 0:self.img_w, :] = self.paint_func('')[0, :, :].T
-                else:
-                    X_data[i, 0:self.img_w, :, 0] = self.paint_func('',)[0, :, :].T
-                labels[i, 0] = self.blank_label
-                input_length[i] = self.img_w // self.downsample_factor - 2
-                label_length[i] = 1
-                source_str.append('')
+        # 4. Anything else is not a valid argument
+    with pytest.raises(ValueError):
+        a = activations.get(6)
+    
+    
+if __name__ == '__main__':
+    pytest.main([__file__])
+
+    
+        left = Sequential()
+    left.add(Dense(num_hidden, input_shape=(input_dim,)))
+    left.add(Activation('relu'))
+    
+        return a
+    
+        print('Classifier Training')
+    print('===================')
+    accuracy, train_time, test_time = {}, {}, {}
+    for name in sorted(args['estimators']):
+        clf = ESTIMATORS[name]
+        try:
+            clf.set_params(random_state=0)
+        except (TypeError, ValueError):
+            pass
+    
+    
+def plot_batch_errors(all_errors, n_features, all_batch_sizes, data):
+    plt.figure()
+    plot_results(all_batch_sizes, all_errors['pca'], label='PCA')
+    plot_results(all_batch_sizes, all_errors['ipca'], label='IncrementalPCA')
+    plt.legend(loc='lower left')
+    plt.suptitle('Algorithm error vs. batch_size for n_components %i\n \
+                 LFW, size %i x %i' % (
+                 n_features, data.shape[0], data.shape[1]))
+    plt.xlabel('Batch size')
+    plt.ylabel('Mean absolute error')
+    
+                gc.collect()
+            print('benchmarking lars_path (without Gram):', end='')
+            sys.stdout.flush()
+            tstart = time()
+            lars_path(X, y, method='lasso')
+            delta = time() - tstart
+            print('%0.3fs' % delta)
+            results['lars_path (without Gram)'].append(delta)
+    
+        for (sbplt, vals, quantity,
+         build_time, query_time) in [(311, Nrange, 'N',
+                                      N_results_build,
+                                      N_results_query),
+                                     (312, Drange, 'D',
+                                      D_results_build,
+                                      D_results_query),
+                                     (313, krange, 'k',
+                                      k_results_build,
+                                      k_results_query)]:
+        ax = plt.subplot(sbplt, yscale='log')
+        plt.grid(True)
+    
+    
+def _get_git_revision():
+    try:
+        revision = subprocess.check_output(REVISION_CMD.split()).strip()
+    except (subprocess.CalledProcessError, OSError):
+        print('Failed to execute git to get revision')
+        return None
+    return revision.decode('utf-8')
+    
+    data, row_idx, col_idx = sg._shuffle(data, random_state=0)
+plt.matshow(data, cmap=plt.cm.Blues)
+plt.title('Shuffled dataset')
+    
+    features_samples_ratio = np.array(n_features_range) / n_train
+    
+    
+class Call(object):
+    
+        def create_signature(self):
+        # Create signature based on url and contents
+        pass
+    
+    def kuwo_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+    if 'www.kuwo.cn/yinyue' in url:
+        rid=match1(url,'yinyue/(\d+)')
+        kuwo_download_by_rid(rid,output_dir, merge, info_only)
+    else:
+        kuwo_playlist_download(url,output_dir,merge,info_only)
+    
+    #----------------------------------------------------------------------
+def sina_xml_to_url_list(xml_data):
+    '''str->list
+    Convert XML to URL List.
+    From Biligrab.
+    '''
+    rawurl = []
+    dom = parseString(xml_data)
+    for node in dom.getElementsByTagName('durl'):
+        url = node.getElementsByTagName('url')[0]
+        rawurl.append(url.childNodes[0].data)
+    return rawurl
+    
+    
+class Waker(interface.Waker):
+    '''Create an OS independent asynchronous pipe.
+    
+            auth_header = self.request.headers.get('Authorization', None)
+        if auth_header is not None:
+            auth_mode, params = auth_header.split(' ', 1)
+            assert auth_mode == 'Digest'
+            param_dict = {}
+            for pair in params.split(','):
+                k, v = pair.strip().split('=', 1)
+                if v[0] == ''' and v[-1] == ''':
+                    v = v[1:-1]
+                param_dict[k] = v
+            assert param_dict['realm'] == realm
+            assert param_dict['opaque'] == opaque
+            assert param_dict['nonce'] == nonce
+            assert param_dict['username'] == username
+            assert param_dict['uri'] == self.request.path
+            h1 = md5(utf8('%s:%s:%s' % (username, realm, password))).hexdigest()
+            h2 = md5(utf8('%s:%s' % (self.request.method,
+                                     self.request.path))).hexdigest()
+            digest = md5(utf8('%s:%s:%s' % (h1, nonce, h2))).hexdigest()
+            if digest == param_dict['response']:
+                self.write('ok')
             else:
-                if K.image_data_format() == 'channels_first':
-                    X_data[i, 0, 0:self.img_w, :] = self.paint_func(self.X_text[index + i])[0, :, :].T
-                else:
-                    X_data[i, 0:self.img_w, :, 0] = self.paint_func(self.X_text[index + i])[0, :, :].T
-                labels[i, :] = self.Y_data[index + i]
-                input_length[i] = self.img_w // self.downsample_factor - 2
-                label_length[i] = self.Y_len[index + i]
-                source_str.append(self.X_text[index + i])
-        inputs = {'the_input': X_data,
-                  'the_labels': labels,
-                  'input_length': input_length,
-                  'label_length': label_length,
-                  'source_str': source_str  # used for visualization only
-                  }
-        outputs = {'ctc': np.zeros([size])}  # dummy data for dummy loss function
-        return (inputs, outputs)
+                self.write('fail')
+        else:
+            self.set_status(401)
+            self.set_header('WWW-Authenticate',
+                            'Digest realm='%s', nonce='%s', opaque='%s'' %
+                            (realm, nonce, opaque))
     
-        def contribute(self):
-        self.blackboard.common_state['problems'] += random.randint(1, 2)
-        self.blackboard.common_state['suggestions'] += random.randint(10, 20)
-        self.blackboard.common_state['contributions'] += [self.__class__.__name__]
-        self.blackboard.common_state['progress'] += random.randint(10, 100)
+            # Prevent 'Future exception was never retrieved' messages.
+        for future in futures:
+            self.assertRaises(TimeoutError, future.result)
     
-        def test_sequential_execution(self):
-        self.command_stack[0].execute()
-        output_after_first_execution = os.listdir(self.test_dir)
-        self.assertEqual(output_after_first_execution[0], 'bar.txt')
-        self.command_stack[1].execute()
-        output_after_second_execution = os.listdir(self.test_dir)
-        self.assertEqual(output_after_second_execution[0], 'baz.txt')
+            yield [connect(c) for c in clients]
     
-        def __init__(self, msg_center):
-        self.provider = msg_center
-    
-        def __new__(cls, name, bases, attrs):
-        new_cls = type.__new__(cls, name, bases, attrs)
-        '''
-            Here the name of the class is used as key but it could be any class
-            parameter.
-        '''
-        cls.REGISTRY[new_cls.__name__] = new_cls
-        return new_cls
-    
-    
-class TestLocalizer(unittest.TestCase):
-    
-        def get_current_time_as_html_fragment(self):
-        current_time = self.time_provider.now()
-        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
-        return current_time_as_html_fragment
-'''
-    
-        @Transactional
-    def do_stuff(self):
-        self.value = '1111'  # <- invalid value
-        self.increment()  # <- will fail and rollback
+        def _log(self):
+        pass
