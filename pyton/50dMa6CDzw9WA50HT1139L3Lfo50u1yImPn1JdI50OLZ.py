27c7ee0d880b7c77a1ac62852b6e0423ef0d1eab
SUNsung <weatherius@gmail.com>
2018-03-15 10:58:55 +0200

    Rewrite [50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py] File in: 2018-03-15 10:58:54

--- pyton/50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py
+++ pyton/50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py
@@ -1,37 +1,119 @@
 
         
-            app.config.update(dict(
-        DATABASE=os.path.join(app.root_path, 'flaskr.db'),
-        DEBUG=True,
-        SECRET_KEY=b'_5#y2L'F4Q8z\n\xec]/',
-        USERNAME='admin',
-        PASSWORD='default'
-    ))
-    app.config.update(config or {})
-    app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+        faces = fetch_lfw_people(resize=.2, min_faces_per_person=5)
+# limit dataset to 5000 people (don't care who they are!)
+X = faces.data[:5000]
+n_samples, h, w = faces.images.shape
+n_features = X.shape[1]
     
-        if address_family == socket.AF_INET:
-        if len(packed_ip) != ctypes.sizeof(addr.ipv4_addr):
-            raise socket.error('packed IP wrong length for inet_ntoa')
-        ctypes.memmove(addr.ipv4_addr, packed_ip, 4)
-    elif address_family == socket.AF_INET6:
-        if len(packed_ip) != ctypes.sizeof(addr.ipv6_addr):
-            raise socket.error('packed IP wrong length for inet_ntoa')
-        ctypes.memmove(addr.ipv6_addr, packed_ip, 16)
-    else:
-        raise socket.error('unknown address family')
+    def plot(func):
+    random_state = check_random_state(0)
+    one_core = []
+    multi_core = []
+    sample_sizes = range(1000, 6000, 1000)
     
-            time_last_read = time.time()
-        while True:
-            if start > end:
-                https_manager.save_ssl_connection_for_reuse(response.ssl_sock, host, call_time=time_request)
-                xlog.info('DIRECT t:%d s:%d %d %s %s', (time.time()-time_request)*1000, length, response.status, host, url)
-                return
+        ###########################################################################
+    # Set SparseRandomProjection input
+    sparse_matrix_params = {
+        'n_components': opts.n_components,
+        'random_state': opts.random_seed,
+        'density': opts.density,
+        'eps': opts.eps,
+    }
+    
+        ###########################################################################
+    # Set custom reservoir based method
+    sampling_algorithm['custom-pool'] = \
+        lambda n_population, n_samples, random_state=None: \
+            sample_without_replacement(n_population,
+                                       n_samples,
+                                       method='pool',
+                                       random_state=random_state)
+    
+        An example with a long-untouched module that everyone has
+    >>> _linkcode_resolve('py', {'module': 'tty',
+    ...                          'fullname': 'setraw'},
+    ...                   package='tty',
+    ...                   url_fmt='http://hg.python.org/cpython/file/'
+    ...                           '{revision}/Lib/{package}/{path}#L{lineno}',
+    ...                   revision='xxxx')
+    'http://hg.python.org/cpython/file/xxxx/Lib/tty/tty.py#L18'
+    '''
+    
+    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
+TRAIN_FOLDER = '20news-bydate-train'
+TEST_FOLDER = '20news-bydate-test'
+    
+            if not in_exercise_region or linestrip.startswith('#'):
+            output_file.write(line)
+    
+    import numpy as np
+from sklearn.covariance import EllipticEnvelope
+from sklearn.svm import OneClassSVM
+import matplotlib.pyplot as plt
+import matplotlib.font_manager
+from sklearn.datasets import load_boston
     
     
-    def seek(self, index):
-        '''
-        Set the input cursor to the position indicated by index.  This is
-        normally used to seek ahead in the input stream.  No buffering is
-        required to do this unless you know your stream will use seek to
-        move backwards such as when backtracking.
+def plot_calibration_curve(est, name, fig_index):
+    '''Plot calibration curve for est w/o and with calibration. '''
+    # Calibrated with isotonic calibration
+    isotonic = CalibratedClassifierCV(est, cv=2, method='isotonic')
+    
+    _help = '''Usage: {} [OPTION]... [URL]...
+TODO
+'''.format(script_name)
+    
+    def ehow_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+	
+	assert re.search(r'http://www.ehow.com/video_', url), 'URL you entered is not supported'
+    
+    class MusicPlayOn(VideoExtractor):
+    name = 'MusicPlayOn'
+    
+            # construct available streams
+        if orig_img: self.streams['original'] = {'url': orig_img}
+        if twit_img: self.streams['small'] = {'url': twit_img}
+    
+        #title
+    title = ''
+    profile_api = 'https://www.showroom-live.com/api/room/profile?room_id={room_id}'.format(room_id = room_id)
+    html = loads(get_content(profile_api))
+    try:
+        title = html['main_name']
+    except KeyError:
+        title = 'Showroom_{room_id}'.format(room_id = room_id)
+    
+        #Unless specified otherwise by the user, this is the default colorscheme
+    colorscheme = 'basic'
+    
+    
+class Action(object):
+    
+        def test_frozen_pool(self):
+        with ObjectPool(self.sample_queue) as pool:
+            self.assertEqual(pool, 'first')
+            self.assertEqual(pool, 'first')
+        self.assertTrue(self.sample_queue.get() == 'second')
+        self.assertFalse(self.sample_queue.empty())
+        self.assertTrue(self.sample_queue.get() == 'first')
+        self.assertTrue(self.sample_queue.empty())
+    
+        def tearDown(self):
+        self.out.close()
+        sys.stdout = self.saved_stdout
+    
+    '''
+Port of the Java example of 'Setter Injection' in
+'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
+(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
+http://xunitpatterns.com/Dependency%20Injection.html.
+    
+    __author__ = 'Ibrahim Diop <ibrahim@sikilabs.com>'
+    
+    
+def count_to(count):
+    '''Counts by word numbers, up to a maximum of five'''
+    numbers = ['one', 'two', 'three', 'four', 'five']
+    for number in numbers[:count]:
+        yield number
