73e72e79d2bd71a2b1ea233b6c1598db6d71f3b9
SUNsung <weatherius@gmail.com>
2018-03-15 04:37:31 +0200

    Rewrite [04w4H3TsRI204RDASoQM3HbE04m2stD3oDo6g04OhQ.py] File in: 2018-03-15 04:37:30

--- pyton/04w4H3TsRI204RDASoQM3HbE04m2stD3oDo6g04OhQ.py
+++ pyton/04w4H3TsRI204RDASoQM3HbE04m2stD3oDo6g04OhQ.py
@@ -1,173 +1,161 @@
 
         
-        entries = []
-for v in versions:
-    fields = v.split('.')
-    year, month, day = map(int, fields[:3])
-    faked = 0
-    patchlevel = 0
-    while True:
-        try:
-            datetime.date(year, month, day)
-        except ValueError:
-            day -= 1
-            faked += 1
-            assert day > 0
-            continue
-        break
-    if len(fields) >= 4:
-        try:
-            patchlevel = int(fields[3])
-        except ValueError:
-            patchlevel = 1
-    timestamp = '%04d-%02d-%02dT00:%02d:%02dZ' % (year, month, day, faked, patchlevel)
+        
+def default_hooks():
+    return dict((event, []) for event in HOOKS)
+    
+        :param url: URL for the new :class:`Request` object.
+    :param data: (optional) Dictionary (will be form-encoded), bytes, or file-like object to send in the body of the :class:`Request`.
+    :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    '''
+    
+        # Client Error.
+    400: ('bad_request', 'bad'),
+    401: ('unauthorized',),
+    402: ('payment_required', 'payment'),
+    403: ('forbidden',),
+    404: ('not_found', '-o-'),
+    405: ('method_not_allowed', 'not_allowed'),
+    406: ('not_acceptable',),
+    407: ('proxy_authentication_required', 'proxy_auth', 'proxy_authentication'),
+    408: ('request_timeout', 'timeout'),
+    409: ('conflict',),
+    410: ('gone',),
+    411: ('length_required',),
+    412: ('precondition_failed', 'precondition'),
+    413: ('request_entity_too_large',),
+    414: ('request_uri_too_large',),
+    415: ('unsupported_media_type', 'unsupported_media', 'media_type'),
+    416: ('requested_range_not_satisfiable', 'requested_range', 'range_not_satisfiable'),
+    417: ('expectation_failed',),
+    418: ('im_a_teapot', 'teapot', 'i_am_a_teapot'),
+    421: ('misdirected_request',),
+    422: ('unprocessable_entity', 'unprocessable'),
+    423: ('locked',),
+    424: ('failed_dependency', 'dependency'),
+    425: ('unordered_collection', 'unordered'),
+    426: ('upgrade_required', 'upgrade'),
+    428: ('precondition_required', 'precondition'),
+    429: ('too_many_requests', 'too_many'),
+    431: ('header_fields_too_large', 'fields_too_large'),
+    444: ('no_response', 'none'),
+    449: ('retry_with', 'retry'),
+    450: ('blocked_by_windows_parental_controls', 'parental_controls'),
+    451: ('unavailable_for_legal_reasons', 'legal_reasons'),
+    499: ('client_closed_request',),
+    
+            s = pickle.loads(pickle.dumps(s))
+        s.proxies = getproxies()
+    
+        print('Loading [World Bank's Health Nutrition and Population Stats]')
+    data.load_world_bank_health_n_pop()
+    
+        @property
+    def groupby_column_names(self):
+        return sorted([c.column_name for c in self.columns if c.groupby])
+    
+        @classmethod
+    def query_datasources_by_permissions(cls, session, database, permissions):
+        datasource_class = ConnectorRegistry.sources[database.type]
+        return (
+            session.query(datasource_class)
+            .filter_by(database_id=database.id)
+            .filter(datasource_class.perm.in_(permissions))
+            .all()
+        )
+    
+    appbuilder.add_link(
+    'Scan New Datasources',
+    label=__('Scan New Datasources'),
+    href='/druid/scan_new_datasources/',
+    category='Sources',
+    category_label=__('Sources'),
+    category_icon='fa-database',
+    icon='fa-refresh')
+appbuilder.add_link(
+    'Refresh Druid Metadata',
+    label=__('Refresh Druid Metadata'),
+    href='/druid/refresh_datasources/',
+    category='Sources',
+    category_label=__('Sources'),
+    category_icon='fa-database',
+    icon='fa-cog')
+    
+    naming_convention = {
+    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+}
     
     
-def _download_restricted(url, filename, age):
-    ''' Returns true if the file has been downloaded '''
+def upgrade():
+    op.add_column('metrics', sa.Column('warning_text', sa.Text(), nullable=True))
+    op.add_column('sql_metrics', sa.Column('warning_text', sa.Text(), nullable=True))
     
-    import random
-import subprocess
-    
-    
-class TestUnicodeLiterals(unittest.TestCase):
-    def test_all_files(self):
-        for dirpath, dirnames, filenames in os.walk(rootDir):
-            for ignore_dir in IGNORED_DIRS:
-                if ignore_dir in dirnames:
-                    # If we remove the directory from dirnames os.walk won't
-                    # recurse into it
-                    dirnames.remove(ignore_dir)
-            for basename in filenames:
-                if not basename.endswith('.py'):
-                    continue
-                if basename in IGNORED_FILES:
-                    continue
+        forbidden_extensions = ['html', 'htm'] if results.nohtml else []
     
     
-class AcademicEarthCourseIE(InfoExtractor):
-    _VALID_URL = r'^https?://(?:www\.)?academicearth\.org/playlists/(?P<id>[^?#/]+)'
-    IE_NAME = 'AcademicEarth:Course'
-    _TEST = {
-        'url': 'http://academicearth.org/playlists/laws-of-nature/',
-        'info_dict': {
-            'id': 'laws-of-nature',
-            'title': 'Laws of Nature',
-            'description': 'Introduce yourself to the laws of nature with these free online college lectures from Yale, Harvard, and MIT.',
-        },
-        'playlist_count': 3,
-    }
+def main():
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([
+        (r'/', MainHandler),
+    ])
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
     
-            duration = parse_duration(self._search_regex(
-            r'<b>Duration:</b> (?:<q itemprop='duration'>)?(\d+:\d+)', webpage, 'duration', fatal=False))
-        view_count = int_or_none(self._html_search_regex(
-            r'<b>Views:</b> (\d+)', webpage, 'view count', fatal=False))
-    
-    flags.DEFINE_string('master', '', 'Master address.')
-flags.DEFINE_integer('task', 0, 'Task id of the replica running the training.')
-flags.DEFINE_integer('ps_tasks', 0, 'Number of parameter servers.')
-flags.DEFINE_string('train_dir', '/tmp/text_train',
-                    'Directory for logs and checkpoints.')
-flags.DEFINE_integer('max_steps', 1000000, 'Number of batches to run.')
-flags.DEFINE_boolean('log_device_placement', False,
-                     'Whether to log device placement.')
-    
-    import tensorflow as tf
-    
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-    
-    def my_model(features, labels, mode, params):
-    '''DNN with three hidden layers, and dropout of 0.1 probability.'''
-    # Create three fully connected layers each layer having a dropout
-    # probability of 0.1.
-    net = tf.feature_column.input_layer(features, params['feature_columns'])
-    for units in params['hidden_units']:
-        net = tf.layers.dense(net, units=units, activation=tf.nn.relu)
-    
-        for pred_dict, expec in zip(predictions, expected):
-        template = ('\nPrediction is '{}' ({:.1f}%), expected '{}'')
-    
-        final_timestep = reverse_seq[-1]
-    eos_id = len(seq) - 1
-    self.assertEqual(final_timestep.token, eos_id)
-    self.assertEqual(final_timestep.label, 0)
-    self.assertEqual(final_timestep.weight, 0.0)
-    
-      # Sort by frequency
-  ordered_vocab_freqs = data_utils.sort_vocab_by_frequency(vocab_freqs)
-    
-      Variable reuse is a critical part of the model, both for sharing variables
-  between the language model and the classifier, and for reusing variables for
-  the adversarial loss calculation. To ensure correct variable reuse, all
-  variables are created in Keras-style layers, wherein stateful layers (i.e.
-  layers with variables) are represented as callable instances of the Layer
-  class. Each time the Layer instance is called, it is using the same variables.
-    
-        def decode(self, x, calc_argmax=True):
-        if calc_argmax:
-            x = x.argmax(axis=-1)
-        return ''.join(self.indices_char[x] for x in x)
-    
-    # we start off with an efficient embedding layer which maps
-# our vocab indices into embedding_dims dimensions
-model.add(Embedding(max_features,
-                    embedding_dims,
-                    input_length=maxlen))
+        @gen.coroutine
+    def resolve(self, host, port, family=0):
+        if is_valid_ip(host):
+            addresses = [host]
+        else:
+            # gethostbyname doesn't take callback as a kwarg
+            self.channel.gethostbyname(host, family, (yield gen.Callback(1)))
+            callback_args = yield gen.Wait(1)
+            assert isinstance(callback_args, gen.Arguments)
+            assert not callback_args.kwargs
+            result, error = callback_args.args
+            if error:
+                raise IOError('C-Ares returned error %s: %s while resolving %s' %
+                              (error, pycares.errno.strerror(error), host))
+            addresses = result.addresses
+        addrinfo = []
+        for address in addresses:
+            if '.' in address:
+                address_family = socket.AF_INET
+            elif ':' in address:
+                address_family = socket.AF_INET6
+            else:
+                address_family = socket.AF_UNSPEC
+            if family != socket.AF_UNSPEC and family != address_family:
+                raise IOError('Requested socket family %d but got %d' %
+                              (family, address_family))
+            addrinfo.append((address_family, (address, port)))
+        raise gen.Return(addrinfo)
+
     
     
-@keras_test
-def test_masking():
-    np.random.seed(1337)
-    x = np.array([[[1], [1]],
-                  [[0], [0]]])
-    model = Sequential()
-    model.add(Masking(mask_value=0, input_shape=(2, 1)))
-    model.add(TimeDistributed(Dense(1, kernel_initializer='one')))
-    model.compile(loss='mse', optimizer='sgd')
-    y = np.array([[[1], [1]],
-                  [[1], [1]]])
-    loss = model.train_on_batch(x, y)
-    assert loss == 0
+MAIN = '''\
+import os
+import sys
     
+        def test_failed_setup(self):
+        self.http_client = self.create_client(max_clients=1)
+        for i in range(5):
+            response = self.fetch(u'/ユニコード')
+            self.assertIsNot(response.error, None)
+
     
-@pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
-def test_he_normal(tensor_shape):
-    fan_in, _ = initializers._compute_fans(tensor_shape)
-    scale = np.sqrt(2. / fan_in)
-    _runner(initializers.he_normal(), tensor_shape,
-            target_mean=0., target_std=None, target_max=2 * scale)
+    # Increasing --n without --keepalive will eventually run into problems
+# due to TIME_WAIT sockets
+define('n', type=int, default=15000)
+define('c', type=int, default=25)
+define('keepalive', type=bool, default=False)
+define('quiet', type=bool, default=False)
     
-            assert_allclose(out1, out2, atol=1e-5)
-    
-    # add the match matrix with the second input vector sequence
-response = add([match, input_encoded_c])  # (samples, story_maxlen, query_maxlen)
-response = Permute((2, 1))(response)  # (samples, query_maxlen, story_maxlen)
-    
-    print('Vectorization...')
-x = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
-y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
-for i, sentence in enumerate(sentences):
-    for t, char in enumerate(sentence):
-        x[i, t, char_indices[char]] = 1
-    y[i, char_indices[next_chars[i]]] = 1
-    
-            (2016-01, url0), 2
-        (2016-01, url1), 1
-        '''
-        yield key, sum(values)
-    
-        def reducer(self, key, value):
-        '''Sum values for each key.
-    
-        def is_face_card(self):
-        '''Jack = 11, Queen = 12, King = 13'''
-        return True if 10 < self._value <= 13 else False
-    
-        def __init__(self):
-        self.head = None
-        self.tail = None
-    
-      Args:
-    raw_lines: list of raw lines.
+        logging.warning('Starting fetch with curl client')
+    curl_client = CurlAsyncHTTPClient()
+    curl_client.fetch('http://localhost:%d/' % options.port,
+                      callback=callback)
+    IOLoop.current().start()
