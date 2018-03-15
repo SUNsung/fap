3dba677cc4d6f6013e5d6c33181787a79a095c3b
SUNsung <weatherius@gmail.com>
2018-03-15 06:29:30 +0200

    Rewrite [331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py] File in: 2018-03-15 06:29:30

--- pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
+++ pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
@@ -1,139 +1,171 @@
 
         
-            if member.__doc__ is None and not member_too_small(member):
-        raise ValueError('{} class doesn't have any documentation'.format(name),
-                         member.__module__, inspect.getmodule(member).__file__)
-    for n, met in inspect.getmembers(member):
-        if inspect.ismethod(met):
-            handle_method(n, met)
+        containers = (('thefuck/python3-fish',
+               u'''FROM python:3
+                   # Use jessie-backports since it has the fish package. See here for details:
+                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
+                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
+                   RUN apt-get update
+                   RUN apt-get install -yy fish''',
+               u'fish'),
+              ('thefuck/python2-fish',
+               u'''FROM python:2
+                   # Use jessie-backports since it has the fish package. See here for details:
+                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
+                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
+                   RUN apt-get update
+                   RUN apt-get install -yy fish''',
+               u'fish'))
     
-        history = model.fit(x, ys, validation_split=0.05, batch_size=10,
-                        verbose=0, epochs=3)
-    ground_truth = -np.log(0.5)
-    assert(np.abs(history.history['loss'][-1] - ground_truth) < 0.06)
+    # If true, '()' will be appended to :func: etc. cross-reference text.
+#add_function_parentheses = True
     
-        assert np.all(model.predict(inputs) == output)
     
-        @classmethod
-    def get_datasource(cls, datasource_type, datasource_id, session):
-        return (
-            session.query(cls.sources[datasource_type])
-            .filter_by(id=datasource_id)
-            .first()
+@app.route('/<username>/follow')
+def follow_user(username):
+    '''Adds the current user as follower of the given user.'''
+    if not g.user:
+        abort(401)
+    whom_id = get_user_id(username)
+    if whom_id is None:
+        abort(404)
+    db = get_db()
+    db.execute('insert into follower (who_id, whom_id) values (?, ?)',
+              [session['user_id'], whom_id])
+    db.commit()
+    flash('You are now following '%s'' % username)
+    return redirect(url_for('user_timeline', username=username))
+    
+    if hasattr(ctypes, 'windll'):
+    WSAStringToAddressA = ctypes.windll.ws2_32.WSAStringToAddressA
+    WSAAddressToStringA = ctypes.windll.ws2_32.WSAAddressToStringA
+else:
+    def not_windows():
+        raise SystemError(
+            'Invalid platform. ctypes.windll must be available.'
         )
+    WSAStringToAddressA = not_windows
+    WSAAddressToStringA = not_windows
     
-            logging.info('Importing %d %s',
-                     len(data.get(DRUID_CLUSTERS_KEY, [])),
-                     DRUID_CLUSTERS_KEY)
-        for datasource in data.get(DRUID_CLUSTERS_KEY, []):
-            DruidCluster.import_from_dict(session, datasource, sync=sync)
-        session.commit()
-    else:
-        logging.info('Supplied object is not a dictionary.')
+    @mainpage
+    
+    _regexp_compile_cache = {}
+    
+    # Can't import these from paths.py because that uses `future` imports
+DIR_OF_CURRENT_SCRIPT = os.path.dirname( os.path.abspath( __file__ ) )
+DIR_OF_YCMD = os.path.join( DIR_OF_CURRENT_SCRIPT, '..', '..', 'third_party',
+                            'ycmd' )
+    
+            Args:
+            max_workers: The maximum number of threads that can be used to
+                execute the given calls.
+        '''
+        self._max_workers = max_workers
+        self._work_queue = queue.Queue()
+        self._threads = set()
+        self._shutdown = False
+        self._shutdown_lock = threading.Lock()
+    
+    _system_limits_checked = False
+_system_limited = None
+def _check_system_limits():
+    global _system_limits_checked, _system_limited
+    if _system_limits_checked:
+        if _system_limited:
+            raise NotImplementedError(_system_limited)
+    _system_limits_checked = True
+    try:
+        import os
+        nsems_max = os.sysconf('SC_SEM_NSEMS_MAX')
+    except (AttributeError, ValueError):
+        # sysconf not available or setting not available
+        return
+    if nsems_max == -1:
+        # indetermine limit, assume that limit is determined
+        # by available memory only
+        return
+    if nsems_max >= 256:
+        # minimum number of semaphores available
+        # according to POSIX
+        return
+    _system_limited = 'system provides too few semaphores (%d available, 256 necessary)' % nsems_max
+    raise NotImplementedError(_system_limited)
+    
+            for future in as_completed(future_to_url):
+            try:
+                url_to_content[future_to_url[future]] = future.result()
+            except:
+                pass
+        return url_to_content
+    finally:
+        executor.shutdown()
+    
+    # Output file base name for HTML help builder.
+htmlhelp_basename = 'futuresdoc'
+    
+    
+class BaseRequest( object ):
+    
+    
+def ExtractKeywordsFromGroup_ContainedSyntaxArgAllowed_test():
+  assert_that( syntax_parse._ExtractKeywordsFromGroup(
+                 syntax_parse.SyntaxGroup( '', [
+                   'contained foo zoq',
+                   'contained bar goo',
+                   'far'
+                 ] ) ),
+               contains_inanyorder( 'foo', 'zoq', 'bar', 'goo', 'far' ) )
 
     
-        def __init__(self, database=None, query=None, table=None, **kwargs):
-        self.database = database
-        self.query = query
-        self.schema = None
-        if query and query.schema:
-            self.schema = query.schema
-        elif table:
-            self.schema = table.schema
-        self.context = {
-            'url_param': url_param,
-            'current_user_id': current_user_id,
-            'current_username': current_username,
-            'form_data': {},
-        }
-        self.context.update(kwargs)
-        self.context.update(BASE_CONTEXT)
-        if self.engine:
-            self.context[self.engine] = self
-        self.env = SandboxedEnvironment()
+        @YouCompleteMeInstance( { 'log_level': 'debug',
+                              'keep_logfiles': 1 } )
+    def Debug_test( ycm ):
+        ...
+  '''
+  def Decorator( test ):
+    @functools.wraps( test )
+    def Wrapper( *args, **kwargs ):
+      ycm = YouCompleteMe( MakeUserOptions( custom_options ) )
+      WaitUntilReady()
+      ycm.CheckIfServerIsReady()
+      try:
+        test( ycm, *args, **kwargs )
+      finally:
+        StopServer( ycm )
+    return Wrapper
+  return Decorator
+
     
     
-def cast_filter_data(form_data):
-    '''Used by cast_form_data to parse the filters'''
-    flts = []
-    having_flts = []
-    fd = form_data
-    filter_pattern = re.compile(r'''((?:[^,'']|'[^']*'|'[^']*')+)''')
-    for i in range(0, 10):
-        for prefix in ['flt', 'having']:
-            col_str = '{}_col_{}'.format(prefix, i)
-            op_str = '{}_op_{}'.format(prefix, i)
-            val_str = '{}_eq_{}'.format(prefix, i)
-            if col_str in fd and op_str in fd and val_str in fd \
-               and len(fd[val_str]) > 0:
-                f = {}
-                f['col'] = fd[col_str]
-                f['op'] = fd[op_str]
-                if prefix == 'flt':
-                    # transfer old strings in filter value to list
-                    splitted = filter_pattern.split(fd[val_str])[1::2]
-                    values = [types.replace(''', '').strip() for types in splitted]
-                    f['val'] = values
-                    flts.append(f)
-                if prefix == 'having':
-                    f['val'] = fd[val_str]
-                    having_flts.append(f)
-            if col_str in fd:
-                del fd[col_str]
-            if op_str in fd:
-                del fd[op_str]
-            if val_str in fd:
-                del fd[val_str]
-    fd['filters'] = flts
-    fd['having_filters'] = having_flts
-    return fd
+@wsgi_safe
+class NamedURLSpecGroupsTest(WebTestCase):
+    def get_handlers(self):
+        class EchoHandler(RequestHandler):
+            def get(self, path):
+                self.write(path)
+    
+        @gen_test
+    def test_get_timeout_preempted(self):
+        q = queues.Queue()
+        get = q.get(timeout=timedelta(seconds=0.01))
+        q.put(0)
+        yield gen.sleep(0.02)
+        self.assertEqual(0, (yield get))
+    
+        def get_app(self):
+        return WSGIContainer(validator(self.wsgi_app))
     
     
-def find_constraint_name(upgrade=True):
-    cols = {'column_name'} if upgrade else {'datasource_name'}
-    return generic_find_constraint_name(
-        table='columns', columns=cols, referenced='datasources', db=db)
-    
-        def __init__(self, parent, title, message, used_names,
-                 *, _htest=False, _utest=False):
-        super().__init__(parent, title, message, used_names=used_names,
-                         _htest=_htest, _utest=_utest)
-    
-        >>> mode(['red', 'blue', 'blue', 'red', 'green', 'red', 'red'])
-    'red'
-    
-            for proto in range(pickle.HIGHEST_PROTOCOL + 1):
-            s = pickle.dumps(d, proto)
-            e = pickle.loads(s)
-            self.assertNotEqual(id(d), id(e))
-            self.assertEqual(type(d), type(e))
-            self.assertEqual(list(d), list(e))
-    
-    def test():
-    for i in range(256):
-        c = chr(i)
-        s = repr(c)
-        e = evalString(s)
-        if e != c:
-            print(i, c, s, e)
-    
-        print('LIBS=', end=' ')
-    for lib in libs: print(''%s'' % (lib), end=' ')
-    print() ; print()
-    
-        def find_byname(self, name):
-        '''Return (red, green, blue) for name'''
-        name = name.lower()
-        try:
-            return self.__byname[name]
-        except KeyError:
-            raise BadColor(name) from None
-    
-            # Misc. other alternate tests
-        self.assertEqual(format((-1.5+0.5j), '#f'), '-1.500000+0.500000j')
-        self.assertEqual(format((-1.5+0.5j), '#.0f'), '-2.+0.j')
-        self.assertEqual(format((-1.5+0.5j), '#e'), '-1.500000e+00+5.000000e-01j')
-        self.assertEqual(format((-1.5+0.5j), '#.0e'), '-2.e+00+5.e-01j')
-        self.assertEqual(format((-1.5+0.5j), '#g'), '-1.50000+0.500000j')
-        self.assertEqual(format((-1.5+0.5j), '.0g'), '-2+0.5j')
-        self.assertEqual(format((-1.5+0.5j), '#.0g'), '-2.+0.5j')
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r'/', MainHandler),
+            (r'/chatsocket', ChatSocketHandler),
+        ]
+        settings = dict(
+            cookie_secret='__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__',
+            template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+            static_path=os.path.join(os.path.dirname(__file__), 'static'),
+            xsrf_cookies=True,
+        )
+        super(Application, self).__init__(handlers, **settings)
