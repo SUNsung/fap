596542b4426309f83137e13dfbc782aa0b41fdc6
SUNsung <weatherius@gmail.com>
2018-03-16 22:33:23 +0200

    Rewrite [35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py] File in: 2018-03-16 22:33:23

--- pyton/35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py
+++ pyton/35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py
@@ -1,130 +1,178 @@
 
         
-        containers = (('thefuck/python3-tcsh',
-               u'''FROM python:3
-                   RUN apt-get update
-                   RUN apt-get install -yy tcsh''',
-               u'tcsh'),
-              ('thefuck/python2-tcsh',
-               u'''FROM python:2
-                   RUN apt-get update
-                   RUN apt-get install -yy tcsh''',
-               u'tcsh'))
+        # TODO: ensure that history changes.
+
+    
+        r = client.get('/hello')
+    assert r.status_code == 200
     
     
-def check_format(filename):
-    '''
-    validates that each line is formatted correctly,
-    appending to error list as needed
-    '''
-    with open(filename) as fp:
-        lines = list(line.rstrip() for line in fp)
-    check_alphabetical(lines)
-    # START Check Entries
-    num_in_category = min_entries_per_section + 1
-    category = ''
-    category_line = 0
-    anchor_re = re.compile('###\s\S+')
-    for line_num, line in enumerate(lines):
-        # check each section for the minimum number of entries
-        if line.startswith(anchor):
-            if not anchor_re.match(line):
-                add_error(line_num, 'section header is not formatted correctly')
-            if num_in_category < min_entries_per_section:
-                add_error(category_line, '{} section does not have the minimum {} entries (only has {})'.format(
-                    category, min_entries_per_section, num_in_category))
-            category = line.split(' ')[1]
-            category_line = line_num
-            num_in_category = 0
-            continue
-        if not line.startswith('|') or line.startswith('|---'):
-            continue
-        num_in_category += 1
-        segments = line.split('|')[1:-1]
-        # START Global
-        for segment in segments:
-            # every line segment should start and end with exactly 1 space
-            if len(segment) - len(segment.lstrip()) != 1 or len(segment) - len(segment.rstrip()) != 1:
-                add_error(line_num, 'each segment must start and end with exactly 1 space')
-        # END Global
-        segments = [seg.strip() for seg in segments]
-        check_entry(line_num, segments)
-    # END Check Entries
+@keras_test
+def test_sequential_sample_weights():
+    model = create_sequential_model()
+    model.compile(loss=loss, optimizer='rmsprop')
+    
+        reference = Sequential()
+    reference.add(wrappers.TimeDistributed(layers.Embedding(5, 6),
+                                           input_shape=(3, 4), dtype='int32'))
+    reference.compile(optimizer='rmsprop', loss='mse')
+    reference.layers[0].set_weights(weights)
+    
+        # a Model inside a Model
+    x = Input(shape=(1,))
+    y = Dense(2)(x)
+    inner_model = Model(x, y)
+    x = Input(shape=(1,))
+    y = inner_model(x)
+    outer_model = Model(x, y)
+    assert outer_model.trainable_weights == inner_model.trainable_weights
+    inner_model.trainable = False
+    assert outer_model.trainable_weights == []
+    inner_model.trainable = True
+    inner_model.layers[-1].trainable = False
+    assert outer_model.trainable_weights == []
     
     
-def jsonify(result, format=False):
-    ''' format JSON output (uncompressed or uncompressed) '''
+@keras_test
+def test_masking():
+    np.random.seed(1337)
+    x = np.array([[[1], [1]],
+                  [[0], [0]]])
+    model = Sequential()
+    model.add(Masking(mask_value=0, input_shape=(2, 1)))
+    model.add(TimeDistributed(Dense(1, kernel_initializer='one')))
+    model.compile(loss='mse', optimizer='sgd')
+    y = np.array([[[1], [1]],
+                  [[1], [1]]])
+    loss = model.train_on_batch(x, y)
+    assert loss == 0
+    
+        # prime the model with 'ab' sequence and let it generate the learned alphabet
+    sentence = alphabet[:sequence_length]
+    generated = sentence
+    for iteration in range(number_of_chars - sequence_length):
+        x = np.zeros((1, sequence_length, number_of_chars))
+        for t, char in enumerate(sentence):
+            x[0, t, ord(char) - ord('a')] = 1.
+        preds = model.predict(x, verbose=0)[0]
+        next_char = chr(np.argmax(preds) + ord('a'))
+        generated += next_char
+        sentence = sentence[1:] + next_char
+    
+        # Arguments
+        config: dict of the form {'class_name': str, 'config': dict}
+        custom_objects: dict mapping class names (or function names)
+            of custom (non-Keras) objects to class/functions
+    
+        right = Sequential(name='branch_2')
+    right.add(Dense(num_hidden, input_shape=(input_dim,), name='dense_2'))
+    right.add(Activation('relu', name='relu_2'))
+    
+            self.assertTrue(self.addr_default.conflicts(self.addr))
+        self.assertTrue(self.addr_default.conflicts(self.addr1))
+        self.assertTrue(self.addr_default.conflicts(self.addr_defined))
+    
+        def __init__(self, args):
+        '''Initializes the plugin with the given command line args'''
+        self._temp_dir = tempfile.mkdtemp()
+        self.le_config = util.create_le_config(self._temp_dir)
+        config_dir = util.extract_configs(args.configs, self._temp_dir)
+        self._configs = [
+            os.path.join(config_dir, config)
+            for config in os.listdir(config_dir)]
+    
+        def tearDown(self):
+        logging.disable(logging.NOTSET)
+    
+        expect = textwrap.dedent('''\
+    [
+        [
+            'blorpie'
+        ],
+        [
+            'whoops'
+        ],
+        [],
+        'd-shtaeou',
+        'd-nthiouh',
+        'i-vhbjkhnth',
+        {
+            'nifty': 87
+        },
+        {
+            'morefield': false,
+            'field': 'yes'
+        }
+    ]
+    ''')
     
     
+    def test_attributes(self):
+        # test the start, stop and step attributes of range objects
+        self.assert_attrs(range(0), 0, 0, 1)
+        self.assert_attrs(range(10), 0, 10, 1)
+        self.assert_attrs(range(-10), 0, -10, 1)
+        self.assert_attrs(range(0, 10, 1), 0, 10, 1)
+        self.assert_attrs(range(0, 10, 3), 0, 10, 3)
+        self.assert_attrs(range(10, 0, -1), 10, 0, -1)
+        self.assert_attrs(range(10, 0, -3), 10, 0, -3)
     
-        def reducer(self, key, value):
-        '''Sum values for each key.
+            # COM methods are boolean True:
+        if sys.platform == 'win32':
+            mth = WINFUNCTYPE(None)(42, 'name', (), None)
+            self.assertEqual(bool(mth), True)
     
-        def __init__(self, MAX_SIZE):
-        self.MAX_SIZE = MAX_SIZE
-        self.size = 0
-        self.lookup = {}  # key: query, value: node
-        self.linked_list = LinkedList()
+        @gen.coroutine
+    def resolve(self, host, port, family=0):
+        if is_valid_ip(host):
+            addresses = [host]
+        else:
+            # gethostbyname doesn't take callback as a kwarg
+            self.channel.gethostbyname(host, family, (yield gen.Callback(1)))
+            callback_args = yield gen.Wait(1)
+            assert isinstance(callback_args, gen.Arguments)
+            assert not callback_args.kwargs
+            result, error = callback_args.args
+            if error:
+                raise IOError('C-Ares returned error %s: %s while resolving %s' %
+                              (error, pycares.errno.strerror(error), host))
+            addresses = result.addresses
+        addrinfo = []
+        for address in addresses:
+            if '.' in address:
+                address_family = socket.AF_INET
+            elif ':' in address:
+                address_family = socket.AF_INET6
+            else:
+                address_family = socket.AF_UNSPEC
+            if family != socket.AF_UNSPEC and family != address_family:
+                raise IOError('Requested socket family %d but got %d' %
+                              (family, address_family))
+            addrinfo.append((address_family, (address, port)))
+        raise gen.Return(addrinfo)
+
     
-      # Remove old YCM libs if present so that YCM can start.
-  old_libs = (
-    glob.glob( p.join( DIR_OF_OLD_LIBS, '*ycm_core.*' ) ) +
-    glob.glob( p.join( DIR_OF_OLD_LIBS, '*ycm_client_support.*' ) ) +
-    glob.glob( p.join( DIR_OF_OLD_LIBS, '*clang*.*') ) )
-  for lib in old_libs:
-    os.remove( lib )
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
+            # Semaphore was released and can be acquired again.
+        self.assertTrue(sem.acquire().done())
     
     
-# This class can be used to keep the ycmd server alive for the duration of the
-# life of the client. By default, ycmd shuts down if it doesn't see a request in
-# a while.
-class YcmdKeepalive( object ):
-  def __init__( self, ping_interval_seconds = 60 * 10 ):
-    self._keepalive_thread = Thread( target = self._ThreadMain )
-    self._keepalive_thread.daemon = True
-    self._ping_interval_seconds = ping_interval_seconds
+class ChunkHandler(RequestHandler):
+    def get(self):
+        for i in xrange(options.num_chunks):
+            self.write('A' * options.chunk_size)
+            self.flush()
+        self.finish()
     
     
-  def IsAllowed( self, diagnostic ):
-    # NOTE: in this class's implementation, we ask vimsupport for
-    #  the current filetypes and delegate automatically; it is probably,
-    #  more efficient, however, to call SubsetForTypes() and reuse
-    #  the returned DiagnosticFilter if it will be checked repeatedly.
-    filetypes = vimsupport.CurrentFiletypes()
-    return self.SubsetForTypes( filetypes ).IsAllowed( diagnostic )
+def main():
+    base_cmd = [
+        sys.executable, '-m', 'timeit', '-s',
+        'from stack_context_benchmark import StackBenchmark, ExceptionBenchmark']
+    cmds = [
+        'StackBenchmark().enter_exit(50)',
+        'StackBenchmark().call_wrapped(50)',
+        'StackBenchmark().enter_exit(500)',
+        'StackBenchmark().call_wrapped(500)',
     
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-        config['pylons.h'] = r2.lib.helpers
-    config['routes.map'] = make_map(config)
-    
-        `app_conf`
-        The application's local configuration. Normally specified in the
-        [app:<name>] section of the Paste ini file (where <name> defaults to
-        main).
-    '''
-    
-        @validate(VAdmin(),
-              award = VAwardByCodename('awardcn'))
-    def GET_winners(self, award):
-        if award is None:
-            abort(404, 'page not found')
-    
-            The user's response to the CAPTCHA should be sent as `captcha`
-        along with your request.
-    
-        def GET_blog(self):
-        return self.redirect('https://blog.%s/' %
-                             get_domain(subreddit=False, no_www=True))
+        def open(self):
+        ChatSocketHandler.waiters.add(self)
