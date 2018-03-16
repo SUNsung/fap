a7d6e6b8dc8a7d7a0b47b186478903c6c67a5cd6
SUNsung <weatherius@gmail.com>
2018-03-16 21:53:58 +0200

    Rewrite [36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py] File in: 2018-03-16 21:53:58

--- pyton/36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py
+++ pyton/36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py
@@ -1,104 +1,193 @@
 
         
-                                with K.name_scope(metric_name):
-                            metric_result = weighted_metric_fn(y_true, y_pred,
-                                                               weights=weights,
-                                                               mask=masks[i])
+        import io
+import optparse
     
-    print('Convert class vector to binary class matrix '
-      '(for use with categorical_crossentropy)')
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-print('y_train shape:', y_train.shape)
-print('y_test shape:', y_test.shape)
-    
-        num_train, num_test = x_train.shape[0], x_test.shape[0]
-    
-        @classmethod
-    def get_all_datasources(cls, session):
-        datasources = []
-        for source_type in ConnectorRegistry.sources:
-            datasources.extend(
-                session.query(ConnectorRegistry.sources[source_type]).all())
-        return datasources
+        def test_proxy_https(self):
+        params = self._check_params(['primary_proxy', 'primary_server_ip'])
+        if params is None:
+            return
+        ydl = FakeYDL({
+            'proxy': params['primary_proxy']
+        })
+        self.assertEqual(
+            ydl.urlopen('https://yt-dl.org/ip').read().decode('utf-8'),
+            params['primary_server_ip'])
     
     
-def export_schema_to_dict(back_references):
-    '''Exports the supported import/export schema to a dictionary'''
-    databases = [Database.export_schema(recursive=True,
-                 include_parent_ref=back_references)]
-    clusters = [DruidCluster.export_schema(recursive=True,
-                include_parent_ref=back_references)]
-    data = dict()
-    if databases:
-        data[DATABASES_KEY] = databases
-    if clusters:
-        data[DRUID_CLUSTERS_KEY] = clusters
-    return data
+class RtspFD(FileDownloader):
+    def real_download(self, filename, info_dict):
+        url = info_dict['url']
+        self.report_destination(filename)
+        tmpfilename = self.temp_name(filename)
     
+    import re
     
-def downgrade():
-    with op.batch_alter_table('sql_metrics') as batch_op_sql_metrics:
-        batch_op_sql_metrics.drop_column('warning_text')
-    with op.batch_alter_table('metrics') as batch_op_metrics:
-        batch_op_metrics.drop_column('warning_text')
+            '''
+        available_plugins = plugin_manager.get_formatters_grouped()
+        self.enabled_plugins = []
+        for group in groups:
+            for cls in available_plugins[group]:
+                p = cls(env=env, **kwargs)
+                if p.enabled:
+                    self.enabled_plugins.append(p)
+    
+        def sum_up(self):
+        actually_downloaded = (
+            self.status.downloaded - self.status.resumed_from)
+        time_taken = self.status.time_finished - self.status.time_started
+    
+        ''')
+)
+positional.add_argument(
+    'method',
+    metavar='METHOD',
+    nargs=OPTIONAL,
+    default=None,
+    help='''
+    The HTTP method to be used for the request (GET, POST, PUT, DELETE, ...).
+    
+        def _migrate_implicit_content_type(self):
+        '''Migrate the removed implicit_content_type config option'''
+        try:
+            implicit_content_type = self.pop('implicit_content_type')
+        except KeyError:
+            self.save()
+        else:
+            if implicit_content_type == 'form':
+                self['default_options'].insert(0, '--form')
+            self.save()
+            self.load()
 
     
-    '''
     
-      from ycmd import server_utils as su
-  su.AddNearestThirdPartyFoldersToSysPath( DIR_OF_CURRENT_SCRIPT )
-  # We need to import ycmd's third_party folders as well since we import and
-  # use ycmd code in the client.
-  su.AddNearestThirdPartyFoldersToSysPath( su.__file__ )
+class ExitStatus:
+    '''Exit status code constants.'''
+    OK = 0
+    ERROR = 1
+    PLUGIN_ERROR = 7
     
-            If the future has been cancelled (cancel() was called and returned
-        True) then any threads waiting on the future completing (though calls
-        to as_completed() or wait()) are notified and False is returned.
+            print('Iteration %s of %s' % (i, n_iter))
     
-    # The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
-#html_logo = None
+        fn = os.path.relpath(fn,
+                         start=os.path.dirname(__import__(package).__file__))
+    try:
+        lineno = inspect.getsourcelines(obj)[1]
+    except Exception:
+        lineno = ''
+    return url_fmt.format(revision=revision, package=package,
+                          path=fn, lineno=lineno)
     
-    from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+        # decode the payload explicitly as UTF-8 since lxml is confused for some
+    # reason
+    with codecs.open(html_filename,'r','utf-8') as html_file:
+        html_content = html_file.read()
+    tree = ElementTree(lxml.html.document_fromstring(html_content))
+    i = 0
+    j = 0
+    for p in tree.findall('//p'):
+        content = p.text_content()
+        if len(content) < 100:
+            # skip paragraphs that are too short - probably too noisy and not
+            # representative of the actual language
+            continue
+    
+        def plot_kernels(self):
+        self.ax.text(-50, -60, 'Linear: $u^T v$')
+        self.ax.text(-20, -60, 'RBF: $\exp (-\gamma \| u-v \|^2)$')
+        self.ax.text(10, -60, 'Poly: $(\gamma \, u^T v + r)^d$')
+    
+    fit_data = data[np.argsort(model.row_labels_)]
+fit_data = fit_data[:, np.argsort(model.column_labels_)]
+    
+    model = SpectralCoclustering(n_clusters=5, random_state=0)
+model.fit(data)
+score = consensus_score(model.biclusters_,
+                        (rows[:, row_idx], columns[:, col_idx]))
+    
+        def mapper(self, _, line):
+        '''Parse each log line, extract and transform relevant lines.
+    
+        @abstractmethod
+    def can_fit_in_spot(self, spot):
+        pass
+    
+    from ..common import *
+from ..extractor import VideoExtractor
+    
+        html = get_content(url)
+    pid = match1(html, r'video\.settings\.pid\s*=\s*\'([^\']+)\'')
+    title = match1(html, r'video\.settings\.title\s*=\s*\'([^\']+)\'')
+    
+            if s: self.streams['video'] = {'url': s }
+        if mp3: self.streams['audio'] = { 'url': mp3 }
+        if pdf: self.streams['slides'] = { 'url': pdf }
     
     
-  # This is the blocking version of the method. See below for async.
-  # |timeout| is num seconds to tolerate no response from server before giving
-  # up; see Requests docs for details (we just pass the param along).
-  @staticmethod
-  def PostDataToHandler( data, handler, timeout = _READ_TIMEOUT_SEC ):
-    return JsonFromFuture( BaseRequest.PostDataToHandlerAsync( data,
-                                                               handler,
-                                                               timeout ) )
+def mtv81_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_content(url)
+    title = HTMLParser().unescape(
+        '|'.join(match1(html, r'<title>(.*?)</title>').split('|')[:-2]))
     
-    from ycm.client.base_request import ( BaseRequest, BuildRequestData,
-                                      HandleServerException )
+    print 'PLUGIN_I18N_PATHS := ' + ','.join(os.path.relpath(plugin.path)
+                                         for plugin in plugins
+                                         if plugin.needs_translation)
     
     
-# This class can be used to keep the ycmd server alive for the duration of the
-# life of the client. By default, ycmd shuts down if it doesn't see a request in
-# a while.
-class YcmdKeepalive( object ):
-  def __init__( self, ping_interval_seconds = 60 * 10 ):
-    self._keepalive_thread = Thread( target = self._ThreadMain )
-    self._keepalive_thread.daemon = True
-    self._ping_interval_seconds = ping_interval_seconds
+def walk_workspace():
+    root = get_root()
+    files = subprocess.check_output(['git', 'ls-files', '--full-name',
+                                     '--', root])
+    for filename in files.splitlines():
+        yield os.path.join(root, filename)
     
-        @YouCompleteMeInstance( { 'log_level': 'debug',
-                              'keep_logfiles': 1 } )
-    def Debug_test( ycm ):
-        ...
-  '''
-  def Decorator( test ):
-    @functools.wraps( test )
-    def Wrapper( *args, **kwargs ):
-      ycm = YouCompleteMe( MakeUserOptions( custom_options ) )
-      WaitUntilReady()
-      ycm.CheckIfServerIsReady()
-      try:
-        test( ycm, *args, **kwargs )
-      finally:
-        StopServer( ycm )
-    return Wrapper
-  return Decorator
+        if ConfigValue.bool(global_conf.get('uncompressedJS')):
+        paths['static_files'] = get_raw_statics_path()
+    else:
+        paths['static_files'] = get_built_statics_path()
+    
+    def is_api(subtype = ''):
+    return c.render_style and c.render_style.startswith(api_type(subtype))
+    
+        @require_oauth2_scope('creddits')
+    @validate(
+        VUser(),
+        target=VByName('fullname'),
+    )
+    @api_doc(
+        api_section.gold,
+        uri='/api/v1/gold/gild/{fullname}',
+    )
+    def POST_gild(self, target):
+        if not isinstance(target, (Comment, Link)):
+            err = RedditError('NO_THING_ID')
+            self.on_validation_error(err)
+    
+        @csrf_exempt
+    @json_validate(
+        VRatelimit(rate_ip=True, prefix='rate_register_'),
+        signature=VSigned(),
+        name=VUname(['user']),
+        email=ValidEmail('email'),
+        password=VPasswordChange(['passwd', 'passwd2']),
+    )
+    def POST_register(self, responder, name, email, password, **kwargs):
+        kwargs.update(dict(
+            controller=self,
+            form=responder('noop'),
+            responder=responder,
+            name=name,
+            email=email,
+            password=password,
+        ))
+        return handle_register(**kwargs)
+    
+        @csrf_exempt
+    def renderurl(self, override=None):
+        if override:
+            path = override
+        else:
+            path = request.path
+    
+    from r2.controllers.reddit_base import MinimalController
+from r2.lib import promote, cache
