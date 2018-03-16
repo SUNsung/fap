4b617bbfd95d4d5e5235293518605c792913bb26
SUNsung <weatherius@gmail.com>
2018-03-16 12:25:39 +0200

    Rewrite [24rY3zWRXCUFc24czSxMo6XTc241VoGmNhVMOY24Jpj.py] File in: 2018-03-16 12:25:38

--- pyton/24rY3zWRXCUFc24czSxMo6XTc241VoGmNhVMOY24Jpj.py
+++ pyton/24rY3zWRXCUFc24czSxMo6XTc241VoGmNhVMOY24Jpj.py
@@ -1,132 +1,126 @@
 
         
-            # Sorting the libraries
-    inner_blocks = sorted(blocks[0].split('##'))
-    for i in range(1 , len(inner_blocks)):
-        if inner_blocks[i][0] != '#':
-            inner_blocks[i] = '##' + inner_blocks[i]
-    inner_blocks=''.join(inner_blocks)
+        from __future__ import print_function
+    
+        try:
+        return json.dumps(result, sort_keys=True, indent=indent, ensure_ascii=False)
+    except UnicodeDecodeError:
+        return json.dumps(result, sort_keys=True, indent=indent)
+
+    
+    from ansible.plugins.terminal import TerminalBase
+from ansible.errors import AnsibleConnectionFailure
     
     
-@csrf_protect
-def render_flatpage(request, f):
-    '''
-    Internal interface to the flat page view.
-    '''
-    # If registration is required for accessing this page, and the user isn't
-    # logged in, redirect to the login page.
-    if f.registration_required and not request.user.is_authenticated:
-        from django.contrib.auth.views import redirect_to_login
-        return redirect_to_login(request.path)
-    if f.template_name:
-        template = loader.select_template((f.template_name, DEFAULT_TEMPLATE))
-    else:
-        template = loader.get_template(DEFAULT_TEMPLATE)
+def get_group_vars(groups):
+    
+        Each code snipped should get additional C++ code around it to help compile the line in context, with
+    some heuristic guessing of what is needed around. The wrapping code should have a token in each line allowing
+    other tools to filter out these lines
     
     
-def get_meta(data):
-    meta = {'hostvars': {}}
-    for node in data:
-        meta['hostvars'][node['hostname']] = {'tags': node['tags']}
-    return meta
+def CheckBracesSpacing(filename, clean_lines, linenum, nesting_state, error):
+  '''Checks for horizontal spacing near commas.
     
-    MIN_TOKEN_TYPE = UP+1
-	
-INVALID_TOKEN_TYPE = 0
+        def path_ok(self):
+        'Simple validity check for menu file path'
+        path = self.path.get().strip()
+        if not path: #no path specified
+            self.showerror('no help file path specified.', self.path_error)
+            return None
+        elif not path.startswith(('www.', 'http')):
+            if path[:5] == 'file:':
+                path = path[5:]
+            if not os.path.exists(path):
+                self.showerror('help file path does not exist.',
+                               self.path_error)
+                return None
+            if platform == 'darwin':  # for Mac Safari
+                path =  'file://' + path
+        return path
     
-                        s = snext
-                    input.consume()
-                    continue
+    
+Calculating variability or spread
+---------------------------------
+    
+            type_indices = array('B')
+        type_indices.fromfile(fileobj, tzh_timecnt)
+    
+        msg: The unformatted error message
+    doc: The JSON document being parsed
+    pos: The start index of doc where parsing failed
+    lineno: The line corresponding to pos
+    colno: The column corresponding to pos
+    
+            return source
+    
+    int WINAPI WinMain(
+    HINSTANCE hInstance,      // handle to current instance
+    HINSTANCE hPrevInstance,  // handle to previous instance
+    LPSTR lpCmdLine,          // pointer to command line
+    int nCmdShow              // show state of window
+    )
+{
+    extern int Py_FrozenMain(int, char **);
+    PyImport_FrozenModules = _PyImport_FrozenModules;
+    return Py_FrozenMain(__argc, __argv);
+}
+'''
+    
+        def nearest(self, red, green, blue):
+        '''Return the name of color nearest (red, green, blue)'''
+        # BAW: should we use Voronoi diagrams, Delaunay triangulation, or
+        # octree for speeding up the locating of nearest point?  Exhaustive
+        # search is inefficient, but seems fast enough.
+        nearest = -1
+        nearest_name = ''
+        for name, aliases in self.__byrgb.values():
+            r, g, b = self.__byname[name.lower()]
+            rdelta = red - r
+            gdelta = green - g
+            bdelta = blue - b
+            distance = rdelta * rdelta + gdelta * gdelta + bdelta * bdelta
+            if nearest == -1 or distance < nearest:
+                nearest = distance
+                nearest_name = name
+        return nearest_name
+    
+            Only one instance can be installed at a time.
+    
+            self.assertEqual(3j ** 0j, 1)
+        self.assertEqual(3j ** 0, 1)
     
     
-    def getInputStream(self):
-        '''@brief From what character stream was this token created.
+def main():
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([
+        (r'/', MainHandler),
+    ])
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
     
-        def test_static_with_range_full_file(self):
-        response = self.get_and_head('/static/robots.txt', headers={
-            'Range': 'bytes=0-'})
-        # Note: Chrome refuses to play audio if it gets an HTTP 206 in response
-        # to ``Range: bytes=0-`` :(
-        self.assertEqual(response.code, 200)
-        robots_file_path = os.path.join(self.static_dir, 'robots.txt')
-        with open(robots_file_path) as f:
-            self.assertEqual(response.body, utf8(f.read()))
-        self.assertEqual(response.headers.get('Content-Length'), '26')
-        self.assertEqual(response.headers.get('Content-Range'), None)
+    import socket
     
-        @gen_test
-    def test_http10_no_content_length(self):
-        # Regression test for a bug in which can_keep_alive would crash
-        # for an HTTP/1.0 (not 1.1) response with no content-length.
-        conn = HTTP1Connection(self.client_stream, True)
-        self.server_stream.write(b'HTTP/1.0 200 Not Modified\r\n\r\nhello')
-        self.server_stream.close()
     
-        def callback(response):
-        response.rethrow()
-        assert len(response.body) == (options.num_chunks * options.chunk_size)
-        logging.warning('fetch completed in %s seconds', response.request_time)
-        IOLoop.current().stop()
+globals().update(wrap_web_tests_application())
     
-                if links:
-                kw = {}
-                if wrapper:
-                    links = wrap_links(links, wrapper = wrapper)
-                else:
-                    links = wrap_links(links)
-                links = list(links)
-                links = max(links, key = lambda x: x._score) if links else None
-            if not links and wrapper:
-                return wrapper(None)
-            return links
-            # note: even if _by_url successed or a link was passed in,
-            # it is possible link_listing.things is empty if the
-            # link(s) is/are members of a private reddit
-            # return the link with the highest score (if more than 1)
-        except:
-            #we don't want to return 500s in other people's pages.
-            import traceback
-            g.log.debug('FULLPATH: get_link error in buttons code')
-            g.log.debug(traceback.format_exc())
-            if wrapper:
-                return wrapper(None)
+            'ExceptionBenchmark().enter_exit(50)',
+        'ExceptionBenchmark().call_wrapped(50)',
+        'ExceptionBenchmark().enter_exit(500)',
+        'ExceptionBenchmark().call_wrapped(500)',
+    ]
+    for cmd in cmds:
+        print(cmd)
+        subprocess.check_call(base_cmd + [cmd])
     
-        def send404(self):
-        if 'usable_error_content' in request.environ:
-            return request.environ['usable_error_content']
-        return pages.RedditError(_('page not found'),
-                                 _('the page you requested does not exist')).render()
+            self.maybe_create_tables()
     
-    from r2.controllers.reddit_base import MinimalController
-from r2.lib.pages import (
-    GoogleTagManagerJail,
-    GoogleTagManager,
-)
-from r2.lib.validator import (
-    validate,
-    VGTMContainerId,
-)
-    
-        def GET_cachehealth(self):
-        results = {}
-        behaviors = {
-            # Passed on to poll(2) in milliseconds
-            'connect_timeout': 1000,
-            # Passed on to setsockopt(2) in microseconds
-            'receive_timeout': int(1e6),
-            'send_timeout': int(1e6),
-        }
-        for server in cache._CACHE_SERVERS:
-            try:
-                if server.startswith('udp:'):
-                    # libmemcached doesn't support UDP get/fetch operations
-                    continue
-                mc = pylibmc.Client([server], behaviors=behaviors)
-                # it's ok that not all caches are mcrouter, we'll just ignore
-                # the miss either way
-                mc.get('__mcrouter__.version')
-                results[server] = 'OK'
-            except pylibmc.Error as e:
-                g.log.warning('Health check for %s FAILED: %s', server, e)
-                results[server] = 'FAILED %s' % e
-        return json.dumps(results)
+    import logging
+import tornado.escape
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.websocket
+import os.path
+import uuid
