38e8bb7423bd8f41106f542f751e540d39aec6eb
SUNsung <weatherius@gmail.com>
2018-03-16 05:59:16 +0200

    Rewrite [563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp] File in: 2018-03-16 05:59:16

--- cccccc/563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp
+++ cccccc/563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp
@@ -1,274 +1,202 @@
-// Generate param traits read methods.
-#include 'ipc/param_traits_read_macros.h'
-namespace IPC {
-#include 'content/nw/src/common/common_message_generator.h'
-}  // namespace IPC
-    
-    #ifndef CONTENT_NW_SRC_API_DISPATCHER_BINDINGS_H_
-#define CONTENT_NW_SRC_API_DISPATCHER_BINDINGS_H_
-    
-      // Send event to C++ object's corresponding js object.
-  void SendEvent(Base* object,
-                 const std::string& event,
-                 const base::ListValue& arguments);
-    
-    void Menu::Create(const base::DictionaryValue& option) {
-  gtk_accel_group = NULL;
-  std::string type;
-  if (option.GetString('type', &type) && type == 'menubar')
-    menu_ = gtk_menu_bar_new();
-  else
-    menu_ = gtk_menu_new();
-    }
-    
-    
-    {#endif
-}  // namespace nwapi
-    
-     protected:
-  ~NwClipboardReadAvailableTypesFunction() override;
-    
-    // Find a file by file name.
-bool PyDescriptorDatabase::FindFileByName(const string& filename,
-                                          FileDescriptorProto* output) {
-  ScopedPyObjectPtr py_descriptor(PyObject_CallMethod(
-      py_database_, 'FindFileByName', 's#', filename.c_str(), filename.size()));
-  return GetFileDescriptorProto(py_descriptor.get(), output);
+
+        
+        
+void Base::Call(const std::string& method, const base::ListValue& arguments,
+                content::RenderFrameHost* rvh) {
+  NOTREACHED() << 'Uncatched call in Base'
+               << ' method:' << method
+               << ' arguments:' << arguments;
 }
     
-    extern PyTypeObject RepeatedCompositeContainer_Type;
     
-    void MapFieldGenerator::GenerateSerializedSizeCode(io::Printer* printer) {
-  printer->Print(
-    variables_,
-    'size += $name$_.CalculateSize(_map_$name$_codec);\n');
-}
+    {};
     
-      virtual void GenerateCloningCode(io::Printer* printer);
-  virtual void GenerateFreezingCode(io::Printer* printer);
-  virtual void GenerateMembers(io::Printer* printer);
-  virtual void GenerateMergingCode(io::Printer* printer);
-  virtual void GenerateParsingCode(io::Printer* printer);
-  virtual void GenerateSerializationCode(io::Printer* printer);
-  virtual void GenerateSerializedSizeCode(io::Printer* printer);
-    
-    #include <string>
-    
-    #include <google/protobuf/compiler/code_generator.h>
-#include <google/protobuf/compiler/plugin.h>
-#include <google/protobuf/descriptor.h>
-#include <google/protobuf/descriptor.pb.h>
-#include <google/protobuf/io/printer.h>
-#include <google/protobuf/io/zero_copy_stream.h>
-#include <google/protobuf/wire_format.h>
-    
-    namespace google {
-namespace protobuf {
-namespace compiler {
-namespace csharp {
-    }
-    }
-    }
-    }
-    
-      if (!status) {                  // If not set then
-    return FALSE;                // ignore other checks
-  }
+      void OnEvent(int object_id,
+               std::string event,
+               const base::ListValue& arguments);
     
     
-    {
-/**
- * @name tess_add_doc_word
- *
- * Add the given word to the document dictionary
- */
-void Tesseract::tess_add_doc_word(WERD_CHOICE *word_choice) {
-  getDict().add_document_word(*word_choice);
-}
-}  // namespace tesseract
+    {}
+    
+    
+    {}  // namespace nw
 
     
-    // Computes and returns the squared evaluation metric for a line fit.
-double DetLineFit::EvaluateLineFit() {
-  // Compute the upper quartile error from the line.
-  double dist = ComputeUpperQuartileError();
-  if (distances_.size() >= kMinPointsForErrorCount &&
-      dist > kMaxRealDistance * kMaxRealDistance) {
-    // Use the number of mis-fitted points as the error metric, as this
-    // gives a better measure of fit for badly fitted lines where more
-    // than a quarter are badly fitted.
-    double threshold = kMaxRealDistance * sqrt(square_length_);
-    dist = NumberOfMisfittedPoints(threshold);
-  }
-  return dist;
+    
+    {  *y = CalculateMenuYPosition(&screen_rect, &menu_req, NULL, *y);
 }
     
-    // Returns the sqrt of the mean squared error measured perpendicular from the
-// line through mean_point() in the direction dir.
-//
-// Derivation:
-//   Lemma:  Let v and x_i (i=1..N) be a k-dimensional vectors (1xk matrices).
-//     Let % be dot product and ' be transpose.  Note that:
-//      Sum[i=1..N] (v % x_i)^2
-//         = v * [x_1' x_2' ... x_N'] * [x_1' x_2' .. x_N']' * v'
-//     If x_i have average 0 we have:
-//       = v * (N * COVARIANCE_MATRIX(X)) * v'
-//     Expanded for the case that k = 2, where we treat the dimensions
-//     as x_i and y_i, this is:
-//       = v * (N * [VAR(X), COV(X,Y); COV(X,Y) VAR(Y)]) * v'
-//  Now, we are trying to calculate the mean squared error, where v is
-//  perpendicular to our line of interest:
-//    Mean squared error
-//      = E [ (v % (x_i - x_avg))) ^2 ]
-//      = Sum (v % (x_i - x_avg))^2 / N
-//      = v * N * [VAR(X) COV(X,Y); COV(X,Y) VAR(Y)] / N * v'
-//      = v * [VAR(X) COV(X,Y); COV(X,Y) VAR(Y)] * v'
-//      = code below
-double LLSQ::rms_orth(const FCOORD &dir) const {
-  FCOORD v = !dir;
-  v.normalise();
-  return sqrt(v.x() * v.x() * x_variance() +
-              2 * v.x() * v.y() * covariance() +
-              v.y() * v.y() * y_variance());
+    // Add a new point. Takes a copy - the pt doesn't need to stay in scope.
+void DetLineFit::Add(const ICOORD& pt) {
+  pts_.push_back(PointWidth(pt, 0));
 }
+// Associates a half-width with the given point if a point overlaps the
+// previous point by more than half the width, and its distance is further
+// than the previous point, then the more distant point is ignored in the
+// distance calculation. Useful for ignoring i dots and other diacritics.
+void DetLineFit::Add(const ICOORD& pt, int halfwidth) {
+  pts_.push_back(PointWidth(pt, halfwidth));
+}
+    
+    // This class fits a line to a set of ICOORD points.
+// There is no restriction on the direction of the line, as it
+// uses a vector method, ie no concern over infinite gradients.
+// The fitted line has the least upper quartile of squares of perpendicular
+// distances of all source points from the line, subject to the constraint
+// that the line is made from one of the pairs of [{p1,p2,p3},{pn-2, pn-1, pn}]
+// i.e. the 9 combinations of one of the first 3 and last 3 points.
+// A fundamental assumption of this algorithm is that one of the first 3 and
+// one of the last 3 points are near the best line fit.
+// The points must be Added in line order for the algorithm to work properly.
+// No floating point calculations are needed* to make an accurate fit,
+// and no random numbers are needed** so the algorithm is deterministic,
+// architecture-stable, and compiler-stable as well as stable to minor
+// changes in the input.
+// *A single floating point division is used to compute each line's distance.
+// This is unlikely to result in choice of a different line, but if it does,
+// it would be easy to replace with a 64 bit integer calculation.
+// **Random numbers are used in the nth_item function, but the worst
+// non-determinism that can result is picking a different result among equals,
+// and that wouldn't make any difference to the end-result distance, so the
+// randomness does not affect the determinism of the algorithm. The random
+// numbers are only there to guarantee average linear time.
+// Fitting time is linear, but with a high constant, as it tries 9 different
+// lines and computes the distance of all points each time.
+// This class is aimed at replacing the LLSQ (linear least squares) and
+// LMS (least median of squares) classes that are currently used for most
+// of the line fitting in Tesseract.
+class DetLineFit {
+ public:
+  DetLineFit();
+  ~DetLineFit();
+    }
+    
+      // Calculate y-shift
+  int bln_yshift = 0, bottom_shift = 0, top_shift = 0;
+  if (bottom < min_bottom - tolerance) {
+    bottom_shift = bottom - min_bottom;
+  } else if (bottom > max_bottom + tolerance) {
+    bottom_shift = bottom - max_bottom;
+  }
+  if (top < min_top - tolerance) {
+    top_shift = top - min_top;
+  } else if (top > max_top + tolerance) {
+    top_shift = top - max_top;
+  }
+  if ((top_shift >= 0 && bottom_shift > 0) ||
+      (top_shift < 0 && bottom_shift < 0)) {
+    bln_yshift = (top_shift + bottom_shift) / 2;
+  }
+  *yshift = bln_yshift * yscale;
     
       // Prints the content of the DENORM for debug purposes.
   void Print() const;
     
-    DEFINE_string(server_address, 'localhost:50052',
-              'Address of the performance database server');
+    FLOAT32 DistanceBetween(FPOINT A, FPOINT B);
     
-    class TestServiceImpl : public ::grpc::testing::EchoTestService::Service {
- public:
-  TestServiceImpl() : signal_client_(false), host_() {}
-  explicit TestServiceImpl(const grpc::string& host)
-      : signal_client_(false), host_(new grpc::string(host)) {}
-    }
+      // Setup the map for the given indexed_features that have been indexed by
+  // feature_map. After use, use Set(..., false) to reset to the initial state
+  // as this is faster than calling Init for sparse spaces.
+  void Set(const GenericVector<int>& indexed_features,
+           int canonical_count, bool value);
     
-    TEST(InlinedVectorTest, CreateAndIterate) {
-  const int kNumElements = 9;
-  InlinedVector<int, 2> v;
-  for (int i = 0; i < kNumElements; ++i) {
-    v.push_back(i);
-  }
-  EXPECT_EQ(static_cast<size_t>(kNumElements), v.size());
-  for (int i = 0; i < kNumElements; ++i) {
-    EXPECT_EQ(i, v[i]);
-  }
-}
-    
-    std::shared_ptr<Channel> CreateTestChannel(
-    const grpc::string& server, const grpc::string& override_hostname,
-    bool enable_ssl, bool use_prod_roots) {
-  return CreateTestChannel(server, override_hostname, enable_ssl,
-                           use_prod_roots, std::shared_ptr<CallCredentials>());
-}
-    
-    using grpc::Server;
-using grpc::ServerAsyncResponseWriter;
-using grpc::ServerBuilder;
-using grpc::ServerContext;
-using grpc::ServerCompletionQueue;
-using grpc::Status;
-using helloworld::HelloRequest;
-using helloworld::HelloReply;
-using helloworld::Greeter;
-    
-    
-    
-    // Data pertaining to configuration of the generator with respect to anything
-// that may be used internally at Google.
-struct GeneratorConfiguration {
-  GeneratorConfiguration();
-  grpc::string grpc_package_root;
-  // TODO(https://github.com/grpc/grpc/issues/8622): Drop this.
-  grpc::string beta_package_root;
-  // TODO(https://github.com/google/protobuf/issues/888): Drop this.
-  grpc::string import_prefix;
-};
-    
-    #ifndef GRPC_INTERNAL_COMPILER_PYTHON_PRIVATE_GENERATOR_H
-#define GRPC_INTERNAL_COMPILER_PYTHON_PRIVATE_GENERATOR_H
-    
-    
-    {// Enable LOG(CONSOLE) for print messages to console.
-#define LOG_CONSOLE ::xgboost::ConsoleLogger()
-// Enable LOG(TRACKER) for print messages to tracker
-#define LOG_TRACKER ::xgboost::TrackerLogger()
-}  // namespace xgboost.
-#endif  // XGBOOST_LOGGING_H_
+    #endif
 
     
-    template<typename StorageIndex>
-class SparsePageLZ4Format : public SparsePage::Format {
- public:
-  explicit SparsePageLZ4Format(bool use_lz4_hc)
-      : use_lz4_hc_(use_lz4_hc) {
-    raw_bytes_ = raw_bytes_value_ = raw_bytes_index_ = 0;
-    encoded_bytes_value_ = encoded_bytes_index_ = 0;
-    nthread_ = dmlc::GetEnv('XGBOOST_LZ4_DECODE_NTHREAD', 4);
-    nthread_write_ = dmlc::GetEnv('XGBOOST_LZ4_COMPRESS_NTHREAD', 12);
-  }
-  virtual ~SparsePageLZ4Format() {
-    size_t encoded_bytes = raw_bytes_ +  encoded_bytes_value_ + encoded_bytes_index_;
-    raw_bytes_ += raw_bytes_value_ + raw_bytes_index_;
-    if (raw_bytes_ != 0) {
-      LOG(CONSOLE) << 'raw_bytes=' << raw_bytes_
-                   << ', encoded_bytes=' << encoded_bytes
-                   << ', ratio=' << double(encoded_bytes) / raw_bytes_
-                   << ', ratio-index=' << double(encoded_bytes_index_) /raw_bytes_index_
-                   << ', ratio-value=' << double(encoded_bytes_value_) /raw_bytes_value_;
-    }
-  }
+    static void RunSynchronousUnaryPingPong() {
+  gpr_log(GPR_INFO, 'Running Synchronous Unary Ping Pong');
     }
     
-    TEST(Param, VectorStreamRead) {
-  std::vector<int> vals = {3, 2, 1};
-  std::stringstream ss;
-  std::vector<int> vals_in;
-    }
     
-    /*! \brief match error */
-struct EvalMultiLogLoss : public EvalMClassBase<EvalMultiLogLoss> {
-  const char* Name() const override {
-    return 'mlogloss';
-  }
-  inline static bst_float EvalRow(int label,
-                                  const bst_float *pred,
-                                  size_t nclass) {
-    const bst_float eps = 1e-16f;
-    size_t k = static_cast<size_t>(label);
-    if (pred[k] > eps) {
-      return -std::log(pred[k]);
+    {
+    {}  // namespace testing
+}  // namespace grpc
+
+    
+      size_t PeekRead(void* dptr, size_t size) {
+    size_t nbuffer = buffer_.length() - buffer_ptr_;
+    if (nbuffer < size) {
+      buffer_ = buffer_.substr(buffer_ptr_, buffer_.length());
+      buffer_ptr_ = 0;
+      buffer_.resize(size);
+      size_t nadd = strm_->Read(dmlc::BeginPtr(buffer_) + nbuffer, size - nbuffer);
+      buffer_.resize(nbuffer + nadd);
+      std::memcpy(dptr, dmlc::BeginPtr(buffer_), buffer_.length());
+      return buffer_.length();
     } else {
-      return -std::log(eps);
+      std::memcpy(dptr, dmlc::BeginPtr(buffer_) + buffer_ptr_, size);
+      return size;
     }
   }
-};
+    
+    
+    {    for (size_t i = 0; i < batch.size; ++i) {
+      offset_[i + 1] = (i + 1) * num_col_;
+      Row<IndexType> row = batch[i];
+      for (uint32_t j = 0; j < num_col_; ++j) {
+        dense_index_[i * num_col_ + j] = j;
+      }
+      for (unsigned k = 0; k < row.length; ++k) {
+        uint32_t index = row.get_index(k);
+        CHECK_LT(index, num_col_)
+            << 'Featuere index larger than num_col';
+        dense_value_[i * num_col_ + index]  = row.get_value(k);
+      }
+    }
+    out_ = batch;
+    out_.index = dmlc::BeginPtr(dense_index_);
+    out_.value = dmlc::BeginPtr(dense_value_);
+    out_.offset = dmlc::BeginPtr(offset_);
+    return true;
+  }
+    
+    namespace xgboost {
+namespace data {
+    }
+    }
     
     
     {
-    {
-    {      // Add back the last sentinel node.
-      tree[j_end + 1] = sentinel;
-    }
-    if (SetDepth(static_cast<int>(2 * n - 1), &tree[0], depth, tree_limit)) {
-      /* We need to pack the Huffman tree in tree_limit bits. If this was not
-         successful, add fake entities to the lowest values and retry. */
-      break;
-    }
+    {void SparsePage::Writer::Alloc(std::shared_ptr<SparsePage>* out_page) {
+  CHECK(out_page->get() == nullptr);
+  if (num_free_buffer_ != 0) {
+    out_page->reset(new SparsePage());
+    --num_free_buffer_;
+  } else {
+    CHECK(qrecycle_.Pop(out_page));
   }
 }
+}  // namespace data
+}  // namespace xgboost
     
-    #include 'guetzli/jpeg_data.h'
+        const jchar* GetStringCritical(jstring string, jboolean* isCopy)
+    { return functions->GetStringCritical(this, string, isCopy); }
     
-    #ifndef GUETZLI_JPEG_HUFFMAN_DECODE_H_
-#define GUETZLI_JPEG_HUFFMAN_DECODE_H_
+    #define FROM_HERE facebook::ProgramLocation(__FUNCTION__, __FILE__, __LINE__)
     
-    class OutputImage {
- public:
-  OutputImage(int w, int h);
+    
+    {  T* operator->() const {
+    return m_instance;
+  }
+private:
+  T* m_instance;
+};
+    
+    namespace detail {
+template <typename T, typename jprim>
+struct JPrimitive : JavaClass<T> {
+  using typename JavaClass<T>::javaobject;
+  using JavaClass<T>::javaClassStatic;
+  static local_ref<javaobject> valueOf(jprim val) {
+    static auto cls = javaClassStatic();
+    static auto method =
+      cls->template getStaticMethod<javaobject(jprim)>('valueOf');
+    return method(cls, val);
+  }
+  jprim value() const {
+    static auto method =
+      javaClassStatic()->template getMethod<jprim()>(T::kValueMethod);
+    return method(this->self());
+  }
+};
     }
-    
-    #ifndef GUETZLI_PREPROCESS_DOWNSAMPLE_H_
-#define GUETZLI_PREPROCESS_DOWNSAMPLE_H_
