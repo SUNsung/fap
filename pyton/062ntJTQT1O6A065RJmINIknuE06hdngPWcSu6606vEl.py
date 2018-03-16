34e5cb91ef5cf2e9e1768ff25926f503c4305f26
SUNsung <weatherius@gmail.com>
2018-03-16 09:43:55 +0200

    Rewrite [062ntJTQT1O6A065RJmINIknuE06hdngPWcSu6606vEl.py] File in: 2018-03-16 09:43:55

--- pyton/062ntJTQT1O6A065RJmINIknuE06hdngPWcSu6606vEl.py
+++ pyton/062ntJTQT1O6A065RJmINIknuE06hdngPWcSu6606vEl.py
@@ -1,35 +1,132 @@
 
         
-        
-from xlog import getLogger
-xlog = getLogger('gae_proxy')
-from config import config
+        for page in itertools.count(1):
+    releases = json.loads(compat_urllib_request.urlopen(
+        'https://api.github.com/repos/rg3/youtube-dl/releases?page=%s' % page
+    ).read().decode('utf-8'))
     
-    ##
-# imaginary tree navigation type; traverse 'get child' link
-DOWN = 2
-##
-#imaginary tree navigation type; finish with a child list
-UP = 3
     
-    def upgrade():
-    op.add_column('table_columns', sa.Column('expression', sa.Text(), nullable=True))
+class TestUnicodeLiterals(unittest.TestCase):
+    def test_all_files(self):
+        for dirpath, dirnames, filenames in os.walk(rootDir):
+            for ignore_dir in IGNORED_DIRS:
+                if ignore_dir in dirnames:
+                    # If we remove the directory from dirnames os.walk won't
+                    # recurse into it
+                    dirnames.remove(ignore_dir)
+            for basename in filenames:
+                if not basename.endswith('.py'):
+                    continue
+                if basename in IGNORED_FILES:
+                    continue
     
-    Revision ID: 2591d77e9831
-Revises: 12d55656cbca
-Create Date: 2015-12-15 17:02:45.128709
+    from .common import InfoExtractor
+from ..utils import (
+    parse_duration,
+    int_or_none,
+)
     
-        try:
-        # wasn't created for some databases in the migration b4456560d4f3
-        if not table_has_constraint('tables', '_customer_location_uc', db):
-            with op.batch_alter_table('tables') as batch_op:
-                batch_op.create_unique_constraint(
-                    '_customer_location_uc',
-                    ['database_id', 'schema', 'table_name'])
-                batch_op.drop_index('table_name')
-    except Exception as e:
-        logging.warning(str(e))
     
-    Revision ID: 430039611635
-Revises: d827694c7555
-Create Date: 2016-02-10 08:47:28.950891
+class AudiMediaIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)?audi-mediacenter\.com/(?:en|de)/audimediatv/(?P<id>[^/?#]+)'
+    _TEST = {
+        'url': 'https://www.audi-mediacenter.com/en/audimediatv/60-seconds-of-audi-sport-104-2015-wec-bahrain-rookie-test-1467',
+        'md5': '79a8b71c46d49042609795ab59779b66',
+        'info_dict': {
+            'id': '1565',
+            'ext': 'mp4',
+            'title': '60 Seconds of Audi Sport 104/2015 - WEC Bahrain, Rookie Test',
+            'description': 'md5:60e5d30a78ced725f7b8d34370762941',
+            'upload_date': '20151124',
+            'timestamp': 1448354940,
+            'duration': 74022,
+            'view_count': int,
+        }
+    }
+    # extracted from https://audimedia.tv/assets/embed/embedded-player.js (dataSourceAuthToken)
+    _AUTH_TOKEN = 'e25b42847dba18c6c8816d5d8ce94c326e06823ebf0859ed164b3ba169be97f2'
+    
+        def _real_extract(self, url):
+        display_id = self._match_id(url)
+    
+      def setUp(self):
+    FLAGS.train_data = os.path.join(self.get_temp_dir() + 'test-text.txt')
+    FLAGS.eval_data = os.path.join(self.get_temp_dir() + 'eval-text.txt')
+    FLAGS.save_path = self.get_temp_dir()
+    with open(FLAGS.train_data, 'w') as f:
+      f.write(
+          '''alice was beginning to get very tired of sitting by her sister on
+          the bank, and of having nothing to do: once or twice she had peeped
+          into the book her sister was reading, but it had no pictures or
+          conversations in it, 'and what is the use of a book,' thought alice
+          'without pictures or conversations?' So she was considering in her own
+          mind (as well as she could, for the hot day made her feel very sleepy
+          and stupid), whether the pleasure of making a daisy-chain would be
+          worth the trouble of getting up and picking the daisies, when suddenly
+          a White rabbit with pink eyes ran close by her.\n''')
+      with open(FLAGS.eval_data, 'w') as f:
+        f.write('alice she rabbit once\n')
+    
+    
+if __name__ == '__main__':
+  tf.test.main()
+
+    
+        # Compute evaluation metrics.
+    accuracy = tf.metrics.accuracy(labels=labels,
+                                   predictions=predicted_classes,
+                                   name='acc_op')
+    metrics = {'accuracy': accuracy}
+    tf.summary.scalar('accuracy', accuracy[1])
+    
+      Returns:
+    SequenceExampleWrapper.
+  '''
+  seq = data.SequenceWrapper()
+  for token in document_generators.tokens(doc):
+    if token in vocab_ids:
+      seq.add_timestep().set_token(vocab_ids[token])
+    
+    # Dependency imports
+    
+    flags = tf.app.flags
+FLAGS = flags.FLAGS
+    
+    py_binary(
+    name = 'cifar10_eval',
+    srcs = [
+        'cifar10_eval.py',
+    ],
+    srcs_version = 'PY2AND3',
+    visibility = ['//tensorflow:__subpackages__'],
+    deps = [
+        ':cifar10',
+    ],
+)
+    
+        A simple stream of integers used when all I care about is the char
+    or token type sequence (such as interpretation).
+    '''
+    
+    from antlr3.constants import EOF, DEFAULT_CHANNEL, INVALID_TOKEN_TYPE
+    
+            total_count = 0
+        translated_count = 0
+        with open(fn) as f:
+            catalog = read_po(f)
+            for msg in catalog:
+                total_count += 1
+                if is_translated(msg):
+                    translated_count += 1
+        pct = translated_count / float(total_count) * 100
+        click.echo('% -7s % 2d%%' % (
+            locale,
+            pct,
+        ), err=True)
+        if pct >= MINIMUM and locale not in rv:
+            rv.append(locale)
+    with open(catalog_file, 'w') as f:
+        json.dump({
+            'supported_locales': sorted(rv)
+        }, f, indent=2)
+        f.write('\n')
