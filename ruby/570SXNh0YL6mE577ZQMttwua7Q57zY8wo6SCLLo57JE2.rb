3cc80e85719011a2a621013d29bcf07931d93810
SUNsung <weatherius@gmail.com>
2018-03-16 19:34:02 +0200

    Rewrite [570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb] File in: 2018-03-16 19:34:01

--- ruby/570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb
+++ ruby/570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb
@@ -1,65 +1,22 @@
+
+        
+                    -- Return the time part and the sequence part. OR appears
+            -- faster here than addition, but they're equivalent:
+            -- time_part has no trailing two bytes, and tail is only
+            -- the last two bytes.
+            RETURN time_part | tail;
+          END
+        $$ LANGUAGE plpgsql VOLATILE;
+      SQL
+    end
+    
+      def meta
+    object.file.meta
+  end
 end
 
     
-      def url
-    object.metadata.url
-  end
-    
-    def util_r7
-  block_get{break 78}
-end
-    
-      def ambient_occlusion(isect)
-    basis = Array.new
-    otherBasis(basis, isect.n)
-    
-      # if rss_url already in existing opml file, use that; otherwise, do a lookup
-  rss_url = nil
-  existing_blog = xml.xpath('//outline[@htmlUrl='#{web_url}']').first if xml
-  if existing_blog
-    rss_url = existing_blog.attr('xmlUrl')
-    puts '#{name}: ALREADY HAVE'
-  end
-    
-          def save_params_and_render_consent_form(endpoint)
-        @o_auth_application = endpoint.o_auth_application
-        @response_type = endpoint.response_type
-        @redirect_uri = endpoint.redirect_uri
-        @scopes = endpoint.scopes
-        save_request_parameters
-        @app = UserApplicationPresenter.new @o_auth_application, @scopes
-        render :new
-      end
-    
-        def get_web_content(url)
-      raw_uri           = URI.parse url
-      proxy             = ENV['http_proxy']
-      if proxy
-        proxy_uri       = URI.parse(proxy)
-        https           = Net::HTTP::Proxy(proxy_uri.host, proxy_uri.port).new raw_uri.host, raw_uri.port
-      else
-        https           = Net::HTTP.new raw_uri.host, raw_uri.port
-      end
-      https.use_ssl     = true
-      https.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      request           = Net::HTTP::Get.new raw_uri.request_uri
-      data              = https.request request
+      describe 'for a suspension' do
+    before do
+      subject.call(DomainBlock.create!(domain: 'evil.org', severity: :suspend))
     end
-  end
-    
-            Dir.chdir(includes_dir) do
-          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
-          if choices.include?(file)
-            source = File.read(file)
-            partial = Liquid::Template.parse(source)
-            context.stack do
-              rtn = rtn + partial.render(context)
-            end
-          else
-            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
-          end
-        end
-      end
-      rtn
-    end
-  end
