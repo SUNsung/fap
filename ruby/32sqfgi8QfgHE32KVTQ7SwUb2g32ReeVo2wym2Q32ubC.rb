e3426bec7fcac0f4bd9d7ad04e5d759637dec8b3
SUNsung <weatherius@gmail.com>
2018-03-17 18:32:38 +0200

    Rewrite [32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb] File in: 2018-03-17 18:32:37

--- ruby/32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb
+++ ruby/32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb
@@ -1,114 +1,95 @@
 
         
-            assert_dom_equal '<script>\n//<![CDATA[\nalert('hello')\n//]]>\n</script>',
-      javascript_tag('alert('hello')')
+          def gcc_42
+    @gcc_42 ||= MacOS.gcc_42_build_version if MacOS.has_apple_developer_tools?
+  end
     
-                  if very_raw_connection && very_raw_connection.respond_to?(:flush)
-                very_raw_connection.flush
-              end
-            end
+        @@remote_tap_formulae ||= Hash.new do |cache, key|
+      user, repo = key.split('/', 2)
+      tree = {}
+    
+          it 'splits correctly' do
+        expected = ['One', 'Two', 'Three', 'Four Token']
+        expect(generator.split_keywords(keywords)).to eq(expected)
+      end
+    end
+    
+          context('when the tag doesn't exist') do
+        before do
+          allow(Fastlane::Actions).to receive(:sh).with('git rev-parse -q --verify refs/tags/1.2.0 || true', { log: nil }).and_return('')
         end
+    
+          it 'properly removes new lines of the build number' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          increment_build_number(build_number: '24\n', xcodeproj: '.xcproject')
+        end').runner.execute(:test)
+    
+          it 'raises an exception when xcode project path wasn't found' do
+        expect do
+          Fastlane::FastFile.new.parse('lane :test do
+            increment_version_number(xcodeproj: '/nothere')
+          end').runner.execute(:test)
+        end.to raise_error('Could not find Xcode project')
+      end
+    
+            # This is called early, before a machine is instantiated, to check
+        # if this provider is usable. This should return true or false.
+        #
+        # If raise_error is true, then instead of returning false, this
+        # should raise an error with a helpful message about why this
+        # provider cannot be used.
+        #
+        # @param [Boolean] raise_error If true, raise exception if not usable.
+        # @return [Boolean]
+        def self.usable?(raise_error=false)
+          # Return true by default for backwards compat since this was
+          # introduced long after providers were being written.
+          true
+        end
+    
+          # This deletes the block with the given key if it exists.
+      def delete(key)
+        key    = Regexp.quote(key)
+        regexp = /^#\s*VAGRANT-BEGIN:\s*#{key}$.*^#\s*VAGRANT-END:\s*#{key}$\r?\n?/m
+        @value.gsub!(regexp, '')
+      end
+    
+      def test_each_pair
+    ENV.each_pair do |k, v|
+      assert_kind_of(String, k)
+      assert_kind_of(String, v)
     end
   end
-end
-
     
-              def uri_parser
-            @uri_parser ||= URI::Parser.new
+      def self.status_of_dying_sleeping_thread
+    t = dying_thread_ensures { Thread.stop; }
+    Thread.pass while t.status and t.status != 'sleep'
+    status = Status.new t
+    t.wakeup
+    t.join
+    status
+  end
+    
+      it 'spawns a new Thread running the block' do
+    run = false
+    t = Thread.send(@method) { run = true }
+    t.should be_kind_of(Thread)
+    t.join
+    
+        exit_loop = true
+    
+    end
+    
+    require 'pathname'
+require './plugins/octopress_filters'
+    
+    module Jekyll
+    
+    module LogStash
+  module Api
+    module Commands
+      module System
+        class Plugins < Commands::Base
+          def run
+            { :total => plugins.count, :plugins => plugins }
           end
-    
-          add_delivery_method :smtp, Mail::SMTP,
-        address:              'localhost',
-        port:                 25,
-        domain:               'localhost.localdomain',
-        user_name:            nil,
-        password:             nil,
-        authentication:       nil,
-        enable_starttls_auto: true
-    
-    def source_dir(*subdirs)
-  test_dir('source', *subdirs)
-end
-    
-    # You can also do other things. Examples:
-# https://github.com/tmm1/stackprof/blob/master/bin/stackprof
-report = StackProf::Report.new(Marshal.load(IO.binread(PROF_OUTPUT_FILE)))
-report.print_text(
-  options[:sort],
-  options[:limit],
-  options[:select_files],
-  options[:reject_files],
-  options[:select_names],
-  options[:reject_names]
-)
-    
-          #
-      # Require a gem or gems. If it's not present, show a very nice error
-      # message that explains everything and is much more helpful than the
-      # normal LoadError.
-      #
-      # names - a string gem name or array of gem names
-      #
-      def require_with_graceful_fail(names)
-        Array(names).each do |name|
-          begin
-            Jekyll.logger.debug 'Requiring:', name.to_s
-            require name
-          rescue LoadError => e
-            Jekyll.logger.error 'Dependency Error:', <<-MSG
-Yikes! It looks like you don't have #{name} or one of its dependencies installed.
-In order to use Jekyll as currently configured, you'll need to install this gem.
-    
-        def add(entry)
-      if entry.is_a? Array
-        entry.each(&method(:add))
-      else
-        add_entry(entry) unless entry.root?
-      end
-    end
-    
-        def as_json
-      @pages
-    end
-    
-            entries
-      end
-    
-          def get_type
-        case slug
-        when 'api'
-          'Reference'
-        when 'configuration'
-          'Reference: Configuration'
-        when 'stpl'
-          'Reference: SimpleTemplate'
-        when 'plugindev'
-          'Reference: Plugin'
-        else
-          'Manual'
-        end
-      end
-    
-          def call
-        title('Gems')
-        table(all_gem_names) do |gem, row|
-          row.yellow if update_available?(gem)
-          row << gem
-          row << installed_gem_version(gem)
-          row << '(update available)' if update_available?(gem)
-        end
-      end
-    
-        # @abstract
-    #
-    # Copy the contents of the cache-repository onto the release path
-    #
-    # @return void
-    #
-    def release
-      raise NotImplementedError, 'Your SCM strategy module should provide a #release method'
-    end
-    
-      desc 'Reverted'
-  task :reverted do
-  end
