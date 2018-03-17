d0a5c6723cffbd993c273e11adb4205a33e82f60
SUNsung <weatherius@gmail.com>
2018-03-17 02:20:44 +0200

    Rewrite [11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py] File in: 2018-03-17 02:20:43

--- pyton/11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py
+++ pyton/11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py
@@ -1,213 +1,195 @@
 
         
-        
-def _mkdir(d):
-    if not os.path.exists(d):
-        os.mkdir(d)
+            plugin_manager.register(Plugin)
+    try:
+        r = http(
+            httpbin + BASIC_AUTH_URL,
+            '--auth-type',
+            Plugin.auth_type,
+        )
+        assert HTTP_OK in r
+        assert r.json == AUTH_OK
+    finally:
+        plugin_manager.unregister(Plugin)
     
-            def _find_spec(key):
-            m = re.search(
-                r'(?m)^//\s*%s:\s*(.*?)\n' % re.escape(key), as_content)
-            if not m:
-                raise ValueError('Cannot find %s in %s' % (key, testfile))
-            return json.loads(m.group(1))
+        def iter_lines(self, chunk_size):
+        yield self.body, b''
     
-    
-class AudiomackIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?audiomack\.com/song/(?P<id>[\w/-]+)'
-    IE_NAME = 'audiomack'
-    _TESTS = [
-        # hosted on audiomack
-        {
-            'url': 'http://www.audiomack.com/song/roosh-williams/extraordinary',
-            'info_dict':
-            {
-                'id': '310086',
-                'ext': 'mp3',
-                'uploader': 'Roosh Williams',
-                'title': 'Extraordinary'
-            }
-        },
-        # audiomack wrapper around soundcloud song
-        {
-            'add_ie': ['Soundcloud'],
-            'url': 'http://www.audiomack.com/song/hip-hop-daily/black-mamba-freestyle',
-            'info_dict': {
-                'id': '258901379',
-                'ext': 'mp3',
-                'description': 'mamba day freestyle for the legend Kobe Bryant ',
-                'title': 'Black Mamba Freestyle [Prod. By Danny Wolf]',
-                'uploader': 'ILOVEMAKONNEN',
-                'upload_date': '20160414',
-            }
-        },
-    ]
-    
-        def __init__(self, orig):
-        self._orig = orig
+                yield line.decode(self.msg.encoding) \
+                      .encode(self.output_encoding, 'replace') + lf
     
     
-@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
-@pytest.mark.parametrize('filename', filenames)
-def test_rst_file_syntax(filename):
-    p = subprocess.Popen(
-        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE
-    )
-    err = p.communicate()[1]
-    assert p.returncode == 0, err.decode('utf8')
+@pytest.mark.parametrize('follow_flag', ['--follow', '-F'])
+def test_follow_without_all_redirects_hidden(httpbin, follow_flag):
+    r = http(follow_flag, httpbin.url + '/redirect/2')
+    assert r.count('HTTP/1.1') == 1
+    assert HTTP_OK in r
+    
+        def is_new(self):
+        return not os.path.exists(self._get_path())
+    
+    '''
+__version__ = '1.0.0-dev'
+__author__ = 'Jakub Roztocil'
+__licence__ = 'BSD'
+    
+      def __init__(self, metagraph, model):
+    self._metagraph = metagraph
+    self.replicate_states(model.initial_state_name)
+    self.replicate_states(model.final_state_name)
+    self.update_snapshot_name('variables')
+    self.update_snapshot_name('trainable_variables')
+    
+      The PTB dataset comes from Tomas Mikolov's webpage:
+    
+            class_id = pred_dict['class_ids'][0]
+        probability = pred_dict['probabilities'][class_id]
+    
+        # 2. Evaluation mode
+    # Return our loss (which is used to evaluate our model)
+    # Set the TensorBoard scalar my_accurace to the accuracy
+    # Obs: This function only sets value during mode == ModeKeys.EVAL
+    # To set values during training, see tf.summary.scalar
+    if mode == tf.estimator.ModeKeys.EVAL:
+        return tf.estimator.EstimatorSpec(
+            mode,
+            loss=loss,
+            eval_metric_ops={'my_accuracy': accuracy})
+    
+    
+def optimize(loss, global_step):
+  return layers_lib.optimize(
+      loss, global_step, FLAGS.max_grad_norm, FLAGS.learning_rate,
+      FLAGS.learning_rate_decay_factor, FLAGS.sync_replicas,
+      FLAGS.replicas_to_aggregate, FLAGS.task)
 
     
-            See https://github.com/jakubroztocil/httpie/issues/212
+    Model trains with adversarial or virtual adversarial training.
     
-        def test_verify_no_OK(self, httpbin_secure):
-        r = http(httpbin_secure.url + '/get', '--verify=no')
-        assert HTTP_OK in r
+    py_test(
+    name = 'cifar10_input_test',
+    size = 'small',
+    srcs = ['cifar10_input_test.py'],
+    srcs_version = 'PY2AND3',
+    deps = [
+        ':cifar10_input',
+        '//tensorflow:tensorflow_py',
+        '//tensorflow/python:framework_test_lib',
+        '//tensorflow/python:platform_test',
+    ],
+)
     
-        ERROR_TIMEOUT = 2
-    ERROR_TOO_MANY_REDIRECTS = 6
+        it = 0
     
-            else:
-            raise ArgumentTypeError(
-                u''%s' is not a valid value' % string)
+            start = time.time()
+        func(X, n_jobs=1)
+        one_core.append(time.time() - start)
     
-        :param url: URL for the new :class:`Request` object.
-    :param params: (optional) Dictionary or bytes to be sent in the query string for the :class:`Request`.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    '''
+    try:
+    from urllib import urlopen
+except ImportError:
+    from urllib.request import urlopen
     
-                if length:
-                self.headers['Content-Length'] = builtin_str(length)
-            else:
-                self.headers['Transfer-Encoding'] = 'chunked'
-        else:
-            # Multi-part file uploads.
-            if files:
-                (body, content_type) = self._encode_files(files, data)
-            else:
-                if data:
-                    body = self._encode_params(data)
-                    if isinstance(data, basestring) or hasattr(data, 'read'):
-                        content_type = None
-                    else:
-                        content_type = 'application/x-www-form-urlencoded'
+    Sentiment analysis can be casted as a binary text classification problem,
+that is fitting a linear classifier on features extracted from the text
+of the user messages so as to guess wether the opinion of the author is
+positive or negative.
     
-        @pytest.fixture(autouse=True)
-    def setup(self):
-        '''CaseInsensitiveDict instance with 'Accept' header.'''
-        self.case_insensitive_dict = CaseInsensitiveDict()
-        self.case_insensitive_dict['Accept'] = 'application/json'
+    Second example
+--------------
+The second example shows the ability of the Minimum Covariance Determinant
+robust estimator of covariance to concentrate on the main mode of the data
+distribution: the location seems to be well estimated, although the covariance
+is hard to estimate due to the banana-shaped distribution. Anyway, we can
+get rid of some outlying observations.
+The One-Class SVM is able to capture the real data structure, but the
+difficulty is to adjust its kernel bandwidth parameter so as to obtain
+a good compromise between the shape of the data scatter matrix and the
+risk of over-fitting the data.
     
-    if is_py2:
-    from urllib import (
-        quote, unquote, quote_plus, unquote_plus, urlencode, getproxies,
-        proxy_bypass, proxy_bypass_environment, getproxies_environment)
-    from urlparse import urlparse, urlunparse, urljoin, urlsplit, urldefrag
-    from urllib2 import parse_http_list
-    import cookielib
-    from Cookie import Morsel
-    from StringIO import StringIO
+            clf_score = brier_score_loss(y_test, prob_pos, pos_label=y.max())
+        print('%s:' % name)
+        print('\tBrier: %1.3f' % (clf_score))
+        print('\tPrecision: %1.3f' % precision_score(y_test, y_pred))
+        print('\tRecall: %1.3f' % recall_score(y_test, y_pred))
+        print('\tF1: %1.3f\n' % f1_score(y_test, y_pred))
     
-    from requests.help import info
+        def mapper(self, _, line):
+        '''Parse each log line, extract and transform relevant lines.
     
-            # It's possible that users might accidentally send a Request object.
-        # Guard against that specific failure case.
-        if isinstance(request, Request):
-            raise ValueError('You can only send PreparedRequests.')
+    class QueryApi(object):
     
-    
-def maybe_restore_pretrained_model(sess, saver_for_restore, model_dir):
-  '''Restores pretrained model if there is no ckpt model.'''
-  ckpt = tf.train.get_checkpoint_state(FLAGS.train_dir)
-  checkpoint_exists = ckpt and ckpt.model_checkpoint_path
-  if checkpoint_exists:
-    tf.logging.info('Checkpoint exists in FLAGS.train_dir; skipping '
-                    'pretraining restore')
-    return
-    
-    
-def model_fn(features, labels, mode, params):
-  '''The model_fn argument for creating an Estimator.'''
-  model = Model(params['data_format'])
-  image = features
-  if isinstance(image, dict):
-    image = features['image']
-    
-          for i in range(3):
-        key, label, uint8image = sess.run([
-            result.key, result.label, result.uint8image])
-        self.assertEqual('%s:%d' % (filename, i), tf.compat.as_text(key))
-        self.assertEqual(labels[i], label)
-        self.assertAllEqual(expected[i], uint8image)
-    
-        # Build 2 hidden layer DNN with 10, 10 units respectively.
-    classifier = tf.estimator.DNNClassifier(
-        feature_columns=my_feature_columns,
-        # Two hidden layers of 10 nodes each.
-        hidden_units=[10, 10],
-        # The model must choose between 3 classes.
-        n_classes=3)
-    
-          content = row[1] + ' ' + row[2]
-      yield Document(
-          content=content,
-          is_validation=is_validation,
-          is_test=False,
-          label=int(row[0]) - 1,  # Labels should start from 0
-          add_tokens=True)
-    
-    
-def build_input_sequence(doc, vocab_ids):
-  '''Builds input sequence from file.
-    
-      # Filter out low-occurring terms
-  vocab_freqs = dict((term, freq) for term, freq in vocab_freqs.iteritems()
-                     if doc_counts[term] > FLAGS.doc_count_threshold)
-    
-    DOCUMENTATION = '''
----
-inventory: openshift
-short_description: Openshift gears external inventory script
-description:
-  - Generates inventory of Openshift gears using the REST interface
-  - this permit to reuse playbook to setup an Openshift gear
-version_added: None
-author: Michael Scherer
-'''
-    
-    
-def secure_hash(filename, hash_func=sha1):
-    ''' Return a secure hash hex digest of local file, None if file is not present or a directory. '''
-    
-            input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/targetHttpProxies/mytargetproxy'
-        actual = GCPUtils.parse_gcp_url(input_url)
-        self.assertEquals('compute', actual['service'])
-        self.assertEquals('v1', actual['api_version'])
-        self.assertEquals('myproject', actual['project'])
-        self.assertFalse('global' in actual)
-        self.assertEquals('targetHttpProxies', actual['resource_name'])
-        self.assertEquals('mytargetproxy', actual['entity_name'])
-    
-    from ansible.plugins.terminal import TerminalBase
-from ansible.errors import AnsibleConnectionFailure
-    
-    
-def get_group_vars(groups):
-    
-    
-def walk_workspace():
-    root = get_root()
-    files = subprocess.check_output(['git', 'ls-files', '--full-name',
-                                     '--', root])
-    for filename in files.splitlines():
-        yield os.path.join(root, filename)
-    
-    class CaptchaController(RedditController):
-    @allow_oauth2_access
-    @api_doc(api_section.captcha, uri='/captcha/{iden}')
-    def GET_captchaimg(self, iden):
+            Accessing a node updates its position to the front of the LRU list.
         '''
-        Request a CAPTCHA image given an `iden`.
+        node = self.lookup[query]
+        if node is None:
+            return None
+        self.linked_list.move_to_front(node)
+        return node.results
+    
+    
+class Car(Vehicle):
+    
+    site = Bigthink()
+download = site.download_by_url
+
+    
+    LIMIT = 100
+    
+    def kugou_download_by_hash(title,hash_val,output_dir = '.', merge = True, info_only = False):
+    #sample
+    #url_sample:http://www.kugou.com/yy/album/single/536957.html
+    #hash ->key  md5(hash+kgcloud')->key  decompile swf
+    #cmd 4 for mp3 cmd 3 for m4a
+    key=hashlib.new('md5',(hash_val+'kgcloud').encode('utf-8')).hexdigest()
+    html=get_html('http://trackercdn.kugou.com/i/?pid=6&key=%s&acceptMp3=1&cmd=4&hash=%s'%(key,hash_val))
+    j=loads(html)
+    url=j['url']
+    songtype, ext, size = url_info(url)
+    print_info(site_info, title, songtype, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir, merge=merge)
+    
+    def kuwo_download_by_rid(rid, output_dir = '.', merge = True, info_only = False):
+    html=get_content('http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_%s'%rid)
+    title=match1(html,r'<name>(.*)</name>')
+    #to get title
+    #format =aac|mp3 ->to get aac format=mp3 ->to get mp3
+    url=get_content('http://antiserver.kuwo.cn/anti.s?format=mp3&rid=MUSIC_%s&type=convert_url&response=url'%rid)
+    songtype, ext, size = url_info(url)
+    print_info(site_info, title, songtype, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir)
+    
+    def miomio_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+    html = get_html(url)
+    
+    site_info = 'Mixcloud.com'
+download = mixcloud_download
+download_playlist = playlist_not_supported('mixcloud')
+
+    
+        def extract(self, **kwargs):
+        for i in self.streams:
+            # for each available stream
+            s = self.streams[i]
+            # fill in 'container' field and 'size' field (optional)
+            _, s['container'], s['size'] = url_info(s['url'])
+            # 'src' field is a list of processed urls for direct downloading
+            # usually derived from 'url'
+            s['src'] = [s['url']]
+    
+    def showroom_download_by_room_id(room_id, output_dir = '.', merge = False, info_only = False, **kwargs):
+    '''Source: Android mobile'''
+    while True:
+        timestamp = str(int(time() * 1000))
+        api_endpoint = 'https://www.showroom-live.com/api/live/streaming_url?room_id={room_id}&_={timestamp}'.format(room_id = room_id, timestamp = timestamp)
+        html = get_content(api_endpoint)
+        html = json.loads(html)
+        #{'streaming_url_list': [{'url': 'rtmp://52.197.69.198:1935/liveedge', 'id': 1, 'label': 'original spec(low latency)', 'is_default': True, 'type': 'rtmp', 'stream_name': '7656a6d5baa1d77075c971f6d8b6dc61b979fc913dc5fe7cc1318281793436ed'}, {'url': 'http://52.197.69.198:1935/liveedge/7656a6d5baa1d77075c971f6d8b6dc61b979fc913dc5fe7cc1318281793436ed/playlist.m3u8', 'is_default': True, 'id': 2, 'type': 'hls', 'label': 'original spec'}, {'url': 'rtmp://52.197.69.198:1935/liveedge', 'id': 3, 'label': 'low spec(low latency)', 'is_default': False, 'type': 'rtmp', 'stream_name': '7656a6d5baa1d77075c971f6d8b6dc61b979fc913dc5fe7cc1318281793436ed_low'}, {'url': 'http://52.197.69.198:1935/liveedge/7656a6d5baa1d77075c971f6d8b6dc61b979fc913dc5fe7cc1318281793436ed_low/playlist.m3u8', 'is_default': False, 'id': 4, 'type': 'hls', 'label': 'low spec'}]}
+        if len(html) >= 1:
+            break
+        log.w('The live show is currently offline.')
+        sleep(1)
+    
+        point = readme_soup.find_all('h1')[1]
