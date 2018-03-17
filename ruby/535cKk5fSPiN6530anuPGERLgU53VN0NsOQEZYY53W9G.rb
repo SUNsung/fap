e76e46691162a477e41094563f73baa04c35ef97
SUNsung <weatherius@gmail.com>
2018-03-17 13:09:08 +0200

    Rewrite [535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb] File in: 2018-03-17 13:09:07

--- ruby/535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb
+++ ruby/535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb
@@ -1,43 +1,159 @@
 
         
-                expect(result).to eq('/usr/local/bin/cloc --exclude-dir=test1,test2,build --by-file --xml  --out=build/cloc.xml')
+          def self.register(user, app_id, params, challenges)
+    u2f = U2F::U2F.new(app_id)
+    registration = self.new
+    
+      MAIN_KEYS = %w[title message text main value]
+    
+        validate :validate_evernote_options
+    
+          if options[:type] == :array && (options[:values].blank? || !options[:values].is_a?(Array))
+        raise ArgumentError.new('When using :array as :type you need to provide the :values as an Array')
       end
     
-          it 'gets the correct version number for 'TargetB'' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          get_version_number(xcodeproj: '.xcproject', target: 'TargetB')
-        end').runner.execute(:test)
-        expect(result).to eq('5.4.3')
-      end
-    
-            expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER]).to match(/cd .* && agvtool new-version -all 24 \&\& cd \-/)
-      end
-    end
+        # Optional
+    #   Use this method the gracefully stop your agent but make sure the run method return, or
+    #   terminate the thread.
+    def stop; end
   end
 end
-
+=end
+module LongRunnable
+  extend ActiveSupport::Concern
     
-          def report
-        <<-EOS
+      def destroy_failed
+    Delayed::Job.where.not(failed_at: nil).delete_all
     
-          def update_if_necessary!
-        if @update && config.verbose?
-          UI.section('\nUpdating Spec Repositories\n'.yellow) do
-            Repo.new(ARGV.new(['update'])).run
-          end
-        end
+    
+    {      delta = 1
+      stat = File.stat(path)
+      assert_in_delta tb,   stat.birthtime.to_f, delta
+      assert_in_delta t0+2, stat.mtime.to_f, delta
+      if stat.birthtime != stat.ctime
+        assert_in_delta t0+4, stat.ctime.to_f, delta
       end
+      if /mswin|mingw/ !~ RUBY_PLATFORM && !Bug::File::Fs.noatime?(path)
+        # Windows delays updating atime
+        assert_in_delta t0+6, stat.atime.to_f, delta
+      end
+    }
+  rescue NotImplementedError
+  end
     
-            def find_address
-          if @order.bill_address_id == params[:id].to_i
-            @order.bill_address
-          elsif @order.ship_address_id == params[:id].to_i
-            @order.ship_address
+        ENV[PATH_ENV] = '/tmp/'.taint
+    assert_equal('/tmp/', ENV[PATH_ENV])
+  end
+    
+      def test_key2?
+    assert_not_send([@cls[], :key?, 1])
+    assert_not_send([@cls[], :key?, nil])
+    assert_send([@h, :key?, nil])
+    assert_send([@h, :key?, 1])
+    assert_not_send([@h, :key?, 'gumby'])
+  end
+    
+    
+    {      assert_equal(false, Set[].proper_subset?(klass[]), klass.name)
+    }
+  end
+    
+      def self.status_of_blocked_thread
+    m = Mutex.new
+    m.lock
+    t = Thread.new { m.lock }
+    Thread.pass while t.status and t.status != 'sleep'
+    status = Status.new t
+    m.unlock
+    t.join
+    status
+  end
+    
+      it 'raises an ArgumentError if not passed a block' do
+    lambda {
+      Thread.send(@method)
+    }.should raise_error(ArgumentError)
+  end
+    
+              render 'admins/pods'
+        end
+        format.mobile { render 'admins/pods' }
+        format.json { render json: pods_json }
+      end
+    end
+    
+        @posts_per_day = Post.where('created_at >= ?', Date.today - 21.days).group('DATE(created_at)').order('DATE(created_at) ASC').count
+    @most_posts_within = @posts_per_day.values.max.to_f
+    
+    # A logger that delays messages until they're explicitly flushed to an inner
+# logger.
+#
+# This can be installed around the current logger by calling \{#install!}, and
+# the original logger can be replaced by calling \{#uninstall!}. The log
+# messages can be flushed by calling \{#flush}.
+class Sass::Logger::Delayed < Sass::Logger::Base
+  # Installs a new delayed logger as the current Sass logger, wrapping the
+  # original logger.
+  #
+  # This can be undone by calling \{#uninstall!}.
+  #
+  # @return [Sass::Logger::Delayed] The newly-created logger.
+  def self.install!
+    logger = Sass::Logger::Delayed.new(Sass.logger)
+    Sass.logger = logger
+    logger
+  end
+    
+            def log_level(name, options = {})
+          if options[:prepend]
+            level = log_levels.values.min
+            level = level.nil? ? 0 : level - 1
           else
-            raise CanCan::AccessDenied
+            level = log_levels.values.max
+            level = level.nil? ? 0 : level + 1
           end
+          log_levels.update(name => level)
+          define_logger(name)
         end
-      end
+    
+        # From asking people, it seems MacPorts does not have a `prefix` command, like
+    # Homebrew does, so make an educated guess:
+    if port_prefix = prefix_from_bin('port')
+      prefixes << port_prefix
     end
+    
+          # @param  [Xcodeproj::Project] project
+      #         The xcode project to generate a podfile for.
+      #
+      # @return [String] the text of the Podfile for the provided project
+      #
+      def podfile_template(project)
+        podfile = ''
+        podfile << 'project '#{@project_path}'\n\n' if @project_path
+        podfile << <<-PLATFORM.strip_heredoc
+          # Uncomment the next line to define a global platform for your project
+          # platform :ios, '9.0'
+        PLATFORM
+    
+          def self.options
+        [
+          ['--update', 'Run `pod repo update` before listing'],
+          ['--stats',  'Show additional stats (like GitHub watchers and forks)'],
+        ].concat(super)
+      end
+    
+        it 'redirects requests with duplicate session cookies' do
+      get '/', {}, 'HTTP_COOKIE' => 'rack.session=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
+      expect(last_response).to be_redirect
+      expect(last_response.location).to eq('/')
+    end
+    
+      it 'denies requests with a changing Accept-Language header' do
+    session = {:foo => :bar}
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'b'
+    expect(session).to be_empty
   end
-end
+    
+        expect(get('/', {}, 'wants' => 'application/xhtml').headers['X-XSS-Protection']).to eq('1; mode=foo')
+  end
