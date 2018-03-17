40da3feb91e15543ba5f169c733d505206b98670
SUNsung <weatherius@gmail.com>
2018-03-17 11:36:32 +0200

    Rewrite [572PnbIzUn.Ek57MW82AAbGao57yg4zL613sPI57Cmx.py] File in: 2018-03-17 11:36:32

--- pyton/572PnbIzUn.Ek57MW82AAbGao57yg4zL613sPI57Cmx.py
+++ pyton/572PnbIzUn.Ek57MW82AAbGao57yg4zL613sPI57Cmx.py
@@ -1,78 +1,85 @@
 
         
-        
-def get_db():
-    '''Opens a new database connection if there is none yet for the
-    current application context.
-    '''
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+          word_to_id = _build_vocab(train_path)
+  train_data = _file_to_word_ids(train_path, word_to_id)
+  valid_data = _file_to_word_ids(valid_path, word_to_id)
+  test_data = _file_to_word_ids(test_path, word_to_id)
+  vocabulary = len(word_to_id)
+  return train_data, valid_data, test_data, vocabulary
     
-        Tests the Flaskr application.
+      Returns:
+    2D np.array where each row contains the magnitudes of the fft_length/2+1
+    unique values of the FFT for the corresponding frame of input samples.
+  '''
+  frames = frame(signal, window_length, hop_length)
+  # Apply frame window to each frame. We use a periodic Hann (cosine of period
+  # window_length) instead of the symmetric Hann of np.hanning (period
+  # window_length-1).
+  window = periodic_hann(window_length)
+  windowed_frames = frames * window
+  return np.abs(np.fft.rfft(windowed_frames, int(fft_length)))
     
+      def setUp(self):
+    FLAGS.train_data = os.path.join(self.get_temp_dir(), 'test-text.txt')
+    FLAGS.eval_data = os.path.join(self.get_temp_dir(), 'eval-text.txt')
+    FLAGS.save_path = self.get_temp_dir()
+    with open(FLAGS.train_data, 'w') as f:
+      f.write(
+          '''alice was beginning to get very tired of sitting by her sister on
+          the bank, and of having nothing to do: once or twice she had peeped
+          into the book her sister was reading, but it had no pictures or
+          conversations in it, 'and what is the use of a book,' thought alice
+          'without pictures or conversations?' So she was considering in her own
+          mind (as well as she could, for the hot day made her feel very sleepy
+          and stupid), whether the pleasure of making a daisy-chain would be
+          worth the trouble of getting up and picking the daisies, when suddenly
+          a White rabbit with pink eyes ran close by her.\n''')
+      with open(FLAGS.eval_data, 'w') as f:
+        f.write('alice she rabbit once\n')
     
-@keras_test
-def test_weighted_metrics_with_weighted_accuracy_metric():
-    model = create_sequential_model()
-    model.compile(loss=loss, optimizer='rmsprop', metrics=['acc'], weighted_metrics=['acc'])
+      def setUp(self):
+    self._string_data = '\n'.join(
+        [' hello there i am',
+         ' rain as day',
+         ' want some cheesy puffs ?'])
     
+      Data from http://ai.stanford.edu/~amaas/data/sentiment/
     
-@keras_test
-def test_locallyconnected_1d():
-    num_samples = 2
-    num_steps = 8
-    input_dim = 5
-    filter_length = 3
-    filters = 4
-    padding = 'valid'
-    strides = 1
-    
-    model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-    
-    
-@keras_test
-def test_convert_weights():
-    def get_model(shape, data_format):
-        model = Sequential()
-        model.add(Conv2D(filters=2,
-                         kernel_size=(4, 3),
-                         input_shape=shape,
-                         data_format=data_format))
-        model.add(Flatten())
-        model.add(Dense(5))
-        return model
-    
-    It is recommended to run this script on GPU, as recurrent
-networks are quite computationally intensive.
-    
-    # Set backend based on KERAS_BACKEND flag, if applicable.
-if 'KERAS_BACKEND' in os.environ:
-    _backend = os.environ['KERAS_BACKEND']
-    assert _backend in {'theano', 'tensorflow', 'cntk'}
-    _BACKEND = _backend
-    
-    class DefaultCategories(Enum):
+    flags.DEFINE_boolean('use_unlabeled', True, 'Whether to use the '
+                     'unlabeled sentiment dataset in the vocabulary.')
+flags.DEFINE_boolean('include_validation', False, 'Whether to include the '
+                     'validation set in the vocabulary.')
+flags.DEFINE_integer('doc_count_threshold', 1, 'The minimum number of '
+                     'documents a word or bigram should occur in to keep '
+                     'it in the vocabulary.')
     
     
-class PersonServer(object):
+def _build_random_vocabulary(vocab_size=100):
+  '''Builds and returns a dict<term, id>.'''
+  vocab = set()
+  while len(vocab) < (vocab_size - 1):
+    rand_word = ''.join(
+        random.choice(string.ascii_lowercase)
+        for _ in range(random.randint(1, 10)))
+    vocab.add(rand_word)
     
-        @mock.patch(
-        'certbot_compatibility_test.validator.crypto_util.probe_sni')
-    def test_certificate_success(self, mock_probe_sni):
-        cert = OpenSSL.crypto.X509()
-        mock_probe_sni.return_value = cert
-        self.assertTrue(self.validator.certificate(
-            cert, 'test.com', '127.0.0.1'))
     
-    # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+def main(_):
+  '''Trains Language Model.'''
+  tf.logging.set_verbosity(tf.logging.INFO)
+  with tf.device(tf.train.replica_device_setter(FLAGS.ps_tasks)):
+    model = graphs.get_model()
+    train_op, loss, global_step = model.language_model_training()
+    train_utils.run_training(train_op, loss, global_step)
     
-    '''
-import zope.interface
-    
-    import os
-import json
-import click
+    py_binary(
+    name = 'cifar10_multi_gpu_train',
+    srcs = [
+        'cifar10_multi_gpu_train.py',
+    ],
+    srcs_version = 'PY2AND3',
+    visibility = ['//tensorflow:__subpackages__'],
+    deps = [
+        ':cifar10',
+    ],
+)
