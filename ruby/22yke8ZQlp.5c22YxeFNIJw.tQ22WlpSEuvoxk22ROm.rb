e6fd0a52f88c2437d805c2138e183939a038eb68
SUNsung <weatherius@gmail.com>
2018-03-17 15:43:27 +0200

    Rewrite [22yke8ZQlp.5c22YxeFNIJw.tQ22WlpSEuvoxk22ROm.rb] File in: 2018-03-17 15:43:27

--- ruby/22yke8ZQlp.5c22YxeFNIJw.tQ22WlpSEuvoxk22ROm.rb
+++ ruby/22yke8ZQlp.5c22YxeFNIJw.tQ22WlpSEuvoxk22ROm.rb
@@ -1,103 +1,142 @@
 
         
-              # Returns constant of subscription adapter specified in config/cable.yml.
-      # If the adapter cannot be found, this will default to the Redis adapter.
-      # Also makes sure proper dependencies are required.
-      def pubsub_adapter
-        adapter = (cable.fetch('adapter') { 'redis' })
+          setup do
+    @old_escape_html_entities_in_json = ActiveSupport.escape_html_entities_in_json
+    ActiveSupport.escape_html_entities_in_json = true
+    @template = self
+    @request = Class.new do
+      def send_early_hints(links) end
+    end.new
+  end
     
-        def env_for(url)
-      Rack::MockRequest.env_for(url)
+                  when_connected do
+                send_command('unsubscribe')
+                @raw_client = nil
+              end
+            end
+    
+    class TestResponseTest < ActiveSupport::TestCase
+  def assert_response_code_range(range, predicate)
+    response = ActionDispatch::TestResponse.new
+    (0..599).each do |status|
+      response.status = status
+      assert_equal range.include?(status), response.send(predicate),
+                   'ActionDispatch::TestResponse.new(#{status}).#{predicate}'
     end
   end
     
-      def test_dasherized_keys_as_json
-    with_test_route_set do
-      post '/?full=1',
-        params: '{'first-key':{'sub-key':'...'}}',
-        headers: { 'CONTENT_TYPE' => 'application/json' }
-      assert_equal 'action, controller, first-key(sub-key), full', @controller.response.body
-      assert_equal '...', @controller.params['first-key']['sub-key']
-    end
-  end
-    
-    require 'active_job'
-    
-        # Returns +text+ wrapped at +len+ columns and indented +indent+ spaces.
-    # By default column length +len+ equals 72 characters and indent
-    # +indent+ equal two spaces.
-    #
-    #   my_text = 'Here is a sample text with more than 40 characters'
-    #
-    #   format_paragraph(my_text, 25, 4)
-    #   # => '    Here is a sample text with\n    more than 40 characters'
-    def format_paragraph(text, len = 72, indent = 2)
-      sentences = [[]]
-    
-    module ActionMailer
-  class NonInferrableMailerError < ::StandardError
-    def initialize(name)
-      super 'Unable to determine the mailer to test from #{name}. ' \
-        'You'll need to specify it using tests YourMailer in your ' \
-        'test case definition'
-    end
-  end
-    
-      # GET /resource/sign_in
-  def new
-    self.resource = resource_class.new(sign_in_params)
-    clean_up_passwords(resource)
-    yield resource if block_given?
-    respond_with(resource, serialize_options(resource))
-  end
-    
-      # Sets the flash message with :key, using I18n. By default you are able
-  # to set up your messages using specific resource scope, and if no message is
-  # found we look to the default scope. Set the 'now' options key to a true
-  # value to populate the flash.now hash in lieu of the default flash hash (so
-  # the flash message will be available to the current action instead of the
-  # next action).
-  # Example (i18n locale file):
-  #
-  #   en:
-  #     devise:
-  #       passwords:
-  #         #default_scope_messages - only if resource_scope is not found
-  #         user:
-  #           #resource_scope_messages
-  #
-  # Please refer to README or en.yml locale file to check what messages are
-  # available.
-  def set_flash_message(key, kind, options = {})
-    message = find_message(kind, options)
-    if options[:now]
-      flash.now[key] = message if message.present?
-    else
-      flash[key] = message if message.present?
-    end
-  end
-    
-        def reset_password_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :reset_password_instructions, opts)
+        test 'the middleware stack is exposed as 'middleware' in the controller' do
+      result = @app.call(env_for('/'))
+      assert_equal 'First!', result[1]['Middleware-Order']
     end
     
-          generate_helpers!(Devise::URL_HELPERS)
-    
-          def stages
-        names = Dir[stage_definitions].map { |f| File.basename(f, '.rb') }
-        assert_valid_stage_names(names)
-        names
+          def handle_exception_with_mailer_class(exception)
+        if klass = mailer_class
+          klass.handle_exception exception
+        else
+          raise exception
+        end
       end
+  end
+end
+
     
-        def after(task, post_task, *args, &block)
-      Rake::Task.define_task(post_task, *args, &block) if block_given?
-      task = Rake::Task[task]
-      task.enhance do
-        post = Rake.application.lookup(post_task, task.scope)
-        raise ArgumentError, 'Task #{post_task.inspect} not found' unless post
-        post.invoke
+        included do
+      # Do not make this inheritable, because we always want it to propagate
+      cattr_accessor :raise_delivery_errors, default: true
+      cattr_accessor :perform_deliveries, default: true
+      cattr_accessor :deliver_later_queue_name, default: :mailers
+    
+          indentation = ' ' * indent
+      sentences.map! { |sentence|
+        '#{indentation}#{sentence.join(' ')}'
+      }.join '\n'
+    end
+  end
+end
+
+    
+            def restore_test_deliveries
+          restore_delivery_method
+          ActionMailer::Base.perform_deliveries = @old_perform_deliveries
+        end
+    
+        AssertSelectMailer.test('<div><p>foo</p><p>bar</p></div>').deliver_now
+    assert_select_email do
+      assert_select 'div:root' do
+        assert_select 'p:first-child', 'foo'
+        assert_select 'p:last-child', 'bar'
       end
     end
+  end
     
-      mkdir_p tasks_dir
+      test 'undefined delivery methods raises errors' do
+    DeliveryMailer.delivery_method = nil
+    error = assert_raise RuntimeError do
+      DeliveryMailer.welcome.deliver_now
+    end
+    assert_equal 'Delivery method cannot be nil', error.message
+  end
+    
+    # No trailing slash
+Benchmark.ips do |x|
+  path = '/some/very/very/long/path/to/a/file/i/like/'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
+end
+
+    
+    CONTENT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+    
+    # You can also do other things. Examples:
+# https://github.com/tmm1/stackprof/blob/master/bin/stackprof
+report = StackProf::Report.new(Marshal.load(IO.binread(PROF_OUTPUT_FILE)))
+report.print_text(
+  options[:sort],
+  options[:limit],
+  options[:select_files],
+  options[:reject_files],
+  options[:select_names],
+  options[:reject_names]
+)
+    
+            parsed_expr = parse_expression(expression)
+        @context.stack do
+          groups = input.group_by do |item|
+            @context[variable] = item
+            parsed_expr.render(@context)
+          end
+          grouped_array(groups)
+        end
+      end
+    
+          def render!(*args)
+        measure_time do
+          measure_bytes do
+            @template.render!(*args)
+          end
+        end
+      end
+    
+        it 'accepts #{method} requests with whitelisted Origin' do
+      mock_app do
+        use Rack::Protection::HttpOrigin, :origin_whitelist => ['http://www.friend.com']
+        run DummyApp
+      end
+      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://www.friend.com')).to be_ok
+    end
+  end
+end
