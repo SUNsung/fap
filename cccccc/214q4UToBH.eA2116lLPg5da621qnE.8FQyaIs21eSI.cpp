fb7959abf5ffa24ef1d3e0b1e6b7c736b1ba17ef
SUNsung <weatherius@gmail.com>
2018-03-17 14:35:21 +0200

    Rewrite [214q4UToBH.eA2116lLPg5da621qnE.8FQyaIs21eSI.cpp] File in: 2018-03-17 14:35:20

--- cccccc/214q4UToBH.eA2116lLPg5da621qnE.8FQyaIs21eSI.cpp
+++ cccccc/214q4UToBH.eA2116lLPg5da621qnE.8FQyaIs21eSI.cpp
@@ -1,70 +1,130 @@
 
         
-          /**
-   * @brief Applies the transformation defined in the data layer's
-   * transform_param block to the data.
-   *
-   * @param datum
-   *    Datum containing the data to be transformed.
-   * @param transformed_blob
-   *    This is destination blob. It can be part of top blob's data if
-   *    set_cpu_data() is used. See data_layer.cpp for an example.
-   */
-  void Transform(const Datum& datum, Blob<Dtype>* transformed_blob);
-    
-    
-    {  Blob<Dtype> col_buffer_;
-  Blob<Dtype> bias_multiplier_;
+        typedef NS_ENUM(NSInteger, ObjectBehaviorAction) {
+    ObjectBehaviorActionRetain,
+    ObjectBehaviorActionCopy,
+    ObjectBehaviorActionMutableCopy
 };
     
-     protected:
-  /// @copydoc BNLLLayer
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-    #endif  // CAFFE_CONCAT_LAYER_HPP_
+    #endif // SWIFT_INDEX_INDEXRECORD_H
 
     
-    namespace caffe {
+      Optional<const swift::markup::ThrowsField*> getThrowsField() const {
+    return Parts.ThrowsField;
+  }
+    
+    
+    {  static bool classof(const MarkupASTNode *N) {
+    return N->getKind() == ASTNodeKind::ParamField;
+  }
+};
+    
+    SILDebugScope::SILDebugScope(SILLocation Loc, SILFunction *SILFn,
+                             const SILDebugScope *ParentScope ,
+                             const SILDebugScope *InlinedCallSite)
+    : Loc(Loc), InlinedCallSite(InlinedCallSite) {
+  if (ParentScope)
+    Parent = ParentScope;
+  else {
+    assert(SILFn && 'no parent provided');
+    Parent = SILFn;
+  }
+}
+    
+    #endif
+
+    
+    namespace swift {
     }
     
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
+    #endif // SWIFT_SYNTAX_REFERENCES_H
+
     
-    namespace caffe {
+    namespace tesseract {
     }
     
-      /**
-   * @brief Computes the error gradient w.r.t. the forwarded inputs.
-   *
-   * @param top output Blob vector (length 1+), providing the error gradient with
-   *        respect to the outputs
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2+), into which the top error
-   *        gradient is copied
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+      // Connects this and other, discarding any existing connections.
+  void Connect(DoublePtr* other) {
+    other->Disconnect();
+    Disconnect();
+    other->other_end_ = this;
+    other_end_ = other;
+  }
+  // Disconnects this and other, making OtherEnd() return NULL for both.
+  void Disconnect() {
+    if (other_end_ != NULL) {
+      other_end_->other_end_ = NULL;
+      other_end_ = NULL;
+    }
+  }
+  // Returns the pointer to the other end of the double pointer.
+  DoublePtr* OtherEnd() const {
+    return other_end_;
+  }
     
-    #define _CRT_NONSTDC_NO_DEPRECATE // make VS accept POSIX functions without _
+    /*---------------------------------------------------------------------------
+          Macros
+----------------------------------------------------------------------------*/
+/* macros for controlling the display of recognized characters */
+#define EnableCharDisplay()   (DisplayCharacters = TRUE)
+#define DisableCharDisplay()    (DisplayCharacters = FALSE)
+    
+    // Clear all data.
+void IntFeatureDist::Clear() {
+  delete [] features_;
+  features_ = NULL;
+  delete [] features_delta_one_;
+  features_delta_one_ = NULL;
+  delete [] features_delta_two_;
+  features_delta_two_ = NULL;
+}
+    
+        size_t m_prevChosenMinibatchSize;
+    double m_lastFinishedEpochTrainLoss;
+    
+        float alpha = 1.0f * rng() / rng.max();
+    Matrix<float>::ScaleAndAdd(alpha, mAdense, mBdense);
+    
+    PrefetchGPUDataTransferer::~PrefetchGPUDataTransferer()
+{
+    try
+    {
+        PrepareDevice(m_deviceId);
+    }
+    catch (...)
+    {
+        // the error is already logged
+        return;
+    }
+    }
+    
+    private:
+    ComputationNetworkPtr m_net;
     
     // ---------------------------------------------------------------------------
-// ConfigException -- all errors from processing the config files are reported as ConfigException
+// Expression -- the entire config is a tree of Expression types
+// We don't use polymorphism here because C++ is so verbose...
 // ---------------------------------------------------------------------------
     
-    #pragma once
-    
-    public:
-    typedef msra::dbn::latticepair latticepair;
-    latticesource(std::pair<std::vector<std::wstring>, std::vector<std::wstring>> latticetocs, const std::unordered_map<std::string, size_t>& modelsymmap, std::wstring RootPathInToc)
-        : numlattices(latticetocs.first, modelsymmap, RootPathInToc), denlattices(latticetocs.second, modelsymmap, RootPathInToc), verbosity(0)
-    {
+            // key=Function(x,y,z) - function with return
+        // HDim=256 - NDL inline
+        // model1=[...] - Embedded NDL script
+        if (equalFound)
+        {
+            size_t tokenStartNew = keyEnd + 1;
+            if (!(tokenStartNew < tokenEnd))
+                RuntimeError('Equal at the end of line not allowed');
+            std::string rightValue = stringParse.substr(tokenStartNew, tokenEnd - tokenStartNew);
+            Trim(rightValue);
     }
+    
+    template<class TString>
+inline bool AreEqualIgnoreCase(
+    const TString& s1,
+    const typename TString::value_type* s2pointer)
+{
+    return AreEqualIgnoreCase(s1, TString(s2pointer));
+}
     
     // ===========================================================================
 // float4 -- wrapper around the rather ugly SSE intrinsics for float[4]
@@ -77,159 +137,57 @@
 // newer ones: (seems no single list available)
 // ===========================================================================
     
-    vector<wstring> /*IConfigRecord::*/ ComputationNodeBase::GetMemberIds() const
-{
-    return vector<wstring>{ L'name', L'operation', L'dim', L'dims', /*L'tag', */L'inputs', OperationName() + L'Args' };
+    #include <regex>
+#include <iostream>
+    
+    FilterBlockReader::FilterBlockReader(const FilterPolicy* policy,
+                                     const Slice& contents)
+    : policy_(policy),
+      data_(NULL),
+      offset_(NULL),
+      num_(0),
+      base_lg_(0) {
+  size_t n = contents.size();
+  if (n < 5) return;  // 1 byte for base_lg_ and 4 for start of offset array
+  base_lg_ = contents[n-1];
+  uint32_t last_word = DecodeFixed32(contents.data() + n - 5);
+  if (last_word > n - 5) return;
+  data_ = contents.data();
+  offset_ = data_ + last_word;
+  num_ = (n - 5 - last_word) / 4;
 }
     
-    int main(int, char**)
-{
-    // Setup Allegro
-    al_init();
-    al_install_keyboard();
-    al_install_mouse();
-    al_init_primitives_addon();
-    al_set_new_display_flags(ALLEGRO_RESIZABLE);
-    ALLEGRO_DISPLAY* display = al_create_display(1280, 720);
-    al_set_window_title(display, 'ImGui Allegro 5 example');
-    ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
-    al_register_event_source(queue, al_get_display_event_source(display));
-    al_register_event_source(queue, al_get_keyboard_event_source());
-    al_register_event_source(queue, al_get_mouse_event_source());
-    }
-    
-        // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them. 
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple. 
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Read 'misc/fonts/README.txt' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Roboto-Medium.ttf', 16.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Cousine-Regular.ttf', 15.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/DroidSans.ttf', 16.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/ProggyTiny.ttf', 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF('c:\\Windows\\Fonts\\ArialUni.ttf', 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you use this binding you'll need to call 5 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXX_CreateFontsTexture(), ImGui_ImplXXXX_NewFrame(), ImGui_ImplXXXX_Render() and ImGui_ImplXXXX_Shutdown().
-// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-      /// Search the list for the (index)'th item (0-based) in (list:key)
-  /// A negative index indicates: 'from end-of-list'
-  /// If index is within range: return true, and return the value in *result.
-  /// If (index < -length OR index>=length), then index is out of range:
-  ///   return false (and *result is left unchanged)
-  /// May throw RedisListException
-  bool Index(const std::string& key, int32_t index,
-             std::string* result);
-    
-    /*
- * Class:     org_rocksdb_IngestExternalFileOptions
- * Method:    newIngestExternalFileOptions
- * Signature: ()J
- */
-jlong Java_org_rocksdb_IngestExternalFileOptions_newIngestExternalFileOptions__(
-    JNIEnv* env, jclass jclazz) {
-  auto* options = new rocksdb::IngestExternalFileOptions();
-  return reinterpret_cast<jlong>(options);
-}
-    
-    #include 'rocksjni/statisticsjni.h'
-    
-    Status DateTieredDBImpl::Write(const WriteOptions& opts, WriteBatch* updates) {
-  class Handler : public WriteBatch::Handler {
-   public:
-    explicit Handler() {}
-    WriteBatch updates_ttl;
-    Status batch_rewrite_status;
-    virtual Status PutCF(uint32_t column_family_id, const Slice& key,
-                         const Slice& value) override {
-      WriteBatchInternal::Put(&updates_ttl, column_family_id, key, value);
-      return Status::OK();
-    }
-    virtual Status MergeCF(uint32_t column_family_id, const Slice& key,
-                           const Slice& value) override {
-      WriteBatchInternal::Merge(&updates_ttl, column_family_id, key, value);
-      return Status::OK();
-    }
-    virtual Status DeleteCF(uint32_t column_family_id,
-                            const Slice& key) override {
-      WriteBatchInternal::Delete(&updates_ttl, column_family_id, key);
-      return Status::OK();
-    }
-    virtual void LogData(const Slice& blob) override {
-      updates_ttl.PutLogData(blob);
-    }
-  };
-  Handler handler;
-  updates->Iterate(&handler);
-  if (!handler.batch_rewrite_status.ok()) {
-    return handler.batch_rewrite_status;
-  } else {
-    return db_->Write(opts, &(handler.updates_ttl));
+      const SnapshotImpl* New(SequenceNumber seq) {
+    SnapshotImpl* s = new SnapshotImpl;
+    s->number_ = seq;
+    s->list_ = this;
+    s->next_ = &list_;
+    s->prev_ = list_.prev_;
+    s->prev_->next_ = s;
+    s->next_->prev_ = s;
+    return s;
   }
-}
     
-    void DupAndCloseStderr() {
-  int OutputFd = DuplicateFile(2);
-  if (OutputFd > 0) {
-    FILE *NewOutputFile = OpenFile(OutputFd, 'w');
-    if (NewOutputFile) {
-      OutputFile = NewOutputFile;
-      if (EF->__sanitizer_set_report_fd)
-        EF->__sanitizer_set_report_fd(reinterpret_cast<void *>(OutputFd));
-      CloseFile(2);
-    }
+      for (size_t i = 0; i < compact_pointers_.size(); i++) {
+    PutVarint32(dst, kCompactPointer);
+    PutVarint32(dst, compact_pointers_[i].first);  // level
+    PutLengthPrefixedSlice(dst, compact_pointers_[i].second.Encode());
   }
+    
+    TEST(CRC, Extend) {
+  ASSERT_EQ(Value('hello world', 11),
+            Extend(Value('hello ', 6), 'world', 5));
 }
     
-    void DupAndCloseStderr();
+    #include 'guetzli/dct_double.h'
     
-    std::string DirName(const std::string &FileName) {
-  char *Tmp = new char[FileName.size() + 1];
-  memcpy(Tmp, FileName.c_str(), FileName.size() + 1);
-  std::string Res = dirname(Tmp);
-  delete [] Tmp;
-  return Res;
-}
+    // Functions for writing a JPEGData object into a jpeg byte stream.
     
-    using namespace std::chrono;
-    
-    #endif  // LLVM_FUZZER_OPTIONS_H
-
-    
-    #ifndef SHA_BIG_ENDIAN
-	// Swap byte order back
-	int i;
-	for (i=0; i<5; i++) {
-		s->state[i]=
-			  (((s->state[i])<<24)& 0xff000000)
-			| (((s->state[i])<<8) & 0x00ff0000)
-			| (((s->state[i])>>8) & 0x0000ff00)
-			| (((s->state[i])>>24)& 0x000000ff);
-	}
-#endif
-    
-    // Computes SHA1 hash of 'Len' bytes in 'Data', writes kSHA1NumBytes to 'Out'.
-void ComputeSHA1(const uint8_t *Data, size_t Len, uint8_t *Out);
-    
-      void TraceSwitchCallback(uintptr_t PC, size_t ValSizeInBits, uint64_t Val,
-                           size_t NumCases, uint64_t *Cases);
-  int TryToAddDesiredData(uint64_t PresentData, uint64_t DesiredData,
-                          size_t DataSize);
-  int TryToAddDesiredData(const uint8_t *PresentData,
-                          const uint8_t *DesiredData, size_t DataSize);
-    
-    bool ExecuteCommandAndReadOutput(const std::string &Command, std::string *Out) {
-  FILE *Pipe = OpenProcessPipe(Command.c_str(), 'r');
-  if (!Pipe) return false;
-  char Buff[1024];
-  size_t N;
-  while ((N = fread(Buff, 1, sizeof(Buff), Pipe)) > 0)
-    Out->append(Buff, N);
-  return true;
+    double ButteraugliScoreForQuality(double quality) {
+  if (quality < kLowestQuality) quality = kLowestQuality;
+  if (quality > kHighestQuality) quality = kHighestQuality;
+  int index = static_cast<int>(quality);
+  double mix = quality - index;
+  return kScoreForQuality[index - kLowestQuality] * (1 - mix) +
+      kScoreForQuality[index - kLowestQuality + 1] * mix;
 }
