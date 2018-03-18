aeb91faeb9d31b52b8e765c88657f561be6648c9
SUNsung <weatherius@gmail.com>
2018-03-18 19:22:25 +0200

    Rewrite [51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb] File in: 2018-03-18 19:22:24

--- ruby/51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb
+++ ruby/51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb
@@ -1,117 +1,48 @@
 
         
-            # Determine the available cores in host system.
-    # This mostly helps on linux, but it couldn't hurt on MacOSX.
-    if RUBY_PLATFORM =~ /linux/
-      cpu_count = `nproc`.to_i
-    elsif RUBY_PLATFORM =~ /darwin/
-      cpu_count = `sysctl -n hw.ncpu`.to_i
-    end
-    
-          topic = Topic.find_by(id: topic_id)
-    
-      def url
-    object.metadata.url
-  end
-    
-          rescue_from OpenIDConnect::HttpError do |e|
-        http_error_page_as_json(e)
-      end
-    
-          def fetch_public_key(o_auth_app, jwt)
-        public_key = fetch_public_key_from_json(o_auth_app.jwks, jwt)
-        if public_key.empty? && o_auth_app.jwks_uri
-          response = Faraday.get(o_auth_app.jwks_uri)
-          public_key = fetch_public_key_from_json(response.body, jwt)
+              out = checks.send(method)
+      unless out.nil? || out.empty?
+        if first_warning
+          $stderr.puts <<-EOS.undent
+            #{Tty.white}Please note that these warnings are just used to help the Homebrew maintainers
+            with debugging if you file an issue. If everything you use Homebrew for is
+            working fine: please don't worry and just ignore them. Thanks!#{Tty.reset}
+          EOS
         end
-        raise Rack::OAuth2::Server::Authorize::BadRequest(:unauthorized_client) if public_key.empty?
-        public_key
-      end
     
-    get '/stream', :provides => 'text/event-stream' do
-  stream :keep_open do |out|
-    connections << out
-    out.callback { connections.delete(out) }
+          export JAVA_HOME='$(/usr/libexec/java_home)'
+      export AWS_ACCESS_KEY='<Your AWS Access ID>'
+      export AWS_SECRET_KEY='<Your AWS Secret Key>'
+      export #{home_name}='#{home_value}'
+    EOS
   end
 end
+
     
-          def instrument(env)
-        return unless i = options[:instrumenter]
-        env['rack.protection.attack'] = self.class.name.split('::').last.downcase
-        i.instrument('rack.protection', env)
-      end
-    
-        # Gives a Geometry representing the given height and width
-    def initialize(width = nil, height = nil, modifier = nil)
-      if width.is_a?(Hash)
-        options = width
-        @height = options[:height].to_f
-        @width = options[:width].to_f
-        @modifier = options[:modifier]
-        @orientation = options[:orientation].to_i
-      else
-        @height = height.to_f
-        @width  = width.to_f
-        @modifier = modifier
-      end
-    end
-    
-            if args.length > 0
-          attachment.to_s(args.first)
-        else
-          attachment
+          opts.on('-F', '--from FORMAT',
+        'The format to convert from. Can be css, scss, sass.',
+        'By default, this is inferred from the input filename.',
+        'If there is none, defaults to css.') do |name|
+        @options[:from] = name.downcase.to_sym
+        raise 'sass-convert no longer supports LessCSS.' if @options[:from] == :less
+        unless [:css, :scss, :sass].include?(@options[:from])
+          raise 'Unknown format for sass-convert --from: #{name}'
         end
       end
-    end
     
-        # Returns an extension based on the content type. e.g. 'jpeg' for
-    # 'image/jpeg'. If the style has a specified format, it will override the
-    # content-type detection.
-    #
-    # Each mime type generally has multiple extensions associated with it, so
-    # if the extension from the original filename is one of these extensions,
-    # that extension is used, otherwise, the first in the list is used.
-    def content_type_extension attachment, style_name
-      mime_type = MIME::Types[attachment.content_type]
-      extensions_for_mime_type = unless mime_type.empty?
-        mime_type.first.extensions
-      else
-        []
-      end
+        @inner.log_level = log_level
+    Sass.logger = @inner
+  end
     
-            def description
-          'have an attachment named #{@attachment_name}'
+        def parse_input(environment, text)
+      case text
+      when Script::MATCH
+        name = $1
+        guarded = !!$3
+        val = Script::Parser.parse($2, @line, text.size - ($3 || '').size - $2.size)
+    
+            def show
+          authorize! :read, @order, order_token
+          @address = find_address
+          respond_with(@address)
         end
-    
-            def accepted_types_and_failures
-          if @allowed_types.present?
-            'Accept content types: #{@allowed_types.join(', ')}\n'.tap do |message|
-              if @missing_allowed_types.present?
-                message << '  #{@missing_allowed_types.join(', ')} were rejected.'
-              else
-                message << '  All were accepted successfully.'
-              end
-            end
-          end
-        end
-        def rejected_types_and_failures
-          if @rejected_types.present?
-            'Reject content types: #{@rejected_types.join(', ')}\n'.tap do |message|
-              if @missing_rejected_types.present?
-                message << '  #{@missing_rejected_types.join(', ')} were accepted.'
-              else
-                message << '  All were rejected successfully.'
-              end
-            end
-          end
-        end
-    
-      context 'called with four colors' do
-    it 'applies different colors to all sides' do
-      rule = 'border-color: #00f #0f0 #f00 #ff0'
-    
-      context 'expands active buttons' do
-    it 'finds selectors' do
-      list = @buttons_list.map { |input| '#{input}:active' }
-      list = list.join(', ')
-      ruleset = 'content: #{list};'
