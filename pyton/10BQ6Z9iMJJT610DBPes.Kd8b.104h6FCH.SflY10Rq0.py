42fb9d8f4ab5b036f0d2ea6180ca501a174f987b
SUNsung <weatherius@gmail.com>
2018-03-19 01:59:15 +0200

    Rewrite [10BQ6Z9iMJJT610DBPes.Kd8b.104h6FCH.SflY10Rq0.py] File in: 2018-03-19 01:59:15

--- pyton/10BQ6Z9iMJJT610DBPes.Kd8b.104h6FCH.SflY10Rq0.py
+++ pyton/10BQ6Z9iMJJT610DBPes.Kd8b.104h6FCH.SflY10Rq0.py
@@ -1,60 +1,141 @@
-    def iter_body(self):
-        '''Return an iterator over the message body.'''
-        raise NotImplementedError()
+
+        
+        
+@pytest.mark.functional
+def test_without_confirmation(proc, TIMEOUT):
+    without_confirmation(proc, TIMEOUT)
     
-        ':' HTTP headers:
-    
-        if explicit_json and body and (not lexer or isinstance(lexer, TextLexer)):
-        # JSON response with an incorrect Content-Type?
-        try:
-            json.loads(body)  # FIXME: the body also gets parsed in json.py
-        except ValueError:
-            pass  # Nope
-        else:
-            lexer = pygments.lexers.get_lexer_by_name('json')
+    # If true, '(C) Copyright ...' is shown in the HTML footer. Default is True.
+#html_show_copyright = True
     
     
-class Config(BaseConfigDict):
-    
-    # A list of files that should not be packed into the epub file.
-#epub_exclude_files = []
-    
-    from flask import Flask, jsonify, render_template, request
-app = Flask(__name__)
-    
-        def test_simple(self):
-        response = self.fetch('/')
-        self.assertEqual(response.body, b'Hello world!')
-    
-        c-ares fails to resolve some names when ``family`` is ``AF_UNSPEC``,
-    so it is only recommended for use in ``AF_INET`` (i.e. IPv4).  This is
-    the default for ``tornado.simple_httpclient``, but other libraries
-    may default to ``AF_UNSPEC``.
-    
-        For use on platforms that don't have os.pipe() (or where pipes cannot
-    be passed to select()), but do have sockets.  This includes Windows
-    and Jython.
-    '''
-    def __init__(self):
-        from .auto import set_close_exec
-        # Based on Zope select_trigger.py:
-        # https://github.com/zopefoundation/Zope/blob/master/src/ZServer/medusa/thread/select_trigger.py
+def test_login_logout(client, app):
+    '''Make sure login and logout works'''
+    rv = login(client, app.config['USERNAME'],
+               app.config['PASSWORD'])
+    assert b'You were logged in' in rv.data
+    rv = logout(client)
+    assert b'You were logged out' in rv.data
+    rv = login(client,app.config['USERNAME'] + 'x',
+               app.config['PASSWORD'])
+    assert b'Invalid username' in rv.data
+    rv = login(client, app.config['USERNAME'],
+               app.config['PASSWORD'] + 'x')
+    assert b'Invalid password' in rv.data
     
     
-def main():
-    base_cmd = [
-        sys.executable, '-m', 'timeit', '-s',
-        'from stack_context_benchmark import StackBenchmark, ExceptionBenchmark']
-    cmds = [
-        'StackBenchmark().enter_exit(50)',
-        'StackBenchmark().call_wrapped(50)',
-        'StackBenchmark().enter_exit(500)',
-        'StackBenchmark().call_wrapped(500)',
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+    import os
+import tempfile
+import pytest
+from minitwit import minitwit
+    
+        return groups
+    
+    # The checksum algorithm must match with the algorithm in ShellModule.checksum() method
+checksum = secure_hash
+checksum_s = secure_hash_s
+    
+        parser.add_argument('--key',
+                        dest='api_key',
+                        default=api_key,
+                        required=api_key is None,
+                        help='api key for accessing Shippable')
+    
+        parser.add_argument('--key',
+                        metavar='KEY',
+                        default=api_key,
+                        required=not api_key,
+                        help='Shippable API key')
+    
+    from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+    
+        #------------------------------------------------------------
+    # varying D
+    D_results_build = dict([(alg, np.zeros(len(Drange)))
+                            for alg in algorithms])
+    D_results_query = dict([(alg, np.zeros(len(Drange)))
+                            for alg in algorithms])
+    
+    plot(euclidean_distances)
+plot(rbf_kernels)
+plt.show()
+
+    
+    for i, n in enumerate(n_samples):
+    for j, p in enumerate(n_features):
+        X = np.random.normal(size=(n, p))
+        t0 = time.time()
+        ward.fit(X)
+        scikits_time[j, i] = time.time() - t0
+        t0 = time.time()
+        hierarchy.ward(X)
+        scipy_time[j, i] = time.time() - t0
     
     
-class BaseHandler(tornado.web.RequestHandler):
-    def get_current_user(self):
-        user_json = self.get_secure_cookie('fbdemo_user')
-        if not user_json:
-            return None
-        return tornado.escape.json_decode(user_json)
+def compute_time(t_start, delta):
+    mu_second = 0.0 + 10 ** 6  # number of microseconds in a second
+    
+        fn = os.path.relpath(fn,
+                         start=os.path.dirname(__import__(package).__file__))
+    try:
+        lineno = inspect.getsourcelines(obj)[1]
+    except Exception:
+        lineno = ''
+    return url_fmt.format(revision=revision, package=package,
+                          path=fn, lineno=lineno)
+    
+    
+# The training data folder must be passed as first argument
+languages_data_folder = sys.argv[1]
+dataset = load_files(languages_data_folder)
+    
+        in_exercise_region = False
+    
+    In both examples below, the main result is that the empirical covariance
+estimate, as a non-robust one, is highly influenced by the heterogeneous
+structure of the observations. Although the robust covariance estimate is
+able to focus on the main mode of the data distribution, it sticks to the
+assumption that the data should be Gaussian distributed, yielding some biased
+estimation of the data structure, but yet accurate to some extent.
+The One-Class SVM does not assume any parametric form of the data distribution
+and can therefore model the complex shape of the data much better.
+    
+    This example illustrates how sigmoid calibration changes predicted
+probabilities for a 3-class classification problem. Illustrated is the
+standard 2-simplex, where the three corners correspond to the three classes.
+Arrows point from the probability vectors predicted by an uncalibrated
+classifier to the probability vectors predicted by the same classifier after
+sigmoid calibration on a hold-out validation set. Colors indicate the true
+class of an instance (red: class 1, green: class 2, blue: class 3).
+    
+        # View probabilities=
+    probas = classifier.predict_proba(Xfull)
+    n_classes = np.unique(y_pred).size
+    for k in range(n_classes):
+        plt.subplot(n_classifiers, n_classes, index * n_classes + k + 1)
+        plt.title('Class %d' % k)
+        if k == 0:
+            plt.ylabel(name)
+        imshow_handle = plt.imshow(probas[:, k].reshape((100, 100)),
+                                   extent=(3, 9, 1, 5), origin='lower')
+        plt.xticks(())
+        plt.yticks(())
+        idx = (y_pred == k)
+        if idx.any():
+            plt.scatter(X[idx, 0], X[idx, 1], marker='o', c='k')
+    
+    
+def generate_data(n_samples, n_features):
+    '''Generate random blob-ish data with noisy features.
+    
+    
+if __name__ == '__main__':
+    cli()
