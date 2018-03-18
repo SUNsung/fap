ddc30ac9fa33126959b633988e95b7a5d20b876b
SUNsung <weatherius@gmail.com>
2018-03-18 03:27:57 +0200

    Rewrite [36E7R9v1X2FA36BlRkmecpPg36kRThnN2EjkQ36v1S.cpp] File in: 2018-03-18 03:27:56

--- cccccc/36E7R9v1X2FA36BlRkmecpPg36kRThnN2EjkQ36v1S.cpp
+++ cccccc/36E7R9v1X2FA36BlRkmecpPg36kRThnN2EjkQ36v1S.cpp
@@ -1,246 +1,58 @@
 
         
-          int AddEntry(const base::DictionaryValue& entry);
-  void RemoveEntry(int id);
+        HINSTANCE g_hInst = NULL;
+D3D_DRIVER_TYPE g_driverType = D3D_DRIVER_TYPE_NULL;
+D3D_FEATURE_LEVEL g_featureLevel = D3D_FEATURE_LEVEL_11_0;
+ID3D11Device* g_pd3dDevice = NULL;
+ID3D11DeviceContext* g_pImmediateContext = NULL;
+IDXGISwapChain* g_pSwapChain = NULL;
     
-     private:
-  // Must be called on IO thread.
-  void DoCancelAuth();
-  void DoLogin(const base::string16& username, const base::string16& password);
     
-    // Parse a string as an accelerator.
-bool StringToAccelerator(const std::string& description,
-                         ui::Accelerator* accelerator);
-    
-    static int make_prefixes(struct ifaddrs* ifaddr, int family, int prefixlen) {
-	char* prefix = NULL;
-	if (family == AF_INET) {
-		sockaddr_in* mask = new sockaddr_in;
-		mask->sin_family = AF_INET;
-		memset(&mask->sin_addr, 0, sizeof(in_addr));
-		ifaddr->ifa_netmask = reinterpret_cast<sockaddr*>(mask);
-		if (prefixlen > 32) {
-			prefixlen = 32;
-		}
-		prefix = reinterpret_cast<char*>(&mask->sin_addr);
-	} else if (family == AF_INET6) {
-		sockaddr_in6* mask = new sockaddr_in6;
-		mask->sin6_family = AF_INET6;
-		memset(&mask->sin6_addr, 0, sizeof(in6_addr));
-		ifaddr->ifa_netmask = reinterpret_cast<sockaddr*>(mask);
-		if (prefixlen > 128) {
-			prefixlen = 128;
-		}
-		prefix = reinterpret_cast<char*>(&mask->sin6_addr);
-	} else {
-		return -1;
-	}
-	for (int i = 0; i < (prefixlen / 8); i++) {
-		*prefix++ = 0xFF;
-	}
-	char remainder = 0xff;
-	remainder <<= (8 - prefixlen % 8);
-	*prefix = remainder;
-	return 0;
-}
-    
-         60,30,500,   1,18.,  128
-  },
-  /* 5: 128 x 17 */
-  {
-    6,{0,1,1,2,3,3},{2,3,3,3},{0,1,2,2},{-1,0,1,2},
-    {{3},{4,5},{-1,6,7,8},{-1,9,10,11}},
-    2,{0,128,  12,46,  4,8,16,  23,33,70,  2,6,10,  14,19,28,  39,58,90},
-    
-        {&_44p1_p2_0,&_44p1_p2_1,0},
-    {&_44p1_p3_0,&_44p1_p3_1,0},
-    {&_44p1_p4_0,&_44p1_p4_1,0},
-    
-    #define todB(x)   (*(x)==0?-400.f:log(*(x)**(x))*4.34294480f)
-#define todB_nn(x)   (*(x)==0.f?-400.f:log(*(x))*8.6858896f)
-    
-    /** 16x32 multiplication, followed by a 16-bit shift right. Results fits in 32 bits */
-#undef MULT16_32_Q16
-static OPUS_INLINE opus_val32 MULT16_32_Q16_armv4(opus_val16 a, opus_val32 b)
-{
-  unsigned rd_lo;
-  int rd_hi;
-  __asm__(
-      '#MULT16_32_Q16\n\t'
-      'smull %0, %1, %2, %3\n\t'
-      : '=&r'(rd_lo), '=&r'(rd_hi)
-      : '%r'(b),'r'(a<<16)
-  );
-  return rd_hi;
-}
-#define MULT16_32_Q16(a, b) (MULT16_32_Q16_armv4(a, b))
-    
-    #elif (defined(_MSC_VER) && _MSC_VER >= 1400) && defined (_M_X64)
-        #include <xmmintrin.h>
-    
-      // Make list of keys from flattened key structure
-  start_.push_back(keys_.size());  // Simplify length computation
-  tmp_keys_.resize(num_keys);
-  for (size_t i = 0; i < num_keys; i++) {
-    const char* base = keys_.data() + start_[i];
-    size_t length = start_[i+1] - start_[i];
-    tmp_keys_[i] = Slice(base, length);
-  }
-    
-    // Snapshots are kept in a doubly-linked list in the DB.
-// Each SnapshotImpl corresponds to a particular sequence number.
-class SnapshotImpl : public Snapshot {
- public:
-  SequenceNumber number_;  // const after creation
-    }
-    
-      while (msg == NULL && GetVarint32(&input, &tag)) {
-    switch (tag) {
-      case kComparator:
-        if (GetLengthPrefixedSlice(&input, &str)) {
-          comparator_ = str.ToString();
-          has_comparator_ = true;
-        } else {
-          msg = 'comparator name';
+    {
+    {
+    {
+    {                        const float eps = 1e-4f;
+                        //TODO: perhaps it is better to normalize the cross product by norms of the tangent vectors
+                        if (fabs(tangentVector_1.cross(tangentVector_2)) < eps)
+                        {
+                            isGeneralPosition = false;
+                        }
+                    }
+                }
+            }
         }
-        break;
+        while(!isGeneralPosition);
     }
-    }
+    else
+    {
+        //create points in a degenerate position (there are at least 3 points belonging to the same line)
     
-      void SetComparatorName(const Slice& name) {
-    has_comparator_ = true;
-    comparator_ = name.ToString();
-  }
-  void SetLogNumber(uint64_t num) {
-    has_log_number_ = true;
-    log_number_ = num;
-  }
-  void SetPrevLogNumber(uint64_t num) {
-    has_prev_log_number_ = true;
-    prev_log_number_ = num;
-  }
-  void SetNextFile(uint64_t num) {
-    has_next_file_number_ = true;
-    next_file_number_ = num;
-  }
-  void SetLastSequence(SequenceNumber seq) {
-    has_last_sequence_ = true;
-    last_sequence_ = seq;
-  }
-  void SetCompactPointer(int level, const InternalKey& key) {
-    compact_pointers_.push_back(std::make_pair(level, key));
-  }
-    
-    #include 'db/dbformat.h'
-#include 'leveldb/write_batch.h'
-    
-        const char* benchmarks = FLAGS_benchmarks;
-    while (benchmarks != NULL) {
-      const char* sep = strchr(benchmarks, ',');
-      Slice name;
-      if (sep == NULL) {
-        name = benchmarks;
-        benchmarks = NULL;
-      } else {
-        name = Slice(benchmarks, sep - benchmarks);
-        benchmarks = sep + 1;
-      }
+                template<typename T>
+            static __device__ __forceinline__ T atomicMin(T* address, T val)
+            {
+#if defined (__CUDA_ARCH__) && (__CUDA_ARCH__ < 120)
+                T count = ::min(*address, val);
+                do
+                {
+                    *address = count;
+                } while (*address > count);
     }
     
-      // Check that the directory is empty.
-  ASSERT_TRUE(!env_->FileExists('/dir/non_existent'));
-  ASSERT_TRUE(!env_->GetFileSize('/dir/non_existent', &file_size).ok());
-  ASSERT_OK(env_->GetChildren('/dir', &children));
-  ASSERT_EQ(0, children.size());
+    CV_EXPORTS_W void compare(InputArray src1, Scalar src2, OutputArray dst, int cmpop);
     
-    Iterator* Block::NewIterator(const Comparator* cmp) {
-  if (size_ < sizeof(uint32_t)) {
-    return NewErrorIterator(Status::Corruption('bad block contents'));
-  }
-  const uint32_t num_restarts = NumRestarts();
-  if (num_restarts == 0) {
-    return NewEmptyIterator();
-  } else {
-    return new Iter(cmp, data_, restart_offset_, num_restarts);
-  }
-}
+    #include 'test/core/util/test_config.h'
+#include 'test/cpp/qps/benchmark_config.h'
+#include 'test/cpp/qps/driver.h'
+#include 'test/cpp/qps/report.h'
+#include 'test/cpp/qps/server.h'
+#include 'test/cpp/util/test_config.h'
+#include 'test/cpp/util/test_credentials_provider.h'
     
+      // Return the current qps (i.e query count divided by the time since this
+  // QpsGauge object created (or Reset() was called))
+  long Get();
     
-    {}  // namespace leveldb
-
-    
-     private:
-  void Update() {
-    valid_ = iter_->Valid();
-    if (valid_) {
-      key_ = iter_->key();
-    }
-  }
-    
-    
-    {    current_->Next();
-    FindSmallest();
-  }
-    
-    #include <memory>
-#include <vector>
-    
-    #ifndef GRPC_INTERNAL_COMPILER_SCHEMA_INTERFACE_H
-#define GRPC_INTERNAL_COMPILER_SCHEMA_INTERFACE_H
-    
-      // Ensure that a type is prefixed with its namespace whenever it is used
-  // outside of its namespace.
-  std::string WrapInNameSpace(const Namespace *ns,
-                              const std::string &name) const;
-    
-      explicit FlatCompiler(const InitParams &params) : params_(params) {}
-    
-    // This is an example of parsing text straight into a buffer and then
-// generating flatbuffer (JSON) text from the buffer.
-int main(int /*argc*/, const char * /*argv*/ []) {
-  // load FlatBuffer schema (.fbs) and JSON from disk
-  std::string schemafile;
-  std::string jsonfile;
-  bool ok = flatbuffers::LoadFile('samples/monster.fbs', false, &schemafile) &&
-            flatbuffers::LoadFile('samples/monsterdata.json', false, &jsonfile);
-  if (!ok) {
-    printf('couldn't load files!\n');
-    return 1;
-  }
+    namespace grpc {
     }
     
-    #include 'flatbuffers/flatc.h'
-    
-    // Mutate the value of a struct's scalar.
-static void MutateScalarFieldOfStruct(const StructDef &struct_def,
-                                      const FieldDef &field,
-                                      std::string *code_ptr) {
-  std::string &code = *code_ptr;
-  std::string type = MakeCamel(GenTypeBasic(field.value.type));
-  std::string setter = 'rcv._tab.Mutate' + type;
-  GenReceiver(struct_def, code_ptr);
-  code += ' Mutate' + MakeCamel(field.name);
-  code += '(n ' + TypeName(field) + ') bool {\n\treturn ' + setter;
-  code += '(rcv._tab.Pos+flatbuffers.UOffsetT(';
-  code += NumToString(field.value.offset) + '), n)\n}\n\n';
-}
-    
-    bool LoadFileRaw(const char *name, bool binary, std::string *buf) {
-  if (DirExists(name)) return false;
-  std::ifstream ifs(name, binary ? std::ifstream::binary : std::ifstream::in);
-  if (!ifs.is_open()) return false;
-  if (binary) {
-    // The fastest way to read a file into a string.
-    ifs.seekg(0, std::ios::end);
-    auto size = ifs.tellg();
-    (*buf).resize(static_cast<size_t>(size));
-    ifs.seekg(0, std::ios::beg);
-    ifs.read(&(*buf)[0], (*buf).size());
-  } else {
-    // This is slower, but works correctly on all platforms for text files.
-    std::ostringstream oss;
-    oss << ifs.rdbuf();
-    *buf = oss.str();
-  }
-  return !ifs.bad();
-}
+    #include 'test/cpp/util/test_credentials_provider.h'
