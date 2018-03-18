fff16d7c88d1137190ad4883a5ba6c9f2e09848d
SUNsung <weatherius@gmail.com>
2018-03-18 18:52:26 +0200

    Rewrite [522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py] File in: 2018-03-18 18:52:25

--- pyton/522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py
+++ pyton/522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py
@@ -1,135 +1,157 @@
 
         
-        # There are two options for replacing |today|: either, you set today to some
-# non-false value, then it is used:
-#today = ''
-# Else, today_fmt is used as the format for a strftime call.
-#today_fmt = '%B %d, %Y'
-    
-        app.config.update(dict(
-        DATABASE=os.path.join(app.root_path, 'flaskr.db'),
-        DEBUG=True,
-        SECRET_KEY=b'_5#y2L'F4Q8z\n\xec]/',
-        USERNAME='admin',
-        PASSWORD='default'
-    ))
-    app.config.update(config or {})
-    app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-    
-    
+        
 def get_db():
     '''Opens a new database connection if there is none yet for the
     current application context.
     '''
-    top = _app_ctx_stack.top
-    if not hasattr(top, 'sqlite_db'):
-        top.sqlite_db = sqlite3.connect(app.config['DATABASE'])
-        top.sqlite_db.row_factory = sqlite3.Row
-    return top.sqlite_db
-    
-    _key = 'landscape'
-    
-        @property
-    def validate_certs(self):
-        return self._validate_certs
-    
-    - BaseRecognizer: Base class with common recognizer functionality.
-- Lexer: Base class for lexers.
-- Parser: Base class for parsers.
-- tree.TreeParser: Base class for %tree parser.
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
     
     
-    def predict(self, input):
-        '''
-        From the input stream, predict what alternative will succeed
-	using this DFA (representing the covering regular approximation
-	to the underlying CFL).  Return an alternative number 1..n.  Throw
-	 an exception upon error.
-	'''
-        mark = input.mark()
-        s = 0 # we always start at s0
-        try:
-            for _ in xrange(50000):
-                #print '***Current state = %d' % s
-                
-                specialState = self.special[s]
-                if specialState >= 0:
-                    #print 'is special'
-                    s = self.specialStateTransition(specialState, input)
-                    if s == -1:
-                        self.noViableAlt(s, input)
-                        return 0
-                    input.consume()
-                    continue
+def register_teardowns(app):
+    @app.teardown_appcontext
+    def close_db(error):
+        '''Closes the database again at the end of the request.'''
+        if hasattr(g, 'sqlite_db'):
+            g.sqlite_db.close()
+
     
+            entrylabel = Label(frame, anchor='w', justify='left',
+                           text=self.message)
+        self.entryvar = StringVar(self, self.text0)
+        self.entry = Entry(frame, width=30, textvariable=self.entryvar)
+        self.entry.focus_set()
+        self.error_font = Font(name='TkCaptionFont',
+                               exists=True, root=self.parent)
+        self.entry_error = Label(frame, text=' ', foreground='red',
+                                 font=self.error_font)
+        self.button_ok = Button(
+                frame, text='OK', default='active', command=self.ok)
+        self.button_cancel = Button(
+                frame, text='Cancel', command=self.cancel)
     
-    def mismatch(self, input, ttype, follow):
-        '''
-        Factor out what to do upon token mismatch so tree parsers can behave
-        differently.  Override and call mismatchRecover(input, ttype, follow)
-        to get single token insertion and deletion. Use this to turn of
-        single token insertion and deletion. Override mismatchRecover
-        to call this instead.
-        '''
+            if self.closed:
+            raise ValueError('I/O operation on closed file')
+        if self.size_read >= self.chunksize:
+            return b''
+        if size < 0:
+            size = self.chunksize - self.size_read
+        if size > self.chunksize - self.size_read:
+            size = self.chunksize - self.size_read
+        data = self.file.read(size)
+        self.size_read = self.size_read + len(data)
+        if self.size_read == self.chunksize and \
+           self.align and \
+           (self.chunksize & 1):
+            dummy = self.file.read(1)
+            self.size_read = self.size_read + len(dummy)
+        return data
     
-        '''
-    
-    def __init__(self, type=None, channel=DEFAULT_CHANNEL, text=None,
-                 input=None, start=None, stop=None, oldToken=None):
-        Token.__init__(self)
-        
-        if oldToken is not None:
-            self.type = oldToken.type
-            self.line = oldToken.line
-            self.charPositionInLine = oldToken.charPositionInLine
-            self.channel = oldToken.channel
-            self.index = oldToken.index
-            self._text = oldToken._text
-            if isinstance(oldToken, CommonToken):
-                self.input = oldToken.input
-                self.start = oldToken.start
-                self.stop = oldToken.stop
-            
-        else:
-            self.type = type
-            self.input = input
-            self.charPositionInLine = -1 # set to invalid position
-            self.line = 0
-            self.channel = channel
-            
-	    #What token number is this from 0..n-1 tokens; < 0 implies invalid index
-            self.index = -1
-            
-            # We need to be able to change the text once in a while.  If
-            # this is non-null, then getText should return this.  Note that
-            # start/stop are not affected by changing this.
-            self._text = text
-    
-    - tree.CommonTree: A basic and most commonly used Tree implementation.
-    
-    In Blackboard pattern several specialised sub-systems (knowledge sources)
-assemble their knowledge to build a possibly partial or approximate solution.
-In this way, the sub-systems work together to solve the problem,
-where the solution is the sum of its parts.
-    
-        def unsubscribe(self, msg, subscriber):
-        self.subscribers[msg].remove(subscriber)
-    
-        @staticmethod
-    def _static_method_2():
-        print('executed method 2!')
-    
-    
-@coroutine
-def coroutine1(target):
+    def JSONArray(s_and_end, scan_once, _w=WHITESPACE.match, _ws=WHITESPACE_STR):
+    s, end = s_and_end
+    values = []
+    nextchar = s[end:end + 1]
+    if nextchar in _ws:
+        end = _w(s, end + 1).end()
+        nextchar = s[end:end + 1]
+    # Look-ahead for trivial empty array
+    if nextchar == ']':
+        return values, end + 1
+    _append = values.append
     while True:
-        request = yield
-        if 0 < request <= 10:
-            print('request {} handled in coroutine 1'.format(request))
+        try:
+            value, end = scan_once(s, end)
+        except StopIteration as err:
+            raise JSONDecodeError('Expecting value', s, err.value) from None
+        _append(value)
+        nextchar = s[end:end + 1]
+        if nextchar in _ws:
+            end = _w(s, end + 1).end()
+            nextchar = s[end:end + 1]
+        end += 1
+        if nextchar == ']':
+            break
+        elif nextchar != ',':
+            raise JSONDecodeError('Expecting ',' delimiter', s, end - 1)
+        try:
+            if s[end] in _ws:
+                end += 1
+                if s[end] in _ws:
+                    end = _w(s, end + 1).end()
+        except IndexError:
+            pass
+    
+    def escape(m):
+    all, tail = m.group(0, 1)
+    assert all.startswith('\\')
+    esc = simple_escapes.get(tail)
+    if esc is not None:
+        return esc
+    if tail.startswith('x'):
+        hexes = tail[1:]
+        if len(hexes) < 2:
+            raise ValueError('invalid hex string escape ('\\%s')' % tail)
+        try:
+            i = int(hexes, 16)
+        except ValueError:
+            raise ValueError('invalid hex string escape ('\\%s')' % tail) from None
+    else:
+        try:
+            i = int(tail, 8)
+        except ValueError:
+            raise ValueError('invalid octal string escape ('\\%s')' % tail) from None
+    return chr(i)
+    
+        print('LIBS=', end=' ')
+    for lib in libs: print(''%s'' % (lib), end=' ')
+    print() ; print()
+    
+        def on_handler_waiting(self):
+        logging.debug('handler waiting')
+        self.stream.close()
+    
+    
+class QueuePutTest(AsyncTestCase):
+    @gen_test
+    def test_blocking_put(self):
+        q = queues.Queue()
+        q.put(0)
+        self.assertEqual(0, q.get_nowait())
+    
+        @gen.coroutine
+    def resolve(self, host, port, family=0):
+        if is_valid_ip(host):
+            addresses = [host]
         else:
-            target.send(request)
+            # gethostbyname doesn't take callback as a kwarg
+            self.channel.gethostbyname(host, family, (yield gen.Callback(1)))
+            callback_args = yield gen.Wait(1)
+            assert isinstance(callback_args, gen.Arguments)
+            assert not callback_args.kwargs
+            result, error = callback_args.args
+            if error:
+                raise IOError('C-Ares returned error %s: %s while resolving %s' %
+                              (error, pycares.errno.strerror(error), host))
+            addresses = result.addresses
+        addrinfo = []
+        for address in addresses:
+            if '.' in address:
+                address_family = socket.AF_INET
+            elif ':' in address:
+                address_family = socket.AF_INET6
+            else:
+                address_family = socket.AF_UNSPEC
+            if family != socket.AF_UNSPEC and family != address_family:
+                raise IOError('Requested socket family %d but got %d' %
+                              (family, address_family))
+            addrinfo.append((address_family, (address, port)))
+        raise gen.Return(addrinfo)
+
     
-    from __future__ import print_function
-    
-    
-class Specification(object):
+        @classmethod
+    def update_cache(cls, chat):
+        cls.cache.append(chat)
+        if len(cls.cache) > cls.cache_size:
+            cls.cache = cls.cache[-cls.cache_size:]
