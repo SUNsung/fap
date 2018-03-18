6825d1304500550217791d96935e7f41d822e3fe
SUNsung <weatherius@gmail.com>
2018-03-18 07:47:21 +0200

    Rewrite [59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp] File in: 2018-03-18 07:47:20

--- cccccc/59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp
+++ cccccc/59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp
@@ -1,239 +1,252 @@
 
         
-          static void BuildPrototype(v8::Isolate* isolate,
-                             v8::Local<v8::FunctionTemplate> prototype);
+        // Asserts that a given statement causes the program to exit, with an
+// integer exit status that satisfies predicate, and emitting error output
+// that matches regex.
+# define ASSERT_EXIT(statement, predicate, regex) \
+    GTEST_DEATH_TEST_(statement, predicate, regex, GTEST_FATAL_FAILURE_)
     
-    void SavePageHandler::OnDownloadUpdated(content::DownloadItem* item) {
-  if (item->IsDone()) {
-    v8::Isolate* isolate = v8::Isolate::GetCurrent();
-    v8::Locker locker(isolate);
-    v8::HandleScope handle_scope(isolate);
-    if (item->GetState() == content::DownloadItem::COMPLETE) {
-      callback_.Run(v8::Null(isolate));
-    } else {
-      v8::Local<v8::String> error_message = v8::String::NewFromUtf8(
-          isolate, 'Fail to save page');
-      callback_.Run(v8::Exception::Error(error_message));
+    INSTANTIATE_TEST_CASE_P(InstantiationName,
+                        FooTest,
+                        Values('meeny', 'miny', 'moe'));
+    
+      // Returns the number of TestPartResult objects in the array.
+  int size() const;
+    
+    // Type-parameterized tests are abstract test patterns parameterized
+// by a type.  Compared with typed tests, type-parameterized tests
+// allow you to define the test pattern without knowing what the type
+// parameters are.  The defined pattern can be instantiated with
+// different types any number of times, in any number of translation
+// units.
+//
+// If you are designing an interface or concept, you can define a
+// suite of type-parameterized tests to verify properties that any
+// valid implementation of the interface/concept should have.  Then,
+// each implementation can easily instantiate the test suite to verify
+// that it conforms to the requirements, without having to write
+// similar tests repeatedly.  Here's an example:
+    
+      // Destructor of TestCase.
+  virtual ~TestCase();
+    
+    // Internal macro for implementing {EXPECT|ASSERT}_PRED_FORMAT2.
+// Don't use this in your code.
+#define GTEST_PRED_FORMAT2_(pred_format, v1, v2, on_failure)\
+  GTEST_ASSERT_(pred_format(#v1, #v2, v1, v2), \
+                on_failure)
+    
+    // This template class represents an IEEE floating-point number
+// (either single-precision or double-precision, depending on the
+// template parameters).
+//
+// The purpose of this class is to do more sophisticated number
+// comparison.  (Due to round-off error, etc, it's very unlikely that
+// two floating-points will be equal exactly.  Hence a naive
+// comparison by the == operation often doesn't work.)
+//
+// Format of IEEE floating-point:
+//
+//   The most-significant bit being the leftmost, an IEEE
+//   floating-point looks like
+//
+//     sign_bit exponent_bits fraction_bits
+//
+//   Here, sign_bit is a single bit that designates the sign of the
+//   number.
+//
+//   For float, there are 8 exponent bits and 23 fraction bits.
+//
+//   For double, there are 11 exponent bits and 52 fraction bits.
+//
+//   More details can be found at
+//   http://en.wikipedia.org/wiki/IEEE_floating-point_standard.
+//
+// Template parameter:
+//
+//   RawType: the raw floating-point type (either float or double)
+template <typename RawType>
+class FloatingPoint {
+ public:
+  // Defines the unsigned integer type that has the same size as the
+  // floating point number.
+  typedef typename TypeWithSize<sizeof(RawType)>::UInt Bits;
     }
-    Destroy(item);
+    
+    #ifdef __GNUC__
+// Ask the compiler to never inline a given function.
+# define GTEST_NO_INLINE_ __attribute__((noinline))
+#else
+# define GTEST_NO_INLINE_
+#endif
+    
+    // 6.1.3.3 Tuple helper classes.
+    
+    // Template lists of length 1, 2, 3, and so on.
+    
+    #include <algorithm>
+    
+    std::string GetDbFileContent(int argc, char** argv);
+    
+    #endif  // GRPC_INTERNAL_COMPILER_NODE_GENERATOR_HELPERS_H
+
+    
+    class PythonGrpcGenerator : public grpc::protobuf::compiler::CodeGenerator {
+ public:
+  PythonGrpcGenerator(const GeneratorConfiguration& config);
+  ~PythonGrpcGenerator();
+    }
+    
+    AuthPropertyIterator SecureAuthContext::begin() const {
+  if (ctx_) {
+    grpc_auth_property_iterator iter =
+        grpc_auth_context_property_iterator(ctx_);
+    const grpc_auth_property* property =
+        grpc_auth_property_iterator_next(&iter);
+    return AuthPropertyIterator(property, &iter);
+  } else {
+    return end();
   }
 }
     
-    AtomQuotaPermissionContext::~AtomQuotaPermissionContext() {
-}
+     */
     
-    net::URLRequestJob* AsarProtocolHandler::MaybeCreateJob(
-    net::URLRequest* request,
-    net::NetworkDelegate* network_delegate) const {
-  base::FilePath full_path;
-  net::FileURLToFilePath(request->url(), &full_path);
-  auto* job = new URLRequestAsarJob(request, network_delegate);
-  job->Initialize(file_task_runner_, full_path);
-  return job;
-}
+    class CodegenTestMinimal : public ::testing::Test {};
     
-    namespace leveldb {
-    }
+     private:
+  static Result Sample();
     
-    namespace leveldb {
-    }
-    
-    // Return a builtin comparator that uses lexicographic byte-wise
-// ordering.  The result remains the property of this module and
-// must not be deleted.
-extern const Comparator* BytewiseComparator();
-    
-    // Dump the contents of the file named by fname in text format to
-// *dst.  Makes a sequence of dst->Append() calls; each call is passed
-// the newline-terminated text corresponding to a single item found
-// in the file.
-//
-// Returns a non-OK result if fname does not name a leveldb storage
-// file, or if the file cannot be read.
-Status DumpFile(Env* env, const std::string& fname, WritableFile* dst);
-    
-    inline int Slice::compare(const Slice& b) const {
-  const size_t min_len = (size_ < b.size_) ? size_ : b.size_;
-  int r = memcmp(data_, b.data_, min_len);
-  if (r == 0) {
-    if (size_ < b.size_) r = -1;
-    else if (size_ > b.size_) r = +1;
+    grpc::string SummarizeService(
+    const grpc::protobuf::ServiceDescriptor* service) {
+  grpc::string result;
+  for (int i = 0; i < service->method_count(); ++i) {
+    result.append(SummarizeMethod(service->method(i)));
   }
-  return r;
+  return result;
 }
     
-    // To distinguish different instances of the pattern, (yes, you
-// can instantiate it more then once) the first argument to the
-// INSTANTIATE_TEST_CASE_P macro is a prefix that will be added to the
-// actual test case name. Remember to pick unique prefixes for different
-// instantiations. The tests from the instantiation above will have
-// these names:
-//
-//    * InstantiationName/FooTest.DoesBlah/0 for 'meeny'
-//    * InstantiationName/FooTest.DoesBlah/1 for 'miny'
-//    * InstantiationName/FooTest.DoesBlah/2 for 'moe'
-//    * InstantiationName/FooTest.HasBlahBlah/0 for 'meeny'
-//    * InstantiationName/FooTest.HasBlahBlah/1 for 'miny'
-//    * InstantiationName/FooTest.HasBlahBlah/2 for 'moe'
-//
-// You can use these names in --gtest_filter.
-//
-// This statement will instantiate all tests from FooTest again, each
-// with parameter values 'cat' and 'dog':
     
-      // Returns a copy of the FilePath with the case-insensitive extension removed.
-  // Example: FilePath('dir/file.exe').RemoveExtension('EXE') returns
-  // FilePath('dir/file'). If a case-insensitive extension is not
-  // found, returns a copy of the original FilePath.
-  FilePath RemoveExtension(const char* extension) const;
-    
-      // If we are given the --check_for_leaks command line flag, installs the
-  // leak checker.
-  if (check_for_leaks) {
-    TestEventListeners& listeners = UnitTest::GetInstance()->listeners();
+    {    for (size_t i = 0; i < sorted_index_set.size(); ++i) {
+      bst_uint cid = sorted_index_set[i];
+      size_t dst_begin = page->offset[i];
+      size_t src_begin = disk_offset_[cid];
+      size_t num = disk_offset_[cid + 1] - disk_offset_[cid];
+      for (size_t j = 0; j < num; ++j) {
+        page->data[dst_begin + j] = SparseBatch::Entry(
+            index_.data[src_begin + j] + min_index_, value_.data[src_begin + j]);
+      }
     }
-    
-      void ServerTryCancel(ServerContext* context);
-    
-      ServerConfig server_config;
-  server_config.set_server_type(SYNC_SERVER);
-    
-    static const int WARMUP = 5;
-static const int BENCHMARK = 5;
-    
-        // What we get from the client.
-    HelloRequest request_;
-    // What we send back to the client.
-    HelloReply reply_;
+    return true;
+  }
     
     
     { private:
-  std::unique_ptr<Greeter::Stub> stub_;
+  /*! \brief the underlying stream */
+  dmlc::Stream *stream_;
+  /*! \brief buffer to hold data */
+  std::string buffer_;
+  /*! \brief length of valid data in buffer */
+  size_t read_len_;
+  /*! \brief pointer in the buffer */
+  size_t read_ptr_;
 };
     
-      ServerBuilder builder;
-  // Listen on the given address without any authentication mechanism.
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  // Register 'service' as the instance through which we'll communicate with
-  // clients. In this case it corresponds to an *synchronous* service.
-  builder.RegisterService(&service);
-  // Finally assemble the server.
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << 'Server listening on ' << server_address << std::endl;
-    
-     private:
-  bool PrintPreamble(grpc_generator::Printer* out);
-  bool PrintBetaPreamble(grpc_generator::Printer* out);
-  bool PrintGAServices(grpc_generator::Printer* out);
-  bool PrintBetaServices(grpc_generator::Printer* out);
-    
-    SecureAuthContext::~SecureAuthContext() {
-  if (take_ownership_) grpc_auth_context_release(ctx_);
-}
-    
-    #if TARGET_OS_IPHONE
-GRPC_XMACRO_ITEM(isWWAN, IsWWAN)
-#endif
-GRPC_XMACRO_ITEM(reachable, Reachable)
-GRPC_XMACRO_ITEM(transientConnection, TransientConnection)
-GRPC_XMACRO_ITEM(connectionRequired, ConnectionRequired)
-GRPC_XMACRO_ITEM(connectionOnTraffic, ConnectionOnTraffic)
-GRPC_XMACRO_ITEM(interventionRequired, InterventionRequired)
-GRPC_XMACRO_ITEM(connectionOnDemand, ConnectionOnDemand)
-GRPC_XMACRO_ITEM(isLocalAddress, IsLocalAddress)
-GRPC_XMACRO_ITEM(isDirect, IsDirect)
-
-    
-    #include <iostream>
-#include <stdexcept>
-    
-    
-    {  size_t size = 0;
-  fs::path mountPoint;
-  dev_t device = 0;
-};
-    
-    #include <condition_variable>
-#include <mutex>
-#include <thread>
-    
-    using std::make_shared;
-using std::string;
-    
-    
-    {} // namespace folly
-
-    
-      /**
-   * Construct an ImmediateFileWriter that writes to the specified File object.
+    #if XGBOOST_CUSTOMIZE_GLOBAL_PRNG
+/*!
+ * \brief An customized random engine, used to be plugged in PRNG from other systems.
+ *  The implementation of this library is not provided by xgboost core library.
+ *  Instead the other library can implement this class, which will be used as GlobalRandomEngine
+ *  If XGBOOST_RANDOM_CUSTOMIZE = 1, by default this is switched off.
+ */
+class CustomGlobalRandomEngine {
+ public:
+  /*! \brief The result type */
+  typedef size_t result_type;
+  /*! \brief The minimum of random numbers generated */
+  inline static constexpr result_type min() {
+    return 0;
+  }
+  /*! \brief The maximum random numbers generated */
+  inline static constexpr result_type max() {
+    return std::numeric_limits<size_t>::max();
+  }
+  /*!
+   * \brief seed function, to be implemented
+   * \param val The value of the seed.
    */
-  explicit ImmediateFileWriter(folly::File&& file);
+  void seed(result_type val);
+  /*!
+   * \return next random number.
+   */
+  result_type operator()();
+};
     
-    bool LogCategoryConfig::operator!=(const LogCategoryConfig& other) const {
-  return !(*this == other);
+    
+    {  std::remove(tmp_file.c_str());
+}
+
+    
+    TEST(Param, VectorStreamRead) {
+  std::vector<int> vals = {3, 2, 1};
+  std::stringstream ss;
+  std::vector<int> vals_in;
+    }
+    
+    template<>
+inline XGBOOST_DEVICE float bst_gpair_internal<int64_t>::GetGrad() const {
+  return grad_ * 1e-4f;
+}
+template<>
+inline XGBOOST_DEVICE float bst_gpair_internal<int64_t>::GetHess() const {
+  return hess_ * 1e-4f;
+}
+template<>
+inline XGBOOST_DEVICE void bst_gpair_internal<int64_t>::SetGrad(float g) {
+  grad_ = static_cast<int64_t>(std::round(g * 1e4));
+}
+template<>
+inline XGBOOST_DEVICE void bst_gpair_internal<int64_t>::SetHess(float h) {
+  hess_ = static_cast<int64_t>(std::round(h * 1e4));
 }
     
-    
-    {} // namespace folly
-
-    
-    #include <string>
-#include <unordered_map>
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you use this binding you'll need to call 4 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXXX_NewFrame(), ImGui::Render() and ImGui_ImplXXXX_Shutdown().
-// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui, Original code by @birthggd
-    
-    IMGUI_API bool        ImGui_ImplDX10_Init(void* hwnd, ID3D10Device* device);
-IMGUI_API void        ImGui_ImplDX10_Shutdown();
-IMGUI_API void        ImGui_ImplDX10_NewFrame();
-IMGUI_API void        ImGui_ImplDX10_RenderDrawData(ImDrawData* draw_data);
-    
-    // Handler for Win32 messages, update mouse/keyboard data.
-// You may or not need this for your implementation, but it can serve as reference for handling inputs.
-// Commented out to avoid dragging dependencies on <windows.h> types. You can copy the extern declaration in your code.
-/*
-IMGUI_API LRESULT   ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-*/
-
-    
-    int32 ImGui_Marmalade_PointerButtonEventCallback(void* SystemData, void* pUserData)
-{
-    // pEvent->m_Button is of type s3ePointerButton and indicates which mouse
-    // button was pressed.  For touchscreen this should always have the value
-    // S3E_POINTER_BUTTON_SELECT
-    s3ePointerEvent* pEvent = (s3ePointerEvent*)SystemData;
+    namespace xgboost {
+namespace metric {
+// tag the this file, used by force static link later.
+DMLC_REGISTRY_FILE_TAG(elementwise_metric);
+    }
     }
     
-    IMGUI_API bool        ImGui_Marmalade_Init(bool install_callbacks);
-IMGUI_API void        ImGui_Marmalade_Shutdown();
-IMGUI_API void        ImGui_Marmalade_NewFrame();
-IMGUI_API void        ImGui_Marmalade_RenderDrawData(ImDrawData* draw_data);
-    
-            for (int i1 = 0; i1 < count; i1++)
-        {
-            const int i2 = (i1+1) == points_count ? 0 : i1+1;
-            ImVec2 diff = points[i2] - points[i1];
-            diff *= ImInvLength(diff, 1.0f);
-            temp_normals[i1].x = diff.y;
-            temp_normals[i1].y = -diff.x;
-        }
-        if (!closed)
-            temp_normals[points_count-1] = temp_normals[points_count-2];
-    
-    namespace ImGui
-{
-    // We should always have a CurrentWindow in the stack (there is an implicit 'Debug' window)
-    // If this ever crash because g.CurrentWindow is NULL it means that either
-    // - ImGui::NewFrame() has never been called, which is illegal.
-    // - You are calling ImGui functions after ImGui::Render() and before the next ImGui::NewFrame(), which is also illegal.
-    inline    ImGuiWindow*  GetCurrentWindowRead()      { ImGuiContext& g = *GImGui; return g.CurrentWindow; }
-    inline    ImGuiWindow*  GetCurrentWindow()          { ImGuiContext& g = *GImGui; g.CurrentWindow->WriteAccessed = true; return g.CurrentWindow; }
-    IMGUI_API ImGuiWindow*  FindWindowByName(const char* name);
-    IMGUI_API void          FocusWindow(ImGuiWindow* window);
-    IMGUI_API void          BringWindowToFront(ImGuiWindow* window);
-    IMGUI_API void          BringWindowToBack(ImGuiWindow* window);
-    IMGUI_API bool          IsWindowChildOf(ImGuiWindow* window, ImGuiWindow* potential_parent);
-    IMGUI_API bool          IsWindowNavFocusable(ImGuiWindow* window);
+    class DNS {
+  public:
+   typedef std::vector<std::string> (*DNSFunc)(const std::string& host);
     }
+    
+    /*
+ * baseevent.h
+ *
+ *  Created on: 2016年3月24日
+ *      Author: caoshaokun
+ */
+    
+    #include <vector>
+#include <string>
+    
+    
+size_t  SimpleShortPackLength(size_t _datalen) {
+    return SimplePackLength<uint16_t>(_datalen);
+}
+    
+    class ServiceBase {
+  public:
+    virtual ~ServiceBase() {}
+    void DependServices(const TServicesMap& _dependservices) { m_dependservices = _dependservices;}
+    const char* ServiceName() const { return m_servicename.c_str();}
+    }
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+        void TestFun0();
+    void TestFun2()  {__TestFun1(1);}
