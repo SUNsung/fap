d65434a685fb48247beb95eb0e6f1c0bda3e3929
SUNsung <weatherius@gmail.com>
2018-03-18 11:19:00 +0200

    Rewrite [36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py] File in: 2018-03-18 11:18:59

--- pyton/36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py
+++ pyton/36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py
@@ -1,193 +1,89 @@
 
         
-        import io
-import optparse
-    
-        def test_proxy_https(self):
-        params = self._check_params(['primary_proxy', 'primary_server_ip'])
-        if params is None:
-            return
-        ydl = FakeYDL({
-            'proxy': params['primary_proxy']
-        })
-        self.assertEqual(
-            ydl.urlopen('https://yt-dl.org/ip').read().decode('utf-8'),
-            params['primary_server_ip'])
-    
-    
-class RtspFD(FileDownloader):
-    def real_download(self, filename, info_dict):
-        url = info_dict['url']
-        self.report_destination(filename)
-        tmpfilename = self.temp_name(filename)
-    
-    import re
-    
-            '''
-        available_plugins = plugin_manager.get_formatters_grouped()
-        self.enabled_plugins = []
-        for group in groups:
-            for cls in available_plugins[group]:
-                p = cls(env=env, **kwargs)
-                if p.enabled:
-                    self.enabled_plugins.append(p)
-    
-        def sum_up(self):
-        actually_downloaded = (
-            self.status.downloaded - self.status.resumed_from)
-        time_taken = self.status.time_finished - self.status.time_started
-    
-        ''')
-)
-positional.add_argument(
-    'method',
-    metavar='METHOD',
-    nargs=OPTIONAL,
-    default=None,
-    help='''
-    The HTTP method to be used for the request (GET, POST, PUT, DELETE, ...).
-    
-        def _migrate_implicit_content_type(self):
-        '''Migrate the removed implicit_content_type config option'''
-        try:
-            implicit_content_type = self.pop('implicit_content_type')
-        except KeyError:
-            self.save()
+        def main():
+    for name, fn in [('sequential', sequential),
+                     ('processes', with_process_pool_executor),
+                     ('threads', with_thread_pool_executor)]:
+        sys.stdout.write('%s: ' % name.ljust(12))
+        start = time.time()
+        if fn() != [True] * len(PRIMES):
+            sys.stdout.write('failed\n')
         else:
-            if implicit_content_type == 'form':
-                self['default_options'].insert(0, '--form')
-            self.save()
-            self.load()
+            sys.stdout.write('%.2f seconds\n' % (time.time() - start))
+    
+                f = Future()
+            f.add_done_callback(raising_fn)
+            f.add_done_callback(fn)
+            f.set_result(5)
+            self.assertTrue(raising_was_called)
+            self.assertTrue(fn_was_called)
+            self.assertIn('Exception: doh!', stderr.getvalue())
+    
+        return self._cached_response if self._cached_response else []
+    
+    if __name__ == '__main__':
+  Main()
 
     
     
-class ExitStatus:
-    '''Exit status code constants.'''
-    OK = 0
-    ERROR = 1
-    PLUGIN_ERROR = 7
-    
-            print('Iteration %s of %s' % (i, n_iter))
-    
-        fn = os.path.relpath(fn,
-                         start=os.path.dirname(__import__(package).__file__))
-    try:
-        lineno = inspect.getsourcelines(obj)[1]
-    except Exception:
-        lineno = ''
-    return url_fmt.format(revision=revision, package=package,
-                          path=fn, lineno=lineno)
-    
-        # decode the payload explicitly as UTF-8 since lxml is confused for some
-    # reason
-    with codecs.open(html_filename,'r','utf-8') as html_file:
-        html_content = html_file.read()
-    tree = ElementTree(lxml.html.document_fromstring(html_content))
-    i = 0
-    j = 0
-    for p in tree.findall('//p'):
-        content = p.text_content()
-        if len(content) < 100:
-            # skip paragraphs that are too short - probably too noisy and not
-            # representative of the actual language
-            continue
-    
-        def plot_kernels(self):
-        self.ax.text(-50, -60, 'Linear: $u^T v$')
-        self.ax.text(-20, -60, 'RBF: $\exp (-\gamma \| u-v \|^2)$')
-        self.ax.text(10, -60, 'Poly: $(\gamma \, u^T v + r)^d$')
-    
-    fit_data = data[np.argsort(model.row_labels_)]
-fit_data = fit_data[:, np.argsort(model.column_labels_)]
-    
-    model = SpectralCoclustering(n_clusters=5, random_state=0)
-model.fit(data)
-score = consensus_score(model.biclusters_,
-                        (rows[:, row_idx], columns[:, col_idx]))
-    
-        def mapper(self, _, line):
-        '''Parse each log line, extract and transform relevant lines.
-    
-        @abstractmethod
-    def can_fit_in_spot(self, spot):
-        pass
-    
-    from ..common import *
-from ..extractor import VideoExtractor
-    
-        html = get_content(url)
-    pid = match1(html, r'video\.settings\.pid\s*=\s*\'([^\']+)\'')
-    title = match1(html, r'video\.settings\.title\s*=\s*\'([^\']+)\'')
-    
-            if s: self.streams['video'] = {'url': s }
-        if mp3: self.streams['audio'] = { 'url': mp3 }
-        if pdf: self.streams['slides'] = { 'url': pdf }
+class _MasterDiagnosticFilter( object ):
     
     
-def mtv81_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_content(url)
-    title = HTMLParser().unescape(
-        '|'.join(match1(html, r'<title>(.*?)</title>').split('|')[:-2]))
+@wsgi_safe
+class StatusReasonTest(SimpleHandlerTestCase):
+    class Handler(RequestHandler):
+        def get(self):
+            reason = self.request.arguments.get('reason', [])
+            self.set_status(int(self.get_argument('code')),
+                            reason=reason[0] if reason else None)
     
-    print 'PLUGIN_I18N_PATHS := ' + ','.join(os.path.relpath(plugin.path)
-                                         for plugin in plugins
-                                         if plugin.needs_translation)
     
+class Configurable(object):
+    '''Base class for configurable interfaces.
     
-def walk_workspace():
-    root = get_root()
-    files = subprocess.check_output(['git', 'ls-files', '--full-name',
-                                     '--', root])
-    for filename in files.splitlines():
-        yield os.path.join(root, filename)
+            p = Popen(
+            [sys.executable, '-m', 'testapp'], stdout=subprocess.PIPE,
+            cwd=path, env=dict(os.environ, PYTHONPATH=pythonpath),
+            universal_newlines=True)
+        out = p.communicate()[0]
+        self.assertEqual(out, 'Starting\nStarting\n')
+
     
-        if ConfigValue.bool(global_conf.get('uncompressedJS')):
-        paths['static_files'] = get_raw_statics_path()
-    else:
-        paths['static_files'] = get_built_statics_path()
-    
-    def is_api(subtype = ''):
-    return c.render_style and c.render_style.startswith(api_type(subtype))
-    
-        @require_oauth2_scope('creddits')
-    @validate(
-        VUser(),
-        target=VByName('fullname'),
-    )
-    @api_doc(
-        api_section.gold,
-        uri='/api/v1/gold/gild/{fullname}',
-    )
-    def POST_gild(self, target):
-        if not isinstance(target, (Comment, Link)):
-            err = RedditError('NO_THING_ID')
-            self.on_validation_error(err)
-    
-        @csrf_exempt
-    @json_validate(
-        VRatelimit(rate_ip=True, prefix='rate_register_'),
-        signature=VSigned(),
-        name=VUname(['user']),
-        email=ValidEmail('email'),
-        password=VPasswordChange(['passwd', 'passwd2']),
-    )
-    def POST_register(self, responder, name, email, password, **kwargs):
-        kwargs.update(dict(
-            controller=self,
-            form=responder('noop'),
-            responder=responder,
-            name=name,
-            email=email,
-            password=password,
-        ))
-        return handle_register(**kwargs)
-    
-        @csrf_exempt
-    def renderurl(self, override=None):
-        if override:
-            path = override
+        @administrator
+    def post(self):
+        key = self.get_argument('key', None)
+        if key:
+            entry = Entry.get(key)
+            entry.title = self.get_argument('title')
+            entry.body_source = self.get_argument('body_source')
+            entry.html = tornado.escape.linkify(
+                self.get_argument('body_source'))
         else:
-            path = request.path
+            title = self.get_argument('title')
+            slug = unicodedata.normalize('NFKD', title).encode(
+                'ascii', 'ignore')
+            slug = re.sub(r'[^\w]+', ' ', slug)
+            slug = '-'.join(slug.lower().strip().split())
+            if not slug:
+                slug = 'entry'
+            while True:
+                existing = db.Query(Entry).filter('slug =', slug).get()
+                if not existing or str(existing.key()) == key:
+                    break
+                slug += '-2'
+            entry = Entry(
+                author=self.current_user,
+                title=title,
+                slug=slug,
+                body_source=self.get_argument('body_source'),
+                html=tornado.escape.linkify(self.get_argument('body_source')),
+            )
+        entry.put()
+        self.redirect('/entry/' + entry.slug)
     
-    from r2.controllers.reddit_base import MinimalController
-from r2.lib import promote, cache
+        def data_received(self, chunk):
+        self.bytes_read += len(chunk)
+    
+    
+class BaseRequestHandler(web.RequestHandler):
+    SUPPORTED_METHODS = ('PUT', 'GET', 'DELETE')
