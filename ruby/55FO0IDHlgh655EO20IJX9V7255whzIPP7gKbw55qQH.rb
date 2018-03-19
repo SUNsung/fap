5b792a819a0f1a6254ee0f5b14303f3ed723a6ce
SUNsung <weatherius@gmail.com>
2018-03-19 13:15:39 +0200

    Rewrite [55FO0IDHlgh655EO20IJX9V7255whzIPP7gKbw55qQH.rb] File in: 2018-03-19 13:15:38

--- ruby/55FO0IDHlgh655EO20IJX9V7255whzIPP7gKbw55qQH.rb
+++ ruby/55FO0IDHlgh655EO20IJX9V7255whzIPP7gKbw55qQH.rb
@@ -1,194 +1,218 @@
 
         
-            assert_equal 'foo', output_buffer, 'javascript_tag without a block should not concat to output_buffer'
-  end
-    
-          # Need to experiment if this priority is the best one: rendered => output_buffer
-      class RenderedViewsCollection
-        def initialize
-          @rendered_views ||= Hash.new { |hash, key| hash[key] = [] }
+              private
+        def listener
+          @listener || @server.mutex.synchronize { @listener ||= Listener.new(self, @server.event_loop) }
         end
     
-            def method_missing(name, *args, &block)
-          # Caches the method definition as a singleton method of the receiver.
-          #
-          # By letting #delegate handle it, we avoid an enclosure that'll capture args.
-          singleton_class.delegate name, to: :instance
+          def first
+        @text = 'Hello world'
+      end
     
-        test 'connect a request to controller instance without dispatch' do
-      env = {}
-      controller = BareController.new
-      controller.set_request! ActionDispatch::Request.new(env)
-      assert controller.request
+    require 'abstract_unit'
+    
+      class ExclaimerMiddleware
+    def initialize(app)
+      @app = app
+    end
+    
+        assert_raises(Interrupt) do
+      req.request_parameters
     end
   end
     
-          private
-        def processed_mailer
-          @processed_mailer ||= @mailer_class.new.tap do |mailer|
-            mailer.params = @params
-            mailer.process @action, *@args
+    ActionMailer::LogSubscriber.attach_to :action_mailer
+
+    
+        begin
+      raise '#{short_type} does not support dry-run' unless can_dry_run?
+      readonly!
+      @dry_run_started_at = Time.zone.now
+      @dry_run_logger.info('Dry Run started')
+      if event
+        raise 'This agent cannot receive an event!' unless can_receive_events?
+        receive([event])
+      else
+        check
+      end
+      @dry_run_logger.info('Dry Run finished')
+    rescue => e
+      @dry_run_logger.info('Dry Run failed')
+      error 'Exception during dry-run. #{e.message}: #{e.backtrace.join('\n')}'
+    end
+    
+        respond_to do |format|
+      format.html { redirect_to jobs_path, notice: 'Failed jobs removed.' }
+      format.json { head :no_content }
+    end
+  end
+    
+      def update
+    @user_credential = current_user.user_credentials.find(params[:id])
+    
+        def as_json
+      @pages
+    end
+    
+            css('[id]').each do |node|
+          # Module
+          if node.name == 'h2'
+            type = node.content.remove 'Backbone.'
+            if type.capitalize! # sync, history
+              entries << [node.content, node['id'], type]
+            end
+            next
           end
-        end
     
-            def application_mailer_file_name
-          @_application_mailer_file_name ||= if mountable_engine?
-            'app/mailers/#{namespaced_path}/application_mailer.rb'
-          else
-            'app/mailers/application_mailer.rb'
-          end
-        end
+      def test_assoc
+    assert_nil(ENV.assoc('test'))
+    ENV['test'] = 'foo'
+    k, v = ENV.assoc('test')
+    if IGNORE_CASE
+      assert_equal('TEST', k.upcase)
+      assert_equal('FOO', v.upcase)
+    else
+      assert_equal('test', k)
+      assert_equal('foo', v)
+    end
+    assert_invalid_env {|var| ENV.assoc(var)}
+    assert_predicate(v, :tainted?)
+    assert_equal(Encoding.find('locale'), v.encoding)
+  end
+    
+        o = Object.new
+    def o.hash; 2 << 100; end
+    assert_equal({o=>1}.hash, @cls[o=>1].hash)
+  end
+    
+        assert_not_equal(set1, set2)
+  end
+    
+      def test_reinitialize
+    bug8099 = '[ruby-core:53436] [Bug #8099]'
+    t2000 = get_t2000
+    assert_raise(TypeError, bug8099) {
+      t2000.send(:initialize, 2013, 03, 14)
+    }
+    assert_equal(get_t2000, t2000, bug8099)
+  end
+    
+      it 'decodes the number of bytes specified by the count modifier including whitespace bytes' do
+    [ ['a bc',  ['a b', 'c']],
+      ['a\fbc', ['a\fb', 'c']],
+      ['a\nbc', ['a\nb', 'c']],
+      ['a\rbc', ['a\rb', 'c']],
+      ['a\tbc', ['a\tb', 'c']],
+      ['a\vbc', ['a\vb', 'c']]
+    ].should be_computed_by(:unpack, unpack_format(3)+unpack_format)
+  end
+    
+      it 'cannot be rescued' do
+    thread = Thread.new do
+      begin
+        Thread.current.send(@method)
+      rescue Exception
+        ScratchPad.record :in_rescue
+      end
+     ScratchPad.record :end_of_thread_block
+    end
+    
+      it 'spawns a new Thread running the block' do
+    run = false
+    t = Thread.send(@method) { run = true }
+    t.should be_kind_of(Thread)
+    t.join
+    
+          connection.execute(<<~SQL)
+        CREATE OR REPLACE FUNCTION timestamp_id(table_name text)
+        RETURNS bigint AS
+        $$
+          DECLARE
+            time_part bigint;
+            sequence_base bigint;
+            tail bigint;
+          BEGIN
+            time_part := (
+              -- Get the time in milliseconds
+              ((date_part('epoch', now()) * 1000))::bigint
+              -- And shift it over two bytes
+              << 16);
+    
+      yield
+end
+    
+          expect(response).to redirect_to(settings_notifications_path)
+      user.reload
+      expect(user.settings['notification_emails']['follow']).to be true
+      expect(user.settings['interactions']['must_be_follower']).to be false
     end
   end
 end
 
     
-    unless File.exist?(PROF_OUTPUT_FILE)
-  StackProf.run(
-    mode: MODE.to_sym,
-    interval: 100,
-    raw: true,
-    out: PROF_OUTPUT_FILE
-  ) do
-    puts 'GC Stats:', JSON.pretty_generate(GC.stat)
-    GC.disable
-    
-    # Create an array of vendored mimetype => [extensions]
-mimes = {}
-json = open('https://raw.githubusercontent.com/jshttp/mime-db/master/db.json').read
-data = JSON.parse(json)
-data.reject! { |mime, meta| meta['extensions'].nil? || meta['extensions'].empty? }
-data.each do |mime, meta|
-  # Normalize extensions and mime-types
-  mime = mime.downcase.strip
-  extensions = meta['extensions'].map { |e| e.downcase.strip }.compact
-    
-    def all_steps_to_path(path)
-  source = source_dir
-  dest = Pathname.new(path).expand_path
-  paths = []
-    
-        def no_subcommand(args)
-      unless args.empty? ||
-          args.first !~ %r(!/^--/!) || %w(--help --version).include?(args.first)
-        deprecation_message 'Jekyll now uses subcommands instead of just switches. \
-                          Run `jekyll help` to find out more.'
-        abort
-      end
-    end
-    
-        def base_directory
-      @base_directory.to_s
-    end
-    
-      # Make it so that network access from the vagrant guest is able to
-  # use SSH private keys that are present on the host without copying
-  # them into the VM.
-  config.ssh.forward_agent = true
-    
-      validate :enough_poll_answers
-  validates :question, presence: true
-    
-          rescue_from Rack::OAuth2::Server::Authorize::BadRequest,
-                  JSON::JWT::InvalidFormat, JSON::JWK::UnknownAlgorithm do |e|
-        logger.info e.backtrace[0, 10].join('\n')
-        render json: {error: :invalid_request, error_description: e.message, status: 400}
-      end
-      rescue_from JSON::JWT::VerificationFailed do |e|
-        logger.info e.backtrace[0, 10].join('\n')
-        render json: {error: :invalid_grant, error_description: e.message, status: 400}
-      end
-    end
-  end
-end
-
-    
-      it 'comparison of Accept-Language header is not case sensitive' do
-    session = {:foo => :bar}
-    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
-    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'A'
-    expect(session).not_to be_empty
-  end
-    
-            private
-    
-            def literal_at_end?(regex_str)
-          # is this regexp 'literal' in the sense of only matching literal
-          # chars, rather than using metachars like . and * and so on?
-          # also, is it anchored at the end of the string?
-          regex_str =~ /\A(?:#{LITERAL_REGEX})+\\z\z/
+          def call
+        title('Gems')
+        table(all_gem_names) do |gem, row|
+          row.yellow if update_available?(gem)
+          row << gem
+          row << installed_gem_version(gem)
+          row << '(update available)' if update_available?(gem)
         end
+      end
     
-    module RuboCop
-  module Cop
-    module Performance
-      # This cop identifies unnecessary use of a regex where
-      # `String#start_with?` would suffice.
-      #
-      # @example
-      #   # bad
-      #   'abc' =~ /\Aab/
-      #   'abc'.match(/\Aab/)
-      #
-      #   # good
-      #   'abc'.start_with?('ab')
-      class StartWith < Cop
-        MSG = 'Use `String#start_with?` instead of a regex match anchored to ' \
-              'the beginning of the string.'.freeze
-        SINGLE_QUOTE = '''.freeze
+          # Prints a table for a given array of records. For each record, the block
+      # is yielded two arguments: the record and a Row object. To print values
+      # for that record, add values using `row << 'some value'`. A row can
+      # optionally be highlighted in yellow using `row.yellow`.
+      def table(records, &block)
+        return if records.empty?
+        rows = collect_rows(records, &block)
+        col_widths = calculate_column_widths(rows)
     
-            def parent_block_node(node)
-          node.each_ancestor(:block).first
+        def ensure_stage
+      Rake::Task.define_task(:ensure_stage) do
+        unless stage_set?
+          puts t(:stage_not_set)
+          exit 1
         end
       end
     end
-  end
-end
-
     
-            def variable_name(node)
-          node.children[0]
-        end
-    
-              optarg_positions.each do |optarg_position|
-            # there can only be one group of optional arguments
-            break if optarg_position > arg_positions.max
-    
-      def as_boolean(string)
-    return true   if string == true   || string =~ (/(true|t|yes|y|1)$/i)
-    return false  if string == false  || string.blank? || string =~ (/(false|f|no|n|0)$/i)
-    raise ArgumentError.new('invalid value for Boolean: \'#{string}\'')
+      # Read and eval a .rake file in such a way that `self` within the .rake file
+  # refers to this plugin instance. This gives the tasks in the file access to
+  # helper methods defined by the plugin.
+  def eval_rakefile(path)
+    contents = IO.read(path)
+    instance_eval(contents, path, 1)
   end
     
-          def started?
-        true
+          it 'filters from ENV[ROLES]' do
+        hosts = dsl.roles(:db)
+        all = dsl.roles(:all)
+        SSHKit::Coordinator.expects(:new).with(hosts).returns(@coordinator)
+        ENV['ROLES'] = 'db'
+        dsl.on(all)
       end
     
-      module CompareExpression0
-    def rvalue1
-      elements[0]
+      it 'overrides the rake method, and sets the sshkit_backend to SSHKit::Backend::Printer' do
+    capture_io do
+      flags '--dry-run', '-n'
     end
-    
-          expect('.border-color-false-third').to have_ruleset(ruleset)
-      expect('.border-color-false-third').to_not have_rule(bad_rule)
-    end
+    sshkit_backend = Capistrano::Configuration.fetch(:sshkit_backend)
+    expect(sshkit_backend).to eq(SSHKit::Backend::Printer)
   end
-end
-
     
-    describe 'buttons' do
-  before(:all) do
-    ParserSupport.parse_file('library/buttons')
-    
-          expect('.margin-false-third').to have_ruleset(ruleset)
-      expect('.margin-false-third').to_not have_rule(bad_rule)
+        # Returns the fingerprint of the instance.
+    def fingerprint attachment, style_name
+      attachment.fingerprint
     end
-  end
-end
-
     
-      context 'called with arguments (1, $ratio: $golden-ratio)' do
-    it 'output the first value from the golden ratio scale' do
-      expect('.one-golden-ratio').to have_rule('font-size: 1.618em')
-    end
-  end
+            def responds?
+          methods = @subject.instance_methods.map(&:to_s)
+          methods.include?('#{@attachment_name}') &&
+            methods.include?('#{@attachment_name}=') &&
+            methods.include?('#{@attachment_name}?')
+        end
+    
+    module Paperclip
+  require 'rails'
