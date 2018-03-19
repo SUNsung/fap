5842ae934dd40115829969e971b19b8e5d3cc711
SUNsung <weatherius@gmail.com>
2018-03-19 06:32:43 +0200

    Rewrite [57LVBZVtgmn257WndSwRWaCnE57BTvJomQu9Mw572GS.py] File in: 2018-03-19 06:32:43

--- pyton/57LVBZVtgmn257WndSwRWaCnE57BTvJomQu9Mw572GS.py
+++ pyton/57LVBZVtgmn257WndSwRWaCnE57BTvJomQu9Mw572GS.py
@@ -1,57 +1,128 @@
 
         
-            def test_same_server(self):
-        from certbot_apache.obj import VirtualHost
-        no_name1 = VirtualHost(
-            'fp', 'vhp', set([self.addr1]), False, False, None)
-        no_name2 = VirtualHost(
-            'fp', 'vhp', set([self.addr2]), False, False, None)
-        no_name3 = VirtualHost(
-            'fp', 'vhp', set([self.addr_default]),
-            False, False, None)
-        no_name4 = VirtualHost(
-            'fp', 'vhp', set([self.addr2, self.addr_default]),
-            False, False, None)
+        
+def test_urls(client):
+    r = client.get('/')
+    assert r.status_code == 200
     
-        # Implement all methods from IInstaller, remembering to add
-    # 'self' as first argument, e.g. def get_all_names(self)...
+    
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != current_app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != current_app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('flaskr.show_entries'))
+    return render_template('login.html', error=error)
+    
+        def test_basic_response(self):
+        '''the basic response server returns an empty http response'''
+        with Server.basic_response_server() as (host, port):
+            r = requests.get('http://{0}:{1}'.format(host, port))
+            assert r.status_code == 200
+            assert r.text == u''
+            assert r.headers['Content-Length'] == '0'
+    
+    #: Python 3.x?
+is_py3 = (_ver[0] == 3)
+    
+    _proxy_combos = []
+for prefix, schemes in _schemes_by_var_prefix:
+    for scheme in schemes:
+        _proxy_combos.append(('{0}_proxy'.format(prefix), scheme))
+    
+        # For pickling to work, the __module__ variable needs to be set to the frame
+    # where the named tuple is created.  Bypass this step in enviroments where
+    # sys._getframe is not defined (Jython for example).
+    if hasattr(_sys, '_getframe'):
+        result.__module__ = _sys._getframe(1).f_globals.get('__name__', '__main__')
+    
+        def test_del_shutdown(self):
+        executor = futures.ProcessPoolExecutor(max_workers=5)
+        list(executor.map(abs, range(-5, 5)))
+        queue_management_thread = executor._queue_management_thread
+        processes = executor._processes
+        del executor
+    
+    
+  # This returns a future! Use JsonFromFuture to get the value.
+  # |method| is either 'POST' or 'GET'.
+  # |timeout| is num seconds to tolerate no response from server before giving
+  # up; see Requests docs for details (we just pass the param along).
+  @staticmethod
+  def _TalkToHandlerAsync( data,
+                           handler,
+                           method,
+                           timeout = _READ_TIMEOUT_SEC ):
+    request_uri = _BuildUri( handler )
+    if method == 'POST':
+      sent_data = _ToUtf8Json( data )
+      return BaseRequest.Session().post(
+          request_uri,
+          data = sent_data,
+          headers = BaseRequest._ExtraHeaders( method,
+                                               request_uri,
+                                               sent_data ),
+          timeout = ( _CONNECT_TIMEOUT_SEC, timeout ) )
+    return BaseRequest.Session().get(
+        request_uri,
+        headers = BaseRequest._ExtraHeaders( method, request_uri ),
+        timeout = ( _CONNECT_TIMEOUT_SEC, timeout ) )
+    
+    
+  def SubsetForTypes( self, filetypes ):
+    # check cache
+    cache_key = ','.join( filetypes )
+    cached = self._cache.get( cache_key )
+    if cached is not None:
+      return cached
+    
+      opts = _JavaFilter( { 'regex' : '.*taco.*',
+                        'level' : 'warning' } )
+  f = _CreateFilterForTypes( opts, [ 'java' ] )
+    
+        def setUp(self):
+        self.e, self.g = get_localizer(language='English'), \
+                         get_localizer(language='Greek')
+    
+        def test_display_current_time_at_current_time(self):
+        '''
+        Just as justification for working example with the time provider used in
+        production. (Will always pass.)
+        '''
+        production_code_time_provider = ProductionCodeTimeProvider()
+        class_under_test = TimeDisplay()
+        current_time = datetime.datetime.now()
+        expected_time = '<span class=\'tinyBoldText\'>{}:{}</span>'.format(current_time.hour, current_time.minute)
+        self.assertEqual(class_under_test.get_current_time_as_html_fragment(production_code_time_provider), expected_time)
+    
+    ### OUTPUT ###
+# request 2 handled in handler 1
+# request 5 handled in handler 1
+# request 14 handled in handler 2
+# request 22 handled in handler 3
+# request 18 handled in handler 2
+# request 3 handled in handler 1
+# end of chain, no handler for 35
+# request 27 handled in handler 3
+# request 20 handled in handler 2
+# ------------------------------
+# request 2 handled in coroutine 1
+# request 5 handled in coroutine 1
+# request 14 handled in coroutine 2
+# request 22 handled in coroutine 3
+# request 18 handled in coroutine 2
+# request 3 handled in coroutine 1
+# end of chain, no coroutine for 35
+# request 27 handled in coroutine 3
+# request 20 handled in coroutine 2
+# (0.2369999885559082, 0.16199994087219238)
 
     
-    def line_length(filename):
-    return sum(1 for line in open(filename))
-    
-      Args:
-    filename: The name of the current file.
-    clean_lines: A CleansedLines instance containing the file.
-    linenum: The number of the line to check.
-    error: The function to call with any errors found.
-  '''
-  line = clean_lines.elided[linenum]
-  if Search(r'\bVLOG\((INFO|ERROR|WARNING|DFATAL|FATAL)\)', line):
-    error(filename, linenum, 'runtime/vlog', 5,
-          'VLOG() should be used with numeric verbosity level.  '
-          'Use LOG() if you want symbolic severity levels.')
-    
-        def __init__(self):
-        self.experts = []
-        self.common_state = {
-            'problems': 0,
-            'suggestions': 0,
-            'contributions': [],
-            'progress': 0   # percentage, if 100 -> task is finished
-        }
-    
-        def subscribe(self, msg, subscriber):
-        self.subscribers.setdefault(msg, []).append(subscriber)
-    
-        def now(self):
-        current_time = datetime.datetime.now()
-        current_time_formatted = '{}:{}'.format(current_time.hour, current_time.minute)
-        return current_time_formatted
-    
-    from __future__ import print_function
-import os
-from os.path import lexists
-    
-        def __init__(self, super_user=False):
-        self.super_user = super_user
+        def commit(self):
+        self.states = [memento(target, self.deep) for target in self.targets]
