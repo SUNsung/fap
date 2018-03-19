767318fbacdbe0eafe8a3bab91c4e432dcfd2ab1
SUNsung <weatherius@gmail.com>
2018-03-19 08:16:40 +0200

    Rewrite [46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb] File in: 2018-03-19 08:16:39

--- ruby/46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb
+++ ruby/46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb
@@ -1,95 +1,33 @@
 
         
-        class BottleCollector
-  def initialize
-    @checksums = {}
-  end
-    
-    
-        Generally there are no consequences of this for you. If you build your
-        own software and it requires this formula, you'll need to add to your
-        build variables:
-    
-      def hardware
-    'CPU: #{Hardware.cores_as_words}-core #{Hardware::CPU.bits}-bit #{Hardware::CPU.family}'
-  end
-    
-        if ARGV.named.empty?
-      slow_checks = %w[
-        check_for_broken_symlinks
-        check_missing_deps
-        check_for_outdated_homebrew
-        check_for_linked_keg_only_brews
-      ]
-      methods = (checks.all.sort - slow_checks) + slow_checks
-    else
-      methods = ARGV.named
+            begin
+      response = U2F::RegisterResponse.load_from_json(params[:device_response])
+      registration_data = u2f.register!(challenges, response)
+      registration.update(certificate: registration_data.certificate,
+                          key_handle: registration_data.key_handle,
+                          public_key: registration_data.public_key,
+                          counter: registration_data.counter,
+                          user: user,
+                          name: params[:name])
+    rescue JSON::ParserError, NoMethodError, ArgumentError
+      registration.errors.add(:base, 'Your U2F device did not send a valid JSON response.')
+    rescue U2F::Error => e
+      registration.errors.add(:base, e.message)
     end
     
-        print_remaining_files remaining_root_files, root, other
-  end
+      class Blockquote < Liquid::Block
+    FullCiteWithTitle = /(\S.*)\s+(https?:\/\/)(\S+)\s+(.+)/i
+    FullCite = /(\S.*)\s+(https?:\/\/)(\S+)/i
+    AuthorTitle = /([^,]+),([^,]+)/
+    Author =  /(.+)/
     
-      def dump
-    # Key Legend: Added (A), Copied (C), Deleted (D), Modified (M), Renamed (R)
     
-      def self.bottle_sha1(*)
-  end
     
-    unless invalids.empty?
-  puts '\n\nFailed links:'
-  invalids.each do |link|
-    puts '- #{link}'
-  end
-  puts 'Done with errors.'
-  exit(1)
-end
+    Liquid::Template.register_tag('include_array', Jekyll::IncludeArrayTag)
+
     
-        def name=(value)
-      @name = value.try :strip
-    end
+    module Jekyll
     
-              if %w(Events Sync).include?(type)
-            name.prepend 'Backbone.'
-          elsif type == 'History'
-            name.prepend 'Backbone.history.'
-          elsif name == 'extend'
-            name.prepend '#{type}.'
-          elsif name.start_with? 'constructor'
-            name = type
-          elsif type != 'Utility'
-            name.prepend '#{type.downcase}.'
-          end
-    
-        def names_for(klass)
-      @attachments[klass].keys
-    end
-    
-        def define_instance_getter
-      name = @name
-      options = @options
-    
-            def type_allowed?(type)
-          @subject.send('#{@attachment_name}_content_type=', type)
-          @subject.valid?
-          @subject.errors[:'#{@attachment_name}_content_type'].blank?
-        end
-    
-        module TableDefinition
-      def attachment(*attachment_names)
-        options = attachment_names.extract_options!
-        attachment_names.each do |attachment_name|
-          COLUMNS.each_pair do |column_name, column_type|
-            column_options = options.merge(options[column_name.to_sym] || {})
-            column('#{attachment_name}_#{column_name}', column_type, column_options)
-          end
-        end
+          unless file.file?
+        return 'File #{file} could not be found'
       end
-    
-            def operator_assignment_node
-          return nil unless node.parent
-          return nil unless OPERATOR_ASSIGNMENT_TYPES.include?(node.parent.type)
-          return nil unless node.sibling_index.zero?
-          node.parent
-        end
-    
-                break if !assignment.branch || assignment.branch == reference.branch
