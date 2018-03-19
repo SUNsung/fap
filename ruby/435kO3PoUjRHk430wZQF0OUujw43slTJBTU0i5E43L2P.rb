99c0e1fca0c725d296bd052b18255bbd75eaffe7
SUNsung <weatherius@gmail.com>
2018-03-19 14:15:22 +0200

    Rewrite [435kO3PoUjRHk430wZQF0OUujw43slTJBTU0i5E43L2P.rb] File in: 2018-03-19 14:15:21

--- ruby/435kO3PoUjRHk430wZQF0OUujw43slTJBTU0i5E43L2P.rb
+++ ruby/435kO3PoUjRHk430wZQF0OUujw43slTJBTU0i5E43L2P.rb
@@ -1,144 +1,164 @@
 
         
-                def boolean_tag_option(key)
-          %(#{key}='#{key}')
-        end
-    
-      def test_redirect_to_params
-    error = assert_raise(ActionController::UnfilteredParameters) do
-      get :redirect_to_params
-    end
-    assert_equal 'unable to convert unpermitted parameters to hash', error.message
-  end
-    
-            def spec(spec, config = {})
-          Resolver.new(config).spec(spec)
-        end
-    
-              # Returns fully resolved connection, accepts hash, string or symbol.
-          # Always returns a hash.
-          #
-          # == Examples
-          #
-          # Symbol representing current environment.
-          #
-          #   Resolver.new('production' => {}).resolve_connection(:production)
-          #   # => {}
-          #
-          # One layer deep hash of connection values.
-          #
-          #   Resolver.new({}).resolve_connection('adapter' => 'sqlite3')
-          #   # => { 'adapter' => 'sqlite3' }
-          #
-          # Connection URL.
-          #
-          #   Resolver.new({}).resolve_connection('postgresql://localhost/foo')
-          #   # => { 'host' => 'localhost', 'database' => 'foo', 'adapter' => 'postgresql' }
-          #
-          def resolve_connection(spec)
-            case spec
-            when Symbol
-              resolve_symbol_connection spec
-            when String
-              resolve_url_connection spec
-            when Hash
-              resolve_hash_connection spec
-            end
-          end
-    
-        test 'response_body value is wrapped in an array when the value is a String' do
-      controller = BareController.new
-      controller.set_request!(ActionDispatch::Request.empty)
-      controller.set_response!(BareController.make_response!(controller.request))
-      controller.index
-      assert_equal ['Hello world'], controller.response_body
-    end
-    
-        # An email was generated.
-    def process(event)
-      debug do
-        mailer = event.payload[:mailer]
-        action = event.payload[:action]
-        '#{mailer}##{action}: processed outbound mail in #{event.duration.round(1)}ms'
-      end
-    end
-    
-    FIXTURE_LOAD_PATH = File.expand_path('fixtures', __dir__)
-ActionMailer::Base.view_paths = FIXTURE_LOAD_PATH
-    
-    def bottle_tag
-  if MacOS.version >= :lion
-    MacOS.cat
-  elsif MacOS.version == :snow_leopard
-    Hardware::CPU.is_64_bit? ? :snow_leopard : :snow_leopard_32
+        def pr(url)
+  if url.end_with?(FORWARD_SLASH)
+    url
   else
-    # Return, e.g., :tiger_g3, :leopard_g5_64, :leopard_64 (which is Intel)
-    if Hardware::CPU.type == :ppc
-      tag = '#{MacOS.cat}_#{Hardware::CPU.family}'.to_sym
+    url_dir = File.dirname(url)
+    url_dir.end_with?(FORWARD_SLASH) ? url_dir : '#{url_dir}/'
+  end
+end
+    
+    def source_dir(*subdirs)
+  test_dir('source', *subdirs)
+end
+    
+    puts 'Stackprof Mode: #{MODE}'
+    
+      p.action do |args, _|
+    if args.empty?
+      Jekyll.logger.error 'A subcommand is required.'
+      puts p
+      abort
     else
-      tag = MacOS.cat
-    end
-    MacOS.prefer_64_bit? ? '#{tag}_64'.to_sym : tag
-  end
-end
-    
-      def <<(o)
-    @settings << o
-    self
-  end
-    
-      def find_internal_commands(directory)
-    directory.children.reduce([]) do |cmds, f|
-      cmds << f.basename.to_s.sub(/\.(?:rb|sh)$/, '') if f.file?
-      cmds
+      subcommand = args.first
+      unless p.has_command? subcommand
+        Jekyll.logger.abort_with 'fatal: 'jekyll #{args.first}' could not' \
+          ' be found. You may need to install the jekyll-#{args.first} gem' \
+          ' or a related gem to be able to use this subcommand.'
+      end
     end
   end
 end
 
     
-        first_warning = true
-    methods.each do |method|
-      unless checks.respond_to?(method)
-        Homebrew.failed = true
-        puts 'No check available by the name: #{method}'
-        next
+    def run_rubygem(args)
+  run_in_shell('gem', *args.strip.split(' '))
+end
+    
+      test 'should accept confirmation email token after 2 days when expiration is set to 3 days' do
+    swap Devise, confirm_within: 3.days do
+      assert confirm_user_by_token_with_confirmation_sent_at(2.days.ago)
+    end
+  end
+    
+        # Check if proper Lockable module methods are present & unlock strategy
+    # allows to unlock resource on password reset
+    def unlockable?(resource)
+      resource.respond_to?(:unlock_access!) &&
+        resource.respond_to?(:unlock_strategy_enabled?) &&
+        resource.unlock_strategy_enabled?(:email)
+    end
+    
+      # Controllers inheriting DeviseController are advised to override this
+  # method so that other controllers inheriting from them would use
+  # existing translations.
+  def translation_scope
+    'devise.#{controller_name}'
+  end
+    
+        def redirect
+      store_location!
+      if is_flashing_format?
+        if flash[:timedout] && flash[:alert]
+          flash.keep(:timedout)
+          flash.keep(:alert)
+        else
+          flash[:alert] = i18n_message
+        end
       end
+      redirect_to redirect_url
+    end
     
-    require 'formula_versions'
-require 'migrator'
-require 'formulary'
-require 'descriptions'
-    
-          To export the needed variables, add them to your dotfiles.
-       * On Bash, add them to `~/.bash_profile`.
-       * On Zsh, add them to `~/.zprofile` instead.
-    
-      def process_bootstrap
-    log_status 'Convert Bootstrap LESS to Sass'
-    puts ' repo   : #@repo_url'
-    puts ' branch : #@branch_sha #@repo_url/tree/#@branch'
-    puts ' save to: #{@save_to.to_json}'
-    puts ' twbs cache: #{@cache_path}'
-    puts '-' * 60
-    
-        def read(file)
-      if file.respond_to?(:read)
-        file.read
+          if valid_type?(type)
+        type.constantize.new(attributes).tap do |instance|
+          instance.user = user if instance.respond_to?(:user=)
+        end
       else
-        open(file, 'rb') {|f| f.read}
+        const_get(:BASE_CLASS_NAME).constantize.new(attributes).tap do |instance|
+          instance.type = type
+          instance.user = user if instance.respond_to?(:user=)
+        end
       end
     end
   end
 end
-
     
-          # Prints a title in blue with surrounding newlines.
-      def title(text)
-        # Use $stdout directly to bypass the indentation that our `puts` does.
-        $stdout.puts(color.colorize('\n#{text}\n', :blue))
+        respond_to do |format|
+      format.html
+      format.json { render json: @events }
+    end
+  end
+    
+      def destroy
+    @services = current_user.services.find(params[:id])
+    @services.destroy
+    
+          def handle_prompt_none
+        if params[:prompt] == 'none'
+          if user_signed_in?
+            handle_prompt_with_signed_in_user
+          else
+            handle_params_error('login_required', 'User must already be logged in when `prompt` is `none`')
+          end
+        else
+          handle_params_error('invalid_request', 'The 'none' value cannot be used with any other prompt value')
+        end
       end
     
-        describe 'when setting user and port' do
-      subject { dsl.roles(:all).map { |server| '#{server.user}@#{server.hostname}:#{server.port}' }.first }
+          rescue_from Api::OpenidConnect::Error::InvalidRedirectUri do |e|
+        validation_fail_redirect_uri(e)
+      end
     
-      it 'provides a --format option which enables the choice of output formatting'
+          def create
+        req = Rack::Request.new(request.env)
+        if req['client_assertion_type'] == 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
+          handle_jwt_bearer(req)
+        end
+        self.status, headers, self.response_body = Api::OpenidConnect::TokenEndpoint.new.call(request.env)
+        headers.each {|name, value| response.headers[name] = value }
+        nil
+      end
+    
+          @options               = options
+      @post_processing       = true
+      @queued_for_delete     = []
+      @queued_for_write      = {}
+      @errors                = {}
+      @dirty                 = false
+      @interpolator          = options[:interpolator]
+      @url_generator         = options[:url_generator].new(self)
+      @source_file_options   = options[:source_file_options]
+      @whiny                 = options[:whiny]
+    
+        # Returns the id of the instance in a split path form. e.g. returns
+    # 000/001/234 for an id of 1234.
+    def id_partition attachment, style_name
+      case id = attachment.instance.id
+      when Integer
+        ('%09d'.freeze % id).scan(/\d{3}/).join('/'.freeze)
+      when String
+        id.scan(/.{3}/).first(3).join('/'.freeze)
+      else
+        nil
+      end
+    end
+    
+          if app.config.respond_to?(:paperclip_defaults)
+        Paperclip::Attachment.default_options.merge!(app.config.paperclip_defaults)
+      end
+    end
+    
+      def as_boolean(string)
+    return true   if string == true   || string =~ (/(true|t|yes|y|1)$/i)
+    return false  if string == false  || string.blank? || string =~ (/(false|f|no|n|0)$/i)
+    raise ArgumentError.new('invalid value for Boolean: \'#{string}\'')
+  end
+    
+          def snapshot
+        agent.metric.collector.snapshot_metric
+      end
+    
+      def twenty_five_percent
+    [1, (maximum * 0.25)].max.floor
+  end
