5a00d80955686c700335dd4ff00f3fe69c8c9d86
SUNsung <weatherius@gmail.com>
2018-03-19 23:43:26 +0200

    Rewrite [06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py] File in: 2018-03-19 23:43:25

--- pyton/06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py
+++ pyton/06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py
@@ -1,166 +1,49 @@
 
         
-            # Separating the 'table of contents' from the contents (blocks)
-    table_of_contents = ''.join(read_me.split('- - -')[0])
-    blocks = ''.join(read_me.split('- - -')[1]).split('\n# ')
-    for i in range(len(blocks)):
-        if i == 0:
-            blocks[i] = blocks[i] + '\n'
-        else:
-            blocks[i] = '# ' + blocks[i] + '\n'
+        - tree.RewriteCardinalityException
+  - tree.RewriteEarlyExitException
+  - tree.RewriteEmptyStreamException
+  .
+.
     
-    # TODO: ensure that history changes.
+    from antlr3.constants import INVALID_TOKEN_TYPE
+    
+            else:
+            msg = str(e)
+    
+                        if snext < 0:
+                        #print 'not a normal transition'
+                        # was in range but not a normal transition
+                        # must check EOT, which is like the else clause.
+                        # eot[s]>=0 indicates that an EOT edge goes to another
+                        # state.
+                        if self.eot[s] >= 0: # EOT Transition to accept state?
+                            #print 'EOT trans to accept state %d' % self.eot[s]
+    
+            Should only be used by Executor implementations and unit tests.
+        '''
+        with self._condition:
+            self._result = result
+            self._state = FINISHED
+            for waiter in self._waiters:
+                waiter.add_result(self)
+            self._condition.notify_all()
+        self._invoke_callbacks()
+    
+    atexit.register(_python_exit)
 
     
-    def print_title(title, pattern = '-'):
-    print('\n'.join(('', title, pattern * len(title)))) 
     
-    # Can't import these from paths.py because that uses `future` imports
-DIR_OF_CURRENT_SCRIPT = os.path.dirname( os.path.abspath( __file__ ) )
-DIR_OF_YCMD = os.path.join( DIR_OF_CURRENT_SCRIPT, '..', '..', 'third_party',
-                            'ycmd' )
-    
-        def _adjust_thread_count(self):
-        # When the executor gets lost, the weakref callback will wake up
-        # the worker threads.
-        def weakref_cb(_, q=self._work_queue):
-            q.put(None)
-        # TODO(bquinlan): Should avoid creating new threads if there are more
-        # idle threads than items in the work queue.
-        if len(self._threads) < self._max_workers:
-            t = threading.Thread(target=_worker,
-                                 args=(weakref.ref(self, weakref_cb),
-                                       self._work_queue))
-            t.daemon = True
-            t.start()
-            self._threads.add(t)
-    
-        # Parse and validate the field names.  Validation serves two purposes,
-    # generating informative error messages and preventing template injection attacks.
-    if isinstance(field_names, basestring):
-        field_names = field_names.replace(',', ' ').split() # names separated by whitespace and/or commas
-    field_names = tuple(map(str, field_names))
-    for name in (typename,) + field_names:
-        if not all(c.isalnum() or c=='_' for c in name):
-            raise ValueError('Type names and field names can only contain alphanumeric characters and underscores: %r' % name)
-        if _iskeyword(name):
-            raise ValueError('Type names and field names cannot be a keyword: %r' % name)
-        if name[0].isdigit():
-            raise ValueError('Type names and field names cannot start with a number: %r' % name)
-    seen_names = set()
-    for name in field_names:
-        if name.startswith('_'):
-            raise ValueError('Field names cannot start with an underscore: %r' % name)
-        if name in seen_names:
-            raise ValueError('Encountered duplicate field name: %r' % name)
-        seen_names.add(name)
-    
-        def test_pending_calls_race(self):
-        # Issue #14406: multi-threaded race condition when waiting on all
-        # futures.
-        event = threading.Event()
-        def future_func():
-            event.wait()
-        oldswitchinterval = sys.getcheckinterval()
-        sys.setcheckinterval(1)
-        try:
-            fs = set(self.executor.submit(future_func) for i in range(100))
-            event.set()
-            futures.wait(fs, return_when=futures.ALL_COMPLETED)
-        finally:
-            sys.setcheckinterval(oldswitchinterval)
-    
-      CheckCall( [ sys.executable, build_file ] + sys.argv[ 1: ] )
+FILTER_COMPILERS = { 'regex' : CompileRegex,
+                     'level' : CompileLevel }
     
     
-# This class can be used to keep the ycmd server alive for the duration of the
-# life of the client. By default, ycmd shuts down if it doesn't see a request in
-# a while.
-class YcmdKeepalive( object ):
-  def __init__( self, ping_interval_seconds = 60 * 10 ):
-    self._keepalive_thread = Thread( target = self._ThreadMain )
-    self._keepalive_thread.daemon = True
-    self._ping_interval_seconds = ping_interval_seconds
-    
-    '''
-Port of the Java example of 'Setter Injection' in
-'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
-(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
-http://xunitpatterns.com/Dependency%20Injection.html.
-    
-    '''
-Port of the Java example of 'Parameter Injection' in
-'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
-(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
-http://xunitpatterns.com/Dependency%20Injection.html.
+def _JavaFilter( config ):
+  return { 'filter_diagnostics' : { 'java': config } }
     
     
-@coroutine
-def coroutine1(target):
-    while True:
-        request = yield
-        if 0 < request <= 10:
-            print('request {} handled in coroutine 1'.format(request))
-        else:
-            target.send(request)
-    
-            # they can be executed later on
-        for cmd in command_stack:
-            cmd.execute()
-    
-    ### OUTPUT ###
-# Counting to two...
-# one two
-# Counting to five...
-# one two three four five
-
-    
-    print 'PLUGIN_I18N_PATHS := ' + ','.join(os.path.relpath(plugin.path)
-                                         for plugin in plugins
-                                         if plugin.needs_translation)
-    
-    
-class SubredditMiddleware(object):
-    sr_pattern = re.compile(r'^/r/([^/]{2,})')
-    
-    
-def register_api_templates(template_name, template_class):
-    for style in ('api', 'api-html', 'api-compact'):
-        tpm.add_handler(
-            name=template_name,
-            style=style,
-            handler=template_class,
-        )
-    
-            res = AdminPage(content = AdminAwardWinners(award),
-                        title='award winners').render()
-        return res
-
-    
-        def GET_button_demo_page(self):
-        # no buttons for domain listings -> redirect to top level
-        if isinstance(c.site, DomainSR):
-            return self.redirect('/buttons')
-        return BoringPage(_('reddit buttons'),
-                          show_sidebar = False, 
-                          content=ButtonDemoPanel()).render()
-    
-    
-redditbroke =  \
-'''<html>
-  <head>
-    <title>reddit broke!</title>
-  </head>
-  <body>
-    <div style='margin: auto; text-align: center'>
-      <p>
-        <a href='/'>
-          <img border='0' src='%s' alt='you broke reddit' />
-        </a>
-      </p>
-      <p>
-        %s
-      </p>
-  </body>
-</html>
-'''
+def MakeUserOptions( custom_options = {} ):
+  options = dict( user_options_store.DefaultOptions() )
+  options.update( DEFAULT_CLIENT_OPTIONS )
+  options.update( custom_options )
+  return options
