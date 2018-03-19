2d3f74c90f1807816dba480c082bd0e9973addac
SUNsung <weatherius@gmail.com>
2018-03-20 00:46:54 +0200

    Rewrite [37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb] File in: 2018-03-20 00:46:53

--- ruby/37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb
+++ ruby/37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb
@@ -1,95 +1,37 @@
 
         
-          if temp_ignores.include?(name)
-    puts '#{name}: IGNORE [TEMPORARILY]'
-    next
-  end
-    
-          rescue_from OpenSSL::SSL::SSLError do |e|
-        validation_fail_as_json(e)
-      end
-    
-    # IMPORTANT: The Capistrano::Plugin system is not yet considered a stable,
-# public API, and is subject to change without notice. Eventually it will be
-# officially documented and supported, but for now, use it at your own risk.
-#
-# Base class for Capistrano plugins. Makes building a Capistrano plugin as easy
-# as writing a `Capistrano::Plugin` subclass and overriding any or all of its
-# three template methods:
-#
-# * set_defaults
-# * register_hooks
-# * define_tasks
-#
-# Within the plugin you can use any methods of the Rake or Capistrano DSLs, like
-# `fetch`, `invoke`, etc. In cases when you need to use SSHKit's backend outside
-# of an `on` block, use the `backend` convenience method. E.g. `backend.test`,
-# `backend.execute`, or `backend.capture`.
-#
-# Package up and distribute your plugin class as a gem and you're good to go!
-#
-# To use a plugin, all a user has to do is install it in the Capfile, like this:
-#
-#   # Capfile
-#   require 'capistrano/superfancy'
-#   install_plugin Capistrano::Superfancy
-#
-# Or, to install the plugin without its hooks:
-#
-#   # Capfile
-#   require 'capistrano/superfancy'
-#   install_plugin Capistrano::Superfancy, load_hooks: false
-#
-class Capistrano::Plugin < Rake::TaskLib
-  include Capistrano::DSL
-    
-      puts I18n.t :capified, scope: :capistrano
-end
-
-    
-          context 'when the variables is undefined' do
-        it 'returns nil' do
-          expect(dsl.fetch(:source_control)).to be_nil
-        end
-      end
+            def initialize
+      @pages = {}
     end
     
-            def index
-          authorize! :read, StockMovement
-          @stock_movements = scope.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
-          respond_with(@stock_movements)
-        end
+              # Underscore methods
+          if name.start_with?('Underscore')
+            node.at_css('~ ul').css('li').each do |li|
+              name = [type.downcase, li.at_css('a').content.split.first].join('.')
+              id = name.parameterize
+              li['id'] = id
+              entries << [name, id, type]
+            end
+            next
+          end
     
-        def definitions_for(klass)
-      parent_classes = klass.ancestors.reverse
-      parent_classes.each_with_object({}) do |ancestor, inherited_definitions|
-        inherited_definitions.deep_merge! @attachments[ancestor]
-      end
-    end
-  end
-end
-
+          def additional_entries
+        entries = []
     
-        def add_required_validations
-      options = Paperclip::Attachment.default_options.deep_merge(@options)
-      if options[:validate_media_type] != false
-        name = @name
-        @klass.validates_media_type_spoof_detection name,
-          :if => ->(instance){ instance.send(name).dirty? }
-      end
+        # Returns the CSS for the media query.
+    #
+    # @return [String]
+    def to_css
+      css = ''
+      css << resolved_modifier
+      css << ' ' unless resolved_modifier.empty?
+      css << resolved_type
+      css << ' and ' unless resolved_type.empty? || expressions.empty?
+      css << expressions.map do |e|
+        # It's possible for there to be script nodes in Expressions even when
+        # we're converting to CSS in the case where we parsed the document as
+        # CSS originally (as in css_test.rb).
+        e.map {|c| c.is_a?(Sass::Script::Tree::Node) ? c.to_sass : c.to_s}.join
+      end.join(' and ')
+      css
     end
-    
-            def no_error_when_valid?
-          @file = StringIO.new('.')
-          @subject.send(@attachment_name).assign(@file)
-          @subject.valid?
-          expected_message = [
-            @attachment_name.to_s.titleize,
-            I18n.t(:blank, scope: [:errors, :messages])
-          ].join(' ')
-          @subject.errors.full_messages.exclude?(expected_message)
-        end
-      end
-    end
-  end
-end
