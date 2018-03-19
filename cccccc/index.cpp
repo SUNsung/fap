8dea02cba3cd5dfca7a0f41d90b8f536383dda83
SUNsung <weatherius@gmail.com>
2018-03-19 09:08:22 +0200

    Rewrite [index.cpp] File in: 2018-03-19 09:08:21

--- cccccc/index.cpp
+++ cccccc/index.cpp
@@ -1,255 +1,297 @@
 
         
-          // Initializes all members to the default options.
-  PluginConfig();
-    
-    bool HloReachabilityMap::IsConnected(const HloInstruction* a,
-                                     const HloInstruction* b) const {
-  return IsReachable(a, b) || IsReachable(b, a);
-}
-    
-    #if GOOGLE_CUDA
-#define REGISTER_DYNAMIC_STITCH_GPU(type)                \
-  REGISTER_KERNEL_BUILDER(Name('DynamicStitch')          \
-                              .Device(DEVICE_GPU)        \
-                              .TypeConstraint<type>('T') \
-                              .HostMemory('indices'),    \
-                          DynamicStitchOpGPU<type>)      \
-  REGISTER_KERNEL_BUILDER(Name('ParallelDynamicStitch')  \
-                              .Device(DEVICE_GPU)        \
-                              .TypeConstraint<type>('T') \
-                              .HostMemory('indices')     \
-                              .HostMemory('data')        \
-                              .HostMemory('merged'),     \
-                          ParallelDynamicStitchOpCPU<type>)
-    
-    #include 'atom/browser/api/event.h'
-    
-    #include 'atom/browser/api/event.h'
-#include 'native_mate/arguments.h'
-#include 'native_mate/dictionary.h'
-#include 'native_mate/object_template_builder.h'
-#include 'ui/events/event_constants.h'
-    
-      const net::AuthChallengeInfo* auth_info() const { return auth_info_.get(); }
-    
-    
-    {}  // namespace accelerator_util
-    
-    int main(int argc, char** argv) {
-  if (argc != 4) {
-    printf('This script converts the MNIST dataset to the leveldb format used\n'
-           'by caffe to train a siamese network.\n'
-           'Usage:\n'
-           '    convert_mnist_data input_image_file input_label_file '
-           'output_db_file\n'
-           'The MNIST dataset could be downloaded at\n'
-           '    http://yann.lecun.com/exdb/mnist/\n'
-           'You should gunzip them after downloading.\n');
-  } else {
-    google::InitGoogleLogging(argv[0]);
-    convert_dataset(argv[1], argv[2], argv[3]);
-  }
-  return 0;
-}
-#else
-int main(int argc, char** argv) {
-  LOG(FATAL) << 'This example requires LevelDB; compile with USE_LEVELDB.';
-}
-#endif  // USE_LEVELDB
-
-    
-      /**
-   * @brief Applies the transformation defined in the data layer's
-   * transform_param block to a cv::Mat
-   *
-   * @param cv_img
-   *    cv::Mat containing the data to be transformed.
-   * @param transformed_blob
-   *    This is destination blob. It can be part of top blob's data if
-   *    set_cpu_data() is used. See image_data_layer.cpp for an example.
-   */
-  void Transform(const cv::Mat& cv_img, Blob<Dtype>* transformed_blob);
-#endif  // USE_OPENCV
-    
-    #include <utility>
-#include <vector>
-    
-    /**
- * @brief Takes at least two Blob%s and concatenates them along either the num
- *        or channel dimension, outputting the result.
- */
-template <typename Dtype>
-class ConcatLayer : public Layer<Dtype> {
- public:
-  explicit ConcatLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+        void CodeCompletionOrganizer::Impl::addCompletionsWithFilter(
+    ArrayRef<Completion *> completions, StringRef filterText, Options options,
+    const FilterRules &rules, Completion *&exactMatch) {
+  assert(rootGroup);
     }
     
-    #endif  // CAFFE_CONV_LAYER_HPP_
-
+    #include 'llvm/ADT/SetVector.h'
     
     
-    {  /// when divided by UINT_MAX, the randomly generated values @f$u\sim U(0,1)@f$
-  Blob<unsigned int> rand_vec_;
-  /// the probability @f$ p @f$ of dropping any input
-  Dtype threshold_;
-  /// the scale for undropped inputs at train time @f$ 1 / (1 - p) @f$
-  Dtype scale_;
-  unsigned int uint_thres_;
+    {/// Extract comments parts from the given Markup node.
+swift::markup::CommentParts
+extractCommentParts(swift::markup::MarkupContext &MC,
+                    swift::markup::MarkupASTNode *Node);
+} // namespace swift
+    
+    /// A SyntaxRewriter for applying a set of formatting rules to a Syntax tree.
+struct FormatSyntaxRewriter : public SyntaxRewriter {
+  virtual StructDeclSyntax
+  rewriteStructDecl(StructDeclSyntax Struct) override;
 };
     
-    /**
- * @brief Compute elementwise operations, such as product and sum,
- *        along multiple input Blobs.
- *
- * TODO(dox): thorough documentation for Forward, Backward, and proto params.
- */
-template <typename Dtype>
-class EltwiseLayer : public Layer<Dtype> {
- public:
-  explicit EltwiseLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
+      /// Indicates whether the diagnostics produced during compilation should be
+  /// checked against expected diagnostics, indicated by markers in the
+  /// input source file.
+  enum {
+    NoVerify,
+    Verify,
+    VerifyAndApplyFixes
+  } VerifyMode = NoVerify;
     
-    /** @defgroup buffer_cast boost::asio::buffer_cast
- *
- * @brief The boost::asio::buffer_cast function is used to obtain a pointer to
- * the underlying memory region associated with a buffer.
- *
- * @par Examples:
- *
- * To access the memory of a non-modifiable buffer, use:
- * @code boost::asio::const_buffer b1 = ...;
- * const unsigned char* p1 = boost::asio::buffer_cast<const unsigned char*>(b1);
- * @endcode
- *
- * To access the memory of a modifiable buffer, use:
- * @code boost::asio::mutable_buffer b2 = ...;
- * unsigned char* p2 = boost::asio::buffer_cast<unsigned char*>(b2);
- * @endcode
- *
- * The boost::asio::buffer_cast function permits violations of type safety, so
- * uses of it in application code should be carefully considered.
- */
-/*@{*/
-    }
+      /// Returns the identifier for the buffer with the given ID.
+  ///
+  /// \p BufferID must be a valid buffer ID.
+  StringRef getIdentifierForBuffer(unsigned BufferID) const;
     
-    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
-# pragma once
-#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
-    
-    #ifndef BOOST_ASIO_DETAIL_BASE_FROM_COMPLETION_COND_HPP
-#define BOOST_ASIO_DETAIL_BASE_FROM_COMPLETION_COND_HPP
-    
-    #if !defined(BOOST_ASIO_WINDOWS) && !defined(__CYGWIN__)
-    
-    #if !defined(BOOST_ASIO_HAS_THREADS)
-typedef null_event event;
-#elif defined(BOOST_ASIO_WINDOWS)
-typedef win_event event;
-#elif defined(BOOST_ASIO_HAS_PTHREADS)
-typedef posix_event event;
-#elif defined(BOOST_ASIO_HAS_STD_MUTEX_AND_CONDVAR)
-typedef std_event event;
-#endif
-    
-    template <typename Function, typename Context>
-inline void invoke(const Function& function, Context& context)
-{
-#if !defined(BOOST_ASIO_HAS_HANDLER_HOOKS)
-  Function tmp(function);
-  tmp();
-#else
-  using boost::asio::asio_handler_invoke;
-  asio_handler_invoke(function, boost::asio::detail::addressof(context));
-#endif
+    // Free allocated memory and clear pointers.
+void DENORM::Clear() {
+  if (x_map_ != NULL) {
+    delete x_map_;
+    x_map_ = NULL;
+  }
+  if (y_map_ != NULL) {
+    delete y_map_;
+    y_map_ = NULL;
+  }
+  if (rotation_ != NULL) {
+    delete rotation_;
+    rotation_ = NULL;
+  }
 }
     
-    class winrt_buffer_impl :
-  public Microsoft::WRL::RuntimeClass<
-    Microsoft::WRL::RuntimeClassFlags<
-      Microsoft::WRL::RuntimeClassType::WinRtClassicComMix>,
-    ABI::Windows::Storage::Streams::IBuffer,
-    Windows::Storage::Streams::IBufferByteAccess>
+    /**----------------------------------------------------------------------------
+          Include Files and Type Defines
+----------------------------------------------------------------------------**/
+#include 'strngs.h'
+    
+    // Tests a pair of classifiers, debugging errors of the new against the old.
+// See errorcounter.h for description of arguments.
+// Iterates over the samples, calling the classifiers in normal/silent mode.
+// If the new_classifier makes a boosting_mode error that the old_classifier
+// does not, it will then call the new_classifier again with a debug flag
+// and a keep_this argument to find out what is going on.
+void ErrorCounter::DebugNewErrors(
+    ShapeClassifier* new_classifier, ShapeClassifier* old_classifier,
+    CountTypes boosting_mode,
+    const FontInfoTable& fontinfo_table,
+    const GenericVector<Pix*>& page_images, SampleIterator* it) {
+  int fontsize = it->sample_set()->NumFonts();
+  ErrorCounter old_counter(old_classifier->GetUnicharset(), fontsize);
+  ErrorCounter new_counter(new_classifier->GetUnicharset(), fontsize);
+  GenericVector<UnicharRating> results;
+    }
+    
+      // Creates a report of the error rate. The report_level controls the detail
+  // that is reported to stderr via tprintf:
+  // 0   -> no output.
+  // >=1 -> bottom-line error rate.
+  // >=3 -> font-level error rate.
+  // boosting_mode determines the return value. It selects which (un-weighted)
+  // error rate to return.
+  // The fontinfo_table from MasterTrainer provides the names of fonts.
+  // The it determines the current subset of the training samples.
+  // If not NULL, the top-choice unichar error rate is saved in unichar_error.
+  // If not NULL, the report string is saved in fonts_report.
+  // (Ignoring report_level).
+  double ReportErrors(int report_level, CountTypes boosting_mode,
+                      const FontInfoTable& fontinfo_table,
+                      const SampleIterator& it,
+                      double* unichar_error,
+                      STRING* fonts_report);
+    
+    void FreeOutlines(LIST Outlines);
+    
+    // same but for event
+void GPUDataTransferer::SyncEvent(cudaEvent_t ev)
 {
+    auto rc = cudaEventQuery(ev);
+    if (rc != cudaErrorNotReady)
+    {
+        // if Event is ready then no need to wait
+        rc || 'cudaEventQuery failed';
+        return;
+    }
+    // we must wait
+    cudaEventSynchronize(ev) || 'cudaEventSynchronize failed';
+}
+    
+                if (!wasDataRead) LogicError('DataRead Failure in batch normalization statistics');
+    
+    using namespace std;
+using namespace Microsoft::MSR::ScriptableObjects;
+    
+    
+    {        // compute global progress
+        bool needToPrint = us.m_progressTracingTimer.ElapsedSeconds() > 10;
+        if (needToPrint || isTimerPaced)
+        {
+            double epochProg = ((100.0f * (double) (us.m_currentStepOffset + epochNumber)) / (double) us.m_totalNumberOfSteps);
+            mbProg = (mbProg * 100.0f) / (double) us.m_totalNumberOfSteps;
+            printf('PROGRESS: %.2f%%\n', epochProg + mbProg);
+            us.m_progressTracingTimer.Restart();
+        }
+        return needToPrint;
+    }
+    
+        void getlattices(const std::wstring& key, std::shared_ptr<const latticepair>& L, size_t expectedframes) const
+    {
+        std::shared_ptr<latticepair> LP(new latticepair);
+        denlattices.getlattice(key, LP->second, expectedframes); // this loads the lattice from disk, using the existing L.second object
+        L = LP;
+    }
+    
+    template <class _T, int _N>
+class hardcoded_array
+{
+    _T data[_N];
+    inline void check_index(size_t i) const
+    {
+        i;
+        assert(i < _N);
+    }
+    inline void check_size(size_t n) const
+    {
+        n;
+        assert(n == _N);
+    }
+    }
+    
+    // ===================================================================
+// behave like a config
+// This allows to access nodes inside a network as if it was an IConfigRecord.
+// This is meant to be used by whatever we will replace MEL.
+// ===================================================================
+    
+        cobj = (CocosDenshion::SimpleAudioEngine*)tolua_tousertype(tolua_S,1,0);
+    
+    
+    
+        void initShader( void );
 public:
-  explicit winrt_buffer_impl(const boost::asio::const_buffer& b)
-  {
-    bytes_ = const_cast<byte*>(boost::asio::buffer_cast<const byte*>(b));
-    length_ = boost::asio::buffer_size(b);
-    capacity_ = boost::asio::buffer_size(b);
+    GLESDebugDraw();
+    
+    
+    {
+    {			float minX = -6.0f;
+			float maxX = 0.0f;
+			float minY = 4.0f;
+			float maxY = 6.0f;
+			
+			for (int32 i = 0; i < 400; ++i)
+			{
+				b2BodyDef bd;
+				bd.type = b2_dynamicBody;
+				bd.position = b2Vec2(RandomFloat(minX,maxX),RandomFloat(minY,maxY));
+				b2Body* body = m_world->CreateBody(&bd);
+				body->CreateFixture(&shape, 0.01f);
+			}
+		}
+		
+		{
+			b2PolygonShape shape;
+			shape.SetAsBox(1.5f, 1.5f);
+			b2BodyDef bd;
+			bd.type = b2_dynamicBody;
+			bd.position.Set(-40.0f,5.0f);
+			bd.bullet = true;
+			b2Body* body = m_world->CreateBody(&bd);
+			body->CreateFixture(&shape, 1.0f);
+			body->SetLinearVelocity(b2Vec2(150.0f, 0.0f));
+		}
+	}
+    
+    
+    {			bd.position.Set(0.0f, 2.0);
+			bd.angle = b2_pi;
+			bd.allowSleep = false;
+			m_body = m_world->CreateBody(&bd);
+			m_body->CreateFixture(&sd1);
+			m_body->CreateFixture(&sd2);
+		}
+    
+    
+    {			b2PolygonShape shape;
+			shape.SetAsBox(0.5f, 2.0f);
+			m_attachment->CreateFixture(&shape, 2.0f);
+		}
+    
+    		b2Body* body2 = m_world->CreateBody(&bd);
+		m_piece2 = body2->CreateFixture(&m_shape2, 1.0f);
+    
+    			bd.position.Set(-1.0f, 0.35f);
+			m_wheel1 = m_world->CreateBody(&bd);
+			m_wheel1->CreateFixture(&fd);
+    
+    class InputCorpus {
+ public:
+  static const size_t kFeatureSetSize = 1 << 16;
+  InputCorpus(const std::string &OutputCorpus) : OutputCorpus(OutputCorpus) {
+    memset(InputSizesPerFeature, 0, sizeof(InputSizesPerFeature));
+    memset(SmallestElementPerFeature, 0, sizeof(SmallestElementPerFeature));
+  }
+  ~InputCorpus() {
+    for (auto II : Inputs)
+      delete II;
+  }
+  size_t size() const { return Inputs.size(); }
+  size_t SizeInBytes() const {
+    size_t Res = 0;
+    for (auto II : Inputs)
+      Res += II->U.size();
+    return Res;
+  }
+  size_t NumActiveUnits() const {
+    size_t Res = 0;
+    for (auto II : Inputs)
+      Res += !II->U.empty();
+    return Res;
+  }
+  bool empty() const { return Inputs.empty(); }
+  const Unit &operator[] (size_t Idx) const { return Inputs[Idx]->U; }
+  void AddToCorpus(const Unit &U, size_t NumFeatures, bool MayDeleteFile = false) {
+    assert(!U.empty());
+    uint8_t Hash[kSHA1NumBytes];
+    if (FeatureDebug)
+      Printf('ADD_TO_CORPUS %zd NF %zd\n', Inputs.size(), NumFeatures);
+    ComputeSHA1(U.data(), U.size(), Hash);
+    Hashes.insert(Sha1ToString(Hash));
+    Inputs.push_back(new InputInfo());
+    InputInfo &II = *Inputs.back();
+    II.U = U;
+    II.NumFeatures = NumFeatures;
+    II.MayDeleteFile = MayDeleteFile;
+    memcpy(II.Sha1, Hash, kSHA1NumBytes);
+    UpdateCorpusDistribution();
+    ValidateFeatureSet();
   }
     }
     
-      op_queue<operation> ops;
-    
-    
-    {        std::string arg0_tmp; ok &= luaval_to_std_string(tolua_S, 2, &arg0_tmp, 'cc.SimpleAudioEngine:preloadBackgroundMusic'); arg0 = arg0_tmp.c_str();
-        if(!ok)
-        {
-            tolua_error(tolua_S,'invalid arguments in function 'lua_cocos2dx_cocosdenshion_SimpleAudioEngine_preloadBackgroundMusic'', nullptr);
-            return 0;
-        }
-        cobj->preloadBackgroundMusic(arg0);
-        lua_settop(tolua_S, 1);
-        return 1;
+    template <typename T>
+static T GetFnPtr(const char *FnName, bool WarnIfMissing) {
+  dlerror(); // Clear any previous errors.
+  void *Fn = dlsym(RTLD_DEFAULT, FnName);
+  if (Fn == nullptr) {
+    if (WarnIfMissing) {
+      const char *ErrorMsg = dlerror();
+      Printf('WARNING: Failed to find function \'%s\'.', FnName);
+      if (ErrorMsg)
+        Printf(' Reason %s.', ErrorMsg);
+      Printf('\n');
     }
-    luaL_error(tolua_S, '%s has wrong number of arguments: %d, was expecting %d \n', 'cc.SimpleAudioEngine:preloadBackgroundMusic',argc, 1);
-    return 0;
-    
-    
-#endif // __cocos2dx_experimental_h__
-
-    
-    
-    
-    #endif
-
-    
-    
-    {		Test::Step(settings);
-	}
-    
-    			b2RevoluteJointDef jd;
-    
-    
-    {    int hashedToken = 0;
-    while (isalnum(*in) || *in == '-' || *in == '_') {
-        if (isdigit(*in)) {
-            hashedToken = hashedToken * 10 - (*in - '0');
-        } else {
-            hashedToken += *in;
-        }
-        in++;
-    }
-    return hashedToken;
+  }
+  return reinterpret_cast<T>(Fn);
 }
     
-    namespace uS {
+    #include 'FuzzerExtFunctions.h'
+#include 'FuzzerIO.h'
+    
+    
+    {}  // namespace fuzzer
+    
+      DWORD LastError = GetLastError();
+  if (LastError != ERROR_NO_MORE_FILES)
+    Printf('FindNextFileA failed (Error code: %lu).\n', LastError);
+    
+    namespace fuzzer {
     }
     
-        static void forceClose(WebSocketState<isServer> *webSocketState) {
-        WebSocket<isServer> *webSocket = static_cast<WebSocket<isServer> *>(webSocketState);
-        webSocket->terminate();
+    namespace fuzzer {
     }
     
-        void run();
-    
-        uv_os_sock_t getFd() {
-#ifdef _WIN32
-        uv_os_sock_t fd;
-        uv_fileno((uv_handle_t *) uv_poll, (uv_os_fd_t *) &fd);
-        return fd;
-#else
-        return uv_poll->io_watcher.fd;
-#endif
-    }
+    #include 'FuzzerIO.h'
+#include <mutex>
+#include <signal.h>
+#include <spawn.h>
+#include <sys/wait.h>
