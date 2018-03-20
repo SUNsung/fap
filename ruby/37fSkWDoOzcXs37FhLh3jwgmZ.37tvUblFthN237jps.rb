9dea5255024c6eee8fffa04cc9c4e67af96d8276
SUNsung <weatherius@gmail.com>
2018-03-20 09:21:57 +0200

    Rewrite [37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb] File in: 2018-03-20 09:21:57

--- ruby/37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb
+++ ruby/37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb
@@ -1,37 +1,43 @@
 
         
-            def initialize
-      @pages = {}
+            def get_mixin_names(file, opts = {})
+      names = get_css_selectors(file).join('\n' * 2).scan(/^\.([\w-]+)\(#{LESS_MIXIN_DEF_ARGS_RE}\)(?: when.*?)?[ ]*\{/).map(&:first).uniq.sort
+      log_file_info 'mixin defs: #{names * ', '}' unless opts[:silent] || names.empty?
+      names
+    end
+    }
+    
+      config.active_support.deprecation = :stderr
+end
+
+    
+          spec['version'] = Bootstrap::VERSION
+    
+    # Single-line step scoper
+When /^(.*) within (.*[^:])$/ do |step, parent|
+  with_scope(parent) { When step }
+end
+    
+        def post_process_styles(*style_args) #:nodoc:
+      post_process_style(:original, styles[:original]) if styles.include?(:original) && process_style?(:original, style_args)
+      styles.reject{ |name, style| name == :original }.each do |name, style|
+        post_process_style(name, style) if process_style?(name, style_args)
+      end
     end
     
-              # Underscore methods
-          if name.start_with?('Underscore')
-            node.at_css('~ ul').css('li').each do |li|
-              name = [type.downcase, li.at_css('a').content.split.first].join('.')
-              id = name.parameterize
-              li['id'] = id
-              entries << [name, id, type]
-            end
-            next
+            def has_column?
+          @subject.column_names.include?('#{@attachment_name}_file_name')
+        end
+      end
+    end
+  end
+end
+
+    
+            def failure_message
+          '#{expected_attachment}\n'.tap do |message|
+            message << accepted_types_and_failures.to_s
+            message << '\n\n' if @allowed_types.present? && @rejected_types.present?
+            message << rejected_types_and_failures.to_s
           end
-    
-          def additional_entries
-        entries = []
-    
-        # Returns the CSS for the media query.
-    #
-    # @return [String]
-    def to_css
-      css = ''
-      css << resolved_modifier
-      css << ' ' unless resolved_modifier.empty?
-      css << resolved_type
-      css << ' and ' unless resolved_type.empty? || expressions.empty?
-      css << expressions.map do |e|
-        # It's possible for there to be script nodes in Expressions even when
-        # we're converting to CSS in the case where we parsed the document as
-        # CSS originally (as in css_test.rb).
-        e.map {|c| c.is_a?(Sass::Script::Tree::Node) ? c.to_sass : c.to_s}.join
-      end.join(' and ')
-      css
-    end
+        end
