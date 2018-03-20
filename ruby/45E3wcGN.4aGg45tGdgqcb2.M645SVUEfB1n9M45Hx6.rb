db3f952923dc35af952158c33514135c57fc446b
SUNsung <weatherius@gmail.com>
2018-03-20 10:41:21 +0200

    Rewrite [45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb] File in: 2018-03-20 10:41:20

--- ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
+++ ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
@@ -1,161 +1,53 @@
 
         
-                def tag_options(options, escape = true)
-          return if options.blank?
-          output = ''.dup
-          sep    = ' '
-          options.each_pair do |key, value|
-            if TAG_PREFIXES.include?(key) && value.is_a?(Hash)
-              value.each_pair do |k, v|
-                next if v.nil?
-                output << sep
-                output << prefix_tag_option(key, k, v, escape)
-              end
-            elsif BOOLEAN_ATTRIBUTES.include?(key)
-              if value
-                output << sep
-                output << boolean_tag_option(key)
-              end
-            elsif !value.nil?
-              output << sep
-              output << tag_option(key, value, escape)
-            end
-          end
-          output unless output.empty?
+                expect(result).to eq('/usr/local/bin/cloc --exclude-dir=test1,test2,build --by-file --xml  --out=build/cloc.xml')
+      end
+    
+          it 'pass a custom build number to the tool' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          increment_build_number(build_number: 24, xcodeproj: '.xcproject')
+        end').runner.execute(:test)
+    
+            expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to end_with('&& agvtool new-marketing-version 1.77.3')
+      end
+    
+        def sort_fn(a, b)
+      if (a.getbyte(0) >= 49 && a.getbyte(0) <= 57) || (b.getbyte(0) >= 49 && b.getbyte(0) <= 57)
+        a_split = a.split(SPLIT_INTS)
+        b_split = b.split(SPLIT_INTS)
+    
+        delegate :empty?, :blank?, to: :pages
+    
+              entries << [name, node['id'], type]
         end
     
-      def test_redirect_with_null_bytes
-    get :redirect_with_null_bytes
-    assert_response :redirect
-    assert_equal 'http://test.host/lolwat', redirect_to_url
+      # Set to :debug to see everything in the log.
+  config.log_level = :info
+    
+      def setup
+    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
+    success = Dir.chdir DUMMY_PATH do
+      silence_stdout_if !ENV['VERBOSE'] do
+        system 'node', 'manifest.js', tmp_dir
+      end
+    end
+    assert success, 'Node.js Mincer compilation failed'
+    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
+    css_name = manifest['assets']['application.css']
+    @css = File.read('#{tmp_dir}/#{css_name}')
   end
+end
+
     
-              # Returns fully resolved connection, accepts hash, string or symbol.
-          # Always returns a hash.
-          #
-          # == Examples
-          #
-          # Symbol representing current environment.
-          #
-          #   Resolver.new('production' => {}).resolve_connection(:production)
-          #   # => {}
-          #
-          # One layer deep hash of connection values.
-          #
-          #   Resolver.new({}).resolve_connection('adapter' => 'sqlite3')
-          #   # => { 'adapter' => 'sqlite3' }
-          #
-          # Connection URL.
-          #
-          #   Resolver.new({}).resolve_connection('postgresql://localhost/foo')
-          #   # => { 'host' => 'localhost', 'database' => 'foo', 'adapter' => 'postgresql' }
-          #
-          def resolve_connection(spec)
-            case spec
-            when Symbol
-              resolve_symbol_connection spec
-            when String
-              resolve_url_connection spec
-            when Hash
-              resolve_hash_connection spec
-            end
-          end
+          spec['version'] = Bootstrap::VERSION
     
-      class TestInheritedMiddleware < TestMiddleware
-    def setup
-      @app = InheritedController.action(:index)
+          expect('.margin-false-third').to have_ruleset(ruleset)
+      expect('.margin-false-third').to_not have_rule(bad_rule)
     end
   end
 end
 
     
-          add_delivery_method :smtp, Mail::SMTP,
-        address:              'localhost',
-        port:                 25,
-        domain:               'localhost.localdomain',
-        user_name:            nil,
-        password:             nil,
-        authentication:       nil,
-        enable_starttls_auto: true
-    
-            def enqueue_delivery(delivery_method, options = {})
-          if processed?
-            super
-          else
-            args = @mailer_class.name, @action.to_s, delivery_method.to_s, @params, *@args
-            ActionMailer::Parameterized::DeliveryJob.set(options).perform_later(*args)
-          end
-        end
-    end
-    
-    # Disable available locale checks to avoid warnings running the test suite.
-I18n.enforce_available_locales = false
-    
-          topic.feature_topic_users(args)
-    end
-    
-            # Checks whether the record requires any confirmation.
-        def pending_any_confirmation
-          if (!confirmed? || pending_reconfirmation?)
-            yield
-          else
-            self.errors.add(:email, :already_confirmed)
-            false
-          end
-        end
-    
-      # GET /resource/sign_in
-  def new
-    self.resource = resource_class.new(sign_in_params)
-    clean_up_passwords(resource)
-    yield resource if block_given?
-    respond_with(resource, serialize_options(resource))
-  end
-    
-        if successfully_sent?(resource)
-      respond_with({}, location: after_sending_unlock_instructions_path_for(resource))
-    else
-      respond_with(resource)
-    end
-  end
-    
-      module Controllers
-    autoload :Helpers,        'devise/controllers/helpers'
-    autoload :Rememberable,   'devise/controllers/rememberable'
-    autoload :ScopedViews,    'devise/controllers/scoped_views'
-    autoload :SignInOut,      'devise/controllers/sign_in_out'
-    autoload :StoreLocation,  'devise/controllers/store_location'
-    autoload :UrlHelpers,     'devise/controllers/url_helpers'
-  end
-    
-            # Registers additional pushes to be available.
-        #
-        # @param [String] name Name of the push.
-        # @param [Hash] options List of options for the push.
-        def self.push(name, options=nil, &block)
-          components.pushes.register(name.to_sym) do
-            [block.call, options]
-          end
-    
-          return nil if parent.nil?
-    
-        # Returns a representation of the query as an array of strings and
-    # potentially {Sass::Script::Tree::Node}s (if there's interpolation in it).
-    # When the interpolation is resolved and the strings are joined together,
-    # this will be the string representation of this query.
-    #
-    # @return [Array<String, Sass::Script::Tree::Node>]
-    def to_a
-      Sass::Util.intersperse(queries.map {|q| q.to_a}, ', ').flatten
-    end
-    
-      context 'called with null values' do
-    it 'writes rules for other three' do
-      ruleset = 'margin-top: 11px; ' +
-                'margin-right: 12px; ' +
-                'margin-left: 13px;'
-      bad_rule = 'margin-bottom: null;'
-    
-          expect('.position-explicit').to have_ruleset(ruleset)
-    end
-  end
+      context 'called with no prefixes' do
+    it 'outputs the spec' do
+      rule = 'appearance: none;'
