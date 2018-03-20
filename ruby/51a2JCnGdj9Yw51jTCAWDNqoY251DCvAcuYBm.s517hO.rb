08070da14ef84ae56b02bc269b47f336fbd63cc3
SUNsung <weatherius@gmail.com>
2018-03-20 23:43:27 +0200

    Rewrite [51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb] File in: 2018-03-20 23:43:26

--- ruby/51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb
+++ ruby/51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb
@@ -1,48 +1,31 @@
 
         
-              out = checks.send(method)
-      unless out.nil? || out.empty?
-        if first_warning
-          $stderr.puts <<-EOS.undent
-            #{Tty.white}Please note that these warnings are just used to help the Homebrew maintainers
-            with debugging if you file an issue. If everything you use Homebrew for is
-            working fine: please don't worry and just ignore them. Thanks!#{Tty.reset}
-          EOS
-        end
-    
-          export JAVA_HOME='$(/usr/libexec/java_home)'
-      export AWS_ACCESS_KEY='<Your AWS Access ID>'
-      export AWS_SECRET_KEY='<Your AWS Secret Key>'
-      export #{home_name}='#{home_value}'
-    EOS
+            def to_json
+      JSON.generate(as_json)
+    end
   end
 end
 
     
-          opts.on('-F', '--from FORMAT',
-        'The format to convert from. Can be css, scss, sass.',
-        'By default, this is inferred from the input filename.',
-        'If there is none, defaults to css.') do |name|
-        @options[:from] = name.downcase.to_sym
-        raise 'sass-convert no longer supports LessCSS.' if @options[:from] == :less
-        unless [:css, :scss, :sass].include?(@options[:from])
-          raise 'Unknown format for sass-convert --from: #{name}'
+        def self.join(*args)
+      PARSER.join(*args)
+    end
+    
+          def get_type
+        case slug
+        when 'api'
+          'Reference'
+        when 'configuration'
+          'Reference: Configuration'
+        when 'stpl'
+          'Reference: SimpleTemplate'
+        when 'plugindev'
+          'Reference: Plugin'
+        else
+          'Manual'
         end
       end
     
-        @inner.log_level = log_level
-    Sass.logger = @inner
-  end
-    
-        def parse_input(environment, text)
-      case text
-      when Script::MATCH
-        name = $1
-        guarded = !!$3
-        val = Script::Parser.parse($2, @line, text.size - ($3 || '').size - $2.size)
-    
-            def show
-          authorize! :read, @order, order_token
-          @address = find_address
-          respond_with(@address)
-        end
+    ###
+### dependencies
+###
