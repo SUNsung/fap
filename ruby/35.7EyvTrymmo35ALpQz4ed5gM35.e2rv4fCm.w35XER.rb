8e32a0e8daeb92b1aac03c370fc62c5f0a564225
SUNsung <weatherius@gmail.com>
2018-03-20 21:25:27 +0200

    Rewrite [35.7EyvTrymmo35ALpQz4ed5gM35.e2rv4fCm.w35XER.rb] File in: 2018-03-20 21:25:26

--- ruby/35.7EyvTrymmo35ALpQz4ed5gM35.e2rv4fCm.w35XER.rb
+++ ruby/35.7EyvTrymmo35ALpQz4ed5gM35.e2rv4fCm.w35XER.rb
@@ -1,84 +1,50 @@
 
         
-            def to_json
-      JSON.generate(as_json)
+            def initialize(options = {})
+      @id = options[:id]
+      @agent = options[:agent]
+      @config = options[:config]
+      @restarting = false
     end
     
-        def to_json
-      JSON.generate(as_json)
+      def table_sort
+    raise('You must call set_table_sort in any action using table_sort.') unless @table_sort_info.present?
+    @table_sort_info[:order]
+  end
+    
+        # Returns the scaling and cropping geometries (in string-based ImageMagick format)
+    # neccessary to transform this Geometry into the Geometry given. If crop is true,
+    # then it is assumed the destination Geometry will be the exact final resolution.
+    # In this case, the source Geometry is scaled so that an image containing the
+    # destination Geometry would be completely filled by the source image, and any
+    # overhanging image would be cropped. Useful for square thumbnail images. The cropping
+    # is weighted at the center of the Geometry.
+    def transformation_to dst, crop = false
+      if crop
+        ratio = Geometry.new( dst.width / self.width, dst.height / self.height )
+        scale_geometry, scale = scaling(dst, ratio)
+        crop_geometry         = cropping(dst, ratio, scale)
+      else
+        scale_geometry        = dst.to_s
+      end
+    
+        # Returns the pluralized form of the attachment name. e.g.
+    # 'avatars' for an attachment of :avatar
+    def attachment attachment, style_name
+      plural_cache.pluralize_symbol(attachment.name)
+    end
+    
+    module Paperclip
+  class << self
+    attr_writer :registered_attachments_styles_path
+    def registered_attachments_styles_path
+      @registered_attachments_styles_path ||= Rails.root.join('public/system/paperclip_attachments.yml').to_s
     end
   end
-end
-
     
-        def join(*args)
-      self.class.join self, *args
-    end
-    
-            css('.note h3', '.warning h3').each do |node|
-          node.before('<p><strong>#{node.inner_html}</strong></p>').remove
+            attachment_names.each do |attachment_name|
+          COLUMNS.keys.each do |column_name|
+            remove_column(table_name, '#{attachment_name}_#{column_name}')
+          end
         end
-    
-          # This inserts a block with the given key and value.
-      #
-      # @param [String] key
-      # @param [String] value
-      def insert(key, value)
-        # Insert the new block into the value
-        new_block = <<BLOCK
-# VAGRANT-BEGIN: #{key}
-#{value.strip}
-# VAGRANT-END: #{key}
-BLOCK
-    
-    def process_args
-  if ARGV.first =~ %r{^-+h(?:elp)?$}
-    puts usage
-    exit 0
-  elsif ARGV.length == 1
-    $app_path = ARGV.first
-  else
-    puts usage
-    exit 1
-  end
-end
-    
-      class << self
-    # Our ID will be composed of the following:
-    # 6 bytes (48 bits) of millisecond-level timestamp
-    # 2 bytes (16 bits) of sequence data
-    #
-    # The 'sequence data' is intended to be unique within a
-    # given millisecond, yet obscure the 'serial number' of
-    # this row.
-    #
-    # To do this, we hash the following data:
-    # * Table name (if provided, skipped if not)
-    # * Secret salt (should not be guessable)
-    # * Timestamp (again, millisecond-level granularity)
-    #
-    # We then take the first two bytes of that value, and add
-    # the lowest two bytes of the table ID sequence number
-    # (`table_name`_id_seq). This means that even if we insert
-    # two rows at the same millisecond, they will have
-    # distinct 'sequence data' portions.
-    #
-    # If this happens, and an attacker can see both such IDs,
-    # they can determine which of the two entries was inserted
-    # first, but not the total number of entries in the table
-    # (even mod 2**16).
-    #
-    # The table name is included in the hash to ensure that
-    # different tables derive separate sequence bases so rows
-    # inserted in the same millisecond in different tables do
-    # not reveal the table ID sequence number for one another.
-    #
-    # The secret salt is included in the hash to ensure that
-    # external users cannot derive the sequence base given the
-    # timestamp and table name, which would allow them to
-    # compute the table ID sequence number.
-    def define_timestamp_id
-      return if already_defined?
-    
-      yield
-end
+      end
