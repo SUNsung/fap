919eaba5d3e8621616b24080a5446dff54654955
SUNsung <weatherius@gmail.com>
2018-03-20 22:37:32 +0200

    Rewrite [252SLGQIo84jk25XjyVCJrWJdo25qlscaAdyTc.25Plb.py] File in: 2018-03-20 22:37:32

--- pyton/252SLGQIo84jk25XjyVCJrWJdo25qlscaAdyTc.25Plb.py
+++ pyton/252SLGQIo84jk25XjyVCJrWJdo25qlscaAdyTc.25Plb.py
@@ -1,43 +1,84 @@
 
         
-            def __init__(self, HierachicalStateMachine):
-        self._hsm = HierachicalStateMachine
+        
+def register_and_login(client, username, password):
+    '''Registers and logs in in one go'''
+    register(client, username, password)
+    return login(client, username, password)
     
-        def test_sales_manager_shall_talk_through_proxy_with_delay(cls):
-        cls.p.busy = 'No'
-        start_time = time()
-        cls.p.talk()
-        end_time = time()
-        execution_time = end_time - start_time
-        print_output = cls.output.getvalue()
-        expected_print_output = 'Proxy checking for Sales Manager availability\n\
-Sales Manager ready to talk\n'
-        cls.assertEqual(print_output, expected_print_output)
-        expected_execution_time = 1
-        cls.assertEqual(int(execution_time*10), expected_execution_time)
+        def get_test_cluster_obj(self):
+        return DruidCluster(
+            cluster_name='test_cluster',
+            coordinator_host='localhost',
+            coordinator_endpoint='druid/coordinator/v1/metadata',
+            coordinator_port=7979,
+            broker_host='localhost',
+            broker_port=7980,
+            metadata_last_refreshed=datetime.now())
+    
+            # If using Hive, this will set hive.server2.proxy.user=$effective_username
+        configuration = {}
+        configuration.update(
+            self.db_engine_spec.get_configuration_for_impersonation(
+                str(url),
+                self.impersonate_user,
+                effective_username))
+        if configuration:
+            params['connect_args'] = {'configuration': configuration}
+    
+    import textwrap
+    
+    FORM_DATA_KEY_WHITELIST = list(frontend_config.get('controls').keys()) + ['slice_id']
+    
+        def run(self):
+        if not self.future.set_running_or_notify_cancel():
+            return
+    
+        # Parse and validate the field names.  Validation serves two purposes,
+    # generating informative error messages and preventing template injection attacks.
+    if isinstance(field_names, basestring):
+        field_names = field_names.replace(',', ' ').split() # names separated by whitespace and/or commas
+    field_names = tuple(map(str, field_names))
+    for name in (typename,) + field_names:
+        if not all(c.isalnum() or c=='_' for c in name):
+            raise ValueError('Type names and field names can only contain alphanumeric characters and underscores: %r' % name)
+        if _iskeyword(name):
+            raise ValueError('Type names and field names cannot be a keyword: %r' % name)
+        if name[0].isdigit():
+            raise ValueError('Type names and field names cannot start with a number: %r' % name)
+    seen_names = set()
+    for name in field_names:
+        if name.startswith('_'):
+            raise ValueError('Field names cannot start with an underscore: %r' % name)
+        if name in seen_names:
+            raise ValueError('Encountered duplicate field name: %r' % name)
+        seen_names.add(name)
+    
+    # Workers are created as daemon threads. This is done to allow the interpreter
+# to exit when there are still idle threads in a ThreadPoolExecutor's thread
+# pool (i.e. shutdown() was not called). However, allowing workers to die with
+# the interpreter has two undesirable properties:
+#   - The workers would still be running during interpretor shutdown,
+#     meaning that they would fail in unpredictable ways.
+#   - The workers could be killed while evaluating a work item, which could
+#     be bad if the callable being evaluated has external side-effects e.g.
+#     writing to a file.
+#
+# To work around this problem, an exit handler is installed which tells the
+# workers to exit when their work queues are empty and then waits until the
+# threads finish.
+    
+    def with_process_pool_executor():
+    with ProcessPoolExecutor(10) as executor:
+        return list(executor.map(is_prime, PRIMES))
     
     
-class BaseRegisteredClass(object):
-    __metaclass__ = RegistryHolder
-    '''
-        Any class that will inherits from BaseRegisteredClass will be included
-        inside the dict RegistryHolder.REGISTRY, the key being the name of the
-        class and the associated value, the class itself.
-    '''
-    pass
+  def Start( self ):
+    request_data = BuildRequestData()
+    request_data.update( { 'filetypes': self.filetypes } )
+    with HandleServerException():
+      self._response = self.PostDataToHandler( request_data,
+                                               'semantic_completion_available' )
     
-        def test_cat_eng_localization(self):
-        self.assertEqual(self.e.get('cat'), 'cat')
     
-    from dft.setter_injection import TimeDisplay, MidnightTimeProvider, ProductionCodeTimeProvider, datetime
-    
-    '''
-Port of the Java example of 'Constructor Injection' in
-'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
-(ISBN-10: 0131495054, ISBN-13: 978-0131495050)
-    
-        def __init__(self):
-        self._observers = []
-    
-        def is_satisfied_by(self, candidate):
-        return bool(not self._wrapped.is_satisfied_by(candidate))
+def CommaSeparatedFiletypes_test():
