8857333325193760f5cedf6a32d4e43aeb2d8689
SUNsung <weatherius@gmail.com>
2018-03-20 16:46:40 +0200

    Rewrite [index.cpp] File in: 2018-03-20 16:46:40

--- cccccc/index.cpp
+++ cccccc/index.cpp
@@ -1,297 +1,109 @@
 
         
-        void CodeCompletionOrganizer::Impl::addCompletionsWithFilter(
-    ArrayRef<Completion *> completions, StringRef filterText, Options options,
-    const FilterRules &rules, Completion *&exactMatch) {
-  assert(rootGroup);
+        namespace api {
     }
     
-    #include 'llvm/ADT/SetVector.h'
+    #endif  // ATOM_BROWSER_ATOM_QUOTA_PERMISSION_CONTEXT_H_
+
     
-    
-    {/// Extract comments parts from the given Markup node.
-swift::markup::CommentParts
-extractCommentParts(swift::markup::MarkupContext &MC,
-                    swift::markup::MarkupASTNode *Node);
-} // namespace swift
-    
-    /// A SyntaxRewriter for applying a set of formatting rules to a Syntax tree.
-struct FormatSyntaxRewriter : public SyntaxRewriter {
-  virtual StructDeclSyntax
-  rewriteStructDecl(StructDeclSyntax Struct) override;
-};
-    
-      /// Indicates whether the diagnostics produced during compilation should be
-  /// checked against expected diagnostics, indicated by markers in the
-  /// input source file.
-  enum {
-    NoVerify,
-    Verify,
-    VerifyAndApplyFixes
-  } VerifyMode = NoVerify;
-    
-      /// Returns the identifier for the buffer with the given ID.
-  ///
-  /// \p BufferID must be a valid buffer ID.
-  StringRef getIdentifierForBuffer(unsigned BufferID) const;
-    
-    // Free allocated memory and clear pointers.
-void DENORM::Clear() {
-  if (x_map_ != NULL) {
-    delete x_map_;
-    x_map_ = NULL;
-  }
-  if (y_map_ != NULL) {
-    delete y_map_;
-    y_map_ = NULL;
-  }
-  if (rotation_ != NULL) {
-    delete rotation_;
-    rotation_ = NULL;
-  }
+    net::URLRequestJob* AsarProtocolHandler::MaybeCreateJob(
+    net::URLRequest* request,
+    net::NetworkDelegate* network_delegate) const {
+  base::FilePath full_path;
+  net::FileURLToFilePath(request->url(), &full_path);
+  auto* job = new URLRequestAsarJob(request, network_delegate);
+  job->Initialize(file_task_runner_, full_path);
+  return job;
 }
     
-    /**----------------------------------------------------------------------------
-          Include Files and Type Defines
-----------------------------------------------------------------------------**/
-#include 'strngs.h'
+      // net::URLRequestJobFactory::ProtocolHandler:
+  net::URLRequestJob* MaybeCreateJob(
+      net::URLRequest* request,
+      net::NetworkDelegate* network_delegate) const override;
+  bool IsSafeRedirectTarget(const GURL& location) const override;
     
-    // Tests a pair of classifiers, debugging errors of the new against the old.
-// See errorcounter.h for description of arguments.
-// Iterates over the samples, calling the classifiers in normal/silent mode.
-// If the new_classifier makes a boosting_mode error that the old_classifier
-// does not, it will then call the new_classifier again with a debug flag
-// and a keep_this argument to find out what is going on.
-void ErrorCounter::DebugNewErrors(
-    ShapeClassifier* new_classifier, ShapeClassifier* old_classifier,
-    CountTypes boosting_mode,
-    const FontInfoTable& fontinfo_table,
-    const GenericVector<Pix*>& page_images, SampleIterator* it) {
-  int fontsize = it->sample_set()->NumFonts();
-  ErrorCounter old_counter(old_classifier->GetUnicharset(), fontsize);
-  ErrorCounter new_counter(new_classifier->GetUnicharset(), fontsize);
-  GenericVector<UnicharRating> results;
+    namespace atom {
     }
     
-      // Creates a report of the error rate. The report_level controls the detail
-  // that is reported to stderr via tprintf:
-  // 0   -> no output.
-  // >=1 -> bottom-line error rate.
-  // >=3 -> font-level error rate.
-  // boosting_mode determines the return value. It selects which (un-weighted)
-  // error rate to return.
-  // The fontinfo_table from MasterTrainer provides the names of fonts.
-  // The it determines the current subset of the training samples.
-  // If not NULL, the top-choice unichar error rate is saved in unichar_error.
-  // If not NULL, the report string is saved in fonts_report.
-  // (Ignoring report_level).
-  double ReportErrors(int report_level, CountTypes boosting_mode,
-                      const FontInfoTable& fontinfo_table,
-                      const SampleIterator& it,
-                      double* unichar_error,
-                      STRING* fonts_report);
     
-    void FreeOutlines(LIST Outlines);
+    {}  // namespace atom
     
-    // same but for event
-void GPUDataTransferer::SyncEvent(cudaEvent_t ev)
-{
-    auto rc = cudaEventQuery(ev);
-    if (rc != cudaErrorNotReady)
-    {
-        // if Event is ready then no need to wait
-        rc || 'cudaEventQuery failed';
-        return;
-    }
-    // we must wait
-    cudaEventSynchronize(ev) || 'cudaEventSynchronize failed';
-}
+     private:
+  // content::NotificationObserver:
+  void Observe(int type,
+               const content::NotificationSource& source,
+               const content::NotificationDetails& details) override;
     
-                if (!wasDataRead) LogicError('DataRead Failure in batch normalization statistics');
-    
-    using namespace std;
-using namespace Microsoft::MSR::ScriptableObjects;
-    
-    
-    {        // compute global progress
-        bool needToPrint = us.m_progressTracingTimer.ElapsedSeconds() > 10;
-        if (needToPrint || isTimerPaced)
-        {
-            double epochProg = ((100.0f * (double) (us.m_currentStepOffset + epochNumber)) / (double) us.m_totalNumberOfSteps);
-            mbProg = (mbProg * 100.0f) / (double) us.m_totalNumberOfSteps;
-            printf('PROGRESS: %.2f%%\n', epochProg + mbProg);
-            us.m_progressTracingTimer.Restart();
-        }
-        return needToPrint;
-    }
-    
-        void getlattices(const std::wstring& key, std::shared_ptr<const latticepair>& L, size_t expectedframes) const
-    {
-        std::shared_ptr<latticepair> LP(new latticepair);
-        denlattices.getlattice(key, LP->second, expectedframes); // this loads the lattice from disk, using the existing L.second object
-        L = LP;
-    }
-    
-    template <class _T, int _N>
-class hardcoded_array
-{
-    _T data[_N];
-    inline void check_index(size_t i) const
-    {
-        i;
-        assert(i < _N);
-    }
-    inline void check_size(size_t n) const
-    {
-        n;
-        assert(n == _N);
-    }
-    }
-    
-    // ===================================================================
-// behave like a config
-// This allows to access nodes inside a network as if it was an IConfigRecord.
-// This is meant to be used by whatever we will replace MEL.
-// ===================================================================
-    
-        cobj = (CocosDenshion::SimpleAudioEngine*)tolua_tousertype(tolua_S,1,0);
-    
-    
-    
-        void initShader( void );
-public:
-    GLESDebugDraw();
-    
-    
-    {
-    {			float minX = -6.0f;
-			float maxX = 0.0f;
-			float minY = 4.0f;
-			float maxY = 6.0f;
-			
-			for (int32 i = 0; i < 400; ++i)
-			{
-				b2BodyDef bd;
-				bd.type = b2_dynamicBody;
-				bd.position = b2Vec2(RandomFloat(minX,maxX),RandomFloat(minY,maxY));
-				b2Body* body = m_world->CreateBody(&bd);
-				body->CreateFixture(&shape, 0.01f);
-			}
-		}
-		
-		{
-			b2PolygonShape shape;
-			shape.SetAsBox(1.5f, 1.5f);
-			b2BodyDef bd;
-			bd.type = b2_dynamicBody;
-			bd.position.Set(-40.0f,5.0f);
-			bd.bullet = true;
-			b2Body* body = m_world->CreateBody(&bd);
-			body->CreateFixture(&shape, 1.0f);
-			body->SetLinearVelocity(b2Vec2(150.0f, 0.0f));
-		}
-	}
-    
-    
-    {			bd.position.Set(0.0f, 2.0);
-			bd.angle = b2_pi;
-			bd.allowSleep = false;
-			m_body = m_world->CreateBody(&bd);
-			m_body->CreateFixture(&sd1);
-			m_body->CreateFixture(&sd2);
-		}
-    
-    
-    {			b2PolygonShape shape;
-			shape.SetAsBox(0.5f, 2.0f);
-			m_attachment->CreateFixture(&shape, 2.0f);
-		}
-    
-    		b2Body* body2 = m_world->CreateBody(&bd);
-		m_piece2 = body2->CreateFixture(&m_shape2, 1.0f);
-    
-    			bd.position.Set(-1.0f, 0.35f);
-			m_wheel1 = m_world->CreateBody(&bd);
-			m_wheel1->CreateFixture(&fd);
-    
-    class InputCorpus {
+    class MenuModelAdapter : public views::MenuModelAdapter {
  public:
-  static const size_t kFeatureSetSize = 1 << 16;
-  InputCorpus(const std::string &OutputCorpus) : OutputCorpus(OutputCorpus) {
-    memset(InputSizesPerFeature, 0, sizeof(InputSizesPerFeature));
-    memset(SmallestElementPerFeature, 0, sizeof(SmallestElementPerFeature));
-  }
-  ~InputCorpus() {
-    for (auto II : Inputs)
-      delete II;
-  }
-  size_t size() const { return Inputs.size(); }
-  size_t SizeInBytes() const {
-    size_t Res = 0;
-    for (auto II : Inputs)
-      Res += II->U.size();
-    return Res;
-  }
-  size_t NumActiveUnits() const {
-    size_t Res = 0;
-    for (auto II : Inputs)
-      Res += !II->U.empty();
-    return Res;
-  }
-  bool empty() const { return Inputs.empty(); }
-  const Unit &operator[] (size_t Idx) const { return Inputs[Idx]->U; }
-  void AddToCorpus(const Unit &U, size_t NumFeatures, bool MayDeleteFile = false) {
-    assert(!U.empty());
-    uint8_t Hash[kSHA1NumBytes];
-    if (FeatureDebug)
-      Printf('ADD_TO_CORPUS %zd NF %zd\n', Inputs.size(), NumFeatures);
-    ComputeSHA1(U.data(), U.size(), Hash);
-    Hashes.insert(Sha1ToString(Hash));
-    Inputs.push_back(new InputInfo());
-    InputInfo &II = *Inputs.back();
-    II.U = U;
-    II.NumFeatures = NumFeatures;
-    II.MayDeleteFile = MayDeleteFile;
-    memcpy(II.Sha1, Hash, kSHA1NumBytes);
-    UpdateCorpusDistribution();
-    ValidateFeatureSet();
-  }
+  explicit MenuModelAdapter(AtomMenuModel* menu_model);
+  virtual ~MenuModelAdapter();
     }
     
-    template <typename T>
-static T GetFnPtr(const char *FnName, bool WarnIfMissing) {
-  dlerror(); // Clear any previous errors.
-  void *Fn = dlsym(RTLD_DEFAULT, FnName);
-  if (Fn == nullptr) {
-    if (WarnIfMissing) {
-      const char *ErrorMsg = dlerror();
-      Printf('WARNING: Failed to find function \'%s\'.', FnName);
-      if (ErrorMsg)
-        Printf(' Reason %s.', ErrorMsg);
-      Printf('\n');
+    namespace atom {
     }
+    
+    	wchar_t message[1024];
+	swprintf_s(message, L'%s\nFunction: %s\nLine: %d', buffer, func, line);
+	LocalFree(buffer);
+    
+    #endif // BOOST_ASIO_BUFFERED_STREAM_FWD_HPP
+
+    
+    template<class T, std::size_t N>
+class array;
+    
+    #if !defined(BOOST_ASIO_HAS_THREADS)
+// Nothing to include.
+#elif defined(BOOST_ASIO_HAS_STD_ATOMIC)
+# include <atomic>
+#else // defined(BOOST_ASIO_HAS_STD_ATOMIC)
+# include <boost/detail/atomic_count.hpp>
+#endif // defined(BOOST_ASIO_HAS_STD_ATOMIC)
+    
+      descriptor_read_op(int descriptor,
+      const MutableBufferSequence& buffers, Handler& handler)
+    : descriptor_read_op_base<MutableBufferSequence>(
+        descriptor, buffers, &descriptor_read_op::do_complete),
+      handler_(BOOST_ASIO_MOVE_CAST(Handler)(handler))
+  {
   }
-  return reinterpret_cast<T>(Fn);
+    
+    #ifndef BOOST_ASIO_DETAIL_FUNCTION_HPP
+#define BOOST_ASIO_DETAIL_FUNCTION_HPP
+    
+    template <typename Function, typename Context>
+inline void invoke(const Function& function, Context& context)
+{
+#if !defined(BOOST_ASIO_HAS_HANDLER_HOOKS)
+  Function tmp(function);
+  tmp();
+#else
+  using boost::asio::asio_handler_invoke;
+  asio_handler_invoke(function, boost::asio::detail::addressof(context));
+#endif
 }
     
-    #include 'FuzzerExtFunctions.h'
-#include 'FuzzerIO.h'
+    #define BOOST_ASIO_SIGNAL_HANDLER_CHECK( \
+    handler_type, handler) \
+  typedef int BOOST_ASIO_UNUSED_TYPEDEF
     
+    #if defined(BOOST_ASIO_HAS_EPOLL)
     
-    {}  // namespace fuzzer
+    template<typename... ARGS>
+inline void logi(const char* tag, const char* msg, ARGS... args) noexcept {
+  log(ANDROID_LOG_INFO, tag, msg, args...);
+}
     
-      DWORD LastError = GetLastError();
-  if (LastError != ERROR_NO_MORE_FILES)
-    Printf('FindNextFileA failed (Error code: %lu).\n', LastError);
+    #pragma once
+#include <cstring>
+#include <string>
+#include <sstream>
     
-    namespace fuzzer {
-    }
-    
-    namespace fuzzer {
-    }
-    
-    #include 'FuzzerIO.h'
-#include <mutex>
-#include <signal.h>
-#include <spawn.h>
-#include <sys/wait.h>
+      template <typename U>
+  RefPtr<T>& operator=(RefPtr<U>&& ref) {
+    unrefIfNecessary(m_ptr);
+    m_ptr = ref.m_ptr;
+    ref.m_ptr = nullptr;
+    return *this;
+  }
