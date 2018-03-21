e53de56ed92437b66e4f1e9d063b10d8034f4c93
SUNsung <weatherius@gmail.com>
2018-03-21 21:41:50 +0200

    Rewrite [36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py] File in: 2018-03-21 21:41:49

--- pyton/36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py
+++ pyton/36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py
@@ -1,89 +1,161 @@
 
         
-        def main():
-    for name, fn in [('sequential', sequential),
-                     ('processes', with_process_pool_executor),
-                     ('threads', with_thread_pool_executor)]:
-        sys.stdout.write('%s: ' % name.ljust(12))
-        start = time.time()
-        if fn() != [True] * len(PRIMES):
-            sys.stdout.write('failed\n')
-        else:
-            sys.stdout.write('%.2f seconds\n' % (time.time() - start))
+            @property
+    def body(self):
+        # Only now the response body is fetched.
+        # Shouldn't be touched unless the body is actually needed.
+        return self._orig.content
     
-                f = Future()
-            f.add_done_callback(raising_fn)
-            f.add_done_callback(fn)
-            f.set_result(5)
-            self.assertTrue(raising_was_called)
-            self.assertTrue(fn_was_called)
-            self.assertIn('Exception: doh!', stderr.getvalue())
+            '''
+        available_plugins = plugin_manager.get_formatters_grouped()
+        self.enabled_plugins = []
+        for group in groups:
+            for cls in available_plugins[group]:
+                p = cls(env=env, **kwargs)
+                if p.enabled:
+                    self.enabled_plugins.append(p)
     
-        return self._cached_response if self._cached_response else []
+        '''
+    exit_status = ExitStatus.OK
+    downloader = None
+    show_traceback = args.debug or args.traceback
     
-    if __name__ == '__main__':
-  Main()
+            $ http :3000                    # => http://localhost:3000
+        $ http :/foo                    # => http://localhost/foo
+    
+        Assumes `from __future__ import division`.
+    
+      Returns:
+    SequenceWrapper with `seq` inputs copied and concatenated, and with labels
+    copied in on the right-hand (i.e. decoder) side with weights set to 1.0.
+    The new sequence will have length `len(seq) * 2 - 1`, as the last timestep
+    of the encoder section and the first step of the decoder section will
+    overlap.
+  '''
+  seq_ae_seq = SequenceWrapper()
+    
+      @tf.test.mock.patch.dict(premade_estimator.__dict__,
+                           {'load_data': four_lines_data})
+  def test_premade_estimator(self):
+    premade_estimator.main([None, '--train_steps=1'])
+    
+      Args:
+    sess: Session.
+    saver: Saver for restoring the checkpoint.
+    
+      Raises:
+    ValueError: if an invalid combination of arguments is provided that does not
+      map to any data files (e.g. pretrain=False, use_seq2seq=True).
+  '''
+  data_spec = (phase, bidir, pretrain, use_seq2seq)
+  data_specs = {
+      ('train', True, True, False): (data_utils.TRAIN_LM,
+                                     data_utils.TRAIN_REV_LM),
+      ('train', True, False, False): (data_utils.TRAIN_BD_CLASS,),
+      ('train', False, True, False): (data_utils.TRAIN_LM,),
+      ('train', False, True, True): (data_utils.TRAIN_SA,),
+      ('train', False, False, False): (data_utils.TRAIN_CLASS,),
+      ('test', True, True, False): (data_utils.TEST_LM,
+                                    data_utils.TRAIN_REV_LM),
+      ('test', True, False, False): (data_utils.TEST_BD_CLASS,),
+      ('test', False, True, False): (data_utils.TEST_LM,),
+      ('test', False, True, True): (data_utils.TEST_SA,),
+      ('test', False, False, False): (data_utils.TEST_CLASS,),
+      ('valid', True, False, False): (data_utils.VALID_BD_CLASS,),
+      ('valid', False, False, False): (data_utils.VALID_CLASS,),
+  }
+  if data_spec not in data_specs:
+    raise ValueError(
+        'Data specification (phase, bidir, pretrain, use_seq2seq) %s not '
+        'supported' % str(data_spec))
+    
+    
+def print_list():
+    data = get_landscape_members_data()
+    nodes = get_nodes(data)
+    groups = get_groups(data)
+    meta = get_meta(data)
+    inventory_data = {_key: nodes, '_meta': meta}
+    inventory_data.update(groups)
+    print(json.dumps(inventory_data))
+    
+        'ipv6': re.compile(
+        r'''^
+            (?:{0}:){{7}}{0}|           # uncompressed: 1:2:3:4:5:6:7:8
+            (?:{0}:){{1,6}}:|           # compressed variants, which are all
+            (?:{0}:)(?::{0}){{1,6}}|    # a::b for various lengths of a,b
+            (?:{0}:){{2}}(?::{0}){{1,5}}|
+            (?:{0}:){{3}}(?::{0}){{1,4}}|
+            (?:{0}:){{4}}(?::{0}){{1,3}}|
+            (?:{0}:){{5}}(?::{0}){{1,2}}|
+            (?:{0}:){{6}}(?::{0})|      # ...all with 2 <= a+b <= 7
+            :(?::{0}){{1,6}}|           # ::ffff(:ffff...)
+            {0}?::|                     # ffff::, ::
+                                        # ipv4-in-ipv6 variants
+            (?:0:){{6}}(?:{0}\.){{3}}{0}|
+            ::(?:ffff:)?(?:{0}\.){{3}}{0}|
+            (?:0:){{5}}ffff:(?:{0}\.){{3}}{0}
+            $
+        '''.format(ipv6_component), re.X | re.I
+    ),
+    
+    try:
+    import json
+except ImportError:
+    import simplejson as json
+    
+    from ansible.plugins.terminal import TerminalBase
+from ansible.errors import AnsibleConnectionFailure
+    
+            (2016-01, shopping), 25
+        (2016-01, shopping), 100
+        (2016-01, gas), 50
+        '''
+        timestamp, seller, amount = line.split('\t')
+        period = self. extract_year_month(timestamp)
+        if period == self.current_year_month():
+            yield (period, category), amount
+    
+        def mapper(self, _, line):
+        '''Parse each log line, extract and transform relevant lines.
+    
+        def escalate_call(self):
+        self.call.level = Rank.DIRECTOR
+        self._escalate_call()
+    
+        def __init__(self, message_id, message, timestamp):
+        self.message_id = message_id
+        self.message = message
+        self.timestamp = timestamp
+    
+        def __init__(self, vehicle_size, license_plate, spot_size):
+        self.vehicle_size = vehicle_size
+        self.license_plate = license_plate
+        self.spot_size
+        self.spots_taken = []
+    
+        def add_link_to_crawl(self, url):
+        '''Add the given link to `links_to_crawl`.'''
+        pass
+    
+                elif opt in ('-l', '--playlist', '--playlists'):
+                # Download playlist whenever possible.
+                conf['playlist'] = True
+    
+    site_info = 'CBS.com'
+download = cbs_download
+download_playlist = playlist_not_supported('cbs')
 
     
+    from ..common import *
     
-class _MasterDiagnosticFilter( object ):
-    
-    
-@wsgi_safe
-class StatusReasonTest(SimpleHandlerTestCase):
-    class Handler(RequestHandler):
-        def get(self):
-            reason = self.request.arguments.get('reason', [])
-            self.set_status(int(self.get_argument('code')),
-                            reason=reason[0] if reason else None)
-    
-    
-class Configurable(object):
-    '''Base class for configurable interfaces.
-    
-            p = Popen(
-            [sys.executable, '-m', 'testapp'], stdout=subprocess.PIPE,
-            cwd=path, env=dict(os.environ, PYTHONPATH=pythonpath),
-            universal_newlines=True)
-        out = p.communicate()[0]
-        self.assertEqual(out, 'Starting\nStarting\n')
-
-    
-        @administrator
-    def post(self):
-        key = self.get_argument('key', None)
-        if key:
-            entry = Entry.get(key)
-            entry.title = self.get_argument('title')
-            entry.body_source = self.get_argument('body_source')
-            entry.html = tornado.escape.linkify(
-                self.get_argument('body_source'))
-        else:
-            title = self.get_argument('title')
-            slug = unicodedata.normalize('NFKD', title).encode(
-                'ascii', 'ignore')
-            slug = re.sub(r'[^\w]+', ' ', slug)
-            slug = '-'.join(slug.lower().strip().split())
-            if not slug:
-                slug = 'entry'
-            while True:
-                existing = db.Query(Entry).filter('slug =', slug).get()
-                if not existing or str(existing.key()) == key:
-                    break
-                slug += '-2'
-            entry = Entry(
-                author=self.current_user,
-                title=title,
-                slug=slug,
-                body_source=self.get_argument('body_source'),
-                html=tornado.escape.linkify(self.get_argument('body_source')),
-            )
-        entry.put()
-        self.redirect('/entry/' + entry.slug)
-    
-        def data_received(self, chunk):
-        self.bytes_read += len(chunk)
-    
-    
-class BaseRequestHandler(web.RequestHandler):
-    SUPPORTED_METHODS = ('PUT', 'GET', 'DELETE')
+    def kuwo_download_by_rid(rid, output_dir = '.', merge = True, info_only = False):
+    html=get_content('http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_%s'%rid)
+    title=match1(html,r'<name>(.*)</name>')
+    #to get title
+    #format =aac|mp3 ->to get aac format=mp3 ->to get mp3
+    url=get_content('http://antiserver.kuwo.cn/anti.s?format=mp3&rid=MUSIC_%s&type=convert_url&response=url'%rid)
+    songtype, ext, size = url_info(url)
+    print_info(site_info, title, songtype, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir)
