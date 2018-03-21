c6ebca64d8cdc5a203fad76122e4f4d3c6f2c9e4
SUNsung <weatherius@gmail.com>
2018-03-21 17:57:46 +0200

    Rewrite [20zhBpI5yonk20GGloQuxhaq.20qVFI5PSZr5o20umM.rb] File in: 2018-03-21 17:57:45

--- ruby/20zhBpI5yonk20GGloQuxhaq.20qVFI5PSZr5o20umM.rb
+++ ruby/20zhBpI5yonk20GGloQuxhaq.20qVFI5PSZr5o20umM.rb
@@ -1,22 +1,124 @@
-    can?(current_user, ability_name, resource)
-  end
+
+        
+        #
     
-        find_union(segments, Project).includes(:namespace).order_id_desc
-  end
-    
-    def ok_failed(condition)
-  if (condition)
-    puts 'OK'
-  else
-    puts 'FAILED'
+    If you run into trouble, you can find helpful resources at https://jekyllrb.com/help/!
+            MSG
+            raise Jekyll::Errors::MissingDependencyException, name
+          end
+        end
+      end
+    end
   end
 end
+
     
-    class SinatraStaticServer < Sinatra::Base
+            self
+      end
     
-            def_node_matcher :on_body_of_reduce, <<-PATTERN
-          (block (send _recv {:reduce :inject} !sym) _blockargs $(begin ...))
-        PATTERN
+      def render_response
+    render json: {
+      html: view_to_html_string('shared/notifications/_button', notification_setting: @notification_setting),
+      saved: @saved
+    }
+  end
     
-            MSG = 'Do not freeze immutable objects, as freezing them has no ' \
-              'effect.'.freeze
+      # if rss_url already in existing opml file, use that; otherwise, do a lookup
+  rss_url = nil
+  existing_blog = xml.xpath('//outline[@htmlUrl='#{web_url}']').first if xml
+  if existing_blog
+    rss_url = existing_blog.attr('xmlUrl')
+    puts '#{name}: ALREADY HAVE'
+  end
+    
+        # Returns a deep copy of this query and all its children.
+    #
+    # @return [Query]
+    def deep_copy
+      Query.new(
+        modifier.map {|c| c.is_a?(Sass::Script::Tree::Node) ? c.deep_copy : c},
+        type.map {|c| c.is_a?(Sass::Script::Tree::Node) ? c.deep_copy : c},
+        expressions.map {|e| e.map {|c| c.is_a?(Sass::Script::Tree::Node) ? c.deep_copy : c}})
+    end
+  end
+    
+    @@ chat
+<pre id='chat'></pre>
+<form>
+  <input id='msg' placeholder='type message here...' />
+</form>
+    
+          def instrument(env)
+        return unless i = options[:instrumenter]
+        env['rack.protection.attack'] = self.class.name.split('::').last.downcase
+        i.instrument('rack.protection', env)
+      end
+    
+    module Rack
+  module Protection
+    ##
+    # Prevented attack::   Cookie Tossing
+    # Supported browsers:: all
+    # More infos::         https://github.com/blog/1466-yummy-cookies-across-domains
+    #
+    # Does not accept HTTP requests if the HTTP_COOKIE header contains more than one
+    # session cookie. This does not protect against a cookie overflow attack.
+    #
+    # Options:
+    #
+    # session_key:: The name of the session cookie (default: 'rack.session')
+    class CookieTossing < Base
+      default_reaction :deny
+    
+    module Rack
+  module Protection
+    ##
+    # Prevented attack::   XSS
+    # Supported browsers:: all
+    # More infos::         http://en.wikipedia.org/wiki/Cross-site_scripting
+    #
+    # Automatically escapes Rack::Request#params so they can be embedded in HTML
+    # or JavaScript without any further issues. Calls +html_safe+ on the escaped
+    # strings if defined, to avoid double-escaping in Rails.
+    #
+    # Options:
+    # escape:: What escaping modes to use, should be Symbol or Array of Symbols.
+    #          Available: :html (default), :javascript, :url
+    class EscapedParams < Base
+      extend Rack::Utils
+    
+        it 'redirects requests with duplicate session cookies' do
+      get '/', {}, 'HTTP_COOKIE' => 'rack.session=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
+      expect(last_response).to be_redirect
+      expect(last_response.location).to eq('/')
+    end
+    
+      context 'escaping' do
+    before do
+      mock_app { |e| [200, {'Content-Type' => 'text/plain'}, [e['PATH_INFO']]] }
+    end
+    
+      describe 'new' do
+    it 'should allow disable session protection' do
+      mock_app do
+        use Rack::Protection, :without_session => true
+        run DummyApp
+      end
+    
+      it 'accepts requests with a changing Accept-Encoding header' do
+    # this is tested because previously it led to clearing the session
+    session = {:foo => :bar}
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_ENCODING' => 'a'
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_ENCODING' => 'b'
+    expect(session).not_to be_empty
+  end
+    
+      # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = :random
+    
+      def pretty?
+    params.has_key?('pretty')
+  end
