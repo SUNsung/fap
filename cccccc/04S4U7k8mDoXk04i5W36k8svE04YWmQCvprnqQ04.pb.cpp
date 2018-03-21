feea0b203ba03e76b1d36fcef5155df47cb119a1
SUNsung <weatherius@gmail.com>
2018-03-21 19:11:41 +0200

    Rewrite [04S4U7k8mDoXk04i5W36k8svE04YWmQCvprnqQ04.pb.cpp] File in: 2018-03-21 19:11:40

--- cccccc/04S4U7k8mDoXk04i5W36k8svE04YWmQCvprnqQ04.pb.cpp
+++ cccccc/04S4U7k8mDoXk04i5W36k8svE04YWmQCvprnqQ04.pb.cpp
@@ -1,105 +1,247 @@
-/// External (extensions) SQL implementation plugin provider for 'sql' registry.
-class ExternalSQLPlugin : public SQLPlugin {
- public:
-  Status query(const std::string& query,
-               QueryData& results,
-               bool use_cache = false) const override {
-    return queryExternal(query, results);
+
+        
+        REGISTER_FILE_SYSTEM('test', TestFileSystem);
+    
+    
+    {
+    {  PersistentTensor handle_ GUARDED_BY(mu_);
+};
+}  // namespace tensorflow
+    
+    #ifndef TENSORFLOW_COMPILER_XLA_SERVICE_HLO_CONSTANT_FOLDING_H_
+#define TENSORFLOW_COMPILER_XLA_SERVICE_HLO_CONSTANT_FOLDING_H_
+    
+    
+    {
+    {}  // end namespace grappler
+}  // end namespace tensorflow
+    
+    #endif  // TENSORFLOW_STREAM_EXECUTOR_PLUGIN_H_
+
+    
+      Status ResetLocked() override {
+    offset_ = 0;
+    reader_.reset(nullptr);
+    file_.reset(nullptr);
+    return ReaderBase::ResetLocked();
   }
+    
+    #define REGISTER_GPU(T)                                           \
+  template void DynamicStitchGPUImpl(                             \
+      const Eigen::GpuDevice& gpu_device, const int32 slice_size, \
+      const int32 first_dim_size,                                 \
+      const CudaDeviceArrayStruct<int32>& input_indices,          \
+      const CudaDeviceArrayStruct<const T*>& input_ptrs, T* output);
+    
+        for (IntType i = lidx; i < output_scan.size; i += blockSize) {
+      smem_col_scan[i] = col_scan[i];
     }
     
-    /**
- * @brief Initialize the osquery Logger facility by dumping the buffered status
- * logs and configurating status log forwarding.
- *
- * initLogger will disable the `BufferedLogSink` facility, dump any status logs
- * emitted between process start and this init call, then configure the new
- * logger facility to receive status logs.
- *
- * The `forward_all` control is used when buffering logs in extensions.
- * It is fine if the logger facility in the core app does not want to receive
- * status logs, but this is NOT an option in extensions/modules. All status
- * logs must be forwarded to the core.
- *
- * @param name The process name.
- */
-void initLogger(const std::string& name);
+    struct Options {
+  int num_concurrent_sessions = 1;   // The number of concurrent sessions
+  int num_concurrent_steps = 10;     // The number of concurrent steps
+  int num_iterations = 100;          // Each step repeats this many times
+  bool use_gpu = false;              // Whether to use gpu in the training
+};
     
-     protected:
-  RegistryFactory() = default;
-  virtual ~RegistryFactory() = default;
-    
-      /**
-   * @brief A constructor which can be used to concisely express the status of
-   * an operation.
-   *
-   * @param c a status code. The idiom is that a zero status code indicates a
-   * successful operation and a non-zero status code indicates a failed
-   * operation.
-   * @param m a message indicating some extra detail regarding the operation.
-   * If all operations were successful, this message should be 'OK'.
-   * Otherwise, it doesn't matter what the string is, as long as both the
-   * setter and caller agree.
-   */
-  Status(int c, std::string m) : code_(c), message_(std::move(m)) {}
-    
-    namespace fs = boost::filesystem;
-namespace pt = boost::property_tree;
-    
-    #include <osquery/config.h>
-#include <osquery/filesystem.h>
-#include <osquery/flags.h>
-#include <osquery/logger.h>
-    
-    Status platformStrncpy(char* dst, size_t nelms, const char* src, size_t count) {
-  if (dst == nullptr || src == nullptr || nelms == 0) {
-    return Status(1, 'Failed to strncpy: invalid arguments');
-  }
+    namespace atom {
     }
     
-    std::unique_ptr<AuthConfig>
-AbstractAuthResolver::getUserDefinedAuthConfig() const
-{
-  return AuthConfig::create(userDefinedUser_, userDefinedPassword_);
+    // The argument separating arguments intended for the relauncher process from
+// those intended for the relaunched process. '---' is chosen instead of '--'
+// because CommandLine interprets '--' as meaning 'end of switches', but
+// for many purposes, the relauncher process' CommandLine ought to interpret
+// arguments intended for the relaunched process, to get the correct settings
+// for such things as logging and the user-data-dir in case it affects crash
+// reporting.
+extern const CharType* kRelauncherArgSeparator;
+    
+    void RenderProcessPreferences::UpdateCache() {
+  if (!cache_needs_update_)
+    return;
+    }
+    
+    #ifndef ATOM_BROWSER_UI_ACCELERATOR_UTIL_H_
+#define ATOM_BROWSER_UI_ACCELERATOR_UTIL_H_
+    
+    gfx::Size NativeFrameView::GetMaximumSize() const {
+  return window_->GetMaximumSize();
 }
     
-    bool AbstractHttpServerResponseCommand::execute()
-{
-  if (e_->getRequestGroupMan()->downloadFinished() || e_->isHaltRequested()) {
-    return true;
-  }
-  try {
-    ssize_t len = httpServer_->sendResponse();
-    if (len > 0) {
-      timeoutTimer_ = global::wallclock();
+    #endif
+
+    
+    
+    {
+    {      return shouldStartAtSuper()? ObjCMessageKind::Super
+                                 : ObjCMessageKind::Peer;
     }
+  };
+    
+    #ifndef SWIFT_INDEX_INDEXRECORD_H
+#define SWIFT_INDEX_INDEXRECORD_H
+    
+    
+    {  for (auto &rel : sym.Relations) {
+    if (!rel.group.empty())
+      return rel.group;
+    if (rel.decl)
+      return findGroupNameForDecl(rel.decl);
   }
-  catch (RecoverableException& e) {
-    A2_LOG_INFO_EX(fmt('CUID#%' PRId64
-                       ' - Error occurred while transmitting response body.',
-                       getCuid()),
-                   e);
-    return true;
-  }
-  if (httpServer_->sendBufferIsEmpty()) {
-    A2_LOG_INFO(fmt('CUID#%' PRId64 ' - HttpServer: all response transmitted.',
-                    getCuid()));
-    afterSend(httpServer_, e_);
-    return true;
-  }
-  else {
-    if (timeoutTimer_.difference(global::wallclock()) >= 30_s) {
-      A2_LOG_INFO(fmt('CUID#%' PRId64
-                      ' - HttpServer: Timeout while trasmitting response.',
-                      getCuid()));
-      return true;
-    }
-    else {
-      updateReadWriteCheck();
-      e_->addCommand(std::unique_ptr<Command>(this));
-      return false;
-    }
-  }
+  llvm_unreachable('did not find group name for reference');
 }
     
-      virtual ~AbstractProxyResponseCommand();
+      bool HadInvalidLocError = false;
+  bool HadAnyError = false;
+    
+    #endif // SWIFT_SYNTAX_REFERENCES_H
+
+    
+      /// Get the canonicalized substitution. If wasCanonical is not nullptr,
+  /// store there whether the current substitution was canonical already.
+  Substitution getCanonicalSubstitution(bool *wasCanonical = nullptr) const;
+    
+    #include 'swift/AST/ASTContext.h'
+#include 'swift/AST/GenericEnvironment.h'
+#include 'swift/AST/Module.h'
+#include 'swift/AST/ProtocolConformance.h'
+#include 'swift/AST/SubstitutionMap.h'
+#include 'swift/AST/Types.h'
+#include 'llvm/ADT/DenseMap.h'
+    
+                            CV_Assert(arr.type() == CV_64FC2);
+                        Point2d tangentVector_1 = arr.at<Point2d>(endPointIndex) - arr.at<Point2d>(startPointIndex);
+                        Point2d tangentVector_2 = arr.at<Point2d>(testPointIndex) - arr.at<Point2d>(startPointIndex);
+    
+    void cv::cuda::HostMem::release()
+{
+#ifdef HAVE_CUDA
+    if (refcount && CV_XADD(refcount, -1) == 1)
+    {
+        cudaFreeHost(datastart);
+        fastFree(refcount);
+    }
+    }
+    
+        // Extension: 1.1
+    typedef void (CODEGEN_FUNCPTR *PFNCULLFACEPROC)(GLenum );
+    typedef void (CODEGEN_FUNCPTR *PFNFRONTFACEPROC)(GLenum );
+    typedef void (CODEGEN_FUNCPTR *PFNHINTPROC)(GLenum , GLenum );
+    typedef void (CODEGEN_FUNCPTR *PFNLINEWIDTHPROC)(GLfloat );
+    typedef void (CODEGEN_FUNCPTR *PFNPOINTSIZEPROC)(GLfloat );
+    typedef void (CODEGEN_FUNCPTR *PFNPOLYGONMODEPROC)(GLenum , GLenum );
+    typedef void (CODEGEN_FUNCPTR *PFNSCISSORPROC)(GLint , GLint , GLsizei , GLsizei );
+    typedef void (CODEGEN_FUNCPTR *PFNTEXPARAMETERFPROC)(GLenum , GLenum , GLfloat );
+    typedef void (CODEGEN_FUNCPTR *PFNTEXPARAMETERFVPROC)(GLenum , GLenum , const GLfloat *);
+    typedef void (CODEGEN_FUNCPTR *PFNTEXPARAMETERIPROC)(GLenum , GLenum , GLint );
+    typedef void (CODEGEN_FUNCPTR *PFNTEXPARAMETERIVPROC)(GLenum , GLenum , const GLint *);
+    typedef void (CODEGEN_FUNCPTR *PFNTEXIMAGE1DPROC)(GLenum , GLint , GLint , GLsizei , GLint , GLenum , GLenum , const GLvoid *);
+    typedef void (CODEGEN_FUNCPTR *PFNTEXIMAGE2DPROC)(GLenum , GLint , GLint , GLsizei , GLsizei , GLint , GLenum , GLenum , const GLvoid *);
+    typedef void (CODEGEN_FUNCPTR *PFNDRAWBUFFERPROC)(GLenum );
+    typedef void (CODEGEN_FUNCPTR *PFNCLEARPROC)(GLbitfield );
+    typedef void (CODEGEN_FUNCPTR *PFNCLEARCOLORPROC)(GLfloat , GLfloat , GLfloat , GLfloat );
+    typedef void (CODEGEN_FUNCPTR *PFNCLEARSTENCILPROC)(GLint );
+    typedef void (CODEGEN_FUNCPTR *PFNCLEARDEPTHPROC)(GLdouble );
+    typedef void (CODEGEN_FUNCPTR *PFNSTENCILMASKPROC)(GLuint );
+    typedef void (CODEGEN_FUNCPTR *PFNCOLORMASKPROC)(GLboolean , GLboolean , GLboolean , GLboolean );
+    typedef void (CODEGEN_FUNCPTR *PFNDEPTHMASKPROC)(GLboolean );
+    typedef void (CODEGEN_FUNCPTR *PFNDISABLEPROC)(GLenum );
+    typedef void (CODEGEN_FUNCPTR *PFNENABLEPROC)(GLenum );
+    typedef void (CODEGEN_FUNCPTR *PFNFINISHPROC)();
+    typedef void (CODEGEN_FUNCPTR *PFNFLUSHPROC)();
+    typedef void (CODEGEN_FUNCPTR *PFNBLENDFUNCPROC)(GLenum , GLenum );
+    typedef void (CODEGEN_FUNCPTR *PFNLOGICOPPROC)(GLenum );
+    typedef void (CODEGEN_FUNCPTR *PFNSTENCILFUNCPROC)(GLenum , GLint , GLuint );
+    typedef void (CODEGEN_FUNCPTR *PFNSTENCILOPPROC)(GLenum , GLenum , GLenum );
+    typedef void (CODEGEN_FUNCPTR *PFNDEPTHFUNCPROC)(GLenum );
+    typedef void (CODEGEN_FUNCPTR *PFNPIXELSTOREFPROC)(GLenum , GLfloat );
+    typedef void (CODEGEN_FUNCPTR *PFNPIXELSTOREIPROC)(GLenum , GLint );
+    typedef void (CODEGEN_FUNCPTR *PFNREADBUFFERPROC)(GLenum );
+    typedef void (CODEGEN_FUNCPTR *PFNREADPIXELSPROC)(GLint , GLint , GLsizei , GLsizei , GLenum , GLenum , GLvoid *);
+    typedef void (CODEGEN_FUNCPTR *PFNGETBOOLEANVPROC)(GLenum , GLboolean *);
+    typedef void (CODEGEN_FUNCPTR *PFNGETDOUBLEVPROC)(GLenum , GLdouble *);
+    typedef GLenum (CODEGEN_FUNCPTR *PFNGETERRORPROC)();
+    typedef void (CODEGEN_FUNCPTR *PFNGETFLOATVPROC)(GLenum , GLfloat *);
+    typedef void (CODEGEN_FUNCPTR *PFNGETINTEGERVPROC)(GLenum , GLint *);
+    typedef const GLubyte * (CODEGEN_FUNCPTR *PFNGETSTRINGPROC)(GLenum );
+    typedef void (CODEGEN_FUNCPTR *PFNGETTEXIMAGEPROC)(GLenum , GLint , GLenum , GLenum , GLvoid *);
+    typedef void (CODEGEN_FUNCPTR *PFNGETTEXPARAMETERFVPROC)(GLenum , GLenum , GLfloat *);
+    typedef void (CODEGEN_FUNCPTR *PFNGETTEXPARAMETERIVPROC)(GLenum , GLenum , GLint *);
+    typedef void (CODEGEN_FUNCPTR *PFNGETTEXLEVELPARAMETERFVPROC)(GLenum , GLint , GLenum , GLfloat *);
+    typedef void (CODEGEN_FUNCPTR *PFNGETTEXLEVELPARAMETERIVPROC)(GLenum , GLint , GLenum , GLint *);
+    typedef GLboolean (CODEGEN_FUNCPTR *PFNISENABLEDPROC)(GLenum );
+    typedef void (CODEGEN_FUNCPTR *PFNDEPTHRANGEPROC)(GLdouble , GLdouble );
+    typedef void (CODEGEN_FUNCPTR *PFNVIEWPORTPROC)(GLint , GLint , GLsizei , GLsizei );
+    typedef void (CODEGEN_FUNCPTR *PFNDRAWARRAYSPROC)(GLenum , GLint , GLsizei );
+    typedef void (CODEGEN_FUNCPTR *PFNDRAWELEMENTSPROC)(GLenum , GLsizei , GLenum , const GLvoid *);
+    typedef void (CODEGEN_FUNCPTR *PFNGETPOINTERVPROC)(GLenum , GLvoid* *);
+    typedef void (CODEGEN_FUNCPTR *PFNPOLYGONOFFSETPROC)(GLfloat , GLfloat );
+    typedef void (CODEGEN_FUNCPTR *PFNCOPYTEXIMAGE1DPROC)(GLenum , GLint , GLenum , GLint , GLint , GLsizei , GLint );
+    typedef void (CODEGEN_FUNCPTR *PFNCOPYTEXIMAGE2DPROC)(GLenum , GLint , GLenum , GLint , GLint , GLsizei , GLsizei , GLint );
+    typedef void (CODEGEN_FUNCPTR *PFNCOPYTEXSUBIMAGE1DPROC)(GLenum , GLint , GLint , GLint , GLint , GLsizei );
+    typedef void (CODEGEN_FUNCPTR *PFNCOPYTEXSUBIMAGE2DPROC)(GLenum , GLint , GLint , GLint , GLint , GLint , GLsizei , GLsizei );
+    typedef void (CODEGEN_FUNCPTR *PFNTEXSUBIMAGE1DPROC)(GLenum , GLint , GLint , GLsizei , GLenum , GLenum , const GLvoid *);
+    typedef void (CODEGEN_FUNCPTR *PFNTEXSUBIMAGE2DPROC)(GLenum , GLint , GLint , GLint , GLsizei , GLsizei , GLenum , GLenum , const GLvoid *);
+    typedef void (CODEGEN_FUNCPTR *PFNBINDTEXTUREPROC)(GLenum , GLuint );
+    typedef void (CODEGEN_FUNCPTR *PFNDELETETEXTURESPROC)(GLsizei , const GLuint *);
+    typedef void (CODEGEN_FUNCPTR *PFNGENTEXTURESPROC)(GLsizei , GLuint *);
+    typedef GLboolean (CODEGEN_FUNCPTR *PFNISTEXTUREPROC)(GLuint );
+    typedef void (CODEGEN_FUNCPTR *PFNINDEXUBPROC)(GLubyte );
+    typedef void (CODEGEN_FUNCPTR *PFNINDEXUBVPROC)(const GLubyte *);
+    
+    //
+// BEGIN OF CUSTOM FUNCTIONS
+//
+    
+        BOOST_CHECK(mD.IsEqualTo(mC, c_epsilonFloatE4));
+    
+        // first try if a NetworkBuilder is present
+    function<ComputationNetworkPtr(DEVICEID_TYPE)> createNetworkFn;
+    bool gotIt = TryGetNetworkFactory<ConfigRecordType, ElemType>(config, createNetworkFn);
+    if (gotIt)
+    {
+        // We have several ways to create a network.
+        net = createNetworkFn(deviceId);
+        if (outputNodeNames.size() > 0)
+        {
+            net->InvalidateCompiledNetwork();
+            PatchOutputNodes(net, outputNodeNames, outputNodeNamesVector);
+            net->CompileNetwork();
+            // BUGBUG: This will generate double Validation output in the log
+        }
+    }
+    else // no NetworkBuilder given: load from 'modelPath'
+    {
+        wstring modelPath = config(L'modelPath');
+    }
+    
+    // ---------------------------------------------------------------------------
+// ConfigException -- all errors from processing the config files are reported as ConfigException
+// ---------------------------------------------------------------------------
+    
+    
+    {        // no config file, parse as regular argument
+        if (compare)
+        {
+            configString += (msra::strfun::utf8(str) + '\n');
+        }
+        else // One or more config file paths specified in a '+'-separated list.
+        {
+            const std::string filePaths = msra::strfun::utf8(str.substr(configDescriptor.length()));
+            std::vector<std::string> filePathsVec = msra::strfun::split(filePaths, '+');
+            for (auto filePath : filePathsVec)
+            {
+                if (std::find(resolvedConfigFiles.begin(), resolvedConfigFiles.end(), filePath) == resolvedConfigFiles.end())
+                {
+                    // if haven't already read this file, read it
+                    resolvedConfigFiles.push_back(filePath);
+                    configString += config.ReadConfigFile(filePath);
+                    // remember all config directories, for use as include paths by BrainScriptNetworkBuilder
+                    GetBrainScriptNetworkBuilderIncludePaths().push_back(File::DirectoryPathOf(msra::strfun::utf16(filePath)));
+                }
+                else
+                    RuntimeError('Cannot specify same config file multiple times at the command line.');
+            }
+        }
+    }
+    // now, configString is a concatenation of lines, including parameters from the command line, with comments stripped
