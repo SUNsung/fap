21538808a197cb9635756732f2cb3acd66f2e3e5
SUNsung <weatherius@gmail.com>
2018-03-21 02:16:04 +0200

    Rewrite [35ingT3M8Z.35wnoMbF08yc35tqPXAfYQPqI35Ikg.py] File in: 2018-03-21 02:16:04

--- pyton/35ingT3M8Z.35wnoMbF08yc35tqPXAfYQPqI35Ikg.py
+++ pyton/35ingT3M8Z.35wnoMbF08yc35tqPXAfYQPqI35Ikg.py
@@ -1,95 +1,96 @@
 
         
-        def main():
-    # First, we load the current README into memory as an array of lines
-    with open('README.md', 'r') as read_me_file:
-        read_me = read_me_file.readlines()
+            :param url: URL for the new :class:`Request` object.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    '''
     
-        format_str = ('step %d, loss = %.2f (%.1f examples/sec; %.3f ' 'sec/batch)')
-    tf.logging.info(format_str % (global_step_val, loss_val, examples_per_sec,
-                                  sec_per_batch))
+            with Server.basic_response_server(wait_to_close_event=block_server) as (host, port):
+            sock = socket.socket()
+            sock.connect((host, port))
+            sock.sendall(b'send something')
+            time.sleep(2.5)
+            sock.sendall(b'still alive')
+            block_server.set()  # release server block
     
-      def _record(self, label, red, green, blue):
-    image_size = 32 * 32
-    record = bytes(bytearray([label] + [red] * image_size +
-                             [green] * image_size + [blue] * image_size))
-    expected = [[[red, green, blue]] * 32] * 32
-    return record, expected
-    
-    def new_input_fn():
-    def decode(x):
-        x = tf.split(x, 4)  # Need to split into our 4 features
-        return dict(zip(feature_names, x))  # To build a dict of them
-    
-      for doc in document_generators.documents(
-      dataset='train', include_unlabeled=True, include_validation=True):
-    input_seq = build_input_sequence(doc, vocab_ids)
-    if len(input_seq) < 2:
-      continue
-    rev_seq = data.build_reverse_sequence(input_seq)
-    lm_seq = data.build_lm_sequence(input_seq)
-    rev_lm_seq = data.build_lm_sequence(rev_seq)
-    seq_ae_seq = data.build_seq_ae_sequence(input_seq)
-    if doc.label is not None:
-      # Used for sentiment classification.
-      label_seq = data.build_labeled_sequence(
-          input_seq,
-          doc.label,
-          label_gain=(FLAGS.label_gain and not doc.is_validation))
-      bd_label_seq = data.build_labeled_sequence(
-          data.build_bidirectional_seq(input_seq, rev_seq),
-          doc.label,
-          label_gain=(FLAGS.label_gain and not doc.is_validation))
-      class_writer = writer_valid_class if doc.is_validation else writer_class
-      bd_class_writer = (writer_bd_valid_class
-                         if doc.is_validation else writer_bd_class)
-      class_writer.write(label_seq.seq.SerializeToString())
-      bd_class_writer.write(bd_label_seq.seq.SerializeToString())
-    
-    from data import data_utils
-from data import document_generators
+        def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        # Keep state in per-thread local storage
+        self._thread_local = threading.local()
     
     
-def read32(bytestream):
-  '''Read 4 bytes from bytestream as an unsigned 32-bit integer.'''
-  dt = np.dtype(np.uint32).newbyteorder('>')
-  return np.frombuffer(bytestream.read(4), dtype=dt)[0]
-    
-        def get(self, key):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                return item.value
-        raise KeyError('Key not found')
-    
-    class DefaultCategories(Enum):
-    
-        def test_pool_behavior_with_single_object_inside(self):
-        sample_queue = queue.Queue()
-        sample_queue.put('yam')
-        with ObjectPool(sample_queue) as obj:
-            # print('Inside with: {}'.format(obj))
-            self.assertEqual(obj, 'yam')
-        self.assertFalse(sample_queue.empty())
-        self.assertTrue(sample_queue.get() == 'yam')
-        self.assertTrue(sample_queue.empty())
-        
-    
-        def test_display_current_time_at_midnight(self):
-        class_under_test = TimeDisplay()
-        expected_time = '24:01'
-        result = class_under_test.get_current_time_as_as_html_fragment()
-        self.assertEqual(result, expected_time)
-'''
-    
-        def get_current_time_as_html_fragment(self, time_provider):
-        current_time = time_provider.now()
-        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
-        return current_time_as_html_fragment
+class ContentDecodingError(RequestException, BaseHTTPError):
+    '''Failed to decode response content'''
     
     
-### OUTPUT ###
-# Specification
-# False
-# True
-# False
+@pytest.mark.parametrize(
+    'value, expected', (
+                (u'test', True),
+                (u'æíöû', False),
+                (u'ジェーピーニック', False),
+    )
+)
+def test_unicode_is_ascii(value, expected):
+    assert unicode_is_ascii(value) is expected
+    
+            class Delegate(HTTPMessageDelegate):
+            def headers_received(self, start_line, headers):
+                test.code = start_line.code
+    
+    from tornado.escape import json_decode
+from tornado.test.httpserver_test import TypeCheckHandler
+from tornado.testing import AsyncHTTPTestCase
+from tornado.web import RequestHandler, Application
+from tornado.wsgi import WSGIApplication, WSGIContainer, WSGIAdapter
+    
+            auth_header = self.request.headers.get('Authorization', None)
+        if auth_header is not None:
+            auth_mode, params = auth_header.split(' ', 1)
+            assert auth_mode == 'Digest'
+            param_dict = {}
+            for pair in params.split(','):
+                k, v = pair.strip().split('=', 1)
+                if v[0] == ''' and v[-1] == ''':
+                    v = v[1:-1]
+                param_dict[k] = v
+            assert param_dict['realm'] == realm
+            assert param_dict['opaque'] == opaque
+            assert param_dict['nonce'] == nonce
+            assert param_dict['username'] == username
+            assert param_dict['uri'] == self.request.path
+            h1 = md5(utf8('%s:%s:%s' % (username, realm, password))).hexdigest()
+            h2 = md5(utf8('%s:%s' % (self.request.method,
+                                     self.request.path))).hexdigest()
+            digest = md5(utf8('%s:%s:%s' % (h1, nonce, h2))).hexdigest()
+            if digest == param_dict['response']:
+                self.write('ok')
+            else:
+                self.write('fail')
+        else:
+            self.set_status(401)
+            self.set_header('WWW-Authenticate',
+                            'Digest realm='%s', nonce='%s', opaque='%s'' %
+                            (realm, nonce, opaque))
+    
+    define('port', default=8888)
+define('num_chunks', default=1000)
+define('chunk_size', default=2048)
+    
+        def new_messages(self, messages):
+        logging.info('Sending new message to %r listeners', len(self.waiters))
+        for future in self.waiters:
+            future.set_result(messages)
+        self.waiters = set()
+        self.cache.extend(messages)
+        if len(self.cache) > self.cache_size:
+            self.cache = self.cache[-self.cache_size:]
+    
+        def open(self):
+        ChatSocketHandler.waiters.add(self)
+    
+    
+# copied from fix_tuple_params.py
+def is_docstring(stmt):
+    return isinstance(stmt, pytree.Node) and stmt.children[0].type == token.STRING
