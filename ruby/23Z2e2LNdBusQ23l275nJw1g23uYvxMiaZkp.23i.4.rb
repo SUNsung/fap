45f6a2d4935dd6a9f5480ec8954393269311fe54
SUNsung <weatherius@gmail.com>
2018-03-23 01:46:50 +0200

    Rewrite [23Z2e2LNdBusQ23l275nJw1g23uYvxMiaZkp.23i.4.rb] File in: 2018-03-23 01:46:49

--- ruby/23Z2e2LNdBusQ23l275nJw1g23uYvxMiaZkp.23i.4.rb
+++ ruby/23Z2e2LNdBusQ23l275nJw1g23uYvxMiaZkp.23i.4.rb
@@ -1,60 +1,64 @@
 
         
-        # Create an array of vendored mimetype => [extensions]
-mimes = {}
-json = open('https://raw.githubusercontent.com/jshttp/mime-db/master/db.json').read
-data = JSON.parse(json)
-data.reject! { |mime, meta| meta['extensions'].nil? || meta['extensions'].empty? }
-data.each do |mime, meta|
-  # Normalize extensions and mime-types
-  mime = mime.downcase.strip
-  extensions = meta['extensions'].map { |e| e.downcase.strip }.compact
+          def present(payload)
+    if payload.is_a?(Hash)
+      payload = ActiveSupport::HashWithIndifferentAccess.new(payload)
+      MAIN_KEYS.each do |key|
+        return { :title => payload[key].to_s, :entries => present_hash(payload, key) } if payload.has_key?(key)
+      end
     
-        def relative_to_source(entry)
-      File.join(
-        base_directory, entry
-      )
-    end
+      def destroy_failed
+    Delayed::Job.where.not(failed_at: nil).delete_all
     
-        # Gets the path to this layout relative to its base
-    attr_reader :relative_path
+      def test_delete_if
+    h1 = {}
+    ENV.each_pair {|k, v| h1[k] = v }
+    ENV['test'] = 'foo'
+    ENV.delete_if {|k, v| IGNORE_CASE ? k.upcase == 'TEST' : k == 'test' }
+    h2 = {}
+    ENV.each_pair {|k, v| h2[k] = v }
+    assert_equal(h1, h2)
     
-      it 'passes errors through if :reaction => :report is used' do
-    mock_app do
-      use Rack::Protection, :reaction => :report
-      run proc { |e| [200, {'Content-Type' => 'text/plain'}, [e['protection.failed'].to_s]] }
-    end
+        iset = Set.new.compare_by_identity
+    assert_send([iset, :compare_by_identity?])
+    iset.merge(array)
+    assert_equal(5, iset.size)
+    assert_equal(array.map(&:object_id).sort, iset.map(&:object_id).sort)
     
-      it 'should allow changing the protection mode' do
-    # I have no clue what other modes are available
-    mock_app do
-      use Rack::Protection::XSSHeader, :xss_mode => :foo
-      run DummyApp
-    end
-    
-    desc 'Generates a dummy app for testing for every Spree engine'
-task :test_app do
-  SPREE_GEMS.each do |gem_name|
-    Dir.chdir('#{File.dirname(__FILE__)}/#{gem_name}') do
-      sh 'rake test_app'
-    end
-  end
-end
-    
-            def address_params
-          params.require(:address).permit(permitted_address_attributes)
-        end
-    
-            def stock_location_params
-          params.require(:stock_location).permit(permitted_stock_location_attributes)
-        end
+      it 'does not call #initialize' do
+    c = Class.new(Thread) do
+      def initialize
+        ScratchPad.record :bad
       end
     end
-  end
-end
-
     
-            def operator
-          assignment_node = meta_assignment_node || @node
-          assignment_node.loc.operator.source
+            def validate!
+          super
+          if @pod_name.nil? && !@wipe_all
+            # Security measure, to avoid removing the pod cache too agressively by mistake
+            help! 'You should either specify a pod name or use the --all flag'
+          end
+        end
+    
+            def run
+          UI.puts('$CACHE_ROOT: #{@cache.root}') if @short_output
+          if @pod_name.nil? # Print all
+            @cache.cache_descriptors_per_pod.each do |pod_name, cache_descriptors|
+              print_pod_cache_infos(pod_name, cache_descriptors)
+            end
+          else # Print only for the requested pod
+            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
+            if cache_descriptors.nil?
+              UI.notice('No cache for pod named #{@pod_name} found')
+            else
+              print_pod_cache_infos(@pod_name, cache_descriptors)
+            end
+          end
+        end
+    
+              if @address.update_attributes(address_params)
+            respond_with(@address, default_template: :show)
+          else
+            invalid_resource!(@address)
+          end
         end
