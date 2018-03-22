22f3d47122ac03d0a87b58cd6b3a473c20e51831
SUNsung <weatherius@gmail.com>
2018-03-22 22:42:59 +0200

    Rewrite [36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py] File in: 2018-03-22 22:42:59

--- pyton/36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py
+++ pyton/36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py
@@ -1,161 +1,54 @@
 
         
-            @property
-    def body(self):
-        # Only now the response body is fetched.
-        # Shouldn't be touched unless the body is actually needed.
-        return self._orig.content
+            def getChild(self, request, name):
+        return self
     
-            '''
-        available_plugins = plugin_manager.get_formatters_grouped()
-        self.enabled_plugins = []
-        for group in groups:
-            for cls in available_plugins[group]:
-                p = cls(env=env, **kwargs)
-                if p.enabled:
-                    self.enabled_plugins.append(p)
+            spider_loader = self.crawler_process.spider_loader
     
-        '''
-    exit_status = ExitStatus.OK
-    downloader = None
-    show_traceback = args.debug or args.traceback
+        def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
+                          help='set spider argument (may be repeated)')
+        parser.add_option('-o', '--output', metavar='FILE',
+                          help='dump scraped items into FILE (use - for stdout)')
+        parser.add_option('-t', '--output-format', metavar='FORMAT',
+                          help='format to use for dumping items with -o')
     
-            $ http :3000                    # => http://localhost:3000
-        $ http :/foo                    # => http://localhost/foo
+        def run(self, args, opts):
+        if opts.list:
+            self._list_templates()
+            return
+        if opts.dump:
+            template_file = self._find_template(opts.dump)
+            if template_file:
+                with open(template_file, 'r') as f:
+                    print(f.read())
+            return
+        if len(args) != 2:
+            raise UsageError()
     
-        Assumes `from __future__ import division`.
+            general form:
+        @returns request(s)/item(s) [min=1 [max]]
     
-      Returns:
-    SequenceWrapper with `seq` inputs copied and concatenated, and with labels
-    copied in on the right-hand (i.e. decoder) side with weights set to 1.0.
-    The new sequence will have length `len(seq) * 2 - 1`, as the last timestep
-    of the encoder section and the first step of the decoder section will
-    overlap.
-  '''
-  seq_ae_seq = SequenceWrapper()
+    # http://docs.readthedocs.org/en/latest/theme.html#how-do-i-use-this-locally-and-on-read-the-docs
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
     
-      @tf.test.mock.patch.dict(premade_estimator.__dict__,
-                           {'load_data': four_lines_data})
-  def test_premade_estimator(self):
-    premade_estimator.main([None, '--train_steps=1'])
+        @mock.patch(
+        'certbot_compatibility_test.validator.crypto_util.probe_sni')
+    def test_certificate_failure(self, mock_probe_sni):
+        cert = OpenSSL.crypto.X509()
+        cert.set_serial_number(1337)
+        mock_probe_sni.return_value = OpenSSL.crypto.X509()
+        self.assertFalse(self.validator.certificate(
+            cert, 'test.com', '127.0.0.1'))
     
-      Args:
-    sess: Session.
-    saver: Saver for restoring the checkpoint.
-    
-      Raises:
-    ValueError: if an invalid combination of arguments is provided that does not
-      map to any data files (e.g. pretrain=False, use_seq2seq=True).
-  '''
-  data_spec = (phase, bidir, pretrain, use_seq2seq)
-  data_specs = {
-      ('train', True, True, False): (data_utils.TRAIN_LM,
-                                     data_utils.TRAIN_REV_LM),
-      ('train', True, False, False): (data_utils.TRAIN_BD_CLASS,),
-      ('train', False, True, False): (data_utils.TRAIN_LM,),
-      ('train', False, True, True): (data_utils.TRAIN_SA,),
-      ('train', False, False, False): (data_utils.TRAIN_CLASS,),
-      ('test', True, True, False): (data_utils.TEST_LM,
-                                    data_utils.TRAIN_REV_LM),
-      ('test', True, False, False): (data_utils.TEST_BD_CLASS,),
-      ('test', False, True, False): (data_utils.TEST_LM,),
-      ('test', False, True, True): (data_utils.TEST_SA,),
-      ('test', False, False, False): (data_utils.TEST_CLASS,),
-      ('valid', True, False, False): (data_utils.VALID_BD_CLASS,),
-      ('valid', False, False, False): (data_utils.VALID_CLASS,),
-  }
-  if data_spec not in data_specs:
-    raise ValueError(
-        'Data specification (phase, bidir, pretrain, use_seq2seq) %s not '
-        'supported' % str(data_spec))
-    
-    
-def print_list():
-    data = get_landscape_members_data()
-    nodes = get_nodes(data)
-    groups = get_groups(data)
-    meta = get_meta(data)
-    inventory_data = {_key: nodes, '_meta': meta}
-    inventory_data.update(groups)
-    print(json.dumps(inventory_data))
-    
-        'ipv6': re.compile(
-        r'''^
-            (?:{0}:){{7}}{0}|           # uncompressed: 1:2:3:4:5:6:7:8
-            (?:{0}:){{1,6}}:|           # compressed variants, which are all
-            (?:{0}:)(?::{0}){{1,6}}|    # a::b for various lengths of a,b
-            (?:{0}:){{2}}(?::{0}){{1,5}}|
-            (?:{0}:){{3}}(?::{0}){{1,4}}|
-            (?:{0}:){{4}}(?::{0}){{1,3}}|
-            (?:{0}:){{5}}(?::{0}){{1,2}}|
-            (?:{0}:){{6}}(?::{0})|      # ...all with 2 <= a+b <= 7
-            :(?::{0}){{1,6}}|           # ::ffff(:ffff...)
-            {0}?::|                     # ffff::, ::
-                                        # ipv4-in-ipv6 variants
-            (?:0:){{6}}(?:{0}\.){{3}}{0}|
-            ::(?:ffff:)?(?:{0}\.){{3}}{0}|
-            (?:0:){{5}}ffff:(?:{0}\.){{3}}{0}
-            $
-        '''.format(ipv6_component), re.X | re.I
-    ),
-    
-    try:
-    import json
-except ImportError:
-    import simplejson as json
-    
-    from ansible.plugins.terminal import TerminalBase
-from ansible.errors import AnsibleConnectionFailure
-    
-            (2016-01, shopping), 25
-        (2016-01, shopping), 100
-        (2016-01, gas), 50
-        '''
-        timestamp, seller, amount = line.split('\t')
-        period = self. extract_year_month(timestamp)
-        if period == self.current_year_month():
-            yield (period, category), amount
-    
-        def mapper(self, _, line):
-        '''Parse each log line, extract and transform relevant lines.
-    
-        def escalate_call(self):
-        self.call.level = Rank.DIRECTOR
-        self._escalate_call()
-    
-        def __init__(self, message_id, message, timestamp):
-        self.message_id = message_id
-        self.message = message
-        self.timestamp = timestamp
-    
-        def __init__(self, vehicle_size, license_plate, spot_size):
-        self.vehicle_size = vehicle_size
-        self.license_plate = license_plate
-        self.spot_size
-        self.spots_taken = []
-    
-        def add_link_to_crawl(self, url):
-        '''Add the given link to `links_to_crawl`.'''
-        pass
-    
-                elif opt in ('-l', '--playlist', '--playlists'):
-                # Download playlist whenever possible.
-                conf['playlist'] = True
-    
-    site_info = 'CBS.com'
-download = cbs_download
-download_playlist = playlist_not_supported('cbs')
-
-    
-    from ..common import *
-    
-    def kuwo_download_by_rid(rid, output_dir = '.', merge = True, info_only = False):
-    html=get_content('http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_%s'%rid)
-    title=match1(html,r'<name>(.*)</name>')
-    #to get title
-    #format =aac|mp3 ->to get aac format=mp3 ->to get mp3
-    url=get_content('http://antiserver.kuwo.cn/anti.s?format=mp3&rid=MUSIC_%s&type=convert_url&response=url'%rid)
-    songtype, ext, size = url_info(url)
-    print_info(site_info, title, songtype, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir)
+    # Add any extra paths that contain custom files (such as robots.txt or
+# .htaccess) here, relative to this directory. These files are copied
+# directly to the root of the documentation.
+#html_extra_path = []
