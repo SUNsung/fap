fbcf03dfb1826f2c844c03fd2a4fcb36656bb113
SUNsung <weatherius@gmail.com>
2018-03-22 03:15:59 +0200

    Rewrite [264QfYILhqLr626tSxsWDpzew26Gx5TWgyd2Mc26u9I.cpp] File in: 2018-03-22 03:15:58

--- cccccc/264QfYILhqLr626tSxsWDpzew26Gx5TWgyd2Mc26u9I.cpp
+++ cccccc/264QfYILhqLr626tSxsWDpzew26Gx5TWgyd2Mc26u9I.cpp
@@ -1,208 +1,119 @@
 
         
-        namespace nw {
+        IPC_MESSAGE_ROUTED3(ShellViewHostMsg_Call_Static_Method,
+                    std::string /* type name */,
+                    std::string /* method name */,
+                    base::ListValue /* arguments */)
+    
+    namespace nwapi {
     }
     
-    EventListener::~EventListener() {
-  for (std::map<int, BaseEvent*>::iterator i = listerners_.begin(); i != listerners_.end(); i++) {
-    delete i->second;
-  }
+    DispatcherHost::DispatcherHost(content::RenderViewHost* host)
+  : content::WebContentsObserver(content::WebContents::FromRenderViewHost(host)),
+    render_view_host_(host),
+    weak_ptr_factory_(this),
+    run_loop_(NULL) {
+  g_dispatcher_host_map[render_view_host_] = this;
 }
     
-    #endif  // CONTENT_NW_SRC_API_MENU_MENU_DELEGATE_H_
+      // RenderViewHostObserver implementation.
+  // WebContentsObserver implementation:
+  bool OnMessageReceived(
+                         content::RenderViewHost* render_view_host,
+                         const IPC::Message& message) override;
+    
+    class BaseEvent {
+  friend class EventListener;
+  DISALLOW_COPY_AND_ASSIGN(BaseEvent);
+    }
+    
+    #include 'content/nw/src/api/menu/menu.h'
+    
+     protected:
+  ~NwAppClearCacheFunction() override;
+    
+    #include <grpc/support/log.h>
+    
+      grpc::testing::RunQPS();
+    
+    #include <algorithm>
+#include <cctype>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include 'route_guide.grpc.pb.h'
+    
+    #if TARGET_OS_IPHONE
+GRPC_XMACRO_ITEM(isWWAN, IsWWAN)
+#endif
+GRPC_XMACRO_ITEM(reachable, Reachable)
+GRPC_XMACRO_ITEM(transientConnection, TransientConnection)
+GRPC_XMACRO_ITEM(connectionRequired, ConnectionRequired)
+GRPC_XMACRO_ITEM(connectionOnTraffic, ConnectionOnTraffic)
+GRPC_XMACRO_ITEM(interventionRequired, InterventionRequired)
+GRPC_XMACRO_ITEM(connectionOnDemand, ConnectionOnDemand)
+GRPC_XMACRO_ITEM(isLocalAddress, IsLocalAddress)
+GRPC_XMACRO_ITEM(isDirect, IsDirect)
 
     
-    DEFINE_string(backend, 'lmdb', 'The backend for storing the result');
-    
-      /**
-   * @brief Applies the transformation defined in the data layer's
-   * transform_param block to a vector of Datum.
-   *
-   * @param datum_vector
-   *    A vector of Datum containing the data to be transformed.
-   * @param transformed_blob
-   *    This is destination blob. It can be part of top blob's data if
-   *    set_cpu_data() is used. See memory_layer.cpp for an example.
-   */
-  void Transform(const vector<Datum> & datum_vector,
-                Blob<Dtype>* transformed_blob);
+    namespace grpc {
+namespace testing {
+    }
+    }
     
     
-    {  int count_;
-  int num_concats_;
-  int concat_input_size_;
-  int concat_axis_;
+    {  const Result start_;
 };
     
-    #include <vector>
-    
-    #ifdef USE_CUDNN
-/*
- * @brief cuDNN implementation of PoolingLayer.
- *        Fallback to PoolingLayer for CPU mode.
-*/
-template <typename Dtype>
-class CuDNNPoolingLayer : public PoolingLayer<Dtype> {
- public:
-  explicit CuDNNPoolingLayer(const LayerParameter& param)
-      : PoolingLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNPoolingLayer();
-  // Currently, cuDNN does not support the extra top blob.
-  virtual inline int MinTopBlobs() const { return -1; }
-  virtual inline int ExactNumTopBlobs() const { return 1; }
-    }
-    
-    #include <vector>
-    
-        // version that calls dls 3 times, to avoid Cayley singularity
-    for (int i = 0; i < 3; ++i)
-    {
-        // Make a random rotation
-        cv::Mat pp = R_[i] * ( p - cv::repeat(mn, 1, p.cols) );
-    }
-    
-    struct CV_EXPORTS IntrinsicParams
-{
-    Vec2d f;
-    Vec2d c;
-    Vec4d k;
-    double alpha;
-    std::vector<uchar> isEstimate;
-    }
-    
-    void CV_ModelEstimator2_Test::fill_array( int test_case_idx, int i, int j, Mat& arr )
-{
-    if( i != INPUT )
-    {
-        cvtest::ArrayTest::fill_array( test_case_idx, i, j, arr );
-        return;
-    }
-    }
-    
-        static void* WinGetProcAddress(const char* name)
-    {
-        static HMODULE opencl_module = NULL;
-        if (!opencl_module)
-        {
-            opencl_module = GetModuleHandleA('clAmdFft.Runtime.dll');
-            if (!opencl_module)
-            {
-                opencl_module = LoadLibraryA('clAmdFft.Runtime.dll');
-                if (!opencl_module)
-                    return NULL;
-            }
-        }
-        return (void*)GetProcAddress(opencl_module, name);
-    }
-    #define CV_CL_GET_PROC_ADDRESS(name) WinGetProcAddress(name)
-#endif // _WIN32
-    
-    // Include automatically generated configuration file if running autoconf.
-#ifdef HAVE_CONFIG_H
-#include 'config_auto.h'
-#endif
-    
-    
-    {  int retry_leading = 0;
-  int retry_trailing = 0;
-  bool is_good = false;
-  WERD_RES *revised = TrySuperscriptSplits(
-      num_chopped_leading, leading_certainty, sp_leading,
-      num_chopped_trailing, trailing_certainty, sp_trailing,
-      word, &is_good, &retry_leading, &retry_trailing);
-  if (is_good) {
-    word->ConsumeWordResults(revised);
-  } else if (retry_leading || retry_trailing) {
-    int retry_chopped_leading =
-        LeadingUnicharsToChopped(revised, retry_leading);
-    int retry_chopped_trailing =
-        TrailingUnicharsToChopped(revised, retry_trailing);
-    WERD_RES *revised2 = TrySuperscriptSplits(
-        retry_chopped_leading, leading_certainty, sp_leading,
-        retry_chopped_trailing, trailing_certainty, sp_trailing,
-        revised, &is_good, &retry_leading, &retry_trailing);
-    if (is_good) {
-      word->ConsumeWordResults(revised2);
-    }
-    delete revised2;
-  }
-  delete revised;
-  return is_good;
-}
-    
-    
-    {  name += UNLV_EXT;              //add extension
-  if ((pdfp = fopen (name.string (), 'rb')) == NULL) {
-    return false;                //didn't read one
-  } else {
-    while (tfscanf(pdfp, '%d %d %d %d %*s', &x, &y, &width, &height) >= 4) {
-                                 //make rect block
-      block = new BLOCK (name.string (), TRUE, 0, 0,
-                         (inT16) x, (inT16) (ysize - y - height),
-                         (inT16) (x + width), (inT16) (ysize - y));
-                                 //on end of list
-      block_it.add_to_end (block);
-    }
-    fclose(pdfp);
-  }
-  return true;
-}
-    
-    // APPROXIMATIONS of the fractions of the character cell taken by
-// the descenders, ascenders, and x-height.
-const double CCStruct::kDescenderFraction = 0.25;
-const double CCStruct::kXHeightFraction = 0.5;
-const double CCStruct::kAscenderFraction = 0.25;
-const double CCStruct::kXHeightCapRatio = CCStruct::kXHeightFraction /
-    (CCStruct::kXHeightFraction + CCStruct::kAscenderFraction);
-    
-    
-    {  tmp = xcoord * vec.x () - ycoord * vec.y ();
-  ycoord = ycoord * vec.x () + xcoord * vec.y ();
-  xcoord = tmp;
-}
-    
-    // Transforms the given coords one step back to source space, without
-// using to any block rotation or predecessor.
-void DENORM::LocalDenormTransform(const TPOINT& pt, TPOINT* original) const {
-  FCOORD src_pt(pt.x, pt.y);
-  FCOORD float_result;
-  LocalDenormTransform(src_pt, &float_result);
-  original->x = IntCastRounded(float_result.x());
-  original->y = IntCastRounded(float_result.y());
-}
-void DENORM::LocalDenormTransform(const FCOORD& pt, FCOORD* original) const {
-  FCOORD rotated(pt.x() - final_xshift_, pt.y() - final_yshift_);
-  if (x_map_ != NULL && y_map_ != NULL) {
-    int x = x_map_->binary_search(rotated.x());
-    original->set_x(x + x_origin_);
-    int y = y_map_->binary_search(rotated.y());
-    original->set_y(y + y_origin_);
-  } else {
-    if (rotation_ != NULL) {
-      FCOORD inverse_rotation(rotation_->x(), -rotation_->y());
-      rotated.rotate(inverse_rotation);
-    }
-    original->set_x(rotated.x() / x_scale_ + x_origin_);
-    float y_scale = y_scale_;
-    original->set_y(rotated.y() / y_scale + y_origin_);
-  }
-}
-    
-      // Returns the bounding box including the desired combination of upper and
-  // lower noise/diacritic elements.
-  TBOX restricted_bounding_box(bool upper_dots, bool lower_dots) const;
-    
-    ELISTIZEH(PARA)
-    
-    
-/**********************************************************************
- * ROW::ROW
+    /*
+ * This implements a Metrics server defined in
+ * src/proto/grpc/testing/metrics.proto. Any
+ * test service can use this to export Metrics (TODO (sreek): Only Gauges for
+ * now).
  *
- * Constructor to build a ROW. Only the stats stuff are given here.
- * The words are added directly.
- **********************************************************************/
+ * Example:
+ *    MetricsServiceImpl metricsImpl;
+ *    ..
+ *    // Create QpsGauge(s). Note: QpsGauges can be created even after calling
+ *    // 'StartServer'.
+ *    QpsGauge qps_gauge1 = metricsImpl.CreateQpsGauge('foo', is_present);
+ *    // qps_gauge1 can now be used anywhere in the program by first making a
+ *    // one-time call qps_gauge1.Reset() and then calling qps_gauge1.Incr()
+ *    // every time to increment a query counter
+ *
+ *    ...
+ *    // Create the metrics server
+ *    std::unique_ptr<grpc::Server> server = metricsImpl.StartServer(port);
+ *    server->Wait(); // Note: This is blocking.
+ */
+namespace grpc {
+namespace testing {
+    }
+    }
+    
+    
+    {  gpr_subprocess* const subprocess_;
+};
+    
+      std::shared_ptr<ChannelCredentials> GetChannelCredentials(
+      const grpc::string& type, ChannelArguments* args) override {
+    if (type == grpc::testing::kInsecureCredentialsType) {
+      return InsecureChannelCredentials();
+    } else if (type == grpc::testing::kTlsCredentialsType) {
+      SslCredentialsOptions ssl_opts = {test_root_cert, '', ''};
+      args->SetSslTargetNameOverride('foo.test.google.fr');
+      return SslCredentials(ssl_opts);
+    } else {
+      std::unique_lock<std::mutex> lock(mu_);
+      auto it(std::find(added_secure_type_names_.begin(),
+                        added_secure_type_names_.end(), type));
+      if (it == added_secure_type_names_.end()) {
+        gpr_log(GPR_ERROR, 'Unsupported credentials type %s.', type.c_str());
+        return nullptr;
+      }
+      return added_secure_type_providers_[it - added_secure_type_names_.begin()]
+          ->GetChannelCredentials(args);
+    }
+  }
