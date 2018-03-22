2228ad5cbc8a079760a4e3e5cc04907b4fe52a5f
SUNsung <weatherius@gmail.com>
2018-03-22 16:12:12 +0200

    Rewrite [26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb] File in: 2018-03-22 16:12:11

--- ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
+++ ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
@@ -1,68 +1,85 @@
 
         
-            Jekyll::Commands::Build.process({'source' => 'docs'})
-    
-      p.action do |args, _|
-    if args.empty?
-      Jekyll.logger.error 'A subcommand is required.'
-      puts p
-      abort
-    else
-      subcommand = args.first
-      unless p.has_command? subcommand
-        Jekyll.logger.abort_with 'fatal: 'jekyll #{args.first}' could not' \
-          ' be found. You may need to install the jekyll-#{args.first} gem' \
-          ' or a related gem to be able to use this subcommand.'
-      end
-    end
-  end
-end
-
-    
-          # Group an array of items by an expression
-      #
-      # input - the object array
-      # variable - the variable to assign each item to in the expression
-      # expression -a Liquid comparison expression passed in as a string
-      #
-      # Returns the filtered array of objects
-      def group_by_exp(input, variable, expression)
-        return input unless groupable?(input)
-    
-        # Gets/Sets the content of this layout.
-    attr_accessor :content
-    
-      def userpaths?
-    @settings.include? :userpaths
-  end
-end
-    
-      def core_tap_head
-    CoreTap.instance.git_head || '(none)'
-  end
-    
-    module Homebrew
-  def doctor
-    checks = Diagnostic::Checks.new
-    
-        find_union(segments, Project).includes(:namespace).order_id_desc
-  end
-    
-        # @mixin transition($transition) {
-    # to:
-    # @mixin transition($transition...) {
-    def varargify_mixin_definitions(scss, *mixins)
-      scss = scss.dup
-      replaced = []
-      mixins.each do |mixin|
-        if scss.gsub! /(@mixin\s*#{Regexp.quote(mixin)})\((#{SCSS_MIXIN_DEF_ARGS_RE})\)/, '\1(\2...)'
-          replaced << mixin
+                    define_method('#{name}=') do |attribute|
+              attributes[name.to_sym] = attribute
+            end
+          end
         end
-      end
-      log_transform *replaced unless replaced.empty?
-      scss
-    end
-    }
-    }
     
-      config.active_support.test_order = :random
+        def call(env)
+      result = @app.call(env)
+      result[1]['Configurable-Message'] = configurable_message
+      result
+    end
+  end
+    
+    require 'tmpdir'
+    
+            def enqueue_delivery(delivery_method, options = {})
+          if processed?
+            super
+          else
+            args = @mailer_class.name, @action.to_s, delivery_method.to_s, @params, *@args
+            ActionMailer::Parameterized::DeliveryJob.set(options).perform_later(*args)
+          end
+        end
+    end
+    
+      # True if a {Formula} is being built with {Formula.head} instead of {Formula.stable}.
+  # <pre>args << '--some-new-stuff' if build.head?</pre>
+  # <pre># If there are multiple conditional arguments use a block instead of lines.
+  #  if build.head?
+  #    args << '--i-want-pizza'
+  #    args << '--and-a-cold-beer' if build.with? 'cold-beer'
+  #  end</pre>
+  def head?
+    include? 'HEAD'
+  end
+    
+      def describe_python
+    python = which 'python'
+    return 'N/A' if python.nil?
+    python_binary = Utils.popen_read python, '-c', 'import sys; sys.stdout.write(sys.executable)'
+    python_binary = Pathname.new(python_binary).realpath
+    if python == python_binary
+      python
+    else
+      '#{python} => #{python_binary}'
+    end
+  end
+    
+        print_remaining_files remaining_root_files, root, other
+  end
+    
+          begin
+        result = Formulary.factory(query).name
+      rescue FormulaUnavailableError
+        result = search_tap(user, repo, name)
+      end
+    
+        def build_for_type(type, user, attributes = {})
+      attributes.delete(:type)
+    
+      private
+    
+      delegate :form_configurable_attributes, to: :class
+  delegate :form_configurable_fields, to: :class
+    
+      def set_table_sort(sort_options)
+    valid_sorts = sort_options[:sorts] or raise ArgumentError.new('You must specify :sorts as an array of valid sort attributes.')
+    default = sort_options[:default] || { valid_sorts.first.to_sym => :desc }
+    
+    def usage
+  <<-EOS
+Usage: list_login_items_for_app <path.app>
+    
+      let(:json) { Oj.dump(payload) }
+    
+    namespace :emojis do
+  desc 'Generate a unicode to filename mapping'
+  task :generate do
+    source = 'http://www.unicode.org/Public/emoji/5.0/emoji-test.txt'
+    codes  = []
+    dest   = Rails.root.join('app', 'javascript', 'mastodon', 'features', 'emoji', 'emoji_map.json')
+    
+      end
