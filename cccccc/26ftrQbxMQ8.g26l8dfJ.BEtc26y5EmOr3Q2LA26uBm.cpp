5bcb8355d745efab04fa3518f41275f90f53ce8f
SUNsung <weatherius@gmail.com>
2018-03-22 08:28:51 +0200

    Rewrite [26ftrQbxMQ8.g26l8dfJ.BEtc26y5EmOr3Q2LA26uBm.cpp] File in: 2018-03-22 08:28:51

--- cccccc/26ftrQbxMQ8.g26l8dfJ.BEtc26y5EmOr3Q2LA26uBm.cpp
+++ cccccc/26ftrQbxMQ8.g26l8dfJ.BEtc26y5EmOr3Q2LA26uBm.cpp
@@ -1,82 +1,259 @@
 
         
-          // Appends the TestPartResult object to the TestPartResultArray
-  // received in the constructor.
-  //
-  // This method is from the TestPartResultReporterInterface
-  // interface.
-  virtual void ReportTestPartResult(const TestPartResult& result);
- private:
-  void Init();
+        
+    {  EXPECT_EQ(status(), param.expected_status);
+}
     
-    // This flag brings the debugger on an assertion failure.
-GTEST_DECLARE_bool_(break_on_failure);
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
     
-      // Given directory = 'dir', base_name = 'test', number = 0,
-  // extension = 'xml', returns 'dir/test.xml'. If number is greater
-  // than zero (e.g., 12), returns 'dir/test_12.xml'.
-  // On Windows platform, uses \ as the separator rather than /.
-  static FilePath MakeFileName(const FilePath& directory,
-                               const FilePath& base_name,
-                               int number,
-                               const char* extension);
+    // ResourceOpKernel<T> is a virtual base class for resource op implementing
+// interface type T. The inherited op looks up the resource name (determined by
+// ContainerInfo), and creates a new resource if necessary.
+//
+// Requirements:
+//  - Op must be marked as stateful.
+//  - Op must have `container` and `shared_name` attributes. Empty `container`
+//  means using the default container. Empty `shared_name` means private
+//  resource.
+//  - Subclass must override CreateResource().
+//  - Subclass is encouraged to override VerifyResource().
+template <typename T>
+class ResourceOpKernel : public OpKernel {
+ public:
+  explicit ResourceOpKernel(OpKernelConstruction* context) : OpKernel(context) {
+    OP_REQUIRES_OK(context,
+                   context->allocate_persistent(DT_STRING, TensorShape({2}),
+                                                &handle_, nullptr));
+  }
+    }
+    
+    // A pass which performs constant folding in order to avoid unnecessary
+// computation on constants.
+class HloConstantFolding : public HloPassInterface {
+ public:
+  tensorflow::StringPiece name() const override { return 'constant_folding'; }
+    }
+    
+    namespace perftools {
+namespace gputools {
+namespace cuda {
+    }
+    }
+    }
+    
+    namespace tensorflow {
+    }
+    
+    #endif  // TENSORFLOW_COMMON_RUNTIME_SYCL_SYCL_DEVICE_CONTEXT_H_
+
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+      // TODO(josh11b): Implement serializing and restoring the state.
     
     
-    {  const T1 v1_;
-  const T2 v2_;
-  const T3 v3_;
-  const T4 v4_;
-  const T5 v5_;
-  const T6 v6_;
-  const T7 v7_;
-  const T8 v8_;
-  const T9 v9_;
-  const T10 v10_;
-  const T11 v11_;
-  const T12 v12_;
-  const T13 v13_;
-  const T14 v14_;
-  const T15 v15_;
-  const T16 v16_;
-  const T17 v17_;
-  const T18 v18_;
-  const T19 v19_;
-  const T20 v20_;
-  const T21 v21_;
-  const T22 v22_;
-  const T23 v23_;
-  const T24 v24_;
-  const T25 v25_;
-  const T26 v26_;
-  const T27 v27_;
-  const T28 v28_;
-};
+    {
+    {} // namespace asio
+} // namespace boost
     
-    $for k [[
-$range m 0..k-1
+    } // namespace date_time
+namespace posix_time {
     
-      bool check_for_leaks = false;
-  if (argc > 1 && strcmp(argv[1], '--check_for_leaks') == 0 )
-    check_for_leaks = true;
-  else
-    printf('%s\n', 'Run this program with --check_for_leaks to enable '
-           'custom leak checking in the tests.');
+    template <typename Function, typename Context>
+inline void invoke(Function& function, Context& context)
+{
+#if !defined(BOOST_ASIO_HAS_HANDLER_HOOKS)
+  Function tmp(function);
+  tmp();
+#else
+  using boost::asio::asio_handler_invoke;
+  asio_handler_invoke(function, boost::asio::detail::addressof(context));
+#endif
+}
     
-    /// Ping an extension manager or extension.
-Status pingExtension(const std::string& path);
+    #define BOOST_ASIO_RESOLVE_HANDLER_CHECK( \
+    handler_type, handler, iter_type) \
+  \
+  typedef BOOST_ASIO_HANDLER_TYPE(handler_type, \
+      void(boost::system::error_code, iter_type)) \
+    asio_true_handler_type; \
+  \
+  BOOST_ASIO_HANDLER_TYPE_REQUIREMENTS_ASSERT( \
+      sizeof(boost::asio::detail::two_arg_handler_test( \
+          boost::asio::detail::clvref< \
+            asio_true_handler_type>(), \
+          static_cast<const boost::system::error_code*>(0), \
+          static_cast<const iter_type*>(0))) == 1, \
+      'ResolveHandler type requirements not met') \
+  \
+  typedef boost::asio::detail::handler_type_requirements< \
+      sizeof( \
+        boost::asio::detail::argbyv( \
+          boost::asio::detail::clvref< \
+            asio_true_handler_type>())) + \
+      sizeof( \
+        boost::asio::detail::lvref< \
+          asio_true_handler_type>()( \
+            boost::asio::detail::lvref<const boost::system::error_code>(), \
+            boost::asio::detail::lvref<const iter_type>()), \
+        char(0))> BOOST_ASIO_UNUSED_TYPEDEF
     
-      /**
-   * @brief Initialize any platform dependent libraries or objects
-   *
-   * On windows, we require the COM libraries be initialized just once
-   */
-  static void platformSetup();
+    void buffer_sequence_adapter_base::init_native_buffer(
+    buffer_sequence_adapter_base::native_buffer_type& buf,
+    const boost::asio::mutable_buffer& buffer)
+{
+  std::memset(&buf, 0, sizeof(native_buffer_type));
+  Microsoft::WRL::ComPtr<IInspectable> insp
+    = Microsoft::WRL::Make<winrt_buffer_impl>(buffer);
+  buf = reinterpret_cast<Windows::Storage::Streams::IBuffer^>(insp.Get());
+}
     
-      /*
-   * @brief A helper function to perform a start to finish carve
-   *
-   * This function walks through the carve, compress, and exfil functions
-   * in one fell swoop. Use of this class should largely happen through
-   * this function.
-   */
-  void start();
+    
+    {  // Cancel any outstanding operations associated with the descriptor.
+  cancel_ops_unlocked(descriptor, boost::asio::error::operation_aborted);
+}
+    
+    #include <boost/asio/detail/pop_options.hpp>
+    
+    // Snapshots are kept in a doubly-linked list in the DB.
+// Each SnapshotImpl corresponds to a particular sequence number.
+class SnapshotImpl : public Snapshot {
+ public:
+  SequenceNumber number_;  // const after creation
+    }
+    
+    
+    {}  // namespace leveldb
+    
+      void PrintHeader() {
+    const int kKeySize = 16;
+    PrintEnvironment();
+    fprintf(stdout, 'Keys:       %d bytes each\n', kKeySize);
+    fprintf(stdout, 'Values:     %d bytes each (%d bytes after compression)\n',
+            FLAGS_value_size,
+            static_cast<int>(FLAGS_value_size * FLAGS_compression_ratio + 0.5));
+    fprintf(stdout, 'Entries:    %d\n', num_);
+    fprintf(stdout, 'RawSize:    %.1f MB (estimated)\n',
+            ((static_cast<int64_t>(kKeySize + FLAGS_value_size) * num_)
+             / 1048576.0));
+    fprintf(stdout, 'FileSize:   %.1f MB (estimated)\n',
+            (((kKeySize + FLAGS_value_size * FLAGS_compression_ratio) * num_)
+             / 1048576.0));
+    PrintWarnings();
+    fprintf(stdout, '------------------------------------------------\n');
+  }
+    
+    class FilterBlockReader {
+ public:
+ // REQUIRES: 'contents' and *policy must stay live while *this is live.
+  FilterBlockReader(const FilterPolicy* policy, const Slice& contents);
+  bool KeyMayMatch(uint64_t block_offset, const Slice& key);
+    }
+    
+    const char* GetVarint64Ptr(const char* p, const char* limit, uint64_t* value) {
+  uint64_t result = 0;
+  for (uint32_t shift = 0; shift <= 63 && p < limit; shift += 7) {
+    uint64_t byte = *(reinterpret_cast<const unsigned char*>(p));
+    p++;
+    if (byte & 128) {
+      // More bytes are present
+      result |= ((byte & 127) << shift);
+    } else {
+      result |= (byte << shift);
+      *value = result;
+      return reinterpret_cast<const char*>(p);
+    }
+  }
+  return NULL;
+}
+    
+    bool AbstractHttpServerResponseCommand::execute()
+{
+  if (e_->getRequestGroupMan()->downloadFinished() || e_->isHaltRequested()) {
+    return true;
+  }
+  try {
+    ssize_t len = httpServer_->sendResponse();
+    if (len > 0) {
+      timeoutTimer_ = global::wallclock();
+    }
+  }
+  catch (RecoverableException& e) {
+    A2_LOG_INFO_EX(fmt('CUID#%' PRId64
+                       ' - Error occurred while transmitting response body.',
+                       getCuid()),
+                   e);
+    return true;
+  }
+  if (httpServer_->sendBufferIsEmpty()) {
+    A2_LOG_INFO(fmt('CUID#%' PRId64 ' - HttpServer: all response transmitted.',
+                    getCuid()));
+    afterSend(httpServer_, e_);
+    return true;
+  }
+  else {
+    if (timeoutTimer_.difference(global::wallclock()) >= 30_s) {
+      A2_LOG_INFO(fmt('CUID#%' PRId64
+                      ' - HttpServer: Timeout while trasmitting response.',
+                      getCuid()));
+      return true;
+    }
+    else {
+      updateReadWriteCheck();
+      e_->addCommand(std::unique_ptr<Command>(this));
+      return false;
+    }
+  }
+}
+    
+    #include <memory>
+    
+    void AbstractOptionHandler::setChangeOptionForReserved(bool f)
+{
+  updateFlags(FLAG_CHANGE_OPTION_FOR_RESERVED, f);
+}
+    
+    bool AbstractProxyRequestCommand::executeInternal()
+{
+  // socket->setBlockingMode();
+  if (httpConnection_->sendBufferIsEmpty()) {
+    auto httpRequest = make_unique<HttpRequest>();
+    httpRequest->setUserAgent(getOption()->get(PREF_USER_AGENT));
+    httpRequest->setRequest(getRequest());
+    httpRequest->setProxyRequest(proxyRequest_);
+    }
+    }
+    
+    class AbstractProxyRequestCommand : public AbstractCommand {
+private:
+  std::shared_ptr<Request> proxyRequest_;
+    }
+    
+    void AnnounceTier::nextEventIfAfterStarted()
+{
+  switch (event) {
+  case STOPPED:
+    event = HALTED;
+    break;
+  case COMPLETED:
+    event = SEEDING;
+    break;
+  default:
+    break;
+  }
+}
+    
+    std::unique_ptr<AuthConfig> AuthConfig::create(std::string user,
+                                               std::string password)
+{
+  if (user.empty()) {
+    return nullptr;
+  }
+  else {
+    return make_unique<AuthConfig>(std::move(user), std::move(password));
+  }
+}
