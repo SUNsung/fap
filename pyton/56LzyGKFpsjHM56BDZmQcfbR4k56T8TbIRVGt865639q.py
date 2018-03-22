a6cc8081b1d83ee3fcf44b184de4625405f40c6e
SUNsung <weatherius@gmail.com>
2018-03-22 23:50:47 +0200

    Rewrite [56LzyGKFpsjHM56BDZmQcfbR4k56T8TbIRVGt865639q.py] File in: 2018-03-22 23:50:46

--- pyton/56LzyGKFpsjHM56BDZmQcfbR4k56T8TbIRVGt865639q.py
+++ pyton/56LzyGKFpsjHM56BDZmQcfbR4k56T8TbIRVGt865639q.py
@@ -1,3 +1,162 @@
 
         
-        
+            data_len = tf.size(raw_data)
+    batch_len = data_len // batch_size
+    data = tf.reshape(raw_data[0 : batch_size * batch_len],
+                      [batch_size, batch_len])
+    
+      Returns:
+    (N+1)-D np.array with as many rows as there are complete frames that can be
+    extracted.
+  '''
+  num_samples = data.shape[0]
+  num_frames = 1 + int(np.floor((num_samples - window_length) / hop_length))
+  shape = (num_frames, window_length) + data.shape[1:]
+  strides = (data.strides[0] * hop_length,) + data.strides
+  return np.lib.stride_tricks.as_strided(data, shape=shape, strides=strides)
+    
+      def setUp(self):
+    FLAGS.train_data = os.path.join(self.get_temp_dir(), 'test-text.txt')
+    FLAGS.eval_data = os.path.join(self.get_temp_dir(), 'eval-text.txt')
+    FLAGS.save_path = self.get_temp_dir()
+    with open(FLAGS.train_data, 'w') as f:
+      f.write(
+          '''alice was beginning to get very tired of sitting by her sister on
+          the bank, and of having nothing to do: once or twice she had peeped
+          into the book her sister was reading, but it had no pictures or
+          conversations in it, 'and what is the use of a book,' thought alice
+          'without pictures or conversations?' So she was considering in her own
+          mind (as well as she could, for the hot day made her feel very sleepy
+          and stupid), whether the pleasure of making a daisy-chain would be
+          worth the trouble of getting up and picking the daisies, when suddenly
+          a White rabbit with pink eyes ran close by her.\n''')
+      with open(FLAGS.eval_data, 'w') as f:
+        f.write('alice she rabbit once\n')
+    
+    
+class RegressionTest(tf.test.TestCase):
+  '''Test the regression examples in this directory.'''
+    
+    
+def main(_):
+  tf.logging.set_verbosity(tf.logging.INFO)
+  tf.gfile.MakeDirs(FLAGS.eval_dir)
+  tf.logging.info('Building eval graph...')
+  output = graphs.get_model().eval_graph(FLAGS.eval_data)
+  eval_ops, moving_averaged_variables = output
+    
+    
+class GraphsTest(tf.test.TestCase):
+  '''Test graph construction methods.'''
+    
+    
+if __name__ == '__main__':
+  tf.app.run()
+
+    
+    filegroup(
+    name = 'all_files',
+    srcs = glob(
+        ['**/*'],
+        exclude = [
+            '**/METADATA',
+            '**/OWNERS',
+        ],
+    ),
+    visibility = ['//tensorflow:__subpackages__'],
+)
+
+    
+    licenses(['notice'])  # Apache 2.0
+    
+    # Make coding more python3-ish
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+    
+    
+def get_group_vars(groups):
+    
+    
+collect_ignore = [
+    # deprecated or moved modules
+    'scrapy/conf.py',
+    'scrapy/stats.py',
+    'scrapy/project.py',
+    'scrapy/utils/decorator.py',
+    'scrapy/statscol.py',
+    'scrapy/squeue.py',
+    'scrapy/log.py',
+    'scrapy/dupefilter.py',
+    'scrapy/command.py',
+    'scrapy/linkextractor.py',
+    'scrapy/spider.py',
+    
+            if opts.pidfile:
+            with open(opts.pidfile, 'w') as f:
+                f.write(str(os.getpid()) + os.linesep)
+    
+    # If true, '()' will be appended to :func: etc. cross-reference text.
+#add_function_parentheses = True
+    
+        def __init__(self, args):
+        '''Initializes the plugin with the given command line args'''
+        self._temp_dir = tempfile.mkdtemp()
+        self.le_config = util.create_le_config(self._temp_dir)
+        config_dir = util.extract_configs(args.configs, self._temp_dir)
+        self._configs = [
+            os.path.join(config_dir, config)
+            for config in os.listdir(config_dir)]
+    
+    
+print('\n# ======================================================================')
+print('#                   Calculating pi, 10000 iterations')
+print('# ======================================================================\n')
+    
+    FILETYPES = [
+    (re.compile('Xorg'), RGBColorDB),
+    (re.compile('XConsortium'), RGBColorDB),
+    (re.compile('HTML'), HTML40DB),
+    (re.compile('lightlink'), LightlinkDB),
+    (re.compile('Websafe'), WebsafeDB),
+    ]
+    
+            contents = []
+        method_items = sorted(methods.items())
+        for key, value in method_items:
+            contents.append(self.docroutine(value, key, funcs=fdict))
+        result = result + self.bigsection(
+            'Methods', '#ffffff', '#eeaa77', ''.join(contents))
+    
+        def proxyval(self, visited):
+        # Guard against infinite loops:
+        if self.as_address() in visited:
+            return ProxyAlreadyVisited('(...)')
+        visited.add(self.as_address())
+        arg_proxy = self.pyop_field('args').proxyval(visited)
+        return ProxyException(self.safe_tp_name(),
+                              arg_proxy)
+    
+                if point.name == 'p':
+                link = point.find('a')
+                if link is not None:
+                    link = clean_pdf_link(link.attrs['href'])
+                    ext = get_extension(link)
+                    print(ext)
+                    if not ext in forbidden_extensions:
+                        print(shorten_title(point.text) + ' (' + link + ')')
+                        try:
+                            name = clean_text(point.text.split('[' + ext + ']')[0])
+                            fullname = '.'.join((name, ext))
+                            if not os.path.exists('/'.join((current_directory, fullname)) ):
+                                download_pdf(link, current_directory, '.'.join((name, ext)))
+                        except KeyboardInterrupt:
+                            try:
+                                print('Press Ctrl-C in 1 second to quit')
+                                time.sleep(1)
+                            except KeyboardInterrupt:
+                                print('Cancelling..')
+                                break
+                        except:
+                            failures.append(point.text)
+                        
+        point = point.next_sibling          
