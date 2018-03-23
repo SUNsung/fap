3ba00f557092acd1c67671aa18970f87d2764c08
SUNsung <weatherius@gmail.com>
2018-03-24 00:00:39 +0200

    Rewrite [30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb] File in: 2018-03-24 00:00:38

--- ruby/30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb
+++ ruby/30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb
@@ -1,93 +1,26 @@
 
         
-          # Allows a bottle tag to specify a specific OS or later,
-  # so the same bottle can target multiple OSs.
-  # Not used in core, used in taps.
-  def find_or_later_tag(tag)
-    begin
-      tag_version = MacOS::Version.from_symbol(tag)
-    rescue ArgumentError
-      return
+          def test_replace
+    ENV['foo'] = 'xxx'
+    ENV.replace({'foo'=>'bar', 'baz'=>'qux'})
+    check(ENV.to_hash.to_a, [%w(foo bar), %w(baz qux)])
+  end
+    
+        iset = Set.new.compare_by_identity
+    assert_send([iset, :compare_by_identity?])
+    iset.merge(array)
+    assert_equal(5, iset.size)
+    assert_equal(array.map(&:object_id).sort, iset.map(&:object_id).sort)
+    
+      Ruby = Struct.new(:version, :platform)
+    
+        thread.join
+    ScratchPad.recorded.should == nil
+  end
+    
+      it 'does not call #initialize' do
+    c = Class.new(Thread) do
+      def initialize
+        ScratchPad.record :bad
+      end
     end
-    
-      def <<(o)
-    @settings << o
-    self
-  end
-    
-    # This formula serves as the base class for several very similar
-# formulae for Amazon Web Services related tools.
-class AmazonWebServicesFormula < Formula
-  # Use this method to peform a standard install for Java-based tools,
-  # keeping the .jars out of HOMEBREW_PREFIX/lib
-  def install
-    rm Dir['bin/*.cmd'] # Remove Windows versions
-    libexec.install Dir['*']
-    bin.install_symlink Dir['#{libexec}/bin/*'] - ['#{libexec}/bin/service']
-  end
-  alias_method :standard_install, :install
-    
-    BASE_URI = ENV['BASE_URI'] || 'https://github.com/jondot/awesome-react-native'
-    
-    unless invalids.empty?
-  puts '\n\nFailed links:'
-  invalids.each do |link|
-    puts '- #{link}'
-  end
-  puts 'Done with errors.'
-  exit(1)
-end
-    
-    
-    
-    
-# replace the stuff
-replaces.uniq!
-replaces.each { |arr|
-	#puts '%32s: %s -> %s' % arr
-	asm.gsub!(arr[1], arr[2])
-}
-    
-    #certCN cannot contain commas
-certCN 		= 'Metasploit Inc.'
-#keytoolOpts 	= '-genkey -alias signFiles -keystore msfkeystore ' +
-#		  '-storepass msfstorepass -dname \'cn=#{certCN}\' ' +
-#		  '-keypass msfkeypass'
-    
-    	def initialize(filename)
-		unless filename.empty?
-			self.file = File.new(filename)
-		else
-			self.file = STDIN
-		end
-    
-              if @address.update_attributes(address_params)
-            respond_with(@address, default_template: :show)
-          else
-            invalid_resource!(@address)
-          end
-        end
-    
-          expect('.margin-implied-left').to have_rule(rule)
-    end
-  end
-    
-      context 'called with arguments (2, $value: 4em 6em)' do
-    it 'outputs sextuple the second value from the default scale' do
-      expect('.two-double-value').to have_rule('font-size: 3.125em')
-    end
-  end
-end
-
-    
-          expect('.padding-alternate').to have_rule(rule)
-    end
-  end
-    
-          expect('.position-explicit').to have_ruleset(ruleset)
-    end
-  end
-    
-      context 'called with auto' do
-    it 'applies to auto to height' do
-      rule = 'height: auto; width: 100px;'
