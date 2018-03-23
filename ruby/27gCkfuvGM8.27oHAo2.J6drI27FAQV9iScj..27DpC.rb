75680263502c88b378a5cda8fe05f4e6b9741c58
SUNsung <weatherius@gmail.com>
2018-03-23 20:30:37 +0200

    Rewrite [27gCkfuvGM8.27oHAo2.J6drI27FAQV9iScj..27DpC.rb] File in: 2018-03-23 20:30:37

--- ruby/27gCkfuvGM8.27oHAo2.J6drI27FAQV9iScj..27DpC.rb
+++ ruby/27gCkfuvGM8.27oHAo2.J6drI27FAQV9iScj..27DpC.rb
@@ -1,32 +1,12 @@
 
         
-            def initialize(*args)
-      if args.empty?
-        super(*Array.new(9))
-      elsif args.length == 1 && args.first.is_a?(Hash)
-        args.first.assert_valid_keys URI::Generic::COMPONENT
-        super(*args.first.values_at(*URI::Generic::COMPONENT))
-      else
-        super
-      end
-    end
+                self.description = <<-DESC
+          Shows the content of the pods cache as a YAML tree output, organized by pod.
+          If `NAME` is given, only the caches for that pod will be included in the output.
+        DESC
     
-            css('.toplang', '#quickview', '.top').remove
+            def print_version
+          output_pipe.puts 'version: '#{Pod::VERSION}''
+        end
     
-          def fetch_public_key_from_json(string, jwt)
-        json = JSON.parse(string)
-        keys = json['keys']
-        public_key = get_key_from_kid(keys, jwt.header['kid'])
-        public_key
-      end
-    
-            raise t('error.invalid_stage_name', name: invalid, path: stage_config_path.join('#{invalid}.rb'))
-      end
-    end
-  end
-end
-
-    
-      tasks_dir = Pathname.new('lib/capistrano/tasks')
-  config_dir = Pathname.new('config')
-  deploy_dir = config_dir.join('deploy')
+            #----------------------------------------#
