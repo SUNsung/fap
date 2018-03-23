545500dda5eeb680334c3034f65d6dd97bd71ae7
SUNsung <weatherius@gmail.com>
2018-03-23 03:34:26 +0200

    Rewrite [36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py] File in: 2018-03-23 03:34:26

--- pyton/36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py
+++ pyton/36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py
@@ -1,54 +1,168 @@
 
         
-            def getChild(self, request, name):
-        return self
+          def update_snapshot_name(self, var_coll_name):
+    var_list = self._metagraph.collection_def[var_coll_name]
+    for i, value in enumerate(var_list.bytes_list.value):
+      var_def = variable_pb2.VariableDef()
+      var_def.ParseFromString(value)
+      # Somehow node Model/global_step/read doesn't have any fanout and seems to
+      # be only used for snapshot; this is different from all other variables.
+      if var_def.snapshot_name != 'Model/global_step/read:0':
+        var_def.snapshot_name = with_autoparallel_prefix(
+            0, var_def.snapshot_name)
+      value = var_def.SerializeToString()
+      var_list.bytes_list.value[i] = value
     
-            spider_loader = self.crawler_process.spider_loader
+      def testBuildFeaturePlaceholders(self):
+    # One time series feature.
+    config = configdict.ConfigDict({
+        'time_feature_1': {
+            'length': 14,
+            'is_time_series': True,
+        }
+    })
+    expected_shapes = {
+        'time_series_features': {
+            'time_feature_1': [None, 14],
+        },
+        'aux_features': {}
+    }
+    features = input_ops.build_feature_placeholders(config)
+    self.assertFeatureShapesEqual(expected_shapes, features)
     
-        def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
-                          help='set spider argument (may be repeated)')
-        parser.add_option('-o', '--output', metavar='FILE',
-                          help='dump scraped items into FILE (use - for stdout)')
-        parser.add_option('-t', '--output-format', metavar='FORMAT',
-                          help='format to use for dumping items with -o')
+      Returns:
+    Dictionary containing 'time_series_features' and 'aux_features'. Each is a
+        dictionary of named numpy arrays of shape [batch_size, length].
+  '''
+  features = {}
+  features['time_series_features'] = {
+      name: np.random.random([batch_size, spec['length']])
+      for name, spec in feature_spec.items() if spec['is_time_series']
+  }
+  features['aux_features'] = {
+      name: np.random.random([batch_size, spec['length']])
+      for name, spec in feature_spec.items() if not spec['is_time_series']
+  }
+  return features
     
-        def run(self, args, opts):
-        if opts.list:
-            self._list_templates()
-            return
-        if opts.dump:
-            template_file = self._find_template(opts.dump)
-            if template_file:
-                with open(template_file, 'r') as f:
-                    print(f.read())
-            return
-        if len(args) != 2:
-            raise UsageError()
+      if quarters is None:
+    quarters = quarter_prefixes.keys()
     
-            general form:
-        @returns request(s)/item(s) [min=1 [max]]
     
-    # http://docs.readthedocs.org/en/latest/theme.html#how-do-i-use-this-locally-and-on-read-the-docs
-# on_rtd is whether we are on readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+def _landscape_client():
+    env = EnvironmentConfig()
+    return API(
+        uri=env.uri,
+        access_key=env.access_key,
+        secret_key=env.secret_key,
+        ssl_ca_file=env.ssl_ca_file)
     
-        @mock.patch(
-        'certbot_compatibility_test.validator.crypto_util.probe_sni')
-    def test_certificate_failure(self, mock_probe_sni):
-        cert = OpenSSL.crypto.X509()
-        cert.set_serial_number(1337)
-        mock_probe_sni.return_value = OpenSSL.crypto.X509()
-        self.assertFalse(self.validator.certificate(
-            cert, 'test.com', '127.0.0.1'))
+        parser.add_argument('--env',
+                        nargs=2,
+                        metavar=('KEY', 'VALUE'),
+                        action='append',
+                        help='environment variable to pass')
     
-    # Add any extra paths that contain custom files (such as robots.txt or
-# .htaccess) here, relative to this directory. These files are copied
-# directly to the root of the documentation.
-#html_extra_path = []
+    #############################################
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+    
+    
+@keras_test
+def test_sequential_temporal_sample_weights():
+    (x_train, y_train), (x_test, y_test), (sample_weight, class_weight, test_ids) = _get_test_data()
+    
+        # by setting the `trainable` argument, in Sequential
+    model = Sequential()
+    layer = Dense(2, input_dim=1)
+    model.add(layer)
+    assert model.trainable_weights == layer.trainable_weights
+    layer.trainable = False
+    assert model.trainable_weights == []
+    
+    
+if __name__ == '__main__':
+    pytest.main([__file__])
+
+    
+    model = Sequential()
+model.add(Embedding(max_features, embedding_size, input_length=maxlen))
+model.add(Dropout(0.25))
+model.add(Conv1D(filters,
+                 kernel_size,
+                 padding='valid',
+                 activation='relu',
+                 strides=1))
+model.add(MaxPooling1D(pool_size=pool_size))
+model.add(LSTM(lstm_output_size))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+    
+    
+@pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
+def test_orthogonal(tensor_shape):
+    _runner(initializers.orthogonal(), tensor_shape,
+            target_mean=0.)
+    
+    
+@keras_test
+def test_convert_weights():
+    def get_model(shape, data_format):
+        model = Sequential()
+        model.add(Conv2D(filters=2,
+                         kernel_size=(4, 3),
+                         input_shape=shape,
+                         data_format=data_format))
+        model.add(Flatten())
+        model.add(Dense(5))
+        return model
+    
+        model = Sequential([
+        layers.Dense(16, input_shape=(x_train.shape[-1],), activation='tanh'),
+        layers.Dense(num_classes)
+    ])
+    
+        buf = surface.get_data()
+    a = np.frombuffer(buf, np.uint8)
+    a.shape = (h, w, 4)
+    a = a[:, :, 0]  # grab single channel
+    a = a.astype(np.float32) / 255
+    a = np.expand_dims(a, 0)
+    if rotate:
+        a = image.random_rotation(a, 3 * (w - top_left_x) / w + 1)
+    a = speckle(a)
+    
+    This network is used to predict the next frame of an artificially
+generated movie which contains moving squares.
+'''
+from keras.models import Sequential
+from keras.layers.convolutional import Conv3D
+from keras.layers.convolutional_recurrent import ConvLSTM2D
+from keras.layers.normalization import BatchNormalization
+import numpy as np
+import pylab as plt
+    
+                if point.name == 'p':
+                link = point.find('a')
+                if link is not None:
+                    link = clean_pdf_link(link.attrs['href'])
+                    ext = get_extension(link)
+                    print(ext)
+                    if not ext in forbidden_extensions:
+                        print(shorten_title(point.text) + ' (' + link + ')')
+                        try:
+                            name = clean_text(point.text.split('[' + ext + ']')[0])
+                            fullname = '.'.join((name, ext))
+                            if not os.path.exists('/'.join((current_directory, fullname)) ):
+                                download_pdf(link, current_directory, '.'.join((name, ext)))
+                        except KeyboardInterrupt:
+                            try:
+                                print('Press Ctrl-C in 1 second to quit')
+                                time.sleep(1)
+                            except KeyboardInterrupt:
+                                print('Cancelling..')
+                                break
+                        except:
+                            failures.append(point.text)
+                        
+        point = point.next_sibling          
