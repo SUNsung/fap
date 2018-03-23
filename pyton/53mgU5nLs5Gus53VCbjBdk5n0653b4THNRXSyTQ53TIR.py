ba349ac8b9ae90ad8abf8d32edb554acd1660d45
SUNsung <weatherius@gmail.com>
2018-03-24 01:55:05 +0200

    Rewrite [53mgU5nLs5Gus53VCbjBdk5n0653b4THNRXSyTQ53TIR.py] File in: 2018-03-24 01:55:05

--- pyton/53mgU5nLs5Gus53VCbjBdk5n0653b4THNRXSyTQ53TIR.py
+++ pyton/53mgU5nLs5Gus53VCbjBdk5n0653b4THNRXSyTQ53TIR.py
@@ -1,138 +1,47 @@
 
         
-            exc = Timeout('Request timed out')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR_TIMEOUT
-    assert error_msg == 'Request timed out (30s).'
+                name, domain = args[0:2]
+        module = sanitize_module_name(name)
+    
+            no_gamma_ds = self.get_or_create(
+            DruidDatasource, {'datasource_name': 'datasource_not_for_gamma'},
+            db.session)
+        no_gamma_ds.cluster = cluster
+        db.session.merge(no_gamma_ds)
+        db.session.commit()
+    
+    # If false, no module index is generated.
+#texinfo_domain_indices = True
+    
+        def test_subselect(self):
+        query = '''
+          SELECT sub.*
+              FROM (
+                    SELECT *
+                      FROM s1.t1
+                     WHERE day_of_week = 'Friday'
+                   ) sub, s2.t2
+          WHERE sub.resolution = 'NONE'
+        '''
+        self.assertEquals({'s1.t1', 's2.t2'},
+                          self.extract_tables(query))
+    
+        worker = celery_app.Worker(optimization='fair')
+    worker.start()
+    
+    def downgrade():
+    constraint = find_constraint_name(False) or 'fk_columns_datasource_name_datasources'
+    with op.batch_alter_table('columns',
+        naming_convention=naming_convention) as batch_op:
+        batch_op.drop_constraint(constraint, type_='foreignkey')
+        batch_op.create_foreign_key(
+            'fk_columns_column_name_datasources',
+            'datasources',
+            ['column_name'], ['datasource_name'])
 
     
+    # revision identifiers, used by Alembic.
+revision = '18e88e1cc004'
+down_revision = '430039611635'
     
-def test_follow_redirect_output_options(httpbin):
-    r = http('--check-status',
-             '--follow',
-             '--all',
-             '--print=h',
-             '--history-print=H',
-             httpbin.url + '/redirect/2')
-    assert r.count('GET /') == 2
-    assert 'HTTP/1.1 302 FOUND' not in r
-    assert HTTP_OK in r
-    
-    
-def load_json_preserve_order(s):
-    return json.loads(s, object_pairs_hook=OrderedDict)
-    
-            (2016-01, shopping), 25
-        (2016-01, shopping), 100
-        (2016-01, gas), 50
-        '''
-        timestamp, seller, amount = line.split('\t')
-        period = self. extract_year_month(timestamp)
-        if period == self.current_year_month():
-            yield (period, category), amount
-    
-            (2016-01, url0), 1
-        (2016-01, url0), 1
-        (2016-01, url1), 1
-        '''
-        url = self.extract_url(line)
-        period = self.extract_year_month(line)
-        yield (period, url), 1
-    
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer),
-            self.mr(mapper=self.mapper_sort,
-                    reducer=self.reducer_identity),
-        ]
-    
-    
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/', None),
-    'acme': ('https://acme-python.readthedocs.org/en/latest/', None),
-    'certbot': ('https://certbot.eff.org/docs/', None),
-}
-
-    
-        @mock.patch('certbot.notify.smtplib.LMTP')
-    @mock.patch('certbot.notify.subprocess.Popen')
-    def test_smtp_failure(self, mock_popen, mock_lmtp):
-        from certbot.notify import notify
-        lmtp_obj = mock.MagicMock()
-        mock_lmtp.return_value = lmtp_obj
-        lmtp_obj.sendmail.side_effect = socket.error(17)
-        proc = mock.MagicMock()
-        mock_popen.return_value = proc
-        self.assertTrue(notify('Goose', 'auntrhody@example.com',
-                               'The old grey goose is dead.'))
-        self.assertEqual(lmtp_obj.sendmail.call_count, 1)
-        self.assertEqual(proc.communicate.call_count, 1)
-    
-        def __init__(self, tokenSource=None, channel=DEFAULT_CHANNEL):
-        '''
-        @param tokenSource A TokenSource instance (usually a Lexer) to pull
-            the tokens from.
-    
-            self.decisionNumber = decisionNumber
-        self.eot = eot
-        self.eof = eof
-        self.min = min
-        self.max = max
-        self.accept = accept
-        self.special = special
-        self.transition = transition
-    
-    
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write('Hello, world')
-    
-        def test_non_reversible(self):
-        # URLSpecs are non-reversible if they include non-constant
-        # regex features outside capturing groups. Currently, this is
-        # only strictly enforced for backslash-escaped character
-        # classes.
-        paths = [
-            r'^/api/v\d+/foo/(\w+)$',
-        ]
-        for path in paths:
-            # A URLSpec can still be created even if it cannot be reversed.
-            url_spec = url(path, None)
-            try:
-                result = url_spec.reverse()
-                self.fail('did not get expected exception when reversing %s. '
-                          'result: %s' % (path, result))
-            except ValueError:
-                pass
-    
-    
-globals().update(wrap_web_tests_application())
-    
-    print('Starting')
-if 'TESTAPP_STARTED' not in os.environ:
-    os.environ['TESTAPP_STARTED'] = '1'
-    sys.stdout.flush()
-    autoreload._reload()
-'''
-    
-    
-@gen.coroutine
-def put(filenames):
-    client = httpclient.AsyncHTTPClient()
-    for filename in filenames:
-        mtype = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
-        headers = {'Content-Type': mtype}
-        producer = partial(raw_producer, filename)
-        url_path = quote(os.path.basename(filename))
-        response = yield client.fetch('http://localhost:8888/%s' % url_path,
-                                      method='PUT',
-                                      headers=headers,
-                                      body_producer=producer)
-        print(response)
-    
-        print('Start on 8888')
-    application.listen(8888, '127.0.0.1')
+        urls = []
