dff19647a729a993a68d5a9afe32350dbb6f3bea
SUNsung <weatherius@gmail.com>
2018-03-23 03:00:59 +0200

    Rewrite [03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py] File in: 2018-03-23 03:00:59

--- pyton/03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py
+++ pyton/03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py
@@ -1,147 +1,98 @@
 
         
-            def _finish(self, request):
-        self.concurrent -= 1
-        if not request.finished and not request._disconnected:
-            request.finish()
+                self.decisionNumber = decisionNumber
+        self.eot = eot
+        self.eof = eof
+        self.min = min
+        self.max = max
+        self.accept = accept
+        self.special = special
+        self.transition = transition
     
-        def help(self):
-        '''An extensive help for the command. It will be shown when using the
-        'help' command. It can contain newlines, since not post-formatting will
-        be applied to its contents.
-        '''
-        return self.long_desc()
+            elif isinstance(e, EarlyExitException):
+            msg = 'required (...)+ loop did not match anything at character ' \
+                  + self.getCharErrorDisplay(e.c)
+            
+        elif isinstance(e, MismatchedNotSetException):
+            msg = 'mismatched character ' \
+                  + self.getCharErrorDisplay(e.c) \
+                  + ' expecting set ' \
+                  + repr(e.expecting)
     
-    
-if __name__ == '__main__':
-    unittest.main()  # pragma: no cover
-
-    
-        def deploy_cert(self, domain, cert_path, key_path, chain_path=None,
-                    fullchain_path=None):
-        '''Installs cert'''
-        cert_path, key_path, chain_path = self.copy_certs_and_keys(
-            cert_path, key_path, chain_path)
-        self._configurator.deploy_cert(
-            domain, cert_path, key_path, chain_path, fullchain_path)
-
-    
-    # If true, SmartyPants will be used to convert quotes and dashes to
-# typographically correct entities.
-#html_use_smartypants = True
-    
-        @mock.patch('certbot.display.enhancements.util')
-    def test_easy(self, mock_util):
-        mock_util().menu.return_value = (display_util.OK, 0)
-        self.assertFalse(self._call())
-    
-    import mock
-    
-        description = 'Example Installer plugin'
-    
-            if cmd.startswith('#'):
-            log.write('%s' % cmd)
-            continue
-    
-        if address_family == socket.AF_INET:
-        if len(packed_ip) != ctypes.sizeof(addr.ipv4_addr):
-            raise socket.error('packed IP wrong length for inet_ntoa')
-        ctypes.memmove(addr.ipv4_addr, packed_ip, 4)
-    elif address_family == socket.AF_INET6:
-        if len(packed_ip) != ctypes.sizeof(addr.ipv6_addr):
-            raise socket.error('packed IP wrong length for inet_ntoa')
-        ctypes.memmove(addr.ipv6_addr, packed_ip, 16)
-    else:
-        raise socket.error('unknown address family')
-    
-    from tornado.options import define, options
+                    if c >= self.min[s] and c <= self.max[s]:
+                    # move to next state
+                    snext = self.transition[s][c-self.min[s]]
+                    #print 'in range, next state = %d' % snext
     
     
-class LifoQueueJoinTest(QueueJoinTest):
-    queue_class = queues.LifoQueue
+class EventNotification( BaseRequest ):
+  def __init__( self, event_name, buffer_number = None, extra_data = None ):
+    super( EventNotification, self ).__init__()
+    self._event_name = event_name
+    self._buffer_number = buffer_number
+    self._extra_data = extra_data
+    self._response_future = None
+    self._cached_response = None
     
     
-def wrap_web_tests_application():
-    result = {}
-    for cls in web_test.wsgi_safe_tests:
-        class WSGIApplicationWrappedTest(cls):  # type: ignore
-            def get_app(self):
-                self.app = WSGIApplication(self.get_handlers(),
-                                           **self.get_app_kwargs())
-                return WSGIContainer(validator(self.app))
-        result['WSGIApplication_' + cls.__name__] = WSGIApplicationWrappedTest
-    return result
-    
-            p = Popen(
-            [sys.executable, '-m', 'testapp'], stdout=subprocess.PIPE,
-            cwd=path, env=dict(os.environ, PYTHONPATH=pythonpath),
-            universal_newlines=True)
-        out = p.communicate()[0]
-        self.assertEqual(out, 'Starting\nStarting\n')
-
-    
-        @gen_test
-    def test_garbage_collection(self):
-        # Test that timed-out waiters are occasionally cleaned from the queue.
-        sem = locks.Semaphore(value=0)
-        futures = [sem.acquire(timedelta(seconds=0.01)) for _ in range(101)]
-    
-    # Increasing --n without --keepalive will eventually run into problems
-# due to TIME_WAIT sockets
-define('n', type=int, default=15000)
-define('c', type=int, default=25)
-define('keepalive', type=bool, default=False)
-define('quiet', type=bool, default=False)
-    
-        @gen.coroutine
-    def post(self):
-        if self.any_author_exists():
-            raise tornado.web.HTTPError(400, 'author already created')
-        hashed_password = yield executor.submit(
-            bcrypt.hashpw, tornado.escape.utf8(self.get_argument('password')),
-            bcrypt.gensalt())
-        author_id = self.db.execute(
-            'INSERT INTO authors (email, name, hashed_password) '
-            'VALUES (%s, %s, %s)',
-            self.get_argument('email'), self.get_argument('name'),
-            hashed_password)
-        self.set_secure_cookie('blogdemo_user', str(author_id))
-        self.redirect(self.get_argument('next', '/'))
+def FormatDebugInfoResponse( response ):
+  if not response:
+    return 'Server errored, no debug info from server\n'
+  message = _FormatYcmdDebugInfo( response )
+  completer = response[ 'completer' ]
+  if completer:
+    message += _FormatCompleterDebugInfo( completer )
+  return message
     
     
-class MessageUpdatesHandler(tornado.web.RequestHandler):
-    @gen.coroutine
-    def post(self):
-        cursor = self.get_argument('cursor', None)
-        # Save the future returned by wait_for_messages so we can cancel
-        # it in wait_for_messages
-        self.future = global_message_buffer.wait_for_messages(cursor=cursor)
-        messages = yield self.future
-        if self.request.connection.stream.closed():
-            return
-        self.write(dict(messages=messages))
+GENERIC_RESPONSE = {
+  'clang': {
+    'has_support': True,
+    'version': 'Clang version'
+  },
+  'completer': {
+    'items': [
+      {
+        'key': 'key',
+        'value': 'value'
+      }
+    ],
+    'name': 'Completer name',
+    'servers': [
+      {
+        'address': '127.0.0.1',
+        'executable': '/path/to/executable',
+        'extras': [
+          {
+            'key': 'key',
+            'value': 'value'
+          }
+        ],
+        'is_running': True,
+        'logfiles': [
+          '/path/to/stdout/logfile',
+          '/path/to/stderr/logfile'
+        ],
+        'name': 'Server name',
+        'pid': 12345,
+        'port': 1234
+      }
+    ]
+  },
+  'extra_conf': {
+    'is_loaded': False,
+    'path': '/path/to/extra/conf'
+  },
+  'python': {
+    'executable': '/path/to/python/interpreter',
+    'version': 'Python version'
+  }
+}
     
-        def current_oauth_client(self):
-        client = self.stacked_proxy_safe_get(c, 'oauth2_client', None)
-        return getattr(client, '_id', None)
     
-    api('reltableitem', RelTableItemJsonTemplate)
-api('bannedtableitem', BannedTableItemJsonTemplate)
-api('mutedtableitem', MutedTableItemJsonTemplate)
-api('invitedmodtableitem', InvitedModTableItemJsonTemplate)
-api('friendtableitem', FriendTableItemJsonTemplate)
-    
-    
-    def send400(self):
-        if 'usable_error_content' in request.environ:
-            return request.environ['usable_error_content']
-        else:
-            res = pages.RedditError(
-                title=_('bad request (%(domain)s)') % dict(domain=g.domain),
-                message=_('you sent an invalid request'),
-                explanation=request.GET.get('explanation'))
-            return res.render()
-    
-    from pylons import app_globals as g
-from pylons import tmpl_context as c
-from pylons import request
+def setUpPackage():
+  # We treat warnings as errors in our tests because warnings raised inside Vim
+  # will interrupt user workflow with a traceback and we don't want that.
+  warnings.filterwarnings( 'error' )
+  # We ignore warnings from nose as we are not interested in them.
+  warnings.filterwarnings( 'ignore', module = 'nose' )
