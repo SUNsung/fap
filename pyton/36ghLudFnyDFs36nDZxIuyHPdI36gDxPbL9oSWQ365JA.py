0283fffb489553b68a654c1fc53f7779eb02b8ea
SUNsung <weatherius@gmail.com>
2018-03-23 07:35:13 +0200

    Rewrite [36ghLudFnyDFs36nDZxIuyHPdI36gDxPbL9oSWQ365JA.py] File in: 2018-03-23 07:35:13

--- pyton/36ghLudFnyDFs36nDZxIuyHPdI36gDxPbL9oSWQ365JA.py
+++ pyton/36ghLudFnyDFs36nDZxIuyHPdI36gDxPbL9oSWQ365JA.py
@@ -1,29 +1,117 @@
 
         
-        def sort_blocks():
-    # First, we load the current README into memory
-    with open('README.md', 'r') as read_me_file:
-        read_me = read_me_file.read()
+        
+def maybe_restore_pretrained_model(sess, saver_for_restore, model_dir):
+  '''Restores pretrained model if there is no ckpt model.'''
+  ckpt = tf.train.get_checkpoint_state(FLAGS.train_dir)
+  checkpoint_exists = ckpt and ckpt.model_checkpoint_path
+  if checkpoint_exists:
+    tf.logging.info('Checkpoint exists in FLAGS.train_dir; skipping '
+                    'pretraining restore')
+    return
     
-        description = proj_info['description'],
-    keywords = proj_info['keywords'],
+      @tf.test.mock.patch.dict(premade_estimator.__dict__,
+                           {'load_data': four_lines_data})
+  def test_premade_estimator(self):
+    premade_estimator.main([None, '--train_steps=1'])
     
-    def kugou_download_playlist(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    html=get_html(url)
-    pattern=re.compile('title='(.*?)'.* data='(\w*)\|.*?'')
-    pairs=pattern.findall(html)
-    for title,hash_val in pairs:
-        kugou_download_by_hash(title,hash_val,output_dir,merge,info_only)
+      def __init__(self,
+               batch,
+               state_name=None,
+               tokens=None,
+               num_states=0,
+               eos_id=None):
+    '''Construct VatxtInput.
     
     
+if __name__ == '__main__':
+  tf.app.run()
+
     
-    from ..common import *
     
-    from xml.dom.minidom import parseString
+def main(_):
+  '''Trains LSTM classification model.'''
+  tf.logging.set_verbosity(tf.logging.INFO)
+  with tf.device(tf.train.replica_device_setter(FLAGS.ps_tasks)):
+    model = graphs.get_model()
+    train_op, loss, global_step = model.classifier_training()
+    train_utils.run_training(
+        train_op,
+        loss,
+        global_step,
+        variables_to_restore=model.pretrained_variables,
+        pretrained_model_dir=FLAGS.pretrained_model_dir)
     
-                    for line in text:
-                    if re.match(r'^>', line):
-                        line = line[:0] + '####' + line[1:]
-                    html = markdown.markdown(line)
-                    out.write(html)
-                out.write(footer)
+        # Loads the final mapping of integer node ID to human-readable string
+    node_id_to_name = {}
+    for key, val in node_id_to_uid.items():
+      if val not in uid_to_human:
+        tf.logging.fatal('Failed to locate: %s', val)
+      name = uid_to_human[val]
+      node_id_to_name[key] = name
+    
+    Input: '535+61'
+Output: '596'
+Padding is handled by using a repeated sentinel character (space)
+    
+    try:
+    path = get_file('babi-tasks-v1-2.tar.gz', origin='https://s3.amazonaws.com/text-datasets/babi_tasks_1-20_v1-2.tar.gz')
+except:
+    print('Error downloading dataset, please download it manually:\n'
+          '$ wget http://www.thespermwhale.com/jaseweston/babi/tasks_1-20_v1-2.tar.gz\n'
+          '$ mv tasks_1-20_v1-2.tar.gz ~/.keras/datasets/babi-tasks-v1-2.tar.gz')
+    raise
+    
+        # Returns
+        Preprocessed tensor.
+    '''
+    global _IMAGENET_MEAN
+    
+        def parse(self, response):
+        for link in self.link_extractor.extract_links(response):
+            yield scrapy.Request(link.url, callback=self.parse)
+
+    
+        description = 'Example Authenticator plugin'
+    
+    
+def main():
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([
+        (r'/', MainHandler),
+    ])
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+    
+    
+def handle_sigchld(sig, frame):
+    IOLoop.current().add_callback_from_signal(IOLoop.current().stop)
+    
+    
+class ChunkHandler(RequestHandler):
+    def get(self):
+        for i in xrange(options.num_chunks):
+            self.write('A' * options.chunk_size)
+            self.flush()
+        self.finish()
+    
+    
+class MessageBuffer(object):
+    def __init__(self):
+        self.waiters = set()
+        self.cache = []
+        self.cache_size = 200
+    
+        @classmethod
+    def send_updates(cls, chat):
+        logging.info('sending message to %d waiters', len(cls.waiters))
+        for waiter in cls.waiters:
+            try:
+                waiter.write_message(chat)
+            except:
+                logging.error('Error sending message', exc_info=True)
+    
+    
+class FixFutureImports(fixer_base.BaseFix):
+    BM_compatible = True
