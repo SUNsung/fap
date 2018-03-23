98d1b3b0668e36e343cd379101df78c62dfa300d
SUNsung <weatherius@gmail.com>
2018-03-23 04:31:48 +0200

    Rewrite [21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb] File in: 2018-03-23 04:31:47

--- ruby/21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb
+++ ruby/21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb
@@ -1,94 +1,65 @@
-        # Checks whether the record requires any confirmation.
-        def pending_any_confirmation
-          if (!confirmed? || pending_reconfirmation?)
-            yield
-          else
-            self.errors.add(:email, :already_confirmed)
-            false
-          end
-        end
-    
-      # Check if there is no signed in user before doing the sign out.
-  #
-  # If there is no signed in user, it will set the flash message and redirect
-  # to the after_sign_out path.
-  def verify_signed_out_user
-    if all_signed_out?
-      set_flash_message! :notice, :already_signed_out
-    
-      # Small method that adds a mapping to Devise.
-  def self.add_mapping(resource, options)
-    mapping = Devise::Mapping.new(resource, options)
-    @@mappings[mapping.name] = mapping
-    @@default_scope ||= mapping.name
-    @@helpers.each { |h| h.define_helpers(mapping) }
-    mapping
-  end
-    
-    module Devise
-  # Failure application that will be called every time :warden is thrown from
-  # any strategy or hook. It is responsible for redirecting the user to the sign
-  # in page based on current scope and mapping. If no scope is given, it
-  # redirects to the default_url.
-  class FailureApp < ActionController::Metal
-    include ActionController::UrlFor
-    include ActionController::Redirecting
-    
-            if b_length > a_length
-          (b_length - a_length).times { a_split.insert(-2, 0) }
-        elsif a_length > b_length
-          (a_length - b_length).times { b_split.insert(-2, 0) }
-        end
-    
-        def merge(hash)
-      return super unless hash.is_a? Hash
-      dup.merge!(hash)
-    end
-    
-            css('.note h3', '.warning h3').each do |node|
-          node.before('<p><strong>#{node.inner_html}</strong></p>').remove
-        end
-    
-              node.css('.method').each do |n|
-            next unless n.at_css('dt[id]')
-            name = n.at_css('.descname').content
-            name = '#{class_name}::#{name}()'
-            id = n.at_css('dt[id]')['id']
-            entries << [name, id]
-          end
-        end
-    
-        def split_colon_path(path)
-      one, two = path.split(':', 2)
-      if one && two && Sass::Util.windows? &&
-          one =~ /\A[A-Za-z]\Z/ && two =~ %r{\A[/\\]}
-        # If we're on Windows and we were passed a drive letter path,
-        # don't split on that colon.
-        one2, two = two.split(':', 2)
-        one = one + ':' + one2
-      end
-      return one, two
-    end
-    
-            Readline::HISTORY << text
-        parse_input(environment, text)
-      end
+
+        
+            def schedule_in(*args, &blk)
+      schedule(:schedule_in, args, &blk)
     end
     
         respond_to do |format|
-      if @book.save
-        format.html { redirect_to @book, notice: 'Book was successfully created.' }
-        format.json { render :show, status: :created, location: @book }
-      else
-        format.html { render :new }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
+      format.html { redirect_to user_credentials_path }
+      format.json { head :no_content }
+    end
+  end
+    
+        def doc_mtime(doc)
+      [@store.mtime(doc.index_path).to_i, @store.mtime(doc.db_path).to_i].max
+    end
+    
+            css('> .section', '#preamble', 'a[href*='dict.html']', 'code var', 'code strong').each do |node|
+          node.before(node.children).remove
+        end
+    
+    lib_path = root.join('lib').to_path
+    
+      fd.close
+    
+        cal = Hash.new
+    cal[ver] = Hash.new
+    
+          def assert_valid_stage_names(names)
+        invalid = names.find { |n| RESERVED_NAMES.include?(n) }
+        return if invalid.nil?
+    
+        def default_tasks
+      %w{install}
+    end
+    
+      # Implemented by subclasses to provide default values for settings needed by
+  # this plugin. Typically done using the `set_if_empty` Capistrano DSL method.
+  #
+  # Example:
+  #
+  #   def set_defaults
+  #     set_if_empty :my_plugin_option, true
+  #   end
+  #
+  def set_defaults; end
+    
+      entries.each do |entry|
+    if File.exist?(entry[:file])
+      warn '[skip] #{entry[:file]} already exists'
+    else
+      File.open(entry[:file], 'w+') do |f|
+        f.write(ERB.new(File.read(entry[:template])).result(binding))
+        puts I18n.t(:written_file, scope: :capistrano, file: entry[:file])
       end
     end
   end
     
-            def literal_at_end?(regex_str)
-          # is this regexp 'literal' in the sense of only matching literal
-          # chars, rather than using metachars like . and * and so on?
-          # also, is it anchored at the end of the string?
-          regex_str =~ /\A(?:#{LITERAL_REGEX})+\\z\z/
-        end
+      def command_line(*options)
+    options.each { |opt| ARGV << opt }
+    subject.define_singleton_method(:exit) do |*_args|
+      throw(:system_exit, :exit)
+    end
+    subject.run
+    subject.options
+  end
