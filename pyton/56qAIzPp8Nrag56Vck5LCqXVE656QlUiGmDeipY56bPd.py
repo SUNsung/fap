f8f2568139d2303ed5049c3168eca088525902ef
SUNsung <weatherius@gmail.com>
2018-03-24 21:09:30 +0200

    Rewrite [56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py] File in: 2018-03-24 21:09:30

--- pyton/56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py
+++ pyton/56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py
@@ -1,10 +1,31 @@
 
         
-        try:
-    import json
-except ImportError:
-    import simplejson as json
+        
+def get_line_value(r, n):
+    rls = r.split('\r\n')
+    if len(rls) < n + 1:
+        return None
     
+        if address_family == socket.AF_INET:
+        if len(packed_ip) != ctypes.sizeof(addr.ipv4_addr):
+            raise socket.error('packed IP wrong length for inet_ntoa')
+        ctypes.memmove(addr.ipv4_addr, packed_ip, 4)
+    elif address_family == socket.AF_INET6:
+        if len(packed_ip) != ctypes.sizeof(addr.ipv6_addr):
+            raise socket.error('packed IP wrong length for inet_ntoa')
+        ctypes.memmove(addr.ipv6_addr, packed_ip, 16)
+    else:
+        raise socket.error('unknown address family')
     
-def sort_groups(groups):
-    return sorted(groups, key=lambda g: (g.depth, g.priority, g.name))
+    - tree.CommonTreeNodeStream: A basic and most commonly used tree.TreeNodeStream
+  implementation.
+  
+    
+    	# The current Token when an error occurred.  Since not all streams
+	# can retrieve the ith Token, we have to track the Token object.
+	# For parsers.  Even when it's a tree parser, token might be set.
+        self.token = None
+    
+        This is an abstract class that must be implemented by a subclass.
+    
+    '''
