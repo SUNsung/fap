fff5bb80d2141f8c6371d924e131705e38c41f8b
SUNsung <weatherius@gmail.com>
2018-03-24 10:26:14 +0200

    Rewrite [00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb] File in: 2018-03-24 10:26:13

--- ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
+++ ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
@@ -1,46 +1,88 @@
-      if options[:type] == :array
-        options[:roles] << :completable
-        class_eval <<-EOF
-          def complete_#{name}
-            #{options[:values]}.map { |v| {text: v, id: v} }
+
+        
+            def as_json
+      @pages
+    end
+    
+        def merge!(hash)
+      return super unless hash.is_a? Hash
+      hash.assert_valid_keys URI::Generic::COMPONENT
+      hash.each_pair do |key, value|
+        send '#{key}=', value
+      end
+      self
+    end
+    
+              # Underscore methods
+          if name.start_with?('Underscore')
+            node.at_css('~ ul').css('li').each do |li|
+              name = [type.downcase, li.at_css('a').content.split.first].join('.')
+              id = name.parameterize
+              li['id'] = id
+              entries << [name, id, type]
+            end
+            next
           end
-        EOF
-      end
     
-      def table_sort
-    raise('You must call set_table_sort in any action using table_sort.') unless @table_sort_info.present?
-    @table_sort_info[:order]
-  end
-    
-        respond_to do |format|
-      format.html
-      format.json { render json: @services }
-    end
-  end
-    
-    Struct.new('Blog', :name, :web_url, :rss_url)
-blogs = []
-    
-          opts.on('-F', '--from FORMAT',
-        'The format to convert from. Can be css, scss, sass.',
-        'By default, this is inferred from the input filename.',
-        'If there is none, defaults to css.') do |name|
-        @options[:from] = name.downcase.to_sym
-        raise 'sass-convert no longer supports LessCSS.' if @options[:from] == :less
-        unless [:css, :scss, :sass].include?(@options[:from])
-          raise 'Unknown format for sass-convert --from: #{name}'
+              css('##{dom_id}-methods ~ h4 code').each do |node|
+            next unless name = node.content[/\('(\w+)'\)/, 1]
+            id = node.parent['id'] = '#{dom_id}-#{name.parameterize}-method'
+            name.prepend '#{dom_id.singularize.titleize}: '
+            name << ' (method)'
+            entries << [name, id]
+          end
         end
-      end
     
-            {
-          :always_update     => false,
-          :template_location => root + '/public/stylesheets/sass',
-          :css_location      => root + '/public/stylesheets',
-          :cache_location    => root + '/tmp/sass-cache',
-          :always_check      => env != 'production',
-          :quiet             => env != 'production',
-          :full_exception    => env != 'production'
-        }.freeze
+      subject { described_class.new }
+    
+      describe 'PUT #update' do
+    it 'updates notifications settings' do
+      user.settings['notification_emails'] = user.settings['notification_emails'].merge('follow' => false)
+      user.settings['interactions'] = user.settings['interactions'].merge('must_be_follower' => true)
+    
+          group.each_key do |key|
+        map[codepoints_to_unicode(key)] = codepoints_to_filename(existing_one)
       end
     end
+    
+          unless leaf.respond_to?(:empty?) && !leaf.empty?
+        raise CaskError, 'Cannot create metadata subdir for empty leaf.'
+      end
+    
+      # Creates a delayed logger wrapping `inner`.
+  #
+  # @param inner [Sass::Logger::Base] The wrapped logger.
+  def initialize(inner)
+    self.log_level = inner.log_level
+    @inner = inner
+    @messages = []
   end
+    
+        # The trailing expressions in the query.
+    #
+    # When parsed as Sass code, each expression contains strings and SassScript
+    # nodes. When parsed as CSS, each one contains a single string.
+    #
+    # @return [Array<Array<String, Sass::Script::Tree::Node>>]
+    attr_accessor :expressions
+    
+      require 'sass/plugin/rack'
+  class Sass::Plugin::MerbBootLoader < Merb::BootLoader
+    after Merb::BootLoader::RackUpApplication
+    
+            def self.options
+          [[
+            '--short', 'Only print the path relative to the cache root'
+          ]].concat(super)
+        end
+    
+          def self.options
+        options = []
+        options.concat(super.reject { |option, _| option == '--silent' })
+      end
+    
+          def initialize(argv)
+        @update = argv.flag?('update')
+        @stats  = argv.flag?('stats')
+        super
+      end
