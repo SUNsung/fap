bfc59fd3a2af80bcb6fa1a903f9d3c15ca85395f
SUNsung <weatherius@gmail.com>
2018-03-24 15:07:21 +0200

    Rewrite [12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb] File in: 2018-03-24 15:07:21

--- ruby/12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb
+++ ruby/12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb
@@ -1,84 +1,60 @@
 
         
-            # This setting makes it so that network access from inside the vagrant guest
-    # is able to resolve DNS using the hosts VPN connection.
-    v.customize ['modifyvm', :id, '--natdnshostresolver1', 'on']
-  end
+            context 'while processing' do
+      setup do
+        clear_dest
+        @site.config['title'] = 'Test Site'
+        @page = setup_page('physical.html', :base => test_dir('fixtures'))
+      end
     
-          topic.feature_topic_users(args)
-    end
+    def dest_dir(*subdirs)
+  test_dir('dest', *subdirs)
+end
     
-    module Jobs
-    
-    BASE_URI = ENV['BASE_URI'] || 'https://github.com/jondot/awesome-react-native'
-    
-            return a.casecmp(b) if a_length == 1 && b_length == 1
-        return 1 if a_length == 1
-        return -1 if b_length == 1
-    
-          unless root?
-        raise Invalid, 'missing name' if !name || name.empty?
-        raise Invalid, 'missing path' if !path || path.empty?
-        raise Invalid, 'missing type' if !type || type.empty?
+        def filter(entries)
+      entries.reject do |e|
+        unless included?(e)
+          special?(e) || backup?(e) || excluded?(e) || symlink?(e)
+        end
       end
     end
     
-        class << self
-      attr_accessor :dir
-    
-        # we assume that the first file that requires 'sinatra' is the
-    # app_file. all other path related options are calculated based
-    # on this path by default.
-    set :app_file, caller_files.first || $0
-    
-          def html?(headers)
-        return false unless header = headers.detect { |k,v| k.downcase == 'content-type' }
-        options[:html_types].include? header.last[/^\w+\/\w+/]
+    If you run into trouble, you can find helpful resources at https://jekyllrb.com/help/!
+            MSG
+            raise Jekyll::Errors::MissingDependencyException, name
+          end
+        end
       end
     end
   end
 end
 
     
-            modes       = Array options[:escape]
-        @escaper    = options[:escaper]
-        @html       = modes.include? :html
-        @javascript = modes.include? :javascript
-        @url        = modes.include? :url
+          # Group an array of items by an expression
+      #
+      # input - the object array
+      # variable - the variable to assign each item to in the expression
+      # expression -a Liquid comparison expression passed in as a string
+      #
+      # Returns the filtered array of objects
+      def group_by_exp(input, variable, expression)
+        return input unless groupable?(input)
     
-        def assign_file_information
-      instance_write(:file_name, cleanup_filename(@file.original_filename))
-      instance_write(:content_type, @file.content_type.to_s.strip)
-      instance_write(:file_size, @file.size)
-    end
+      # The test environment is used exclusively to run your application's
+  # test suite. You never need to work with it otherwise. Remember that
+  # your test database is 'scratch space' for the test suite and is wiped
+  # and recreated between test runs. Don't rely on the data there!
+  config.cache_classes = true
     
-        # Extracts the Geometry from a 'WxH,O' string
-    # Where W is the width, H is the height,
-    # and O is the EXIF orientation
-    def self.parse(string)
-      GeometryParser.new(string).make
-    end
-    
-            def responds?
-          methods = @subject.instance_methods.map(&:to_s)
-          methods.include?('#{@attachment_name}') &&
-            methods.include?('#{@attachment_name}=') &&
-            methods.include?('#{@attachment_name}?')
-        end
-    
-            def rejecting *types
-          @rejected_types = types.flatten
-          self
-        end
-    
-    
-    {  # Returns hash with styles missing from recent run of rake paperclip:refresh:missing_styles
-  #   {
-  #     :User => {:avatar => [:big]},
-  #     :Book => {
-  #       :cover => [:croppable]},
-  #     }
-  #   }
-  def self.missing_attachments_styles
-    current_styles = current_attachments_styles
-    registered_styles = get_registered_attachments_styles
+    def list_login_items_for_app(app_path)
+  out, err, status = Open3.capture3(
+    '/usr/bin/osascript', '-e',
+    'tell application \'System Events\' to get the name of every login item ' \
+    'whose path contains \'#{File.basename(app_path)}\''
+  )
+  if status.exitstatus > 0
+    $stderr.puts err
+    exit status.exitstatus
+  end
+  puts out.gsub(', ', '\n')
+end
