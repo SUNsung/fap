45c08dba269ea3ac558fd3937cfa74faade9643f
SUNsung <weatherius@gmail.com>
2018-03-24 03:49:25 +0200

    Rewrite [35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py] File in: 2018-03-24 03:49:24

--- pyton/35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py
+++ pyton/35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py
@@ -1,178 +1,137 @@
 
         
-        # TODO: ensure that history changes.
-
-    
-        r = client.get('/hello')
-    assert r.status_code == 200
-    
-    
-@keras_test
-def test_sequential_sample_weights():
-    model = create_sequential_model()
-    model.compile(loss=loss, optimizer='rmsprop')
-    
-        reference = Sequential()
-    reference.add(wrappers.TimeDistributed(layers.Embedding(5, 6),
-                                           input_shape=(3, 4), dtype='int32'))
-    reference.compile(optimizer='rmsprop', loss='mse')
-    reference.layers[0].set_weights(weights)
-    
-        # a Model inside a Model
+            # by setting the `trainable` argument, in Model
     x = Input(shape=(1,))
-    y = Dense(2)(x)
-    inner_model = Model(x, y)
-    x = Input(shape=(1,))
-    y = inner_model(x)
-    outer_model = Model(x, y)
-    assert outer_model.trainable_weights == inner_model.trainable_weights
-    inner_model.trainable = False
-    assert outer_model.trainable_weights == []
-    inner_model.trainable = True
-    inner_model.layers[-1].trainable = False
-    assert outer_model.trainable_weights == []
+    layer = Dense(2)
+    y = layer(x)
+    model = Model(x, y)
+    assert model.trainable_weights == layer.trainable_weights
+    layer.trainable = False
+    assert model.trainable_weights == []
+    
+            layer_test(local.LocallyConnected2D,
+                   kwargs={'filters': filters,
+                           'kernel_size': (3, 3),
+                           'padding': padding,
+                           'kernel_regularizer': 'l2',
+                           'bias_regularizer': 'l2',
+                           'activity_regularizer': 'l2',
+                           'strides': strides,
+                           'data_format': 'channels_first'},
+                   input_shape=(num_samples, stack_size, num_row, num_col))
+    
+        model = Sequential()
+    model.add(layers.GRU(8,
+                         input_shape=(x_train.shape[1], x_train.shape[2])))
+    model.add(layers.Dense(y_train.shape[-1], activation='softmax'))
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+    model.summary()
+    history = model.fit(x_train, y_train, epochs=4, batch_size=10,
+                        validation_data=(x_test, y_test),
+                        verbose=0)
+    assert(history.history['acc'][-1] >= 0.8)
+    config = model.get_config()
+    model = Sequential.from_config(config)
+    
+        model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test))
+    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=2, validation_split=0.1)
+    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=0)
+    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, shuffle=False)
     
     
-@keras_test
-def test_masking():
-    np.random.seed(1337)
-    x = np.array([[[1], [1]],
-                  [[0], [0]]])
-    model = Sequential()
-    model.add(Masking(mask_value=0, input_shape=(2, 1)))
-    model.add(TimeDistributed(Dense(1, kernel_initializer='one')))
-    model.compile(loss='mse', optimizer='sgd')
-    y = np.array([[[1], [1]],
-                  [[1], [1]]])
-    loss = model.train_on_batch(x, y)
-    assert loss == 0
-    
-        # prime the model with 'ab' sequence and let it generate the learned alphabet
-    sentence = alphabet[:sequence_length]
-    generated = sentence
-    for iteration in range(number_of_chars - sequence_length):
-        x = np.zeros((1, sequence_length, number_of_chars))
-        for t, char in enumerate(sentence):
-            x[0, t, ord(char) - ord('a')] = 1.
-        preds = model.predict(x, verbose=0)[0]
-        next_char = chr(np.argmax(preds) + ord('a'))
-        generated += next_char
-        sentence = sentence[1:] + next_char
-    
-        # Arguments
-        config: dict of the form {'class_name': str, 'config': dict}
-        custom_objects: dict mapping class names (or function names)
-            of custom (non-Keras) objects to class/functions
-    
-        right = Sequential(name='branch_2')
-    right.add(Dense(num_hidden, input_shape=(input_dim,), name='dense_2'))
-    right.add(Activation('relu', name='relu_2'))
-    
-            self.assertTrue(self.addr_default.conflicts(self.addr))
-        self.assertTrue(self.addr_default.conflicts(self.addr1))
-        self.assertTrue(self.addr_default.conflicts(self.addr_defined))
-    
-        def __init__(self, args):
-        '''Initializes the plugin with the given command line args'''
-        self._temp_dir = tempfile.mkdtemp()
-        self.le_config = util.create_le_config(self._temp_dir)
-        config_dir = util.extract_configs(args.configs, self._temp_dir)
-        self._configs = [
-            os.path.join(config_dir, config)
-            for config in os.listdir(config_dir)]
-    
-        def tearDown(self):
-        logging.disable(logging.NOTSET)
-    
-        expect = textwrap.dedent('''\
-    [
-        [
-            'blorpie'
-        ],
-        [
-            'whoops'
-        ],
-        [],
-        'd-shtaeou',
-        'd-nthiouh',
-        'i-vhbjkhnth',
-        {
-            'nifty': 87
-        },
-        {
-            'morefield': false,
-            'field': 'yes'
-        }
-    ]
-    ''')
+def plot_feature_times(all_times, batch_size, all_components, data):
+    plt.figure()
+    plot_results(all_components, all_times['pca'], label='PCA')
+    plot_results(all_components, all_times['ipca'],
+                 label='IncrementalPCA, bsize=%i' % batch_size)
+    plot_results(all_components, all_times['rpca'], label='RandomizedPCA')
+    plt.legend(loc='upper left')
+    plt.suptitle('Algorithm runtime vs. n_components\n \
+                 LFW, size %i x %i' % data.shape)
+    plt.xlabel('Number of components (out of max %i)' % data.shape[1])
+    plt.ylabel('Time (seconds)')
     
     
-    def test_attributes(self):
-        # test the start, stop and step attributes of range objects
-        self.assert_attrs(range(0), 0, 0, 1)
-        self.assert_attrs(range(10), 0, 10, 1)
-        self.assert_attrs(range(-10), 0, -10, 1)
-        self.assert_attrs(range(0, 10, 1), 0, 10, 1)
-        self.assert_attrs(range(0, 10, 3), 0, 10, 3)
-        self.assert_attrs(range(10, 0, -1), 10, 0, -1)
-        self.assert_attrs(range(10, 0, -3), 10, 0, -3)
+def bench_sample(sampling, n_population, n_samples):
+    gc.collect()
+    # start time
+    t_start = datetime.now()
+    sampling(n_population, n_samples)
+    delta = (datetime.now() - t_start)
+    # stop time
+    time = compute_time(t_start, delta)
+    return time
     
-            # COM methods are boolean True:
-        if sys.platform == 'win32':
-            mth = WINFUNCTYPE(None)(42, 'name', (), None)
-            self.assertEqual(bool(mth), True)
+        print('============================================')
+    print('Warning: this is going to take a looong time')
+    print('============================================')
     
-        @gen.coroutine
-    def resolve(self, host, port, family=0):
-        if is_valid_ip(host):
-            addresses = [host]
-        else:
-            # gethostbyname doesn't take callback as a kwarg
-            self.channel.gethostbyname(host, family, (yield gen.Callback(1)))
-            callback_args = yield gen.Wait(1)
-            assert isinstance(callback_args, gen.Arguments)
-            assert not callback_args.kwargs
-            result, error = callback_args.args
-            if error:
-                raise IOError('C-Ares returned error %s: %s while resolving %s' %
-                              (error, pycares.errno.strerror(error), host))
-            addresses = result.addresses
-        addrinfo = []
-        for address in addresses:
-            if '.' in address:
-                address_family = socket.AF_INET
-            elif ':' in address:
-                address_family = socket.AF_INET6
-            else:
-                address_family = socket.AF_UNSPEC
-            if family != socket.AF_UNSPEC and family != address_family:
-                raise IOError('Requested socket family %d but got %d' %
-                              (family, address_family))
-            addrinfo.append((address_family, (address, port)))
-        raise gen.Return(addrinfo)
-
+            text_filename = os.path.join(text_lang_folder,
+                                     '%s_%04d.txt' % (lang, i))
+        print('Writing %s' % text_filename)
+        open(text_filename, 'wb').write(content.encode('utf-8', 'ignore'))
+        i += 1
     
-            # Semaphore was released and can be acquired again.
-        self.assertTrue(sem.acquire().done())
+    # Define 'classifiers' to be used
+classifiers = {
+    'Empirical Covariance': EllipticEnvelope(support_fraction=1.,
+                                             contamination=0.261),
+    'Robust Covariance (Minimum Covariance Determinant)':
+    EllipticEnvelope(contamination=0.261),
+    'OCSVM': OneClassSVM(nu=0.261, gamma=0.05)}
+colors = ['m', 'g', 'b']
+legend1 = {}
+legend2 = {}
     
+    # Author: Kemal Eren <kemal@kemaleren.com>
+# License: BSD 3 clause
     
-class ChunkHandler(RequestHandler):
-    def get(self):
-        for i in xrange(options.num_chunks):
-            self.write('A' * options.chunk_size)
-            self.flush()
-        self.finish()
+    plt.matshow(fit_data, cmap=plt.cm.Blues)
+plt.title('After biclustering; rearranged to show biclusters')
+    
+    n_classifiers = len(classifiers)
+    
+    plt.plot(features_samples_ratio, acc_clf1, linewidth=2,
+         label='Linear Discriminant Analysis with shrinkage', color='navy')
+plt.plot(features_samples_ratio, acc_clf2, linewidth=2,
+         label='Linear Discriminant Analysis', color='gold')
+    
+        >>> import tornado.escape
+    >>> import_object('tornado.escape') is tornado.escape
+    True
+    >>> import_object('tornado.escape.utf8') is tornado.escape.utf8
+    True
+    >>> import_object('tornado') is tornado
+    True
+    >>> import_object('tornado.missing_module')
+    Traceback (most recent call last):
+        ...
+    ImportError: No module named missing_module
+    '''
+    if not isinstance(name, str):
+        # on python 2 a byte string is required.
+        name = name.encode('utf-8')
+    if name.count('.') == 0:
+        return __import__(name, None, None)
+    
+        .. versionchanged:: 5.0
+       The ``io_loop`` argument (deprecated since version 4.1) has been removed.
+    '''
+    def initialize(self):
+        self.io_loop = IOLoop.current()
+        self.channel = pycares.Channel(sock_state_cb=self._sock_state_cb)
+        self.fds = {}
     
     
 def main():
-    base_cmd = [
-        sys.executable, '-m', 'timeit', '-s',
-        'from stack_context_benchmark import StackBenchmark, ExceptionBenchmark']
-    cmds = [
-        'StackBenchmark().enter_exit(50)',
-        'StackBenchmark().call_wrapped(50)',
-        'StackBenchmark().enter_exit(500)',
-        'StackBenchmark().call_wrapped(500)',
+    parse_command_line()
+    t = Timer(e1)
+    results = t.timeit(options.num) / options.num
+    print('engine: %0.3f ms per iteration' % (results * 1000))
+    t = Timer(c1)
+    results = t.timeit(options.num) / options.num
+    print('coroutine: %0.3f ms per iteration' % (results * 1000))
     
-        def open(self):
-        ChatSocketHandler.waiters.add(self)
+        q.put(base_url)
