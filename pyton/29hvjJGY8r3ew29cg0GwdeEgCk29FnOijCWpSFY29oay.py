c4f5e964ce74df36babd32bf3c6d01cd1c66ec8a
SUNsung <weatherius@gmail.com>
2018-03-26 01:37:47 +0300

    Rewrite [29hvjJGY8r3ew29cg0GwdeEgCk29FnOijCWpSFY29oay.py] File in: 2018-03-26 01:37:46

--- pyton/29hvjJGY8r3ew29cg0GwdeEgCk29FnOijCWpSFY29oay.py
+++ pyton/29hvjJGY8r3ew29cg0GwdeEgCk29FnOijCWpSFY29oay.py
@@ -1,222 +1,110 @@
 
         
-        
-atom_template = textwrap.dedent('''\
-    <?xml version='1.0' encoding='utf-8'?>
-    <feed xmlns='http://www.w3.org/2005/Atom'>
-        <link rel='self' href='http://rg3.github.io/youtube-dl/update/releases.atom' />
-        <title>youtube-dl releases</title>
-        <id>https://yt-dl.org/feed/youtube-dl-updates-feed</id>
-        <updated>@TIMESTAMP@</updated>
-        @ENTRIES@
-    </feed>''')
-    
-    import shutil
-    
-            self.port = random.randint(20000, 30000)
-        self.server_process = subprocess.Popen([
-            'srelay', '-f', '-i', '127.0.0.1:%d' % self.port],
-            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
-    
-class RtspFD(FileDownloader):
-    def real_download(self, filename, info_dict):
-        url = info_dict['url']
-        self.report_destination(filename)
-        tmpfilename = self.temp_name(filename)
-    
-    
-def gen_extractor_classes():
-    ''' Return a list of supported extractors.
-    The order does matter; the first extractor matched is the one handling the URL.
-    '''
-    return _ALL_CLASSES
-    
-            return {
-            '_type': 'playlist',
-            'id': playlist_id,
-            'title': title,
-            'description': description,
-            'entries': entries,
-        }
-
-    
-        h = history.history
-    assert_array_almost_equal(h['val_loss'], h['val_' + loss_full_name], decimal=decimal)
-    assert_array_almost_equal(h['val_loss'], h['val_weighted_' + loss_full_name], decimal=decimal)
-    
-        # by setting the `trainable` argument, in Model
-    x = Input(shape=(1,))
-    layer = Dense(2)
-    y = layer(x)
-    model = Model(x, y)
-    assert model.trainable_weights == layer.trainable_weights
-    layer.trainable = False
-    assert model.trainable_weights == []
-    
-    
-def style_loss(style, combination):
-    assert K.ndim(style) == 3
-    assert K.ndim(combination) == 3
-    S = gram_matrix(style)
-    C = gram_matrix(combination)
-    channels = 3
-    size = img_nrows * img_ncols
-    return K.sum(K.square(S - C)) / (4. * (channels ** 2) * (size ** 2))
-    
-    
-def get_example_array():
-    np.random.seed(3537)
-    example_array = np.random.random((100, 100)) * 100. - 50.
-    example_array[0, 0] = 0.  # 0 could possibly cause trouble
-    return example_array
-    
-    
-@keras_test
-def test_maxout_dense():
-    layer_test(legacy_layers.MaxoutDense,
-               kwargs={'output_dim': 3},
-               input_shape=(3, 2))
-    
-        # 4. Anything else is not a valid argument
-    with pytest.raises(ValueError):
-        a = activations.get(6)
-    
-    
-if __name__ == '__main__':
-    pytest.main([__file__])
-
-    
-        left = Sequential()
-    left.add(Dense(num_hidden, input_shape=(input_dim,)))
-    left.add(Activation('relu'))
-    
-        return a
-    
-        print('Classifier Training')
-    print('===================')
-    accuracy, train_time, test_time = {}, {}, {}
-    for name in sorted(args['estimators']):
-        clf = ESTIMATORS[name]
-        try:
-            clf.set_params(random_state=0)
-        except (TypeError, ValueError):
-            pass
-    
-    
-def plot_batch_errors(all_errors, n_features, all_batch_sizes, data):
-    plt.figure()
-    plot_results(all_batch_sizes, all_errors['pca'], label='PCA')
-    plot_results(all_batch_sizes, all_errors['ipca'], label='IncrementalPCA')
-    plt.legend(loc='lower left')
-    plt.suptitle('Algorithm error vs. batch_size for n_components %i\n \
-                 LFW, size %i x %i' % (
-                 n_features, data.shape[0], data.shape[1]))
-    plt.xlabel('Batch size')
-    plt.ylabel('Mean absolute error')
-    
-                gc.collect()
-            print('benchmarking lars_path (without Gram):', end='')
-            sys.stdout.flush()
-            tstart = time()
-            lars_path(X, y, method='lasso')
-            delta = time() - tstart
-            print('%0.3fs' % delta)
-            results['lars_path (without Gram)'].append(delta)
-    
-        for (sbplt, vals, quantity,
-         build_time, query_time) in [(311, Nrange, 'N',
-                                      N_results_build,
-                                      N_results_query),
-                                     (312, Drange, 'D',
-                                      D_results_build,
-                                      D_results_query),
-                                     (313, krange, 'k',
-                                      k_results_build,
-                                      k_results_query)]:
-        ax = plt.subplot(sbplt, yscale='log')
-        plt.grid(True)
-    
-    
-def _get_git_revision():
+            plugin_manager.register(Plugin)
     try:
-        revision = subprocess.check_output(REVISION_CMD.split()).strip()
-    except (subprocess.CalledProcessError, OSError):
-        print('Failed to execute git to get revision')
-        return None
-    return revision.decode('utf-8')
+        r = http(
+            httpbin + BASIC_AUTH_URL,
+            '--auth-type',
+            Plugin.auth_type,
+            '--auth',
+            BASIC_AUTH_HEADER_VALUE,
+        )
+        assert HTTP_OK in r
+        assert r.json == AUTH_OK
+    finally:
+        plugin_manager.unregister(Plugin)
     
-    data, row_idx, col_idx = sg._shuffle(data, random_state=0)
-plt.matshow(data, cmap=plt.cm.Blues)
-plt.title('Shuffled dataset')
+    
+filenames = list(rst_filenames())
+assert filenames
+    
+    
+def test_follow_all_output_options_used_for_redirects(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 3
+    assert HTTP_OK not in r
+    
+    
+def plot_results(X, y, label):
+    plt.plot(X, y, label=label, marker='o')
+    
+    import time
+    
+    plt.matshow(data, cmap=plt.cm.Blues)
+plt.title('Original dataset')
     
     features_samples_ratio = np.array(n_features_range) / n_train
     
     
-class Call(object):
+class _BenchSpider(scrapy.Spider):
+    '''A spider that follows all links'''
+    name = 'follow'
+    total = 10000
+    show = 20
+    baseurl = 'http://localhost:8998'
+    link_extractor = LinkExtractor()
     
-        def create_signature(self):
-        # Create signature based on url and contents
-        pass
+                request.callback = wrapper
     
-    def kuwo_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    if 'www.kuwo.cn/yinyue' in url:
-        rid=match1(url,'yinyue/(\d+)')
-        kuwo_download_by_rid(rid,output_dir, merge, info_only)
-    else:
-        kuwo_playlist_download(url,output_dir,merge,info_only)
+        def post_process(self, output):
+        occurrences = 0
+        for x in output:
+            if isinstance(x, self.obj_type):
+                occurrences += 1
     
-    #----------------------------------------------------------------------
-def sina_xml_to_url_list(xml_data):
-    '''str->list
-    Convert XML to URL List.
-    From Biligrab.
-    '''
-    rawurl = []
-    dom = parseString(xml_data)
-    for node in dom.getElementsByTagName('durl'):
-        url = node.getElementsByTagName('url')[0]
-        rawurl.append(url.childNodes[0].data)
-    return rawurl
+    ### OUTPUT ###
+# ['Student',
+#  'Student',
+#  'Scientist',
+#  'Student',
+#  'Scientist',
+#  'Student',
+#  'Scientist',
+#  'Student',
+#  'Scientist',
+#  'Student',
+#  'Scientist',
+#  'Professor']
+
     
+        def test_car_adapter_shall_make_very_loud_noise(self):
+        car = Car()
+        car_adapter = Adapter(car, make_noise=car.make_noise)
+        noise = car_adapter.make_noise(10)
+        expected_noise = 'vroom!!!!!!!!!!'
     
-class Waker(interface.Waker):
-    '''Create an OS independent asynchronous pipe.
+        def tearDown(cls):
+        ''' Function/test case scope teardown. '''
+        cls.output.close()
+        sys.stdout = cls.saved_stdout
     
-            auth_header = self.request.headers.get('Authorization', None)
-        if auth_header is not None:
-            auth_mode, params = auth_header.split(' ', 1)
-            assert auth_mode == 'Digest'
-            param_dict = {}
-            for pair in params.split(','):
-                k, v = pair.strip().split('=', 1)
-                if v[0] == ''' and v[-1] == ''':
-                    v = v[1:-1]
-                param_dict[k] = v
-            assert param_dict['realm'] == realm
-            assert param_dict['opaque'] == opaque
-            assert param_dict['nonce'] == nonce
-            assert param_dict['username'] == username
-            assert param_dict['uri'] == self.request.path
-            h1 = md5(utf8('%s:%s:%s' % (username, realm, password))).hexdigest()
-            h2 = md5(utf8('%s:%s' % (self.request.method,
-                                     self.request.path))).hexdigest()
-            digest = md5(utf8('%s:%s:%s' % (h1, nonce, h2))).hexdigest()
-            if digest == param_dict['response']:
-                self.write('ok')
-            else:
-                self.write('fail')
-        else:
-            self.set_status(401)
-            self.set_header('WWW-Authenticate',
-                            'Digest realm='%s', nonce='%s', opaque='%s'' %
-                            (realm, nonce, opaque))
+        def test_items_recoil(self):
+        with ObjectPool(self.sample_queue, True) as pool:
+            self.assertEqual(pool, 'first')
+        self.assertTrue(self.sample_queue.get() == 'second')
+        self.assertFalse(self.sample_queue.empty())
+        self.assertTrue(self.sample_queue.get() == 'first')
+        self.assertTrue(self.sample_queue.empty())
     
-            # Prevent 'Future exception was never retrieved' messages.
-        for future in futures:
-            self.assertRaises(TimeoutError, future.result)
+        def test_parrot_eng_localization(self):
+        self.assertEqual(self.e.get('parrot'), 'parrot')
     
-            yield [connect(c) for c in clients]
+        def test_tc2_output(self):
+        self.tc2.run()
+        output = self.out.getvalue().strip()
+        self.assertEqual(output, self.average_result_tc2)
     
-        def _log(self):
-        pass
+    Test code which will almost always fail (if not exactly 12:01) when untestable
+production code (production code time provider is datetime) is used:
+    
+        def now(self):
+        current_time = datetime.datetime.now()
+        current_time_formatted = '{}:{}'.format(current_time.hour, current_time.minute)
+        return current_time_formatted
+    
+        def execute(self):
+        self.rename(self.src, self.dest)
