2a68c1f1574645fd58da5dd37280b5c6086303ee
SUNsung <weatherius@gmail.com>
2018-03-26 13:38:28 +0300

    Rewrite [287wQA2GA7xNM28JG8kRyUjwQ.288dIPvD3Dki.28ZsS.py] File in: 2018-03-26 13:38:28

--- pyton/287wQA2GA7xNM28JG8kRyUjwQ.288dIPvD3Dki.28ZsS.py
+++ pyton/287wQA2GA7xNM28JG8kRyUjwQ.288dIPvD3Dki.28ZsS.py
@@ -1,145 +1,126 @@
 
         
-            try:
-        s2 = os.lstat(parent)
-    except OSError:
-        # one should handle the returned OSError with more care to figure
-        # out whether this is still a mount
-        return False
+            def test_tumblr(self):
+        self.assertMatch('http://tatianamaslanydaily.tumblr.com/post/54196191430/orphan-black-dvd-extra-behind-the-scenes', ['Tumblr'])
+        self.assertMatch('http://tatianamaslanydaily.tumblr.com/post/54196191430', ['Tumblr'])
     
-    # Note, sha1 is the only hash algorithm compatible with python2.4 and with
-# FIPS-140 mode (as of 11-2014)
-try:
-    from hashlib import sha1 as sha1
-except ImportError:
-    from sha import sha as sha1
     
-            params1 = {'one': 1}
-        params2 = {'two': 2}
-        actual = GCPUtils.are_params_equal(params1, params2)
-        self.assertFalse(actual)
+def gen_extractor_classes():
+    ''' Return a list of supported extractors.
+    The order does matter; the first extractor matched is the one handling the URL.
+    '''
+    return _ALL_CLASSES
     
-        def get(self, key):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                return item.value
-        raise KeyError('Key not found')
+            webpage = self._download_webpage(url, video_id)
+        key = self._search_regex(
+            r'src=['\']https?://[^/]+/embed/([A-Za-z0-9_-]+)', webpage, 'key')
     
-        def mapper(self, _, line):
-        '''Parse each log line, extract and transform relevant lines.
+            duration = parse_duration(self._search_regex(
+            r'<b>Duration:</b> (?:<q itemprop='duration'>)?(\d+:\d+)', webpage, 'duration', fatal=False))
+        view_count = int_or_none(self._html_search_regex(
+            r'<b>Views:</b> (\d+)', webpage, 'view count', fatal=False))
     
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer),
-            self.mr(mapper=self.mapper_sort,
-                    reducer=self.reducer_identity),
-        ]
+        :param url: URL for the new :class:`Request` object.
+    :param data: (optional) Dictionary (will be form-encoded), bytes, or file-like object to send in the body of the :class:`Request`.
+    :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    '''
     
-        def take_call(self, call):
-        '''Assume the employee will always successfully take the call.'''
-        self.call = call
-        self.call.employee = self
-        self.call.state = CallState.IN_PROGRESS
+        def get(self, key, default=None):
+        return self.__dict__.get(key, default)
+
     
-            When updating an entry, updates its position to the front of the LRU list.
-        If the entry is new and the cache is at capacity, removes the oldest entry
-        before the new entry is added.
+        print('20 newsgroups')
+    print('=============')
+    print('X_train.shape = {0}'.format(X_train.shape))
+    print('X_train.format = {0}'.format(X_train.format))
+    print('X_train.dtype = {0}'.format(X_train.dtype))
+    print('X_train density = {0}'
+          ''.format(X_train.nnz / np.product(X_train.shape)))
+    print('y_train {0}'.format(y_train.shape))
+    print('X_test {0}'.format(X_test.shape))
+    print('X_test.format = {0}'.format(X_test.format))
+    print('X_test.dtype = {0}'.format(X_test.dtype))
+    print('y_test {0}'.format(y_test.shape))
+    print()
+    
+    FORMATS = {
+    'sequences': lambda y: [list(np.flatnonzero(s)) for s in y],
+    'dense': lambda y: y,
+    'csr': lambda y: sp.csr_matrix(y),
+    'csc': lambda y: sp.csc_matrix(y),
+}
+    
+        # decode the payload explicitly as UTF-8 since lxml is confused for some
+    # reason
+    with codecs.open(html_filename,'r','utf-8') as html_file:
+        html_content = html_file.read()
+    tree = ElementTree(lxml.html.document_fromstring(html_content))
+    i = 0
+    j = 0
+    for p in tree.findall('//p'):
+        content = p.text_content()
+        if len(content) < 100:
+            # skip paragraphs that are too short - probably too noisy and not
+            # representative of the actual language
+            continue
+    
+    # Train uncalibrated random forest classifier on whole train and validation
+# data and evaluate on test data
+clf = RandomForestClassifier(n_estimators=25)
+clf.fit(X_train_valid, y_train_valid)
+clf_probs = clf.predict_proba(X_test)
+score = log_loss(y_test, clf_probs)
+    
+    if (twisted_version.major, twisted_version.minor, twisted_version.micro) >= (15, 5, 0):
+    collect_ignore += _py_files('scrapy/xlib/tx')
+    
+    
+class _BenchServer(object):
+    
+            assert len(self.args) in [1, 2, 3]
+        self.obj_name = self.args[0] or None
+        self.obj_type = self.objects[self.obj_name]
+    
+        def __init__(self, template_categories_to_budget_map):
+        self.categories_to_budget_map = template_categories_to_budget_map
+    
+    
+if __name__ == '__main__':
+    HitCounts.run()
+
+    
+    
+class Page(object):
+    
+            Accessing a node updates its position to the front of the LRU list.
         '''
         node = self.lookup[query]
-        if node is not None:
-            # Key exists in cache, update the value
-            node.results = results
-            self.linked_list.move_to_front(node)
-        else:
-            # Key does not exist in cache
-            if self.size == self.MAX_SIZE:
-                # Remove the oldest entry from the linked list and lookup
-                self.lookup.pop(self.linked_list.tail.query, None)
-                self.linked_list.remove_from_tail()
-            else:
-                self.size += 1
-            # Add the new key and value
-            new_node = Node(results)
-            self.linked_list.append_to_front(new_node)
-            self.lookup[query] = new_node
-
+        if node is None:
+            return None
+        self.linked_list.move_to_front(node)
+        return node.results
     
-        def __init__(self, license_plate):
-        super(Car, self).__init__(VehicleSize.COMPACT, license_plate, spot_size=1)
-    
-        def create_signature(self):
-        # Create signature based on url and contents
-        pass
-    
-        def test_eq(self):
-        self.assertTrue(self.vhost1b == self.vhost1)
-        self.assertFalse(self.vhost1 == self.vhost2)
-        self.assertEqual(str(self.vhost1b), str(self.vhost1))
-        self.assertFalse(self.vhost1b == 1234)
-    
-    # If true, '()' will be appended to :func: etc. cross-reference text.
-#add_function_parentheses = True
-    
-    if __name__ == '__main__':
-    unittest.main()  # pragma: no cover
-
+        def test_bear_eng_localization(self):
+        self.assertEqual(self.e.get('bear'), 'bear')
     
     '''
-import zope.interface
+Port of the Java example of 'Constructor Injection' in
+'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
+(ISBN-10: 0131495054, ISBN-13: 978-0131495050)
     
-        def add_missing_metrics(self, metrics):
-        exisiting_metrics = {m.metric_name for m in self.metrics}
-        for metric in metrics:
-            if metric.metric_name not in exisiting_metrics:
-                metric.table_id = self.id
-                self.metrics += [metric]
+    # Test the generator
+count_to_two = lambda: count_to(2)
+count_to_five = lambda: count_to(5)
     
-        @classmethod
-    def is_dimension(cls, dtype, column_name):
-        if cls.is_id(column_name):
-            return False
-        return dtype.name in ('object', 'bool')
+        def update(self):
+        print('Updating the test results in Database')
+        time.sleep(0.1)
     
+        def __repr__(self):
+        return '<%s: %r>' % (self.__class__.__name__, self.value)
     
-def import_from_dict(session, data, sync=[]):
-    '''Imports databases and druid clusters from dictionary'''
-    if isinstance(data, dict):
-        logging.info('Importing %d %s',
-                     len(data.get(DATABASES_KEY, [])),
-                     DATABASES_KEY)
-        for database in data.get(DATABASES_KEY, []):
-            Database.import_from_dict(session, database, sync=sync)
-    
-    
-def upgrade():
-    try:
-        constraint = find_constraint_name() or 'fk_columns_column_name_datasources'
-        with op.batch_alter_table('columns',
-                naming_convention=naming_convention) as batch_op:
-            batch_op.drop_constraint(constraint, type_='foreignkey')
-            batch_op.create_foreign_key(
-                'fk_columns_datasource_name_datasources',
-                'datasources',
-                ['datasource_name'], ['datasource_name'])
-    except:
-        logging.warning(
-            'Could not find or drop constraint on `columns`')
-    
-    # revision identifiers, used by Alembic.
-revision = '1296d28ec131'
-down_revision = '6414e83d82b7'
-    
-    
-def downgrade():
-    pass
-
-    
-    
-def downgrade():
-    with op.batch_alter_table('sql_metrics') as batch_op_sql_metrics:
-        batch_op_sql_metrics.drop_column('warning_text')
-    with op.batch_alter_table('metrics') as batch_op_metrics:
-        batch_op_metrics.drop_column('warning_text')
+        def or_specification(self, candidate):
+        return OrSpecification(self, candidate)
