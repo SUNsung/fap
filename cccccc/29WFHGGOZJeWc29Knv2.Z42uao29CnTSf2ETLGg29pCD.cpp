fbb3a8818c6004a84dcfdc38ae22b0c28a559c00
SUNsung <weatherius@gmail.com>
2018-03-26 04:16:24 +0300

    Rewrite [29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp] File in: 2018-03-26 04:16:23

--- cccccc/29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp
+++ cccccc/29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp
@@ -1,372 +1,183 @@
 
         
         
-    {} // namespace swift
+/**********************************************************************
+ * LLSQ::remove
+ *
+ * Delete an element from the acculuator.
+ **********************************************************************/
     
-      /// Build the components of an Objective-C method descriptor for the given
-  /// property's method implementations.
-  void emitObjCGetterDescriptorParts(IRGenModule &IGM,
-                                     VarDecl *property,
-                                     llvm::Constant *&selectorRef,
-                                     llvm::Constant *&atEncoding,
-                                     llvm::Constant *&impl);
-    
-    
-    {  /// Returns true if \p BB is a dead-end block.
-  bool isDeadEnd(SILBasicBlock *BB) {
-    if (!isComputed) {
-      // Lazily compute the dataflow.
-      compute();
-      isComputed = true;
-    }
-    return ReachableBlocks.count(BB) == 0;
+    void QLSQ::remove(double x, double y) {
+  if (n <= 0) {
+    tprintf('Can't remove an element from an empty QLSQ accumulator!\n');
+    return;
   }
-};
-    
-    #ifndef LLVM_SOURCEKIT_LIB_SWIFTLANG_SWIFTEDITORDIAGCONSUMER_H
-#define LLVM_SOURCEKIT_LIB_SWIFTLANG_SWIFTEDITORDIAGCONSUMER_H
-    
-    #ifndef SWIFT_INDEX_INDEXSYMBOL_H
-#define SWIFT_INDEX_INDEXSYMBOL_H
-    
-    bool Substitution::isCanonical() const {
-  if (!getReplacement()->isCanonical())
-    return false;
-  for (auto conf : getConformances()) {
-    if (!conf.isCanonical())
-      return false;
-  }
-  return true;
+  n--;                           // Count elements.
+  sigx -= x;                     // Update accumulators.
+  sigy -= y;
+  sigxx -= x * x;
+  sigxy -= x * y;
+  sigyy -= y * y;
+  sigxxx -= static_cast<long double>(x) * x * x;
+  sigxxy -= static_cast<long double>(x) * x * y;
+  sigxxxx -= static_cast<long double>(x) * x * x * x;
 }
+    
+    /* Returns true if the |stream| was successfully started and is now done
+ * (succeeded, canceled, or failed).
+ * Returns false if the |stream| stream is not yet started or is in progress.
+ */
+GRPC_SUPPORT_EXPORT
+bool bidirectional_stream_is_done(bidirectional_stream* stream);
+    
+    #endif
 
     
-      string data = message.SerializeAsString();
+    #include 'test/cpp/qps/benchmark_config.h'
+#include 'test/cpp/qps/driver.h'
+#include 'test/cpp/qps/report.h'
+#include 'test/cpp/qps/server.h'
+#include 'test/cpp/util/test_config.h'
+#include 'test/cpp/util/test_credentials_provider.h'
     
     
-    {
-    {
-    {
-    {
     {}  // namespace
-}  // namespace csharp
-}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-
     
-      virtual void WriteHash(io::Printer* printer);
-  virtual void WriteEquals(io::Printer* printer);
-  virtual void WriteToString(io::Printer* printer);
-    
-    // Generator options (used by csharp_generator.cc):
-struct Options {
-  Options() :
-      file_extension('.cs'),
-      base_namespace(''),
-      base_namespace_specified(false),
-      internal_access(false) {
-  }
-  // Extension of the generated file. Defaults to '.cs'
-  string file_extension;
-  // Base namespace to use to create directory hierarchy. Defaults to ''.
-  // This option allows the simple creation of a conventional C# file layout,
-  // where directories are created relative to a project-specific base
-  // namespace. For example, in a project with a base namespace of PetShop, a
-  // proto of user.proto with a C# namespace of PetShop.Model.Shared would
-  // generate Model/Shared/User.cs underneath the specified --csharp_out
-  // directory.
-  //
-  // If no base namespace is specified, all files are generated in the
-  // --csharp_out directory, with no subdirectories created automatically.
-  string base_namespace;
-  // Whether the base namespace has been explicitly specified by the user.
-  // This is required as the base namespace can be explicitly set to the empty
-  // string, meaning 'create a full directory hierarchy, starting from the first
-  // segment of the namespace.'
-  bool base_namespace_specified;
-  // Whether the generated classes should have accessibility level of 'internal'.
-  // Defaults to false that generates 'public' classes.
-  bool internal_access;
-};
-    
-    namespace google {
-namespace protobuf {
-namespace compiler {
-namespace csharp {
-    }
-    }
+    namespace grpc {
+namespace testing {
     }
     }
     
-    RepeatedPrimitiveFieldGenerator::RepeatedPrimitiveFieldGenerator(
-    const FieldDescriptor* descriptor, int fieldOrdinal, const Options *options)
-    : FieldGeneratorBase(descriptor, fieldOrdinal, options) {
+    double UsageTimer::Now() {
+  auto ts = gpr_now(GPR_CLOCK_REALTIME);
+  return ts.tv_sec + 1e-9 * ts.tv_nsec;
 }
     
-    // Author: kenton@google.com (Kenton Varda)
-//  Based on original Protocol Buffers design by
-//  Sanjay Ghemawat, Jeff Dean, and others.
+      Result Mark() const;
     
-    #include <google/protobuf/compiler/java/java_context.h>
-#include <google/protobuf/compiler/java/java_lazy_message_field.h>
-#include <google/protobuf/compiler/java/java_doc_comment.h>
-#include <google/protobuf/compiler/java/java_helpers.h>
-#include <google/protobuf/io/printer.h>
     
-    #include 'glog/logging.h'
-#include 'google/protobuf/text_format.h'
-#include 'stdint.h'
+    {
+    {}  // namespace testing
+}  // namespace grpc
     
-      // Get a layer using a LayerParameter.
-  static shared_ptr<Layer<Dtype> > CreateLayer(const LayerParameter& param) {
-    if (Caffe::root_solver()) {
-      LOG(INFO) << 'Creating layer ' << param.name();
-    }
-    const string& type = param.type();
-    CreatorRegistry& registry = Registry();
-    CHECK_EQ(registry.count(type), 1) << 'Unknown layer type: ' << type
-        << ' (known types: ' << LayerTypeListString() << ')';
-    return registry[type](param);
+    grpc::string DescribeMethod(const grpc::protobuf::MethodDescriptor* method) {
+  std::stringstream result;
+  result << '  rpc ' << method->name()
+         << (method->client_streaming() ? '(stream ' : '(')
+         << method->input_type()->full_name() << ') returns '
+         << (method->server_streaming() ? '(stream ' : '(')
+         << method->output_type()->full_name() << ') {}\n';
+  if (method->options().deprecated()) {
+    result << ' DEPRECATED';
   }
+  return result.str();
+}
     
+    namespace xgboost {
+ConsoleLogger::~ConsoleLogger() {
+  dmlc::CustomLogMessage::Log(log_stream_.str());
+}
+TrackerLogger::~TrackerLogger() {
+  dmlc::CustomLogMessage::Log(log_stream_.str());
+}
+}  // namespace xgboost
     
-    {  bool handles_setup_;
-  cudnnHandle_t             handle_;
-  cudnnTensorDescriptor_t bottom_desc_;
-  cudnnTensorDescriptor_t top_desc_;
-  cudnnActivationDescriptor_t activ_desc_;
-};
+    namespace xgboost {
+namespace common {
+/*! \brief buffer reader of the stream that allows you to get */
+class StreamBufferReader {
+ public:
+  explicit StreamBufferReader(size_t buffer_size)
+      :stream_(NULL),
+       read_len_(1), read_ptr_(1) {
+    buffer_.resize(buffer_size);
+  }
+  /*!
+   * \brief set input stream
+   */
+  inline void set_stream(dmlc::Stream *stream) {
+    stream_ = stream;
+    read_len_ = read_ptr_ = 1;
+  }
+  /*!
+   * \brief allows quick read using get char
+   */
+  inline char GetChar(void) {
+    while (true) {
+      if (read_ptr_ < read_len_) {
+        return buffer_[read_ptr_++];
+      } else {
+        read_len_ = stream_->Read(&buffer_[0], buffer_.length());
+        if (read_len_ == 0) return EOF;
+        read_ptr_ = 0;
+      }
+    }
+  }
+  /*! \brief whether we are reaching the end of file */
+  inline bool AtEnd(void) const {
+    return read_len_ == 0;
+  }
+    }
+    }
+    }
+    
+    #else
+/*!
+ * \brief global random engine
+ */
+typedef RandomEngine GlobalRandomEngine;
 #endif
     
-    #endif  // CAFFE_CUDNN_SOFTMAX_LAYER_HPP_
-
+    /*! \brief High precision gradient statistics pair with integer backed
+ * storage. Operators are associative where floating point versions are not
+ * associative. */
+typedef detail::bst_gpair_internal<int64_t> bst_gpair_integer;
     
-    #include <vector>
-    
-    /**
- * @brief Exponential Linear Unit non-linearity @f$
- *        y = \left\{
- *        \begin{array}{lr}
- *            x                  & \mathrm{if} \; x > 0 \\
- *            \alpha (\exp(x)-1) & \mathrm{if} \; x \le 0
- *        \end{array} \right.
- *      @f$.  
- */
-template <typename Dtype>
-class ELULayer : public NeuronLayer<Dtype> {
+    class SparsePageRawFormat : public SparsePage::Format {
  public:
-  /**
-   * @param param provides ELUParameter elu_param,
-   *     with ELULayer options:
-   *   - alpha (\b optional, default 1).
-   *     the value @f$ \alpha @f$ by which controls saturation for negative inputs.
-   */
-  explicit ELULayer(const LayerParameter& param)
-      : NeuronLayer<Dtype>(param) {}
+  bool Read(SparsePage* page, dmlc::SeekStream* fi) override {
+    if (!fi->Read(&(page->offset))) return false;
+    CHECK_NE(page->offset.size(), 0U) << 'Invalid SparsePage file';
+    page->data.resize(page->offset.back());
+    if (page->data.size() != 0) {
+      CHECK_EQ(fi->Read(dmlc::BeginPtr(page->data),
+                        (page->data).size() * sizeof(SparseBatch::Entry)),
+               (page->data).size() * sizeof(SparseBatch::Entry))
+          << 'Invalid SparsePage file';
     }
-    }
-    
-     protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-      /**
-   * @brief Computes the error gradient w.r.t. the exp inputs.
-   *
-   * @param top output Blob vector (length 1), providing the error gradient with
-   *      respect to the outputs
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
-   *      with respect to computed outputs @f$ y @f$
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$; Backward fills their diff with
-   *      gradients @f$
-   *        \frac{\partial E}{\partial x} =
-   *            \frac{\partial E}{\partial y} y \alpha \log_e(gamma)
-   *      @f$ if propagate_down[0]
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    
-    {}  // namespace caffe
-    
-    // Finally, you are free to instantiate the pattern with the types you
-// want.  If you put the above code in a header file, you can #include
-// it in multiple C++ source files and instantiate it multiple times.
-//
-// To distinguish different instances of the pattern, the first
-// argument to the INSTANTIATE_* macro is a prefix that will be added
-// to the actual test case name.  Remember to pick unique prefixes for
-// different instances.
-typedef testing::Types<char, int, unsigned int> MyTypes;
-INSTANTIATE_TYPED_TEST_CASE_P(My, FooTest, MyTypes);
-    
-    // Traps C++ exceptions escaping statement and reports them as test
-// failures. Note that trapping SEH exceptions is not implemented here.
-# if GTEST_HAS_EXCEPTIONS
-#  define GTEST_EXECUTE_DEATH_TEST_STATEMENT_(statement, death_test) \
-  try { \
-    GTEST_SUPPRESS_UNREACHABLE_CODE_WARNING_BELOW_(statement); \
-  } catch (const ::std::exception& gtest_exception) { \
-    fprintf(\
-        stderr, \
-        '\n%s: Caught std::exception-derived exception escaping the ' \
-        'death test statement. Exception message: %s\n', \
-        ::testing::internal::FormatFileLocation(__FILE__, __LINE__).c_str(), \
-        gtest_exception.what()); \
-    fflush(stderr); \
-    death_test->Abort(::testing::internal::DeathTest::TEST_THREW_EXCEPTION); \
-  } catch (...) { \
-    death_test->Abort(::testing::internal::DeathTest::TEST_THREW_EXCEPTION); \
+    return true;
   }
+    }
     
-      // Returns a copy of the FilePath with the case-insensitive extension removed.
-  // Example: FilePath('dir/file.exe').RemoveExtension('EXE') returns
-  // FilePath('dir/file'). If a case-insensitive extension is not
-  // found, returns a copy of the original FilePath.
-  FilePath RemoveExtension(const char* extension) const;
-    
-    template<typename T> inline
-bool operator==(T* ptr, const linked_ptr<T>& x) {
-  return ptr == x.get();
+    SparsePage::Writer::Writer(
+    const std::vector<std::string>& name_shards,
+    const std::vector<std::string>& format_shards,
+    size_t extra_buffer_capacity)
+    : num_free_buffer_(extra_buffer_capacity + name_shards.size()),
+      clock_ptr_(0),
+      workers_(name_shards.size()),
+      qworkers_(name_shards.size()) {
+  CHECK_EQ(name_shards.size(), format_shards.size());
+  // start writer threads
+  for (size_t i = 0; i < name_shards.size(); ++i) {
+    std::string name_shard = name_shards[i];
+    std::string format_shard = format_shards[i];
+    auto* wqueue = &qworkers_[i];
+    workers_[i].reset(new std::thread(
+        [this, name_shard, format_shard, wqueue] () {
+          std::unique_ptr<dmlc::Stream> fo(
+              dmlc::Stream::Create(name_shard.c_str(), 'w'));
+          std::unique_ptr<SparsePage::Format> fmt(
+              SparsePage::Format::Create(format_shard));
+          fo->Write(format_shard);
+          std::shared_ptr<SparsePage> page;
+          while (wqueue->Pop(&page)) {
+            if (page.get() == nullptr) break;
+            fmt->Write(*page, fo.get());
+            qrecycle_.Push(std::move(page));
+          }
+          fo.reset(nullptr);
+          LOG(CONSOLE) << 'SparsePage::Writer Finished writing to ' << name_shard;
+        }));
+  }
 }
-    
-      template <typename T>
-  operator ParamGenerator<T>() const {
-    const T array[] = {static_cast<T>(v1_), static_cast<T>(v2_),
-        static_cast<T>(v3_), static_cast<T>(v4_), static_cast<T>(v5_),
-        static_cast<T>(v6_), static_cast<T>(v7_), static_cast<T>(v8_),
-        static_cast<T>(v9_), static_cast<T>(v10_), static_cast<T>(v11_),
-        static_cast<T>(v12_), static_cast<T>(v13_), static_cast<T>(v14_),
-        static_cast<T>(v15_), static_cast<T>(v16_), static_cast<T>(v17_),
-        static_cast<T>(v18_), static_cast<T>(v19_)};
-    return ValuesIn(array);
-  }
-    
-    # define GTEST_BIND_(TmplSel, T) \
-  TmplSel::template Bind<T>::type
-    
-    
-    {    return -1;
-  }
-    
-    
-    {  return result;
-}
-    
-    #endif // __cocos2dx_experimental_video_h__
-#endif //#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS) && !defined(CC_TARGET_OS_TVOS)
-
-    
-    			b2PolygonShape shape;
-			shape.SetAsBox(0.5f, 4.0f, b2Vec2(4.0f, 0.0f), 0.5f * b2_pi);
-    
-    void BuildSequentialHuffmanCodes(
-    const JPEGData& jpg,
-    std::vector<HuffmanCodeTable>* dc_huffman_code_tables,
-    std::vector<HuffmanCodeTable>* ac_huffman_code_tables) {
-  JPEGOutput out(NullOut, nullptr);
-  BuildAndEncodeHuffmanCodes(jpg, out, dc_huffman_code_tables,
-                             ac_huffman_code_tables);
-}
-    
-    // Performs in-place floating point 8x8 DCT on block[0..63].
-// Note that the DCT used here is the DCT-2 with the first term multiplied by
-// 1/sqrt(2) and the result scaled by 1/2.
-void ComputeBlockDCTDouble(double block[64]);
-    
-    #include <inttypes.h>
-    
-    #include <stdint.h>
-#include <vector>
-    
-      // Relinquish the pointer to the caller.
-  DetachedBuffer release() {
-    DetachedBuffer fb(allocator_, own_allocator_, buf_, reserved_, cur_,
-                      size());
-    allocator_ = nullptr;
-    own_allocator_ = false;
-    buf_ = nullptr;
-    clear();
-    return fb;
-  }
-    
-    CheckedError Parser::ParseProtoCurliesOrIdent() {
-  if (Is('{')) {
-    NEXT();
-    for (int nesting = 1; nesting;) {
-      if (token_ == '{')
-        nesting++;
-      else if (token_ == '}')
-        nesting--;
-      NEXT();
-    }
-  } else {
-    NEXT();  // Any single token.
-  }
-  return NoError();
-}
-    }
-    
-    class Builder FLATBUFFERS_FINAL_CLASS {
- public:
-  Builder(size_t initial_size = 256,
-          BuilderFlag flags = BUILDER_FLAG_SHARE_KEYS)
-      : buf_(initial_size),
-        finished_(false),
-        flags_(flags),
-        force_min_bit_width_(BIT_WIDTH_8),
-        key_pool(KeyOffsetCompare(buf_)),
-        string_pool(StringOffsetCompare(buf_)) {
-    buf_.clear();
-  }
-    }
-    
-    inline bool ClientOnlyStreaming(const grpc_generator::Method *method) {
-  return method->ClientStreaming() && !method->ServerStreaming();
-}
-    
-      // Start the server. Lock to change the variable we're changing.
-  wait_for_server.lock();
-  server_instance = builder.BuildAndStart().release();
-  wait_for_server.unlock();
-  server_instance_cv.notify_one();
-    
-      // Parameters required to initialize the FlatCompiler.
-  struct InitParams {
-    InitParams()
-        : generators(nullptr),
-          num_generators(0),
-          warn_fn(nullptr),
-          error_fn(nullptr) {}
-    }
-    
-      // Converts a binary buffer to text using one of the schemas in the registry,
-  // use the file_identifier to indicate which.
-  // If DetachedBuffer::data() is null then parsing failed.
-  DetachedBuffer TextToFlatBuffer(const char *text,
-                                  const char *file_identifier) {
-    // Load and parse the schema.
-    Parser parser;
-    if (!LoadSchema(file_identifier, &parser)) return DetachedBuffer();
-    // Parse the text.
-    if (!parser.Parse(text)) {
-      lasterror_ = parser.error_;
-      return DetachedBuffer();
-    }
-    // We have a valid FlatBuffer. Detach it from the builder and return.
-    return parser.builder_.ReleaseBufferPointer();
-  }
-    
-      // We now have a FlatBuffer we can store on disk or send over a network.
