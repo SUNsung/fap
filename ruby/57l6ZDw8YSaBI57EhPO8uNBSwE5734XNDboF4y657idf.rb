d6a51380ab999b19bd41e66f550cdef0269deab1
SUNsung <weatherius@gmail.com>
2018-03-26 21:55:47 +0300

    Rewrite [57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb] File in: 2018-03-26 21:55:46

--- ruby/57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb
+++ ruby/57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb
@@ -1,83 +1,220 @@
 
         
-            # Who has a single core cpu these days anyways?
-    cpu_count = 2
+              # Returns an HTML tag.
+      #
+      # === Building HTML tags
+      #
+      # Builds HTML5 compliant tags with a tag proxy. Every tag can be built with:
+      #
+      #   tag.<tag name>(optional content, options)
+      #
+      # where tag name can be e.g. br, div, section, article, or any tag really.
+      #
+      # ==== Passing content
+      #
+      # Tags can pass content to embed within it:
+      #
+      #   tag.h1 'All titles fit to print' # => <h1>All titles fit to print</h1>
+      #
+      #   tag.div tag.p('Hello world!')  # => <div><p>Hello world!</p></div>
+      #
+      # Content can also be captured with a block, which is useful in templates:
+      #
+      #   <%= tag.p do %>
+      #     The next great American novel starts here.
+      #   <% end %>
+      #   # => <p>The next great American novel starts here.</p>
+      #
+      # ==== Options
+      #
+      # Use symbol keyed options to add attributes to the generated tag.
+      #
+      #   tag.section class: %w( kitties puppies )
+      #   # => <section class='kitties puppies'></section>
+      #
+      #   tag.section id: dom_id(@post)
+      #   # => <section id='<generated dom id>'></section>
+      #
+      # Pass +true+ for any attributes that can render with no values, like +disabled+ and +readonly+.
+      #
+      #   tag.input type: 'text', disabled: true
+      #   # => <input type='text' disabled='disabled'>
+      #
+      # HTML5 <tt>data-*</tt> attributes can be set with a single +data+ key
+      # pointing to a hash of sub-attributes.
+      #
+      # To play nicely with JavaScript conventions, sub-attributes are dasherized.
+      #
+      #   tag.article data: { user_id: 123 }
+      #   # => <article data-user-id='123'></article>
+      #
+      # Thus <tt>data-user-id</tt> can be accessed as <tt>dataset.userId</tt>.
+      #
+      # Data attribute values are encoded to JSON, with the exception of strings, symbols and
+      # BigDecimals.
+      # This may come in handy when using jQuery's HTML5-aware <tt>.data()</tt>
+      # from 1.4.3.
+      #
+      #   tag.div data: { city_state: %w( Chicago IL ) }
+      #   # => <div data-city-state='[&quot;Chicago&quot;,&quot;IL&quot;]'></div>
+      #
+      # The generated attributes are escaped by default. This can be disabled using
+      # +escape_attributes+.
+      #
+      #   tag.img src: 'open & shut.png'
+      #   # => <img src='open &amp; shut.png'>
+      #
+      #   tag.img src: 'open & shut.png', escape_attributes: false
+      #   # => <img src='open & shut.png'>
+      #
+      # The tag builder respects
+      # {HTML5 void elements}[https://www.w3.org/TR/html5/syntax.html#void-elements]
+      # if no content is passed, and omits closing tags for those elements.
+      #
+      #   # A standard element:
+      #   tag.div # => <div></div>
+      #
+      #   # A void element:
+      #   tag.br  # => <br>
+      #
+      # === Legacy syntax
+      #
+      # The following format is for legacy syntax support. It will be deprecated in future versions of Rails.
+      #
+      #   tag(name, options = nil, open = false, escape = true)
+      #
+      # It returns an empty HTML tag of type +name+ which by default is XHTML
+      # compliant. Set +open+ to true to create an open tag compatible
+      # with HTML 4.0 and below. Add HTML attributes by passing an attributes
+      # hash to +options+. Set +escape+ to false to disable attribute value
+      # escaping.
+      #
+      # ==== Options
+      #
+      # You can use symbols or strings for the attribute names.
+      #
+      # Use +true+ with boolean attributes that can render with no value, like
+      # +disabled+ and +readonly+.
+      #
+      # HTML5 <tt>data-*</tt> attributes can be set with a single +data+ key
+      # pointing to a hash of sub-attributes.
+      #
+      # ==== Examples
+      #
+      #   tag('br')
+      #   # => <br />
+      #
+      #   tag('br', nil, true)
+      #   # => <br>
+      #
+      #   tag('input', type: 'text', disabled: true)
+      #   # => <input type='text' disabled='disabled' />
+      #
+      #   tag('input', type: 'text', class: ['strong', 'highlight'])
+      #   # => <input class='strong highlight' type='text' />
+      #
+      #   tag('img', src: 'open & shut.png')
+      #   # => <img src='open &amp; shut.png' />
+      #
+      #   tag('img', {src: 'open &amp; shut.png'}, false, false)
+      #   # => <img src='open &amp; shut.png' />
+      #
+      #   tag('div', data: {name: 'Stephen', city_state: %w(Chicago IL)})
+      #   # => <div data-name='Stephen' data-city-state='[&quot;Chicago&quot;,&quot;IL&quot;]' />
+      def tag(name = nil, options = nil, open = false, escape = true)
+        if name.nil?
+          tag_builder
+        else
+          '<#{name}#{tag_builder.tag_options(options, escape) if options}#{open ? '>' : ' />'}'.html_safe
+        end
+      end
     
-      # Asynchronously send an email
-  class TestEmail < Jobs::Base
+      def redirect_to_existing_record
+    redirect_to Workshop.new(5)
+  end
     
+          # Returns constant of subscription adapter specified in config/cable.yml.
+      # If the adapter cannot be found, this will default to the Redis adapter.
+      # Also makes sure proper dependencies are required.
+      def pubsub_adapter
+        adapter = (cable.fetch('adapter') { 'redis' })
+    
+                @when_connected = []
+    
+            def test_spec_name_with_inline_config
+          spec = spec('adapter' => 'sqlite3')
+          assert_equal 'primary', spec.name, 'should default to primary id'
+        end
+      end
     end
+  end
+end
 
     
-      def enabled_setting
-    object.enabled_site_setting
-  end
+    module ActionMailer
+  # The <tt>ActionMailer::DeliveryJob</tt> class is used when you
+  # want to send emails outside of the request-response cycle.
+  #
+  # Exceptions are rescued and handled by the mailer class.
+  class DeliveryJob < ActiveJob::Base # :nodoc:
+    queue_as { ActionMailer::Base.deliver_later_queue_name }
     
-      def test_realpath_encoding
-    fsenc = Encoding.find('filesystem')
-    nonascii = '\u{0391 0410 0531 10A0 05d0 2C00 3042}'
-    tst = 'A'
-    nonascii.each_char {|c| tst << c.encode(fsenc) rescue nil}
-    Dir.mktmpdir('rubytest-realpath') {|tmpdir|
-      realdir = File.realpath(tmpdir)
-      open(File.join(tmpdir, tst), 'w') {}
-      a = File.join(tmpdir, 'x')
-      begin
-        File.symlink(tst, a)
-      rescue Errno::EACCES, Errno::EPERM
-        skip 'need privilege'
+        groups << @user.authorized_groups.visible_to_user(current_user) if current_user
+    groups << @user.authorized_groups.public_to_user(current_user)
+    
+      def all_projects(current_user)
+    projects = []
+    
+      attr_reader :mmin, :path
+    
+          class << self
+        # Mark a given block of code as a 'busy' block of code, which will
+        # register a SIGINT handler for the duration of the block. When a
+        # SIGINT occurs, the `sig_callback` proc will be called. It is up
+        # to the callback to behave properly and exit the application.
+        def busy(sig_callback)
+          register(sig_callback)
+          return yield
+        ensure
+          unregister(sig_callback)
+        end
+    
+          protected
+    
+          # Reads data from an IO object while it can, returning the data it reads.
+      # When it encounters a case when it can't read anymore, it returns the
+      # data.
+      #
+      # @return [String]
+      def self.read_until_block(io)
+        data = ''
+    
+          def initialize(string)
+        @value = string
       end
-      assert_equal(File.join(realdir, tst), File.realpath(a))
-      File.unlink(a)
-    }
     
-      def test_at
-    assert_equal(100000, Time.at('0.1'.to_r).usec)
-    assert_equal(10000, Time.at('0.01'.to_r).usec)
-    assert_equal(1000, Time.at('0.001'.to_r).usec)
-    assert_equal(100, Time.at('0.0001'.to_r).usec)
-    assert_equal(10, Time.at('0.00001'.to_r).usec)
-    assert_equal(1, Time.at('0.000001'.to_r).usec)
-    assert_equal(100000000, Time.at('0.1'.to_r).nsec)
-    assert_equal(10000000, Time.at('0.01'.to_r).nsec)
-    assert_equal(1000000, Time.at('0.001'.to_r).nsec)
-    assert_equal(100000, Time.at('0.0001'.to_r).nsec)
-    assert_equal(10000, Time.at('0.00001'.to_r).nsec)
-    assert_equal(1000, Time.at('0.000001'.to_r).nsec)
-    assert_equal(100, Time.at('0.0000001'.to_r).nsec)
-    assert_equal(10, Time.at('0.00000001'.to_r).nsec)
-    assert_equal(1, Time.at('0.000000001'.to_r).nsec)
-    assert_equal(100000, Time.at(0.1).usec)
-    assert_equal(10000, Time.at(0.01).usec)
-    assert_equal(1000, Time.at(0.001).usec)
-    assert_equal(100, Time.at(0.0001).usec)
-    assert_equal(10, Time.at(0.00001).usec)
-    assert_equal(3, Time.at(0.000003).usec)
-    assert_equal(100000000, Time.at(0.1).nsec)
-    assert_equal(10000000, Time.at(0.01).nsec)
-    assert_equal(1000000, Time.at(0.001).nsec)
-    assert_equal(100000, Time.at(0.0001).nsec)
-    assert_equal(10000, Time.at(0.00001).nsec)
-    assert_equal(3000, Time.at(0.000003).nsec)
-    assert_equal(200, Time.at(0.0000002r).nsec)
-    assert_equal(199, Time.at(0.0000002).nsec)
-    assert_equal(10, Time.at(0.00000001).nsec)
-    assert_equal(1, Time.at(0.000000001).nsec)
+              # Parse the options
+          argv = parse_options(opts)
+          return if !argv
+          if argv.empty? || argv.length > 2
+            raise Vagrant::Errors::CLIInvalidUsage,
+              help: opts.help.chomp
+          end
     
-      it 'does not decode a double when fewer bytes than a double remain and the '*' modifier is passed' do
-    [ ['\xff', []],
-      ['\xff\x00', []],
-      ['\xff\x00\xff', []],
-      ['\xff\x00\xff\x00', []],
-      ['\xff\x00\xff\x00\xff', []],
-      ['\xff\x00\xff\x00\xff\x00', []],
-      ['\xff\x00\xff\x00\xff\x00\xff', []]
-    ].should be_computed_by(:unpack, unpack_format('*'))
+        def get_cache_file_for(gist, file)
+      bad_chars = /[^a-zA-Z0-9\-_.]/
+      gist      = gist.gsub bad_chars, ''
+      file      = file.gsub bad_chars, ''
+      md5       = Digest::MD5.hexdigest '#{gist}-#{file}'
+      File.join @cache_folder, '#{gist}-#{file}-#{md5}.cache'
+    end
+    
+        def render(context)
+      if @img
+        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
+      else
+        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
+      end
+    end
   end
-    
-      def self.status_of_dying_running_thread
-    status = nil
-    t = dying_thread_ensures { status = Status.new Thread.current }
-    t.join
-    status
-  end
+end
