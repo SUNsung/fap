e74fb291679aa11e060de4506d0df7b4b3ed72e8
SUNsung <weatherius@gmail.com>
2018-03-26 03:42:34 +0300

    Rewrite [550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb] File in: 2018-03-26 03:42:34

--- ruby/550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb
+++ ruby/550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb
@@ -1,50 +1,17 @@
 
         
-          def test_simple_redirect
-    get :simple_redirect
-    assert_response :redirect
-    assert_equal 'http://test.host/redirect/hello_world', redirect_to_url
-  end
-    
-                        if callbacks = @subscribe_callbacks[chan]
-                      next_callback = callbacks.shift
-                      @event_loop.post(&next_callback) if next_callback
-                      @subscribe_callbacks.delete(chan) if callbacks.empty?
-                    end
-                  end
-                end
-    
-        assert_response :unauthorized
-    assert_equal 'Authentication Failed\n', @response.body
-    assert_equal 'Token realm='SuperSecret'', @response.headers['WWW-Authenticate']
-  end
-    
-    module MiddlewareTest
-  class MyMiddleware
-    def initialize(app)
-      @app = app
+        invalids = []
+Parallel.each(links, in_threads: 4) do |link|
+  href = link.attribute('href').to_s
+  begin
+    case check_link(URI.join(BASE_URI, href))
+    when (200...300)
+      putc('.')
+    when (300..302)
+      putc('w')
     end
-    
-        private
-      # 'Deserialize' the mailer class name by hand in case another argument
-      # (like a Global ID reference) raised DeserializationError.
-      def mailer_class
-        if mailer = Array(@serialized_arguments).first || Array(arguments).first
-          mailer.constantize
-        end
-      end
-    
-      def clean_up_export_files
-    Gitlab::Popen.popen(%W(find #{path} -not -path #{path} -mmin +#{mmin} -delete))
+  rescue => e
+    putc('F')
+    invalids << '#{href} (reason: #{e.message})'
   end
 end
-
-    
-      def validate_email_options
-    errors.add(:base, 'subject and expected_receive_period_in_days are required') unless options['subject'].present? && options['expected_receive_period_in_days'].present?
-    
-      def worker_id(config = nil)
-    '#{self.class.to_s}-#{id}-#{Digest::SHA1.hexdigest((config.presence || options).to_json)}'
-  end
-    
-        @services = current_user.services.reorder(table_sort).page(params[:page])
