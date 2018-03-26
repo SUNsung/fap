51e9040bcc24493ca5e348e9c02fada24990fef4
SUNsung <weatherius@gmail.com>
2018-03-26 19:01:32 +0300

    Rewrite [55NWEeJ9ino8A55prpVi7sUY55se.oPMBHfXo55UR.rb] File in: 2018-03-26 19:01:31

--- ruby/55NWEeJ9ino8A55prpVi7sUY55se.oPMBHfXo55UR.rb
+++ ruby/55NWEeJ9ino8A55prpVi7sUY55se.oPMBHfXo55UR.rb
@@ -1,31 +1,51 @@
 
         
-        if Encoding.default_external != Encoding::UTF_8
+          # Make it so that network access from the vagrant guest is able to
+  # use SSH private keys that are present on the host without copying
+  # them into the VM.
+  config.ssh.forward_agent = true
     
-      def ruby_bin
-    File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name'])
+        sidekiq_options queue: 'critical'
+    
+      def recipients(payload = {})
+    emails = interpolated(payload)['recipients']
+    if emails.present?
+      if emails.is_a?(String)
+        [emails]
+      else
+        emails
+      end
+    else
+      [user.email]
+    end
   end
     
-            private
+          if options[:roles].is_a?(Symbol)
+        options[:roles] = [options[:roles]]
+      end
     
-          def run
-        update_if_necessary!
+      def destroy_failed
+    Delayed::Job.where.not(failed_at: nil).delete_all
     
-        # Returns an array containing the errors on this attachment.
-    def errors
-      @errors
+        it 'returns false if osxfuse include directory is a symlink' do
+      allow(File).to receive(:exist?).and_return(true)
+      allow(File).to receive(:symlink?).and_return(true)
+      expect(described_class).not_to be_binary_osxfuse_installed
     end
+  end
     
-        def names_for(klass)
-      @attachments[klass].keys
-    end
-    
-            raise LoadError, 'Could not find the '#{name}' processor in any of these paths: #{directories.join(', ')}' unless required.any?
+      # Removes any empty directories in the formula's prefix subtree
+  # Keeps any empty directions projected by skip_clean
+  # Removes any unresolved symlinks
+  def prune
+    dirs = []
+    symlinks = []
+    @f.prefix.find do |path|
+      if path == @f.libexec || @f.skip_clean?(path)
+        Find.prune
+      elsif path.symlink?
+        symlinks << path
+      elsif path.directory?
+        dirs << path
       end
     end
-    
-      class Railtie < Rails::Railtie
-    initializer 'paperclip.insert_into_active_record' do |app|
-      ActiveSupport.on_load :active_record do
-        Paperclip::Railtie.insert
-      end
