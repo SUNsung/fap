f39b897c23b30f7602d7abc4c21aea717eb060ba
SUNsung <weatherius@gmail.com>
2018-03-27 19:39:19 +0300

    Rewrite [54m9P9woVKAxQ54ZipMQ4u6P5k54QIlBuGgIVkA540oC.rb] File in: 2018-03-27 19:39:19

--- ruby/54m9P9woVKAxQ54ZipMQ4u6P5k54QIlBuGgIVkA540oC.rb
+++ ruby/54m9P9woVKAxQ54ZipMQ4u6P5k54QIlBuGgIVkA540oC.rb
@@ -1,87 +1,128 @@
 
         
-            Jekyll::Commands::Build.process({'source' => 'docs'})
-    
-        def process(args)
-      arg_is_present? args, '--server', 'The --server command has been replaced by the \
-                          'serve' subcommand.'
-      arg_is_present? args, '--serve', 'The --serve command has been replaced by the \
-                          'serve' subcommand.'
-      arg_is_present? args, '--no-server', 'To build Jekyll without launching a server, \
-                          use the 'build' subcommand.'
-      arg_is_present? args, '--auto', 'The switch '--auto' has been replaced with \
-                          '--watch'.'
-      arg_is_present? args, '--no-auto', 'To disable auto-replication, simply leave off \
-                          the '--watch' switch.'
-      arg_is_present? args, '--pygments', 'The 'pygments'settings has been removed in \
-                          favour of 'highlighter'.'
-      arg_is_present? args, '--paginate', 'The 'paginate' setting can only be set in \
-                          your config files.'
-      arg_is_present? args, '--url', 'The 'url' setting can only be set in your \
-                          config files.'
-      no_subcommand(args)
-    end
-    
-          #
-      # The version constraint required to activate a given gem.
-      # Usually the gem version requirement is '> 0,' because any version
-      # will do. In the case of jekyll-docs, however, we require the exact
-      # same version as Jekyll.
-      #
-      # Returns a String version constraint in a parseable form for
-      # RubyGems.
-      def version_constraint(gem_name)
-        return '= #{Jekyll::VERSION}' if gem_name.to_s.eql?('jekyll-docs')
-        '> 0'
+              def self.example_code
+        [
+          'create_keychain(
+            name: 'KeychainName',
+            default_keychain: true,
+            unlock: true,
+            timeout: 3600,
+            lock_when_sleeps: true
+          )'
+        ]
       end
     
-          private
+          it 'raises an exception when use passes workspace' do
+        expect do
+          Fastlane::FastFile.new.parse('lane :test do
+            increment_build_number(xcodeproj: 'project.xcworkspace')
+          end').runner.execute(:test)
+        end.to raise_error('Please pass the path to the project, not the workspace')
+      end
     
-        def translation_scope
-      'devise.confirmations'
+          it 'prefers a custom version number over a boring version bump' do
+        Fastlane::FastFile.new.parse('lane :test do
+          increment_version_number(version_number: '1.77.3', bump_type: 'major')
+        end').runner.execute(:test)
+    
+      test 'should not call after_confirmation if not confirmed' do
+    user = create_user
+    assert user.confirm
+    user.define_singleton_method :after_confirmation do
+      self.username = self.username.to_s + 'updated'
     end
-end
-
-    
-      # POST /resource/sign_in
-  def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
+    old = user.username
+    refute user.confirm
+    assert_equal user.username, old
   end
     
-    # All Devise controllers are inherited from here.
-class DeviseController < Devise.parent_controller.constantize
-  include Devise::Controllers::ScopedViews
+      def failure_message
+    exception = request.respond_to?(:get_header) ? request.get_header('omniauth.error') : request.env['omniauth.error']
+    error   = exception.error_reason if exception.respond_to?(:error_reason)
+    error ||= exception.error        if exception.respond_to?(:error)
+    error ||= (request.respond_to?(:get_header) ? request.get_header('omniauth.error.type') : request.env['omniauth.error.type']).to_s
+    error.to_s.humanize if error
+  end
     
-          def get_type
-        case slug
-        when 'api'
-          'Reference'
-        when 'configuration'
-          'Reference: Configuration'
-        when 'stpl'
-          'Reference: SimpleTemplate'
-        when 'plugindev'
-          'Reference: Plugin'
-        else
-          'Manual'
+        # The path used after resending confirmation instructions.
+    def after_resending_confirmation_instructions_path_for(resource_name)
+      is_navigational_format? ? new_session_path(resource_name) : '/'
+    end
+    
+    class Devise::PasswordsController < DeviseController
+  prepend_before_action :require_no_authentication
+  # Render the #edit only if coming from a reset password email link
+  append_before_action :assert_reset_token_passed, only: :edit
+    
+        if resource.errors.empty?
+      set_flash_message! :notice, :unlocked
+      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
+  end
+    
+                  define_method method do |resource_or_scope, *args|
+                scope = Devise::Mapping.find_scope!(resource_or_scope)
+                router_name = Devise.mappings[scope].router_name
+                context = router_name ? send(router_name) : _devise_route_context
+                context.send('#{action}#{scope}_#{module_name}_#{path_or_url}', *args)
+              end
+            end
+          end
         end
       end
     
-      # Update version.rb file with BOOTSTRAP_SHA
-  def store_version
-    path    = 'lib/bootstrap-sass/version.rb'
-    content = File.read(path).sub(/BOOTSTRAP_SHA\s*=\s*[''][\w]+['']/, 'BOOTSTRAP_SHA = '#@branch_sha'')
-    File.open(path, 'w') { |f| f.write(content) }
+      module ClassMethods
+    def load_types_in(module_name, my_name = module_name.singularize)
+      const_set(:MODULE_NAME, module_name)
+      const_set(:BASE_CLASS_NAME, my_name)
+      const_set(:TYPES, Dir[Rails.root.join('app', 'models', module_name.underscore, '*.rb')].map { |path| module_name + '::' + File.basename(path, '.rb').camelize })
+    end
+    
+        valid_oauth_providers :evernote
+    
+        respond_to do |format|
+      format.html { redirect_to services_path }
+      format.json { head :no_content }
+    end
   end
-end
-
     
-      # Disable automatic flushing of the log to improve performance.
-  # config.autoflush_log = false
+      # Before we load the schema, define the timestamp_id function.
+  # Idiomatically, we might do this in a migration, but then it
+  # wouldn't end up in schema.rb, so we'd need to figure out a way to
+  # get it in before doing db:setup as well. This is simpler, and
+  # ensures it's always in place.
+  Rake::Task['db:schema:load'].enhance ['db:define_timestamp_id']
     
-            # Remove it form the session objects so freeup
-        sessions.delete(s[:session])
+          it 'renders application layout' do
+        get :show, params: { id: 'test', max_id: late.id }
+        expect(response).to render_template layout: 'application'
+      end
+    end
+    
+      def type
+    'Emoji'
+  end
+    
+      config = Merb::Plugins.config[:sass] || Merb::Plugins.config['sass'] || {}
+    
+            if args.length > 0
+          attachment.to_s(args.first)
+        else
+          attachment
+        end
+      end
+    end
+    
+            def passes_validation_with_size(new_size)
+          file = StringIO.new('.')
+          override_method(file, :size){ new_size }
+          override_method(file, :to_tempfile){ file }
+    
+            required = directories.map do |directory|
+          pathname = File.expand_path(Rails.root.join(directory, filename))
+          file_exists = File.exist?(pathname)
+          require pathname if file_exists
+          file_exists
+        end
