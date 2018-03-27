3649931f458a51c3dc55e8cfa53d6059bad4d964
SUNsung <weatherius@gmail.com>
2018-03-27 14:36:18 +0300

    Rewrite [50P0ZcCidDhPE50XBIzNK6Jj1k507j87Sc5zQU50Mcd.cpp] File in: 2018-03-27 14:36:17

--- cccccc/50P0ZcCidDhPE50XBIzNK6Jj1k507j87Sc5zQU50Mcd.cpp
+++ cccccc/50P0ZcCidDhPE50XBIzNK6Jj1k507j87Sc5zQU50Mcd.cpp
@@ -1,321 +1,267 @@
 
-    {  inline SILDebuggerClient *getAsSILDebuggerClient() {
-    return this;
-  }
-private:
-  virtual void anchor();
+        
+        
+    {  DISALLOW_COPY_AND_ASSIGN(DesktopCapturer);
 };
     
-    NS_ASSUME_NONNULL_END
-
-    
-    /// A SyntaxRewriter for applying a set of formatting rules to a Syntax tree.
-struct FormatSyntaxRewriter : public SyntaxRewriter {
-  virtual StructDeclSyntax
-  rewriteStructDecl(StructDeclSyntax Struct) override;
-};
-    
-      /// Indicates whether to allow diagnostics for \c <unknown> locations if
-  /// \c VerifyMode is not \c NoVerify.
-  bool VerifyIgnoreUnknown = false;
-    
-    #ifndef SWIFT_INDEX_INDEXSYMBOL_H
-#define SWIFT_INDEX_INDEXSYMBOL_H
-    
-    /**
- * @name tess_segment_pass_n
- *
- * Segment a word using the pass_n conditions of the tess segmenter.
- * @param pass_n pass number
- * @param word word to do
- */
-    
-    bool read_unlv_file(                    //print list of sides
-                     STRING name,        //basename of file
-                     inT32 xsize,        //image size
-                     inT32 ysize,        //image size
-                     BLOCK_LIST *blocks  //output list
-                    ) {
-  FILE *pdfp;                    //file pointer
-  BLOCK *block;                  //current block
-  int x;                         //current top-down coords
-  int y;
-  int width;                     //of current block
-  int height;
-  BLOCK_IT block_it = blocks;    //block iterator
+    class RenderProcessPreferences
+    : public mate::Wrappable<RenderProcessPreferences> {
+ public:
+  static mate::Handle<RenderProcessPreferences>
+      ForAllWebContents(v8::Isolate* isolate);
     }
     
-    CCStruct::~CCStruct() {
+    #ifndef ATOM_BROWSER_ATOM_QUOTA_PERMISSION_CONTEXT_H_
+#define ATOM_BROWSER_ATOM_QUOTA_PERMISSION_CONTEXT_H_
+    
+    net::URLRequestJob* HttpProtocolHandler::MaybeCreateJob(
+    net::URLRequest* request,
+    net::NetworkDelegate* network_delegate) const {
+  return net::URLRequestHttpJob::Factory(request,
+                                         network_delegate,
+                                         scheme_);
+}
+    
+    class HttpProtocolHandler : public net::URLRequestJobFactory::ProtocolHandler {
+ public:
+  explicit HttpProtocolHandler(const std::string&);
+  virtual ~HttpProtocolHandler();
+    }
+    
+    namespace atom {
+    }
+    
+    namespace atom {
+    }
+    
+    // Find the file that declares the given fully-qualified symbol name.
+bool PyDescriptorDatabase::FindFileContainingSymbol(
+    const string& symbol_name, FileDescriptorProto* output) {
+  ScopedPyObjectPtr py_descriptor(
+      PyObject_CallMethod(py_database_, 'FindFileContainingSymbol', 's#',
+                          symbol_name.c_str(), symbol_name.size()));
+  return GetFileDescriptorProto(py_descriptor.get(), output);
 }
     
     
-/**********************************************************************
- * operator-
- *
- * Subtract 2 FCOORDS.
- **********************************************************************/
+    {
+    {
+    {
+    {}  // namespace csharp
+}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
     
+    class SourceGeneratorBase {
+ protected:
+  SourceGeneratorBase(const FileDescriptor* descriptor, const Options* options);
+  virtual ~SourceGeneratorBase();
+    }
     
-    {  // Best available image.
-  Pix* pix_;
-  // True if the source image is white-on-black.
-  bool inverse_;
-  // Block the word came from. If not null, block->re_rotation() takes the
-  // 'untransformed' coordinates even further back to the original image.
-  // Used only on the first DENORM in a chain.
-  const BLOCK* block_;
-  // Rotation to apply between translation to the origin and scaling.
-  const FCOORD* rotation_;
-  // Previous transformation in a chain.
-  const DENORM* predecessor_;
-  // Non-linear transformation maps directly from each integer offset from the
-  // origin to the corresponding x-coord. Owned by the DENORM.
-  GenericVector<float>* x_map_;
-  // Non-linear transformation maps directly from each integer offset from the
-  // origin to the corresponding y-coord. Owned by the DENORM.
-  GenericVector<float>* y_map_;
-  // x-coordinate to be mapped to final_xshift_ in the result.
-  float x_origin_;
-  // y-coordinate to be mapped to final_yshift_ in the result.
-  float y_origin_;
-  // Scale factors for x and y coords. Applied to pre-rotation system.
-  float x_scale_;
-  float y_scale_;
-  // Destination coords of the x_origin_ and y_origin_.
-  float final_xshift_;
-  float final_yshift_;
-};
-#endif
-
+    TEST(JavaDocCommentTest, Escaping) {
+  EXPECT_EQ('foo /&#42; bar *&#47; baz', EscapeJavadoc('foo /* bar */ baz'));
+  EXPECT_EQ('foo /&#42;&#47; baz', EscapeJavadoc('foo /*/ baz'));
+  EXPECT_EQ('{&#64;foo}', EscapeJavadoc('{@foo}'));
+  EXPECT_EQ('&lt;i&gt;&amp;&lt;/i&gt;', EscapeJavadoc('<i>&</i>'));
+  EXPECT_EQ('foo&#92;u1234bar', EscapeJavadoc('foo\\u1234bar'));
+  EXPECT_EQ('&#64;deprecated', EscapeJavadoc('@deprecated'));
+}
     
-      tesseract::ParagraphJustification justification() const {
-    return justification_;
+    MessageGenerator* ImmutableGeneratorFactory::NewMessageGenerator(
+    const Descriptor* descriptor) const {
+  if (HasDescriptorMethods(descriptor, context_->EnforceLite())) {
+    return new ImmutableMessageGenerator(descriptor, context_);
+  } else {
+    return new ImmutableMessageLiteGenerator(descriptor, context_);
   }
-  int margin() const { return margin_; }
-  int first_indent() const { return first_indent_; }
-  int body_indent() const { return body_indent_; }
-  int tolerance() const { return tolerance_; }
-  bool is_flush() const {
-    return (justification_ == tesseract::JUSTIFICATION_LEFT ||
-            justification_ == tesseract::JUSTIFICATION_RIGHT) &&
-        abs(first_indent_ - body_indent_) <= tolerance_;
-  }
+}
     
-        ///Convert to unit vec
-    bool normalise();
+        'return this;\n');
     
-    /*The number of extra bits of precision at which to store rate metrics.*/
-# define OC_BIT_SCALE  (6)
-/*The number of extra bits of precision at which to store RMSE metrics.
-  This must be at least half OC_BIT_SCALE (rounded up).*/
-# define OC_RMSE_SCALE (5)
-/*The number of bins to partition statistics into.*/
-# define OC_SAD_BINS   (24)
-/*The number of bits of precision to drop from SAD scores to assign them to a
-   bin.*/
-# define OC_SAD_SHIFT  (9)
-    
-    /*Note that we do not provide a macro for abs(), because it is provided as a
-   library function, which we assume is translated into an intrinsic to avoid
-   the function call overhead and then implemented in the smartest way for the
-   target platform.
-  With modern gcc (4.x), this is true: it uses cmov instructions if the
-   architecture supports it and branchless bit-twiddling if it does not (the
-   speed difference between the two approaches is not measurable).
-  Interestingly, the bit-twiddling method was patented in 2000 (US 6,073,150)
-   by Sun Microsystems, despite prior art dating back to at least 1996:
-   http://web.archive.org/web/19961201174141/www.x86.org/ftp/articles/pentopt/PENTOPT.TXT
-  On gcc 3.x, however, our assumption is not true, as abs() is translated to a
-   conditional jump, which is horrible on deeply piplined architectures (e.g.,
-   all consumer architectures for the past decade or more).
-  Also be warned that -C*abs(x) where C is a constant is mis-optimized as
-   abs(C*x) on every gcc release before 4.2.3.
-  See bug http://gcc.gnu.org/bugzilla/show_bug.cgi?id=34130 */
-    
-        {&_44p7_p6_0,&_44p7_p6_1,&_44p7_p5_1},
-    {&_44p7_p7_0,&_44p7_p7_1,&_44p7_p7_2,&_44p7_p7_3}
-   }
-};
-static const static_bookblock _resbook_44p_8={
+      // Return the amount of unread data the is in the buffer.
+  size_type size() const
   {
-    {0},
-    {0,0,&_44p8_p1_0},
-    {0,&_44p8_p2_0,0},
+    return end_offset_ - begin_offset_;
+  }
     
-    static const vorbis_mapping_template _mapres_template_8_stereo[2]={
-  { _map_nominal, _res_8s_0 }, /* 0 */
-  { _map_nominal, _res_8s_1 }, /* 1 */
-};
+    template<class T, class TimeSystem>
+class base_time;
     
-    #define todB_nn(x) todB(x)
-    
-    #if (defined(__GNUC__) && defined(__STDC__) && __STDC__ && __STDC_VERSION__ >= 199901L)
-        /* supported by gcc in C99 mode, but not by all other compilers */
-        #warning 'Don't have the functions lrint() and lrintf ().'
-        #warning 'Replacing these functions with a standard C cast.'
-#endif /* __STDC_VERSION__ >= 199901L */
-        #include <math.h>
-        #define float2int(flt) ((int)(floor(.5+flt)))
-#endif
-    
-    /* Invert int32 value and return result as int32 in a given Q-domain */
-static OPUS_INLINE opus_int32 silk_INVERSE32_varQ(   /* O    returns a good approximation of '(1 << Qres) / b32' */
-    const opus_int32     b32,                   /* I    denominator (Q0)                */
-    const opus_int       Qres                   /* I    Q-domain of result (> 0)        */
-)
-{
-    opus_int   b_headrm, lshift;
-    opus_int32 b32_inv, b32_nrm, err_Q32, result;
+      static bool do_perform(reactor_op* base)
+  {
+    descriptor_read_op_base* o(static_cast<descriptor_read_op_base*>(base));
     }
     
-    #include 'osquery/core/conversions.h'
+          if (events[i].events & (POLLIN | POLLERR | POLLHUP))
+        more_reads = op_queue_[read_op].perform_operations(descriptor, ops);
+      else
+        more_reads = op_queue_[read_op].has_operation(descriptor);
     
-      r['sycall_addr_modified'] = syscall_addr_modified;
-  r['text_segment_hash'] = text_segment_hash;
-  results.push_back(r);
+    #endif // defined(BOOST_ASIO_HAS_EPOLL)
     
-    /**
- * @brief Helper struct for managing extenion metadata.
- *
- * This structure should match the members of Thrift's InternalExtensionInfo.
- */
-struct ExtensionInfo {
-  std::string name;
-  std::string version;
-  std::string min_sdk_version;
-  std::string sdk_version;
-};
-    
-    
-    {  /**
-   * @brief The host identifier at the time when logs are flushed.
-   *
-   * There is occasionally a delay between logging a status and decorating
-   * with the host identifier. In most cases the identifier is static so this
-   * does not matter. In some cases the host identifier causes database lookups.
-   */
-  std::string identifier;
-};
-    
-    /**
- * @brief Getter for the current time, in a human-readable format.
- *
- * @return the current date/time in the format: 'Wed Sep 21 10:27:52 2011'
- */
-std::string getAsciiTime();
-    
-    /**
- * @brief Iterate the discovered decorators for a given point type.
- *
- * The configuration maintains various sources, each may contain a set of
- * decorators. The source tracking is abstracted for the decorator iterator.
- *
- * @param point request execution of decorators for this given point.
- * @param time an optional time for points using intervals.
- * @param source restrict run to a specific config source.
- */
-void runDecorators(DecorationPoint point,
-                   size_t time = 0,
-                   const std::string& source = '');
-    
-    bool PlatformProcess::operator!=(const PlatformProcess& process) const {
-  return (nativeHandle() != process.nativeHandle());
-}
-    
-    extern JSClass  *jsb_cocostudio_InputDelegate_class;
-extern JSObject *jsb_cocostudio_InputDelegate_prototype;
-    
-    
-    
-    
-    
-    #ifndef APPLY_FORCE_H
-#define APPLY_FORCE_H
-    
-    		for (int32 i = 0; i < 3; ++i)
-		{
-			b2CircleShape shape;
-			shape.m_radius = 0.5f;
-    }
-    
-    class memory_resource {
+    class DropPrivileges : private boost::noncopyable {
  public:
-  virtual ~memory_resource() = default;
-  virtual void* allocate(
-      const size_t bytes,
-      const size_t alignment = max_align_v) = 0;
-  virtual void deallocate(
-      void* p,
-      const size_t bytes,
-      const size_t alignment = max_align_v) = 0;
-};
-    
-    std::shared_ptr<LogHandler> FileHandlerFactory::createHandler(
-    const Options& options) {
-  WriterFactory writerFactory;
-  return StandardLogHandlerFactory::createHandler(
-      getType(), &writerFactory, options);
-}
-    
-    /**
- * A LogWriter implementation that immediately writes to a file descriptor when
- * it is invoked.
- *
- * The downside of this class is that logging I/O occurs directly in your
- * normal program threads, so that logging I/O may block or slow down normal
- * processing.
- *
- * However, one benefit of this class is that log messages are written out
- * immediately, so if your program crashes, all log messages generated before
- * the crash will have already been written, and no messages will be lost.
- */
-class ImmediateFileWriter : public LogWriter {
- public:
-  /**
-   * Construct an ImmediateFileWriter that appends to the file at the specified
-   * path.
-   */
-  explicit ImmediateFileWriter(folly::StringPiece path);
-    }
-    
-    void LogConfig::update(const LogConfig& other) {
-  // Update handlerConfigs_ with all of the entries from the other LogConfig.
-  // Any entries already present in our handlerConfigs_ are replaced wholesale.
-  for (const auto& entry : other.handlerConfigs_) {
-    if (entry.second.type.hasValue()) {
-      // This is a complete LogHandlerConfig that should be inserted
-      // or completely replace an existing handler config with this name.
-      auto result = handlerConfigs_.insert(entry);
-      if (!result.second) {
-        result.first->second = entry.second;
-      }
-    } else {
-      // This config is updating an existing LogHandlerConfig rather than
-      // completely replacing it.
-      auto iter = handlerConfigs_.find(entry.first);
-      if (iter == handlerConfigs_.end()) {
-        throw std::invalid_argument(to<std::string>(
-            'cannot update configuration for unknown log handler \'',
-            entry.first,
-            '\''));
-      }
-      iter->second.update(entry.second);
-    }
+  /// Make call sites use 'dropTo' booleans to improve the UI.
+  static DropPrivilegesRef get() {
+    DropPrivilegesRef handle = DropPrivilegesRef(new DropPrivileges());
+    return handle;
   }
     }
     
-    dynamic logConfigToDynamic(const LogCategoryConfig& config) {
-  auto value = dynamic::object('level', logLevelToString(config.level))(
-      'inherit', config.inheritParentLevel);
-  if (config.handlers.hasValue()) {
-    auto handlers = dynamic::array();
-    for (const auto& handlerName : config.handlers.value()) {
-      handlers.push_back(handlerName);
+    // OSQuery character device switch structure.
+static struct cdevsw osquery_cdevsw = {
+    osquery_open, // open_close_fcn_t *d_open;
+    osquery_close, // open_close_fcn_t *d_close;
+    eno_rdwrt, // read_write_fcn_t *d_read;
+    eno_rdwrt, // read_write_fcn_t *d_write;
+    &osquery_ioctl, // ioctl_fcn_t      *d_ioctl;
+    eno_stop, // stop_fcn_t       *d_stop;
+    eno_reset, // reset_fcn_t      *d_reset;
+    NULL, // struct tty      **d_ttys;
+    eno_select, // select_fcn_t     *d_select;
+    eno_mmap, // mmap_fcn_t       *d_mmap;
+    eno_strat, // strategy_fcn_t   *d_strategy;
+    eno_getc, // getc_fcn_t       *d_getc;
+    eno_putc, // putc_fcn_t       *d_putc;
+    0 // int               d_type;
+};
+    
+    
+    {    // Check if overflowed
+    if (value > 0) {
+      return boost::lexical_cast<std::string>(std::llround(value));
     }
-    value('handlers', std::move(handlers));
   }
-  return std::move(value);
+    
+      if (result.front().at('symlink') == '1') {
+    // The file is a symlink, inspect the owner of the link.
+    struct stat link_stat;
+    if (lstat(parent.c_str(), &link_stat) != 0) {
+      return false;
+    }
+    }
+    
+    bool starts_with(const path& pth, const path& prefix) {
+  path::const_iterator it;
+  return skipPrefix(pth, prefix, it);
 }
     
-    LogHandlerConfig::LogHandlerConfig(Optional<StringPiece> t)
-    : type{t.hasValue() ? Optional<string>{t->str()} : Optional<string>{}} {}
+    
+    {  mmapFileCopy(srcFile, destPath.c_str());
+}
+    
+    // Get raw huge page sizes (without mount points, they'll be filled later)
+HugePageSizeVec readRawHugePageSizes() {
+  // We need to parse file names from /sys/kernel/mm/hugepages
+  static const boost::regex regex(R'!(hugepages-(\d+)kB)!');
+  boost::smatch match;
+  HugePageSizeVec vec;
+  fs::path path('/sys/kernel/mm/hugepages');
+  for (fs::directory_iterator it(path); it != fs::directory_iterator(); ++it) {
+    std::string filename(it->path().filename().string());
+    if (boost::regex_match(filename, match, regex)) {
+      StringPiece numStr(
+          filename.data() + match.position(1), size_t(match.length(1)));
+      vec.emplace_back(to<size_t>(numStr) * 1024);
+    }
+  }
+  return vec;
+}
+    
+    namespace folly {
+    }
+    
+    
+    {} // namespace folly
+
+    
+    // TODO(t10737622): Improve on-device symbolification
+void getStackTraceSymbols(vector<StackTraceElement>& symbols,
+                          const vector<InstructionPointer>& trace) {
+  symbols.clear();
+  symbols.reserve(trace.size());
+    }
+    
+        Config(void);
+    
+    
+    {} // namespace facebook
+
+    
+    class Countable : public noncopyable, public nonmovable {
+public:
+  // RefPtr expects refcount to start at 0
+  Countable() : m_refcount(0) {}
+  virtual ~Countable()
+  {
+    FBASSERT(m_refcount == 0);
+  }
+    }
+    
+      // Creates a strong reference from a raw pointer, assuming that it points to a
+  // freshly-created object. See the documentation for RefPtr for usage.
+  static inline RefPtr<T> adoptRef(T* ptr) {
+    return RefPtr<T>(ptr, ConstructionMode::Adopted);
+  }
+    
+      void reset(T* other = NULL) {
+    T* old = (T*)pthread_getspecific(m_key);
+    if (old != other) {
+      FBASSERT(m_cleanup);
+      m_cleanup(old);
+      pthread_setspecific(m_key, other);
+    }
+  }
+    
+      bool HasUnit(const Unit &U) { return Hashes.count(Hash(U)); }
+  bool HasUnit(const std::string &H) { return Hashes.count(H); }
+  InputInfo &ChooseUnitToMutate(Random &Rand) {
+    InputInfo &II = *Inputs[ChooseUnitIdxToMutate(Rand)];
+    assert(!II.U.empty());
+    return II;
+  };
+    
+    #include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <string>
+#include <vector>
+    
+    ExternalFunctions::ExternalFunctions() {
+#define EXT_FUNC(NAME, RETURN_TYPE, FUNC_SIG, WARN)                            \
+  this->NAME = GetFnPtr<decltype(::NAME)>(::NAME, ::NAME##Def, #NAME, WARN);
+    }
+    
+    
+    {  std::ofstream OF(CFPath, std::ofstream::out | std::ofstream::app);
+  for (size_t i = M.FirstNotProcessedFile; i < M.Files.size(); i++) {
+    auto U = FileToVector(M.Files[i].Name);
+    if (U.size() > MaxInputLen) {
+      U.resize(MaxInputLen);
+      U.shrink_to_fit();
+    }
+    std::ostringstream StartedLine;
+    // Write the pre-run marker.
+    OF << 'STARTED ' << std::dec << i << ' ' << U.size() << '\n';
+    OF.flush();  // Flush is important since ExecuteCommand may crash.
+    // Run.
+    TPC.ResetMaps();
+    ExecuteCallback(U.data(), U.size());
+    // Collect coverage.
+    std::set<size_t> Features;
+    TPC.CollectFeatures([&](size_t Feature) -> bool {
+      Features.insert(Feature);
+      return true;
+    });
+    // Show stats.
+    TotalNumberOfRuns++;
+    if (!(TotalNumberOfRuns & (TotalNumberOfRuns - 1)))
+      PrintStats('pulse ');
+    // Write the post-run marker and the coverage.
+    OF << 'DONE ' << i;
+    for (size_t F : Features)
+      OF << ' ' << std::hex << F;
+    OF << '\n';
+  }
+}
