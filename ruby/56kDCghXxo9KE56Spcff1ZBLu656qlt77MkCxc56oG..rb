82f22af822f140d69a594b9bc71b0ff737f1cf07
SUNsung <weatherius@gmail.com>
2018-03-28 12:29:23 +0300

    Rewrite [56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb] File in: 2018-03-28 12:29:22

--- ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
+++ ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
@@ -1,133 +1,41 @@
 
         
-        def bottle_resolve_formula_names(bottle_file)
-  receipt_file_path = bottle_receipt_path bottle_file
-  receipt_file = Utils.popen_read('tar', '-xOzf', bottle_file, receipt_file_path)
-  name = receipt_file_path.split('/').first
-  tap = Tab.from_file_content(receipt_file, '#{bottle_file}/#{receipt_file_path}').tap
-    
-        # Remove directories opposite from traversal, so that a subtree with no
-    # actual files gets removed correctly.
-    dirs.reverse_each do |d|
-      if d.children.empty?
-        puts 'rmdir: #{d} (empty)' if ARGV.verbose?
-        d.rmdir
-      end
-    end
-    
-            if file_is_stale || ARGV.switch?('s') && !f.installed? || bottle_file_outdated?(f, file)
-          cleanup_path(file) { file.unlink }
-        end
-      end
-    end
-    
-      def internal_development_commands
-    find_internal_commands HOMEBREW_LIBRARY_PATH/'dev-cmd'
+          def []=(key, value)
+    @checksums[key] = value
   end
     
-      def kernel
-    `uname -m`.chomp
-  end
-    
-      UNBREWED_EXCLUDE_FILES = %w[.DS_Store]
-  UNBREWED_EXCLUDE_PATHS = %w[
-    .github/*
-    bin/brew
-    lib/gdk-pixbuf-2.0/*
-    lib/gio/*
-    lib/node_modules/*
-    lib/python[23].[0-9]/*
-    lib/pypy/*
-    lib/pypy3/*
-    lib/ruby/gems/[12].*
-    lib/ruby/site_ruby/[12].*
-    lib/ruby/vendor_ruby/[12].*
-    share/pypy/*
-    share/pypy3/*
-    share/doc/homebrew/*
-    share/info/dir
-    share/man/man1/brew.1
-    share/man/whatis
-  ]
-    
-    if $PROGRAM_NAME == __FILE__ && !ENV['COCOAPODS_NO_BUNDLER']
-  ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
-  require 'rubygems'
-  require 'bundler/setup'
-  $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-elsif ENV['COCOAPODS_NO_BUNDLER']
-  require 'rubygems'
-  gem 'cocoapods'
-end
-    
-    (deny default)
-EOS
-    
-          def initialize(argv)
-        @podfile_path = Pathname.pwd + 'Podfile'
-        @project_path = argv.shift_argument
-        @project_paths = Pathname.pwd.children.select { |pn| pn.extname == '.xcodeproj' }
-        super
-      end
-    
-        def define_remote_file_task(task, target_roles)
-      Capistrano::UploadTask.define_task(task) do |t|
-        prerequisite_file = t.prerequisites.first
-        file = shared_path.join(t.name)
-    
-      entries.each do |entry|
-    if File.exist?(entry[:file])
-      warn '[skip] #{entry[:file]} already exists'
-    else
-      File.open(entry[:file], 'w+') do |f|
-        f.write(ERB.new(File.read(entry[:template])).result(binding))
-        puts I18n.t(:written_file, scope: :capistrano, file: entry[:file])
-      end
+      class Honda < Car
+    def initialize(*args)
+      self.make = 'Honda'
+      super(*args)
     end
   end
     
-          describe 'using the :user property' do
-        it 'takes precedence over in the host string' do
-          dsl.server 'db@example1.com:1234', roles: %w{db}, active: true, user: 'brian'
-          expect(subject).to eq('brian@example1.com:1234')
+    process_args
+list_login_items_for_app $app_path
+
+    
+    def usage
+  <<-EOS
+list_running_app_ids [ -t <bundle-id> ]
+    
+        def possible_types
+      MIME::Types.type_for(@filepath).collect(&:content_type)
+    end
+    
+            def expected_attachment
+          'Expected #{@attachment_name}:\n'
         end
+    
+      class Railtie < Rails::Railtie
+    initializer 'paperclip.insert_into_active_record' do |app|
+      ActiveSupport.on_load :active_record do
+        Paperclip::Railtie.insert
       end
     
-            def description
-          'validate the content types allowed on attachment #{@attachment_name}'
-        end
+            private
     
-          class ValidateAttachmentPresenceMatcher
-        def initialize attachment_name
-          @attachment_name = attachment_name
-        end
-    
-      class Railtie
-    def self.insert
-      Paperclip.options[:logger] = Rails.logger
-    
-    module Paperclip
-  # Provides helper methods that can be used in migrations.
-  module Schema
-    COLUMNS = {:file_name    => :string,
-               :content_type => :string,
-               :file_size    => :integer,
-               :updated_at   => :datetime}
-    
-            def autocorrect(node)
-          lambda do |corrector|
-            each_unnecessary_space_match(node) do |range|
-              corrector.replace(range, ' ')
-            end
+              arguments.each_with_index do |argument, index|
+            optarg_positions << index if argument.optarg_type?
+            arg_positions << index if argument.arg_type?
           end
-        end
-    
-            def on_case(case_node)
-          case_node.when_branches.each_with_object([]) do |when_node, previous|
-            when_node.each_condition do |condition|
-              next unless repeated_condition?(previous, condition)
-    
-            def on_if(node)
-          return unless nested_variable_comparison?(node.condition)
-          add_offense(node)
-        end
