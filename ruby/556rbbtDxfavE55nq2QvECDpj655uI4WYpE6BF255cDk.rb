af81833b726b389eba7894df423a242c8279813b
SUNsung <weatherius@gmail.com>
2018-03-29 00:41:18 +0300

    Rewrite [556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb] File in: 2018-03-29 00:41:18

--- ruby/556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb
+++ ruby/556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb
@@ -1,83 +1,110 @@
 
         
-          def remote_url
-    object.remote_url.presence
-  end
-    
-          it 'renders application layout' do
-        get :show, params: { id: 'test', max_id: late.id }
-        expect(response).to render_template layout: 'application'
-      end
-    end
-    
-      def type
-    'Emoji'
-  end
-    
-        File.write(dest, Oj.dump(map))
-    puts 'Wrote emojo to destination! (#{dest})'
+            include Behavior
   end
 end
 
     
-      describe 'for a suspension' do
-    before do
-      subject.call(DomainBlock.create!(domain: 'evil.org', severity: :suspend))
-    end
+      test 'response status aliases deprecated' do
+    response = ActionDispatch::TestResponse.create
+    assert_deprecated { response.success? }
+    assert_deprecated { response.missing? }
+    assert_deprecated { response.error? }
+  end
+end
+
     
-          output = input_path if @options[:in_place]
-      write_output(out, output)
-    rescue Sass::SyntaxError => e
-      raise e if @options[:trace]
-      file = ' of #{e.sass_filename}' if e.sass_filename
-      raise 'Error on line #{e.sass_line}#{file}: #{e.message}\n  Use --trace for backtrace'
-    rescue LoadError => err
-      handle_load_error(err)
-    end
-    
-    # A logger that delays messages until they're explicitly flushed to an inner
-# logger.
-#
-# This can be installed around the current logger by calling \{#install!}, and
-# the original logger can be replaced by calling \{#uninstall!}. The log
-# messages can be flushed by calling \{#flush}.
-class Sass::Logger::Delayed < Sass::Logger::Base
-  # Installs a new delayed logger as the current Sass logger, wrapping the
-  # original logger.
-  #
-  # This can be undone by calling \{#uninstall!}.
-  #
-  # @return [Sass::Logger::Delayed] The newly-created logger.
-  def self.install!
-    logger = Sass::Logger::Delayed.new(Sass.logger)
-    Sass.logger = logger
-    logger
+      def test_helper_attr
+    assert_nothing_raised { @controller_class.helper_attr :delegate_attr }
+    assert_includes master_helper_methods, :delegate_attr
+    assert_includes master_helper_methods, :delegate_attr=
   end
     
-        def self.definitions_for(klass)
-      instance.definitions_for(klass)
-    end
-    
-        def calculated_type_matches
-      possible_types.select do |content_type|
-        content_type == type_from_file_contents
-      end
-    end
-    
-        Hash.new.tap do |missing_styles|
-      current_styles.each do |klass, attachment_definitions|
-        attachment_definitions.each do |attachment_name, styles|
-          registered = registered_styles[klass][attachment_name] || [] rescue []
-          missed = styles - registered
-          if missed.present?
-            klass_sym = klass.to_s.to_sym
-            missing_styles[klass_sym] ||= Hash.new
-            missing_styles[klass_sym][attachment_name.to_sym] ||= Array.new
-            missing_styles[klass_sym][attachment_name.to_sym].concat(missed.to_a)
-            missing_styles[klass_sym][attachment_name.to_sym].map!(&:to_s).sort!.map!(&:to_sym).uniq!
-          end
+        private
+      # 'Deserialize' the mailer class name by hand in case another argument
+      # (like a Global ID reference) raised DeserializationError.
+      def mailer_class
+        if mailer = Array(@serialized_arguments).first || Array(arguments).first
+          mailer.constantize
         end
       end
+    
+          add_delivery_method :smtp, Mail::SMTP,
+        address:              'localhost',
+        port:                 25,
+        domain:               'localhost.localdomain',
+        user_name:            nil,
+        password:             nil,
+        authentication:       nil,
+        enable_starttls_auto: true
+    
+        # Use the logger configured for ActionMailer::Base.
+    def logger
+      ActionMailer::Base.logger
     end
   end
 end
+    
+        # Access the mailer instance.
+    def mailer
+      @_controller
+    end
+    
+          included do
+        class_attribute :_mailer_class
+        setup :initialize_test_deliveries
+        setup :set_expected_mail
+        teardown :restore_test_deliveries
+        ActiveSupport.run_load_hooks(:action_mailer_test_case, self)
+      end
+    
+      # Skips the current run on Rubinius using Minitest::Assertions#skip
+  private def rubinius_skip(message = '')
+    skip message if RUBY_ENGINE == 'rbx'
+  end
+  # Skips the current run on JRuby using Minitest::Assertions#skip
+  private def jruby_skip(message = '')
+    skip message if defined?(JRUBY_VERSION)
+  end
+end
+
+    
+        AssertSelectMailer.test('<div><p>foo</p><p>bar</p></div>').deliver_now
+    assert_select_email do
+      assert_select 'div:root' do
+        assert_select 'p:first-child', 'foo'
+        assert_select 'p:last-child', 'bar'
+      end
+    end
+  end
+    
+      p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
+  p.option 'destination', '-d', '--destination [DIR]',
+    'Destination directory (defaults to ./_site)'
+  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
+  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
+    'Plugins directory (defaults to ./_plugins)'
+  p.option 'layouts_dir', '--layouts DIR', String,
+    'Layouts directory (defaults to ./_layouts)'
+  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
+    
+      def execute
+    Gitlab::Metrics.measure(:import_export_clean_up) do
+      return unless File.directory?(path)
+    
+      # if rss_url already in existing opml file, use that; otherwise, do a lookup
+  rss_url = nil
+  existing_blog = xml.xpath('//outline[@htmlUrl='#{web_url}']').first if xml
+  if existing_blog
+    rss_url = existing_blog.attr('xmlUrl')
+    puts '#{name}: ALREADY HAVE'
+  end
+    
+          opts.on('--trace', :NONE, 'Show a full Ruby stack trace on error') do
+        @options[:trace] = true
+      end
+    end
+    
+      if defined? config.symbolize_keys!
+    config.symbolize_keys!
+  end
