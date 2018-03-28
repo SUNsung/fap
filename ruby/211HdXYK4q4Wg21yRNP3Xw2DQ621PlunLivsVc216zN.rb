33344a257aec24e321278e019977a5f449a4dea4
SUNsung <weatherius@gmail.com>
2018-03-28 06:44:15 +0300

    Rewrite [211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb] File in: 2018-03-28 06:44:14

--- ruby/211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb
+++ ruby/211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb
@@ -1,99 +1,96 @@
 
         
-          def present(payload)
-    if payload.is_a?(Hash)
-      payload = ActiveSupport::HashWithIndifferentAccess.new(payload)
-      MAIN_KEYS.each do |key|
-        return { :title => payload[key].to_s, :entries => present_hash(payload, key) } if payload.has_key?(key)
+            # Determine the available cores in host system.
+    # This mostly helps on linux, but it couldn't hurt on MacOSX.
+    if RUBY_PLATFORM =~ /linux/
+      cpu_count = `nproc`.to_i
+    elsif RUBY_PLATFORM =~ /darwin/
+      cpu_count = `sysctl -n hw.ncpu`.to_i
+    end
+    
+          def update_if_necessary!
+        if @update && config.verbose?
+          UI.section('\nUpdating Spec Repositories\n'.yellow) do
+            Repo.new(ARGV.new(['update'])).run
+          end
+        end
       end
     
-      # Optional
-  #   Override this method if you need to group multiple agents based on an API key,
-  #   or server they connect to.
-  #   Have a look at the TwitterStreamAgent for an example.
-  def self.setup_worker; end
-    
-      def running?
-    (@job.locked_at || @job.locked_by) && @job.failed_at.nil?
-  end
-end
-
-    
-        respond_to do |format|
-      format.html
-      format.json { render json: @user_credential }
-    end
-  end
-    
-                -- Finally, add our sequence number to our base, and chop
-            -- it to the last two bytes
-            tail := (
-              (sequence_base + nextval(table_name || '_id_seq'))
-              & 65535);
-    
-      if Rails.env == 'development'
-    test_conf = ActiveRecord::Base.configurations['test']
-    
-      # Configure static asset server for tests with Cache-Control for performance.
-  if config.respond_to?(:serve_static_files)
-    # rails >= 4.2
-    config.serve_static_files = true
-  elsif config.respond_to?(:serve_static_assets)
-    # rails < 4.2
-    config.serve_static_assets = true
-  end
-  config.static_cache_control = 'public, max-age=3600'
-    
-    namespace :bower do
+          # Prints a title in blue with surrounding newlines.
+      def title(text)
+        # Use $stdout directly to bypass the indentation that our `puts` does.
+        $stdout.puts(color.colorize('\n#{text}\n', :blue))
+      end
     
           def assert_valid_stage_names(names)
         invalid = names.find { |n| RESERVED_NAMES.include?(n) }
         return if invalid.nil?
     
-        def after(task, post_task, *args, &block)
-      Rake::Task.define_task(post_task, *args, &block) if block_given?
-      task = Rake::Task[task]
-      task.enhance do
-        post = Rake.application.lookup(post_task, task.scope)
-        raise ArgumentError, 'Task #{post_task.inspect} not found' unless post
-        post.invoke
+        # Provide a wrapper for the SCM that loads a strategy for the user.
+    #
+    # @param [Rake] context     The context in which the strategy should run
+    # @param [Module] strategy  A module to include into the SCM instance. The
+    #    module should provide the abstract methods of Capistrano::SCM
+    #
+    def initialize(context, strategy)
+      @context = context
+      singleton = class << self; self; end
+      singleton.send(:include, strategy)
+    end
+    
+          instance.run_paperclip_callbacks(:post_process) do
+        instance.run_paperclip_callbacks(:'#{name}_post_process') do
+          if !@options[:check_validity_before_processing] || !instance.errors.any?
+            post_process_styles(*style_args)
+          end
+        end
       end
     end
     
-      # Read and eval a .rake file in such a way that `self` within the .rake file
-  # refers to this plugin instance. This gives the tasks in the file access to
-  # helper methods defined by the plugin.
-  def eval_rakefile(path)
-    contents = IO.read(path)
-    instance_eval(contents, path, 1)
-  end
+        def initialize(filepath)
+      @filepath = filepath
+    end
     
-      def execute
-    validate_params
-    source = File.join(File.dirname(__FILE__), 'templates', '#{type}-plugin')
-    @target_path = File.join(path, full_plugin_name)
-    FileUtils.mkdir(@target_path)
-    puts ' Creating #{@target_path}'
+        # Extracts the Geometry from a 'WxH,O' string
+    # Where W is the width, H is the height,
+    # and O is the EXIF orientation
+    def self.parse(string)
+      GeometryParser.new(string).make
+    end
     
-      public
-  def flush(&block)
-    # does nothing by default.
-    # if your codec needs a flush method (like you are spooling things)
-    # you must implement this.
-  end
+        def add_required_validations
+      options = Paperclip::Attachment.default_options.deep_merge(@options)
+      if options[:validate_media_type] != false
+        name = @name
+        @klass.validates_media_type_spoof_detection name,
+          :if => ->(instance){ instance.send(name).dirty? }
+      end
+    end
     
-          expect('.border-color-alternate').to have_rule(rule)
+            def failure_message
+          'Attachment #{@attachment_name} should be required'
+        end
+    
+      context 'called with four styles' do
+    it 'applies different styles to all sides' do
+      rule = 'border-style: dotted groove ridge none'
+    
+          expect('.border-width-false-third').to have_ruleset(ruleset)
+      expect('.border-width-false-third').to_not have_rule(bad_rule)
     end
   end
+end
+
     
-          expect('.border-style-explicit').to have_rule(rule)
+      context 'called with arguments (2, $value: 4em 6em)' do
+    it 'outputs sextuple the second value from the default scale' do
+      expect('.two-double-value').to have_rule('font-size: 3.125em')
     end
   end
+end
+
     
-          expect('.border-width-implied-left').to have_rule(rule)
+          expect('.size-auto').to have_ruleset(rule)
     end
   end
-    
-      context 'called with one size' do
-    it 'applies same width to all sides' do
-      rule = 'padding: 1px'
+end
