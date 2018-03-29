7fd8c65d9f91411a1f67b60779561e39fd019cbc
SUNsung <weatherius@gmail.com>
2018-03-29 10:35:40 +0300

    Rewrite [28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb] File in: 2018-03-29 10:35:40

--- ruby/28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb
+++ ruby/28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb
@@ -1,152 +1,163 @@
 
         
-              # Overwrite this factory method for Redis connections if you want to use a different Redis library than the redis gem.
-      # This is needed, for example, when using Makara proxies for distributed Redis.
-      cattr_accessor :redis_connector, default: ->(config) do
-        ::Redis.new(config.slice(:url, :host, :port, :db, :password))
-      end
+              # Reads data from an IO object while it can, returning the data it reads.
+      # When it encounters a case when it can't read anymore, it returns the
+      # data.
+      #
+      # @return [String]
+      def self.read_until_block(io)
+        data = ''
     
-            # Returns a hash with database connection information.
-        #
-        # == Examples
-        #
-        # Full hash Configuration.
-        #
-        #   configurations = { 'production' => { 'host' => 'localhost', 'database' => 'foo', 'adapter' => 'sqlite3' } }
-        #   Resolver.new(configurations).resolve(:production)
-        #   # => { 'host' => 'localhost', 'database' => 'foo', 'adapter' => 'sqlite3'}
-        #
-        # Initialized with URL configuration strings.
-        #
-        #   configurations = { 'production' => 'postgresql://localhost/foo' }
-        #   Resolver.new(configurations).resolve(:production)
-        #   # => { 'host' => 'localhost', 'database' => 'foo', 'adapter' => 'postgresql' }
-        #
-        def resolve(config)
-          if config
-            resolve_connection config
-          elsif env = ActiveRecord::ConnectionHandling::RAILS_ENV.call
-            resolve_symbol_connection env.to_sym
-          else
-            raise AdapterNotSpecified
-          end
-        end
+          def execute
+        options = {}
+        options[:check] = false
     
-      test 'response status aliases deprecated' do
-    response = ActionDispatch::TestResponse.create
-    assert_deprecated { response.success? }
-    assert_deprecated { response.missing? }
-    assert_deprecated { response.error? }
+      it 'implicitly has a count of one when no count is specified' do
+    'abc'.unpack(unpack_format).should == ['a']
+  end
+    
+      Ruby = Struct.new(:version, :platform)
+    
+      def self.create_and_kill_critical_thread(pass_after_kill=false)
+    ThreadSpecs.create_critical_thread do
+      Thread.current.kill
+      Thread.pass if pass_after_kill
+      ScratchPad.record('status=' + Thread.current.status)
+    end
   end
 end
 
     
-          def clear_all # :nodoc:
-        reset_all
-        current_instances.clear
-      end
+            private
     
-        class SetsResponseBody < ControllerWithCallbacks
-      before_action :set_body
-    
-      class BareEmptyTest < ActiveSupport::TestCase
-    test 'response body is nil' do
-      controller = BareEmptyController.new
-      controller.set_request!(ActionDispatch::Request.empty)
-      controller.set_response!(BareController.make_response!(controller.request))
-      controller.index
-      assert_nil controller.response_body
-    end
-  end
-    
-      class BlockMiddleware
-    attr_accessor :configurable_message
-    def initialize(app, &block)
-      @app = app
-      yield(self) if block_given?
-    end
-    
-        s = 'This formula is keg-only, which means it was not symlinked into #{HOMEBREW_PREFIX}.'
-    s << '\n\n#{f.keg_only_reason}'
-    if f.lib.directory? || f.include.directory?
-      s <<
-        <<-EOS.undent_________________________________________________________72
-    
-        # Get rid of any info 'dir' files, so they don't conflict at the link stage
-    info_dir_file = @f.info + 'dir'
-    if info_dir_file.file? && !@f.skip_clean?(info_dir_file)
-      observe_file_removal info_dir_file
-    end
-    
-          return true if prune == 'all'
-    
-      def participation_answer(user)
-    poll_participations.find_by(author_id: user.person.id)
-  end
-    
-          def fetch_public_key(o_auth_app, jwt)
-        public_key = fetch_public_key_from_json(o_auth_app.jwks, jwt)
-        if public_key.empty? && o_auth_app.jwks_uri
-          response = Faraday.get(o_auth_app.jwks_uri)
-          public_key = fetch_public_key_from_json(response.body, jwt)
+            # Prints the list of specs & pod cache dirs for a single pod name.
+        #
+        # This output is valid YAML so it can be parsed with 3rd party tools
+        #
+        # @param [Array<Hash>] cache_descriptors
+        #        The various infos about a pod cache. Keys are
+        #        :spec_file, :version, :release and :slug
+        #
+        def print_pod_cache_infos(pod_name, cache_descriptors)
+          UI.puts '#{pod_name}:'
+          cache_descriptors.each do |desc|
+            if @short_output
+              [:spec_file, :slug].each { |k| desc[k] = desc[k].relative_path_from(@cache.root) }
+            end
+            UI.puts('  - Version: #{desc[:version]}')
+            UI.puts('    Type:    #{pod_type(desc)}')
+            UI.puts('    Spec:    #{desc[:spec_file]}')
+            UI.puts('    Pod:     #{desc[:slug]}')
+          end
         end
-        raise Rack::OAuth2::Server::Authorize::BadRequest(:unauthorized_client) if public_key.empty?
-        public_key
       end
+    end
+  end
+end
+
     
-        def split_colon_path(path)
-      one, two = path.split(':', 2)
-      if one && two && Sass::Util.windows? &&
-          one =~ /\A[A-Za-z]\Z/ && two =~ %r{\A[/\\]}
-        # If we're on Windows and we were passed a drive letter path,
-        # don't split on that colon.
-        one2, two = two.split(':', 2)
-        one = one + ':' + one2
+            self.summary = 'The repl listens to commands on standard input'
+        self.description = <<-DESC
+          The repl listens to commands on standard input and prints their
+          result to standard output.
+          It accepts all the other ipc subcommands. The repl will signal the
+          end of output with the the ASCII CR+LF `\\n\\r`.
+        DESC
+    
+            # Checks if a template URL is given else returns the TEMPLATE_REPO URL
+        #
+        # @return String
+        #
+        def template_repo_url
+          @template_url || TEMPLATE_REPO
+        end
       end
-      return one, two
+    end
+  end
+end
+
+    
+        def register_new_attachment
+      Paperclip::AttachmentRegistry.register(@klass, @name, @options)
     end
     
-    # A logger that delays messages until they're explicitly flushed to an inner
-# logger.
-#
-# This can be installed around the current logger by calling \{#install!}, and
-# the original logger can be replaced by calling \{#uninstall!}. The log
-# messages can be flushed by calling \{#flush}.
-class Sass::Logger::Delayed < Sass::Logger::Base
-  # Installs a new delayed logger as the current Sass logger, wrapping the
-  # original logger.
-  #
-  # This can be undone by calling \{#uninstall!}.
-  #
-  # @return [Sass::Logger::Delayed] The newly-created logger.
-  def self.install!
-    logger = Sass::Logger::Delayed.new(Sass.logger)
-    Sass.logger = logger
-    logger
+            def description
+          'have an attachment named #{@attachment_name}'
+        end
+    
+            def rejected_types_rejected?
+          @missing_rejected_types ||= @rejected_types.select { |type| type_allowed?(type) }
+          @missing_rejected_types.none?
+        end
+      end
+    end
+  end
+end
+
+    
+    module Paperclip
+  class << self
+    attr_writer :registered_attachments_styles_path
+    def registered_attachments_styles_path
+      @registered_attachments_styles_path ||= Rails.root.join('public/system/paperclip_attachments.yml').to_s
+    end
   end
     
-          temp_file = File.open('_escaped_params_tmp_file', 'w')
-      begin
-        temp_file.write('hello world')
-        temp_file.close
+        def processor(name) #:nodoc:
+      @known_processors ||= {}
+      if @known_processors[name.to_s]
+        @known_processors[name.to_s]
+      else
+        name = name.to_s.camelize
+        load_processor(name) unless Paperclip.const_defined?(name)
+        processor = Paperclip.const_get(name)
+        @known_processors[name.to_s] = processor
+      end
+    end
     
-      it 'accepts requests with the same Accept-Language header' do
-    session = {:foo => :bar}
-    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
-    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
-    expect(session).not_to be_empty
+      def pretty?
+    params.has_key?('pretty')
   end
+    
+    module LogStash
+  module Api
+    module Commands
+      module System
+        class Plugins < Commands::Base
+          def run
+            { :total => plugins.count, :plugins => plugins }
+          end
+    
+      public
+  # DEPRECATED: Prefer defining encode_sync or multi_encode
+  def encode(event)
+    encoded = multi_encode([event])
+    encoded.each {|event,data| @on_event.call(event,data) }
+  end # def encode
     
       context 'called with one color' do
-    it 'applies same width to all sides' do
-      rule = 'border-width: 1px'
+    it 'applies same color to all sides' do
+      rule = 'border-color: #f00'
     
-          expect('.padding-explicit').to have_rule(rule)
+          expect('.border-style-explicit').to have_rule(rule)
     end
   end
     
-      context 'expands hover text inputs' do
-    it 'finds selectors' do
-      list = @inputs_list.map { |input| '#{input}:hover' }
-      list = list.join(', ')
-      ruleset = 'content: #{list};'
+        @buttons_list = %w(
+      button
+      [type='button']
+      [type='reset']
+      [type='submit']
+    )
+  end
+    
+      context 'called with one size' do
+    it 'applies same width to all sides' do
+      rule = 'margin: 1px'
+    
+      context 'called with arguments (2, $value: 4em 6em)' do
+    it 'outputs sextuple the second value from the default scale' do
+      expect('.two-double-value').to have_rule('font-size: 3.125em')
+    end
+  end
+end
