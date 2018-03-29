d64d5c5102bf1ebbc12899b9a6426d2c0ca279df
SUNsung <weatherius@gmail.com>
2018-03-29 13:59:23 +0300

    Rewrite [52Ib8VUs.nJIM52PhZ1nEz32Mk52jfuzLA..aY65239b.cpp] File in: 2018-03-29 13:59:22

--- cccccc/52Ib8VUs.nJIM52PhZ1nEz32Mk52jfuzLA..aY65239b.cpp
+++ cccccc/52Ib8VUs.nJIM52PhZ1nEz32Mk52jfuzLA..aY65239b.cpp
@@ -1,130 +1,225 @@
-#define printmat(m) msra::math::printmatf(#m, m)
-#define printmatfile(m, f) msra::math::printmatf(#m, m, f)
-    
-    
-    {
-    {            // nodeToDelete is a child
-            if (child == nodeToDelete)
-            {
-                // this used to call DetatchInputs(), but it's better for MEL to retain other inputs
-                node->SetInput(i, nullptr);
-                break;
-            }
-        }
-    }
-    
-                auto runMeanParameterPtr = node->Input(3);
-            auto runStdParameterPtr  = node->Input(4);
-    
-        // EvaluateNDLSnippet - evaluate the passed snippet of NDL into a computational network
-    // script - [in] text of the NDL snippet
-    // network - [in/out] computation network to insert NDL into
-    void EvaluateNDLSnippet(const ConfigValue& script, ComputationNetworkPtr network)
-    {
-        NDLUtil<ElemType> ndlUtil(network);
-        ndlUtil.ProcessNDLConfig(script);
-    }
-    
-    // If the Tracing flag is set, print out a timestamp with no new line at the end
-#define PREPENDTS(stream) \
-    do \
-    { \
-        if (ProgressTracing::GetTimestampingFlag()) \
-        { \
-            char mbstr[30]; \
-            fprintf(stream, '%s: ', ProgressTracing::Timestamp(mbstr));  \
-        } \
-    } while(0)
-    
-    vector<wstring> /*IConfigRecord::*/ ComputationNodeBase::GetMemberIds() const
-{
-    return vector<wstring>{ L'name', L'operation', L'dim', L'dims', /*L'tag', */L'inputs', OperationName() + L'Args' };
-}
-    
-    
-    {
-    {// use R's PRNG to replacd
-CustomGlobalRandomEngine::result_type
-CustomGlobalRandomEngine::operator()() {
-  return static_cast<result_type>(
-      std::floor(unif_rand() * CustomGlobalRandomEngine::max()));
-}
-}  // namespace common
-}  // namespace xgboost
 
+        
+        
+    {
+    {    if (++count % 1000 == 0) {
+      txn->Commit();
+    }
+  }
+  // write the last batch
+  if (count % 1000 != 0) {
+      txn->Commit();
+  }
+  LOG(INFO) << 'Processed ' << count << ' files.';
+  delete[] pixels;
+  db->Close();
+}
     
-    #else
-/*!
- * \brief global random engine
+      /**
+   * @brief Computes the Contrastive error gradient w.r.t. the inputs.
+   *
+   * Computes the gradients with respect to the two input vectors (bottom[0] and
+   * bottom[1]), but not the similarity label (bottom[2]).
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *      respect to the outputs
+   *   -# @f$ (1 \times 1 \times 1 \times 1) @f$
+   *      This Blob's diff will simply contain the loss_weight* @f$ \lambda @f$,
+   *      as @f$ \lambda @f$ is the coefficient of this layer's output
+   *      @f$\ell_i@f$ in the overall Net loss
+   *      @f$ E = \lambda_i \ell_i + \mbox{other loss terms}@f$; hence
+   *      @f$ \frac{\partial E}{\partial \ell_i} = \lambda_i @f$.
+   *      (*Assuming that this top Blob is not used as a bottom (input) by any
+   *      other layer of the Net.)
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length 2)
+   *   -# @f$ (N \times C \times 1 \times 1) @f$
+   *      the features @f$a@f$; Backward fills their diff with
+   *      gradients if propagate_down[0]
+   *   -# @f$ (N \times C \times 1 \times 1) @f$
+   *      the features @f$b@f$; Backward fills their diff with gradients if
+   *      propagate_down[1]
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
+    
+    /**
+ * @brief During training only, sets a random portion of @f$x@f$ to 0, adjusting
+ *        the rest of the vector magnitude accordingly.
+ *
+ * @param bottom input Blob vector (length 1)
+ *   -# @f$ (N \times C \times H \times W) @f$
+ *      the inputs @f$ x @f$
+ * @param top output Blob vector (length 1)
+ *   -# @f$ (N \times C \times H \times W) @f$
+ *      the computed outputs @f$ y = |x| @f$
  */
-typedef RandomEngine GlobalRandomEngine;
+template <typename Dtype>
+class DropoutLayer : public NeuronLayer<Dtype> {
+ public:
+  /**
+   * @param param provides DropoutParameter dropout_param,
+   *     with DropoutLayer options:
+   *   - dropout_ratio (\b optional, default 0.5).
+   *     Sets the probability @f$ p @f$ that any given unit is dropped.
+   */
+  explicit DropoutLayer(const LayerParameter& param)
+      : NeuronLayer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    }
+    
+      virtual inline const char* type() const { return 'Eltwise'; }
+  virtual inline int MinBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+    
+    
+    {  auto const ret = make_map_array(
+    s_sec, (int)tp.tv_sec,
+    s_usec, (int)tp.tv_usec,
+    s_minuteswest, (int)(-offset->offset / 60),
+    s_dsttime, (int)offset->is_dst
+  );
+  timelib_time_offset_dtor(offset);
+  return ret;
+}
+    
+    
+    {///////////////////////////////////////////////////////////////////////////////
+}
+    
+      int64_t getTime() const;
+    
+    /*
+ * Assert that Vreg widths match between defs and uses.
+ *
+ * This should only be run before any zero-extending or truncating copies get
+ * reduced to regular copies---so, before simplify() or the various lowering
+ * passes.
+ */
+bool checkWidths(Vunit& unit);
+    
+    public const char *
+magic_getpath(const char *magicfile, int action)
+{
+  if (magicfile != NULL)
+    return magicfile;
+    }
+    
+    #include 'hphp/runtime/vm/hhbc.h'
+    
+      void assertCanEmit(size_t nBytes) {
+    if (!canEmit(nBytes) && !grow(nBytes)) reportFull(nBytes);
+  }
+    
+    using std::vector;
+    
+    void mergeInto(Cluster& into, Cluster&& other, const double /*aw*/ = 0) {
+  into.targets.insert(into.targets.end(),
+                      other.targets.begin(), other.targets.end());
+  into.size += other.size;
+  into.samples += other.samples;
+    }
+    
+    #include <cstdint>
+#include <string>
+    
+      std::vector<size_t> feature_counts_;
+  std::vector<ColumnType> type_;
+  std::vector<uint32_t> index_;  // index_: may store smaller integers; needs padding
+  std::vector<size_t> row_ind_;
+  std::vector<ColumnBoundary> boundary_;
+    
+    #if defined(__GNUC__) && ((__GNUC__ == 4 && __GNUC_MINOR__ >= 8) || __GNUC__ > 4) && \
+    !defined(__CUDACC__)
+#include <parallel/algorithm>
+#define XGBOOST_PARALLEL_SORT(X, Y, Z) __gnu_parallel::sort((X), (Y), (Z))
+#define XGBOOST_PARALLEL_STABLE_SORT(X, Y, Z) \
+  __gnu_parallel::stable_sort((X), (Y), (Z))
+#elif defined(_MSC_VER) && (!__INTEL_COMPILER)
+#include <ppl.h>
+#define XGBOOST_PARALLEL_SORT(X, Y, Z) concurrency::parallel_sort((X), (Y), (Z))
+#define XGBOOST_PARALLEL_STABLE_SORT(X, Y, Z) std::stable_sort((X), (Y), (Z))
+#else
+#define XGBOOST_PARALLEL_SORT(X, Y, Z) std::sort((X), (Y), (Z))
+#define XGBOOST_PARALLEL_STABLE_SORT(X, Y, Z) std::stable_sort((X), (Y), (Z))
 #endif
     
     
     {
-    {}  // namespace common
+    {XGBOOST_REGISTER_METRIC(MultiLogLoss, 'mlogloss')
+.describe('Multiclass negative loglikelihood.')
+.set_body([](const char* param) { return new EvalMultiLogLoss(); });
+}  // namespace metric
 }  // namespace xgboost
-#endif  // XGBOOST_COMMON_COLUMN_MATRIX_H_
 
     
-    DMLC_REGISTRY_FILE_TAG(sparse_page_raw_format);
+        // 2. Show another simple window. In most cases you will use an explicit Begin/End pair to name your windows.
+    if (hud->show_another_window)
+    {
+        ImGui::Begin('Another Window', &hud->show_another_window);
+        ImGui::Text('Hello from another window!');
+        ImGui::ColorEdit3('Cube 1 Color', hud->cubeColor1);
+        ImGui::ColorEdit3('Cube 2 Color', hud->cubeColor2);
+        ImGui::SliderFloat('Rotation Speed', &hud->rotation_speed, 0.0f, 200.0f);
+        if (ImGui::Button('Close Me'))
+            hud->show_another_window = false;
+        ImGui::End();
+    }
     
-    /**
- * Symbolicates a stack trace into a new vector
- *
- * @param stackTrace The input stack trace
- */
-FBEXPORT inline std::vector<StackTraceElement> getStackTraceSymbols(
-    const std::vector<InstructionPointer>& trace) {
-  auto symbols = std::vector<StackTraceElement>{};
-  getStackTraceSymbols(symbols, trace);
-  return symbols;
-}
+        for (int n = 0; n < 50; n++)
+    {
+        printf('NewFrame() %d\n', n);
+        io.DisplaySize = ImVec2(1920, 1080);
+        io.DeltaTime = 1.0f / 60.0f;
+        ImGui::NewFrame();
+    }
     
-    bool Config::isExperimentalFeatureEnabled(int feature) const
+    IMGUI_API bool        ImGui_ImplDX10_Init(void* hwnd, ID3D10Device* device);
+IMGUI_API void        ImGui_ImplDX10_Shutdown();
+IMGUI_API void        ImGui_ImplDX10_NewFrame();
+IMGUI_API void        ImGui_ImplDX10_RenderDrawData(ImDrawData* draw_data);
+    
+            for( int i=0; i < nVert; i++ )
+        {
+            // TODO: optimize multiplication on gpu using vertex shader/projection matrix.
+            pVertStream[i].x = cmd_list->VtxBuffer[i].pos.x * g_RenderScale.x;
+            pVertStream[i].y = cmd_list->VtxBuffer[i].pos.y * g_RenderScale.y;
+            pUVStream[i].x = cmd_list->VtxBuffer[i].uv.x;
+            pUVStream[i].y = cmd_list->VtxBuffer[i].uv.y;
+            pColStream[i] = cmd_list->VtxBuffer[i].col;
+        }
+    
+                if (ImGui::Button('Button'))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text('counter = %d', counter);
+    
+    
+    {        // Handle loss of D3D9 device
+        if (result == D3DERR_DEVICELOST && g_pd3dDevice->TestCooperativeLevel() == D3DERR_DEVICENOTRESET)
+        {
+            ImGui_ImplDX9_InvalidateDeviceObjects();
+            g_pd3dDevice->Reset(&g_d3dpp);
+            ImGui_ImplDX9_CreateDeviceObjects();
+        }
+    }
+    
+    // Draw channels are used by the Columns API to 'split' the render list into different channels while building, so items of each column can be batched together.
+// You can also use them to simulate drawing layers and submit primitives in a different order than how they will be rendered.
+struct ImDrawChannel
 {
-    return YGConfigIsExperimentalFeatureEnabled(m_config, static_cast<YGExperimentalFeature>(feature));
-}
-
+    ImVector<ImDrawCmd>     CmdBuffer;
+    ImVector<ImDrawIdx>     IdxBuffer;
+};
     
-        void setExperimentalFeatureEnabled(int feature, bool enabled);
-    void setPointScaleFactor(float pixelsInPoint);
- 
- public: // Getters
-    
-     public:
-    
-        method(setPadding);
-    method(setPaddingPercent);
-    
-    namespace facebook {
-    }
-    
-    template<typename... ARGS>
-inline void log(int level, const char* tag, const char* msg) noexcept {
-  __android_log_write(level, tag, msg);
-}
-    
-      bool hasOnlyOneRef() const {
-    return m_refcount == 1;
-  }
-    
-    ///////////////////////////////////////////////////////////////////////////////
-    
-        totalConnections = atoi(argv[1]);
-    port = atoi(argv[2]);
-    
-        sockaddr_storage addr;
-    socklen_t addrLength = sizeof(addr);
-    if (getpeername(fd, (sockaddr *) &addr, &addrLength) == -1) {
-        return {0, '', ''};
-    }
-    
-    struct WIN32_EXPORT Hub : protected uS::Node, public Group<SERVER>, public Group<CLIENT> {
-protected:
-    struct ConnectionData {
-        std::string path;
-        void *user;
-        Group<CLIENT> *group;
-    };
-    }
+                static float arr[] = { 0.6f, 0.1f, 1.0f, 0.5f, 0.92f, 0.1f, 0.2f };
+            ImGui::PlotLines('Frame Times', arr, IM_ARRAYSIZE(arr));
