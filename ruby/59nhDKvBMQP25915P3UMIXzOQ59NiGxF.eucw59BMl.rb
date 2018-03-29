a61f79823a249d6264d9ca39eaabc4be3deb7c17
SUNsung <weatherius@gmail.com>
2018-03-29 15:51:59 +0300

    Rewrite [59nhDKvBMQP25915P3UMIXzOQ59NiGxF.eucw59BMl.rb] File in: 2018-03-29 15:51:58

--- ruby/59nhDKvBMQP25915P3UMIXzOQ59NiGxF.eucw59BMl.rb
+++ ruby/59nhDKvBMQP25915P3UMIXzOQ59NiGxF.eucw59BMl.rb
@@ -1,96 +1,33 @@
 
         
-              if rails?
-        register_rails_engine
-      elsif lotus?
-        register_lotus
-      elsif sprockets?
-        register_sprockets
-      end
+          </body>
+</html>
+HTML
+CONTENT_NOT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
     
-          opts.on('--unix-newlines', 'Use Unix-style newlines in written files.',
-                                 ('Always true on Unix.' unless Sass::Util.windows?)) do
-        @options[:unix_newlines] = true if Sass::Util.windows?
-      end
-    end
-    
-    # A logger that delays messages until they're explicitly flushed to an inner
-# logger.
-#
-# This can be installed around the current logger by calling \{#install!}, and
-# the original logger can be replaced by calling \{#uninstall!}. The log
-# messages can be flushed by calling \{#flush}.
-class Sass::Logger::Delayed < Sass::Logger::Base
-  # Installs a new delayed logger as the current Sass logger, wrapping the
-  # original logger.
-  #
-  # This can be undone by calling \{#uninstall!}.
-  #
-  # @return [Sass::Logger::Delayed] The newly-created logger.
-  def self.install!
-    logger = Sass::Logger::Delayed.new(Sass.logger)
-    Sass.logger = logger
-    logger
-  end
-    
-        # Merges this query list with another. The returned query list
-    # queries for the intersection between the two inputs.
     #
-    # Both query lists should be resolved.
+    
+        # Processes the options set by the command-line arguments,
+    # and runs the CSS compiler appropriately.
+    def process_result
+      require 'sass'
+    
+        # Returns the three components of the interpolation, `before`, `mid`, and `after`.
     #
-    # @param other [QueryList]
-    # @return [QueryList?] The merged list, or nil if there is no intersection.
-    def merge(other)
-      new_queries = queries.map {|q1| other.queries.map {|q2| q1.merge(q2)}}.flatten.compact
-      return if new_queries.empty?
-      QueryList.new(new_queries)
-    end
-    
-      config = Merb::Plugins.config[:sass] || Merb::Plugins.config['sass'] || {}
-    
-          # Override `Kernel#puts` to prepend four spaces to each line.
-      def puts(string=nil)
-        $stdout.puts(string.to_s.gsub(/^/, '    '))
-      end
-    
-        # @abstract
-    #
-    # Create a (new) clone of the remote-repository on the deployment target
-    #
-    # @return void
-    #
-    def clone
-      raise NotImplementedError, 'Your SCM strategy module should provide a #clone method'
-    end
-    
-        # we assume that the first file that requires 'sinatra' is the
-    # app_file. all other path related options are calculated based
-    # on this path by default.
-    set :app_file, caller_files.first || $0
-    
-          def escape(object)
-        case object
-        when Hash   then escape_hash(object)
-        when Array  then object.map { |o| escape(o) }
-        when String then escape_string(object)
-        when Tempfile then object
-        else nil
-        end
-      end
-    
-      describe '#random_string' do
-    it 'outputs a string of 32 characters' do
-      expect(subject.random_string.length).to eq(32)
-    end
-  end
-    
-      describe '#react' do
-    it 'prevents attacks and warns about it' do
-      io = StringIO.new
-      mock_app do
-        use Rack::Protection, :logger => Logger.new(io)
-        run DummyApp
-      end
-      post('/', {}, 'rack.session' => {}, 'HTTP_ORIGIN' => 'http://malicious.com')
-      expect(io.string).to match(/prevented.*Origin/)
+    # @return [Array<Node>]
+    # @see #initialize
+    # @see Node#children
+    def children
+      [@before, @mid, @after].compact
     end
