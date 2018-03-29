e8c500fbdd6924904d1b463baaf25809321e4902
SUNsung <weatherius@gmail.com>
2018-03-30 02:44:15 +0300

    Rewrite [12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb] File in: 2018-03-30 02:44:14

--- ruby/12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb
+++ ruby/12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb
@@ -1,41 +1,127 @@
 
         
-        # puts '\nDone.'
-
+          def test_escape_javascript
+    assert_equal '', escape_javascript(nil)
+    assert_equal %(This \\'thing\\' is really\\n netos\\'), escape_javascript(%(This 'thing' is really\n netos'))
+    assert_equal %(backslash\\\\test), escape_javascript(%(backslash\\test))
+    assert_equal %(dont <\\/close> tags), escape_javascript(%(dont </close> tags))
+    assert_equal %(unicode &#x2028; newline), escape_javascript(%(unicode \342\200\250 newline).dup.force_encoding(Encoding::UTF_8).encode!)
+    assert_equal %(unicode &#x2029; newline), escape_javascript(%(unicode \342\200\251 newline).dup.force_encoding(Encoding::UTF_8).encode!)
     
-        s = SortedSet.new(['one', 'two', 'three', 'four'])
-    a = []
-    ret = s.reject! { |o| a << o; false }
-    assert_same(nil, ret)
-    assert_equal(['four', 'one', 'three', 'two'], s.to_a)
-    assert_equal(['four', 'one', 'three', 'two'], a)
+            def test_url_invalid_adapter
+          error = assert_raises(LoadError) do
+            spec 'ridiculous://foo?encoding=utf8'
+          end
+    
+      test 'helpers' do
+    assert_response_code_range 200..299, :successful?
+    assert_response_code_range [404],    :not_found?
+    assert_response_code_range 300..399, :redirection?
+    assert_response_code_range 500..599, :server_error?
+    assert_response_code_range 400..499, :client_error?
   end
     
-      it 'ignores NULL bytes between directives' do
-    array = 'hgfedcbadcfehgba'.unpack(unpack_format('\000', 2))
-    array.should == [7523094288207667809, 7233738012216484449]
-  end
+      class HeadTest < ActiveSupport::TestCase
+    test 'head works on its own' do
+      status = HeadController.action(:index).call(Rack::MockRequest.env_for('/')).first
+      assert_equal 404, status
+    end
     
-    describe :string_unpack_Aa, shared: true do
-  it 'decodes the number of bytes specified by the count modifier including NULL bytes' do
-    'a\x00bc'.unpack(unpack_format(3)+unpack_format).should == ['a\x00b', 'c']
-  end
+          add_delivery_method :smtp, Mail::SMTP,
+        address:              'localhost',
+        port:                 25,
+        domain:               'localhost.localdomain',
+        user_name:            nil,
+        password:             nil,
+        authentication:       nil,
+        enable_starttls_auto: true
     
-        it 'sets $! in outer ensure clause if inner ensure clause raises exception' do
-      ThreadSpecs.join_dying_thread_with_outer_ensure(@method) { ScratchPad.record $! }
-      ScratchPad.recorded.to_s.should == 'In dying thread'
+            def application_mailer_file_name
+          @_application_mailer_file_name ||= if mountable_engine?
+            'app/mailers/#{namespaced_path}/application_mailer.rb'
+          else
+            'app/mailers/application_mailer.rb'
+          end
+        end
     end
   end
+end
+
     
-      # Clean a top-level (bin, sbin, lib) directory, recursively, by fixing file
-  # permissions and removing .la files, unless the files (or parent
-  # directories) are protected by skip_clean.
-  #
-  # bin and sbin should not have any subdirectories; if either do that is
-  # caught as an audit warning
-  #
-  # lib may have a large directory tree (see Erlang for instance), and
-  # clean_dir applies cleaning rules to the entire tree
-  def clean_dir(d)
-    d.find do |path|
-      path.extend(ObserverPathnameExtension)
+          it 'does set the output directory' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            cloc(output_directory: '/tmp')
+          end').runner.execute(:test)
+    
+        def ==(other)
+      other.is_a?(self.class) && filters == other.filters
+    end
+    
+        def as_json
+      { name: name, path: path, type: type }
+    end
+  end
+end
+
+    
+        def initialize
+      @pages = {}
+    end
+    
+          def additional_entries
+        entries = []
+    
+            css('.method-signature', 'pre').each do |node|
+          node.name = 'pre'
+          node.content = node.content.strip
+          node['data-language'] = 'php'
+        end
+    
+    module RuboCop
+  module Cop
+    class VariableForce
+      # This class represents each assignment of a variable.
+      class Assignment
+        include Branchable
+    
+            def each_unnecessary_space_match(node, &blk)
+          each_match_range(
+            contents_range(node),
+            MULTIPLE_SPACES_BETWEEN_ITEMS_REGEX,
+            &blk
+          )
+        end
+      end
+    end
+  end
+end
+
+    
+    module RuboCop
+  module Cop
+    module Lint
+      # In math and Python, we can use `x < y < z` style comparison to compare
+      # multiple value. However, we can't use the comparison in Ruby. However,
+      # the comparison is not syntax error. This cop checks the bad usage of
+      # comparison operators.
+      #
+      # @example
+      #
+      #   # bad
+      #
+      #   x < y < z
+      #   10 <= x <= 20
+      #
+      # @example
+      #
+      #   # good
+      #
+      #   x < y && y < z
+      #   10 <= x && x <= 20
+      class MultipleCompare < Cop
+        MSG = 'Use the `&&` operator to compare multiple values.'.freeze
+    
+          expect('.prefix--moz-ms').to have_ruleset(rule)
+    end
+  end
+end
