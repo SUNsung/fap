d02bd7c5443fd709c2ce652a7eb77865cee49d05
SUNsung <weatherius@gmail.com>
2018-03-29 09:53:07 +0300

    Rewrite [21mwvZV80BpnQ2166uW53VX0Ac21DeQj8mSfRWU21MP2.rb] File in: 2018-03-29 09:53:06

--- ruby/21mwvZV80BpnQ2166uW53VX0Ac21DeQj8mSfRWU21MP2.rb
+++ ruby/21mwvZV80BpnQ2166uW53VX0Ac21DeQj8mSfRWU21MP2.rb
@@ -1,126 +1,128 @@
-CONTENT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+
+        
+          def relative_url_redirect_with_status_hash
+    redirect_to('/things/stuff', status: 301)
+  end
     
-    # You can also do other things. Examples:
-# https://github.com/tmm1/stackprof/blob/master/bin/stackprof
-report = StackProf::Report.new(Marshal.load(IO.binread(PROF_OUTPUT_FILE)))
-report.print_text(
-  options[:sort],
-  options[:limit],
-  options[:select_files],
-  options[:reject_files],
-  options[:select_names],
-  options[:reject_names]
-)
+            # Require the adapter itself and give useful feedback about
+        #   1. Missing adapter gems and
+        #   2. Adapter gems' missing dependencies.
+        path_to_adapter = 'action_cable/subscription_adapter/#{adapter}'
+        begin
+          require path_to_adapter
+        rescue LoadError => e
+          # We couldn't require the adapter itself. Raise an exception that
+          # points out config typos and missing gems.
+          if e.path == path_to_adapter
+            # We can assume that a non-builtin adapter was specified, so it's
+            # either misspelled or missing from Gemfile.
+            raise e.class, 'Could not load the '#{adapter}' Action Cable pubsub adapter. Ensure that the adapter is spelled correctly in config/cable.yml and that you've added the necessary adapter gem to your Gemfile.', e.backtrace
     
-    Mercenary.program(:jekyll) do |p|
-  p.version Jekyll::VERSION
-  p.description 'Jekyll is a blog-aware, static site generator in Ruby'
-  p.syntax 'jekyll <subcommand> [options]'
+            def test_spec_name_on_key_lookup
+          spec = spec(:readonly, 'readonly' => { 'adapter' => 'sqlite3' })
+          assert_equal 'readonly', spec.name
+        end
     
-      [name, full_name]
-end
+      test 'helpers' do
+    assert_response_code_range 200..299, :successful?
+    assert_response_code_range [404],    :not_found?
+    assert_response_code_range 300..399, :redirection?
+    assert_response_code_range 500..599, :server_error?
+    assert_response_code_range 400..499, :client_error?
+  end
     
-      def post_superenv_hacks
-    # Only allow Homebrew-approved directories into the PATH, unless
-    # a formula opts-in to allowing the user's path.
-    if formula.env.userpaths? || reqs.any? { |rq| rq.env.userpaths? }
-      ENV.userpaths!
+        test 'middleware stack accepts only and except as options' do
+      result = ActionsController.action(:show).call(env_for('/'))
+      assert_equal 'First!', result[1]['Middleware-Order']
+    
+        def wrap_delivery_behavior!(*args) # :nodoc:
+      self.class.wrap_delivery_behavior(message, *args)
     end
   end
-    
-            if Pathname::BOTTLE_EXTNAME_RX === file.to_s
-          version = bottle_resolve_version(file) rescue file.version
-        else
-          version = file.version
-        end
-        next unless version
-        next unless (name = file.basename.to_s[/(.*)-(?:#{Regexp.escape(version)})/, 1])
-    
-      def origin
-    Homebrew.git_origin || '(none)'
-  end
-    
-        first_warning = true
-    methods.each do |method|
-      unless checks.respond_to?(method)
-        Homebrew.failed = true
-        puts 'No check available by the name: #{method}'
-        next
-      end
-    
-          # Sign in a user that already was authenticated. This helper is useful for logging
-      # users in after sign up. All options given to sign_in is passed forward
-      # to the set_user method in warden.
-      #
-      # Examples:
-      #
-      #   sign_in :user, @user                      # sign_in(scope, resource)
-      #   sign_in @user                             # sign_in(resource)
-      #   sign_in @user, event: :authentication     # sign_in(resource, options)
-      #   sign_in @user, store: false               # sign_in(resource, options)
-      #
-      def sign_in(resource_or_scope, *args)
-        options  = args.extract_options!
-        scope    = Devise::Mapping.find_scope!(resource_or_scope)
-        resource = args.last || resource_or_scope
-    
-    # TODO:
-# group :mongoid do
-#   gem 'mongoid', '~> 4.0.0'
-# end
+end
 
     
-        if notice
-      set_flash_message! :notice, notice
-      true
-    end
-  end
-    
-      included do
-    include Oauthable
-    
-      def validate_option(method)
-    if self.respond_to? 'validate_#{method}'.to_sym
-      self.send('validate_#{method}'.to_sym)
-    else
-      false
-    end
-  end
-    
-      def index
-    set_table_sort sorts: %w[provider name global], default: { provider: :asc }
-    
-          respond_to do |format|
-        if new_credentials.map(&:save).all?
-          format.html { redirect_to user_credentials_path, notice: 'The file was successfully uploaded.'}
-        else
-          format.html { redirect_to user_credentials_path, notice: 'One or more of the uploaded credentials was not imported due to an error. Perhaps an existing credential had the same name?'}
-        end
+        # An email was generated.
+    def process(event)
+      debug do
+        mailer = event.payload[:mailer]
+        action = event.payload[:action]
+        '#{mailer}##{action}: processed outbound mail in #{event.duration.round(1)}ms'
       end
-    else
-      redirect_to user_credentials_path, notice: 'No file was chosen to be uploaded.' 
+    end
+    
+          private
+        def method_missing(method_name, *args)
+          if @mailer.action_methods.include?(method_name.to_s)
+            ActionMailer::Parameterized::MessageDelivery.new(@mailer, method_name, @params, *args)
+          else
+            super
+          end
+        end
+    
+            def application_mailer_file_name
+          @_application_mailer_file_name ||= if mountable_engine?
+            'app/mailers/#{namespaced_path}/application_mailer.rb'
+          else
+            'app/mailers/application_mailer.rb'
+          end
+        end
     end
   end
-    
-    # Single-line step scoper
-When /^(.*) within (.*[^:])$/ do |step, parent|
-  with_scope(parent) { When step }
 end
+
     
-        def type_from_file_command
-      @type_from_file_command ||=
-        FileCommandContentTypeDetector.new(@filepath).detect
+      def validate_type
+    errors.add(:type, 'cannot be changed once an instance has been created') if type_changed? && !new_record?
+    errors.add(:type, 'is not a valid type') unless self.class.valid_type?(type)
+  end
+    
+          _form_configurable_fields[name] = options
+    end
+    
+          respond_with do |format|
+        format.html { redirect_to admin_pods_path }
+        format.json { render json: PodPresenter.new(pod).as_json }
+      end
     end
   end
 end
+
+    
+          def http_error_page_as_json(e)
+        render json: {error: :invalid_request, error_description: e.message}, status: 400
+      end
+    
+          def validate!
+        super
+        raise Informative, 'Existing Podfile found in directory' unless config.podfile_path_in_dir(Pathname.pwd).nil?
+        if @project_path
+          help! 'Xcode project at #{@project_path} does not exist' unless File.exist? @project_path
+          project_path = @project_path
+        else
+          raise Informative, 'No Xcode project found, please specify one' unless @project_paths.length > 0
+          raise Informative, 'Multiple Xcode projects found, please specify one' unless @project_paths.length == 1
+          project_path = @project_paths.first
+        end
+        @xcode_project = Xcodeproj::Project.open(project_path)
+      end
+    
+    # usage rake isolate[my-post]
+desc 'Move all other posts than the one currently being worked on to a temporary stash location (stash) so regenerating the site happens much more quickly.'
+task :isolate, :filename do |t, args|
+  stash_dir = '#{source_dir}/#{stash_dir}'
+  FileUtils.mkdir(stash_dir) unless File.exist?(stash_dir)
+  Dir.glob('#{source_dir}/#{posts_dir}/*.*') do |post|
+    FileUtils.mv post, stash_dir unless post.include?(args.filename)
+  end
+end
+    
+        def initialize(tag_name, markup, tokens)
+      attributes = ['class', 'src', 'width', 'height', 'title']
+    
+        def initialize(tag_name, markup, tokens)
+      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
+      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
+      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
+      super
+    end
