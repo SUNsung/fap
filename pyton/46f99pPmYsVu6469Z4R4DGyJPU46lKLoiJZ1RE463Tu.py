2febdf9b2842f4be27b86fdea5926b77bdb1944a
SUNsung <weatherius@gmail.com>
2018-03-30 13:58:33 +0300

    Rewrite [46f99pPmYsVu6469Z4R4DGyJPU46lKLoiJZ1RE463Tu.py] File in: 2018-03-30 13:58:33

--- pyton/46f99pPmYsVu6469Z4R4DGyJPU46lKLoiJZ1RE463Tu.py
+++ pyton/46f99pPmYsVu6469Z4R4DGyJPU46lKLoiJZ1RE463Tu.py
@@ -1,107 +1,64 @@
 
         
-                Keyword:                   'bold #004461',   # class: 'k'
-        Keyword.Constant:          'bold #004461',   # class: 'kc'
-        Keyword.Declaration:       'bold #004461',   # class: 'kd'
-        Keyword.Namespace:         'bold #004461',   # class: 'kn'
-        Keyword.Pseudo:            'bold #004461',   # class: 'kp'
-        Keyword.Reserved:          'bold #004461',   # class: 'kr'
-        Keyword.Type:              'bold #004461',   # class: 'kt'
+        
+def side_effect(old_cmd, command):
+    with tarfile.TarFile(_tar_file(old_cmd.script_parts)[0]) as archive:
+        for file in archive.getnames():
+            try:
+                os.remove(file)
+            except OSError:
+                # does not try to remove directories as we cannot know if they
+                # already existed before
+                pass
+
+    
+            (foo, 2), p1
+        (bar, 3), p1
+        (foo, 3), p2
+        (bar, 10), p3
+        (foo, 1), p4
+    
+        def _dispatch_call(self, call, employees):
+        for employee in employees:
+            if employee.call is None:
+                employee.take_call(call)
+                return employee
+        return None
+    
+        def extract_max_priority_page(self):
+        '''Return the highest priority link in `links_to_crawl`.'''
+        pass
+    
+    +----------+     +----------+       +--------+     +-----------+    +---------+
+|          |  => | Work Ids |    => |        |  => | Call Q    | => |         |
+|          |     +----------+       |        |     +-----------+    |         |
+|          |     | ...      |       |        |     | ...       |    |         |
+|          |     | 6        |       |        |     | 5, call() |    |         |
+|          |     | 7        |       |        |     | ...       |    |         |
+| Process  |     | ...      |       | Local  |     +-----------+    | Process |
+|  Pool    |     +----------+       | Worker |                      |  #1..n  |
+| Executor |                        | Thread |                      |         |
+|          |     +----------- +     |        |     +-----------+    |         |
+|          | <=> | Work Items | <=> |        | <=  | Result Q  | <= |         |
+|          |     +------------+     |        |     +-----------+    |         |
+|          |     | 6: call()  |     |        |     | ...       |    |         |
+|          |     |    future  |     |        |     | 4, result |    |         |
+|          |     | ...        |     |        |     | 3, except |    |         |
++----------+     +------------+     +--------+     +-----------+    +---------+
+    
+    # If true, sectionauthor and moduleauthor directives will be shown in the
+# output. They are ignored by default.
+#show_authors = False
     
     
-def prepare_url(value):
-    # Issue #1483: Make sure the URL always has a trailing slash
-    httpbin_url = value.url.rstrip('/') + '/'
+def SendEventNotificationAsync( event_name,
+                                buffer_number = None,
+                                extra_data = None ):
+  event = EventNotification( event_name, buffer_number, extra_data )
+  event.Start()
+
     
-        def run(self):
-        try:
-            self.server_sock = self._create_socket_and_bind()
-            # in case self.port = 0
-            self.port = self.server_sock.getsockname()[1]
-            self.ready_event.set()
-            self._handle_requests()
-    
-    '''
-requests._internal_utils
-~~~~~~~~~~~~~~
-    
-        See https://github.com/requests/requests/issues/3772.
-    '''
-    text_200_chal = (b'HTTP/1.1 200 OK\r\n'
-                     b'Content-Length: 0\r\n'
-                     b'WWW-Authenticate: Digest nonce='6bf5d6e4da1ce66918800195d6b9130d''
-                     b', opaque='372825293d1c26955496c80ed6426e9e', '
-                     b'realm='me@kennethreitz.com', qop=auth\r\n\r\n')
-    
-            form_data = {
-            'viz_type': 'table',
-            'granularity': 'one+day',
-            'druid_time_origin': '',
-            'since': '7+days+ago',
-            'until': 'now',
-            'row_limit': 5000,
-            'include_search': 'false',
-            'metrics': ['count'],
-            'groupby': ['dim1', 'dim2d'],
-            'force': 'true',
-        }
-        # two groupby
-        url = ('/superset/explore_json/druid/{}/'.format(datasource_id))
-        resp = self.get_json_resp(url, {'form_data': json.dumps(form_data)})
-        self.assertEqual('Canada', resp['data']['records'][0]['dim1'])
-    
-    # If true, keep warnings as 'system message' paragraphs in the built documents.
-#keep_warnings = False
-    
-        def test_select_in_expression(self):
-        query = 'SELECT f1, (SELECT count(1) FROM t2) FROM t1'
-        self.assertEquals({'t1', 't2'}, self.extract_tables(query))
-    
-        sources = {}
-    
-        for c in i_datasource.columns:
-        new_c = c.copy()
-        new_c.table_id = datasource.id
-        logging.info('Importing column {} from the datasource: {}'.format(
-            new_c.to_json(), i_datasource.full_name))
-        imported_c = i_datasource.column_class.import_obj(new_c)
-        if (imported_c.column_name not in
-                [c.column_name for c in datasource.columns]):
-            datasource.columns.append(imported_c)
-    session.flush()
-    return datasource.id
-    
-        '''
-    
-    # revision identifiers, used by Alembic.
-revision = '18e88e1cc004'
-down_revision = '430039611635'
-    
-    from alembic import op
-import sqlalchemy as sa
-    
-        packages = find_packages('src'),
-    package_dir = {'' : 'src'},
-    
-    
-    {
-def extract_json_data(url, **params):
-    url = construct_url(url, **params)
-    html = get_content(url, headers=fake_headers)
-    json_string = match1(html, r'app.page\['board'\] = (.*?});')
-    json_data = json.loads(json_string)
-    return json_data
-    
-            mp3 = match1(content, r'name='filename'\s*value='([^']+\.mp3)'')
-        if mp3: mp3 = 'http://res.infoq.com/downloads/mp3downloads/%s' % mp3
-    
-    from ..common import *
-    
-        # mgid%3Auma%3Avideo%3Amtv81.com%3A897974
-    vid = match1(html, r'getTheVideo\('(.*?)'')
-    xml = parseString(
-        get_content('http://intl.esperanto.mtvi.com/www/xml/media/mediaGen.jhtml?uri={}&flashPlayer=LNX%2013,0,0,206&geo=CN&sid=123456'.format(vid)))
-    
-        with open('README.md') as readme:
-        readme_html = mistune.markdown(readme.read())
-        readme_soup = BeautifulSoup.BeautifulSoup(readme_html, 'html.parser')
+      if not p.isfile( build_file ):
+    sys.exit(
+      'File {0} does not exist; you probably forgot to run:\n'
+      '\tgit submodule update --init --recursive\n'.format( build_file ) )
