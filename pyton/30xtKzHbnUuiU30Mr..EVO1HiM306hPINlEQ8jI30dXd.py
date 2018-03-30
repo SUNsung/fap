5b8693cf2ea9016ed6206868c10f72b3b29bb010
SUNsung <weatherius@gmail.com>
2018-03-30 12:58:26 +0300

    Rewrite [30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py] File in: 2018-03-30 12:58:25

--- pyton/30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py
+++ pyton/30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py
@@ -1,158 +1,110 @@
 
         
-            def get_headers(self):
-        '''Return the headers' bytes.'''
-        return self.msg.headers.encode('utf8')
+            Reference: Armin Ronacher, 'Flask for Fun and for Profit' PyBay 2016.
+    '''
+    for name in find_modules('flaskr.blueprints'):
+        mod = import_string(name)
+        if hasattr(mod, 'bp'):
+            app.register_blueprint(mod.bp)
+    return None
     
-        '''
-    return path.replace('\\', '\\\\\\')
+    if __name__ == '__main__':
+    app.run()
+
     
-        if explicit_json and body and (not lexer or isinstance(lexer, TextLexer)):
-        # JSON response with an incorrect Content-Type?
-        try:
-            json.loads(body)  # FIXME: the body also gets parsed in json.py
-        except ValueError:
-            pass  # Nope
-        else:
-            lexer = pygments.lexers.get_lexer_by_name('json')
-    
-    
-class ExitStatus:
-    '''Exit status code constants.'''
-    OK = 0
-    ERROR = 1
-    PLUGIN_ERROR = 7
+        samples_range = np.linspace(10, 2000, 5).astype(np.int)
+    features_range = np.linspace(10, 2000, 5).astype(np.int)
+    results = compute_bench(samples_range, features_range)
     
     
-class PluginManager(object):
+def euclidean_distances(X, n_jobs):
+    return pairwise_distances(X, metric='euclidean', n_jobs=n_jobs)
     
+    plt.figure('scikit-learn Ward's method benchmark results')
+plt.imshow(np.log(ratio), aspect='auto', origin='lower')
+plt.colorbar()
+plt.contour(ratio, levels=[1, ], colors='k')
+plt.yticks(range(len(n_features)), n_features.astype(np.int))
+plt.ylabel('N features')
+plt.xticks(range(len(n_samples)), n_samples.astype(np.int))
+plt.xlabel('N samples')
+plt.title('Scikit's time, in units of scipy time (log)')
+plt.show()
+
     
-class FlaskyStyle(Style):
-    background_color = '#f8f8f8'
-    default_style = ''
+            if not in_exercise_region or linestrip.startswith('#'):
+            output_file.write(line)
     
-    
-def default_hooks():
-    return dict((event, []) for event in HOOKS)
-    
-        def __exit__(self, exc_type, exc_value, traceback):
-        if exc_type is None:
-            self.stop_event.wait(self.WAIT_EVENT_TIMEOUT)
-        else:
-            if self.wait_to_close_event:
-                # avoid server from waiting for event timeouts
-                # if an exception is found in the main thread
-                self.wait_to_close_event.set()
-    
-        def __init__(self, headers):
-        '''Make a MockResponse for `cookielib` to read.
-    
-            #: Dictionary mapping protocol or protocol and host to the URL of the proxy
-        #: (e.g. {'http': 'foo.bar:3128', 'http://host.name': 'foo.bar:4012'}) to
-        #: be used on each :class:`Request <Request>`.
-        self.proxies = {}
-    
-        close_server = threading.Event()
-    server = Server(response_handler, wait_to_close_event=close_server)
-    
-    # Backwards compat only
-try:
-    from hashlib import md5 as _md5
-except ImportError:
-    try:
-        from md5 import md5 as _md5
-    except ImportError:
-        # Assume we're running in FIPS mode here
-        _md5 = None
-    
-        @g_connect
-    def create_import_task(self, github_user, github_repo, reference=None, role_name=None):
-        '''
-        Post an import request
-        '''
-        url = '%s/imports/' % self.baseurl
-        args = {
-            'github_user': github_user,
-            'github_repo': github_repo,
-            'github_reference': reference if reference else ''
-        }
-        if role_name:
-            args['alternate_role_name'] = role_name
-        elif github_repo.startswith('ansible-role'):
-            args['alternate_role_name'] = github_repo[len('ansible-role') + 1:]
-        data = self.__call_galaxy(url, args=urlencode(args))
-        if data.get('results', None):
-            return data['results']
-        return data
-    
-            input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/targetHttpProxies'
-        actual = GCPUtils.parse_gcp_url(input_url)
-        self.assertEquals('compute', actual['service'])
-        self.assertEquals('v1', actual['api_version'])
-        self.assertEquals('myproject', actual['project'])
-        self.assertFalse('global' in actual)
-        self.assertEquals('targetHttpProxies', actual['resource_name'])
-    
-        def on_open_shell(self):
-        try:
-            for cmd in (b'set terminal length 0', b'set terminal width 512'):
-                self._exec_cli_command(cmd)
-            self._exec_cli_command(b'set terminal length %d' % self.terminal_length)
-        except AnsibleConnectionFailure:
-            raise AnsibleConnectionFailure('unable to set terminal parameters')
+    plt.show()
 
     
     
-def sort_groups(groups):
-    return sorted(groups, key=lambda g: (g.depth, g.priority, g.name))
+def plot_calibration_curve(est, name, fig_index):
+    '''Plot calibration curve for est w/o and with calibration. '''
+    # Calibrated with isotonic calibration
+    isotonic = CalibratedClassifierCV(est, cv=2, method='isotonic')
     
-        def __init__(self, blackboard):
-        self.blackboard = blackboard
+    # Plot modifications of calibrator
+for i in range(prediction.shape[0]):
+    plt.arrow(p[i, 0], p[i, 1],
+              prediction[i, 0] - p[i, 0], prediction[i, 1] - p[i, 1],
+              head_width=1e-2, color=colors[np.argmax(p[i])])
+# Plot boundaries of unit simplex
+plt.plot([0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], 'k', label='Simplex')
     
-        def on_message(self, message_type):  # message ignored
-        if message_type in self.message_types.keys():
-            self.message_types[message_type]()
-        else:
-            raise UnsupportedMessageType
+    # Author: Alexandre Gramfort <alexandre.gramfort@inria.fr>
+# License: BSD 3 clause
     
-        def test_car_adapter_shall_make_very_loud_noise(self):
-        car = Car()
-        car_adapter = Adapter(car, make_noise=car.make_noise)
-        noise = car_adapter.make_noise(10)
-        expected_noise = 'vroom!!!!!!!!!!'
     
-        def unsubscribe(self, msg):
-        self.provider.unsubscribe(msg, self)
+class HierachicalStateMachine(object):
     
-        def tearDown(self):
-        self.out.close()
-        sys.stdout = self.saved_stdout
+        def __new__(cls, name, bases, attrs):
+        new_cls = type.__new__(cls, name, bases, attrs)
+        '''
+            Here the name of the class is used as key but it could be any class
+            parameter.
+        '''
+        cls.REGISTRY[new_cls.__name__] = new_cls
+        return new_cls
+    
+        def setUp(self):
+        self.e, self.g = get_localizer(language='English'), \
+                         get_localizer(language='Greek')
+    
+    Test code which will almost always fail (if not exactly 12:01) when untestable
+production code (production code time provider is datetime) is used:
     
         def test_display_current_time_at_current_time(self):
         '''
-        Just as justification for working example. (Will always pass.)
+        Just as justification for working example with the time provider used in
+        production. (Will always pass.)
         '''
         production_code_time_provider = ProductionCodeTimeProvider()
-        class_under_test = TimeDisplay(production_code_time_provider)
+        class_under_test = TimeDisplay()
         current_time = datetime.datetime.now()
         expected_time = '<span class=\'tinyBoldText\'>{}:{}</span>'.format(current_time.hour, current_time.minute)
-        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
-    
-    production code which is untestable:
+        self.assertEqual(class_under_test.get_current_time_as_html_fragment(production_code_time_provider), expected_time)
     
     
-@coroutine
-def coroutine2(target):
-    while True:
-        request = yield
-        if 10 < request <= 20:
-            print('request {} handled in coroutine 2'.format(request))
+def count_to(count):
+    '''Counts by word numbers, up to a maximum of five'''
+    numbers = ['one', 'two', 'three', 'four', 'five']
+    for number in numbers[:count]:
+        yield number
+    
+        def tearDown(self):
+        if not self._bProblem:
+            print('Tearing down')
+            time.sleep(0.1)
+            self._tm.publishReport()
         else:
-            target.send(request)
+            print('Test not executed. No tear down required.')
     
-    import random
-import time
-    
-    
-if __name__ == '__main__':
-    main()
+        def __get__(self, obj, T):
+        def transaction(*args, **kwargs):
+            state = memento(obj)
+            try:
+                return self.method(obj, *args, **kwargs)
+            except Exception as e:
+                state()
+                raise e
