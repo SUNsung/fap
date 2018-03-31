c9457bc91f43c0a5bb3ee5199b8a5b6fdaf0988a
SUNsung <weatherius@gmail.com>
2018-03-31 21:44:44 +0300

    Rewrite [535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb] File in: 2018-03-31 21:44:43

--- ruby/535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb
+++ ruby/535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb
@@ -1,131 +1,147 @@
 
         
-        class BuildEnvironment
-  def initialize(*settings)
-    @settings = Set.new(*settings)
-  end
-    
-          export JAVA_HOME='$(/usr/libexec/java_home)'
-      export AWS_ACCESS_KEY='<Your AWS Access ID>'
-      export AWS_SECRET_KEY='<Your AWS Secret Key>'
-      export #{home_name}='#{home_value}'
-    EOS
-  end
-end
-
-    
-      def failed_strategy
-    request.respond_to?(:get_header) ? request.get_header('omniauth.error.strategy') : request.env['omniauth.error.strategy']
-  end
-    
-        def unlock_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :unlock_instructions, opts)
-    end
-    
-      def test_file_share_delete
-    Dir.mktmpdir(__method__.to_s) do |tmpdir|
-      tmp = File.join(tmpdir, 'x')
-      File.open(tmp, mode: IO::WRONLY | IO::CREAT | IO::BINARY | IO::SHARE_DELETE) do |f|
-        assert_file.exist?(tmp)
-        assert_nothing_raised do
-          File.unlink(tmp)
-        end
-      end
-      assert_file.not_exist?(tmp)
-    end
-  end
-    
-      def test_filter
-    ENV['test'] = 'foo'
-    h = ENV.filter {|k| IGNORE_CASE ? k.upcase == 'TEST' : k == 'test' }
-    assert_equal(1, h.size)
-    k = h.keys.first
-    v = h.values.first
-    if IGNORE_CASE
-      assert_equal('TEST', k.upcase)
-      assert_equal('FOO', v.upcase)
-    else
-      assert_equal('test', k)
-      assert_equal('foo', v)
-    end
-  end
-    
-      it 'adds nil for each element requested beyond the end of the String' do
-    [ ['',          [nil, nil, nil]],
-      ['abcde',     [1684234849, nil, nil]],
-      ['abcdefg',   [1684234849, nil, nil]],
-      ['abcdefgh',  [1684234849, 1751606885, nil]]
-    ].should be_computed_by(:unpack, unpack_format(3))
-  end
-    
-      def self.critical_is_reset
-    # Create another thread to verify that it can call Thread.critical=
-    t = Thread.new do
-      initial_critical = Thread.critical
-      Thread.critical = true
-      Thread.critical = false
-      initial_critical == false && Thread.critical == false
-    end
-    v = t.value
-    t.join
-    v
-  end
-    
-        t.join
-  end
-    
-        10.times { sleep 0.1 if after_sleep1 != true }
-    10.times { sleep 0.1 if t.status and t.status != 'sleep' }
-    after_sleep2.should == false # t should be blocked on the second sleep
-    t.send(@method)
-    
-        def log_processed(name)
-      puts green '    #{name}'
-    end
-    
-      # Disable Rails's static asset server (Apache or nginx will already do this).
-  if config.respond_to?(:serve_static_files)
-    # rails >= 4.2
-    config.serve_static_files = true
-  elsif config.respond_to?(:serve_static_assets)
-    # rails < 4.2
-    config.serve_static_assets = true
-  end
-    
-            def type_allowed?(type)
-          @subject.send('#{@attachment_name}_content_type=', type)
-          @subject.valid?
-          @subject.errors[:'#{@attachment_name}_content_type'].blank?
-        end
-    
-            def no_error_when_valid?
-          @file = StringIO.new('.')
-          @subject.send(@attachment_name).assign(@file)
-          @subject.valid?
-          expected_message = [
-            @attachment_name.to_s.titleize,
-            I18n.t(:blank, scope: [:errors, :messages])
-          ].join(' ')
-          @subject.errors.full_messages.exclude?(expected_message)
-        end
+          # Removes any empty directories in the formula's prefix subtree
+  # Keeps any empty directions projected by skip_clean
+  # Removes any unresolved symlinks
+  def prune
+    dirs = []
+    symlinks = []
+    @f.prefix.find do |path|
+      if path == @f.libexec || @f.skip_clean?(path)
+        Find.prune
+      elsif path.symlink?
+        symlinks << path
+      elsif path.directory?
+        dirs << path
       end
     end
-  end
-end
-
     
-            def matches? subject
-          @subject = subject
-          @subject = @subject.new if @subject.class == Class
-          lower_than_low? && higher_than_low? && lower_than_high? && higher_than_high?
-        end
-    
-    module Paperclip
-  require 'rails'
-    
-            def on_block(node)
-          on_body_of_reduce(node) do |body|
-            void_next = body.each_node(:next).find do |n|
-              n.children.empty? && parent_block_node(n) == node
+        def self.rm_DS_Store
+      paths = Queue.new
+      %w[Cellar Frameworks Library bin etc include lib opt sbin share var].
+        map { |p| HOMEBREW_PREFIX/p }.each { |p| paths << p if p.exist? }
+      workers = (0...Hardware::CPU.cores).map do
+        Thread.new do
+          begin
+            while p = paths.pop(true)
+              quiet_system 'find', p, '-name', '.DS_Store', '-delete'
             end
+          rescue ThreadError # ignore empty queue error
+          end
+        end
+      end
+      workers.map(&:join)
+    end
+    
+      def all_projects(current_user)
+    projects = []
+    
+      module ClassMethods
+    def load_types_in(module_name, my_name = module_name.singularize)
+      const_set(:MODULE_NAME, module_name)
+      const_set(:BASE_CLASS_NAME, my_name)
+      const_set(:TYPES, Dir[Rails.root.join('app', 'models', module_name.underscore, '*.rb')].map { |path| module_name + '::' + File.basename(path, '.rb').camelize })
+    end
+    
+      def complete_option(method)
+    if self.respond_to? 'complete_#{method}'.to_sym
+      self.send('complete_#{method}'.to_sym)
+    end
+  end
+    
+      included do
+    helper SortableTableHelper
+  end
+    
+      def index
+    if params[:agent_id]
+      @agent = current_user.agents.find(params[:agent_id])
+      @events = @agent.events.page(params[:page])
+    else
+      @events = current_user.events.preload(:agent).page(params[:page])
+    end
+    
+        respond_to do |format|
+      if !running? && @job.update_attributes!(run_at: Time.now, failed_at: nil)
+        format.html { redirect_to jobs_path, notice: 'Job enqueued.' }
+        format.json { render json: @job, status: :ok }
+      else
+        format.html { redirect_to jobs_path, alert: 'Can not enqueue a running job.' }
+        format.json { render json: @job.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+    
+        respond_to do |format|
+      format.html { redirect_to services_path }
+      format.json { head :no_content }
+    end
+  end
+    
+        def initialize(name = nil, path = nil, type = nil)
+      self.name = name
+      self.path = path
+      self.type = type
+    
+        def relative_path_from(url)
+      self.class.parse(url).relative_path_to(self)
+    end
+  end
+end
+
+    
+            css('p > code:first-child:last-child', 'td > code:first-child:last-child').each do |node|
+          next if node.previous.try(:content).present? || node.next.try(:content).present?
+          node.inner_html = node.inner_html.squish.gsub(/<br(\ \/)?>\s*/, '\n')
+          node.content = node.content.strip
+          node.name = 'pre' if node.content =~ /\s/
+          node.parent.before(node.parent.children).remove if node.parent.name == 'p'
+        end
+    
+            css('.method-name', '.property-name').each do |node|
+          source = node.at_css('a')
+          source.before(%(<span class='name'>#{source.content}</span>))
+          source.content = 'source'
+          source['class'] = 'source'
+        end
+    
+        # @abstract
+    #
+    # Update the clone on the deployment target
+    #
+    # @return void
+    #
+    def update
+      raise NotImplementedError, 'Your SCM strategy module should provide a #update method'
+    end
+    
+      it 'overrides the rake method, and sets the sshkit_backend to SSHKit::Backend::Printer' do
+    capture_io do
+      flags '--dry-run', '-n'
+    end
+    sshkit_backend = Capistrano::Configuration.fetch(:sshkit_backend)
+    expect(sshkit_backend).to eq(SSHKit::Backend::Printer)
+  end
+    
+          class HaveAttachedFileMatcher
+        def initialize attachment_name
+          @attachment_name = attachment_name
+        end
+    
+        Hash.new.tap do |missing_styles|
+      current_styles.each do |klass, attachment_definitions|
+        attachment_definitions.each do |attachment_name, styles|
+          registered = registered_styles[klass][attachment_name] || [] rescue []
+          missed = styles - registered
+          if missed.present?
+            klass_sym = klass.to_s.to_sym
+            missing_styles[klass_sym] ||= Hash.new
+            missing_styles[klass_sym][attachment_name.to_sym] ||= Array.new
+            missing_styles[klass_sym][attachment_name.to_sym].concat(missed.to_a)
+            missing_styles[klass_sym][attachment_name.to_sym].map!(&:to_s).sort!.map!(&:to_sym).uniq!
+          end
+        end
+      end
+    end
+  end
+end
