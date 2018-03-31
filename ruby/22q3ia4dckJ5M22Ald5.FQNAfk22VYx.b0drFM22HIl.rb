669ff240d1ba4d59c48ff327051dcce91deb26a6
SUNsung <weatherius@gmail.com>
2018-03-31 05:02:23 +0300

    Rewrite [22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb] File in: 2018-03-31 05:02:22

--- ruby/22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb
+++ ruby/22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb
@@ -1,119 +1,84 @@
 
         
-                def tag_option(key, value, escape)
-          if value.is_a?(Array)
-            value = escape ? safe_join(value, ' '.freeze) : value.join(' '.freeze)
-          else
-            value = escape ? ERB::Util.unwrapped_html_escape(value) : value.to_s
-          end
-          %(#{key}='#{value.gsub('''.freeze, '&quot;'.freeze)}')
-        end
-    
-          get :redirect_to_with_block_and_options
-    
-          def handle_exception_with_mailer_class(exception)
-        if klass = mailer_class
-          klass.handle_exception exception
-        else
-          raise exception
-        end
-      end
-  end
-end
-
-    
-          text.split.each do |word|
-        if sentences.first.present? && (sentences.last + [word]).join(' ').length > len
-          sentences << [word]
-        else
-          sentences.last << word
-        end
-      end
-    
-            def initialize_test_deliveries
-          set_delivery_method :test
-          @old_perform_deliveries = ActionMailer::Base.perform_deliveries
-          ActionMailer::Base.perform_deliveries = true
-          ActionMailer::Base.deliveries.clear
-        end
-    
-      #
-  # Test assert_select_email
-  #
-    
-      test 'default file delivery settings (with Rails.root)' do
-    settings = { location: '#{Rails.root}/tmp/mails' }
-    assert_equal settings, ActionMailer::Base.file_settings
+          it 'adds nil for each element requested beyond the end of the String' do
+    [ ['',          [nil, nil, nil]],
+      ['dcbae',     [1684234849, nil, nil]],
+      ['dcbaefg',   [1684234849, nil, nil]],
+      ['dcbahgfe',  [1684234849, 1751606885, nil]]
+    ].should be_computed_by(:unpack, unpack_format(3))
   end
     
-        def add(entry)
-      if entry.is_a? Array
-        entry.each(&method(:add))
+        def metadata_subdir(leaf, version: self.version, timestamp: :latest, create: false)
+      if create && timestamp == :latest
+        raise CaskError, 'Cannot create metadata subdir when timestamp is :latest.'
+      end
+    
+        # we assume that the first file that requires 'sinatra' is the
+    # app_file. all other path related options are calculated based
+    # on this path by default.
+    set :app_file, caller_files.first || $0
+    
+    module Rack
+  module Protection
+    ##
+    # Prevented attack::   Cookie Tossing
+    # Supported browsers:: all
+    # More infos::         https://github.com/blog/1466-yummy-cookies-across-domains
+    #
+    # Does not accept HTTP requests if the HTTP_COOKIE header contains more than one
+    # session cookie. This does not protect against a cookie overflow attack.
+    #
+    # Options:
+    #
+    # session_key:: The name of the session cookie (default: 'rack.session')
+    class CookieTossing < Base
+      default_reaction :deny
+    
+      it 'should allow changing the protection mode' do
+    # I have no clue what other modes are available
+    mock_app do
+      use Rack::Protection::FrameOptions, :frame_options => :deny
+      run DummyApp
+    end
+    
+      it 'should not leak changes to env' do
+    klass    = described_class
+    detector = Struct.new(:app) do
+      def call(env)
+        was = env.dup
+        res = app.call(env)
+        was.each do |k,v|
+          next if env[k] == v
+          fail 'env[#{k.inspect}] changed from #{v.inspect} to #{env[k].inspect}'
+        end
+        res
+      end
+    end
+    
+        def blank_name?
+      @filepath.nil? || @filepath.empty?
+    end
+    
+        def cropping dst, ratio, scale
+      if ratio.horizontal? || ratio.square?
+        '%dx%d+%d+%d' % [ dst.width, dst.height, 0, (self.height * scale - dst.height) / 2 ]
       else
-        add_entry(entry) unless entry.root?
+        '%dx%d+%d+%d' % [ dst.width, dst.height, (self.width * scale - dst.width) / 2, 0 ]
       end
     end
     
-              # Built-in events
-          if node['id'] == 'Events-catalog'
-            node.next_element.css('li').each do |li|
-              name = '#{li.at_css('b').content.delete(''').strip} event'
-              id = name.parameterize
-              li['id'] = id
-              entries << [name, id, type] unless name == entries.last[0]
-            end
-            next
-          end
+          class ValidateAttachmentContentTypeMatcher
+        def initialize attachment_name
+          @attachment_name = attachment_name
+          @allowed_types = []
+          @rejected_types = []
+        end
     
-          def additional_entries
-        return [] if root_page?
-        entries = []
+        def load_processor(name)
+      if defined?(Rails.root) && Rails.root
+        filename = '#{name.to_s.underscore}.rb'
+        directories = %w(lib/paperclip lib/paperclip_processors)
     
-    def ask(message, valid_options)
-  if valid_options
-    answer = get_stdin('#{message} #{valid_options.to_s.gsub(/'/, '').gsub(/, /,'/')} ') while !valid_options.include?(answer)
-  else
-    answer = get_stdin(message)
-  end
-  answer
-end
-    
-      # Used on the blog index to split posts on the <!--more--> marker
-  def excerpt(input)
-    if input.index(/<!--\s*more\s*-->/i)
-      input.split(/<!--\s*more\s*-->/i)[0]
-    else
-      input
-    end
-  end
-    
-        def poster
-      'poster='#{@poster}'' if @poster
-    end
-    
-        def unlink_files(files)
-      Array(files).each do |file|
-        file.close unless file.closed?
-        file.unlink if file.respond_to?(:unlink) && file.path.present? && File.exist?(file.path)
-      end
-    end
-    
-        def self.register(klass, attachment_name, attachment_options)
-      instance.register(klass, attachment_name, attachment_options)
-    end
-    
-      context 'called with one color' do
-    it 'applies same width to all sides' do
-      rule = 'border-width: 1px'
-    
-      context 'called with arguments (2, $value: 4em 6em)' do
-    it 'outputs sextuple the second value from the default scale' do
-      expect('.two-double-value').to have_rule('font-size: 3.125em')
-    end
-  end
-end
-
-    
-          expect('.size-implicit').to have_ruleset(rule)
-    end
-  end
+        module Statements
+      def add_attachment(table_name, *attachment_names)
+        raise ArgumentError, 'Please specify attachment name in your add_attachment call in your migration.' if attachment_names.empty?
