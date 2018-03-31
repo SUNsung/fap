a42ae7d4abd5f2d3502b6b5e69e3e26250b785f8
SUNsung <weatherius@gmail.com>
2018-03-31 11:48:53 +0300

    Rewrite [58yyJlKlvbREg58UmTVuSHhf.582FWBMyACIM58mQF.rb] File in: 2018-03-31 11:48:52

--- ruby/58yyJlKlvbREg58UmTVuSHhf.582FWBMyACIM58mQF.rb
+++ ruby/58yyJlKlvbREg58UmTVuSHhf.582FWBMyACIM58mQF.rb
@@ -1,13 +1,180 @@
 
         
-              def installed_gem_version(gem_name)
-        Gem.loaded_specs[gem_name].version
+            it 'should have a way of getting the service configurations' do
+      configs = key.service_configs_for(Spaceship::Portal::Key::MUSIC_KIT_ID)
+      expect(configs).to be_instance_of(Array)
+      expect(configs.sample).to be_instance_of(Hash)
+      expect(configs.first['identifier']).to eq('music.com.snatchev.test')
+    end
+    
+          command = ['git describe']
+      command << '--tags' if match_lightweight
+      command << '`git rev-list --tags#{tag_pattern_param} --max-count=1`'
+      Actions.sh(command.compact.join(' '), log: false).chomp
+    rescue
+      nil
+    end
+    
+            expect(result).to eq('svn info | grep Revision | egrep -o '[0-9]+'')
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('svn info | grep Revision | egrep -o '[0-9]+'')
+      end
+    end
+    
+        ret = set + [2,4,6]
+    assert_not_same(set, ret)
+    assert_equal(Set[1,2,3,4,6], ret)
+  end
+    
+      class Apple < Struct; end
+    
+      def self.dying_thread_ensures(kill_method_name=:kill)
+    Thread.new do
+      Thread.current.report_on_exception = false
+      begin
+        Thread.current.send(kill_method_name)
+      ensure
+        yield
+      end
+    end
+  end
+    
+        it 'propagates inner exception to Thread.join if there is an outer ensure clause' do
+      thread = ThreadSpecs.dying_thread_with_outer_ensure(@method) { }
+      lambda { thread.join }.should raise_error(RuntimeError, 'In dying thread')
+    end
+    
+    (allow file-ioctl)
+(allow sysctl-read)
+(allow mach-lookup)
+(allow ipc-posix-shm)
+(allow process-fork)
+(allow system-socket)
+    
+            def validate!
+          super
+          if @pod_name.nil? && !@wipe_all
+            # Security measure, to avoid removing the pod cache too agressively by mistake
+            help! 'You should either specify a pod name or use the --all flag'
+          end
+        end
+    
+          def plugins_string
+        UI::ErrorReport.plugins_string
       end
     
-        # Fetch a var from the context
-    # @param [Symbol] variable The variable to fetch
-    # @param [Object] default  The default value if not found
-    #
-    def fetch(*args)
-      context.fetch(*args)
+             RUBY
+                         else
+                           <<-RUBY
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+    
+            def initialize(argv)
+          @name = argv.shift_argument
+          @template_url = argv.option('template-url', TEMPLATE_REPO)
+          super
+          @additional_args = argv.remainder!
+        end
+    
+    desc 'copy dot files for deployment'
+task :copydot, :source, :dest do |t, args|
+  FileList['#{args.source}/**/.*'].exclude('**/.', '**/..', '**/.DS_Store', '**/._*').each do |file|
+    cp_r file, file.gsub(/#{args.source}/, '#{args.dest}') unless File.directory?(file)
+  end
+end
+    
+    def config_tag(config, key, tag=nil, classname=nil)
+  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
+  tag       ||= 'div'
+  classname ||= key.sub(/_/, '-').sub(/\./, '-')
+  output      = '<#{tag} class='#{classname}''
+    
+        def render(context)
+      if parts = @text.match(/([a-zA-Z\d]*) (.*)/)
+        gist, file = parts[1].strip, parts[2].strip
+      else
+        gist, file = @text.strip, ''
+      end
+      if gist.empty?
+        ''
+      else
+        script_url = script_url_for gist, file
+        code       = get_cached_gist(gist, file) || get_gist_from_web(gist, file)
+        html_output_for script_url, code
+      end
     end
+    
+      class IncludeArrayTag < Liquid::Tag
+    Syntax = /(#{Liquid::QuotedFragment}+)/
+    def initialize(tag_name, markup, tokens)
+      if markup =~ Syntax
+        @array_name = $1
+      else
+        raise SyntaxError.new('Error in tag 'include_array' - Valid syntax: include_array [array from _config.yml]')
+      end
+    
+      class IncludeCodeTag < Liquid::Tag
+    def initialize(tag_name, markup, tokens)
+      @title = nil
+      @file = nil
+      if markup.strip =~ /\s*lang:(\S+)/i
+        @filetype = $1
+        markup = markup.strip.sub(/lang:\S+/i,'')
+      end
+      if markup.strip =~ /(.*)?(\s+|^)(\/*\S+)/i
+        @title = $1 || nil
+        @file = $3
+      end
+      super
+    end
+    
+        def add_required_validations
+      options = Paperclip::Attachment.default_options.deep_merge(@options)
+      if options[:validate_media_type] != false
+        name = @name
+        @klass.validates_media_type_spoof_detection name,
+          :if => ->(instance){ instance.send(name).dirty? }
+      end
+    end
+    
+            def error_when_not_valid?
+          @subject.send(@attachment_name).assign(nil)
+          @subject.valid?
+          @subject.errors[:'#{@attachment_name}'].present?
+        end
+    
+          if defined?(ActiveRecord)
+        Paperclip.options[:logger] = ActiveRecord::Base.logger
+        ActiveRecord::Base.send(:include, Paperclip::Glue)
+      end
+    end
+  end
+end
+
+    
+    module RuboCop
+  module Cop
+    class VariableForce
+      # A Variable represents existence of a local variable.
+      # This holds a variable declaration node,
+      # and some states of the variable.
+      class Variable
+        VARIABLE_DECLARATION_TYPES =
+          (VARIABLE_ASSIGNMENT_TYPES + ARGUMENT_DECLARATION_TYPES).freeze
+    
+            def_node_matcher :multiple_compare?, <<-PATTERN
+          (send (send _ {:< :> :<= :>=} $_) {:< :> :<= :>=} _)
+        PATTERN
+    
+            def comparison?(node)
+          simple_comparison?(node) || nested_comparison?(node)
+        end
+      end
+    end
+  end
+end
+
+    
+              each_misplaced_optional_arg(arguments) do |argument|
+            add_offense(argument)
+          end
+        end
