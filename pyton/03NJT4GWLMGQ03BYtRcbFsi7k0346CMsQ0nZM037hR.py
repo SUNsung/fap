b264dec99ccef74cba496d99afb10e0d3330c991
SUNsung <weatherius@gmail.com>
2018-03-31 22:22:52 +0300

    Rewrite [03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py] File in: 2018-03-31 22:22:51

--- pyton/03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py
+++ pyton/03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py
@@ -1,41 +1,76 @@
 
         
-            def test_key_error(self):
-        self.assertRaises(errors.Error, self._call, 'unknown_enhancement')
+        
+def test_messages(client, app):
+    '''Test that messages work'''
+    login(client, app.config['USERNAME'],
+          app.config['PASSWORD'])
+    rv = client.post('/add', data=dict(
+        title='<Hello>',
+        text='<strong>HTML</strong> allowed here'
+    ), follow_redirects=True)
+    assert b'No entries here so far' not in rv.data
+    assert b'&lt;Hello&gt;' in rv.data
+    assert b'<strong>HTML</strong> allowed here' in rv.data
+
     
-    from certbot import interfaces
-from certbot.plugins import common
+        A microblogging application written with Flask and sqlite3.
     
-      Args:
-    filename: The name of the current file.
-    clean_lines: A CleansedLines instance containing the file.
-    linenum: The number of the line to check.
-    error: The function to call with any errors found.
-  '''
     
-            # morsel/dict
-        morsel = cookies.Morsel()
-        morsel.set(*base_case)
-        morsel.update(attribs)
-        self.assertTrue(morsel == dict(morsel))
-        self.assertFalse(morsel != dict(morsel))
+@pytest.fixture()
+def chdir(tmpdir):
+    '''Change to pytest-provided temporary directory'''
+    tmpdir.chdir()
+
     
-        def parseURI(self, uri):
-        if self.entityResolver:
-            input = self.entityResolver.resolveEntity(None, uri)
-        else:
-            input = DOMEntityResolver().resolveEntity(None, uri)
-        return self.parse(input)
+        def run(self):
+        if not self.future.set_running_or_notify_cancel():
+            return
     
-        def test_varargs1_kw(self):
-        self.assertRaises(TypeError, {}.__contains__, x=2)
+    def _add_call_item_to_queue(pending_work_items,
+                            work_ids,
+                            call_queue):
+    '''Fills call_queue with _WorkItems from pending_work_items.
     
-            with self.assertRaises(AttributeError):
-            rangeobj.start = 0
-        with self.assertRaises(AttributeError):
-            rangeobj.stop = 10
-        with self.assertRaises(AttributeError):
-            rangeobj.step = 1
+      current_filepath = vimsupport.GetBufferFilepath( current_buffer )
+  line, column = vimsupport.CurrentLineAndColumn()
     
-            a[0], a[-1] = 200, -200
-        self.assertEqual(x[:], a.tolist())
+    
+def _FormatYcmdDebugInfo( ycmd ):
+  python = ycmd[ 'python' ]
+  clang = ycmd[ 'clang' ]
+  message = ( 'Server Python interpreter: {0}\n'
+              'Server Python version: {1}\n'
+              'Server has Clang support compiled in: {2}\n'
+              'Clang version: {3}\n'.format( python[ 'executable' ],
+                                             python[ 'version' ],
+                                             clang[ 'has_support' ],
+                                             clang[ 'version' ] ) )
+  extra_conf = ycmd[ 'extra_conf' ]
+  extra_conf_path = extra_conf[ 'path' ]
+  if not extra_conf_path:
+    message += 'No extra configuration file found\n'
+  elif not extra_conf[ 'is_loaded' ]:
+    message += ( 'Extra configuration file found but not loaded\n'
+                 'Extra configuration path: {0}\n'.format( extra_conf_path ) )
+  else:
+    message += ( 'Extra configuration file found and loaded\n'
+                 'Extra configuration path: {0}\n'.format( extra_conf_path ) )
+  return message
+    
+          with HandleServerException( display = False ):
+        BaseRequest.GetDataFromHandler( 'healthy' )
+
+    
+        def test_display_current_time_at_midnight(self):
+        '''
+        Will almost always fail (despite of right at/after midnight).
+        '''
+        time_provider_stub = MidnightTimeProvider()
+        class_under_test = TimeDisplay(time_provider_stub)
+        expected_time = '<span class=\'tinyBoldText\'>24:01</span>'
+        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
+    
+        def restore():
+        obj.__dict__.clear()
+        obj.__dict__.update(state)
