8b0a45f7222791674fd3756b48fab3c2cd4bcaac
SUNsung <weatherius@gmail.com>
2018-03-31 13:16:50 +0300

    Rewrite [430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb] File in: 2018-03-31 13:16:49

--- ruby/430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb
+++ ruby/430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb
@@ -1,44 +1,84 @@
 
         
-              if cask_version.empty?
-        raise CaskError, 'Cannot create metadata path with empty version.'
-      end
+            Or via the Cask:
+      brew cask install ngrok
+    EOS
+  end
+end
+
     
-          private
+      # True if a {Formula} is being built without a specific option.
+  # <pre>args << '--no-spam-plz' if build.without? 'spam'
+  def without?(val)
+    !with?(val)
+  end
     
-      entries = [{ template: deploy_rb, file: config_dir.join('deploy.rb') }]
-  entries += envs.split(',').map { |stage| { template: stage_rb, file: deploy_dir.join('#{stage}.rb') } }
-    
-      describe 'role_properties()' do
-    before do
-      dsl.role :redis, %w[example1.com example2.com], redis: { port: 6379, type: :slave }
-      dsl.server 'example1.com', roles: %w{web}, active: true, web: { port: 80 }
-      dsl.server 'example2.com', roles: %w{web redis}, web: { port: 81 }, redis: { type: :master }
-      dsl.server 'example3.com', roles: %w{app}, primary: true
+          update_disk_cleanup_size(path.disk_usage)
     end
     
-        def add_required_validations
-      options = Paperclip::Attachment.default_options.deep_merge(@options)
-      if options[:validate_media_type] != false
-        name = @name
-        @klass.validates_media_type_spoof_detection name,
-          :if => ->(instance){ instance.send(name).dirty? }
-      end
-    end
-    
-            def failure_message_when_negated
-          'Attachment #{@attachment_name} cannot be between #{@low} and #{@high} bytes'
+          case status
+      when 'A', 'D'
+        @report[status.to_sym] << tap.formula_file_to_name(src)
+      when 'M'
+        begin
+          formula = Formulary.factory(tap.path/src)
+          new_version = formula.pkg_version
+          old_version = FormulaVersions.new(formula).formula_at_revision(@initial_revision, &:pkg_version)
+          next if new_version == old_version
+        rescue Exception => e
+          onoe e if ARGV.homebrew_developer?
         end
-        alias negative_failure_message failure_message_when_negated
+        @report[:M] << tap.formula_file_to_name(src)
+      when /^R\d{0,3}/
+        @report[:D] << tap.formula_file_to_name(src) if tap.formula_file?(src)
+        @report[:A] << tap.formula_file_to_name(dst) if tap.formula_file?(dst)
+      end
+    end
     
+      def python(_options = {}, &block)
+    opoo 'Formula#python is deprecated and will go away shortly.'
+    block.call if block_given?
+    PythonRequirement.new
+  end
+  alias_method :python2, :python
+  alias_method :python3, :python
+end
+
     
-    {  # Returns hash with styles missing from recent run of rake paperclip:refresh:missing_styles
-  #   {
-  #     :User => {:avatar => [:big]},
-  #     :Book => {
-  #       :cover => [:croppable]},
-  #     }
-  #   }
-  def self.missing_attachments_styles
-    current_styles = current_attachments_styles
-    registered_styles = get_registered_attachments_styles
+      describe '::binary_osxfuse_installed?' do
+    it 'returns false if fuse.h does not exist' do
+      allow(File).to receive(:exist?).and_return(false)
+      expect(described_class).not_to be_binary_osxfuse_installed
+    end
+    
+      # Clean the keg of formula @f
+  def clean
+    ObserverPathnameExtension.reset_counts!
+    
+            raise t('error.invalid_stage_name', name: invalid, path: stage_config_path.join('#{invalid}.rb'))
+      end
+    end
+  end
+end
+
+    
+      tasks_dir = Pathname.new('lib/capistrano/tasks')
+  config_dir = Pathname.new('config')
+  deploy_dir = config_dir.join('deploy')
+    
+          context 'when the variables is undefined' do
+        it 'calls the block' do
+          expect(dsl.fetch(:source_control) { :svn }).to eq :svn
+        end
+      end
+    end
+  end
+    
+      def command_line(*options)
+    options.each { |opt| ARGV << opt }
+    subject.define_singleton_method(:exit) do |*_args|
+      throw(:system_exit, :exit)
+    end
+    subject.run
+    subject.options
+  end
