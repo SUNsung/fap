51e09b38705c6ec4a51faa674d542e5f3d8ea2b2
SUNsung <weatherius@gmail.com>
2018-03-31 18:19:32 +0300

    Rewrite [56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb] File in: 2018-03-31 18:19:32

--- ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
+++ ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
@@ -1,41 +1,6 @@
 
         
-          def []=(key, value)
-    @checksums[key] = value
+            ret = route.slice(:location, :label)
+    ret[:full_location] = 'adminPlugins.#{ret[:location]}'
+    ret
   end
-    
-      class Honda < Car
-    def initialize(*args)
-      self.make = 'Honda'
-      super(*args)
-    end
-  end
-    
-    process_args
-list_login_items_for_app $app_path
-
-    
-    def usage
-  <<-EOS
-list_running_app_ids [ -t <bundle-id> ]
-    
-        def possible_types
-      MIME::Types.type_for(@filepath).collect(&:content_type)
-    end
-    
-            def expected_attachment
-          'Expected #{@attachment_name}:\n'
-        end
-    
-      class Railtie < Rails::Railtie
-    initializer 'paperclip.insert_into_active_record' do |app|
-      ActiveSupport.on_load :active_record do
-        Paperclip::Railtie.insert
-      end
-    
-            private
-    
-              arguments.each_with_index do |argument, index|
-            optarg_positions << index if argument.optarg_type?
-            arg_positions << index if argument.arg_type?
-          end
