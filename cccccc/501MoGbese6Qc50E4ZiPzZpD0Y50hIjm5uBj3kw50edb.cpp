4dd09a2970be200558d147fb4e10fa311010619c
SUNsung <weatherius@gmail.com>
2018-04-01 05:13:53 +0300

    Rewrite [501MoGbese6Qc50E4ZiPzZpD0Y50hIjm5uBj3kw50edb.cpp] File in: 2018-04-01 05:13:53

--- cccccc/501MoGbese6Qc50E4ZiPzZpD0Y50hIjm5uBj3kw50edb.cpp
+++ cccccc/501MoGbese6Qc50E4ZiPzZpD0Y50hIjm5uBj3kw50edb.cpp
@@ -1,90 +1,181 @@
 
         
-          /// Build the components of an Objective-C method descriptor for the given
-  /// property's method implementations.
-  void emitObjCSetterDescriptorParts(IRGenModule &IGM,
-                                     VarDecl *property,
-                                     llvm::Constant *&selectorRef,
-                                     llvm::Constant *&atEncoding,
-                                     llvm::Constant *&impl);
-    
-          NameStyle style(completion->getName());
-      bool hideUnderscore = options.hideUnderscores && style.leadingUnderscores;
-      if (hideUnderscore && options.reallyHideAllUnderscores)
-        continue;
-    
-    
-    {  bool checkValue(SILValue Value);
-};
-    
-    typedef NS_ENUM(NSInteger, ObjectBehaviorAction) {
-    ObjectBehaviorActionRetain,
-    ObjectBehaviorActionCopy,
-    ObjectBehaviorActionMutableCopy
-};
-    
-    static std::unique_ptr<IndexRecordingConsumer>
-makeRecordingConsumer(std::string Filename, std::string indexStorePath,
-                      DiagnosticEngine *diags,
-                      std::string *outRecordFile,
-                      bool *outFailed) {
-  return llvm::make_unique<IndexRecordingConsumer>([=](SymbolTracker &record) {
-    *outFailed = writeRecord(record, Filename, indexStorePath, diags,
-                             *outRecordFile);
-  });
-}
-    
-    namespace swift {
+        namespace testing {
     }
     
-    #ifndef SWIFT_SYNTAX_REFERENCES_H
-#define SWIFT_SYNTAX_REFERENCES_H
+    # define TEST_P(test_case_name, test_name) \
+  class GTEST_TEST_CLASS_NAME_(test_case_name, test_name) \
+      : public test_case_name { \
+   public: \
+    GTEST_TEST_CLASS_NAME_(test_case_name, test_name)() {} \
+    virtual void TestBody(); \
+   private: \
+    static int AddToRegistry() { \
+      ::testing::UnitTest::GetInstance()->parameterized_test_registry(). \
+          GetTestCasePatternHolder<test_case_name>(\
+              #test_case_name, __FILE__, __LINE__)->AddTestPattern(\
+                  #test_case_name, \
+                  #test_name, \
+                  new ::testing::internal::TestMetaFactory< \
+                      GTEST_TEST_CLASS_NAME_(test_case_name, test_name)>()); \
+      return 0; \
+    } \
+    static int gtest_registering_dummy_; \
+    GTEST_DISALLOW_COPY_AND_ASSIGN_(\
+        GTEST_TEST_CLASS_NAME_(test_case_name, test_name)); \
+  }; \
+  int GTEST_TEST_CLASS_NAME_(test_case_name, \
+                             test_name)::gtest_registering_dummy_ = \
+      GTEST_TEST_CLASS_NAME_(test_case_name, test_name)::AddToRegistry(); \
+  void GTEST_TEST_CLASS_NAME_(test_case_name, test_name)::TestBody()
     
-    bool Substitution::isCanonical() const {
-  if (!getReplacement()->isCanonical())
-    return false;
-  for (auto conf : getConformances()) {
-    if (!conf.isCanonical())
-      return false;
+    // Prints the given number of elements in an array, without printing
+// the curly braces.
+template <typename T>
+void PrintRawArrayTo(const T a[], size_t count, ::std::ostream* os) {
+  UniversalPrint(a[0], os);
+  for (size_t i = 1; i != count; i++) {
+    *os << ', ';
+    UniversalPrint(a[i], os);
   }
-  return true;
 }
-
     
-    int FlatCompiler::Compile(int argc, const char **argv) {
-  if (params_.generators == nullptr || params_.num_generators == 0) {
-    return 0;
+      virtual ParamIteratorInterface<T>* Begin() const {
+    return new Iterator(this, begin_, 0, step_);
   }
+  virtual ParamIteratorInterface<T>* End() const {
+    return new Iterator(this, end_, end_index_, step_);
+  }
+    
+    template <GTEST_TEMPLATE_ T1, GTEST_TEMPLATE_ T2, GTEST_TEMPLATE_ T3,
+    GTEST_TEMPLATE_ T4, GTEST_TEMPLATE_ T5, GTEST_TEMPLATE_ T6,
+    GTEST_TEMPLATE_ T7>
+struct Templates7 {
+  typedef TemplateSel<T1> Head;
+  typedef Templates6<T2, T3, T4, T5, T6, T7> Tail;
+};
+    
+      // Trivial case 2: even numbers
+  if (n % 2 == 0) return n == 2;
+    
+      /// Move-construct a basic_socket_acceptor from an acceptor of another
+  /// protocol type.
+  /**
+   * This constructor moves an acceptor from one object to another.
+   *
+   * @param other The other basic_socket_acceptor object from which the move
+   * will occur.
+   *
+   * @note Following the move, the moved-from object is in the same state as if
+   * constructed using the @c basic_socket(io_service&) constructor.
+   */
+  template <typename Protocol1, typename SocketAcceptorService1>
+  basic_socket_acceptor(
+      basic_socket_acceptor<Protocol1, SocketAcceptorService1>&& other,
+      typename enable_if<is_convertible<Protocol1, Protocol>::value>::type* = 0)
+    : basic_io_object<SocketAcceptorService>(other.get_io_service())
+  {
+    this->get_service().template converting_move_construct<Protocol1>(
+        this->get_implementation(), other.get_implementation());
+  }
+    
+    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+    
+    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+    
+    namespace boost {
     }
     
-      if (type.base_type == BASE_TYPE_BOOL) {
-    text += val != 0 ? 'true' : 'false';
-  } else {
-    text += NumToString(val);
+    class ptime;
+    
+    #include <boost/asio/detail/config.hpp>
+    
+    #if defined(BOOST_ASIO_WINDOWS) || defined(__CYGWIN__)
+typedef win_fd_set_adapter fd_set_adapter;
+#else
+typedef posix_fd_set_adapter fd_set_adapter;
+#endif
+    
+    // Calls to asio_handler_allocate and asio_handler_deallocate must be made from
+// a namespace that does not contain any overloads of these functions. The
+// boost_asio_handler_alloc_helpers namespace is defined here for that purpose.
+namespace boost_asio_handler_alloc_helpers {
+    }
+    
+    #include <boost/asio/detail/pop_options.hpp>
+    
+    // This function will create a Huffman tree.
+//
+// The catch here is that the tree cannot be arbitrarily deep.
+// Brotli specifies a maximum depth of 15 bits for 'code trees'
+// and 7 bits for 'code length code trees.'
+//
+// count_limit is the value that is to be faked as the minimum value
+// and this minimum value is raised until the tree matches the
+// maximum length requirement.
+//
+// This algorithm is not of excellent performance for very long data blocks,
+// especially when population counts are longer than 2**tree_limit, but
+// we are not planning to use this with extremely long blocks.
+//
+// See http://en.wikipedia.org/wiki/Huffman_coding
+void CreateHuffmanTree(const uint32_t *data,
+                       const size_t length,
+                       const int tree_limit,
+                       HuffmanTree* tree,
+                       uint8_t *depth) {
+  // For block sizes below 64 kB, we never need to do a second iteration
+  // of this loop. Probably all of our block sizes will be smaller than
+  // that, so this loop is mostly of academic interest. If we actually
+  // would need this, we would be better off with the Katajainen algorithm.
+  for (uint32_t count_limit = 1; ; count_limit *= 2) {
+    size_t n = 0;
+    for (size_t i = length; i != 0;) {
+      --i;
+      if (data[i]) {
+        const uint32_t count = std::max<uint32_t>(data[i], count_limit);
+        tree[n++] = HuffmanTree(count, -1, static_cast<int16_t>(i));
+      }
+    }
+    }
+    }
+    
+    inline int Log2FloorNonZero(uint32_t n) {
+#ifdef __GNUC__
+  return 31 ^ __builtin_clz(n);
+#else
+  unsigned int result = 0;
+  while (n >>= 1) result++;
+  return result;
+#endif
+}
+    
+    // Computes the DCT (Discrete Cosine Transform) of the 8x8 array in 'block',
+// scaled up by a factor of 16. The values in 'block' are laid out row-by-row
+// and the result is written to the same memory area.
+void ComputeBlockDCT(coeff_t* block);
+    
+      int iquant[3 * kDCTBlockSize];
+  int idx = 0;
+  for (int i = 0; i < 3; ++i) {
+    for (int j = 0; j < kDCTBlockSize; ++j) {
+      int v = quant[idx];
+      jpg->quant[i].values[j] = v;
+      iquant[idx++] = ((1 << kIQuantBits) + 1) / v;
+    }
   }
     
-      uint32_t AsUInt32() const { return static_cast<uint32_t>(AsUInt64()); }
-  uint16_t AsUInt16() const { return static_cast<uint16_t>(AsUInt64()); }
-  uint8_t AsUInt8() const { return static_cast<uint8_t>(AsUInt64()); }
-    
-    
-    {}  // namespace grpc
-    
-    
-    { private:
-  std::unique_ptr<Greeter::Stub> stub_;
+    struct HuffmanCodeTable {
+  uint8_t depth[256];
+  int code[256];
 };
     
     
-    {  return 0;
-}
-
+    {}  // namespace guetzli
     
-    #endif  // FLATBUFFERS_CODE_GENERATORS_H_
-
-    
-    // Get a field's default, if you know it's floating point and its exact type.
-template<typename T> T GetFieldDefaultF(const reflection::Field &field) {
-  assert(sizeof(T) == GetTypeSize(field.type()->base_type()));
-  return static_cast<T>(field.default_real());
-}
+    constexpr int kLowestQuality = 70;
+constexpr int kHighestQuality = 110;
