1e6a0d395c029572667d99b6c4b3431e18d6486d
SUNsung <weatherius@gmail.com>
2018-04-01 12:42:32 +0300

    Rewrite [32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py] File in: 2018-04-01 12:42:31

--- pyton/32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py
+++ pyton/32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py
@@ -1,89 +1,35 @@
 
         
-          def replicate_states(self, state_coll_name):
-    state_list = self._metagraph.collection_def[state_coll_name]
-    num_states = len(state_list.node_list.value)
-    for replica_id in range(1, FLAGS.num_gpus):
-      for i in range(num_states):
-        state_list.node_list.value.append(state_list.node_list.value[i])
-    for replica_id in range(FLAGS.num_gpus):
-      for i in range(num_states):
-        index = replica_id * num_states + i
-        state_list.node_list.value[index] = with_autoparallel_prefix(
-            replica_id, state_list.node_list.value[index])
+            def add_options(self, parser):
+        '''
+        Populate option parse with options available for this command
+        '''
+        group = OptionGroup(parser, 'Global Options')
+        group.add_option('--logfile', metavar='FILE',
+            help='log file. if omitted stderr will be used')
+        group.add_option('-L', '--loglevel', metavar='LEVEL', default=None,
+            help='log level (default: %s)' % self.settings['LOG_LEVEL'])
+        group.add_option('--nolog', action='store_true',
+            help='disable logging completely')
+        group.add_option('--profile', metavar='FILE', default=None,
+            help='write python cProfile stats to FILE')
+        group.add_option('--pidfile', metavar='FILE',
+            help='write process ID to FILE')
+        group.add_option('-s', '--set', action='append', default=[], metavar='NAME=VALUE',
+            help='set/override setting (may be repeated)')
+        group.add_option('--pdb', action='store_true', help='enable pdb on failure')
     
-    # Fetch and store Training and Test dataset files
-PATH_DATASET = PATH + os.sep + 'dataset'
-FILE_TRAIN = PATH_DATASET + os.sep + 'iris_training.csv'
-FILE_TEST = PATH_DATASET + os.sep + 'iris_test.csv'
-URL_TRAIN = 'http://download.tensorflow.org/data/iris_training.csv'
-URL_TEST = 'http://download.tensorflow.org/data/iris_test.csv'
+            infos = []
+        if not self.wasSuccessful():
+            write('FAILED')
+            failed, errored = map(len, (self.failures, self.errors))
+            if failed:
+                infos.append('failures=%d' % failed)
+            if errored:
+                infos.append('errors=%d' % errored)
+        else:
+            write('OK')
     
-      return seq
-    
-      Args:
-    doc: Document to read tokens from.
-    vocab_freqs: dict<token, frequency count>
-    doc_counts: dict<token, document count>
-    
-        elif bidir:
-      # Classifier bidirectional LSTM
-      # Shared data source, but separate token/state streams
-      fname, = filenames
-      batch = _read_and_batch(
-          data_dir,
-          fname,
-          state_name,
-          state_size,
-          num_layers,
-          unroll_steps,
-          batch_size,
-          bidir_input=True)
-      forward_tokens, reverse_tokens = _split_bidir_tokens(batch)
-      forward_input = VatxtInput(
-          batch,
-          state_name=state_name,
-          tokens=forward_tokens,
-          num_states=num_layers)
-      reverse_input = VatxtInput(
-          batch,
-          state_name=state_name + '_reverse',
-          tokens=reverse_tokens,
-          num_states=num_layers)
-      return forward_input, reverse_input
-    else:
-      # Unidirectional LM or classifier
-      fname, = filenames
-      batch = _read_and_batch(
-          data_dir,
-          fname,
-          state_name,
-          state_size,
-          num_layers,
-          unroll_steps,
-          batch_size,
-          bidir_input=False)
-      return VatxtInput(
-          batch, state_name=state_name, num_states=num_layers, eos_id=eos_id)
-
-    
-    licenses(['notice'])  # Apache 2.0
-    
-    Run image classification with Inception trained on ImageNet 2012 Challenge data
-set.
-    
-            for i in html_json['sources']:
-            if 'src' in i:  #to avoid KeyError
-                if i['src'].startswith('https'):
-                    link_list.append((str(i['height']), i['src']))
-    
-    
-    
-            print_info(site_info, title, type_, size_full)
-        if not info_only:
-            download_urls(url_list, title, ext, total_size=size_full, output_dir=output_dir, merge=merge, headers=fake_headers)
-    else:
-        raise NotImplementedError(flashvars)
-    
-        #This is mainly for testing the M3U FFmpeg parser so I would ignore any non-m3u ones
-    stream_url = [i['url'] for i in html['streaming_url_list'] if i['is_default'] and i['type'] == 'hls'][0]
+        name = '%s_%s' % (spider, method.__name__)
+    setattr(ContractTestCase, name, lambda x: x)
+    return ContractTestCase(name)
