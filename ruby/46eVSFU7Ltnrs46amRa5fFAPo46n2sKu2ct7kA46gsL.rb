75b519d9218ed97dd4ca3059b3e929c79949ae75
SUNsung <weatherius@gmail.com>
2018-04-01 21:57:57 +0300

    Rewrite [46eVSFU7Ltnrs46amRa5fFAPo46n2sKu2ct7kA46gsL.rb] File in: 2018-04-01 21:57:57

--- ruby/46eVSFU7Ltnrs46amRa5fFAPo46n2sKu2ct7kA46gsL.rb
+++ ruby/46eVSFU7Ltnrs46amRa5fFAPo46n2sKu2ct7kA46gsL.rb
@@ -1,45 +1,87 @@
 
         
-            describe '#download' do
-      it 'returns the p8 file' do
-        mock_client_response(:download_key) do
-          %{
------BEGIN PRIVATE KEY-----
-this is the encoded private key contents
------END PRIVATE KEY-----
-          }
-        end
-        p8_string = key.download
-        expect(p8_string).to include('PRIVATE KEY')
+              # Returns constant of subscription adapter specified in config/cable.yml.
+      # If the adapter cannot be found, this will default to the Redis adapter.
+      # Also makes sure proper dependencies are required.
+      def pubsub_adapter
+        adapter = (cable.fetch('adapter') { 'redis' })
+    
+          test 'when a callback is modified in a child with :only, it works for the :only action' do
+        @controller.process(:index)
+        assert_equal 'Hello world', @controller.response_body
       end
+    
+        test 'head :continue (100) does not return a content-type header' do
+      headers = HeadController.action(:continue).call(Rack::MockRequest.env_for('/')).second
+      assert_nil headers['Content-Type']
+      assert_nil headers['Content-Length']
     end
     
-        class CreateKeychainAction < Action
-      def self.run(params)
-        escaped_password = params[:password].shellescape
+          result = ActionsController.action(:index).call(env_for('/'))
+      assert_nil result[1]['Middleware-Order']
+    end
     
-            expect(result).to eq('hg parent --template {rev}')
-        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('hg parent --template {rev}')
+          def handle_exception_with_mailer_class(exception)
+        if klass = mailer_class
+          klass.handle_exception exception
+        else
+          raise exception
+        end
+      end
+  end
+end
+
+    
+          text.split.each do |word|
+        if sentences.first.present? && (sentences.last + [word]).join(' ').length > len
+          sentences << [word]
+        else
+          sentences.last << word
+        end
+      end
+    
+            in_root do
+          if behavior == :invoke && !File.exist?(application_mailer_file_name)
+            template 'application_mailer.rb', application_mailer_file_name
+          end
+        end
+      end
+    
+    require 'active_support/testing/autorun'
+require 'active_support/testing/method_call_assertions'
+require 'action_mailer'
+require 'action_mailer/test_case'
+    
+        if as == :json
+      if api_error?(data)
+        data = generate_error_hash(data)
+      else
+        selected_fields = extract_fields(filter.to_s.strip)
+        data.select! { |k,v| selected_fields.include?(k) } unless selected_fields.empty?
+        unless options.include?(:exclude_default_metadata)
+          data = data.to_hash
+          if data.values.size == 0 && selected_fields.size > 0
+            raise LogStash::Api::NotFoundError
+          end
+          data = default_metadata.merge(data)
+        end
+      end
+    
+            end
       end
     end
   end
 end
 
     
-            # With reconfirmable, notify the original email when the user first
-        # requests the email change, instead of when the change is confirmed.
-        def send_email_changed_notification?
-          if self.class.reconfirmable
-            self.class.send_email_changed_notification && reconfirmation_required?
-          else
-            super
-          end
-        end
+      public
+  def flush(&block)
+    # does nothing by default.
+    # if your codec needs a flush method (like you are spooling things)
+    # you must implement this.
+  end
     
-        # Check if a reset_password_token is provided in the request
-    def assert_reset_token_passed
-      if params[:reset_password_token].blank?
-        set_flash_message(:alert, :no_token)
-        redirect_to new_session_path(resource_name)
-      end
-    end
+      def max_minus_two
+    [1, (maximum - 2)].max.floor
+  end
+end end end
