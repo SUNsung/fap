255af66104a9d8e1b87ec733ce5e018ae804de12
SUNsung <weatherius@gmail.com>
2018-04-02 02:46:03 +0300

    Rewrite [32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py] File in: 2018-04-02 02:46:02

--- pyton/32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py
+++ pyton/32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py
@@ -1,3 +1,111 @@
 
         
-            forbidden_extensions = ['html', 'htm'] if results.nohtml else []
+                pathlabel.grid(column=0, row=10, columnspan=3, padx=5, pady=[10,0],
+                       sticky=W)
+        self.path.grid(column=0, row=11, columnspan=2, padx=5, sticky=W+E,
+                       pady=[10,0])
+        browse.grid(column=2, row=11, padx=5, sticky=W+S)
+        self.path_error.grid(column=0, row=12, columnspan=3, padx=5,
+                             sticky=W+E)
+    
+    NaN = float('nan')
+PosInf = float('inf')
+NegInf = float('-inf')
+    
+    def evalString(s):
+    assert s.startswith(''') or s.startswith('''), repr(s[:1])
+    q = s[0]
+    if s[:3] == q*3:
+        q = q*3
+    assert s.endswith(q), repr(s[-len(q):])
+    assert len(s) >= 2*len(q)
+    s = s[len(q):-len(q)]
+    return re.sub(r'\\(\'|\'|\\|[abfnrtv]|x.{0,2}|[0-7]{1,3})', escape, s)
+    
+    def _unlock_file(f):
+    '''Unlock file f using lockf and dot locking.'''
+    if fcntl:
+        fcntl.lockf(f, fcntl.LOCK_UN)
+    if os.path.exists(f.name + '.lock'):
+        os.remove(f.name + '.lock')
+    
+            # RFC 4642 2.2.2: Both the client and the server MUST know if there is
+        # a TLS session active.  A client MUST NOT attempt to start a TLS
+        # session if a TLS session is already active.
+        self.tls_on = False
+    
+            for func, args, kwargs, expected in self.CALLS_KWARGS:
+            with self.subTest(func=func, args=args, kwargs=kwargs):
+                result = _testcapi.pyobject_fastcalldict(func, args, kwargs)
+                self.check_result(result, expected)
+    
+        expect_without_sort_keys = textwrap.dedent('''\
+    [
+        [
+            'blorpie'
+        ],
+        [
+            'whoops'
+        ],
+        [],
+        'd-shtaeou',
+        'd-nthiouh',
+        'i-vhbjkhnth',
+        {
+            'nifty': 87
+        },
+        {
+            'field': 'yes',
+            'morefield': false
+        }
+    ]
+    ''')
+    
+            a = sys.maxsize**10
+        b = 0
+        c = -2*sys.maxsize
+        expected_len = 1 + (b - a) // c
+        x = range(a, b, c)
+        self.assertIn(a, x)
+        self.assertNotIn(b, x)
+        self.assertRaises(OverflowError, len, x)
+        self.assertTrue(x)
+        self.assertEqual(_range_len(x), expected_len)
+        self.assertEqual(x[0], a)
+        idx = sys.maxsize+1
+        self.assertEqual(x[idx], a+(idx*c))
+        self.assertEqual(x[idx:idx+1][0], a+(idx*c))
+        with self.assertRaises(IndexError):
+            x[-expected_len-1]
+        with self.assertRaises(IndexError):
+            x[expected_len]
+    
+            dll = CDLL(_ctypes_test.__file__)
+        # This function expects a function pointer,
+        # and calls this with an integer pointer as parameter.
+        # The int pointer points to a table containing the numbers 1..10
+        doit = dll._testfunc_callback_with_pointer
+    
+        def _send_diagnostics_failure_report(self):
+        return 'send diagnostics failure report'
+    
+        def test_data_change_shall_notify_all_observers_once(cls):
+        with patch.object(cls.dec_obs, 'update') as mock_dec_obs_update,\
+                patch.object(cls.hex_obs, 'update') as mock_hex_obs_update:
+            cls.sub.data = 10
+            cls.assertEqual(mock_dec_obs_update.call_count, 1)
+            cls.assertEqual(mock_hex_obs_update.call_count, 1)
+    
+        def setUp(self):
+        self.sample_queue = queue.Queue()
+        self.sample_queue.put('first')
+        self.sample_queue.put('second')
+    
+        def _instance_method_2(self):
+        print('Value {}'.format(self.x2))
+    
+    
+class ClientCoroutine:
+    
+    
+class MoveFileCommand(object):
