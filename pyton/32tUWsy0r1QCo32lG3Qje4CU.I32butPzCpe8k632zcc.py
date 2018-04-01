aa47a0065e067b564748b511d0aa474782ede179
SUNsung <weatherius@gmail.com>
2018-04-01 19:10:03 +0300

    Rewrite [32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py] File in: 2018-04-01 19:10:03

--- pyton/32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py
+++ pyton/32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py
@@ -1,35 +1,58 @@
 
         
-            def add_options(self, parser):
-        '''
-        Populate option parse with options available for this command
-        '''
-        group = OptionGroup(parser, 'Global Options')
-        group.add_option('--logfile', metavar='FILE',
-            help='log file. if omitted stderr will be used')
-        group.add_option('-L', '--loglevel', metavar='LEVEL', default=None,
-            help='log level (default: %s)' % self.settings['LOG_LEVEL'])
-        group.add_option('--nolog', action='store_true',
-            help='disable logging completely')
-        group.add_option('--profile', metavar='FILE', default=None,
-            help='write python cProfile stats to FILE')
-        group.add_option('--pidfile', metavar='FILE',
-            help='write process ID to FILE')
-        group.add_option('-s', '--set', action='append', default=[], metavar='NAME=VALUE',
-            help='set/override setting (may be repeated)')
-        group.add_option('--pdb', action='store_true', help='enable pdb on failure')
+        
+def format_size(bytes):
+    return '%s (%d bytes)' % (format_bytes(bytes), bytes)
     
-            infos = []
-        if not self.wasSuccessful():
-            write('FAILED')
-            failed, errored = map(len, (self.failures, self.errors))
-            if failed:
-                infos.append('failures=%d' % failed)
-            if errored:
-                infos.append('errors=%d' % errored)
+    from .nuevo import NuevoBaseIE
+    
+            mvp_id = self._search_mvp_id(webpage)
+    
+    
+for package_info in packages.values():
+    print('''
+  resource '{name}' do
+    url '{url}'
+    sha256 '{sha256}'
+  end'''.format(**package_info))
+
+    
+    
+with open(BIN_FILE_PATH, 'rb') as f:
+    BIN_FILE_CONTENT = f.read()
+    
+    
+def test_max_redirects(httpbin):
+    r = http('--max-redirects=1', '--follow', httpbin.url + '/redirect/3',
+             error_exit_ok=True)
+    assert r.exit_status == ExitStatus.ERROR_TOO_MANY_REDIRECTS
+
+    
+    
+def test_unicode_url_query_arg_item_verbose(httpbin):
+    r = http('--verbose', httpbin.url + '/get', u'test==%s' % UNICODE)
+    assert HTTP_OK in r
+    assert UNICODE in r
+    
+        Uses threading to periodically update the status (speed, ETA, etc.).
+    
+            if color_scheme != PRESET_STYLE and env.colors == 256:
+            fmt_class = Terminal256Formatter
         else:
-            write('OK')
+            fmt_class = TerminalFormatter
+        self.formatter = fmt_class(style=style_class)
     
-        name = '%s_%s' % (spider, method.__name__)
-    setattr(ContractTestCase, name, lambda x: x)
-    return ContractTestCase(name)
+        def test_verify_custom_ca_bundle_invalid_path(self, httpbin_secure):
+        # since 2.14.0 requests raises IOError
+        with pytest.raises((SSLError, IOError)):
+            http(httpbin_secure.url + '/get', '--verify', '/__not_found__')
+    
+    '''
+__version__ = '1.0.0-dev'
+__author__ = 'Jakub Roztocil'
+__licence__ = 'BSD'
+    
+        def _apply_no_options(self, no_options):
+        '''For every `--no-OPTION` in `no_options`, set `args.OPTION` to
+        its default value. This allows for un-setting of options, e.g.,
+        specified in config.
