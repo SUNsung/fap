c5732bd27c32e5637e5d3ea1c3d32c9c2a9d937c
SUNsung <weatherius@gmail.com>
2018-04-01 20:25:01 +0300

    Rewrite [44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp] File in: 2018-04-01 20:25:00

--- cccccc/44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp
+++ cccccc/44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp
@@ -1,221 +1,81 @@
 
         
-        DetLineFit::DetLineFit() : square_length_(0.0) {
-}
-    
-      // Constrained fit with a supplied direction vector. Finds the best line_pt,
-  // that is one of the supplied points having the median cross product with
-  // direction, ignoring points that have a cross product outside of the range
-  // [min_dist, max_dist]. Returns the resulting error metric using the same
-  // reduced set of points.
-  // *Makes use of floating point arithmetic*
-  double ConstrainedFit(const FCOORD& direction,
-                        double min_dist, double max_dist,
-                        bool debug, ICOORD* line_pt);
-    
-    void QLSQ::remove(double x, double y) {
-  if (n <= 0) {
-    tprintf('Can't remove an element from an empty QLSQ accumulator!\n');
-    return;
-  }
-  n--;                           // Count elements.
-  sigx -= x;                     // Update accumulators.
-  sigy -= y;
-  sigxx -= x * x;
-  sigxy -= x * y;
-  sigyy -= y * y;
-  sigxxx -= static_cast<long double>(x) * x * x;
-  sigxxy -= static_cast<long double>(x) * x * y;
-  sigxxxx -= static_cast<long double>(x) * x * x * x;
-}
-    
-    // A smart pointer class that implements a double-ended pointer. Each end
-// points to the other end. The copy constructor and operator= have MOVE
-// semantics, meaning that the relationship with the other end moves to the
-// destination of the copy, leaving the source unattached.
-// For this reason both the copy constructor and the operator= take a non-const
-// reference argument, and the const reference versions cannot be used.
-// DoublePtr is useful to incorporate into structures that are part of a
-// collection such as GenericVector or STL containers, where reallocs can
-// relocate the members. DoublePtr is also useful in a GenericHeap, where it
-// can correctly maintain the pointer to an element of the heap despite it
-// getting moved around on the heap.
-class DoublePtr {
- public:
-  DoublePtr() : other_end_(NULL) {}
-  // Copy constructor steals the partner off src and is therefore a non
-  // const reference arg.
-  // Copying a const DoublePtr generates a compiler error.
-  DoublePtr(DoublePtr& src) {
-    other_end_ = src.other_end_;
-    if (other_end_ != NULL) {
-      other_end_->other_end_ = this;
-      src.other_end_ = NULL;
+            // Operator signature declaration.
+    // It defines input formal parameter, output formal parameters and
+    // attributes.
+    // Once an operator signature created, it's 'Read-Only'.
+    class OpSignature
+    {
+    public:
     }
-  }
-  // Operator= steals the partner off src, and therefore needs src to be a non-
-  // const reference.
-  // Assigning from a const DoublePtr generates a compiler error.
-  void operator=(DoublePtr& src) {
-    Disconnect();
-    other_end_ = src.other_end_;
-    if (other_end_ != NULL) {
-      other_end_->other_end_ = this;
-      src.other_end_ = NULL;
-    }
-  }
-    }
-    
-    // Constructor is private. Only anticipated use of ErrorCounter is via
-// the static ComputeErrorRate.
-ErrorCounter::ErrorCounter(const UNICHARSET& unicharset, int fontsize)
-  : scaled_error_(0.0), rating_epsilon_(kRatingEpsilon),
-    unichar_counts_(unicharset.size(), unicharset.size(), 0),
-    ok_score_hist_(0, 101), bad_score_hist_(0, 101),
-    unicharset_(unicharset) {
-  Counts empty_counts;
-  font_counts_.init_to_size(fontsize, empty_counts);
-  multi_unichar_counts_.init_to_size(unicharset.size(), 0);
-}
-ErrorCounter::~ErrorCounter() {
-}
-    
-    
-    {  // Size of the indexed feature space.
-  int size_;
-  // Total weight of features currently stored in the maps.
-  double total_feature_weight_;
-  // Pointer to IntFeatureMap given at Init to find offset features.
-  const IntFeatureMap* feature_map_;
-  // Array of bools indicating presence of a feature.
-  bool* features_;
-  // Array indicating the presence of a feature offset by one unit.
-  bool* features_delta_one_;
-  // Array indicating the presence of a feature offset by two units.
-  bool* features_delta_two_;
-};
-    
-    
-    {        Graph* m_graph;
-    };
     
         // Taken from RS4
-    REGISTER_OPERATOR_SCHEMA(Softsign)
-        .Description('Softsign takes one input data (Tensor<T>) and produces one output '
-            'data (Tensor<T>) where the function, y = x / (1 + abs(x)), is applied to the '
-            'tensor elementwise.')
-        .Input('input', 'Input tensor, typically 1-D.', 'T')
+    REGISTER_OPERATOR_SCHEMA(Hardmax)
+        .Description('Compute the hardmax normalized values for each layer in the batch '
+            'of the given input. The input is a 2-D tensor (Tensor<float>) of size '
+            '(batch_size x input_feature_dimensions). The output tensor has the same shape '
+            'and contains the softmax normalized values of the corresponding input. '
+            '\n'
+            'X does not need to explicitly be a 2D vector; rather, it will be coerced into '
+            'one. For an arbitrary n-dimensional tensor X in [a_0, a_1, ..., a_{k-1}, '
+            'a_k, ..., a_{n-1}] and k is the axis provided, then X will be coerced into a '
+            '2-dimensional tensor with dimensions [a_0 * ... * a_{k-1}, a_k * ... * a_{n-1}]. '
+            'For the default case where axis=1, this means the X tensor will be coerced into '
+            'a 2D tensor of dimensions [a_0, a_1 * ... * a_{n-1}], where a_0 is often the '
+            'batch size.  In this situation, we must have a_0 = N and a_1 * ... * a_{n-1} = D. '
+            'Each of these dimensions must be matched correctly, or else the operator will '
+            'throw errors.')
+        .Input('input', 'The input tensor that's coerced into a 2D matrix of size (NxD) as '
+            'described above.', 'T')
         .Output('output', 'Output tensor of same shape and type as input X.', 'T')
         .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
-            'Constrain input and output types to float tensors.');
+            'Constrain input and output types to float tensors.')
+        .Attr('axis', 'Default to 1; describes the axis of the inputs when coerced to 2D; '
+            'defaults to one because the 0th axis most likely describes the batch size.',
+            AttrType::AttributeProto_AttributeType_INT, int64_t(1));
     
-    #define REGISTER_BINARY_COMPARISON_OPERATOR_SCHEMA(OpName)                                                      \
-    REGISTER_OPERATOR_SCHEMA(OpName)                                                                            \
-        .Description('Returns the tensor resulted from performing the ''#OpName'' logical operation'            \
-        'elementwise on the input tensors A and B. If broadcasting is enabled, the right-hand-side'             \
-        'argument will be broadcasted to match the shape of left-hand-side argument. Refer to Add for'          \
-        'a detailed description of the broadcasting rules.')                                                    \
-        .Input('A', 'First operand, should share the type with the second operand.', 'T1')                      \
-        .Input('B', 'Second operand. With broadcasting can be of smaller size than A.'                          \
-            'If broadcasting is disabled, it should be of the same size.', 'T1')                                \
-        .Output('C', 'Result, has same dimensions as A and type bool.', 'T2')                                   \
-        .TypeConstraint('T1', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },                         \
-                'Constrain input to float tensors.')                                                            \
-        .TypeConstraint('T2', { 'tensor(bool)' }, 'Constrain output types to bool tensor.')                     \
-        .Attr('axis', 'If set, defines the broadcast dimensions.',                                              \
-            AttrType::AttributeProto_AttributeType_INT)                                                         \
-        .Attr('broadcast', 'Pass 1 to enable broadcasting.',                                                    \
-            AttrType::AttributeProto_AttributeType_INT);
-    
-        // Taken from ONNX
-    REGISTER_OPERATOR_SCHEMA(Ceil)
-        .Description('Ceil takes one input data (Tensor<T>) and produces one output data'
-            '(Tensor<T>) where the ceil is, y = ceil(x), is applied to'
-            'the tensor elementwise.')
-        .Input('input', 'Input tensor of any shape', 'T')
-        .Output('output', 'Output tensor of same shape and type as input X.', 'T')
+        REGISTER_OPERATOR_SCHEMA(Dropout)
+        .Description('Dropout takes one input data (Tensor<float>) and produces two Tensor outputs, '
+            'output (Tensor<float>) and mask (Tensor<bool>). Depending on whether it is in '
+            'test mode or not, the output Y will either be a random dropout, or a simple '
+            'copy of the input. Note that our implementation of Dropout does scaling in '
+            'the training phase, so during testing nothing needs to be done.')
+        .Input('data', 'The input data as Tensor.', 'T')
+        .Output('output', 'The output.', 'T')
+        .Output('mask',
+            'The output mask. If is_test is nonzero, this output is not filled.', 'T')
         .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
-            'Constrain input and output types to float tensors.');
+            'Constrain input and output types to float tensors.')
+        .Attr('ratio',
+            '(float, default 0.5) the ratio of random dropout',
+            AttrType::AttributeProto_AttributeType_FLOAT, float(0.5))
+        .Attr('is_test',
+            '(int, default 0) if nonzero, run dropout in test mode where '
+            'the output is simply Y = X.',
+            AttrType::AttributeProto_AttributeType_INT, int64_t(0));
     
-        std::function<void(OperatorSchemaSetter&)> RNNDocGeneratorInputX() {
-        return [=](OperatorSchemaSetter& schema) {
-            schema.Input('X',
-                'The input sequences packed (and potentially padded) into one 3-D '
-                'tensor with the shape of `[seq_length, batch_size, input_size]`.', 'T');
-            schema.TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
-                'Constrain input and output types to float tensors.');
-        };
-    }
-    
-        // Get start frame index inside chunk.
-    size_t GetStartFrameIndexInsideChunk(size_t index) const
+        virtual void BackpropToNonLooping(size_t inputIndex) override
     {
-        return m_firstFrames[index];
+        FrameRange fr(Input(0)->GetMBLayout());
+        if (inputIndex == 0)
+            LogicError('DummyCriterionNode: Gradients with respect to objective features are not necessary, not implemented.\n');
+        else if (inputIndex == 1)
+            LogicError('DummyCriterionNode: Gradients with respect to derivative features are not necessary, not implemented.\n');
+        else if (inputIndex == 2)
+        {
+            // predictionsGradient += userSuppliedGradient * scalarGradientFromTop
+            auto gradient = Input(2)->GradientFor(fr);
+            Matrix<ElemType>::Multiply1x1AndWeightedAdd(+1.0f, /*gradient from top:*/Gradient() /*1x1*/, /*user-supplied gradient:*/Input(1)->ValueFor(fr), 1.0f, /*add to:*/gradient);
+        }
     }
     
-    // ===========================================================================
-// ssematrixfrombuffer -- an ssematrixbase allocated in a vector buffer
-// If you need many little matrices in your own heap
-// ===========================================================================
-    
-      /// Put the runnable into an interruptible sleep.
-  virtual void pauseMilli(std::chrono::milliseconds milli);
-    
-      FlagAlias(const std::string& /*alias*/,
-            const std::string& /*type*/,
-            std::string name,
-            T* /*storage*/)
-      : name_(std::move(name)) {}
-    
-      /// The name of the file emitting the status log.
-  std::string filename;
-    
-    #include <gtest/gtest.h>
-    
-      std::stringstream result;
-  CFDataGetBytes(cf_data, range, (UInt8*)buffer);
-  for (CFIndex i = 0; i < range.length; ++i) {
-    uint8_t byte = buffer[i];
-    if (isprint(byte)) {
-      result << byte;
-    } else if (buffer[i] == 0) {
-      result << ' ';
-    } else {
-      result << '%' << std::setfill('0') << std::setw(2) << std::hex
-             << (int)byte;
-    }
-  }
-    
-        std::string arg_interval('--interval');
-    arguments.push_back(arg_interval.c_str());
-    arguments.push_back(extensions_interval.c_str());
-    arguments.push_back(nullptr);
-    
-    #include <string>
-    
-    #include 'osquery/core/utils.h'
-    
-    XGBOOST_REGISTER_SPARSE_PAGE_FORMAT(lz4i16hc)
-.describe('Apply LZ4 binary data compression(16 bit index mode) for ext memory.')
-.set_body([]() {
-    return new SparsePageLZ4Format<uint16_t>(true);
-  });
-    
-    namespace {
+        // Returns all frames of a given utterance.
+    msra::dbn::matrixstripe GetUtteranceFrames(size_t index) const
+    {
+        if (!IsInRam())
+        {
+            LogicError('GetUtteranceFrames was called when data have not yet been paged in.');
+        }
     }
     
-    
-    {}  // namespace guetzli
-    
-    namespace guetzli {
-    }
-    
-      int iquant[3 * kDCTBlockSize];
-  int idx = 0;
-  for (int i = 0; i < 3; ++i) {
-    for (int j = 0; j < kDCTBlockSize; ++j) {
-      int v = quant[idx];
-      jpg->quant[i].values[j] = v;
-      iquant[idx++] = ((1 << kIQuantBits) + 1) / v;
-    }
-  }
+    using namespace Microsoft::MSR::CNTK;
