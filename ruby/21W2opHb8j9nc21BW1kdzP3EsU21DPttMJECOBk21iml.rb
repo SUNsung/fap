f033ae3b4fb39b7e1a0bce64933d0f36a906d0a1
SUNsung <weatherius@gmail.com>
2018-04-02 11:39:09 +0300

    Rewrite [21W2opHb8j9nc21BW1kdzP3EsU21DPttMJECOBk21iml.rb] File in: 2018-04-02 11:39:09

--- ruby/21W2opHb8j9nc21BW1kdzP3EsU21DPttMJECOBk21iml.rb
+++ ruby/21W2opHb8j9nc21BW1kdzP3EsU21DPttMJECOBk21iml.rb
@@ -1,100 +1,76 @@
 
         
-        CONTENT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+          end
     
-    module Jekyll
-  binding.pry
-end
-
-    
-      Jekyll::External.require_if_present(Jekyll::External.blessed_gems) do |g, ver_constraint|
-    cmd = g.split('-').last
-    p.command(cmd.to_sym) do |c|
-      c.syntax cmd
-      c.action do
-        Jekyll.logger.abort_with 'You must install the '#{g}' gem' \
-          ' version #{ver_constraint} to use the 'jekyll #{cmd}' command.'
-      end
-    end
+      # any user that is either a moderator or an admin
+  def staff?
+    admin || moderator
   end
     
-    module Jekyll
-  module External
-    class << self
-      #
-      # Gems that, if installed, should be loaded.
-      # Usually contain subcommands.
-      #
-      def blessed_gems
-        %w(
-          jekyll-docs
-          jekyll-import
-        )
-      end
+            css('code').each do |node|
+          node.inner_html = node.inner_html.squish
+        end
     
-          self.data = {}
+            # This returns all the registered host capabilities.
+        #
+        # @return [Hash]
+        def host_capabilities
+          results = Hash.new { |h, k| h[k] = Registry.new }
     
-    def excluded_bundle_id(bundle_id)
-  %r{^com\.apple\.}.match(bundle_id)
-end
+            # Get the proper capability host to check
+        cap_host = nil
+        if type == :host
+          cap_host = @env.host
+        else
+          with_target_vms([]) do |vm|
+            cap_host = case type
+                       when :provider
+                         vm.provider
+                       when :guest
+                         vm.guest
+                       else
+                         raise Vagrant::Errors::CLIInvalidUsage,
+                           help: opts.help.chomp
+                       end
+          end
+        end
+    
+      def as_json(options={})
+    {
+      poll_id:             id,
+      post_id:             status_message.id,
+      question:            question,
+      poll_answers:        poll_answers,
+      participation_count: participation_count
+    }
+  end
     
           private
     
-    module Capistrano
-  module TaskEnhancements
-    def before(task, prerequisite, *args, &block)
-      prerequisite = Rake::Task.define_task(prerequisite, *args, &block) if block_given?
-      Rake::Task[task].enhance [prerequisite]
-    end
+            private
     
-        context 'without a default' do
-      context 'when the variables is defined' do
-        it 'returns the variable' do
-          expect(dsl.fetch(:scm)).to eq :git
+    module RuboCop
+  module Cop
+    class VariableForce
+      # A Variable represents existence of a local variable.
+      # This holds a variable declaration node,
+      # and some states of the variable.
+      class Variable
+        VARIABLE_DECLARATION_TYPES =
+          (VARIABLE_ASSIGNMENT_TYPES + ARGUMENT_DECLARATION_TYPES).freeze
+    
+            MSG = 'Use only a single space inside array percent literal.'.freeze
+        MULTIPLE_SPACES_BETWEEN_ITEMS_REGEX =
+          /(?:[\S&&[^\\]](?:\\ )*)( {2,})(?=\S)/
+    
+              add_offense(node)
         end
-      end
     
-        return captured_stdout.string, captured_stderr.string
-  ensure
-    $stdout = orig_stdout
-    $stderr = orig_stderr
+            private
+    
+          def extract_metrics(path, *keys)
+        snapshot.metric_store.extract_metrics(path, *keys)
+      end
+    end
   end
 end
-
-    
-        def cropping dst, ratio, scale
-      if ratio.horizontal? || ratio.square?
-        '%dx%d+%d+%d' % [ dst.width, dst.height, 0, (self.height * scale - dst.height) / 2 ]
-      else
-        '%dx%d+%d+%d' % [ dst.width, dst.height, (self.width * scale - dst.width) / 2, 0 ]
-      end
-    end
-    
-        # Returns the id of the instance.
-    def id attachment, style_name
-      attachment.instance.id
-    end
-    
-            def responds?
-          methods = @subject.instance_methods.map(&:to_s)
-          methods.include?('#{@attachment_name}') &&
-            methods.include?('#{@attachment_name}=') &&
-            methods.include?('#{@attachment_name}?')
-        end
-    
-          def has_attached_file(*attachment_names)
-        ActiveSupport::Deprecation.warn 'Method `t.has_attached_file` in the migration has been deprecated and will be replaced by `t.attachment`.'
-        attachment(*attachment_names)
-      end
-    end
