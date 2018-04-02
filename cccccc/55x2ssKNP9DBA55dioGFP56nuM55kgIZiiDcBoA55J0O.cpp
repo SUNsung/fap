7e10d7460c81b9c7c48c767e1cd1f4ed8613c15b
SUNsung <weatherius@gmail.com>
2018-04-02 20:38:17 +0300

    Rewrite [55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp] File in: 2018-04-02 20:38:16

--- cccccc/55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp
+++ cccccc/55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp
@@ -1,160 +1,104 @@
 
         
+        // Get basic type definitions.
+#define IPC_MESSAGE_IMPL
+#include 'content/nw/src/common/common_message_generator.h'
+    
+        return;
+  } else if (method == 'ClearCache') {
+    ClearCache(GetRenderProcessHost());
+    return;
+  } else if (method == 'CreateShortcut') {
+#if defined(OS_WIN)
+    base::string16 path;
+    arguments.GetString(0, &path);
+    
+    class App {
+ public:
+  static void Call(const std::string& method,
+                   const base::ListValue& arguments);
+    }
+    
+    #include 'base/memory/weak_ptr.h'
+    
+    // static
+void
+DispatcherBindings::DeallocateObject(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  RenderView* render_view = GetCurrentRenderView();
+  if (!render_view) {
+    args.GetReturnValue().Set(isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate,
+                                     'Unable to get render view in DeallocateObject'))));
+    return;
+  }
+    }
+    
+    EventListener::EventListener(int id,
+  const base::WeakPtr<DispatcherHost>& dispatcher_host,
+  const base::DictionaryValue& option) : Base(id, dispatcher_host, option) {
+    }
+    
+    namespace nwapi {
+    }
+    
+      SelfType operator--(int) {
+    SelfType temp(*this);
+    --*this;
+    return temp;
+  }
+    
+        // Adds the module variable 'api_version'.
+    if (PyModule_AddIntConstant(
+        module,
+        const_cast<char*>(kImplVersionName),
+        kImplVersion))
+#if PY_MAJOR_VERSION < 3
+      return;
+#else
+      { Py_DECREF(module); return NULL; }
+    
+    
+    
+    #include <google/protobuf/compiler/csharp/csharp_source_generator_base.h>
+#include <google/protobuf/compiler/csharp/csharp_helpers.h>
+#include <google/protobuf/compiler/csharp/csharp_names.h>
+#include <google/protobuf/compiler/csharp/csharp_options.h>
+    
+    ImmutableGeneratorFactory::ImmutableGeneratorFactory(
+    Context* context) : context_(context) {
+}
+ImmutableGeneratorFactory::~ImmutableGeneratorFactory() {}
+    
+    #ifdef ANDROID
+#include <jni.h>
+    
+    #endif /* defined(COMM_COMM_FREQUENCY_LIMIT_H_) */
+
+    
+    
+    {        return NULL;
+    }
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+    class TSpy :  public Spy {
+  public:
+    TSpy(void* _this): Spy(_this) {}
+    }
+    
+    ScopeJEnv::ScopeJEnv(JavaVM* jvm, jint _capacity)
+    : vm_(jvm), env_(NULL), we_attach_(false), status_(0) {
+    ASSERT(jvm);
+    do {
+        env_ = (JNIEnv*)pthread_getspecific(g_env_key);
         
-    {  for (size_t i = 0; i < new_files_.size(); i++) {
-    const FileMetaData& f = new_files_[i].second;
-    PutVarint32(dst, kNewFile);
-    PutVarint32(dst, new_files_[i].first);  // level
-    PutVarint64(dst, f.number);
-    PutVarint64(dst, f.file_size);
-    PutLengthPrefixedSlice(dst, f.smallest.Encode());
-    PutLengthPrefixedSlice(dst, f.largest.Encode());
-  }
-}
-    
-    #include 'db/dbformat.h'
-#include 'leveldb/write_batch.h'
-    
-      void Run() {
-    PrintHeader();
-    Open(false);
-    }
-    
-      iter->Seek('5');
-  ASSERT_EQ(iter->key().ToString(), '5');
-  iter->Prev();
-  ASSERT_EQ(iter->key().ToString(), '4');
-  iter->Prev();
-  ASSERT_EQ(iter->key().ToString(), '3');
-  iter->Next();
-  ASSERT_EQ(iter->key().ToString(), '4');
-  iter->Next();
-  ASSERT_EQ(iter->key().ToString(), '5');
-    
-      // Return the offset in data_ just past the end of the current entry.
-  inline uint32_t NextEntryOffset() const {
-    return (value_.data() + value_.size()) - data_;
-  }
-    
-      // Takes ownership of 'iter' and will delete it when destroyed, or
-  // when Set() is invoked again.
-  void Set(Iterator* iter) {
-    delete iter_;
-    iter_ = iter;
-    if (iter_ == NULL) {
-      valid_ = false;
-    } else {
-      Update();
-    }
-  }
-    
-    SequentialFile::~SequentialFile() {
-}
-    
-    std::ostream& operator<<(std::ostream& os, const AsyncIOOp& op) {
-  os << '{' << op.state_ << ', ';
+        if (NULL != env_) {
+            break;
+        }
+        
+        status_ = vm_->GetEnv((void**) &env_, JNI_VERSION_1_6);
     }
     }
-    
-      /**
-   * Create an AsyncIO context capable of holding at most 'capacity' pending
-   * requests at the same time.  As requests complete, others can be scheduled,
-   * as long as this limit is not exceeded.
-   *
-   * Note: the maximum number of allowed concurrent requests is controlled
-   * by the fs.aio-max-nr sysctl, the default value is usually 64K.
-   *
-   * If pollMode is POLLABLE, pollFd() will return a file descriptor that
-   * can be passed to poll / epoll / select and will become readable when
-   * any IOs on this AsyncIO have completed.  If you do this, you must use
-   * pollCompleted() instead of wait() -- do not read from the pollFd()
-   * file descriptor directly.
-   *
-   * You may use the same AsyncIO object from multiple threads, as long as
-   * there is only one concurrent caller of wait() / pollCompleted() / cancel()
-   * (perhaps by always calling it from the same thread, or by providing
-   * appropriate mutual exclusion).  In this case, pending() returns a snapshot
-   * of the current number of pending requests.
-   */
-  explicit AsyncIO(size_t capacity, PollMode pollMode = NOT_POLLABLE);
-  ~AsyncIO();
-    
-    inline bool operator==(const HugePageSize& a, const HugePageSize& b) {
-  return a.size == b.size;
-}
-    
-    
-    {std::string AsyncFileWriter::getNumDiscardedMsg(size_t numDiscarded) {
-  // We may want to make this customizable in the future (e.g., to allow it to
-  // conform to the LogFormatter style being used).
-  // For now just return a simple fixed message.
-  return folly::to<std::string>(
-      numDiscarded,
-      ' log messages discarded: logging faster than we can write\n');
-}
-} // namespace folly
-
-    
-      /**
-   * Get the output file.
-   */
-  const folly::File& getFile() const {
-    return file_;
-  }
-    
-    using std::make_shared;
-using std::string;
-    
-    #include <folly/Optional.h>
-#include <folly/Range.h>
-#include <memory>
-    
-    
-    {
-    {      if (end == msgData.size()) {
-        break;
-      }
-      idx = end + 1;
-    }
-  } else {
-    buffer.reserve(headerLengthGuess + msgData.size());
-    headerFormatter.appendTo(buffer);
-    buffer.append(msgData.data(), msgData.size());
-    buffer.push_back('\n');
-  }
-    
-    namespace folly {
-    }
-    
-    
-    {} // namespace folly
-
-    
-      /**
-   * Update this LogConfig object by merging in settings from another
-   * LogConfig.
-   *
-   * All LogHandler settings from the other LogConfig will be inserted into
-   * this LogConfig.  If a log handler with the same name was already defined
-   * in this LogConfig it will be replaced with the new settings.
-   *
-   * All LogCategory settings from the other LogConfig will be inserted into
-   * this LogConfig.  If a log category with the same name was already defined
-   * in this LogConfig, its settings will be updated with settings from the
-   * other LogConfig.  However, if the other LogConfig does not define handler
-   * settings for the category it will retain its current handler settings.
-   *
-   * This method allows LogConfig objects to be combined before applying them.
-   * Using LogConfig::update() will produce the same results as if
-   * LoggerDB::updateConfig() had been called with both configs sequentially.
-   * In other words, this operation:
-   *
-   *   configA.update(configB);
-   *   loggerDB.updateConfig(configA);
-   *
-   * will produce the same results as:
-   *
-   *   loggerDB.updateConfig(configA);
-   *   loggerDB.updateConfig(configA);
-   */
-  void update(const LogConfig& other);
