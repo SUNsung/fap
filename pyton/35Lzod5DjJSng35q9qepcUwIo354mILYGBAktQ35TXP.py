7c1536a8b2e55288976b3548e09a82e2b65f68c1
SUNsung <weatherius@gmail.com>
2018-04-02 14:24:46 +0300

    Rewrite [35Lzod5DjJSng35q9qepcUwIo354mILYGBAktQ35TXP.py] File in: 2018-04-02 14:24:46

--- pyton/35Lzod5DjJSng35q9qepcUwIo354mILYGBAktQ35TXP.py
+++ pyton/35Lzod5DjJSng35q9qepcUwIo354mILYGBAktQ35TXP.py
@@ -1,202 +1,62 @@
 
         
-        containers = (('thefuck/python3-tcsh',
-               u'''FROM python:3
-                   RUN apt-get update
-                   RUN apt-get install -yy tcsh''',
-               u'tcsh'),
-              ('thefuck/python2-tcsh',
-               u'''FROM python:2
-                   RUN apt-get update
-                   RUN apt-get install -yy tcsh''',
-               u'tcsh'))
-    
-      def testBuildLMSeq(self):
-    seq = self._buildDummySequence()
-    lm_seq = data.build_lm_sequence(seq)
-    for i, ts in enumerate(lm_seq):
-      # For end of sequence, the token and label should be same, and weight
-      # should be 0.0.
-      if i == len(lm_seq) - 1:
-        self.assertEqual(ts.token, i)
-        self.assertEqual(ts.label, i)
-        self.assertEqual(ts.weight, 0.0)
-      else:
-        self.assertEqual(ts.token, i)
-        self.assertEqual(ts.label, i + 1)
-        self.assertEqual(ts.weight, 1.0)
-    
-      def testBidirClassifier(self):
-    at_methods = [None, 'rp', 'at', 'vat', 'atvat']
-    for method in at_methods:
-      FLAGS.adv_training_method = method
-      with tf.Graph().as_default():
-        graphs.VatxtBidirModel().classifier_graph()
-    
-    def get_args_for_config(config_name):
-  args = get_default_args()
-  config_name, mode = config_name.split('+')
-  vars = get_vars(config_name)
-  
-  logging.info('config_name: %s, mode: %s', config_name, mode)
-  
-  args.buildinger.task_params.n_ori = int(vars[2])
-  args.solver.freeze_conv = True
-  args.solver.pretrained_path = rgb_resnet_v2_50_path
-  args.buildinger.task_params.img_channels = 5
-  args.solver.data_loss_wt = 0.00001
- 
-  if vars[0] == 'v0':
-    None
-  else:
-    logging.error('config_name: %s undefined', config_name)
-    
-        # Creates node ID --> English string lookup.
-    node_lookup = NodeLookup()
-    
-    
-def default_hooks():
-    return dict((event, []) for event in HOOKS)
-    
-            KD = lambda s, d: hash_utf8('%s:%s' % (s, d))
-    
-    
-class MissingSchema(RequestException, ValueError):
-    '''The URL schema (e.g. http or https) is missing.'''
-    
-        def initialize_options(self):
-        TestCommand.initialize_options(self)
-        try:
-            from multiprocessing import cpu_count
-            self.pytest_args = ['-n', str(cpu_count()), '--boxed']
-        except (ImportError, NotImplementedError):
-            self.pytest_args = ['-n', '1', '--boxed']
-    
-            bypass_proxy = should_bypass_proxies(url, no_proxy=no_proxy)
-        if self.trust_env and not bypass_proxy:
-            environ_proxies = get_environ_proxies(url, no_proxy=no_proxy)
-    
-        Parameters
-    ----------
-    metrics : array-like of callables (1d or 0d)
-        The metric functions to time.
-    
-    
-def rbf_kernels(X, n_jobs):
-    return pairwise_kernels(X, metric='rbf', n_jobs=n_jobs, gamma=0.1)
-    
-    import os
-import tarfile
-from contextlib import closing
-    
-            if not in_exercise_region or linestrip.startswith('#'):
-            output_file.write(line)
-    
-    import matplotlib.pyplot as plt
-    
-    from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
-from sklearn import datasets
-    
-    _options = [
-    'help',
-    'version',
-    'gui',
-    'force',
-    'playlists',
-]
-_short_options = 'hVgfl'
-    
-            html = get_content(self.url)
-    
-    def kugou_download_by_hash(title,hash_val,output_dir = '.', merge = True, info_only = False):
-    #sample
-    #url_sample:http://www.kugou.com/yy/album/single/536957.html
-    #hash ->key  md5(hash+kgcloud')->key  decompile swf
-    #cmd 4 for mp3 cmd 3 for m4a
-    key=hashlib.new('md5',(hash_val+'kgcloud').encode('utf-8')).hexdigest()
-    html=get_html('http://trackercdn.kugou.com/i/?pid=6&key=%s&acceptMp3=1&cmd=4&hash=%s'%(key,hash_val))
-    j=loads(html)
-    url=j['url']
-    songtype, ext, size = url_info(url)
-    print_info(site_info, title, songtype, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge=merge)
-    
-    def kuwo_download_by_rid(rid, output_dir = '.', merge = True, info_only = False):
-    html=get_content('http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_%s'%rid)
-    title=match1(html,r'<name>(.*)</name>')
-    #to get title
-    #format =aac|mp3 ->to get aac format=mp3 ->to get mp3
-    url=get_content('http://antiserver.kuwo.cn/anti.s?format=mp3&rid=MUSIC_%s&type=convert_url&response=url'%rid)
-    songtype, ext, size = url_info(url)
-    print_info(site_info, title, songtype, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir)
-    
-    from ..common import *
-    
-        # mgid%3Auma%3Avideo%3Amtv81.com%3A897974
-    vid = match1(html, r'getTheVideo\('(.*?)'')
-    xml = parseString(
-        get_content('http://intl.esperanto.mtvi.com/www/xml/media/mediaGen.jhtml?uri={}&flashPlayer=LNX%2013,0,0,206&geo=CN&sid=123456'.format(vid)))
-    
-        What it should do it take a markdown file, and split it into more files. A targetfile should have the same
-    number of lines as the original, with source code snippets and markdown non-words removed, for spell-checking.
-    
+            Reference: Armin Ronacher, 'Flask for Fun and for Profit' PyBay 2016.
     '''
-    
-    from tornado.http1connection import HTTP1Connection
-from tornado.httputil import HTTPMessageDelegate
-from tornado.iostream import IOStream
-from tornado.locks import Event
-from tornado.netutil import add_accept_handler
-from tornado.testing import AsyncTestCase, bind_unused_port, gen_test
-    
-        @gen_test
-    def test_float_maxsize(self):
-        # Non-int maxsize must round down: http://bugs.python.org/issue21723
-        q = queues.Queue(maxsize=1.3)
-        self.assertTrue(q.empty())
-        self.assertFalse(q.full())
-        q.put_nowait(0)
-        q.put_nowait(1)
-        self.assertFalse(q.empty())
-        self.assertTrue(q.full())
-        self.assertRaises(queues.QueueFull, q.put_nowait, 2)
-        self.assertEqual(0, q.get_nowait())
-        self.assertFalse(q.empty())
-        self.assertFalse(q.full())
-    
-    print('Starting')
-if 'TESTAPP_STARTED' not in os.environ:
-    os.environ['TESTAPP_STARTED'] = '1'
-    sys.stdout.flush()
-    autoreload._reload()
-'''
+    for name in find_modules('flaskr.blueprints'):
+        mod = import_string(name)
+        if hasattr(mod, 'bp'):
+            app.register_blueprint(mod.bp)
+    return None
     
     
-def handle_sigchld(sig, frame):
-    IOLoop.current().add_callback_from_signal(IOLoop.current().stop)
+@app.route('/')
+def timeline():
+    '''Shows a users timeline or if no user is logged in it will
+    redirect to the public timeline.  This timeline shows the user's
+    messages as well as all the messages of followed users.
+    '''
+    if not g.user:
+        return redirect(url_for('public_timeline'))
+    return render_template('timeline.html', messages=query_db('''
+        select message.*, user.* from message, user
+        where message.author_id = user.user_id and (
+            user.user_id = ? or
+            user.user_id in (select whom_id from follower
+                                    where who_id = ?))
+        order by message.pub_date desc limit ?''',
+        [session['user_id'], session['user_id'], PER_PAGE]))
     
-        def call_wrapped_inner(self, queue, count):
-        if count < 0:
-            return
-        with self.make_context():
-            queue.append(stack_context.wrap(
-                functools.partial(self.call_wrapped_inner, queue, count - 1)))
     
-    from tornado.concurrent import Future
-from tornado import gen
-from tornado.options import define, options, parse_command_line
+def get_line_value(r, n):
+    rls = r.split('\r\n')
+    if len(rls) < n + 1:
+        return None
     
-        @classmethod
-    def send_updates(cls, chat):
-        logging.info('sending message to %d waiters', len(cls.waiters))
-        for waiter in cls.waiters:
-            try:
-                waiter.write_message(chat)
-            except:
-                logging.error('Error sending message', exc_info=True)
+    ## Anything on different channel than DEFAULT_CHANNEL is not parsed
+# by parser.
+HIDDEN_CHANNEL = 99
+    
+        # Parse and validate the field names.  Validation serves two purposes,
+    # generating informative error messages and preventing template injection attacks.
+    if isinstance(field_names, basestring):
+        field_names = field_names.replace(',', ' ').split() # names separated by whitespace and/or commas
+    field_names = tuple(map(str, field_names))
+    for name in (typename,) + field_names:
+        if not all(c.isalnum() or c=='_' for c in name):
+            raise ValueError('Type names and field names can only contain alphanumeric characters and underscores: %r' % name)
+        if _iskeyword(name):
+            raise ValueError('Type names and field names cannot be a keyword: %r' % name)
+        if name[0].isdigit():
+            raise ValueError('Type names and field names cannot start with a number: %r' % name)
+    seen_names = set()
+    for name in field_names:
+        if name.startswith('_'):
+            raise ValueError('Field names cannot start with an underscore: %r' % name)
+        if name in seen_names:
+            raise ValueError('Encountered duplicate field name: %r' % name)
+        seen_names.add(name)
+    
+    
+def MergeMultipleFiletypes_test():
+    
+      Example usage:
