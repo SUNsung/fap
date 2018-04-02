5ef391cda62113d761e7bb86c95c25b1daa1f9dc
SUNsung <weatherius@gmail.com>
2018-04-03 00:13:26 +0300

    Rewrite [06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py] File in: 2018-04-03 00:13:26

--- pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
+++ pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
@@ -1,96 +1,50 @@
 
         
-          def __init__(self, metagraph, model):
-    self._metagraph = metagraph
-    self.replicate_states(model.initial_state_name)
-    self.replicate_states(model.final_state_name)
-    self.update_snapshot_name('variables')
-    self.update_snapshot_name('trainable_variables')
+        
+@pytest.mark.functional
+def test_with_confirmation(proc, TIMEOUT):
+    with_confirmation(proc, TIMEOUT)
     
-        if mode == tf.estimator.ModeKeys.PREDICT:
-        tf.logging.info('my_model_fn: PREDICT, {}'.format(mode))
-    elif mode == tf.estimator.ModeKeys.EVAL:
-        tf.logging.info('my_model_fn: EVAL, {}'.format(mode))
-    elif mode == tf.estimator.ModeKeys.TRAIN:
-        tf.logging.info('my_model_fn: TRAIN, {}'.format(mode))
+    :copyright: © 2010 by the Pallets team.
+:license: BSD, see LICENSE for more details.
+'''
     
-        final_timestep = label_seq[-1]
-    eos_id = len(seq) - 1
-    self.assertAllEqual(final_timestep.tokens, [eos_id, eos_id])
-    self.assertEqual(final_timestep.label, 1)
-    self.assertEqual(final_timestep.weight, 1.0)
     
-      def _lm_loss(self,
-               inputs,
-               emb_key='lm_embedded',
-               lstm_layer='lstm',
-               lm_loss_layer='lm_loss',
-               loss_name='lm_loss',
-               compute_loss=True):
-    embedded = self.layers['embedding'](inputs.tokens)
-    self.tensors[emb_key] = embedded
-    lstm_out, next_state = self.layers[lstm_layer](embedded, inputs.state,
-                                                   inputs.length)
-    if compute_loss:
-      loss = self.layers[lm_loss_layer](
-          [lstm_out, inputs.labels, inputs.weights])
-      with tf.control_dependencies([inputs.save_state(next_state)]):
-        loss = tf.identity(loss)
-        tf.summary.scalar(loss_name, loss)
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != current_app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != current_app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('flaskr.show_entries'))
+    return render_template('login.html', error=error)
     
-      tokens_shape = [2] if bidir_input else []
-  seq_key, ctx, sequence = _read_single_sequence_example(
-      [data_path], tokens_shape=tokens_shape)
-  # Set up stateful queue reader.
-  state_names = _get_tuple_state_names(num_layers, state_name)
-  initial_states = {}
-  for c_state, h_state in state_names:
-    initial_states[c_state] = tf.zeros(state_size)
-    initial_states[h_state] = tf.zeros(state_size)
-  if bidir_input:
-    rev_state_names = _get_tuple_state_names(num_layers,
-                                             '{}_reverse'.format(state_name))
-    for rev_c_state, rev_h_state in rev_state_names:
-      initial_states[rev_c_state] = tf.zeros(state_size)
-      initial_states[rev_h_state] = tf.zeros(state_size)
-  batch = tf.contrib.training.batch_sequences_with_states(
-      input_key=seq_key,
-      input_sequences=sequence,
-      input_context=ctx,
-      input_length=tf.shape(sequence['token_id'])[0],
-      initial_states=initial_states,
-      num_unroll=unroll_steps,
-      batch_size=batch_size,
-      allow_small_batch=False,
-      num_threads=4,
-      capacity=batch_size * 10,
-      make_keys_unique=True,
-      make_keys_unique_seed=29392)
-  return batch
     
-      task_params = utils.Foo(num_actions=4, step_size=4, num_steps=0,
-                          batch_size=32, room_seed=0, base_class='Building',
-                          task='mapping', n_ori=6, data_augment=data_augment,
-                          output_transform_to_global_map=False,
-                          output_canonical_map=False,
-                          output_incremental_transform=False,
-                          output_free_space=False, move_type='shortest_path',
-                          toy_problem=0)
+def register(client, username, password, password2=None, email=None):
+    '''Helper function to register a user'''
+    if password2 is None:
+        password2 = password
+    if email is None:
+        email = username + '@example.com'
+    return client.post('/register', data={
+        'username':     username,
+        'password':     password,
+        'password2':    password2,
+        'email':        email,
+    }, follow_redirects=True)
     
-    py_test(
-    name = 'cifar10_input_test',
-    size = 'small',
-    srcs = ['cifar10_input_test.py'],
-    srcs_version = 'PY2AND3',
-    deps = [
-        ':cifar10_input',
-        '//tensorflow:tensorflow_py',
-        '//tensorflow/python:framework_test_lib',
-        '//tensorflow/python:platform_test',
-    ],
-)
+        project_id = response.json()[0]['id']
     
-      def load(self, label_lookup_path, uid_lookup_path):
-    '''Loads a human readable English name for each softmax node.
+        # This matches a bare IPv4 address or hostname (or host pattern including
+    # [x:y(:z)] ranges) with a port specification.
     
-    package(default_visibility = ['//visibility:public'])
+            # params1 has exclude fields, params2 doesn't. Should be equal
+        actual = GCPUtils.are_params_equal(params1, params2)
+        self.assertTrue(actual)
+    
+    from ansible.utils.vars import combine_vars
