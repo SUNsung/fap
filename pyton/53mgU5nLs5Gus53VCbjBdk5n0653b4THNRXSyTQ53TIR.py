4df5cfd5ac1179dbe967592e76238dd829cf3612
SUNsung <weatherius@gmail.com>
2018-04-03 07:24:45 +0300

    Rewrite [53mgU5nLs5Gus53VCbjBdk5n0653b4THNRXSyTQ53TIR.py] File in: 2018-04-03 07:24:44

--- pyton/53mgU5nLs5Gus53VCbjBdk5n0653b4THNRXSyTQ53TIR.py
+++ pyton/53mgU5nLs5Gus53VCbjBdk5n0653b4THNRXSyTQ53TIR.py
@@ -1,47 +1,92 @@
 
         
-                name, domain = args[0:2]
-        module = sanitize_module_name(name)
+            model.compile(loss=loss_map, optimizer='sgd', metrics=metrics, weighted_metrics=weighted_metrics)
     
-            no_gamma_ds = self.get_or_create(
-            DruidDatasource, {'datasource_name': 'datasource_not_for_gamma'},
-            db.session)
-        no_gamma_ds.cluster = cluster
-        db.session.merge(no_gamma_ds)
-        db.session.commit()
+        # Normally the trailing 1 is added by standardize_weights
+    weights = np.ones((3,))
+    mask = np.ones((3, 4))
+    mask[1, 0] = 0
     
-    # If false, no module index is generated.
-#texinfo_domain_indices = True
-    
-        def test_subselect(self):
-        query = '''
-          SELECT sub.*
-              FROM (
-                    SELECT *
-                      FROM s1.t1
-                     WHERE day_of_week = 'Friday'
-                   ) sub, s2.t2
-          WHERE sub.resolution = 'NONE'
+            # Arguments
+            batch: integer, index of batch within the current epoch.
+            logs: dictionary of logs.
         '''
-        self.assertEquals({'s1.t1', 's2.t2'},
-                          self.extract_tables(query))
+        logs = logs or {}
+        t_before_callbacks = time.time()
+        for callback in self.callbacks:
+            callback.on_batch_begin(batch, logs)
+        self._delta_ts_batch_begin.append(time.time() - t_before_callbacks)
+        delta_t_median = np.median(self._delta_ts_batch_begin)
+        if (self._delta_t_batch > 0. and
+           delta_t_median > 0.95 * self._delta_t_batch and
+           delta_t_median > 0.1):
+            warnings.warn('Method on_batch_begin() is slow compared '
+                          'to the batch update (%f). Check your callbacks.'
+                          % delta_t_median)
+        self._t_enter_batch = time.time()
     
-        worker = celery_app.Worker(optimization='fair')
-    worker.start()
     
-    def downgrade():
-    constraint = find_constraint_name(False) or 'fk_columns_datasource_name_datasources'
-    with op.batch_alter_table('columns',
-        naming_convention=naming_convention) as batch_op:
-        batch_op.drop_constraint(constraint, type_='foreignkey')
-        batch_op.create_foreign_key(
-            'fk_columns_column_name_datasources',
-            'datasources',
-            ['column_name'], ['datasource_name'])
+@keras_test
+def test_locallyconnected_2d():
+    num_samples = 5
+    filters = 3
+    stack_size = 4
+    num_row = 6
+    num_col = 8
+    padding = 'valid'
+    
+    print('Loading data...')
+(x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=max_words,
+                                                         test_split=0.2)
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
+    
+        branch_1_2 = models.Sequential([legacy_layers.Merge([branch_1, branch_2], mode='concat')], name='branch_1_2')
+    branch_1_2.add(layers.Dense(16, name='dense_1_2-0'))
+    # test whether impromptu input_shape breaks the model
+    branch_1_2.add(layers.Dense(16, input_shape=(16,), name='dense_1_2-1'))
+    
+        result = f([test_values])[0]
+    expected = softmax(test_values)
+    assert_allclose(result, expected, rtol=1e-05)
+    
+        def set(self, key, value):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                item.value = value
+                return
+        self.table[hash_index].append(Item(key, value))
+    
+        HEART = 0
+    DIAMOND = 1
+    CLUBS = 2
+    SPADE = 3
+    
+            When updating an entry, updates its position to the front of the LRU list.
+        If the entry is new and the cache is at capacity, removes the oldest entry
+        before the new entry is added.
+        '''
+        node = self.lookup[query]
+        if node is not None:
+            # Key exists in cache, update the value
+            node.results = results
+            self.linked_list.move_to_front(node)
+        else:
+            # Key does not exist in cache
+            if self.size == self.MAX_SIZE:
+                # Remove the oldest entry from the linked list and lookup
+                self.lookup.pop(self.linked_list.tail.query, None)
+                self.linked_list.remove_from_tail()
+            else:
+                self.size += 1
+            # Add the new key and value
+            new_node = Node(results)
+            self.linked_list.append_to_front(new_node)
+            self.lookup[query] = new_node
 
     
-    # revision identifiers, used by Alembic.
-revision = '18e88e1cc004'
-down_revision = '430039611635'
+        def move_to_front(self, node):
+        ...
     
-        urls = []
+        point = readme_soup.find_all('h1')[1]
