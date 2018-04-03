ac063f6e9a65d4c1a04027e8ebc950d7e48e18a5
SUNsung <weatherius@gmail.com>
2018-04-03 05:53:11 +0300

    Rewrite [56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb] File in: 2018-04-03 05:53:11

--- ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
+++ ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
@@ -1,6 +1,76 @@
 
         
-            ret = route.slice(:location, :label)
-    ret[:full_location] = 'adminPlugins.#{ret[:location]}'
-    ret
+          def updated
+    object.updated_at.iso8601
   end
+    
+          put :update, params: {
+        user: {
+          notification_emails: { follow: '1' },
+          interactions: { must_be_follower: '0' },
+        }
+      }
+    
+    namespace :emojis do
+  desc 'Generate a unicode to filename mapping'
+  task :generate do
+    source = 'http://www.unicode.org/Public/emoji/5.0/emoji-test.txt'
+    codes  = []
+    dest   = Rails.root.join('app', 'javascript', 'mastodon', 'features', 'emoji', 'emoji_map.json')
+    
+          case Rails.env
+      when 'development'
+        config.eager_load = false
+      when 'test'
+        config.eager_load = false
+      when 'production'
+        config.eager_load = true
+      end
+    end
+  end
+end
+    
+    
+class SnifferIMAP < BaseProtocolParser
+    
+    sock = TCPSocket.new(ip, port)
+    
+    	def parse_file(file)
+		while (line = file.gets)
+			parse_line(line)
+		end
+    
+      self.include_root_in_json = false
+    
+          def response_type_as_space_seperated_values
+        [*session[:response_type]].join(' ')
+      end
+    
+          rescue_from Rack::OAuth2::Server::Authorize::BadRequest,
+                  JSON::JWT::InvalidFormat, JSON::JWK::UnknownAlgorithm do |e|
+        logger.info e.backtrace[0, 10].join('\n')
+        render json: {error: :invalid_request, error_description: e.message, status: 400}
+      end
+      rescue_from JSON::JWT::VerificationFailed do |e|
+        logger.info e.backtrace[0, 10].join('\n')
+        render json: {error: :invalid_grant, error_description: e.message, status: 400}
+      end
+    end
+  end
+end
+
+    
+    module Hbc
+  class DSL
+    class Container
+      VALID_KEYS = Set.new [
+        :type,
+        :nested,
+      ]
+    
+          def started?
+        true
+      end
+    
+      public
+  def close; end;
