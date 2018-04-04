da95170a0dbbab8066789fc8e07623aad91afb69
SUNsung <weatherius@gmail.com>
2018-04-04 05:11:40 +0300

    Rewrite [52S5RObwiVrkk52avGEptlTUIw52896Hbba4orU52s3B.rb] File in: 2018-04-04 05:11:40

--- ruby/52S5RObwiVrkk52avGEptlTUIw52896Hbba4orU52s3B.rb
+++ ruby/52S5RObwiVrkk52avGEptlTUIw52896Hbba4orU52s3B.rb
@@ -1,184 +1,120 @@
 
         
-            A binary installer is available:
-      https://www.haskell.org/platform/mac.html
-    EOS
-  when 'mysqldump-secure' then <<-EOS.undent
-    The creator of mysqldump-secure tried to game our popularity metrics.
-    EOS
-  when 'ngrok' then <<-EOS.undent
-    Upstream sunsetted 1.x in March 2016 and 2.x is not open-source.
-    
-      # True if a {Formula} is being built with {Formula.head} instead of {Formula.stable}.
-  # <pre>args << '--some-new-stuff' if build.head?</pre>
-  # <pre># If there are multiple conditional arguments use a block instead of lines.
-  #  if build.head?
-  #    args << '--i-want-pizza'
-  #    args << '--and-a-cold-beer' if build.with? 'cold-beer'
-  #  end</pre>
-  def head?
-    include? 'HEAD'
-  end
-    
-          if options[:type] == :array && (options[:values].blank? || !options[:values].is_a?(Array))
-        raise ArgumentError.new('When using :array as :type you need to provide the :values as an Array')
+            class Callback3 < ControllerWithCallbacks
+      before_action do |c|
+        c.instance_variable_set('@text', 'Hello world')
       end
     
-      def test_aset
-    assert_nothing_raised { ENV['test'] = nil }
-    assert_equal(nil, ENV['test'])
-    assert_invalid_env {|v| ENV[v] = 'test'}
-    assert_invalid_env {|v| ENV['test'] = v}
-    
-      def test_ASET # '[]='
-    t = Time.now
-    h = @cls.new
-    h[1]     = 'one'
-    h[2]     = 'two'
-    h[3]     = 'three'
-    h[self]  = 'self'
-    h[t]     = 'time'
-    h[nil]   = 'nil'
-    h['nil'] = nil
-    assert_equal('one',   h[1])
-    assert_equal('two',   h[2])
-    assert_equal('three', h[3])
-    assert_equal('self',  h[self])
-    assert_equal('time',  h[t])
-    assert_equal('nil',   h[nil])
-    assert_equal(nil,     h['nil'])
-    assert_equal(nil,     h['koala'])
-    
-      def test_huge_difference
-    if negative_time_t?
-      assert_equal(Time.at(-0x80000000), Time.at(0x7fffffff) - 0xffffffff, '[ruby-dev:22619]')
-      assert_equal(Time.at(-0x80000000), Time.at(0x7fffffff) + (-0xffffffff))
-      assert_equal(Time.at(0x7fffffff), Time.at(-0x80000000) + 0xffffffff, '[ruby-dev:22619]')
-      assert_equal(Time.at(0x7fffffff), Time.at(-0x80000000) - (-0xffffffff))
-    end
-  end
-    
-      it 'decodes a negative double' do
-    '\x9a\x99\x99\x99\x99\x19A\xc0'.unpack(unpack_format).should == [-34.2]
-  end
-    
-        t.join
-  end
-    
-    desc 'Move source to source.old, install source theme updates, replace source/_includes/navigation.html with source.old's navigation'
-task :update_source, :theme do |t, args|
-  theme = args.theme || 'classic'
-  if File.directory?('#{source_dir}.old')
-    puts '## Removed existing #{source_dir}.old directory'
-    rm_r '#{source_dir}.old', :secure=>true
-  end
-  mkdir '#{source_dir}.old'
-  cp_r '#{source_dir}/.', '#{source_dir}.old'
-  puts '## Copied #{source_dir} into #{source_dir}.old/'
-  cp_r '#{themes_dir}/'+theme+'/source/.', source_dir, :remove_destination=>true
-  cp_r '#{source_dir}.old/_includes/custom/.', '#{source_dir}/_includes/custom/', :remove_destination=>true
-  cp '#{source_dir}.old/favicon.png', source_dir
-  mv '#{source_dir}/index.html', '#{blog_index_dir}', :force=>true if blog_index_dir != source_dir
-  cp '#{source_dir}.old/index.html', source_dir if blog_index_dir != source_dir && File.exists?('#{source_dir}.old/index.html')
-  puts '## Updated #{source_dir} ##'
-end
-    
-    # The project root directory
-$root = ::File.dirname(__FILE__)
-    
-        # Outputs a single category as an <a> link.
-    #
-    #  +category+ is a category string to format as an <a> link
-    #
-    # Returns string
-    #
-    def category_link(category)
-      dir = @context.registers[:site].config['category_dir']
-      '<a class='category' href='/#{dir}/#{category.to_url}/'>#{category}</a>'
-    end
-    
-    def config_tag(config, key, tag=nil, classname=nil)
-  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
-  tag       ||= 'div'
-  classname ||= key.sub(/_/, '-').sub(/\./, '-')
-  output      = '<#{tag} class='#{classname}''
-    
-        def render(context)
-      if @img
-        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
-      else
-        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
-      end
-    end
-  end
-end
-    
-      class IncludeCodeTag < Liquid::Tag
-    def initialize(tag_name, markup, tokens)
-      @title = nil
-      @file = nil
-      if markup.strip =~ /\s*lang:(\S+)/i
-        @filetype = $1
-        markup = markup.strip.sub(/lang:\S+/i,'')
-      end
-      if markup.strip =~ /(.*)?(\s+|^)(\/*\S+)/i
-        @title = $1 || nil
-        @file = $3
-      end
-      super
-    end
-    
-        def render(context)
-      output = super
-      types = {
-        '.mp4' => 'type='video/mp4; codecs=\'avc1.42E01E, mp4a.40.2\''',
-        '.ogv' => 'type='video/ogg; codecs=theora, vorbis'',
-        '.webm' => 'type='video/webm; codecs=vp8, vorbis''
-      }
-      if @videos.size > 0
-        video =  '<video #{sizes} preload='metadata' controls #{poster}>'
-        @videos.each do |v|
-          video << '<source src='#{v}' #{types[File.extname(v)]}>'
+            if value.is_a?(Hash) || value.is_a?(ActionController::Parameters)
+          value = '(#{dump_params_keys(value)})'
+        else
+          value = ''
         end
-        video += '</video>'
-      else
-        'Error processing input, expected syntax: {% video url/to/video [url/to/video] [url/to/video] [width height] [url/to/poster] %}'
-      end
-    end
     
-        def definitions_for(klass)
-      parent_classes = klass.ancestors.reverse
-      parent_classes.each_with_object({}) do |ancestor, inherited_definitions|
-        inherited_definitions.deep_merge! @attachments[ancestor]
-      end
-    end
+    module ActionMailer
+  # The <tt>ActionMailer::DeliveryJob</tt> class is used when you
+  # want to send emails outside of the request-response cycle.
+  #
+  # Exceptions are rescued and handled by the mailer class.
+  class DeliveryJob < ActiveJob::Base # :nodoc:
+    queue_as { ActionMailer::Base.deliver_later_queue_name }
+    
+      # Skips the current run on Rubinius using Minitest::Assertions#skip
+  private def rubinius_skip(message = '')
+    skip message if RUBY_ENGINE == 'rbx'
+  end
+  # Skips the current run on JRuby using Minitest::Assertions#skip
+  private def jruby_skip(message = '')
+    skip message if defined?(JRUBY_VERSION)
   end
 end
 
     
-            def failure_message
-          '#{expected_attachment}\n'.tap do |message|
-            message << accepted_types_and_failures.to_s
-            message << '\n\n' if @allowed_types.present? && @rejected_types.present?
-            message << rejected_types_and_failures.to_s
+    def dest_dir(*subdirs)
+  test_dir('dest', *subdirs)
+end
+    
+    options = {
+  sort: true,
+  limit: 30,
+  format: :text,
+}
+    
+          def delete(key)
+        super(convert_key(key))
+      end
+    
+              if argv.length == 2
+            # @deprecated
+            @env.ui.warn('WARNING: The second argument to `vagrant box remove`')
+            @env.ui.warn('is deprecated. Please use the --provider flag. This')
+            @env.ui.warn('feature will stop working in the next version.')
+            options[:provider] = argv[1]
           end
-        end
     
+              # Repackage the box
+          output_name = @env.vagrantfile.config.package.name || 'package.box'
+          output_path = Pathname.new(File.expand_path(output_name, FileUtils.pwd))
+          box.repackage(output_path)
     
-    {  # Returns hash with styles missing from recent run of rake paperclip:refresh:missing_styles
-  #   {
-  #     :User => {:avatar => [:big]},
-  #     :Book => {
-  #       :cover => [:croppable]},
-  #     }
-  #   }
-  def self.missing_attachments_styles
-    current_styles = current_attachments_styles
-    registered_styles = get_registered_attachments_styles
+      def test_bom_32le
+    assert_bom(['\xFF\xFE\0', '\0'], __method__)
+  end
     
-            required = directories.map do |directory|
-          pathname = File.expand_path(Rails.root.join(directory, filename))
-          file_exists = File.exist?(pathname)
-          require pathname if file_exists
-          file_exists
-        end
+      it 'decodes positive Infinity' do
+    '\x00\x00\x80\x7f'.unpack(unpack_format).should == [infinity_value]
+  end
+    
+      it 'decodes the number of characters specified by the count modifier' do
+    [ ['\xc2\x80\xc2\x81\xc2\x82\xc2\x83', 'U1', [0x80]],
+      ['\xc2\x80\xc2\x81\xc2\x82\xc2\x83', 'U2', [0x80, 0x81]],
+      ['\xc2\x80\xc2\x81\xc2\x82\xc2\x83', 'U3', [0x80, 0x81, 0x82]]
+    ].should be_computed_by(:unpack)
+  end
+    
+      def self.main_thread2(critical_thread)
+    Thread.pass # The join below seems to cause a deadlock with CRuby unless Thread.pass is called first
+    critical_thread.join
+    Thread.critical.should == false
+  end
+    
+        10.times { sleep 0.1 if after_sleep1 != true }
+    10.times { sleep 0.1 if t.status and t.status != 'sleep' }
+    after_sleep2.should == false # t should be blocked on the second sleep
+    t.send(@method)
+    
+      class Callbacks
+    def self.around_create(record)
+      now = Time.now.utc
+    
+      # Before we load the schema, define the timestamp_id function.
+  # Idiomatically, we might do this in a migration, but then it
+  # wouldn't end up in schema.rb, so we'd need to figure out a way to
+  # get it in before doing db:setup as well. This is simpler, and
+  # ensures it's always in place.
+  Rake::Task['db:schema:load'].enhance ['db:define_timestamp_id']
+    
+      def set_body_classes
+    @body_classes = 'tag-body'
+  end
+    
+      def icon
+    object.image
+  end
+    
+        it 'Reads referrer from Host header when Referer header is relative' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
+      expect(subject.referrer(env)).to eq('foo.com')
+    end
+    
+      it 'should set the X-Frame-Options' do
+    expect(get('/', {}, 'wants' => 'text/html').headers['X-Frame-Options']).to eq('SAMEORIGIN')
+  end
+    
+        %w[/foo/bar /foo/bar/ / /.f /a.x].each do |path|
+      it('does not touch #{path.inspect}') { expect(get(path).body).to eq(path) }
+    end
+    
+      it 'should set the X-XSS-Protection' do
+    expect(get('/', {}, 'wants' => 'text/html;charset=utf-8').headers['X-XSS-Protection']).to eq('1; mode=block')
+  end
