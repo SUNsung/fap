8cd9142ed03dda592beb05673a8898aee5f4fee6
SUNsung <weatherius@gmail.com>
2018-04-04 09:22:27 +0300

    Rewrite [05VxaODfdT.c.05VqVToGgSMSM05WsTXfpx1eQ205qub.cpp] File in: 2018-04-04 09:22:27

--- cccccc/05VxaODfdT.c.05VqVToGgSMSM05WsTXfpx1eQ205qub.cpp
+++ cccccc/05VxaODfdT.c.05VqVToGgSMSM05WsTXfpx1eQ205qub.cpp
@@ -1,177 +1,110 @@
 
         
-        
-#define REGISTER_LAYER_CREATOR(type, creator)                                  \
-  static LayerRegisterer<float> g_creator_f_##type(#type, creator<float>);     \
-  static LayerRegisterer<double> g_creator_d_##type(#type, creator<double>)    \
+          /// Prepare a callee for an Objective-C method.
+  Callee getObjCMethodCallee(IRGenFunction &IGF, const ObjCMethod &method,
+                             llvm::Value *selfValue, CalleeInfo &&info);
     
-     protected:
-  /**
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the computed outputs. At training time, we have @f$
-   *      y_{\mbox{train}} = \left\{
-   *         \begin{array}{ll}
-   *            \frac{x}{1 - p} & \mbox{if } u > p \\
-   *            0 & \mbox{otherwise}
-   *         \end{array} \right.
-   *      @f$, where @f$ u \sim U(0, 1)@f$ is generated independently for each
-   *      input at each iteration. At test time, we simply have
-   *      @f$ y_{\mbox{test}} = \mathbb{E}[y_{\mbox{train}}] = x @f$.
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    public:
+  DeadEndBlocks(const SILFunction *F) : F(F) {}
+    
+      /// Indicates whether the diagnostics produced during compilation should be
+  /// checked against expected diagnostics, indicated by markers in the
+  /// input source file.
+  enum {
+    NoVerify,
+    Verify,
+    VerifyAndApplyFixes
+  } VerifyMode = NoVerify;
+    
+    /// \brief Diagnostic consumer that displays diagnostics to standard error.
+class PrintingDiagnosticConsumer : public DiagnosticConsumer {
+  llvm::raw_ostream &Stream;
+  bool ForceColors = false;
+  bool DidErrorOccur = false;
+public:
+  PrintingDiagnosticConsumer(llvm::raw_ostream &stream = llvm::errs()) :
+    Stream(stream) { }
     }
     
-      virtual inline const char* type() const { return 'Filter'; }
-  virtual inline int MinBottomBlobs() const { return 2; }
-  virtual inline int MinTopBlobs() const { return 1; }
+    #ifndef SWIFT_BASIC_SOURCEMANAGER_H
+#define SWIFT_BASIC_SOURCEMANAGER_H
     
-            // Build and verify node connection (edges).
-        // Verify NodeArg name/type/shape matching correctly.
-        Status BuildConnections(
-            const std::unordered_map<std::string, Node::EdgeEnd>& p_outputArgs,
-            const std::unordered_map<std::string, NODEINDEX>& p_nodeNameToIndex);
+    #ifndef SWIFT_SYNTAX_REFERENCES_H
+#define SWIFT_SYNTAX_REFERENCES_H
     
-            // 'int' rather than 'size_t' because of a protobuf design choice; let callers handle type checks
-        static Status LoadFromBytes(int count, void *pBytes, /*out*/ std::shared_ptr<Model>* p_model);
-    
-                // Attribute description.
-            std::string m_description;
-    
-    #define REGISTER_BINARY_COMPARISON_OPERATOR_SCHEMA(OpName)                                                      \
-    REGISTER_OPERATOR_SCHEMA(OpName)                                                                            \
-        .Description('Returns the tensor resulted from performing the ''#OpName'' logical operation'            \
-        'elementwise on the input tensors A and B. If broadcasting is enabled, the right-hand-side'             \
-        'argument will be broadcasted to match the shape of left-hand-side argument. Refer to Add for'          \
-        'a detailed description of the broadcasting rules.')                                                    \
-        .Input('A', 'First operand, should share the type with the second operand.', 'T1')                      \
-        .Input('B', 'Second operand. With broadcasting can be of smaller size than A.'                          \
-            'If broadcasting is disabled, it should be of the same size.', 'T1')                                \
-        .Output('C', 'Result, has same dimensions as A and type bool.', 'T2')                                   \
-        .TypeConstraint('T1', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },                         \
-                'Constrain input to float tensors.')                                                            \
-        .TypeConstraint('T2', { 'tensor(bool)' }, 'Constrain output types to bool tensor.')                     \
-        .Attr('axis', 'If set, defines the broadcast dimensions.',                                              \
-            AttrType::AttributeProto_AttributeType_INT)                                                         \
-        .Attr('broadcast', 'Pass 1 to enable broadcasting.',                                                    \
-            AttrType::AttributeProto_AttributeType_INT);
-    
-        REGISTER_OPERATOR_SCHEMA(MeanVarianceNormalization)
-        .Description('Perform mean variance normalization.')
-        .Input('input', 'Input tensor of any shape', 'T')
-        .Output('output', 'Output tensor of same shape and type as input X.', 'T')
-        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' }, 'Constrain input and output '
-            'types to float tensors.')
-        .Attr('across_channels', 'If true, mean and variance are computed across channels. '
-            'Default is false.', AttrType::AttributeProto_AttributeType_INT, int64_t(0))
-        .Attr('normalize_variance', 'If false, normalize the mean only. Default is true.',
-            AttrType::AttributeProto_AttributeType_INT, int64_t(1));
-    
-        ConfigHelper config(streamConfig);
-    auto context = config.GetContextWindow();
-    
-            auto operandPlaceholder = PlaceholderVariable();
-        auto operandSquare = Square(operandPlaceholder);
-        operandSquare = Reshape(operandSquare, { NDShape::InferredDimension, 1 }, Axis(2), Axis(3));
-        auto weights = Constant({ 1, 1, 2 * depthRadius + 1, 1 }, operand.GetDataType(), alpha / (2 * depthRadius + 1));
-        auto convResult = Convolution(weights, operandSquare);
-        convResult = Reshape(convResult, { NDShape::InferredDimension }, Axis(2), Axis(4));
-        auto denom = Exp(ElementTimes(Constant::Scalar(operand.GetDataType(), beta), Log(Plus(Constant::Scalar(operand.GetDataType(), bias), convResult))));
-    
-    template <class ssematrixbase>
-class ssematrixstriperef : public ssematrixbase
+    Substitution::Substitution(Type Replacement,
+                           ArrayRef<ProtocolConformanceRef> Conformance)
+  : Replacement(Replacement), Conformance(Conformance)
 {
-    // do not assign this; instead pass around by reference
-    // (we could give this up easily, but why if never needed so far)
-    ssematrixstriperef &operator=(ssematrixstriperef &other);
-    ssematrixstriperef(ssematrixstriperef &other);
-    }
-    
-      // Check that the file exists.
-  ASSERT_TRUE(env_->FileExists('/dir/f'));
-  ASSERT_OK(env_->GetFileSize('/dir/f', &file_size));
-  ASSERT_EQ(0, file_size);
-  ASSERT_OK(env_->GetChildren('/dir', &children));
-  ASSERT_EQ(1, children.size());
-  ASSERT_EQ('f', children[0]);
-    
-    class Block::Iter : public Iterator {
- private:
-  const Comparator* const comparator_;
-  const char* const data_;      // underlying block contents
-  uint32_t const restarts_;     // Offset of restart array (list of fixed32)
-  uint32_t const num_restarts_; // Number of uint32_t entries in restart array
-    }
-    
-    Iterator::Iterator() {
-  cleanup_.function = NULL;
-  cleanup_.next = NULL;
+  // The replacement type must be materializable.
+  assert(Replacement->isMaterializable()
+         && 'cannot substitute with a non-materializable type');
 }
     
-    #include 'leveldb/filter_policy.h'
+    ScrollView* bln_word_window_handle();  //return handle
+void build_image_window(int width, int height);
+void display_bln_lines(ScrollView window,
+                       ScrollView::Color colour,
+                       float scale_factor,
+                       float y_offset,
+                       float minx,
+                       float maxx);
+                                 //function to call
+void pgeditor_msg(  //message display
+                  const char *msg);
+void pgeditor_show_point(  //display coords
+                         SVEvent *event);
+                                 //put bln word in       box
+void show_point(PAGE_RES* page_res, float x, float y);
     
-    // Abort the program after logging the mesage if the given condition is not
-// true. Otherwise, do nothing.
-#define GRPC_CODEGEN_CHECK(x)                                            \
-  (x) ? (void)0                                                          \
-      : LogMessageVoidify() & LogHelper(&std::cerr).get_os()             \
-                                  << 'CHECK FAILED: ' << __FILE__ << ':' \
-                                  << __LINE__ << ': '
-    
-    std::string GenType(const Type &type) {
-  if (type.enum_def != nullptr && !type.enum_def->is_union) {
-    // it is a reference to an enum type
-    return GenTypeRef(type.enum_def);
+      // Connects this and other, discarding any existing connections.
+  void Connect(DoublePtr* other) {
+    other->Disconnect();
+    Disconnect();
+    other->other_end_ = this;
+    other_end_ = other;
   }
-  switch (type.base_type) {
-    case BASE_TYPE_VECTOR: {
-      std::string typeline;
-      typeline.append('\'type\' : \'array\', \'items\' : { ');
-      if (type.element == BASE_TYPE_STRUCT) {
-        typeline.append(GenTypeRef(type.struct_def));
-      } else {
-        typeline.append(GenType(GenNativeType(type.element)));
-      }
-      typeline.append(' }');
-      return typeline;
+  // Disconnects this and other, making OtherEnd() return NULL for both.
+  void Disconnect() {
+    if (other_end_ != NULL) {
+      other_end_->other_end_ = NULL;
+      other_end_ = NULL;
     }
-    case BASE_TYPE_STRUCT: {
-      return GenTypeRef(type.struct_def);
-    }
-    case BASE_TYPE_UNION: {
-      std::string union_type_string('\'anyOf\': [');
-      const auto &union_types = type.enum_def->vals.vec;
-      for (auto ut = union_types.cbegin(); ut < union_types.cend(); ++ut) {
-        auto &union_type = *ut;
-        if (union_type->union_type.base_type == BASE_TYPE_NONE) { continue; }
-        if (union_type->union_type.base_type == BASE_TYPE_STRUCT) {
-          union_type_string.append(
-              '{ ' + GenTypeRef(union_type->union_type.struct_def) + ' }');
-        }
-        if (union_type != *type.enum_def->vals.vec.rbegin()) {
-          union_type_string.append(',');
-        }
-      }
-      union_type_string.append(']');
-      return union_type_string;
-    }
-    case BASE_TYPE_UTYPE: return GenTypeRef(type.enum_def);
-    default: return GenType(GenNativeType(type.base_type));
   }
-}
+  // Returns the pointer to the other end of the double pointer.
+  DoublePtr* OtherEnd() const {
+    return other_end_;
+  }
     
-    bool GeneratePython(const Parser &parser, const std::string &path,
-                    const std::string &file_name) {
-  python::PythonGenerator generator(parser, path, file_name);
-  return generator.generate();
-}
+    // Tests a classifier, computing its error rate.
+// See errorcounter.h for description of arguments.
+// Iterates over the samples, calling the classifier in normal/silent mode.
+// If the classifier makes a CT_UNICHAR_TOPN_ERR error, and the appropriate
+// report_level is set (4 or greater), it will then call the classifier again
+// with a debug flag and a keep_this argument to find out what is going on.
+double ErrorCounter::ComputeErrorRate(ShapeClassifier* classifier,
+    int report_level, CountTypes boosting_mode,
+    const FontInfoTable& fontinfo_table,
+    const GenericVector<Pix*>& page_images, SampleIterator* it,
+    double* unichar_error,  double* scaled_error, STRING* fonts_report) {
+  int fontsize = it->sample_set()->NumFonts();
+  ErrorCounter counter(classifier->GetUnicharset(), fontsize);
+  GenericVector<UnicharRating> results;
+    }
+    
+    
+    
+        template <typename ElementType>
+    ElementType Value::AsScalar() const
+    {
+        if (Mask())
+            LogicError('Value::AsScalar: Scalar Value object must not have an associated mask');
+    }
+    
+        mAsparse.SwitchToMatrixType(MatrixType::SPARSE, matrixFormatSparseCSR, true);
+    mBsparse.SwitchToMatrixType(MatrixType::SPARSE, matrixFormatSparseCSR, true);
+    Matrix<float>::ScaleAndAdd(alpha, mAsparse, mBsparse);
+    
+        StreamMinibatchInputs inputMatrices;
+    for (auto& node : featureNodes)
+        inputMatrices.AddInput(node->NodeName(), node->ValuePtr(), node->GetMBLayout(), node->GetSampleLayout());
