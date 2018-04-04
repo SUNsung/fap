7d5a5a0c3f432d53173631fa60942e6271a878a1
SUNsung <weatherius@gmail.com>
2018-04-05 02:56:13 +0300

    Rewrite [56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb] File in: 2018-04-05 02:56:12

--- ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
+++ ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
@@ -1,76 +1,50 @@
 
         
-          def updated
-    object.updated_at.iso8601
+              def self.example_code
+        [
+          'create_keychain(
+            name: 'KeychainName',
+            default_keychain: true,
+            unlock: true,
+            timeout: 3600,
+            lock_when_sleeps: true
+          )'
+        ]
+      end
+    
+        context 'Mercurial repository' do
+      before do
+        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_svn?).and_return(false)
+        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_git_svn?).and_return(false)
+        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_git?).and_return(false)
+        expect(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_hg?).and_return(true)
+      end
+    
+          it 'increments the build number of the Xcode project' do
+        Fastlane::FastFile.new.parse('lane :test do
+          increment_build_number(xcodeproj: '.xcproject')
+        end').runner.execute(:test)
+    
+      def tumblr_oauth_token_secret
+    service.secret
   end
     
-          put :update, params: {
-        user: {
-          notification_emails: { follow: '1' },
-          interactions: { must_be_follower: '0' },
-        }
-      }
-    
-    namespace :emojis do
-  desc 'Generate a unicode to filename mapping'
-  task :generate do
-    source = 'http://www.unicode.org/Public/emoji/5.0/emoji-test.txt'
-    codes  = []
-    dest   = Rails.root.join('app', 'javascript', 'mastodon', 'features', 'emoji', 'emoji_map.json')
-    
-          case Rails.env
-      when 'development'
-        config.eager_load = false
-      when 'test'
-        config.eager_load = false
-      when 'production'
-        config.eager_load = true
-      end
+        def register_rails_engine
+      require 'bootstrap-sass/engine'
     end
+    
+    class NodeMincerTest < Minitest::Test
+  DUMMY_PATH = 'test/dummy_node_mincer'
+    
+      path = 'assets/stylesheets'
+  css_path = args.with_defaults(css_path: 'tmp')[:css_path]
+  puts Term::ANSIColor.bold 'Compiling SCSS in #{path}'
+  Dir.mkdir(css_path) unless File.directory?(css_path)
+  %w(_bootstrap bootstrap/_theme).each do |file|
+    save_path = '#{css_path}/#{file.sub(/(^|\/)?_+/, '\1').sub('/', '-')}.css'
+    puts Term::ANSIColor.cyan('  #{save_path}') + '...'
+    engine    = Sass::Engine.for_file('#{path}/#{file}.scss', syntax: :scss, load_paths: [path])
+    css       = engine.render
+    File.open(save_path, 'w') { |f| f.write css }
   end
 end
-    
-    
-class SnifferIMAP < BaseProtocolParser
-    
-    sock = TCPSocket.new(ip, port)
-    
-    	def parse_file(file)
-		while (line = file.gets)
-			parse_line(line)
-		end
-    
-      self.include_root_in_json = false
-    
-          def response_type_as_space_seperated_values
-        [*session[:response_type]].join(' ')
-      end
-    
-          rescue_from Rack::OAuth2::Server::Authorize::BadRequest,
-                  JSON::JWT::InvalidFormat, JSON::JWK::UnknownAlgorithm do |e|
-        logger.info e.backtrace[0, 10].join('\n')
-        render json: {error: :invalid_request, error_description: e.message, status: 400}
-      end
-      rescue_from JSON::JWT::VerificationFailed do |e|
-        logger.info e.backtrace[0, 10].join('\n')
-        render json: {error: :invalid_grant, error_description: e.message, status: 400}
-      end
-    end
-  end
-end
-
-    
-    module Hbc
-  class DSL
-    class Container
-      VALID_KEYS = Set.new [
-        :type,
-        :nested,
-      ]
-    
-          def started?
-        true
-      end
-    
-      public
-  def close; end;
