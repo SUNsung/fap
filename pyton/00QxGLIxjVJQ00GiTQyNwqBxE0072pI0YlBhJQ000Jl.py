9291e2045afc4aeafce2796b195c57bfb28ab320
SUNsung <weatherius@gmail.com>
2018-04-05 08:10:50 +0300

    Rewrite [00QxGLIxjVJQ00GiTQyNwqBxE0072pI0YlBhJQ000Jl.py] File in: 2018-04-05 08:10:50

--- pyton/00QxGLIxjVJQ00GiTQyNwqBxE0072pI0YlBhJQ000Jl.py
+++ pyton/00QxGLIxjVJQ00GiTQyNwqBxE0072pI0YlBhJQ000Jl.py
@@ -1,233 +1,51 @@
-        if any([s_line.startswith(s) for s in ['* [', '- [']]):
-            if indent == last_indent:
-                blocks[-1].append(line)
-            else:
-                blocks.append([line])
-            last_indent = indent
-        else:
-            blocks.append([line])
-            last_indent = None
+
+        
+        # Dynamic inventory script which lets you use nodes discovered by Serf
+# (https://serfdom.io/).
+#
+# Requires the `serfclient` Python module from
+# https://pypi.python.org/pypi/serfclient
+#
+# Environment variables
+# ---------------------
+#   - `SERF_RPC_ADDR`
+#   - `SERF_RPC_AUTH`
+#
+# These variables are described at https://www.serfdom.io/docs/commands/members.html#_rpc_addr
     
-    filenames = {
-    'bin': 'youtube-dl',
-    'exe': 'youtube-dl.exe',
-    'tar': 'youtube-dl-%s.tar.gz' % version}
-build_dir = os.path.join('..', '..', 'build', version)
-for key, filename in filenames.items():
-    url = 'https://yt-dl.org/downloads/%s/%s' % (version, filename)
-    fn = os.path.join(build_dir, filename)
-    with open(fn, 'rb') as f:
-        data = f.read()
-    if not data:
-        raise ValueError('File %s is empty!' % fn)
-    sha256sum = hashlib.sha256(data).hexdigest()
-    new_version[key] = (url, sha256sum)
+        if s1.st_dev != s2.st_dev:
+        return True     # path/.. on a different device as path
+    if s1.st_ino == s2.st_ino:
+        return True     # path/.. is the same i-node as path, i.e. path=='/'
+    return False
+
     
-    now = datetime.datetime.now()
-now_iso = now.isoformat() + 'Z'
+        def test_get_entity_name_from_resource_name(self):
+        input_data = 'urlMaps'
+        actual = GCPUtils.get_entity_name_from_resource_name(input_data)
+        self.assertEqual('urlMap', actual)
+        input_data = 'targetHttpProxies'
+        actual = GCPUtils.get_entity_name_from_resource_name(input_data)
+        self.assertEqual('targetHttpProxy', actual)
+        input_data = 'globalForwardingRules'
+        actual = GCPUtils.get_entity_name_from_resource_name(input_data)
+        self.assertEqual('forwardingRule', actual)
+        input_data = ''
+        actual = GCPUtils.get_entity_name_from_resource_name(input_data)
+        self.assertEqual(None, actual)
+        input_data = 666
+        actual = GCPUtils.get_entity_name_from_resource_name(input_data)
+        self.assertEqual(None, actual)
     
-        def test_youtube(self):
-        self._assert_restricted('07FYdnEawAQ', '07FYdnEawAQ.mp4', 10)
+        terminal_stdout_re = [
+        re.compile(r'[\r\n]?<.+>(?:\s*)$'),
+        re.compile(r'[\r\n]?\[.+\](?:\s*)$'),
+    ]
     
-    IGNORED_DIRS = [
-    '.git',
-    '.tox',
-]
-    
-    from .nuevo import NuevoBaseIE
-    
-    from .common import InfoExtractor
-from ..utils import (
-    parse_duration,
-    int_or_none,
-)
-    
-    
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '--data-dir',
-      type=str,
-      default='',
-      help='Directory to download and extract CIFAR-10 to.')
-    
-    from delf import datum_io
-    
-    import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.spatial import cKDTree
-from skimage.feature import plot_matches
-from skimage.measure import ransac
-from skimage.transform import AffineTransform
-import tensorflow as tf
-    
-        serialized = feature_io.SerializeToString(locations, scales, descriptors,
-                                              attention, orientations)
-    parsed_data = feature_io.ParseFromString(serialized)
-    
-        # Generate predictions from the model
-    expected = ['Setosa', 'Versicolor', 'Virginica']
-    predict_x = {
-        'SepalLength': [5.1, 5.9, 6.9],
-        'SepalWidth': [3.3, 3.0, 3.1],
-        'PetalLength': [1.7, 4.2, 5.4],
-        'PetalWidth': [0.5, 1.5, 2.1],
-    }
-    
-      @tf.test.mock.patch.dict(custom_estimator.__dict__,
-                           {'load_data': four_lines_data})
-  def test_custom_estimator(self):
-    custom_estimator.main([None, '--train_steps=1'])
-    
-      def testReverseSeq(self):
-    seq = self._buildDummySequence()
-    reverse_seq = data.build_reverse_sequence(seq)
-    for i, ts in enumerate(reversed(reverse_seq[:-1])):
-      self.assertEqual(ts.token, i)
-      self.assertEqual(ts.label, 0)
-      self.assertEqual(ts.weight, 0.0)
-    
-    # Rotten Tomatoes
-flags.DEFINE_string('rt_input_dir', '',
-                    'The Rotten Tomatoes dataset input directory.')
-    
-        if bidir and pretrain:
-      # Bidirectional pretraining
-      # Requires separate forward and reverse language model data.
-      forward_fname, reverse_fname = filenames
-      forward_batch = _read_and_batch(data_dir, forward_fname, state_name,
-                                      state_size, num_layers, unroll_steps,
-                                      batch_size)
-      state_name_rev = state_name + '_reverse'
-      reverse_batch = _read_and_batch(data_dir, reverse_fname, state_name_rev,
-                                      state_size, num_layers, unroll_steps,
-                                      batch_size)
-      forward_input = VatxtInput(
-          forward_batch,
-          state_name=state_name,
-          num_states=num_layers,
-          eos_id=eos_id)
-      reverse_input = VatxtInput(
-          reverse_batch,
-          state_name=state_name_rev,
-          num_states=num_layers,
-          eos_id=eos_id)
-      return forward_input, reverse_input
-    
-    
-collect_ignore = [
-    # deprecated or moved modules
-    'scrapy/conf.py',
-    'scrapy/stats.py',
-    'scrapy/project.py',
-    'scrapy/utils/decorator.py',
-    'scrapy/statscol.py',
-    'scrapy/squeue.py',
-    'scrapy/log.py',
-    'scrapy/dupefilter.py',
-    'scrapy/command.py',
-    'scrapy/linkextractor.py',
-    'scrapy/spider.py',
-    
-    
-class _BenchSpider(scrapy.Spider):
-    '''A spider that follows all links'''
-    name = 'follow'
-    total = 10000
-    show = 20
-    baseurl = 'http://localhost:8998'
-    link_extractor = LinkExtractor()
-    
-        def process_options(self, args, opts):
-        ScrapyCommand.process_options(self, args, opts)
-        try:
-            opts.spargs = arglist_to_dict(opts.spargs)
-        except ValueError:
-            raise UsageError('Invalid -a value, use -a NAME=VALUE', print_help=False)
-        if opts.output:
-            if opts.output == '-':
-                self.settings.set('FEED_URI', 'stdout:', priority='cmdline')
-            else:
-                self.settings.set('FEED_URI', opts.output, priority='cmdline')
-            feed_exporters = without_none_values(
-                self.settings.getwithbase('FEED_EXPORTERS'))
-            valid_output_formats = feed_exporters.keys()
-            if not opts.output_format:
-                opts.output_format = os.path.splitext(opts.output)[1].replace('.', '')
-            if opts.output_format not in valid_output_formats:
-                raise UsageError('Unrecognized output format '%s', set one'
-                                 ' using the '-t' switch or as a file extension'
-                                 ' from the supported list %s' % (opts.output_format,
-                                                                  tuple(valid_output_formats)))
-            self.settings.set('FEED_FORMAT', opts.output_format, priority='cmdline')
-    
-        for url in urls:
-        if not url.startswith('http'):
-            print('markdown file name: ' + url)
-            continue
-        if check_live_url(url):
-            print(url)
-        else:
-            print(url, file=sys.stderr)
-    
-            pieces = []
-        for fragment in pattern.split('('):
-            if ')' in fragment:
-                paren_loc = fragment.index(')')
-                if paren_loc >= 0:
-                    pieces.append('%s' + fragment[paren_loc + 1:])
-            else:
-                try:
-                    unescaped_fragment = re_unescape(fragment)
-                except ValueError:
-                    # If we can't unescape part of it, we can't
-                    # reverse this url.
-                    return (None, None)
-                pieces.append(unescaped_fragment)
-    
-        @classmethod
-    def _restore_configuration(cls, saved):
-        # type: (Tuple[type, Dict[str, Any]]) -> None
-        base = cls.configurable_base()
-        base.__impl_class = saved[0]
-        base.__impl_kwargs = saved[1]
-    
-    
-class WSGIContainerTest(AsyncHTTPTestCase):
-    def wsgi_app(self, environ, start_response):
-        status = '200 OK'
-        response_headers = [('Content-Type', 'text/plain')]
-        start_response(status, response_headers)
-        return [b'Hello world!']
-    
-    
-class Application(tornado.web.Application):
-    def __init__(self):
-        handlers = [
-            (r'/', MainHandler),
-            (r'/auth/login', AuthLoginHandler),
-            (r'/auth/logout', AuthLogoutHandler),
-        ]
-        settings = dict(
-            cookie_secret='__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__',
-            login_url='/auth/login',
-            template_path=os.path.join(os.path.dirname(__file__), 'templates'),
-            static_path=os.path.join(os.path.dirname(__file__), 'static'),
-            xsrf_cookies=True,
-            facebook_api_key=options.facebook_api_key,
-            facebook_secret=options.facebook_secret,
-            ui_modules={'Post': PostModule},
-            debug=True,
-            autoescape=None,
-        )
-        tornado.web.Application.__init__(self, handlers, **settings)
-    
-        def new_future_import(self, old):
-        new = FromImport('__future__',
-                         [Name('absolute_import', prefix=' '), Comma(),
-                          Name('division', prefix=' '), Comma(),
-                          Name('print_function', prefix=' ')])
-        if old is not None:
-            new.prefix = old.prefix
-        return new
+        def deploy_cert(self, domain, cert_path, key_path, chain_path=None,
+                    fullchain_path=None):
+        '''Installs cert'''
+        cert_path, key_path, chain_path = self.copy_certs_and_keys(
+            cert_path, key_path, chain_path)
+        self._configurator.deploy_cert(
+            domain, cert_path, key_path, chain_path, fullchain_path)
