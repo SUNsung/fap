d36fa1fdea4bdc768a2680b8dc15b6068aa3c6ab
SUNsung <weatherius@gmail.com>
2018-04-05 08:40:54 +0300

    Rewrite [02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp] File in: 2018-04-05 08:40:54

--- cccccc/02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp
+++ cccccc/02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp
@@ -1,29 +1,234 @@
 
         
-        #endif  // TESSERACT_CCMAIN_PARAGRAPHS_H_
+        
+    {
+    {bool RunQuit(const grpc::string& credential_type);
+}  // namespace testing
+}  // namespace grpc
+    
+    
+    {  return 0;
+}
 
     
-    namespace tesseract  {
-    }
+    #include 'test/core/util/test_config.h'
+#include 'test/cpp/qps/benchmark_config.h'
+#include 'test/cpp/qps/driver.h'
+#include 'test/cpp/qps/report.h'
+#include 'test/cpp/qps/server.h'
+#include 'test/cpp/util/test_config.h'
+#include 'test/cpp/util/test_credentials_provider.h'
     
-    // Returns true if there were enough points at the last call to Fit or
-// ConstrainedFit for the fitted points to be used on a badly fitted line.
-bool DetLineFit::SufficientPointsForIndependentFit() const {
-  return distances_.size() >= kMinPointsForErrorCount;
+    #include <grpc/grpc_security.h>
+    
+    grpc::string DescribeMethod(const grpc::protobuf::MethodDescriptor* method) {
+  std::stringstream result;
+  result << '  rpc ' << method->name()
+         << (method->client_streaming() ? '(stream ' : '(')
+         << method->input_type()->full_name() << ') returns '
+         << (method->server_streaming() ? '(stream ' : '(')
+         << method->output_type()->full_name() << ') {}\n';
+  if (method->options().deprecated()) {
+    result << ' DEPRECATED';
+  }
+  return result.str();
 }
     
-    #ifndef TESSERACT_CCUTIL_DOUBLEPTR_H_
-#define TESSERACT_CCUTIL_DOUBLEPTR_H_
     
-    // Vector of IntKDPair.
-class KDVector : public GenericVector<IntKDPair> {
-  // TODO(rays) Add some code to manipulate a KDVector. For now there
-  // is nothing and this class is effectively a specialization typedef.
+    {  gpr_subprocess* const subprocess_;
 };
     
-    /**----------------------------------------------------------------------------
-          Include Files and Type Defines
-----------------------------------------------------------------------------**/
-#include 'host.h'
-#include <stdio.h>
-#include <math.h>
+            template <typename T, typename std::enable_if<std::is_same<T, int>::value>::type* = nullptr>
+        T& Value()
+        {
+            VerifyType<T>();
+            return m_data.m_int;
+        }
+    
+        // don't allow cross network child copy unless caller explicitly handles children fixup
+    if ((flags & CopyNodeFlags::copyNodeInputLinks) &&
+        this != &fromNet && !(flags & CopyNodeFlags::copyNodeAcrossNetworks))
+    {
+        LogicError('CopyNode: Copying node children across network is invalid.');
+    }
+    
+        bnNodes = m_net->SortByGlobalEvalOrder(bnNodes);
+    for (auto& node : bnNodes)
+    {
+        let bnNode = static_pointer_cast<BatchNormalizationNode<ElemType>>(node);
+        size_t actualMBSize = 0;
+    }
+    
+    #include 'stdafx.h'
+#include 'Basics.h'
+#include 'Actions.h'
+#include 'ComputationNetwork.h'
+#include 'ComputationNode.h'
+#include 'DataReader.h'
+#include 'DataWriter.h'
+#include 'SimpleNetworkBuilder.h'
+#include 'Config.h'
+#include 'ScriptableObjects.h'
+    
+    // ---------------------------------------------------------------------------
+// ConfigException -- all errors from processing the config files are reported as ConfigException
+// ---------------------------------------------------------------------------
+    
+    // ReadConfigFile - read a configuration file, and return all lines, stripped of comments, concatenated by newlines, as one long string (no other processing, expansion etc.)
+// filePath - the path to the config file to read
+// returns: a string with the concatentated file contents
+std::string ConfigParser::ReadConfigFile(const std::wstring& filePath)
+{
+    File file(filePath, fileOptionsRead);
+    }
+    
+    public:
+    // DataWriter Constructor
+    // config - [in] configuration parameters for the datareader
+    template <class ConfigRecordType>
+    DataWriter(const ConfigRecordType& config);
+    // constructor from Scripting
+    DataWriter(const ScriptableObjects::IConfigRecordPtr configp)
+        : DataWriter(*configp)
+    {
+    }
+    virtual ~DataWriter();
+    
+    #include 'jsapi.h'
+#include 'jsfriendapi.h'
+    
+    
+    
+    
+    
+    	void Step(Settings* settings)
+	{
+		// Drive the kinematic body.
+		if (m_platform->GetType() == b2_kinematicBody)
+		{
+			b2Vec2 p = m_platform->GetTransform().p;
+			b2Vec2 v = m_platform->GetLinearVelocity();
+    }
+    }
+    
+    		b2BodyDef bd;
+		bd.type = b2_dynamicBody;
+		bd.position = body1->GetPosition();
+		bd.angle = body1->GetAngle();
+    
+    	static Test* Create()
+	{
+		return new Car;
+	}
+    
+    ExternalFunctions::ExternalFunctions() {
+#define EXT_FUNC(NAME, RETURN_TYPE, FUNC_SIG, WARN)                            \
+  this->NAME = GetFnPtr<decltype(ExternalFunctions::NAME)>(#NAME, WARN)
+    }
+    
+    
+    {} // namespace fuzzer
+    
+    // Somewhere in the past we have observed a comparison instructions
+// with arguments Arg1 Arg2. This function tries to guess a dictionary
+// entry that will satisfy that comparison.
+// It first tries to find one of the arguments (possibly swapped) in the
+// input and if it succeeds it creates a DE with a position hint.
+// Otherwise it creates a DE with one of the arguments w/o a position hint.
+template <class T>
+DictionaryEntry MutationDispatcher::MakeDictionaryEntryFromCMP(
+    T Arg1, T Arg2, const uint8_t *Data, size_t Size) {
+  ScopedDoingMyOwnMemmem scoped_doing_my_own_memmem;
+  bool HandleFirst = Rand.RandBool();
+  T ExistingBytes, DesiredBytes;
+  Word W;
+  const uint8_t *End = Data + Size;
+  for (int Arg = 0; Arg < 2; Arg++) {
+    ExistingBytes = HandleFirst ? Arg1 : Arg2;
+    DesiredBytes = HandleFirst ? Arg2 : Arg1;
+    DesiredBytes += Rand(-1, 1);
+    if (Rand.RandBool()) ExistingBytes = Bswap(ExistingBytes);
+    if (Rand.RandBool()) DesiredBytes = Bswap(DesiredBytes);
+    HandleFirst = !HandleFirst;
+    W.Set(reinterpret_cast<uint8_t*>(&DesiredBytes), sizeof(T));
+    const size_t kMaxNumPositions = 8;
+    size_t Positions[kMaxNumPositions];
+    size_t NumPositions = 0;
+    for (const uint8_t *Cur = Data;
+         Cur < End && NumPositions < kMaxNumPositions; Cur++) {
+      Cur = (uint8_t *)SearchMemory(Cur, End - Cur, &ExistingBytes, sizeof(T));
+      if (!Cur) break;
+      Positions[NumPositions++] = Cur - Data;
+    }
+    if (!NumPositions) break;
+    return DictionaryEntry(W, Positions[Rand(NumPositions)]);
+  }
+  DictionaryEntry DE(W);
+  return DE;
+}
+    
+    struct FuzzingOptions {
+  int Verbosity = 1;
+  size_t MaxLen = 0;
+  int UnitTimeoutSec = 300;
+  int TimeoutExitCode = 77;
+  int ErrorExitCode = 77;
+  int MaxTotalTimeSec = 0;
+  int RssLimitMb = 0;
+  bool DoCrossOver = true;
+  int MutateDepth = 5;
+  bool UseCounters = false;
+  bool UseIndirCalls = true;
+  bool UseMemcmp = true;
+  bool UseMemmem = true;
+  bool UseCmp = false;
+  bool UseValueProfile = false;
+  bool Shrink = false;
+  int ReloadIntervalSec = 1;
+  bool ShuffleAtStartUp = true;
+  bool PreferSmall = true;
+  size_t MaxNumberOfRuns = -1L;
+  int ReportSlowUnits = 10;
+  bool OnlyASCII = false;
+  std::string OutputCorpus;
+  std::string ArtifactPrefix = './';
+  std::string ExactArtifactPath;
+  std::string ExitOnSrcPos;
+  std::string ExitOnItem;
+  bool SaveArtifacts = true;
+  bool PrintNEW = true; // Print a status line when new units are found;
+  bool OutputCSV = false;
+  bool PrintNewCovPcs = false;
+  bool PrintFinalStats = false;
+  bool PrintCorpusStats = false;
+  bool PrintCoverage = false;
+  bool DumpCoverage = false;
+  bool DetectLeaks = true;
+  int  TraceMalloc = 0;
+  bool HandleAbrt = false;
+  bool HandleBus = false;
+  bool HandleFpe = false;
+  bool HandleIll = false;
+  bool HandleInt = false;
+  bool HandleSegv = false;
+  bool HandleTerm = false;
+};
+    
+    void TracePC::PrintModuleInfo() {
+  Printf('INFO: Loaded %zd modules (%zd guards): ', NumModules, NumGuards);
+  for (size_t i = 0; i < NumModules; i++)
+    Printf('[%p, %p), ', Modules[i].Start, Modules[i].Stop);
+  Printf('\n');
+}
+    
+      // Make sure the child process uses the default handlers for the
+  // following signals rather than inheriting what the parent has.
+  sigset_t DefaultSigSet;
+  (void)sigemptyset(&DefaultSigSet);
+  (void)sigaddset(&DefaultSigSet, SIGQUIT);
+  (void)sigaddset(&DefaultSigSet, SIGINT);
+  (void)posix_spawnattr_setsigdefault(&SpawnAttributes, &DefaultSigSet);
+  // Make sure the child process doesn't block SIGCHLD
+  (void)posix_spawnattr_setsigmask(&SpawnAttributes, &OldBlockedSignalsSet);
+  short SpawnFlags = POSIX_SPAWN_SETSIGDEF | POSIX_SPAWN_SETSIGMASK;
+  (void)posix_spawnattr_setflags(&SpawnAttributes, SpawnFlags);
