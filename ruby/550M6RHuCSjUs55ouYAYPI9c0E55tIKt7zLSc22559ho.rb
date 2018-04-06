5975d02873c215976d10833fe4c56e885007f56e
SUNsung <weatherius@gmail.com>
2018-04-06 21:31:09 +0300

    Rewrite [550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb] File in: 2018-04-06 21:31:08

--- ruby/550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb
+++ ruby/550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb
@@ -1,49 +1,40 @@
 
         
-        module Docs
-  class PageDb
-    attr_reader :pages
-    
-        def merge(hash)
-      return super unless hash.is_a? Hash
-      dup.merge!(hash)
-    end
-    
-          IGNORE_ENTRIES = %w(
-        Overview
-        Introduction
-        Usage
-        Methods
-        Options
-      )
-    end
-  end
-end
-
-    
-    module Capistrano
-  module TaskEnhancements
-    def before(task, prerequisite, *args, &block)
-      prerequisite = Rake::Task.define_task(prerequisite, *args, &block) if block_given?
-      Rake::Task[task].enhance [prerequisite]
-    end
-    
-    module Jekyll
-    
-        def render(context)
-      output = super
-      types = {
-        '.mp4' => 'type='video/mp4; codecs=\'avc1.42E01E, mp4a.40.2\''',
-        '.ogv' => 'type='video/ogg; codecs=theora, vorbis'',
-        '.webm' => 'type='video/webm; codecs=vp8, vorbis''
-      }
-      if @videos.size > 0
-        video =  '<video #{sizes} preload='metadata' controls #{poster}>'
-        @videos.each do |v|
-          video << '<source src='#{v}' #{types[File.extname(v)]}>'
-        end
-        video += '</video>'
+            def cropping dst, ratio, scale
+      if ratio.horizontal? || ratio.square?
+        '%dx%d+%d+%d' % [ dst.width, dst.height, 0, (self.height * scale - dst.height) / 2 ]
       else
-        'Error processing input, expected syntax: {% video url/to/video [url/to/video] [url/to/video] [width height] [url/to/poster] %}'
+        '%dx%d+%d+%d' % [ dst.width, dst.height, (self.width * scale - dst.width) / 2, 0 ]
       end
     end
+    
+            protected
+    
+            def failure_message
+          'Attachment #{@attachment_name} should be required'
+        end
+    
+            def lower_than_low?
+          @low.nil? || !passes_validation_with_size(@low - 1)
+        end
+    
+    module Paperclip
+  class << self
+    attr_writer :registered_attachments_styles_path
+    def registered_attachments_styles_path
+      @registered_attachments_styles_path ||= Rails.root.join('public/system/paperclip_attachments.yml').to_s
+    end
+  end
+    
+          if app.config.respond_to?(:paperclip_defaults)
+        Paperclip::Attachment.default_options.merge!(app.config.paperclip_defaults)
+      end
+    end
+    
+          def drop_attached_file(*args)
+        ActiveSupport::Deprecation.warn 'Method `drop_attached_file` in the migration has been deprecated and will be replaced by `remove_attachment`.'
+        remove_attachment(*args)
+      end
+    end
+    
+      TYPES = [ 'input', 'filter', 'output', 'codec' ]
