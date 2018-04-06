1a4a86c537f56280e3103de4f7eaf11714de5e66
SUNsung <weatherius@gmail.com>
2018-04-06 20:33:37 +0300

    Rewrite [21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp] File in: 2018-04-06 20:33:36

--- cccccc/21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp
+++ cccccc/21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp
@@ -1,195 +1,34 @@
 
         
-        #include <string>
-    
-    class ObjectManager;
-    
-    void Dispatcher::documentCallback(const char* ev, blink::WebLocalFrame* frame) {
-  v8::Isolate* isolate = v8::Isolate::GetCurrent();
-  blink::WebView* web_view = render_view()->GetWebView();
-  v8::HandleScope scope(isolate);
-    }
-    
-    #include 'base/basictypes.h'
-#include 'content/public/renderer/render_view_observer.h'
-#include 'third_party/WebKit/public/web/WebNavigationPolicy.h'
-#include <v8.h>
-#include <string>
-    
-    // Similar to node's `require` function, save functions in `exports`.
-void RequireFromResource(v8::Handle<v8::Object> root,
-                         v8::Handle<v8::Object> gui,
-                         v8::Handle<v8::Object> window,
-                         v8::Handle<v8::String> name,
-                         int resource_id) {
-  v8::Isolate* isolate = v8::Isolate::GetCurrent();
-  v8::HandleScope handle_scope(isolate);
-    }
-    
-      // v8::Extension implementation.
-  v8::Handle<v8::FunctionTemplate>
-      GetNativeFunctionTemplate(
-                        v8::Isolate* isolate,
-                        v8::Handle<v8::String> name) override;
-    
-    using content::WebContents;
-using content::ShellBrowserContext;
-using content::Shell;
+        extern JSClass  *jsb_cocosbuilder_CCBReader_class;
+extern JSObject *jsb_cocosbuilder_CCBReader_prototype;
     
     
-#include 'content/nw/src/api/event/event.h'
-#include 'base/values.h'
-#include 'content/nw/src/api/dispatcher_host.h'
-#include 'ui/gfx/screen.h'
+    
+    #if COCOS2D_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
     
     
-#include 'base/basictypes.h'
     
-    void Menu::Destroy() {
+        return 0;
 }
-    
-        bool Write(ClipboardData& data) {
-      switch(data.type) {
-        case TYPE_TEXT:
-        return WriteText(data);
-        break;
-        case TYPE_HTML:
-        return WriteHTML(data);
-        break;
-        case TYPE_RTF:
-        return WriteRTF(data);
-        break;
-        case TYPE_PNG:
-        case TYPE_JPEG:
-        return WriteImage(data);
-        break;
-        case TYPE_NONE:
-        NOTREACHED();
-        return false;
-      }
-      NOTREACHED();
-      return false;
-    }
-    
-        TableBuilder* builder = new TableBuilder(options, file);
-    meta->smallest.DecodeFrom(iter->key());
-    for (; iter->Valid(); iter->Next()) {
-      Slice key = iter->key();
-      meta->largest.DecodeFrom(key);
-      builder->Add(key, iter->value());
-    }
-    
-    class Env;
-class Iterator;
-class TableCache;
-class VersionEdit;
-    
-    struct leveldb_comparator_t : public Comparator {
-  void* state_;
-  void (*destructor_)(void*);
-  int (*compare_)(
-      void*,
-      const char* a, size_t alen,
-      const char* b, size_t blen);
-  const char* (*name_)(void*);
-    }
-    
-    // Return a new iterator that converts internal keys (yielded by
-// '*internal_iter') that were live at the specified 'sequence' number
-// into appropriate user keys.
-extern Iterator* NewDBIterator(
-    DBImpl* db,
-    const Comparator* user_key_comparator,
-    Iterator* internal_iter,
-    SequenceNumber sequence,
-    uint32_t seed);
-    
-    static std::string Shorten(const std::string& s, const std::string& l) {
-  std::string result = s;
-  InternalKeyComparator(BytewiseComparator()).FindShortestSeparator(&result, l);
-  return result;
-}
-    
-      if (msg == NULL && !input.empty()) {
-    msg = 'invalid tag';
-  }
-    
-      ASSERT_TRUE(! Overlaps('a', 'b'));
-  ASSERT_TRUE(! Overlaps('z1', 'z2'));
-  ASSERT_TRUE(Overlaps('a', 'p'));
-  ASSERT_TRUE(Overlaps('a', 'q'));
-  ASSERT_TRUE(Overlaps('a', 'z'));
-  ASSERT_TRUE(Overlaps('p', 'p1'));
-  ASSERT_TRUE(Overlaps('p', 'q'));
-  ASSERT_TRUE(Overlaps('p', 'z'));
-  ASSERT_TRUE(Overlaps('p1', 'p2'));
-  ASSERT_TRUE(Overlaps('p1', 'z'));
-  ASSERT_TRUE(Overlaps('q', 'q'));
-  ASSERT_TRUE(Overlaps('q', 'q1'));
-    
-    WriteBatch::~WriteBatch() { }
-    
-    class Env;
-    
-    // Return a builtin comparator that uses lexicographic byte-wise
-// ordering.  The result remains the property of this module and
-// must not be deleted.
-extern const Comparator* BytewiseComparator();
-    
-      // Return the name of this policy.  Note that if the filter encoding
-  // changes in an incompatible way, the name returned by this method
-  // must be changed.  Otherwise, old incompatible filters may be
-  // passed to methods of this type.
-  virtual const char* Name() const = 0;
-    
-    /*static*/ struct DataReaderHelpers
+int lua_cocos2dx_physics_PhysicsBody_setMoment(lua_State* tolua_S)
 {
-    template <class ElemType>
-    static void NotifyChangedNodes(ComputationNetworkPtr net, StreamMinibatchInputs& inputMatrices)
-    {
-        // reader will have resized input node's m_value directly. Nodes must be notified to do necessary internal state updates from that.
-        // TODO: This is a stopgap. SGD will at some point change from sets of matrices to sets of nodes. Then this will become much simpler.
-        std::set<MatrixBasePtr> matrices;
-        for (const auto& iter : inputMatrices)
-            matrices.insert(iter.second.matrix);
-        for (auto& node : net->FeatureNodes())
-            if (matrices.find(node->As<ComputationNode<ElemType>>()->ValuePtr()) != matrices.end())
-                node->NotifyFunctionValuesMBSizeModified();
-        for (auto& node : net->LabelNodes())
-            if (matrices.find(node->As<ComputationNode<ElemType>>()->ValuePtr()) != matrices.end())
-                node->NotifyFunctionValuesMBSizeModified();
+    int argc = 0;
+    cocos2d::PhysicsBody* cobj = nullptr;
+    bool ok  = true;
+    
+    #endif
+
+    
+    				body->CreateFixture(&fd);
+    
+    	BulletTest()
+	{
+		{
+			b2BodyDef bd;
+			bd.position.Set(0.0f, 0.0f);
+			b2Body* body = m_world->CreateBody(&bd);
     }
     }
-    
-    
-    {
-    {        // N.B. - this is the magical call, the reason for the function
-        // dimensions would be outputRank * numSamples * minibatch * time.
-        // This call establishes outputRank * numSamples, the rest will be filled in
-        // dynamically though the MBLayout.
-        SetDims(TensorShape(dimsC), HasMBLayout());
-    }
-};
-    
-    // understand and execute from the syntactic expression tree
-ConfigValuePtr Evaluate(ExpressionPtr);                               // evaluate the expression tree
-void Do(ExpressionPtr e);                                             // evaluate e.do
-shared_ptr<Object> EvaluateField(ExpressionPtr e, const wstring& id); // for experimental CNTK integration
-    
-    
-    {  // For fragments
-  kFirstType = 2,
-  kMiddleType = 3,
-  kLastType = 4
-};
-static const int kMaxRecordType = kLastType;
-    
-      static Slice Contents(const WriteBatch* batch) {
-    return Slice(batch->rep_);
-  }
-    
-    #include <algorithm>
-#include <assert.h>
-#include 'leveldb/comparator.h'
-#include 'leveldb/table_builder.h'
-#include 'util/coding.h'
