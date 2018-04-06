96b7885b8850b45ffa7aeaba4d39c506efb998bc
SUNsung <weatherius@gmail.com>
2018-04-06 07:27:59 +0300

    Rewrite [46f99pPmYsVu6469Z4R4DGyJPU46lKLoiJZ1RE463Tu.py] File in: 2018-04-06 07:27:58

--- pyton/46f99pPmYsVu6469Z4R4DGyJPU46lKLoiJZ1RE463Tu.py
+++ pyton/46f99pPmYsVu6469Z4R4DGyJPU46lKLoiJZ1RE463Tu.py
@@ -1,64 +1,77 @@
 
         
-        
-def side_effect(old_cmd, command):
-    with tarfile.TarFile(_tar_file(old_cmd.script_parts)[0]) as archive:
-        for file in archive.getnames():
-            try:
-                os.remove(file)
-            except OSError:
-                # does not try to remove directories as we cannot know if they
-                # already existed before
-                pass
-
+          train_path = os.path.join(data_path, 'ptb.train.txt')
+  valid_path = os.path.join(data_path, 'ptb.valid.txt')
+  test_path = os.path.join(data_path, 'ptb.test.txt')
     
-            (foo, 2), p1
-        (bar, 3), p1
-        (foo, 3), p2
-        (bar, 10), p3
-        (foo, 1), p4
+        # Compute evaluation metrics.
+    accuracy = tf.metrics.accuracy(labels=labels,
+                                   predictions=predicted_classes,
+                                   name='acc_op')
+    metrics = {'accuracy': accuracy}
+    tf.summary.scalar('accuracy', accuracy[1])
     
-        def _dispatch_call(self, call, employees):
-        for employee in employees:
-            if employee.call is None:
-                employee.take_call(call)
-                return employee
-        return None
+      for token in document_generators.tokens(doc):
+    if doc.add_tokens or token in vocab_freqs:
+      vocab_freqs[token] += 1
+    if token not in doc_seen:
+      doc_counts[token] += 1
+      doc_seen.add(token)
     
-        def extract_max_priority_page(self):
-        '''Return the highest priority link in `links_to_crawl`.'''
-        pass
+        # Loads the final mapping of integer node ID to human-readable string
+    node_id_to_name = {}
+    for key, val in node_id_to_uid.items():
+      if val not in uid_to_human:
+        tf.logging.fatal('Failed to locate: %s', val)
+      name = uid_to_human[val]
+      node_id_to_name[key] = name
     
-    +----------+     +----------+       +--------+     +-----------+    +---------+
-|          |  => | Work Ids |    => |        |  => | Call Q    | => |         |
-|          |     +----------+       |        |     +-----------+    |         |
-|          |     | ...      |       |        |     | ...       |    |         |
-|          |     | 6        |       |        |     | 5, call() |    |         |
-|          |     | 7        |       |        |     | ...       |    |         |
-| Process  |     | ...      |       | Local  |     +-----------+    | Process |
-|  Pool    |     +----------+       | Worker |                      |  #1..n  |
-| Executor |                        | Thread |                      |         |
-|          |     +----------- +     |        |     +-----------+    |         |
-|          | <=> | Work Items | <=> |        | <=  | Result Q  | <= |         |
-|          |     +------------+     |        |     +-----------+    |         |
-|          |     | 6: call()  |     |        |     | ...       |    |         |
-|          |     |    future  |     |        |     | 4, result |    |         |
-|          |     | ...        |     |        |     | 3, except |    |         |
-+----------+     +------------+     +--------+     +-----------+    +---------+
+        # By using the 'with' statement we are sure the session is closed, thus we
+    # avoid leaving sockets open which can trigger a ResourceWarning in some
+    # cases, and look like a memory leak in others.
+    with sessions.Session() as session:
+        return session.request(method=method, url=url, **kwargs)
     
-    # If true, sectionauthor and moduleauthor directives will be shown in the
-# output. They are ignored by default.
-#show_authors = False
+        def prepare(self):
+        '''Constructs a :class:`PreparedRequest <PreparedRequest>` for transmission and returns it.'''
+        p = PreparedRequest()
+        p.prepare(
+            method=self.method,
+            url=self.url,
+            headers=self.headers,
+            files=self.files,
+            data=self.data,
+            json=self.json,
+            params=self.params,
+            auth=self.auth,
+            cookies=self.cookies,
+            hooks=self.hooks,
+        )
+        return p
+    
+        def test_text_response(self):
+        '''the text_response_server sends the given text'''
+        server = Server.text_response_server(
+            'HTTP/1.1 200 OK\r\n' +
+            'Content-Length: 6\r\n' +
+            '\r\nroflol'
+        )
+    
+        return authstr
+    
+        def get_header(self, name, default=None):
+        return self._r.headers.get(name, self._new_headers.get(name, default))
+    
+    elif is_py3:
+    from urllib.parse import urlparse, urlunparse, urljoin, urlsplit, urlencode, quote, unquote, quote_plus, unquote_plus, urldefrag
+    from urllib.request import parse_http_list, getproxies, proxy_bypass, proxy_bypass_environment, getproxies_environment
+    from http import cookiejar as cookielib
+    from http.cookies import Morsel
+    from io import StringIO
+    from collections import OrderedDict
     
     
-def SendEventNotificationAsync( event_name,
-                                buffer_number = None,
-                                extra_data = None ):
-  event = EventNotification( event_name, buffer_number, extra_data )
-  event.Start()
-
-    
-      if not p.isfile( build_file ):
-    sys.exit(
-      'File {0} does not exist; you probably forgot to run:\n'
-      '\tgit submodule update --init --recursive\n'.format( build_file ) )
+@pytest.mark.skipif(sys.version_info < (2,7), reason='Only run on Python 2.7+')
+def test_system_ssl():
+    '''Verify we're actually setting system_ssl when it should be available.'''
+    assert info()['system_ssl']['version'] != ''
