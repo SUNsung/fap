3b0651b3d4b8d48f8bf4644d3621595398ef6b6b
SUNsung <weatherius@gmail.com>
2018-04-08 00:40:05 +0300

    Rewrite [062ntJTQT1O6A065RJmINIknuE06hdngPWcSu6606vEl.py] File in: 2018-04-08 00:40:04

--- pyton/062ntJTQT1O6A065RJmINIknuE06hdngPWcSu6606vEl.py
+++ pyton/062ntJTQT1O6A065RJmINIknuE06hdngPWcSu6606vEl.py
@@ -1,25 +1,59 @@
 
         
-            By default this will get the strings from the blns.txt file
+                try:
+            expected = int_to_float(n)
+        except OverflowError:
+            expected = 'overflow'
     
-        @mock.patch('certbot.notify.smtplib.LMTP')
-    @mock.patch('certbot.notify.subprocess.Popen')
-    def test_everything_fails(self, mock_popen, mock_lmtp):
-        from certbot.notify import notify
-        lmtp_obj = mock.MagicMock()
-        mock_lmtp.return_value = lmtp_obj
-        lmtp_obj.sendmail.side_effect = socket.error(17)
-        proc = mock.MagicMock()
-        mock_popen.return_value = proc
-        proc.communicate.side_effect = OSError('What we have here is a '
-                                               'failure to communicate.')
-        self.assertFalse(notify('Goose', 'auntrhody@example.com',
-                                'The old grey goose is dead.'))
-        self.assertEqual(lmtp_obj.sendmail.call_count, 1)
-        self.assertEqual(proc.communicate.call_count, 1)
+        def path_ok(self):
+        'Simple validity check for menu file path'
+        path = self.path.get().strip()
+        if not path: #no path specified
+            self.showerror('no help file path specified.', self.path_error)
+            return None
+        elif not path.startswith(('www.', 'http')):
+            if path[:5] == 'file:':
+                path = path[5:]
+            if not os.path.exists(path):
+                self.showerror('help file path does not exist.',
+                               self.path_error)
+                return None
+            if platform == 'darwin':  # for Mac Safari
+                path =  'file://' + path
+        return path
     
+            elements = 'ABCDEFGHI'
+        for i in range(-len(elements), len(elements)):
+            d = deque(elements, maxlen=len(elements)+1)
+            d.insert(i, 'Z')
+            if i >= 0:
+                self.assertEqual(d[i], 'Z')
+            else:
+                self.assertEqual(d[i-1], 'Z')
     
-@zope.interface.implementer(interfaces.IInstaller)
-@zope.interface.provider(interfaces.IPluginFactory)
-class Installer(common.Plugin):
-    '''Example Installer.'''
+            self.assertRaises(TypeError, Array.from_buffer_copy, b'123')
+        self.assertRaises(TypeError, Structure.from_buffer_copy, b'123')
+        self.assertRaises(TypeError, Union.from_buffer_copy, b'123')
+        self.assertRaises(TypeError, _CFuncPtr.from_buffer_copy, b'123')
+        self.assertRaises(TypeError, _Pointer.from_buffer_copy, b'123')
+        self.assertRaises(TypeError, _SimpleCData.from_buffer_copy, b'123')
+    
+        @classmethod
+    def setUpClass(cls):
+        cls.s = Subject()
+        cls.dec_obs = DecimalViewer()
+        cls.hex_obs = HexViewer()
+    
+        def do_action(self):
+        print(self.name, self.action.name, end=' ')
+        return self.action
+    
+    *What does this example do?
+In this example queue.Queue is used to create the pool (wrapped in a
+custom ObjectPool object to use with the with statement), and it is
+populated with strings.
+As we can see, the first string object put in 'yam' is USED by the
+with statement. But because it is released back into the pool
+afterwards it is reused by the explicit call to sample_queue.get().
+Same thing happens with 'sam', when the ObjectPool created insided the
+function is deleted (by the GC) and the object is returned.
