0c7095fae6977be32e188ae07370b383dedb563b
SUNsung <weatherius@gmail.com>
2018-04-07 13:49:44 +0300

    Rewrite [21W2opHb8j9nc21BW1kdzP3EsU21DPttMJECOBk21iml.rb] File in: 2018-04-07 13:49:44

--- ruby/21W2opHb8j9nc21BW1kdzP3EsU21DPttMJECOBk21iml.rb
+++ ruby/21W2opHb8j9nc21BW1kdzP3EsU21DPttMJECOBk21iml.rb
@@ -1,76 +1,81 @@
 
         
-          end
-    
-      # any user that is either a moderator or an admin
-  def staff?
-    admin || moderator
-  end
-    
-            css('code').each do |node|
-          node.inner_html = node.inner_html.squish
+                if b_length > a_length
+          (b_length - a_length).times { a_split.insert(-2, 0) }
+        elsif a_length > b_length
+          (a_length - b_length).times { b_split.insert(-2, 0) }
         end
     
-            # This returns all the registered host capabilities.
-        #
-        # @return [Hash]
-        def host_capabilities
-          results = Hash.new { |h, k| h[k] = Registry.new }
+            css('[id]').each do |node|
+          # Module
+          if node.name == 'h2'
+            type = node.content.remove 'Backbone.'
+            if type.capitalize! # sync, history
+              entries << [node.content, node['id'], type]
+            end
+            next
+          end
     
-            # Get the proper capability host to check
-        cap_host = nil
-        if type == :host
-          cap_host = @env.host
-        else
-          with_target_vms([]) do |vm|
-            cap_host = case type
-                       when :provider
-                         vm.provider
-                       when :guest
-                         vm.guest
-                       else
-                         raise Vagrant::Errors::CLIInvalidUsage,
-                           help: opts.help.chomp
-                       end
+              node.css('.method').each do |n|
+            next unless n.at_css('dt[id]')
+            name = n.at_css('.descname').content
+            name = '#{class_name}::#{name}()'
+            id = n.at_css('dt[id]')['id']
+            entries << [name, id]
           end
         end
     
-      def as_json(options={})
-    {
-      poll_id:             id,
-      post_id:             status_message.id,
-      question:            question,
-      poll_answers:        poll_answers,
-      participation_count: participation_count
-    }
-  end
-    
-          private
-    
-            private
-    
-    module RuboCop
-  module Cop
-    class VariableForce
-      # A Variable represents existence of a local variable.
-      # This holds a variable declaration node,
-      # and some states of the variable.
-      class Variable
-        VARIABLE_DECLARATION_TYPES =
-          (VARIABLE_ASSIGNMENT_TYPES + ARGUMENT_DECLARATION_TYPES).freeze
-    
-            MSG = 'Use only a single space inside array percent literal.'.freeze
-        MULTIPLE_SPACES_BETWEEN_ITEMS_REGEX =
-          /(?:[\S&&[^\\]](?:\\ )*)( {2,})(?=\S)/
-    
-              add_offense(node)
-        end
-    
-            private
-    
-          def extract_metrics(path, *keys)
-        snapshot.metric_store.extract_metrics(path, *keys)
+            doc
       end
     end
   end
 end
+
+    
+          def merge!(other)
+        other.each do |key, value|
+          self[convert_key(key)] = value
+        end
+        self
+      end
+    
+      def admin_inviter
+    inviter = InvitationCode.default_inviter_or(current_user)
+    email = params[:identifier]
+    user = User.find_by_email(email)
+    
+    Liquid::Template.register_tag('blockquote', Jekyll::Blockquote)
+
+    
+    def config_tag(config, key, tag=nil, classname=nil)
+  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
+  tag       ||= 'div'
+  classname ||= key.sub(/_/, '-').sub(/\./, '-')
+  output      = '<#{tag} class='#{classname}''
+    
+        def render(context)
+      if @img
+        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
+      else
+        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
+      end
+    end
+  end
+end
+    
+        def render(context)
+      code_dir = (context.registers[:site].config['code_dir'].sub(/^\//,'') || 'downloads/code')
+      code_path = (Pathname.new(context.registers[:site].source) + code_dir).expand_path
+      file = code_path + @file
+    
+      class RenderPartialTag < Liquid::Tag
+    include OctopressFilters
+    def initialize(tag_name, markup, tokens)
+      @file = nil
+      @raw = false
+      if markup =~ /^(\S+)\s?(\w+)?/
+        @file = $1.strip
+        @raw = $2 == 'raw'
+      end
+      super
+    end
