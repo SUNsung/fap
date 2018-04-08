037dc9eed43909e2065f9f76cf7a9e42f1533df7
SUNsung <weatherius@gmail.com>
2018-04-08 03:44:14 +0300

    Rewrite [44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp] File in: 2018-04-08 03:44:13

--- cccccc/44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp
+++ cccccc/44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp
@@ -1,81 +1,199 @@
 
         
-            // Operator signature declaration.
-    // It defines input formal parameter, output formal parameters and
-    // attributes.
-    // Once an operator signature created, it's 'Read-Only'.
-    class OpSignature
-    {
-    public:
+        SavePageHandler::SavePageHandler(content::WebContents* web_contents,
+                                 const SavePageCallback& callback)
+    : web_contents_(web_contents),
+      callback_(callback) {
+}
+    
+    namespace base {
+class FilePath;
+}
+    
+    
+    { private:
+  DISALLOW_COPY_AND_ASSIGN(AtomQuotaPermissionContext);
+};
+    
+      NativeWindowViews* window_;
+  gfx::AcceleratedWidget xid_;
+    
+    
+    {  /// Return a hash code of any components from these options that should
+  /// contribute to a Swift Bridging PCH hash.
+  llvm::hash_code getPCHHashComponents() const {
+    // Nothing here that contributes anything significant when emitting the PCH.
+    return llvm::hash_value(0);
+  }
+};
+    
+    #include 'swift/AST/ASTContext.h'
+#include 'swift/AST/GenericEnvironment.h'
+#include 'swift/AST/Module.h'
+#include 'swift/AST/ProtocolConformance.h'
+#include 'swift/AST/SubstitutionMap.h'
+#include 'swift/AST/Types.h'
+#include 'llvm/ADT/DenseMap.h'
+    
+    #endif
+
+    
+    #endif
+    
+      void WrapInputLayer(std::vector<cv::Mat>* input_channels);
+    
+    #include 'caffe/common.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
+    
+    
+    {  /**
+   * @brief Computes the error gradient w.r.t. the absolute value inputs.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *      respect to the outputs
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
+   *      with respect to computed outputs @f$ y @f$
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length 2)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$; Backward fills their diff with
+   *      gradients @f$
+   *        \frac{\partial E}{\partial x} =
+   *            \mathrm{sign}(x) \frac{\partial E}{\partial y}
+   *      @f$ if propagate_down[0]
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+};
+    
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
+#include 'caffe/util/im2col.hpp'
+    
+      /**
+   * @brief Computes the error gradient w.r.t. the reordered input.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient
+   *        with respect to the outputs
+   *   -# @f$ (M \times ...) @f$:
+   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
+   *      with respect to concatenated outputs @f$ y @f$
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length 2):
+   *   - @f$ \frac{\partial E}{\partial y} @f$ is de-indexed (summing where
+   *     required) back to the input x_1
+   *   - This layer cannot backprop to x_2, i.e. propagate_down[1] must be
+   *     false.
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
+    
+    #endif  // CAFFE_ELU_LAYER_HPP_
+
+    
+    /**
+ * @brief A layer for learning 'embeddings' of one-hot vector input.
+ *        Equivalent to an InnerProductLayer with one-hot vectors as input, but
+ *        for efficiency the input is the 'hot' index of each column itself.
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+template <typename Dtype>
+class EmbedLayer : public Layer<Dtype> {
+ public:
+  explicit EmbedLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
     }
     
-        // Taken from RS4
-    REGISTER_OPERATOR_SCHEMA(Hardmax)
-        .Description('Compute the hardmax normalized values for each layer in the batch '
-            'of the given input. The input is a 2-D tensor (Tensor<float>) of size '
-            '(batch_size x input_feature_dimensions). The output tensor has the same shape '
-            'and contains the softmax normalized values of the corresponding input. '
-            '\n'
-            'X does not need to explicitly be a 2D vector; rather, it will be coerced into '
-            'one. For an arbitrary n-dimensional tensor X in [a_0, a_1, ..., a_{k-1}, '
-            'a_k, ..., a_{n-1}] and k is the axis provided, then X will be coerced into a '
-            '2-dimensional tensor with dimensions [a_0 * ... * a_{k-1}, a_k * ... * a_{n-1}]. '
-            'For the default case where axis=1, this means the X tensor will be coerced into '
-            'a 2D tensor of dimensions [a_0, a_1 * ... * a_{n-1}], where a_0 is often the '
-            'batch size.  In this situation, we must have a_0 = N and a_1 * ... * a_{n-1} = D. '
-            'Each of these dimensions must be matched correctly, or else the operator will '
-            'throw errors.')
-        .Input('input', 'The input tensor that's coerced into a 2D matrix of size (NxD) as '
-            'described above.', 'T')
-        .Output('output', 'Output tensor of same shape and type as input X.', 'T')
-        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
-            'Constrain input and output types to float tensors.')
-        .Attr('axis', 'Default to 1; describes the axis of the inputs when coerced to 2D; '
-            'defaults to one because the 0th axis most likely describes the batch size.',
-            AttrType::AttributeProto_AttributeType_INT, int64_t(1));
-    
-        REGISTER_OPERATOR_SCHEMA(Dropout)
-        .Description('Dropout takes one input data (Tensor<float>) and produces two Tensor outputs, '
-            'output (Tensor<float>) and mask (Tensor<bool>). Depending on whether it is in '
-            'test mode or not, the output Y will either be a random dropout, or a simple '
-            'copy of the input. Note that our implementation of Dropout does scaling in '
-            'the training phase, so during testing nothing needs to be done.')
-        .Input('data', 'The input data as Tensor.', 'T')
-        .Output('output', 'The output.', 'T')
-        .Output('mask',
-            'The output mask. If is_test is nonzero, this output is not filled.', 'T')
-        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
-            'Constrain input and output types to float tensors.')
-        .Attr('ratio',
-            '(float, default 0.5) the ratio of random dropout',
-            AttrType::AttributeProto_AttributeType_FLOAT, float(0.5))
-        .Attr('is_test',
-            '(int, default 0) if nonzero, run dropout in test mode where '
-            'the output is simply Y = X.',
-            AttrType::AttributeProto_AttributeType_INT, int64_t(0));
-    
-        virtual void BackpropToNonLooping(size_t inputIndex) override
-    {
-        FrameRange fr(Input(0)->GetMBLayout());
-        if (inputIndex == 0)
-            LogicError('DummyCriterionNode: Gradients with respect to objective features are not necessary, not implemented.\n');
-        else if (inputIndex == 1)
-            LogicError('DummyCriterionNode: Gradients with respect to derivative features are not necessary, not implemented.\n');
-        else if (inputIndex == 2)
-        {
-            // predictionsGradient += userSuppliedGradient * scalarGradientFromTop
-            auto gradient = Input(2)->GradientFor(fr);
-            Matrix<ElemType>::Multiply1x1AndWeightedAdd(+1.0f, /*gradient from top:*/Gradient() /*1x1*/, /*user-supplied gradient:*/Input(1)->ValueFor(fr), 1.0f, /*add to:*/gradient);
-        }
+    Handle<Value> DBWrapper::CompactRange(const Arguments& args) {
+  HandleScope scope;
     }
     
-        // Returns all frames of a given utterance.
-    msra::dbn::matrixstripe GetUtteranceFrames(size_t index) const
+    #pragma once
+#include 'rocksdb/merge_operator.h'
+#include 'rocksdb/slice.h'
+    
+      // when we know more data has been written to the file. we can use this
+  // function to force the reader to look again in the file.
+  // Also aligns the file position indicator to the start of the next block
+  // by reading the rest of the data from the EOF position to the end of the
+  // block that was partially read.
+  void UnmarkEOF();
+    
+    
     {
-        if (!IsInRam())
-        {
-            LogicError('GetUtteranceFrames was called when data have not yet been paged in.');
-        }
+    {  releaseJniEnv(attached_thread);
+}
+// @lint-ignore TXT4 T25377293 Grandfathered in
+}  // namespace rocksdb
+    
+    
+    {}  // namespace rocksdb
+    
+    
+    {    return status;
+  }
+    
+    
+    {private:
+  uint8_t Size = 0;
+  uint8_t Data[kMaxSize];
+};
+    
+    extern 'C' {
+// Declare these symbols as weak to allow them to be optionally defined.
+#define EXT_FUNC(NAME, RETURN_TYPE, FUNC_SIG, WARN)                            \
+  RETURN_TYPE NAME##Def FUNC_SIG {                                             \
+    Printf('ERROR: Function \'%s\' not defined.\n', #NAME);                    \
+    exit(1);                                                                   \
+  }                                                                            \
+  RETURN_TYPE NAME FUNC_SIG __attribute__((weak, alias(#NAME 'Def')));
     }
     
-    using namespace Microsoft::MSR::CNTK;
+    
+    {  for (const auto &U : *InitialCorpus) {
+    if (size_t NumFeatures = RunOne(U)) {
+      CheckExitOnSrcPosOrItem();
+      Corpus.AddToCorpus(U, NumFeatures);
+      if (Options.Verbosity >= 2)
+        Printf('NEW0: %zd L %zd\n', MaxCoverage.BlockCoverage, U.size());
+    }
+    TryDetectingAMemoryLeak(U.data(), U.size(),
+                            /*DuringInitialCorpusExecution*/ true);
+  }
+  PrintStats('INITED');
+  if (Corpus.empty()) {
+    Printf('ERROR: no interesting inputs were found. '
+           'Is the code instrumented for coverage? Exiting.\n');
+    exit(1);
+  }
+}
+    
+      void StartTraceRecording() {
+    if (!Options.UseMemcmp)
+      return;
+    RecordingMemcmp = Options.UseMemcmp;
+    RecordingMemmem = Options.UseMemmem;
+    NumMutations = 0;
+    InterestingWords.clear();
+    MD.ClearAutoDictionary();
+  }
+    
+    // Changes U to contain only ASCII (isprint+isspace) characters.
+// Returns true iff U has been changed.
+bool ToASCII(uint8_t *Data, size_t Size);
