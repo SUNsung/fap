12654c024dda41df625430da90f6f21fe4896bc7
SUNsung <weatherius@gmail.com>
2018-04-08 20:10:48 +0300

    Rewrite [12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb] File in: 2018-04-08 20:10:48

--- ruby/12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb
+++ ruby/12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb
@@ -1,73 +1,127 @@
 
         
-        
-    {    # @include transition(#{border-color ease-in-out .15s, box-shadow ease-in-out .15s})
-    # to
-    # @include transition(border-color ease-in-out .15s, box-shadow ease-in-out .15s)
-    def deinterpolate_vararg_mixins(scss)
-      scss = scss.dup
-      VARARG_MIXINS.each do |mixin|
-        if scss.gsub! /(@include\s*#{Regexp.quote(mixin)})\(\s*\#\{([^}]+)\}\s*\)/, '\1(\2)'
-          log_transform mixin
+                def redis_connection_for_broadcasts
+          @redis_connection_for_broadcasts || @server.mutex.synchronize do
+            @redis_connection_for_broadcasts ||= redis_connection
+          end
         end
-      end
-      scss
+    
+      test 'raw_params returns a tuple of two key value pair strings' do
+    auth = sample_request('rcHu+HzSFw89Ypyhn/896A=').authorization.to_s
+    actual = ActionController::HttpAuthentication::Token.raw_params(auth)
+    expected = ['token=\'rcHu+HzSFw89Ypyhn/896A=\'', 'nonce=\'def\'']
+    assert_equal(expected, actual)
+  end
+    
+        test 'connect a request to controller instance without dispatch' do
+      env = {}
+      controller = BareController.new
+      controller.set_request! ActionDispatch::Request.new(env)
+      assert controller.request
     end
+  end
+    
+    ActionMailer::LogSubscriber.attach_to :action_mailer
+
+    
+          module ClassMethods
+        def tests(mailer)
+          case mailer
+          when String, Symbol
+            self._mailer_class = mailer.to_s.camelize.constantize
+          when Module
+            self._mailer_class = mailer
+          else
+            raise NonInferrableMailerError.new(mailer)
+          end
+        end
+    
+    # These are the normal settings that will be set up by Railties
+# TODO: Have these tests support other combinations of these values
+silence_warnings do
+  Encoding.default_internal = Encoding::UTF_8
+  Encoding.default_external = Encoding::UTF_8
+end
+    
+    class CustomDeliveryMethodsTest < ActiveSupport::TestCase
+  setup do
+    @old_delivery_method = ActionMailer::Base.delivery_method
+    ActionMailer::Base.add_delivery_method :custom, MyCustomDelivery
+  end
+    
+    class I18nTestMailer < ActionMailer::Base
+  configure do |c|
+    c.assets_dir = ''
+  end
+    
+      def detected_language
+    if content_language_map?
+      @object['contentMap'].keys.first
+    elsif name_language_map?
+      @object['nameMap'].keys.first
+    elsif supported_object_type?
+      LanguageDetector.instance.detect(text_from_content, @account)
+    end
+  end
+    
+      def preview_url
+    if object.needs_redownload?
+      media_proxy_url(object.id, :small)
+    else
+      full_asset_url(object.file.url(:small))
+    end
+  end
+    
+    class TagsController < ApplicationController
+  before_action :set_body_classes
+  before_action :set_instance_presenter
+    
+      describe 'GET #show' do
+    let!(:tag)     { Fabricate(:tag, name: 'test') }
+    let!(:local)   { Fabricate(:status, tags: [tag], text: 'local #test') }
+    let!(:remote)  { Fabricate(:status, tags: [tag], text: 'remote #test', account: Fabricate(:account, domain: 'remote')) }
+    let!(:late)    { Fabricate(:status, tags: [tag], text: 'late #test') }
+    
+        def register_sprockets
+      Sprockets.append_path(stylesheets_path)
+      Sprockets.append_path(fonts_path)
+      Sprockets.append_path(javascripts_path)
+    end
+  end
+end
+    
+        # apply general less to scss conversion
+    def convert_to_scss(file)
+      # get local mixin names before converting the definitions
+      mixins = shared_mixins + read_mixins(file)
+      file   = replace_vars(file)
+      file   = replace_mixin_definitions(file)
+      file   = replace_mixins(file, mixins)
+      file   = extract_mixins_from_selectors(file, CLASSES_TO_MIXINS.inject({}) { |h, cl| h.update('.#{cl}' => cl) })
+      file   = replace_spin(file)
+      file   = replace_fadein(file)
+      file   = replace_image_urls(file)
+      file   = replace_escaping(file)
+      file   = convert_less_ampersand(file)
+      file   = deinterpolate_vararg_mixins(file)
+      file   = replace_calculation_semantics(file)
+      file   = replace_file_imports(file)
+      file   = wrap_at_groups_with_at_root(file)
+      file
+    end
+    
+        def log_status(status)
+      puts bold status
+    end
+    
+      # Generate digests for assets URLs.
+  config.assets.digest = true
     
       # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
     
-        def path_for(file)
-      return file.path if file.is_a?(File)
-      return file if file.is_a?(String)
-    end
-    
-          # Returns the time the given Sass file was last modified.
-      #
-      # If the given file has been deleted or the time can't be accessed
-      # for some other reason, this should return nil.
-      #
-      # @param uri [String] The URI of the file to check.
-      #   Comes from a `:filename` option set on an engine returned by this importer.
-      # @param options [{Symbol => Object}] Options for the Sass file
-      #   containing the `@import` currently being checked.
-      # @return [Time, nil]
-      def mtime(uri, options)
-        Sass::Util.abstract(self)
-      end
-    
-        def initialize(tag_name, markup, tokens)
-      @by = nil
-      @source = nil
-      @title = nil
-      if markup =~ FullCiteWithTitle
-        @by = $1
-        @source = $2 + $3
-        @title = $4.titlecase.strip
-      elsif markup =~ FullCite
-        @by = $1
-        @source = $2 + $3
-      elsif markup =~ AuthorTitle
-        @by = $1
-        @title = $2.titlecase.strip
-      elsif markup =~ Author
-        @by = $1
-      end
-      super
-    end
-    
-        def post_render(page)
-      OctopressFilters::post_filter(page)
-    end
-  end
-    
-    describe 'border-color' do
-  before(:all) do
-    ParserSupport.parse_file('library/border-color')
-  end
-    
-      context 'called with four styles' do
-    it 'applies different styles to all sides' do
-      rule = 'border-style: dotted groove ridge none'
+            def find_order
+          @order = Spree::Order.find_by!(number: order_id)
+        end
