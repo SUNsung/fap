903837c6f77786b33014f89f024181d37df4651b
SUNsung <weatherius@gmail.com>
2018-04-08 06:23:18 +0300

    Rewrite [04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb] File in: 2018-04-08 06:23:17

--- ruby/04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb
+++ ruby/04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb
@@ -1,38 +1,82 @@
 
         
-            def push(*names)
-      @filters.push *filter_const(names)
-    end
+          private
     
-        def name=(value)
-      @name = value.try :strip
-    end
-    
-            css('.method-detail').each do |node|
-          node.at_css('.method-name')['id'] = node.at_css('a')['id']
-        end
-    
-                -- Finally, add our sequence number to our base, and chop
-            -- it to the last two bytes
-            tail := (
-              (sequence_base + nextval(table_name || '_id_seq'))
-              & 65535);
-    
-            expect(response).to have_http_status(:missing)
-      end
+      def xcode
+    if instance_variable_defined?(:@xcode)
+      @xcode
+    elsif MacOS::Xcode.installed?
+      @xcode = MacOS::Xcode.version
+      @xcode += ' => #{MacOS::Xcode.prefix}' unless MacOS::Xcode.default_prefix?
+      @xcode
     end
   end
+    
+        if ARGV.include?('--pinned') || ARGV.include?('--versions')
+      filtered_list
+    elsif ARGV.named.empty?
+      if ARGV.include? '--full-name'
+        full_names = Formula.installed.map(&:full_name).sort do |a, b|
+          if a.include?('/') && !b.include?('/')
+            1
+          elsif !a.include?('/') && b.include?('/')
+            -1
+          else
+            a <=> b
+          end
+        end
+        puts_columns full_names
+      else
+        ENV['CLICOLOR'] = nil
+        exec 'ls', *ARGV.options_only << HOMEBREW_CELLAR
+      end
+    elsif ARGV.verbose? || !$stdout.tty?
+      exec 'find', *ARGV.kegs.map(&:to_s) + %w[-not -type d -print]
+    else
+      ARGV.kegs.each { |keg| PrettyListing.new keg }
+    end
+  end
+    
+        names = @@remote_tap_formulae['#{user}/#{repo}']
+    user = user.downcase if user == 'Homebrew' # special handling for the Homebrew organization
+    names.select { |name| rx === name }.map { |name| '#{user}/#{repo}/#{name}' }
+  rescue GitHub::HTTPNotFoundError => e
+    opoo 'Failed to search tap: #{user}/#{repo}. Please run `brew update`'
+    []
+  rescue GitHub::Error => e
+    SEARCH_ERROR_QUEUE << e
+    []
+  end
+    
+        def initialize(filters = nil)
+      @filters = filters ? filters.dup : []
+    end
+    
+    desc 'Deploy a new release.'
+task :deploy do
+  set(:deploying, true)
+  %w{ starting started
+      updating updated
+      publishing published
+      finishing finished }.each do |task|
+    invoke 'deploy:#{task}'
+  end
 end
+task default: :deploy
 
     
-    def test_app(bundle)
-  @bundle_ids.include?(bundle) ? 0 : 1
-end
+      context 'called with null values' do
+    it 'writes rules for other three' do
+      ruleset = 'border-top-color: #0f0; ' +
+                'border-right-color: #ff0; ' +
+                'border-left-color: #00f;'
+      bad_rule = 'border-bottom-color: null;'
     
-          expect('.margin-implied-left').to have_rule(rule)
-    end
-  end
+      context 'expands plain buttons' do
+    it 'finds selectors' do
+      list = @buttons_list.join(', ')
+      ruleset = 'content: #{list};'
     
-          expect('.all-text-inputs-hover').to have_ruleset(ruleset)
-    end
-  end
+      context 'called with no prefixes' do
+    it 'outputs the spec' do
+      rule = 'appearance: none;'
