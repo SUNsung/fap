b052d2e30bf36afd3d7b3f9ea35e882eeb20b433
SUNsung <weatherius@gmail.com>
2018-04-08 22:10:27 +0300

    Rewrite [34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb] File in: 2018-04-08 22:10:26

--- ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
+++ ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
@@ -1,68 +1,112 @@
 
         
-        module Jobs
+              def initialize
+        super
+        self.class.controller_path = ''
+        @request = ActionController::TestRequest.create(self.class)
+        @response = ActionDispatch::TestResponse.new
     
-      def all_projects(current_user)
-    projects = []
-    
-          if dest.end_with? '/'
-        dest_dir.relative_path_from(base_dir).to_s.tap do |result|
-          result << '/' if result != '.'
+              # Bubbled up from the adapter require. Prefix the exception message
+          # with some guidance about how to address it and reraise.
+          else
+            raise e.class, 'Error loading the '#{adapter}' Action Cable pubsub adapter. Missing a gem it depends on? #{e.message}', e.backtrace
+          end
         end
-      else
-        dest_dir.parent.relative_path_from(base_dir).join(dest.split('/').last).to_s
+    
+          def not_index
+        @text ||= nil
+        self.response_body = @text.to_s
       end
     end
     
-            entries
+      def test_lib_helper_methods_after_clear_helpers
+    assert_nothing_raised do
+      call_controller(JustMeController, 'lib')
+    end
+  end
+    
+          def handle_exception_with_mailer_class(exception)
+        if klass = mailer_class
+          klass.handle_exception exception
+        else
+          raise exception
+        end
+      end
+  end
+end
+
+    
+          text.split.each do |word|
+        if sentences.first.present? && (sentences.last + [word]).join(' ').length > len
+          sentences << [word]
+        else
+          sentences.last << word
+        end
+      end
+    
+            def set_delivery_method(method)
+          @old_delivery_method = ActionMailer::Base.delivery_method
+          ActionMailer::Base.delivery_method = method
+        end
+    
+          check_class_collision suffix: 'Mailer'
+    
+      def mail_with_i18n_subject(recipient)
+    @recipient  = recipient
+    I18n.locale = :de
+    mail(to: recipient, subject: I18n.t(:email_subject),
+      from: 'system@loudthinking.com', date: Time.local(2004, 12, 12))
+  end
+end
+    
+    def envygeeks(url)
+  return url if url.end_with?(FORWARD_SLASH) || url == FORWARD_SLASH
+    
+    # Create an array of vendored mimetype => [extensions]
+mimes = {}
+json = open('https://raw.githubusercontent.com/jshttp/mime-db/master/db.json').read
+data = JSON.parse(json)
+data.reject! { |mime, meta| meta['extensions'].nil? || meta['extensions'].empty? }
+data.each do |mime, meta|
+  # Normalize extensions and mime-types
+  mime = mime.downcase.strip
+  extensions = meta['extensions'].map { |e| e.downcase.strip }.compact
+    
+        # --
+    # NOTE: Pathutil#in_path? gets the realpath.
+    # @param [<Anything>] entry the entry you want to validate.
+    # Check if a path is outside of our given root.
+    # --
+    def symlink_outside_site_source?(entry)
+      !Pathutil.new(entry).in_path?(
+        site.in_source_dir
+      )
+    end
+    
+    If you run into trouble, you can find helpful resources at https://jekyllrb.com/help/!
+            MSG
+            raise Jekyll::Errors::MissingDependencyException, name
+          end
+        end
       end
     end
   end
 end
 
     
-              css('##{dom_id}-methods ~ h4 code').each do |node|
-            next unless name = node.content[/\('(\w+)'\)/, 1]
-            id = node.parent['id'] = '#{dom_id}-#{name.parameterize}-method'
-            name.prepend '#{dom_id.singularize.titleize}: '
-            name << ' (method)'
-            entries << [name, id]
-          end
+          private
+      def grouped_array(groups)
+        groups.each_with_object([]) do |item, array|
+          array << {
+            'name'  => item.first,
+            'items' => item.last,
+            'size'  => item.last.size,
+          }
         end
-    
-    answer_ary = answer.to_a
-# puts answer_ary
-
-    
-    Given an Application (app) bundle directory on disk, find all
-login items associated with that app, which you can use in a
-Cask uninstall stanza, eg
-    
-    describe 'margin' do
-  before(:all) do
-    ParserSupport.parse_file('library/margin')
-  end
-    
-          expect('.padding-all').to have_rule(rule)
+      end
     end
   end
+end
+
     
-        @inputs_list = %w(
-      [type='color']
-      [type='date']
-      [type='datetime']
-      [type='datetime-local']
-      [type='email']
-      [type='month']
-      [type='number']
-      [type='password']
-      [type='search']
-      [type='tel']
-      [type='text']
-      [type='time']
-      [type='url']
-      [type='week']
-      input:not([type])
-      textarea
-    )
-  end
+          raise Discourse::InvalidParameters.new(:to_address) unless args[:to_address].present?
