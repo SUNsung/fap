ca17068f868359cbf894fa5efd1437a054d00e1f
SUNsung <weatherius@gmail.com>
2018-04-09 09:58:05 +0300

    Rewrite [062ntJTQT1O6A065RJmINIknuE06hdngPWcSu6606vEl.py] File in: 2018-04-09 09:58:04

--- pyton/062ntJTQT1O6A065RJmINIknuE06hdngPWcSu6606vEl.py
+++ pyton/062ntJTQT1O6A065RJmINIknuE06hdngPWcSu6606vEl.py
@@ -1,59 +1,124 @@
 
         
-                try:
-            expected = int_to_float(n)
-        except OverflowError:
-            expected = 'overflow'
+            Code is a simple port of what is already in the /scripts directory
     
-        def path_ok(self):
-        'Simple validity check for menu file path'
-        path = self.path.get().strip()
-        if not path: #no path specified
-            self.showerror('no help file path specified.', self.path_error)
-            return None
-        elif not path.startswith(('www.', 'http')):
-            if path[:5] == 'file:':
-                path = path[5:]
-            if not os.path.exists(path):
-                self.showerror('help file path does not exist.',
-                               self.path_error)
-                return None
-            if platform == 'darwin':  # for Mac Safari
-                path =  'file://' + path
-        return path
+        def on_fault_trigger(self):
+        self._hsm._next_state('suspect')
+        self._hsm._send_diagnostics_request()
+        self._hsm._raise_alarm()
     
-            elements = 'ABCDEFGHI'
-        for i in range(-len(elements), len(elements)):
-            d = deque(elements, maxlen=len(elements)+1)
-            d.insert(i, 'Z')
-            if i >= 0:
-                self.assertEqual(d[i], 'Z')
+        def setUp(self):
+        self.e, self.g = get_localizer(language='English'), \
+                         get_localizer(language='Greek')
+    
+        def test_initial_am_station(self):
+        station = self.radio.state.stations[self.radio.state.pos]
+        expected_station = '1250'
+        self.assertEqual(station, expected_station)
+    
+        def setUp(self):
+        self.tc1 = TC1()
+        self.tc2 = TC2()
+        self.tc3 = TC3()
+        self.average_result_tc1 = '###### In Test 1 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc2 = '###### In Test 2 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc3 = '###### In Test 3 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.runner = TestRunner()
+        self.out = StringIO()
+        self.saved_stdout = sys.stdout
+        sys.stdout = self.out
+    
+        def get_current_time_as_html_fragment(self, time_provider):
+        current_time = time_provider.now()
+        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
+        return current_time_as_html_fragment
+    
+    '''
+Port of the Java example of 'Parameter Injection' in
+'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
+(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
+http://xunitpatterns.com/Dependency%20Injection.html.
+    
+        def delegate(self, requests):
+        for request in requests:
+            self.target.send(request)
+    
+            # and can also be undone at will
+        for cmd in reversed(command_stack):
+            cmd.undo()
+    finally:
+        os.unlink('foo.txt')
+    
+        def update(self, subject):
+        print(u'HexViewer: Subject %s has data 0x%x' %
+              (subject.name, subject.data))
+    
+    
+def get_changed_files(old_ref, new_ref):
+    '''Return a list of files that have changed from one ref to another.'''
+    root = get_root()
+    changed_files_text = subprocess.check_output(['git', 'diff', '--name-only',
+                                                  old_ref, new_ref])
+    changed_files = changed_files_text.splitlines()
+    return [os.path.join(root, x) for x in changed_files]
+    
+    
+# blanket fallback rule
+api('templated', NullJsonTemplate)
+    
+            The user's response to the CAPTCHA should be sent as `captcha`
+        along with your request.
+    
+    from BeautifulSoup import BeautifulSoup, Tag
+    
+                if code in (204, 304):
+                # NEVER return a content body on 204/304 or downstream
+                # caches may become very confused.
+                return ''
+            elif c.render_style not in self.allowed_render_styles:
+                return str(code)
+            elif c.render_style in extensions.API_TYPES:
+                data = request.environ.get('extra_error_data', {'error': code})
+                message = request.GET.get('message', '')
+                if message:
+                    data['message'] = message
+                if request.environ.get('WANT_RAW_JSON'):
+                    return scriptsafe_dumps(data)
+                return websafe_json(json.dumps(data))
+            elif takedown and code == 404:
+                link = Link._by_fullname(takedown)
+                return pages.TakedownPage(link).render()
+            elif code == 400:
+                return self.send400()
+            elif code == 403:
+                return self.send403()
+            elif code == 429:
+                return self.send429()
+            elif code == 500:
+                failien_url = make_failien_url()
+                sad_message = get_funny_translated_string('500_page')
+                sad_message %= {'admin': random.choice(self.admins)}
+                sad_message = safemarkdown(sad_message)
+                return redditbroke % (failien_url, sad_message)
+            elif code == 503:
+                return self.send503()
+            elif c.site:
+                return self.send404()
             else:
-                self.assertEqual(d[i-1], 'Z')
+                return 'page not found'
+        except Exception as e:
+            return handle_awful_failure('ErrorController.GET_document: %r' % e)
     
-            self.assertRaises(TypeError, Array.from_buffer_copy, b'123')
-        self.assertRaises(TypeError, Structure.from_buffer_copy, b'123')
-        self.assertRaises(TypeError, Union.from_buffer_copy, b'123')
-        self.assertRaises(TypeError, _CFuncPtr.from_buffer_copy, b'123')
-        self.assertRaises(TypeError, _Pointer.from_buffer_copy, b'123')
-        self.assertRaises(TypeError, _SimpleCData.from_buffer_copy, b'123')
-    
-        @classmethod
-    def setUpClass(cls):
-        cls.s = Subject()
-        cls.dec_obs = DecimalViewer()
-        cls.hex_obs = HexViewer()
-    
-        def do_action(self):
-        print(self.name, self.action.name, end=' ')
-        return self.action
-    
-    *What does this example do?
-In this example queue.Queue is used to create the pool (wrapped in a
-custom ObjectPool object to use with the with statement), and it is
-populated with strings.
-As we can see, the first string object put in 'yam' is USED by the
-with statement. But because it is released back into the pool
-afterwards it is reused by the explicit call to sample_queue.get().
-Same thing happens with 'sam', when the ObjectPool created insided the
-function is deleted (by the GC) and the object is returned.
+            c.allow_framing = True
