7b665645c06ff41f3c4a706969ffb87f7502cb1f
SUNsung <weatherius@gmail.com>
2018-04-09 12:02:08 +0300

    Rewrite [58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py] File in: 2018-04-09 12:02:08

--- pyton/58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py
+++ pyton/58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py
@@ -1,163 +1,270 @@
 
         
-                String:                    '#4e9a06',        # class: 's'
-        String.Backtick:           '#4e9a06',        # class: 'sb'
-        String.Char:               '#4e9a06',        # class: 'sc'
-        String.Doc:                'italic #8f5902', # class: 'sd' - like a comment
-        String.Double:             '#4e9a06',        # class: 's2'
-        String.Escape:             '#4e9a06',        # class: 'se'
-        String.Heredoc:            '#4e9a06',        # class: 'sh'
-        String.Interpol:           '#4e9a06',        # class: 'si'
-        String.Other:              '#4e9a06',        # class: 'sx'
-        String.Regex:              '#4e9a06',        # class: 'sr'
-        String.Single:             '#4e9a06',        # class: 's1'
-        String.Symbol:             '#4e9a06',        # class: 'ss'
+                status_line = 'HTTP/{version} {status} {reason}'.format(
+            version=version,
+            status=original.status,
+            reason=original.reason
+        )
+        headers = [status_line]
+        try:
+            # `original.msg` is a `http.client.HTTPMessage` on Python 3
+            # `_headers` is a 2-tuple
+            headers.extend(
+                '%s: %s' % header for header in original.msg._headers)
+        except AttributeError:
+            # and a `httplib.HTTPMessage` on Python 2.x
+            # `headers` is a list of `name: val<CRLF>`.
+            headers.extend(h.strip() for h in original.msg.headers)
     
     
-@pytest.fixture
-def httpbin(httpbin):
-    return prepare_url(httpbin)
+def is_valid_mime(mime):
+    return mime and MIME_RE.match(mime)
     
-        :param url: URL for the new :class:`Request` object.
-    :param data: (optional) Dictionary (will be form-encoded), bytes, or file-like object to send in the body of the :class:`Request`.
-    :param json: (optional) json data to send in the body of the :class:`Request`.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    '''
     
-        @possible_keys
-    def test_delitem(self, key):
-        del self.case_insensitive_dict[key]
-        assert key not in self.case_insensitive_dict
+@mock.patch('httpie.core.get_response')
+def test_timeout(get_response):
+    def error(msg, *args, **kwargs):
+        global error_msg
+        error_msg = msg % args
     
-        def __eq__(self, other):
-        return all([
-            self.username == getattr(other, 'username', None),
-            self.password == getattr(other, 'password', None)
-        ])
     
-        def __setitem__(self, name, value):
-        '''Dict-like __setitem__ for compatibility with client code. Throws
-        exception if there is already a cookie of that name in the jar. In that
-        case, use the more explicit set() method instead.
+def test_follow_all_output_options_used_for_redirects(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 3
+    assert HTTP_OK not in r
+    
+        def __iter__(self):
+        return iter(sorted(plugin_manager.get_auth_plugin_mapping().keys()))
+    
+            for i in html_json['sources']:
+            if 'src' in i:  #to avoid KeyError
+                if i['src'].startswith('https'):
+                    link_list.append((str(i['height']), i['src']))
+    
+    site_info = 'ehow.com'
+download = ehow_download
+download_playlist = playlist_not_supported('ehow')
+
+    
+        fc2video_download_by_upid(upid, output_dir, merge, info_only)
+    
+    
+def huaban_download_board(url, output_dir, **kwargs):
+    kwargs['merge'] = False
+    board = extract_board_data(url)
+    output_dir = os.path.join(output_dir, board.title)
+    print_info(site_info, board.title, 'jpg', float('Inf'))
+    for pin in board.pins:
+        download_urls([pin.url], pin.id, pin.ext, float('Inf'),
+                      output_dir=output_dir, faker=True, **kwargs)
+    
+    __all__ = ['kuwo_download']
+    
+        type_, ext, size = url_info(stream_url)
+    print_info(site_info, title, type_, size)
+    if not info_only:
+        download_url_ffmpeg(url=stream_url, title=title, ext= 'mp4', output_dir=output_dir)
+    
+            msg = ('Error in conversion of integer {} to float.  '
+               'Got {}, expected {}.'.format(n, actual, expected))
+        self.assertEqual(actual, expected, msg)
+    
+        def test_copy_method(self):
+        mut = [10]
+        d = deque([mut])
+        e = d.copy()
+        self.assertEqual(list(d), list(e))
+        mut[0] = 11
+        self.assertNotEqual(id(d), id(e))
+        self.assertEqual(list(d), list(e))
+    
+        def test_string_with_utf8_bom(self):
+        # see #18958
+        bom_json = '[1,2,3]'.encode('utf-8-sig').decode('utf-8')
+        with self.assertRaises(self.JSONDecodeError) as cm:
+            self.loads(bom_json)
+        self.assertIn('BOM', str(cm.exception))
+        with self.assertRaises(self.JSONDecodeError) as cm:
+            self.json.load(StringIO(bom_json))
+        self.assertIn('BOM', str(cm.exception))
+        # make sure that the BOM is not detected in the middle of a string
+        bom_in_str = ''{}''.format(''.encode('utf-8-sig').decode('utf-8'))
+        self.assertEqual(self.loads(bom_in_str), '\ufeff')
+        self.assertEqual(self.json.load(StringIO(bom_in_str)), '\ufeff')
+    
+        def seek(self, pos, whence=0):
+        '''Seek to specified position into the chunk.
+        Default position is 0 (start of chunk).
+        If the file is not seekable, this will result in an error.
         '''
-        self.set(name, value)
     
+    def py_make_scanner(context):
+    parse_object = context.parse_object
+    parse_array = context.parse_array
+    parse_string = context.parse_string
+    match_number = NUMBER_RE.match
+    strict = context.strict
+    parse_float = context.parse_float
+    parse_int = context.parse_int
+    parse_constant = context.parse_constant
+    object_hook = context.object_hook
+    object_pairs_hook = context.object_pairs_hook
+    memo = context.memo
     
-class TestGetEnvironProxies:
-    '''Ensures that IP addresses are correctly matches with ranges
-    in no_proxy variable.
-    '''
-    
-            :param url: URL for the new :class:`Request` object.
-        :param data: (optional) Dictionary, bytes, or file-like object to send in the body of the :class:`Request`.
-        :param \*\*kwargs: Optional arguments that ``request`` takes.
-        :rtype: requests.Response
+        def xover(self, start, end, *, file=None):
+        '''Process an XOVER command (optional server extension) Arguments:
+        - start: start of range
+        - end: end of range
+        - file: Filename string or file object to store the result in
+        Returns:
+        - resp: server response if successful
+        - list: list of dicts containing the response fields
         '''
+        resp, lines = self._longcmdstring('XOVER {0}-{1}'.format(start, end),
+                                          file)
+        fmt = self._getoverviewfmt()
+        return resp, _parse_overview(lines, fmt)
+    
+        # This dictionary maps from (feature,value) to a list of
+    # (option,value) pairs that should be set on the Options object.
+    # If a (feature,value) setting is not in this dictionary, it is
+    # not supported by the DOMBuilder.
+    #
+    _settings = {
+        ('namespace_declarations', 0): [
+            ('namespace_declarations', 0)],
+        ('namespace_declarations', 1): [
+            ('namespace_declarations', 1)],
+        ('validation', 0): [
+            ('validation', 0)],
+        ('external_general_entities', 0): [
+            ('external_general_entities', 0)],
+        ('external_general_entities', 1): [
+            ('external_general_entities', 1)],
+        ('external_parameter_entities', 0): [
+            ('external_parameter_entities', 0)],
+        ('external_parameter_entities', 1): [
+            ('external_parameter_entities', 1)],
+        ('validate_if_schema', 0): [
+            ('validate_if_schema', 0)],
+        ('create_entity_ref_nodes', 0): [
+            ('create_entity_ref_nodes', 0)],
+        ('create_entity_ref_nodes', 1): [
+            ('create_entity_ref_nodes', 1)],
+        ('entities', 0): [
+            ('create_entity_ref_nodes', 0),
+            ('entities', 0)],
+        ('entities', 1): [
+            ('entities', 1)],
+        ('whitespace_in_element_content', 0): [
+            ('whitespace_in_element_content', 0)],
+        ('whitespace_in_element_content', 1): [
+            ('whitespace_in_element_content', 1)],
+        ('cdata_sections', 0): [
+            ('cdata_sections', 0)],
+        ('cdata_sections', 1): [
+            ('cdata_sections', 1)],
+        ('comments', 0): [
+            ('comments', 0)],
+        ('comments', 1): [
+            ('comments', 1)],
+        ('charset_overrides_xml_encoding', 0): [
+            ('charset_overrides_xml_encoding', 0)],
+        ('charset_overrides_xml_encoding', 1): [
+            ('charset_overrides_xml_encoding', 1)],
+        ('infoset', 0): [],
+        ('infoset', 1): [
+            ('namespace_declarations', 0),
+            ('validate_if_schema', 0),
+            ('create_entity_ref_nodes', 0),
+            ('entities', 0),
+            ('cdata_sections', 0),
+            ('datatype_normalization', 1),
+            ('whitespace_in_element_content', 1),
+            ('comments', 1),
+            ('charset_overrides_xml_encoding', 1)],
+        ('supported_mediatypes_only', 0): [
+            ('supported_mediatypes_only', 0)],
+        ('namespaces', 0): [
+            ('namespaces', 0)],
+        ('namespaces', 1): [
+            ('namespaces', 1)],
+    }
     
     
-def test_digestauth_401_count_reset_on_redirect():
-    '''Ensure we correctly reset num_401_calls after a successful digest auth,
-    followed by a 302 redirect to another digest auth prompt.
+
+if __name__ == '__main__':
+    colordb = get_colordb('/usr/openwin/lib/rgb.txt')
+    if not colordb:
+        print('No parseable color database found')
+        sys.exit(1)
+    # on my system, this color matches exactly
+    target = 'navy'
+    red, green, blue = rgbtuple = colordb.find_byname(target)
+    print(target, ':', red, green, blue, triplet_to_rrggbb(rgbtuple))
+    name, aliases = colordb.find_byrgb(rgbtuple)
+    print('name:', name, 'aliases:', COMMASPACE.join(aliases))
+    r, g, b = (1, 1, 128)                         # nearest to navy
+    r, g, b = (145, 238, 144)                     # nearest to lightgreen
+    r, g, b = (255, 251, 250)                     # snow
+    print('finding nearest to', target, '...')
+    import time
+    t0 = time.time()
+    nearest = colordb.nearest(r, g, b)
+    t1 = time.time()
+    print('found nearest color', nearest, 'in', t1-t0, 'seconds')
+    # dump the database
+    for n in colordb.unique_names():
+        r, g, b = colordb.find_byname(n)
+        aliases = colordb.aliases_of(r, g, b)
+        print('%20s: (%3d/%3d/%3d) == %s' % (n, r, g, b,
+                                             SPACE.join(aliases[1:])))
+
     
-        old_layer = keras.layers.Convolution3D(5, 3, 3, 4,
-                                           init='normal',
-                                           subsample=(2, 2, 2),
-                                           border_mode='valid',
-                                           dim_ordering='th',
-                                           W_regularizer='l1',
-                                           b_regularizer='l2',
-                                           W_constraint='maxnorm',
-                                           b_constraint='unitnorm',
-                                           name='conv')
-    new_layer = keras.layers.Conv3D(5, (3, 3, 4),
-                                    kernel_initializer='normal',
-                                    strides=(2, 2, 2),
-                                    padding='valid',
-                                    kernel_regularizer='l1',
-                                    bias_regularizer='l2',
-                                    kernel_constraint='max_norm',
-                                    bias_constraint='unit_norm',
-                                    data_format='channels_first',
-                                    name='conv')
-    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer.get_config())
+        def test_sane_len(self):
+        # this test just tests our assumptions about __len__
+        # this will start failing if __len__ changes assertions
+        for badval in ['illegal', -1, 1 << 32]:
+            class A:
+                def __len__(self):
+                    return badval
+            try:
+                bool(A())
+            except (Exception) as e_bool:
+                try:
+                    len(A())
+                except (Exception) as e_len:
+                    self.assertEqual(str(e_bool), str(e_len))
     
-        # test with functional API
-    x = Input(shape=(3, 2))
-    y = wrappers.TimeDistributed(model)(x)
-    outer_model = Model(x, y)
-    outer_model.compile(optimizer='rmsprop', loss='mse')
-    outer_model.fit(np.random.random((10, 3, 2)), np.random.random((10, 3, 3)),
-                    epochs=1, batch_size=10)
+            return None
     
-        preds = clf.predict(X_test, batch_size=batch_size)
-    assert preds.shape == (num_test, )
-    for prediction in np.unique(preds):
-        assert prediction in range(num_classes)
+            p = Popen(
+            [sys.executable, '-m', 'testapp'], stdout=subprocess.PIPE,
+            cwd=path, env=dict(os.environ, PYTHONPATH=pythonpath),
+            universal_newlines=True)
+        out = p.communicate()[0]
+        self.assertEqual(out, 'Starting\nStarting\n')
+
     
-        # a Model inside a Model
-    x = Input(shape=(1,))
-    y = Dense(2)(x)
-    inner_model = Model(x, y)
-    x = Input(shape=(1,))
-    y = inner_model(x)
-    outer_model = Model(x, y)
-    assert outer_model.trainable_weights == inner_model.trainable_weights
-    inner_model.trainable = False
-    assert outer_model.trainable_weights == []
-    inner_model.trainable = True
-    inner_model.layers[-1].trainable = False
-    assert outer_model.trainable_weights == []
-    
-        out = K.eval(weighted_loss(K.variable(x),
-                               K.variable(y),
-                               K.variable(weights),
-                               K.variable(mask)))
-    
-            x = np.random.random((1,) + shape)
+            c.notify_all()
+        self.loop_briefly()
+        self.history.append('notify_all')
     
     
-@keras_test
-def test_min_max_norm():
-    array = get_example_array()
-    for m in get_test_values():
-        norm_instance = constraints.min_max_norm(min_value=m, max_value=m * 2)
-        normed = norm_instance(K.variable(array))
-        value = K.eval(normed)
-        l2 = np.sqrt(np.sum(np.square(value), axis=0))
-        assert not l2[l2 < m]
-        assert not l2[l2 > m * 2 + 1e-5]
-    
-        # three different types of merging
-    merged_sum = legacy_layers.merge([masked_a, masked_b], mode='sum')
-    merged_concat = legacy_layers.merge([masked_a, masked_b], mode='concat', concat_axis=1)
-    merged_concat_mixed = legacy_layers.merge([masked_a, input_b], mode='concat', concat_axis=1)
-    
-    from ..utils.generic_utils import deserialize_keras_object
-from ..engine import Layer
-from ..engine import Input
-from ..engine import InputLayer
-from ..engine import InputSpec
-from .merge import *
-from .core import *
-from .convolutional import *
-from .pooling import *
-from .local import *
-from .recurrent import *
-from .cudnn_recurrent import *
-from .normalization import *
-from .embeddings import *
-from .noise import *
-from .advanced_activations import *
-from .wrappers import *
-from .convolutional_recurrent import *
-from ..legacy.layers import *
-    
-    def speckle(img):
-    severity = np.random.uniform(0, 0.6)
-    blur = ndimage.gaussian_filter(np.random.randn(*img.shape) * severity, 1)
-    img_speck = (img + blur)
-    img_speck[img_speck > 1] = 1
-    img_speck[img_speck <= 0] = 0
-    return img_speck
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r'/', MainHandler),
+            (r'/chatsocket', ChatSocketHandler),
+        ]
+        settings = dict(
+            cookie_secret='__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__',
+            template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+            static_path=os.path.join(os.path.dirname(__file__), 'static'),
+            xsrf_cookies=True,
+        )
+        super(Application, self).__init__(handlers, **settings)
