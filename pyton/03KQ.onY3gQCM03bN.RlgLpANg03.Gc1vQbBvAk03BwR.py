b9429f2935d0e0c9b82b8ab301900bb18f2d0be8
SUNsung <weatherius@gmail.com>
2018-04-09 03:07:27 +0300

    Rewrite [03KQ.onY3gQCM03bN.RlgLpANg03.Gc1vQbBvAk03BwR.py] File in: 2018-04-09 03:07:26

--- pyton/03KQ.onY3gQCM03bN.RlgLpANg03.Gc1vQbBvAk03BwR.py
+++ pyton/03KQ.onY3gQCM03bN.RlgLpANg03.Gc1vQbBvAk03BwR.py
@@ -1,165 +1,145 @@
 
         
-        from sklearn.dummy import DummyClassifier
+        # build some special values
+special = [0, 1, 2, BASE, BASE >> 1, 0x5555555555555555, 0xaaaaaaaaaaaaaaaa]
+#  some solid strings of one bits
+p2 = 4  # 0 and 1 already added
+for i in range(2*SHIFT):
+    special.append(p2 - 1)
+    p2 = p2 << 1
+del p2
+# add complements & negations
+special += [~x for x in special] + [-x for x in special]
     
-    from sklearn.datasets.samples_generator import make_regression
+        from idlelib.idle_test.htest import run
+    run(Query, HelpSource)
+
     
-      * scikit-learn
+    __test__ = {'libreftest' : libreftest}
     
-    pages = {
-    u'ar': u'http://ar.wikipedia.org/wiki/%D9%88%D9%8A%D9%83%D9%8A%D8%A8%D9%8A%D8%AF%D9%8A%D8%A7',
-    u'de': u'http://de.wikipedia.org/wiki/Wikipedia',
-    u'en': u'https://en.wikipedia.org/wiki/Wikipedia',
-    u'es': u'http://es.wikipedia.org/wiki/Wikipedia',
-    u'fr': u'http://fr.wikipedia.org/wiki/Wikip%C3%A9dia',
-    u'it': u'http://it.wikipedia.org/wiki/Wikipedia',
-    u'ja': u'http://ja.wikipedia.org/wiki/Wikipedia',
-    u'nl': u'http://nl.wikipedia.org/wiki/Wikipedia',
-    u'pl': u'http://pl.wikipedia.org/wiki/Wikipedia',
-    u'pt': u'http://pt.wikipedia.org/wiki/Wikip%C3%A9dia',
-    u'ru': u'http://ru.wikipedia.org/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D0%BF%D0%B5%D0%B4%D0%B8%D1%8F',
-#    u'zh': u'http://zh.wikipedia.org/wiki/Wikipedia',
-}
+        @classmethod
+    def zonelist(cls, zonedir='/usr/share/zoneinfo'):
+        zones = []
+        for root, _, files in os.walk(zonedir):
+            for f in files:
+                p = os.path.join(root, f)
+                with open(p, 'rb') as o:
+                    magic =  o.read(4)
+                if magic == b'TZif':
+                    zones.append(p[len(zonedir) + 1:])
+        return zones
     
-    from sklearn import datasets
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import LinearSVC
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import (brier_score_loss, precision_score, recall_score,
-                             f1_score)
-from sklearn.calibration import CalibratedClassifierCV, calibration_curve
-from sklearn.model_selection import train_test_split
+    make_scanner = c_make_scanner or py_make_scanner
+
     
-    Non-adjusted measures such as the V-Measure show a dependency between
-the number of clusters and the number of samples: the mean V-Measure
-of random labeling increases significantly as the number of clusters is
-closer to the total number of samples used to compute the measure.
-    
-                # exhausted iterator
-            self.assertRaises(StopIteration, next, itorg)
-            dump = pickle.dumps((itorg, orig), proto)
-            it, d = pickle.loads(dump)
-            for i, x in enumerate(data):
-                d[i] = x
-            self.assertEqual(type(it), type(itorg))
-            self.assertEqual(list(it), [])
-    
-        def test_set_properties(self):
-        morsel = cookies.Morsel()
-        with self.assertRaises(AttributeError):
-            morsel.key = ''
-        with self.assertRaises(AttributeError):
-            morsel.value = ''
-        with self.assertRaises(AttributeError):
-            morsel.coded_value = ''
-    
-        def skip(self):
-        '''Skip the rest of the chunk.
-        If you are not interested in the contents of the chunk,
-        this method should be called so that the file points to
-        the start of the next chunk.
-        '''
-    
-    simple_escapes = {'a': '\a',
-                  'b': '\b',
-                  'f': '\f',
-                  'n': '\n',
-                  'r': '\r',
-                  't': '\t',
-                  'v': '\v',
-                  ''': ''',
-                  ''': ''',
-                  '\\': '\\'}
-    
-        # We only ever write one 'entry point' symbol - either
-    # 'main' or 'WinMain'.  Therefore, there is no need to
-    # pass a subsystem switch to the linker as it works it
-    # out all by itself.  However, the subsystem _does_ determine
-    # the file extension and additional linker flags.
-    target_link_flags = ''
-    target_ext = '.exe'
-    if subsystem_details[vars['subsystem']][2]:
-        target_link_flags = '-dll'
-        target_ext = '.dll'
-    
-    class PyBaseExceptionObjectPtr(PyObjectPtr):
-    '''
-    Class wrapping a gdb.Value that's a PyBaseExceptionObject* i.e. an exception
-    within the process being debugged.
-    '''
-    _typename = 'PyBaseExceptionObject'
-    
-        def test_help_flag(self):
-        rc, out, err = assert_python_ok('-m', 'json.tool', '-h')
-        self.assertEqual(rc, 0)
-        self.assertTrue(out.startswith(b'usage: '))
-        self.assertEqual(err, b'')
-    
-            try:
-            result = self.fn(*self.args, **self.kwargs)
-        except BaseException:
-            e = sys.exc_info()[1]
-            self.future.set_exception(e)
-        else:
-            self.future.set_result(result)
-    
-    def is_prime(n):
-    if n % 2 == 0:
-        return False
+        def remove_flag(self, flag):
+        '''Unset the given string flag(s) without changing others.'''
+        if 'Status' in self or 'X-Status' in self:
+            self.set_flags(''.join(set(self.get_flags()) - set(flag)))
     
     
-def _FormatCompleterDebugInfo( completer ):
-  message = '{0} completer debug information:\n'.format( completer[ 'name' ] )
-  for server in completer[ 'servers' ]:
-    name = server[ 'name' ]
-    if server[ 'is_running' ]:
-      address = server[ 'address' ]
-      port = server[ 'port' ]
-      if address and port:
-        message += '  {0} running at: http://{1}:{2}\n'.format( name,
-                                                                address,
-                                                                port )
-      else:
-        message += '  {0} running\n'.format( name )
-      message += '  {0} process ID: {1}\n'.format( name, server[ 'pid' ] )
-    else:
-      message += '  {0} not running\n'.format( name )
-    message += '  {0} executable: {1}\n'.format( name, server[ 'executable'] )
-    logfiles = server[ 'logfiles' ]
-    if logfiles:
-      message += '  {0} logfiles:\n'.format( name )
-      for logfile in logfiles:
-        message += '    {0}\n'.format( logfile )
-    else:
-      message += '  No logfiles available\n'
-    if 'extras' in server:
-      for extra in server[ 'extras' ]:
-        message += '  {0} {1}: {2}\n'.format( name,
-                                              extra[ 'key' ],
-                                              extra[ 'value' ] )
-  for item in completer[ 'items' ]:
-    message += '  {0}: {1}\n'.format( item[ 'key' ].capitalize(),
-                                      item[ 'value' ] )
-  return message
+
+# format is a tuple (RE, SCANLINES, CLASS) where RE is a compiled regular
+# expression, SCANLINES is the number of header lines to scan, and CLASS is
+# the class to instantiate if a match is found
     
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
+            class A(POINTER(c_ulong)):
+            pass
+    
+        # For pickling to work, the __module__ variable needs to be set to the frame
+    # where the named tuple is created.  Bypass this step in enviroments where
+    # sys._getframe is not defined (Jython for example).
+    if hasattr(_sys, '_getframe'):
+        result.__module__ = _sys._getframe(1).f_globals.get('__name__', '__main__')
+    
+                self._work_queue.put(w)
+            self._adjust_thread_count()
+            return f
+    submit.__doc__ = _base.Executor.submit.__doc__
     
     
-def FormatDebugInfoResponse_ExtraConfFoundAndLoaded_test():
+def FormatDebugInfoResponse_Completer_ServerNotRunningWithNoLogfiles_test():
   response = deepcopy( GENERIC_RESPONSE )
-  response[ 'extra_conf' ].update( {
-    'is_loaded': True,
-    'path': '/path/to/extra/conf'
+  response[ 'completer' ][ 'servers' ][ 0 ].update( {
+    'is_running': False,
+    'logfiles': []
   } )
   assert_that(
     FormatDebugInfoResponse( response ),
     contains_string(
-      'Extra configuration file found and loaded\n'
-      'Extra configuration path: /path/to/extra/conf\n'
+      'Completer name completer debug information:\n'
+      '  Server name not running\n'
+      '  Server name executable: /path/to/executable\n'
+      '  No logfiles available\n'
+      '  Server name key: value\n'
+      '  Key: value\n'
     )
   )
+
+    
+    
+def KeywordsFromSyntaxListOutput_PythonSyntax_test():
+  expected_keywords = (
+    'bytearray', 'IndexError', 'all', 'help', 'vars', 'SyntaxError', 'global',
+    'elif', 'unicode', 'sorted', 'memoryview', 'isinstance', 'except',
+    'nonlocal', 'NameError', 'finally', 'BytesWarning', 'dict', 'IOError',
+    'pass', 'oct', 'bin', 'SystemExit', 'return', 'StandardError', 'format',
+    'TabError', 'break', 'next', 'not', 'UnicodeDecodeError', 'False',
+    'RuntimeWarning', 'list', 'iter', 'try', 'reload', 'Warning', 'round',
+    'dir', 'cmp', 'set', 'bytes', 'UnicodeTranslateError', 'intern',
+    'issubclass', 'yield', 'Ellipsis', 'hash', 'locals', 'BufferError',
+    'slice', 'for', 'FloatingPointError', 'sum', 'VMSError', 'getattr', 'abs',
+    'print', 'import', 'True', 'FutureWarning', 'ImportWarning', 'None',
+    'EOFError', 'len', 'frozenset', 'ord', 'super', 'raise', 'TypeError',
+    'KeyboardInterrupt', 'UserWarning', 'filter', 'range', 'staticmethod',
+    'SystemError', 'or', 'BaseException', 'pow', 'RuntimeError', 'float',
+    'MemoryError', 'StopIteration', 'globals', 'divmod', 'enumerate', 'apply',
+    'LookupError', 'open', 'basestring', 'from', 'UnicodeError', 'zip', 'hex',
+    'long', 'IndentationError', 'int', 'chr', '__import__', 'type',
+    'Exception', 'continue', 'tuple', 'reduce', 'reversed', 'else', 'assert',
+    'UnicodeEncodeError', 'input', 'with', 'hasattr', 'delattr', 'setattr',
+    'raw_input', 'PendingDeprecationWarning', 'compile', 'ArithmeticError',
+    'while', 'del', 'str', 'property', 'def', 'and', 'GeneratorExit',
+    'ImportError', 'xrange', 'is', 'EnvironmentError', 'KeyError', 'coerce',
+    'SyntaxWarning', 'file', 'in', 'unichr', 'ascii', 'any', 'as', 'if',
+    'OSError', 'DeprecationWarning', 'min', 'UnicodeWarning', 'execfile', 'id',
+    'complex', 'bool', 'ValueError', 'NotImplemented', 'map', 'exec', 'buffer',
+    'max', 'class', 'object', 'repr', 'callable', 'ZeroDivisionError', 'eval',
+    '__debug__', 'ReferenceError', 'AssertionError', 'classmethod',
+    'UnboundLocalError', 'NotImplementedError', 'lambda', 'AttributeError',
+    'OverflowError', 'WindowsError' )
+    
+    import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+    
+    from tornado.escape import json_decode
+from tornado.test.httpserver_test import TypeCheckHandler
+from tornado.testing import AsyncHTTPTestCase
+from tornado.web import RequestHandler, Application
+from tornado.wsgi import WSGIApplication, WSGIContainer, WSGIAdapter
+    
+        def fileno(self):
+        return self.reader.fileno()
+    
+            @gen.coroutine
+        def connect(c):
+            try:
+                yield c.connect(server_addr)
+            except EnvironmentError:
+                pass
+            else:
+                connected_clients.append(c)
+    
+        def call_wrapped(self, count):
+        '''Wraps and calls a function at each level of stack depth
+        to measure the overhead of the wrapped function.
+        '''
+        # This queue is analogous to IOLoop.add_callback, but lets us
+        # benchmark the stack_context in isolation without system call
+        # overhead.
+        queue = collections.deque()
+        self.call_wrapped_inner(queue, count)
+        while queue:
+            queue.popleft()()
