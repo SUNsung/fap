70b41fd33dd457efe9ada5f6051637b6e6523065
SUNsung <weatherius@gmail.com>
2018-04-09 07:03:09 +0300

    Rewrite [index.rb] File in: 2018-04-09 07:03:09

--- ruby/index.rb
+++ ruby/index.rb
@@ -1,64 +1,207 @@
 
         
-            def execute(args)
-      topic_id = args[:topic_id]
-      raise Discourse::InvalidParameters.new(:topic_id) unless topic_id.present?
+            To install Clojure you should install Leiningen:
+      brew install leiningen
+    and then follow the tutorial:
+      https://github.com/technomancy/leiningen/blob/stable/doc/TUTORIAL.md
+    EOS
+  when 'osmium' then <<-EOS.undent
+    The creator of Osmium requests that it not be packaged and that people
+    use the GitHub master branch instead.
+    EOS
+  when 'gfortran' then <<-EOS.undent
+    GNU Fortran is now provided as part of GCC, and can be installed with:
+      brew install gcc
+    EOS
+  when 'play' then <<-EOS.undent
+    Play 2.3 replaces the play command with activator:
+      brew install typesafe-activator
     
-    class ApplicationSerializer < ActiveModel::Serializer
-  embed :ids, include: true
-    
-      included do
-    validate :validate_type
-  end
-    
-      included do
-    self.validate :validate_email_options
-  end
-    
-      def complete_option(method)
-    if self.respond_to? 'complete_#{method}'.to_sym
-      self.send('complete_#{method}'.to_sym)
-    end
-  end
-    
-    Note that you will likely need to have opened the app at least
-once for any login items to be present.
-    
-    # Scheduled processing
-    
-      def as_json(options={})
-    {
-      poll_id:             id,
-      post_id:             status_message.id,
-      question:            question,
-      poll_answers:        poll_answers,
-      participation_count: participation_count
-    }
-  end
-    
-          def validation_fail_as_json(e)
-        render json: {error: :invalid_client_metadata, error_description: e.message}, status: 400
+        keys.each do |key|
+      value = env[key]
+      s = '#{key}: #{value}'
+      case key
+      when 'CC', 'CXX', 'LD'
+        s << ' => #{Pathname.new(value).realpath}' if File.symlink?(value)
       end
-    
-          def accepts?(env)
-        cookie_header = env['HTTP_COOKIE']
-        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
-        cookies.each do |k, v|
-          if k == session_key && Array(v).size > 1
-            bad_cookies << k
-          elsif k != session_key && Rack::Utils.unescape(k) == session_key
-            bad_cookies << k
-          end
-        end
-        bad_cookies.empty?
-      end
-    
-        it 'accepts #{method} requests with whitelisted Origin' do
-      mock_app do
-        use Rack::Protection::HttpOrigin, :origin_whitelist => ['http://www.friend.com']
-        run DummyApp
-      end
-      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://www.friend.com')).to be_ok
+      f.puts s
     end
   end
 end
+
+    
+        if ARGV.named.empty?
+      slow_checks = %w[
+        check_for_broken_symlinks
+        check_missing_deps
+        check_for_outdated_homebrew
+        check_for_linked_keg_only_brews
+      ]
+      methods = (checks.all.sort - slow_checks) + slow_checks
+    else
+      methods = ARGV.named
+    end
+    
+        if initial_revision != current_revision
+      puts 'Updated Homebrew from #{shorten_revision(initial_revision)} to #{shorten_revision(current_revision)}.'
+      updated = true
+    end
+    
+          export JAVA_HOME='$(/usr/libexec/java_home)'
+      export AWS_ACCESS_KEY='<Your AWS Access ID>'
+      export AWS_SECRET_KEY='<Your AWS Secret Key>'
+      export #{home_name}='#{home_value}'
+    EOS
+  end
+end
+
+    
+        def types_as_json
+      @types.values.sort! { |a, b| sort_fn(a.name, b.name) }.map(&:as_json)
+    end
+    
+        def self.join(*args)
+      PARSER.join(*args)
+    end
+    
+            css('.toplang', '#quickview', '.top').remove
+    
+            %w(modals dropdowns scrollspy tabs tooltips popovers alerts buttons collapse carousel affix).each do |dom_id|
+          css('##{dom_id}-options + p + div tbody td:first-child').each do |node|
+            name = node.content.strip
+            id = node.parent['id'] = '#{dom_id}-#{name.parameterize}-option'
+            name.prepend '#{dom_id.singularize.titleize}: '
+            name << ' (option)'
+            entries << [name, id]
+          end
+    
+      def show
+    @tag = Tag.find_by!(name: params[:id].downcase)
+    
+      def initialize(repo: 'twbs/bootstrap', branch: 'master', save_to: {}, cache_path: 'tmp/converter-cache-bootstrap')
+    @logger     = Logger.new
+    @repo       = repo
+    @branch     = branch || 'master'
+    @branch_sha = get_branch_sha
+    @cache_path = cache_path
+    @repo_url   = 'https://github.com/#@repo'
+    @save_to    = {
+        js:    'assets/javascripts/bootstrap',
+        scss:  'assets/stylesheets/bootstrap',
+        fonts: 'assets/fonts/bootstrap'}.merge(save_to)
+  end
+    
+        alias log puts
+    
+      def test_font_helper_with_suffix_sharp
+    assert_match %r(url\(['']?/assets/.*svg#.+['']?\)), @css
+  end
+    
+    
+end
+
+    
+        if extension_pathname.readable?
+      ENV['BUNDLE_GEMFILE'] = extension_pathname.to_path
+      break
+    end
+  end
+end
+    
+    # Copyright (C) 2008 Rapid7, Inc.
+    
+          if(pkt.payload =~ self.sigs[k])
+        matched = k
+        matches = $1
+        sessions[s[:session]].merge!({k => matches})
+      end
+    
+    parser = Parser.new(filename)
+parser.parse
+print parser.get_result
+    
+    if profile_filename = ENV['PROFILE']
+  require 'ruby-prof'
+  reporter =
+    case (profile_extname = File.extname(profile_filename))
+    when '.txt'
+      RubyProf::FlatPrinterWithLineNumbers
+    when '.html'
+      RubyProf::GraphHtmlPrinter
+    when '.callgrind'
+      RubyProf::CallTreePrinter
+    else
+      raise 'Unknown profiler format indicated by extension: #{profile_extname}'
+    end
+  File.open(profile_filename, 'w') do |io|
+    reporter.new(RubyProf.profile { Pod::Command.run(ARGV) }).print(io)
+  end
+else
+  Pod::Command.run(ARGV)
+end
+
+    
+            private
+    
+          def actual_path
+        $PROGRAM_NAME
+      end
+    end
+  end
+end
+
+    
+            # Checks if a template URL is given else returns the TEMPLATE_REPO URL
+        #
+        # @return String
+        #
+        def template_repo_url
+          @template_url || TEMPLATE_REPO
+        end
+      end
+    end
+  end
+end
+
+    
+          def initialize(argv)
+        @update = argv.flag?('update')
+        @stats  = argv.flag?('stats')
+        super
+      end
+    
+    @@ layout
+<html>
+  <head>
+    <title>Super Simple Chat with Sinatra</title>
+    <meta charset='utf-8' />
+    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
+  </head>
+  <body><%= yield %></body>
+</html>
+    
+        it 'denies requests with duplicate session cookies' do
+      get '/', {}, 'HTTP_COOKIE' => 'rack.session=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
+      expect(last_response).not_to be_ok
+    end
+    
+      it 'accepts requests with the same Accept-Language header' do
+    session = {:foo => :bar}
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
+    expect(session).not_to be_empty
+  end
+    
+      it 'allows passing on values in env' do
+    klass    = described_class
+    changer  = Struct.new(:app) do
+      def call(env)
+        env['foo.bar'] = 42
+        app.call(env)
+      end
+    end
+    detector = Struct.new(:app) do
+      def call(env)
+        app.call(env)
+      end
+    end
