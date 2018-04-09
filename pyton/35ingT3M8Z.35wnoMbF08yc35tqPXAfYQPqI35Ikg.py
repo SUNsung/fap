417291d086f3cd6a54b17832d8e3ed49c0ce2c2f
SUNsung <weatherius@gmail.com>
2018-04-09 12:46:58 +0300

    Rewrite [35ingT3M8Z.35wnoMbF08yc35tqPXAfYQPqI35Ikg.py] File in: 2018-04-09 12:46:56

--- pyton/35ingT3M8Z.35wnoMbF08yc35tqPXAfYQPqI35Ikg.py
+++ pyton/35ingT3M8Z.35wnoMbF08yc35tqPXAfYQPqI35Ikg.py
@@ -1,85 +1,150 @@
 
         
-            if s1.st_dev != s2.st_dev:
-        return True     # path/.. on a different device as path
-    if s1.st_ino == s2.st_ino:
-        return True     # path/.. is the same i-node as path, i.e. path=='/'
-    return False
-
+        
+class EnvironmentConfig(object):
+    uri = os.getenv('LANDSCAPE_API_URI')
+    access_key = os.getenv('LANDSCAPE_API_KEY')
+    secret_key = os.getenv('LANDSCAPE_API_SECRET')
+    ssl_ca_file = os.getenv('LANDSCAPE_API_SSL_CA_FILE')
     
-            try:
-            data = json.loads(to_text(return_data.read(), errors='surrogate_or_strict'))
-        except Exception as e:
-            raise AnsibleError('Could not process data from the API server (%s): %s ' % (url, to_native(e)))
+    import os
     
-        def setUp(self):
-        from acme.errors import MissingNonce
-        self.response = mock.MagicMock(headers={})
-        self.response.request.method = 'FOO'
-        self.error = MissingNonce(self.response)
+    # (c) 2016 Red Hat, Inc.
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+'''CLI tool for starting new Shippable CI runs.'''
     
-        def get_all_names_answer(self):
-        '''Returns the set of domain names that the plugin should find'''
-        if self._all_names:
-            return self._all_names
+        results = benchmark([METRICS[k] for k in args.metrics],
+                        [FORMATS[k] for k in args.formats],
+                        args.samples, args.classes, args.density,
+                        args.n_times)
+    
+    
+def variable_batch_size_comparison(data):
+    batch_sizes = [i.astype(int) for i in np.linspace(data.shape[0] // 10,
+                                                      data.shape[0], num=10)]
+    
+                gc.collect()
+            print('benchmarking lasso_path (with Gram):', end='')
+            sys.stdout.flush()
+            tstart = time()
+            lasso_path(X, y, precompute=True)
+            delta = time() - tstart
+            print('%0.3fs' % delta)
+            results['lasso_path (with Gram)'].append(delta)
+    
+        if revision is None:
+        return
+    if domain not in ('py', 'pyx'):
+        return
+    if not info.get('module') or not info.get('fullname'):
+        return
+    
+    try:
+    from urllib import urlopen
+except ImportError:
+    from urllib.request import urlopen
+    
+    # Plot the results (= shape of the data points cloud)
+plt.figure(1)  # two clusters
+plt.title('Outlier detection on a real data set (boston housing)')
+plt.scatter(X1[:, 0], X1[:, 1], color='black')
+bbox_args = dict(boxstyle='round', fc='0.8')
+arrow_args = dict(arrowstyle='->')
+plt.annotate('several confounded points', xy=(24, 19),
+             xycoords='data', textcoords='data',
+             xytext=(13, 10), bbox=bbox_args, arrowprops=arrow_args)
+plt.xlim((xx1.min(), xx1.max()))
+plt.ylim((yy1.min(), yy1.max()))
+plt.legend((legend1_values_list[0].collections[0],
+            legend1_values_list[1].collections[0],
+            legend1_values_list[2].collections[0]),
+           (legend1_keys_list[0], legend1_keys_list[1], legend1_keys_list[2]),
+           loc='upper center',
+           prop=matplotlib.font_manager.FontProperties(size=12))
+plt.ylabel('accessibility to radial highways')
+plt.xlabel('pupil-teacher ratio by town')
+    
+        def get_testable_domain_names(self):
+        '''Returns the set of domain names that can be tested against'''
+        if self._test_names:
+            return self._test_names
         else:
-            raise errors.Error('No configuration file loaded')
+            return {'example.com'}
     
-    # Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named 'default.css' will overwrite the builtin 'default.css'.
-html_static_path = ['_static']
+        @classmethod
+    def _call(cls, enhancement):
+        from certbot.display.enhancements import ask
+        return ask(enhancement)
     
-    import sys
-import os
-import shlex
     
-    site_info = 'FC2Video'
-download = fc2video_download
-download_playlist = playlist_not_supported('fc2video')
+class NotifyTests(unittest.TestCase):
+    '''Tests for the notifier.'''
+    
+        def test_car_adapter_shall_make_loud_noise(self):
+        car = Car()
+        car_adapter = Adapter(car, make_noise=car.make_noise)
+        noise = car_adapter.make_noise(1)
+        expected_noise = 'vroom!'
+        self.assertEqual(noise, expected_noise)
+    
+        @classmethod
+    def setUpClass(cls):
+        cls.dec_obs = DecimalViewer()
+        cls.hex_obs = HexViewer()
+        cls.sub = Data('Data')
+        # inherited behavior already tested with TestSubject
+        cls.sub.attach(cls.dec_obs)
+        cls.sub.attach(cls.hex_obs)
+    
+        def test_bear_greek_localization(self):
+        self.assertEqual(self.g.get('bear'), 'bear')
 
     
-    
-def huaban_download(url, output_dir='.', **kwargs):
-    if re.match(r'http://huaban\.com/boards/\d+/', url):
-        huaban_download_board(url, output_dir, **kwargs)
-    else:
-        print('Only board (画板) pages are supported currently')
-        print('ex: http://huaban.com/boards/12345678/')
-    
-    def kugou_download_playlist(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    html=get_html(url)
-    pattern=re.compile('title='(.*?)'.* data='(\w*)\|.*?'')
-    pairs=pattern.findall(html)
-    for title,hash_val in pairs:
-        kugou_download_by_hash(title,hash_val,output_dir,merge,info_only)
-    
-    def kuwo_playlist_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    html=get_content(url)
-    matched=set(re.compile('yinyue/(\d+)').findall(html))#reduce duplicated
-    for rid in matched:
-        kuwo_download_by_rid(rid,output_dir,merge,info_only)
-    
-    api('subredditstylesheet', StylesheetTemplate)
-api('subredditstylesheetsource', StylesheetTemplate)
-api('createsubreddit', SubredditSettingsTemplate)
-api('uploadedimage', UploadedImageJsonTemplate)
-    
-            To request a new CAPTCHA,
-        use [/api/new_captcha](#POST_api_new_captcha).
+        def test_display_current_time_at_current_time(self):
         '''
-        image = captcha.get_image(iden)
-        f = StringIO.StringIO()
-        image.save(f, 'PNG')
-        response.content_type = 'image/png;'
-        return f.getvalue()
+        Just as justification for working example. (Will always pass.)
+        '''
+        production_code_time_provider = ProductionCodeTimeProvider()
+        class_under_test = TimeDisplay(production_code_time_provider)
+        current_time = datetime.datetime.now()
+        expected_time = '<span class=\'tinyBoldText\'>{}:{}</span>'.format(current_time.hour, current_time.minute)
+        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
     
-
+        def _handle(self, request):
+        if 0 < request <= 10:
+            print('request {} handled in handler 1'.format(request))
+            return True
     
-            # c.error_page is special-cased in a couple places to bypass
-        # c.site checks. We shouldn't allow the user to get here other
-        # than through `middleware.py:error_mapper`.
-        if not request.environ.get('pylons.error_call'):
-            abort(403, 'direct access to error controller disallowed')
     
-            c.allow_framing = True
+if __name__ == '__main__':
+    reporter = Reporter()
+    db = DB()
+    tm = TestManager()
+    tm.setReporter(reporter)
+    tm.setDB(db)
+    reporter.setTM(tm)
+    db.setTM(tm)
+    # For simplification we are looping on the same test.
+    # Practically, it could be about various unique test classes and their
+    # objects
+    for i in range(3):
+        tc = TC()
+        tc.setTM(tm)
+        tm.setTC(tc)
+        tc.setup()
+        tc.execute()
+        tc.tearDown()
