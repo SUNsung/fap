49e32dd80bb9fc4a55c5eb6ab2ee81cd3d7abddd
SUNsung <weatherius@gmail.com>
2018-04-10 00:41:27 +0300

    Rewrite [34PLB0sJVoLVQ34p1GX8l.ao.634ZsW9pDQdSQo34Qkh.cpp] File in: 2018-04-10 00:41:27

--- cccccc/34PLB0sJVoLVQ34p1GX8l.ao.634ZsW9pDQdSQo34Qkh.cpp
+++ cccccc/34PLB0sJVoLVQ34p1GX8l.ao.634ZsW9pDQdSQo34Qkh.cpp
@@ -1,142 +1,232 @@
 
         
-          // DesktopMediaListObserver overrides.
-  void OnSourceAdded(int index) override;
-  void OnSourceRemoved(int index) override;
-  void OnSourceMoved(int old_index, int new_index) override;
-  void OnSourceNameChanged(int index) override;
-  void OnSourceThumbnailChanged(int index) override;
-  bool OnRefreshFinished() override;
-    
-      AcceleratorCallbackMap accelerator_callback_map_;
-    
-    
-    {  content::WebContents* web_contents_;  // weak
-  SavePageCallback callback_;
-};
-    
-    
-    {}  // namespace atom
-    
-    #if defined(OS_WIN)
-StringType GetWaitEventName(base::ProcessId pid);
-    
-    MenuModelAdapter::MenuModelAdapter(AtomMenuModel* menu_model)
-  : views::MenuModelAdapter(menu_model),
-    menu_model_(menu_model) {
-}
-    
-    #ifndef ATOM_BROWSER_UI_VIEWS_NATIVE_FRAME_VIEW_H_
-#define ATOM_BROWSER_UI_VIEWS_NATIVE_FRAME_VIEW_H_
-    
-      /// Access an array element (zero based index ).
-  /// If the array contains less than index element, then null value are
-  /// inserted
-  /// in the array so that its size is index+1.
-  /// (You may need to say 'value[0u]' to get your compiler to distinguish
-  ///  this from the operator[] which takes a string.)
-  Value& operator[](ArrayIndex index);
-    
-    TEST(AnyTest, TestIs) {
-  protobuf_unittest::TestAny submessage;
-  submessage.set_int32_value(12345);
-  google::protobuf::Any any;
-  any.PackFrom(submessage);
-  ASSERT_TRUE(any.ParseFromString(any.SerializeAsString()));
-  EXPECT_TRUE(any.Is<protobuf_unittest::TestAny>());
-  EXPECT_FALSE(any.Is<google::protobuf::Any>());
-    }
-    
-    #include <google/protobuf/compiler/code_generator.h>
-#include <google/protobuf/compiler/csharp/csharp_source_generator_base.h>
-    
-      virtual void WriteHash(io::Printer* printer);
-  virtual void WriteEquals(io::Printer* printer);
-  virtual void WriteToString(io::Printer* printer);
-    
-      virtual void GenerateCloningCode(io::Printer* printer);
-  virtual void GenerateFreezingCode(io::Printer* printer);
-  virtual void GenerateMembers(io::Printer* printer);
-  virtual void GenerateMergingCode(io::Printer* printer);
-  virtual void GenerateParsingCode(io::Printer* printer);
-  virtual void GenerateSerializationCode(io::Printer* printer);
-  virtual void GenerateSerializedSizeCode(io::Printer* printer);
-    
-    // Author: kenton@google.com (Kenton Varda)
-//  Based on original Protocol Buffers design by
-//  Sanjay Ghemawat, Jeff Dean, and others.
-//
-// Generates Java code for a given .proto file.
-    
-    
-    {}  // namespace google
-#endif  // GOOGLE_PROTOBUF_COMPILER_JAVA_GENERATOR_FACTORY_H__
+        
+    {}
 
     
-    void ImmutableLazyMessageFieldGenerator::
-GenerateSerializedSizeCode(io::Printer* printer) const {
-  printer->Print(variables_,
-    'if ($get_has_field_bit_message$) {\n'
-    '  size += com.google.protobuf.CodedOutputStream\n'
-    '    .computeLazyFieldSize($number$, $name$_);\n'
-    '}\n');
+    ELISTIZEH(PARA)
+    
+    
+    {}  // namespace tesseract.
+    
+    #include 'ccutil.h'
+#include 'errcode.h'
+#include 'genericvector.h'
+#include 'tesscallback.h'
+    
+    // Initialize the table to the given size of feature space.
+void IntFeatureDist::Init(const IntFeatureMap* feature_map) {
+  size_ = feature_map->sparse_size();
+  Clear();
+  feature_map_ = feature_map;
+  features_ = new bool[size_];
+  features_delta_one_ = new bool[size_];
+  features_delta_two_ = new bool[size_];
+  memset(features_, false, size_ * sizeof(features_[0]));
+  memset(features_delta_one_, false, size_ * sizeof(features_delta_one_[0]));
+  memset(features_delta_two_, false, size_ * sizeof(features_delta_two_[0]));
+  total_feature_weight_ = 0.0;
 }
     
-    
-    {  if (rv == 0) {
-    return FutexResult::AWOKEN;
-  } else {
-    switch(errno) {
-      case ETIMEDOUT:
-        assert(timeout != nullptr);
-        return FutexResult::TIMEDOUT;
-      case EINTR:
-        return FutexResult::INTERRUPTED;
-      case EWOULDBLOCK:
-        return FutexResult::VALUE_CHANGED;
-      default:
-        assert(false);
-        // EINVAL, EACCESS, or EFAULT.  EINVAL means there was an invalid
-        // op (should be impossible) or an invalid timeout (should have
-        // been sanitized by timeSpecFromTimePoint).  EACCESS or EFAULT
-        // means *addr points to invalid memory, which is unlikely because
-        // the caller should have segfaulted already.  We can either
-        // crash, or return a value that lets the process continue for
-        // a bit. We choose the latter. VALUE_CHANGED probably turns the
-        // caller into a spin lock.
-        return FutexResult::VALUE_CHANGED;
+    static void RunQPS() {
+  gpr_log(GPR_INFO, 'Running QPS test, open-loop');
     }
+    
+    namespace routeguide {
+    }
+    
+    namespace routeguide {
+class Feature;
+    }
+    
+    template <class T, class F>
+double sum(const T& container, F functor) {
+  double r = 0;
+  for (auto v = container.begin(); v != container.end(); v++) {
+    r += functor(*v);
+  }
+  return r;
+}
+    
+    #include <initializer_list>
+#include <string>
+#include <vector>
+    
+        // Taken from RS4
+    REGISTER_OPERATOR_SCHEMA(Softsign)
+        .Description('Softsign takes one input data (Tensor<T>) and produces one output '
+            'data (Tensor<T>) where the function, y = x / (1 + abs(x)), is applied to the '
+            'tensor elementwise.')
+        .Input('input', 'Input tensor, typically 1-D.', 'T')
+        .Output('output', 'Output tensor of same shape and type as input X.', 'T')
+        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
+            'Constrain input and output types to float tensors.');
+    
+        //‘GREATER’, ‘LESS’, ‘EQUALS,
+    REGISTER_BINARY_COMPARISON_OPERATOR_SCHEMA(Greater)
+        REGISTER_BINARY_COMPARISON_OPERATOR_SCHEMA(Less)
+        REGISTER_BINARY_COMPARISON_OPERATOR_SCHEMA(Equal)
+    
+    
+    {    private:
+        // test if data is in memory at the moment
+        bool IsInRam() const
+        {
+            return !m_frames.empty();
+        }
+};
+    
+    template <class ElemType>
+void ReaderShim<ElemType>::SetCurrentSamplePosition(size_t currentSamplePosition)
+{
+    if (GetCurrentSamplePosition() == currentSamplePosition)
+        return;
+    }
+    
+    	if (cmderOptions.registerApp == true) {
+		RegisterShellMenu(cmderOptions.cmderRegScope, SHELL_MENU_REGISTRY_PATH_BACKGROUND);
+		RegisterShellMenu(cmderOptions.cmderRegScope, SHELL_MENU_REGISTRY_PATH_LISTITEM);
+	}
+	else if (cmderOptions.unRegisterApp == true)
+	{
+		UnregisterShellMenu(cmderOptions.cmderRegScope, SHELL_MENU_REGISTRY_PATH_BACKGROUND);
+		UnregisterShellMenu(cmderOptions.cmderRegScope, SHELL_MENU_REGISTRY_PATH_LISTITEM);
+	}
+	else if (cmderOptions.error == true)
+	{
+		return 1;
+	}
+	else
+	{
+		StartCmder(cmderOptions.cmderStart, cmderOptions.cmderSingle, cmderOptions.cmderTask, cmderOptions.cmderCfgRoot);
+	}
+    
+    #define STB__TRY(t,p)  /* avoid retrying a match we already tried */ \
+    if (p ? dist != q-t : 1)                             \
+    if ((m = stb_matchlen(t, q, match_max)) > best)     \
+    if (stb__nc(m,q-(t)))                                \
+    best = m, dist = q - (t)
+    
+            if (ImGui::Button('Button'))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text('counter = %d', counter);
+    
+    // Use if you want to reset your rendering device without losing ImGui state.
+IMGUI_API void        ImGui_ImplDX10_InvalidateDeviceObjects();
+IMGUI_API bool        ImGui_ImplDX10_CreateDeviceObjects();
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you use this binding you'll need to call 4 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXXX_NewFrame(), ImGui::Render() and ImGui_ImplXXXX_Shutdown().
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you use this binding you'll need to call 4 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXXX_NewFrame(), ImGui::Render() and ImGui_ImplXXXX_Shutdown().
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+    //---- Avoid multiple STB libraries implementations, or redefine path/filenames to prioritize another version
+// By default the embedded implementations are declared static and not available outside of imgui cpp files.
+//#define IMGUI_STB_TRUETYPE_FILENAME   'my_folder/stb_truetype.h'
+//#define IMGUI_STB_RECT_PACK_FILENAME  'my_folder/stb_rect_pack.h'
+//#define IMGUI_DISABLE_STB_TRUETYPE_IMPLEMENTATION
+//#define IMGUI_DISABLE_STB_RECT_PACK_IMPLEMENTATION
+    
+    // CHANGELOG
+// (minor and older changes stripped away, please see git history for details)
+//  2018-03-20: Misc: Setup io.BackendFlags ImGuiBackendFlags_HasMouseCursors flag + honor ImGuiConfigFlags_NoMouseCursorChange flag.
+//  2018-02-16: Inputs: Added support for mouse cursors, honoring ImGui::GetMouseCursor() value.
+//  2018-02-16: Misc: Obsoleted the io.RenderDrawListsFn callback and exposed ImGui_ImplSdlGL2_RenderDrawData() in the .h file so you can call it yourself.
+//  2018-02-06: Misc: Removed call to ImGui::Shutdown() which is not available from 1.60 WIP, user needs to call CreateContext/DestroyContext themselves.
+//  2018-02-06: Inputs: Added mapping for ImGuiKey_Space.
+//  2018-02-05: Misc: Using SDL_GetPerformanceCounter() instead of SDL_GetTicks() to be able to handle very high framerate (1000+ FPS).
+//  2018-02-05: Inputs: Keyboard mapping is using scancodes everywhere instead of a confusing mixture of keycodes and scancodes. 
+//  2018-01-20: Inputs: Added Horizontal Mouse Wheel support.
+//  2018-01-19: Inputs: When available (SDL 2.0.4+) using SDL_CaptureMouse() to retrieve coordinates outside of client area when dragging. Otherwise (SDL 2.0.3 and before) testing for SDL_WINDOW_INPUT_FOCUS instead of SDL_WINDOW_MOUSE_FOCUS.
+//  2018-01-18: Inputs: Added mapping for ImGuiKey_Insert.
+//  2017-09-01: OpenGL: Save and restore current polygon mode.
+//  2017-08-25: Inputs: MousePos set to -FLT_MAX,-FLT_MAX when mouse is unavailable/missing (instead of -1,-1).
+//  2016-10-15: Misc: Added a void* user_data parameter to Clipboard function handlers.
+//  2016-09-05: OpenGL: Fixed save and restore of current scissor rectangle.
+//  2016-07-29: OpenGL: Explicitly setting GL_UNPACK_ROW_LENGTH to reduce issues because SDL changes it. (#752)
+    
+    
+    {    return true;
+}
+    
+        // Create Descriptor Set:
+    {
+        VkDescriptorSetAllocateInfo alloc_info = {};
+        alloc_info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+        alloc_info.descriptorPool = g_DescriptorPool;
+        alloc_info.descriptorSetCount = 1;
+        alloc_info.pSetLayouts = &g_DescriptorSetLayout;
+        err = vkAllocateDescriptorSets(g_Device, &alloc_info, &g_DescriptorSet);
+        ImGui_ImplGlfwVulkan_VkResult(err);
+    }
+    
+            // 3. Show the ImGui demo window. Most of the sample code is in ImGui::ShowDemoWindow(). Read its code to learn more about Dear ImGui!
+        if (show_demo_window)
+        {
+            ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver); // Normally user code doesn't need/want to call this because positions are saved in .ini file anyway. Here we just want to make the demo initial state a bit more friendly!
+            ImGui::ShowDemoWindow(&show_demo_window);
+        }
+    
+    /**
+ * Check whether 'path' starts with 'prefix'.
+ * That is, if prefix has n path elements, then the first n elements of
+ * path must be the same as prefix.
+ *
+ * There is a special case if prefix ends with a slash:
+ * /foo/bar/ is not a prefix of /foo/bar, but both /foo/bar and /foo/bar/
+ * are prefixes of /foo/bar/baz.
+ */
+bool starts_with(const path& p, const path& prefix);
+    
+    
+    {  size_t foundCompleted = 0;
+  for (auto& op : ops) {
+    if (op->state() == AsyncIOOp::State::COMPLETED) {
+      ++foundCompleted;
+    } else {
+      EXPECT_TRUE(op->state() == AsyncIOOp::State::CANCELED) << *op;
+    }
+  }
+  EXPECT_EQ(foundCompleted, completed);
+}
+
+    
+    
+    {} // namespace folly
+
+    
+      /**
+   * Get the output file.
+   */
+  const folly::File& getFile() const {
+    return file_;
+  }
+    
+    
+    {  // Propagate the message up to our parent LogCategory.
+  //
+  // Maybe in the future it might be worth adding a flag to control if a
+  // LogCategory should propagate messages to its parent or not.  (This would
+  // be similar to log4j's 'additivity' flag.)
+  // For now I don't have a strong use case for this.
+  if (parent_) {
+    parent_->processMessage(message);
   }
 }
     
-    #include <folly/Chrono.h>
-#include <folly/portability/GTest.h>
-#include <folly/portability/Time.h>
-    
-    
-    
-      size_t i = nextAlignedIndex(haystack.data());
-  for (; i < haystack.size(); i += 16) {
-    ret = scanHaystackBlock<true>(haystack, needles, i);
-    if (ret != std::string::npos) {
-      return ret;
+    namespace folly {
     }
-  }
     
-    
-    {    // An object containing some of these keys:
-    //   'key_errors' -- {'key': 'description of error looking up said key'}
-    //   'error' -- why did we fail to parse this value?
-    //   'value' -- a copy of the input causing the error, and
-    //   'nested' -- {'key' or integer for arrays: <another errors_ object>}
-    //
-    // 'nested' will contain identically structured objects with keys (array
-    // indices) identifying the origin of the errors.  Of course, 'input'
-    // would no longer refer to the whole input, but to a part.
-    folly::dynamic errors_;
-    // We only materialize errors_ sub-objects when needed. This stores keys
-    // for unmaterialized errors, from outermost to innermost.
-    std::vector<const folly::dynamic*> unmaterializedSubErrorKeys_;
-    // Materialized errors, from outermost to innermost
-    std::vector<folly::dynamic*> subErrors_;  // Point into errors_
-  };
+    /**
+ * Parse a folly::dynamic object.
+ *
+ * The input should be an object data type, and is parsed the same as a JSON
+ * object accpted by parseLogConfigJson().
+ */
+LogConfig parseLogConfigDynamic(const dynamic& value);
