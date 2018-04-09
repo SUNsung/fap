cc09b3c0ade53fff0cafe03c95c83e456ad71b5c
SUNsung <weatherius@gmail.com>
2018-04-10 01:15:11 +0300

    Rewrite [12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb] File in: 2018-04-10 01:15:10

--- ruby/12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb
+++ ruby/12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb
@@ -1,95 +1,155 @@
 
         
-          def test_escape_javascript
-    assert_equal '', escape_javascript(nil)
-    assert_equal %(This \\'thing\\' is really\\n netos\\'), escape_javascript(%(This 'thing' is really\n netos'))
-    assert_equal %(backslash\\\\test), escape_javascript(%(backslash\\test))
-    assert_equal %(dont <\\/close> tags), escape_javascript(%(dont </close> tags))
-    assert_equal %(unicode &#x2028; newline), escape_javascript(%(unicode \342\200\250 newline).dup.force_encoding(Encoding::UTF_8).encode!)
-    assert_equal %(unicode &#x2029; newline), escape_javascript(%(unicode \342\200\251 newline).dup.force_encoding(Encoding::UTF_8).encode!)
+                    @adapter = adapter
+            @event_loop = event_loop
     
-            def test_url_invalid_adapter
-          error = assert_raises(LoadError) do
-            spec 'ridiculous://foo?encoding=utf8'
-          end
-    
-      test 'helpers' do
-    assert_response_code_range 200..299, :successful?
-    assert_response_code_range [404],    :not_found?
-    assert_response_code_range 300..399, :redirection?
-    assert_response_code_range 500..599, :server_error?
-    assert_response_code_range 400..499, :client_error?
+      def test_helper_method
+    assert_nothing_raised { @controller_class.helper_method :delegate_method }
+    assert_includes master_helper_methods, :delegate_method
   end
     
-      class HeadTest < ActiveSupport::TestCase
-    test 'head works on its own' do
-      status = HeadController.action(:index).call(Rack::MockRequest.env_for('/')).first
-      assert_equal 404, status
+        def call(env)
+      result = @app.call(env)
+      result[1]['Middleware-Order'] += '!'
+      result
     end
+  end
     
-          add_delivery_method :smtp, Mail::SMTP,
-        address:              'localhost',
-        port:                 25,
-        domain:               'localhost.localdomain',
-        user_name:            nil,
-        password:             nil,
-        authentication:       nil,
-        enable_starttls_auto: true
+          # Adds a new delivery method through the given class using the given
+      # symbol as alias and the default options supplied.
+      #
+      #   add_delivery_method :sendmail, Mail::Sendmail,
+      #     location:  '/usr/sbin/sendmail',
+      #     arguments: '-i'
+      def add_delivery_method(symbol, klass, default_options = {})
+        class_attribute(:'#{symbol}_settings') unless respond_to?(:'#{symbol}_settings')
+        send(:'#{symbol}_settings=', default_options)
+        self.delivery_methods = delivery_methods.merge(symbol.to_sym => klass).freeze
+      end
     
-            def application_mailer_file_name
-          @_application_mailer_file_name ||= if mountable_engine?
-            'app/mailers/#{namespaced_path}/application_mailer.rb'
+    module ActionMailer
+  # Provides helper methods for ActionMailer::Base that can be used for easily
+  # formatting messages, accessing mailer or message instances, and the
+  # attachments list.
+  module MailHelper
+    # Take the text and format it, indented two spaces for each line, and
+    # wrapped at 72 columns:
+    #
+    #   text = <<-TEXT
+    #     This is
+    #     the      paragraph.
+    #
+    #     * item1 * item2
+    #   TEXT
+    #
+    #   block_format text
+    #   # => '  This is the paragraph.\n\n  * item1\n  * item2\n'
+    def block_format(text)
+      formatted = text.split(/\n\r?\n/).collect { |paragraph|
+        format_paragraph(paragraph)
+      }.join('\n\n')
+    
+            def mailer_class
+          if mailer = _mailer_class
+            mailer
           else
-            'app/mailers/application_mailer.rb'
+            tests determine_default_mailer(name)
           end
         end
+    
+          it 'splits correctly' do
+        expected = [
+          'One',
+          'Two',
+          'Three',
+          'Four Token',
+          'Or',
+          'Newlines',
+          'Everywhere'
+        ]
+        expect(generator.split_keywords(keywords)).to eq(expected)
+      end
     end
   end
 end
 
     
-          it 'does set the output directory' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            cloc(output_directory: '/tmp')
-          end').runner.execute(:test)
+      def use_sandbox?
+    ENV['USE_EVERNOTE_SANDBOX'] == 'true'
+  end
     
-        def ==(other)
-      other.is_a?(self.class) && filters == other.filters
-    end
-    
-        def as_json
-      { name: name, path: path, type: type }
-    end
+      def load_event
+    @event = current_user.events.find(params[:id])
   end
 end
 
     
-        def initialize
-      @pages = {}
+          respond_to do |format|
+        if new_credentials.map(&:save).all?
+          format.html { redirect_to user_credentials_path, notice: 'The file was successfully uploaded.'}
+        else
+          format.html { redirect_to user_credentials_path, notice: 'One or more of the uploaded credentials was not imported due to an error. Perhaps an existing credential had the same name?'}
+        end
+      end
+    else
+      redirect_to user_credentials_path, notice: 'No file was chosen to be uploaded.' 
+    end
+  end
+    
+                -- Finally, add our sequence number to our base, and chop
+            -- it to the last two bytes
+            tail := (
+              (sequence_base + nextval(table_name || '_id_seq'))
+              & 65535);
+    
+      subject { described_class.new }
+    
+      def text_url
+    object.local? ? medium_url(object) : nil
+  end
+    
+      def set_body_classes
+    @body_classes = 'tag-body'
+  end
+    
+      def as_json(options={})
+    {
+      poll_id:             id,
+      post_id:             status_message.id,
+      question:            question,
+      poll_answers:        poll_answers,
+      participation_count: participation_count
+    }
+  end
+    
+        unless user
+      EmailInviter.new(email, inviter).send!
+      flash[:notice] = 'invitation sent to #{email}'
+    else
+      flash[:notice]= 'error sending invite to #{email}'
+    end
+    redirect_to user_search_path, :notice => flash[:notice]
+  end
+    
+          if post_processing
+        post_process(*only_process)
+      end
     end
     
-          def additional_entries
-        entries = []
-    
-            css('.method-signature', 'pre').each do |node|
-          node.name = 'pre'
-          node.content = node.content.strip
-          node['data-language'] = 'php'
+          class HaveAttachedFileMatcher
+        def initialize attachment_name
+          @attachment_name = attachment_name
         end
     
-    module RuboCop
-  module Cop
-    class VariableForce
-      # This class represents each assignment of a variable.
-      class Assignment
-        include Branchable
-    
-            def each_unnecessary_space_match(node, &blk)
-          each_match_range(
-            contents_range(node),
-            MULTIPLE_SPACES_BETWEEN_ITEMS_REGEX,
-            &blk
-          )
+            def no_error_when_valid?
+          @file = StringIO.new('.')
+          @subject.send(@attachment_name).assign(@file)
+          @subject.valid?
+          expected_message = [
+            @attachment_name.to_s.titleize,
+            I18n.t(:blank, scope: [:errors, :messages])
+          ].join(' ')
+          @subject.errors.full_messages.exclude?(expected_message)
         end
       end
     end
@@ -97,31 +157,5 @@ end
 end
 
     
-    module RuboCop
-  module Cop
-    module Lint
-      # In math and Python, we can use `x < y < z` style comparison to compare
-      # multiple value. However, we can't use the comparison in Ruby. However,
-      # the comparison is not syntax error. This cop checks the bad usage of
-      # comparison operators.
-      #
-      # @example
-      #
-      #   # bad
-      #
-      #   x < y < z
-      #   10 <= x <= 20
-      #
-      # @example
-      #
-      #   # good
-      #
-      #   x < y && y < z
-      #   10 <= x && x <= 20
-      class MultipleCompare < Cop
-        MSG = 'Use the `&&` operator to compare multiple values.'.freeze
-    
-          expect('.prefix--moz-ms').to have_ruleset(rule)
-    end
-  end
-end
+    module Paperclip
+  require 'rails'
