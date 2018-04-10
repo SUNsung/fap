009f28d3ca4dcd029007e61b62521d86951cdddb
SUNsung <weatherius@gmail.com>
2018-04-10 06:57:58 +0300

    Rewrite [35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb] File in: 2018-04-10 06:57:57

--- ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
+++ ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
@@ -1,105 +1,44 @@
 
         
-              message = TestMailer.send_test(args[:to_address])
-      Email::Sender.new(message, :test_message).send
+          test 'helpers' do
+    assert_response_code_range 200..299, :successful?
+    assert_response_code_range [404],    :not_found?
+    assert_response_code_range 300..399, :redirection?
+    assert_response_code_range 500..599, :server_error?
+    assert_response_code_range 400..499, :client_error?
+  end
+    
+        def env_for(url)
+      Rack::MockRequest.env_for(url)
     end
+  end
     
-        # Gets the last git commit information formatted into a String by the provided
-    # pretty format String. See the git-log documentation for valid format placeholders
-    def self.last_git_commit_formatted_with(pretty_format, date_format = nil)
-      command = ['git log -1']
-      command << '--pretty=\'#{pretty_format}\''
-      command << '--date=\'#{date_format}\'' if date_format
-      Actions.sh(command.compact.join(' '), log: false).chomp
-    rescue
-      nil
-    end
+          add_delivery_method :smtp, Mail::SMTP,
+        address:              'localhost',
+        port:                 25,
+        domain:               'localhost.localdomain',
+        user_name:            nil,
+        password:             nil,
+        authentication:       nil,
+        enable_starttls_auto: true
     
-      private
-    
-      # Finds the projects '@user' contributed to, limited to either public projects
-  # or projects visible to the given user.
-  #
-  # current_user - When given the list of the projects is limited to those only
-  #                visible by this user.
-  #
-  # Returns an ActiveRecord::Relation.
-  def execute(current_user = nil)
-    segments = all_projects(current_user)
-    
-      # Finds the groups of the source user, optionally limited to those visible to
-  # the current user.
-  def execute(current_user = nil)
-    segments = all_groups(current_user)
-    
-      def all_projects(current_user)
-    projects = []
-    
-    module Docs
-  class PageDb
-    attr_reader :pages
-    
-          def get_type
-        case slug
-        when 'api'
-          'Reference'
-        when 'configuration'
-          'Reference: Configuration'
-        when 'stpl'
-          'Reference: SimpleTemplate'
-        when 'plugindev'
-          'Reference: Plugin'
-        else
-          'Manual'
-        end
+    module ActionMailer
+  # Implements the ActiveSupport::LogSubscriber for logging notifications when
+  # email is delivered or received.
+  class LogSubscriber < ActiveSupport::LogSubscriber
+    # An email was delivered.
+    def deliver(event)
+      info do
+        recipients = Array(event.payload[:to]).join(', ')
+        'Sent mail to #{recipients} (#{event.duration.round(1)}ms)'
       end
     
-            # Move dummy anchor to method and property name
+      def full_plugin_name
+    @full_plugin_name ||= 'logstash-#{type}-#{name.downcase}'
+  end
+end
+
     
-            rows.each do |row|
-          line = row.values.each_with_index.map do |value, col|
-            value.to_s.ljust(col_widths[col])
-          end.join(' ').rstrip
-          line = color.colorize(line, row.color) if row.color
-          puts line
-        end
-      end
-    
-        def ensure_stage
-      Rake::Task.define_task(:ensure_stage) do
-        unless stage_set?
-          puts t(:stage_not_set)
-          exit 1
-        end
-      end
-    end
-    
-            def destroy
-          authorize! :destroy, stock_location
-          stock_location.destroy
-          respond_with(stock_location, status: 204)
-        end
-    
-            def create
-          authorize! :create, StockMovement
-          @stock_movement = scope.new(stock_movement_params)
-          if @stock_movement.save
-            respond_with(@stock_movement, status: 201, default_template: :show)
-          else
-            invalid_resource!(@stock_movement)
+              def find_plugins_gem_specs
+            @specs ||= ::Gem::Specification.find_all.select{|spec| logstash_plugin_gem_spec?(spec)}
           end
-        end
-    
-    
-require 'uri'
-require 'cgi'
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'support', 'paths'))
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'support', 'selectors'))
-    
-        EXIF_ROTATED_ORIENTATION_VALUES = [5, 6, 7, 8]
-    
-            def matches? subject
-          @subject = subject
-          @subject = @subject.new if @subject.class == Class
-          lower_than_low? && higher_than_low? && lower_than_high? && higher_than_high?
-        end
