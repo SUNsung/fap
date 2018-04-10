995eca9b97c891cf88d7aee88d8a3ba0a2d5660f
SUNsung <weatherius@gmail.com>
2018-04-10 20:01:19 +0300

    Rewrite [233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py] File in: 2018-04-10 20:01:18

--- pyton/233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py
+++ pyton/233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py
@@ -1,127 +1,54 @@
 
         
-        versions_info['versions'][version] = new_version
-versions_info['latest'] = version
+        from mrjob.job import MRJob
     
-        def to_screen(self, s, skip_eol=None):
-        print(s)
-    
-        def test_secondary_proxy_http(self):
-        params = self._check_params(['secondary_proxy', 'secondary_server_ip'])
-        if params is None:
-            return
-        ydl = FakeYDL()
-        req = compat_urllib_request.Request('http://yt-dl.org/ip')
-        req.add_header('Ytdl-request-proxy', params['secondary_proxy'])
-        self.assertEqual(
-            ydl.urlopen(req).read().decode('utf-8'),
-            params['secondary_server_ip'])
-    
-            webpage = self._download_webpage(url, playlist_id)
-        title = self._html_search_regex(
-            r'<h1 class='playlist-name'[^>]*?>(.*?)</h1>', webpage, 'title')
-        description = self._html_search_regex(
-            r'<p class='excerpt'[^>]*?>(.*?)</p>',
-            webpage, 'description', fatal=False)
-        urls = re.findall(
-            r'<li class='lecture-preview'>\s*?<a target='_blank' href='([^']+)'>',
-            webpage)
-        entries = [self.url_result(u) for u in urls]
-    
-            page = self._download_json(
-            'http://vxml.56.com/json/%s/' % text_id, text_id, 'Downloading video info')
+        def mapper(self, _, line):
+        yield line, 1
     
     
-@pytest.fixture(params=containers)
-def proc(request, spawnu, TIMEOUT):
-    proc = spawnu(*request.param)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(u'thefuck --alias > ~/.config/fish/config.fish')
-    proc.sendline(u'fish')
-    return proc
+class Crawler(object):
     
+        def move_to_front(self, node):
+        ...
     
-class _BenchSpider(scrapy.Spider):
-    '''A spider that follows all links'''
-    name = 'follow'
-    total = 10000
-    show = 20
-    baseurl = 'http://localhost:8998'
-    link_extractor = LinkExtractor()
+        if WSAStringToAddressA(
+            ip_string,
+            address_family,
+            None,
+            ctypes.byref(addr),
+            ctypes.byref(addr_size)
+    ) != 0:
+        raise socket.error(ctypes.FormatError())
     
+    ## Anything on different channel than DEFAULT_CHANNEL is not parsed
+# by parser.
+HIDDEN_CHANNEL = 99
     
-def sanitize_module_name(module_name):
-    '''Sanitize the given module name, by replacing dashes and points
-    with underscores and prefixing it with a letter if it doesn't start
-    with one
-    '''
-    module_name = module_name.replace('-', '_').replace('.', '_')
-    if module_name[0] not in string.ascii_letters:
-        module_name = 'a' + module_name
-    return module_name
+        def __str__(self):
+        #return 'MismatchedTokenException('+self.expecting+')'
+        return 'MismatchedTokenException(%r!=%r)' % (
+            self.getUnexpectedType(), self.expecting
+            )
+    __repr__ = __str__
     
-    from scrapy.http import Request
-from scrapy.utils.spider import iterate_spider_output
-from scrapy.utils.python import get_spec
+            You must be careful, if you subclass a generated recognizers.
+        The default implementation will only search the module of self
+        for rules, but the subclass will not contain any rules.
+        You probably want to override this method to look like
     
-            try:
-            self.min_bound = int(self.args[1])
-        except IndexError:
-            self.min_bound = 1
+    class TimeoutError(Error):
+    '''The operation exceeded the given deadline.'''
+    pass
     
+        def tearDown(self):
+        self.executor.shutdown(wait=True)
+        dt = time.time() - self.t1
+        if test_support.verbose:
+            print('%.2fs' % dt)
+        self.assertLess(dt, 60, 'synchronization issue: test lasted too long')
     
-if __name__ == '__main__':
-    HitCounts.run()
-
-    
-    
-class RemoveDuplicateUrls(MRJob):
-    
-    
-class Operator(Employee):
-    
-        def bfs(self, source, dest):
-        # Use self.visited_ids to track visited nodes
-        # Use self.lookup to translate a person_id to a Person
-        pass
-
-    
-        def crawl(self):
-        while True:
-            page = self.data_store.extract_max_priority_page()
-            if page is None:
-                break
-            if self.data_store.crawled_similar(page.signature):
-                self.data_store.reduce_priority_link_to_crawl(page.url)
-            else:
-                self.crawl_page(page)
-            page = self.data_store.extract_max_priority_page()
-
-    
-    
-def main(path):
-    
-        output_directory = 'pdfs' if results.directory is None else results.directory
-    
-        def get_app(self):
-        return WSGIContainer(validator(self.wsgi_app))
-    
-    from tornado import gen
-from tornado.ioloop import IOLoop
-from tornado.netutil import Resolver, is_valid_ip
-    
-            self.assertGreater(len(connected_clients), 0,
-                           'all clients failed connecting')
-        try:
-            if len(connected_clients) == N:
-                # Ideally we'd make the test deterministic, but we're testing
-                # for a race condition in combination with the system's TCP stack...
-                self.skipTest('at least one client should fail connecting '
-                              'for the test to be meaningful')
-        finally:
-            for c in connected_clients:
-                c.close()
-    
-        def open(self):
-        ChatSocketHandler.waiters.add(self)
+    import functools
+import os
+import requests
+import time
+import warnings
