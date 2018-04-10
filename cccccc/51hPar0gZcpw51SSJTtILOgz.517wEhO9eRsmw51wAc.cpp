c6c2b6022b644b3b04597c35f669c6524ca5f401
SUNsung <weatherius@gmail.com>
2018-04-10 23:54:33 +0300

    Rewrite [51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp] File in: 2018-04-10 23:54:32

--- cccccc/51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp
+++ cccccc/51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp
@@ -1,290 +1,41 @@
 
         
-          // The resource is deleted from the resource manager only when it is private
-  // to kernel. Ideally the resource should be deleted when it is no longer held
-  // by anyone, but it would break backward compatibility.
-  ~ResourceOpKernel() override {
-    if (resource_ != nullptr) {
-      resource_->Unref();
-      if (cinfo_.resource_is_private_to_kernel()) {
-        if (!cinfo_.resource_manager()
-                 ->template Delete<T>(cinfo_.container(), cinfo_.name())
-                 .ok()) {
-          // Do nothing; the resource can have been deleted by session resets.
-        }
-      }
-    }
-  }
+        static const char* kImplVersionName = 'api_version';
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+    #include <google/protobuf/descriptor_database.h>
     
-    #include 'tensorflow/core/grappler/optimizers/graph_optimizer.h'
-    
-    #include 'third_party/eigen3/unsupported/Eigen/CXX11/Tensor'
-#include 'tensorflow/core/framework/tensor_types.h'
-#include 'tensorflow/core/framework/types.h'
-#include 'tensorflow/core/kernels/scatter_functor.h'
-    
-    // A PluginConfig describes the set of plugins to be used by a StreamExecutor
-// instance. Each plugin is defined by an arbitrary identifier, usually best set
-// to the address static member in the implementation (to avoid conflicts).
-//
-// A PluginConfig may be passed to the StreamExecutor constructor - the plugins
-// described therein will be used to provide BLAS, DNN, FFT, and RNG
-// functionality. Platform-appropriate defaults will be used for any un-set
-// libraries. If a platform does not support a specified plugin (ex. cuBLAS on
-// an OpenCL executor), then an error will be logged and no plugin operations
-// will succeed.
-//
-// The StreamExecutor BUILD target does not link ANY plugin libraries - even
-// common host fallbacks! Any plugins must be explicitly linked by dependent
-// targets. See the cuda, opencl and host BUILD files for implemented plugin
-// support (search for 'plugin').
-class PluginConfig {
+    class EnumGenerator : public SourceGeneratorBase {
  public:
-  // Value specifying the platform's default option for that plugin.
-  static const PluginId kDefault;
+  EnumGenerator(const EnumDescriptor* descriptor, const Options* options);
+  ~EnumGenerator();
     }
     
-    namespace internal {
-template <typename Scalar>
-struct functor_traits<scalar_clip_op<Scalar> > {
-  enum {
-    Cost = NumTraits<Scalar>::AddCost * 3,
-    PacketAccess = packet_traits<Scalar>::HasMax &&
-                   packet_traits<Scalar>::HasMin &&
-                   packet_traits<Scalar>::HasNegate
-  };
-};
-}  // namespace internal
-    
-      Status ReadLocked(string* key, string* value, bool* produced,
-                    bool* at_end) override {
-    Status status = input_buffer_->ReadLine(value);
-    ++line_number_;
-    if (status.ok()) {
-      *key = strings::StrCat(current_work(), ':', line_number_);
-      *produced = true;
-      return status;
-    }
-    if (errors::IsOutOfRange(status)) {  // End of file, advance to the next.
-      *at_end = true;
-      return Status::OK();
-    } else {  // Some other reading error
-      return status;
-    }
-  }
-    
-        NodeDef* const_node3 = graph_def.add_node();
-    const_node3->set_name('const_node3');
-    const_node3->set_op('Const');
-    
-      CHECK(worker_name.size() < max_worker_name_length)
-      << 'Specified worker name is too long.';
-  snprintf(my_name, max_worker_name_length, worker_name.c_str());
-  std::vector<char> worker_names(number_of_procs * max_worker_name_length);
-  MPI_CHECK(MPI_Allgather(my_name, max_worker_name_length, MPI_CHAR,
-                          &worker_names[0], max_worker_name_length, MPI_CHAR,
-                          MPI_COMM_WORLD));
-    
-    v8::Handle<v8::Value> AllocateId(int routing_id) {
-  v8::Isolate* isolate = v8::Isolate::GetCurrent();
-  v8::EscapableHandleScope scope(isolate);
+    class RepeatedMessageFieldGenerator : public FieldGeneratorBase {
+ public:
+  RepeatedMessageFieldGenerator(const FieldDescriptor* descriptor,
+                                int fieldOrdinal,
+                                const Options *options);
+  ~RepeatedMessageFieldGenerator();
     }
     
-    namespace remote {
-    }
-    
-      v8::Handle<v8::Value> args = converter.ToV8Value(&arguments, context);
-  DCHECK(!args.IsEmpty()) << 'Invalid 'arguments' in Dispatcher::OnEvent';
-  v8::Handle<v8::Value> argv[] = {
-    v8::Integer::New(isolate, object_id), v8_str(event.c_str()), args };
-    
-      std::set<int> objects_;
-    
-    
-    {}  // namespace nw
-    
-    // Popup menus may get squished if they open up too close to the bottom of the
-// screen. This function takes the size of the screen, the size of the menu,
-// an optional widget, the Y position of the mouse click, and adjusts the popup
-// menu's Y position to make it fit if it's possible to do so.
-// Returns the new Y position of the popup menu.
-int CalculateMenuYPosition(const GdkRectangle* screen_rect,
-                           const GtkRequisition* menu_req,
-                           GtkWidget* widget, const int y) {
-  CHECK(screen_rect);
-  CHECK(menu_req);
-  // If the menu would run off the bottom of the screen, and there is enough
-  // screen space upwards to accommodate the menu, then pop upwards. If there
-  // is a widget, then also move the anchor point to the top of the widget
-  // rather than the bottom.
-  const int screen_top = screen_rect->y;
-  const int screen_bottom = screen_rect->y + screen_rect->height;
-  const int menu_bottom = y + menu_req->height;
-  int alternate_y = y - menu_req->height;
-  if (widget) {
-    GtkAllocation allocation;
-    gtk_widget_get_allocation(widget, &allocation);
-    alternate_y -= allocation.height;
-  }
-  if (menu_bottom >= screen_bottom && alternate_y >= screen_top)
-    return alternate_y;
-  return y;
+    SourceGeneratorBase::~SourceGeneratorBase() {
 }
     
+    // Generates code for a lite extension, which may be within the scope of some
+// message or may be at file scope.  This is much simpler than FieldGenerator
+// since extensions are just simple identifiers with interesting types.
+class ImmutableExtensionLiteGenerator : public ExtensionGenerator {
+ public:
+  explicit ImmutableExtensionLiteGenerator(const FieldDescriptor* descriptor,
+                                           Context* context);
+  virtual ~ImmutableExtensionLiteGenerator();
+    }
     
-    {  gtk_widget_show(menu_item_);
-  g_object_ref_sink(G_OBJECT(menu_item_));
-} 
-    
-    AuthPropertyIterator SecureAuthContext::begin() const {
-  if (ctx_) {
-    grpc_auth_property_iterator iter =
-        grpc_auth_context_property_iterator(ctx_);
-    const grpc_auth_property* property =
-        grpc_auth_property_iterator_next(&iter);
-    return AuthPropertyIterator(property, &iter);
+    ExtensionGenerator* ImmutableGeneratorFactory::NewExtensionGenerator(
+    const FieldDescriptor* descriptor) const {
+  if (HasDescriptorMethods(descriptor->file(), context_->EnforceLite())) {
+    return new ImmutableExtensionGenerator(descriptor, context_);
   } else {
-    return end();
+    return new ImmutableExtensionLiteGenerator(descriptor, context_);
   }
 }
-    
-    #include <map>
-#include <mutex>
-    
-     private:
-  SubProcess(const SubProcess& other);
-  SubProcess& operator=(const SubProcess& other);
-    
-      // Is there no unread data in the buffer.
-  bool empty() const
-  {
-    return begin_offset_ == end_offset_;
-  }
-    
-    #if defined(BOOST_ASIO_WINDOWS) || defined(__CYGWIN__)
-typedef win_fd_set_adapter fd_set_adapter;
-#else
-typedef posix_fd_set_adapter fd_set_adapter;
-#endif
-    
-    
-    {
-    {
-    {} // namespace detail
-} // namespace asio
-} // namespace boost
-    
-    // Calls to asio_handler_invoke must be made from a namespace that does not
-// contain overloads of this function. The boost_asio_handler_invoke_helpers
-// namespace is defined here for that purpose.
-namespace boost_asio_handler_invoke_helpers {
-    }
-    
-      // Destructor.
-  ~hash_map()
-  {
-    delete[] buckets_;
-  }
-    
-      if (result >= 0)
-  {
-    ec = boost::system::error_code();
-    if (value)
-      state |= user_set_non_blocking;
-    else
-    {
-      // Clearing the user-set non-blocking mode always overrides any
-      // internally-set non-blocking flag. Any subsequent asynchronous
-      // operations will need to re-enable non-blocking I/O.
-      state &= ~(user_set_non_blocking | internal_non_blocking);
-    }
-    return true;
-  }
-    
-    void dev_poll_reactor::shutdown_service()
-{
-  boost::asio::detail::mutex::scoped_lock lock(mutex_);
-  shutdown_ = true;
-  lock.unlock();
-    }
-    
-    template <typename Time_Traits>
-void epoll_reactor::schedule_timer(timer_queue<Time_Traits>& queue,
-    const typename Time_Traits::time_type& time,
-    typename timer_queue<Time_Traits>::per_timer_data& timer, wait_op* op)
-{
-  mutex::scoped_lock lock(mutex_);
-    }
-    
-        // Build atlas
-    unsigned char* tex_pixels = NULL;
-    int tex_w, tex_h;
-    io.Fonts->GetTexDataAsRGBA32(&tex_pixels, &tex_w, &tex_h);
-    
-                if (ImGui::Button('Button'))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text('counter = %d', counter);
-    
-            // Rendering
-        IwGxSetColClear(clear_color.x * 255, clear_color.y * 255, clear_color.z * 255, clear_color.w * 255);
-        IwGxClear();
-        ImGui::Render();
-        ImGui_Marmalade_RenderDrawData(ImGui::GetDrawData());
-        IwGxSwapBuffers();
-    
-    IMGUI_API bool        ImGui_ImplGlfwGL3_Init(GLFWwindow* window, bool install_callbacks, const char* glsl_version = NULL);
-IMGUI_API void        ImGui_ImplGlfwGL3_Shutdown();
-IMGUI_API void        ImGui_ImplGlfwGL3_NewFrame();
-IMGUI_API void        ImGui_ImplGlfwGL3_RenderDrawData(ImDrawData* draw_data);
-    
-    
-    {        // Rendering
-        glViewport(0, 0, (int)ImGui::GetIO().DisplaySize.x, (int)ImGui::GetIO().DisplaySize.y);
-        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui::Render();
-        ImGui_ImplSdlGL3_RenderDrawData(ImGui::GetDrawData());
-        SDL_GL_SwapWindow(window);
-    }
-    
-    static bool ImGui_ImplWin32_UpdateMouseCursor()
-{
-    ImGuiIO& io = ImGui::GetIO();
-    if (io.ConfigFlags & ImGuiConfigFlags_NoMouseCursorChange)
-        return false;
-    }
-    
-        // Setup viewport
-    D3DVIEWPORT9 vp;
-    vp.X = vp.Y = 0;
-    vp.Width = (DWORD)io.DisplaySize.x;
-    vp.Height = (DWORD)io.DisplaySize.y;
-    vp.MinZ = 0.0f;
-    vp.MaxZ = 1.0f;
-    g_pd3dDevice->SetViewport(&vp);
-    
-    
-    {    int ret = _GetIoctx(fpath)->stat(fid, file_size, &mtime);
-    if (ret < 0) {
-      LOG_DEBUG('%i\n', ret);
-      if (-ENOENT == ret) {
-        *file_size = 0;
-        s = Status::OK();
-      } else {
-        s = err_to_status(ret);
-      }
-    } else {
-      s = Status::OK();
-    }
-  } while (0);
-    
-    
-    {}  // namespace rocksdb
