24e1c0a98bed2ea3be1ff429c2b702c468ba69e1
SUNsung <weatherius@gmail.com>
2018-04-10 08:34:05 +0300

    Rewrite [56LzyGKFpsjHM56BDZmQcfbR4k56T8TbIRVGt865639q.py] File in: 2018-04-10 08:34:04

--- pyton/56LzyGKFpsjHM56BDZmQcfbR4k56T8TbIRVGt865639q.py
+++ pyton/56LzyGKFpsjHM56BDZmQcfbR4k56T8TbIRVGt865639q.py
@@ -1,162 +1,80 @@
 
         
-            data_len = tf.size(raw_data)
-    batch_len = data_len // batch_size
-    data = tf.reshape(raw_data[0 : batch_size * batch_len],
-                      [batch_size, batch_len])
+            to remove replace in this file ismount( -> os.path.ismount( and remove this
+    function'''
     
-      Returns:
-    (N+1)-D np.array with as many rows as there are complete frames that can be
-    extracted.
-  '''
-  num_samples = data.shape[0]
-  num_frames = 1 + int(np.floor((num_samples - window_length) / hop_length))
-  shape = (num_frames, window_length) + data.shape[1:]
-  strides = (data.strides[0] * hop_length,) + data.strides
-  return np.lib.stride_tricks.as_strided(data, shape=shape, strides=strides)
+            if response.status_code != 200:
+            raise Exception(response.content)
     
-      def setUp(self):
-    FLAGS.train_data = os.path.join(self.get_temp_dir(), 'test-text.txt')
-    FLAGS.eval_data = os.path.join(self.get_temp_dir(), 'eval-text.txt')
-    FLAGS.save_path = self.get_temp_dir()
-    with open(FLAGS.train_data, 'w') as f:
-      f.write(
-          '''alice was beginning to get very tired of sitting by her sister on
-          the bank, and of having nothing to do: once or twice she had peeped
-          into the book her sister was reading, but it had no pictures or
-          conversations in it, 'and what is the use of a book,' thought alice
-          'without pictures or conversations?' So she was considering in her own
-          mind (as well as she could, for the hot day made her feel very sleepy
-          and stupid), whether the pleasure of making a daisy-chain would be
-          worth the trouble of getting up and picking the daisies, when suddenly
-          a White rabbit with pink eyes ran close by her.\n''')
-      with open(FLAGS.eval_data, 'w') as f:
-        f.write('alice she rabbit once\n')
+        args = parser.parse_args()
     
-    
-class RegressionTest(tf.test.TestCase):
-  '''Test the regression examples in this directory.'''
-    
-    
-def main(_):
-  tf.logging.set_verbosity(tf.logging.INFO)
-  tf.gfile.MakeDirs(FLAGS.eval_dir)
-  tf.logging.info('Building eval graph...')
-  output = graphs.get_model().eval_graph(FLAGS.eval_data)
-  eval_ops, moving_averaged_variables = output
-    
-    
-class GraphsTest(tf.test.TestCase):
-  '''Test graph construction methods.'''
-    
-    
-if __name__ == '__main__':
-  tf.app.run()
+        return results
 
     
-    filegroup(
-    name = 'all_files',
-    srcs = glob(
-        ['**/*'],
-        exclude = [
-            '**/METADATA',
-            '**/OWNERS',
-        ],
-    ),
-    visibility = ['//tensorflow:__subpackages__'],
-)
+        def on_open_shell(self):
+        try:
+            self._exec_cli_command(b'environment no more')
+        except AnsibleConnectionFailure:
+            raise AnsibleConnectionFailure('unable to set terminal parameters')
 
     
-    licenses(['notice'])  # Apache 2.0
+            lines = process.communicate()[0].splitlines()
+        ws_root = get_root()
+        ws_filepath = os.path.relpath(filepath, ws_root)
+        for line in lines:
+            line = extract_line_info(line, ws_filepath, toolname)
+            if line:
+                report.append(line)
+    return report
     
-    # Make coding more python3-ish
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
+        @csrf_exempt
+    @json_validate(
+        signature=VSigned(),
+        user=VThrottledLogin(['user', 'passwd']),
+    )
+    def POST_login(self, responder, user, **kwargs):
+        kwargs.update(dict(
+            controller=self,
+            form=responder('noop'),
+            responder=responder,
+            user=user,
+        ))
+        return handle_login(**kwargs)
     
-    
-def get_group_vars(groups):
-    
-    
-collect_ignore = [
-    # deprecated or moved modules
-    'scrapy/conf.py',
-    'scrapy/stats.py',
-    'scrapy/project.py',
-    'scrapy/utils/decorator.py',
-    'scrapy/statscol.py',
-    'scrapy/squeue.py',
-    'scrapy/log.py',
-    'scrapy/dupefilter.py',
-    'scrapy/command.py',
-    'scrapy/linkextractor.py',
-    'scrapy/spider.py',
-    
-            if opts.pidfile:
-            with open(opts.pidfile, 'w') as f:
-                f.write(str(os.getpid()) + os.linesep)
-    
-    # If true, '()' will be appended to :func: etc. cross-reference text.
-#add_function_parentheses = True
-    
-        def __init__(self, args):
-        '''Initializes the plugin with the given command line args'''
-        self._temp_dir = tempfile.mkdtemp()
-        self.le_config = util.create_le_config(self._temp_dir)
-        config_dir = util.extract_configs(args.configs, self._temp_dir)
-        self._configs = [
-            os.path.join(config_dir, config)
-            for config in os.listdir(config_dir)]
+            def builder_wrapper(thing = None):
+            kw = {}
+            if not thing:
+                kw['url'] = url
+                kw['title'] = title
+            return ButtonLite(thing,
+                              image = 1 if buttonimage is None else buttonimage,
+                              target = '_new' if newwindow else '_parent',
+                              styled = styled, **kw)
     
     
-print('\n# ======================================================================')
-print('#                   Calculating pi, 10000 iterations')
-print('# ======================================================================\n')
+def handle_awful_failure(fail_text):
+    '''
+    Makes sure that no errors generated in the error handler percolate
+    up to the user unless debug is enabled.
+    '''
+    if g.debug:
+        import sys
+        s = sys.exc_info()
+        # reraise the original error with the original stack trace
+        raise s[1], None, s[2]
+    try:
+        # log the traceback, and flag the 'path' as the error location
+        import traceback
+        log.write_error_summary(fail_text)
+        for line in traceback.format_exc().splitlines():
+            g.log.error(line)
+        return redditbroke % (make_failien_url(), websafe(fail_text))
+    except:
+        # we are doomed.  Admit defeat
+        return 'This is an error that should never occur.  You win.'
+
     
-    FILETYPES = [
-    (re.compile('Xorg'), RGBColorDB),
-    (re.compile('XConsortium'), RGBColorDB),
-    (re.compile('HTML'), HTML40DB),
-    (re.compile('lightlink'), LightlinkDB),
-    (re.compile('Websafe'), WebsafeDB),
-    ]
-    
-            contents = []
-        method_items = sorted(methods.items())
-        for key, value in method_items:
-            contents.append(self.docroutine(value, key, funcs=fdict))
-        result = result + self.bigsection(
-            'Methods', '#ffffff', '#eeaa77', ''.join(contents))
-    
-        def proxyval(self, visited):
-        # Guard against infinite loops:
-        if self.as_address() in visited:
-            return ProxyAlreadyVisited('(...)')
-        visited.add(self.as_address())
-        arg_proxy = self.pyop_field('args').proxyval(visited)
-        return ProxyException(self.safe_tp_name(),
-                              arg_proxy)
-    
-                if point.name == 'p':
-                link = point.find('a')
-                if link is not None:
-                    link = clean_pdf_link(link.attrs['href'])
-                    ext = get_extension(link)
-                    print(ext)
-                    if not ext in forbidden_extensions:
-                        print(shorten_title(point.text) + ' (' + link + ')')
-                        try:
-                            name = clean_text(point.text.split('[' + ext + ']')[0])
-                            fullname = '.'.join((name, ext))
-                            if not os.path.exists('/'.join((current_directory, fullname)) ):
-                                download_pdf(link, current_directory, '.'.join((name, ext)))
-                        except KeyboardInterrupt:
-                            try:
-                                print('Press Ctrl-C in 1 second to quit')
-                                time.sleep(1)
-                            except KeyboardInterrupt:
-                                print('Cancelling..')
-                                break
-                        except:
-                            failures.append(point.text)
-                        
-        point = point.next_sibling          
+        def GET_health(self):
+        if os.path.exists('/var/opt/reddit/quiesce'):
+            request.environ['usable_error_content'] = 'No thanks, I'm full.'
+            abort(503)
