6ba760475d11fd6fd5153b9917f898489e07fea9
SUNsung <weatherius@gmail.com>
2018-04-10 12:10:50 +0300

    Rewrite [400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py] File in: 2018-04-10 12:10:50

--- pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
+++ pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
@@ -1,61 +1,64 @@
 
         
-        print('Populating sources directory with templates.')
-for subdir, dirs, fnames in os.walk('templates'):
-    for fname in fnames:
-        new_subdir = subdir.replace('templates', 'sources')
-        if not os.path.exists(new_subdir):
-            os.makedirs(new_subdir)
-        if fname[-3:] == '.md':
-            fpath = os.path.join(subdir, fname)
-            new_fpath = fpath.replace('templates', 'sources')
-            shutil.copy(fpath, new_fpath)
+                (category1, 1), product4
+        (category1, 2), product1
+        (category1, 3), product2
+        (category2, 3), product1
+        (category2, 7), product3
+        '''
+        category, product_id = key
+        quantity = value
+        yield (category, quantity), product_id
     
-            pool = multiprocessing.pool.ThreadPool()
-        function_partial = partial(_count_valid_files_in_directory,
-                                   white_list_formats=white_list_formats,
-                                   follow_links=follow_links,
-                                   split=split)
-        self.samples = sum(pool.map(function_partial,
-                                    (os.path.join(directory, subdir)
-                                     for subdir in classes)))
+        def __init__(self, from_user_id, to_user_id, request_status, timestamp):
+        self.from_user_id = from_user_id
+        self.to_user_id = to_user_id
+        self.request_status = request_status
+        self.timestamp = timestamp
     
-    
-@keras_test
-@pytest.mark.skipif((K.backend() != 'tensorflow'),
-                    reason='Requires TensorFlow backend')
-def test_TensorBoard_histogram_freq_must_have_validation_data(tmpdir):
-    np.random.seed(np.random.randint(1, 1e7))
-    filepath = str(tmpdir / 'logs')
+        def __init__(self, value, suit):
+        super(BlackJackCard, self).__init__(value, suit)
     
     
-@pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
-def test_glorot_uniform(tensor_shape):
-    fan_in, fan_out = initializers._compute_fans(tensor_shape)
-    scale = np.sqrt(6. / (fan_in + fan_out))
-    _runner(initializers.glorot_uniform(), tensor_shape,
-            target_mean=0., target_max=scale, target_min=-scale)
+@zope.interface.implementer(interfaces.IAuthenticator)
+@zope.interface.provider(interfaces.IPluginFactory)
+class Authenticator(common.Plugin):
+    '''Example Authenticator.'''
     
-        @abc.abstractmethod
-    def contribute(self):
-        raise NotImplementedError('Must provide implementation in subclass.')
+            # Convert unsigned big-endian byte arrays to integers.
+        tests3 = {
+            b'': 0,
+            b'\x00': 0,
+            b'\x01': 1,
+            b'\x7f': 127,
+            b'\x80': 128,
+            b'\xff': 255,
+            b'\x01\x00': 256,
+            b'\x7f\xff': 32767,
+            b'\x80\x00': 32768,
+            b'\xff\xff': 65535,
+            b'\x01\x00\x00': 65536,
+        }
+        check(tests3, 'big', signed=False)
     
+        def test_create_widgets(self):
+        self.dialog.create_entries = Func()
+        self.dialog.create_option_buttons = Func()
+        self.dialog.create_other_buttons = Func()
+        self.dialog.create_command_buttons = Func()
     
-class TestData(unittest.TestCase):
+        def proxyval(self, visited):
+        return None
     
-        def subscribe(self, msg, subscriber):
-        self.subscribers.setdefault(msg, []).append(subscriber)
+            self.assertRaises(TypeError, _CFuncPtr, 13, 'name', 42, 'iid')
     
-    
-class RegistryHolder(type):
-    
-    '''
-Port of the Java example of 'Parameter Injection' in
-'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
-(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
-http://xunitpatterns.com/Dependency%20Injection.html.
-    
-        def _handle(self, request):
-        if 10 < request <= 20:
-            print('request {} handled in handler 2'.format(request))
-            return True
+    class StructFieldsTestCase(unittest.TestCase):
+    # Structure/Union classes must get 'finalized' sooner or
+    # later, when one of these things happen:
+    #
+    # 1. _fields_ is set.
+    # 2. An instance is created.
+    # 3. The type is used as field of another Structure/Union.
+    # 4. The type is subclassed
+    #
+    # When they are finalized, assigning _fields_ is no longer allowed.
