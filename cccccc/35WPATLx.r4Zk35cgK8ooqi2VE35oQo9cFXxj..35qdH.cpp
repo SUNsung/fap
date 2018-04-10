664d6d3521e90e2f27f665a9bb4333d1308bd03d
SUNsung <weatherius@gmail.com>
2018-04-11 00:23:46 +0300

    Rewrite [35WPATLx.r4Zk35cgK8ooqi2VE35oQo9cFXxj..35qdH.cpp] File in: 2018-04-11 00:23:45

--- cccccc/35WPATLx.r4Zk35cgK8ooqi2VE35oQo9cFXxj..35qdH.cpp
+++ cccccc/35WPATLx.r4Zk35cgK8ooqi2VE35oQo9cFXxj..35qdH.cpp
@@ -1,46 +1,270 @@
 
         
-        /// Remove a range of keys in domain.
-Status deleteDatabaseRange(const std::string& domain,
-                           const std::string& low,
-                           const std::string& high);
+        namespace tensorflow {
+    }
+    
+    
+    {  void Feedback(Cluster* cluster, const GrapplerItem& item,
+                const GraphDef& pruned_graph, double result) override;
+};
     
     
     { private:
-  std::string process_name_;
+  // Smoothness constant of smooth hinge loss
+  // TODO(sibyl-Aix6ihai): expose this parameter
+  const double gamma = 1;
 };
     
-      /// The group this instance dropped privileges to.
-  gid_t to_group_{0};
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-      // Helper function to return the carve directory
-  boost::filesystem::path getCarveDir() {
-    return carveDir_;
+    class RecordWriter {
+ public:
+  // Create a writer that will append data to '*dest'.
+  // '*dest' must be initially empty.
+  // '*dest' must remain live while this Writer is in use.
+  RecordWriter(WritableFile* dest,
+               const RecordWriterOptions& options = RecordWriterOptions());
+    }
+    
+    namespace internal {
+template <typename T>
+struct functor_traits<scalar_tanh_fast_derivative_op<T> > {
+  enum {
+    Cost = NumTraits<T>::AddCost * 2 + NumTraits<T>::MulCost * 1,
+    PacketAccess = packet_traits<T>::HasAdd && packet_traits<T>::HasMul &&
+                   packet_traits<T>::HasNegate
+  };
+};
+}  // namespace internal
+    
+    
+    {}  // namespace tensorflow
+
+    
+      // Fill *indices and *sizes from *this (so that we can use the slice()
+  // function in eigen tensor). We need a tensor shape in case some of the
+  // slices are full slices.
+  // We allow NDIMS to be greater than dims(), in which case we will pad the
+  // higher dimensions with trivial dimensions.
+  template <int NDIMS>
+  void FillIndicesAndSizes(
+      const TensorShape& shape,
+      Eigen::DSizes<Eigen::DenseIndex, NDIMS>* indices,
+      Eigen::DSizes<Eigen::DenseIndex, NDIMS>* sizes) const;
+    
+      v8::Handle<v8::Value> val = GetWindowId(web_view->mainFrame());
+  if (val.IsEmpty())
+    return;
+  if (val->IsNull() || val->IsUndefined())
+    return;
+    
+      static v8::Handle<v8::Object> GetObjectRegistry();
+  static v8::Handle<v8::Value> GetWindowId(blink::WebFrame* frame);
+  static void ZoomLevelChanged(blink::WebView* web_view);
+  static void willHandleNavigationPolicy(
+    content::RenderView* rv,
+    blink::WebFrame* frame,
+    const blink::WebURLRequest& request,
+    blink::WebNavigationPolicy* policy,
+    blink::WebString* manifest);
+    
+     private:
+  // Helper functions for bindings.
+  static void RequireNwGui(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void GetAbsolutePath(const v8::FunctionCallbackInfo<v8::Value>& args);
+    
+    
+    {} // namespace nwapi
+
+    
+    
+    {  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
+  if (!item)
+    return false;
+  return item->is_enabled_;
+}
+    
+    
+    {  DISALLOW_COPY_AND_ASSIGN(MenuDelegate);
+};
+    
+    #include <string.h>
+    
+    /* Bidirectional Stream API */
+    
+    
+    {  return 0;
+}
+
+    
+    
+    {
+    {}  // namespace testing
+}  // namespace grpc
+    
+    // Data pertaining to configuration of the generator with respect to anything
+// that may be used internally at Google.
+struct GeneratorConfiguration {
+  GeneratorConfiguration();
+  grpc::string grpc_package_root;
+  // TODO(https://github.com/grpc/grpc/issues/8622): Drop this.
+  grpc::string beta_package_root;
+  // TODO(https://github.com/google/protobuf/issues/888): Drop this.
+  grpc::string import_prefix;
+};
+    
+    namespace grpc {
+namespace {
+    }
+    }
+    
+    static void get_resource_usage(double* utime, double* stime) {
+#ifdef __linux__
+  struct rusage usage;
+  getrusage(RUSAGE_SELF, &usage);
+  *utime = time_double(&usage.ru_utime);
+  *stime = time_double(&usage.ru_stime);
+#else
+  *utime = 0;
+  *stime = 0;
+#endif
+}
+    
+      /**
+   * @brief Halt the EventPublisher run loop.
+   *
+   * Any EventSubscriber%s with Subscription%s for this EventPublisher will
+   * become useless. osquery callers MUST deregister events.
+   * EventPublisher%s assume they can hook/trampoline, which requires cleanup.
+   * This will tear down and remove the publisher if the run loop did not start.
+   * Otherwise it will call end on the publisher and assume the run loop will
+   * tear down and remove.
+   *
+   * @param pub The string label for the EventPublisher.
+   *
+   * @return Did the EventPublisher deregister cleanly.
+   */
+  static Status deregisterEventPublisher(const EventPublisherRef& pub);
+    
+      Status getQueryTables(const std::string& query,
+                        std::vector<std::string>& tables) const override {
+    return Status(0, 'Not used');
   }
+    
+    
+    {  /// Configurations may set 'custom_' flags.
+  std::map<std::string, std::string> custom_;
+};
+    
+    /// Inspect the number of internal-buffered status log lines.
+size_t queuedStatuses();
     
       EXPECT_EQ(carves.size(), 2U);
   s = archive(carves,
               carveFSPath + '/' + kTestCarveNamePrefix + guid_ + '.tar');
   EXPECT_TRUE(s.ok());
     
-    /**
- * @brief Access the internal storage of the Decorator parser.
- *
- * The decoration set is a map of column name to value. It contains the opaque
- * set of decoration point results.
- *
- * Decorations are applied to log items before they are sent to the downstream
- * logging APIs: logString, logSnapshot, etc.
- *
- * @param results the output parameter to write decorations.
- */
-void getDecorations(std::map<std::string, std::string>& results);
+    // Use if you want to reset your rendering device without losing ImGui state.
+IMGUI_API void        ImGui_Marmalade_InvalidateDeviceObjects();
+IMGUI_API bool        ImGui_Marmalade_CreateDeviceObjects();
     
     
-    {      multi_pack.put_child(fs::path(path).stem().string(), single_pack);
+    {            ImGui::Text('Application average %.3f ms/frame (%.1f FPS)', 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        }
+    
+    
+    {    return true;
+}
+    
+        // Channels
+    // - Use to simulate layers. By switching channels to can render out-of-order (e.g. submit foreground primitives before background primitives)
+    // - Use to minimize draw calls (e.g. if going back-and-forth between multiple non-overlapping clipping rectangles, prefer to append into separate channels then merge at the end)
+    IMGUI_API void  ChannelsSplit(int channels_count);
+    IMGUI_API void  ChannelsMerge();
+    IMGUI_API void  ChannelsSetCurrent(int channel_index);
+    
+            // Decode and advance source
+        unsigned int c = (unsigned int)*s;
+        if (c < 0x80)
+        {
+            s += 1;
+        }
+        else
+        {
+            s += ImTextCharFromUtf8(&c, s, text_end);
+            if (c == 0) // Malformed UTF-8?
+                break;
+        }
+    
+    unsigned int XXH32 (const void* input, int len, unsigned int seed);
+    
+      std::string res;
+  std::unique_ptr<rocksdb::Iterator> it(db_->NewIterator(ReadOptions()));
+  std::string k1('k1');
+  std::string k2('k2');
+  bool first = true;
+  for (it->Seek(k1); it->Valid(); it->Next()) {
+    res = it->value().ToString();
+    if (first) {
+      ASSERT_EQ(res, 'v1,v2,v3');
+      first = false;
+    } else {
+      ASSERT_EQ(res, 'a1,a2,a3');
     }
+  }
+  slists.Append('k2', 'a4');
+  slists.Append('k1', 'v4');
     
-    DECLARE_uint64(alarm_timeout);
+      virtual void SeekToFirst() override;
     
-    #include <string.h>
-#include <time.h>
+    
+    {    // if file not exist
+    if (ret < 0) {
+      _file_size = 0;
+    }
+  }
+    
+      WriteOptions write_options;
+  // Insert something into CFB so lots of log files will be kept
+  // before creating the checkpoint.
+  ASSERT_OK(txdb->CreateColumnFamily(cf_options, 'CFB', &cfb));
+  ASSERT_OK(txdb->Put(write_options, cfb, '', ''));
+    
+      // Allocate a buf of len size for storing key. The idea is that a
+  // specific memtable representation knows its underlying data structure
+  // better. By allowing it to allocate memory, it can possibly put
+  // correlated stuff in consecutive memory area to make processor
+  // prefetching more efficient.
+  virtual KeyHandle Allocate(const size_t len, char** buf);
+    
+      void Validate(TestInlineSkipList* list) {
+    // Check keys exist.
+    for (Key key : keys_) {
+      ASSERT_TRUE(list->Contains(Encode(&key)));
+    }
+    // Iterate over the list, make sure keys appears in order and no extra
+    // keys exist.
+    TestInlineSkipList::Iterator iter(list);
+    ASSERT_FALSE(iter.Valid());
+    Key zero = 0;
+    iter.Seek(Encode(&zero));
+    for (Key key : keys_) {
+      ASSERT_TRUE(iter.Valid());
+      ASSERT_EQ(key, Decode(iter.key()));
+      iter.Next();
+    }
+    ASSERT_FALSE(iter.Valid());
+    // Validate the list is well-formed.
+    list->TEST_Validate();
+  }
+    
+    void SyncPoint::LoadDependencyAndMarkers(
+  const std::vector<SyncPointPair>& dependencies,
+  const std::vector<SyncPointPair>& markers) {
+  impl_->LoadDependencyAndMarkers(dependencies, markers);
+}
