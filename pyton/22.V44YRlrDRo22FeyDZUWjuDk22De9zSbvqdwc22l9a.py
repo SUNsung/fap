f384a6585ffaaca6d9395e48c14dab0a278237ed
SUNsung <weatherius@gmail.com>
2018-04-12 02:31:16 +0300

    Rewrite [22.V44YRlrDRo22FeyDZUWjuDk22De9zSbvqdwc22l9a.py] File in: 2018-04-12 02:31:15

--- pyton/22.V44YRlrDRo22FeyDZUWjuDk22De9zSbvqdwc22l9a.py
+++ pyton/22.V44YRlrDRo22FeyDZUWjuDk22De9zSbvqdwc22l9a.py
@@ -1,121 +1,53 @@
 
         
-        from sklearn.datasets.samples_generator import make_regression
+            def mapper(self, _, line):
+        '''Parse each log line, extract and transform relevant lines.
     
-        ###########################################################################
-    # Set Python core input
-    sampling_algorithm['python-core-sample'] = \
-        lambda n_population, n_sample: \
-            random.sample(xrange(n_population), n_sample)
+        def steps(self):
+        '''Run the map and reduce steps.'''
+        return [
+            self.mr(mapper=self.mapper,
+                    reducer=self.reducer)
+        ]
     
-    pages = {
-    u'ar': u'http://ar.wikipedia.org/wiki/%D9%88%D9%8A%D9%83%D9%8A%D8%A8%D9%8A%D8%AF%D9%8A%D8%A7',
-    u'de': u'http://de.wikipedia.org/wiki/Wikipedia',
-    u'en': u'https://en.wikipedia.org/wiki/Wikipedia',
-    u'es': u'http://es.wikipedia.org/wiki/Wikipedia',
-    u'fr': u'http://fr.wikipedia.org/wiki/Wikip%C3%A9dia',
-    u'it': u'http://it.wikipedia.org/wiki/Wikipedia',
-    u'ja': u'http://ja.wikipedia.org/wiki/Wikipedia',
-    u'nl': u'http://nl.wikipedia.org/wiki/Wikipedia',
-    u'pl': u'http://pl.wikipedia.org/wiki/Wikipedia',
-    u'pt': u'http://pt.wikipedia.org/wiki/Wikip%C3%A9dia',
-    u'ru': u'http://ru.wikipedia.org/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D0%BF%D0%B5%D0%B4%D0%B8%D1%8F',
-#    u'zh': u'http://zh.wikipedia.org/wiki/Wikipedia',
-}
+        @mock.patch('certbot.display.enhancements.util')
+    def test_redirect(self, mock_util):
+        mock_util().menu.return_value = (display_util.OK, 1)
+        self.assertTrue(self._call('redirect'))
     
-        # the training data folder must be passed as first argument
-    movie_reviews_data_folder = sys.argv[1]
-    dataset = load_files(movie_reviews_data_folder, shuffle=False)
-    print('n_samples: %d' % len(dataset.data))
-    
-    # Author: Kemal Eren <kemal@kemaleren.com>
-# License: BSD 3 clause
-    
-    model = SpectralCoclustering(n_clusters=5, random_state=0)
-model.fit(data)
-score = consensus_score(model.biclusters_,
-                        (rows[:, row_idx], columns[:, col_idx]))
-    
-                group.append(HTML('htmlout.html').render())
-            print('Rendered page {} of the directory {}'.format(str(i), operating_sys))
-            i += 1
-        
-        allmd.clear()
-    
-            :arg httputil.HTTPServerRequest request: current HTTP request
-        :returns: a dict of parameters to be passed to the target handler
-            (for example, ``handler_kwargs``, ``path_args``, ``path_kwargs``
-            can be passed for proper `~.web.RequestHandler` instantiation).
-            An empty dict is a valid (and common) return value to indicate a match
-            when the argument-passing features are not used.
-            ``None`` must be returned to indicate that there is no match.'''
+            Returns:
+            A Future representing the given call.
+        '''
         raise NotImplementedError()
     
-            add_accept_handler(listener, accept_callback)
-        self.client_stream = IOStream(socket.socket())
-        self.addCleanup(self.client_stream.close)
-        yield [self.client_stream.connect(('127.0.0.1', port)),
-               event.wait()]
-        self.io_loop.remove_handler(listener)
-        listener.close()
-    
-        @gen_test
-    def test_blocking_get_wait(self):
-        q = queues.Queue()
-        q.put(0)
-        self.io_loop.call_later(0.01, q.put, 1)
-        self.io_loop.call_later(0.02, q.put, 2)
-        self.assertEqual(0, (yield q.get(timeout=timedelta(seconds=1))))
-        self.assertEqual(1, (yield q.get(timeout=timedelta(seconds=1))))
-    
-        c-ares fails to resolve some names when ``family`` is ``AF_UNSPEC``,
-    so it is only recommended for use in ``AF_INET`` (i.e. IPv4).  This is
-    the default for ``tornado.simple_httpclient``, but other libraries
-    may default to ``AF_UNSPEC``.
-    
-    print('Starting')
-if 'TESTAPP_STARTED' not in os.environ:
-    os.environ['TESTAPP_STARTED'] = '1'
-    sys.stdout.flush()
-    autoreload._reload()
-'''
-    
-            # This fires before the wait times out.
-        self.io_loop.call_later(0.01, sem.release)
-        acquire = sem.acquire(timedelta(seconds=0.02))
-        yield gen.sleep(0.03)
-        yield acquire  # No TimeoutError.
+      return {
+    'filepath': current_filepath,
+    'line_num': line + 1,
+    'column_num': column + 1,
+    'working_dir': working_dir,
+    'file_data': vimsupport.GetUnsavedAndSpecifiedBufferData( current_buffer,
+                                                              current_filepath )
+  }
     
     
-if __name__ == '__main__':
-    main()
-
+class EventNotification( BaseRequest ):
+  def __init__( self, event_name, buffer_number = None, extra_data = None ):
+    super( EventNotification, self ).__init__()
+    self._event_name = event_name
+    self._buffer_number = buffer_number
+    self._extra_data = extra_data
+    self._response_future = None
+    self._cached_response = None
     
     
-class Application(tornado.web.Application):
-    def __init__(self):
-        handlers = [
-            (r'/', HomeHandler),
-            (r'/archive', ArchiveHandler),
-            (r'/feed', FeedHandler),
-            (r'/entry/([^/]+)', EntryHandler),
-            (r'/compose', ComposeHandler),
-            (r'/auth/create', AuthCreateHandler),
-            (r'/auth/login', AuthLoginHandler),
-            (r'/auth/logout', AuthLogoutHandler),
-        ]
-        settings = dict(
-            blog_title=u'Tornado Blog',
-            template_path=os.path.join(os.path.dirname(__file__), 'templates'),
-            static_path=os.path.join(os.path.dirname(__file__), 'static'),
-            ui_modules={'Entry': EntryModule},
-            xsrf_cookies=True,
-            cookie_secret='__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__',
-            login_url='/auth/login',
-            debug=True,
-        )
-        super(Application, self).__init__(handlers, **settings)
-        # Have one global connection to the blog DB across all handlers
-        self.db = torndb.Connection(
-            host=options.mysql_host, database=options.mysql_database,
-            user=options.mysql_user, password=options.mysql_password)
+  def Start( self ):
+    request_data = BuildRequestData()
+    if self._extra_data:
+      request_data.update( self._extra_data )
+    with HandleServerException( display = False ):
+      self._response = self.PostDataToHandler( request_data, 'debug_info' )
+    
+    
+def RegexNotFiltered_test():
+  opts = _JavaFilter( { 'regex' : 'taco' } )
+  f = _CreateFilterForTypes( opts, [ 'cs' ] )
