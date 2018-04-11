5dff8bffb0305c981fde4a2c10c4bae1ada04654
SUNsung <weatherius@gmail.com>
2018-04-11 07:49:48 +0300

    Rewrite [23tBSaulI6yvc23965VM05QshI23jMdeyuHjCdM2397.rb] File in: 2018-04-11 07:49:48

--- ruby/23tBSaulI6yvc23965VM05QshI23jMdeyuHjCdM2397.rb
+++ ruby/23tBSaulI6yvc23965VM05QshI23jMdeyuHjCdM2397.rb
@@ -1,124 +1,56 @@
 
         
-          def test_redirect_to_url_with_network_path_reference
-    get :redirect_to_url_with_network_path_reference
-    assert_response :redirect
-    assert_equal '//www.rubyonrails.org/', redirect_to_url
+          def self.expire_cache_fragment!(name)
+    fragment_cache.delete(name)
   end
     
-            # Require the adapter itself and give useful feedback about
-        #   1. Missing adapter gems and
-        #   2. Adapter gems' missing dependencies.
-        path_to_adapter = 'action_cable/subscription_adapter/#{adapter}'
-        begin
-          require path_to_adapter
-        rescue LoadError => e
-          # We couldn't require the adapter itself. Raise an exception that
-          # points out config typos and missing gems.
-          if e.path == path_to_adapter
-            # We can assume that a non-builtin adapter was specified, so it's
-            # either misspelled or missing from Gemfile.
-            raise e.class, 'Could not load the '#{adapter}' Action Cable pubsub adapter. Ensure that the adapter is spelled correctly in config/cable.yml and that you've added the necessary adapter gem to your Gemfile.', e.backtrace
-    
-            def test_url_from_environment
-          spec = resolve :production, 'production' => 'abstract://foo?encoding=utf8'
-          assert_equal({
-            'adapter'  =>  'abstract',
-            'host'     =>  'foo',
-            'encoding' => 'utf8',
-            'name'     => 'production' }, spec)
+          def create
+        req = Rack::Request.new(request.env)
+        if req['client_assertion_type'] == 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
+          handle_jwt_bearer(req)
         end
+        self.status, headers, self.response_body = Api::OpenidConnect::TokenEndpoint.new.call(request.env)
+        headers.each {|name, value| response.headers[name] = value }
+        nil
+      end
     
-        # Reset all attributes. Should be called before and after actions, when used as a per-request singleton.
-    def reset
-      run_callbacks :reset do
-        self.attributes = {}
+        # Alias to +url+ that allows using the expiring_url method provided by the cloud
+    # storage implementations, but keep using filesystem storage for development and
+    # testing.
+    def expiring_url(time = 3600, style_name = default_style)
+      url(style_name)
+    end
+    
+        def self.names_for(klass)
+      instance.names_for(klass)
+    end
+    
+          [ scale_geometry, crop_geometry ]
+    end
+    
+          original_extension = extension(attachment, style_name)
+      style = attachment.styles[style_name.to_s.to_sym]
+      if style && style[:format]
+        style[:format].to_s
+      elsif extensions_for_mime_type.include? original_extension
+        original_extension
+      elsif !extensions_for_mime_type.empty?
+        extensions_for_mime_type.first
+      else
+        # It's possible, though unlikely, that the mime type is not in the
+        # database, so just use the part after the '/' in the mime type as the
+        # extension.
+        %r{/([^/]*)\z}.match(attachment.content_type)[1]
       end
     end
     
-      def call_controller(klass, action)
-    klass.action(action).call(@request.env)
-  end
-    
-    module Rails
-  def self.root
-    File.expand_path('..', __dir__)
-  end
-end
-    
-      class FeatureTopicUsers < Jobs::Base
-    
-        user.reload
-    assert user.confirmed?
-    assert_equal original_token, user.confirmation_token
-  end
-    
-      def test_returns_success
-    Warden.test_mode!
-    
-          opts.on('-s', '--stdin', :NONE,
-              'Read input from standard input instead of an input file.',
-              'This is the default if no input file is specified. Requires --from.') do
-        @options[:input] = $stdin
-      end
-    
-        # Merges this query with another. The returned query queries for
-    # the intersection between the two inputs.
-    #
-    # Both queries should be resolved.
-    #
-    # @param other [Query]
-    # @return [Query?] The merged query, or nil if there is no intersection.
-    def merge(other)
-      m1, t1 = resolved_modifier.downcase, resolved_type.downcase
-      m2, t2 = other.resolved_modifier.downcase, other.resolved_type.downcase
-      t1 = t2 if t1.empty?
-      t2 = t1 if t2.empty?
-      if (m1 == 'not') ^ (m2 == 'not')
-        return if t1 == t2
-        type = m1 == 'not' ? t2 : t1
-        mod = m1 == 'not' ? m2 : m1
-      elsif m1 == 'not' && m2 == 'not'
-        # CSS has no way of representing 'neither screen nor print'
-        return unless t1 == t2
-        type = t1
-        mod = 'not'
-      elsif t1 != t2
-        return
-      else # t1 == t2, neither m1 nor m2 are 'not'
-        type = t1
-        mod = m1.empty? ? m2 : m1
-      end
-      Query.new([mod], [type], other.expressions + expressions)
-    end
-    
-      module Sass::Plugin::Configuration
-    # Different default options in a m environment.
-    def default_options
-      @default_options ||= begin
-        version = Merb::VERSION.split('.').map {|n| n.to_i}
-        if version[0] <= 0 && version[1] < 5
-          root = MERB_ROOT
-          env  = MERB_ENV
-        else
-          root = Merb.root.to_s
-          env  = Merb.environment
-        end
-    
-            Readline::HISTORY << text
-        parse_input(environment, text)
-      end
-    end
-    
-          expect('.margin-all').to have_rule(rule)
+    module Paperclip
+  class << self
+    attr_writer :registered_attachments_styles_path
+    def registered_attachments_styles_path
+      @registered_attachments_styles_path ||= Rails.root.join('public/system/paperclip_attachments.yml').to_s
     end
   end
     
-          expect('.padding-explicit').to have_rule(rule)
-    end
+        rake_tasks { load 'tasks/paperclip.rake' }
   end
-    
-          expect('.prefix--moz-ms').to have_ruleset(rule)
-    end
-  end
-end
