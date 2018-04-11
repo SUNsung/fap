e6ee04141fba149261514843137b32cd8ebe8a13
SUNsung <weatherius@gmail.com>
2018-04-11 10:58:05 +0300

    Rewrite [48OVGjp5cyM1I48h1rt460Nw9M48HkNCFu.5ceI48qPS.cpp] File in: 2018-04-11 10:57:56

--- cccccc/48OVGjp5cyM1I48h1rt460Nw9M48HkNCFu.5ceI48qPS.cpp
+++ cccccc/48OVGjp5cyM1I48h1rt460Nw9M48HkNCFu.5ceI48qPS.cpp
@@ -1,300 +1,68 @@
 
         
         
-    {}  // namespace
+    {}
     
-    // The entry point from ChromeMain into the relauncher process.
-int RelauncherMain(const content::MainFunctionParams& main_parameters);
+    /*
+ * Order blocks for lowering to machine code.  May use different layout
+ * algorithms depending on the TransKind of `unit'.
+ *
+ * The output is guaranteed to be partitioned by area relative to `text'.  This
+ * is almost the same as partitioning by AreaIndex, except we may interleave,
+ * e.g., Main and Cold blocks in the same partition if their actual code areas
+ * in `text' are the same.
+ */
+jit::vector<Vlabel> layoutBlocks(Vunit& unit, const Vtext& text);
     
-      int next_id_;
-  std::unordered_map<int, std::unique_ptr<base::DictionaryValue>> entries_;
-    
-    void DragFileItems(const std::vector<base::FilePath>& files,
-                   const gfx::Image& icon,
-                   gfx::NativeView view);
-    
-    namespace atom {
-    }
-    
-    
-    {}  // namespace atom
-    
-    // Return the name of the lock file for the db named by
-// 'dbname'.  The result will be prefixed with 'dbname'.
-extern std::string LockFileName(const std::string& dbname);
-    
-      fname = LogFileName('foo', 192);
-  ASSERT_EQ('foo/', std::string(fname.data(), 4));
-  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
-  ASSERT_EQ(192, number);
-  ASSERT_EQ(kLogFile, type);
-    
-      std::string comparator_;
-  uint64_t log_number_;
-  uint64_t prev_log_number_;
-  uint64_t next_file_number_;
-  SequenceNumber last_sequence_;
-  bool has_comparator_;
-  bool has_log_number_;
-  bool has_prev_log_number_;
-  bool has_next_file_number_;
-  bool has_last_sequence_;
-    
-    // A Comparator object provides a total order across slices that are
-// used as keys in an sstable or a database.  A Comparator implementation
-// must be thread-safe since leveldb may invoke its methods concurrently
-// from multiple threads.
-class Comparator {
- public:
-  virtual ~Comparator();
-    }
-    
-    
-    {}  // namespace leveldb
-    
-      string model_file   = argv[1];
-  string trained_file = argv[2];
-  string mean_file    = argv[3];
-  string label_file   = argv[4];
-  Classifier classifier(model_file, trained_file, mean_file, label_file);
-    
-      image_file.read(reinterpret_cast<char*>(&magic), 4);
-  magic = swap_endian(magic);
-  CHECK_EQ(magic, 2051) << 'Incorrect image file magic.';
-  label_file.read(reinterpret_cast<char*>(&magic), 4);
-  magic = swap_endian(magic);
-  CHECK_EQ(magic, 2049) << 'Incorrect label file magic.';
-  image_file.read(reinterpret_cast<char*>(&num_items), 4);
-  num_items = swap_endian(num_items);
-  label_file.read(reinterpret_cast<char*>(&num_labels), 4);
-  num_labels = swap_endian(num_labels);
-  CHECK_EQ(num_items, num_labels);
-  image_file.read(reinterpret_cast<char*>(&rows), 4);
-  rows = swap_endian(rows);
-  image_file.read(reinterpret_cast<char*>(&cols), 4);
-  cols = swap_endian(cols);
-    
-    
-    { protected:
-  /**
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (N \times 1 \times K) @f$ or, if out_max_val
-   *      @f$ (N \times 2 \times K) @f$ unless axis set than e.g.
-   *      @f$ (N \times K \times H \times W) @f$ if axis == 1
-   *      the computed outputs @f$
-   *       y_n = \arg\max\limits_i x_{ni}
-   *      @f$ (for @f$ K = 1 @f$).
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  /// @brief Not implemented (non-differentiable function)
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
-    NOT_IMPLEMENTED;
-  }
-  bool out_max_val_;
-  size_t top_k_;
-  bool has_axis_;
-  int axis_;
-};
-    
-     protected:
-  /// @copydoc ContrastiveLossLayer
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-      vector<cudnnTensorDescriptor_t> bottom_descs_, top_descs_;
-  cudnnTensorDescriptor_t    bias_desc_;
-  cudnnFilterDescriptor_t      filter_desc_;
-  vector<cudnnConvolutionDescriptor_t> conv_descs_;
-  int bottom_offset_, top_offset_, bias_offset_;
-    
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-      /**
-   * @brief Computes the error gradient w.r.t. the exp inputs.
-   *
-   * @param top output Blob vector (length 1), providing the error gradient with
-   *      respect to the outputs
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
-   *      with respect to computed outputs @f$ y @f$
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$; Backward fills their diff with
-   *      gradients @f$
-   *        \frac{\partial E}{\partial x} =
-   *            \frac{\partial E}{\partial y} y \alpha \log_e(gamma)
-   *      @f$ if propagate_down[0]
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    #include 'precomp.hpp'
-#include 'opencv2/calib3d/calib3d_c.h'
-    
-    bool dls::is_empty(const cv::Mat * M)
-{
-    cv::MatConstIterator_<double> it = M->begin<double>(), it_end = M->end<double>();
-    for(; it != it_end; ++it)
-    {
-        if(*it < 0) return false;
-    }
-    return true;
-}
-    
-    
-    {    RNG &rng = ts->get_rng();
-    int modelPoints = cvtest::randInt(rng);
-    CvSize modelSize = cvSize(2, modelPoints);
-    int maxBasicSolutions = cvtest::randInt(rng);
-    BareModelEstimator modelEstimator(modelPoints, modelSize, maxBasicSolutions);
-    checkSubsetResult = modelEstimator.checkSubsetPublic(&_input, usedPointsCount, checkPartialSubsets);
-}
-    
-    #endif // __OPENCV_CORE_BUFFER_POOL_IMPL_HPP__
-
-    
-    // APPROXIMATIONS of the fractions of the character cell taken by
-// the descenders, ascenders, and x-height.
-const double CCStruct::kDescenderFraction = 0.25;
-const double CCStruct::kXHeightFraction = 0.5;
-const double CCStruct::kAscenderFraction = 0.25;
-const double CCStruct::kXHeightCapRatio = CCStruct::kXHeightFraction /
-    (CCStruct::kXHeightFraction + CCStruct::kAscenderFraction);
-    
-    // Fits a line to the points, ignoring the skip_first initial points and the
-// skip_last final points, returning the fitted line as a pair of points,
-// and the upper quartile error.
-double DetLineFit::Fit(int skip_first, int skip_last,
-                       ICOORD* pt1, ICOORD* pt2) {
-  // Do something sensible with no points.
-  if (pts_.empty()) {
-    pt1->set_x(0);
-    pt1->set_y(0);
-    *pt2 = *pt1;
-    return 0.0;
-  }
-  // Count the points and find the first and last kNumEndPoints.
-  int pt_count = pts_.size();
-  ICOORD* starts[kNumEndPoints];
-  if (skip_first >= pt_count) skip_first = pt_count - 1;
-  int start_count = 0;
-  int end_i = MIN(skip_first + kNumEndPoints, pt_count);
-  for (int i = skip_first; i < end_i; ++i) {
-    starts[start_count++] = &pts_[i].pt;
-  }
-  ICOORD* ends[kNumEndPoints];
-  if (skip_last >= pt_count) skip_last = pt_count - 1;
-  int end_count = 0;
-  end_i = MAX(0, pt_count - kNumEndPoints - skip_last);
-  for (int i = pt_count - 1 - skip_last; i >= end_i; --i) {
-    ends[end_count++] = &pts_[i].pt;
-  }
-  // 1 or 2 points need special treatment.
-  if (pt_count <= 2) {
-    *pt1 = *starts[0];
-    if (pt_count > 1)
-      *pt2 = *ends[0];
-    else
-      *pt2 = *pt1;
-    return 0.0;
-  }
-  // Although with between 2 and 2*kNumEndPoints-1 points, there will be
-  // overlap in the starts, ends sets, this is OK and taken care of by the
-  // if (*start != *end) test below, which also tests for equal input points.
-  double best_uq = -1.0;
-  // Iterate each pair of points and find the best fitting line.
-  for (int i = 0; i < start_count; ++i) {
-    ICOORD* start = starts[i];
-    for (int j = 0; j < end_count; ++j) {
-      ICOORD* end = ends[j];
-      if (*start != *end) {
-        ComputeDistances(*start, *end);
-        // Compute the upper quartile error from the line.
-        double dist = EvaluateLineFit();
-        if (dist < best_uq || best_uq < 0.0) {
-          best_uq = dist;
-          *pt1 = *start;
-          *pt2 = *end;
-        }
-      }
-    }
-  }
-  // Finally compute the square root to return the true distance.
-  return best_uq > 0.0 ? sqrt(best_uq) : best_uq;
-}
-    
-    
-    {    int n[CT_SIZE];
-  };
-    
-    
-    {    _state = EXPECTS_CRASH_DUMP_END;
-    return 0;
-}
-    
-    // Licensed under the MIT License (the 'License'); you may not use this file except in 
-// compliance with the License. You may obtain a copy of the License at
-// http://opensource.org/licenses/MIT
-    
-    //
-//  comm_frequency_limit.cc
-//  comm
-//
-//  Created by liucan on 13-11-23.
-//
-    
-    #define DEFINE_SERVICE() \
-    static  const char* ServiceName();\
-    static const std::set<std::string>& DependServicesName();
+    #endif
 #endif
 
     
-    std::string AbstractOptionHandler::toTagString() const
-{
-  std::string s;
-  for (int i = 0; i < MAX_HELP_TAG; ++i) {
-    if (tags_ & (1 << i)) {
-      s += strHelpTag(i);
-      s += ', ';
+      /* Third, try to get magic file relative to dll location */
+  dllpath = malloc(sizeof(*dllpath) * (MAX_PATH + 1));
+  dllpath[MAX_PATH] = 0;  /* just in case long path gets truncated and not null terminated */
+  if (GetModuleFileNameA(NULL, dllpath, MAX_PATH)) {
+    PathRemoveFileSpecA(dllpath);
+    if (strlen(dllpath) > 3 &&
+        stricmp(&dllpath[strlen(dllpath) - 3], 'bin') == 0) {
+      if (asprintf(&tmppath,
+          '%s/../share/misc/magic.mgc', dllpath) >= 0)
+        APPENDPATH();
+    } else {
+      if (asprintf(&tmppath,
+          '%s/share/misc/magic.mgc', dllpath) >= 0)
+        APPENDPATH();
+      else if (asprintf(&tmppath,
+          '%s/magic.mgc', dllpath) >= 0)
+        APPENDPATH();
     }
   }
-  if (!s.empty()) {
-    s.resize(s.size() - 2);
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    
+    
+    {}  // namespace grpc_node_generator
+    
+    #include <grpc/grpc_security.h>
+    
+      NSDictionary *flagNames = @{
+#define GRPC_XMACRO_ITEM(methodName, FlagName) \
+    @(kSCNetworkReachabilityFlags ## FlagName): @#methodName,
+#include 'GRXReachabilityFlagNames.xmacro.h'
+#undef GRPC_XMACRO_ITEM
+  };
+    
+      std::unique_ptr<grpc::Server> StartServer(int port);
+    
+    grpc::string DescribeMethod(const grpc::protobuf::MethodDescriptor* method) {
+  std::stringstream result;
+  result << '  rpc ' << method->name()
+         << (method->client_streaming() ? '(stream ' : '(')
+         << method->input_type()->full_name() << ') returns '
+         << (method->server_streaming() ? '(stream ' : '(')
+         << method->output_type()->full_name() << ') {}\n';
+  if (method->options().deprecated()) {
+    result << ' DEPRECATED';
   }
-  return s;
+  return result.str();
 }
-    
-    class ActivePeerConnectionCommand : public Command {
-private:
-  RequestGroup* requestGroup_;
-  std::shared_ptr<BtRuntime> btRuntime_;
-  std::shared_ptr<PieceStorage> pieceStorage_;
-  std::shared_ptr<PeerStorage> peerStorage_;
-  std::shared_ptr<BtAnnounce> btAnnounce_;
-    }
-    
-    #include 'common.h'
