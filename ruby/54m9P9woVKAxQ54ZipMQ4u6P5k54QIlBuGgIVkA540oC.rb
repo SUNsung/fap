1615bcd5dd1b39f28b092eb0e308d21b68c44fbc
SUNsung <weatherius@gmail.com>
2018-04-11 08:08:05 +0300

    Rewrite [54m9P9woVKAxQ54ZipMQ4u6P5k54QIlBuGgIVkA540oC.rb] File in: 2018-04-11 08:08:04

--- ruby/54m9P9woVKAxQ54ZipMQ4u6P5k54QIlBuGgIVkA540oC.rb
+++ ruby/54m9P9woVKAxQ54ZipMQ4u6P5k54QIlBuGgIVkA540oC.rb
@@ -1,128 +1,114 @@
 
         
-              def self.example_code
-        [
-          'create_keychain(
-            name: 'KeychainName',
-            default_keychain: true,
-            unlock: true,
-            timeout: 3600,
-            lock_when_sleeps: true
-          )'
-        ]
+        # Just a slash
+Benchmark.ips do |x|
+  path = '/'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
+end
+    
+      def self.source_dir; SOURCE_DIR; end
+end
+    
+          #
+      # The version constraint required to activate a given gem.
+      # Usually the gem version requirement is '> 0,' because any version
+      # will do. In the case of jekyll-docs, however, we require the exact
+      # same version as Jekyll.
+      #
+      # Returns a String version constraint in a parseable form for
+      # RubyGems.
+      def version_constraint(gem_name)
+        return '= #{Jekyll::VERSION}' if gem_name.to_s.eql?('jekyll-docs')
+        '> 0'
       end
     
-          it 'raises an exception when use passes workspace' do
-        expect do
-          Fastlane::FastFile.new.parse('lane :test do
-            increment_build_number(xcodeproj: 'project.xcworkspace')
-          end').runner.execute(:test)
-        end.to raise_error('Please pass the path to the project, not the workspace')
+          if options[:roles].is_a?(Symbol)
+        options[:roles] = [options[:roles]]
       end
-    
-          it 'prefers a custom version number over a boring version bump' do
-        Fastlane::FastFile.new.parse('lane :test do
-          increment_version_number(version_number: '1.77.3', bump_type: 'major')
-        end').runner.execute(:test)
-    
-      test 'should not call after_confirmation if not confirmed' do
-    user = create_user
-    assert user.confirm
-    user.define_singleton_method :after_confirmation do
-      self.username = self.username.to_s + 'updated'
-    end
-    old = user.username
-    refute user.confirm
-    assert_equal user.username, old
-  end
-    
-      def failure_message
-    exception = request.respond_to?(:get_header) ? request.get_header('omniauth.error') : request.env['omniauth.error']
-    error   = exception.error_reason if exception.respond_to?(:error_reason)
-    error ||= exception.error        if exception.respond_to?(:error)
-    error ||= (request.respond_to?(:get_header) ? request.get_header('omniauth.error.type') : request.env['omniauth.error.type']).to_s
-    error.to_s.humanize if error
-  end
-    
-        # The path used after resending confirmation instructions.
-    def after_resending_confirmation_instructions_path_for(resource_name)
-      is_navigational_format? ? new_session_path(resource_name) : '/'
-    end
-    
-    class Devise::PasswordsController < DeviseController
-  prepend_before_action :require_no_authentication
-  # Render the #edit only if coming from a reset password email link
-  append_before_action :assert_reset_token_passed, only: :edit
-    
-        if resource.errors.empty?
-      set_flash_message! :notice, :unlocked
-      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
-    end
-  end
-    
-                  define_method method do |resource_or_scope, *args|
-                scope = Devise::Mapping.find_scope!(resource_or_scope)
-                router_name = Devise.mappings[scope].router_name
-                context = router_name ? send(router_name) : _devise_route_context
-                context.send('#{action}#{scope}_#{module_name}_#{path_or_url}', *args)
-              end
-            end
-          end
-        end
-      end
-    
-      module ClassMethods
-    def load_types_in(module_name, my_name = module_name.singularize)
-      const_set(:MODULE_NAME, module_name)
-      const_set(:BASE_CLASS_NAME, my_name)
-      const_set(:TYPES, Dir[Rails.root.join('app', 'models', module_name.underscore, '*.rb')].map { |path| module_name + '::' + File.basename(path, '.rb').camelize })
-    end
-    
-        valid_oauth_providers :evernote
     
         respond_to do |format|
       format.html { redirect_to services_path }
-      format.json { head :no_content }
+      format.json { render json: @service }
     end
   end
+end
+
     
-      # Before we load the schema, define the timestamp_id function.
-  # Idiomatically, we might do this in a migration, but then it
-  # wouldn't end up in schema.rb, so we'd need to figure out a way to
-  # get it in before doing db:setup as well. This is simpler, and
-  # ensures it's always in place.
-  Rake::Task['db:schema:load'].enhance ['db:define_timestamp_id']
+        def log_file_info(s)
+      puts '    #{magenta s}'
+    end
     
-          it 'renders application layout' do
-        get :show, params: { id: 'test', max_id: late.id }
-        expect(response).to render_template layout: 'application'
+      get(/.+/) do
+    send_sinatra_file(request.path) {404}
+  end
+    
+        def paragraphize(input)
+      '<p>#{input.lstrip.rstrip.gsub(/\n\n/, '</p><p>').gsub(/\n/, '<br/>')}</p>'
+    end
+  end
+end
+    
+        # Returns the content_type of the file as originally assigned, and lives
+    # in the <attachment>_content_type attribute of the model.
+    def content_type
+      instance_read(:content_type)
+    end
+    
+        def define_class_getter
+      @klass.extend(ClassMethods)
+    end
+    
+        # Returns an extension based on the content type. e.g. 'jpeg' for
+    # 'image/jpeg'. If the style has a specified format, it will override the
+    # content-type detection.
+    #
+    # Each mime type generally has multiple extensions associated with it, so
+    # if the extension from the original filename is one of these extensions,
+    # that extension is used, otherwise, the first in the list is used.
+    def content_type_extension attachment, style_name
+      mime_type = MIME::Types[attachment.content_type]
+      extensions_for_mime_type = unless mime_type.empty?
+        mime_type.first.extensions
+      else
+        []
       end
-    end
     
-      def type
-    'Emoji'
-  end
+            def matches? subject
+          @subject = subject
+          @subject = @subject.class unless Class === @subject
+          responds? && has_column?
+        end
     
-      config = Merb::Plugins.config[:sass] || Merb::Plugins.config['sass'] || {}
-    
-            if args.length > 0
-          attachment.to_s(args.first)
-        else
-          attachment
+            def no_error_when_valid?
+          @file = StringIO.new('.')
+          @subject.send(@attachment_name).assign(@file)
+          @subject.valid?
+          expected_message = [
+            @attachment_name.to_s.titleize,
+            I18n.t(:blank, scope: [:errors, :messages])
+          ].join(' ')
+          @subject.errors.full_messages.exclude?(expected_message)
         end
       end
     end
+  end
+end
+
     
-            def passes_validation_with_size(new_size)
-          file = StringIO.new('.')
-          override_method(file, :size){ new_size }
-          override_method(file, :to_tempfile){ file }
+        def clear_processors!
+      @known_processors.try(:clear)
+    end
     
-            required = directories.map do |directory|
-          pathname = File.expand_path(Rails.root.join(directory, filename))
-          file_exists = File.exist?(pathname)
-          require pathname if file_exists
-          file_exists
+        module TableDefinition
+      def attachment(*attachment_names)
+        options = attachment_names.extract_options!
+        attachment_names.each do |attachment_name|
+          COLUMNS.each_pair do |column_name, column_type|
+            column_options = options.merge(options[column_name.to_sym] || {})
+            column('#{attachment_name}_#{column_name}', column_type, column_options)
+          end
         end
+      end
