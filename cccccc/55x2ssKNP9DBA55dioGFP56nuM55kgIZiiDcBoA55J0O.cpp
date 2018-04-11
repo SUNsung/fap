2614debead96bdf600d093cb5ce08452ca81362e
SUNsung <weatherius@gmail.com>
2018-04-11 12:58:31 +0300

    Rewrite [55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp] File in: 2018-04-11 12:58:30

--- cccccc/55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp
+++ cccccc/55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp
@@ -1,110 +1,139 @@
 
         
-        static THDTensor *THDTensor_(_alloc)() {
-  THDTensor *new_tensor = new THDTensor();
-  std::memset(reinterpret_cast<void*>(new_tensor), 0, sizeof(THDTensor));
-  new_tensor->size = nullptr;
-  new_tensor->stride = nullptr;
-  new_tensor->nDimension = 0;
-    }
-    
-      assert(peekType(msg) == thpp::Type::LONG_STORAGE);
-  THLongStorage *storage2 = unpackTHLongStorage(msg);
-  assert(storage2->size == STORAGE_SIZE);
-  for (int64_t i = 0; i < STORAGE_SIZE; i++)
-    assert(storage2->data[i] == i);
-  
-  int vec_size = unpackInteger(msg);
-  assert(vec_size == VEC_SIZE);
-  for (int i = 0; i < VEC_SIZE; i++)
-    assert(unpackInteger(msg) == 7);
-    
-    #include 'ATen/Config.h'
-namespace at {
-namespace native {
-    }
-    }
-    
-    ${Storage}::${Storage}(Context* context, ${THStorage}* storage):
-    storage(storage), context(context) {}
-    
-    THDTensorDescriptor THDTensorDescriptor_newFromTHCudaByteTensor(THCudaByteTensor *tensor) {
-  return at::getType(at::Backend::CUDA, at::ScalarType::Byte).unsafeTensorFromTH((void*)tensor, true);
+        void UnregisterShellMenu(std::wstring opt, wchar_t* keyBaseName)
+{
+	HKEY root = GetRootKey(opt);
+	HKEY cmderKey;
+	FAIL_ON_ERROR(RegCreateKeyEx(root, keyBaseName, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &cmderKey, NULL));
+	FAIL_ON_ERROR(RegDeleteTree(cmderKey, NULL));
+	RegCloseKey(cmderKey);
+	RegCloseKey(root);
 }
-#endif
-
+    
+    const std::string kKernelSyscallAddrModifiedPath = '/sys/kernel/camb/syscall_addr_modified';
+const std::string kKernelTextHashPath = '/sys/kernel/camb/text_segment_hash';
+    
+      // Create the IOCTL (and more) device node.
+  osquery.devfs = devfs_make_node(makedev(osquery.major_number, 0),
+                                  DEVFS_CHAR,
+                                  UID_ROOT,
+                                  GID_WHEEL,
+                                  0600,
+                                  'osquery',
+                                  0);
+  if (osquery.devfs == NULL) {
+    dbg_printf('Could not get a devfs entry!\n');
+    goto error_exit;
+  }
+    
+    #include 'osquery/tests/test_util.h'
+    
+    bool platformModuleClose(ModuleHandle module) {
+  return (::dlclose(module) == 0);
+}
+    
+    // customize random engine.
+void CustomGlobalRandomEngine::seed(CustomGlobalRandomEngine::result_type val) {
+  // ignore the seed
+}
     
     
-    {
-    {
-    {  inline void PutChar(char ch) {
-    out_buf += ch;
-    if (out_buf.length() >= kBufferSize) Flush();
-  }
-  inline void Flush(void) {
-    if (out_buf.length() != 0) {
-      fp->Write(&out_buf[0], out_buf.length());
-      out_buf.clear();
+    {    int nindex = index_.num_chunk();
+    int nvalue = value_.num_chunk();
+    int ntotal = nindex + nvalue;
+    #pragma omp parallel for schedule(dynamic, 1)  num_threads(nthread_write_)
+    for (int i = 0; i < ntotal; ++i) {
+      if (i < nindex) {
+        index_.Compress(i, use_lz4_hc_);
+      } else {
+        value_.Compress(i - nindex, use_lz4_hc_);
+      }
     }
+    index_.Write(fo);
+    value_.Write(fo);
+    // statistics
+    raw_bytes_index_ += index_.RawBytes() * sizeof(bst_uint) / sizeof(StorageIndex);
+    raw_bytes_value_ += value_.RawBytes();
+    encoded_bytes_index_ += index_.EncodedBytes();
+    encoded_bytes_value_ += value_.EncodedBytes();
+    raw_bytes_ += page.offset.size() * sizeof(size_t);
   }
-};
-}  // namespace common
-}  // namespace xgboost
-#endif  // XGBOOST_COMMON_BASE64_H_
-
     
-    namespace xgboost {
-namespace common {
-/*!
- * \brief base implementation of config reader
- */
-class ConfigReaderBase {
- public:
-  /*!
-   * \brief get current name, called after Next returns true
-   * \return current parameter name
-   */
-  inline const char *name(void) const {
-    return s_name.c_str();
-  }
-  /*!
-   * \brief get current value, called after Next returns true
-   * \return current parameter value
-   */
-  inline const char *val(void) const {
-    return s_val.c_str();
-  }
-  /*!
-   * \brief move iterator to next position
-   * \return true if there is value in next position
-   */
-  inline bool Next(void) {
-    while (!this->IsEnd()) {
-      GetNextToken(&s_name);
-      if (s_name == '=') return false;
-      if (GetNextToken(&s_buf) || s_buf != '=')  return false;
-      if (GetNextToken(&s_val) || s_val == '=')  return false;
-      return true;
-    }
-    return false;
-  }
-  // called before usage
-  inline void Init(void) {
-    ch_buf = this->GetChar();
-  }
-    }
-    }
+        index_.resize((boundary_[nfeature - 1].index_end
+                   + (packing_factor_ - 1)) / packing_factor_);
+    row_ind_.resize(boundary_[nfeature - 1].row_ind_end);
+    
+    TEST(Param, VectorIOStream) {
+  std::vector<int> vals = {3, 2, 1};
+  std::stringstream ss;
+  std::vector<int> vals_in;
+  
+  ss << vals;
+  EXPECT_EQ(ss.str(), '(3,2,1)');
     }
     
-    #include <dmlc/io.h>
+    // Used to encapsulate a particular instance of an opened database.
+//
+// This object should not be used directly in C++; it exists solely to provide
+// a mapping from a JavaScript object to a C++ code that can use the RocksDB
+// API.
+class DBWrapper : public node::ObjectWrap {
+  public:
+    static void Init(Handle<Object> exports);
+    }
+    
+      virtual const char* Name() const override;
+    
+    #pragma once
+#include <deque>
 #include <string>
-#include <cstring>
-#include './sync.h'
     
-    template<typename IndexType>
-class DensifyParser : public dmlc::Parser<IndexType> {
- public:
-  DensifyParser(dmlc::Parser<IndexType>* parser, uint32_t num_col)
-      : parser_(parser), num_col_(num_col) {
-  }
+      /// Search the list for the (index)'th item (0-based) in (list:key)
+  /// A negative index indicates: 'from end-of-list'
+  /// If index is within range: return true, and return the value in *result.
+  /// If (index < -length OR index>=length), then index is out of range:
+  ///   return false (and *result is left unchanged)
+  /// May throw RedisListException
+  bool Index(const std::string& key, int32_t index,
+             std::string* result);
+    
+      bool StatisticsJni::HistEnabledForType(uint32_t type) const {
+    if (type >= HISTOGRAM_ENUM_MAX) {
+      return false;
     }
+    
+    if (m_ignore_histograms.count(type) > 0) {
+        return false;
+    }
+    }
+    
+    
+    {}  // namespace rocksdb
+    
+      virtual size_t GetSimCapacity() const override {
+    return key_only_cache_->GetCapacity();
+  }
+  virtual size_t GetSimUsage() const override {
+    return key_only_cache_->GetUsage();
+  }
+  virtual void SetSimCapacity(size_t capacity) override {
+    key_only_cache_->SetCapacity(capacity);
+  }
+    
+      TransactionDB* snapshotDB;
+  std::vector<ColumnFamilyDescriptor> column_families;
+  column_families.push_back(
+      ColumnFamilyDescriptor(kDefaultColumnFamilyName, ColumnFamilyOptions()));
+  column_families.push_back(
+      ColumnFamilyDescriptor('CFA', ColumnFamilyOptions()));
+  column_families.push_back(
+      ColumnFamilyDescriptor('CFB', ColumnFamilyOptions()));
+  std::vector<rocksdb::ColumnFamilyHandle*> cf_handles;
+  ASSERT_OK(TransactionDB::Open(options, txn_db_options, snapshot_name_,
+                                column_families, &cf_handles, &snapshotDB));
+  ASSERT_OK(snapshotDB->Get(read_options, 'foo', &value));
+  ASSERT_EQ(value, 'bar');
+  ASSERT_OK(snapshotDB->Get(read_options, cf_handles[1], 'foocfa', &value));
+  ASSERT_EQ(value, 'barcfa');
+    
+      bool CanHandleDuplicatedKey() const override { return true; }
