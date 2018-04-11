e6d2e66ef829fdd621a45c393ff351e2df699238
SUNsung <weatherius@gmail.com>
2018-04-11 23:14:19 +0300

    Rewrite [23Z2e2LNdBusQ23l275nJw1g23uYvxMiaZkp.23i.4.rb] File in: 2018-04-11 23:14:18

--- ruby/23Z2e2LNdBusQ23l275nJw1g23uYvxMiaZkp.23i.4.rb
+++ ruby/23Z2e2LNdBusQ23l275nJw1g23uYvxMiaZkp.23i.4.rb
@@ -1,119 +1,96 @@
 
         
-            To install Clojure you should install Leiningen:
-      brew install leiningen
-    and then follow the tutorial:
-      https://github.com/technomancy/leiningen/blob/stable/doc/TUTORIAL.md
-    EOS
-  when 'osmium' then <<-EOS.undent
-    The creator of Osmium requests that it not be packaged and that people
-    use the GitHub master branch instead.
-    EOS
-  when 'gfortran' then <<-EOS.undent
-    GNU Fortran is now provided as part of GCC, and can be installed with:
-      brew install gcc
-    EOS
-  when 'play' then <<-EOS.undent
-    Play 2.3 replaces the play command with activator:
-      brew install typesafe-activator
+            # Returns an array of addresses ranges
+    #
+    # @return [Array<String>]
+    def addresses
+      (boundary || '').split('\n')
+    end
     
-      def install
-    formula_deps = deps.map(&:to_formula)
-    keg_only_deps = formula_deps.select(&:keg_only?)
+        inp.attributes.keys.each do |ikey|
+      if (ikey.downcase == 'value')
+        inp[ikey] = ''
+        next
+      end
     
-        option_names.any? do |name|
-      if option_defined? 'with-#{name}'
-        include? 'with-#{name}'
-      elsif option_defined? 'without-#{name}'
-        !include? 'without-#{name}'
+        # We want to return immediatly if we do not have a packet which is handled by us
+    return unless pkt.is_tcp?
+    return if (pkt.tcp_sport != 143 and pkt.tcp_dport != 143)
+    s = find_session((pkt.tcp_sport == 143) ? get_session_src(pkt) : get_session_dst(pkt))
+    s[:sname] ||= 'imap4'
+    
+    
+jarsignerOpts	= ['-keystore', 'msfkeystore', '-storepass', 'msfstorepass',
+		   '-keypass', 'msfkeypass', '-signedJar', 's#{outputJar}',
+		   outputJar, 'signFiles']
+    
+    		elsif line =~ /\w+:\t/
+			block_do(line)
+    
+    post '/' do
+  connections.each { |out| out << 'data: #{params[:msg]}\n\n' }
+  204 # response without entity body
+end
+    
+      context 'escaping' do
+    it 'escapes html entities' do
+      mock_app do |env|
+        request = Rack::Request.new(env)
+        [200, {'Content-Type' => 'text/plain'}, [request.params['foo']]]
+      end
+      get '/', :foo => '<bar>'
+      expect(body).to eq('&lt;bar&gt;')
+    end
+    
+      %w(GET HEAD POST PUT DELETE).each do |method|
+    it 'accepts #{method} requests with no Origin' do
+      expect(send(method.downcase, '/')).to be_ok
+    end
+  end
+    
+      context 'escaping' do
+    before do
+      mock_app { |e| [200, {'Content-Type' => 'text/plain'}, [e['PATH_INFO']]] }
+    end
+    
+      # rspec-mocks config goes here. You can use an alternate test double
+  # library (such as bogus or mocha) by changing the `mock_with` option here.
+  config.mock_with :rspec do |mocks|
+    # Enable only the newer, non-monkey-patching expect syntax.
+    # For more details, see:
+    #   - http://teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
+    mocks.syntax = :expect
+    
+    When /^(?:|I )press '([^']*)'$/ do |button|
+  click_button(button)
+end
+    
+        def add_active_record_callbacks
+      name = @name
+      @klass.send(:after_save) { send(name).send(:save) }
+      @klass.send(:before_destroy) { send(name).send(:queue_all_for_delete) }
+      if @klass.respond_to?(:after_commit)
+        @klass.send(:after_commit, on: :destroy) do
+          send(name).send(:flush_deletes)
+        end
       else
-        false
+        @klass.send(:after_destroy) { send(name).send(:flush_deletes) }
       end
     end
+    
+        def load_processor(name)
+      if defined?(Rails.root) && Rails.root
+        filename = '#{name.to_s.underscore}.rb'
+        directories = %w(lib/paperclip lib/paperclip_processors)
+    
+      # GET /books/new
+  def new
+    @book = Book.new
   end
     
-      def external_commands
-    paths.reduce([]) do |cmds, path|
-      Dir['#{path}/brew-*'].each do |file|
-        next unless File.executable?(file)
-        cmd = File.basename(file, '.rb')[5..-1]
-        cmds << cmd unless cmd.include?('.')
-      end
-      cmds
-    end.sort
-  end
+            def on_case(case_node)
+          case_node.when_branches.each_with_object([]) do |when_node, previous|
+            when_node.each_condition do |condition|
+              next unless repeated_condition?(previous, condition)
     
-      protected
-    
-        begin
-      response = U2F::RegisterResponse.load_from_json(params[:device_response])
-      registration_data = u2f.register!(challenges, response)
-      registration.update(certificate: registration_data.certificate,
-                          key_handle: registration_data.key_handle,
-                          public_key: registration_data.public_key,
-                          counter: registration_data.counter,
-                          user: user,
-                          name: params[:name])
-    rescue JSON::ParserError, NoMethodError, ArgumentError
-      registration.errors.add(:base, 'Your U2F device did not send a valid JSON response.')
-    rescue U2F::Error => e
-      registration.errors.add(:base, e.message)
-    end
-    
-      def test_font_helper_with_suffix_sharp
-    assert_match %r(url\(['']?/assets/.*svg#.+['']?\)), @css
-  end
-    
-                res << inp.to_html
-          end
-          res << '</form>'
-    
-          case matched
-    
-    #certCN cannot contain commas
-certCN 		= 'Metasploit Inc.'
-#keytoolOpts 	= '-genkey -alias signFiles -keystore msfkeystore ' +
-#		  '-storepass msfstorepass -dname \'cn=#{certCN}\' ' +
-#		  '-keypass msfkeypass'
-    
-      def get_result
-    result
-  end
-end
-    
-      [jekyllPid, compassPid].each { |pid| Process.wait(pid) }
-end
-    
-        # Outputs a list of categories as comma-separated <a> links. This is used
-    # to output the category list for each post on a category page.
-    #
-    #  +categories+ is the list of categories to format.
-    #
-    # Returns string
-    #
-    def category_links(categories)
-      categories.sort.map { |c| category_link c }.join(', ')
-    end
-    
-    class ConfigTag < Liquid::Tag
-  def initialize(tag_name, options, tokens)
-    super
-    options = options.split(' ').map {|i| i.strip }
-    @key = options.slice!(0)
-    @tag = nil
-    @classname = nil
-    options.each do |option|
-      @tag = $1 if option =~ /tag:(\S+)/ 
-      @classname = $1 if option =~ /classname:(\S+)/
-    end
-  end
-    
-    module Jekyll
-    
-        def sizes
-      attrs = 'width='#{@sizes[0]}'' if @sizes[0]
-      attrs += ' height='#{@sizes[1]}'' if @sizes[1]
-      attrs
-    end
-  end
-end
+            private
