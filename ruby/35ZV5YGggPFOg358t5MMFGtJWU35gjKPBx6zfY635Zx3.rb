99a6ea76f217288b3bcfd5fd72b24ac34a1aa491
SUNsung <weatherius@gmail.com>
2018-04-12 09:10:59 +0300

    Rewrite [35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb] File in: 2018-04-12 09:10:58

--- ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
+++ ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
@@ -1,44 +1,27 @@
 
         
-          test 'helpers' do
-    assert_response_code_range 200..299, :successful?
-    assert_response_code_range [404],    :not_found?
-    assert_response_code_range 300..399, :redirection?
-    assert_response_code_range 500..599, :server_error?
-    assert_response_code_range 400..499, :client_error?
-  end
-    
-        def env_for(url)
-      Rack::MockRequest.env_for(url)
-    end
-  end
-    
-          add_delivery_method :smtp, Mail::SMTP,
-        address:              'localhost',
-        port:                 25,
-        domain:               'localhost.localdomain',
-        user_name:            nil,
-        password:             nil,
-        authentication:       nil,
-        enable_starttls_auto: true
-    
-    module ActionMailer
-  # Implements the ActiveSupport::LogSubscriber for logging notifications when
-  # email is delivered or received.
-  class LogSubscriber < ActiveSupport::LogSubscriber
-    # An email was delivered.
-    def deliver(event)
-      info do
-        recipients = Array(event.payload[:to]).join(', ')
-        'Sent mail to #{recipients} (#{event.duration.round(1)}ms)'
+                  # Success, exit status 0
+          0
+        end
       end
-    
-      def full_plugin_name
-    @full_plugin_name ||= 'logstash-#{type}-#{name.downcase}'
+    end
   end
 end
 
     
-              def find_plugins_gem_specs
-            @specs ||= ::Gem::Specification.find_all.select{|spec| logstash_plugin_gem_spec?(spec)}
+        # Convert a snippet of bootstrap LESS to Scss
+    def convert_less(less)
+      less = convert_to_scss(less)
+      less = yield(less) if block_given?
+      less
+    end
+    
+            def create
+          authorize! :create, StockMovement
+          @stock_movement = scope.new(stock_movement_params)
+          if @stock_movement.save
+            respond_with(@stock_movement, status: 201, default_template: :show)
+          else
+            invalid_resource!(@stock_movement)
           end
+        end
