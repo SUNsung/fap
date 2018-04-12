262c0b77bdcd845fd3878d63fe88094f56bbab2d
SUNsung <weatherius@gmail.com>
2018-04-12 06:02:50 +0300

    Rewrite [59nhDKvBMQP25915P3UMIXzOQ59NiGxF.eucw59BMl.rb] File in: 2018-04-12 06:02:49

--- ruby/59nhDKvBMQP25915P3UMIXzOQ59NiGxF.eucw59BMl.rb
+++ ruby/59nhDKvBMQP25915P3UMIXzOQ59NiGxF.eucw59BMl.rb
@@ -1,101 +1,41 @@
 
         
-            test 'head :not_modified (304) does not return a content-type header' do
-      headers = HeadController.action(:not_modified).call(Rack::MockRequest.env_for('/')).second
-      assert_nil headers['Content-Type']
-      assert_nil headers['Content-Length']
-    end
-    
-      class TestInheritedMiddleware < TestMiddleware
-    def setup
-      @app = InheritedController.action(:index)
-    end
-  end
-end
-
-    
-    module ActionMailer
-  # Implements the ActiveSupport::LogSubscriber for logging notifications when
-  # email is delivered or received.
-  class LogSubscriber < ActiveSupport::LogSubscriber
-    # An email was delivered.
-    def deliver(event)
-      info do
-        recipients = Array(event.payload[:to]).join(', ')
-        'Sent mail to #{recipients} (#{event.duration.round(1)}ms)'
+              def []=(key, value)
+        super(convert_key(key), value)
       end
     
-          # Make list points stand on their own line
-      formatted.gsub!(/[ ]*([*]+) ([^*]*)/) { '  #{$1} #{$2.strip}\n' }
-      formatted.gsub!(/[ ]*([#]+) ([^#]*)/) { '  #{$1} #{$2.strip}\n' }
+            while true
+          begin
+            if Platform.windows?
+              # Windows doesn't support non-blocking reads on
+              # file descriptors or pipes so we have to get
+              # a bit more creative.
     
-          module ClassMethods
-        def tests(mailer)
-          case mailer
-          when String, Symbol
-            self._mailer_class = mailer.to_s.camelize.constantize
-          when Module
-            self._mailer_class = mailer
-          else
-            raise NonInferrableMailerError.new(mailer)
-          end
+        set = Set.new(1..10)
+    enum = set.keep_if
+    assert_equal(set.size, enum.size)
+    assert_same(set, enum.each { |i| i % 3 != 0 })
+    assert_equal(Set[1,2,4,5,7,8,10], set)
+  end
+    
+    describe :string_unpack_32bit_be, shared: true do
+  it 'decodes one int for a single format character' do
+    'dcba'.unpack(unpack_format).should == [1684234849]
+  end
+    
+        t.join
+  end
+    
+      describe '::binary_osxfuse_installed?' do
+    it 'returns false if fuse.h does not exist' do
+      allow(File).to receive(:exist?).and_return(false)
+      expect(described_class).not_to be_binary_osxfuse_installed
+    end
+    
+            unless guarded && environment.var(name)
+          environment.set_var(name, val.perform(environment))
         end
     
-            # Find a user by its confirmation token and try to confirm it.
-        # If no user is found, returns a new user with an error.
-        # If the user is already confirmed, create an error for the user
-        # Options must have the confirmation_token
-        def confirm_by_token(confirmation_token)
-          confirmable = find_first_by_auth_conditions(confirmation_token: confirmation_token)
-          unless confirmable
-            confirmation_digest = Devise.token_generator.digest(self, :confirmation_token, confirmation_token)
-            confirmable = find_or_initialize_with_error_by(:confirmation_token, confirmation_digest)
-          end
-    
-      def passthru
-    render status: 404, plain: 'Not found. Authentication passthru.'
-  end
-    
-      def all_signed_out?
-    users = Devise.mappings.keys.map { |s| warden.user(scope: s, run_callbacks: false) }
-    
-    module Devise
-  module Controllers
-    # A module that may be optionally included in a controller in order
-    # to provide remember me behavior. Useful when signing in is done
-    # through a callback, like in OmniAuth.
-    module Rememberable
-      # Return default cookie values retrieved from session options.
-      def self.cookie_values
-        Rails.configuration.session_options.slice(:path, :domain, :secure)
+          def started?
+        true
       end
-    
-          generate_helpers!(Devise::URL_HELPERS)
-    
-          def redirect(env)
-        request = Request.new(env)
-        warn env, 'attack prevented by #{self.class}'
-        [302, {'Content-Type' => 'text/html', 'Location' => request.path}, []]
-      end
-    
-        { # yes, this is ugly, feel free to change that
-      '/..' => '/', '/a/../b' => '/b', '/a/../b/' => '/b/', '/a/.' => '/a/',
-      '/%2e.' => '/', '/a/%2E%2e/b' => '/b', '/a%2f%2E%2e%2Fb/' => '/b/',
-      '//' => '/', '/%2fetc%2Fpasswd' => '/etc/passwd'
-    }.each do |a, b|
-      it('replaces #{a.inspect} with #{b.inspect}') { expect(get(a).body).to eq(b) }
-    end
-    
-      it 'should set the X-XSS-Protection' do
-    expect(get('/', {}, 'wants' => 'text/html;charset=utf-8').headers['X-XSS-Protection']).to eq('1; mode=block')
-  end
-    
-      # rspec-expectations config goes here. You can use an alternate
-  # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you prefer.
-  config.expect_with :rspec do |expectations|
-    # Enable only the newer, non-monkey-patching expect syntax.
-    # For more details, see:
-    #   - http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax
-    expectations.syntax = :expect
-  end
