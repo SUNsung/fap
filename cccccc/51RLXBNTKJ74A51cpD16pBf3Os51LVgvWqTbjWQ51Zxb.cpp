2db667b2116cbe4693601b1b4365ed6b4b130373
SUNsung <weatherius@gmail.com>
2018-04-12 19:03:15 +0300

    Rewrite [51RLXBNTKJ74A51cpD16pBf3Os51LVgvWqTbjWQ51Zxb.cpp] File in: 2018-04-12 19:03:14

--- cccccc/51RLXBNTKJ74A51cpD16pBf3Os51LVgvWqTbjWQ51Zxb.cpp
+++ cccccc/51RLXBNTKJ74A51cpD16pBf3Os51LVgvWqTbjWQ51Zxb.cpp
@@ -1,123 +1,135 @@
 
         
-        TEST(EnvPosixTest, TestOpenOnRead) {
-  // Write some test data to a single file that will be opened |n| times.
-  std::string test_dir;
-  ASSERT_OK(env_->GetTestDirectory(&test_dir));
-  std::string test_file = test_dir + '/open_on_read.txt';
-    }
+          content::WebContents* web_contents_;  // Weak Reference.
+  scoped_refptr<content::DevToolsAgentHost> agent_host_;
     
+    class AtomBrowserContext;
     
-    { private:
-  // Dummy head of doubly-linked list of snapshots
-  SnapshotImpl list_;
-};
-    
-    static void TestEncodeDecode(const VersionEdit& edit) {
-  std::string encoded, encoded2;
-  edit.EncodeTo(&encoded);
-  VersionEdit parsed;
-  Status s = parsed.DecodeFrom(encoded);
-  ASSERT_TRUE(s.ok()) << s.ToString();
-  parsed.EncodeTo(&encoded2);
-  ASSERT_EQ(encoded, encoded2);
+    bool SavePageHandler::Handle(const base::FilePath& full_path,
+                             const content::SavePageType& save_type) {
+  auto download_manager = content::BrowserContext::GetDownloadManager(
+      web_contents_->GetBrowserContext());
+  download_manager->AddObserver(this);
+  // Chromium will create a 'foo_files' directory under the directory of saving
+  // page 'foo.html' for holding other resource files of 'foo.html'.
+  base::FilePath saved_main_directory_path = full_path.DirName().Append(
+      full_path.RemoveExtension().BaseName().value() +
+      FILE_PATH_LITERAL('_files'));
+  bool result = web_contents_->SavePage(full_path,
+                                        saved_main_directory_path,
+                                        save_type);
+  download_manager->RemoveObserver(this);
+  // If initialization fails which means fail to create |DownloadItem|, we need
+  // to delete the |SavePageHandler| instance to avoid memory-leak.
+  if (!result)
+    delete this;
+  return result;
 }
     
-      Benchmark()
-  : db_(NULL),
-    db_num_(0),
-    num_(FLAGS_num),
-    reads_(FLAGS_reads < 0 ? FLAGS_num : FLAGS_reads),
-    bytes_(0),
-    rand_(301) {
-    std::vector<std::string> files;
-    std::string test_dir;
-    Env::Default()->GetTestDirectory(&test_dir);
-    Env::Default()->GetChildren(test_dir, &files);
-    if (!FLAGS_use_existing_db) {
-      for (int i = 0; i < files.size(); i++) {
-        if (Slice(files[i]).starts_with('dbbench_sqlite3')) {
-          std::string file_name(test_dir);
-          file_name += '/';
-          file_name += files[i];
-          Env::Default()->DeleteFile(file_name.c_str());
-        }
-      }
-    }
-  }
+    #include 'atom/browser/net/asar/asar_protocol_handler.h'
     
-     private:
-  const Comparator* comparator_;
-  std::string data_;
-  Block* block_;
+    // Parse a string as an accelerator.
+bool StringToAccelerator(const std::string& description,
+                         ui::Accelerator* accelerator);
     
-    const char* GetVarint64Ptr(const char* p, const char* limit, uint64_t* value) {
-  uint64_t result = 0;
-  for (uint32_t shift = 0; shift <= 63 && p < limit; shift += 7) {
-    uint64_t byte = *(reinterpret_cast<const unsigned char*>(p));
-    p++;
-    if (byte & 128) {
-      // More bytes are present
-      result |= ((byte & 127) << shift);
-    } else {
-      result |= (byte << shift);
-      *value = result;
-      return reinterpret_cast<const char*>(p);
-    }
-  }
-  return NULL;
-}
+            REGISTER_OPERATOR_SCHEMA(Not)
+        .Description('Performs element-wise negation.')
+        .Input('X', 'Input tensor of type bool.', 'T')
+        .Output('Y', '  Output tensor of type bool.', 'T')
+        .TypeConstraint('T', { 'tensor(bool)' }, 'Constrain input and output types to bool tensor.');
     
-    namespace leveldb {
-    }
+        // Stores all frames of the chunk consecutively (mutable since this is a cache).
+    mutable msra::dbn::matrix m_frames;
     
-    
-    {// Enable LOG(CONSOLE) for print messages to console.
-#define LOG_CONSOLE ::xgboost::ConsoleLogger()
-// Enable LOG(TRACKER) for print messages to tracker
-#define LOG_TRACKER ::xgboost::TrackerLogger()
-}  // namespace xgboost.
-#endif  // XGBOOST_LOGGING_H_
+    #endif  // STORAGE_LEVELDB_DB_SNAPSHOT_H_
 
     
-     protected:
-  /*!
-   * \brief to be implemented by subclass,
-   * get next token, return EOF if end of file
-   */
-  virtual char GetChar(void) = 0;
-  /*! \brief to be implemented by child, check if end of stream */
-  virtual bool IsEnd(void) = 0;
+      // Add string delta to buffer_ followed by value
+  buffer_.append(key.data() + shared, non_shared);
+  buffer_.append(value.data(), value.size());
     
-    
-    {  friend std::ostream &operator<<(std::ostream &os,
-                                  const bst_gpair_internal<T> &g) {
-    os << g.GetGrad() << '/' << g.GetHess();
-    return os;
+      // Read the block contents as well as the type/crc footer.
+  // See table_builder.cc for the code that built this structure.
+  size_t n = static_cast<size_t>(handle.size());
+  char* buf = new char[n + kBlockTrailerSize];
+  Slice contents;
+  Status s = file->Read(handle.offset(), n + kBlockTrailerSize, &contents, buf);
+  if (!s.ok()) {
+    delete[] buf;
+    return s;
   }
-};
+  if (contents.size() != n + kBlockTrailerSize) {
+    delete[] buf;
+    return Status::Corruption('truncated block read');
+  }
     
-    // Saves the COM marker segment as a string to *jpg.
-bool ProcessCOM(const uint8_t* data, const size_t len, size_t* pos,
-                JPEGData* jpg) {
-  VERIFY_LEN(2);
-  size_t marker_len = ReadUint16(data, pos);
-  VERIFY_INPUT(marker_len, 2, 65535, MARKER_LEN);
-  VERIFY_LEN(marker_len - 2);
-  std::string com_str(reinterpret_cast<const char*>(
-      &data[*pos - 2]), marker_len);
-  *pos += marker_len - 2;
-  jpg->com_data.push_back(com_str);
-  return true;
+    class Histogram {
+ public:
+  Histogram() { }
+  ~Histogram() { }
+    }
+    
+      // Randomly returns true ~'1/n' of the time, and false otherwise.
+  // REQUIRES: n > 0
+  bool OneIn(int n) { return (Next() % n) == 0; }
+    
+    #if 0
+static void DumpInternalIter(Iterator* iter) {
+  for (iter->SeekToFirst(); iter->Valid(); iter->Next()) {
+    ParsedInternalKey k;
+    if (!ParseInternalKey(iter->key(), &k)) {
+      fprintf(stderr, 'Corrupt '%s'\n', EscapeString(iter->key()).c_str());
+    } else {
+      fprintf(stderr, '@ '%s'\n', k.DebugString().c_str());
+    }
+  }
+}
+#endif
+    
+    void InternalKeyComparator::FindShortSuccessor(std::string* key) const {
+  Slice user_key = ExtractUserKey(*key);
+  std::string tmp(user_key.data(), user_key.size());
+  user_comparator_->FindShortSuccessor(&tmp);
+  if (tmp.size() < user_key.size() &&
+      user_comparator_->Compare(user_key, tmp) < 0) {
+    // User key has become shorter physically, but larger logically.
+    // Tack on the earliest possible number to the shortened user key.
+    PutFixed64(&tmp, PackSequenceAndType(kMaxSequenceNumber,kValueTypeForSeek));
+    assert(this->Compare(*key, tmp) < 0);
+    key->swap(tmp);
+  }
 }
     
-      // Sets the coordinates of the current macro-block for the purpose of
-  // CompareBlock() calls.
-  virtual void SwitchBlock(int block_x, int block_y,
-                           int factor_x, int factor_y) = 0;
+        RecordType type;
+    const bool end = (left == fragment_length);
+    if (begin && end) {
+      type = kFullType;
+    } else if (begin) {
+      type = kFirstType;
+    } else if (end) {
+      type = kLastType;
+    } else {
+      type = kMiddleType;
+    }
+    
+    void OutputImageComponent::GetCoeffBlock(int block_x, int block_y,
+                                         coeff_t block[kDCTBlockSize]) const {
+  assert(block_x < width_in_blocks_);
+  assert(block_y < height_in_blocks_);
+  int offset = (block_y * width_in_blocks_ + block_x) * kDCTBlockSize;
+  memcpy(block, &coeffs_[offset], kDCTBlockSize * sizeof(coeffs_[0]));
+}
+    
+    #include 'guetzli/dct_double.h'
+    
+    #ifndef GUETZLI_FAST_LOG_H_
+#define GUETZLI_FAST_LOG_H_
     
     
     {}  // namespace guetzli
+    
+    namespace guetzli {
+    }
     
       tmp0 = in[4 * stride];
   tmp1 = kIDCTMatrix[ 4] * tmp0;
@@ -130,57 +142,77 @@ bool ProcessCOM(const uint8_t* data, const size_t len, size_t* pos,
   out[6] -= tmp1;
   out[7] += tmp1;
     
-    
-    {}  // namespace guetzli
+    #include 'guetzli/output_image.h'
     
     // Creates a JPEG from the rgb pixel data. Returns true on success. The given
 // quantization table must have 3 * kDCTBlockSize values.
 bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
                      const int* quant, JPEGData* jpg);
     
-    #endif  // GUETZLI_JPEG_ERROR_H_
-
-    
-    AsyncIOOp::~AsyncIOOp() {
-  CHECK_NE(state_, State::PENDING);
-}
-    
-      std::shared_ptr<LogHandler> createHandler(const Options& options) override;
-    
-    namespace folly {
-    }
-    
-    
-    {private:
-  uint8_t Size = 0;
-  uint8_t Data[kMaxSize];
+    // Output callback function with associated data.
+struct JPEGOutput {
+  JPEGOutput(JPEGOutputHook cb, void* data) : cb(cb), data(data) {}
+  bool Write(const uint8_t* buf, size_t len) const {
+    return (len == 0) || (cb(data, buf, len) == len);
+  }
+ private:
+  JPEGOutputHook cb;
+  void* data;
 };
     
-    long GetEpoch(const std::string &Path) {
-  struct stat St;
-  if (stat(Path.c_str(), &St))
-    return 0;  // Can't stat, be conservative.
-  return St.st_mtime;
-}
-    
-    void ListFilesInDirRecursive(const std::string &Dir, long *Epoch,
-                             std::vector<std::string> *V, bool TopDir) {
-  auto E = GetEpoch(Dir);
-  if (Epoch)
-    if (E && *Epoch >= E) return;
+        Value(int unit, double value)
+    : unit(unit)
+    , value(value)
+    {
     }
     
-    // Crash on a single malloc that exceeds the rss limit.
-void Fuzzer::HandleMalloc(size_t Size) {
-  if (!Options.RssLimitMb || (Size >> 20) < (size_t)Options.RssLimitMb)
-    return;
-  Printf('==%d== ERROR: libFuzzer: out-of-memory (malloc(%zd))\n', GetPid(),
-         Size);
-  Printf('   To change the out-of-memory limit use -rss_limit_mb=<N>\n\n');
-  if (EF->__sanitizer_print_stack_trace)
-    EF->__sanitizer_print_stack_trace();
-  DumpCurrentUnit('oom-');
-  Printf('SUMMARY: libFuzzer: out-of-memory\n');
-  PrintFinalStats();
-  _Exit(Options.ErrorExitCode); // Stop right now.
-}
+      YGNodeCalculateLayout(root, 200, 100, YGDirectionLTR);
+    
+    
+    {    void toJS(nbind::cbOutput expose) const
+    {
+        expose(left, right, top, bottom, width, height);
+    }
+};
+
+    
+        void setAspectRatio(double aspectRatio);
+    
+    #include <nbind/api.h>
+#include <nbind/BindDefiner.h>
+    
+    #include <cstdarg>
+#include <stdio.h>
+    
+    class Countable : public noncopyable, public nonmovable {
+public:
+  // RefPtr expects refcount to start at 0
+  Countable() : m_refcount(0) {}
+  virtual ~Countable()
+  {
+    FBASSERT(m_refcount == 0);
+  }
+    }
+    
+    // Class that lets you declare a global but does not add a static constructor
+// to the binary. Eventually I'd like to have this auto-initialize in a
+// multithreaded environment but for now it's easiest just to use manual
+// initialization.
+template <typename T>
+class StaticInitialized {
+public:
+  constexpr StaticInitialized() :
+    m_instance(nullptr)
+  {}
+    }
+    
+      /**
+   * As above but with a custom cleanup function
+   */
+  typedef void (*CleanupFunction)(void* obj);
+  explicit ThreadLocal(CleanupFunction cleanup) :
+    m_key(0),
+    m_cleanup(cleanup) {
+    FBASSERT(cleanup);
+    initialize();
+  }
