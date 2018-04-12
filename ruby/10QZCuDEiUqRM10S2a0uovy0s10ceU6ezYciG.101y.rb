9a4930c87ab8c23724c16249d73599ea2afc3dd7
SUNsung <weatherius@gmail.com>
2018-04-12 11:32:56 +0300

    Rewrite [10QZCuDEiUqRM10S2a0uovy0s10ceU6ezYciG.101y.rb] File in: 2018-04-12 11:32:55

--- ruby/10QZCuDEiUqRM10S2a0uovy0s10ceU6ezYciG.101y.rb
+++ ruby/10QZCuDEiUqRM10S2a0uovy0s10ceU6ezYciG.101y.rb
@@ -1,18 +1,44 @@
 
         
-              case Rails.env
-      when 'development'
-        config.eager_load = false
-      when 'test'
-        config.eager_load = false
-      when 'production'
-        config.eager_load = true
+              def task_available?(task)
+        load_all_tasks
+        return tasks.collect(&:title).include?(task)
       end
-    end
+    
+            expect(result).to eq('/usr/local/bin/cloc  --by-file --xml  --out=/tmp/cloc.xml')
+      end
+    
+          it 'pass a custom build number to the tool' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          increment_build_number(build_number: 24, xcodeproj: '.xcproject')
+        end').runner.execute(:test)
+    
+        groups
   end
 end
+
     
-          if (pkt.payload =~ self.sigs[k])
-        matched = k
-        matches = [$1,$2]
-      end
+    
+  it 'should allow changing the protection mode to a string' do
+    # I have no clue what other modes are available
+    mock_app do
+      use Rack::Protection::FrameOptions, :frame_options => 'ALLOW-FROM foo'
+      run DummyApp
+    end
+    
+      %w(GET HEAD).each do |method|
+    it 'accepts #{method} requests with non-whitelisted Origin' do
+      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://malicious.com')).to be_ok
+    end
+  end
+    
+      it 'should allow changing the protection mode' do
+    # I have no clue what other modes are available
+    mock_app do
+      use Rack::Protection::XSSHeader, :xss_mode => :foo
+      run DummyApp
+    end
+    
+            def address_params
+          params.require(:address).permit(permitted_address_attributes)
+        end
