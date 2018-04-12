e73d4d691051840036f8efa2e6dd021e98f80aee
SUNsung <weatherius@gmail.com>
2018-04-12 19:33:03 +0300

    Rewrite [51XE1w6QQIVIs51wNOVn3Ah69k51VT4Z5QE4hUQ513I2.rb] File in: 2018-04-12 19:33:02

--- ruby/51XE1w6QQIVIs51wNOVn3Ah69k51VT4Z5QE4hUQ513I2.rb
+++ ruby/51XE1w6QQIVIs51wNOVn3Ah69k51VT4Z5QE4hUQ513I2.rb
@@ -1,199 +1,65 @@
 
         
-            class CallbacksWithArrayConditions < ControllerWithCallbacks
-      before_action :list, only: [:index, :listy]
-      before_action :authenticate, except: [:index, :listy]
-    
-      def test_helper_proxy_in_instance
-    methods = AllHelpersController.new.helpers.methods
-    
-    module ActionMailer
-  # This module handles everything related to mail delivery, from registering
-  # new delivery methods to configuring the mail object to be sent.
-  module DeliveryMethods
-    extend ActiveSupport::Concern
-    
-          include ActiveSupport::Testing::ConstantLookup
-      include TestHelper
-      include Rails::Dom::Testing::Assertions::SelectorAssertions
-      include Rails::Dom::Testing::Assertions::DomAssertions
-    
-            in_root do
-          if behavior == :invoke && !File.exist?(application_mailer_file_name)
-            template 'application_mailer.rb', application_mailer_file_name
-          end
-        end
-      end
-    
-      # Skips the current run on Rubinius using Minitest::Assertions#skip
-  private def rubinius_skip(message = '')
-    skip message if RUBY_ENGINE == 'rbx'
-  end
-  # Skips the current run on JRuby using Minitest::Assertions#skip
-  private def jruby_skip(message = '')
-    skip message if defined?(JRUBY_VERSION)
-  end
-end
-
-    
-      test 'delivery method can be customized in subclasses not changing the parent' do
-    DeliveryMailer.delivery_method = :test
-    assert_equal :smtp, ActionMailer::Base.delivery_method
-    email = DeliveryMailer.welcome.deliver_now
-    assert_instance_of Mail::TestMailer, email.delivery_method
-  end
-    
-    class U2fRegistration < ActiveRecord::Base
-  belongs_to :user
-    
-          clean_up_export_files
-    end
-  end
-    
-          if rails?
-        register_rails_engine
-      elsif lotus?
-        register_lotus
-      elsif sprockets?
-        register_sprockets
-      end
-    
-      # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both thread web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
-    
-    desc 'Dumps output to a CSS file for testing'
-task :debug do
-  require 'sass'
-  path = Bootstrap.stylesheets_path
-  %w(bootstrap).each do |file|
-    engine = Sass::Engine.for_file('#{path}/#{file}.scss', syntax: :scss, load_paths: [path])
-    File.open('./#{file}.css', 'w') { |f| f.write(engine.render) }
-  end
-end
-    
-    require 'cocoapods'
-    
-            # Prints the list of specs & pod cache dirs for a single pod name.
-        #
-        # This output is valid YAML so it can be parsed with 3rd party tools
-        #
-        # @param [Array<Hash>] cache_descriptors
-        #        The various infos about a pod cache. Keys are
-        #        :spec_file, :version, :release and :slug
-        #
-        def print_pod_cache_infos(pod_name, cache_descriptors)
-          UI.puts '#{pod_name}:'
-          cache_descriptors.each do |desc|
-            if @short_output
-              [:spec_file, :slug].each { |k| desc[k] = desc[k].relative_path_from(@cache.root) }
-            end
-            UI.puts('  - Version: #{desc[:version]}')
-            UI.puts('    Type:    #{pod_type(desc)}')
-            UI.puts('    Spec:    #{desc[:spec_file]}')
-            UI.puts('    Pod:     #{desc[:slug]}')
-          end
-        end
+            if options['recipients'].present?
+      emails = options['recipients']
+      emails = [emails] if emails.is_a?(String)
+      unless emails.all? { |email| email =~ Devise.email_regexp || email =~ /\{/ }
+        errors.add(:base, ''when provided, 'recipients' should be an email address or an array of email addresses')
       end
     end
   end
-end
-
+    }
     
-          def stack
-        UI::ErrorReport.stack
-      end
+      delegate :form_configurable_attributes, to: :class
+  delegate :form_configurable_fields, to: :class
     
-          # @param  [[Xcodeproj::PBXTarget]] targets
-      #         An array which always has a target as its first item
-      #         and may optionally contain a second target as its test target
-      #
-      # @return [String] the text for the target module
-      #
-      def template_contents(path, prefix, fallback)
-        if path.exist?
-          path.read.chomp.lines.map { |line| '#{prefix}#{line}' }.join('\n')
-        else
-          '#{prefix}# #{fallback}'
-        end
+        def terminate_thread!
+      if thread
+        thread.instance_eval { ActiveRecord::Base.connection_pool.release_connection }
+        thread.wakeup if thread.status == 'sleep'
+        thread.terminate
       end
+    end
+    
+      private
+    
+        respond_to do |format|
+      format.html
+      format.json {
+        send_data Utils.pretty_jsonify(@user_credentials.limit(nil).as_json), disposition: 'attachment'
+      }
     end
   end
-end
-
     
-    @@ login
-<form action='/'>
-  <label for='user'>User Name:</label>
-  <input name='user' value='' />
-  <input type='submit' value='GO!' />
-</form>
-    
-          def accepts?(env)
-        cookie_header = env['HTTP_COOKIE']
-        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
-        cookies.each do |k, v|
-          if k == session_key && Array(v).size > 1
-            bad_cookies << k
-          elsif k != session_key && Rack::Utils.unescape(k) == session_key
-            bad_cookies << k
-          end
-        end
-        bad_cookies.empty?
+          def key?(key)
+        super(convert_key(key))
       end
     
-    begin
-  require 'escape_utils'
-rescue LoadError
-end
+              # Repackage the box
+          output_name = @env.vagrantfile.config.package.name || 'package.box'
+          output_path = Pathname.new(File.expand_path(output_name, FileUtils.pwd))
+          box.repackage(output_path)
     
-        it 'Reads referrer from Host header when Referer header is relative' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
-      expect(subject.referrer(env)).to eq('foo.com')
+                when :user
+              # When the last command was a username login
+              # We might keep track on this one in future
+            when :pass
+              # Perfect we get an +OK after a PASS command this means right password given :-)
+    
+      // writing
+  $('form').on('submit',function(e) {
+    $.post('/', {msg: '<%= user %>: ' + $('#msg').val()});
+    $('#msg').val(''); $('#msg').focus();
+    e.preventDefault();
+  });
+</script>
+    
+          def empty_cookie(host, path)
+        {:value => '', :domain => host, :path => path, :expires => Time.at(0)}
+      end
+    
+      describe '#referrer' do
+    it 'Reads referrer from Referer header' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
+      expect(subject.referrer(env)).to eq('bar.com')
     end
-    
-        { # yes, this is ugly, feel free to change that
-      '/..' => '/', '/a/../b' => '/b', '/a/../b/' => '/b/', '/a/.' => '/a/',
-      '/%2e.' => '/', '/a/%2E%2e/b' => '/b', '/a%2f%2E%2e%2Fb/' => '/b/',
-      '//' => '/', '/%2fetc%2Fpasswd' => '/etc/passwd'
-    }.each do |a, b|
-      it('replaces #{a.inspect} with #{b.inspect}') { expect(get(a).body).to eq(b) }
-    end
-    
-      it 'should not override the header if already set' do
-    mock_app with_headers('X-XSS-Protection' => '0')
-    expect(get('/', {}, 'wants' => 'text/html').headers['X-XSS-Protection']).to eq('0')
-  end
-    
-        def render(context)
-      quote = paragraphize(super)
-      author = '<strong>#{@by.strip}</strong>' if @by
-      if @source
-        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
-        parts = []
-        url.each do |part|
-          if (parts + [part]).join('/').length < 32
-            parts << part
-          end
-        end
-        source = parts.join('/')
-        source << '/&hellip;' unless source == @source
-      end
-      if !@source.nil?
-        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
-      elsif !@title.nil?
-        cite = ' <cite>#{@title}</cite>'
-      end
-      blockquote = if @by.nil?
-        quote
-      elsif cite
-        '#{quote}<footer>#{author + cite}</footer>'
-      else
-        '#{quote}<footer>#{author}</footer>'
-      end
-      '<blockquote>#{blockquote}</blockquote>'
-    end
-    
-    Liquid::Template.register_tag('config_tag', ConfigTag)
