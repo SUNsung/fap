08fc82c8a412285ebac6c9559f7771ec0d5c4756
SUNsung <weatherius@gmail.com>
2018-04-13 01:13:56 +0300

    Rewrite [45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb] File in: 2018-04-13 01:13:56

--- ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
+++ ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
@@ -1,51 +1,77 @@
 
         
-                css('> .section', '#preamble', 'a[href*='dict.html']', 'code var', 'code strong').each do |node|
-          node.before(node.children).remove
-        end
+        module Jobs
     
-            # Helper method to get access to the class variable. This is mostly
-        # exposed for tests. This shouldn't be mucked with directly, since it's
-        # structure may change at any time.
-        def registered; @@registered; end
+      def save_and_refresh_staff_groups!
+    transaction do
+      self.save!
+      Group.refresh_automatic_groups!(:admins, :moderators, :staff)
+    end
+  end
+    
+      describe '.all' do
+    it 'uses the client to fetch all keys' do
+      mock_client_response(:list_keys, with: no_args) do
+        [
+          {
+            canDownload: false,
+            canRevoke: true,
+            keyId: 'some-key-id',
+            keyName: 'Test Key via fastlane',
+            servicesCount: 2
+          },
+          {
+            canDownload: true,
+            canRevoke: true,
+            keyId: 'B92NE4F7RG',
+            keyName: 'Test Key via browser',
+            servicesCount: 2
+          }
+        ]
+      end
+    
+            expect(result).to eq('/usr/local/bin/cloc  --by-file  --out=build/cloc.txt')
       end
     end
   end
 end
 
     
-      def test_size
-    assert_equal(0, Set[].size)
-    assert_equal(2, Set[1,2].size)
-    assert_equal(2, Set[1,2,1].size)
+            expect(result).to eq('hg parent --template {rev}')
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('hg parent --template {rev}')
+      end
+    end
   end
+end
+
     
-      def test_1970
-    assert_equal(0, Time.utc(1970, 1, 1, 0, 0, 0).tv_sec)
-    [
-      [1969, 12, 31, 23, 59, 59],
-      [1970, 1, 1, 0, 0, 0],
-      [1970, 1, 1, 0, 0, 1],
-    ].each {|year, mon, day, hour, min, sec|
-      t = Time.utc(year, mon, day, hour, min, sec)
-      assert_equal(year, t.year)
-      assert_equal(mon, t.mon)
-      assert_equal(day, t.day)
-      assert_equal(hour, t.hour)
-      assert_equal(min, t.min)
-      assert_equal(sec, t.sec)
+        # .visible-sm { @include responsive-visibility() }
+    # to:
+    # @include responsive-visibility('.visible-sm')
+    def apply_mixin_parent_selector(file, rule_sel)
+      log_transform rule_sel
+      replace_rules file, '\s*' + rule_sel, comments: false do |rule, rule_pos, css|
+        body = unwrap_rule_block(rule.dup).strip
+        next rule unless body =~ /^@include \w+/m || body =~ /^@media/ && body =~ /\{\s*@include/
+        rule =~ /(#{COMMENT_RE}*)([#{SELECTOR_CHAR}\s*]+?)#{RULE_OPEN_BRACE_RE}/
+        cmt, sel = $1, $2.strip
+        # take one up selector chain if this is an &. selector
+        if sel.start_with?('&')
+          parent_sel = selector_for_pos(css, rule_pos.begin)
+          sel        = parent_sel + sel[1..-1]
+        end
+        # unwrap, and replace @include
+        unindent unwrap_rule_block(rule).gsub(/(@include [\w-]+)\(?([\$\w\-,\s]*)\)?/) {
+          name, args = $1, $2
+          sel.gsub(/\s+/, ' ').split(/,\s*/ ).map { |sel_part|
+            '#{cmt}#{name}('#{sel_part}'#{', ' if args && !args.empty?}#{args})'
+          }.join(';\n')
+        }
+      end
+    end
     }
-  end
     
-      it 'decodes the remaining floats when passed the '*' modifier after another directive' do
-    array = '@\xa9\x99\x9aA\x1333'.unpack(unpack_format()+unpack_format('*'))
-    array.should == [5.300000190734863, 9.199999809265137]
-  end
+      # Use a different logger for distributed setups.
+  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
     
-        run.should be_true
-  end
-    
-        10.times { sleep 0.1 if after_sleep1 != true }
-    10.times { sleep 0.1 if t.status and t.status != 'sleep' }
-    after_sleep2.should == false # t should be blocked on the second sleep
-    t.send(@method)
+          spec['version'] = Bootstrap::VERSION
