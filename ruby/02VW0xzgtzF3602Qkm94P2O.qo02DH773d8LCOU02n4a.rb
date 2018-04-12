f1ee2e2b6d4ac9762a966d04a0a7448a2906345e
SUNsung <weatherius@gmail.com>
2018-04-12 04:19:25 +0300

    Rewrite [02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb] File in: 2018-04-12 04:19:24

--- ruby/02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb
+++ ruby/02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb
@@ -1,68 +1,40 @@
 
         
-            def translation_scope
-      'devise.unlocks'
+        IMAGE_WIDTH = 256
+IMAGE_HEIGHT = 256
+NSUBSAMPLES = 2
+NAO_SAMPLES = 8
+    
+    msfenv_real_pathname = Pathname.new(__FILE__).realpath
+root = msfenv_real_pathname.parent.parent
+    
+    meterp = Rex::Post::Meterpreter::Client.new(sock)
+    
+      context 'called with four styles' do
+    it 'applies different styles to all sides' do
+      rule = 'border-style: dotted groove ridge none'
+    
+      context 'called with arguments (2, $value: 4em 6em)' do
+    it 'outputs sextuple the second value from the default scale' do
+      expect('.two-double-value').to have_rule('font-size: 3.125em')
     end
+  end
 end
 
     
-        if last_request_at.is_a? Integer
-      last_request_at = Time.at(last_request_at).utc
-    elsif last_request_at.is_a? String
-      last_request_at = Time.parse(last_request_at)
-    end
+      context 'called with four sizes' do
+    it 'applies different widths to all sides' do
+      ruleset = 'position: fixed; ' +
+                'top: 7px; ' +
+                'right: 8px; ' +
+                'bottom: 9px; ' +
+                'left: 10px;'
     
-      # Read and eval a .rake file in such a way that `self` within the .rake file
-  # refers to this plugin instance. This gives the tasks in the file access to
-  # helper methods defined by the plugin.
-  def eval_rakefile(path)
-    contents = IO.read(path)
-    instance_eval(contents, path, 1)
+          expect('.size-implicit').to have_ruleset(rule)
+    end
   end
     
-      deploy_rb = File.expand_path('../../templates/deploy.rb.erb', __FILE__)
-  stage_rb = File.expand_path('../../templates/stage.rb.erb', __FILE__)
-  capfile = File.expand_path('../../templates/Capfile', __FILE__)
-    
-      Rake::Task[:copydot].invoke(source_dir, public_dir)
-  Rake::Task['#{deploy_default}'].execute
+          expect('.all-text-inputs-invalid').to have_ruleset(ruleset)
+    end
+  end
 end
-    
-        def cache(gist, file, data)
-      cache_file = get_cache_file_for gist, file
-      File.open(cache_file, 'w') do |io|
-        io.write data
-      end
-    end
-    
-          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
-        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
-        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
-          @img['title']  = title
-          @img['alt']    = alt
-        else
-          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
-        end
-        @img['class'].gsub!(/'/, '') if @img['class']
-      end
-      super
-    end
-    
-          super
-    end
-    
-      # Summary is used on the Archive pages to return the first block of content from a post.
-  def summary(input)
-    if input.index(/\n\n/)
-      input.split(/\n\n/)[0]
-    else
-      input
-    end
-  end
-    
-    require 'pathname'
-require './plugins/octopress_filters'
-    
-        def poster
-      'poster='#{@poster}'' if @poster
-    end
