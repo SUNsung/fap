77b6f351f46395cd96b13dea11c66ec571a81fe0
SUNsung <weatherius@gmail.com>
2018-04-12 08:02:51 +0300

    Rewrite [226VqKJ1U1MY224ENy6hm3N6Y22Lz9EdFJ66eI22aNH.cpp] File in: 2018-04-12 08:02:50

--- cccccc/226VqKJ1U1MY224ENy6hm3N6Y22Lz9EdFJ66eI22aNH.cpp
+++ cccccc/226VqKJ1U1MY224ENy6hm3N6Y22Lz9EdFJ66eI22aNH.cpp
@@ -1,168 +1,267 @@
 
         
-        
-    {  std::unique_ptr<PartialRunState> partial_run = MakeUnique<PartialRunState>();
-  partial_run->cancellation_manager = MakeUnique<CancellationManager>();
-  *cancellation_manager = partial_run->cancellation_manager.get();
-  step_id_to_partial_run_[step_id] = std::move(partial_run);
-  return true;
+          content::WebContents* web_contents_;  // Weak Reference.
+  scoped_refptr<content::DevToolsAgentHost> agent_host_;
+    
+    #ifndef ATOM_BROWSER_API_ATOM_API_WEB_REQUEST_H_
+#define ATOM_BROWSER_API_ATOM_API_WEB_REQUEST_H_
+    
+    void AtomQuotaPermissionContext::RequestQuotaPermission(
+    const content::StorageQuotaParams& params,
+    int render_process_id,
+    const PermissionCallback& callback) {
+  callback.Run(response::QUOTA_PERMISSION_RESPONSE_ALLOW);
 }
     
-    TEST(PartialRunMgr, PartialRunRemoved) {
-  // Test that PartialRunMgr ensures that the PartialRun is deleted after
-  // ExecutorDone and PartialRunDone are called.
-  PartialRunMgr partial_run_mgr;
-  int step_id = 1;
-  CancellationManager* cancellation_manager;
-  partial_run_mgr.FindOrCreate(step_id, &cancellation_manager);
+    
+    {  DISALLOW_COPY_AND_ASSIGN(LoginHandler);
+};
+    
+    #ifndef ATOM_BROWSER_NET_URL_REQUEST_ASYNC_ASAR_JOB_H_
+#define ATOM_BROWSER_NET_URL_REQUEST_ASYNC_ASAR_JOB_H_
+    
+      // Sets the accelerator for |item|.
+  void RegisterAccelerator(DbusmenuMenuitem* item,
+                           const ui::Accelerator& accelerator);
+    
+    namespace atom {
+    }
+    
+      void EncodeTo(std::string* dst) const;
+  Status DecodeFrom(const Slice& src);
+    
+    class Env;
+    
+    #endif  // STORAGE_LEVELDB_INCLUDE_FILTER_POLICY_H_
+
+    
+    bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_constructor(JSContext *cx, uint32_t argc, jsval *vp);
+void js_cocos2dx_physics3d_Physics3DConeTwistConstraint_finalize(JSContext *cx, JSObject *obj);
+void js_register_cocos2dx_physics3d_Physics3DConeTwistConstraint(JSContext *cx, JS::HandleObject global);
+void register_all_cocos2dx_physics3d(JSContext* cx, JS::HandleObject obj);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getBFrame(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_setFixThresh(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getFrameOffsetB(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getFrameOffsetA(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getFixThresh(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getSwingSpan2(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getSwingSpan1(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_setMaxMotorImpulse(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_setFrames(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getTwistAngle(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_GetPointForAngle(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_setMaxMotorImpulseNormalized(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getTwistSpan(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_setDamping(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_setLimit(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_getAFrame(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_enableMotor(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_create(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_physics3d_Physics3DConeTwistConstraint_Physics3DConeTwistConstraint(JSContext *cx, uint32_t argc, jsval *vp);
+    
+    
+#endif // __cocos2dx_navmesh_h__
+#endif //#if CC_USE_NAVMESH
+
+    
+    
+    
+    #if COCOS2D_DEBUG >= 1
+    if (!cobj) 
+    {
+        tolua_error(tolua_S,'invalid 'cobj' in function 'lua_cocos2dx_physics_PhysicsJointMotor_createConstraints'', nullptr);
+        return 0;
+    }
+#endif
+    
+    
+    
+        glVertexAttribPointer(GLProgram::VERTEX_ATTRIB_POSITION, 2, GL_FLOAT, GL_FALSE, 0, glVertices);
+    glDrawArrays(GL_LINE_LOOP, 0, 4);
+    
+    #endif
+
+    
+    
+    {	b2Body* m_attachment;
+	b2Body* m_platform;
+	float32 m_speed;
+};
+    
+    			b2Body* prevBody = ground;
+			for (int32 i = 0; i < e_count; ++i)
+			{
+				b2BodyDef bd;
+				bd.type = b2_dynamicBody;
+				bd.position.Set(-14.5f + 1.0f * i, 5.0f);
+				b2Body* body = m_world->CreateBody(&bd);
+				body->CreateFixture(&fd);
     }
     
     
-    {  virtual ~SessionFactory() {}
-  static void Register(const string& runtime_type, SessionFactory* factory);
-  static Status GetFactory(const SessionOptions& options,
-                           SessionFactory** out_factory);
+    {	b2Body* m_middle;
 };
     
-    class Cluster;
-struct GrapplerItem;
+    // Implemented features:
+//  [X] User texture binding. Use 'CIwTexture*' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
     
-    #include 'third_party/eigen3/unsupported/Eigen/CXX11/Tensor'
-#include 'tensorflow/core/framework/tensor_types.h'
-#include 'tensorflow/core/framework/types.h'
-#include 'tensorflow/core/kernels/scatter_functor.h'
+    IMGUI_API bool        ImGui_ImplGlfwGL3_Init(GLFWwindow* window, bool install_callbacks, const char* glsl_version = NULL);
+IMGUI_API void        ImGui_ImplGlfwGL3_Shutdown();
+IMGUI_API void        ImGui_ImplGlfwGL3_NewFrame();
+IMGUI_API void        ImGui_ImplGlfwGL3_RenderDrawData(ImDrawData* draw_data);
     
-    // Converts a string of a form like '331.79' to a DriverVersion{331, 79}.
-port::StatusOr<DriverVersion> StringToDriverVersion(const string &value);
+        // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them. 
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple. 
+    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Read 'misc/fonts/README.txt' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Roboto-Medium.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Cousine-Regular.ttf', 15.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/DroidSans.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/ProggyTiny.ttf', 10.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF('c:\\Windows\\Fonts\\ArialUni.ttf', 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+    //IM_ASSERT(font != NULL);
     
+    // Implemented features:
+//  [X] User texture binding. Cast 'GLuint' OpenGL texture identifier as void*/ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
+// Missing features:
+//  [ ] SDL2 handling of IME under Windows appears to be broken and it explicitly disable the regular Windows IME. You can restore Windows IME by compiling SDL with SDL_DISABLE_WINDOWS_IME.
     
-    {  void CopyDeviceTensorToCPU(const Tensor *device_tensor, StringPiece edge_name,
-                             Device *device, Tensor *cpu_tensor,
-                             StatusCallback done) override;
-};
-    
-    #include 'tensorflow/core/lib/strings/strcat.h'
-    
-    void HloReachabilityMap::SetReachable(const HloInstruction* a,
-                                      const HloInstruction* b) {
-  GetBitVector(b).Set(GetIndex(a));
-}
-    
-    // Create a Shell and returns its routing id.
-IPC_SYNC_MESSAGE_ROUTED2_1(ShellViewHostMsg_CreateShell,
-                           std::string /* url */,
-                           base::DictionaryValue /* manifest */,
-                           int /* result */)
-    
-    #include 'content/nw/src/api/api_messages.h'
-#include 'content/nw/src/api/dispatcher_host.h'
-#include 'content/nw/src/api/shortcut/global_shortcut_listener.h'
-#include 'content/nw/src/api/shortcut/shortcut.h'
-#include 'content/nw/src/breakpad_linux.h'
-#include 'content/nw/src/browser/native_window.h'
-#include 'content/nw/src/browser/net_disk_cache_remover.h'
-#include 'content/nw/src/nw_package.h'
-#include 'content/nw/src/nw_shell.h'
-#include 'content/nw/src/shell_browser_context.h'
-#include 'content/common/view_messages.h'
-#include 'content/public/browser/browser_thread.h'
-#include 'content/public/browser/web_contents.h'
-#include 'content/public/browser/render_process_host.h'
-#include 'net/proxy/proxy_config.h'
-#include 'net/proxy/proxy_config_service_fixed.h'
-#include 'net/proxy/proxy_service.h'
-#include 'net/url_request/url_request_context.h'
-#include 'net/url_request/url_request_context_getter.h'
-    
-    
-    {}  // namespace nwapi
-    
-      void InitializeOnIOThread(
-      const scoped_refptr<net::CertVerifyProc>& verify_proc);
-    
-    Clipboard::Clipboard(int id,
-           const base::WeakPtr<DispatcherHost>& dispatcher_host,
-           const base::DictionaryValue& option)
-    : Base(id, dispatcher_host, option) {
-}
-    
-    namespace content {
-class RenderView;
-}
-    
-    namespace base {
-class DictionaryValue;
-class ListValue;
-class RunLoop;
-}
-    
-    #include <map>
-    
-    
-    {}
-    
-    void cvValidateDisparity( CvArr* _disp, const CvArr* _cost, int minDisparity,
-                         int numberOfDisparities, int disp12MaxDiff )
+    // Helper: Manually clip large list of items.
+// If you are submitting lots of evenly spaced items and you have a random access to the list, you can perform coarse clipping based on visibility to save yourself from processing those items at all.
+// The clipper calculates the range of visible items and advance the cursor to compensate for the non-visible items we have skipped. 
+// ImGui already clip items based on their bounds but it needs to measure text size to do so. Coarse clipping before submission makes this cost and your own data fetching/submission cost null.
+// Usage:
+//     ImGuiListClipper clipper(1000);  // we have 1000 elements, evenly spaced.
+//     while (clipper.Step())
+//         for (int i = clipper.DisplayStart; i < clipper.DisplayEnd; i++)
+//             ImGui::Text('line number %d', i);
+// - Step 0: the clipper let you process the first element, regardless of it being visible or not, so we can measure the element height (step skipped if we passed a known height as second arg to constructor).
+// - Step 1: the clipper infer height from first element, calculate the actual range of elements to display, and position the cursor before the first element.
+// - (Step 2: dummy step only required if an explicit items_height was passed to constructor or Begin() and user call Step(). Does nothing and switch to Step 3.)
+// - Step 3: the clipper validate that we have reached the expected Y position (corresponding to element DisplayEnd), advance the cursor to the end of the list and then returns 'false' to end the loop.
+struct ImGuiListClipper
 {
-    cv::Mat disp = cv::cvarrToMat(_disp), cost = cv::cvarrToMat(_cost);
-    cv::validateDisparity( disp, cost, minDisparity, numberOfDisparities, disp12MaxDiff );
+    float   StartPosY;
+    float   ItemsHeight;
+    int     ItemsCount, StepNo, DisplayStart, DisplayEnd;
+    }
+    
+    // DirectX data
+static LPDIRECT3DDEVICE9        g_pd3dDevice = NULL;
+static LPDIRECT3DVERTEXBUFFER9  g_pVB = NULL;
+static LPDIRECT3DINDEXBUFFER9   g_pIB = NULL;
+static LPDIRECT3DTEXTURE9       g_FontTexture = NULL;
+static int                      g_VertexBufferSize = 5000, g_IndexBufferSize = 10000;
+    
+    static uint32_t ImGui_ImplGlfwVulkan_MemoryType(VkMemoryPropertyFlags properties, uint32_t type_bits)
+{
+    VkPhysicalDeviceMemoryProperties prop;
+    vkGetPhysicalDeviceMemoryProperties(g_Gpu, &prop);
+    for (uint32_t i = 0; i < prop.memoryTypeCount; i++)
+        if ((prop.memoryTypes[i].propertyFlags & properties) == properties && type_bits & (1<<i))
+            return i;
+    return 0xffffffff; // Unable to find memoryType
+}
+    
+    
+    {    return 0;
 }
 
     
     
-    {}
+/*
+ * DumpCrashStack.h
+ *
+ *  Created on: 2012-9-28
+ *      Author: yerungui
+ */
     
-    Vec3d median3d(InputArray m);
+        if (st.url_length + st.head_length > st.total_length) return __LINE__;
+    
+            __FirstGetCreater(T::ServiceName());
     
     
-    {
-    {
-    {} } } // namespace cv::ocl::runtime
     
-    inline int ReadUint8(const uint8_t* data, size_t* pos) {
-  return data[(*pos)++];
-}
     
-      // Choose sharpened, blurred or original per pixel
-  std::vector<float> sharpened = Sharpen(yuv[channel], w, h, sigma, amount);
-  std::vector<float> blurred = Blur(yuv[channel], w, h);
-  for (int y = 0; y < h; y++) {
-    for (int x = 0; x < w; x++) {
-      size_t index = y * w + x;
-    }
-    }
+    {  private:
+    JavaVM* vm_;
+    JNIEnv* env_;
+    bool we_attach_;
+    int status_;
+};
     
-    void DCT1d(const double* in, int stride, double* out) {
-  for (int x = 0; x < 8; ++x) {
-    out[x * stride] = 0.0;
-    for (int u = 0; u < 8; ++u) {
-      out[x * stride] += kDCTMatrix[8 * x + u] * in[u * stride];
-    }
+    std::string CloneArgsWithoutX(const std::vector<std::string> &Args,
+                              const char *X1, const char *X2) {
+  std::string Cmd;
+  for (auto &S : Args) {
+    if (FlagValue(S.c_str(), X1) || FlagValue(S.c_str(), X2))
+      continue;
+    Cmd += S + ' ';
   }
+  return Cmd;
 }
     
+    private:
+  void AlarmCallback();
+  void CrashCallback();
+  void InterruptCallback();
+  void MutateAndTestOne();
+  void ReportNewCoverage(InputInfo *II, const Unit &U);
+  size_t RunOne(const Unit &U) { return RunOne(U.data(), U.size()); }
+  void WriteToOutputCorpus(const Unit &U);
+  void WriteUnitToFileWithPrefix(const Unit &U, const char *Prefix);
+  void PrintStats(const char *Where, const char *End = '\n', size_t Units = 0);
+  void PrintStatusForNewUnit(const Unit &U);
+  void ShuffleCorpus(UnitVector *V);
+  void AddToCorpus(const Unit &U);
+  void CheckExitOnSrcPosOrItem();
     
-    {}  // namespace guetzli
+    // The control file example:
+//
+// 3 # The number of inputs
+// 1 # The number of inputs in the first corpus, <= the previous number
+// file0
+// file1
+// file2  # One file name per line.
+// STARTED 0 123  # FileID, file size
+// DONE 0 1 4 6 8  # FileID COV1 COV2 ...
+// STARTED 1 456  # If DONE is missing, the input crashed while processing.
+// STARTED 2 567
+// DONE 2 8 9
+bool Merger::Parse(std::istream &IS, bool ParseCoverage) {
+  LastFailure.clear();
+  std::string Line;
+    }
     
-    #ifndef GUETZLI_FAST_LOG_H_
-#define GUETZLI_FAST_LOG_H_
+    /* public API - prototypes - TODO: doxygen*/
     
-    // Computes the DCT (Discrete Cosine Transform) of the 8x8 array in 'block',
-// scaled up by a factor of 16. The values in 'block' are laid out row-by-row
-// and the result is written to the same memory area.
-void ComputeBlockDCT(coeff_t* block);
-    
-    #include 'guetzli/output_image.h'
-    
-    static const int kJpegHuffmanRootTableBits = 8;
-// Maximum huffman lookup table size.
-// According to zlib/examples/enough.c, 758 entries are always enough for
-// an alphabet of 257 symbols (256 + 1 special symbol for the all 1s code) and
-// max bit length 16 if the root table has 8 bits.
-static const int kJpegHuffmanLutSize = 758;
-    
-    // Gamma-compensated chroma subsampling.
-// Returns Y, U, V image planes, each with width x height dimensions, but the
-// U and V planes are composed of 2x2 blocks with the same values.
-std::vector<std::vector<float> > RGBToYUV420(
-    const std::vector<uint8_t>& rgb_in, const int width, const int height);
+      pid_t Pid;
+  char **Environ = environ; // Read from global
+  const char *CommandCStr = Command.c_str();
+  const char *Argv[] = {'sh', '-c', CommandCStr, NULL};
+  int ErrorCode = 0, ProcessStatus = 0;
+  // FIXME: We probably shouldn't hardcode the shell path.
+  ErrorCode = posix_spawn(&Pid, '/bin/sh', NULL, &SpawnAttributes,
+                          (char *const *)Argv, Environ);
+  (void)posix_spawnattr_destroy(&SpawnAttributes);
+  if (!ErrorCode) {
+    pid_t SavedPid = Pid;
+    do {
+      // Repeat until call completes uninterrupted.
+      Pid = waitpid(SavedPid, &ProcessStatus, /*options=*/0);
+    } while (Pid == -1 && errno == EINTR);
+    if (Pid == -1) {
+      // Fail for some other reason.
+      ProcessStatus = -1;
+    }
+  } else if (ErrorCode == ENOMEM || ErrorCode == EAGAIN) {
+    // Fork failure.
+    ProcessStatus = -1;
+  } else {
+    // Shell execution failure.
+    ProcessStatus = W_EXITCODE(127, 0);
+  }
