9c427bbfb146412298f997b5bc80d5806e89de61
SUNsung <weatherius@gmail.com>
2018-04-14 01:46:46 +0300

    Rewrite [17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py] File in: 2018-04-14 01:46:45

--- pyton/17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py
+++ pyton/17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py
@@ -1,20 +1,109 @@
 
         
-        
-class PollErrorTest(unittest.TestCase):
-    '''Tests for acme.errors.PollError.'''
+        import collections
+import os
+import sys
+    
+      @tf.test.mock.patch.dict(premade_estimator.__dict__,
+                           {'load_data': four_lines_data})
+  def test_premade_estimator(self):
+    premade_estimator.main([None, '--train_steps=1'])
+    
+      tf.logging.info('Eval metric values:')
+  summary = tf.summary.Summary()
+  for name, val in zip(metric_names, values):
+    summary.value.add(tag=name, simple_value=val)
+    tf.logging.info('%s = %.3f', name, val)
+    
+    Computational time:
+  2 days to train 100000 steps on 1 layer 1024 hidden units LSTM,
+  256 embeddings, 400 truncated BP, 256 minibatch and on single GPU (Pascal
+  Titan X, cuDNNv5).
+'''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+    
+    Change the --image_file argument to any jpg image to compute a
+classification of that image.
+    
+        def set(self, key, value):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                item.value = value
+                return
+        self.table[hash_index].append(Item(key, value))
+    
+        def add_user(self, user_id, name, pass_hash):
+        pass
+    
+        def test_create_command_buttons(self):
+        self.dialog.top = self.root
+        self.dialog.create_command_buttons()
+        # Look for close button command in buttonframe
+        closebuttoncommand = ''
+        for child in self.dialog.buttonframe.winfo_children():
+            if child['text'] == 'close':
+                closebuttoncommand = child['command']
+        self.assertIn('close', closebuttoncommand)
+    
+        def __init__(self, parent, title, message, text0,
+                 *, _htest=False, _utest=False):
+        super().__init__(parent, title, message, text0=text0,
+                       _htest=_htest, _utest=_utest)
+    
+        def getname(self):
+        '''Return the name (ID) of the current chunk.'''
+        return self.chunkname
+    
+    def evalString(s):
+    assert s.startswith(''') or s.startswith('''), repr(s[:1])
+    q = s[0]
+    if s[:3] == q*3:
+        q = q*3
+    assert s.endswith(q), repr(s[-len(q):])
+    assert len(s) >= 2*len(q)
+    s = s[len(q):-len(q)]
+    return re.sub(r'\\(\'|\'|\\|[abfnrtv]|x.{0,2}|[0-7]{1,3})', escape, s)
     
     
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/', None),
-    'acme': ('https://acme-python.readthedocs.org/en/latest/', None),
-    'certbot': ('https://certbot.eff.org/docs/', None),
-    'certbot-apache': (
-        'https://letsencrypt-apache.readthedocs.org/en/latest/', None),
-    'certbot-nginx': (
-        'https://letsencrypt-nginx.readthedocs.org/en/latest/', None),
-}
-
+@cpython_only
+class FastCallTests(unittest.TestCase):
+    # Test calls with positional arguments
+    CALLS_POSARGS = (
+        # (func, args: tuple, result)
     
-        def tearDown(self):
-        logging.disable(logging.NOTSET)
+            self.assertEqual(x[:], a.tolist())
+    
+        def test_3(self):
+        class X(Structure):
+            pass
+        class Y(Structure):
+            _fields_ = [('x', X)] # finalizes X
+        self.assertRaises(AttributeError, setattr, X, '_fields_', [])
+    
+        failures = []
+    while point is not None:
+        if point.name:
+            if re.search('h[1-2]', point.name):
+                if point.name == 'h1':
+                    h1_directory = os.path.join(output_directory, clean_text(point.text))
+                    current_directory = h1_directory
+                elif point.name == 'h2':
+                    current_directory = os.path.join(h1_directory, clean_text(point.text))  
+                if not os.path.exists(current_directory):
+                    os.makedirs(current_directory)
+                print_title(point.text)
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    
+  def Start( self ):
+    with HandleServerException( display = False ):
+      self.PostDataToHandler( {}, 'shutdown', TIMEOUT_SECONDS )
