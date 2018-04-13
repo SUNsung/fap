2ebc7af794eea5370668f7e523d4739350cbce20
SUNsung <weatherius@gmail.com>
2018-04-13 15:19:53 +0300

    Rewrite [56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp] File in: 2018-04-13 15:19:52

--- cccccc/56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp
+++ cccccc/56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp
@@ -1,45 +1,19 @@
 
         
-          // Publish metadata about the debugged Session::Run() call.
-  //
-  // See the doc string of DebuggerStateInterface::PublishDebugMetadata() for
-  // details.
-  Status PublishDebugMetadata(const int64 global_step,
-                              const int64 session_run_count,
-                              const int64 executor_step_count,
-                              const std::vector<string>& input_names,
-                              const std::vector<string>& output_names,
-                              const std::vector<string>& target_names) override;
-    
-    TEST_P(StatusPropagationTest, ExecutorDoneFirst) {
-  // Tests error propagation when ExecutorDone is called first.
-  StatusTestParam param = GetParam();
-  int step_id = 1;
+        namespace tensorflow {
     }
     
-    #include 'tensorflow/core/framework/op_kernel.h'
-#include 'tensorflow/core/framework/resource_mgr.h'
-#include 'tensorflow/core/framework/tensor_shape.h'
-#include 'tensorflow/core/platform/logging.h'
-#include 'tensorflow/core/platform/mutex.h'
-#include 'tensorflow/core/platform/thread_annotations.h'
-#include 'tensorflow/core/platform/types.h'
-    
-    // A pass which performs constant folding in order to avoid unnecessary
-// computation on constants.
-class HloConstantFolding : public HloPassInterface {
- public:
-  tensorflow::StringPiece name() const override { return 'constant_folding'; }
-    }
-    
-    // Prune a model to make it more efficient:
-// * Remove unnecessary operations.
-// * Optimize gradient computations.
-class ModelPruner : public GraphOptimizer {
- public:
-  ModelPruner() {}
-  ~ModelPruner() override {}
-    }
+          T* resource;
+      OP_REQUIRES_OK(
+          context,
+          mgr->LookupOrCreate<T>(cinfo_.container(), cinfo_.name(), &resource,
+                                 [this](T** ret) EXCLUSIVE_LOCKS_REQUIRED(mu_) {
+                                   Status s = CreateResource(ret);
+                                   if (!s.ok() && *ret != nullptr) {
+                                     CHECK((*ret)->Unref());
+                                   }
+                                   return s;
+                                 }));
     
     Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an 'AS IS' BASIS,
@@ -48,193 +22,205 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
     
-    #endif
-
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    #pragma mark - NSData verification
+      // The underlying CUDA event element.
+  const CUevent& cuda_event();
     
-    /// Index the given module and store the results to \p indexStorePath.
-///
-/// \param module The module to index.
-///
-/// \param indexUnitTokens A list of unique identifiers for the index units to
-/// be written. This may either be one unit per source file of \p module, or it
-/// may be a single unit, in which case all the index information will be
-/// combined into a single unit.
-///
-/// \param moduleUnitToken A unique identifier for this module unit in the form
-/// of a file path. Only used if \p indexUnitTokens are specified for each
-/// source file, otherwise the single \p indexUnitTokens value is used instead.
-///
-/// \param indexStorePath The location to write the indexing data to.
-///
-/// \param indexSystemModules If true, emit index data for imported serialized
-/// swift system modules.
-///
-/// \param isDebugCompilation true for non-optimized compiler invocation.
-///
-/// \param targetTriple The target for this compilation.
-///
-/// \param dependencyTracker The set of dependencies seen while building.
-bool indexAndRecord(ModuleDecl *module, ArrayRef<std::string> indexUnitTokens,
-                    StringRef moduleUnitToken, StringRef indexStorePath,
-                    bool indexSystemModules, bool isDebugCompilation,
-                    StringRef targetTriple,
-                    const DependencyTracker &dependencyTracker);
-// FIXME: indexUnitTokens could be StringRef, but that creates an impedance
-// mismatch in the caller.
-    
-      /// Returns true if range \c R contains the location \c Loc.  The location
-  /// \c Loc should point at the beginning of the token.
-  bool rangeContainsTokenLoc(SourceRange R, SourceLoc Loc) const {
-    return Loc == R.Start || Loc == R.End ||
-           (isBeforeInBuffer(R.Start, Loc) && isBeforeInBuffer(Loc, R.End));
-  }
-    
-    
-    {}  // namespace caffe
-    
-    #include <vector>
-    
-    namespace caffe {
-    }
-    
-    #ifdef USE_CUDNN
-template <typename Dtype>
-class CuDNNLRNLayer : public LRNLayer<Dtype> {
+    // A PluginConfig describes the set of plugins to be used by a StreamExecutor
+// instance. Each plugin is defined by an arbitrary identifier, usually best set
+// to the address static member in the implementation (to avoid conflicts).
+//
+// A PluginConfig may be passed to the StreamExecutor constructor - the plugins
+// described therein will be used to provide BLAS, DNN, FFT, and RNG
+// functionality. Platform-appropriate defaults will be used for any un-set
+// libraries. If a platform does not support a specified plugin (ex. cuBLAS on
+// an OpenCL executor), then an error will be logged and no plugin operations
+// will succeed.
+//
+// The StreamExecutor BUILD target does not link ANY plugin libraries - even
+// common host fallbacks! Any plugins must be explicitly linked by dependent
+// targets. See the cuda, opencl and host BUILD files for implemented plugin
+// support (search for 'plugin').
+class PluginConfig {
  public:
-  explicit CuDNNLRNLayer(const LayerParameter& param)
-      : LRNLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNLRNLayer();
+  // Value specifying the platform's default option for that plugin.
+  static const PluginId kDefault;
     }
     
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+      // Calls Close() and logs if an error occurs.
+  //
+  // TODO(jhseu): Require that callers explicitly call Close() and remove the
+  // implicit Close() call in the destructor.
+  ~RecordWriter();
     
-      virtual inline const char* type() const { return 'Deconvolution'; }
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    #include 'caffe/layers/neuron_layer.hpp'
-    
-    /** Returns the benchmark Reporter instance.
- *
- * The returned instance will take care of generating reports for all the actual
- * reporters configured via the 'enable_*_reporter' command line flags (see
- * benchmark_config.cc). */
-std::shared_ptr<Reporter> GetReporter();
-    
-    #include 'test/cpp/qps/benchmark_config.h'
-#include 'test/cpp/qps/driver.h'
-#include 'test/cpp/qps/report.h'
-#include 'test/cpp/qps/server.h'
-#include 'test/cpp/util/test_config.h'
-#include 'test/cpp/util/test_credentials_provider.h'
-    
-    static void RunQPS() {
-  gpr_log(GPR_INFO, 'Running QPS test, open-loop');
+    bool HloReachabilityMap::SetReachabilityToUnion(
+    tensorflow::gtl::ArraySlice<const HloInstruction*> inputs,
+    const HloInstruction* instruction) {
+  BitVector& bit_vector = GetBitVector(instruction);
+  tmp_bit_vector_ = bit_vector;
     }
     
-    static void sigint_handler(int x) {
-  gpr_atm_no_barrier_store(&grpc::testing::interop::g_got_sigint, true);
+    sampled_audio: A rank 2 tensor containing all tracks of the audio. Dimension 0
+    is time and dimension 1 is the channel.
+contents: The binary audio file contents.
+file_format: A string describing the audio file format. This must be 'wav'.
+samples_per_second: The number of samples per second that the audio should have.
+bits_per_second: The approximate bitrate of the encoded audio file. This is
+    ignored by the 'wav' file format.
+)doc');
+    
+        NodeDef* add_node2 = graph_def.add_node();
+    add_node2->set_name('add_node2');
+    add_node2->set_op('Add');
+    add_node2->add_input('const_node1');
+    add_node2->add_input('const_node2');
+    add_node2->set_device('//device:GPU:1');
+    
+    class DebuggerClient {
+protected:
+  ASTContext &Ctx;
+public:
+  typedef SmallVectorImpl<LookupResultEntry> ResultVector;
+    }
+    
+    namespace swift {
+    }
+    
+    unsigned swift::unicode::extractFirstUnicodeScalar(StringRef S) {
+  unsigned Scalar;
+  bool Result = extractFirstUnicodeScalarImpl(S, Scalar);
+  assert(Result && 'string does not consist of one Unicode scalar');
+  (void)Result;
+  return Scalar;
 }
     
-    template <class T, class F>
-double sum(const T& container, F functor) {
-  double r = 0;
-  for (auto v = container.begin(); v != container.end(); v++) {
-    r += functor(*v);
+    namespace swift {
+class DependencyTracker;
+class ModuleDecl;
+class SourceFile;
+    }
+    
+    /// Attempt to get a doc comment from the declaration, or other inherited
+/// sources, like from base classes or protocols.
+Optional<DocComment *> getCascadingDocComment(swift::markup::MarkupContext &MC,
+                                             const Decl *D);
+    
+    /// Format a Syntax tree with the given rules.
+Syntax format(Syntax Tree);
+// TODO: Representation for formatting rules, etc. This is just a figment
+// for now.
+    
+    /// \brief Diagnostic consumer that displays diagnostics to standard error.
+class PrintingDiagnosticConsumer : public DiagnosticConsumer {
+  llvm::raw_ostream &Stream;
+  bool ForceColors = false;
+  bool DidErrorOccur = false;
+public:
+  PrintingDiagnosticConsumer(llvm::raw_ostream &stream = llvm::errs()) :
+    Stream(stream) { }
+    }
+    
+      /// Returns a buffer ID for a previously added buffer with the given
+  /// buffer identifier, or None if there is no such buffer.
+  Optional<unsigned> getIDForBufferIdentifier(StringRef BufIdentifier);
+    
+            bool isGeneralPosition;
+        do
+        {
+            ArrayTest::fill_array(test_case_idx, i, j, arr);
+    }
+    
+    
+    {
+    {
+    {            static __device__ __forceinline__ int atomicMax(int* address, int val)
+            {
+                return ::atomicMax(address, val);
+            }
+            static __device__ __forceinline__ float atomicMax(float* address, float val)
+            {
+            #if __CUDA_ARCH__ >= 120
+                int* address_as_i = (int*) address;
+                int old = *address_as_i, assumed;
+                do {
+                    assumed = old;
+                    old = ::atomicCAS(address_as_i, assumed,
+                        __float_as_int(::fmaxf(val, __int_as_float(assumed))));
+                } while (assumed != old);
+                return __int_as_float(old);
+            #else
+                (void) address;
+                (void) val;
+                return 0.0f;
+            #endif
+            }
+            static __device__ __forceinline__ double atomicMax(double* address, double val)
+            {
+            #if __CUDA_ARCH__ >= 130
+                unsigned long long int* address_as_ull = (unsigned long long int*) address;
+                unsigned long long int old = *address_as_ull, assumed;
+                do {
+                    assumed = old;
+                    old = ::atomicCAS(address_as_ull, assumed,
+                        __double_as_longlong(::fmax(val, __longlong_as_double(assumed))));
+                } while (assumed != old);
+                return __longlong_as_double(old);
+            #else
+                (void) address;
+                (void) val;
+                return 0.0;
+            #endif
+            }
+        };
+    }; //struct Emulation
+}}} // namespace cv { namespace cuda { namespace cudev
+    
+                // Extension: ARB_uniform_buffer_object
+            GetUniformIndices = Switch_GetUniformIndices;
+            GetActiveUniformsiv = Switch_GetActiveUniformsiv;
+            GetActiveUniformName = Switch_GetActiveUniformName;
+            GetUniformBlockIndex = Switch_GetUniformBlockIndex;
+            GetActiveUniformBlockiv = Switch_GetActiveUniformBlockiv;
+            GetActiveUniformBlockName = Switch_GetActiveUniformBlockName;
+            UniformBlockBinding = Switch_UniformBlockBinding;
+    
+    
+    {  default:
+    mpz_clear(gmpDataA);
+    mpz_clear(gmpDataB);
+    mpz_clear(gmpReturn);
+    raise_warning(cs_GMP_INVALID_ROUNDING_MODE, 'gmp_div_r');
+    return false;
   }
-  return r;
-}
+    
+    #include <atomic>
+#include 'stddef.h'
+    
+    NormalizedInstruction::NormalizedInstruction() { }
+    
+      [[noreturn]]
+  void reportMallocError(size_t nbytes) const;
     
     
-    {  return cli.Run(argc, argv);
-}
-
+    {}
     
-    bool js_cocos2dx_builder_CCBAnimationManager_constructor(JSContext *cx, uint32_t argc, jsval *vp);
-void js_cocos2dx_builder_CCBAnimationManager_finalize(JSContext *cx, JSObject *obj);
-void js_register_cocos2dx_builder_CCBAnimationManager(JSContext *cx, JS::HandleObject global);
-void register_all_cocos2dx_builder(JSContext* cx, JS::HandleObject obj);
-bool js_cocos2dx_builder_CCBAnimationManager_moveAnimationsFromNode(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setAutoPlaySequenceId(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getDocumentCallbackNames(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_actionForSoundChannel(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setBaseValue(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getDocumentOutletNodes(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getLastCompletedSequenceName(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setRootNode(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_runAnimationsForSequenceNamedTweenDuration(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_addDocumentOutletName(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getRootContainerSize(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setDocumentControllerName(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setObject(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getContainerSize(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_actionForCallbackChannel(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getDocumentOutletNames(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_addDocumentCallbackControlEvents(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_init(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getKeyframeCallbacks(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getDocumentCallbackControlEvents(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setRootContainerSize(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_runAnimationsForSequenceIdTweenDuration(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getRunningSequenceName(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getAutoPlaySequenceId(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_addDocumentCallbackName(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getRootNode(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_addDocumentOutletNode(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setDelegate(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getSequenceDuration(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_addDocumentCallbackNode(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_runAnimationsForSequenceNamed(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getSequenceId(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setCallFunc(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getDocumentCallbackNodes(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setSequences(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_debug(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getDocumentControllerName(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_CCBAnimationManager(JSContext *cx, uint32_t argc, jsval *vp);
-    
-    
-#if COCOS2D_DEBUG >= 1
-    if (!tolua_isusertype(tolua_S,1,'cc.SimpleAudioEngine',0,&tolua_err)) goto tolua_lerror;
-#endif
-    
-    int register_all_cocos2dx_csloader(lua_State* tolua_S);
-    
-    	AddPair()
-	{
-		m_world->SetGravity(b2Vec2(0.0f,0.0f));
-		{
-			b2CircleShape shape;
-			shape.m_p.SetZero();
-			shape.m_radius = 0.1f;
-    }
-    }
-    
-    	Breakable()
-	{
-		// Ground body
-		{
-			b2BodyDef bd;
-			b2Body* ground = m_world->CreateBody(&bd);
-    }
-    }
-    
-    		if (b2_toiCalls > 0)
-		{
-			m_debugDraw.DrawString(5, m_textLine, 'toi calls = %d, ave toi iters = %3.1f, max toi iters = %d',
-				b2_toiCalls, b2_toiIters / float32(b2_toiCalls), b2_toiMaxRootIters);
-			m_textLine += DRAW_STRING_NEW_LINE;
-    }
-    
-    	static Test* Create()
-	{
-		return new Cantilever;
-	}
+      static void Close();
+  static bool Available();
+  static void Initialize(const std::string &prefix, int count,
+                         bool trackProcessTimes,
+                         const std::vector<int> &inherited_fds);
+  static void ChangeUser(const std::string &username);
+  static void ChangeUser(int afdt, const std::string &username);
