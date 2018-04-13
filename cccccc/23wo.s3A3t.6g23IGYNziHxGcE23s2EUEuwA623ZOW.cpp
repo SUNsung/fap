82e4924f088bf946a48ded51d3177e8b6317bec2
SUNsung <weatherius@gmail.com>
2018-04-13 03:41:47 +0300

    Rewrite [23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp] File in: 2018-04-13 03:41:46

--- cccccc/23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp
+++ cccccc/23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp
@@ -1,138 +1,236 @@
 
         
-        
-    {}  // namespace api
+        CV_EXPORTS Mat NormalizePixels(const Mat& imagePoints, const IntrinsicParams& param);
     
-    namespace api {
+        // find corner idx
+    for(size_t i = 0; i < contour.size(); i++)
+    {
+        double dist = norm(contour[i] - point);
+        if(dist < min_dist)
+        {
+            min_dist = dist;
+            min_idx = (int)i;
+        }
     }
+    CV_Assert(min_idx >= 0);
     
-    namespace content {
-class WebContents;
+    void CV_ModelEstimator2_Test::prepare_to_validation( int test_case_idx )
+{
+    test_mat[OUTPUT][0].at<uchar>(0) = checkSubsetResult;
+    test_mat[REF_OUTPUT][0].at<uchar>(0) = test_case_idx < generalPositionsCount || usedPointsCount <= 2;
 }
     
-    #include 'net/url_request/url_request_http_job.h'
     
-    // Like URLRequestAsarJob, but asks the JavaScript handler for file path.
-class URLRequestAsyncAsarJob : public JsAsker<asar::URLRequestAsarJob> {
+    {            ptr[tid] = partial = partial + ptr[tid + 16];
+            ptr[tid] = partial = partial + ptr[tid + 8];
+            ptr[tid] = partial = partial + ptr[tid + 4];
+            ptr[tid] = partial = partial + ptr[tid + 2];
+            ptr[tid] = partial = partial + ptr[tid + 1];
+        }
+    
+    int lapack_LU32f(float* a, size_t a_step, int m, float* b, size_t b_step, int n, int* info);
+int lapack_LU64f(double* a, size_t a_step, int m, double* b, size_t b_step, int n, int* info);
+int lapack_Cholesky32f(float* a, size_t a_step, int m, float* b, size_t b_step, int n, bool* info);
+int lapack_Cholesky64f(double* a, size_t a_step, int m, double* b, size_t b_step, int n, bool* info);
+int lapack_SVD32f(float* a, size_t a_step, float* w, float* u, size_t u_step, float* vt, size_t v_step, int m, int n, int flags);
+int lapack_SVD64f(double* a, size_t a_step, double* w, double* u, size_t u_step, double* vt, size_t v_step, int m, int n, int flags);
+int lapack_QR32f(float* src1, size_t src1_step, int m, int n, int k, float* src2, size_t src2_step, float* dst, int* info);
+int lapack_QR64f(double* src1, size_t src1_step, int m, int n, int k, double* src2, size_t src2_step, double* dst, int* info);
+int lapack_gemm32f(const float* src1, size_t src1_step, const float* src2, size_t src2_step,
+                   float alpha, const float* src3, size_t src3_step, float beta, float* dst, size_t dst_step,
+                   int m, int n, int k, int flags);
+int lapack_gemm64f(const double* src1, size_t src1_step, const double* src2, size_t src2_step,
+                   double alpha, const double* src3, size_t src3_step, double beta, double* dst, size_t dst_step,
+                   int m, int n, int k, int flags);
+int lapack_gemm32fc(const float* src1, size_t src1_step, const float* src2, size_t src2_step,
+                   float alpha, const float* src3, size_t src3_step, float beta, float* dst, size_t dst_step,
+                   int m, int n, int k, int flags);
+int lapack_gemm64fc(const double* src1, size_t src1_step, const double* src2, size_t src2_step,
+                   double alpha, const double* src3, size_t src3_step, double beta, double* dst, size_t dst_step,
+                   int m, int n, int k, int flags);
+    
+    class Classifier {
  public:
-  URLRequestAsyncAsarJob(net::URLRequest*, net::NetworkDelegate*);
+  Classifier(const string& model_file,
+             const string& trained_file,
+             const string& mean_file,
+             const string& label_file);
     }
     
-      // URLRequestSimpleJob:
-  int GetRefCountedData(std::string* mime_type,
-                        std::string* charset,
-                        scoped_refptr<base::RefCountedMemory>* data,
-                        const net::CompletionCallback& callback) const override;
-    
-    // The entry point from ChromeMain into the relauncher process.
-int RelauncherMain(const content::MainFunctionParams& main_parameters);
-    
-    bool MenuModelAdapter::GetAccelerator(int id,
-                                      ui::Accelerator* accelerator) const {
-  ui::MenuModel* model = menu_model_;
-  int index = 0;
-  if (ui::MenuModel::GetModelAndIndexForCommandId(id, &model, &index)) {
-    return static_cast<AtomMenuModel*>(model)->
-      GetAcceleratorAtWithParams(index, true, accelerator);
+    int main(int argc, char** argv) {
+  if (argc != 4) {
+    printf('This script converts the MNIST dataset to the leveldb format used\n'
+           'by caffe to train a siamese network.\n'
+           'Usage:\n'
+           '    convert_mnist_data input_image_file input_label_file '
+           'output_db_file\n'
+           'The MNIST dataset could be downloaded at\n'
+           '    http://yann.lecun.com/exdb/mnist/\n'
+           'You should gunzip them after downloading.\n');
+  } else {
+    google::InitGoogleLogging(argv[0]);
+    convert_dataset(argv[1], argv[2], argv[3]);
   }
-  return false;
+  return 0;
 }
-    
-    #endif
+#else
+int main(int argc, char** argv) {
+  LOG(FATAL) << 'This example requires LevelDB; compile with USE_LEVELDB.';
+}
+#endif  // USE_LEVELDB
 
     
-    // skip isnan and isinf check for integral types
-template<typename To, typename From>
-typename std::enable_if<std::is_integral<From>::value, bool>::type overflows(From f) {
-  using limit = std::numeric_limits<To>;
-  return f < limit::lowest() || f > limit::max();
+    #if 0
+    
+    # define REGISTER_TYPED_TEST_CASE_P(CaseName, ...) \
+  namespace GTEST_CASE_NAMESPACE_(CaseName) { \
+  typedef ::testing::internal::Templates<__VA_ARGS__>::type gtest_AllTests_; \
+  } \
+  static const char* const GTEST_REGISTERED_TEST_NAMES_(CaseName) = \
+      GTEST_TYPED_TEST_CASE_P_STATE_(CaseName).VerifyRegisteredTestNames(\
+          __FILE__, __LINE__, #__VA_ARGS__)
+    
+    // GTEST_ASSERT_ is the basic statement to which all of the assertions
+// in this file reduce.  Don't use this in your code.
+    
+    // This macro is for implementing ASSERT/EXPECT_DEBUG_DEATH when compiled in
+// NDEBUG mode. In this case we need the statements to be executed, the regex is
+// ignored, and the macro must accept a streamed message even though the message
+// is never printed.
+# define GTEST_EXECUTE_STATEMENT_(statement, regex) \
+  GTEST_AMBIGUOUS_ELSE_BLOCKER_ \
+  if (::testing::internal::AlwaysTrue()) { \
+     GTEST_SUPPRESS_UNREACHABLE_CODE_WARNING_BELOW_(statement); \
+  } else \
+    ::testing::Message()
+    
+    # endif  // GTEST_HAS_COMBINE
+    
+    
+// Step 1. Include necessary header files such that the stuff your
+// test logic needs is declared.
+//
+// Don't forget gtest.h, which declares the testing framework.
+    
+    	if (!xlogger_IsEnabledFor((TLogLevel)level)) {
+		return;
+	}
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+    void CommFrequencyLimit::__DelOlderTouchTime(uint64_t _time) {
+    for (std::list<uint64_t>::iterator iter = touch_times_.begin(); iter != touch_times_.end();) {
+        if ((_time - (*iter)) > time_span_) {
+            iter = touch_times_.erase(iter);
+            continue;
+        }
+    }
+    }
+    
+    #include <string>
+#include <set>
+#include <map>
+    
+    //
+//  testspy_spy.cpp
+//  PublicComponent
+//
+//  Created by yerungui on 14-5-13.
+//
+    
+        void        (*GetBooleanArrayRegion)(JNIEnv*, jbooleanArray,
+                        jsize, jsize, jboolean*);
+    void        (*GetByteArrayRegion)(JNIEnv*, jbyteArray,
+                        jsize, jsize, jbyte*);
+    void        (*GetCharArrayRegion)(JNIEnv*, jcharArray,
+                        jsize, jsize, jchar*);
+    void        (*GetShortArrayRegion)(JNIEnv*, jshortArray,
+                        jsize, jsize, jshort*);
+    void        (*GetIntArrayRegion)(JNIEnv*, jintArray,
+                        jsize, jsize, jint*);
+    void        (*GetLongArrayRegion)(JNIEnv*, jlongArray,
+                        jsize, jsize, jlong*);
+    void        (*GetFloatArrayRegion)(JNIEnv*, jfloatArray,
+                        jsize, jsize, jfloat*);
+    void        (*GetDoubleArrayRegion)(JNIEnv*, jdoubleArray,
+                        jsize, jsize, jdouble*);
+    
+    #include <yoga/Yoga.h>
+    
+    namespace facebook {
+    }
+    
+    // Class that lets you declare a global but does not add a static constructor
+// to the binary. Eventually I'd like to have this auto-initialize in a
+// multithreaded environment but for now it's easiest just to use manual
+// initialization.
+template <typename T>
+class StaticInitialized {
+public:
+  constexpr StaticInitialized() :
+    m_instance(nullptr)
+  {}
+    }
+    
+    // Return the prologue of the generated mock file.
+grpc::string GetMockPrologue(grpc_generator::File *file,
+                             const Parameters &params);
+    
+    	if (genSend) {
+		printer->Print(vars, 'func (x *$StreamType$) Send(m *$Request$) error {\n');
+		printer->Indent();
+		printer->Print('return x.ClientStream.SendMsg(m)\n');
+		printer->Outdent();
+		printer->Print('}\n\n');
+	}
+    
+    namespace grpc_go_generator {
+    }
+    
+      auto channel =
+      grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials());
+  GreeterClient greeter(channel);
+    
+    #include <grpc++/impl/codegen/async_stream.h>
+#include <grpc++/impl/codegen/async_unary_call.h>
+#include <grpc++/impl/codegen/method_handler_impl.h>
+#include <grpc++/impl/codegen/proto_utils.h>
+#include <grpc++/impl/codegen/rpc_method.h>
+#include <grpc++/impl/codegen/service_type.h>
+#include <grpc++/impl/codegen/status.h>
+#include <grpc++/impl/codegen/stub_options.h>
+#include <grpc++/impl/codegen/sync_stream.h>
+    
+    inline std::string FlatBufferToString(const uint8_t *buffer,
+                                      const TypeTable *type_table) {
+  ToStringVisitor tostring_visitor;
+  IterateFlatBuffer(buffer, type_table, &tostring_visitor);
+  return tostring_visitor.s;
 }
     
-    #include <assert.h>
-#include <stdint.h>
-#include <stdexcept>
-#include <string>
-#include <utility>
+    #ifndef FLATBUFFERS_REGISTRY_H_
+#define FLATBUFFERS_REGISTRY_H_
     
-    #include <xgboost/logging.h>
-#include 'src/common/random.h'
-#include './xgboost_R.h'
-    
-    /*!
- * \brief Input stream that support additional PeekRead
- *  operation, besides read.
- */
-class PeekableInStream : public dmlc::Stream {
- public:
-  explicit PeekableInStream(dmlc::Stream* strm)
-      : strm_(strm), buffer_ptr_(0) {}
-    }
-    
-    namespace xgboost {
-namespace metric {
-// tag the this file, used by force static link later.
-DMLC_REGISTRY_FILE_TAG(elementwise_metric);
-    }
-    }
-    
-      T operator+=(T v) noexcept {
-    DeterministicSchedule::beforeSharedAccess();
-    T rv = (data += v);
-    FOLLY_TEST_DSCHED_VLOG(this << ' += ' << std::hex << v << ' -> ' << std::hex
-                                << rv);
-    DeterministicSchedule::afterSharedAccess(true);
-    return rv;
+      // Get and test the `weapons` FlatBuffers's `vector`.
+  std::string expected_weapon_names[] = { 'Sword', 'Axe' };
+  short expected_weapon_damages[] = { 3, 5 };
+  auto weps = monster->weapons();
+  for (unsigned int i = 0; i < weps->size(); i++) {
+    assert(weps->Get(i)->name()->str() == expected_weapon_names[i]);
+    assert(weps->Get(i)->damage() == expected_weapon_damages[i]);
   }
-    
-    using namespace folly::detail;
-using namespace folly::test;
-using namespace std;
-using namespace std::chrono;
-using folly::chrono::coarse_steady_clock;
-    
-    
-    {  atomic_shared_ptr<foo> asp;
-  asp.store(alias);
-  a.reset();
-  alias.reset();
-  auto res1 = asp.load();
-  auto res2 = asp.exchange(nullptr);
-  EXPECT_EQ(b, res1.get());
-  EXPECT_EQ(b, res2.get());
-  EXPECT_EQ(2, c_count);
-  EXPECT_EQ(0, d_count);
-  res1.reset();
-  res2.reset();
-  EXPECT_EQ(2, c_count);
-  EXPECT_EQ(1, d_count);
-  delete b;
-  EXPECT_EQ(2, c_count);
-  EXPECT_EQ(2, d_count);
-}
-    
-    #include <algorithm>
-#include <string>
-    
-    
-    {  // Check that global flags can still be given after the command
-  EXPECT_EQ(
-      'running foo\n'
-      'foo global-foo 43\n'
-      'foo local-foo 44\n'
-      'foo arg a\n'
-      'foo arg b\n',
-      callHelper({'foo', '--global-foo', '43', '--local-foo', '44',
-                 'a', 'b'}));
-}
-    
-     private:
-friend class AtomicHashMap<KeyT,
-                           ValueT,
-                           HashFcn,
-                           EqualFcn,
-                           Allocator,
-                           ProbeFcn>;
-    
-        if (isEnd() || other.isEnd()) {
-      return isEnd() == other.isEnd();
-    }
+  (void)expected_weapon_names;
+  (void)expected_weapon_damages;
