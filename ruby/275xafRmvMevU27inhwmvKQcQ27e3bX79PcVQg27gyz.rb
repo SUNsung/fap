056a181009d61fe30a60b0bbd1c7b342c789a2a5
SUNsung <weatherius@gmail.com>
2018-04-13 03:19:22 +0300

    Rewrite [275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb] File in: 2018-04-13 03:19:22

--- ruby/275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb
+++ ruby/275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb
@@ -1,125 +1,93 @@
 
         
-              def self.is_supported?(platform)
-        true
+        # No trailing slash
+Benchmark.ips do |x|
+  path = '/some/very/very/long/path/to/a/file/i/like/'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
+end
+
+    
+    CONTENT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+    
+    def test_dir(*subdirs)
+  File.join(TEST_DIR, *subdirs)
+end
+    
+        def no_subcommand(args)
+      unless args.empty? ||
+          args.first !~ %r(!/^--/!) || %w(--help --version).include?(args.first)
+        deprecation_message 'Jekyll now uses subcommands instead of just switches. \
+                          Run `jekyll help` to find out more.'
+        abort
+      end
+    end
+    
+        # Gets the path to this layout relative to its base
+    attr_reader :relative_path
+    
+          def warnings
+        @template.warnings
       end
     
-            command = [adb_path, 'devices'].join(' ')
-        output = Actions.sh(command, log: false)
-        output.split('\n').each do |line|
-          if (result = line.match(/(.*)\tdevice$/))
-            self.devices << AdbDevice.new(serial: result[1])
+        nil
+  end
+end
+    
+    @@ chat
+<pre id='chat'></pre>
+<form>
+  <input id='msg' placeholder='type message here...' />
+</form>
+    
+          def accepts?(env)
+        cookie_header = env['HTTP_COOKIE']
+        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
+        cookies.each do |k, v|
+          if k == session_key && Array(v).size > 1
+            bad_cookies << k
+          elsif k != session_key && Rack::Utils.unescape(k) == session_key
+            bad_cookies << k
           end
         end
-        self.devices
+        bad_cookies.empty?
       end
+    
+        it 'Returns nil when Referer header is invalid' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/bad|uri'}
+      expect(subject.referrer(env)).to be_nil
     end
   end
 end
 
     
-          it 'does set the output directory' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            cloc(output_directory: '/tmp')
-          end').runner.execute(:test)
-    
-    puts 'Validating #{links.size} links...'
-    
-        private
-    
-    module Metasploit
-  module Framework
-    class Application < Rails::Application
-      include Metasploit::Framework::CommonEngine
-    
-        if extension_pathname.readable?
-      ENV['BUNDLE_GEMFILE'] = extension_pathname.to_path
-      break
+        { # yes, this is ugly, feel free to change that
+      '/..' => '/', '/a/../b' => '/b', '/a/../b/' => '/b/', '/a/.' => '/a/',
+      '/%2e.' => '/', '/a/%2E%2e/b' => '/b', '/a%2f%2E%2e%2Fb/' => '/b/',
+      '//' => '/', '/%2fetc%2Fpasswd' => '/etc/passwd'
+    }.each do |a, b|
+      it('replaces #{a.inspect} with #{b.inspect}') { expect(get(a).body).to eq(b) }
     end
+    
+      # Many RSpec users commonly either run the entire suite or an individual
+  # file, and it's useful to allow more verbose output when running an
+  # individual spec file.
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured
+    # (e.g. via a command-line flag).
+    config.default_formatter = 'doc'
   end
-end
-    
-    #
-# This script extracts the forms from the main page of each
-# web site in a list. The output of this can be used with
-# Metasploit (and other tools) to obtain the saved form data
-# of these domains.
-#
-    
-      def parse(pkt)
-    # We want to return immediatly if we do not have a packet which is handled by us
-    return unless pkt.is_tcp?
-    return if (pkt.tcp_sport != 21 and pkt.tcp_dport != 21)
-    s = find_session((pkt.tcp_sport == 21) ? get_session_src(pkt) : get_session_dst(pkt))
-    s[:sname] ||= 'ftp'
-    
-          case matched
-        when :ok
-          # Last command was successful, in addition most servers transmit a banner with the first +OK
-          case s[:last]
-            when nil
-              # Its the first +OK must include the banner, worst case its just +OK
-              s[:info]  = matches
-              s[:proto] = 'tcp'
-              s[:name]  = 'pop3'
-              report_service(s)
-    
-    
-    
-    meterp.core.use('Stdapi')
-    
-    classNames.each { |name|
-	filesIn << clsFile.new_with_sig('Ljava.lang.String;', '#{outputDir}/#{name}.class')
-}
-    
-    		elsif line =~ /\w+:\t/
-			block_do(line)
-    
-    Then /^(?:|I )should see '([^']*)'$/ do |text|
-  expect(page).to have_content(text)
-end
-
-    
-        def self.names_for(klass)
-      instance.names_for(klass)
-    end
-    
-        def add_active_record_callbacks
-      name = @name
-      @klass.send(:after_save) { send(name).send(:save) }
-      @klass.send(:before_destroy) { send(name).send(:queue_all_for_delete) }
-      if @klass.respond_to?(:after_commit)
-        @klass.send(:after_commit, on: :destroy) do
-          send(name).send(:flush_deletes)
-        end
-      else
-        @klass.send(:after_destroy) { send(name).send(:flush_deletes) }
-      end
-    end
-    
-            def type_allowed?(type)
-          @subject.send('#{@attachment_name}_content_type=', type)
-          @subject.valid?
-          @subject.errors[:'#{@attachment_name}_content_type'].blank?
-        end
-    
-            def matches? subject
-          @subject = subject
-          @subject = subject.new if subject.class == Class
-          error_when_not_valid? && no_error_when_valid?
-        end
-    
-        # You can add your own processor via the Paperclip configuration. Normally
-    # Paperclip will load all processors from the
-    # Rails.root/lib/paperclip_processors directory, but here you can add any
-    # existing class using this mechanism.
-    #
-    #   Paperclip.configure do |c|
-    #     c.register_processor :watermarker, WatermarkingProcessor.new
-    #   end
-    def register_processor(name, processor)
-      @known_processors ||= {}
-      @known_processors[name.to_s] = processor
-    end
-  end
-end
