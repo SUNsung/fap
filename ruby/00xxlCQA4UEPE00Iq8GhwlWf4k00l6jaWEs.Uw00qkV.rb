e2d3b5ac1c3afb546d4076cf44e86ca085283090
SUNsung <weatherius@gmail.com>
2018-04-13 11:26:45 +0300

    Rewrite [00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb] File in: 2018-04-13 11:26:45

--- ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
+++ ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
@@ -1,92 +1,81 @@
 
         
-          def expand_deps
-    formula.recursive_dependencies do |dependent, dep|
-      build = effective_build_options_for(dependent)
-      if (dep.optional? || dep.recommended?) && build.without?(dep)
-        Dependency.prune
-      elsif dep.build? && dependent != formula
-        Dependency.prune
-      elsif dep.build?
-        Dependency.keep_but_prune_recursive_deps
+        module Docs
+  class PageDb
+    attr_reader :pages
+    
+        def origin
+      if scheme && host
+        origin = '#{scheme}://#{host}'
+        origin.downcase!
+        origin << ':#{port}' if port
+        origin
+      else
+        nil
+      end
+    end
+    
+          def merge(other)
+        dup.merge!(other)
+      end
+    
+              # Repackage the box
+          output_name = @env.vagrantfile.config.package.name || 'package.box'
+          output_path = Pathname.new(File.expand_path(output_name, FileUtils.pwd))
+          box.repackage(output_path)
+    
+              o.on('--check', 'Only checks for a capability, does not execute') do |f|
+            options[:check] = f
+          end
+        end
+    
+      def test_image_helper
+    assert_match %r(url\(['']?/assets/apple-touch-icon-144-precomposed.*png['']?\)), @css
+  end
+    
+          def to_yaml
+        @pairs.to_yaml
+      end
+    
+            expect(
+          [
+            :core,
+            :core2,
+            :penryn,
+            :nehalem,
+            :arrandale,
+            :sandybridge,
+            :ivybridge,
+            :haswell,
+            :broadwell,
+            :skylake,
+            :kabylake,
+            :dunno,
+          ],
+        ).to include(described_class.family)
+      end
+    end
+    
+            private
+    
+    When /^(?:|I )fill in '([^']*)' with '([^']*)'$/ do |field, value|
+  fill_in(field, :with => value)
+end
+    
+        # Returns the pluralized form of the attachment name. e.g.
+    # 'avatars' for an attachment of :avatar
+    def attachment attachment, style_name
+      plural_cache.pluralize_symbol(attachment.name)
+    end
+    
+            def failure_message
+          'Should have an attachment named #{@attachment_name}'
+        end
+    
+          if defined?(ActiveRecord)
+        Paperclip.options[:logger] = ActiveRecord::Base.logger
+        ActiveRecord::Base.send(:include, Paperclip::Glue)
       end
     end
   end
-    
-      # True if a {Formula} is being built with a specific option.
-  # <pre>args << '--i-want-spam' if build.with? 'spam'
-  #
-  # args << '--qt-gui' if build.with? 'qt' # '--with-qt' ==> build.with? 'qt'
-  #
-  # # If a formula presents a user with a choice, but the choice must be fulfilled:
-  # if build.with? 'example2'
-  #   args << '--with-example2'
-  # else
-  #   args << '--with-example1'
-  # end</pre>
-  def with?(val)
-    option_names = val.respond_to?(:option_names) ? val.option_names : [val]
-    
-      def elisp_caveats
-    return if f.keg_only?
-    if keg && keg.elisp_installed?
-      <<-EOS.undent
-        Emacs Lisp files have been installed to:
-          #{HOMEBREW_PREFIX}/share/emacs/site-lisp/#{f.name}
-      EOS
-    end
-  end
-    
-      def internal_commands
-    find_internal_commands HOMEBREW_LIBRARY_PATH/'cmd'
-  end
-    
-        # Exclude cache, logs, and repository, if they are located under the prefix.
-    [HOMEBREW_CACHE, HOMEBREW_LOGS, HOMEBREW_REPOSITORY].each do |dir|
-      dirs.delete dir.relative_path_from(HOMEBREW_PREFIX).to_s
-    end
-    dirs.delete 'etc'
-    dirs.delete 'var'
-    
-      # Sets flash message if is_flashing_format? equals true
-  def set_flash_message!(key, kind, options = {})
-    if is_flashing_format?
-      set_flash_message(key, kind, options)
-    end
-  end
-    
-          private
-    
-    # A logger that delays messages until they're explicitly flushed to an inner
-# logger.
-#
-# This can be installed around the current logger by calling \{#install!}, and
-# the original logger can be replaced by calling \{#uninstall!}. The log
-# messages can be flushed by calling \{#flush}.
-class Sass::Logger::Delayed < Sass::Logger::Base
-  # Installs a new delayed logger as the current Sass logger, wrapping the
-  # original logger.
-  #
-  # This can be undone by calling \{#uninstall!}.
-  #
-  # @return [Sass::Logger::Delayed] The newly-created logger.
-  def self.install!
-    logger = Sass::Logger::Delayed.new(Sass.logger)
-    Sass.logger = logger
-    logger
-  end
-    
-      module Sass::Plugin::Configuration
-    # Different default options in a m environment.
-    def default_options
-      @default_options ||= begin
-        version = Merb::VERSION.split('.').map {|n| n.to_i}
-        if version[0] <= 0 && version[1] < 5
-          root = MERB_ROOT
-          env  = MERB_ENV
-        else
-          root = Merb.root.to_s
-          env  = Merb.environment
-        end
-    
-      config_name 'codec'
+end
