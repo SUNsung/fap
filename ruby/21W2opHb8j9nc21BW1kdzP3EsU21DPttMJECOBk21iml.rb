7a1f7c9a15754a8eea710ebcd8014b2bb229d57c
SUNsung <weatherius@gmail.com>
2018-04-13 13:29:44 +0300

    Rewrite [21W2opHb8j9nc21BW1kdzP3EsU21DPttMJECOBk21iml.rb] File in: 2018-04-13 13:29:44

--- ruby/21W2opHb8j9nc21BW1kdzP3EsU21DPttMJECOBk21iml.rb
+++ ruby/21W2opHb8j9nc21BW1kdzP3EsU21DPttMJECOBk21iml.rb
@@ -1,81 +1,94 @@
 
         
-                if b_length > a_length
-          (b_length - a_length).times { a_split.insert(-2, 0) }
-        elsif a_length > b_length
-          (a_length - b_length).times { b_split.insert(-2, 0) }
-        end
-    
-            css('[id]').each do |node|
-          # Module
-          if node.name == 'h2'
-            type = node.content.remove 'Backbone.'
-            if type.capitalize! # sync, history
-              entries << [node.content, node['id'], type]
-            end
-            next
           end
     
-              node.css('.method').each do |n|
-            next unless n.at_css('dt[id]')
-            name = n.at_css('.descname').content
-            name = '#{class_name}::#{name}()'
-            id = n.at_css('dt[id]')['id']
-            entries << [name, id]
-          end
-        end
+      def revoke_moderation!
+    set_permission('moderator', false)
+  end
     
-            doc
+        find_union(segments, Group).order_id_desc
+  end
+    
+        def to_a
+      @filters.dup
+    end
+    
+      def test_frozen
+    ENV[PATH_ENV] = '/'
+    ENV.each do |k, v|
+      assert_predicate(k, :frozen?)
+      assert_predicate(v, :frozen?)
+    end
+    ENV.each_key do |k|
+      assert_predicate(k, :frozen?)
+    end
+    ENV.each_value do |v|
+      assert_predicate(v, :frozen?)
+    end
+    ENV.each_key do |k|
+      assert_predicate(ENV[k], :frozen?, '[#{k.dump}]')
+      assert_predicate(ENV.fetch(k), :frozen?, 'fetch(#{k.dump})')
+    end
+  end
+    
+      class Honda < Car
+    def initialize(*args)
+      self.make = 'Honda'
+      super(*args)
+    end
+  end
+    
+      it 'respects Thread subclasses' do
+    c = Class.new(Thread)
+    t = c.send(@method) { }
+    t.should be_kind_of(c)
+    
+        def metadata_subdir(leaf, version: self.version, timestamp: :latest, create: false)
+      if create && timestamp == :latest
+        raise CaskError, 'Cannot create metadata subdir when timestamp is :latest.'
       end
+    
+      # Clean a top-level (bin, sbin, lib) directory, recursively, by fixing file
+  # permissions and removing .la files, unless the files (or parent
+  # directories) are protected by skip_clean.
+  #
+  # bin and sbin should not have any subdirectories; if either do that is
+  # caught as an audit warning
+  #
+  # lib may have a large directory tree (see Erlang for instance), and
+  # clean_dir applies cleaning rules to the entire tree
+  def clean_dir(d)
+    d.find do |path|
+      path.extend(ObserverPathnameExtension)
+    
+      context 'called with three colors' do
+    it 'applies second color to left and right' do
+      rule = 'border-color: #f00 #0f0 #00f'
+    
+          expect('.all-buttons-active').to have_ruleset(ruleset)
+    end
+  end
+    
+      context 'called with null values' do
+    it 'writes rules for other three' do
+      ruleset = 'margin-top: 11px; ' +
+                'margin-right: 12px; ' +
+                'margin-left: 13px;'
+      bad_rule = 'margin-bottom: null;'
+    
+      context 'called with one size' do
+    it 'applies same width to all sides' do
+      rule = 'padding: 1px'
+    
+      context 'called with two sizes' do
+    it 'applies to alternating sides' do
+      ruleset = 'position: absolute; ' +
+                'top: 2px; ' +
+                'right: 3px; ' +
+                'bottom: 2px; ' +
+                'left: 3px;'
+    
+          expect('.prefix--moz-ms').to have_ruleset(rule)
     end
   end
 end
-
-    
-          def merge!(other)
-        other.each do |key, value|
-          self[convert_key(key)] = value
-        end
-        self
-      end
-    
-      def admin_inviter
-    inviter = InvitationCode.default_inviter_or(current_user)
-    email = params[:identifier]
-    user = User.find_by_email(email)
-    
-    Liquid::Template.register_tag('blockquote', Jekyll::Blockquote)
-
-    
-    def config_tag(config, key, tag=nil, classname=nil)
-  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
-  tag       ||= 'div'
-  classname ||= key.sub(/_/, '-').sub(/\./, '-')
-  output      = '<#{tag} class='#{classname}''
-    
-        def render(context)
-      if @img
-        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
-      else
-        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
-      end
-    end
-  end
-end
-    
-        def render(context)
-      code_dir = (context.registers[:site].config['code_dir'].sub(/^\//,'') || 'downloads/code')
-      code_path = (Pathname.new(context.registers[:site].source) + code_dir).expand_path
-      file = code_path + @file
-    
-      class RenderPartialTag < Liquid::Tag
-    include OctopressFilters
-    def initialize(tag_name, markup, tokens)
-      @file = nil
-      @raw = false
-      if markup =~ /^(\S+)\s?(\w+)?/
-        @file = $1.strip
-        @raw = $2 == 'raw'
-      end
-      super
-    end
