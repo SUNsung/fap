351c3a5de0c952e32adcf49948d165ab95b05a64
SUNsung <weatherius@gmail.com>
2018-04-14 19:40:30 +0300

    Rewrite [400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py] File in: 2018-04-14 19:40:29

--- pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
+++ pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
@@ -1,40 +1,68 @@
 
         
-            for filename in filenames:
-        fd = codecs.open(filename, mode='r', encoding='utf-8')
-        for line in fd.readlines():
-            refs = re.findall(r'(?<=<a href=')[^']*', markdown.markdown(line))
-            for ref in refs:
-                if ref not in urls:
-                    urls.append(ref)
+                current = current.parent
     
-    from r2.lib.translation import I18N_PATH
-from r2.lib.plugin import PluginLoader
-from r2.lib import js
+        def get_expiration_time(self, app, session):
+        '''A helper method that returns an expiration date for the session
+        or ``None`` if the session is linked to the browser session.  The
+        default implementation returns now + the permanent session
+        lifetime configured on the application.
+        '''
+        if session.permanent:
+            return datetime.utcnow() + app.permanent_session_lifetime
     
-        app.config = config
+    from jinja2 import BaseLoader, Environment as BaseEnvironment, \
+     TemplateNotFound
     
-    api('reltableitem', RelTableItemJsonTemplate)
-api('bannedtableitem', BannedTableItemJsonTemplate)
-api('mutedtableitem', MutedTableItemJsonTemplate)
-api('invitedmodtableitem', InvitedModTableItemJsonTemplate)
-api('friendtableitem', FriendTableItemJsonTemplate)
+        When you want to decorate a pluggable view you will have to either do that
+    when the view function is created (by wrapping the return value of
+    :meth:`as_view`) or you can use the :attr:`decorators` attribute::
     
-        @csrf_exempt
-    @json_validate(
-        signature=VSigned(),
-        user=VThrottledLogin(['user', 'passwd']),
-    )
-    def POST_login(self, responder, user, **kwargs):
-        kwargs.update(dict(
-            controller=self,
-            form=responder('noop'),
-            responder=responder,
-            user=user,
-        ))
-        return handle_login(**kwargs)
     
-        def GET_health(self):
-        if os.path.exists('/var/opt/reddit/quiesce'):
-            request.environ['usable_error_content'] = 'No thanks, I'm full.'
-            abort(503)
+def parse_date(string):
+    string = _date_strip_re.sub('', string)
+    return datetime.strptime(string, '%B %d %Y')
+    
+        @subgroup.command(with_appcontext=True)
+    def test2():
+        click.echo(current_app.name)
+    
+    
+def test_explicit_instance_paths(modules_tmpdir):
+    with pytest.raises(ValueError) as excinfo:
+        flask.Flask(__name__, instance_path='instance')
+    assert 'must be absolute' in str(excinfo.value)
+    
+        def test_print(self):
+        d = deque(range(200))
+        d.append(d)
+        try:
+            support.unlink(support.TESTFN)
+            fo = open(support.TESTFN, 'w')
+            print(d, file=fo, end='')
+            fo.close()
+            fo = open(support.TESTFN, 'r')
+            self.assertEqual(fo.read(), repr(d))
+        finally:
+            fo.close()
+            support.unlink(support.TESTFN)
+    
+        def _scan_once(string, idx):
+        try:
+            nextchar = string[idx]
+        except IndexError:
+            raise StopIteration(idx) from None
+    
+            x.value = -99
+        self.assertEqual(res.contents.value, -99)
+    
+        def test_print(self):
+        try:
+            fo = open(support.TESTFN, 'w')
+            print(False, True, file=fo)
+            fo.close()
+            fo = open(support.TESTFN, 'r')
+            self.assertEqual(fo.read(), 'False True\n')
+        finally:
+            fo.close()
+            os.remove(support.TESTFN)
