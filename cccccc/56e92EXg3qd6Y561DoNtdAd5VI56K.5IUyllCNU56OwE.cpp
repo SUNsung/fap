31e007444909f5c30f4f00085f3bd07fe8ff2fe4
SUNsung <weatherius@gmail.com>
2018-04-14 08:53:18 +0300

    Rewrite [56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp] File in: 2018-04-14 08:53:17

--- cccccc/56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp
+++ cccccc/56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp
@@ -1,226 +1,143 @@
 
         
-        namespace tensorflow {
-    }
-    
-          T* resource;
-      OP_REQUIRES_OK(
-          context,
-          mgr->LookupOrCreate<T>(cinfo_.container(), cinfo_.name(), &resource,
-                                 [this](T** ret) EXCLUSIVE_LOCKS_REQUIRED(mu_) {
-                                   Status s = CreateResource(ret);
-                                   if (!s.ok() && *ret != nullptr) {
-                                     CHECK((*ret)->Unref());
-                                   }
-                                   return s;
-                                 }));
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-      // The underlying CUDA event element.
-  const CUevent& cuda_event();
-    
-    // A PluginConfig describes the set of plugins to be used by a StreamExecutor
-// instance. Each plugin is defined by an arbitrary identifier, usually best set
-// to the address static member in the implementation (to avoid conflicts).
-//
-// A PluginConfig may be passed to the StreamExecutor constructor - the plugins
-// described therein will be used to provide BLAS, DNN, FFT, and RNG
-// functionality. Platform-appropriate defaults will be used for any un-set
-// libraries. If a platform does not support a specified plugin (ex. cuBLAS on
-// an OpenCL executor), then an error will be logged and no plugin operations
-// will succeed.
-//
-// The StreamExecutor BUILD target does not link ANY plugin libraries - even
-// common host fallbacks! Any plugins must be explicitly linked by dependent
-// targets. See the cuda, opencl and host BUILD files for implemented plugin
-// support (search for 'plugin').
-class PluginConfig {
- public:
-  // Value specifying the platform's default option for that plugin.
-  static const PluginId kDefault;
-    }
-    
-      // Calls Close() and logs if an error occurs.
-  //
-  // TODO(jhseu): Require that callers explicitly call Close() and remove the
-  // implicit Close() call in the destructor.
-  ~RecordWriter();
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    bool HloReachabilityMap::SetReachabilityToUnion(
-    tensorflow::gtl::ArraySlice<const HloInstruction*> inputs,
-    const HloInstruction* instruction) {
-  BitVector& bit_vector = GetBitVector(instruction);
-  tmp_bit_vector_ = bit_vector;
-    }
-    
-    sampled_audio: A rank 2 tensor containing all tracks of the audio. Dimension 0
-    is time and dimension 1 is the channel.
-contents: The binary audio file contents.
-file_format: A string describing the audio file format. This must be 'wav'.
-samples_per_second: The number of samples per second that the audio should have.
-bits_per_second: The approximate bitrate of the encoded audio file. This is
-    ignored by the 'wav' file format.
-)doc');
-    
-        NodeDef* add_node2 = graph_def.add_node();
-    add_node2->set_name('add_node2');
-    add_node2->set_op('Add');
-    add_node2->add_input('const_node1');
-    add_node2->add_input('const_node2');
-    add_node2->set_device('//device:GPU:1');
-    
-    class DebuggerClient {
-protected:
-  ASTContext &Ctx;
-public:
-  typedef SmallVectorImpl<LookupResultEntry> ResultVector;
-    }
-    
-    namespace swift {
-    }
-    
-    unsigned swift::unicode::extractFirstUnicodeScalar(StringRef S) {
-  unsigned Scalar;
-  bool Result = extractFirstUnicodeScalarImpl(S, Scalar);
-  assert(Result && 'string does not consist of one Unicode scalar');
-  (void)Result;
-  return Scalar;
+        inline void findCurvesCross(const std::vector<float>& /*curve1*/, const std::vector<float>& /*curve2*/, Point2f& /*cross_point*/)
+{
 }
     
-    namespace swift {
-class DependencyTracker;
-class ModuleDecl;
-class SourceFile;
-    }
-    
-    /// Attempt to get a doc comment from the declaration, or other inherited
-/// sources, like from base classes or protocols.
-Optional<DocComment *> getCascadingDocComment(swift::markup::MarkupContext &MC,
-                                             const Decl *D);
-    
-    /// Format a Syntax tree with the given rules.
-Syntax format(Syntax Tree);
-// TODO: Representation for formatting rules, etc. This is just a figment
-// for now.
-    
-    /// \brief Diagnostic consumer that displays diagnostics to standard error.
-class PrintingDiagnosticConsumer : public DiagnosticConsumer {
-  llvm::raw_ostream &Stream;
-  bool ForceColors = false;
-  bool DidErrorOccur = false;
-public:
-  PrintingDiagnosticConsumer(llvm::raw_ostream &stream = llvm::errs()) :
-    Stream(stream) { }
-    }
-    
-      /// Returns a buffer ID for a previously added buffer with the given
-  /// buffer identifier, or None if there is no such buffer.
-  Optional<unsigned> getIDForBufferIdentifier(StringRef BufIdentifier);
-    
-            bool isGeneralPosition;
-        do
+            struct smem
         {
-            ArrayTest::fill_array(test_case_idx, i, j, arr);
+            enum { TAG_MASK = (1U << ( (sizeof(unsigned int) << 3) - 5U)) - 1U };
     }
     
-    
+    namespace
+{
+    template <class ObjType>
+    void ensureSizeIsEnoughImpl(int rows, int cols, int type, ObjType& obj)
     {
-    {
-    {            static __device__ __forceinline__ int atomicMax(int* address, int val)
-            {
-                return ::atomicMax(address, val);
-            }
-            static __device__ __forceinline__ float atomicMax(float* address, float val)
-            {
-            #if __CUDA_ARCH__ >= 120
-                int* address_as_i = (int*) address;
-                int old = *address_as_i, assumed;
-                do {
-                    assumed = old;
-                    old = ::atomicCAS(address_as_i, assumed,
-                        __float_as_int(::fmaxf(val, __int_as_float(assumed))));
-                } while (assumed != old);
-                return __int_as_float(old);
-            #else
-                (void) address;
-                (void) val;
-                return 0.0f;
-            #endif
-            }
-            static __device__ __forceinline__ double atomicMax(double* address, double val)
-            {
-            #if __CUDA_ARCH__ >= 130
-                unsigned long long int* address_as_ull = (unsigned long long int*) address;
-                unsigned long long int old = *address_as_ull, assumed;
-                do {
-                    assumed = old;
-                    old = ::atomicCAS(address_as_ull, assumed,
-                        __double_as_longlong(::fmax(val, __longlong_as_double(assumed))));
-                } while (assumed != old);
-                return __longlong_as_double(old);
-            #else
-                (void) address;
-                (void) val;
-                return 0.0;
-            #endif
-            }
-        };
-    }; //struct Emulation
-}}} // namespace cv { namespace cuda { namespace cudev
+        if (obj.empty() || obj.type() != type || obj.data != obj.datastart)
+        {
+            obj.create(rows, cols, type);
+        }
+        else
+        {
+            const size_t esz = obj.elemSize();
+            const ptrdiff_t delta2 = obj.dataend - obj.datastart;
+    }
+    }
+    }
     
-                // Extension: ARB_uniform_buffer_object
-            GetUniformIndices = Switch_GetUniformIndices;
-            GetActiveUniformsiv = Switch_GetActiveUniformsiv;
-            GetActiveUniformName = Switch_GetActiveUniformName;
-            GetUniformBlockIndex = Switch_GetUniformBlockIndex;
-            GetActiveUniformBlockiv = Switch_GetActiveUniformBlockiv;
-            GetActiveUniformBlockName = Switch_GetActiveUniformBlockName;
-            UniformBlockBinding = Switch_UniformBlockBinding;
+    #ifdef HAVE_CLAMDFFT
     
-    
-    {  default:
-    mpz_clear(gmpDataA);
-    mpz_clear(gmpDataB);
-    mpz_clear(gmpReturn);
-    raise_warning(cs_GMP_INVALID_ROUNDING_MODE, 'gmp_div_r');
-    return false;
+    SlowTimer::~SlowTimer() {
+  int64_t msec = getTime();
+  if (msec >= m_msThreshold && m_msThreshold != 0) {
+    Logger::Error('SlowTimer [%' PRId64 'ms] at %s: %s',
+                  msec, m_location.c_str(), m_info.c_str());
   }
+}
     
-    #include <atomic>
-#include 'stddef.h'
+    #include <folly/Optional.h>
     
-    NormalizedInstruction::NormalizedInstruction() { }
     
-      [[noreturn]]
-  void reportMallocError(size_t nbytes) const;
+static Variant HHVM_FUNCTION(gmp_legendre,
+                             const Variant& dataA,
+                             const Variant& dataB) {
+  mpz_t gmpDataA, gmpDataB;
+    }
     
     
     {}
     
-      static void Close();
-  static bool Available();
-  static void Initialize(const std::string &prefix, int count,
-                         bool trackProcessTimes,
-                         const std::vector<int> &inherited_fds);
-  static void ChangeUser(const std::string &username);
-  static void ChangeUser(int afdt, const std::string &username);
+    struct TransMetaInfo {
+  SrcKey sk;
+  CodeCache::View emitView; // View code was emitted into (may be thread local)
+  TransKind viewKind; // TransKind used to select code view
+  TransKind transKind; // TransKind used for translation
+  TransRange range;
+  CGMeta meta;
+  TransRec transRec;
+};
+    
+    const char* OfflineCode::getArchName() { return 'PPC64'; }
+    
+    bool get_embedded_data(const char* section, embedded_data* desc,
+                       const std::string& filename /*= '' */) {
+  auto const fname = filename.empty() ? current_executable_path() : filename;
+    }
+    
+      /**
+   * For later light processes to close their pipes to previous ones.
+   */
+  void closeFiles();
+    
+    #include 'test/cpp/qps/benchmark_config.h'
+#include 'test/cpp/qps/driver.h'
+#include 'test/cpp/qps/report.h'
+#include 'test/cpp/qps/server.h'
+#include 'test/cpp/util/test_config.h'
+#include 'test/cpp/util/test_credentials_provider.h'
+    
+    int main(int argc, char** argv) {
+  grpc::testing::InitTest(&argc, &argv, true);
+    }
+    
+    #include <algorithm>
+    
+    // Data pertaining to configuration of the generator with respect to anything
+// that may be used internally at Google.
+struct GeneratorConfiguration {
+  GeneratorConfiguration();
+  grpc::string grpc_package_root;
+  // TODO(https://github.com/grpc/grpc/issues/8622): Drop this.
+  grpc::string beta_package_root;
+  // TODO(https://github.com/google/protobuf/issues/888): Drop this.
+  grpc::string import_prefix;
+};
+    
+    class QpsGauge {
+ public:
+  QpsGauge();
+    }
+    
+    CredentialsProvider* GetCredentialsProvider() {
+  if (g_provider == nullptr) {
+    g_provider = new DefaultCredentialsProvider;
+  }
+  return g_provider;
+}
+    
+    static int populate_ifaddrs(struct ifaddrs* ifaddr, ifaddrmsg* msg, void* bytes,
+		     size_t len) {
+	if (set_ifname(ifaddr, msg->ifa_index) != 0) {
+		return -1;
+	}
+	if (set_flags(ifaddr) != 0) {
+		return -1;
+	}
+	if (set_addresses(ifaddr, msg, bytes, len) != 0) {
+		return -1;
+	}
+	if (make_prefixes(ifaddr, msg->ifa_family, msg->ifa_prefixlen) != 0) {
+		return -1;
+	}
+	return 0;
+}
+    
+    void VideoStreamPlaybackTheora::set_file(const String &p_file) {
+    }
+    
+    
+    {		if (!S_ISREG(st.st_mode))
+			return ERR_FILE_CANT_OPEN;
+	};
+    
+    #endif // JAVASCRIPT_ENABLED
+
+    
+    	static RWLock *lock;
+	static HashMap<StringName, ClassInfo, StringNameHasher> classes;
+	static HashMap<StringName, StringName, StringNameHasher> resource_base_extensions;
+	static HashMap<StringName, StringName, StringNameHasher> compat_classes;
+    
+    	void _fill_points(const PoolVector<Vector2> p_points, const Dictionary &p_op);
+	void _erase_points(const PoolVector<Vector2> p_points);
