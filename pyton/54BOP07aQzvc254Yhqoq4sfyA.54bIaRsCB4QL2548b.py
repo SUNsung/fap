d3bf4343a46138d4c4db2ebc8cae2f42509293cf
SUNsung <weatherius@gmail.com>
2018-04-14 21:02:00 +0300

    Rewrite [54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py] File in: 2018-04-14 21:01:59

--- pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
+++ pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
@@ -1,129 +1,193 @@
 
         
-        if __name__ == '__main__':
-    main()
-
-    
-    
-total_bytes = 0
-    
-    # Allow direct execution
-import os
-import sys
-import unittest
-import collections
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-    
-            input_args = _find_spec('input')
-        output = _find_spec('output')
-    
-    
-class TestUnicodeLiterals(unittest.TestCase):
-    def test_all_files(self):
-        for dirpath, dirnames, filenames in os.walk(rootDir):
-            for ignore_dir in IGNORED_DIRS:
-                if ignore_dir in dirnames:
-                    # If we remove the directory from dirnames os.walk won't
-                    # recurse into it
-                    dirnames.remove(ignore_dir)
-            for basename in filenames:
-                if not basename.endswith('.py'):
-                    continue
-                if basename in IGNORED_FILES:
-                    continue
-    
-    
-class RtspFD(FileDownloader):
-    def real_download(self, filename, info_dict):
-        url = info_dict['url']
-        self.report_destination(filename)
-        tmpfilename = self.temp_name(filename)
-    
-        def _real_extract(self, url):
-        display_id = self._match_id(url)
-    
-        if address_family == socket.AF_INET:
-        if len(packed_ip) != ctypes.sizeof(addr.ipv4_addr):
-            raise socket.error('packed IP wrong length for inet_ntoa')
-        ctypes.memmove(addr.ipv4_addr, packed_ip, 4)
-    elif address_family == socket.AF_INET6:
-        if len(packed_ip) != ctypes.sizeof(addr.ipv6_addr):
-            raise socket.error('packed IP wrong length for inet_ntoa')
-        ctypes.memmove(addr.ipv6_addr, packed_ip, 16)
-    else:
-        raise socket.error('unknown address family')
-    
-    
-    def mismatchIsMissingToken(self, input, follow):
-        if follow is None:
-            # we have no information about the follow; we can only consume
-            # a single token and hope for the best
-            return False
         
-        # compute what can follow this grammar element reference
-        if EOR_TOKEN_TYPE in follow:
-            if len(self._state.following) > 0:
-                # remove EOR if we're not the start symbol
-                follow = follow - set([EOR_TOKEN_TYPE])
+def with_autoparallel_prefix(replica_id, name):
+  return with_prefix('AutoParallel-Replica-%d' % replica_id, name)
     
+      An n-dimensional array of shape (num_samples, ...) is converted into an
+  (n+1)-D array of shape (num_frames, window_length, ...), where each frame
+  starts hop_length points after the preceding one.
     
-    def skipOffTokenChannels(self, i):
-        '''
-        Given a starting index, return the index of the first on-channel
-        token.
-        '''
+    from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
     
-            #Conversion of md to HTML
-        for md in allmd:
+      @tf.test.mock.patch.dict(premade_estimator.__dict__,
+                           {'load_data': four_lines_data})
+  def test_premade_estimator(self):
+    premade_estimator.main([None, '--train_steps=1'])
     
-                RuleRouter([
-                ('/handler', Target),
-            ])
+    # Create a custom estimator using my_model_fn to define the model
+tf.logging.info('Before classifier construction')
+classifier = tf.estimator.Estimator(
+    model_fn=my_model_fn,
+    model_dir=PATH)  # Path to where checkpoints etc are stored
+tf.logging.info('...done constructing classifier')
     
-            namespace = exec_test(globals(), locals(), '''
-        async def f(idx):
-            async with lock:
-                history.append(idx)
-        ''')
-        futures = [namespace['f'](i) for i in range(N)]
-        lock.release()
-        yield futures
-        self.assertEqual(list(range(N)), history)
+    # Flags for input data are in document_generators.py
+flags.DEFINE_string('vocab_file', '', 'Path to the vocabulary file. Defaults '
+                    'to FLAGS.output_dir/vocab.txt.')
+flags.DEFINE_string('output_dir', '', 'Path to save tfrecords.')
     
-            @gen.coroutine
-        def connect(c):
-            try:
-                yield c.connect(server_addr)
-            except EnvironmentError:
-                pass
-            else:
-                connected_clients.append(c)
+    Computational time:
+  2 days to train 100000 steps on 1 layer 1024 hidden units LSTM,
+  256 embeddings, 400 truncated BP, 256 minibatch and on single GPU (Pascal
+  Titan X, cuDNNv5).
+'''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+    
+        scikit_results = []
+    glmnet_results = []
+    n = 20
+    step = 100
+    n_samples = 500
+    
+        n_samples = 2000
+    list_n_features = np.linspace(500, 3000, 5).astype(np.int)
+    lasso_results, lars_lasso_results = compute_bench(alpha, [n_samples],
+                                           list_n_features, precompute=False)
+    plt.subplot(212)
+    plt.plot(list_n_features, lasso_results, 'b-', label='Lasso')
+    plt.plot(list_n_features, lars_lasso_results, 'r-', label='LassoLars')
+    plt.title('%d samples, alpha=%s' % (n_samples, alpha))
+    plt.legend(loc='upper left')
+    plt.xlabel('number of features')
+    plt.ylabel('Time (s)')
+    plt.axis('tight')
+    plt.show()
+
     
     
 if __name__ == '__main__':
-    main()
-
+    ap = argparse.ArgumentParser()
+    ap.add_argument('metrics', nargs='*', default=sorted(METRICS),
+                    help='Specifies metrics to benchmark, defaults to all. '
+                         'Choices are: {}'.format(sorted(METRICS)))
+    ap.add_argument('--formats', nargs='+', choices=sorted(FORMATS),
+                    help='Specifies multilabel formats to benchmark '
+                         '(defaults to all).')
+    ap.add_argument('--samples', type=int, default=1000,
+                    help='The number of samples to generate')
+    ap.add_argument('--classes', type=int, default=10,
+                    help='The number of classes')
+    ap.add_argument('--density', type=float, default=.2,
+                    help='The average density of labels per sample')
+    ap.add_argument('--plot', choices=['classes', 'density', 'samples'],
+                    default=None,
+                    help='Plot time with respect to this parameter varying '
+                         'up to the specified value')
+    ap.add_argument('--n-steps', default=10, type=int,
+                    help='Plot this many points for each metric')
+    ap.add_argument('--n-times',
+                    default=5, type=int,
+                    help='Time performance over n_times trials')
+    args = ap.parse_args()
     
-        @gen.coroutine
-    def post(self):
-        author = self.db.get('SELECT * FROM authors WHERE email = %s',
-                             self.get_argument('email'))
-        if not author:
-            self.render('login.html', error='email not found')
-            return
-        hashed_password = yield executor.submit(
-            bcrypt.hashpw, tornado.escape.utf8(self.get_argument('password')),
-            tornado.escape.utf8(author.hashed_password))
-        if hashed_password == author.hashed_password:
-            self.set_secure_cookie('blogdemo_user', str(author.id))
-            self.redirect(self.get_argument('next', '/'))
+    
+def plot_batch_errors(all_errors, n_features, all_batch_sizes, data):
+    plt.figure()
+    plot_results(all_batch_sizes, all_errors['pca'], label='PCA')
+    plot_results(all_batch_sizes, all_errors['ipca'], label='IncrementalPCA')
+    plt.legend(loc='lower left')
+    plt.suptitle('Algorithm error vs. batch_size for n_components %i\n \
+                 LFW, size %i x %i' % (
+                 n_features, data.shape[0], data.shape[1]))
+    plt.xlabel('Batch size')
+    plt.ylabel('Mean absolute error')
+    
+    
+def bench_scikit_tree_classifier(X, Y):
+    '''Benchmark with scikit-learn decision tree classifier'''
+    
+    html_folder = u'html'
+text_folder = u'paragraphs'
+short_text_folder = u'short_paragraphs'
+n_words_per_short_text = 5
+    
+    import sys
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+    
+    # Learn a frontier for outlier detection with several classifiers
+xx1, yy1 = np.meshgrid(np.linspace(-8, 28, 500), np.linspace(3, 40, 500))
+xx2, yy2 = np.meshgrid(np.linspace(3, 10, 500), np.linspace(-5, 45, 500))
+for i, (clf_name, clf) in enumerate(classifiers.items()):
+    plt.figure(1)
+    clf.fit(X1)
+    Z1 = clf.decision_function(np.c_[xx1.ravel(), yy1.ravel()])
+    Z1 = Z1.reshape(xx1.shape)
+    legend1[clf_name] = plt.contour(
+        xx1, yy1, Z1, levels=[0], linewidths=2, colors=colors[i])
+    plt.figure(2)
+    clf.fit(X2)
+    Z2 = clf.decision_function(np.c_[xx2.ravel(), yy2.ravel()])
+    Z2 = Z2.reshape(xx2.shape)
+    legend2[clf_name] = plt.contour(
+        xx2, yy2, Z2, levels=[0], linewidths=2, colors=colors[i])
+    
+        def plot_decision_surface(self, surface, type):
+        X1, X2, Z = surface
+        if type == 0:
+            levels = [-1.0, 0.0, 1.0]
+            linestyles = ['dashed', 'solid', 'dashed']
+            colors = 'k'
+            self.contours.append(self.ax.contour(X1, X2, Z, levels,
+                                                 colors=colors,
+                                                 linestyles=linestyles))
+        elif type == 1:
+            self.contours.append(self.ax.contourf(X1, X2, Z, 10,
+                                                  cmap=matplotlib.cm.bone,
+                                                  origin='lower', alpha=0.85))
+            self.contours.append(self.ax.contour(X1, X2, Z, [0.0], colors='k',
+                                                 linestyles=['solid']))
         else:
-            self.render('login.html', error='incorrect password')
+            raise ValueError('surface type unknown')
+    
+    print('consensus score: {:.1f}'.format(score))
+    
+        fig = plt.figure(fig_index, figsize=(10, 10))
+    ax1 = plt.subplot2grid((3, 1), (0, 0), rowspan=2)
+    ax2 = plt.subplot2grid((3, 1), (2, 0))
+    
+    Plot the classification probability for different classifiers. We use a 3
+class dataset, and we classify it with a Support Vector classifier, L1
+and L2 penalized logistic regression with either a One-Vs-Rest or multinomial
+setting, and Gaussian process classification.
+    
+        '''
     
     
-class BaseHandler(tornado.web.RequestHandler):
-    def get_current_user(self):
-        user_json = self.get_secure_cookie('fbdemo_user')
-        if not user_json:
-            return None
-        return tornado.escape.json_decode(user_json)
+# This class can be used to keep the ycmd server alive for the duration of the
+# life of the client. By default, ycmd shuts down if it doesn't see a request in
+# a while.
+class YcmdKeepalive( object ):
+  def __init__( self, ping_interval_seconds = 60 * 10 ):
+    self._keepalive_thread = Thread( target = self._ThreadMain )
+    self._keepalive_thread.daemon = True
+    self._ping_interval_seconds = ping_interval_seconds
+    
+    API_TYPES = ('api', 'json')
+RSS_TYPES = ('rss', 'xml')
+    
+    
+class APIv1UserController(OAuth2OnlyController):
+    @require_oauth2_scope('identity')
+    @validate(
+        VUser(),
+    )
+    @api_doc(api_section.account)
+    def GET_me(self):
+        'Returns the identity of the user currently authenticated via OAuth.'
+        resp = IdentityJsonTemplate().data(c.oauth_user)
+        return self.api_wrapper(resp)
+    
+        def GET_blog(self):
+        return self.redirect('https://blog.%s/' %
+                             get_domain(subreddit=False, no_www=True))
