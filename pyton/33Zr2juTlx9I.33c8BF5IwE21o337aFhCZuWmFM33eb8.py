1637ca5f50b8e39a7d0535fb9d8c7a282f423f65
SUNsung <weatherius@gmail.com>
2018-04-14 13:57:34 +0300

    Rewrite [33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py] File in: 2018-04-14 13:57:33

--- pyton/33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py
+++ pyton/33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py
@@ -1,75 +1,56 @@
 
         
-        
-@keras_test
-def test_TimeDistributed_trainable():
-    # test layers that need learning_phase to be set
-    x = Input(shape=(3, 2))
-    layer = wrappers.TimeDistributed(layers.BatchNormalization())
-    _ = layer(x)
-    assert len(layer.updates) == 2
-    assert len(layer.trainable_weights) == 2
-    layer.trainable = False
-    assert len(layer.updates) == 0
-    assert len(layer.trainable_weights) == 0
-    layer.trainable = True
-    assert len(layer.updates) == 2
-    assert len(layer.trainable_weights) == 2
+            if (has_actual_code and not has_question_marks):
+        linebuffer = clean_trailing_newlines(linebuffer)
+        write_with_harness(codefile, sourcefile, start_linenum, linebuffer)
+    return (line, linenum)
     
-        cbks = [
-        callbacks.ReduceLROnPlateau(
-            monitor='val_loss',
-            factor=0.5,
-            patience=4,
-            verbose=1),
-        callbacks.TensorBoard(
-            log_dir=filepath)]
+        def add_done_callback(self, fn):
+        '''Attaches a callable that will be called when the future finishes.
     
-        # Arguments
-        data: Indexable generator (such as list or Numpy array)
-            containing consecutive data points (timesteps).
-            The data should be at 2D, and axis 0 is expected
-            to be the time dimension.
-        targets: Targets corresponding to timesteps in `data`.
-            It should have same length as `data`.
-        length: Length of the output sequences (in number of timesteps).
-        sampling_rate: Period between successive individual timesteps
-            within sequences. For rate `r`, timesteps
-            `data[i]`, `data[i-r]`, ... `data[i - length]`
-            are used for create a sample sequence.
-        stride: Period between successive output sequences.
-            For stride `s`, consecutive output samples would
-            be centered around `data[i]`, `data[i+s]`, `data[i+2*s]`, etc.
-        start_index, end_index: Data points earlier than `start_index`
-            or later than `end_index` will not be used in the output sequences.
-            This is useful to reserve part of the data for test or validation.
-        shuffle: Whether to shuffle output samples,
-            or instead draw them in chronological order.
-        reverse: Boolean: if `true`, timesteps in each output sample will be
-            in reverse chronological order.
-        batch_size: Number of timeseries samples in each batch
-            (except maybe the last one).
+    # If true, an OpenSearch description file will be output, and all pages will
+# contain a <link> tag referring to it.  The value of this option must be the
+# base URL from which the finished HTML is served.
+#html_use_opensearch = ''
+    
+    if __name__ == '__main__':
+    main()
+
     
     
-@keras_test
-def test_temporal_classification():
-    '''
-    Classify temporal sequences of float numbers
-    of length 3 into 2 classes using
-    single layer of GRU units and softmax applied
-    to the last activations of the units
-    '''
-    np.random.seed(1337)
-    (x_train, y_train), (x_test, y_test) = get_test_data(num_train=200,
-                                                         num_test=20,
-                                                         input_shape=(3, 4),
-                                                         classification=True,
-                                                         num_classes=2)
-    y_train = to_categorical(y_train)
-    y_test = to_categorical(y_test)
+  def Start( self ):
+    request_data = BuildRequestData()
+    if self._extra_data:
+      request_data.update( self._extra_data )
+    with HandleServerException( display = False ):
+      self._response = self.PostDataToHandler( request_data, 'debug_info' )
     
-            #Creating directory title page for current directory
-        with open('dir_title.html', 'w') as os_html:
-            os_html.write(header + dir_title)
-        
-        group.append(HTML('dir_title.html').render())
+    
+def FormatDebugInfoResponse_NoExtraConf_test():
+  response = deepcopy( GENERIC_RESPONSE )
+  response[ 'extra_conf' ].update( {
+    'is_loaded': False,
+    'path': None
+  } )
+  assert_that(
+    FormatDebugInfoResponse( response ),
+    contains_string(
+      'No extra configuration file found\n'
+    )
+  )
+    
+    
+def KeywordsFromSyntaxListOutput_TypeHierarchy_test():
+  assert_that( syntax_parse._KeywordsFromSyntaxListOutput( '''
+baa xxx foo bar
+        links to Foo
+Foo xxx zoo goo
+        links to Bar
+Bar xxx qux moo
+        links to Type''' ),
+              contains_inanyorder( 'foo', 'bar', 'zoo', 'goo', 'qux', 'moo' ) )
+    
+    from ycm.client.base_request import BaseRequest
+from ycm.youcompleteme import YouCompleteMe
+from ycmd import user_options_store
+from ycmd.utils import CloseStandardStreams, WaitUntilProcessIsTerminated
