299ce86db2aea60e26f57363dad116691eced500
SUNsung <weatherius@gmail.com>
2018-04-14 04:28:50 +0300

    Rewrite [25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py] File in: 2018-04-14 04:28:50

--- pyton/25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py
+++ pyton/25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py
@@ -1,230 +1,150 @@
 
         
-        versions_info['signature'] = signature
-with open('update/versions.json', 'w') as versionsf:
-    json.dump(versions_info, versionsf, indent=4, sort_keys=True)
-
+          def setUp(self):
+    FLAGS.train_data = os.path.join(self.get_temp_dir() + 'test-text.txt')
+    FLAGS.eval_data = os.path.join(self.get_temp_dir() + 'eval-text.txt')
+    FLAGS.save_path = self.get_temp_dir()
+    with open(FLAGS.train_data, 'w') as f:
+      f.write(
+          '''alice was beginning to get very tired of sitting by her sister on
+          the bank, and of having nothing to do: once or twice she had peeped
+          into the book her sister was reading, but it had no pictures or
+          conversations in it, 'and what is the use of a book,' thought alice
+          'without pictures or conversations?' So she was considering in her own
+          mind (as well as she could, for the hot day made her feel very sleepy
+          and stupid), whether the pleasure of making a daisy-chain would be
+          worth the trouble of getting up and picking the daisies, when suddenly
+          a White rabbit with pink eyes ran close by her.\n''')
+      with open(FLAGS.eval_data, 'w') as f:
+        f.write('alice she rabbit once\n')
+    
+    from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+    
+        # Write
+    lm_seq_ser = lm_seq.seq.SerializeToString()
+    seq_ae_seq_ser = seq_ae_seq.seq.SerializeToString()
+    writer_lm_all.write(lm_seq_ser)
+    writer_seq_ae_all.write(seq_ae_seq_ser)
+    if not doc.is_validation:
+      writer_lm.write(lm_seq_ser)
+      writer_rev_lm.write(rev_lm_seq.seq.SerializeToString())
+      writer_seq_ae.write(seq_ae_seq_ser)
     
     
-def main():
-    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
-    options, args = parser.parse_args()
-    if len(args) != 2:
-        parser.error('Expected an input and an output filename')
-    
-    
-for testfile in os.listdir(TEST_DIR):
-    _make_testfunc(testfile)
-    
-    
-class RtspFD(FileDownloader):
-    def real_download(self, filename, info_dict):
-        url = info_dict['url']
-        self.report_destination(filename)
-        tmpfilename = self.temp_name(filename)
-    
-    
-class C56IE(InfoExtractor):
-    _VALID_URL = r'https?://(?:(?:www|player)\.)?56\.com/(?:.+?/)?(?:v_|(?:play_album.+-))(?P<textid>.+?)\.(?:html|swf)'
-    IE_NAME = '56.com'
-    _TESTS = [{
-        'url': 'http://www.56.com/u39/v_OTM0NDA3MTY.html',
-        'md5': 'e59995ac63d0457783ea05f93f12a866',
-        'info_dict': {
-            'id': '93440716',
-            'ext': 'flv',
-            'title': '网事知多少 第32期：车怒',
-            'duration': 283.813,
-        },
-    }, {
-        'url': 'http://www.56.com/u47/v_MTM5NjQ5ODc2.html',
-        'md5': '',
-        'info_dict': {
-            'id': '82247482',
-            'title': '爱的诅咒之杜鹃花开',
-        },
-        'playlist_count': 7,
-        'add_ie': ['Sohu'],
-    }]
-    
-            uploader = self._search_regex(
-            r'Added by\s*:\s*<a[^>]+>([^<]+)</a>', webpage, 'uploader', default=None)
-        upload_date = unified_strdate(self._search_regex(
-            r'Added on\s*:\s*([\d-]+)', webpage, 'upload date', default=None))
-    
-    import os
-    
-        @g_connect
-    def get_list(self, what):
-        '''
-        Fetch the list of items specified.
-        '''
-        try:
-            url = '%s/%s/?page_size' % (self.baseurl, what)
-            data = self.__call_galaxy(url)
-            if 'results' in data:
-                results = data['results']
-            else:
-                results = data
-            done = True
-            if 'next' in data:
-                done = (data.get('next_link', None) is None)
-            while not done:
-                url = '%s%s' % (self._api_server, data['next_link'])
-                data = self.__call_galaxy(url)
-                results += data['results']
-                done = (data.get('next_link', None) is None)
-            return results
-        except Exception as error:
-            raise AnsibleError('Failed to download the %s list: %s' % (what, str(error)))
-    
-            input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/targetHttpProxies/mytargetproxy'
-        actual = GCPUtils.parse_gcp_url(input_url)
-        self.assertEquals('compute', actual['service'])
-        self.assertEquals('v1', actual['api_version'])
-        self.assertEquals('myproject', actual['project'])
-        self.assertFalse('global' in actual)
-        self.assertEquals('targetHttpProxies', actual['resource_name'])
-        self.assertEquals('mytargetproxy', actual['entity_name'])
-    
-        def on_open_shell(self):
-        try:
-            self._exec_cli_command('screen-length 0 temporary')
-        except AnsibleConnectionFailure:
-            raise AnsibleConnectionFailure('unable to set terminal parameters')
-
-    
-        # Arguments
-        yaml_string: YAML string encoding a model configuration.
-        custom_objects: Optional dictionary mapping names
-            (strings) to custom classes or functions to be
-            considered during deserialization.
-    
-        for data_format in ['channels_last', 'channels_first']:
-        for shape in [(4, 5), (4, 5, 6)]:
-            if data_format == 'channels_last':
-                input_shape = (2,) + shape + (3,)
-            else:
-                input_shape = (2, 3) + shape
-            layer_test(layers.SpatialDropout2D if len(shape) == 2 else layers.SpatialDropout3D,
-                       kwargs={'rate': 0.5,
-                               'data_format': data_format},
-                       input_shape=input_shape)
-    
-        # References
-        - [Convolutional LSTM Network: A Machine Learning Approach for
-        Precipitation Nowcasting](http://arxiv.org/abs/1506.04214v1)
-        The current implementation does not include the feedback loop on the
-        cells output
-    '''
-    
-    
-def _runner(init, shape, target_mean=None, target_std=None,
-            target_max=None, target_min=None):
-    variable = K.variable(init(shape))
-    output = K.get_value(variable)
-    lim = 3e-2
-    if target_std is not None:
-        assert abs(output.std() - target_std) < lim
-    if target_mean is not None:
-        assert abs(output.mean() - target_mean) < lim
-    if target_max is not None:
-        assert abs(output.max() - target_max) < lim
-    if target_min is not None:
-        assert abs(output.min() - target_min) < lim
-    
-    - With default word, sentence, and query vector sizes, the GRU model achieves:
-  - 100% test accuracy on QA1 in 20 epochs (2 seconds per epoch on CPU)
-  - 50% test accuracy on QA2 in 20 epochs (16 seconds per epoch on CPU)
-In comparison, the Facebook paper achieves 50% and 20% for the LSTM baseline.
-    
-    # Vectorize the data.
-input_texts = []
-target_texts = []
-input_characters = set()
-target_characters = set()
-with open(data_path, 'r', encoding='utf-8') as f:
-    lines = f.read().split('\n')
-for line in lines[: min(num_samples, len(lines) - 1)]:
-    input_text, target_text = line.split('\t')
-    # We use 'tab' as the 'start sequence' character
-    # for the targets, and '\n' as 'end sequence' character.
-    target_text = '\t' + target_text + '\n'
-    input_texts.append(input_text)
-    target_texts.append(target_text)
-    for char in input_text:
-        if char not in input_characters:
-            input_characters.add(char)
-    for char in target_text:
-        if char not in target_characters:
-            target_characters.add(char)
-    
-    
-def build_discriminator():
-    # build a relatively standard conv net, with LeakyReLUs as suggested in
-    # the reference paper
-    cnn = Sequential()
-    
-        ax.set_xlabel('ratio of n_sample / n_population')
-    ax.set_ylabel('Time (s)')
-    ax.legend()
-    
-    Shows how shrinkage improves classification.
-'''
-    
-        When fixed_n_classes is not None the first labeling is considered a ground
-    truth class assignment with fixed number of classes.
-    '''
-    random_labels = np.random.RandomState(seed).randint
-    scores = np.zeros((len(n_clusters_range), n_runs))
-    
-        def current_year_month(self):
-        '''Return the current year and month.'''
-        ...
-    
-            (category1, 1), product4
-        (category1, 2), product1
-        (category1, 3), product2
-        (category2, 3), product1
-        (category2, 7), product3
-        '''
-        category, product_id = key
-        quantity = value
-        yield (category, quantity), product_id
+def _build_random_sequence(vocab_ids):
+  seq_len = random.randint(10, 200)
+  ids = vocab_ids.values()
+  seq = data.SequenceWrapper()
+  for token_id in [random.choice(ids) for _ in range(seq_len)]:
+    seq.add_timestep().set_token(token_id)
+  return seq
     
     
 if __name__ == '__main__':
-    RemoveDuplicateUrls.run()
+  tf.app.run()
 
     
-        def message_user(self, friend_id, message):
-        pass
+        # Loads mapping from string UID to integer node ID.
+    node_id_to_uid = {}
+    proto_as_ascii = tf.gfile.GFile(label_lookup_path).readlines()
+    for line in proto_as_ascii:
+      if line.startswith('  target_class:'):
+        target_class = int(line.split(': ')[1])
+      if line.startswith('  target_class_string:'):
+        target_class_string = line.split(': ')[1]
+        node_id_to_uid[target_class] = target_class_string[1:-2]
     
-        def crawl_page(self, page):
-        for url in page.child_urls:
-            self.data_store.add_link_to_crawl(url)
-        self.reverse_index_queue.generate(page)
-        self.doc_index_queue.generate(page)
-        self.data_store.remove_link_to_crawl(page.url)
-        self.data_store.insert_crawled_link(page.url, page.signature)
+        ```python
+        model = Sequential()
+        model.add(Permute((2, 1), input_shape=(10, 64)))
+        # now: model.output_shape == (None, 64, 10)
+        # note: `None` is the batch dimension
+    ```
     
         @property
-    @abstractmethod
-    def value(self):
-        pass
+    def use_bias(self):
+        return self.cell.use_bias
     
-        def get_people(self, ids):
-        results = []
-        for id in ids:
-            if id in self.people:
-                results.append(self.people[id])
-        return results
+    # this will contain our generated image
+if K.image_data_format() == 'channels_first':
+    combination_image = K.placeholder((1, 3, img_nrows, img_ncols))
+else:
+    combination_image = K.placeholder((1, img_nrows, img_ncols, 3))
     
-    api('organiclisting',       OrganicListingJsonTemplate)
-api('subreddittraffic', TrafficJsonTemplate)
-api('takedownpane', TakedownJsonTemplate)
-api('policyview', PolicyViewJsonTemplate)
+            # Test equivalence of convert_dense_weights_data_format
+        out1 = model1.predict(x)
+        layer_utils.convert_dense_weights_data_format(model1.layers[2], prev_shape, target_data_format)
+        for (src, dst) in zip(model1.layers, model2.layers):
+            dst.set_weights(src.get_weights())
+        out2 = model2.predict(transpose(x))
     
-    from pylons import request
-from pylons import tmpl_context as c
-from pylons import app_globals as g
+    question = layers.Input(shape=(query_maxlen,), dtype='int32')
+encoded_question = layers.Embedding(vocab_size, EMBED_HIDDEN_SIZE)(question)
+encoded_question = layers.Dropout(0.3)(encoded_question)
+encoded_question = RNN(EMBED_HIDDEN_SIZE)(encoded_question)
+encoded_question = layers.RepeatVector(story_maxlen)(encoded_question)
+    
+        print('int:')
+    print('calculation time: %fs' % (end_calc-start_calc))
+    print('conversion time: %fs\n\n' % (end_conv-start_conv))
+    
+    >>> from nntplib import NNTP
+>>> s = NNTP('news')
+>>> resp, count, first, last, name = s.group('comp.lang.python')
+>>> print('Group', name, 'has', count, 'articles, range', first, 'to', last)
+Group comp.lang.python has 51 articles, range 5770 to 5821
+>>> resp, subs = s.xhdr('subject', '{0}-{1}'.format(first, last))
+>>> resp = s.quit()
+>>>
+    
+        print('ADDN_LINK_FILES=', end=' ')
+    for addn in vars['addn_link']: print(''%s'' % (addn), end=' ')
+    print() ; print()
+    
+        def _create_infile(self):
+        infile = support.TESTFN
+        with open(infile, 'w') as fp:
+            self.addCleanup(os.remove, infile)
+            fp.write(self.data)
+        return infile
+    
+            self.assertEqual(x[:], a.tolist()[1:])
+        with self.assertRaises(ValueError):
+            c_int.from_buffer(a, -1)
+        with self.assertRaises(ValueError):
+            (c_int * 16).from_buffer(a, sizeof(c_int))
+        with self.assertRaises(ValueError):
+            (c_int * 1).from_buffer(a, 16 * sizeof(c_int))
+    
+            # This is no longer true, now that WINFUNCTYPE caches created types internally.
+        ## # CFuncPtr subclasses are compared by identity, so this raises a TypeError:
+        ## self.assertRaises(TypeError, setattr, wndclass,
+        ##                  'lpfnWndProc', WNDPROC_2(wndproc))
+        # instead:
+    
+            # to not leak references, we must clean _pointer_type_cache
+        from ctypes import _pointer_type_cache
+        del _pointer_type_cache[id(P)]
+    
+            total_count = 0
+        translated_count = 0
+        with open(fn) as f:
+            catalog = read_po(f)
+            for msg in catalog:
+                total_count += 1
+                if is_translated(msg):
+                    translated_count += 1
+        pct = translated_count / float(total_count) * 100
+        click.echo('% -7s % 2d%%' % (
+            locale,
+            pct,
+        ), err=True)
+        if pct >= MINIMUM and locale not in rv:
+            rv.append(locale)
+    with open(catalog_file, 'w') as f:
+        json.dump({
+            'supported_locales': sorted(rv)
+        }, f, indent=2)
+        f.write('\n')
