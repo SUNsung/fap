bd1b2cc36cb6494cc7ce4f67bd799d0de958bfc1
SUNsung <weatherius@gmail.com>
2018-04-14 21:43:54 +0300

    Rewrite [35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py] File in: 2018-04-14 21:43:53

--- pyton/35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py
+++ pyton/35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py
@@ -1,165 +1,158 @@
 
         
-        
-def get_info(package_name):
-    api_url = 'https://pypi.python.org/pypi/{}/json'.format(package_name)
-    resp = requests.get(api_url).json()
-    hasher = hashlib.sha256()
-    for release in resp['urls']:
-        download_url = release['url']
-        if download_url.endswith('.tar.gz'):
-            hasher.update(requests.get(download_url).content)
-            return {
-                'name': package_name,
-                'url': download_url,
-                'sha256': hasher.hexdigest(),
-            }
-    else:
-        raise RuntimeError(
-            '{}: download not found: {}'.format(package_name, resp))
-    
-    
-@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
-@pytest.mark.parametrize('filename', filenames)
-def test_rst_file_syntax(filename):
-    p = subprocess.Popen(
-        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE
-    )
-    err = p.communicate()[1]
-    assert p.returncode == 0, err.decode('utf8')
+        UNICODE = FILE_CONTENT
 
     
-        exc = Timeout('Request timed out')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR_TIMEOUT
-    assert error_msg == 'Request timed out (30s).'
-
-    
-    
-def test_max_redirects(httpbin):
-    r = http('--max-redirects=1', '--follow', httpbin.url + '/redirect/3',
-             error_exit_ok=True)
-    assert r.exit_status == ExitStatus.ERROR_TOO_MANY_REDIRECTS
-
-    
-        include_debug_info = '--debug' in args
-    include_traceback = include_debug_info or '--traceback' in args
-    
-    
-class HTTPBasicAuth(requests.auth.HTTPBasicAuth):
-    
-        # Adapters
-    def get_transport_plugins(self):
-        return [plugin for plugin in self
-                if issubclass(plugin, TransportPlugin)]
-
-    
-    
-def get_line_value(r, n):
-    rls = r.split('\r\n')
-    if len(rls) < n + 1:
-        return None
-    
-            raise NotImplementedError
-    
-    def setChannel(self, channel):
-        '''@brief Set the channel of the token
-    
-    Here is a little overview over the most commonly used classes provided by
-this runtime:
-    
-        def test_open_and_close(self):
-        # open calls create_widgets, which needs default_command
-        self.dialog.default_command = None
-    
-            entrylabel.grid(column=0, row=0, columnspan=3, padx=5, sticky=W)
-        self.entry.grid(column=0, row=1, columnspan=3, padx=5, sticky=W+E,
-                        pady=[10,0])
-        self.entry_error.grid(column=0, row=2, columnspan=3, padx=5,
-                              sticky=W+E)
-        self.button_ok.grid(column=1, row=99, padx=5)
-        self.button_cancel.grid(column=2, row=99, padx=5)
-    
-        >>> harmonic_mean([2.5, 3, 10])  # For an equal investment portfolio.
-    3.6
-    
-        def read(self, size=-1):
-        '''Read at most size bytes from the chunk.
-        If size is omitted or negative, read until the end
-        of the chunk.
-        '''
-    
-    class FormatError(Error):
-    '''A file appears to have an invalid format.'''
-
-    
-            # determine the encoding if the transport provided it
-        source.encoding = self._guess_media_encoding(source)
-    
-        def test_varargs14_kw(self):
-        msg = r'^product\(\) takes at most 1 keyword argument \(2 given\)$'
-        self.assertRaisesRegex(TypeError, msg,
-                               itertools.product, 0, repeat=1, foo=2)
-    
-            # if we predefine some constants, then eval(repr(z)) should
-        # also work, except that it might change the sign of zeros
-        inf, nan = float('inf'), float('nan')
-        infj, nanj = complex(0.0, inf), complex(0.0, nan)
-        for x in vals:
-            for y in vals:
-                z = complex(x, y)
-                roundtrip = eval(repr(z))
-                # adding 0.0 has no effect beside changing -0.0 to 0.0
-                self.assertFloatsAreIdentical(0.0 + z.real,
-                                              0.0 + roundtrip.real)
-                self.assertFloatsAreIdentical(0.0 + z.imag,
-                                              0.0 + roundtrip.imag)
-    
-        def test_comparison(self):
-        test_ranges = [range(0), range(0, -1), range(1, 1, 3),
-                       range(1), range(5, 6), range(5, 6, 2),
-                       range(5, 7, 2), range(2), range(0, 4, 2),
-                       range(0, 5, 2), range(0, 6, 2)]
-        test_tuples = list(map(tuple, test_ranges))
-    
-    
-        f = wndclass.lpfnWndProc
-    
-    
-class Student(AbstractExpert):
-    
-    
-class TestSubject(unittest.TestCase):
-    
-    '''
-Port of the Java example of 'Setter Injection' in
-'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
-(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
-http://xunitpatterns.com/Dependency%20Injection.html.
-    
-        def now(self):
-        current_time = datetime.datetime.now()
-        current_time_formatted = '{}:{}'.format(current_time.hour, current_time.minute)
-        return current_time_formatted
-    
-    
-@coroutine
-def coroutine2(target):
-    while True:
-        request = yield
-        if 10 < request <= 20:
-            print('request {} handled in coroutine 2'.format(request))
+        def _migrate_implicit_content_type(self):
+        '''Migrate the removed implicit_content_type config option'''
+        try:
+            implicit_content_type = self.pop('implicit_content_type')
+        except KeyError:
+            self.save()
         else:
-            target.send(request)
+            if implicit_content_type == 'form':
+                self['default_options'].insert(0, '--form')
+            self.save()
+            self.load()
+
     
-        # verify that none of the target files exist
-    assert(not lexists('foo.txt'))
-    assert(not lexists('bar.txt'))
-    assert(not lexists('baz.txt'))
-    try:
-        with open('foo.txt', 'w'):  # Creating the file
+    '''TensorFlow utilities for unit tests.'''
+    
+    
+def make_estimator():
+  data_format = 'channels_last'
+  if tf.test.is_built_with_cuda():
+    data_format = 'channels_first'
+  return tf.estimator.Estimator(
+      model_fn=mnist.model_fn, params={
+          'data_format': data_format
+      })
+    
+      # Instantiate the model.
+  with tf.Graph().as_default():
+    with tf.variable_scope('lexnet'):
+      instance = tf.placeholder(dtype=tf.string)
+      model = path_model.PathBasedModel(
+          hparams, lemma_embeddings, instance)
+    
+      Returns:
+    data: Numpy array.
+  '''
+  with tf.gfile.FastGFile(file_path, 'rb') as f:
+    return ParseFromString(f.read())
+    
+        # not a test, but looks like a test
+    'scrapy/utils/testsite.py',
+    
+    
+class _BenchSpider(scrapy.Spider):
+    '''A spider that follows all links'''
+    name = 'follow'
+    total = 10000
+    show = 20
+    baseurl = 'http://localhost:8998'
+    link_extractor = LinkExtractor()
+    
+            try:
+            spidercls = self.crawler_process.spider_loader.load(name)
+        except KeyError:
             pass
+        else:
+            # if spider already exists and not --force then halt
+            if not opts.force:
+                print('Spider %r already exists in module:' % name)
+                print('  %s' % spidercls.__module__)
+                return
+        template_file = self._find_template(opts.template)
+        if template_file:
+            self._genspider(module, name, domain, opts.template, template_file)
+            if opts.edit:
+                self.exitcode = os.system('scrapy edit '%s'' % name)
+    
+    
+def SetUpSystemPaths():
+  sys.path.insert( 0, os.path.join( DIR_OF_YCMD ) )
+    
+        def _adjust_process_count(self):
+        for _ in range(len(self._processes), self._max_workers):
+            p = multiprocessing.Process(
+                    target=_process_worker,
+                    args=(self._call_queue,
+                          self._result_queue))
+            p.start()
+            self._processes.add(p)
+    
+            for future in as_completed(future_to_url):
+            try:
+                url_to_content[future_to_url[future]] = future.result()
+            except:
+                pass
+        return url_to_content
+    finally:
+        executor.shutdown()
+    
+    
+  @staticmethod
+  def _ExtraHeaders( method, request_uri, request_body = None ):
+    if not request_body:
+      request_body = bytes( b'' )
+    headers = dict( _HEADERS )
+    headers[ _HMAC_HEADER ] = b64encode(
+        CreateRequestHmac( ToBytes( method ),
+                           ToBytes( urlparse( request_uri ).path ),
+                           request_body,
+                           BaseRequest.hmac_secret ) )
+    return headers
+    
+    
+class DebugInfoRequest( BaseRequest ):
+  def __init__( self, extra_data = None ):
+    super( DebugInfoRequest, self ).__init__()
+    self._extra_data = extra_data
+    self._response = None
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+      _assert_rejects( f, { 'text' : 'This is an unimportant taco',
+                        'kind' : 'WARNING' } )
+  _assert_rejects( f, { 'text' : 'This taco will NOT be shown',
+                        'kind' : 'ERROR' } )
+  _assert_accepts( f, { 'text' : 'This burrito WILL be shown',
+                        'kind' : 'ERROR' } )
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+        @YouCompleteMeInstance( { 'log_level': 'debug',
+                              'keep_logfiles': 1 } )
+    def Debug_test( ycm ):
+        ...
+  '''
+  def Decorator( test ):
+    @functools.wraps( test )
+    def Wrapper( *args, **kwargs ):
+      ycm = YouCompleteMe( MakeUserOptions( custom_options ) )
+      WaitUntilReady()
+      ycm.CheckIfServerIsReady()
+      try:
+        test( ycm, *args, **kwargs )
+      finally:
+        StopServer( ycm )
+    return Wrapper
+  return Decorator
