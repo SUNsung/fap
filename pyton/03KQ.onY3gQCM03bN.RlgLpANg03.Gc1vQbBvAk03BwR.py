60c7c63a42f7db00ac1fe6104e26d8f2e2c2f605
SUNsung <weatherius@gmail.com>
2018-04-15 23:46:01 +0300

    Rewrite [03KQ.onY3gQCM03bN.RlgLpANg03.Gc1vQbBvAk03BwR.py] File in: 2018-04-15 23:46:00

--- pyton/03KQ.onY3gQCM03bN.RlgLpANg03.Gc1vQbBvAk03BwR.py
+++ pyton/03KQ.onY3gQCM03bN.RlgLpANg03.Gc1vQbBvAk03BwR.py
@@ -1,145 +1,65 @@
 
         
-        # build some special values
-special = [0, 1, 2, BASE, BASE >> 1, 0x5555555555555555, 0xaaaaaaaaaaaaaaaa]
-#  some solid strings of one bits
-p2 = 4  # 0 and 1 already added
-for i in range(2*SHIFT):
-    special.append(p2 - 1)
-    p2 = p2 << 1
-del p2
-# add complements & negations
-special += [~x for x in special] + [-x for x in special]
-    
-        from idlelib.idle_test.htest import run
-    run(Query, HelpSource)
-
-    
-    __test__ = {'libreftest' : libreftest}
-    
-        @classmethod
-    def zonelist(cls, zonedir='/usr/share/zoneinfo'):
-        zones = []
-        for root, _, files in os.walk(zonedir):
-            for f in files:
-                p = os.path.join(root, f)
-                with open(p, 'rb') as o:
-                    magic =  o.read(4)
-                if magic == b'TZif':
-                    zones.append(p[len(zonedir) + 1:])
-        return zones
-    
-    make_scanner = c_make_scanner or py_make_scanner
-
-    
-        def remove_flag(self, flag):
-        '''Unset the given string flag(s) without changing others.'''
-        if 'Status' in self or 'X-Status' in self:
-            self.set_flags(''.join(set(self.get_flags()) - set(flag)))
+            cmd = cmd.split()
     
     
-
-# format is a tuple (RE, SCANLINES, CLASS) where RE is a compiled regular
-# expression, SCANLINES is the number of header lines to scan, and CLASS is
-# the class to instantiate if a match is found
-    
-            class A(POINTER(c_ulong)):
-            pass
-    
-        # For pickling to work, the __module__ variable needs to be set to the frame
-    # where the named tuple is created.  Bypass this step in enviroments where
-    # sys._getframe is not defined (Jython for example).
-    if hasattr(_sys, '_getframe'):
-        result.__module__ = _sys._getframe(1).f_globals.get('__name__', '__main__')
-    
-                self._work_queue.put(w)
-            self._adjust_thread_count()
-            return f
-    submit.__doc__ = _base.Executor.submit.__doc__
+@pytest.fixture(params=containers)
+def proc(request, spawnu, TIMEOUT):
+    proc = spawnu(*request.param)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(u'tcsh')
+    proc.sendline(u'setenv PYTHONIOENCODING utf8')
+    proc.sendline(u'eval `thefuck --alias`')
+    return proc
     
     
-def FormatDebugInfoResponse_Completer_ServerNotRunningWithNoLogfiles_test():
-  response = deepcopy( GENERIC_RESPONSE )
-  response[ 'completer' ][ 'servers' ][ 0 ].update( {
-    'is_running': False,
-    'logfiles': []
-  } )
-  assert_that(
-    FormatDebugInfoResponse( response ),
-    contains_string(
-      'Completer name completer debug information:\n'
-      '  Server name not running\n'
-      '  Server name executable: /path/to/executable\n'
-      '  No logfiles available\n'
-      '  Server name key: value\n'
-      '  Key: value\n'
-    )
-  )
-
+collect_ignore = [
+    # deprecated or moved modules
+    'scrapy/conf.py',
+    'scrapy/stats.py',
+    'scrapy/project.py',
+    'scrapy/utils/decorator.py',
+    'scrapy/statscol.py',
+    'scrapy/squeue.py',
+    'scrapy/log.py',
+    'scrapy/dupefilter.py',
+    'scrapy/command.py',
+    'scrapy/linkextractor.py',
+    'scrapy/spider.py',
     
+        def render(self, request):
+        now = time()
+        delta = now - self.lasttime
     
-def KeywordsFromSyntaxListOutput_PythonSyntax_test():
-  expected_keywords = (
-    'bytearray', 'IndexError', 'all', 'help', 'vars', 'SyntaxError', 'global',
-    'elif', 'unicode', 'sorted', 'memoryview', 'isinstance', 'except',
-    'nonlocal', 'NameError', 'finally', 'BytesWarning', 'dict', 'IOError',
-    'pass', 'oct', 'bin', 'SystemExit', 'return', 'StandardError', 'format',
-    'TabError', 'break', 'next', 'not', 'UnicodeDecodeError', 'False',
-    'RuntimeWarning', 'list', 'iter', 'try', 'reload', 'Warning', 'round',
-    'dir', 'cmp', 'set', 'bytes', 'UnicodeTranslateError', 'intern',
-    'issubclass', 'yield', 'Ellipsis', 'hash', 'locals', 'BufferError',
-    'slice', 'for', 'FloatingPointError', 'sum', 'VMSError', 'getattr', 'abs',
-    'print', 'import', 'True', 'FutureWarning', 'ImportWarning', 'None',
-    'EOFError', 'len', 'frozenset', 'ord', 'super', 'raise', 'TypeError',
-    'KeyboardInterrupt', 'UserWarning', 'filter', 'range', 'staticmethod',
-    'SystemError', 'or', 'BaseException', 'pow', 'RuntimeError', 'float',
-    'MemoryError', 'StopIteration', 'globals', 'divmod', 'enumerate', 'apply',
-    'LookupError', 'open', 'basestring', 'from', 'UnicodeError', 'zip', 'hex',
-    'long', 'IndentationError', 'int', 'chr', '__import__', 'type',
-    'Exception', 'continue', 'tuple', 'reduce', 'reversed', 'else', 'assert',
-    'UnicodeEncodeError', 'input', 'with', 'hasattr', 'delattr', 'setattr',
-    'raw_input', 'PendingDeprecationWarning', 'compile', 'ArithmeticError',
-    'while', 'del', 'str', 'property', 'def', 'and', 'GeneratorExit',
-    'ImportError', 'xrange', 'is', 'EnvironmentError', 'KeyError', 'coerce',
-    'SyntaxWarning', 'file', 'in', 'unichr', 'ascii', 'any', 'as', 'if',
-    'OSError', 'DeprecationWarning', 'min', 'UnicodeWarning', 'execfile', 'id',
-    'complex', 'bool', 'ValueError', 'NotImplemented', 'map', 'exec', 'buffer',
-    'max', 'class', 'object', 'repr', 'callable', 'ZeroDivisionError', 'eval',
-    '__debug__', 'ReferenceError', 'AssertionError', 'classmethod',
-    'UnboundLocalError', 'NotImplementedError', 'lambda', 'AttributeError',
-    'OverflowError', 'WindowsError' )
+        def set_crawler(self, crawler):
+        assert not hasattr(self, '_crawler'), 'crawler already set'
+        self._crawler = crawler
     
-    import tornado.httpserver
-import tornado.ioloop
-import tornado.options
-import tornado.web
+        def run(self, args, opts):
+        with _BenchServer():
+            self.crawler_process.crawl(_BenchSpider, total=100000)
+            self.crawler_process.start()
     
-    from tornado.escape import json_decode
-from tornado.test.httpserver_test import TypeCheckHandler
-from tornado.testing import AsyncHTTPTestCase
-from tornado.web import RequestHandler, Application
-from tornado.wsgi import WSGIApplication, WSGIContainer, WSGIAdapter
+            # start checks
+        if opts.list:
+            for spider, methods in sorted(contract_reqs.items()):
+                if not methods and not opts.verbose:
+                    continue
+                print(spider)
+                for method in sorted(methods):
+                    print('  * %s' % method)
+        else:
+            start = time.time()
+            self.crawler_process.start()
+            stop = time.time()
     
-        def fileno(self):
-        return self.reader.fileno()
+        The name for each file chosen consists os the section id in the markdown document, a counter for the snippet inside the section.
     
-            @gen.coroutine
-        def connect(c):
-            try:
-                yield c.connect(server_addr)
-            except EnvironmentError:
-                pass
-            else:
-                connected_clients.append(c)
-    
-        def call_wrapped(self, count):
-        '''Wraps and calls a function at each level of stack depth
-        to measure the overhead of the wrapped function.
-        '''
-        # This queue is analogous to IOLoop.add_callback, but lets us
-        # benchmark the stack_context in isolation without system call
-        # overhead.
-        queue = collections.deque()
-        self.call_wrapped_inner(queue, count)
-        while queue:
-            queue.popleft()()
+        for filename in filenames:
+        fd = codecs.open(filename, mode='r', encoding='utf-8')
+        for line in fd.readlines():
+            refs = re.findall(r'(?<=<a href=')[^']*', markdown.markdown(line))
+            for ref in refs:
+                if ref not in urls:
+                    urls.append(ref)
