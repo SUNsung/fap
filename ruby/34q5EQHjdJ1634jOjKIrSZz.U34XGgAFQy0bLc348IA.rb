b6d265e4a7d5ccaae38e45292d5d2202abaf7572
SUNsung <weatherius@gmail.com>
2018-04-15 14:13:47 +0300

    Rewrite [34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb] File in: 2018-04-15 14:13:46

--- ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
+++ ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
@@ -1,38 +1,77 @@
 
         
-          def media_attachments
-    { accept_content_types: MediaAttachment::IMAGE_FILE_EXTENSIONS + MediaAttachment::VIDEO_FILE_EXTENSIONS + MediaAttachment::IMAGE_MIME_TYPES + MediaAttachment::VIDEO_MIME_TYPES }
+          def process_status_params
+    {
+      uri: @object['id'],
+      url: object_url || @object['id'],
+      account: @account,
+      text: text_from_content || '',
+      language: detected_language,
+      spoiler_text: @object['summary'] || '',
+      created_at: @options[:override_timestamps] ? nil : @object['published'],
+      reply: @object['inReplyTo'].present?,
+      sensitive: @object['sensitive'] || false,
+      visibility: visibility_from_audience,
+      thread: replied_to_status,
+      conversation: conversation_from_uri(@object['conversation']),
+      media_attachment_ids: process_attachments.take(4).map(&:id),
+    }
   end
-end
-
     
-              env['S3_ENDPOINT'] = prompt.ask('Minio endpoint URL:') do |q|
-            q.required true
-            q.modify :strip
-          end
-    
-      def unsilence?
-    staff?
-  end
-    
-      it 'renders admin layout as an admin' do
-    routes.draw { get 'success' => 'admin/base#success' }
-    sign_in(Fabricate(:user, admin: true))
-    get :success
-    expect(response).to render_template layout: 'admin'
-  end
-end
-
-    
-            def initialize(argv)
-          @pod_name = argv.shift_argument
-          @wipe_all = argv.flag?('all')
-          super
+    namespace :db do
+  namespace :migrate do
+    desc 'Setup the db or migrate depending on state of db'
+    task setup: :environment do
+      begin
+        if ActiveRecord::Migrator.current_version.zero?
+          Rake::Task['db:migrate'].invoke
+          Rake::Task['db:seed'].invoke
         end
+      rescue ActiveRecord::NoDatabaseError
+        Rake::Task['db:setup'].invoke
+      else
+        Rake::Task['db:migrate'].invoke
+      end
+    end
+  end
     
-            self.description = <<-DESC
-          Creates a scaffold for the development of a new Pod named `NAME`
-          according to the CocoaPods best practices.
-          If a `TEMPLATE_URL`, pointing to a git repo containing a compatible
-          template, is specified, it will be used in place of the default one.
-        DESC
+      attributes :id, :type, :url, :preview_url,
+             :remote_url, :text_url, :meta,
+             :description
+    
+      def initial_state_params
+    {
+      settings: {},
+      token: current_session&.token,
+    }
+  end
+end
+
+    
+        def puts(*args)
+      STDERR.puts *args unless @silence
+    end
+    
+      desc 'update main and version in bower.json'
+  task :generate do
+    require 'bootstrap-sass'
+    Dir.chdir Bootstrap.gem_path do
+      spec       = JSON.parse(File.read 'bower.json')
+    
+        # @return [Node] The SassScript within the interpolation
+    attr_reader :mid
+    
+          def core_gem_names
+        %w(capistrano airbrussh rake sshkit net-ssh) & Gem.loaded_specs.keys
+      end
+    
+            rows.each do |row|
+          line = row.values.each_with_index.map do |value, col|
+            value.to_s.ljust(col_widths[col])
+          end.join(' ').rstrip
+          line = color.colorize(line, row.color) if row.color
+          puts line
+        end
+      end
+    
+      mkdir_p tasks_dir
