5bbd6cd1ae896a4a06c99ac1b39166196619746a
SUNsung <weatherius@gmail.com>
2018-04-15 22:32:17 +0300

    Rewrite [51hXMkdt8WGAA51JJR8NrWODcQ51yhTJ94Lf9yk51oFf.cpp] File in: 2018-04-15 22:32:16

--- cccccc/51hXMkdt8WGAA51JJR8NrWODcQ51yhTJ94Lf9yk51oFf.cpp
+++ cccccc/51hXMkdt8WGAA51JJR8NrWODcQ51yhTJ94Lf9yk51oFf.cpp
@@ -1,110 +1,322 @@
 
         
-        #include 'CodeCompletionOrganizer.h'
-#include 'SourceKit/Support/FuzzyStringMatcher.h'
-#include 'swift/AST/ASTContext.h'
-#include 'swift/AST/Module.h'
-#include 'swift/Frontend/Frontend.h'
-#include 'clang/Basic/CharInfo.h'
-#include 'clang/Basic/Module.h'
-#include 'llvm/ADT/DenseSet.h'
-#include 'llvm/ADT/ilist.h'
-#include 'llvm/ADT/ilist_node.h'
-#include <deque>
-    
-    /// A utility for finding dead-end blocks.
-///
-/// Dead-end blocks are blocks from which there is no path to the function exit
-/// (either return or throw). These are blocks which end with an unreachable
-/// instruction and blocks from which all paths end in 'unreachable' blocks.
-/// This utility is needed to determine if the a value definition can have a
-/// lack of users ignored along a specific path.
-class DeadEndBlocks {
-  llvm::SetVector<const SILBasicBlock *> ReachableBlocks;
-  const SILFunction *F;
-  bool isComputed = false;
-    }
-    
-    namespace swift {
-    }
-    
-    @interface CalendarBridgingTester : NSObject
-- (NSCalendar *)autoupdatingCurrentCalendar;
-- (BOOL)verifyAutoupdatingCalendar:(NSCalendar *)calendar;
-@end
-    
-    #ifndef SWIFT_INDEX_INDEXRECORD_H
-#define SWIFT_INDEX_INDEXRECORD_H
-    
-    namespace swift {
-namespace syntax {
-    }
-    }
-    
-    #endif // SWIFT_BASIC_DIAGNOSTICOPTIONS_H
-
-    
-    #ifndef SWIFT_PRINTINGDIAGNOSTICCONSUMER_H
-#define SWIFT_PRINTINGDIAGNOSTICCONSUMER_H
-    
-      IndexSymbol() = default;
-    
-      /// Returns the line and column represented by the given source location.
-  ///
-  /// If \p BufferID is provided, \p Loc must come from that source buffer.
-  ///
-  /// This respects #line directives.
-  std::pair<unsigned, unsigned>
-  getLineAndColumn(SourceLoc Loc, unsigned BufferID = 0) const {
-    assert(Loc.isValid());
-    int LineOffset = getLineOffset(Loc);
-    int l, c;
-    std::tie(l, c) = LLVMSourceMgr.getLineAndColumn(Loc.Value, BufferID);
-    assert(LineOffset+l > 0 && 'bogus line offset');
-    return { LineOffset + l, c };
+          uint64_t Size(const Slice& start, const Slice& limit) {
+    Range r(start, limit);
+    uint64_t size;
+    db_->GetApproximateSizes(&r, 1, &size);
+    return size;
   }
     
-    namespace swift {
-    }
+    // Soft limit on number of level-0 files.  We slow down writes at this point.
+static const int kL0_SlowdownWritesTrigger = 8;
     
-    namespace content {
-class RenderFrameHost;
+    TEST(FormatTest, InternalKeyShortestSuccessor) {
+  ASSERT_EQ(IKey('g', kMaxSequenceNumber, kValueTypeForSeek),
+            ShortSuccessor(IKey('foo', 100, kTypeValue)));
+  ASSERT_EQ(IKey('\xff\xff', 100, kTypeValue),
+            ShortSuccessor(IKey('\xff\xff', 100, kTypeValue)));
 }
-    
-    void AutofillAgent::AcceptDataListSuggestion(const string16& suggested_value) {
-  string16 new_value = suggested_value;
-  // If this element takes multiple values then replace the last part with
-  // the suggestion.
-  if (element_.isMultiple() &&
-      element_.formControlType() == WebString::fromUTF8('email')) {
-    std::vector<string16> parts;
-    }
-    }
-    
-    
-    {    blink::WebSecurityPolicy::removeOriginAccessWhitelistEntry(GURL(sourceOrigin),
-                                                             blink::WebString::fromUTF8(destinationProtocol),
-                                                             blink::WebString::fromUTF8(destinationHost),
-                                                             allowDestinationSubdomains);
-    args.GetReturnValue().Set(v8::Undefined(isolate));
-    return;
-  }
-  scoped_ptr<base::Value> value_args(
-                                     converter->FromV8Value(args[2], isolate->GetCurrentContext()));
-  if (!value_args.get() ||
-      !value_args->IsType(base::Value::TYPE_LIST)) {
-    args.GetReturnValue().Set(isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate,
-                                     'Unable to convert 'args' passed to CallStaticMethodSync'))));
-    return;
-  }
-    
-    #include 'base/values.h'
-#include 'components/zoom/zoom_controller.h'
-#include 'content/nw/src/api/object_manager.h'
-#include 'content/nw/src/api/menuitem/menuitem.h'
-#include 'content/public/browser/web_contents.h'
-#include 'content/public/common/page_zoom.h'
-#include 'ui/views/controls/menu/menu_runner.h'
     
     
     {}  // namespace
+    
+    
+// Owned filenames have the form:
+//    dbname/CURRENT
+//    dbname/LOCK
+//    dbname/LOG
+//    dbname/LOG.old
+//    dbname/MANIFEST-[0-9]+
+//    dbname/[0-9]+.(log|sst|ldb)
+bool ParseFileName(const std::string& fname,
+                   uint64_t* number,
+                   FileType* type) {
+  Slice rest(fname);
+  if (rest == 'CURRENT') {
+    *number = 0;
+    *type = kCurrentFile;
+  } else if (rest == 'LOCK') {
+    *number = 0;
+    *type = kDBLockFile;
+  } else if (rest == 'LOG' || rest == 'LOG.old') {
+    *number = 0;
+    *type = kInfoLogFile;
+  } else if (rest.starts_with('MANIFEST-')) {
+    rest.remove_prefix(strlen('MANIFEST-'));
+    uint64_t num;
+    if (!ConsumeDecimalNumber(&rest, &num)) {
+      return false;
+    }
+    if (!rest.empty()) {
+      return false;
+    }
+    *type = kDescriptorFile;
+    *number = num;
+  } else {
+    // Avoid strtoull() to keep filename format independent of the
+    // current locale
+    uint64_t num;
+    if (!ConsumeDecimalNumber(&rest, &num)) {
+      return false;
+    }
+    Slice suffix = rest;
+    if (suffix == Slice('.log')) {
+      *type = kLogFile;
+    } else if (suffix == Slice('.sst') || suffix == Slice('.ldb')) {
+      *type = kTableFile;
+    } else if (suffix == Slice('.dbtmp')) {
+      *type = kTempFile;
+    } else {
+      return false;
+    }
+    *number = num;
+  }
+  return true;
+}
+    
+      fname = CurrentFileName('foo');
+  ASSERT_EQ('foo/', std::string(fname.data(), 4));
+  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
+  ASSERT_EQ(0, number);
+  ASSERT_EQ(kCurrentFile, type);
+    
+    static void UnrefEntry(void* arg1, void* arg2) {
+  Cache* cache = reinterpret_cast<Cache*>(arg1);
+  Cache::Handle* h = reinterpret_cast<Cache::Handle*>(arg2);
+  cache->Release(h);
+}
+    
+      // Return an iterator for the specified file number (the corresponding
+  // file length must be exactly 'file_size' bytes).  If 'tableptr' is
+  // non-NULL, also sets '*tableptr' to point to the Table object
+  // underlying the returned iterator, or NULL if no Table object underlies
+  // the returned iterator.  The returned '*tableptr' object is owned by
+  // the cache and should not be deleted, and is valid for as long as the
+  // returned iterator is live.
+  Iterator* NewIterator(const ReadOptions& options,
+                        uint64_t file_number,
+                        uint64_t file_size,
+                        Table** tableptr = NULL);
+    
+          case kPrevLogNumber:
+        if (GetVarint64(&input, &prev_log_number_)) {
+          has_prev_log_number_ = true;
+        } else {
+          msg = 'previous log number';
+        }
+        break;
+    
+      // State kept for progress messages
+  int done_;
+  int next_report_;     // When to report next
+    
+    #endif  // STORAGE_LEVELDB_INCLUDE_FILTER_POLICY_H_
+
+    
+    void ComputeExtrinsicRefine(const Mat& imagePoints, const Mat& objectPoints, Mat& rvec,
+                            Mat&  tvec, Mat& J, const int MaxIter,
+                            const IntrinsicParams& param, const double thresh_cond);
+CV_EXPORTS Mat ComputeHomography(Mat m, Mat M);
+    
+    
+    {
+    {
+    {
+    {                        const float eps = 1e-4f;
+                        //TODO: perhaps it is better to normalize the cross product by norms of the tangent vectors
+                        if (fabs(tangentVector_1.cross(tangentVector_2)) < eps)
+                        {
+                            isGeneralPosition = false;
+                        }
+                    }
+                }
+            }
+        }
+        while(!isGeneralPosition);
+    }
+    else
+    {
+        //create points in a degenerate position (there are at least 3 points belonging to the same line)
+    
+            template<int CTA_SIZE>
+        static __forceinline__ __device__ int Ballot(int predicate)
+        {
+#if defined (__CUDA_ARCH__) && (__CUDA_ARCH__ >= 200)
+            return __ballot(predicate);
+#else
+            __shared__ volatile int cta_buffer[CTA_SIZE];
+    }
+    
+    //! @cond IGNORED
+    
+    #ifndef CODEGEN_FUNCPTR
+    #define CODEGEN_REMOVE_FUNCPTR
+    #if defined(_WIN32)
+        #define CODEGEN_FUNCPTR APIENTRY
+    #else
+        #define CODEGEN_FUNCPTR
+    #endif
+#endif // CODEGEN_FUNCPTR
+    
+    // Computes and returns the squared evaluation metric for a line fit.
+double DetLineFit::EvaluateLineFit() {
+  // Compute the upper quartile error from the line.
+  double dist = ComputeUpperQuartileError();
+  if (distances_.size() >= kMinPointsForErrorCount &&
+      dist > kMaxRealDistance * kMaxRealDistance) {
+    // Use the number of mis-fitted points as the error metric, as this
+    // gives a better measure of fit for badly fitted lines where more
+    // than a quarter are badly fitted.
+    double threshold = kMaxRealDistance * sqrt(square_length_);
+    dist = NumberOfMisfittedPoints(threshold);
+  }
+  return dist;
+}
+    
+    #include <stdio.h>
+#include <math.h>
+#include 'quadlsq.h'
+#include 'tprintf.h'
+    
+    /**----------------------------------------------------------------------------
+          Include Files and Type Defines
+----------------------------------------------------------------------------**/
+#include 'strngs.h'
+    
+    // Feature distance calculator designed to provide a fast distance calculation
+// based on set difference between a given feature set and many other feature
+// sets in turn.
+// Representation of a feature set as an array of bools that are sparsely
+// true, and companion arrays that allow fast feature set distance
+// calculations with allowance of offsets in position.
+// Init is expensive, so for greatest efficiency, to re-initialize for a new
+// feature set, use Set(..., false) on the SAME feature set as was used to
+// setup with Set(..., true), to return to its initialized state before
+// reuse with Set(..., true) on a new feature set.
+class IntFeatureDist {
+ public:
+  IntFeatureDist();
+  ~IntFeatureDist();
+    }
+    
+            const char* StringRange::Data() const
+        {
+            return m_data;
+        }
+    
+    
+    {            // m_start and m_end track the captured range.
+            // m_end advances when LStrip() is called.
+            const char* m_start;
+            const char* m_end;
+        };
+    
+        // Taken from ONNX
+    REGISTER_OPERATOR_SCHEMA(Gather)
+        .Description('Given data tensor of rank r >= 1, and indices tensor of rank q, gather '
+            'entries of the outer-most dimension of data indexed by indices, and concatenate '
+            'them in an output tensor of rank q + (r - 1). '
+            'Example 1: data = [ [1.0, 1.2], [2.3, 3.4], [4.5, 5.7], ] '
+            '           indices = [ [0, 1], [1, 2], ] '
+            '           output = [ [ [1.0, 1.2], [2.3, 3.4], ], [ [2.3, 3.4], [4.5, 5.7], ], ]'
+            'Example 2: data = [ [1.0, 1.2, 1.9], [2.3, 3.4, 3.9], [4.5, 5.7, 5.9], ] '
+            '           indices = [0, 2], ] axis = 1, '
+            '           output = [ [ [1.0, 1.9], [2.3, 3.9], [4.5, 5.9], ], ]')
+        .Input('data', 'Tensor of rank r >= 1.', 'T')
+        .Input('indices', 'Tensor of int32/int64 indices, of any rank q.', 'Tind')
+        .Output('output', 'Tensor of rank q + (r - 1).', 'T')
+        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
+            'Constrain input types to float tensors.')
+        .TypeConstraint('Tind', { 'tensor(int32)', 'tensor(int64)' },
+            'Constrain indices types to float tensors.')
+        .Attr('axis',
+            'Which axis to gather on, defaults to 0. Negative value means counting dimensions '
+            'from the back. Accepted range in [-r, r-1]',
+            AttrType::AttributeProto_AttributeType_INT, int64_t(0));
+    
+    // Class represents a description of an HTK chunk.
+// It is only used internally by the HTK deserializer.
+// Can exist without associated data and provides methods for requiring/releasing chunk data.
+// TODO: We should consider splitting data load from the description in the future versions.
+class HTKChunkInfo
+{
+    // All utterances in the chunk.
+    std::vector<UtteranceDescription> m_utterances;
+    }
+    
+    
+    {
+    {        if (flags & CopyNodeFlags::copyNodeInputLinks)
+        {
+            // copy the children structure but use the new nodes generated
+            for (int i = 0; i < fromNode->GetNumInputs(); i++)
+                toNode->SetInput(i, GetNodeFromName(toNamePrefix + fromNode->GetInputs()[i]->NodeName()));
+        }
+    }
+}
+    
+    
+    {
+    {
+    {// utility function to round an integer up to a multiple of size
+size_t RoundUp(size_t value, size_t size);
+// HIGH and LOW DWORD functions
+DWORD HIDWORD(size_t size);
+DWORD LODWORD(size_t size);
+} } }
+
+    
+    /// An internal runnable used throughout osquery as dispatcher services.
+using InternalRunnableRef = std::shared_ptr<InternalRunnable>;
+using InternalThreadRef = std::shared_ptr<std::thread>;
+    
+    
+    { private:
+  std::string process_name_;
+};
+    
+     private:
+  AddExternalCallback add_;
+  RemoveExternalCallback remove_;
+    
+      // Enables pretty-printing in gtest (ASSERT|EXPECT)_(EQ|NE)
+  friend ::std::ostream& operator<<(::std::ostream& os, const Status& s);
+    
+        size_t length = 0;
+    void *e = NULL;
+    switch (*(int *)data) {
+    case 0:
+      e = osquery_cqueue_reserve(
+          &osquery.cqueue, OSQUERY_TEST_EVENT_0, sizeof(test_event_0_data_t));
+      length = 4096;
+      break;
+    case 1:
+      e = osquery_cqueue_reserve(
+          &osquery.cqueue, OSQUERY_TEST_EVENT_1, sizeof(test_event_1_data_t));
+      length = 33;
+      break;
+    default:
+      return -ENOTTY;
+    }
+    if (!e) {
+      return -EINVAL;
+    }
+    
+      /*
+   * @brief a unique ID identifying the 'carve'
+   *
+   * This unique generated GUID is used to identify the carve session from
+   * other carves. It is also used by our backend service to derive a
+   * session key for exfiltration.
+   */
+  std::string carveGuid_;
+    
+    class TLSConfigPlugin : public ConfigPlugin,
+                        public std::enable_shared_from_this<TLSConfigPlugin> {
+ public:
+  Status setUp() override;
+  Status genConfig(std::map<std::string, std::string>& config) override;
+    }
+    
+    namespace osquery {
+    }
