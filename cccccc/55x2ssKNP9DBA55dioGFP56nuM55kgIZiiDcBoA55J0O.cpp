342c85ee7d81d8291b77c751439d89c9960dded1
SUNsung <weatherius@gmail.com>
2018-04-15 04:40:53 +0300

    Rewrite [55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp] File in: 2018-04-15 04:40:53

--- cccccc/55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp
+++ cccccc/55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp
@@ -1,139 +1,180 @@
 
         
-        void UnregisterShellMenu(std::wstring opt, wchar_t* keyBaseName)
-{
-	HKEY root = GetRootKey(opt);
-	HKEY cmderKey;
-	FAIL_ON_ERROR(RegCreateKeyEx(root, keyBaseName, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &cmderKey, NULL));
-	FAIL_ON_ERROR(RegDeleteTree(cmderKey, NULL));
-	RegCloseKey(cmderKey);
-	RegCloseKey(root);
-}
-    
-    const std::string kKernelSyscallAddrModifiedPath = '/sys/kernel/camb/syscall_addr_modified';
-const std::string kKernelTextHashPath = '/sys/kernel/camb/text_segment_hash';
-    
-      // Create the IOCTL (and more) device node.
-  osquery.devfs = devfs_make_node(makedev(osquery.major_number, 0),
-                                  DEVFS_CHAR,
-                                  UID_ROOT,
-                                  GID_WHEEL,
-                                  0600,
-                                  'osquery',
-                                  0);
-  if (osquery.devfs == NULL) {
-    dbg_printf('Could not get a devfs entry!\n');
-    goto error_exit;
-  }
-    
-    #include 'osquery/tests/test_util.h'
-    
-    bool platformModuleClose(ModuleHandle module) {
-  return (::dlclose(module) == 0);
-}
-    
-    // customize random engine.
-void CustomGlobalRandomEngine::seed(CustomGlobalRandomEngine::result_type val) {
-  // ignore the seed
+        
+void Base::Call(const std::string& method, const base::ListValue& arguments,
+                content::RenderFrameHost* rvh) {
+  NOTREACHED() << 'Uncatched call in Base'
+               << ' method:' << method
+               << ' arguments:' << arguments;
 }
     
     
-    {    int nindex = index_.num_chunk();
-    int nvalue = value_.num_chunk();
-    int ntotal = nindex + nvalue;
-    #pragma omp parallel for schedule(dynamic, 1)  num_threads(nthread_write_)
-    for (int i = 0; i < ntotal; ++i) {
-      if (i < nindex) {
-        index_.Compress(i, use_lz4_hc_);
-      } else {
-        value_.Compress(i - nindex, use_lz4_hc_);
-      }
+    {  DISALLOW_COPY_AND_ASSIGN(Base);
+};
+    
+    
+    {}
+    
+       void Call(const std::string& method,
+                    const base::ListValue& arguments) override;
+   void CallSync(const std::string& method,
+                        const base::ListValue& arguments,
+                        base::ListValue* result) override;
+    
+    namespace base {
+class ListValue;
+}
+    
+    
+    {  *y = CalculateMenuYPosition(&screen_rect, &menu_req, NULL, *y);
+}
+    
+    
+    {  base::ListValue args;
+  dispatcher_host()->SendEvent(this, 'click', args);
+}
+    
+    
+    {  DECLARE_EXTENSION_FUNCTION('nw.App.clearCache', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwAppClearCacheFunction);
+};
+    
+    
+    {    ui::Clipboard* clipboard_;
+    std::string error_;
+  };
+    
+    #include 'extensions/browser/extension_function.h'
+    
+        UINT createDeviceFlags = 0;
+    
+        f3coeff[1] = 2*D[1][2] - 2*D[1][4] + 2*D[2][1] + 2*D[2][5] + 2*D[2][9] - 2*D[4][1] - 2*D[4][5] - 2*D[4][9] + 2*D[5][2] - 2*D[5][4] + 2*D[9][2] - 2*D[9][4]; // constant term
+    f3coeff[2] = 2*D[1][6] + 2*D[1][8] + 4*D[2][3] + 4*D[2][7] + 4*D[3][2] + 4*D[3][4] + 4*D[4][3] + 4*D[4][7] - 2*D[5][6] - 2*D[5][8] + 2*D[6][1] - 2*D[6][5] - 2*D[6][9] + 4*D[7][2] + 4*D[7][4] + 2*D[8][1] - 2*D[8][5] - 2*D[8][9] - 2*D[9][6] - 2*D[9][8]; // s1^2  * s2
+    f3coeff[3] = 8*D[2][2] - 8*D[3][3] - 8*D[4][4] + 8*D[6][6] + 8*D[7][7] - 8*D[8][8]; // s1 * s2
+    f3coeff[4] = 4*D[1][8] - 4*D[1][6] + 8*D[2][3] + 8*D[2][7] + 8*D[3][2] - 8*D[3][4] - 8*D[4][3] - 8*D[4][7] - 4*D[5][6] + 4*D[5][8] - 4*D[6][1] - 4*D[6][5] + 4*D[6][9] + 8*D[7][2] - 8*D[7][4] + 4*D[8][1] + 4*D[8][5] - 4*D[8][9] + 4*D[9][6] - 4*D[9][8]; // s1 * s3
+    f3coeff[5] = 4*D[1][3] - 4*D[1][7] + 8*D[2][6] + 8*D[2][8] + 4*D[3][1] + 4*D[3][5] - 4*D[3][9] - 8*D[4][6] - 8*D[4][8] + 4*D[5][3] - 4*D[5][7] + 8*D[6][2] - 8*D[6][4] - 4*D[7][1] - 4*D[7][5] + 4*D[7][9] + 8*D[8][2] - 8*D[8][4] - 4*D[9][3] + 4*D[9][7]; // s2 * s3
+    f3coeff[6] = 4*D[1][1] - 4*D[5][5] + 4*D[5][9] + 8*D[6][6] + 8*D[6][8] + 8*D[8][6] + 8*D[8][8] + 4*D[9][5] - 4*D[9][9]; // s2^2 * s3
+    f3coeff[7] = 2*D[5][6] - 2*D[1][8] - 2*D[1][6] + 2*D[5][8] - 2*D[6][1] + 2*D[6][5] - 2*D[6][9] - 2*D[8][1] + 2*D[8][5] - 2*D[8][9] - 2*D[9][6] - 2*D[9][8]; // s2^3
+    f3coeff[8] = 6*D[3][9] - 6*D[1][7] - 6*D[3][1] - 6*D[3][5] - 6*D[1][3] - 6*D[5][3] - 6*D[5][7] - 6*D[7][1] - 6*D[7][5] + 6*D[7][9] + 6*D[9][3] + 6*D[9][7]; // s1 * s3^2
+    f3coeff[9] = 2*D[1][3] + 2*D[1][7] + 4*D[2][6] - 4*D[2][8] + 2*D[3][1] + 2*D[3][5] + 2*D[3][9] - 4*D[4][6] + 4*D[4][8] + 2*D[5][3] + 2*D[5][7] + 4*D[6][2] - 4*D[6][4] + 2*D[7][1] + 2*D[7][5] + 2*D[7][9] - 4*D[8][2] + 4*D[8][4] + 2*D[9][3] + 2*D[9][7]; // s1
+    f3coeff[10] = 8*D[2][2] - 4*D[1][5] - 4*D[1][1] - 8*D[2][4] - 8*D[4][2] + 8*D[4][4] - 4*D[5][1] - 4*D[5][5] + 4*D[9][9]; // s3
+    f3coeff[11] = 2*D[1][6] + 2*D[1][8] - 4*D[2][3] + 4*D[2][7] - 4*D[3][2] + 4*D[3][4] + 4*D[4][3] - 4*D[4][7] + 2*D[5][6] + 2*D[5][8] + 2*D[6][1] + 2*D[6][5] + 2*D[6][9] + 4*D[7][2] - 4*D[7][4] + 2*D[8][1] + 2*D[8][5] + 2*D[8][9] + 2*D[9][6] + 2*D[9][8]; // s2
+    f3coeff[12] = 6*D[6][9] - 6*D[1][8] - 6*D[5][6] - 6*D[5][8] - 6*D[6][1] - 6*D[6][5] - 6*D[1][6] - 6*D[8][1] - 6*D[8][5] + 6*D[8][9] + 6*D[9][6] + 6*D[9][8]; // s2 * s3^2
+    f3coeff[13] = 2*D[1][2] - 2*D[1][4] + 2*D[2][1] - 2*D[2][5] - 2*D[2][9] + 4*D[3][6] - 4*D[3][8] - 2*D[4][1] + 2*D[4][5] + 2*D[4][9] - 2*D[5][2] + 2*D[5][4] + 4*D[6][3] + 4*D[6][7] + 4*D[7][6] - 4*D[7][8] - 4*D[8][3] - 4*D[8][7] - 2*D[9][2] + 2*D[9][4]; // s1^2
+    f3coeff[14] = 6*D[1][4] - 6*D[1][2] - 6*D[2][1] - 6*D[2][5] + 6*D[2][9] + 6*D[4][1] + 6*D[4][5] - 6*D[4][9] - 6*D[5][2] + 6*D[5][4] + 6*D[9][2] - 6*D[9][4]; // s3^2
+    f3coeff[15] = 2*D[1][4] - 2*D[1][2] - 2*D[2][1] + 2*D[2][5] - 2*D[2][9] - 4*D[3][6] - 4*D[3][8] + 2*D[4][1] - 2*D[4][5] + 2*D[4][9] + 2*D[5][2] - 2*D[5][4] - 4*D[6][3] + 4*D[6][7] + 4*D[7][6] + 4*D[7][8] - 4*D[8][3] + 4*D[8][7] - 2*D[9][2] + 2*D[9][4]; // s2^2
+    f3coeff[16] = 4*D[1][1] + 4*D[1][5] - 4*D[1][9] + 4*D[5][1] + 4*D[5][5] - 4*D[5][9] - 4*D[9][1] - 4*D[9][5] + 4*D[9][9]; // s3^3
+    f3coeff[17] = 4*D[2][9] - 4*D[1][4] - 4*D[2][1] - 4*D[2][5] - 4*D[1][2] + 8*D[3][6] + 8*D[3][8] - 4*D[4][1] - 4*D[4][5] + 4*D[4][9] - 4*D[5][2] - 4*D[5][4] + 8*D[6][3] + 8*D[6][7] + 8*D[7][6] + 8*D[7][8] + 8*D[8][3] + 8*D[8][7] + 4*D[9][2] + 4*D[9][4]; // s1 * s2 * s3
+    f3coeff[18] = 4*D[2][6] - 2*D[1][7] - 2*D[1][3] + 4*D[2][8] - 2*D[3][1] + 2*D[3][5] - 2*D[3][9] + 4*D[4][6] + 4*D[4][8] + 2*D[5][3] + 2*D[5][7] + 4*D[6][2] + 4*D[6][4] - 2*D[7][1] + 2*D[7][5] - 2*D[7][9] + 4*D[8][2] + 4*D[8][4] - 2*D[9][3] - 2*D[9][7]; // s1 * s2^2
+    f3coeff[19] = 4*D[1][9] - 4*D[1][1] + 8*D[3][3] + 8*D[3][7] + 4*D[5][5] + 8*D[7][3] + 8*D[7][7] + 4*D[9][1] - 4*D[9][9]; // s1^2 * s3
+    f3coeff[20] = 2*D[1][3] + 2*D[1][7] + 2*D[3][1] - 2*D[3][5] - 2*D[3][9] - 2*D[5][3] - 2*D[5][7] + 2*D[7][1] - 2*D[7][5] - 2*D[7][9] - 2*D[9][3] - 2*D[9][7]; // s1^3
+    
+    void dAB(cv::InputArray A, InputArray B, OutputArray dABdA, OutputArray dABdB);
+    
+        if (new_width * new_cn != total_width)
+        CV_Error(cv::Error::BadNumChannels, 'The total width is not divisible by the new number of channels');
+    
+        static void* GetProcAddress (const char* name)
+    {
+        static void* h = NULL;
+        if (!h)
+        {
+            h = dlopen('libclAmdBlas.so', RTLD_LAZY | RTLD_GLOBAL);
+            if (!h)
+                return NULL;
+        }
     }
-    index_.Write(fo);
-    value_.Write(fo);
-    // statistics
-    raw_bytes_index_ += index_.RawBytes() * sizeof(bst_uint) / sizeof(StorageIndex);
-    raw_bytes_value_ += value_.RawBytes();
-    encoded_bytes_index_ += index_.EncodedBytes();
-    encoded_bytes_value_ += value_.EncodedBytes();
-    raw_bytes_ += page.offset.size() * sizeof(size_t);
-  }
     
-        index_.resize((boundary_[nfeature - 1].index_end
-                   + (packing_factor_ - 1)) / packing_factor_);
-    row_ind_.resize(boundary_[nfeature - 1].row_ind_end);
+            std::unique_ptr<ZeroCopyInputStream> raw_input(new FileInputStream(p_fd));
+        std::unique_ptr<CodedInputStream> coded_input(
+            new CodedInputStream(raw_input.get()));
+        // Allows protobuf library versions < 3.2.0 to parse messages greater than 64MB.
+        coded_input->SetTotalBytesLimit(INT_MAX, INT_MAX);
+        std::unique_ptr<ModelProto> modelProto(new ModelProto);
+        bool result = modelProto->ParseFromCodedStream(coded_input.get());
+        coded_input.reset();
+        raw_input.reset();
+        if (!result)
+        {
+            return Status(ONNX, INVALID_PROTOBUF, 'Protobuf parsing failed.');
+        }
     
-    TEST(Param, VectorIOStream) {
-  std::vector<int> vals = {3, 2, 1};
-  std::stringstream ss;
-  std::vector<int> vals_in;
-  
-  ss << vals;
-  EXPECT_EQ(ss.str(), '(3,2,1)');
+        const std::string& OperatorSchema::Domain() const
+    {
+        return m_opSignature.Domain();
     }
     
-    // Used to encapsulate a particular instance of an opened database.
-//
-// This object should not be used directly in C++; it exists solely to provide
-// a mapping from a JavaScript object to a C++ code that can use the RocksDB
-// API.
-class DBWrapper : public node::ObjectWrap {
-  public:
-    static void Init(Handle<Object> exports);
+        std::function<void(OperatorSchemaSetter&)> RNNDocGeneratorInputInitialH() {
+        return [=](OperatorSchemaSetter& schema) {
+            schema.Input('initial_h',
+                'Optional initial value of the hidden. If not specified - assumed '
+                'to be 0. It has shape `[num_directions, batch_size, hidden_size]`.', 'T', true /*optional*/);
+        };
     }
     
-      virtual const char* Name() const override;
+        // Taken from ONNX
+    REGISTER_OPERATOR_SCHEMA(Split)
+        .Description('Split a tensor into a list of tensors, along the specified 'axis'. '
+            'The lengths of the split can be specified using argument 'axis' or '
+            'optional second input blob to the operator. Otherwise, the tensor is split '
+            'to equal sized parts.')
+        .Input('input', 'The tensor to split', 'T')
+        .Input('split', 'Optional list of output lengths (see also arg 'split')', 'T')
+        .Output('output', 'A list of output tensors', 'T')
+        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
+            'Constrain input and output types to float tensors.')
+        .Attr('axis', 'Which axis to split on', AttrType::AttributeProto_AttributeType_INT)
+        .Attr('split', 'Number of tensors to output.', AttrType::AttributeProto_AttributeType_INTS);
     
-    #pragma once
-#include <deque>
-#include <string>
     
-      /// Search the list for the (index)'th item (0-based) in (list:key)
-  /// A negative index indicates: 'from end-of-list'
-  /// If index is within range: return true, and return the value in *result.
-  /// If (index < -length OR index>=length), then index is out of range:
-  ///   return false (and *result is left unchanged)
-  /// May throw RedisListException
-  bool Index(const std::string& key, int32_t index,
-             std::string* result);
-    
-      bool StatisticsJni::HistEnabledForType(uint32_t type) const {
-    if (type >= HISTOGRAM_ENUM_MAX) {
-      return false;
-    }
-    
-    if (m_ignore_histograms.count(type) > 0) {
-        return false;
-    }
+    {
+    {            ForwardPropS(sliceOutputValue, sliceUnnormedPrior, sliceMean, sliceLogstddev, sliceFeature,
+                         slicePrior, sliceStddev, sliceNormedDeviationVectors, sliceNormedDeviation, slicePosterior, *m_temp);
+        }
+        else // should not reach the code since validation should fail already
+            RuntimeError('GMMLogLikelihoodNode: UnnormedPrior should either have same number of columns as the features or have only one column.');
     }
     
     
-    {}  // namespace rocksdb
+    {
+    {
+    {    template <class matrixbase>
+    void tomatrix(msra::math::ssematrix<matrixbase> &to) const
+    {
+        for (size_t j = 0; j < ncols; j++)
+            for (size_t i = 0; i < nrows; i++)
+                to(i, j) = (float) (*this)(i, j);
+    }
+};
+};
+}; // namespaces
     
-      virtual size_t GetSimCapacity() const override {
-    return key_only_cache_->GetCapacity();
-  }
-  virtual size_t GetSimUsage() const override {
-    return key_only_cache_->GetUsage();
-  }
-  virtual void SetSimCapacity(size_t capacity) override {
-    key_only_cache_->SetCapacity(capacity);
-  }
+    extern JSClass  *jsb_cocos2d_Physics3DComponent_class;
+extern JSObject *jsb_cocos2d_Physics3DComponent_prototype;
     
-      TransactionDB* snapshotDB;
-  std::vector<ColumnFamilyDescriptor> column_families;
-  column_families.push_back(
-      ColumnFamilyDescriptor(kDefaultColumnFamilyName, ColumnFamilyOptions()));
-  column_families.push_back(
-      ColumnFamilyDescriptor('CFA', ColumnFamilyOptions()));
-  column_families.push_back(
-      ColumnFamilyDescriptor('CFB', ColumnFamilyOptions()));
-  std::vector<rocksdb::ColumnFamilyHandle*> cf_handles;
-  ASSERT_OK(TransactionDB::Open(options, txn_db_options, snapshot_name_,
-                                column_families, &cf_handles, &snapshotDB));
-  ASSERT_OK(snapshotDB->Get(read_options, 'foo', &value));
-  ASSERT_EQ(value, 'bar');
-  ASSERT_OK(snapshotDB->Get(read_options, cf_handles[1], 'foocfa', &value));
-  ASSERT_EQ(value, 'barcfa');
     
-      bool CanHandleDuplicatedKey() const override { return true; }
+    {        ok &= luaval_to_int32(tolua_S, 2,(int *)&arg0, 'cc.PhysicsBody:setContactTestBitmask');
+        if(!ok)
+        {
+            tolua_error(tolua_S,'invalid arguments in function 'lua_cocos2dx_physics_PhysicsBody_setContactTestBitmask'', nullptr);
+            return 0;
+        }
+        cobj->setContactTestBitmask(arg0);
+        lua_settop(tolua_S, 1);
+        return 1;
+    }
+    luaL_error(tolua_S, '%s has wrong number of arguments: %d, was expecting %d \n', 'cc.PhysicsBody:setContactTestBitmask',argc, 1);
+    return 0;
+    
+    
+    
+    			b2Body* prevBody = ground;
+			for (int32 i = 0; i < e_count; ++i)
+			{
+				b2BodyDef bd;
+				bd.type = b2_dynamicBody;
+				bd.position.Set(-4.5f + 1.0f * i, 5.0f);
+				b2Body* body = m_world->CreateBody(&bd);
+				body->CreateFixture(&fd);
+    }
