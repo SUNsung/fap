d36d4ce929c00ce17a3424a7080745e809a99ee7
SUNsung <weatherius@gmail.com>
2018-04-15 12:30:34 +0300

    Rewrite [23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp] File in: 2018-04-15 12:30:32

--- cccccc/23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp
+++ cccccc/23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp
@@ -1,216 +1,60 @@
 
         
-          RenderView* render_view = GetCurrentRenderView();
-  if (!render_view) {
-    args.GetReturnValue().Set(isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate,
-                                     'Unable to get render view in CallObjectMethod'))));
-    return;
-  }
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(DispatcherHost);
-};
-    
-    namespace nwapi {
-    }
-    
-    #ifndef incl_HPHP_WORKLOAD_STATS_H_
-#define incl_HPHP_WORKLOAD_STATS_H_
-    
-    Timer::~Timer() {
-  if (!m_finished) stop();
-}
-    
-    
+        
     {
-    {}}
-
-    
-    
-    {  return false;
-}
-    
-    
-    {  unsigned next_vr{Vreg::V0};
-  Vlabel entry;
-  jit::vector<Vframe> frames;
-  jit::vector<Vblock> blocks;
-  jit::hash_map<Vconst,Vreg,Vconst::Hash> constToReg;
-  jit::hash_map<size_t,Vconst> regToConst;
-  jit::vector<VregList> tuples;
-  jit::vector<VcallArgs> vcallArgs;
-  jit::vector<VdataBlock> dataBlocks;
-  uint16_t cur_voff{0};  // current instruction index managed by Vout
-  bool padding{false};
-  bool profiling{false};
-  folly::Optional<TransContext> context;
-  StructuredLogEntry* log_entry{nullptr};
-};
-    
-    
-    {    raise_warning(cs_GMP_INVALID_VALUE_MUST_NOT_BE_ZERO, 'gmp_div_r');
-    return false;
-  }
-    
-    #endif
-#endif
-
-    
-    NormalizedInstruction::NormalizedInstruction(SrcKey sk, const Unit* u)
-  : source(sk)
-  , funcd(nullptr)
-  , m_unit(u)
-  , immVec()
-  , endsRegion(false)
-  , preppedByRef(false)
-  , ignoreInnerType(false)
-  , interp(false)
-  , forceSurpriseCheck(false)
-{
-  memset(imm, 0, sizeof(imm));
-  populateImmediates(*this);
-}
-    
-    
-    {
-    {///////////////////////////////////////////////////////////////////////////////
-}}
-#endif
-
-    
-    /*
- * Insert a jump to destSk at toSmash. If no top translation for destSk exists
- * no action is performed. On return, the value of smashed indicated whether a
- * new address was written into the TC.
- */
-TCA bindJmp(TCA toSmash, SrcKey destSk, TransFlags trflags, bool& smashed);
-    
-      DataBlock(DataBlock&& other) = default;
-  DataBlock& operator=(DataBlock&& other) = default;
-    
-    
-    {}  // namespace grpc_node_generator
-    
-    class PythonGrpcGenerator : public grpc::protobuf::compiler::CodeGenerator {
- public:
-  PythonGrpcGenerator(const GeneratorConfiguration& config);
-  ~PythonGrpcGenerator();
-    }
-    
-    bool SecureAuthContext::IsPeerAuthenticated() const {
-  if (!ctx_) return false;
-  return grpc_auth_context_peer_is_authenticated(ctx_) != 0;
-}
-    
-    #if TARGET_OS_IPHONE
-GRPC_XMACRO_ITEM(isWWAN, IsWWAN)
-#endif
-GRPC_XMACRO_ITEM(reachable, Reachable)
-GRPC_XMACRO_ITEM(transientConnection, TransientConnection)
-GRPC_XMACRO_ITEM(connectionRequired, ConnectionRequired)
-GRPC_XMACRO_ITEM(connectionOnTraffic, ConnectionOnTraffic)
-GRPC_XMACRO_ITEM(interventionRequired, InterventionRequired)
-GRPC_XMACRO_ITEM(connectionOnDemand, ConnectionOnDemand)
-GRPC_XMACRO_ITEM(isLocalAddress, IsLocalAddress)
-GRPC_XMACRO_ITEM(isDirect, IsDirect)
-
-    
-    int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
-
-    
-    #include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-    
-    
-    {  delete tensor;
-  delete tensor2;
-  cout << 'OK' << endl;
-  return 0;
-}
-
-    
-      /// Peek at the incoming data on the stream. Returns the number of bytes read,
-  /// or 0 if an error occurred.
-  template <typename MutableBufferSequence>
-  std::size_t peek(const MutableBufferSequence& buffers,
-      boost::system::error_code& ec);
-    
-    #ifndef BOOST_ASIO_BUFFERED_WRITE_STREAM_FWD_HPP
-#define BOOST_ASIO_BUFFERED_WRITE_STREAM_FWD_HPP
-    
-    #if !defined(BOOST_ASIO_HAS_THREADS)
-typedef long atomic_count;
-inline void increment(atomic_count& a, long b) { a += b; }
-#elif defined(BOOST_ASIO_HAS_STD_ATOMIC)
-typedef std::atomic<long> atomic_count;
-inline void increment(atomic_count& a, long b) { a += b; }
-#else // defined(BOOST_ASIO_HAS_STD_ATOMIC)
-typedef boost::detail::atomic_count atomic_count;
-inline void increment(atomic_count& a, long b) { while (b > 0) ++a, --b; }
-#endif // defined(BOOST_ASIO_HAS_STD_ATOMIC)
-    
-      static bool all_empty(const Buffers& buffer_sequence)
-  {
-    typename Buffers::const_iterator iter = buffer_sequence.begin();
-    typename Buffers::const_iterator end = buffer_sequence.end();
-    std::size_t i = 0;
-    for (; iter != end && i < max_buffers; ++iter, ++i)
-      if (boost::asio::buffer_size(Buffer(*iter)) > 0)
-        return false;
-    return true;
-  }
-    
-    
-    {    // Make the upcall if required.
-    if (owner)
-    {
-      fenced_block b(fenced_block::half);
-      BOOST_ASIO_HANDLER_INVOCATION_BEGIN((handler.arg1_, handler.arg2_));
-      boost_asio_handler_invoke_helpers::invoke(handler, handler.handler_);
-      BOOST_ASIO_HANDLER_INVOCATION_END;
+    {    int last_dc = 0;
+    const coeff_t* src_coeffs = components_[c].coeffs();
+    coeff_t* dest_coeffs = &comp->coeffs[0];
+    for (int block_y = 0; block_y < comp->height_in_blocks; ++block_y) {
+      for (int block_x = 0; block_x < comp->width_in_blocks; ++block_x) {
+        if (block_y >= components_[c].height_in_blocks() ||
+            block_x >= components_[c].width_in_blocks()) {
+          dest_coeffs[0] = last_dc;
+          for (int k = 1; k < kDCTBlockSize; ++k) {
+            dest_coeffs[k] = 0;
+          }
+        } else {
+          for (int k = 0; k < kDCTBlockSize; ++k) {
+            const int quant = q[c][k];
+            int coeff = src_coeffs[k];
+            assert(coeff % quant == 0);
+            dest_coeffs[k] = coeff / quant;
+          }
+          src_coeffs += kDCTBlockSize;
+        }
+        last_dc = dest_coeffs[0];
+        dest_coeffs += kDCTBlockSize;
+      }
     }
   }
+  SaveQuantTables(q, jpg);
+}
     
-    #if !defined(BOOST_ASIO_HAS_THREADS) \
-  || defined(BOOST_ASIO_DISABLE_FENCED_BLOCK)
-# include <boost/asio/detail/null_fenced_block.hpp>
-#elif defined(__MACH__) && defined(__APPLE__)
-# include <boost/asio/detail/macos_fenced_block.hpp>
-#elif defined(__sun)
-# include <boost/asio/detail/solaris_fenced_block.hpp>
-#elif defined(__GNUC__) && defined(__arm__) \
-  && !defined(__GCC_HAVE_SYNC_COMPARE_AND_SWAP_4)
-# include <boost/asio/detail/gcc_arm_fenced_block.hpp>
-#elif defined(__GNUC__) && (defined(__hppa) || defined(__hppa__))
-# include <boost/asio/detail/gcc_hppa_fenced_block.hpp>
-#elif defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
-# include <boost/asio/detail/gcc_x86_fenced_block.hpp>
-#elif defined(__GNUC__) \
-  && ((__GNUC__ == 4 && __GNUC_MINOR__ >= 1) || (__GNUC__ > 4)) \
-  && !defined(__INTEL_COMPILER) && !defined(__ICL) \
-  && !defined(__ICC) && !defined(__ECC) && !defined(__PATHSCALE__)
-# include <boost/asio/detail/gcc_sync_fenced_block.hpp>
-#elif defined(BOOST_ASIO_WINDOWS) && !defined(UNDER_CE)
-# include <boost/asio/detail/win_fenced_block.hpp>
-#else
-# include <boost/asio/detail/null_fenced_block.hpp>
-#endif
+    #include 'guetzli/gamma_correct.h'
     
-      // Remove the descriptor from /dev/poll. Since this function is only called
-  // during a fork, we can apply the change immediately.
-  ::pollfd ev = { 0, 0, 0 };
-  ev.fd = descriptor;
-  ev.events = POLLREMOVE;
-  ev.revents = 0;
-  ::write(dev_poll_fd_, &ev, sizeof(ev));
-    
-      if (shutdown_)
-  {
-    io_service_.post_immediate_completion(op, false);
-    return;
+    // Mimic libjpeg's heuristics to guess jpeg color space.
+// Requires that the jpg has 3 components.
+bool HasYCbCrColorSpace(const JPEGData& jpg) {
+  bool has_Adobe_marker = false;
+  uint8_t Adobe_transform = 0;
+  for (const std::string& app : jpg.app_data) {
+    if (static_cast<uint8_t>(app[0]) == 0xe0) {
+      return true;
+    } else if (static_cast<uint8_t>(app[0]) == 0xee && app.size() >= 15) {
+      has_Adobe_marker = true;
+      Adobe_transform = app[14];
+    }
   }
+  if (has_Adobe_marker) {
+    return (Adobe_transform != 0);
+  }
+  const int cid0 = jpg.components[0].id;
+  const int cid1 = jpg.components[1].id;
+  const int cid2 = jpg.components[2].id;
+  return (cid0 != 'R' || cid1 != 'G' || cid2 != 'B');
+}
+    
+    // Library to decode jpeg coefficients into an RGB image.
+    
+    #include <stddef.h>
+#include <stdint.h>
