2e5d910d029ded2c64e4681a3589281b5549ea4b
SUNsung <weatherius@gmail.com>
2018-04-15 19:08:17 +0300

    Rewrite [31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py] File in: 2018-04-15 19:08:17

--- pyton/31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py
+++ pyton/31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py
@@ -1,73 +1,166 @@
 
         
-          Raises:
-    tf.errors.InvalidArgumentError: if batch_size or num_steps are too high.
-  '''
-  with tf.name_scope(name, 'PTBProducer', [raw_data, batch_size, num_steps]):
-    raw_data = tf.convert_to_tensor(raw_data, name='raw_data', dtype=tf.int32)
+        
+if not PY2:
+    text_type = str
+    string_types = (str,)
+    integer_types = (int,)
     
+        The other use case of this function is to force the return value of a
+    view function into a response which is helpful with view
+    decorators::
     
-class CIFAR10InputTest(tf.test.TestCase):
-    
-    import iris_data
-    
-      # Small utility function to evaluate a dataset by feeding batches of data to
-  # {eval_data} and pulling the results from {eval_predictions}.
-  # Saves memory and enables this to run on smaller GPUs.
-  def eval_in_batches(data, sess):
-    '''Get all predictions for a dataset by running it in small batches.'''
-    size = data.shape[0]
-    if size < EVAL_BATCH_SIZE:
-      raise ValueError('batch size for evals larger than dataset: %d' % size)
-    predictions = numpy.ndarray(shape=(size, NUM_LABELS), dtype=numpy.float32)
-    for begin in xrange(0, size, EVAL_BATCH_SIZE):
-      end = begin + EVAL_BATCH_SIZE
-      if end <= size:
-        predictions[begin:end, :] = sess.run(
-            eval_prediction,
-            feed_dict={eval_data: data[begin:end, ...]})
-      else:
-        batch_predictions = sess.run(
-            eval_prediction,
-            feed_dict={eval_data: data[-EVAL_BATCH_SIZE:, ...]})
-        predictions[begin:, :] = batch_predictions[begin - size:, :]
-    return predictions
-    
-    Forward-backward pass:
-Run on Tesla K40c: 480 +/- 48 ms / batch
-Run on Titan X:    244 +/- 30 ms / batch
-'''
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-    
-                # is there a channel override for token type?
+            for srcobj, loader in self._iter_loaders(template):
             try:
-                overrideChannel = self.channelOverrideMap[t.type]
-                
-            except KeyError:
-                # no override for this type
-                pass
-            
-            else:
-                if overrideChannel == self.channel:
-                    t.channel = overrideChannel
-                else:
-                    discard = True
-            
-            if not discard:
-                t.index = index
-                self.tokens.append(t)
-                index += 1
+                rv = loader.get_source(environment, template)
+                if trv is None:
+                    trv = rv
+            except TemplateNotFound:
+                rv = None
+            attempts.append((loader, srcobj, rv))
     
-    from alembic import op
-import sqlalchemy as sa
+            start = datetime.now()
+        ridge = linear_model.Ridge(alpha=1.)
+        ridge.fit(X, Y)
+        time_ridge[i] = total_seconds(datetime.now() - start)
     
     
-def downgrade():
-    op.drop_table('logs')
+def _plot(results, metrics, formats, title, x_ticks, x_label,
+          format_markers=('x', '|', 'o', '+'),
+          metric_colors=('c', 'm', 'y', 'k', 'g', 'r', 'b')):
+    '''
+    Plot the results by metric, format and some other variable given by
+    x_label
+    '''
+    fig = plt.figure('scikit-learn multilabel metrics benchmarks')
+    plt.title(title)
+    ax = fig.add_subplot(111)
+    for i, metric in enumerate(metrics):
+        for j, format in enumerate(formats):
+            ax.plot(x_ticks, results[i, j].flat,
+                    label='{}, {}'.format(metric, format),
+                    marker=format_markers[j],
+                    color=metric_colors[i % len(metric_colors)])
+    ax.set_xlabel(x_label)
+    ax.set_ylabel('Time (s)')
+    ax.legend()
+    plt.show()
+    
+    
+def plot_feature_errors(all_errors, batch_size, all_components, data):
+    plt.figure()
+    plot_results(all_components, all_errors['pca'], label='PCA')
+    plot_results(all_components, all_errors['ipca'],
+                 label='IncrementalPCA, bsize=%i' % batch_size)
+    plot_results(all_components, all_errors['rpca'], label='RandomizedPCA')
+    plt.legend(loc='lower left')
+    plt.suptitle('Algorithm error vs. n_components\n'
+                 'LFW, size %i x %i' % data.shape)
+    plt.xlabel('Number of components (out of max %i)' % data.shape[1])
+    plt.ylabel('Mean absolute error')
+    
+        results = defaultdict(lambda: [])
+    
+        for i, DD in enumerate(Drange):
+        print('D = %i (%i out of %i)' % (DD, i + 1, len(Drange)))
+        X = get_data(N, DD, dataset)
+        for algorithm in algorithms:
+            nbrs = neighbors.NearestNeighbors(n_neighbors=k,
+                                              algorithm=algorithm,
+                                              leaf_size=leaf_size)
+            t0 = time()
+            nbrs.fit(X)
+            t1 = time()
+            nbrs.kneighbors(X)
+            t2 = time()
+    
+    def plot(func):
+    random_state = check_random_state(0)
+    one_core = []
+    multi_core = []
+    sample_sizes = range(1000, 6000, 1000)
+    
+        (opts, args) = op.parse_args()
+    if len(args) > 0:
+        op.error('this script takes no arguments.')
+        sys.exit(1)
+    
+        url_fmt is along the lines of ('https://github.com/USER/PROJECT/'
+                                   'blob/{revision}/{package}/'
+                                   '{path}#L{lineno}')
+    '''
+    revision = _get_git_revision()
+    return partial(_linkcode_resolve, revision=revision, package=package,
+                   url_fmt=url_fmt)
 
     
-    Revision ID: 3c3ffe173e4f
-Revises: ad82a75afd82
-Create Date: 2016-08-18 14:06:28.784699
+        # Print and plot the confusion matrix
+    cm = metrics.confusion_matrix(y_test, y_predicted)
+    print(cm)
+    
+    plt.matshow(np.outer(np.sort(model.row_labels_) + 1,
+                     np.sort(model.column_labels_) + 1),
+            cmap=plt.cm.Blues)
+plt.title('Checkerboard structure of rearranged data')
+    
+    
+def generate_data(n_samples, n_features):
+    '''Generate random blob-ish data with noisy features.
+    
+        To avoid English-only error messages and to generally make things
+    as flexible as possible, these exceptions are not created with strings,
+    but rather the information necessary to generate an error.  Then
+    the various reporting methods in Parser and Lexer can be overridden
+    to generate a localized error message.  For example, MismatchedToken
+    exceptions are built with the expected token type.
+    So, don't expect getMessage() to return anything.
+    
+    
+    def getLine(self):
+        '''ANTLR tracks the line information automatically'''
+    
+        def on_diagnostics_failed(self):
+        super(Suspect, self).send_diagnostics_failure_report()
+        super(Suspect, self).next_state('failed')
+    
+        def setUp(self):
+        self.tc1 = TC1()
+        self.tc2 = TC2()
+        self.tc3 = TC3()
+        self.average_result_tc1 = '###### In Test 1 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc2 = '###### In Test 2 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc3 = '###### In Test 3 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.runner = TestRunner()
+        self.out = StringIO()
+        self.saved_stdout = sys.stdout
+        sys.stdout = self.out
+    
+    print 'PLUGIN_I18N_PATHS := ' + ','.join(os.path.relpath(plugin.path)
+                                         for plugin in plugins
+                                         if plugin.needs_translation)
+    
+            def mako_module_path(filename, uri):
+            filename = filename.lstrip('/').replace('/', '-')
+            path = os.path.join(module_directory, filename + '.py')
+            return os.path.abspath(path)
+    else:
+        # disable caching templates since we don't know where they should go.
+        module_directory = mako_module_path = None
+    
+    weberror.evalexception.EvalException.post_traceback = _stub
+weberror.evalexception.EvalException.relay = _stub
+    
+            The user's response to the CAPTCHA should be sent as `captcha`
+        along with your request.
