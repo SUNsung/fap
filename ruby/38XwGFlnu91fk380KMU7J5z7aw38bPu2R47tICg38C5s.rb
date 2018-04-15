5da69343fa04fb3917b50ff150d9ff9426ac34f6
SUNsung <weatherius@gmail.com>
2018-04-15 18:20:19 +0300

    Rewrite [38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb] File in: 2018-04-15 18:20:19

--- ruby/38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb
+++ ruby/38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb
@@ -1,46 +1,37 @@
 
         
-                def content_tag_string(name, content, options, escape = true)
-          tag_options = tag_options(options, escape) if options
-          content     = ERB::Util.unwrapped_html_escape(content) if escape
-          '<#{name}#{tag_options}>#{PRE_CONTENT_STRINGS[name]}#{content}</#{name}>'.html_safe
+          Jekyll::Command.subclasses.each { |c| c.init_with_program(p) }
+    
+            css('p > code:first-child:last-child', 'td > code:first-child:last-child').each do |node|
+          next if node.previous.try(:content).present? || node.next.try(:content).present?
+          node.inner_html = node.inner_html.squish.gsub(/<br(\ \/)?>\s*/, '\n')
+          node.content = node.content.strip
+          node.name = 'pre' if node.content =~ /\s/
+          node.parent.before(node.parent.children).remove if node.parent.name == 'p'
         end
     
-      def test_check_parameters
-    with_test_route_set do
-      get '/'
-      assert_equal '', @controller.response.body
+          def additional_entries
+        entries = []
+    
+      def test_conflicting_encodings
+    Dir.mktmpdir(__method__.to_s) do |tmpdir|
+      tmp = File.join(tmpdir, 'x')
+      File.open(tmp, 'wb', :encoding => Encoding::EUC_JP) do |x|
+        assert_equal Encoding::EUC_JP, x.external_encoding
+      end
     end
   end
     
-          add_delivery_method :test, Mail::TestMailer
+      class SubclassX
+    attr_reader :key
+    def initialize(*)
+      @key = :value
+      super
     end
-    
-            def set_expected_mail
-          @expected = Mail.new
-          @expected.content_type ['text', 'plain', { 'charset' => charset }]
-          @expected.mime_version = '1.0'
-        end
-    
-          def warnings
-        @template.warnings
-      end
-    
-        find_union(segments, Project).includes(:namespace).order_id_desc
   end
+end
+
     
-        def recheck
-      pod = Pod.find(params[:pod_id])
-      pod.test_connection!
-    
-          def auth_user_unless_prompt_none!
-        prompt = params[:prompt]
-        if prompt && prompt.include?('none')
-          handle_prompt_none
-        elsif prompt && prompt.include?('login')
-          new_params = params.except('controller', 'action').permit!.to_h.merge(prompt: prompt.remove('login'))
-          reauthenticate(new_params)
-        else
-          authenticate_user!
-        end
-      end
+            def update
+          authorize! :update, @order, order_token
+          @address = find_address
