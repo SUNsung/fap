8edd75ab1b9b538d6c4c36d9193137aed43312ba
SUNsung <weatherius@gmail.com>
2018-04-15 15:34:30 +0300

    Rewrite [572PnbIzUn.Ek57MW82AAbGao57yg4zL613sPI57Cmx.py] File in: 2018-04-15 15:34:29

--- pyton/572PnbIzUn.Ek57MW82AAbGao57yg4zL613sPI57Cmx.py
+++ pyton/572PnbIzUn.Ek57MW82AAbGao57yg4zL613sPI57Cmx.py
@@ -1,45 +1,78 @@
 
         
-        import re
-    
-        def _real_extract(self, url):
-        # URLs end with [uploader name]/[uploader title]
-        # this title is whatever the user types in, and is rarely
-        # the proper song title.  Real metadata is in the api response
-        album_url_tag = self._match_id(url)
-        result = {'_type': 'playlist', 'entries': []}
-        # There is no one endpoint for album metadata - instead it is included/repeated in each song's metadata
-        # Therefore we don't know how many songs the album has and must infi-loop until failure
-        for track_no in itertools.count():
-            # Get song's metadata
-            api_response = self._download_json(
-                'http://www.audiomack.com/api/music/url/album/%s/%d?extended=1&_=%d'
-                % (album_url_tag, track_no, time.time()), album_url_tag,
-                note='Querying song information (%d)' % (track_no + 1))
+        # backwards compat, goes away in 1.0
+from .sessions import SecureCookieSession as Session
+json_available = True
+
     
     
-def prepare_url(value):
-    # Issue #1483: Make sure the URL always has a trailing slash
-    httpbin_url = value.url.rstrip('/') + '/'
-    
-        def test_text_response(self):
-        '''the text_response_server sends the given text'''
-        server = Server.text_response_server(
-            'HTTP/1.1 200 OK\r\n' +
-            'Content-Length: 6\r\n' +
-            '\r\nroflol'
-        )
-    
-        # Check urllib3 for compatibility.
-    major, minor, patch = urllib3_version  # noqa: F811
-    major, minor, patch = int(major), int(minor), int(patch)
-    # urllib3 >= 1.21.1, <= 1.22
-    assert major == 1
-    assert minor >= 21
-    assert minor <= 22
+class JSONTag(object):
+    '''Base class for defining type tags for :class:`TaggedJSONSerializer`.'''
     
     
-class ContentDecodingError(RequestException, BaseHTTPError):
-    '''Failed to decode response content'''
+@pytest.mark.skipif(not PY2, reason='This only works under Python 2.')
+def test_meta_path_loader_without_is_package(request, modules_tmpdir):
+    app = modules_tmpdir.join('unimportable.py')
+    app.write('import flask\napp = flask.Flask(__name__)')
     
-    from .structures import LookupDict
+    DEFAULT_DB = None
+SPACE = ' '
+COMMASPACE = ', '
+    
+            [['blorpie'],[ 'whoops' ] , [
+                                 ],\t'd-shtaeou',\r'd-nthiouh',
+        'i-vhbjkhnth', {'nifty':87}, {'morefield' :\tfalse,'field'
+            :'yes'}  ]
+           '''
+    
+            with self.assertRaises(RuntimeError):
+            range(0, 10)[:IX()]
+    
+        def test_pointers_bool(self):
+        # NULL pointers have a boolean False value, non-NULL pointers True.
+        self.assertEqual(bool(POINTER(c_int)()), False)
+        self.assertEqual(bool(pointer(c_int())), True)
+    
+            class MyCUnion(Union):
+            _fields_ = (('field', c_int),)
+        self.assertRaises(TypeError,
+                          MyCUnion.field.__set__, 'wrong type self', 42)
+    
+            self.assertEqual(True*1, 1)
+        self.assertEqual(False*1, 0)
+        self.assertIsNot(False*1, False)
+    
+    # Can't import these from paths.py because that uses `future` imports
+DIR_OF_CURRENT_SCRIPT = os.path.dirname( os.path.abspath( __file__ ) )
+DIR_OF_YCMD = os.path.join( DIR_OF_CURRENT_SCRIPT, '..', '..', 'third_party',
+                            'ycmd' )
+    
+    
+def namedtuple(typename, field_names):
+    '''Returns a new subclass of tuple with named fields.
+    
+        def shutdown(self, wait=True):
+        with self._shutdown_lock:
+            self._shutdown = True
+            self._work_queue.put(None)
+        if wait:
+            for t in self._threads:
+                t.join()
+    shutdown.__doc__ = _base.Executor.shutdown.__doc__
+
+    
+    
+def ExtractKeywordsFromGroup_Basic_test():
+  assert_that( syntax_parse._ExtractKeywordsFromGroup(
+                 syntax_parse.SyntaxGroup( '', [
+                   'foo bar',
+                   'zoo goo',
+                 ] ) ),
+               contains_inanyorder( 'foo', 'bar', 'zoo', 'goo' ) )
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
