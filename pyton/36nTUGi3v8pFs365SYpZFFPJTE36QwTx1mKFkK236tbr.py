f00255603bbfeb2433b164a02a1326a6457b0e92
SUNsung <weatherius@gmail.com>
2018-04-15 04:22:51 +0300

    Rewrite [36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py] File in: 2018-04-15 04:22:50

--- pyton/36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py
+++ pyton/36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py
@@ -1,73 +1,120 @@
 
         
-                def get_auth(self, username=None, password=None):
-            assert self.raw_auth == USERNAME
-            assert username == USERNAME
-            assert password is None
-            return basic_auth()
+        
+if len(sys.argv) <= 1:
+    print('Specify the version number as parameter')
+    sys.exit()
+version = sys.argv[1]
     
+    entry_template = textwrap.dedent('''
+    <entry>
+        <id>https://yt-dl.org/feed/youtube-dl-updates-feed/youtube-dl-@VERSION@</id>
+        <title>New version @VERSION@</title>
+        <link href='http://rg3.github.io/youtube-dl' />
+        <content type='xhtml'>
+            <div xmlns='http://www.w3.org/1999/xhtml'>
+                Downloads available at <a href='https://yt-dl.org/downloads/@VERSION@/'>https://yt-dl.org/downloads/@VERSION@/</a>
+            </div>
+        </content>
+        <author>
+            <name>The youtube-dl maintainers</name>
+        </author>
+        <updated>@TIMESTAMP@</updated>
+    </entry>
+    ''')
     
-httpie_info = packages.pop('httpie')
-print('''
-  url '{url}'
-  sha256 '{sha256}'
-'''.format(**httpie_info))
-    
-        ''')
-)
-positional.add_argument(
-    'method',
-    metavar='METHOD',
-    nargs=OPTIONAL,
-    default=None,
-    help='''
-    The HTTP method to be used for the request (GET, POST, PUT, DELETE, ...).
-    
-    if is_windows:
-    # Colors on Windows via colorama don't look that
-    # great and fruity seems to give the best result there
-    DEFAULT_STYLE = 'fruity'
-else:
-    DEFAULT_STYLE = 'solarized'
-    
-        def test_self_signed_server_cert_by_default_raises_ssl_error(
-            self,
-            httpbin_secure_untrusted):
-        with pytest.raises(SSLError):
-            http(httpbin_secure_untrusted.url + '/get')
-    
-        def __init__(self, *args, **kwargs):
-        kwargs['add_help'] = False
-        super(HTTPieArgumentParser, self).__init__(*args, **kwargs)
-    
-        return request('post', url, data=data, json=json, **kwargs)
-    
-                sock2.connect(address)
-            sock2.sendall(second_request)
-            sock2.close()
-    
-    import sys
-    
-    from . import utils
-from . import packages
-from .models import Request, Response, PreparedRequest
-from .api import request, get, head, post, patch, put, delete, options
-from .sessions import session, Session
-from .status_codes import codes
-from .exceptions import (
-    RequestException, Timeout, URLRequired,
-    TooManyRedirects, HTTPError, ConnectionError,
-    FileModeWarning, ConnectTimeout, ReadTimeout
-)
-    
-    _init()
+    if __name__ == '__main__':
+    unittest.main()
 
     
-    about = {}
-with open(os.path.join(here, 'requests', '__version__.py'), 'r', 'utf-8') as f:
-    exec(f.read(), about)
+        class Plugin(AuthPlugin):
+        auth_type = 'test-require-false-yet-provided'
+        auth_require = False
     
-        forbidden_extensions = ['html', 'htm'] if results.nohtml else []
     
-    from r2.controllers.api_docs import api_doc, api_section
-from r2.controllers.oauth2 import allow_oauth2_access
+def get_info(package_name):
+    api_url = 'https://pypi.python.org/pypi/{}/json'.format(package_name)
+    resp = requests.get(api_url).json()
+    hasher = hashlib.sha256()
+    for release in resp['urls']:
+        download_url = release['url']
+        if download_url.endswith('.tar.gz'):
+            hasher.update(requests.get(download_url).content)
+            return {
+                'name': package_name,
+                'url': download_url,
+                'sha256': hasher.hexdigest(),
+            }
+    else:
+        raise RuntimeError(
+            '{}: download not found: {}'.format(package_name, resp))
+    
+    
+FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
+FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
+JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
+BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
+    
+        exc = Timeout('Request timed out')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
+    assert ret == ExitStatus.ERROR_TIMEOUT
+    assert error_msg == 'Request timed out (30s).'
+
+    
+    
+def test_follow_all_redirects_shown(httpbin):
+    r = http('--follow', '--all', httpbin.url + '/redirect/2')
+    assert r.count('HTTP/1.1') == 3
+    assert r.count('HTTP/1.1 302 FOUND', 2)
+    assert HTTP_OK in r
+    
+        @property
+    def has_finished(self):
+        return self.time_finished is not None
+    
+    output_options.add_argument(
+    '--print', '-p',
+    dest='output_options',
+    metavar='WHAT',
+    help='''
+    String specifying what the output should contain:
+    
+        def test_cert_and_key(self, httpbin_secure):
+        r = http(httpbin_secure + '/get',
+                 '--cert', CLIENT_CERT,
+                 '--cert-key', CLIENT_KEY)
+        assert HTTP_OK in r
+    
+    from httpie import __version__
+from httpie.compat import is_windows
+    
+    
+SESSIONS_DIR_NAME = 'sessions'
+DEFAULT_SESSIONS_DIR = os.path.join(DEFAULT_CONFIG_DIR, SESSIONS_DIR_NAME)
+VALID_SESSION_NAME_PATTERN = re.compile('^[a-zA-Z0-9_.-]+$')
+# Request headers starting with these prefixes won't be stored in sessions.
+# They are specific to each request.
+# http://en.wikipedia.org/wiki/List_of_HTTP_header_fields#Requests
+SESSION_IGNORED_HEADER_PREFIXES = ['Content-', 'If-']
+    
+    # If true, the current module name will be prepended to all description
+# unit titles (such as .. function::).
+#add_module_names = True
+    
+        @mock.patch('certbot.notify.smtplib.LMTP')
+    @mock.patch('certbot.notify.subprocess.Popen')
+    def test_everything_fails(self, mock_popen, mock_lmtp):
+        from certbot.notify import notify
+        lmtp_obj = mock.MagicMock()
+        mock_lmtp.return_value = lmtp_obj
+        lmtp_obj.sendmail.side_effect = socket.error(17)
+        proc = mock.MagicMock()
+        mock_popen.return_value = proc
+        proc.communicate.side_effect = OSError('What we have here is a '
+                                               'failure to communicate.')
+        self.assertFalse(notify('Goose', 'auntrhody@example.com',
+                                'The old grey goose is dead.'))
+        self.assertEqual(lmtp_obj.sendmail.call_count, 1)
+        self.assertEqual(proc.communicate.call_count, 1)
