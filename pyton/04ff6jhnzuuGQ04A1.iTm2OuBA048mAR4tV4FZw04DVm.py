0a1dc0c3a077befcad6d5f69ee47577dc7e7ea03
SUNsung <weatherius@gmail.com>
2018-04-17 02:13:00 +0300

    Rewrite [04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py] File in: 2018-04-17 02:12:59

--- pyton/04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py
+++ pyton/04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py
@@ -1,75 +1,79 @@
 
         
-                content_length = int(response.getheader('Content-Length', 0))
-        content_range = response.getheader('Content-Range', '')
-        if content_range:
-            start, end, length = tuple(int(x) for x in re.search(r'bytes (\d+)-(\d+)/(\d+)', content_range).group(1, 2, 3))
-        else:
-            start, end, length = 0, content_length-1, content_length
+        def shorten_title(title):
+    m1 = re.search('[[0-9]*]', title)
+    m2 = re.search(''.*'', title)
+    if m1:
+        title = m1.group(0)
+    if m2:
+        title = ' '.join((title, m2.group(0)))   
+    return title[:50] + ' [...]'    
     
-        version_string = 'Version:%d-%d; Build:%d; Platform:%d; CSD:%s; ServicePack:%d-%d; Suite:%d; ProductType:%d' %  (
-        os_version.dwMajorVersion, os_version.dwMinorVersion,
-        os_version.dwBuildNumber,
-        os_version.dwPlatformId,
-        os_version.szCSDVersion,
-        os_version.wServicePackMajor, os_version.wServicePackMinor,
-        os_version.wSuiteMask,
-        os_version.wReserved
+    from __future__ import with_statement
+import atexit
+import multiprocessing
+import threading
+import weakref
+import sys
+    
+    
+def KeywordsFromSyntaxListOutput_Basic_test():
+  assert_that( syntax_parse._KeywordsFromSyntaxListOutput( '''
+foogroup xxx foo bar
+             zoo goo
+             links to Statement''' ),
+              contains_inanyorder( 'foo', 'bar', 'zoo', 'goo' ) )
+    
+    
+def setUpPackage():
+  # We treat warnings as errors in our tests because warnings raised inside Vim
+  # will interrupt user workflow with a traceback and we don't want that.
+  warnings.filterwarnings( 'error' )
+  # We ignore warnings from nose as we are not interested in them.
+  warnings.filterwarnings( 'ignore', module = 'nose' )
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+        # when mako loads a previously compiled template file from its cache, it
+    # doesn't check that the original template path matches the current path.
+    # in the event that a new plugin defines a template overriding a reddit
+    # template, unless the mtime newer, mako doesn't update the compiled
+    # template. as a workaround, this makes mako store compiled templates with
+    # the original path in the filename, forcing it to update with the path.
+    if 'cache_dir' in app_conf:
+        module_directory = os.path.join(app_conf['cache_dir'], 'templates')
+    
+    def api_type(subtype = ''):
+    return 'api-' + subtype if subtype else 'api'
+    
+        FRIEND_JSON_SPEC = VValidatedJSON.PartialObject({
+        'name': VAccountByName('name'),
+        'note': VLength('note', 300),
+    })
+    FRIEND_JSON_VALIDATOR = VValidatedJSON('json', spec=FRIEND_JSON_SPEC,
+                                           body=True)
+    @require_oauth2_scope('subscribe')
+    @validate(
+        VUser(),
+        friend=VAccountByName('username'),
+        notes_json=FRIEND_JSON_VALIDATOR,
     )
+    @api_doc(api_section.users, json_model=FRIEND_JSON_VALIDATOR,
+             uri='/api/v1/me/friends/{username}')
+    def PUT_friends(self, friend, notes_json):
+        '''Create or update a 'friend' relationship.
     
-    
-    def recover(self, input, re):
-        '''
-        Recover from an error found on the input stream.  This is
-        for NoViableAlt and mismatched symbol exceptions.  If you enable
-        single token insertion and deletion, this will usually not
-        handle mismatched symbol exceptions but there could be a mismatched
-        token that the match() routine could not recover from.
-        '''
-        
-        # PROBLEM? what if input stream is not the same as last time
-        # perhaps make lastErrorIndex a member of input
-        if self._state.lastErrorIndex == input.index():
-            # uh oh, another error at same token index; must be a case
-            # where LT(1) is in the recovery token set so nothing is
-            # consumed; consume a single token so at least to prevent
-            # an infinite loop; this is a failsafe.
-            input.consume()
-    
-        def browse_file(self):
-        filetypes = [
-            ('HTML Files', '*.htm *.html', 'TEXT'),
-            ('PDF Files', '*.pdf', 'TEXT'),
-            ('Windows Help Files', '*.chm'),
-            ('Text Files', '*.txt', 'TEXT'),
-            ('All Files', '*')]
-        path = self.pathvar.get()
-        if path:
-            dir, base = os.path.split(path)
-        else:
-            base = None
-            if platform[:3] == 'win':
-                dir = os.path.join(os.path.dirname(executable), 'Doc')
-                if not os.path.isdir(dir):
-                    dir = os.getcwd()
-            else:
-                dir = os.getcwd()
-        file = self.askfilename(filetypes, dir, base)
-        if file:
-            self.pathvar.set(file)
-    
-        def test_setitem(self):
-        n = 200
-        d = deque(range(n))
-        for i in range(n):
-            d[i] = 10 * i
-        self.assertEqual(list(d), [10*i for i in range(n)])
-        l = list(d)
-        for i in range(1-n, 0, -1):
-            d[i] = 7*i
-            l[i] = 7*i
-        self.assertEqual(list(d), l)
-    
-    This file contains one class, called ColorDB, and several utility functions.
-The class must be instantiated by the get_colordb() function in this file,
-passing it a filename to read a database out of.
+        @validate(buttonimage = VInt('i', 0, 14),
+              title = nop('title'),
+              url = VSanitizedUrl('url'),
+              newwindow = VBoolean('newwindow', default = False),
+              styled = VBoolean('styled', default=True))
+    def GET_button_lite(self, buttonimage, title, url, styled, newwindow):
+        c.user = UnloggedUser([c.lang])
+        c.user_is_loggedin = False
+        c.render_style = 'js'
