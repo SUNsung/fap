7aebf71b858567ba5c9df9b4d86476cab2b5d0c8
SUNsung <weatherius@gmail.com>
2018-04-16 06:01:53 +0300

    Rewrite [28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp] File in: 2018-04-16 06:01:53

--- cccccc/28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp
+++ cccccc/28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp
@@ -1,179 +1,213 @@
 
         
-        #endif  // GTEST_INCLUDE_GTEST_GTEST_DEATH_TEST_H_
-
-    
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12, typename T13, typename T14, typename T15,
-    typename T16, typename T17, typename T18, typename T19, typename T20,
-    typename T21, typename T22, typename T23, typename T24, typename T25,
-    typename T26, typename T27, typename T28, typename T29, typename T30,
-    typename T31, typename T32, typename T33, typename T34, typename T35,
-    typename T36, typename T37, typename T38, typename T39, typename T40,
-    typename T41>
-internal::ValueArray41<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
-    T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
-    T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39, T40,
-    T41> Values(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9,
-    T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15, T16 v16, T17 v17,
-    T18 v18, T19 v19, T20 v20, T21 v21, T22 v22, T23 v23, T24 v24, T25 v25,
-    T26 v26, T27 v27, T28 v28, T29 v29, T30 v30, T31 v31, T32 v32, T33 v33,
-    T34 v34, T35 v35, T36 v36, T37 v37, T38 v38, T39 v39, T40 v40, T41 v41) {
-  return internal::ValueArray41<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
-      T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25,
-      T26, T27, T28, T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39,
-      T40, T41>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14,
-      v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27, v28,
-      v29, v30, v31, v32, v33, v34, v35, v36, v37, v38, v39, v40, v41);
-}
-    
-    TEST_P(DerivedTest, DoesBlah) {
-  // GetParam works just the same here as if you inherit from TestWithParam.
-  EXPECT_TRUE(foo.Blah(GetParam()));
-}
-    
-     private:
-  std::string file_;
-  int line_;
-  int index_;
-  int write_fd_;
-    
-    
-    {  const T1 v1_;
-  const T2 v2_;
-  const T3 v3_;
-  const T4 v4_;
-  const T5 v5_;
-  const T6 v6_;
-  const T7 v7_;
-  const T8 v8_;
-  const T9 v9_;
-  const T10 v10_;
-  const T11 v11_;
-  const T12 v12_;
-  const T13 v13_;
-  const T14 v14_;
-  const T15 v15_;
-  const T16 v16_;
-  const T17 v17_;
-  const T18 v18_;
-  const T19 v19_;
-  const T20 v20_;
-  const T21 v21_;
-  const T22 v22_;
-  const T23 v23_;
-  const T24 v24_;
-  const T25 v25_;
-  const T26 v26_;
-  const T27 v27_;
-  const T28 v28_;
-  const T29 v29_;
-  const T30 v30_;
-  const T31 v31_;
-  const T32 v32_;
-  const T33 v33_;
-  const T34 v34_;
-  const T35 v35_;
-  const T36 v36_;
-  const T37 v37_;
-  const T38 v38_;
-  const T39 v39_;
-  const T40 v40_;
-  const T41 v41_;
-  const T42 v42_;
-  const T43 v43_;
-};
-    
-    // Typed tests need <typeinfo> and variadic macros, which GCC, VC++ 8.0,
-// Sun Pro CC, IBM Visual Age, and HP aCC support.
-#if defined(__GNUC__) || (_MSC_VER >= 1400) || defined(__SUNPRO_CC) || \
-    defined(__IBMCPP__) || defined(__HP_aCC)
-# define GTEST_HAS_TYPED_TEST 1
-# define GTEST_HAS_TYPED_TEST_P 1
+            DXGI_SWAP_CHAIN_DESC sd;
+    ZeroMemory( &sd, sizeof( sd ) );
+    sd.BufferCount = 1;
+    sd.BufferDesc.Width = width;
+    sd.BufferDesc.Height = height;
+#ifdef CHECK_NV12
+    sd.BufferDesc.Format = DXGI_FORMAT_NV12;
+#else
+    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 #endif
+    sd.BufferDesc.RefreshRate.Numerator = 60;
+    sd.BufferDesc.RefreshRate.Denominator = 1;
+    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    sd.OutputWindow = NULL; //g_hWnd;
+    sd.SampleDesc.Count = 1;
+    sd.SampleDesc.Quality = 0;
+    sd.Windowed = TRUE;
     
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12, typename T13, typename T14, typename T15,
-    typename T16, typename T17, typename T18, typename T19, typename T20,
-    typename T21, typename T22, typename T23, typename T24, typename T25,
-    typename T26, typename T27, typename T28, typename T29>
-struct Types29 {
-  typedef T1 Head;
-  typedef Types28<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15,
-      T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
-      T29> Tail;
-};
+    //
+// BEGIN OF CUSTOM FUNCTIONS
+//
     
-    // The TypeList template makes it possible to use either a single type
-// or a Types<...> list in TYPED_TEST_CASE() and
-// INSTANTIATE_TYPED_TEST_CASE_P().
+    #ifndef incl_HPHP_WORKLOAD_STATS_H_
+#define incl_HPHP_WORKLOAD_STATS_H_
     
-    
-    {    return -1;
-  }
-    
-    // Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
-int Factorial(int n) {
-  int result = 1;
-  for (int i = 1; i <= n; i++) {
-    result *= i;
-  }
+    struct PageletServer {
+  static bool Enabled();
+  static void Restart();
+  static void Stop();
     }
     
-    // This function ensure a directory exists, by recursively
-// creating dirs for any parts of the path that don't exist yet.
-inline void EnsureDirExists(const std::string &filepath) {
-  auto parent = StripFileName(filepath);
-  if (parent.length()) EnsureDirExists(parent);
-    // clang-format off
-  #ifdef _WIN32
-    (void)_mkdir(filepath.c_str());
-  #else
-    mkdir(filepath.c_str(), S_IRWXU|S_IRGRP|S_IXGRP);
-  #endif
-  // clang-format on
+    VcallArgsId Vunit::makeVcallArgs(VcallArgs&& args) {
+  VcallArgsId i(vcallArgs.size());
+  vcallArgs.emplace_back(std::move(args));
+  return i;
 }
     
-    // Abort the program after logging the mesage if the given condition is not
-// true. Otherwise, do nothing.
-#define GRPC_CODEGEN_CHECK(x)                                            \
-  (x) ? (void)0                                                          \
-      : LogMessageVoidify() & LogHelper(&std::cerr).get_os()             \
-                                  << 'CHECK FAILED: ' << __FILE__ << ':' \
-                                  << __LINE__ << ': '
-    
-    namespace flatbuffers {
+      bool ret = true;
+  bitmask* run_nodes = numa_get_run_node_mask();
+  bitmask* mem_nodes = numa_get_mems_allowed();
+  for (int i = 0; i <= max_node; i++) {
+    if (!numa_bitmask_isbitset(run_nodes, i) ||
+        !numa_bitmask_isbitset(mem_nodes, i)) {
+      // Only deal with the case of a contiguous set of nodes where we can
+      // run/allocate memory on each node.
+      ret = false;
+      break;
     }
-    
-    class AbstractAuthResolver : public AuthResolver {
-public:
-  AbstractAuthResolver();
-    }
-    
-      BtRequestFactory* getBtRequestFactory() const { return requestFactory_; }
-    
-    #include 'DiskWriter.h'
-#include <string>
-    
-    bool AbstractProxyRequestCommand::executeInternal()
-{
-  // socket->setBlockingMode();
-  if (httpConnection_->sendBufferIsEmpty()) {
-    auto httpRequest = make_unique<HttpRequest>();
-    httpRequest->setUserAgent(getOption()->get(PREF_USER_AGENT));
-    httpRequest->setRequest(getRequest());
-    httpRequest->setProxyRequest(proxyRequest_);
-    }
-    }
-    
-      SecIdentityRef id;
-  while (SecIdentitySearchCopyNext(search, &id) == errSecSuccess) {
-    if (!checkIdentity(id, fp, ht)) {
-      continue;
-    }
-    A2_LOG_INFO('Found cert with matching fingerprint');
-    credentials_ = id;
-    return true;
+    numa_node_set |= (uint32_t)1 << i;
+    numa_num_nodes++;
   }
+  numa_bitmask_free(run_nodes);
+  numa_bitmask_free(mem_nodes);
+    
+    #include 'hphp/runtime/vm/jit/normalized-instruction.h'
+#include 'hphp/runtime/vm/jit/switch-profile.h'
+#include 'hphp/runtime/vm/jit/target-profile.h'
+    
+    //////////////////////////////////////////////////////////////////////
+    
+      void undo() {
+    m_cb.setFrontier(m_oldFrontier);
+  }
+    
+    
+    {
+    {  if (c1headc2head == max) {
+    // flip c1
+    std::reverse(c1->targets.begin(), c1->targets.end());
+  } else if (c1headc2tail == max) {
+    // flip c1 c2
+    std::reverse(c1->targets.begin(), c1->targets.end());
+    std::reverse(c2->targets.begin(), c2->targets.end());
+  } else if (c1tailc2tail == max) {
+    // flip c2
+    std::reverse(c2->targets.begin(), c2->targets.end());
+  }
+}
+}
+    
+    
+    {  if (n == 1) {
+    if (imm_s == 0x3F) {
+      return 0;
+    }
+    uint64_t bits = (1UL << (imm_s + 1)) - 1;
+    return RotateRight(bits, imm_r, 64);
+  } else {
+    if ((imm_s >> 1) == 0x1F) {
+      return 0;
+    }
+    for (int width = 0x20; width >= 0x2; width >>= 1) {
+      if ((imm_s & width) == 0) {
+        int mask = width - 1;
+        if ((imm_s & mask) == mask) {
+          return 0;
+        }
+        uint64_t bits = (1UL << ((imm_s & mask) + 1)) - 1;
+        return RepeatBitsAcrossReg(reg_size,
+                                   RotateRight(bits, imm_r & mask, width),
+                                   width);
+      }
+    }
+  }
+  not_reached();
+  return 0;
+}
+    
+    namespace HPHP {
+    }
+    
+        do 
+    {
+        if (argc == 1)
+        {
+            double arg0;
+            ok &= luaval_to_number(tolua_S, 2,&arg0, 'cc.PhysicsBody:create');
+            if (!ok) { break; }
+            cocos2d::PhysicsBody* ret = cocos2d::PhysicsBody::create(arg0);
+            object_to_luaval<cocos2d::PhysicsBody>(tolua_S, 'cc.PhysicsBody',(cocos2d::PhysicsBody*)ret);
+            return 1;
+        }
+    } while (0);
+    ok  = true;
+    do 
+    {
+        if (argc == 0)
+        {
+            cocos2d::PhysicsBody* ret = cocos2d::PhysicsBody::create();
+            object_to_luaval<cocos2d::PhysicsBody>(tolua_S, 'cc.PhysicsBody',(cocos2d::PhysicsBody*)ret);
+            return 1;
+        }
+    } while (0);
+    ok  = true;
+    do 
+    {
+        if (argc == 2)
+        {
+            double arg0;
+            ok &= luaval_to_number(tolua_S, 2,&arg0, 'cc.PhysicsBody:create');
+            if (!ok) { break; }
+            double arg1;
+            ok &= luaval_to_number(tolua_S, 3,&arg1, 'cc.PhysicsBody:create');
+            if (!ok) { break; }
+            cocos2d::PhysicsBody* ret = cocos2d::PhysicsBody::create(arg0, arg1);
+            object_to_luaval<cocos2d::PhysicsBody>(tolua_S, 'cc.PhysicsBody',(cocos2d::PhysicsBody*)ret);
+            return 1;
+        }
+    } while (0);
+    ok  = true;
+    luaL_error(tolua_S, '%s has wrong number of arguments: %d, was expecting %d', 'cc.PhysicsBody:create',argc, 2);
+    return 0;
+#if COCOS2D_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,'#ferror in function 'lua_cocos2dx_physics_PhysicsBody_create'.',&tolua_err);
+#endif
+    return 0;
+}
+int lua_cocos2dx_physics_PhysicsBody_createEdgeBox(lua_State* tolua_S)
+{
+    int argc = 0;
+    bool ok  = true;
+    
+        GLfloat    glVertices[] = 
+    {
+        p1.x * mRatio, p1.y * mRatio,
+        p2.x * mRatio, p2.y * mRatio
+    };
+    glVertexAttribPointer(GLProgram::VERTEX_ATTRIB_POSITION, 2, GL_FLOAT, GL_FALSE, 0, glVertices);
+    
+    			b2FixtureDef sd1;
+			sd1.shape = &poly1;
+			sd1.density = 4.0f;
+    
+    	void Step(Settings* settings)
+	{
+		m_debugDraw.DrawString(5, m_textLine, 'Keys: left = a, brake = s, right = d, hz down = q, hz up = e');
+		m_textLine += DRAW_STRING_NEW_LINE;
+		m_debugDraw.DrawString(5, m_textLine, 'frequency = %g hz, damping ratio = %g', m_hz, m_zeta);
+		m_textLine += DRAW_STRING_NEW_LINE;
+    }
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+    // bool JNU_Jstring2Wstring( JNIEnv* _env, const jstring jstr, std::wstring& wstr); //in linux sizeof(wchar_t)==4 but sizeof(jchar)==2
+wchar_t* JNU_Jstring2Wchar(JNIEnv* _env, const jstring jstr);
+void JNU_FreeWchar(JNIEnv* _env, jstring str, wchar_t* wchar);
+jstring JNU_Wstring2Jstring(JNIEnv* _env, const std::wstring& wstr);
+jstring JNU_Wchar2JString(JNIEnv* _env, wchar_t* wchar);
+    
+    ScopeJEnv::ScopeJEnv(JavaVM* jvm, jint _capacity)
+    : vm_(jvm), env_(NULL), we_attach_(false), status_(0) {
+    ASSERT(jvm);
+    do {
+        env_ = (JNIEnv*)pthread_getspecific(g_env_key);
+        
+        if (NULL != env_) {
+            break;
+        }
+        
+        status_ = vm_->GetEnv((void**) &env_, JNI_VERSION_1_6);
+    }
+    }
