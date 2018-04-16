3a61db42f6edc44c55c3b72aea76ef82969d3843
SUNsung <weatherius@gmail.com>
2018-04-16 16:43:31 +0300

    Rewrite [50O19NO6i0MmA50YulTflKI0WA50bkB5NDmtF.I50jgJ.rb] File in: 2018-04-16 16:43:31

--- ruby/50O19NO6i0MmA50YulTflKI0WA50bkB5NDmtF.I50jgJ.rb
+++ ruby/50O19NO6i0MmA50YulTflKI0WA50bkB5NDmtF.I50jgJ.rb
@@ -1,110 +1,53 @@
 
         
-                css('p > code:first-child:last-child', 'td > code:first-child:last-child').each do |node|
-          next if node.previous.try(:content).present? || node.next.try(:content).present?
-          node.inner_html = node.inner_html.squish.gsub(/<br(\ \/)?>\s*/, '\n')
-          node.content = node.content.strip
-          node.name = 'pre' if node.content =~ /\s/
-          node.parent.before(node.parent.children).remove if node.parent.name == 'p'
-        end
+            brew cask install mactex
+    EOS
+  when 'pip' then <<-EOS.undent
+    Homebrew provides pip via: `brew install python`. However you will then
+    have two Pythons installed on your Mac, so alternatively you can install
+    pip via the instructions at:
     
-              if %w(Events Sync).include?(type)
-            name.prepend 'Backbone.'
-          elsif type == 'History'
-            name.prepend 'Backbone.history.'
-          elsif name == 'extend'
-            name.prepend '#{type}.'
-          elsif name.start_with? 'constructor'
-            name = type
-          elsif type != 'Utility'
-            name.prepend '#{type.downcase}.'
-          end
-    
-            css('h6').each do |node|
-          node.name = 'h4'
-        end
-    
-      # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-end
-
-    
-      # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protection = false
-    
-    namespace :bower do
-    
-        if run? && ARGV.any?
-      require 'optparse'
-      OptionParser.new { |op|
-        op.on('-p port',   'set the port (default is 4567)')                { |val| set :port, Integer(val) }
-        op.on('-o addr',   'set the host (default is #{bind})')             { |val| set :bind, val }
-        op.on('-e env',    'set the environment (default is development)')  { |val| set :environment, val.to_sym }
-        op.on('-s server', 'specify rack server/handler (default is thin)') { |val| set :server, val }
-        op.on('-q',        'turn on quiet mode (default is off)')           {       set :quiet, true }
-        op.on('-x',        'turn on the mutex lock (default is off)')       {       set :lock, true }
-      }.parse!(ARGV.dup)
+      # This allows generic Altivec PPC bottles to be supported in some
+  # formulae, while also allowing specific bottles in others; e.g.,
+  # sometimes a formula has just :tiger_altivec, other times it has
+  # :tiger_g4, :tiger_g5, etc.
+  def find_altivec_tag(tag)
+    if tag.to_s =~ /(\w+)_(g4|g4e|g5)$/
+      altivec_tag = '#{$1}_altivec'.to_sym
+      altivec_tag if key?(altivec_tag)
     end
   end
     
-          def remove_bad_cookies(request, response)
-        return if bad_cookies.empty?
-        paths = cookie_paths(request.path)
-        bad_cookies.each do |name|
-          paths.each { |path| response.set_cookie name, empty_cookie(request.host, path) }
+            if ARGV.git?
+          puts 'This directory is now a git repo. Make your changes and then use:'
+          puts '  git diff | pbcopy'
+          puts 'to copy the diff to the clipboard.'
         end
-      end
     
-          class << self
-        alias escape_url escape
-        public :escape_html
-      end
-    
-      it 'denies requests with a changing User-Agent header' do
-    session = {:foo => :bar}
-    get '/', {}, 'rack.session' => session, 'HTTP_USER_AGENT' => 'a'
-    get '/', {}, 'rack.session' => session, 'HTTP_USER_AGENT' => 'b'
-    expect(session).to be_empty
-  end
-    
-      it 'should not override the header if already set X-Content-Type-Options' do
-    mock_app with_headers('X-Content-Type-Options' => 'sniff')
-    expect(get('/', {}, 'wants' => 'text/html').headers['X-Content-Type-Options']).to eq('sniff')
+      def snow_leopard_64?
+    MacOS.prefer_64_bit?
   end
 end
-
     
-      it 'should not leak changes to env' do
-    klass    = described_class
-    detector = Struct.new(:app) do
-      def call(env)
-        was = env.dup
-        res = app.call(env)
-        was.each do |k,v|
-          next if env[k] == v
-          fail 'env[#{k.inspect}] changed from #{v.inspect} to #{env[k].inspect}'
-        end
-        res
-      end
-    end
+        groups << @user.authorized_groups.visible_to_user(current_user) if current_user
+    groups << @user.authorized_groups.public_to_user(current_user)
     
-            def each_unnecessary_space_match(node, &blk)
-          each_match_range(
-            contents_range(node),
-            MULTIPLE_SPACES_BETWEEN_ITEMS_REGEX,
-            &blk
-          )
-        end
-      end
-    end
+      # Finds the projects belonging to the user in '@user', limited to either
+  # public projects or projects visible to the given user.
+  #
+  # current_user - When given the list of projects is limited to those only
+  #                visible by this user.
+  #
+  # Returns an ActiveRecord::Relation.
+  def execute(current_user = nil)
+    segments = all_projects(current_user)
+    
+      # Configure static asset server for tests with Cache-Control for performance.
+  if config.respond_to?(:serve_static_files)
+    # rails >= 4.2
+    config.serve_static_files = true
+  elsif config.respond_to?(:serve_static_assets)
+    # rails < 4.2
+    config.serve_static_assets = true
   end
-end
-
-    
-                previous.push(when_node.conditions)
-          end
-        end
-    
-        expect(cop.offenses.size).to eq(1)
-    expect(cop.messages).to eq(['Prefer `to_sym` over `intern`.'])
-    expect(cop.highlights).to eq(%w[intern])
+  config.static_cache_control = 'public, max-age=3600'
