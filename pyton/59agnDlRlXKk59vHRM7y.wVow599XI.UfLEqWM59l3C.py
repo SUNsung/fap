1b22a55211419b21756315e70115a48c4bd666a3
SUNsung <weatherius@gmail.com>
2018-04-16 09:49:04 +0300

    Rewrite [59agnDlRlXKk59vHRM7y.wVow599XI.UfLEqWM59l3C.py] File in: 2018-04-16 09:49:04

--- pyton/59agnDlRlXKk59vHRM7y.wVow599XI.UfLEqWM59l3C.py
+++ pyton/59agnDlRlXKk59vHRM7y.wVow599XI.UfLEqWM59l3C.py
@@ -1,110 +1,108 @@
 
         
-        
-def get_new_command(command):
-    dir = shell.quote(_tar_file(command.script_parts)[1])
-    return shell.and_('mkdir -p {dir}', '{cmd} -C {dir}') \
-        .format(dir=dir, cmd=command.script)
+            # To avoid having to always use the '|safe' filter in flatpage templates,
+    # mark the title and content as already safe (since they are raw HTML
+    # content in the first place).
+    f.title = mark_safe(f.title)
+    f.content = mark_safe(f.content)
     
+        def test_create_entries(self):
+        self.dialog.top = self.root
+        self.dialog.row = 0
+        self.engine.setpat('hello')
+        self.dialog.create_entries()
+        self.assertIn(self.dialog.ent.get(), 'hello')
     
-@pytest.mark.functional
-def test_refuse_with_confirmation(proc, TIMEOUT):
-    refuse_with_confirmation(proc, TIMEOUT)
+        def test_delitem(self):
+        n = 500         # O(n**2) test, don't make this too big
+        d = deque(range(n))
+        self.assertRaises(IndexError, d.__delitem__, -n-1)
+        self.assertRaises(IndexError, d.__delitem__, n)
+        for i in range(n):
+            self.assertEqual(len(d), n-i)
+            j = random.randrange(-len(d), len(d))
+            val = d[j]
+            self.assertIn(val, d)
+            del d[j]
+            self.assertNotIn(val, d)
+        self.assertEqual(len(d), 0)
     
-    - Preprocessing
-    Sequence Preprocessing
-    Text Preprocessing
-    Image Preprocessing
+        def set_sequences(self, sequences):
+        '''Set sequences using the given name-to-key-list dictionary.'''
+        f = open(os.path.join(self._path, '.mh_sequences'), 'r+', encoding='ASCII')
+        try:
+            os.close(os.open(f.name, os.O_WRONLY | os.O_TRUNC))
+            for name, keys in sequences.items():
+                if len(keys) == 0:
+                    continue
+                f.write(name + ':')
+                prev = None
+                completing = False
+                for key in sorted(set(keys)):
+                    if key - 1 == prev:
+                        if not completing:
+                            completing = True
+                            f.write('-')
+                    elif completing:
+                        completing = False
+                        f.write('%s %s' % (prev, key))
+                    else:
+                        f.write(' %s' % key)
+                    prev = key
+                if completing:
+                    f.write(str(prev) + '\n')
+                else:
+                    f.write('\n')
+        finally:
+            _sync_close(f)
     
-        (X_train, y_train), (X_test, y_test) = get_test_data(num_train=train_samples,
-                                                         num_test=test_samples,
-                                                         input_shape=(input_dim,),
-                                                         classification=True,
-                                                         num_classes=num_classes)
-    y_test = np_utils.to_categorical(y_test)
-    y_train = np_utils.to_categorical(y_train)
+    def _parse_overview_fmt(lines):
+    '''Parse a list of string representing the response to LIST OVERVIEW.FMT
+    and return a list of header/metadata names.
+    Raises NNTPDataError if the response is not compliant
+    (cf. RFC 3977, section 8.4).'''
+    fmt = []
+    for line in lines:
+        if line[0] == ':':
+            # Metadata name (e.g. ':bytes')
+            name, _, suffix = line[1:].partition(':')
+            name = ':' + name
+        else:
+            # Header name (e.g. 'Subject:' or 'Xref:full')
+            name, _, suffix = line.partition(':')
+        name = name.lower()
+        name = _OVERVIEW_FMT_ALTERNATIVES.get(name, name)
+        # Should we do something with the suffix?
+        fmt.append(name)
+    defaults = _DEFAULT_OVERVIEW_FMT
+    if len(fmt) < len(defaults):
+        raise NNTPDataError('LIST OVERVIEW.FMT response too short')
+    if fmt[:len(defaults)] != defaults:
+        raise NNTPDataError('LIST OVERVIEW.FMT redefines default fields')
+    return fmt
     
+        whatToShow = NodeFilter.SHOW_ALL
     
-@keras_test
-def test_locallyconnected_1d():
-    num_samples = 2
-    num_steps = 8
-    input_dim = 5
-    filter_length = 3
-    filters = 4
-    padding = 'valid'
-    strides = 1
+        print('$(temp_dir):')
+    print(r'  if not exist $(temp_dir)\. mkdir $(temp_dir)')
+    print()
     
-        for data_format in ['channels_first', 'channels_last']:
-        if data_format == 'channels_first':
-            shape = (3, 5, 5)
-            target_shape = (5, 5, 3)
-            prev_shape = (2, 3, 2)
-            flip = lambda x: np.flip(np.flip(x, axis=2), axis=3)
-            transpose = lambda x: np.transpose(x, (0, 2, 3, 1))
-            target_data_format = 'channels_last'
-        elif data_format == 'channels_last':
-            shape = (5, 5, 3)
-            target_shape = (3, 5, 5)
-            prev_shape = (2, 2, 3)
-            flip = lambda x: np.flip(np.flip(x, axis=1), axis=2)
-            transpose = lambda x: np.transpose(x, (0, 3, 1, 2))
-            target_data_format = 'channels_first'
+        def __init__(self, func, args, kwds):
+        self.gen = func(*args, **kwds)
+        self.func, self.args, self.kwds = func, args, kwds
+        # Issue 19330: ensure context manager instances have good docstrings
+        doc = getattr(func, '__doc__', None)
+        if doc is None:
+            doc = type(self).__doc__
+        self.__doc__ = doc
+        # Unfortunately, this still doesn't provide good help output when
+        # inspecting the created context manager instances, since pydoc
+        # currently bypasses the instance docstring and shows the docstring
+        # for the class instead.
+        # See http://bugs.python.org/issue19404 for more details.
     
-    
-def get_test_values():
-    return [0.1, 0.5, 3, 8, 1e-7]
-    
-    
-@keras_test
-def test_sequential_regression():
-    # start with a basic example of using a Sequential model
-    # inside the functional API
-    seq = models.Sequential()
-    seq.add(layers.Dense(10, input_shape=(10,)))
-    
-                # test for return state:
-            x = Input(batch_shape=inputs.shape)
-            kwargs = {'data_format': data_format,
-                      'return_sequences': return_sequences,
-                      'return_state': True,
-                      'stateful': True,
-                      'filters': filters,
-                      'kernel_size': (num_row, num_col),
-                      'padding': 'valid'}
-            layer = convolutional_recurrent.ConvLSTM2D(**kwargs)
-            layer.build(inputs.shape)
-            outputs = layer(x)
-            output, states = outputs[0], outputs[1:]
-            assert len(states) == 2
-            model = Model(x, states[0])
-            state = model.predict(inputs)
-            np.testing.assert_allclose(
-                K.eval(layer.states[0]), state, atol=1e-4)
-    
-    from ..utils.generic_utils import deserialize_keras_object
-from ..engine import Layer
-from ..engine import Input
-from ..engine import InputLayer
-from ..engine import InputSpec
-from .merge import *
-from .core import *
-from .convolutional import *
-from .pooling import *
-from .local import *
-from .recurrent import *
-from .cudnn_recurrent import *
-from .normalization import *
-from .embeddings import *
-from .noise import *
-from .advanced_activations import *
-from .wrappers import *
-from .convolutional_recurrent import *
-from ..legacy.layers import *
-    
-      Args:
-    filename: The name of the current file.
-    clean_lines: A CleansedLines instance containing the file.
-    linenum: The number of the line to check.
-    error: The function to call with any errors found.
-  '''
+            # Alternate doesn't make a difference for these, they format the same with or without it
+        self.assertEqual(format(1+1j, '.1e'),  '1.0e+00+1.0e+00j')
+        self.assertEqual(format(1+1j, '#.1e'), '1.0e+00+1.0e+00j')
+        self.assertEqual(format(1+1j, '.1f'),  '1.0+1.0j')
+        self.assertEqual(format(1+1j, '#.1f'), '1.0+1.0j')
