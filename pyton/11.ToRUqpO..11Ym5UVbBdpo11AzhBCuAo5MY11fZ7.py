e7a1b36996af4dc6098e955520249de2e407064a
SUNsung <weatherius@gmail.com>
2018-04-16 17:46:41 +0300

    Rewrite [11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py] File in: 2018-04-16 17:46:40

--- pyton/11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py
+++ pyton/11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py
@@ -1,34 +1,71 @@
 
         
-                Punctuation:               'bold #000000',   # class: 'p'
+        import scrapy
+from scrapy.commands import ScrapyCommand
+from scrapy.utils.template import render_templatefile, string_camelcase
+from scrapy.exceptions import UsageError
     
-        @pytest.fixture(autouse=True)
-    def setup(self):
-        '''CaseInsensitiveDict instance with 'Accept' header.'''
-        self.case_insensitive_dict = CaseInsensitiveDict()
-        self.case_insensitive_dict['Accept'] = 'application/json'
+            general form:
+        @returns request(s)/item(s) [min=1 [max]]
     
-        @property
-    def unverifiable(self):
-        return self.is_unverifiable()
+        def create_widgets(self):  # Call from override, if any.
+        # Bind to self widgets needed for entry_ok or unittest.
+        self.frame = frame = Frame(self, padding=10)
+        frame.grid(column=0, row=0, sticky='news')
+        frame.grid_columnconfigure(0, weight=1)
     
-    import chardet
+        def test_decoder_optimizations(self):
+        # Several optimizations were made that skip over calls to
+        # the whitespace regex, so this test is designed to try and
+        # exercise the uncommon cases. The array cases are already covered.
+        rval = self.loads('{   'key'    :    'value'    ,  'k':'v'    }')
+        self.assertEqual(rval, {'key':'value', 'k':'v'})
     
-    This module contains the set of Requests' exceptions.
-'''
-from urllib3.exceptions import HTTPError as BaseHTTPError
+        def close(self):
+        if not self.closed:
+            try:
+                self.skip()
+            finally:
+                self.closed = True
     
-    from .structures import LookupDict
+    def py_make_scanner(context):
+    parse_object = context.parse_object
+    parse_array = context.parse_array
+    parse_string = context.parse_string
+    match_number = NUMBER_RE.match
+    strict = context.strict
+    parse_float = context.parse_float
+    parse_int = context.parse_int
+    parse_constant = context.parse_constant
+    object_hook = context.object_hook
+    object_pairs_hook = context.object_pairs_hook
+    memo = context.memo
     
-                # Update history and keep track of redirects.
-            # resp.history must ignore the original request in this loop
-            hist.append(resp)
-            resp.history = hist[1:]
+    def test():
+    for i in range(256):
+        c = chr(i)
+        s = repr(c)
+        e = evalString(s)
+        if e != c:
+            print(i, c, s, e)
     
-        def initialize_options(self):
-        TestCommand.initialize_options(self)
-        try:
-            from multiprocessing import cpu_count
-            self.pytest_args = ['-n', str(cpu_count()), '--boxed']
-        except (ImportError, NotImplementedError):
-            self.pytest_args = ['-n', '1', '--boxed']
+        def readable(self):
+        return self._file.readable()
+    
+        def test_truediv(self):
+        simple_real = [float(i) for i in range(-5, 6)]
+        simple_complex = [complex(x, y) for x in simple_real for y in simple_real]
+        for x in simple_complex:
+            for y in simple_complex:
+                self.check_div(x, y)
+    
+            class WNDCLASS(Structure):
+            _fields_ = [('style', c_uint),
+                        ('lpfnWndProc', WNDPROC),
+                        ('cbClsExtra', c_int),
+                        ('cbWndExtra', c_int),
+                        ('hInstance', HINSTANCE),
+                        ('hIcon', HICON),
+                        ('hCursor', HCURSOR),
+                        ('lpszMenuName', LPCTSTR),
+                        ('lpszClassName', LPCTSTR)]
