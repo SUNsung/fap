303da1d9e71a7e0eae510f59167af6bd7f149c5b
SUNsung <weatherius@gmail.com>
2018-04-16 09:04:31 +0300

    Rewrite [33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb] File in: 2018-04-16 09:04:31

--- ruby/33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb
+++ ruby/33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb
@@ -1,87 +1,86 @@
 
         
-                assert_predicate(x, :autoclose?)
-        assert_equal Encoding::ASCII_8BIT, x.external_encoding
-        x.write 'hello'
-    
-      # Classifies the set by the return value of the given block and
-  # returns a hash of {value => set of elements} pairs.  The block is
-  # called once for each element of the set, passing the element as
-  # parameter.
-  #
-  #   require 'set'
-  #   files = Set.new(Dir.glob('*.rb'))
-  #   hash = files.classify { |f| File.mtime(f).year }
-  #   hash       #=> {2000=>#<Set: {'a.rb', 'b.rb'}>,
-  #              #    2001=>#<Set: {'c.rb', 'd.rb', 'e.rb'}>,
-  #              #    2002=>#<Set: {'f.rb'}>}
-  #
-  # Returns an enumerator if no block is given.
-  def classify # :yields: o
-    block_given? or return enum_for(__method__) { size }
-    
-      it 'adds nil for each element requested beyond the end of the String' do
-    [ ['\xff\x00\xff\x00\xff\x00\xff',  [nil, nil, nil]],
-      ['\xb8\x1e\x85\xebQ\xb8\xf6?abc', [1.42, nil, nil]],
-      ['333333\x07@ffffff\xf6?abcd',    [2.9, 1.4, nil]]
-    ].should be_computed_by(:unpack, unpack_format(3))
-  end
-    
-      it 'decodes the number of bytes specified by the count modifier including whitespace bytes' do
-    [ ['a bc',  ['a b', 'c']],
-      ['a\fbc', ['a\fb', 'c']],
-      ['a\nbc', ['a\nb', 'c']],
-      ['a\rbc', ['a\rb', 'c']],
-      ['a\tbc', ['a\tb', 'c']],
-      ['a\vbc', ['a\vb', 'c']]
-    ].should be_computed_by(:unpack, unpack_format(3)+unpack_format)
-  end
-    
-      it 'decodes the number of characters specified by the count modifier' do
-    [ ['\xc2\x80\xc2\x81\xc2\x82\xc2\x83', 'U1', [0x80]],
-      ['\xc2\x80\xc2\x81\xc2\x82\xc2\x83', 'U2', [0x80, 0x81]],
-      ['\xc2\x80\xc2\x81\xc2\x82\xc2\x83', 'U3', [0x80, 0x81, 0x82]]
-    ].should be_computed_by(:unpack)
-  end
-    
-      context 'with redirect reaction' do
-    before(:each) do
-      mock_app do
-        use Rack::Protection::CookieTossing, :reaction => :redirect
-        run DummyApp
+              def make_test_case_available_to_view!
+        test_case_instance = self
+        _helpers.module_eval do
+          unless private_method_defined?(:_test_case)
+            define_method(:_test_case) { test_case_instance }
+            private :_test_case
+          end
+        end
       end
+    
+            def method_missing(name, *args, &block)
+          # Caches the method definition as a singleton method of the receiver.
+          #
+          # By letting #delegate handle it, we avoid an enclosure that'll capture args.
+          singleton_class.delegate name, to: :instance
+    
+      def test_default_helpers_only
+    assert_equal [JustMeHelper], JustMeController._helpers.ancestors.reject(&:anonymous?)
+    assert_equal [MeTooHelper, JustMeHelper], MeTooController._helpers.ancestors.reject(&:anonymous?)
+  end
+    
+        # Access the message attachments list.
+    def attachments
+      mailer.attachments
     end
     
-        expect(get('/', {}, 'wants' => 'text/html').headers['X-Frame-Options']).to eq('ALLOW-FROM foo')
+        class MessageDelivery < ActionMailer::MessageDelivery # :nodoc:
+      def initialize(mailer_class, action, params, *args)
+        super(mailer_class, action, *args)
+        @params = params
+      end
+    
+        if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      if Devise.sign_in_after_reset_password
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message!(:notice, flash_message)
+        sign_in(resource_name, resource)
+      else
+        set_flash_message!(:notice, :updated_not_active)
+      end
+      respond_with resource, location: after_resetting_password_path_for(resource)
+    else
+      set_minimum_password_length
+      respond_with resource
+    end
   end
     
-      it 'denies requests with a changing Accept-Language header' do
-    session = {:foo => :bar}
-    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
-    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'b'
-    expect(session).to be_empty
+        def password_change(record, opts={})
+      devise_mail(record, :password_change, opts)
+    end
   end
+end
+
     
-        expect(get('/').headers['X-Content-Type-Options']).to be_nil
+          def remember_key(resource, scope)
+        resource.rememberable_options.fetch(:key, 'remember_#{scope}_token')
+      end
+    end
   end
+end
+
     
-            def expected_attachment
-          'Expected #{@attachment_name}:\n'
+                  define_method method do |resource_or_scope, *args|
+                scope = Devise::Mapping.find_scope!(resource_or_scope)
+                router_name = Devise.mappings[scope].router_name
+                context = router_name ? send(router_name) : _devise_route_context
+                context.send('#{action}#{scope}_#{module_name}_#{path_or_url}', *args)
+              end
+            end
+          end
         end
+      end
     
-      context 'called with null values' do
-    it 'writes rules for other three' do
-      ruleset = 'margin-top: 11px; ' +
-                'margin-right: 12px; ' +
-                'margin-left: 13px;'
-      bad_rule = 'margin-bottom: null;'
+      it 'decodes two doubles for two format characters' do
+    '@\x07333333?\xf6ffffff'.unpack(unpack_format(nil, 2)).should == [2.9, 1.4]
+  end
     
-      context 'called with four sizes' do
-    it 'applies different widths to all sides' do
-      rule = 'padding: 7px 8px 9px 10px'
+      it 'ignores spaces between directives' do
+    'dcbahgfe'.unpack(unpack_format(' ', 2)).should == [1684234849, 1751606885]
+  end
+end
     
-      context 'called with multiple prefixes' do
-    it 'applies the prefixes to the property' do
-      rule = '-moz-appearance: none; ' +
-             '-ms-appearance: none; ' +
-             'appearance: none;'
+      Ruby = Struct.new(:version, :platform)
