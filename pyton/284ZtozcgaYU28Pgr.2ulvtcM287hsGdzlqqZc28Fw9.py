b36dd75bb0992c8d43d1cb2a236ad27d5eacea02
SUNsung <weatherius@gmail.com>
2018-04-17 04:22:58 +0300

    Rewrite [284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py] File in: 2018-04-17 04:22:57

--- pyton/284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py
+++ pyton/284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py
@@ -1,135 +1,226 @@
 
         
-        if hasattr(ctypes, 'windll'):
-    WSAStringToAddressA = ctypes.windll.ws2_32.WSAStringToAddressA
-    WSAAddressToStringA = ctypes.windll.ws2_32.WSAAddressToStringA
-else:
-    def not_windows():
-        raise SystemError(
-            'Invalid platform. ctypes.windll must be available.'
-        )
-    WSAStringToAddressA = not_windows
-    WSAAddressToStringA = not_windows
+            # Final checkpoint
+    if is_chief and global_step_val >= FLAGS.max_steps:
+      sv.saver.save(sess, sv.save_path, global_step=global_step)
     
-    Here is a little overview over the most commonly used classes provided by
-this runtime:
+    FLAGS = flags.FLAGS
     
-    ##
-# imaginary tree navigation type; traverse 'get child' link
-DOWN = 2
-##
-#imaginary tree navigation type; finish with a child list
-UP = 3
+    from six.moves import StringIO
     
-            This is different from rewind in its multi-directional
-        requirement and in that its argument is strictly an input cursor
-        (index).
+    # Predict the type of some Iris flowers.
+# Let's predict the examples in FILE_TEST, repeat only once.
+predict_results = classifier.predict(
+    input_fn=lambda: my_input_fn(FILE_TEST, 1))
+tf.logging.info('Prediction on test file')
+for prediction in predict_results:
+    # Will print the predicted class, i.e: 0, 1, or 2 if the prediction
+    # is Iris Setosa, Vericolor, Virginica, respectively.
+    tf.logging.info('...{}'.format(prediction['class_ids']))
     
-                                s = self.eot[s]
-                            input.consume()
-                            # TODO: I had this as return accept[eot[s]]
-                            # which assumed here that the EOT edge always
-                            # went to an accept...faster to do this, but
-                            # what about predicated edges coming from EOT
-                            # target?
-                            continue
+    # The amazon reviews input file to use in either the RT or IMDB datasets.
+flags.DEFINE_string('amazon_unlabeled_input_file', '',
+                    'The unlabeled Amazon Reviews dataset input file. If set, '
+                    'the input file is used to augment RT and IMDB vocab.')
     
-            result_item = result_queue.get(block=True)
-        if result_item is not None:
-            work_item = pending_work_items[result_item.work_id]
-            del pending_work_items[result_item.work_id]
-    
-    # If true, '()' will be appended to :func: etc. cross-reference text.
-#add_function_parentheses = True
-    
-    if __name__ == '__main__':
-    main()
-
+      return seq
     
     
-def FormatDebugInfoResponse_ExtraConfFoundAndLoaded_test():
-  response = deepcopy( GENERIC_RESPONSE )
-  response[ 'extra_conf' ].update( {
-    'is_loaded': True,
-    'path': '/path/to/extra/conf'
-  } )
-  assert_that(
-    FormatDebugInfoResponse( response ),
-    contains_string(
-      'Extra configuration file found and loaded\n'
-      'Extra configuration path: /path/to/extra/conf\n'
-    )
-  )
-    
-    from ycm.tests.test_utils import MockVimModule
-MockVimModule()
-    
-        @YouCompleteMeInstance( { 'log_level': 'debug',
-                              'keep_logfiles': 1 } )
-    def Debug_test( ycm ):
-        ...
-  '''
-  def Decorator( test ):
-    @functools.wraps( test )
-    def Wrapper( *args, **kwargs ):
-      ycm = YouCompleteMe( MakeUserOptions( custom_options ) )
-      WaitUntilReady()
-      ycm.CheckIfServerIsReady()
-      try:
-        test( ycm, *args, **kwargs )
-      finally:
-        StopServer( ycm )
-    return Wrapper
-  return Decorator
-
+def _build_vocab_frequencies(seqs, vocab_ids):
+  vocab_freqs = defaultdict(int)
+  ids_to_words = dict([(i, word) for word, i in vocab_ids.iteritems()])
+  for seq in seqs:
+    for timestep in seq:
+      vocab_freqs[ids_to_words[timestep.token]] += 1
     
     
-def LastEnteredCharIsIdentifierChar_FiletypeHtml_test():
-  with MockCurrentFiletypes( ['html'] ):
-    with MockCurrentColumnAndLineContents( 3, 'ab-' ):
-      ok_( base.LastEnteredCharIsIdentifierChar() )
+def main(_):
+  '''Trains Language Model.'''
+  tf.logging.set_verbosity(tf.logging.INFO)
+  with tf.device(tf.train.replica_device_setter(FLAGS.ps_tasks)):
+    model = graphs.get_model()
+    train_op, loss, global_step = model.language_model_training()
+    train_utils.run_training(train_op, loss, global_step)
     
-    API_TYPES = ('api', 'json')
-RSS_TYPES = ('rss', 'xml')
+    exports_files(['LICENSE'])
     
-        def current_subreddit(self):
-        site = self.stacked_proxy_safe_get(c, 'site')
-        if not site:
-            # In non-request code (eg queued jobs), there isn't necessarily a
-            # site name (or other request-type data).  In those cases, we don't
-            # want to trigger any subreddit-specific code.
-            return ''
-        return site.name
+    exports_files(['LICENSE'])
     
+            Because the user is not required to use a token with an index stored
+        in it, we must provide a means for two token objects themselves to
+        indicate the start/end location.  Most often this will just delegate
+        to the other toString(int,int).  This is also parallel with
+        the TreeNodeStream.toString(Object,Object).
+	'''
     
-def register_api_templates(template_name, template_class):
-    for style in ('api', 'api-html', 'api-compact'):
-        tpm.add_handler(
-            name=template_name,
-            style=style,
-            handler=template_class,
-        )
+    Coverage: 99%. The only thing not covered is inconsequential --
+testing skipping of suite when self.needwrapbutton is false.
+'''
+import unittest
+from test.support import requires
+from tkinter import Tk, Frame  ##, BooleanVar, StringVar
+from idlelib import searchengine as se
+from idlelib import searchbase as sdb
+from idlelib.idle_test.mock_idle import Func
+## from idlelib.idle_test.mock_tk import Var
     
-    from pylons import request
-from pylons import app_globals as g
-from reddit_base import RedditController
-from r2.lib.pages import AdminPage, AdminAwards
-from r2.lib.pages import AdminAwardGive, AdminAwardWinners
-from r2.lib.validator import *
-    
-    class ButtonsController(RedditController):
-    def get_wrapped_link(self, url, link = None, wrapper = None):
-        try:
-            links = []
-            if link:
-                links = [link]
+        def browse_file(self):
+        filetypes = [
+            ('HTML Files', '*.htm *.html', 'TEXT'),
+            ('PDF Files', '*.pdf', 'TEXT'),
+            ('Windows Help Files', '*.chm'),
+            ('Text Files', '*.txt', 'TEXT'),
+            ('All Files', '*')]
+        path = self.pathvar.get()
+        if path:
+            dir, base = os.path.split(path)
+        else:
+            base = None
+            if platform[:3] == 'win':
+                dir = os.path.join(os.path.dirname(executable), 'Doc')
+                if not os.path.isdir(dir):
+                    dir = os.getcwd()
             else:
-                sr = None if isinstance(c.site, FakeSubreddit) else c.site
-                try:
-                    links = Link._by_url(url, sr)
-                except NotFound:
-                    pass
+                dir = os.getcwd()
+        file = self.askfilename(filetypes, dir, base)
+        if file:
+            self.pathvar.set(file)
     
-            An iden is given as the `captcha` field with a `BAD_CAPTCHA`
-        error, you should use this endpoint if you get a
-        `BAD_CAPTCHA` error response.
+        ``mode`` assumes discrete data, and returns a single value. This is the
+    standard treatment of the mode as commonly taught in schools:
+    
+        def test_invalid_escape(self):
+        s = '['abc\\y']'
+        msg = 'escape'
+        self.assertRaisesRegex(self.JSONDecodeError, msg, self.loads, s)
+    
+        def remove_flag(self, flag):
+        '''Unset the given string flag(s) without changing others.'''
+        if 'Status' in self or 'X-Status' in self:
+            self.set_flags(''.join(set(self.get_flags()) - set(flag)))
+    
+        def parseURI(self, uri):
+        if self.entityResolver:
+            input = self.entityResolver.resolveEntity(None, uri)
+        else:
+            input = DOMEntityResolver().resolveEntity(None, uri)
+        return self.parse(input)
+    
+        tokens = {}
+    prev_val = None
+    for line in lines:
+        match = prog.match(line)
+        if match:
+            name, val = match.group(1, 2)
+            val = int(val)
+            tokens[val] = {'token': name}          # reverse so we can sort them...
+            prev_val = val
+        else:
+            comment_match = comment_regex.match(line)
+            if comment_match and prev_val is not None:
+                comment = comment_match.group(1)
+                tokens[prev_val]['comment'] = comment
+    keys = sorted(tokens.keys())
+    # load the output skeleton from the target:
+    try:
+        fp = open(outFileName)
+    except OSError as err:
+        sys.stderr.write('I/O error: %s\n' % str(err))
+        sys.exit(2)
+    with fp:
+        format = fp.read().split('\n')
+    try:
+        start = format.index('#--start constants--') + 1
+        end = format.index('#--end constants--')
+    except ValueError:
+        sys.stderr.write('target does not contain format markers')
+        sys.exit(3)
+    lines = []
+    for key in keys:
+        lines.append('%s = %d' % (tokens[key]['token'], key))
+        if 'comment' in tokens[key]:
+            lines.append('# %s' % tokens[key]['comment'])
+    format[start:end] = lines
+    try:
+        fp = open(outFileName, 'w')
+    except OSError as err:
+        sys.stderr.write('I/O error: %s\n' % str(err))
+        sys.exit(4)
+    with fp:
+        fp.write('\n'.join(format))
+    
+        # Wire up the pretty-printer
+    obj.pretty_printers.append(pretty_printer_lookup)
+    
+        @gen_test
+    def test_put_clears_timed_out_getters(self):
+        q = queues.Queue()
+        getters = [q.get(timedelta(seconds=0.01)) for _ in range(10)]
+        get = q.get()
+        q.get()
+        self.assertEqual(12, len(q._getters))
+        yield gen.sleep(0.02)
+        self.assertEqual(12, len(q._getters))
+        self.assertFalse(get.done())  # Final waiters still active.
+        q.put(0)  # put() clears the waiters.
+        self.assertEqual(1, len(q._getters))
+        self.assertEqual(0, (yield get))
+        for getter in getters:
+            self.assertRaises(TimeoutError, getter.result)
+    
+        def wake(self):
+        try:
+            self.writer.send(b'x')
+        except (IOError, socket.error, ValueError):
+            pass
+    
+    
+def main():
+    parse_command_line()
+    t = Timer(e1)
+    results = t.timeit(options.num) / options.num
+    print('engine: %0.3f ms per iteration' % (results * 1000))
+    t = Timer(c1)
+    results = t.timeit(options.num) / options.num
+    print('coroutine: %0.3f ms per iteration' % (results * 1000))
+    
+    from tornado.auth import TwitterMixin
+from tornado.escape import json_decode, json_encode
+from tornado.ioloop import IOLoop
+from tornado import gen
+from tornado.options import define, options, parse_command_line, parse_config_file
+from tornado.web import Application, RequestHandler, authenticated
+    
+    # Most of our tests depend on IOLoop, which is not usable on app engine.
+# Run the tests that work, and check that everything else is at least
+# importable (via tornado.test.import_test)
+TEST_MODULES = [
+    'tornado.httputil.doctests',
+    'tornado.iostream.doctests',
+    'tornado.util.doctests',
+    #'tornado.test.auth_test',
+    #'tornado.test.concurrent_test',
+    #'tornado.test.curl_httpclient_test',
+    'tornado.test.escape_test',
+    #'tornado.test.gen_test',
+    #'tornado.test.httpclient_test',
+    #'tornado.test.httpserver_test',
+    'tornado.test.httputil_test',
+    'tornado.test.import_test',
+    #'tornado.test.ioloop_test',
+    #'tornado.test.iostream_test',
+    'tornado.test.locale_test',
+    #'tornado.test.netutil_test',
+    #'tornado.test.log_test',
+    'tornado.test.options_test',
+    #'tornado.test.process_test',
+    #'tornado.test.simple_httpclient_test',
+    #'tornado.test.stack_context_test',
+    'tornado.test.template_test',
+    #'tornado.test.testing_test',
+    #'tornado.test.twisted_test',
+    'tornado.test.util_test',
+    #'tornado.test.web_test',
+    #'tornado.test.websocket_test',
+    #'tornado.test.wsgi_test',
+]
