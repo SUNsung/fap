83a8145262283df525dd24d7b1e69921b8289669
SUNsung <weatherius@gmail.com>
2018-04-17 16:28:09 +0300

    Rewrite [351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb] File in: 2018-04-17 16:28:08

--- ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
+++ ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
@@ -1,23 +1,20 @@
 
         
-              # Topic may be hard deleted due to spam, no point complaining
-      # we would have to look at the topics table id sequence to find cases
-      # where this was called with an invalid id, no point really
-      return unless topic.present?
+                rows.each do |row|
+          line = row.values.each_with_index.map do |value, col|
+            value.to_s.ljust(col_widths[col])
+          end.join(' ').rstrip
+          line = color.colorize(line, row.color) if row.color
+          puts line
+        end
+      end
     
-        # The path used after resending confirmation instructions.
-    def after_resending_confirmation_instructions_path_for(resource_name)
-      is_navigational_format? ? new_session_path(resource_name) : '/'
+          def stage_set?
+        !!fetch(:stage, false)
+      end
+    
+        def deploying?
+      fetch(:deploying, false)
     end
-    
-      # Check if there is no signed in user before doing the sign out.
-  #
-  # If there is no signed in user, it will set the flash message and redirect
-  # to the after_sign_out path.
-  def verify_signed_out_user
-    if all_signed_out?
-      set_flash_message! :notice, :already_signed_out
-    
-      def maximum
-    LogStash::Config::Defaults.cpu_cores
   end
+end
