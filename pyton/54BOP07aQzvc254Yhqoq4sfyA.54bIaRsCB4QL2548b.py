60f004f5add1f63fb50f4e0a24ced617c3334252
SUNsung <weatherius@gmail.com>
2018-04-17 03:30:57 +0300

    Rewrite [54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py] File in: 2018-04-17 03:30:56

--- pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
+++ pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
@@ -1,193 +1,91 @@
 
         
-        
-def with_autoparallel_prefix(replica_id, name):
-  return with_prefix('AutoParallel-Replica-%d' % replica_id, name)
-    
-      An n-dimensional array of shape (num_samples, ...) is converted into an
-  (n+1)-D array of shape (num_frames, window_length, ...), where each frame
-  starts hop_length points after the preceding one.
-    
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-    
-      @tf.test.mock.patch.dict(premade_estimator.__dict__,
-                           {'load_data': four_lines_data})
-  def test_premade_estimator(self):
-    premade_estimator.main([None, '--train_steps=1'])
-    
-    # Create a custom estimator using my_model_fn to define the model
-tf.logging.info('Before classifier construction')
-classifier = tf.estimator.Estimator(
-    model_fn=my_model_fn,
-    model_dir=PATH)  # Path to where checkpoints etc are stored
-tf.logging.info('...done constructing classifier')
-    
-    # Flags for input data are in document_generators.py
-flags.DEFINE_string('vocab_file', '', 'Path to the vocabulary file. Defaults '
-                    'to FLAGS.output_dir/vocab.txt.')
-flags.DEFINE_string('output_dir', '', 'Path to save tfrecords.')
-    
-    Computational time:
-  2 days to train 100000 steps on 1 layer 1024 hidden units LSTM,
-  256 embeddings, 400 truncated BP, 256 minibatch and on single GPU (Pascal
-  Titan X, cuDNNv5).
-'''
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-    
-        scikit_results = []
-    glmnet_results = []
-    n = 20
-    step = 100
-    n_samples = 500
-    
-        n_samples = 2000
-    list_n_features = np.linspace(500, 3000, 5).astype(np.int)
-    lasso_results, lars_lasso_results = compute_bench(alpha, [n_samples],
-                                           list_n_features, precompute=False)
-    plt.subplot(212)
-    plt.plot(list_n_features, lasso_results, 'b-', label='Lasso')
-    plt.plot(list_n_features, lars_lasso_results, 'r-', label='LassoLars')
-    plt.title('%d samples, alpha=%s' % (n_samples, alpha))
-    plt.legend(loc='upper left')
-    plt.xlabel('number of features')
-    plt.ylabel('Time (s)')
-    plt.axis('tight')
-    plt.show()
+        # TODO: ensure that history changes.
 
     
+    # TODO: ensure that history changes.
+
     
-if __name__ == '__main__':
-    ap = argparse.ArgumentParser()
-    ap.add_argument('metrics', nargs='*', default=sorted(METRICS),
-                    help='Specifies metrics to benchmark, defaults to all. '
-                         'Choices are: {}'.format(sorted(METRICS)))
-    ap.add_argument('--formats', nargs='+', choices=sorted(FORMATS),
-                    help='Specifies multilabel formats to benchmark '
-                         '(defaults to all).')
-    ap.add_argument('--samples', type=int, default=1000,
-                    help='The number of samples to generate')
-    ap.add_argument('--classes', type=int, default=10,
-                    help='The number of classes')
-    ap.add_argument('--density', type=float, default=.2,
-                    help='The average density of labels per sample')
-    ap.add_argument('--plot', choices=['classes', 'density', 'samples'],
-                    default=None,
-                    help='Plot time with respect to this parameter varying '
-                         'up to the specified value')
-    ap.add_argument('--n-steps', default=10, type=int,
-                    help='Plot this many points for each metric')
-    ap.add_argument('--n-times',
-                    default=5, type=int,
-                    help='Time performance over n_times trials')
-    args = ap.parse_args()
+        def steps(self):
+        '''Run the map and reduce steps.'''
+        return [
+            self.mr(mapper=self.mapper,
+                    reducer=self.reducer)
+        ]
     
-    
-def plot_batch_errors(all_errors, n_features, all_batch_sizes, data):
-    plt.figure()
-    plot_results(all_batch_sizes, all_errors['pca'], label='PCA')
-    plot_results(all_batch_sizes, all_errors['ipca'], label='IncrementalPCA')
-    plt.legend(loc='lower left')
-    plt.suptitle('Algorithm error vs. batch_size for n_components %i\n \
-                 LFW, size %i x %i' % (
-                 n_features, data.shape[0], data.shape[1]))
-    plt.xlabel('Batch size')
-    plt.ylabel('Mean absolute error')
-    
-    
-def bench_scikit_tree_classifier(X, Y):
-    '''Benchmark with scikit-learn decision tree classifier'''
-    
-    html_folder = u'html'
-text_folder = u'paragraphs'
-short_text_folder = u'short_paragraphs'
-n_words_per_short_text = 5
-    
-    import sys
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import LinearSVC
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV
-from sklearn.datasets import load_files
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-    
-    # Learn a frontier for outlier detection with several classifiers
-xx1, yy1 = np.meshgrid(np.linspace(-8, 28, 500), np.linspace(3, 40, 500))
-xx2, yy2 = np.meshgrid(np.linspace(3, 10, 500), np.linspace(-5, 45, 500))
-for i, (clf_name, clf) in enumerate(classifiers.items()):
-    plt.figure(1)
-    clf.fit(X1)
-    Z1 = clf.decision_function(np.c_[xx1.ravel(), yy1.ravel()])
-    Z1 = Z1.reshape(xx1.shape)
-    legend1[clf_name] = plt.contour(
-        xx1, yy1, Z1, levels=[0], linewidths=2, colors=colors[i])
-    plt.figure(2)
-    clf.fit(X2)
-    Z2 = clf.decision_function(np.c_[xx2.ravel(), yy2.ravel()])
-    Z2 = Z2.reshape(xx2.shape)
-    legend2[clf_name] = plt.contour(
-        xx2, yy2, Z2, levels=[0], linewidths=2, colors=colors[i])
-    
-        def plot_decision_surface(self, surface, type):
-        X1, X2, Z = surface
-        if type == 0:
-            levels = [-1.0, 0.0, 1.0]
-            linestyles = ['dashed', 'solid', 'dashed']
-            colors = 'k'
-            self.contours.append(self.ax.contour(X1, X2, Z, levels,
-                                                 colors=colors,
-                                                 linestyles=linestyles))
-        elif type == 1:
-            self.contours.append(self.ax.contourf(X1, X2, Z, 10,
-                                                  cmap=matplotlib.cm.bone,
-                                                  origin='lower', alpha=0.85))
-            self.contours.append(self.ax.contour(X1, X2, Z, [0.0], colors='k',
-                                                 linestyles=['solid']))
-        else:
-            raise ValueError('surface type unknown')
-    
-    print('consensus score: {:.1f}'.format(score))
-    
-        fig = plt.figure(fig_index, figsize=(10, 10))
-    ax1 = plt.subplot2grid((3, 1), (0, 0), rowspan=2)
-    ax2 = plt.subplot2grid((3, 1), (2, 0))
-    
-    Plot the classification probability for different classifiers. We use a 3
-class dataset, and we classify it with a Support Vector classifier, L1
-and L2 penalized logistic regression with either a One-Vs-Rest or multinomial
-setting, and Gaussian process classification.
-    
+            (category1, 1), product4
+        (category1, 2), product1
+        (category1, 3), product2
+        (category2, 3), product1
+        (category2, 7), product3
         '''
+        category, product_id = key
+        quantity = value
+        yield (category, quantity), product_id
     
+        def __init__(self):
+        self.users_by_id = {}  # key: user id, value: User
     
-# This class can be used to keep the ycmd server alive for the duration of the
-# life of the client. By default, ycmd shuts down if it doesn't see a request in
-# a while.
-class YcmdKeepalive( object ):
-  def __init__( self, ping_interval_seconds = 60 * 10 ):
-    self._keepalive_thread = Thread( target = self._ThreadMain )
-    self._keepalive_thread.daemon = True
-    self._ping_interval_seconds = ping_interval_seconds
+        def crawl(self):
+        while True:
+            page = self.data_store.extract_max_priority_page()
+            if page is None:
+                break
+            if self.data_store.crawled_similar(page.signature):
+                self.data_store.reduce_priority_link_to_crawl(page.url)
+            else:
+                self.crawl_page(page)
+            page = self.data_store.extract_max_priority_page()
+
     
-    API_TYPES = ('api', 'json')
-RSS_TYPES = ('rss', 'xml')
+        def get_person(self, person_id):
+        person_server = self.lookup[person_id]
+        return person_server.people[person_id]
     
+    try:
+    from urllib2 import urlopen
+except ImportError:
+    from urllib.request import urlopen
     
-class APIv1UserController(OAuth2OnlyController):
-    @require_oauth2_scope('identity')
-    @validate(
-        VUser(),
-    )
-    @api_doc(api_section.account)
-    def GET_me(self):
-        'Returns the identity of the user currently authenticated via OAuth.'
-        resp = IdentityJsonTemplate().data(c.oauth_user)
-        return self.api_wrapper(resp)
+    def with_thread_pool_executor():
+    with ThreadPoolExecutor(10) as executor:
+        return list(executor.map(is_prime, PRIMES))
     
-        def GET_blog(self):
-        return self.redirect('https://blog.%s/' %
-                             get_domain(subreddit=False, no_www=True))
+    try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+    
+      # Start by looking for a single character match
+  # and increase length until no match is found.
+  best = 0
+  length = 1
+  while True:
+    pattern = left_string[ -length: ]
+    found = right_string.find( pattern )
+    if found < 0:
+      return best
+    length += found
+    if left_string[ -length: ] == right_string[ :length ]:
+      best = length
+      length += 1
+
+    
+        The style used here is: each file is a <testsuite>; each violation
+    will be a <testcase> (always failed) whose 'classname' shall be the tool
+    used (e.g., pep8) and 'name' shall be the type of violation and line
+    number. Any additional information shall be included as the <failure>
+    message.
+    
+            # See if the target is already an existing friend.
+        # If not, create the friend relationship.
+        friend_rel = Account.get_friend(c.user, friend)
+        rel_exists = bool(friend_rel)
+        if not friend_rel:
+            friend_rel = c.user.add_friend(friend)
+            response.status = 201
+    
+        def GET_health(self):
+        if os.path.exists('/var/opt/reddit/quiesce'):
+            request.environ['usable_error_content'] = 'No thanks, I'm full.'
+            abort(503)
