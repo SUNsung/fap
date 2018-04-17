9581fea5d8a389041ce74649cd2f000dcd1d1569
SUNsung <weatherius@gmail.com>
2018-04-17 19:40:30 +0300

    Rewrite [14blTeQcPYeAE147cd3tHBDa9o14IFKNake5W4Y14Lj6.rb] File in: 2018-04-17 19:40:29

--- ruby/14blTeQcPYeAE147cd3tHBDa9o14IFKNake5W4Y14Lj6.rb
+++ ruby/14blTeQcPYeAE147cd3tHBDa9o14IFKNake5W4Y14Lj6.rb
@@ -1,28 +1,18 @@
 
         
-            def initialize(tag_name, markup, tokens)
-      @by = nil
-      @source = nil
-      @title = nil
-      if markup =~ FullCiteWithTitle
-        @by = $1
-        @source = $2 + $3
-        @title = $4.titlecase.strip
-      elsif markup =~ FullCite
-        @by = $1
-        @source = $2 + $3
-      elsif markup =~ AuthorTitle
-        @by = $1
-        @title = $2.titlecase.strip
-      elsif markup =~ Author
-        @by = $1
-      end
-      super
+        describe 'border-style' do
+  before(:all) do
+    ParserSupport.parse_file('library/border-style')
+  end
+    
+      context 'called with two sizes' do
+    it 'applies to alternating sides' do
+      rule = 'margin: 2px 3px'
+    
+          expect('.padding-explicit').to have_rule(rule)
     end
+  end
     
-    Liquid::Template.register_tag('include_array', Jekyll::IncludeArrayTag)
-
-    
-          if File.symlink?(code_path)
-        return 'Code directory '#{code_path}' cannot be a symlink'
-      end
+          expect('.size-implicit').to have_ruleset(rule)
+    end
+  end
