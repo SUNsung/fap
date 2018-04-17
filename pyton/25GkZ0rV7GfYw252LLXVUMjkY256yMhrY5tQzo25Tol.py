b697931d0b018149ed3e57803b53e8f4c18beaee
SUNsung <weatherius@gmail.com>
2018-04-17 09:49:56 +0300

    Rewrite [25GkZ0rV7GfYw252LLXVUMjkY256yMhrY5tQzo25Tol.py] File in: 2018-04-17 09:49:56

--- pyton/25GkZ0rV7GfYw252LLXVUMjkY256yMhrY5tQzo25Tol.py
+++ pyton/25GkZ0rV7GfYw252LLXVUMjkY256yMhrY5tQzo25Tol.py
@@ -1,118 +1,80 @@
 
         
-        
-def delete(url, **kwargs):
-    r'''Sends a DELETE request.
+        # Note, sha1 is the only hash algorithm compatible with python2.4 and with
+# FIPS-140 mode (as of 11-2014)
+try:
+    from hashlib import sha1 as sha1
+except ImportError:
+    from sha import sha as sha1
+    
+        # If it isn't any of the above, we don't understand it.
+    if not host:
+        raise AnsibleError('Not a valid network hostname: %s' % address)
+    
+        indent = None
+    if format:
+        indent = 4
+    
+        @g_connect
+    def authenticate(self, github_token):
+        '''
+        Retrieve an authentication token
+        '''
+        url = '%s/tokens/' % self.baseurl
+        args = urlencode({'github_token': github_token})
+        resp = open_url(url, data=args, validate_certs=self._validate_certs, method='POST')
+        data = json.loads(to_text(resp.read(), errors='surrogate_or_strict'))
+        return data
+    
+    import re
+    
+        results = {}
+    for group in sort_groups(groups):
+        results = combine_vars(results, group.get_vars())
     
     
-class Server(threading.Thread):
-    '''Dummy server using for unit testing'''
-    WAIT_EVENT_TIMEOUT = 5
+def best_server():
+    # TODO: find and use the best server
+    # teredo.remlab.net / teredo - debian.remlab.net(Germany)
+    # teredo.ngix.ne.kr(South Korea)
+    # teredo.managemydedi.com(USA, Chicago)
+    # teredo.trex.fi(Finland)
+    # win8.ipv6.microsoft.com(The Teredo server hidden in Windows RT 8.1) of which Windows 7 has no knowledge.
+    # win10.ipv6.microsoft.com
+    return 'teredo.remlab.net'
     
-        :param str u_string: unicode string to check. Must be unicode
-        and not Python 2 `str`.
-    :rtype: bool
-    '''
-    assert isinstance(u_string, str)
-    try:
-        u_string.encode('ascii')
-        return True
-    except UnicodeEncodeError:
-        return False
+    
+    def setText(self, text):
+        '''
+        Override the text for this token.  getText() will return this text
+        rather than pulling from the buffer.  Note that this does not mean
+        that start/stop indexes are not valid.  It means that that input
+        was converted to a new string in the token object.
+	'''
+        self._text = text
+    
+        def test_strange_subclass(self):
+        class X(deque):
+            def __iter__(self):
+                return iter([])
+        d1 = X([1,2,3])
+        d2 = X([4,5,6])
+        d1 == d2   # not clear if this is supposed to be True or False,
+                   # but it used to give a SystemError
+    
+    
+print('\n# ======================================================================')
+print('#                   Calculating pi, 10000 iterations')
+print('# ======================================================================\n')
+    
+    make_scanner = c_make_scanner or py_make_scanner
 
     
-        @possible_keys
-    def test_delitem(self, key):
-        del self.case_insensitive_dict[key]
-        assert key not in self.case_insensitive_dict
+            # RFC 4642 2.2.2: Both the client and the server MUST know if there is
+        # a TLS session active.  A client MUST NOT attempt to start a TLS
+        # session if a TLS session is already active.
+        self.tls_on = False
     
-            with server as address:
-            sock1 = socket.socket()
-            sock2 = socket.socket()
-    
-            .. warning:: operation is O(n), not O(1).
-        '''
-        return self._find_no_duplicates(name)
-    
-        # Client Error.
-    400: ('bad_request', 'bad'),
-    401: ('unauthorized',),
-    402: ('payment_required', 'payment'),
-    403: ('forbidden',),
-    404: ('not_found', '-o-'),
-    405: ('method_not_allowed', 'not_allowed'),
-    406: ('not_acceptable',),
-    407: ('proxy_authentication_required', 'proxy_auth', 'proxy_authentication'),
-    408: ('request_timeout', 'timeout'),
-    409: ('conflict',),
-    410: ('gone',),
-    411: ('length_required',),
-    412: ('precondition_failed', 'precondition'),
-    413: ('request_entity_too_large',),
-    414: ('request_uri_too_large',),
-    415: ('unsupported_media_type', 'unsupported_media', 'media_type'),
-    416: ('requested_range_not_satisfiable', 'requested_range', 'range_not_satisfiable'),
-    417: ('expectation_failed',),
-    418: ('im_a_teapot', 'teapot', 'i_am_a_teapot'),
-    421: ('misdirected_request',),
-    422: ('unprocessable_entity', 'unprocessable'),
-    423: ('locked',),
-    424: ('failed_dependency', 'dependency'),
-    425: ('unordered_collection', 'unordered'),
-    426: ('upgrade_required', 'upgrade'),
-    428: ('precondition_required', 'precondition'),
-    429: ('too_many_requests', 'too_many'),
-    431: ('header_fields_too_large', 'fields_too_large'),
-    444: ('no_response', 'none'),
-    449: ('retry_with', 'retry'),
-    450: ('blocked_by_windows_parental_controls', 'parental_controls'),
-    451: ('unavailable_for_legal_reasons', 'legal_reasons'),
-    499: ('client_closed_request',),
-    
-        model_1.test_on_batch(input_a_df,
-                          output_a_df)
-    model_2.test_on_batch([input_a_df, input_b_df],
-                          [output_a_df, output_b_df])
-    model_1.test_on_batch([input_a_df],
-                          [output_a_df])
-    model_1.test_on_batch({'input_a': input_a_df},
-                          output_a_df)
-    model_2.test_on_batch({'input_a': input_a_df, 'input_b': input_b_df},
-                          [output_a_df, output_b_df])
-    
-        # Normally the trailing 1 is added by standardize_weights
-    weights = np.ones((3,))
-    mask = np.ones((3, 4))
-    mask[1, 0] = 0
-    
-    print('Convert class vector to binary class matrix '
-      '(for use with categorical_crossentropy)')
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-print('y_train shape:', y_train.shape)
-print('y_test shape:', y_test.shape)
-    
-    
-def eval_loss_and_grads(x):
-    if K.image_data_format() == 'channels_first':
-        x = x.reshape((1, 3, img_nrows, img_ncols))
-    else:
-        x = x.reshape((1, img_nrows, img_ncols, 3))
-    outs = f_outputs([x])
-    loss_value = outs[0]
-    if len(outs[1:]) == 1:
-        grad_values = outs[1].flatten().astype('float64')
-    else:
-        grad_values = np.array(outs[1:]).flatten().astype('float64')
-    return loss_value, grad_values
-    
-    
-def test_softmax_valid():
-    '''Test using a reference implementation of softmax.
-    '''
-    def softmax(values):
-        m = np.max(values)
-        e = np.exp(values - m)
-        return e / np.sum(e)
-    
-        toplot = track[i, ::, ::, 0]
+        def test_from_buffer_with_offset(self):
+        a = array.array('i', range(16))
+        x = (c_int * 15).from_buffer(a, sizeof(c_int))
