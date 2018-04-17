25c33a9973c87fe941fd47c0151cc7a92bcee386
SUNsung <weatherius@gmail.com>
2018-04-17 20:37:28 +0300

    Rewrite [22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb] File in: 2018-04-17 20:37:28

--- ruby/22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb
+++ ruby/22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb
@@ -1,110 +1,39 @@
 
         
-                        on.unsubscribe do |chan, count|
-                  if count == 0
-                    @subscription_lock.synchronize do
-                      @raw_client = nil
-                    end
-                  end
-                end
-              end
-            end
-          end
-    
-      test 'response status aliases deprecated' do
-    response = ActionDispatch::TestResponse.create
-    assert_deprecated { response.success? }
-    assert_deprecated { response.missing? }
-    assert_deprecated { response.error? }
+          def test_redirect_with_header_break
+    get :redirect_with_header_break
+    assert_response :redirect
+    assert_equal 'http://test.host/lolwat', redirect_to_url
   end
+    
+          def set_body
+        self.response_body = 'Success'
+      end
+    end
+    
+      class C < A
+    helper { def shout; 'C' end }
+    
+          argument :actions, type: :array, default: [], banner: 'method method'
+    
+      # any user that is either a moderator or an admin
+  def staff?
+    admin || moderator
+  end
+    
+    def mkpieces
+  piece(0,[],[0])
+  $p.each do |a|
+    a0 = a[0]
+    a[1] = ud(a0)
+    a[2] = rl(a0)
+    a[3] = ud(rl(a0))
+    a[4] = xy(a0)
+    a[5] = ud(xy(a0))
+    a[6] = rl(xy(a0))
+    a[7] = ud(rl(xy(a0)))
+    a.sort!
+    a.uniq!
+  end
+  $p.uniq!.sort! {|x,y| x[0] <=> y[0] }
 end
-
-    
-          test 'before_action with overwritten condition' do
-        @controller = Callback2Overwrite.new
-        @controller.process(:index)
-        assert_equal '', @controller.response_body
-      end
-    end
-    
-      def setup
-    super
-    @request.action = 'index'
-  end
-    
-          debug { event.payload[:mail] }
-    end
-    
-        class MessageDelivery < ActionMailer::MessageDelivery # :nodoc:
-      def initialize(mailer_class, action, params, *args)
-        super(mailer_class, action, *args)
-        @params = params
-      end
-    
-    # Show backtraces for deprecated behavior for quicker cleanup.
-ActiveSupport::Deprecation.debug = true
-    
-      class AssertMultipartSelectMailer < ActionMailer::Base
-    def test(options)
-      mail subject: 'Test e-mail', from: 'test@test.host', to: 'test <test@test.host>' do |format|
-        format.text { render plain: options[:text] }
-        format.html { render plain: options[:html] }
-      end
-    end
-  end
-    
-      # True if a {Formula} is being built with a specific option.
-  # <pre>args << '--i-want-spam' if build.with? 'spam'
-  #
-  # args << '--qt-gui' if build.with? 'qt' # '--with-qt' ==> build.with? 'qt'
-  #
-  # # If a formula presents a user with a choice, but the choice must be fulfilled:
-  # if build.with? 'example2'
-  #   args << '--with-example2'
-  # else
-  #   args << '--with-example1'
-  # end</pre>
-  def with?(val)
-    option_names = val.respond_to?(:option_names) ? val.option_names : [val]
-    
-          puts_columns Array(result)
-    else
-      query = ARGV.first
-      rx = query_regexp(query)
-      local_results = search_formulae(rx)
-      puts_columns(local_results)
-      tap_results = search_taps(rx)
-      puts_columns(tap_results)
-    
-    module Homebrew
-  def update_report
-    install_core_tap_if_necessary
-    
-        def self.git_log_between(pretty_format, from, to, merge_commit_filtering, date_format = nil, ancestry_path)
-      command = ['git log']
-      command << '--pretty=\'#{pretty_format}\''
-      command << '--date=\'#{date_format}\'' if date_format
-      command << '--ancestry-path' if ancestry_path
-      command << '#{from.shellescape}...#{to.shellescape}'
-      command << git_log_merge_commit_filtering_option(merge_commit_filtering)
-      Actions.sh(command.compact.join(' '), log: false).chomp
-    rescue
-      nil
-    end
-    
-    describe Deliver::HtmlGenerator do
-  let(:generator) { Deliver::HtmlGenerator.new }
-    
-            expect(result).to eq('/usr/local/bin/cloc  --by-file --xml  --out=build/cloc.xml MyCoolApp')
-      end
-    
-        context 'Mercurial repository' do
-      before do
-        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_svn?).and_return(false)
-        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_git_svn?).and_return(false)
-        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_git?).and_return(false)
-        expect(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_hg?).and_return(true)
-      end
-    
-            expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER]).to match(/cd .* && agvtool new-version -all 24/)
-      end
