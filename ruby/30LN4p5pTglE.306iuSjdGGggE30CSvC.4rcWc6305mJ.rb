ec564d2d68d84056201eaf492b0f66143086e672
SUNsung <weatherius@gmail.com>
2018-04-18 08:24:56 +0300

    Rewrite [30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb] File in: 2018-04-18 08:24:56

--- ruby/30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb
+++ ruby/30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb
@@ -1,82 +1,53 @@
 
         
-          included do
-    self.validate :validate_email_options
-  end
-    
-      def remote_url
-    object.remote_url.presence
-  end
-    
-      describe 'GET #show' do
-    let!(:tag)     { Fabricate(:tag, name: 'test') }
-    let!(:local)   { Fabricate(:status, tags: [tag], text: 'local #test') }
-    let!(:remote)  { Fabricate(:status, tags: [tag], text: 'remote #test', account: Fabricate(:account, domain: 'remote')) }
-    let!(:late)    { Fabricate(:status, tags: [tag], text: 'late #test') }
-    
-      # Strip out the value
-  form.search('//input') do |inp|
-    
-    puts '* System info:'
-    
-    		self.block[-1] << ';'
-		self.block[-1] << ''
-	end
-    
-      def weekly_user_stats
-    @created_users_by_week = Hash.new{ |h,k| h[k] = [] }
-    @created_users = User.where('username IS NOT NULL and created_at IS NOT NULL')
-    @created_users.find_each do |u|
-      week = u.created_at.beginning_of_week.strftime('%Y-%m-%d')
-      @created_users_by_week[week] << u.username
+          def test_read_all_extended_file
+    [nil, {:textmode=>true}, {:binmode=>true}].each do |mode|
+      Tempfile.create('test-extended-file', mode) {|f|
+        assert_nil(f.getc)
+        f.print 'a'
+        f.rewind
+        assert_equal('a', f.read, 'mode = <#{mode}>')
+      }
     end
+  end
     
-          def handle_prompt_with_signed_in_user
-        client_id = params[:client_id]
-        if client_id
-          auth = Api::OpenidConnect::Authorization.find_by_client_id_user_and_scopes(client_id,
-                                                                                     current_user, params[:scope])
-          if auth
-            process_authorization_consent('true')
-          else
-            handle_params_error('interaction_required', 'User must already be authorized when `prompt` is `none`')
-          end
-        else
-          handle_params_error('bad_request', 'Client ID is missing from request')
+      def assert_invalid_env(msg = nil)
+    all_assertions(msg) do |a|
+      INVALID_ENVVARS.each do |v|
+        a.for(v) do
+          assert_raise(ArgumentError) {yield v}
         end
       end
-    
-          def fetch_public_key_from_json(string, jwt)
-        json = JSON.parse(string)
-        keys = json['keys']
-        public_key = get_key_from_kid(keys, jwt.header['kid'])
-        public_key
-      end
-    
-        # The trailing expressions in the query.
-    #
-    # When parsed as Sass code, each expression contains strings and SassScript
-    # nodes. When parsed as CSS, each one contains a single string.
-    #
-    # @return [Array<Array<String, Sass::Script::Tree::Node>>]
-    attr_accessor :expressions
-    
-          def report(env)
-        warn env, 'attack reported by #{self.class}'
-        env[options[:report_key]] = true
-      end
-    
-        it 'Returns nil when Referer header is invalid' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/bad|uri'}
-      expect(subject.referrer(env)).to be_nil
     end
   end
+    
+      it 'decodes the remaining floats when passed the '*' modifier after another directive' do
+    array = '\x9a\x99\xa9@33\x13A'.unpack(unpack_format()+unpack_format('*'))
+    array.should == [5.300000190734863, 9.199999809265137]
+  end
+    
+      it 'ignores spaces between directives' do
+    'badc'.unpack(unpack_format(' ', 2)).should == [25185, 25699]
+  end
 end
-
+    
+        it 'Reads referrer from Host header when Referer header is relative' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
+      expect(subject.referrer(env)).to eq('foo.com')
+    end
     
     
-  it 'should allow changing the nosniff-mode off' do
+  it 'should allow changing the protection mode to a string' do
+    # I have no clue what other modes are available
     mock_app do
-      use Rack::Protection::XSSHeader, :nosniff => false
+      use Rack::Protection::FrameOptions, :frame_options => 'ALLOW-FROM foo'
       run DummyApp
     end
+    
+      it 'accepts requests with a changing Accept-Encoding header' do
+    # this is tested because previously it led to clearing the session
+    session = {:foo => :bar}
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_ENCODING' => 'a'
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_ENCODING' => 'b'
+    expect(session).not_to be_empty
+  end
