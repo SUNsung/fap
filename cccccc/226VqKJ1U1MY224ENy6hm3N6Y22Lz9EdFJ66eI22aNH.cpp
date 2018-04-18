dac3da5ee147d9a14402f2337fce6b834bd064d9
SUNsung <weatherius@gmail.com>
2018-04-18 18:47:28 +0300

    Rewrite [226VqKJ1U1MY224ENy6hm3N6Y22Lz9EdFJ66eI22aNH.cpp] File in: 2018-04-18 18:47:27

--- cccccc/226VqKJ1U1MY224ENy6hm3N6Y22Lz9EdFJ66eI22aNH.cpp
+++ cccccc/226VqKJ1U1MY224ENy6hm3N6Y22Lz9EdFJ66eI22aNH.cpp
@@ -1,223 +1,244 @@
 
         
-        
-    {  if (value_type == kTypeDeletion) {
-    // End
-    valid_ = false;
-    saved_key_.clear();
-    ClearSavedValue();
-    direction_ = kForward;
+        namespace api {
+    }
+    
+    #include 'storage/common/quota/quota_types.h'
+    
+    namespace atom {
+    }
+    
+    namespace atom {
+    }
+    
+      void UpdateCache();
+    
+    namespace atom {
+    }
+    
+    #include 'atom/browser/ui/views/menu_model_adapter.h'
+    
+    const char* NativeFrameView::GetClassName() const {
+  return kViewClassName;
+}
+    
+    // Generate constructors.
+#include 'ipc/struct_constructor_macros.h'
+#include 'content/nw/src/common/common_message_generator.h'
+    
+    namespace content {
+class RenderFrameHost;
+}
+    
+      static v8::Handle<v8::Object> GetObjectRegistry();
+  static v8::Handle<v8::Value> GetWindowId(blink::WebFrame* frame);
+  static void ZoomLevelChanged(blink::WebView* web_view);
+  static void willHandleNavigationPolicy(
+    content::RenderView* rv,
+    blink::WebFrame* frame,
+    const blink::WebURLRequest& request,
+    blink::WebNavigationPolicy* policy,
+    blink::WebString* manifest);
+    
+      // Get Shell's corresponding js object's id.
+  static void GetShellIdForCurrentContext(
+      const v8::FunctionCallbackInfo<v8::Value>& args);
+    
+    
+    {  int object_id = 0;
+  if (new_manifest->GetInteger('object_id', &object_id)) {
+    DispatcherHost* dhost = FindDispatcherHost(web_contents->GetRenderViewHost());
+    dhost->OnAllocateObject(object_id, 'Window', *new_manifest.get());
+  }
+}
+    
+    #include 'content/nw/src/api/menu/menu.h'
+    
+    bool MenuDelegate::GetIconForCommandId(int command_id,
+                                       gfx::Image* icon) const {
+  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
+  if (!item)
+    return false;
+  if (item->icon_.IsEmpty())
+    return false;
+    }
+    
+    void MenuItem::CallSync(const std::string& method,
+                        const base::ListValue& arguments,
+                        base::ListValue* result) {
+  if (method == 'GetChecked') {
+    result->AppendBoolean(GetChecked());
   } else {
-    valid_ = true;
+    NOTREACHED() << 'Invalid call to MenuItem method:' << method
+                 << ' arguments:' << arguments;
   }
 }
     
-    // Return the name of the sstable with the specified number
-// in the db named by 'dbname'.  The result will be prefixed with
-// 'dbname'.
-extern std::string TableFileName(const std::string& dbname, uint64_t number);
-    
-      // If a seek to internal key 'k' in specified file finds an entry,
-  // call (*handle_result)(arg, found_key, found_value).
-  Status Get(const ReadOptions& options,
-             uint64_t file_number,
-             uint64_t file_size,
-             const Slice& k,
-             void* arg,
-             void (*handle_result)(void*, const Slice&, const Slice&));
-    
-      for (DeletedFileSet::const_iterator iter = deleted_files_.begin();
-       iter != deleted_files_.end();
-       ++iter) {
-    PutVarint32(dst, kDeletedFile);
-    PutVarint32(dst, iter->first);   // level
-    PutVarint64(dst, iter->second);  // file number
+    int64_t TimeStamp::Get(bool &error, int hou, int min, int sec, int mon, int day,
+                   int yea, bool gmt) {
+  auto dt = req::make<DateTime>(Current());
+  if (gmt) {
+    dt->setTimezone(req::make<TimeZone>('UTC'));
   }
-    
-    #ifndef STORAGE_LEVELDB_INCLUDE_DUMPFILE_H_
-#define STORAGE_LEVELDB_INCLUDE_DUMPFILE_H_
-    
-    #endif  // STORAGE_LEVELDB_INCLUDE_FILTER_POLICY_H_
-
-    
-    void convert_dataset(const char* image_filename, const char* label_filename,
-        const char* db_filename) {
-  // Open files
-  std::ifstream image_file(image_filename, std::ios::in | std::ios::binary);
-  std::ifstream label_file(label_filename, std::ios::in | std::ios::binary);
-  CHECK(image_file) << 'Unable to open file ' << image_filename;
-  CHECK(label_file) << 'Unable to open file ' << label_filename;
-  // Read the magic and the meta data
-  uint32_t magic;
-  uint32_t num_items;
-  uint32_t num_labels;
-  uint32_t rows;
-  uint32_t cols;
-    }
-    
-      virtual inline int MinBottomBlobs() const { return 1; }
-  virtual inline int MinTopBlobs() const { return 1; }
-  virtual inline bool EqualNumBottomTopBlobs() const { return true; }
-    
-      /**
-   * @brief Computes the Contrastive error gradient w.r.t. the inputs.
-   *
-   * Computes the gradients with respect to the two input vectors (bottom[0] and
-   * bottom[1]), but not the similarity label (bottom[2]).
-   *
-   * @param top output Blob vector (length 1), providing the error gradient with
-   *      respect to the outputs
-   *   -# @f$ (1 \times 1 \times 1 \times 1) @f$
-   *      This Blob's diff will simply contain the loss_weight* @f$ \lambda @f$,
-   *      as @f$ \lambda @f$ is the coefficient of this layer's output
-   *      @f$\ell_i@f$ in the overall Net loss
-   *      @f$ E = \lambda_i \ell_i + \mbox{other loss terms}@f$; hence
-   *      @f$ \frac{\partial E}{\partial \ell_i} = \lambda_i @f$.
-   *      (*Assuming that this top Blob is not used as a bottom (input) by any
-   *      other layer of the Net.)
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2)
-   *   -# @f$ (N \times C \times 1 \times 1) @f$
-   *      the features @f$a@f$; Backward fills their diff with
-   *      gradients if propagate_down[0]
-   *   -# @f$ (N \times C \times 1 \times 1) @f$
-   *      the features @f$b@f$; Backward fills their diff with gradients if
-   *      propagate_down[1]
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    namespace caffe {
-    }
-    
-    
-    {}  // namespace caffe
-    
-    namespace caffe {
-    }
-    
-      /// Sets the non-blocking mode of the acceptor.
-  /**
-   * @param mode If @c true, the acceptor's synchronous operations will fail
-   * with boost::asio::error::would_block if they are unable to perform the
-   * requested operation immediately. If @c false, synchronous operations will
-   * block until complete.
-   *
-   * @param ec Set to indicate what error occurred, if any.
-   *
-   * @note The non-blocking mode has no effect on the behaviour of asynchronous
-   * operations. Asynchronous operations will never fail with the error
-   * boost::asio::error::would_block.
-   */
-  boost::system::error_code non_blocking(
-      bool mode, boost::system::error_code& ec)
-  {
-    return this->get_service().non_blocking(
-        this->get_implementation(), mode, ec);
-  }
-    
-    
-    {  // The data in the buffer.
-  detail::buffered_stream_storage storage_;
-};
-    
-    template <typename CompletionCondition>
-class base_from_completion_cond
-{
-protected:
-  explicit base_from_completion_cond(CompletionCondition completion_condition)
-    : completion_condition_(completion_condition)
-  {
-  }
-    }
-    
-    #include <boost/asio/detail/pop_options.hpp>
-    
-    
-    {private:
-  // The top of the stack of calls for the current thread.
-  static tss_ptr<context> top_;
-};
-    
-    #endif // BOOST_ASIO_DETAIL_DATE_TIME_FWD_HPP
-
-    
-    inline std::size_t calculate_hash_value(int i)
-{
-  return static_cast<std::size_t>(i);
+  dt->set(hou, min, sec, mon, day, yea);
+  return dt->toTimeStamp(error);
 }
     
-    #if defined(BOOST_ASIO_HAS_DEV_POLL)
+    void Timer::Dump() {
+  if (!Trace::moduleEnabledRelease(Trace::jittime)) return;
+  Trace::traceRelease('%s', Show().c_str());
+}
     
-    namespace boost {
-namespace asio {
-namespace detail {
-    }
-    }
-    }
+    #include 'hphp/util/compatibility.h'
     
-        // stream signature
-    stb_out(0x57); stb_out(0xbc);
-    stb_out2(0);
-    
-    IMGUI_API bool        ImGui_ImplDX10_Init(void* hwnd, ID3D10Device* device);
-IMGUI_API void        ImGui_ImplDX10_Shutdown();
-IMGUI_API void        ImGui_ImplDX10_NewFrame();
-IMGUI_API void        ImGui_ImplDX10_RenderDrawData(ImDrawData* draw_data);
-    
-        // Upload texture to graphics system
-    g_FontTexture = new CIwTexture();
-    g_FontTexture->SetModifiable(true);
-    CIwImage& image = g_FontTexture->GetImage();
-    image.SetFormat(CIwImage::ARGB_8888);
-    image.SetWidth(width);
-    image.SetHeight(height);
-    image.SetBuffers();                                    // allocates and own buffers
-    image.ReadTexels(pixels);
-    g_FontTexture->SetMipMapping(false);
-    g_FontTexture->SetFiltering(false);
-    g_FontTexture->Upload();
-    
-            // 1. Show a simple window.
-        // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called 'Debug'.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-            ImGui::Text('Hello, world!');                           // Display some text (you can use a format string too)
-            ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
+    namespace HPHP {
     }
     
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you use this binding you'll need to call 4 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXXX_NewFrame(), ImGui::Render() and ImGui_ImplXXXX_Shutdown().
-// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
+      for (auto& block : blocks) {
+    for (auto& inst : block.code) {
+      if (inst.op == Vinstr::copyargs) return true;
+    }
+  }
     
-                ImGui::Text('NavInputs down:'); for (int i = 0; i < IM_ARRAYSIZE(io.NavInputs); i++) if (io.NavInputs[i] > 0.0f)                    { ImGui::SameLine(); ImGui::Text('[%d] %.2f', i, io.NavInputs[i]); }
-            ImGui::Text('NavInputs pressed:'); for (int i = 0; i < IM_ARRAYSIZE(io.NavInputs); i++) if (io.NavInputsDownDuration[i] == 0.0f)    { ImGui::SameLine(); ImGui::Text('[%d]', i); }
-            ImGui::Text('NavInputs duration:'); for (int i = 0; i < IM_ARRAYSIZE(io.NavInputs); i++) if (io.NavInputsDownDuration[i] >= 0.0f)   { ImGui::SameLine(); ImGui::Text('[%d] %.2f', i, io.NavInputsDownDuration[i]); }
     
-                if (ImGui::Button('Button'))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text('counter = %d', counter);
+    {  numa_node_mask = folly::nextPowTwo(numa_num_nodes) - 1;
+}
     
-    // Helpers: Math
-// We are keeping those not leaking to the user by default, in the case the user has implicit cast operators between ImVec2 and its own types (when IM_VEC2_CLASS_EXTRA is defined)
-#ifdef IMGUI_DEFINE_MATH_OPERATORS
-static inline ImVec2 operator*(const ImVec2& lhs, const float rhs)              { return ImVec2(lhs.x*rhs, lhs.y*rhs); }
-static inline ImVec2 operator/(const ImVec2& lhs, const float rhs)              { return ImVec2(lhs.x/rhs, lhs.y/rhs); }
-static inline ImVec2 operator+(const ImVec2& lhs, const ImVec2& rhs)            { return ImVec2(lhs.x+rhs.x, lhs.y+rhs.y); }
-static inline ImVec2 operator-(const ImVec2& lhs, const ImVec2& rhs)            { return ImVec2(lhs.x-rhs.x, lhs.y-rhs.y); }
-static inline ImVec2 operator*(const ImVec2& lhs, const ImVec2& rhs)            { return ImVec2(lhs.x*rhs.x, lhs.y*rhs.y); }
-static inline ImVec2 operator/(const ImVec2& lhs, const ImVec2& rhs)            { return ImVec2(lhs.x/rhs.x, lhs.y/rhs.y); }
-static inline ImVec2& operator+=(ImVec2& lhs, const ImVec2& rhs)                { lhs.x += rhs.x; lhs.y += rhs.y; return lhs; }
-static inline ImVec2& operator-=(ImVec2& lhs, const ImVec2& rhs)                { lhs.x -= rhs.x; lhs.y -= rhs.y; return lhs; }
-static inline ImVec2& operator*=(ImVec2& lhs, const float rhs)                  { lhs.x *= rhs; lhs.y *= rhs; return lhs; }
-static inline ImVec2& operator/=(ImVec2& lhs, const float rhs)                  { lhs.x /= rhs; lhs.y /= rhs; return lhs; }
-static inline ImVec4 operator+(const ImVec4& lhs, const ImVec4& rhs)            { return ImVec4(lhs.x+rhs.x, lhs.y+rhs.y, lhs.z+rhs.z, lhs.w+rhs.w); }
-static inline ImVec4 operator-(const ImVec4& lhs, const ImVec4& rhs)            { return ImVec4(lhs.x-rhs.x, lhs.y-rhs.y, lhs.z-rhs.z, lhs.w-rhs.w); }
-static inline ImVec4 operator*(const ImVec4& lhs, const ImVec4& rhs)            { return ImVec4(lhs.x*rhs.x, lhs.y*rhs.y, lhs.z*rhs.z, lhs.w*rhs.w); }
-#endif
+    extern uint32_t numa_node_set;
+extern uint32_t numa_node_mask;
+extern uint32_t numa_num_nodes;
+extern std::vector<bitmask*> node_to_cpu_mask;
+extern bool use_numa;
+    
+      LdSSwitchData data;
+  data.numCases   = numCases;
+  data.cases      = &cases[0];
+  data.defaultSk  = SrcKey{curSrcKey(env),
+                           bcOff(env) + iv.strvec()[iv.size() - 1].dest};
+  data.bcSPOff    = spOffBCFromFP(env);
+    
+    #include 'hphp/runtime/vm/hhbc.h'
+    
+    inline void liveRelocate(bool random) {
+  return liveRelocate(random ? -1 : 20);
+}
+    
+      auto const dest_file = ::mkstemp(tmp_filename);
+  if (dest_file < 0) {
+    Logger::Error('dlopen_embedded_data: Unable to create temporary file: %s',
+                  folly::errnoStr(errno).c_str());
+    return nullptr;
+  }
+    
+    
+    {  pid_t m_shadowProcess;
+  Mutex m_procMutex;
+  int m_afdt_fd;
+  std::map<FILE*, pid_t> m_popenMap;
+};
+    
+    TEST(WriteChainAsyncTransportWrapperTest, TestSimpleIov) {
+  TestWriteChainAsyncTransportWrapper transport;
+  auto buf = folly::IOBuf::copyBuffer('foo');
+    }
+    
+    Range<AsyncIO::Op**> AsyncIO::pollCompleted() {
+  CHECK(ctx_);
+  CHECK_NE(pollFd_, -1) << 'pollCompleted() only allowed on pollable object';
+  uint64_t numEvents;
+  // This sets the eventFd counter to 0, see
+  // http://www.kernel.org/doc/man-pages/online/pages/man2/eventfd.2.html
+  ssize_t rc;
+  do {
+    rc = ::read(pollFd_, &numEvents, 8);
+  } while (rc == -1 && errno == EINTR);
+  if (UNLIKELY(rc == -1 && errno == EAGAIN)) {
+    return Range<Op**>(); // nothing completed
+  }
+  checkUnixError(rc, 'AsyncIO: read from event fd failed');
+  DCHECK_EQ(rc, 8);
+    }
+    
+      enum PollMode {
+    NOT_POLLABLE,
+    POLLABLE,
+  };
+    
+    int main(int argc, char* argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  if (FLAGS_cp) {
+    if (argc != 3) {
+      usage(argv[0]);
+    }
+    copy(argv[1], argv[2]);
+  } else {
+    if (argc != 1) {
+      usage(argv[0]);
+    }
+    list();
+  }
+  return 0;
+}
+
+    
+    namespace {
+    }
+    
+      std::shared_ptr<LogHandler> createHandler(const Options& options) override;
+    
+    
+    {} // namespace folly
+
+    
+    using namespace v8;
+    
+    
+    {  // Bitmap used to record the bytes that we read, use atomic to protect
+  // against multiple threads updating the same bit
+  std::atomic<uint32_t>* bitmap_;
+  // (1 << bytes_per_bit_pow_) is bytes_per_bit. Use power of 2 to optimize
+  // muliplication and division
+  uint8_t bytes_per_bit_pow_;
+  // Pointer to DB Statistics object, Since this bitmap may outlive the DB
+  // this pointer maybe invalid, but the DB will update it to a valid pointer
+  // by using SetStatistics() before calling Mark()
+  std::atomic<Statistics*> statistics_;
+  uint32_t rnd_;
+};
+    
+    #pragma once
+#include <memory>
+#include <stdint.h>
+    
+      bool StatisticsJni::HistEnabledForType(uint32_t type) const {
+    if (type >= HISTOGRAM_ENUM_MAX) {
+      return false;
+    }
+    
+    if (m_ignore_histograms.count(type) > 0) {
+        return false;
+    }
+    }
+    
+    #include <memory>
+#include <set>
+#include <string>
+#include 'rocksdb/statistics.h'
+#include 'monitoring/statistics.h'
+    
+    template <class Comparator>
+struct InlineSkipList<Comparator>::Splice {
+  // The invariant of a Splice is that prev_[i+1].key <= prev_[i].key <
+  // next_[i].key <= next_[i+1].key for all i.  That means that if a
+  // key is bracketed by prev_[i] and next_[i] then it is bracketed by
+  // all higher levels.  It is _not_ required that prev_[i]->Next(i) ==
+  // next_[i] (it probably did at some point in the past, but intervening
+  // or concurrent operations might have inserted nodes in between).
+  int height_ = 0;
+  Node** prev_;
+  Node** next_;
+};
+    
+    void SyncPoint::EnableProcessing() {
+  impl_->EnableProcessing();
+}
