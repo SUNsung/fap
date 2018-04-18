ec7be941d5348704b0828d3c1651a21b06d85441
SUNsung <weatherius@gmail.com>
2018-04-18 22:26:01 +0300

    Rewrite [42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py] File in: 2018-04-18 22:26:01

--- pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
+++ pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
@@ -1,82 +1,105 @@
 
         
-        '''
-from utils import http, HTTP_OK
-from fixtures import UNICODE
+          pretrain_ckpt = tf.train.get_checkpoint_state(model_dir)
+  if not (pretrain_ckpt and pretrain_ckpt.model_checkpoint_path):
+    raise ValueError(
+        'Asked to restore model from %s but no checkpoint found.' % model_dir)
+  saver_for_restore.restore(sess, pretrain_ckpt.model_checkpoint_path)
     
-      1. Read, validate and process the input (args, `stdin`).
-  2. Create and send a request.
-  3. Stream, and possibly process and format, the parts
-     of the request-response exchange selected by output options.
-  4. Simultaneously write to `stdout`
-  5. Exit.
+      def setUp(self):
+    self._string_data = '\n'.join(
+        [' hello there i am',
+         ' rain as day',
+         ' want some cheesy puffs ?'])
     
+    def four_lines_data():
+  text = StringIO(FOUR_LINES)
     
-def trim_filename_if_needed(filename, directory='.', extra=0):
-    max_len = get_filename_max_length(directory) - extra
-    if len(filename) > max_len:
-        filename = trim_filename(filename, max_len)
-    return filename
+      def testSeqAE(self):
+    FLAGS.use_seq2seq_autoencoder = True
+    graphs.VatxtModel().language_model_training()
     
-        def __init__(self, **kwargs):
-        '''
-        :param env: an class:`Environment` instance
-        :param kwargs: additional keyword argument that some
-                       processor might require.
+                    if self.eot[s] >= 0:
+                    #print 'EOT to %d' % self.eot[s]
+                    
+                    s = self.eot[s]
+                    input.consume()
+                    continue
     
-    '''
-__version__ = '1.0.0-dev'
-__author__ = 'Jakub Roztocil'
-__licence__ = 'BSD'
+            # What is index of token/char were we looking at when the error
+        # occurred?
+        self.index = None
     
+            video_id = match1(html, r'data-brightcove-id='(\d+)'')
+        
+        assert account_number, video_id
     
-FILEPATH = os.path.join(
-    os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'blns.txt')
-'''Path to the file'''
+        theplatform_download_by_pid(pid, title, output_dir=output_dir, merge=merge, info_only=info_only)
     
-        def test_str(self):
-        self.assertTrue('FOO' in str(self.error))
-        self.assertTrue('{}' in str(self.error))
+        def extract(self, **kwargs):
+        for i in self.streams:
+            s = self.streams[i]
+            _, s['container'], s['size'] = url_info(s['url'])
+            s['src'] = [s['url']]
     
-            self.args = args
-        self.http_port = 80
-        self.https_port = 443
-        self._configurator = self._all_names = self._test_names = None
+        def extract(self, **kwargs):
+        for i in self.streams:
+            # for each available stream
+            s = self.streams[i]
+            # fill in 'container' field and 'size' field (optional)
+            _, s['container'], s['size'] = url_info(s['url'])
+            # 'src' field is a list of processed urls for direct downloading
+            # usually derived from 'url'
+            s['src'] = [s['url']]
     
-        @mock.patch('certbot_compatibility_test.validator.requests.get')
-    def test_hsts_expire(self, mock_get_request):
-        mock_get_request.return_value = create_response(
-            headers={'strict-transport-security': 'max-age=3600'})
-        self.assertFalse(self.validator.hsts('test.com'))
+        # set up the templating system
+    config['pylons.app_globals'].mako_lookup = TemplateLookup(
+        directories=paths['templates'],
+        error_handler=handle_mako_error,
+        module_directory=module_directory,
+        input_encoding='utf-8',
+        default_filters=['conditional_websafe'],
+        filesystem_checks=getattr(g, 'reload_templates', False),
+        imports=[
+            'from r2.lib.filters import websafe, unsafe, conditional_websafe',
+            'from pylons import request',
+            'from pylons import tmpl_context as c',
+            'from pylons import app_globals as g',
+            'from pylons.i18n import _, ungettext',
+        ],
+        modulename_callable=mako_module_path,
+    )
     
-        @classmethod
-    def _call(cls, enhancement):
-        from certbot.display.enhancements import ask
-        return ask(enhancement)
+    from r2.controllers.api_docs import api_doc, api_section
+from r2.controllers.oauth2 import require_oauth2_scope
+from r2.controllers.reddit_base import OAuth2OnlyController
+from r2.lib.jsontemplates import (
+    FriendTableItemJsonTemplate,
+    get_usertrophies,
+    IdentityJsonTemplate,
+    KarmaListJsonTemplate,
+    PrefsJsonTemplate,
+)
+from r2.lib.pages import FriendTableItem
+from r2.lib.validator import (
+    validate,
+    VAccountByName,
+    VFriendOfMine,
+    VLength,
+    VList,
+    VUser,
+    VValidatedJSON,
+)
+from r2.models import Account, Trophy
+import r2.lib.errors as errors
+import r2.lib.validator.preferences as vprefs
     
-        description = 'Example Installer plugin'
+            res = AdminPage(content = AdminAwardGive(award, recipient, desc,
+                                                 url, hours),
+                        title='give an award').render()
+        return res
     
-    try:
-    # compatible for python2
-    from urllib2 import urlopen
-    from urllib2 import HTTPError
-    from urllib2 import URLError
-except ImportError:
-    # compatible for python3
-    from urllib.request import urlopen
-    from urllib.error import HTTPError
-    from urllib.error import URLError
+            c.allow_framing = True
     
-        fftv = Publisher(message_center)
-    
-        def test_parrot_greek_localization(self):
-        self.assertEqual(self.g.get('parrot'), 'parrot')
-    
-        def prepareReporting(self):
-        rvalue = self._db.insert()
-        if rvalue == -1:
-            self._tc.setProblem(1)
-            self._reporter.prepare()
-    
-        def __init__(self):
-        self._observers = []
+    import json
+import os
