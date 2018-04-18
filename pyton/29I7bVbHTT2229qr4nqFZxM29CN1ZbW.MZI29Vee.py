f9fbc6d160961df8d475d9ad0986d89944fc7ce7
SUNsung <weatherius@gmail.com>
2018-04-19 01:29:16 +0300

    Rewrite [29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py] File in: 2018-04-19 01:29:15

--- pyton/29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py
+++ pyton/29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py
@@ -1,110 +1,146 @@
 
         
-            old_layer = keras.layers.LSTM(input_dim=5, input_length=3,
-                                  output_dim=2, name='d', consume_less='mem')
-    new_layer = keras.layers.LSTM(2, input_shape=[3, 5], name='d', implementation=1)
-    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer.get_config())
-    
-        # Output shape
-        5D tensor with shape:
-        - If `data_format` is `'channels_last'`:
-            `(batch, first_cropped_axis, second_cropped_axis, third_cropped_axis, depth)`
-        - If `data_format` is `'channels_first'`:
-            `(batch, depth, first_cropped_axis, second_cropped_axis, third_cropped_axis)`
+        
+@implements_to_string
+class DebugFilesKeyError(KeyError, AssertionError):
+    '''Raised from request.files during debugging.  The idea is that it can
+    provide a better error message than just a generic KeyError/BadRequest.
     '''
     
-        layer_test(local.LocallyConnected1D,
-               kwargs={'filters': filters,
-                       'kernel_size': filter_length,
-                       'padding': padding,
-                       'kernel_regularizer': 'l2',
-                       'bias_regularizer': 'l2',
-                       'activity_regularizer': 'l2',
-                       'strides': strides},
-               input_shape=(num_samples, num_steps, input_dim))
+        def check(self, value):
+        return isinstance(value, list)
     
-    # Training
-batch_size = 30
-epochs = 2
-    
-    # get the symbolic outputs of each 'key' layer (we gave them unique names).
-outputs_dict = dict([(layer.name, layer.output) for layer in model.layers])
+            rv = app.config['SERVER_NAME']
     
     
-@pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
-def test_truncated_normal(tensor_shape):
-    _runner(initializers.TruncatedNormal(mean=0, stddev=1), tensor_shape,
-            target_mean=0., target_std=None, target_max=2)
-    
-            assert_allclose(convout1, convout2, atol=1e-5)
-    
-        # generate char sequences of length 'sequence_length' out of alphabet and store the next char as label (e.g. 'ab'->'c')
-    sequence_length = 2
-    sentences = [alphabet[i: i + sequence_length] for i in range(len(alphabet) - sequence_length)]
-    next_chars = [alphabet[i + sequence_length] for i in range(len(alphabet) - sequence_length)]
-    
-        test_values = get_standard_values()
-    result = f([test_values])[0]
-    assert_allclose(result, test_values, rtol=1e-05)
+def test_config_missing():
+    app = flask.Flask(__name__)
+    with pytest.raises(IOError) as e:
+        app.config.from_pyfile('missing.cfg')
+    msg = str(e.value)
+    assert msg.startswith('[Errno 2] Unable to load configuration '
+                          'file (No such file or directory):')
+    assert msg.endswith('missing.cfg'')
+    assert not app.config.from_pyfile('missing.cfg', silent=True)
     
     
-collect_ignore = [
-    # deprecated or moved modules
-    'scrapy/conf.py',
-    'scrapy/stats.py',
-    'scrapy/project.py',
-    'scrapy/utils/decorator.py',
-    'scrapy/statscol.py',
-    'scrapy/squeue.py',
-    'scrapy/log.py',
-    'scrapy/dupefilter.py',
-    'scrapy/command.py',
-    'scrapy/linkextractor.py',
-    'scrapy/spider.py',
+def test_existing_handler(app):
+    logging.root.addHandler(logging.StreamHandler())
+    assert app.logger.level == logging.NOTSET
+    assert not app.logger.handlers
     
-        def __init__(self):
-        Resource.__init__(self)
-        self.concurrent = 0
-        self.tail = deque(maxlen=100)
-        self._reset_stats()
+        n_times : int
+        Time calling the metric n_times times.
     
-        def __exit__(self, exc_type, exc_value, traceback):
-        self.proc.kill()
-        self.proc.wait()
-        time.sleep(0.2)
+                gc.collect()
+            print('benchmarking lars_path (without Gram):', end='')
+            sys.stdout.flush()
+            tstart = time()
+            lars_path(X, y, method='lasso')
+            delta = time() - tstart
+            print('%0.3fs' % delta)
+            results['lars_path (without Gram)'].append(delta)
     
-            return [token.text for token in tokens]
     
-                    # throw exception unless disjoint or identical
-                disjoint = (prevRop.lastIndex < rop.index
-                            or prevRop.index > rop.lastIndex)
-                same = (prevRop.index == rop.index
-                        and prevRop.lastIndex == rop.lastIndex)
-                if not disjoint and not same:
-                    raise ValueError(
-                        'replace op boundaries of %s overlap with previous %s'
-                        % (rop, prevRop))
+def bench_sample(sampling, n_population, n_samples):
+    gc.collect()
+    # start time
+    t_start = datetime.now()
+    sampling(n_population, n_samples)
+    delta = (datetime.now() - t_start)
+    # stop time
+    time = compute_time(t_start, delta)
+    return time
     
-    - CommonToken: A basic and most commonly used Token implementation.
-- ClassicToken: A Token object as used in ANTLR 2.x, used to %tree
-  construction.
     
-    import tornado.httpserver
-import tornado.ioloop
-import tornado.options
-import tornado.web
+def _linkcode_resolve(domain, info, package, url_fmt, revision):
+    '''Determine a link to online source for a class/method/function
     
-                self.finish(resources[path])
+                short_text_filename = os.path.join(short_text_lang_folder,
+                                               '%s_%04d.txt' % (lang, j))
+            print('Writing %s' % short_text_filename)
+            open(short_text_filename, 'wb').write(
+                small_content.encode('utf-8', 'ignore'))
+            j += 1
+            if j >= 1000:
+                break
     
-        `mask` is a `bytes` object of length 4; `data` is a `bytes` object of any length.
-    Returns a `bytes` object of the same length as `data` with the mask applied
-    as specified in section 5.3 of RFC 6455.
+        if not os.path.exists(ARCHIVE_NAME):
+        print('Downloading dataset from %s (3 MB)' % URL)
+        opener = urlopen(URL)
+        with open(ARCHIVE_NAME, 'wb') as archive:
+            archive.write(opener.read())
     
-        For use on platforms that don't have os.pipe() (or where pipes cannot
-    be passed to select()), but do have sockets.  This includes Windows
-    and Jython.
-    '''
-    def __init__(self):
-        from .auto import set_close_exec
-        # Based on Zope select_trigger.py:
-        # https://github.com/zopefoundation/Zope/blob/master/src/ZServer/medusa/thread/select_trigger.py
+    In this examples we will use a movie review dataset.
+    
+        def plot_support_vectors(self, support_vectors):
+        '''Plot the support vectors by placing circles over the
+        corresponding data points and adds the circle collection
+        to the contours list.'''
+        cs = self.ax.scatter(support_vectors[:, 0], support_vectors[:, 1],
+                             s=80, edgecolors='k', facecolors='none')
+        self.contours.append(cs)
+    
+    plt.plot(features_samples_ratio, acc_clf1, linewidth=2,
+         label='Linear Discriminant Analysis with shrinkage', color='navy')
+plt.plot(features_samples_ratio, acc_clf2, linewidth=2,
+         label='Linear Discriminant Analysis', color='gold')
+    
+    
+def uniform_labelings_scores(score_func, n_samples, n_clusters_range,
+                             fixed_n_classes=None, n_runs=5, seed=42):
+    '''Compute score for 2 random uniform cluster labelings.
+    
+        html = get_content(url)
+    pid = match1(html, r'video\.settings\.pid\s*=\s*\'([^\']+)\'')
+    title = match1(html, r'video\.settings\.title\s*=\s*\'([^\']+)\'')
+    
+    __all__ = ['ehow_download']
+    
+            mp3 = match1(content, r'name='filename'\s*value='([^']+\.mp3)'')
+        if mp3: mp3 = 'http://res.infoq.com/downloads/mp3downloads/%s' % mp3
+    
+    from ..common import *
+    
+    site_info = 'Showroom'
+download = showroom_download
+download_playlist = playlist_not_supported('showroom')
+
+    
+        def test_frozen_pool(self):
+        with ObjectPool(self.sample_queue) as pool:
+            self.assertEqual(pool, 'first')
+            self.assertEqual(pool, 'first')
+        self.assertTrue(self.sample_queue.get() == 'second')
+        self.assertFalse(self.sample_queue.empty())
+        self.assertTrue(self.sample_queue.get() == 'first')
+        self.assertTrue(self.sample_queue.empty())
+    
+        def test_display_current_time_at_current_time(self):
+        '''
+        Just as justification for working example. (Will always pass.)
+        '''
+        production_code_time_provider = ProductionCodeTimeProvider()
+        class_under_test = TimeDisplay(production_code_time_provider)
+        current_time = datetime.datetime.now()
+        expected_time = '<span class=\'tinyBoldText\'>{}:{}</span>'.format(current_time.hour, current_time.minute)
+        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
+    
+            # simple test to validate param value
+        if param in self._static_method_choices.keys():
+            self.param = param
+        else:
+            raise ValueError('Invalid Value for Param: {0}'.format(param))
+    
+            # and can also be undone at will
+        for cmd in reversed(command_stack):
+            cmd.undo()
+    finally:
+        os.unlink('foo.txt')
+    
+    print()
+    
+        def notify(self, modifier=None):
+        for observer in self._observers:
+            if modifier != observer:
+                observer.update(self)
