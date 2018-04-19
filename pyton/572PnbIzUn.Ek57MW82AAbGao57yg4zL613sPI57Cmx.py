41092973521d61339a9e74339244d33581b36dfa
SUNsung <weatherius@gmail.com>
2018-04-19 20:43:28 +0300

    Rewrite [572PnbIzUn.Ek57MW82AAbGao57yg4zL613sPI57Cmx.py] File in: 2018-04-19 20:43:27

--- pyton/572PnbIzUn.Ek57MW82AAbGao57yg4zL613sPI57Cmx.py
+++ pyton/572PnbIzUn.Ek57MW82AAbGao57yg4zL613sPI57Cmx.py
@@ -1,32 +1,69 @@
 
         
-                Notice that ')' is not included, because b would have to have
-        been called from a different context in rule a for ')' to be
-        included.
+            def __init__(self, app):
+        self.app = app
     
-        @classmethod
-    def setUpClass(cls):
-        ''' Class scope setup. '''
-        cls.p = Proxy()
+        if version in tags:
+        fail('Version '%s' is already tagged', version)
     
-        def notify(self, msg):
-        self.msg_queue.append(msg)
+        @mock.patch('certbot.notify.smtplib.LMTP')
+    @mock.patch('certbot.notify.subprocess.Popen')
+    def test_smtp_failure(self, mock_popen, mock_lmtp):
+        from certbot.notify import notify
+        lmtp_obj = mock.MagicMock()
+        mock_lmtp.return_value = lmtp_obj
+        lmtp_obj.sendmail.side_effect = socket.error(17)
+        proc = mock.MagicMock()
+        mock_popen.return_value = proc
+        self.assertTrue(notify('Goose', 'auntrhody@example.com',
+                               'The old grey goose is dead.'))
+        self.assertEqual(lmtp_obj.sendmail.call_count, 1)
+        self.assertEqual(proc.communicate.call_count, 1)
     
-        '''def test_object(queue):
-           queue_object = QueueObject(queue, True)
-           print('Inside func: {}'.format(queue_object.object))'''
+    def check_live_url(url):
     
-        def test_2nd_am_station_after_scan(self):
-        self.radio.scan()
-        station = self.radio.state.stations[self.radio.state.pos]
-        expected_station = '1380'
-        self.assertEqual(station, expected_station)
+    extension_mapping = {
+    'rss': ('xml', 'application/atom+xml; charset=UTF-8'),
+    'xml': ('xml', 'application/atom+xml; charset=UTF-8'),
+    'js': ('js', 'text/javascript; charset=UTF-8'),
+    'embed': ('htmllite', 'text/javascript; charset=UTF-8'),
+    'mobile': ('mobile', 'text/html; charset=UTF-8'),
+    'png': ('png', 'image/png'),
+    'css': ('css', 'text/css'),
+    'csv': ('csv', 'text/csv; charset=UTF-8'),
+    'api': (api_type(), 'application/json; charset=UTF-8'),
+    'json-html': (api_type('html'), 'application/json; charset=UTF-8'),
+    'json-compact': (api_type('compact'), 'application/json; charset=UTF-8'),
+    'compact': ('compact', 'text/html; charset=UTF-8'),
+    'json': (api_type(), 'application/json; charset=UTF-8'),
+    'i': ('compact', 'text/html; charset=UTF-8'),
+}
     
-        def test_tc3_output(self):
-        self.tc3.run()
-        output = self.out.getvalue().strip()
-        self.assertEqual(output, self.average_result_tc3)
+            '''
+        try:
+            return getattr(stacked_proxy, key)
+        except TypeError:
+            return default
     
-    *TL;DR80
-Traverses a container and accesses the container's elements.
-'''
+        @require_oauth2_scope('creddits')
+    @validate(
+        VUser(),
+        user=VAccountByName('username'),
+        months=VInt('months', min=1, max=36),
+        timeout=VNotInTimeout(),
+    )
+    @api_doc(
+        api_section.gold,
+        uri='/api/v1/gold/give/{username}',
+    )
+    def POST_give(self, user, months, timeout):
+        self._gift_using_creddits(
+            recipient=user,
+            months=months,
+            proxying_for=request.POST.get('proxying_for'),
+        )
+
+    
+            res = AdminPage(content = AdminAwardWinners(award),
+                        title='award winners').render()
+        return res
