64badfb7e932fbb092dc347b71e657393bec550a
SUNsung <weatherius@gmail.com>
2018-04-19 18:30:40 +0300

    Rewrite [12ASXjxmDDbDk12vZ0bEefHEes12rwRA9zkvEIM12CN0.py] File in: 2018-04-19 18:30:39

--- pyton/12ASXjxmDDbDk12vZ0bEefHEes12rwRA9zkvEIM12CN0.py
+++ pyton/12ASXjxmDDbDk12vZ0bEefHEes12rwRA9zkvEIM12CN0.py
@@ -1,112 +1,34 @@
 
         
-        new_version = {}
+        
+def has_level_handler(logger):
+    '''Check if there is a handler in the logging chain that will handle the
+    given logger's :meth:`effective level <~logging.Logger.getEffectiveLevel>`.
+    '''
+    level = logger.getEffectiveLevel()
+    current = logger
     
-    header = oldreadme[:oldreadme.index('# OPTIONS')]
-footer = oldreadme[oldreadme.index('# CONFIGURATION'):]
-    
-        # Are checkable fields missing from the test case definition?
-    test_info_dict = dict((key, value if not isinstance(value, compat_str) or len(value) < 250 else 'md5:' + md5(value))
-                          for key, value in got_dict.items()
-                          if value and key in ('id', 'title', 'description', 'uploader', 'upload_date', 'timestamp', 'uploader_id', 'location', 'age_limit'))
-    missing_keys = set(test_info_dict.keys()) - set(expected_dict.keys())
-    if missing_keys:
-        def _repr(v):
-            if isinstance(v, compat_str):
-                return ''%s'' % v.replace('\\', '\\\\').replace(''', '\\'').replace('\n', '\\n')
-            else:
-                return repr(v)
-        info_dict_str = ''
-        if len(missing_keys) != len(expected_dict):
-            info_dict_str += ''.join(
-                '    %s: %s,\n' % (_repr(k), _repr(v))
-                for k, v in test_info_dict.items() if k not in missing_keys)
-    
-    
-class RtspFD(FileDownloader):
-    def real_download(self, filename, info_dict):
-        url = info_dict['url']
-        self.report_destination(filename)
-        tmpfilename = self.temp_name(filename)
-    
-            # Video URL construction algorithm is reverse-engineered from cwhplayer.swf
-        rtmp_url = 'rtmp://camwithher.tv/clipshare/%s' % (
-            ('mp4:%s.mp4' % flv_id) if int(flv_id) > 2010 else flv_id)
-    
-    
-@for_app('tar')
-def match(command):
-    return ('-C' not in command.script
-            and _is_tar_extract(command.script)
-            and _tar_file(command.script_parts) is not None)
-    
-    
-@pytest.fixture(params=containers)
-def proc(request, spawnu, TIMEOUT):
-    proc = spawnu(*request.param)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(u'tcsh')
-    proc.sendline(u'setenv PYTHONIOENCODING utf8')
-    proc.sendline(u'eval `thefuck --alias`')
-    return proc
-    
-            return request
-    
-        name = 'url'
-    
-        @classmethod
-    def setUpClass(self):
+        def get_expiration_time(self, app, session):
+        '''A helper method that returns an expiration date for the session
+        or ``None`` if the session is linked to the browser session.  The
+        default implementation returns now + the permanent session
+        lifetime configured on the application.
         '''
-        - Create a temporary directory and file
-        /test_command
-           /foo.txt
-        - get the temporary test directory
-        - and initializes the command stack.
-        '''
-        os.mkdir('tests/test_command')
-        open('tests/test_command/foo.txt', 'w').close()
-        self.__get_test_directory()
-        self.command_stack = []
-        self.command_stack.append(MoveFileCommand(os.path.join(
-            self.test_dir, 'foo.txt'), os.path.join(self.test_dir, 'bar.txt')))
-        self.command_stack.append(MoveFileCommand(os.path.join(
-            self.test_dir, 'bar.txt'), os.path.join(self.test_dir, 'baz.txt')))
+        if session.permanent:
+            return datetime.utcnow() + app.permanent_session_lifetime
     
-        def test_b_observers_shall_be_attachable(cls):
-        cls.s.attach(cls.dec_obs)
-        cls.assertEqual(isinstance(cls.s._observers[0], DecimalViewer), True)
-        cls.assertEqual(len(cls.s._observers), 1)
-        cls.s.attach(cls.hex_obs)
-        cls.assertEqual(isinstance(cls.s._observers[1], HexViewer), True)
-        cls.assertEqual(len(cls.s._observers), 2)
     
-    ### OUTPUT ###
-# Jack move 5m then stop
-
+def main():
+    os.chdir(os.path.join(os.path.dirname(__file__), '..'))
     
-        def test_frozen_pool(self):
-        with ObjectPool(self.sample_queue) as pool:
-            self.assertEqual(pool, 'first')
-            self.assertEqual(pool, 'first')
-        self.assertTrue(self.sample_queue.get() == 'second')
-        self.assertFalse(self.sample_queue.empty())
-        self.assertTrue(self.sample_queue.get() == 'first')
-        self.assertTrue(self.sample_queue.empty())
     
-        def test_2nd_am_station_after_scan(self):
-        self.radio.scan()
-        station = self.radio.state.stations[self.radio.state.pos]
-        expected_station = '1380'
-        self.assertEqual(station, expected_station)
-    
-    from dft.parameter_injection import TimeDisplay, MidnightTimeProvider, ProductionCodeTimeProvider, datetime
-    
-        def __get__(self, obj, T):
-        def transaction(*args, **kwargs):
-            state = memento(obj)
-            try:
-                return self.method(obj, *args, **kwargs)
-            except Exception as e:
-                state()
-                raise e
+def test_uninstalled_package_paths(modules_tmpdir, purge_module):
+    app = modules_tmpdir.mkdir('config_package_app')
+    init = app.join('__init__.py')
+    init.write(
+        'import os\n'
+        'import flask\n'
+        'here = os.path.abspath(os.path.dirname(__file__))\n'
+        'app = flask.Flask(__name__)\n'
+    )
+    purge_module('config_package_app')
