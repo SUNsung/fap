5b58571db38319ef792415e375e143a876d83979
SUNsung <weatherius@gmail.com>
2018-04-19 06:27:34 +0300

    Rewrite [04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb] File in: 2018-04-19 06:27:34

--- ruby/04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb
+++ ruby/04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb
@@ -1,85 +1,27 @@
 
         
-                def test_url_sub_key_merges_correctly
-          hash = { 'url' => 'abstract://foo?encoding=utf8&', 'adapter' => 'sqlite3', 'host' => 'bar', 'pool' => '3' }
-          spec = resolve :production, 'production' => hash
-          assert_equal({
-            'adapter'  => 'abstract',
-            'host'     => 'foo',
-            'encoding' => 'utf8',
-            'pool'     => '3',
-            'name'     => 'production' }, spec)
-        end
-    
-      test 'response status aliases deprecated' do
-    response = ActionDispatch::TestResponse.create
-    assert_deprecated { response.success? }
-    assert_deprecated { response.missing? }
-    assert_deprecated { response.error? }
-  end
-end
-
-    
-                define_method('#{name}=') do |attribute|
-              attributes[name.to_sym] = attribute
-            end
-          end
-        end
-    
-    # These are the normal settings that will be set up by Railties
-# TODO: Have these tests support other combinations of these values
-silence_warnings do
-  Encoding.default_internal = Encoding::UTF_8
-  Encoding.default_external = Encoding::UTF_8
-end
-    
-        AssertSelectMailer.test('<div><p>foo</p><p>bar</p></div>').deliver_now
-    assert_select_email do
-      assert_select 'div:root' do
-        assert_select 'p:first-child', 'foo'
-        assert_select 'p:last-child', 'bar'
+            if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      if Devise.sign_in_after_reset_password
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message!(:notice, flash_message)
+        sign_in(resource_name, resource)
+      else
+        set_flash_message!(:notice, :updated_not_active)
       end
-    end
-  end
-    
-      test 'undefined delivery methods raises errors' do
-    DeliveryMailer.delivery_method = nil
-    error = assert_raise RuntimeError do
-      DeliveryMailer.welcome.deliver_now
-    end
-    assert_equal 'Delivery method cannot be nil', error.message
-  end
-    
-      # Finds the groups of the source user, optionally limited to those visible to
-  # the current user.
-  def execute(current_user = nil)
-    segments = all_groups(current_user)
-    
-    if profile_filename = ENV['PROFILE']
-  require 'ruby-prof'
-  reporter =
-    case (profile_extname = File.extname(profile_filename))
-    when '.txt'
-      RubyProf::FlatPrinterWithLineNumbers
-    when '.html'
-      RubyProf::GraphHtmlPrinter
-    when '.callgrind'
-      RubyProf::CallTreePrinter
+      respond_with resource, location: after_resetting_password_path_for(resource)
     else
-      raise 'Unknown profiler format indicated by extension: #{profile_extname}'
+      set_minimum_password_length
+      respond_with resource
     end
-  File.open(profile_filename, 'w') do |io|
-    reporter.new(RubyProf.profile { Pod::Command.run(ARGV) }).print(io)
-  end
-else
-  Pod::Command.run(ARGV)
-end
-
-    
-        prefixes
   end
     
-      context 'called with one prefix' do
-    it 'applies the prefix to the property' do
-      rule = '-webkit-appearance: none; ' +
-             'appearance: none;'
+      if respond_to?(:helper_method)
+    helpers = %w(resource scope_name resource_name signed_in_resource
+                 resource_class resource_params devise_mapping)
+    helper_method(*helpers)
+  end
+    
+      if record && record.respond_to?(:timedout?) && warden.authenticated?(scope) &&
+     options[:store] != false && !env['devise.skip_timeoutable']
+    last_request_at = warden.session(scope)['last_request_at']
