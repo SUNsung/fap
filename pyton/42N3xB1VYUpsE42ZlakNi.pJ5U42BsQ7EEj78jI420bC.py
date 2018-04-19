adb90d60837d90699b1ef778b7e50bc1cb059cde
SUNsung <weatherius@gmail.com>
2018-04-19 08:22:51 +0300

    Rewrite [42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py] File in: 2018-04-19 08:22:50

--- pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
+++ pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
@@ -1,105 +1,142 @@
 
         
-          pretrain_ckpt = tf.train.get_checkpoint_state(model_dir)
-  if not (pretrain_ckpt and pretrain_ckpt.model_checkpoint_path):
-    raise ValueError(
-        'Asked to restore model from %s but no checkpoint found.' % model_dir)
-  saver_for_restore.restore(sess, pretrain_ckpt.model_checkpoint_path)
-    
-      def setUp(self):
-    self._string_data = '\n'.join(
-        [' hello there i am',
-         ' rain as day',
-         ' want some cheesy puffs ?'])
-    
-    def four_lines_data():
-  text = StringIO(FOUR_LINES)
-    
-      def testSeqAE(self):
-    FLAGS.use_seq2seq_autoencoder = True
-    graphs.VatxtModel().language_model_training()
-    
-                    if self.eot[s] >= 0:
-                    #print 'EOT to %d' % self.eot[s]
-                    
-                    s = self.eot[s]
-                    input.consume()
-                    continue
-    
-            # What is index of token/char were we looking at when the error
-        # occurred?
-        self.index = None
-    
-            video_id = match1(html, r'data-brightcove-id='(\d+)'')
         
-        assert account_number, video_id
+def decode_args(args, stdin_encoding):
+    '''
+    Convert all bytes args to str
+    by decoding them using stdin encoding.
     
-        theplatform_download_by_pid(pid, title, output_dir=output_dir, merge=merge, info_only=info_only)
+            Keyword:                   'bold #004461',   # class: 'k'
+        Keyword.Constant:          'bold #004461',   # class: 'kc'
+        Keyword.Declaration:       'bold #004461',   # class: 'kd'
+        Keyword.Namespace:         'bold #004461',   # class: 'kn'
+        Keyword.Pseudo:            'bold #004461',   # class: 'kp'
+        Keyword.Reserved:          'bold #004461',   # class: 'kr'
+        Keyword.Type:              'bold #004461',   # class: 'kt'
     
-        def extract(self, **kwargs):
-        for i in self.streams:
-            s = self.streams[i]
-            _, s['container'], s['size'] = url_info(s['url'])
-            s['src'] = [s['url']]
     
-        def extract(self, **kwargs):
-        for i in self.streams:
-            # for each available stream
-            s = self.streams[i]
-            # fill in 'container' field and 'size' field (optional)
-            _, s['container'], s['size'] = url_info(s['url'])
-            # 'src' field is a list of processed urls for direct downloading
-            # usually derived from 'url'
-            s['src'] = [s['url']]
+def dispatch_hook(key, hooks, hook_data, **kwargs):
+    '''Dispatches a hook dictionary on a given piece of data.'''
+    hooks = hooks or dict()
+    hooks = hooks.get(key)
+    if hooks:
+        if hasattr(hooks, '__call__'):
+            hooks = [hooks]
+        for hook in hooks:
+            _hook_data = hook(hook_data, **kwargs)
+            if _hook_data is not None:
+                hook_data = _hook_data
+    return hook_data
+
     
-        # set up the templating system
-    config['pylons.app_globals'].mako_lookup = TemplateLookup(
-        directories=paths['templates'],
-        error_handler=handle_mako_error,
-        module_directory=module_directory,
-        input_encoding='utf-8',
-        default_filters=['conditional_websafe'],
-        filesystem_checks=getattr(g, 'reload_templates', False),
-        imports=[
-            'from r2.lib.filters import websafe, unsafe, conditional_websafe',
-            'from pylons import request',
-            'from pylons import tmpl_context as c',
-            'from pylons import app_globals as g',
-            'from pylons.i18n import _, ungettext',
-        ],
-        modulename_callable=mako_module_path,
+        def prepare_content_length(self, body):
+        '''Prepare Content-Length header based on request method and body'''
+        if body is not None:
+            length = super_len(body)
+            if length:
+                # If length exists, set it. Otherwise, we fallback
+                # to Transfer-Encoding: chunked.
+                self.headers['Content-Length'] = builtin_str(length)
+        elif self.method not in ('GET', 'HEAD') and self.headers.get('Content-Length') is None:
+            # Set Content-Length to 0 for methods that can have a body
+            # but don't provide one. (i.e. not GET or HEAD)
+            self.headers['Content-Length'] = '0'
+    
+        get_item_parameters = pytest.mark.parametrize(
+        'key, value', (
+            ('bad_gateway', 502),
+            ('not_a_key', None)
+        )
     )
     
-    from r2.controllers.api_docs import api_doc, api_section
-from r2.controllers.oauth2 import require_oauth2_scope
-from r2.controllers.reddit_base import OAuth2OnlyController
-from r2.lib.jsontemplates import (
-    FriendTableItemJsonTemplate,
-    get_usertrophies,
-    IdentityJsonTemplate,
-    KarmaListJsonTemplate,
-    PrefsJsonTemplate,
-)
-from r2.lib.pages import FriendTableItem
-from r2.lib.validator import (
-    validate,
-    VAccountByName,
-    VFriendOfMine,
-    VLength,
-    VList,
-    VUser,
-    VValidatedJSON,
-)
-from r2.models import Account, Trophy
-import r2.lib.errors as errors
-import r2.lib.validator.preferences as vprefs
+        def test_server_finishes_when_no_connections(self):
+        '''the server thread exits even if there are no connections'''
+        server = Server.basic_response_server()
+        with server:
+            pass
     
-            res = AdminPage(content = AdminAwardGive(award, recipient, desc,
-                                                 url, hours),
-                        title='give an award').render()
-        return res
+        # Informational.
+    100: ('continue',),
+    101: ('switching_protocols',),
+    102: ('processing',),
+    103: ('checkpoint',),
+    122: ('uri_too_long', 'request_uri_too_long'),
+    200: ('ok', 'okay', 'all_ok', 'all_okay', 'all_good', '\\o/', '✓'),
+    201: ('created',),
+    202: ('accepted',),
+    203: ('non_authoritative_info', 'non_authoritative_information'),
+    204: ('no_content',),
+    205: ('reset_content', 'reset'),
+    206: ('partial_content', 'partial'),
+    207: ('multi_status', 'multiple_status', 'multi_stati', 'multiple_stati'),
+    208: ('already_reported',),
+    226: ('im_used',),
     
-            c.allow_framing = True
     
-    import json
-import os
+@pytest.mark.parametrize(
+    'value, expected', (
+        ('foo='is a fish', bar='as well'', {'foo': 'is a fish', 'bar': 'as well'}),
+        ('key_without_value', {'key_without_value': None})
+    ))
+def test_parse_dict_header(value, expected):
+    assert parse_dict_header(value) == expected
+    
+        close_server = threading.Event()
+    server = Server(response_handler, wait_to_close_event=close_server)
+    
+    
+def _serf_client():
+    env = EnvironmentConfig()
+    return SerfClient(host=env.host, port=env.port, rpc_auth=env.auth_key)
+    
+        # If we get to this point, we know that any included ranges are valid.
+    # If the caller is prepared to handle them, all is well.
+    # Otherwise we treat it as a parse failure.
+    if not allow_ranges and '[' in host:
+        raise AnsibleParserError('Detected range in host but was asked to ignore ranges')
+    
+    from ansible.plugins.terminal import TerminalBase
+from ansible.errors import AnsibleConnectionFailure
+    
+        for data_format in ['channels_first', 'channels_last']:
+        if data_format == 'channels_first':
+            shape = (3, 5, 5)
+            target_shape = (5, 5, 3)
+            prev_shape = (2, 3, 2)
+            flip = lambda x: np.flip(np.flip(x, axis=2), axis=3)
+            transpose = lambda x: np.transpose(x, (0, 2, 3, 1))
+            target_data_format = 'channels_last'
+        elif data_format == 'channels_last':
+            shape = (5, 5, 3)
+            target_shape = (3, 5, 5)
+            prev_shape = (2, 2, 3)
+            flip = lambda x: np.flip(np.flip(x, axis=1), axis=2)
+            transpose = lambda x: np.transpose(x, (0, 3, 1, 2))
+            target_data_format = 'channels_first'
+    
+    
+@keras_test
+def test_temporal_classification_functional():
+    '''
+    Classify temporal sequences of float numbers
+    of length 3 into 2 classes using
+    single layer of GRU units and softmax applied
+    to the last activations of the units
+    '''
+    np.random.seed(1337)
+    (x_train, y_train), (x_test, y_test) = get_test_data(num_train=200,
+                                                         num_test=20,
+                                                         input_shape=(3, 4),
+                                                         classification=True,
+                                                         num_classes=2)
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+    
+    
+def test_softmax_valid():
+    '''Test using a reference implementation of softmax.
+    '''
+    def softmax(values):
+        m = np.max(values)
+        e = np.exp(values - m)
+        return e / np.sum(e)
