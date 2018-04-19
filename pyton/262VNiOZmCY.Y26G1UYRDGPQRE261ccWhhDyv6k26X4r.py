d362a472432c2fa07da1a49f813c344ff84a2607
SUNsung <weatherius@gmail.com>
2018-04-19 07:59:44 +0300

    Rewrite [262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py] File in: 2018-04-19 07:59:43

--- pyton/262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py
+++ pyton/262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py
@@ -1,118 +1,187 @@
 
         
-            return inner
+                def get_auth(self, username=None, password=None):
+            assert username is None
+            assert password is None
+            assert self.raw_auth == BASIC_AUTH_HEADER_VALUE
+            return basic_auth(self.raw_auth)
     
-        :param url: URL for the new :class:`Request` object.
-    :param data: (optional) Dictionary (will be form-encoded), bytes, or file-like object to send in the body of the :class:`Request`.
-    :param json: (optional) json data to send in the body of the :class:`Request`.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
+        def iter_body(self):
+        first_chunk = True
+        iter_lines = self.msg.iter_lines(self.CHUNK_SIZE)
+        for line, lf in iter_lines:
+            if b'\0' in line:
+                if first_chunk:
+                    converter = self.conversion.get_converter(self.mime)
+                    if converter:
+                        body = bytearray()
+                        # noinspection PyAssignmentToLoopOrWithParameter
+                        for line, lf in chain([(line, lf)], iter_lines):
+                            body.extend(line)
+                            body.extend(lf)
+                        self.mime, body = converter.convert(body)
+                        assert isinstance(body, str)
+                        yield self.process_body(body)
+                        return
+                raise BinarySuppressedError()
+            yield self.process_body(line) + lf
+            first_chunk = False
+    
+    
+with codecs.open(FILE_PATH, encoding='utf8') as f:
+    # Strip because we don't want new lines in the data so that we can
+    # easily count occurrences also when embedded in JSON (where the new
+    # line would be escaped).
+    FILE_CONTENT = f.read().strip()
+    
+        exc = ConnectionError('Connection aborted')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
+    assert ret == ExitStatus.ERROR
+    assert error_msg == (
+        'ConnectionError: '
+        'Connection aborted while doing GET request to URL: '
+        'http://www.google.com')
+    
     '''
+from utils import http, HTTP_OK
+from fixtures import UNICODE
     
-        def __getitem__(self, key):
-        # We allow fall-through here, so values default to None
+            Referer:http://httpie.org  Cookie:foo=bar  User-Agent:bacon/1.0
     
-            :param name: a string containing name of cookie
-        :param domain: (optional) string containing domain of cookie
-        :param path: (optional) string containing path of cookie
-        :raises KeyError: if cookie is not found
-        :raises CookieConflictError: if there are multiple cookies
-            that match name and optionally domain and path
-        :return: cookie.value
-        '''
-        toReturn = None
-        for cookie in iter(self):
-            if cookie.name == name:
-                if domain is None or cookie.domain == domain:
-                    if path is None or cookie.path == path:
-                        if toReturn is not None:  # if there are multiple cookies that meet passed in criteria
-                            raise CookieConflictError('There are multiple cookies with name, %r' % (name))
-                        toReturn = cookie.value  # we will eventually return this as long as no cookie conflict
-    
-            auth = HTTPDigestAuth('user', 'wrongpass')
-        url = httpbin('digest-auth', 'auth', 'user', 'pass')
-    
-    current_path = os.path.dirname(os.path.abspath(__file__))
-root_path = os.path.abspath(os.path.join(current_path, os.pardir, os.pardir, os.pardir))
-data_path = os.path.abspath(os.path.join(root_path, os.pardir, os.pardir, 'data', 'gae_proxy'))
-if not os.path.isdir(data_path):
-    data_path = current_path
-    
-                            #print 'no viable alt'
-                        self.noViableAlt(s, input)
-                        return 0
-    
-            if self._state is None:
-            # no shared state work to do
-            return
-        
-        # wack Lexer state variables
-        self._state.token = None
-        self._state.type = INVALID_TOKEN_TYPE
-        self._state.channel = DEFAULT_CHANNEL
-        self._state.tokenStartCharIndex = -1
-        self._state.tokenStartLine = -1
-        self._state.tokenStartCharPositionInLine = -1
-        self._state.text = None
-    
-    URLS = ['http://www.google.com/',
-        'http://www.apple.com/',
-        'http://www.ibm.com',
-        'http://www.thisurlprobablydoesnotexist.com',
-        'http://www.slashdot.org/',
-        'http://www.python.org/',
-        'http://www.bing.com/',
-        'http://www.facebook.com/',
-        'http://www.yahoo.com/',
-        'http://www.youtube.com/',
-        'http://www.blogger.com/']
-    
-      return {
-    'filepath': current_filepath,
-    'line_num': line + 1,
-    'column_num': column + 1,
-    'working_dir': working_dir,
-    'file_data': vimsupport.GetUnsavedAndSpecifiedBufferData( current_buffer,
-                                                              current_filepath )
-  }
+        def _get_path(self):
+        '''Return the config file path without side-effects.'''
+        raise NotImplementedError()
     
     
-def _FormatCompleterDebugInfo( completer ):
-  message = '{0} completer debug information:\n'.format( completer[ 'name' ] )
-  for server in completer[ 'servers' ]:
-    name = server[ 'name' ]
-    if server[ 'is_running' ]:
-      address = server[ 'address' ]
-      port = server[ 'port' ]
-      if address and port:
-        message += '  {0} running at: http://{1}:{2}\n'.format( name,
-                                                                address,
-                                                                port )
-      else:
-        message += '  {0} running\n'.format( name )
-      message += '  {0} process ID: {1}\n'.format( name, server[ 'pid' ] )
-    else:
-      message += '  {0} not running\n'.format( name )
-    message += '  {0} executable: {1}\n'.format( name, server[ 'executable'] )
-    logfiles = server[ 'logfiles' ]
-    if logfiles:
-      message += '  {0} logfiles:\n'.format( name )
-      for logfile in logfiles:
-        message += '    {0}\n'.format( logfile )
-    else:
-      message += '  No logfiles available\n'
-    if 'extras' in server:
-      for extra in server[ 'extras' ]:
-        message += '  {0} {1}: {2}\n'.format( name,
-                                              extra[ 'key' ],
-                                              extra[ 'value' ] )
-  for item in completer[ 'items' ]:
-    message += '  {0}: {1}\n'.format( item[ 'key' ].capitalize(),
-                                      item[ 'value' ] )
-  return message
+class PluginManager(object):
+    
+        @property
+    def cookies(self):
+        jar = RequestsCookieJar()
+        for name, cookie_dict in self['cookies'].items():
+            jar.set_cookie(create_cookie(
+                name, cookie_dict.pop('value'), **cookie_dict))
+        jar.clear_expired_cookies()
+        return jar
     
     
-  def Start( self ):
-    with HandleServerException( display = False ):
-      self.PostDataToHandler( {}, 'shutdown', TIMEOUT_SECONDS )
+@pytest.mark.functional
+def test_with_confirmation(proc, TIMEOUT):
+    with_confirmation(proc, TIMEOUT)
+    
+      def testSimple(self):
+    labels = [9, 3, 0]
+    records = [self._record(labels[0], 0, 128, 255),
+               self._record(labels[1], 255, 0, 1),
+               self._record(labels[2], 254, 255, 0)]
+    contents = b''.join([record for record, _ in records])
+    expected = [expected for _, expected in records]
+    filename = os.path.join(self.get_temp_dir(), 'cifar')
+    open(filename, 'wb').write(contents)
+    
+    import argparse
+import tensorflow as tf
+    
+    
+def main(_):
+  tf.logging.set_verbosity(tf.logging.INFO)
+  tf.logging.info('Assigning vocabulary ids...')
+  vocab_ids = make_vocab_ids(
+      FLAGS.vocab_file or os.path.join(FLAGS.output_dir, 'vocab.txt'))
+    
+    # Flags controlling input are in document_generators.py
+    
+      def testBidirLM(self):
+    graphs.VatxtBidirModel().language_model_graph()
+    
+    Computational time:
+  2 days to train 100000 steps on 1 layer 1024 hidden units LSTM,
+  256 embeddings, 400 truncated BP, 256 minibatch and on single GPU (Pascal
+  Titan X, cuDNNv5).
+'''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+    
+    
+def _landscape_client():
+    env = EnvironmentConfig()
+    return API(
+        uri=env.uri,
+        access_key=env.access_key,
+        secret_key=env.secret_key,
+        ssl_ca_file=env.ssl_ca_file)
+    
+            # global, resource
+        input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/global/urlMaps'
+        actual = GCPUtils.parse_gcp_url(input_url)
+        self.assertEquals('compute', actual['service'])
+        self.assertEquals('v1', actual['api_version'])
+        self.assertEquals('myproject', actual['project'])
+        self.assertTrue('global' in actual)
+        self.assertTrue(actual['global'])
+        self.assertEquals('urlMaps', actual['resource_name'])
+    
+    class StatisticsError(ValueError):
+    pass
+    
+        def test_keys_reuse(self):
+        s = '[{'a_key': 1, 'b_\xe9': 2}, {'a_key': 3, 'b_\xe9': 4}]'
+        self.check_keys_reuse(s, self.loads)
+        self.check_keys_reuse(s, self.json.decoder.JSONDecoder().decode)
+    
+    import re
+    
+        def assertAlmostEqual(self, a, b):
+        if isinstance(a, complex):
+            if isinstance(b, complex):
+                unittest.TestCase.assertAlmostEqual(self, a.real, b.real)
+                unittest.TestCase.assertAlmostEqual(self, a.imag, b.imag)
+            else:
+                unittest.TestCase.assertAlmostEqual(self, a.real, b)
+                unittest.TestCase.assertAlmostEqual(self, a.imag, 0.)
+        else:
+            if isinstance(b, complex):
+                unittest.TestCase.assertAlmostEqual(self, a, b.real)
+                unittest.TestCase.assertAlmostEqual(self, 0., b.imag)
+            else:
+                unittest.TestCase.assertAlmostEqual(self, a, b)
+    
+        def _create_infile(self):
+        infile = support.TESTFN
+        with open(infile, 'w') as fp:
+            self.addCleanup(os.remove, infile)
+            fp.write(self.data)
+        return infile
+    
+            a.append(100)
+        self.assertEqual(x[:], list(range(16)))
+    
+        @require_oauth2_scope('subscribe')
+    @validate(
+        VUser(),
+        friend_rel=VFriendOfMine('username'),
+    )
+    @api_doc(api_section.users, uri='/api/v1/me/friends/{username}')
+    def DELETE_friends(self, friend_rel):
+        '''Stop being friends with a user.'''
+        c.user.remove_friend(friend_rel._thing2)
+        if c.user.gold:
+            c.user.friend_rels_cache(_update=True)
+        response.status = 204
+
+    
+    class ButtonsController(RedditController):
+    def get_wrapped_link(self, url, link = None, wrapper = None):
+        try:
+            links = []
+            if link:
+                links = [link]
+            else:
+                sr = None if isinstance(c.site, FakeSubreddit) else c.site
+                try:
+                    links = Link._by_url(url, sr)
+                except NotFound:
+                    pass
