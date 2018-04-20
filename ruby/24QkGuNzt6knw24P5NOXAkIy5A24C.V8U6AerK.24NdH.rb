74b5c539f45c6f3e0792ce023fe9223596243735
SUNsung <weatherius@gmail.com>
2018-04-20 03:55:21 +0300

    Rewrite [24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb] File in: 2018-04-20 03:55:20

--- ruby/24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb
+++ ruby/24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb
@@ -1,53 +1,67 @@
 
         
-          describe :split_keywords do
-    context 'only commas' do
-      let(:keywords) { 'One,Two, Three, Four Token,' }
+              # Topic may be hard deleted due to spam, no point complaining
+      # we would have to look at the topics table id sequence to find cases
+      # where this was called with an invalid id, no point really
+      return unless topic.present?
     
-        find_union(segments, Group).order_id_desc
+      def revoke_admin!
+    set_permission('admin', false)
   end
     
-      def execute
-    Gitlab::Metrics.measure(:import_export_clean_up) do
-      return unless File.directory?(path)
-    
-        def valid_type?(type)
-      const_get(:TYPES).include?(type)
-    end
-    
-      def validate_option(method)
-    if self.respond_to? 'validate_#{method}'.to_sym
-      self.send('validate_#{method}'.to_sym)
-    else
-      false
-    end
-  end
-    
-      included do
-    include Oauthable
-    
-      def load_event
-    @event = current_user.events.find(params[:id])
+      def cache_fragment(name)
+    ApplicationSerializer.fragment_cache[name] ||= yield
   end
 end
 
     
-            # Unregisters a SIGINT handler.
-        def unregister(sig_callback)
-          @@mutex.synchronize do
-            registered.delete(sig_callback)
+        # Returns a String describing the file's content type
+    def detect
+      if blank_name?
+        SENSIBLE_DEFAULT
+      elsif empty_file?
+        EMPTY_TYPE
+      elsif calculated_type_matches.any?
+        calculated_type_matches.first
+      else
+        type_from_file_contents || SENSIBLE_DEFAULT
+      end.to_s
+    end
     
-            opts = OptionParser.new do |o|
-          o.banner = 'Usage: vagrant cap [options] TYPE NAME [args]'
-          o.separator ''
-          o.separator 'This is an advanced command. If you don't know what this'
-          o.separator 'does and you aren't explicitly trying to use it, you probably'
-          o.separator 'don't want to use this.'
-          o.separator ''
-          o.separator 'This command checks or executes arbitrary capabilities that'
-          o.separator 'Vagrant has for hosts, guests, and providers.'
-          o.separator ''
-          o.separator 'Options:'
-          o.separator ''
+        def add_active_record_callbacks
+      name = @name
+      @klass.send(:after_save) { send(name).send(:save) }
+      @klass.send(:before_destroy) { send(name).send(:queue_all_for_delete) }
+      if @klass.respond_to?(:after_commit)
+        @klass.send(:after_commit, on: :destroy) do
+          send(name).send(:flush_deletes)
+        end
+      else
+        @klass.send(:after_destroy) { send(name).send(:flush_deletes) }
+      end
+    end
     
-    SPREE_GEMS = %w(core api cmd backend frontend sample).freeze
+            def responds?
+          methods = @subject.instance_methods.map(&:to_s)
+          methods.include?('#{@attachment_name}') &&
+            methods.include?('#{@attachment_name}=') &&
+            methods.include?('#{@attachment_name}?')
+        end
+    
+        def processor(name) #:nodoc:
+      @known_processors ||= {}
+      if @known_processors[name.to_s]
+        @known_processors[name.to_s]
+      else
+        name = name.to_s.camelize
+        load_processor(name) unless Paperclip.const_defined?(name)
+        processor = Paperclip.const_get(name)
+        @known_processors[name.to_s] = processor
+      end
+    end
+    
+          def drop_attached_file(*args)
+        ActiveSupport::Deprecation.warn 'Method `drop_attached_file` in the migration has been deprecated and will be replaced by `remove_attachment`.'
+        remove_attachment(*args)
+      end
+    end
