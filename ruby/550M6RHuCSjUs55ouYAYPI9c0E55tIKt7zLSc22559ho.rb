4566381598a997847d220d417062d7e4a80673a0
SUNsung <weatherius@gmail.com>
2018-04-20 23:05:47 +0300

    Rewrite [550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb] File in: 2018-04-20 23:05:46

--- ruby/550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb
+++ ruby/550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb
@@ -1,40 +1,105 @@
 
         
-            def cropping dst, ratio, scale
-      if ratio.horizontal? || ratio.square?
-        '%dx%d+%d+%d' % [ dst.width, dst.height, 0, (self.height * scale - dst.height) / 2 ]
-      else
-        '%dx%d+%d+%d' % [ dst.width, dst.height, (self.width * scale - dst.width) / 2, 0 ]
+            def defaults_deprecate_type(old, current)
+      Jekyll.logger.warn 'Defaults:', 'The '#{old}' type has become '#{current}'.'
+      Jekyll.logger.warn 'Defaults:', 'Please update your front-matter defaults to use \
+                        'type: #{current}'.'
+    end
+  end
+end
+
+    
+            parsed_expr = parse_expression(expression)
+        @context.stack do
+          groups = input.group_by do |item|
+            @context[variable] = item
+            parsed_expr.render(@context)
+          end
+          grouped_array(groups)
+        end
       end
+    
+          if site.theme && site.theme.layouts_path.eql?(base)
+        @base_dir = site.theme.root
+        @path = site.in_theme_dir(base, name)
+      else
+        @base_dir = site.source
+        @path = site.in_source_dir(base, name)
+      end
+      @relative_path = @path.sub(@base_dir, '')
+    
+    
+# extract label addresses
+addrs = {}
+dtrans.each_line { |ln|
+	if ln =~ /;[^ ].*:/
+		parts = ln.split(' ')
+		label = parts[1]
+		label = label.slice(1,label.index(':')-1)
+		addr = parts[0].split(':')[1].to_i(16)
+		#puts '%s => %x' % [label, addr]
+		one = { label => addr }
+		addrs.merge!(one)
+	end
+}
+#puts addrs.inspect
+    
+    
+
+    
+        # Returns a representation of the query as an array of strings and
+    # potentially {Sass::Script::Tree::Node}s (if there's interpolation in it).
+    # When the interpolation is resolved and the strings are joined together,
+    # this will be the string representation of this query.
+    #
+    # @return [Array<String, Sass::Script::Tree::Node>]
+    def to_a
+      Sass::Util.intersperse(queries.map {|q| q.to_a}, ', ').flatten
+    end
+    
+        # True if the dimensions represent a vertical rectangle
+    def vertical?
+      height > width
+    end
+    
+        # Returns the extension of the file. e.g. 'jpg' for 'file.jpg'
+    # If the style has a format defined, it will return the format instead
+    # of the actual extension.
+    def extension attachment, style_name
+      ((style = attachment.styles[style_name.to_s.to_sym]) && style[:format]) ||
+        File.extname(attachment.original_filename).sub(/\A\.+/, ''.freeze)
     end
     
             protected
     
-            def failure_message
-          'Attachment #{@attachment_name} should be required'
-        end
-    
-            def lower_than_low?
-          @low.nil? || !passes_validation_with_size(@low - 1)
-        end
-    
-    module Paperclip
-  class << self
-    attr_writer :registered_attachments_styles_path
-    def registered_attachments_styles_path
-      @registered_attachments_styles_path ||= Rails.root.join('public/system/paperclip_attachments.yml').to_s
+        def clear_processors!
+      @known_processors.try(:clear)
     end
-  end
     
           if app.config.respond_to?(:paperclip_defaults)
         Paperclip::Attachment.default_options.merge!(app.config.paperclip_defaults)
       end
     end
     
-          def drop_attached_file(*args)
-        ActiveSupport::Deprecation.warn 'Method `drop_attached_file` in the migration has been deprecated and will be replaced by `remove_attachment`.'
-        remove_attachment(*args)
+            attachment_names.each do |attachment_name|
+          COLUMNS.keys.each do |column_name|
+            remove_column(table_name, '#{attachment_name}_#{column_name}')
+          end
+        end
       end
-    end
     
-      TYPES = [ 'input', 'filter', 'output', 'codec' ]
+              def find_plugins_gem_specs
+            @specs ||= ::Gem::Specification.find_all.select{|spec| logstash_plugin_gem_spec?(spec)}
+          end
+    
+      def setup_multi_encode!
+    @has_encode_sync = self.methods.include?(:encode_sync)
+    
+      def seventy_five_percent
+    [1, (maximum * 0.75)].max.floor
+  end
+    
+      module Branch2
+    def if
+      elements[0]
+    end
