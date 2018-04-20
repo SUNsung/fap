1efaa48a24686213ea26c1b4b2e50bd1d39097af
SUNsung <weatherius@gmail.com>
2018-04-20 15:08:38 +0300

    Rewrite [26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb] File in: 2018-04-20 15:08:37

--- ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
+++ ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
@@ -1,34 +1,121 @@
 
         
-          # True if a {Formula} is being built with a specific option
-  # (which isn't named `with-*` or `without-*`).
-  # @deprecated
-  def include?(name)
-    @args.include?('--#{name}')
+            def matching_username
+      Account.where(Account.arel_table[:username].lower.eq username.to_s.downcase)
+    end
+    
+        # specify git tag or commit hash here
+    def source_tag
+      nil
+    end
+    
+        respond_to do |format|
+      format.html do
+        serializable_resource = ActiveModelSerializers::SerializableResource.new(InitialStatePresenter.new(initial_state_params), serializer: InitialStateSerializer)
+        @initial_state_json   = serializable_resource.to_json
+      end
+    
+          expect(response).to redirect_to(settings_notifications_path)
+      user.reload
+      expect(user.settings['notification_emails']['follow']).to be true
+      expect(user.settings['interactions']['must_be_follower']).to be false
+    end
+  end
+end
+
+    
+        uninstall quit: 'bundle.id.goes.here'
+    
+    module Hardware
+  describe CPU do
+    describe '::type' do
+      it 'returns the current CPU's type as a symbol, or :dunno if it cannot be detected' do
+        expect(
+          [
+            :intel,
+            :ppc,
+            :dunno,
+          ],
+        ).to include(described_class.type)
+      end
+    end
+    
+      def login!
+    print 'GitHub User: '
+    ENV['HOMEBREW_GITHUB_API_USERNAME'] = $stdin.gets.chomp
+    print 'Password: '
+    ENV['HOMEBREW_GITHUB_API_PASSWORD'] = noecho_gets.chomp
+    puts
   end
     
-          Find.prune if @f.skip_clean? path
+        if ARGV.dry_run?
+      if ARGV.include? '--strict'
+        ohai 'brew audit --strict #{formula.path.basename}'
+      elsif ARGV.include? '--audit'
+        ohai 'brew audit #{formula.path.basename}'
+      end
+    else
+      failed_audit = false
+      if ARGV.include? '--strict'
+        system HOMEBREW_BREW_FILE, 'audit', '--strict', formula.path
+        failed_audit = !$CHILD_STATUS.success?
+      elsif ARGV.include? '--audit'
+        system HOMEBREW_BREW_FILE, 'audit', formula.path
+        failed_audit = !$CHILD_STATUS.success?
+      end
+      if failed_audit
+        formula.path.atomic_write(backup_file)
+        odie 'brew audit failed!'
+      end
+    end
     
-            def run
-          UI.puts('$CACHE_ROOT: #{@cache.root}') if @short_output
-          if @pod_name.nil? # Print all
-            @cache.cache_descriptors_per_pod.each do |pod_name, cache_descriptors|
-              print_pod_cache_infos(pod_name, cache_descriptors)
-            end
-          else # Print only for the requested pod
-            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
-            if cache_descriptors.nil?
-              UI.notice('No cache for pod named #{@pod_name} found')
-            else
-              print_pod_cache_infos(@pod_name, cache_descriptors)
-            end
+            def log_level(name, options = {})
+          if options[:prepend]
+            level = log_levels.values.min
+            level = level.nil? ? 0 : level - 1
+          else
+            level = log_levels.values.max
+            level = level.nil? ? 0 : level + 1
           end
+          log_levels.update(name => level)
+          define_logger(name)
         end
     
-      context 'called with one style' do
-    it 'applies same style to all sides' do
-      rule = 'border-style: solid'
+        # @see \{MediaQuery#to\_a}
+    def to_a
+      res = []
+      res += modifier
+      res << ' ' unless modifier.empty?
+      res += type
+      res << ' and ' unless type.empty? || expressions.empty?
+      res += Sass::Util.intersperse(expressions, ' and ').flatten
+      res
+    end
     
-          expect('.padding-explicit').to have_rule(rule)
+    module Jekyll
+    
+      class RenderPartialTag < Liquid::Tag
+    include OctopressFilters
+    def initialize(tag_name, markup, tokens)
+      @file = nil
+      @raw = false
+      if markup =~ /^(\S+)\s?(\w+)?/
+        @file = $1.strip
+        @raw = $2 == 'raw'
+      end
+      super
+    end
+    
+      context 'called with three widths' do
+    it 'applies second width to left and right' do
+      rule = 'border-width: 4px 5px 6px'
+    
+      context 'called with argument (2)' do
+    it 'outputs the second value from the default scale' do
+      expect('.two-base-one').to have_rule('font-size: 1.5625em')
+    end
+  end
+    
+          expect('.size-both').to have_ruleset(rule)
     end
   end
