9b22a082ec9bf829de9578d63dac178d243bc352
SUNsung <weatherius@gmail.com>
2018-04-20 19:01:02 +0300

    Rewrite [23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp] File in: 2018-04-20 19:01:01

--- cccccc/23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp
+++ cccccc/23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp
@@ -1,134 +1,86 @@
 
         
-        #include <string>
-    
-    
-    {  content::WebContents* web_contents_;  // weak
-  SavePageCallback callback_;
-};
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(LoginHandler);
-};
-    
-    #include 'base/memory/ref_counted.h'
-#include 'net/url_request/url_request_job_factory.h'
-    
-    #endif  // ATOM_BROWSER_NET_HTTP_PROTOCOL_HANDLER_H_
-
-    
-    class URLRequestStringJob : public JsAsker<net::URLRequestSimpleJob> {
- public:
-  URLRequestStringJob(net::URLRequest*, net::NetworkDelegate*);
-    }
-    
-    #include <vector>
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    
-    {  /**
-   * @brief Computes the error gradient w.r.t. the BNLL inputs.
-   *
-   * @param top output Blob vector (length 1), providing the error gradient with
-   *      respect to the outputs
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
-   *      with respect to computed outputs @f$ y @f$
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$; Backward fills their diff with
-   *      gradients @f$
-   *        \frac{\partial E}{\partial x}
-   *      @f$ if propagate_down[0]
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-};
-    
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    
-    {}  // namespace caffe
-    
-    
-    { protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual inline bool reverse_dimensions() { return true; }
-  virtual void compute_output_shape();
-};
-    
-     protected:
-  /**
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the computed outputs. At training time, we have @f$
-   *      y_{\mbox{train}} = \left\{
-   *         \begin{array}{ll}
-   *            \frac{x}{1 - p} & \mbox{if } u > p \\
-   *            0 & \mbox{otherwise}
-   *         \end{array} \right.
-   *      @f$, where @f$ u \sim U(0, 1)@f$ is generated independently for each
-   *      input at each iteration. At test time, we simply have
-   *      @f$ y_{\mbox{test}} = \mathbb{E}[y_{\mbox{train}}] = x @f$.
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    }
-    
-      virtual inline const char* type() const { return 'Eltwise'; }
-  virtual inline int MinBottomBlobs() const { return 2; }
-  virtual inline int ExactNumTopBlobs() const { return 1; }
-    
-      /**
-   * @brief Computes the error gradient w.r.t. the forwarded inputs.
-   *
-   * @param top output Blob vector (length 1+), providing the error gradient with
-   *        respect to the outputs
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2+), into which the top error
-   *        gradient is copied
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    template<> AT_API Half convert(float f);
-template<> AT_API float convert(Half f);
-template<> AT_API Half convert(double f);
-template<> AT_API double convert(Half f);
-template<> AT_API Half convert(int64_t f);
-template<> AT_API int64_t convert(Half f);
-    
-    std::size_t ${Storage}::elementSize() const {
-  return sizeof(${ScalarType});
+        void leveldb_options_set_max_open_files(leveldb_options_t* opt, int n) {
+  opt->rep.max_open_files = n;
 }
     
-    #include 'python_numbers.h'
-#include 'torch/csrc/Exceptions.h'
+    #include 'db/filename.h'
+#include 'db/db_impl.h'
+#include 'db/dbformat.h'
+#include 'leveldb/env.h'
+#include 'leveldb/iterator.h'
+#include 'port/port.h'
+#include 'util/logging.h'
+#include 'util/mutexlock.h'
+#include 'util/random.h'
+    
+    // Return a new iterator that converts internal keys (yielded by
+// '*internal_iter') that were live at the specified 'sequence' number
+// into appropriate user keys.
+extern Iterator* NewDBIterator(
+    DBImpl* db,
+    const Comparator* user_key_comparator,
+    Iterator* internal_iter,
+    SequenceNumber sequence,
+    uint32_t seed);
+    
+    // Attempt to parse an internal key from 'internal_key'.  On success,
+// stores the parsed data in '*result', and returns true.
+//
+// On error, returns false, leaves '*result' in an undefined state.
+extern bool ParseInternalKey(const Slice& internal_key,
+                             ParsedInternalKey* result);
+    
+    
+    {  delete iter;
+  delete table;
+  delete file;
+  return Status::OK();
+}
+    
+    // Return the legacy file name for an sstable with the specified number
+// in the db named by 'dbname'. The result will be prefixed with
+// 'dbname'.
+extern std::string SSTTableFileName(const std::string& dbname, uint64_t number);
+    
+      // If a seek to internal key 'k' in specified file finds an entry,
+  // call (*handle_result)(arg, found_key, found_value).
+  Status Get(const ReadOptions& options,
+             uint64_t file_number,
+             uint64_t file_size,
+             const Slice& k,
+             void* arg,
+             void (*handle_result)(void*, const Slice&, const Slice&));
+    
+    
+    {  edit.SetComparatorName('foo');
+  edit.SetLogNumber(kBig + 100);
+  edit.SetNextFile(kBig + 200);
+  edit.SetLastSequence(kBig + 1000);
+  TestEncodeDecode(edit);
+}
+    
+    #ifndef STORAGE_LEVELDB_INCLUDE_DUMPFILE_H_
+#define STORAGE_LEVELDB_INCLUDE_DUMPFILE_H_
+    
+    DEFINE_FIND_STATIC_METHOD(KXlog_logWrite, KXlog, 'logWrite', '(Lcom/tencent/mars/xlog/Xlog$XLoggerInfo;Ljava/lang/String;)V')
+JNIEXPORT void JNICALL Java_com_tencent_mars_xlog_Xlog_logWrite
+  (JNIEnv *env, jclass, jobject _log_info, jstring _log) {
+    }
+    
+    #include 'assert/__assert.h'
+    
+    
+    {  private:
+    void* object_;
+};
+    
+    
+size_t  SimpleShortPackLength(size_t _datalen) {
+    return SimplePackLength<uint16_t>(_datalen);
+}
+    
+    #ifndef COMM_HAS_MEMBER_H_
+#define COMM_HAS_MEMBER_H_
+    
+    #include <jni.h>
