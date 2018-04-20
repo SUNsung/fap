d4d8b134b87686873e7bba3f4fbcc21b27230248
SUNsung <weatherius@gmail.com>
2018-04-20 17:31:06 +0300

    Rewrite [44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb] File in: 2018-04-20 17:31:05

--- ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
+++ ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
@@ -1,81 +1,26 @@
 
         
-        end
-
-    
-        def boolify(value)
-      agent.send(:boolify, value)
+            def self.interpolators_cache
+      @interpolators_cache ||= all.reverse!.map! { |method| [method, ':#{method}'] }
     end
     
-          # This inserts a block with the given key and value.
-      #
-      # @param [String] key
-      # @param [String] value
-      def insert(key, value)
-        # Insert the new block into the value
-        new_block = <<BLOCK
-# VAGRANT-BEGIN: #{key}
-#{value.strip}
-# VAGRANT-END: #{key}
-BLOCK
-    
-    module VagrantPlugins
-  module CommandBox
-    module Command
-      class Repackage < Vagrant.plugin('2', :command)
-        def execute
-          opts = OptionParser.new do |o|
-            o.banner = 'Usage: vagrant box repackage <name> <provider> <version>'
-          end
-    
-    class ActivityPub::Activity::Create < ActivityPub::Activity
-  SUPPORTED_TYPES = %w(Note).freeze
-  CONVERTED_TYPES = %w(Image Video Article).freeze
-    
-        def to_s
-      [to_a.join('.'), flags].join
-    end
-    
-    namespace :db do
-  namespace :migrate do
-    desc 'Setup the db or migrate depending on state of db'
-    task setup: :environment do
-      begin
-        if ActiveRecord::Migrator.current_version.zero?
-          Rake::Task['db:migrate'].invoke
-          Rake::Task['db:seed'].invoke
+            def no_error_when_valid?
+          @file = StringIO.new('.')
+          @subject.send(@attachment_name).assign(@file)
+          @subject.valid?
+          expected_message = [
+            @attachment_name.to_s.titleize,
+            I18n.t(:blank, scope: [:errors, :messages])
+          ].join(' ')
+          @subject.errors.full_messages.exclude?(expected_message)
         end
-      rescue ActiveRecord::NoDatabaseError
-        Rake::Task['db:setup'].invoke
-      else
-        Rake::Task['db:migrate'].invoke
       end
     end
   end
+end
+
     
-      has_one :icon, serializer: ActivityPub::ImageSerializer
-    
-        private
-    
-    module Paperclip
-  class << self
-    attr_writer :registered_attachments_styles_path
-    def registered_attachments_styles_path
-      @registered_attachments_styles_path ||= Rails.root.join('public/system/paperclip_attachments.yml').to_s
+          if app.config.respond_to?(:paperclip_defaults)
+        Paperclip::Attachment.default_options.merge!(app.config.paperclip_defaults)
+      end
     end
-  end
-    
-            def initialize(name, declaration_node, scope)
-          unless VARIABLE_DECLARATION_TYPES.include?(declaration_node.type)
-            raise ArgumentError,
-                  'Node type must be any of #{VARIABLE_DECLARATION_TYPES}, ' \
-                  'passed #{declaration_node.type}'
-          end
-    
-            def on_if(node)
-          return unless nested_variable_comparison?(node.condition)
-          add_offense(node)
-        end
-    
-      it 'registers an offense' do
-    inspect_source(source)
