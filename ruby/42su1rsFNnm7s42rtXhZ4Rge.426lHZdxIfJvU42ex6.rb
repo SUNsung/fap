216b604c4966706989a5493d0081370b785ba2f3
SUNsung <weatherius@gmail.com>
2018-04-20 23:46:26 +0300

    Rewrite [42su1rsFNnm7s42rtXhZ4Rge.426lHZdxIfJvU42ex6.rb] File in: 2018-04-20 23:46:25

--- ruby/42su1rsFNnm7s42rtXhZ4Rge.426lHZdxIfJvU42ex6.rb
+++ ruby/42su1rsFNnm7s42rtXhZ4Rge.426lHZdxIfJvU42ex6.rb
@@ -1,91 +1,38 @@
 
         
-                hash.each do |key, value|
-          self[convert_key(key)] = value
-        end
+            uninstall login_item: 'login item name'
+    
+          respond_with do |format|
+        format.html do
+          gon.preloads[:pods] = pods_json
+          gon.unchecked_count = Pod.unchecked.count
+          gon.version_failed_count = Pod.version_failed.count
+          gon.error_count = Pod.check_failed.count
+    
+      def create_hash(model, opts={})
+    opts[:range] ||= 1.day
+    plural = model.to_s.underscore.pluralize
+    eval(<<DATA
+      @#{plural} = {
+        :day_before => #{model}.where(:created_at => ((Time.now.midnight - #{opts[:range]*2})..Time.now.midnight - #{opts[:range]})).count,
+        :yesterday => #{model}.where(:created_at => ((Time.now.midnight - #{opts[:range]})..Time.now.midnight)).count
+      }
+      @#{plural}[:change] = percent_change(@#{plural}[:yesterday], @#{plural}[:day_before])
+DATA
+    )
+  end
+    
+          def redirect_prompt_error_display(error, error_description)
+        redirect_params_hash = {error: error, error_description: error_description, state: params[:state]}
+        redirect_fragment = redirect_params_hash.compact.map {|key, value| key.to_s + '=' + value }.join('&')
+        redirect_to params[:redirect_uri] + '?' + redirect_fragment
       end
     
-              # Verify the box exists that we want to repackage
-          box = @env.boxes.find(box_name, box_provider, '= #{box_version}')
-          if !box
-            raise Vagrant::Errors::BoxNotFoundWithProviderAndVersion,
-              name: box_name,
-              provider: box_provider.to_s,
-              version: box_version
-          end
-    
-              o.on('--check', 'Only checks for a capability, does not execute') do |f|
-            options[:check] = f
-          end
-        end
-    
-    
-    {      ary.empty? or raise 'forgotten elements: #{ary.join(', ')}'
-    }
-    
-      def self.critical_is_reset
-    # Create another thread to verify that it can call Thread.critical=
-    t = Thread.new do
-      initial_critical = Thread.critical
-      Thread.critical = true
-      Thread.critical = false
-      initial_critical == false && Thread.critical == false
+      def command_line(*options)
+    options.each { |opt| ARGV << opt }
+    subject.define_singleton_method(:exit) do |*_args|
+      throw(:system_exit, :exit)
     end
-    v = t.value
-    t.join
-    v
+    subject.run
+    subject.options
   end
-    
-      it 'runs nested ensure clauses' do
-    ScratchPad.record []
-    @outer = Thread.new do
-      begin
-        @inner = Thread.new do
-          begin
-            sleep
-          ensure
-            ScratchPad << :inner_ensure_clause
-          end
-        end
-        sleep
-      ensure
-        ScratchPad << :outer_ensure_clause
-        @inner.send(@method)
-        @inner.join
-      end
-    end
-    Thread.pass while @outer.status and @outer.status != 'sleep'
-    Thread.pass until @inner
-    Thread.pass while @inner.status and @inner.status != 'sleep'
-    @outer.send(@method)
-    @outer.join
-    ScratchPad.recorded.should include(:inner_ensure_clause)
-    ScratchPad.recorded.should include(:outer_ensure_clause)
-  end
-    
-      it 'raises a ThreadError when trying to wake up a dead thread' do
-    t = Thread.new { 1 }
-    t.join
-    lambda { t.send @method }.should raise_error(ThreadError)
-  end
-end
-
-    
-      # if rss_url already in existing opml file, use that; otherwise, do a lookup
-  rss_url = nil
-  existing_blog = xml.xpath('//outline[@htmlUrl='#{web_url}']').first if xml
-  if existing_blog
-    rss_url = existing_blog.attr('xmlUrl')
-    puts '#{name}: ALREADY HAVE'
-  end
-    
-    describe 'modular-scale' do
-  before(:all) do
-    ParserSupport.parse_file('library/modular-scale')
-  end
-    
-      context 'called with multiple prefixes' do
-    it 'applies the prefixes to the property' do
-      rule = '-moz-appearance: none; ' +
-             '-ms-appearance: none; ' +
-             'appearance: none;'
