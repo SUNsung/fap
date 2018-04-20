e281aba7ce4349db9b72c4f01c1c3912212e6a49
SUNsung <weatherius@gmail.com>
2018-04-20 09:52:23 +0300

    Rewrite [26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp] File in: 2018-04-20 09:52:23

--- cccccc/26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp
+++ cccccc/26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp
@@ -1,263 +1,96 @@
 
         
-        // FIXME: Soon this will change.
-using SubstitutionList = ArrayRef<Substitution>;
-    
-    /// Options for controlling diagnostics.
-class DiagnosticOptions {
-public:
-  /// Indicates whether textual diagnostics should use color.
-  bool UseColor = false;
-    }
-    
-    namespace swift {
-  class GenericEnvironment;
-  class SubstitutionMap;
-    }
-    
-    #include 'swift/AST/ASTContext.h'
-#include 'swift/AST/GenericEnvironment.h'
-#include 'swift/AST/Module.h'
-#include 'swift/AST/ProtocolConformance.h'
-#include 'swift/AST/SubstitutionMap.h'
-#include 'swift/AST/Types.h'
-#include 'llvm/ADT/DenseMap.h'
-    
-    static const int kValueSize = 200 * 1024;
-static const int kTotalSize = 100 * 1024 * 1024;
-static const int kCount = kTotalSize / kValueSize;
-    
-      Cache::Handle* handle = NULL;
-  Status s = FindTable(file_number, file_size, &handle);
-  if (!s.ok()) {
-    return NewErrorIterator(s);
-  }
-    
-     private:
-  Env* const env_;
-  const std::string dbname_;
-  const Options* options_;
-  Cache* cache_;
-    
-    namespace leveldb {
-    }
-    
-    
-    {    // Write to database
-    for (int i = 0; i < num_entries; i++)
+        
     {
-      const int k = (order == SEQUENTIAL) ? i : (rand_.Next() % num_entries);
-      char key[100];
-      snprintf(key, sizeof(key), '%016d', k);
-      bytes_ += value_size + strlen(key);
-      std::string cpp_key = key;
-      if (!db_->set(cpp_key, gen_.Generate(value_size).ToString())) {
-        fprintf(stderr, 'set error: %s\n', db_->error().name());
-      }
-      FinishedSingleOp();
+    {}  // namespace testing
+}  // namespace grpc
+    
+    std::unique_ptr<ScenarioResult> RunScenario(
+    const grpc::testing::ClientConfig& client_config, size_t num_clients,
+    const grpc::testing::ServerConfig& server_config, size_t num_servers,
+    int warmup_seconds, int benchmark_seconds, int spawn_local_worker_count,
+    const grpc::string& qps_server_target_override,
+    const grpc::string& credential_type, bool run_inproc);
+    
+    namespace grpc_node_generator {
     }
-  }
     
-    
-    {}  // namespace leveldb
-    
-    #include <assert.h>
-#include <stddef.h>
-#include <string.h>
-#include <string>
-    
-        for (UINT driverTypeIndex = 0; driverTypeIndex < numDriverTypes; driverTypeIndex++)
-    {
-        g_driverType = driverTypes[driverTypeIndex];
-        hr = D3D11CreateDeviceAndSwapChain(NULL, g_driverType, NULL, createDeviceFlags, featureLevels, numFeatureLevels,
-                D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &g_featureLevel, &g_pImmediateContext);
-        if (SUCCEEDED(hr))
-            break;
-    }
-    if (FAILED(hr))
-        return hr;
-    
-            std::vector<std::vector<Point> > white_contours, black_contours;
-        std::vector<Vec4i> white_hierarchy, black_hierarchy;
-        findContours(black_comp, black_contours, black_hierarchy, RETR_LIST, CHAIN_APPROX_SIMPLE);
-        findContours(white_comp, white_contours, white_hierarchy, RETR_LIST, CHAIN_APPROX_SIMPLE);
-    
-    
-    {
-    {
-    {            static __device__ __forceinline__ int atomicMax(int* address, int val)
-            {
-                return ::atomicMax(address, val);
-            }
-            static __device__ __forceinline__ float atomicMax(float* address, float val)
-            {
-            #if __CUDA_ARCH__ >= 120
-                int* address_as_i = (int*) address;
-                int old = *address_as_i, assumed;
-                do {
-                    assumed = old;
-                    old = ::atomicCAS(address_as_i, assumed,
-                        __float_as_int(::fmaxf(val, __int_as_float(assumed))));
-                } while (assumed != old);
-                return __int_as_float(old);
-            #else
-                (void) address;
-                (void) val;
-                return 0.0f;
-            #endif
-            }
-            static __device__ __forceinline__ double atomicMax(double* address, double val)
-            {
-            #if __CUDA_ARCH__ >= 130
-                unsigned long long int* address_as_ull = (unsigned long long int*) address;
-                unsigned long long int old = *address_as_ull, assumed;
-                do {
-                    assumed = old;
-                    old = ::atomicCAS(address_as_ull, assumed,
-                        __double_as_longlong(::fmax(val, __longlong_as_double(assumed))));
-                } while (assumed != old);
-                return __longlong_as_double(old);
-            #else
-                (void) address;
-                (void) val;
-                return 0.0;
-            #endif
-            }
-        };
-    }; //struct Emulation
-}}} // namespace cv { namespace cuda { namespace cudev
-    
-    CV_EXPORTS_W void divide(InputArray src1, Scalar src2, OutputArray dst, double scale=1, int dtype=-1);
-    
-    static void* openclamdblas_check_fn(int ID)
-{
-    assert(ID >= 0 && ID < (int)(sizeof(openclamdblas_fn)/sizeof(openclamdblas_fn[0])));
-    const struct DynamicFnEntry* e = openclamdblas_fn[ID];
-    void* func = CV_CL_GET_PROC_ADDRESS(e->fnName);
-    if (!func)
-    {
-        throw cv::Exception(cv::Error::OpenCLApiCallError,
-                cv::format('OpenCL AMD BLAS function is not available: [%s]', e->fnName),
-                CV_Func, __FILE__, __LINE__);
-    }
-    *(e->ppFn) = func;
-    return func;
-}
-    
-    #if GTEST_OS_SYMBIAN
-  // These are needed as the Nokia Symbian Compiler cannot decide between
-  // const T& and const T* in a function template. The Nokia compiler _can_
-  // decide between class template specializations for T and T*, so a
-  // tr1::type_traits-like is_pointer works, and we can overload on that.
-  template <typename T>
-  inline void StreamHelper(internal::true_type /*is_pointer*/, T* pointer) {
-    if (pointer == NULL) {
-      *ss_ << '(null)';
-    } else {
-      *ss_ << pointer;
-    }
-  }
-  template <typename T>
-  inline void StreamHelper(internal::false_type /*is_pointer*/,
-                           const T& value) {
-    // See the comments in Message& operator <<(const T&) above for why
-    // we need this using statement.
-    using ::operator <<;
-    *ss_ << value;
-  }
-#endif  // GTEST_OS_SYMBIAN
-    
-    class FooTest : public ::testing::TestWithParam<const char*> {
-  // You can implement all the usual class fixture members here.
+    // Data pertaining to configuration of the generator with respect to anything
+// that may be used internally at Google.
+struct GeneratorConfiguration {
+  GeneratorConfiguration();
+  grpc::string grpc_package_root;
+  // TODO(https://github.com/grpc/grpc/issues/8622): Drop this.
+  grpc::string beta_package_root;
+  // TODO(https://github.com/google/protobuf/issues/888): Drop this.
+  grpc::string import_prefix;
 };
     
-    // Used to print a pointer that is neither a char pointer nor a member
-// pointer, when the user doesn't define PrintTo() for it.  (A member
-// variable pointer or member function pointer doesn't really point to
-// a location in the address space.  Their representation is
-// implementation-defined.  Therefore they will be printed as raw
-// bytes.)
-template <typename T>
-void DefaultPrintTo(IsNotContainer /* dummy */,
-                    true_type /* is a pointer */,
-                    T* p, ::std::ostream* os) {
-  if (p == NULL) {
-    *os << 'NULL';
-  } else {
-    // C++ doesn't allow casting from a function pointer to any object
-    // pointer.
-    //
-    // IsTrue() silences warnings: 'Condition is always true',
-    // 'unreachable code'.
-    if (IsTrue(ImplicitlyConvertible<T*, const void*>::value)) {
-      // T is not a function type.  We just call << to print p,
-      // relying on ADL to pick up user-defined << for their pointer
-      // types, if any.
-      *os << p;
-    } else {
-      // T is a function type, so '*os << p' doesn't do what we want
-      // (it just prints p as bool).  We want to print p as a const
-      // void*.  However, we cannot cast it to const void* directly,
-      // even using reinterpret_cast, as earlier versions of gcc
-      // (e.g. 3.4.5) cannot compile the cast when p is a function
-      // pointer.  Casting to UInt64 first solves the problem.
-      *os << reinterpret_cast<const void*>(
-          reinterpret_cast<internal::UInt64>(p));
+    UsageTimer::Result UsageTimer::Mark() const {
+  Result s = Sample();
+  Result r;
+  r.wall = s.wall - start_.wall;
+  r.user = s.user - start_.user;
+  r.system = s.system - start_.system;
+  r.total_cpu_time = s.total_cpu_time - start_.total_cpu_time;
+  r.idle_cpu_time = s.idle_cpu_time - start_.idle_cpu_time;
+    }
+    
+    template <typename C>
+struct compare_less_equal : detail::cmp_pred<C, ordering::gt, 1> {
+  using detail::cmp_pred<C, ordering::gt, 1>::cmp_pred;
+};
+    
+    // Pass the key as folly::dynamic, without conversion
+//
+// folly::dynamic, folly::dynamic (no conversion of value, either)
+template <typename Fn> EnableForArgTypes<Fn, folly::dynamic, folly::dynamic>
+invokeForKeyValue(Fn fn, const folly::dynamic& k, const folly::dynamic& v) {
+  fn(k, v);
+}
+// folly::dynamic, int64_t
+template <typename Fn> EnableForArgTypes<Fn, folly::dynamic, int64_t>
+invokeForKeyValue(Fn fn, const folly::dynamic& k, const folly::dynamic& v) {
+  fn(k, v.asInt());
+}
+// folly::dynamic, bool
+template <typename Fn> EnableForArgTypes<Fn, folly::dynamic, bool>
+invokeForKeyValue(Fn fn, const folly::dynamic& k, const folly::dynamic& v) {
+  fn(k, v.asBool());
+}
+// folly::dynamic, double
+template <typename Fn> EnableForArgTypes<Fn, folly::dynamic, double>
+invokeForKeyValue(Fn fn, const folly::dynamic& k, const folly::dynamic& v) {
+  fn(k, v.asDouble());
+}
+// folly::dynamic, std::string
+template <typename Fn> EnableForArgTypes<Fn, folly::dynamic, std::string>
+invokeForKeyValue(Fn fn, const folly::dynamic& k, const folly::dynamic& v) {
+  fn(k, v.asString());
+}
+    
+    
+    {  std::array<AtomicBlockType, N> data_;
+};
+    
+    using InstructionPointer = const void*;
+    
+    /* static */ void Config::destroy(Config * node)
+{
+    delete node;
+}
+    
+     private:
+    
+    #pragma once
+    
+    template<typename... ARGS>
+inline void logi(const char* tag, const char* msg, ARGS... args) noexcept {
+  log(ANDROID_LOG_INFO, tag, msg, args...);
+}
+    
+      static void OnThreadExit(void *obj) {
+    if (NULL != obj) {
+      delete (T*)obj;
     }
   }
-}
-    
-      // Same as above, but you can choose the interception scope of this object.
-  ScopedFakeTestPartResultReporter(InterceptMode intercept_mode,
-                                   TestPartResultArray* result);
-    
-    // Type-parameterized tests are abstract test patterns parameterized
-// by a type.  Compared with typed tests, type-parameterized tests
-// allow you to define the test pattern without knowing what the type
-// parameters are.  The defined pattern can be instantiated with
-// different types any number of times, in any number of translation
-// units.
-//
-// If you are designing an interface or concept, you can define a
-// suite of type-parameterized tests to verify properties that any
-// valid implementation of the interface/concept should have.  Then,
-// each implementation can easily instantiate the test suite to verify
-// that it conforms to the requirements, without having to write
-// similar tests repeatedly.  Here's an example:
-    
-    template <typename T>
-class linked_ptr {
- public:
-  typedef T element_type;
-    }
-    
-        const typename ParamGenerator<T$j>::iterator begin$(j)_;
-    const typename ParamGenerator<T$j>::iterator end$(j)_;
-    typename ParamGenerator<T$j>::iterator current$(j)_;
-]]
-    
-    // A sample program demonstrating using Google C++ testing framework.
-//
-// Author: wan@google.com (Zhanyong Wan)
-    
-    	mbedtls_ssl_set_bio(&ssl, this, bio_send, bio_recv, NULL);
-    
-    	virtual Error put_data(const uint8_t *p_data, int p_bytes);
-	virtual Error put_partial_data(const uint8_t *p_data, int p_bytes, int &r_sent);
-    
-    #if defined(MBEDTLS_SSL_TLS_C) && (!defined(MBEDTLS_SSL_PROTO_SSL3) && \
-    !defined(MBEDTLS_SSL_PROTO_TLS1) && !defined(MBEDTLS_SSL_PROTO_TLS1_1) && \
-    !defined(MBEDTLS_SSL_PROTO_TLS1_2))
-#error 'MBEDTLS_SSL_TLS_C defined, but no protocols are active'
-#endif
-    
-    	void set_break_language(ScriptLanguage *p_lang);
-	ScriptLanguage *get_break_language() const;
-    
-    
-    {	search_box->set_text('');
-	_update_class_list();
-}
-    
-    public:
-	HBoxContainer *get_toolbar() const { return toolbar; }
