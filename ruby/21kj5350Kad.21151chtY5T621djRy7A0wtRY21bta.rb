2ea47de3435673f2798aaea776522f5ce2e22d31
SUNsung <weatherius@gmail.com>
2018-04-20 11:05:46 +0300

    Rewrite [21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb] File in: 2018-04-20 11:05:46

--- ruby/21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb
+++ ruby/21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb
@@ -1,46 +1,81 @@
 
         
-          def include_url?
-    url.present?
-  end
+            describe '#revoke!' do
+      it 'revokes the key with the client' do
+        mock_client_response(:revoke_key!)
     
-    def usage
-  <<-EOS
-list_running_app_ids [ -t <bundle-id> ]
-    
-      belongs_to :status_message
-  has_many :poll_answers, -> { order 'id ASC' }, dependent: :destroy
-  has_many :poll_participations, dependent: :destroy
-  has_one :author, through: :status_message
-    
-          respond_with do |format|
-        format.html { redirect_to admin_pods_path }
-        format.json { render json: PodPresenter.new(pod).as_json }
+        private_class_method
+    def self.git_log_merge_commit_filtering_option(merge_commit_filtering)
+      case merge_commit_filtering
+      when :exclude_merges
+        '--no-merges'
+      when :only_include_merges
+        '--merges'
+      when :include_merges
+        nil
       end
     end
   end
 end
 
     
-          if @options[:to] == @options[:from] && !@options[:in_place]
-        fmt = @options[:from]
-        raise 'Error: converting from #{fmt} to #{fmt} without --in-place'
-      end
+    module SortableTable
+  extend ActiveSupport::Concern
     
-          # Returns the time the given Sass file was last modified.
-      #
-      # If the given file has been deleted or the time can't be accessed
-      # for some other reason, this should return nil.
-      #
-      # @param uri [String] The URI of the file to check.
-      #   Comes from a `:filename` option set on an engine returned by this importer.
-      # @param options [{Symbol => Object}] Options for the Sass file
-      #   containing the `@import` currently being checked.
-      # @return [Time, nil]
-      def mtime(uri, options)
-        Sass::Util.abstract(self)
-      end
+      def index
+    set_table_sort sorts: %w[credential_name credential_value], default: { credential_name: :asc }
     
-            def find_order
-          @order = Spree::Order.find_by!(number: order_id)
+        def find_local(username)
+      find_remote(username, nil)
+    end
+    
+            expect_any_instance_of(ActivityPub::LinkedDataSignature).to receive(:verify_account!).and_return(actor)
+        expect(ActivityPub::Activity).to receive(:factory).with(instance_of(Hash), actor, instance_of(Hash))
+    
+    class REST::MediaAttachmentSerializer < ActiveModel::Serializer
+  include RoutingHelper
+    
+      describe 'GET #show' do
+    it 'returns http success' do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
+    
+        HTTP.get(source).to_s.split('\n').each do |line|
+      next if line.start_with? '#'
+      parts = line.split(';').map(&:strip)
+      next if parts.size < 2
+      codes << [parts[0], parts[1].start_with?('fully-qualified')]
+    end
+    
+        # If {#limit_to_network} is disabled, this will always return `true`.
+    # Otherwise, return `true` only if all of the given IPs are within the
+    # project {#boundary boundaries}.
+    
         end
+    
+      def parse(pkt)
+    # We want to return immediatly if we do not have a packet which is handled by us
+    return unless pkt.is_tcp?
+    return if (pkt.tcp_sport != 110 and pkt.tcp_dport != 110)
+    s = find_session((pkt.tcp_sport == 110) ? get_session_src(pkt) : get_session_dst(pkt))
+    
+        def initialize(attributes={})
+      assign_attributes(attributes)
+      yield(self) if block_given?
+    end
+    
+          def create
+        req = Rack::Request.new(request.env)
+        if req['client_assertion_type'] == 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
+          handle_jwt_bearer(req)
+        end
+        self.status, headers, self.response_body = Api::OpenidConnect::TokenEndpoint.new.call(request.env)
+        headers.each {|name, value| response.headers[name] = value }
+        nil
+      end
+    
+      context 'called with two colors' do
+    it 'applies to alternating sides' do
+      rule = 'border-color: #0f0 #00f'
