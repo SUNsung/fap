82587db7e835f530b37c21443c7ed4588a489a73
SUNsung <weatherius@gmail.com>
2018-04-20 06:10:51 +0300

    Rewrite [182hulkjoQTk18px2kvJYxCgQ18AOSD9BFQs9Q18krD.rb] File in: 2018-04-20 06:10:51

--- ruby/182hulkjoQTk18px2kvJYxCgQ18AOSD9BFQs9Q18krD.rb
+++ ruby/182hulkjoQTk18px2kvJYxCgQ18AOSD9BFQs9Q18krD.rb
@@ -1,103 +1,153 @@
 
         
-          def present_hash(hash, skip_key = nil)
-    hash.to_a.sort_by {|a| a.first.to_s }.map { |k, v| '#{k}: #{v}' unless k.to_s == skip_key.to_s }.compact
-  end
-end
-
-    
-          if options[:type] == :array && (options[:values].blank? || !options[:values].is_a?(Array))
-        raise ArgumentError.new('When using :array as :type you need to provide the :values as an Array')
-      end
-    
-        respond_to do |format|
-      format.html
-      format.json { render json: @user_credential }
-    end
+            response = ActionDispatch::TestResponse.create(200, { 'Content-Type' => 'application/json' }, '{ 'foo': 'fighters' }')
+    assert_equal({ 'foo' => 'fighters' }, response.parsed_body)
   end
     
-        def matching_username
-      Account.where(Account.arel_table[:username].lower.eq username.to_s.downcase)
+        def show
+      render plain: 'Only for loooooong credentials'
     end
     
-      describe 'PUT #update' do
-    it 'updates notifications settings' do
-      user.settings['notification_emails'] = user.settings['notification_emails'].merge('follow' => false)
-      user.settings['interactions'] = user.settings['interactions'].merge('must_be_follower' => true)
-    
-    describe Settings::PreferencesController do
-  render_views
-    
-    module Admin
-  class PodsController < AdminController
-    respond_to :html, :json, :mobile
-    
-        def initialize(attributes={})
-      assign_attributes(attributes)
-      yield(self) if block_given?
-    end
-    
-          # Inspired by https://github.com/nov/openid_connect_sample/blob/master/app/controllers/connect/clients_controller.rb#L24
-      def create
-        registrar = OpenIDConnect::Client::Registrar.new(request.url, params)
-        client = Api::OpenidConnect::OAuthApplication.register! registrar
-        render json: client.as_json(root: false)
-      end
-    
-    module Api
-  module OpenidConnect
-    class TokenEndpointController < ApplicationController
-      skip_before_action :verify_authenticity_token
-    
-      def _log(level, message)
-    @messages << [level, message]
-  end
-end
-
-    
-      module Sass::Plugin::Configuration
-    # Different default options in a m environment.
-    def default_options
-      @default_options ||= begin
-        version = Merb::VERSION.split('.').map {|n| n.to_i}
-        if version[0] <= 0 && version[1] < 5
-          root = MERB_ROOT
-          env  = MERB_ENV
-        else
-          root = Merb.root.to_s
-          env  = Merb.environment
-        end
-    
-        # Starts the read-eval-print loop.
-    def run
-      environment = Environment.new
-      @line = 0
-      loop do
-        @line += 1
-        unless (text = Readline.readline('>> '))
-          puts
-          return
-        end
-    
-            def keyword_argument?
-          %i[kwarg kwoptarg].include?(@declaration_node.type)
-        end
-    
-              each_misplaced_optional_arg(arguments) do |argument|
-            add_offense(argument)
+            case method
+        when NilClass
+          raise 'Delivery method cannot be nil'
+        when Symbol
+          if klass = delivery_methods[method]
+            mail.delivery_method(klass, (send(:'#{method}_settings') || {}).merge(options || {}))
+          else
+            raise 'Invalid delivery method #{method.inspect}'
           end
+        else
+          mail.delivery_method(method)
         end
     
-    module RuboCop
-  module Cop
-    module Style
-      # This cop check for uses of Object#freeze on immutable objects.
-      #
-      # @example
-      #   # bad
-      #   CONST = 1.freeze
-      #
-      #   # good
-      #   CONST = 1
-      class RedundantFreeze < Cop
-        include FrozenStringLiteral
+    module ActionMailer
+  # Provides the option to parameterize mailers in order to share instance variable
+  # setup, processing, and common headers.
+  #
+  # Consider this example that does not use parameterization:
+  #
+  #   class InvitationsMailer < ApplicationMailer
+  #     def account_invitation(inviter, invitee)
+  #       @account = inviter.account
+  #       @inviter = inviter
+  #       @invitee = invitee
+  #
+  #       subject = '#{@inviter.name} invited you to their Basecamp (#{@account.name})'
+  #
+  #       mail \
+  #         subject:   subject,
+  #         to:        invitee.email_address,
+  #         from:      common_address(inviter),
+  #         reply_to:  inviter.email_address_with_name
+  #     end
+  #
+  #     def project_invitation(project, inviter, invitee)
+  #       @account = inviter.account
+  #       @project = project
+  #       @inviter = inviter
+  #       @invitee = invitee
+  #       @summarizer = ProjectInvitationSummarizer.new(@project.bucket)
+  #
+  #       subject = '#{@inviter.name.familiar} added you to a project in Basecamp (#{@account.name})'
+  #
+  #       mail \
+  #         subject:   subject,
+  #         to:        invitee.email_address,
+  #         from:      common_address(inviter),
+  #         reply_to:  inviter.email_address_with_name
+  #     end
+  #
+  #     def bulk_project_invitation(projects, inviter, invitee)
+  #       @account  = inviter.account
+  #       @projects = projects.sort_by(&:name)
+  #       @inviter  = inviter
+  #       @invitee  = invitee
+  #
+  #       subject = '#{@inviter.name.familiar} added you to some new stuff in Basecamp (#{@account.name})'
+  #
+  #       mail \
+  #         subject:   subject,
+  #         to:        invitee.email_address,
+  #         from:      common_address(inviter),
+  #         reply_to:  inviter.email_address_with_name
+  #     end
+  #   end
+  #
+  #   InvitationsMailer.account_invitation(person_a, person_b).deliver_later
+  #
+  # Using parameterized mailers, this can be rewritten as:
+  #
+  #   class InvitationsMailer < ApplicationMailer
+  #     before_action { @inviter, @invitee = params[:inviter], params[:invitee] }
+  #     before_action { @account = params[:inviter].account }
+  #
+  #     default to:       -> { @invitee.email_address },
+  #             from:     -> { common_address(@inviter) },
+  #             reply_to: -> { @inviter.email_address_with_name }
+  #
+  #     def account_invitation
+  #       mail subject: '#{@inviter.name} invited you to their Basecamp (#{@account.name})'
+  #     end
+  #
+  #     def project_invitation
+  #       @project    = params[:project]
+  #       @summarizer = ProjectInvitationSummarizer.new(@project.bucket)
+  #
+  #       mail subject: '#{@inviter.name.familiar} added you to a project in Basecamp (#{@account.name})'
+  #     end
+  #
+  #     def bulk_project_invitation
+  #       @projects = params[:projects].sort_by(&:name)
+  #
+  #       mail subject: '#{@inviter.name.familiar} added you to some new stuff in Basecamp (#{@account.name})'
+  #     end
+  #   end
+  #
+  #   InvitationsMailer.with(inviter: person_a, invitee: person_b).account_invitation.deliver_later
+  module Parameterized
+    extend ActiveSupport::Concern
+    
+            def initialize_test_deliveries
+          set_delivery_method :test
+          @old_perform_deliveries = ActionMailer::Base.perform_deliveries
+          ActionMailer::Base.perform_deliveries = true
+          ActionMailer::Base.deliveries.clear
+        end
+    
+    require 'active_support/testing/autorun'
+require 'active_support/testing/method_call_assertions'
+require 'action_mailer'
+require 'action_mailer/test_case'
+    
+    class I18nTestMailer < ActionMailer::Base
+  configure do |c|
+    c.assets_dir = ''
+  end
+    
+          def sanitized_opts(opts, is_safe)
+        if is_safe
+          Hash[[
+            [:startinline, opts.fetch(:startinline, nil)],
+            [:hl_lines,    opts.fetch(:hl_lines, nil)],
+            [:linenos,     opts.fetch(:linenos, nil)],
+            [:encoding,    opts.fetch(:encoding, 'utf-8')],
+            [:cssclass,    opts.fetch(:cssclass, nil)],
+          ].reject { |f| f.last.nil? }]
+        else
+          opts
+        end
+      end
+    
+    strlen = mimes.keys.max_by(&:length).length
+output = ''
+output << '# Woah there. Do not edit this file directly.\n'
+output << '# This file is generated automatically by script/vendor-mimes.\n\n'
+mimes = mimes.sort_by { |k,v| k }
+output << mimes.map { |mime,extensions| '#{mime.ljust(strlen)} #{extensions.join(' ')}' }.join('\n')
+    
+      class FeatureTopicUsers < Jobs::Base
+    
+          message = TestMailer.send_test(args[:to_address])
+      Email::Sender.new(message, :test_message).send
+    end
