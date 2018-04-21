adf8508293a4a3b9d35f98403a4631aa56fc2093
SUNsung <weatherius@gmail.com>
2018-04-21 11:11:38 +0300

    Rewrite [04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb] File in: 2018-04-21 11:11:38

--- ruby/04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb
+++ ruby/04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb
@@ -1,27 +1,114 @@
 
         
-            if resource.errors.empty?
-      resource.unlock_access! if unlockable?(resource)
-      if Devise.sign_in_after_reset_password
-        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
-        set_flash_message!(:notice, flash_message)
-        sign_in(resource_name, resource)
-      else
-        set_flash_message!(:notice, :updated_not_active)
+              def self.description
+        'Create a new Keychain'
       end
-      respond_with resource, location: after_resetting_password_path_for(resource)
-    else
-      set_minimum_password_length
-      respond_with resource
+    
+          # Read-only path to the shell-escaped gradle script, suitable for use in shell commands
+      attr_reader :escaped_gradle_path
+    
+            expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER]).to match(/cd .* && agvtool new-version -all 24 \&\& cd \-/)
+      end
     end
   end
+end
+
     
-      if respond_to?(:helper_method)
-    helpers = %w(resource scope_name resource_name signed_in_resource
-                 resource_class resource_params devise_mapping)
-    helper_method(*helpers)
+        attr_reader :filters
+    
+        def ==(other)
+      other.name == name && other.path == path && other.type == type
+    end
+    
+        def normalized_path
+      path == '' ? '/' : path
+    end
+    
+          def get_type
+        name
+      end
+    
+              # Verify the box exists that we want to repackage
+          box = @env.boxes.find(box_name, box_provider, '= #{box_version}')
+          if !box
+            raise Vagrant::Errors::BoxNotFoundWithProviderAndVersion,
+              name: box_name,
+              provider: box_provider.to_s,
+              version: box_version
+          end
+    
+            opts = OptionParser.new do |o|
+          o.banner = 'Usage: vagrant cap [options] TYPE NAME [args]'
+          o.separator ''
+          o.separator 'This is an advanced command. If you don't know what this'
+          o.separator 'does and you aren't explicitly trying to use it, you probably'
+          o.separator 'don't want to use this.'
+          o.separator ''
+          o.separator 'This command checks or executes arbitrary capabilities that'
+          o.separator 'Vagrant has for hosts, guests, and providers.'
+          o.separator ''
+          o.separator 'Options:'
+          o.separator ''
+    
+      # After we load the schema, make sure we have sequences for each
+  # table using timestamp IDs.
+  Rake::Task['db:schema:load'].enhance do
+    Rake::Task['db:ensure_id_sequences_exist'].invoke
   end
     
-      if record && record.respond_to?(:timedout?) && warden.authenticated?(scope) &&
-     options[:store] != false && !env['devise.skip_timeoutable']
-    last_request_at = warden.session(scope)['last_request_at']
+            subject.call(json, forwarder)
+      end
+    end
+  end
+end
+
+    
+    class TagsController < ApplicationController
+  before_action :set_body_classes
+  before_action :set_instance_presenter
+    
+      def as_json(options={})
+    {
+      poll_id:             id,
+      post_id:             status_message.id,
+      question:            question,
+      poll_answers:        poll_answers,
+      participation_count: participation_count
+    }
+  end
+    
+          def find
+        client = Api::OpenidConnect::OAuthApplication.find_by(client_name: params[:client_name])
+        if client
+          render json: {client_id: client.client_id}
+        else
+          render json: {error: 'Client with name #{params[:client_name]} does not exist'}
+        end
+      end
+    
+          def create
+        req = Rack::Request.new(request.env)
+        if req['client_assertion_type'] == 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
+          handle_jwt_bearer(req)
+        end
+        self.status, headers, self.response_body = Api::OpenidConnect::TokenEndpoint.new.call(request.env)
+        headers.each {|name, value| response.headers[name] = value }
+        nil
+      end
+    
+        # Provide a wrapper for the SCM that loads a strategy for the user.
+    #
+    # @param [Rake] context     The context in which the strategy should run
+    # @param [Module] strategy  A module to include into the SCM instance. The
+    #    module should provide the abstract methods of Capistrano::SCM
+    #
+    def initialize(context, strategy)
+      @context = context
+      singleton = class << self; self; end
+      singleton.send(:include, strategy)
+    end
+    
+      describe 'configuration SSHKit' do
+    let(:config) { SSHKit.config }
+    let(:backend) { SSHKit.config.backend.config }
+    let(:default_env) { { rails_env: :production } }
