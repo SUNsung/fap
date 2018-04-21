ccfc1fbef83c0951420d255d1d489ed4d50b007a
SUNsung <weatherius@gmail.com>
2018-04-22 00:04:28 +0300

    Rewrite [44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp] File in: 2018-04-22 00:04:27

--- cccccc/44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp
+++ cccccc/44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp
@@ -1,90 +1,142 @@
 
         
-        namespace osquery {
-namespace tables {
-    }
+        PyObject* NewFileServicesByName(const FileDescriptor* descriptor);
+    
+    
+    {
+    {
+    {}  // namespace python
+}  // namespace protobuf
+}  // namespace google
+
+    
+    TEST(AnyTest, TestPackAndUnpack) {
+  protobuf_unittest::TestAny submessage;
+  submessage.set_int32_value(12345);
+  protobuf_unittest::TestAny message;
+  message.mutable_any_value()->PackFrom(submessage);
     }
     
-      /// Pause until the requested millisecond delay has elapsed or a cancel.
-  void pause(size_t milli) {
-    pause(std::chrono::milliseconds(milli));
+    
+    {
+}  // namespace google
+#endif  // GOOGLE_PROTOBUF_COMPILER_CSHARP_OPTIONS_H__
+
+    
+    
+    {
+    {
+    {
+    {}  // namespace csharp
+}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
+    
+    #include <google/protobuf/compiler/code_generator.h>
+    
+    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
+//   can only really compare the output against a golden value, which is a
+//   fairly tedious and fragile testing strategy.  If we want to go that route,
+//   it probably makes sense to bite the bullet and write a test that compares
+//   the whole generated output for unittest.proto against a golden value, with
+//   a very simple script that can be run to regenerate it with the latest code.
+//   This would mean that updates to the golden file would have to be included
+//   in any change to the code generator, which would actually be fairly useful
+//   as it allows the reviewer to see clearly how the generated code is
+//   changing.
+    
+    
+    {  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(ImmutableExtensionLiteGenerator);
+};
+    
+    template<typename T>
+inline Vptr emitTLSAddr(Vout& v, TLSDatum<T> datum) {
+  switch (arch()) {
+    case Arch::X64:
+      return x64::detail::emitTLSAddr(v, datum);
+    case Arch::ARM:
+      return arm::detail::emitTLSAddr(v, datum);
+    case Arch::PPC64:
+      return ppc64::detail::emitTLSAddr(v, datum);
   }
-    
-      /// Keep a lookup of the external item name to assigned extension UUID.
-  std::map<std::string, RouteUUID> external_;
-    
-      // Create the IOCTL (and more) device node.
-  osquery.devfs = devfs_make_node(makedev(osquery.major_number, 0),
-                                  DEVFS_CHAR,
-                                  UID_ROOT,
-                                  GID_WHEEL,
-                                  0600,
-                                  'osquery',
-                                  0);
-  if (osquery.devfs == NULL) {
-    dbg_printf('Could not get a devfs entry!\n');
-    goto error_exit;
-  }
-    
-    TEST_F(YogaTest_HadOverflowTests, spacing_overflow_no_wrap_and_no_flex_children) {
-  const YGNodeRef child0 = YGNodeNewWithConfig(config);
-  YGNodeStyleSetWidth(child0, 80);
-  YGNodeStyleSetHeight(child0, 40);
-  YGNodeStyleSetMargin(child0, YGEdgeTop, 10);
-  YGNodeStyleSetMargin(child0, YGEdgeBottom, 10);
-  YGNodeInsertChild(root, child0, 0);
-  const YGNodeRef child1 = YGNodeNewWithConfig(config);
-  YGNodeStyleSetWidth(child1, 80);
-  YGNodeStyleSetHeight(child1, 40);
-  YGNodeStyleSetMargin(child1, YGEdgeBottom, 5);
-  YGNodeInsertChild(root, child1, 1);
-    }
-    
-    #include <yoga/Yoga.h>
-    
-    #include 'FuzzerExtFunctions.h'
-#include 'FuzzerIO.h'
-#include <dlfcn.h>
-    
-    static bool IsSeparator(char C) {
-  return C == '\\' || C == '/';
+  not_reached();
 }
     
-    // Tries detecting a memory leak on the particular input that we have just
-// executed before calling this function.
-void Fuzzer::TryDetectingAMemoryLeak(const uint8_t *Data, size_t Size,
-                                     bool DuringInitialCorpusExecution) {
-  if (!HasMoreMallocsThanFrees) return;  // mallocs==frees, a leak is unlikely.
-  if (!Options.DetectLeaks) return;
-  if (!&(EF->__lsan_enable) || !&(EF->__lsan_disable) ||
-      !(EF->__lsan_do_recoverable_leak_check))
-    return;  // No lsan.
-  // Run the target once again, but with lsan disabled so that if there is
-  // a real leak we do not report it twice.
-  EF->__lsan_disable();
-  ExecuteCallback(Data, Size);
-  EF->__lsan_enable();
-  if (!HasMoreMallocsThanFrees) return;  // a leak is unlikely.
-  if (NumberOfLeakDetectionAttempts++ > 1000) {
-    Options.DetectLeaks = false;
-    Printf('INFO: libFuzzer disabled leak detection after every mutation.\n'
-           '      Most likely the target function accumulates allocated\n'
-           '      memory in a global state w/o actually leaking it.\n'
-           '      You may try running this binary with -trace_malloc=[12]'
-           '      to get a trace of mallocs and frees.\n'
-           '      If LeakSanitizer is enabled in this process it will still\n'
-           '      run on the process shutdown.\n');
-    return;
-  }
-  // Now perform the actual lsan pass. This is expensive and we must ensure
-  // we don't call it too often.
-  if (EF->__lsan_do_recoverable_leak_check()) { // Leak is found, report it.
-    if (DuringInitialCorpusExecution)
-      Printf('\nINFO: a leak has been found in the initial corpus.\n\n');
-    Printf('INFO: to ignore leaks on libFuzzer side use -detect_leaks=0.\n\n');
-    CurrentUnitSize = Size;
-    DumpCurrentUnit('leak-');
-    PrintFinalStats();
-    _Exit(Options.ErrorExitCode);  // not exit() to disable lsan further on.
-  }
+      Array getAsyncResults(bool allow_empty);
+    
+    static void HHVM_FUNCTION(gmp_random, int64_t /*limiter*/) {
+  throw_not_implemented('gmp_random');
 }
+    
+    /*
+ * Vtuple is an index to a tuple in Vunit::tuples.
+ */
+DECLARE_VNUM(Vtuple, true, 'T');
+    
+      if (asprintf(&default_magic, '%s:%s', hmagicpath, MAGIC) < 0)
+    goto out;
+  free(hmagicpath);
+  return default_magic;
+out:
+  default_magic = NULL;
+  free(hmagicpath);
+  return MAGIC;
+#else
+  char *hmagicp = hmagicpath;
+  char *tmppath = NULL;
+  LPTSTR dllpath;
+    
+    
+// Logical immediates can't encode zero, so a return value of zero is used to
+// indicate a failure case. Specifically, where the constraints on imm_s are
+// not met.
+uint64_t Instruction::ImmLogical() {
+  unsigned reg_size = SixtyFourBits() ? kXRegSize : kWRegSize;
+  int64_t n = BitN();
+  int64_t imm_s = ImmSetBits();
+  int64_t imm_r = ImmRotate();
+    }
+    
+    CurlShareResource::CurlShareResource() {
+  m_share = curl_share_init();
+}
+    
+    struct CurlShareResource : SweepableResourceData {
+  DECLARE_RESOURCE_ALLOCATION(CurlShareResource)
+  CLASSNAME_IS('curl_share')
+  const String& o_getClassNameHook() const override { return classnameof(); }
+  bool isInvalid() const override { return !m_share; }
+    }
+    
+    
+    
+    
+    
+    
+    
+    			b2Transform xf2;
+			xf2.q.Set(-0.3524f * b2_pi);
+			xf2.p = -xf2.q.GetXAxis();
+    
+    			pjd.maxMotorForce = 1000.0f;
+			pjd.enableMotor = true;
+			pjd.lowerTranslation = -10.0f;
+			pjd.upperTranslation = 10.0f;
+			pjd.enableLimit = true;
+    
+    class Bridge : public Test
+{
+public:
+    }
+    
+    
+    {	b2Body* m_middle;
+};
+    
+    			jd.Initialize(m_car, m_wheel1, m_wheel1->GetPosition(), axis);
+			jd.motorSpeed = 0.0f;
+			jd.maxMotorTorque = 20.0f;
+			jd.enableMotor = true;
+			jd.frequencyHz = m_hz;
+			jd.dampingRatio = m_zeta;
+			m_spring1 = (b2WheelJoint*)m_world->CreateJoint(&jd);
