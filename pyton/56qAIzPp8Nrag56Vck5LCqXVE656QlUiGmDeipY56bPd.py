a00441751775868940e70032e8ebb88419642bbd
SUNsung <weatherius@gmail.com>
2018-04-21 19:01:55 +0300

    Rewrite [56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py] File in: 2018-04-21 19:01:55

--- pyton/56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py
+++ pyton/56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py
@@ -1,251 +1,227 @@
 
         
-        versions_info['versions'][version] = new_version
-versions_info['latest'] = version
+            def test_cache(self):
+        ydl = FakeYDL({
+            'cachedir': self.test_dir,
+        })
+        c = Cache(ydl)
+        obj = {'x': 1, 'y': ['ä', '\\a', True]}
+        self.assertEqual(c.load('test_cache', 'k.'), None)
+        c.store('test_cache', 'k.', obj)
+        self.assertEqual(c.load('test_cache', 'k2'), None)
+        self.assertFalse(_is_empty(self.test_dir))
+        self.assertEqual(c.load('test_cache', 'k.'), obj)
+        self.assertEqual(c.load('test_cache', 'y'), None)
+        self.assertEqual(c.load('test_cache2', 'k.'), None)
+        c.remove()
+        self.assertFalse(os.path.exists(self.test_dir))
+        self.assertEqual(c.load('test_cache', 'k.'), None)
     
-    if isinstance(helptext, bytes):
-    helptext = helptext.decode('utf-8')
-    
-        params = {
-        'age_limit': age,
-        'skip_download': True,
-        'writeinfojson': True,
-        'outtmpl': '%(id)s.%(ext)s',
-    }
-    ydl = YoutubeDL(params)
-    ydl.add_default_info_extractors()
-    json_filename = os.path.splitext(filename)[0] + '.info.json'
-    try_rm(json_filename)
-    ydl.download([url])
-    res = os.path.exists(json_filename)
-    try_rm(json_filename)
-    return res
+    from test.helper import assertRegexpMatches
     
         def _real_extract(self, url):
         playlist_id = self._match_id(url)
     
-    from .nuevo import NuevoBaseIE
     
-    # utilities we import from Werkzeug and Jinja2 that are unused
-# in the module but are exported as public interface.
-from werkzeug.exceptions import abort
-from werkzeug.utils import redirect
-from jinja2 import Markup, escape
+def check_entry(line_num, segments):
+    # START Title
+    title = segments[index_title].upper()
+    if title.endswith(' API'):
+        add_error(line_num, 'Title should not contain 'API'')
+    # END Title
+    # START Description
+    # first character should be capitalized
+    char = segments[index_desc][0]
+    if char.upper() != char:
+        add_error(line_num, 'first character of description is not capitalized')
+    # last character should not punctuation
+    char = segments[index_desc][-1]
+    if char in punctuation:
+        add_error(line_num, 'description should not end with {}'.format(char))
+    desc_length = len(segments[index_desc])
+    if desc_length > 100:
+        add_error(line_num, 'description should not exceed 100 characters (currently {})'.format(desc_length))
+    # END Description
+    # START Auth
+    # values should conform to valid options only
+    auth = segments[index_auth]
+    if auth != 'No' and (not auth.startswith('`') or not auth.endswith('`')):
+        add_error(line_num, 'auth value is not enclosed with `backticks`')
+    if auth.replace('`', '') not in auth_keys:
+        add_error(line_num, '{} is not a valid Auth option'.format(auth))
+    # END Auth
+    # START HTTPS
+    # values should conform to valid options only
+    https = segments[index_https]
+    if https not in https_keys:
+        add_error(line_num, '{} is not a valid HTTPS option'.format(https))
+    # END HTTPS
+    # START CORS
+    # values should conform to valid options only
+    cors = segments[index_cors]
+    if cors not in cors_keys:
+        add_error(line_num, '{} is not a valid CORS option'.format(cors))
+    # END CORS
+    # START Link
+    # url should be wrapped in '[Go!]()' Markdown syntax
+    link = segments[index_link]
+    if not link.startswith('[Go!](http') or not link.endswith(')'):
+        add_error(line_num, 'link syntax should be '[Go!](LINK)'')
+    # END Link
+    
+            .. versionadded:: 0.11
+        '''
+        mappings = []
+        if len(mapping) == 1:
+            if hasattr(mapping[0], 'items'):
+                mappings.append(mapping[0].items())
+            else:
+                mappings.append(mapping[0])
+        elif len(mapping) > 1:
+            raise TypeError(
+                'expected at most 1 positional argument, got %d' % len(mapping)
+            )
+        mappings.append(kwargs.items())
+        for mapping in mappings:
+            for (key, value) in mapping:
+                if key.isupper():
+                    self[key] = value
+        return True
+    
+        app.logger.info('\n'.join(info))
+    
+        if rv is None:
+        fail('Could not parse changelog')
     
     
-# Certain versions of pypy have a bug where clearing the exception stack
-# breaks the __exit__ function in a very peculiar way.  The second level of
-# exception blocks is necessary because pypy seems to forget to check if an
-# exception happened until the next bytecode instruction?
-#
-# Relevant PyPy bugfix commit:
-# https://bitbucket.org/pypy/pypy/commits/77ecf91c635a287e88e60d8ddb0f4e9df4003301
-# According to ronan on #pypy IRC, it is released in PyPy2 2.3 and later
-# versions.
-#
-# Ubuntu 14.04 has PyPy 2.2.1, which does exhibit this bug.
-BROKEN_PYPY_CTXMGR_EXIT = False
-if hasattr(sys, 'pypy_version_info'):
-    class _Mgr(object):
-        def __enter__(self):
-            return self
-        def __exit__(self, *args):
-            if hasattr(sys, 'exc_clear'):
-                # Python 3 (PyPy3) doesn't have exc_clear
-                sys.exc_clear()
-    try:
-        try:
-            with _Mgr():
-                raise AssertionError()
-        except:
-            raise
-    except TypeError:
-        BROKEN_PYPY_CTXMGR_EXIT = True
-    except AssertionError:
-        pass
+def test_installed_module_paths(modules_tmpdir, modules_tmpdir_prefix,
+                                purge_module, site_packages, limit_loader):
+    site_packages.join('site_app.py').write(
+        'import flask\n'
+        'app = flask.Flask(__name__)\n'
+    )
+    purge_module('site_app')
+    
+    
+def test_existing_handler(app):
+    logging.root.addHandler(logging.StreamHandler())
+    assert app.logger.level == logging.NOTSET
+    assert not app.logger.handlers
+    
+        out = StringIO()
+    app = SuppressedFlask(__name__)
+    
+            if not qop:
+            respdig = KD(HA1, '%s:%s' % (nonce, HA2))
+        elif qop == 'auth' or 'auth' in qop.split(','):
+            noncebit = '%s:%s:%s:%s:%s' % (
+                nonce, ncvalue, cnonce, 'auth', HA2
+            )
+            respdig = KD(HA1, noncebit)
+        else:
+            # XXX handle auth-int.
+            return None
+    
+        def __setitem__(self, name, value):
+        '''Dict-like __setitem__ for compatibility with client code. Throws
+        exception if there is already a cookie of that name in the jar. In that
+        case, use the more explicit set() method instead.
+        '''
+        self.set(name, value)
+    
+    
+def test_idna_with_version_attribute(mocker):
+    '''Verify we're actually setting idna version when it should be available.'''
+    mocker.patch('requests.help.idna', new=VersionedPackage('2.6'))
+    assert info()['idna'] == {'version': '2.6'}
 
     
-        def default(self, o):
-        '''Implement this method in a subclass such that it returns a
-        serializable object for ``o``, or calls the base implementation (to
-        raise a :exc:`TypeError`).
     
+class HTTPError(RequestException):
+    '''An HTTP error occurred.'''
     
-@pytest.mark.parametrize('value,path,result', (
-    ('test', cwd, 'test'),
-    ('test.py', cwd, 'test'),
-    ('a/test', os.path.join(cwd, 'a'), 'test'),
-    ('test/__init__.py', cwd, 'test'),
-    ('test/__init__', cwd, 'test'),
-    # nested package
-    (
-        os.path.join(test_path, 'cliapp', 'inner1', '__init__'),
-        test_path, 'cliapp.inner1'
-    ),
-    (
-        os.path.join(test_path, 'cliapp', 'inner1', 'inner2'),
-        test_path, 'cliapp.inner1.inner2'
-    ),
-    # dotted name
-    ('test.a.b', cwd, 'test.a.b'),
-    (os.path.join(test_path, 'cliapp.app'), test_path, 'cliapp.app'),
-    # not a Python file, will be caught during import
-    (
-        os.path.join(test_path, 'cliapp', 'message.txt'),
-        test_path, 'cliapp.message.txt'
-    ),
-))
-def test_prepare_import(request, value, path, result):
-    '''Expect the correct path to be set and the correct import and app names
-    to be returned.
+    from .structures import LookupDict
     
+    In both cases, only 10% of the features are informative.
+'''
+import gc
+from time import time
+import numpy as np
     
-@pytest.fixture
-def httpbin_secure(httpbin_secure):
-    return prepare_url(httpbin_secure)
+            start = time.time()
+        func(X, n_jobs=-1)
+        multi_core.append(time.time() - start)
+    
+    plt.figure('scikit-learn Ward's method benchmark results')
+plt.imshow(np.log(ratio), aspect='auto', origin='lower')
+plt.colorbar()
+plt.contour(ratio, levels=[1, ], colors='k')
+plt.yticks(range(len(n_features)), n_features.astype(np.int))
+plt.ylabel('N features')
+plt.xticks(range(len(n_samples)), n_samples.astype(np.int))
+plt.xlabel('N samples')
+plt.title('Scikit's time, in units of scipy time (log)')
+plt.show()
 
     
-        :param url: URL for the new :class:`Request` object.
-    :param data: (optional) Dictionary (will be form-encoded), bytes, or file-like object to send in the body of the :class:`Request`.
-    :param json: (optional) json data to send in the body of the :class:`Request`.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
+        An example with a long-untouched module that everyone has
+    >>> _linkcode_resolve('py', {'module': 'tty',
+    ...                          'fullname': 'setraw'},
+    ...                   package='tty',
+    ...                   url_fmt='http://hg.python.org/cpython/file/'
+    ...                           '{revision}/Lib/{package}/{path}#L{lineno}',
+    ...                   revision='xxxx')
+    'http://hg.python.org/cpython/file/xxxx/Lib/tty/tty.py#L18'
     '''
     
-    import threading
-import socket
-import select
+        # decode the payload explicitly as UTF-8 since lxml is confused for some
+    # reason
+    with codecs.open(html_filename,'r','utf-8') as html_file:
+        html_content = html_file.read()
+    tree = ElementTree(lxml.html.document_fromstring(html_content))
+    i = 0
+    j = 0
+    for p in tree.findall('//p'):
+        content = p.text_content()
+        if len(content) < 100:
+            # skip paragraphs that are too short - probably too noisy and not
+            # representative of the actual language
+            continue
     
-    from urllib3.fields import RequestField
-from urllib3.filepost import encode_multipart_formdata
-from urllib3.util import parse_url
-from urllib3.exceptions import (
-    DecodeError, ReadTimeoutError, ProtocolError, LocationParseError)
-    
-            :param name: a string containing name of cookie
-        :param domain: (optional) string containing domain of cookie
-        :param path: (optional) string containing path of cookie
-        :raises KeyError: if cookie is not found
-        :raises CookieConflictError: if there are multiple cookies
-            that match name and optionally domain and path
-        :return: cookie.value
-        '''
-        toReturn = None
-        for cookie in iter(self):
-            if cookie.name == name:
-                if domain is None or cookie.domain == domain:
-                    if path is None or cookie.path == path:
-                        if toReturn is not None:  # if there are multiple cookies that meet passed in criteria
-                            raise CookieConflictError('There are multiple cookies with name, %r' % (name))
-                        toReturn = cookie.value  # we will eventually return this as long as no cookie conflict
-    
-    with open('README.rst', 'r', 'utf-8') as f:
-    readme = f.read()
-with open('HISTORY.rst', 'r', 'utf-8') as f:
-    history = f.read()
-    
-        def long_desc(self):
-        '''A long description of the command. Return short description when not
-        available. It cannot contain newlines, since contents will be formatted
-        by optparser which removes newlines and wraps text.
-        '''
-        return self.short_desc()
-    
-            return self
-    
-            if self.closed:
-            raise ValueError('I/O operation on closed file')
-        if self.seekable:
-            try:
-                n = self.chunksize - self.size_read
-                # maybe fix alignment
-                if self.align and (self.chunksize & 1):
-                    n = n + 1
-                self.file.seek(n, 1)
-                self.size_read = self.size_read + n
-                return
-            except OSError:
-                pass
-        while self.size_read < self.chunksize:
-            n = min(8192, self.chunksize - self.size_read)
-            dummy = self.read(n)
-            if not dummy:
-                raise EOFError
+        print('Decompressing %s' % ARCHIVE_NAME)
+    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
+        archive.extractall(path='.')
+    os.remove(ARCHIVE_NAME)
 
     
-            if nextchar == ''':
-            return parse_string(string, idx + 1, strict)
-        elif nextchar == '{':
-            return parse_object((string, idx + 1), strict,
-                _scan_once, object_hook, object_pairs_hook, memo)
-        elif nextchar == '[':
-            return parse_array((string, idx + 1), _scan_once)
-        elif nextchar == 'n' and string[idx:idx + 4] == 'null':
-            return None, idx + 4
-        elif nextchar == 't' and string[idx:idx + 4] == 'true':
-            return True, idx + 4
-        elif nextchar == 'f' and string[idx:idx + 5] == 'false':
-            return False, idx + 5
-    }
+    from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import Perceptron
+from sklearn.pipeline import Pipeline
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
     
-    def evalString(s):
-    assert s.startswith(''') or s.startswith('''), repr(s[:1])
-    q = s[0]
-    if s[:3] == q*3:
-        q = q*3
-    assert s.endswith(q), repr(s[-len(q):])
-    assert len(s) >= 2*len(q)
-    s = s[len(q):-len(q)]
-    return re.sub(r'\\(\'|\'|\\|[abfnrtv]|x.{0,2}|[0-7]{1,3})', escape, s)
+    Sentiment analysis can be casted as a binary text classification problem,
+that is fitting a linear classifier on features extracted from the text
+of the user messages so as to guess wether the opinion of the author is
+positive or negative.
     
+    import numpy as np
+from sklearn.covariance import EllipticEnvelope
+from sklearn.svm import OneClassSVM
+import matplotlib.pyplot as plt
+import matplotlib.font_manager
+from sklearn.datasets import load_boston
     
-def triplet_to_brightness(rgbtuple):
-    # return the brightness (grey level) along the scale 0.0==black to
-    # 1.0==white
-    r = 0.299
-    g = 0.587
-    b = 0.114
-    return r*rgbtuple[0] + g*rgbtuple[1] + b*rgbtuple[2]
+        def set_surface(self, surface):
+        self.surface = surface
     
-        def user_has_beta_enabled(self, user):
-        if not user:
-            return False
-        return user.pref_beta
+    The outer product of the row and column label vectors shows a
+representation of the checkerboard structure.
     
-    from pylons import request
-from pylons import tmpl_context as c
-from pylons import app_globals as g
-    
-    class EmbedController(RedditController):
-    allow_stylesheets = True
-    
-        @validate(
-        container_id=VGTMContainerId('id')
-    )
-    def GET_jail(self, container_id):
-        return GoogleTagManagerJail(container_id=container_id).render()
-    
-        def GET_cachehealth(self):
-        results = {}
-        behaviors = {
-            # Passed on to poll(2) in milliseconds
-            'connect_timeout': 1000,
-            # Passed on to setsockopt(2) in microseconds
-            'receive_timeout': int(1e6),
-            'send_timeout': int(1e6),
-        }
-        for server in cache._CACHE_SERVERS:
-            try:
-                if server.startswith('udp:'):
-                    # libmemcached doesn't support UDP get/fetch operations
-                    continue
-                mc = pylibmc.Client([server], behaviors=behaviors)
-                # it's ok that not all caches are mcrouter, we'll just ignore
-                # the miss either way
-                mc.get('__mcrouter__.version')
-                results[server] = 'OK'
-            except pylibmc.Error as e:
-                g.log.warning('Health check for %s FAILED: %s', server, e)
-                results[server] = 'FAILED %s' % e
-        return json.dumps(results)
+    The dataset is generated using the ``make_biclusters`` function, which
+creates a matrix of small values and implants bicluster with large
+values. The rows and columns are then shuffled and passed to the
+Spectral Co-Clustering algorithm. Rearranging the shuffled matrix to
+make biclusters contiguous shows how accurately the algorithm found
+the biclusters.
