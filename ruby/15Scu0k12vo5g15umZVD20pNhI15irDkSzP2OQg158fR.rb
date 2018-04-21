5e08f404d99368d0f530dcca23ad4b9e86abacab
SUNsung <weatherius@gmail.com>
2018-04-21 23:20:36 +0300

    Rewrite [15Scu0k12vo5g15umZVD20pNhI15irDkSzP2OQg158fR.rb] File in: 2018-04-21 23:20:35

--- ruby/15Scu0k12vo5g15umZVD20pNhI15irDkSzP2OQg158fR.rb
+++ ruby/15Scu0k12vo5g15umZVD20pNhI15irDkSzP2OQg158fR.rb
@@ -1,154 +1,79 @@
 
         
-        require 'abstract_unit'
+          # True if a {Formula} is being built with {Formula.devel} instead of {Formula.stable}.
+  # <pre>args << '--some-beta' if build.devel?</pre>
+  def devel?
+    include? 'devel'
+  end
     
-          def sekrit_data
-        self.response_body = (@list + [@authenticated]).join(', ')
-      end
-    
-      test 'successful authentication request with Bearer instead of Token' do
-    @request.env['HTTP_AUTHORIZATION'] = 'Bearer lifo'
-    get :index
-    
-        test 'the middleware stack is exposed as 'middleware' in the controller' do
-      result = @app.call(env_for('/'))
-      assert_equal 'First!', result[1]['Middleware-Order']
-    end
-    
-        private
-      # 'Deserialize' the mailer class name by hand in case another argument
-      # (like a Global ID reference) raised DeserializationError.
-      def mailer_class
-        if mailer = Array(@serialized_arguments).first || Array(arguments).first
-          mailer.constantize
-        end
-      end
-    
-        AssertSelectMailer.test('<div><p>foo</p><p>bar</p></div>').deliver_now
-    assert_select_email do
-      assert_select 'div:root' do
-        assert_select 'p:first-child', 'foo'
-        assert_select 'p:last-child', 'bar'
+      # Removes any empty directories in the formula's prefix subtree
+  # Keeps any empty directions projected by skip_clean
+  # Removes any unresolved symlinks
+  def prune
+    dirs = []
+    symlinks = []
+    @f.prefix.find do |path|
+      if path == @f.libexec || @f.skip_clean?(path)
+        Find.prune
+      elsif path.symlink?
+        symlinks << path
+      elsif path.directory?
+        dirs << path
       end
     end
-  end
     
-        def push(*names)
-      @filters.push *filter_const(names)
+          return false unless prune_time
+    
+        def find_remote!(username, domain)
+      find_remote(username, domain) || raise(ActiveRecord::RecordNotFound)
     end
     
-    module Docs
-  class Entry
-    class Invalid < StandardError; end
-    
-        def add(path, content)
-      @pages[path] = content
+      task :ensure_id_sequences_exist do
+    each_schema_load_environment do
+      Mastodon::Snowflake.ensure_id_sequences_exist
     end
+  end
+end
+
     
-            # This is called early, before a machine is instantiated, to check
-        # if this provider is usable. This should return true or false.
-        #
-        # If raise_error is true, then instead of returning false, this
-        # should raise an error with a helpful message about why this
-        # provider cannot be used.
-        #
-        # @param [Boolean] raise_error If true, raise exception if not usable.
-        # @return [Boolean]
-        def self.usable?(raise_error=false)
-          # Return true by default for backwards compat since this was
-          # introduced long after providers were being written.
-          true
+      def id
+    ActivityPub::TagManager.instance.uri_for(object)
+  end
+    
+          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
+        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
+        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
+          @img['title']  = title
+          @img['alt']    = alt
+        else
+          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
         end
-    
-          # Renders the template using the class intance as the binding. Because the
-      # renderer inherits from `OpenStruct`, additional view variables can be
-      # added like normal accessors.
-      #
-      # @return [String]
-      def render
-        old_template = template
-        result = nil
-        File.open(full_template_path, 'r') do |f|
-          self.template = f.read
-          result = render_string
-        end
-    
-      def test_repeated_combination_with_callcc
-    need_continuation
-    n = 1000
-    cont = nil
-    ary = [1,2,3]
-    begin
-      ary.repeated_combination(2) {
-        callcc {|k| cont = k} unless cont
-      }
-    rescue => e
-    end
-    n -= 1
-    cont.call if 0 < n
-    assert_instance_of(RuntimeError, e)
-    assert_match(/reentered/, e.message)
-  end
-    
-        bug6323 = '[ruby-core:44447]'
-    t = t2000.getlocal('+00:36')
-    assert_equal('      +036', t.strftime('%_10z'), bug6323)
-    assert_equal('+000000036', t.strftime('%10z'), bug6323)
-    assert_equal('     +0:36', t.strftime('%_10:z'), bug6323)
-    assert_equal('+000000:36', t.strftime('%10:z'), bug6323)
-    assert_equal('  +0:36:00', t.strftime('%_10::z'), bug6323)
-    assert_equal('+000:36:00', t.strftime('%10::z'), bug6323)
-    assert_equal('+000000:36', t.strftime('%10:::z'))
-    t = t2000.getlocal('-00:55')
-    assert_equal('      -055', t.strftime('%_10z'), bug6323)
-    assert_equal('-000000055', t.strftime('%10z'), bug6323)
-    assert_equal('     -0:55', t.strftime('%_10:z'), bug6323)
-    assert_equal('-000000:55', t.strftime('%10:z'), bug6323)
-    assert_equal('  -0:55:00', t.strftime('%_10::z'), bug6323)
-    assert_equal('-000:55:00', t.strftime('%10::z'), bug6323)
-    assert_equal('-000000:55', t.strftime('%10:::z'))
-  end
-    
-      it 'returns an empty string and does not decode any bytes when the count modifier is zero' do
-    'abc'.unpack(unpack_format(0)+unpack_format).should == ['', 'a']
-  end
-    
-      class SubclassX < Struct
-  end
-    
-      # Clean a top-level (bin, sbin, lib) directory, recursively, by fixing file
-  # permissions and removing .la files, unless the files (or parent
-  # directories) are protected by skip_clean.
-  #
-  # bin and sbin should not have any subdirectories; if either do that is
-  # caught as an audit warning
-  #
-  # lib may have a large directory tree (see Erlang for instance), and
-  # clean_dir applies cleaning rules to the entire tree
-  def clean_dir(d)
-    d.find do |path|
-      path.extend(ObserverPathnameExtension)
-    
-            rows.each do |row|
-          line = row.values.each_with_index.map do |value, col|
-            value.to_s.ljust(col_widths[col])
-          end.join(' ').rstrip
-          line = color.colorize(line, row.color) if row.color
-          puts line
-        end
+        @img['class'].gsub!(/'/, '') if @img['class']
       end
-    
-      if File.exist?('Capfile')
-    warn '[skip] Capfile already exists'
-  else
-    FileUtils.cp(capfile, 'Capfile')
-    puts I18n.t(:written_file, scope: :capistrano, file: 'Capfile')
-  end
-    
-      describe 'role_properties()' do
-    before do
-      dsl.role :redis, %w[example1.com example2.com], redis: { port: 6379, type: :slave }
-      dsl.server 'example1.com', roles: %w{web}, active: true, web: { port: 80 }
-      dsl.server 'example2.com', roles: %w{web redis}, web: { port: 81 }, redis: { type: :master }
-      dsl.server 'example3.com', roles: %w{app}, primary: true
+      super
     end
+    
+          # Parsed representation of code annotated with the `^^^ Message` style
+      class AnnotatedSource
+        ANNOTATION_PATTERN = /\A\s*\^+ /
+    
+      it 'registers an offense' do
+    inspect_source(source)
+    
+            context 'source with constants' do
+          let(:source) do
+            <<-RUBY.strip_indent
+              #{type} Parent
+                #{type} SomeObject
+                  URL = %q(http://example.com)
+                  def do_something
+                  end
+                end
+              end
+            RUBY
+          end
+    
+          def initialize(agent)
+        @agent = agent
+        logger.debug('[api-service] start') if logger.debug?
+      end
