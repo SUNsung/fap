fc65c48013d16c90db2472d1a327108c6f9a9dfc
SUNsung <weatherius@gmail.com>
2018-04-21 09:43:51 +0300

    Rewrite [23pHAgfzyr4o23ev1fqdRiZq.23b79LvE58k2M2395v.cpp] File in: 2018-04-21 09:43:50

--- cccccc/23pHAgfzyr4o23ev1fqdRiZq.23b79LvE58k2M2395v.cpp
+++ cccccc/23pHAgfzyr4o23ev1fqdRiZq.23b79LvE58k2M2395v.cpp
@@ -1,348 +1,81 @@
 
         
-        #endif  // TENSORFLOW_DEBUGGER_STATE_IMPL_H_
+        #include 'ui/gfx/image/image.h'
+    
+      // views::View:
+  const char* GetClassName() const override;
+    
+    bool IsUnresponsiveEventSuppressed() {
+  return g_suppress_level > 0;
+}
+    
+    class PreferencesManager : public content::RenderThreadObserver {
+ public:
+  PreferencesManager();
+  ~PreferencesManager() override;
+    }
+    
+      // Suspend/Resume global shortcut handling. Note that when suspending,
+  // RegisterAccelerator/UnregisterAccelerator/UnregisterAccelerators are not
+  // allowed to be called until shortcut handling has been resumed.
+  void SetShortcutHandlingSuspended(bool suspended);
+    
+    #endif  // CHROME_BROWSER_PRINTING_PRINTING_UI_WEB_CONTENTS_OBSERVER_H_
 
     
-    namespace internal {
-template <typename T>
-struct functor_traits<scalar_tanh_fast_derivative_op<T> > {
-  enum {
-    Cost = NumTraits<T>::AddCost * 2 + NumTraits<T>::MulCost * 1,
-    PacketAccess = packet_traits<T>::HasAdd && packet_traits<T>::HasMul &&
-                   packet_traits<T>::HasNegate
-  };
-};
-}  // namespace internal
     
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    The input audio has one row of the tensor for each channel in the audio file.
-Each channel contains audio samples starting at the beginning of the audio and
-having `1/samples_per_second` time between them. The output file will contain
-all of the audio channels contained in the tensor.
-    
-    TEST_F(RemoveDeviceTest, TestRemoveDevice) { TestRemoveDevice(); }
-    
-    
-    {  // The compiler for the target platform.  This is included in place of
-  // the Service::execute_backend_'s compiler, since execute_backend_ is a
-  // nullptr in CompileOnlyService.
-  Compiler* compiler_;
-};
-    
-      // content::DevToolsAgentHostClient:
-  void AgentHostClosed(content::DevToolsAgentHost* agent_host,
-                       bool replaced_with_another_client) override;
-  void DispatchProtocolMessage(content::DevToolsAgentHost* agent_host,
-                               const std::string& message) override;
-    
-    namespace api {
-    }
-    
-     private:
-  atom::RenderProcessPreferences preferences_;
-    
-      // content::DownloadItem::Observer:
-  void OnDownloadUpdated(content::DownloadItem* item) override;
-    
-    #include 'storage/common/quota/quota_types.h'
-    
-    HttpProtocolHandler::HttpProtocolHandler(const std::string& scheme)
-    : scheme_(scheme) {
-}
-    
-    // Identical to RelaunchApp, but uses |helper| as the path to the relauncher
-// process, and allows additional arguments to be supplied to the relauncher
-// process in relauncher_args. Unlike args[0], |helper| must be a pathname to
-// an executable file. The helper path given must be from the same version of
-// Chrome as the running parent browser process, as there are no guarantees
-// that the parent and relauncher processes from different versions will be
-// able to communicate with one another. This variant can be useful to
-// relaunch the same version of Chrome from another location, using that
-// location's helper.
-bool RelaunchAppWithHelper(const base::FilePath& helper,
-                           const StringVector& relauncher_args,
-                           const StringVector& args);
-    
-    RenderProcessPreferences::~RenderProcessPreferences() {
-}
-    
-    #include 'base/callback.h'
-#include 'base/values.h'
-#include 'content/public/browser/notification_observer.h'
-#include 'content/public/browser/notification_registrar.h'
-    
-    #endif  // ATOM_BROWSER_UI_DRAG_UTIL_H_
-
-    
-    #ifndef ATOM_BROWSER_UI_VIEWS_MENU_MODEL_ADAPTER_H_
-#define ATOM_BROWSER_UI_VIEWS_MENU_MODEL_ADAPTER_H_
-    
-    
-    {}  // namespace atom
-    
-    #ifndef TEST_QPS_DRIVER_H
-#define TEST_QPS_DRIVER_H
-    
-    #include <algorithm>
-#include <cctype>
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-#include 'route_guide.grpc.pb.h'
-    
-    grpc::string DescribeServiceList(std::vector<grpc::string> service_list,
-                                 grpc::protobuf::DescriptorPool& desc_pool) {
-  std::stringstream result;
-  for (auto it = service_list.begin(); it != service_list.end(); it++) {
-    auto const& service = *it;
-    const grpc::protobuf::ServiceDescriptor* service_desc =
-        desc_pool.FindServiceByName(service);
-    if (service_desc != nullptr) {
-      result << DescribeService(service_desc);
-    }
-  }
-  return result.str();
-}
-    
-    #endif  // GRPC_TEST_CPP_UTIL_SUBPROCESS_H
-
-    
-    #include <google/protobuf/compiler/command_line_interface.h>
-#include <google/protobuf/compiler/python/python_generator.h>
-    
-    
-    {	return paused;
-};
-    
-    	if (!connected)
-		return;
-    
-    	static void _bind_methods();
-    
-    
-    {		} break;
-    
-    
-    {	// !BAS! But seeing we apply this frame by frame, makes more sense to me to make this time based
-	// keeping in mind our anti roll factor
-	real_t contactDamping = s->get_step() / p_rollInfluence;
-#define ONLY_USE_LINEAR_MASS
-#ifdef ONLY_USE_LINEAR_MASS
-	real_t massTerm = real_t(1.) / ((1.0 / mass) + b2invmass);
-	impulse = -contactDamping * rel_vel * massTerm;
-#else
-	real_t velocityImpulse = -contactDamping * rel_vel * jacDiagABInv;
-	impulse = velocityImpulse;
-#endif
-}
-    
-    
-    {			facesptr[i+old_faces]=face;
-		}
-    
-    #endif // BOOST_ASIO_BUFFERED_STREAM_FWD_HPP
-
-    
-    #endif // BOOST_ASIO_BUFFERED_WRITE_STREAM_FWD_HPP
-
-    
-    #if !defined(BOOST_ASIO_HAS_THREADS)
-// Nothing to include.
-#elif defined(BOOST_ASIO_HAS_STD_ATOMIC)
-# include <atomic>
-#else // defined(BOOST_ASIO_HAS_STD_ATOMIC)
-# include <boost/detail/atomic_count.hpp>
-#endif // defined(BOOST_ASIO_HAS_STD_ATOMIC)
-    
-    namespace boost {
-namespace asio {
-namespace detail {
-    }
-    }
-    }
-    
-    #include <boost/asio/detail/config.hpp>
-    
-    ::pollfd& dev_poll_reactor::add_pending_event_change(int descriptor)
-{
-  hash_map<int, std::size_t>::iterator iter
-    = pending_event_change_index_.find(descriptor);
-  if (iter == pending_event_change_index_.end())
-  {
-    std::size_t index = pending_event_changes_.size();
-    pending_event_changes_.reserve(pending_event_changes_.size() + 1);
-    pending_event_change_index_.insert(std::make_pair(descriptor, index));
-    pending_event_changes_.push_back(::pollfd());
-    pending_event_changes_[index].fd = descriptor;
-    pending_event_changes_[index].revents = 0;
-    return pending_event_changes_[index];
-  }
-  else
-  {
-    return pending_event_changes_[iter->second];
-  }
-}
-    
-    // Header is checksum (4 bytes), length (2 bytes), type (1 byte).
-static const int kHeaderSize = 4 + 2 + 1;
-    
-    TEST(VersionEditTest, EncodeDecode) {
-  static const uint64_t kBig = 1ull << 50;
-    }
-    
-    namespace leveldb {
-    }
-    
-      // Third filter is empty
-    
-    
-    {  ASSERT_EQ(Hash(0, 0, 0xbc9f1d34), 0xbc9f1d34);
-  ASSERT_EQ(
-      Hash(reinterpret_cast<const char*>(data1), sizeof(data1), 0xbc9f1d34),
-      0xef1345c4);
-  ASSERT_EQ(
-      Hash(reinterpret_cast<const char*>(data2), sizeof(data2), 0xbc9f1d34),
-      0x5b663814);
-  ASSERT_EQ(
-      Hash(reinterpret_cast<const char*>(data3), sizeof(data3), 0xbc9f1d34),
-      0x323c078f);
-  ASSERT_EQ(
-      Hash(reinterpret_cast<const char*>(data4), sizeof(data4), 0xbc9f1d34),
-      0xed21633a);
-  ASSERT_EQ(
-      Hash(reinterpret_cast<const char*>(data5), sizeof(data5), 0x12345678),
-      0xf333dabb);
-}
-    
-    
-    {  // Skewed: pick 'base' uniformly from range [0,max_log] and then
-  // return 'base' random bits.  The effect is to pick a number in the
-  // range [0,2^max_log-1] with exponential bias towards smaller numbers.
-  uint32_t Skewed(int max_log) {
-    return Uniform(1 << Uniform(max_log + 1));
-  }
-};
-    
-    #if 0
-static void DumpInternalIter(Iterator* iter) {
-  for (iter->SeekToFirst(); iter->Valid(); iter->Next()) {
-    ParsedInternalKey k;
-    if (!ParseInternalKey(iter->key(), &k)) {
-      fprintf(stderr, 'Corrupt '%s'\n', EscapeString(iter->key()).c_str());
-    } else {
-      fprintf(stderr, '@ '%s'\n', k.DebugString().c_str());
-    }
-  }
-}
-#endif
-    
-      // When user keys are different, but correctly ordered
-  ASSERT_EQ(IKey('g', kMaxSequenceNumber, kValueTypeForSeek),
-            Shorten(IKey('foo', 100, kTypeValue),
-                    IKey('hello', 200, kTypeValue)));
-    
-     private:
-  WritableFile* dest_;
-  int block_offset_;       // Current offset in block
-    
-    bool MemTable::Get(const LookupKey& key, std::string* value, Status* s) {
-  Slice memkey = key.memtable_key();
-  Table::Iterator iter(&table_);
-  iter.Seek(memkey.data());
-  if (iter.Valid()) {
-    // entry format is:
-    //    klength  varint32
-    //    userkey  char[klength]
-    //    tag      uint64
-    //    vlength  varint32
-    //    value    char[vlength]
-    // Check that it belongs to same user key.  We do not check the
-    // sequence number since the Seek() call above should have skipped
-    // all entries with overly large sequence numbers.
-    const char* entry = iter.key();
-    uint32_t key_length;
-    const char* key_ptr = GetVarint32Ptr(entry, entry+5, &key_length);
-    if (comparator_.comparator.user_comparator()->Compare(
-            Slice(key_ptr, key_length - 8),
-            key.user_key()) == 0) {
-      // Correct user key
-      const uint64_t tag = DecodeFixed64(key_ptr + key_length - 8);
-      switch (static_cast<ValueType>(tag & 0xff)) {
-        case kTypeValue: {
-          Slice v = GetLengthPrefixedSlice(key_ptr + key_length);
-          value->assign(v.data(), v.size());
-          return true;
-        }
-        case kTypeDeletion:
-          *s = Status::NotFound(Slice());
-          return true;
-      }
-    }
-  }
-  return false;
-}
-    
-        // Build atlas
-    unsigned char* tex_pixels = NULL;
-    int tex_w, tex_h;
-    io.Fonts->GetTexDataAsRGBA32(&tex_pixels, &tex_w, &tex_h);
-    
-    // Use if you want to reset your rendering device without losing ImGui state.
-IMGUI_API void        ImGui_ImplSdlGL2_InvalidateDeviceObjects();
-IMGUI_API bool        ImGui_ImplSdlGL2_CreateDeviceObjects();
-
-    
-            ImGui::BeginChild('#colors', ImVec2(0, 300), true, ImGuiWindowFlags_AlwaysVerticalScrollbar | ImGuiWindowFlags_AlwaysHorizontalScrollbar | ImGuiWindowFlags_NavFlattened);
-        ImGui::PushItemWidth(-160);
-        for (int i = 0; i < ImGuiCol_COUNT; i++)
-        {
-            const char* name = ImGui::GetStyleColorName(i);
-            if (!filter.PassFilter(name))
-                continue;
-            ImGui::PushID(i);
-            ImGui::ColorEdit4('##color', (float*)&style.Colors[i], ImGuiColorEditFlags_AlphaBar | alpha_flags);
-            if (memcmp(&style.Colors[i], &ref->Colors[i], sizeof(ImVec4)) != 0)
-            {
-                // Tips: in a real user application, you may want to merge and use an icon font into the main font, so instead of 'Save'/'Revert' you'd use icons.
-                // Read the FAQ and misc/fonts/README.txt about using icon fonts. It's really easy and super convenient!
-                ImGui::SameLine(0.0f, style.ItemInnerSpacing.x); if (ImGui::Button('Save')) ref->Colors[i] = style.Colors[i];
-                ImGui::SameLine(0.0f, style.ItemInnerSpacing.x); if (ImGui::Button('Revert')) style.Colors[i] = ref->Colors[i];
-            }
-            ImGui::SameLine(0.0f, style.ItemInnerSpacing.x);
-            ImGui::TextUnformatted(name);
-            ImGui::PopID();
-        }
-        ImGui::PopItemWidth();
-        ImGui::EndChild();
-    
-    
-    {        g_pSwapChain->Present(1, 0); // Present with vsync
-        //g_pSwapChain->Present(0, 0); // Present without vsync
-    }
-    
-    #ifdef IMGUI_VULKAN_DEBUG_REPORT
-static VKAPI_ATTR VkBool32 VKAPI_CALL debug_report(
-    VkDebugReportFlagsEXT flags, VkDebugReportObjectTypeEXT objectType, uint64_t object, size_t location, int32_t messageCode, const char* pLayerPrefix, const char* pMessage, void* pUserData)
-{
-    (void)flags; (void)object; (void)pUserData; (void)pLayerPrefix; (void)messageCode; (void)location;
-    printf('[vulkan] ObjectType: %i\nMessage: %s\n\n', objectType, pMessage );
-    return VK_FALSE;
-}
-#endif // IMGUI_VULKAN_DEBUG_REPORT
-    
-    void ImFontAtlasBuildSetupFont(ImFontAtlas* atlas, ImFont* font, ImFontConfig* font_config, float ascent, float descent)
-{
-    if (!font_config->MergeMode)
+    {    // SaveMapping - save a map into the file
+    // saveId - name of the section to save into (section:subsection format)
+    // labelMapping - map we are saving to the file
+    virtual void SaveMapping(std::wstring saveId, const std::map<typename BinaryWriter<ElemType>::LabelIdType, typename BinaryWriter<ElemType>::LabelType>& labelMapping);
+    virtual bool SupportMultiUtterances() const 
     {
-        font->ClearOutputData();
-        font->FontSize = font_config->SizePixels;
-        font->ConfigData = font_config;
-        font->ContainerAtlas = atlas;
-        font->Ascent = ascent;
-        font->Descent = descent;
-    }
-    font->ConfigDataCount++;
+        return false;
+    };
+};
+    
+    void GPUDataTransferer::CopyGPUToCPUAsync(void* gpuBuffer, size_t totalSize, void* cpuBuffer)
+{
+    m_inner->CopyGPUToCPUAsync(gpuBuffer, 1, totalSize, cpuBuffer);
+    m_inner->RecordGPUToCPUCopy();
 }
+    
+    
+/**
+ * Captures and symbolicates a stack trace
+ *
+ * Beware of a bug on some platforms, which makes the trace loop until the
+ * buffer is full when it reaches a noexpr function. It seems to be fixed in
+ * newer versions of gcc. https://gcc.gnu.org/bugzilla/show_bug.cgi?id=56846
+ *
+ * @param skip The number of frames before capturing the trace
+ *
+ * @param limit The maximum number of frames captured
+ */
+FBEXPORT inline std::vector<StackTraceElement> getStackTraceSymbols(
+    size_t skip = 0,
+    size_t limit = kDefaultLimit) {
+  return getStackTraceSymbols(getStackTrace(skip + 1, limit));
+}
+    
+     public:
+  IosFlagsSaver(ios_base& ios)
+  : ios_(ios),
+    flags_(ios.flags())
+  {}
+    
+        void setWidth(double width);
+    void setWidthPercent(double width);
+    void setWidthAuto();
+    void setHeight(double height);
+    void setHeightPercent(double height);
+    void setHeightAuto();
+    
+    template<typename... ARGS>
+inline void logw(const char* tag, const char* msg, ARGS... args) noexcept {
+  log(ANDROID_LOG_WARN, tag, msg, args...);
+}
+    
+    #include <fb/visibility.h>
+    
+    #pragma once
