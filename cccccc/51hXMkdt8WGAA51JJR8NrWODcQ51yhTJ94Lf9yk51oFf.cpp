1d0c6deadb2d959132569d20883333e0fb97698c
SUNsung <weatherius@gmail.com>
2018-04-22 08:10:49 +0300

    Rewrite [51hXMkdt8WGAA51JJR8NrWODcQ51yhTJ94Lf9yk51oFf.cpp] File in: 2018-04-22 08:10:48

--- cccccc/51hXMkdt8WGAA51JJR8NrWODcQ51yhTJ94Lf9yk51oFf.cpp
+++ cccccc/51hXMkdt8WGAA51JJR8NrWODcQ51yhTJ94Lf9yk51oFf.cpp
@@ -1,322 +1,214 @@
 
         
-          uint64_t Size(const Slice& start, const Slice& limit) {
-    Range r(start, limit);
-    uint64_t size;
-    db_->GetApproximateSizes(&r, 1, &size);
-    return size;
+        namespace nwapi {
+    }
+    
+    base::StringPiece GetStringResource(int resource_id) {
+  return ResourceBundle::GetSharedInstance().GetRawDataResource(resource_id);
+}
+    
+    namespace blink {
+class WebFrame;
+class WebURLRequest;
+class WebView;
+}
+    
+    
+    {}  // namespace nwapi
+    
+    void DispatcherHost::OnCallStaticMethodSync(
+    const std::string& type,
+    const std::string& method,
+    const base::ListValue& arguments,
+    base::ListValue* result) {
+  DLOG(INFO) << 'OnCallStaticMethodSync: '
+             << ' type:' << type
+             << ' method:' << method
+             << ' arguments:' << arguments;
+    }
+    
+    EventListener::EventListener(int id,
+  const base::WeakPtr<DispatcherHost>& dispatcher_host,
+  const base::DictionaryValue& option) : Base(id, dispatcher_host, option) {
+    }
+    
+    namespace nw {
+    }
+    
+    
+void MenuItem::UpdateKeys(GtkAccelGroup *gtk_accel_group){
+  this->gtk_accel_group = gtk_accel_group;
+  if (enable_shortcut && GTK_IS_ACCEL_GROUP(gtk_accel_group)){
+    gtk_widget_add_accelerator(
+      menu_item_,
+      'activate',
+      gtk_accel_group,
+      keyval,
+      modifiers_mask,
+      GTK_ACCEL_VISIBLE);
   }
-    
-    // Soft limit on number of level-0 files.  We slow down writes at this point.
-static const int kL0_SlowdownWritesTrigger = 8;
-    
-    TEST(FormatTest, InternalKeyShortestSuccessor) {
-  ASSERT_EQ(IKey('g', kMaxSequenceNumber, kValueTypeForSeek),
-            ShortSuccessor(IKey('foo', 100, kTypeValue)));
-  ASSERT_EQ(IKey('\xff\xff', 100, kTypeValue),
-            ShortSuccessor(IKey('\xff\xff', 100, kTypeValue)));
-}
-    
-    
-    {}  // namespace
-    
-    
-// Owned filenames have the form:
-//    dbname/CURRENT
-//    dbname/LOCK
-//    dbname/LOG
-//    dbname/LOG.old
-//    dbname/MANIFEST-[0-9]+
-//    dbname/[0-9]+.(log|sst|ldb)
-bool ParseFileName(const std::string& fname,
-                   uint64_t* number,
-                   FileType* type) {
-  Slice rest(fname);
-  if (rest == 'CURRENT') {
-    *number = 0;
-    *type = kCurrentFile;
-  } else if (rest == 'LOCK') {
-    *number = 0;
-    *type = kDBLockFile;
-  } else if (rest == 'LOG' || rest == 'LOG.old') {
-    *number = 0;
-    *type = kInfoLogFile;
-  } else if (rest.starts_with('MANIFEST-')) {
-    rest.remove_prefix(strlen('MANIFEST-'));
-    uint64_t num;
-    if (!ConsumeDecimalNumber(&rest, &num)) {
-      return false;
-    }
-    if (!rest.empty()) {
-      return false;
-    }
-    *type = kDescriptorFile;
-    *number = num;
-  } else {
-    // Avoid strtoull() to keep filename format independent of the
-    // current locale
-    uint64_t num;
-    if (!ConsumeDecimalNumber(&rest, &num)) {
-      return false;
-    }
-    Slice suffix = rest;
-    if (suffix == Slice('.log')) {
-      *type = kLogFile;
-    } else if (suffix == Slice('.sst') || suffix == Slice('.ldb')) {
-      *type = kTableFile;
-    } else if (suffix == Slice('.dbtmp')) {
-      *type = kTempFile;
-    } else {
-      return false;
-    }
-    *number = num;
+  if (submenu_ != NULL){
+    submenu_->UpdateKeys(gtk_accel_group);
   }
-  return true;
+  return;
 }
     
-      fname = CurrentFileName('foo');
-  ASSERT_EQ('foo/', std::string(fname.data(), 4));
-  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
-  ASSERT_EQ(0, number);
-  ASSERT_EQ(kCurrentFile, type);
+      // Ignore first non-switch arg if it's not a standalone package.
+  bool ignore_arg = !package->self_extract();
+  for (unsigned i = 1; i < argv.size(); ++i) {
+    if (ignore_arg && args.size() && argv[i] == args[0]) {
+      ignore_arg = false;
+      continue;
+    }
+    }
     
-    static void UnrefEntry(void* arg1, void* arg2) {
-  Cache* cache = reinterpret_cast<Cache*>(arg1);
-  Cache::Handle* h = reinterpret_cast<Cache::Handle*>(arg2);
-  cache->Release(h);
-}
+      grpc::testing::RunQPS();
     
-      // Return an iterator for the specified file number (the corresponding
-  // file length must be exactly 'file_size' bytes).  If 'tableptr' is
-  // non-NULL, also sets '*tableptr' to point to the Table object
-  // underlying the returned iterator, or NULL if no Table object underlies
-  // the returned iterator.  The returned '*tableptr' object is owned by
-  // the cache and should not be deleted, and is valid for as long as the
-  // returned iterator is live.
-  Iterator* NewIterator(const ReadOptions& options,
-                        uint64_t file_number,
-                        uint64_t file_size,
-                        Table** tableptr = NULL);
+    std::string GetDbFileContent(int argc, char** argv);
     
-          case kPrevLogNumber:
-        if (GetVarint64(&input, &prev_log_number_)) {
-          has_prev_log_number_ = true;
-        } else {
-          msg = 'previous log number';
-        }
-        break;
-    
-      // State kept for progress messages
-  int done_;
-  int next_report_;     // When to report next
-    
-    #endif  // STORAGE_LEVELDB_INCLUDE_FILTER_POLICY_H_
+    #endif  // GRPC_INTERNAL_COMPILER_NODE_GENERATOR_HELPERS_H
 
     
-    void ComputeExtrinsicRefine(const Mat& imagePoints, const Mat& objectPoints, Mat& rvec,
-                            Mat&  tvec, Mat& J, const int MaxIter,
-                            const IntrinsicParams& param, const double thresh_cond);
-CV_EXPORTS Mat ComputeHomography(Mat m, Mat M);
-    
-    
-    {
-    {
-    {
-    {                        const float eps = 1e-4f;
-                        //TODO: perhaps it is better to normalize the cross product by norms of the tangent vectors
-                        if (fabs(tangentVector_1.cross(tangentVector_2)) < eps)
-                        {
-                            isGeneralPosition = false;
-                        }
-                    }
-                }
-            }
-        }
-        while(!isGeneralPosition);
-    }
-    else
-    {
-        //create points in a degenerate position (there are at least 3 points belonging to the same line)
-    
-            template<int CTA_SIZE>
-        static __forceinline__ __device__ int Ballot(int predicate)
-        {
-#if defined (__CUDA_ARCH__) && (__CUDA_ARCH__ >= 200)
-            return __ballot(predicate);
-#else
-            __shared__ volatile int cta_buffer[CTA_SIZE];
-    }
-    
-    //! @cond IGNORED
-    
-    #ifndef CODEGEN_FUNCPTR
-    #define CODEGEN_REMOVE_FUNCPTR
-    #if defined(_WIN32)
-        #define CODEGEN_FUNCPTR APIENTRY
-    #else
-        #define CODEGEN_FUNCPTR
-    #endif
-#endif // CODEGEN_FUNCPTR
-    
-    // Computes and returns the squared evaluation metric for a line fit.
-double DetLineFit::EvaluateLineFit() {
-  // Compute the upper quartile error from the line.
-  double dist = ComputeUpperQuartileError();
-  if (distances_.size() >= kMinPointsForErrorCount &&
-      dist > kMaxRealDistance * kMaxRealDistance) {
-    // Use the number of mis-fitted points as the error metric, as this
-    // gives a better measure of fit for badly fitted lines where more
-    // than a quarter are badly fitted.
-    double threshold = kMaxRealDistance * sqrt(square_length_);
-    dist = NumberOfMisfittedPoints(threshold);
-  }
-  return dist;
+    int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
-    
-    #include <stdio.h>
-#include <math.h>
-#include 'quadlsq.h'
-#include 'tprintf.h'
-    
-    /**----------------------------------------------------------------------------
-          Include Files and Type Defines
-----------------------------------------------------------------------------**/
-#include 'strngs.h'
-    
-    // Feature distance calculator designed to provide a fast distance calculation
-// based on set difference between a given feature set and many other feature
-// sets in turn.
-// Representation of a feature set as an array of bools that are sparsely
-// true, and companion arrays that allow fast feature set distance
-// calculations with allowance of offsets in position.
-// Init is expensive, so for greatest efficiency, to re-initialize for a new
-// feature set, use Set(..., false) on the SAME feature set as was used to
-// setup with Set(..., true), to return to its initialized state before
-// reuse with Set(..., true) on a new feature set.
-class IntFeatureDist {
- public:
-  IntFeatureDist();
-  ~IntFeatureDist();
-    }
-    
-            const char* StringRange::Data() const
-        {
-            return m_data;
-        }
-    
-    
-    {            // m_start and m_end track the captured range.
-            // m_end advances when LStrip() is called.
-            const char* m_start;
-            const char* m_end;
-        };
-    
-        // Taken from ONNX
-    REGISTER_OPERATOR_SCHEMA(Gather)
-        .Description('Given data tensor of rank r >= 1, and indices tensor of rank q, gather '
-            'entries of the outer-most dimension of data indexed by indices, and concatenate '
-            'them in an output tensor of rank q + (r - 1). '
-            'Example 1: data = [ [1.0, 1.2], [2.3, 3.4], [4.5, 5.7], ] '
-            '           indices = [ [0, 1], [1, 2], ] '
-            '           output = [ [ [1.0, 1.2], [2.3, 3.4], ], [ [2.3, 3.4], [4.5, 5.7], ], ]'
-            'Example 2: data = [ [1.0, 1.2, 1.9], [2.3, 3.4, 3.9], [4.5, 5.7, 5.9], ] '
-            '           indices = [0, 2], ] axis = 1, '
-            '           output = [ [ [1.0, 1.9], [2.3, 3.9], [4.5, 5.9], ], ]')
-        .Input('data', 'Tensor of rank r >= 1.', 'T')
-        .Input('indices', 'Tensor of int32/int64 indices, of any rank q.', 'Tind')
-        .Output('output', 'Tensor of rank q + (r - 1).', 'T')
-        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
-            'Constrain input types to float tensors.')
-        .TypeConstraint('Tind', { 'tensor(int32)', 'tensor(int64)' },
-            'Constrain indices types to float tensors.')
-        .Attr('axis',
-            'Which axis to gather on, defaults to 0. Negative value means counting dimensions '
-            'from the back. Accepted range in [-r, r-1]',
-            AttrType::AttributeProto_AttributeType_INT, int64_t(0));
-    
-    // Class represents a description of an HTK chunk.
-// It is only used internally by the HTK deserializer.
-// Can exist without associated data and provides methods for requiring/releasing chunk data.
-// TODO: We should consider splitting data load from the description in the future versions.
-class HTKChunkInfo
-{
-    // All utterances in the chunk.
-    std::vector<UtteranceDescription> m_utterances;
-    }
-    
-    
-    {
-    {        if (flags & CopyNodeFlags::copyNodeInputLinks)
-        {
-            // copy the children structure but use the new nodes generated
-            for (int i = 0; i < fromNode->GetNumInputs(); i++)
-                toNode->SetInput(i, GetNodeFromName(toNamePrefix + fromNode->GetInputs()[i]->NodeName()));
-        }
-    }
-}
-    
-    
-    {
-    {
-    {// utility function to round an integer up to a multiple of size
-size_t RoundUp(size_t value, size_t size);
-// HIGH and LOW DWORD functions
-DWORD HIDWORD(size_t size);
-DWORD LODWORD(size_t size);
-} } }
 
     
-    /// An internal runnable used throughout osquery as dispatcher services.
-using InternalRunnableRef = std::shared_ptr<InternalRunnable>;
-using InternalThreadRef = std::shared_ptr<std::thread>;
+    #include 'test/cpp/qps/histogram.h'
     
     
     { private:
-  std::string process_name_;
+  /*! \brief the underlying stream */
+  dmlc::Stream *stream_;
+  /*! \brief buffer to hold data */
+  std::string buffer_;
+  /*! \brief length of valid data in buffer */
+  size_t read_len_;
+  /*! \brief pointer in the buffer */
+  size_t read_ptr_;
 };
     
-     private:
-  AddExternalCallback add_;
-  RemoveExternalCallback remove_;
+      delete metric;
+  metric = xgboost::Metric::Create('map@-');
+  ASSERT_STREQ(metric->Name(), 'map@-');
+  EXPECT_NEAR(GetMetricEval(metric, {}, {}), 0, 1e-10);
     
-      // Enables pretty-printing in gtest (ASSERT|EXPECT)_(EQ|NE)
-  friend ::std::ostream& operator<<(::std::ostream& os, const Status& s);
+      XGBOOST_DEVICE GradientPairInternal<T> &operator+=(
+      const GradientPairInternal<T> &rhs) {
+    grad_ += rhs.grad_;
+    hess_ += rhs.hess_;
+    return *this;
+  }
     
-        size_t length = 0;
-    void *e = NULL;
-    switch (*(int *)data) {
-    case 0:
-      e = osquery_cqueue_reserve(
-          &osquery.cqueue, OSQUERY_TEST_EVENT_0, sizeof(test_event_0_data_t));
-      length = 4096;
-      break;
-    case 1:
-      e = osquery_cqueue_reserve(
-          &osquery.cqueue, OSQUERY_TEST_EVENT_1, sizeof(test_event_1_data_t));
-      length = 33;
-      break;
-    default:
-      return -ENOTTY;
-    }
-    if (!e) {
-      return -EINVAL;
-    }
-    
-      /*
-   * @brief a unique ID identifying the 'carve'
-   *
-   * This unique generated GUID is used to identify the carve session from
-   * other carves. It is also used by our backend service to derive a
-   * session key for exfiltration.
+      virtual void InitColAccess(const std::vector<bool>& enabled,
+                             float subsample,
+                             size_t max_row_perbatch, bool sorted) = 0;
+  // the following are column meta data, should be able to answer them fast.
+  /*! \return whether column access is enabled */
+  virtual bool HaveColAccess(bool sorted) const = 0;
+  /*! \return Whether the data columns single column block. */
+  virtual bool SingleColBlock() const = 0;
+  /*! \brief get number of non-missing entries in column */
+  virtual size_t GetColSize(size_t cidx) const = 0;
+  /*! \brief get column density */
+  virtual float GetColDensity(size_t cidx) const = 0;
+  /*! \return reference of buffered rowset, in column access */
+  virtual const RowSet& BufferedRowset() const = 0;
+  /*! \brief virtual destructor */
+  virtual ~DMatrix() = default;
+  /*!
+   * \brief Save DMatrix to local file.
+   *  The saved file only works for non-sharded dataset(single machine training).
+   *  This API is deprecated and dis-encouraged to use.
+   * \param fname The file name to be saved.
+   * \return The created DMatrix.
    */
-  std::string carveGuid_;
+  virtual void SaveToLocalFile(const std::string& fname);
+  /*!
+   * \brief Load DMatrix from URI.
+   * \param uri The URI of input.
+   * \param silent Whether print information during loading.
+   * \param load_row_split Flag to read in part of rows, divided among the workers in distributed mode.
+   * \param file_format The format type of the file, used for dmlc::Parser::Create.
+   *   By default 'auto' will be able to load in both local binary file.
+   * \return The created DMatrix.
+   */
+  static DMatrix* Load(const std::string& uri,
+                       bool silent,
+                       bool load_row_split,
+                       const std::string& file_format = 'auto');
+  /*!
+   * \brief create a new DMatrix, by wrapping a row_iterator, and meta info.
+   * \param source The source iterator of the data, the create function takes ownership of the source.
+   * \param cache_prefix The path to prefix of temporary cache file of the DMatrix when used in external memory mode.
+   *     This can be nullptr for common cases, and in-memory mode will be used.
+   * \return a Created DMatrix.
+   */
+  static DMatrix* Create(std::unique_ptr<DataSource>&& source,
+                         const std::string& cache_prefix = '');
+  /*!
+   * \brief Create a DMatrix by loading data from parser.
+   *  Parser can later be deleted after the DMatrix i created.
+   * \param parser The input data parser
+   * \param cache_prefix The path to prefix of temporary cache file of the DMatrix when used in external memory mode.
+   *     This can be nullptr for common cases, and in-memory mode will be used.
+   * \sa dmlc::Parser
+   * \note dmlc-core provides efficient distributed data parser for libsvm format.
+   *  User can create and register customized parser to load their own format using DMLC_REGISTER_DATA_PARSER.
+   *  See 'dmlc-core/include/dmlc/data.h' for detail.
+   * \return A created DMatrix.
+   */
+  static DMatrix* Create(dmlc::Parser<uint32_t>* parser,
+                         const std::string& cache_prefix = '');
     
-    class TLSConfigPlugin : public ConfigPlugin,
-                        public std::enable_shared_from_this<TLSConfigPlugin> {
- public:
-  Status setUp() override;
-  Status genConfig(std::map<std::string, std::string>& config) override;
-    }
+    /*!
+ * \brief Registry entry for tree updater.
+ */
+struct GradientBoosterReg
+    : public dmlc::FunctionRegEntryBase<
+  GradientBoosterReg,
+  std::function<GradientBooster* (const std::vector<std::shared_ptr<DMatrix> > &cached_mats,
+                                  bst_float base_margin)> > {
+};
     
-    namespace osquery {
+    
+    {    void throw_exception( std::exception const & e ) {
+        xfatal2(TSF'boost exception:%_', e.what());
+        
+#ifdef ANDROID
+        char stack[4096] = {0};
+        android_callstack(stack, sizeof(stack));
+        xfatal2(TSF'%_', stack);
+#endif
     }
+}
+
+    
+    
+/*
+ * ServiceBase.h
+ *
+ *  Created on: 2013-6-20
+ *      Author: yerungui
+ */
+    
+    
+    {  private:
+    void* m_this;
+    std::map<const std::string, boost::any> m_variablemap;
+};
+    
+    
+    {    SPY_DETACH_VARIABLE('TestFun0 i');
+}
+    
+      private:
+    int __TestFun1(int i);
+    
+    //
+//  testspy_spy.cpp
+//  PublicComponent
+//
+//  Created by yerungui on 14-5-14.
+//
