56867c8625b9e8ae8e53de935293b038b2225af2
SUNsung <weatherius@gmail.com>
2018-04-22 22:46:30 +0300

    Rewrite [56L6S30E224Vw56oLDuUT9rTCQ562FgL6HV0yd656X2I.py] File in: 2018-04-22 22:46:30

--- pyton/56L6S30E224Vw56oLDuUT9rTCQ562FgL6HV0yd656X2I.py
+++ pyton/56L6S30E224Vw56oLDuUT9rTCQ562FgL6HV0yd656X2I.py
@@ -2,86 +2,65 @@
         
         
 @keras_test
-def test_conv1d_legacy_interface():
-    old_layer = keras.layers.Convolution1D(5,
-                                           filter_length=3,
-                                           input_dim=3,
-                                           input_length=4,
-                                           name='conv')
-    new_layer = keras.layers.Conv1D(5, 3, name='conv', input_shape=(4, 3))
-    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer.get_config())
-    
-        # test fit
-    out = model.fit(None,
-                    output_a_np, epochs=1, batch_size=10)
-    out = model.fit(None,
-                    output_a_np, epochs=1, batch_size=10)
-    
-    from keras.models import Sequential
-from keras.engine.training import _weighted_masked_objective
-from keras.layers import TimeDistributed, Masking, Dense
-from keras.utils.test_utils import keras_test
-from keras import losses
-from keras import backend as K
-    
-    print('Build model...')
+def test_masking():
+    np.random.seed(1337)
+    x = np.array([[[1], [1]],
+                  [[0], [0]]])
+    model = Sequential()
+    model.add(Masking(mask_value=0, input_shape=(2, 1)))
+    model.add(TimeDistributed(Dense(1, kernel_initializer='one')))
+    model.compile(loss='mse', optimizer='sgd')
+    y = np.array([[[1], [1]],
+                  [[1], [1]]])
+    loss = model.train_on_batch(x, y)
+    assert loss == 0
     
     
 @pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
-def test_glorot_uniform(tensor_shape):
-    fan_in, fan_out = initializers._compute_fans(tensor_shape)
-    scale = np.sqrt(6. / (fan_in + fan_out))
-    _runner(initializers.glorot_uniform(), tensor_shape,
-            target_mean=0., target_max=scale, target_min=-scale)
+def test_normal(tensor_shape):
+    _runner(initializers.RandomNormal(mean=0, stddev=1), tensor_shape,
+            target_mean=0., target_std=1)
+    
+        nloss = model.evaluate([x_test, x_test], y_test, verbose=0)
+    assert(loss == nloss)
     
     
-@keras_test
-def test_unit_norm():
-    unit_norm_instance = constraints.unit_norm()
-    normalized = unit_norm_instance(K.variable(get_example_array()))
-    norm_of_normalized = np.sqrt(np.sum(K.eval(normalized) ** 2, axis=0))
-    # In the unit norm constraint, it should be equal to 1.
-    difference = norm_of_normalized - 1.
-    largest_difference = np.max(np.abs(difference))
-    assert(np.abs(largest_difference) < 10e-5)
+@zope.interface.implementer(interfaces.IAuthenticator)
+@zope.interface.provider(interfaces.IPluginFactory)
+class Authenticator(common.Plugin):
+    '''Example Authenticator.'''
     
-        # Test with Sequential API
-    model = Sequential([
-        layers.Dense(16, input_shape=(x_train.shape[-1],), activation='relu'),
-        layers.Dense(8),
-        layers.Activation('relu'),
-        layers.Dense(num_classes, activation='softmax')
-    ])
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='rmsprop',
-                  metrics=['accuracy'])
-    model.summary()
-    history = model.fit(x_train, y_train, epochs=15, batch_size=16,
-                        validation_data=(x_test, y_test),
-                        verbose=0)
-    assert(history.history['val_acc'][-1] > 0.8)
-    config = model.get_config()
-    model = Sequential.from_config(config)
     
-    for j in range(16):
-    new_pos = seq.predict(track[np.newaxis, ::, ::, ::, ::])
-    new = new_pos[::, -1, ::, ::, ::]
-    track = np.concatenate((track, new), axis=0)
+def set_up_logging(verbosity):
+    levels = {-2: logging.ERROR, -1: logging.WARN, 0: logging.INFO,
+              1: logging.DEBUG}
+    max_level = max(levels.keys())
+    min_level = min(levels.keys())
+    verbosity = min(verbosity, max_level)
+    verbosity = max(verbosity, min_level)
+    level = levels[verbosity]
+    format_ = '%(levelname)s %(message)s'
+    logging.basicConfig(level=level, format=format_)
     
-        def __init__(self, size):
-        self.size = size
-        self.table = [[] for _ in range(self.size)]
+        g.plugins.load_plugins(config)
+    config['r2.plugins'] = g.plugins
+    g.startup_timer.intermediate('plugins')
     
-        def deal_card(self):
-        try:
-            card = self.cards[self.deal_index]
-            card.is_available = False
-            self.deal_index += 1
-        except IndexError:
-            return None
-        return card
     
-        def __init__(self, person_ids, lookup):
-        self.lookup = lookup
-        self.person_ids = person_ids
-        self.visited_ids = set()
+class LimitUploadSize(object):
+    '''
+    Middleware for restricting the size of uploaded files (such as
+    image files for the CSS editing capability).
+    '''
+    def __init__(self, app, max_size=1024*500):
+        self.app = app
+        self.max_size = max_size
+    
+    api('organiclisting',       OrganicListingJsonTemplate)
+api('subreddittraffic', TrafficJsonTemplate)
+api('takedownpane', TakedownJsonTemplate)
+api('policyview', PolicyViewJsonTemplate)
+    
+        def GET_promohealth(self):
+        response.content_type = 'application/json'
+        return json.dumps(promote.health_check())
