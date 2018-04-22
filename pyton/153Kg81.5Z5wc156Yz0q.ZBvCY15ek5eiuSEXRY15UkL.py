58207dad5b918f3164d1b68161c7a42817075299
SUNsung <weatherius@gmail.com>
2018-04-22 20:40:30 +0300

    Rewrite [153Kg81.5Z5wc156Yz0q.ZBvCY15ek5eiuSEXRY15UkL.py] File in: 2018-04-22 20:40:30

--- pyton/153Kg81.5Z5wc156Yz0q.ZBvCY15ek5eiuSEXRY15UkL.py
+++ pyton/153Kg81.5Z5wc156Yz0q.ZBvCY15ek5eiuSEXRY15UkL.py
@@ -1,143 +1,79 @@
 
         
-        '''
-    The approach taken is explained below. I decided to do it simply.
-    Initially I was considering parsing the data into some sort of
-    structure and then generating an appropriate README. I am still
-    considering doing it - but for now this should work. The only issue
-    I see is that it only sorts the entries at the lowest level, and that
-    the order of the top-level contents do not match the order of the actual
-    entries.
+            from inspect import getfullargspec as getargspec
+    from io import StringIO
     
-    # TODO: ensure that history changes.
+        def __set__(self, obj, value):
+        obj.config[self.__name__] = value
+    
+    
+# Core signals.  For usage examples grep the source code or consult
+# the API documentation in docs/api.rst as well as docs/signals.rst
+template_rendered = _signals.signal('template-rendered')
+before_render_template = _signals.signal('before-render-template')
+request_started = _signals.signal('request-started')
+request_finished = _signals.signal('request-finished')
+request_tearing_down = _signals.signal('request-tearing-down')
+got_request_exception = _signals.signal('got-request-exception')
+appcontext_tearing_down = _signals.signal('appcontext-tearing-down')
+appcontext_pushed = _signals.signal('appcontext-pushed')
+appcontext_popped = _signals.signal('appcontext-popped')
+message_flashed = _signals.signal('message-flashed')
 
     
-      def build_time_series_hidden_layers(self):
-    '''Builds hidden layers for the time series features.
+    
+def test_config_from_envvar():
+    env = os.environ
+    try:
+        os.environ = {}
+        app = flask.Flask(__name__)
+        with pytest.raises(RuntimeError) as e:
+            app.config.from_envvar('FOO_SETTINGS')
+        assert ''FOO_SETTINGS' is not set' in str(e.value)
+        assert not app.config.from_envvar('FOO_SETTINGS', silent=True)
     
     
-class InputOpsTest(tf.test.TestCase):
+def test_installed_package_paths(limit_loader, modules_tmpdir,
+                                 modules_tmpdir_prefix, purge_module,
+                                 monkeypatch):
+    installed_path = modules_tmpdir.mkdir('path')
+    monkeypatch.syspath_prepend(installed_path)
+    
+    import sys, os
     
     
-def unflatten(flat_config):
-  '''Transforms a flat configuration dictionary into a nested dictionary.
+def FormatDebugInfoResponse_ExtraConfFoundButNotLoaded_test():
+  response = deepcopy( GENERIC_RESPONSE )
+  response[ 'extra_conf' ].update( {
+    'is_loaded': False,
+    'path': '/path/to/extra/conf'
+  } )
+  assert_that(
+    FormatDebugInfoResponse( response ),
+    contains_string(
+      'Extra configuration file found but not loaded\n'
+      'Extra configuration path: /path/to/extra/conf\n'
+    )
+  )
     
-      return all_time, all_flux
-
+        def test_cat_adapter_shall_make_noise(self):
+        cat = Cat()
+        cat_adapter = Adapter(cat, make_noise=cat.meow)
+        noise = cat_adapter.make_noise()
+        expected_noise = 'meow!'
+        self.assertEqual(noise, expected_noise)
     
-      for _ in range(maxiter):
-    if spline is None:
-      mask = np.ones_like(time, dtype=np.bool)  # Try to fit all points.
-    else:
-      # Choose points where the absolute deviation from the median residual is
-      # less than 3*sigma, where sigma is a robust estimate of the standard
-      # deviation of the residuals from the previous spline.
-      residuals = flux - spline
-      _, _, new_mask = robust_mean.robust_mean(residuals, cut=outlier_cut)
+        def test_items_recoil(self):
+        with ObjectPool(self.sample_queue, True) as pool:
+            self.assertEqual(pool, 'first')
+        self.assertTrue(self.sample_queue.get() == 'second')
+        self.assertFalse(self.sample_queue.empty())
+        self.assertTrue(self.sample_queue.get() == 'first')
+        self.assertTrue(self.sample_queue.empty())
     
-      hparams.num_classes = len(classes)
-  print('Model will predict into %d classes' % hparams.num_classes)
+        @classmethod
+    def get_registry(cls):
+        return dict(cls.REGISTRY)
     
-    
-def _get_file_names():
-  '''Returns the file names expected to exist in the input_dir.'''
-  file_names = {}
-  file_names['train'] = ['data_batch_%d' % i for i in xrange(1, 5)]
-  file_names['validation'] = ['data_batch_5']
-  file_names['eval'] = ['test_batch']
-  return file_names
-    
-    
-def get_nodes(data):
-    return [node['hostname'] for node in data]
-    
-    
-def get_nodes(data):
-    return [node['Name'] for node in data]
-    
-        input_a_np = np.random.random((10, 3))
-    output_a_np = np.random.random((10, 4))
-    
-            if self.return_state:
-            if not isinstance(states, (list, tuple)):
-                states = [states]
-            else:
-                states = list(states)
-            return [output] + states
-        else:
-            return output
-    
-        # Normally the trailing 1 is added by standardize_weights
-    weights = np.ones((3,))
-    mask = np.ones((3, 4))
-    mask[1, 0] = 0
-    
-        # fit with validation data and accuracy
-    model.fit([X_train] * 2, [y_train] * 2, batch_size=batch_size,
-              validation_data=([X_test] * 2, [y_test] * 2),
-              callbacks=callbacks_factory(histogram_freq=1), epochs=2)
-    
-    print('Train...')
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_data=(x_test, y_test))
-score, acc = model.evaluate(x_test, y_test, batch_size=batch_size)
-print('Test score:', score)
-print('Test accuracy:', acc)
-
-    
-    # this Evaluator class makes it possible
-# to compute loss and gradients in one pass
-# while retrieving them via two separate functions,
-# 'loss' and 'grads'. This is done because scipy.optimize
-# requires separate functions for loss and gradients,
-# but computing them separately would be inefficient.
-    
-    
-def test_time_distributed_softmax():
-    x = K.placeholder(shape=(1, 1, 5))
-    f = K.function([x], [activations.softmax(x)])
-    test_values = get_standard_values()
-    test_values = np.reshape(test_values, (1, 1, np.size(test_values)))
-    f([test_values])[0]
-    
-    This module is used internally by Tornado.  It is not necessarily expected
-that the functions and classes defined here will be useful to other
-applications, but they are documented here in case they are.
-    
-        @gen_test
-    def test_http10_no_content_length(self):
-        # Regression test for a bug in which can_keep_alive would crash
-        # for an HTTP/1.0 (not 1.1) response with no content-length.
-        conn = HTTP1Connection(self.client_stream, True)
-        self.server_stream.write(b'HTTP/1.0 200 Not Modified\r\n\r\nhello')
-        self.server_stream.close()
-    
-    
-# This is kind of hacky, but run some of the HTTPServer and web tests
-# through WSGIContainer and WSGIApplication to make sure everything
-# survives repeated disassembly and reassembly.
-class WSGIConnectionTest(httpserver_test.HTTPConnectionTest):
-    def get_app(self):
-        return WSGIContainer(validator(WSGIApplication(self.get_handlers())))
-    
-            sock, port = bind_unused_port()
-        server = namespace['TestServer']()
-        server.add_socket(sock)
-        client = IOStream(socket.socket())
-        yield client.connect(('localhost', port))
-        result = yield client.read_until_close()
-        self.assertEqual(result, b'data')
-        server.stop()
-        client.close()
-    
-    
-def handle_sigchld(sig, frame):
-    IOLoop.current().add_callback_from_signal(IOLoop.current().stop)
-    
-        def callback(response):
-        response.rethrow()
-        assert len(response.body) == (options.num_chunks * options.chunk_size)
-        logging.warning('fetch completed in %s seconds', response.request_time)
-        IOLoop.current().stop()
+        def test_bear_greek_localization(self):
+        self.assertEqual(self.g.get('bear'), 'bear')
