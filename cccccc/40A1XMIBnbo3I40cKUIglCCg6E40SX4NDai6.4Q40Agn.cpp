a71c8592b06abc8d81e9cd0b53b7cca04616aee6
SUNsung <weatherius@gmail.com>
2018-04-22 14:32:19 +0300

    Rewrite [40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp] File in: 2018-04-22 14:32:18

--- cccccc/40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp
+++ cccccc/40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp
@@ -1,172 +1,212 @@
 
         
-        namespace message_descriptor {
-PyObject* NewMessageFieldsByName(const Descriptor* descriptor);
-PyObject* NewMessageFieldsByCamelcaseName(const Descriptor* descriptor);
-PyObject* NewMessageFieldsByNumber(const Descriptor* descriptor);
-PyObject* NewMessageFieldsSeq(const Descriptor* descriptor);
-    }
+          ConvertUTF8toUTF32(&SourceNext, SourceStart + S.size(), &TargetStart,
+                     TargetStart + 1, llvm::lenientConversion);
+  if (TargetStart == &C) {
+    // The source string contains an ill-formed subsequence at the end.
+    return false;
+  }
     
-      virtual void GenerateCloningCode(io::Printer* printer);
-  virtual void GenerateFreezingCode(io::Printer* printer);
-  virtual void GenerateMembers(io::Printer* printer);
-  virtual void GenerateMergingCode(io::Printer* printer);
-  virtual void GenerateParsingCode(io::Printer* printer);
-  virtual void GenerateSerializationCode(io::Printer* printer);
-  virtual void GenerateSerializedSizeCode(io::Printer* printer);
+      // Only allow allocation using the allocator in MarkupContext or by
+  // placement new.
+  void *operator new(size_t Bytes, swift::markup::MarkupContext &MC,
+                     unsigned Alignment = alignof(DocComment));
+  void *operator new(size_t Bytes, void *Mem) {
+    assert(Mem);
+    return Mem;
+  }
     
-    #include <string>
-    
-      void WriteIntroduction(io::Printer* printer);
-  void WriteDescriptor(io::Printer* printer);
-  void WriteGeneratedCodeInfo(const Descriptor* descriptor,
-                              io::Printer* printer,
-                              bool last);
-    
-    #endif  // GOOGLE_PROTOBUF_COMPILER_CSHARP_REPEATED_ENUM_FIELD_H__
-    
-    void SourceGeneratorBase::WriteGeneratedCodeAttributes(io::Printer* printer) {
-  printer->Print('[global::System.Diagnostics.DebuggerNonUserCodeAttribute]\n');
-}
+    namespace llvm {
+class FoldingSetNodeID;
+} // end namespace llvm
     
     
-    {  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(SourceGeneratorBase);
+    {  IndexRelation() = default;
 };
     
-    #include <google/protobuf/compiler/java/java_generator_factory.h>
+    #include 'swift/AST/Type.h'
+#include 'llvm/ADT/ArrayRef.h'
+#include 'llvm/ADT/Optional.h'
     
-    
-    {
-    {
-    {
-    {                        const float eps = 1e-4f;
-                        //TODO: perhaps it is better to normalize the cross product by norms of the tangent vectors
-                        if (fabs(tangentVector_1.cross(tangentVector_2)) < eps)
-                        {
-                            isGeneralPosition = false;
-                        }
-                    }
-                }
-            }
-        }
-        while(!isGeneralPosition);
-    }
-    else
-    {
-        //create points in a degenerate position (there are at least 3 points belonging to the same line)
-    
-      virtual ParamIteratorInterface<ParamType>* Begin() const {
-    return new Iterator(this, g1_, g1_.begin(), g2_, g2_.begin(), g3_,
-        g3_.begin(), g4_, g4_.begin());
-  }
-  virtual ParamIteratorInterface<ParamType>* End() const {
-    return new Iterator(this, g1_, g1_.end(), g2_, g2_.end(), g3_, g3_.end(),
-        g4_, g4_.end());
-  }
-    
-    
-    {        static_cast<ParamGenerator<T$j> >(g$(j)_)
-]]));
-  }
-    
-      template <GTEST_9_TYPENAMES_(U)>
-  tuple& operator=(const GTEST_9_TUPLE_(U)& t) {
-    return CopyFrom(t);
-  }
-    
-      // Returns true iff n is a prime number.
-  virtual bool IsPrime(int n) const = 0;
-    
-    template <typename Stream>
-class buffered_stream;
-    
-    #endif // BOOST_ASIO_DETAIL_ATOMIC_COUNT_HPP
-
-    
-      std::size_t count() const
-  {
-    return count_;
-  }
-    
-      STDMETHODIMP put_Length(UINT32 value)
-  {
-    if (value > capacity_)
-      return E_INVALIDARG;
-    length_ = value;
-    return S_OK;
-  }
-    
-    #include <boost/asio/detail/config.hpp>
-    
-    
-    {}  // namespace guetzli
-    
-    namespace guetzli {
-    }
-    
-    // Output callback function with associated data.
-struct JPEGOutput {
-  JPEGOutput(JPEGOutputHook cb, void* data) : cb(cb), data(data) {}
-  bool Write(const uint8_t* buf, size_t len) const {
-    return (len == 0) || (cb(data, buf, len) == len);
-  }
- private:
-  JPEGOutputHook cb;
-  void* data;
-};
-    
-    #include 'guetzli/jpeg_data.h'
-    
-    struct HuffmanTableEntry {
-  // Initialize the value to an invalid symbol so that we can recognize it
-  // when reading the bit stream using a Huffman code with space > 0.
-  HuffmanTableEntry() : bits(0), value(0xffff) {}
-    }
-    
-    
-    {}  // namespace
-    
-    
-    {    void toJS(nbind::cbOutput expose) const
-    {
-        expose(unit, value);
-    }
-};
-
-    
-    TEST_F(YogaTest_HadOverflowTests, spacing_overflow_no_wrap_and_no_flex_children) {
-  const YGNodeRef child0 = YGNodeNewWithConfig(config);
-  YGNodeStyleSetWidth(child0, 80);
-  YGNodeStyleSetHeight(child0, 40);
-  YGNodeStyleSetMargin(child0, YGEdgeTop, 10);
-  YGNodeStyleSetMargin(child0, YGEdgeBottom, 10);
-  YGNodeInsertChild(root, child0, 0);
-  const YGNodeRef child1 = YGNodeNewWithConfig(config);
-  YGNodeStyleSetWidth(child1, 80);
-  YGNodeStyleSetHeight(child1, 40);
-  YGNodeStyleSetMargin(child1, YGEdgeBottom, 5);
-  YGNodeInsertChild(root, child1, 1);
-    }
-    
-    #pragma once
-    
-    void Node::setFlexBasisPercent(double flexBasis)
+    Substitution::Substitution(Type Replacement,
+                           ArrayRef<ProtocolConformanceRef> Conformance)
+  : Replacement(Replacement), Conformance(Conformance)
 {
-    YGNodeStyleSetFlexBasisPercent(m_node, flexBasis);
+  // The replacement type must be materializable.
+  assert(Replacement->isMaterializable()
+         && 'cannot substitute with a non-materializable type');
 }
     
-    private:
-  void ref() {
-    ++m_refcount;
-  }
+    Error HTTPClient::get_response_headers(List<String> *r_response) {
+    }
     
-      std::string asFormattedString() const {
-    std::stringstream str;
-    str << 'Function ' << m_functionName << ' in file ' << m_fileName << ':' << m_lineNumber;
-    return str.str();
-  }
+    Size2 OS_JavaScript::get_screen_size(int p_screen) const {
+    }
     
-    template <typename T, typename U>
-inline bool operator==(const RefPtr<T>& ref, U* ptr) {
-  return ref.get() == ptr;
+    	String issues_file = 'msbuild_issues.csv';
+	DirAccessRef d = DirAccess::create_for_path(logs_dir);
+	if (d->file_exists(issues_file)) {
+		Error err = d->remove(issues_file);
+		if (err != OK) {
+			exited = true;
+			String file_path = ProjectSettings::get_singleton()->localize_path(logs_dir).plus_file(issues_file);
+			String message = 'Cannot remove issues file: ' + file_path;
+			build_tab->on_build_exec_failed(message);
+			ERR_EXPLAIN(message);
+			ERR_FAIL();
+		}
+	}
+    
+      {2,0,32,  &_residue_44_low,
+   &_huff_book__44c0_s_long,&_huff_book__44c0_sm_long,
+   &_resbook_44s_0,&_resbook_44sm_0}
+};
+static const vorbis_residue_template _res_44s_1[]={
+  {2,0,16,  &_residue_44_low,
+   &_huff_book__44c1_s_short,&_huff_book__44c1_sm_short,
+   &_resbook_44s_1,&_resbook_44sm_1},
+    
+    /* mapping conventions:
+   only one submap (this would change for efficient 5.1 support for example)*/
+/* Four psychoacoustic profiles are used, one for each blocktype */
+static const vorbis_info_mapping0 _map_nominal_u[2]={
+  {1, {0,0,0,0,0,0}, {0}, {0}, 0,{0},{0}},
+  {1, {0,0,0,0,0,0}, {1}, {1}, 0,{0},{0}}
+};
+    
+    /* The bark scale equations are approximations, since the original
+   table was somewhat hand rolled.  The below are chosen to have the
+   best possible fit to the rolled tables, thus their somewhat odd
+   appearance (these are more accurate and over a longer range than
+   the oft-quoted bark equations found in the texts I have).  The
+   approximations are valid from 0 - 30kHz (nyquist) or so.
+    
+    
+/** 16x32 multiplication, followed by a 15-bit shift right. Results fits in 32 bits */
+#undef MULT16_32_Q15
+static OPUS_INLINE opus_val32 MULT16_32_Q15_armv4(opus_val16 a, opus_val32 b)
+{
+  unsigned rd_lo;
+  int rd_hi;
+  __asm__(
+      '#MULT16_32_Q15\n\t'
+      'smull %0, %1, %2, %3\n\t'
+      : '=&r'(rd_lo), '=&r'(rd_hi)
+      : '%r'(b), 'r'(a<<16)
+  );
+  /*We intentionally don't OR in the high bit of rd_lo for speed.*/
+  return rd_hi<<1;
 }
+#define MULT16_32_Q15(a, b) (MULT16_32_Q15_armv4(a, b))
+    
+      assert(peekType(msg) == thpp::Type::LONG_STORAGE);
+  THLongStorage *storage2 = unpackTHLongStorage(msg);
+  assert(storage2->size == STORAGE_SIZE);
+  for (int64_t i = 0; i < STORAGE_SIZE; i++)
+    assert(storage2->data[i] == i);
+  
+  int vec_size = unpackInteger(msg);
+  assert(vec_size == VEC_SIZE);
+  for (int i = 0; i < VEC_SIZE; i++)
+    assert(unpackInteger(msg) == 7);
+    
+    #define DEFINE_ACCESSOR(type,name,member) \
+  type to##name () const { \
+    if (Tag::HAS_t == tag) { \
+      return local().to##name(); \
+    } else if (Tag::HAS_d == tag) { \
+      return checked_convert<type, double>(v.d, #type); \
+    } else { \
+      return checked_convert<type, int64_t>(v.i, #type); \
+    } \
+  }
+    
+    void* ${Storage}::data() {
+  return storage->data;
+}
+    
+    static PyObject* recursive_to_list(
+    char* data, IntList sizes, IntList strides, int64_t dim,
+    ScalarType scalarType, int64_t elementSize)
+{
+  int64_t ndim = sizes.size();
+  if (dim == ndim) {
+    return torch::utils::load_scalar(data, scalarType);
+  }
+  auto n = sizes[dim];
+  auto list = THPObjectPtr(PyList_New(n));
+  if (!list) throw python_error();
+  for (int64_t i = 0; i < n; i++) {
+    PyObject* obj = recursive_to_list(data, sizes, strides, dim + 1, scalarType, elementSize);
+    if (!obj) throw python_error();
+    PyList_SET_ITEM(list.get(), i, obj);
+    data += strides[dim] * elementSize;
+  }
+  return list.release();
+}
+    
+                ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our windows open/close state
+            ImGui::Checkbox('Another Window', &show_another_window);
+    
+        // Setup ImGui binding
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+    ImGui_ImplSdlGL2_Init(window);
+    
+    #define IM_VEC4_CLASS_EXTRA                                                 \
+        ImVec4(const MyVec4& f) { x = f.x; y = f.y; z = f.z; w = f.w; }     \
+        operator MyVec4() const { return MyVec4(x,y,z,w); }
+*/
+    
+        bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    
+    // Data
+static ID3D11Device*            g_pd3dDevice = NULL;
+static ID3D11DeviceContext*     g_pd3dDeviceContext = NULL;
+static IDXGISwapChain*          g_pSwapChain = NULL;
+static ID3D11RenderTargetView*  g_mainRenderTargetView = NULL;
+    
+    #include <stdio.h>          // printf, fprintf
+#include <stdlib.h>         // abort
+#define GLFW_INCLUDE_NONE
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+    
+    TEST_F(MockEnvTest, Corrupt) {
+  const std::string kGood = 'this is a good string, synced to disk';
+  const std::string kCorrupted = 'this part may be corrupted';
+  const std::string kFileName = '/dir/f';
+  unique_ptr<WritableFile> writable_file;
+  ASSERT_OK(env_->NewWritableFile(kFileName, &writable_file, soptions_));
+  ASSERT_OK(writable_file->Append(kGood));
+  ASSERT_TRUE(writable_file->GetFileSize() == kGood.size());
+    }
+    
+    namespace rocksdb {
+    }
+    
+    #ifndef JAVA_ROCKSJNI_STATISTICSJNI_H_
+#define JAVA_ROCKSJNI_STATISTICSJNI_H_
+    
+    namespace rocksdb {
+    }
+    
+    #include <assert.h>
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+    
+    // Update the sequence number in the internal key.
+// Guarantees not to invalidate ikey.data().
+inline void UpdateInternalKey(std::string* ikey, uint64_t seq, ValueType t) {
+  size_t ikey_sz = ikey->size();
+  assert(ikey_sz >= 8);
+  uint64_t newval = (seq << 8) | t;
+    }
