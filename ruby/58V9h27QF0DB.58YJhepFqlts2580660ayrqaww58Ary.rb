c2cb8c16410947ff459df8d574b9d2e2e9c5831f
SUNsung <weatherius@gmail.com>
2018-04-22 23:58:24 +0300

    Rewrite [58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb] File in: 2018-04-22 23:58:24

--- ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
+++ ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
@@ -1,114 +1,99 @@
 
         
-            s = nil
-    homebrew_site_packages = Language::Python.homebrew_site_packages
-    user_site_packages = Language::Python.user_site_packages 'python'
-    pth_file = user_site_packages/'homebrew.pth'
-    instructions = <<-EOS.undent.gsub(/^/, '  ')
-      mkdir -p #{user_site_packages}
-      echo 'import site; site.addsitedir('#{homebrew_site_packages}')' >> #{pth_file}
-    EOS
+          def test_escape_javascript
+    assert_equal '', escape_javascript(nil)
+    assert_equal %(This \\'thing\\' is really\\n netos\\'), escape_javascript(%(This 'thing' is really\n netos'))
+    assert_equal %(backslash\\\\test), escape_javascript(%(backslash\\test))
+    assert_equal %(dont <\\/close> tags), escape_javascript(%(dont </close> tags))
+    assert_equal %(unicode &#x2028; newline), escape_javascript(%(unicode \342\200\250 newline).dup.force_encoding(Encoding::UTF_8).encode!)
+    assert_equal %(unicode &#x2029; newline), escape_javascript(%(unicode \342\200\251 newline).dup.force_encoding(Encoding::UTF_8).encode!)
     
-        def self.cleanup_logs
-      return unless HOMEBREW_LOGS.directory?
-      HOMEBREW_LOGS.subdirs.each do |dir|
-        cleanup_path(dir) { dir.rmtree } if prune?(dir, :days_default => 14)
-      end
-    end
+          include ActionDispatch::Assertions, ActionDispatch::TestProcess
+      include Rails::Dom::Testing::Assertions
+      include ActionController::TemplateAssertions
+      include ActionView::Context
     
-        def merge(hash)
-      return super unless hash.is_a? Hash
-      dup.merge!(hash)
-    end
-    
-              # Underscore methods
-          if name.start_with?('Underscore')
-            node.at_css('~ ul').css('li').each do |li|
-              name = [type.downcase, li.at_css('a').content.split.first].join('.')
-              id = name.parameterize
-              li['id'] = id
-              entries << [name, id, type]
-            end
-            next
-          end
-    
-            css('.property-detail').each do |node|
-          node.at_css('.property-name')['id'] = node.at_css('a')['id']
-        end
-    
-        s = Set.new(ary) { |o| o * 2 }
-    assert_equal([2,4,6], s.sort)
-  end
-    
-      it 'decodes negative Infinity' do
-    '\xff\x80\x00\x00'.unpack(unpack_format).should == [-infinity_value]
-  end
-    
-      class Apple < Struct; end
-    
-      def self.completed_thread
-    Thread.new {}
-  end
-    
-        # margin: a -b
-    # LESS: sets 2 values
-    # Sass: sets 1 value (a-b)
-    # This wraps a and -b so they evaluates to 2 values in Sass
-    def replace_calculation_semantics(file)
-      # split_prop_val.call('(@navbar-padding-vertical / 2) -@navbar-padding-horizontal')
-      # #=> ['(navbar-padding-vertical / 2)', '-navbar-padding-horizontal']
-      split_prop_val = proc { |val|
-        s         = CharStringScanner.new(val)
-        r         = []
-        buff      = ''
-        d         = 0
-        prop_char = %r([\$\w\-/\*\+%!])
-        while (token = s.scan_next(/([\)\(]|\s+|#{prop_char}+)/))
-          buff << token
-          case token
-            when '('
-              d += 1
-            when ')'
-              d -= 1
-              if d == 0
-                r << buff
-                buff = ''
-              end
-            when /\s/
-              if d == 0 && !buff.strip.empty?
-                r << buff
-                buff = ''
-              end
-          end
-        end
-        r << buff unless buff.empty?
-        r.map(&:strip)
-      }
-    
-        def log_http_get_files(files, from, cached = false)
-      return if files.empty?
-      s = '  #{'CACHED ' if cached}GET #{files.length} files from #{from} #{files * ' '}...'
-      if cached
-        puts dark green s
-      else
-        puts dark cyan s
-      end
-    end
-    
-            def rejected_types_rejected?
-          @missing_rejected_types ||= @rejected_types.select { |type| type_allowed?(type) }
-          @missing_rejected_types.none?
-        end
+            adapter = adapter.camelize
+        adapter = 'PostgreSQL' if adapter == 'Postgresql'
+        'ActionCable::SubscriptionAdapter::#{adapter}'.constantize
       end
     end
   end
 end
 
     
-    module Paperclip
-  # Provides helper methods that can be used in migrations.
-  module Schema
-    COLUMNS = {:file_name    => :string,
-               :content_type => :string,
-               :file_size    => :integer,
-               :updated_at   => :datetime}
+    class TestResponseTest < ActiveSupport::TestCase
+  def assert_response_code_range(range, predicate)
+    response = ActionDispatch::TestResponse.new
+    (0..599).each do |status|
+      response.status = status
+      assert_equal range.include?(status), response.send(predicate),
+                   'ActionDispatch::TestResponse.new(#{status}).#{predicate}'
+    end
+  end
+    
+      class B < A
+    helper { def shout; 'B' end }
+    
+          assert_equal 'entry', @controller.response.body
+      assert @controller.params.has_key?(:entry)
+      assert_equal 'content...', @controller.params['entry']['summary']
+    end
+  end
+    
+    class AssertSelectEmailTest < ActionMailer::TestCase
+  class AssertSelectMailer < ActionMailer::Base
+    def test(html)
+      mail body: html, content_type: 'text/html',
+        subject: 'Test e-mail', from: 'test@test.host', to: 'test <test@test.host>'
+    end
+  end
+    
+            # This returns all the config classes for the various provisioners.
+        #
+        # @return [Registry]
+        def provisioner_configs
+          Registry.new.tap do |result|
+            @registered.each do |plugin|
+              result.merge!(plugin.components.configs[:provisioner])
+            end
+          end
+        end
+    
+          class << self
+        # Mark a given block of code as a 'busy' block of code, which will
+        # register a SIGINT handler for the duration of the block. When a
+        # SIGINT occurs, the `sig_callback` proc will be called. It is up
+        # to the callback to behave properly and exit the application.
+        def busy(sig_callback)
+          register(sig_callback)
+          return yield
+        ensure
+          unregister(sig_callback)
+        end
+    
+          protected
+    
+                # Break out if we're supposed to. Otherwise re-raise the error
+            # because it is a real problem.
+            break if breakable
+            raise
+          end
+        end
+    
+              if argv.length == 2
+            # @deprecated
+            @env.ui.warn('WARNING: The second argument to `vagrant box remove`')
+            @env.ui.warn('is deprecated. Please use the --provider flag. This')
+            @env.ui.warn('feature will stop working in the next version.')
+            options[:provider] = argv[1]
+          end
+    
+    module VagrantPlugins
+  module CommandBox
+    module Command
+      class Repackage < Vagrant.plugin('2', :command)
+        def execute
+          opts = OptionParser.new do |o|
+            o.banner = 'Usage: vagrant box repackage <name> <provider> <version>'
+          end
