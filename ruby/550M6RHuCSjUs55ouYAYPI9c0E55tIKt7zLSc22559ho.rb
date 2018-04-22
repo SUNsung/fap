cf4c3a4bae9c7e469309d50959485f5d59fe09d9
SUNsung <weatherius@gmail.com>
2018-04-22 17:13:22 +0300

    Rewrite [550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb] File in: 2018-04-22 17:13:22

--- ruby/550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb
+++ ruby/550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb
@@ -1,105 +1,82 @@
 
         
-            def defaults_deprecate_type(old, current)
-      Jekyll.logger.warn 'Defaults:', 'The '#{old}' type has become '#{current}'.'
-      Jekyll.logger.warn 'Defaults:', 'Please update your front-matter defaults to use \
-                        'type: #{current}'.'
+            projects << @user.personal_projects.visible_to_user(current_user) if current_user
+    projects << @user.personal_projects.public_to_user(current_user)
+    
+        if registration
+      u2f.authenticate!(challenges, response, Base64.decode64(registration.public_key), registration.counter)
+      registration.update(counter: response.counter)
+      true
+    end
+  rescue JSON::ParserError, NoMethodError, ArgumentError, U2F::Error
+    false
+  end
+end
+
+    
+          # Skips sending the confirmation/reconfirmation notification email after_create/after_update. Unlike
+      # #skip_confirmation!, record still requires confirmation.
+      def skip_confirmation_notification!
+        @skip_confirmation_notification = true
+      end
+    
+    class Devise::PasswordsController < DeviseController
+  prepend_before_action :require_no_authentication
+  # Render the #edit only if coming from a reset password email link
+  append_before_action :assert_reset_token_passed, only: :edit
+    
+        def reset_password_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :reset_password_instructions, opts)
+    end
+    
+          # Set up a subject doing an I18n lookup. At first, it attempts to set a subject
+      # based on the current mapping:
+      #
+      #   en:
+      #     devise:
+      #       mailer:
+      #         confirmation_instructions:
+      #           user_subject: '...'
+      #
+      # If one does not exist, it fallbacks to ActionMailer default:
+      #
+      #   en:
+      #     devise:
+      #       mailer:
+      #         confirmation_instructions:
+      #           subject: '...'
+      #
+      def subject_for(key)
+        I18n.t(:'#{devise_mapping.name}_subject', scope: [:devise, :mailer, key],
+          default: [:subject, key.to_s.humanize])
+      end
     end
   end
 end
 
     
-            parsed_expr = parse_expression(expression)
-        @context.stack do
-          groups = input.group_by do |item|
-            @context[variable] = item
-            parsed_expr.render(@context)
-          end
-          grouped_array(groups)
-        end
-      end
+    (allow file-ioctl)
+(allow sysctl-read)
+(allow mach-lookup)
+(allow ipc-posix-shm)
+(allow process-fork)
+(allow system-socket)
     
-          if site.theme && site.theme.layouts_path.eql?(base)
-        @base_dir = site.theme.root
-        @path = site.in_theme_dir(base, name)
-      else
-        @base_dir = site.source
-        @path = site.in_source_dir(base, name)
-      end
-      @relative_path = @path.sub(@base_dir, '')
+            self.summary = 'The repl listens to commands on standard input'
+        self.description = <<-DESC
+          The repl listens to commands on standard input and prints their
+          result to standard output.
+          It accepts all the other ipc subcommands. The repl will signal the
+          end of output with the the ASCII CR+LF `\\n\\r`.
+        DESC
     
+            private
     
-# extract label addresses
-addrs = {}
-dtrans.each_line { |ln|
-	if ln =~ /;[^ ].*:/
-		parts = ln.split(' ')
-		label = parts[1]
-		label = label.slice(1,label.index(':')-1)
-		addr = parts[0].split(':')[1].to_i(16)
-		#puts '%s => %x' % [label, addr]
-		one = { label => addr }
-		addrs.merge!(one)
-	end
-}
-#puts addrs.inspect
-    
-    
-
-    
-        # Returns a representation of the query as an array of strings and
-    # potentially {Sass::Script::Tree::Node}s (if there's interpolation in it).
-    # When the interpolation is resolved and the strings are joined together,
-    # this will be the string representation of this query.
-    #
-    # @return [Array<String, Sass::Script::Tree::Node>]
-    def to_a
-      Sass::Util.intersperse(queries.map {|q| q.to_a}, ', ').flatten
-    end
-    
-        # True if the dimensions represent a vertical rectangle
-    def vertical?
-      height > width
-    end
-    
-        # Returns the extension of the file. e.g. 'jpg' for 'file.jpg'
-    # If the style has a format defined, it will return the format instead
-    # of the actual extension.
-    def extension attachment, style_name
-      ((style = attachment.styles[style_name.to_s.to_sym]) && style[:format]) ||
-        File.extname(attachment.original_filename).sub(/\A\.+/, ''.freeze)
-    end
-    
-            protected
-    
-        def clear_processors!
-      @known_processors.try(:clear)
-    end
-    
-          if app.config.respond_to?(:paperclip_defaults)
-        Paperclip::Attachment.default_options.merge!(app.config.paperclip_defaults)
-      end
-    end
-    
-            attachment_names.each do |attachment_name|
-          COLUMNS.keys.each do |column_name|
-            remove_column(table_name, '#{attachment_name}_#{column_name}')
+          def update_if_necessary!
+        if @update && config.verbose?
+          UI.section('\nUpdating Spec Repositories\n'.yellow) do
+            Repo.new(ARGV.new(['update'])).run
           end
         end
       end
-    
-              def find_plugins_gem_specs
-            @specs ||= ::Gem::Specification.find_all.select{|spec| logstash_plugin_gem_spec?(spec)}
-          end
-    
-      def setup_multi_encode!
-    @has_encode_sync = self.methods.include?(:encode_sync)
-    
-      def seventy_five_percent
-    [1, (maximum * 0.75)].max.floor
-  end
-    
-      module Branch2
-    def if
-      elements[0]
-    end
