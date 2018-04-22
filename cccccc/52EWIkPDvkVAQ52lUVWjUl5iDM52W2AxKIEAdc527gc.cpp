9a1d70e7ed8200ceef17e59cce25f3fb22184bd5
SUNsung <weatherius@gmail.com>
2018-04-22 14:52:51 +0300

    Rewrite [52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp] File in: 2018-04-22 14:52:51

--- cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
+++ cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
@@ -1,254 +1,276 @@
 
         
-        #endif  // ATOM_BROWSER_API_ATOM_API_DESKTOP_CAPTURER_H_
+          DebuggerClient(ASTContext &C) : Ctx(C) { }
+  virtual ~DebuggerClient() = default;
+  
+  // DebuggerClient is consulted at the beginning of the parsing
+  // of various DeclKinds to see whether the decl should be parsed
+  // in the global context rather than the current context.
+  // This question will only be asked if the decl's current context
+  // is a function marked with the LLDBDebuggerFunction attribute.
+  virtual bool shouldGlobalize(Identifier Name, DeclKind kind) = 0;
+  
+  virtual void didGlobalize (Decl *Decl) = 0;
+    
+    
+    {} // namespace swift
+    
+    class HTML final : public MarkupASTNode {
+  StringRef LiteralContent;
+  HTML(StringRef LiteralContent)
+      : MarkupASTNode(ASTNodeKind::HTML),
+        LiteralContent(LiteralContent) {}
+public:
+  static HTML *create(MarkupContext &MC, StringRef LiteralContent);
+  StringRef getLiteralContent() const { return LiteralContent; };
+    }
+    
+      llvm::SMDiagnostic GetMessage(SourceLoc Loc, llvm::SourceMgr::DiagKind Kind,
+                                const Twine &Msg,
+                                ArrayRef<llvm::SMRange> Ranges,
+                                ArrayRef<llvm::SMFixIt> FixIts) const;
+    
+    #ifndef SWIFT_SYNTAX_REFERENCES_H
+#define SWIFT_SYNTAX_REFERENCES_H
+    
+    #ifndef SWIFT_AST_SUBSTITUTION_H
+#define SWIFT_AST_SUBSTITUTION_H
+    
+    void convert_dataset(const string& input_folder, const string& output_folder,
+    const string& db_type) {
+  scoped_ptr<db::DB> train_db(db::GetDB(db_type));
+  train_db->Open(output_folder + '/cifar10_train_' + db_type, db::NEW);
+  scoped_ptr<db::Transaction> txn(train_db->NewTransaction());
+  // Data buffer
+  int label;
+  char str_buffer[kCIFARImageNBytes];
+  Datum datum;
+  datum.set_channels(3);
+  datum.set_height(kCIFARSize);
+  datum.set_width(kCIFARSize);
+    }
+    
+    #endif  // CAFFE_CUDNN_CONV_LAYER_HPP_
 
     
-    AsarProtocolHandler::AsarProtocolHandler(
-    const scoped_refptr<base::TaskRunner>& file_task_runner)
-    : file_task_runner_(file_task_runner) {}
+      virtual inline const char* type() const { return 'Exp'; }
     
-    #include 'net/url_request/url_request_job_factory.h'
-    
-    RenderProcessPreferences::~RenderProcessPreferences() {
-}
-    
-      // We need to convert the |entries_| to ListValue for multiple times, this
-  // caches is only updated when we are sending messages.
-  bool cache_needs_update_;
-  base::ListValue cached_entries_;
-    
-    // Parse a string as an accelerator.
-bool StringToAccelerator(const std::string& description,
-                         ui::Accelerator* accelerator);
-    
-    
-    {}  // namespace atom
-    
-        UINT width = 640;
-    UINT height = 480;
-    
-            // clear for new data
-        C_est_.clear();
-        t_est_.clear();
-        cost_.clear();
-    
-            // cross two lines
-        Point2f origin1 = quad_corners[0];
-        Point2f dir1 = quad_corners[1] - quad_corners[0];
-        Point2f origin2 = quad_corners[2];
-        Point2f dir2 = quad_corners[3] - quad_corners[2];
-        double angle = acos(dir1.dot(dir2)/(norm(dir1)*norm(dir2)));
-        if(cvIsNaN(angle) || cvIsInf(angle) || angle < 0.5 || angle > CV_PI - 0.5) continue;
-    
-      // Instead of 1/0, we want to see true/false for bool values.
-  Message& operator <<(bool b) {
-    return *this << (b ? 'true' : 'false');
-  }
-    
-    // Now the tricky part: you need to register all test patterns before
-// you can instantiate them.  The first argument of the macro is the
-// test case name; the rest are the names of the tests in this test
-// case.
-REGISTER_TYPED_TEST_CASE_P(FooTest,
-                           DoesBlah, HasPropertyA);
-    
-    // Returns the type ID of ::testing::Test.  Always call this instead
-// of GetTypeId< ::testing::Test>() to get the type ID of
-// ::testing::Test, as the latter may give the wrong result due to a
-// suspected linker bug when compiling Google Test as a Mac OS X
-// framework.
-GTEST_API_ TypeId GetTestTypeId();
-    
-      // Smart pointer members.
-  void reset(T* ptr = NULL) {
-    depart();
-    capture(ptr);
-  }
-  T* get() const { return value_; }
-  T* operator->() const { return value_; }
-  T& operator*() const { return *value_; }
-    
-      // Creates a UTF-16 wide string from the given ANSI string, allocating
-  // memory using new. The caller is responsible for deleting the return
-  // value using delete[]. Returns the wide string, or NULL if the
-  // input is NULL.
-  //
-  // The wide string is created using the ANSI codepage (CP_ACP) to
-  // match the behaviour of the ANSI versions of Win32 calls and the
-  // C runtime.
-  static LPCWSTR AnsiToUtf16(const char* c_str);
-    
-    $for i [[
-$range j 1..i
-$range k 2..i
-template <$for j, [[GTEST_TEMPLATE_ T$j]]>
-struct Templates$i {
-  typedef TemplateSel<T1> Head;
-  typedef Templates$(i-1)<$for k, [[T$k]]> Tail;
-};
-    
-    void run_all_tests(std::shared_ptr<thd::DataChannel> data_channel, int workers) {
-  test_send_recv_tensor(data_channel);
-  test_send_recv_tensor_any_source(data_channel, workers);
-  test_send_recv_scalar(data_channel);
-  test_broadcast(data_channel);
-  test_reduce(data_channel, workers);
-  test_allReduce(data_channel, workers);
-  test_scatter(data_channel);
-  test_gather(data_channel);
-  test_allGather(data_channel);
-  test_barrier(data_channel);
-  test_isend(data_channel);
-  test_irecv(data_channel);
-  test_interlaces(data_channel);
+    namespace HPHP {
+/////////////////////////////////////////////////////////////////////////////
     }
     
-    template<> AT_API Half convert(int64_t f) {
-  return convert<Half,double>(static_cast<double>(f));
-}
-template<> AT_API int64_t convert(Half f) {
-  return static_cast<int64_t>(convert<double,Half>(f));
+    void Timer::RequestExit() {
+  Dump();
 }
     
-      explicit Scalar(const detail::TensorBase & t)
-  : tag(Tag::HAS_t), t(t) {
-    AT_ASSERT(t.defined(), 'Attempting to create a Scalar from an undefined tensor');
-    AT_ASSERT(t.dim() == 0, 'Attempting to create a Scalar from a %d dim tensor', t.dim());
+    int gettime(clockid_t, struct timespec*);
+int64_t gettime_ns(clockid_t);
+    
+    Vtuple Vunit::makeTuple(VregList&& regs) {
+  auto i = tuples.size();
+  tuples.emplace_back(std::move(regs));
+  return Vtuple{i};
+}
+    
+      if (data.isArray()
+      || data.isResource()
+      || (data.isString() && data.toString().empty())
+      || (data.isObject() && !data.toObject().instanceof(s_GMP_GMP))
+      || !variantToGMPData('gmp_intval', gmpData, data)) {
+    return data.toInt64();
   }
     
-    #include 'ATen/Config.h'
-namespace at {
-namespace native {
-    }
-    }
     
-    struct curandStateMtgp32* THCRandom_generatorStates(struct THCState* state)
-{
-  return THCRandom_getGenerator(state)->gen_states;
+    {  return env.irb->makeBlock(sk, curProfCount(env));
 }
-    
-    #ifdef WITH_CUDA
-    
-    bool js_cocos2dx_builder_CCBReader_constructor(JSContext *cx, uint32_t argc, jsval *vp);
-void js_cocos2dx_builder_CCBReader_finalize(JSContext *cx, JSObject *obj);
-void js_register_cocos2dx_builder_CCBReader(JSContext *cx, JS::HandleObject global);
-void register_all_cocos2dx_builder(JSContext* cx, JS::HandleObject obj);
-bool js_cocos2dx_builder_CCBReader_getAnimationManager(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_setAnimationManager(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_addOwnerOutletName(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_getOwnerCallbackNames(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_addDocumentCallbackControlEvents(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_setCCBRootPath(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_addOwnerOutletNode(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_getOwnerCallbackNodes(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_readSoundKeyframesForSeq(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_getCCBRootPath(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_getOwnerCallbackControlEvents(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_getOwnerOutletNodes(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_readUTF8(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_addOwnerCallbackControlEvents(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_getOwnerOutletNames(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_readCallbackKeyframesForSeq(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_getAnimationManagersForNodes(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_getNodesWithAnimationManagers(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_setResolutionScale(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_CCBReader(JSContext *cx, uint32_t argc, jsval *vp);
-    
-    bool js_cocos2dx_studio_ComController_constructor(JSContext *cx, uint32_t argc, jsval *vp);
-void js_cocos2dx_studio_ComController_finalize(JSContext *cx, JSObject *obj);
-void js_register_cocos2dx_studio_ComController(JSContext *cx, JS::HandleObject global);
-void register_all_cocos2dx_studio(JSContext* cx, JS::HandleObject obj);
-bool js_cocos2dx_studio_ComController_create(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_ComController_ComController(JSContext *cx, uint32_t argc, jsval *vp);
-    
-    
-    
-    
-    
-    
-    
-    #ifdef __cplusplus
-extern 'C' {
-#endif
-#include 'tolua++.h'
-#ifdef __cplusplus
-}
-#endif
-    
-    
-    {    CHECK_GL_ERROR_DEBUG();
-}
-
     
     
     {
-    {			float minX = -6.0f;
-			float maxX = 0.0f;
-			float minY = 4.0f;
-			float maxY = 6.0f;
-			
-			for (int32 i = 0; i < 400; ++i)
-			{
-				b2BodyDef bd;
-				bd.type = b2_dynamicBody;
-				bd.position = b2Vec2(RandomFloat(minX,maxX),RandomFloat(minY,maxY));
-				b2Body* body = m_world->CreateBody(&bd);
-				body->CreateFixture(&shape, 0.01f);
+    {} } // HPHP::jit
+
+    
+    void orderFuncs(const TargetGraph& cg, Cluster* c1, Cluster* c2) {
+  TargetId c1head = c1->targets[0];
+  TargetId c1tail = c1->targets[c1->targets.size() - 1];
+  TargetId c2head = c2->targets[0];
+  TargetId c2tail = c2->targets[c2->targets.size() - 1];
+    }
+    
+    
+Instruction* Instruction::ImmPCOffsetTarget(Instruction* from) {
+  ptrdiff_t offset;
+  if (IsPCRelAddressing()) {
+    // PC-relative addressing. Only ADR is supported.
+    offset = ImmPCRel();
+  } else if (IsLoadOrStore()) {
+    offset = ImmLLiteral() << kInstructionSizeLog2;
+  } else {
+    // All PC-relative branches.
+    assert(BranchType() != UnknownBranchType);
+    // Relative branch offsets are instruction-size-aligned.
+    offset = ImmBranch() << kInstructionSizeLog2;
+  }
+  return (!from ? this : from) + offset;
+}
+    
+    
+    {
+    {			if (szArgList[i + 1] != NULL && szArgList[i + 1][0] != '/') {
+				cmderOptions.cmderCfgRoot = szArgList[i + 1];
+				i++;
 			}
 		}
-		
+		else if (_wcsicmp(L'/start', szArgList[i]) == 0)
 		{
-			b2PolygonShape shape;
-			shape.SetAsBox(1.5f, 1.5f);
-			b2BodyDef bd;
-			bd.type = b2_dynamicBody;
-			bd.position.Set(-40.0f,5.0f);
-			bd.bullet = true;
-			b2Body* body = m_world->CreateBody(&bd);
-			body->CreateFixture(&shape, 1.0f);
-			body->SetLinearVelocity(b2Vec2(150.0f, 0.0f));
+			if (PathFileExists(szArgList[i + 1]))
+			{
+				cmderOptions.cmderStart = szArgList[i + 1];
+				i++;
+			}
+			else {
+				MessageBox(NULL, szArgList[i + 1], L'/START - Folder doses not exist!', MB_OK);
+			}
+		}
+		else if (_wcsicmp(L'/task', szArgList[i]) == 0)
+		{
+			cmderOptions.cmderTask = szArgList[i + 1];
+			i++;
+		}
+		else if (_wcsicmp(L'/single', szArgList[i]) == 0)
+		{
+			cmderOptions.cmderSingle = true;
+		}
+		else if (_wcsicmp(L'/register', szArgList[i]) == 0)
+		{
+			cmderOptions.registerApp = true;
+			cmderOptions.unRegisterApp = false;
+			if (szArgList[i + 1] != NULL)
+			{
+				if (_wcsicmp(L'all', szArgList[i + 1]) == 0 || _wcsicmp(L'user', szArgList[i + 1]) == 0)
+				{
+					cmderOptions.cmderRegScope = szArgList[i + 1];
+					i++;
+				}
+			}
+		}
+		else if (_wcsicmp(L'/unregister', szArgList[i]) == 0)
+		{
+			cmderOptions.unRegisterApp = true;
+			cmderOptions.registerApp = false;
+			if (szArgList[i + 1] != NULL)
+			{
+				if (_wcsicmp(L'all', szArgList[i + 1]) == 0 || _wcsicmp(L'user', szArgList[i + 1]) == 0)
+				{
+					cmderOptions.cmderRegScope = szArgList[i + 1];
+					i++;
+				}
+			}
+		}
+		else if (cmderOptions.cmderStart == L'' && PathFileExists(szArgList[i]))
+		{
+			cmderOptions.cmderStart = szArgList[i];
+		}
+		else {
+			MessageBox(NULL, L'Unrecognized parameter.\n\nValid options:\n\n    /c [CMDER User Root Path]\n\n    /task [ConEmu Task Name]\n\n    [/start [Start in Path] | [Start in Path]]\n\n    /single\n\nor\n\n    /register [USER | ALL]\n\nor\n\n    /unregister [USER | ALL]\n', MB_TITLE, MB_OK);
+			cmderOptions.error = true;
 		}
 	}
     
-    			b2FixtureDef sd2;
-			sd2.shape = &poly2;
-			sd2.density = 2.0f;
+    #include <boost/algorithm/string/trim.hpp>
+#include <boost/lexical_cast.hpp>
     
-    			b2FixtureDef fd;
-			fd.shape = &shape;
-			fd.density = 1.0f;
+     private:
+  /*
+   * @brief When `get`ing event results, return EventID%s from time indexes.
+   *
+   * Used by EventSubscriber::get to retrieve EventID, EventTime indexes. This
+   * applies the lookup-efficiency checks for time list appropriate bins.
+   * If the time range in 24 hours and there is a 24-hour list bin it will
+   * be queried using a single backing store `Get` followed by two `Get`s of
+   * the most-specific boundary lists.
+   *
+   * @param index the set of index to scan.
+   * @param optimize if true apply optimization checks.
+   *
+   * @return List of EventID, EventTime%s
+   */
+  std::vector<EventRecord> getRecords(const std::vector<std::string>& indexes,
+                                      bool optimize = true);
     
-    		m_x = RandomFloat(-1.0f, 1.0f);
-		m_bullet->SetTransform(b2Vec2(m_x, 10.0f), 0.0f);
-		m_bullet->SetLinearVelocity(b2Vec2(0.0f, -50.0f));
-		m_bullet->SetAngularVelocity(0.0f);
+    /// Start an ExtensionWatcher thread.
+Status startExtensionWatcher(const std::string& manager_path,
+                             size_t interval,
+                             bool fatal);
     
     
-    {
+    {  /// Mutex to protect use of the shutdown callable.
+  static RecursiveMutex shutdown_mutex_;
+};
+    
+    
     {
     { private:
-  /*! \brief input stream */
-  dmlc::Stream *strm_;
-  /*! \brief current buffer pointer */
-  size_t buffer_ptr_;
-  /*! \brief internal buffer */
-  std::string buffer_;
+  friend class TLSConfigTests;
 };
-}  // namespace common
-}  // namespace xgboost
-#endif  // XGBOOST_COMMON_IO_H_
+}
 
     
-      XGBOOST_DEVICE bst_gpair_internal<T> operator+(
-      const bst_gpair_internal<T> &rhs) const {
-    bst_gpair_internal<T> g;
-    g.grad_ = grad_ + rhs.grad_;
-    g.hess_ = hess_ + rhs.hess_;
-    return g;
+    
+    {  // Support a signed 64, a double, and treat everything else as a signed int.
+  if (type == kCFNumberSInt64Type) {
+    long long int value;
+    if (CFNumberGetValue((CFNumberRef)cf_number, type, &value)) {
+      return boost::lexical_cast<std::string>(value);
+    }
+  } else if (type == kCFNumberDoubleType) {
+    double value;
+    if (CFNumberGetValue((CFNumberRef)cf_number, type, &value)) {
+      return boost::lexical_cast<std::string>(value);
+    }
+  } else {
+    unsigned int value;
+    if (CFNumberGetValue((CFNumberRef)cf_number, type, &value)) {
+      return boost::lexical_cast<std::string>(value);
+    }
   }
+  // Cast as a string.
+  return '0';
+}
+    
+    std::shared_ptr<PlatformProcess> PlatformProcess::launchTestPythonScript(
+    const std::string& args) {
+  std::string osquery_path;
+  auto osquery_path_option = getEnvVar('OSQUERY_DEPS');
+  if (osquery_path_option) {
+    osquery_path = *osquery_path_option;
+  } else {
+    if (!isPlatform(PlatformType::TYPE_FREEBSD)) {
+      osquery_path = '/usr/local/osquery';
+    } else {
+      osquery_path = '/usr/local';
+    }
+  }
+    }
     
     
-    { private:
-  /*! \brief external memory column offset */
-  std::vector<size_t> disk_offset_;
-};
+    {
+    {    struct addrinfo* res = nullptr;
+    if (getaddrinfo(fqdn_string.c_str(), nullptr, &hints, &res) == 0) {
+      if (res->ai_canonname != nullptr) {
+        fqdn_string = res->ai_canonname;
+      }
+    }
+    if (res != nullptr) {
+      freeaddrinfo(res);
+    }
+#endif
+    return fqdn_string;
+  } else {
+    unsigned long size = 256;
+    std::vector<char> fqdn(size, 0x0);
+#ifdef WIN32
+    GetComputerNameEx(ComputerNameDnsFullyQualified, fqdn.data(), &size);
+#endif
+    return fqdn.data();
+  }
+}
