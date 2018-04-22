794f27bd4f2fb91d5646fdfba86a593f2da57942
SUNsung <weatherius@gmail.com>
2018-04-23 02:35:17 +0300

    Rewrite [52.bqbKMhvfOM52N7aY8IXUfb652o3BctdN94k526.0.cpp] File in: 2018-04-23 02:35:16

--- cccccc/52.bqbKMhvfOM52N7aY8IXUfb652o3BctdN94k526.0.cpp
+++ cccccc/52.bqbKMhvfOM52N7aY8IXUfb652o3BctdN94k526.0.cpp
@@ -1,106 +1,229 @@
 
         
-          static void ClearCache(content::RenderProcessHost* render_view_host);
-  static void SetProxyConfig(content::RenderProcessHost* render_process_host,
-                             const std::string& proxy_config);
+          // Abort and close all existing sessions, disconnecting their resources from
+  // future sessions.
+  //
+  // Reset() allows misbehaving or slow sessions to be aborted and closed, and
+  // causes their resources eventually to be released.  Reset() does not wait
+  // for the computations in old sessions to cease; it merely starts the
+  // process of tearing them down.  However, if a new session is started after
+  // a Reset(), the new session is isolated from changes that old sessions
+  // (started prior to the Reset()) may continue to make to resources, provided
+  // all those resources are in containers listed in 'containers'.
+  //
+  // Old sessions may continue to have side-effects on resources not in
+  // containers listed in 'containers', and thus may affect future
+  // sessions' results in ways that are hard to predict.  Thus, if well-defined
+  // behavior is desired, is it recommended that all containers be listed in
+  // 'containers'.
+  //
+  // If the 'containers' vector is empty, the default container is assumed.
+  // If the 'containers' vector is non-empty, the default container should be
+  // listed explicitly.
+  //
+  // Sessions that support resource containers should override this function.
+  virtual Status Reset(const SessionOptions& options,
+                       const std::vector<string>& containers) {
+    return errors::Unimplemented('Reset()');
+  }
     
-    #ifndef CONTENT_NW_SRC_API_BASE_BASE_H_
-#define CONTENT_NW_SRC_API_BASE_BASE_H_
+    // Prune a model to make it more efficient:
+// * Remove unnecessary operations.
+// * Optimize gradient computations.
+class ModelPruner : public GraphOptimizer {
+ public:
+  ModelPruner() {}
+  ~ModelPruner() override {}
+    }
     
-      int object_id = args[0]->Int32Value();
-  std::string name = *v8::String::Utf8Value(args[1]);
+    #ifndef TENSORFLOW_LIB_IO_RECORD_WRITER_H_
+#define TENSORFLOW_LIB_IO_RECORD_WRITER_H_
     
-    #endif  // CONTENT_NW_SRC_API_DISPATCHER_BINDINGS_H_
+    #if !TENSORFLOW_USE_SYCL
+#error This file must only be included when building TensorFlow with SYCL support
+#endif
+    
+     private:
+  // a length value of kFullExtent (-1) means we have a full slice at this
+  // dimension. It's defined in tensor_slice.cc.
+  static const int64 kFullExtent;
+    
+    // Comparison ops.
+REGISTER_OP_NO_GRADIENT('Less');
+REGISTER_OP_NO_GRADIENT('LessEqual');
+REGISTER_OP_NO_GRADIENT('Greater');
+REGISTER_OP_NO_GRADIENT('GreaterEqual');
+REGISTER_OP_NO_GRADIENT('Equal');
+REGISTER_OP_NO_GRADIENT('NotEqual');
+    
+    #endif // BITCOIN_INDIRECTMAP_H
 
     
-    class DispatcherHost : public content::WebContentsObserver {
- public:
-  explicit DispatcherHost(content::RenderViewHost* render_view_host);
-  ~DispatcherHost() final;
-    }
-    
-    
-    {}
-    
-    // Popup menus may get squished if they open up too close to the bottom of the
-// screen. This function takes the size of the screen, the size of the menu,
-// an optional widget, the Y position of the mouse click, and adjusts the popup
-// menu's Y position to make it fit if it's possible to do so.
-// Returns the new Y position of the popup menu.
-int CalculateMenuYPosition(const GdkRectangle* screen_rect,
-                           const GtkRequisition* menu_req,
-                           GtkWidget* widget, const int y) {
-  CHECK(screen_rect);
-  CHECK(menu_req);
-  // If the menu would run off the bottom of the screen, and there is enough
-  // screen space upwards to accommodate the menu, then pop upwards. If there
-  // is a widget, then also move the anchor point to the top of the widget
-  // rather than the bottom.
-  const int screen_top = screen_rect->y;
-  const int screen_bottom = screen_rect->y + screen_rect->height;
-  const int menu_bottom = y + menu_req->height;
-  int alternate_y = y - menu_req->height;
-  if (widget) {
-    GtkAllocation allocation;
-    gtk_widget_get_allocation(widget, &allocation);
-    alternate_y -= allocation.height;
-  }
-  if (menu_bottom >= screen_bottom && alternate_y >= screen_top)
-    return alternate_y;
-  return y;
-}
-    
-    void MenuItem::SetChecked(bool checked) {
-  // Set active will cause 'activate' to be emitted, so block here
-  block_active_ = true;
-  gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(menu_item_), checked);
-  block_active_ = false;
-}
-    
-    
-    {  if (enable_shortcut_) {
-    focus_manager_->UnregisterAccelerator(accelerator_, this);
-  }
-  if (submenu_) {
-    submenu_->RemoveKeys();
-  }
-  focus_manager_ = NULL;
-}
-    
-    namespace HPHP {
-    }
-    
-    
-    {    insertOrInc(s, d, arc.weight);
-  }
-    
-    
-    {}
-    
-    namespace HPHP {
-/////////////////////////////////////////////////////////////////////////////
-    }
-    
-        for (std::size_t i = 1; i < data_channel->getNumProcesses(); ++i) {
-      auto int_tensor = buildTensor<int>({1, 2, 3, 4, 5}, 20);
-      data_channel->send(*int_tensor, i);
-    }
+    LookupKey::LookupKey(const Slice& user_key, SequenceNumber s) {
+  size_t usize = user_key.size();
+  size_t needed = usize + 13;  // A conservative estimate
+  char* dst;
+  if (needed <= sizeof(space_)) {
+    dst = space_;
   } else {
-    auto int_tensor1 = buildTensor<int>({1, 2, 3, 4, 5}, -1);
-    auto request = std::shared_ptr<thd::DataChannel::Request>(
-      data_channel->ireceive(*int_tensor1, 0)
-    );
+    dst = new char[needed];
+  }
+  start_ = dst;
+  dst = EncodeVarint32(dst, usize + 8);
+  kstart_ = dst;
+  memcpy(dst, user_key.data(), usize);
+  dst += usize;
+  EncodeFixed64(dst, PackSequenceAndType(s, kValueTypeForSeek));
+  dst += 8;
+  end_ = dst;
+}
     
-    template<> AT_API Half convert(float f);
-template<> AT_API float convert(Half f);
-template<> AT_API Half convert(double f);
-template<> AT_API double convert(Half f);
-template<> AT_API Half convert(int64_t f);
-template<> AT_API int64_t convert(Half f);
+    int main(int argc, char** argv) {
+  return leveldb::test::RunAllTests();
+}
+
     
-    namespace at {
+    TableCache::TableCache(const std::string& dbname,
+                       const Options* options,
+                       int entries)
+    : env_(options->env),
+      dbname_(dbname),
+      options_(options),
+      cache_(NewLRUCache(entries)) {
+}
+    
+    TEST(WriteBatchTest, Multiple) {
+  WriteBatch batch;
+  batch.Put(Slice('foo'), Slice('bar'));
+  batch.Delete(Slice('box'));
+  batch.Put(Slice('baz'), Slice('boo'));
+  WriteBatchInternal::SetSequence(&batch, 100);
+  ASSERT_EQ(100, WriteBatchInternal::Sequence(&batch));
+  ASSERT_EQ(3, WriteBatchInternal::Count(&batch));
+  ASSERT_EQ('Put(baz, boo)@102'
+            'Delete(box)@101'
+            'Put(foo, bar)@100',
+            PrintContents(&batch));
+}
+    
+    #endif  // STORAGE_LEVELDB_HELPERS_MEMENV_MEMENV_H_
+
+    
+    #ifndef STORAGE_LEVELDB_INCLUDE_DUMPFILE_H_
+#define STORAGE_LEVELDB_INCLUDE_DUMPFILE_H_
+    
+        // F1 COEFFICIENT
+    
+    #endif /* OPENCV_CUDA_WARP_REDUCE_HPP__ */
+
+    
+    namespace cv
+{
     }
     
-    THDTensorDescriptor THDTensorDescriptor_newFromTHCudaFloatTensor(THCudaTensor *tensor) {
-  return at::getType(at::Backend::CUDA, at::ScalarType::Float).unsafeTensorFromTH((void*)tensor, true);
+    cv::cuda::GpuMat::GpuMat(const GpuMat& m, Range rowRange_, Range colRange_)
+{
+    flags = m.flags;
+    step = m.step; refcount = m.refcount;
+    data = m.data; datastart = m.datastart; dataend = m.dataend;
+    allocator = m.allocator;
+    }
+    
+    
+    {}
+
+    
+    
+    {  private:
+    QueueWorker::Request _request;
+  };
+    
+    
+    {} // namespace gloo_cache
+    
+    #define THCPStorage TH_CONCAT_3(THCP,Real,Storage)
+#define THCPStorageStr TH_CONCAT_STRING_3(torch.cuda.,Real,Storage)
+#define THCPStorageClass TH_CONCAT_3(THCP,Real,StorageClass)
+#define THCPStorage_(NAME) TH_CONCAT_4(THCP,Real,Storage_,NAME)
+    
+    /**
+ * \fn  std::shared_ptr<xgboost::DMatrix> CreateDMatrix(int rows, int columns, float sparsity, int seed);
+ *
+ * \brief Creates dmatrix with uniform random data between 0-1.
+ *
+ * \param rows      The rows.
+ * \param columns   The columns.
+ * \param sparsity  The sparsity.
+ * \param seed      The seed.
+ *
+ * \return  The new d matrix.
+ */
+    
+    
+    {}  // namespace xgboost
+    
+          CompressedIterator<int> ci(buffer.data(), alphabet_size);
+      std::vector<int> output(input.size());
+      for (int i = 0; i < input.size(); i++) {
+        output[i] = ci[i];
+      }
+    
+    // helper functions
+// set handle
+void setHandle(JNIEnv *jenv, jlongArray jhandle, void* handle) {
+#ifdef __APPLE__
+  jlong out = (long) handle;
+#else
+  int64_t out = (int64_t) handle;
+#endif
+  jenv->SetLongArrayRegion(jhandle, 0, 1, &out);
+}
+    
+    // common regressions
+// linear regression
+struct LinearSquareLoss {
+  // duplication is necessary, as __device__ specifier
+  // cannot be made conditional on template parameter
+  XGBOOST_DEVICE static bst_float PredTransform(bst_float x) { return x; }
+  XGBOOST_DEVICE static bool CheckLabel(bst_float x) { return true; }
+  XGBOOST_DEVICE static bst_float FirstOrderGradient(bst_float predt, bst_float label) {
+    return predt - label;
+  }
+  XGBOOST_DEVICE static bst_float SecondOrderGradient(bst_float predt, bst_float label) {
+    return 1.0f;
+  }
+  template <typename T>
+  static T PredTransform(T x) { return x; }
+  template <typename T>
+  static T FirstOrderGradient(T predt, T label) { return predt - label; }
+  template <typename T>
+  static T SecondOrderGradient(T predt, T label) { return T(1.0f); }
+  static bst_float ProbToMargin(bst_float base_score) { return base_score; }
+  static const char* LabelErrorMsg() { return ''; }
+  static const char* DefaultEvalMetric() { return 'rmse'; }
+};
+    
+    // objectives
+#include '../src/objective/objective.cc'
+#include '../src/objective/regression_obj.cc'
+#include '../src/objective/multiclass_obj.cc'
+#include '../src/objective/rank_obj.cc'
+    
+    
+    {inline bool RowSet::Load(dmlc::Stream* fi) {
+  if (!fi->Read(&rows_)) return false;
+  if (rows_.size() != 0) return true;
+  return fi->Read(&size_, sizeof(size_)) == sizeof(size_);
+}
+}  // namespace xgboost
+    
+    // implementation of inline functions.
+inline void Learner::Predict(const SparseBatch::Inst& inst,
+                             bool output_margin,
+                             HostDeviceVector<bst_float>* out_preds,
+                             unsigned ntree_limit) const {
+  gbm_->PredictInstance(inst, &out_preds->HostVector(), ntree_limit);
+  if (!output_margin) {
+    obj_->PredTransform(out_preds);
+  }
 }
