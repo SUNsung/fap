5e113f3603bb50284dd8f510355b8f90535ba5d0
SUNsung <weatherius@gmail.com>
2018-04-22 13:36:58 +0300

    Rewrite [56DSmyqAHmacg56ixhpGmeu2WE56kK5aV3JCnQ656slk.py] File in: 2018-04-22 13:36:57

--- pyton/56DSmyqAHmacg56ixhpGmeu2WE56kK5aV3JCnQ656slk.py
+++ pyton/56DSmyqAHmacg56ixhpGmeu2WE56kK5aV3JCnQ656slk.py
@@ -1,131 +1,70 @@
 
         
-        
-def default_hooks():
-    return dict((event, []) for event in HOOKS)
+                if opts.pidfile:
+            with open(opts.pidfile, 'w') as f:
+                f.write(str(os.getpid()) + os.linesep)
     
-        def inner(*suffix):
-        return urljoin(httpbin_url, '/'.join(suffix))
+        def short_desc(self):
+        return 'Generate new spider using pre-defined templates'
     
-        def init_per_thread_state(self):
-        # Ensure state is initialized just once per-thread
-        if not hasattr(self._thread_local, 'init'):
-            self._thread_local.init = True
-            self._thread_local.last_nonce = ''
-            self._thread_local.nonce_count = 0
-            self._thread_local.chal = {}
-            self._thread_local.pos = None
-            self._thread_local.num_401_calls = None
+        def extract_contracts(self, method):
+        contracts = []
+        for line in method.__doc__.split('\n'):
+            line = line.strip()
     
+        name = 'scrapes'
     
-class UnrewindableBodyError(RequestException):
-    '''Requests encountered an error when trying to rewind a body'''
+        def add_cancelled(self, future):
+        self.finished_futures.append(future)
     
+        >>> Point = namedtuple('Point', 'x y')
+    >>> Point.__doc__                   # docstring for the new class
+    'Point(x, y)'
+    >>> p = Point(11, y=22)             # instantiate with positional args or keywords
+    >>> p[0] + p[1]                     # indexable like a plain tuple
+    33
+    >>> x, y = p                        # unpack like a regular tuple
+    >>> x, y
+    (11, 22)
+    >>> p.x + p.y                       # fields also accessable by name
+    33
+    >>> d = p._asdict()                 # convert to a dictionary
+    >>> d['x']
+    11
+    >>> Point(**d)                      # convert from a dictionary
+    Point(x=11, y=22)
+    >>> p._replace(x=100)               # _replace() is like str.replace() but targets named fields
+    Point(x=100, y=22)
     
-collect_ignore = [
-    # deprecated or moved modules
-    'scrapy/conf.py',
-    'scrapy/stats.py',
-    'scrapy/project.py',
-    'scrapy/utils/decorator.py',
-    'scrapy/statscol.py',
-    'scrapy/squeue.py',
-    'scrapy/log.py',
-    'scrapy/dupefilter.py',
-    'scrapy/command.py',
-    'scrapy/linkextractor.py',
-    'scrapy/spider.py',
+      # Remove old YCM libs if present so that YCM can start.
+  old_libs = (
+    glob.glob( p.join( DIR_OF_OLD_LIBS, '*ycm_core.*' ) ) +
+    glob.glob( p.join( DIR_OF_OLD_LIBS, '*ycm_client_support.*' ) ) +
+    glob.glob( p.join( DIR_OF_OLD_LIBS, '*clang*.*') ) )
+  for lib in old_libs:
+    os.remove( lib )
     
-    
-class _BenchServer(object):
-    
-            try:
-            spidercls = self.crawler_process.spider_loader.load(name)
-        except KeyError:
-            pass
-        else:
-            # if spider already exists and not --force then halt
-            if not opts.force:
-                print('Spider %r already exists in module:' % name)
-                print('  %s' % spidercls.__module__)
-                return
-        template_file = self._find_template(opts.template)
-        if template_file:
-            self._genspider(module, name, domain, opts.template, template_file)
-            if opts.edit:
-                self.exitcode = os.system('scrapy edit '%s'' % name)
-    
-    # Can't import these from paths.py because that uses `future` imports
-DIR_OF_CURRENT_SCRIPT = os.path.dirname( os.path.abspath( __file__ ) )
-DIR_OF_YCMD = os.path.join( DIR_OF_CURRENT_SCRIPT, '..', '..', 'third_party',
-                            'ycmd' )
-    
-            try:
-            result = self.fn(*self.args, **self.kwargs)
-        except BaseException:
-            e = sys.exc_info()[1]
-            self.future.set_exception(e)
-        else:
-            self.future.set_result(result)
-    
-        return True
-    
-      def NewCandidateInsertionText( to_insert, text_after_cursor ):
-    overlap_len = OverlapLength( to_insert, text_after_cursor )
-    if overlap_len:
-      return to_insert[ :-overlap_len ]
-    return to_insert
+      if config_entry is None:
+    return []
     
     
-def RawResponse_ConvertedFromOmniCompleter_test():
-  vim_results = [
-    { 'word': 'WORD', 'abbr': 'ABBR', 'menu': 'MENU',
-      'kind': 'KIND', 'info': 'INFO' },
-    { 'word': 'WORD2', 'abbr': 'ABBR2', 'menu': 'MENU2',
-      'kind': 'KIND2', 'info': 'INFO' },
-    { 'word': 'WORD', 'abbr': 'ABBR',  },
-    {  },
-  ]
-  expected_results = [
-    has_entries( { 'insertion_text': 'WORD', 'menu_text': 'ABBR',
-                   'extra_menu_info': 'MENU', 'kind': [ 'KIND' ],
-                   'detailed_info': 'INFO' } ),
-    has_entries( { 'insertion_text': 'WORD2', 'menu_text': 'ABBR2',
-                   'extra_menu_info': 'MENU2', 'kind': [ 'KIND2' ],
-                   'detailed_info': 'INFO' } ),
-    has_entries( { 'insertion_text': 'WORD', 'menu_text': 'ABBR',  } ),
-    has_entries( {  } ),
-  ]
-  request = BuildOmnicompletionRequest( vim_results )
+def FormatDebugInfoResponse_NoExtraConf_test():
+  response = deepcopy( GENERIC_RESPONSE )
+  response[ 'extra_conf' ].update( {
+    'is_loaded': False,
+    'path': None
+  } )
+  assert_that(
+    FormatDebugInfoResponse( response ),
+    contains_string(
+      'No extra configuration file found\n'
+    )
+  )
     
-      # On UNIX platforms, we use sys.executable as the Python interpreter path.
-  # We cannot use sys.executable on Windows because for unknown reasons, it
-  # returns the Vim executable. Instead, we use sys.exec_prefix to deduce the
-  # interpreter path.
-  python_interpreter = ( WIN_PYTHON_PATH if utils.OnWindows() else
-                         sys.executable )
-  if _EndsWithPython( python_interpreter ):
-    return python_interpreter
-    
-            def find_handler(self, request, **kwargs):
-            handler = GetResource if request.method == 'GET' else PostResource
-            return self.app.get_handler_delegate(request, handler, path_args=[request.path])
-    
-            class Delegate(HTTPMessageDelegate):
-            def headers_received(self, start_line, headers):
-                test.code = start_line.code
+      _assert_rejects( f, 'This is a Taco' )
+  _assert_accepts( f, 'This is a Burrito' )
+
     
     
-globals().update(wrap_web_tests_application())
-    
-    
-class Waker(interface.Waker):
-    '''Create an OS independent asynchronous pipe.
-    
-        def test_single(self):
-        # As a sanity check, run the single-process version through this test
-        # harness too.
-        code = textwrap.dedent('''
-            from __future__ import print_function
-            from tornado.ioloop import IOLoop
-            from tornado.tcpserver import TCPServer
+def _IsReady():
+  return BaseRequest.GetDataFromHandler( 'ready' )
