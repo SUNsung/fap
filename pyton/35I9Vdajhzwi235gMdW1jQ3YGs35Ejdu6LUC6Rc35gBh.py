9f4a27ea2b7a655a52906e71db89858c7fd30bf1
SUNsung <weatherius@gmail.com>
2018-04-22 04:09:57 +0300

    Rewrite [35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py] File in: 2018-04-22 04:09:56

--- pyton/35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py
+++ pyton/35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py
@@ -1,158 +1,34 @@
 
         
-        UNICODE = FILE_CONTENT
-
+            if include_debug_info:
+        print_debug_info(env)
+        if args == ['--debug']:
+            return ExitStatus.OK
     
-        def _migrate_implicit_content_type(self):
-        '''Migrate the removed implicit_content_type config option'''
-        try:
-            implicit_content_type = self.pop('implicit_content_type')
-        except KeyError:
-            self.save()
-        else:
-            if implicit_content_type == 'form':
-                self['default_options'].insert(0, '--form')
-            self.save()
-            self.load()
-
+        This argument can be omitted in which case HTTPie will use POST if there
+    is some data to be sent, otherwise GET:
     
-    '''TensorFlow utilities for unit tests.'''
+        def test_verify_custom_ca_bundle_path(
+            self, httpbin_secure_untrusted):
+        r = http(httpbin_secure_untrusted + '/get', '--verify', CA_BUNDLE)
+        assert HTTP_OK in r
     
+    '''
+__version__ = '1.0.0-dev'
+__author__ = 'Jakub Roztocil'
+__licence__ = 'BSD'
     
-def make_estimator():
-  data_format = 'channels_last'
-  if tf.test.is_built_with_cuda():
-    data_format = 'channels_first'
-  return tf.estimator.Estimator(
-      model_fn=mnist.model_fn, params={
-          'data_format': data_format
-      })
+            If decode_unicode is True, content will be decoded using the best
+        available encoding based on the response.
+        '''
     
-      # Instantiate the model.
-  with tf.Graph().as_default():
-    with tf.variable_scope('lexnet'):
-      instance = tf.placeholder(dtype=tf.string)
-      model = path_model.PathBasedModel(
-          hparams, lemma_embeddings, instance)
+            assert server.handler_results[0] == first_request
+        assert server.handler_results[1] == second_request
     
-      Returns:
-    data: Numpy array.
-  '''
-  with tf.gfile.FastGFile(file_path, 'rb') as f:
-    return ParseFromString(f.read())
-    
-        # not a test, but looks like a test
-    'scrapy/utils/testsite.py',
-    
-    
-class _BenchSpider(scrapy.Spider):
-    '''A spider that follows all links'''
-    name = 'follow'
-    total = 10000
-    show = 20
-    baseurl = 'http://localhost:8998'
-    link_extractor = LinkExtractor()
-    
-            try:
-            spidercls = self.crawler_process.spider_loader.load(name)
-        except KeyError:
-            pass
-        else:
-            # if spider already exists and not --force then halt
-            if not opts.force:
-                print('Spider %r already exists in module:' % name)
-                print('  %s' % spidercls.__module__)
-                return
-        template_file = self._find_template(opts.template)
-        if template_file:
-            self._genspider(module, name, domain, opts.template, template_file)
-            if opts.edit:
-                self.exitcode = os.system('scrapy edit '%s'' % name)
-    
-    
-def SetUpSystemPaths():
-  sys.path.insert( 0, os.path.join( DIR_OF_YCMD ) )
-    
-        def _adjust_process_count(self):
-        for _ in range(len(self._processes), self._max_workers):
-            p = multiprocessing.Process(
-                    target=_process_worker,
-                    args=(self._call_queue,
-                          self._result_queue))
-            p.start()
-            self._processes.add(p)
-    
-            for future in as_completed(future_to_url):
-            try:
-                url_to_content[future_to_url[future]] = future.result()
-            except:
-                pass
-        return url_to_content
-    finally:
-        executor.shutdown()
-    
-    
-  @staticmethod
-  def _ExtraHeaders( method, request_uri, request_body = None ):
-    if not request_body:
-      request_body = bytes( b'' )
-    headers = dict( _HEADERS )
-    headers[ _HMAC_HEADER ] = b64encode(
-        CreateRequestHmac( ToBytes( method ),
-                           ToBytes( urlparse( request_uri ).path ),
-                           request_body,
-                           BaseRequest.hmac_secret ) )
-    return headers
-    
-    
-class DebugInfoRequest( BaseRequest ):
-  def __init__( self, extra_data = None ):
-    super( DebugInfoRequest, self ).__init__()
-    self._extra_data = extra_data
-    self._response = None
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-      _assert_rejects( f, { 'text' : 'This is an unimportant taco',
-                        'kind' : 'WARNING' } )
-  _assert_rejects( f, { 'text' : 'This taco will NOT be shown',
-                        'kind' : 'ERROR' } )
-  _assert_accepts( f, { 'text' : 'This burrito WILL be shown',
-                        'kind' : 'ERROR' } )
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-        @YouCompleteMeInstance( { 'log_level': 'debug',
-                              'keep_logfiles': 1 } )
-    def Debug_test( ycm ):
-        ...
-  '''
-  def Decorator( test ):
-    @functools.wraps( test )
-    def Wrapper( *args, **kwargs ):
-      ycm = YouCompleteMe( MakeUserOptions( custom_options ) )
-      WaitUntilReady()
-      ycm.CheckIfServerIsReady()
-      try:
-        test( ycm, *args, **kwargs )
-      finally:
-        StopServer( ycm )
-    return Wrapper
-  return Decorator
+    elif is_py3:
+    from urllib.parse import urlparse, urlunparse, urljoin, urlsplit, urlencode, quote, unquote, quote_plus, unquote_plus, urldefrag
+    from urllib.request import parse_http_list, getproxies, proxy_bypass, proxy_bypass_environment, getproxies_environment
+    from http import cookiejar as cookielib
+    from http.cookies import Morsel
+    from io import StringIO
+    from collections import OrderedDict
