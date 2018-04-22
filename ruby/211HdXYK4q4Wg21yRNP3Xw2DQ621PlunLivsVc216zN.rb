5495eecbca703ce0c96e1049076d8604ef9dc253
SUNsung <weatherius@gmail.com>
2018-04-22 10:23:18 +0300

    Rewrite [211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb] File in: 2018-04-22 10:23:18

--- ruby/211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb
+++ ruby/211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb
@@ -1,96 +1,86 @@
 
         
-            # Determine the available cores in host system.
-    # This mostly helps on linux, but it couldn't hurt on MacOSX.
-    if RUBY_PLATFORM =~ /linux/
-      cpu_count = `nproc`.to_i
-    elsif RUBY_PLATFORM =~ /darwin/
-      cpu_count = `sysctl -n hw.ncpu`.to_i
-    end
+          def tumblr_consumer_key
+    ENV['TUMBLR_OAUTH_KEY']
+  end
     
-          def update_if_necessary!
-        if @update && config.verbose?
-          UI.section('\nUpdating Spec Repositories\n'.yellow) do
-            Repo.new(ARGV.new(['update'])).run
+        respond_to do |format|
+      if @user_credential.save
+        format.html { redirect_to user_credentials_path, notice: 'Your credential was successfully created.' }
+        format.json { render json: @user_credential, status: :created, location: @user_credential }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user_credential.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+    
+      def test_stat
+    tb = Process.clock_gettime(Process::CLOCK_REALTIME)
+    Tempfile.create('stat') {|file|
+      tb = (tb + Process.clock_gettime(Process::CLOCK_REALTIME)) / 2
+      file.close
+      path = file.path
+    }
+    
+      it 'returns an empty string and does not decode any bytes when the count modifier is zero' do
+    'abc'.unpack(unpack_format(0)+unpack_format).should == ['', 'a']
+  end
+    
+      it 'decodes UTF-8 BMP codepoints' do
+    [ ['\xc2\x80',      [0x80]],
+      ['\xdf\xbf',      [0x7ff]],
+      ['\xe0\xa0\x80',  [0x800]],
+      ['\xef\xbf\xbf',  [0xffff]]
+    ].should be_computed_by(:unpack, 'U')
+  end
+    
+      Car = Struct.new(:make, :model, :year)
+    
+      # Hangs on 1.8.6.114 OS X, possibly also on Linux
+  quarantine! do
+    it 'is deferred if ensure clause sleeps' do
+      ThreadSpecs.wakeup_dying_sleeping_thread(@method) { sleep; ScratchPad.record :after_sleep }
+      ScratchPad.recorded.should == :after_sleep
+    end
+  end
+    
+        10.times { sleep 0.1 if after_sleep1 != true }
+    10.times { sleep 0.1 if t.status and t.status != 'sleep' }
+    after_sleep2.should == false # t should be blocked on the second sleep
+    t.send(@method)
+    
+        res << inp.to_html
+  end
+  res << '</form>'
+end
+    
+    puts '* System info:'
+    
+      self.include_root_in_json = false
+    
+          def handle_params_error(error, error_description)
+        if params[:client_id] && params[:redirect_uri]
+          handle_params_error_when_client_id_and_redirect_uri_exists(error, error_description)
+        else
+          render_error I18n.t('api.openid_connect.error_page.could_not_authorize'), error_description
+        end
+      end
+    
+          def accepts?(env)
+        cookie_header = env['HTTP_COOKIE']
+        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
+        cookies.each do |k, v|
+          if k == session_key && Array(v).size > 1
+            bad_cookies << k
+          elsif k != session_key && Rack::Utils.unescape(k) == session_key
+            bad_cookies << k
           end
         end
+        bad_cookies.empty?
       end
     
-          # Prints a title in blue with surrounding newlines.
-      def title(text)
-        # Use $stdout directly to bypass the indentation that our `puts` does.
-        $stdout.puts(color.colorize('\n#{text}\n', :blue))
-      end
-    
-          def assert_valid_stage_names(names)
-        invalid = names.find { |n| RESERVED_NAMES.include?(n) }
-        return if invalid.nil?
-    
-        # Provide a wrapper for the SCM that loads a strategy for the user.
-    #
-    # @param [Rake] context     The context in which the strategy should run
-    # @param [Module] strategy  A module to include into the SCM instance. The
-    #    module should provide the abstract methods of Capistrano::SCM
-    #
-    def initialize(context, strategy)
-      @context = context
-      singleton = class << self; self; end
-      singleton.send(:include, strategy)
-    end
-    
-          instance.run_paperclip_callbacks(:post_process) do
-        instance.run_paperclip_callbacks(:'#{name}_post_process') do
-          if !@options[:check_validity_before_processing] || !instance.errors.any?
-            post_process_styles(*style_args)
-          end
-        end
-      end
-    end
-    
-        def initialize(filepath)
-      @filepath = filepath
-    end
-    
-        # Extracts the Geometry from a 'WxH,O' string
-    # Where W is the width, H is the height,
-    # and O is the EXIF orientation
-    def self.parse(string)
-      GeometryParser.new(string).make
-    end
-    
-        def add_required_validations
-      options = Paperclip::Attachment.default_options.deep_merge(@options)
-      if options[:validate_media_type] != false
-        name = @name
-        @klass.validates_media_type_spoof_detection name,
-          :if => ->(instance){ instance.send(name).dirty? }
-      end
-    end
-    
-            def failure_message
-          'Attachment #{@attachment_name} should be required'
-        end
-    
-      context 'called with four styles' do
-    it 'applies different styles to all sides' do
-      rule = 'border-style: dotted groove ridge none'
-    
-          expect('.border-width-false-third').to have_ruleset(ruleset)
-      expect('.border-width-false-third').to_not have_rule(bad_rule)
-    end
-  end
-end
-
-    
-      context 'called with arguments (2, $value: 4em 6em)' do
-    it 'outputs sextuple the second value from the default scale' do
-      expect('.two-double-value').to have_rule('font-size: 3.125em')
-    end
-  end
-end
-
-    
-          expect('.size-auto').to have_ruleset(rule)
-    end
-  end
+    begin
+  require 'escape_utils'
+rescue LoadError
 end
