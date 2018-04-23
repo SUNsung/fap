41b8fb087f7d6cae849ebd81b4590636df28e905
SUNsung <weatherius@gmail.com>
2018-04-23 21:37:31 +0300

    Rewrite [58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb] File in: 2018-04-23 21:37:31

--- ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
+++ ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
@@ -1,99 +1,61 @@
 
         
-          def test_escape_javascript
-    assert_equal '', escape_javascript(nil)
-    assert_equal %(This \\'thing\\' is really\\n netos\\'), escape_javascript(%(This 'thing' is really\n netos'))
-    assert_equal %(backslash\\\\test), escape_javascript(%(backslash\\test))
-    assert_equal %(dont <\\/close> tags), escape_javascript(%(dont </close> tags))
-    assert_equal %(unicode &#x2028; newline), escape_javascript(%(unicode \342\200\250 newline).dup.force_encoding(Encoding::UTF_8).encode!)
-    assert_equal %(unicode &#x2029; newline), escape_javascript(%(unicode \342\200\251 newline).dup.force_encoding(Encoding::UTF_8).encode!)
+                command = [escaped_gradle_path, 'tasks', '--console=plain'].join(' ')
+        output = Action.sh(command, print_command: false, print_command_output: false)
+        output.split('\n').each do |line|
+          if (result = line.match(/(\w+)\s\-\s([\w\s]+)/))
+            self.tasks << GradleTask.new(title: result[1], description: result[2])
+          end
+        end
     
-          include ActionDispatch::Assertions, ActionDispatch::TestProcess
-      include Rails::Dom::Testing::Assertions
-      include ActionController::TemplateAssertions
-      include ActionView::Context
+          it 'get HG revision number' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          get_build_number_repository(
+            use_hg_revision_number: true
+          )
+        end').runner.execute(:test)
     
-            adapter = adapter.camelize
-        adapter = 'PostgreSQL' if adapter == 'Postgresql'
-        'ActionCable::SubscriptionAdapter::#{adapter}'.constantize
       end
+    
+        if extension_pathname.readable?
+      ENV['BUNDLE_GEMFILE'] = extension_pathname.to_path
+      break
     end
   end
 end
-
     
-    class TestResponseTest < ActiveSupport::TestCase
-  def assert_response_code_range(range, predicate)
-    response = ActionDispatch::TestResponse.new
-    (0..599).each do |status|
-      response.status = status
-      assert_equal range.include?(status), response.send(predicate),
-                   'ActionDispatch::TestResponse.new(#{status}).#{predicate}'
-    end
+      def register_sigs
+    self.sigs = {
+      :banner		=> /^(\*\s+OK[^\n\r]*)/i,
+      :login		=> /^CAPABILITY\s+LOGIN\s+([^\s]+)\s+([^\n\r]+)/i,
+      :login_pass => /^CAPABILITY\s+OK\s+(Login[^\n\r]*)/i,
+      :login_bad	=> /^CAPABILITY\s+BAD\s+(Login[^\n\r]*)/i,
+      :login_fail => /^CAPABILITY\s+NO\s+(Login[^\n\r]*)/i
+    }
   end
     
-      class B < A
-    helper { def shout; 'B' end }
+    clsJavaCompile 	= Rjb::import('javaCompile.CompileSourceInMemory')
+clsCreateJar	= Rjb::import('javaCompile.CreateJarFile')
+clsFile			= Rjb::import('java.io.File')
+system			= Rjb::import('java.lang.System')
+#clsString	= Rjb::import('java.lang.String')
     
-          assert_equal 'entry', @controller.response.body
-      assert @controller.params.has_key?(:entry)
-      assert_equal 'content...', @controller.params['entry']['summary']
-    end
+        super
   end
+end
     
-    class AssertSelectEmailTest < ActionMailer::TestCase
-  class AssertSelectMailer < ActionMailer::Base
-    def test(html)
-      mail body: html, content_type: 'text/html',
-        subject: 'Test e-mail', from: 'test@test.host', to: 'test <test@test.host>'
-    end
-  end
-    
-            # This returns all the config classes for the various provisioners.
+            # Runs the template configuration utilities.
         #
-        # @return [Registry]
-        def provisioner_configs
-          Registry.new.tap do |result|
-            @registered.each do |plugin|
-              result.merge!(plugin.components.configs[:provisioner])
+        # @return [void]
+        #
+        def configure_template
+          UI.section('Configuring #{@name} template.') do
+            Dir.chdir(@name) do
+              if File.exist?('configure')
+                system({ 'COCOAPODS_VERSION' => Pod::VERSION }, './configure', @name, *@additional_args)
+              else
+                UI.warn 'Template does not have a configure file.'
+              end
             end
           end
         end
-    
-          class << self
-        # Mark a given block of code as a 'busy' block of code, which will
-        # register a SIGINT handler for the duration of the block. When a
-        # SIGINT occurs, the `sig_callback` proc will be called. It is up
-        # to the callback to behave properly and exit the application.
-        def busy(sig_callback)
-          register(sig_callback)
-          return yield
-        ensure
-          unregister(sig_callback)
-        end
-    
-          protected
-    
-                # Break out if we're supposed to. Otherwise re-raise the error
-            # because it is a real problem.
-            break if breakable
-            raise
-          end
-        end
-    
-              if argv.length == 2
-            # @deprecated
-            @env.ui.warn('WARNING: The second argument to `vagrant box remove`')
-            @env.ui.warn('is deprecated. Please use the --provider flag. This')
-            @env.ui.warn('feature will stop working in the next version.')
-            options[:provider] = argv[1]
-          end
-    
-    module VagrantPlugins
-  module CommandBox
-    module Command
-      class Repackage < Vagrant.plugin('2', :command)
-        def execute
-          opts = OptionParser.new do |o|
-            o.banner = 'Usage: vagrant box repackage <name> <provider> <version>'
-          end
