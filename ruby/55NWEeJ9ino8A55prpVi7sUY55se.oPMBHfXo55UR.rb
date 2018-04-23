c5b436bd5dbb61c5e602b869072c489b213a0351
SUNsung <weatherius@gmail.com>
2018-04-23 09:06:30 +0300

    Rewrite [55NWEeJ9ino8A55prpVi7sUY55se.oPMBHfXo55UR.rb] File in: 2018-04-23 09:06:29

--- ruby/55NWEeJ9ino8A55prpVi7sUY55se.oPMBHfXo55UR.rb
+++ ruby/55NWEeJ9ino8A55prpVi7sUY55se.oPMBHfXo55UR.rb
@@ -1,15 +1,3 @@
 
         
-        html_readme = '<html>#{Kramdown::Document.new(open('README.md').read).to_html}</html>'
-readme_doctree = REXML::Document.new(html_readme)
-links = REXML::XPath.match(readme_doctree, '//a')
-    
-            target_module << if app.resolved_build_setting('SWIFT_OPTIMIZATION_LEVEL').values.any?
-                           <<-RUBY
-  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
-  use_frameworks!
-    
-            sets = config.sources_manager.aggregate.all_sets
-        sets.each { |set| UI.pod(set, :name_and_version) }
-        UI.puts '\n#{sets.count} pods were found'
-      end
+        
