ff7e6db09eb2d52d4ddfdbb25f06e81451d3157e
SUNsung <weatherius@gmail.com>
2018-04-23 09:58:07 +0300

    Rewrite [17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp] File in: 2018-04-23 09:58:06

--- cccccc/17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp
+++ cccccc/17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp
@@ -1,194 +1,376 @@
 
         
-        
-    {  /**
-   * @brief Computes the error gradient w.r.t. the BNLL inputs.
-   *
-   * @param top output Blob vector (length 1), providing the error gradient with
-   *      respect to the outputs
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
-   *      with respect to computed outputs @f$ y @f$
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$; Backward fills their diff with
-   *      gradients @f$
-   *        \frac{\partial E}{\partial x}
-   *      @f$ if propagate_down[0]
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+          bool Register(const ui::Accelerator& accelerator,
+                const base::Closure& callback);
+  bool IsRegistered(const ui::Accelerator& accelerator);
+  void Unregister(const ui::Accelerator& accelerator);
+  void UnregisterAll();
+    
+    #ifndef ATOM_BROWSER_NET_URL_REQUEST_ASYNC_ASAR_JOB_H_
+#define ATOM_BROWSER_NET_URL_REQUEST_ASYNC_ASAR_JOB_H_
+    
+    
+    {}  // namespace atom
+    
+    #ifndef ATOM_COMMON_API_REMOTE_CALLBACK_FREER_H_
+#define ATOM_COMMON_API_REMOTE_CALLBACK_FREER_H_
+#include 'atom/common/api/object_life_monitor.h'
+#include 'content/public/browser/web_contents_observer.h'
+    
+    namespace base {
+class FilePath;
+}
+    
+    #ifndef CHROME_BROWSER_PRINTING_PRINT_VIEW_MANAGER_OBSERVER_H_
+#define CHROME_BROWSER_PRINTING_PRINT_VIEW_MANAGER_OBSERVER_H_
+    
+    using namespace swift;
+    
+    typedef NS_ENUM(NSInteger, ObjectBehaviorAction) {
+    ObjectBehaviorActionRetain,
+    ObjectBehaviorActionCopy,
+    ObjectBehaviorActionMutableCopy
 };
     
-    #ifdef USE_CUDNN
-/*
- * @brief cuDNN implementation of ConvolutionLayer.
- *        Fallback to ConvolutionLayer for CPU mode.
- *
- * cuDNN accelerates convolution through forward kernels for filtering and bias
- * plus backward kernels for the gradient w.r.t. the filters, biases, and
- * inputs. Caffe + cuDNN further speeds up the computation through forward
- * parallelism across groups and backward parallelism across gradients.
- *
- * The CUDNN engine does not have memory overhead for matrix buffers. For many
- * input and filter regimes the CUDNN engine is faster than the CAFFE engine,
- * but for fully-convolutional models and large inputs the CAFFE engine can be
- * faster as long as it fits in memory.
-*/
-template <typename Dtype>
-class CuDNNConvolutionLayer : public ConvolutionLayer<Dtype> {
- public:
-  explicit CuDNNConvolutionLayer(const LayerParameter& param)
-      : ConvolutionLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNConvolutionLayer();
+      ArrayRef<StringRef> getTags() const {
+    return llvm::makeArrayRef(Parts.Tags.begin(), Parts.Tags.end());
+  }
+    
+    #include 'swift/Syntax/Rewriter.h'
+    
+    void dump(SubstitutionList subs);
+    
+    namespace index {
     }
     
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+      /// Associates buffer identifiers to buffer IDs.
+  llvm::StringMap<unsigned> BufIdentIDMap;
     
+    bool Substitution::operator==(const Substitution &other) const {
+  // The archetypes may be missing, but we can compare them directly
+  // because archetypes are always canonical.
+  return
+    Replacement->isEqual(other.Replacement) &&
+    Conformance.equals(other.Conformance);
+}
     
+    #endif  // STORAGE_LEVELDB_DB_DB_ITER_H_
 
     
-      // The first paragraph on a page often lacks a first line indent, but should
-  // still be modeled by the same model as other body text paragraphs on the
-  // page.
-  bool is_very_first_or_continuation;
     
-    #include 'fontinfo.h'
-#include 'ndminx.h'
-#include 'sampleiterator.h'
-#include 'shapeclassifier.h'
-#include 'shapetable.h'
-#include 'trainingsample.h'
-#include 'trainingsampleset.h'
-#include 'unicity_table.h'
-    
-    FLOAT32 DistanceBetween(FPOINT A, FPOINT B);
-    
-    #include <folly/Optional.h>
-    
-    
-    {  unsigned next_vr{Vreg::V0};
-  Vlabel entry;
-  jit::vector<Vframe> frames;
-  jit::vector<Vblock> blocks;
-  jit::hash_map<Vconst,Vreg,Vconst::Hash> constToReg;
-  jit::hash_map<size_t,Vconst> regToConst;
-  jit::vector<VregList> tuples;
-  jit::vector<VcallArgs> vcallArgs;
-  jit::vector<VdataBlock> dataBlocks;
-  uint16_t cur_voff{0};  // current instruction index managed by Vout
-  bool padding{false};
-  bool profiling{false};
-  folly::Optional<TransContext> context;
-  StructuredLogEntry* log_entry{nullptr};
-};
-    
-      if (default_magic) {
-    free(default_magic);
-    default_magic = NULL;
+    {  Table* table = reinterpret_cast<TableAndFile*>(cache_->Value(handle))->table;
+  Iterator* result = table->NewIterator(options);
+  result->RegisterCleanup(&UnrefEntry, cache_, handle);
+  if (tableptr != NULL) {
+    *tableptr = table;
   }
+  return result;
+}
     
-    void OfflineCode::disasm(FILE* file,
-                         TCA fileStartAddr,
-                         TCA codeStartAddr,
-                         uint64_t codeLen,
-                         const PerfEventsMap<TCA>& perfEvents,
-                         BCMappingInfo bcMappingInfo,
-                         bool printAddr,
-                         bool printBinary) {
+      // If a seek to internal key 'k' in specified file finds an entry,
+  // call (*handle_result)(arg, found_key, found_value).
+  Status Get(const ReadOptions& options,
+             uint64_t file_number,
+             uint64_t file_size,
+             const Slice& k,
+             void* arg,
+             void (*handle_result)(void*, const Slice&, const Slice&));
+    
+    TEST(FindFileTest, OverlappingFiles) {
+  Add('150', '600');
+  Add('400', '500');
+  disjoint_sorted_files_ = false;
+  ASSERT_TRUE(! Overlaps('100', '149'));
+  ASSERT_TRUE(! Overlaps('601', '700'));
+  ASSERT_TRUE(Overlaps('100', '150'));
+  ASSERT_TRUE(Overlaps('100', '200'));
+  ASSERT_TRUE(Overlaps('100', '300'));
+  ASSERT_TRUE(Overlaps('100', '400'));
+  ASSERT_TRUE(Overlaps('100', '500'));
+  ASSERT_TRUE(Overlaps('375', '400'));
+  ASSERT_TRUE(Overlaps('450', '450'));
+  ASSERT_TRUE(Overlaps('450', '500'));
+  ASSERT_TRUE(Overlaps('450', '700'));
+  ASSERT_TRUE(Overlaps('600', '700'));
+}
+    
+    // Returns a new environment that stores its data in memory and delegates
+// all non-file-storage tasks to base_env. The caller must delete the result
+// when it is no longer needed.
+// *base_env must remain live while the result is in use.
+Env* NewMemEnv(Env* base_env);
+    
+      // Return a string that contains the copy of the referenced data.
+  std::string ToString() const { return std::string(data_, size_); }
+    
+            // cross two lines
+        Point2f origin1 = quad_corners[0];
+        Point2f dir1 = quad_corners[1] - quad_corners[0];
+        Point2f origin2 = quad_corners[2];
+        Point2f dir2 = quad_corners[3] - quad_corners[2];
+        double angle = acos(dir1.dot(dir2)/(norm(dir1)*norm(dir2)));
+        if(cvIsNaN(angle) || cvIsInf(angle) || angle < 0.5 || angle > CV_PI - 0.5) continue;
+    
+                //a simple check that the position is general:
+            //  for each line check that all other points don't belong to it
+            isGeneralPosition = true;
+            for (int startPointIndex = 0; startPointIndex < usedPointsCount && isGeneralPosition; startPointIndex++)
+            {
+                for (int endPointIndex = startPointIndex + 1; endPointIndex < usedPointsCount && isGeneralPosition; endPointIndex++)
+                {
+    }
     }
     
     
-    {}
+    {                return (count & TAG_MASK) - val;
+#else
+                return ::atomicAdd(address, val);
+#endif
+            }
     
-    bool js_cocos2dx_physics3d_Physics3DPointToPointConstraint_constructor(JSContext *cx, uint32_t argc, jsval *vp);
-void js_cocos2dx_physics3d_Physics3DPointToPointConstraint_finalize(JSContext *cx, JSObject *obj);
-void js_register_cocos2dx_physics3d_Physics3DPointToPointConstraint(JSContext *cx, JS::HandleObject global);
-void register_all_cocos2dx_physics3d(JSContext* cx, JS::HandleObject obj);
-bool js_cocos2dx_physics3d_Physics3DPointToPointConstraint_getPivotPointInA(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DPointToPointConstraint_getPivotPointInB(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DPointToPointConstraint_init(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DPointToPointConstraint_setPivotPointInA(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DPointToPointConstraint_setPivotPointInB(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DPointToPointConstraint_create(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_physics3d_Physics3DPointToPointConstraint_Physics3DPointToPointConstraint(JSContext *cx, uint32_t argc, jsval *vp);
-    
-    
-    
-    
-    
-    			// Top horizontal
-			shape.Set(b2Vec2(-20.0f, 20.0f), b2Vec2(20.0f, 20.0f));
-			ground->CreateFixture(&sd);
-    
-    	void Break()
-	{
-		// Create two bodies from one.
-		b2Body* body1 = m_piece1->GetBody();
-		b2Vec2 center = body1->GetWorldCenter();
+    namespace grpc {
+namespace testing {
+    }
     }
     
-        // Try to grow in place.
-    //
-    // Note that xallocx(MALLOCX_ZERO) will only zero newly allocated memory,
-    // even if a previous allocation allocated more than we requested.
-    // This is fine; we always use MALLOCX_ZERO with jemalloc and we
-    // always expand our allocation to the real size.
-    if (prevCapacity * sizeof(ElementWrapper) >= jemallocMinInPlaceExpandable) {
-      success =
-          (xallocx(threadEntry->elements, newByteSize, 0, MALLOCX_ZERO) ==
-           newByteSize);
-    }
+    std::unique_ptr<ScenarioResult> RunScenario(
+    const grpc::testing::ClientConfig& client_config, size_t num_clients,
+    const grpc::testing::ServerConfig& server_config, size_t num_servers,
+    int warmup_seconds, int benchmark_seconds, int spawn_local_worker_count,
+    const grpc::string& qps_server_target_override,
+    const grpc::string& credential_type, bool run_inproc);
     
-    template <typename T>
-struct constexpr_abs_helper<
-    T,
-    typename std::enable_if<std::is_floating_point<T>::value>::type> {
-  static constexpr T go(T t) {
-    return t < static_cast<T>(0) ? -t : t;
-  }
-};
+    #include <initializer_list>
+#include <string>
+#include <vector>
     
-    TDigest TDigest::merge(Range<const TDigest*> digests) {
-  size_t nCentroids = 0;
-  for (auto it = digests.begin(); it != digests.end(); it++) {
-    nCentroids += it->centroids_.size();
-  }
-    }
     
-    using namespace folly;
+    {					if (mix_callback) {
+						int mixed = mix_callback(mix_udata, aux_buffer, m);
+						to_read -= mixed;
+						if (mixed != m) { //could mix no more
+							buffer_full = true;
+							break;
+						}
+					} else {
+						to_read -= m; //just pretend we sent the audio
+					}
+				}
     
-      switch (cb.aio_lio_opcode) {
-    case IO_CMD_PREAD:
-    case IO_CMD_PWRITE:
-      os << folly::format(
-          'buf={}, offset={}, nbytes={}, ',
-          cb.u.c.buf,
-          cb.u.c.offset,
-          cb.u.c.nbytes);
-      break;
-    default:
-      os << '[TODO: write debug string for '
-         << iocbCmdToString(cb.aio_lio_opcode) << '] ';
-      break;
-  }
     
-    TEST(AsyncIO, ZeroAsyncDataPollable) {
-  testReads({{0, 0}}, AsyncIO::POLLABLE);
+    {	if (_is_multiplayer) {
+		// need to wait for ID confirmation...
+	} else {
+		emit_signal('connection_established', p_protocol);
+	}
 }
     
     
-    { private:
-  class WriterFactory;
+    {	ClassInfo *ti = classes.getptr(p_class);
+	ERR_FAIL_COND_V(!ti, false);
+	return ti->exposed;
+}
+    
+    	{
+    }
+    
+    	brake = p_brake;
+}
+float VehicleBody::get_brake() const {
+    
+    		for (int j = 0; j < vc; j++) {
+    }
+    
+    	String _validate_name(const String &p_name) const;
+    
+    	virtual bool _set(const StringName &p_name, const Variant &p_property);
+	virtual bool _get(const StringName &p_name, Variant &r_property) const;
+	virtual void _get_property_list(List<PropertyInfo> *p_list) const;
+    
+        // replace children
+    // This looks for nodes in the network that have the same name as its current inputs, and then relinks its inputs to those.
+    // I.e. this allows to move a node from network to another and reconnect by the names if its inputs.
+    for (int i = 0; i < newNode->GetNumInputs(); ++i)
+    {
+        if (m_nameToNodeMap.find(newNode->GetInputs()[i]->NodeName()) == m_nameToNodeMap.end())
+            RuntimeError('Child node %ls is not part of the network.', newNode->GetInputs()[i]->NodeName().c_str());
+        newNode->SetInput(i, m_nameToNodeMap[newNode->GetInputs()[i]->NodeName()]);
+    }
+    
+        mBsparse.SwitchToMatrixType(MatrixType::SPARSE, matrixFormatSparseCSR, true);
+    Matrix<float>::ScaleAndAdd(alpha, mAdense, mBsparse);
+    
+            // take off the prefix
+        std::string search;
+        if (firstStart == symbolIn.length())
+        {
+            // this case is just the model label with nothing else, in that case we want the all nodes
+            search = '*';
+            inWildcard = true;
+        }
+        else
+        {
+            search = symbolIn.substr(firstStart);
+        }
+    
+    
+    {        // no config file, parse as regular argument
+        if (compare)
+        {
+            configString += (msra::strfun::utf8(str) + '\n');
+        }
+        else // One or more config file paths specified in a '+'-separated list.
+        {
+            const std::string filePaths = msra::strfun::utf8(str.substr(configDescriptor.length()));
+            std::vector<std::string> filePathsVec = msra::strfun::split(filePaths, '+');
+            for (auto filePath : filePathsVec)
+            {
+                if (std::find(resolvedConfigFiles.begin(), resolvedConfigFiles.end(), filePath) == resolvedConfigFiles.end())
+                {
+                    // if haven't already read this file, read it
+                    resolvedConfigFiles.push_back(filePath);
+                    configString += config.ReadConfigFile(filePath);
+                    // remember all config directories, for use as include paths by BrainScriptNetworkBuilder
+                    GetBrainScriptNetworkBuilderIncludePaths().push_back(File::DirectoryPathOf(msra::strfun::utf16(filePath)));
+                }
+                else
+                    RuntimeError('Cannot specify same config file multiple times at the command line.');
+            }
+        }
+    }
+    // now, configString is a concatenation of lines, including parameters from the command line, with comments stripped
+    
+        // call TraceTotalNumberOfSteps() to set the total number of steps
+    // Calling this with totalNumberOfSteps>0 will enable progress tracing.
+    static void TraceTotalNumberOfSteps(size_t totalNumberOfSteps)
+    {
+        auto& us = GetStaticInstance();
+        us.m_enabled = totalNumberOfSteps > 0;
+        if (us.m_enabled)
+        {
+            us.m_totalNumberOfSteps = totalNumberOfSteps;
+            us.m_progressTracingTimer.Start();
+        }
+    }
+    
+    template<class TString>
+inline bool AreEqualIgnoreCase(
+    const TString& s1,
+    const typename TString::value_type* s2pointer)
+{
+    return AreEqualIgnoreCase(s1, TString(s2pointer));
+}
+    
+    // ---------------------------------------------------------------------------
+// latticesource -- manages loading of lattices for MMI (in pairs for numer and denom)
+// ---------------------------------------------------------------------------
+    
+    constexpr std::array<int, 4> WORKERS_NUM = {2, 4, 7, 13};
+constexpr int MASTER_PORT = 45678;
+constexpr int BARRIER_WAIT_TIME = 200; // milliseconds
+    
+    int64_t size(const Tensor& self, int64_t dim) {
+  // false is passed to maybe_wrap_dim so behavior is identical to array access (but with wrapping)
+  dim = maybe_wrap_dim(dim, self.dim(), false);
+  return self.sizes()[dim];
+}
+    
+    #if ${isCUDA}
+static cudaError_t call_deleter(void * ctx, void * data) {
+  auto fnptr = (std::function<void(void*)>*) ctx;
+  (*fnptr)(data);
+  delete fnptr;
+  return cudaSuccess;
+}
+static THCDeviceAllocator storage_deleter = {
+  nullptr,
+  nullptr,
+  call_deleter,
+  nullptr,
+  nullptr,
 };
+static cudaError_t wrapped_alloc(void * ctx, void** result, size_t size, cudaStream_t stream) {
+  auto ac = static_cast<detail::AllocatorRetainable*>(ctx);
+  ac->retain();
+  *result = ac->allocate(size);
+  return cudaSuccess;
+}
+static cudaError_t wrapped_free(void * ctx, void * data) {
+  auto ac = static_cast<detail::AllocatorRetainable*>(ctx);
+  ac->deallocate(data);
+  ac->release();
+  return cudaSuccess;
+}
+static THCDeviceAllocator wrapped_allocator = {
+  wrapped_alloc,
+  nullptr,
+  wrapped_free,
+  nullptr,
+  nullptr,
+};
+#else
+static void call_deleter(void * ctx, void * data) {
+  auto fnptr = (std::function<void(void*)>*) ctx;
+  (*fnptr)(data);
+  delete fnptr;
+}
+static THAllocator storage_deleter = {
+  nullptr,
+  nullptr,
+  call_deleter,
+};
+static void* wrapped_alloc(void * ctx, ptrdiff_t size) {
+  auto ac = static_cast<detail::AllocatorRetainable*>(ctx);
+  ac->retain();
+  return ac->allocate(size);
+}
+static void wrapped_free(void * ctx, void * data) {
+  auto ac = static_cast<detail::AllocatorRetainable*>(ctx);
+  ac->deallocate(data);
+  ac->release();
+}
+static THAllocator wrapped_allocator = {
+  wrapped_alloc,
+  nullptr,
+  wrapped_free,
+};
+#endif
+    
+    
+namespace thd {
+    }
+    
+    
+#endif /* COMM_COMM_DNS_H_ */
+
+    
+    
+    {        vecdump_.push_back(strstack);
+        strstack.clear();
+    }
+    
+        void Dump(const std::string& _processname);
+    const std::vector<std::string>& StackList() const;
+    
+    
+    {  private:
+    TServicesMap m_services;
+    TServicesMap m_publicservices;
+    std::vector<ServiceBase*> m_releasevec;
+};
+    
+    #ifndef PublicComponent_spy_h
+#define PublicComponent_spy_h
+    
+    void Test_Spy_Sample::TestFun0()
+{
+    SPY_HOOK_THIS_API(TestFun0);
+    int i = 0;
+    SPY_ATTACH_VARIABLE('TestFun0 i', i);
+    i++;
+    xinfo2(TSF'Test');
+    }
+    
+    // char* to jstring
+jstring JNU_Chars2Jstring(JNIEnv* _env, const char* pat);
+void JNU_FreeJstring(JNIEnv* _env, jstring str);
