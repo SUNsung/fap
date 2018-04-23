28b6dd5d71e3b4cae23bbacc29511c719c32178c
SUNsung <weatherius@gmail.com>
2018-04-23 21:13:35 +0300

    Rewrite [23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp] File in: 2018-04-23 21:13:34

--- cccccc/23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp
+++ cccccc/23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp
@@ -1,60 +1,109 @@
 
         
         
-    {
-    {    int last_dc = 0;
-    const coeff_t* src_coeffs = components_[c].coeffs();
-    coeff_t* dest_coeffs = &comp->coeffs[0];
-    for (int block_y = 0; block_y < comp->height_in_blocks; ++block_y) {
-      for (int block_x = 0; block_x < comp->width_in_blocks; ++block_x) {
-        if (block_y >= components_[c].height_in_blocks() ||
-            block_x >= components_[c].width_in_blocks()) {
-          dest_coeffs[0] = last_dc;
-          for (int k = 1; k < kDCTBlockSize; ++k) {
-            dest_coeffs[k] = 0;
-          }
-        } else {
-          for (int k = 0; k < kDCTBlockSize; ++k) {
-            const int quant = q[c][k];
-            int coeff = src_coeffs[k];
-            assert(coeff % quant == 0);
-            dest_coeffs[k] = coeff / quant;
-          }
-          src_coeffs += kDCTBlockSize;
-        }
-        last_dc = dest_coeffs[0];
-        dest_coeffs += kDCTBlockSize;
-      }
-    }
-  }
-  SaveQuantTables(q, jpg);
+    {	tolua_endmodule(tolua_S);
+	return 1;
 }
     
-    #include 'guetzli/gamma_correct.h'
     
-    // Mimic libjpeg's heuristics to guess jpeg color space.
-// Requires that the jpg has 3 components.
-bool HasYCbCrColorSpace(const JPEGData& jpg) {
-  bool has_Adobe_marker = false;
-  uint8_t Adobe_transform = 0;
-  for (const std::string& app : jpg.app_data) {
-    if (static_cast<uint8_t>(app[0]) == 0xe0) {
-      return true;
-    } else if (static_cast<uint8_t>(app[0]) == 0xee && app.size() >= 15) {
-      has_Adobe_marker = true;
-      Adobe_transform = app[14];
+    
+    
+    
+    
+    
+    #if COCOS2D_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+    
+    
+    
+    bool Test::MouseDown(const b2Vec2& p)
+{
+	m_mouseWorld = p;
+	
+	if (m_mouseJoint != nullptr)
+	{
+		return false;
+	}
     }
-  }
-  if (has_Adobe_marker) {
-    return (Adobe_transform != 0);
-  }
-  const int cid0 = jpg.components[0].id;
-  const int cid1 = jpg.components[1].id;
-  const int cid2 = jpg.components[2].id;
-  return (cid0 != 'R' || cid1 != 'G' || cid2 != 'B');
+    
+    /// Test settings. Some can be controlled in the GUI.
+struct Settings
+{
+	Settings()
+	{
+		viewCenter.Set(0.0f, 20.0f);
+		hz = 60.0f;
+		velocityIterations = 8;
+		positionIterations = 3;
+		drawShapes = 1;
+		drawJoints = 1;
+		drawAABBs = 0;
+		drawContactPoints = 0;
+		drawContactNormals = 0;
+		drawContactImpulse = 0;
+		drawFrictionImpulse = 0;
+		drawCOMs = 0;
+		drawStats = 0;
+		drawProfile = 0;
+		enableWarmStarting = 1;
+		enableContinuous = 1;
+		enableSubStepping = 0;
+		enableSleep = 1;
+		pause = 0;
+		singleStep = 0;
+	}
+    }
+    
+    	enum
+	{
+		e_count = 7
+	};
+    
+    // This is a fun demo that shows off the wheel joint
+class Car : public Test
+{
+public:
+	Car()
+	{		
+		m_hz = 4.0f;
+		m_zeta = 0.7f;
+		m_speed = 50.0f;
+    }
+    }
+    
+    #ifdef ANDROID
+#include 'comm/android/callstack.h'
+#endif
+    
+    
+/*
+ * ServiceBase.h
+ *
+ *  Created on: 2013-6-20
+ *      Author: yerungui
+ */
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+      private:
+    int __TestFun1(int i);
+    
+     public: // Setters
+    
+    
+    {    return Node::fromYGNode(nodePtr);
 }
     
-    // Library to decode jpeg coefficients into an RGB image.
+        method(getMargin);
     
-    #include <stddef.h>
-#include <stdint.h>
+    template<typename... ARGS>
+inline void log(int level, const char* tag, const char* msg) noexcept {
+  __android_log_write(level, tag, msg);
+}
+    
+    
+    {}
