1d6fcaa29daa08450b17661c123751c9ce385f57
SUNsung <weatherius@gmail.com>
2018-04-24 01:26:14 +0300

    Rewrite [31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb] File in: 2018-04-24 01:26:13

--- ruby/31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb
+++ ruby/31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb
@@ -1,76 +1,130 @@
-  before do
-    sign_in user, scope: :user
-  end
-    
-    def process_args
-  until ARGV.empty?
-    if ARGV.first =~ %r{^-+t(?:est)?$} && ARGV.length > 1
-      ARGV.shift
-      $opt_test = ARGV.shift
-    elsif ARGV.first =~ %r{^-+h(?:elp)?$}
-      puts usage
-      exit 0
-    else
-      puts usage
-      exit 1
-    end
-  end
+
+        
+        # Just a slash
+Benchmark.ips do |x|
+  path = '/'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
 end
     
-          # bootstrap requires minimum precision of 8, see https://github.com/twbs/bootstrap-sass/issues/409
-      ::Sass::Script::Number.precision = [8, ::Sass::Script::Number.precision].max
-    end
+    # Create an array of vendored mimetype => [extensions]
+mimes = {}
+json = open('https://raw.githubusercontent.com/jshttp/mime-db/master/db.json').read
+data = JSON.parse(json)
+data.reject! { |mime, meta| meta['extensions'].nil? || meta['extensions'].empty? }
+data.each do |mime, meta|
+  # Normalize extensions and mime-types
+  mime = mime.downcase.strip
+  extensions = meta['extensions'].map { |e| e.downcase.strip }.compact
     
-      def test_font_helper_with_suffix_question
-    assert_match %r(url\(['']?/assets/.*eot\?.*['']?\)), @css
-  end
+      [before || '.',
+    after || '.',]
+end
     
-      find_files = ->(path) {
-    Find.find(Pathname.new(path).relative_path_from(Pathname.new Dir.pwd).to_s).map do |path|
-      path if File.file?(path)
-    end.compact
-  }
-    
-      subject { described_class.new(lambda {}) }
-    
-      context 'with custom session key' do
-    it 'denies requests with duplicate session cookies' do
-      mock_app do
-        use Rack::Protection::CookieTossing, :session_key => '_session'
-        run DummyApp
+      def present(payload)
+    if payload.is_a?(Hash)
+      payload = ActiveSupport::HashWithIndifferentAccess.new(payload)
+      MAIN_KEYS.each do |key|
+        return { :title => payload[key].to_s, :entries => present_hash(payload, key) } if payload.has_key?(key)
       end
     
-            post '/', :file => Rack::Test::UploadedFile.new(temp_file.path), :other => '<bar>'
-        expect(body).to eq('_escaped_params_tmp_file\nhello world\n&lt;bar&gt;')
-      ensure
-        File.unlink(temp_file.path)
+      module SortableTableHelper
+    # :call-seq:
+    #   sortable_column(attribute, default_direction = 'desc', name: attribute.humanize)
+    def sortable_column(attribute, default_direction = nil, options = nil)
+      if options.nil? && (options = Hash.try_convert(default_direction))
+        default_direction = nil
       end
+      default_direction ||= 'desc'
+      options ||= {}
+      name = options[:name] || attribute.humanize
+      selected = @table_sort_info[:attribute].to_s == attribute
+      if selected
+        direction = @table_sort_info[:direction]
+        new_direction = direction.to_s == 'desc' ? 'asc' : 'desc'
+        classes = 'selected #{direction}'
+      else
+        classes = ''
+        new_direction = default_direction
+      end
+      link_to(name, url_for(sort: '#{attribute}.#{new_direction}'), class: classes)
     end
   end
 end
 
     
-      before(:each) do
-    mock_app do
-      use Rack::Protection::HttpOrigin
-      run DummyApp
+      def load_event
+    @event = current_user.events.find(params[:id])
+  end
+end
+
+    
+        respond_to do |format|
+      if @user_credential.update_attributes(user_credential_params)
+        format.html { redirect_to user_credentials_path, notice: 'Your credential was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @user_credential.errors, status: :unprocessable_entity }
+      end
     end
   end
     
-      it 'allows passing on values in env' do
-    klass    = described_class
-    changer  = Struct.new(:app) do
-      def call(env)
-        env['foo.bar'] = 42
-        app.call(env)
-      end
-    end
-    detector = Struct.new(:app) do
-      def call(env)
-        app.call(env)
-      end
-    end
+    if defined?(ActionMailer)
+  class Devise::Mailer < Devise.parent_mailer.constantize
+    include Devise::Mailers::Helpers
     
-            def_node_matcher :on_body_of_reduce, <<-PATTERN
-          (block (send _recv {:reduce :inject} !sym) _blockargs $(begin ...))
-        PATTERN
+          # Forgets the given resource by deleting a cookie
+      def forget_me(resource)
+        scope = Devise::Mapping.find_scope!(resource)
+        resource.forget_me!
+        cookies.delete(remember_key(resource, scope), forget_cookie_values(resource))
+      end
+    
+    module Devise
+  module Controllers
+    # Create url helpers to be used with resource/scope configuration. Acts as
+    # proxies to the generated routes created by devise.
+    # Resource param can be a string or symbol, a class, or an instance object.
+    # Example using a :user resource:
+    #
+    #   new_session_path(:user)      => new_user_session_path
+    #   session_path(:user)          => user_session_path
+    #   destroy_session_path(:user)  => destroy_user_session_path
+    #
+    #   new_password_path(:user)     => new_user_password_path
+    #   password_path(:user)         => user_password_path
+    #   edit_password_path(:user)    => edit_user_password_path
+    #
+    #   new_confirmation_path(:user) => new_user_confirmation_path
+    #   confirmation_path(:user)     => user_confirmation_path
+    #
+    # Those helpers are included by default to ActionController::Base.
+    #
+    # In case you want to add such helpers to another class, you can do
+    # that as long as this new class includes both url_helpers and
+    # mounted_helpers. Example:
+    #
+    #     include Rails.application.routes.url_helpers
+    #     include Rails.application.routes.mounted_helpers
+    #
+    module UrlHelpers
+      def self.remove_helpers!
+        self.instance_methods.map(&:to_s).grep(/_(url|path)$/).each do |method|
+          remove_method method
+        end
+      end
+    
+      if record && record.respond_to?(:timedout?) && warden.authenticated?(scope) &&
+     options[:store] != false && !env['devise.skip_timeoutable']
+    last_request_at = warden.session(scope)['last_request_at']
+    
+            # Registers a SIGINT handler. This typically is called from {busy}.
+        # Callbacks are only registered once, so calling this multiple times
+        # with the same callback has no consequence.
+        def register(sig_callback)
+          @@mutex.synchronize do
+            registered << sig_callback
+            registered.uniq!
