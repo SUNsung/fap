e4cf8de2759caf23b9485ae87dcedea4ced4f4ab
SUNsung <weatherius@gmail.com>
2018-04-24 00:08:24 +0300

    Rewrite [56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py] File in: 2018-04-24 00:08:23

--- pyton/56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py
+++ pyton/56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py
@@ -1,227 +1,239 @@
 
         
-            def test_cache(self):
-        ydl = FakeYDL({
-            'cachedir': self.test_dir,
-        })
-        c = Cache(ydl)
-        obj = {'x': 1, 'y': ['ä', '\\a', True]}
-        self.assertEqual(c.load('test_cache', 'k.'), None)
-        c.store('test_cache', 'k.', obj)
-        self.assertEqual(c.load('test_cache', 'k2'), None)
-        self.assertFalse(_is_empty(self.test_dir))
-        self.assertEqual(c.load('test_cache', 'k.'), obj)
-        self.assertEqual(c.load('test_cache', 'y'), None)
-        self.assertEqual(c.load('test_cache2', 'k.'), None)
-        c.remove()
-        self.assertFalse(os.path.exists(self.test_dir))
-        self.assertEqual(c.load('test_cache', 'k.'), None)
+        import io
+import sys
+import re
     
-    from test.helper import assertRegexpMatches
+        def _get_ip(self, protocol):
+        if self._SKIP_SOCKS_TEST:
+            return '127.0.0.1'
     
-        def _real_extract(self, url):
-        playlist_id = self._match_id(url)
+    IGNORED_DIRS = [
+    '.git',
+    '.tox',
+]
     
-    
-def check_entry(line_num, segments):
-    # START Title
-    title = segments[index_title].upper()
-    if title.endswith(' API'):
-        add_error(line_num, 'Title should not contain 'API'')
-    # END Title
-    # START Description
-    # first character should be capitalized
-    char = segments[index_desc][0]
-    if char.upper() != char:
-        add_error(line_num, 'first character of description is not capitalized')
-    # last character should not punctuation
-    char = segments[index_desc][-1]
-    if char in punctuation:
-        add_error(line_num, 'description should not end with {}'.format(char))
-    desc_length = len(segments[index_desc])
-    if desc_length > 100:
-        add_error(line_num, 'description should not exceed 100 characters (currently {})'.format(desc_length))
-    # END Description
-    # START Auth
-    # values should conform to valid options only
-    auth = segments[index_auth]
-    if auth != 'No' and (not auth.startswith('`') or not auth.endswith('`')):
-        add_error(line_num, 'auth value is not enclosed with `backticks`')
-    if auth.replace('`', '') not in auth_keys:
-        add_error(line_num, '{} is not a valid Auth option'.format(auth))
-    # END Auth
-    # START HTTPS
-    # values should conform to valid options only
-    https = segments[index_https]
-    if https not in https_keys:
-        add_error(line_num, '{} is not a valid HTTPS option'.format(https))
-    # END HTTPS
-    # START CORS
-    # values should conform to valid options only
-    cors = segments[index_cors]
-    if cors not in cors_keys:
-        add_error(line_num, '{} is not a valid CORS option'.format(cors))
-    # END CORS
-    # START Link
-    # url should be wrapped in '[Go!]()' Markdown syntax
-    link = segments[index_link]
-    if not link.startswith('[Go!](http') or not link.endswith(')'):
-        add_error(line_num, 'link syntax should be '[Go!](LINK)'')
-    # END Link
-    
-            .. versionadded:: 0.11
-        '''
-        mappings = []
-        if len(mapping) == 1:
-            if hasattr(mapping[0], 'items'):
-                mappings.append(mapping[0].items())
-            else:
-                mappings.append(mapping[0])
-        elif len(mapping) > 1:
-            raise TypeError(
-                'expected at most 1 positional argument, got %d' % len(mapping)
-            )
-        mappings.append(kwargs.items())
-        for mapping in mappings:
-            for (key, value) in mapping:
-                if key.isupper():
-                    self[key] = value
-        return True
-    
-        app.logger.info('\n'.join(info))
-    
-        if rv is None:
-        fail('Could not parse changelog')
+        _TEST = {
+        'url': 'http://www.anitube.se/video/36621',
+        'md5': '59d0eeae28ea0bc8c05e7af429998d43',
+        'info_dict': {
+            'id': '36621',
+            'ext': 'mp4',
+            'title': 'Recorder to Randoseru 01',
+            'duration': 180.19,
+        },
+        'skip': 'Blocked in the US',
+    }
     
     
-def test_installed_module_paths(modules_tmpdir, modules_tmpdir_prefix,
-                                purge_module, site_packages, limit_loader):
-    site_packages.join('site_app.py').write(
-        'import flask\n'
-        'app = flask.Flask(__name__)\n'
-    )
-    purge_module('site_app')
+class AnySexIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)?anysex\.com/(?P<id>\d+)'
+    _TEST = {
+        'url': 'http://anysex.com/156592/',
+        'md5': '023e9fbb7f7987f5529a394c34ad3d3d',
+        'info_dict': {
+            'id': '156592',
+            'ext': 'mp4',
+            'title': 'Busty and sexy blondie in her bikini strips for you',
+            'description': 'md5:de9e418178e2931c10b62966474e1383',
+            'categories': ['Erotic'],
+            'duration': 270,
+            'age_limit': 18,
+        }
+    }
+    
+                return {
+                'id': video_id,
+                'title': json_data['title'],
+                'description': json_data.get('subtitle'),
+                'thumbnail': json_data.get('thumbnail_image', {}).get('file'),
+                'timestamp': parse_iso8601(json_data.get('publication_date')),
+                'duration': int_or_none(json_data.get('duration')),
+                'view_count': int_or_none(json_data.get('view_count')),
+                'formats': formats,
+            }
+
+    
+        def implements_to_string(cls):
+        cls.__unicode__ = cls.__str__
+        cls.__str__ = lambda x: x.__unicode__().encode('utf-8')
+        return cls
+    
+            app.config['SESSION_COOKIE_DOMAIN'] = rv
+        return rv
+    
+    import os
+import re
+import sys
+from datetime import date, datetime
+from subprocess import PIPE, Popen
     
     
-def test_existing_handler(app):
-    logging.root.addHandler(logging.StreamHandler())
-    assert app.logger.level == logging.NOTSET
-    assert not app.logger.handlers
+@pytest.fixture
+def httpbin_secure(httpbin_secure):
+    return prepare_url(httpbin_secure)
+
     
-        out = StringIO()
-    app = SuppressedFlask(__name__)
+    from .compat import is_py2, builtin_str, str
     
-            if not qop:
-            respdig = KD(HA1, '%s:%s' % (nonce, HA2))
-        elif qop == 'auth' or 'auth' in qop.split(','):
-            noncebit = '%s:%s:%s:%s:%s' % (
-                nonce, ncvalue, cnonce, 'auth', HA2
-            )
-            respdig = KD(HA1, noncebit)
-        else:
-            # XXX handle auth-int.
-            return None
+        def test_repr(self):
+        assert repr(self.lookup_dict) == '<lookup 'test'>'
     
-        def __setitem__(self, name, value):
-        '''Dict-like __setitem__ for compatibility with client code. Throws
-        exception if there is already a cookie of that name in the jar. In that
-        case, use the more explicit set() method instead.
-        '''
-        self.set(name, value)
-    
-    
-def test_idna_with_version_attribute(mocker):
-    '''Verify we're actually setting idna version when it should be available.'''
-    mocker.patch('requests.help.idna', new=VersionedPackage('2.6'))
-    assert info()['idna'] == {'version': '2.6'}
+        builtin_str = str
+    str = str
+    bytes = bytes
+    basestring = (str, bytes)
+    numeric_types = (int, float)
+    integer_types = (int,)
 
     
     
-class HTTPError(RequestException):
-    '''An HTTP error occurred.'''
+class InvalidHeader(RequestException, ValueError):
+    '''The header value provided was somehow invalid.'''
     
-    from .structures import LookupDict
     
-    In both cases, only 10% of the features are informative.
-'''
-import gc
-from time import time
-import numpy as np
+    {    # Server Error.
+    500: ('internal_server_error', 'server_error', '/o\\', '✗'),
+    501: ('not_implemented',),
+    502: ('bad_gateway',),
+    503: ('service_unavailable', 'unavailable'),
+    504: ('gateway_timeout',),
+    505: ('http_version_not_supported', 'http_version'),
+    506: ('variant_also_negotiates',),
+    507: ('insufficient_storage',),
+    509: ('bandwidth_limit_exceeded', 'bandwidth'),
+    510: ('not_extended',),
+    511: ('network_authentication_required', 'network_auth', 'network_authentication'),
+}
     
-            start = time.time()
-        func(X, n_jobs=-1)
-        multi_core.append(time.time() - start)
+        def initialize_options(self):
+        TestCommand.initialize_options(self)
+        try:
+            from multiprocessing import cpu_count
+            self.pytest_args = ['-n', str(cpu_count()), '--boxed']
+        except (ImportError, NotImplementedError):
+            self.pytest_args = ['-n', '1', '--boxed']
     
-    plt.figure('scikit-learn Ward's method benchmark results')
-plt.imshow(np.log(ratio), aspect='auto', origin='lower')
-plt.colorbar()
-plt.contour(ratio, levels=[1, ], colors='k')
-plt.yticks(range(len(n_features)), n_features.astype(np.int))
-plt.ylabel('N features')
-plt.xticks(range(len(n_samples)), n_samples.astype(np.int))
-plt.xlabel('N samples')
-plt.title('Scikit's time, in units of scipy time (log)')
-plt.show()
-
+                headers = prepared_request.headers
+            try:
+                del headers['Cookie']
+            except KeyError:
+                pass
     
-        An example with a long-untouched module that everyone has
-    >>> _linkcode_resolve('py', {'module': 'tty',
-    ...                          'fullname': 'setraw'},
-    ...                   package='tty',
-    ...                   url_fmt='http://hg.python.org/cpython/file/'
-    ...                           '{revision}/Lib/{package}/{path}#L{lineno}',
-    ...                   revision='xxxx')
-    'http://hg.python.org/cpython/file/xxxx/Lib/tty/tty.py#L18'
-    '''
+        with server as (host, port):
+        url = u'http://{0}:{1}'.format(host, port)
+        r = requests.get(url=url, allow_redirects=True)
+        assert r.status_code == 200
+        assert len(r.history) == 1
+        assert r.history[0].status_code == 301
+        assert redirect_request[0].startswith(b'GET /' + expected_path + b' HTTP/1.1')
+        assert r.url == u'{0}/{1}'.format(url, expected_path.decode('ascii'))
     
-        # decode the payload explicitly as UTF-8 since lxml is confused for some
-    # reason
-    with codecs.open(html_filename,'r','utf-8') as html_file:
-        html_content = html_file.read()
-    tree = ElementTree(lxml.html.document_fromstring(html_content))
-    i = 0
-    j = 0
-    for p in tree.findall('//p'):
-        content = p.text_content()
-        if len(content) < 100:
-            # skip paragraphs that are too short - probably too noisy and not
-            # representative of the actual language
-            continue
+    import os, json, imp
+here = os.path.abspath(os.path.dirname(__file__))
+proj_info = json.loads(open(os.path.join(here, PROJ_METADATA), encoding='utf-8').read())
+try:
+    README = open(os.path.join(here, 'README.rst'), encoding='utf-8').read()
+except:
+    README = ''
+CHANGELOG = open(os.path.join(here, 'CHANGELOG.rst'), encoding='utf-8').read()
+VERSION = imp.load_source('version', os.path.join(here, 'src/%s/version.py' % PACKAGE_NAME)).__version__
     
-        print('Decompressing %s' % ARCHIVE_NAME)
-    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
-        archive.extractall(path='.')
-    os.remove(ARCHIVE_NAME)
-
+    def mixcloud_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html(url, faker=True)
+    title = r1(r'<meta property='og:title' content='([^']*)'', html)
+    preview_url = r1(r'm-preview=\'([^\']+)\'', html)
+    preview = r1(r'previews(.*)\.mp3$', preview_url)
     
-    from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import Perceptron
-from sklearn.pipeline import Pipeline
-from sklearn.datasets import load_files
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
+        # ordered list of supported stream types / qualities on this site
+    # order: high quality -> low quality
+    stream_types = [
+        {'id': 'original'}, # contains an 'id' or 'itag' field at minimum
+        {'id': 'small'},
+    ]
     
-    Sentiment analysis can be casted as a binary text classification problem,
-that is fitting a linear classifier on features extracted from the text
-of the user messages so as to guess wether the opinion of the author is
-positive or negative.
+        #This is mainly for testing the M3U FFmpeg parser so I would ignore any non-m3u ones
+    stream_url = [i['url'] for i in html['streaming_url_list'] if i['is_default'] and i['type'] == 'hls'][0]
     
-    import numpy as np
-from sklearn.covariance import EllipticEnvelope
-from sklearn.svm import OneClassSVM
-import matplotlib.pyplot as plt
-import matplotlib.font_manager
-from sklearn.datasets import load_boston
+        See ``pvariance`` for arguments and other details.
     
-        def set_surface(self, surface):
-        self.surface = surface
+        def test_setitem(self):
+        n = 200
+        d = deque(range(n))
+        for i in range(n):
+            d[i] = 10 * i
+        self.assertEqual(list(d), [10*i for i in range(n)])
+        l = list(d)
+        for i in range(1-n, 0, -1):
+            d[i] = 7*i
+            l[i] = 7*i
+        self.assertEqual(list(d), l)
     
-    The outer product of the row and column label vectors shows a
-representation of the checkerboard structure.
+    def pi_cdecimal():
+    '''cdecimal'''
+    D = C.Decimal
+    lasts, t, s, n, na, d, da = D(0), D(3), D(3), D(1), D(0), D(0), D(24)
+    while s != lasts:
+        lasts = s
+        n, na = n+na, na+8
+        d, da = d+da, da+32
+        t = (t * n) / d
+        s += t
+    return s
     
-    The dataset is generated using the ``make_biclusters`` function, which
-creates a matrix of small values and implants bicluster with large
-values. The rows and columns are then shuffled and passed to the
-Spectral Co-Clustering algorithm. Rearranging the shuffled matrix to
-make biclusters contiguous shows how accurately the algorithm found
-the biclusters.
+            openedFile = None
+        try:
+            # If a string was passed then open a file with that name
+            if isinstance(file, (str, bytes)):
+                openedFile = file = open(file, 'wb')
+    
+    
+_tripdict = {}
+def triplet_to_rrggbb(rgbtuple):
+    '''Converts a (red, green, blue) tuple to #rrggbb.'''
+    global _tripdict
+    hexname = _tripdict.get(rgbtuple)
+    if hexname is None:
+        hexname = '#%02x%02x%02x' % rgbtuple
+        _tripdict[rgbtuple] = hexname
+    return hexname
+    
+        def test_varargs17_kw(self):
+        msg = r'^print\(\) takes at most 4 keyword arguments \(5 given\)$'
+        self.assertRaisesRegex(TypeError, msg,
+                               print, 0, sep=1, end=2, file=3, flush=4, foo=5)
+    
+                iter1 = reversed(range(start, end, step))
+            iter2 = pyrange_reversed(start, end, step)
+            test_id = 'reversed(range({}, {}, {}))'.format(start, end, step)
+            self.assert_iterators_equal(iter1, iter2, test_id, limit=100)
+    
+            class WNDCLASS(Structure):
+            _fields_ = [('style', c_uint),
+                        ('lpfnWndProc', WNDPROC),
+                        ('cbClsExtra', c_int),
+                        ('cbWndExtra', c_int),
+                        ('hInstance', HINSTANCE),
+                        ('hIcon', HICON),
+                        ('hCursor', HCURSOR),
+                        ('lpszMenuName', LPCTSTR),
+                        ('lpszClassName', LPCTSTR)]
+    
+        def test_print(self):
+        try:
+            fo = open(support.TESTFN, 'w')
+            print(False, True, file=fo)
+            fo.close()
+            fo = open(support.TESTFN, 'r')
+            self.assertEqual(fo.read(), 'False True\n')
+        finally:
+            fo.close()
+            os.remove(support.TESTFN)
+    
+        def test_no_comdat_folding(self):
+        # Issue 8847: In the PGO build, the MSVC linker's COMDAT folding
+        # optimization causes failures in code that relies on distinct
+        # function addresses.
+        class L(list): pass
+        with self.assertRaises(TypeError):
+            (3,) + L([1,2])
