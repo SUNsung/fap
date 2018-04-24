9c51b475478932ee1ab117d56a1b86687063b887
SUNsung <weatherius@gmail.com>
2018-04-25 02:02:30 +0300

    Rewrite [50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py] File in: 2018-04-25 02:02:29

--- pyton/50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py
+++ pyton/50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py
@@ -1,181 +1,76 @@
 
         
-        with open('update/versions.json', 'w') as jsonf:
-    json.dump(versions_info, jsonf, indent=4, sort_keys=True)
+            def __init__(self, groups, env=Environment(), **kwargs):
+        '''
+        :param groups: names of processor groups to be applied
+        :param env: Environment
+        :param kwargs: additional keyword arguments for processors
+    
+    
+def rst_filenames():
+    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
+        if '.tox' not in root:
+            for filename in fnmatch.filter(filenames, '*.rst'):
+                yield os.path.join(root, filename)
+    
+    
+@mock.patch('httpie.core.get_response')
+def test_timeout(get_response):
+    def error(msg, *args, **kwargs):
+        global error_msg
+        error_msg = msg % args
+    
+        # noinspection PyMethodOverriding
+    def get_auth(self, username, password):
+        return HTTPBasicAuth(username, password)
+    
+        def __getattr__(self, item):
+        return self[item]
+    
+    
+def load_json_preserve_order(s):
+    return json.loads(s, object_pairs_hook=OrderedDict)
+    
+    from httpie.compat import urlsplit
+from httpie.config import BaseConfigDict, DEFAULT_CONFIG_DIR
+from httpie.plugins import plugin_manager
+    
+    
+class MissingNonceTest(unittest.TestCase):
+    '''Tests for acme.errors.MissingNonce.'''
+    
+    # If true, keep warnings as 'system message' paragraphs in the built documents.
+#keep_warnings = False
+    
+    
+def huaban_download_board(url, output_dir, **kwargs):
+    kwargs['merge'] = False
+    board = extract_board_data(url)
+    output_dir = os.path.join(output_dir, board.title)
+    print_info(site_info, board.title, 'jpg', float('Inf'))
+    for pin in board.pins:
+        download_urls([pin.url], pin.id, pin.ext, float('Inf'),
+                      output_dir=output_dir, faker=True, **kwargs)
+    
+    site = Infoq()
+download = site.download_by_url
+download_playlist = site.download_by_url
 
     
-    versions_info = json.load(open('update/versions.json'))
-if 'signature' in versions_info:
-    del versions_info['signature']
+            self.title = match1(content,
+                            r'setup\[\'title\'\] = '([^']+)';')
     
-    import io
-import optparse
+    import os
+import mimetypes
     
-    options = helptext[helptext.index('  General Options:') + 19:]
-options = re.sub(r'(?m)^  (\w.+)$', r'## \1', options)
-options = '# OPTIONS\n' + options + '\n'
+        def current_loid_obj(self):
+        return self.stacked_proxy_safe_get(c, 'loid')
     
     
-def expect_value(self, got, expected, field):
-    if isinstance(expected, compat_str) and expected.startswith('re:'):
-        match_str = expected[len('re:'):]
-        match_rex = re.compile(match_str)
-    
-    import shutil
-    
-    
-class AudiomackAlbumIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?audiomack\.com/album/(?P<id>[\w/-]+)'
-    IE_NAME = 'audiomack:album'
-    _TESTS = [
-        # Standard album playlist
-        {
-            'url': 'http://www.audiomack.com/album/flytunezcom/tha-tour-part-2-mixtape',
-            'playlist_count': 15,
-            'info_dict':
-            {
-                'id': '812251',
-                'title': 'Tha Tour: Part 2 (Official Mixtape)'
-            }
-        },
-        # Album playlist ripped from fakeshoredrive with no metadata
-        {
-            'url': 'http://www.audiomack.com/album/fakeshoredrive/ppp-pistol-p-project',
-            'info_dict': {
-                'title': 'PPP (Pistol P Project)',
-                'id': '837572',
-            },
-            'playlist': [{
-                'info_dict': {
-                    'title': 'PPP (Pistol P Project) - 9. Heaven or Hell (CHIMACA) ft Zuse (prod by DJ FU)',
-                    'id': '837577',
-                    'ext': 'mp3',
-                    'uploader': 'Lil Herb a.k.a. G Herbo',
-                }
-            }],
-            'params': {
-                'playliststart': 9,
-                'playlistend': 9,
-            }
-        }
-    ]
-    
-    import time
-    
-        op.add_option('--density',
-                  dest='density', default=1 / 3,
-                  help='Density used by the sparse random projection.'
-                       ' ('auto' or float (0.0, 1.0]')
-    
-        package is the name of the root module of the package
-    
-        # TASK: print the cross-validated scores for the each parameters set
-    # explored by the grid search
-    
-    '''
-# Author: Olivier Grisel <olivier.grisel@ensta.org>
-# License: Simplified BSD
-    
-    for f in solutions:
-    if not f.endswith('.py'):
-        continue
-    
-            # update decision surface if already fitted.
-        self.refit()
-    
-    from sklearn.datasets import make_biclusters
-from sklearn.datasets import samples_generator as sg
-from sklearn.cluster.bicluster import SpectralCoclustering
-from sklearn.metrics import consensus_score
-    
-    plt.xlabel('n_features / n_samples')
-plt.ylabel('Classification accuracy')
-    
-        def setUp(self):
-        from acme.errors import BadNonce
-        self.error = BadNonce(nonce='xxx', error='error')
-    
-    # If true, '(C) Copyright ...' is shown in the HTML footer. Default is True.
-#html_show_copyright = True
-    
-    # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.0'
-    
-        @mock.patch('certbot.display.enhancements.util')
-    def test_redirect(self, mock_util):
-        mock_util().menu.return_value = (display_util.OK, 1)
-        self.assertTrue(self._call('redirect'))
-    
-            add_accept_handler(listener, accept_callback)
-        self.client_stream = IOStream(socket.socket())
-        self.addCleanup(self.client_stream.close)
-        yield [self.client_stream.connect(('127.0.0.1', port)),
-               event.wait()]
-        self.io_loop.remove_handler(listener)
-        listener.close()
-    
-    
-class EntryHandler(BaseHandler):
-    def get(self, slug):
-        entry = db.Query(Entry).filter('slug =', slug).get()
-        if not entry:
-            raise tornado.web.HTTPError(404)
-        self.render('entry.html', entry=entry)
-    
-            start_pos = 0
-        if marker:
-            start_pos = bisect.bisect_right(object_names, marker, start_pos)
-        if prefix:
-            start_pos = bisect.bisect_left(object_names, prefix, start_pos)
-    
-    # Most of our tests depend on IOLoop, which is not usable on app engine.
-# Run the tests that work, and check that everything else is at least
-# importable (via tornado.test.import_test)
-TEST_MODULES = [
-    'tornado.httputil.doctests',
-    'tornado.iostream.doctests',
-    'tornado.util.doctests',
-    #'tornado.test.auth_test',
-    #'tornado.test.concurrent_test',
-    #'tornado.test.curl_httpclient_test',
-    'tornado.test.escape_test',
-    #'tornado.test.gen_test',
-    #'tornado.test.httpclient_test',
-    #'tornado.test.httpserver_test',
-    'tornado.test.httputil_test',
-    'tornado.test.import_test',
-    #'tornado.test.ioloop_test',
-    #'tornado.test.iostream_test',
-    'tornado.test.locale_test',
-    #'tornado.test.netutil_test',
-    #'tornado.test.log_test',
-    'tornado.test.options_test',
-    #'tornado.test.process_test',
-    #'tornado.test.simple_httpclient_test',
-    #'tornado.test.stack_context_test',
-    'tornado.test.template_test',
-    #'tornado.test.testing_test',
-    #'tornado.test.twisted_test',
-    'tornado.test.util_test',
-    #'tornado.test.web_test',
-    #'tornado.test.websocket_test',
-    #'tornado.test.wsgi_test',
-]
-    
-        def test_permanent_redirect(self):
-        self.check_url('/redirect/hello?status=301', expected_status=301)
-    
-    api('organiclisting',       OrganicListingJsonTemplate)
-api('subreddittraffic', TrafficJsonTemplate)
-api('takedownpane', TakedownJsonTemplate)
-api('policyview', PolicyViewJsonTemplate)
-    
-            Responds with a 120x50 `image/png` which should be displayed
-        to the user.
-    
-    
-class ErrorController(RedditController):
-    '''Generates error documents as and when they are required.
-    
-            response.content_type = 'application/json'
-        return json.dumps(g.versions, sort_keys=True, indent=4)
+class APIv1GoldController(OAuth2OnlyController):
+    def _gift_using_creddits(self, recipient, months=1, thing_fullname=None,
+            proxying_for=None):
+        with creddits_lock(c.user):
+            if not c.user.employee and c.user.gold_creddits < months:
+                err = RedditError('INSUFFICIENT_CREDDITS')
+                self.on_validation_error(err)
