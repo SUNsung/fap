4f89f510ce1f4f52b67cd5da7ef5592cdb4547aa
SUNsung <weatherius@gmail.com>
2018-04-24 08:19:26 +0300

    Rewrite [55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp] File in: 2018-04-24 08:19:25

--- cccccc/55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp
+++ cccccc/55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp
@@ -1,180 +1,127 @@
 
         
-        
-void Base::Call(const std::string& method, const base::ListValue& arguments,
-                content::RenderFrameHost* rvh) {
-  NOTREACHED() << 'Uncatched call in Base'
-               << ' method:' << method
-               << ' arguments:' << arguments;
+        class SILDebuggerClient;
+    
+      // Imports from -import-name such as Playground auxiliary sources are treated
+  // specially by applying import depth 0.
+  llvm::StringSet<> auxImports;
+  for (StringRef moduleName :
+       invocation.getFrontendOptions().ImplicitImportModuleNames)
+    auxImports.insert(moduleName);
+    
+      /// The list of regular users from the last run of the checker.
+  llvm::SmallVector<SILInstruction *, 16> RegularUsers;
+    
+    /// Index the given module and store the results to \p indexStorePath.
+///
+/// \param module The module to index.
+///
+/// \param indexUnitTokens A list of unique identifiers for the index units to
+/// be written. This may either be one unit per source file of \p module, or it
+/// may be a single unit, in which case all the index information will be
+/// combined into a single unit.
+///
+/// \param moduleUnitToken A unique identifier for this module unit in the form
+/// of a file path. Only used if \p indexUnitTokens are specified for each
+/// source file, otherwise the single \p indexUnitTokens value is used instead.
+///
+/// \param indexStorePath The location to write the indexing data to.
+///
+/// \param indexSystemModules If true, emit index data for imported serialized
+/// swift system modules.
+///
+/// \param isDebugCompilation true for non-optimized compiler invocation.
+///
+/// \param targetTriple The target for this compilation.
+///
+/// \param dependencyTracker The set of dependencies seen while building.
+bool indexAndRecord(ModuleDecl *module, ArrayRef<std::string> indexUnitTokens,
+                    StringRef moduleUnitToken, StringRef indexStorePath,
+                    bool indexSystemModules, bool isDebugCompilation,
+                    StringRef targetTriple,
+                    const DependencyTracker &dependencyTracker);
+// FIXME: indexUnitTokens could be StringRef, but that creates an impedance
+// mismatch in the caller.
+    
+      size_t NumChildren;
+    
+    SILDebugScope::SILDebugScope(SILLocation Loc, SILFunction *SILFn,
+                             const SILDebugScope *ParentScope ,
+                             const SILDebugScope *InlinedCallSite)
+    : Loc(Loc), InlinedCallSite(InlinedCallSite) {
+  if (ParentScope)
+    Parent = ParentScope;
+  else {
+    assert(SILFn && 'no parent provided');
+    Parent = SILFn;
+  }
 }
     
+    using clang::index::SymbolKind;
+using clang::index::SymbolLanguage;
+using clang::index::SymbolSubKind;
+using clang::index::SymbolProperty;
+using clang::index::SymbolPropertySet;
+using clang::index::SymbolRole;
+using clang::index::SymbolRoleSet;
+using clang::index::SymbolRelation;
+using clang::index::SymbolInfo;
     
-    {  DISALLOW_COPY_AND_ASSIGN(Base);
-};
+    #ifndef SWIFT_SYNTAX_REFERENCES_H
+#define SWIFT_SYNTAX_REFERENCES_H
+    
+    #ifndef TESSERACT_CCUTIL_QRSEQUENCE_H_
+#define TESSERACT_CCUTIL_QRSEQUENCE_H_
+    
+      // Setup the map for the given indexed_features that have been indexed by
+  // feature_map. After use, use Set(..., false) to reset to the initial state
+  // as this is faster than calling Init for sparse spaces.
+  void Set(const GenericVector<int>& indexed_features,
+           int canonical_count, bool value);
     
     
-    {}
+#ifndef TESSERACT_TEXTORD_BLOBGRID_H_
+#define TESSERACT_TEXTORD_BLOBGRID_H_
     
-       void Call(const std::string& method,
-                    const base::ListValue& arguments) override;
-   void CallSync(const std::string& method,
-                        const base::ListValue& arguments,
-                        base::ListValue* result) override;
+      /**
+   * In extreme cases only, usually with a subclass of Thresholder, it
+   * is possible to provide a different Thresholder. The Thresholder may
+   * be preloaded with an image, settings etc, or they may be set after.
+   * Note that Tesseract takes ownership of the Thresholder and will
+   * delete it when it it is replaced or the API is destructed.
+   */
+  void SetThresholder(ImageThresholder* thresholder) {
+    delete thresholder_;
+    thresholder_ = thresholder;
+    ClearResults();
+  }
     
-    namespace base {
-class ListValue;
+        /**
+     * Starts a new document with the given title.
+     * This clears the contents of the output data.
+     * Title should use UTF-8 encoding.
+     */
+    bool BeginDocument(const char* title);
+    
+    #include 'ATen/Tensor.h'
+#include 'ATen/Context.h'
+    
+    bool cudnn_is_acceptable(const Tensor& self) {
+  if (!globalContext().userEnabledCuDNN()) return false;
+  if (!self.is_cuda()) return false;
+  auto st = self.type().scalarType();
+  if (!(st == kDouble || st == kFloat || st == kHalf)) return false;
+  if (!AT_CUDNN_ENABLED()) return false;
+  // NB: In the old Python code, there was also a test to see if the
+  // cuDNN library was actually dynamically linked or not.  I'm not
+  // sure if we can actually test this.
+  return true;
 }
     
-    
-    {  *y = CalculateMenuYPosition(&screen_rect, &menu_req, NULL, *y);
-}
-    
-    
-    {  base::ListValue args;
-  dispatcher_host()->SendEvent(this, 'click', args);
-}
-    
-    
-    {  DECLARE_EXTENSION_FUNCTION('nw.App.clearCache', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwAppClearCacheFunction);
-};
-    
-    
-    {    ui::Clipboard* clipboard_;
-    std::string error_;
-  };
-    
-    #include 'extensions/browser/extension_function.h'
-    
-        UINT createDeviceFlags = 0;
-    
-        f3coeff[1] = 2*D[1][2] - 2*D[1][4] + 2*D[2][1] + 2*D[2][5] + 2*D[2][9] - 2*D[4][1] - 2*D[4][5] - 2*D[4][9] + 2*D[5][2] - 2*D[5][4] + 2*D[9][2] - 2*D[9][4]; // constant term
-    f3coeff[2] = 2*D[1][6] + 2*D[1][8] + 4*D[2][3] + 4*D[2][7] + 4*D[3][2] + 4*D[3][4] + 4*D[4][3] + 4*D[4][7] - 2*D[5][6] - 2*D[5][8] + 2*D[6][1] - 2*D[6][5] - 2*D[6][9] + 4*D[7][2] + 4*D[7][4] + 2*D[8][1] - 2*D[8][5] - 2*D[8][9] - 2*D[9][6] - 2*D[9][8]; // s1^2  * s2
-    f3coeff[3] = 8*D[2][2] - 8*D[3][3] - 8*D[4][4] + 8*D[6][6] + 8*D[7][7] - 8*D[8][8]; // s1 * s2
-    f3coeff[4] = 4*D[1][8] - 4*D[1][6] + 8*D[2][3] + 8*D[2][7] + 8*D[3][2] - 8*D[3][4] - 8*D[4][3] - 8*D[4][7] - 4*D[5][6] + 4*D[5][8] - 4*D[6][1] - 4*D[6][5] + 4*D[6][9] + 8*D[7][2] - 8*D[7][4] + 4*D[8][1] + 4*D[8][5] - 4*D[8][9] + 4*D[9][6] - 4*D[9][8]; // s1 * s3
-    f3coeff[5] = 4*D[1][3] - 4*D[1][7] + 8*D[2][6] + 8*D[2][8] + 4*D[3][1] + 4*D[3][5] - 4*D[3][9] - 8*D[4][6] - 8*D[4][8] + 4*D[5][3] - 4*D[5][7] + 8*D[6][2] - 8*D[6][4] - 4*D[7][1] - 4*D[7][5] + 4*D[7][9] + 8*D[8][2] - 8*D[8][4] - 4*D[9][3] + 4*D[9][7]; // s2 * s3
-    f3coeff[6] = 4*D[1][1] - 4*D[5][5] + 4*D[5][9] + 8*D[6][6] + 8*D[6][8] + 8*D[8][6] + 8*D[8][8] + 4*D[9][5] - 4*D[9][9]; // s2^2 * s3
-    f3coeff[7] = 2*D[5][6] - 2*D[1][8] - 2*D[1][6] + 2*D[5][8] - 2*D[6][1] + 2*D[6][5] - 2*D[6][9] - 2*D[8][1] + 2*D[8][5] - 2*D[8][9] - 2*D[9][6] - 2*D[9][8]; // s2^3
-    f3coeff[8] = 6*D[3][9] - 6*D[1][7] - 6*D[3][1] - 6*D[3][5] - 6*D[1][3] - 6*D[5][3] - 6*D[5][7] - 6*D[7][1] - 6*D[7][5] + 6*D[7][9] + 6*D[9][3] + 6*D[9][7]; // s1 * s3^2
-    f3coeff[9] = 2*D[1][3] + 2*D[1][7] + 4*D[2][6] - 4*D[2][8] + 2*D[3][1] + 2*D[3][5] + 2*D[3][9] - 4*D[4][6] + 4*D[4][8] + 2*D[5][3] + 2*D[5][7] + 4*D[6][2] - 4*D[6][4] + 2*D[7][1] + 2*D[7][5] + 2*D[7][9] - 4*D[8][2] + 4*D[8][4] + 2*D[9][3] + 2*D[9][7]; // s1
-    f3coeff[10] = 8*D[2][2] - 4*D[1][5] - 4*D[1][1] - 8*D[2][4] - 8*D[4][2] + 8*D[4][4] - 4*D[5][1] - 4*D[5][5] + 4*D[9][9]; // s3
-    f3coeff[11] = 2*D[1][6] + 2*D[1][8] - 4*D[2][3] + 4*D[2][7] - 4*D[3][2] + 4*D[3][4] + 4*D[4][3] - 4*D[4][7] + 2*D[5][6] + 2*D[5][8] + 2*D[6][1] + 2*D[6][5] + 2*D[6][9] + 4*D[7][2] - 4*D[7][4] + 2*D[8][1] + 2*D[8][5] + 2*D[8][9] + 2*D[9][6] + 2*D[9][8]; // s2
-    f3coeff[12] = 6*D[6][9] - 6*D[1][8] - 6*D[5][6] - 6*D[5][8] - 6*D[6][1] - 6*D[6][5] - 6*D[1][6] - 6*D[8][1] - 6*D[8][5] + 6*D[8][9] + 6*D[9][6] + 6*D[9][8]; // s2 * s3^2
-    f3coeff[13] = 2*D[1][2] - 2*D[1][4] + 2*D[2][1] - 2*D[2][5] - 2*D[2][9] + 4*D[3][6] - 4*D[3][8] - 2*D[4][1] + 2*D[4][5] + 2*D[4][9] - 2*D[5][2] + 2*D[5][4] + 4*D[6][3] + 4*D[6][7] + 4*D[7][6] - 4*D[7][8] - 4*D[8][3] - 4*D[8][7] - 2*D[9][2] + 2*D[9][4]; // s1^2
-    f3coeff[14] = 6*D[1][4] - 6*D[1][2] - 6*D[2][1] - 6*D[2][5] + 6*D[2][9] + 6*D[4][1] + 6*D[4][5] - 6*D[4][9] - 6*D[5][2] + 6*D[5][4] + 6*D[9][2] - 6*D[9][4]; // s3^2
-    f3coeff[15] = 2*D[1][4] - 2*D[1][2] - 2*D[2][1] + 2*D[2][5] - 2*D[2][9] - 4*D[3][6] - 4*D[3][8] + 2*D[4][1] - 2*D[4][5] + 2*D[4][9] + 2*D[5][2] - 2*D[5][4] - 4*D[6][3] + 4*D[6][7] + 4*D[7][6] + 4*D[7][8] - 4*D[8][3] + 4*D[8][7] - 2*D[9][2] + 2*D[9][4]; // s2^2
-    f3coeff[16] = 4*D[1][1] + 4*D[1][5] - 4*D[1][9] + 4*D[5][1] + 4*D[5][5] - 4*D[5][9] - 4*D[9][1] - 4*D[9][5] + 4*D[9][9]; // s3^3
-    f3coeff[17] = 4*D[2][9] - 4*D[1][4] - 4*D[2][1] - 4*D[2][5] - 4*D[1][2] + 8*D[3][6] + 8*D[3][8] - 4*D[4][1] - 4*D[4][5] + 4*D[4][9] - 4*D[5][2] - 4*D[5][4] + 8*D[6][3] + 8*D[6][7] + 8*D[7][6] + 8*D[7][8] + 8*D[8][3] + 8*D[8][7] + 4*D[9][2] + 4*D[9][4]; // s1 * s2 * s3
-    f3coeff[18] = 4*D[2][6] - 2*D[1][7] - 2*D[1][3] + 4*D[2][8] - 2*D[3][1] + 2*D[3][5] - 2*D[3][9] + 4*D[4][6] + 4*D[4][8] + 2*D[5][3] + 2*D[5][7] + 4*D[6][2] + 4*D[6][4] - 2*D[7][1] + 2*D[7][5] - 2*D[7][9] + 4*D[8][2] + 4*D[8][4] - 2*D[9][3] - 2*D[9][7]; // s1 * s2^2
-    f3coeff[19] = 4*D[1][9] - 4*D[1][1] + 8*D[3][3] + 8*D[3][7] + 4*D[5][5] + 8*D[7][3] + 8*D[7][7] + 4*D[9][1] - 4*D[9][9]; // s1^2 * s3
-    f3coeff[20] = 2*D[1][3] + 2*D[1][7] + 2*D[3][1] - 2*D[3][5] - 2*D[3][9] - 2*D[5][3] - 2*D[5][7] + 2*D[7][1] - 2*D[7][5] - 2*D[7][9] - 2*D[9][3] - 2*D[9][7]; // s1^3
-    
-    void dAB(cv::InputArray A, InputArray B, OutputArray dABdA, OutputArray dABdB);
-    
-        if (new_width * new_cn != total_width)
-        CV_Error(cv::Error::BadNumChannels, 'The total width is not divisible by the new number of channels');
-    
-        static void* GetProcAddress (const char* name)
-    {
-        static void* h = NULL;
-        if (!h)
-        {
-            h = dlopen('libclAmdBlas.so', RTLD_LAZY | RTLD_GLOBAL);
-            if (!h)
-                return NULL;
-        }
-    }
-    
-            std::unique_ptr<ZeroCopyInputStream> raw_input(new FileInputStream(p_fd));
-        std::unique_ptr<CodedInputStream> coded_input(
-            new CodedInputStream(raw_input.get()));
-        // Allows protobuf library versions < 3.2.0 to parse messages greater than 64MB.
-        coded_input->SetTotalBytesLimit(INT_MAX, INT_MAX);
-        std::unique_ptr<ModelProto> modelProto(new ModelProto);
-        bool result = modelProto->ParseFromCodedStream(coded_input.get());
-        coded_input.reset();
-        raw_input.reset();
-        if (!result)
-        {
-            return Status(ONNX, INVALID_PROTOBUF, 'Protobuf parsing failed.');
-        }
-    
-        const std::string& OperatorSchema::Domain() const
-    {
-        return m_opSignature.Domain();
-    }
-    
-        std::function<void(OperatorSchemaSetter&)> RNNDocGeneratorInputInitialH() {
-        return [=](OperatorSchemaSetter& schema) {
-            schema.Input('initial_h',
-                'Optional initial value of the hidden. If not specified - assumed '
-                'to be 0. It has shape `[num_directions, batch_size, hidden_size]`.', 'T', true /*optional*/);
-        };
-    }
-    
-        // Taken from ONNX
-    REGISTER_OPERATOR_SCHEMA(Split)
-        .Description('Split a tensor into a list of tensors, along the specified 'axis'. '
-            'The lengths of the split can be specified using argument 'axis' or '
-            'optional second input blob to the operator. Otherwise, the tensor is split '
-            'to equal sized parts.')
-        .Input('input', 'The tensor to split', 'T')
-        .Input('split', 'Optional list of output lengths (see also arg 'split')', 'T')
-        .Output('output', 'A list of output tensors', 'T')
-        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
-            'Constrain input and output types to float tensors.')
-        .Attr('axis', 'Which axis to split on', AttrType::AttributeProto_AttributeType_INT)
-        .Attr('split', 'Number of tensors to output.', AttrType::AttributeProto_AttributeType_INTS);
-    
-    
-    {
-    {            ForwardPropS(sliceOutputValue, sliceUnnormedPrior, sliceMean, sliceLogstddev, sliceFeature,
-                         slicePrior, sliceStddev, sliceNormedDeviationVectors, sliceNormedDeviation, slicePosterior, *m_temp);
-        }
-        else // should not reach the code since validation should fail already
-            RuntimeError('GMMLogLikelihoodNode: UnnormedPrior should either have same number of columns as the features or have only one column.');
-    }
-    
-    
-    {
-    {
-    {    template <class matrixbase>
-    void tomatrix(msra::math::ssematrix<matrixbase> &to) const
-    {
-        for (size_t j = 0; j < ncols; j++)
-            for (size_t i = 0; i < nrows; i++)
-                to(i, j) = (float) (*this)(i, j);
-    }
-};
-};
-}; // namespaces
-    
-    extern JSClass  *jsb_cocos2d_Physics3DComponent_class;
-extern JSObject *jsb_cocos2d_Physics3DComponent_prototype;
-    
-    
-    {        ok &= luaval_to_int32(tolua_S, 2,(int *)&arg0, 'cc.PhysicsBody:setContactTestBitmask');
-        if(!ok)
-        {
-            tolua_error(tolua_S,'invalid arguments in function 'lua_cocos2dx_physics_PhysicsBody_setContactTestBitmask'', nullptr);
-            return 0;
-        }
-        cobj->setContactTestBitmask(arg0);
-        lua_settop(tolua_S, 1);
-        return 1;
-    }
-    luaL_error(tolua_S, '%s has wrong number of arguments: %d, was expecting %d \n', 'cc.PhysicsBody:setContactTestBitmask',argc, 1);
-    return 0;
-    
-    
-    
-    			b2Body* prevBody = ground;
-			for (int32 i = 0; i < e_count; ++i)
-			{
-				b2BodyDef bd;
-				bd.type = b2_dynamicBody;
-				bd.position.Set(-4.5f + 1.0f * i, 5.0f);
-				b2Body* body = m_world->CreateBody(&bd);
-				body->CreateFixture(&fd);
-    }
+    #define THCPDoubleStorage_CData(obj)  (obj)->cdata
+#define THCPFloatStorage_CData(obj)   (obj)->cdata
+#define THCPLongStorage_CData(obj)    (obj)->cdata
+#define THCPIntStorage_CData(obj)     (obj)->cdata
+#define THCPShortStorage_CData(obj)   (obj)->cdata
+#define THCPCharStorage_CData(obj)    (obj)->cdata
+#define THCPByteStorage_CData(obj)    (obj)->cdata
