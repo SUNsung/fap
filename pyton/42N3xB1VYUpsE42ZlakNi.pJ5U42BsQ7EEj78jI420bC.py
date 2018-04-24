aaf5fa448ae24fe5e970f9742489a5d7abad63fb
SUNsung <weatherius@gmail.com>
2018-04-24 19:40:12 +0300

    Rewrite [42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py] File in: 2018-04-24 19:40:11

--- pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
+++ pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
@@ -1,142 +1,61 @@
 
         
-        
-def decode_args(args, stdin_encoding):
-    '''
-    Convert all bytes args to str
-    by decoding them using stdin encoding.
-    
-            Keyword:                   'bold #004461',   # class: 'k'
-        Keyword.Constant:          'bold #004461',   # class: 'kc'
-        Keyword.Declaration:       'bold #004461',   # class: 'kd'
-        Keyword.Namespace:         'bold #004461',   # class: 'kn'
-        Keyword.Pseudo:            'bold #004461',   # class: 'kp'
-        Keyword.Reserved:          'bold #004461',   # class: 'kr'
-        Keyword.Type:              'bold #004461',   # class: 'kt'
+                :param namespace: a configuration namespace
+        :param lowercase: a flag indicating if the keys of the resulting
+                          dictionary should be lowercase
+        :param trim_namespace: a flag indicating if the keys of the resulting
+                          dictionary should not include the namespace
     
     
-def dispatch_hook(key, hooks, hook_data, **kwargs):
-    '''Dispatches a hook dictionary on a given piece of data.'''
-    hooks = hooks or dict()
-    hooks = hooks.get(key)
-    if hooks:
-        if hasattr(hooks, '__call__'):
-            hooks = [hooks]
-        for hook in hooks:
-            _hook_data = hook(hook_data, **kwargs)
-            if _hook_data is not None:
-                hook_data = _hook_data
-    return hook_data
+def render_template(template_name_or_list, **context):
+    '''Renders a template from the template folder with the given
+    context.
+    
+        def dispatch_request(self, *args, **kwargs):
+        meth = getattr(self, request.method.lower(), None)
+    
+        monkeypatch.setitem(sys.modules, 'ssl_context', ssl_context)
+    ctx = run_command.make_context('run', ['--cert', 'ssl_context'])
+    assert ctx.params['cert'] is ssl_context
+    
+        app.testing = False
+    stream = StringIO()
+    rv = client.get('/', errors_stream=stream)
+    assert rv.status_code == 500
+    assert rv.data
+    err = stream.getvalue()
+    assert 'Exception on / [GET]' in err
+    assert 'Exception: test' in err
 
     
-        def prepare_content_length(self, body):
-        '''Prepare Content-Length header based on request method and body'''
-        if body is not None:
-            length = super_len(body)
-            if length:
-                # If length exists, set it. Otherwise, we fallback
-                # to Transfer-Encoding: chunked.
-                self.headers['Content-Length'] = builtin_str(length)
-        elif self.method not in ('GET', 'HEAD') and self.headers.get('Content-Length') is None:
-            # Set Content-Length to 0 for methods that can have a body
-            # but don't provide one. (i.e. not GET or HEAD)
-            self.headers['Content-Length'] = '0'
+    # Scrapy version
+import pkgutil
+__version__ = pkgutil.get_data(__package__, 'VERSION').decode('ascii').strip()
+version_info = tuple(int(v) if v.isdigit() else v
+                     for v in __version__.split('.'))
+del pkgutil
     
-        get_item_parameters = pytest.mark.parametrize(
-        'key, value', (
-            ('bad_gateway', 502),
-            ('not_a_key', None)
-        )
-    )
+        def __enter__(self):
+        from scrapy.utils.test import get_testenv
+        pargs = [sys.executable, '-u', '-m', 'scrapy.utils.benchserver']
+        self.proc = subprocess.Popen(pargs, stdout=subprocess.PIPE,
+                                     env=get_testenv())
+        self.proc.stdout.readline()
     
-        def test_server_finishes_when_no_connections(self):
-        '''the server thread exits even if there are no connections'''
-        server = Server.basic_response_server()
-        with server:
-            pass
+            infos = []
+        if not self.wasSuccessful():
+            write('FAILED')
+            failed, errored = map(len, (self.failures, self.errors))
+            if failed:
+                infos.append('failures=%d' % failed)
+            if errored:
+                infos.append('errors=%d' % errored)
+        else:
+            write('OK')
     
-        # Informational.
-    100: ('continue',),
-    101: ('switching_protocols',),
-    102: ('processing',),
-    103: ('checkpoint',),
-    122: ('uri_too_long', 'request_uri_too_long'),
-    200: ('ok', 'okay', 'all_ok', 'all_okay', 'all_good', '\\o/', '✓'),
-    201: ('created',),
-    202: ('accepted',),
-    203: ('non_authoritative_info', 'non_authoritative_information'),
-    204: ('no_content',),
-    205: ('reset_content', 'reset'),
-    206: ('partial_content', 'partial'),
-    207: ('multi_status', 'multiple_status', 'multi_stati', 'multiple_stati'),
-    208: ('already_reported',),
-    226: ('im_used',),
-    
-    
-@pytest.mark.parametrize(
-    'value, expected', (
-        ('foo='is a fish', bar='as well'', {'foo': 'is a fish', 'bar': 'as well'}),
-        ('key_without_value', {'key_without_value': None})
-    ))
-def test_parse_dict_header(value, expected):
-    assert parse_dict_header(value) == expected
-    
-        close_server = threading.Event()
-    server = Server(response_handler, wait_to_close_event=close_server)
-    
-    
-def _serf_client():
-    env = EnvironmentConfig()
-    return SerfClient(host=env.host, port=env.port, rpc_auth=env.auth_key)
-    
-        # If we get to this point, we know that any included ranges are valid.
-    # If the caller is prepared to handle them, all is well.
-    # Otherwise we treat it as a parse failure.
-    if not allow_ranges and '[' in host:
-        raise AnsibleParserError('Detected range in host but was asked to ignore ranges')
-    
-    from ansible.plugins.terminal import TerminalBase
-from ansible.errors import AnsibleConnectionFailure
-    
-        for data_format in ['channels_first', 'channels_last']:
-        if data_format == 'channels_first':
-            shape = (3, 5, 5)
-            target_shape = (5, 5, 3)
-            prev_shape = (2, 3, 2)
-            flip = lambda x: np.flip(np.flip(x, axis=2), axis=3)
-            transpose = lambda x: np.transpose(x, (0, 2, 3, 1))
-            target_data_format = 'channels_last'
-        elif data_format == 'channels_last':
-            shape = (5, 5, 3)
-            target_shape = (3, 5, 5)
-            prev_shape = (2, 2, 3)
-            flip = lambda x: np.flip(np.flip(x, axis=1), axis=2)
-            transpose = lambda x: np.transpose(x, (0, 3, 1, 2))
-            target_data_format = 'channels_first'
-    
-    
-@keras_test
-def test_temporal_classification_functional():
-    '''
-    Classify temporal sequences of float numbers
-    of length 3 into 2 classes using
-    single layer of GRU units and softmax applied
-    to the last activations of the units
-    '''
-    np.random.seed(1337)
-    (x_train, y_train), (x_test, y_test) = get_test_data(num_train=200,
-                                                         num_test=20,
-                                                         input_shape=(3, 4),
-                                                         classification=True,
-                                                         num_classes=2)
-    y_train = to_categorical(y_train)
-    y_test = to_categorical(y_test)
-    
-    
-def test_softmax_valid():
-    '''Test using a reference implementation of softmax.
-    '''
-    def softmax(values):
-        m = np.max(values)
-        e = np.exp(values - m)
-        return e / np.sum(e)
+        def run(self, args, opts):
+        if len(args) < 1:
+            raise UsageError()
+        elif len(args) > 1:
+            raise UsageError('running 'scrapy crawl' with more than one spider is no longer supported')
+        spname = args[0]
