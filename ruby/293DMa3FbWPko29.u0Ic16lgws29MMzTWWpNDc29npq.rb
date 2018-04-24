dd7cd411f28b728b84a5281ac14840ce7cb54ca7
SUNsung <weatherius@gmail.com>
2018-04-24 06:58:21 +0300

    Rewrite [293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb] File in: 2018-04-24 06:58:20

--- ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
+++ ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
@@ -1,118 +1,49 @@
 
         
-            it 'should have a way of getting the service configurations' do
-      configs = key.service_configs_for(Spaceship::Portal::Key::MUSIC_KIT_ID)
-      expect(configs).to be_instance_of(Array)
-      expect(configs.sample).to be_instance_of(Hash)
-      expect(configs.first['identifier']).to eq('music.com.snatchev.test')
-    end
+          class FeatureTopicUsers < Jobs::Base
     
-            expect(result).to eq('git svn info | grep Revision | egrep -o '[0-9]+'')
-        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('git svn info | grep Revision | egrep -o '[0-9]+'')
-      end
-    end
+          raise Discourse::InvalidParameters.new(:to_address) unless args[:to_address].present?
     
-        def merge!(hash)
-      return super unless hash.is_a? Hash
-      hash.assert_valid_keys URI::Generic::COMPONENT
-      hash.each_pair do |key, value|
-        send '#{key}=', value
-      end
-      self
-    end
-    
-              id = link['href'].remove('#')
-          entries << [name, id]
-          next if name =~ /Sass|Less|Glyphicons/
-    
-              node.css('.method').each do |n|
-            next unless n.at_css('dt[id]')
-            name = n.at_css('.descname').content
-            name = '#{class_name}::#{name}()'
-            id = n.at_css('dt[id]')['id']
-            entries << [name, id]
+          ext = @options[:from]
+      Sass::Util.glob('#{@options[:input]}/**/*.#{ext}') do |f|
+        output =
+          if @options[:in_place]
+            f
+          elsif @options[:output]
+            output_name = f.gsub(/\.(c|sa|sc|le)ss$/, '.#{@options[:to]}')
+            output_name[0...@options[:input].size] = @options[:output]
+            output_name
+          else
+            f.gsub(/\.(c|sa|sc|le)ss$/, '.#{@options[:to]}')
           end
-        end
     
-    module Sass::Exec
-  # The `sass-convert` executable.
-  class SassConvert < Base
-    # @param args [Array<String>] The command-line arguments
-    def initialize(args)
-      super
-      require 'sass'
-      @options[:for_tree] = {}
-      @options[:for_engine] = {:cache => false, :read_cache => true}
-    end
-    
-      # Converts an interpolation array to source.
-  #
-  # @param interp [Array<String, Sass::Script::Tree::Node>] The interpolation array to convert.
-  # @param options [{Symbol => Object}] An options hash (see {Sass::CSS#initialize}).
-  # @return [String]
-  def self._interp_to_src(interp, options)
-    interp.map {|r| r.is_a?(String) ? r : r.to_sass(options)}.join
-  end
-end
-
-    
-        private
-    
-          def call(env)
-        request  = Request.new(env)
-        get_was  = handle(request.GET)
-        post_was = handle(request.POST) rescue nil
-        app.call env
-      ensure
-        request.GET.replace  get_was  if get_was
-        request.POST.replace post_was if post_was
+          # Get the publicly-visible URL for an imported file. This URL is used by
+      # source maps to link to the source stylesheet. This may return `nil` to
+      # indicate that no public URL is available; however, this will cause
+      # sourcemap generation to fail if any CSS is generated from files imported
+      # from this importer.
+      #
+      # If an absolute 'file:' URI can be produced for an imported file, that
+      # should be preferred to returning `nil`. However, a URL relative to
+      # `sourcemap_directory` should be preferred over an absolute 'file:' URI.
+      #
+      # @param uri [String] A URI known to be valid for this importer.
+      # @param sourcemap_directory [String, NilClass] The absolute path to a
+      #   directory on disk where the sourcemap will be saved. If uri refers to
+      #   a file on disk that's accessible relative to sourcemap_directory, this
+      #   may return a relative URL. This may be `nil` if the sourcemap's
+      #   eventual location is unknown.
+      # @return [String?] The publicly-visible URL for this file, or `nil`
+      #   indicating that no publicly-visible URL exists. This should be
+      #   appropriately URL-escaped.
+      def public_url(uri, sourcemap_directory)
+        return if @public_url_warning_issued
+        @public_url_warning_issued = true
+        Sass::Util.sass_warn <<WARNING
+WARNING: #{self.class.name} should define the #public_url method.
+WARNING
+        nil
       end
     
-      describe '#random_string' do
-    it 'outputs a string of 32 characters' do
-      expect(subject.random_string.length).to eq(32)
-    end
-  end
-    
-          get '/', {}, 'HTTP_COOKIE' => '_session=EVIL_SESSION_TOKEN; _session=SESSION_TOKEN'
-      expect(last_response).not_to be_ok
-    end
-  end
-end
-
-    
-            post '/', :file => Rack::Test::UploadedFile.new(temp_file.path), :other => '<bar>'
-        expect(body).to eq('_escaped_params_tmp_file\nhello world\n&lt;bar&gt;')
-      ensure
-        File.unlink(temp_file.path)
-      end
-    end
-  end
-end
-
-    
-      it 'allows passing on values in env' do
-    klass    = described_class
-    changer  = Struct.new(:app) do
-      def call(env)
-        env['foo.bar'] = 42
-        app.call(env)
-      end
-    end
-    detector = Struct.new(:app) do
-      def call(env)
-        app.call(env)
-      end
-    end
-    
-          expect('.border-color-all').to have_rule(rule)
-    end
-  end
-    
-          expect('.margin-alternate').to have_rule(rule)
-    end
-  end
-    
-          expect('.size-implicit').to have_ruleset(rule)
-    end
-  end
+        # @return [Node] The SassScript within the interpolation
+    attr_reader :mid
