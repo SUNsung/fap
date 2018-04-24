54bdbd3623be38c3c5ae14caed09fa89f687f4d0
SUNsung <weatherius@gmail.com>
2018-04-24 06:02:25 +0300

    Rewrite [01svmYlRW7kqk01PdPN7ROsCdU01p9RPcI9ESO601r2G.rb] File in: 2018-04-24 06:02:24

--- ruby/01svmYlRW7kqk01PdPN7ROsCdU01p9RPcI9ESO601r2G.rb
+++ ruby/01svmYlRW7kqk01PdPN7ROsCdU01p9RPcI9ESO601r2G.rb
@@ -1,118 +1,34 @@
 
         
-        class MeTooController < JustMeController
-end
-    
-      class HeadTest < ActiveSupport::TestCase
-    test 'head works on its own' do
-      status = HeadController.action(:index).call(Rack::MockRequest.env_for('/')).first
-      assert_equal 404, status
-    end
-    
-          add_delivery_method :sendmail, Mail::Sendmail,
-        location:  '/usr/sbin/sendmail',
-        arguments: '-i'
-    
-          # Make list points stand on their own line
-      formatted.gsub!(/[ ]*([*]+) ([^*]*)/) { '  #{$1} #{$2.strip}\n' }
-      formatted.gsub!(/[ ]*([#]+) ([^#]*)/) { '  #{$1} #{$2.strip}\n' }
-    
-        def call(env)
-      @stack.call(env)
-    end
-  end
-    
-    class HelperMailer < ActionMailer::Base
-  def use_mail_helper
-    @text = 'But soft! What light through yonder window breaks? It is the east, ' \
-            'and Juliet is the sun. Arise, fair sun, and kill the envious moon, ' \
-            'which is sick and pale with grief that thou, her maid, art far more ' \
-            'fair than she. Be not her maid, for she is envious! Her vestal ' \
-            'livery is but sick and green, and none but fools do wear it. Cast ' \
-            'it off!'
-    
-      included do |base|
-    AgentRunner.register(base)
-  end
-    
-      module SortableTableHelper
-    # :call-seq:
-    #   sortable_column(attribute, default_direction = 'desc', name: attribute.humanize)
-    def sortable_column(attribute, default_direction = nil, options = nil)
-      if options.nil? && (options = Hash.try_convert(default_direction))
-        default_direction = nil
-      end
-      default_direction ||= 'desc'
-      options ||= {}
-      name = options[:name] || attribute.humanize
-      selected = @table_sort_info[:attribute].to_s == attribute
-      if selected
-        direction = @table_sort_info[:direction]
-        new_direction = direction.to_s == 'desc' ? 'asc' : 'desc'
-        classes = 'selected #{direction}'
-      else
-        classes = ''
-        new_direction = default_direction
-      end
-      link_to(name, url_for(sort: '#{attribute}.#{new_direction}'), class: classes)
-    end
-  end
+        # No trailing slash
+Benchmark.ips do |x|
+  x.report('with body include?') { CONTENT_CONTAINING.include?('<body') }
+  x.report('with body regexp')   { CONTENT_CONTAINING =~ /<\s*body/ }
+  x.compare!
 end
 
     
-      def destroy
-    @job = Delayed::Job.find(params[:id])
+      # If a given extension is listed multiple times, prefer the first one listed
+  extensions.reject! { |extension| mimes.values.flatten.include?(extension) }
     
-        def insert_after(index, *names)
-      insert assert_index(index) + 1, *names
-    end
+      p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
+  p.option 'destination', '-d', '--destination [DIR]',
+    'Destination directory (defaults to ./_site)'
+  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
+  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
+    'Plugins directory (defaults to ./_plugins)'
+  p.option 'layouts_dir', '--layouts DIR', String,
+    'Layouts directory (defaults to ./_layouts)'
+  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
     
-        def add(path, content)
-      @pages[path] = content
-    end
-    
-            css('.note h3', '.warning h3').each do |node|
-          node.before('<p><strong>#{node.inner_html}</strong></p>').remove
-        end
-    
-            entries
-      end
-    end
-  end
-end
-
-    
-            css('.section', '#content', '.description', '.list', 'span.attributes').each do |node|
-          node.before(node.children).remove
-        end
-    
-      # Implemented by subclasses to provide default values for settings needed by
-  # this plugin. Typically done using the `set_if_empty` Capistrano DSL method.
-  #
-  # Example:
-  #
-  #   def set_defaults
-  #     set_if_empty :my_plugin_option, true
-  #   end
-  #
-  def set_defaults; end
-    
-    module RuboCop
-  module Cop
-    class VariableForce
-      # A Variable represents existence of a local variable.
-      # This holds a variable declaration node,
-      # and some states of the variable.
-      class Variable
-        VARIABLE_DECLARATION_TYPES =
-          (VARIABLE_ASSIGNMENT_TYPES + ARGUMENT_DECLARATION_TYPES).freeze
-    
-            def each_unnecessary_space_match(node, &blk)
-          each_match_range(
-            contents_range(node),
-            MULTIPLE_SPACES_BETWEEN_ITEMS_REGEX,
-            &blk
-          )
+          private
+      def grouped_array(groups)
+        groups.each_with_object([]) do |item, array|
+          array << {
+            'name'  => item.first,
+            'items' => item.last,
+            'size'  => item.last.size,
+          }
         end
       end
     end
@@ -120,27 +36,77 @@ end
 end
 
     
-            def_node_matcher :on_body_of_reduce, <<-PATTERN
-          (block (send _recv {:reduce :inject} !sym) _blockargs $(begin ...))
-        PATTERN
+        # Gets the Site object.
+    attr_reader :site
     
-    module RuboCop
-  module Cop
-    module Style
-      # This cop checks for optional arguments to methods
-      # that do not come at the end of the argument list
-      #
-      # @example
-      #   # bad
-      #   def foo(a = 1, b, c)
-      #   end
-      #
-      #   # good
-      #   def baz(a, b, c = 1)
-      #   end
-      #
-      #   def foobar(a = 1, b = 2, c = 3)
-      #   end
-      class OptionalArguments < Cop
-        MSG = 'Optional arguments should appear at the end ' \
-              'of the argument list.'.freeze
+          def measure_bytes
+        yield.tap do |str|
+          @renderer.increment_bytes(@filename, str.bytesize)
+        end
+      end
+    
+            Devise::Models.config(self, :allow_unconfirmed_access_for, :confirmation_keys, :reconfirmable, :confirm_within)
+      end
+    end
+  end
+end
+
+    
+      # GET /resource/confirmation?confirmation_token=abcdef
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+    
+        if record.timedout?(last_request_at) &&
+        !env['devise.skip_timeout'] &&
+        !proxy.remember_me_is_active?(record)
+      Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
+      throw :warden, scope: scope, message: :timeout
+    end
+    
+        inp.attributes.keys.each do |ikey|
+      if (ikey.downcase == 'value')
+        inp[ikey] = ''
+        next
+      end
+    
+          when :banner
+        # Because some ftp server send multiple banner we take only the first one and ignore the rest
+        if not (s[:info])
+          s[:info] = matches
+          report_service(s)
+        end
+    
+    
+# replace calls, jmps, and read/write handle/filename references
+replaces = []
+asm.each_line { |ln|
+	if ln =~ /call /
+		parts = ln.split(' ')
+		if (parts[0] == 'call' and parts[2] == ';call')
+			old = parts[1]
+			func = parts[3]
+			new = addrs[func]
+			#puts '%32s: %s -> %x' % [func, old, new]
+			replaces << [func, old, new.to_s(16)]
+		end
+	end
+    }
+    
+    keytoolOpts 	= ['-genkey', '-alias', 'signFiles', '-keystore', 'msfkeystore',
+		   '-storepass', 'msfstorepass', '-dname', 'cn=#{certCN}',
+		   '-keypass', 'msfkeypass']
+    
+          def http_error_page_as_json(e)
+        render json: {error: :invalid_request, error_description: e.message}, status: 400
+      end
+    
+          def create
+        req = Rack::Request.new(request.env)
+        if req['client_assertion_type'] == 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
+          handle_jwt_bearer(req)
+        end
+        self.status, headers, self.response_body = Api::OpenidConnect::TokenEndpoint.new.call(request.env)
+        headers.each {|name, value| response.headers[name] = value }
+        nil
+      end
