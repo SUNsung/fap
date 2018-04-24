e43476842400eb7d536c6b1b8ab602f28ca4160b
SUNsung <weatherius@gmail.com>
2018-04-24 07:28:28 +0300

    Rewrite [26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb] File in: 2018-04-24 07:28:27

--- ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
+++ ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
@@ -1,121 +1,44 @@
 
         
-            def matching_username
-      Account.where(Account.arel_table[:username].lower.eq username.to_s.downcase)
-    end
-    
-        # specify git tag or commit hash here
-    def source_tag
-      nil
-    end
-    
-        respond_to do |format|
-      format.html do
-        serializable_resource = ActiveModelSerializers::SerializableResource.new(InitialStatePresenter.new(initial_state_params), serializer: InitialStateSerializer)
-        @initial_state_json   = serializable_resource.to_json
+              def _routes
+        @controller._routes if @controller.respond_to?(:_routes)
       end
     
-          expect(response).to redirect_to(settings_notifications_path)
-      user.reload
-      expect(user.settings['notification_emails']['follow']).to be true
-      expect(user.settings['interactions']['must_be_follower']).to be false
+                          until @when_connected.empty?
+                        @when_connected.shift.call
+                      end
+                    end
+    
+      def test_assert_select_email_multipart
+    AssertMultipartSelectMailer.test(html: '<div><p>foo</p><p>bar</p></div>', text: 'foo bar').deliver_now
+    assert_select_email do
+      assert_select 'div:root' do
+        assert_select 'p:first-child', 'foo'
+        assert_select 'p:last-child', 'bar'
+      end
     end
   end
 end
 
     
-        uninstall quit: 'bundle.id.goes.here'
-    
-    module Hardware
-  describe CPU do
-    describe '::type' do
-      it 'returns the current CPU's type as a symbol, or :dunno if it cannot be detected' do
-        expect(
-          [
-            :intel,
-            :ppc,
-            :dunno,
-          ],
-        ).to include(described_class.type)
-      end
-    end
-    
-      def login!
-    print 'GitHub User: '
-    ENV['HOMEBREW_GITHUB_API_USERNAME'] = $stdin.gets.chomp
-    print 'Password: '
-    ENV['HOMEBREW_GITHUB_API_PASSWORD'] = noecho_gets.chomp
-    puts
-  end
-    
-        if ARGV.dry_run?
-      if ARGV.include? '--strict'
-        ohai 'brew audit --strict #{formula.path.basename}'
-      elsif ARGV.include? '--audit'
-        ohai 'brew audit #{formula.path.basename}'
-      end
-    else
-      failed_audit = false
-      if ARGV.include? '--strict'
-        system HOMEBREW_BREW_FILE, 'audit', '--strict', formula.path
-        failed_audit = !$CHILD_STATUS.success?
-      elsif ARGV.include? '--audit'
-        system HOMEBREW_BREW_FILE, 'audit', formula.path
-        failed_audit = !$CHILD_STATUS.success?
-      end
-      if failed_audit
-        formula.path.atomic_write(backup_file)
-        odie 'brew audit failed!'
-      end
-    end
-    
-            def log_level(name, options = {})
-          if options[:prepend]
-            level = log_levels.values.min
-            level = level.nil? ? 0 : level - 1
-          else
-            level = log_levels.values.max
-            level = level.nil? ? 0 : level + 1
-          end
-          log_levels.update(name => level)
-          define_logger(name)
-        end
-    
-        # @see \{MediaQuery#to\_a}
-    def to_a
-      res = []
-      res += modifier
-      res << ' ' unless modifier.empty?
-      res += type
-      res << ' and ' unless type.empty? || expressions.empty?
-      res += Sass::Util.intersperse(expressions, ' and ').flatten
-      res
-    end
-    
-    module Jekyll
-    
-      class RenderPartialTag < Liquid::Tag
-    include OctopressFilters
-    def initialize(tag_name, markup, tokens)
-      @file = nil
-      @raw = false
-      if markup =~ /^(\S+)\s?(\w+)?/
-        @file = $1.strip
-        @raw = $2 == 'raw'
-      end
-      super
-    end
-    
-      context 'called with three widths' do
-    it 'applies second width to left and right' do
-      rule = 'border-width: 4px 5px 6px'
-    
-      context 'called with argument (2)' do
-    it 'outputs the second value from the default scale' do
-      expect('.two-base-one').to have_rule('font-size: 1.5625em')
+      test 'does not append the deliveries collection if told not to perform the delivery' do
+    old_perform_deliveries = DeliveryMailer.perform_deliveries
+    begin
+      DeliveryMailer.perform_deliveries = false
+      DeliveryMailer.welcome.deliver_now
+      assert_equal [], DeliveryMailer.deliveries
+    ensure
+      DeliveryMailer.perform_deliveries = old_perform_deliveries
     end
   end
     
-          expect('.size-both').to have_ruleset(rule)
+        mail_with_defaults do |format|
+      format.html { render(inline: '<%= format_paragraph @text, 15, 1 %>') }
     end
+  end
+    
+          raise Discourse::InvalidParameters.new(:to_address) unless args[:to_address].present?
+    
+      def grant_admin!
+    set_permission('admin', true)
   end
