6bde55984189363b218307891ee09bbe8dc3727e
SUNsung <weatherius@gmail.com>
2018-04-24 12:49:55 +0300

    Rewrite [17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp] File in: 2018-04-24 12:49:54

--- cccccc/17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp
+++ cccccc/17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp
@@ -1,376 +1,164 @@
 
         
-          bool Register(const ui::Accelerator& accelerator,
-                const base::Closure& callback);
-  bool IsRegistered(const ui::Accelerator& accelerator);
-  void Unregister(const ui::Accelerator& accelerator);
-  void UnregisterAll();
-    
-    #ifndef ATOM_BROWSER_NET_URL_REQUEST_ASYNC_ASAR_JOB_H_
-#define ATOM_BROWSER_NET_URL_REQUEST_ASYNC_ASAR_JOB_H_
-    
-    
-    {}  // namespace atom
-    
-    #ifndef ATOM_COMMON_API_REMOTE_CALLBACK_FREER_H_
-#define ATOM_COMMON_API_REMOTE_CALLBACK_FREER_H_
-#include 'atom/common/api/object_life_monitor.h'
-#include 'content/public/browser/web_contents_observer.h'
-    
-    namespace base {
-class FilePath;
+        bool cudnn_is_acceptable(const Tensor& self) {
+  if (!globalContext().userEnabledCuDNN()) return false;
+  if (!self.is_cuda()) return false;
+  auto st = self.type().scalarType();
+  if (!(st == kDouble || st == kFloat || st == kHalf)) return false;
+  if (!AT_CUDNN_ENABLED()) return false;
+  // NB: In the old Python code, there was also a test to see if the
+  // cuDNN library was actually dynamically linked or not.  I'm not
+  // sure if we can actually test this.
+  return true;
 }
     
-    #ifndef CHROME_BROWSER_PRINTING_PRINT_VIEW_MANAGER_OBSERVER_H_
-#define CHROME_BROWSER_PRINTING_PRINT_VIEW_MANAGER_OBSERVER_H_
+    template<typename T>
+struct algorithm_spec<CollectiveType::ALL_REDUCE, T> {
+  static GlooCache::key_type key(
+    THDGroup group_id, DeviceType device, std::size_t input_bytes,
+    std::size_t unused_count, THDReduceOp op
+  ) {
+    int stream = UNUSED_STREAM;
+    if (device == DeviceType::CUDA) {
+      auto cuda_stream = THCState_getCurrentStream(THDGetCudaState());
+      stream = THDGetStreamId(cuda_stream);
+    }
+    return std::make_tuple(CollectiveType::ALL_REDUCE, group_id, device, stream,
+                           input_bytes, input_bytes, op, UNUSED_RANK);
+  }
+    }
     
-    using namespace swift;
+    THDTensorDescriptor THDTensorDescriptor_newFromTHDoubleTensor(THDoubleTensor *tensor) {
+  return at::getType(at::Backend::CPU, at::ScalarType::Double).unsafeTensorFromTH((void*)tensor, true);
+}
     
-    typedef NS_ENUM(NSInteger, ObjectBehaviorAction) {
-    ObjectBehaviorActionRetain,
-    ObjectBehaviorActionCopy,
-    ObjectBehaviorActionMutableCopy
-};
+    const std::string kKernelSyscallAddrModifiedPath = '/sys/kernel/camb/syscall_addr_modified';
+const std::string kKernelTextHashPath = '/sys/kernel/camb/text_segment_hash';
     
-      ArrayRef<StringRef> getTags() const {
-    return llvm::makeArrayRef(Parts.Tags.begin(), Parts.Tags.end());
+    class RunnerInterruptPoint : private boost::noncopyable {
+ public:
+  RunnerInterruptPoint() = default;
+    }
+    
+     protected:
+  /// A helper value counting the number of fired events tracked by publishers.
+  EventContextID event_count_{0};
+    
+      /**
+   * @brief If dropping explicitly to a user and group also drop groups.
+   *
+   * Original process groups before explicitly dropping privileges.
+   * On restore, if there are any groups in this list, they will be added
+   * to the processes group list.
+   */
+  gid_t* original_groups_{nullptr};
+    
+    
+    {  /// IOCTL API handling mutex.
+  lck_mtx_t *mtx;
+} osquery = {.open_count = 0,
+             .buffer = NULL,
+             .buf_size = 0,
+             .md = NULL,
+             .mm = NULL,
+             .devfs = NULL,
+             .major_number = OSQUERY_MAJOR};
+    
+      pid_t result = ::waitpid(nativeHandle(), &process_status, WNOHANG);
+  if (result < 0) {
+    if (errno == ECHILD) {
+      return PROCESS_EXITED;
+    }
+    process_status = -1;
+    return PROCESS_ERROR;
   }
     
-    #include 'swift/Syntax/Rewriter.h'
     
-    void dump(SubstitutionList subs);
-    
-    namespace index {
+    {    auto stale_status = checkStalePid(content);
+    if (!stale_status.ok()) {
+      return stale_status;
     }
-    
-      /// Associates buffer identifiers to buffer IDs.
-  llvm::StringMap<unsigned> BufIdentIDMap;
-    
-    bool Substitution::operator==(const Substitution &other) const {
-  // The archetypes may be missing, but we can compare them directly
-  // because archetypes are always canonical.
-  return
-    Replacement->isEqual(other.Replacement) &&
-    Conformance.equals(other.Conformance);
-}
-    
-    #endif  // STORAGE_LEVELDB_DB_DB_ITER_H_
-
-    
-    
-    {  Table* table = reinterpret_cast<TableAndFile*>(cache_->Value(handle))->table;
-  Iterator* result = table->NewIterator(options);
-  result->RegisterCleanup(&UnrefEntry, cache_, handle);
-  if (tableptr != NULL) {
-    *tableptr = table;
   }
-  return result;
-}
     
-      // If a seek to internal key 'k' in specified file finds an entry,
-  // call (*handle_result)(arg, found_key, found_value).
-  Status Get(const ReadOptions& options,
-             uint64_t file_number,
-             uint64_t file_size,
-             const Slice& k,
-             void* arg,
-             void (*handle_result)(void*, const Slice&, const Slice&));
-    
-    TEST(FindFileTest, OverlappingFiles) {
-  Add('150', '600');
-  Add('400', '500');
-  disjoint_sorted_files_ = false;
-  ASSERT_TRUE(! Overlaps('100', '149'));
-  ASSERT_TRUE(! Overlaps('601', '700'));
-  ASSERT_TRUE(Overlaps('100', '150'));
-  ASSERT_TRUE(Overlaps('100', '200'));
-  ASSERT_TRUE(Overlaps('100', '300'));
-  ASSERT_TRUE(Overlaps('100', '400'));
-  ASSERT_TRUE(Overlaps('100', '500'));
-  ASSERT_TRUE(Overlaps('375', '400'));
-  ASSERT_TRUE(Overlaps('450', '450'));
-  ASSERT_TRUE(Overlaps('450', '500'));
-  ASSERT_TRUE(Overlaps('450', '700'));
-  ASSERT_TRUE(Overlaps('600', '700'));
-}
-    
-    // Returns a new environment that stores its data in memory and delegates
-// all non-file-storage tasks to base_env. The caller must delete the result
-// when it is no longer needed.
-// *base_env must remain live while the result is in use.
-Env* NewMemEnv(Env* base_env);
-    
-      // Return a string that contains the copy of the referenced data.
-  std::string ToString() const { return std::string(data_, size_); }
-    
-            // cross two lines
-        Point2f origin1 = quad_corners[0];
-        Point2f dir1 = quad_corners[1] - quad_corners[0];
-        Point2f origin2 = quad_corners[2];
-        Point2f dir2 = quad_corners[3] - quad_corners[2];
-        double angle = acos(dir1.dot(dir2)/(norm(dir1)*norm(dir2)));
-        if(cvIsNaN(angle) || cvIsInf(angle) || angle < 0.5 || angle > CV_PI - 0.5) continue;
-    
-                //a simple check that the position is general:
-            //  for each line check that all other points don't belong to it
-            isGeneralPosition = true;
-            for (int startPointIndex = 0; startPointIndex < usedPointsCount && isGeneralPosition; startPointIndex++)
-            {
-                for (int endPointIndex = startPointIndex + 1; endPointIndex < usedPointsCount && isGeneralPosition; endPointIndex++)
-                {
-    }
-    }
-    
-    
-    {                return (count & TAG_MASK) - val;
-#else
-                return ::atomicAdd(address, val);
-#endif
-            }
-    
-    namespace grpc {
-namespace testing {
-    }
-    }
-    
-    std::unique_ptr<ScenarioResult> RunScenario(
-    const grpc::testing::ClientConfig& client_config, size_t num_clients,
-    const grpc::testing::ServerConfig& server_config, size_t num_servers,
-    int warmup_seconds, int benchmark_seconds, int spawn_local_worker_count,
-    const grpc::string& qps_server_target_override,
-    const grpc::string& credential_type, bool run_inproc);
-    
-    #include <initializer_list>
-#include <string>
-#include <vector>
-    
-    
-    {					if (mix_callback) {
-						int mixed = mix_callback(mix_udata, aux_buffer, m);
-						to_read -= mixed;
-						if (mixed != m) { //could mix no more
-							buffer_full = true;
-							break;
-						}
-					} else {
-						to_read -= m; //just pretend we sent the audio
-					}
-				}
-    
-    
-    {	if (_is_multiplayer) {
-		// need to wait for ID confirmation...
-	} else {
-		emit_signal('connection_established', p_protocol);
-	}
-}
-    
-    
-    {	ClassInfo *ti = classes.getptr(p_class);
-	ERR_FAIL_COND_V(!ti, false);
-	return ti->exposed;
-}
-    
-    	{
-    }
-    
-    	brake = p_brake;
-}
-float VehicleBody::get_brake() const {
-    
-    		for (int j = 0; j < vc; j++) {
-    }
-    
-    	String _validate_name(const String &p_name) const;
-    
-    	virtual bool _set(const StringName &p_name, const Variant &p_property);
-	virtual bool _get(const StringName &p_name, Variant &r_property) const;
-	virtual void _get_property_list(List<PropertyInfo> *p_list) const;
-    
-        // replace children
-    // This looks for nodes in the network that have the same name as its current inputs, and then relinks its inputs to those.
-    // I.e. this allows to move a node from network to another and reconnect by the names if its inputs.
-    for (int i = 0; i < newNode->GetNumInputs(); ++i)
-    {
-        if (m_nameToNodeMap.find(newNode->GetInputs()[i]->NodeName()) == m_nameToNodeMap.end())
-            RuntimeError('Child node %ls is not part of the network.', newNode->GetInputs()[i]->NodeName().c_str());
-        newNode->SetInput(i, m_nameToNodeMap[newNode->GetInputs()[i]->NodeName()]);
-    }
-    
-        mBsparse.SwitchToMatrixType(MatrixType::SPARSE, matrixFormatSparseCSR, true);
-    Matrix<float>::ScaleAndAdd(alpha, mAdense, mBsparse);
-    
-            // take off the prefix
-        std::string search;
-        if (firstStart == symbolIn.length())
-        {
-            // this case is just the model label with nothing else, in that case we want the all nodes
-            search = '*';
-            inWildcard = true;
-        }
-        else
-        {
-            search = symbolIn.substr(firstStart);
-        }
-    
-    
-    {        // no config file, parse as regular argument
-        if (compare)
-        {
-            configString += (msra::strfun::utf8(str) + '\n');
-        }
-        else // One or more config file paths specified in a '+'-separated list.
-        {
-            const std::string filePaths = msra::strfun::utf8(str.substr(configDescriptor.length()));
-            std::vector<std::string> filePathsVec = msra::strfun::split(filePaths, '+');
-            for (auto filePath : filePathsVec)
-            {
-                if (std::find(resolvedConfigFiles.begin(), resolvedConfigFiles.end(), filePath) == resolvedConfigFiles.end())
-                {
-                    // if haven't already read this file, read it
-                    resolvedConfigFiles.push_back(filePath);
-                    configString += config.ReadConfigFile(filePath);
-                    // remember all config directories, for use as include paths by BrainScriptNetworkBuilder
-                    GetBrainScriptNetworkBuilderIncludePaths().push_back(File::DirectoryPathOf(msra::strfun::utf16(filePath)));
-                }
-                else
-                    RuntimeError('Cannot specify same config file multiple times at the command line.');
-            }
-        }
-    }
-    // now, configString is a concatenation of lines, including parameters from the command line, with comments stripped
-    
-        // call TraceTotalNumberOfSteps() to set the total number of steps
-    // Calling this with totalNumberOfSteps>0 will enable progress tracing.
-    static void TraceTotalNumberOfSteps(size_t totalNumberOfSteps)
-    {
-        auto& us = GetStaticInstance();
-        us.m_enabled = totalNumberOfSteps > 0;
-        if (us.m_enabled)
-        {
-            us.m_totalNumberOfSteps = totalNumberOfSteps;
-            us.m_progressTracingTimer.Start();
-        }
-    }
-    
-    template<class TString>
-inline bool AreEqualIgnoreCase(
-    const TString& s1,
-    const typename TString::value_type* s2pointer)
-{
-    return AreEqualIgnoreCase(s1, TString(s2pointer));
-}
-    
-    // ---------------------------------------------------------------------------
-// latticesource -- manages loading of lattices for MMI (in pairs for numer and denom)
-// ---------------------------------------------------------------------------
-    
-    constexpr std::array<int, 4> WORKERS_NUM = {2, 4, 7, 13};
-constexpr int MASTER_PORT = 45678;
-constexpr int BARRIER_WAIT_TIME = 200; // milliseconds
-    
-    int64_t size(const Tensor& self, int64_t dim) {
-  // false is passed to maybe_wrap_dim so behavior is identical to array access (but with wrapping)
-  dim = maybe_wrap_dim(dim, self.dim(), false);
-  return self.sizes()[dim];
-}
-    
-    #if ${isCUDA}
-static cudaError_t call_deleter(void * ctx, void * data) {
-  auto fnptr = (std::function<void(void*)>*) ctx;
-  (*fnptr)(data);
-  delete fnptr;
-  return cudaSuccess;
-}
-static THCDeviceAllocator storage_deleter = {
-  nullptr,
-  nullptr,
-  call_deleter,
-  nullptr,
-  nullptr,
-};
-static cudaError_t wrapped_alloc(void * ctx, void** result, size_t size, cudaStream_t stream) {
-  auto ac = static_cast<detail::AllocatorRetainable*>(ctx);
-  ac->retain();
-  *result = ac->allocate(size);
-  return cudaSuccess;
-}
-static cudaError_t wrapped_free(void * ctx, void * data) {
-  auto ac = static_cast<detail::AllocatorRetainable*>(ctx);
-  ac->deallocate(data);
-  ac->release();
-  return cudaSuccess;
-}
-static THCDeviceAllocator wrapped_allocator = {
-  wrapped_alloc,
-  nullptr,
-  wrapped_free,
-  nullptr,
-  nullptr,
-};
-#else
-static void call_deleter(void * ctx, void * data) {
-  auto fnptr = (std::function<void(void*)>*) ctx;
-  (*fnptr)(data);
-  delete fnptr;
-}
-static THAllocator storage_deleter = {
-  nullptr,
-  nullptr,
-  call_deleter,
-};
-static void* wrapped_alloc(void * ctx, ptrdiff_t size) {
-  auto ac = static_cast<detail::AllocatorRetainable*>(ctx);
-  ac->retain();
-  return ac->allocate(size);
-}
-static void wrapped_free(void * ctx, void * data) {
-  auto ac = static_cast<detail::AllocatorRetainable*>(ctx);
-  ac->deallocate(data);
-  ac->release();
-}
-static THAllocator wrapped_allocator = {
-  wrapped_alloc,
-  nullptr,
-  wrapped_free,
-};
-#endif
-    
-    
-namespace thd {
-    }
-    
-    
-#endif /* COMM_COMM_DNS_H_ */
-
-    
-    
-    {        vecdump_.push_back(strstack);
-        strstack.clear();
-    }
-    
-        void Dump(const std::string& _processname);
-    const std::vector<std::string>& StackList() const;
-    
-    
-    {  private:
-    TServicesMap m_services;
-    TServicesMap m_publicservices;
-    std::vector<ServiceBase*> m_releasevec;
+    static const int kCrToRedTable[256] = {
+  -179, -178, -177, -175, -174, -172, -171, -170, -168, -167, -165, -164,
+  -163, -161, -160, -158, -157, -156, -154, -153, -151, -150, -149, -147,
+  -146, -144, -143, -142, -140, -139, -137, -136, -135, -133, -132, -130,
+  -129, -128, -126, -125, -123, -122, -121, -119, -118, -116, -115, -114,
+  -112, -111, -109, -108, -107, -105, -104, -102, -101, -100,  -98,  -97,
+   -95,  -94,  -93,  -91,  -90,  -88,  -87,  -86,  -84,  -83,  -81,  -80,
+   -79,  -77,  -76,  -74,  -73,  -72,  -70,  -69,  -67,  -66,  -64,  -63,
+   -62,  -60,  -59,  -57,  -56,  -55,  -53,  -52,  -50,  -49,  -48,  -46,
+   -45,  -43,  -42,  -41,  -39,  -38,  -36,  -35,  -34,  -32,  -31,  -29,
+   -28,  -27,  -25,  -24,  -22,  -21,  -20,  -18,  -17,  -15,  -14,  -13,
+   -11,  -10,   -8,   -7,   -6,   -4,   -3,   -1,    0,    1,    3,    4,
+     6,    7,    8,   10,   11,   13,   14,   15,   17,   18,   20,   21,
+    22,   24,   25,   27,   28,   29,   31,   32,   34,   35,   36,   38,
+    39,   41,   42,   43,   45,   46,   48,   49,   50,   52,   53,   55,
+    56,   57,   59,   60,   62,   63,   64,   66,   67,   69,   70,   72,
+    73,   74,   76,   77,   79,   80,   81,   83,   84,   86,   87,   88,
+    90,   91,   93,   94,   95,   97,   98,  100,  101,  102,  104,  105,
+   107,  108,  109,  111,  112,  114,  115,  116,  118,  119,  121,  122,
+   123,  125,  126,  128,  129,  130,  132,  133,  135,  136,  137,  139,
+   140,  142,  143,  144,  146,  147,  149,  150,  151,  153,  154,  156,
+   157,  158,  160,  161,  163,  164,  165,  167,  168,  170,  171,  172,
+   174,  175,  177,  178
 };
     
-    #ifndef PublicComponent_spy_h
-#define PublicComponent_spy_h
     
-    void Test_Spy_Sample::TestFun0()
-{
-    SPY_HOOK_THIS_API(TestFun0);
-    int i = 0;
-    SPY_ATTACH_VARIABLE('TestFun0 i', i);
-    i++;
-    xinfo2(TSF'Test');
-    }
+    {}  // namespace guetzli
     
-    // char* to jstring
-jstring JNU_Chars2Jstring(JNIEnv* _env, const char* pat);
-void JNU_FreeJstring(JNIEnv* _env, jstring str);
+    // This function will create a Huffman tree.
+//
+// The (data,length) contains the population counts.
+// The tree_limit is the maximum bit depth of the Huffman codes.
+//
+// The depth contains the tree, i.e., how many bits are used for
+// the symbol.
+//
+// The actual Huffman tree is constructed in the tree[] array, which has to
+// be at least 2 * length + 1 long.
+//
+// See http://en.wikipedia.org/wiki/Huffman_coding
+void CreateHuffmanTree(const uint32_t *data,
+                       const size_t length,
+                       const int tree_limit,
+                       HuffmanTree* tree,
+                       uint8_t *depth);
+    
+    const double* Srgb8ToLinearTable() {
+  static const double* const kSrgb8ToLinearTable = NewSrgb8ToLinearTable();
+  return kSrgb8ToLinearTable;
+}
+    
+    
+    {  return total_size;
+}
+    
+    // Gamma-compensated chroma subsampling.
+// Returns Y, U, V image planes, each with width x height dimensions, but the
+// U and V planes are composed of 2x2 blocks with the same values.
+std::vector<std::vector<float> > RGBToYUV420(
+    const std::vector<uint8_t>& rgb_in, const int width, const int height);
+    
+    	ScopedJstring tag_jstr(env, tag);
+	ScopedJstring filename_jstr(env, filename);
+	ScopedJstring funcname_jstr(env, funcname);
+	ScopedJstring log_jst(env, _log);
+    
+    #include 'wakeuplock.h'
+#include 'assert/__assert.h'
+#include 'xlogger/xlogger.h'
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+    
+/*
+ * scop_jenv.cpp
+ *
+ *  Created on: 2012-8-21
+ *      Author: yanguoyue
+ */
