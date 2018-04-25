ee97dd83aaf3c38855131ff43b5960e46d553fa9
SUNsung <weatherius@gmail.com>
2018-04-25 10:40:01 +0300

    Rewrite [293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb] File in: 2018-04-25 10:40:01

--- ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
+++ ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
@@ -1,49 +1,58 @@
 
         
-          class FeatureTopicUsers < Jobs::Base
-    
-          raise Discourse::InvalidParameters.new(:to_address) unless args[:to_address].present?
-    
-          ext = @options[:from]
-      Sass::Util.glob('#{@options[:input]}/**/*.#{ext}') do |f|
-        output =
-          if @options[:in_place]
-            f
-          elsif @options[:output]
-            output_name = f.gsub(/\.(c|sa|sc|le)ss$/, '.#{@options[:to]}')
-            output_name[0...@options[:input].size] = @options[:output]
-            output_name
+                  # Bubbled up from the adapter require. Prefix the exception message
+          # with some guidance about how to address it and reraise.
           else
-            f.gsub(/\.(c|sa|sc|le)ss$/, '.#{@options[:to]}')
+            raise e.class, 'Error loading the '#{adapter}' Action Cable pubsub adapter. Missing a gem it depends on? #{e.message}', e.backtrace
           end
+        end
     
-          # Get the publicly-visible URL for an imported file. This URL is used by
-      # source maps to link to the source stylesheet. This may return `nil` to
-      # indicate that no public URL is available; however, this will cause
-      # sourcemap generation to fail if any CSS is generated from files imported
-      # from this importer.
-      #
-      # If an absolute 'file:' URI can be produced for an imported file, that
-      # should be preferred to returning `nil`. However, a URL relative to
-      # `sourcemap_directory` should be preferred over an absolute 'file:' URI.
-      #
-      # @param uri [String] A URI known to be valid for this importer.
-      # @param sourcemap_directory [String, NilClass] The absolute path to a
-      #   directory on disk where the sourcemap will be saved. If uri refers to
-      #   a file on disk that's accessible relative to sourcemap_directory, this
-      #   may return a relative URL. This may be `nil` if the sourcemap's
-      #   eventual location is unknown.
-      # @return [String?] The publicly-visible URL for this file, or `nil`
-      #   indicating that no publicly-visible URL exists. This should be
-      #   appropriately URL-escaped.
-      def public_url(uri, sourcemap_directory)
-        return if @public_url_warning_issued
-        @public_url_warning_issued = true
-        Sass::Util.sass_warn <<WARNING
-WARNING: #{self.class.name} should define the #public_url method.
-WARNING
-        nil
-      end
+      test 'response status aliases deprecated' do
+    response = ActionDispatch::TestResponse.create
+    assert_deprecated { response.success? }
+    assert_deprecated { response.missing? }
+    assert_deprecated { response.error? }
+  end
+end
+
     
-        # @return [Node] The SassScript within the interpolation
-    attr_reader :mid
+        test 'head :continue (100) does not return a content-type header' do
+      headers = HeadController.action(:continue).call(Rack::MockRequest.env_for('/')).second
+      assert_nil headers['Content-Type']
+      assert_nil headers['Content-Length']
+    end
+    
+        rescue_from StandardError, with: :handle_exception_with_mailer_class
+    
+    module ActionMailer
+  class NonInferrableMailerError < ::StandardError
+    def initialize(name)
+      super 'Unable to determine the mailer to test from #{name}. ' \
+        'You'll need to specify it using tests YourMailer in your ' \
+        'test case definition'
+    end
+  end
+    
+      # Skips the current run on Rubinius using Minitest::Assertions#skip
+  private def rubinius_skip(message = '')
+    skip message if RUBY_ENGINE == 'rbx'
+  end
+  # Skips the current run on JRuby using Minitest::Assertions#skip
+  private def jruby_skip(message = '')
+    skip message if defined?(JRUBY_VERSION)
+  end
+end
+
+    
+        { # yes, this is ugly, feel free to change that
+      '/..' => '/', '/a/../b' => '/b', '/a/../b/' => '/b/', '/a/.' => '/a/',
+      '/%2e.' => '/', '/a/%2E%2e/b' => '/b', '/a%2f%2E%2e%2Fb/' => '/b/',
+      '//' => '/', '/%2fetc%2Fpasswd' => '/etc/passwd'
+    }.each do |a, b|
+      it('replaces #{a.inspect} with #{b.inspect}') { expect(get(a).body).to eq(b) }
+    end
+    
+      it 'should not override the header if already set' do
+    mock_app with_headers('X-XSS-Protection' => '0')
+    expect(get('/', {}, 'wants' => 'text/html').headers['X-XSS-Protection']).to eq('0')
+  end
