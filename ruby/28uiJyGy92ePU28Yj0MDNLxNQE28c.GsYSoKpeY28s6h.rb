af655136ba9d28af74cadc0b8a7b110737557661
SUNsung <weatherius@gmail.com>
2018-04-26 01:53:21 +0300

    Rewrite [28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb] File in: 2018-04-26 01:53:20

--- ruby/28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb
+++ ruby/28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb
@@ -1,163 +1,91 @@
 
         
-              # Reads data from an IO object while it can, returning the data it reads.
-      # When it encounters a case when it can't read anymore, it returns the
-      # data.
-      #
-      # @return [String]
-      def self.read_until_block(io)
-        data = ''
-    
-          def execute
-        options = {}
-        options[:check] = false
-    
-      it 'implicitly has a count of one when no count is specified' do
-    'abc'.unpack(unpack_format).should == ['a']
-  end
-    
-      Ruby = Struct.new(:version, :platform)
-    
-      def self.create_and_kill_critical_thread(pass_after_kill=false)
-    ThreadSpecs.create_critical_thread do
-      Thread.current.kill
-      Thread.pass if pass_after_kill
-      ScratchPad.record('status=' + Thread.current.status)
-    end
-  end
-end
-
-    
-            private
-    
-            # Prints the list of specs & pod cache dirs for a single pod name.
-        #
-        # This output is valid YAML so it can be parsed with 3rd party tools
-        #
-        # @param [Array<Hash>] cache_descriptors
-        #        The various infos about a pod cache. Keys are
-        #        :spec_file, :version, :release and :slug
-        #
-        def print_pod_cache_infos(pod_name, cache_descriptors)
-          UI.puts '#{pod_name}:'
-          cache_descriptors.each do |desc|
-            if @short_output
-              [:spec_file, :slug].each { |k| desc[k] = desc[k].relative_path_from(@cache.root) }
-            end
-            UI.puts('  - Version: #{desc[:version]}')
-            UI.puts('    Type:    #{pod_type(desc)}')
-            UI.puts('    Spec:    #{desc[:spec_file]}')
-            UI.puts('    Pod:     #{desc[:slug]}')
-          end
-        end
+                expect(result).to eq(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER])
       end
-    end
-  end
-end
-
     
-            self.summary = 'The repl listens to commands on standard input'
-        self.description = <<-DESC
-          The repl listens to commands on standard input and prints their
-          result to standard output.
-          It accepts all the other ipc subcommands. The repl will signal the
-          end of output with the the ASCII CR+LF `\\n\\r`.
-        DESC
-    
-            # Checks if a template URL is given else returns the TEMPLATE_REPO URL
-        #
-        # @return String
-        #
-        def template_repo_url
-          @template_url || TEMPLATE_REPO
-        end
+          def [](key)
+        super(convert_key(key))
       end
-    end
-  end
-end
-
     
-        def register_new_attachment
-      Paperclip::AttachmentRegistry.register(@klass, @name, @options)
-    end
-    
-            def description
-          'have an attachment named #{@attachment_name}'
-        end
-    
-            def rejected_types_rejected?
-          @missing_rejected_types ||= @rejected_types.select { |type| type_allowed?(type) }
-          @missing_rejected_types.none?
-        end
-      end
-    end
-  end
-end
-
-    
-    module Paperclip
-  class << self
-    attr_writer :registered_attachments_styles_path
-    def registered_attachments_styles_path
-      @registered_attachments_styles_path ||= Rails.root.join('public/system/paperclip_attachments.yml').to_s
-    end
-  end
-    
-        def processor(name) #:nodoc:
-      @known_processors ||= {}
-      if @known_processors[name.to_s]
-        @known_processors[name.to_s]
-      else
-        name = name.to_s.camelize
-        load_processor(name) unless Paperclip.const_defined?(name)
-        processor = Paperclip.const_get(name)
-        @known_processors[name.to_s] = processor
-      end
-    end
-    
-      def pretty?
-    params.has_key?('pretty')
-  end
-    
-    module LogStash
-  module Api
-    module Commands
-      module System
-        class Plugins < Commands::Base
-          def run
-            { :total => plugins.count, :plugins => plugins }
+              # Parse the options
+          argv = parse_options(opts)
+          return if !argv
+          if argv.empty? || argv.length > 2
+            raise Vagrant::Errors::CLIInvalidUsage,
+              help: opts.help.chomp
           end
     
-      public
-  # DEPRECATED: Prefer defining encode_sync or multi_encode
-  def encode(event)
-    encoded = multi_encode([event])
-    encoded.each {|event,data| @on_event.call(event,data) }
-  end # def encode
+    module VagrantPlugins
+  module CommandBox
+    module Command
+      class Repackage < Vagrant.plugin('2', :command)
+        def execute
+          opts = OptionParser.new do |o|
+            o.banner = 'Usage: vagrant box repackage <name> <provider> <version>'
+          end
     
-      context 'called with one color' do
-    it 'applies same color to all sides' do
-      rule = 'border-color: #f00'
+      def as_json(options={})
+    {
+      poll_id:             id,
+      post_id:             status_message.id,
+      question:            question,
+      poll_answers:        poll_answers,
+      participation_count: participation_count
+    }
+  end
     
-          expect('.border-style-explicit').to have_rule(rule)
+              render 'admins/pods'
+        end
+        format.mobile { render 'admins/pods' }
+        format.json { render json: pods_json }
+      end
+    end
+    
+          # Inspired by https://github.com/nov/openid_connect_sample/blob/master/app/controllers/connect/clients_controller.rb#L24
+      def create
+        registrar = OpenIDConnect::Client::Registrar.new(request.url, params)
+        client = Api::OpenidConnect::OAuthApplication.register! registrar
+        render json: client.as_json(root: false)
+      end
+    
+          def create
+        req = Rack::Request.new(request.env)
+        if req['client_assertion_type'] == 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
+          handle_jwt_bearer(req)
+        end
+        self.status, headers, self.response_body = Api::OpenidConnect::TokenEndpoint.new.call(request.env)
+        headers.each {|name, value| response.headers[name] = value }
+        nil
+      end
+    
+        # Returns a deep copy of this query and all its children.
+    #
+    # @return [Query]
+    def deep_copy
+      Query.new(
+        modifier.map {|c| c.is_a?(Sass::Script::Tree::Node) ? c.deep_copy : c},
+        type.map {|c| c.is_a?(Sass::Script::Tree::Node) ? c.deep_copy : c},
+        expressions.map {|e| e.map {|c| c.is_a?(Sass::Script::Tree::Node) ? c.deep_copy : c}})
     end
   end
     
-        @buttons_list = %w(
-      button
-      [type='button']
-      [type='reset']
-      [type='submit']
-    )
-  end
-    
-      context 'called with one size' do
-    it 'applies same width to all sides' do
-      rule = 'margin: 1px'
-    
-      context 'called with arguments (2, $value: 4em 6em)' do
-    it 'outputs sextuple the second value from the default scale' do
-      expect('.two-double-value').to have_rule('font-size: 3.125em')
+        def self.run
+      # Apparently there's no better way than this to add Sass
+      # to Merb's Rack stack.
+      Merb::Config[:app] = Sass::Plugin::Rack.new(Merb::Config[:app])
     end
   end
 end
+
+    
+          val = @mid.perform(environment)
+      if @warn_for_color && val.is_a?(Sass::Script::Value::Color) && val.name
+        alternative = Operation.new(Sass::Script::Value::String.new('', :string), @mid, :plus)
+        Sass::Util.sass_warn <<MESSAGE
+WARNING on line #{line}, column #{source_range.start_pos.offset}#{' of #{filename}' if filename}:
+You probably don't mean to use the color value `#{val}' in interpolation here.
+It may end up represented as #{val.inspect}, which will likely produce invalid CSS.
+Always quote color names when using them as strings (for example, '#{val}').
+If you really want to use the color value here, use `#{alternative.to_sass}'.
+MESSAGE
+      end
