53e71b9ff711fb3a1ddf1e4e3cfe32ce756f5bdf
SUNsung <weatherius@gmail.com>
2018-04-25 07:07:27 +0300

    Rewrite [03KQ.onY3gQCM03bN.RlgLpANg03.Gc1vQbBvAk03BwR.py] File in: 2018-04-25 07:07:27

--- pyton/03KQ.onY3gQCM03bN.RlgLpANg03.Gc1vQbBvAk03BwR.py
+++ pyton/03KQ.onY3gQCM03bN.RlgLpANg03.Gc1vQbBvAk03BwR.py
@@ -1,132 +1,74 @@
 
         
-        
-def import_state_tuples(state_tuples, name, num_replicas):
-  restored = []
-  for i in range(len(state_tuples) * num_replicas):
-    c = tf.get_collection_ref(name)[2 * i + 0]
-    h = tf.get_collection_ref(name)[2 * i + 1]
-    restored.append(tf.contrib.rnn.LSTMStateTuple(c, h))
-  return tuple(restored)
+                class SecretView(View):
+            methods = ['GET']
+            decorators = [superuser_required]
     
-      def testPtbProducer(self):
-    raw_data = [4, 3, 2, 1, 0, 5, 6, 1, 1, 1, 1, 0, 3, 4, 1]
-    batch_size = 3
-    num_steps = 2
-    x, y = reader.ptb_producer(raw_data, batch_size, num_steps)
-    with self.test_session() as session:
-      coord = tf.train.Coordinator()
-      tf.train.start_queue_runners(session, coord=coord)
-      try:
-        xval, yval = session.run([x, y])
-        self.assertAllEqual(xval, [[4, 3], [5, 6], [1, 0]])
-        self.assertAllEqual(yval, [[3, 2], [6, 1], [0, 3]])
-        xval, yval = session.run([x, y])
-        self.assertAllEqual(xval, [[2, 1], [1, 1], [3, 4]])
-        self.assertAllEqual(yval, [[1, 0], [1, 1], [4, 1]])
-      finally:
-        coord.request_stop()
-        coord.join()
+                version = match.group(1).strip()
     
-    # Dependency imports
-    
-      if summary_writer is not None:
-    global_step_val = sess.run(tf.train.get_global_step())
-    tf.logging.info('Finished eval for step ' + str(global_step_val))
-    summary_writer.add_summary(summary, global_step_val)
-    
-    import graphs
-import train_utils
-    
-        Returns:
-      dict from integer node ID to human-readable string.
-    '''
-    if not tf.gfile.Exists(uid_lookup_path):
-      tf.logging.fatal('File does not exist %s', uid_lookup_path)
-    if not tf.gfile.Exists(label_lookup_path):
-      tf.logging.fatal('File does not exist %s', label_lookup_path)
+                def g():
+                assert not flask.request
+                assert not flask.current_app
+                with reqctx:
+                    assert flask.request
+                    assert flask.current_app == app
+                    assert flask.request.path == '/'
+                    assert flask.request.args['foo'] == 'bar'
+                assert not flask.request
+                return 42
     
     
-    def initializeProgram(self, name):
-        p = []
-        self.programs[name] = p
-        return p
+def test_suppressed_exception_logging():
+    class SuppressedFlask(flask.Flask):
+        def log_exception(self, exc_info):
+            pass
     
-    class ClassicToken(Token):
-    '''@brief Alternative token implementation.
+        old_layer = keras.layers.LSTM(input_dim=5, input_length=3,
+                                  output_dim=2, name='d', consume_less='mem')
+    new_layer = keras.layers.LSTM(2, input_shape=[3, 5], name='d', implementation=1)
+    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer.get_config())
     
-    A Token object like we'd use in ANTLR 2.x; has an actual string created
-    and associated with this object.  These objects are needed for imaginary
-    tree nodes that have payload objects.  We need to create a Token object
-    that has a string; the tree node will point at this token.  CommonToken
-    has indexes into a char stream and hence cannot be used to introduce
-    new strings.
-    '''
-    
-        def __init__(self):
-        self._active_state = Active(self)  # Unit.Inservice.Active()
-        self._standby_state = Standby(self)  # Unit.Inservice.Standby()
-        self._suspect_state = Suspect(self)  # Unit.OutOfService.Suspect()
-        self._failed_state = Failed(self)  # Unit.OutOfService.Failed()
-        self._current_state = self._standby_state
-        self.states = {'active': self._active_state,
-                       'standby': self._standby_state,
-                       'suspect': self._suspect_state,
-                       'failed': self._failed_state}
-        self.message_types = {'fault trigger': self._current_state.on_fault_trigger,
-                              'switchover': self._current_state.on_switchover,
-                              'diagnostics passed': self._current_state.on_diagnostics_passed,
-                              'diagnostics failed': self._current_state.on_diagnostics_failed,
-                              'operator inservice': self._current_state.on_operator_inservice}
-    
-        def amount(self, val):
-        print(val, end=' ')
-        return self
+    - Models
+    About Keras models
+        explain when one should use Sequential or functional API
+        explain compilation step
+        explain weight saving, weight loading
+        explain serialization, deserialization
+    Sequential
+    Model (functional API)
     
     
-class TestPool(unittest.TestCase):
+@keras_test
+def test_masking():
+    np.random.seed(1337)
+    x = np.array([[[1], [1]],
+                  [[0], [0]]])
+    model = Sequential()
+    model.add(Masking(mask_value=0, input_shape=(2, 1)))
+    model.add(TimeDistributed(Dense(1, kernel_initializer='one')))
+    model.compile(loss='mse', optimizer='sgd')
+    y = np.array([[[1], [1]],
+                  [[1], [1]]])
+    loss = model.train_on_batch(x, y)
+    assert loss == 0
     
-        def test_cat_greek_localization(self):
-        self.assertEqual(self.g.get('cat'), 'γάτα')
+        def data_generator():
+        i = 0
+        max_batch_index = len(X_train) // batch_size
+        tot = 0
+        while 1:
+            if tot > 3 * len(X_train):
+                yield np.ones([batch_size, input_dim]) * np.nan, np.ones([batch_size, num_classes]) * np.nan
+            else:
+                yield (X_train[i * batch_size: (i + 1) * batch_size],
+                       y_train[i * batch_size: (i + 1) * batch_size])
+            i += 1
+            tot += 1
+            i = i % max_batch_index
     
-    class ProductionCodeTimeProvider(object):
-    '''
-    Production code version of the time provider (just a wrapper for formatting
-    datetime for this example).
-    '''
-    
-        # commands are just pushed into the command stack
-    command_stack.append(MoveFileCommand('foo.txt', 'bar.txt'))
-    command_stack.append(MoveFileCommand('bar.txt', 'baz.txt'))
-    
-    
-### OUTPUT ###
-# <NumObj: -1>
-# <NumObj: 0>
-# <NumObj: 1>
-# <NumObj: 2>
-# -- committed
-# <NumObj: 3>
-# <NumObj: 4>
-# <NumObj: 5>
-# -- rolled back
-# <NumObj: 2>
-# -- now doing stuff ...
-# -> doing stuff failed!
-# Traceback (most recent call last):
-# File 'memento.py', line 97, in <module>
-#     num_obj.do_stuff()
-#   File 'memento.py', line 52, in transaction
-#     raise e
-#   File 'memento.py', line 49, in transaction
-#     return self.method(obj, *args, **kwargs)
-#   File 'memento.py', line 70, in do_stuff
-#     self.increment()     # <- will fail and rollback
-#   File 'memento.py', line 65, in increment
-#     self.value += 1
-# TypeError: Can't convert 'int' object to str implicitly
-# <NumObj: 2>
-
-    
-        def not_specification(self):
-        raise NotImplementedError()
+    ##
+# imaginary tree navigation type; traverse 'get child' link
+DOWN = 2
+##
+#imaginary tree navigation type; finish with a child list
+UP = 3
