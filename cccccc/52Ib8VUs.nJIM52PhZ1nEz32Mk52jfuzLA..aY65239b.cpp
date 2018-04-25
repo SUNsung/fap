526adc3107420f5021a26e97ee60c03b2e3249eb
SUNsung <weatherius@gmail.com>
2018-04-26 00:53:26 +0300

    Rewrite [52Ib8VUs.nJIM52PhZ1nEz32Mk52jfuzLA..aY65239b.cpp] File in: 2018-04-26 00:53:26

--- cccccc/52Ib8VUs.nJIM52PhZ1nEz32Mk52jfuzLA..aY65239b.cpp
+++ cccccc/52Ib8VUs.nJIM52PhZ1nEz32Mk52jfuzLA..aY65239b.cpp
@@ -1,13 +1,208 @@
 
         
-        #endif
+        #include 'llvm/ADT/SetVector.h'
+    
+    #ifndef SWIFT_SIL_OWNERSHIPCHECKER_H
+#define SWIFT_SIL_OWNERSHIPCHECKER_H
+    
+    /// Index the given module and store the results to \p indexStorePath.
+///
+/// \param module The module to index.
+///
+/// \param indexUnitTokens A list of unique identifiers for the index units to
+/// be written. This may either be one unit per source file of \p module, or it
+/// may be a single unit, in which case all the index information will be
+/// combined into a single unit.
+///
+/// \param moduleUnitToken A unique identifier for this module unit in the form
+/// of a file path. Only used if \p indexUnitTokens are specified for each
+/// source file, otherwise the single \p indexUnitTokens value is used instead.
+///
+/// \param indexStorePath The location to write the indexing data to.
+///
+/// \param indexSystemModules If true, emit index data for imported serialized
+/// swift system modules.
+///
+/// \param isDebugCompilation true for non-optimized compiler invocation.
+///
+/// \param targetTriple The target for this compilation.
+///
+/// \param dependencyTracker The set of dependencies seen while building.
+bool indexAndRecord(ModuleDecl *module, ArrayRef<std::string> indexUnitTokens,
+                    StringRef moduleUnitToken, StringRef indexStorePath,
+                    bool indexSystemModules, bool isDebugCompilation,
+                    StringRef targetTriple,
+                    const DependencyTracker &dependencyTracker);
+// FIXME: indexUnitTokens could be StringRef, but that creates an impedance
+// mismatch in the caller.
+    
+    /// Attempt to get a doc comment from the declaration, or other inherited
+/// sources, like from base classes or protocols.
+Optional<DocComment *> getCascadingDocComment(swift::markup::MarkupContext &MC,
+                                             const Decl *D);
+    
+    SILFunction *SILDebugScope::getParentFunction() const {
+  if (InlinedCallSite)
+    return InlinedCallSite->getParentFunction();
+  if (auto *ParentScope = Parent.dyn_cast<const SILDebugScope *>())
+    return ParentScope->getParentFunction();
+  return Parent.get<SILFunction *>();
+}
 
     
-    #include '../master_worker/common/RPC.hpp'
-#include 'TH/THStorage.h'
     
-    int64_t size(const Tensor& self, int64_t dim) {
-  // false is passed to maybe_wrap_dim so behavior is identical to array access (but with wrapping)
-  dim = maybe_wrap_dim(dim, self.dim(), false);
-  return self.sizes()[dim];
+    {  /// Return a hash code of any components from these options that should
+  /// contribute to a Swift Bridging PCH hash.
+  llvm::hash_code getPCHHashComponents() const {
+    // Nothing here that contributes anything significant when emitting the PCH.
+    return llvm::hash_value(0);
+  }
+};
+    
+    namespace swift {
+    }
+    
+    #include 'swift/AST/Type.h'
+#include 'llvm/ADT/ArrayRef.h'
+#include 'llvm/ADT/Optional.h'
+    
+    /**
+ * Base class for all tesseract APIs.
+ * Specific classes can add ability to work on different inputs or produce
+ * different outputs.
+ * This class is mostly an interface layer on top of the Tesseract instance
+ * class to hide the data types so that users of this class don't have to
+ * include any other Tesseract headers.
+ */
+class TESS_API TessBaseAPI {
+ public:
+  TessBaseAPI();
+  virtual ~TessBaseAPI();
+    }
+    
+    
+    {    FILE* fout_;                  // output file pointer
+    TessResultRenderer* next_;    // Can link multiple renderers together
+    bool happy_;                  // I get grumpy when the disk fills up, etc.
+};
+    
+    namespace tesseract {
+double DotProductAVX(const double* u, const double* v, int n) {
+  fprintf(stderr, 'DotProductAVX can't be used on Android\n');
+  abort();
 }
+}  // namespace tesseract
+    
+    // Factory makes and returns an IntSimdMatrix (sub)class of the best
+// available type for the current architecture.
+/* static */
+IntSimdMatrix* IntSimdMatrix::GetFastestMultiplier() {
+  IntSimdMatrix* multiplier = nullptr;
+  if (SIMDDetect::IsAVX2Available()) {
+    multiplier = new IntSimdMatrixAVX2();
+  } else if (SIMDDetect::IsSSEAvailable()) {
+    multiplier = new IntSimdMatrixSSE();
+  } else {
+    // Default c++ implementation.
+    multiplier = new IntSimdMatrix();
+  }
+  return multiplier;
+}
+    
+      // Rounds the size up to a multiple of the input register size (in int8_t).
+  int RoundInputs(int size) const {
+    return Roundup(size, num_inputs_per_register_);
+  }
+  // Rounds the size up to a multiple of the output register size (in int32_t).
+  int RoundOutputs(int size) const {
+    return Roundup(size, num_outputs_per_register_);
+  }
+    
+    /*************************************************************************
+ * unrej_good_chs()
+ * Unreject POTENTIAL rejects if the blob passes the blob and outline checks
+ *************************************************************************/
+void Tesseract::unrej_good_chs(WERD_RES *word, ROW *row) {
+  if (word->bln_boxes == nullptr ||
+      word->rebuild_word == nullptr || word->rebuild_word->blobs.empty())
+    return;
+    }
+    
+      // Make a copy of part, and reset parts_splitted.
+  ColPartition* right_part = part->CopyButDontOwnBlobs();
+  parts_splitted->delete_data_pointers();
+  parts_splitted->clear();
+    
+    	new_anim = memnew(Button);
+	new_anim->set_flat(true);
+	hbc_animlist->add_child(new_anim);
+	new_anim->set_h_size_flags(SIZE_EXPAND_FILL);
+	new_anim->connect('pressed', this, '_animation_add');
+    
+    void VideoStreamPlaybackTheora::set_mix_callback(AudioMixCallback p_callback, void *p_userdata) {
+    }
+    
+    #if defined(MBEDTLS_PLATFORM_SNPRINTF_MACRO) && !defined(MBEDTLS_PLATFORM_C)
+#error 'MBEDTLS_PLATFORM_SNPRINTF_MACRO defined, but not all prerequisites'
+#endif
+    
+    
+    {		void (*tree_create_node_func)(void *, const NodePath &p_parent, const String &p_type, const String &p_name);
+		void (*tree_instance_node_func)(void *, const NodePath &p_parent, const String &p_path, const String &p_name);
+		void (*tree_remove_node_func)(void *, const NodePath &p_at);
+		void (*tree_remove_and_keep_node_func)(void *, const NodePath &p_at, ObjectID p_keep_id);
+		void (*tree_restore_node_func)(void *, ObjectID p_id, const NodePath &p_at, int p_at_pos);
+		void (*tree_duplicate_node_func)(void *, const NodePath &p_at, const String &p_new_name);
+		void (*tree_reparent_node_func)(void *, const NodePath &p_at, const NodePath &p_new_place, const String &p_new_name, int p_at_pos);
+	};
+    
+    		class_desc->push_indent(1);
+		class_desc->push_table(2);
+		class_desc->set_table_column_expand(1, 1);
+    
+    				//const btTransform& wheelTrans = getWheelTransformWS( i );
+    
+    	// Copy the built assembly to the assemblies directory
+	String api_assembly_dir = api_sln_dir.plus_file('bin').plus_file(api_build_config);
+	if (!GodotSharpBuilds::copy_api_assembly(api_assembly_dir, res_assemblies_dir, api_name, p_api_type))
+		return false;
+    
+    	bool *bucket_cache_visited;
+	Rect2i bucket_cache_rect;
+	int bucket_cache_tile;
+	PoolVector<Vector2> bucket_cache;
+	List<Point2i> bucket_queue;
+    
+    void WakeUpLock::Lock() {
+    ::wakeupLock_Lock(object_);
+}
+    
+    #include <exception>
+#include 'comm/xlogger/xlogger.h'
+    
+    // Licensed under the MIT License (the 'License'); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+// http://opensource.org/licenses/MIT
+    
+    // Licensed under the MIT License (the 'License'); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+// http://opensource.org/licenses/MIT
+    
+    ScopeJEnv::ScopeJEnv(JavaVM* jvm, jint _capacity)
+    : vm_(jvm), env_(NULL), we_attach_(false), status_(0) {
+    ASSERT(jvm);
+    do {
+        env_ = (JNIEnv*)pthread_getspecific(g_env_key);
+        
+        if (NULL != env_) {
+            break;
+        }
+        
+        status_ = vm_->GetEnv((void**) &env_, JNI_VERSION_1_6);
+    }
+    }
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
