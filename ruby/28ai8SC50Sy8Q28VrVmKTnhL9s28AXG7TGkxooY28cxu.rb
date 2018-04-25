bb2c470d70dd318b97fcbc979d2d206448d88c6a
SUNsung <weatherius@gmail.com>
2018-04-25 22:10:29 +0300

    Rewrite [28ai8SC50Sy8Q28VrVmKTnhL9s28AXG7TGkxooY28cxu.rb] File in: 2018-04-25 22:10:29

--- ruby/28ai8SC50Sy8Q28VrVmKTnhL9s28AXG7TGkxooY28cxu.rb
+++ ruby/28ai8SC50Sy8Q28VrVmKTnhL9s28AXG7TGkxooY28cxu.rb
@@ -1,47 +1,96 @@
 
         
-              # Set the correct feed URL.
-      self.data['feed_url'] = '#{category_dir}/#{name}'
-    end
-    
-    class ConfigTag < Liquid::Tag
-  def initialize(tag_name, options, tokens)
-    super
-    options = options.split(' ').map {|i| i.strip }
-    @key = options.slice!(0)
-    @tag = nil
-    @classname = nil
-    options.each do |option|
-      @tag = $1 if option =~ /tag:(\S+)/ 
-      @classname = $1 if option =~ /classname:(\S+)/
-    end
+          test 'token_and_options returns correct token with slashes' do
+    token = 'rcHu+\\\\'/896A'
+    actual = ActionController::HttpAuthentication::Token.token_and_options(sample_request(token)).first
+    expected = token
+    assert_equal(expected, actual)
   end
     
-          rtn = ''
-      (context.environments.first['site'][@array_name] || []).each do |file|
-        if file !~ /^[a-zA-Z0-9_\/\.-]+$/ || file =~ /\.\// || file =~ /\/\./
-          rtn = rtn + 'Include file '#{file}' contains invalid characters or sequences'
+          result = ActionsController.action(:index).call(env_for('/'))
+      assert_nil result[1]['Middleware-Order']
+    end
+    
+    module ActionMailer
+  # The <tt>ActionMailer::DeliveryJob</tt> class is used when you
+  # want to send emails outside of the request-response cycle.
+  #
+  # Exceptions are rescued and handled by the mailer class.
+  class DeliveryJob < ActiveJob::Base # :nodoc:
+    queue_as { ActionMailer::Base.deliver_later_queue_name }
+    
+    require 'active_support/log_subscriber'
+    
+            def initialize(argv)
+          @pod_name = argv.shift_argument
+          @wipe_all = argv.flag?('all')
+          super
         end
     
-          unless file.file?
-        return 'File #{file} could not be found'
+            def run
+          UI.puts('$CACHE_ROOT: #{@cache.root}') if @short_output
+          if @pod_name.nil? # Print all
+            @cache.cache_descriptors_per_pod.each do |pod_name, cache_descriptors|
+              print_pod_cache_infos(pod_name, cache_descriptors)
+            end
+          else # Print only for the requested pod
+            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
+            if cache_descriptors.nil?
+              UI.notice('No cache for pod named #{@pod_name} found')
+            else
+              print_pod_cache_infos(@pod_name, cache_descriptors)
+            end
+          end
+        end
+    
+            def block_argument?
+          argument? && @scope.node.block_type?
+        end
+    
+    module RuboCop
+  module Cop
+    module Layout
+      # Checks for unnecessary additional spaces inside array percent literals
+      # (i.e. %i/%w).
+      #
+      # @example
+      #
+      #   # bad
+      #   %w(foo  bar  baz)
+      #   # good
+      #   %i(foo bar baz)
+      class SpaceInsideArrayPercentLiteral < Cop
+        include MatchRange
+        include PercentLiteral
+    
+    module RuboCop
+  module Cop
+    module Lint
+      # Don't omit the accumulator when calling `next` in a `reduce` block.
+      #
+      # @example
+      #
+      #   # bad
+      #
+      #   result = (1..4).reduce(0) do |acc, i|
+      #     next if i.odd?
+      #     acc + i
+      #   end
+      #
+      # @example
+      #
+      #   # good
+      #
+      #   result = (1..4).reduce(0) do |acc, i|
+      #     next acc if i.odd?
+      #     acc + i
+      #   end
+      class NextWithoutAccumulator < Cop
+        MSG = 'Use `next` with an accumulator argument in a `reduce`.'.freeze
+    
+              [optarg_positions, arg_positions]
+        end
       end
-    
-        def poster
-      'poster='#{@poster}'' if @poster
     end
-    
-            def matches? subject
-          @subject = subject
-          @subject = @subject.new if @subject.class == Class
-          @allowed_types && @rejected_types &&
-          allowed_types_allowed? && rejected_types_rejected?
-        end
-    
-      # Get list of styles saved on previous deploy (running rake paperclip:refresh:missing_styles)
-  def self.get_registered_attachments_styles
-    YAML.load_file(Paperclip.registered_attachments_styles_path)
-  rescue Errno::ENOENT
-    nil
   end
-  private_class_method :get_registered_attachments_styles
+end
