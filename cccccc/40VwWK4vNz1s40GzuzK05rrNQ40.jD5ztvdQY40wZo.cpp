e52d90fd86bc0c8514eff54eccf5fd0e58db97c1
SUNsung <weatherius@gmail.com>
2018-04-25 03:13:52 +0300

    Rewrite [40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp] File in: 2018-04-25 03:13:52

--- cccccc/40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp
+++ cccccc/40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp
@@ -1,341 +1,159 @@
 
         
-        /** Change ',' to '.' everywhere in buffer.
+        #include <chrono>
+    
+    #include 'hphp/runtime/vm/jit/vasm-unit.h'
+    
+      Variant ret = mpzToGMPObject(gmpReturn);
+    
+    namespace irgen {
+    }
+    
+    /*
+ * Emit a function prologue from rec.
  *
- * We had a sophisticated way, but it did not work in WinCE.
- * @see https://github.com/open-source-parsers/jsoncpp/pull/9
+ * Precondition: calling thread owns both code and metadata locks
  */
-static inline void fixNumericLocale(char* begin, char* end) {
-  while (begin < end) {
-    if (*begin == ',') {
-      *begin = '.';
-    }
-    ++begin;
-  }
+TCA emitFuncPrologueOptInternal(ProfTransRec* rec);
+    
+    
+    {/////////////////////////////////////////////////////////////////////////////
 }
-    
-    // Find a file by file name.
-bool PyDescriptorDatabase::FindFileByName(const string& filename,
-                                          FileDescriptorProto* output) {
-  ScopedPyObjectPtr py_descriptor(PyObject_CallMethod(
-      py_database_, 'FindFileByName', 's#', filename.c_str(), filename.size()));
-  return GetFileDescriptorProto(py_descriptor.get(), output);
-}
-    
-    #include <google/protobuf/testing/googletest.h>
-#include <gtest/gtest.h>
-#include <google/protobuf/testing/file.h>
-    
-    #include <string>
-    
-    
-    
-    #include <google/protobuf/compiler/csharp/csharp_source_generator_base.h>
-#include <google/protobuf/compiler/csharp/csharp_helpers.h>
-#include <google/protobuf/compiler/csharp/csharp_names.h>
-#include <google/protobuf/compiler/csharp/csharp_options.h>
-    
-      std::string class_access_level();
-  const Options* options();
-    
-    TEST(JavaDocCommentTest, Escaping) {
-  EXPECT_EQ('foo /&#42; bar *&#47; baz', EscapeJavadoc('foo /* bar */ baz'));
-  EXPECT_EQ('foo /&#42;&#47; baz', EscapeJavadoc('foo /*/ baz'));
-  EXPECT_EQ('{&#64;foo}', EscapeJavadoc('{@foo}'));
-  EXPECT_EQ('&lt;i&gt;&amp;&lt;/i&gt;', EscapeJavadoc('<i>&</i>'));
-  EXPECT_EQ('foo&#92;u1234bar', EscapeJavadoc('foo\\u1234bar'));
-  EXPECT_EQ('&#64;deprecated', EscapeJavadoc('@deprecated'));
-}
-    
-    #include <google/protobuf/stubs/common.h>
-#include <google/protobuf/compiler/java/java_extension.h>
-    
-    namespace google {
-namespace protobuf {
-  class FieldDescriptor;         // descriptor.h
-  class Descriptor;              // descriptor.h
-  class ServiceDescriptor;       // descriptor.h
-  namespace compiler {
-    namespace java {
-      class MessageGenerator;    // message.h
-      class ExtensionGenerator;  // extension.h
-      class ServiceGenerator;    // service.h
-      class Context;             // context.h
-    }
-  }
-}
-    }
-    
-    ImmutableLazyMessageFieldGenerator::
-ImmutableLazyMessageFieldGenerator(
-    const FieldDescriptor* descriptor,
-    int messageBitIndex,
-    int builderBitIndex,
-    Context* context)
-    : ImmutableMessageFieldGenerator(
-          descriptor, messageBitIndex, builderBitIndex, context) {
-}
-    
-      char label_i;
-  char label_j;
-  char* pixels = new char[2 * rows * cols];
-  std::string value;
-    
-      void Transform(const Datum& datum, Dtype* transformed_data);
-  // Tranformation parameters
-  TransformationParameter param_;
-    
-    #endif  // CAFFE_CONCAT_LAYER_HPP_
+#endif
 
     
-      vector<cudnnTensorDescriptor_t> bottom_descs_, top_descs_;
-  cudnnTensorDescriptor_t    bias_desc_;
-  cudnnFilterDescriptor_t      filter_desc_;
-  vector<cudnnConvolutionDescriptor_t> conv_descs_;
-  int bottom_offset_, top_offset_, bias_offset_;
+    // Reads the Start of Frame (SOF) marker segment and fills in *jpg with the
+// parsed data.
+bool ProcessSOF(const uint8_t* data, const size_t len,
+                JpegReadMode mode, size_t* pos, JPEGData* jpg) {
+  if (jpg->width != 0) {
+    fprintf(stderr, 'Duplicate SOF marker.\n');
+    jpg->error = JPEG_DUPLICATE_SOF;
+    return false;
+  }
+  const size_t start_pos = *pos;
+  VERIFY_LEN(8);
+  size_t marker_len = ReadUint16(data, pos);
+  int precision = ReadUint8(data, pos);
+  int height = ReadUint16(data, pos);
+  int width = ReadUint16(data, pos);
+  int num_components = ReadUint8(data, pos);
+  VERIFY_INPUT(precision, 8, 8, PRECISION);
+  VERIFY_INPUT(height, 1, 65535, HEIGHT);
+  VERIFY_INPUT(width, 1, 65535, WIDTH);
+  VERIFY_INPUT(num_components, 1, kMaxComponents, NUMCOMP);
+  VERIFY_LEN(3 * num_components);
+  jpg->height = height;
+  jpg->width = width;
+  jpg->components.resize(num_components);
+    }
     
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    OutputImageComponent::OutputImageComponent(int w, int h)
+    : width_(w), height_(h) {
+  Reset(1, 1);
+}
     
-    #endif  // CAFFE_CUDNN_SOFTMAX_LAYER_HPP_
-
-    
-    
-    {  /// when divided by UINT_MAX, the randomly generated values @f$u\sim U(0,1)@f$
-  Blob<unsigned int> rand_vec_;
-  /// the probability @f$ p @f$ of dropping any input
-  Dtype threshold_;
-  /// the scale for undropped inputs at train time @f$ 1 / (1 - p) @f$
-  Dtype scale_;
-  unsigned int uint_thres_;
+    static const int kCbToGreenTable[256] = {
+  2919680,  2897126,  2874572,  2852018,  2829464,  2806910,  2784356,  2761802,
+  2739248,  2716694,  2694140,  2671586,  2649032,  2626478,  2603924,  2581370,
+  2558816,  2536262,  2513708,  2491154,  2468600,  2446046,  2423492,  2400938,
+  2378384,  2355830,  2333276,  2310722,  2288168,  2265614,  2243060,  2220506,
+  2197952,  2175398,  2152844,  2130290,  2107736,  2085182,  2062628,  2040074,
+  2017520,  1994966,  1972412,  1949858,  1927304,  1904750,  1882196,  1859642,
+  1837088,  1814534,  1791980,  1769426,  1746872,  1724318,  1701764,  1679210,
+  1656656,  1634102,  1611548,  1588994,  1566440,  1543886,  1521332,  1498778,
+  1476224,  1453670,  1431116,  1408562,  1386008,  1363454,  1340900,  1318346,
+  1295792,  1273238,  1250684,  1228130,  1205576,  1183022,  1160468,  1137914,
+  1115360,  1092806,  1070252,  1047698,  1025144,  1002590,   980036,   957482,
+   934928,   912374,   889820,   867266,   844712,   822158,   799604,   777050,
+   754496,   731942,   709388,   686834,   664280,   641726,   619172,   596618,
+   574064,   551510,   528956,   506402,   483848,   461294,   438740,   416186,
+   393632,   371078,   348524,   325970,   303416,   280862,   258308,   235754,
+   213200,   190646,   168092,   145538,   122984,   100430,    77876,    55322,
+    32768,    10214,   -12340,   -34894,   -57448,   -80002,  -102556,  -125110,
+  -147664,  -170218,  -192772,  -215326,  -237880,  -260434,  -282988,  -305542,
+  -328096,  -350650,  -373204,  -395758,  -418312,  -440866,  -463420,  -485974,
+  -508528,  -531082,  -553636,  -576190,  -598744,  -621298,  -643852,  -666406,
+  -688960,  -711514,  -734068,  -756622,  -779176,  -801730,  -824284,  -846838,
+  -869392,  -891946,  -914500,  -937054,  -959608,  -982162, -1004716, -1027270,
+ -1049824, -1072378, -1094932, -1117486, -1140040, -1162594, -1185148, -1207702,
+ -1230256, -1252810, -1275364, -1297918, -1320472, -1343026, -1365580, -1388134,
+ -1410688, -1433242, -1455796, -1478350, -1500904, -1523458, -1546012, -1568566,
+ -1591120, -1613674, -1636228, -1658782, -1681336, -1703890, -1726444, -1748998,
+ -1771552, -1794106, -1816660, -1839214, -1861768, -1884322, -1906876, -1929430,
+ -1951984, -1974538, -1997092, -2019646, -2042200, -2064754, -2087308, -2109862,
+ -2132416, -2154970, -2177524, -2200078, -2222632, -2245186, -2267740, -2290294,
+ -2312848, -2335402, -2357956, -2380510, -2403064, -2425618, -2448172, -2470726,
+ -2493280, -2515834, -2538388, -2560942, -2583496, -2606050, -2628604, -2651158,
+ -2673712, -2696266, -2718820, -2741374, -2763928, -2786482, -2809036, -2831590,
 };
     
-    /**
- * @brief Computes @f$ y = \gamma ^ {\alpha x + \beta} @f$,
- *        as specified by the scale @f$ \alpha @f$, shift @f$ \beta @f$,
- *        and base @f$ \gamma @f$.
- */
-template <typename Dtype>
-class ExpLayer : public NeuronLayer<Dtype> {
- public:
-  /**
-   * @param param provides ExpParameter exp_param,
-   *     with ExpLayer options:
-   *   - scale (\b optional, default 1) the scale @f$ \alpha @f$
-   *   - shift (\b optional, default 0) the shift @f$ \beta @f$
-   *   - base (\b optional, default -1 for a value of @f$ e \approx 2.718 @f$)
-   *         the base @f$ \gamma @f$
-   */
-  explicit ExpLayer(const LayerParameter& param)
-      : NeuronLayer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    
-    {  assert(msg.isEmpty());
-  try {
-    double arg6 = unpackFloat(msg);
-    assert(false);
-  } catch (exception &e) {}
-  std::cout << 'OK' << std::endl;
-  return 0;
-}
+    #endif  // GUETZLI_DCT_DOUBLE_H_
 
     
-    void * CUDAGenerator::unsafeGetTH() {
-  return (void *) generator;
-}
-    
-    inline void store_scalar(void* data, at::ScalarType scalarType, PyObject* obj) {
-  switch (scalarType) {
-    case at::kByte: *(uint8_t*)data = (uint8_t)THPUtils_unpackLong(obj); break;
-    case at::kChar: *(char*)data = (char)THPUtils_unpackLong(obj); break;
-    case at::kShort: *(int16_t*)data = (int16_t)THPUtils_unpackLong(obj); break;
-    case at::kInt: *(int32_t*)data = (int32_t)THPUtils_unpackLong(obj); break;
-    case at::kLong: *(int64_t*)data = THPUtils_unpackLong(obj); break;
-    case at::kHalf:
-      *(at::Half*)data = at::convert<at::Half, double>(THPUtils_unpackDouble(obj));
-      break;
-    case at::kFloat: *(float*)data = (float)THPUtils_unpackDouble(obj); break;
-    case at::kDouble: *(double*)data = THPUtils_unpackDouble(obj); break;
-    default: throw std::runtime_error('invalid type');
-  }
-}
-    
-      auto ilen = input_size[0];
-  auto batchSize = input_size[1];
-  auto inputPlanes = input_size[2];
-  auto outputPlanes = weight_size[2];
-  auto kw = weight_size[0];
-  auto olen = input_size[0] - kw + 1 + pad * 2;
-  auto real_pad = (olen - ilen + kw - 1) / 2;
-    
-    template <typename C>
-struct compare_less : detail::cmp_pred<C, ordering::lt, 0> {
-  using detail::cmp_pred<C, ordering::lt, 0>::cmp_pred;
-};
-    
-    /**
- * Wrapper around AsyncIO that allows you to schedule more requests than
- * the AsyncIO's object capacity.  Other requests are queued and processed
- * in a FIFO order.
- */
-class AsyncIOQueue {
- public:
-  /**
-   * Create a queue, using the given AsyncIO object.
-   * The AsyncIO object may not be used by anything else until the
-   * queue is destroyed.
-   */
-  explicit AsyncIOQueue(AsyncIO* asyncIO);
-  ~AsyncIOQueue();
+    namespace guetzli {
     }
     
+    #include <stdint.h>
+#include <string.h>
+#include <vector>
     
-    {std::string AsyncFileWriter::getNumDiscardedMsg(size_t numDiscarded) {
-  // We may want to make this customizable in the future (e.g., to allow it to
-  // conform to the LogFormatter style being used).
-  // For now just return a simple fixed message.
-  return folly::to<std::string>(
-      numDiscarded,
-      ' log messages discarded: logging faster than we can write\n');
-}
-} // namespace folly
-
-    
-    /**
- * FileHandlerFactory is a LogHandlerFactory that constructs log handlers
- * that write to a file.
- *
- * Note that FileHandlerFactory allows opening and appending to arbitrary files
- * based on the handler options.  This may make it unsafe to use
- * FileHandlerFactory in some contexts: for instance, a setuid binary should
- * generally avoid registering the FileHandlerFactory if they allow log
- * handlers to be configured via command line parameters, since otherwise this
- * may allow non-root users to append to files that they otherwise would not
- * have write permissions for.
- */
-class FileHandlerFactory : public LogHandlerFactory {
- public:
-  StringPiece getType() const override {
-    return 'file';
-  }
+    namespace guetzli {
     }
     
+    double ButteraugliScoreForQuality(double quality) {
+  if (quality < kLowestQuality) quality = kLowestQuality;
+  if (quality > kHighestQuality) quality = kHighestQuality;
+  int index = static_cast<int>(quality);
+  double mix = quality - index;
+  return kScoreForQuality[index - kLowestQuality] * (1 - mix) +
+      kScoreForQuality[index - kLowestQuality + 1] * mix;
+}
     
-    {private:
-  DictionaryEntry DE[kMaxDictSize];
-  size_t Size = 0;
+    #include 'debug_hud.h'
+#include 'imgui.h'
+    
+    struct VERTEX_CONSTANT_BUFFER
+{
+    float        mvp[4][4];
 };
     
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you use this binding you'll need to call 4 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXXX_NewFrame(), ImGui::Render() and ImGui_ImplXXXX_Shutdown().
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
     
-    {
-    {    std::string ArtifactPath = 'minimized-from-' + Hash(U);
-    Cmd += ' -minimize_crash_internal_step=1 -exact_artifact_path=' +
-        ArtifactPath;
-    Printf('CRASH_MIN: executing: %s\n', Cmd.c_str());
-    ExitCode = ExecuteCommand(Cmd);
-    if (ExitCode == 0) {
-      if (Flags.exact_artifact_path) {
-        CurrentFilePath = Flags.exact_artifact_path;
-        WriteToFile(U, CurrentFilePath);
-      }
-      Printf('CRASH_MIN: failed to minimize beyond %s (%d bytes), exiting\n',
-             CurrentFilePath.c_str(), U.size());
+     private:
+  std::shared_ptr<Logger> info_log_;
+  const unique_ptr<SequentialFileReader> file_;
+  Reporter* const reporter_;
+  bool const checksum_;
+  char* const backing_store_;
+  Slice buffer_;
+  bool eof_;   // Last Read() indicated EOF by returning < kBlockSize
+  bool read_error_;   // Error occurred while reading from file
+    
+      const ImmutableMemTableOptions* GetImmutableMemTableOptions() const {
+    return &moptions_;
+  }
+    
+    
+    {  int operator()(const char* a, const DecodedType b) const {
+    if (Decode(a) < b) {
+      return -1;
+    } else if (Decode(a) > b) {
+      return +1;
+    } else {
       return 0;
     }
-    CurrentFilePath = ArtifactPath;
-    Printf('\n\n\n\n\n\n*********************************\n');
   }
-  return 0;
-}
+};
     
-    void ListFilesInDirRecursive(const std::string &Dir, long *Epoch,
-                             std::vector<std::string> *V, bool TopDir) {
-  auto E = GetEpoch(Dir);
-  if (Epoch)
-    if (E && *Epoch >= E) return;
-    }
-    
-    
-    {    size_t BlockCoverage;
-    size_t CallerCalleeCoverage;
-    // Precalculated number of bits in CounterBitmap.
-    size_t CounterBitmapBits;
-    std::vector<uint8_t> CounterBitmap;
-    ValueBitMap VPMap;
-  };
-    
-    #if defined(__has_include)
-#if __has_include(<sanitizer / coverage_interface.h>)
-#include <sanitizer/coverage_interface.h>
-#endif
-#if __has_include(<sanitizer / lsan_interface.h>)
-#include <sanitizer/lsan_interface.h>
-#endif
-#endif
-    
-    
-    {  // Execute the inner process untill it passes.
-  // Every inner process should execute at least one input.
-  std::string BaseCmd = CloneArgsWithoutX(Args, 'keep-all-flags');
-  for (size_t i = 1; i <= AllFiles.size(); i++) {
-    Printf('MERGE-OUTER: attempt %zd\n', i);
-    auto ExitCode =
-        ExecuteCommand(BaseCmd + ' -merge_control_file=' + CFPath);
-    if (!ExitCode) {
-      Printf('MERGE-OUTER: succesfull in %zd attempt(s)\n', i);
-      break;
+    void TestKillRandom(std::string kill_point, int odds,
+                    const std::string& srcfile, int srcline) {
+  for (auto& p : rocksdb_kill_prefix_blacklist) {
+    if (kill_point.substr(0, p.length()) == p) {
+      return;
     }
   }
-  // Read the control file and do the merge.
-  Merger M;
-  std::ifstream IF(CFPath);
-  M.ParseOrExit(IF, true);
-  IF.close();
-  std::vector<std::string> NewFiles;
-  size_t NumNewFeatures = M.Merge(&NewFiles);
-  Printf('MERGE-OUTER: %zd new files with %zd new features added\n',
-         NewFiles.size(), NumNewFeatures);
-  for (auto &F: NewFiles)
-    WriteToOutputCorpus(FileToVector(F));
-  // We are done, delete the control file.
-  RemoveFile(CFPath);
-}
-    
-    void TracePC::PrintNewPCs() {
-  if (DoPrintNewPCs) {
-    if (!PrintedPCs)
-      PrintedPCs = new std::set<uintptr_t>;
-    for (size_t i = 1; i < GetNumPCs(); i++)
-      if (PCs[i] && PrintedPCs->insert(PCs[i]).second)
-        PrintPC('\tNEW_PC: %p %F %L\n', '\tNEW_PC: %p\n', PCs[i]);
-  }
-}
-    
-    int TraceState::TryToAddDesiredData(uint64_t PresentData, uint64_t DesiredData,
-                                    size_t DataSize) {
-  if (NumMutations >= kMaxMutations || !WantToHandleOneMoreMutation()) return 0;
-  ScopedDoingMyOwnMemmem scoped_doing_my_own_memmem;
-  const uint8_t *UnitData;
-  auto UnitSize = F->GetCurrentUnitInFuzzingThead(&UnitData);
-  int Res = 0;
-  const uint8_t *Beg = UnitData;
-  const uint8_t *End = Beg + UnitSize;
-  for (const uint8_t *Cur = Beg; Cur < End; Cur++) {
-    Cur = (uint8_t *)SearchMemory(Cur, End - Cur, &PresentData, DataSize);
-    if (!Cur)
-      break;
-    size_t Pos = Cur - Beg;
-    assert(Pos < UnitSize);
-    AddMutation(Pos, DataSize, DesiredData);
-    AddMutation(Pos, DataSize, DesiredData + 1);
-    AddMutation(Pos, DataSize, DesiredData - 1);
-    Res++;
-  }
-  return Res;
-}
+    }
