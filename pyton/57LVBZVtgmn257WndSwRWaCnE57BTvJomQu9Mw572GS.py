06e21991af11abc79007fb63c3f796dca35550ac
SUNsung <weatherius@gmail.com>
2018-04-25 08:04:27 +0300

    Rewrite [57LVBZVtgmn257WndSwRWaCnE57BTvJomQu9Mw572GS.py] File in: 2018-04-25 08:04:26

--- pyton/57LVBZVtgmn257WndSwRWaCnE57BTvJomQu9Mw572GS.py
+++ pyton/57LVBZVtgmn257WndSwRWaCnE57BTvJomQu9Mw572GS.py
@@ -1,105 +1,155 @@
 
         
-        
-def build_output_stream(args, env, request, response, output_options):
-    '''Build and return a chain of iterators over the `request`-`response`
-    exchange each of which yields `bytes` chunks.
+        import os
+import sys
+import codecs
+import re
     
+        @support.cpython_only
+    @support.bigmemtest(sys.maxsize + 1000, memuse=2/15 * 2, dry_run=False)
+    def test_huge_lshift(self, size):
+        self.assertEqual(1 << (sys.maxsize + 1000), 1 << 1000 << sys.maxsize)
     
-def rst_filenames():
-    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
-        if '.tox' not in root:
-            for filename in fnmatch.filter(filenames, '*.rst'):
-                yield os.path.join(root, filename)
+            self.assertIn(entry.get(), 'hello')
+        egi = entry.grid_info()
+        equal(int(egi['row']), 0)
+        equal(int(egi['column']), 1)
+        equal(int(egi['rowspan']), 1)
+        equal(int(egi['columnspan']), 1)
+        equal(self.dialog.row, 1)
     
-    
-def test_follow_redirect_output_options(httpbin):
-    r = http('--check-status',
-             '--follow',
-             '--all',
-             '--print=h',
-             '--history-print=H',
-             httpbin.url + '/redirect/2')
-    assert r.count('GET /') == 2
-    assert 'HTTP/1.1 302 FOUND' not in r
-    assert HTTP_OK in r
-    
-    
-class ProgressReporterThread(threading.Thread):
-    '''
-    Reports download progress based on its status.
-    
-        def __init__(self, **kwargs):
+        def read(self, size=-1):
+        '''Read at most size bytes from the chunk.
+        If size is omitted or negative, read until the end
+        of the chunk.
         '''
-        :param env: an class:`Environment` instance
-        :param kwargs: additional keyword argument that some
-                       processor might require.
     
-        @staticmethod
-    def make_header(username, password):
-        credentials = u'%s:%s' % (username, password)
-        token = b64encode(credentials.encode('utf8')).strip().decode('latin1')
-        return 'Basic %s' % token
-    
-        '''
-)
-positional.add_argument(
-    'items',
-    metavar='REQUEST_ITEM',
-    nargs=ZERO_OR_MORE,
-    default=None,
-    type=KeyValueArgType(*SEP_GROUP_ALL_ITEMS),
-    help=r'''
-    Optional key-value pairs to be included in the request. The separator used
-    determines the type:
-    
-        # Used only when requested with --check-status:
-    ERROR_HTTP_3XX = 3
-    ERROR_HTTP_4XX = 4
-    ERROR_HTTP_5XX = 5
+    __all__ = ['make_scanner']
     
     
-ENTRY_POINT_NAMES = [
-    'httpie.plugins.auth.v1',
-    'httpie.plugins.formatter.v1',
-    'httpie.plugins.converter.v1',
-    'httpie.plugins.transport.v1',
-]
+
+if __name__ == '__main__':
+    colordb = get_colordb('/usr/openwin/lib/rgb.txt')
+    if not colordb:
+        print('No parseable color database found')
+        sys.exit(1)
+    # on my system, this color matches exactly
+    target = 'navy'
+    red, green, blue = rgbtuple = colordb.find_byname(target)
+    print(target, ':', red, green, blue, triplet_to_rrggbb(rgbtuple))
+    name, aliases = colordb.find_byrgb(rgbtuple)
+    print('name:', name, 'aliases:', COMMASPACE.join(aliases))
+    r, g, b = (1, 1, 128)                         # nearest to navy
+    r, g, b = (145, 238, 144)                     # nearest to lightgreen
+    r, g, b = (255, 251, 250)                     # snow
+    print('finding nearest to', target, '...')
+    import time
+    t0 = time.time()
+    nearest = colordb.nearest(r, g, b)
+    t1 = time.time()
+    print('found nearest color', nearest, 'in', t1-t0, 'seconds')
+    # dump the database
+    for n in colordb.unique_names():
+        r, g, b = colordb.find_byname(n)
+        aliases = colordb.aliases_of(r, g, b)
+        print('%20s: (%3d/%3d/%3d) == %s' % (n, r, g, b,
+                                             SPACE.join(aliases[1:])))
+
     
-        def write(self, content):
-        self.fd.write(content + '\r\n')
-        self.fd.flush()
+            methods = {}
     
-    - CommonTokenStream: A basic and most commonly used TokenStream
-  implementation.
-- TokenRewriteStream: A modification of CommonTokenStream that allows the
-  stream to be altered (by the Parser). See the 'tweak' example for a usecase.
+            # COM methods are boolean True:
+        if sys.platform == 'win32':
+            mth = WINFUNCTYPE(None)(42, 'name', (), None)
+            self.assertEqual(bool(mth), True)
+    
+        # __set__ and __get__ should raise a TypeError in case their self
+    # argument is not a ctype instance.
+    def test___set__(self):
+        class MyCStruct(Structure):
+            _fields_ = (('field', c_int),)
+        self.assertRaises(TypeError,
+                          MyCStruct.field.__set__, 'wrong type self', 42)
+    
+            for b in False, True:
+            for i in 0, 1, 2:
+                self.assertEqual(b**i, int(b)**i)
+                self.assertIsNot(b**i, bool(int(b)**i))
     
     
-    def skip(self):
-        '''
-        Instruct the lexer to skip creating a token for current lexer rule
-        and look for another token.  nextToken() knows to keep looking when
-        a lexer rule finishes with token set to SKIP_TOKEN.  Recall that
-        if token==null at end of any token rule, it creates one for you
-        and emits it.
-        '''
-        
-        self._state.token = SKIP_TOKEN
+@click.command()
+@click.argument('catalog_file', type=click.Path())
+def cli(catalog_file):
+    # Read the old ones back.  Once we are in, we will never go.
+    with open(catalog_file) as f:
+        rv = json.load(f)['supported_locales']
     
-            Terence implemented packed table initializers, because Java has a
-        size restriction on .class files and the lookup tables can grow
-        pretty large. The generated JavaLexer.java of the Java.g example
-        would be about 15MB with uncompressed array initializers.
+      return YouCompleteMe( user_options_store.GetAll() )
+
     
-        Snippets without code (only comments) or containing lines starting with ??? should not yeld files,
-    but the counter for naming snippets should still increment.
-    '''
-    parser = argparse.ArgumentParser(description='Split md file into plain text and code blocks')
-    parser.add_argument('sourcefile',
-                        help='which file to read')
-    parser.add_argument('targetfile',
-                        help='where to put plain text')
-    parser.add_argument('codedir',
-                        help='where to put codeblocks')
-    args = parser.parse_args()
+        # For pickling to work, the __module__ variable needs to be set to the frame
+    # where the named tuple is created.  Bypass this step in enviroments where
+    # sys._getframe is not defined (Jython for example).
+    if hasattr(_sys, '_getframe'):
+        result.__module__ = _sys._getframe(1).f_globals.get('__name__', '__main__')
+    
+            try:
+            result = self.fn(*self.args, **self.kwargs)
+        except BaseException:
+            e = sys.exc_info()[1]
+            self.future.set_exception(e)
+        else:
+            self.future.set_result(result)
+    
+    def main():
+    for name, fn in [('sequential', sequential),
+                     ('processes', with_process_pool_executor),
+                     ('threads', with_thread_pool_executor)]:
+        sys.stdout.write('%s: ' % name.ljust(12))
+        start = time.time()
+        if fn() != [True] * len(PRIMES):
+            sys.stdout.write('failed\n')
+        else:
+            sys.stdout.write('%.2f seconds\n' % (time.time() - start))
+    
+            f1 = create_future(state=PENDING)
+        t = threading.Thread(target=notification)
+        t.start()
+    
+        return _MasterDiagnosticFilter( compiled_by_type )
+    
+      opts = { 'filter_diagnostics' : {
+    'java' : { 'regex' : '.*taco.*' },
+    'xml'  : { 'regex' : '.*burrito.*' } } }
+    
+    
+def KeywordsFromSyntaxListOutput_CppSyntax_test():
+  expected_keywords = (
+    'int_fast32_t', 'FILE', 'size_t', 'bitor', 'typedef', 'const', 'struct',
+    'uint8_t', 'fpos_t', 'thread_local', 'unsigned', 'uint_least16_t', 'do',
+    'intptr_t', 'uint_least64_t', 'return', 'auto', 'void', '_Complex',
+    'break', '_Alignof', 'not', 'using', '_Static_assert', '_Thread_local',
+    'public', 'uint_fast16_t', 'this', 'continue', 'char32_t', 'int16_t',
+    'intmax_t', 'static', 'clock_t', 'sizeof', 'int_fast64_t', 'mbstate_t',
+    'try', 'xor', 'uint_fast32_t', 'int_least8_t', 'div_t', 'volatile',
+    'template', 'char16_t', 'new', 'ldiv_t', 'int_least16_t', 'va_list',
+    'uint_least8_t', 'goto', 'noreturn', 'enum', 'static_assert', 'bitand',
+    'compl', 'imaginary', 'jmp_buf', 'throw', 'asm', 'ptrdiff_t', 'uint16_t',
+    'or', 'uint_fast8_t', '_Bool', 'int32_t', 'float', 'private', 'restrict',
+    'wint_t', 'operator', 'not_eq', '_Imaginary', 'alignas', 'union', 'long',
+    'uint_least32_t', 'int_least64_t', 'friend', 'uintptr_t', 'int8_t', 'else',
+    'export', 'int_fast8_t', 'catch', 'true', 'case', 'default', 'double',
+    '_Noreturn', 'signed', 'typename', 'while', 'protected', 'wchar_t',
+    'wctrans_t', 'uint64_t', 'delete', 'and', 'register', 'false', 'int',
+    'uintmax_t', 'off_t', 'char', 'int64_t', 'int_fast16_t', 'DIR', '_Atomic',
+    'time_t', 'xor_eq', 'namespace', 'virtual', 'complex', 'bool', 'mutable',
+    'if', 'int_least32_t', 'sig_atomic_t', 'and_eq', 'ssize_t', 'alignof',
+    '_Alignas', '_Generic', 'extern', 'class', 'typeid', 'short', 'for',
+    'uint_fast64_t', 'wctype_t', 'explicit', 'or_eq', 'switch', 'uint32_t',
+    'inline' )
+    
+      python_interpreter = vim.eval( 'g:ycm_server_python_interpreter' )
+  if python_interpreter:
+    python_interpreter = utils.FindExecutable( python_interpreter )
+    if python_interpreter:
+      return python_interpreter
