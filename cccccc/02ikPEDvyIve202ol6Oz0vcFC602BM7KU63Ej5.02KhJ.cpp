5b1e20517850e69b2e772b72ad70a0002cf44682
SUNsung <weatherius@gmail.com>
2018-04-25 06:40:53 +0300

    Rewrite [02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp] File in: 2018-04-25 06:40:53

--- cccccc/02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp
+++ cccccc/02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp
@@ -1,170 +1,256 @@
 
         
-        
-    {  while (!got_sigint && !worker.Done()) {
-    gpr_sleep_until(gpr_time_add(gpr_now(GPR_CLOCK_REALTIME),
-                                 gpr_time_from_seconds(5, GPR_TIMESPAN)));
+          // Read until size drops significantly.
+  std::string limit_key = Key(n);
+  for (int read = 0; true; read++) {
+    ASSERT_LT(read, 100) << 'Taking too long to compact';
+    Iterator* iter = db_->NewIterator(ReadOptions());
+    for (iter->SeekToFirst();
+         iter->Valid() && iter->key().ToString() < limit_key;
+         iter->Next()) {
+      // Drop data
+    }
+    delete iter;
+    // Wait a little bit to allow any triggered compactions to complete.
+    Env::Default()->SleepForMicroseconds(1000000);
+    uint64_t size = Size(Key(0), Key(n));
+    fprintf(stderr, 'iter %3d => %7.3f MB [other %7.3f MB]\n',
+            read+1, size/1048576.0, Size(Key(n), Key(kCount))/1048576.0);
+    if (size <= initial_size/10) {
+      break;
+    }
   }
+    
+    Iterator* NewDBIterator(
+    DBImpl* db,
+    const Comparator* user_key_comparator,
+    Iterator* internal_iter,
+    SequenceNumber sequence,
+    uint32_t seed) {
+  return new DBIter(db, user_key_comparator, internal_iter, sequence, seed);
 }
     
-    #include 'test/cpp/qps/usage_timer.h'
+    inline ValueType ExtractValueType(const Slice& internal_key) {
+  assert(internal_key.size() >= 8);
+  const size_t n = internal_key.size();
+  uint64_t num = DecodeFixed64(internal_key.data() + n - 8);
+  unsigned char c = num & 0xff;
+  return static_cast<ValueType>(c);
+}
     
-    // TODO: make this independent of ElemType. Then these repeated dynamic_pointer_casts will go away
-// TODO: why is this a class, and not just a procedure? Then we wouldn't have to include the massive header
-template <class ElemType>
-class SGD : public SGDParams
+    // Print contents of a log file. (*func)() is called on every record.
+Status PrintLogContents(Env* env, const std::string& fname,
+                        void (*func)(uint64_t, Slice, WritableFile*),
+                        WritableFile* dst) {
+  SequentialFile* file;
+  Status s = env->NewSequentialFile(fname, &file);
+  if (!s.ok()) {
+    return s;
+  }
+  CorruptionReporter reporter;
+  reporter.dst_ = dst;
+  log::Reader reader(file, &reporter, true, 0);
+  Slice record;
+  std::string scratch;
+  while (reader.ReadRecord(&record, &scratch)) {
+    (*func)(reader.LastRecordOffset(), record, dst);
+  }
+  delete file;
+  return Status::OK();
+}
+    
+      fname = TableFileName('bar', 200);
+  ASSERT_EQ('bar/', std::string(fname.data(), 4));
+  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
+  ASSERT_EQ(200, number);
+  ASSERT_EQ(kTableFile, type);
+    
+    
+    {  edit.SetComparatorName('foo');
+  edit.SetLogNumber(kBig + 100);
+  edit.SetNextFile(kBig + 200);
+  edit.SetLastSequence(kBig + 1000);
+  TestEncodeDecode(edit);
+}
+    
+    
+    {      // End read transaction
+      if (FLAGS_transaction && transaction) {
+        status = sqlite3_step(end_trans_stmt);
+        StepErrorCheck(status);
+        status = sqlite3_reset(end_trans_stmt);
+        ErrorCheck(status);
+      }
+    }
+    
+    
+    {    done_++;
+    if (done_ >= next_report_) {
+      if      (next_report_ < 1000)   next_report_ += 100;
+      else if (next_report_ < 5000)   next_report_ += 500;
+      else if (next_report_ < 10000)  next_report_ += 1000;
+      else if (next_report_ < 50000)  next_report_ += 5000;
+      else if (next_report_ < 100000) next_report_ += 10000;
+      else if (next_report_ < 500000) next_report_ += 50000;
+      else                            next_report_ += 100000;
+      fprintf(stderr, '... finished %d ops%30s\r', done_, '');
+      fflush(stderr);
+    }
+  }
+    
+    namespace leveldb {
+    }
+    
+    // Dump the contents of the file named by fname in text format to
+// *dst.  Makes a sequence of dst->Append() calls; each call is passed
+// the newline-terminated text corresponding to a single item found
+// in the file.
+//
+// Returns a non-OK result if fname does not name a leveldb storage
+// file, or if the file cannot be read.
+Status DumpFile(Env* env, const std::string& fname, WritableFile* dst);
+    
+    // Streams a Message to an ostream.
+inline std::ostream& operator <<(std::ostream& os, const Message& sb) {
+  return os << sb.GetString();
+}
+    
+    # define TEST_P(test_case_name, test_name) \
+  class GTEST_TEST_CLASS_NAME_(test_case_name, test_name) \
+      : public test_case_name { \
+   public: \
+    GTEST_TEST_CLASS_NAME_(test_case_name, test_name)() {} \
+    virtual void TestBody(); \
+   private: \
+    static int AddToRegistry() { \
+      ::testing::UnitTest::GetInstance()->parameterized_test_registry(). \
+          GetTestCasePatternHolder<test_case_name>(\
+              #test_case_name, __FILE__, __LINE__)->AddTestPattern(\
+                  #test_case_name, \
+                  #test_name, \
+                  new ::testing::internal::TestMetaFactory< \
+                      GTEST_TEST_CLASS_NAME_(test_case_name, test_name)>()); \
+      return 0; \
+    } \
+    static int gtest_registering_dummy_; \
+    GTEST_DISALLOW_COPY_AND_ASSIGN_(\
+        GTEST_TEST_CLASS_NAME_(test_case_name, test_name)); \
+  }; \
+  int GTEST_TEST_CLASS_NAME_(test_case_name, \
+                             test_name)::gtest_registering_dummy_ = \
+      GTEST_TEST_CLASS_NAME_(test_case_name, test_name)::AddToRegistry(); \
+  void GTEST_TEST_CLASS_NAME_(test_case_name, test_name)::TestBody()
+    
+    
+    {  template <typename Tuple>
+  static void TersePrintPrefixToStrings(const Tuple& t, Strings* strings) {
+    ::std::stringstream ss;
+    UniversalTersePrint(::std::tr1::get<0>(t), &ss);
+    strings->push_back(ss.str());
+  }
+};
+    
+    
+    {  return AssertionFailure() << pred_text << '('
+                            << e1 << ', '
+                            << e2 << ', '
+                            << e3 << ', '
+                            << e4 << ', '
+                            << e5 << ') evaluates to false, where'
+                            << '\n' << e1 << ' evaluates to ' << v1
+                            << '\n' << e2 << ' evaluates to ' << v2
+                            << '\n' << e3 << ' evaluates to ' << v3
+                            << '\n' << e4 << ' evaluates to ' << v4
+                            << '\n' << e5 << ' evaluates to ' << v5;
+}
+    
+    // We cannot use std::numeric_limits<T>::max() as it clashes with the max()
+// macro defined by <windows.h>.
+template <>
+inline float FloatingPoint<float>::Max() { return FLT_MAX; }
+template <>
+inline double FloatingPoint<double>::Max() { return DBL_MAX; }
+    
+    
+    { private:
+  static int allocated_;
+};
+    
+    // Tests factorial of negative numbers.
+TEST(FactorialTest, Negative) {
+  // This test is named 'Negative', and belongs to the 'FactorialTest'
+  // test case.
+  EXPECT_EQ(1, Factorial(-5));
+  EXPECT_EQ(1, Factorial(-1));
+  EXPECT_GT(Factorial(-10), 0);
+    }
+    
+    namespace grpc {
+namespace testing {
+    }
+    }
+    
+    #ifndef GRPC_COMMON_CPP_ROUTE_GUIDE_HELPER_H_
+#define GRPC_COMMON_CPP_ROUTE_GUIDE_HELPER_H_
+    
+    bool SecureAuthContext::IsPeerAuthenticated() const {
+  if (!ctx_) return false;
+  return grpc_auth_context_peer_is_authenticated(ctx_) != 0;
+}
+    
+    
+    {
+    {}  // namespace
+}  // namespace grpc
+    
+    #ifndef TEST_QPS_STATS_UTILS_H
+#define TEST_QPS_STATS_UTILS_H
+    
+      // gRPC Python
+  grpc_python_generator::GeneratorConfiguration grpc_py_config;
+  grpc_python_generator::PythonGrpcGenerator grpc_py_generator(grpc_py_config);
+  cli.RegisterGenerator('--grpc_python_out', &grpc_py_generator,
+                        'Generate Python source file.');
+    
+        int unit;
+    double value;
+    
+      if (!elm.functionName().empty()) {
+    out << ' func=' << elm.functionName() << '()+' << elm.functionOffset();
+  }
+    
+      YGNodeStyleSetFlexShrink(child1, 1);
+    
+    #include <nbind/api.h>
+#include <nbind/BindDefiner.h>
+    
+     public: // Layout mutators
+    
+    struct Size
 {
-protected:
-    typedef shared_ptr<ComputationNode<ElemType>> ComputationNodePtr;
-    typedef ClassBasedCrossEntropyWithSoftmaxNode<ElemType>* ClassBasedCrossEntropyWithSoftmaxNodePtr;
+    double width;
+    double height;
     }
     
-    
-    {        (*p_model).reset(new Model(std::move(modelProto)));
-        if ((*p_model)->MainGraph() != nullptr)
-        {
-            RETURN_IF_ERROR((*p_model)->MainGraph()->Resolve());
-        }
-        return Status::OK();
-    }
-    
-    #include <vector>
-    
-        // Taken from ONNX
-    REGISTER_OPERATOR_SCHEMA(Gather)
-        .Description('Given data tensor of rank r >= 1, and indices tensor of rank q, gather '
-            'entries of the outer-most dimension of data indexed by indices, and concatenate '
-            'them in an output tensor of rank q + (r - 1). '
-            'Example 1: data = [ [1.0, 1.2], [2.3, 3.4], [4.5, 5.7], ] '
-            '           indices = [ [0, 1], [1, 2], ] '
-            '           output = [ [ [1.0, 1.2], [2.3, 3.4], ], [ [2.3, 3.4], [4.5, 5.7], ], ]'
-            'Example 2: data = [ [1.0, 1.2, 1.9], [2.3, 3.4, 3.9], [4.5, 5.7, 5.9], ] '
-            '           indices = [0, 2], ] axis = 1, '
-            '           output = [ [ [1.0, 1.9], [2.3, 3.9], [4.5, 5.9], ], ]')
-        .Input('data', 'Tensor of rank r >= 1.', 'T')
-        .Input('indices', 'Tensor of int32/int64 indices, of any rank q.', 'Tind')
-        .Output('output', 'Tensor of rank q + (r - 1).', 'T')
-        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
-            'Constrain input types to float tensors.')
-        .TypeConstraint('Tind', { 'tensor(int32)', 'tensor(int64)' },
-            'Constrain indices types to float tensors.')
-        .Attr('axis',
-            'Which axis to gather on, defaults to 0. Negative value means counting dimensions '
-            'from the back. Accepted range in [-r, r-1]',
-            AttrType::AttributeProto_AttributeType_INT, int64_t(0));
-    
-        void SetGammarCalculationParam(const double& amf, const double& lmf, const double& wp, const double& bMMIfactor, const bool& sMBR)
-    {
-        msra::lattices::SeqGammarCalParam param;
-        param.amf = amf;
-        param.lmf = lmf;
-        param.wp = wp;
-        param.bMMIfactor = bMMIfactor;
-        param.sMBRmode = sMBR;
-        m_gammaCalculator.SetGammarCalculationParams(param);
-    }
-    
-            if (!IsInRam())
-        {
-            LogicError('Cannot page-out data that is not memory.');
-        }
-    
-        // Let's check that there is no outstanding copies.
-    // Wait on all events if there are any pending copy operations in flight.
-    if (m_dataTransferers[m_currentDataTransferIndex])
-        m_dataTransferers[m_currentDataTransferIndex]->WaitForCopyCPUToGPU();
-    
-        FunctionPtr CosineDistance(const Variable& leftOperand, const Variable& rightOperand, const std::wstring& name)
-    {
-        return BinaryOp(PrimitiveOpType::CosDistance, leftOperand, rightOperand, Dictionary(), name);
-    }
-    
-        // Note: the necessary update of m_allSEQNodes is hanlded by the InvalidateCompiledNetwork() call above
-    
-      /*
-   * insert --
-   *
-   *   Returns a pair with iterator to the element at r.first and bool success.
-   *   Retrieve the index with ret.first.getIndex().
-   *
-   *   Fails on key collision (does not overwrite) or if map becomes
-   *   full, at which point no element is inserted, iterator is set to end(),
-   *   and success is set false.  On collisions, success is set false, but the
-   *   iterator is set to the existing entry.
-   */
-  std::pair<iterator,bool> insert(const value_type& r) {
-    return emplace(r.first, r.second);
-  }
-  std::pair<iterator,bool> insert(value_type&& r) {
-    return emplace(r.first, std::move(r.second));
-  }
-    
-    void benchmarkResultsFromDynamic(
-    const dynamic& d,
-    vector<detail::BenchmarkResult>& results) {
-  for (auto& datum : d) {
-    results.push_back(
-        {datum[0].asString(), datum[1].asString(), datum[2].asDouble()});
-  }
-}
-    
-      T* get() {
-    return &inner_;
-  }
-    
-        // Append, Flush, Append, Get
-    slists.Append('a', 'x');
-    slists.Append('b', 'y');
-    db->Flush(rocksdb::FlushOptions());
-    slists.Append('a', 't');
-    slists.Append('a', 'r');
-    slists.Append('b', '2');
-    
-     public: // Insert/Update
-  /// Insert value before/after pivot in (list: key). Return the length.
-  /// May throw RedisListException
-  int InsertBefore(const std::string& key, const std::string& pivot,
-                   const std::string& value);
-  int InsertAfter(const std::string& key, const std::string& pivot,
-                  const std::string& value);
-    
-      StatisticsJni::StatisticsJni(std::shared_ptr<Statistics> stats,
-      const std::set<uint32_t> ignore_histograms) : StatisticsImpl(stats, false),
-      m_ignore_histograms(ignore_histograms) {
-  }
-    
-      virtual size_t GetSimCapacity() const override {
-    return key_only_cache_->GetCapacity();
-  }
-  virtual size_t GetSimUsage() const override {
-    return key_only_cache_->GetUsage();
-  }
-  virtual void SetSimCapacity(size_t capacity) override {
-    key_only_cache_->SetCapacity(capacity);
-  }
-    
-    ::grpc::ClientReader< flatbuffers::grpc::Message<Monster>>* MonsterStorage::Stub::RetrieveRaw(::grpc::ClientContext* context, const flatbuffers::grpc::Message<Stat>& request) {
-  return ::grpc::internal::ClientReaderFactory< flatbuffers::grpc::Message<Monster>>::Create(channel_.get(), rpcmethod_Retrieve_, context, request);
-}
+    #define ASSERT_BUF_SIZE 4096
+static char sAssertBuf[ASSERT_BUF_SIZE];
+static AssertHandler gAssertHandler;
     
     
-    {    flatbuffers::grpc::Message<Monster> response;
-    auto stream = stub->Retrieve(&context, request);
-    while (stream->Read(&response)) {
-      auto resp = response.GetRoot()->name();
-      std::cout << 'RPC Streaming response: ' << resp->str() << std::endl;
-    }
-  }
+#ifdef LOG_TAG
+# define ALOGV(...) ::facebook::alog::logv(LOG_TAG, __VA_ARGS__)
+# define ALOGD(...) ::facebook::alog::logd(LOG_TAG, __VA_ARGS__)
+# define ALOGI(...) ::facebook::alog::logi(LOG_TAG, __VA_ARGS__)
+# define ALOGW(...) ::facebook::alog::logw(LOG_TAG, __VA_ARGS__)
+# define ALOGE(...) ::facebook::alog::loge(LOG_TAG, __VA_ARGS__)
+# define ALOGF(...) ::facebook::alog::logf(LOG_TAG, __VA_ARGS__)
+#endif
     
-      auto name = builder.CreateString('MyMonster');
+      const char* functionName() const { return m_functionName; }
+  const char* fileName() const { return m_fileName; }
+  int lineNumber() const { return m_lineNumber; }
     
-    namespace jsons {
-    }
     
-    FileExistsFunction SetFileExistsFunction(
-    FileExistsFunction file_exists_function) {
-  FileExistsFunction previous_function = g_file_exists_function;
-  g_file_exists_function =
-      file_exists_function ? file_exists_function : FileExistsRaw;
-  return previous_function;
-}
+    {  uint8_t* getDirectBytes() const;
+  size_t getDirectSize() const;
+};
