0e308836bfd4a781266857fb8c32d909b3f815dc
SUNsung <weatherius@gmail.com>
2018-04-25 05:40:20 +0300

    Rewrite [28ai8SC50Sy8Q28VrVmKTnhL9s28AXG7TGkxooY28cxu.rb] File in: 2018-04-25 05:40:19

--- ruby/28ai8SC50Sy8Q28VrVmKTnhL9s28AXG7TGkxooY28cxu.rb
+++ ruby/28ai8SC50Sy8Q28VrVmKTnhL9s28AXG7TGkxooY28cxu.rb
@@ -1,17 +1,47 @@
 
         
-          def test_url_redirect_with_status_hash
-    get :url_redirect_with_status_hash
-    assert_response 301
-    assert_equal 'http://www.example.com', redirect_to_url
+              # Set the correct feed URL.
+      self.data['feed_url'] = '#{category_dir}/#{name}'
+    end
+    
+    class ConfigTag < Liquid::Tag
+  def initialize(tag_name, options, tokens)
+    super
+    options = options.split(' ').map {|i| i.strip }
+    @key = options.slice!(0)
+    @tag = nil
+    @classname = nil
+    options.each do |option|
+      @tag = $1 if option =~ /tag:(\S+)/ 
+      @classname = $1 if option =~ /classname:(\S+)/
+    end
   end
     
-        # Set default test helper.
-    self.test_helper = LocalAbcHelper
+          rtn = ''
+      (context.environments.first['site'][@array_name] || []).each do |file|
+        if file !~ /^[a-zA-Z0-9_\/\.-]+$/ || file =~ /\.\// || file =~ /\/\./
+          rtn = rtn + 'Include file '#{file}' contains invalid characters or sequences'
+        end
+    
+          unless file.file?
+        return 'File #{file} could not be found'
+      end
+    
+        def poster
+      'poster='#{@poster}'' if @poster
+    end
+    
+            def matches? subject
+          @subject = subject
+          @subject = @subject.new if @subject.class == Class
+          @allowed_types && @rejected_types &&
+          allowed_types_allowed? && rejected_types_rejected?
+        end
+    
+      # Get list of styles saved on previous deploy (running rake paperclip:refresh:missing_styles)
+  def self.get_registered_attachments_styles
+    YAML.load_file(Paperclip.registered_attachments_styles_path)
+  rescue Errno::ENOENT
+    nil
   end
-    
-    class MyCustomDelivery
-end
-    
-      included do
-    include Oauthable
+  private_class_method :get_registered_attachments_styles
