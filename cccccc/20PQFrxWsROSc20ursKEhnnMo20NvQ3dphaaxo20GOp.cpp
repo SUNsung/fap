f1a183220b73f6aa4767a3943ce86d2f1cd6207b
SUNsung <weatherius@gmail.com>
2018-04-26 07:40:25 +0300

    Rewrite [20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp] File in: 2018-04-26 07:40:25

--- cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
+++ cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
@@ -1,181 +1,146 @@
 
         
-        
-    {  // We declare (but don't implement) this to prevent the compiler
-  // from implementing the assignment operator.
-  void operator=(const Message&);
-};
+        // Use if you want to reset your rendering device without losing ImGui state.
+IMGUI_API void        ImGui_ImplDX10_InvalidateDeviceObjects();
+IMGUI_API bool        ImGui_ImplDX10_CreateDeviceObjects();
     
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12, typename T13, typename T14, typename T15,
-    typename T16, typename T17, typename T18, typename T19, typename T20,
-    typename T21, typename T22, typename T23, typename T24, typename T25,
-    typename T26, typename T27, typename T28>
-internal::ValueArray28<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
-    T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27,
-    T28> Values(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9,
-    T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15, T16 v16, T17 v17,
-    T18 v18, T19 v19, T20 v20, T21 v21, T22 v22, T23 v23, T24 v24, T25 v25,
-    T26 v26, T27 v27, T28 v28) {
-  return internal::ValueArray28<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
-      T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25,
-      T26, T27, T28>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13,
-      v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27,
-      v28);
+    // Implemented features:
+//  [X] User texture binding. Cast 'GLuint' OpenGL texture identifier as void*/ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
+// Missing features:
+//  [ ] SDL2 handling of IME under Windows appears to be broken and it explicitly disable the regular Windows IME. You can restore Windows IME by compiling SDL with SDL_DISABLE_WINDOWS_IME.
+    
+    
+    {    // Restore modified GL state
+    glUseProgram(last_program);
+    glBindTexture(GL_TEXTURE_2D, last_texture);
+    glBindSampler(0, last_sampler);
+    glActiveTexture(last_active_texture);
+    glBindVertexArray(last_vertex_array);
+    glBindBuffer(GL_ARRAY_BUFFER, last_array_buffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, last_element_array_buffer);
+    glBlendEquationSeparate(last_blend_equation_rgb, last_blend_equation_alpha);
+    glBlendFuncSeparate(last_blend_src_rgb, last_blend_dst_rgb, last_blend_src_alpha, last_blend_dst_alpha);
+    if (last_enable_blend) glEnable(GL_BLEND); else glDisable(GL_BLEND);
+    if (last_enable_cull_face) glEnable(GL_CULL_FACE); else glDisable(GL_CULL_FACE);
+    if (last_enable_depth_test) glEnable(GL_DEPTH_TEST); else glDisable(GL_DEPTH_TEST);
+    if (last_enable_scissor_test) glEnable(GL_SCISSOR_TEST); else glDisable(GL_SCISSOR_TEST);
+    glPolygonMode(GL_FRONT_AND_BACK, (GLenum)last_polygon_mode[0]);
+    glViewport(last_viewport[0], last_viewport[1], (GLsizei)last_viewport[2], (GLsizei)last_viewport[3]);
+    glScissor(last_scissor_box[0], last_scissor_box[1], (GLsizei)last_scissor_box[2], (GLsizei)last_scissor_box[3]);
 }
     
-    // We print a protobuf using its ShortDebugString() when the string
-// doesn't exceed this many characters; otherwise we print it using
-// DebugString() for better readability.
-const size_t kProtobufOneLinerMaxLength = 50;
-    
-    #endif  // GTEST_HAS_TYPED_TEST
-    
-    // A function to convert T* into linked_ptr<T>
-// Doing e.g. make_linked_ptr(new FooBarBaz<type>(arg)) is a shorter notation
-// for linked_ptr<FooBarBaz<type> >(new FooBarBaz<type>(arg))
-template <typename T>
-linked_ptr<T> make_linked_ptr(T* ptr) {
-  return linked_ptr<T>(ptr);
-}
-    
-    // INTERNAL IMPLEMENTATION - DO NOT USE IN USER CODE.
-//
-// Outputs a message explaining invalid registration of different
-// fixture class for the same test case. This may happen when
-// TEST_P macro is used to define two tests with the same name
-// but in different namespaces.
-GTEST_API_ void ReportInvalidTestCaseType(const char* test_case_name,
-                                          const char* file, int line);
-    
-    #endif  // GTEST_USE_OWN_TR1_TUPLE
-    
-      // Trivial case 2: even numbers
-  if (n % 2 == 0) return n == 2;
-    
-    void Vunit::freeScratchBlock(Vlabel l) {
-  // This will leak blocks if anything's been added since the corresponding
-  // call to makeScratchBlock(), but it's harmless.
-  if (l == blocks.size() - 1) blocks.pop_back();
-}
-    
-    #ifndef incl_HPHP_JIT_TC_PROLOGUE_H_
-#define incl_HPHP_JIT_TC_PROLOGUE_H_
-    
-      /*
-   * @brief Print help-style output to stdout for a given flag set.
-   *
-   * @param shell Only print shell flags.
-   * @param external Only print external flags (from extensions).
-   */
-  static void printFlags(bool shell = false,
-                         bool external = false,
-                         bool cli = false);
-    
-    /// Helper replacement for REGISTER, used within extension modules.
-#define REGISTER_MODULE(t, r, n)                                               \
-  auto t##Module = Registry::get().registry(r)->add(n, std::make_shared<t>());
-    
-    #ifdef WIN32
-#include <osquery/windows/system.h>
-#else
-#include <osquery/posix/system.h>
-#endif
-    
-    namespace fs = boost::filesystem;
-namespace pt = boost::property_tree;
-    
-    
-    {  // Check the default value accessor.
-  std::string default_value;
-  auto status = Flag::getDefaultValue('test_mistake', default_value);
-  EXPECT_FALSE(status.ok());
-  status = Flag::getDefaultValue('test_string_flag', default_value);
-  EXPECT_TRUE(status.ok());
-  EXPECT_EQ(default_value, 'TEST STRING');
-}
-    
-    // Licensed under the MIT License (the 'License'); you may not use this file except in 
-// compliance with the License. You may obtain a copy of the License at
-// http://opensource.org/licenses/MIT
-    
-    #define DEFINE_HAS_MEMBER(member_name) \
-    template <typename T>\
-    class has_##member_name {\
-      private:\
-        struct yes_type { char x[1]; };\
-        struct no_type { char x[2]; };\
-        template <int> struct tester;\
-        template <typename U> static yes_type test(tester<sizeof(&U::member_name)>*);\
-        template <typename U> static no_type test(...);\
-      public:\
-        static const bool value = (sizeof(test<T>(0)) == sizeof(yes_type));\
-    };
-    
-    class FBEXPORT StackTraceElement {
- public:
-  StackTraceElement(InstructionPointer absoluteProgramCounter,
-                    InstructionPointer libraryBase,
-                    InstructionPointer functionAddress, std::string libraryName,
-                    std::string functionName)
-      : absoluteProgramCounter_{absoluteProgramCounter},
-        libraryBase_{libraryBase},
-        functionAddress_{functionAddress},
-        libraryName_{std::move(libraryName)},
-        functionName_{std::move(functionName)} {}
+        {
+        D3D12_COMMAND_QUEUE_DESC desc = {};
+        desc.Type     = D3D12_COMMAND_LIST_TYPE_DIRECT;
+        desc.Flags    = D3D12_COMMAND_QUEUE_FLAG_NONE;
+        desc.NodeMask = 1;
+        if (g_pd3dDevice->CreateCommandQueue(&desc, IID_PPV_ARGS(&g_pd3dCommandQueue)) != S_OK)
+            return E_FAIL;
     }
     
-        void reset(void);
+    #include <s3eKeyboard.h>
+#include <s3ePointer.h>
+#include <IwGx.h>
     
-    #include <cstdarg>
-#include <stdio.h>
     
-      void reset(T* other = NULL) {
-    T* old = (T*)pthread_getspecific(m_key);
-    if (old != other) {
-      FBASSERT(m_cleanup);
-      m_cleanup(old);
-      pthread_setspecific(m_key, other);
+    {  auto iov = buf->getIov();
+  transport.writev(nullptr, iov.data(), iov.size());
+}
+    
+      // create a read buffer for the buffer chain
+  Cursor rcursor(iobuf1.get());
+  EXPECT_EQ(1, rcursor.readBE<uint64_t>());
+  EXPECT_EQ(10, rcursor.readBE<uint64_t>());
+  EXPECT_EQ(20, rcursor.readBE<uint32_t>());
+  EXPECT_EQ(0, rcursor.totalLength());
+  rcursor.reset(iobuf1.get());
+  EXPECT_EQ(20, rcursor.totalLength());
+    
+    
+    {   private:
+    Deleter deleter_;
+  };
+    
+      uint32_t size3 = 4321;
+  unique_ptr<uint8_t[]> buf3(new uint8_t[size3]);
+  IOBuf iobuf3(IOBuf::WRAP_BUFFER, buf3.get(), size3);
+  EXPECT_EQ(buf3.get(), iobuf3.data());
+  EXPECT_EQ(size3, iobuf3.length());
+  EXPECT_EQ(buf3.get(), iobuf3.buffer());
+  EXPECT_EQ(size3, iobuf3.capacity());
+    
+      template <typename T>
+  static std::shared_ptr<T> get_shared_ptr_from_counted_base(
+      counted_base* base,
+      bool inc = true) {
+    if (!base) {
+      return nullptr;
     }
+    std::shared_ptr<const void> newp;
+    if (inc) {
+      inc_shared_count(base, 1);
+    }
+    newp.*fieldPtr(access_shared_ptr_ptr{}) =
+        get_shared_ptr<const void>(base); // _M_ptr
+    (newp.*fieldPtr(access_refcount{})).*fieldPtr(access_base{}) = base;
+    // reinterpret_pointer_cast<T>
+    auto res = reinterpret_cast<std::shared_ptr<T>*>(&newp);
+    return std::move(*res);
   }
     
-    struct InputInfo {
-  Unit U;  // The actual input data.
-  uint8_t Sha1[kSHA1NumBytes];  // Checksum.
-  // Number of features that this input has and no smaller input has.
-  size_t NumFeatures = 0;
-  size_t Tmp = 0; // Used by ValidateFeatureSet.
-  // Stats.
-  size_t NumExecutedMutations = 0;
-  size_t NumSuccessfullMutations = 0;
-  bool MayDeleteFile = false;
-};
+    #pragma once
     
-    #ifdef __x86_64
-#define ATTRIBUTE_TARGET_POPCNT __attribute__((target('popcnt')))
-#else
-#define ATTRIBUTE_TARGET_POPCNT
-#endif
     
-    ExternalFunctions::ExternalFunctions() {
-#define EXT_FUNC(NAME, RETURN_TYPE, FUNC_SIG, WARN)                            \
-  this->NAME = GetFnPtr<decltype(::NAME)>(::NAME, ::NAME##Def, #NAME, WARN);
+    {
+    {    idx = ProbeFcn()(idx, numProbes, capacity_);
+  }
+}
+    
+    
+    {
+}; // AtomicHashArray
+    
+    void DupAndCloseStderr();
+    
+      // Aggregates all available coverage measurements.
+  struct Coverage {
+    Coverage() { Reset(); }
     }
     
-    void CopyFileToErr(const std::string &Path);
-    
-      // Remove all features that we already know from all other inputs.
-  for (size_t i = NumFilesInFirstCorpus; i < Files.size(); i++) {
-    auto &Cur = Files[i].Features;
-    std::vector<uint32_t> Tmp;
-    std::set_difference(Cur.begin(), Cur.end(), AllFeatures.begin(),
-                        AllFeatures.end(), std::inserter(Tmp, Tmp.begin()));
-    Cur.swap(Tmp);
+    template<class T>
+size_t ChangeBinaryInteger(uint8_t *Data, size_t Size, Random &Rand) {
+  if (Size < sizeof(T)) return 0;
+  size_t Off = Rand(Size - sizeof(T) + 1);
+  assert(Off + sizeof(T) <= Size);
+  T Val;
+  if (Off < 64 && !Rand(4)) {
+    Val = Size;
+    if (Rand.RandBool())
+      Val = Bswap(Val);
+  } else {
+    memcpy(&Val, Data + Off, sizeof(Val));
+    T Add = Rand(21);
+    Add -= 10;
+    if (Rand.RandBool())
+      Val = Bswap(T(Bswap(Val) + Add)); // Add assuming different endiannes.
+    else
+      Val = Val + Add;               // Add assuming current endiannes.
+    if (Add == 0 || Rand.RandBool()) // Maybe negate.
+      Val = -Val;
   }
+  memcpy(Data + Off, &Val, sizeof(Val));
+  return Size;
+}
     
-    // Private copy of SHA1 implementation.
-static const int kSHA1NumBytes = 20;
+    void TracePC::HandleInit(uint32_t *Start, uint32_t *Stop) {
+  if (Start == Stop || *Start) return;
+  assert(NumModules < sizeof(Modules) / sizeof(Modules[0]));
+  for (uint32_t *P = Start; P < Stop; P++)
+    *P = ++NumGuards;
+  Modules[NumModules].Start = Start;
+  Modules[NumModules].Stop = Stop;
+  NumModules++;
+}
     
       void HandleTrace(uint32_t *guard, uintptr_t PC);
   void HandleInit(uint32_t *start, uint32_t *stop);
@@ -190,14 +155,3 @@ static const int kSHA1NumBytes = 20;
   bool UpdateValueProfileMap(ValueBitMap *MaxValueProfileMap) {
     return UseValueProfile && MaxValueProfileMap->MergeFrom(ValueProfileMap);
   }
-    
-    #if LLVM_FUZZER_DEFINES_SANITIZER_WEAK_HOOOKS
-void __sanitizer_weak_hook_memcmp(void *caller_pc, const void *s1,
-                                  const void *s2, size_t n, int result) {
-  fuzzer::TPC.AddValueForMemcmp(caller_pc, s1, s2, n);
-  if (!RecordingMemcmp) return;
-  if (result == 0) return;  // No reason to mutate.
-  if (n <= 1) return;  // Not interesting.
-  TS->TraceMemcmpCallback(n, reinterpret_cast<const uint8_t *>(s1),
-                          reinterpret_cast<const uint8_t *>(s2));
-}
