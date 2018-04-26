4952203817cc48e5f531849cd9c4b04f62454066
SUNsung <weatherius@gmail.com>
2018-04-26 17:51:59 +0300

    Rewrite [58yyJlKlvbREg58UmTVuSHhf.582FWBMyACIM58mQF.rb] File in: 2018-04-26 17:51:58

--- ruby/58yyJlKlvbREg58UmTVuSHhf.582FWBMyACIM58mQF.rb
+++ ruby/58yyJlKlvbREg58UmTVuSHhf.582FWBMyACIM58mQF.rb
@@ -1,136 +1,115 @@
 
         
-            if ARGV.named.empty?
-      slow_checks = %w[
-        check_for_broken_symlinks
-        check_missing_deps
-        check_for_outdated_homebrew
-        check_for_linked_keg_only_brews
-      ]
-      methods = (checks.all.sort - slow_checks) + slow_checks
-    else
-      methods = ARGV.named
-    end
+              class_attribute :delivery_methods, default: {}.freeze
+      class_attribute :delivery_method, default: :smtp
     
-        if $stdout.tty?
-      metacharacters = %w[\\ | ( ) [ ] { } ^ $ * + ? .]
-      bad_regex = metacharacters.any? do |char|
-        ARGV.any? do |arg|
-          arg.include?(char) && !arg.start_with?('/')
-        end
-      end
-      if ARGV.any? && bad_regex
-        ohai 'Did you mean to perform a regular expression search?'
-        ohai 'Surround your query with /slashes/ to search by regex.'
-      end
-    end
+      # Skips the current run on Rubinius using Minitest::Assertions#skip
+  private def rubinius_skip(message = '')
+    skip message if RUBY_ENGINE == 'rbx'
+  end
+  # Skips the current run on JRuby using Minitest::Assertions#skip
+  private def jruby_skip(message = '')
+    skip message if defined?(JRUBY_VERSION)
+  end
+end
+
     
-      def self.all
-    opoo 'Formula.all is deprecated, use Formula.map instead'
-    map
+      def test_deliver_is_notified
+    BaseMailer.welcome.deliver_now
+    wait
+    
+      before do
+    Spaceship::Portal::Key.client = mock_client
   end
     
-      describe '.all' do
-    it 'uses the client to fetch all keys' do
-      mock_client_response(:list_keys, with: no_args) do
-        [
-          {
-            canDownload: false,
-            canRevoke: true,
-            keyId: 'some-key-id',
-            keyName: 'Test Key via fastlane',
-            servicesCount: 2
-          },
-          {
-            canDownload: true,
-            canRevoke: true,
-            keyId: 'B92NE4F7RG',
-            keyName: 'Test Key via browser',
-            servicesCount: 2
-          }
-        ]
-      end
-    
-            command = 'security set-keychain-settings'
-        command << ' -t #{params[:timeout]}' if params[:timeout]
-        command << ' -l' if params[:lock_when_sleeps]
-        command << ' -u' if params[:lock_after_timeout]
-        command << ' #{keychain_path}'
-    
-    describe Deliver::HtmlGenerator do
-  let(:generator) { Deliver::HtmlGenerator.new }
-    
-            expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER]).to match(/cd .* && agvtool new-version -all 24/)
-      end
-    
-        valid_oauth_providers :evernote
-    
-      def destroy_all
-    Delayed::Job.where(locked_at: nil).delete_all
-    
-        def replace(index, name)
-      @filters[assert_index(index)] = filter_const(name)
-    end
-    
-          unless root?
-        raise Invalid, 'missing name' if !name || name.empty?
-        raise Invalid, 'missing path' if !path || path.empty?
-        raise Invalid, 'missing type' if !type || type.empty?
+            expect(result).to eq('/usr/local/bin/cloc  --by-file  --out=build/cloc.txt')
       end
     end
+  end
+end
+
     
-          if dest.end_with? '/'
-        dest_dir.relative_path_from(base_dir).to_s.tap do |result|
-          result << '/' if result != '.'
-        end
-      else
-        dest_dir.parent.relative_path_from(base_dir).join(dest.split('/').last).to_s
-      end
-    end
+    require 'open3'
     
-              # Underscore methods
-          if name.start_with?('Underscore')
-            node.at_css('~ ul').css('li').each do |li|
-              name = [type.downcase, li.at_css('a').content.split.first].join('.')
-              id = name.parameterize
-              li['id'] = id
-              entries << [name, id, type]
+            def run
+          if @pod_name.nil?
+            # Note: at that point, @wipe_all is always true (thanks to `validate!`)
+            # Remove all
+            clear_cache
+          else
+            # Remove only cache for this pod
+            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
+            if cache_descriptors.nil?
+              UI.notice('No cache for pod named #{@pod_name} found')
+            elsif cache_descriptors.count > 1 && !@wipe_all
+              # Ask which to remove
+              choices = cache_descriptors.map { |c| '#{@pod_name} v#{c[:version]} (#{pod_type(c)})' }
+              index = UI.choose_from_array(choices, 'Which pod cache do you want to remove?')
+              remove_caches([cache_descriptors[index]])
+            else
+              # Remove all found cache of this pod
+              remove_caches(cache_descriptors)
             end
-            next
           end
-    
-          when :login_fail
-        if(s[:user] and s[:pass])
-          report_auth_info(s.merge({:active => false}))
-          print_status('Failed FTP Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]}')
-    
-      def parse(pkt)
-    
-                  s[:proto] = 'tcp'
-              s[:name]  = 'pop3'
-              s[:extra] = 'Successful Login. Banner: #{s[:banner]}'
-              report_auth_info(s)
-              print_status('Successful POP3 Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]} (#{s[:banner].strip})')
-    
-        self.sigs.each_key do |k|
-    
-    success = clsJavaCompile._invoke('CompileFromMemory','[Ljava.lang.String;[Ljava.lang.String;[Ljava.lang.String;', classNames, codez, compileOpts)
-    
-    
-signer._invoke('KeyToolMSF','[Ljava.lang.String;',keytoolOpts)
-    
-            protected
-    
-            def error_when_not_valid?
-          @subject.send(@attachment_name).assign(nil)
-          @subject.valid?
-          @subject.errors[:'#{@attachment_name}'].present?
         end
     
-            def greater_than size
-          @low = size
-          self
+            def find_order
+          @order = Spree::Order.find_by!(number: order_id)
         end
     
-        rake_tasks { load 'tasks/paperclip.rake' }
+    module RuboCop
+  module Cop
+    class VariableForce
+      # A Variable represents existence of a local variable.
+      # This holds a variable declaration node,
+      # and some states of the variable.
+      class Variable
+        VARIABLE_DECLARATION_TYPES =
+          (VARIABLE_ASSIGNMENT_TYPES + ARGUMENT_DECLARATION_TYPES).freeze
+    
+            def on_case(case_node)
+          case_node.when_branches.each_with_object([]) do |when_node, previous|
+            when_node.each_condition do |condition|
+              next unless repeated_condition?(previous, condition)
+    
+            def nested_comparison?(node)
+          if node.or_type?
+            node.node_parts.all? { |node_part| comparison? node_part }
+          else
+            false
+          end
+        end
+    
+                  include Something
+              def do_something; end
+    
+          def extract_metrics(path, *keys)
+        snapshot.metric_store.extract_metrics(path, *keys)
+      end
+    end
   end
+end
+
+    
+          def define_deprecated_writer_for(option, opts, &block)
+        define_method(option.write_method) do |value|
+          self.class.logger.warn 'DEPRECATION WARNING: The flag #{option.switches} has been deprecated, please use \'--#{opts[:new_flag]}=#{opts[:new_value]}\' instead.'
+          LogStash::SETTINGS.set(opts[:new_flag], opts[:new_value])
+        end
+      end
+    end
+    
+      context 'called with null values' do
+    it 'writes rules for other three' do
+      ruleset = 'margin-top: 11px; ' +
+                'margin-right: 12px; ' +
+                'margin-left: 13px;'
+      bad_rule = 'margin-bottom: null;'
+    
+      context 'called with three sizes' do
+    it 'applies second width to left and right' do
+      ruleset = 'position: relative; ' +
+                'top: 4px; ' +
+                'right: 5px; ' +
+                'bottom: 6px; ' +
+                'left: 5px;'
