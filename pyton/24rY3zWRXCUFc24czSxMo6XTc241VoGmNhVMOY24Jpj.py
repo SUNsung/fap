28f403b72bfe68c5a010b7d6493aa22fb1459647
SUNsung <weatherius@gmail.com>
2018-04-26 10:02:27 +0300

    Rewrite [24rY3zWRXCUFc24czSxMo6XTc241VoGmNhVMOY24Jpj.py] File in: 2018-04-26 10:02:27

--- pyton/24rY3zWRXCUFc24czSxMo6XTc241VoGmNhVMOY24Jpj.py
+++ pyton/24rY3zWRXCUFc24czSxMo6XTc241VoGmNhVMOY24Jpj.py
@@ -1,90 +1,211 @@
 
         
         
-def plot_batch_errors(all_errors, n_features, all_batch_sizes, data):
-    plt.figure()
-    plot_results(all_batch_sizes, all_errors['pca'], label='PCA')
-    plot_results(all_batch_sizes, all_errors['ipca'], label='IncrementalPCA')
-    plt.legend(loc='lower left')
-    plt.suptitle('Algorithm error vs. batch_size for n_components %i\n \
-                 LFW, size %i x %i' % (
-                 n_features, data.shape[0], data.shape[1]))
-    plt.xlabel('Batch size')
-    plt.ylabel('Mean absolute error')
+atom_template = textwrap.dedent('''\
+    <?xml version='1.0' encoding='utf-8'?>
+    <feed xmlns='http://www.w3.org/2005/Atom'>
+        <link rel='self' href='http://rg3.github.io/youtube-dl/update/releases.atom' />
+        <title>youtube-dl releases</title>
+        <id>https://yt-dl.org/feed/youtube-dl-updates-feed</id>
+        <updated>@TIMESTAMP@</updated>
+        @ENTRIES@
+    </feed>''')
     
-    pages = {
-    u'ar': u'http://ar.wikipedia.org/wiki/%D9%88%D9%8A%D9%83%D9%8A%D8%A8%D9%8A%D8%AF%D9%8A%D8%A7',
-    u'de': u'http://de.wikipedia.org/wiki/Wikipedia',
-    u'en': u'https://en.wikipedia.org/wiki/Wikipedia',
-    u'es': u'http://es.wikipedia.org/wiki/Wikipedia',
-    u'fr': u'http://fr.wikipedia.org/wiki/Wikip%C3%A9dia',
-    u'it': u'http://it.wikipedia.org/wiki/Wikipedia',
-    u'ja': u'http://ja.wikipedia.org/wiki/Wikipedia',
-    u'nl': u'http://nl.wikipedia.org/wiki/Wikipedia',
-    u'pl': u'http://pl.wikipedia.org/wiki/Wikipedia',
-    u'pt': u'http://pt.wikipedia.org/wiki/Wikip%C3%A9dia',
-    u'ru': u'http://ru.wikipedia.org/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D0%BF%D0%B5%D0%B4%D0%B8%D1%8F',
-#    u'zh': u'http://zh.wikipedia.org/wiki/Wikipedia',
-}
-    
-    skeleton_dir = os.path.abspath(os.path.join(exercise_dir, '..', 'skeletons'))
-if not os.path.exists(skeleton_dir):
-    os.makedirs(skeleton_dir)
-    
-    legend2_values_list = list(legend2.values())
-legend2_keys_list = list(legend2.keys())
-    
-    plt.matshow(np.outer(np.sort(model.row_labels_) + 1,
-                     np.sort(model.column_labels_) + 1),
-            cmap=plt.cm.Blues)
-plt.title('Checkerboard structure of rearranged data')
-    
-    plt.show()
+    with io.open(README_FILE, 'w', encoding='utf-8') as f:
+    f.write(header)
+    f.write(options)
+    f.write(footer)
 
     
-    # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+        def test_vimeo_matching(self):
+        self.assertMatch('https://vimeo.com/channels/tributes', ['vimeo:channel'])
+        self.assertMatch('https://vimeo.com/channels/31259', ['vimeo:channel'])
+        self.assertMatch('https://vimeo.com/channels/31259/53576664', ['vimeo'])
+        self.assertMatch('https://vimeo.com/user7108434', ['vimeo:user'])
+        self.assertMatch('https://vimeo.com/user7108434/videos', ['vimeo:user'])
+        self.assertMatch('https://vimeo.com/user21297594/review/75524534/3c257a1b5d', ['vimeo:review'])
     
-        if len(sys.argv) == 2:
-        main(sys.argv[1])
+                    if ''' not in code and ''' not in code:
+                    continue
+                assertRegexpMatches(
+                    self,
+                    code,
+                    r'(?:(?:#.*?|\s*)\n)*from __future__ import (?:[a-z_]+,\s*)*unicode_literals',
+                    'unicode_literals import  missing in %s' % fn)
+    
+    import os
+import subprocess
+    
+            title = self._html_search_regex(
+            r'<div[^>]+style='float:left'[^>]*>\s*<h2>(.+?)</h2>', webpage, 'title')
+        description = self._html_search_regex(
+            r'>Description:</span>(.+?)</div>', webpage, 'description', default=None)
+    
+            remaining_layers = hparams.num_local_layers - 1
+      else:
+        remaining_layers = hparams.num_local_layers
+    
+      Returns:
+    labels: An int64 tf.Placeholder with shape [batch_size].
+  '''
+  batch_size = None  # Batch size will be dynamically specified.
+  return tf.placeholder(dtype=tf.int64, shape=[batch_size], name='labels')
+
+    
+        # One aux feature.
+    config = configdict.ConfigDict({
+        'time_feature_1': {
+            'length': 14,
+            'is_time_series': True,
+        },
+        'aux_feature_1': {
+            'length': 1,
+            'is_time_series': False,
+        }
+    })
+    expected_shapes = {
+        'time_series_features': {
+            'time_feature_1': [None, 14],
+        },
+        'aux_features': {
+            'aux_feature_1': [None, 1]
+        }
+    }
+    features = input_ops.build_feature_placeholders(config)
+    self.assertFeatureShapesEqual(expected_shapes, features)
+    
+    
+def log_and_save_config(config, output_dir):
+  '''Logs and writes a JSON-serializable configuration object.
+    
+        Args:
+      initial_dictionary: Optional dictionary or ConfigDict containing initial
+        parameters.
+    '''
+    if initial_dictionary:
+      for field, value in initial_dictionary.items():
+        initial_dictionary[field] = _maybe_convert_dict(value)
+    super(ConfigDict, self).__init__(initial_dictionary)
+    
+      Returns:
+    3-D tensor with cropped image.
+  '''
+  shape = tf.shape(image)
+  height, width = shape[0], shape[1]
+    
+    FLAGS = tf.flags.FLAGS
+    
+        if batched:
+      return act
     else:
-        print('Choose one path as argument one')
+      return zip(*act)[0]
+    
+      def testConversion2dWithType(self):
+    self.Conversion2dTestWithType(np.int8)
+    self.Conversion2dTestWithType(np.int16)
+    self.Conversion2dTestWithType(np.int32)
+    self.Conversion2dTestWithType(np.int64)
+    self.Conversion2dTestWithType(np.float16)
+    self.Conversion2dTestWithType(np.float32)
+    self.Conversion2dTestWithType(np.float64)
+    
+        return inner
+    
+            with pytest.raises(socket.error):
+            new_sock = socket.socket()
+            new_sock.connect((host, port))
+    
+    
+class RequestsDependencyWarning(RequestsWarning):
+    '''An imported dependency doesn't match the expected version range.'''
+    pass
 
     
+    _codes = {
+    }
     
-def FormatDebugInfoResponse_ExtraConfFoundButNotLoaded_test():
-  response = deepcopy( GENERIC_RESPONSE )
-  response[ 'extra_conf' ].update( {
-    'is_loaded': False,
-    'path': '/path/to/extra/conf'
-  } )
-  assert_that(
-    FormatDebugInfoResponse( response ),
-    contains_string(
-      'Extra configuration file found but not loaded\n'
-      'Extra configuration path: /path/to/extra/conf\n'
-    )
-  )
+            # Send the request.
+        send_kwargs = {
+            'timeout': timeout,
+            'allow_redirects': allow_redirects,
+        }
+        send_kwargs.update(settings)
+        resp = self.send(prep, **send_kwargs)
     
     
-def RegexMultiList_test():
-  opts = _JavaFilter( { 'regex' : [ 'taco', 'burrito' ] } )
-  f = _CreateFilterForTypes( opts, [ 'java' ] )
+def get_args(args_list):
+    parser = argparse.ArgumentParser(
+        description='ansible inventory script reading from landscape cluster')
+    mutex_group = parser.add_mutually_exclusive_group(required=True)
+    help_list = 'list all hosts from landscape cluster'
+    mutex_group.add_argument('--list', action='store_true', help=help_list)
+    help_host = 'display variables for a host'
+    mutex_group.add_argument('--host', help=help_host)
+    return parser.parse_args(args_list)
     
-    plugins = PluginLoader()
-print 'PLUGINS := ' + ' '.join(plugin.name for plugin in plugins
-                               if plugin.needs_static_build)
+    response = get_json_from_api('%s/domains/%s/applications' %
+                             (broker_url, response[0]['id']), username, password)
     
-    class CaptchaController(RedditController):
-    @allow_oauth2_access
-    @api_doc(api_section.captcha, uri='/captcha/{iden}')
-    def GET_captchaimg(self, iden):
-        '''
-        Request a CAPTCHA image given an `iden`.
+        terminal_stderr_re = [
+        re.compile(r'% ?Error: '),
+        re.compile(r'^% \w+', re.M),
+        re.compile(r'% ?Bad secret'),
+        re.compile(r'invalid input', re.I),
+        re.compile(r'(?:incomplete|ambiguous) command', re.I),
+        re.compile(r'connection timed out', re.I),
+        re.compile(r'[^\r\n]+ not found', re.I),
+        re.compile(r''[^']' +returned error code: ?\d+'),
+        re.compile(r'syntax error'),
+        re.compile(r'unknown command'),
+        re.compile(r'Error\[\d+\]: ', re.I),
+        re.compile(r'Error:', re.I)
+    ]
     
-        @csrf_exempt
-    def renderurl(self, override=None):
-        if override:
-            path = override
-        else:
-            path = request.path
+        def setUp(self):
+        self.dog = Dog()
+        self.cat = Cat()
+        self.human = Human()
+        self.car = Car()
+    
+        def setUp(self):
+        self.sample_queue = queue.Queue()
+        self.sample_queue.put('first')
+        self.sample_queue.put('second')
+    
+        def test_bear_greek_localization(self):
+        self.assertEqual(self.g.get('bear'), 'bear')
+
+    
+        def setUp(self):
+        self.tc1 = TC1()
+        self.tc2 = TC2()
+        self.tc3 = TC3()
+        self.average_result_tc1 = '###### In Test 1 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc2 = '###### In Test 2 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc3 = '###### In Test 3 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.runner = TestRunner()
+        self.out = StringIO()
+        self.saved_stdout = sys.stdout
+        sys.stdout = self.out
+    
+    # Test the generator
+count_to_two = lambda: count_to(2)
+count_to_five = lambda: count_to(5)
+    
+        def setDB(self, db):
+        self._db = db
+    
+    
+class HexViewer:
