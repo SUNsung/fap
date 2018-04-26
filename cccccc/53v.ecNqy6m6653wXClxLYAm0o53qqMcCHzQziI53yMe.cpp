4c6802e8d3b1956b7c5914969cebaef86f0d9734
SUNsung <weatherius@gmail.com>
2018-04-26 14:40:31 +0300

    Rewrite [53v.ecNqy6m6653wXClxLYAm0o53qqMcCHzQziI53yMe.cpp] File in: 2018-04-26 14:40:30

--- cccccc/53v.ecNqy6m6653wXClxLYAm0o53qqMcCHzQziI53yMe.cpp
+++ cccccc/53v.ecNqy6m6653wXClxLYAm0o53qqMcCHzQziI53yMe.cpp
@@ -1,219 +1,226 @@
 
         
-        	FILE *g;
-	//printf('opening file %s\n', p_fname.c_str());
-	String filename = fix_path(p_name);
-	g = _wfopen(filename.c_str(), L'rb');
-	if (g == NULL) {
+              T* resource;
+      OP_REQUIRES_OK(
+          context,
+          mgr->LookupOrCreate<T>(cinfo_.container(), cinfo_.name(), &resource,
+                                 [this](T** ret) EXCLUSIVE_LOCKS_REQUIRED(mu_) {
+                                   Status s = CreateResource(ret);
+                                   if (!s.ok() && *ret != nullptr) {
+                                     CHECK((*ret)->Unref());
+                                   }
+                                   return s;
+                                 }));
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    /** scalar_sigmoid_fast_derivative_op
+  * \ingroup CXX11_NeuralNetworks_Module
+  * \brief Template functor to compute the fast derivative of a sigmoid
+  *
+  * Input should be the backpropagated gradient.
+  *
+  * \sa class CwiseUnaryOp, Cwise::sigmoid_fast_derivative()
+  */
+template <typename T>
+struct scalar_sigmoid_fast_derivative_op {
+  EIGEN_EMPTY_STRUCT_CTOR(scalar_sigmoid_fast_derivative_op)
+  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE T operator()(const T& y) const {
+    const T one = T(1);
+    return (one - y) * y;
+  }
     }
-    
-    StreamPeerSSL::LoadCertsFromMemory StreamPeerSSL::load_certs_func = NULL;
-bool StreamPeerSSL::available = false;
-bool StreamPeerSSL::initialize_certs = true;
-    
-    #ifndef STREAM_PEER_SSL_H
-#define STREAM_PEER_SSL_H
-    
-    
-    {	ScriptDebugger();
-	virtual ~ScriptDebugger() { singleton = NULL; }
-};
-    
-    void EditorHelp::_class_desc_select(const String &p_select) {
-    }
-    
-    	for (const List<StringName>::Element *E = order.back(); E; E = E->prev()) {
-    }
-    
-    	EditorNode *editor;
-	UndoRedo *undo_redo;
-	Control *canvas_item_editor;
-    
-    XGBOOST_REGISTER_SPARSE_PAGE_FORMAT(lz4hc)
-.describe('Apply LZ4 binary data compression(high compression ratio) for ext memory.')
-.set_body([]() {
-    return new SparsePageLZ4Format<bst_uint>(true);
-  });
     
     
     {
+    {}  // namespace port
+}  // namespace tensorflow
+    
+      void CopyCPUTensorToDevice(const Tensor *cpu_tensor, Device *device,
+                             Tensor *device_tensor,
+                             StatusCallback done) const override;
+    
+    #include 'tensorflow/compiler/xla/service/versioned_computation_handle.h'
+    
+    #include <memory>
+#include 'tensorflow/core/framework/reader_base.h'
+#include 'tensorflow/core/framework/reader_op_kernel.h'
+#include 'tensorflow/core/lib/core/errors.h'
+#include 'tensorflow/core/lib/io/inputbuffer.h'
+#include 'tensorflow/core/lib/strings/strcat.h'
+#include 'tensorflow/core/platform/env.h'
+    
+      Status ResetLocked() override {
+    offset_ = 0;
+    reader_.reset(nullptr);
+    file_.reset(nullptr);
+    return ReaderBase::ResetLocked();
+  }
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    namespace tensorflow {
+namespace functor {
+DEFINE_UNARY2(get_angle, complex64, complex128);
+}  // namespace functor
+}  // namespace tensorflow
+    
+    namespace ONNXIR
+{
+    namespace Utils
     {
-    {  inline void PutChar(char ch) {
-    out_buf += ch;
-    if (out_buf.length() >= kBufferSize) Flush();
-  }
-  inline void Flush(void) {
-    if (out_buf.length() != 0) {
-      fp->Write(&out_buf[0], out_buf.length());
-      out_buf.clear();
+        class TensorUtils
+        {
+        public:
+#define DEFINE_UNPACK_TENSOR(T, Type, fieldName, fieldSize)                                                                    \
+    static Common::Status UnpackTensor(const onnx::TensorProto& p_tensor, /*out*/T* p_data, int64_t p_expected_size)           \
+    {                                                                                                                          \
+        if (Type != p_tensor.data_type()                                                                                       \
+            || nullptr == p_data)                                                                                              \
+        {                                                                                                                      \
+            return Common::Status(Common::StatusCategory::ONNX, Common::StatusCode::INVALID_ARGUMENT);                        \
+        }                                                                                                                      \
+        if (p_tensor.has_raw_data())                                                                                           \
+        {                                                                                                                      \
+            if (p_tensor.raw_data().size() != (p_expected_size) * sizeof(T))                                                   \
+                return Common::Status(Common::StatusCategory::ONNX, Common::StatusCode::FAIL,                                 \
+                                               'UnpackTensor: the pre-allocate size does not match the raw data size');        \
+            UnpackTensorWithRawData(p_tensor, p_data);                                                                         \
+            return Common::Status::OK();                                                                                       \
+        }                                                                                                                      \
+        if (p_tensor.fieldSize() != p_expected_size)                                                                           \
+            return Common::Status(Common::StatusCategory::ONNX, Common::StatusCode::FAIL,                                     \
+                                            'UnpackTensor: the pre-allocate size does not match the size in proto');           \
+        for (auto elem : p_tensor.fieldName())                                                                                 \
+        {                                                                                                                      \
+            *p_data++ = static_cast<T>(elem);                                                                                  \
+        }                                                                                                                      \
+        return Common::Status::OK();                                                                                           \
     }
-  }
-};
-}  // namespace common
-}  // namespace xgboost
-#endif  // XGBOOST_COMMON_BASE64_H_
-
-    
-      inline void SkipLine(void) {
-    do {
-      ch_buf = this->GetChar();
-    } while (ch_buf != EOF && ch_buf != '\n' && ch_buf != '\r');
-  }
-    
-    #if XGBOOST_CUSTOMIZE_GLOBAL_PRNG
-/*!
- * \brief An customized random engine, used to be plugged in PRNG from other systems.
- *  The implementation of this library is not provided by xgboost core library.
- *  Instead the other library can implement this class, which will be used as GlobalRandomEngine
- *  If XGBOOST_RANDOM_CUSTOMIZE = 1, by default this is switched off.
- */
-class CustomGlobalRandomEngine {
- public:
-  /*! \brief The result type */
-  typedef size_t result_type;
-  /*! \brief The minimum of random numbers generated */
-  inline static constexpr result_type min() {
-    return 0;
-  }
-  /*! \brief The maximum random numbers generated */
-  inline static constexpr result_type max() {
-    return std::numeric_limits<size_t>::max();
-  }
-  /*!
-   * \brief seed function, to be implemented
-   * \param val The value of the seed.
-   */
-  void seed(result_type val);
-  /*!
-   * \return next random number.
-   */
-  result_type operator()();
-};
-    
-      uint64_t uint64_t2[2] = {1U, 2U};
-  EXPECT_EQ(info.group_ptr.size(), 0);
-  info.SetInfo('group', uint64_t2, xgboost::kUInt64, 2);
-  ASSERT_EQ(info.group_ptr.size(), 3);
-  EXPECT_EQ(info.group_ptr[2], 3);
-    
-    struct EvalPoissonNegLogLik : public EvalEWiseBase<EvalPoissonNegLogLik> {
-  const char *Name() const override {
-    return 'poisson-nloglik';
-  }
-  inline bst_float EvalRow(bst_float y, bst_float py) const {
-    const bst_float eps = 1e-16f;
-    if (py < eps) py = eps;
-    return common::LogGamma(y + 1.0f) + py - std::log(py) * y;
-  }
-};
-    
-    namespace rocksdb {
+    }
+    }
     }
     
-    #pragma once
-#include <deque>
-#include <string>
-    
-    #include 'rocksjni/statisticsjni.h'
-    
-    namespace rocksdb {
+    namespace ONNXIR {
+    // Taken from ONNX
+    REGISTER_OPERATOR_SCHEMA(Sigmoid)
+        .Description('Sigmoid takes one input data (Tensor<T>) and produces one output data '
+            '(Tensor<T>) where the sigmoid function, y = 1 / (1 + exp(-x)), is applied to the '
+            'tensor elementwise.')
+        .Input('X', 'input tensor', 'T')
+        .Output('Y', 'The sigmoid value of the input tensor computed element-wise', 'T')
+        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
+            'Constrain input and output types to float tensors.');
     }
     
-    /**
- * @brief rename fid
- * @details Only modify object in rados once,
- * so this rename operation is atomic in term of rados
- *
- * @param old_fname [description]
- * @param new_fname [description]
- *
- * @return [description]
- */
-Status EnvLibrados::_RenameFid(const std::string& old_fname,
-                               const std::string& new_fname) {
-  std::string fid;
-  Status s = _GetFid(old_fname, fid);
-    }
+        REGISTER_OPERATOR_SCHEMA(Dropout)
+        .Description('Dropout takes one input data (Tensor<float>) and produces two Tensor outputs, '
+            'output (Tensor<float>) and mask (Tensor<bool>). Depending on whether it is in '
+            'test mode or not, the output Y will either be a random dropout, or a simple '
+            'copy of the input. Note that our implementation of Dropout does scaling in '
+            'the training phase, so during testing nothing needs to be done.')
+        .Input('data', 'The input data as Tensor.', 'T')
+        .Output('output', 'The output.', 'T')
+        .Output('mask',
+            'The output mask. If is_test is nonzero, this output is not filled.', 'T')
+        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
+            'Constrain input and output types to float tensors.')
+        .Attr('ratio',
+            '(float, default 0.5) the ratio of random dropout',
+            AttrType::AttributeProto_AttributeType_FLOAT, float(0.5))
+        .Attr('is_test',
+            '(int, default 0) if nonzero, run dropout in test mode where '
+            'the output is simply Y = X.',
+            AttrType::AttributeProto_AttributeType_INT, int64_t(0));
     
-    inline
-bool BlockFetcher::TryGetCompressedBlockFromPersistentCache() {
-  if (cache_options_.persistent_cache &&
-      cache_options_.persistent_cache->IsCompressed()) {
-    // lookup uncompressed cache mode p-cache
-    status_ = PersistentCacheHelper::LookupRawPage(
-        cache_options_, handle_, &heap_buf_, block_size_ + kBlockTrailerSize);
-    if (status_.ok()) {
-      used_buf_ = heap_buf_.get();
-      slice_ = Slice(heap_buf_.get(), block_size_);
-      return true;
-    } else if (!status_.IsNotFound() && ioptions_.info_log) {
-      assert(!status_.ok());
-      ROCKS_LOG_INFO(ioptions_.info_log,
-                     'Error reading from persistent cache. %s',
-                     status_.ToString().c_str());
+        // TODO: We should be able to configure IO chunks based on size.
+    // distribute utterances over chunks
+    // We simply count off frames until we reach the chunk size.
+    // Note that we first randomize the chunks, i.e. when used, chunks are non-consecutive and thus cause the disk head to seek for each chunk.
+    
+    
+    {    auto status = stub_->SayHello(&context, request_msg, &response_msg);
+    if (status.ok()) {
+      const HelloReply *response = response_msg.GetRoot();
+      return response->message()->str();
+    } else {
+      std::cerr << status.error_code() << ': ' << status.error_message()
+                << std::endl;
+      return 'RPC failed';
     }
   }
-  return false;
+    
+    static void Error(const flatbuffers::FlatCompiler *flatc,
+                  const std::string &err, bool usage, bool show_exe_name) {
+  if (show_exe_name) { printf('%s: ', g_program_name); }
+  printf('error: %s\n', err.c_str());
+  if (usage) { printf('%s', flatc->GetUsageString(g_program_name).c_str()); }
+  exit(1);
 }
     
-      void LoadDependency(const std::vector<SyncPointPair>& dependencies);
-  void LoadDependencyAndMarkers(const std::vector<SyncPointPair>& dependencies,
-    const std::vector<SyncPointPair>& markers);
-  bool PredecessorsAllCleared(const std::string& point);
-  void SetCallBack(const std::string& point,
-    const std::function<void(void*)>& callback) {
-  std::lock_guard<std::mutex> lock(mutex_);
-  callbacks_[point] = callback;
-}
-    
-    #pragma once
-    
-    void setAssertHandler(AssertHandler assertHandler) {
-    gAssertHandler = assertHandler;
-}
-    
-    /**
- * RAII Object that attaches a thread to the JVM. Failing to detach from a thread before it
- * exits will cause a crash, as will calling Detach an extra time, and this guard class helps
- * keep that straight. In addition, it remembers whether it performed the attach or not, so it
- * is safe to nest it with itself or with non-fbjni code that manages the attachment correctly.
- *
- * Potential concerns:
- *  - Attaching to the JVM is fast (~100us on MotoG), but ideally you would attach while the
- *    app is not busy.
- *  - Having a thread detach at arbitrary points is not safe in Dalvik; you need to be sure that
- *    there is no Java code on the current stack or you run the risk of a crash like:
- *      ERROR: detaching thread with interp frames (count=18)
- *    (More detail at https://groups.google.com/forum/#!topic/android-ndk/2H8z5grNqjo)
- *    ThreadScope won't do a detach if the thread was already attached before the guard is
- *    instantiated, but there's probably some usage that could trip this up.
- *  - Newly attached C++ threads only get the bootstrap class loader -- i.e. java language
- *    classes, not any of our application's classes. This will be different behavior than threads
- *    that were initiated on the Java side. A workaround is to pass a global reference for a
- *    class or instance to the new thread; this bypasses the need for the class loader.
- *    (See http://docs.oracle.com/javase/7/docs/technotes/guides/jni/spec/invocation.html#attach_current_thread)
- *    If you need access to the application's classes, you can use ThreadScope::WithClassLoader.
- */
-class FBEXPORT ThreadScope {
- public:
-  ThreadScope();
-  ThreadScope(ThreadScope&) = delete;
-  ThreadScope(ThreadScope&&) = default;
-  ThreadScope& operator=(ThreadScope&) = delete;
-  ThreadScope& operator=(ThreadScope&&) = delete;
-  ~ThreadScope();
-    }
-    
-    #pragma once
-#include <cstring>
-#include <string>
-#include <sstream>
-    
-      // The STL assumes rvalue references are unique and for simplicity's sake, we
-  // make the same assumption here, that &ref != this.
-  RefPtr<T>& operator=(RefPtr<T>&& ref) {
-    unrefIfNecessary(m_ptr);
-    m_ptr = ref.m_ptr;
-    ref.m_ptr = nullptr;
-    return *this;
+    std::string GenType(const Type &type) {
+  if (type.enum_def != nullptr && !type.enum_def->is_union) {
+    // it is a reference to an enum type
+    return GenTypeRef(type.enum_def);
   }
+  switch (type.base_type) {
+    case BASE_TYPE_VECTOR: {
+      std::string typeline;
+      typeline.append('\'type\' : \'array\', \'items\' : { ');
+      if (type.element == BASE_TYPE_STRUCT) {
+        typeline.append(GenTypeRef(type.struct_def));
+      } else {
+        typeline.append(GenType(GenNativeType(type.element)));
+      }
+      typeline.append(' }');
+      return typeline;
+    }
+    case BASE_TYPE_STRUCT: {
+      return GenTypeRef(type.struct_def);
+    }
+    case BASE_TYPE_UNION: {
+      std::string union_type_string('\'anyOf\': [');
+      const auto &union_types = type.enum_def->vals.vec;
+      for (auto ut = union_types.cbegin(); ut < union_types.cend(); ++ut) {
+        auto &union_type = *ut;
+        if (union_type->union_type.base_type == BASE_TYPE_NONE) { continue; }
+        if (union_type->union_type.base_type == BASE_TYPE_STRUCT) {
+          union_type_string.append(
+              '{ ' + GenTypeRef(union_type->union_type.struct_def) + ' }');
+        }
+        if (union_type != *type.enum_def->vals.vec.rbegin()) {
+          union_type_string.append(',');
+        }
+      }
+      union_type_string.append(']');
+      return union_type_string;
+    }
+    case BASE_TYPE_UTYPE: return GenTypeRef(type.enum_def);
+    default: return GenType(GenNativeType(type.base_type));
+  }
+}
+    
+          ss << '\n';
+    
+    #include 'flatbuffers/flatbuffers.h'
