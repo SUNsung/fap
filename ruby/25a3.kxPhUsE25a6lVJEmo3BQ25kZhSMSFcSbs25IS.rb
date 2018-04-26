a6061b291cb80076aa5256f5c398cc8e1c3f4880
SUNsung <weatherius@gmail.com>
2018-04-26 15:41:26 +0300

    Rewrite [25a3.kxPhUsE25a6lVJEmo3BQ25kZhSMSFcSbs25IS.rb] File in: 2018-04-26 15:41:26

--- ruby/25a3.kxPhUsE25a6lVJEmo3BQ25kZhSMSFcSbs25IS.rb
+++ ruby/25a3.kxPhUsE25a6lVJEmo3BQ25kZhSMSFcSbs25IS.rb
@@ -1,66 +1,81 @@
 
         
-                css('h6').each do |node|
-          node.name = 'h4'
-        end
+            def replace(index, name)
+      @filters[assert_index(index)] = filter_const(name)
+    end
     
-              if argv.length == 2
-            # @deprecated
-            @env.ui.warn('WARNING: The second argument to `vagrant box remove`')
-            @env.ui.warn('is deprecated. Please use the --provider flag. This')
-            @env.ui.warn('feature will stop working in the next version.')
-            options[:provider] = argv[1]
+        def initialize
+      @pages = {}
+    end
+    
+      def apply
+    super
+  ensure
+    clear_cache
+  end
+end
+
+    
+            <<~EOS
+          Anaconda is known to frequently break Homebrew builds, including Vim and
+          MacVim, due to bundling many duplicates of system and Homebrew-available
+          tools.
+    
+        if ObserverPathnameExtension.total.zero?
+      puts 'Nothing pruned' if ARGV.verbose?
+    else
+      n, d = ObserverPathnameExtension.counts
+      print 'Pruned #{n} symbolic links '
+      print 'and #{d} directories ' if d.positive?
+      puts 'from #{HOMEBREW_PREFIX}'
+    end
+  end
+end
+
+    
+          # If the importer is based on files on the local filesystem
+      # this method should return folders which should be watched
+      # for changes.
+      #
+      # @return [Array<String>] List of absolute paths of directories to watch
+      def directories_to_watch
+        []
+      end
+    
+      # Uninstalls this logger from \{Sass.logger\}. This should only be called if
+  # the logger was installed using \{#install!}
+  def uninstall!
+    if Sass.logger != self
+      throw Exception.new('Can't uninstall a logger that's not currently installed.')
+    end
+    
+          res << val.to_s(:quote => :none)
+      res << ' ' if @after && @whitespace_after
+      res << @after.perform(environment).to_s if @after
+      str = Sass::Script::Value::String.new(
+        res, :identifier,
+        (to_quoted_equivalent.to_sass if deprecation == :potential))
+      str.source_range = source_range
+      opts(str)
+    end
+    
+            def validate!
+          super
+          if @pod_name.nil? && !@wipe_all
+            # Security measure, to avoid removing the pod cache too agressively by mistake
+            help! 'You should either specify a pod name or use the --all flag'
           end
+        end
     
-    # Copyright (C) 2008 Rapid7, Inc.
+            attr_reader :node, :variable, :referenced, :references
+        alias referenced? referenced
     
-    # This is a completely hackish way to do this, and could break with future
-# versions of the JDK.  Need to find a better way to use sun.security.tools.KeyTool
-# and .JarSigner than modifying the source.  These rely on internal APIs that may
-# change.
-signer = Rjb::import('javaCompile.SignJar')
-#clsKeyTool = Rjb::import('sun.security.tools.KeyTool')
-#clsKeyTool = Rjb::import('sun.security.tools.KeyToolMSF')
-#clsJarSigner = Rjb::import('javaCompile.SignJar.JarSignerMSF')
-#clsJarSigner = Rjb::import('sun.security.tools.JarSigner')
-#clsJarSigner = Rjb::import('sun.security.tools.JarSignerMSF')
-    
-        def index
-      pods_json = PodPresenter.as_collection(Pod.all)
-    
-      def user_search
-    if params[:admins_controller_user_search]
-      search_params = params.require(:admins_controller_user_search)
-                            .permit(:username, :email, :guid, :under13)
-      @search = UserSearch.new(search_params)
-      @users = @search.perform
-    end
-    
-          def reauthenticate(params)
-        sign_out current_user
-        redirect_to new_api_openid_connect_authorization_path(params)
-      end
-    
-          # Inspired by https://github.com/nov/openid_connect_sample/blob/master/app/controllers/connect/clients_controller.rb#L24
-      def create
-        registrar = OpenIDConnect::Client::Registrar.new(request.url, params)
-        client = Api::OpenidConnect::OAuthApplication.register! registrar
-        render json: client.as_json(root: false)
-      end
-    
-        # Returns true if a file has been assigned.
-    def file?
-      original_filename.present?
-    end
-    
-        def self.names_for(klass)
-      instance.names_for(klass)
-    end
-    
-        private
-    
-            def has_column?
-          @subject.column_names.include?('#{@attachment_name}_file_name')
+            def each_unnecessary_space_match(node, &blk)
+          each_match_range(
+            contents_range(node),
+            MULTIPLE_SPACES_BETWEEN_ITEMS_REGEX,
+            &blk
+          )
         end
       end
     end
@@ -68,74 +83,6 @@ signer = Rjb::import('javaCompile.SignJar')
 end
 
     
-        def self.included(base)
-      ActiveRecord::ConnectionAdapters::Table.send :include, TableDefinition
-      ActiveRecord::ConnectionAdapters::TableDefinition.send :include, TableDefinition
-      ActiveRecord::ConnectionAdapters::AbstractAdapter.send :include, Statements
-      ActiveRecord::Migration::CommandRecorder.send :include, CommandRecorder
-    end
-    
-        # Never trust parameters from the scary internet, only allow the white list through.
-    def book_params
-      params.require(:book).permit(:name)
-    end
-end
-
-    
-              @node = node
-          @variable = variable
-          @referenced = false
-          @references = []
-        end
-    
-    module RuboCop
-  module Cop
-    module Layout
-      # Checks for unnecessary additional spaces inside array percent literals
-      # (i.e. %i/%w).
-      #
-      # @example
-      #
-      #   # bad
-      #   %w(foo  bar  baz)
-      #   # good
-      #   %i(foo bar baz)
-      class SpaceInsideArrayPercentLiteral < Cop
-        include MatchRange
-        include PercentLiteral
-    
-            def repeated_condition?(previous, condition)
-          previous.any? { |c| c.include?(condition) }
-        end
-      end
-    end
+        expect(corrected).to eq(''something'.to_sym')
   end
 end
-
-    
-    module RuboCop
-  module Cop
-    module Lint
-      # In math and Python, we can use `x < y < z` style comparison to compare
-      # multiple value. However, we can't use the comparison in Ruby. However,
-      # the comparison is not syntax error. This cop checks the bad usage of
-      # comparison operators.
-      #
-      # @example
-      #
-      #   # bad
-      #
-      #   x < y < z
-      #   10 <= x <= 20
-      #
-      # @example
-      #
-      #   # good
-      #
-      #   x < y && y < z
-      #   10 <= x && x <= 20
-      class MultipleCompare < Cop
-        MSG = 'Use the `&&` operator to compare multiple values.'.freeze
-    
-                  include Something
-              def do_something; end
