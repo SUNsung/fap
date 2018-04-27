3c1442948f3caff79aeab5285cd2bf347077667b
SUNsung <weatherius@gmail.com>
2018-04-27 05:19:55 +0300

    Rewrite [19x0PypItE6h.19Yd6Q1PYvSOI19qv4GZ.ce.So19DeN.py] File in: 2018-04-27 05:19:54

--- pyton/19x0PypItE6h.19Yd6Q1PYvSOI19qv4GZ.ce.So19DeN.py
+++ pyton/19x0PypItE6h.19Yd6Q1PYvSOI19qv4GZ.ce.So19DeN.py
@@ -1,34 +1,76 @@
 
         
-        
-class TerminalModule(TerminalBase):
+        import sys
     
-            for n in (-10, -1, 0, 1, 2, 10, 499, 500, 501, 1000):
-            d = deque('abcdef', 500)
-            d *= n
-            self.assertEqual(d, deque(('abcdef' * n)[-500:]))
-            self.assertEqual(d.maxlen, 500)
+            for srcobj, loader in self._iter_loaders(template):
+            try:
+                rv = loader.get_source(environment, template)
+                if trv is None:
+                    trv = rv
+            except TemplateNotFound:
+                rv = None
+            attempts.append((loader, srcobj, rv))
     
-        def __init__(self):
-        CGIXMLRPCRequestHandler.__init__(self)
-        XMLRPCDocGenerator.__init__(self)
     
-            del x; del y; gc.collect(); gc.collect(); gc.collect()
-        a.append(100)
-        a.pop()
-        x = (c_int * 16).from_buffer(a)
+class MethodView(with_metaclass(MethodViewType, View)):
+    '''A class-based view that dispatches request methods to the corresponding
+    class methods. For example, if you implement a ``get`` method, it will be
+    used to handle ``GET`` requests. ::
     
-            WNDPROC_2 = WINFUNCTYPE(c_long, c_int, c_int, c_int, c_int)
+        with pytest.raises(NoAppException):
+        locate_app(info, iname, aname)
     
-    def download_pdf(link, location, name):
-    try:
-        response = requests.get(link)
-        with open(os.path.join(location, name), 'wb') as f:
-        	f.write(response.content)
-        	f.close()
-    except HTTPError:
-        print('>>> Error 404: cannot be downloaded!\n') 
-        raise   
-    except socket.timeout:
-        print(' '.join(('can't download', link, 'due to connection timeout!')) )
-        raise
+    https://github.com/Homebrew/homebrew-core/blob/master/Formula/httpie.rb
+    
+    
+def test_unicode_basic_auth(httpbin):
+    # it doesn't really authenticate us because httpbin
+    # doesn't interpret the utf8-encoded auth
+    http('--verbose', '--auth', u'test:%s' % UNICODE,
+         httpbin.url + u'/basic-auth/test/' + UNICODE)
+    
+        def get_auth(self, username=None, password=None):
+        '''
+        If `auth_parse` is set to `True`, then `username`
+        and `password` contain the parsed credentials.
+    
+        >>> humanize_bytes(1)
+    '1 B'
+    >>> humanize_bytes(1024, precision=1)
+    '1.0 kB'
+    >>> humanize_bytes(1024 * 123, precision=1)
+    '123.0 kB'
+    >>> humanize_bytes(1024 * 12342, precision=1)
+    '12.1 MB'
+    >>> humanize_bytes(1024 * 12342, precision=2)
+    '12.05 MB'
+    >>> humanize_bytes(1024 * 1234, precision=2)
+    '1.21 MB'
+    >>> humanize_bytes(1024 * 1234 * 1111, precision=2)
+    '1.31 GB'
+    >>> humanize_bytes(1024 * 1234 * 1111, precision=1)
+    '1.3 GB'
+    
+    
+RequestItems = namedtuple('RequestItems',
+                          ['headers', 'data', 'files', 'params'])
+    
+        requests_session.cookies = session.cookies
+    
+    # This view is called from FlatpageFallbackMiddleware.process_response
+# when a 404 is raised, which often means CsrfViewMiddleware.process_view
+# has not been called even if CsrfViewMiddleware is installed. So we need
+# to use @csrf_protect, in case the template needs {% csrf_token %}.
+# However, we can't just wrap this view; if no matching flatpage exists,
+# or a redirect is required for authentication, the 404 needs to be returned
+# without any CSRF checks. Therefore, we only
+# CSRF protect the internal implementation.
+    
+            start = datetime.now()
+        ols = linear_model.LinearRegression()
+        ols.fit(X, Y)
+        time_ols[i] = total_seconds(datetime.now() - start)
+    
+    # TASK: Predict the outcome on the testing set in a variable named y_predicted
+    
+    n_clusters = 3
