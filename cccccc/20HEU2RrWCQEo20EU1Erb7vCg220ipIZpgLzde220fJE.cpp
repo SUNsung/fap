89bd60bd9429284b1405a7d61f6c74839af17399
SUNsung <weatherius@gmail.com>
2018-04-27 19:57:00 +0300

    Rewrite [20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp] File in: 2018-04-27 19:56:59

--- cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
+++ cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
@@ -1,181 +1,325 @@
 
         
-          ServerConfig server_config;
-  server_config.set_server_type(SYNC_SERVER);
+        // Generate param traits read methods.
+#include 'ipc/param_traits_read_macros.h'
+namespace IPC {
+#include 'content/nw/src/common/common_message_generator.h'
+}  // namespace IPC
     
-    #endif  // GRPC_INTERNAL_COMPILER_NODE_GENERATOR_HELPERS_H
-
-    
-    void SecureAuthContext::AddProperty(const grpc::string& key,
-                                    const grpc::string_ref& value) {
-  if (!ctx_) return;
-  grpc_auth_context_add_property(ctx_, key.c_str(), value.data(), value.size());
+    Base::Base(int id,
+           const base::WeakPtr<ObjectManager>& object_manager,
+           const base::DictionaryValue& option,
+	   const std::string& extension_id)
+    : extension_id_(extension_id),
+      id_(id),
+      delay_destruction_(false),
+      pending_destruction_(false),
+      object_manager_(object_manager) {
 }
     
-    #endif  // GRPC_TEST_CPP_UTIL_SUBPROCESS_H
-
+    v8::Handle<v8::Value> DeallocateObject(int routing_id,
+                                       int object_id) {
+  RenderThread::Get()->Send(new ShellViewHostMsg_Deallocate_Object(
+      routing_id, object_id));
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  return v8::Undefined(isolate);
+}
     
-      /// Read some data from the stream. Returns the number of bytes read. Throws
-  /// an exception on failure.
-  template <typename MutableBufferSequence>
-  std::size_t read_some(const MutableBufferSequence& buffers);
-    
-    // Standard library components can't be forward declared, so we'll have to
-// include the array header. Fortunately, it's fairly lightweight and doesn't
-// add significantly to the compile time.
-#if defined(BOOST_ASIO_HAS_STD_ARRAY)
-# include <array>
-#endif // defined(BOOST_ASIO_HAS_STD_ARRAY)
-    
-    namespace boost {
-namespace date_time {
-    }
-    }
-    
-    #endif // BOOST_ASIO_DETAIL_FENCED_BLOCK_HPP
-
-    
-      explicit winrt_buffer_impl(const boost::asio::mutable_buffer& b)
-  {
-    bytes_ = const_cast<byte*>(boost::asio::buffer_cast<const byte*>(b));
-    length_ = 0;
-    capacity_ = boost::asio::buffer_size(b);
+    void Clipboard::CallSync(const std::string& method,
+                         const base::ListValue& arguments,
+                         base::ListValue* result) {
+  if (method == 'Get') {
+    result->AppendString(GetText());
+  } else {
+    NOTREACHED() << 'Invalid call to Clipboard method:' << method
+                 << ' arguments:' << arguments;
   }
-    
-      if (shutdown_)
-  {
-    io_service_.post_immediate_completion(op, false);
-    return;
-  }
-    
-    Status FilesystemConfigPlugin::genPack(const std::string& name,
-                                       const std::string& value,
-                                       std::string& pack) {
-  if (name == '*') {
-    // The config requested a multi-pack.
-    std::vector<std::string> paths;
-    resolveFilePattern(value, paths);
-    }
-    }
+}
     
     
-    {    // Check if overflowed
-    if (value > 0) {
-      return boost::lexical_cast<std::string>(std::llround(value));
-    }
-  }
-    
-    #include 'osquery/core/utils.h'
-    
-    struct ExceptionInfo {
-  const std::type_info* type{nullptr};
-  // The values in frames are IP (instruction pointer) addresses.
-  // They are only filled if the low-level exception tracer library is
-  // linked in or LD_PRELOADed.
-  std::vector<uintptr_t> frames; // front() is top of stack
+    {protected:
+  BaseEvent(){}
+  virtual ~BaseEvent(){}
 };
     
-    template <typename T>
-constexpr T constexpr_log2(T t) {
-  return detail::constexpr_log2_(T(0), t);
-}
+    namespace nwapi {
+    }
     
-    class memory_resource {
+    class NwObjCreateFunction : public NWSyncExtensionFunction {
  public:
-  virtual ~memory_resource() = default;
-  virtual void* allocate(
-      const size_t bytes,
-      const size_t alignment = max_align_v) = 0;
-  virtual void deallocate(
-      void* p,
-      const size_t bytes,
-      const size_t alignment = max_align_v) = 0;
-};
-    
-    AsyncIO::AsyncIO(size_t capacity, PollMode pollMode) : capacity_(capacity) {
-  CHECK_GT(capacity_, 0);
-  completed_.reserve(capacity_);
-  if (pollMode == POLLABLE) {
-    pollFd_ = eventfd(0, EFD_NONBLOCK);
-    checkUnixError(pollFd_, 'AsyncIO: eventfd creation failed');
-  }
-}
-    
-    /**
- * Canonicalize the parent path, leaving the filename (last component)
- * unchanged.  You may use this before creating a file instead of
- * boost::filesystem::canonical, which requires that the entire path exists.
- */
-path canonical_parent(const path& p, const path& basePath = current_path());
-    
-    void AsyncFileWriter::writeMessage(std::string&& buffer, uint32_t flags) {
-  auto data = data_.lock();
-  if ((data->currentBufferSize >= data->maxBufferBytes) &&
-      !(flags & NEVER_DISCARD)) {
-    ++data->numDiscarded;
-    return;
-  }
+  NwObjCreateFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
     }
     
-      /**
-   * Get the output file.
+    DEFINE_string(backend, 'lmdb', 'The backend for storing the result');
+    
+      // Get a layer using a LayerParameter.
+  static shared_ptr<Layer<Dtype> > CreateLayer(const LayerParameter& param) {
+    if (Caffe::root_solver()) {
+      LOG(INFO) << 'Creating layer ' << param.name();
+    }
+    const string& type = param.type();
+    CreatorRegistry& registry = Registry();
+    CHECK_EQ(registry.count(type), 1) << 'Unknown layer type: ' << type
+        << ' (known types: ' << LayerTypeListString() << ')';
+    return registry[type](param);
+  }
+    
+     protected:
+  /// @copydoc BNLLLayer
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #endif  // CAFFE_CUDNN_LRN_LAYER_HPP_
+
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
+    
+     protected:
+  /**
+   * @param bottom input Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$
+   * @param top output Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the computed outputs @f$
+   *        y = \gamma ^ {\alpha x + \beta}
+   *      @f$
    */
-  const folly::File& getFile() const {
-    return file_;
-  }
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
     
-    std::shared_ptr<LogHandler> FileHandlerFactory::createHandler(
-    const Options& options) {
-  WriterFactory writerFactory;
-  return StandardLogHandlerFactory::createHandler(
-      getType(), &writerFactory, options);
+    // Computes all the cross product distances of the points from the line,
+// storing the actual (signed) cross products in distances.
+// Ignores distances of points that are further away than the previous point,
+// and overlaps the previous point by at least half.
+void DetLineFit::ComputeDistances(const ICOORD& start, const ICOORD& end) {
+  distances_.truncate(0);
+  ICOORD line_vector = end;
+  line_vector -= start;
+  square_length_ = line_vector.sqlength();
+  int line_length = IntCastRounded(sqrt(square_length_));
+  // Compute the distance of each point from the line.
+  int prev_abs_dist = 0;
+  int prev_dot = 0;
+  for (int i = 0; i < pts_.size(); ++i) {
+    ICOORD pt_vector = pts_[i].pt;
+    pt_vector -= start;
+    int dot = line_vector % pt_vector;
+    // Compute |line_vector||pt_vector|sin(angle between)
+    int dist = line_vector * pt_vector;
+    int abs_dist = dist < 0 ? -dist : dist;
+    if (abs_dist > prev_abs_dist && i > 0) {
+      // Ignore this point if it overlaps the previous one.
+      int separation = abs(dot - prev_dot);
+      if (separation < line_length * pts_[i].halfwidth ||
+          separation < line_length * pts_[i - 1].halfwidth)
+        continue;
+    }
+    distances_.push_back(DistPointPair(dist, pts_[i].pt));
+    prev_abs_dist = abs_dist;
+    prev_dot = dot;
+  }
 }
     
-      std::shared_ptr<LogHandler> createHandler(const Options& options) override;
-    
-    namespace folly {
+      // Main worker method that retrieves the next number in the sequence.
+  // Returns kInvalidVal if called more than N times after object initialization
+  int GetVal() {
+    const int kInvalidVal = -1;
+    const int kMaxNaturalNumberValue = 1 << num_bits_;
+    if (next_num_ >= kMaxNaturalNumberValue)
+      return kInvalidVal;
+    int n = next_num_;
     }
     
-      if (Att == INVALID_FILE_ATTRIBUTES) {
-    Printf('GetFileAttributesA() failed for \'%s\' (Error code: %lu).\n',
-        Path.c_str(), GetLastError());
-    return false;
+    template<typename T>
+inline void
+emitTLSLoad(Vout& v, TLSDatum<ThreadLocalNoCheck<T>> datum, Vreg d) {
+  auto const off = ThreadLocalNoCheck<T>::node_ptr_offset();
+  v << load{emitTLSAddr(v, datum) + safe_cast<int32_t>(off), d};
+}
+    
+    struct PageletTransport;
+struct PageletServerTaskEvent;
+    
+    Vtuple Vunit::makeTuple(const VregList& regs) {
+  auto i = tuples.size();
+  tuples.emplace_back(regs);
+  return Vtuple{i};
+}
+    
+      Variant ret = mpzToGMPObject(gmpData);
+    
+    /*
+ * find type of named file
+ */
+public const char *
+magic_file(struct magic_set *ms, const char *inname)
+{
+  if (ms == NULL)
+    return NULL;
+  return file_or_stream(ms, inname, NULL);
+}
+    
+    #include 'hphp/runtime/vm/hhbc.h'
+    
+    #include <cstdint>
+#include <cstring>
+#include <map>
+#include <set>
+    
+      return true;
+#elif !defined(__APPLE__) // LINUX/ELF
+  folly::symbolizer::ElfFile file;
+  if (file.openNoThrow(fname.c_str()) != 0) return false;
+    
+      static FILE *LightPopenImpl(const char *cmd, const char *type,
+                              const char *cwd);
+  static FILE *HeavyPopenImpl(const char *cmd, const char *type,
+                              const char *cwd);
+    
+        // Parallel training related with MA / BM
+    size_t m_modelAggregationBlockSize;
+    bool   m_resetSGDMomentum; 
+    bool   m_useNesterovBlockMomentum;
+    double m_blockLearningRate; 
+    double m_blockMomentumAsTimeConstant;
+    
+            // Sync graph inputs/outputs when serializing to proto.
+        void SyncGraphInputsOutputs();
+    
+            // Attribute representation, including name, type, and allowed values.
+        // The first element of allowed values (if specified) is the default
+        // value.
+        class Attribute
+        {
+        public:
+    }
+    
+    namespace ONNXIR
+{
+    namespace Common
+    {
+        Status::Status(StatusCategory p_category, int p_code, const std::string& p_msg)
+        {
+            m_state.reset(new State());
+            m_state->m_category = p_category;
+            m_state->m_code = p_code;
+            m_state->m_msg = p_msg;
+        }
+    }
+    }
+    
+            // Taken from ONNX
+        REGISTER_OPERATOR_SCHEMA(Neg)
+        .Description('Neg takes one input data (Tensor<T>) and produces one output data '
+            '(Tensor<T>) where each element flipped sign, y = -x, is applied to '
+            'the tensor elementwise.')
+        .Input('X', 'Input tensor of any shape', 'T')
+        .Output('Y', 'Output tensor of same shape and type as input X.', 'T')
+        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
+            'Constrain input and output types to float tensors.');
+    
+    
+    {}
+
+    
+    
+    REGISTER_OPERATOR_SCHEMA(LinearRegressor)
+        .SetDomain(c_mlDomain)
+        .Input('X', 'Data to be regressed', 'T')
+        .Output('Y', 'Regression outputs (one per target, per example', 'tensor(float)')
+        .Description(R'DOC(
+            Generalized linear regression evaluation.
+            If targets is set to 1 (default) then univariate regression is performed.
+            If targets is set to M then M sets of coefficients must be passed in as a sequence
+            and M results will be output for each input n in N.
+            Coefficients are of the same length as an n, and coefficents for each target are contiguous.
+           'Intercepts are optional but if provided must match the number of targets.
+            )DOC')
+        .TypeConstraint('T', { 'tensor(float)', 'tensor(double)', 'tensor(int64)', 'tensor(int32)' }, ' allowed types.')
+        .Attr('coefficients', 'weights of the model(s)', AttrType::AttributeProto_AttributeType_FLOATS)
+        .Attr('intercepts', 'weights of the intercepts (if used)', AttrType::AttributeProto_AttributeType_FLOATS)
+        .Attr('targets', 'total number of regression targets (default is 1)', AttrType::AttributeProto_AttributeType_INT)
+        .Attr('post_transform', 'post eval transform for score, enum 'NONE', 'SOFTMAX', 'LOGISTIC', 'SOFTMAX_ZERO', 'PROBIT'', AttrType::AttributeProto_AttributeType_STRING);
+    
+    
+    {        totalNumSequences += chunk.NumberOfSequences();
+        m_chunkToFileIndex.insert(make_pair(&chunk, m_latticeFiles.size() - 1));
+        m_chunks.push_back(&chunk);
+        if (m_chunks.size() >= numeric_limits<ChunkIdType>::max())
+            RuntimeError('Number of chunks exceeded overflow limit.');
+    }
+    
+      void ResetFeatureSet() {
+    assert(Inputs.empty());
+    memset(InputSizesPerFeature, 0, sizeof(InputSizesPerFeature));
+    memset(SmallestElementPerFeature, 0, sizeof(SmallestElementPerFeature));
   }
     
-    void Fuzzer::PrintStats(const char *Where, const char *End, size_t Units) {
-  size_t ExecPerSec = execPerSec();
-  if (Options.OutputCSV) {
-    static bool csvHeaderPrinted = false;
-    if (!csvHeaderPrinted) {
-      csvHeaderPrinted = true;
-      Printf('runs,block_cov,bits,cc_cov,corpus,execs_per_sec,tbms,reason\n');
-    }
-    Printf('%zd,%zd,%zd,%zd,%zd,%zd,%s\n', TotalNumberOfRuns,
-           MaxCoverage.BlockCoverage, MaxCoverage.CounterBitmapBits,
-           MaxCoverage.CallerCalleeCoverage, Corpus.size(), ExecPerSec, Where);
-  }
+    ExternalFunctions::ExternalFunctions() {
+#define EXT_FUNC(NAME, RETURN_TYPE, FUNC_SIG, WARN)                            \
+  this->NAME = ::NAME;                                                         \
+  CheckFnPtr((void *)::NAME, #NAME, WARN);
     }
     
-    size_t MutationDispatcher::ApplyDictionaryEntry(uint8_t *Data, size_t Size,
-                                                size_t MaxSize,
-                                                DictionaryEntry &DE) {
-  const Word &W = DE.GetW();
-  bool UsePositionHint = DE.HasPositionHint() &&
-                         DE.GetPositionHint() + W.size() < Size &&
-                         Rand.RandBool();
-  if (Rand.RandBool()) {  // Insert W.
-    if (Size + W.size() > MaxSize) return 0;
-    size_t Idx = UsePositionHint ? DE.GetPositionHint() : Rand(Size + 1);
-    memmove(Data + Idx + W.size(), Data + Idx, Size - Idx);
-    memcpy(Data + Idx, W.data(), W.size());
-    Size += W.size();
-  } else {  // Overwrite some bytes with W.
-    if (W.size() > Size) return 0;
-    size_t Idx = UsePositionHint ? DE.GetPositionHint() : Rand(Size - W.size());
-    memcpy(Data + Idx, W.data(), W.size());
+    
+    {} // namespace fuzzer
+    
+    // Parse a location, like:
+// \\?\UNC\Server\Share\  \\?\C:\  \\Server\Share\  \  C:\  C:
+// Returns number of characters considered if successful.
+static size_t ParseLocation(const std::string &FileName) {
+  size_t Pos = 0, Res;
+    }
+    
+    void Fuzzer::RereadOutputCorpus(size_t MaxSize) {
+  if (Options.OutputCorpus.empty() || !Options.ReloadIntervalSec) return;
+  std::vector<Unit> AdditionalCorpus;
+  ReadDirToVectorOfUnits(Options.OutputCorpus.c_str(), &AdditionalCorpus,
+                         &EpochOfLastReadOfOutputCorpus, MaxSize,
+                         /*ExitOnError*/ false);
+  if (Options.Verbosity >= 2)
+    Printf('Reload: read %zd new units.\n', AdditionalCorpus.size());
+  bool Reloaded = false;
+  for (auto &U : AdditionalCorpus) {
+    if (U.size() > MaxSize)
+      U.resize(MaxSize);
+    if (!Corpus.HasUnit(U)) {
+      if (size_t NumFeatures = RunOne(U)) {
+        CheckExitOnSrcPosOrItem();
+        Corpus.AddToCorpus(U, NumFeatures);
+        Reloaded = true;
+      }
+    }
   }
-  return Size;
+  if (Reloaded)
+    PrintStats('RELOAD');
 }
     
     
     {}  // namespace fuzzer
+    
+    uint32_t sha1_rol32(uint32_t number, uint8_t bits) {
+	return ((number << bits) | (number >> (32-bits)));
+}
+    
+    int ExecuteCommand(const std::string &Command);
