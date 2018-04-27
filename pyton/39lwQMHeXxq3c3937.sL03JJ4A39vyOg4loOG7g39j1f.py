cb2dccbd1cd677b93b37aee4ee5e554bb3d9a8ba
SUNsung <weatherius@gmail.com>
2018-04-27 07:13:37 +0300

    Rewrite [39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py] File in: 2018-04-27 07:13:37

--- pyton/39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py
+++ pyton/39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py
@@ -1,235 +1,70 @@
 
         
-        
-def main(argv):
-  del argv  # Unused.
+            def from_spider(self, spider, results):
+        requests = []
+        for method in self.tested_methods_from_spidercls(type(spider)):
+            bound_method = spider.__getattribute__(method)
+            requests.append(self.from_method(bound_method, results))
     
     
-def count_transit_points(time, event):
-  '''Computes the number of points in each transit of a given event.
+class Vehicle(metaclass=ABCMeta):
     
-    DATA_URL = 'https://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz'
+        def remove_link_to_crawl(self, url):
+        '''Remove the given link from `links_to_crawl`.'''
+        pass
     
-      eval_file_path = os.path.join(FLAGS.data_dir, EVAL_FILE)
-  _download_and_clean_file(eval_file_path, EVAL_URL)
+        @property
+    def value(self):
+        if self.is_ace() == 1:
+            return 1
+        elif self.is_face_card():
+            return 10
+        else:
+            return self._value
     
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-    
-      # Save the path vocabulary
-  path_vocab_filename = os.path.join(
-      embeddings_base_path, '%d_path_vocab' % model.lstm_output_dim)
-  with open(path_vocab_filename, 'w') as f_out:
-    f_out.write('\n'.join(path_vocab))
-    f_out.write('\n')
-    
-        if self.discretize_actions:
-      new_actions = []
-      idx = 0
-      for i, (dim, typ) in enumerate(zip(self._act_dims, self._act_types)):
-        if typ == spaces.discrete:
-          new_actions.append(actions[idx])
-          idx += 1
-        elif typ == spaces.box:
-          low, high = self.act_info[i]
-          cur_action = []
-          for j in xrange(dim):
-            cur_action.append(
-                low[j] + (high[j] - low[j]) * actions[idx] /
-                float(self.discretize_actions))
-            idx += 1
-          new_actions.append(np.hstack(cur_action))
-      actions = new_actions
-    
-    
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '--data-dir',
-      type=str,
-      default='',
-      help='Directory to download and extract CIFAR-10 to.')
-    
-        h = history.history
-    assert_array_almost_equal(h['loss'], h[loss_full_name], decimal=decimal)
-    assert_array_almost_equal(h['loss'], h['weighted_' + loss_full_name], decimal=decimal)
-    
-        x = Input(shape=(1,))
-    y = inner_model(x)
-    outer_model = Model(x, y)
-    assert outer_model.trainable_weights == inner_model.trainable_weights
-    inner_model.trainable = False
-    assert outer_model.trainable_weights == []
-    inner_model.trainable = True
-    inner_model.layers[-1].trainable = False
-    assert outer_model.trainable_weights == []
-    
-    
-EXCLUDE = {
-    'Optimizer',
-    'Wrapper',
-    'get_session',
-    'set_session',
-    'CallbackList',
-    'serialize',
-    'deserialize',
-    'get',
-    'set_image_dim_ordering',
-    'image_dim_ordering',
-    'get_variable_shape',
-}
-    
-    - The length and noise (i.e. 'useless' story components) impact the ability for
-LSTMs / GRUs to provide the correct answer. Given only the supporting facts,
-these RNNs can achieve 100% accuracy on many tasks. Memory networks and neural
-networks that use attentional processes can efficiently search through this
-noise to find the relevant statements, improving performance substantially.
-This becomes especially obvious on QA2 and QA3, both far longer than QA1.
-'''
-    
-    
-if six.PY3:
-    for line in open('tests/py3-ignores.txt'):
-        file_path = line.strip()
-        if file_path and file_path[0] != '#':
-            collect_ignore.append(file_path)
-    
-        def long_desc(self):
-        '''A long description of the command. Return short description when not
-        available. It cannot contain newlines, since contents will be formatted
-        by optparser which removes newlines and wraps text.
+            Accessing a node updates its position to the front of the LRU list.
         '''
-        return self.short_desc()
+        node = self.lookup[query]
+        if node is None:
+            return None
+        self.linked_list.move_to_front(node)
+        return node.results
     
-        def start_requests(self):
-        qargs = {'total': self.total, 'show': self.show}
-        url = '{}?{}'.format(self.baseurl, urlencode(qargs, doseq=1))
-        return [scrapy.Request(url, dont_filter=True)]
+        def __init__(self):
+        self.lookup = {}  # key: person_id, value: person_server
     
-        if WSAAddressToStringA(
-            ctypes.byref(addr),
-            addr_size,
-            None,
-            ip_string,
-            ctypes.byref(ip_string_size)
-    ) != 0:
-        raise socket.error(ctypes.FormatError())
+            (2016-01, url0), 2
+        (2016-01, url1), 1
+        '''
+        yield key, sum(values)
     
-    
-class NoViableAltException(RecognitionException):
-    '''@brief Unable to decide which alternative to choose.'''
-    
-        def test_create_entries(self):
-        self.dialog.top = self.root
-        self.dialog.row = 0
-        self.engine.setpat('hello')
-        self.dialog.create_entries()
-        self.assertIn(self.dialog.ent.get(), 'hello')
-    
-        def test_string_with_utf8_bom(self):
-        # see #18958
-        bom_json = '[1,2,3]'.encode('utf-8-sig').decode('utf-8')
-        with self.assertRaises(self.JSONDecodeError) as cm:
-            self.loads(bom_json)
-        self.assertIn('BOM', str(cm.exception))
-        with self.assertRaises(self.JSONDecodeError) as cm:
-            self.json.load(StringIO(bom_json))
-        self.assertIn('BOM', str(cm.exception))
-        # make sure that the BOM is not detected in the middle of a string
-        bom_in_str = ''{}''.format(''.encode('utf-8-sig').decode('utf-8'))
-        self.assertEqual(self.loads(bom_in_str), '\ufeff')
-        self.assertEqual(self.json.load(StringIO(bom_in_str)), '\ufeff')
-    
-    #
-# NOTE: This is the pi function from the decimal documentation, modified
-# for benchmarking purposes. Since floats do not have a context, the higher
-# intermediate precision from the original is NOT used, so the modified
-# algorithm only gives an approximation to the correctly rounded result.
-# For serious use, refer to the documentation or the appropriate literature.
-#
-def pi_float():
-    '''native float'''
-    lasts, t, s, n, na, d, da = 0, 3.0, 3, 1, 0, 0, 24
-    while s != lasts:
-        lasts = s
-        n, na = n+na, na+8
-        d, da = d+da, da+32
-        t = (t * n) / d
-        s += t
-    return s
-    
-            if self.closed:
-            raise ValueError('I/O operation on closed file')
-        if self.seekable:
-            try:
-                n = self.chunksize - self.size_read
-                # maybe fix alignment
-                if self.align and (self.chunksize & 1):
-                    n = n + 1
-                self.file.seek(n, 1)
-                self.size_read = self.size_read + n
-                return
-            except OSError:
-                pass
-        while self.size_read < self.chunksize:
-            n = min(8192, self.chunksize - self.size_read)
-            dummy = self.read(n)
-            if not dummy:
-                raise EOFError
-
-    
-        def add_folder(self, folder):
-        '''Create a folder and return an MH instance representing it.'''
-        return MH(os.path.join(self._path, folder),
-                  factory=self._factory)
+        def test_tc1_output(self):
+        self.tc1.run()
+        output = self.out.getvalue().strip()
+        self.assertEqual(output, self.average_result_tc1)
     
     
-# Test retrieval when run as a script.
-if __name__ == '__main__':
-    import argparse
+class ConcreteHandler1(Handler):
     
-        def accept_encodings(self):
-        r = {}
-        ae = self.headers.get('Accept-Encoding', '')
-        for e in ae.split(','):
-            match = self.aepattern.match(e)
-            if match:
-                v = match.group(3)
-                v = float(v) if v else 1.0
-                r[match.group(1)] = v
-        return r
+        def tearDown(self):
+        if not self._bProblem:
+            print('Tearing down')
+            time.sleep(0.1)
+            self._tm.publishReport()
+        else:
+            print('Test not executed. No tear down required.')
     
-        def test_stdin_stdout(self):
-        args = sys.executable, '-m', 'json.tool'
-        with Popen(args, stdin=PIPE, stdout=PIPE, stderr=PIPE) as proc:
-            out, err = proc.communicate(self.data.encode())
-        self.assertEqual(out.splitlines(), self.expect.encode().splitlines())
-        self.assertEqual(err, b'')
+        a_transaction = Transaction(True, num_obj)
+    try:
+        for i in range(3):
+            num_obj.increment()
+            print(num_obj)
+        a_transaction.commit()
+        print('-- committed')
     
-            WNDPROC_2 = WINFUNCTYPE(c_long, c_int, c_int, c_int, c_int)
+        def update(self, subject):
+        print(u'DecimalViewer: Subject %s has data %d' %
+              (subject.name, subject.data))
     
-            #Creating directory title page for current directory
-        with open('dir_title.html', 'w') as os_html:
-            os_html.write(header + dir_title)
-        
-        group.append(HTML('dir_title.html').render())
     
-    print 'PLUGIN_I18N_PATHS := ' + ','.join(os.path.relpath(plugin.path)
-                                         for plugin in plugins
-                                         if plugin.needs_translation)
-    
-    def set_extension(environ, ext):
-    environ['extension'] = ext
-    environ['render_style'], environ['content_type'] = extension_mapping[ext]
-
-    
-    from pylons import request
-from pylons import tmpl_context as c
-from pylons import app_globals as g
-    
-        @validate(VAdmin(),
-              award = VAwardByCodename('awardcn'))
-    def GET_winners(self, award):
-        if award is None:
-            abort(404, 'page not found')
+class User(object):
