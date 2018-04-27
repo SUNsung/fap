9f7e64bf680e302c1f29aca600a8a3f3a241d5e3
SUNsung <weatherius@gmail.com>
2018-04-27 15:32:16 +0300

    Rewrite [08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py] File in: 2018-04-27 15:32:16

--- pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
+++ pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
@@ -1,43 +1,74 @@
 
         
-                e = d.__copy__()
-        self.assertEqual(type(d), type(e))
-        self.assertEqual(list(d), list(e))
+            with open('README.md', 'w+') as sorted_file:
+        # Then all of the blocks are sorted individually
+        blocks = [''.join(sorted(block, key=lambda s: s.lower())) for block in blocks]
+        # And the result is written back to README.md
+        sorted_file.write(''.join(blocks))
     
-    __all__ = ['make_scanner']
+        def implements_to_string(cls):
+        cls.__unicode__ = cls.__str__
+        cls.__str__ = lambda x: x.__unicode__().encode('utf-8')
+        return cls
     
-            for func, args, kwargs, expected in self.CALLS_KWARGS:
-            with self.subTest(func=func, args=args, kwargs=kwargs):
-                result = _testcapi.pyobject_fastcalldict(func, args, kwargs)
-                self.check_result(result, expected)
     
-            If the optional allow_dotted_names argument is true and the
-        instance does not have a _dispatch method, method names
-        containing dots are supported and resolved, as long as none of
-        the name segments start with an '_'.
-    
-    class PyCFunctionObjectPtr(PyObjectPtr):
+def has_level_handler(logger):
+    '''Check if there is a handler in the logging chain that will handle the
+    given logger's :meth:`effective level <~logging.Logger.getEffectiveLevel>`.
     '''
-    Class wrapping a gdb.Value that's a PyCFunctionObject*
-    (see Include/methodobject.h and Objects/methodobject.c)
-    '''
-    _typename = 'PyCFunctionObject'
+    level = logger.getEffectiveLevel()
+    current = logger
     
-            self.assertRaises(TypeError, complex, '1', '1')
-        self.assertRaises(TypeError, complex, 1, '1')
     
-            self.assertRaises(TypeError, Array.from_buffer_copy, b'123')
-        self.assertRaises(TypeError, Structure.from_buffer_copy, b'123')
-        self.assertRaises(TypeError, Union.from_buffer_copy, b'123')
-        self.assertRaises(TypeError, _CFuncPtr.from_buffer_copy, b'123')
-        self.assertRaises(TypeError, _Pointer.from_buffer_copy, b'123')
-        self.assertRaises(TypeError, _SimpleCData.from_buffer_copy, b'123')
+def main():
+    os.chdir(os.path.join(os.path.dirname(__file__), '..'))
     
-        def test_4(self):
-        class X(Structure):
-            pass
-        class Y(X):
-            pass
-        self.assertRaises(AttributeError, setattr, X, '_fields_', [])
-        Y._fields_ = []
-        self.assertRaises(AttributeError, setattr, X, '_fields_', [])
+    
+def test_appgroup(runner):
+    '''Test of with_appcontext.'''
+    
+    import flask
+    
+        data_train = fetch_20newsgroups_vectorized(subset='train')
+    data_test = fetch_20newsgroups_vectorized(subset='test')
+    X_train = check_array(data_train.data, dtype=np.float32,
+                          accept_sparse='csc')
+    X_test = check_array(data_test.data, dtype=np.float32, accept_sparse='csr')
+    y_train = data_train.target
+    y_test = data_test.target
+    
+    fixed_batch_size_comparison(X)
+variable_batch_size_comparison(X)
+plt.show()
+
+    
+    from sklearn.cluster import AgglomerativeClustering
+    
+    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
+TRAIN_FOLDER = '20news-bydate-train'
+TEST_FOLDER = '20news-bydate-test'
+    
+        Both random labelings have the same number of clusters for each value
+    possible value in ``n_clusters_range``.
+    
+    plt.axis('tight')
+plt.axis('off')
+plt.suptitle('Ground truth', size=20)
+    
+        def test_repr(self):
+        self.assertEqual(repr(self.addr2), 'certbot_apache.obj.Addr(('127.0.0.1', '443'))')
+    
+    
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/', None),
+    'acme': ('https://acme-python.readthedocs.org/en/latest/', None),
+    'certbot': ('https://certbot.eff.org/docs/', None),
+    'certbot-apache': (
+        'https://letsencrypt-apache.readthedocs.org/en/latest/', None),
+    'certbot-nginx': (
+        'https://letsencrypt-nginx.readthedocs.org/en/latest/', None),
+}
+
+    
+    # The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
