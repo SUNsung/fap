213570dba901f910ff299311a7860497b16caf25
SUNsung <weatherius@gmail.com>
2018-04-27 03:27:51 +0300

    Rewrite [331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py] File in: 2018-04-27 03:27:50

--- pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
+++ pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
@@ -1,194 +1,56 @@
 
         
-          words, _ = list(zip(*count_pairs))
-  word_to_id = dict(zip(words, range(len(words))))
-    
-    
-                                     predictions
-                                          ^
-                                          |
-                                       logits
-                                          ^
-                                          |
-                                (fully connected layers)
-                                          ^
-                                          |
-                                   pre_logits_concat
-                                          ^
-                                          |
-                                    (concatenate)
-    
-      Returns:
-    labels: An int64 tf.Placeholder with shape [batch_size].
-  '''
-  batch_size = None  # Batch size will be dynamically specified.
-  return tf.placeholder(dtype=tf.int64, shape=[batch_size], name='labels')
+        with open('update/versions.json', 'w') as jsonf:
+    json.dump(versions_info, jsonf, indent=4, sort_keys=True)
 
     
-    '''Utility functions for configurations.'''
+        infile, outfile = args
     
+    README_FILE = 'README.md'
+helptext = sys.stdin.read()
     
-if __name__ == '__main__':
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(argv=[sys.argv[0]] + unparsed)
-
+        def tearDown(self):
+        if os.path.exists(self.test_dir):
+            shutil.rmtree(self.test_dir)
     
-    Note that these steps are colloquially referred to as 'ResNet preprocessing,'
-and they differ from 'VGG preprocessing,' which does not use bounding boxes
-and instead does an aspect-preserving resize followed by random crop during
-training. (These both differ from 'Inception preprocessing,' which introduces
-color distortion steps.)
+        def test_secondary_proxy_https(self):
+        params = self._check_params(['secondary_proxy', 'secondary_server_ip'])
+        if params is None:
+            return
+        ydl = FakeYDL()
+        req = compat_urllib_request.Request('https://yt-dl.org/ip')
+        req.add_header('Ytdl-request-proxy', params['secondary_proxy'])
+        self.assertEqual(
+            ydl.urlopen(req).read().decode('utf-8'),
+            params['secondary_server_ip'])
     
-      Args:
-    pairs: the word pairs (list of tuple of two strings).
-    labels: the gold-standard labels for these pairs (array of rel ID).
-    predictions: the predicted labels for these pairs (array of rel ID).
-    classes: a list of relation names.
-    predictions_file: where to save the predictions.
-  '''
-  with open(predictions_file, 'w') as f_out:
-    for pair, label, pred in zip(pairs, labels, predictions):
-      w1, w2 = pair
-      f_out.write('\t'.join([w1, w2, classes[label], classes[pred]]) + '\n')
-
+                    if ''' not in code and ''' not in code:
+                    continue
+                assertRegexpMatches(
+                    self,
+                    code,
+                    r'(?:(?:#.*?|\s*)\n)*from __future__ import (?:[a-z_]+,\s*)*unicode_literals',
+                    'unicode_literals import  missing in %s' % fn)
     
+            # TODO: handle s and e stage_mode (live streams and ended live streams)
+        if stage_mode not in ('s', 'e'):
+            request = sanitized_Request(
+                'https://audimedia.tv/api/video/v1/videos/%s?embed[]=video_versions&embed[]=thumbnail_image&where[content_language_iso]=%s' % (video_id, lang),
+                headers={'X-Auth-Token': self._AUTH_TOKEN})
+            json_data = self._download_json(request, video_id)['results']
+            formats = []
     
-def _get_file_names():
-  '''Returns the file names expected to exist in the input_dir.'''
-  file_names = {}
-  file_names['train'] = ['data_batch_%d' % i for i in xrange(1, 5)]
-  file_names['validation'] = ['data_batch_5']
-  file_names['eval'] = ['test_batch']
-  return file_names
+            # Video URL construction algorithm is reverse-engineered from cwhplayer.swf
+        rtmp_url = 'rtmp://camwithher.tv/clipshare/%s' % (
+            ('mp4:%s.mp4' % flv_id) if int(flv_id) > 2010 else flv_id)
     
-        # This matches an IPv6 address, but also permits range expressions.
-    #
-    # This expression looks complex, but it really only spells out the various
-    # combinations in which the basic unit of an IPv6 address (0..ffff) can be
-    # written, from :: to 1:2:3:4:5:6:7:8, plus the IPv4-in-IPv6 variants such
-    # as ::ffff:192.0.2.3.
-    #
-    # Note that we can't just use ipaddress.ip_address() because we also have to
-    # accept ranges in place of each component.
+            title = self._search_regex(
+            r'<title>(.+?)\s*\|\s*.+?</title>', webpage, 'video title')
     
-    try:
-    import json
-except ImportError:
-    import simplejson as json
+        def mapper(self, _, line):
+        yield line, 1
     
-    from ansible.plugins.terminal import TerminalBase
-from ansible.errors import AnsibleConnectionFailure
+        def reducer_identity(self, key, value):
+        yield key, value
     
-    import re
-    
-        plt.figure('scikit-learn LASSO benchmark results')
-    plt.subplot(211)
-    plt.plot(list_n_samples, lasso_results, 'b-',
-                            label='Lasso')
-    plt.plot(list_n_samples, lars_lasso_results, 'r-',
-                            label='LassoLars')
-    plt.title('precomputed Gram matrix, %d features, alpha=%s' % (n_features,
-                            alpha))
-    plt.legend(loc='upper left')
-    plt.xlabel('number of samples')
-    plt.ylabel('Time (s)')
-    plt.axis('tight')
-    
-    ratio = scikits_time / scipy_time
-    
-        op.add_option('--density',
-                  dest='density', default=1 / 3,
-                  help='Density used by the sparse random projection.'
-                       ' ('auto' or float (0.0, 1.0]')
-    
-        plt.show()
-
-    
-    
-if not os.path.exists(html_folder):
-    os.makedirs(html_folder)
-    
-    import os
-import tarfile
-from contextlib import closing
-try:
-    from urllib import urlopen
-except ImportError:
-    from urllib.request import urlopen
-    
-    
-if __name__ == '__main__':
-    # NOTE: we put the following in a 'if __name__ == '__main__'' protected
-    # block to be able to use a multi-core grid search that also works under
-    # Windows, see: http://docs.python.org/library/multiprocessing.html#windows
-    # The multiprocessing module is used as the backend of joblib.Parallel
-    # that is used when n_jobs != 1 in GridSearchCV
-    
-        # TASK: Build a vectorizer / classifier pipeline that filters out tokens
-    # that are too rare or too frequent
-    pipeline = Pipeline([
-        ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
-        ('clf', LinearSVC(C=1000)),
-    ])
-    
-    # Author: Virgile Fritsch <virgile.fritsch@inria.fr>
-# License: BSD 3 clause
-    
-        def remove_surface(self):
-        '''Remove old decision surface.'''
-        if len(self.contours) > 0:
-            for contour in self.contours:
-                if isinstance(contour, ContourSet):
-                    for lineset in contour.collections:
-                        lineset.remove()
-                else:
-                    contour.remove()
-            self.contours = []
-    
-    The data is generated with the ``make_checkerboard`` function, then
-shuffled and passed to the Spectral Biclustering algorithm. The rows
-and columns of the shuffled matrix are rearranged to show the
-biclusters found by the algorithm.
-    
-        fig = plt.figure(fig_index, figsize=(10, 10))
-    ax1 = plt.subplot2grid((3, 1), (0, 0), rowspan=2)
-    ax2 = plt.subplot2grid((3, 1), (2, 0))
-    
-    
-n_train = 20  # samples for training
-n_test = 200  # samples for testing
-n_averages = 50  # how often to repeat classification
-n_features_max = 75  # maximum number of features
-step = 4  # step size for the calculation
-    
-        def _scan_once(string, idx):
-        try:
-            nextchar = string[idx]
-        except IndexError:
-            raise StopIteration(idx) from None
-    
-        errorHandler = None
-    filter = None
-    
-        print('# The following line assumes you have built Python using the standard instructions')
-    print('# Otherwise fix the following line to point to the library.')
-    print('pythonlib = '$(pythonhome)/pcbuild/python%s$(debug_suffix).lib'' % version_suffix)
-    print()
-    
-        def test_4(self):
-        class X(Structure):
-            pass
-        class Y(X):
-            pass
-        self.assertRaises(AttributeError, setattr, X, '_fields_', [])
-        Y._fields_ = []
-        self.assertRaises(AttributeError, setattr, X, '_fields_', [])
-    
-        def test_fileclosed(self):
-        try:
-            f = open(support.TESTFN, 'w')
-            self.assertIs(f.closed, False)
-            f.close()
-            self.assertIs(f.closed, True)
-        finally:
-            os.remove(support.TESTFN)
+        By default this will get the strings from the blns.txt file
