4052f45497c79014e0a8445380f61c16b7f73da4
SUNsung <weatherius@gmail.com>
2018-04-27 06:04:30 +0300

    Rewrite [284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py] File in: 2018-04-27 06:04:29

--- pyton/284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py
+++ pyton/284ZtozcgaYU28Pgr.2ulvtcM287hsGdzlqqZc28Fw9.py
@@ -1,226 +1,95 @@
 
         
-            # Final checkpoint
-    if is_chief and global_step_val >= FLAGS.max_steps:
-      sv.saver.save(sess, sv.save_path, global_step=global_step)
+            def get_expiration_time(self, app, session):
+        '''A helper method that returns an expiration date for the session
+        or ``None`` if the session is linked to the browser session.  The
+        default implementation returns now + the permanent session
+        lifetime configured on the application.
+        '''
+        if session.permanent:
+            return datetime.utcnow() + app.permanent_session_lifetime
     
-    FLAGS = flags.FLAGS
+    import flask
     
-    from six.moves import StringIO
-    
-    # Predict the type of some Iris flowers.
-# Let's predict the examples in FILE_TEST, repeat only once.
-predict_results = classifier.predict(
-    input_fn=lambda: my_input_fn(FILE_TEST, 1))
-tf.logging.info('Prediction on test file')
-for prediction in predict_results:
-    # Will print the predicted class, i.e: 0, 1, or 2 if the prediction
-    # is Iris Setosa, Vericolor, Virginica, respectively.
-    tf.logging.info('...{}'.format(prediction['class_ids']))
-    
-    # The amazon reviews input file to use in either the RT or IMDB datasets.
-flags.DEFINE_string('amazon_unlabeled_input_file', '',
-                    'The unlabeled Amazon Reviews dataset input file. If set, '
-                    'the input file is used to augment RT and IMDB vocab.')
-    
-      return seq
+    # Add any paths that contain custom themes here, relative to this directory.
+#html_theme_path = []
     
     
-def _build_vocab_frequencies(seqs, vocab_ids):
-  vocab_freqs = defaultdict(int)
-  ids_to_words = dict([(i, word) for word, i in vocab_ids.iteritems()])
-  for seq in seqs:
-    for timestep in seq:
-      vocab_freqs[ids_to_words[timestep.token]] += 1
+def FormatDebugInfoResponse_Completer_ServerRunningWithoutHost_test():
+  response = deepcopy( GENERIC_RESPONSE )
+  response[ 'completer' ][ 'servers' ][ 0 ].update( {
+    'address': None,
+    'port': None
+  } )
+  assert_that(
+    FormatDebugInfoResponse( response ),
+    contains_string(
+      'Completer name completer debug information:\n'
+      '  Server name running\n'
+      '  Server name process ID: 12345\n'
+      '  Server name executable: /path/to/executable\n'
+      '  Server name logfiles:\n'
+      '    /path/to/stdout/logfile\n'
+      '    /path/to/stderr/logfile\n'
+      '  Server name key: value\n'
+      '  Key: value\n'
+    )
+  )
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+      For instance ('|' represents the cursor):
+    1. Buffer state: 'foo.|bar'
+    2. A completion candidate of 'zoobar' is shown and the user selects it.
+    3. Buffer state: 'foo.zoobar|bar' instead of 'foo.zoo|bar' which is what the
+    user wanted.
     
     
-def main(_):
-  '''Trains Language Model.'''
-  tf.logging.set_verbosity(tf.logging.INFO)
-  with tf.device(tf.train.replica_device_setter(FLAGS.ps_tasks)):
-    model = graphs.get_model()
-    train_op, loss, global_step = model.language_model_training()
-    train_utils.run_training(train_op, loss, global_step)
+def EndsWithPython_BadPaths_test():
+  not_python_paths = [
+    None,
+    '',
+    '/opt/local/bin/vim',
+    r'C:\Program Files\Vim\vim74\gvim.exe',
+    '/usr/bin/python2.5',
+    '/home/user/.pyenv/shims/python3.2',
+  ]
     
-    exports_files(['LICENSE'])
+        '''def test_object(queue):
+           queue_object = QueueObject(queue, True)
+           print('Inside func: {}'.format(queue_object.object))'''
     
-    exports_files(['LICENSE'])
+        def setUp(self):
+        self.tc1 = TC1()
+        self.tc2 = TC2()
+        self.tc3 = TC3()
+        self.average_result_tc1 = '###### In Test 1 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc2 = '###### In Test 2 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc3 = '###### In Test 3 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.runner = TestRunner()
+        self.out = StringIO()
+        self.saved_stdout = sys.stdout
+        sys.stdout = self.out
     
-            Because the user is not required to use a token with an index stored
-        in it, we must provide a means for two token objects themselves to
-        indicate the start/end location.  Most often this will just delegate
-        to the other toString(int,int).  This is also parallel with
-        the TreeNodeStream.toString(Object,Object).
-	'''
+        def undo(self):
+        self.rename(self.dest, self.src)
     
-    Coverage: 99%. The only thing not covered is inconsequential --
-testing skipping of suite when self.needwrapbutton is false.
-'''
-import unittest
-from test.support import requires
-from tkinter import Tk, Frame  ##, BooleanVar, StringVar
-from idlelib import searchengine as se
-from idlelib import searchbase as sdb
-from idlelib.idle_test.mock_idle import Func
-## from idlelib.idle_test.mock_tk import Var
-    
-        def browse_file(self):
-        filetypes = [
-            ('HTML Files', '*.htm *.html', 'TEXT'),
-            ('PDF Files', '*.pdf', 'TEXT'),
-            ('Windows Help Files', '*.chm'),
-            ('Text Files', '*.txt', 'TEXT'),
-            ('All Files', '*')]
-        path = self.pathvar.get()
-        if path:
-            dir, base = os.path.split(path)
-        else:
-            base = None
-            if platform[:3] == 'win':
-                dir = os.path.join(os.path.dirname(executable), 'Doc')
-                if not os.path.isdir(dir):
-                    dir = os.getcwd()
-            else:
-                dir = os.getcwd()
-        file = self.askfilename(filetypes, dir, base)
-        if file:
-            self.pathvar.set(file)
-    
-        ``mode`` assumes discrete data, and returns a single value. This is the
-    standard treatment of the mode as commonly taught in schools:
-    
-        def test_invalid_escape(self):
-        s = '['abc\\y']'
-        msg = 'escape'
-        self.assertRaisesRegex(self.JSONDecodeError, msg, self.loads, s)
-    
-        def remove_flag(self, flag):
-        '''Unset the given string flag(s) without changing others.'''
-        if 'Status' in self or 'X-Status' in self:
-            self.set_flags(''.join(set(self.get_flags()) - set(flag)))
-    
-        def parseURI(self, uri):
-        if self.entityResolver:
-            input = self.entityResolver.resolveEntity(None, uri)
-        else:
-            input = DOMEntityResolver().resolveEntity(None, uri)
-        return self.parse(input)
-    
-        tokens = {}
-    prev_val = None
-    for line in lines:
-        match = prog.match(line)
-        if match:
-            name, val = match.group(1, 2)
-            val = int(val)
-            tokens[val] = {'token': name}          # reverse so we can sort them...
-            prev_val = val
-        else:
-            comment_match = comment_regex.match(line)
-            if comment_match and prev_val is not None:
-                comment = comment_match.group(1)
-                tokens[prev_val]['comment'] = comment
-    keys = sorted(tokens.keys())
-    # load the output skeleton from the target:
-    try:
-        fp = open(outFileName)
-    except OSError as err:
-        sys.stderr.write('I/O error: %s\n' % str(err))
-        sys.exit(2)
-    with fp:
-        format = fp.read().split('\n')
-    try:
-        start = format.index('#--start constants--') + 1
-        end = format.index('#--end constants--')
-    except ValueError:
-        sys.stderr.write('target does not contain format markers')
-        sys.exit(3)
-    lines = []
-    for key in keys:
-        lines.append('%s = %d' % (tokens[key]['token'], key))
-        if 'comment' in tokens[key]:
-            lines.append('# %s' % tokens[key]['comment'])
-    format[start:end] = lines
-    try:
-        fp = open(outFileName, 'w')
-    except OSError as err:
-        sys.stderr.write('I/O error: %s\n' % str(err))
-        sys.exit(4)
-    with fp:
-        fp.write('\n'.join(format))
-    
-        # Wire up the pretty-printer
-    obj.pretty_printers.append(pretty_printer_lookup)
-    
-        @gen_test
-    def test_put_clears_timed_out_getters(self):
-        q = queues.Queue()
-        getters = [q.get(timedelta(seconds=0.01)) for _ in range(10)]
-        get = q.get()
-        q.get()
-        self.assertEqual(12, len(q._getters))
-        yield gen.sleep(0.02)
-        self.assertEqual(12, len(q._getters))
-        self.assertFalse(get.done())  # Final waiters still active.
-        q.put(0)  # put() clears the waiters.
-        self.assertEqual(1, len(q._getters))
-        self.assertEqual(0, (yield get))
-        for getter in getters:
-            self.assertRaises(TimeoutError, getter.result)
-    
-        def wake(self):
-        try:
-            self.writer.send(b'x')
-        except (IOError, socket.error, ValueError):
-            pass
-    
-    
-def main():
-    parse_command_line()
-    t = Timer(e1)
-    results = t.timeit(options.num) / options.num
-    print('engine: %0.3f ms per iteration' % (results * 1000))
-    t = Timer(c1)
-    results = t.timeit(options.num) / options.num
-    print('coroutine: %0.3f ms per iteration' % (results * 1000))
-    
-    from tornado.auth import TwitterMixin
-from tornado.escape import json_decode, json_encode
-from tornado.ioloop import IOLoop
-from tornado import gen
-from tornado.options import define, options, parse_command_line, parse_config_file
-from tornado.web import Application, RequestHandler, authenticated
-    
-    # Most of our tests depend on IOLoop, which is not usable on app engine.
-# Run the tests that work, and check that everything else is at least
-# importable (via tornado.test.import_test)
-TEST_MODULES = [
-    'tornado.httputil.doctests',
-    'tornado.iostream.doctests',
-    'tornado.util.doctests',
-    #'tornado.test.auth_test',
-    #'tornado.test.concurrent_test',
-    #'tornado.test.curl_httpclient_test',
-    'tornado.test.escape_test',
-    #'tornado.test.gen_test',
-    #'tornado.test.httpclient_test',
-    #'tornado.test.httpserver_test',
-    'tornado.test.httputil_test',
-    'tornado.test.import_test',
-    #'tornado.test.ioloop_test',
-    #'tornado.test.iostream_test',
-    'tornado.test.locale_test',
-    #'tornado.test.netutil_test',
-    #'tornado.test.log_test',
-    'tornado.test.options_test',
-    #'tornado.test.process_test',
-    #'tornado.test.simple_httpclient_test',
-    #'tornado.test.stack_context_test',
-    'tornado.test.template_test',
-    #'tornado.test.testing_test',
-    #'tornado.test.twisted_test',
-    'tornado.test.util_test',
-    #'tornado.test.web_test',
-    #'tornado.test.websocket_test',
-    #'tornado.test.wsgi_test',
-]
+    import random
+import time
