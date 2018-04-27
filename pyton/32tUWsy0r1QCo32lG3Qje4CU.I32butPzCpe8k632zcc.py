b861b8cc2d23884b5d2690baf32fc90b0da20677
SUNsung <weatherius@gmail.com>
2018-04-27 22:00:10 +0300

    Rewrite [32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py] File in: 2018-04-27 22:00:10

--- pyton/32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py
+++ pyton/32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py
@@ -1,84 +1,159 @@
 
         
-          if np.isnan(loss_val):
-    raise OverflowError('Loss is nan')
+            styles = {
+        # No corresponding class for the following:
+        #Text:                     '', # class:  ''
+        Whitespace:                'underline #f8f8f8',      # class: 'w'
+        Error:                     '#a40000 border:#ef2929', # class: 'err'
+        Other:                     '#000000',                # class 'x'
+    }
+    
+        def inner(*suffix):
+        return urljoin(httpbin_url, '/'.join(suffix))
+    
+                handler_result = self.handler(sock)
     
     
-if __name__ == '__main__':
-  tf.test.main()
+def to_native_string(string, encoding='ascii'):
+    '''Given a string object, regardless of type, returns a representation of
+    that string in the native string type, encoding and decoding where
+    necessary. This assumes ASCII unless told otherwise.
+    '''
+    if isinstance(string, builtin_str):
+        out = string
+    else:
+        if is_py2:
+            out = string.encode(encoding)
+        else:
+            out = string.decode(encoding)
+    
+        def test_HTTP_307_ALLOW_REDIRECT_POST(self, httpbin):
+        r = requests.post(httpbin('redirect-to'), data='test', params={'url': 'post', 'status_code': 307})
+        assert r.status_code == 200
+        assert r.history[0].status_code == 307
+        assert r.history[0].is_redirect
+        assert r.json()['data'] == 'test'
+    
+    
+def md5(filename):
+    if not _md5:
+        raise ValueError('MD5 not available.  Possibly running in FIPS mode')
+    return secure_hash(filename, _md5)
 
     
-      tf.logging.info('Eval metric values:')
-  summary = tf.summary.Summary()
-  for name, val in zip(metric_names, values):
-    summary.value.add(tag=name, simple_value=val)
-    tf.logging.info('%s = %.3f', name, val)
+            directory = os.path.dirname(path)
+    
+        try:
+        with open(path, 'r') as key_fd:
+            return key_fd.read().strip()
+    except IOError:
+        return None
+    
+    # Make coding more python3-ish
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+    
+    The runtime provides these Token implementations:
+    
+        Snippets without code (only comments) or containing lines starting with ??? should not yeld files,
+    but the counter for naming snippets should still increment.
+    '''
+    parser = argparse.ArgumentParser(description='Split md file into plain text and code blocks')
+    parser.add_argument('sourcefile',
+                        help='which file to read')
+    parser.add_argument('targetfile',
+                        help='where to put plain text')
+    parser.add_argument('codedir',
+                        help='where to put codeblocks')
+    args = parser.parse_args()
+    
+        item_ok = SectionName.entry_ok  # localize for test override
+    
+            # Handle comparison errors
+        d = deque(['a', 'b', BadCmp(), 'c'])
+        e = deque(d)
+        self.assertRaises(RuntimeError, d.remove, 'c')
+        for x, y in zip(d, e):
+            # verify that original order and values are retained.
+            self.assertTrue(x is y)
+    
+    class NNTPDataError(NNTPError):
+    '''Error in response data'''
+    pass
+    
+        def test_format(self):
+        # empty format string is same as str()
+        self.assertEqual(format(1+3j, ''), str(1+3j))
+        self.assertEqual(format(1.5+3.5j, ''), str(1.5+3.5j))
+        self.assertEqual(format(3j, ''), str(3j))
+        self.assertEqual(format(3.2j, ''), str(3.2j))
+        self.assertEqual(format(3+0j, ''), str(3+0j))
+        self.assertEqual(format(3.2+0j, ''), str(3.2+0j))
+    
+        def test_print(self):
+        try:
+            fo = open(support.TESTFN, 'w')
+            print(False, True, file=fo)
+            fo.close()
+            fo = open(support.TESTFN, 'r')
+            self.assertEqual(fo.read(), 'False True\n')
+        finally:
+            fo.close()
+            os.remove(support.TESTFN)
+    
+        def test_repr_large(self):
+        # Check the repr of large list objects
+        def check(n):
+            l = [0] * n
+            s = repr(l)
+            self.assertEqual(s,
+                '[' + ', '.join(['0'] * n) + ']')
+        check(10)       # check our checking code
+        check(1000000)
     
     
-def fill_vocab_from_doc(doc, vocab_freqs, doc_counts):
-  '''Fills vocabulary and doc counts with tokens from doc.
+class Controller(object):
     
-    Computational time:
-  2 days to train 100000 steps on 1 layer 1024 hidden units LSTM,
-  256 embeddings, 400 truncated BP, 256 minibatch and on single GPU (Pascal
-  Titan X, cuDNNv5).
-'''
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+        def test_cat_adapter_shall_make_noise(self):
+        cat = Cat()
+        cat_adapter = Adapter(cat, make_noise=cat.meow)
+        noise = cat_adapter.make_noise()
+        expected_noise = 'meow!'
+        self.assertEqual(noise, expected_noise)
     
-    filegroup(
-    name = 'all_files',
-    srcs = glob(
-        ['**/*'],
-        exclude = [
-            '**/METADATA',
-            '**/OWNERS',
-        ],
-    ),
-    visibility = ['//tensorflow:__subpackages__'],
-)
-
+        def test_c_observers_shall_be_detachable(cls):
+        cls.s.detach(cls.dec_obs)
+        # hex viewer shall be remaining if dec viewer is detached first
+        cls.assertEqual(isinstance(cls.s._observers[0], HexViewer), True)
+        cls.assertEqual(len(cls.s._observers), 1)
+        cls.s.detach(cls.hex_obs)
+        cls.assertEqual(len(cls.s._observers), 0)
     
-                gc.collect()
-            print('- benchmarking LassoLars')
-            clf = LassoLars(alpha=alpha, fit_intercept=False,
-                            normalize=False, precompute=precompute)
-            tstart = time()
-            clf.fit(X, Y)
-            lars_lasso_results.append(time() - tstart)
     
-                gc.collect()
-            print('benchmarking lars_path (with Gram):', end='')
-            sys.stdout.flush()
-            tstart = time()
-            G = np.dot(X.T, X)  # precomputed Gram matrix
-            Xy = np.dot(X.T, y)
-            lars_path(X, y, Xy=Xy, Gram=G, method='lasso')
-            delta = time() - tstart
-            print('%0.3fs' % delta)
-            results['lars_path (with Gram)'].append(delta)
+class Action(object):
     
-        #------------------------------------------------------------
-    # varying N
-    N_results_build = dict([(alg, np.zeros(len(Nrange)))
-                            for alg in algorithms])
-    N_results_query = dict([(alg, np.zeros(len(Nrange)))
-                            for alg in algorithms])
+        def __init__(self, deep, *targets):
+        self.deep = deep
+        self.targets = targets
+        self.commit()
     
-        for line in input_file:
-        linestrip = line.strip()
-        if len(linestrip) == 0:
-            in_exercise_region = False
-        elif linestrip.startswith('# TASK:'):
-            in_exercise_region = True
     
-    model = SpectralBiclustering(n_clusters=n_clusters, method='log',
-                             random_state=0)
-model.fit(data)
-score = consensus_score(model.biclusters_,
-                        (rows[:, row_idx], columns[:, col_idx]))
+# Example usage...
+def main():
+    data1 = Data('Data 1')
+    data2 = Data('Data 2')
+    view1 = DecimalViewer()
+    view2 = HexViewer()
+    data1.attach(view1)
+    data1.attach(view2)
+    data2.attach(view2)
+    data2.attach(view1)
     
-    iris = datasets.load_iris()
-X = iris.data[:, 0:2]  # we only take the first two features for visualization
-y = iris.target
+    print 'PLUGIN_I18N_PATHS := ' + ','.join(os.path.relpath(plugin.path)
+                                         for plugin in plugins
+                                         if plugin.needs_translation)
+    
+            try:
+            tmpfile = tempfile.NamedTemporaryFile(prefix='profile',
+                                                  dir=self.directory,
+                                                  delete=False)
