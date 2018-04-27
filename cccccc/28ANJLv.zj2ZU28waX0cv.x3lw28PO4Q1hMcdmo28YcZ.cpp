08bfcf705405a2db04c87ce7ae4ff8e03b23b56f
SUNsung <weatherius@gmail.com>
2018-04-27 12:13:00 +0300

    Rewrite [28ANJLv.zj2ZU28waX0cv.x3lw28PO4Q1hMcdmo28YcZ.cpp] File in: 2018-04-27 12:12:59

--- cccccc/28ANJLv.zj2ZU28waX0cv.x3lw28PO4Q1hMcdmo28YcZ.cpp
+++ cccccc/28ANJLv.zj2ZU28waX0cv.x3lw28PO4Q1hMcdmo28YcZ.cpp
@@ -1,199 +1,61 @@
 
         
-        
-    {  cached_entries_.Clear();
-  for (const auto& iter : entries_)
-    cached_entries_.Append(iter.second->CreateDeepCopy());
-  cache_needs_update_ = false;
+        // dims=[5, 4, 3, 2]->[5, 3, 2, 4]
+TEST_F(MatrixTest, RotatingTranspose_1_3) {
+  GENERIC_2D_ARRAY<int> m;
+  src_.RotatingTranspose(dims_, kNumDims_, 1, 3, &m);
+  m.ResizeNoInit(kInputSize_ / 4, 4);
+  // Verify that the result is:
+  // output tensor=[[[[0, 6, 12, 18][1, 7, 13, 19]]
+  //                 [[2, 8, 14, 20][3, 9, 15, 21]]
+  //                 [[4, 10, 16, 22][5, 11, 17, 23]]]
+  //                [[[24, 30, 36, 42]...
+  EXPECT_EQ(0, m(0, 0));
+  EXPECT_EQ(6, m(0, 1));
+  EXPECT_EQ(1, m(1, 0));
+  EXPECT_EQ(2, m(2, 0));
+  EXPECT_EQ(3, m(3, 0));
+  EXPECT_EQ(4, m(4, 0));
+  EXPECT_EQ(5, m(5, 0));
+  EXPECT_EQ(24, m(6, 0));
+  EXPECT_EQ(30, m(6, 1));
 }
     
-      // The |predicate| is used to determine whether to set preferences for a
-  // render process.
-  explicit RenderProcessPreferences(const Predicate& predicate);
-  virtual ~RenderProcessPreferences();
+    // Computes and returns the dot product of the n-vectors u and v.
+// Uses Intel SSE intrinsics to access the SIMD instruction set.
+double DotProductSSE(const double* u, const double* v, int n);
+// Computes and returns the dot product of the n-vectors u and v.
+// Uses Intel SSE intrinsics to access the SIMD instruction set.
+int32_t IntDotProductSSE(const int8_t* u, const int8_t* v, int n);
+    
+    class QRSequenceGenerator {
+ public:
+  // Object is initalized with the size of the output range.
+  explicit QRSequenceGenerator(int N) : N_(N), next_num_(0) {
+    num_bits_ = static_cast<int>(ceil(log(static_cast<double>(N)) / log(2.0)));
+  }
+    }
+    
+    /**
+ * Flushes pending writes. This method should not be called before invocation of
+ * on_stream_ready() method of the bidirectional_stream_callback.
+ * For each previously called bidirectional_stream_write()
+ * a corresponding on_write_completed() callback will be invoked when the buffer
+ * is sent.
+ */
+GRPC_SUPPORT_EXPORT
+void bidirectional_stream_flush(bidirectional_stream* stream);
+    
+    int main(int argc, char** argv) {
+  grpc::testing::InitTest(&argc, &argv, true);
+    }
+    
+    namespace grpc_node_generator {
+    }
     
      private:
-  // views::StatusIconLinux::Delegate:
-  void OnClick() override;
-  bool HasClickAction() override;
+  static Result Sample();
     
-    MenuModelAdapter::MenuModelAdapter(AtomMenuModel* menu_model)
-  : views::MenuModelAdapter(menu_model),
-    menu_model_(menu_model) {
-}
-    
-    
-    {}  // namespace atom
-    
-    #include 'test/cpp/qps/report.h'
-    
-    namespace grpc {
-namespace testing {
-    }
-    }
-    
-    
-    {  bool failed_ = false;
-  std::string db_;
-  size_t current_ = 0;
-  const std::string location_ = '\'location\':';
-  const std::string latitude_ = '\'latitude\':';
-  const std::string longitude_ = '\'longitude\':';
-  const std::string name_ = '\'name\':';
-};
-    
-    #endif  // GRPC_INTERNAL_COMPILER_NODE_GENERATOR_HELPERS_H
-
-    
-      bool PrintBetaServicer(const grpc_generator::Service* service,
-                         grpc_generator::Printer* out);
-  bool PrintBetaServerFactory(
-      const grpc::string& package_qualified_service_name,
-      const grpc_generator::Service* service, grpc_generator::Printer* out);
-  bool PrintBetaStub(const grpc_generator::Service* service,
-                     grpc_generator::Printer* out);
-  bool PrintBetaStubFactory(const grpc::string& package_qualified_service_name,
-                            const grpc_generator::Service* service,
-                            grpc_generator::Printer* out);
-    
-    
-    {
-    {}  // namespace testing
-}  // namespace grpc
-    
-    #include 'rocksdb/merge_operator.h'
-#include 'rocksdb/slice.h'
-    
-      /// Search the list for the (index)'th item (0-based) in (list:key)
-  /// A negative index indicates: 'from end-of-list'
-  /// If index is within range: return true, and return the value in *result.
-  /// If (index < -length OR index>=length), then index is out of range:
-  ///   return false (and *result is left unchanged)
-  /// May throw RedisListException
-  bool Index(const std::string& key, int32_t index,
-             std::string* result);
-    
-      // Drop reference count.
-  // If the refcount goes to zero return this memtable, otherwise return null.
-  // REQUIRES: external synchronization to prevent simultaneous
-  // operations on the same MemTable.
-  MemTable* Unref() {
-    --refs_;
-    assert(refs_ >= 0);
-    if (refs_ <= 0) {
-      return this;
-    }
-    return nullptr;
-  }
-    
-    // This class contains a fixed array of buckets, each
-// pointing to a skiplist (null if the bucket is empty).
-// bucket_count: number of fixed array buckets
-// skiplist_height: the max height of the skiplist
-// skiplist_branching_factor: probabilistic size ratio between adjacent
-//                            link lists in the skiplist
-extern MemTableRepFactory* NewHashSkipListRepFactory(
-    size_t bucket_count = 1000000, int32_t skiplist_height = 4,
-    int32_t skiplist_branching_factor = 4
-);
-    
-      int recompute_height = 0;
-  if (splice->height_ < max_height) {
-    // Either splice has never been used or max_height has grown since
-    // last use.  We could potentially fix it in the latter case, but
-    // that is tricky.
-    splice->prev_[max_height] = head_;
-    splice->next_[max_height] = nullptr;
-    splice->height_ = max_height;
-    recompute_height = max_height;
-  } else {
-    // Splice is a valid proper-height splice that brackets some
-    // key, but does it bracket this one?  We need to validate it and
-    // recompute a portion of the splice (levels 0..recompute_height-1)
-    // that is a superset of all levels that don't bracket the new key.
-    // Several choices are reasonable, because we have to balance the work
-    // saved against the extra comparisons required to validate the Splice.
-    //
-    // One strategy is just to recompute all of orig_splice_height if the
-    // bottom level isn't bracketing.  This pessimistically assumes that
-    // we will either get a perfect Splice hit (increasing sequential
-    // inserts) or have no locality.
-    //
-    // Another strategy is to walk up the Splice's levels until we find
-    // a level that brackets the key.  This strategy lets the Splice
-    // hint help for other cases: it turns insertion from O(log N) into
-    // O(log D), where D is the number of nodes in between the key that
-    // produced the Splice and the current insert (insertion is aided
-    // whether the new key is before or after the splice).  If you have
-    // a way of using a prefix of the key to map directly to the closest
-    // Splice out of O(sqrt(N)) Splices and we make it so that splices
-    // can also be used as hints during read, then we end up with Oshman's
-    // and Shavit's SkipTrie, which has O(log log N) lookup and insertion
-    // (compare to O(log N) for skip list).
-    //
-    // We control the pessimistic strategy with allow_partial_splice_fix.
-    // A good strategy is probably to be pessimistic for seq_splice_,
-    // optimistic if the caller actually went to the work of providing
-    // a Splice.
-    while (recompute_height < max_height) {
-      if (splice->prev_[recompute_height]->Next(recompute_height) !=
-          splice->next_[recompute_height]) {
-        // splice isn't tight at this level, there must have been some inserts
-        // to this
-        // location that didn't update the splice.  We might only be a little
-        // stale, but if
-        // the splice is very stale it would be O(N) to fix it.  We haven't used
-        // up any of
-        // our budget of comparisons, so always move up even if we are
-        // pessimistic about
-        // our chances of success.
-        ++recompute_height;
-      } else if (splice->prev_[recompute_height] != head_ &&
-                 !KeyIsAfterNode(key_decoded,
-                                 splice->prev_[recompute_height])) {
-        // key is from before splice
-        if (allow_partial_splice_fix) {
-          // skip all levels with the same node without more comparisons
-          Node* bad = splice->prev_[recompute_height];
-          while (splice->prev_[recompute_height] == bad) {
-            ++recompute_height;
-          }
-        } else {
-          // we're pessimistic, recompute everything
-          recompute_height = max_height;
-        }
-      } else if (KeyIsAfterNode(key_decoded,
-                                splice->next_[recompute_height])) {
-        // key is from after splice
-        if (allow_partial_splice_fix) {
-          Node* bad = splice->next_[recompute_height];
-          while (splice->next_[recompute_height] == bad) {
-            ++recompute_height;
-          }
-        } else {
-          recompute_height = max_height;
-        }
-      } else {
-        // this level brackets the key, we won!
-        break;
-      }
-    }
-  }
-  assert(recompute_height <= max_height);
-  if (recompute_height > 0) {
-    RecomputeSpliceLevels(key_decoded, splice, recompute_height);
-  }
-    
-      int operator()(const char* a, const char* b) const {
-    if (Decode(a) < Decode(b)) {
-      return -1;
-    } else if (Decode(a) > Decode(b)) {
-      return +1;
-    } else {
-      return 0;
-    }
-  }
-    
-    Status BlockFetcher::ReadBlockContents() {
-  block_size_ = static_cast<size_t>(handle_.size());
-    }
+      // Return the current qps (i.e query count divided by the time since this
+  // QpsGauge object created (or Reset() was called))
+  long Get();
