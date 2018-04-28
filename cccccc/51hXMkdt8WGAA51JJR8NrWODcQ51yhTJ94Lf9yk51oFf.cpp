af88df61c562be96a831d6ca8b890c5a17cf1546
SUNsung <weatherius@gmail.com>
2018-04-28 08:17:46 +0300

    Rewrite [51hXMkdt8WGAA51JJR8NrWODcQ51yhTJ94Lf9yk51oFf.cpp] File in: 2018-04-28 08:17:46

--- cccccc/51hXMkdt8WGAA51JJR8NrWODcQ51yhTJ94Lf9yk51oFf.cpp
+++ cccccc/51hXMkdt8WGAA51JJR8NrWODcQ51yhTJ94Lf9yk51oFf.cpp
@@ -1,214 +1,352 @@
 
         
-        namespace nwapi {
+        #ifndef ATOM_BROWSER_API_ATOM_API_GLOBAL_SHORTCUT_H_
+#define ATOM_BROWSER_API_ATOM_API_GLOBAL_SHORTCUT_H_
+    
+    #if defined(OS_POSIX)
+// The 'magic' file descriptor that the relauncher process' write side of the
+// pipe shows up on. Chosen to avoid conflicting with stdin, stdout, and
+// stderr.
+extern const int kRelauncherSyncFD;
+#endif
+    
+    #ifndef ATOM_BROWSER_UI_VIEWS_MENU_MODEL_ADAPTER_H_
+#define ATOM_BROWSER_UI_VIEWS_MENU_MODEL_ADAPTER_H_
+    
+    namespace printing {
     }
     
-    base::StringPiece GetStringResource(int resource_id) {
-  return ResourceBundle::GetSharedInstance().GetRawDataResource(resource_id);
+      // Method run on the UI thread to get the display information.
+  void FetchMonitorFromWidget();
+    
+        if (TargetStart == C + 1) {
+      // End of source string or the source string contains an ill-formed
+      // subsequence at the end.
+      return S.slice(0, C1Offset);
+    }
+    
+      for (auto &import : imports) {
+    ModuleDecl *mod = import.second;
+    if (mod->getNameStr() == SWIFT_ONONE_SUPPORT)
+      continue; // ignore the Onone support library.
+    if (mod->isSwiftShimsModule())
+      continue;
+    }
+    
+      const Decl *getDecl() const { return D; }
+    
+    class Strong final : public InlineContent,
+    private llvm::TrailingObjects<Strong, MarkupASTNode *> {
+  friend TrailingObjects;
+    }
+    
+      /// Indicates whether the diagnostics produced during compilation should be
+  /// checked against expected diagnostics, indicated by markers in the
+  /// input source file.
+  enum {
+    NoVerify,
+    Verify,
+    VerifyAndApplyFixes
+  } VerifyMode = NoVerify;
+    
+    #endif // SWIFT_INDEX_INDEXSYMBOL_H
+
+    
+    Substitution::Substitution(Type Replacement,
+                           ArrayRef<ProtocolConformanceRef> Conformance)
+  : Replacement(Replacement), Conformance(Conformance)
+{
+  // The replacement type must be materializable.
+  assert(Replacement->isMaterializable()
+         && 'cannot substitute with a non-materializable type');
 }
     
-    namespace blink {
-class WebFrame;
-class WebURLRequest;
-class WebView;
-}
+    #endif  // CONTENT_NW_SRC_API_BINDINGS_COMMON_H_
+
     
     
     {}  // namespace nwapi
     
-    void DispatcherHost::OnCallStaticMethodSync(
-    const std::string& type,
-    const std::string& method,
-    const base::ListValue& arguments,
-    base::ListValue* result) {
-  DLOG(INFO) << 'OnCallStaticMethodSync: '
-             << ' type:' << type
-             << ' method:' << method
-             << ' arguments:' << arguments;
-    }
-    
-    EventListener::EventListener(int id,
-  const base::WeakPtr<DispatcherHost>& dispatcher_host,
-  const base::DictionaryValue& option) : Base(id, dispatcher_host, option) {
-    }
-    
-    namespace nw {
-    }
-    
-    
-void MenuItem::UpdateKeys(GtkAccelGroup *gtk_accel_group){
-  this->gtk_accel_group = gtk_accel_group;
-  if (enable_shortcut && GTK_IS_ACCEL_GROUP(gtk_accel_group)){
-    gtk_widget_add_accelerator(
-      menu_item_,
-      'activate',
-      gtk_accel_group,
-      keyval,
-      modifiers_mask,
-      GTK_ACCEL_VISIBLE);
+    EventListener::~EventListener() {
+  for (std::map<int, BaseEvent*>::iterator i = listerners_.begin(); i != listerners_.end(); i++) {
+    delete i->second;
   }
-  if (submenu_ != NULL){
-    submenu_->UpdateKeys(gtk_accel_group);
-  }
-  return;
 }
     
-      // Ignore first non-switch arg if it's not a standalone package.
-  bool ignore_arg = !package->self_extract();
-  for (unsigned i = 1; i < argv.size(); ++i) {
-    if (ignore_arg && args.size() && argv[i] == args[0]) {
-      ignore_arg = false;
-      continue;
-    }
+    namespace nwapi {
     }
     
-      grpc::testing::RunQPS();
+      GdkRectangle screen_rect;
+  gdk_screen_get_monitor_geometry(screen, monitor, &screen_rect);
     
-    std::string GetDbFileContent(int argc, char** argv);
     
-    #endif  // GRPC_INTERNAL_COMPILER_NODE_GENERATOR_HELPERS_H
-
-    
-    int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    {  gfx::Image originImage;
+  nw::Package* package = nw::InitNWPackage();
+  if (nw::GetImage(package, base::FilePath::FromUTF8Unsafe(icon), &originImage)) {
+    const gfx::ImageSkia* originImageSkia = originImage.ToImageSkia();
+    gfx::ImageSkia resizedImageSkia = gfx::ImageSkiaOperations::CreateResizedImage(*originImageSkia,
+                                                                                   skia::ImageOperations::RESIZE_GOOD,
+                                                                                   gfx::Size(kIconWidth, kIconHeight));
+    icon_ = gfx::Image(resizedImageSkia);
+  }
 }
-
     
-    #include 'test/cpp/qps/histogram.h'
+          if (data.type == TYPE_PNG &&
+         !gfx::PNGCodec::EncodeBGRASkBitmap(bitmap, false, &encoded_image)) {
+        LOG(INFO) << 'NwClipboardGetSyncFunction::RunSync(' << nwapi::nw__clipboard::ToString(data.type) << ') failed when converting to PNG';
+        error_ = 'Failed to encode as PNG';
+        return false;
+      } else if (data.type == TYPE_JPEG &&
+                 !gfx::JPEGCodec::Encode(bitmap, kQuality, &encoded_image)) {
+        LOG(INFO) << 'NwClipboardGetSyncFunction::RunSync(' << nwapi::nw__clipboard::ToString(data.type) << ') failed when converting to JPEG';
+        error_ = 'Failed to encode as JPEG';
+        return false;
+      }
+    
+    class NwClipboardReadAvailableTypesFunction : public NWSyncExtensionFunction {
+ public:
+  NwClipboardReadAvailableTypesFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+    namespace extensions {
+    }
     
     
-    { private:
-  /*! \brief the underlying stream */
-  dmlc::Stream *stream_;
-  /*! \brief buffer to hold data */
-  std::string buffer_;
-  /*! \brief length of valid data in buffer */
-  size_t read_len_;
-  /*! \brief pointer in the buffer */
-  size_t read_ptr_;
+    {  DECLARE_EXTENSION_FUNCTION('nw.Obj.callObjectMethod', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwObjCallObjectMethodFunction);
 };
     
-      delete metric;
-  metric = xgboost::Metric::Create('map@-');
-  ASSERT_STREQ(metric->Name(), 'map@-');
-  EXPECT_NEAR(GetMetricEval(metric, {}, {}), 0, 1e-10);
+    void leveldb_iter_next(leveldb_iterator_t* iter) {
+  iter->rep->Next();
+}
     
-      XGBOOST_DEVICE GradientPairInternal<T> &operator+=(
-      const GradientPairInternal<T> &rhs) {
-    grad_ += rhs.grad_;
-    hess_ += rhs.hess_;
-    return *this;
+    #ifndef STORAGE_LEVELDB_DB_DB_ITER_H_
+#define STORAGE_LEVELDB_DB_DB_ITER_H_
+    
+    void AppendInternalKey(std::string* result, const ParsedInternalKey& key) {
+  result->append(key.user_key.data(), key.user_key.size());
+  PutFixed64(result, PackSequenceAndType(key.sequence, key.type));
+}
+    
+      // Return an internal key (suitable for passing to an internal iterator)
+  Slice internal_key() const { return Slice(kstart_, end_ - kstart_); }
+    
+    // Called on every log record (each one of which is a WriteBatch)
+// found in a kDescriptorFile.
+static void VersionEditPrinter(uint64_t pos, Slice record, WritableFile* dst) {
+  std::string r = '--- offset ';
+  AppendNumberTo(&r, pos);
+  r += '; ';
+  VersionEdit edit;
+  Status s = edit.DecodeFrom(record);
+  if (!s.ok()) {
+    r += s.ToString();
+    r.push_back('\n');
+  } else {
+    r += edit.DebugString();
+  }
+  dst->Append(r);
+}
+    
+    // A utility routine: write 'data' to the named file and Sync() it.
+extern Status WriteStringToFileSync(Env* env, const Slice& data,
+                                    const std::string& fname);
+    
+          case kPrevLogNumber:
+        if (GetVarint64(&input, &prev_log_number_)) {
+          has_prev_log_number_ = true;
+        } else {
+          msg = 'previous log number';
+        }
+        break;
+    
+      void SetComparatorName(const Slice& name) {
+    has_comparator_ = true;
+    comparator_ = name.ToString();
+  }
+  void SetLogNumber(uint64_t num) {
+    has_log_number_ = true;
+    log_number_ = num;
+  }
+  void SetPrevLogNumber(uint64_t num) {
+    has_prev_log_number_ = true;
+    prev_log_number_ = num;
+  }
+  void SetNextFile(uint64_t num) {
+    has_next_file_number_ = true;
+    next_file_number_ = num;
+  }
+  void SetLastSequence(SequenceNumber seq) {
+    has_last_sequence_ = true;
+    last_sequence_ = seq;
+  }
+  void SetCompactPointer(int level, const InternalKey& key) {
+    compact_pointers_.push_back(std::make_pair(level, key));
   }
     
-      virtual void InitColAccess(const std::vector<bool>& enabled,
-                             float subsample,
-                             size_t max_row_perbatch, bool sorted) = 0;
-  // the following are column meta data, should be able to answer them fast.
-  /*! \return whether column access is enabled */
-  virtual bool HaveColAccess(bool sorted) const = 0;
-  /*! \return Whether the data columns single column block. */
-  virtual bool SingleColBlock() const = 0;
-  /*! \brief get number of non-missing entries in column */
-  virtual size_t GetColSize(size_t cidx) const = 0;
-  /*! \brief get column density */
-  virtual float GetColDensity(size_t cidx) const = 0;
-  /*! \return reference of buffered rowset, in column access */
-  virtual const RowSet& BufferedRowset() const = 0;
-  /*! \brief virtual destructor */
-  virtual ~DMatrix() = default;
-  /*!
-   * \brief Save DMatrix to local file.
-   *  The saved file only works for non-sharded dataset(single machine training).
-   *  This API is deprecated and dis-encouraged to use.
-   * \param fname The file name to be saved.
-   * \return The created DMatrix.
-   */
-  virtual void SaveToLocalFile(const std::string& fname);
-  /*!
-   * \brief Load DMatrix from URI.
-   * \param uri The URI of input.
-   * \param silent Whether print information during loading.
-   * \param load_row_split Flag to read in part of rows, divided among the workers in distributed mode.
-   * \param file_format The format type of the file, used for dmlc::Parser::Create.
-   *   By default 'auto' will be able to load in both local binary file.
-   * \return The created DMatrix.
-   */
-  static DMatrix* Load(const std::string& uri,
-                       bool silent,
-                       bool load_row_split,
-                       const std::string& file_format = 'auto');
-  /*!
-   * \brief create a new DMatrix, by wrapping a row_iterator, and meta info.
-   * \param source The source iterator of the data, the create function takes ownership of the source.
-   * \param cache_prefix The path to prefix of temporary cache file of the DMatrix when used in external memory mode.
-   *     This can be nullptr for common cases, and in-memory mode will be used.
-   * \return a Created DMatrix.
-   */
-  static DMatrix* Create(std::unique_ptr<DataSource>&& source,
-                         const std::string& cache_prefix = '');
-  /*!
-   * \brief Create a DMatrix by loading data from parser.
-   *  Parser can later be deleted after the DMatrix i created.
-   * \param parser The input data parser
-   * \param cache_prefix The path to prefix of temporary cache file of the DMatrix when used in external memory mode.
-   *     This can be nullptr for common cases, and in-memory mode will be used.
-   * \sa dmlc::Parser
-   * \note dmlc-core provides efficient distributed data parser for libsvm format.
-   *  User can create and register customized parser to load their own format using DMLC_REGISTER_DATA_PARSER.
-   *  See 'dmlc-core/include/dmlc/data.h' for detail.
-   * \return A created DMatrix.
-   */
-  static DMatrix* Create(dmlc::Parser<uint32_t>* parser,
-                         const std::string& cache_prefix = '');
-    
-    /*!
- * \brief Registry entry for tree updater.
- */
-struct GradientBoosterReg
-    : public dmlc::FunctionRegEntryBase<
-  GradientBoosterReg,
-  std::function<GradientBooster* (const std::vector<std::shared_ptr<DMatrix> > &cached_mats,
-                                  bst_float base_margin)> > {
-};
-    
-    
-    {    void throw_exception( std::exception const & e ) {
-        xfatal2(TSF'boost exception:%_', e.what());
-        
-#ifdef ANDROID
-        char stack[4096] = {0};
-        android_callstack(stack, sizeof(stack));
-        xfatal2(TSF'%_', stack);
-#endif
+    namespace leveldb {
     }
-}
-
     
-    
-/*
- * ServiceBase.h
- *
- *  Created on: 2013-6-20
- *      Author: yerungui
- */
-    
-    
-    {  private:
-    void* m_this;
-    std::map<const std::string, boost::any> m_variablemap;
-};
-    
-    
-    {    SPY_DETACH_VARIABLE('TestFun0 i');
-}
-    
-      private:
-    int __TestFun1(int i);
-    
-    //
-//  testspy_spy.cpp
-//  PublicComponent
+    // Dump the contents of the file named by fname in text format to
+// *dst.  Makes a sequence of dst->Append() calls; each call is passed
+// the newline-terminated text corresponding to a single item found
+// in the file.
 //
-//  Created by yerungui on 14-5-14.
-//
+// Returns a non-OK result if fname does not name a leveldb storage
+// file, or if the file cannot be read.
+Status DumpFile(Env* env, const std::string& fname, WritableFile* dst);
+    
+    inline int Slice::compare(const Slice& b) const {
+  const size_t min_len = (size_ < b.size_) ? size_ : b.size_;
+  int r = memcmp(data_, b.data_, min_len);
+  if (r == 0) {
+    if (size_ < b.size_) r = -1;
+    else if (size_ > b.size_) r = +1;
+  }
+  return r;
+}
+    
+    #include <osquery/core.h>
+#include <osquery/database.h>
+#include <osquery/filesystem.h>
+#include <osquery/logger.h>
+#include <osquery/tables.h>
+    
+      /**
+   * @brief Inspect the number of events, expire those overflowing events_max.
+   *
+   * When the event manager starts, or after a checkpoint number of events,
+   * the EventFactory will call expireCheck for each subscriber.
+   *
+   * The subscriber must count the number of buffered records and check if
+   * that count exceeds the configured `events_max` limit. If an overflow
+   * occurs the subscriber will expire N-events_max from the end of the queue.
+   */
+  void expireCheck();
+    
+      /// The group this instance dropped privileges to.
+  gid_t to_group_{0};
+    
+    
+    {  auto tarPath = carveFSPath + '/' + kTestCarveNamePrefix + guid_ + '.tar';
+  PlatformFile tar(tarPath, PF_OPEN_EXISTING | PF_READ);
+  EXPECT_TRUE(tar.isValid());
+  EXPECT_GT(tar.size(), 0U);
+}
+    
+    namespace osquery {
+    }
+    
+    
+    {      multi_pack.put_child(fs::path(path).stem().string(), single_pack);
+    }
+    
+    boost::optional<std::string> getEnvVar(const std::string& name) {
+  char* value = ::getenv(name.c_str());
+  if (value) {
+    return std::string(value);
+  }
+  return boost::none;
+}
+    
+    
+    {  // Now that the dropper is gone, the effective user/group should be restored.
+  EXPECT_EQ(geteuid(), getuid());
+  EXPECT_EQ(getegid(), getgid());
+}
+    
+    XGBOOST_REGISTER_SPARSE_PAGE_FORMAT(lz4i16hc)
+.describe('Apply LZ4 binary data compression(16 bit index mode) for ext memory.')
+.set_body([]() {
+    return new SparsePageLZ4Format<uint16_t>(true);
+  });
+    
+    namespace xgboost {
+namespace common {
+/*! \brief buffer reader of the stream that allows you to get */
+class StreamBufferReader {
+ public:
+  explicit StreamBufferReader(size_t buffer_size)
+      :stream_(NULL),
+       read_len_(1), read_ptr_(1) {
+    buffer_.resize(buffer_size);
+  }
+  /*!
+   * \brief set input stream
+   */
+  inline void set_stream(dmlc::Stream *stream) {
+    stream_ = stream;
+    read_len_ = read_ptr_ = 1;
+  }
+  /*!
+   * \brief allows quick read using get char
+   */
+  inline char GetChar(void) {
+    while (true) {
+      if (read_ptr_ < read_len_) {
+        return buffer_[read_ptr_++];
+      } else {
+        read_len_ = stream_->Read(&buffer_[0], buffer_.length());
+        if (read_len_ == 0) return EOF;
+        read_ptr_ = 0;
+      }
+    }
+  }
+  /*! \brief whether we are reaching the end of file */
+  inline bool AtEnd(void) const {
+    return read_len_ == 0;
+  }
+    }
+    }
+    }
+    
+    
+    {    for (size_t i = 0; i < batch.size; ++i) {
+      offset_[i + 1] = (i + 1) * num_col_;
+      Row<IndexType> row = batch[i];
+      for (uint32_t j = 0; j < num_col_; ++j) {
+        dense_index_[i * num_col_ + j] = j;
+      }
+      for (unsigned k = 0; k < row.length; ++k) {
+        uint32_t index = row.get_index(k);
+        CHECK_LT(index, num_col_)
+            << 'Featuere index larger than num_col';
+        dense_value_[i * num_col_ + index]  = row.get_value(k);
+      }
+    }
+    out_ = batch;
+    out_.index = dmlc::BeginPtr(dense_index_);
+    out_.value = dmlc::BeginPtr(dense_value_);
+    out_.offset = dmlc::BeginPtr(offset_);
+    return true;
+  }
+    
+    TEST(Metric, MAE) {
+  xgboost::Metric * metric = xgboost::Metric::Create('mae');
+  ASSERT_STREQ(metric->Name(), 'mae');
+  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 0, 1e-10);
+  EXPECT_NEAR(GetMetricEval(metric,
+                            {0.1f, 0.9f, 0.1f, 0.9f},
+                            {  0,   0,   1,   1}),
+              0.5f, 0.001f);
+}
+    
+    SEXP XGDMatrixGetInfo_R(SEXP handle, SEXP field) {
+  SEXP ret;
+  R_API_BEGIN();
+  bst_ulong olen;
+  const float *res;
+  CHECK_CALL(XGDMatrixGetFloatInfo(R_ExternalPtrAddr(handle),
+                                   CHAR(asChar(field)),
+                                 &olen,
+                                 &res));
+  ret = PROTECT(allocVector(REALSXP, olen));
+  for (size_t i = 0; i < olen; ++i) {
+    REAL(ret)[i] = res[i];
+  }
+  R_API_END();
+  UNPROTECT(1);
+  return ret;
+}
