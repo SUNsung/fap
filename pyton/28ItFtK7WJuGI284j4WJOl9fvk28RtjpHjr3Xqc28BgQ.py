941be89369a3a8773e2672ae40b4cb7f30ffbbec
SUNsung <weatherius@gmail.com>
2018-04-28 11:13:07 +0300

    Rewrite [28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py] File in: 2018-04-28 11:13:06

--- pyton/28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py
+++ pyton/28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py
@@ -1,82 +1,48 @@
 
         
-            # Are checkable fields missing from the test case definition?
-    test_info_dict = dict((key, value if not isinstance(value, compat_str) or len(value) < 250 else 'md5:' + md5(value))
-                          for key, value in got_dict.items()
-                          if value and key in ('id', 'title', 'description', 'uploader', 'upload_date', 'timestamp', 'uploader_id', 'location', 'age_limit'))
-    missing_keys = set(test_info_dict.keys()) - set(expected_dict.keys())
-    if missing_keys:
-        def _repr(v):
-            if isinstance(v, compat_str):
-                return ''%s'' % v.replace('\\', '\\\\').replace(''', '\\'').replace('\n', '\\n')
-            else:
-                return repr(v)
-        info_dict_str = ''
-        if len(missing_keys) != len(expected_dict):
-            info_dict_str += ''.join(
-                '    %s: %s,\n' % (_repr(k), _repr(v))
-                for k, v in test_info_dict.items() if k not in missing_keys)
+                Number:                    '#990000',        # class: 'm'
     
-        def test_no_duplicated_ie_names(self):
-        name_accu = collections.defaultdict(list)
-        for ie in self.ies:
-            name_accu[ie.IE_NAME.lower()].append(type(ie).__name__)
-        for (ie_name, ie_list) in name_accu.items():
-            self.assertEqual(
-                len(ie_list), 1,
-                'Multiple extractors with the same IE_NAME '%s' (%s)' % (ie_name, ', '.join(ie_list)))
     
-            mvp_id = self._search_mvp_id(webpage)
-    
-            (2016-01, shopping), 25
-        (2016-01, shopping), 100
-        (2016-01, gas), 50
-        '''
-        timestamp, seller, amount = line.split('\t')
-        period = self. extract_year_month(timestamp)
-        if period == self.current_year_month():
-            yield (period, category), amount
-    
-        def __init__(self, cards):
-        self.cards = cards
-    
-        def process_query(self, query):
-        query = self.parse_query(query)
-        results = self.memory_cache.get(query)
-        if results is None:
-            results = self.reverse_index_cluster.process_search(query)
-            self.memory_cache.set(query, results)
-        return results
-    
-        def get_person(self, person_id):
-        person_server = self.lookup[person_id]
-        return person_server.people[person_id]
-    
-    def set_extension(environ, ext):
-    environ['extension'] = ext
-    environ['render_style'], environ['content_type'] = extension_mapping[ext]
+def dispatch_hook(key, hooks, hook_data, **kwargs):
+    '''Dispatches a hook dictionary on a given piece of data.'''
+    hooks = hooks or dict()
+    hooks = hooks.get(key)
+    if hooks:
+        if hasattr(hooks, '__call__'):
+            hooks = [hooks]
+        for hook in hooks:
+            _hook_data = hook(hook_data, **kwargs)
+            if _hook_data is not None:
+                hook_data = _hook_data
+    return hook_data
 
     
-    api('reltableitem', RelTableItemJsonTemplate)
-api('bannedtableitem', BannedTableItemJsonTemplate)
-api('mutedtableitem', MutedTableItemJsonTemplate)
-api('invitedmodtableitem', InvitedModTableItemJsonTemplate)
-api('friendtableitem', FriendTableItemJsonTemplate)
+            # Decode unicode from given encoding.
+        try:
+            content = str(self.content, encoding, errors='replace')
+        except (LookupError, TypeError):
+            # A LookupError is raised if the encoding was not found which could
+            # indicate a misspelling or similar mistake.
+            #
+            # A TypeError can be raised if encoding is None
+            #
+            # So we try blindly encoding.
+            content = str(self.content, errors='replace')
     
-        @require_oauth2_scope('read')
-    @validate(
-        user=VAccountByName('username'),
-    )
-    @api_doc(
-        section=api_section.users,
-        uri='/api/v1/user/{username}/trophies',
-    )
-    def GET_usertrophies(self, user):
-        '''Return a list of trophies for the a given user.'''
-        return self.api_wrapper(get_usertrophies(user))
+                assert r.status_code == 200
+            assert r.text == u'roflol'
+            assert r.headers['Content-Length'] == '6'
     
-            template = g.mako_lookup.get_template(template_name)
-        return template.render(
-            logo_url=static(g.default_header_url),
-            retry_after=retry_after,
-        )
+        def test_update(self):
+        cid = CaseInsensitiveDict()
+        cid['spam'] = 'blueval'
+        cid.update({'sPam': 'notblueval'})
+        assert cid['spam'] == 'notblueval'
+        cid = CaseInsensitiveDict({'Foo': 'foo', 'BAr': 'bar'})
+        cid.update({'fOO': 'anotherfoo', 'bAR': 'anotherbar'})
+        assert len(cid) == 2
+        assert cid['foo'] == 'anotherfoo'
+        assert cid['bar'] == 'anotherbar'
+    
+    
+if __name__ == '__main__':
