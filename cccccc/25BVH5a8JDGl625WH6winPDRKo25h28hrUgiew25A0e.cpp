014abeb388078b71fb823a9a870fee0c42341860
SUNsung <weatherius@gmail.com>
2018-04-28 09:31:05 +0300

    Rewrite [25BVH5a8JDGl625WH6winPDRKo25h28hrUgiew25A0e.cpp] File in: 2018-04-28 09:31:05

--- cccccc/25BVH5a8JDGl625WH6winPDRKo25h28hrUgiew25A0e.cpp
+++ cccccc/25BVH5a8JDGl625WH6winPDRKo25h28hrUgiew25A0e.cpp
@@ -1,3 +1,189 @@
 
         
-        
+                    // The following does m_netInputMatrixPtr = decimatedMatrices; with ownership shenanigans.
+            for (auto& x : decimatedMatrices)
+            {
+                const wstring& name = x.first;
+                m_netInputMatrixPtr.GetInputMatrix<ElemType>(name).SetValue(decimatedMatrices.GetInputMatrix<ElemType>(name));
+            }
+    
+        size_t m_prevChosenMinibatchSize;
+    double m_lastFinishedEpochTrainLoss;
+    
+    
+    {            assert(false);
+            return '';
+        }
+    
+        // Taken from ONNX
+    REGISTER_OPERATOR_SCHEMA(MaxPool)
+        .Description('MaxPool consumes an input tensor X and applies max pooling across the'
+            'the tensor according to kernel sizes, stride sizes, and pad lengths.'
+            'Average pooling consisting of averaging all values of a subset of the'
+            'input tensor according to the kernel size and downsampling the'
+            'data into the output tensor Y for further processing.')
+        .Input('X',
+            'Input data tensor from the previous operator; dimensions for image case '
+            'are (N x C x H x W), where N is the batch size, C is the number of channels, '
+            'and H and W are the height and the width of the data. For non image case, the '
+            'dimension are in the form of (N x D1 x D2 ... Dn), where N is the batch size.',
+            'T')
+        .Output('Y',
+            'Output data tensor from max pooling across the input tensor. '
+            'Dimensions will vary based on various kernel, stride, and pad sizes.',
+            'T')
+        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
+            'Constrain input and output types to float tensors.')
+        .Attr('auto_pad',
+            'auto_pad must be either SAME_UPPER, SAME_LOWER or VALID. Where SAME_UPPER '
+            'or SAME_LOWER mean pad the input so that the ouput size match the input. '
+            'In case of odd number add the extra padding at the end for SAME_UPPER and '
+            'at the begining for SAME_LOWER. VALID mean no padding, therefore, read the '
+            'pixel values from the pads attribute.',
+            AttrType::AttributeProto_AttributeType_STRING)
+        .Attr('kernel_shape',
+            'The size of the kernel along each axis.',
+            AttrType::AttributeProto_AttributeType_INTS)
+        .Attr('strides',
+            'Stride along each axis.',
+            AttrType::AttributeProto_AttributeType_INTS)
+        .Attr('pads',
+            'Padding along each axis, can take the value 0 (False) or non 0 (True)',
+            AttrType::AttributeProto_AttributeType_INTS)
+        .Attr('dilations',
+            'Dilaton along each axis, 1 mean no dilation.',
+            AttrType::AttributeProto_AttributeType_INTS);
+    
+                Equations:
+              - Ht = Activation(Wi*Xt + Ri*Ht-1 + Wbi + Rbi)
+            )DOC')
+        .FillUsing(RNNDocGeneratorInputX())
+        .Input('W',
+            'The weight tensor for input gate. Concatenation of `Wi` and `WBi` '
+            '(if bidirectional). The tensor has shape '
+            '`[num_directions, hidden_size, input_size]`.', 'T')
+        .Input('R',
+            'The recurrence weight tensor. Concatenation of `Ri` and `RBi` '
+            '(if bidirectional). The tensor has shape '
+            '`[num_directions, hidden_size, hidden_size]`.', 'T')
+        .Input('B',
+            'The bias tensor for input gate. Concatenation of `[Wbi, Rbi]` '
+            'and `[WBbi, RBbi]` (if bidirectional). The tensor has shape '
+            '`[num_directions, 2*hidden_size]`, Optional: If not specified - assumed '
+            'to be 0.', 'T',
+            true)
+        .FillUsing(RNNDocGeneratorInputSeqLen())
+        .FillUsing(RNNDocGeneratorInputInitialH())
+        .Attr('activations', 'One (or two if bidirectional) activation function for '
+            'input gate. It must be one of tanh and ReLU. Default `tanh`.',
+            AttrType::AttributeProto_AttributeType_STRINGS)
+        .FillUsing(RNNDocGeneratorActivationArgs())
+        .FillUsing(RNNDocGeneratorAttrOutput());
+    
+    
+    REGISTER_OPERATOR_SCHEMA(TreeEnsembleRegressor)
+        .SetDomain(c_mlDomain)
+        .Input('X', 'Input N,F', 'T')
+        .Output('Y', 'NxE floats', 'tensor(float)')
+        .Description(R'DOC(
+            Tree Ensemble regressor.  Returns the regressed values for each input in N.
+            All args with nodes_ are fields of a tuple of tree nodes, and
+            it is assumed they are the same length, and an index i will decode the
+            tuple across these inputs.  Each node id can appear only once
+            for each tree id.
+            All fields prefixed with target_ are tuples of votes at the leaves.
+            A leaf may have multiple votes, where each vote is weighted by
+            the associated target_weights index.
+            All trees must have their node ids start at 0 and increment by 1.
+            Mode enum is BRANCH_LEQ, BRANCH_LT, BRANCH_GTE, BRANCH_GT, BRANCH_EQ, BRANCH_NEQ, LEAF
+            )DOC')
+        .TypeConstraint('T', { 'tensor(float)', 'tensor(double)', 'tensor(int64)', 'tensor(int32)' }, ' allowed types.')
+        .Attr('nodes_treeids', 'tree id for this node', AttrType::AttributeProto_AttributeType_INTS)
+        .Attr('nodes_nodeids', 'node id for this node, node ids may restart at zero for each tree (but not required).', AttrType::AttributeProto_AttributeType_INTS)
+        .Attr('nodes_featureids', 'feature id for this node', AttrType::AttributeProto_AttributeType_INTS)
+        .Attr('nodes_values', 'thresholds to do the splitting on for this node.', AttrType::AttributeProto_AttributeType_FLOATS)
+        .Attr('nodes_hitrates', '', AttrType::AttributeProto_AttributeType_FLOATS)
+        .Attr('nodes_modes', 'enum of behavior for this node.  enum 'BRANCH_LEQ', 'BRANCH_LT', 'BRANCH_GTE', 'BRANCH_GT', 'BRANCH_EQ', 'BRANCH_NEQ', 'LEAF'', AttrType::AttributeProto_AttributeType_STRINGS)
+        .Attr('nodes_truenodeids', 'child node if expression is true', AttrType::AttributeProto_AttributeType_INTS)
+        .Attr('nodes_falsenodeids', 'child node if expression is false', AttrType::AttributeProto_AttributeType_INTS)
+        .Attr('nodes_missing_value_tracks_true', 'for each node, decide if the value is missing (nan) then use true branch, this field can be left unset and will assume false for all nodes', AttrType::AttributeProto_AttributeType_INTS)
+        .Attr('target_treeids', 'tree that this node is in', AttrType::AttributeProto_AttributeType_INTS)
+        .Attr('target_nodeids', 'node id that this weight is for', AttrType::AttributeProto_AttributeType_INTS)
+        .Attr('target_ids', 'index of the class list that this weight is for', AttrType::AttributeProto_AttributeType_INTS)
+        .Attr('target_weights', 'the weight for the class in target_id', AttrType::AttributeProto_AttributeType_FLOATS)
+        .Attr('n_targets', 'number of regression targets', AttrType::AttributeProto_AttributeType_INT)
+        .Attr('post_transform', 'post eval transform for score, enum 'NONE', 'SOFTMAX', 'LOGISTIC', 'SOFTMAX_ZERO', 'PROBIT'', AttrType::AttributeProto_AttributeType_STRING)
+        .Attr('aggregate_function', 'post eval transform for score,  enum 'AVERAGE', 'SUM', 'MIN', 'MAX'', AttrType::AttributeProto_AttributeType_STRING)
+        .Attr('base_values', 'base values for regression, added to final score, size must be the same as n_outputs or can be left unassigned (assumed 0)', AttrType::AttributeProto_AttributeType_FLOATS);
+    
+    
+    {
+    {        m_augmentationWindow.first = m_augmentationWindow.second = extent;
+    }
+}
+    
+    // https://github.com/onnx/onnx/blob/master/docs/Operators.md#inputs-3---6
+// size of weight/bias matrix is a multiple of hidden size
+enum
+{
+    GRUWeightDimensionHiddenMultiplier = 3,
+    GRUBiasDimensionHiddenMultiplier = 6
+};
+    
+    
+    {    // Note: keyword 'noexcept' was added so that stl vector first looks for
+    //       the move constructor instead of the private copy constructor.
+    ssematrixfrombuffer(ssematrixfrombuffer &&other) noexcept
+    {
+        std::move(other);
+    }
+};
+    
+    /// PrefetchGPUDataTransferer
+    
+    #endif // BOOST_ASIO_BUFFERED_STREAM_FWD_HPP
+
+    
+    template <typename ConstBufferSequence, typename Handler>
+class descriptor_write_op
+  : public descriptor_write_op_base<ConstBufferSequence>
+{
+public:
+  BOOST_ASIO_DEFINE_HANDLER_PTR(descriptor_write_op);
+    }
+    
+    #endif // !defined(BOOST_ASIO_WINDOWS_RUNTIME)
+    
+    
+    {
+    {
+    {} // namespace detail
+} // namespace asio
+} // namespace boost
+    
+    #define BOOST_ASIO_COMPOSED_CONNECT_HANDLER_CHECK( \
+    handler_type, handler, iter_type) \
+  typedef int BOOST_ASIO_UNUSED_TYPEDEF
+    
+      // Constructor.
+  hash_map()
+    : size_(0),
+      buckets_(0),
+      num_buckets_(0)
+  {
+  }
+    
+    void buffer_sequence_adapter_base::init_native_buffer(
+    buffer_sequence_adapter_base::native_buffer_type& buf,
+    const boost::asio::const_buffer& buffer)
+{
+  std::memset(&buf, 0, sizeof(native_buffer_type));
+  Microsoft::WRL::ComPtr<IInspectable> insp
+    = Microsoft::WRL::Make<winrt_buffer_impl>(buffer);
+  Platform::Object^ buf_obj = reinterpret_cast<Platform::Object^>(insp.Get());
+  buf = reinterpret_cast<Windows::Storage::Streams::IBuffer^>(insp.Get());
+}
+    
+    
+    {  return false;
+}
