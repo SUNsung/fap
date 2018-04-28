f959a1429b365f16b728b2332bc86b8d425dbb5d
SUNsung <weatherius@gmail.com>
2018-04-28 20:37:59 +0300

    Rewrite [18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp] File in: 2018-04-28 20:37:58

--- cccccc/18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp
+++ cccccc/18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp
@@ -1,323 +1,198 @@
 
         
-          /**
-   * @brief Applies the transformation defined in the data layer's
-   * transform_param block to a cv::Mat
-   *
-   * @param cv_img
-   *    cv::Mat containing the data to be transformed.
-   * @param transformed_blob
-   *    This is destination blob. It can be part of top blob's data if
-   *    set_cpu_data() is used. See image_data_layer.cpp for an example.
-   */
-  void Transform(const cv::Mat& cv_img, Blob<Dtype>* transformed_blob);
-#endif  // USE_OPENCV
+        typedef NS_ENUM(NSInteger, ObjectBehaviorAction) {
+    ObjectBehaviorActionRetain,
+    ObjectBehaviorActionCopy,
+    ObjectBehaviorActionMutableCopy
+};
     
-    
-template <typename Dtype>
-class LayerRegisterer {
- public:
-  LayerRegisterer(const string& type,
-                  shared_ptr<Layer<Dtype> > (*creator)(const LayerParameter&)) {
-    // LOG(INFO) << 'Registering layer type: ' << type;
-    LayerRegistry<Dtype>::AddCreator(type, creator);
+      unsigned getCodeCompletionBufferID() const {
+    return CodeCompletionBufferID;
   }
-};
-    
-    
-    {  /**
-   * @brief Computes the error gradient w.r.t. the BNLL inputs.
-   *
-   * @param top output Blob vector (length 1), providing the error gradient with
-   *      respect to the outputs
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
-   *      with respect to computed outputs @f$ y @f$
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$; Backward fills their diff with
-   *      gradients @f$
-   *        \frac{\partial E}{\partial x}
-   *      @f$ if propagate_down[0]
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-};
-    
-    #include 'caffe/layers/conv_layer.hpp'
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    #include 'caffe/layers/pooling_layer.hpp'
-    
-    
-    {  bool handles_setup_;
-  cudnnHandle_t             handle_;
-  cudnnTensorDescriptor_t bottom_desc_;
-  cudnnTensorDescriptor_t top_desc_;
-};
-#endif
-    
-    #include 'caffe/layers/base_conv_layer.hpp'
-    
-    namespace caffe {
-    }
-    
-    /**
- * @brief Exponential Linear Unit non-linearity @f$
- *        y = \left\{
- *        \begin{array}{lr}
- *            x                  & \mathrm{if} \; x > 0 \\
- *            \alpha (\exp(x)-1) & \mathrm{if} \; x \le 0
- *        \end{array} \right.
- *      @f$.  
- */
-template <typename Dtype>
-class ELULayer : public NeuronLayer<Dtype> {
- public:
-  /**
-   * @param param provides ELUParameter elu_param,
-   *     with ELULayer options:
-   *   - alpha (\b optional, default 1).
-   *     the value @f$ \alpha @f$ by which controls saturation for negative inputs.
-   */
-  explicit ELULayer(const LayerParameter& param)
-      : NeuronLayer<Dtype>(param) {}
-    }
-    }
-    
-    static int set_ifname(struct ifaddrs* ifaddr, int interface) {
-	char buf[IFNAMSIZ] = {0};
-	char* name = if_indextoname(interface, buf);
-	if (name == NULL) {
-		return -1;
-	}
-	ifaddr->ifa_name = new char[strlen(name) + 1];
-	strncpy(ifaddr->ifa_name, name, strlen(name) + 1);
-	return 0;
-}
-    
-    	if (certs_path != '') {
-    }
-    
-    	void _on_peer_packet();
-	void _on_connect(String p_protocol);
-	void _on_disconnect();
-	void _on_error();
     
     public:
-	virtual bool can_instance() const = 0;
-    
-    		real_t hitDistance = param * raylen;
-		wheel.m_raycastInfo.m_suspensionLength = hitDistance - wheel.m_wheelRadius;
-		//clamp on max suspension travel
-    
-    	for (int i = 0; i < anim_tree->node_get_input_count(p_node); i++) {
-    }
-    
-    
-    {
-    {		property.hint_string = options;
-	}
-}
-    
-    						copydata.clear();
-						selection_active = false;
-    
-    		Dictionary d = script->call('get_variable_info', var);
-    
-      /// Construct, passing the specified argument to initialise the next layer.
-  template <typename Arg>
-  explicit buffered_read_stream(Arg& a)
-    : next_layer_(a),
-      storage_(default_buffer_size)
-  {
+  /// The replacement type.
+  Type getReplacement() const { return Replacement; }
+  
+  /// The protocol conformances for the replacement. These appear in the same
+  /// order as Archetype->getConformsTo() for the substituted archetype.
+  const ArrayRef<ProtocolConformanceRef> getConformances() const {
+    return Conformance;
   }
+  
+  Substitution() {}
+  
+  Substitution(Type Replacement, ArrayRef<ProtocolConformanceRef> Conformance);
     
-    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
-# pragma once
-#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
-    
-    #include <boost/asio/detail/config.hpp>
-    
-    #if defined(BOOST_ASIO_HAS_STD_FUNCTION)
-using std::function;
-#else // defined(BOOST_ASIO_HAS_STD_FUNCTION)
-using boost::function;
-#endif // defined(BOOST_ASIO_HAS_STD_FUNCTION)
-    
-    template <typename Handler>
-inline void* allocate(std::size_t s, Handler& h)
-{
-#if !defined(BOOST_ASIO_HAS_HANDLER_HOOKS)
-  return ::operator new(s);
+    /// Runs the given function with the given context argument exactly once.
+/// The predicate argument must point to a global or static variable of static
+/// extent of type swift_once_t.
+void swift::swift_once(swift_once_t *predicate, void (*fn)(void *),
+                       void *context) {
+#if defined(__APPLE__)
+  dispatch_once_f(predicate, context, fn);
+#elif defined(__CYGWIN__)
+  _swift_once_f(predicate, context, fn);
 #else
-  using boost::asio::asio_handler_allocate;
-  return asio_handler_allocate(s, boost::asio::detail::addressof(h));
+  std::call_once(*predicate, [fn, context]() { fn(context); });
 #endif
 }
+
     
-    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
-# pragma once
-#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
-    
-        size_t bucket = calculate_hash_value(it->first) % num_buckets_;
-    bool is_first = (it == buckets_[bucket].first);
-    bool is_last = (it == buckets_[bucket].last);
-    if (is_first && is_last)
-      buckets_[bucket].first = buckets_[bucket].last = values_.end();
-    else if (is_first)
-      ++buckets_[bucket].first;
-    else if (is_last)
-      --buckets_[bucket].last;
-    
-      STDMETHODIMP put_Length(UINT32 value)
-  {
-    if (value > capacity_)
-      return E_INVALIDARG;
-    length_ = value;
-    return S_OK;
+      void OCRTester(const char* imgname, const char* groundtruth, const char* tessdatadir, const char* lang) {
+    //log.info() << tessdatadir << ' for language: ' << lang << std::endl;
+    char *outText;
+    std::locale loc('C'); // You can also use '' for the default system locale
+    std::ifstream file(groundtruth);
+    file.imbue(loc); // Use it for file input
+    std::string gtText((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+    ASSERT_FALSE(api->Init(tessdatadir, lang)) << 'Could not initialize tesseract.';
+    Pix *image = pixRead(imgname);
+    ASSERT_TRUE(image != nullptr) << 'Failed to read test image.';
+    api->SetImage(image);
+    outText = api->GetUTF8Text();
+    EXPECT_EQ(gtText,outText) << 'Phototest.tif OCR does not match ground truth for ' << ::testing::PrintToString(lang);
+    api->End();
+    delete [] outText;
+    pixDestroy(&image);
   }
     
-    bool js_cocos2dx_builder_CCBReader_constructor(JSContext *cx, uint32_t argc, jsval *vp);
-void js_cocos2dx_builder_CCBReader_finalize(JSContext *cx, JSObject *obj);
-void js_register_cocos2dx_builder_CCBReader(JSContext *cx, JS::HandleObject global);
-void register_all_cocos2dx_builder(JSContext* cx, JS::HandleObject obj);
-bool js_cocos2dx_builder_CCBReader_getAnimationManager(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_setAnimationManager(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_addOwnerOutletName(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_getOwnerCallbackNames(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_addDocumentCallbackControlEvents(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_setCCBRootPath(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_addOwnerOutletNode(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_getOwnerCallbackNodes(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_readSoundKeyframesForSeq(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_getCCBRootPath(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_getOwnerCallbackControlEvents(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_getOwnerOutletNodes(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_readUTF8(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_addOwnerCallbackControlEvents(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_getOwnerOutletNames(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_readCallbackKeyframesForSeq(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_getAnimationManagersForNodes(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_getNodesWithAnimationManagers(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_setResolutionScale(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBReader_CCBReader(JSContext *cx, uint32_t argc, jsval *vp);
-    
-    extern JSClass  *jsb_cocos2d_NavMeshObstacle_class;
-extern JSObject *jsb_cocos2d_NavMeshObstacle_prototype;
-    
-    extern JSClass  *jsb_cocos2d_Physics3DSliderConstraint_class;
-extern JSObject *jsb_cocos2d_Physics3DSliderConstraint_prototype;
-    
-    
-    {        std::string arg0_tmp; ok &= luaval_to_std_string(tolua_S, 2, &arg0_tmp, 'cc.SimpleAudioEngine:preloadBackgroundMusic'); arg0 = arg0_tmp.c_str();
-        if(!ok)
-        {
-            tolua_error(tolua_S,'invalid arguments in function 'lua_cocos2dx_cocosdenshion_SimpleAudioEngine_preloadBackgroundMusic'', nullptr);
-            return 0;
-        }
-        cobj->preloadBackgroundMusic(arg0);
-        lua_settop(tolua_S, 1);
-        return 1;
-    }
-    luaL_error(tolua_S, '%s has wrong number of arguments: %d, was expecting %d \n', 'cc.SimpleAudioEngine:preloadBackgroundMusic',argc, 1);
-    return 0;
-    
-    
-    
-    
-    
-    
-    
-    
-    
-        const float32 k_segments = 16.0f;
-    int vertexCount=16;
-    const float32 k_increment = 2.0f * b2_pi / k_segments;
-    float32 theta = 0.0f;
-    
-    GLfloat*    glVertices = new (std::nothrow) GLfloat[vertexCount*2];
-    for (int i = 0; i < k_segments; ++i)
-    {
-        b2Vec2 v = center + radius * b2Vec2(cosf(theta), sinf(theta));
-        glVertices[i*2]=v.x * mRatio;
-        glVertices[i*2+1]=v.y * mRatio;
-        theta += k_increment;
-    }
-    
-    mShaderProgram->setUniformLocationWith4f(mColorLocation, color.r, color.g, color.b, 1);
-    glVertexAttribPointer(GLProgram::VERTEX_ATTRIB_POSITION, 2, GL_FLOAT, GL_FALSE, 0, glVertices);
-    
-    				// For a circle: I = 0.5 * m * r * r ==> r = sqrt(2 * I / m)
-				float32 radius = b2Sqrt(2.0f * I / mass);
-    
-    			b2Body* prevBody = ground;
-			for (int32 i = 0; i < e_count; ++i)
-			{
-				b2BodyDef bd;
-				bd.type = b2_dynamicBody;
-				bd.position.Set(5.5f + 1.0f * i, 10.0f);
-				b2Body* body = m_world->CreateBody(&bd);
-				body->CreateFixture(&fd);
-    }
-    
-    TEST_F(YogaTest_HadOverflowTests, no_overflow_no_wrap_and_flex_children) {
-  const YGNodeRef child0 = YGNodeNewWithConfig(config);
-  YGNodeStyleSetWidth(child0, 80);
-  YGNodeStyleSetHeight(child0, 40);
-  YGNodeStyleSetMargin(child0, YGEdgeTop, 10);
-  YGNodeStyleSetMargin(child0, YGEdgeBottom, 10);
-  YGNodeInsertChild(root, child0, 0);
-  const YGNodeRef child1 = YGNodeNewWithConfig(config);
-  YGNodeStyleSetWidth(child1, 80);
-  YGNodeStyleSetHeight(child1, 40);
-  YGNodeStyleSetMargin(child1, YGEdgeBottom, 5);
-  YGNodeStyleSetFlexShrink(child1, 1);
-  YGNodeInsertChild(root, child1, 1);
-    }
-    
-    /* static */ void Config::destroy(Config * node)
-{
-    delete node;
+    /** Find lines from the image making the BLOCK_LIST. */
+BLOCK_LIST* TessBaseAPI::FindLinesCreateBlockList() {
+  FindLines();
+  BLOCK_LIST* result = block_list_;
+  block_list_ = nullptr;
+  return result;
 }
     
-     public:
+      protected:
+    /**
+     * Called by concrete classes.
+     *
+     * outputbase is the name of the output file excluding
+     * extension. For example, '/path/to/chocolate-chip-cookie-recipe'
+     *
+     * extension indicates the file extension to be used for output
+     * files. For example 'pdf' will produce a .pdf file, and 'hocr'
+     * will produce .hocr files.
+     */
+    TessResultRenderer(const char *outputbase,
+                       const char* extension);
     
-    double Node::getFlexShrink(void) const
-{
-    return YGNodeStyleGetFlexShrink(m_node);
+     protected:
+  // Function to compute part of a matrix.vector multiplication. The weights
+  // are in a very specific order (see above) in w, which is multiplied by
+  // u of length num_in, to produce output v after scaling the integer results
+  // by the corresponding member of scales.
+  // The amount of w and scales consumed is fixed and not available to the
+  // caller. The number of outputs written to v will be at most num_out.
+  typedef void (*PartialFunc)(const int8_t* w, const double* scales,
+                              const int8_t* u, int num_in, int num_out,
+                              double* v);
+    
+    // Computes one set of 4x8 products of inputs and weights, adding to result.
+// Horizontally adds 4 adjacent results, making 8x32-bit results.
+// rep_input is assumed to be an 8x replicated set of 4x8-bit signed integers.
+// Note that wi must previously have been re-organized with blocks of 4x8
+// weights in contiguous memory.
+// ones is a register of 16x16-bit values all equal to 1.
+// Note: wi is incremented by the amount of data read.
+// weights and reps are scratch registers.
+// This function must be inlined with references in order for the compiler to
+// correctly use the registers declared in the caller.
+inline void MultiplyGroup(const __m256i& rep_input, const __m256i& ones,
+                          const int8_t*& wi, __m256i& weights, __m256i& reps,
+                          __m256i& result) {
+  // Load a 4x8 block of weights.
+  weights = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(wi));
+  wi += kNumInputsPerRegister;
+  // Normalize the signs on rep_input, weights, so weights is always +ve.
+  reps = _mm256_sign_epi8(rep_input, weights);
+  weights = _mm256_sign_epi8(weights, weights);
+  // Multiply 32x8-bit reps by 32x8-bit weights to make 16x16-bit results,
+  // with adjacent pairs added.
+  weights = _mm256_maddubs_epi16(weights, reps);
+  // Multiply 16x16-bit result by 16x16-bit ones to make 8x32-bit results,
+  // with  adjacent pairs added. What we really want is a horizontal add of
+  // 16+16=32 bit result, but there is no such instruction, so multiply by
+  // 16-bit ones instead. It is probably faster than all the sign-extending,
+  // permuting and adding that would otherwise be required.
+  weights = _mm256_madd_epi16(weights, ones);
+  result = _mm256_add_epi32(result, weights);
 }
     
-    namespace facebook {
+      // After identify the math blocks, we do one more scanning on all text
+  // partitions, and check if any of them is the satellite of:
+  // math blocks: here a p is the satellite of q if:
+  // 1. q is the nearest vertical neighbor of p, and
+  // 2. y_gap(p, q) is less than a threshold, and
+  // 3. x_overlap(p, q) is over a threshold.
+  // Note that p can be the satellites of two blocks: its top neighbor and
+  // bottom neighbor.
+  void ProcessMathBlockSatelliteParts();
+    
+    #include          <string.h>
+#include          <ctype.h>
+#include          'params.h'
+#include          'float2int.h'
+#include          'tesseractclass.h'
+    
+    using namespace std;
+using namespace thd;
+using namespace thd::rpc;
+    
+    using namespace std;
+    
+    template<> AT_API Half convert(int64_t f) {
+  return convert<Half,double>(static_cast<double>(f));
+}
+template<> AT_API int64_t convert(Half f) {
+  return static_cast<int64_t>(convert<double,Half>(f));
+}
+    
+    int64_t size(const Tensor& self, int64_t dim) {
+  // false is passed to maybe_wrap_dim so behavior is identical to array access (but with wrapping)
+  dim = maybe_wrap_dim(dim, self.dim(), false);
+  return self.sizes()[dim];
+}
+    
+    ${Storage}::${Storage}(Context* context, std::size_t size, std::unique_ptr<Allocator> allocator)
+  : storage(nullptr),
+    context(context) {
+  auto ctx = new detail::AllocatorRetainable(std::move(allocator));
+  storage = ${THStorage}_newWithAllocator(${state,} size, &wrapped_allocator, ctx);
+  ctx->release();
+  ${THStorage}_clearFlag(${state,} storage, TH_STORAGE_RESIZABLE);
+}
+    
+    
+    {  private:
+    QueueWorker::Request _request;
+  };
+    
+        std::shared_ptr<GlooCache::algorithm_type> algo;
+    if (device == DeviceType::CPU) {
+      algo = std::make_shared<::gloo::BroadcastOneToAll<T>>(
+        context,
+        std::initializer_list<T*>{reinterpret_cast<T*>(input_buffer.get())},
+        count,
+        src_rank);
+#ifdef WITH_CUDA
+    } else if (device == DeviceType::CUDA) {
+      auto stream = THCState_getCurrentStream(THDGetCudaState());
     }
     
-      const char* functionName() const { return m_functionName; }
-  const char* fileName() const { return m_fileName; }
-  int lineNumber() const { return m_lineNumber; }
+    THDTensorDescriptor THDTensorDescriptor_newFromTHCudaDoubleTensor(THCudaDoubleTensor *tensor) {
+  return at::getType(at::Backend::CUDA, at::ScalarType::Double).unsafeTensorFromTH((void*)tensor, true);
+}
     
-    namespace facebook {
-    }
     
-    private:
-  void initialize() {
-    int ret = pthread_key_create(&m_key, m_cleanup);
-    if (ret != 0) {
-      const char *msg = '(unknown error)';
-      switch (ret) {
-      case EAGAIN:
-        msg = 'PTHREAD_KEYS_MAX (1024) is exceeded';
-        break;
-      case ENOMEM:
-        msg = 'Out-of-memory';
-        break;
-      }
-      (void) msg;
-      FBASSERTMSGF(0, 'pthread_key_create failed: %d %s', ret, msg);
-    }
-  }
+    
+    
+    
+    
+    
+    
+    {    delete[] glVertices;
+}
