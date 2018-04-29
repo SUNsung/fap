4b25d2eeaae8edb7f7f2b3c41255e3e062c635ef
SUNsung <weatherius@gmail.com>
2018-04-29 03:14:18 +0300

    Rewrite [04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb] File in: 2018-04-29 03:14:18

--- ruby/04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb
+++ ruby/04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb
@@ -1,114 +1,54 @@
 
         
-              def self.description
-        'Create a new Keychain'
+          def test_safe_redirect_back_from_other_host
+    @request.env['HTTP_REFERER'] = 'http://another.host/coming/from'
+    get :safe_redirect_back_with_status
+    
+      test 'response status aliases deprecated' do
+    response = ActionDispatch::TestResponse.create
+    assert_deprecated { response.success? }
+    assert_deprecated { response.missing? }
+    assert_deprecated { response.error? }
+  end
+end
+
+    
+        test 'head :continue (100) does not return any content' do
+      content = body(HeadController.action(:continue).call(Rack::MockRequest.env_for('/')))
+      assert_empty content
+    end
+    
+        class MessageDelivery < ActionMailer::MessageDelivery # :nodoc:
+      def initialize(mailer_class, action, params, *args)
+        super(mailer_class, action, *args)
+        @params = params
       end
     
-          # Read-only path to the shell-escaped gradle script, suitable for use in shell commands
-      attr_reader :escaped_gradle_path
-    
-            expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER]).to match(/cd .* && agvtool new-version -all 24 \&\& cd \-/)
+            expect(result).to eq('hg parent --template {rev}')
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('hg parent --template {rev}')
       end
     end
   end
 end
 
     
-        attr_reader :filters
-    
-        def ==(other)
-      other.name == name && other.path == path && other.type == type
-    end
-    
-        def normalized_path
-      path == '' ? '/' : path
-    end
-    
-          def get_type
-        name
-      end
-    
-              # Verify the box exists that we want to repackage
-          box = @env.boxes.find(box_name, box_provider, '= #{box_version}')
-          if !box
-            raise Vagrant::Errors::BoxNotFoundWithProviderAndVersion,
-              name: box_name,
-              provider: box_provider.to_s,
-              version: box_version
-          end
-    
-            opts = OptionParser.new do |o|
-          o.banner = 'Usage: vagrant cap [options] TYPE NAME [args]'
-          o.separator ''
-          o.separator 'This is an advanced command. If you don't know what this'
-          o.separator 'does and you aren't explicitly trying to use it, you probably'
-          o.separator 'don't want to use this.'
-          o.separator ''
-          o.separator 'This command checks or executes arbitrary capabilities that'
-          o.separator 'Vagrant has for hosts, guests, and providers.'
-          o.separator ''
-          o.separator 'Options:'
-          o.separator ''
-    
-      # After we load the schema, make sure we have sequences for each
-  # table using timestamp IDs.
-  Rake::Task['db:schema:load'].enhance do
-    Rake::Task['db:ensure_id_sequences_exist'].invoke
+      def icon
+    object.image
   end
     
-            subject.call(json, forwarder)
-      end
+    ###
+### methods
+###
+    
+          common_options(opts)
+      style(opts)
+      input_and_output(opts)
+      miscellaneous(opts)
     end
-  end
-end
-
     
-    class TagsController < ApplicationController
-  before_action :set_body_classes
-  before_action :set_instance_presenter
-    
-      def as_json(options={})
-    {
-      poll_id:             id,
-      post_id:             status_message.id,
-      question:            question,
-      poll_answers:        poll_answers,
-      participation_count: participation_count
-    }
-  end
-    
-          def find
-        client = Api::OpenidConnect::OAuthApplication.find_by(client_name: params[:client_name])
-        if client
-          render json: {client_id: client.client_id}
-        else
-          render json: {error: 'Client with name #{params[:client_name]} does not exist'}
+          module ClassMethods
+        def inherited(subclass)
+          subclass.log_levels = subclass.superclass.log_levels.dup
         end
-      end
     
-          def create
-        req = Rack::Request.new(request.env)
-        if req['client_assertion_type'] == 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
-          handle_jwt_bearer(req)
-        end
-        self.status, headers, self.response_body = Api::OpenidConnect::TokenEndpoint.new.call(request.env)
-        headers.each {|name, value| response.headers[name] = value }
-        nil
-      end
-    
-        # Provide a wrapper for the SCM that loads a strategy for the user.
-    #
-    # @param [Rake] context     The context in which the strategy should run
-    # @param [Module] strategy  A module to include into the SCM instance. The
-    #    module should provide the abstract methods of Capistrano::SCM
-    #
-    def initialize(context, strategy)
-      @context = context
-      singleton = class << self; self; end
-      singleton.send(:include, strategy)
-    end
-    
-      describe 'configuration SSHKit' do
-    let(:config) { SSHKit.config }
-    let(:backend) { SSHKit.config.backend.config }
-    let(:default_env) { { rails_env: :production } }
+      Sass::Plugin.options.merge!(config)
