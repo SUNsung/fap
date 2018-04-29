06491e3242b4e8db7e544b9d4eb3ab7fef61e1b6
SUNsung <weatherius@gmail.com>
2018-04-29 22:01:03 +0300

    Rewrite [00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp] File in: 2018-04-29 22:01:02

--- cccccc/00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp
+++ cccccc/00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp
@@ -1,303 +1,82 @@
 
         
-        #ifndef TENSORFLOW_GRAPPLER_COSTS_MEASURING_COST_ESTIMATOR_H_
-#define TENSORFLOW_GRAPPLER_COSTS_MEASURING_COST_ESTIMATOR_H_
+        #ifndef CONTENT_NW_SRC_API_APP_APP_H_
+#define CONTENT_NW_SRC_API_APP_APP_H_
     
-    #endif  // TENSORFLOW_LIB_IO_RECORD_WRITER_H_
-
+    Base::Base(int id,
+           const base::WeakPtr<ObjectManager>& object_manager,
+           const base::DictionaryValue& option,
+	   const std::string& extension_id)
+    : extension_id_(extension_id),
+      id_(id),
+      delay_destruction_(false),
+      pending_destruction_(false),
+      object_manager_(object_manager) {
+}
     
-    class TFRecordReader : public ReaderBase {
+    v8::Handle<v8::Value> AllocateId(int routing_id);
+    
+    void Menu::Append(MenuItem* menu_item) {
+  menu_items.push_back(menu_item);
+  if (GTK_IS_ACCEL_GROUP(gtk_accel_group)){
+    menu_item->UpdateKeys(gtk_accel_group);
+  }
+  gtk_menu_shell_append(GTK_MENU_SHELL(menu_), menu_item->menu_item_);
+}
+    
+    void Menu::Create(const base::DictionaryValue& option) {
+  is_menu_modified_ = true;
+  menu_delegate_.reset(new MenuDelegate(object_manager()));
+  menu_model_.reset(new ui::NwMenuModel(menu_delegate_.get()));
+    }
+    
+    void MenuItem::Destroy() {
+  gtk_widget_destroy(menu_item_);
+  g_object_unref(G_OBJECT(menu_item_));
+}
+    
+    class NwClipboardClearSyncFunction : public NWSyncExtensionFunction {
  public:
-  TFRecordReader(const string& node_name, const string& compression_type,
-                 Env* env)
-      : ReaderBase(strings::StrCat('TFRecordReader '', node_name, ''')),
-        env_(env),
-        offset_(0),
-        compression_type_(compression_type) {}
+  NwClipboardClearSyncFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
     }
     
-    template <typename T>
-void DynamicStitchGPUImpl(const Eigen::GpuDevice& gpu_device,
-                          const int32 slice_size, const int32 first_dim_size,
-                          const CudaDeviceArrayStruct<int>& input_indices,
-                          const CudaDeviceArrayStruct<const T*>& input_ptrs,
-                          T* output) {
-  const int32 output_size = first_dim_size * slice_size;
-  auto config = GetCudaLaunchConfig(output_size, gpu_device);
+    // For desktop capture APIs
+#include 'base/base64.h'
+#include 'base/strings/string16.h'
+#include 'base/strings/utf_string_conversions.h'
+#include 'chrome/browser/media/webrtc/desktop_media_list_observer.h'
+#include 'chrome/browser/media/webrtc/desktop_streams_registry.h'
+#include 'chrome/browser/media/webrtc/media_capture_devices_dispatcher.h'
+#include 'chrome/browser/media/webrtc/native_desktop_media_list.h'
+#include 'content/public/browser/render_frame_host.h'
+#include 'content/public/browser/render_process_host.h'
+#include 'content/public/browser/web_contents.h'
+#include 'third_party/webrtc/modules/desktop_capture/desktop_capture_options.h'
+#include 'third_party/webrtc/modules/desktop_capture/desktop_capturer.h'
+#include 'ui/gfx/codec/png_codec.h'
+#include 'ui/gfx/image/image.h'
+#include 'ui/gfx/image/image_skia.h'
+    
+        if (LIKELY(!(flags() & kFlagMaybeShared))) {
+      return false;
     }
     
-    // TODO: Enable GPU support for angle op after resolving
-// build failures on GPU (See #10643 for context).
-#if 0 && GOOGLE_CUDA
-REGISTER_COMPLEX(GPU, float, complex64);
-REGISTER_COMPLEX(GPU, double, complex128);
+    //  GCC 4.9 with ASAN has a problem: a function with no_sanitize_address calling
+//  a function with always_inline fails to build. The _mm_* functions are marked
+//  always_inline.
+//  https://gcc.gnu.org/bugzilla/show_bug.cgi?id=67368
+#if defined FOLLY_SANITIZE_ADDRESS && FOLLY_SANITIZE_ADDRESS == 1 && \
+    __GNUC_PREREQ(4, 9)
+#define _mm_load_si128(p) (*(p))
+#define _mm_loadu_si128(p) ((__m128i)__builtin_ia32_loaddqu((const char*)(p)))
+#ifdef _mm_cmpestri
+#undef _mm_cmpestri
+#endif
+#define _mm_cmpestri(a, b, c, d, e) \
+  __builtin_ia32_pcmpestri128((__v16qi)(a), b, (__v16qi)(c), d, e)
 #endif
     
-    namespace caffe {
-    }
-    
-    /**
- * @brief Abstract base class that factors out the BLAS code common to
- *        ConvolutionLayer and DeconvolutionLayer.
- */
-template <typename Dtype>
-class BaseConvolutionLayer : public Layer<Dtype> {
- public:
-  explicit BaseConvolutionLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-     protected:
-  /**
-   * @param bottom input Blob vector (length 2+)
-   *   -# @f$ (N \times ...) @f$
-   *      the inputs @f$ x_1 @f$
-   *   -# @f$ (M) @f$
-   *      the inputs @f$ x_2 @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (M \times ...) @f$:
-   *      the reindexed array @f$
-   *        y = x_1[x_2]
-   *      @f$
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-    #endif  // CAFFE_CONTRASTIVE_LOSS_LAYER_HPP_
-
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    #include 'caffe/layers/pooling_layer.hpp'
-    
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    /**
- * @brief During training only, sets a random portion of @f$x@f$ to 0, adjusting
- *        the rest of the vector magnitude accordingly.
- *
- * @param bottom input Blob vector (length 1)
- *   -# @f$ (N \times C \times H \times W) @f$
- *      the inputs @f$ x @f$
- * @param top output Blob vector (length 1)
- *   -# @f$ (N \times C \times H \times W) @f$
- *      the computed outputs @f$ y = |x| @f$
- */
-template <typename Dtype>
-class DropoutLayer : public NeuronLayer<Dtype> {
- public:
-  /**
-   * @param param provides DropoutParameter dropout_param,
-   *     with DropoutLayer options:
-   *   - dropout_ratio (\b optional, default 0.5).
-   *     Sets the probability @f$ p @f$ that any given unit is dropped.
-   */
-  explicit DropoutLayer(const LayerParameter& param)
-      : NeuronLayer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    
-    {  bool stable_prod_grad_;
-};
-    
-    namespace caffe {
-    }
-    
-      virtual inline const char* type() const { return 'Exp'; }
-    
-    ///////////////////////////////////////////////////////////////////////////////
-    
-    //////////////////////////////////////////////////////////////////////
-    
-    // Disassemble the code from the given raw file, whose initial address is given
-// by fileStartAddr, for the address range given by
-// [codeStartAddr, codeStartAddr + codeLen)
-    
-      for (auto& arc : cg.arcs) {
-    if (arc.weight == 0) continue;
-    }
-    
-    #include <string>
-#include <vector>
-#include <cstdio>
-#include <cstdlib>
-#include <map>
-    
-    
-    {  auto iov = buf->getIov();
-  transport.writev(nullptr, iov.data(), iov.size());
-}
-    
-      /**
-   * Construct a new string by replacing the characters denoted by the half-open
-   *   range [`first`,`last`) within this string with the characters from string
-   *   `that` starting at position `that_pos`.
-   * \pre `that_pos <= that.size()`
-   * \note Equivalent to
-   *   <tt>creplace(first - data(), last - first, that, that_pos,
-   *   that.size() - that_pos)</tt>
-   */
-  template <std::size_t M>
-  constexpr BasicFixedString<Char, N + M> creplace(
-      const Char* first,
-      const Char* last,
-      const BasicFixedString<Char, M>& that,
-      std::size_t that_pos = 0u) const noexcept(false) {
-    return creplace(
-        first - data_,
-        last - first,
-        that,
-        that_pos,
-        that.size_ - detail::fixedstring::checkOverflow(that_pos, that.size_));
-  }
-    
-    size_t IOBuf::countChainElements() const {
-  size_t numElements = 1;
-  for (IOBuf* current = next_; current != this; current = current->next_) {
-    ++numElements;
-  }
-  return numElements;
-}
-    
-    Range<AsyncIO::Op**> AsyncIO::doWait(
-    WaitType type,
-    size_t minRequests,
-    size_t maxRequests,
-    std::vector<Op*>& result) {
-  io_event events[maxRequests];
-    }
-    
-    namespace folly {
-namespace fs {
-    }
-    }
-    
-            options.clear();
-        split(',', parts[3], options);
-        size_t pageSize = defaultHugePageSize;
-        // Search for the 'pagesize' option, which must have a value
-        for (auto& option : options) {
-          // key=value
-          const char* p = static_cast<const char*>(
-              memchr(option.data(), '=', option.size()));
-          if (!p) {
-            continue;
-          }
-          if (StringPiece(option.data(), p) != 'pagesize') {
-            continue;
-          }
-          pageSize = parsePageSizeValue(StringPiece(p + 1, option.end()));
-          break;
-        }
-    
-    namespace folly {
-    }
-    
-      // Updates the probability distribution for the units in the corpus.
-  // Must be called whenever the corpus or unit weights are changed.
-  void UpdateCorpusDistribution() {
-    size_t N = Inputs.size();
-    Intervals.resize(N + 1);
-    Weights.resize(N);
-    std::iota(Intervals.begin(), Intervals.end(), 0);
-    if (CountingFeatures)
-      for (size_t i = 0; i < N; i++)
-        Weights[i] = Inputs[i]->NumFeatures * (i + 1);
-    else
-      std::iota(Weights.begin(), Weights.end(), 1);
-    CorpusDistribution = std::piecewise_constant_distribution<double>(
-        Intervals.begin(), Intervals.end(), Weights.begin());
-  }
-  std::piecewise_constant_distribution<double> CorpusDistribution;
-    
-    
-#ifdef __clang__  // avoid gcc warning.
-#  define ATTRIBUTE_NO_SANITIZE_MEMORY __attribute__((no_sanitize('memory')))
-#else
-#  define ATTRIBUTE_NO_SANITIZE_MEMORY
-#endif
-    
-      bool HasPositionHint() const { return PositionHint != std::numeric_limits<size_t>::max(); }
-  size_t GetPositionHint() const {
-    assert(HasPositionHint());
-    return PositionHint;
-  }
-  void IncUseCount() { UseCount++; }
-  void IncSuccessCount() { SuccessCount++; }
-  size_t GetUseCount() const { return UseCount; }
-  size_t GetSuccessCount() const {return SuccessCount; }
-    
-    void ReadDirToVectorOfUnits(const char *Path, std::vector<Unit> *V,
-                            long *Epoch, size_t MaxSize, bool ExitOnError);
-    
-    void ListFilesInDirRecursive(const std::string &Dir, long *Epoch,
-                             std::vector<std::string> *V, bool TopDir) {
-  auto E = GetEpoch(Dir);
-  if (Epoch)
-    if (E && *Epoch >= E) return;
-    }
-    
-    
-    {  bool InMergeMode = false;
-};
-    
-    
-    {  std::ofstream OF(CFPath, std::ofstream::out | std::ofstream::app);
-  for (size_t i = M.FirstNotProcessedFile; i < M.Files.size(); i++) {
-    auto U = FileToVector(M.Files[i].Name);
-    if (U.size() > MaxInputLen) {
-      U.resize(MaxInputLen);
-      U.shrink_to_fit();
-    }
-    std::ostringstream StartedLine;
-    // Write the pre-run marker.
-    OF << 'STARTED ' << std::dec << i << ' ' << U.size() << '\n';
-    OF.flush();  // Flush is important since ExecuteCommand may crash.
-    // Run.
-    TPC.ResetMaps();
-    ExecuteCallback(U.data(), U.size());
-    // Collect coverage.
-    std::set<size_t> Features;
-    TPC.CollectFeatures([&](size_t Feature) -> bool {
-      Features.insert(Feature);
-      return true;
-    });
-    // Show stats.
-    TotalNumberOfRuns++;
-    if (!(TotalNumberOfRuns & (TotalNumberOfRuns - 1)))
-      PrintStats('pulse ');
-    // Write the post-run marker and the coverage.
-    OF << 'DONE ' << i;
-    for (size_t F : Features)
-      OF << ' ' << std::hex << F;
-    OF << '\n';
-  }
-}
-    
-    
-    {  bool Parse(std::istream &IS, bool ParseCoverage);
-  bool Parse(const std::string &Str, bool ParseCoverage);
-  void ParseOrExit(std::istream &IS, bool ParseCoverage);
-  size_t Merge(std::vector<std::string> *NewFiles);
-};
-    
-    #ifndef LLVM_FUZZER_OPTIONS_H
-#define LLVM_FUZZER_OPTIONS_H
-    
-    TracePC TPC;
+    template <typename Fn>
+using ArgumentTypes =
+  typename ArgumentTypesByKind<IdentifyCallable::getKind<Fn>(), Fn>::type;
