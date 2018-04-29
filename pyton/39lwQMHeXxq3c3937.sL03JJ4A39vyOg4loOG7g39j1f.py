28c86a8c2b230a159d94a30ee56dd474180691ca
SUNsung <weatherius@gmail.com>
2018-04-29 21:37:52 +0300

    Rewrite [39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py] File in: 2018-04-29 21:37:51

--- pyton/39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py
+++ pyton/39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py
@@ -1,70 +1,153 @@
 
         
-            def from_spider(self, spider, results):
-        requests = []
-        for method in self.tested_methods_from_spidercls(type(spider)):
-            bound_method = spider.__getattribute__(method)
-            requests.append(self.from_method(bound_method, results))
+        import pytest
+    
+        seems_fishy = False
+    if total_found == 0:
+        info.append('Error: the template could not be found.')
+        seems_fishy = True
+    elif total_found > 1:
+        info.append('Warning: multiple loaders returned a match for the template.')
+        seems_fishy = True
+    
+        If there is no handler for the logger's effective level, add a
+    :class:`~logging.StreamHandler` for
+    :func:`~flask.logging.wsgi_errors_stream` with a basic format.
+    '''
+    logger = logging.getLogger('flask.app')
     
     
-class Vehicle(metaclass=ABCMeta):
+# Core signals.  For usage examples grep the source code or consult
+# the API documentation in docs/api.rst as well as docs/signals.rst
+template_rendered = _signals.signal('template-rendered')
+before_render_template = _signals.signal('before-render-template')
+request_started = _signals.signal('request-started')
+request_finished = _signals.signal('request-finished')
+request_tearing_down = _signals.signal('request-tearing-down')
+got_request_exception = _signals.signal('got-request-exception')
+appcontext_tearing_down = _signals.signal('appcontext-tearing-down')
+appcontext_pushed = _signals.signal('appcontext-pushed')
+appcontext_popped = _signals.signal('appcontext-popped')
+message_flashed = _signals.signal('message-flashed')
+
     
-        def remove_link_to_crawl(self, url):
-        '''Remove the given link from `links_to_crawl`.'''
-        pass
+            for blueprint in self.app.iter_blueprints():
+            loader = blueprint.jinja_loader
+            if loader is not None:
+                for template in loader.list_templates():
+                    result.add(template)
     
-        @property
-    def value(self):
-        if self.is_ace() == 1:
-            return 1
-        elif self.is_face_card():
-            return 10
-        else:
-            return self._value
     
-            Accessing a node updates its position to the front of the LRU list.
+def test_config_from_mapping():
+    app = flask.Flask(__name__)
+    app.config.from_mapping({
+        'SECRET_KEY': 'config',
+        'TEST_KEY': 'foo'
+    })
+    common_object_test(app)
+    
+    
+def test_explicit_instance_paths(modules_tmpdir):
+    with pytest.raises(ValueError) as excinfo:
+        flask.Flask(__name__, instance_path='instance')
+    assert 'must be absolute' in str(excinfo.value)
+    
+        def entry_ok(self):
+        'Return apparently valid (name, path) or None'
+        self.entry_error['text'] = ''
+        self.path_error['text'] = ''
+        name = self.item_ok()
+        path = self.path_ok()
+        return None if name is None or path is None else (name, path)
+    
+        print('int:')
+    print('calculation time: %fs' % (end_calc-start_calc))
+    print('conversion time: %fs\n\n' % (end_conv-start_conv))
+    
+        def seek(self, pos, whence=0):
+        '''Seek to specified position into the chunk.
+        Default position is 0 (start of chunk).
+        If the file is not seekable, this will result in an error.
         '''
-        node = self.lookup[query]
-        if node is None:
-            return None
-        self.linked_list.move_to_front(node)
-        return node.results
     
-        def __init__(self):
-        self.lookup = {}  # key: person_id, value: person_server
+    def py_make_scanner(context):
+    parse_object = context.parse_object
+    parse_array = context.parse_array
+    parse_string = context.parse_string
+    match_number = NUMBER_RE.match
+    strict = context.strict
+    parse_float = context.parse_float
+    parse_int = context.parse_int
+    parse_constant = context.parse_constant
+    object_hook = context.object_hook
+    object_pairs_hook = context.object_pairs_hook
+    memo = context.memo
     
-            (2016-01, url0), 2
-        (2016-01, url1), 1
-        '''
-        yield key, sum(values)
+        def _grouplist(self, lines):
+        # Parse lines into 'group last first flag'
+        return [GroupInfo(*line.split()) for line in lines]
     
-        def test_tc1_output(self):
-        self.tc1.run()
-        output = self.out.getvalue().strip()
-        self.assertEqual(output, self.average_result_tc1)
+        The DOMBuilder class uses an instance of this class to pass settings to
+    the ExpatBuilder class.
+    '''
     
+        def find_byname(self, name):
+        '''Return (red, green, blue) for name'''
+        name = name.lower()
+        try:
+            return self.__byname[name]
+        except KeyError:
+            raise BadColor(name) from None
     
-class ConcreteHandler1(Handler):
+                    # kwargs={}
+                result = _testcapi.pyobject_fastcalldict(func, args, {})
+                self.check_result(result, expected)
     
-        def tearDown(self):
-        if not self._bProblem:
-            print('Tearing down')
-            time.sleep(0.1)
-            self._tm.publishReport()
-        else:
-            print('Test not executed. No tear down required.')
+            class complex2(complex):
+            '''Make sure that __complex__() calls fail if anything other than a
+            complex is returned'''
+            def __complex__(self):
+                return None
     
-        a_transaction = Transaction(True, num_obj)
-    try:
-        for i in range(3):
-            num_obj.increment()
-            print(num_obj)
-        a_transaction.commit()
-        print('-- committed')
+        def test_infile_outfile(self):
+        infile = self._create_infile()
+        outfile = support.TESTFN + '.out'
+        rc, out, err = assert_python_ok('-m', 'json.tool', infile, outfile)
+        self.addCleanup(os.remove, outfile)
+        with open(outfile, 'r') as fp:
+            self.assertEqual(fp.read(), self.expect)
+        self.assertEqual(rc, 0)
+        self.assertEqual(out, b'')
+        self.assertEqual(err, b'')
     
-        def update(self, subject):
-        print(u'DecimalViewer: Subject %s has data %d' %
-              (subject.name, subject.data))
+        def test_math(self):
+        self.assertEqual(+False, 0)
+        self.assertIsNot(+False, False)
+        self.assertEqual(-False, 0)
+        self.assertIsNot(-False, False)
+        self.assertEqual(abs(False), 0)
+        self.assertIsNot(abs(False), False)
+        self.assertEqual(+True, 1)
+        self.assertIsNot(+True, True)
+        self.assertEqual(-True, -1)
+        self.assertEqual(abs(True), 1)
+        self.assertIsNot(abs(True), True)
+        self.assertEqual(~False, -1)
+        self.assertEqual(~True, -2)
     
+                # exhausted iterator
+            self.assertRaises(StopIteration, next, itorig)
+            d = pickle.dumps((itorig, orig), proto)
+            it, a = pickle.loads(d)
+            a[:] = data
+            self.assertEqual(list(it), [])
     
-class User(object):
+    class _AcquireFutures(object):
+    '''A context manager that does an ordered acquire of Future conditions.'''
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
