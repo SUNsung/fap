2b3e9de10511f3ce315d5e757afff46fee1d9500
SUNsung <weatherius@gmail.com>
2018-04-30 17:00:51 +0300

    Rewrite [01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py] File in: 2018-04-30 17:00:50

--- pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
+++ pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
@@ -1,327 +1,74 @@
 
         
-            with io.open(outfile, 'w', encoding='utf-8') as outf:
-        outf.write(out)
+        
+def maybe_restore_pretrained_model(sess, saver_for_restore, model_dir):
+  '''Restores pretrained model if there is no ckpt model.'''
+  ckpt = tf.train.get_checkpoint_state(FLAGS.train_dir)
+  checkpoint_exists = ckpt and ckpt.model_checkpoint_path
+  if checkpoint_exists:
+    tf.logging.info('Checkpoint exists in FLAGS.train_dir; skipping '
+                    'pretraining restore')
+    return
     
-        def to_screen(self, s, skip_eol=None):
-        print(s)
+    from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
     
-        def matching_ies(self, url):
-        return [ie.IE_NAME for ie in self.ies if ie.suitable(url) and ie.IE_NAME != 'generic']
+      def test_add_child_idempotency(self):
+    root = MCTSNode(utils_test.BOARD_SIZE, go.Position(utils_test.BOARD_SIZE))
+    child = root.maybe_add_child(17)
+    current_children = copy.copy(root.children)
+    child2 = root.maybe_add_child(17)
+    self.assertEqual(child, child2)
+    self.assertEqual(current_children, root.children)
     
+      def assertEqualData(self, data1, data2):
+    # Assert that two data are equal, where both are of form:
+    # data = List<Tuple<feature_array, pi_array, value>>
+    self.assertEqual(len(data1), len(data2))
+    for datum1, datum2 in zip(data1, data2):
+      # feature
+      self.assertEqualNPArray(datum1[0], datum2[0])
+      # pi
+      self.assertEqualNPArray(datum1[1], datum2[1])
+      # value
+      self.assertEqual(datum1[2], datum2[2])
     
-def _make_testfunc(testfile):
-    m = re.match(r'^(.*)\.(as)$', testfile)
-    if not m:
-        return
-    test_id = m.group(1)
+        self.assertEqualPositions(last_position, last_position2)
     
-    # Allow direct execution
-import os
-import sys
-import unittest
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    tf.logging.set_verbosity(tf.logging.ERROR)
     
-            self._debug_cmd(args)
+        def set(self, key, value):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                item.value = value
+                return
+        self.table[hash_index].append(Item(key, value))
     
-            webpage = self._download_webpage(url, playlist_id)
-        title = self._html_search_regex(
-            r'<h1 class='playlist-name'[^>]*?>(.*?)</h1>', webpage, 'title')
-        description = self._html_search_regex(
-            r'<p class='excerpt'[^>]*?>(.*?)</p>',
-            webpage, 'description', fatal=False)
-        urls = re.findall(
-            r'<li class='lecture-preview'>\s*?<a target='_blank' href='([^']+)'>',
-            webpage)
-        entries = [self.url_result(u) for u in urls]
+        def park_vehicle(self, vehicle):
+        pass
     
+        def insert_crawled_link(self, url, signature):
+        '''Add the given link to `crawled_links`.'''
+        pass
     
-class ClipRsIE(OnetBaseIE):
-    _VALID_URL = r'https?://(?:www\.)?clip\.rs/(?P<id>[^/]+)/\d+'
-    _TEST = {
-        'url': 'http://www.clip.rs/premijera-frajle-predstavljaju-novi-spot-za-pesmu-moli-me-moli/3732',
-        'md5': 'c412d57815ba07b56f9edc7b5d6a14e5',
-        'info_dict': {
-            'id': '1488842.1399140381',
-            'ext': 'mp4',
-            'title': 'PREMIJERA Frajle predstavljaju novi spot za pesmu Moli me, moli',
-            'description': 'md5:56ce2c3b4ab31c5a2e0b17cb9a453026',
-            'duration': 229,
-            'timestamp': 1459850243,
-            'upload_date': '20160405',
-        }
-    }
-    
-    
-# Certain versions of pypy have a bug where clearing the exception stack
-# breaks the __exit__ function in a very peculiar way.  The second level of
-# exception blocks is necessary because pypy seems to forget to check if an
-# exception happened until the next bytecode instruction?
-#
-# Relevant PyPy bugfix commit:
-# https://bitbucket.org/pypy/pypy/commits/77ecf91c635a287e88e60d8ddb0f4e9df4003301
-# According to ronan on #pypy IRC, it is released in PyPy2 2.3 and later
-# versions.
-#
-# Ubuntu 14.04 has PyPy 2.2.1, which does exhibit this bug.
-BROKEN_PYPY_CTXMGR_EXIT = False
-if hasattr(sys, 'pypy_version_info'):
-    class _Mgr(object):
-        def __enter__(self):
-            return self
-        def __exit__(self, *args):
-            if hasattr(sys, 'exc_clear'):
-                # Python 3 (PyPy3) doesn't have exc_clear
-                sys.exc_clear()
-    try:
-        try:
-            with _Mgr():
-                raise AssertionError()
-        except:
-            raise
-    except TypeError:
-        BROKEN_PYPY_CTXMGR_EXIT = True
-    except AssertionError:
+        def bfs(self, source, dest):
+        # Use self.visited_ids to track visited nodes
+        # Use self.lookup to translate a person_id to a Person
         pass
 
     
-    from .globals import request
+        def mapper(self, _, line):
+        '''Parse each log line, extract and transform relevant lines.
     
-        #: Setting this disables or force-enables the automatic options handling.
-    provide_automatic_options = None
+            Emit key value pairs of the form:
     
-        parent = os.path.join(path, os.path.pardir)
-    parent = os.path.realpath(parent)
-    
-    # The checksum algorithm must match with the algorithm in ShellModule.checksum() method
-checksum = secure_hash
-checksum_s = secure_hash_s
-    
-        # If we get to this point, we know that any included ranges are valid.
-    # If the caller is prepared to handle them, all is well.
-    # Otherwise we treat it as a parse failure.
-    if not allow_ranges and '[' in host:
-        raise AnsibleParserError('Detected range in host but was asked to ignore ranges')
-    
-        terminal_stdout_re = [
-        re.compile(br'[\r\n]?[\w+\-\.:\/\[\]]+(?:\([^\)]+\)){,3}(?:>|#) ?$'),
-        re.compile(br'\@[\w\-\.]+:\S+?[>#\$] ?$')
-    ]
-    
-    # Train the model each generation and show predictions against the validation
-# dataset.
-for iteration in range(1, 200):
-    print()
-    print('-' * 50)
-    print('Iteration', iteration)
-    model.fit(x_train, y_train,
-              batch_size=BATCH_SIZE,
-              epochs=1,
-              validation_data=(x_val, y_val))
-    # Select 10 samples from the validation set at random so we can visualize
-    # errors.
-    for i in range(10):
-        ind = np.random.randint(0, len(x_val))
-        rowx, rowy = x_val[np.array([ind])], y_val[np.array([ind])]
-        preds = model.predict_classes(rowx, verbose=0)
-        q = ctable.decode(rowx[0])
-        correct = ctable.decode(rowy[0])
-        guess = ctable.decode(preds[0], calc_argmax=False)
-        print('Q', q[::-1] if REVERSE else q, end=' ')
-        print('T', correct, end=' ')
-        if correct == guess:
-            print(colors.ok + '☑' + colors.close, end=' ')
+        for url in urls:
+        if not url.startswith('http'):
+            print('markdown file name: ' + url)
+            continue
+        if check_live_url(url):
+            print(url)
         else:
-            print(colors.fail + '☒' + colors.close, end=' ')
-        print(guess)
-
-    
-    
-@keras_test
-def test_model_trainability_switch():
-    # a non-trainable model has no trainable weights
-    x = Input(shape=(1,))
-    y = Dense(2)(x)
-    model = Model(x, y)
-    model.trainable = False
-    assert model.trainable_weights == []
-    
-        if not blocks:
-        raise RuntimeError('Found no content for page ' +
-                           page_data['page'])
-    
-    print('Loading data...')
-(x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=max_words,
-                                                         test_split=0.2)
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
-    
-    # Default QA1 with 1000 samples
-# challenge = 'tasks_1-20_v1-2/en/qa1_single-supporting-fact_{}.txt'
-# QA1 with 10,000 samples
-# challenge = 'tasks_1-20_v1-2/en-10k/qa1_single-supporting-fact_{}.txt'
-# QA2 with 1000 samples
-challenge = 'tasks_1-20_v1-2/en/qa2_two-supporting-facts_{}.txt'
-# QA2 with 10,000 samples
-# challenge = 'tasks_1-20_v1-2/en-10k/qa2_two-supporting-facts_{}.txt'
-with tarfile.open(path) as tar:
-    train = get_stories(tar.extractfile(challenge.format('train')))
-    test = get_stories(tar.extractfile(challenge.format('test')))
-    
-    It is recommended to run this script on GPU, as recurrent
-networks are quite computationally intensive.
-    
-        # Arguments
-        path: where to cache the data (relative to `~/.keras/dataset`).
-    
-    
-@ignore_warnings
-def benchmark(metrics=tuple(v for k, v in sorted(METRICS.items())),
-              formats=tuple(v for k, v in sorted(FORMATS.items())),
-              samples=1000, classes=4, density=.2,
-              n_times=5):
-    '''Times metric calculations for a number of inputs
-    
-    for i, n in enumerate(n_samples):
-    for j, p in enumerate(n_features):
-        X = np.random.normal(size=(n, p))
-        t0 = time.time()
-        ward.fit(X)
-        scikits_time[j, i] = time.time() - t0
-        t0 = time.time()
-        hierarchy.ward(X)
-        scipy_time[j, i] = time.time() - t0
-    
-        This is called by sphinx.ext.linkcode
-    
-        print('Decompressing %s' % ARCHIVE_NAME)
-    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
-        archive.extractall(path='.')
-    os.remove(ARCHIVE_NAME)
-
-    
-    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
-TRAIN_FOLDER = '20news-bydate-train'
-TEST_FOLDER = '20news-bydate-test'
-    
-        # the training data folder must be passed as first argument
-    movie_reviews_data_folder = sys.argv[1]
-    dataset = load_files(movie_reviews_data_folder, shuffle=False)
-    print('n_samples: %d' % len(dataset.data))
-    
-    # Train uncalibrated random forest classifier on whole train and validation
-# data and evaluate on test data
-clf = RandomForestClassifier(n_estimators=25)
-clf.fit(X_train_valid, y_train_valid)
-clf_probs = clf.predict_proba(X_test)
-score = log_loss(y_test, clf_probs)
-    
-    
-# Random labeling with varying n_clusters against ground class labels
-# with fixed number of clusters
-    
-        filenames = []
-    for (dirpath, dnames, fnames) in os.walk(path):
-        for fname in fnames:
-            if fname.endswith('.md'):
-                filenames.append(os.sep.join([dirpath, fname]))
-    
-    
-# Executing the interpreter in a subprocess
-def _assert_python(expected_success, *args, **env_vars):
-    cmd_line = [sys.executable]
-    if not env_vars:
-        cmd_line.append('-E')
-    # Need to preserve the original environment, for in-place testing of
-    # shared library builds.
-    env = os.environ.copy()
-    # But a special flag that can be set to override -- in this case, the
-    # caller is responsible to pass the full environment.
-    if env_vars.pop('__cleanenv', None):
-        env = {}
-    env.update(env_vars) 
-    cmd_line.extend(args)
-    p = subprocess.Popen(cmd_line, stdin=subprocess.PIPE,
-                         stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                         env=env)
-    try:
-        out, err = p.communicate()
-    finally:
-        subprocess._cleanup()
-        p.stdout.close()
-        p.stderr.close()
-    rc = p.returncode
-    err = strip_python_stderr(err)
-    if (rc and expected_success) or (not rc and not expected_success):
-        raise AssertionError(
-            'Process return code is %d, '
-            'stderr follows:\n%s' % (rc, err.decode('ascii', 'ignore')))
-    return rc, out, err
-    
-    
-  @staticmethod
-  def CreateFromOptions( user_options ):
-    all_filters = dict( user_options.get( 'filter_diagnostics', {} ) )
-    compiled_by_type = {}
-    for type_spec, filter_value in iteritems( dict( all_filters ) ):
-      filetypes = [ type_spec ]
-      if type_spec.find( ',' ) != -1:
-        filetypes = type_spec.split( ',' )
-      for filetype in filetypes:
-        compiled_by_type[ filetype ] = _CompileFilters( filter_value )
-    
-    
-def LastEnteredCharIsIdentifierChar_FiletypeHtml_test():
-  with MockCurrentFiletypes( ['html'] ):
-    with MockCurrentColumnAndLineContents( 3, 'ab-' ):
-      ok_( base.LastEnteredCharIsIdentifierChar() )
-    
-      # As a last resort, we search python in the PATH. We prefer Python 2 over 3
-  # for the sake of backwards compatibility with ycm_extra_conf.py files out
-  # there; few people wrote theirs to work on py3.
-  # So we check 'python2' before 'python' because on some distributions (Arch
-  # Linux for example), python refers to python3.
-  python_interpreter = utils.PathToFirstExistingExecutable( [ 'python2',
-                                                              'python',
-                                                              'python3' ] )
-  if python_interpreter:
-    return python_interpreter
-    
-      for path in python_paths:
-    yield EndsWithPython_Good, path
-    
-            # Make sure the tornado module under test is available to the test
-        # application
-        pythonpath = os.getcwd()
-        if 'PYTHONPATH' in os.environ:
-            pythonpath += os.pathsep + os.environ['PYTHONPATH']
-    
-    
-@skipIfNonUnix
-class TestMultiprocess(unittest.TestCase):
-    # These tests verify that the two multiprocess examples from the
-    # TCPServer docs work. Both tests start a server with three worker
-    # processes, each of which prints its task id to stdout (a single
-    # byte, so we don't have to worry about atomicity of the shared
-    # stdout stream) and then exits.
-    def run_subproc(self, code):
-        proc = subprocess.Popen(sys.executable,
-                                stdin=subprocess.PIPE,
-                                stdout=subprocess.PIPE)
-        proc.stdin.write(utf8(code))
-        proc.stdin.close()
-        proc.wait()
-        stdout = proc.stdout.read()
-        proc.stdout.close()
-        if proc.returncode != 0:
-            raise RuntimeError('Process returned %d. stdout=%r' % (
-                proc.returncode, stdout))
-        return to_unicode(stdout)
-    
-    
-if __name__ == '__main__':
-    main()
-
-    
-        def __handle_exception(self, typ, value, tb):
-        pass
+            print(url, file=sys.stderr)
