0c7cacf3fa40e73757cae69c6e2b49f9a5b4981d
SUNsung <weatherius@gmail.com>
2018-04-30 11:16:36 +0300

    Rewrite [400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py] File in: 2018-04-30 11:16:36

--- pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
+++ pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
@@ -1,74 +1,57 @@
 
         
-                '''
-        available_plugins = plugin_manager.get_formatters_grouped()
-        self.enabled_plugins = []
-        for group in groups:
-            for cls in available_plugins[group]:
-                p = cls(env=env, **kwargs)
-                if p.enabled:
-                    self.enabled_plugins.append(p)
+            fig = plt.figure('scikit-learn sample w/o replacement benchmark results')
+    plt.title('n_population = %s, n_times = %s' %
+              (opts.n_population, opts.n_times))
+    ax = fig.add_subplot(111)
+    for name in sampling_algorithm:
+        ax.plot(ratio, time[name], label=name)
     
+        dim = start_dim
+    for i in range(0, n):
+        print('============================================')
+        print('Entering iteration %s of %s' % (i, n))
+        print('============================================')
+        dim += step
+        X = np.random.randn(100, dim)
+        Y = np.random.randint(0, n_classes, (100,))
+        bench_scikit_tree_classifier(X, Y)
+        Y = np.random.randn(100)
+        bench_scikit_tree_regressor(X, Y)
     
-def has_docutils():
+    URL = ('http://people.csail.mit.edu/jrennie/'
+       '20Newsgroups/20news-bydate.tar.gz')
+    
+        # TASK: Build a vectorizer / classifier pipeline that filters out tokens
+    # that are too rare or too frequent
+    pipeline = Pipeline([
+        ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
+        ('clf', LinearSVC(C=1000)),
+    ])
+    
+    import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+    
     try:
-        # noinspection PyUnresolvedReferences
-        import docutils
-        return True
-    except ImportError:
-        return False
+    from urllib.parse import unquote
+except ImportError:
+    # Python 2.
+    from urllib import unquote
     
     
-def test_follow_all_redirects_shown(httpbin):
-    r = http('--follow', '--all', httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 3
-    assert r.count('HTTP/1.1 302 FOUND', 2)
-    assert HTTP_OK in r
-    
-    # (c) 2015, Marc Abramowitz <marca@surveymonkey.com>
-#
-# This file is part of Ansible.
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible. If not, see <http://www.gnu.org/licenses/>.
-    
-    # (c) 2016 Red Hat, Inc.
-#
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-'''CLI tool for starting new Shippable CI runs.'''
-    
-        def on_open_shell(self):
-        try:
-            for cmd in (b'set terminal length 0', b'set terminal width 512'):
-                self._exec_cli_command(cmd)
-            self._exec_cli_command(b'set terminal length %d' % self.terminal_length)
-        except AnsibleConnectionFailure:
-            raise AnsibleConnectionFailure('unable to set terminal parameters')
-
-    
-    
-if __name__ == '__main__':
-    HitCounts.run()
+class CollectHandler(web.RequestHandler):
+    @gen.coroutine
+    def get(self):
+        self.write('Collected: {}\n'.format(gc.collect()))
+        self.write('Garbage: {}\n'.format(len(gc.garbage)))
+        for circular in find_circular_references():
+            print('\n==========\n Circular \n==========')
+            for item in circular:
+                print('    ', repr(item))
+            for item in circular:
+                if isinstance(item, types.FrameType):
+                    print('\nLocals:', item.f_locals)
+                    print('\nTraceback:', repr(item))
+                    traceback.print_stack(item)
