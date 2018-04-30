7c65b7465761f36f007ea8e87b6157d2050a4152
SUNsung <weatherius@gmail.com>
2018-05-01 00:29:25 +0300

    Rewrite [30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb] File in: 2018-05-01 00:29:25

--- ruby/30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb
+++ ruby/30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb
@@ -1,135 +1,69 @@
 
         
-              https://pip.readthedocs.org/en/stable/installing/#install-pip
-    EOS
-  when 'pil' then <<-EOS.undent
-    Instead of PIL, consider `pip install pillow` or `brew install Homebrew/python/pillow`.
-    EOS
-  when 'macruby' then <<-EOS.undent
-    MacRuby works better when you install their package:
-      http://www.macruby.org/
-    EOS
-  when /(lib)?lzma/
-    'lzma is now part of the xz formula.'
-  when 'xcode'
-    if MacOS.version >= :lion
-      <<-EOS.undent
-      Xcode can be installed from the App Store.
-      EOS
+            if ARGV.named.empty?
+      slow_checks = %w[
+        check_for_broken_symlinks
+        check_missing_deps
+        check_for_outdated_homebrew
+        check_for_linked_keg_only_brews
+      ]
+      methods = (checks.all.sort - slow_checks) + slow_checks
     else
-      <<-EOS.undent
-      Xcode can be installed from https://developer.apple.com/xcode/downloads/
-      EOS
+      methods = ARGV.named
     end
-  when 'gtest', 'googletest', 'google-test' then <<-EOS.undent
-    Installing gtest system-wide is not recommended; it should be vendored
-    in your projects that use it.
-    EOS
-  when 'gmock', 'googlemock', 'google-mock' then <<-EOS.undent
-    Installing gmock system-wide is not recommended; it should be vendored
-    in your projects that use it.
-    EOS
-  when 'sshpass' then <<-EOS.undent
-    We won't add sshpass because it makes it too easy for novice SSH users to
-    ruin SSH's security.
-    EOS
-  when 'gsutil' then <<-EOS.undent
-    Install gsutil with `pip install gsutil`
-    EOS
-  when 'clojure' then <<-EOS.undent
-    Clojure isn't really a program but a library managed as part of a
-    project and Leiningen is the user interface to that library.
     
-      def std?
-    @settings.include? :std
+        @services = current_user.services.reorder(table_sort).page(params[:page])
+    
+        respond_to do |format|
+      if @user_credential.save
+        format.html { redirect_to user_credentials_path, notice: 'Your credential was successfully created.' }
+        format.json { render json: @user_credential, status: :created, location: @user_credential }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user_credential.errors, status: :unprocessable_entity }
+      end
+    end
   end
     
-      # True if a {Formula} is being built in C++11 mode.
-  def cxx11?
-    include?('c++11') && option_defined?('c++11')
+        assert_equal('two', h1.delete(2))
+    assert_equal(@cls[], h1)
+    
+    describe :string_unpack_float_le, shared: true do
+  it 'decodes one float for a single format character' do
+    '\x8f\xc2\xb5?'.unpack(unpack_format).should == [1.4199999570846558]
   end
     
-        if ARGV.include?('--pinned') || ARGV.include?('--versions')
-      filtered_list
-    elsif ARGV.named.empty?
-      if ARGV.include? '--full-name'
-        full_names = Formula.installed.map(&:full_name).sort do |a, b|
-          if a.include?('/') && !b.include?('/')
-            1
-          elsif !a.include?('/') && b.include?('/')
-            -1
-          else
-            a <=> b
+    describe :string_unpack_unicode, shared: true do
+  it 'decodes Unicode codepoints as ASCII values' do
+    [ ['\x00',      [0]],
+      ['\x01',      [1]],
+      ['\x08',      [8]],
+      ['\x0f',      [15]],
+      ['\x18',      [24]],
+      ['\x1f',      [31]],
+      ['\x7f',      [127]],
+      ['\xc2\x80',  [128]],
+      ['\xc2\x81',  [129]],
+      ['\xc3\xbf',  [255]]
+    ].should be_computed_by(:unpack, 'U')
+  end
+    
+            # Remove it form the session objects so freeup
+        sessions.delete(s[:session])
+    
+    # Sniffer class for GET URL's
+class SnifferURL < BaseProtocolParser
+  def register_sigs
+    self.sigs = {
+      :get		=> /^GET\s+([^\n]+)\s+HTTP\/\d\.\d/i,
+      :webhost	=> /^HOST\:\s+([^\n\r]+)/i,
+    }
+  end
+    
+              def plugins
+            @plugins ||= find_plugins_gem_specs.map do |spec|
+              { :name => spec.name, :version => spec.version.to_s }
+            end.sort_by do |spec|
+              spec[:name]
+            end
           end
-        end
-        puts_columns full_names
-      else
-        ENV['CLICOLOR'] = nil
-        exec 'ls', *ARGV.options_only << HOMEBREW_CELLAR
-      end
-    elsif ARGV.verbose? || !$stdout.tty?
-      exec 'find', *ARGV.kegs.map(&:to_s) + %w[-not -type d -print]
-    else
-      ARGV.kegs.each { |keg| PrettyListing.new keg }
-    end
-  end
-    
-        @report = Hash.new { |h, k| h[k] = [] }
-    return @report unless updated?
-    
-    class Ray
-  def initialize(org, dir)
-    @org = org
-    @dir = dir
-  end
-    
-        # Returns a String describing the file's content type
-    def detect
-      if blank_name?
-        SENSIBLE_DEFAULT
-      elsif empty_file?
-        EMPTY_TYPE
-      elsif calculated_type_matches.any?
-        calculated_type_matches.first
-      else
-        type_from_file_contents || SENSIBLE_DEFAULT
-      end.to_s
-    end
-    
-        # Returns the pluralized form of the attachment name. e.g.
-    # 'avatars' for an attachment of :avatar
-    def attachment attachment, style_name
-      plural_cache.pluralize_symbol(attachment.name)
-    end
-    
-          class HaveAttachedFileMatcher
-        def initialize attachment_name
-          @attachment_name = attachment_name
-        end
-    
-            def lower_than_low?
-          @low.nil? || !passes_validation_with_size(@low - 1)
-        end
-    
-          if app.config.respond_to?(:paperclip_defaults)
-        Paperclip::Attachment.default_options.merge!(app.config.paperclip_defaults)
-      end
-    end
-    
-          expect('.border-style-alternate').to have_rule(rule)
-    end
-  end
-    
-      context 'called with null values' do
-    it 'writes rules for other three' do
-      ruleset = 'margin-top: 11px; ' +
-                'margin-right: 12px; ' +
-                'margin-left: 13px;'
-      bad_rule = 'margin-bottom: null;'
-    
-      context 'called with arguments (2, $value: 4em 6em)' do
-    it 'outputs sextuple the second value from the default scale' do
-      expect('.two-double-value').to have_rule('font-size: 3.125em')
-    end
-  end
-end
