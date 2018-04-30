30afcae60d94f45086d6c6fcc3d9801803743535
SUNsung <weatherius@gmail.com>
2018-04-30 03:22:23 +0300

    Rewrite [11K1vG63mYufg119k2mckGD8Nc11OVUxJP25mso11RVD.py] File in: 2018-04-30 03:22:23

--- pyton/11K1vG63mYufg119k2mckGD8Nc11OVUxJP25mso11RVD.py
+++ pyton/11K1vG63mYufg119k2mckGD8Nc11OVUxJP25mso11RVD.py
@@ -1,47 +1,146 @@
 
         
-        
-def side_effect(old_cmd, command):
-    with tarfile.TarFile(_tar_file(old_cmd.script_parts)[0]) as archive:
-        for file in archive.getnames():
-            try:
-                os.remove(file)
-            except OSError:
-                # does not try to remove directories as we cannot know if they
-                # already existed before
-                pass
-
-    
-    containers = (('thefuck/python3-tcsh',
-               u'''FROM python:3
-                   RUN apt-get update
-                   RUN apt-get install -yy tcsh''',
-               u'tcsh'),
-              ('thefuck/python2-tcsh',
-               u'''FROM python:2
-                   RUN apt-get update
-                   RUN apt-get install -yy tcsh''',
-               u'tcsh'))
+                def get_auth(self, username=None, password=None):
+            assert username is None
+            assert password is None
+            assert self.raw_auth == BASIC_AUTH_HEADER_VALUE
+            return basic_auth(self.raw_auth)
     
     
-class Command(ScrapyCommand):
+httpie_info = packages.pop('httpie')
+print('''
+  url '{url}'
+  sha256 '{sha256}'
+'''.format(**httpie_info))
     
-            e.g.:
-        @returns request
-        @returns request 2
-        @returns request 2 10
-        @returns request 0 10
+            return '\r\n'.join(headers)
+    
+    
+FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
+FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
+JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
+BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
+    
+    
+def humanize_bytes(n, precision=2):
+    # Author: Doug Latornell
+    # Licence: MIT
+    # URL: http://code.activestate.com/recipes/577081/
+    '''Return a humanized string representation of a number of bytes.
+    
     '''
+__version__ = '1.0.0-dev'
+__author__ = 'Jakub Roztocil'
+__licence__ = 'BSD'
     
-                f = _base.Future()
-            w = _WorkItem(f, fn, args, kwargs)
+            x = np.random.random((4, 3, 2, 1))
+        out1, out2 = model.predict(x)
+        assert out1.shape == (4, 3, 2, 1)
+        assert out2.shape == (4, 3, 2, 1)
+        assert_allclose(out1, x * 0.2, atol=1e-4)
+        assert_allclose(out2, x * 0.3, atol=1e-4)
     
+    from keras.utils.test_utils import layer_test
+from keras.utils.test_utils import keras_test
+from keras.layers import local
+    
+    model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+    
+    
+@pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
+def test_zero(tensor_shape):
+    _runner(initializers.zeros(), tensor_shape,
+            target_mean=0., target_max=0.)
+    
+    from __future__ import print_function
+from functools import reduce
+import re
+import tarfile
+    
+    input_characters = sorted(list(input_characters))
+target_characters = sorted(list(target_characters))
+num_encoder_tokens = len(input_characters)
+num_decoder_tokens = len(target_characters)
+max_encoder_seq_length = max([len(txt) for txt in input_texts])
+max_decoder_seq_length = max([len(txt) for txt in target_texts])
+    
+        # upsample to (14, 14, ...)
+    cnn.add(Conv2DTranspose(96, 5, strides=2, padding='same',
+                            activation='relu',
+                            kernel_initializer='glorot_normal'))
+    cnn.add(BatchNormalization())
+    
+        # Note: you have to use shuffle='batch' or False with HDF5Matrix
+    model.fit(X_train, y_train, batch_size=32, shuffle='batch', verbose=False)
+    # test that evalutation and prediction don't crash and return reasonable results
+    out_pred = model.predict(X_test, batch_size=32, verbose=False)
+    out_eval = model.evaluate(X_test, y_test, batch_size=32, verbose=False)
+    
+    
+print('\n(0) building teacher model ...')
+teacher_model = make_teacher_model(x_train, y_train,
+                                   x_test, y_test,
+                                   epochs=epochs)
+    
+    
+@keras_test
+def test_min_max_norm():
+    array = get_example_array()
+    for m in get_test_values():
+        norm_instance = constraints.min_max_norm(min_value=m, max_value=m * 2)
+        normed = norm_instance(K.variable(array))
+        value = K.eval(normed)
+        l2 = np.sqrt(np.sum(np.square(value), axis=0))
+        assert not l2[l2 < m]
+        assert not l2[l2 > m * 2 + 1e-5]
+    
+    from mrjob.job import MRJob
+    
+        def approve_friend_request(self, from_user_id, to_user_id):
+        pass
+    
+    
+class LinkedList(object):
+    
+            (2016-01, shopping), 25
+        (2016-01, shopping), 100
+        (2016-01, gas), 50
+        '''
+        timestamp, seller, amount = line.split('\t')
+        period = self. extract_year_month(timestamp)
+        if period == self.current_year_month():
+            yield (period, category), amount
+    
+    
+class SalesRanker(MRJob):
+    
+    PROJ_METADATA = '%s.json' % PROJ_NAME
+    
+        # Get options and arguments.
     try:
-    import queue
-except ImportError:
-    import Queue as queue
+        opts, args = getopt.getopt(sys.argv[1:], _short_options, _options)
+    except getopt.GetoptError as e:
+        log.wtf('''
+    [Fatal] {}.
+    Try '{} --help' for more options.'''.format(e, script_name))
+    
+    	type, ext, size = url_info(url)
+	print_info(site_info, title, type, size)
+	
+	if not info_only:
+		download_urls([url], title, ext, size, output_dir, merge = merge)
+    
+        def prepare(self, **kwargs):
+        content = get_content(self.url)
     
     
-  def Response( self ):
-    if self._cached_response:
-      return self._cached_response
+#----------------------------------------------------------------------
+def showroom_download(url, output_dir = '.', merge = False, info_only = False, **kwargs):
+    ''''''
+    if re.match( r'(\w+)://www.showroom-live.com/([-\w]+)', url):
+        room_url_key = match1(url, r'\w+://www.showroom-live.com/([-\w]+)')
+        room_id = showroom_get_roomid_by_room_url_key(room_url_key)
+        showroom_download_by_room_id(room_id, output_dir, merge,
+                                    info_only)
