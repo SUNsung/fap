770de739c5515193e826656b7a2a37e6c836d755
SUNsung <weatherius@gmail.com>
2018-04-30 17:34:51 +0300

    Rewrite [33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py] File in: 2018-04-30 17:34:50

--- pyton/33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py
+++ pyton/33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py
@@ -1,84 +1,171 @@
 
         
-        entries = []
-for v in versions:
-    fields = v.split('.')
-    year, month, day = map(int, fields[:3])
-    faked = 0
-    patchlevel = 0
-    while True:
-        try:
-            datetime.date(year, month, day)
-        except ValueError:
-            day -= 1
-            faked += 1
-            assert day > 0
-            continue
-        break
-    if len(fields) >= 4:
-        try:
-            patchlevel = int(fields[3])
-        except ValueError:
-            patchlevel = 1
-    timestamp = '%04d-%02d-%02dT00:%02d:%02dZ' % (year, month, day, faked, patchlevel)
+        from youtube_dl.swfinterp import SWFInterpreter
     
-    IGNORED_FILES = [
-    'setup.py',  # http://bugs.python.org/issue13943
-    'conf.py',
-    'buildserver.py',
-]
     
-            retval = subprocess.call(args)
-        if retval == 0:
-            fsize = os.path.getsize(encodeFilename(tmpfilename))
-            self.to_screen('\r[%s] %s bytes' % (args[0], fsize))
-            self.try_rename(tmpfilename, filename)
-            self._hook_progress({
-                'downloaded_bytes': fsize,
-                'total_bytes': fsize,
-                'filename': filename,
-                'status': 'finished',
-            })
-            return True
+class AudiomackIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)?audiomack\.com/song/(?P<id>[\w/-]+)'
+    IE_NAME = 'audiomack'
+    _TESTS = [
+        # hosted on audiomack
+        {
+            'url': 'http://www.audiomack.com/song/roosh-williams/extraordinary',
+            'info_dict':
+            {
+                'id': '310086',
+                'ext': 'mp3',
+                'uploader': 'Roosh Williams',
+                'title': 'Extraordinary'
+            }
+        },
+        # audiomack wrapper around soundcloud song
+        {
+            'add_ie': ['Soundcloud'],
+            'url': 'http://www.audiomack.com/song/hip-hop-daily/black-mamba-freestyle',
+            'info_dict': {
+                'id': '258901379',
+                'ext': 'mp3',
+                'description': 'mamba day freestyle for the legend Kobe Bryant ',
+                'title': 'Black Mamba Freestyle [Prod. By Danny Wolf]',
+                'uploader': 'ILOVEMAKONNEN',
+                'upload_date': '20160414',
+            }
+        },
+    ]
+    
+            runtime = self._search_regex(
+            r'Runtime\s*:\s*(.+?) \|', webpage, 'duration', default=None)
+        if runtime:
+            runtime = re.sub(r'[\s-]', '', runtime)
+        duration = parse_duration(runtime)
+        view_count = int_or_none(self._search_regex(
+            r'Views\s*:\s*(\d+)', webpage, 'view count', default=None))
+        comment_count = int_or_none(self._search_regex(
+            r'Comments\s*:\s*(\d+)', webpage, 'comment count', default=None))
+    
+    
+class ClipRsIE(OnetBaseIE):
+    _VALID_URL = r'https?://(?:www\.)?clip\.rs/(?P<id>[^/]+)/\d+'
+    _TEST = {
+        'url': 'http://www.clip.rs/premijera-frajle-predstavljaju-novi-spot-za-pesmu-moli-me-moli/3732',
+        'md5': 'c412d57815ba07b56f9edc7b5d6a14e5',
+        'info_dict': {
+            'id': '1488842.1399140381',
+            'ext': 'mp4',
+            'title': 'PREMIJERA Frajle predstavljaju novi spot za pesmu Moli me, moli',
+            'description': 'md5:56ce2c3b4ab31c5a2e0b17cb9a453026',
+            'duration': 229,
+            'timestamp': 1459850243,
+            'upload_date': '20160405',
+        }
+    }
+    
+    
+def _tar_file(cmd):
+    for c in cmd:
+        for ext in tar_extensions:
+            if c.endswith(ext):
+                return (c, c[0:len(c) - len(ext)])
+    
+    
+@pytest.mark.functional
+def test_select_command_with_arrows(proc, TIMEOUT):
+    select_command_with_arrows(proc, TIMEOUT)
+    
+    
+class HashTable(object):
+    
+        def insert_crawled_link(self, url, signature):
+        '''Add the given link to `crawled_links`.'''
+        pass
+    
+        def __init__(self, memory_cache, reverse_index_cluster):
+        self.memory_cache = memory_cache
+        self.reverse_index_cluster = reverse_index_cluster
+    
+        def test_bit_length(self):
+        tiny = 1e-10
+        for x in range(-65000, 65000):
+            k = x.bit_length()
+            # Check equivalence with Python version
+            self.assertEqual(k, len(bin(x).lstrip('-0b')))
+            # Behaviour as specified in the docs
+            if x != 0:
+                self.assertTrue(2**(k-1) <= abs(x) < 2**k)
+            else:
+                self.assertEqual(k, 0)
+            # Alternative definition: x.bit_length() == 1 + floor(log_2(x))
+            if x != 0:
+                # When x is an exact power of 2, numeric errors can
+                # cause floor(log(x)/log(2)) to be one too small; for
+                # small x this can be fixed by adding a small quantity
+                # to the quotient before taking the floor.
+                self.assertEqual(k, 1 + math.floor(
+                        math.log(abs(x))/math.log(2) + tiny))
+    
+    
+
+# generic class
+class ColorDB:
+    def __init__(self, fp):
+        lineno = 2
+        self.__name = fp.name
+        # Maintain several dictionaries for indexing into the color database.
+        # Note that while Tk supports RGB intensities of 4, 8, 12, or 16 bits,
+        # for now we only support 8 bit intensities.  At least on OpenWindows,
+        # all intensities in the /usr/openwin/lib/rgb.txt file are 8-bit
+        #
+        # key is (red, green, blue) tuple, value is (name, [aliases])
+        self.__byrgb = {}
+        # key is name, value is (red, green, blue)
+        self.__byname = {}
+        # all unique names (non-aliases).  built-on demand
+        self.__allnames = None
+        for line in fp:
+            # get this compiled regular expression from derived class
+            mo = self._re.match(line)
+            if not mo:
+                print('Error in', fp.name, ' line', lineno, file=sys.stderr)
+                lineno += 1
+                continue
+            # extract the red, green, blue, and name
+            red, green, blue = self._extractrgb(mo)
+            name = self._extractname(mo)
+            keyname = name.lower()
+            # BAW: for now the `name' is just the first named color with the
+            # rgb values we find.  Later, we might want to make the two word
+            # version the `name', or the CapitalizedVersion, etc.
+            key = (red, green, blue)
+            foundname, aliases = self.__byrgb.get(key, (name, []))
+            if foundname != name and foundname not in aliases:
+                aliases.append(name)
+            self.__byrgb[key] = (foundname, aliases)
+            # add to byname lookup
+            self.__byname[keyname] = key
+            lineno = lineno + 1
+    
+        def is_rpc_path_valid(self):
+        if self.rpc_paths:
+            return self.path in self.rpc_paths
         else:
-            self.to_stderr('\n')
-            self.report_error('%s exited with code %d' % (args[0], retval))
-            return False
-
+            # If .rpc_paths is empty, just assume all paths are legal
+            return True
     
-    In the second benchmark, we increase the number of dimensions of the
-training set. Then we plot the computation time as function of
-the number of dimensions.
+            self.assertRaises(TypeError, range, 0.0, 2, 1)
+        self.assertRaises(TypeError, range, 1, 2.0, 1)
+        self.assertRaises(TypeError, range, 1, 2, 1.0)
+        self.assertRaises(TypeError, range, 1e100, 1e101, 1e101)
     
-                gc.collect()
-            print('- benchmarking LassoLars')
-            clf = LassoLars(alpha=alpha, fit_intercept=False,
-                            normalize=False, precompute=precompute)
-            tstart = time()
-            clf.fit(X, Y)
-            lars_lasso_results.append(time() - tstart)
-    
-    from sklearn.cluster import AgglomerativeClustering
-    
-        short_text_lang_folder = os.path.join(short_text_folder, lang)
-    if not os.path.exists(short_text_lang_folder):
-        os.makedirs(short_text_lang_folder)
-    
-        print('Decompressing %s' % ARCHIVE_NAME)
-    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
-        archive.extractall(path='.')
-    os.remove(ARCHIVE_NAME)
-
-    
-    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
-TRAIN_FOLDER = '20news-bydate-train'
-TEST_FOLDER = '20news-bydate-test'
-    
-    # Train uncalibrated random forest classifier on whole train and validation
-# data and evaluate on test data
-clf = RandomForestClassifier(n_estimators=25)
-clf.fit(X_train_valid, y_train_valid)
-clf_probs = clf.predict_proba(X_test)
-score = log_loss(y_test, clf_probs)
-    
-    n_classifiers = len(classifiers)
+            if sys.maxsize == 0x7fffffff:
+            # This test can currently only work on 32-bit machines.
+            # XXX If/when PySequence_Length() returns a ssize_t, it should be
+            # XXX re-enabled.
+            # Verify clearing of bug #556025.
+            # This assumes that the max data size (sys.maxint) == max
+            # address size this also assumes that the address size is at
+            # least 4 bytes with 8 byte addresses, the bug is not well
+            # tested
+            #
+            # Note: This test is expected to SEGV under Cygwin 1.3.12 or
+            # earlier due to a newlib bug.  See the following mailing list
+            # thread for the details:
