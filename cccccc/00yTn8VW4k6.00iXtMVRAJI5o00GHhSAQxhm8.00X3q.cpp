ea27c157ae1fae2ef1efc7103e7deafadd55949d
SUNsung <weatherius@gmail.com>
2018-05-02 02:52:30 +0300

    Rewrite [00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp] File in: 2018-05-02 02:52:29

--- cccccc/00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp
+++ cccccc/00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp
@@ -1,82 +1,180 @@
 
         
-        #ifndef CONTENT_NW_SRC_API_APP_APP_H_
-#define CONTENT_NW_SRC_API_APP_APP_H_
+        // Mappings and Sequences of descriptors.
+// They implement containers like fields_by_name, EnumDescriptor.values...
+// See descriptor_containers.cc for more description.
+#include <Python.h>
     
-    Base::Base(int id,
-           const base::WeakPtr<ObjectManager>& object_manager,
-           const base::DictionaryValue& option,
-	   const std::string& extension_id)
-    : extension_id_(extension_id),
-      id_(id),
-      delay_destruction_(false),
-      pending_destruction_(false),
-      object_manager_(object_manager) {
+    TEST(CSharpEnumValue, PascalCasedPrefixStripping) {
+  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'BAR'));
+  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'BAR_BAZ'));
+  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'FOO_BAR'));
+  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'FOO__BAR'));
+  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'FOO_BAR_BAZ'));
+  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'Foo_BarBaz'));
+  EXPECT_EQ('Bar', GetEnumValueName('FO_O', 'FOO_BAR'));
+  EXPECT_EQ('Bar', GetEnumValueName('FOO', 'F_O_O_BAR'));
+  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'BAR'));
+  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'BAR_BAZ'));
+  EXPECT_EQ('Foo', GetEnumValueName('Foo', 'FOO'));
+  EXPECT_EQ('Foo', GetEnumValueName('Foo', 'FOO___'));
+  // Identifiers can't start with digits
+  EXPECT_EQ('_2Bar', GetEnumValueName('Foo', 'FOO_2_BAR'));
+  EXPECT_EQ('_2', GetEnumValueName('Foo', 'FOO___2'));
 }
     
-    v8::Handle<v8::Value> AllocateId(int routing_id);
+      std::string class_access_level();
+  const Options* options();
     
-    void Menu::Append(MenuItem* menu_item) {
-  menu_items.push_back(menu_item);
-  if (GTK_IS_ACCEL_GROUP(gtk_accel_group)){
-    menu_item->UpdateKeys(gtk_accel_group);
-  }
-  gtk_menu_shell_append(GTK_MENU_SHELL(menu_), menu_item->menu_item_);
-}
+    void WriteMessageDocComment(io::Printer* printer, const Descriptor* message);
+void WriteFieldDocComment(io::Printer* printer, const FieldDescriptor* field);
+void WriteEnumDocComment(io::Printer* printer, const EnumDescriptor* enum_);
+void WriteEnumValueDocComment(io::Printer* printer,
+                              const EnumValueDescriptor* value);
+void WriteServiceDocComment(io::Printer* printer,
+                            const ServiceDescriptor* service);
+void WriteMethodDocComment(io::Printer* printer,
+                           const MethodDescriptor* method);
     
-    void Menu::Create(const base::DictionaryValue& option) {
-  is_menu_modified_ = true;
-  menu_delegate_.reset(new MenuDelegate(object_manager()));
-  menu_model_.reset(new ui::NwMenuModel(menu_delegate_.get()));
+    namespace google {
+namespace protobuf {
+namespace compiler {
+namespace java {
+    }
+    }
+    }
     }
     
-    void MenuItem::Destroy() {
-  gtk_widget_destroy(menu_item_);
-  g_object_unref(G_OBJECT(menu_item_));
-}
+            // Get model's doc string.
+        // Return null pointer if not specified.
+        const std::string& DocString() const;
+        // Set models' doc string.
+        void SetDocString(const std::string& p_docString);
     
-    class NwClipboardClearSyncFunction : public NWSyncExtensionFunction {
- public:
-  NwClipboardClearSyncFunction();
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
+                // Get formal parameter name.
+            const std::string& GetName() const;
+    
+        REGISTER_OPERATOR_SCHEMA(ArrayFeatureExtractor)
+        .SetDomain(c_mlDomain)
+        .Input('X', 'Data to be selected from', 'T1')
+        .Input('Y', 'Data to be selected from', 'T2')
+        .Output('Z', 'Selected data as an array', 'T1')
+        .Description(R'DOC(
+            Select a subset of the data from input1 based on the indices provided in input2.
+            )DOC')
+        .TypeConstraint('T1', { 'tensor(float)', 'tensor(double)', 'tensor(int64)', 'tensor(int32)', 'tensor(string)' }, ' allowed types.')
+        .TypeConstraint('T2', { 'tensor(int64)' }, ' Index value types .');
+    
+        deque<UtteranceDescription> utterances;
+    size_t totalNumberOfFrames = 0;
+    std::unordered_map<size_t, std::vector<string>> duplicates;
+    {
+        std::unordered_set<size_t> uniqueIds;
+        string line, key;
+        while (getline(scp, line))
+        {
+            config.AdjustUtterancePath(rootPath, scpDir, line);
+            key.clear();
+    }
     }
     
-    // For desktop capture APIs
-#include 'base/base64.h'
-#include 'base/strings/string16.h'
-#include 'base/strings/utf_string_conversions.h'
-#include 'chrome/browser/media/webrtc/desktop_media_list_observer.h'
-#include 'chrome/browser/media/webrtc/desktop_streams_registry.h'
-#include 'chrome/browser/media/webrtc/media_capture_devices_dispatcher.h'
-#include 'chrome/browser/media/webrtc/native_desktop_media_list.h'
-#include 'content/public/browser/render_frame_host.h'
-#include 'content/public/browser/render_process_host.h'
-#include 'content/public/browser/web_contents.h'
-#include 'third_party/webrtc/modules/desktop_capture/desktop_capture_options.h'
-#include 'third_party/webrtc/modules/desktop_capture/desktop_capturer.h'
-#include 'ui/gfx/codec/png_codec.h'
-#include 'ui/gfx/image/image.h'
-#include 'ui/gfx/image/image_skia.h'
+        // Let's wait till the previous memcopy has finished.
+    if (m_dataTransferers[currentDataTransferIndex])
+        m_dataTransferers[currentDataTransferIndex]->WaitForCopyCPUToGPU();
     
-        if (LIKELY(!(flags() & kFlagMaybeShared))) {
-      return false;
+        static void dotprod(const_array_ref<float> a, const_array_ref<float> b, float &result,
+                        bool addtoresult, const float thisscale, const float weight)
+    {
+        assert(a.size() == b.size());
+        assert((15 & reinterpret_cast<uintptr_t>(&a[0])) == 0);
+        assert((15 & reinterpret_cast<uintptr_t>(&b[0])) == 0); // enforce SSE alignment
     }
     
-    //  GCC 4.9 with ASAN has a problem: a function with no_sanitize_address calling
-//  a function with always_inline fails to build. The _mm_* functions are marked
-//  always_inline.
-//  https://gcc.gnu.org/bugzilla/show_bug.cgi?id=67368
-#if defined FOLLY_SANITIZE_ADDRESS && FOLLY_SANITIZE_ADDRESS == 1 && \
-    __GNUC_PREREQ(4, 9)
-#define _mm_load_si128(p) (*(p))
-#define _mm_loadu_si128(p) ((__m128i)__builtin_ia32_loaddqu((const char*)(p)))
-#ifdef _mm_cmpestri
-#undef _mm_cmpestri
-#endif
-#define _mm_cmpestri(a, b, c, d, e) \
-  __builtin_ia32_pcmpestri128((__v16qi)(a), b, (__v16qi)(c), d, e)
-#endif
+        if (toName == L'')
+        toName = fromName;
     
-    template <typename Fn>
-using ArgumentTypes =
-  typename ArgumentTypesByKind<IdentifyCallable::getKind<Fn>(), Fn>::type;
+    // Use if you want to reset your rendering device without losing ImGui state.
+IMGUI_API void        ImGui_ImplDX10_InvalidateDeviceObjects();
+IMGUI_API bool        ImGui_ImplDX10_CreateDeviceObjects();
+    
+    // Use if you want to reset your rendering device without losing ImGui state.
+IMGUI_API void        ImGui_ImplGlfwGL3_InvalidateDeviceObjects();
+IMGUI_API bool        ImGui_ImplGlfwGL3_CreateDeviceObjects();
+    
+        // Setup display size (every frame to accommodate for window resizing)
+    RECT rect;
+    GetClientRect(g_hWnd, &rect);
+    io.DisplaySize = ImVec2((float)(rect.right - rect.left), (float)(rect.bottom - rect.top));
+    
+        for (int i = 0; i < num_frames_in_flight; i++)
+    {
+        g_pFrameResources[i].IB = NULL;
+        g_pFrameResources[i].VB = NULL;
+        g_pFrameResources[i].VertexBufferSize = 5000;
+        g_pFrameResources[i].IndexBufferSize = 10000;
+    }
+    
+    static bool ImGui_ImplDX9_CreateFontsTexture()
+{
+    // Build texture atlas
+    ImGuiIO& io = ImGui::GetIO();
+    unsigned char* pixels;
+    int width, height, bytes_per_pixel;
+    io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height, &bytes_per_pixel);
+    }
+    
+        // Recreate the VAO every time 
+    // (This is to easily allow multiple GL contexts. VAO are not shared among GL contexts, and we don't track creation/deletion of windows so we don't have an obvious key to use to cache them.)
+    GLuint vao_handle = 0;
+    glGenVertexArrays(1, &vao_handle);
+    glBindVertexArray(vao_handle);
+    glBindBuffer(GL_ARRAY_BUFFER, g_VboHandle);
+    glEnableVertexAttribArray(g_AttribLocationPosition);
+    glEnableVertexAttribArray(g_AttribLocationUV);
+    glEnableVertexAttribArray(g_AttribLocationColor);
+    glVertexAttribPointer(g_AttribLocationPosition, 2, GL_FLOAT, GL_FALSE, sizeof(ImDrawVert), (GLvoid*)IM_OFFSETOF(ImDrawVert, pos));
+    glVertexAttribPointer(g_AttribLocationUV, 2, GL_FLOAT, GL_FALSE, sizeof(ImDrawVert), (GLvoid*)IM_OFFSETOF(ImDrawVert, uv));
+    glVertexAttribPointer(g_AttribLocationColor, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(ImDrawVert), (GLvoid*)IM_OFFSETOF(ImDrawVert, col));
+    
+        if (install_callbacks)
+        ImGui_ImplGlfw_InstallCallbacks(window);
+    
+        ALLEGRO_MOUSE_STATE mouse;
+    if (keys.display == g_Display)
+    {
+        al_get_mouse_state(&mouse);
+        io.MousePos = ImVec2((float)mouse.x, (float)mouse.y);
+    }
+    else
+    {
+        io.MousePos = ImVec2(-FLT_MAX, -FLT_MAX);
+    }
+    
+    //---- Don't define obsolete functions/enums names. Consider enabling from time to time after updating to avoid using soon-to-be obsolete function/names
+//#define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
+    
+        // Main loop
+    bool running = true;
+    while (running)
+    {
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        ALLEGRO_EVENT ev;
+        while (al_get_next_event(queue, &ev))
+        {
+            ImGui_ImplA5_ProcessEvent(&ev);
+            if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) 
+                running = false;
+            if (ev.type == ALLEGRO_EVENT_DISPLAY_RESIZE)
+            {
+                ImGui_ImplA5_InvalidateDeviceObjects();
+                al_acknowledge_resize(display);
+                Imgui_ImplA5_CreateDeviceObjects();
+            }
+        }
+        ImGui_ImplA5_NewFrame();
+    }
+    
+        CreateRenderTarget();
