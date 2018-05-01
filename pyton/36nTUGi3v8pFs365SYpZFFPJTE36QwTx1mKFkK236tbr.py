0006f012919a79fcdee6118e00d6f31c53934e87
SUNsung <weatherius@gmail.com>
2018-05-01 13:25:01 +0300

    Rewrite [36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py] File in: 2018-05-01 13:25:01

--- pyton/36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py
+++ pyton/36nTUGi3v8pFs365SYpZFFPJTE36QwTx1mKFkK236tbr.py
@@ -1,120 +1,36 @@
 
         
-        
-if len(sys.argv) <= 1:
-    print('Specify the version number as parameter')
-    sys.exit()
-version = sys.argv[1]
-    
-    entry_template = textwrap.dedent('''
-    <entry>
-        <id>https://yt-dl.org/feed/youtube-dl-updates-feed/youtube-dl-@VERSION@</id>
-        <title>New version @VERSION@</title>
-        <link href='http://rg3.github.io/youtube-dl' />
-        <content type='xhtml'>
-            <div xmlns='http://www.w3.org/1999/xhtml'>
-                Downloads available at <a href='https://yt-dl.org/downloads/@VERSION@/'>https://yt-dl.org/downloads/@VERSION@/</a>
-            </div>
-        </content>
-        <author>
-            <name>The youtube-dl maintainers</name>
-        </author>
-        <updated>@TIMESTAMP@</updated>
-    </entry>
-    ''')
-    
-    if __name__ == '__main__':
-    unittest.main()
-
-    
-        class Plugin(AuthPlugin):
-        auth_type = 'test-require-false-yet-provided'
-        auth_require = False
+            (x_train, y_train), (x_test, y_test), (sample_weight, class_weight, test_ids) = _get_test_data()
     
     
-def get_info(package_name):
-    api_url = 'https://pypi.python.org/pypi/{}/json'.format(package_name)
-    resp = requests.get(api_url).json()
-    hasher = hashlib.sha256()
-    for release in resp['urls']:
-        download_url = release['url']
-        if download_url.endswith('.tar.gz'):
-            hasher.update(requests.get(download_url).content)
-            return {
-                'name': package_name,
-                'url': download_url,
-                'sha256': hasher.hexdigest(),
-            }
+def test_regression_build_fn():
+    reg = KerasRegressor(
+        build_fn=build_fn_reg, hidden_dims=hidden_dims,
+        batch_size=batch_size, epochs=epochs)
+    
+    
+@pytest.mark.parametrize('tensor_shape', [(100, 100), (1, 2, 3, 4)], ids=['FC', 'CONV'])
+def test_identity(tensor_shape):
+    if len(tensor_shape) > 2:
+        with pytest.raises(ValueError):
+            _runner(initializers.identity(), tensor_shape,
+                    target_mean=1. / tensor_shape[0], target_max=1.)
     else:
-        raise RuntimeError(
-            '{}: download not found: {}'.format(package_name, resp))
+        _runner(initializers.identity(), tensor_shape,
+                target_mean=1. / tensor_shape[0], target_max=1.)
     
     
-FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
-FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
-JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
-BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
+@keras_test
+def test_vector_regression():
+    '''
+    Perform float data prediction (regression) using 2 layer MLP
+    with tanh and sigmoid activations.
+    '''
+    (x_train, y_train), (x_test, y_test) = get_test_data(num_train=500,
+                                                         num_test=200,
+                                                         input_shape=(20,),
+                                                         output_shape=(num_classes,),
+                                                         classification=False)
     
-        exc = Timeout('Request timed out')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR_TIMEOUT
-    assert error_msg == 'Request timed out (30s).'
-
-    
-    
-def test_follow_all_redirects_shown(httpbin):
-    r = http('--follow', '--all', httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 3
-    assert r.count('HTTP/1.1 302 FOUND', 2)
-    assert HTTP_OK in r
-    
-        @property
-    def has_finished(self):
-        return self.time_finished is not None
-    
-    output_options.add_argument(
-    '--print', '-p',
-    dest='output_options',
-    metavar='WHAT',
-    help='''
-    String specifying what the output should contain:
-    
-        def test_cert_and_key(self, httpbin_secure):
-        r = http(httpbin_secure + '/get',
-                 '--cert', CLIENT_CERT,
-                 '--cert-key', CLIENT_KEY)
-        assert HTTP_OK in r
-    
-    from httpie import __version__
-from httpie.compat import is_windows
-    
-    
-SESSIONS_DIR_NAME = 'sessions'
-DEFAULT_SESSIONS_DIR = os.path.join(DEFAULT_CONFIG_DIR, SESSIONS_DIR_NAME)
-VALID_SESSION_NAME_PATTERN = re.compile('^[a-zA-Z0-9_.-]+$')
-# Request headers starting with these prefixes won't be stored in sessions.
-# They are specific to each request.
-# http://en.wikipedia.org/wiki/List_of_HTTP_header_fields#Requests
-SESSION_IGNORED_HEADER_PREFIXES = ['Content-', 'If-']
-    
-    # If true, the current module name will be prepended to all description
-# unit titles (such as .. function::).
-#add_module_names = True
-    
-        @mock.patch('certbot.notify.smtplib.LMTP')
-    @mock.patch('certbot.notify.subprocess.Popen')
-    def test_everything_fails(self, mock_popen, mock_lmtp):
-        from certbot.notify import notify
-        lmtp_obj = mock.MagicMock()
-        mock_lmtp.return_value = lmtp_obj
-        lmtp_obj.sendmail.side_effect = socket.error(17)
-        proc = mock.MagicMock()
-        mock_popen.return_value = proc
-        proc.communicate.side_effect = OSError('What we have here is a '
-                                               'failure to communicate.')
-        self.assertFalse(notify('Goose', 'auntrhody@example.com',
-                                'The old grey goose is dead.'))
-        self.assertEqual(lmtp_obj.sendmail.call_count, 1)
-        self.assertEqual(proc.communicate.call_count, 1)
+                # Size of the square
+            w = np.random.randint(2, 4)
