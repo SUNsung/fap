c3488b00d6495a263fbe77582fc1632a6f8e9e57
SUNsung <weatherius@gmail.com>
2018-05-01 15:16:27 +0300

    Rewrite [264QfYILhqLr626tSxsWDpzew26Gx5TWgyd2Mc26u9I.cpp] File in: 2018-05-01 15:16:26

--- cccccc/264QfYILhqLr626tSxsWDpzew26Gx5TWgyd2Mc26u9I.cpp
+++ cccccc/264QfYILhqLr626tSxsWDpzew26Gx5TWgyd2Mc26u9I.cpp
@@ -1,205 +1,140 @@
 
         
-        
-    {  // No-ops since the C binding does not support key shortening methods.
-  virtual void FindShortestSeparator(std::string*, const Slice&) const { }
-  virtual void FindShortSuccessor(std::string* key) const { }
-};
+          virtual bool lookupAdditions(DeclBaseName Name, DeclContext *DC,
+                               SourceLoc Loc, bool IsTypeLookup,
+                               ResultVector &RV) = 0;
     
-    // Return the name of the sstable with the specified number
-// in the db named by 'dbname'.  The result will be prefixed with
-// 'dbname'.
-extern std::string TableFileName(const std::string& dbname, uint64_t number);
+    using namespace swift;
     
-      fname = LogFileName('foo', 192);
-  ASSERT_EQ('foo/', std::string(fname.data(), 4));
-  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
-  ASSERT_EQ(192, number);
-  ASSERT_EQ(kLogFile, type);
+      llvm::SetVector<const clang::FileEntry *> fileDependencies;
+  // FIXME: This is not desirable because:
+  // 1. It picks shim header files as file dependencies
+  // 2. Having all the other swift files of the module as file dependencies ends
+  //   up making all of them associated with all the other files as main files.
+  //   It's better to associate each swift file with the unit that recorded it
+  //   as the main one.
+  // Keeping the code in case we want to revisit.
+#if 0
+  auto *module = primarySourceFile->getParentModule();
+  collectFileDependencies(fileDependencies, dependencyTracker, module, fileMgr);
+#endif
     
+    #endif // LLVM_SWIFT_AST_COMMENT_H
     
-    {  input.remove_prefix(kHeader);
-  Slice key, value;
-  int found = 0;
-  while (!input.empty()) {
-    found++;
-    char tag = input[0];
-    input.remove_prefix(1);
-    switch (tag) {
-      case kTypeValue:
-        if (GetLengthPrefixedSlice(&input, &key) &&
-            GetLengthPrefixedSlice(&input, &value)) {
-          handler->Put(key, value);
-        } else {
-          return Status::Corruption('bad WriteBatch Put');
-        }
-        break;
-      case kTypeDeletion:
-        if (GetLengthPrefixedSlice(&input, &key)) {
-          handler->Delete(key);
-        } else {
-          return Status::Corruption('bad WriteBatch Delete');
-        }
-        break;
-      default:
-        return Status::Corruption('unknown WriteBatch tag');
-    }
-  }
-  if (found != WriteBatchInternal::Count(this)) {
-    return Status::Corruption('WriteBatch has wrong count');
-  } else {
-    return Status::OK();
+      CodeBlock(StringRef LiteralContent, StringRef Language)
+      : MarkupASTNode(ASTNodeKind::CodeBlock),
+        LiteralContent(LiteralContent),
+        Language(Language) {}
+    
+    SILDebugScope::SILDebugScope(SILLocation Loc, SILFunction *SILFn,
+                             const SILDebugScope *ParentScope ,
+                             const SILDebugScope *InlinedCallSite)
+    : Loc(Loc), InlinedCallSite(InlinedCallSite) {
+  if (ParentScope)
+    Parent = ParentScope;
+  else {
+    assert(SILFn && 'no parent provided');
+    Parent = SILFn;
   }
 }
     
-      // Return a string that contains the copy of the referenced data.
-  std::string ToString() const { return std::string(data_, size_); }
+    #include 'swift/Syntax/Rewriter.h'
     
-            enum Type
-        {
-            // A main graph.
-            Main = 1,
-            // A sub graph (function).
-            Sub = 2,
-            // A graph with strict type checking.
-            Strict = 4,
-        };
     
-    namespace ONNXIR
-{
-    namespace Common
-    {
-        Status::Status(StatusCategory p_category, int p_code, const std::string& p_msg)
-        {
-            m_state.reset(new State());
-            m_state->m_category = p_category;
-            m_state->m_code = p_code;
-            m_state->m_msg = p_msg;
-        }
-    }
-    }
+    {} // end namespace swift
     
-    namespace ONNXIR
-{
-    namespace Utils
-    {
-        class TensorUtils
-        {
-        public:
-#define DEFINE_UNPACK_TENSOR(T, Type, fieldName, fieldSize)                                                                    \
-    static Common::Status UnpackTensor(const onnx::TensorProto& p_tensor, /*out*/T* p_data, int64_t p_expected_size)           \
-    {                                                                                                                          \
-        if (Type != p_tensor.data_type()                                                                                       \
-            || nullptr == p_data)                                                                                              \
-        {                                                                                                                      \
-            return Common::Status(Common::StatusCategory::ONNX, Common::StatusCode::INVALID_ARGUMENT);                        \
-        }                                                                                                                      \
-        if (p_tensor.has_raw_data())                                                                                           \
-        {                                                                                                                      \
-            if (p_tensor.raw_data().size() != (p_expected_size) * sizeof(T))                                                   \
-                return Common::Status(Common::StatusCategory::ONNX, Common::StatusCode::FAIL,                                 \
-                                               'UnpackTensor: the pre-allocate size does not match the raw data size');        \
-            UnpackTensorWithRawData(p_tensor, p_data);                                                                         \
-            return Common::Status::OK();                                                                                       \
-        }                                                                                                                      \
-        if (p_tensor.fieldSize() != p_expected_size)                                                                           \
-            return Common::Status(Common::StatusCategory::ONNX, Common::StatusCode::FAIL,                                     \
-                                            'UnpackTensor: the pre-allocate size does not match the size in proto');           \
-        for (auto elem : p_tensor.fieldName())                                                                                 \
-        {                                                                                                                      \
-            *p_data++ = static_cast<T>(elem);                                                                                  \
-        }                                                                                                                      \
-        return Common::Status::OK();                                                                                           \
-    }
-    }
-    }
+      IndexSymbol() = default;
+    
+    #endif
+    
+    //#include 'log.h'
+#include 'include_gunit.h'
+#include 'baseapi.h'
+#include 'leptonica/allheaders.h'
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <locale>
+#include <limits.h>
+#include <time.h>
+    
+    // dims=[5, 4, 3, 2]->[5, 3, 2, 4]
+TEST_F(MatrixTest, RotatingTranspose_1_3) {
+  GENERIC_2D_ARRAY<int> m;
+  src_.RotatingTranspose(dims_, kNumDims_, 1, 3, &m);
+  m.ResizeNoInit(kInputSize_ / 4, 4);
+  // Verify that the result is:
+  // output tensor=[[[[0, 6, 12, 18][1, 7, 13, 19]]
+  //                 [[2, 8, 14, 20][3, 9, 15, 21]]
+  //                 [[4, 10, 16, 22][5, 11, 17, 23]]]
+  //                [[[24, 30, 36, 42]...
+  EXPECT_EQ(0, m(0, 0));
+  EXPECT_EQ(6, m(0, 1));
+  EXPECT_EQ(1, m(1, 0));
+  EXPECT_EQ(2, m(2, 0));
+  EXPECT_EQ(3, m(3, 0));
+  EXPECT_EQ(4, m(4, 0));
+  EXPECT_EQ(5, m(5, 0));
+  EXPECT_EQ(24, m(6, 0));
+  EXPECT_EQ(30, m(6, 1));
+}
+    
+    // Holds a collection of other networks and forwards calls to each of them.
+class Plumbing : public Network {
+ public:
+  // ni_ and no_ will be set by AddToStack.
+  explicit Plumbing(const STRING& name);
+  virtual ~Plumbing();
     }
     
-        // Taken from RS4
-    REGISTER_OPERATOR_SCHEMA(Identity)
-        .Description('Identity takes one input data (Tensor<T>) and produces one '
-            'output data (Tensor<T>) where the function, y = x, is applied to the '
-            'tensor elementwise.')
-        .Input('input', 'input tensor', 'T')
-        .Output('output', 'output tensor', 'T')
-        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
-            'Constrain input and output types to float tensors.');
+    #ifndef TESS_CAPI_INCLUDE_BASEAPI
+#   define TESS_CAPI_INCLUDE_BASEAPI
+#endif
+#include 'capi.h'
+#include 'genericvector.h'
+#include 'strngs.h'
     
-        // Taken from ONNX
-    REGISTER_OPERATOR_SCHEMA(Ceil)
-        .Description('Ceil takes one input data (Tensor<T>) and produces one output data'
-            '(Tensor<T>) where the ceil is, y = ceil(x), is applied to'
-            'the tensor elementwise.')
-        .Input('input', 'Input tensor of any shape', 'T')
-        .Output('output', 'Output tensor of same shape and type as input X.', 'T')
-        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
-            'Constrain input and output types to float tensors.');
     
-    // Gets sequences for a particular chunk.
-// This information is used by the randomizer to fill in current windows of sequences.
-void HTKDeserializer::SequenceInfosForChunk(ChunkIdType chunkId, vector<SequenceInfo>& result)
-{
-    const HTKChunkInfo& chunk = m_chunks[chunkId];
-    result.reserve(m_frameMode ? chunk.GetTotalFrames() : chunk.GetNumberOfUtterances());
-    size_t offsetInChunk = 0;
-    for (size_t i = 0; i < chunk.GetNumberOfUtterances(); ++i)
-    {
-        auto utterance = chunk.GetUtterance(i);
-        // Currently we do not support common prefix, so simply assign the minor to the key.
-        size_t sequence = utterance->GetId();
-    }
-    }
+    {  return true;
+}
     
-        // Let's check that there is no outstanding copies.
-    // Wait on all events if there are any pending copy operations in flight.
-    if (m_dataTransferers[m_currentDataTransferIndex])
-        m_dataTransferers[m_currentDataTransferIndex]->WaitForCopyCPUToGPU();
+    // Number of outputs held in each register. 8 x 32 bit ints.
+constexpr int kNumOutputsPerRegister = 8;
+// Maximum number of registers that we will use.
+constexpr int kMaxOutputRegisters = 8;
+// Number of inputs in the inputs register.
+constexpr int kNumInputsPerRegister = 32;
+// Number of inputs in each weight group.
+constexpr int kNumInputsPerGroup = 4;
+// Number of groups of inputs to be broadcast.
+constexpr int kNumInputGroups = kNumInputsPerRegister / kNumInputsPerGroup;
     
-            // get data as long vectors
-        // ... why do I need to explicitly use operator T ()?
-        array_ref<msra::math::float4> us4(us.operator array_ref<msra::math::float4>());
-        const_array_ref<msra::math::float4> other4(other.operator const_array_ref<msra::math::float4>());
-        assert(us4.size() == other4.size());
+    #include 'intsimdmatrixsse.h'
     
-    struct ExceptionInfo {
-  const std::type_info* type{nullptr};
-  // The values in frames are IP (instruction pointer) addresses.
-  // They are only filled if the low-level exception tracer library is
-  // linked in or LD_PRELOADed.
-  std::vector<uintptr_t> frames; // front() is top of stack
-};
+    #include 'imgui.h'
+#include 'imgui_impl_marmalade.h'
     
-      /**
-   * Return true if all IOBufs in this chain are managed by the usual
-   * refcounting mechanism (and so the lifetime of the underlying memory
-   * can be extended by clone()).
-   */
-  bool isManaged() const {
-    const IOBuf* current = this;
-    while (true) {
-      if (!current->isManagedOne()) {
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you use this binding you'll need to call 4 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXXX_NewFrame(), ImGui::Render() and ImGui_ImplXXXX_Shutdown().
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+        // Upload texture to graphics system
+    g_FontTexture = NULL;
+    if (g_pd3dDevice->CreateTexture(width, height, 1, D3DUSAGE_DYNAMIC, D3DFMT_A8R8G8B8, D3DPOOL_DEFAULT, &g_FontTexture, NULL) < 0)
         return false;
-      }
-      current = current->next_;
-      if (current == this) {
-        return true;
-      }
-    }
-  }
+    D3DLOCKED_RECT tex_locked_rect;
+    if (g_FontTexture->LockRect(0, &tex_locked_rect, NULL, 0) != D3D_OK)
+        return false;
+    for (int y = 0; y < height; y++)
+        memcpy((unsigned char *)tex_locked_rect.pBits + tex_locked_rect.Pitch * y, pixels + (width * bytes_per_pixel) * y, (width * bytes_per_pixel));
+    g_FontTexture->UnlockRect(0);
     
-    #include <folly/Exception.h>
+        VkResult err;
     
-    [[noreturn]] void usage(const char* name) {
-  std::cerr << folly::format(
-      'Usage: {0}\n'
-      '         list all huge page sizes and their mount points\n'
-      '       {0} -cp <src_file> <dest_nameprefix>\n'
-      '         copy src_file to a huge page file\n',
-      name);
-  exit(1);
+    
+    {    // Build all fonts lookup tables
+    for (int i = 0; i < atlas->Fonts.Size; i++)
+        if (atlas->Fonts[i]->DirtyLookupTables)
+            atlas->Fonts[i]->BuildLookupTable();
 }
-    
-      /**
-   * Get the maximum buffer size for this AsyncFileWriter, in bytes.
-   */
-  size_t getMaxBufferSize() const;
