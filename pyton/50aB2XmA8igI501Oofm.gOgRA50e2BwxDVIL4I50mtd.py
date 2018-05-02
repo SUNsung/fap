8ccd6eff46eef3377ac25a698fe7cd3838a197b5
SUNsung <weatherius@gmail.com>
2018-05-02 18:44:04 +0300

    Rewrite [50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py] File in: 2018-05-02 18:44:03

--- pyton/50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py
+++ pyton/50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py
@@ -1,103 +1,167 @@
 
         
-                for chunk in self.msg.iter_body(self.CHUNK_SIZE):
-            if not converter and b'\0' in chunk:
-                converter = self.conversion.get_converter(self.mime)
-                if not converter:
-                    raise BinarySuppressedError()
-            body.extend(chunk)
+            def check(self, value):
+        return callable(getattr(value, '__html__', None))
     
-        exc = Timeout('Request timed out')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR_TIMEOUT
-    assert error_msg == 'Request timed out (30s).'
+            for blueprint in self.app.iter_blueprints():
+            loader = blueprint.jinja_loader
+            if loader is not None:
+                for template in loader.list_templates():
+                    result.add(template)
+    
+        with open(filename, 'w') as f:
+        f.write(contents)
+    
+    
+def test_config_from_envvar():
+    env = os.environ
+    try:
+        os.environ = {}
+        app = flask.Flask(__name__)
+        with pytest.raises(RuntimeError) as e:
+            app.config.from_envvar('FOO_SETTINGS')
+        assert ''FOO_SETTINGS' is not set' in str(e.value)
+        assert not app.config.from_envvar('FOO_SETTINGS', silent=True)
+    
+        with app.test_request_context():
+        assert buffer == []
+        try:
+            raise Exception('dummy')
+        except Exception:
+            pass
+    assert buffer == [None]
+    
+      def setUp(self):
+    FLAGS.train_data = os.path.join(self.get_temp_dir(), 'test-text.txt')
+    FLAGS.eval_data = os.path.join(self.get_temp_dir(), 'eval-text.txt')
+    FLAGS.save_path = self.get_temp_dir()
+    with open(FLAGS.train_data, 'w') as f:
+      f.write(
+          '''alice was beginning to get very tired of sitting by her sister on
+          the bank, and of having nothing to do: once or twice she had peeped
+          into the book her sister was reading, but it had no pictures or
+          conversations in it, 'and what is the use of a book,' thought alice
+          'without pictures or conversations?' So she was considering in her own
+          mind (as well as she could, for the hot day made her feel very sleepy
+          and stupid), whether the pleasure of making a daisy-chain would be
+          worth the trouble of getting up and picking the daisies, when suddenly
+          a White rabbit with pink eyes ran close by her.\n''')
+      with open(FLAGS.eval_data, 'w') as f:
+        f.write('alice she rabbit once\n')
+    
+          with self.assertRaises(tf.errors.OutOfRangeError):
+        sess.run([result.key, result.uint8image])
+    
+    
+class PtbReaderTest(tf.test.TestCase):
+    
+    TEST_POSITION = go.Position(
+    utils_test.BOARD_SIZE,
+    board=TEST_BOARD,
+    n=3,
+    komi=6.5,
+    caps=(1, 2),
+    ko=None,
+    recent=(go.PlayerMove(go.BLACK, (0, 1)),
+            go.PlayerMove(go.WHITE, (0, 8)),
+            go.PlayerMove(go.BLACK, (1, 0))),
+    to_play=go.BLACK,
+)
+    
+      def test_place_stone(self):
+    board = utils_test.load_board('X........' + EMPTY_ROW * 8)
+    lib_tracker = LibertyTracker.from_board(utils_test.BOARD_SIZE, board)
+    lib_tracker.add_stone(BLACK, coords.from_kgs(utils_test.BOARD_SIZE, 'B9'))
+    self.assertEqual(len(lib_tracker.groups), 1)
+    self.assertNotEqual(
+        lib_tracker.group_index[coords.from_kgs(
+            utils_test.BOARD_SIZE, 'A9')], go.MISSING_GROUP_ID)
+    self.assertEqual(lib_tracker.liberty_cache[coords.from_kgs(
+        utils_test.BOARD_SIZE, 'A9')], 3)
+    self.assertEqual(lib_tracker.liberty_cache[coords.from_kgs(
+        utils_test.BOARD_SIZE, 'B9')], 3)
+    sole_group = lib_tracker.groups[lib_tracker.group_index[coords.from_kgs(
+        utils_test.BOARD_SIZE, 'A9')]]
+    self.assertEqual(sole_group.stones, coords_from_kgs_set('A9 B9'))
+    self.assertEqual(sole_group.liberties,
+                     coords_from_kgs_set('C9 A8 B8'))
+    self.assertEqual(sole_group.color, BLACK)
+    
+      def get_move(self, color):
+    self.accomodate_out_of_turn(color)
+    move = self.suggest_move(self.position)
+    if self.should_resign():
+      return gtp.RESIGN
+    return coords.to_pygtp(move)
+    
+        Args:
+      value: the value to be propagated (1 = black wins, -1 = white wins)
+      up_to: the node to propagate until.
+    '''
+    self.W += value
+    if self.parent is None or self is up_to:
+      return
+    self.parent.backup_value(value, up_to)
+    
+      def test_pick_moves(self):
+    player = initialize_basic_player()
+    root = player.root
+    root.child_N[coords.to_flat(utils_test.BOARD_SIZE, (2, 0))] = 10
+    root.child_N[coords.to_flat(utils_test.BOARD_SIZE, (1, 0))] = 5
+    root.child_N[coords.to_flat(utils_test.BOARD_SIZE, (3, 0))] = 1
+    
+        if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        print('Choose one path as argument one')
 
     
+        '''
+    # Constants, depending only on the floating-point format in use.
+    # We use an extra 2 bits of precision for rounding purposes.
+    PRECISION = sys.float_info.mant_dig + 2
+    SHIFT_MAX = sys.float_info.max_exp - PRECISION
+    Q_MAX = 1 << PRECISION
+    ROUND_HALF_TO_EVEN_CORRECTION = [0, -1, -2, 1, 0, -1, 2, 1]
     
-def test_follow_redirect_output_options(httpbin):
-    r = http('--check-status',
-             '--follow',
-             '--all',
-             '--print=h',
-             '--history-print=H',
-             httpbin.url + '/redirect/2')
-    assert r.count('GET /') == 2
-    assert 'HTTP/1.1 302 FOUND' not in r
-    assert HTTP_OK in r
+        def test_create_other_buttons(self):
+        for state in (False, True):
+            var = self.engine.backvar
+            var.set(state)
+            frame, others = self.btn_test_setup(
+                self.dialog.create_other_buttons)
+            buttons = frame.pack_slaves()
+            for spec, button in zip(others, buttons):
+                val, label = spec
+                self.assertEqual(button['text'], label)
+                if val == state:
+                    # hit other button, then this one
+                    # indexes depend on button order
+                    self.assertEqual(var.get(), state)
     
+        def scan_once(string, idx):
+        try:
+            return _scan_once(string, idx)
+        finally:
+            memo.clear()
     
-def test_unicode_basic_auth(httpbin):
-    # it doesn't really authenticate us because httpbin
-    # doesn't interpret the utf8-encoded auth
-    http('--verbose', '--auth', u'test:%s' % UNICODE,
-         httpbin.url + u'/basic-auth/test/' + UNICODE)
-    
-            :return: RawStream, output_file
-    
-            '''
-        raise NotImplementedError()
-    
-        name = 'Basic HTTP auth'
-    auth_type = 'basic'
-    
-    
-@keras_test
-def test_masking():
-    np.random.seed(1337)
-    x = np.array([[[1], [1]],
-                  [[0], [0]]])
-    model = Sequential()
-    model.add(Masking(mask_value=0, input_shape=(2, 1)))
-    model.add(TimeDistributed(Dense(1, kernel_initializer='one')))
-    model.compile(loss='mse', optimizer='sgd')
-    y = np.array([[[1], [1]],
-                  [[1], [1]]])
-    loss = model.train_on_batch(x, y)
-    assert loss == 0
-    
-    
-@pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
-def test_zero(tensor_shape):
-    _runner(initializers.zeros(), tensor_shape,
-            target_mean=0., target_max=0.)
-    
-    
-@pytest.fixture
-def in_tmpdir(tmpdir):
-    '''Runs a function in a temporary directory.
-    
-    seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
-                   padding='same', return_sequences=True))
-seq.add(BatchNormalization())
-    
-        # not a test, but looks like a test
-    'scrapy/utils/testsite.py',
-    
-            # start checks
-        if opts.list:
-            for spider, methods in sorted(contract_reqs.items()):
-                if not methods and not opts.verbose:
-                    continue
-                print(spider)
-                for method in sorted(methods):
-                    print('  * %s' % method)
-        else:
-            start = time.time()
-            self.crawler_process.start()
-            stop = time.time()
-    
-                    # execute pre and post hooks in order
-                for contract in reversed(contracts):
-                    request = contract.add_pre_hook(request, results)
-                for contract in contracts:
-                    request = contract.add_post_hook(request, results)
-    
-    ## Anything on different channel than DEFAULT_CHANNEL is not parsed
-# by parser.
-HIDDEN_CHANNEL = 99
-    
-    
-##     def specialTransition(self, state, symbol):
-##         return 0
+    def escape(m):
+    all, tail = m.group(0, 1)
+    assert all.startswith('\\')
+    esc = simple_escapes.get(tail)
+    if esc is not None:
+        return esc
+    if tail.startswith('x'):
+        hexes = tail[1:]
+        if len(hexes) < 2:
+            raise ValueError('invalid hex string escape ('\\%s')' % tail)
+        try:
+            i = int(hexes, 16)
+        except ValueError:
+            raise ValueError('invalid hex string escape ('\\%s')' % tail) from None
+    else:
+        try:
+            i = int(tail, 8)
+        except ValueError:
+            raise ValueError('invalid octal string escape ('\\%s')' % tail) from None
+    return chr(i)
