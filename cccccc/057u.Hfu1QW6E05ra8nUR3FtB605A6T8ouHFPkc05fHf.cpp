f0d0c7513f3ca5d36b67411456f839c6e5fe32fa
SUNsung <weatherius@gmail.com>
2018-05-02 17:21:36 +0300

    Rewrite [057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp] File in: 2018-05-02 17:21:36

--- cccccc/057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp
+++ cccccc/057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp
@@ -1,124 +1,42 @@
 
         
-        namespace base {
-class FilePath;
-}
-    
-    class NativeWindow;
-    
-    #include 'atom/browser/unresponsive_suppressor.h'
-    
-    #include <memory>
-    
-      // content::WebContentsObserver:
-  void RenderViewDeleted(content::RenderViewHost*) override;
-    
-    #include <map>
+          if (nDimension != self->nDimension)
+    hasRequiredSize = false;
     
     
-    { protected:
-  virtual ~PrintViewManagerObserver() {}
-};
-    
-    int main(int /*argc*/, char** /*argv*/)
-{
-    InitDevice();
-    return 0;
+    {  delete tensor;
+  delete tensor2;
+  cout << 'OK' << endl;
+  return 0;
 }
 
     
-    #ifndef OPENCV_CORE_HAL_INTERNAL_HPP
-#define OPENCV_CORE_HAL_INTERNAL_HPP
-    
-    #if defined(__linux__)
-    #include <dlfcn.h>
-    #include <stdio.h>
-    
-    #define CUSTOM_FUNCTION_ID 1000
-    
-    
-    {
-    {
-    {} } } // namespace cv::ocl::runtime
-    
-    	animations = memnew(Tree);
-	sub_vb->add_child(animations);
-	animations->set_v_size_flags(SIZE_EXPAND_FILL);
-	animations->set_hide_root(true);
-	animations->connect('cell_selected', this, '_animation_select');
-	animations->connect('item_edited', this, '_animation_name_edited');
-	animations->set_allow_reselect(true);
-    
-    class StreamPeerSSL : public StreamPeer {
-	GDCLASS(StreamPeerSSL, StreamPeer);
-    }
-    
-    	if (p_ssl) {
-		i.ssl_connection = LCCSCF_USE_SSL;
-		if (!verify_ssl)
-			i.ssl_connection |= LCCSCF_ALLOW_SELFSIGNED;
-	} else {
-		i.ssl_connection = 0;
-	}
-    
-    #if defined(MBEDTLS_ECP_RANDOMIZE_JAC_ALT) && !defined(MBEDTLS_ECP_INTERNAL_ALT)
-#error 'MBEDTLS_ECP_RANDOMIZE_JAC_ALT defined, but not all prerequisites'
-#endif
-    
-    	if (k.is_valid() && (k->get_scancode() == KEY_UP ||
-								k->get_scancode() == KEY_DOWN ||
-								k->get_scancode() == KEY_PAGEUP ||
-								k->get_scancode() == KEY_PAGEDOWN)) {
-    }
-    
-    	real_t vrel = contactPoint.m_frictionDirectionWorld.dot(vel);
-    
-    	void _resolve_single_bilateral(PhysicsDirectBodyState *s, const Vector3 &pos1, PhysicsBody *body2, const Vector3 &pos2, const Vector3 &normal, real_t &impulse, real_t p_rollInfluence);
-	real_t _calc_rolling_friction(btVehicleWheelContactPoint &contactPoint);
-    
-    	float *gen_uvs;
-	int *gen_vertices;
-	int *gen_indices;
-	int gen_vertex_count;
-	int gen_index_count;
-	int size_x;
-	int size_y;
-    
-    
-    {	if (match) {
-		BuildProcess &bp = *match;
-		bp.start();
-		return !bp.exited; // failed to start
-	} else {
-		BuildProcess bp = BuildProcess(p_build_info, p_callback);
-		bp.start();
-		builds.set(p_build_info, bp);
-		return !bp.exited; // failed to start
-	}
+    bool cudnn_is_acceptable(const Tensor& self) {
+  if (!globalContext().userEnabledCuDNN()) return false;
+  if (!self.is_cuda()) return false;
+  auto st = self.type().scalarType();
+  if (!(st == kDouble || st == kFloat || st == kHalf)) return false;
+  if (!AT_CUDNN_ENABLED()) return false;
+  // NB: In the old Python code, there was also a test to see if the
+  // cuDNN library was actually dynamically linked or not.  I'm not
+  // sure if we can actually test this.
+  return true;
 }
     
-    /// It performs an additional check allow exclusions.
-struct GodotClosestRayResultCallback : public btCollisionWorld::ClosestRayResultCallback {
-	const Set<RID> *m_exclude;
-	bool m_pickRay;
-	int m_shapeId;
+    int ${Storage}::getDevice() const {
+  ${storage_device} //storage->device;
+}
+    
+    int64_t ${Tensor}::dim() const {
+  if(isScalar())
+    return 0;
+  int64_t d = ${THTensor_nDimension};
+  // See Note [Empty versus 0-dim tensors]
+  if (d != 0)
+    return d;
+  return kEmptySizes.size();
+}
+    
+    namespace torch { namespace utils {
     }
-    
-    	struct TileData {
-		Point2i pos;
-		int cell;
-		bool flip_h;
-		bool flip_v;
-		bool transpose;
-		int auto_x;
-		int auto_y;
-	};
-    
-    	for (List<int>::Element *F = to_erase.front(); F; F = F->next()) {
     }
-    
-    	ClassDB::bind_method(D_METHOD('initialize'), &GDNative::initialize);
-	ClassDB::bind_method(D_METHOD('terminate'), &GDNative::terminate);
-    
-    	String current_library_path;
-	Vector<String> current_dependencies;
