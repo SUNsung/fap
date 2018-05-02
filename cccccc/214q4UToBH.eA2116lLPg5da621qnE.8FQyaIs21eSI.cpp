7c9507b889a4043b4fed2a9bb960dd043039652d
SUNsung <weatherius@gmail.com>
2018-05-02 15:07:55 +0300

    Rewrite [214q4UToBH.eA2116lLPg5da621qnE.8FQyaIs21eSI.cpp] File in: 2018-05-02 15:07:54

--- cccccc/214q4UToBH.eA2116lLPg5da621qnE.8FQyaIs21eSI.cpp
+++ cccccc/214q4UToBH.eA2116lLPg5da621qnE.8FQyaIs21eSI.cpp
@@ -1,193 +1,177 @@
 
         
-        typedef NS_ENUM(NSInteger, ObjectBehaviorAction) {
-    ObjectBehaviorActionRetain,
-    ObjectBehaviorActionCopy,
-    ObjectBehaviorActionMutableCopy
-};
+          virtual void Call(const std::string& method,
+                    const base::ListValue& arguments,
+                    content::RenderFrameHost* rvh = nullptr);
+  virtual void CallSync(const std::string& method,
+                        const base::ListValue& arguments,
+                        base::ListValue* result);
     
-    #endif // SWIFT_INDEX_INDEXRECORD_H
+    
+    {} // namespace nwapi
 
     
-      Optional<const swift::markup::ThrowsField*> getThrowsField() const {
-    return Parts.ThrowsField;
-  }
+    void Menu::Call(const std::string& method,
+                const base::ListValue& arguments,
+                content::RenderFrameHost* rvh) {
+  if (method == 'Append') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    Append(object_manager()->GetApiObject<MenuItem>(object_id));
+  } else if (method == 'Insert') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    int pos = 0;
+    arguments.GetInteger(1, &pos);
+    Insert(object_manager()->GetApiObject<MenuItem>(object_id), pos);
+  } else if (method == 'Remove') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    int pos = 0;
+    arguments.GetInteger(1, &pos);
+    Remove(object_manager()->GetApiObject<MenuItem>(object_id), pos);
+  } else if (method == 'Popup') {
+    int x = 0;
+    arguments.GetInteger(0, &x);
+    int y = 0;
+    arguments.GetInteger(1, &y);
+    content::WebContents* web_contents = content::WebContents::FromRenderFrameHost(rvh);
+    DCHECK(web_contents);
+    zoom::ZoomController* zoom_controller = zoom::ZoomController::FromWebContents(web_contents);
+    }
+    }
     
+      if (!item->enable_shortcut_)
+    return false;
     
-    {  static bool classof(const MarkupASTNode *N) {
-    return N->getKind() == ASTNodeKind::ParamField;
-  }
-};
+      GtkRequisition menu_req;
+  gtk_widget_size_request(GTK_WIDGET(menu), &menu_req);
+  GdkScreen* screen;
+  gdk_display_get_pointer(gdk_display_get_default(), &screen, NULL, NULL, NULL);
+  gint monitor = gdk_screen_get_monitor_at_point(screen, *x, *y);
     
-    SILDebugScope::SILDebugScope(SILLocation Loc, SILFunction *SILFn,
-                             const SILDebugScope *ParentScope ,
-                             const SILDebugScope *InlinedCallSite)
-    : Loc(Loc), InlinedCallSite(InlinedCallSite) {
-  if (ParentScope)
-    Parent = ParentScope;
-  else {
-    assert(SILFn && 'no parent provided');
-    Parent = SILFn;
+    aura::Window* Menu::GetActiveNativeView(content::RenderFrameHost* rfh) {
+  content::WebContents* web_contents =
+    content::WebContents::FromRenderFrameHost(rfh);
+  if (!web_contents) {
+    LOG(ERROR) << 'Menu: couldn't find WebContents';
+    return NULL;
   }
+  return web_contents->GetFullscreenRenderWidgetHostView()
+             ? web_contents->GetFullscreenRenderWidgetHostView()
+                   ->GetNativeView()
+             : web_contents->GetNativeView();
 }
     
-    #endif
+    #include 'content/nw/src/api/menuitem/menuitem.h'
+    
+        bool Read(ClipboardData& data) {
+      switch(data.type) {
+        case TYPE_TEXT:
+        return ReadText(data);
+        break;
+        case TYPE_HTML:
+        return ReadHTML(data);
+        break;
+        case TYPE_RTF:
+        return ReadRTF(data);
+        break;
+        case TYPE_PNG:
+        case TYPE_JPEG:
+        return ReadImage(data);
+        break;
+        case TYPE_NONE:
+        NOTREACHED();
+        return false;
+      }
+      NOTREACHED();
+      return false;      
+    }
+    
+    class NwMenuGetNSStringWithFixupFunction : public NWSyncExtensionFunction {
+ public:
+  NwMenuGetNSStringWithFixupFunction(){}
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    
+ protected:
+  ~NwMenuGetNSStringWithFixupFunction() override {}
+    
+  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringWithFixup', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringWithFixupFunction);
+};
+    
+                // Constructor with default value.
+            explicit Attribute(const std::string& p_attrName,
+                AttrType p_type,
+                const std::string& p_description,
+                const AttributeProto& p_defaultVal);
+    
+        // Taken from RS4
+    REGISTER_OPERATOR_SCHEMA(HardSigmoid)
+        .Description('HardSigmoid takes one input data (Tensor<T>) and produces one output '
+            'data (Tensor<T>) where the hard sigmoid function, f(x) = max⁡(0,min⁡(alpha*x+beta,1)), '
+            'is applied to the  tensor elementwise.')
+        .Input('X', 'Input tensor of any shape', 'T')
+        .Output('Y', 'Output tensor of same shape and type as input X.', 'T')
+        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
+            'Constrain input and output types to float tensors.')
+        .Attr('alpha', 'Scaling value', AttrType::AttributeProto_AttributeType_FLOAT)
+        .Attr('beta', 'Scalar offset', AttrType::AttributeProto_AttributeType_FLOAT);
+    
+        // Taken from ONNX
+    REGISTER_OPERATOR_SCHEMA(Split)
+        .Description('Split a tensor into a list of tensors, along the specified 'axis'. '
+            'The lengths of the split can be specified using argument 'axis' or '
+            'optional second input blob to the operator. Otherwise, the tensor is split '
+            'to equal sized parts.')
+        .Input('input', 'The tensor to split', 'T')
+        .Input('split', 'Optional list of output lengths (see also arg 'split')', 'T')
+        .Output('output', 'A list of output tensors', 'T')
+        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
+            'Constrain input and output types to float tensors.')
+        .Attr('axis', 'Which axis to split on', AttrType::AttributeProto_AttributeType_INT)
+        .Attr('split', 'Number of tensors to output.', AttrType::AttributeProto_AttributeType_INTS);
+    
+    // Computes the DCT (Discrete Cosine Transform) of the 8x8 array in 'block',
+// scaled up by a factor of 16. The values in 'block' are laid out row-by-row
+// and the result is written to the same memory area.
+void ComputeBlockDCT(coeff_t* block);
+    
+    namespace guetzli {
+    }
+    
+    
+    {}  // namespace guetzli
 
     
-    namespace swift {
-    }
+    #include 'guetzli/jpeg_data.h'
     
-    #endif // SWIFT_SYNTAX_REFERENCES_H
-
+      std::vector<uint8_t> ToSRGB(int xmin, int ymin, int xsize, int ysize) const;
     
-    namespace tesseract {
-    }
+    extern const std::string NIL;
     
-      // Connects this and other, discarding any existing connections.
-  void Connect(DoublePtr* other) {
-    other->Disconnect();
-    Disconnect();
-    other->other_end_ = this;
-    other_end_ = other;
-  }
-  // Disconnects this and other, making OtherEnd() return NULL for both.
-  void Disconnect() {
-    if (other_end_ != NULL) {
-      other_end_->other_end_ = NULL;
-      other_end_ = NULL;
-    }
-  }
-  // Returns the pointer to the other end of the double pointer.
-  DoublePtr* OtherEnd() const {
-    return other_end_;
-  }
-    
-    /*---------------------------------------------------------------------------
-          Macros
-----------------------------------------------------------------------------*/
-/* macros for controlling the display of recognized characters */
-#define EnableCharDisplay()   (DisplayCharacters = TRUE)
-#define DisableCharDisplay()    (DisplayCharacters = FALSE)
-    
-    // Clear all data.
-void IntFeatureDist::Clear() {
-  delete [] features_;
-  features_ = NULL;
-  delete [] features_delta_one_;
-  features_delta_one_ = NULL;
-  delete [] features_delta_two_;
-  features_delta_two_ = NULL;
-}
-    
-        size_t m_prevChosenMinibatchSize;
-    double m_lastFinishedEpochTrainLoss;
-    
-        float alpha = 1.0f * rng() / rng.max();
-    Matrix<float>::ScaleAndAdd(alpha, mAdense, mBdense);
-    
-    PrefetchGPUDataTransferer::~PrefetchGPUDataTransferer()
+    void AbstractAuthResolver::setDefaultCred(std::string user,
+                                          std::string password)
 {
-    try
-    {
-        PrepareDevice(m_deviceId);
-    }
-    catch (...)
-    {
-        // the error is already logged
-        return;
-    }
-    }
+  defaultUser_ = std::move(user);
+  defaultPassword_ = std::move(password);
+}
     
     private:
-    ComputationNetworkPtr m_net;
+  std::string userDefinedUser_;
+  std::string userDefinedPassword_;
     
-    // ---------------------------------------------------------------------------
-// Expression -- the entire config is a tree of Expression types
-// We don't use polymorphism here because C++ is so verbose...
-// ---------------------------------------------------------------------------
     
-            // key=Function(x,y,z) - function with return
-        // HDim=256 - NDL inline
-        // model1=[...] - Embedded NDL script
-        if (equalFound)
-        {
-            size_t tokenStartNew = keyEnd + 1;
-            if (!(tokenStartNew < tokenEnd))
-                RuntimeError('Equal at the end of line not allowed');
-            std::string rightValue = stringParse.substr(tokenStartNew, tokenEnd - tokenStartNew);
-            Trim(rightValue);
-    }
+    {} // namespace aria2
     
-    template<class TString>
-inline bool AreEqualIgnoreCase(
-    const TString& s1,
-    const typename TString::value_type* s2pointer)
-{
-    return AreEqualIgnoreCase(s1, TString(s2pointer));
-}
+    public:
+  AbstractOptionHandler(PrefPtr pref, const char* description = NO_DESCRIPTION,
+                        const std::string& defaultValue = NO_DEFAULT_VALUE,
+                        ARG_TYPE argType = REQ_ARG, char shortName = 0);
     
-    // ===========================================================================
-// float4 -- wrapper around the rather ugly SSE intrinsics for float[4]
-//
-// Do not use the intrinsics outside anymore; instead add all you need into this class.
-//
-// MSDN links:
-// basic: http://msdn.microsoft.com/en-us/library/x5c07e2a%28v=VS.80%29.aspx
-// load/store: (add this)
-// newer ones: (seems no single list available)
-// ===========================================================================
+      std::shared_ptr<HttpConnection> httpConnection_;
     
-    #include <regex>
-#include <iostream>
+    #include 'Notifier.h'
     
-    FilterBlockReader::FilterBlockReader(const FilterPolicy* policy,
-                                     const Slice& contents)
-    : policy_(policy),
-      data_(NULL),
-      offset_(NULL),
-      num_(0),
-      base_lg_(0) {
-  size_t n = contents.size();
-  if (n < 5) return;  // 1 byte for base_lg_ and 4 for start of offset array
-  base_lg_ = contents[n-1];
-  uint32_t last_word = DecodeFixed32(contents.data() + n - 5);
-  if (last_word > n - 5) return;
-  data_ = contents.data();
-  offset_ = data_ + last_word;
-  num_ = (n - 5 - last_word) / 4;
-}
-    
-      const SnapshotImpl* New(SequenceNumber seq) {
-    SnapshotImpl* s = new SnapshotImpl;
-    s->number_ = seq;
-    s->list_ = this;
-    s->next_ = &list_;
-    s->prev_ = list_.prev_;
-    s->prev_->next_ = s;
-    s->next_->prev_ = s;
-    return s;
-  }
-    
-      for (size_t i = 0; i < compact_pointers_.size(); i++) {
-    PutVarint32(dst, kCompactPointer);
-    PutVarint32(dst, compact_pointers_[i].first);  // level
-    PutLengthPrefixedSlice(dst, compact_pointers_[i].second.Encode());
-  }
-    
-    TEST(CRC, Extend) {
-  ASSERT_EQ(Value('hello world', 11),
-            Extend(Value('hello ', 6), 'world', 5));
-}
-    
-    #include 'guetzli/dct_double.h'
-    
-    // Functions for writing a JPEGData object into a jpeg byte stream.
-    
-    double ButteraugliScoreForQuality(double quality) {
-  if (quality < kLowestQuality) quality = kLowestQuality;
-  if (quality > kHighestQuality) quality = kHighestQuality;
-  int index = static_cast<int>(quality);
-  double mix = quality - index;
-  return kScoreForQuality[index - kLowestQuality] * (1 - mix) +
-      kScoreForQuality[index - kLowestQuality + 1] * mix;
-}
+    #endif // D_ASYNC_NAME_RESOLVER_MAN_H
