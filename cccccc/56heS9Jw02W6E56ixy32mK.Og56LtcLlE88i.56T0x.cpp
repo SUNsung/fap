b825fd4b6e880bc2c30e3ae09e342134a9e14d32
SUNsung <weatherius@gmail.com>
2018-05-02 18:16:09 +0300

    Rewrite [56heS9Jw02W6E56ixy32mK.Og56LtcLlE88i.56T0x.cpp] File in: 2018-05-02 18:16:08

--- cccccc/56heS9Jw02W6E56ixy32mK.Og56LtcLlE88i.56T0x.cpp
+++ cccccc/56heS9Jw02W6E56ixy32mK.Og56LtcLlE88i.56T0x.cpp
@@ -1,155 +1,215 @@
 
         
-        #include 'base/command_line.h'
-#include 'base/logging.h'
-#include 'base/message_loop/message_loop.h'
-#include 'base/values.h'
+        
+    {}
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
     
     
-    {  RenderThread::Get()->Send(new ShellViewHostMsg_Call_Object_Method(
-      routing_id,
-      object_id,
-      type,
-      method,
-      *static_cast<base::ListValue*>(value_args.get())));
-  return v8::Undefined(isolate);
+    {    _data.Write((const char*)_rawbuf + st.head_length + st.url_length, st.total_length - (st.head_length + st.url_length));
+    return LONGLINKPACK_OK;
 }
     
-      private:
-    bool ReadText(ClipboardData& data) {
-      DCHECK(data.type == TYPE_TEXT);
-      base::string16 text;
-      clipboard_->ReadText(ui::CLIPBOARD_TYPE_COPY_PASTE, &text);
-      data.data.reset(new std::string(base::UTF16ToUTF8(text)));
-      return true;
+    
+    {    void throw_exception( std::exception const & e ) {
+        xfatal2(TSF'boost exception:%_', e.what());
+        
+#ifdef ANDROID
+        char stack[4096] = {0};
+        android_callstack(stack, sizeof(stack));
+        xfatal2(TSF'%_', stack);
+#endif
     }
-    
-    class NwClipboardSetListSyncFunction : public NWSyncExtensionFunction {
- public:
-  NwClipboardSetListSyncFunction();
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    }
-    
-    // Computes the Otsu threshold(s) for the given histogram.
-// Also returns H = total count in histogram, and
-// omega0 = count of histogram below threshold.
-int OtsuStats(const int* histogram, int* H_out, int* omega0_out);
-    
-    #define ASSERT_HOST_MSG(x, ...)                                                \
-  if (!(x)) {                                                                  \
-    tprintf(__VA_ARGS__);                                                      \
-    ASSERT_FAILED.error(#x, ABORT, 'in file %s, line %d', __FILE__, __LINE__); \
-  }
-    
-    void THDTensor_(_resize3d)(THDTensor *tensor, int64_t size0, int64_t size1, int64_t size2) {
-  int64_t sizes[] = {size0, size1, size2};
-  THDTensor_(_resize)(tensor, 2, sizes, nullptr);
 }
+
     
-    constexpr char RANK_ENV[] = 'RANK';
-constexpr char WORLD_SIZE_ENV[] = 'WORLD_SIZE';
-constexpr char MASTER_PORT_ENV[] = 'MASTER_PORT';
-constexpr char MASTER_ADDR_ENV[] = 'MASTER_ADDR';
+    #include 'comm/debugger/test_spy_sample.h'
+#include 'comm/xlogger/xlogger.h'
     
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
     
-int main() {
-  thpp::FloatTensor *tensor = new thpp::THTensor<float>();
-  thpp::FloatTensor *tensor2 = new thpp::THTensor<float>();
-  assert(tensor->nDim() == 0);
-    }
+    jvalue JNU_CallMethodByMethodInfo(JNIEnv* _env, jobject _obj, JniMethodInfo _method_info, ...);
     
-    template<> AT_API Half convert(float f);
-template<> AT_API float convert(Half f);
-template<> AT_API Half convert(double f);
-template<> AT_API double convert(Half f);
-template<> AT_API Half convert(int64_t f);
-template<> AT_API int64_t convert(Half f);
+      // There must be a goodMallocSize between 100 and 1024...
+  EXPECT_LT(1u, queue.front()->countChainElements());
+  const IOBuf* buf = queue.front();
+  do {
+    EXPECT_LE(100u, buf->capacity());
+    buf = buf->next();
+  } while (buf != queue.front());
     
-        std::shared_ptr<GlooCache::algorithm_type> algo;
-    if (device == DeviceType::CPU) {
-      algo = std::make_shared<::gloo::AllgatherRing<T>>(
-        context,
-        std::initializer_list<const T*>{reinterpret_cast<const T*>(input_buffer.get())},
-        reinterpret_cast<T*>(output_buffer.get()),
-        count);
-    } else {
-      throw std::runtime_error('unsupported device in Gloo allGather');
-    }
+    template <typename C>
+struct compare_less : detail::cmp_pred<C, ordering::lt, 0> {
+  using detail::cmp_pred<C, ordering::lt, 0>::cmp_pred;
+};
     
-    #undef THPTensor_
-#undef THPTensor_stateless_
-#undef THPTensor
-#undef THPTensorStr
-#undef THPTensorBaseStr
-#undef THPTensorClass
-    
-    // Reads the Start of Scan (SOS) marker segment and fills in *scan_info with the
-// parsed data.
-bool ProcessSOS(const uint8_t* data, const size_t len, size_t* pos,
-                JPEGData* jpg) {
-  const size_t start_pos = *pos;
-  VERIFY_LEN(3);
-  size_t marker_len = ReadUint16(data, pos);
-  int comps_in_scan = ReadUint8(data, pos);
-  VERIFY_INPUT(comps_in_scan, 1, static_cast<int>(jpg->components.size()),
-               COMPS_IN_SCAN);
-    }
-    
-    std::vector<float> Blur(const std::vector<float>& image, int w, int h) {
-    // This is only made for small sigma, e.g. 1.3.
-    static const double kSigma = 1.3;
-    std::vector<double> kernel(5);
-    for (size_t i = 0; i < kernel.size(); i++) {
-      kernel[i] = Normal(1.0 * i - kernel.size() / 2, kSigma);
-    }
-    }
+    TEST(Futex, basic_live) {
+  run_basic_tests<std::atomic>();
+  run_wait_until_tests<std::atomic>();
+}
     
     
     {
-    {
-    {      // Add back the last sentinel node.
-      tree[j_end + 1] = sentinel;
-    }
-    if (SetDepth(static_cast<int>(2 * n - 1), &tree[0], depth, tree_limit)) {
-      /* We need to pack the Huffman tree in tree_limit bits. If this was not
-         successful, add fake entities to the lowest values and retry. */
-      break;
-    }
-  }
-}
+    {} // namespace detail
+} // namespace folly
+
     
-      tmp0 = in[stride];
-  tmp1 = kIDCTMatrix[ 1] * tmp0;
-  tmp2 = kIDCTMatrix[ 9] * tmp0;
-  tmp3 = kIDCTMatrix[17] * tmp0;
-  tmp4 = kIDCTMatrix[25] * tmp0;
-  out[0] += tmp1;
-  out[1] += tmp2;
-  out[2] += tmp3;
-  out[3] += tmp4;
-  out[4] -= tmp4;
-  out[5] -= tmp3;
-  out[6] -= tmp2;
-  out[7] -= tmp1;
-    
-    namespace guetzli {
-    }
-    
-    // Single pixel rgb to 16-bit yuv conversion.
-// The returned yuv values are signed integers in the
-// range [-128, 127] inclusive.
-inline static void RGBToYUV16(const uint8_t* const rgb,
-                              coeff_t *out) {
-  enum { FRAC = 16, HALF = 1 << (FRAC - 1) };
-  const int r = rgb[0];
-  const int g = rgb[1];
-  const int b = rgb[2];
-  out[0] = (19595 * r  + 38469 * g +  7471 * b - (128 << 16) + HALF) >> FRAC;
-  out[64] = (-11059 * r - 21709 * g + 32768 * b + HALF - 1) >> FRAC;
-  out[128] = (32768 * r  - 27439 * g -  5329 * b + HALF - 1) >> FRAC;
-}
-    
-    
-    {}  // namespace guetzli
-    
-    // Preprocesses U and V channel for better results after downsampling.
+    /**
+ * DynamicParser provides a tiny DSL for easily, correctly, and losslessly
+ * parsing a folly::dynamic into any other representation.
+ *
+ * To make this concrete, this lets you take a JSON config that potentially
+ * contains user errors, and parse __all__ of its valid parts, while
+ * automatically and __reversibly__ recording any parts that cause errors:
+ *
+ *   {'my values': {
+ *     'an int': 'THIS WILL BE RECORDED AS AN ERROR, BUT WE'LL PARSE THE REST',
+ *     'a double': 3.1415,
+ *     'keys & values': {
+ *       'the sky is blue': true,
+ *       'THIS WILL ALSO BE RECORDED AS AN ERROR': 'cheese',
+ *       '2+2=5': false,
+ *     }
+ *   }}
+ *
+ * To parse this JSON, you need no exception handling, it is as easy as:
+ *
+ *   folly::dynamic d = ...;  // Input
+ *   int64_t integer;  // Three outputs
+ *   double real;
+ *   std::map<std::string, bool> enabled_widgets;
+ *   DynamicParser p(DynamicParser::OnError::RECORD, &d);
+ *   p.required('my values', [&]() {
+ *     p.optional('an int', [&](int64_t v) { integer = v; });
+ *     p.required('a double', [&](double v) { real = v; });
+ *     p.optional('keys & values', [&]() {
+ *       p.objectItems([&](std::string widget, bool enabled) {
+ *         enabled_widgets.emplace(widget, enabled);
+ *       });
+ *     });
+ *   });
+ *
+ * Your code in the lambdas can throw, and this will be reported just like
+ * missing key and type conversion errors, with precise context on what part
+ * of the folly::dynamic caused the error.  No need to throw:
+ *   std::runtime_error('Value X at key Y caused a flux capacitor overload')
+ * This will do:
+ *   std::runtime_error('Flux capacitor overload')
+ *
+ * == Keys and values are auto-converted to match your callback ==
+ *
+ * DynamicParser's optional(), required(), objectItems(), and
+ * arrayItems() automatically convert the current key and value to match the
+ * signature of the provided callback.  parser.key() and parser.value() can
+ * be used to access the same data without conversion.
+ *
+ * The following types are supported -- you should generally take arguments
+ * by-value, or by-const-reference for dynamics & strings you do not copy.
+ *
+ *   Key: folly::dynamic (no conversion), std::string, int64_t
+ *   Value: folly::dynamic (no conversion), int64_t, bool, double, std::string
+ *
+ * There are 21 supported callback signatures, of three kinds:
+ *
+ *   1: No arguments -- useful if you will just call more parser methods.
+ *
+ *   5: The value alone -- the common case for optional() and required().
+ *        [&](whatever_t value) {}
+ *
+ *   15: Both the key and the value converted according to the rules above:
+ *        [&](whatever_t key, whatever_t) {}
+ *
+ * NB: The key alone should be rarely needed, but these callback styles
+ *     provide it with no conversion overhead, and only minimal verbosity:
+ *       [&](const std::string& k, const folly::dynamic&) {}
+ *       [&]() { auto k = p.key().asString(); }
+ *
+ * == How `releaseErrors()` can make your parse lossless ==
+ *
+ * If you write parsing code by hand, you usually end up with error-handling
+ * resembling that of OnError::THROW -- the first error you hit aborts the
+ * whole parse, and you report it.
+ *
+ * OnError::RECORD offers a more user-friendly alternative for 'parse,
+ * serialize, re-parse' pipelines, akin to what web-forms do.  All
+ * exception-causing parts are losslessly recorded in a parallel
+ * folly::dynamic, available via releaseErrors() at the end of the parse.
+ *
+ * Suppose we fail to look up 'key1' at the root, and hit a value error in
+ * 'key2': {'subkey2': ...}.  The error report will have the form:
+ *
+ *   {'nested': {
+ *     'key_errors': {'key1': 'explanatory message'},
+ *     'value': <whole input>,
+ *     'nested': { 'key2': { 'nested': {
+ *       'subkey2': {'value': <original value>, 'error': 'message'}
+ *     } } }
+ *   }}
+ *
+ * Errors in array items are handled just the same, but using integer keys.
+ *
+ * The advantage of this approach is that your parsing can throw wherever,
+ * and DynamicParser isolates it, allowing the good parts to parse.
+ *
+ * Put another way, this makes it easy to implement a transformation that
+ * splits a `folly::dynamic` into a 'parsed' part (which might be your
+ * struct meant for runtime use), and a matching 'errors' part.  As long as
+ * your successful parses are lossless, you can always reconstruct the
+ * original input from the parse output and the recorded 'errors'.
+ *
+ * == Limitations ==
+ *
+ *  - The input dynamic should be an object or array. wrapError() could be
+ *    exposed to allow parsing single scalars, but this would not be a
+ *    significant usability improvement over try-catch.
+ *
+ *  - Do NOT try to parse the same part of the input dynamic twice. You
+ *    might report multiple value errors, which is currently unsupported.
+ *
+ *  - optional() does not support defaulting. This is unavoidable, since
+ *    DynamicParser does not dictate how you record parsed data.  If your
+ *    parse writes into an output struct, then it ought to be initialized at
+ *    construction time.  If your output is initialized to default values,
+ *    then you need no 'default' feature.  If it is not initialized, you are
+ *    in trouble anyway.  Suppose your optional() parse hits an error.  What
+ *    does your output contain?
+ *      - Uninitialized data :(
+ *      - You rely on an optional() feature to fall back to parsing some
+ *        default dynamic.  Sadly, the default hits a parse error.  Now what?
+ *    Since there is no good way to default, DynamicParser leaves it out.
+ *
+ * == Future: un-parsed items ==
+ *
+ * DynamicParser could support erroring on un-parsed items -- the parts of
+ * the folly::dynamic, which were never asked for.  Here is an ok design:
+ *
+ * (i) At the start of parsing any value, the user may call:
+ *   parser.recursivelyForbidUnparsed();
+ *   parser.recursivelyAllowUnparsed();
+ *   parser.locallyForbidUnparsed();
+ *   parser.locallyAllowUnparsed();
+ *
+ * (ii) At the end of the parse, any unparsed items are dumped to 'errors'.
+ * For example, failing to parse index 1 out of ['v1', 'v2', 'v3'] yields:
+ *   'nested': {1: {'unparsed': 'v2'}}
+ * or perhaps more verbosely:
+ *   'nested': {1: {'error': 'unparsed value', 'value': 'v2'}}
+ *
+ * By default, unparsed items are allowed. Calling a 'forbid' function after
+ * some keys have already been parsed is allowed to fail (this permits a
+ * lazy implementation, which has minimal overhead when 'forbid' is not
+ * requested).
+ *
+ * == Future: multiple value errors ==
+ *
+ * The present contract is that exactly one value error is reported per
+ * location in the input (multiple key lookup errors are, of course,
+ * supported).  If the need arises, multiple value errors could easily be
+ * supported by replacing the 'error' string with an 'errors' array.
+ */
