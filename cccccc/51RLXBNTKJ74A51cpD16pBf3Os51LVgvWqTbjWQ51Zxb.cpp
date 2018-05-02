72796f3ac32f45d68858146caf75d175c139200d
SUNsung <weatherius@gmail.com>
2018-05-03 01:06:43 +0300

    Rewrite [51RLXBNTKJ74A51cpD16pBf3Os51LVgvWqTbjWQ51Zxb.cpp] File in: 2018-05-03 01:06:42

--- cccccc/51RLXBNTKJ74A51cpD16pBf3Os51LVgvWqTbjWQ51Zxb.cpp
+++ cccccc/51RLXBNTKJ74A51cpD16pBf3Os51LVgvWqTbjWQ51Zxb.cpp
@@ -1,45 +1,283 @@
 
         
-            D3D_FEATURE_LEVEL featureLevels[] =
-    {
-        D3D_FEATURE_LEVEL_11_0,
-        D3D_FEATURE_LEVEL_10_1,
-        D3D_FEATURE_LEVEL_10_0,
-    };
-    UINT numFeatureLevels = ARRAYSIZE(featureLevels);
+          AcceleratorCallbackMap accelerator_callback_map_;
     
-    using namespace cv::ocl::runtime;
+    void DragFileItems(const std::vector<base::FilePath>& files,
+                   const gfx::Image& icon,
+                   gfx::NativeView view);
     
-    #ifdef USE_GCC_FAST_TLS
-    
-      mpz_clear(gmpReturn);
-    
-      if (!ret || numa_num_nodes <= 1) return;
-    
-    #else // HAVE_NUMA undefined
-namespace HPHP {
+    namespace atom {
     }
     
-      size_t numFrees()   const { return m_nfree; }
-  size_t numAllocs()  const { return m_nalloc; }
-  size_t bytesFree()  const { return m_bytesFree; }
-  size_t blocksFree() const { return m_freeRanges.size(); }
+    
+    {}  // namespace atom
     
     
-static uint64_t RepeatBitsAcrossReg(unsigned reg_size,
-                                    uint64_t value,
-                                    unsigned width) {
-  assert((width == 2) || (width == 4) || (width == 8) || (width == 16) ||
-         (width == 32));
-  assert((reg_size == kWRegSize) || (reg_size == kXRegSize));
-  uint64_t result = value & ((1UL << width) - 1UL);
-  for (unsigned i = width; i < reg_size; i *= 2) {
-    result |= (result << i);
-  }
-  return result;
+    { protected:
+  virtual ~WindowListObserver() {}
+};
+    
+      // Sets ID of the hosting desktop picker dialog. The window with this ID will
+  // be filtered out from the list of sources.
+  virtual void SetViewDialogWindowId(content::DesktopMediaID::Id dialog_id) = 0;
+    
+      base::WeakPtrFactory<PepperFlashDRMHost> weak_factory_;
+    
+    #include <string>
+    
+    #ifdef _MSC_VER
+    
+    IPC_MESSAGE_ROUTED3(ShellViewMsg_Object_On_Event,
+                    int /* object id */,
+                    std::string /* event name */,
+                    base::ListValue /* arguments */)
+    
+    
+void Base::Call(const std::string& method, const base::ListValue& arguments,
+                content::RenderFrameHost* rvh) {
+  NOTREACHED() << 'Uncatched call in Base'
+               << ' method:' << method
+               << ' arguments:' << arguments;
 }
     
-    void cgDefSP(IRLS& env, const IRInstruction* inst) {
-  auto const sp = dstLoc(env, inst, 0).reg();
-  auto& v = vmain(env);
+    namespace nw {
     }
+    
+    void Menu::Create(const base::DictionaryValue& option) {
+  is_menu_modified_ = true;
+  menu_delegate_.reset(new MenuDelegate(object_manager()));
+  menu_model_.reset(new ui::NwMenuModel(menu_delegate_.get()));
+    }
+    
+    void cvFindStereoCorrespondenceBM( const CvArr* leftarr, const CvArr* rightarr,
+                                   CvArr* disparr, CvStereoBMState* state )
+{
+    cv::Mat left = cv::cvarrToMat(leftarr), right = cv::cvarrToMat(rightarr);
+    const cv::Mat disp = cv::cvarrToMat(disparr);
+    }
+    
+    void ComputeExtrinsicRefine(const Mat& imagePoints, const Mat& objectPoints, Mat& rvec,
+                            Mat&  tvec, Mat& J, const int MaxIter,
+                            const IntrinsicParams& param, const double thresh_cond);
+CV_EXPORTS Mat ComputeHomography(Mat m, Mat M);
+    
+    
+    {        return ptr[tid - lane];
+    }
+}}} // namespace cv { namespace cuda { namespace cudev {
+    
+    namespace cv { namespace ocl { namespace runtime {
+    }
+    }
+    }
+    
+    	wchar_t commandStr[MAX_PATH + 20] = { 0 };
+	swprintf_s(commandStr, L'\'%s\' \'%%V\'', exePath);
+    
+      // Store the specified number as the sequence number for the start of
+  // this batch.
+  static void SetSequence(WriteBatch* batch, SequenceNumber seq);
+    
+      void StartBlock(uint64_t block_offset);
+  void AddKey(const Slice& key);
+  Slice Finish();
+    
+    uint32_t Hash(const char* data, size_t n, uint32_t seed) {
+  // Similar to murmur hash
+  const uint32_t m = 0xc6a4a793;
+  const uint32_t r = 24;
+  const char* limit = data + n;
+  uint32_t h = seed ^ (n * m);
+    }
+    
+    
+    {}  // namespace leveldb
+    
+    // Handler for Win32 messages, update mouse/keyboard data.
+// You may or not need this for your implementation, but it can serve as reference for handling inputs.
+// Commented out to avoid dragging dependencies on <windows.h> types. You can copy the extern declaration in your code.
+/*
+IMGUI_API LRESULT   ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+*/
+
+    
+    
+    {        // Create the constant buffer
+        {
+            D3D10_BUFFER_DESC desc;
+            desc.ByteWidth = sizeof(VERTEX_CONSTANT_BUFFER);
+            desc.Usage = D3D10_USAGE_DYNAMIC;
+            desc.BindFlags = D3D10_BIND_CONSTANT_BUFFER;
+            desc.CPUAccessFlags = D3D10_CPU_ACCESS_WRITE;
+            desc.MiscFlags = 0;
+            g_pd3dDevice->CreateBuffer(&desc, NULL, &g_pVertexConstantBuffer);
+        }
+    }
+    
+            D3D12_RESOURCE_DESC desc;
+        ZeroMemory(&desc, sizeof(desc));
+        desc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+        desc.Alignment = 0;
+        desc.Width = width;
+        desc.Height = height;
+        desc.DepthOrArraySize = 1;
+        desc.MipLevels = 1;
+        desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+        desc.SampleDesc.Count = 1;
+        desc.SampleDesc.Quality = 0;
+        desc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
+        desc.Flags = D3D12_RESOURCE_FLAG_NONE;
+    
+    bool    ImGui_ImplGlfwGL2_Init(GLFWwindow* window, bool install_callbacks)
+{
+    g_Window = window;
+    }
+    
+        ImGuiIO& io = ImGui::GetIO();
+    io.KeyMap[ImGuiKey_Tab] = ALLEGRO_KEY_TAB;
+    io.KeyMap[ImGuiKey_LeftArrow] = ALLEGRO_KEY_LEFT;
+    io.KeyMap[ImGuiKey_RightArrow] = ALLEGRO_KEY_RIGHT;
+    io.KeyMap[ImGuiKey_UpArrow] = ALLEGRO_KEY_UP;
+    io.KeyMap[ImGuiKey_DownArrow] = ALLEGRO_KEY_DOWN;
+    io.KeyMap[ImGuiKey_PageUp] = ALLEGRO_KEY_PGUP;
+    io.KeyMap[ImGuiKey_PageDown] = ALLEGRO_KEY_PGDN;
+    io.KeyMap[ImGuiKey_Home] = ALLEGRO_KEY_HOME;
+    io.KeyMap[ImGuiKey_End] = ALLEGRO_KEY_END;
+    io.KeyMap[ImGuiKey_Insert] = ALLEGRO_KEY_INSERT;
+    io.KeyMap[ImGuiKey_Delete] = ALLEGRO_KEY_DELETE;
+    io.KeyMap[ImGuiKey_Backspace] = ALLEGRO_KEY_BACKSPACE;
+    io.KeyMap[ImGuiKey_Space] = ALLEGRO_KEY_SPACE;
+    io.KeyMap[ImGuiKey_Enter] = ALLEGRO_KEY_ENTER;
+    io.KeyMap[ImGuiKey_Escape] = ALLEGRO_KEY_ESCAPE;
+    io.KeyMap[ImGuiKey_A] = ALLEGRO_KEY_A;
+    io.KeyMap[ImGuiKey_C] = ALLEGRO_KEY_C;
+    io.KeyMap[ImGuiKey_V] = ALLEGRO_KEY_V;
+    io.KeyMap[ImGuiKey_X] = ALLEGRO_KEY_X;
+    io.KeyMap[ImGuiKey_Y] = ALLEGRO_KEY_Y;
+    io.KeyMap[ImGuiKey_Z] = ALLEGRO_KEY_Z;
+    
+    //---- Define constructor and implicit cast operators to convert back<>forth from your math types and ImVec2/ImVec4.
+// This will be inlined as part of ImVec2 and ImVec4 class declarations.
+/*
+#define IM_VEC2_CLASS_EXTRA                                                 \
+        ImVec2(const MyVec2& f) { x = f.x; y = f.y; }                       \
+        operator MyVec2() const { return MyVec2(x,y); }
+    
+        // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them. 
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple. 
+    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Read 'misc/fonts/README.txt' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Roboto-Medium.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Cousine-Regular.ttf', 15.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/DroidSans.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/ProggyTiny.ttf', 10.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF('c:\\Windows\\Fonts\\ArialUni.ttf', 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+    //IM_ASSERT(font != NULL);
+    
+    // Licensed under the MIT License (the 'License'); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+// http://opensource.org/licenses/MIT
+    
+    // true pass, false limit
+bool CommFrequencyLimit::Check() {
+    uint64_t now = ::gettickcount();
+    if (!touch_times_.empty() && (now<touch_times_.front()) ) { //if user modify the time, amend it
+    	xwarn2(TSF'Must be modified time.now=%_', now);
+    	size_t size = touch_times_.size();
+    	touch_times_.clear();
+    	for (size_t i=0; i<size; ++i) {
+    		touch_times_.push_back(now-1);
+    	}
+    }
+    }
+    
+    
+    {  private:
+    void* m_this;
+    std::map<const std::string, boost::any> m_variablemap;
+};
+    
+    
+    
+    
+bool TSpy::SpyHookLogFunc(XLoggerInfo_t& _info, std::string& _log)
+{
+    __attribute__((unused)) int i = 0;
+    return true;
+}
+    
+    #include <jni.h>
+#include <string>
+struct JniMethodInfo;
+class AutoBuffer;
+    
+    
+    {
+    {
+    {}  // namespace math
+}  // namespace common
+}  // namespace apollo
+
+    
+    void SubTopoGraph::AddPotentialOutEdge(
+    TopoNode* const sub_node,
+    const std::unordered_set<const TopoEdge*> origin_edge) {
+  std::unordered_set<TopoNode*> other_sub_nodes;
+  for (const auto* out_edge : origin_edge) {
+    if (GetSubNodes(out_edge->ToNode(), &other_sub_nodes)) {
+      for (auto* sub_to_node : other_sub_nodes) {
+        if (sub_node->GetOutEdgeTo(sub_to_node) != nullptr) {
+          continue;
+        }
+        if (!IsReachable(sub_node, sub_to_node)) {
+          continue;
+        }
+        std::shared_ptr<TopoEdge> topo_edge_ptr;
+        topo_edge_ptr.reset(
+            new TopoEdge(out_edge->PbEdge(), sub_node, sub_to_node));
+        sub_node->AddOutEdge(topo_edge_ptr.get());
+        sub_to_node->AddInEdge(topo_edge_ptr.get());
+        topo_edges_.push_back(std::move(topo_edge_ptr));
+      }
+    } else {
+      if (sub_node->GetOutEdgeTo(out_edge->ToNode()) != nullptr) {
+        continue;
+      }
+      std::shared_ptr<TopoEdge> topo_edge_ptr;
+      topo_edge_ptr.reset(
+          new TopoEdge(out_edge->PbEdge(), sub_node, out_edge->ToNode()));
+      sub_node->AddOutEdge(topo_edge_ptr.get());
+      topo_edges_.push_back(std::move(topo_edge_ptr));
+    }
+  }
+}
+    
+    double Accel6b::longitudinal_acceleration(const std::uint8_t *bytes,
+                                          const int32_t length) const {
+  DCHECK_GE(length, 4);
+  return parse_two_frames(bytes[2], bytes[3]);
+}
+    
+    bool Brakeinfo74::is_abs_enabled(const std::uint8_t *bytes,
+                                 int32_t length) const {
+  Byte frame(bytes + 7);
+  return frame.is_bit_1(3);
+}
+    
+    using ::apollo::drivers::canbus::Byte;
+    
+    int32_t Gear67::gear_state(const std::uint8_t *bytes, int32_t length) const {
+  Byte frame(bytes + 0);
+  int32_t x = frame.get_byte(0, 3);
+  return x;
+}
+    
+    int32_t Gps6e::seconds(const std::uint8_t *bytes, int32_t length) const {
+  Byte frame(bytes + 5);
+  int32_t x = frame.get_byte(0, 6);
+  return x;
+}
