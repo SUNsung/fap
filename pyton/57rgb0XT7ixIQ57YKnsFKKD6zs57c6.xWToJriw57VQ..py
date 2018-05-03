268c398b56a3d52afb9239460cc68b27aaa6df3d
SUNsung <weatherius@gmail.com>
2018-05-03 21:34:39 +0300

    Rewrite [57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py] File in: 2018-05-03 21:34:39

--- pyton/57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py
+++ pyton/57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py
@@ -1,176 +1,78 @@
 
         
-        with io.open(README_FILE, 'w', encoding='utf-8') as f:
-    f.write(header)
-    f.write(options)
-    f.write(footer)
-
-    
-        def _real_extract(self, url):
-        playlist_id = self._match_id(url)
-    
-    import re
-    
-    from .onet import OnetBaseIE
+            def _find_available_spot(self, vehicle):
+        '''Find an available spot where vehicle can fit, or return None'''
+        pass
     
     
-@keras_test
-def test_upsampling1d_legacy_interface():
-    old_layer = keras.layers.UpSampling1D(length=3, name='us1d')
-    new_layer_1 = keras.layers.UpSampling1D(size=3, name='us1d')
-    new_layer_2 = keras.layers.UpSampling1D(3, name='us1d')
-    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer_1.get_config())
-    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer_2.get_config())
+class GroupChat(Chat):
     
-    - Models
-    About Keras models
-        explain when one should use Sequential or functional API
-        explain compilation step
-        explain weight saving, weight loading
-        explain serialization, deserialization
-    Sequential
-    Model (functional API)
-    
-    print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
-    
-    from keras import backend as K
-from keras.models import Sequential, Model
-from keras.layers import convolutional_recurrent, Input
-from keras.utils.test_utils import layer_test
-from keras import regularizers
-    
-    
-collect_ignore = [
-    # deprecated or moved modules
-    'scrapy/conf.py',
-    'scrapy/stats.py',
-    'scrapy/project.py',
-    'scrapy/utils/decorator.py',
-    'scrapy/statscol.py',
-    'scrapy/squeue.py',
-    'scrapy/log.py',
-    'scrapy/dupefilter.py',
-    'scrapy/command.py',
-    'scrapy/linkextractor.py',
-    'scrapy/spider.py',
-    
-        def __enter__(self):
-        from scrapy.utils.test import get_testenv
-        pargs = [sys.executable, '-u', '-m', 'scrapy.utils.benchserver']
-        self.proc = subprocess.Popen(pargs, stdout=subprocess.PIPE,
-                                     env=get_testenv())
-        self.proc.stdout.readline()
-    
-        def syntax(self):
-        return '[options] <spider>'
-    
-            return requests
-    
-        if WSAStringToAddressA(
-            ip_string,
-            address_family,
-            None,
-            ctypes.byref(addr),
-            ctypes.byref(addr_size)
-    ) != 0:
-        raise socket.error(ctypes.FormatError())
-    
-    A Parser needs a TokenStream as input (which in turn is usually fed by a
-Lexer):
-    
-            Python does not have any size restrictions, but the compilation of
-        such large source files seems to be pretty memory hungry. The memory
-        consumption of the python process grew to >1.5GB when importing a
-        15MB lexer, eating all my swap space and I was to impacient to see,
-        if it could finish at all. With packed initializers that are unpacked
-        at import time of the lexer module, everything works like a charm.
-        
+            (2016-01, url0), 1
+        (2016-01, url0), 1
+        (2016-01, url1), 1
         '''
-        
-        ret = []
-        for i in range(len(string) / 2):
-            (n, v) = ord(string[i*2]), ord(string[i*2+1])
+        url = self.extract_url(line)
+        period = self.extract_year_month(line)
+        yield (period, url), 1
     
-            Negative indexes are allowed.  LA(-1) is previous token (token
-	just matched).  LA(-i) where i is before first token should
-	yield -1, invalid char / EOF.
-	'''
-        
-        raise NotImplementedError
-        
+            The shuffle/sort step of MapReduce will then do a
+        distributed sort on the keys, resulting in:
     
-        def __init__(
-        self,
-        recognizer, decisionNumber,
-        eot, eof, min, max, accept, special, transition
-        ):
-        ## Which recognizer encloses this DFA?  Needed to check backtracking
-        self.recognizer = recognizer
+    from mock import MagicMock
+from nose.tools import eq_
+from hamcrest import assert_that, has_entries
     
-        def test_eq(self):
-        self.assertTrue(self.vhost1b == self.vhost1)
-        self.assertFalse(self.vhost1 == self.vhost2)
-        self.assertEqual(str(self.vhost1b), str(self.vhost1))
-        self.assertFalse(self.vhost1b == 1234)
     
-        def __init__(self, args):
-        '''Initializes the plugin with the given command line args'''
-        self._temp_dir = tempfile.mkdtemp()
-        self.le_config = util.create_le_config(self._temp_dir)
-        config_dir = util.extract_configs(args.configs, self._temp_dir)
-        self._configs = [
-            os.path.join(config_dir, config)
-            for config in os.listdir(config_dir)]
+def BuildYcmdLibs( args ):
+  if not args.skip_build:
+    subprocess.check_call( [
+      sys.executable,
+      p.join( DIR_OF_THIS_SCRIPT, 'third_party', 'ycmd', 'build.py' )
+    ] )
     
-        @mock.patch('certbot.notify.smtplib.LMTP')
-    @mock.patch('certbot.notify.subprocess.Popen')
-    def test_everything_fails(self, mock_popen, mock_lmtp):
-        from certbot.notify import notify
-        lmtp_obj = mock.MagicMock()
-        mock_lmtp.return_value = lmtp_obj
-        lmtp_obj.sendmail.side_effect = socket.error(17)
-        proc = mock.MagicMock()
-        mock_popen.return_value = proc
-        proc.communicate.side_effect = OSError('What we have here is a '
-                                               'failure to communicate.')
-        self.assertFalse(notify('Goose', 'auntrhody@example.com',
-                                'The old grey goose is dead.'))
-        self.assertEqual(lmtp_obj.sendmail.call_count, 1)
-        self.assertEqual(proc.communicate.call_count, 1)
+    print 'POTFILE := ' + os.path.join(I18N_PATH, 'r2.pot')
     
-    For full examples, see `certbot.plugins`.
+        g = Globals(config, global_conf, app_conf, paths)
+    config['pylons.app_globals'] = g
     
-                group.append(HTML('htmlout.html').render())
-            print('Rendered page {} of the directory {}'.format(str(i), operating_sys))
-            i += 1
-        
-        allmd.clear()
+        def is_user_loggedin(self, user):
+        if not (user or self.current_user()):
+            return False
+        return True
     
-    print 'PLUGIN_I18N_PATHS := ' + ','.join(os.path.relpath(plugin.path)
-                                         for plugin in plugins
-                                         if plugin.needs_translation)
+            controllers = importlib.import_module(self.package_name +
+                                              '.controllers')
+        controllers.load_controllers()
+        self.config['r2.plugins'].load_controllers()
+        self._controllers = controllers
+        self._check_csrf_prevention()
     
-        return config
-
+        @require_oauth2_scope('identity')
+    @validate(
+        VUser(),
+        fields=VList(
+            'fields',
+            choices=PREFS_JSON_SPEC.spec.keys(),
+            error=errors.errors.NON_PREFERENCE,
+        ),
+    )
+    @api_doc(api_section.account, uri='/api/v1/me/prefs')
+    def GET_prefs(self, fields):
+        '''Return the preference settings of the logged in user'''
+        resp = PrefsJsonTemplate(fields).data(c.oauth_user)
+        return self.api_wrapper(resp)
     
-    class HTTPTooManyRequests(webob.exc.HTTPClientError):
-    code = 429
-    title = 'Too Many Requests'
-    explanation = ('The server has received too many requests from the client.')
+    class ButtonsController(RedditController):
+    def get_wrapped_link(self, url, link = None, wrapper = None):
+        try:
+            links = []
+            if link:
+                links = [link]
+            else:
+                sr = None if isinstance(c.site, FakeSubreddit) else c.site
+                try:
+                    links = Link._by_url(url, sr)
+                except NotFound:
+                    pass
     
-        @validate(VAdmin(),
-              award = VAwardByCodename('awardcn'),
-              recipient = nop('recipient'),
-              desc = nop('desc'),
-              url = nop('url'),
-              hours = nop('hours'))
-    def GET_give(self, award, recipient, desc, url, hours):
-        if award is None:
-            abort(404, 'page not found')
-    
-    from r2.controllers.api_docs import api_doc, api_section
-from r2.controllers.oauth2 import allow_oauth2_access
+            MinimalController.pre(self)
