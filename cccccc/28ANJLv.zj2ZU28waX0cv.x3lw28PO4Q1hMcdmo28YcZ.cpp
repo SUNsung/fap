dfd4fcce5be3270b0ba257110e057028026adf1a
SUNsung <weatherius@gmail.com>
2018-05-03 22:46:28 +0300

    Rewrite [28ANJLv.zj2ZU28waX0cv.x3lw28PO4Q1hMcdmo28YcZ.cpp] File in: 2018-05-03 22:46:27

--- cccccc/28ANJLv.zj2ZU28waX0cv.x3lw28PO4Q1hMcdmo28YcZ.cpp
+++ cccccc/28ANJLv.zj2ZU28waX0cv.x3lw28PO4Q1hMcdmo28YcZ.cpp
@@ -1,61 +1,144 @@
 
         
-        // dims=[5, 4, 3, 2]->[5, 3, 2, 4]
-TEST_F(MatrixTest, RotatingTranspose_1_3) {
-  GENERIC_2D_ARRAY<int> m;
-  src_.RotatingTranspose(dims_, kNumDims_, 1, 3, &m);
-  m.ResizeNoInit(kInputSize_ / 4, 4);
-  // Verify that the result is:
-  // output tensor=[[[[0, 6, 12, 18][1, 7, 13, 19]]
-  //                 [[2, 8, 14, 20][3, 9, 15, 21]]
-  //                 [[4, 10, 16, 22][5, 11, 17, 23]]]
-  //                [[[24, 30, 36, 42]...
-  EXPECT_EQ(0, m(0, 0));
-  EXPECT_EQ(6, m(0, 1));
-  EXPECT_EQ(1, m(1, 0));
-  EXPECT_EQ(2, m(2, 0));
-  EXPECT_EQ(3, m(3, 0));
-  EXPECT_EQ(4, m(4, 0));
-  EXPECT_EQ(5, m(5, 0));
-  EXPECT_EQ(24, m(6, 0));
-  EXPECT_EQ(30, m(6, 1));
+        class FooTest : public ::testing::TestWithParam<const char*> {
+  // You can implement all the usual class fixture members here.
+};
+    
+    INSTANTIATE_TEST_CASE_P(InstantiationName,
+                        FooTest,
+                        Values('meeny', 'miny', 'moe'));
+    
+    template <typename T>
+class TypeWithoutFormatter<T, kProtobuf> {
+ public:
+  static void PrintValue(const T& value, ::std::ostream* os) {
+    const ::testing::internal::string short_str = value.ShortDebugString();
+    const ::testing::internal::string pretty_str =
+        short_str.length() <= kProtobufOneLinerMaxLength ?
+        short_str : ('\n' + value.DebugString());
+    *os << ('<' + pretty_str + '>');
+  }
+};
+    
+    namespace internal {
+    }
+    
+    
+    {  return AssertionFailure() << pred_text << '('
+                            << e1 << ', '
+                            << e2 << ', '
+                            << e3 << ', '
+                            << e4 << ') evaluates to false, where'
+                            << '\n' << e1 << ' evaluates to ' << v1
+                            << '\n' << e2 << ' evaluates to ' << v2
+                            << '\n' << e3 << ' evaluates to ' << v3
+                            << '\n' << e4 << ' evaluates to ' << v4;
 }
     
-    // Computes and returns the dot product of the n-vectors u and v.
-// Uses Intel SSE intrinsics to access the SIMD instruction set.
-double DotProductSSE(const double* u, const double* v, int n);
-// Computes and returns the dot product of the n-vectors u and v.
-// Uses Intel SSE intrinsics to access the SIMD instruction set.
-int32_t IntDotProductSSE(const int8_t* u, const int8_t* v, int n);
+    #ifndef GTEST_INCLUDE_GTEST_INTERNAL_GTEST_DEATH_TEST_INTERNAL_H_
+#define GTEST_INCLUDE_GTEST_INTERNAL_GTEST_DEATH_TEST_INTERNAL_H_
     
-    class QRSequenceGenerator {
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9, typename T10,
+    typename T11, typename T12, typename T13, typename T14, typename T15,
+    typename T16, typename T17>
+class ValueArray17 {
  public:
-  // Object is initalized with the size of the output range.
-  explicit QRSequenceGenerator(int N) : N_(N), next_num_(0) {
-    num_bits_ = static_cast<int>(ceil(log(static_cast<double>(N)) / log(2.0)));
+  ValueArray17(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9,
+      T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15, T16 v16,
+      T17 v17) : v1_(v1), v2_(v2), v3_(v3), v4_(v4), v5_(v5), v6_(v6), v7_(v7),
+      v8_(v8), v9_(v9), v10_(v10), v11_(v11), v12_(v12), v13_(v13), v14_(v14),
+      v15_(v15), v16_(v16), v17_(v17) {}
+    }
+    
+    // INTERNAL IMPLEMENTATION - DO NOT USE IN USER CODE.
+//
+// Stores a parameter value and later creates tests parameterized with that
+// value.
+template <class TestClass>
+class ParameterizedTestFactory : public TestFactoryBase {
+ public:
+  typedef typename TestClass::ParamType ParamType;
+  explicit ParameterizedTestFactory(ParamType parameter) :
+      parameter_(parameter) {}
+  virtual Test* CreateTest() {
+    TestClass::SetParam(&parameter_);
+    return new TestClass();
   }
     }
     
-    /**
- * Flushes pending writes. This method should not be called before invocation of
- * on_stream_ready() method of the bidirectional_stream_callback.
- * For each previously called bidirectional_stream_write()
- * a corresponding on_write_completed() callback will be invoked when the buffer
- * is sent.
- */
-GRPC_SUPPORT_EXPORT
-void bidirectional_stream_flush(bidirectional_stream* stream);
+      tuple& operator=(const tuple& t) { return CopyFrom(t); }
     
-    int main(int argc, char** argv) {
-  grpc::testing::InitTest(&argc, &argv, true);
-    }
+      void operator delete(void* block, size_t /* allocation_size */) {
+    allocated_--;
+    free(block);
+  }
     
-    namespace grpc_node_generator {
-    }
     
-     private:
-  static Result Sample();
+    {  new_tensor->tensor_id = THDState::s_nextId++;
+  return new_tensor;
+}
     
-      // Return the current qps (i.e query count divided by the time since this
-  // QpsGauge object created (or Reset() was called))
-  long Get();
+    
+    {  delete tensor;
+  delete tensor2;
+  cout << 'OK' << endl;
+  return 0;
+}
+
+    
+    #include 'torch/csrc/Exceptions.h'
+#include 'torch/csrc/utils/auto_gil.h'
+#include 'torch/csrc/utils/python_scalars.h'
+    
+    THDTensorDescriptor THDTensorDescriptor_newFromTHCudaByteTensor(THCudaByteTensor *tensor) {
+  return at::getType(at::Backend::CUDA, at::ScalarType::Byte).unsafeTensorFromTH((void*)tensor, true);
+}
+#endif
+
+    
+      /**
+   * @brief Optionally handle each published event via the logger.
+   *
+   * It is possible to skip the database representation of event subscribers
+   * and instead forward each added event to the active logger plugin.
+   */
+  virtual Status logEvent(const std::string& /*s*/) {
+    return Status(1, 'Not enabled');
+  }
+    
+    
+    {// Remove registry-helper macros from the SDK.
+#undef REGISTER
+#define REGISTER 'Do not REGISTER in the osquery SDK'
+#undef REGISTER_INTERNAL
+#define REGISTER_INTERNAL 'Do not REGISTER_INTERNAL in the osquery SDK'
+#undef CREATE_REGISTRY
+#define CREATE_REGISTRY 'Do not CREATE_REGISTRY in the osquery SDK'
+#undef CREATE_LAZY_REGISTRY
+#define CREATE_LAZY_REGISTRY 'Do not CREATE_LAZY_REGISTRY in the osquery SDK'
+}
+
+    
+    
+    {  /// IOCTL API handling mutex.
+  lck_mtx_t *mtx;
+} osquery = {.open_count = 0,
+             .buffer = NULL,
+             .buf_size = 0,
+             .md = NULL,
+             .mm = NULL,
+             .devfs = NULL,
+             .major_number = OSQUERY_MAJOR};
+    
+    #include <osquery/config.h>
+#include <osquery/database.h>
+    
+    FLAG(int32, test_int32, 1, 'none');
+FLAG_ALIAS(google::int32, test_int32_alias, test_int32);
+    
+    TEST_F(StatusTests, test_constructor) {
+  auto s = Status(5, 'message');
+  EXPECT_EQ(s.getCode(), 5);
+  EXPECT_EQ(s.getMessage(), 'message');
+}
