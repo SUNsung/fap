a3146e086fd82529083c205d1e47cd8bb52e32e4
SUNsung <weatherius@gmail.com>
2018-05-04 01:59:38 +0300

    Rewrite [21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb] File in: 2018-05-04 01:59:37

--- ruby/21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb
+++ ruby/21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb
@@ -1,47 +1,81 @@
 
         
-              # This gets the value of the block with the given key.
-      def get(key)
-        key    = Regexp.quote(key)
-        regexp = /^#\s*VAGRANT-BEGIN:\s*#{key}$\r?\n?(.*?)\r?\n?^#\s*VAGRANT-END:\s*#{key}$\r?\n?/m
-        match  = regexp.match(@value)
-        return nil if !match
-        match[1]
+            # Gets the last git commit information formatted into a String by the provided
+    # pretty format String. See the git-log documentation for valid format placeholders
+    def self.last_git_commit_formatted_with(pretty_format, date_format = nil)
+      command = ['git log -1']
+      command << '--pretty=\'#{pretty_format}\''
+      command << '--date=\'#{date_format}\'' if date_format
+      Actions.sh(command.compact.join(' '), log: false).chomp
+    rescue
+      nil
+    end
+    
+          it 'does set the source directory' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            cloc(source_directory: 'MyCoolApp')
+          end').runner.execute(:test)
+    
+        alias_method :insert_before, :insert
+    
+        def to_json
+      JSON.generate(as_json)
+    end
+  end
+end
+
+    
+          if options && options[:ignore_case]
+        base = base.downcase
+        dest = dest.downcase
       end
     
-    module VagrantPlugins
-  module CommandBox
-    module Command
-      class Repackage < Vagrant.plugin('2', :command)
-        def execute
-          opts = OptionParser.new do |o|
-            o.banner = 'Usage: vagrant box repackage <name> <provider> <version>'
-          end
-    
-      it 'does not decode a float when fewer bytes than a float remain and the '*' modifier is passed' do
-    [ ['\xff', []],
-      ['\xff\x00', []],
-      ['\xff\x00\xff', []]
-    ].should be_computed_by(:unpack, unpack_format('*'))
-  end
-    
-        def paragraphize(input)
-      '<p>#{input.lstrip.rstrip.gsub(/\n\n/, '</p><p>').gsub(/\n/, '<br/>')}</p>'
-    end
-  end
-end
-    
-    When /^(?:|I )fill in '([^']*)' with '([^']*)'$/ do |field, value|
-  fill_in(field, :with => value)
-end
-    
-        def self.definitions_for(klass)
-      instance.definitions_for(klass)
-    end
-    
-          class ValidateAttachmentContentTypeMatcher
-        def initialize attachment_name
-          @attachment_name = attachment_name
-          @allowed_types = []
-          @rejected_types = []
+            css('code').each do |node|
+          node.inner_html = node.inner_html.squish
         end
+    
+                  # An IO::WaitReadable means there may be more IO but this
+              # IO object is not ready to be read from yet. No problem,
+              # we read as much as we can, so we break.
+              breakable = true
+            elsif e.is_a?(Errno::EAGAIN)
+              # Otherwise, we just look for the EAGAIN error which should be
+              # all that IO::WaitReadable does in Ruby 1.9.
+              breakable = true
+            end
+    
+      if options.respond_to? 'keys'
+    options.each do |k,v|
+      unless v.nil?
+        v = v.join ',' if v.respond_to? 'join'
+        v = v.to_json if v.respond_to? 'keys'
+        output += ' data-#{k.sub'_','-'}='#{v}''
+      end
+    end
+  elsif options.respond_to? 'join'
+    output += ' data-value='#{config[key].join(',')}''
+  else
+    output += ' data-value='#{config[key]}''
+  end
+  output += '></#{tag}>'
+end
+    
+    end
+    
+      context 'called with null values' do
+    it 'writes rules for other three' do
+      ruleset = 'border-top-style: inset; ' +
+                'border-right-style: none; ' +
+                'border-left-style: double;'
+      bad_rule = 'border-bottom-style: null;'
+    
+          expect('.all-buttons-hover').to have_ruleset(ruleset)
+    end
+  end
+end
+
+    
+    describe 'margin' do
+  before(:all) do
+    ParserSupport.parse_file('library/margin')
+  end
