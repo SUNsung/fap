d828c6efa0558dbca2ba8faf80e5b8f3163b8acd
SUNsung <weatherius@gmail.com>
2018-05-05 02:11:23 +0300

    Rewrite [52OoP4VmtUm2Q52rbB.Bc7w0s52r8fc3m15LO2522rd.py] File in: 2018-05-05 02:11:22

--- pyton/52OoP4VmtUm2Q52rbB.Bc7w0s52r8fc3m15LO2522rd.py
+++ pyton/52OoP4VmtUm2Q52rbB.Bc7w0s52r8fc3m15LO2522rd.py
@@ -1,67 +1,122 @@
 
         
-        def _worker(executor_reference, work_queue):
-    try:
-        while True:
-            work_item = work_queue.get(block=True)
-            if work_item is not None:
-                work_item.run()
-                continue
-            executor = executor_reference()
-            # Exit if:
-            #   - The interpreter is shutting down OR
-            #   - The executor that owns the worker has been collected OR
-            #   - The executor that owns the worker has been shutdown.
-            if _shutdown or executor is None or executor._shutdown:
-                # Notice other workers
-                work_queue.put(None)
-                return
-            del executor
-    except BaseException:
-        _base.LOGGER.critical('Exception in worker', exc_info=True)
+        FLAGS = flags.FLAGS
     
-    def download_urls_with_executor(urls, executor, timeout=60):
-    try:
-        url_to_content = {}
-        future_to_url = dict((executor.submit(load_url, url, timeout), url)
-                             for url in urls)
+    # We provide more than 19 entries here in case of boards larger than 19 x 19.
+_SGF_COLUMNS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+_KGS_COLUMNS = 'ABCDEFGHJKLMNOPQRSTUVWXYZ'
     
-    # The encoding of source files.
-#source_encoding = 'utf-8'
+        self.assertEqual(coords.to_sgf((0, 8)), 'ia')
+    self.assertEqual(coords.to_flat(utils_test.BOARD_SIZE, (0, 8)), 8)
+    self.assertEqual(coords.to_kgs(utils_test.BOARD_SIZE, (0, 8)), 'J9')
+    self.assertEqual(coords.to_pygtp(utils_test.BOARD_SIZE, (0, 8)), (9, 9))
+    
+    tf.logging.set_verbosity(tf.logging.ERROR)
+    
+        self.assertEqualNPArray(f[:, :, 1], utils_test.load_board('''
+      X.X......
+      .X.......''' + EMPTY_ROW * 7))
+    
+    TEST_SGF = '''(;CA[UTF-8]SZ[9]PB[Murakawa Daisuke]PW[Iyama Yuta]KM[6.5]
+           HA[0]RE[W+1.5]GM[1];B[fd];W[cf])'''
+    
+      def to_sgf(self, use_comments=True):
+    assert self.result_string is not None
+    pos = self.root.position
+    if use_comments:
+      comments = self.comments or ['No comments.']
+      comments[0] = ('Resign Threshold: %0.3f\n' %
+                     self.resign_threshold) + comments[0]
+    else:
+      comments = []
+    return sgf_wrapper.make_sgf(
+        self.board_size, pos.recent, self.result_string,
+        white_name=os.path.basename(self.network.save_file) or 'Unknown',
+        black_name=os.path.basename(self.network.save_file) or 'Unknown',
+        comments=comments)
+    
+        self.assertEqualNPArray(
+        lib_tracker1.liberty_cache, lib_tracker2.liberty_cache)
     
     
-# Not caching the result of this function; users shouldn't have to restart Vim
-# after running the install script or setting the
-# `g:ycm_server_python_interpreter` option.
-def PathToPythonInterpreter():
-  # Not calling the Python interpreter to check its version as it significantly
-  # impacts startup time.
-  from ycmd import utils
+def get_args(args_list):
+    parser = argparse.ArgumentParser(
+        description='ansible inventory script reading from serf cluster')
+    mutex_group = parser.add_mutually_exclusive_group(required=True)
+    help_list = 'list all hosts from serf cluster'
+    mutex_group.add_argument('--list', action='store_true', help=help_list)
+    help_host = 'display variables for a host'
+    mutex_group.add_argument('--host', help=help_host)
+    return parser.parse_args(args_list)
+    
+        try:
+        s1 = os.lstat(path)
+    except OSError:
+        # the OSError should be handled with more care
+        # it could be a 'permission denied' but path is still a mount
+        return False
+    else:
+        # A symlink can never be a mount point
+        if os.path.stat.S_ISLNK(s1.st_mode):
+            return False
+    
+    # Note, sha1 is the only hash algorithm compatible with python2.4 and with
+# FIPS-140 mode (as of 11-2014)
+try:
+    from hashlib import sha1 as sha1
+except ImportError:
+    from sha import sha as sha1
+    
+            url = 'https://api.shippable.com/runs?projectIds=%s&runNumbers=%s' % (project_id, run_number)
+    
+        def on_open_shell(self):
+        try:
+            self._exec_cli_command(b'environment no more')
+        except AnsibleConnectionFailure:
+            raise AnsibleConnectionFailure('unable to set terminal parameters')
+
     
     
-# This is kind of hacky, but run some of the HTTPServer and web tests
-# through WSGIContainer and WSGIApplication to make sure everything
-# survives repeated disassembly and reassembly.
-class WSGIConnectionTest(httpserver_test.HTTPConnectionTest):
-    def get_app(self):
-        return WSGIContainer(validator(WSGIApplication(self.get_handlers())))
+    def traceIn(self, ruleName, ruleIndex):
+        inputSymbol = '%s line=%d:%s' % (self.input.LT(1),
+                                         self.getLine(),
+                                         self.getCharPositionInLine()
+                                         )
+        
+        BaseRecognizer.traceIn(self, ruleName, ruleIndex, inputSymbol)
     
-        For use on platforms that don't have os.pipe() (or where pipes cannot
-    be passed to select()), but do have sockets.  This includes Windows
-    and Jython.
-    '''
-    def __init__(self):
-        from .auto import set_close_exec
-        # Based on Zope select_trigger.py:
-        # https://github.com/zopefoundation/Zope/blob/master/src/ZServer/medusa/thread/select_trigger.py
+                                s = self.eot[s]
+                            input.consume()
+                            # TODO: I had this as return accept[eot[s]]
+                            # which assumed here that the EOT edge always
+                            # went to an accept...faster to do this, but
+                            # what about predicated edges coming from EOT
+                            # target?
+                            continue
     
-    define('ioloop', type=str, default=None)
+        packages = find_packages('src'),
+    package_dir = {'' : 'src'},
     
-        def new_messages(self, messages):
-        logging.info('Sending new message to %r listeners', len(self.waiters))
-        for future in self.waiters:
-            future.set_result(messages)
-        self.waiters = set()
-        self.cache.extend(messages)
-        if len(self.cache) > self.cache_size:
-            self.cache = self.cache[-self.cache_size:]
+        def prepare(self, **kwargs):
+        content = get_content(self.url)
+        self.title = match1(content, r'<title>([^<]+)</title>')
+        s = match1(content, r'P\.s\s*=\s*\'([^\']+)\'')
+        scp = match1(content, r'InfoQConstants\.scp\s*=\s*\'([^\']+)\'')
+        scs = match1(content, r'InfoQConstants\.scs\s*=\s*\'([^\']+)\'')
+        sck = match1(content, r'InfoQConstants\.sck\s*=\s*\'([^\']+)\'')
+    
+    from .tudou import tudou_download_by_id
+from .youku import youku_download_by_vid
+from xml.dom.minidom import parseString
+    
+    
+def mtv81_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_content(url)
+    title = HTMLParser().unescape(
+        '|'.join(match1(html, r'<title>(.*?)</title>').split('|')[:-2]))
+    
+            # extract raw urls
+        orig_img = match1(content,
+                         r'<meta itemprop='image' content='([^']+/originals/[^']+)'')
+        twit_img = match1(content,
+                          r'<meta property='twitter:image:src' name='twitter:image:src' content='([^']+)'')
