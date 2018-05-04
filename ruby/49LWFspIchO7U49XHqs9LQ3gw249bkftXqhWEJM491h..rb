5850839be98e1943838aeb618f2905b64c30d861
SUNsung <weatherius@gmail.com>
2018-05-04 23:22:27 +0300

    Rewrite [49LWFspIchO7U49XHqs9LQ3gw249bkftXqhWEJM491h..rb] File in: 2018-05-04 23:22:27

--- ruby/49LWFspIchO7U49XHqs9LQ3gw249bkftXqhWEJM491h..rb
+++ ruby/49LWFspIchO7U49XHqs9LQ3gw249bkftXqhWEJM491h..rb
@@ -1,40 +1,70 @@
 
         
-              topic.feature_topic_users(args)
+        class JavaScriptHelperTest < ActionView::TestCase
+  tests ActionView::Helpers::JavaScriptHelper
+    
+        # Expose one or more attributes within a block. Old values are returned after the block concludes.
+    # Example demonstrating the common use of needing to set Current attributes outside the request-cycle:
+    #
+    #   class Chat::PublicationJob < ApplicationJob
+    #     def perform(attributes, room_number, creator)
+    #       Current.set(person: creator) do
+    #         Chat::Publisher.publish(attributes: attributes, room_number: room_number)
+    #       end
+    #     end
+    #   end
+    def set(set_attributes)
+      old_attributes = compute_attributes(set_attributes.keys)
+      assign_attributes(set_attributes)
+      yield
+    ensure
+      assign_attributes(old_attributes)
     end
     
-      def self.authenticate(user, app_id, json_response, challenges)
-    response = U2F::SignResponse.load_from_json(json_response)
-    registration = user.u2f_registrations.find_by_key_handle(response.key_handle)
-    u2f = U2F::U2F.new(app_id)
-    
-      private
-    
-      public
-  def close; end;
-    
-      extend self
-    
-        def _2
-      elements[3]
-    end
-    
-      context 'called with one color' do
-    it 'applies same color to all sides' do
-      rule = 'border-color: #f00'
-    
-    describe 'border-width' do
-  before(:all) do
-    ParserSupport.parse_file('library/border-width')
+      test 'token_and_options returns nil with no value after the equal sign' do
+    actual = ActionController::HttpAuthentication::Token.token_and_options(malformed_request).first
+    assert_nil actual
   end
     
-      context 'called with null values' do
-    it 'writes rules for other three' do
-      ruleset = 'padding-top: 11px; ' +
-                'padding-right: 12px; ' +
-                'padding-left: 13px;'
-      bad_rule = 'padding-bottom: null;'
+          def handle_exception_with_mailer_class(exception)
+        if klass = mailer_class
+          klass.handle_exception exception
+        else
+          raise exception
+        end
+      end
+  end
+end
+
     
-      context 'called with one size' do
-    it 'applies same width to both height and width' do
-      rule = 'height: 10px; width: 10px;'
+    require 'active_support/log_subscriber'
+    
+        class DeliveryJob < ActionMailer::DeliveryJob # :nodoc:
+      def perform(mailer, mail_method, delivery_method, params, *args)
+        mailer.constantize.with(params).public_send(mail_method, *args).send(delivery_method)
+      end
+    end
+  end
+end
+
+    
+    
+    {    def replace_escaping(less)
+      less = less.gsub(/~'([^']+)'/, '\1').gsub(/~'([^']+)'/, '\1') # Get rid of ~'' escape
+      less.gsub!(/\$\{([^}]+)\}/, '$\1') # Get rid of @{} escape
+      less.gsub!(/'([^'\n]*)(\$[\w\-]+)([^'\n]*)'/, ''\1#{\2}\3'') # interpolate variable in string, e.g. url('$file-1x') => url('#{$file-1x}')
+      less.gsub(/(\W)e\(%\('?([^']*)'?\)\)/, '\1\2') # Get rid of e(%('')) escape
+    end
+    
+      # Full error reports are disabled and caching is turned on.
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+    
+    require 'rake/testtask'
+Rake::TestTask.new do |t|
+  t.libs << 'test'
+  t.test_files = FileList['test/**/*_test.rb']
+  t.verbose = true
+end
+    
+                      include Something
