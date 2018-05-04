e70712575c25b8918c640c79b7cea7d6455be8a5
SUNsung <weatherius@gmail.com>
2018-05-04 14:32:54 +0300

    Rewrite [264QfYILhqLr626tSxsWDpzew26Gx5TWgyd2Mc26u9I.cpp] File in: 2018-05-04 14:32:54

--- cccccc/264QfYILhqLr626tSxsWDpzew26Gx5TWgyd2Mc26u9I.cpp
+++ cccccc/264QfYILhqLr626tSxsWDpzew26Gx5TWgyd2Mc26u9I.cpp
@@ -1,180 +1,272 @@
 
         
-        
-    { protected:
-  /**
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (N \times 1 \times K) @f$ or, if out_max_val
-   *      @f$ (N \times 2 \times K) @f$ unless axis set than e.g.
-   *      @f$ (N \times K \times H \times W) @f$ if axis == 1
-   *      the computed outputs @f$
-   *       y_n = \arg\max\limits_i x_{ni}
-   *      @f$ (for @f$ K = 1 @f$).
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  /// @brief Not implemented (non-differentiable function)
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
-    NOT_IMPLEMENTED;
+          std::string toStyledString() const;
+    
+      // Find a file by file name.
+  bool FindFileByName(const string& filename,
+                      FileDescriptorProto* output);
+    
+    // CodeGenerator implementation which generates a C++ source file and
+// header.  If you create your own protocol compiler binary and you want
+// it to support C++ output, you can do so by registering an instance of this
+// CodeGenerator with the CommandLineInterface in your main() function.
+class LIBPROTOC_EXPORT CppGenerator : public CodeGenerator {
+ public:
+  CppGenerator();
+  ~CppGenerator();
+    }
+    
+      std::string namespace_;
+  std::string reflectionClassname_;
+    
+    
+    
+    #include <google/protobuf/compiler/csharp/csharp_doc_comment.h>
+#include <google/protobuf/compiler/csharp/csharp_helpers.h>
+#include <google/protobuf/compiler/csharp/csharp_repeated_primitive_field.h>
+    
+    
+    
+    #ifndef incl_HPHP_WORKLOAD_STATS_H_
+#define incl_HPHP_WORKLOAD_STATS_H_
+    
+    int gettime(clockid_t clock, timespec* ts) {
+  if (clock != CLOCK_THREAD_CPUTIME_ID) {
+    return folly::chrono::clock_gettime(clock, ts);
   }
-  bool out_max_val_;
-  size_t top_k_;
-  bool has_axis_;
-  int axis_;
-};
-    
-    namespace caffe {
     }
     
-    
-    {  int size_;
-  Dtype alpha_, beta_, k_;
-};
-#endif
-    
-    
-    {  bool handles_setup_;
-  cudnnHandle_t             handle_;
-  cudnnTensorDescriptor_t bottom_desc_, top_desc_;
-  cudnnPoolingDescriptor_t  pooling_desc_;
-  cudnnPoolingMode_t        mode_;
-};
-#endif
-    
-    namespace caffe {
-    }
-    
-    
-    { protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual inline bool reverse_dimensions() { return true; }
-  virtual void compute_output_shape();
-};
-    
-     protected:
-  /**
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the computed outputs. At training time, we have @f$
-   *      y_{\mbox{train}} = \left\{
-   *         \begin{array}{ll}
-   *            \frac{x}{1 - p} & \mbox{if } u > p \\
-   *            0 & \mbox{otherwise}
-   *         \end{array} \right.
-   *      @f$, where @f$ u \sim U(0, 1)@f$ is generated independently for each
-   *      input at each iteration. At test time, we simply have
-   *      @f$ y_{\mbox{test}} = \mathbb{E}[y_{\mbox{train}}] = x @f$.
+      /**
+   * Add a piece of response to the pipeline.
    */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    }
+  static void AddToPipeline(const std::string &s);
     
+    //////////////////////////////////////////////////////////////////////
     
-    {  /* Invoked when data is read into the buffer passed to
-   * bidirectional_stream_read(). Only part of the buffer may be
-   * populated. To continue reading, call bidirectional_stream_read().
-   * It may be invoked after on_response_trailers_received()}, if there was
-   * pending read data before trailers were received.
+    #include 'hphp/runtime/vm/jit/vasm-unit.h'
+    
+      /*
+   * Create a block intended to be used temporarily, as part of modifying
+   * existing code.
    *
-   * If |bytes_read| is 0, it means the remote side has signaled that it will
-   * send no more data; future calls to bidirectional_stream_read()
-   * will result in the on_data_read() callback or on_succeded() callback if
-   * bidirectional_stream_write() was invoked with end_of_stream set to
-   * true.
+   * Although not necessary for correctness, the block may be freed with
+   * freeScratchBlock when finished.
    */
-  void (*on_read_completed)(bidirectional_stream* stream,
-                            char* data,
-                            int bytes_read);
+  Vlabel makeScratchBlock();
     
-    #include <set>
     
-    #include <gflags/gflags.h>
-#include <grpc/grpc.h>
-#include <grpc/support/time.h>
+    {
+    {
+    {}}}
+
     
-    namespace grpc_node_generator {
+    #ifndef incl_HPHP_JIT_TC_PROLOGUE_H_
+#define incl_HPHP_JIT_TC_PROLOGUE_H_
+    
+    std::unique_ptr<ScenarioResult> RunScenario(
+    const grpc::testing::ClientConfig& client_config, size_t num_clients,
+    const grpc::testing::ServerConfig& server_config, size_t num_servers,
+    int warmup_seconds, int benchmark_seconds, int spawn_local_worker_count,
+    const grpc::string& qps_server_target_override,
+    const grpc::string& credential_type, bool run_inproc);
+    
+    #include <iostream>
+#include <vector>
+    
+    class CodegenTestMinimal : public ::testing::Test {};
+    
+        // Taken from Caffe2
+    REGISTER_OPERATOR_SCHEMA(Softplus)
+        .Description('Softplus takes one input data (Tensor<T>) and produces one output '
+            'data (Tensor<T>) where the function, y = ln(1 + exp(steepness * x)), is '
+            'applied to the tensor elementwise.')
+        .Input('X', 'Input tensor, typically 1-D.', 'T')
+        .Output('Y', 'Output tensor of same shape and type as input X.', 'T')
+        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
+            'Constrain input and output types to float tensors.');
+    
+        //‘GREATER’, ‘LESS’, ‘EQUALS,
+    REGISTER_BINARY_COMPARISON_OPERATOR_SCHEMA(Greater)
+        REGISTER_BINARY_COMPARISON_OPERATOR_SCHEMA(Less)
+        REGISTER_BINARY_COMPARISON_OPERATOR_SCHEMA(Equal)
+    
+        // Taken from ONNX
+    REGISTER_OPERATOR_SCHEMA(BatchNormalization)
+        .Description('Carries out batch normalization as described in the paper'
+            'https://arxiv.org/abs/1502.03167. Depending on the mode it is being run,'
+            'there are multiple cases for the number of outputs, which we list below:'
+            ''
+            'Output case #1: Y, mean, var, saved_mean, saved_var (training mode)'
+            'Output case #2: Y (test mode)')
+        .Input('X',
+            'The input 4-dimensional tensor of shape NCHW or NHWC depending '
+            'on the order parameter.',
+            'T')
+        .Input('scale',
+            'The scale as a 1-dimensional tensor of size C to be applied to the '
+            'output.',
+            'T')
+        .Input('B',
+            'The bias as a 1-dimensional tensor of size C to be applied to the '
+            'output.',
+            'T')
+        .Input('mean',
+            'The running mean (training) or the estimated mean (testing) '
+            'as a 1-dimensional tensor of size C.',
+            'T')
+        .Input('var',
+            'The running variance (training) or the estimated '
+            'variance (testing) as a 1-dimensional tensor of size C.',
+            'T')
+        .Output('Y', 'The output 4-dimensional tensor of the same shape as X.',
+            'T')
+        .Output('mean',
+            'The running mean after the BatchNormalization operator. Must be in-place '
+            'with the input mean. Should not be used for testing.',
+            'T')
+        .Output('var',
+            'The running variance after the BatchNormalization operator. Must be '
+            'in-place with the input var. Should not be used for testing.',
+            'T')
+        .Output('saved_mean',
+            'Saved mean used during training to speed up gradient '
+            'computation. Should not be used for testing.',
+            'T')
+        .Output('saved_var',
+            'Saved variance used during training to speed up '
+            'gradient computation. Should not be used for testing.',
+            'T')
+        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
+            'Constrain input and output types to float tensors.')
+        .Attr('epsilon',
+            'The epsilon value to use to avoid division by zero.',
+            AttrType::AttributeProto_AttributeType_FLOAT)
+        .Attr('is_test',
+            'If set to nonzero, run spatial batch normalization in test mode.',
+            AttrType::AttributeProto_AttributeType_INT)
+        .Attr('momentum',
+            'Factor used in computing the running mean and variance.'
+            'e.g., running_mean = running_mean * momentum + mean * (1 - momentum)',
+            AttrType::AttributeProto_AttributeType_FLOAT)
+        .Attr('spatial',
+            'Compute the mean and variance across all spatial elements or per feature.',
+            AttrType::AttributeProto_AttributeType_INT);
+    
+    
+    {        totalNumSequences += chunk.NumberOfSequences();
+        m_chunkToFileIndex.insert(make_pair(&chunk, m_latticeFiles.size() - 1));
+        m_chunks.push_back(&chunk);
+        if (m_chunks.size() >= numeric_limits<ChunkIdType>::max())
+            RuntimeError('Number of chunks exceeded overflow limit.');
     }
     
-    #ifndef TEST_QPS_STATS_UTILS_H
-#define TEST_QPS_STATS_UTILS_H
+    cudaStream_t GPUDataTransferer::s_assignStream = NULL;
     
-    class SubProcess {
- public:
-  SubProcess(const std::vector<std::string>& args);
-  ~SubProcess();
+    class DNS {
+  public:
+   typedef std::vector<std::string> (*DNSFunc)(const std::string& host);
     }
     
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
     
-    { private:
-  const FilterPolicy* policy_;
-  const char* data_;    // Pointer to filter data (at block-start)
-  const char* offset_;  // Pointer to beginning of offset array (at block-end)
-  size_t num_;          // Number of entries in offset array
-  size_t base_lg_;      // Encoding parameter (see kFilterBaseLg in .cc file)
-};
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
     
-    // A very simple random number generator.  Not especially good at
-// generating truly random bits, but good enough for our needs in this
-// package.
-class Random {
- private:
-  uint32_t seed_;
- public:
-  explicit Random(uint32_t s) : seed_(s & 0x7fffffffu) {
-    // Avoid bad seeds.
-    if (seed_ == 0 || seed_ == 2147483647L) {
-      seed_ = 1;
+    TEST(IOBuf, QueueAppenderInsertClone) {
+  IOBuf buf{IOBuf::CREATE, 100};
+  folly::IOBufQueue queue;
+  QueueAppender appender{&queue, 100};
+  // Buffer is shared, so we create a new buffer to write to
+  appender.insert(buf);
+  uint8_t x = 42;
+  appender.pushAtMost(&x, 1);
+  EXPECT_EQ(2, queue.front()->countChainElements());
+  EXPECT_EQ(0, queue.front()->length());
+  EXPECT_LT(0, queue.front()->tailroom());
+  EXPECT_EQ(1, queue.front()->next()->length());
+  EXPECT_EQ(x, queue.front()->next()->data()[0]);
+}
+    
+    /**
+ * Get current exceptions being handled.  front() is the most recent exception.
+ * There should be at most one unless rethrowing.
+ */
+std::vector<ExceptionInfo> getCurrentExceptions();
+    
+        // Try to grow in place.
+    //
+    // Note that xallocx(MALLOCX_ZERO) will only zero newly allocated memory,
+    // even if a previous allocation allocated more than we requested.
+    // This is fine; we always use MALLOCX_ZERO with jemalloc and we
+    // always expand our allocation to the real size.
+    if (prevCapacity * sizeof(ElementWrapper) >= jemallocMinInPlaceExpandable) {
+      success =
+          (xallocx(threadEntry->elements, newByteSize, 0, MALLOCX_ZERO) ==
+           newByteSize);
+    }
+    
+    #pragma once
+    
+        copy.unshareOne();
+    EXPECT_TRUE(buf->isShared());
+    EXPECT_FALSE(copy.isShared());
+    EXPECT_NE((void*)buf->data(), (void*)copy.data());
+    EXPECT_TRUE(copy2.isShared());
+    
+    #include <sys/stat.h>
+#include <sys/types.h>
+#include <cstddef>
+#include <string>
+#include <utility>
+#include <vector>
+    
+    
+    {
+    {    for (size_t i = 0; i < nrRead; i++) {
+      int id = completed[i] - ops.get();
+      EXPECT_GE(id, 0);
+      EXPECT_LT(id, specs.size());
+      EXPECT_TRUE(pending[id]);
+      pending[id] = false;
+      ssize_t res = ops[id].result();
+      EXPECT_LE(0, res) << folly::errnoStr(-res);
+      EXPECT_EQ(specs[id].size, res);
     }
   }
-  uint32_t Next() {
-    static const uint32_t M = 2147483647L;   // 2^31-1
-    static const uint64_t A = 16807;  // bits 14, 8, 7, 5, 2, 1, 0
-    // We are computing
-    //       seed_ = (seed_ * A) % M,    where M = 2^31-1
-    //
-    // seed_ must not be zero or M, or else all subsequent computed values
-    // will be zero or M respectively.  For all other values, seed_ will end
-    // up cycling through every number in [1,M-1]
-    uint64_t product = seed_ * A;
+  EXPECT_EQ(specs.size(), aioReader.totalSubmits());
+  EXPECT_EQ(aioReader.pending(), 0);
+  EXPECT_EQ(aioQueue.queued(), 0);
+  for (size_t i = 0; i < pending.size(); i++) {
+    EXPECT_FALSE(pending[i]);
+  }
+}
+    
+    template <typename Observable, typename Traits>
+class ObserverCreator<Observable, Traits>::Context {
+ public:
+  template <typename... Args>
+  Context(Args&&... args) : observable_(std::forward<Args>(args)...) {
+    updateValue();
+  }
     }
-    }
-    
-      // Get initial measurement of the space we will be reading.
-  const int64_t initial_size = Size(Key(0), Key(n));
-  const int64_t initial_other_size = Size(Key(n), Key(kCount));
-    
-    extern JSClass  *jsb_cocos2d_Physics3DConstraint_class;
-extern JSObject *jsb_cocos2d_Physics3DConstraint_prototype;
-    
-    #if COCOS2D_DEBUG >= 1
-    tolua_lerror:
-    tolua_error(tolua_S,'#ferror in function 'lua_cocos2dx_physics_PhysicsBody_resetForces'.',&tolua_err);
-#endif
     
     
+    {
+    {
+    {  std::once_flag observerInit_;
+  folly::Optional<Observer<T>> observer_;
+};
+} // namespace observer
+} // namespace folly
     
-    
-    {			b2Vec2 anchor(160.0f + 2.0f * N, -0.125f);
-			jd.Initialize(prevBody, ground, anchor);
-			m_world->CreateJoint(&jd);
-		}
+      /**
+   * Gets current view of the observed object.
+   * This is safe to call from any thread. If this is called from other Observer
+   * functor then that Observer is marked as dependent on current Observer.
+   */
+  VersionedData getData();
