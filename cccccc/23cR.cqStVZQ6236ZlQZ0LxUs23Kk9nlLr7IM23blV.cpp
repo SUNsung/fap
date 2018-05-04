0c45c64e61b478b930c25d592934053f757aebef
SUNsung <weatherius@gmail.com>
2018-05-04 12:34:27 +0300

    Rewrite [23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp] File in: 2018-05-04 12:34:26

--- cccccc/23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp
+++ cccccc/23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp
@@ -1,172 +1,53 @@
 
         
-          // During the first Compute(), resource is either created or looked up using
-  // shared_name. In the latter case, the resource found should be verified if
-  // it is compatible with this op's configuration. The verification may fail in
-  // cases such as two graphs asking queues of the same shared name to have
-  // inconsistent capacities.
-  virtual Status VerifyResource(T* resource) { return Status::OK(); }
-    
-    
-    {  void CopyDeviceTensorToCPU(const Tensor *device_tensor, StringPiece edge_name,
-                             Device *device, Tensor *cpu_tensor,
-                             StatusCallback done) override;
-};
-    
-      CHECK(worker_name.size() < max_worker_name_length)
-      << 'Specified worker name is too long.';
-  snprintf(my_name, max_worker_name_length, worker_name.c_str());
-  std::vector<char> worker_names(number_of_procs * max_worker_name_length);
-  MPI_CHECK(MPI_Allgather(my_name, max_worker_name_length, MPI_CHAR,
-                          &worker_names[0], max_worker_name_length, MPI_CHAR,
-                          MPI_COMM_WORLD));
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    REGISTER_OP_NO_GRADIENT('Floor');
-REGISTER_OP_NO_GRADIENT('FloorDiv');
-REGISTER_OP_NO_GRADIENT('TruncateDiv');
-    
-    
-    {inline void AddTraceEvent(const TraceEvent &event, string *json) {
-  Appendf(json, R'({'pid':%u,'tid':%u,'ts':%.5f,)', event.device_id(),
-          event.resource_id(), event.timestamp_ps() / kPicosPerMicro);
-  AppendEscapedName(json, event.name());
-  StrAppend(json, ',');
-  if (event.duration_ps() > 0) {
-    Appendf(json, R'('ph':'X','dur':%.5f},)',
-            event.duration_ps() / kPicosPerMicro);
-  } else {
-    StrAppend(json, R'('ph':'i','s':'t'},)');
+          void OCRTester(const char* imgname, const char* groundtruth, const char* tessdatadir, const char* lang) {
+    //log.info() << tessdatadir << ' for language: ' << lang << std::endl;
+    char *outText;
+    std::locale loc('C'); // You can also use '' for the default system locale
+    std::ifstream file(groundtruth);
+    file.imbue(loc); // Use it for file input
+    std::string gtText((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+    ASSERT_FALSE(api->Init(tessdatadir, lang)) << 'Could not initialize tesseract.';
+    Pix *image = pixRead(imgname);
+    ASSERT_TRUE(image != nullptr) << 'Failed to read test image.';
+    api->SetImage(image);
+    outText = api->GetUTF8Text();
+    EXPECT_EQ(gtText,outText) << 'Phototest.tif OCR does not match ground truth for ' << ::testing::PrintToString(lang);
+    api->End();
+    delete [] outText;
+    pixDestroy(&image);
   }
+    
+    
+    {    while (next_num_ < kMaxNaturalNumberValue) {
+      n = GetBinaryReversedInteger(next_num_++);
+      if (n < N_) break;
+    }
+    return (next_num_ > kMaxNaturalNumberValue) ? kInvalidVal : n;
+  }
+    
+        return 0;
 }
-    
-    TEST_F(GrpcDebugTest, SendDebugTensorsThroughMultipleRoundsUnderReadWriteMode) {
-  // Prepare the tensor to send.
-  const DebugNodeKey kDebugNodeKey('/job:localhost/replica:0/task:0/cpu:0',
-                                   'test_namescope/test_node', 0,
-                                   'DebugIdentity');
-  Tensor tensor(DT_INT32, TensorShape({1, 1}));
-  tensor.flat<int>()(0) = 42;
-    }
-    
-      // Size of the sink buffer where the compressed/decompressed data produced by
-  // zlib is cached.
-  int64 output_buffer_size = 256 << 10;
-    
-      /// The time point type of the clock.
-  typedef typename clock_type::time_point time_point;
-    
-    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
-# pragma once
-#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
-    
-    #endif // BOOST_ASIO_DETAIL_ARRAY_FWD_HPP
-
-    
-    #ifndef BOOST_ASIO_DETAIL_EVENT_HPP
-#define BOOST_ASIO_DETAIL_EVENT_HPP
-    
-    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
-# pragma once
-#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
-    
-    #define BOOST_ASIO_DEFINE_HANDLER_PTR(op) \
-  struct ptr \
-  { \
-    Handler* h; \
-    void* v; \
-    op* p; \
-    ~ptr() \
-    { \
-      reset(); \
-    } \
-    void reset() \
-    { \
-      if (p) \
-      { \
-        p->~op(); \
-        p = 0; \
-      } \
-      if (v) \
-      { \
-        boost_asio_handler_alloc_helpers::deallocate(v, sizeof(op), *h); \
-        v = 0; \
-      } \
-    } \
-  } \
-  /**/
-    
-    template <typename T> T& lvref();
-template <typename T> T& lvref(T);
-template <typename T> const T& clvref();
-template <typename T> const T& clvref(T);
-template <typename T> char argbyv(T);
-    
-    std::size_t sync_read(int d, state_type state, buf* bufs,
-    std::size_t count, bool all_empty, boost::system::error_code& ec)
+int lua_cocos2dx_cocosdenshion_SimpleAudioEngine_isBackgroundMusicPlaying(lua_State* tolua_S)
 {
-  if (d == -1)
-  {
-    ec = boost::asio::error::bad_descriptor;
-    return 0;
-  }
-    }
+    int argc = 0;
+    CocosDenshion::SimpleAudioEngine* cobj = nullptr;
+    bool ok  = true;
     
-    template <typename Time_Traits>
-void dev_poll_reactor::schedule_timer(timer_queue<Time_Traits>& queue,
-    const typename Time_Traits::time_type& time,
-    typename timer_queue<Time_Traits>::per_timer_data& timer, wait_op* op)
-{
-  boost::asio::detail::mutex::scoped_lock lock(mutex_);
-    }
+    			b2Vec2 vertices[3];
+			vertices[0] = b2Mul(xf1, b2Vec2(-1.0f, 0.0f));
+			vertices[1] = b2Mul(xf1, b2Vec2(1.0f, 0.0f));
+			vertices[2] = b2Mul(xf1, b2Vec2(0.0f, 0.5f));
+			
+			b2PolygonShape poly1;
+			poly1.Set(vertices, 3);
     
-    #include <boost/asio/detail/pop_options.hpp>
+    	static Test* Create()
+	{
+		return new BodyTypes;
+	}
     
-    #include <osquery/core.h>
-    
-    /**
- * @brief Perform an action while waiting for an the extension timeout.
- *
- * We define a 'global' extension timeout using CLI flags.
- * There are several locations where code may act assuming an extension has
- * loaded or broadcasted a registry.
- *
- * @param predicate return true or set stop to end the timeout loop.
- * @return the last status from the predicate.
- */
-Status applyExtensionDelay(std::function<Status(bool& stop)> predicate);
-    
-    #include <osquery/config.h>
-#include <osquery/core.h>
-#include <osquery/database.h>
-#include <osquery/events.h>
-#include <osquery/extensions.h>
-#include <osquery/filesystem.h>
-#include <osquery/flags.h>
-#include <osquery/logger.h>
-#include <osquery/registry.h>
-#include <osquery/sql.h>
-#include <osquery/status.h>
-#include <osquery/tables.h>
-    
-    #include <boost/filesystem/path.hpp>
-    
-    CLI_FLAG(string,
-         config_path,
-         (fs::path(OSQUERY_HOME) / 'osquery.conf').make_preferred().string(),
-         'Path to JSON config file');
-    
-    FLAG_ALIAS(bool, shell_only_alias, shell_only);
-    
-    typedef std::vector<uint8_t> Unit;
-typedef std::vector<Unit> UnitVector;
-typedef int (*UserCallback)(const uint8_t *Data, size_t Size);
-    
-    namespace fuzzer {
-    }
+    				b2Vec2 anchor(-15.0f + 2.0f * i, 15.0f);
+				jd.Initialize(prevBody, body, anchor);
+				m_world->CreateJoint(&jd);
