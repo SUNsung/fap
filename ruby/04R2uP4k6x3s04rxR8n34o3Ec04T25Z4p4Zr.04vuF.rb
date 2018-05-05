479ccc6232c6f26fea3e5a9016072536f0818a6b
SUNsung <weatherius@gmail.com>
2018-05-05 16:54:38 +0300

    Rewrite [04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb] File in: 2018-05-05 16:54:37

--- ruby/04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb
+++ ruby/04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb
@@ -1,10 +1,188 @@
 
         
-                def signal_end_of_output
-          output_pipe.puts(END_OF_OUTPUT_SIGNAL)
-          STDOUT.flush
+                  assert_match 'Could not load the 'ridiculous' Active Record adapter. Ensure that the adapter is spelled correctly in config/database.yml and that you've added the necessary adapter gem to your Gemfile.', error.message
         end
     
-      desc 'Started'
-  task :started do
+    module ActiveSupport
+  # Abstract super class that provides a thread-isolated attributes singleton, which resets automatically
+  # before and after each request. This allows you to keep all the per-request attributes easily
+  # available to the whole system.
+  #
+  # The following full app-like example demonstrates how to use a Current class to
+  # facilitate easy access to the global, per-request attributes without passing them deeply
+  # around everywhere:
+  #
+  #   # app/models/current.rb
+  #   class Current < ActiveSupport::CurrentAttributes
+  #     attribute :account, :user
+  #     attribute :request_id, :user_agent, :ip_address
+  #
+  #     resets { Time.zone = nil }
+  #
+  #     def user=(user)
+  #       super
+  #       self.account = user.account
+  #       Time.zone    = user.time_zone
+  #     end
+  #   end
+  #
+  #   # app/controllers/concerns/authentication.rb
+  #   module Authentication
+  #     extend ActiveSupport::Concern
+  #
+  #     included do
+  #       before_action :authenticate
+  #     end
+  #
+  #     private
+  #       def authenticate
+  #         if authenticated_user = User.find_by(id: cookies.encrypted[:user_id])
+  #           Current.user = authenticated_user
+  #         else
+  #           redirect_to new_session_url
+  #         end
+  #       end
+  #   end
+  #
+  #   # app/controllers/concerns/set_current_request_details.rb
+  #   module SetCurrentRequestDetails
+  #     extend ActiveSupport::Concern
+  #
+  #     included do
+  #       before_action do
+  #         Current.request_id = request.uuid
+  #         Current.user_agent = request.user_agent
+  #         Current.ip_address = request.ip
+  #       end
+  #     end
+  #   end
+  #
+  #   class ApplicationController < ActionController::Base
+  #     include Authentication
+  #     include SetCurrentRequestDetails
+  #   end
+  #
+  #   class MessagesController < ApplicationController
+  #     def create
+  #       Current.account.messages.create(message_params)
+  #     end
+  #   end
+  #
+  #   class Message < ApplicationRecord
+  #     belongs_to :creator, default: -> { Current.user }
+  #     after_create { |message| Event.create(record: message) }
+  #   end
+  #
+  #   class Event < ApplicationRecord
+  #     before_create do
+  #       self.request_id = Current.request_id
+  #       self.user_agent = Current.user_agent
+  #       self.ip_address = Current.ip_address
+  #     end
+  #   end
+  #
+  # A word of caution: It's easy to overdo a global singleton like Current and tangle your model as a result.
+  # Current should only be used for a few, top-level globals, like account, user, and request details.
+  # The attributes stuck in Current should be used by more or less all actions on all requests. If you start
+  # sticking controller-specific attributes in there, you're going to create a mess.
+  class CurrentAttributes
+    include ActiveSupport::Callbacks
+    define_callbacks :reset
+    
+    module Fun
+  class GamesController < ActionController::Base
+    def render_hello_world
+      render inline: 'hello: <%= stratego %>'
+    end
+  end
+    
+      test 'token_and_options returns correct token with quotes' do
+    token = '\'quote\' pretty'
+    actual = ActionController::HttpAuthentication::Token.token_and_options(sample_request(token)).first
+    expected = token
+    assert_equal(expected, actual)
+  end
+    
+        test 'head :reset_content (205) does not return any content' do
+      content = body(HeadController.action(:reset_content).call(Rack::MockRequest.env_for('/')))
+      assert_empty content
+    end
+    
+      test 'default delivery options can be overridden per mail instance' do
+    settings = {
+      address: 'localhost',
+      port: 25,
+      domain: 'localhost.localdomain',
+      user_name: nil,
+      password: nil,
+      authentication: nil,
+      enable_starttls_auto: true
+    }
+    assert_equal settings, ActionMailer::Base.smtp_settings
+    overridden_options = { user_name: 'overridden', password: 'somethingobtuse' }
+    mail_instance = DeliveryMailer.welcome(delivery_method_options: overridden_options)
+    delivery_method_instance = mail_instance.delivery_method
+    assert_equal 'overridden', delivery_method_instance.settings[:user_name]
+    assert_equal 'somethingobtuse', delivery_method_instance.settings[:password]
+    assert_equal delivery_method_instance.settings.merge(overridden_options), delivery_method_instance.settings
+    
+        def unlock_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :unlock_instructions, opts)
+    end
+    
+        def fullpath
+      '/#{@path_prefix}/#{@path}'.squeeze('/')
+    end
+    
+            def update
+          authorize! :update, stock_location
+          if stock_location.update_attributes(stock_location_params)
+            respond_with(stock_location, status: 200, default_template: :show)
+          else
+            invalid_resource!(stock_location)
+          end
+        end
+    
+            def on_percent_literal(node)
+          each_unnecessary_space_match(node) do |range|
+            add_offense(node, location: range)
+          end
+        end
+    
+            def variables_in_node(node)
+          if node.or_type?
+            node.node_parts
+                .flat_map { |node_part| variables_in_node(node_part) }
+                .uniq
+          else
+            variables_in_simple_node(node)
+          end
+        end
+    
+    module RuboCop
+  module Cop
+    module Style
+      # This cop check for uses of Object#freeze on immutable objects.
+      #
+      # @example
+      #   # bad
+      #   CONST = 1.freeze
+      #
+      #   # good
+      #   CONST = 1
+      class RedundantFreeze < Cop
+        include FrozenStringLiteral
+    
+              def plugins
+            @plugins ||= find_plugins_gem_specs.map do |spec|
+              { :name => spec.name, :version => spec.version.to_s }
+            end.sort_by do |spec|
+              spec[:name]
+            end
+          end
+    
+    describe 'modular-scale' do
+  before(:all) do
+    ParserSupport.parse_file('library/modular-scale')
   end
