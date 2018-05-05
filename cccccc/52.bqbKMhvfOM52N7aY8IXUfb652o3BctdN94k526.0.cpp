b65f8e45be39d37176c975d77e0761ee2adff89d
SUNsung <weatherius@gmail.com>
2018-05-05 08:07:48 +0300

    Rewrite [52.bqbKMhvfOM52N7aY8IXUfb652o3BctdN94k526.0.cpp] File in: 2018-05-05 08:07:47

--- cccccc/52.bqbKMhvfOM52N7aY8IXUfb652o3BctdN94k526.0.cpp
+++ cccccc/52.bqbKMhvfOM52N7aY8IXUfb652o3BctdN94k526.0.cpp
@@ -1,229 +1,93 @@
 
         
-          // Abort and close all existing sessions, disconnecting their resources from
-  // future sessions.
-  //
-  // Reset() allows misbehaving or slow sessions to be aborted and closed, and
-  // causes their resources eventually to be released.  Reset() does not wait
-  // for the computations in old sessions to cease; it merely starts the
-  // process of tearing them down.  However, if a new session is started after
-  // a Reset(), the new session is isolated from changes that old sessions
-  // (started prior to the Reset()) may continue to make to resources, provided
-  // all those resources are in containers listed in 'containers'.
-  //
-  // Old sessions may continue to have side-effects on resources not in
-  // containers listed in 'containers', and thus may affect future
-  // sessions' results in ways that are hard to predict.  Thus, if well-defined
-  // behavior is desired, is it recommended that all containers be listed in
-  // 'containers'.
-  //
-  // If the 'containers' vector is empty, the default container is assumed.
-  // If the 'containers' vector is non-empty, the default container should be
-  // listed explicitly.
-  //
-  // Sessions that support resource containers should override this function.
-  virtual Status Reset(const SessionOptions& options,
-                       const std::vector<string>& containers) {
-    return errors::Unimplemented('Reset()');
-  }
-    
-    // Prune a model to make it more efficient:
-// * Remove unnecessary operations.
-// * Optimize gradient computations.
-class ModelPruner : public GraphOptimizer {
- public:
-  ModelPruner() {}
-  ~ModelPruner() override {}
-    }
-    
-    #ifndef TENSORFLOW_LIB_IO_RECORD_WRITER_H_
-#define TENSORFLOW_LIB_IO_RECORD_WRITER_H_
-    
-    #if !TENSORFLOW_USE_SYCL
-#error This file must only be included when building TensorFlow with SYCL support
-#endif
-    
-     private:
-  // a length value of kFullExtent (-1) means we have a full slice at this
-  // dimension. It's defined in tensor_slice.cc.
-  static const int64 kFullExtent;
-    
-    // Comparison ops.
-REGISTER_OP_NO_GRADIENT('Less');
-REGISTER_OP_NO_GRADIENT('LessEqual');
-REGISTER_OP_NO_GRADIENT('Greater');
-REGISTER_OP_NO_GRADIENT('GreaterEqual');
-REGISTER_OP_NO_GRADIENT('Equal');
-REGISTER_OP_NO_GRADIENT('NotEqual');
-    
-    #endif // BITCOIN_INDIRECTMAP_H
-
-    
-    LookupKey::LookupKey(const Slice& user_key, SequenceNumber s) {
-  size_t usize = user_key.size();
-  size_t needed = usize + 13;  // A conservative estimate
-  char* dst;
-  if (needed <= sizeof(space_)) {
-    dst = space_;
-  } else {
-    dst = new char[needed];
-  }
-  start_ = dst;
-  dst = EncodeVarint32(dst, usize + 8);
-  kstart_ = dst;
-  memcpy(dst, user_key.data(), usize);
-  dst += usize;
-  EncodeFixed64(dst, PackSequenceAndType(s, kValueTypeForSeek));
-  dst += 8;
-  end_ = dst;
-}
-    
-    int main(int argc, char** argv) {
-  return leveldb::test::RunAllTests();
-}
-
-    
-    TableCache::TableCache(const std::string& dbname,
-                       const Options* options,
-                       int entries)
-    : env_(options->env),
-      dbname_(dbname),
-      options_(options),
-      cache_(NewLRUCache(entries)) {
-}
-    
-    TEST(WriteBatchTest, Multiple) {
-  WriteBatch batch;
-  batch.Put(Slice('foo'), Slice('bar'));
-  batch.Delete(Slice('box'));
-  batch.Put(Slice('baz'), Slice('boo'));
-  WriteBatchInternal::SetSequence(&batch, 100);
-  ASSERT_EQ(100, WriteBatchInternal::Sequence(&batch));
-  ASSERT_EQ(3, WriteBatchInternal::Count(&batch));
-  ASSERT_EQ('Put(baz, boo)@102'
-            'Delete(box)@101'
-            'Put(foo, bar)@100',
-            PrintContents(&batch));
-}
-    
-    #endif  // STORAGE_LEVELDB_HELPERS_MEMENV_MEMENV_H_
-
-    
-    #ifndef STORAGE_LEVELDB_INCLUDE_DUMPFILE_H_
-#define STORAGE_LEVELDB_INCLUDE_DUMPFILE_H_
-    
-        // F1 COEFFICIENT
-    
-    #endif /* OPENCV_CUDA_WARP_REDUCE_HPP__ */
-
-    
-    namespace cv
-{
-    }
-    
-    cv::cuda::GpuMat::GpuMat(const GpuMat& m, Range rowRange_, Range colRange_)
-{
-    flags = m.flags;
-    step = m.step; refcount = m.refcount;
-    data = m.data; datastart = m.datastart; dataend = m.dataend;
-    allocator = m.allocator;
-    }
+          // Set up security params
+  SecurityParams security;
+  security.set_use_test_ca(true);
+  security.set_server_host_override('foo.test.google.fr');
+  client_config.mutable_security_params()->CopyFrom(security);
+  server_config.mutable_security_params()->CopyFrom(security);
     
     
-    {}
-
+    {
+    {}  // namespace testing
+}  // namespace grpc
     
-    
-    {  private:
-    QueueWorker::Request _request;
-  };
-    
-    
-    {} // namespace gloo_cache
-    
-    #define THCPStorage TH_CONCAT_3(THCP,Real,Storage)
-#define THCPStorageStr TH_CONCAT_STRING_3(torch.cuda.,Real,Storage)
-#define THCPStorageClass TH_CONCAT_3(THCP,Real,StorageClass)
-#define THCPStorage_(NAME) TH_CONCAT_4(THCP,Real,Storage_,NAME)
-    
-    /**
- * \fn  std::shared_ptr<xgboost::DMatrix> CreateDMatrix(int rows, int columns, float sparsity, int seed);
+    /*
+ * This implements a Metrics server defined in
+ * src/proto/grpc/testing/metrics.proto. Any
+ * test service can use this to export Metrics (TODO (sreek): Only Gauges for
+ * now).
  *
- * \brief Creates dmatrix with uniform random data between 0-1.
+ * Example:
+ *    MetricsServiceImpl metricsImpl;
+ *    ..
+ *    // Create QpsGauge(s). Note: QpsGauges can be created even after calling
+ *    // 'StartServer'.
+ *    QpsGauge qps_gauge1 = metricsImpl.CreateQpsGauge('foo', is_present);
+ *    // qps_gauge1 can now be used anywhere in the program by first making a
+ *    // one-time call qps_gauge1.Reset() and then calling qps_gauge1.Incr()
+ *    // every time to increment a query counter
  *
- * \param rows      The rows.
- * \param columns   The columns.
- * \param sparsity  The sparsity.
- * \param seed      The seed.
- *
- * \return  The new d matrix.
+ *    ...
+ *    // Create the metrics server
+ *    std::unique_ptr<grpc::Server> server = metricsImpl.StartServer(port);
+ *    server->Wait(); // Note: This is blocking.
  */
+namespace grpc {
+namespace testing {
+    }
+    }
     
     
-    {}  // namespace xgboost
-    
-          CompressedIterator<int> ci(buffer.data(), alphabet_size);
-      std::vector<int> output(input.size());
-      for (int i = 0; i < input.size(); i++) {
-        output[i] = ci[i];
-      }
-    
-    // helper functions
-// set handle
-void setHandle(JNIEnv *jenv, jlongArray jhandle, void* handle) {
-#ifdef __APPLE__
-  jlong out = (long) handle;
-#else
-  int64_t out = (int64_t) handle;
-#endif
-  jenv->SetLongArrayRegion(jhandle, 0, 1, &out);
+    {  return Status(0, 'OK');
 }
     
-    // common regressions
-// linear regression
-struct LinearSquareLoss {
-  // duplication is necessary, as __device__ specifier
-  // cannot be made conditional on template parameter
-  XGBOOST_DEVICE static bst_float PredTransform(bst_float x) { return x; }
-  XGBOOST_DEVICE static bool CheckLabel(bst_float x) { return true; }
-  XGBOOST_DEVICE static bst_float FirstOrderGradient(bst_float predt, bst_float label) {
-    return predt - label;
-  }
-  XGBOOST_DEVICE static bst_float SecondOrderGradient(bst_float predt, bst_float label) {
-    return 1.0f;
-  }
-  template <typename T>
-  static T PredTransform(T x) { return x; }
-  template <typename T>
-  static T FirstOrderGradient(T predt, T label) { return predt - label; }
-  template <typename T>
-  static T SecondOrderGradient(T predt, T label) { return T(1.0f); }
-  static bst_float ProbToMargin(bst_float base_score) { return base_score; }
-  static const char* LabelErrorMsg() { return ''; }
-  static const char* DefaultEvalMetric() { return 'rmse'; }
+    #include <boost/lexical_cast.hpp>
+    
+    class TimeTests : public testing::Test {};
+    
+    #ifdef __cplusplus
+extern 'C' {
+#endif
+#include 'tolua++.h'
+#ifdef __cplusplus
+}
+#endif
+    
+    
+    
+    
+    
+    
+    
+    int lua_cocos2dx_physics_PhysicsContactPostSolve_getFriction(lua_State* tolua_S)
+{
+    int argc = 0;
+    cocos2d::PhysicsContactPostSolve* cobj = nullptr;
+    bool ok  = true;
+    }
+    
+    
+    {	b2Body* m_body;
 };
     
-    // objectives
-#include '../src/objective/objective.cc'
-#include '../src/objective/regression_obj.cc'
-#include '../src/objective/multiclass_obj.cc'
-#include '../src/objective/rank_obj.cc'
+    			for (int32 i = 0; i < 10; ++i)
+			{
+				float32 y2 = hs[i];
+				shape.Set(b2Vec2(x, y1), b2Vec2(x + dx, y2));
+				ground->CreateFixture(&fd);
+				y1 = y2;
+				x += dx;
+			}
     
+      YGNodeCalculateLayout(root, 200, 100, YGDirectionLTR);
     
-    {inline bool RowSet::Load(dmlc::Stream* fi) {
-  if (!fi->Read(&rows_)) return false;
-  if (rows_.size() != 0) return true;
-  return fi->Read(&size_, sizeof(size_)) == sizeof(size_);
+    Config::~Config(void)
+{
+    YGConfigFree(m_config);
 }
-}  // namespace xgboost
     
-    // implementation of inline functions.
-inline void Learner::Predict(const SparseBatch::Inst& inst,
-                             bool output_margin,
-                             HostDeviceVector<bst_float>* out_preds,
-                             unsigned ntree_limit) const {
-  gbm_->PredictInstance(inst, &out_preds->HostVector(), ntree_limit);
-  if (!output_margin) {
-    obj_->PredTransform(out_preds);
-  }
+    void Node::setMarginPercent(int edge, double margin)
+{
+    YGNodeStyleSetMarginPercent(m_node, static_cast<YGEdge>(edge), margin);
 }
