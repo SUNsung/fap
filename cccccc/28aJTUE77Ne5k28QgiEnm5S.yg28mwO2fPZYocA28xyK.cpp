7f263b352920f46e15a6ca8b516d6c35e40f9edc
SUNsung <weatherius@gmail.com>
2018-05-06 01:31:49 +0300

    Rewrite [28aJTUE77Ne5k28QgiEnm5S.yg28mwO2fPZYocA28xyK.cpp] File in: 2018-05-06 01:31:49

--- cccccc/28aJTUE77Ne5k28QgiEnm5S.yg28mwO2fPZYocA28xyK.cpp
+++ cccccc/28aJTUE77Ne5k28QgiEnm5S.yg28mwO2fPZYocA28xyK.cpp
@@ -1,158 +1,101 @@
 
         
-            // Adds the module variable 'api_version'.
-    if (PyModule_AddIntConstant(
-        module,
-        const_cast<char*>(kImplVersionName),
-        kImplVersion))
-#if PY_MAJOR_VERSION < 3
-      return;
-#else
-      { Py_DECREF(module); return NULL; }
-    
-      // Find the file that declares the given fully-qualified symbol name.
-  bool FindFileContainingSymbol(const string& symbol_name,
-                                FileDescriptorProto* output);
-    
-    SourceGeneratorBase::~SourceGeneratorBase() {
+        UnresponsiveSuppressor::~UnresponsiveSuppressor() {
+  g_suppress_level--;
 }
     
-    #include <google/protobuf/compiler/java/java_generator_factory.h>
-    
-    /** Returns the benchmark Reporter instance.
- *
- * The returned instance will take care of generating reports for all the actual
- * reporters configured via the 'enable_*_reporter' command line flags (see
- * benchmark_config.cc). */
-std::shared_ptr<Reporter> GetReporter();
-    
-    int main(int argc, char** argv) {
-  grpc::testing::InitTest(&argc, &argv, true);
+    class UnresponsiveSuppressor {
+ public:
+  UnresponsiveSuppressor();
+  ~UnresponsiveSuppressor();
     }
     
-    namespace grpc_node_generator {
+      // Stop listening for the given |accelerator|, does nothing if shortcut
+  // handling is suspended.
+  void UnregisterAccelerator(const ui::Accelerator& accelerator,
+                             Observer* observer);
+    
+    #include <memory>
+    
+    #include 'chrome/browser/speech/tts_platform.h'
+    
+    class VersionEditTest { };
+    
+    namespace leveldb {
     }
     
+    // A very simple random number generator.  Not especially good at
+// generating truly random bits, but good enough for our needs in this
+// package.
+class Random {
+ private:
+  uint32_t seed_;
+ public:
+  explicit Random(uint32_t s) : seed_(s & 0x7fffffffu) {
+    // Avoid bad seeds.
+    if (seed_ == 0 || seed_ == 2147483647L) {
+      seed_ = 1;
+    }
+  }
+  uint32_t Next() {
+    static const uint32_t M = 2147483647L;   // 2^31-1
+    static const uint64_t A = 16807;  // bits 14, 8, 7, 5, 2, 1, 0
+    // We are computing
+    //       seed_ = (seed_ * A) % M,    where M = 2^31-1
+    //
+    // seed_ must not be zero or M, or else all subsequent computed values
+    // will be zero or M respectively.  For all other values, seed_ will end
+    // up cycling through every number in [1,M-1]
+    uint64_t product = seed_ * A;
+    }
+    }
     
-    {  return 0;
-}
-
+     private:
+  const Options*        options_;
+  std::string           buffer_;      // Destination buffer
+  std::vector<uint32_t> restarts_;    // Restart points
+  int                   counter_;     // Number of entries emitted since restart
+  bool                  finished_;    // Has Finish() been called?
+  std::string           last_key_;
     
-      // gRPC Python
-  grpc_python_generator::GeneratorConfiguration grpc_py_config;
-  grpc_python_generator::PythonGrpcGenerator grpc_py_generator(grpc_py_config);
-  cli.RegisterGenerator('--grpc_python_out', &grpc_py_generator,
-                        'Generate Python source file.');
     
-    int PowerIphone::get_power_seconds_left() {
-	if (UpdatePowerInfo()) {
-		return nsecs_left;
-	} else {
-		return -1;
+    
+        cobj = (CocosDenshion::SimpleAudioEngine*)tolua_tousertype(tolua_S,1,0);
+    
+    
+    
+    class Test;
+struct Settings;
+    
+    
+    {	static Test* Create()
+	{
+		return new AddPair;
 	}
-}
-    
-    	if (p_ssl) {
-		i.ssl_connection = LCCSCF_USE_SSL;
-		if (!verify_ssl)
-			i.ssl_connection |= LCCSCF_ALLOW_SELFSIGNED;
-	} else {
-		i.ssl_connection = 0;
-	}
+};
     
     
-    {	OBJTYPE_WLOCK;
-	ClassInfo *type = classes.getptr(p_class);
-	ClassInfo *check = type;
-	ERR_FAIL_COND(!check);
-	ERR_FAIL_COND(!check->method_map.has(p_method));
-	check->method_map[p_method]->set_hint_flags(p_flags);
-}
-    
-    	ClassDB::bind_method('_class_list_select', &EditorHelp::_class_list_select);
-	ClassDB::bind_method('_class_desc_select', &EditorHelp::_class_desc_select);
-	ClassDB::bind_method('_class_desc_input', &EditorHelp::_class_desc_input);
-	ClassDB::bind_method('_request_help', &EditorHelp::_request_help);
-	ClassDB::bind_method('_unhandled_key_input', &EditorHelp::_unhandled_key_input);
-	ClassDB::bind_method('_search', &EditorHelp::_search);
-	ClassDB::bind_method('_search_cbk', &EditorHelp::_search_cbk);
-	ClassDB::bind_method('_help_callback', &EditorHelp::_help_callback);
-    
-    	void set_roll_influence(float p_value);
-	float get_roll_influence() const;
-    
-    
-    {
-    {
-    {		} break;
-		case MENU_OPTION_DEBUG_UV1: {
-			Ref<Mesh> mesh = node->get_mesh();
-			if (!mesh.is_valid()) {
-				err_dialog->set_text(TTR('No mesh to debug.'));
-				err_dialog->popup_centered_minsize();
-				return;
-			}
-			_create_uv_lines(0);
-		} break;
-		case MENU_OPTION_DEBUG_UV2: {
-			Ref<Mesh> mesh = node->get_mesh();
-			if (!mesh.is_valid()) {
-				err_dialog->set_text(TTR('No mesh to debug.'));
-				err_dialog->popup_centered_minsize();
-				return;
-			}
-			_create_uv_lines(1);
-		} break;
-	}
-}
-    
-    
-    {			undo_redo->add_do_method(func.ptr(), 'set_name', new_name);
-			undo_redo->add_undo_method(func.ptr(), 'set_name', name);
+    {			ground->CreateFixture(&fd);
 		}
-		undo_redo->add_do_method(this, '_update_members');
-		undo_redo->add_undo_method(this, '_update_members');
-		undo_redo->add_do_method(this, '_update_graph');
-		undo_redo->add_undo_method(this, '_update_graph');
-		undo_redo->add_do_method(this, 'emit_signal', 'edited_script_changed');
-		undo_redo->add_undo_method(this, 'emit_signal', 'edited_script_changed');
-		undo_redo->commit_action();
     
-    static const char *FlagValue(const char *Param, const char *Name) {
-  size_t Len = strlen(Name);
-  if (Param[0] == '-' && strstr(Param + 1, Name) == Param + 1 &&
-      Param[Len + 1] == '=')
-      return &Param[Len + 2];
-  return nullptr;
-}
+    			m_bullet = m_world->CreateBody(&bd);
+			m_bullet->CreateFixture(&box, 100.0f);
     
-    void ReadDirToVectorOfUnits(const char *Path, std::vector<Unit> *V,
-                            long *Epoch, size_t MaxSize, bool ExitOnError) {
-  long E = Epoch ? *Epoch : 0;
-  std::vector<std::string> Files;
-  ListFilesInDirRecursive(Path, Epoch, &Files, /*TopDir*/true);
-  size_t NumLoaded = 0;
-  for (size_t i = 0; i < Files.size(); i++) {
-    auto &X = Files[i];
-    if (Epoch && GetEpoch(X) < E) continue;
-    NumLoaded++;
-    if ((NumLoaded & (NumLoaded - 1)) == 0 && NumLoaded >= 1024)
-      Printf('Loaded %zd/%zd files from %s\n', NumLoaded, Files.size(), Path);
-    auto S = FileToVector(X, MaxSize, ExitOnError);
-    if (!S.empty())
-      V->push_back(S);
-  }
-}
+    			bd.position.Set(-1.0f, 0.35f);
+			m_wheel1 = m_world->CreateBody(&bd);
+			m_wheel1->CreateFixture(&fd);
     
-      if (Options.UseCounters) {
-    uint64_t CounterDelta =
-        EF->__sanitizer_update_counter_bitset_and_clear_counters(
-            C->CounterBitmap.data());
-    if (CounterDelta > 0) {
-      Res = true;
-      C->CounterBitmapBits += CounterDelta;
-    }
-  }
+    	if (NULL != _tag) {
+		tag_cstr = env->GetStringUTFChars(_tag, NULL);
+	}
     
-    #include <istream>
-#include <set>
+    #ifdef ANDROID
+    
+        void __DelOlderTouchTime(uint64_t _time);
+    
+        void TestFun0();
+    void TestFun2()  {__TestFun1(1);}
+    
+    
+    
+    #include <jni.h>
