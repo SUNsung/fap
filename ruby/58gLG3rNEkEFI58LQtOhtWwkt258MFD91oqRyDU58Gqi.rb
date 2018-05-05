6ae6a8081e8888cd54e899939448c917aa939be1
SUNsung <weatherius@gmail.com>
2018-05-05 09:42:31 +0300

    Rewrite [58gLG3rNEkEFI58LQtOhtWwkt258MFD91oqRyDU58Gqi.rb] File in: 2018-05-05 09:42:30

--- ruby/58gLG3rNEkEFI58LQtOhtWwkt258MFD91oqRyDU58Gqi.rb
+++ ruby/58gLG3rNEkEFI58LQtOhtWwkt258MFD91oqRyDU58Gqi.rb
@@ -1,3 +1,9 @@
 
         
-        BASE_URI = ENV['BASE_URI'] || 'https://github.com/jondot/awesome-react-native'
+        describe Deliver::HtmlGenerator do
+  let(:generator) { Deliver::HtmlGenerator.new }
+    
+          it 'automatically removes new lines from the version number' do
+        Fastlane::FastFile.new.parse('lane :test do
+          increment_version_number(version_number: '1.77.3\n', bump_type: 'major')
+        end').runner.execute(:test)
