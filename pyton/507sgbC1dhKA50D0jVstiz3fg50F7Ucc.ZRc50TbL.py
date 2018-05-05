eaf8b47d3bb81a498081413a6a8f3d3e105b9a5a
SUNsung <weatherius@gmail.com>
2018-05-05 12:48:46 +0300

    Rewrite [507sgbC1dhKA50D0jVstiz3fg50F7Ucc.ZRc50TbL.py] File in: 2018-05-05 12:48:46

--- pyton/507sgbC1dhKA50D0jVstiz3fg50F7Ucc.ZRc50TbL.py
+++ pyton/507sgbC1dhKA50D0jVstiz3fg50F7Ucc.ZRc50TbL.py
@@ -1,153 +1,37 @@
 
         
-            if not releases:
-        break
-    
-            webpage = self._download_webpage(url, video_id)
-        key = self._search_regex(
-            r'src=['\']https?://[^/]+/embed/([A-Za-z0-9_-]+)', webpage, 'key')
-    
-            return {
-            'id': api_response.get('id', album_url_tag),
-            'uploader': api_response.get('artist'),
-            'title': api_response.get('title'),
-            'url': api_response['url'],
-        }
-    
-    from httpie.utils import repr_dict_nice
-    
-        def get_converter(self, mime):
-        if is_valid_mime(mime):
-            for converter_class in plugin_manager.get_converters():
-                if converter_class.supports(mime):
-                    return converter_class(mime)
-    
-    
-with codecs.open(JSON_FILE_PATH, encoding='utf8') as f:
-    JSON_FILE_CONTENT = f.read()
-    
+            Models: `flatpages.flatpages`
+    Templates: Uses the template defined by the ``template_name`` field,
+        or :template:`flatpages/default.html` if template_name is not defined.
+    Context:
+        flatpage
+            `flatpages.flatpages` object
     '''
-__version__ = '1.0.0-dev'
-__author__ = 'Jakub Roztocil'
-__licence__ = 'BSD'
+    if not url.startswith('/'):
+        url = '/' + url
+    site_id = get_current_site(request).id
+    try:
+        f = get_object_or_404(FlatPage, url=url, sites=site_id)
+    except Http404:
+        if not url.endswith('/') and settings.APPEND_SLASH:
+            url += '/'
+            f = get_object_or_404(FlatPage, url=url, sites=site_id)
+            return HttpResponsePermanentRedirect('%s/' % request.path)
+        else:
+            raise
+    return render_flatpage(request, f)
     
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-    
-    
-def to_pygtp(board_size, coord):
-  '''Converts from a MiniGo coordinate to a pygtp coordinate.'''
-  if coord is None:
-    return gtp.PASS
-  return coord[1] + 1, board_size - coord[0]
-
-    
-      def test_inference(self):
-    with tempfile.TemporaryDirectory() as working_dir, \
-        tempfile.TemporaryDirectory() as export_dir:
-      dualnet.bootstrap(working_dir, model_params.DummyMiniGoParams())
-      exported_model = os.path.join(export_dir, 'bootstrap-model')
-      dualnet.export_model(working_dir, exported_model)
-    
-        Raises:
-      IllegalMove: if the input c is an illegal move.
-    '''
-    if color is None:
-      color = self.to_play
-    
-      def test_is_game_over(self):
-    root = go.Position(utils_test.BOARD_SIZE)
-    self.assertFalse(root.is_game_over())
-    first_pass = root.play_move(None)
-    self.assertFalse(first_pass.is_game_over())
-    second_pass = first_pass.play_move(None)
-    self.assertTrue(second_pass.is_game_over())
+        def __init__(self, rank):
+        self.state = CallState.READY
+        self.rank = rank
+        self.employee = None
     
     
-class CGOSPlayer(CGOSPlayerMixin, GtpInterface):
-  pass
+class Graph(object):
     
-    
-def sgf_prop(value_list):
-  '''Converts raw sgf library output to sensible value.'''
-  if value_list is None:
-    return None
-  if len(value_list) == 1:
-    return value_list[0]
-  else:
-    return value_list
-    
-      def test_make_sgf(self):
-    all_pwcs = list(replay_sgf(utils_test.BOARD_SIZE, NO_HANDICAP_SGF))
-    second_last_position, last_move, _ = all_pwcs[-1]
-    last_position = second_last_position.play_move(last_move)
-    
-      def test_uniqueness(self):
-    all_symmetries_f = [
-        symmetries.apply_symmetry_feat(
-            s, self.feat) for s in symmetries.SYMMETRIES
-    ]
-    all_symmetries_pi = [
-        symmetries.apply_symmetry_pi(
-            utils_test.BOARD_SIZE, s, self.pi) for s in symmetries.SYMMETRIES
-    ]
-    for f1, f2 in itertools.combinations(all_symmetries_f, 2):
-      self.assertNotEqualNPArray(f1, f2)
-    for pi1, pi2 in itertools.combinations(all_symmetries_pi, 2):
-      self.assertNotEqualNPArray(pi1, pi2)
-    
-            bottom = 10 ** np.min([min(np.floor(np.log10(build_time[alg])))
-                               for alg in algorithms])
-    
-        opener = build_opener()
-    html_filename = os.path.join(html_folder, lang + '.html')
-    if not os.path.exists(html_filename):
-        print('Downloading %s' % page)
-        request = Request(page)
-        # change the User Agent to avoid being blocked by Wikipedia
-        # downloading a couple of articles should not be considered abusive
-        request.add_header('User-Agent', 'OpenAnything/1.0')
-        html_content = opener.open(request).read()
-        open(html_filename, 'wb').write(html_content)
-    
-    
-if not os.path.exists(DATA_FOLDER):
-    
-        def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('-l', '--list', dest='list', action='store_true',
-                          help='only list contracts, without checking them')
-        parser.add_option('-v', '--verbose', dest='verbose', default=False, action='store_true',
-                          help='print contract tests for all spiders')
-    
-                elif opt in ('-l', '--playlist', '--playlists'):
-                # Download playlist whenever possible.
-                conf['playlist'] = True
-    
-    __all__ = ['cbs_download']
-    
-            # cookie handler
-        ssl_context = request.HTTPSHandler(
-            context=ssl.SSLContext(ssl.PROTOCOL_TLSv1))
-        cookie_handler = request.HTTPCookieProcessor()
-        opener = request.build_opener(ssl_context, cookie_handler)
-        opener.addheaders = [
-            ('Referer', self.url),
-            ('Cookie',
-             'CloudFront-Policy=%s;CloudFront-Signature=%s;CloudFront-Key-Pair-Id=%s' % (scp, scs, sck))
-        ]
-        request.install_opener(opener)
-    
-    
-site_info = 'kugou.com'
-download = kugou_download
-# download_playlist = playlist_not_supported('kugou')
-download_playlist=kugou_download_playlist
-
-    
-    __all__ = ['miomio_download']
-    
-    from xml.dom.minidom import parseString
-    
-    __all__ = ['showroom_download']
+            (2016-01, shopping), 125
+        (2016-01, gas), 50
+        '''
+        total = sum(values)
+        self.handle_budget_notifications(key, total)
+        yield key, sum(values)
