21e779fbbbebc42c732e2f64994f592aa2acb3bb
SUNsung <weatherius@gmail.com>
2018-05-05 14:57:58 +0300

    Rewrite [08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py] File in: 2018-05-05 14:57:57

--- pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
+++ pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
@@ -1,122 +1,63 @@
 
         
-            # Then we call the sorting method
-    sort_blocks()
+                class Delegate(HTTPMessageDelegate):
+            def headers_received(self, start_line, headers):
+                test.code = start_line.code
     
-    Available hooks:
+        @gen_test
+    def test_get_clears_timed_out_getters(self):
+        q = queues.Queue()
+        getters = [q.get(timedelta(seconds=0.01)) for _ in range(10)]
+        get = q.get()
+        self.assertEqual(11, len(q._getters))
+        yield gen.sleep(0.02)
+        self.assertEqual(11, len(q._getters))
+        self.assertFalse(get.done())  # Final waiter is still active.
+        q.get()  # get() clears the waiters.
+        self.assertEqual(2, len(q._getters))
+        for getter in getters:
+            self.assertRaises(TimeoutError, getter.result)
     
-                return self.server_sock.accept()[0]
-        except (select.error, socket.error):
-            return None
+            # Make sure the tornado module under test is available to the test
+        # application
+        pythonpath = os.getcwd()
+        if 'PYTHONPATH' in os.environ:
+            pythonpath += os.pathsep + os.environ['PYTHONPATH']
     
-        def lower_items(self):
-        '''Like iteritems(), but with all lowercase keys.'''
-        return (
-            (lowerkey, keyval[1])
-            for (lowerkey, keyval)
-            in self._store.items()
-        )
+        @gen_test
+    def test_yield_lock(self):
+        # Ensure we catch a 'with (yield lock)', which should be
+        # 'with (yield lock.acquire())'.
+        with self.assertRaises(gen.BadYieldError):
+            with (yield locks.Lock()):
+                pass
+    
+        def _on_stream(self, stream):
+        if stream is None:
+            # Session may have expired
+            self.redirect('/auth/login')
+            return
+        self.render('stream.html', stream=stream)
+    
+        @classmethod
+    def setUpClass(cls):
+        ''' Class scope setup. '''
+        cls.p = Proxy()
+    
+    Test code which will almost always fail (if not exactly 12:01) when untestable
+production code (have a look into constructor_injection.py) is used:
+    
+        # verify that none of the target files exist
+    assert(not lexists('foo.txt'))
+    assert(not lexists('bar.txt'))
+    assert(not lexists('baz.txt'))
+    try:
+        with open('foo.txt', 'w'):  # Creating the file
+            pass
+    
+    '''
+http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
+Implementation of the iterator pattern with a generator
     
     
-class HTTPDigestAuth(AuthBase):
-    '''Attaches HTTP Digest Authentication to the given Request object.'''
-    
-        def initialize_options(self):
-        TestCommand.initialize_options(self)
-        try:
-            from multiprocessing import cpu_count
-            self.pytest_args = ['-n', str(cpu_count()), '--boxed']
-        except (ImportError, NotImplementedError):
-            self.pytest_args = ['-n', '1', '--boxed']
-    
-            post1 = requests.post(url, data={'some': 'data'})
-        assert post1.status_code == 200
-    
-            if args.test_results:
-            path = os.path.join(output_dir, '%s/test.json' % job_number)
-            url = 'https://api.shippable.com/jobs/%s/jobTestReports' % job_id
-            download(args, headers, path, url)
-            extract_contents(args, path, os.path.join(output_dir, '%s/test' % job_number))
-    
-            return ret
-    
-        preds = reg.predict(X_test, batch_size=batch_size)
-    assert preds.shape == (num_test, )
-    
-        # by setting the `trainable` argument, in Sequential
-    model = Sequential()
-    layer = Dense(2, input_dim=1)
-    model.add(layer)
-    assert model.trainable_weights == layer.trainable_weights
-    layer.trainable = False
-    assert model.trainable_weights == []
-    
-        layer_test(local.LocallyConnected1D,
-               kwargs={'filters': filters,
-                       'kernel_size': filter_length,
-                       'padding': padding,
-                       'kernel_regularizer': 'l2',
-                       'bias_regularizer': 'l2',
-                       'activity_regularizer': 'l2',
-                       'strides': strides},
-               input_shape=(num_samples, num_steps, input_dim))
-    
-    import numpy as np
-import keras
-from keras.datasets import reuters
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
-from keras.preprocessing.text import Tokenizer
-    
-        # concatenation
-    merged_concat = legacy_layers.merge([rnn_a, rnn_b], mode='concat', concat_axis=-1)
-    model = models.Model([input_a, input_b], [merged_concat])
-    model.compile(loss='mse', optimizer='sgd')
-    model.fit([rand(2, 3), rand(2, 3)], [rand(2, 3, 6)])
-    
-        yaml_str = model.to_yaml()
-    model_from_yaml(yaml_str)
-    
-    print('Train...')
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=4,
-          validation_data=[x_test, y_test])
-
-    
-        def set(self, key, value):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                item.value = value
-                return
-        self.table[hash_index].append(Item(key, value))
-    
-    from mrjob.job import MRJob
-    
-    def get_api_subtype():
-    if is_api() and c.render_style.startswith('api-'):
-        return c.render_style[4:]
-    
-        def live_config(self, name):
-        live = self.stacked_proxy_safe_get(g, 'live_config', {})
-        return live.get(name)
-    
-    api('flairlist', FlairListJsonTemplate)
-api('flaircsv', FlairCsvJsonTemplate)
-api('flairselector', FlairSelectorJsonTemplate)
-    
-                    errpage = pages.InterstitialPage(
-                    _('suspended'),
-                    content=pages.InTimeoutInterstitial(
-                        timeout_days_remaining=timeout_days_remaining,
-                    ),
-                )
-                request.environ['usable_error_content'] = errpage.render()
-    
-            c.allow_framing = True
-    
-    import pylibmc
-from pylons import request, response
-from pylons import app_globals as g
-from pylons.controllers.util import abort
+class SuperUserSpecification(CompositeSpecification):
