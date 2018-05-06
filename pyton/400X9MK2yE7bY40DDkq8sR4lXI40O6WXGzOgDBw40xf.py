2c8caece062f6774e10a5e11d3ed3a7e55fe6743
SUNsung <weatherius@gmail.com>
2018-05-06 07:28:00 +0300

    Rewrite [400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py] File in: 2018-05-06 07:27:59

--- pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
+++ pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
@@ -1,119 +1,111 @@
 
         
-                def check(self, value):
-            return isinstance(value, Foo)
+        
+@keras_test
+def test_weighted_metrics_with_multiple_outputs():
+    decimal = decimal_precision[K.backend()]
     
     
-def info(message, *args):
-    print(message % args, file=sys.stderr)
+def render_function(function, method=True):
+    subblocks = []
+    signature = get_function_signature(function, method=method)
+    signature = signature.replace(function.__module__ + '.', '')
+    level = 3
+    subblocks.append('#' * level + ' ' + function.__name__ + '\n')
+    subblocks.append(code_snippet(signature))
+    docstring = function.__doc__
+    if docstring:
+        subblocks.append(process_docstring(docstring))
+    return '\n\n'.join(subblocks)
+    
+            layer_test(local.LocallyConnected2D,
+                   kwargs={'filters': filters,
+                           'kernel_size': (3, 3),
+                           'padding': padding,
+                           'kernel_regularizer': 'l2',
+                           'bias_regularizer': 'l2',
+                           'activity_regularizer': 'l2',
+                           'strides': strides,
+                           'data_format': 'channels_first'},
+                   input_shape=(num_samples, stack_size, num_row, num_col))
+    
+    print('Convert class vector to binary class matrix '
+      '(for use with categorical_crossentropy)')
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+print('y_train shape:', y_train.shape)
+print('y_test shape:', y_test.shape)
     
     
-def test_config_from_envvar_missing():
-    env = os.environ
+@pytest.mark.parametrize('tensor_shape', [FC_SHAPE, CONV_SHAPE], ids=['FC', 'CONV'])
+def test_normal(tensor_shape):
+    _runner(initializers.RandomNormal(mean=0, stddev=1), tensor_shape,
+            target_mean=0., target_std=1)
+    
+            # Test equivalence of convert_dense_weights_data_format
+        out1 = model1.predict(x)
+        layer_utils.convert_dense_weights_data_format(model1.layers[2], prev_shape, target_data_format)
+        for (src, dst) in zip(model1.layers, model2.layers):
+            dst.set_weights(src.get_weights())
+        out2 = model2.predict(transpose(x))
+    
+    
+@keras_test
+def test_vector_regression():
+    '''
+    Perform float data prediction (regression) using 2 layer MLP
+    with tanh and sigmoid activations.
+    '''
+    (x_train, y_train), (x_test, y_test) = get_test_data(num_train=500,
+                                                         num_test=200,
+                                                         input_shape=(20,),
+                                                         output_shape=(num_classes,),
+                                                         classification=False)
+    
+            start_time = time.time()
+        parallel_model.fit(x, y, epochs=epochs, batch_size=batch_size)
+        total_time = time.time() - start_time
+        print('%d gpus training:' % i, total_time)
+    
+    
+def check_ref_out(ref):
+    '''Ask git to check out the specified ref.'''
     try:
-        os.environ = {'FOO_SETTINGS': 'missing.cfg'}
-        with pytest.raises(IOError) as e:
-            app = flask.Flask(__name__)
-            app.config.from_envvar('FOO_SETTINGS')
-        msg = str(e.value)
-        assert msg.startswith('[Errno 2] Unable to load configuration '
-                              'file (No such file or directory):')
-        assert msg.endswith('missing.cfg'')
-        assert not app.config.from_envvar('FOO_SETTINGS', silent=True)
-    finally:
-        os.environ = env
+        subprocess.check_call(
+            ['git', 'checkout', ref],
+            stdout=DEVNULL,
+            stderr=DEVNULL,
+        )
+    except subprocess.CalledProcessError:
+        logging.error('failed to check out %s', ref)
+        sys.exit(1)
     
-    
-def test_installed_module_paths(modules_tmpdir, modules_tmpdir_prefix,
-                                purge_module, site_packages, limit_loader):
-    site_packages.join('site_app.py').write(
-        'import flask\n'
-        'app = flask.Flask(__name__)\n'
+        FRIEND_JSON_SPEC = VValidatedJSON.PartialObject({
+        'name': VAccountByName('name'),
+        'note': VLength('note', 300),
+    })
+    FRIEND_JSON_VALIDATOR = VValidatedJSON('json', spec=FRIEND_JSON_SPEC,
+                                           body=True)
+    @require_oauth2_scope('subscribe')
+    @validate(
+        VUser(),
+        friend=VAccountByName('username'),
+        notes_json=FRIEND_JSON_VALIDATOR,
     )
-    purge_module('site_app')
+    @api_doc(api_section.users, json_model=FRIEND_JSON_VALIDATOR,
+             uri='/api/v1/me/friends/{username}')
+    def PUT_friends(self, friend, notes_json):
+        '''Create or update a 'friend' relationship.
     
-    from flask._compat import StringIO
-from flask.logging import default_handler, has_level_handler, \
-    wsgi_errors_stream
+    from reddit_base import RedditController, UnloggedUser
+from r2.lib.pages import (ButtonLite, ButtonDemoPanel, WidgetDemoPanel,
+                          BoringPage)
+from r2.lib.pages.things import wrap_links
+from r2.models import *
+from r2.lib.validator import *
+from pylons import request, response
+from pylons import tmpl_context as c
+from pylons.i18n import _
     
-    
-def get_new_command(command):
-    dir = shell.quote(_tar_file(command.script_parts)[1])
-    return shell.and_('mkdir -p {dir}', '{cmd} -C {dir}') \
-        .format(dir=dir, cmd=command.script)
-    
-    
-@pytest.mark.functional
-def test_select_command_with_arrows(proc, TIMEOUT):
-    select_command_with_arrows(proc, TIMEOUT)
-    
-        print('Classifier Training')
-    print('===================')
-    accuracy, train_time, test_time = {}, {}, {}
-    for name in sorted(args['estimators']):
-        clf = ESTIMATORS[name]
-        try:
-            clf.set_params(random_state=0)
-        except (TypeError, ValueError):
-            pass
-    
-        print('')
-    print('')
-
-    
-        dim = start_dim
-    for i in range(0, n):
-        print('============================================')
-        print('Entering iteration %s of %s' % (i, n))
-        print('============================================')
-        dim += step
-        X = np.random.randn(100, dim)
-        Y = np.random.randint(0, n_classes, (100,))
-        bench_scikit_tree_classifier(X, Y)
-        Y = np.random.randn(100)
-        bench_scikit_tree_regressor(X, Y)
-    
-    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
-TRAIN_FOLDER = '20news-bydate-train'
-TEST_FOLDER = '20news-bydate-test'
-    
-    # Split the dataset in training and test set:
-docs_train, docs_test, y_train, y_test = train_test_split(
-    dataset.data, dataset.target, test_size=0.5)
-    
-        in_exercise_region = False
-    
-    
-class View(object):
-    '''Test docstring. '''
-    def __init__(self, root, controller):
-        f = Figure()
-        ax = f.add_subplot(111)
-        ax.set_xticks([])
-        ax.set_yticks([])
-        ax.set_xlim((x_min, x_max))
-        ax.set_ylim((y_min, y_max))
-        canvas = FigureCanvasTkAgg(f, master=root)
-        canvas.show()
-        canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
-        canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
-        canvas.mpl_connect('button_press_event', self.onclick)
-        toolbar = NavigationToolbar2TkAgg(canvas, root)
-        toolbar.update()
-        self.controllbar = ControllBar(root, controller)
-        self.f = f
-        self.ax = ax
-        self.canvas = canvas
-        self.controller = controller
-        self.contours = []
-        self.c_labels = None
-        self.plot_kernels()
-    
-    print('consensus score: {:.3f}'.format(score))
-    
-    
-n_train = 20  # samples for training
-n_test = 200  # samples for testing
-n_averages = 50  # how often to repeat classification
-n_features_max = 75  # maximum number of features
-step = 4  # step size for the calculation
+    from r2.controllers.reddit_base import MinimalController
+from r2.lib import promote, cache
