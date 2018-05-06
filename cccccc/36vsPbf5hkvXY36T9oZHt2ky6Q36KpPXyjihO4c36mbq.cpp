3f705e49bf53f5ebcaf7c3f521b0064f8c729a34
SUNsung <weatherius@gmail.com>
2018-05-06 09:48:15 +0300

    Rewrite [36vsPbf5hkvXY36T9oZHt2ky6Q36KpPXyjihO4c36mbq.cpp] File in: 2018-05-06 09:48:14

--- cccccc/36vsPbf5hkvXY36T9oZHt2ky6Q36KpPXyjihO4c36mbq.cpp
+++ cccccc/36vsPbf5hkvXY36T9oZHt2ky6Q36KpPXyjihO4c36mbq.cpp
@@ -1,72 +1,215 @@
 
         
-        #include 'caffe/layers/neuron_layer.hpp'
+        #include 'base/strings/string16.h'
+#include 'base/time/time.h'
+#include 'content/public/browser/desktop_media_id.h'
+#include 'ui/gfx/image/image_skia.h'
     
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    #endif  // CAFFE_CUDNN_LCN_LAYER_HPP_
-
-    
-    #include 'caffe/layers/pooling_layer.hpp'
-    
-     protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    namespace caffe {
+    class ChromeBrowserPepperHostFactory : public ppapi::host::HostFactory {
+ public:
+  // Non-owning pointer to the filter must outlive this class.
+  explicit ChromeBrowserPepperHostFactory(content::BrowserPpapiHost* host);
+  ~ChromeBrowserPepperHostFactory() override;
     }
     
-    /* Starts the stream by sending request to |url| using |method| and |headers|.
- * If |end_of_stream| is true, then no data is expected to be written. The
- * |method| is HTTP verb, with PUT having a special meaning to mark idempotent
- * request, which could use QUIC 0-RTT.
- */
-GRPC_SUPPORT_EXPORT
-int bidirectional_stream_start(bidirectional_stream* stream,
-                               const char* url,
-                               int priority,
-                               const char* method,
-                               const bidirectional_stream_header_array* headers,
-                               bool end_of_stream);
+      int32_t OnOpenFileSystem(ppapi::host::HostMessageContext* context,
+                           PP_IsolatedFileSystemType_Private type);
+  int32_t OpenPluginPrivateFileSystem(ppapi::host::HostMessageContext* context);
     
-    template <class T, class F>
-double average(const T& container, F functor) {
-  return sum(container, functor) / container.size();
+    std::string TtsPlatformImpl::error() {
+  return error_;
 }
     
-    void BuildACHistograms(const JPEGData& jpg, JpegHistogram* histo) {
-  for (size_t i = 0; i < jpg.components.size(); ++i) {
-    const JPEGComponent& c = jpg.components[i];
-    JpegHistogram* ac_histogram = &histo[i];
-    for (size_t j = 0; j < c.coeffs.size(); j += kDCTBlockSize) {
-      UpdateACHistogramForDCTBlock(&c.coeffs[j], ac_histogram);
+      // When user keys are misordered
+  ASSERT_EQ(IKey('foo', 100, kTypeValue),
+            Shorten(IKey('foo', 100, kTypeValue),
+                    IKey('bar', 99, kTypeValue)));
+    
+    // A utility routine: write 'data' to the named file and Sync() it.
+extern Status WriteStringToFileSync(Env* env, const Slice& data,
+                                    const std::string& fname);
+    
+      fname = LogFileName('foo', 192);
+  ASSERT_EQ('foo/', std::string(fname.data(), 4));
+  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
+  ASSERT_EQ(192, number);
+  ASSERT_EQ(kLogFile, type);
+    
+    TEST(VersionEditTest, EncodeDecode) {
+  static const uint64_t kBig = 1ull << 50;
     }
-  }
+    
+    // Print histogram of operation timings
+static bool FLAGS_histogram = false;
+    
+    // Returns a new environment that stores its data in memory and delegates
+// all non-file-storage tasks to base_env. The caller must delete the result
+// when it is no longer needed.
+// *base_env must remain live while the result is in use.
+Env* NewMemEnv(Env* base_env);
+    
+      // Return a pointer to the beginning of the referenced data
+  const char* data() const { return data_; }
+    
+    // Version constant.
+// This is either 0 for python, 1 for CPP V1, 2 for CPP V2.
+//
+// 0 is default and is equivalent to
+//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+//
+// 1 is set with -DPYTHON_PROTO2_CPP_IMPL_V1 and is equivalent to
+//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
+// and
+//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=1
+//
+// 2 is set with -DPYTHON_PROTO2_CPP_IMPL_V2 and is equivalent to
+//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
+// and
+//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=2
+#ifdef PYTHON_PROTO2_CPP_IMPL_V1
+#error 'PYTHON_PROTO2_CPP_IMPL_V1 is no longer supported.'
+#else
+#ifdef PYTHON_PROTO2_CPP_IMPL_V2
+static int kImplVersion = 2;
+#else
+#ifdef PYTHON_PROTO2_PYTHON_IMPL
+static int kImplVersion = 0;
+#else
+    
+    #include <google/protobuf/stubs/logging.h>
+#include <google/protobuf/stubs/common.h>
+#include <google/protobuf/descriptor.pb.h>
+#include <google/protobuf/pyext/message.h>
+#include <google/protobuf/pyext/scoped_pyobject_ptr.h>
+    
+    #ifndef GOOGLE_PROTOBUF_COMPILER_CSHARP_ENUM_H__
+#define GOOGLE_PROTOBUF_COMPILER_CSHARP_ENUM_H__
+    
+    #ifndef GOOGLE_PROTOBUF_COMPILER_CSHARP_SOURCE_GENERATOR_BASE_H__
+#define GOOGLE_PROTOBUF_COMPILER_CSHARP_SOURCE_GENERATOR_BASE_H__
+    
+    #include <google/protobuf/compiler/java/java_doc_comment.h>
+    
+      // Returns an estimate of the number of bytes the printed code will compile to
+  virtual int GenerateNonNestedInitializationCode(io::Printer* printer);
+    
+      // implements CodeGenerator ----------------------------------------
+  bool Generate(const FileDescriptor* file,
+                const string& parameter,
+                GeneratorContext* context,
+                string* error) const;
+    
+    #include <google/protobuf/stubs/common.h>
+    
+        '$name$_.setValue(builderForValue.build());\n'
+    '$on_changed$\n',
+    
+      // Gets the text streamed to this object so far as an std::string.
+  // Each '\0' character in the buffer is replaced with '\\0'.
+  //
+  // INTERNAL IMPLEMENTATION - DO NOT USE IN A USER PROGRAM.
+  std::string GetString() const;
+    
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7, typename T8, typename T9, typename T10>
+void PrintTo(
+    const ::std::tr1::tuple<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>& t,
+    ::std::ostream* os) {
+  PrintTupleTo(t, os);
+}
+#endif  // GTEST_HAS_TR1_TUPLE
+    
+    // We don't want to require the users to write TypesN<...> directly,
+// as that would require them to count the length.  Types<...> is much
+// easier to write, but generates horrible messages when there is a
+// compiler error, as gcc insists on printing out each template
+// argument, even if it has the default value (this means Types<int>
+// will appear as Types<int, None, None, ..., None> in the compiler
+// errors).
+//
+// Our solution is to combine the best part of the two approaches: a
+// user would write Types<T1, ..., TN>, and Google Test will translate
+// that to TypesN<T1, ..., TN> internally to make error messages
+// readable.  The translation is done by the 'type' member of the
+// Types template.
+    
+    // Implementation #1 calculates the primes on-the-fly.
+class OnTheFlyPrimeTable : public PrimeTable {
+ public:
+  virtual bool IsPrime(int n) const {
+    if (n <= 1) return false;
+    }
+    }
+    
+    extern JSClass  *jsb_cocostudio_timeline_InnerActionFrame_class;
+extern JSObject *jsb_cocostudio_timeline_InnerActionFrame_prototype;
+    
+    
+    
+    
+    
+    				// For a circle: I = 0.5 * m * r * r ==> r = sqrt(2 * I / m)
+				float32 radius = b2Sqrt(2.0f * I / mass);
+    
+    
+    {
+    {			body->CreateFixture(&fd);
+		}
+	}
+    
+    			edge.Set(b2Vec2(-10.0f, 0.0f), b2Vec2(10.0f, 0.0f));
+			body->CreateFixture(&edge, 0.0f);
+    
+    void SimpleShortPack(const void* _data, size_t _datalen, AutoBuffer& _outbuf) {
+    SimplePack<uint16_t>(_data, _datalen, _outbuf);
 }
     
-    // Parses the jpeg stream contained in data[*pos ... len) and fills in *jpg with
-// the parsed information.
-// If mode is JPEG_READ_HEADER, it fills in only the image dimensions in *jpg.
-// Returns false if the data is not valid jpeg, or if it contains an unsupported
-// jpeg feature.
-bool ReadJpeg(const uint8_t* data, const size_t len, JpegReadMode mode,
-              JPEGData* jpg);
-// string variant
-bool ReadJpeg(const std::string& data, JpegReadMode mode,
-              JPEGData* jpg);
+        bool Check();  // true pass, false limit
+    
+      protected:
+    ServiceBase(const char* _servicename) : m_servicename(_servicename) {}
     
     
-    {  uint8_t bits;     // number of bits used for this symbol
-  uint16_t value;   // symbol value or table offset
+    {  private:
+//    int m_t;
 };
     
-    constexpr int kLowestQuality = 70;
-constexpr int kHighestQuality = 110;
+      auto b = fromStr('hello');
+    
+    namespace detail {
+    }
+    
+      DCHECK_GT(numEvents, 0);
+  DCHECK_LE(numEvents, pending_);
+    
+    namespace folly {
+namespace fs {
+    }
+    }
+    
+    /**
+ * Return the mount point for the requested huge page size.
+ * 0 = use smallest available.
+ * Returns nullptr if the requested huge page size is not available.
+ */
+const HugePageSize* getHugePageSize(size_t size = 0);
+    
+      template <typename... Args>
+  explicit ObserverCreator(Args&&... args);
+    
+     private:
+  void dfs(NodeSet& visitedNodes, NodeId node) {
+    // We don't terminate early if cycle is detected, because this is considered
+    // an error condition, so not worth optimizing for.
+    if (visitedNodes.count(node)) {
+      return;
+    }
+    }
+    
+    
+    {      if (auto instance = getInstance()) {
+        instance->cycleDetector_.withLock([&](CycleDetector& cycleDetector) {
+          cycleDetector.removeEdge(&currentDependencies_->core, &core);
+        });
+      }
+    }
