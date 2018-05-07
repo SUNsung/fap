eb7273232b860ad425a9ed8e1dc600e4addb40a5
SUNsung <weatherius@gmail.com>
2018-05-07 09:19:03 +0300

    Rewrite [58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py] File in: 2018-05-07 09:19:02

--- pyton/58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py
+++ pyton/58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py
@@ -1,139 +1,20 @@
 
         
-        ESTIMATORS = {
-    'dummy': DummyClassifier(),
-    'random_forest': RandomForestClassifier(n_estimators=100,
-                                            max_features='sqrt',
-                                            min_samples_split=10),
-    'extra_trees': ExtraTreesClassifier(n_estimators=100,
-                                        max_features='sqrt',
-                                        min_samples_split=10),
-    'logistic_regression': LogisticRegression(),
-    'naive_bayes': MultinomialNB(),
-    'adaboost': AdaBoostClassifier(n_estimators=10),
-}
+        MIN_TOKEN_TYPE = UP+1
+	
+INVALID_TOKEN_TYPE = 0
     
-            start = time.time()
-        func(X, n_jobs=-1)
-        multi_core.append(time.time() - start)
     
-        fig = plt.figure('scikit-learn sample w/o replacement benchmark results')
-    plt.title('n_population = %s, n_times = %s' %
-              (opts.n_population, opts.n_times))
-    ax = fig.add_subplot(111)
-    for name in sampling_algorithm:
-        ax.plot(ratio, time[name], label=name)
+class DFA(object):
+    '''@brief A DFA implemented as a set of transition tables.
     
-        in_exercise_region = False
+            ## State of a lexer, parser, or tree parser are collected into a state
+        # object so the state can be shared.  This sharing is needed to
+        # have one grammar import others and share same error variables
+        # and other state variables.  It's a kind of explicit multiple
+        # inheritance via delegation of methods and shared state.
+        if state is None:
+            state = RecognizerSharedState()
+        self._state = state
     
-    import numpy as np
-from sklearn.covariance import EllipticEnvelope
-from sklearn.svm import OneClassSVM
-import matplotlib.pyplot as plt
-import matplotlib.font_manager
-from sklearn.datasets import load_boston
-    
-    We generate data from three groups of waveforms. Two of the waveforms
-(waveform 1 and waveform 2) are proportional one to the other. The cosine
-distance is invariant to a scaling of the data, as a result, it cannot
-distinguish these two waveforms. Thus even with no noise, clustering
-using this distance will not separate out waveform 1 and 2.
-    
-            for underflow in ['1 / huge', '2 / huge', '-1 / huge', '-2 / huge',
-                         '100 / mhuge', '200 / mhuge']:
-            result = eval(underflow, namespace)
-            self.assertEqual(result, 0.0,
-                             'expected underflow to 0 from %r' % underflow)
-    
-        def seek(self, pos, whence=0):
-        '''Seek to specified position into the chunk.
-        Default position is 0 (start of chunk).
-        If the file is not seekable, this will result in an error.
-        '''
-    
-    '''Safely evaluate Python string literals without using eval().'''
-    
-        # This dictionary maps from (feature,value) to a list of
-    # (option,value) pairs that should be set on the Options object.
-    # If a (feature,value) setting is not in this dictionary, it is
-    # not supported by the DOMBuilder.
-    #
-    _settings = {
-        ('namespace_declarations', 0): [
-            ('namespace_declarations', 0)],
-        ('namespace_declarations', 1): [
-            ('namespace_declarations', 1)],
-        ('validation', 0): [
-            ('validation', 0)],
-        ('external_general_entities', 0): [
-            ('external_general_entities', 0)],
-        ('external_general_entities', 1): [
-            ('external_general_entities', 1)],
-        ('external_parameter_entities', 0): [
-            ('external_parameter_entities', 0)],
-        ('external_parameter_entities', 1): [
-            ('external_parameter_entities', 1)],
-        ('validate_if_schema', 0): [
-            ('validate_if_schema', 0)],
-        ('create_entity_ref_nodes', 0): [
-            ('create_entity_ref_nodes', 0)],
-        ('create_entity_ref_nodes', 1): [
-            ('create_entity_ref_nodes', 1)],
-        ('entities', 0): [
-            ('create_entity_ref_nodes', 0),
-            ('entities', 0)],
-        ('entities', 1): [
-            ('entities', 1)],
-        ('whitespace_in_element_content', 0): [
-            ('whitespace_in_element_content', 0)],
-        ('whitespace_in_element_content', 1): [
-            ('whitespace_in_element_content', 1)],
-        ('cdata_sections', 0): [
-            ('cdata_sections', 0)],
-        ('cdata_sections', 1): [
-            ('cdata_sections', 1)],
-        ('comments', 0): [
-            ('comments', 0)],
-        ('comments', 1): [
-            ('comments', 1)],
-        ('charset_overrides_xml_encoding', 0): [
-            ('charset_overrides_xml_encoding', 0)],
-        ('charset_overrides_xml_encoding', 1): [
-            ('charset_overrides_xml_encoding', 1)],
-        ('infoset', 0): [],
-        ('infoset', 1): [
-            ('namespace_declarations', 0),
-            ('validate_if_schema', 0),
-            ('create_entity_ref_nodes', 0),
-            ('entities', 0),
-            ('cdata_sections', 0),
-            ('datatype_normalization', 1),
-            ('whitespace_in_element_content', 1),
-            ('comments', 1),
-            ('charset_overrides_xml_encoding', 1)],
-        ('supported_mediatypes_only', 0): [
-            ('supported_mediatypes_only', 0)],
-        ('namespaces', 0): [
-            ('namespaces', 0)],
-        ('namespaces', 1): [
-            ('namespaces', 1)],
-    }
-    
-        def test___get__(self):
-        class MyCStruct(Structure):
-            _fields_ = (('field', c_int),)
-        self.assertRaises(TypeError,
-                          MyCStruct.field.__get__, 'wrong type self', 42)
-    
-    print 'PLUGIN_I18N_PATHS := ' + ','.join(os.path.relpath(plugin.path)
-                                         for plugin in plugins
-                                         if plugin.needs_translation)
-    
-    def get_api_subtype():
-    if is_api() and c.render_style.startswith('api-'):
-        return c.render_style[4:]
-    
-    from r2.controllers.api_docs import api_doc, api_section
-from r2.controllers.oauth2 import allow_oauth2_access
-    
-        GET_help = POST_help = renderurl
+    MIN_TOKEN_TYPE = UP+1
