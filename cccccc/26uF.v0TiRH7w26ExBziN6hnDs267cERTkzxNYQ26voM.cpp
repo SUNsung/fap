c8544abfb3e8fd4202324e6acfb7a46446415ed9
SUNsung <weatherius@gmail.com>
2018-05-07 23:46:31 +0300

    Rewrite [26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp] File in: 2018-05-07 23:46:31

--- cccccc/26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp
+++ cccccc/26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp
@@ -1,96 +1,219 @@
 
         
-        
-    {
-    {}  // namespace testing
-}  // namespace grpc
+        #include 'content/nw/src/api/api_messages.h'
+#include 'content/nw/src/api/dispatcher_host.h'
+#include 'content/nw/src/api/shortcut/global_shortcut_listener.h'
+#include 'content/nw/src/api/shortcut/shortcut.h'
+#include 'content/nw/src/breakpad_linux.h'
+#include 'content/nw/src/browser/native_window.h'
+#include 'content/nw/src/browser/net_disk_cache_remover.h'
+#include 'content/nw/src/nw_package.h'
+#include 'content/nw/src/nw_shell.h'
+#include 'content/nw/src/shell_browser_context.h'
+#include 'content/common/view_messages.h'
+#include 'content/public/browser/browser_thread.h'
+#include 'content/public/browser/web_contents.h'
+#include 'content/public/browser/render_process_host.h'
+#include 'net/proxy/proxy_config.h'
+#include 'net/proxy/proxy_config_service_fixed.h'
+#include 'net/proxy/proxy_service.h'
+#include 'net/url_request/url_request_context.h'
+#include 'net/url_request/url_request_context_getter.h'
     
-    std::unique_ptr<ScenarioResult> RunScenario(
-    const grpc::testing::ClientConfig& client_config, size_t num_clients,
-    const grpc::testing::ServerConfig& server_config, size_t num_servers,
-    int warmup_seconds, int benchmark_seconds, int spawn_local_worker_count,
-    const grpc::string& qps_server_target_override,
-    const grpc::string& credential_type, bool run_inproc);
     
-    namespace grpc_node_generator {
+    {}
+    
+       bool GetAcceleratorForCommandId(
+      int command_id,
+      ui::Accelerator* accelerator) const override;
+    
+      if (type == 'separator') {
+    menu_item_ = gtk_separator_menu_item_new();
+  } else {
+    if (type == 'checkbox') {
+      menu_item_ = gtk_check_menu_item_new();
+      bool checked;
+      if (option.GetBoolean('checked', &checked))
+        SetChecked(checked);
+    } else {
+      menu_item_ = gtk_image_menu_item_new();
+      std::string icon;
+      if (option.GetString('icon', &icon))
+        SetIcon(icon);
+    }
     }
     
-    // Data pertaining to configuration of the generator with respect to anything
-// that may be used internally at Google.
-struct GeneratorConfiguration {
-  GeneratorConfiguration();
-  grpc::string grpc_package_root;
-  // TODO(https://github.com/grpc/grpc/issues/8622): Drop this.
-  grpc::string beta_package_root;
-  // TODO(https://github.com/google/protobuf/issues/888): Drop this.
-  grpc::string import_prefix;
-};
     
-    UsageTimer::Result UsageTimer::Mark() const {
-  Result s = Sample();
-  Result r;
-  r.wall = s.wall - start_.wall;
-  r.user = s.user - start_.user;
-  r.system = s.system - start_.system;
-  r.total_cpu_time = s.total_cpu_time - start_.total_cpu_time;
-  r.idle_cpu_time = s.idle_cpu_time - start_.idle_cpu_time;
+    {  // Send event.
+  base::ListValue args;
+  object_manager()->SendEvent(this, 'click', args);
+}
+    
+    NwAppSetProxyConfigFunction::~NwAppSetProxyConfigFunction() {
+}
+    
+    class NwAppClearCacheFunction : public NWSyncExtensionFunction, public content::BrowsingDataRemover::Observer {
+ public:
+  NwAppClearCacheFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+  void OnBrowsingDataRemoverDone() override;
     }
     
-    template <typename C>
-struct compare_less_equal : detail::cmp_pred<C, ordering::gt, 1> {
-  using detail::cmp_pred<C, ordering::gt, 1>::cmp_pred;
-};
+    #include 'extensions/browser/extension_function.h'
     
-    // Pass the key as folly::dynamic, without conversion
-//
-// folly::dynamic, folly::dynamic (no conversion of value, either)
-template <typename Fn> EnableForArgTypes<Fn, folly::dynamic, folly::dynamic>
-invokeForKeyValue(Fn fn, const folly::dynamic& k, const folly::dynamic& v) {
-  fn(k, v);
-}
-// folly::dynamic, int64_t
-template <typename Fn> EnableForArgTypes<Fn, folly::dynamic, int64_t>
-invokeForKeyValue(Fn fn, const folly::dynamic& k, const folly::dynamic& v) {
-  fn(k, v.asInt());
-}
-// folly::dynamic, bool
-template <typename Fn> EnableForArgTypes<Fn, folly::dynamic, bool>
-invokeForKeyValue(Fn fn, const folly::dynamic& k, const folly::dynamic& v) {
-  fn(k, v.asBool());
-}
-// folly::dynamic, double
-template <typename Fn> EnableForArgTypes<Fn, folly::dynamic, double>
-invokeForKeyValue(Fn fn, const folly::dynamic& k, const folly::dynamic& v) {
-  fn(k, v.asDouble());
-}
-// folly::dynamic, std::string
-template <typename Fn> EnableForArgTypes<Fn, folly::dynamic, std::string>
-invokeForKeyValue(Fn fn, const folly::dynamic& k, const folly::dynamic& v) {
-  fn(k, v.asString());
-}
+    #include 'extensions/browser/extension_function.h'
+    
+     protected:
+  ~NwObjCallObjectMethodSyncFunction() override;
     
     
-    {  std::array<AtomicBlockType, N> data_;
-};
+    {    cvReleaseMat( &(*state)->preFilteredImg0 );
+    cvReleaseMat( &(*state)->preFilteredImg1 );
+    cvReleaseMat( &(*state)->slidingSumBuf );
+    cvReleaseMat( &(*state)->disp );
+    cvReleaseMat( &(*state)->cost );
+    cvFree( state );
+}
     
-    using InstructionPointer = const void*;
+        sizes[INPUT][0] = cvSize(1, pointsCount);
+    types[INPUT][0] = CV_64FC2;
     
-    /* static */ void Config::destroy(Config * node)
+    namespace cv { namespace cuda { namespace device
 {
-    delete node;
-}
-    
-     private:
-    
-    #pragma once
-    
-    template<typename... ARGS>
-inline void logi(const char* tag, const char* msg, ARGS... args) noexcept {
-  log(ANDROID_LOG_INFO, tag, msg, args...);
-}
-    
-      static void OnThreadExit(void *obj) {
-    if (NULL != obj) {
-      delete (T*)obj;
+    template <class T>
+    __device__ __forceinline__ T warp_reduce(volatile T *ptr , const unsigned int tid = threadIdx.x)
+    {
+        const unsigned int lane = tid & 31; // index of thread in warp (0..31)
     }
+    }
+    }
+    }
+    
+            // Core Extension: ARB_framebuffer_object
+        INVALID_FRAMEBUFFER_OPERATION    = 0x0506,
+        FRAMEBUFFER_ATTACHMENT_COLOR_ENCODING = 0x8210,
+        FRAMEBUFFER_ATTACHMENT_COMPONENT_TYPE = 0x8211,
+        FRAMEBUFFER_ATTACHMENT_RED_SIZE  = 0x8212,
+        FRAMEBUFFER_ATTACHMENT_GREEN_SIZE = 0x8213,
+        FRAMEBUFFER_ATTACHMENT_BLUE_SIZE = 0x8214,
+        FRAMEBUFFER_ATTACHMENT_ALPHA_SIZE = 0x8215,
+        FRAMEBUFFER_ATTACHMENT_DEPTH_SIZE = 0x8216,
+        FRAMEBUFFER_ATTACHMENT_STENCIL_SIZE = 0x8217,
+        FRAMEBUFFER_DEFAULT              = 0x8218,
+        FRAMEBUFFER_UNDEFINED            = 0x8219,
+        DEPTH_STENCIL_ATTACHMENT         = 0x821A,
+        INDEX                            = 0x8222,
+        MAX_RENDERBUFFER_SIZE            = 0x84E8,
+        DEPTH_STENCIL                    = 0x84F9,
+        UNSIGNED_INT_24_8                = 0x84FA,
+        DEPTH24_STENCIL8                 = 0x88F0,
+        TEXTURE_STENCIL_SIZE             = 0x88F1,
+        TEXTURE_RED_TYPE                 = 0x8C10,
+        TEXTURE_GREEN_TYPE               = 0x8C11,
+        TEXTURE_BLUE_TYPE                = 0x8C12,
+        TEXTURE_ALPHA_TYPE               = 0x8C13,
+        TEXTURE_DEPTH_TYPE               = 0x8C16,
+        UNSIGNED_NORMALIZED              = 0x8C17,
+        FRAMEBUFFER_BINDING              = 0x8CA6,
+        DRAW_FRAMEBUFFER_BINDING         = 0x8CA6,
+        RENDERBUFFER_BINDING             = 0x8CA7,
+        READ_FRAMEBUFFER                 = 0x8CA8,
+        DRAW_FRAMEBUFFER                 = 0x8CA9,
+        READ_FRAMEBUFFER_BINDING         = 0x8CAA,
+        RENDERBUFFER_SAMPLES             = 0x8CAB,
+        FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE = 0x8CD0,
+        FRAMEBUFFER_ATTACHMENT_OBJECT_NAME = 0x8CD1,
+        FRAMEBUFFER_ATTACHMENT_TEXTURE_LEVEL = 0x8CD2,
+        FRAMEBUFFER_ATTACHMENT_TEXTURE_CUBE_MAP_FACE = 0x8CD3,
+        FRAMEBUFFER_ATTACHMENT_TEXTURE_LAYER = 0x8CD4,
+        FRAMEBUFFER_COMPLETE             = 0x8CD5,
+        FRAMEBUFFER_INCOMPLETE_ATTACHMENT = 0x8CD6,
+        FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT = 0x8CD7,
+        FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER = 0x8CDB,
+        FRAMEBUFFER_INCOMPLETE_READ_BUFFER = 0x8CDC,
+        FRAMEBUFFER_UNSUPPORTED          = 0x8CDD,
+        MAX_COLOR_ATTACHMENTS            = 0x8CDF,
+        COLOR_ATTACHMENT0                = 0x8CE0,
+        COLOR_ATTACHMENT1                = 0x8CE1,
+        COLOR_ATTACHMENT2                = 0x8CE2,
+        COLOR_ATTACHMENT3                = 0x8CE3,
+        COLOR_ATTACHMENT4                = 0x8CE4,
+        COLOR_ATTACHMENT5                = 0x8CE5,
+        COLOR_ATTACHMENT6                = 0x8CE6,
+        COLOR_ATTACHMENT7                = 0x8CE7,
+        COLOR_ATTACHMENT8                = 0x8CE8,
+        COLOR_ATTACHMENT9                = 0x8CE9,
+        COLOR_ATTACHMENT10               = 0x8CEA,
+        COLOR_ATTACHMENT11               = 0x8CEB,
+        COLOR_ATTACHMENT12               = 0x8CEC,
+        COLOR_ATTACHMENT13               = 0x8CED,
+        COLOR_ATTACHMENT14               = 0x8CEE,
+        COLOR_ATTACHMENT15               = 0x8CEF,
+        DEPTH_ATTACHMENT                 = 0x8D00,
+        STENCIL_ATTACHMENT               = 0x8D20,
+        FRAMEBUFFER                      = 0x8D40,
+        RENDERBUFFER                     = 0x8D41,
+        RENDERBUFFER_WIDTH               = 0x8D42,
+        RENDERBUFFER_HEIGHT              = 0x8D43,
+        RENDERBUFFER_INTERNAL_FORMAT     = 0x8D44,
+        STENCIL_INDEX1                   = 0x8D46,
+        STENCIL_INDEX4                   = 0x8D47,
+        STENCIL_INDEX8                   = 0x8D48,
+        STENCIL_INDEX16                  = 0x8D49,
+        RENDERBUFFER_RED_SIZE            = 0x8D50,
+        RENDERBUFFER_GREEN_SIZE          = 0x8D51,
+        RENDERBUFFER_BLUE_SIZE           = 0x8D52,
+        RENDERBUFFER_ALPHA_SIZE          = 0x8D53,
+        RENDERBUFFER_DEPTH_SIZE          = 0x8D54,
+        RENDERBUFFER_STENCIL_SIZE        = 0x8D55,
+        FRAMEBUFFER_INCOMPLETE_MULTISAMPLE = 0x8D56,
+        MAX_SAMPLES                      = 0x8D57,
+        TEXTURE_LUMINANCE_TYPE           = 0x8C14,
+        TEXTURE_INTENSITY_TYPE           = 0x8C15,
+    
+    #ifndef OPENCV_CORE_HAL_INTERNAL_HPP
+#define OPENCV_CORE_HAL_INTERNAL_HPP
+    
+    #ifndef __OPENCV_CORE_OCL_RUNTIME_COMMON_HPP__
+#define __OPENCV_CORE_OCL_RUNTIME_COMMON_HPP__
+    
+      if (flags & TrackVtsc) {
+    flags |= TrackCPU;
   }
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    
+    /*
+ * Determine the next NUMA node according to state maintained in `curr_node`.
+ */
+int next_numa_node(std::atomic_int& curr_node);
+/*
+ * The number of numa nodes in the system
+ */
+inline int num_numa_nodes() {
+  return use_numa ? numa_num_nodes : 1;
+}
+/*
+ * Enable numa interleaving for the specified address range
+ */
+void numa_interleave(void* start, size_t size);
+/*
+ * Allocate the specified address range on the given node
+ */
+void numa_bind_to(void* start, size_t size, int node);
+/*
+ * Return true if node is part of the allowed set of numa nodes
+ */
+inline bool numa_node_allowed(int node) {
+  if (numa_node_set == 0) return true;
+  return numa_node_set & (1u << node);
+}
+    
+        // Skip if no predCluster (predecessor w/ no samples), or if same
+    // as cluster, of it's frozen.
+    if (predCluster == nullptr || predCluster == cluster ||
+        predCluster->frozen) {
+      continue;
+    }
+    
+    #else //__cplusplus
