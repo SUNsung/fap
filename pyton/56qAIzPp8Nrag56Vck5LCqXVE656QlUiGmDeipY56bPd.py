a9ee35e540beaceda496e1e0408094dac33aebc4
SUNsung <weatherius@gmail.com>
2018-05-08 03:17:22 +0300

    Rewrite [56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py] File in: 2018-05-08 03:17:22

--- pyton/56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py
+++ pyton/56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py
@@ -1,111 +1,144 @@
 
         
         
-if __name__ == '__main__':
-    unittest.main()
-
+def test_duplicate_tag():
+    class TagDict(JSONTag):
+        key = ' d'
+    
+            os.environ = {'FOO_SETTINGS': __file__.rsplit('.', 1)[0] + '.py'}
+        assert app.config.from_envvar('FOO_SETTINGS')
+        common_object_test(app)
+    finally:
+        os.environ = env
+    
+        logger.propagate = False
+    assert not has_level_handler(logger)
+    logger.propagate = True
+    
+        def __init__(self, groups, env=Environment(), **kwargs):
+        '''
+        :param groups: names of processor groups to be applied
+        :param env: Environment
+        :param kwargs: additional keyword arguments for processors
+    
+        '''
+    if not env.stdout_isatty and not args.prettify:
+        Stream = partial(
+            RawStream,
+            chunk_size=RawStream.CHUNK_SIZE_BY_LINE
+            if args.stream
+            else RawStream.CHUNK_SIZE
+        )
+    elif args.prettify:
+        Stream = partial(
+            PrettyStream if args.stream else BufferedPrettyStream,
+            env=env,
+            conversion=Conversion(),
+            formatting=Formatting(
+                env=env,
+                groups=args.prettify,
+                color_scheme=args.style,
+                explicit_json=args.json,
+            ),
+        )
+    else:
+        Stream = partial(EncodedStream, env=env)
     
     
-TEST_DIR = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 'swftests')
+def test_follow_all_redirects_shown(httpbin):
+    r = http('--follow', '--all', httpbin.url + '/redirect/2')
+    assert r.count('HTTP/1.1') == 3
+    assert r.count('HTTP/1.1 302 FOUND', 2)
+    assert HTTP_OK in r
     
     
-def prepare_url(value):
-    # Issue #1483: Make sure the URL always has a trailing slash
-    httpbin_url = value.url.rstrip('/') + '/'
+class Downloader(object):
     
-        def __exit__(self, exc_type, exc_value, traceback):
-        if exc_type is None:
-            self.stop_event.wait(self.WAIT_EVENT_TIMEOUT)
-        else:
-            if self.wait_to_close_event:
-                # avoid server from waiting for event timeouts
-                # if an exception is found in the main thread
-                self.wait_to_close_event.set()
+        def __call__(self, r):
+        '''
+        Override username/password serialization to allow unicode.
     
-    import os
-import re
-import time
-import hashlib
-import threading
-import warnings
+        def test_cert_pem(self, httpbin_secure):
+        r = http(httpbin_secure + '/get',
+                 '--cert', CLIENT_PEM)
+        assert HTTP_OK in r
     
-        def test_iter(self):
-        cid = CaseInsensitiveDict({'Spam': 'spam', 'Eggs': 'eggs'})
-        keys = frozenset(['Spam', 'Eggs'])
-        assert frozenset(iter(cid)) == keys
     
-        parent = os.path.join(path, os.path.pardir)
-    parent = os.path.realpath(parent)
+def get_response(requests_session, session_name,
+                 config_dir, args, read_only=False):
+    '''Like `client.get_responses`, but applies permanent
+    aspects of the session to the request.
     
-        parser = argparse.ArgumentParser(description='Start a new Shippable run.')
-    
-        def test_filter_gcp_fields(self):
-        input_data = {
-            u'kind': u'compute#httpsHealthCheck',
-            u'description': u'',
-            u'timeoutSec': 5,
-            u'checkIntervalSec': 5,
-            u'port': 443,
-            u'healthyThreshold': 2,
-            u'host': u'',
-            u'requestPath': u'/',
-            u'unhealthyThreshold': 2,
-            u'creationTimestamp': u'2017-05-16T15:09:36.546-07:00',
-            u'id': u'8727093129334146639',
-            u'selfLink': u'https://www.googleapis.com/compute/v1/projects/myproject/global/httpsHealthChecks/myhealthcheck',
-            u'name': u'myhealthcheck'}
-    
-        terminal_stdout_re = [
-        re.compile(br'[\r\n]?[\w+\-\.:\/\[\]]+(?:\([^\)]+\)){,3}(?:>|#) ?$'),
-        re.compile(br'\@[\w\-\.]+:\S+?[>#\$] ?$')
-    ]
-    
-        def on_open_shell(self):
+        print('Classifier Training')
+    print('===================')
+    accuracy, train_time, test_time = {}, {}, {}
+    for name in sorted(args['estimators']):
+        clf = ESTIMATORS[name]
         try:
-            self._exec_cli_command(b'environment no more')
-        except AnsibleConnectionFailure:
-            raise AnsibleConnectionFailure('unable to set terminal parameters')
+            clf.set_params(random_state=0)
+        except (TypeError, ValueError):
+            pass
+    
+    
+def plot_feature_times(all_times, batch_size, all_components, data):
+    plt.figure()
+    plot_results(all_components, all_times['pca'], label='PCA')
+    plot_results(all_components, all_times['ipca'],
+                 label='IncrementalPCA, bsize=%i' % batch_size)
+    plot_results(all_components, all_times['rpca'], label='RandomizedPCA')
+    plt.legend(loc='upper left')
+    plt.suptitle('Algorithm runtime vs. n_components\n \
+                 LFW, size %i x %i' % data.shape)
+    plt.xlabel('Number of components (out of max %i)' % data.shape[1])
+    plt.ylabel('Time (seconds)')
+    
+    import numpy as np
+import scipy.sparse as sp
+    
+        xx = np.arange(start_dim, start_dim + n * step, step)
+    plt.subplot(212)
+    plt.title('Learning in high dimensional spaces')
+    plt.plot(xx, scikit_classifier_results, 'g-', label='classification')
+    plt.plot(xx, scikit_regressor_results, 'r-', label='regression')
+    plt.legend(loc='upper left')
+    plt.xlabel('number of dimensions')
+    plt.ylabel('Time (s)')
+    plt.axis('tight')
+    plt.show()
 
     
-        def add_user(self, user):
-        pass
     
-        def __init__(self, MAX_SIZE):
-        self.MAX_SIZE = MAX_SIZE
-        self.size = 0
-        self.lookup = {}
-        self.linked_list = LinkedList()
+if not os.path.exists(DATA_FOLDER):
     
-        def mapper(self, _, line):
-        '''Parse each log line, extract and transform relevant lines.
-    
-    from tornado.options import define, options
+    for s, p in zip(sentences, predicted):
+    print(u'The language of '%s' is '%s'' % (s, dataset.target_names[p]))
+
     
     
-class ComposeHandler(BaseHandler):
-    @administrator
-    def get(self):
-        key = self.get_argument('key', None)
-        entry = Entry.get(key) if key else None
-        self.render('compose.html', entry=entry)
+if __name__ == '__main__':
+    # NOTE: we put the following in a 'if __name__ == '__main__'' protected
+    # block to be able to use a multi-core grid search that also works under
+    # Windows, see: http://docs.python.org/library/multiprocessing.html#windows
+    # The multiprocessing module is used as the backend of joblib.Parallel
+    # that is used when n_jobs != 1 in GridSearchCV
     
+    y_min, y_max = -50, 50
+x_min, x_max = -50, 50
     
-def main():
-    parse_command_line()
-    t = Timer(e1)
-    results = t.timeit(options.num) / options.num
-    print('engine: %0.3f ms per iteration' % (results * 1000))
-    t = Timer(c1)
-    results = t.timeit(options.num) / options.num
-    print('coroutine: %0.3f ms per iteration' % (results * 1000))
+    plt.show()
+
     
+    if __name__ == '__main__':
+    main()
+
     
-class POSTHandler(tornado.web.RequestHandler):
-    def post(self):
-        for field_name, files in self.request.files.items():
-            for info in files:
-                filename, content_type = info['filename'], info['content_type']
-                body = info['body']
-                logging.info('POST '%s' '%s' %d bytes',
-                             filename, content_type, len(body))
+        theplatform_download_by_pid(pid, title, output_dir=output_dir, merge=merge, info_only=info_only)
+    
+    def ehow_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+	
+	assert re.search(r'http://www.ehow.com/video_', url), 'URL you entered is not supported'
+    
+        fc2video_download_by_upid(upid, output_dir, merge, info_only)
+    
+    from ..common import *
+from ..extractor import VideoExtractor
