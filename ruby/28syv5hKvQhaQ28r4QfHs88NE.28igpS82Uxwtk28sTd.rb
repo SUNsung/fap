20d45ac3beca337dd007f83989a1a2afeaa7fb6b
SUNsung <weatherius@gmail.com>
2018-05-08 14:53:50 +0300

    Rewrite [28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb] File in: 2018-05-08 14:53:49

--- ruby/28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb
+++ ruby/28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb
@@ -1,85 +1,104 @@
 
         
-          def print_remaining_files(files, root, other = '')
-    case files.length
-    when 0
-      # noop
-    when 1
-      puts files
-    else
-      puts '#{root}/ (#{files.length} #{other}files)'
-    end
-  end
-end
-
-    
-      # Use this method to generate standard caveats.
-  def standard_instructions(home_name, home_value = libexec)
-    <<-EOS.undent
-      Before you can use these tools you must export some variables to your $SHELL.
-    
-      # Equivalent to Set#flatten, but replaces the receiver with the
-  # result in place.  Returns nil if no modifications were made.
-  def flatten!
-    replace(flatten()) if any? { |e| e.is_a?(Set) }
+            find_union(segments, Group).order_id_desc
   end
     
-      def test_eq
-    set1 = Set[2,3,1]
-    set2 = Set[1,2,3]
-    
-        t = Time.utc(2015, 12, 30, 20, 0, 0).getlocal('+05:00')
-    assert_equal('365', t.strftime('%j'))
-    t = Time.utc(2016, 12, 30, 20, 0, 0).getlocal('+05:00')
-    assert_equal('366', t.strftime('%j'))
-    
-          configure_sass
-    end
-    
-      def process_bootstrap
-    log_status 'Convert Bootstrap LESS to Sass'
-    puts ' repo   : #@repo_url'
-    puts ' branch : #@branch_sha #@repo_url/tree/#@branch'
-    puts ' save to: #{@save_to.to_json}'
-    puts ' twbs cache: #{@cache_path}'
-    puts '-' * 60
-    
-      def setup
-    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
-    success = Dir.chdir DUMMY_PATH do
-      silence_stdout_if !ENV['VERBOSE'] do
-        system 'node', 'manifest.js', tmp_dir
-      end
-    end
-    assert success, 'Node.js Mincer compilation failed'
-    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
-    css_name = manifest['assets']['application.css']
-    @css = File.read('#{tmp_dir}/#{css_name}')
+      def tumblr_consumer_key
+    ENV['TUMBLR_OAUTH_KEY']
   end
-end
-
     
-          spec['version'] = Bootstrap::VERSION
+        @table_sort_info = {
+      order: { attribute.to_sym => direction.to_sym },
+      attribute: attribute,
+      direction: direction
+    }
+  end
     
-          output = input_path if @options[:in_place]
-      write_output(out, output)
-    rescue Sass::SyntaxError => e
-      raise e if @options[:trace]
-      file = ' of #{e.sass_filename}' if e.sass_filename
-      raise 'Error on line #{e.sass_line}#{file}: #{e.message}\n  Use --trace for backtrace'
-    rescue LoadError => err
-      handle_load_error(err)
-    end
+      def failure
+    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
+    redirect_to after_omniauth_failure_path_for(resource_name)
+  end
     
-          # If the importer is based on files on the local filesystem
-      # this method should return folders which should be watched
-      # for changes.
-      #
-      # @return [Array<String>] List of absolute paths of directories to watch
-      def directories_to_watch
-        []
+      protected
+    
+          # Remembers the given resource by setting up a cookie
+      def remember_me(resource)
+        return if request.env['devise.skip_storage']
+        scope = Devise::Mapping.find_scope!(resource)
+        resource.remember_me!
+        cookies.signed[remember_key(resource, scope)] = remember_cookie_values(resource)
       end
     
-      require 'sass/plugin/rack'
-  class Sass::Plugin::MerbBootLoader < Merb::BootLoader
-    after Merb::BootLoader::RackUpApplication
+          attr_reader :scope_name, :resource
+    
+      if ARGV.include? '--no-ansi'
+    STDERR.puts <<-DOC
+    WARNING: CocoaPods requires your terminal to be using UTF-8 encoding.
+    Consider adding the following to ~/.profile:
+    
+      # TODO: raise SAFE level (0) to 4 if possible.
+  def generate
+    ERB.new(PROFILE_ERB_TEMPLATE, 0, '>').result(binding)
+  end
+end
+    
+            def run
+          UI.puts('$CACHE_ROOT: #{@cache.root}') if @short_output
+          if @pod_name.nil? # Print all
+            @cache.cache_descriptors_per_pod.each do |pod_name, cache_descriptors|
+              print_pod_cache_infos(pod_name, cache_descriptors)
+            end
+          else # Print only for the requested pod
+            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
+            if cache_descriptors.nil?
+              UI.notice('No cache for pod named #{@pod_name} found')
+            else
+              print_pod_cache_infos(@pod_name, cache_descriptors)
+            end
+          end
+        end
+    
+    #{stack}
+#{executable_path}
+### Plugins
+    
+            # Clones the template from the remote in the working directory using
+        # the name of the Pod.
+        #
+        # @return [void]
+        #
+        def clone_template
+          UI.section('Cloning `#{template_repo_url}` into `#{@name}`.') do
+            git! ['clone', template_repo_url, @name]
+          end
+        end
+    
+            def <<(value)
+          values << value
+        end
+    
+          def stages
+        names = Dir[stage_definitions].map { |f| File.basename(f, '.rb') }
+        assert_valid_stage_names(names)
+        names
+      end
+    
+      desc 'Reverted'
+  task :reverted do
+  end
+    
+        describe 'when defining role with reserved name' do
+      it 'fails with ArgumentError' do
+        expect do
+          dsl.role :all, %w{example1.com}
+        end.to raise_error(ArgumentError, 'all reserved name for role. Please choose another name')
+      end
+    end
+    
+              def plugins
+            @plugins ||= find_plugins_gem_specs.map do |spec|
+              { :name => spec.name, :version => spec.version.to_s }
+            end.sort_by do |spec|
+              spec[:name]
+            end
+          end
