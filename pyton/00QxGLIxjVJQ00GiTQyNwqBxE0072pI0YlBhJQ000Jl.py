60567cc059fcee9a19597632365668b62ea16a77
SUNsung <weatherius@gmail.com>
2018-05-08 18:03:44 +0300

    Rewrite [00QxGLIxjVJQ00GiTQyNwqBxE0072pI0YlBhJQ000Jl.py] File in: 2018-05-08 18:03:43

--- pyton/00QxGLIxjVJQ00GiTQyNwqBxE0072pI0YlBhJQ000Jl.py
+++ pyton/00QxGLIxjVJQ00GiTQyNwqBxE0072pI0YlBhJQ000Jl.py
@@ -1,66 +1,34 @@
 
         
-        # This view is called from FlatpageFallbackMiddleware.process_response
-# when a 404 is raised, which often means CsrfViewMiddleware.process_view
-# has not been called even if CsrfViewMiddleware is installed. So we need
-# to use @csrf_protect, in case the template needs {% csrf_token %}.
-# However, we can't just wrap this view; if no matching flatpage exists,
-# or a redirect is required for authentication, the 404 needs to be returned
-# without any CSRF checks. Therefore, we only
-# CSRF protect the internal implementation.
-    
-        print(link)
-    return link
-    
-    def get_header_guard_dmlc(filename):
-    '''Get Header Guard Convention for DMLC Projects.
-    For headers in include, directly use the path
-    For headers in src, use project name plus path
-    Examples: with project-name = dmlc
-        include/dmlc/timer.h -> DMLC_TIMTER_H_
-        src/io/libsvm_parser.h -> DMLC_IO_LIBSVM_PARSER_H_
-    '''
-    fileinfo = cpplint.FileInfo(filename)
-    file_path_from_root = fileinfo.RepositoryName()
-    inc_list = ['include', 'api', 'wrapper']
-    
-        parser = argparse.ArgumentParser(description='sparse training')
-    parser.add_argument('--pruning_switch_epoch', type=str)
-    parser.add_argument('--weight_sparsity', type=str, default=None)
-    parser.add_argument('--bias_sparsity', type=str, default=None)
-    parser.add_argument('--weight_threshold', type=str, default=None)
-    parser.add_argument('--bias_threshold', type=str, default=None)
-    args = parser.parse_args()
-    
-            # FC for i, u, o gates, from summation of children states to hidden state
-        hs2h_iuo = F.FullyConnected(data=hs, weight=hs2h_weight, bias=hs2h_bias,
-                                    num_hidden=self._hidden_size*3,
-                                    name='%shs2h'%name)
-        i2h_iuo = i2h_iuo + hs2h_iuo
+            with open('README.md', 'w+') as sorted_file:
+        # Then all of the blocks are sorted individually
+        blocks = [''.join(sorted(block, key=lambda s: s.lower())) for block in blocks]
+        # And the result is written back to README.md
+        sorted_file.write(''.join(blocks))
     
     
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='score a model on a dataset')
-    parser.add_argument('--model', type=str, required=True,
-                        help = 'the model name.')
-    parser.add_argument('--gpus', type=str, default='0')
-    parser.add_argument('--batch-size', type=int, default=64)
-    parser.add_argument('--rgb-mean', type=str, default='0,0,0')
-    parser.add_argument('--data-val', type=str, required=True)
-    parser.add_argument('--image-shape', type=str, default='3,224,224')
-    parser.add_argument('--data-nthreads', type=int, default=4,
-                        help='number of threads for data decoding')
-    args = parser.parse_args()
+def test_regression_class_build_fn():
+    class ClassBuildFnReg(object):
     
-        # linear classifier
-    flatten = mx.symbol.Flatten(data=pool)
-    fc1 = mx.symbol.FullyConnected(data=flatten, num_hidden=num_classes)
-    softmax = mx.symbol.SoftmaxOutput(data=fc1, name='softmax')
-    return softmax
+    history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_split=0.1)
+score = model.evaluate(x_test, y_test,
+                       batch_size=batch_size, verbose=1)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
 
     
-    Implemented the following paper:
-Saining Xie, Ross Girshick, Piotr Dollar, Zhuowen Tu, Kaiming He. 'Aggregated Residual Transformations for Deep Neural Network'
-'''
-import mxnet as mx
-import numpy as np
+        # Arguments
+        path: path where to cache the dataset locally
+            (relative to ~/.keras/datasets).
+        test_split: fraction of the data to reserve as test set.
+        seed: Random seed for shuffling the data
+            before computing the test split.
+    
+        for i in range(1, 6):
+        fpath = os.path.join(path, 'data_batch_' + str(i))
+        (x_train[(i - 1) * 10000: i * 10000, :, :, :],
+         y_train[(i - 1) * 10000: i * 10000]) = load_batch(fpath)
