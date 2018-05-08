b1fcab6d6c3feb88e069c9509ecd10c222500d51
SUNsung <weatherius@gmail.com>
2018-05-08 09:22:01 +0300

    Rewrite [33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb] File in: 2018-05-08 09:22:00

--- ruby/33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb
+++ ruby/33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb
@@ -1,140 +1,36 @@
 
         
-            # Returns the unwrapped subject and body of the last commit
-    # <b>DEPRECATED:</b> Use <tt>last_git_commit_message</tt> instead.
-    def self.last_git_commit
-      UI.important('`last_git_commit` is deprecated. Please use `last_git_commit_message` instead.')
-      last_git_commit_message
-    end
-    
-          it 'renders HTML' do
-        capture = render(options, screenshots)
-        expect(capture).to match(/<html>/)
-        expect(capture).to include('<li>Some</li>')
-        expect(capture).to include('<li>key</li>')
-        expect(capture).to include('<li>words</li>')
-      end
-    end
-    
-          def initialize(title: nil, description: nil)
-        self.title = title
-        self.description = description
-      end
-    end
-    
-      # Finds the projects '@user' contributed to, limited to either public projects
-  # or projects visible to the given user.
-  #
-  # current_user - When given the list of the projects is limited to those only
-  #                visible by this user.
-  #
-  # Returns an ActiveRecord::Relation.
-  def execute(current_user = nil)
-    segments = all_projects(current_user)
-    
-      basis[1] = basis[2].vcross(basis[0])
-  basis[1] = basis[1].vnormalize
-end
-    
-    def to_integer(proc)
-  proc[-> n { n + 1 }][0]
-end
-    
-        it 'Returns nil when Referer header is missing and allow_empty_referrer is false' do
-      env = {'HTTP_HOST' => 'foo.com'}
-      subject.options[:allow_empty_referrer] = false
-      expect(subject.referrer(env)).to be_nil
-    end
-    
-    
-  it 'should allow changing the protection mode to a string' do
-    # I have no clue what other modes are available
-    mock_app do
-      use Rack::Protection::FrameOptions, :frame_options => 'ALLOW-FROM foo'
-      run DummyApp
-    end
-    
-      %w(GET HEAD POST PUT DELETE).each do |method|
-    it 'accepts #{method} requests when allow_if is true' do
-      mock_app do
-        use Rack::Protection::HttpOrigin, :allow_if => lambda{|env| env.has_key?('HTTP_ORIGIN') }
-        run DummyApp
-      end
-      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://any.domain.com')).to be_ok
-    end
+        def list_login_items_for_app(app_path)
+  out, err, status = Open3.capture3(
+    '/usr/bin/osascript', '-e',
+    'tell application \'System Events\' to get the name of every login item ' \
+    'whose path contains \'#{File.basename(app_path)}\''
+  )
+  if status.exitstatus > 0
+    $stderr.puts err
+    exit status.exitstatus
   end
+  puts out.gsub(', ', '\n')
+end
     
-            def operator_assignment_node
-          return nil unless node.parent
-          return nil unless OPERATOR_ASSIGNMENT_TYPES.include?(node.parent.type)
-          return nil unless node.sibling_index.zero?
-          node.parent
+      puts 'bundle_id\tapp_name\n'
+  puts '--------------------------------------\n'
+  puts running.to_a.sort
+end
+    
+            def name
+          @node.children.first
         end
     
-    module RuboCop
-  module Cop
-    class VariableForce
-      # A Variable represents existence of a local variable.
-      # This holds a variable declaration node,
-      # and some states of the variable.
-      class Variable
-        VARIABLE_DECLARATION_TYPES =
-          (VARIABLE_ASSIGNMENT_TYPES + ARGUMENT_DECLARATION_TYPES).freeze
-    
-              new_source =
-            node.receiver.source + ' =~ ' + node.first_argument.source
-    
-    module RuboCop
-  module Cop
-    module Lint
-      # In math and Python, we can use `x < y < z` style comparison to compare
-      # multiple value. However, we can't use the comparison in Ruby. However,
-      # the comparison is not syntax error. This cop checks the bad usage of
-      # comparison operators.
-      #
-      # @example
-      #
-      #   # bad
-      #
-      #   x < y < z
-      #   10 <= x <= 20
-      #
-      # @example
-      #
-      #   # good
-      #
-      #   x < y && y < z
-      #   10 <= x && x <= 20
-      class MultipleCompare < Cop
-        MSG = 'Use the `&&` operator to compare multiple values.'.freeze
-    
-            def on_block(node)
-          on_body_of_reduce(node) do |body|
-            void_next = body.each_node(:next).find do |n|
-              n.children.empty? && parent_block_node(n) == node
-            end
-    
-    module RuboCop
-  module Cop
-    module Style
-      # This cop checks against comparing a variable with multiple items, where
-      # `Array#include?` could be used instead to avoid code repetition.
-      #
-      # @example
-      #   # bad
-      #   a = 'a'
-      #   foo if a == 'a' || a == 'b' || a == 'c'
-      #
-      #   # good
-      #   a = 'a'
-      #   foo if ['a', 'b', 'c'].include?(a)
-      class MultipleComparison < Cop
-        MSG = 'Avoid comparing a variable with multiple items ' \
-          'in a conditional, use `Array#include?` instead.'.freeze
-    
-              each_misplaced_optional_arg(arguments) do |argument|
-            add_offense(argument)
+            def nested_comparison?(node)
+          if node.or_type?
+            node.node_parts.all? { |node_part| comparison? node_part }
+          else
+            false
           end
         end
     
-                  def do_something; end
+                end
+          RUBY
+        end
+      end
