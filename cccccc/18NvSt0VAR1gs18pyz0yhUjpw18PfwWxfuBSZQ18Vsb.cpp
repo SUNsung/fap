b08d08f390602ebaad25fddd2cc374abe5427017
SUNsung <weatherius@gmail.com>
2018-05-08 07:02:24 +0300

    Rewrite [18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp] File in: 2018-05-08 07:02:23

--- cccccc/18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp
+++ cccccc/18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp
@@ -1,198 +1,286 @@
 
         
-        typedef NS_ENUM(NSInteger, ObjectBehaviorAction) {
-    ObjectBehaviorActionRetain,
-    ObjectBehaviorActionCopy,
-    ObjectBehaviorActionMutableCopy
-};
-    
-      unsigned getCodeCompletionBufferID() const {
-    return CodeCompletionBufferID;
-  }
-    
-    public:
-  /// The replacement type.
-  Type getReplacement() const { return Replacement; }
-  
-  /// The protocol conformances for the replacement. These appear in the same
-  /// order as Archetype->getConformsTo() for the substituted archetype.
-  const ArrayRef<ProtocolConformanceRef> getConformances() const {
-    return Conformance;
-  }
-  
-  Substitution() {}
-  
-  Substitution(Type Replacement, ArrayRef<ProtocolConformanceRef> Conformance);
-    
-    /// Runs the given function with the given context argument exactly once.
-/// The predicate argument must point to a global or static variable of static
-/// extent of type swift_once_t.
-void swift::swift_once(swift_once_t *predicate, void (*fn)(void *),
-                       void *context) {
-#if defined(__APPLE__)
-  dispatch_once_f(predicate, context, fn);
-#elif defined(__CYGWIN__)
-  _swift_once_f(predicate, context, fn);
-#else
-  std::call_once(*predicate, [fn, context]() { fn(context); });
-#endif
-}
-
-    
-      void OCRTester(const char* imgname, const char* groundtruth, const char* tessdatadir, const char* lang) {
-    //log.info() << tessdatadir << ' for language: ' << lang << std::endl;
-    char *outText;
-    std::locale loc('C'); // You can also use '' for the default system locale
-    std::ifstream file(groundtruth);
-    file.imbue(loc); // Use it for file input
-    std::string gtText((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
-    ASSERT_FALSE(api->Init(tessdatadir, lang)) << 'Could not initialize tesseract.';
-    Pix *image = pixRead(imgname);
-    ASSERT_TRUE(image != nullptr) << 'Failed to read test image.';
-    api->SetImage(image);
-    outText = api->GetUTF8Text();
-    EXPECT_EQ(gtText,outText) << 'Phototest.tif OCR does not match ground truth for ' << ::testing::PrintToString(lang);
-    api->End();
-    delete [] outText;
-    pixDestroy(&image);
-  }
-    
-    /** Find lines from the image making the BLOCK_LIST. */
-BLOCK_LIST* TessBaseAPI::FindLinesCreateBlockList() {
-  FindLines();
-  BLOCK_LIST* result = block_list_;
-  block_list_ = nullptr;
-  return result;
-}
-    
-      protected:
-    /**
-     * Called by concrete classes.
-     *
-     * outputbase is the name of the output file excluding
-     * extension. For example, '/path/to/chocolate-chip-cookie-recipe'
-     *
-     * extension indicates the file extension to be used for output
-     * files. For example 'pdf' will produce a .pdf file, and 'hocr'
-     * will produce .hocr files.
-     */
-    TessResultRenderer(const char *outputbase,
-                       const char* extension);
-    
-     protected:
-  // Function to compute part of a matrix.vector multiplication. The weights
-  // are in a very specific order (see above) in w, which is multiplied by
-  // u of length num_in, to produce output v after scaling the integer results
-  // by the corresponding member of scales.
-  // The amount of w and scales consumed is fixed and not available to the
-  // caller. The number of outputs written to v will be at most num_out.
-  typedef void (*PartialFunc)(const int8_t* w, const double* scales,
-                              const int8_t* u, int num_in, int num_out,
-                              double* v);
-    
-    // Computes one set of 4x8 products of inputs and weights, adding to result.
-// Horizontally adds 4 adjacent results, making 8x32-bit results.
-// rep_input is assumed to be an 8x replicated set of 4x8-bit signed integers.
-// Note that wi must previously have been re-organized with blocks of 4x8
-// weights in contiguous memory.
-// ones is a register of 16x16-bit values all equal to 1.
-// Note: wi is incremented by the amount of data read.
-// weights and reps are scratch registers.
-// This function must be inlined with references in order for the compiler to
-// correctly use the registers declared in the caller.
-inline void MultiplyGroup(const __m256i& rep_input, const __m256i& ones,
-                          const int8_t*& wi, __m256i& weights, __m256i& reps,
-                          __m256i& result) {
-  // Load a 4x8 block of weights.
-  weights = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(wi));
-  wi += kNumInputsPerRegister;
-  // Normalize the signs on rep_input, weights, so weights is always +ve.
-  reps = _mm256_sign_epi8(rep_input, weights);
-  weights = _mm256_sign_epi8(weights, weights);
-  // Multiply 32x8-bit reps by 32x8-bit weights to make 16x16-bit results,
-  // with adjacent pairs added.
-  weights = _mm256_maddubs_epi16(weights, reps);
-  // Multiply 16x16-bit result by 16x16-bit ones to make 8x32-bit results,
-  // with  adjacent pairs added. What we really want is a horizontal add of
-  // 16+16=32 bit result, but there is no such instruction, so multiply by
-  // 16-bit ones instead. It is probably faster than all the sign-extending,
-  // permuting and adding that would otherwise be required.
-  weights = _mm256_madd_epi16(weights, ones);
-  result = _mm256_add_epi32(result, weights);
-}
-    
-      // After identify the math blocks, we do one more scanning on all text
-  // partitions, and check if any of them is the satellite of:
-  // math blocks: here a p is the satellite of q if:
-  // 1. q is the nearest vertical neighbor of p, and
-  // 2. y_gap(p, q) is less than a threshold, and
-  // 3. x_overlap(p, q) is over a threshold.
-  // Note that p can be the satellites of two blocks: its top neighbor and
-  // bottom neighbor.
-  void ProcessMathBlockSatelliteParts();
-    
-    #include          <string.h>
-#include          <ctype.h>
-#include          'params.h'
-#include          'float2int.h'
-#include          'tesseractclass.h'
-    
-    using namespace std;
-using namespace thd;
-using namespace thd::rpc;
-    
-    using namespace std;
-    
-    template<> AT_API Half convert(int64_t f) {
-  return convert<Half,double>(static_cast<double>(f));
-}
-template<> AT_API int64_t convert(Half f) {
-  return static_cast<int64_t>(convert<double,Half>(f));
-}
-    
-    int64_t size(const Tensor& self, int64_t dim) {
-  // false is passed to maybe_wrap_dim so behavior is identical to array access (but with wrapping)
-  dim = maybe_wrap_dim(dim, self.dim(), false);
-  return self.sizes()[dim];
-}
-    
-    ${Storage}::${Storage}(Context* context, std::size_t size, std::unique_ptr<Allocator> allocator)
-  : storage(nullptr),
-    context(context) {
-  auto ctx = new detail::AllocatorRetainable(std::move(allocator));
-  storage = ${THStorage}_newWithAllocator(${state,} size, &wrapped_allocator, ctx);
-  ctx->release();
-  ${THStorage}_clearFlag(${state,} storage, TH_STORAGE_RESIZABLE);
-}
-    
-    
-    {  private:
-    QueueWorker::Request _request;
-  };
-    
-        std::shared_ptr<GlooCache::algorithm_type> algo;
-    if (device == DeviceType::CPU) {
-      algo = std::make_shared<::gloo::BroadcastOneToAll<T>>(
-        context,
-        std::initializer_list<T*>{reinterpret_cast<T*>(input_buffer.get())},
-        count,
-        src_rank);
-#ifdef WITH_CUDA
-    } else if (device == DeviceType::CUDA) {
-      auto stream = THCState_getCurrentStream(THDGetCudaState());
+        // Like the views::NativeFrameView, but returns the min/max size from the
+// NativeWindowViews.
+class NativeFrameView : public views::NativeFrameView {
+ public:
+  NativeFrameView(NativeWindow* window, views::Widget* widget);
     }
     
-    THDTensorDescriptor THDTensorDescriptor_newFromTHCudaDoubleTensor(THCudaDoubleTensor *tensor) {
-  return at::getType(at::Backend::CUDA, at::ScalarType::Double).unsafeTensorFromTH((void*)tensor, true);
+    #ifndef ATOM_BROWSER_UI_VIEWS_WIN_FRAME_VIEW_H_
+#define ATOM_BROWSER_UI_VIEWS_WIN_FRAME_VIEW_H_
+    
+    bool IsUnresponsiveEventSuppressed();
+    
+    
+    { protected:
+  virtual ~WindowListObserver() {}
+};
+    
+     private:
+  void* operator new(size_t size);
+  void operator delete(void*, size_t);
+    
+    namespace atom {
+    }
+    
+      // Panels Notifications. The Panels are small browser windows near the bottom
+  // of the screen.
+  // Sent when all nonblocking bounds animations are finished across panels.
+  // Used only in unit testing.
+  NOTIFICATION_PANEL_BOUNDS_ANIMATIONS_FINISHED,
+    
+    // Windows-specific implementation of the GlobalShortcutListener class that
+// listens for global shortcuts. Handles setting up a keyboard hook and
+// forwarding its output to the base class for processing.
+class GlobalShortcutListenerWin : public GlobalShortcutListener {
+ public:
+  GlobalShortcutListenerWin();
+  virtual ~GlobalShortcutListenerWin();
+    }
+    
+    
+    { protected:
+  virtual ~PrintViewManagerObserver() {}
+};
+    
+      // Return the parent NativeView of the observed WebContents.
+  gfx::NativeView GetParentView();
+    
+    // MonitorFinder maps a RenderFrameHost to the display ID on which the widget
+// is painting. This class operates on the IO thread while the RenderFrameHost
+// is on the UI thread, so the value returned by GetMonitor() may be 0 until
+// the information can be retrieved asynchronously.
+class MonitorFinder : public base::RefCountedThreadSafe<MonitorFinder> {
+ public:
+  MonitorFinder(int process_id, int render_frame_id);
+    }
+    
+    namespace chrome {
+class MonitorFinder;
+    }
+    
+    namespace ppapi {
+namespace host {
+struct HostMessageContext;
+}  // namespace host
+}  // namespace ppapi
+    
+    void TtsPlatformImpl::set_error(const std::string& error) {
+  error_ = error;
 }
     
+      // The user's custom colors.  Kept process-wide so that they can be persisted
+  // from one dialog invocation to the next.
+  static COLORREF g_custom_colors[16];
     
+      StringRef getLiteralContent() const { return LiteralContent; };
     
-    
-    
-    
-    
-    
-    {    delete[] glVertices;
+    SILDebugScope::SILDebugScope(SILLocation Loc, SILFunction *SILFn,
+                             const SILDebugScope *ParentScope ,
+                             const SILDebugScope *InlinedCallSite)
+    : Loc(Loc), InlinedCallSite(InlinedCallSite) {
+  if (ParentScope)
+    Parent = ParentScope;
+  else {
+    assert(SILFn && 'no parent provided');
+    Parent = SILFn;
+  }
 }
+    
+      virtual void handleDiagnostic(SourceManager &SM, SourceLoc Loc,
+                                DiagnosticKind Kind,
+                                StringRef FormatString,
+                                ArrayRef<DiagnosticArgument> FormatArgs,
+                                const DiagnosticInfo &Info) override;
+    
+    
+    {
+    {} // end namespace index
+} // end namespace swift
+    
+    /// A shorthand to clearly indicate that a value is a reference counted and
+/// heap-allocated.
+template <typename Inner>
+using RC = llvm::IntrusiveRefCntPtr<Inner>;
+    
+    
+    {
+    {} // end namespace index
+} // end namespace swift
+    
+      virtual bool enableWarnings() { return false; }
+  virtual bool indexLocals() { return false; }
+    
+        void TrainOrAdaptModel(int startEpoch, ComputationNetworkPtr net,
+                           bool networkLoadedFromCheckpoint,
+                           ComputationNetworkPtr refNet,
+                           ComputationNodeBasePtr refNode,
+                           IDataReader* trainSetDataReader,
+                           IDataReader* validationSetDataReader);
+    
+    
+    {            return result;
+        }
+    
+        // Taken from RS4
+    REGISTER_OPERATOR_SCHEMA(Hardmax)
+        .Description('Compute the hardmax normalized values for each layer in the batch '
+            'of the given input. The input is a 2-D tensor (Tensor<float>) of size '
+            '(batch_size x input_feature_dimensions). The output tensor has the same shape '
+            'and contains the softmax normalized values of the corresponding input. '
+            '\n'
+            'X does not need to explicitly be a 2D vector; rather, it will be coerced into '
+            'one. For an arbitrary n-dimensional tensor X in [a_0, a_1, ..., a_{k-1}, '
+            'a_k, ..., a_{n-1}] and k is the axis provided, then X will be coerced into a '
+            '2-dimensional tensor with dimensions [a_0 * ... * a_{k-1}, a_k * ... * a_{n-1}]. '
+            'For the default case where axis=1, this means the X tensor will be coerced into '
+            'a 2D tensor of dimensions [a_0, a_1 * ... * a_{n-1}], where a_0 is often the '
+            'batch size.  In this situation, we must have a_0 = N and a_1 * ... * a_{n-1} = D. '
+            'Each of these dimensions must be matched correctly, or else the operator will '
+            'throw errors.')
+        .Input('input', 'The input tensor that's coerced into a 2D matrix of size (NxD) as '
+            'described above.', 'T')
+        .Output('output', 'Output tensor of same shape and type as input X.', 'T')
+        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
+            'Constrain input and output types to float tensors.')
+        .Attr('axis', 'Default to 1; describes the axis of the inputs when coerced to 2D; '
+            'defaults to one because the 0th axis most likely describes the batch size.',
+            AttrType::AttributeProto_AttributeType_INT, int64_t(1));
+    
+        // Taken from ONNX
+    REGISTER_OPERATOR_SCHEMA(Pad)
+        .Description('Given data tensor, paddings, mode, and value. '
+            'Example: Insert 0 paddings to the beginning of the second dimension. '
+            'data = [ [1.0, 1.2], [2.3, 3.4], [4.5, 5.7], ] paddings = [0, 0, 2, 0] '
+            'output = [ [ [0.0, 0.0, 1.0, 1.2], [0.0, 0.0, 2.3, 3.4], [0.0, 0.0, 4.5, 5.7] ] ]')
+        .Input('data', 'Input tensor.', 'T')
+        .Output('output', 'Tensor after padding.', 'T')
+        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
+            'Constrain input and output types to float tensors.')
+        .Attr('pads',
+              'List of integers indicate the padding sizes, paddings's length '
+              'should be the double of input's dimension. '
+              'The order should be axis_0_begin, axis_0_end, axis_1_begin, ..., '
+              'axis_n_begin, axis_n_end, n is input's dimension.',
+              AttrType::AttributeProto_AttributeType_INTS, int64_t(1))
+        .Attr('mode',
+              'Three modes: constant(default), reflect, edge',
+              AttrType::AttributeProto_AttributeType_STRING, std::string('constant'))
+        .Attr('value',
+              'One float, indicates the value to be filled, default is 0',
+              AttrType::AttributeProto_AttributeType_FLOAT, float(0));
+    
+    
+    {
+    {        cd.m_numberOfSequences = m_chunks[i]->NumberOfSequences();
+        cd.m_numberOfSamples = m_chunks[i]->NumberOfSamples();
+        chunks.push_back(cd);
+    }
+    return chunks;
+}
+    
+    #include <algorithm>
+#include 'CNTKLibrary.h'
+#include <functional>
+    
+    /// Copies bytes from a source buffer sequence to a target buffer.
+/**
+ * @param target A modifiable buffer representing the memory region to which
+ * the bytes will be copied.
+ *
+ * @param source A non-modifiable buffer sequence representing the memory
+ * regions from which the bytes will be copied.
+ *
+ * @returns The number of bytes copied.
+ *
+ * @note The number of bytes copied is the lesser of:
+ *
+ * @li @c buffer_size(target)
+ *
+ * @li @c buffer_size(source)
+ *
+ * This function is implemented in terms of @c memcpy, and consequently it
+ * cannot be used to copy between overlapping memory regions.
+ */
+template <typename ConstBufferSequence>
+inline std::size_t buffer_copy(const mutable_buffers_1& target,
+    const ConstBufferSequence& source)
+{
+  return buffer_copy(static_cast<const mutable_buffer&>(target), source);
+}
+    
+    namespace boost {
+namespace asio {
+    }
+    }
+    
+    template <typename Stream>
+class buffered_write_stream;
+    
+    namespace boost {
+    }
+    
+    #include <boost/asio/detail/pop_options.hpp>
+    
+    #if !defined(BOOST_ASIO_WINDOWS_RUNTIME)
+    
+    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+    
+    #include <boost/asio/detail/push_options.hpp>
+    
+    #define BOOST_ASIO_READ_HANDLER_CHECK( \
+    handler_type, handler) \
+  \
+  typedef BOOST_ASIO_HANDLER_TYPE(handler_type, \
+      void(boost::system::error_code, std::size_t)) \
+    asio_true_handler_type; \
+  \
+  BOOST_ASIO_HANDLER_TYPE_REQUIREMENTS_ASSERT( \
+      sizeof(boost::asio::detail::two_arg_handler_test( \
+          boost::asio::detail::clvref< \
+            asio_true_handler_type>(), \
+          static_cast<const boost::system::error_code*>(0), \
+          static_cast<const std::size_t*>(0))) == 1, \
+      'ReadHandler type requirements not met') \
+  \
+  typedef boost::asio::detail::handler_type_requirements< \
+      sizeof( \
+        boost::asio::detail::argbyv( \
+          boost::asio::detail::clvref< \
+            asio_true_handler_type>())) + \
+      sizeof( \
+        boost::asio::detail::lvref< \
+          asio_true_handler_type>()( \
+            boost::asio::detail::lvref<const boost::system::error_code>(), \
+            boost::asio::detail::lvref<const std::size_t>()), \
+        char(0))> BOOST_ASIO_UNUSED_TYPEDEF
+    
+    
+    {private:
+  byte* bytes_;
+  UINT32 length_;
+  UINT32 capacity_;
+};
+    
+    #include <boost/asio/detail/config.hpp>
+#include <cerrno>
+#include <boost/asio/detail/descriptor_ops.hpp>
+#include <boost/asio/error.hpp>
+    
+    #if defined(BOOST_ASIO_HAS_DEV_POLL)
+    
+    template <typename T> struct jni_sig_from_cxx_t;
+template <typename R, typename... Args>
+struct jni_sig_from_cxx_t<R(Args...)> {
+  using JniRet = typename Convert<typename std::decay<R>::type>::jniType;
+  using JniSig = JniRet(typename Convert<typename std::decay<Args>::type>::jniType...);
+};
+    
+        int getPositionType(void) const;
+    Value getPosition(int edge) const;
+    
+    #ifndef FBASSERT_H
+#define FBASSERT_H
