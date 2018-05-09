4b34b32f8bb318723da0f5cc07727a837b7fb2e3
SUNsung <weatherius@gmail.com>
2018-05-09 03:15:56 +0300

    Rewrite [52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp] File in: 2018-05-09 03:15:55

--- cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
+++ cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
@@ -1,276 +1,73 @@
 
         
-          DebuggerClient(ASTContext &C) : Ctx(C) { }
-  virtual ~DebuggerClient() = default;
-  
-  // DebuggerClient is consulted at the beginning of the parsing
-  // of various DeclKinds to see whether the decl should be parsed
-  // in the global context rather than the current context.
-  // This question will only be asked if the decl's current context
-  // is a function marked with the LLDBDebuggerFunction attribute.
-  virtual bool shouldGlobalize(Identifier Name, DeclKind kind) = 0;
-  
-  virtual void didGlobalize (Decl *Decl) = 0;
-    
-    
-    {} // namespace swift
-    
-    class HTML final : public MarkupASTNode {
-  StringRef LiteralContent;
-  HTML(StringRef LiteralContent)
-      : MarkupASTNode(ASTNodeKind::HTML),
-        LiteralContent(LiteralContent) {}
-public:
-  static HTML *create(MarkupContext &MC, StringRef LiteralContent);
-  StringRef getLiteralContent() const { return LiteralContent; };
+        namespace grpc {
+namespace testing {
+    }
     }
     
-      llvm::SMDiagnostic GetMessage(SourceLoc Loc, llvm::SourceMgr::DiagKind Kind,
-                                const Twine &Msg,
-                                ArrayRef<llvm::SMRange> Ranges,
-                                ArrayRef<llvm::SMFixIt> FixIts) const;
+    #include 'test/cpp/qps/benchmark_config.h'
+#include 'test/cpp/qps/driver.h'
+#include 'test/cpp/qps/report.h'
+#include 'test/cpp/qps/server.h'
+#include 'test/cpp/util/test_config.h'
+#include 'test/cpp/util/test_credentials_provider.h'
     
-    #ifndef SWIFT_SYNTAX_REFERENCES_H
-#define SWIFT_SYNTAX_REFERENCES_H
+    static const int WARMUP = 5;
+static const int BENCHMARK = 5;
     
-    #ifndef SWIFT_AST_SUBSTITUTION_H
-#define SWIFT_AST_SUBSTITUTION_H
+    #include <signal.h>
     
-    void convert_dataset(const string& input_folder, const string& output_folder,
-    const string& db_type) {
-  scoped_ptr<db::DB> train_db(db::GetDB(db_type));
-  train_db->Open(output_folder + '/cifar10_train_' + db_type, db::NEW);
-  scoped_ptr<db::Transaction> txn(train_db->NewTransaction());
-  // Data buffer
-  int label;
-  char str_buffer[kCIFARImageNBytes];
-  Datum datum;
-  datum.set_channels(3);
-  datum.set_height(kCIFARSize);
-  datum.set_width(kCIFARSize);
+      // Proto2 Python
+  google::protobuf::compiler::python::Generator py_generator;
+  cli.RegisterGenerator('--python_out', &py_generator,
+                        'Generate Python source file.');
+    
+    TEST_F(MockEnvTest, Corrupt) {
+  const std::string kGood = 'this is a good string, synced to disk';
+  const std::string kCorrupted = 'this part may be corrupted';
+  const std::string kFileName = '/dir/f';
+  unique_ptr<WritableFile> writable_file;
+  ASSERT_OK(env_->NewWritableFile(kFileName, &writable_file, soptions_));
+  ASSERT_OK(writable_file->Append(kGood));
+  ASSERT_TRUE(writable_file->GetFileSize() == kGood.size());
     }
     
-    #endif  // CAFFE_CUDNN_CONV_LAYER_HPP_
+    
+    {} // namespace rocksdb
 
     
-      virtual inline const char* type() const { return 'Exp'; }
-    
-    namespace HPHP {
-/////////////////////////////////////////////////////////////////////////////
+    TEST_F(StringAppendOperatorTest, SimpleDelimiterTest) {
+  auto db = OpenDb('|');
+  StringLists slists(db);
     }
     
-    void Timer::RequestExit() {
-  Dump();
+      /// If list is empty, return false and leave *result unchanged.
+  /// Else, remove the first/last elem, store it in *result, and return true
+  bool PopLeft(const std::string& key, std::string* result);  // First
+  bool PopRight(const std::string& key, std::string* result); // Last
+    
+      Options GetOptions() { return DBTestBase::GetOptions(GetParam()); }
+    
+    // Return the length of the encoding of 'key'.
+inline size_t InternalKeyEncodingLength(const ParsedInternalKey& key) {
+  return key.user_key.size() + 8;
 }
     
-    int gettime(clockid_t, struct timespec*);
-int64_t gettime_ns(clockid_t);
+      // Returns an estimate of the number of bytes of data in use by this
+  // data structure.
+  //
+  // REQUIRES: external synchronization to prevent simultaneous
+  // operations on the same MemTable (unless this Memtable is immutable).
+  size_t ApproximateMemoryUsage();
     
-    Vtuple Vunit::makeTuple(VregList&& regs) {
-  auto i = tuples.size();
-  tuples.emplace_back(std::move(regs));
-  return Vtuple{i};
-}
-    
-      if (data.isArray()
-      || data.isResource()
-      || (data.isString() && data.toString().empty())
-      || (data.isObject() && !data.toObject().instanceof(s_GMP_GMP))
-      || !variantToGMPData('gmp_intval', gmpData, data)) {
-    return data.toInt64();
-  }
-    
-    
-    {  return env.irb->makeBlock(sk, curProfCount(env));
-}
-    
-    
-    {
-    {} } // HPHP::jit
-
-    
-    void orderFuncs(const TargetGraph& cg, Cluster* c1, Cluster* c2) {
-  TargetId c1head = c1->targets[0];
-  TargetId c1tail = c1->targets[c1->targets.size() - 1];
-  TargetId c2head = c2->targets[0];
-  TargetId c2tail = c2->targets[c2->targets.size() - 1];
+    TEST_F(InlineSkipTest, ConcurrentInsertWithoutThreads) {
+  ConcurrentTest test;
+  Random rnd(test::RandomSeed());
+  for (int i = 0; i < 10000; i++) {
+    test.ReadStep(&rnd);
+    uint32_t base = rnd.Next();
+    for (int j = 0; j < 4; ++j) {
+      test.ConcurrentWriteStep((base + j) % ConcurrentTest::K);
     }
-    
-    
-Instruction* Instruction::ImmPCOffsetTarget(Instruction* from) {
-  ptrdiff_t offset;
-  if (IsPCRelAddressing()) {
-    // PC-relative addressing. Only ADR is supported.
-    offset = ImmPCRel();
-  } else if (IsLoadOrStore()) {
-    offset = ImmLLiteral() << kInstructionSizeLog2;
-  } else {
-    // All PC-relative branches.
-    assert(BranchType() != UnknownBranchType);
-    // Relative branch offsets are instruction-size-aligned.
-    offset = ImmBranch() << kInstructionSizeLog2;
-  }
-  return (!from ? this : from) + offset;
-}
-    
-    
-    {
-    {			if (szArgList[i + 1] != NULL && szArgList[i + 1][0] != '/') {
-				cmderOptions.cmderCfgRoot = szArgList[i + 1];
-				i++;
-			}
-		}
-		else if (_wcsicmp(L'/start', szArgList[i]) == 0)
-		{
-			if (PathFileExists(szArgList[i + 1]))
-			{
-				cmderOptions.cmderStart = szArgList[i + 1];
-				i++;
-			}
-			else {
-				MessageBox(NULL, szArgList[i + 1], L'/START - Folder doses not exist!', MB_OK);
-			}
-		}
-		else if (_wcsicmp(L'/task', szArgList[i]) == 0)
-		{
-			cmderOptions.cmderTask = szArgList[i + 1];
-			i++;
-		}
-		else if (_wcsicmp(L'/single', szArgList[i]) == 0)
-		{
-			cmderOptions.cmderSingle = true;
-		}
-		else if (_wcsicmp(L'/register', szArgList[i]) == 0)
-		{
-			cmderOptions.registerApp = true;
-			cmderOptions.unRegisterApp = false;
-			if (szArgList[i + 1] != NULL)
-			{
-				if (_wcsicmp(L'all', szArgList[i + 1]) == 0 || _wcsicmp(L'user', szArgList[i + 1]) == 0)
-				{
-					cmderOptions.cmderRegScope = szArgList[i + 1];
-					i++;
-				}
-			}
-		}
-		else if (_wcsicmp(L'/unregister', szArgList[i]) == 0)
-		{
-			cmderOptions.unRegisterApp = true;
-			cmderOptions.registerApp = false;
-			if (szArgList[i + 1] != NULL)
-			{
-				if (_wcsicmp(L'all', szArgList[i + 1]) == 0 || _wcsicmp(L'user', szArgList[i + 1]) == 0)
-				{
-					cmderOptions.cmderRegScope = szArgList[i + 1];
-					i++;
-				}
-			}
-		}
-		else if (cmderOptions.cmderStart == L'' && PathFileExists(szArgList[i]))
-		{
-			cmderOptions.cmderStart = szArgList[i];
-		}
-		else {
-			MessageBox(NULL, L'Unrecognized parameter.\n\nValid options:\n\n    /c [CMDER User Root Path]\n\n    /task [ConEmu Task Name]\n\n    [/start [Start in Path] | [Start in Path]]\n\n    /single\n\nor\n\n    /register [USER | ALL]\n\nor\n\n    /unregister [USER | ALL]\n', MB_TITLE, MB_OK);
-			cmderOptions.error = true;
-		}
-	}
-    
-    #include <boost/algorithm/string/trim.hpp>
-#include <boost/lexical_cast.hpp>
-    
-     private:
-  /*
-   * @brief When `get`ing event results, return EventID%s from time indexes.
-   *
-   * Used by EventSubscriber::get to retrieve EventID, EventTime indexes. This
-   * applies the lookup-efficiency checks for time list appropriate bins.
-   * If the time range in 24 hours and there is a 24-hour list bin it will
-   * be queried using a single backing store `Get` followed by two `Get`s of
-   * the most-specific boundary lists.
-   *
-   * @param index the set of index to scan.
-   * @param optimize if true apply optimization checks.
-   *
-   * @return List of EventID, EventTime%s
-   */
-  std::vector<EventRecord> getRecords(const std::vector<std::string>& indexes,
-                                      bool optimize = true);
-    
-    /// Start an ExtensionWatcher thread.
-Status startExtensionWatcher(const std::string& manager_path,
-                             size_t interval,
-                             bool fatal);
-    
-    
-    {  /// Mutex to protect use of the shutdown callable.
-  static RecursiveMutex shutdown_mutex_;
-};
-    
-    
-    {
-    { private:
-  friend class TLSConfigTests;
-};
-}
-
-    
-    
-    {  // Support a signed 64, a double, and treat everything else as a signed int.
-  if (type == kCFNumberSInt64Type) {
-    long long int value;
-    if (CFNumberGetValue((CFNumberRef)cf_number, type, &value)) {
-      return boost::lexical_cast<std::string>(value);
-    }
-  } else if (type == kCFNumberDoubleType) {
-    double value;
-    if (CFNumberGetValue((CFNumberRef)cf_number, type, &value)) {
-      return boost::lexical_cast<std::string>(value);
-    }
-  } else {
-    unsigned int value;
-    if (CFNumberGetValue((CFNumberRef)cf_number, type, &value)) {
-      return boost::lexical_cast<std::string>(value);
-    }
-  }
-  // Cast as a string.
-  return '0';
-}
-    
-    std::shared_ptr<PlatformProcess> PlatformProcess::launchTestPythonScript(
-    const std::string& args) {
-  std::string osquery_path;
-  auto osquery_path_option = getEnvVar('OSQUERY_DEPS');
-  if (osquery_path_option) {
-    osquery_path = *osquery_path_option;
-  } else {
-    if (!isPlatform(PlatformType::TYPE_FREEBSD)) {
-      osquery_path = '/usr/local/osquery';
-    } else {
-      osquery_path = '/usr/local';
-    }
-  }
-    }
-    
-    
-    {
-    {    struct addrinfo* res = nullptr;
-    if (getaddrinfo(fqdn_string.c_str(), nullptr, &hints, &res) == 0) {
-      if (res->ai_canonname != nullptr) {
-        fqdn_string = res->ai_canonname;
-      }
-    }
-    if (res != nullptr) {
-      freeaddrinfo(res);
-    }
-#endif
-    return fqdn_string;
-  } else {
-    unsigned long size = 256;
-    std::vector<char> fqdn(size, 0x0);
-#ifdef WIN32
-    GetComputerNameEx(ComputerNameDnsFullyQualified, fqdn.data(), &size);
-#endif
-    return fqdn.data();
   }
 }
