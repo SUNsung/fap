1486fa67ea1139fccd0db3a4fbaaf9c3b31ed9ef
SUNsung <weatherius@gmail.com>
2018-05-09 11:19:28 +0300

    Rewrite [057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp] File in: 2018-05-09 11:19:27

--- cccccc/057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp
+++ cccccc/057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp
@@ -1,42 +1,116 @@
 
         
-          if (nDimension != self->nDimension)
-    hasRequiredSize = false;
+        cv::Mat dls::LeftMultVec(const cv::Mat& v)
+{
+    cv::Mat mat_ = cv::Mat::zeros(3, 9, CV_64F);
+    }
     
     
-    {  delete tensor;
-  delete tensor2;
-  cout << 'OK' << endl;
-  return 0;
-}
-
-    
-    bool cudnn_is_acceptable(const Tensor& self) {
-  if (!globalContext().userEnabledCuDNN()) return false;
-  if (!self.is_cuda()) return false;
-  auto st = self.type().scalarType();
-  if (!(st == kDouble || st == kFloat || st == kHalf)) return false;
-  if (!AT_CUDNN_ENABLED()) return false;
-  // NB: In the old Python code, there was also a test to see if the
-  // cuDNN library was actually dynamically linked or not.  I'm not
-  // sure if we can actually test this.
-  return true;
+    {    // temporary solution, have to make something more precise
+    corner = contour[min_idx];
+    return;
 }
     
-    int ${Storage}::getDevice() const {
-  ${storage_device} //storage->device;
+            if (lane < 16)
+        {
+            T partial = ptr[tid];
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    
+    /*
+ * Vlabel wraps a block number.
+ */
+DECLARE_VNUM(Vlabel, true, 'B');
+    
+      std::vector<LdSSwitchData::Elm> cases(numCases);
+  for (int i = 0; i < numCases; ++i) {
+    auto const& kv = iv.strvec()[i];
+    cases[i].str  = curUnit(env)->lookupLitstrId(kv.str);
+    cases[i].dest = SrcKey{curSrcKey(env), bcOff(env) + kv.dest};
+  }
+    
+    //////////////////////////////////////////////////////////////////////
+    
+    
+void Instruction::SetBranchImmTarget(Instruction* target) {
+  assert(((target - this) & 3) == 0);
+  Instr branch_imm = 0;
+  uint32_t imm_mask = 0;
+  int offset = (target - this) >> kInstructionSizeLog2;
+  switch (BranchType()) {
+    case CondBranchType: {
+      branch_imm = Assembler::ImmCondBranch(offset);
+      imm_mask = ImmCondBranch_mask;
+      break;
+    }
+    case UncondBranchType: {
+      branch_imm = Assembler::ImmUncondBranch(offset);
+      imm_mask = ImmUncondBranch_mask;
+      break;
+    }
+    case CompareBranchType: {
+      branch_imm = Assembler::ImmCmpBranch(offset);
+      imm_mask = ImmCmpBranch_mask;
+      break;
+    }
+    case TestBranchType: {
+      branch_imm = Assembler::ImmTestBranch(offset);
+      imm_mask = ImmTestBranch_mask;
+      break;
+    }
+    default: not_reached();
+  }
+  SetInstructionBits(Mask(~imm_mask) | branch_imm);
 }
     
-    int64_t ${Tensor}::dim() const {
-  if(isScalar())
-    return 0;
-  int64_t d = ${THTensor_nDimension};
-  // See Note [Empty versus 0-dim tensors]
-  if (d != 0)
-    return d;
-  return kEmptySizes.size();
-}
+    bool get_embedded_data(const char* section, embedded_data* desc,
+                       const std::string& filename = '');
     
-    namespace torch { namespace utils {
+      static FILE *popen(const char *cmd, const char *type,
+                     const char *cwd = nullptr);
+  static int pclose(FILE *f);
+    
+            std::map<wstring, shared_ptr<IStatefulNode>> EnumerateStatefulNode(ComputationNetwork& net,
+                                                                           const std::vector<ComputationNodeBasePtr>& criterionNode,
+                                                                           const std::vector<ComputationNodeBasePtr>& evaluationNode)
+        {
+            std::map<wstring, shared_ptr<IStatefulNode>> statefulNodes;
+            for (auto& root : criterionNode)
+                EnumerateStatefulNodesForRoot(net, root, statefulNodes);
+            for (auto& root : evaluationNode)
+                EnumerateStatefulNodesForRoot(net, root, statefulNodes);
+            return statefulNodes;
+        }
+    
+        const OperatorSchema* OpSchemaRegistry::Schema(
+        const std::string& p_key,
+        const int p_maxInclusiveVersion,
+        const std::string& p_domain)
+    {
+        auto& m = map();
+        if (m.count(p_key) && m[p_key].count(p_domain))
+        {
+            auto pos = m[p_key][p_domain].lower_bound(p_maxInclusiveVersion);
+            if (m[p_key][p_domain].begin() == pos && pos->first > p_maxInclusiveVersion)
+            {
+                // All versions are greater than specified version.
+                return nullptr;
+            }
     }
     }
+    
+        REGISTER_ELEMENTWISE_BROADCAST_OPERATOR_SCHEMA(Add)
+        REGISTER_ELEMENTWISE_BROADCAST_OPERATOR_SCHEMA(Sub)
+        REGISTER_ELEMENTWISE_BROADCAST_OPERATOR_SCHEMA(Mul)
+        REGISTER_ELEMENTWISE_BROADCAST_OPERATOR_SCHEMA(Div)
+    
+        virtual void CopyTo(ComputationNodeBasePtr nodeP, const std::wstring& newName, const CopyNodeFlags flags) const override
+    {
+        Base::CopyTo(nodeP, newName, flags);
+    }
+    
+        // Let's check that there is no outstanding copies.
+    // Wait on all events if there are any pending copy operations in flight.
+    if (m_dataTransferers[m_currentDataTransferIndex])
+        m_dataTransferers[m_currentDataTransferIndex]->WaitForCopyCPUToGPU();
