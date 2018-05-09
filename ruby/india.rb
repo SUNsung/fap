57e7d1a39e4ec13e16bf2c47aeb3626c065b48ea
SUNsung <weatherius@gmail.com>
2018-05-10 02:59:15 +0300

    Rewrite [india.rb] File in: 2018-05-10 02:59:15

--- ruby/india.rb
+++ ruby/india.rb
@@ -1,24 +1,80 @@
 
         
-          def save_and_refresh_staff_groups!
-    transaction do
-      self.save!
-      Group.refresh_automatic_groups!(:admins, :moderators, :staff)
+        def bottle_resolve_version(bottle_file)
+  PkgVersion.parse bottle_receipt_path(bottle_file).split('/')[1]
+end
+    
+      def find_internal_commands(directory)
+    directory.children.reduce([]) do |cmds, f|
+      cmds << f.basename.to_s.sub(/\.(?:rb|sh)$/, '') if f.file?
+      cmds
+    end
+  end
+end
+
+    
+      SEARCHABLE_TAPS = OFFICIAL_TAPS.map { |tap| ['Homebrew', tap] } + [
+    %w[Caskroom cask],
+    %w[Caskroom versions]
+  ]
+    
+          export JAVA_HOME='$(/usr/libexec/java_home)'
+      export AWS_ACCESS_KEY='<Your AWS Access ID>'
+      export AWS_SECRET_KEY='<Your AWS Secret Key>'
+      export #{home_name}='#{home_value}'
+    EOS
+  end
+end
+
+    
+      def present(payload)
+    if payload.is_a?(Hash)
+      payload = ActiveSupport::HashWithIndifferentAccess.new(payload)
+      MAIN_KEYS.each do |key|
+        return { :title => payload[key].to_s, :entries => present_hash(payload, key) } if payload.has_key?(key)
+      end
+    
+    class Agents::ExampleAgent < Agent
+  include LongRunnable
+    
+      # POST /resource/password
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    yield resource if block_given?
+    
+        if successfully_sent?(resource)
+      respond_with({}, location: after_sending_unlock_instructions_path_for(resource))
+    else
+      respond_with(resource)
     end
   end
     
-      context 'called with four sizes' do
-    it 'applies different widths to all sides' do
-      rule = 'padding: 7px 8px 9px 10px'
+          def remember_key(resource, scope)
+        resource.rememberable_options.fetch(:key, 'remember_#{scope}_token')
+      end
+    end
+  end
+end
+
     
-      context 'called with three sizes' do
-    it 'applies second width to left and right' do
-      ruleset = 'position: relative; ' +
-                'top: 4px; ' +
-                'right: 5px; ' +
-                'bottom: 6px; ' +
-                'left: 5px;'
+        def insert_after(index, *names)
+      insert assert_index(index) + 1, *names
+    end
     
-      context 'called with no prefixes' do
-    it 'outputs the spec' do
-      rule = 'appearance: none;'
+        def initialize
+      @pages = {}
+    end
+    
+          if options && options[:ignore_case]
+        base = base.downcase
+        dest = dest.downcase
+      end
+    
+            css('.note h3', '.warning h3').each do |node|
+          node.before('<p><strong>#{node.inner_html}</strong></p>').remove
+        end
+    
+            css('.bs-docs-sidenav > li').each do |node|
+          link = node.at_css('a')
+          name = link.content
+          next if IGNORE_ENTRIES.include?(name)
