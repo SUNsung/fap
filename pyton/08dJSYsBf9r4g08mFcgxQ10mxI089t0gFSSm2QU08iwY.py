08d2baf3e5a28752c79c11600575755064f70d52
SUNsung <weatherius@gmail.com>
2018-05-09 16:42:04 +0300

    Rewrite [08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py] File in: 2018-05-09 16:42:03

--- pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
+++ pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
@@ -1,111 +1,170 @@
 
         
-            def test_format(self):
-        for x in special:
-            self.check_format_1(x)
-        for i in range(10):
-            for lenx in range(1, MAXDIGITS+1):
-                x = self.getran(lenx)
-                self.check_format_1(x)
+            def to_python(self, value):
+        return UUID(value)
     
-            frame, label = self.dialog.make_frame('testlabel')
-        self.assertEqual(label['text'], 'testlabel')
-        self.assertIsInstance(frame, Frame)
+    else:
+    text_type = unicode
+    string_types = (str, unicode)
+    integer_types = (int, long)
     
-    import importlib
-import os
-from sys import executable, platform  # Platform is set for one test.
+        Either you can fill the config from a config file::
     
-    class TestSubclassWithKwargs(unittest.TestCase):
-    def test_subclass_with_kwargs(self):
-        # SF bug #1486663 -- this used to erroneously raise a TypeError
-        SubclassWithKwargs(newarg=1)
+                if next(lineiter).count('-') != len(match.group(0)):
+                continue
+    
+    
+def test_config_missing():
+    app = flask.Flask(__name__)
+    with pytest.raises(IOError) as e:
+        app.config.from_pyfile('missing.cfg')
+    msg = str(e.value)
+    assert msg.startswith('[Errno 2] Unable to load configuration '
+                          'file (No such file or directory):')
+    assert msg.endswith('missing.cfg'')
+    assert not app.config.from_pyfile('missing.cfg', silent=True)
+    
+        def __init__(self, chunk_size=CHUNK_SIZE, **kwargs):
+        super(RawStream, self).__init__(**kwargs)
+        self.chunk_size = chunk_size
+    
+    
+with codecs.open(FILE_PATH, encoding='utf8') as f:
+    # Strip because we don't want new lines in the data so that we can
+    # easily count occurrences also when embedded in JSON (where the new
+    # line would be escaped).
+    FILE_CONTENT = f.read().strip()
+    
+    
+def test_follow_all_output_options_used_for_redirects(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 3
+    assert HTTP_OK not in r
+    
+        try:
+        if args.download:
+            args.follow = True  # --download implies --follow.
+            downloader = Downloader(
+                output_file=args.output_file,
+                progress_file=env.stderr,
+                resume=args.download_resume
+            )
+            downloader.pre_request(args.headers)
+    
+        # If both `auth_parse` and `prompt_password` are set to `True`,
+    # and the value of `-a` lacks the password part,
+    # then the user will be prompted to type the password in.
+    prompt_password = True
+    
+            See https://github.com/jakubroztocil/httpie/issues/212
+    
+    sessions.add_argument(
+    '--session',
+    metavar='SESSION_NAME_OR_PATH',
+    type=session_name_validator,
+    help='''
+    Create, or reuse and update a session. Within a session, custom headers,
+    auth credential, as well as any cookies sent by the server persist between
+    requests.
+    
+        # Used only when requested with --check-status:
+    ERROR_HTTP_3XX = 3
+    ERROR_HTTP_4XX = 4
+    ERROR_HTTP_5XX = 5
+    
+            self.env = env
+        self.args, no_options = super(HTTPieArgumentParser, self)\
+            .parse_known_args(args, namespace)
+    
+        def remove(self, key):
+        hash_index = self._hash_function(key)
+        for index, item in enumerate(self.table[hash_index]):
+            if item.key == key:
+                del self.table[hash_index][index]
+                return
+        raise KeyError('Key not found')
+    
+        OPERATOR = 0
+    SUPERVISOR = 1
+    DIRECTOR = 2
+    
+        def reduce_priority_link_to_crawl(self, url):
+        '''Reduce the priority of a link in `links_to_crawl` to avoid cycles.'''
+        pass
+    
+        def mapper(self, _, line):
+        '''Parse each log line, extract and transform relevant lines.
+    
+        def __init__(self, level, row, spot_number, spot_size, vehicle_size):
+        self.level = level
+        self.row = row
+        self.spot_number = spot_number
+        self.spot_size = spot_size
+        self.vehicle_size = vehicle_size
+        self.vehicle = None
+    
+    from enum import Enum
+    
+        if WSAAddressToStringA(
+            ctypes.byref(addr),
+            addr_size,
+            None,
+            ip_string,
+            ctypes.byref(ip_string_size)
+    ) != 0:
+        raise socket.error(ctypes.FormatError())
+    
+                    if c >= self.min[s] and c <= self.max[s]:
+                    # move to next state
+                    snext = self.transition[s][c-self.min[s]]
+                    #print 'in range, next state = %d' % snext
+                    
+                    if snext < 0:
+                        #print 'not a normal transition'
+                        # was in range but not a normal transition
+                        # must check EOT, which is like the else clause.
+                        # eot[s]>=0 indicates that an EOT edge goes to another
+                        # state.
+                        if self.eot[s] >= 0: # EOT Transition to accept state?
+                            #print 'EOT trans to accept state %d' % self.eot[s]
+                            
+                            s = self.eot[s]
+                            input.consume()
+                            # TODO: I had this as return accept[eot[s]]
+                            # which assumed here that the EOT edge always
+                            # went to an accept...faster to do this, but
+                            # what about predicated edges coming from EOT
+                            # target?
+                            continue
+    
+    def version_str_to_tuple(version_str):
+    import re
+    import sys
+    
+        '''
+    
+        def deploy_cert(self, domain, cert_path, key_path, chain_path=None,
+                    fullchain_path=None):
+        '''Installs cert'''
+        cert_path, key_path, chain_path = self.copy_certs_and_keys(
+            cert_path, key_path, chain_path)
+        self._configurator.deploy_cert(
+            domain, cert_path, key_path, chain_path, fullchain_path)
+
     
         @classmethod
-    def zonelist(cls, zonedir='/usr/share/zoneinfo'):
-        zones = []
-        for root, _, files in os.walk(zonedir):
-            for f in files:
-                p = os.path.join(root, f)
-                with open(p, 'rb') as o:
-                    magic =  o.read(4)
-                if magic == b'TZif':
-                    zones.append(p[len(zonedir) + 1:])
-        return zones
-    
-        def head(self, message_spec=None, *, file=None):
-        '''Process a HEAD command.  Argument:
-        - message_spec: article number or message id
-        - file: filename string or file object to store the headers in
-        Returns:
-        - resp: server response if successful
-        - ArticleInfo: (article number, message id, list of header lines)
-        '''
-        if message_spec is not None:
-            cmd = 'HEAD {0}'.format(message_spec)
-        else:
-            cmd = 'HEAD'
-        return self._artcmd(cmd, file)
-    
-        def test_infile_outfile(self):
-        infile = self._create_infile()
-        outfile = support.TESTFN + '.out'
-        rc, out, err = assert_python_ok('-m', 'json.tool', infile, outfile)
-        self.addCleanup(os.remove, outfile)
-        with open(outfile, 'r') as fp:
-            self.assertEqual(fp.read(), self.expect)
-        self.assertEqual(rc, 0)
-        self.assertEqual(out, b'')
-        self.assertEqual(err, b'')
-    
-    class Test(unittest.TestCase):
-    def test_from_buffer(self):
-        a = array.array('i', range(16))
-        x = (c_int * 16).from_buffer(a)
-    
-        def test_4(self):
-        class X(Structure):
-            pass
-        class Y(X):
-            pass
-        self.assertRaises(AttributeError, setattr, X, '_fields_', [])
-        Y._fields_ = []
-        self.assertRaises(AttributeError, setattr, X, '_fields_', [])
-    
-                # running iterator
-            next(itorig)
-            d = pickle.dumps((itorig, orig), proto)
-            it, a = pickle.loads(d)
-            a[:] = data
-            self.assertEqual(type(it), type(itorig))
-            self.assertEqual(list(it), data[len(orig)-2::-1])
-    
-    define('port', default=8888, help='run on the given port', type=int)
-    
-                def finish(self):
-                event.set()
+    def _call(cls, enhancement):
+        from certbot.display.enhancements import ask
+        return ask(enhancement)
     
     
-def convert_yielded(yielded):
-    '''Convert a yielded object into a `.Future`.
-    
-        .. versionchanged:: 4.3
-       Added ``async with`` support in Python 3.5.
-    
-           ssl_ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-       ssl_ctx.load_cert_chain(os.path.join(data_dir, 'mydomain.crt'),
-                               os.path.join(data_dir, 'mydomain.key'))
-       TCPServer(ssl_options=ssl_ctx)
-    
-        def filter(self, record):
-        if record.exc_info:
-            self.logged_stack = True
-        message = record.getMessage()
-        if self.regex.match(message):
-            self.matched = True
-            return False
-        return True
-    
-    
-class FallbackHandler(RequestHandler):
-    '''A `RequestHandler` that wraps another HTTP server callback.
+@click.command()
+@click.argument('catalog_file', type=click.Path())
+def cli(catalog_file):
+    # Read the old ones back.  Once we are in, we will never go.
+    with open(catalog_file) as f:
+        rv = json.load(f)['supported_locales']
