0f5a31e1e6d7334490faae0654fce73f9eded439
SUNsung <weatherius@gmail.com>
2018-05-09 17:56:23 +0300

    Rewrite [03KQ.onY3gQCM03bN.RlgLpANg03.Gc1vQbBvAk03BwR.py] File in: 2018-05-09 17:56:22

--- pyton/03KQ.onY3gQCM03bN.RlgLpANg03.Gc1vQbBvAk03BwR.py
+++ pyton/03KQ.onY3gQCM03bN.RlgLpANg03.Gc1vQbBvAk03BwR.py
@@ -1,74 +1,177 @@
 
         
-                class SecretView(View):
-            methods = ['GET']
-            decorators = [superuser_required]
+        
+def auto_parallel(metagraph, model):
+  from tensorflow.python.grappler import tf_optimizer
+  rewriter_config = rewriter_config_pb2.RewriterConfig()
+  rewriter_config.optimizers.append('autoparallel')
+  rewriter_config.auto_parallel.enable = True
+  rewriter_config.auto_parallel.num_replicas = FLAGS.num_gpus
+  optimized_graph = tf_optimizer.OptimizeGraph(rewriter_config, metagraph)
+  metagraph.graph_def.CopyFrom(optimized_graph)
+  UpdateCollection(metagraph, model)
+
     
-                version = match.group(1).strip()
+        # Create a HashTable mapping label strings to integer ids.
+    table_initializer = tf.contrib.lookup.KeyValueTensorInitializer(
+        keys=list(input_config.label_map.keys()),
+        values=list(input_config.label_map.values()),
+        key_dtype=tf.string,
+        value_dtype=tf.int32)
+    label_to_id = tf.contrib.lookup.HashTable(
+        table_initializer, default_value=-1)
     
-                def g():
-                assert not flask.request
-                assert not flask.current_app
-                with reqctx:
-                    assert flask.request
-                    assert flask.current_app == app
-                    assert flask.request.path == '/'
-                    assert flask.request.args['foo'] == 'bar'
-                assert not flask.request
-                return 42
+    from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+    
+      This function assumes the directory structure of the Mikulski Archive for
+  Space Telescopes (http://archive.stsci.edu/pub/kepler/lightcurves).
+  Specifically, the filenames for a particular Kepler target star have the
+  following format:
+    
+      Args:
+    time: 1D numpy array of time values.
+    period: A positive real scalar; the period to fold over.
+    t0: The center of the resulting folded vector; this value is mapped to 0.
     
     
-def test_suppressed_exception_logging():
-    class SuppressedFlask(flask.Flask):
-        def log_exception(self, exc_info):
-            pass
+def compute_path_embeddings(model, session, instances):
+  '''Compute the path embeddings for all the distinct paths.
     
-        old_layer = keras.layers.LSTM(input_dim=5, input_length=3,
-                                  output_dim=2, name='d', consume_less='mem')
-    new_layer = keras.layers.LSTM(2, input_shape=[3, 5], name='d', implementation=1)
+    
+def ArrayToDatum(arr):
+  '''Converts numpy array to DatumProto.
+    
+        old_layer = keras.layers.MaxPooling2D((2, 2), 2, 'valid', name='maxpool2d')
+    new_layer = keras.layers.MaxPool2D(pool_size=2, strides=2, padding='valid', name='maxpool2d')
     assert json.dumps(old_layer.get_config()) == json.dumps(new_layer.get_config())
-    
-    - Models
-    About Keras models
-        explain when one should use Sequential or functional API
-        explain compilation step
-        explain weight saving, weight loading
-        explain serialization, deserialization
-    Sequential
-    Model (functional API)
     
     
 @keras_test
-def test_masking():
-    np.random.seed(1337)
-    x = np.array([[[1], [1]],
-                  [[0], [0]]])
-    model = Sequential()
-    model.add(Masking(mask_value=0, input_shape=(2, 1)))
-    model.add(TimeDistributed(Dense(1, kernel_initializer='one')))
-    model.compile(loss='mse', optimizer='sgd')
-    y = np.array([[[1], [1]],
-                  [[1], [1]]])
-    loss = model.train_on_batch(x, y)
-    assert loss == 0
+def test_locallyconnected_2d():
+    num_samples = 5
+    filters = 3
+    stack_size = 4
+    num_row = 6
+    num_col = 8
+    padding = 'valid'
     
-        def data_generator():
-        i = 0
-        max_batch_index = len(X_train) // batch_size
-        tot = 0
-        while 1:
-            if tot > 3 * len(X_train):
-                yield np.ones([batch_size, input_dim]) * np.nan, np.ones([batch_size, num_classes]) * np.nan
-            else:
-                yield (X_train[i * batch_size: (i + 1) * batch_size],
-                       y_train[i * batch_size: (i + 1) * batch_size])
-            i += 1
-            tot += 1
-            i = i % max_batch_index
+        # learn the alphabet with stacked LSTM
+    model = Sequential([
+        layers.LSTM(16, return_sequences=True, input_shape=(sequence_length, number_of_chars)),
+        layers.LSTM(16, return_sequences=False),
+        layers.Dense(number_of_chars, activation='softmax')
+    ])
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    model.fit(x, y, batch_size=1, epochs=60, verbose=1)
     
-    ##
-# imaginary tree navigation type; traverse 'get child' link
-DOWN = 2
-##
-#imaginary tree navigation type; finish with a child list
-UP = 3
+    
+if __name__ == '__main__':
+    pytest.main([__file__])
+
+    
+        for i in range(10, 30):
+        url = 'https://stream{i}.mixcloud.com/c/m4a/64{p}.m4a'.format(
+            i = i,
+            p = preview
+        )
+        try:
+            mime, ext, size = url_info(url)
+            break
+        except: continue
+    
+        def prepare(self, **kwargs):
+        content = get_content(self.url)
+    
+        >>> Point = namedtuple('Point', 'x y')
+    >>> Point.__doc__                   # docstring for the new class
+    'Point(x, y)'
+    >>> p = Point(11, y=22)             # instantiate with positional args or keywords
+    >>> p[0] + p[1]                     # indexable like a plain tuple
+    33
+    >>> x, y = p                        # unpack like a regular tuple
+    >>> x, y
+    (11, 22)
+    >>> p.x + p.y                       # fields also accessable by name
+    33
+    >>> d = p._asdict()                 # convert to a dictionary
+    >>> d['x']
+    11
+    >>> Point(**d)                      # convert from a dictionary
+    Point(x=11, y=22)
+    >>> p._replace(x=100)               # _replace() is like str.replace() but targets named fields
+    Point(x=100, y=22)
+    
+                self._pending_work_items[self._queue_count] = w
+            self._work_ids.put(self._queue_count)
+            self._queue_count += 1
+            # Wake up queue management thread
+            self._result_queue.put(None)
+    
+    def with_process_pool_executor():
+    with ProcessPoolExecutor(10) as executor:
+        return list(executor.map(is_prime, PRIMES))
+    
+        def test_done_callback_already_cancelled(self):
+        was_cancelled = [None]
+        def fn(callback_future):
+            was_cancelled[0] = callback_future.cancelled()
+    
+    
+class OmniCompletionRequest( CompletionRequest ):
+  def __init__( self, omni_completer, request_data ):
+    super( OmniCompletionRequest, self ).__init__( request_data )
+    self._omni_completer = omni_completer
+    
+    
+def EndsWithPython_Bad( path ):
+  ok_( not _EndsWithPython( path ),
+       'Path {0} does end with a Python name.'.format( path ) )
+    
+    
+def ParseArguments():
+  parser = argparse.ArgumentParser()
+  parser.add_argument( '--skip-build', action = 'store_true',
+                       help = 'Do not build ycmd before testing' )
+  parser.add_argument( '--coverage', action = 'store_true',
+                       help = 'Enable coverage report' )
+  parser.add_argument( '--no-flake8', action = 'store_true',
+                       help = 'Do not run flake8' )
+    
+    If the `~functools.singledispatch` library is available (standard in
+Python 3.4, available via the `singledispatch
+<https://pypi.python.org/pypi/singledispatch>`_ package on older
+versions), additional types of objects may be yielded. Tornado includes
+support for ``asyncio.Future`` and Twisted's ``Deferred`` class when
+``tornado.platform.asyncio`` and ``tornado.platform.twisted`` are imported.
+See the `convert_yielded` function to extend this mechanism.
+    
+        '''
+    def __init__(self, async_client_class=None, **kwargs):
+        # Initialize self._closed at the beginning of the constructor
+        # so that an exception raised here doesn't lead to confusing
+        # failures in __del__.
+        self._closed = True
+        self._io_loop = IOLoop(make_current=False)
+        if async_client_class is None:
+            async_client_class = AsyncHTTPClient
+        # Create the client while our IOLoop is 'current', without
+        # clobbering the thread's real current IOLoop (if any).
+        self._async_client = self._io_loop.run_sync(
+            gen.coroutine(lambda: async_client_class(**kwargs)))
+        self._closed = False
+    
+        .. testoutput::
+    
+    from tornado.concurrent import (Future, return_future, ReturnValueIgnoredError,
+                                run_on_executor, future_set_result_unless_cancelled)
+from tornado.escape import utf8, to_unicode
+from tornado import gen
+from tornado.ioloop import IOLoop
+from tornado.iostream import IOStream
+from tornado.log import app_log
+from tornado import stack_context
+from tornado.tcpserver import TCPServer
+from tornado.testing import AsyncTestCase, ExpectLog, bind_unused_port, gen_test
+from tornado.test.util import unittest, skipBefore35, exec_test, ignore_deprecation
