e705cc3e6d1ec1ad30666f11ec0a2abe47ff2e20
SUNsung <weatherius@gmail.com>
2018-05-10 04:01:03 +0300

    Rewrite [22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb] File in: 2018-05-10 04:01:02

--- ruby/22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb
+++ ruby/22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb
@@ -1,69 +1,26 @@
 
         
-        # Create an array of vendored mimetype => [extensions]
-mimes = {}
-json = open('https://raw.githubusercontent.com/jshttp/mime-db/master/db.json').read
-data = JSON.parse(json)
-data.reject! { |mime, meta| meta['extensions'].nil? || meta['extensions'].empty? }
-data.each do |mime, meta|
-  # Normalize extensions and mime-types
-  mime = mime.downcase.strip
-  extensions = meta['extensions'].map { |e| e.downcase.strip }.compact
+            #@posts[:new_public] = Post.where(:type => ['StatusMessage','ActivityStreams::Photo'],
+    #                                 :public => true).order('created_at DESC').limit(15).all
     
-          new_theme_name = args.join('_')
-      theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
-      if theme.path.exist?
-        Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.'
+          def create
+        req = Rack::Request.new(request.env)
+        if req['client_assertion_type'] == 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
+          handle_jwt_bearer(req)
+        end
+        self.status, headers, self.response_body = Api::OpenidConnect::TokenEndpoint.new.call(request.env)
+        headers.each {|name, value| response.headers[name] = value }
+        nil
       end
     
-        ENV.activate_extensions!
-    
-            next unless path.file?
-        file = path
-    
-      def llvm
-    @llvm ||= MacOS.llvm_build_version if MacOS.has_apple_developer_tools?
-  end
-    
-        print_remaining_files remaining_root_files, root, other
-  end
-    
-          export JAVA_HOME='$(/usr/libexec/java_home)'
-      export AWS_ACCESS_KEY='<Your AWS Access ID>'
-      export AWS_SECRET_KEY='<Your AWS Secret Key>'
-      export #{home_name}='#{home_value}'
-    EOS
+        it 'Returns nil when Referer header is invalid' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/bad|uri'}
+      expect(subject.referrer(env)).to be_nil
+    end
   end
 end
 
     
-            commands = []
-        commands << Fastlane::Actions.sh('security create-keychain -p #{escaped_password} #{keychain_path}', log: false)
-    
-    require_relative '../mastodon/snowflake'
-    
-        grouped_codes = codes.reduce([]) do |agg, current|
-      if current[1]
-        agg << [current[0]]
-      else
-        agg.last << current[0]
-        agg
-      end
-    end
-    
-    class DigestMailerWorker
-  include Sidekiq::Worker
-    
-      def initial_state_params
-    text = [params[:title], params[:text], params[:url]].compact.join(' ')
-    {
-      settings: Web::Setting.find_by(user: current_user)&.data || {},
-      push_subscription: current_account.user.web_push_subscription(current_session),
-      current_account: current_account,
-      token: current_session.token,
-      admin: Account.find_local(Setting.site_contact_username),
-      text: text,
-    }
+      it 'should set the X-Frame-Options' do
+    expect(get('/', {}, 'wants' => 'text/html').headers['X-Frame-Options']).to eq('SAMEORIGIN')
   end
-    
-    puts 'DONE: #{blogs.count} written to #{OUTPUT_FILENAME}'
