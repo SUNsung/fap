63d7a10e80be8495b92bd06fe521ed69f94b2d9e
SUNsung <weatherius@gmail.com>
2018-05-10 16:19:56 +0300

    Rewrite [56heS9Jw02W6E56ixy32mK.Og56LtcLlE88i.56T0x.cpp] File in: 2018-05-10 16:19:56

--- cccccc/56heS9Jw02W6E56ixy32mK.Og56LtcLlE88i.56T0x.cpp
+++ cccccc/56heS9Jw02W6E56ixy32mK.Og56LtcLlE88i.56T0x.cpp
@@ -1,215 +1,217 @@
 
         
-        
-    {}
-    
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
-    
-    
-    {    _data.Write((const char*)_rawbuf + st.head_length + st.url_length, st.total_length - (st.head_length + st.url_length));
-    return LONGLINKPACK_OK;
-}
-    
-    
-    {    void throw_exception( std::exception const & e ) {
-        xfatal2(TSF'boost exception:%_', e.what());
-        
-#ifdef ANDROID
-        char stack[4096] = {0};
-        android_callstack(stack, sizeof(stack));
-        xfatal2(TSF'%_', stack);
-#endif
+        void dls::build_coeff_matrix(const cv::Mat& pp, cv::Mat& Mtilde, cv::Mat& D)
+{
+    cv::Mat eye = cv::Mat::eye(3, 3, CV_64F);
     }
+    
+    class CV_ModelEstimator2_Test : public cvtest::ArrayTest
+{
+public:
+    CV_ModelEstimator2_Test();
+    }
+    
+            if (lane < 16)
+        {
+            T partial = ptr[tid];
+    }
+    
+    CV_EXPORTS_W void divide(InputArray src1, Scalar src2, OutputArray dst, double scale=1, int dtype=-1);
+    
+    #include 'runtime_common.hpp'
+    
+    // dims=[5, 4, 3, 2]->[4, 3, 5, 2]
+TEST_F(MatrixTest, RotatingTranspose_0_2) {
+  GENERIC_2D_ARRAY<int> m;
+  src_.RotatingTranspose(dims_, kNumDims_, 0, 2, &m);
+  m.ResizeNoInit(kInputSize_ / 2, 2);
+  // Verify that the result is:
+  // output tensor=[[[[0, 1][24, 25][48, 49][72, 73][96, 97]]
+  //                 [[2, 3][26, 27][50, 51][74, 75][98, 99]]
+  //                 [[4, 5][28, 29][52, 53][76, 77][100, 101]]]
+  //                [[[6, 7]...
+  EXPECT_EQ(0, m(0, 0));
+  EXPECT_EQ(1, m(0, 1));
+  EXPECT_EQ(24, m(1, 0));
+  EXPECT_EQ(25, m(1, 1));
+  EXPECT_EQ(96, m(4, 0));
+  EXPECT_EQ(97, m(4, 1));
+  EXPECT_EQ(2, m(5, 0));
+  EXPECT_EQ(6, m(15, 0));
 }
+    
+    extern BLOCK_LIST *current_block_list;
+extern STRING_VAR_H (editor_image_win_name, 'EditorImage',
+'Editor image window name');
+extern INT_VAR_H (editor_image_xpos, 590, 'Editor image X Pos');
+extern INT_VAR_H (editor_image_ypos, 10, 'Editor image Y Pos');
+extern INT_VAR_H (editor_image_height, 680, 'Editor image height');
+extern INT_VAR_H (editor_image_width, 655, 'Editor image width');
+extern INT_VAR_H (editor_image_word_bb_color, BLUE,
+'Word bounding box colour');
+extern INT_VAR_H (editor_image_blob_bb_color, YELLOW,
+'Blob bounding box colour');
+extern INT_VAR_H (editor_image_text_color, WHITE, 'Correct text colour');
+extern STRING_VAR_H (editor_dbwin_name, 'EditorDBWin',
+'Editor debug window name');
+extern INT_VAR_H (editor_dbwin_xpos, 50, 'Editor debug window X Pos');
+extern INT_VAR_H (editor_dbwin_ypos, 500, 'Editor debug window Y Pos');
+extern INT_VAR_H (editor_dbwin_height, 24, 'Editor debug window height');
+extern INT_VAR_H (editor_dbwin_width, 80, 'Editor debug window width');
+extern STRING_VAR_H (editor_word_name, 'BlnWords',
+'BL normalised word window');
+extern INT_VAR_H (editor_word_xpos, 60, 'Word window X Pos');
+extern INT_VAR_H (editor_word_ypos, 510, 'Word window Y Pos');
+extern INT_VAR_H (editor_word_height, 240, 'Word window height');
+extern INT_VAR_H (editor_word_width, 655, 'Word window width');
+extern double_VAR_H (editor_smd_scale_factor, 1.0, 'Scaling for smd image');
+    
+    namespace tesseract {
+class CCStruct : public CUtil {
+ public:
+  CCStruct();
+  ~CCStruct();
+    }
+    }
+    
+    // Class to hold a Pixa collection of debug images with captions and save them
+// to a PDF file.
+class DebugPixa {
+ public:
+  // TODO(rays) add another constructor with size control.
+  DebugPixa() {
+    pixa_ = pixaCreate(0);
+    fonts_ = bmfCreate(nullptr, 14);
+  }
+  // If the filename_ has been set and there are any debug images, they are
+  // written to the set filename_.
+  ~DebugPixa() {
+    pixaDestroy(&pixa_);
+    bmfDestroy(&fonts_);
+  }
+    }
+    
+    #endif  // TESSERACT_CCMAIN_OTSUTHR_H_
 
     
-    #include 'comm/debugger/test_spy_sample.h'
-#include 'comm/xlogger/xlogger.h'
     
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
+    {}  // namespace tesseract
     
-    jvalue JNU_CallMethodByMethodInfo(JNIEnv* _env, jobject _obj, JniMethodInfo _method_info, ...);
+    /* Location of code at error codes Reserve 0..2 (status codes 0..23 for UNLV)*/
+#define LOC_UNUSED0        0
+#define LOC_UNUSED1        1
+#define LOC_UNUSED2        2
+#define LOC_INIT      3
+#define LOC_EDGE_PROG   4
+#define LOC_TEXT_ORD_ROWS 5
+#define LOC_TEXT_ORD_WORDS  6
+#define LOC_PASS1     7
+#define LOC_PASS2     8
+/* Reserve up to 8..13 for adding subloc 0/3 plus subsubloc 0/1/2 */
+#define LOC_FUZZY_SPACE   14
+/* Reserve up to 14..20 for adding subloc 0/3 plus subsubloc 0/1/2 */
+#define LOC_MM_ADAPT    21
+#define LOC_DOC_BLK_REJ   22
+#define LOC_WRITE_RESULTS 23
+#define LOC_ADAPTIVE    24
+/* DON'T DEFINE ANY LOCATION > 31 !!! */
     
-      // There must be a goodMallocSize between 100 and 1024...
-  EXPECT_LT(1u, queue.front()->countChainElements());
-  const IOBuf* buf = queue.front();
-  do {
-    EXPECT_LE(100u, buf->capacity());
-    buf = buf->next();
-  } while (buf != queue.front());
-    
-    template <typename C>
-struct compare_less : detail::cmp_pred<C, ordering::lt, 0> {
-  using detail::cmp_pred<C, ordering::lt, 0>::cmp_pred;
-};
-    
-    TEST(Futex, basic_live) {
-  run_basic_tests<std::atomic>();
-  run_wait_until_tests<std::atomic>();
-}
-    
-    
-    {
-    {} // namespace detail
-} // namespace folly
-
+      // Return true if the given unichar representation is already present in the
+  // UNICHARMAP. The first length characters (maximum) from unichar_repr are
+  // used. The length MUST be non-zero.
+  bool contains(const char* const unichar_repr, int length) const;
     
     /**
- * DynamicParser provides a tiny DSL for easily, correctly, and losslessly
- * parsing a folly::dynamic into any other representation.
+ * @brief A factory for associating event generators to EventPublisherID%s.
  *
- * To make this concrete, this lets you take a JSON config that potentially
- * contains user errors, and parse __all__ of its valid parts, while
- * automatically and __reversibly__ recording any parts that cause errors:
+ * This factory both registers new event types and the subscriptions that use
+ * them. An EventPublisher is also a factory, the single event factory
+ * arbitrates Subscription creation and management for each associated
+ * EventPublisher.
  *
- *   {'my values': {
- *     'an int': 'THIS WILL BE RECORDED AS AN ERROR, BUT WE'LL PARSE THE REST',
- *     'a double': 3.1415,
- *     'keys & values': {
- *       'the sky is blue': true,
- *       'THIS WILL ALSO BE RECORDED AS AN ERROR': 'cheese',
- *       '2+2=5': false,
- *     }
- *   }}
- *
- * To parse this JSON, you need no exception handling, it is as easy as:
- *
- *   folly::dynamic d = ...;  // Input
- *   int64_t integer;  // Three outputs
- *   double real;
- *   std::map<std::string, bool> enabled_widgets;
- *   DynamicParser p(DynamicParser::OnError::RECORD, &d);
- *   p.required('my values', [&]() {
- *     p.optional('an int', [&](int64_t v) { integer = v; });
- *     p.required('a double', [&](double v) { real = v; });
- *     p.optional('keys & values', [&]() {
- *       p.objectItems([&](std::string widget, bool enabled) {
- *         enabled_widgets.emplace(widget, enabled);
- *       });
- *     });
- *   });
- *
- * Your code in the lambdas can throw, and this will be reported just like
- * missing key and type conversion errors, with precise context on what part
- * of the folly::dynamic caused the error.  No need to throw:
- *   std::runtime_error('Value X at key Y caused a flux capacitor overload')
- * This will do:
- *   std::runtime_error('Flux capacitor overload')
- *
- * == Keys and values are auto-converted to match your callback ==
- *
- * DynamicParser's optional(), required(), objectItems(), and
- * arrayItems() automatically convert the current key and value to match the
- * signature of the provided callback.  parser.key() and parser.value() can
- * be used to access the same data without conversion.
- *
- * The following types are supported -- you should generally take arguments
- * by-value, or by-const-reference for dynamics & strings you do not copy.
- *
- *   Key: folly::dynamic (no conversion), std::string, int64_t
- *   Value: folly::dynamic (no conversion), int64_t, bool, double, std::string
- *
- * There are 21 supported callback signatures, of three kinds:
- *
- *   1: No arguments -- useful if you will just call more parser methods.
- *
- *   5: The value alone -- the common case for optional() and required().
- *        [&](whatever_t value) {}
- *
- *   15: Both the key and the value converted according to the rules above:
- *        [&](whatever_t key, whatever_t) {}
- *
- * NB: The key alone should be rarely needed, but these callback styles
- *     provide it with no conversion overhead, and only minimal verbosity:
- *       [&](const std::string& k, const folly::dynamic&) {}
- *       [&]() { auto k = p.key().asString(); }
- *
- * == How `releaseErrors()` can make your parse lossless ==
- *
- * If you write parsing code by hand, you usually end up with error-handling
- * resembling that of OnError::THROW -- the first error you hit aborts the
- * whole parse, and you report it.
- *
- * OnError::RECORD offers a more user-friendly alternative for 'parse,
- * serialize, re-parse' pipelines, akin to what web-forms do.  All
- * exception-causing parts are losslessly recorded in a parallel
- * folly::dynamic, available via releaseErrors() at the end of the parse.
- *
- * Suppose we fail to look up 'key1' at the root, and hit a value error in
- * 'key2': {'subkey2': ...}.  The error report will have the form:
- *
- *   {'nested': {
- *     'key_errors': {'key1': 'explanatory message'},
- *     'value': <whole input>,
- *     'nested': { 'key2': { 'nested': {
- *       'subkey2': {'value': <original value>, 'error': 'message'}
- *     } } }
- *   }}
- *
- * Errors in array items are handled just the same, but using integer keys.
- *
- * The advantage of this approach is that your parsing can throw wherever,
- * and DynamicParser isolates it, allowing the good parts to parse.
- *
- * Put another way, this makes it easy to implement a transformation that
- * splits a `folly::dynamic` into a 'parsed' part (which might be your
- * struct meant for runtime use), and a matching 'errors' part.  As long as
- * your successful parses are lossless, you can always reconstruct the
- * original input from the parse output and the recorded 'errors'.
- *
- * == Limitations ==
- *
- *  - The input dynamic should be an object or array. wrapError() could be
- *    exposed to allow parsing single scalars, but this would not be a
- *    significant usability improvement over try-catch.
- *
- *  - Do NOT try to parse the same part of the input dynamic twice. You
- *    might report multiple value errors, which is currently unsupported.
- *
- *  - optional() does not support defaulting. This is unavoidable, since
- *    DynamicParser does not dictate how you record parsed data.  If your
- *    parse writes into an output struct, then it ought to be initialized at
- *    construction time.  If your output is initialized to default values,
- *    then you need no 'default' feature.  If it is not initialized, you are
- *    in trouble anyway.  Suppose your optional() parse hits an error.  What
- *    does your output contain?
- *      - Uninitialized data :(
- *      - You rely on an optional() feature to fall back to parsing some
- *        default dynamic.  Sadly, the default hits a parse error.  Now what?
- *    Since there is no good way to default, DynamicParser leaves it out.
- *
- * == Future: un-parsed items ==
- *
- * DynamicParser could support erroring on un-parsed items -- the parts of
- * the folly::dynamic, which were never asked for.  Here is an ok design:
- *
- * (i) At the start of parsing any value, the user may call:
- *   parser.recursivelyForbidUnparsed();
- *   parser.recursivelyAllowUnparsed();
- *   parser.locallyForbidUnparsed();
- *   parser.locallyAllowUnparsed();
- *
- * (ii) At the end of the parse, any unparsed items are dumped to 'errors'.
- * For example, failing to parse index 1 out of ['v1', 'v2', 'v3'] yields:
- *   'nested': {1: {'unparsed': 'v2'}}
- * or perhaps more verbosely:
- *   'nested': {1: {'error': 'unparsed value', 'value': 'v2'}}
- *
- * By default, unparsed items are allowed. Calling a 'forbid' function after
- * some keys have already been parsed is allowed to fail (this permits a
- * lazy implementation, which has minimal overhead when 'forbid' is not
- * requested).
- *
- * == Future: multiple value errors ==
- *
- * The present contract is that exactly one value error is reported per
- * location in the input (multiple key lookup errors are, of course,
- * supported).  If the need arises, multiple value errors could easily be
- * supported by replacing the 'error' string with an 'errors' array.
+ * Since event types may be plugins, they are created using the factory.
+ * Since subscriptions may be configured/disabled they are also factory-managed.
  */
+class EventFactory : private boost::noncopyable {
+ public:
+  /// Access to the EventFactory instance.
+  static EventFactory& getInstance();
+    }
+    
+      /**
+   * @brief Optionally handle each published event via the logger.
+   *
+   * It is possible to skip the database representation of event subscribers
+   * and instead forward each added event to the active logger plugin.
+   */
+  virtual Status logEvent(const std::string& /*s*/) {
+    return Status(1, 'Not enabled');
+  }
+    
+    
+    {// Remove registry-helper macros from the SDK.
+#undef REGISTER
+#define REGISTER 'Do not REGISTER in the osquery SDK'
+#undef REGISTER_INTERNAL
+#define REGISTER_INTERNAL 'Do not REGISTER_INTERNAL in the osquery SDK'
+#undef CREATE_REGISTRY
+#define CREATE_REGISTRY 'Do not CREATE_REGISTRY in the osquery SDK'
+#undef CREATE_LAZY_REGISTRY
+#define CREATE_LAZY_REGISTRY 'Do not CREATE_LAZY_REGISTRY in the osquery SDK'
+}
+
+    
+      // This buffer will be shared, create a descriptor.
+  osquery.md =
+      IOMemoryDescriptor::withAddressRange((mach_vm_address_t)osquery.buffer,
+                                           osquery.buf_size,
+                                           kIODirectionInOut,
+                                           kernel_task);
+  if (osquery.md == NULL) {
+    err = -EINVAL;
+    goto error_exit;
+  }
+    
+    class TLSConfigPlugin;
+    
+    
+    {  // Cleanup allocations.
+  std::string result(buffer);
+  free(buffer);
+  return result;
+}
+    
+      EXPECT_EQ(FLAGS_test_int64_alias, (int64_t)1 << 34);
+  FLAGS_test_int64_alias = (int64_t)1 << 35;
+  EXPECT_EQ(FLAGS_test_int64, (int64_t)1 << 35);
+  FLAGS_test_int64 = (int64_t)1 << 36;
+  EXPECT_EQ(FLAGS_test_int64_alias, (int64_t)1 << 36);
+  EXPECT_TRUE(FLAGS_test_int64_alias > 0);
+    
+      // Test operator lower bounds.
+  EXPECT_FALSE(cl2.matches(0));
+  EXPECT_FALSE(cl2.matches(1));
+    
+    class TimeTests : public testing::Test {};
+    
+    #include <string>
+#include <set>
+#include <map>
+    
+    
+    {        for (it = m_strmap.begin(); it != m_strmap.end(); ++it) {
+            if (it->second->This() == _this) {
+                m_strmap.erase(it);
+                break;
+            }
+        }
+    }
+    
+    
+    static bool SpyHookLogFunc(struct XLoggerInfo_t& _info, std::string& _log);
+    void TestFun0();
+    int __TestFun1(int i);
+    
+    jbyteArray JNU_Buffer2JbyteArray(JNIEnv* _env, const AutoBuffer& ab);
+jbyteArray JNU_Buffer2JbyteArray(JNIEnv* _env, const void* _buffer, size_t _length);
+void JNU_FreeJbyteArray(JNIEnv* _env, jbyteArray bytes);
