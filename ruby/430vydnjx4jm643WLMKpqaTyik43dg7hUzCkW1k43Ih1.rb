ab3bcc4e7f9239d9eadb733aa9494ab9aeedf81e
SUNsung <weatherius@gmail.com>
2018-05-10 19:40:36 +0300

    Rewrite [430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb] File in: 2018-05-10 19:40:36

--- ruby/430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb
+++ ruby/430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb
@@ -1,15 +1,71 @@
 
         
-              alias_method :_view, :view
+        def check_link(uri)
+  HTTParty.head(uri, :verify => false).code.to_i.tap do |status|
+    if (400..422).include?(status)
+      if status != 403 && !uri.exclude?('udemy.com')
+        raise 'Request had status #{status}'
+      else
+        putc('S')
+      end
+    end
+  end
+end
     
-      def redirect_with_protocol
-    redirect_to action: 'hello_world', protocol: 'https'
+              saved = if pending_reconfirmation?
+            skip_reconfirmation!
+            self.email = unconfirmed_email
+            self.unconfirmed_email = nil
+    
+      def failure_message
+    exception = request.respond_to?(:get_header) ? request.get_header('omniauth.error') : request.env['omniauth.error']
+    error   = exception.error_reason if exception.respond_to?(:error_reason)
+    error ||= exception.error        if exception.respond_to?(:error)
+    error ||= (request.respond_to?(:get_header) ? request.get_header('omniauth.error.type') : request.env['omniauth.error.type']).to_s
+    error.to_s.humanize if error
   end
     
-          class_attribute :delivery_methods, default: {}.freeze
-      class_attribute :delivery_method, default: :smtp
+    module Devise
+  module Controllers
+    # A module that may be optionally included in a controller in order
+    # to provide remember me behavior. Useful when signing in is done
+    # through a callback, like in OmniAuth.
+    module Rememberable
+      # Return default cookie values retrieved from session options.
+      def self.cookie_values
+        Rails.configuration.session_options.slice(:path, :domain, :secure)
+      end
     
-      test 'allow to add custom delivery method' do
-    ActionMailer::Base.delivery_method = :custom
-    assert_equal :custom, ActionMailer::Base.delivery_method
-  end
+      if ARGV.include? '--no-ansi'
+    STDERR.puts <<-DOC
+    WARNING: CocoaPods requires your terminal to be using UTF-8 encoding.
+    Consider adding the following to ~/.profile:
+    
+            self.description = <<-DESC
+          Shows the content of the pods cache as a YAML tree output, organized by pod.
+          If `NAME` is given, only the caches for that pod will be included in the output.
+        DESC
+    
+          def initialize(argv)
+        super
+        config.silent = false
+      end
+    
+          def validate!
+        super
+        raise Informative, 'Existing Podfile found in directory' unless config.podfile_path_in_dir(Pathname.pwd).nil?
+        if @project_path
+          help! 'Xcode project at #{@project_path} does not exist' unless File.exist? @project_path
+          project_path = @project_path
+        else
+          raise Informative, 'No Xcode project found, please specify one' unless @project_paths.length > 0
+          raise Informative, 'Multiple Xcode projects found, please specify one' unless @project_paths.length == 1
+          project_path = @project_paths.first
+        end
+        @xcode_project = Xcodeproj::Project.open(project_path)
+      end
+    
+            def signal_end_of_output
+          output_pipe.puts(END_OF_OUTPUT_SIGNAL)
+          STDOUT.flush
+        end
