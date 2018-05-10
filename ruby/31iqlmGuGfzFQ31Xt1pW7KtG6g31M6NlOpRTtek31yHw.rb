bfc45e4316d5180eb2be5ee6716959bf43351586
SUNsung <weatherius@gmail.com>
2018-05-10 21:23:28 +0300

    Rewrite [31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb] File in: 2018-05-10 21:23:27

--- ruby/31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb
+++ ruby/31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb
@@ -1,57 +1,105 @@
 
         
-          # GET /books/1
-  # GET /books/1.json
-  def show
+                adapter = adapter.camelize
+        adapter = 'PostgreSQL' if adapter == 'Postgresql'
+        'ActionCable::SubscriptionAdapter::#{adapter}'.constantize
+      end
+    end
+  end
+end
+
+    
+            def test_url_host_no_db
+          spec = resolve 'abstract://foo?encoding=utf8'
+          assert_equal({
+            'adapter'  => 'abstract',
+            'host'     => 'foo',
+            'encoding' => 'utf8' }, spec)
+        end
+    
+        response = ActionDispatch::TestResponse.create(200, { 'Content-Type' => 'application/json' }, '{ 'foo': 'fighters' }')
+    assert_equal({ 'foo' => 'fighters' }, response.parsed_body)
   end
     
-            MSG = 'Use only a single space inside array percent literal.'.freeze
-        MULTIPLE_SPACES_BETWEEN_ITEMS_REGEX =
-          /(?:[\S&&[^\\]](?:\\ )*)( {2,})(?=\S)/
+    module MiddlewareTest
+  class MyMiddleware
+    def initialize(app)
+      @app = app
+    end
     
-    module RuboCop
-  module Cop
-    module Performance
-      # This cop identifies the use of `Regexp#match` or `String#match`, which
-      # returns `#<MatchData>`/`nil`. The return value of `=~` is an integral
-      # index/`nil` and is more performant.
-      #
-      # @example
-      #   # bad
-      #   do_something if str.match(/regex/)
-      #   while regex.match('str')
-      #     do_something
-      #   end
-      #
-      #   # good
-      #   method(str =~ /regex/)
-      #   return value unless regex =~ 'str'
-      class RedundantMatch < Cop
-        MSG = 'Use `=~` in places where the `MatchData` returned by ' \
-              '`#match` will not be used.'.freeze
+    module ActionMailer
+  # This module handles everything related to mail delivery, from registering
+  # new delivery methods to configuring the mail object to be sent.
+  module DeliveryMethods
+    extend ActiveSupport::Concern
     
-            def on_if(node)
-          return unless nested_variable_comparison?(node.condition)
-          add_offense(node)
-        end
+        include Behavior
+  end
+end
+
     
-                yield arguments[optarg_position]
-          end
-        end
+        assert_equal(2, @logger.logged(:debug).size)
+    assert_match(/BaseMailer#welcome: processed outbound mail in [\d.]+ms/, @logger.logged(:debug).first)
+    assert_match(/Welcome/, @logger.logged(:debug).second)
+  ensure
+    BaseMailer.deliveries.clear
+  end
     
-            private
+    class MailerHelperTest < ActionMailer::TestCase
+  def test_use_mail_helper
+    mail = HelperMailer.use_mail_helper
+    assert_match %r{  But soft!}, mail.body.encoded
+    assert_match %r{east, and\r\n  Juliet}, mail.body.encoded
+  end
     
-            # @param lines [Array<String>]
-        # @param annotations [Array<(Integer, String)>]
-        #   each entry is the annotated line number and the annotation text
-        #
-        # @note annotations are sorted so that reconstructing the annotation
-        #   text via {#to_s} is deterministic
-        def initialize(lines, annotations)
-          @lines       = lines.freeze
-          @annotations = annotations.sort.freeze
-        end
+        def execute(args)
+      topic_id = args[:topic_id]
+      raise Discourse::InvalidParameters.new(:topic_id) unless topic_id.present?
     
-                end
-          RUBY
-        end
+    map = {}
+dups = []
+    
+    # puts '\nDone.'
+
+    
+          def initialize(*args, &block)
+        @bypass_confirmation_postpone = false
+        @skip_reconfirmation_in_callback = false
+        @reconfirmation_required = false
+        @skip_confirmation_notification = false
+        @raw_confirmation_token = nil
+        super
+      end
+    
+    class Devise::UnlocksController < DeviseController
+  prepend_before_action :require_no_authentication
+    
+          def remember_cookie_values(resource)
+        options = { httponly: true }
+        options.merge!(forget_cookie_values(resource))
+        options.merge!(
+          value: resource.class.serialize_into_cookie(resource),
+          expires: resource.remember_expires_at
+        )
+      end
+    
+            routes.each do |module_name, actions|
+          [:path, :url].each do |path_or_url|
+            actions.each do |action|
+              action = action ? '#{action}_' : ''
+              method = :'#{action}#{module_name}_#{path_or_url}'
+    
+        def push(*names)
+      @filters.push *filter_const(names)
+    end
+    
+    module Docs
+  class Entry
+    class Invalid < StandardError; end
+    
+              # Method
+          name = node.at_css('.header').content.split.first
+    
+    ###
+### methods
+###
