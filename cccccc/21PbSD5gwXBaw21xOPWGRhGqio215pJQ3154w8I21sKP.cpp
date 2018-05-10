fb9281f06108d9bedbe714e3d8c9292c0371707a
SUNsung <weatherius@gmail.com>
2018-05-10 10:16:57 +0300

    Rewrite [21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp] File in: 2018-05-10 10:16:56

--- cccccc/21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp
+++ cccccc/21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp
@@ -1,95 +1,211 @@
 
         
-        IPC_SYNC_MESSAGE_ROUTED4_1(ShellViewHostMsg_Call_Object_Method_Sync,
-                           int /* object id */,
-                           std::string /* type name */,
-                           std::string /* method name */,
-                           base::ListValue /* arguments */,
-                           base::ListValue /* result */)
-    
-      // Post 'reopen' event.
-  // (This event is received when the user clicked the icon in the Dock).
-  static void EmitReopenEvent();
-    
-    
-void Base::Call(const std::string& method, const base::ListValue& arguments,
-                content::RenderFrameHost* rvh) {
-  NOTREACHED() << 'Uncatched call in Base'
+        void Base::CallSync(const std::string& method,
+                    const base::ListValue& arguments,
+                    base::ListValue* result) {
+  NOTREACHED() << 'Uncatched callAsync in Base'
                << ' method:' << method
                << ' arguments:' << arguments;
 }
     
-    Clipboard::~Clipboard() {
-}
-    
-    public:
-  EventListener(int id,
-                const base::WeakPtr<DispatcherHost>& dispatcher_host,
-                const base::DictionaryValue& option);
-    
-    bool MenuDelegate::IsItemForCommandIdDynamic(int command_id) const {
-  if (command_id < 0)
+    bool MenuDelegate::GetIconForCommandId(int command_id,
+                                       gfx::Image* icon) const {
+  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
+  if (!item)
+    return false;
+  if (item->icon_.IsEmpty())
     return false;
     }
     
-    bool NwObjCreateFunction::RunNWSync(base::ListValue* response, std::string* error) {
-  base::DictionaryValue* options = nullptr;
-  int id = 0;
-  std::string type;
-  EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(0, &id));
-  EXTENSION_FUNCTION_VALIDATE(args_->GetString(1, &type));
-  EXTENSION_FUNCTION_VALIDATE(args_->GetDictionary(2, &options));
+    typedef std::map<std::string,std::string> KeyMap;
+    
+    class NwAppCloseAllWindowsFunction : public AsyncExtensionFunction {
+ public:
+  NwAppCloseAllWindowsFunction() {}
     }
     
-    /// The following are confusable internal word punctuation symbols
-/// which we normalize to the first variant when matching in dawgs.
-extern const char *kHyphenLikeUTF8[];
-extern const char *kApostropheLikeUTF8[];
+    class NwMenuGetNSStringWithFixupFunction : public NWSyncExtensionFunction {
+ public:
+  NwMenuGetNSStringWithFixupFunction(){}
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
     
-    	PoolByteArray out;
-	String certs_path = GLOBAL_DEF('network/ssl/certificates', '');
-	ProjectSettings::get_singleton()->set_custom_property_info('network/ssl/certificates', PropertyInfo(Variant::STRING, 'network/ssl/certificates', PROPERTY_HINT_FILE, '*.crt'));
+ protected:
+  ~NwMenuGetNSStringWithFixupFunction() override {}
     
-    		case LWS_CALLBACK_CLIENT_RECEIVE:
-			peer->read_wsi(in, len);
-			if (peer->get_available_packet_count() > 0)
-				_on_peer_packet();
-			break;
+  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringWithFixup', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringWithFixupFunction);
+};
     
-    	virtual Vector<StackInfo> debug_get_current_stack_info() { return Vector<StackInfo>(); }
-    
-    	struct btVehicleWheelContactPoint {
-		PhysicsDirectBodyState *m_s;
-		PhysicsBody *m_body1;
-		Vector3 m_frictionPositionWorld;
-		Vector3 m_frictionDirectionWorld;
-		real_t m_jacDiagABInv;
-		real_t m_maxImpulse;
+        TableBuilder* builder = new TableBuilder(options, file);
+    meta->smallest.DecodeFrom(iter->key());
+    for (; iter->Valid(); iter->Next()) {
+      Slice key = iter->key();
+      meta->largest.DecodeFrom(key);
+      builder->Add(key, iter->value());
     }
     
-    	List<VisualScript::DataConnection> data_conns;
-	script->get_data_connection_list(edited_func, &data_conns);
-    
-    	Error result = OS::get_singleton()->get_dynamic_library_symbol_handle(
-			native_handle,
-			p_procedure_name,
-			r_handle,
-			p_optional);
-    
-    namespace xgboost {
-ConsoleLogger::~ConsoleLogger() {
-  dmlc::CustomLogMessage::Log(log_stream_.str());
+    void leveldb_writeoptions_set_sync(
+    leveldb_writeoptions_t* opt, unsigned char v) {
+  opt->rep.sync = v;
 }
-TrackerLogger::~TrackerLogger() {
-  dmlc::CustomLogMessage::Log(log_stream_.str());
-}
-}  // namespace xgboost
     
-      delete metric;
-  metric = xgboost::Metric::Create('ndcg@2');
-  ASSERT_STREQ(metric->Name(), 'ndcg@2');
-  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 1, 1e-10);
-  EXPECT_NEAR(GetMetricEval(metric,
-                            {0.1f, 0.9f, 0.1f, 0.9f},
-                            {  0,   0,   1,   1}),
-              0.3868f, 0.001f);
+    static std::string IKey(const std::string& user_key,
+                        uint64_t seq,
+                        ValueType vt) {
+  std::string encoded;
+  AppendInternalKey(&encoded, ParsedInternalKey(user_key, seq, vt));
+  return encoded;
+}
+    
+    // Return the name of a temporary file owned by the db named 'dbname'.
+// The result will be prefixed with 'dbname'.
+extern std::string TempFileName(const std::string& dbname, uint64_t number);
+    
+      fname = TableFileName('bar', 200);
+  ASSERT_EQ('bar/', std::string(fname.data(), 4));
+  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
+  ASSERT_EQ(200, number);
+  ASSERT_EQ(kTableFile, type);
+    
+      for (size_t i = 0; i < compact_pointers_.size(); i++) {
+    PutVarint32(dst, kCompactPointer);
+    PutVarint32(dst, compact_pointers_[i].first);  // level
+    PutLengthPrefixedSlice(dst, compact_pointers_[i].second.Encode());
+  }
+    
+    void WriteBatch::Put(const Slice& key, const Slice& value) {
+  WriteBatchInternal::SetCount(this, WriteBatchInternal::Count(this) + 1);
+  rep_.push_back(static_cast<char>(kTypeValue));
+  PutLengthPrefixedSlice(&rep_, key);
+  PutLengthPrefixedSlice(&rep_, value);
+}
+    
+        const char* benchmarks = FLAGS_benchmarks;
+    while (benchmarks != NULL) {
+      const char* sep = strchr(benchmarks, ',');
+      Slice name;
+      if (sep == NULL) {
+        name = benchmarks;
+        benchmarks = NULL;
+      } else {
+        name = Slice(benchmarks, sep - benchmarks);
+        benchmarks = sep + 1;
+      }
+    }
+    
+    #endif  // STORAGE_LEVELDB_HELPERS_MEMENV_MEMENV_H_
+
+    
+    #include <string>
+    
+    #ifdef USE_MKL2017DNN
+    
+            NDArrayViewPtr deviceValueData;
+        if (device == valueData->Device())
+        {
+            if (readOnly)
+                deviceValueData = valueData->Alias(readOnly);
+            else
+                deviceValueData = valueData;
+        }
+        else
+            deviceValueData = valueData->DeepClone(device, readOnly);
+    
+    public:
+    // assign a sub-rectangle from a 0-based matrix of the same size
+    void assignpatch(const ssematrixbase &patch, const size_t i0, const size_t i1, const size_t j0, const size_t j1)
+    {
+        auto &us = *this;
+        assert(i1 - i0 == patch.rows() && j1 - j0 == patch.cols());
+        assert(i0 <= i1 && j0 <= j1);
+        assert(i1 <= rows() && j1 <= cols());
+    }
+    
+        if (newNode->NodeName() != nodeName) // TODO: This was not tested for earlier; I hope no code depends on this.
+        InvalidArgument('ChangeNode: newNode must have the same name as the old node.');
+    
+        // Note: Do NOT use cudaEventBlockingSync (which supposedly yields the process)--it will totally break cudaEventSynchronize(), causing it to take 50 or 100 ms randomly.
+    // NOTE: We never saw this in reading prefetch.
+    unsigned flags = cudaEventDisableTiming;
+    if (blocking)
+        flags |= cudaEventBlockingSync;
+    
+        // first try if a NetworkBuilder is present
+    function<ComputationNetworkPtr(DEVICEID_TYPE)> createNetworkFn;
+    bool gotIt = TryGetNetworkFactory<ConfigRecordType, ElemType>(config, createNetworkFn);
+    if (gotIt)
+    {
+        // We have several ways to create a network.
+        net = createNetworkFn(deviceId);
+        if (outputNodeNames.size() > 0)
+        {
+            net->InvalidateCompiledNetwork();
+            PatchOutputNodes(net, outputNodeNames, outputNodeNamesVector);
+            net->CompileNetwork();
+            // BUGBUG: This will generate double Validation output in the log
+        }
+    }
+    else // no NetworkBuilder given: load from 'modelPath'
+    {
+        wstring modelPath = config(L'modelPath');
+    }
+    
+        const wstring modelPath = config('modelPath');
+    wstring dbnModelPath = config('dbnModelPath');
+    
+                // make sure that there are some nodes to copy to
+            if (nodesOut.size() == 0)
+                RuntimeError('Setting a single input to multiple outputs requires the multiple outputs to exist. In %ls = %ls, %ls does not match any nodes.', nameOut.c_str(), name.c_str(), nameOut.c_str());
+    
+        // SaveData - save data in the file/files
+    // recordStart - Starting record number
+    // matricies - a map of section name (section:subsection) to data pointer. Data sepcifications from config file will be used to determine where and how to save data
+    // numRecords - number of records we are saving, can be zero if not applicable
+    // datasetSize - size of the dataset (in records)
+    // byteVariableSized - for variable sized data, size of current block to be written, zero when not used, or ignored if not variable sized data
+    virtual bool SaveData(size_t recordStart, const std::map<std::wstring, void*, nocase_compare>& matrices, size_t numRecords, size_t datasetSize, size_t byteVariableSized = 0);
+    
+        void        (*SetStaticObjectField)(JNIEnv*, jclass, jfieldID, jobject);
+    void        (*SetStaticBooleanField)(JNIEnv*, jclass, jfieldID, jboolean);
+    void        (*SetStaticByteField)(JNIEnv*, jclass, jfieldID, jbyte);
+    void        (*SetStaticCharField)(JNIEnv*, jclass, jfieldID, jchar);
+    void        (*SetStaticShortField)(JNIEnv*, jclass, jfieldID, jshort);
+    void        (*SetStaticIntField)(JNIEnv*, jclass, jfieldID, jint);
+    void        (*SetStaticLongField)(JNIEnv*, jclass, jfieldID, jlong);
+    void        (*SetStaticFloatField)(JNIEnv*, jclass, jfieldID, jfloat);
+    void        (*SetStaticDoubleField)(JNIEnv*, jclass, jfieldID, jdouble);
+    
+    /* static */ void Config::destroy(Config * node)
+{
+    delete node;
+}
+    
+    #pragma once
+    
+    double Node::getFlexShrink(void) const
+{
+    return YGNodeStyleGetFlexShrink(m_node);
+}
+    
+        void setFlex(double flex);
+    void setFlexBasis(double flexBasis);
+    void setFlexBasisPercent(double flexBasis);
+    void setFlexBasisAuto();
+    void setFlexGrow(double flexGrow);
+    void setFlexShrink(double flexShrink);
+    
+    // Keeps a thread-local reference to the current thread's JNIEnv.
+struct Environment {
+  // May be null if this thread isn't attached to the JVM
+  FBEXPORT static JNIEnv* current();
+  static void initialize(JavaVM* vm);
+    }
+    
+    
+    {}
+
+    
+    
+    {} // namespace facebook
+#endif // FBASSERT_H
