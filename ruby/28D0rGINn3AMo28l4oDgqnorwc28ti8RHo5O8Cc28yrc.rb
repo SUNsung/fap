12024036e738df8c864133bed916344a254d6617
SUNsung <weatherius@gmail.com>
2018-05-11 04:22:54 +0300

    Rewrite [28D0rGINn3AMo28l4oDgqnorwc28ti8RHo5O8Cc28yrc.rb] File in: 2018-05-11 04:22:54

--- ruby/28D0rGINn3AMo28l4oDgqnorwc28ti8RHo5O8Cc28yrc.rb
+++ ruby/28D0rGINn3AMo28l4oDgqnorwc28ti8RHo5O8Cc28yrc.rb
@@ -1,20 +1,130 @@
-    private
+
+        
+          def redirect_to_nil
+    redirect_to nil
+  end
     
-    module Docs
-  class URL < URI::Generic
-    PARSER = URI::Parser.new
+    module ActionCable
+  module Server
+    # An instance of this configuration object is available via ActionCable.server.config, which allows you to tweak Action Cable configuration
+    # in a Rails config initializer.
+    class Configuration
+      attr_accessor :logger, :log_tags
+      attr_accessor :connection_class, :worker_pool_size
+      attr_accessor :disable_request_forgery_protection, :allowed_request_origins, :allow_same_origin_as_host
+      attr_accessor :cable, :url, :mount_path
     
-            css('.note h3', '.warning h3').each do |node|
-          node.before('<p><strong>#{node.inner_html}</strong></p>').remove
-        end
+      test 'response parsing' do
+    response = ActionDispatch::TestResponse.create(200, {}, '')
+    assert_equal response.body, response.parsed_body
     
-          # Instead of letting Clamp set up accessors for the options
-      # weŕe going to tightly controlling them through
-      # LogStash::SETTINGS
-      def define_simple_writer_for(option, &block)
-        LogStash::SETTINGS.get(option.attribute_name)
-        define_method(option.write_method) do |value|
-          value = instance_exec(value, &block) if block
-          LogStash::SETTINGS.set_value(option.attribute_name, value)
+          def authenticate_long_credentials
+        authenticate_or_request_with_http_token do |token, options|
+          token == '1234567890123456789012345678901234567890' && options[:algorithm] == 'test'
         end
       end
+  end
+    
+            case method
+        when NilClass
+          raise 'Delivery method cannot be nil'
+        when Symbol
+          if klass = delivery_methods[method]
+            mail.delivery_method(klass, (send(:'#{method}_settings') || {}).merge(options || {}))
+          else
+            raise 'Invalid delivery method #{method.inspect}'
+          end
+        else
+          mail.delivery_method(method)
+        end
+    
+        module Behavior
+      extend ActiveSupport::Concern
+    
+    class ActiveSupport::TestCase
+  include ActiveSupport::Testing::MethodCallAssertions
+    
+        def add(path, content)
+      @pages[path] = content
+    end
+    
+              if %w(Events Sync).include?(type)
+            name.prepend 'Backbone.'
+          elsif type == 'History'
+            name.prepend 'Backbone.history.'
+          elsif name == 'extend'
+            name.prepend '#{type}.'
+          elsif name.start_with? 'constructor'
+            name = type
+          elsif type != 'Utility'
+            name.prepend '#{type.downcase}.'
+          end
+    
+            css('.method-name', '.property-name').each do |node|
+          source = node.at_css('a')
+          source.before(%(<span class='name'>#{source.content}</span>))
+          source.content = 'source'
+          source['class'] = 'source'
+        end
+    
+          case Rails.env
+      when 'development'
+        config.eager_load = false
+      when 'test'
+        config.eager_load = false
+      when 'production'
+        config.eager_load = true
+      end
+    end
+  end
+end
+    
+    begin
+  require 'bundler/setup'
+rescue LoadError
+  $stderr.puts '[*] Metasploit requires the Bundler gem to be installed'
+  $stderr.puts '    $ gem install bundler'
+  exit(1)
+end
+    
+      # Extract the form
+  res = '<form'
+  form.attributes.each do |attr|
+    res << ' #{attr[0]}='#{attr[1].gsub(''', '')}''
+  end
+  res << '> '
+    
+              fd.write(res)
+        end
+      end
+      break
+    rescue ::Timeout::Error
+      $stderr.puts '#{prefix}#{site} timed out'
+    rescue ::Interrupt
+      raise $!
+    rescue ::Exception => e
+      $stderr.puts '#{prefix}#{site} #{e.class} #{e}'
+    end
+  end
+    
+    
+# replace the stuff
+replaces.uniq!
+replaces.each { |arr|
+	#puts '%32s: %s -> %s' % arr
+	asm.gsub!(arr[1], arr[2])
+}
+    
+    require 'rubygems'
+require 'rjb'
+    
+    	attr_accessor :file, :block, :block_size
+    
+        begin
+      create_scaffold(source, @target_path)
+    rescue Errno::EACCES => exception
+      report_exception('Permission denied when executing the plugin manager', exception)
+    rescue => exception
+      report_exception('Plugin creation Aborted', exception)
+    end
+  end
