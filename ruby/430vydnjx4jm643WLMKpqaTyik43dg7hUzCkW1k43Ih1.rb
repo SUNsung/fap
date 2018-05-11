edef4714030908883035ed24c4071c7c4ebf73ee
SUNsung <weatherius@gmail.com>
2018-05-11 20:46:27 +0300

    Rewrite [430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb] File in: 2018-05-11 20:46:27

--- ruby/430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb
+++ ruby/430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb
@@ -1,71 +1,11 @@
 
         
-        def check_link(uri)
-  HTTParty.head(uri, :verify => false).code.to_i.tap do |status|
-    if (400..422).include?(status)
-      if status != 403 && !uri.exclude?('udemy.com')
-        raise 'Request had status #{status}'
-      else
-        putc('S')
-      end
-    end
-  end
-end
+        Print a list of currently running Applications and associated
+Bundle IDs, which may be useful in a Cask uninstall stanza, eg
     
-              saved = if pending_reconfirmation?
-            skip_reconfirmation!
-            self.email = unconfirmed_email
-            self.unconfirmed_email = nil
-    
-      def failure_message
-    exception = request.respond_to?(:get_header) ? request.get_header('omniauth.error') : request.env['omniauth.error']
-    error   = exception.error_reason if exception.respond_to?(:error_reason)
-    error ||= exception.error        if exception.respond_to?(:error)
-    error ||= (request.respond_to?(:get_header) ? request.get_header('omniauth.error.type') : request.env['omniauth.error.type']).to_s
-    error.to_s.humanize if error
-  end
-    
-    module Devise
-  module Controllers
-    # A module that may be optionally included in a controller in order
-    # to provide remember me behavior. Useful when signing in is done
-    # through a callback, like in OmniAuth.
-    module Rememberable
-      # Return default cookie values retrieved from session options.
-      def self.cookie_values
-        Rails.configuration.session_options.slice(:path, :domain, :secure)
-      end
-    
-      if ARGV.include? '--no-ansi'
-    STDERR.puts <<-DOC
-    WARNING: CocoaPods requires your terminal to be using UTF-8 encoding.
-    Consider adding the following to ~/.profile:
-    
-            self.description = <<-DESC
-          Shows the content of the pods cache as a YAML tree output, organized by pod.
-          If `NAME` is given, only the caches for that pod will be included in the output.
-        DESC
-    
-          def initialize(argv)
-        super
-        config.silent = false
-      end
-    
-          def validate!
-        super
-        raise Informative, 'Existing Podfile found in directory' unless config.podfile_path_in_dir(Pathname.pwd).nil?
-        if @project_path
-          help! 'Xcode project at #{@project_path} does not exist' unless File.exist? @project_path
-          project_path = @project_path
-        else
-          raise Informative, 'No Xcode project found, please specify one' unless @project_paths.length > 0
-          raise Informative, 'Multiple Xcode projects found, please specify one' unless @project_paths.length == 1
-          project_path = @project_paths.first
-        end
-        @xcode_project = Xcodeproj::Project.open(project_path)
-      end
-    
-            def signal_end_of_output
-          output_pipe.puts(END_OF_OUTPUT_SIGNAL)
-          STDOUT.flush
-        end
+        # A list of classes that will be extracted into mixins
+    # Only the top-level selectors of form .CLASS { ... } are extracted. CLASS must not be used in any other rule definition.
+    # This is a work-around for libsass @extend issues
+    CLASSES_TO_MIXINS = %w(
+      list-unstyled form-inline
+    )
