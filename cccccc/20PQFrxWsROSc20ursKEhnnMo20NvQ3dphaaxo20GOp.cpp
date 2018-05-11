42678889bb9095bb63377a5e5802d30307337fe3
SUNsung <weatherius@gmail.com>
2018-05-11 05:37:57 +0300

    Rewrite [20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp] File in: 2018-05-11 05:37:57

--- cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
+++ cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
@@ -1,157 +1,198 @@
 
         
-        // Use if you want to reset your rendering device without losing ImGui state.
-IMGUI_API void        ImGui_ImplDX10_InvalidateDeviceObjects();
-IMGUI_API bool        ImGui_ImplDX10_CreateDeviceObjects();
+            D3D_DRIVER_TYPE driverTypes[] =
+    {
+        D3D_DRIVER_TYPE_HARDWARE,
+        D3D_DRIVER_TYPE_WARP,
+        D3D_DRIVER_TYPE_REFERENCE,
+    };
+    UINT numDriverTypes = ARRAYSIZE(driverTypes);
     
-    // Implemented features:
-//  [X] User texture binding. Cast 'GLuint' OpenGL texture identifier as void*/ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
-// Missing features:
-//  [ ] SDL2 handling of IME under Windows appears to be broken and it explicitly disable the regular Windows IME. You can restore Windows IME by compiling SDL with SDL_DISABLE_WINDOWS_IME.
+        // A\B
+    cv::solve(H, A, A, cv::DECOMP_NORMAL);
+    H.release();
     
+    void CV_ModelEstimator2_Test::fill_array( int test_case_idx, int i, int j, Mat& arr )
+{
+    if( i != INPUT )
+    {
+        cvtest::ArrayTest::fill_array( test_case_idx, i, j, arr );
+        return;
+    }
+    }
     
-    {    // Restore modified GL state
-    glUseProgram(last_program);
-    glBindTexture(GL_TEXTURE_2D, last_texture);
-    glBindSampler(0, last_sampler);
-    glActiveTexture(last_active_texture);
-    glBindVertexArray(last_vertex_array);
-    glBindBuffer(GL_ARRAY_BUFFER, last_array_buffer);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, last_element_array_buffer);
-    glBlendEquationSeparate(last_blend_equation_rgb, last_blend_equation_alpha);
-    glBlendFuncSeparate(last_blend_src_rgb, last_blend_dst_rgb, last_blend_src_alpha, last_blend_dst_alpha);
-    if (last_enable_blend) glEnable(GL_BLEND); else glDisable(GL_BLEND);
-    if (last_enable_cull_face) glEnable(GL_CULL_FACE); else glDisable(GL_CULL_FACE);
-    if (last_enable_depth_test) glEnable(GL_DEPTH_TEST); else glDisable(GL_DEPTH_TEST);
-    if (last_enable_scissor_test) glEnable(GL_SCISSOR_TEST); else glDisable(GL_SCISSOR_TEST);
-    glPolygonMode(GL_FRONT_AND_BACK, (GLenum)last_polygon_mode[0]);
-    glViewport(last_viewport[0], last_viewport[1], (GLsizei)last_viewport[2], (GLsizei)last_viewport[3]);
-    glScissor(last_scissor_box[0], last_scissor_box[1], (GLsizei)last_scissor_box[2], (GLsizei)last_scissor_box[3]);
+                template<typename T>
+            static __device__ __forceinline__ T atomicMin(T* address, T val)
+            {
+#if defined (__CUDA_ARCH__) && (__CUDA_ARCH__ < 120)
+                T count = ::min(*address, val);
+                do
+                {
+                    *address = count;
+                } while (*address > count);
+    }
+    
+    #endif /* OPENCV_CUDA_WARP_REDUCE_HPP__ */
+
+    
+    CV_EXPORTS_W void setErrorVerbosity(bool verbose);
+    
+    #undef cv_hal_gemm32f
+#define cv_hal_gemm32f lapack_gemm32f
+#undef cv_hal_gemm64f
+#define cv_hal_gemm64f lapack_gemm64f
+#undef cv_hal_gemm32fc
+#define cv_hal_gemm32fc lapack_gemm32fc
+#undef cv_hal_gemm64fc
+#define cv_hal_gemm64fc lapack_gemm64fc
+    
+    inline int64_t factorial(int n) {
+  int64_t a = 1;
+  for (int64_t i = 1; i <= n; ++i) { a *= i; }
+  return a;
 }
     
+    template<typename To, typename From> To checked_convert(From f, const char* name) {
+  if (overflows<To, From>(f)) {
+    std::string msg = 'value cannot be converted to type ';
+    msg += name;
+    msg += ' without overflow: ';
+    msg += std::to_string(f);
+    throw std::domain_error(std::move(msg));
+  }
+  return convert<To, From>(f);
+}
+    
+    using THDGeneratorTypes = std::tuple<THDGenerator>;
+    
+            // decide start column and end column
+        size_t st = numParallelSequences *  rank      / numProcs;
+        size_t en = numParallelSequences * (rank + 1) / numProcs;
+        assert(rank < numProcs);
+        en = en > numParallelSequences ? numParallelSequences : en; // TODO: why are these two tests necessary? We should rather test rank
+        en = (rank + 1 == numProcs) ? numParallelSequences : en;
+        size_t numNewParallelSequence = en - st;
+    
+    #define REGISTER_BINARY_LOGIC_OPERATOR_SCHEMA(OpName)                                                           \
+    REGISTER_OPERATOR_SCHEMA(OpName)                                                                            \
+        .Description('Returns the tensor resulted from performing the ''#OpName'' logical operation'            \
+            'elementwise on the input tensors A and B. If broadcasting is enabled, the right-hand-side'         \
+            'argument will be broadcasted to match the shape of left-hand-side argument. Refer to Add'          \
+            ' for a detailed description of the broadcasting rules.')                                           \
+        .Input('A', 'First operand.', 'T')                                                                      \
+        .Input('B', 'Second operand. With broadcasting can be of smaller size than A. If broadcasting'          \
+            'is disabled, it should be of the same size.', 'T')                                                 \
+        .Output('C', 'Result, has same dimensions and A and type bool.', 'T')                                   \
+        .TypeConstraint('T', { 'tensor(bool)' }, 'Constrain input and output types to bool tensor.')            \
+        .Attr('axis', 'If set, defines the broadcast dimensions.',                                              \
+            AttrType::AttributeProto_AttributeType_INT)                                                         \
+        .Attr('broadcast', 'Pass 1 to enable broadcasting.',                                                    \
+            AttrType::AttributeProto_AttributeType_INT);
+    
+        REGISTER_OPERATOR_SCHEMA(LpNormalization)
+        .Description('Given a matrix, apply Lp-normalization along the provided axis. '
+            'For RS4 default of p = 2 and it will perform L2 normalization. Divide each '
+            'element by the square root of the sum of squares of all elements in the input tensor.')
+        .Input('input', 'Input tensor of any shape', 'T')
+        .Output('output', 'Output tensor of same shape and type as input X.', 'T')
+        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(float)' }, 'Constrain input and output '
+            'types to float tensors.')
+        .Attr('axis', 'Axis along which to perform normalization.',
+            AttrType::AttributeProto_AttributeType_INT)
+        .Attr('p', '(int64, default 2) the order of the normalization, only 1 or 2 are supported.',
+            AttrType::AttributeProto_AttributeType_INT);
+    
+        // Compute gradients to input observations, the weights to the observations, and the class log posterior probabilities
+    virtual void BackpropToNonLooping(size_t inputIndex) override
+    {
+        // Left node must be a scalar
+        if (inputIndex == 0)  //left derivative
         {
-        D3D12_COMMAND_QUEUE_DESC desc = {};
-        desc.Type     = D3D12_COMMAND_LIST_TYPE_DIRECT;
-        desc.Flags    = D3D12_COMMAND_QUEUE_FLAG_NONE;
-        desc.NodeMask = 1;
-        if (g_pd3dDevice->CreateCommandQueue(&desc, IID_PPV_ARGS(&g_pd3dCommandQueue)) != S_OK)
-            return E_FAIL;
+            BackpropToLeft(*m_logSoftmaxOfRight, InputRef(inputIndex).Gradient(), Gradient());
+        }
+        else if (inputIndex == 1)
+        {
+            FrameRange frameRange(InputRef(0).GetMBLayout());
+            BackpropToRight(*m_softmaxOfRight, InputRef(inputIndex).Gradient(), Gradient(), *m_CTCposterior);
+            InputRef(inputIndex).MaskMissingGradientColumnsToZero(frameRange);
+        }
+        else
+           RuntimeError('ForwardBackwardNode criterion expects only two inputs: labels and network output.');
     }
     
-    #include <s3eKeyboard.h>
-#include <s3ePointer.h>
-#include <IwGx.h>
+    FunctionPtr CreateGRU(const ONNXIR::Node *node, const std::vector<Variable> &inputs, const std::string &direction,
+    const std::vector<string> &activations, const std::vector<float> &activation_alpha, const std::vector<float> &activation_beta);
     
-    
-    {  auto iov = buf->getIov();
-  transport.writev(nullptr, iov.data(), iov.size());
-}
-    
-      // create a read buffer for the buffer chain
-  Cursor rcursor(iobuf1.get());
-  EXPECT_EQ(1, rcursor.readBE<uint64_t>());
-  EXPECT_EQ(10, rcursor.readBE<uint64_t>());
-  EXPECT_EQ(20, rcursor.readBE<uint32_t>());
-  EXPECT_EQ(0, rcursor.totalLength());
-  rcursor.reset(iobuf1.get());
-  EXPECT_EQ(20, rcursor.totalLength());
-    
-    
-    {   private:
-    Deleter deleter_;
-  };
-    
-      uint32_t size3 = 4321;
-  unique_ptr<uint8_t[]> buf3(new uint8_t[size3]);
-  IOBuf iobuf3(IOBuf::WRAP_BUFFER, buf3.get(), size3);
-  EXPECT_EQ(buf3.get(), iobuf3.data());
-  EXPECT_EQ(size3, iobuf3.length());
-  EXPECT_EQ(buf3.get(), iobuf3.buffer());
-  EXPECT_EQ(size3, iobuf3.capacity());
-    
-      template <typename T>
-  static std::shared_ptr<T> get_shared_ptr_from_counted_base(
-      counted_base* base,
-      bool inc = true) {
-    if (!base) {
-      return nullptr;
+        // assignment
+    ssematrix &operator=(const ssematrix &other)
+    {
+        assign(other);
+        return *this;
     }
-    std::shared_ptr<const void> newp;
-    if (inc) {
-      inc_shared_count(base, 1);
+    ssematrix &operator=(const ssematrixbase &other)
+    {
+        assign(other);
+        return *this;
     }
-    newp.*fieldPtr(access_shared_ptr_ptr{}) =
-        get_shared_ptr<const void>(base); // _M_ptr
-    (newp.*fieldPtr(access_refcount{})).*fieldPtr(access_base{}) = base;
-    // reinterpret_pointer_cast<T>
-    auto res = reinterpret_cast<std::shared_ptr<T>*>(&newp);
-    return std::move(*res);
-  }
-    
-    #pragma once
+    ssematrix &operator=(ssematrix &&other)
+    {
+        delete_sse(this->p);
+        move(other);
+        return *this;
+    }
     
     
     {
-    {    idx = ProbeFcn()(idx, numProbes, capacity_);
-  }
-}
-    
-    
-    {
-}; // AtomicHashArray
-    
-    void DupAndCloseStderr();
-    
-      // Aggregates all available coverage measurements.
-  struct Coverage {
-    Coverage() { Reset(); }
+    {        if (flags & CopyNodeFlags::copyNodeInputLinks)
+        {
+            // copy the children structure but use the new nodes generated
+            for (int i = 0; i < fromNode->GetNumInputs(); i++)
+                toNode->SetInput(i, GetNodeFromName(toNamePrefix + fromNode->GetInputs()[i]->NodeName()));
+        }
     }
-    
-    template<class T>
-size_t ChangeBinaryInteger(uint8_t *Data, size_t Size, Random &Rand) {
-  if (Size < sizeof(T)) return 0;
-  size_t Off = Rand(Size - sizeof(T) + 1);
-  assert(Off + sizeof(T) <= Size);
-  T Val;
-  if (Off < 64 && !Rand(4)) {
-    Val = Size;
-    if (Rand.RandBool())
-      Val = Bswap(Val);
-  } else {
-    memcpy(&Val, Data + Off, sizeof(Val));
-    T Add = Rand(21);
-    Add -= 10;
-    if (Rand.RandBool())
-      Val = Bswap(T(Bswap(Val) + Add)); // Add assuming different endiannes.
-    else
-      Val = Val + Add;               // Add assuming current endiannes.
-    if (Add == 0 || Rand.RandBool()) // Maybe negate.
-      Val = -Val;
-  }
-  memcpy(Data + Off, &Val, sizeof(Val));
-  return Size;
 }
     
-    void TracePC::HandleInit(uint32_t *Start, uint32_t *Stop) {
-  if (Start == Stop || *Start) return;
-  assert(NumModules < sizeof(Modules) / sizeof(Modules[0]));
-  for (uint32_t *P = Start; P < Stop; P++)
-    *P = ++NumGuards;
-  Modules[NumModules].Start = Start;
-  Modules[NumModules].Stop = Stop;
-  NumModules++;
+    void GPUDataTransferer::CopyGPUToCPUAsync(void* gpuBuffer, size_t totalSize, void* cpuBuffer)
+{
+    m_inner->CopyGPUToCPUAsync(gpuBuffer, 1, totalSize, cpuBuffer);
+    m_inner->RecordGPUToCPUCopy();
 }
     
-      void HandleTrace(uint32_t *guard, uintptr_t PC);
-  void HandleInit(uint32_t *start, uint32_t *stop);
-  void HandleCallerCallee(uintptr_t Caller, uintptr_t Callee);
-  void HandleValueProfile(size_t Value) { ValueProfileMap.AddValue(Value); }
-  template <class T> void HandleCmp(void *PC, T Arg1, T Arg2);
-  size_t GetTotalPCCoverage();
-  void SetUseCounters(bool UC) { UseCounters = UC; }
-  void SetUseValueProfile(bool VP) { UseValueProfile = VP; }
-  void SetPrintNewPCs(bool P) { DoPrintNewPCs = P; }
-  template <class Callback> size_t CollectFeatures(Callback CB);
-  bool UpdateValueProfileMap(ValueBitMap *MaxValueProfileMap) {
-    return UseValueProfile && MaxValueProfileMap->MergeFrom(ValueProfileMap);
+    void ComputeBlockIDCT(const coeff_t* block, uint8_t* out) {
+  coeff_t colidcts[kDCTBlockSize];
+  const int kColScale = 11;
+  const int kColRound = 1 << (kColScale - 1);
+  for (int x = 0; x < 8; ++x) {
+    int colbuf[8] = { 0 };
+    Compute1dIDCT(&block[x], 8, colbuf);
+    for (int y = 0; y < 8; ++y) {
+      colidcts[8 * y + x] = (colbuf[y] + kColRound) >> kColScale;
+    }
   }
+  const int kRowScale = 18;
+  const int kRowRound = 257 << (kRowScale - 1);  // includes offset by 128
+  for (int y = 0; y < 8; ++y) {
+    const int rowidx = 8 * y;
+    int rowbuf[8] = { 0 };
+    Compute1dIDCT(&colidcts[rowidx], 1, rowbuf);
+    for (int x = 0; x < 8; ++x) {
+      out[rowidx + x] =
+          std::max(0, std::min(255, (rowbuf[x] + kRowRound) >> kRowScale));
+    }
+  }
+}
+    
+    // Fills in 'result' with the inverse DCT of 'block'.
+// The arguments 'block' and 'result' point to 8x8 arrays that are arranged in
+// a row-by-row memory layout.
+void ComputeBlockIDCT(const coeff_t* block, uint8_t* result);
+    
+    // Creates a JPEG from the rgb pixel data. Returns true on success. The given
+// quantization table must have 3 * kDCTBlockSize values.
+bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
+                     const int* quant, JPEGData* jpg);
+    
+    #include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
