5b3e749e8aa093b5923d75a30aefc4f12d4a81a8
SUNsung <weatherius@gmail.com>
2018-05-12 22:21:27 +0300

    Rewrite [235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb] File in: 2018-05-12 22:21:27

--- ruby/235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb
+++ ruby/235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb
@@ -1,84 +1,104 @@
 
         
-              def method_missing(selector, *args)
-        begin
-          routes = @controller.respond_to?(:_routes) && @controller._routes
-        rescue
-          # Don't call routes, if there is an error on _routes call
-        end
+        class ApplicationSerializer < ActiveModel::Serializer
+  embed :ids, include: true
     
-      def redirect_to_with_block
-    redirect_to proc { 'http://www.rubyonrails.org/' }
-  end
-    
-          assert_kind_of Hash, headers, 'Headers must be a Hash'
-      assert headers['Content-Type'], 'Content-Type must exist'
-    
-        included do
-      # Do not make this inheritable, because we always want it to propagate
-      cattr_accessor :raise_delivery_errors, default: true
-      cattr_accessor :perform_deliveries, default: true
-      cattr_accessor :deliver_later_queue_name, default: :mailers
-    
-      def tumblr
-    Tumblr.configure do |config|
-      config.consumer_key = tumblr_consumer_key
-      config.consumer_secret = tumblr_consumer_secret
-      config.oauth_token = tumblr_oauth_token
-      config.oauth_token_secret = tumblr_oauth_token_secret
+            expect(result).to eq('hg parent --template {rev}')
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('hg parent --template {rev}')
+      end
     end
-    
-    Tumblr::Client.new
   end
 end
+
     
-      def destroy
-    @event.destroy
-    
-      def destroy_all
-    Delayed::Job.where(locked_at: nil).delete_all
-    
-          respond_to do |format|
-        if new_credentials.map(&:save).all?
-          format.html { redirect_to user_credentials_path, notice: 'The file was successfully uploaded.'}
-        else
-          format.html { redirect_to user_credentials_path, notice: 'One or more of the uploaded credentials was not imported due to an error. Perhaps an existing credential had the same name?'}
-        end
+            expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER]).to match(/cd .* && agvtool next-version -all/)
       end
-    else
-      redirect_to user_credentials_path, notice: 'No file was chosen to be uploaded.' 
+    
+        attr_reader :filters
+    
+            css('> h6').each do |node|
+          node.name = 'h2'
+        end
+    
+          # A string representation of the importer.
+      # Should be overridden by subclasses.
+      #
+      # This is used to help debugging,
+      # and should usually just show the load path encapsulated by this importer.
+      #
+      # @return [String]
+      def to_s
+        Sass::Util.abstract(self)
+      end
+    
+      if defined? config.symbolize_keys!
+    config.symbolize_keys!
+  end
+    
+        private
+    
+          def all_gem_names
+        core_gem_names + plugin_gem_names
+      end
+    
+          def assert_valid_stage_names(names)
+        invalid = names.find { |n| RESERVED_NAMES.include?(n) }
+        return if invalid.nil?
+    
+    module Capistrano
+  module TaskEnhancements
+    def before(task, prerequisite, *args, &block)
+      prerequisite = Rake::Task.define_task(prerequisite, *args, &block) if block_given?
+      Rake::Task[task].enhance [prerequisite]
+    end
+    
+          it 'selects nothing when a roles filter is present' do
+        dsl.set :filter, roles: 'web'
+        SSHKit::Coordinator.expects(:new).with([]).returns(@coordinator)
+        dsl.on('my.server')
+      end
+    
+      # PATCH/PUT /books/1
+  # PATCH/PUT /books/1.json
+  def update
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
     end
   end
     
-          included do
-        before_create :generate_confirmation_token, if: :confirmation_required?
-        after_create :skip_reconfirmation_in_callback!, if: :send_confirmation_notification?
-        if defined?(ActiveRecord) && self < ActiveRecord::Base # ActiveRecord
-          after_commit :send_on_create_confirmation_instructions, on: :create, if: :send_confirmation_notification?
-          after_commit :send_reconfirmation_instructions, on: :update, if: :reconfirmation_required?
-        else # Mongoid
-          after_create :send_on_create_confirmation_instructions, if: :send_confirmation_notification?
-          after_update :send_reconfirmation_instructions, if: :reconfirmation_required?
-        end
-        before_update :postpone_email_change_until_confirmation_and_regenerate_confirmation_token, if: :postpone_email_change?
-      end
+            private
     
-        if successfully_sent?(resource)
-      respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
-    else
-      respond_with(resource)
-    end
-  end
+    module RuboCop
+  module Cop
+    module Lint
+      # Don't omit the accumulator when calling `next` in a `reduce` block.
+      #
+      # @example
+      #
+      #   # bad
+      #
+      #   result = (1..4).reduce(0) do |acc, i|
+      #     next if i.odd?
+      #     acc + i
+      #   end
+      #
+      # @example
+      #
+      #   # good
+      #
+      #   result = (1..4).reduce(0) do |acc, i|
+      #     next acc if i.odd?
+      #     acc + i
+      #   end
+      class NextWithoutAccumulator < Cop
+        MSG = 'Use `next` with an accumulator argument in a `reduce`.'.freeze
     
-          def self.generate_helpers!(routes=nil)
-        routes ||= begin
-          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
-          Devise::URL_HELPERS.slice(*mappings)
-        end
-    
-    require 'rake/testtask'
-Rake::TestTask.new do |t|
-  t.libs << 'test'
-  t.test_files = FileList['test/**/*_test.rb']
-  t.verbose = true
-end
+    Dir.chdir APP_ROOT do
+  # This script is a starting point to setup your application.
+  # Add necessary setup steps to this file:
