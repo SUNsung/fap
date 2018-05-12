fbac7ff8ac85137a09cd9e16c36ce3c184ca3ddd
SUNsung <weatherius@gmail.com>
2018-05-12 11:38:50 +0300

    Rewrite [430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb] File in: 2018-05-12 11:38:49

--- ruby/430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb
+++ ruby/430vydnjx4jm643WLMKpqaTyik43dg7hUzCkW1k43Ih1.rb
@@ -1,11 +1,123 @@
 
         
-        Print a list of currently running Applications and associated
-Bundle IDs, which may be useful in a Cask uninstall stanza, eg
+          test 'response status aliases deprecated' do
+    response = ActionDispatch::TestResponse.create
+    assert_deprecated { response.success? }
+    assert_deprecated { response.missing? }
+    assert_deprecated { response.error? }
+  end
+end
+
     
-        # A list of classes that will be extracted into mixins
-    # Only the top-level selectors of form .CLASS { ... } are extracted. CLASS must not be used in any other rule definition.
-    # This is a work-around for libsass @extend issues
-    CLASSES_TO_MIXINS = %w(
-      list-unstyled form-inline
-    )
+      def flash
+    render inline: '<h1><%= notice %></h1>'
+  end
+    
+    class WebServiceTest < ActionDispatch::IntegrationTest
+  class TestController < ActionController::Base
+    def assign_parameters
+      if params[:full]
+        render plain: dump_params_keys
+      else
+        render plain: (params.keys - ['controller', 'action']).sort.join(', ')
+      end
+    end
+    
+          unless root?
+        raise Invalid, 'missing name' if !name || name.empty?
+        raise Invalid, 'missing path' if !path || path.empty?
+        raise Invalid, 'missing type' if !type || type.empty?
+      end
+    end
+    
+    module Docs
+  class PageDb
+    attr_reader :pages
+    
+              # Built-in events
+          if node['id'] == 'Events-catalog'
+            node.next_element.css('li').each do |li|
+              name = '#{li.at_css('b').content.delete(''').strip} event'
+              id = name.parameterize
+              li['id'] = id
+              entries << [name, id, type] unless name == entries.last[0]
+            end
+            next
+          end
+    
+            css('.method-name', '.property-name').each do |node|
+          source = node.at_css('a')
+          source.before(%(<span class='name'>#{source.content}</span>))
+          source.content = 'source'
+          source['class'] = 'source'
+        end
+    
+            # Registers a SIGINT handler. This typically is called from {busy}.
+        # Callbacks are only registered once, so calling this multiple times
+        # with the same callback has no consequence.
+        def register(sig_callback)
+          @@mutex.synchronize do
+            registered << sig_callback
+            registered.uniq!
+    
+            expect_updated_sign_in_at(user)
+        expect(Redis.current.get('account:#{user.account_id}:regeneration')).to eq 'true'
+        expect(RegenerationWorker).to have_received(:perform_async)
+      end
+    
+      def start_url
+    '/web/timelines/home'
+  end
+    
+        def compass?
+      defined?(::Compass::Frameworks)
+    end
+    
+      # Disable Rails's static asset server (Apache or nginx will already do this).
+  if config.respond_to?(:serve_static_files)
+    # rails >= 4.2
+    config.serve_static_files = true
+  elsif config.respond_to?(:serve_static_assets)
+    # rails < 4.2
+    config.serve_static_assets = true
+  end
+    
+    module Sass::Exec
+  # The `sass-convert` executable.
+  class SassConvert < Base
+    # @param args [Array<String>] The command-line arguments
+    def initialize(args)
+      super
+      require 'sass'
+      @options[:for_tree] = {}
+      @options[:for_engine] = {:cache => false, :read_cache => true}
+    end
+    
+          # If this importer is based on files on the local filesystem This method
+      # should return true if the file, when changed, should trigger a
+      # recompile.
+      #
+      # It is acceptable for non-sass files to be watched and trigger a recompile.
+      #
+      # @param filename [String] The absolute filename for a file that has changed.
+      # @return [Boolean] When the file changed should cause a recompile.
+      def watched_file?(filename)
+        false
+      end
+    end
+  end
+end
+
+    
+      module Sass::Plugin::Configuration
+    # Different default options in a m environment.
+    def default_options
+      @default_options ||= begin
+        version = Merb::VERSION.split('.').map {|n| n.to_i}
+        if version[0] <= 0 && version[1] < 5
+          root = MERB_ROOT
+          env  = MERB_ENV
+        else
+          root = Merb.root.to_s
+          env  = Merb.environment
+        end
