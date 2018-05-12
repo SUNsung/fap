86f57276e35578d61e3739a9e3f5318b3a94f942
SUNsung <weatherius@gmail.com>
2018-05-12 23:34:55 +0300

    Rewrite [28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb] File in: 2018-05-12 23:34:54

--- ruby/28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb
+++ ruby/28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb
@@ -1,91 +1,32 @@
 
         
-                expect(result).to eq(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER])
-      end
-    
-          def [](key)
-        super(convert_key(key))
-      end
-    
-              # Parse the options
-          argv = parse_options(opts)
-          return if !argv
-          if argv.empty? || argv.length > 2
-            raise Vagrant::Errors::CLIInvalidUsage,
-              help: opts.help.chomp
-          end
-    
-    module VagrantPlugins
-  module CommandBox
-    module Command
-      class Repackage < Vagrant.plugin('2', :command)
-        def execute
-          opts = OptionParser.new do |o|
-            o.banner = 'Usage: vagrant box repackage <name> <provider> <version>'
-          end
-    
-      def as_json(options={})
-    {
-      poll_id:             id,
-      post_id:             status_message.id,
-      question:            question,
-      poll_answers:        poll_answers,
-      participation_count: participation_count
-    }
-  end
-    
-              render 'admins/pods'
-        end
-        format.mobile { render 'admins/pods' }
-        format.json { render json: pods_json }
-      end
-    end
-    
-          # Inspired by https://github.com/nov/openid_connect_sample/blob/master/app/controllers/connect/clients_controller.rb#L24
-      def create
-        registrar = OpenIDConnect::Client::Registrar.new(request.url, params)
-        client = Api::OpenidConnect::OAuthApplication.register! registrar
-        render json: client.as_json(root: false)
-      end
-    
-          def create
-        req = Rack::Request.new(request.env)
-        if req['client_assertion_type'] == 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
-          handle_jwt_bearer(req)
-        end
-        self.status, headers, self.response_body = Api::OpenidConnect::TokenEndpoint.new.call(request.env)
-        headers.each {|name, value| response.headers[name] = value }
-        nil
-      end
-    
-        # Returns a deep copy of this query and all its children.
-    #
-    # @return [Query]
-    def deep_copy
-      Query.new(
-        modifier.map {|c| c.is_a?(Sass::Script::Tree::Node) ? c.deep_copy : c},
-        type.map {|c| c.is_a?(Sass::Script::Tree::Node) ? c.deep_copy : c},
-        expressions.map {|e| e.map {|c| c.is_a?(Sass::Script::Tree::Node) ? c.deep_copy : c}})
-    end
-  end
-    
-        def self.run
-      # Apparently there's no better way than this to add Sass
-      # to Merb's Rack stack.
-      Merb::Config[:app] = Sass::Plugin::Rack.new(Merb::Config[:app])
+            def register_sprockets
+      Sprockets.append_path(stylesheets_path)
+      Sprockets.append_path(fonts_path)
+      Sprockets.append_path(javascripts_path)
     end
   end
 end
-
     
-          val = @mid.perform(environment)
-      if @warn_for_color && val.is_a?(Sass::Script::Value::Color) && val.name
-        alternative = Operation.new(Sass::Script::Value::String.new('', :string), @mid, :plus)
-        Sass::Util.sass_warn <<MESSAGE
-WARNING on line #{line}, column #{source_range.start_pos.offset}#{' of #{filename}' if filename}:
-You probably don't mean to use the color value `#{val}' in interpolation here.
-It may end up represented as #{val.inspect}, which will likely produce invalid CSS.
-Always quote color names when using them as strings (for example, '#{val}').
-If you really want to use the color value here, use `#{alternative.to_sass}'.
-MESSAGE
+      # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+    
+          def delete_authorization_session_variables
+        session.delete(:client_id)
+        session.delete(:response_type)
+        session.delete(:redirect_uri)
+        session.delete(:scopes)
+        session.delete(:state)
+        session.delete(:nonce)
+      end
+    
+          attr_reader :agent
+    
+          def define_deprecated_accessors_for(option, opts, &block)
+        define_deprecated_writer_for(option, opts, &block)
+      end
+    
+          def clear
+        entries.clear
       end
