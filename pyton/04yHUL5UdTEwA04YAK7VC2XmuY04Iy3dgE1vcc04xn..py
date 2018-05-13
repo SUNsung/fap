22217689150fa0a3b4d08e101144d626b0027ffb
SUNsung <weatherius@gmail.com>
2018-05-13 12:51:56 +0300

    Rewrite [04yHUL5UdTEwA04YAK7VC2XmuY04Iy3dgE1vcc04xn..py] File in: 2018-05-13 12:51:56

--- pyton/04yHUL5UdTEwA04YAK7VC2XmuY04Iy3dgE1vcc04xn..py
+++ pyton/04yHUL5UdTEwA04YAK7VC2XmuY04Iy3dgE1vcc04xn..py
@@ -1,184 +1,110 @@
 
         
-        
-def check_format(filename):
-    '''
-    validates that each line is formatted correctly,
-    appending to error list as needed
-    '''
-    with open(filename) as fp:
-        lines = list(line.rstrip() for line in fp)
-    check_alphabetical(lines)
-    # START Check Entries
-    num_in_category = min_entries_per_section + 1
-    category = ''
-    category_line = 0
-    for line_num, line in enumerate(lines):
-        if section_title_re.match(line):
-            title_links.append(section_title_re.match(line).group(1))
-        # check each section for the minimum number of entries
-        if line.startswith(anchor):
-            match = anchor_re.match(line)
-            if match:
-                if match.group(1) not in title_links:
-                    add_error(line_num, 'section header ({}) not added as a title link'.format(match.group(1)))
-            else:
-                add_error(line_num, 'section header is not formatted correctly')
-            if num_in_category < min_entries_per_section:
-                add_error(category_line, '{} section does not have the minimum {} entries (only has {})'.format(
-                    category, min_entries_per_section, num_in_category))
-            category = line.split(' ')[1]
-            category_line = line_num
-            num_in_category = 0
-            continue
-        # skips lines that we do not care about
-        if not line.startswith('|') or line.startswith('|---'):
-            continue
-        num_in_category += 1
-        segments = line.split('|')[1:-1]
-        if len(segments) < num_segments:
-            add_error(line_num, 'entry does not have all the required sections (have {}, need {})'.format(
-                len(segments), num_segments))
-            continue
-        # START Global
-        for segment in segments:
-            # every line segment should start and end with exactly 1 space
-            if len(segment) - len(segment.lstrip()) != 1 or len(segment) - len(segment.rstrip()) != 1:
-                add_error(line_num, 'each segment must start and end with exactly 1 space')
-        # END Global
-        segments = [seg.strip() for seg in segments]
-        check_entry(line_num, segments)
-    # END Check Entries
+          Returns:
+    A 1D np.array containing the periodic hann window.
+  '''
+  return 0.5 - (0.5 * np.cos(2 * np.pi / window_length *
+                             np.arange(window_length)))
     
-        def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('-l', '--list', dest='list', action='store_true',
-                          help='only list contracts, without checking them')
-        parser.add_option('-v', '--verbose', dest='verbose', default=False, action='store_true',
-                          help='print contract tests for all spiders')
-    
-        if WSAStringToAddressA(
-            ip_string,
-            address_family,
-            None,
-            ctypes.byref(addr),
-            ctypes.byref(addr_size)
-    ) != 0:
-        raise socket.error(ctypes.FormatError())
-    
-            # The index into the input stream where the last error occurred.
-        # This is used to prevent infinite loops where an error is found
-        # but no token is consumed during recovery...another error is found,
-        # ad naseum.  This is a failsafe mechanism to guarantee that at least
-        # one token/tree node is consumed for two errors.
-        self.lastErrorIndex = -1
-    
-    @mainpage
-    
-    try:
-    # compatible for python2
-    from urllib2 import urlopen
-    from urllib2 import HTTPError
-    from urllib2 import URLError
-except ImportError:
-    # compatible for python3
-    from urllib.request import urlopen
-    from urllib.error import HTTPError
-    from urllib.error import URLError
-    
-      base.LoadJsonDefaultsIntoVim()
-    
-        # Create and fill-in the class template
-    numfields = len(field_names)
-    argtxt = repr(field_names).replace(''', '')[1:-1]   # tuple repr without parens or quotes
-    reprtxt = ', '.join('%s=%%r' % name for name in field_names)
-    dicttxt = ', '.join('%r: t[%d]' % (name, pos) for pos, name in enumerate(field_names))
-    template = '''class %(typename)s(tuple):
-        '%(typename)s(%(argtxt)s)' \n
-        __slots__ = () \n
-        _fields = %(field_names)r \n
-        def __new__(_cls, %(argtxt)s):
-            return _tuple.__new__(_cls, (%(argtxt)s)) \n
-        @classmethod
-        def _make(cls, iterable, new=tuple.__new__, len=len):
-            'Make a new %(typename)s object from a sequence or iterable'
-            result = new(cls, iterable)
-            if len(result) != %(numfields)d:
-                raise TypeError('Expected %(numfields)d arguments, got %%d' %% len(result))
-            return result \n
-        def __repr__(self):
-            return '%(typename)s(%(reprtxt)s)' %% self \n
-        def _asdict(t):
-            'Return a new dict which maps field names to their values'
-            return {%(dicttxt)s} \n
-        def _replace(_self, **kwds):
-            'Return a new %(typename)s object replacing specified fields with new values'
-            result = _self._make(map(kwds.pop, %(field_names)r, _self))
-            if kwds:
-                raise ValueError('Got unexpected field names: %%r' %% kwds.keys())
-            return result \n
-        def __getnewargs__(self):
-            return tuple(self) \n\n''' % locals()
-    for i, name in enumerate(field_names):
-        template += '        %s = _property(_itemgetter(%d))\n' % (name, i)
-    
-    _threads_queues = weakref.WeakKeyDictionary()
-_shutdown = False
-    
-    def main():
-    for name, fn in [('sequential',
-                      functools.partial(download_urls_sequential, URLS)),
-                     ('processes',
-                      functools.partial(download_urls_with_executor,
-                                        URLS,
-                                        ProcessPoolExecutor(10))),
-                     ('threads',
-                      functools.partial(download_urls_with_executor,
-                                        URLS,
-                                        ThreadPoolExecutor(10)))]:
-        sys.stdout.write('%s: ' % name.ljust(12))
-        start = time.time()
-        url_map = fn()
-        sys.stdout.write('%.2f seconds (%d of %d downloaded)\n' %
-                         (time.time() - start, len(url_map), len(URLS)))
-    
-        sqrt_n = int(math.floor(math.sqrt(n)))
-    for i in range(3, sqrt_n + 1, 2):
-        if n % i == 0:
-            return False
-    return True
-    
-        def test_done_callback_with_exception(self):
-        callback_exception = [None]
-        def fn(callback_future):
-            callback_exception[0] = callback_future.exception()
+    from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
     
     
-def FormatDebugInfoResponse_Completer_ServerRunningWithHost_test():
-  response = deepcopy( GENERIC_RESPONSE )
-  assert_that(
-    FormatDebugInfoResponse( response ),
-    contains_string(
-      'Completer name completer debug information:\n'
-      '  Server name running at: http://127.0.0.1:1234\n'
-      '  Server name process ID: 12345\n'
-      '  Server name executable: /path/to/executable\n'
-      '  Server name logfiles:\n'
-      '    /path/to/stdout/logfile\n'
-      '    /path/to/stderr/logfile\n'
-      '  Server name key: value\n'
-      '  Key: value\n'
+def to_pygtp(board_size, coord):
+  '''Converts from a MiniGo coordinate to a pygtp coordinate.'''
+  if coord is None:
+    return gtp.PASS
+  return coord[1] + 1, board_size - coord[0]
+
+    
+        self.assertEqual(coords.to_sgf((0, 8)), 'ia')
+    self.assertEqual(coords.to_flat(utils_test.BOARD_SIZE, (0, 8)), 8)
+    self.assertEqual(coords.to_kgs(utils_test.BOARD_SIZE, (0, 8)), 'J9')
+    self.assertEqual(coords.to_pygtp(utils_test.BOARD_SIZE, (0, 8)), (9, 9))
+    
+      def test_add_child(self):
+    root = MCTSNode(utils_test.BOARD_SIZE, go.Position(utils_test.BOARD_SIZE))
+    child = root.maybe_add_child(17)
+    self.assertIn(17, root.children)
+    self.assertEqual(child.parent, root)
+    self.assertEqual(child.fmove, 17)
+    
+        # print some stats on anything with probability > 1%
+    if self.verbosity > 2:
+      print(self.root.describe(), file=sys.stderr)
+      print('\n\n', file=sys.stderr)
+    if self.verbosity > 3:
+      print(self.root.position, file=sys.stderr)
+    
+      def test_chinese_handicap_handling(self):
+    intermediate_board = utils_test.load_board('''
+      .........
+      .........
+      ......X..
+      .........
+      .........
+      .........
+      .........
+      .........
+      .........
+    ''')
+    intermediate_position = go.Position(
+        utils_test.BOARD_SIZE,
+        intermediate_board,
+        n=1,
+        komi=5.5,
+        caps=(0, 0),
+        recent=(go.PlayerMove(go.BLACK, coords.from_kgs(
+            utils_test.BOARD_SIZE, 'G7')),),
+        to_play=go.BLACK,
     )
-  )
+    final_board = utils_test.load_board('''
+      ....OX...
+      .O.OOX...
+      O.O.X.X..
+      .OXXX....
+      OX...XX..
+      .X.XXO...
+      X.XOOXXX.
+      XXXO.OOX.
+      .XOOX.O..
+    ''')
+    final_position = go.Position(
+        utils_test.BOARD_SIZE,
+        final_board,
+        n=50,
+        komi=5.5,
+        caps=(7, 2),
+        ko=None,
+        recent=(
+            go.PlayerMove(
+                go.WHITE, coords.from_kgs(utils_test.BOARD_SIZE, 'E9')),
+            go.PlayerMove(
+                go.BLACK, coords.from_kgs(utils_test.BOARD_SIZE, 'F9')),),
+        to_play=go.WHITE
+    )
+    positions_w_context = list(replay_sgf(
+        utils_test.BOARD_SIZE, CHINESE_HANDICAP_SGF))
+    self.assertEqualPositions(
+        intermediate_position, positions_w_context[1].position)
+    self.assertEqual(
+        positions_w_context[1].next_move, coords.from_kgs(
+            utils_test.BOARD_SIZE, 'C3'))
+    final_replayed_position = positions_w_context[-1].position.play_move(
+        positions_w_context[-1].next_move)
+    self.assertEqualPositions(final_position, final_replayed_position)
     
+        # Train the Model.
+    classifier.train(
+        input_fn=lambda:iris_data.train_input_fn(train_x, train_y, args.batch_size),
+        steps=args.train_steps)
     
-class OmniCompletionRequest( CompletionRequest ):
-  def __init__( self, omni_completer, request_data ):
-    super( OmniCompletionRequest, self ).__init__( request_data )
-    self._omni_completer = omni_completer
-    
-        def current_user(self):
-        if c.user_is_loggedin:
-            return self.stacked_proxy_safe_get(c, 'user')
-    
-            c.allow_framing = True
+        for url in urls:
+        if not url.startswith('http'):
+            print('markdown file name: ' + url)
+            continue
+        if check_live_url(url):
+            print(url)
+        else:
+            print(url, file=sys.stderr)
