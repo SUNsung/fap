0984ff2dc7d7ce2e00e7197f1de3595df987d63c
SUNsung <weatherius@gmail.com>
2018-05-13 11:45:58 +0300

    Rewrite [06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py] File in: 2018-05-13 11:45:57

--- pyton/06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py
+++ pyton/06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py
@@ -1,57 +1,111 @@
 
         
-            item_ok = SectionName.entry_ok  # localize for test override
+        with open('update/LATEST_VERSION', 'w') as f:
+    f.write(version)
     
-    import os
-import time
-import calendar
-import socket
-import errno
-import copy
-import warnings
-import email
-import email.message
-import email.generator
-import io
-import contextlib
-try:
-    import fcntl
-except ImportError:
-    fcntl = None
     
-            self.assertEqual(format(1.5+3j, '20.2f'), '          1.50+3.00j')
-        self.assertEqual(format(1.5+3j, '>20.2f'), '          1.50+3.00j')
-        self.assertEqual(format(1.5+3j, '<20.2f'), '1.50+3.00j          ')
-        self.assertEqual(format(1.5e20+3j, '<20.2f'), '150000000000000000000.00+3.00j')
-        self.assertEqual(format(1.5e20+3j, '>40.2f'), '          150000000000000000000.00+3.00j')
-        self.assertEqual(format(1.5e20+3j, '^40,.2f'), '  150,000,000,000,000,000,000.00+3.00j  ')
-        self.assertEqual(format(1.5e21+3j, '^40,.2f'), ' 1,500,000,000,000,000,000,000.00+3.00j ')
-        self.assertEqual(format(1.5e21+3000j, ',.2f'), '1,500,000,000,000,000,000,000.00+3,000.00j')
+if __name__ == '__main__':
+    unittest.main()
+
     
-        def test_format(self):
-        self.assertEqual('%d' % False, '0')
-        self.assertEqual('%d' % True, '1')
-        self.assertEqual('%x' % False, '0')
-        self.assertEqual('%x' % True, '1')
+        def tearDown(self):
+        if os.path.exists(self.test_dir):
+            shutil.rmtree(self.test_dir)
     
-    from concurrent.futures import _base
     
-    def main():
-    for name, fn in [('sequential', sequential),
-                     ('processes', with_process_pool_executor),
-                     ('threads', with_thread_pool_executor)]:
-        sys.stdout.write('%s: ' % name.ljust(12))
-        start = time.time()
-        if fn() != [True] * len(PRIMES):
-            sys.stdout.write('failed\n')
+TEST_DIR = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'swftests')
+    
+    
+class TestUnicodeLiterals(unittest.TestCase):
+    def test_all_files(self):
+        for dirpath, dirnames, filenames in os.walk(rootDir):
+            for ignore_dir in IGNORED_DIRS:
+                if ignore_dir in dirnames:
+                    # If we remove the directory from dirnames os.walk won't
+                    # recurse into it
+                    dirnames.remove(ignore_dir)
+            for basename in filenames:
+                if not basename.endswith('.py'):
+                    continue
+                if basename in IGNORED_FILES:
+                    continue
+    
+            # Request the extended version of the api for extra fields like artist and title
+        api_response = self._download_json(
+            'http://www.audiomack.com/api/music/url/song/%s?extended=1&_=%d' % (
+                album_url_tag, time.time()),
+            album_url_tag)
+    
+    
+class CloserToTruthIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)?closertotruth\.com/(?:[^/]+/)*(?P<id>[^/?#&]+)'
+    _TESTS = [{
+        'url': 'http://closertotruth.com/series/solutions-the-mind-body-problem#video-3688',
+        'info_dict': {
+            'id': '0_zof1ktre',
+            'display_id': 'solutions-the-mind-body-problem',
+            'ext': 'mov',
+            'title': 'Solutions to the Mind-Body Problem?',
+            'upload_date': '20140221',
+            'timestamp': 1392956007,
+            'uploader_id': 'CTTXML'
+        },
+        'params': {
+            'skip_download': True,
+        },
+    }, {
+        'url': 'http://closertotruth.com/episodes/how-do-brains-work',
+        'info_dict': {
+            'id': '0_iuxai6g6',
+            'display_id': 'how-do-brains-work',
+            'ext': 'mov',
+            'title': 'How do Brains Work?',
+            'upload_date': '20140221',
+            'timestamp': 1392956024,
+            'uploader_id': 'CTTXML'
+        },
+        'params': {
+            'skip_download': True,
+        },
+    }, {
+        'url': 'http://closertotruth.com/interviews/1725',
+        'info_dict': {
+            'id': '1725',
+            'title': 'AyaFr-002',
+        },
+        'playlist_mincount': 2,
+    }]
+    
+        output_directory = 'pdfs' if results.directory is None else results.directory
+    
+    class ProcessPoolExecutor(_base.Executor):
+    def __init__(self, max_workers=None):
+        '''Initializes a new ProcessPoolExecutor instance.
+    
         else:
-            sys.stdout.write('%.2f seconds\n' % (time.time() - start))
+      self._filters = _CompileFilters( config_or_filters )
     
-    from ycm.client.debug_info_request import FormatDebugInfoResponse
     
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
+def FormatDebugInfoResponse_Completer_ServerRunningWithHost_test():
+  response = deepcopy( GENERIC_RESPONSE )
+  assert_that(
+    FormatDebugInfoResponse( response ),
+    contains_string(
+      'Completer name completer debug information:\n'
+      '  Server name running at: http://127.0.0.1:1234\n'
+      '  Server name process ID: 12345\n'
+      '  Server name executable: /path/to/executable\n'
+      '  Server name logfiles:\n'
+      '    /path/to/stdout/logfile\n'
+      '    /path/to/stderr/logfile\n'
+      '  Server name key: value\n'
+      '  Key: value\n'
+    )
+  )
+    
+      _assert_rejects( f, 'This is a Taco' )
+  _assert_rejects( f, 'This is a Burrito' )
+    
+      for path in not_python_paths:
+    yield EndsWithPython_Bad, path
