9ec7f44bfd3f10716bd9563bf2fdf9c2560ec383
SUNsung <weatherius@gmail.com>
2018-05-13 08:10:54 +0300

    Rewrite [41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb] File in: 2018-05-13 08:10:53

--- ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
+++ ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
@@ -1,75 +1,43 @@
 
         
-            it 'should have a way of getting the service configurations' do
-      configs = key.service_configs_for(Spaceship::Portal::Key::MUSIC_KIT_ID)
-      expect(configs).to be_instance_of(Array)
-      expect(configs.sample).to be_instance_of(Hash)
-      expect(configs.first['identifier']).to eq('music.com.snatchev.test')
-    end
+            assert_equal %(dont <\\/close> tags), j(%(dont </close> tags))
+  end
     
-          it 'splits correctly' do
-        expected = ['One', 'Two', 'Three', 'Four Token']
-        expect(generator.split_keywords(keywords)).to eq(expected)
-      end
-    end
+      tests DummyController
     
-            # Helper method to get access to the class variable. This is mostly
-        # exposed for tests. This shouldn't be mucked with directly, since it's
-        # structure may change at any time.
-        def registered; @@registered; end
+            assert_equal 'summary, title', @controller.response.body
+        assert @controller.params.has_key?(:summary)
+        assert @controller.params.has_key?(:title)
+        assert_equal 'content...', @controller.params['summary']
+        assert_equal 'JSON', @controller.params['title']
       end
     end
   end
-end
+    
+            mail.perform_deliveries    = perform_deliveries
+        mail.raise_delivery_errors = raise_delivery_errors
+      end
+    end
+    
+          raise Discourse::InvalidParameters.new(:to_address) unless args[:to_address].present?
+    
+    end
 
     
-      GEMFILE_EXTENSIONS.each do |extension|
-    extension_pathname = root.join('Gemfile#{extension}')
+      API_URL = 'https://api.github.com'.freeze
     
-      File.unlink(out) if (File.size(out) == 0)
-    
-      def parse(pkt)
-    # We want to return immediantly if	we do not have a packet which is handled by us
-    return unless pkt.is_tcp?
-    return if (pkt.tcp_sport != 80 and pkt.tcp_dport != 80)
-    s = find_session((pkt.tcp_sport == 80) ? get_session_src(pkt) : get_session_dst(pkt))
-    
-    $:.unshift(File.join(File.dirname(__FILE__), '..', '..', '..', 'lib'))
-    
-    success = clsJavaCompile._invoke('CompileFromMemory','[Ljava.lang.String;[Ljava.lang.String;[Ljava.lang.String;', classNames, codez, compileOpts)
-    
-      when '6.1.2'
-    __NR_execve      = 7
-    __NR_getpeername = 205
-    __NR_accept      = 232
-    __NR_listen      = 235
-    __NR_bind        = 237
-    __NR_socket      = 238
-    __NR_connect     = 239
-    __NR_close       = 272
-    __NR_kfcntl      = 635
-    
-    	def dump_all
-		block.each do |block|
-			block.each do |line|
-				print '#{line}\n'
-			end
-		end
-	end
-    
-        # Fetch a var from the context
-    # @param [Symbol] variable The variable to fetch
-    # @param [Object] default  The default value if not found
-    #
-    def fetch(*args)
-      context.fetch(*args)
-    end
-    
-      desc 'Revert server(s) to previous release.'
-  task :reverting do
-  end
-    
-            it 'returns the servers' do
-          expect(subject.map(&:hostname)).to eq %w{example3.com}
+            def operator_assignment_node
+          return nil unless node.parent
+          return nil unless OPERATOR_ASSIGNMENT_TYPES.include?(node.parent.type)
+          return nil unless node.sibling_index.zero?
+          node.parent
         end
-      end
+    
+                unless assignment.run_exclusively_with?(reference)
+              assignment.reference!(node)
+            end
+    
+            def on_case(case_node)
+          case_node.when_branches.each_with_object([]) do |when_node, previous|
+            when_node.each_condition do |condition|
+              next unless repeated_condition?(previous, condition)
