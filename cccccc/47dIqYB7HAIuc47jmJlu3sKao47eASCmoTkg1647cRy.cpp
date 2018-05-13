82e930cf144bb4596f129174f454ea0c439a885c
SUNsung <weatherius@gmail.com>
2018-05-13 19:15:15 +0300

    Rewrite [47dIqYB7HAIuc47jmJlu3sKao47eASCmoTkg1647cRy.cpp] File in: 2018-05-13 19:15:14

--- cccccc/47dIqYB7HAIuc47jmJlu3sKao47eASCmoTkg1647cRy.cpp
+++ cccccc/47dIqYB7HAIuc47jmJlu3sKao47eASCmoTkg1647cRy.cpp
@@ -1,220 +1,237 @@
 
         
-        #include <unordered_set>
-#include <vector>
+          base::win::ScopedHICON icon;
+  HWND hWnd = getHWND(getAppWindow(this));
+  if (hWnd == NULL) {
+    error_ = kNoAssociatedAppWindow;
+    LOG(ERROR) << error_;
+    return false;
+  }
+  const float scale = display::win::GetDPIScale();
+  if (badge.size())
+    icon = createBadgeIcon(hWnd, base::UTF8ToUTF16(badge).c_str(), 16 * scale, 16 * scale);
     
-    class SmoothHingeLossUpdater : public DualLossUpdater {
+    
+    {}  // namespace
+    
+    
+    {  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
+  if (!item)
+    return false;
+  return !item->icon_.IsEmpty();
+}
+    
+       bool GetAcceleratorForCommandId(
+      int command_id,
+      ui::Accelerator* accelerator) const override;
+    
+    void Menu::Remove(MenuItem* menu_item, int pos) {
+  std::vector<MenuItem*>::iterator begin = menu_items.begin();
+  menu_items.erase(begin+pos);
+  gtk_container_remove(GTK_CONTAINER(menu_), menu_item->menu_item_);
+}
+    
+      if (type == 'separator') {
+    menu_item_ = gtk_separator_menu_item_new();
+  } else {
+    if (type == 'checkbox') {
+      menu_item_ = gtk_check_menu_item_new();
+      bool checked;
+      if (option.GetBoolean('checked', &checked))
+        SetChecked(checked);
+    } else {
+      menu_item_ = gtk_image_menu_item_new();
+      std::string icon;
+      if (option.GetString('icon', &icon))
+        SetIcon(icon);
+    }
+    }
+    
+    
+    {  if (menu_)
+    menu_->UpdateStates();
+}
+    
+    class NwMenuGetNSStringFWithFixupFunction : public NWSyncExtensionFunction {
  public:
-  // Computes the updated dual variable (corresponding) to a single example. The
-  // updated dual value maximizes the objective function of the dual
-  // optimization problem associated with smooth hinge loss. The computations
-  // are detailed in readme.md.
-  double ComputeUpdatedDual(const int num_partitions, const double label,
-                            const double example_weight,
-                            const double current_dual, const double wx,
-                            const double weighted_example_norm) const final {
-    // Intutitvely there are 3 cases:
-    // a. new optimal value of the dual variable falls within the admissible
-    // range [0, 1]. In this case we set new dual to this value.
-    // b. new optimal value is < 0. Then, because of convexity, the optimal
-    // valid value for new dual = 0
-    // c. new optimal value > 1.0. Then new optimal value should be set to 1.0.
-    const double candidate_optimal_dual =
-        current_dual +
-        (label - wx - gamma * current_dual) /
-            (num_partitions * example_weight * weighted_example_norm + gamma);
-    if (label * candidate_optimal_dual < 0) {
-      return 0.0;
-    }
-    if (label * candidate_optimal_dual > 1.0) {
-      return label;
-    }
-    return candidate_optimal_dual;
+  NwMenuGetNSStringFWithFixupFunction() {}
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    
+ protected:
+  ~NwMenuGetNSStringFWithFixupFunction() override {}
+    
+  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringFWithFixup', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringFWithFixupFunction);
+};
+    
+    /** Consume entire stream and use its begin/end.
+  * Someday we might have a real StreamReader, but for now this
+  * is convenient.
+  */
+bool JSON_API parseFromStream(
+    CharReader::Factory const&,
+    std::istream&,
+    Value* root, std::string* errs);
+    
+    
+    {  if (!indented_) writeIndent();
+  const std::string& comment = root.getComment(commentBefore);
+  std::string::const_iterator iter = comment.begin();
+  while (iter != comment.end()) {
+    *sout_ << *iter;
+    if (*iter == '\n' &&
+       (iter != comment.end() && *(iter + 1) == '/'))
+      // writeIndent();  // would write extra newline
+      *sout_ << indentString_;
+    ++iter;
   }
-    }
-    
-    
-    {}  // namespace tensorflow
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-      // Size of the sink buffer where the compressed/decompressed data produced by
-  // zlib is cached.
-  int64 output_buffer_size = 256 << 10;
-    
-    
-    
-    namespace grpc {
-    }
-    
-    		FileAccess *f = FileAccess::open(certs_path, FileAccess::READ);
-		if (f) {
-			int flen = f->get_len();
-			out.resize(flen + 1);
-			{
-				PoolByteArray::Write w = out.write();
-				f->get_buffer(w.ptr(), flen);
-				w[flen] = 0; //end f string
-			}
-    }
-    
-    
-    {	return _peer;
+  indented_ = false;
 }
     
-    	Vector<Pair<String, int> > section_line;
-	Map<String, int> method_line;
-	Map<String, int> signal_line;
-	Map<String, int> property_line;
-	Map<String, int> theme_property_line;
-	Map<String, int> constant_line;
-	Map<String, int> enum_line;
-	Map<String, Map<String, int> > enum_values_line;
-	int description_line;
-    
-    public:
-	GodotClosestRayResultCallback(const btVector3 &rayFromWorld, const btVector3 &rayToWorld, const Set<RID> *p_exclude) :
-			btCollisionWorld::ClosestRayResultCallback(rayFromWorld, rayToWorld),
-			m_exclude(p_exclude),
-			m_pickRay(false),
-			m_shapeId(0) {}
-    
-    
-    {	return NULL;
-}
-    
-    void jpeg_decoder::word_clear(void *p, uint16 c, uint n)
-{
-  uint8 *pD = (uint8*)p;
-  const uint8 l = c & 0xFF, h = (c >> 8) & 0xFF;
-  while (n)
-  {
-    pD[0] = l; pD[1] = h; pD += 2;
-    n--;
-  }
-}
-    
-      /// Open the acceptor using the specified protocol.
-  /**
-   * This function opens the socket acceptor so that it will use the specified
-   * protocol.
-   *
-   * @param protocol An object specifying which protocol is to be used.
-   *
-   * @throws boost::system::system_error Thrown on failure.
-   *
-   * @par Example
-   * @code
-   * boost::asio::ip::tcp::acceptor acceptor(io_service);
-   * acceptor.open(boost::asio::ip::tcp::v4());
-   * @endcode
-   */
-  void open(const protocol_type& protocol = protocol_type())
-  {
-    boost::system::error_code ec;
-    this->get_service().open(this->get_implementation(), protocol, ec);
-    boost::asio::detail::throw_error(ec, 'open');
-  }
-    
-    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
-# pragma once
-#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
-    
-      std::size_t check_for_completion(
-      const boost::system::error_code& ec,
-      std::size_t total_transferred)
-  {
-    return detail::adapt_completion_condition_result(
-        completion_condition_(ec, total_transferred));
-  }
-    
-    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
-# pragma once
-#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
-    
-      // Destructor.
-  ~gcc_arm_fenced_block()
-  {
-    barrier();
-  }
-    
-    bool non_blocking_read(int d, buf* bufs, std::size_t count,
-    boost::system::error_code& ec, std::size_t& bytes_transferred)
-{
-  for (;;)
-  {
-    // Read some data.
-    errno = 0;
-    signed_size_type bytes = error_wrapper(::readv(
-          d, bufs, static_cast<int>(count)), ec);
-    }
+    TEST(AnyTest, TestPackAndUnpackAny) {
+  // We can pack a Any message inside another Any message.
+  protobuf_unittest::TestAny submessage;
+  submessage.set_int32_value(12345);
+  google::protobuf::Any any;
+  any.PackFrom(submessage);
+  protobuf_unittest::TestAny message;
+  message.mutable_any_value()->PackFrom(any);
     }
     
-    #include <boost/asio/detail/pop_options.hpp>
+      std::string namespace_;
+  std::string reflectionClassname_;
     
-      // folder with contents returns relative path to test dir
-  ASSERT_OK(env_->CreateDirIfMissing(test_dir_ + '/niu'));
-  ASSERT_OK(env_->CreateDirIfMissing(test_dir_ + '/you'));
-  ASSERT_OK(env_->CreateDirIfMissing(test_dir_ + '/guo'));
-  ASSERT_OK(env_->GetChildren(test_dir_, &children));
-  ASSERT_OK(env_->GetChildrenFileAttributes(test_dir_, &childAttr));
-  ASSERT_EQ(3U, children.size());
-  ASSERT_EQ(3U, childAttr.size());
-  for (auto each : children) {
-    env_->DeleteDir(test_dir_ + '/' + each);
-  }  // necessary for default POSIX env
+    #include <google/protobuf/compiler/csharp/csharp_source_generator_base.h>
+#include <google/protobuf/compiler/csharp/csharp_helpers.h>
+#include <google/protobuf/compiler/csharp/csharp_names.h>
+#include <google/protobuf/compiler/csharp/csharp_options.h>
     
-    class StringAppendOperator : public AssociativeMergeOperator {
- public:
-  // Constructor: specify delimiter
-  explicit StringAppendOperator(char delim_char);
-    }
+    void WriteMessageDocComment(io::Printer* printer, const Descriptor* message);
+void WriteFieldDocComment(io::Printer* printer, const FieldDescriptor* field);
+void WriteEnumDocComment(io::Printer* printer, const EnumDescriptor* enum_);
+void WriteEnumValueDocComment(io::Printer* printer,
+                              const EnumValueDescriptor* value);
+void WriteServiceDocComment(io::Printer* printer,
+                            const ServiceDescriptor* service);
+void WriteMethodDocComment(io::Printer* printer,
+                           const MethodDescriptor* method);
     
-      /// Push / Insert value at beginning/end of the list. Return the length.
-  /// May throw RedisListException
-  int PushLeft(const std::string& key, const std::string& value);
-  int PushRight(const std::string& key, const std::string& value);
     
-      StatisticsJni::StatisticsJni(std::shared_ptr<Statistics> stats)
-      : StatisticsImpl(stats, false), m_ignore_histograms() {
-  }
-    
-    #include <memory>
-#include <set>
-#include <string>
-#include 'rocksdb/statistics.h'
-#include 'monitoring/statistics.h'
+    { private:
+  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(JavaGenerator);
+};
     
     
     {
-    {  void ClearCallBack(const std::string& point);
-  void ClearAllCallBacks();
-  void EnableProcessing() {
-    std::lock_guard<std::mutex> lock(mutex_);
-    enabled_ = true;
-  }
-  void DisableProcessing() {
-    std::lock_guard<std::mutex> lock(mutex_);
-    enabled_ = false;
-  }
-  void ClearTrace() {
-    std::lock_guard<std::mutex> lock(mutex_);
-    cleared_points_.clear();
-  }
-  bool DisabledByMarker(const std::string& point,
-                        std::thread::id thread_id) {
-    auto marked_point_iter = marked_thread_id_.find(point);
-    return marked_point_iter != marked_thread_id_.end() &&
-           thread_id != marked_point_iter->second;
-  }
-  void Process(const std::string& point, void* cb_arg);
+    {}}
+    
+    #ifndef OPENCV_CUDA_EMULATION_HPP_
+#define OPENCV_CUDA_EMULATION_HPP_
+    
+    CV_EXPORTS_W void multiply(InputArray src1, Scalar src2, OutputArray dst, double scale=1, int dtype=-1);
+    
+      /**
+   * @brief Applies the transformation defined in the data layer's
+   * transform_param block to a cv::Mat
+   *
+   * @param cv_img
+   *    cv::Mat containing the data to be transformed.
+   * @param transformed_blob
+   *    This is destination blob. It can be part of top blob's data if
+   *    set_cpu_data() is used. See image_data_layer.cpp for an example.
+   */
+  void Transform(const cv::Mat& cv_img, Blob<Dtype>* transformed_blob);
+#endif  // USE_OPENCV
+    
+    
+    {}  // namespace caffe
+    
+    
+    {}  // namespace caffe
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    
+    {  bool handles_setup_;
+  cudnnHandle_t             handle_;
+  cudnnTensorDescriptor_t bottom_desc_, top_desc_;
+  cudnnPoolingDescriptor_t  pooling_desc_;
+  cudnnPoolingMode_t        mode_;
 };
+#endif
+    
+    namespace caffe {
+    }
+    
+    #endif  // CAFFE_CUDNN_TANH_LAYER_HPP_
+
+    
+    
+    {}  // namespace caffe
+    
+     protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #include <vector>
+    
+    bool js_cocos2dx_navmesh_NavMeshObstacle_constructor(JSContext *cx, uint32_t argc, jsval *vp);
+void js_cocos2dx_navmesh_NavMeshObstacle_finalize(JSContext *cx, JSObject *obj);
+void js_register_cocos2dx_navmesh_NavMeshObstacle(JSContext *cx, JS::HandleObject global);
+void register_all_cocos2dx_navmesh(JSContext* cx, JS::HandleObject obj);
+bool js_cocos2dx_navmesh_NavMeshObstacle_getSyncFlag(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_initWith(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_syncToObstacle(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_syncToNode(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_getHeight(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_setSyncFlag(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_getRadius(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_create(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_getNavMeshObstacleComponentName(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_navmesh_NavMeshObstacle_NavMeshObstacle(JSContext *cx, uint32_t argc, jsval *vp);
+    
+    bool js_cocos2dx_studio_ZOrderFrame_constructor(JSContext *cx, uint32_t argc, jsval *vp);
+void js_cocos2dx_studio_ZOrderFrame_finalize(JSContext *cx, JSObject *obj);
+void js_register_cocos2dx_studio_ZOrderFrame(JSContext *cx, JS::HandleObject global);
+void register_all_cocos2dx_studio(JSContext* cx, JS::HandleObject obj);
+bool js_cocos2dx_studio_ZOrderFrame_getZOrder(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ZOrderFrame_setZOrder(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ZOrderFrame_create(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_ZOrderFrame_ZOrderFrame(JSContext *cx, uint32_t argc, jsval *vp);
+    
+    #ifdef __cplusplus
+extern 'C' {
+#endif
+#include 'tolua++.h'
+#ifdef __cplusplus
 }
-#endif // NDEBUG
+#endif
+    
+    #endif // __cocos2dx_experimental_video_h__
+#endif //#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_IOS) && !defined(CC_TARGET_OS_TVOS)
+
+    
+    			bd.position.Set(1.0f, 0.4f);
+			m_wheel2 = m_world->CreateBody(&bd);
+			m_wheel2->CreateFixture(&fd);
+    
+    #define ASSERT_BUF_SIZE 4096
+static char sAssertBuf[ASSERT_BUF_SIZE];
+static AssertHandler gAssertHandler;
+    
+      template <typename ...Args>
+  void initialize(Args&&... arguments) {
+    FBASSERT(!m_instance);
+    m_instance = new T(std::forward<Args>(arguments)...);
+  }
