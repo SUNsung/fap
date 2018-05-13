d8fa22f858da8934c893e6b35b98eee045b3a7f8
SUNsung <weatherius@gmail.com>
2018-05-13 16:31:33 +0300

    Rewrite [53wdC.W5QjTLI53uEskFTVYW4I53KPqLGg1La2I53laL.py] File in: 2018-05-13 16:31:32

--- pyton/53wdC.W5QjTLI53uEskFTVYW4I53KPqLGg1La2I53laL.py
+++ pyton/53wdC.W5QjTLI53uEskFTVYW4I53KPqLGg1La2I53laL.py
@@ -1,84 +1,85 @@
 
         
-                return self.__dict__.get(key, None)
+            def _migrate_implicit_content_type(self):
+        '''Migrate the removed implicit_content_type config option'''
+        try:
+            implicit_content_type = self.pop('implicit_content_type')
+        except KeyError:
+            self.save()
+        else:
+            if implicit_content_type == 'form':
+                self['default_options'].insert(0, '--form')
+            self.save()
+            self.load()
+
     
     
-def test_idna_without_version_attribute(mocker):
-    '''Older versions of IDNA don't provide a __version__ attribute, verify
-    that if we have such a package, we don't blow up.
+class ExitStatus:
+    '''Exit status code constants.'''
+    OK = 0
+    ERROR = 1
+    PLUGIN_ERROR = 7
+    
+    ratio = scikits_time / scipy_time
+    
+    import gc
+import sys
+import optparse
+from datetime import datetime
+import operator
+    
+        if not os.path.exists(ARCHIVE_NAME):
+        print('Downloading dataset from %s (3 MB)' % URL)
+        opener = urlopen(URL)
+        with open(ARCHIVE_NAME, 'wb') as archive:
+            archive.write(opener.read())
+    
+    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
+TRAIN_FOLDER = '20news-bydate-train'
+TEST_FOLDER = '20news-bydate-test'
+    
     '''
-    mocker.patch('requests.help.idna', new=None)
-    assert info()['idna'] == {'version': ''}
+# Author: Olivier Grisel <olivier.grisel@ensta.org>
+# License: Simplified BSD
     
-        def test_different_encodings_dont_break_post(self, httpbin):
-        r = requests.post(httpbin('post'),
-            data={'stuff': json.dumps({'a': 123})},
-            params={'blah': 'asdf1234'},
-            files={'file': ('test_requests.py', open(__file__, 'rb'))})
-        assert r.status_code == 200
+            if event == 'clear':
+            self.ax.clear()
+            self.ax.set_xticks([])
+            self.ax.set_yticks([])
+            self.contours = []
+            self.c_labels = None
+            self.plot_kernels()
     
-            self.concurrent -= 1
-        return ''
+    from sklearn.datasets import make_biclusters
+from sklearn.datasets import samples_generator as sg
+from sklearn.cluster.bicluster import SpectralCoclustering
+from sklearn.metrics import consensus_score
     
-        def from_method(self, method, results):
-        contracts = self.extract_contracts(method)
-        if contracts:
-            # calculate request args
-            args, kwargs = get_spec(Request.__init__)
-            kwargs['callback'] = method
-            for contract in contracts:
-                kwargs = contract.adjust_request_args(kwargs)
+    plt.title('Change of predicted probabilities after sigmoid calibration')
+plt.xlabel('Probability class 1')
+plt.ylabel('Probability class 2')
+plt.xlim(-0.05, 1.05)
+plt.ylim(-0.05, 1.05)
+plt.legend(loc='best')
     
-            assertion = (self.min_bound <= occurrences <= self.max_bound)
+    n_classifiers = len(classifiers)
     
-    print()
+    def kugou_download_by_hash(title,hash_val,output_dir = '.', merge = True, info_only = False):
+    #sample
+    #url_sample:http://www.kugou.com/yy/album/single/536957.html
+    #hash ->key  md5(hash+kgcloud')->key  decompile swf
+    #cmd 4 for mp3 cmd 3 for m4a
+    key=hashlib.new('md5',(hash_val+'kgcloud').encode('utf-8')).hexdigest()
+    html=get_html('http://trackercdn.kugou.com/i/?pid=6&key=%s&acceptMp3=1&cmd=4&hash=%s'%(key,hash_val))
+    j=loads(html)
+    url=j['url']
+    songtype, ext, size = url_info(url)
+    print_info(site_info, title, songtype, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir, merge=merge)
     
-        current_ref = get_current_ref()
-    base_ref = get_merge_base()
-    if options.files:
-        files = options.files
+            print_info(site_info, title, type_, size_full)
+        if not info_only:
+            download_urls(url_list, title, ext, total_size=size_full, output_dir=output_dir, merge=merge, headers=fake_headers)
     else:
-        files = get_changed_files(base_ref, current_ref)
-        logging.debug('files changed: %r', files)
-    
-    
-def register_api_templates(template_name, template_class):
-    for style in ('api', 'api-html', 'api-compact'):
-        tpm.add_handler(
-            name=template_name,
-            style=style,
-            handler=template_class,
-        )
-    
-    from r2.controllers.api_docs import api_doc, api_section
-from r2.controllers.oauth2 import require_oauth2_scope
-from r2.controllers.reddit_base import OAuth2OnlyController
-from r2.controllers.ipn import send_gift
-from r2.lib.errors import RedditError
-from r2.lib.validator import (
-    validate,
-    VAccountByName,
-    VByName,
-    VInt,
-    VNotInTimeout,
-)
-from r2.models import Account, Comment, Link, NotFound
-from r2.models.gold import creddits_lock
-from r2.lib.validator import VUser
-    
-                code =  request.GET.get('code', '')
-            try:
-                code = int(code)
-            except ValueError:
-                code = 404
-            srname = request.GET.get('srname', '')
-            takedown = request.GET.get('takedown', '')
-            error_name = request.GET.get('error_name', '')
-    
-    
-class GoogleTagManagerController(MinimalController):
-    def pre(self):
-        if request.host != g.media_domain:
-            # don't serve up untrusted content except on our
-            # specifically untrusted domain
-            self.abort404()
+        raise NotImplementedError(flashvars)
