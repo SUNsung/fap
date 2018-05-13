b1fb9c3b1918b0d725c949924d6667eac14661be
SUNsung <weatherius@gmail.com>
2018-05-13 22:02:40 +0300

    Rewrite [057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp] File in: 2018-05-13 22:02:40

--- cccccc/057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp
+++ cccccc/057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp
@@ -1,116 +1,147 @@
 
         
-        cv::Mat dls::LeftMultVec(const cv::Mat& v)
+          /// Keep emitting subsequent diagnostics after a fatal error.
+  bool ShowDiagnosticsAfterFatalError = false;
+    
+    #include 'swift/Basic/LLVM.h'
+#include 'swift/AST/DiagnosticConsumer.h'
+    
+      /// Returns the buffer ID for the specified *valid* location.
+  ///
+  /// Because a valid source location always corresponds to a source buffer,
+  /// this routine always returns a valid buffer ID.
+  unsigned findBufferContainingLoc(SourceLoc Loc) const;
+    
+    
+    {
+    {}  // namespace
+}  // namespace tesseract
+
+    
+      // Suspends/Enables training by setting the training_ flag. Serialize and
+  // DeSerialize only operate on the run-time data if state is false.
+  void SetEnableTraining(TrainingState state) override;
+    
+      // Writes to the given file. Returns false in case of error.
+  bool Serialize(TFile* fp) const override;
+  // Reads from the given file. Returns false in case of error.
+  bool DeSerialize(TFile* fp) override;
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you use this binding you'll need to call 4 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXXX_NewFrame(), ImGui::Render() and ImGui_ImplXXXX_Shutdown().
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you use this binding you'll need to call 4 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXXX_NewFrame(), ImGui::Render() and ImGui_ImplXXXX_Shutdown().
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+    // Implemented features:
+//  [X] User texture binding. Cast 'GLuint' OpenGL texture identifier as void*/ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
+// Missing features:
+//  [ ] SDL2 handling of IME under Windows appears to be broken and it explicitly disable the regular Windows IME. You can restore Windows IME by compiling SDL with SDL_DISABLE_WINDOWS_IME.
+    
+    #if defined(__GNUC__)
+#pragma GCC diagnostic ignored '-Wunused-function'          // warning: 'xxxx' defined but not used
+#endif
+    
+    //---- Use 32-bit vertex indices (default is 16-bit) to allow meshes with more than 64K vertices. Render function needs to support it.
+//#define ImDrawIdx unsigned int
+    
+        // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them. 
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple. 
+    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Read 'misc/fonts/README.txt' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Roboto-Medium.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Cousine-Regular.ttf', 15.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/DroidSans.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/ProggyTiny.ttf', 10.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF('c:\\Windows\\Fonts\\ArialUni.ttf', 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+    //IM_ASSERT(font != NULL);
+    
+    void CleanupDeviceD3D()
 {
-    cv::Mat mat_ = cv::Mat::zeros(3, 9, CV_64F);
-    }
-    
-    
-    {    // temporary solution, have to make something more precise
-    corner = contour[min_idx];
-    return;
+    CleanupRenderTarget();
+    if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = NULL; }
+    if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = NULL; }
 }
     
-            if (lane < 16)
-        {
-            T partial = ptr[tid];
+        // Setup Dear ImGui binding
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+    ImGui_ImplDX12_Init(hwnd, NUM_FRAMES_IN_FLIGHT, g_pd3dDevice,
+        DXGI_FORMAT_R8G8B8A8_UNORM,
+        g_pd3dSrvDescHeap->GetCPUDescriptorHandleForHeapStart(),
+        g_pd3dSrvDescHeap->GetGPUDescriptorHandleForHeapStart());
+    
+            // Rendering
+        IwGxSetColClear(clear_color.x * 255, clear_color.y * 255, clear_color.z * 255, clear_color.w * 255);
+        IwGxClear();
+        ImGui::Render();
+        ImGui_Marmalade_RenderDrawData(ImGui::GetDrawData());
+        IwGxSwapBuffers();
+    
+    int main(int, char**)
+{
+    // Setup window
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    GLFWwindow* window = glfwCreateWindow(1280, 720, 'ImGui GLFW+OpenGL3 example', NULL, NULL);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+    gl3wInit();
     }
     
-    ///////////////////////////////////////////////////////////////////////////////
-    
-    /*
- * Vlabel wraps a block number.
- */
-DECLARE_VNUM(Vlabel, true, 'B');
-    
-      std::vector<LdSSwitchData::Elm> cases(numCases);
-  for (int i = 0; i < numCases; ++i) {
-    auto const& kv = iv.strvec()[i];
-    cases[i].str  = curUnit(env)->lookupLitstrId(kv.str);
-    cases[i].dest = SrcKey{curSrcKey(env), bcOff(env) + kv.dest};
-  }
-    
-    //////////////////////////////////////////////////////////////////////
-    
-    
-void Instruction::SetBranchImmTarget(Instruction* target) {
-  assert(((target - this) & 3) == 0);
-  Instr branch_imm = 0;
-  uint32_t imm_mask = 0;
-  int offset = (target - this) >> kInstructionSizeLog2;
-  switch (BranchType()) {
-    case CondBranchType: {
-      branch_imm = Assembler::ImmCondBranch(offset);
-      imm_mask = ImmCondBranch_mask;
-      break;
+    TEST_F(YogaTest_HadOverflowTests, spacing_overflow_in_nested_nodes) {
+  const YGNodeRef child0 = YGNodeNewWithConfig(config);
+  YGNodeStyleSetWidth(child0, 80);
+  YGNodeStyleSetHeight(child0, 40);
+  YGNodeStyleSetMargin(child0, YGEdgeTop, 10);
+  YGNodeStyleSetMargin(child0, YGEdgeBottom, 10);
+  YGNodeInsertChild(root, child0, 0);
+  const YGNodeRef child1 = YGNodeNewWithConfig(config);
+  YGNodeStyleSetWidth(child1, 80);
+  YGNodeStyleSetHeight(child1, 40);
+  YGNodeInsertChild(root, child1, 1);
+  const YGNodeRef child1_1 = YGNodeNewWithConfig(config);
+  YGNodeStyleSetWidth(child1_1, 80);
+  YGNodeStyleSetHeight(child1_1, 40);
+  YGNodeStyleSetMargin(child1_1, YGEdgeBottom, 5);
+  YGNodeInsertChild(child1, child1_1, 0);
     }
-    case UncondBranchType: {
-      branch_imm = Assembler::ImmUncondBranch(offset);
-      imm_mask = ImmUncondBranch_mask;
-      break;
-    }
-    case CompareBranchType: {
-      branch_imm = Assembler::ImmCmpBranch(offset);
-      imm_mask = ImmCmpBranch_mask;
-      break;
-    }
-    case TestBranchType: {
-      branch_imm = Assembler::ImmTestBranch(offset);
-      imm_mask = ImmTestBranch_mask;
-      break;
-    }
-    default: not_reached();
-  }
-  SetInstructionBits(Mask(~imm_mask) | branch_imm);
+    
+    void setAssertHandler(AssertHandler assertHandler) {
+    gAssertHandler = assertHandler;
 }
     
-    bool get_embedded_data(const char* section, embedded_data* desc,
-                       const std::string& filename = '');
+      bool hasOnlyOneRef() const {
+    return m_refcount == 1;
+  }
     
-      static FILE *popen(const char *cmd, const char *type,
-                     const char *cwd = nullptr);
-  static int pclose(FILE *f);
-    
-            std::map<wstring, shared_ptr<IStatefulNode>> EnumerateStatefulNode(ComputationNetwork& net,
-                                                                           const std::vector<ComputationNodeBasePtr>& criterionNode,
-                                                                           const std::vector<ComputationNodeBasePtr>& evaluationNode)
-        {
-            std::map<wstring, shared_ptr<IStatefulNode>> statefulNodes;
-            for (auto& root : criterionNode)
-                EnumerateStatefulNodesForRoot(net, root, statefulNodes);
-            for (auto& root : evaluationNode)
-                EnumerateStatefulNodesForRoot(net, root, statefulNodes);
-            return statefulNodes;
-        }
-    
-        const OperatorSchema* OpSchemaRegistry::Schema(
-        const std::string& p_key,
-        const int p_maxInclusiveVersion,
-        const std::string& p_domain)
-    {
-        auto& m = map();
-        if (m.count(p_key) && m[p_key].count(p_domain))
-        {
-            auto pos = m[p_key][p_domain].lower_bound(p_maxInclusiveVersion);
-            if (m[p_key][p_domain].begin() == pos && pos->first > p_maxInclusiveVersion)
-            {
-                // All versions are greater than specified version.
-                return nullptr;
-            }
+    namespace facebook {
+namespace jni {
     }
     }
     
-        REGISTER_ELEMENTWISE_BROADCAST_OPERATOR_SCHEMA(Add)
-        REGISTER_ELEMENTWISE_BROADCAST_OPERATOR_SCHEMA(Sub)
-        REGISTER_ELEMENTWISE_BROADCAST_OPERATOR_SCHEMA(Mul)
-        REGISTER_ELEMENTWISE_BROADCAST_OPERATOR_SCHEMA(Div)
-    
-        virtual void CopyTo(ComputationNodeBasePtr nodeP, const std::wstring& newName, const CopyNodeFlags flags) const override
-    {
-        Base::CopyTo(nodeP, newName, flags);
+    namespace facebook {
     }
     
-        // Let's check that there is no outstanding copies.
-    // Wait on all events if there are any pending copy operations in flight.
-    if (m_dataTransferers[m_currentDataTransferIndex])
-        m_dataTransferers[m_currentDataTransferIndex]->WaitForCopyCPUToGPU();
+      T* release() {
+    T* obj = get();
+    pthread_setspecific(m_key, NULL);
+    return obj;
+  }
