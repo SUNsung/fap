ee7b148e7bd621df56429b7902dba10ff73afa70
SUNsung <weatherius@gmail.com>
2018-05-14 01:13:25 +0300

    Rewrite [02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py] File in: 2018-05-14 01:13:25

--- pyton/02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py
+++ pyton/02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py
@@ -1,132 +1,52 @@
 
         
-        entries = []
-for v in versions:
-    fields = v.split('.')
-    year, month, day = map(int, fields[:3])
-    faked = 0
-    patchlevel = 0
-    while True:
+        codes = LookupDict(name='status_codes')
+    
+        @pytest.mark.parametrize(
+        'encoding', (
+            'utf-32', 'utf-8-sig', 'utf-16', 'utf-8', 'utf-16-be', 'utf-16-le',
+            'utf-32-be', 'utf-32-le'
+        ))
+    def test_encoded(self, encoding):
+        data = '{}'.encode(encoding)
+        assert guess_json_utf(data) == encoding
+    
+        def process_options(self, args, opts):
         try:
-            datetime.date(year, month, day)
+            self.settings.setdict(arglist_to_dict(opts.set),
+                                  priority='cmdline')
         except ValueError:
-            day -= 1
-            faked += 1
-            assert day > 0
-            continue
-        break
-    if len(fields) >= 4:
-        try:
-            patchlevel = int(fields[3])
-        except ValueError:
-            patchlevel = 1
-    timestamp = '%04d-%02d-%02dT00:%02d:%02dZ' % (year, month, day, faked, patchlevel)
+            raise UsageError('Invalid -s value, use -s NAME=VALUE', print_help=False)
     
+        def short_desc(self):
+        return 'Run quick benchmark test'
     
-def gettestcases(include_onlymatching=False):
-    for ie in youtube_dl.extractor.gen_extractors():
-        for tc in ie.get_testcases(include_onlymatching):
-            yield tc
-    
-    
-def get_info_extractor(ie_name):
-    '''Returns the info extractor class with the given ie_name'''
-    return globals()[ie_name + 'IE']
+        name = '%s_%s' % (spider, method.__name__)
+    setattr(ContractTestCase, name, lambda x: x)
+    return ContractTestCase(name)
 
     
-            title = self._html_search_regex(r'<title>(.*?)</title>', webpage, 'title')
-        description = self._html_search_regex(
-            r'<div class='description'[^>]*>([^<]+)</div>', webpage, 'description', fatal=False)
-        thumbnail = self._html_search_regex(
-            r'preview_url\s*:\s*\'(.*?)\'', webpage, 'thumbnail', fatal=False)
+    def _process_worker(call_queue, result_queue):
+    '''Evaluates calls from call_queue and places the results in result_queue.
+    
+      try:
+    filepath = os.path.join( DIR_OF_YCMD, 'PYTHON_USED_DURING_BUILDING' )
+    return utils.ReadFile( filepath ).strip()
+  # We need to check for IOError for Python2 and OSError for Python3
+  except ( IOError, OSError ):
+    return None
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
     
     
-@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
-@pytest.mark.parametrize('filename', filenames)
-def test_rst_file_syntax(filename):
-    p = subprocess.Popen(
-        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE
-    )
-    err = p.communicate()[1]
-    assert p.returncode == 0, err.decode('utf8')
-
-    
-            final_response = get_response(args, config_dir=env.config.directory)
-        if args.all:
-            responses = final_response.history + [final_response]
-        else:
-            responses = [final_response]
-    
-    
-# noinspection PyAbstractClass
-class BuiltinAuthPlugin(AuthPlugin):
-    
-        for factor, suffix in abbrevs:
-        if n >= factor:
-            break
-    
-        # Auth
-    def get_auth_plugins(self):
-        return [plugin for plugin in self if issubclass(plugin, AuthPlugin)]
-    
-    try:
-    import argcomplete
-except ImportError:
-    argcomplete = None
-    
-            expected = {
-            'name': 'myhealthcheck',
-            'checkIntervalSec': 5,
-            'port': 443,
-            'unhealthyThreshold': 2,
-            'healthyThreshold': 2,
-            'host': '',
-            'timeoutSec': 5,
-            'requestPath': '/'}
-    
-        def construct_vault_encrypted_unicode(self, node):
-        value = self.construct_scalar(node)
-        b_ciphertext_data = to_bytes(value)
-        # could pass in a key id here to choose the vault to associate with
-        # TODO/FIXME: plugin vault selector
-        vault = self._vaults['default']
-        if vault.secrets is None:
-            raise ConstructorError(context=None, context_mark=None,
-                                   problem='found !vault but no vault password provided',
-                                   problem_mark=node.start_mark,
-                                   note=None)
-        ret = AnsibleVaultEncryptedUnicode(b_ciphertext_data)
-        ret.vault = vault
-        return ret
-    
-    
-collect_ignore = [
-    # deprecated or moved modules
-    'scrapy/conf.py',
-    'scrapy/stats.py',
-    'scrapy/project.py',
-    'scrapy/utils/decorator.py',
-    'scrapy/statscol.py',
-    'scrapy/squeue.py',
-    'scrapy/log.py',
-    'scrapy/dupefilter.py',
-    'scrapy/command.py',
-    'scrapy/linkextractor.py',
-    'scrapy/spider.py',
-    
-    # Ignore noisy twisted deprecation warnings
-import warnings
-warnings.filterwarnings('ignore', category=DeprecationWarning, module='twisted')
-del warnings
-    
-    from scrapy.utils.conf import arglist_to_dict
-from scrapy.exceptions import UsageError
-    
-        def tested_methods_from_spidercls(self, spidercls):
-        methods = []
-        for key, value in vars(spidercls).items():
-            if (callable(value) and value.__doc__ and
-                    re.search(r'^\s*@', value.__doc__, re.MULTILINE)):
-                methods.append(key)
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
