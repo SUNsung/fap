a0b56393df41cdc6d0a92d415df1537de438dd04
SUNsung <weatherius@gmail.com>
2018-05-14 13:55:12 +0300

    Rewrite [27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py] File in: 2018-05-14 13:55:11

--- pyton/27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py
+++ pyton/27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py
@@ -1,66 +1,83 @@
 
         
         
-class Word2VecTest(tf.test.TestCase):
+def check_alphabetical(lines):
+    '''
+    checks if all entries per section are in alphabetical order based in entry title
+    '''
+    sections = {}
+    section_line_num = {}
+    for line_num, line in enumerate(lines):
+        if line.startswith(anchor):
+            category = line.split(anchor)[1].strip()
+            sections[category] = []
+            section_line_num[category] = line_num
+            continue
+        if not line.startswith('|') or line.startswith('|---'):
+            continue
+        title = [x.strip() for x in line.split('|')[1:-1]][0].upper()
+        sections[category].append(title)
+    
+        base = 'src/sentry/locale'
+    for locale in os.listdir(base):
+        fn = os.path.join(base, locale, 'LC_MESSAGES', 'django.po')
+        if not os.path.isfile(fn):
+            continue
+    
+    from tornado.options import define, options
+    
+        .. versionchanged:: 5.0:
+       Unified ``tornado.gen.TimeoutError`` and
+       ``tornado.ioloop.TimeoutError`` as ``tornado.util.TimeoutError``.
+       Both former names remain as aliases.
+    '''
     
     
-class Word2VecTest(tf.test.TestCase):
+def wrap_web_tests_application():
+    result = {}
+    for cls in web_test.wsgi_safe_tests:
+        def class_factory():
+            class WSGIApplicationWrappedTest(cls):  # type: ignore
+                def setUp(self):
+                    self.warning_catcher = ignore_deprecation()
+                    self.warning_catcher.__enter__()
+                    super(WSGIApplicationWrappedTest, self).setUp()
     
-        self.assertEqual(captured, set())
+    import twisted.internet.abstract  # type: ignore
+from twisted.internet.defer import Deferred  # type: ignore
+from twisted.internet.posixbase import PosixReactorBase  # type: ignore
+from twisted.internet.interfaces import IReactorFDSet, IDelayedCall, IReactorTime, IReadDescriptor, IWriteDescriptor  # type: ignore # noqa: E501
+from twisted.python import failure, log  # type: ignore
+from twisted.internet import error  # type: ignore
+import twisted.names.cache  # type: ignore
+import twisted.names.client  # type: ignore
+import twisted.names.hosts  # type: ignore
+import twisted.names.resolve  # type: ignore
     
-      def cmd_loadsgf(self, arguments):
-    args = arguments.split()
-    if len(args) == 2:
-      file_, movenum = args
-      movenum = int(movenum)
-      print('movenum =', movenum, file=sys.stderr)
-    else:
-      file_ = args[0]
-      movenum = None
+        def _release(self):
+        if self.release_callback is not None:
+            release_callback = self.release_callback
+            self.release_callback = None
+            release_callback()
     
-        # and even after injecting noise, we should still not select an illegal move
-    for _ in range(10):
-      root.inject_noise()
-      leaf = root.select_leaf()
-      self.assertNotEqual(leaf.fmove, 1)
+        return exc
     
-      It does NOT return the very final position, as there is no follow up.
-  To get the final position, call pwc.position.play_move(pwc.next_move)
-  on the last PositionWithContext returned.
-  Example usage:
-  with open(filename) as f:
-    for position_w_context in replay_sgf(f.read()):
-      print(position_w_context.position)
+        def test_remove_without_add(self):
+        # remove_handler should not throw an exception if called on an fd
+        # was never added.
+        sock, port = bind_unused_port()
+        try:
+            self.io_loop.remove_handler(sock.fileno())
+        finally:
+            sock.close()
     
-      def should_resign(self):
-    '''Returns true if the player resigned.
     
-      def test_proper_move_transform(self):
-    # Check that the reinterpretation of 362 = 19*19 + 1 during symmetry
-    # application is consistent with coords.from_flat
-    move_array = np.arange(utils_test.BOARD_SIZE ** 2 + 1)
-    coord_array = np.zeros([utils_test.BOARD_SIZE, utils_test.BOARD_SIZE])
-    for c in range(utils_test.BOARD_SIZE ** 2):
-      coord_array[coords.from_flat(utils_test.BOARD_SIZE, c)] = c
-    for s in symmetries.SYMMETRIES:
-      with self.subTest(symmetry=s):
-        transformed_moves = symmetries.apply_symmetry_pi(
-            utils_test.BOARD_SIZE, s, move_array)
-        transformed_board = symmetries.apply_symmetry_feat(s, coord_array)
-        for new_coord, old_coord in enumerate(transformed_moves[:-1]):
-          self.assertEqual(
-              old_coord,
-              transformed_board[
-                  coords.from_flat(utils_test.BOARD_SIZE, new_coord)])
-    
-    # Windows users: You only need to change PATH, rest is platform independent
-PATH = '/tmp/tf_custom_estimators'
-    
-        def syntax(self):
-        return '[options] <spider>'
-    
-        @property
-    def templates_dir(self):
-        _templates_base_dir = self.settings['TEMPLATES_DIR'] or \
-            join(scrapy.__path__[0], 'templates')
-        return join(_templates_base_dir, 'spiders')
+class AsyncTestCaseTest(AsyncTestCase):
+    def test_exception_in_callback(self):
+        with ignore_deprecation():
+            self.io_loop.add_callback(lambda: 1 / 0)
+            try:
+                self.wait()
+                self.fail('did not get expected exception')
+            except ZeroDivisionError:
+                pass
