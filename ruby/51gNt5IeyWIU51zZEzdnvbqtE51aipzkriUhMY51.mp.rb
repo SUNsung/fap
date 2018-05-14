c868acfd79eb2efe6bb8ec14823f1db22a70b936
SUNsung <weatherius@gmail.com>
2018-05-14 13:38:48 +0300

    Rewrite [51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb] File in: 2018-05-14 13:38:48

--- ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
+++ ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
@@ -1,141 +1,99 @@
 
         
-        Benchmark.ips do |x|
-  x.report('no body include?') { CONTENT_NOT_CONTAINING.include?('<body') }
-  x.report('no body regexp')   { CONTENT_NOT_CONTAINING =~ /<\s*body/ }
-  x.compare!
-end
+          class FeatureTopicUsers < Jobs::Base
     
-    #
-    
-    html_readme = '<html>#{Kramdown::Document.new(open('README.md').read).to_html}</html>'
-readme_doctree = REXML::Document.new(html_readme)
-links = REXML::XPath.match(readme_doctree, '//a')
-    
-      def start_worker?
-    true
-  end
-    
-      def edit
-    @user_credential = current_user.user_credentials.find(params[:id])
-  end
-    
-      def test_untainted_path
-    bug5374 = '[ruby-core:39745]'
-    cwd = ('./'*40+'.'.taint).dup.untaint
-    in_safe = proc {|safe| $SAFE = safe; File.stat(cwd)}
-    assert_not_send([cwd, :tainted?])
-    (0..1).each do |level|
-      assert_nothing_raised(SecurityError, bug5374) {in_safe[level]}
+            expect(result).to eq('git svn info | grep Revision | egrep -o '[0-9]+'')
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('git svn info | grep Revision | egrep -o '[0-9]+'')
+      end
     end
-  ensure
-    $SAFE = 0
-  end
     
-      def test_hash2
-    assert_kind_of(Integer, @cls[].hash)
-    h = @cls[1=>2]
-    h.shift
-    assert_equal({}.hash, h.hash, '[ruby-core:38650]')
-    bug9231 = '[ruby-core:58993] [Bug #9231]'
-    assert_not_equal(0, @cls[].hash, bug9231)
-  end
+          protected
     
-        set = Set.new(1..10)
-    enum = set.classify
+            while true
+          begin
+            if Platform.windows?
+              # Windows doesn't support non-blocking reads on
+              # file descriptors or pipes so we have to get
+              # a bit more creative.
     
-      it 'decodes NaN' do
-    # mumble mumble NaN mumble https://bugs.ruby-lang.org/issues/5884
-    [nan_value].pack(unpack_format).unpack(unpack_format).first.nan?.should be_true
-  end
+          # This deletes the block with the given key if it exists.
+      def delete(key)
+        key    = Regexp.quote(key)
+        regexp = /^#\s*VAGRANT-BEGIN:\s*#{key}$.*^#\s*VAGRANT-END:\s*#{key}$\r?\n?/m
+        @value.gsub!(regexp, '')
+      end
     
-      it 'decodes the remaining ints when passed the '*' modifier' do
-    'acbdegfh'.unpack(unpack_format('*')).should == [1684169569, 1751541605]
-  end
+              box_name     = argv[0]
+          box_provider = argv[1].to_sym
+          box_version  = argv[2]
     
-        t.join
-  end
-    
-        t.join
-  end
-    
-      #forward some requests to status message, because a poll is just attached to a status message and is not sharable itself
-  delegate :author_id, :diaspora_handle, :public?, :subscribers, to: :status_message
-    
-      def create_hash(model, opts={})
-    opts[:range] ||= 1.day
-    plural = model.to_s.underscore.pluralize
-    eval(<<DATA
-      @#{plural} = {
-        :day_before => #{model}.where(:created_at => ((Time.now.midnight - #{opts[:range]*2})..Time.now.midnight - #{opts[:range]})).count,
-        :yesterday => #{model}.where(:created_at => ((Time.now.midnight - #{opts[:range]})..Time.now.midnight)).count
-      }
-      @#{plural}[:change] = percent_change(@#{plural}[:yesterday], @#{plural}[:day_before])
-DATA
-    )
-  end
-    
-          def handle_prompt_with_signed_in_user
-        client_id = params[:client_id]
-        if client_id
-          auth = Api::OpenidConnect::Authorization.find_by_client_id_user_and_scopes(client_id,
-                                                                                     current_user, params[:scope])
-          if auth
-            process_authorization_consent('true')
-          else
-            handle_params_error('interaction_required', 'User must already be authorized when `prompt` is `none`')
-          end
+            # Get the proper capability host to check
+        cap_host = nil
+        if type == :host
+          cap_host = @env.host
         else
-          handle_params_error('bad_request', 'Client ID is missing from request')
+          with_target_vms([]) do |vm|
+            cap_host = case type
+                       when :provider
+                         vm.provider
+                       when :guest
+                         vm.guest
+                       else
+                         raise Vagrant::Errors::CLIInvalidUsage,
+                           help: opts.help.chomp
+                       end
+          end
+        end
+    
+            def run
+          if @pod_name.nil?
+            # Note: at that point, @wipe_all is always true (thanks to `validate!`)
+            # Remove all
+            clear_cache
+          else
+            # Remove only cache for this pod
+            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
+            if cache_descriptors.nil?
+              UI.notice('No cache for pod named #{@pod_name} found')
+            elsif cache_descriptors.count > 1 && !@wipe_all
+              # Ask which to remove
+              choices = cache_descriptors.map { |c| '#{@pod_name} v#{c[:version]} (#{pod_type(c)})' }
+              index = UI.choose_from_array(choices, 'Which pod cache do you want to remove?')
+              remove_caches([cache_descriptors[index]])
+            else
+              # Remove all found cache of this pod
+              remove_caches(cache_descriptors)
+            end
+          end
+        end
+    
+          def update_if_necessary!
+        if @update && config.verbose?
+          UI.section('\nUpdating Spec Repositories\n'.yellow) do
+            Repo.new(ARGV.new(['update'])).run
+          end
         end
       end
     
-            def stock_location_params
-          params.require(:stock_location).permit(permitted_stock_location_attributes)
+            def_node_matcher :multiple_compare?, <<-PATTERN
+          (send (send _ {:< :> :<= :>=} $_) {:< :> :<= :>=} _)
+        PATTERN
+    
+            def on_block(node)
+          on_body_of_reduce(node) do |body|
+            void_next = body.each_node(:next).find do |n|
+              n.children.empty? && parent_block_node(n) == node
+            end
+    
+            def_node_matcher :simple_double_comparison?, '(send $lvar :== $lvar)'
+        def_node_matcher :simple_comparison?, <<-PATTERN
+          {(send $lvar :== _)
+           (send _ :== $lvar)}
+        PATTERN
+    
+            def autocorrect(node)
+          lambda do |corrector|
+            corrector.remove(node.loc.dot)
+            corrector.remove(node.loc.selector)
+          end
         end
-      end
-    end
-  end
-end
-
-    
-            def index
-          authorize! :read, StockMovement
-          @stock_movements = scope.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
-          respond_with(@stock_movements)
-        end
-    
-      context 'called with null values' do
-    it 'writes rules for other three' do
-      ruleset = 'margin-top: 11px; ' +
-                'margin-right: 12px; ' +
-                'margin-left: 13px;'
-      bad_rule = 'margin-bottom: null;'
-    
-      context 'called with two sizes' do
-    it 'applies to alternating sides' do
-      rule = 'padding: 2px 3px'
-    
-      context 'called with two sizes' do
-    it 'applies to height and width' do
-      rule = 'height: 2em; width: 1em;'
-    
-        @inputs_list = %w(
-      [type='color']
-      [type='date']
-      [type='datetime']
-      [type='datetime-local']
-      [type='email']
-      [type='month']
-      [type='number']
-      [type='password']
-      [type='search']
-      [type='tel']
-      [type='text']
-      [type='time']
-      [type='url']
-      [type='week']
-      input:not([type])
-      textarea
-    )
-  end
