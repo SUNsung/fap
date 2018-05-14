4372ab461b8920c55b4306aff6dde57e5e96e6d6
SUNsung <weatherius@gmail.com>
2018-05-14 06:25:02 +0300

    Rewrite [11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py] File in: 2018-05-14 06:25:01

--- pyton/11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py
+++ pyton/11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py
@@ -1,119 +1,80 @@
 
         
-                if not os.path.exists(directory):
-            os.makedirs(directory)
-    
-        indent = None
-    if format:
-        indent = 4
-    
-    import re
-    
-        return results
-
-    
-    import matplotlib.pyplot as plt
-import scipy.sparse as sp
-import numpy as np
-    
-    n_samples = np.logspace(.5, 3, 9)
-n_features = np.logspace(1, 3.5, 7)
-N_samples, N_features = np.meshgrid(n_samples,
-                                    n_features)
-scikits_time = np.zeros(N_samples.shape)
-scipy_time = np.zeros(N_samples.shape)
-    
-    html_folder = u'html'
-text_folder = u'paragraphs'
-short_text_folder = u'short_paragraphs'
-n_words_per_short_text = 5
-    
-    
-if not os.path.exists(DATA_FOLDER):
-    
-    #import matplotlib.pyplot as plt
-#plt.matshow(cm, cmap=plt.cm.jet)
-#plt.show()
-    
-    exercise_dir = os.path.dirname(__file__)
-if exercise_dir == '':
-    exercise_dir = '.'
-    
-        def test_items_recoil(self):
-        with ObjectPool(self.sample_queue, True) as pool:
-            self.assertEqual(pool, 'first')
-        self.assertTrue(self.sample_queue.get() == 'second')
-        self.assertFalse(self.sample_queue.empty())
-        self.assertTrue(self.sample_queue.get() == 'first')
-        self.assertTrue(self.sample_queue.empty())
-    
-        def __new__(cls, name, bases, attrs):
-        new_cls = type.__new__(cls, name, bases, attrs)
-        '''
-            Here the name of the class is used as key but it could be any class
-            parameter.
-        '''
-        cls.REGISTRY[new_cls.__name__] = new_cls
-        return new_cls
-    
-        def test_display_current_time_at_current_time(self):
-        '''
-        Just as justification for working example. (Will always pass.)
-        '''
-        production_code_time_provider = ProductionCodeTimeProvider()
-        class_under_test = TimeDisplay(production_code_time_provider)
-        current_time = datetime.datetime.now()
-        expected_time = '<span class=\'tinyBoldText\'>{}:{}</span>'.format(current_time.hour, current_time.minute)
-        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
-    
-        @staticmethod
-    def _static_method_2():
-        print('executed method 2!')
-    
-    
-def timeit(func):
-    
-        This is, in fact, just syntactic sugar around a memento closure.
+            Models: `flatpages.flatpages`
+    Templates: Uses the template defined by the ``template_name`` field,
+        or :template:`flatpages/default.html` if template_name is not defined.
+    Context:
+        flatpage
+            `flatpages.flatpages` object
     '''
-    deep = False
-    states = []
+    if not url.startswith('/'):
+        url = '/' + url
+    site_id = get_current_site(request).id
+    try:
+        f = get_object_or_404(FlatPage, url=url, sites=site_id)
+    except Http404:
+        if not url.endswith('/') and settings.APPEND_SLASH:
+            url += '/'
+            f = get_object_or_404(FlatPage, url=url, sites=site_id)
+            return HttpResponsePermanentRedirect('%s/' % request.path)
+        else:
+            raise
+    return render_flatpage(request, f)
     
-    *TL;DR80
-Maintains a list of dependents and notifies them of any state changes.
+        if (has_actual_code and not has_question_marks):
+        linebuffer = clean_trailing_newlines(linebuffer)
+        write_with_harness(codefile, sourcefile, start_linenum, linebuffer)
+    return (line, linenum)
+    
+      Args:
+    line: The line in question.
+    filename: The name of the current file.
+    linenum: The number of the line to check.
+    next_line_start: The first non-whitespace column of the next line.
+    error: The function to call with any errors found.
+  '''
+  commentpos = line.find('//')
+  if commentpos != -1:
+    # Check if the // may be in quotes.  If so, ignore it
+    if re.sub(r'\\.', '', line[0:commentpos]).count(''') % 2 == 0:
+      # Allow one space for new scopes, two spaces otherwise:
+      if (not (Match(r'^.*{ *//', line) and next_line_start == commentpos) and
+          ((commentpos >= 1 and
+            line[commentpos-1] not in string.whitespace) or
+           (commentpos >= 2 and
+            line[commentpos-2] not in string.whitespace))):
+        error(filename, linenum, 'whitespace/comments', 2,
+              'At least two spaces is best between code and comments')
+    }
+    
+        def test_3rd_am_station_after_scan(self):
+        self.radio.scan()
+        station = self.radio.state.stations[self.radio.state.pos]
+        expected_station = '1510'
+        self.assertEqual(station, expected_station)
+    
+        def test_display_current_time_at_midnight(self):
+        class_under_test = TimeDisplay()
+        expected_time = '24:01'
+        result = class_under_test.get_current_time_as_as_html_fragment()
+        self.assertEqual(result, expected_time)
 '''
     
-    extension_mapping = {
-    'rss': ('xml', 'application/atom+xml; charset=UTF-8'),
-    'xml': ('xml', 'application/atom+xml; charset=UTF-8'),
-    'js': ('js', 'text/javascript; charset=UTF-8'),
-    'embed': ('htmllite', 'text/javascript; charset=UTF-8'),
-    'mobile': ('mobile', 'text/html; charset=UTF-8'),
-    'png': ('png', 'image/png'),
-    'css': ('css', 'text/css'),
-    'csv': ('csv', 'text/csv; charset=UTF-8'),
-    'api': (api_type(), 'application/json; charset=UTF-8'),
-    'json-html': (api_type('html'), 'application/json; charset=UTF-8'),
-    'json-compact': (api_type('compact'), 'application/json; charset=UTF-8'),
-    'compact': ('compact', 'text/html; charset=UTF-8'),
-    'json': (api_type(), 'application/json; charset=UTF-8'),
-    'i': ('compact', 'text/html; charset=UTF-8'),
-}
+        def test_display_current_time_at_midnight(self):
+        '''
+        Will almost always fail (despite of right at/after midnight).
+        '''
+        time_provider_stub = MidnightTimeProvider()
+        class_under_test = TimeDisplay(time_provider_stub)
+        expected_time = '<span class=\'tinyBoldText\'>24:01</span>'
+        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
     
-            controller_cls = self._controllers.get_controller(controller_name)
-        self.controller_classes[controller_name] = controller_cls
-        return controller_cls
+        def __init__(self, successor=None):
+        self._successor = successor
     
-    # class specific overrides
-api('link',          LinkJsonTemplate)
-api('promotedlink',  PromotedLinkJsonTemplate)
-api('message',       MessageJsonTemplate)
-api('subreddit',     SubredditJsonTemplate)
-api('labeledmulti',  LabeledMultiJsonTemplate)
-api('reddit',        RedditJsonTemplate)
-api('panestack',     PanestackJsonTemplate)
-api('htmlpanestack', NullJsonTemplate)
-api('listing',       ListingJsonTemplate)
-api('searchlisting', SearchListingJsonTemplate)
-api('userlisting',   UserListingJsonTemplate)
-api('usertableitem', UserTableItemJsonTemplate)
-api('account',       AccountJsonTemplate)
+    
+def memento(obj, deep=False):
+    state = deepcopy(obj.__dict__) if deep else copy(obj.__dict__)
+    
+    
+class CompositeSpecification(Specification):
