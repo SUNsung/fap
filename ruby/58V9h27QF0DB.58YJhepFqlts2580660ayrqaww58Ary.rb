413d7b320672e6e749c2b8bf96e60bd802c5aa46
SUNsung <weatherius@gmail.com>
2018-05-14 14:31:50 +0300

    Rewrite [58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb] File in: 2018-05-14 14:31:50

--- ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
+++ ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
@@ -1,59 +1,80 @@
 
         
-        invalids = []
-Parallel.each(links, in_threads: 4) do |link|
-  href = link.attribute('href').to_s
-  begin
-    case check_link(URI.join(BASE_URI, href))
-    when (200...300)
-      putc('.')
-    when (300..302)
-      putc('w')
+                if params[:add_to_search_list]
+          keychains = Action.sh('security list-keychains -d user').shellsplit
+          keychains << File.expand_path(keychain_path)
+          commands << Fastlane::Actions.sh('security list-keychains -s #{keychains.shelljoin}', log: false)
+        end
+    
+          it 'get SVN build number' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            get_build_number_repository
+        end').runner.execute(:test)
+    
+              results
+        end
+    
+    module Vagrant
+  module Util
+    class IO
+      # The chunk size for reading from subprocess IO.
+      READ_CHUNK_SIZE = 4096
+    
+          # This inserts a block with the given key and value.
+      #
+      # @param [String] key
+      # @param [String] value
+      def insert(key, value)
+        # Insert the new block into the value
+        new_block = <<BLOCK
+# VAGRANT-BEGIN: #{key}
+#{value.strip}
+# VAGRANT-END: #{key}
+BLOCK
+    
+      if options.respond_to? 'keys'
+    options.each do |k,v|
+      unless v.nil?
+        v = v.join ',' if v.respond_to? 'join'
+        v = v.to_json if v.respond_to? 'keys'
+        output += ' data-#{k.sub'_','-'}='#{v}''
+      end
     end
-  rescue => e
-    putc('F')
-    invalids << '#{href} (reason: #{e.message})'
+  elsif options.respond_to? 'join'
+    output += ' data-value='#{config[key].join(',')}''
+  else
+    output += ' data-value='#{config[key]}''
   end
+  output += '></#{tag}>'
 end
     
-          unless root?
-        raise Invalid, 'missing name' if !name || name.empty?
-        raise Invalid, 'missing path' if !path || path.empty?
-        raise Invalid, 'missing type' if !type || type.empty?
+            Dir.chdir(includes_dir) do
+          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
+          if choices.include?(file)
+            source = File.read(file)
+            partial = Liquid::Template.parse(source)
+            context.stack do
+              rtn = rtn + partial.render(context)
+            end
+          else
+            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
+          end
+        end
       end
-    end
-    
-            entries
-      end
+      rtn
     end
   end
-end
-
     
-      belongs_to :status_message
-  has_many :poll_answers, -> { order 'id ASC' }, dependent: :destroy
-  has_many :poll_participations, dependent: :destroy
-  has_one :author, through: :status_message
+      # Removes trailing forward slash from a string for easily appending url segments
+  def strip_slash(input)
+    if input =~ /(.+)\/$|^\/$/
+      input = $1
+    end
+    input
+  end
     
-        def index
-      pods_json = PodPresenter.as_collection(Pod.all)
-    
-          def create
-        restore_request_parameters
-        process_authorization_consent(params[:approve])
-      end
-    
-          def find
-        client = Api::OpenidConnect::OAuthApplication.find_by(client_name: params[:client_name])
-        if client
-          render json: {client_id: client.client_id}
-        else
-          render json: {error: 'Client with name #{params[:client_name]} does not exist'}
-        end
-      end
-    
-          def get_key_from_kid(keys, kid)
-        keys.each do |key|
-          return key if key.has_value?(kid)
-        end
-      end
+        # Determines whether the instance responds to this attribute. Used to prevent
+    # calculations on fields we won't even store.
+    def instance_respond_to?(attr)
+      instance.respond_to?(:'#{name}_#{attr}')
+    end
