59b1619c2519f7521e5df3465e7fa3921092b613
SUNsung <weatherius@gmail.com>
2018-05-15 13:22:09 +0300

    Create [09gkEXFpszwGA09f4A3iMUXHPI09asuL6jgEdk09WsI.rb] File in: 2018-05-15 13:22:09

---
+++ ruby/09gkEXFpszwGA09f4A3iMUXHPI09asuL6jgEdk09WsI.rb
@@ -0,0 +1,32 @@
+      def task_available?(task)
+        load_all_tasks
+        return tasks.collect(&:title).include?(task)
+      end
+    
+            expect(result).to eq('/usr/local/bin/cloc  --by-file --xml  --out=/tmp/cloc.xml')
+      end
+    
+          it 'increments the build number of the Xcode project' do
+        Fastlane::FastFile.new.parse('lane :test do
+          increment_build_number(xcodeproj: '.xcproject')
+        end').runner.execute(:test)
+    
+            # This returns all the config classes for the various provisioners.
+        #
+        # @return [Registry]
+        def provisioner_configs
+          Registry.new.tap do |result|
+            @registered.each do |plugin|
+              result.merge!(plugin.components.configs[:provisioner])
+            end
+          end
+        end
+    
+            # Helper method to get access to the class variable. This is mostly
+        # exposed for tests. This shouldn't be mucked with directly, since it's
+        # structure may change at any time.
+        def registered; @@registered; end
+      end
+    end
+  end
+end
