1dccd7287af5ed05a2de1c06d321d9d4a3b984e3
SUNsung <weatherius@gmail.com>
2018-05-15 22:07:54 +0300

    Rewrite [214q4UToBH.eA2116lLPg5da621qnE.8FQyaIs21eSI.cpp] File in: 2018-05-15 22:07:53

--- cccccc/214q4UToBH.eA2116lLPg5da621qnE.8FQyaIs21eSI.cpp
+++ cccccc/214q4UToBH.eA2116lLPg5da621qnE.8FQyaIs21eSI.cpp
@@ -1,171 +1,126 @@
 
         
-          void compute();
+          // ExtensionFunction:
+  bool RunAsync() override;
+  enum FailureReason {
+    FAILURE_REASON_UNKNOWN,
+    FAILURE_REASON_ENCODING_FAILED,
+    FAILURE_REASON_VIEW_INVISIBLE
+  };
+  virtual void OnCaptureFailure(FailureReason reason);
+  DECLARE_EXTENSION_FUNCTION('nw.currentWindowInternal.capturePageInternal', UNKNOWN)
     
-    NS_ASSUME_NONNULL_BEGIN
+    bool NwCurrentWindowInternalGetTitleInternalFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  AppWindow* window = getAppWindow(this);
+  response->AppendString(window->title_override());
+  return true;
+}
     
-      virtual inline const char* type() const { return 'ArgMax'; }
-  virtual inline int ExactNumBottomBlobs() const { return 1; }
-  virtual inline int ExactNumTopBlobs() const { return 1; }
+    
+    {}  // namespace nwapi
     
     
-    {}  // namespace caffe
+    {  int result = 0;
+  RenderThread::Get()->Send(new ShellViewHostMsg_AllocateId(
+      routing_id,
+      &result));
+  return scope.Escape(v8::Integer::New(isolate, result));
+}
     
-      /**
-   * @brief Computes the Contrastive error gradient w.r.t. the inputs.
-   *
-   * Computes the gradients with respect to the two input vectors (bottom[0] and
-   * bottom[1]), but not the similarity label (bottom[2]).
-   *
-   * @param top output Blob vector (length 1), providing the error gradient with
-   *      respect to the outputs
-   *   -# @f$ (1 \times 1 \times 1 \times 1) @f$
-   *      This Blob's diff will simply contain the loss_weight* @f$ \lambda @f$,
-   *      as @f$ \lambda @f$ is the coefficient of this layer's output
-   *      @f$\ell_i@f$ in the overall Net loss
-   *      @f$ E = \lambda_i \ell_i + \mbox{other loss terms}@f$; hence
-   *      @f$ \frac{\partial E}{\partial \ell_i} = \lambda_i @f$.
-   *      (*Assuming that this top Blob is not used as a bottom (input) by any
-   *      other layer of the Net.)
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2)
-   *   -# @f$ (N \times C \times 1 \times 1) @f$
-   *      the features @f$a@f$; Backward fills their diff with
-   *      gradients if propagate_down[0]
-   *   -# @f$ (N \times C \times 1 \times 1) @f$
-   *      the features @f$b@f$; Backward fills their diff with gradients if
-   *      propagate_down[1]
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    class NwClipboardSetListSyncFunction : public NWSyncExtensionFunction {
+ public:
+  NwClipboardSetListSyncFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
     
     #include <vector>
     
-    #include 'caffe/layers/softmax_layer.hpp'
-    
-    #include 'caffe/layers/neuron_layer.hpp'
-    
-    extern JSClass  *jsb_cocos2d_Physics3DShape_class;
-extern JSObject *jsb_cocos2d_Physics3DShape_prototype;
-    
-    
-    
-    
-    
-    
-    
-    
-    mShaderProgram->setUniformLocationWith4f(mColorLocation, color.r, color.g, color.b, 1);
-    glDrawArrays(GL_LINE_LOOP, 0, vertexCount);
-    
-    
-    {	static Test* Create()
-	{
-		return new AddPair;
-	}
-};
-    
-    			b2FixtureDef sd1;
-			sd1.shape = &poly1;
-			sd1.density = 4.0f;
-    
-    		for (int32 i = 0; i < 3; ++i)
-		{
-			b2CircleShape shape;
-			shape.m_radius = 0.5f;
+    void convert_dataset(const char* image_filename, const char* label_filename,
+        const char* db_filename) {
+  // Open files
+  std::ifstream image_file(image_filename, std::ios::in | std::ios::binary);
+  std::ifstream label_file(label_filename, std::ios::in | std::ios::binary);
+  CHECK(image_file) << 'Unable to open file ' << image_filename;
+  CHECK(label_file) << 'Unable to open file ' << label_filename;
+  // Read the magic and the meta data
+  uint32_t magic;
+  uint32_t num_items;
+  uint32_t num_labels;
+  uint32_t rows;
+  uint32_t cols;
     }
     
-    #ifndef BULLET_TEST_H
-#define BULLET_TEST_H
+    #include 'caffe/layers/base_conv_layer.hpp'
     
-    template<typename DType>
-inline void CompressArray<DType>::InitCompressChunks(
-    const std::vector<bst_uint>& chunk_ptr) {
-  raw_chunks_ = chunk_ptr;
-  CHECK_GE(raw_chunks_.size(), 2);
-  out_buffer_.resize(raw_chunks_.size() - 1);
-  for (size_t i = 0; i < out_buffer_.size(); ++i) {
-    out_buffer_[i].resize(raw_chunks_[i + 1] - raw_chunks_[i]);
-  }
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
+    
+    
+    {  /// when divided by UINT_MAX, the randomly generated values @f$u\sim U(0,1)@f$
+  Blob<unsigned int> rand_vec_;
+  /// the probability @f$ p @f$ of dropping any input
+  Dtype threshold_;
+  /// the scale for undropped inputs at train time @f$ 1 / (1 - p) @f$
+  Dtype scale_;
+  unsigned int uint_thres_;
+};
+    
+    ]]
+# endif  // GTEST_HAS_COMBINE
+    
+    #ifndef GTEST_INCLUDE_GTEST_GTEST_SPI_H_
+#define GTEST_INCLUDE_GTEST_GTEST_SPI_H_
+    
+    // Internal macro for implementing {EXPECT|ASSERT}_PRED2.  Don't use
+// this in your code.
+#define GTEST_PRED2_(pred, v1, v2, on_failure)\
+  GTEST_ASSERT_(::testing::AssertPred2Helper(#pred, \
+                                             #v1, \
+                                             #v2, \
+                                             pred, \
+                                             v1, \
+                                             v2), on_failure)
+    
+      explicit tuple(GTEST_BY_REF_(T0) f0, GTEST_BY_REF_(T1) f1,
+      GTEST_BY_REF_(T2) f2, GTEST_BY_REF_(T3) f3, GTEST_BY_REF_(T4) f4,
+      GTEST_BY_REF_(T5) f5, GTEST_BY_REF_(T6) f6, GTEST_BY_REF_(T7) f7,
+      GTEST_BY_REF_(T8) f8) : f0_(f0), f1_(f1), f2_(f2), f3_(f3), f4_(f4),
+      f5_(f5), f6_(f6), f7_(f7), f8_(f8) {}
+    
+      // Now, we have that n is odd and n >= 3.
+    
+    // Tests factorial of negative numbers.
+TEST(FactorialTest, Negative) {
+  // This test is named 'Negative', and belongs to the 'FactorialTest'
+  // test case.
+  EXPECT_EQ(1, Factorial(-5));
+  EXPECT_EQ(1, Factorial(-1));
+  EXPECT_GT(Factorial(-10), 0);
+    }
+    
+      if (Flags.minimize_crash_internal_step)
+    return MinimizeCrashInputInternalStep(F, Corpus);
+    
+    #endif
+
+    
+    void CloseStdout() {
+  CloseFile(1);
 }
     
-    #include <xgboost/base.h>
-    
-    namespace xgboost {
-    }
-    
-    
-    {  // --- data structure ---
-  GBTreeModel model_;
-  // training parameter
-  GBTreeTrainParam tparam_;
-  // ----training fields----
-  // configurations for tree
-  std::vector<std::pair<std::string, std::string> > cfg_;
-  // the updaters that can be applied to each of tree
-  std::vector<std::unique_ptr<TreeUpdater>> updaters_;
-  // Cached matrices
-  std::vector<std::shared_ptr<DMatrix>> cache_;
-  std::unique_ptr<Predictor> predictor_;
-  common::Monitor monitor_;
-};
-    
-    /*
- * Class:     ml_dmlc_xgboost4j_java_XGBoostJNI
- * Method:    XGDMatrixCreateFromCSCEx
- * Signature: ([J[J[F)J
- */
-JNIEXPORT jint JNICALL Java_ml_dmlc_xgboost4j_java_XGBoostJNI_XGDMatrixCreateFromCSCEx
-  (JNIEnv *jenv, jclass jcls, jlongArray jindptr, jintArray jindices, jfloatArray jdata, jint jrow, jlongArray jout) {
-  DMatrixHandle result;
-  jlong* indptr = jenv->GetLongArrayElements(jindptr, NULL);
-  jint* indices = jenv->GetIntArrayElements(jindices, 0);
-  jfloat* data = jenv->GetFloatArrayElements(jdata, NULL);
-  bst_ulong nindptr = (bst_ulong)jenv->GetArrayLength(jindptr);
-  bst_ulong nelem = (bst_ulong)jenv->GetArrayLength(jdata);
-    }
-    
-    // common regressions
-// linear regression
-struct LinearSquareLoss {
-  // duplication is necessary, as __device__ specifier
-  // cannot be made conditional on template parameter
-  XGBOOST_DEVICE static bst_float PredTransform(bst_float x) { return x; }
-  XGBOOST_DEVICE static bool CheckLabel(bst_float x) { return true; }
-  XGBOOST_DEVICE static bst_float FirstOrderGradient(bst_float predt, bst_float label) {
-    return predt - label;
+      if (Att == INVALID_FILE_ATTRIBUTES) {
+    Printf('GetFileAttributesA() failed for \'%s\' (Error code: %lu).\n',
+        Path.c_str(), GetLastError());
+    return false;
   }
-  XGBOOST_DEVICE static bst_float SecondOrderGradient(bst_float predt, bst_float label) {
-    return 1.0f;
-  }
-  template <typename T>
-  static T PredTransform(T x) { return x; }
-  template <typename T>
-  static T FirstOrderGradient(T predt, T label) { return predt - label; }
-  template <typename T>
-  static T SecondOrderGradient(T predt, T label) { return T(1.0f); }
-  static bst_float ProbToMargin(bst_float base_score) { return base_score; }
-  static const char* LabelErrorMsg() { return ''; }
-  static const char* DefaultEvalMetric() { return 'rmse'; }
-};
     
-    // data
-#include '../src/data/data.cc'
-#include '../src/data/simple_csr_source.cc'
-#include '../src/data/simple_dmatrix.cc'
-#include '../src/data/sparse_page_raw_format.cc'
+      // Parse NumFilesInFirstCorpus.
+  if (!std::getline(IS, Line, '\n')) return false;
+  std::istringstream L2(Line);
+  NumFilesInFirstCorpus = NumFiles + 1;
+  L2 >> NumFilesInFirstCorpus;
+  if (NumFilesInFirstCorpus > NumFiles) return false;
     
-    //
-//  boost_exception.cpp
-//  comm
-//
-//  Created by yanguoyue on 16/5/20.
-//
-    
-        template<typename T>
-    T* _Service() {
-        if (m_dependservices.end() != m_dependservices.find(T::ServiceName()))
-            return (T*)m_dependservices[T::ServiceName()];
-    }
+    // Private copy of SHA1 implementation.
+static const int kSHA1NumBytes = 20;
