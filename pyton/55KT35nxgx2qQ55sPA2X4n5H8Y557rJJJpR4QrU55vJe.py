f88bd7278f303d61aa6c3242e591cc076c2e9645
SUNsung <weatherius@gmail.com>
2018-05-15 18:46:57 +0300

    Rewrite [55KT35nxgx2qQ55sPA2X4n5H8Y557rJJJpR4QrU55vJe.py] File in: 2018-05-15 18:46:57

--- pyton/55KT35nxgx2qQ55sPA2X4n5H8Y557rJJJpR4QrU55vJe.py
+++ pyton/55KT35nxgx2qQ55sPA2X4n5H8Y557rJJJpR4QrU55vJe.py
@@ -1,52 +1,110 @@
 
         
-            with open('README.md', 'w+') as sorted_file:
-        # Then all of the blocks are sorted individually
-        blocks = [''.join(sorted(block, key=lambda s: s.lower())) for block in blocks]
-        # And the result is written back to README.md
-        sorted_file.write(''.join(blocks))
-    
-    import sys
+        import rsa
+import json
+from binascii import hexlify
     
     
-#: Log messages to :func:`~flask.logging.wsgi_errors_stream` with the format
-#: ``[%(asctime)s] %(levelname)s in %(module)s: %(message)s``.
-default_handler = logging.StreamHandler(wsgi_errors_stream)
-default_handler.setFormatter(logging.Formatter(
-    '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
-))
-    
-        class _FakeSignal(object):
-        '''If blinker is unavailable, create a fake class with the same
-        interface that allows sending of signals but will fail with an
-        error on anything else.  Instead of doing anything on send, it
-        will just ignore the arguments and do nothing instead.
-        '''
+atom_template = textwrap.dedent('''\
+    <?xml version='1.0' encoding='utf-8'?>
+    <feed xmlns='http://www.w3.org/2005/Atom'>
+        <link rel='self' href='http://rg3.github.io/youtube-dl/update/releases.atom' />
+        <title>youtube-dl releases</title>
+        <id>https://yt-dl.org/feed/youtube-dl-updates-feed</id>
+        <updated>@TIMESTAMP@</updated>
+        @ENTRIES@
+    </feed>''')
     
     
-def test_uninstalled_module_paths(modules_tmpdir, purge_module):
-    app = modules_tmpdir.join('config_module_app.py').write(
+def main():
+    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error('Expected an input and an output filename')
+    
+    
+from test.helper import gettestcases
+    
+    if __name__ == '__main__':
+    unittest.main()
+
+    
+    from test.helper import assertRegexpMatches
+    
+        _TEST = {
+        'url': 'http://www.anitube.se/video/36621',
+        'md5': '59d0eeae28ea0bc8c05e7af429998d43',
+        'info_dict': {
+            'id': '36621',
+            'ext': 'mp4',
+            'title': 'Recorder to Randoseru 01',
+            'duration': 180.19,
+        },
+        'skip': 'Blocked in the US',
+    }
+    
+        def _real_extract(self, url):
+        # URLs end with [uploader name]/[uploader title]
+        # this title is whatever the user types in, and is rarely
+        # the proper song title.  Real metadata is in the api response
+        album_url_tag = self._match_id(url)
+    
+        def _real_extract(self, url):
+        video_id = self._match_id(url)
+    
+    
+def test_custom_tag():
+    class Foo(object):
+        def __init__(self, data):
+            self.data = data
+    
+    from .globals import request
+    
+    
+def test_uninstalled_package_paths(modules_tmpdir, purge_module):
+    app = modules_tmpdir.mkdir('config_package_app')
+    init = app.join('__init__.py')
+    init.write(
         'import os\n'
         'import flask\n'
         'here = os.path.abspath(os.path.dirname(__file__))\n'
         'app = flask.Flask(__name__)\n'
     )
-    purge_module('config_module_app')
+    purge_module('config_package_app')
     
-        @app.after_request
-    def after_request_handler(response):
-        calls.append('after-handler')
-        response.data = 'stuff'
-        return response
+        # This test only works on CPython 2.7.
+    if sys.version_info >= (2, 7) and \
+            not hasattr(sys, 'pypy_translation_info'):
+        with assert_no_leak():
+            for x in range(10):
+                fire()
     
-        t = r1(r'type=(\w+)', flashvars)
-    id = r1(r'vid=([^']+)', flashvars)
-    if t == 'youku':
-        youku_download_by_vid(id, title=title, output_dir=output_dir, merge=merge, info_only=info_only)
-    elif t == 'tudou':
-        tudou_download_by_id(id, title, output_dir=output_dir, merge=merge, info_only=info_only)
-    elif t == 'sina' or t == 'video':
-        fake_headers['Referer'] = url
-        url = 'http://www.miomio.tv/mioplayer/mioplayerconfigfiles/sina.php?vid=' + id
-        xml_data = get_content(url, headers=fake_headers, decoded=True)
-        url_list = sina_xml_to_url_list(xml_data)
+    
+class PollErrorTest(unittest.TestCase):
+    '''Tests for acme.errors.PollError.'''
+    
+    
+class RedirectTest(unittest.TestCase):
+    '''Test the redirect_by_default method.'''
+    @classmethod
+    def _call(cls):
+        from certbot.display.enhancements import redirect_by_default
+        return redirect_by_default()
+    
+        def current_subreddit(self):
+        site = self.stacked_proxy_safe_get(c, 'site')
+        if not site:
+            # In non-request code (eg queued jobs), there isn't necessarily a
+            # site name (or other request-type data).  In those cases, we don't
+            # want to trigger any subreddit-specific code.
+            return ''
+        return site.name
+    
+    tpm = tp_manager.tp_manager()
+    
+    import pylons
+    
+    
+class HealthController(MinimalController):
+    def pre(self):
+        pass
