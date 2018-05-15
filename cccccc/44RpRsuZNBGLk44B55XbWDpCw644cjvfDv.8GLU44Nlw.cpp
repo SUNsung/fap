72115570d076845cebb355eba8514b21af6bb7cc
SUNsung <weatherius@gmail.com>
2018-05-15 19:59:34 +0300

    Rewrite [44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp] File in: 2018-05-15 19:59:33

--- cccccc/44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp
+++ cccccc/44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp
@@ -1,158 +1,168 @@
 
         
-        class NwCurrentWindowInternalGetZoomFunction : public NWSyncExtensionFunction {
- public:
-  NwCurrentWindowInternalGetZoomFunction() {}
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    }
-    
-    IPC_SYNC_MESSAGE_ROUTED3_1(ShellViewHostMsg_Call_Static_Method_Sync,
-                           std::string /* type name */,
-                           std::string /* method name */,
-                           base::ListValue /* arguments */,
-                           base::ListValue /* result */)
-    
-    #include 'base/values.h'
-#include 'components/zoom/zoom_controller.h'
-#include 'content/nw/src/api/object_manager.h'
-#include 'content/nw/src/api/menuitem/menuitem.h'
-#include 'content/public/browser/web_contents.h'
-#include 'content/public/common/page_zoom.h'
-#include 'ui/views/controls/menu/menu_runner.h'
-    
-    #include 'ui/base/models/simple_menu_model.h'
-    
-    aura::Window* Menu::GetActiveNativeView(content::RenderFrameHost* rfh) {
-  content::WebContents* web_contents =
-    content::WebContents::FromRenderFrameHost(rfh);
-  if (!web_contents) {
-    LOG(ERROR) << 'Menu: couldn't find WebContents';
-    return NULL;
-  }
-  return web_contents->GetFullscreenRenderWidgetHostView()
-             ? web_contents->GetFullscreenRenderWidgetHostView()
-                   ->GetNativeView()
-             : web_contents->GetNativeView();
-}
-    
-    NwObjCallObjectMethodFunction::NwObjCallObjectMethodFunction() {
-}
-    
-    #ifndef STORAGE_LEVELDB_DB_BUILDER_H_
-#define STORAGE_LEVELDB_DB_BUILDER_H_
-    
-    namespace leveldb {
-    }
-    
-    namespace leveldb {
-    }
-    
-     private:
-  Env* const env_;
-  const std::string dbname_;
-  const Options* options_;
-  Cache* cache_;
-    
-      // Temporary storage for parsing
-  int level;
-  uint64_t number;
-  FileMetaData f;
-  Slice str;
-  InternalKey key;
-    
-    static void TestEncodeDecode(const VersionEdit& edit) {
-  std::string encoded, encoded2;
-  edit.EncodeTo(&encoded);
-  VersionEdit parsed;
-  Status s = parsed.DecodeFrom(encoded);
-  ASSERT_TRUE(s.ok()) << s.ToString();
-  parsed.EncodeTo(&encoded2);
-  ASSERT_EQ(encoded, encoded2);
-}
-    
-    // Use the db with the following name.
-static const char* FLAGS_db = NULL;
-    
-    // Returns a new environment that stores its data in memory and delegates
-// all non-file-storage tasks to base_env. The caller must delete the result
-// when it is no longer needed.
-// *base_env must remain live while the result is in use.
-Env* NewMemEnv(Env* base_env);
-    
-    class FilterPolicy {
- public:
-  virtual ~FilterPolicy();
-    }
-    
-    void ComputeJacobians(InputArrayOfArrays objectPoints, InputArrayOfArrays imagePoints,
-                      const IntrinsicParams& param,  InputArray omc, InputArray Tc,
-                      const int& check_cond, const double& thresh_cond, Mat& JJ2_inv, Mat& ex3);
-    
-      template <typename T>
-  static void init_iov_base(T& base, void* addr)
-  {
-    base = static_cast<T>(addr);
-  }
-    
-    #if !defined(BOOST_ASIO_HAS_THREADS) \
-  || defined(BOOST_ASIO_DISABLE_FENCED_BLOCK)
-typedef null_fenced_block fenced_block;
-#elif defined(__MACH__) && defined(__APPLE__)
-typedef macos_fenced_block fenced_block;
-#elif defined(__sun)
-typedef solaris_fenced_block fenced_block;
-#elif defined(__GNUC__) && defined(__arm__) \
-  && !defined(__GCC_HAVE_SYNC_COMPARE_AND_SWAP_4)
-typedef gcc_arm_fenced_block fenced_block;
-#elif defined(__GNUC__) && (defined(__hppa) || defined(__hppa__))
-typedef gcc_hppa_fenced_block fenced_block;
-#elif defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
-typedef gcc_x86_fenced_block fenced_block;
-#elif defined(__GNUC__) \
-  && ((__GNUC__ == 4 && __GNUC_MINOR__ >= 1) || (__GNUC__ > 4)) \
-  && !defined(__INTEL_COMPILER) && !defined(__ICL) \
-  && !defined(__ICC) && !defined(__ECC) && !defined(__PATHSCALE__)
-typedef gcc_sync_fenced_block fenced_block;
-#elif defined(BOOST_ASIO_WINDOWS) && !defined(UNDER_CE)
-typedef win_fenced_block fenced_block;
-#else
-typedef null_fenced_block fenced_block;
-#endif
-    
-    #include <boost/asio/detail/push_options.hpp>
-    
-    #include <boost/asio/detail/push_options.hpp>
-    
-      ~winrt_buffer_impl()
-  {
-  }
-    
-    template <typename C>
-struct compare_not_equal_to : detail::cmp_pred<C, ordering::eq, 1> {
-  using detail::cmp_pred<C, ordering::eq, 1>::cmp_pred;
+        
+    {    bool checkSubsetPublic( const CvMat* ms1, int count, bool checkPartialSubset );
 };
     
-    memory_resource* get_default_resource();
-void set_default_resource(memory_resource*);
-memory_resource* new_delete_resource();
+    // GL type for 'half' precision (s10e5) float data in host memory
+#ifndef GL_ARB_half_float_pixel
+    typedef unsigned short GLhalfARB;
+#endif
+#ifndef GL_NV_half_float
+    typedef unsigned short GLhalfNV;
+#endif
     
-    AsyncIOQueue::AsyncIOQueue(AsyncIO* asyncIO) : asyncIO_(asyncIO) {}
+    void freeifaddrs(struct ifaddrs* addrs) {
+	struct ifaddrs* last = NULL;
+	struct ifaddrs* cursor = addrs;
+	while (cursor) {
+		delete[] cursor->ifa_name;
+		delete cursor->ifa_addr;
+		delete cursor->ifa_netmask;
+		last = cursor;
+		cursor = cursor->ifa_next;
+		delete last;
+	}
+}
+
     
-    path canonical_parent(const path& pth, const path& base) {
-  return canonical(pth.parent_path(), base) / pth.filename();
+    #include 'io/stream_peer.h'
+    
+    	_create = _create_func;
+	load_certs_func = _load_certs;
+    
+    #if defined(MBEDTLS_GCM_C) && (                                        \
+        !defined(MBEDTLS_AES_C) && !defined(MBEDTLS_CAMELLIA_C) )
+#error 'MBEDTLS_GCM_C defined, but not all prerequisites'
+#endif
+    
+    	MonoObject *mono_object = mono_object_new(mono_domain_get(), klass->get_mono_ptr());
+    
+    	ClassDB::bind_method(D_METHOD('play'), &VideoPlayer::play);
+	ClassDB::bind_method(D_METHOD('stop'), &VideoPlayer::stop);
+    
+    	friend class GDNativeLibraryResourceLoader;
+	friend class GDNative;
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+    
+    
+    
+    #ifdef __cplusplus
+extern 'C' {
+#endif
+#include 'tolua++.h'
+#ifdef __cplusplus
+}
+#endif
+    
+    
+    
+    
+    
+    // This class implements debug drawing callbacks that are invoked
+// inside b2World::Step.
+class GLESDebugDraw : public b2Draw
+{
+    float32 mRatio;
+    cocos2d::GLProgram* mShaderProgram;
+    GLint        mColorLocation;
+    }
+    
+    
+    {	if (callback.m_fixture)
+	{
+		b2Body* body = callback.m_fixture->GetBody();
+		b2MouseJointDef md;
+		md.bodyA = m_groundBody;
+		md.bodyB = body;
+		md.target = p;
+		md.maxForce = 1000.0f * body->GetMass();
+		m_mouseJoint = (b2MouseJoint*)m_world->CreateJoint(&md);
+		body->SetAwake(true);
+        return true;
+	}
+    
+    return false;
 }
     
-    [[noreturn]] void usage(const char* name) {
-  std::cerr << folly::format(
-      'Usage: {0}\n'
-      '         list all huge page sizes and their mount points\n'
-      '       {0} -cp <src_file> <dest_nameprefix>\n'
-      '         copy src_file to a huge page file\n',
-      name);
-  exit(1);
-}
+        int unit;
+    double value;
     
-      auto canceled = aioReader.cancel();
-  EXPECT_EQ(canceled.size(), ops.size() - result.size());
-  EXPECT_EQ(aioReader.pending(), 0);
-  EXPECT_EQ(completed, result.size());
+    class YogaTest_HadOverflowTests : public Test {
+protected:
+  YogaTest_HadOverflowTests() {
+    config = YGConfigNew();
+    root = YGNodeNewWithConfig(config);
+    YGNodeStyleSetWidth(root, 200);
+    YGNodeStyleSetHeight(root, 100);
+    YGNodeStyleSetFlexDirection(root, YGFlexDirectionColumn);
+    YGNodeStyleSetFlexWrap(root, YGWrapNoWrap);
+  }
+    }
+    
+        ~Config(void);
+    
+    Node * Node::getParent(void)
+{
+    auto nodePtr = YGNodeGetParent(m_node);
+    }
+    
+    
+    {  // There are subtle issues with calling the next functions directly. It is
+  // much better to always use a ThreadScope to manage attaching/detaching for
+  // you.
+  FBEXPORT static JNIEnv* ensureCurrentThreadIsAttached();
+  FBEXPORT static void detachCurrentThread();
+};
+    
+    
+    {}
+
+    
+      virtual ~AbstractAuthResolver();
+    
+      // Returns proxy method for given protocol. Either V_GET or V_TUNNEL
+  // is returned.  For HTTPS, always returns V_TUNNEL.
+  const std::string& resolveProxyMethod(const std::string& protocol) const;
+    
+      virtual void writeData(const unsigned char* data, size_t len,
+                         int64_t offset) CXX11_OVERRIDE;
+    
+      enum Flag {
+    FLAG_HIDDEN = 1,
+    FLAG_ERASE_AFTER_PARSE = 1 << 1,
+    FLAG_INITIAL_OPTION = 1 << 2,
+    FLAG_CHANGE_OPTION = 1 << 3,
+    FLAG_CHANGE_OPTION_FOR_RESERVED = 1 << 4,
+    FLAG_CHANGE_GLOBAL_OPTION = 1 << 5,
+    FLAG_CUMULATIVE = 1 << 6
+  };
+    
+    AdaptiveFileAllocationIterator::~AdaptiveFileAllocationIterator() = default;
+    
+    class AnnounceTier {
+public:
+  enum AnnounceEvent {
+    STARTED,
+    STARTED_AFTER_COMPLETION,
+    DOWNLOADING,
+    STOPPED,
+    COMPLETED,
+    SEEDING,
+    HALTED
+  };
+    }
+    
+    #endif // D_LIBSSL_TLS_CONTEXT_H
+
+    
+    
+    {} // namespace aria2
