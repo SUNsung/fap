927502d6b9ace01103864d2f7e0f4f4de0ef4b90
SUNsung <weatherius@gmail.com>
2018-05-15 20:27:51 +0300

    Rewrite [04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb] File in: 2018-05-15 20:27:51

--- ruby/04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb
+++ ruby/04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb
@@ -1,54 +1,127 @@
 
         
-          def test_safe_redirect_back_from_other_host
-    @request.env['HTTP_REFERER'] = 'http://another.host/coming/from'
-    get :safe_redirect_back_with_status
-    
-      test 'response status aliases deprecated' do
-    response = ActionDispatch::TestResponse.create
-    assert_deprecated { response.success? }
-    assert_deprecated { response.missing? }
-    assert_deprecated { response.error? }
-  end
-end
-
-    
-        test 'head :continue (100) does not return any content' do
-      content = body(HeadController.action(:continue).call(Rack::MockRequest.env_for('/')))
-      assert_empty content
-    end
-    
-        class MessageDelivery < ActionMailer::MessageDelivery # :nodoc:
-      def initialize(mailer_class, action, params, *args)
-        super(mailer_class, action, *args)
-        @params = params
-      end
-    
-            expect(result).to eq('hg parent --template {rev}')
-        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('hg parent --template {rev}')
-      end
+                      if very_raw_connection && very_raw_connection.respond_to?(:flush)
+                very_raw_connection.flush
+              end
+            end
+        end
     end
   end
 end
 
     
-      def icon
-    object.image
-  end
+              define_singleton_method('#{name}=') do |attribute|
+            instance.public_send('#{name}=', attribute)
+          end
+        end
+      end
     
-    ###
-### methods
-###
-    
-          common_options(opts)
-      style(opts)
-      input_and_output(opts)
-      miscellaneous(opts)
+        test 'head :no_content (204) does not return a content-type header' do
+      headers = HeadController.action(:no_content).call(Rack::MockRequest.env_for('/')).second
+      assert_nil headers['Content-Type']
+      assert_nil headers['Content-Length']
     end
     
-          module ClassMethods
-        def inherited(subclass)
-          subclass.log_levels = subclass.superclass.log_levels.dup
+          add_delivery_method :smtp, Mail::SMTP,
+        address:              'localhost',
+        port:                 25,
+        domain:               'localhost.localdomain',
+        user_name:            nil,
+        password:             nil,
+        authentication:       nil,
+        enable_starttls_auto: true
+    
+            def restore_delivery_method
+          ActionMailer::Base.deliveries.clear
+          ActionMailer::Base.delivery_method = @old_delivery_method
         end
     
-      Sass::Plugin.options.merge!(config)
+      def test_assert_select_email_multipart
+    AssertMultipartSelectMailer.test(html: '<div><p>foo</p><p>bar</p></div>', text: 'foo bar').deliver_now
+    assert_select_email do
+      assert_select 'div:root' do
+        assert_select 'p:first-child', 'foo'
+        assert_select 'p:last-child', 'bar'
+      end
+    end
+  end
+end
+
+    
+      def setup
+    super
+    ActionMailer::LogSubscriber.attach_to :action_mailer
+  end
+    
+    * item1 * item2
+  * item3
+    TEXT
+    
+    #
+    
+    # No trailing slash
+Benchmark.ips do |x|
+  x.report('with body include?') { CONTENT_CONTAINING.include?('<body') }
+  x.report('with body regexp')   { CONTENT_CONTAINING =~ /<\s*body/ }
+  x.compare!
+end
+
+    
+        Jekyll::Commands::Build.process({'source' => 'docs'})
+    
+          def measure_bytes
+        yield.tap do |str|
+          @renderer.increment_bytes(@filename, str.bytesize)
+        end
+      end
+    
+      end
+    
+      # Asynchronously send an email
+  class TestEmail < Jobs::Base
+    
+    require 'hbc/container/base'
+    
+      // writing
+  $('form').on('submit',function(e) {
+    $.post('/', {msg: '<%= user %>: ' + $('#msg').val()});
+    $('#msg').val(''); $('#msg').focus();
+    e.preventDefault();
+  });
+</script>
+    
+      describe '#referrer' do
+    it 'Reads referrer from Referer header' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
+      expect(subject.referrer(env)).to eq('bar.com')
+    end
+    
+      %w(GET HEAD).each do |method|
+    it 'accepts #{method} requests with non-whitelisted Origin' do
+      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://malicious.com')).to be_ok
+    end
+  end
+    
+      it 'should not leak changes to env' do
+    klass    = described_class
+    detector = Struct.new(:app) do
+      def call(env)
+        was = env.dup
+        res = app.call(env)
+        was.each do |k,v|
+          next if env[k] == v
+          fail 'env[#{k.inspect}] changed from #{v.inspect} to #{env[k].inspect}'
+        end
+        res
+      end
+    end
+    
+          origin = caller[1]
+      if origin =~ /rubygems\/custom_require/
+        origin = caller[3]
+        if origin.nil?
+          STDERR.puts 'Unknown origin'
+          STDERR.puts caller.join('\n')
+        end
+      end
+      origin = origin.gsub(/:[0-9]+:in .*/, '') if origin
