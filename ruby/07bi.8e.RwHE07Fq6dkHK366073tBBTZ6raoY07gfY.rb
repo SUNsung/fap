114839ba39b5f7c7f40304c0070d3fcaca4be7ff
SUNsung <weatherius@gmail.com>
2018-05-16 16:52:10 +0300

    Rewrite [07bi.8e.RwHE07Fq6dkHK366073tBBTZ6raoY07gfY.rb] File in: 2018-05-16 16:52:09

--- ruby/07bi.8e.RwHE07Fq6dkHK366073tBBTZ6raoY07gfY.rb
+++ ruby/07bi.8e.RwHE07Fq6dkHK366073tBBTZ6raoY07gfY.rb
@@ -1,76 +1,13 @@
 
         
-            def valid_type?(type)
-      const_get(:TYPES).include?(type)
-    end
+        describe Spaceship::Portal::Key do
+  let(:mock_client) { double('MockClient') }
     
-      def use_sandbox?
-    ENV['USE_EVERNOTE_SANDBOX'] == 'true'
+        export LANG=en_US.UTF-8
+    \e[0m
+    DOC
   end
     
-        def initialize(name = nil, path = nil, type = nil)
-      self.name = name
-      self.path = path
-      self.type = type
-    
-        def as_json
-      @pages
-    end
-    
-          def get_type
-        case slug
-        when 'api'
-          'Reference'
-        when 'configuration'
-          'Reference: Configuration'
-        when 'stpl'
-          'Reference: SimpleTemplate'
-        when 'plugindev'
-          'Reference: Plugin'
-        else
-          'Manual'
-        end
+          def plugins_string
+        UI::ErrorReport.plugins_string
       end
-    
-      let(:payload) do
-    {
-      '@context': 'https://www.w3.org/ns/activitystreams',
-      id: 'foo',
-      type: 'Create',
-      actor: ActivityPub::TagManager.instance.uri_for(actor),
-      object: {
-        id: 'bar',
-        type: 'Note',
-        content: 'Lorem ipsum',
-      },
-    }
-  end
-    
-      def id
-    object.id.to_s
-  end
-    
-    	if ln =~ /\(jmp\)/
-		parts = ln.split(' ')
-		if (parts[0][0,1] == 'j' and parts[2][0,2] == ';j' and parts[4] == '(jmp)')
-			old = parts[1]
-			func = parts[3]
-			new = addrs[func]
-			#puts '%32s: %s -> %x' % [func, old, new]
-			replaces << [func, old, new.to_s(16)]
-		end
-	end
-    
-            def create
-          authorize! :create, StockMovement
-          @stock_movement = scope.new(stock_movement_params)
-          if @stock_movement.save
-            respond_with(@stock_movement, status: 201, default_template: :show)
-          else
-            invalid_resource!(@stock_movement)
-          end
-        end
-    
-          expect('.size-both').to have_ruleset(rule)
-    end
-  end
