d3198d36faebae7d39066cabbbf84092717b8d9a
SUNsung <weatherius@gmail.com>
2018-05-16 07:39:55 +0300

    Rewrite [23Z2e2LNdBusQ23l275nJw1g23uYvxMiaZkp.23i.4.rb] File in: 2018-05-16 07:39:55

--- ruby/23Z2e2LNdBusQ23l275nJw1g23uYvxMiaZkp.23i.4.rb
+++ ruby/23Z2e2LNdBusQ23l275nJw1g23uYvxMiaZkp.23i.4.rb
@@ -1,48 +1,8 @@
 
         
-              include ActionDispatch::Routing::PolymorphicRoutes
+          if ARGV.include? '--no-ansi'
+    STDERR.puts <<-DOC
+    WARNING: CocoaPods requires your terminal to be using UTF-8 encoding.
+    Consider adding the following to ~/.profile:
     
-            def test_url_port
-          spec = resolve 'abstract://foo:123?encoding=utf8'
-          assert_equal({
-            'adapter'  => 'abstract',
-            'port'     => 123,
-            'host'     => 'foo',
-            'encoding' => 'utf8' }, spec)
-        end
-    
-      def call_controller(klass, action)
-    klass.action(action).call(@request.env)
-  end
-    
-        test 'head :continue (100) does not return any content' do
-      content = body(HeadController.action(:continue).call(Rack::MockRequest.env_for('/')))
-      assert_empty content
-    end
-    
-        def env_for(url)
-      Rack::MockRequest.env_for(url)
-    end
-  end
-    
-      def resolve_thread(status)
-    return unless status.reply? && status.thread.nil?
-    ThreadResolveWorker.perform_async(status.id, in_reply_to_uri)
-  end
-    
-                -- Return the time part and the sequence part. OR appears
-            -- faster here than addition, but they're equivalent:
-            -- time_part has no trailing two bytes, and tail is only
-            -- the last two bytes.
-            RETURN time_part | tail;
-          END
-        $$ LANGUAGE plpgsql VOLATILE;
-      SQL
-    end
-    
-        it 'does not create a domain block' do
-      expect(DomainBlock.blocked?('evil.org')).to be false
-    end
-    
-    RSpec.describe RuboCop::Cop::Style::StringMethods, :config do
-  subject(:cop) { described_class.new(config) }
+    task default: :test
