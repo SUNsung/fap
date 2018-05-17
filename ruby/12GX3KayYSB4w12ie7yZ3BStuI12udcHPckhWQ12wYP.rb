a20c37432616d7601dbb4916229e2b4f612e25f4
SUNsung <weatherius@gmail.com>
2018-05-17 17:27:04 +0300

    Rewrite [12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb] File in: 2018-05-17 17:27:03

--- ruby/12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb
+++ ruby/12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb
@@ -1,23 +1,116 @@
 
         
-              # generate variables template
-      save_file 'templates/project/_bootstrap-variables.sass',
-                '// Override Bootstrap variables here (defaults from bootstrap-sass v#{Bootstrap::VERSION}):\n\n' +
-                    File.read('#{save_to}/_variables.scss').lines[1..-1].join.gsub(/^(?=\$)/, '// ').gsub(/ !default;/, '')
-    end
+                a_split.each_with_index { |s, i| a_split[i] = s.to_i unless i == a_length - 1 }
+        b_split.each_with_index { |s, i| b_split[i] = s.to_i unless i == b_length - 1 }
     
-        alias log puts
+              # Underscore methods
+          if name.start_with?('Underscore')
+            node.at_css('~ ul').css('li').each do |li|
+              name = [type.downcase, li.at_css('a').content.split.first].join('.')
+              id = name.parameterize
+              li['id'] = id
+              entries << [name, id, type]
+            end
+            next
+          end
     
-        status data.respond_to?(:status_code) ? data.status_code : 200
+            css('.bs-docs-sidenav > li').each do |node|
+          link = node.at_css('a')
+          name = link.content
+          next if IGNORE_ENTRIES.include?(name)
     
-        r0
+          def get_type
+        case slug
+        when 'api'
+          'Reference'
+        when 'configuration'
+          'Reference: Configuration'
+        when 'stpl'
+          'Reference: SimpleTemplate'
+        when 'plugindev'
+          'Reference: Plugin'
+        else
+          'Manual'
+        end
+      end
+    
+      def save_file(path, content, mode='w')
+    dir = File.dirname(path)
+    FileUtils.mkdir_p(dir) unless File.directory?(dir)
+    File.open(path, mode) { |file| file.write(content) }
   end
     
-          # Only print require() calls that did actual work.
-      # require() returns true on load, false if already loaded.
-      if result
-        source = caller[0]
-        #p source.include?('/lib/polyglot.rb:63:in `require'') => source
-        if source.include?('/lib/polyglot.rb:63:in `require'')
-          source = caller[1]
+        def log_http_get_file(url, cached = false)
+      s = '  #{'CACHED ' if cached}GET #{url}...'
+      if cached
+        puts dark green s
+      else
+        puts dark cyan s
+      end
+    end
+    
+      # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
+    
+    require 'metasploit/framework/common_engine'
+require 'metasploit/framework/database'
+    
+    require 'rubygems'  # install rubygems
+require 'hpricot'   # gem install hpricot
+require 'uri'
+require 'timeout'
+    
+            # Remove it form the session objects so freeup
+        sessions.delete(s[:session])
+    
+    # Sniffer class for GET URL's
+class SnifferURL < BaseProtocolParser
+  def register_sigs
+    self.sigs = {
+      :get		=> /^GET\s+([^\n]+)\s+HTTP\/\d\.\d/i,
+      :webhost	=> /^HOST\:\s+([^\n\r]+)/i,
+    }
+  end
+    
+          # @param  [[Xcodeproj::PBXTarget]] targets
+      #         An array which always has a target as its first item
+      #         and may optionally contain a second target as its test target
+      #
+      # @return [String] the text for the target module
+      #
+      def template_contents(path, prefix, fallback)
+        if path.exist?
+          path.read.chomp.lines.map { |line| '#{prefix}#{line}' }.join('\n')
+        else
+          '#{prefix}# #{fallback}'
+        end
+      end
+    end
+  end
+end
+
+    
+            def validate!
+          super
+          help! 'A name for the Pod is required.' unless @name
+          help! 'The Pod name cannot contain spaces.' if @name =~ /\s/
+          help! 'The Pod name cannot contain plusses.' if @name =~ /\+/
+          help! 'The Pod name cannot begin with a '.'' if @name[0, 1] == '.'
+        end
+    
+            def used?
+          @variable.captured_by_block? || @referenced
+        end
+    
+            # @param lines [Array<String>]
+        # @param annotations [Array<(Integer, String)>]
+        #   each entry is the annotated line number and the annotation text
+        #
+        # @note annotations are sorted so that reconstructing the annotation
+        #   text via {#to_s} is deterministic
+        def initialize(lines, annotations)
+          @lines       = lines.freeze
+          @annotations = annotations.sort.freeze
         end
