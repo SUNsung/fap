53a97d328ae9a5af127b3b06cb0de6e4b80b047d
SUNsung <weatherius@gmail.com>
2018-05-18 02:58:28 +0300

    Rewrite [55NWEeJ9ino8A55prpVi7sUY55se.oPMBHfXo55UR.rb] File in: 2018-05-18 02:58:28

--- ruby/55NWEeJ9ino8A55prpVi7sUY55se.oPMBHfXo55UR.rb
+++ ruby/55NWEeJ9ino8A55prpVi7sUY55se.oPMBHfXo55UR.rb
@@ -1,51 +1,33 @@
 
         
-          def updated
-    object.updated_at.iso8601
+              # Topic may be hard deleted due to spam, no point complaining
+      # we would have to look at the topics table id sequence to find cases
+      # where this was called with an invalid id, no point really
+      return unless topic.present?
+    
+      included do
+    scope :admins, -> { where(admin: true) }
+    scope :moderators, -> { where(moderator: true) }
+    scope :staff, -> { where('moderator or admin ') }
   end
     
-        grouped_codes = codes.reduce([]) do |agg, current|
-      if current[1]
-        agg << [current[0]]
-      else
-        agg.last << current[0]
-        agg
-      end
-    end
+      # Check if there is no signed in user before doing the sign out.
+  #
+  # If there is no signed in user, it will set the flash message and redirect
+  # to the after_sign_out path.
+  def verify_signed_out_user
+    if all_signed_out?
+      set_flash_message! :notice, :already_signed_out
     
-    See CONTRIBUTING.md for more information.
-    
-        def initialize(tag_name, markup, tokens)
-      attributes = ['class', 'src', 'width', 'height', 'title']
-    
-    end
-Liquid::Template.register_filter OctopressLiquidFilters
-    
-        def initialize(tag_name, markup, tokens)
-      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
-      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
-      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
-      super
-    end
-    
-            def failure_message_when_negated
-          'Attachment #{@attachment_name} cannot be between #{@low} and #{@high} bytes'
-        end
-        alias negative_failure_message failure_message_when_negated
-    
-        def processor(name) #:nodoc:
-      @known_processors ||= {}
-      if @known_processors[name.to_s]
-        @known_processors[name.to_s]
-      else
-        name = name.to_s.camelize
-        load_processor(name) unless Paperclip.const_defined?(name)
-        processor = Paperclip.const_get(name)
-        @known_processors[name.to_s] = processor
-      end
-    end
-    
-          if app.config.respond_to?(:paperclip_defaults)
-        Paperclip::Attachment.default_options.merge!(app.config.paperclip_defaults)
-      end
+      node[:applications].each do |app, data|
+    template '/etc/monit.d/sidekiq_#{app}.monitrc' do 
+      owner 'root' 
+      group 'root' 
+      mode 0644 
+      source 'monitrc.conf.erb' 
+      variables({ 
+        :num_workers => worker_count,
+        :app_name => app, 
+        :rails_env => node[:environment][:framework_env] 
+      }) 
     end
