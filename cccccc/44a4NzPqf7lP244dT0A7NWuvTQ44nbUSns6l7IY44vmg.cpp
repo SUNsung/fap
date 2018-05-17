e2c5de3eb0ada05f49eee920144633b34fdf3fd1
SUNsung <weatherius@gmail.com>
2018-05-17 05:49:08 +0300

    Rewrite [44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp] File in: 2018-05-17 05:49:07

--- cccccc/44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp
+++ cccccc/44a4NzPqf7lP244dT0A7NWuvTQ44nbUSns6l7IY44vmg.cpp
@@ -1,142 +1,78 @@
 
         
-        PyObject* NewFileServicesByName(const FileDescriptor* descriptor);
-    
-    
-    {
-    {
-    {}  // namespace python
-}  // namespace protobuf
-}  // namespace google
-
-    
-    TEST(AnyTest, TestPackAndUnpack) {
-  protobuf_unittest::TestAny submessage;
-  submessage.set_int32_value(12345);
-  protobuf_unittest::TestAny message;
-  message.mutable_any_value()->PackFrom(submessage);
+        void ELIST::assign_to_sublist(                           //to this list
+                              ELIST_ITERATOR *start_it,  //from list start
+                              ELIST_ITERATOR *end_it) {  //from list end
+  const ERRCODE LIST_NOT_EMPTY =
+    'Destination list must be empty before extracting a sublist';
     }
     
-    
-    {
-}  // namespace google
-#endif  // GOOGLE_PROTOBUF_COMPILER_CSHARP_OPTIONS_H__
-
+      /** Sets Dict::letter_is_okay_ function to point to the given function. */
+  void SetDictFunc(DictFunc f);
     
     
     {
-    {
-    {
-    {}  // namespace csharp
-}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-    
-    #include <google/protobuf/compiler/code_generator.h>
-    
-    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
-//   can only really compare the output against a golden value, which is a
-//   fairly tedious and fragile testing strategy.  If we want to go that route,
-//   it probably makes sense to bite the bullet and write a test that compares
-//   the whole generated output for unittest.proto against a golden value, with
-//   a very simple script that can be run to regenerate it with the latest code.
-//   This would mean that updates to the golden file would have to be included
-//   in any change to the code generator, which would actually be fairly useful
-//   as it allows the reviewer to see clearly how the generated code is
-//   changing.
-    
-    
-    {  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(ImmutableExtensionLiteGenerator);
-};
-    
-    template<typename T>
-inline Vptr emitTLSAddr(Vout& v, TLSDatum<T> datum) {
-  switch (arch()) {
-    case Arch::X64:
-      return x64::detail::emitTLSAddr(v, datum);
-    case Arch::ARM:
-      return arm::detail::emitTLSAddr(v, datum);
-    case Arch::PPC64:
-      return ppc64::detail::emitTLSAddr(v, datum);
+    {    if (tessedit_enable_doc_dict && !word->IsAmbiguous())
+      tess_add_doc_word(word->best_choice);
   }
-  not_reached();
 }
     
-      Array getAsyncResults(bool allow_empty);
+        // Helper methods
+    static bool HasFamilyNamed(std::string& name, DBWrapper* db);
+    static bool AddToBatch(rocksdb::WriteBatch& batch, bool del,
+        Handle<Array> array);
+    static bool AddToBatch(rocksdb::WriteBatch& batch, bool del,
+        Handle<Array> array, DBWrapper* db_wrapper, std::string cf);
+    static Handle<Value> CompactRangeDefault(const v8::Arguments& args);
+    static Handle<Value> CompactColumnFamily(const Arguments& args);
+    static Handle<Value> CompactOptions(const Arguments& args);
+    static Handle<Value> CompactAll(const Arguments& args);
     
-    static void HHVM_FUNCTION(gmp_random, int64_t /*limiter*/) {
-  throw_not_implemented('gmp_random');
-}
+    Name            Speed       Q.Score   Author
+xxHash          5.4 GB/s     10
+CrapWow         3.2 GB/s      2       Andrew
+MumurHash 3a    2.7 GB/s     10       Austin Appleby
+SpookyHash      2.0 GB/s     10       Bob Jenkins
+SBox            1.4 GB/s      9       Bret Mulvey
+Lookup3         1.2 GB/s      9       Bob Jenkins
+SuperFastHash   1.2 GB/s      1       Paul Hsieh
+CityHash64      1.05 GB/s    10       Pike & Alakuijala
+FNV             0.55 GB/s     5       Fowler, Noll, Vo
+CRC32           0.43 GB/s     9
+MD5-32          0.33 GB/s    10       Ronald L. Rivest
+SHA1-32         0.28 GB/s    10
     
-    /*
- * Vtuple is an index to a tuple in Vunit::tuples.
- */
-DECLARE_VNUM(Vtuple, true, 'T');
+    #include 'rocksdb/merge_operator.h'
+#include 'rocksdb/slice.h'
     
-      if (asprintf(&default_magic, '%s:%s', hmagicpath, MAGIC) < 0)
-    goto out;
-  free(hmagicpath);
-  return default_magic;
-out:
-  default_magic = NULL;
-  free(hmagicpath);
-  return MAGIC;
-#else
-  char *hmagicp = hmagicpath;
-  char *tmppath = NULL;
-  LPTSTR dllpath;
-    
-    
-// Logical immediates can't encode zero, so a return value of zero is used to
-// indicate a failure case. Specifically, where the constraints on imm_s are
-// not met.
-uint64_t Instruction::ImmLogical() {
-  unsigned reg_size = SixtyFourBits() ? kXRegSize : kWRegSize;
-  int64_t n = BitN();
-  int64_t imm_s = ImmSetBits();
-  int64_t imm_r = ImmRotate();
-    }
-    
-    CurlShareResource::CurlShareResource() {
-  m_share = curl_share_init();
-}
-    
-    struct CurlShareResource : SweepableResourceData {
-  DECLARE_RESOURCE_ALLOCATION(CurlShareResource)
-  CLASSNAME_IS('curl_share')
-  const String& o_getClassNameHook() const override { return classnameof(); }
-  bool isInvalid() const override { return !m_share; }
+    namespace rocksdb {
     }
     
     
-    
-    
-    
-    
-    
-    			b2Transform xf2;
-			xf2.q.Set(-0.3524f * b2_pi);
-			xf2.p = -xf2.q.GetXAxis();
-    
-    			pjd.maxMotorForce = 1000.0f;
-			pjd.enableMotor = true;
-			pjd.lowerTranslation = -10.0f;
-			pjd.upperTranslation = 10.0f;
-			pjd.enableLimit = true;
-    
-    class Bridge : public Test
-{
-public:
-    }
-    
-    
-    {	b2Body* m_middle;
+    {  // No copying allowed
+  Reader(const Reader&);
+  void operator=(const Reader&);
 };
     
-    			jd.Initialize(m_car, m_wheel1, m_wheel1->GetPosition(), axis);
-			jd.motorSpeed = 0.0f;
-			jd.maxMotorTorque = 20.0f;
-			jd.enableMotor = true;
-			jd.frequencyHz = m_hz;
-			jd.dampingRatio = m_zeta;
-			m_spring1 = (b2WheelJoint*)m_world->CreateJoint(&jd);
+    
+    {    // TODO: the lock will never expire. It may cause problem if the process crash or abnormally exit.
+    while (!_io_ctx->lock_exclusive(
+             _obj_name,
+             _lock_name,
+             _cookie,
+             'description', nullptr, 0));
+  }
+    
+    Status DateTieredDBImpl::Delete(const WriteOptions& options, const Slice& key) {
+  int64_t timestamp = 0;
+  Status s;
+  s = GetTimestamp(key, &timestamp);
+  if (!s.ok()) {
+    return s;
+  }
+  DropObsoleteColumnFamilies();
+  // Prune request to obsolete data
+  if (IsStale(timestamp, ttl_, db_->GetEnv())) {
+    return Status::NotFound();
+  }
+    }
