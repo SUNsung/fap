fc071d2a44a5f247afa3fd592124cd9b5ec63165
SUNsung <weatherius@gmail.com>
2018-05-17 09:24:14 +0300

    Rewrite [31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb] File in: 2018-05-17 09:24:13

--- ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
+++ ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
@@ -1,213 +1,99 @@
-  </body>
-</html>
-HTML
-CONTENT_NOT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
-    
-      private
-    
-      private
-    
-        registration
-  end
-    
-            # With reconfirmable, notify the original email when the user first
-        # requests the email change, instead of when the change is confirmed.
-        def send_email_changed_notification?
-          if self.class.reconfirmable
-            self.class.send_email_changed_notification && reconfirmation_required?
-          else
-            super
-          end
-        end
-    
-        if resource.errors.empty?
-      set_flash_message! :notice, :unlocked
-      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
-    end
-  end
-    
-        def confirmation_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :confirmation_instructions, opts)
-    end
-    
-    ###
-### methods
-###
-    
-    if $PROGRAM_NAME == __FILE__ && !ENV['COCOAPODS_NO_BUNDLER']
-  ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
-  require 'rubygems'
-  require 'bundler/setup'
-  $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-elsif ENV['COCOAPODS_NO_BUNDLER']
-  require 'rubygems'
-  gem 'cocoapods'
-end
-    
-    profile = Profile.new
-# puts profile.generate
-command = ['/usr/bin/sandbox-exec', '-p', profile.generate, profile.pod_bin, *ARGV]
-exec(*command)
 
-    
-          def stages
-        names = Dir[stage_definitions].map { |f| File.basename(f, '.rb') }
-        assert_valid_stage_names(names)
-        names
-      end
-    
-    describe Capistrano::Application do
-  it 'provides a --trace option which enables SSHKit/NetSSH trace output'
-    
-        def possible_types
-      MIME::Types.type_for(@filepath).collect(&:content_type)
-    end
-    
-        # Gives a Geometry representing the given height and width
-    def initialize(width = nil, height = nil, modifier = nil)
-      if width.is_a?(Hash)
-        options = width
-        @height = options[:height].to_f
-        @width = options[:width].to_f
-        @modifier = options[:modifier]
-        @orientation = options[:orientation].to_i
-      else
-        @height = height.to_f
-        @width  = width.to_f
-        @modifier = modifier
-      end
-    end
-    
-        # Returns the Rails.env constant.
-    def rails_env attachment, style_name
-      Rails.env
-    end
-    
-            def no_error_when_valid?
-          @file = StringIO.new('.')
-          @subject.send(@attachment_name).assign(@file)
-          @subject.valid?
-          expected_message = [
-            @attachment_name.to_s.titleize,
-            I18n.t(:blank, scope: [:errors, :messages])
-          ].join(' ')
-          @subject.errors.full_messages.exclude?(expected_message)
-        end
-      end
+        
+            def defaults_deprecate_type(old, current)
+      Jekyll.logger.warn 'Defaults:', 'The '#{old}' type has become '#{current}'.'
+      Jekyll.logger.warn 'Defaults:', 'Please update your front-matter defaults to use \
+                        'type: #{current}'.'
     end
   end
 end
 
     
-              @subject.send(@attachment_name).post_processing = false
-          @subject.send(@attachment_name).assign(file)
-          @subject.valid?
-          @subject.errors[:'#{@attachment_name}_file_size'].blank?
-        ensure
-          @subject.send(@attachment_name).post_processing = true
-        end
+          topic = Topic.find_by(id: topic_id)
     
-        def clear_processors!
-      @known_processors.try(:clear)
-    end
+      def revoke_moderation!
+    set_permission('moderator', false)
+  end
     
-          def failure_message
-        failure_message_helper
+        it 'adds the correct Set-Cookie header' do
+      get '/some/path', {}, 'HTTP_COOKIE' => 'rack.%73ession=EVIL_SESSION_TOKEN; rack.session=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
+    
+      %w(GET HEAD POST PUT DELETE).each do |method|
+    it 'accepts #{method} requests when allow_if is true' do
+      mock_app do
+        use Rack::Protection::HttpOrigin, :allow_if => lambda{|env| env.has_key?('HTTP_ORIGIN') }
+        run DummyApp
       end
-    
-        native.xpath(form_elements_xpath).map do |field|
-      case field.name
-      when 'input'
-        add_input_param(field, params)
-      when 'select'
-        add_select_param(field, params)
-      when 'textarea'
-        add_textarea_param(field, params)
-      end
-    end
-    merge_param!(params, button[:name], button[:value] || '') if button[:name]
-    
-          def drag_to(element)
-        raise NotImplementedError
-      end
-    
-          ##
-      # Checks if the page has the given title.
-      #
-      # @macro title_query_params
-      # @return [Boolean]
-      #
-      def has_title?(title, **options)
-        assert_title(title, options)
-      rescue Capybara::ExpectationNotMet
-        return false
-      end
-    
-          ##
-      #
-      # An XPath expression describing where on the page the element can be found
-      #
-      # @return [String]      An XPath expression
-      #
-      def path
-        native.path
-      end
-    
-        @current_scheme = new_uri.scheme
-    @current_host = new_uri.host
-    @current_port = new_uri.port
-    
-            def default
-          @options[:default]
-        end
-    
-          ##
-      #
-      # Right Click the Element
-      #
-      # @macro click_modifiers
-      # @return [Capybara::Node::Element]  The element
-      def right_click(*keys, **offset)
-        synchronize { base.right_click(keys, offset) }
-        self
-      end
-    
-      def flash_now(type, message, options = {})
-    respond_to do |wants|
-      wants.html do
-        flash.now[type] = message
-        if options[:render_action]
-          render options[:render_action]
-        end
-      end
-      wants.json { render :json => {:flash => {type => message}} }
+      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://any.domain.com')).to be_ok
     end
   end
     
-      def add_organization_to_page_title
-    page_title << organization.name
+      it 'accepts a session without changes to tracked parameters' do
+    session = {:foo => :bar}
+    get '/', {}, 'rack.session' => session
+    get '/', {}, 'rack.session' => session
+    expect(session[:foo]).to eq(:bar)
   end
     
-      def destroy
-    @credential.destroy
-    redirect_to_with_json [organization, @server, :credentials]
-  end
-    
+      it 'allows passing on values in env' do
+    klass    = described_class
+    changer  = Struct.new(:app) do
+      def call(env)
+        env['foo.bar'] = 42
+        app.call(env)
+      end
     end
-
+    detector = Struct.new(:app) do
+      def call(env)
+        app.call(env)
+      end
+    end
     
-      private
+      def _nt_plugin
+    start_index = index
+    if node_cache[:plugin].has_key?(index)
+      cached = node_cache[:plugin][index]
+      if cached
+        cached = SyntaxNode.new(input, index...(index + 1)) if cached == true
+        @index = cached.interval.end
+      end
+      return cached
+    end
+    
+          def define_deprecated_accessors_for(option, opts, &block)
+        define_deprecated_writer_for(option, opts, &block)
+      end
+    
+    # These drivers are only used for testing driver switching.
+# They don't actually need to process javascript so use RackTest
+    
+    Before do
+  Capybara.use_default_driver
+end
+    
+    module Capybara
+  class Selector
+    module Filters
+      class Base
+        def initialize(name, block, **options)
+          @name = name
+          @block = block
+          @options = options
+          @options[:valid_values] = [true, false] if options[:boolean]
+        end
+    
+      it 'should not accept the alert if the text doesnt match' do
+    expect do
+      @session.accept_alert 'Incorrect Text' do
+        @session.click_link('Open alert')
+      end
+    end.to raise_error(Capybara::ModalNotFound)
+  end
+    
+      it 'should return the message presented' do
+    message = @session.accept_prompt with: 'the response' do
+      @session.click_link('Open prompt')
+    end
+    expect(message).to eq('Prompt opened')
+  end
+end
