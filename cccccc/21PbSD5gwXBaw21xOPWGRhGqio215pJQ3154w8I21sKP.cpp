b81618bcd5125f843ce93c9298ff93fbd7eedd6f
SUNsung <weatherius@gmail.com>
2018-05-17 22:55:30 +0300

    Rewrite [21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp] File in: 2018-05-17 22:55:30

--- cccccc/21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp
+++ cccccc/21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp
@@ -1,211 +1,194 @@
 
         
-        void Base::CallSync(const std::string& method,
-                    const base::ListValue& arguments,
-                    base::ListValue* result) {
-  NOTREACHED() << 'Uncatched callAsync in Base'
-               << ' method:' << method
-               << ' arguments:' << arguments;
+        #else // USE_GCC_FAST_TLS
+    
+    #include 'hphp/runtime/vm/jit/vasm-unit.h'
+    
+    /*
+ * Block of Vinstrs, managed by Vunit.
+ *
+ * A Vblock, in addition to containing a Vinstr stream, also specifies where it
+ * should be emitted to.
+ */
+struct Vblock {
+  explicit Vblock(AreaIndex area_idx, uint64_t w)
+    : area_idx(area_idx)
+    , weight(w) {}
+    }
+    
+        if (mpz_sgn(gmpData) < 0) {
+      mpz_clear(gmpData);
+    }
+    
+    #ifdef HAVE_NUMA
+    
+    inline void initNuma() {}
+inline constexpr int next_numa_node(std::atomic_int& curr_node) { return 0; }
+inline constexpr int num_numa_nodes() { return 1; }
+inline void numa_interleave(void* start, size_t size) {}
+inline void numa_bind_to(void* start, size_t size, int node) {}
+inline constexpr bool numa_node_allowed(int node) { return true; }
+    
+    /*
+ * Helpers for unconditional and conditional jumps.
+ */
+void surpriseCheck(IRGS&);
+void surpriseCheck(IRGS&, Offset);
+void jmpImpl(IRGS&, Offset);
+void implCondJmp(IRGS&, Offset taken, bool negate, SSATmp*);
+    
+    /*
+ * dlopen() the embedded shared object given by `desc'.
+ *
+ * Unfortunately, there's no way to do the equivalent of dlopen() on data
+ * within another file, or even in memory.  This means we have to copy the
+ * section into a temporary file and then dlopen() that.
+ *
+ * Returns the result of dlopen() on success, else nullptr.  Also logs the
+ * failure condition with Logger on failure.
+ */
+void* dlopen_embedded_data(const embedded_data& desc, char* tmp_filename);
+    
+    
+    {/////////////////////////////////////////////////////////////////////////////
+}
+#endif
+
+    
+    TEST(LogTest, ReadThirdStart) {
+  CheckInitialOffsetRecord(20014, 2);
 }
     
-    bool MenuDelegate::GetIconForCommandId(int command_id,
-                                       gfx::Image* icon) const {
-  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
-  if (!item)
-    return false;
-  if (item->icon_.IsEmpty())
-    return false;
-    }
     
-    typedef std::map<std::string,std::string> KeyMap;
+    {  edit.SetComparatorName('foo');
+  edit.SetLogNumber(kBig + 100);
+  edit.SetNextFile(kBig + 200);
+  edit.SetLastSequence(kBig + 1000);
+  TestEncodeDecode(edit);
+}
     
-    class NwAppCloseAllWindowsFunction : public AsyncExtensionFunction {
- public:
-  NwAppCloseAllWindowsFunction() {}
-    }
+    // Helper class that locks a mutex on construction and unlocks the mutex when
+// the destructor of the MutexLock object is invoked.
+//
+// Typical usage:
+//
+//   void MyClass::MyMethod() {
+//     MutexLock l(&mu_);       // mu_ is an instance variable
+//     ... some complex code, possibly with multiple return paths ...
+//   }
     
-    class NwMenuGetNSStringWithFixupFunction : public NWSyncExtensionFunction {
- public:
-  NwMenuGetNSStringWithFixupFunction(){}
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    
- protected:
-  ~NwMenuGetNSStringWithFixupFunction() override {}
-    
-  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringWithFixup', UNKNOWN)
+    // A very simple random number generator.  Not especially good at
+// generating truly random bits, but good enough for our needs in this
+// package.
+class Random {
  private:
-  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringWithFixupFunction);
-};
-    
-        TableBuilder* builder = new TableBuilder(options, file);
-    meta->smallest.DecodeFrom(iter->key());
-    for (; iter->Valid(); iter->Next()) {
-      Slice key = iter->key();
-      meta->largest.DecodeFrom(key);
-      builder->Add(key, iter->value());
+  uint32_t seed_;
+ public:
+  explicit Random(uint32_t s) : seed_(s & 0x7fffffffu) {
+    // Avoid bad seeds.
+    if (seed_ == 0 || seed_ == 2147483647L) {
+      seed_ = 1;
     }
-    
-    void leveldb_writeoptions_set_sync(
-    leveldb_writeoptions_t* opt, unsigned char v) {
-  opt->rep.sync = v;
-}
-    
-    static std::string IKey(const std::string& user_key,
-                        uint64_t seq,
-                        ValueType vt) {
-  std::string encoded;
-  AppendInternalKey(&encoded, ParsedInternalKey(user_key, seq, vt));
-  return encoded;
-}
-    
-    // Return the name of a temporary file owned by the db named 'dbname'.
-// The result will be prefixed with 'dbname'.
-extern std::string TempFileName(const std::string& dbname, uint64_t number);
-    
-      fname = TableFileName('bar', 200);
-  ASSERT_EQ('bar/', std::string(fname.data(), 4));
-  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
-  ASSERT_EQ(200, number);
-  ASSERT_EQ(kTableFile, type);
-    
-      for (size_t i = 0; i < compact_pointers_.size(); i++) {
-    PutVarint32(dst, kCompactPointer);
-    PutVarint32(dst, compact_pointers_[i].first);  // level
-    PutLengthPrefixedSlice(dst, compact_pointers_[i].second.Encode());
   }
-    
-    void WriteBatch::Put(const Slice& key, const Slice& value) {
-  WriteBatchInternal::SetCount(this, WriteBatchInternal::Count(this) + 1);
-  rep_.push_back(static_cast<char>(kTypeValue));
-  PutLengthPrefixedSlice(&rep_, key);
-  PutLengthPrefixedSlice(&rep_, value);
-}
-    
-        const char* benchmarks = FLAGS_benchmarks;
-    while (benchmarks != NULL) {
-      const char* sep = strchr(benchmarks, ',');
-      Slice name;
-      if (sep == NULL) {
-        name = benchmarks;
-        benchmarks = NULL;
-      } else {
-        name = Slice(benchmarks, sep - benchmarks);
-        benchmarks = sep + 1;
-      }
+  uint32_t Next() {
+    static const uint32_t M = 2147483647L;   // 2^31-1
+    static const uint64_t A = 16807;  // bits 14, 8, 7, 5, 2, 1, 0
+    // We are computing
+    //       seed_ = (seed_ * A) % M,    where M = 2^31-1
+    //
+    // seed_ must not be zero or M, or else all subsequent computed values
+    // will be zero or M respectively.  For all other values, seed_ will end
+    // up cycling through every number in [1,M-1]
+    uint64_t product = seed_ * A;
+    }
     }
     
-    #endif  // STORAGE_LEVELDB_HELPERS_MEMENV_MEMENV_H_
-
     
-    #include <string>
+    {}  // namespace leveldb
     
-    #ifdef USE_MKL2017DNN
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you use this binding you'll need to call 4 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXXX_NewFrame(), ImGui::Render() and ImGui_ImplXXXX_Shutdown().
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
     
-            NDArrayViewPtr deviceValueData;
-        if (device == valueData->Device())
+        io.KeyCtrl = s3eKeyboardGetState(s3eKeyLeftControl) == S3E_KEY_STATE_DOWN || s3eKeyboardGetState(s3eKeyRightControl) == S3E_KEY_STATE_DOWN;
+    io.KeyShift = s3eKeyboardGetState(s3eKeyLeftShift) == S3E_KEY_STATE_DOWN || s3eKeyboardGetState(s3eKeyRightShift) == S3E_KEY_STATE_DOWN;
+    io.KeyAlt = s3eKeyboardGetState(s3eKeyLeftAlt) == S3E_KEY_STATE_DOWN || s3eKeyboardGetState(s3eKeyRightAlt) == S3E_KEY_STATE_DOWN;
+    io.KeySuper = s3eKeyboardGetState(s3eKeyLeftWindows) == S3E_KEY_STATE_DOWN || s3eKeyboardGetState(s3eKeyRightWindows) == S3E_KEY_STATE_DOWN;
+    
+    // Win32 data
+static HWND                     g_hWnd = 0;
+static INT64                    g_Time = 0;
+static INT64                    g_TicksPerSecond = 0;
+static ImGuiMouseCursor         g_LastMouseCursor = ImGuiMouseCursor_COUNT;
+    
+        // Main loop
+    bool running = true;
+    while (running)
+    {
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        ALLEGRO_EVENT ev;
+        while (al_get_next_event(queue, &ev))
         {
-            if (readOnly)
-                deviceValueData = valueData->Alias(readOnly);
-            else
-                deviceValueData = valueData;
+            ImGui_ImplA5_ProcessEvent(&ev);
+            if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) 
+                running = false;
+            if (ev.type == ALLEGRO_EVENT_DISPLAY_RESIZE)
+            {
+                ImGui_ImplA5_InvalidateDeviceObjects();
+                al_acknowledge_resize(display);
+                Imgui_ImplA5_CreateDeviceObjects();
+            }
         }
-        else
-            deviceValueData = valueData->DeepClone(device, readOnly);
-    
-    public:
-    // assign a sub-rectangle from a 0-based matrix of the same size
-    void assignpatch(const ssematrixbase &patch, const size_t i0, const size_t i1, const size_t j0, const size_t j1)
-    {
-        auto &us = *this;
-        assert(i1 - i0 == patch.rows() && j1 - j0 == patch.cols());
-        assert(i0 <= i1 && j0 <= j1);
-        assert(i1 <= rows() && j1 <= cols());
+        ImGui_ImplA5_NewFrame();
     }
     
-        if (newNode->NodeName() != nodeName) // TODO: This was not tested for earlier; I hope no code depends on this.
-        InvalidArgument('ChangeNode: newNode must have the same name as the old node.');
-    
-        // Note: Do NOT use cudaEventBlockingSync (which supposedly yields the process)--it will totally break cudaEventSynchronize(), causing it to take 50 or 100 ms randomly.
-    // NOTE: We never saw this in reading prefetch.
-    unsigned flags = cudaEventDisableTiming;
-    if (blocking)
-        flags |= cudaEventBlockingSync;
-    
-        // first try if a NetworkBuilder is present
-    function<ComputationNetworkPtr(DEVICEID_TYPE)> createNetworkFn;
-    bool gotIt = TryGetNetworkFactory<ConfigRecordType, ElemType>(config, createNetworkFn);
-    if (gotIt)
+        // Main loop
+    MSG msg;
+    ZeroMemory(&msg, sizeof(msg));
+    while (msg.message != WM_QUIT)
     {
-        // We have several ways to create a network.
-        net = createNetworkFn(deviceId);
-        if (outputNodeNames.size() > 0)
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
         {
-            net->InvalidateCompiledNetwork();
-            PatchOutputNodes(net, outputNodeNames, outputNodeNamesVector);
-            net->CompileNetwork();
-            // BUGBUG: This will generate double Validation output in the log
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+            continue;
         }
+        ImGui_ImplDX10_NewFrame();
     }
-    else // no NetworkBuilder given: load from 'modelPath'
+    
+        // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them. 
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple. 
+    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Read 'misc/fonts/README.txt' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Roboto-Medium.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Cousine-Regular.ttf', 15.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/DroidSans.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/ProggyTiny.ttf', 10.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF('c:\\Windows\\Fonts\\ArialUni.ttf', 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+    //IM_ASSERT(font != NULL);
+    
+        // Main loop
+    while (!glfwWindowShouldClose(window))
     {
-        wstring modelPath = config(L'modelPath');
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        glfwPollEvents();
+        ImGui_ImplGlfwGL3_NewFrame();
     }
     
-        const wstring modelPath = config('modelPath');
-    wstring dbnModelPath = config('dbnModelPath');
-    
-                // make sure that there are some nodes to copy to
-            if (nodesOut.size() == 0)
-                RuntimeError('Setting a single input to multiple outputs requires the multiple outputs to exist. In %ls = %ls, %ls does not match any nodes.', nameOut.c_str(), name.c_str(), nameOut.c_str());
-    
-        // SaveData - save data in the file/files
-    // recordStart - Starting record number
-    // matricies - a map of section name (section:subsection) to data pointer. Data sepcifications from config file will be used to determine where and how to save data
-    // numRecords - number of records we are saving, can be zero if not applicable
-    // datasetSize - size of the dataset (in records)
-    // byteVariableSized - for variable sized data, size of current block to be written, zero when not used, or ignored if not variable sized data
-    virtual bool SaveData(size_t recordStart, const std::map<std::wstring, void*, nocase_compare>& matrices, size_t numRecords, size_t datasetSize, size_t byteVariableSized = 0);
-    
-        void        (*SetStaticObjectField)(JNIEnv*, jclass, jfieldID, jobject);
-    void        (*SetStaticBooleanField)(JNIEnv*, jclass, jfieldID, jboolean);
-    void        (*SetStaticByteField)(JNIEnv*, jclass, jfieldID, jbyte);
-    void        (*SetStaticCharField)(JNIEnv*, jclass, jfieldID, jchar);
-    void        (*SetStaticShortField)(JNIEnv*, jclass, jfieldID, jshort);
-    void        (*SetStaticIntField)(JNIEnv*, jclass, jfieldID, jint);
-    void        (*SetStaticLongField)(JNIEnv*, jclass, jfieldID, jlong);
-    void        (*SetStaticFloatField)(JNIEnv*, jclass, jfieldID, jfloat);
-    void        (*SetStaticDoubleField)(JNIEnv*, jclass, jfieldID, jdouble);
-    
-    /* static */ void Config::destroy(Config * node)
-{
-    delete node;
-}
-    
-    #pragma once
-    
-    double Node::getFlexShrink(void) const
-{
-    return YGNodeStyleGetFlexShrink(m_node);
-}
-    
-        void setFlex(double flex);
-    void setFlexBasis(double flexBasis);
-    void setFlexBasisPercent(double flexBasis);
-    void setFlexBasisAuto();
-    void setFlexGrow(double flexGrow);
-    void setFlexShrink(double flexShrink);
-    
-    // Keeps a thread-local reference to the current thread's JNIEnv.
-struct Environment {
-  // May be null if this thread isn't attached to the JVM
-  FBEXPORT static JNIEnv* current();
-  static void initialize(JavaVM* vm);
-    }
-    
-    
-    {}
-
-    
-    
-    {} // namespace facebook
-#endif // FBASSERT_H
+                ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our windows open/close state
+            ImGui::Checkbox('Another Window', &show_another_window);
