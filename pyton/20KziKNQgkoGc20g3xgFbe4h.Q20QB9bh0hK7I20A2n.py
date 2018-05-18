f06957d2c9614f20bcaf9d27cf9c63beeda9105a
SUNsung <weatherius@gmail.com>
2018-05-18 23:47:07 +0300

    Rewrite [20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py] File in: 2018-05-18 23:47:06

--- pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
+++ pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
@@ -1,158 +1,73 @@
 
         
-        
-@pytest.mark.functional
-def test_refuse_with_confirmation(proc, TIMEOUT):
-    refuse_with_confirmation(proc, TIMEOUT)
+            with open('README.md', 'w+') as sorted_file:
+        sorted_file.write(final_README)
     
-            input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/targetHttpProxies'
-        actual = GCPUtils.parse_gcp_url(input_url)
-        self.assertEquals('compute', actual['service'])
-        self.assertEquals('v1', actual['api_version'])
-        self.assertEquals('myproject', actual['project'])
-        self.assertFalse('global' in actual)
-        self.assertEquals('targetHttpProxies', actual['resource_name'])
+        __slots__ = ('serializer',)
     
-    import re
+            app.config.from_envvar('YOURAPPLICATION_SETTINGS')
     
-        return results
-
-    
-                if key in mapping:
-                display.warning(u'While constructing a mapping from {1}, line {2}, column {3}, found a duplicate dict key ({0}).'
-                                u' Using last defined value only.'.format(key, *mapping.ansible_pos))
-    
-                else:
-                raise RuntimeError('DFA bang!')
-            
-        finally:
-            input.rewind(mark)
-    
-        def getText(self):
-        '''@brief Get the text of the token.
-    
-    from google.appengine._internal.antlr3.constants import EOF
-from google.appengine._internal.antlr3.exceptions import NoViableAltException, BacktrackingFailed
-    
-        @mock.patch('certbot.notify.smtplib.LMTP')
-    @mock.patch('certbot.notify.subprocess.Popen')
-    def test_smtp_failure(self, mock_popen, mock_lmtp):
-        from certbot.notify import notify
-        lmtp_obj = mock.MagicMock()
-        mock_lmtp.return_value = lmtp_obj
-        lmtp_obj.sendmail.side_effect = socket.error(17)
-        proc = mock.MagicMock()
-        mock_popen.return_value = proc
-        self.assertTrue(notify('Goose', 'auntrhody@example.com',
-                               'The old grey goose is dead.'))
-        self.assertEqual(lmtp_obj.sendmail.call_count, 1)
-        self.assertEqual(proc.communicate.call_count, 1)
-    
-        def test_big_queue_popleft(self):
-        pass
-        d = deque()
-        append, pop = d.append, d.popleft
-        for i in range(BIG):
-            append(i)
-        for i in range(BIG):
-            x = pop()
-            if x != i:
-                self.assertEqual(x, i)
-    
-        def test_string_with_utf8_bom(self):
-        # see #18958
-        bom_json = '[1,2,3]'.encode('utf-8-sig').decode('utf-8')
-        with self.assertRaises(self.JSONDecodeError) as cm:
-            self.loads(bom_json)
-        self.assertIn('BOM', str(cm.exception))
-        with self.assertRaises(self.JSONDecodeError) as cm:
-            self.json.load(StringIO(bom_json))
-        self.assertIn('BOM', str(cm.exception))
-        # make sure that the BOM is not detected in the middle of a string
-        bom_in_str = ''{}''.format(''.encode('utf-8-sig').decode('utf-8'))
-        self.assertEqual(self.loads(bom_in_str), '\ufeff')
-        self.assertEqual(self.json.load(StringIO(bom_in_str)), '\ufeff')
-    
-    def factorial(n, m):
-    if (n > m):
-        return factorial(m, n)
-    elif m == 0:
-        return 1
-    elif n == m:
-        return n
-    else:
-        return factorial(n, (n+m)//2) * factorial((n+m)//2 + 1, m)
-    
-    def evalString(s):
-    assert s.startswith(''') or s.startswith('''), repr(s[:1])
-    q = s[0]
-    if s[:3] == q*3:
-        q = q*3
-    assert s.endswith(q), repr(s[-len(q):])
-    assert len(s) >= 2*len(q)
-    s = s[len(q):-len(q)]
-    return re.sub(r'\\(\'|\'|\\|[abfnrtv]|x.{0,2}|[0-7]{1,3})', escape, s)
-    
-        def get_file(self, key):
-        '''Return a file-like representation or raise a KeyError.'''
-        try:
-            f = open(os.path.join(self._path, str(key)), 'rb')
-        except OSError as e:
-            if e.errno == errno.ENOENT:
-                raise KeyError('No message with key: %s' % key)
-            else:
-                raise
-        return _ProxyFile(f)
-    
-            NOTE: the 'message id' form isn't supported by XOVER
-        '''
-        cmd = 'OVER' if 'OVER' in self._caps else 'XOVER'
-        if isinstance(message_spec, (tuple, list)):
-            start, end = message_spec
-            cmd += ' {0}-{1}'.format(start, end or '')
-        elif message_spec is not None:
-            cmd = cmd + ' ' + message_spec
-        resp, lines = self._longcmdstring(cmd, file)
-        fmt = self._getoverviewfmt()
-        return resp, _parse_overview(lines, fmt)
-    
-        _legal_actions = (ACTION_REPLACE, ACTION_APPEND_AS_CHILDREN,
-                      ACTION_INSERT_AFTER, ACTION_INSERT_BEFORE)
-    
-            a = 0
-        b = 2 * sys.maxsize
-        expected_len = b - a
-        x = range(a, b)
-        self.assertIn(a, x)
-        self.assertNotIn(b, x)
-        self.assertRaises(OverflowError, len, x)
-        self.assertTrue(x)
-        self.assertEqual(_range_len(x), expected_len)
-        self.assertEqual(x[0], a)
-        idx = sys.maxsize+1
-        self.assertEqual(x[idx], a+idx)
-        self.assertEqual(x[idx:idx+1][0], a+idx)
-        with self.assertRaises(IndexError):
-            x[-expected_len-1]
-        with self.assertRaises(IndexError):
-            x[expected_len]
-    
-        def test_boolean(self):
-        self.assertEqual(True & 1, 1)
-        self.assertNotIsInstance(True & 1, bool)
-        self.assertIs(True & True, True)
-    
-        def test_reversed_pickle(self):
-        orig = self.type2test([4, 5, 6, 7])
-        data = [10, 11, 12, 13, 14, 15]
-        for proto in range(pickle.HIGHEST_PROTOCOL + 1):
-            # initial iterator
-            itorig = reversed(orig)
-            d = pickle.dumps((itorig, orig), proto)
-            it, a = pickle.loads(d)
-            a[:] = data
-            self.assertEqual(type(it), type(itorig))
-            self.assertEqual(list(it), data[len(orig)-1::-1])
+            if trv is not None:
+            return trv
+        raise TemplateNotFound(template)
     
     
-if __name__ == '__main__':
+class BadNonceTest(unittest.TestCase):
+    '''Tests for acme.errors.BadNonce.'''
+    
+        def __exit__(self, typ, value, tb):
+        if value is not None:
+            self.logger.error('Uncaught exception', exc_info=(typ, value, tb))
+            raise _QuietException
+    
+        def create_client(self, **kwargs):
+        return CurlAsyncHTTPClient(force_instance=True,
+                                   defaults=dict(allow_ipv6=False),
+                                   **kwargs)
+    
+        def test_types(self):
+        response = self.fetch('/hello')
+        self.assertEqual(type(response.body), bytes)
+        self.assertEqual(type(response.headers['Content-Type']), str)
+        self.assertEqual(type(response.code), int)
+        self.assertEqual(type(response.effective_url), str)
+    
+        def test_large_body_buffered(self):
+        with ExpectLog(gen_log, '.*Content-Length too long'):
+            response = self.fetch('/buffered', method='PUT', body=b'a' * 10240)
+        self.assertEqual(response.code, 400)
+    
+        def test_exception_logging(self):
+        '''Uncaught exceptions get logged by the IOLoop.'''
+        # Use a NullContext to keep the exception from being caught by
+        # AsyncTestCase.
+        with NullContext():
+            self.io_loop.add_callback(lambda: 1 / 0)
+            self.io_loop.add_callback(self.stop)
+            with ExpectLog(app_log, 'Exception in callback'):
+                self.wait()
+    
+            with closing(self.create_client(resolver=TimeoutResolver())) as client:
+            with ignore_deprecation():
+                response = yield client.fetch(self.get_url('/hello'),
+                                              connect_timeout=timeout,
+                                              raise_error=False)
+                self.assertEqual(response.code, 599)
+                self.assertTrue(timeout_min < response.request_time < timeout_max,
+                                response.request_time)
+                self.assertEqual(str(response.error), 'Timeout while connecting')
+    
+            with self.assertRaises(StackContextInconsistentError):
+            f()
+            self.wait()
+        # Cleanup: to avoid GC warnings (which for some reason only seem
+        # to show up on py33-asyncio), invoke the callback (which will do
+        # nothing since the gen.Runner is already finished) and delete it.
+        self.callback()
+        del self.callback
+    
+        def test_failed_write_error(self):
+        with ExpectLog(app_log, 'Uncaught exception'):
+            response = self.fetch('/failed_write_error')
+            self.assertEqual(response.code, 500)
+            self.assertEqual(b'', response.body)
