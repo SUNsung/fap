68df2b531e5d8d66ebbd572305723fce88172523
SUNsung <weatherius@gmail.com>
2018-05-19 01:10:31 +0300

    Rewrite [28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb] File in: 2018-05-19 01:10:30

--- ruby/28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb
+++ ruby/28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb
@@ -1,32 +1,23 @@
 
         
-            def register_sprockets
-      Sprockets.append_path(stylesheets_path)
-      Sprockets.append_path(fonts_path)
-      Sprockets.append_path(javascripts_path)
+            # Description formatted to work well as page title when viewing gist
+    if f.core_formula?
+      descr = '#{f.name} on #{OS_VERSION} - Homebrew build logs'
+    else
+      descr = '#{f.name} (#{f.full_name}) on #{OS_VERSION} - Homebrew build logs'
     end
-  end
-end
+    url = create_gist(files, descr)
     
-      # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+        private
     
-          def delete_authorization_session_variables
-        session.delete(:client_id)
-        session.delete(:response_type)
-        session.delete(:redirect_uri)
-        session.delete(:scopes)
-        session.delete(:state)
-        session.delete(:nonce)
-      end
+            def autocorrect(node)
+          center = multiple_compare?(node)
+          new_center = '#{center.source} && #{center.source}'
     
-          attr_reader :agent
+            def on_if(node)
+          return unless nested_variable_comparison?(node.condition)
+          add_offense(node)
+        end
     
-          def define_deprecated_accessors_for(option, opts, &block)
-        define_deprecated_writer_for(option, opts, &block)
-      end
-    
-          def clear
-        entries.clear
-      end
+    RSpec.describe RuboCop::Cop::Style::StringMethods, :config do
+  subject(:cop) { described_class.new(config) }
