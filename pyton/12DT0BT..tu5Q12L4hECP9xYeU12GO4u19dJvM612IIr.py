6c51da896784d0491e438a76fee8d8914a47bb3f
SUNsung <weatherius@gmail.com>
2018-05-18 19:46:38 +0300

    Rewrite [12DT0BT..tu5Q12L4hECP9xYeU12GO4u19dJvM612IIr.py] File in: 2018-05-18 19:46:37

--- pyton/12DT0BT..tu5Q12L4hECP9xYeU12GO4u19dJvM612IIr.py
+++ pyton/12DT0BT..tu5Q12L4hECP9xYeU12GO4u19dJvM612IIr.py
@@ -1,153 +1,124 @@
 
         
         
-def dispatch_hook(key, hooks, hook_data, **kwargs):
-    '''Dispatches a hook dictionary on a given piece of data.'''
-    hooks = hooks or dict()
-    hooks = hooks.get(key)
-    if hooks:
-        if hasattr(hooks, '__call__'):
-            hooks = [hooks]
-        for hook in hooks:
-            _hook_data = hook(hook_data, **kwargs)
-            if _hook_data is not None:
-                hook_data = _hook_data
-    return hook_data
+packages = {
+    package_name: get_info(package_name) for package_name in PACKAGES
+}
+    
+            if env.stdout_isatty:
+            # Use the encoding supported by the terminal.
+            output_encoding = env.stdout_encoding
+        else:
+            # Preserve the message encoding.
+            output_encoding = self.msg.encoding
+    
+    
+@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
+@pytest.mark.parametrize('filename', filenames)
+def test_rst_file_syntax(filename):
+    p = subprocess.Popen(
+        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
+    err = p.communicate()[1]
+    assert p.returncode == 0, err.decode('utf8')
 
     
-    from requests.structures import CaseInsensitiveDict, LookupDict
+    
+@mock.patch('httpie.core.get_response')
+def test_error_traceback(get_response):
+    exc = ConnectionError('Connection aborted')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    with raises(ConnectionError):
+        main(['--ignore-stdin', '--traceback', 'www.google.com'])
     
     
-@pytest.mark.skipif(sys.version_info[:2] != (2,6), reason='Only run on Python 2.6')
-def test_system_ssl_py26():
-    '''OPENSSL_VERSION_NUMBER isn't provided in Python 2.6, verify we don't
-    blow up in this case.
-    '''
-    assert info()['system_ssl'] == {'version': ''}
-    
-    Requests is an HTTP library, written in Python, for human beings. Basic GET
-usage:
-    
-    from .auth import _basic_auth_str
-from .compat import cookielib, is_py3, OrderedDict, urljoin, urlparse
-from .cookies import (
-    cookiejar_from_dict, extract_cookies_to_jar, RequestsCookieJar, merge_cookies)
-from .models import Request, PreparedRequest, DEFAULT_REDIRECT_LIMIT
-from .hooks import default_hooks, dispatch_hook
-from ._internal_utils import to_native_string
-from .utils import to_key_val_list, default_headers
-from .exceptions import (
-    TooManyRedirects, InvalidSchema, ChunkedEncodingError, ContentDecodingError)
-    
-        'hostport': re.compile(
-        r'''^
-            ((?:                        # We want to match:
-                [^:\[\]]                # (a non-range character
-                |                       # ...or...
-                \[[^\]]*\]              # a complete bracketed expression)
-            )*)                         # repeated as many times as possible
-            :([0-9]+)                   # followed by a port number
-            $
-        ''', re.X
-    ),
-    
-    
-def test_classify_build_fn():
-    clf = KerasClassifier(
-        build_fn=build_fn_clf, hidden_dims=hidden_dims,
-        batch_size=batch_size, epochs=epochs)
-    
-    
-WEIGHTS_PATH = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.5/inception_v3_weights_tf_dim_ordering_tf_kernels.h5'
-WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.5/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5'
-    
-        # Block 5
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv4')(x)
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
-    
-            inputs = Input((None, dim))
-        outputs = wrappers.Bidirectional(rnn(output_dim, dropout=dropout_rate,
-                                             recurrent_dropout=dropout_rate),
-                                         merge_mode=mode)(inputs)
-        model = Model(inputs, outputs)
-        model.compile(loss='mse', optimizer='sgd')
-        model.fit(x, y, epochs=1, batch_size=1)
-    
-        assert len(merge_layer._inbound_nodes) == 1
-    assert len(merge_layer._outbound_nodes) == 0
-    
-                # Test with `shuffle=True`
-            for x, y in generator.flow(images, np.arange(images.shape[0]),
-                                       shuffle=True, save_to_dir=str(tmpdir),
-                                       batch_size=3):
-                assert x.shape == images[:3].shape
-                # Check that the sequence is shuffled.
-                assert list(y) != [0, 1, 2]
-                break
-    
-    
-@pytest.mark.skipif((K.backend() != 'tensorflow'),
-                    reason='NASNets are supported only on TensorFlow')
-def test_nasnet():
-    app, last_dim = random.choice(NASNET_LIST)
-    _test_application_basic(app)
-    _test_application_notop(app, last_dim)
-    _test_application_variable_input_channels(app, last_dim)
-    _test_app_pooling(app, last_dim)
-    
-    from .. import backend as K
-from .. import activations
-from .. import initializers
-from .. import regularizers
-from .. import constraints
-from .recurrent import _generate_dropout_mask
-from .recurrent import _standardize_args
-    
-            for p, g, m, u in zip(params, grads, ms, us):
-    
-    print('Convert class vector to binary class matrix '
-      '(for use with categorical_crossentropy)')
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-print('y_train shape:', y_train.shape)
-print('y_test shape:', y_test.shape)
-    
-    
-MINIMUM = 80
-    
-    print 'PLUGIN_I18N_PATHS := ' + ','.join(os.path.relpath(plugin.path)
-                                         for plugin in plugins
-                                         if plugin.needs_translation)
-    
-    def is_api(subtype = ''):
-    return c.render_style and c.render_style.startswith(api_type(subtype))
-    
-    def api(type, cls):
-    tpm.add_handler(type, 'api', cls())
-    tpm.add_handler(type, 'api-html', cls())
-    tpm.add_handler(type, 'api-compact', cls())
-    
-        @require_oauth2_scope('creddits')
-    @validate(
-        VUser(),
-        user=VAccountByName('username'),
-        months=VInt('months', min=1, max=36),
-        timeout=VNotInTimeout(),
-    )
-    @api_doc(
-        api_section.gold,
-        uri='/api/v1/gold/give/{username}',
-    )
-    def POST_give(self, user, months, timeout):
-        self._gift_using_creddits(
-            recipient=user,
-            months=months,
-            proxying_for=request.POST.get('proxying_for'),
-        )
+def test_max_redirects(httpbin):
+    r = http('--max-redirects=1', '--follow', httpbin.url + '/redirect/3',
+             error_exit_ok=True)
+    assert r.exit_status == ExitStatus.ERROR_TOO_MANY_REDIRECTS
 
     
-            return self.redirect('/static/button/button%s.js' % buttontype,
-                             code=301)
+        The default behaviour is '{default}' (i.e., the response headers and body
+    is printed), if standard output is not redirected. If the output is piped
+    to another program or to a file, then only the response body is printed
+    by default.
+    
+    
+@keras_test
+def test_model_trainability_switch():
+    # a non-trainable model has no trainable weights
+    x = Input(shape=(1,))
+    y = Dense(2)(x)
+    model = Model(x, y)
+    model.trainable = False
+    assert model.trainable_weights == []
+    
+        # test if the state of a BiRNN is the concatenation of the underlying RNNs
+    y_merged = y_merged[-n_states * 2:]
+    y_forward = y_forward[-n_states:]
+    y_backward = y_backward[-n_states:]
+    for state_birnn, state_inner in zip(y_merged, y_forward + y_backward):
+        assert_allclose(state_birnn, state_inner, atol=1e-5)
+    
+        # we don't check names of first 2 layers (inputs) because
+    # ordering of same-level layers is not fixed
+    assert [layer.name for layer in final_model.layers][2:] == ['model', 'dense_4']
+    assert model.compute_mask([e, f], [None, None]) == [None, None]
+    assert final_model.compute_output_shape([(10, 32), (10, 32)]) == [(10, 7), (10, 64)]
+    
+        # Arguments
+        cell: A RNN cell instance. A RNN cell is a class that has:
+            - a `call(input_at_t, states_at_t)` method, returning
+                `(output_at_t, states_at_t_plus_1)`. The call method of the
+                cell can also take the optional argument `constants`, see
+                section 'Note on passing external constants' below.
+            - a `state_size` attribute. This can be a single integer
+                (single state) in which case it is
+                the number of channels of the recurrent state
+                (which should be the same as the number of channels of the cell output).
+                This can also be a list/tuple of integers
+                (one size per state). In this case, the first entry
+                (`state_size[0]`) should be the same as
+                the size of the cell output.
+        return_sequences: Boolean. Whether to return the last output.
+            in the output sequence, or the full sequence.
+        return_state: Boolean. Whether to return the last state
+            in addition to the output.
+        go_backwards: Boolean (default False).
+            If True, process the input sequence backwards and return the
+            reversed sequence.
+        stateful: Boolean (default False). If True, the last state
+            for each sample at index i in a batch will be used as initial
+            state for the sample of index i in the following batch.
+        input_shape: Use this argument to specify the shape of the
+            input when this layer is the first one in a model.
+    
+    if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    help_ = 'Load h5 model trained weights'
+    parser.add_argument('-w', '--weights', help=help_)
+    help_ = 'Use mse loss instead of binary cross entropy (default)'
+    parser.add_argument('-m', '--mse', help=help_, action='store_true')
+    args = parser.parse_args()
+    models = (encoder, decoder)
+    data = (x_test, y_test)
+    
+    
+class Pin:
+    host = 'http://img.hb.aicdn.com/'
+    
+    from ..common import *
+import re
+    
+    site_info = 'MioMio.tv'
+download = miomio_download
+download_playlist = playlist_not_supported('miomio')
+
+    
+    from xml.dom.minidom import parseString
+    
+            self.title = match1(content,
+                            r'setup\[\'title\'\] = '([^']+)';')
