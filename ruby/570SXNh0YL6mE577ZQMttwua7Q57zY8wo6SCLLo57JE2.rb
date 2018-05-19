2342d9c198a65bca9cd5330b68e293c7f19f52db
SUNsung <weatherius@gmail.com>
2018-05-19 19:09:39 +0300

    Rewrite [570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb] File in: 2018-05-19 19:09:38

--- ruby/570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb
+++ ruby/570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb
@@ -1,45 +1,109 @@
 
         
-              it 'does set the exclude directories' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            cloc(exclude_dir: 'test1,test2,build')
-          end').runner.execute(:test)
+          test 'helpers' do
+    assert_response_code_range 200..299, :successful?
+    assert_response_code_range [404],    :not_found?
+    assert_response_code_range 300..399, :redirection?
+    assert_response_code_range 500..599, :server_error?
+    assert_response_code_range 400..499, :client_error?
+  end
     
-          it 'increments the build number of the Xcode project' do
-        Fastlane::FastFile.new.parse('lane :test do
-          increment_build_number(xcodeproj: '.xcproject')
-        end').runner.execute(:test)
-    
-            expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to match(/cd .* && agvtool new-marketing-version 1.1.0/)
-      end
-    
-              box_name     = argv[0]
-          box_provider = argv[1].to_sym
-          box_version  = argv[2]
-    
-            # Otherwise, call it
-        cap_host.capability(name, *argv)
-    
-          def stages
-        names = Dir[stage_definitions].map { |f| File.basename(f, '.rb') }
-        assert_valid_stage_names(names)
-        names
-      end
-    
-        # The repository path according to the context
-    def repo_path
-      context.repo_path
+      class BareEmptyTest < ActiveSupport::TestCase
+    test 'response body is nil' do
+      controller = BareEmptyController.new
+      controller.set_request!(ActionDispatch::Request.empty)
+      controller.set_response!(BareController.make_response!(controller.request))
+      controller.index
+      assert_nil controller.response_body
     end
-    
-      desc 'Finish the deployment, clean up server(s).'
-  task :finishing do
   end
     
-    describe 'border-color' do
-  before(:all) do
-    ParserSupport.parse_file('library/border-color')
+      def test_parsing_json_doesnot_rescue_exception
+    req = Class.new(ActionDispatch::Request) do
+      def params_parsers
+        { json: Proc.new { |data| raise Interrupt } }
+      end
+    
+      class AssertMultipartSelectMailer < ActionMailer::Base
+    def test(options)
+      mail subject: 'Test e-mail', from: 'test@test.host', to: 'test <test@test.host>' do |format|
+        format.text { render plain: options[:text] }
+        format.html { render plain: options[:html] }
+      end
+    end
   end
     
-          expect('.all-buttons-focus').to have_ruleset(ruleset)
+    module AccountFinderConcern
+  extend ActiveSupport::Concern
+    
+    require_relative '../mastodon/snowflake'
+    
+    class ActivityPub::EmojiSerializer < ActiveModel::Serializer
+  include RoutingHelper
+    
+      def perform(user_id)
+    @user = User.find(user_id)
+    deliver_digest if @user.allows_digest_emails?
+  end
+    
+          it 'regenerates feed when sign in is older than two weeks' do
+        get :show
+    
+      def test_font_helper_with_suffix_question
+    assert_match %r(url\(['']?/assets/.*eot\?.*['']?\)), @css
+  end
+    
+    unless $LOAD_PATH.include? lib_path
+  $LOAD_PATH.unshift lib_path
+end
+
+    
+    
+  ['', 'www.'].each do |prefix|
+    begin
+      Timeout.timeout(10) do
+        doc = Hpricot(URI.parse('http://#{prefix}#{site}/').open)
+        doc.search('//form').each do |form|
+    
+        end # end of each_key
+  end # end of parse
+end
+    
+          case matched
+        when :ok
+          # Last command was successful, in addition most servers transmit a banner with the first +OK
+          case s[:last]
+            when nil
+              # Its the first +OK must include the banner, worst case its just +OK
+              s[:info]  = matches
+              s[:proto] = 'tcp'
+              s[:name]  = 'pop3'
+              report_service(s)
+    
+    
+    {	if ln =~ /;(read|write)_(handle|filename)=/
+		parts = ln.split(' ')
+		if (parts[0] == 'mov')
+			parts2 = parts[2].split('=')
+			label = parts2[0]
+			label.slice!(0,1)
+			old = parts2[1]
+			new = addrs[label]
+			#puts '%32s: %s -> %x' % [label, old, new]
+			replaces << [label, old, new.to_s(16)]
+		end
+	end
+}
+    
+    module Capistrano
+  module Doctor
+    # Prints table of all Capistrano-related gems and their version numbers. If
+    # there is a newer version of a gem available, call attention to it.
+    class GemsDoctor
+      include Capistrano::Doctor::OutputHelpers
+    
+      %w(quiet silent verbose).each do |switch|
+    it 'doesn't include --#{switch} in help' do
+      expect(help_output).not_to match(/--#{switch}/)
     end
   end
