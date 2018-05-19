f0d8d76e09187820bae118b3d2b10409369bc852
SUNsung <weatherius@gmail.com>
2018-05-19 03:36:36 +0300

    Rewrite [58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py] File in: 2018-05-19 03:36:35

--- pyton/58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py
+++ pyton/58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py
@@ -1,53 +1,121 @@
 
         
-            def __init__(self, request):
-        exc = request.routing_exception
-        buf = ['A request was sent to this URL (%s) but a redirect was '
-               'issued automatically by the routing system to '%s'.'
-               % (request.url, exc.new_url)]
+            assert_regression_works(reg)
     
-        def open_session(self, app, request):
-        s = self.get_signing_serializer(app)
-        if s is None:
-            return None
-        val = request.cookies.get(app.session_cookie_name)
-        if not val:
-            return self.session_class()
-        max_age = total_seconds(app.permanent_session_lifetime)
-        try:
-            data = s.loads(val, max_age=max_age)
-            return self.session_class(data)
-        except BadSignature:
-            return self.session_class()
+        if input_tensor is None:
+        img_input = Input(shape=input_shape)
+    else:
+        if not K.is_keras_tensor(input_tensor):
+            img_input = Input(tensor=input_tensor, shape=input_shape)
+        else:
+            img_input = input_tensor
     
-                def dispatch_request(self):
-                ...
+        branch3x3dbl = conv2d_bn(x, 64, 1, 1)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+    branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
     
+        # load weights
+    if weights == 'imagenet':
+        if include_top:
+            weights_path = get_file(
+                'resnet50_weights_tf_dim_ordering_tf_kernels.h5',
+                WEIGHTS_PATH,
+                cache_subdir='models',
+                md5_hash='a7b3fe01876f51b976af0dea6bc144eb')
+        else:
+            weights_path = get_file(
+                'resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
+                WEIGHTS_PATH_NO_TOP,
+                cache_subdir='models',
+                md5_hash='a268eb855778b3df3c7506639542a6af')
+        model.load_weights(weights_path)
+        if K.backend() == 'theano':
+            layer_utils.convert_all_kernels_in_model(model)
+    elif weights is not None:
+        model.load_weights(weights)
     
-def git_is_clean():
-    return Popen(['git', 'diff', '--quiet']).wait() == 0
+            original = np.prod(input_shape, dtype=int)
+        if unknown is not None:
+            if known == 0 or original % known != 0:
+                raise ValueError(msg)
+            output_shape[unknown] = original // known
+        elif original != known:
+            raise ValueError(msg)
     
+        for layer_class in [keras.layers.CuDNNGRU, keras.layers.CuDNNLSTM]:
+        model = keras.models.Sequential()
+        model.add(keras.layers.Embedding(10, input_size,
+                                         input_length=timesteps,
+                                         batch_input_shape=(num_samples,
+                                                            timesteps)))
+        layer = layer_class(units,
+                            return_sequences=False,
+                            stateful=True,
+                            weights=None)
+        model.add(layer)
+        model.compile(optimizer='sgd', loss='mse')
+        out1 = model.predict(np.ones((num_samples, timesteps)))
+        assert(out1.shape == (num_samples, units))
     
-class HashTable(object):
+        def get_initial_state(self, inputs):
+        # (samples, timesteps, rows, cols, filters)
+        initial_state = K.zeros_like(inputs)
+        # (samples, rows, cols, filters)
+        initial_state = K.sum(initial_state, axis=1)
+        shape = list(self.cell.kernel_shape)
+        shape[-1] = self.cell.filters
+        initial_state = self.cell.input_conv(initial_state,
+                                             K.zeros(tuple(shape)),
+                                             padding=self.cell.padding)
+        # Fix for Theano because it needs
+        # K.int_shape to work in call() with initial_state.
+        keras_shape = list(K.int_shape(inputs))
+        keras_shape.pop(1)
+        if K.image_data_format() == 'channels_first':
+            indices = 2, 3
+        else:
+            indices = 1, 2
+        for i, j in enumerate(indices):
+            keras_shape[j] = conv_utils.conv_output_length(
+                keras_shape[j],
+                shape[i],
+                padding=self.cell.padding,
+                stride=self.cell.strides[i],
+                dilation=self.cell.dilation_rate[i])
+        initial_state._keras_shape = keras_shape
     
-        def __init__(self, results):
-        self.results = results
-        self.next = next
+        # test include top
+    assert utils._obtain_input_shape(
+        input_shape=(3, 200, 200),
+        default_size=None,
+        min_size=139,
+        data_format='channels_first',
+        require_flatten=True) == (3, 200, 200)
     
-        def park_vehicle(self, vehicle):
-        for level in self.levels:
-            if level.park_vehicle(vehicle):
-                return True
-        return False
+        filename = os.path.join(model_name, 'vae_mean.png')
+    # display a 2D plot of the digit classes in the latent space
+    z_mean, _, _ = encoder.predict(x_test,
+                                   batch_size=batch_size)
+    plt.figure(figsize=(12, 10))
+    plt.scatter(z_mean[:, 0], z_mean[:, 1], c=y_test)
+    plt.colorbar()
+    plt.xlabel('z[0]')
+    plt.ylabel('z[1]')
+    plt.savefig(filename)
+    plt.show()
     
-            # put all lines in the file into a Python list
-        strings = f.readlines()
-        
-        # above line leaves trailing newline characters; strip them out
-        strings = [x.strip(u'\n') for x in strings]
-        
-        # remove empty-lines and comments
-        strings = [x for x in strings if x and not x.startswith(u'#')]
-        
-        # insert empty string since all are being removed
-        strings.insert(0, u'')
+        for i, yi in enumerate(grid_y):
+        for j, xi in enumerate(grid_x):
+            z_sample = np.array([[xi, yi]])
+            x_decoded = decoder.predict(z_sample)
+            digit = x_decoded[0].reshape(digit_size, digit_size)
+            figure[i * digit_size: (i + 1) * digit_size,
+                   j * digit_size: (j + 1) * digit_size] = digit
+    
+    from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.layers import Embedding
+from keras.layers import LSTM
+from keras.layers import Conv1D, MaxPooling1D
+from keras.datasets import imdb
