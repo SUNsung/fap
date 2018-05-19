99eb005086fce232875c8c048e4db48363c94ef6
SUNsung <weatherius@gmail.com>
2018-05-19 04:08:20 +0300

    Rewrite [293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb] File in: 2018-05-19 04:08:19

--- ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
+++ ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
@@ -1,126 +1,124 @@
 
         
-        invalids = []
-Parallel.each(links, in_threads: 4) do |link|
-  href = link.attribute('href').to_s
-  begin
-    case check_link(URI.join(BASE_URI, href))
-    when (200...300)
-      putc('.')
-    when (300..302)
-      putc('w')
+          def test_check_parameters
+    with_test_route_set do
+      get '/'
+      assert_equal '', @controller.response.body
     end
-  rescue => e
-    putc('F')
-    invalids << '#{href} (reason: #{e.message})'
   end
-end
     
-        def create_event(event)
-      return super unless dry_run?
-      if can_create_events?
-        event = build_event(event)
-        @dry_run_results[:events] << event.payload
-        event
-      else
-        error 'This Agent cannot create events!'
-      end
+          # Make list points stand on their own line
+      formatted.gsub!(/[ ]*([*]+) ([^*]*)/) { '  #{$1} #{$2.strip}\n' }
+      formatted.gsub!(/[ ]*([#]+) ([^#]*)/) { '  #{$1} #{$2.strip}\n' }
+    
+    # Disable available locale checks to avoid warnings running the test suite.
+I18n.enforce_available_locales = false
+    
+    class AssertSelectEmailTest < ActionMailer::TestCase
+  class AssertSelectMailer < ActionMailer::Base
+    def test(html)
+      mail body: html, content_type: 'text/html',
+        subject: 'Test e-mail', from: 'test@test.host', to: 'test <test@test.host>'
+    end
+  end
+    
+      test 'delivery method can be customized in subclasses not changing the parent' do
+    DeliveryMailer.delivery_method = :test
+    assert_equal :smtp, ActionMailer::Base.delivery_method
+    email = DeliveryMailer.welcome.deliver_now
+    assert_instance_of Mail::TestMailer, email.delivery_method
+  end
+    
+      def test_use_cache
+    assert_nothing_raised do
+      mail = HelperMailer.use_cache
+      assert_equal 'Greetings from a cache helper block', mail.body.encoded
     end
   end
 end
 
     
-      def present(payload)
-    if payload.is_a?(Hash)
-      payload = ActiveSupport::HashWithIndifferentAccess.new(payload)
-      MAIN_KEYS.each do |key|
-        return { :title => payload[key].to_s, :entries => present_hash(payload, key) } if payload.has_key?(key)
+      </body>
+</html>
+HTML
+CONTENT_NOT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+    
+        puts 'GC Stats:'
+    GC.start(full_mark: true, immediate_sweep: false)
+    puts JSON.pretty_generate(GC.stat)
+  end
+end
+    
+    config = File.expand_path '../lib/jekyll/mime.types', __dir__
+    
+          new_theme_name = args.join('_')
+      theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
+      if theme.path.exist?
+        Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.'
       end
     
-        validate :validate_evernote_options
-    
-      def tumblr
-    Tumblr.configure do |config|
-      config.consumer_key = tumblr_consumer_key
-      config.consumer_secret = tumblr_consumer_secret
-      config.oauth_token = tumblr_oauth_token
-      config.oauth_token_secret = tumblr_oauth_token_secret
-    end
-    
-    Tumblr::Client.new
-  end
-end
-    
-      def load_event
-    @event = current_user.events.find(params[:id])
-  end
-end
-
-    
-      def destroy
-    @job = Delayed::Job.find(params[:id])
-    
-          case Rails.env
-      when 'development'
-        config.eager_load = false
-      when 'test'
-        config.eager_load = false
-      when 'production'
-        config.eager_load = true
+    module Jekyll
+  module Filters
+    module GroupingFilters
+      # Group an array of items by a property
+      #
+      # input - the inputted Enumerable
+      # property - the property
+      #
+      # Returns an array of Hashes, each looking something like this:
+      #  {'name'  => 'larry'
+      #   'items' => [...] } # all the items where `property` == 'larry'
+      def group_by(input, property)
+        if groupable?(input)
+          groups = input.group_by { |item| item_property(item, property).to_s }
+          grouped_array(groups)
+        else
+          input
+        end
       end
+    
+          {
+          author: last_git_commit_formatted_with('%an'),
+          message: last_git_commit_formatted_with('%B'),
+          commit_hash: last_git_commit_formatted_with('%H'),
+          abbreviated_commit_hash: last_git_commit_formatted_with('%h')
+      }
     end
-  end
+    
+          it 'does set the source directory' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            cloc(source_directory: 'MyCoolApp')
+          end').runner.execute(:test)
+    
+                  s[:proto]='pop3'
+              s[:extra]='Failed Login. Banner: #{s[:banner]}'
+              report_auth_info(s)
+              print_status('Invalid POP3 Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]} (#{s[:banner].strip})')
+              s[:pass]=''
+          end
+        when nil
+          # No matches, no saved state
+        else
+          s[:last]=matched
+          sessions[s[:session]].merge!({k => matches})
+      end # end case matched
+    end # end of each_key
+  end # end of parse
 end
     
-        self.sigs.each_key do |k|
-      # There is only one pattern per run to test
-      matched = nil
-      matches = nil
-    
-        # We want to return immediatly if we do not have a packet which is handled by us
+      def parse(pkt)
+    # We want to return immediantly if	we do not have a packet which is handled by us
     return unless pkt.is_tcp?
-    return if (pkt.tcp_sport != 143 and pkt.tcp_dport != 143)
-    s = find_session((pkt.tcp_sport == 143) ? get_session_src(pkt) : get_session_dst(pkt))
-    s[:sname] ||= 'imap4'
-    
-    success = clsJavaCompile._invoke('CompileFromMemory','[Ljava.lang.String;[Ljava.lang.String;[Ljava.lang.String;', classNames, codez, compileOpts)
-    
-        _cal[ver].each_pair do |key, value|
-      cal[ver][key] = Array.new
-      cal[ver][key] << String.new
-      cal[ver][key][-1] << '#ifdef AIX%s' % ver.delete('.')
-      cal[ver][key][-1] << '\n'
-      cal[ver][key][-1] << '''.rjust(5)
-      value.each_byte do |c|
-        cal[ver][key][-1] << '\x%02x' % c
-      end
-      cal[ver][key][-1] << '''.ljust(7)
-      cal[ver][key][-1] << '/*  cal     r2,-%d(r29)' %
-          (65536 - value.unpack('nn')[1])
-      cal[ver][key][-1] << '*/'.rjust(15)
-      cal[ver][key][-1] << '\n'
-      cal[ver][key][-1] << '#endif'
-      cal[ver][key][-1] << '\n'
-    end
-    
-        def render(context)
-      if @img
-        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
-      else
-        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
-      end
-    end
-  end
-end
-    
-          Dir.chdir(code_path) do
-        code = file.read
-        @filetype = file.extname.sub('.','') if @filetype.nil?
-        title = @title ? '#{@title} (#{file.basename})' : file.basename
-        url = '/#{code_dir}/#{@file}'
-        source = '<figure class='code'><figcaption><span>#{title}</span> <a href='#{url}'>download</a></figcaption>\n'
-        source += '#{HighlightCode::highlight(code, @filetype)}</figure>'
-        TemplateWrapper::safe_wrap(source)
-      end
-    end
-  end
+    return if (pkt.tcp_sport != 80 and pkt.tcp_dport != 80)
+    s = find_session((pkt.tcp_sport == 80) ? get_session_src(pkt) : get_session_dst(pkt))
