28ba89a356fb1343196ad4e4da8c1801db85d3c3
SUNsung <weatherius@gmail.com>
2018-05-21 01:00:31 +0300

    Rewrite [54nUkZoVjnS1U54PzGrTkEx0CY54l3rSh8xSIjM54uS5.rb] File in: 2018-05-21 01:00:31

--- ruby/54nUkZoVjnS1U54PzGrTkEx0CY54l3rSh8xSIjM54uS5.rb
+++ ruby/54nUkZoVjnS1U54PzGrTkEx0CY54l3rSh8xSIjM54uS5.rb
@@ -1,134 +1,15 @@
-  def destroy_failed
-    Delayed::Job.where.not(failed_at: nil).delete_all
+
+        
+        require 'cocoapods'
     
-      def toggle_availability
-    @service = current_user.services.find(params[:id])
-    @service.toggle_availability!
+            target_module << if app.resolved_build_setting('SWIFT_OPTIMIZATION_LEVEL').values.any?
+                           <<-RUBY
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
     
-        if resource.errors.empty?
-      set_flash_message!(:notice, :confirmed)
-      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+            # !@group Private helpers
+    
+          #-----------------------------------------------------------------------#
     end
   end
-    
-      it 'decodes UTF-8 BMP codepoints' do
-    [ ['\xc2\x80',      [0x80]],
-      ['\xdf\xbf',      [0x7ff]],
-      ['\xe0\xa0\x80',  [0x800]],
-      ['\xef\xbf\xbf',  [0xffff]]
-    ].should be_computed_by(:unpack, 'U')
-  end
-    
-      class SubclassX < Struct
-  end
-    
-      def self.status_of_dying_thread_after_sleep
-    status = nil
-    t = dying_thread_ensures {
-      Thread.stop
-      status = Status.new(Thread.current)
-    }
-    Thread.pass while t.status and t.status != 'sleep'
-    t.wakeup
-    Thread.pass while t.status and t.status == 'sleep'
-    t.join
-    status
-  end
-    
-        10.times { sleep 0.1 if after_sleep1 != true }
-    10.times { sleep 0.1 if t.status and t.status != 'sleep' }
-    after_sleep2.should == false # t should be blocked on the second sleep
-    t.send(@method)
-    
-      # if rss_url already in existing opml file, use that; otherwise, do a lookup
-  rss_url = nil
-  existing_blog = xml.xpath('//outline[@htmlUrl='#{web_url}']').first if xml
-  if existing_blog
-    rss_url = existing_blog.attr('xmlUrl')
-    puts '#{name}: ALREADY HAVE'
-  end
-    
-            self.summary = 'The repl listens to commands on standard input'
-        self.description = <<-DESC
-          The repl listens to commands on standard input and prints their
-          result to standard output.
-          It accepts all the other ipc subcommands. The repl will signal the
-          end of output with the the ASCII CR+LF `\\n\\r`.
-        DESC
-    
-        def assign_file_information
-      instance_write(:file_name, cleanup_filename(@file.original_filename))
-      instance_write(:content_type, @file.content_type.to_s.strip)
-      instance_write(:file_size, @file.size)
-    end
-    
-        def define_flush_errors
-      @klass.send(:validates_each, @name) do |record, attr, value|
-        attachment = record.send(@name)
-        attachment.send(:flush_errors)
-      end
-    end
-    
-            def responds?
-          methods = @subject.instance_methods.map(&:to_s)
-          methods.include?('#{@attachment_name}') &&
-            methods.include?('#{@attachment_name}=') &&
-            methods.include?('#{@attachment_name}?')
-        end
-    
-            def matches? subject
-          @subject = subject
-          @subject = subject.new if subject.class == Class
-          error_when_not_valid? && no_error_when_valid?
-        end
-    
-            def matches? subject
-          @subject = subject
-          @subject = @subject.new if @subject.class == Class
-          lower_than_low? && higher_than_low? && lower_than_high? && higher_than_high?
-        end
-    
-            required = directories.map do |directory|
-          pathname = File.expand_path(Rails.root.join(directory, filename))
-          file_exists = File.exist?(pathname)
-          require pathname if file_exists
-          file_exists
-        end
-    
-            attachment_names.each do |attachment_name|
-          COLUMNS.each_pair do |column_name, column_type|
-            column_options = options.merge(options[column_name.to_sym] || {})
-            add_column(table_name, '#{attachment_name}_#{column_name}', column_type, column_options)
-          end
-        end
-      end
-    
-      node[:applications].each do |app, data|
-    template '/etc/monit.d/sidekiq_#{app}.monitrc' do 
-      owner 'root' 
-      group 'root' 
-      mode 0644 
-      source 'monitrc.conf.erb' 
-      variables({ 
-        :num_workers => worker_count,
-        :app_name => app, 
-        :rails_env => node[:environment][:framework_env] 
-      }) 
-    end
-    
-    module Sidekiq
-  module Extensions
-    ##
-    # Adds 'delay', 'delay_for' and `delay_until` methods to ActionMailer to offload arbitrary email
-    # delivery to Sidekiq.  Example:
-    #
-    #    UserMailer.delay.send_welcome_email(new_user)
-    #    UserMailer.delay_for(5.days).send_welcome_email(new_user)
-    #    UserMailer.delay_until(5.days.from_now).send_welcome_email(new_user)
-    class DelayedMailer
-      include Sidekiq::Worker
-    
-      puts '\n== Preparing database =='
-  system 'bin/rake db:setup'
+end
