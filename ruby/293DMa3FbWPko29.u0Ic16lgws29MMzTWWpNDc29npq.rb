6d19c375e85c9a8cf92fcef43a1c149bfe62304e
SUNsung <weatherius@gmail.com>
2018-05-20 08:28:23 +0300

    Rewrite [293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb] File in: 2018-05-20 08:28:22

--- ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
+++ ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
@@ -1,124 +1,24 @@
 
         
-          def test_check_parameters
-    with_test_route_set do
-      get '/'
-      assert_equal '', @controller.response.body
-    end
-  end
+          # Implemented by subclasses to provide default values for settings needed by
+  # this plugin. Typically done using the `set_if_empty` Capistrano DSL method.
+  #
+  # Example:
+  #
+  #   def set_defaults
+  #     set_if_empty :my_plugin_option, true
+  #   end
+  #
+  def set_defaults; end
     
-          # Make list points stand on their own line
-      formatted.gsub!(/[ ]*([*]+) ([^*]*)/) { '  #{$1} #{$2.strip}\n' }
-      formatted.gsub!(/[ ]*([#]+) ([^#]*)/) { '  #{$1} #{$2.strip}\n' }
-    
-    # Disable available locale checks to avoid warnings running the test suite.
-I18n.enforce_available_locales = false
-    
-    class AssertSelectEmailTest < ActionMailer::TestCase
-  class AssertSelectMailer < ActionMailer::Base
-    def test(html)
-      mail body: html, content_type: 'text/html',
-        subject: 'Test e-mail', from: 'test@test.host', to: 'test <test@test.host>'
-    end
-  end
-    
-      test 'delivery method can be customized in subclasses not changing the parent' do
-    DeliveryMailer.delivery_method = :test
-    assert_equal :smtp, ActionMailer::Base.delivery_method
-    email = DeliveryMailer.welcome.deliver_now
-    assert_instance_of Mail::TestMailer, email.delivery_method
-  end
-    
-      def test_use_cache
-    assert_nothing_raised do
-      mail = HelperMailer.use_cache
-      assert_equal 'Greetings from a cache helper block', mail.body.encoded
-    end
-  end
-end
-
-    
-      </body>
-</html>
-HTML
-CONTENT_NOT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
-    
-        puts 'GC Stats:'
-    GC.start(full_mark: true, immediate_sweep: false)
-    puts JSON.pretty_generate(GC.stat)
-  end
-end
-    
-    config = File.expand_path '../lib/jekyll/mime.types', __dir__
-    
-          new_theme_name = args.join('_')
-      theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
-      if theme.path.exist?
-        Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.'
+          it 'filters from ENV[HOSTS]' do
+        hosts = dsl.roles(:db)
+        all = dsl.roles(:all)
+        SSHKit::Coordinator.expects(:new).with(hosts).returns(@coordinator)
+        ENV['HOSTS'] = 'example5.com'
+        dsl.on(all)
       end
     
-    module Jekyll
-  module Filters
-    module GroupingFilters
-      # Group an array of items by a property
-      #
-      # input - the inputted Enumerable
-      # property - the property
-      #
-      # Returns an array of Hashes, each looking something like this:
-      #  {'name'  => 'larry'
-      #   'items' => [...] } # all the items where `property` == 'larry'
-      def group_by(input, property)
-        if groupable?(input)
-          groups = input.group_by { |item| item_property(item, property).to_s }
-          grouped_array(groups)
-        else
-          input
+            def scope
+          @stock_location.stock_movements.accessible_by(current_ability, :read)
         end
-      end
-    
-          {
-          author: last_git_commit_formatted_with('%an'),
-          message: last_git_commit_formatted_with('%B'),
-          commit_hash: last_git_commit_formatted_with('%H'),
-          abbreviated_commit_hash: last_git_commit_formatted_with('%h')
-      }
-    end
-    
-          it 'does set the source directory' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            cloc(source_directory: 'MyCoolApp')
-          end').runner.execute(:test)
-    
-                  s[:proto]='pop3'
-              s[:extra]='Failed Login. Banner: #{s[:banner]}'
-              report_auth_info(s)
-              print_status('Invalid POP3 Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]} (#{s[:banner].strip})')
-              s[:pass]=''
-          end
-        when nil
-          # No matches, no saved state
-        else
-          s[:last]=matched
-          sessions[s[:session]].merge!({k => matches})
-      end # end case matched
-    end # end of each_key
-  end # end of parse
-end
-    
-      def parse(pkt)
-    # We want to return immediantly if	we do not have a packet which is handled by us
-    return unless pkt.is_tcp?
-    return if (pkt.tcp_sport != 80 and pkt.tcp_dport != 80)
-    s = find_session((pkt.tcp_sport == 80) ? get_session_src(pkt) : get_session_dst(pkt))
