c01693e78eeee7113fa85639f915dba386e7c29d
SUNsung <weatherius@gmail.com>
2018-05-20 22:54:14 +0300

    Rewrite [02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py] File in: 2018-05-20 22:54:13

--- pyton/02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py
+++ pyton/02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py
@@ -1,108 +1,131 @@
 
         
-        Available hooks:
+            cmd = cmd.split()
     
-        while True:
-        more_to_read = select.select([sock], [], [], timeout)[0]
-        if not more_to_read:
-            break
+        old_layer = keras.layers.GlobalAveragePooling2D(dim_ordering='default', name='global_avgpool2d')
+    new_layer = keras.layers.GlobalAvgPool2D(name='global_avgpool2d')
+    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer.get_config())
     
-                if not cert_loc:
-                cert_loc = extract_zipped_paths(DEFAULT_CA_BUNDLE_PATH)
+        # Block 5
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv4')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
     
-        shortcut = Conv2D(filters3, (1, 1), strides=strides,
-                      name=conv_name_base + '1')(input_tensor)
-    shortcut = BatchNormalization(axis=bn_axis, name=bn_name_base + '1')(shortcut)
+            model.add(Lambda(antirectifier,
+                         output_shape=antirectifier_output_shape))
+    ```
+    
+            loaded_im = image.load_img(filename)
+        loaded_im_array = image.img_to_array(loaded_im)
+        assert loaded_im_array.shape == original_im_array.shape
+        assert np.all(loaded_im_array == original_im_array)
+    
+        # Zero-center by mean pixel
+    if data_format == 'channels_first':
+        if x.ndim == 3:
+            x[0, :, :] -= mean[0]
+            x[1, :, :] -= mean[1]
+            x[2, :, :] -= mean[2]
+            if std is not None:
+                x[0, :, :] /= std[0]
+                x[1, :, :] /= std[1]
+                x[2, :, :] /= std[2]
+        else:
+            x[:, 0, :, :] -= mean[0]
+            x[:, 1, :, :] -= mean[1]
+            x[:, 2, :, :] -= mean[2]
+            if std is not None:
+                x[:, 0, :, :] /= std[0]
+                x[:, 1, :, :] /= std[1]
+                x[:, 2, :, :] /= std[2]
+    else:
+        x[..., 0] -= mean[0]
+        x[..., 1] -= mean[1]
+        x[..., 2] -= mean[2]
+        if std is not None:
+            x[..., 0] /= std[0]
+            x[..., 1] /= std[1]
+            x[..., 2] /= std[2]
+    return x
+    
+    Tests comparing this model to the existing Tensorflow model can be
+found at [mobilenet_v2_keras](https://github.com/JonathanCMitchell/mobilenet_v2_keras)
+'''
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+    
+    The VAE has a modular design. The encoder, decoder and VAE
+are 3 models that share weights. After training the VAE model,
+the encoder can be used to  generate latent vectors.
+The decoder can be used to generate MNIST digits by sampling the
+latent vector from a Gaussian distribution with mean=0 and std=1.
     
     
-@keras_test
-@skipif_no_tf_gpu
-def test_cudnn_rnn_basics():
-    input_size = 10
-    timesteps = 6
-    units = 2
-    num_samples = 32
-    for layer_class in [keras.layers.CuDNNGRU, keras.layers.CuDNNLSTM]:
-        for return_sequences in [True, False]:
-            with keras.utils.CustomObjectScope(
-                    {'keras.layers.CuDNNGRU': keras.layers.CuDNNGRU,
-                     'keras.layers.CuDNNLSTM': keras.layers.CuDNNLSTM}):
-                layer_test(
-                    layer_class,
-                    kwargs={'units': units,
-                            'return_sequences': return_sequences},
-                    input_shape=(num_samples, timesteps, input_size))
-        for go_backwards in [True, False]:
-            with keras.utils.CustomObjectScope(
-                    {'keras.layers.CuDNNGRU': keras.layers.CuDNNGRU,
-                     'keras.layers.CuDNNLSTM': keras.layers.CuDNNLSTM}):
-                layer_test(
-                    layer_class,
-                    kwargs={'units': units,
-                            'go_backwards': go_backwards},
-                    input_shape=(num_samples, timesteps, input_size))
+if __name__ == '__main__':
+    unittest.main()  # pragma: no cover
+
+    
+        @mock.patch('certbot.notify.smtplib.LMTP')
+    @mock.patch('certbot.notify.subprocess.Popen')
+    def test_everything_fails(self, mock_popen, mock_lmtp):
+        from certbot.notify import notify
+        lmtp_obj = mock.MagicMock()
+        mock_lmtp.return_value = lmtp_obj
+        lmtp_obj.sendmail.side_effect = socket.error(17)
+        proc = mock.MagicMock()
+        mock_popen.return_value = proc
+        proc.communicate.side_effect = OSError('What we have here is a '
+                                               'failure to communicate.')
+        self.assertFalse(notify('Goose', 'auntrhody@example.com',
+                                'The old grey goose is dead.'))
+        self.assertEqual(lmtp_obj.sendmail.call_count, 1)
+        self.assertEqual(proc.communicate.call_count, 1)
+    
+        html = get_content(url)
+    pid = match1(html, r'video\.settings\.pid\s*=\s*\'([^\']+)\'')
+    title = match1(html, r'video\.settings\.title\s*=\s*\'([^\']+)\'')
+    
+    from ..common import *
+    
+        def test_b_observers_shall_be_attachable(cls):
+        cls.s.attach(cls.dec_obs)
+        cls.assertEqual(isinstance(cls.s._observers[0], DecimalViewer), True)
+        cls.assertEqual(len(cls.s._observers), 1)
+        cls.s.attach(cls.hex_obs)
+        cls.assertEqual(isinstance(cls.s._observers[1], HexViewer), True)
+        cls.assertEqual(len(cls.s._observers), 2)
     
     
-def main():
-    tornado.options.parse_command_line()
-    application = tornado.web.Application([
-        (r'/', MainHandler),
-    ])
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(options.port)
-    tornado.ioloop.IOLoop.current().start()
+def check_ref_out(ref):
+    '''Ask git to check out the specified ref.'''
+    try:
+        subprocess.check_call(
+            ['git', 'checkout', ref],
+            stdout=DEVNULL,
+            stderr=DEVNULL,
+        )
+    except subprocess.CalledProcessError:
+        logging.error('failed to check out %s', ref)
+        sys.exit(1)
     
-            assert len(self.regex.groupindex) in (0, self.regex.groups), \
-            ('groups in url regexes must either be all named or all '
-             'positional: %r' % self.regex.pattern)
+        def current_subdomain(self):
+        return self.stacked_proxy_safe_get(c, 'subdomain')
     
-           This class will be removed in Tornado 6.0. Use
-       ``twisted.internet.asyncioreactor.AsyncioSelectorReactor``
-       instead.
+    from reddit_base import RedditController
+import StringIO
+import r2.lib.captcha as captcha
+from pylons import response
     
-        def __init__(self, client, request, release_callback,
-                 final_callback, max_buffer_size, tcp_client,
-                 max_header_size, max_body_size):
-        self.io_loop = IOLoop.current()
-        self.start_time = self.io_loop.time()
-        self.client = client
-        self.request = request
-        self.release_callback = release_callback
-        self.final_callback = final_callback
-        self.max_buffer_size = max_buffer_size
-        self.tcp_client = tcp_client
-        self.max_header_size = max_header_size
-        self.max_body_size = max_body_size
-        self.code = None
-        self.headers = None
-        self.chunks = []
-        self._decompressor = None
-        # Timeout handle returned by IOLoop.add_timeout
-        self._timeout = None
-        self._sockaddr = None
-        IOLoop.current().add_callback(self.run)
+    @memoize('renderurl_cached', time=60)
+def renderurl_cached(path):
+    # Needed so http://reddit.com/help/ works
+    fp = path.rstrip('/')
+    u = 'https://code.reddit.com/wiki' + fp + '?stripped=1'
     
-        @skipBefore35
-    @unittest.skipIf(platform.python_implementation() == 'PyPy',
-                     'pypy destructor warnings cannot be silenced')
-    def test_undecorated_coroutine(self):
-        namespace = exec_test(globals(), locals(), '''
-        class Test(AsyncTestCase):
-            async def test_coro(self):
-                pass
-        ''')
     
-        class ClassRegistree(BaseRegisteredClass):
-    
-        def test_shall_toggle_from_am_to_fm(self):
-        self.radio.toggle_amfm()
-        state = self.radio.state.name
-        expected_state_name = 'FM'
-        self.assertEqual(state, expected_state_name)
-    
-        def test_bunch_launch(self):
-        self.runner.runAll()
-        output = self.out.getvalue().strip()
-        self.assertEqual(output, str(self.average_result_tc1 + '\n\n' +
-                         self.average_result_tc2 + '\n\n' +
-                         self.average_result_tc3))
+class HealthController(MinimalController):
+    def pre(self):
+        pass
