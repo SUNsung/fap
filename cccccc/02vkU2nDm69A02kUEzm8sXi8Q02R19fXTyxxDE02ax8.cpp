2d84e448a1d9af484a1125aeac021005930af1f2
SUNsung <weatherius@gmail.com>
2018-05-20 07:35:00 +0300

    Rewrite [02vkU2nDm69A02kUEzm8sXi8Q02R19fXTyxxDE02ax8.cpp] File in: 2018-05-20 07:34:59

--- cccccc/02vkU2nDm69A02kUEzm8sXi8Q02R19fXTyxxDE02ax8.cpp
+++ cccccc/02vkU2nDm69A02kUEzm8sXi8Q02R19fXTyxxDE02ax8.cpp
@@ -1,287 +1,223 @@
 
         
-          static void ClearCache(content::RenderProcessHost* render_view_host);
-  static void SetProxyConfig(content::RenderProcessHost* render_process_host,
-                             const std::string& proxy_config);
+        #include 'mbedtls/config.h'
+#include 'mbedtls/ctr_drbg.h'
+#include 'mbedtls/debug.h'
+#include 'mbedtls/entropy.h'
+#include 'mbedtls/net.h'
+#include 'mbedtls/ssl.h'
     
-      bool delay_destruction() { return delay_destruction_; }
-  void set_delay_destruction(bool val) { delay_destruction_ = val; }
-  bool pending_destruction() { return pending_destruction_; }
-  void set_pending_destruction (bool val) { pending_destruction_ = val; }
- protected:
-  int id_;
-  bool delay_destruction_;
-  bool pending_destruction_;
-  base::WeakPtr<ObjectManager> object_manager_;
+    			if (delta == 0)
+				return;
     
-    namespace remote {
-    }
+    	inline static GDNativeCallRegistry *get_singleton() {
+		return singleton;
+	}
     
-    NwClipboardClearSyncFunction::~NwClipboardClearSyncFunction() {
-    }
+        const int z1 = MULTIPLY(z2 + z3, FIX_0_541196100);
+    const int tmp2 = z1 + MULTIPLY(z3, - FIX_1_847759065);
+    const int tmp3 = z1 + MULTIPLY(z2, FIX_0_765366865);
     
-    void StyledWriter::writeValue(const Value& value) {
-  switch (value.type()) {
-  case nullValue:
-    pushValue('null');
-    break;
-  case intValue:
-    pushValue(valueToString(value.asLargestInt()));
-    break;
-  case uintValue:
-    pushValue(valueToString(value.asLargestUInt()));
-    break;
-  case realValue:
-    pushValue(valueToString(value.asDouble()));
-    break;
-  case stringValue:
+    
+    
+      /// Place the acceptor into the state where it will listen for new
+  /// connections.
+  /**
+   * This function puts the socket acceptor into the state where it may accept
+   * new connections.
+   *
+   * @param backlog The maximum length of the queue of pending connections.
+   *
+   * @throws boost::system::system_error Thrown on failure.
+   */
+  void listen(int backlog = socket_base::max_connections)
   {
-    // Is NULL possible for value.string_?
-    char const* str;
-    char const* end;
-    bool ok = value.getString(&str, &end);
-    if (ok) pushValue(valueToQuotedStringN(str, static_cast<unsigned>(end-str)));
-    else pushValue('');
-    break;
+    boost::system::error_code ec;
+    this->get_service().listen(this->get_implementation(), backlog, ec);
+    boost::asio::detail::throw_error(ec, 'listen');
   }
-  case booleanValue:
-    pushValue(valueToString(value.asBool()));
-    break;
-  case arrayValue:
-    writeArrayValue(value);
-    break;
-  case objectValue: {
-    Value::Members members(value.getMemberNames());
-    if (members.empty())
-      pushValue('{}');
-    else {
-      writeWithIndent('{');
-      indent();
-      Value::Members::iterator it = members.begin();
-      for (;;) {
-        const std::string& name = *it;
-        const Value& childValue = value[name];
-        writeCommentBeforeValue(childValue);
-        writeWithIndent(valueToQuotedString(name.c_str()));
-        document_ += ' : ';
-        writeValue(childValue);
-        if (++it == members.end()) {
-          writeCommentAfterValueOnSameLine(childValue);
-          break;
-        }
-        document_ += ',';
-        writeCommentAfterValueOnSameLine(childValue);
-      }
-      unindent();
-      writeWithIndent('}');
+    
+    template <typename Stream>
+class buffered_stream;
+    
+    // Helper class to determine whether or not the current thread is inside an
+// invocation of io_service::run() for a specified io_service object.
+template <typename Key, typename Value = unsigned char>
+class call_stack
+{
+public:
+  // Context class automatically pushes the key/value pair on to the stack.
+  class context
+    : private noncopyable
+  {
+  public:
+    // Push the key on to the stack.
+    explicit context(Key* k)
+      : key_(k),
+        next_(call_stack<Key, Value>::top_)
+    {
+      value_ = reinterpret_cast<unsigned char*>(this);
+      call_stack<Key, Value>::top_ = this;
     }
-  } break;
-  }
-}
-    
-    PyObject* NewMessageNestedTypesSeq(const Descriptor* descriptor);
-PyObject* NewMessageNestedTypesByName(const Descriptor* descriptor);
-    
-    #include <google/protobuf/compiler/command_line_interface.h>
-#include <google/protobuf/compiler/csharp/csharp_helpers.h>
-#include <google/protobuf/io/zero_copy_stream.h>
-#include <google/protobuf/io/printer.h>
-    
-      virtual void WriteHash(io::Printer* printer);
-  virtual void WriteEquals(io::Printer* printer);
-  virtual void WriteToString(io::Printer* printer);
-    
-    // Generator options (used by csharp_generator.cc):
-struct Options {
-  Options() :
-      file_extension('.cs'),
-      base_namespace(''),
-      base_namespace_specified(false),
-      internal_access(false) {
-  }
-  // Extension of the generated file. Defaults to '.cs'
-  string file_extension;
-  // Base namespace to use to create directory hierarchy. Defaults to ''.
-  // This option allows the simple creation of a conventional C# file layout,
-  // where directories are created relative to a project-specific base
-  // namespace. For example, in a project with a base namespace of PetShop, a
-  // proto of user.proto with a C# namespace of PetShop.Model.Shared would
-  // generate Model/Shared/User.cs underneath the specified --csharp_out
-  // directory.
-  //
-  // If no base namespace is specified, all files are generated in the
-  // --csharp_out directory, with no subdirectories created automatically.
-  string base_namespace;
-  // Whether the base namespace has been explicitly specified by the user.
-  // This is required as the base namespace can be explicitly set to the empty
-  // string, meaning 'create a full directory hierarchy, starting from the first
-  // segment of the namespace.'
-  bool base_namespace_specified;
-  // Whether the generated classes should have accessibility level of 'internal'.
-  // Defaults to false that generates 'public' classes.
-  bool internal_access;
-};
-    
-      virtual void GenerateCloningCode(io::Printer* printer);
-  virtual void GenerateFreezingCode(io::Printer* printer);
-  virtual void GenerateMembers(io::Printer* printer);
-  virtual void GenerateMergingCode(io::Printer* printer);
-  virtual void GenerateParsingCode(io::Printer* printer);
-  virtual void GenerateSerializationCode(io::Printer* printer);
-  virtual void GenerateSerializedSizeCode(io::Printer* printer);
-    
-    void WriteMessageDocComment(io::Printer* printer, const Descriptor* message);
-void WriteFieldDocComment(io::Printer* printer, const FieldDescriptor* field);
-void WriteEnumDocComment(io::Printer* printer, const EnumDescriptor* enum_);
-void WriteEnumValueDocComment(io::Printer* printer,
-                              const EnumValueDescriptor* value);
-void WriteServiceDocComment(io::Printer* printer,
-                            const ServiceDescriptor* service);
-void WriteMethodDocComment(io::Printer* printer,
-                           const MethodDescriptor* method);
-    
-      // Returns an estimate of the number of bytes the printed code will compile to
-  virtual int GenerateRegistrationCode(io::Printer* printer);
-    
-    MessageGenerator* ImmutableGeneratorFactory::NewMessageGenerator(
-    const Descriptor* descriptor) const {
-  if (HasDescriptorMethods(descriptor, context_->EnforceLite())) {
-    return new ImmutableMessageGenerator(descriptor, context_);
-  } else {
-    return new ImmutableMessageLiteGenerator(descriptor, context_);
-  }
-}
-    
-    TEST_F(UnicharcompressTest, DoesKorean) {
-  LOG(INFO) << 'Testing kor';
-  LoadUnicharset('kor.unicharset');
-  ExpectCorrect('kor');
-}
-    
-    // C++ Implementation of the Softmax (output) class from lstm.py.
-class FullyConnected : public Network {
- public:
-  FullyConnected(const STRING& name, int ni, int no, NetworkType type);
-  virtual ~FullyConnected();
+    }
     }
     
-    #endif  // TESSERACT_LSTM_PLUMBING_H_
+    } // namespace date_time
+namespace posix_time {
+    
+    #endif // BOOST_ASIO_DETAIL_EVENT_HPP
 
     
+    #include <boost/asio/detail/push_options.hpp>
     
-    { protected:
-  virtual bool AddImageHandler(TessBaseAPI* api);
+    void buffer_sequence_adapter_base::init_native_buffer(
+    buffer_sequence_adapter_base::native_buffer_type& buf,
+    const boost::asio::const_buffer& buffer)
+{
+  std::memset(&buf, 0, sizeof(native_buffer_type));
+  Microsoft::WRL::ComPtr<IInspectable> insp
+    = Microsoft::WRL::Make<winrt_buffer_impl>(buffer);
+  Platform::Object^ buf_obj = reinterpret_cast<Platform::Object^>(insp.Get());
+  buf = reinterpret_cast<Windows::Storage::Streams::IBuffer^>(insp.Get());
+}
+    
+      void Write(const SparsePage& page, dmlc::Stream* fo) override {
+    CHECK(page.offset.size() != 0 && page.offset[0] == 0);
+    CHECK_EQ(page.offset.back(), page.data.size());
+    fo->Write(page.offset);
+    min_index_ = page.min_index;
+    fo->Write(&min_index_, sizeof(min_index_));
+    index_.data.resize(page.data.size());
+    value_.data.resize(page.data.size());
+    }
+    
+    TEST(Metric, LogLoss) {
+  xgboost::Metric * metric = xgboost::Metric::Create('logloss');
+  ASSERT_STREQ(metric->Name(), 'logloss');
+  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 0, 1e-10);
+  EXPECT_NEAR(GetMetricEval(metric,
+                            {0.1f, 0.9f, 0.1f, 0.9f},
+                            {  0,   0,   1,   1}),
+              1.2039f, 0.001f);
+}
+    
+    #include <algorithm>
+#include <cstdlib>
+#include <initializer_list>
+#include <vector>
+    
+      void Configure(const std::vector<std::pair<std::string, std::string> >& cfg) override {
+    this->cfg_ = cfg;
+    model_.Configure(cfg);
+    // initialize the updaters only when needed.
+    std::string updater_seq = tparam_.updater_seq;
+    tparam_.InitAllowUnknown(cfg);
+    if (updater_seq != tparam_.updater_seq) updaters_.clear();
+    for (const auto& up : updaters_) {
+      up->Init(cfg);
+    }
+    // for the 'update' process_type, move trees into trees_to_update
+    if (tparam_.process_type == kUpdate) {
+      model_.InitTreesToUpdate();
+    }
+    }
+    
+    // objectives
+#include '../src/objective/objective.cc'
+#include '../src/objective/regression_obj.cc'
+#include '../src/objective/multiclass_obj.cc'
+#include '../src/objective/rank_obj.cc'
+    
+    DMLC_REGISTRY_FILE_TAG(updater_shotgun);
+    
+    TEST(WriteChainAsyncTransportWrapperTest, TestSimpleBuf) {
+  TestWriteChainAsyncTransportWrapper transport;
+  auto buf = folly::IOBuf::copyBuffer('foobar');
+    }
+    
+    struct ExceptionInfo {
+  const std::type_info* type{nullptr};
+  // The values in frames are IP (instruction pointer) addresses.
+  // They are only filled if the low-level exception tracer library is
+  // linked in or LD_PRELOADed.
+  std::vector<uintptr_t> frames; // front() is top of stack
 };
     
-    namespace tesseract {
-double DotProductSSE(const double* u, const double* v, int n) {
-  fprintf(stderr, 'DotProductSSE can't be used on Android\n');
-  abort();
-}
-int32_t IntDotProductSSE(const int8_t* u, const int8_t* v, int n) {
-  fprintf(stderr, 'IntDotProductSSE can't be used on Android\n');
-  abort();
-}
-}  // namespace tesseract
-    
-      // Rounds the size up to a multiple of the input register size (in int8_t).
-  int RoundInputs(int size) const {
-    return Roundup(size, num_inputs_per_register_);
-  }
-  // Rounds the size up to a multiple of the output register size (in int32_t).
-  int RoundOutputs(int size) const {
-    return Roundup(size, num_outputs_per_register_);
+      /**
+   * Finds the first occurrence of the character sequence `that` in this string.
+   * \note Equivalent to `find(that.data(), 0, strlen(that))`
+   */
+  constexpr std::size_t find(const Char* that) const noexcept {
+    return find(that, 0u, folly::constexpr_strlen(that));
   }
     
-    IntSimdMatrixSSE::IntSimdMatrixSSE() {
-#ifdef __SSE4_1__
-  partial_funcs_ = {PartialMatrixDotVector1};
-#endif  // __SSE4_1__
-}
+      {
+    auto copy = *buf;
+    EXPECT_TRUE(buf->isShared());
+    EXPECT_TRUE(copy.isShared());
+    EXPECT_EQ((void*)buf->data(), (void*)copy.data());
+    EXPECT_NE(buf->next(), copy.next());  // actually different buffers
+    }
     
-    	r_received = 0;
+    template <typename C>
+struct compare_less : detail::cmp_pred<C, ordering::lt, 0> {
+  using detail::cmp_pred<C, ordering::lt, 0>::cmp_pred;
+};
     
-    
-    {	return verify_ssl;
-}
-    
-    	Transform m_worldTransform;
-	Transform local_xform;
-	bool engine_traction;
-	bool steers;
-    
-    	MemberType member_type;
-	String member_name;
-    
-          p[0] = static_cast<jpgd_block_t>(s * q[0]);
-    
-    static std::unique_ptr<Env> def_env(new NormalizingEnvWrapper(Env::Default()));
-INSTANTIATE_TEST_CASE_P(EnvDefault, EnvBasicTestWithParam,
-                        ::testing::Values(def_env.get()));
-INSTANTIATE_TEST_CASE_P(EnvDefault, EnvMoreTestWithParam,
-                        ::testing::Values(def_env.get()));
-    
-        // Helper methods
-    static bool HasFamilyNamed(std::string& name, DBWrapper* db);
-    static bool AddToBatch(rocksdb::WriteBatch& batch, bool del,
-        Handle<Array> array);
-    static bool AddToBatch(rocksdb::WriteBatch& batch, bool del,
-        Handle<Array> array, DBWrapper* db_wrapper, std::string cf);
-    static Handle<Value> CompactRangeDefault(const v8::Arguments& args);
-    static Handle<Value> CompactColumnFamily(const Arguments& args);
-    static Handle<Value> CompactOptions(const Arguments& args);
-    static Handle<Value> CompactAll(const Arguments& args);
-    
-    //****************************
-// Deprecated function names
-//****************************
-// The following translations are provided to ease code transition
-// You are encouraged to no longer this function names
-#define XXH32_feed   XXH32_update
-#define XXH32_result XXH32_digest
-#define XXH32_getIntermediateResult XXH32_intermediateDigest
-    
-      StatisticsJni::StatisticsJni(std::shared_ptr<Statistics> stats,
-      const std::set<uint32_t> ignore_histograms) : StatisticsImpl(stats, false),
-      m_ignore_histograms(ignore_histograms) {
+      template <class... Args>
+  iterator emplace_hint(const_iterator /*hint*/, Args&&... args) {
+    return emplace(std::forward<Args>(args)...).first;
   }
     
-      // Get total number of deletes in the mem table.
-  // REQUIRES: external synchronization to prevent simultaneous
-  // operations on the same MemTable (unless this Memtable is immutable).
-  uint64_t num_deletes() const {
-    return num_deletes_.load(std::memory_order_relaxed);
+      for (size_t i = 0; i < ps.size(); i++) {
+    ts[i].join();
   }
     
-    // Simple test that does single-threaded testing of the ConcurrentTest
-// scaffolding.
-TEST_F(InlineSkipTest, ConcurrentReadWithoutThreads) {
-  ConcurrentTest test;
-  Random rnd(test::RandomSeed());
-  for (int i = 0; i < 10000; i++) {
-    test.ReadStep(&rnd);
-    test.WriteStep(&rnd);
-  }
-}
-    
-      // enable sync point processing (disabled on startup)
-  void EnableProcessing();
-    
-    inline
-void BlockFetcher::PrepareBufferForBlockFromFile() {
-  // cache miss read from device
-  if (do_uncompress_ &&
-      block_size_ + kBlockTrailerSize < kDefaultStackBufferSize) {
-    // If we've got a small enough hunk of data, read it in to the
-    // trivially allocated stack buffer instead of needing a full malloc()
-    used_buf_ = &stack_buf_[0];
-  } else {
-    heap_buf_.reset(new char[block_size_ + kBlockTrailerSize]);
-    used_buf_ = heap_buf_.get();
-  }
-}
-    
-    INSTANTIATE_TEST_CASE_P(DBWriteTestInstance, DBWriteTest,
-                        testing::Values(DBTestBase::kDefault,
-                                        DBTestBase::kConcurrentWALWrites,
-                                        DBTestBase::kPipelinedWrite));
+      // Loop until:
+  // - a read attempt would block
+  // - readCallback_ is uninstalled
+  // - the number of loop iterations exceeds the optional maximum
+  // - this AsyncSocket is moved to another EventBase
+  //
+  // When we invoke readDataAvailable() it may uninstall the readCallback_,
+  // which is why need to check for it here.
+  //
+  // The last bullet point is slightly subtle.  readDataAvailable() may also
+  // detach this socket from this EventBase.  However, before
+  // readDataAvailable() returns another thread may pick it up, attach it to
+  // a different EventBase, and install another readCallback_.  We need to
+  // exit immediately after readDataAvailable() returns if the eventBase_ has
+  // changed.  (The caller must perform some sort of locking to transfer the
+  // AsyncSocket between threads properly.  This will be sufficient to ensure
+  // that this thread sees the updated eventBase_ variable after
+  // readDataAvailable() returns.)
+  uint16_t numReads = 0;
+  EventBase* originalEventBase = eventBase_;
+  while (readCallback_ && eventBase_ == originalEventBase) {
+    // Get the buffer to read into.
+    void* buf = nullptr;
+    size_t buflen = 0, offset = 0;
+    try {
+      prepareReadBuffer(&buf, &buflen);
+      VLOG(5) << 'prepareReadBuffer() buf=' << buf << ', buflen=' << buflen;
+    } catch (const AsyncSocketException& ex) {
+      return failRead(__func__, ex);
+    } catch (const std::exception& ex) {
+      AsyncSocketException tex(AsyncSocketException::BAD_ARGS,
+                              string('ReadCallback::getReadBuffer() '
+                                     'threw exception: ') +
+                              ex.what());
+      return failRead(__func__, tex);
+    } catch (...) {
+      AsyncSocketException ex(AsyncSocketException::BAD_ARGS,
+                             'ReadCallback::getReadBuffer() threw '
+                             'non-exception type');
+      return failRead(__func__, ex);
+    }
+    if (!isBufferMovable_ && (buf == nullptr || buflen == 0)) {
+      AsyncSocketException ex(AsyncSocketException::BAD_ARGS,
+                             'ReadCallback::getReadBuffer() returned '
+                             'empty buffer');
+      return failRead(__func__, ex);
+    }
+    }
