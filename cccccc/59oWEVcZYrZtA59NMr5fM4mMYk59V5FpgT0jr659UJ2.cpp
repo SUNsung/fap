afbb3c2cc8a2407c9a6c4249084aba25a31727fc
SUNsung <weatherius@gmail.com>
2018-05-20 14:20:18 +0300

    Rewrite [59oWEVcZYrZtA59NMr5fM4mMYk59V5FpgT0jr659UJ2.cpp] File in: 2018-05-20 14:20:17

--- cccccc/59oWEVcZYrZtA59NMr5fM4mMYk59V5FpgT0jr659UJ2.cpp
+++ cccccc/59oWEVcZYrZtA59NMr5fM4mMYk59V5FpgT0jr659UJ2.cpp
@@ -1,311 +1,244 @@
 
         
-        #endif
-// TENSORFLOW_KERNELS_SMOOTH_HINGE_LOSS_H_
-
+        using namespace swift;
+using namespace swift::syntax;
     
-    namespace Eigen {
+    class StdlibGroupsIndexRecordingConsumer : public IndexDataConsumer {
+  llvm::StringMap<std::unique_ptr<SymbolTracker>> TrackerByGroup;
+  // Keep a USR map to uniquely identify Decls.
+  // FIXME: if we just passed the original Decl * through we could use that,
+  // which would also let us avoid producing the USR/Name/etc. for decls unless
+  // we actually need it (once per Decl instead of once per occurrence).
+  std::vector<IndexSymbol> symbolStack;
     }
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    
-    {    OP_REQUIRES_OK(
-        context, context->GetAttr('samples_per_second', &samples_per_second_));
-    OP_REQUIRES(context, samples_per_second_ > 0,
-                errors::InvalidArgument('samples_per_second must be > 0.'));
-    OP_REQUIRES_OK(context,
-                   context->GetAttr('bits_per_second', &bits_per_second_));
-  }
-    
-    #include 'tensorflow/core/framework/register_types.h'
-#include 'tensorflow/core/framework/tensor_types.h'
-#include 'tensorflow/core/kernels/cuda_device_array_gpu.h'
-#include 'tensorflow/core/util/cuda_kernel_helper.h'
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    #endif  // GOOGLE_CUDA
-
-    
-      const std::vector<string> urls({server_data_.url});
-  for (int i = 0; i < 3; ++i) {
-    server_data_.server->ClearReceivedDebugData();
-    const uint64 wall_time = Env::Default()->NowMicros();
+    SILFunction *SILDebugScope::getInlinedFunction() const {
+  if (Parent.isNull())
+    return nullptr;
     }
     
-      class MatchGroundTruth : public QuickTest ,
-      public ::testing::WithParamInterface<const char*> {
+      /// Indicates whether diagnostic passes should be skipped.
+  bool SkipDiagnosticPasses = false;
+    
+    #endif
+
+    
+      // #line directive handling.
+  struct VirtualFile {
+    CharSourceRange Range;
+    std::string Name;
+    int LineOffset;
   };
-  
-  TEST_P(MatchGroundTruth, FastPhototestOCR) {
-    OCRTester(TESTING_DIR '/phototest.tif',
-              TESTING_DIR '/phototest.txt',
-              TESSDATA_DIR '_fast', GetParam());
-  }
-  
-  INSTANTIATE_TEST_CASE_P( EngLatinDevaArabLang, MatchGroundTruth, 
-                        ::testing::Values('eng', 'script/Latin', 'script/Devanagari', 'script/Arabic') );
+  std::map<const char *, VirtualFile> VirtualFiles;
+  mutable std::pair<const char *, const VirtualFile*> CachedVFile = {nullptr, nullptr};
     
-    // dims=[5, 4, 3, 2]->[3, 5, 4, 2]
-TEST_F(MatrixTest, RotatingTranspose_2_0) {
+    // Tests that the RotatingTranspose function does the right thing for various
+// transformations.
+// dims=[5, 4, 3, 2]->[5, 2, 4, 3]
+TEST_F(MatrixTest, RotatingTranspose_3_1) {
   GENERIC_2D_ARRAY<int> m;
-  src_.RotatingTranspose(dims_, kNumDims_, 2, 0, &m);
-  m.ResizeNoInit(kInputSize_ / 2, 2);
+  src_.RotatingTranspose(dims_, kNumDims_, 3, 1, &m);
+  m.ResizeNoInit(kInputSize_ / 3, 3);
   // Verify that the result is:
-  // output tensor=[[[[0, 1][6, 7][12, 13][18, 19]]
-  //                 [[24, 25][30, 31][36, 37][42, 43]]
-  //                 [[48, 49][54, 55][60, 61][66, 67]]
-  //                 [[72, 73][78, 79][84, 85][90, 91]]
-  //                 [[96, 97][102, 103][108, 109][114, 115]]]
-  //                [[[2,3]...
+  // output tensor=[[[[0, 2, 4][6, 8, 10][12, 14, 16][18, 20, 22]]
+  //                 [[1, 3, 5][7, 9, 11][13, 15, 17][19, 21, 23]]]
+  //                [[[24, 26, 28]...
   EXPECT_EQ(0, m(0, 0));
-  EXPECT_EQ(1, m(0, 1));
+  EXPECT_EQ(2, m(0, 1));
+  EXPECT_EQ(4, m(0, 2));
   EXPECT_EQ(6, m(1, 0));
-  EXPECT_EQ(7, m(1, 1));
-  EXPECT_EQ(24, m(4, 0));
-  EXPECT_EQ(25, m(4, 1));
-  EXPECT_EQ(30, m(5, 0));
-  EXPECT_EQ(2, m(20, 0));
+  EXPECT_EQ(1, m(4, 0));
+  EXPECT_EQ(24, m(8, 0));
+  EXPECT_EQ(26, m(8, 1));
+  EXPECT_EQ(25, m(12, 0));
+}
+    
+      // Returns true if the given type is derived from Plumbing, and thus contains
+  // multiple sub-networks that can have their own learning rate.
+  bool IsPlumbingType() const override { return true; }
+    
+      // Returns an integer reduction factor that the network applies to the
+  // time sequence. Assumes that any 2-d is already eliminated. Used for
+  // scaling bounding boxes of truth data.
+  // WARNING: if GlobalMinimax is used to vary the scale, this will return
+  // the last used scale factor. Call it before any forward, and it will return
+  // the minimum scale factor of the paths through the GlobalMinimax.
+  int XScaleFactor() const override;
+    
+      /**
+   * Read a 'config' file containing a set of param, value pairs.
+   * Searches the standard places: tessdata/configs, tessdata/tessconfigs
+   * and also accepts a relative or absolute path name.
+   * Note: only non-init params will be set (init params are set by Init()).
+   */
+  void ReadConfigFile(const char* filename);
+  /** Same as above, but only set debug params from the given config file. */
+  void ReadDebugConfigFile(const char* filename);
+    
+    TessHOcrRenderer::TessHOcrRenderer(const char *outputbase, bool font_info)
+    : TessResultRenderer(outputbase, 'hocr') {
+    font_info_ = font_info;
 }
     
     // Computes and returns the dot product of the n-vectors u and v.
-// Uses Intel SSE intrinsics to access the SIMD instruction set.
-double DotProductSSE(const double* u, const double* v, int n);
-// Computes and returns the dot product of the n-vectors u and v.
-// Uses Intel SSE intrinsics to access the SIMD instruction set.
-int32_t IntDotProductSSE(const int8_t* u, const int8_t* v, int n);
-    
-    // Computes all the cross product distances of the points from the line,
-// storing the actual (signed) cross products in distances.
-// Ignores distances of points that are further away than the previous point,
-// and overlaps the previous point by at least half.
-void DetLineFit::ComputeDistances(const ICOORD& start, const ICOORD& end) {
-  distances_.truncate(0);
-  ICOORD line_vector = end;
-  line_vector -= start;
-  square_length_ = line_vector.sqlength();
-  int line_length = IntCastRounded(sqrt(square_length_));
-  // Compute the distance of each point from the line.
-  int prev_abs_dist = 0;
-  int prev_dot = 0;
-  for (int i = 0; i < pts_.size(); ++i) {
-    ICOORD pt_vector = pts_[i].pt;
-    pt_vector -= start;
-    int dot = line_vector % pt_vector;
-    // Compute |line_vector||pt_vector|sin(angle between)
-    int dist = line_vector * pt_vector;
-    int abs_dist = dist < 0 ? -dist : dist;
-    if (abs_dist > prev_abs_dist && i > 0) {
-      // Ignore this point if it overlaps the previous one.
-      int separation = abs(dot - prev_dot);
-      if (separation < line_length * pts_[i].halfwidth ||
-          separation < line_length * pts_[i - 1].halfwidth)
-        continue;
+// Uses Intel AVX intrinsics to access the SIMD instruction set.
+double DotProductAVX(const double* u, const double* v, int n) {
+  int max_offset = n - 4;
+  int offset = 0;
+  // Accumulate a set of 4 sums in sum, by loading pairs of 4 values from u and
+  // v, and multiplying them together in parallel.
+  __m256d sum = _mm256_setzero_pd();
+  if (offset <= max_offset) {
+    offset = 4;
+    // Aligned load is reputedly faster but requires 32 byte aligned input.
+    if ((reinterpret_cast<uintptr_t>(u) & 31) == 0 &&
+        (reinterpret_cast<uintptr_t>(v) & 31) == 0) {
+      // Use aligned load.
+      __m256d floats1 = _mm256_load_pd(u);
+      __m256d floats2 = _mm256_load_pd(v);
+      // Multiply.
+      sum = _mm256_mul_pd(floats1, floats2);
+      while (offset <= max_offset) {
+        floats1 = _mm256_load_pd(u + offset);
+        floats2 = _mm256_load_pd(v + offset);
+        offset += 4;
+        __m256d product = _mm256_mul_pd(floats1, floats2);
+        sum = _mm256_add_pd(sum, product);
+      }
+    } else {
+      // Use unaligned load.
+      __m256d floats1 = _mm256_loadu_pd(u);
+      __m256d floats2 = _mm256_loadu_pd(v);
+      // Multiply.
+      sum = _mm256_mul_pd(floats1, floats2);
+      while (offset <= max_offset) {
+        floats1 = _mm256_loadu_pd(u + offset);
+        floats2 = _mm256_loadu_pd(v + offset);
+        offset += 4;
+        __m256d product = _mm256_mul_pd(floats1, floats2);
+        sum = _mm256_add_pd(sum, product);
+      }
     }
-    distances_.push_back(DistPointPair(dist, pts_[i].pt));
-    prev_abs_dist = abs_dist;
-    prev_dot = dot;
   }
-}
-    
-    static STRING ParagraphJustificationToString(
-    tesseract::ParagraphJustification justification) {
-  switch (justification) {
-    case JUSTIFICATION_LEFT:
-      return 'LEFT';
-    case JUSTIFICATION_RIGHT:
-      return 'RIGHT';
-    case JUSTIFICATION_CENTER:
-      return 'CENTER';
-    default:
-      return 'UNKNOWN';
+  // Add the 4 product sums together horizontally. Not so easy as with sse, as
+  // there is no add across the upper/lower 128 bit boundary, so permute to
+  // move the upper 128 bits to lower in another register.
+  __m256d sum2 = _mm256_permute2f128_pd(sum, sum, 1);
+  sum = _mm256_hadd_pd(sum, sum2);
+  sum = _mm256_hadd_pd(sum, sum);
+  double result;
+  // _mm256_extract_f64 doesn't exist, but resist the temptation to use an sse
+  // instruction, as that introduces a 70 cycle delay. All this casting is to
+  // fool the intrinsics into thinking we are extracting the bottom int64.
+  auto cast_sum = _mm256_castpd_si256(sum);
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored '-Wstrict-aliasing'
+  *(reinterpret_cast<int64_t*>(&result)) =
+#if defined(_WIN32) || defined(__i386__)
+      // This is a very simple workaround that is activated
+      // for all platforms that do not have _mm256_extract_epi64.
+      // _mm256_extract_epi64(X, Y) == ((uint64_t*)&X)[Y]
+      ((uint64_t*)&cast_sum)[0]
+#else
+      _mm256_extract_epi64(cast_sum, 0)
+#endif
+      ;
+#pragma GCC diagnostic pop
+  while (offset < n) {
+    result += u[offset] * v[offset];
+    ++offset;
   }
-}
-    
-    #endif  // TESSERACT_CCMAIN_OTSUTHR_H_
-
-    
-    // Maximum number of unichars in the small and medium sized words
-static const int kMaxSmallWordUnichars = 3;
-static const int kMaxMediumWordUnichars = 6;
-    
-    #ifndef TESSERACT_CCUTIL_UNICODES_H_
-#define TESSERACT_CCUTIL_UNICODES_H_
-    
-    
-    {}
-    
-    
-    {  auto& counter = s_counters[m_name];
-  counter.total += elapsed;
-  ++counter.count;
-  counter.max = std::max(counter.max, elapsed);
-  counter.wall_time_elapsed += elapsed_wall_clock;
-  m_finished = true;
-  return elapsed;
-}
-    
-    public const char *
-magic_stream(struct magic_set *ms, php_stream *stream)
-{
-  if (ms == NULL)
-    return NULL;
-  return file_or_stream(ms, NULL, stream);
-}
-    
-    /*
- * Returns an IR block corresponding to the given bytecode offset. If the block
- * starts with a DefLabel expecting a StkPtr, this function will return an
- * intermediate block that passes the current sp.
- */
-Block* getBlock(IRGS& env, Offset offset);
-    
-        if (s == nullptr || d == nullptr) continue;
-    
-    
-inline int Instruction::ImmBranch() const {
-  switch (BranchType()) {
-    case CondBranchType: return ImmCondBranch();
-    case UncondBranchType: return ImmUncondBranch();
-    case CompareBranchType: return ImmCmpBranch();
-    case TestBranchType: return ImmTestBranch();
-    default: not_reached();
-  }
-  return 0;
-}
-    
-      resourceInfo = FindResource(moduleHandle, section, RT_RCDATA);
-  if (!resourceInfo) {
-    return false;
-  }
-    
-    enum RecordType {
-  // Zero is reserved for preallocated files
-  kZeroType = 0,
-    }
-    
-    
-    {  FileMetaData() : refs(0), allowed_seeks(1 << 30), file_size(0) { }
-};
-    
-    
-    {  static void Append(WriteBatch* dst, const WriteBatch* src);
-};
-    
-    Slice BlockBuilder::Finish() {
-  // Append restart array
-  for (size_t i = 0; i < restarts_.size(); i++) {
-    PutFixed32(&buffer_, restarts_[i]);
-  }
-  PutFixed32(&buffer_, restarts_.size());
-  finished_ = true;
-  return Slice(buffer_);
-}
-    
-    #include <stddef.h>
-#include <stdint.h>
-#include <string>
-#include <vector>
-#include 'leveldb/slice.h'
-#include 'util/hash.h'
-    
-      memset(buf, 0, sizeof(buf));
-  ASSERT_EQ(0x8a9136aa, Value(buf, sizeof(buf)));
-    
-    namespace leveldb {
-    }
-    
-    std::string ParsedInternalKey::DebugString() const {
-  char buf[50];
-  snprintf(buf, sizeof(buf), '' @ %llu : %d',
-           (unsigned long long) sequence,
-           int(type));
-  std::string result = ''';
-  result += EscapeString(user_key.ToString());
-  result += buf;
   return result;
 }
     
-    int main(int argc, char** argv) {
-  leveldb::Env* env = leveldb::Env::Default();
-  bool ok = true;
-  if (argc < 2) {
-    Usage();
-    ok = false;
-  } else {
-    std::string command = argv[1];
-    if (command == 'dump') {
-      ok = leveldb::HandleDumpCommand(env, argv+2, argc-2);
-    } else {
-      Usage();
-      ok = false;
-    }
-  }
-  return (ok ? 0 : 1);
-}
-
+      // Factory makes and returns an IntSimdMatrix (sub)class of the best
+  // available type for the current architecture.
+  static IntSimdMatrix* GetFastestMultiplier();
     
-      const char* data_;
-  size_t size_;
-  uint32_t restart_offset_;     // Offset in data_ of restart array
-  bool owned_;                  // Block owns data_[]
-    
-    
-    {  // No copying allowed
-  BlockBuilder(const BlockBuilder&);
-  void operator=(const BlockBuilder&);
-};
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you use this binding you'll need to call 4 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXXX_NewFrame(), ImGui::Render() and ImGui_ImplXXXX_Shutdown().
-// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-    IMGUI_API bool        ImGui_ImplDX9_Init(void* hwnd, IDirect3DDevice9* device);
-IMGUI_API void        ImGui_ImplDX9_Shutdown();
-IMGUI_API void        ImGui_ImplDX9_NewFrame();
-IMGUI_API void        ImGui_ImplDX9_RenderDrawData(ImDrawData* draw_data);
-    
-    struct GLFWwindow;
-    
-    IMGUI_API bool        ImGui_ImplSdlGL2_Init(SDL_Window* window);
-IMGUI_API void        ImGui_ImplSdlGL2_Shutdown();
-IMGUI_API void        ImGui_ImplSdlGL2_NewFrame(SDL_Window* window);
-IMGUI_API void        ImGui_ImplSdlGL2_RenderDrawData(ImDrawData* draw_data);
-IMGUI_API bool        ImGui_ImplSdlGL2_ProcessEvent(SDL_Event* event);
-    
-    int main(int, char**)
-{
-    // Create application window
-    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T('ImGui Example'), NULL };
-    RegisterClassEx(&wc);
-    HWND hwnd = CreateWindow(_T('ImGui Example'), _T('ImGui DirectX11 Example'), WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc.hInstance, NULL);
+    namespace osquery {
     }
     
-    #endif // D_A2_STR_H
-
+    #pragma once
     
-    void AnnounceList::announceSuccess()
-{
-  if (currentTrackerInitialized_) {
-    (*currentTier_)->nextEvent();
-    auto url = *currentTracker_;
-    (*currentTier_)->urls.erase(currentTracker_);
-    (*currentTier_)->urls.push_front(std::move(url));
-    currentTier_ = std::begin(tiers_);
-    currentTracker_ = std::begin((*currentTier_)->urls);
+    TEST_F(FlagsTests, test_details) {
+  // Make sure flag details are tracked correctly.
+  auto all_flags = Flag::flags();
+  auto flag_info = all_flags['test_string_flag'];
+    }
+    
+      auto status = ::asctime_s(buffer.data(), buffer.size(), timeptr);
+  if (status != 0) {
+    return '';
   }
+    
+    template <typename T>
+static T GetFnPtr(const char *FnName, bool WarnIfMissing) {
+  dlerror(); // Clear any previous errors.
+  void *Fn = dlsym(RTLD_DEFAULT, FnName);
+  if (Fn == nullptr) {
+    if (WarnIfMissing) {
+      const char *ErrorMsg = dlerror();
+      Printf('WARNING: Failed to find function \'%s\'.', FnName);
+      if (ErrorMsg)
+        Printf(' Reason %s.', ErrorMsg);
+      Printf('\n');
+    }
+  }
+  return reinterpret_cast<T>(Fn);
 }
     
-    #endif // D_ANNOUNCE_LIST_H
-
     
-      AnnounceTier(std::deque<std::string> urls);
+    {#undef EXT_FUNC
+}
     
-    #include 'common.h'
+    #ifndef LLVM_FUZZER_IO_H
+#define LLVM_FUZZER_IO_H
+    
+    
+    {  DIR *D = opendir(Dir.c_str());
+  if (!D) {
+    Printf('No such directory: %s; exiting\n', Dir.c_str());
+    exit(1);
+  }
+  while (auto E = readdir(D)) {
+    std::string Path = DirPlusFile(Dir, E->d_name);
+    if (E->d_type == DT_REG || E->d_type == DT_LNK)
+      V->push_back(Path);
+    else if (E->d_type == DT_DIR && *E->d_name != '.')
+      ListFilesInDirRecursive(Path, Epoch, V, false);
+  }
+  closedir(D);
+  if (Epoch && TopDir)
+    *Epoch = E;
+}
+    
+    std::string Sha1ToString(const uint8_t Sha1[kSHA1NumBytes]) {
+  std::stringstream SS;
+  for (int i = 0; i < kSHA1NumBytes; i++)
+    SS << std::hex << std::setfill('0') << std::setw(2) << (unsigned)Sha1[i];
+  return SS.str();
+}
+    
+      void HandleTrace(uint32_t *guard, uintptr_t PC);
+  void HandleInit(uint32_t *start, uint32_t *stop);
+  void HandleCallerCallee(uintptr_t Caller, uintptr_t Callee);
+  void HandleValueProfile(size_t Value) { ValueProfileMap.AddValue(Value); }
+  template <class T> void HandleCmp(void *PC, T Arg1, T Arg2);
+  size_t GetTotalPCCoverage();
+  void SetUseCounters(bool UC) { UseCounters = UC; }
+  void SetUseValueProfile(bool VP) { UseValueProfile = VP; }
+  void SetPrintNewPCs(bool P) { DoPrintNewPCs = P; }
+  template <class Callback> size_t CollectFeatures(Callback CB);
+  bool UpdateValueProfileMap(ValueBitMap *MaxValueProfileMap) {
+    return UseValueProfile && MaxValueProfileMap->MergeFrom(ValueProfileMap);
+  }
+    
+    unsigned NumberOfCpuCores() {
+  unsigned N = std::thread::hardware_concurrency();
+  if (!N) {
+    Printf('WARNING: std::thread::hardware_concurrency not well defined for '
+           'your platform. Assuming CPU count of 1.\n');
+    N = 1;
+  }
+  return N;
+}
