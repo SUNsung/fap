997ba44e0ee13d391325f9f7f5329d1d785b5599
SUNsung <weatherius@gmail.com>
2018-05-21 15:46:36 +0300

    Rewrite [179vSmi2tdSPs17Ed5AjURpkRg17Ko0ZHklS5Ps1774q.py] File in: 2018-05-21 15:46:35

--- pyton/179vSmi2tdSPs17Ed5AjURpkRg17Ko0ZHklS5Ps1774q.py
+++ pyton/179vSmi2tdSPs17Ed5AjURpkRg17Ko0ZHklS5Ps1774q.py
@@ -1,111 +1,101 @@
 
         
-        ] + _py_files('scrapy/contrib') + _py_files('scrapy/contrib_exp')
+        
+if len(sys.argv) <= 1:
+    print('Specify the version number as parameter')
+    sys.exit()
+version = sys.argv[1]
+    
+        test_func.__name__ = str('test_swf_' + test_id)
+    setattr(TestSWFInterpreter, test_func.__name__, test_func)
     
     
-class Command(ScrapyCommand):
-    requires_project = True
-    default_settings = {'LOG_ENABLED': False}
+def gen_extractors():
+    ''' Return a list of an instance of every supported extractor.
+    The order does matter; the first extractor matched is the one handling the URL.
+    '''
+    return [klass() for klass in gen_extractor_classes()]
     
-        def from_method(self, method, results):
-        contracts = self.extract_contracts(method)
-        if contracts:
-            # calculate request args
-            args, kwargs = get_spec(Request.__init__)
-            kwargs['callback'] = method
-            for contract in contracts:
-                kwargs = contract.adjust_request_args(kwargs)
-    
-        name = 'scrapes'
-    
-        platforms = 'any',
-    zip_safe = True,
-    include_package_data = True,
-    
-    __all__ = ['ehow_download']
-    
-            total_count = 0
-        translated_count = 0
-        with open(fn) as f:
-            catalog = read_po(f)
-            for msg in catalog:
-                total_count += 1
-                if is_translated(msg):
-                    translated_count += 1
-        pct = translated_count / float(total_count) * 100
-        click.echo('% -7s % 2d%%' % (
-            locale,
-            pct,
-        ), err=True)
-        if pct >= MINIMUM and locale not in rv:
-            rv.append(locale)
-    with open(catalog_file, 'w') as f:
-        json.dump({
-            'supported_locales': sorted(rv)
-        }, f, indent=2)
-        f.write('\n')
-    
-        def is_employee(self, user):
-        if not user:
-            return False
-        return user.employee
-    
-        def __call__(self, environ, start_response):
-        def custom_start_response(status, headers, exc_info = None):
-            fixed = []
-            seen = set()
-            for head, val in reversed(headers):
-                head = head.lower()
-                key = (head, val.split('=', 1)[0])
-                if key not in seen:
-                    fixed.insert(0, (head, val))
-                    seen.add(key)
-            return start_response(status, fixed, exc_info)
-        return self.app(environ, custom_start_response)
-    
-    from pylons import request
-from pylons import app_globals as g
-from reddit_base import RedditController
-from r2.lib.pages import AdminPage, AdminAwards
-from r2.lib.pages import AdminAwardGive, AdminAwardWinners
-from r2.lib.validator import *
-    
-            def builder_wrapper(thing = None):
-            kw = {}
-            if not thing:
-                kw['url'] = url
-                kw['title'] = title
-            return ButtonLite(thing,
-                              image = 1 if buttonimage is None else buttonimage,
-                              target = '_new' if newwindow else '_parent',
-                              styled = styled, **kw)
-    
-    @memoize('renderurl_cached', time=60)
-def renderurl_cached(path):
-    # Needed so http://reddit.com/help/ works
-    fp = path.rstrip('/')
-    u = 'https://code.reddit.com/wiki' + fp + '?stripped=1'
-    
-        def GET_cachehealth(self):
-        results = {}
-        behaviors = {
-            # Passed on to poll(2) in milliseconds
-            'connect_timeout': 1000,
-            # Passed on to setsockopt(2) in microseconds
-            'receive_timeout': int(1e6),
-            'send_timeout': int(1e6),
+            return {
+            '_type': 'playlist',
+            'id': playlist_id,
+            'title': title,
+            'description': description,
+            'entries': entries,
         }
-        for server in cache._CACHE_SERVERS:
-            try:
-                if server.startswith('udp:'):
-                    # libmemcached doesn't support UDP get/fetch operations
-                    continue
-                mc = pylibmc.Client([server], behaviors=behaviors)
-                # it's ok that not all caches are mcrouter, we'll just ignore
-                # the miss either way
-                mc.get('__mcrouter__.version')
-                results[server] = 'OK'
-            except pylibmc.Error as e:
-                g.log.warning('Health check for %s FAILED: %s', server, e)
-                results[server] = 'FAILED %s' % e
-        return json.dumps(results)
+
+    
+    
+@pytest.mark.functional
+def test_select_command_with_arrows(proc, TIMEOUT):
+    select_command_with_arrows(proc, TIMEOUT)
+    
+    from pylons import tmpl_context as c
+    
+        if asbool(full_stack):
+        # Handle Python exceptions
+        app = ErrorHandler(app, global_conf, **config['pylons.errorware'])
+    
+            if target.subreddit_slow.quarantine:
+            err = RedditError('GILDING_NOT_ALLOWED')
+            self.on_validation_error(err)
+        VNotInTimeout().run(target=target, subreddit=target.subreddit_slow)
+    
+    from r2.config.extensions import set_extension
+from r2.controllers.reddit_base import RedditController, generate_modhash
+from r2.controllers.login import handle_login, handle_register
+from r2.lib.csrf import csrf_exempt
+from r2.lib.validator import (
+    json_validate,
+    ValidEmail,
+    VPasswordChange,
+    VRatelimit,
+    VSigned,
+    VThrottledLogin,
+    VUname,
+)
+    
+            '''
+        err = None
+        if 'name' in notes_json and notes_json['name'] != friend:
+            # The 'name' in the JSON is optional, but if present, must
+            # match the username from the URL
+            err = errors.RedditError('BAD_USERNAME', fields='name')
+        if 'note' in notes_json and not c.user.gold:
+            err = errors.RedditError('GOLD_REQUIRED', fields='note')
+        if err:
+            self.on_validation_error(err)
+    
+            Responds with a 120x50 `image/png` which should be displayed
+        to the user.
+    
+    
+redditbroke =  \
+'''<html>
+  <head>
+    <title>reddit broke!</title>
+  </head>
+  <body>
+    <div style='margin: auto; text-align: center'>
+      <p>
+        <a href='/'>
+          <img border='0' src='%s' alt='you broke reddit' />
+        </a>
+      </p>
+      <p>
+        %s
+      </p>
+  </body>
+</html>
+'''
+    
+    
+class GoogleTagManagerController(MinimalController):
+    def pre(self):
+        if request.host != g.media_domain:
+            # don't serve up untrusted content except on our
+            # specifically untrusted domain
+            self.abort404()
+    
+    from r2.controllers.reddit_base import MinimalController
+from r2.lib import promote, cache
