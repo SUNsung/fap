dcc6a342785cd5af88e57b3fea7abb2a8dda4961
SUNsung <weatherius@gmail.com>
2018-05-21 09:52:54 +0300

    Rewrite [12ASXjxmDDbDk12vZ0bEefHEes12rwRA9zkvEIM12CN0.py] File in: 2018-05-21 09:52:54

--- pyton/12ASXjxmDDbDk12vZ0bEefHEes12rwRA9zkvEIM12CN0.py
+++ pyton/12ASXjxmDDbDk12vZ0bEefHEes12rwRA9zkvEIM12CN0.py
@@ -1,123 +1,109 @@
 
         
-        def md5s(data):
-    if not _md5:
-        raise ValueError('MD5 not available.  Possibly running in FIPS mode')
-    return secure_hash_s(data, _md5)
-    
-        args = parser.parse_args()
-    
-        headers = dict(
-        Authorization='apiToken %s' % args.key,
-    )
-    
-        'ipv4': re.compile(
-        r'''^
-            (?:{i4}\.){{3}}{i4}         # Three parts followed by dots plus one
-            $
-        '''.format(i4=ipv4_component), re.X | re.I
-    ),
-    
-            expected = {
-            'description': 'foo_url_map description',
-            'hostRules': [
-                {
-                    'description': 'host rules description',
-                    'hosts': [
-                        'www.example.com',
-                        'www2.example.com'
-                    ],
-                    'pathMatcher': 'host_rules_path_matcher'
-                }
-            ],
-            'name': 'foo_url_map_name',
-            'pathMatchers': [
-                {
-                    'defaultService': 'bes-pathmatcher-one-default',
-                    'description': 'path matcher one',
-                    'name': 'path_matcher_one',
-                    'pathRules': [
-                        {
-                            'paths': [
-                                '/',
-                                '/aboutus'
-                            ],
-                            'service': 'my-one-bes'
-                        }
-                    ]
-                },
-                {
-                    'defaultService': 'bes-pathmatcher-two-default',
-                    'description': 'path matcher two',
-                    'name': 'path_matcher_two',
-                    'pathRules': [
-                        {
-                            'paths': [
-                                '/webapp',
-                                '/graphs'
-                            ],
-                            'service': 'my-two-bes'
-                        }
-                    ]
-                }
-            ]
-        }
-    
-        def construct_yaml_str(self, node, unsafe=False):
-        # Override the default string handling function
-        # to always return unicode objects
-        value = self.construct_scalar(node)
-        ret = AnsibleUnicode(value)
-    
-        def test_full_jitter(self):
-        retries = 5
-        seed = 1
-    
-        author = proj_info['author'],
-    author_email = proj_info['author_email'],
-    url = proj_info['url'],
-    license = proj_info['license'],
-    
-                    log.println('    platform: {}'.format(platform.platform()))
-                log.println('    python:   {}'.format(sys.version.split('\n')[0]))
-    
-        theplatform_download_by_pid(pid, title, output_dir=output_dir, merge=merge, info_only=info_only)
-    
-    
-def construct_url(url, **params):
-    param_str = urlparse.urlencode(params)
-    return url + '?' + param_str
-    
-    from ..common import *
-from ..extractor import VideoExtractor
-    
-    
-def mtv81_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_content(url)
-    title = HTMLParser().unescape(
-        '|'.join(match1(html, r'<title>(.*?)</title>').split('|')[:-2]))
-    
+        
+def side_effect(old_cmd, command):
+    with tarfile.TarFile(_tar_file(old_cmd.script_parts)[0]) as archive:
+        for file in archive.getnames():
             try:
-            result = self.fn(*self.args, **self.kwargs)
-        except BaseException:
-            e = sys.exc_info()[1]
-            self.future.set_exception(e)
-        else:
-            self.future.set_result(result)
-    
-        # For pickling to work, the __module__ variable needs to be set to the frame
-    # where the named tuple is created.  Bypass this step in enviroments where
-    # sys._getframe is not defined (Jython for example).
-    if hasattr(_sys, '_getframe'):
-        result.__module__ = _sys._getframe(1).f_globals.get('__name__', '__main__')
+                os.remove(file)
+            except OSError:
+                # does not try to remove directories as we cannot know if they
+                # already existed before
+                pass
+
     
     
-def CompileRegex( raw_regex ):
-  pattern = re.compile( raw_regex, re.IGNORECASE )
+# Executing the interpreter in a subprocess
+def _assert_python(expected_success, *args, **env_vars):
+    cmd_line = [sys.executable]
+    if not env_vars:
+        cmd_line.append('-E')
+    # Need to preserve the original environment, for in-place testing of
+    # shared library builds.
+    env = os.environ.copy()
+    # But a special flag that can be set to override -- in this case, the
+    # caller is responsible to pass the full environment.
+    if env_vars.pop('__cleanenv', None):
+        env = {}
+    env.update(env_vars) 
+    cmd_line.extend(args)
+    p = subprocess.Popen(cmd_line, stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                         env=env)
+    try:
+        out, err = p.communicate()
+    finally:
+        subprocess._cleanup()
+        p.stdout.close()
+        p.stderr.close()
+    rc = p.returncode
+    err = strip_python_stderr(err)
+    if (rc and expected_success) or (not rc and not expected_success):
+        raise AssertionError(
+            'Process return code is %d, '
+            'stderr follows:\n%s' % (rc, err.decode('ascii', 'ignore')))
+    return rc, out, err
+    
+      f = _CreateFilterForTypes( opts, [ 'cs' ] )
     
     
-def OverlapLength_LongestOverlap_test():
-  eq_( 7, base.OverlapLength( 'bar foo foo', 'foo foo bar' ) )
+def get_changed_files(old_ref, new_ref):
+    '''Return a list of files that have changed from one ref to another.'''
+    root = get_root()
+    changed_files_text = subprocess.check_output(['git', 'diff', '--name-only',
+                                                  old_ref, new_ref])
+    changed_files = changed_files_text.splitlines()
+    return [os.path.join(root, x) for x in changed_files]
     
-      # 0 is not False
-  assert_that( _HandlePollResponse( 0, None ), equal_to( True ) )
+    from r2.controllers.api_docs import api_doc, api_section
+from r2.controllers.oauth2 import require_oauth2_scope
+from r2.controllers.reddit_base import OAuth2OnlyController
+from r2.lib.jsontemplates import (
+    FriendTableItemJsonTemplate,
+    get_usertrophies,
+    IdentityJsonTemplate,
+    KarmaListJsonTemplate,
+    PrefsJsonTemplate,
+)
+from r2.lib.pages import FriendTableItem
+from r2.lib.validator import (
+    validate,
+    VAccountByName,
+    VFriendOfMine,
+    VLength,
+    VList,
+    VUser,
+    VValidatedJSON,
+)
+from r2.models import Account, Trophy
+import r2.lib.errors as errors
+import r2.lib.validator.preferences as vprefs
+    
+    from pylons import request
+from pylons import app_globals as g
+from reddit_base import RedditController
+from r2.lib.pages import AdminPage, AdminAwards
+from r2.lib.pages import AdminAwardGive, AdminAwardWinners
+from r2.lib.validator import *
+    
+        @validate(buttonimage = VInt('i', 0, 14),
+              title = nop('title'),
+              url = VSanitizedUrl('url'),
+              newwindow = VBoolean('newwindow', default = False),
+              styled = VBoolean('styled', default=True))
+    def GET_button_lite(self, buttonimage, title, url, styled, newwindow):
+        c.user = UnloggedUser([c.lang])
+        c.user_is_loggedin = False
+        c.render_style = 'js'
+    
+    class CaptchaController(RedditController):
+    @allow_oauth2_access
+    @api_doc(api_section.captcha, uri='/captcha/{iden}')
+    def GET_captchaimg(self, iden):
+        '''
+        Request a CAPTCHA image given an `iden`.
+    
+            fp, content = renderurl_cached(path)
+        if content is None:
+            self.abort404()
+        return self.rendercontent(content, fp)
