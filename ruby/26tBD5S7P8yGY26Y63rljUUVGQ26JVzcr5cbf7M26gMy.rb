a87107d9e0237348b52a1694a34b8db8bdc6b952
SUNsung <weatherius@gmail.com>
2018-05-21 04:04:19 +0300

    Rewrite [26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb] File in: 2018-05-21 04:04:19

--- ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
+++ ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
@@ -1,14 +1,69 @@
 
         
-          def can_read?(resource)
-    ability_name = resource.class.name.downcase
-    ability_name = 'read_#{ability_name}'.to_sym
+              unless root?
+        raise Invalid, 'missing name' if !name || name.empty?
+        raise Invalid, 'missing path' if !path || path.empty?
+        raise Invalid, 'missing type' if !type || type.empty?
+      end
+    end
     
-      def self.authenticate(user, app_id, json_response, challenges)
-    response = U2F::SignResponse.load_from_json(json_response)
-    registration = user.u2f_registrations.find_by_key_handle(response.key_handle)
-    u2f = U2F::U2F.new(app_id)
+          if options && options[:ignore_case]
+        base = base.downcase
+        dest = dest.downcase
+      end
     
-      def external?
-    true
+          def get_type
+        case slug
+        when 'api'
+          'Reference'
+        when 'configuration'
+          'Reference: Configuration'
+        when 'stpl'
+          'Reference: SimpleTemplate'
+        when 'plugindev'
+          'Reference: Plugin'
+        else
+          'Manual'
+        end
+      end
+    
+      def setup
+    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
+    success = Dir.chdir DUMMY_PATH do
+      silence_stdout_if !ENV['VERBOSE'] do
+        system 'node', 'manifest.js', tmp_dir
+      end
+    end
+    assert success, 'Node.js Mincer compilation failed'
+    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
+    css_name = manifest['assets']['application.css']
+    @css = File.read('#{tmp_dir}/#{css_name}')
+  end
+end
+
+    
+        if run? && ARGV.any?
+      require 'optparse'
+      OptionParser.new { |op|
+        op.on('-p port',   'set the port (default is 4567)')                { |val| set :port, Integer(val) }
+        op.on('-o addr',   'set the host (default is #{bind})')             { |val| set :bind, val }
+        op.on('-e env',    'set the environment (default is development)')  { |val| set :environment, val.to_sym }
+        op.on('-s server', 'specify rack server/handler (default is thin)') { |val| set :server, val }
+        op.on('-q',        'turn on quiet mode (default is off)')           {       set :quiet, true }
+        op.on('-x',        'turn on the mutex lock (default is off)')       {       set :lock, true }
+      }.parse!(ARGV.dup)
+    end
+  end
+    
+    
+  it 'should allow changing the protection mode to a string' do
+    # I have no clue what other modes are available
+    mock_app do
+      use Rack::Protection::FrameOptions, :frame_options => 'ALLOW-FROM foo'
+      run DummyApp
+    end
+    
+    
+  it 'should set the X-Content-Type-Options for other content types' do
+    expect(get('/', {}, 'wants' => 'application/foo').header['X-Content-Type-Options']).to eq('nosniff')
   end
