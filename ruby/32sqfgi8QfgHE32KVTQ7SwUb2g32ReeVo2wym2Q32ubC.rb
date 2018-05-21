08ec5fe93bda3c47a7855f9b7bb9fa89df3338d5
SUNsung <weatherius@gmail.com>
2018-05-21 08:42:31 +0300

    Rewrite [32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb] File in: 2018-05-21 08:42:31

--- ruby/32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb
+++ ruby/32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb
@@ -1,49 +1,15 @@
 
         
-                def execute_repl_command(repl_command)
-          unless repl_command == '\n'
-            repl_commands = repl_command.split
-            subcommand = repl_commands.shift.capitalize
-            arguments = repl_commands
-            subcommand_class = Pod::Command::IPC.const_get(subcommand)
-            subcommand_class.new(CLAide::ARGV.new(arguments)).run
-            signal_end_of_output
+                def find_order
+          @order = Spree::Order.find_by!(number: order_id)
+        end
+    
+            def create
+          authorize! :create, StockLocation
+          @stock_location = StockLocation.new(stock_location_params)
+          if @stock_location.save
+            respond_with(@stock_location, status: 201, default_template: :show)
+          else
+            invalid_resource!(@stock_location)
           end
         end
-      end
-    end
-  end
-end
-
-    
-          def initialize(argv)
-        @update = argv.flag?('update')
-        @stats  = argv.flag?('stats')
-        super
-      end
-    
-      context 'called with two styles' do
-    it 'applies to alternating sides' do
-      rule = 'border-style: dotted dashed'
-    
-          expect('.margin-explicit').to have_rule(rule)
-    end
-  end
-    
-      context 'called with argument (2)' do
-    it 'outputs the second value from the default scale' do
-      expect('.two-base-one').to have_rule('font-size: 1.5625em')
-    end
-  end
-    
-          expect('.padding-alternate').to have_rule(rule)
-    end
-  end
-    
-      context 'called with three sizes' do
-    it 'applies second width to left and right' do
-      ruleset = 'position: relative; ' +
-                'top: 4px; ' +
-                'right: 5px; ' +
-                'bottom: 6px; ' +
-                'left: 5px;'
