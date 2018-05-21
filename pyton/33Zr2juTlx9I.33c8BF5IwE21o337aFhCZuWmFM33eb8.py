8e6c0bc5d35b48933b126477794f903dcef4e024
SUNsung <weatherius@gmail.com>
2018-05-21 19:56:01 +0300

    Rewrite [33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py] File in: 2018-05-21 19:56:00

--- pyton/33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py
+++ pyton/33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py
@@ -1,67 +1,104 @@
 
         
-            def __init__(self):
-        self.people = {}  # key: person_id, value: person
+        from youtube_dl.compat import (
+    compat_print,
+    compat_urllib_request,
+)
+from youtube_dl.utils import format_bytes
     
-        def __init__(self):
-        self.head = None
-        self.tail = None
+    
+class TestCache(unittest.TestCase):
+    def setUp(self):
+        TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+        TESTDATA_DIR = os.path.join(TEST_DIR, 'testdata')
+        _mkdir(TESTDATA_DIR)
+        self.test_dir = os.path.join(TESTDATA_DIR, 'cache_test')
+        self.tearDown()
+    
+            webpage = self._download_webpage(url, playlist_id)
+        title = self._html_search_regex(
+            r'<h1 class='playlist-name'[^>]*?>(.*?)</h1>', webpage, 'title')
+        description = self._html_search_regex(
+            r'<p class='excerpt'[^>]*?>(.*?)</p>',
+            webpage, 'description', fatal=False)
+        urls = re.findall(
+            r'<li class='lecture-preview'>\s*?<a target='_blank' href='([^']+)'>',
+            webpage)
+        entries = [self.url_result(u) for u in urls]
+    
+        exc = Timeout('Request timed out')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
+    assert ret == ExitStatus.ERROR_TIMEOUT
+    assert error_msg == 'Request timed out (30s).'
+
+    
+    
+def test_unicode_headers_verbose(httpbin):
+    # httpbin doesn't interpret utf8 headers
+    r = http('--verbose', httpbin.url + '/headers', u'Test:%s' % UNICODE)
+    assert HTTP_OK in r
+    assert UNICODE in r
     
         try:
-        for future in finished:
-            yield future
+        s1 = os.lstat(path)
+    except OSError:
+        # the OSError should be handled with more care
+        # it could be a 'permission denied' but path is still a mount
+        return False
+    else:
+        # A symlink can never be a mount point
+        if os.path.stat.S_ISLNK(s1.st_mode):
+            return False
     
-    def _python_exit():
-    global _shutdown
-    _shutdown = True
-    items = list(_threads_queues.items())
-    for t, q in items:
-        q.put(None)
-    for t, q in items:
-        t.join()
+            if args.console_logs:
+            path = os.path.join(output_dir, '%s/console.log' % job_number)
+            url = 'https://api.shippable.com/jobs/%s/consoles?download=true' % job_id
+            download(args, headers, path, url, is_json=False)
     
-    # -- General configuration -----------------------------------------------------
+            # global, resource
+        input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/global/urlMaps'
+        actual = GCPUtils.parse_gcp_url(input_url)
+        self.assertEquals('compute', actual['service'])
+        self.assertEquals('v1', actual['api_version'])
+        self.assertEquals('myproject', actual['project'])
+        self.assertTrue('global' in actual)
+        self.assertTrue(actual['global'])
+        self.assertEquals('urlMaps', actual['resource_name'])
     
-            self.assertTrue(f3.cancel())
-        self.assertEqual(f3._state, CANCELLED)
+        def test_timeout(self):
+        self.assertTrue(self.timeout.timeout)
+        self.assertFalse(self.invalid.timeout)
+    
+        def test_same_server(self):
+        from certbot_apache.obj import VirtualHost
+        no_name1 = VirtualHost(
+            'fp', 'vhp', set([self.addr1]), False, False, None)
+        no_name2 = VirtualHost(
+            'fp', 'vhp', set([self.addr2]), False, False, None)
+        no_name3 = VirtualHost(
+            'fp', 'vhp', set([self.addr_default]),
+            False, False, None)
+        no_name4 = VirtualHost(
+            'fp', 'vhp', set([self.addr2, self.addr_default]),
+            False, False, None)
+    
+    # If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath(os.path.join(here, '..')))
+    
+        @mock.patch(
+        'certbot_compatibility_test.validator.crypto_util.probe_sni')
+    def test_certificate_success(self, mock_probe_sni):
+        cert = OpenSSL.crypto.X509()
+        mock_probe_sni.return_value = cert
+        self.assertTrue(self.validator.certificate(
+            cert, 'test.com', '127.0.0.1'))
     
     
-def FormatDebugInfoResponse_ExtraConfFoundButNotLoaded_test():
-  response = deepcopy( GENERIC_RESPONSE )
-  response[ 'extra_conf' ].update( {
-    'is_loaded': False,
-    'path': '/path/to/extra/conf'
-  } )
-  assert_that(
-    FormatDebugInfoResponse( response ),
-    contains_string(
-      'Extra configuration file found but not loaded\n'
-      'Extra configuration path: /path/to/extra/conf\n'
-    )
-  )
-    
-    
-class CommandTest(unittest.TestCase):
-    
-        def tearDown(cls):
-        ''' Function/test case scope teardown. '''
-        cls.output.close()
-        sys.stdout = cls.saved_stdout
-    
-    from __future__ import print_function
-    
-        def test_3rd_am_station_after_scan(self):
-        self.radio.scan()
-        station = self.radio.state.stations[self.radio.state.pos]
-        expected_station = '1510'
-        self.assertEqual(station, expected_station)
-    
-    if __name__ == '__main__':
-    
-        def _handle(self, request):
-        print('end of chain, no handler for {}'.format(request))
-        return True
-    
-        def rename(self, src, dest):
-        print(u'renaming %s to %s' % (src, dest))
-        os.rename(src, dest)
+class AskTest(unittest.TestCase):
+    '''Test the ask method.'''
+    def setUp(self):
+        logging.disable(logging.CRITICAL)
