86ba8150598f7bc59264cf4a9116b6c31b085588
SUNsung <weatherius@gmail.com>
2018-05-21 05:37:23 +0300

    Rewrite [35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb] File in: 2018-05-21 05:37:22

--- ruby/35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb
+++ ruby/35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb
@@ -1,84 +1,62 @@
 
         
-          def test_redirect_to_with_block_and_accepted_options
-    with_routing do |set|
-      set.draw do
-        ActiveSupport::Deprecation.silence do
-          get ':controller/:action'
-        end
-      end
+          </body>
+</html>
+HTML
     
-        # Reset all attributes. Should be called before and after actions, when used as a per-request singleton.
-    def reset
-      run_callbacks :reset do
-        self.attributes = {}
+          should 'respect front matter defaults' do
+        assert_nil @page.data['title']
+        assert_equal 'John Doe', @page.data['author']
+        assert_equal 'default', @page.data['layout']
       end
     end
     
-      test 'token_and_options returns nil with no value after the equal sign' do
-    actual = ActionController::HttpAuthentication::Token.token_and_options(malformed_request).first
-    assert_nil actual
+          new_theme_name = args.join('_')
+      theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
+      if theme.path.exist?
+        Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.'
+      end
+    
+        # Gets the path to this layout relative to its base
+    attr_reader :relative_path
+    
+      # DELETE /resource/sign_out
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :notice, :signed_out if signed_out
+    yield if block_given?
+    respond_to_on_destroy
   end
     
-      class MyController < ActionController::Metal
-    use BlockMiddleware do |config|
-      config.configurable_message = 'Configured by block.'
+        def confirmation_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :confirmation_instructions, opts)
     end
-    use MyMiddleware
-    middleware.insert_before MyMiddleware, ExclaimerMiddleware
     
-    class CustomDeliveryMethodsTest < ActiveSupport::TestCase
-  setup do
-    @old_delivery_method = ActionMailer::Base.delivery_method
-    ActionMailer::Base.add_delivery_method :custom, MyCustomDelivery
-  end
-    
-        puts 'Downloading emojos from source... (#{source})'
-    
-          it 'sets a regeneration marker while regenerating' do
-        allow(RegenerationWorker).to receive(:perform_async)
-        get :show
-    
-      def scope
-    root_url
-  end
-    
-          opts.on('-C', '--no-cache', 'Don't cache to sassc files.') do
-        @options[:for_engine][:read_cache] = false
-      end
-    
-            def validate!
-          super
-          if @pod_name.nil? && !@wipe_all
-            # Security measure, to avoid removing the pod cache too agressively by mistake
-            help! 'You should either specify a pod name or use the --all flag'
-          end
-        end
-    
-            # !@group Private helpers
-    
-      %w(GET HEAD POST PUT DELETE).each do |method|
-    it 'accepts #{method} requests when allow_if is true' do
-      mock_app do
-        use Rack::Protection::HttpOrigin, :allow_if => lambda{|env| env.has_key?('HTTP_ORIGIN') }
-        run DummyApp
-      end
-      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://any.domain.com')).to be_ok
+        def find_remote(username, domain)
+      AccountFinder.new(username, domain).account
     end
   end
     
-      it 'accepts requests with a changing Version header'do
-    session = {:foo => :bar}
-    get '/', {}, 'rack.session' => session, 'HTTP_VERSION' => '1.0'
-    get '/', {}, 'rack.session' => session, 'HTTP_VERSION' => '1.1'
-    expect(session[:foo]).to eq(:bar)
+      def icon
+    object.image
+  end
+    
+        File.write(dest, Oj.dump(map))
+    puts 'Wrote emojo to destination! (#{dest})'
   end
 end
 
     
-      # These two settings work together to allow you to limit a spec run
-  # to individual examples or groups you care about by tagging them with
-  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
-  # get run.
-  config.filter_run :focus
-  config.run_all_when_everything_filtered = true
+      def deliver_digest
+    NotificationMailer.digest(user.account).deliver_now!
+    user.touch(:last_emailed_at)
+  end
+end
+
+    
+      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/follow
+  def follow
+    f = Follow.last
+    NotificationMailer.follow(f.target_account, Notification.find_by(activity: f))
+  end
