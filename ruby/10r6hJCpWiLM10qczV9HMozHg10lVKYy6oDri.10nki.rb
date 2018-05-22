b8e410519397ca94018a3ec8510107b3eb0c58bb
SUNsung <weatherius@gmail.com>
2018-05-22 19:56:53 +0300

    Rewrite [10r6hJCpWiLM10qczV9HMozHg10lVKYy6oDri.10nki.rb] File in: 2018-05-22 19:56:53

--- ruby/10r6hJCpWiLM10qczV9HMozHg10lVKYy6oDri.10nki.rb
+++ ruby/10r6hJCpWiLM10qczV9HMozHg10lVKYy6oDri.10nki.rb
@@ -1,167 +1,105 @@
 
         
-          </body>
-</html>
-HTML
-CONTENT_NOT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+              # Returns constant of subscription adapter specified in config/cable.yml.
+      # If the adapter cannot be found, this will default to the Redis adapter.
+      # Also makes sure proper dependencies are required.
+      def pubsub_adapter
+        adapter = (cable.fetch('adapter') { 'redis' })
     
-    def dest_dir(*subdirs)
-  test_dir('dest', *subdirs)
-end
+                Thread.pass while @thread.alive?
+          end
     
-    # Create an array of vendored mimetype => [extensions]
-mimes = {}
-json = open('https://raw.githubusercontent.com/jshttp/mime-db/master/db.json').read
-data = JSON.parse(json)
-data.reject! { |mime, meta| meta['extensions'].nil? || meta['extensions'].empty? }
-data.each do |mime, meta|
-  # Normalize extensions and mime-types
-  mime = mime.downcase.strip
-  extensions = meta['extensions'].map { |e| e.downcase.strip }.compact
+            def test_url_port
+          spec = resolve 'abstract://foo:123?encoding=utf8'
+          assert_equal({
+            'adapter'  => 'abstract',
+            'port'     => 123,
+            'host'     => 'foo',
+            'encoding' => 'utf8' }, spec)
+        end
     
-    def jekyll_run_output
-  if Paths.output_file.file?
-    then return Paths.output_file.read
+      class InheritedController < MyController
+  end
+    
+        # Helpers for creating and wrapping delivery behavior, used by DeliveryMethods.
+    module ClassMethods
+      # Provides a list of emails that have been delivered by Mail::TestMailer
+      delegate :deliveries, :deliveries=, to: Mail::TestMailer
+    
+        AssertSelectMailer.test('<div><p>foo</p><p>bar</p></div>').deliver_now
+    assert_select_email do
+      assert_select 'div:root' do
+        assert_select 'p:first-child', 'foo'
+        assert_select 'p:last-child', 'bar'
+      end
+    end
+  end
+    
+      teardown do
+    ActionMailer::Base.delivery_method = @old_delivery_method
+    new = ActionMailer::Base.delivery_methods.dup
+    new.delete(:custom)
+    ActionMailer::Base.delivery_methods = new
+  end
+    
+    class HelperMailer < ActionMailer::Base
+  def use_mail_helper
+    @text = 'But soft! What light through yonder window breaks? It is the east, ' \
+            'and Juliet is the sun. Arise, fair sun, and kill the envious moon, ' \
+            'which is sick and pale with grief that thou, her maid, art far more ' \
+            'fair than she. Be not her maid, for she is envious! Her vestal ' \
+            'livery is but sick and green, and none but fools do wear it. Cast ' \
+            'it off!'
+    
+    def usage
+  <<-EOS
+Usage: list_login_items_for_app <path.app>
+    
+        def silence_log
+      @silence = true
+      yield
+    ensure
+      @silence = false
+    end
   end
 end
+
     
-          # Group an array of items by an expression
-      #
-      # input - the object array
-      # variable - the variable to assign each item to in the expression
-      # expression -a Liquid comparison expression passed in as a string
-      #
-      # Returns the filtered array of objects
-      def group_by_exp(input, variable, expression)
-        return input unless groupable?(input)
+      find_files = ->(path) {
+    Find.find(Pathname.new(path).relative_path_from(Pathname.new Dir.pwd).to_s).map do |path|
+      path if File.file?(path)
+    end.compact
+  }
     
-        # Public: Setup the plugin search path
-    #
-    # Returns an Array of plugin search paths
-    def plugins_path
-      if site.config['plugins_dir'].eql? Jekyll::Configuration::DEFAULTS['plugins_dir']
-        [site.in_source_dir(site.config['plugins_dir'])]
-      else
-        Array(site.config['plugins_dir']).map { |d| File.expand_path(d) }
-      end
-    end
+      private
     
-      module ClassMethods
-    def load_types_in(module_name, my_name = module_name.singularize)
-      const_set(:MODULE_NAME, module_name)
-      const_set(:BASE_CLASS_NAME, my_name)
-      const_set(:TYPES, Dir[Rails.root.join('app', 'models', module_name.underscore, '*.rb')].map { |path| module_name + '::' + File.basename(path, '.rb').camelize })
-    end
-    
-      def tumblr_oauth_token_secret
-    service.secret
-  end
-    
-      def show
-    respond_to do |format|
-      format.html
-      format.json { render json: @event }
-    end
-  end
-    
-        #
-    # @param ips [String] IP range(s)
-    # @return [true] if actions on ips are allowed.
-    # @return [false] if actions are not allowed on ips.
-    def allow_actions_on?(ips)
-      return true unless limit_to_network
-      return true unless boundary
-      return true if boundary.empty?
-      boundaries = Shellwords.split(boundary)
-      return true if boundaries.empty? # It's okay if there is no boundary range after all
-      given_range = Rex::Socket::RangeWalker.new(ips)
-      return false unless given_range # Can't do things to nonexistant IPs
-      allowed = false
-      boundaries.each do |boundary_range|
-        ok_range = Rex::Socket::RangeWalker.new(boundary)
-        allowed  = true if ok_range.include_range? given_range
-      end
-      return allowed
-    end
-    
-    require 'rex/post/meterpreter'
-    
-            attr_writer :log_levels
-    
-        # Returns the CSS for the media query.
-    #
-    # @return [String]
-    def to_css
-      css = ''
-      css << resolved_modifier
-      css << ' ' unless resolved_modifier.empty?
-      css << resolved_type
-      css << ' and ' unless resolved_type.empty? || expressions.empty?
-      css << expressions.map do |e|
-        # It's possible for there to be script nodes in Expressions even when
-        # we're converting to CSS in the case where we parsed the document as
-        # CSS originally (as in css_test.rb).
-        e.map {|c| c.is_a?(Sass::Script::Tree::Node) ? c.to_sass : c.to_s}.join
-      end.join(' and ')
-      css
-    end
-    
-      module Sass::Plugin::Configuration
-    # Different default options in a m environment.
-    def default_options
-      @default_options ||= begin
-        version = Merb::VERSION.split('.').map {|n| n.to_i}
-        if version[0] <= 0 && version[1] < 5
-          root = MERB_ROOT
-          env  = MERB_ENV
+        case list
+    when Hash
+      list
+    when Array, String, :DATA
+      { p1: list }
+    else
+      {}
+    end.each_pair do |strip, urls|
+      Array(urls).each do |url|
+        case url
+        when :DATA
+          patch = DATAPatch.new(strip)
         else
-          root = Merb.root.to_s
-          env  = Merb.environment
+          patch = LegacyPatch.new(strip, url)
         end
-    
-        private
-    
-        def render(context)
-      output = super
-      types = {
-        '.mp4' => 'type='video/mp4; codecs=\'avc1.42E01E, mp4a.40.2\''',
-        '.ogv' => 'type='video/ogg; codecs=theora, vorbis'',
-        '.webm' => 'type='video/webm; codecs=vp8, vorbis''
-      }
-      if @videos.size > 0
-        video =  '<video #{sizes} preload='metadata' controls #{poster}>'
-        @videos.each do |v|
-          video << '<source src='#{v}' #{types[File.extname(v)]}>'
-        end
-        video += '</video>'
-      else
-        'Error processing input, expected syntax: {% video url/to/video [url/to/video] [url/to/video] [width height] [url/to/poster] %}'
+        patches << patch
       end
     end
     
-          def deliver(msg)
-        if msg.respond_to?(:deliver_now)
-          # Rails 4.2/5.0
-          msg.deliver_now
-        else
-          # Rails 3.2/4.0/4.1
-          msg.deliver
+          def fetch_public_key(o_auth_app, jwt)
+        public_key = fetch_public_key_from_json(o_auth_app.jwks, jwt)
+        if public_key.empty? && o_auth_app.jwks_uri
+          response = Faraday.get(o_auth_app.jwks_uri)
+          public_key = fetch_public_key_from_json(response.body, jwt)
         end
+        raise Rack::OAuth2::Server::Authorize::BadRequest(:unauthorized_client) if public_key.empty?
+        public_key
       end
-    end
     
-      # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+          mid_string_or_interp = to_string_interpolation(interp.mid)
