a0c3558c8485030eedd38ddce50717cacf052909
SUNsung <weatherius@gmail.com>
2018-05-22 17:23:49 +0300

    Rewrite [21OoCH90be48s21HgEJgye.9U21LTJYFLB1xk21rkf.rb] File in: 2018-05-22 17:23:49

--- ruby/21OoCH90be48s21HgEJgye.9U21LTJYFLB1xk21rkf.rb
+++ ruby/21OoCH90be48s21HgEJgye.9U21LTJYFLB1xk21rkf.rb
@@ -1,35 +1,87 @@
 
         
-            private
+            formula.brew do
+      formula.patch
     
-        def normalized_path
-      path == '' ? '/' : path
+      # True if a {Formula} is being built in C++11 mode.
+  def cxx11?
+    include?('c++11') && option_defined?('c++11')
+  end
+    
+      def observe_file_removal(path)
+    path.extend(ObserverPathnameExtension).unlink if path.exist?
+  end
+    
+      def clang_build
+    @clang_build ||= MacOS.clang_build_version if MacOS.has_apple_developer_tools?
+  end
+    
+        first_warning = true
+    methods.each do |method|
+      unless checks.respond_to?(method)
+        Homebrew.failed = true
+        puts 'No check available by the name: #{method}'
+        next
+      end
+    
+      it 'implicitly has a count of one when no count is specified' do
+    'abc'.unpack(unpack_format).should == ['a']
+  end
+    
+        t.join
+  end
+    
+      # Send deprecation notices to registered listeners.
+  config.active_support.deprecation = :notify
+    
+      # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
+    
+        #
+    # Instance Methods
+    #
+    
+    unless ENV['BUNDLE_GEMFILE']
+  require 'pathname'
+    
+            report_auth_info(s.merge({:active => false}))
+        print_status('Failed IMAP Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]} (#{s[:banner].strip})')
+    
+    meterp.sock.close
+
+    
+    run SinatraStaticServer
+
+    
+        def paragraphize(input)
+      '<p>#{input.lstrip.rstrip.gsub(/\n\n/, '</p><p>').gsub(/\n/, '<br/>')}</p>'
     end
+  end
+end
     
-            css('p > code:first-child:last-child', 'td > code:first-child:last-child').each do |node|
-          next if node.previous.try(:content).present? || node.next.try(:content).present?
-          node.inner_html = node.inner_html.squish.gsub(/<br(\ \/)?>\s*/, '\n')
-          node.content = node.content.strip
-          node.name = 'pre' if node.content =~ /\s/
-          node.parent.before(node.parent.children).remove if node.parent.name == 'p'
-        end
+          locations = Array.new
+      while (data.code.to_i == 301 || data.code.to_i == 302)
+        data = handle_gist_redirecting(data)
+        break if locations.include? data.header['Location']
+        locations << data.header['Location']
+      end
     
-      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/follow
-  def follow
-    f = Follow.last
-    NotificationMailer.follow(f.target_account, Notification.find_by(activity: f))
+    module Jekyll
+    
+      # DELETE /books/1
+  # DELETE /books/1.json
+  def destroy
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
     
-      def generate_error_hash(error)
-    {
-      :path => request.path,
-      :status => error.status_code,
-      :error => error.to_hash
-    }
-  end
+            MSG = 'Do not freeze immutable objects, as freezing them has no ' \
+              'effect.'.freeze
     
-              private
-    
-    Dir.chdir APP_ROOT do
-  # This script is a starting point to setup your application.
-  # Add necessary setup steps to this file:
+                  def do_something
+              end
