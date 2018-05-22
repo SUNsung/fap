4f04f0893a5817b2d2a663ef35aeddf37e0a6f11
SUNsung <weatherius@gmail.com>
2018-05-22 06:02:24 +0300

    Rewrite [10JBr5iBKzbA108a0LhGRZCM10xflgi7biODk10vnu.rb] File in: 2018-05-22 06:02:23

--- ruby/10JBr5iBKzbA108a0LhGRZCM10xflgi7biODk10vnu.rb
+++ ruby/10JBr5iBKzbA108a0LhGRZCM10xflgi7biODk10vnu.rb
@@ -1,63 +1,66 @@
-    def subpath_from(url, options = nil)
-      self.class.parse(url).subpath_to(self, options)
+
+        
+            begin
+      response = U2F::RegisterResponse.load_from_json(params[:device_response])
+      registration_data = u2f.register!(challenges, response)
+      registration.update(certificate: registration_data.certificate,
+                          key_handle: registration_data.key_handle,
+                          public_key: registration_data.public_key,
+                          counter: registration_data.counter,
+                          user: user,
+                          name: params[:name])
+    rescue JSON::ParserError, NoMethodError, ArgumentError
+      registration.errors.add(:base, 'Your U2F device did not send a valid JSON response.')
+    rescue U2F::Error => e
+      registration.errors.add(:base, e.message)
     end
     
-            css('p > code:first-child:last-child', 'td > code:first-child:last-child').each do |node|
-          next if node.previous.try(:content).present? || node.next.try(:content).present?
-          node.inner_html = node.inner_html.squish.gsub(/<br(\ \/)?>\s*/, '\n')
-          node.content = node.content.strip
-          node.name = 'pre' if node.content =~ /\s/
-          node.parent.before(node.parent.children).remove if node.parent.name == 'p'
-        end
-    
-              if %w(Events Sync).include?(type)
-            name.prepend 'Backbone.'
-          elsif type == 'History'
-            name.prepend 'Backbone.history.'
-          elsif name == 'extend'
-            name.prepend '#{type}.'
-          elsif name.start_with? 'constructor'
-            name = type
-          elsif type != 'Utility'
-            name.prepend '#{type.downcase}.'
-          end
-    
-      def test_empty?
-    assert_empty(@cls[])
-    assert_not_empty(@h)
-  end
-    
-      it 'decodes the number of bytes specified by the count modifier including whitespace bytes' do
-    [ ['a bc',  ['a b', 'c']],
-      ['a\fbc', ['a\fb', 'c']],
-      ['a\nbc', ['a\nb', 'c']],
-      ['a\rbc', ['a\rb', 'c']],
-      ['a\tbc', ['a\tb', 'c']],
-      ['a\vbc', ['a\vb', 'c']]
-    ].should be_computed_by(:unpack, unpack_format(3)+unpack_format)
-  end
-    
-      it 'creates a time based on given C-style gmtime arguments, interpreted as UTC (GMT)' do
-    time = Time.send(@method, 1, 15, 20, 1, 1, 2000, :ignored, :ignored, :ignored, :ignored)
-    time.inspect.should == '2000-01-01 20:15:01 UTC'
-  end
-    
-        describe 'feed regeneration' do
-      before do
-        alice = Fabricate(:account)
-        bob   = Fabricate(:account)
-    
-            p environment.var(name)
-      else
-        p Script::Parser.parse(text, @line, 0).perform(environment)
+        def add_required_validations
+      options = Paperclip::Attachment.default_options.deep_merge(@options)
+      if options[:validate_media_type] != false
+        name = @name
+        @klass.validates_media_type_spoof_detection name,
+          :if => ->(instance){ instance.send(name).dirty? }
       end
-    rescue Sass::SyntaxError => e
-      puts 'SyntaxError: #{e.message}'
-      if @options[:trace]
-        e.backtrace.each do |line|
-          puts '\tfrom #{line}'
+    end
+    
+            def matches? subject
+          @subject = subject
+          @subject = @subject.class unless Class === @subject
+          responds? && has_column?
         end
+    
+      # Get list of styles saved on previous deploy (running rake paperclip:refresh:missing_styles)
+  def self.get_registered_attachments_styles
+    YAML.load_file(Paperclip.registered_attachments_styles_path)
+  rescue Errno::ENOENT
+    nil
+  end
+  private_class_method :get_registered_attachments_styles
+    
+        def processor(name) #:nodoc:
+      @known_processors ||= {}
+      if @known_processors[name.to_s]
+        @known_processors[name.to_s]
+      else
+        name = name.to_s.camelize
+        load_processor(name) unless Paperclip.const_defined?(name)
+        processor = Paperclip.const_get(name)
+        @known_processors[name.to_s] = processor
+      end
+    end
+    
+          if defined?(ActiveRecord)
+        Paperclip.options[:logger] = ActiveRecord::Base.logger
+        ActiveRecord::Base.send(:include, Paperclip::Glue)
       end
     end
   end
 end
+
+    
+        def _2
+      elements[3]
+    end
+    
+        alias_method :load_debug, :load
