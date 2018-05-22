119e43dce57a7802b4f7ecb8a9853608b10cccca
SUNsung <weatherius@gmail.com>
2018-05-22 12:43:32 +0300

    Rewrite [46ztBjb7dXnOE46Zrz0c6Uk3M46M4uroKtzJqE46u2E.py] File in: 2018-05-22 12:43:31

--- pyton/46ztBjb7dXnOE46Zrz0c6Uk3M46M4uroKtzJqE46u2E.py
+++ pyton/46ztBjb7dXnOE46Zrz0c6Uk3M46M4uroKtzJqE46u2E.py
@@ -1,19 +1,54 @@
 
         
-        
-def _is_tar_extract(cmd):
-    if '--extract' in cmd:
-        return True
-    
-            Accessing a node updates its position to the front of the LRU list.
-        '''
-        node = self.lookup[query]
-        if node is None:
-            return None
-        self.linked_list.move_to_front(node)
-        return node.results
+        import rsa
+import json
+from binascii import hexlify
     
     
-class GroupChat(Chat):
+def format_size(bytes):
+    return '%s (%d bytes)' % (format_bytes(bytes), bytes)
     
-    from enum import Enum
+        params = {
+        'age_limit': age,
+        'skip_download': True,
+        'writeinfojson': True,
+        'outtmpl': '%(id)s.%(ext)s',
+    }
+    ydl = YoutubeDL(params)
+    ydl.add_default_info_extractors()
+    json_filename = os.path.splitext(filename)[0] + '.info.json'
+    try_rm(json_filename)
+    ydl.download([url])
+    res = os.path.exists(json_filename)
+    try_rm(json_filename)
+    return res
+    
+                    if ''' not in code and ''' not in code:
+                    continue
+                assertRegexpMatches(
+                    self,
+                    code,
+                    r'(?:(?:#.*?|\s*)\n)*from __future__ import (?:[a-z_]+,\s*)*unicode_literals',
+                    'unicode_literals import  missing in %s' % fn)
+    
+        _ALL_CLASSES = [
+        klass
+        for name, klass in globals().items()
+        if name.endswith('IE') and name != 'GenericIE'
+    ]
+    _ALL_CLASSES.append(GenericIE)
+    
+            formats = [
+            {
+                'format_id': f['type'],
+                'filesize': int(f['filesize']),
+                'url': f['url']
+            } for f in info['rfiles']
+        ]
+        self._sort_formats(formats)
+    
+        base = 'src/sentry/locale'
+    for locale in os.listdir(base):
+        fn = os.path.join(base, locale, 'LC_MESSAGES', 'django.po')
+        if not os.path.isfile(fn):
+            continue
