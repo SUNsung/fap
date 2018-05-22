d8d7638fac65877f02f1ad9c17d10194c245666f
SUNsung <weatherius@gmail.com>
2018-05-22 12:13:10 +0300

    Rewrite [264QfYILhqLr626tSxsWDpzew26Gx5TWgyd2Mc26u9I.cpp] File in: 2018-05-22 12:13:10

--- cccccc/264QfYILhqLr626tSxsWDpzew26Gx5TWgyd2Mc26u9I.cpp
+++ cccccc/264QfYILhqLr626tSxsWDpzew26Gx5TWgyd2Mc26u9I.cpp
@@ -1,272 +1,102 @@
 
         
-          std::string toStyledString() const;
+            cv::eigen2cv(eigval_real, eigenval_real);
+    cv::eigen2cv(eigval_imag, eigenval_imag);
+    cv::eigen2cv(eigvec_real, eigenvec_real);
+    cv::eigen2cv(eigvec_imag, eigenvec_imag);
+#else
+    EigenvalueDecomposition es(Mtilde);
+    eigenval_real = es.eigenvalues();
+    eigenvec_real = es.eigenvectors();
+    eigenval_imag = eigenvec_imag = cv::Mat();
+#endif
     
-      // Find a file by file name.
-  bool FindFileByName(const string& filename,
-                      FileDescriptorProto* output);
-    
-    // CodeGenerator implementation which generates a C++ source file and
-// header.  If you create your own protocol compiler binary and you want
-// it to support C++ output, you can do so by registering an instance of this
-// CodeGenerator with the CommandLineInterface in your main() function.
-class LIBPROTOC_EXPORT CppGenerator : public CodeGenerator {
- public:
-  CppGenerator();
-  ~CppGenerator();
+                //a simple check that the position is general:
+            //  for each line check that all other points don't belong to it
+            isGeneralPosition = true;
+            for (int startPointIndex = 0; startPointIndex < usedPointsCount && isGeneralPosition; startPointIndex++)
+            {
+                for (int endPointIndex = startPointIndex + 1; endPointIndex < usedPointsCount && isGeneralPosition; endPointIndex++)
+                {
+    }
     }
     
-      std::string namespace_;
-  std::string reflectionClassname_;
+    #undef cv_hal_gemm32f
+#define cv_hal_gemm32f lapack_gemm32f
+#undef cv_hal_gemm64f
+#define cv_hal_gemm64f lapack_gemm64f
+#undef cv_hal_gemm32fc
+#define cv_hal_gemm32fc lapack_gemm32fc
+#undef cv_hal_gemm64fc
+#define cv_hal_gemm64fc lapack_gemm64fc
     
+    #include <vector>
     
-    
-    #include <google/protobuf/compiler/csharp/csharp_doc_comment.h>
-#include <google/protobuf/compiler/csharp/csharp_helpers.h>
-#include <google/protobuf/compiler/csharp/csharp_repeated_primitive_field.h>
-    
-    
-    
-    #ifndef incl_HPHP_WORKLOAD_STATS_H_
-#define incl_HPHP_WORKLOAD_STATS_H_
-    
-    int gettime(clockid_t clock, timespec* ts) {
-  if (clock != CLOCK_THREAD_CPUTIME_ID) {
-    return folly::chrono::clock_gettime(clock, ts);
+    void OutputImage::SaveToJpegData(JPEGData* jpg) const {
+  assert(components_[0].factor_x() == 1);
+  assert(components_[0].factor_y() == 1);
+  jpg->width = width_;
+  jpg->height = height_;
+  jpg->max_h_samp_factor = 1;
+  jpg->max_v_samp_factor = 1;
+  jpg->MCU_cols = components_[0].width_in_blocks();
+  jpg->MCU_rows = components_[0].height_in_blocks();
+  int ncomp = components_[1].IsAllZero() && components_[2].IsAllZero() ? 1 : 3;
+  for (int i = 1; i < ncomp; ++i) {
+    jpg->max_h_samp_factor = std::max(jpg->max_h_samp_factor,
+                                      components_[i].factor_x());
+    jpg->max_v_samp_factor = std::max(jpg->max_h_samp_factor,
+                                      components_[i].factor_y());
+    jpg->MCU_cols = std::min(jpg->MCU_cols, components_[i].width_in_blocks());
+    jpg->MCU_rows = std::min(jpg->MCU_rows, components_[i].height_in_blocks());
   }
+  jpg->components.resize(ncomp);
+  int q[3][kDCTBlockSize];
+  for (int c = 0; c < 3; ++c) {
+    memcpy(&q[c][0], components_[c].quant(), kDCTBlockSize * sizeof(q[0][0]));
+  }
+  for (int c = 0; c < ncomp; ++c) {
+    JPEGComponent* comp = &jpg->components[c];
+    assert(jpg->max_h_samp_factor % components_[c].factor_x() == 0);
+    assert(jpg->max_v_samp_factor % components_[c].factor_y() == 0);
+    comp->id = c;
+    comp->h_samp_factor = jpg->max_h_samp_factor / components_[c].factor_x();
+    comp->v_samp_factor = jpg->max_v_samp_factor / components_[c].factor_y();
+    comp->width_in_blocks = jpg->MCU_cols * comp->h_samp_factor;
+    comp->height_in_blocks = jpg->MCU_rows * comp->v_samp_factor;
+    comp->num_blocks = comp->width_in_blocks * comp->height_in_blocks;
+    comp->coeffs.resize(kDCTBlockSize * comp->num_blocks);
+    }
     }
     
-      /**
-   * Add a piece of response to the pipeline.
-   */
-  static void AddToPipeline(const std::string &s);
+    // This function will create a Huffman tree.
+//
+// The (data,length) contains the population counts.
+// The tree_limit is the maximum bit depth of the Huffman codes.
+//
+// The depth contains the tree, i.e., how many bits are used for
+// the symbol.
+//
+// The actual Huffman tree is constructed in the tree[] array, which has to
+// be at least 2 * length + 1 long.
+//
+// See http://en.wikipedia.org/wiki/Huffman_coding
+void CreateHuffmanTree(const uint32_t *data,
+                       const size_t length,
+                       const int tree_limit,
+                       HuffmanTree* tree,
+                       uint8_t *depth);
     
-    //////////////////////////////////////////////////////////////////////
     
-    #include 'hphp/runtime/vm/jit/vasm-unit.h'
-    
-      /*
-   * Create a block intended to be used temporarily, as part of modifying
-   * existing code.
-   *
-   * Although not necessary for correctness, the block may be freed with
-   * freeScratchBlock when finished.
-   */
-  Vlabel makeScratchBlock();
-    
-    
-    {
-    {
-    {}}}
+    {}  // namespace guetzli
 
     
-    #ifndef incl_HPHP_JIT_TC_PROLOGUE_H_
-#define incl_HPHP_JIT_TC_PROLOGUE_H_
     
-    std::unique_ptr<ScenarioResult> RunScenario(
-    const grpc::testing::ClientConfig& client_config, size_t num_clients,
-    const grpc::testing::ServerConfig& server_config, size_t num_servers,
-    int warmup_seconds, int benchmark_seconds, int spawn_local_worker_count,
-    const grpc::string& qps_server_target_override,
-    const grpc::string& credential_type, bool run_inproc);
+    {}  // namespace guetzli
     
-    #include <iostream>
-#include <vector>
+    #include 'guetzli/jpeg_data.h'
     
-    class CodegenTestMinimal : public ::testing::Test {};
+    #endif  // GUETZLI_JPEG_DATA_ENCODER_H_
+
     
-        // Taken from Caffe2
-    REGISTER_OPERATOR_SCHEMA(Softplus)
-        .Description('Softplus takes one input data (Tensor<T>) and produces one output '
-            'data (Tensor<T>) where the function, y = ln(1 + exp(steepness * x)), is '
-            'applied to the tensor elementwise.')
-        .Input('X', 'Input tensor, typically 1-D.', 'T')
-        .Output('Y', 'Output tensor of same shape and type as input X.', 'T')
-        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
-            'Constrain input and output types to float tensors.');
-    
-        //‘GREATER’, ‘LESS’, ‘EQUALS,
-    REGISTER_BINARY_COMPARISON_OPERATOR_SCHEMA(Greater)
-        REGISTER_BINARY_COMPARISON_OPERATOR_SCHEMA(Less)
-        REGISTER_BINARY_COMPARISON_OPERATOR_SCHEMA(Equal)
-    
-        // Taken from ONNX
-    REGISTER_OPERATOR_SCHEMA(BatchNormalization)
-        .Description('Carries out batch normalization as described in the paper'
-            'https://arxiv.org/abs/1502.03167. Depending on the mode it is being run,'
-            'there are multiple cases for the number of outputs, which we list below:'
-            ''
-            'Output case #1: Y, mean, var, saved_mean, saved_var (training mode)'
-            'Output case #2: Y (test mode)')
-        .Input('X',
-            'The input 4-dimensional tensor of shape NCHW or NHWC depending '
-            'on the order parameter.',
-            'T')
-        .Input('scale',
-            'The scale as a 1-dimensional tensor of size C to be applied to the '
-            'output.',
-            'T')
-        .Input('B',
-            'The bias as a 1-dimensional tensor of size C to be applied to the '
-            'output.',
-            'T')
-        .Input('mean',
-            'The running mean (training) or the estimated mean (testing) '
-            'as a 1-dimensional tensor of size C.',
-            'T')
-        .Input('var',
-            'The running variance (training) or the estimated '
-            'variance (testing) as a 1-dimensional tensor of size C.',
-            'T')
-        .Output('Y', 'The output 4-dimensional tensor of the same shape as X.',
-            'T')
-        .Output('mean',
-            'The running mean after the BatchNormalization operator. Must be in-place '
-            'with the input mean. Should not be used for testing.',
-            'T')
-        .Output('var',
-            'The running variance after the BatchNormalization operator. Must be '
-            'in-place with the input var. Should not be used for testing.',
-            'T')
-        .Output('saved_mean',
-            'Saved mean used during training to speed up gradient '
-            'computation. Should not be used for testing.',
-            'T')
-        .Output('saved_var',
-            'Saved variance used during training to speed up '
-            'gradient computation. Should not be used for testing.',
-            'T')
-        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(double)' },
-            'Constrain input and output types to float tensors.')
-        .Attr('epsilon',
-            'The epsilon value to use to avoid division by zero.',
-            AttrType::AttributeProto_AttributeType_FLOAT)
-        .Attr('is_test',
-            'If set to nonzero, run spatial batch normalization in test mode.',
-            AttrType::AttributeProto_AttributeType_INT)
-        .Attr('momentum',
-            'Factor used in computing the running mean and variance.'
-            'e.g., running_mean = running_mean * momentum + mean * (1 - momentum)',
-            AttrType::AttributeProto_AttributeType_FLOAT)
-        .Attr('spatial',
-            'Compute the mean and variance across all spatial elements or per feature.',
-            AttrType::AttributeProto_AttributeType_INT);
-    
-    
-    {        totalNumSequences += chunk.NumberOfSequences();
-        m_chunkToFileIndex.insert(make_pair(&chunk, m_latticeFiles.size() - 1));
-        m_chunks.push_back(&chunk);
-        if (m_chunks.size() >= numeric_limits<ChunkIdType>::max())
-            RuntimeError('Number of chunks exceeded overflow limit.');
-    }
-    
-    cudaStream_t GPUDataTransferer::s_assignStream = NULL;
-    
-    class DNS {
-  public:
-   typedef std::vector<std::string> (*DNSFunc)(const std::string& host);
-    }
-    
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
-    
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
-    
-    TEST(IOBuf, QueueAppenderInsertClone) {
-  IOBuf buf{IOBuf::CREATE, 100};
-  folly::IOBufQueue queue;
-  QueueAppender appender{&queue, 100};
-  // Buffer is shared, so we create a new buffer to write to
-  appender.insert(buf);
-  uint8_t x = 42;
-  appender.pushAtMost(&x, 1);
-  EXPECT_EQ(2, queue.front()->countChainElements());
-  EXPECT_EQ(0, queue.front()->length());
-  EXPECT_LT(0, queue.front()->tailroom());
-  EXPECT_EQ(1, queue.front()->next()->length());
-  EXPECT_EQ(x, queue.front()->next()->data()[0]);
-}
-    
-    /**
- * Get current exceptions being handled.  front() is the most recent exception.
- * There should be at most one unless rethrowing.
- */
-std::vector<ExceptionInfo> getCurrentExceptions();
-    
-        // Try to grow in place.
-    //
-    // Note that xallocx(MALLOCX_ZERO) will only zero newly allocated memory,
-    // even if a previous allocation allocated more than we requested.
-    // This is fine; we always use MALLOCX_ZERO with jemalloc and we
-    // always expand our allocation to the real size.
-    if (prevCapacity * sizeof(ElementWrapper) >= jemallocMinInPlaceExpandable) {
-      success =
-          (xallocx(threadEntry->elements, newByteSize, 0, MALLOCX_ZERO) ==
-           newByteSize);
-    }
-    
-    #pragma once
-    
-        copy.unshareOne();
-    EXPECT_TRUE(buf->isShared());
-    EXPECT_FALSE(copy.isShared());
-    EXPECT_NE((void*)buf->data(), (void*)copy.data());
-    EXPECT_TRUE(copy2.isShared());
-    
-    #include <sys/stat.h>
-#include <sys/types.h>
-#include <cstddef>
-#include <string>
-#include <utility>
-#include <vector>
-    
-    
-    {
-    {    for (size_t i = 0; i < nrRead; i++) {
-      int id = completed[i] - ops.get();
-      EXPECT_GE(id, 0);
-      EXPECT_LT(id, specs.size());
-      EXPECT_TRUE(pending[id]);
-      pending[id] = false;
-      ssize_t res = ops[id].result();
-      EXPECT_LE(0, res) << folly::errnoStr(-res);
-      EXPECT_EQ(specs[id].size, res);
-    }
-  }
-  EXPECT_EQ(specs.size(), aioReader.totalSubmits());
-  EXPECT_EQ(aioReader.pending(), 0);
-  EXPECT_EQ(aioQueue.queued(), 0);
-  for (size_t i = 0; i < pending.size(); i++) {
-    EXPECT_FALSE(pending[i]);
-  }
-}
-    
-    template <typename Observable, typename Traits>
-class ObserverCreator<Observable, Traits>::Context {
- public:
-  template <typename... Args>
-  Context(Args&&... args) : observable_(std::forward<Args>(args)...) {
-    updateValue();
-  }
-    }
-    
-    
-    {
-    {
-    {  std::once_flag observerInit_;
-  folly::Optional<Observer<T>> observer_;
-};
-} // namespace observer
-} // namespace folly
-    
-      /**
-   * Gets current view of the observed object.
-   * This is safe to call from any thread. If this is called from other Observer
-   * functor then that Observer is marked as dependent on current Observer.
-   */
-  VersionedData getData();
+    #include <string>
