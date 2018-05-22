16b2cc600b6583fd4a39ab8169c8ddd83f44610c
SUNsung <weatherius@gmail.com>
2018-05-22 19:12:38 +0300

    Rewrite [44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp] File in: 2018-05-22 19:12:38

--- cccccc/44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp
+++ cccccc/44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp
@@ -1,168 +1,231 @@
 
         
         
-    {    bool checkSubsetPublic( const CvMat* ms1, int count, bool checkPartialSubset );
-};
+    {}  // namespace atom
     
-    // GL type for 'half' precision (s10e5) float data in host memory
-#ifndef GL_ARB_half_float_pixel
-    typedef unsigned short GLhalfARB;
-#endif
-#ifndef GL_NV_half_float
-    typedef unsigned short GLhalfNV;
-#endif
-    
-    void freeifaddrs(struct ifaddrs* addrs) {
-	struct ifaddrs* last = NULL;
-	struct ifaddrs* cursor = addrs;
-	while (cursor) {
-		delete[] cursor->ifa_name;
-		delete cursor->ifa_addr;
-		delete cursor->ifa_netmask;
-		last = cursor;
-		cursor = cursor->ifa_next;
-		delete last;
-	}
-}
-
-    
-    #include 'io/stream_peer.h'
-    
-    	_create = _create_func;
-	load_certs_func = _load_certs;
-    
-    #if defined(MBEDTLS_GCM_C) && (                                        \
-        !defined(MBEDTLS_AES_C) && !defined(MBEDTLS_CAMELLIA_C) )
-#error 'MBEDTLS_GCM_C defined, but not all prerequisites'
-#endif
-    
-    	MonoObject *mono_object = mono_object_new(mono_domain_get(), klass->get_mono_ptr());
-    
-    	ClassDB::bind_method(D_METHOD('play'), &VideoPlayer::play);
-	ClassDB::bind_method(D_METHOD('stop'), &VideoPlayer::stop);
-    
-    	friend class GDNativeLibraryResourceLoader;
-	friend class GDNative;
-    
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-    
-    
-    
-    #ifdef __cplusplus
-extern 'C' {
-#endif
-#include 'tolua++.h'
-#ifdef __cplusplus
-}
-#endif
-    
-    
-    
-    
-    
-    // This class implements debug drawing callbacks that are invoked
-// inside b2World::Step.
-class GLESDebugDraw : public b2Draw
-{
-    float32 mRatio;
-    cocos2d::GLProgram* mShaderProgram;
-    GLint        mColorLocation;
+    namespace atom {
     }
     
+      // Sent when an omnibox extension has sent back omnibox suggestions. The
+  // source is the profile, and the details are an
+  // extensions::api::omnibox::SendSuggestions::Params object.
+  NOTIFICATION_EXTENSION_OMNIBOX_SUGGESTIONS_READY,
     
-    {	if (callback.m_fixture)
-	{
-		b2Body* body = callback.m_fixture->GetBody();
-		b2MouseJointDef md;
-		md.bodyA = m_groundBody;
-		md.bodyB = body;
-		md.target = p;
-		md.maxForce = 1000.0f * body->GetMass();
-		m_mouseJoint = (b2MouseJoint*)m_world->CreateJoint(&md);
-		body->SetAwake(true);
-        return true;
-	}
+     private:
+  // Non-owning pointer.
+  content::BrowserPpapiHost* host_;
     
-    return false;
-}
+    #endif  // CHROME_BROWSER_UI_VIEWS_COLOR_CHOOSER_DIALOG_H_
+
     
-        int unit;
-    double value;
+    #define FPL FILE_PATH_LITERAL
     
-    class YogaTest_HadOverflowTests : public Test {
-protected:
-  YogaTest_HadOverflowTests() {
-    config = YGConfigNew();
-    root = YGNodeNewWithConfig(config);
-    YGNodeStyleSetWidth(root, 200);
-    YGNodeStyleSetHeight(root, 100);
-    YGNodeStyleSetFlexDirection(root, YGFlexDirectionColumn);
-    YGNodeStyleSetFlexWrap(root, YGWrapNoWrap);
+    
+    {} // namespace swift
+    
+    #include 'swift/AST/SyntaxASTMap.h'
+#include 'swift/AST/Expr.h'
+#include 'swift/AST/Decl.h'
+#include 'swift/AST/Stmt.h'
+#include 'swift/Syntax/Syntax.h'
+    
+    public:
+  Impl(CompletionKind kind, bool hasExpectedTypes);
+    
+    /// Index the given module and store the results to \p indexStorePath.
+///
+/// \param module The module to index.
+///
+/// \param indexUnitTokens A list of unique identifiers for the index units to
+/// be written. This may either be one unit per source file of \p module, or it
+/// may be a single unit, in which case all the index information will be
+/// combined into a single unit.
+///
+/// \param moduleUnitToken A unique identifier for this module unit in the form
+/// of a file path. Only used if \p indexUnitTokens are specified for each
+/// source file, otherwise the single \p indexUnitTokens value is used instead.
+///
+/// \param indexStorePath The location to write the indexing data to.
+///
+/// \param indexSystemModules If true, emit index data for imported serialized
+/// swift system modules.
+///
+/// \param isDebugCompilation true for non-optimized compiler invocation.
+///
+/// \param targetTriple The target for this compilation.
+///
+/// \param dependencyTracker The set of dependencies seen while building.
+bool indexAndRecord(ModuleDecl *module, ArrayRef<std::string> indexUnitTokens,
+                    StringRef moduleUnitToken, StringRef indexStorePath,
+                    bool indexSystemModules, bool isDebugCompilation,
+                    StringRef targetTriple,
+                    const DependencyTracker &dependencyTracker);
+// FIXME: indexUnitTokens could be StringRef, but that creates an impedance
+// mismatch in the caller.
+    
+        // Finish and check for builder errors
+    if (s.ok()) {
+      s = builder->Finish();
+      if (s.ok()) {
+        meta->file_size = builder->FileSize();
+        assert(meta->file_size > 0);
+      }
+    } else {
+      builder->Abandon();
+    }
+    delete builder;
+    
+    #include <stdlib.h>
+#include <unistd.h>
+#include 'leveldb/cache.h'
+#include 'leveldb/comparator.h'
+#include 'leveldb/db.h'
+#include 'leveldb/env.h'
+#include 'leveldb/filter_policy.h'
+#include 'leveldb/iterator.h'
+#include 'leveldb/options.h'
+#include 'leveldb/status.h'
+#include 'leveldb/write_batch.h'
+    
+      for (size_t i = 0; i < compact_pointers_.size(); i++) {
+    PutVarint32(dst, kCompactPointer);
+    PutVarint32(dst, compact_pointers_[i].first);  // level
+    PutLengthPrefixedSlice(dst, compact_pointers_[i].second.Encode());
   }
+    
+      ASSERT_TRUE(! Overlaps('a', 'b'));
+  ASSERT_TRUE(! Overlaps('z1', 'z2'));
+  ASSERT_TRUE(Overlaps('a', 'p'));
+  ASSERT_TRUE(Overlaps('a', 'q'));
+  ASSERT_TRUE(Overlaps('a', 'z'));
+  ASSERT_TRUE(Overlaps('p', 'p1'));
+  ASSERT_TRUE(Overlaps('p', 'q'));
+  ASSERT_TRUE(Overlaps('p', 'z'));
+  ASSERT_TRUE(Overlaps('p1', 'p2'));
+  ASSERT_TRUE(Overlaps('p1', 'z'));
+  ASSERT_TRUE(Overlaps('q', 'q'));
+  ASSERT_TRUE(Overlaps('q', 'q1'));
+    
+    void WriteBatchInternal::SetSequence(WriteBatch* b, SequenceNumber seq) {
+  EncodeFixed64(&b->rep_[0], seq);
+}
+    
+        const char* benchmarks = FLAGS_benchmarks;
+    while (benchmarks != NULL) {
+      const char* sep = strchr(benchmarks, ',');
+      Slice name;
+      if (sep == NULL) {
+        name = benchmarks;
+        benchmarks = NULL;
+      } else {
+        name = Slice(benchmarks, sep - benchmarks);
+        benchmarks = sep + 1;
+      }
     }
     
-        ~Config(void);
+    #ifndef STORAGE_LEVELDB_INCLUDE_FILTER_POLICY_H_
+#define STORAGE_LEVELDB_INCLUDE_FILTER_POLICY_H_
     
-    Node * Node::getParent(void)
-{
-    auto nodePtr = YGNodeGetParent(m_node);
-    }
+    For CIDFonts, its a little more complicated. Because CIDFonts can be
+large, using a glyph name as the key is unreasonable (it would also
+lead to unfeasibly large Encoding arrays), so instead we use a 'CID'
+as the key. CIDs are just numbers.
     
     
-    {  // There are subtle issues with calling the next functions directly. It is
-  // much better to always use a ThreadScope to manage attaching/detaching for
-  // you.
-  FBEXPORT static JNIEnv* ensureCurrentThreadIsAttached();
-  FBEXPORT static void detachCurrentThread();
+    {}  // namespace tesseract
+    
+      word_res = word_res_it.data();
+  if (word_res->word->flag(W_REP_CHAR) ||
+      word_res->combination ||
+      word_res->part_of_combo ||
+      !word_res->word->flag(W_DONT_CHOP))
+    return;
+    
+    #include <set>
+    
+    #include 'src/compiler/config.h'
+#include 'src/compiler/generator_helpers.h'
+    
+    
+    {  // Get all comments (leading, leading_detached, trailing) and print them as a
+  // docstring. Any leading space of a line will be removed, but the line
+  // wrapping will not be changed.
+  void PrintAllComments(std::vector<grpc::string> comments,
+                        grpc_generator::Printer* out);
 };
     
+    std::vector<grpc::string_ref> SecureAuthContext::FindPropertyValues(
+    const grpc::string& name) const {
+  if (!ctx_) {
+    return std::vector<grpc::string_ref>();
+  }
+  grpc_auth_property_iterator iter =
+      grpc_auth_context_find_properties_by_name(ctx_, name.c_str());
+  const grpc_auth_property* property = nullptr;
+  std::vector<grpc::string_ref> values;
+  while ((property = grpc_auth_property_iterator_next(&iter))) {
+    values.push_back(grpc::string_ref(property->value, property->value_length));
+  }
+  return values;
+}
     
-    {}
-
+    #ifndef TEST_QPS_USAGE_TIMER_H
+#define TEST_QPS_USAGE_TIMER_H
     
-      virtual ~AbstractAuthResolver();
-    
-      // Returns proxy method for given protocol. Either V_GET or V_TUNNEL
-  // is returned.  For HTTPS, always returns V_TUNNEL.
-  const std::string& resolveProxyMethod(const std::string& protocol) const;
-    
-      virtual void writeData(const unsigned char* data, size_t len,
-                         int64_t offset) CXX11_OVERRIDE;
-    
-      enum Flag {
-    FLAG_HIDDEN = 1,
-    FLAG_ERASE_AFTER_PARSE = 1 << 1,
-    FLAG_INITIAL_OPTION = 1 << 2,
-    FLAG_CHANGE_OPTION = 1 << 3,
-    FLAG_CHANGE_OPTION_FOR_RESERVED = 1 << 4,
-    FLAG_CHANGE_GLOBAL_OPTION = 1 << 5,
-    FLAG_CUMULATIVE = 1 << 6
-  };
-    
-    AdaptiveFileAllocationIterator::~AdaptiveFileAllocationIterator() = default;
-    
-    class AnnounceTier {
-public:
-  enum AnnounceEvent {
-    STARTED,
-    STARTED_AFTER_COMPLETION,
-    DOWNLOADING,
-    STOPPED,
-    COMPLETED,
-    SEEDING,
-    HALTED
-  };
+    	while (!stateflag) {
+		int ret = buffer_data();
+		if (ret == 0) break;
+		while (ogg_sync_pageout(&oy, &og) > 0) {
+			ogg_stream_state test;
+    }
     }
     
-    #endif // D_LIBSSL_TLS_CONTEXT_H
-
+    //#define SIMPLE_METHODDEF
     
     
-    {} // namespace aria2
+    {
+    {		property.hint_string = options;
+	}
+}
+    
+    
+    {	virtual btScalar addSingleResult(btCollisionWorld::LocalConvexResult &convexResult, bool normalInWorldSpace);
+};
+    
+        FreeType font driver for pcf fonts
+    
+    SEXP XGBoosterEvalOneIter_R(SEXP handle, SEXP iter, SEXP dmats, SEXP evnames) {
+  const char *ret;
+  R_API_BEGIN();
+  CHECK_EQ(length(dmats), length(evnames))
+      << 'dmats and evnams must have same length';
+  int len = length(dmats);
+  std::vector<void*> vec_dmats;
+  std::vector<std::string> vec_names;
+  std::vector<const char*> vec_sptr;
+  for (int i = 0; i < len; ++i) {
+    vec_dmats.push_back(R_ExternalPtrAddr(VECTOR_ELT(dmats, i)));
+    vec_names.push_back(std::string(CHAR(asChar(VECTOR_ELT(evnames, i)))));
+  }
+  for (int i = 0; i < len; ++i) {
+    vec_sptr.push_back(vec_names[i].c_str());
+  }
+  CHECK_CALL(XGBoosterEvalOneIter(R_ExternalPtrAddr(handle),
+                                asInteger(iter),
+                                BeginPtr(vec_dmats),
+                                BeginPtr(vec_sptr),
+                                len, &ret));
+  R_API_END();
+  return mkString(ret);
+}
+    
+    TEST(Metric, MAP) {
+  xgboost::Metric * metric = xgboost::Metric::Create('map');
+  ASSERT_STREQ(metric->Name(), 'map');
+  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 1, 1e-10);
+  EXPECT_NEAR(GetMetricEval(metric,
+                            {0.1f, 0.9f, 0.1f, 0.9f},
+                            {  0,   0,   1,   1}),
+              0.5f, 0.001f);
+  EXPECT_NEAR(GetMetricEval(metric, {}, {}), 1, 1e-10);
+    }
