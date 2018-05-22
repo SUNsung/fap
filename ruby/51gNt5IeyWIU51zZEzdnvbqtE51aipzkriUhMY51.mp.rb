cf451af533eb8a03f25c2427abae8ce0cbd251e6
SUNsung <weatherius@gmail.com>
2018-05-22 10:49:22 +0300

    Rewrite [51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb] File in: 2018-05-22 10:49:21

--- ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
+++ ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
@@ -1,176 +1,77 @@
 
         
-        class JavaScriptHelperTest < ActionView::TestCase
-  tests ActionView::Helpers::JavaScriptHelper
-    
-            @disable_request_forgery_protection = false
-        @allow_same_origin_as_host = true
-      end
-    
-        def index
-      self.response_body = 'Hello World'
-    end
-  end
-    
-            assert_equal 'summary, title', @controller.response.body
-        assert @controller.params.has_key?(:summary)
-        assert @controller.params.has_key?(:title)
-        assert_equal 'content...', @controller.params['summary']
-        assert_equal 'JSON', @controller.params['title']
+                key.revoke!
       end
     end
   end
-    
-    module ActionMailer
-  # The <tt>ActionMailer::DeliveryJob</tt> class is used when you
-  # want to send emails outside of the request-response cycle.
-  #
-  # Exceptions are rescued and handled by the mailer class.
-  class DeliveryJob < ActiveJob::Base # :nodoc:
-    queue_as { ActionMailer::Base.deliver_later_queue_name }
-    
-    module ActionMailer
-  # This module handles everything related to mail delivery, from registering
-  # new delivery methods to configuring the mail object to be sent.
-  module DeliveryMethods
-    extend ActiveSupport::Concern
-    
-          text.split.each do |word|
-        if sentences.first.present? && (sentences.last + [word]).join(' ').length > len
-          sentences << [word]
-        else
-          sentences.last << word
-        end
-      end
-    
-    module ActionMailer
-  class NonInferrableMailerError < ::StandardError
-    def initialize(name)
-      super 'Unable to determine the mailer to test from #{name}. ' \
-        'You'll need to specify it using tests YourMailer in your ' \
-        'test case definition'
-    end
-  end
-    
-          return true if prune == 'all'
-    
-      def xcode
-    if instance_variable_defined?(:@xcode)
-      @xcode
-    elsif MacOS::Xcode.installed?
-      @xcode = MacOS::Xcode.version
-      @xcode += ' => #{MacOS::Xcode.prefix}' unless MacOS::Xcode.default_prefix?
-      @xcode
-    end
-  end
-    
-      # Optional
-  #   Override this method if you need to group multiple agents based on an API key,
-  #   or server they connect to.
-  #   Have a look at the TwitterStreamAgent for an example.
-  def self.setup_worker; end
-    
-      included do
-    include Oauthable
-    
-      def destroy_all
-    Delayed::Job.where(locked_at: nil).delete_all
-    
-      def update
-    @user_credential = current_user.user_credentials.find(params[:id])
-    
-              self.confirmed_at = Time.now.utc
-    
-        def translation_scope
-      'devise.unlocks'
-    end
 end
 
     
-        def unlock_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :unlock_instructions, opts)
-    end
+            command = 'security set-keychain-settings'
+        command << ' -t #{params[:timeout]}' if params[:timeout]
+        command << ' -l' if params[:lock_when_sleeps]
+        command << ' -u' if params[:lock_after_timeout]
+        command << ' #{keychain_path}'
     
-          # Forgets the given resource by deleting a cookie
-      def forget_me(resource)
-        scope = Devise::Mapping.find_scope!(resource)
-        resource.forget_me!
-        cookies.delete(remember_key(resource, scope), forget_cookie_values(resource))
+            expect(result).to eq('/usr/local/bin/cloc  --by-file --xml  --out=build/cloc.xml')
       end
     
-                  define_method method do |resource_or_scope, *args|
-                scope = Devise::Mapping.find_scope!(resource_or_scope)
-                router_name = Devise.mappings[scope].router_name
-                context = router_name ? send(router_name) : _devise_route_context
-                context.send('#{action}#{scope}_#{module_name}_#{path_or_url}', *args)
-              end
-            end
-          end
+      def present(payload)
+    if payload.is_a?(Hash)
+      payload = ActiveSupport::HashWithIndifferentAccess.new(payload)
+      MAIN_KEYS.each do |key|
+        return { :title => payload[key].to_s, :entries => present_hash(payload, key) } if payload.has_key?(key)
+      end
+    
+      def tumblr
+    Tumblr.configure do |config|
+      config.consumer_key = tumblr_consumer_key
+      config.consumer_secret = tumblr_consumer_secret
+      config.oauth_token = tumblr_oauth_token
+      config.oauth_token_secret = tumblr_oauth_token_secret
+    end
+    
+    Tumblr::Client.new
+  end
+end
+    
+    begin
+  require 'bundler/setup'
+rescue LoadError
+  $stderr.puts '[*] Metasploit requires the Bundler gem to be installed'
+  $stderr.puts '    $ gem install bundler'
+  exit(1)
+end
+    
+          when :banner
+        # Because some ftp server send multiple banner we take only the first one and ignore the rest
+        if not (s[:info])
+          s[:info] = matches
+          report_service(s)
         end
-      end
     
-    # Each time a record is set we check whether its session has already timed out
-# or not, based on last request time. If so, the record is logged out and
-# redirected to the sign in page. Also, each time the request comes and the
-# record is set, we set the last request time inside its scoped session to
-# verify timeout in the following request.
-Warden::Manager.after_set_user do |record, warden, options|
-  scope = options[:scope]
-  env   = warden.request.env
     
-          def devise_mapping
-        @devise_mapping ||= Devise.mappings[scope_name]
-      end
+# replace calls, jmps, and read/write handle/filename references
+replaces = []
+asm.each_line { |ln|
+	if ln =~ /call /
+		parts = ln.split(' ')
+		if (parts[0] == 'call' and parts[2] == ';call')
+			old = parts[1]
+			func = parts[3]
+			new = addrs[func]
+			#puts '%32s: %s -> %x' % [func, old, new]
+			replaces << [func, old, new.to_s(16)]
+		end
+	end
+    }
     
-            # Registers a SIGINT handler. This typically is called from {busy}.
-        # Callbacks are only registered once, so calling this multiple times
-        # with the same callback has no consequence.
-        def register(sig_callback)
-          @@mutex.synchronize do
-            registered << sig_callback
-            registered.uniq!
+    signer._invoke('JarSignerMSF','[Ljava.lang.String;',jarsignerOpts)
     
-              # Verify the box exists that we want to repackage
-          box = @env.boxes.find(box_name, box_provider, '= #{box_version}')
-          if !box
-            raise Vagrant::Errors::BoxNotFoundWithProviderAndVersion,
-              name: box_name,
-              provider: box_provider.to_s,
-              version: box_version
-          end
-    
-          opts.on('-?', '-h', '--help', 'Show this help message.') do
-        puts opts
-        exit
-      end
-    
-        # Merges this query with another. The returned query queries for
-    # the intersection between the two inputs.
-    #
-    # Both queries should be resolved.
-    #
-    # @param other [Query]
-    # @return [Query?] The merged query, or nil if there is no intersection.
-    def merge(other)
-      m1, t1 = resolved_modifier.downcase, resolved_type.downcase
-      m2, t2 = other.resolved_modifier.downcase, other.resolved_type.downcase
-      t1 = t2 if t1.empty?
-      t2 = t1 if t2.empty?
-      if (m1 == 'not') ^ (m2 == 'not')
-        return if t1 == t2
-        type = m1 == 'not' ? t2 : t1
-        mod = m1 == 'not' ? m2 : m1
-      elsif m1 == 'not' && m2 == 'not'
-        # CSS has no way of representing 'neither screen nor print'
-        return unless t1 == t2
-        type = t1
-        mod = 'not'
-      elsif t1 != t2
-        return
-      else # t1 == t2, neither m1 nor m2 are 'not'
-        type = t1
-        mod = m1.empty? ? m2 : m1
-      end
-      Query.new([mod], [type], other.expressions + expressions)
-    end
+    	def block_end
+		# Insert the block size
+		self.block[-1][0] = block[-1][0].ljust(SIZE1)
+		self.block[-1][0] << '/*  '
+		self.block[-1][0] << '#{block_size} bytes'
+		self.block[-1][0] = block[-1][0].ljust(SIZE2)
+		self.block[-1][0] << '  */'
