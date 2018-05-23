b99047d39288fa9a1fc100652324e7b9c6fbd01a
SUNsung <weatherius@gmail.com>
2018-05-23 13:49:52 +0300

    Rewrite [386VqW3J45jbQ38NKlHIv6Q38638SYVGpi0mZ2k38bFe.rb] File in: 2018-05-23 13:49:52

--- ruby/386VqW3J45jbQ38NKlHIv6Q38638SYVGpi0mZ2k38bFe.rb
+++ ruby/386VqW3J45jbQ38NKlHIv6Q38638SYVGpi0mZ2k38bFe.rb
@@ -1,28 +1,88 @@
 
         
-        load './tasks/bower.rake'
+                class Listener < SubscriberMap
+          def initialize(adapter, event_loop)
+            super()
     
-          def handle_jwt_bearer(req)
-        jwt_string = req['client_assertion']
-        jwt = JSON::JWT.decode jwt_string, :skip_verification
-        o_auth_app = Api::OpenidConnect::OAuthApplication.find_by(client_id: jwt['iss'])
-        raise Rack::OAuth2::Server::Authorize::BadRequest(:invalid_request) unless o_auth_app
-        public_key = fetch_public_key(o_auth_app, jwt)
-        JSON::JWT.decode(jwt_string, JSON::JWK.new(public_key).to_key)
-        req.update_param('client_id', o_auth_app.client_id)
-        req.update_param('client_secret', o_auth_app.client_secret)
+            def test_url_host_db
+          spec = resolve 'abstract://foo/bar?encoding=utf8'
+          assert_equal({
+            'adapter'  => 'abstract',
+            'database' => 'bar',
+            'host'     => 'foo',
+            'encoding' => 'utf8' }, spec)
+        end
+    
+    class TestResponseTest < ActiveSupport::TestCase
+  def assert_response_code_range(range, predicate)
+    response = ActionDispatch::TestResponse.new
+    (0..599).each do |status|
+      response.status = status
+      assert_equal range.include?(status), response.send(predicate),
+                   'ActionDispatch::TestResponse.new(#{status}).#{predicate}'
+    end
+  end
+    
+    class AssertSelectEmailTest < ActionMailer::TestCase
+  class AssertSelectMailer < ActionMailer::Base
+    def test(html)
+      mail body: html, content_type: 'text/html',
+        subject: 'Test e-mail', from: 'test@test.host', to: 'test <test@test.host>'
+    end
+  end
+    
+        html_filters.push 'django/entries', 'sphinx/clean_html', 'django/clean_html'
+    text_filters.push 'django/fix_urls'
+    
+        options[:attribution] = <<-HTML
+      &copy; 2012 Plataformatec<br>
+      Licensed under the Apache License, Version 2.0.
+    HTML
+    
+        version '4' do
+      self.release = '4.5.0'
+      self.base_url = 'http://sinonjs.org/releases/v#{release}/'
+    end
+    
+        def parse(response)
+      unless response.url == root_url || self.class.version == 'Guide'
+        response.body.sub!(/<nav class='devsite-nav-responsive-sidebar.+?<\/nav>/m, '')
+        response.body.gsub!(/<li class='devsite-nav-item'>.+?<\/li>/m, '')
       end
     
-            # @param annotated_source [String] string passed to the matchers
-        #
-        # Separates annotation lines from source lines. Tracks the real
-        # source line number that each annotation corresponds to.
-        #
-        # @return [AnnotatedSource]
-        def self.parse(annotated_source)
-          source      = []
-          annotations = []
-    
-        expect(corrected).to eq(''something'.to_sym')
+          # If this importer is based on files on the local filesystem This method
+      # should return true if the file, when changed, should trigger a
+      # recompile.
+      #
+      # It is acceptable for non-sass files to be watched and trigger a recompile.
+      #
+      # @param filename [String] The absolute filename for a file that has changed.
+      # @return [Boolean] When the file changed should cause a recompile.
+      def watched_file?(filename)
+        false
+      end
+    end
   end
 end
+
+    
+      require 'sass/plugin/rack'
+  class Sass::Plugin::MerbBootLoader < Merb::BootLoader
+    after Merb::BootLoader::RackUpApplication
+    
+    module Capistrano
+  module Doctor
+    # Prints table of all Capistrano-related gems and their version numbers. If
+    # there is a newer version of a gem available, call attention to it.
+    class GemsDoctor
+      include Capistrano::Doctor::OutputHelpers
+    
+          def color
+        @color ||= SSHKit::Color.new($stdout)
+      end
+    end
+  end
+end
+
+    
+      mkdir_p tasks_dir
