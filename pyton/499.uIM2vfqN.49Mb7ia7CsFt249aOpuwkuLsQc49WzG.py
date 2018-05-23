bdc96a989a76d0623c4e94cf0f81f52567cb6b96
SUNsung <weatherius@gmail.com>
2018-05-23 16:52:07 +0300

    Rewrite [499.uIM2vfqN.49Mb7ia7CsFt249aOpuwkuLsQc49WzG.py] File in: 2018-05-23 16:52:06

--- pyton/499.uIM2vfqN.49Mb7ia7CsFt249aOpuwkuLsQc49WzG.py
+++ pyton/499.uIM2vfqN.49Mb7ia7CsFt249aOpuwkuLsQc49WzG.py
@@ -1,59 +1,103 @@
 
         
-        versions_info = json.load(open('update/versions.json'))
-if 'signature' in versions_info:
-    del versions_info['signature']
+            if not opts and not args:
+        # Display help.
+        print(_help)
+        # Enter GUI mode.
+        #from .gui import gui_main
+        #gui_main()
+    else:
+        conf = {}
+        for opt, arg in opts:
+            if opt in ('-h', '--help'):
+                # Display help.
+                print(_help)
     
-        # Get the version from youtube_dl/version.py without importing the package
-    exec(compile(open('youtube_dl/version.py').read(),
-                 'youtube_dl/version.py', 'exec'))
+    site_info = 'CBS.com'
+download = cbs_download
+download_playlist = playlist_not_supported('cbs')
+
     
-            # TODO: handle s and e stage_mode (live streams and ended live streams)
-        if stage_mode not in ('s', 'e'):
-            request = sanitized_Request(
-                'https://audimedia.tv/api/video/v1/videos/%s?embed[]=video_versions&embed[]=thumbnail_image&where[content_language_iso]=%s' % (video_id, lang),
-                headers={'X-Auth-Token': self._AUTH_TOKEN})
-            json_data = self._download_json(request, video_id)['results']
-            formats = []
+    	html = get_html(url)
+	contentid = r1(r'<meta name='contentid' scheme='DMINSTR2' content='([^']+)' />', html)
+	vid = r1(r''demand_ehow_videoid':'([^']+)'', html)
+	assert vid
     
-            info = page['info']
-    
-        styles = {
-        # No corresponding class for the following:
-        #Text:                     '', # class:  ''
-        Whitespace:                'underline #f8f8f8',      # class: 'w'
-        Error:                     '#a40000 border:#ef2929', # class: 'err'
-        Other:                     '#000000',                # class 'x'
-    }
-    
-        while True:
-        more_to_read = select.select([sock], [], [], timeout)[0]
-        if not more_to_read:
-            break
-    
-        return out
-    
-    Data structures that power Requests.
-'''
-    
-            errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
+    site_info = 'FC2Video'
+download = fc2video_download
+download_playlist = playlist_not_supported('fc2video')
+
     
     
-def variable_batch_size_comparison(data):
-    batch_sizes = [i.astype(int) for i in np.linspace(data.shape[0] // 10,
-                                                      data.shape[0], num=10)]
+download = huaban_download
+download_playlist = playlist_not_supported('huaban')
+
     
-        if revision is None:
-        return
-    if domain not in ('py', 'pyx'):
-        return
-    if not info.get('module') or not info.get('fullname'):
-        return
     
-        # TASK: print the cross-validated scores for the each parameters set
-    # explored by the grid search
     
-    '''
-# Author: Olivier Grisel <olivier.grisel@ensta.org>
-# License: Simplified BSD
+        stream_types = [
+        {'id': '720p HD'},
+        {'id': '360p SD'},
+    ]
+    
+                # log10(value) == exp, so log(value) == log10(value)/log10(e) ==
+            # exp/LOG10E
+            expected = exp / LOG10E
+            log = math.log(value)
+            self.assertAlmostEqual(log, expected)
+    
+        def test_create_entries(self):
+        self.dialog.top = self.root
+        self.dialog.row = 0
+        self.engine.setpat('hello')
+        self.dialog.create_entries()
+        self.assertIn(self.dialog.ent.get(), 'hello')
+    
+            User enters a name for the Help resource and a web url or file
+        name. The user can browse for the file.
+        '''
+        self.filepath = filepath
+        message = 'Name for item on Help menu:'
+        super().__init__(
+                parent, title, message, text0=menuitem,
+                used_names=used_names, _htest=_htest, _utest=_utest)
+    
+            # Handle comparison errors
+        d = deque(['a', 'b', BadCmp(), 'c'])
+        e = deque(d)
+        self.assertRaises(RuntimeError, d.remove, 'c')
+        for x, y in zip(d, e):
+            # verify that original order and values are retained.
+            self.assertTrue(x is y)
+    
+                    if not args:
+                    # args=NULL, nargs=0
+                    result = _testcapi.pyobject_fastcall(func, None)
+                    self.check_result(result, expected)
+    
+    try:
+    import zlib
+except ImportError:
+    zlib = None
+    
+            # unregister an unknown file obj
+        self.assertRaises(KeyError, s.unregister, 999999)
+    
+        except (AttributeError, OSError):
+        return default
+    output = proc.communicate()[0].decode('latin-1')
+    rc = proc.wait()
+    if not output or rc:
+        return default
+    else:
+        return output
+    
+        def test_exec_failure(self):
+        self.create_module('foo', '1/0')
+        try:
+            import foo
+        except ZeroDivisionError as e:
+            tb = e.__traceback__
+        else:
+            self.fail('ZeroDivisionError should have been raised')
+        self.assert_traceback(tb, [__file__, 'foo.py'])
