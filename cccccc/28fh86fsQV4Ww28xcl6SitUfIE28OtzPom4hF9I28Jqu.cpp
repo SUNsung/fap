00366cae5f6cc3875fa249fbd5787a27da0f4d2b
SUNsung <weatherius@gmail.com>
2018-05-23 12:48:57 +0300

    Rewrite [28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp] File in: 2018-05-23 12:48:57

--- cccccc/28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp
+++ cccccc/28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp
@@ -1,276 +1,66 @@
 
         
-        
-    {}  // namespace xla
+        /** Returns the benchmark Reporter instance.
+ *
+ * The returned instance will take care of generating reports for all the actual
+ * reporters configured via the 'enable_*_reporter' command line flags (see
+ * benchmark_config.cc). */
+std::shared_ptr<Reporter> GetReporter();
     
-    
-    { private:
-  enum { kBufferSize = 256 << 10 /* 256 kB */ };
-  const int skip_header_lines_;
-  Env* const env_;
-  int64 line_number_;
-  std::unique_ptr<RandomAccessFile> file_;  // must outlive input_buffer_
-  std::unique_ptr<io::InputBuffer> input_buffer_;
-};
-    
-      Status ReadLocked(string* key, string* value, bool* produced,
-                    bool* at_end) override {
-    *key = strings::StrCat(current_work(), ':', offset_);
-    Status status = reader_->ReadRecord(&offset_, value);
-    if (errors::IsOutOfRange(status)) {
-      *at_end = true;
-      return Status::OK();
+    namespace grpc {
+namespace testing {
     }
-    if (!status.ok()) return status;
-    *produced = true;
-    return Status::OK();
+    }
+    
+    std::vector<grpc::string_ref> SecureAuthContext::FindPropertyValues(
+    const grpc::string& name) const {
+  if (!ctx_) {
+    return std::vector<grpc::string_ref>();
   }
+  grpc_auth_property_iterator iter =
+      grpc_auth_context_find_properties_by_name(ctx_, name.c_str());
+  const grpc_auth_property* property = nullptr;
+  std::vector<grpc::string_ref> values;
+  while ((property = grpc_auth_property_iterator_next(&iter))) {
+    values.push_back(grpc::string_ref(property->value, property->value_length));
+  }
+  return values;
+}
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+    UsageTimer::Result UsageTimer::Sample() {
+  Result r;
+  r.wall = Now();
+  get_resource_usage(&r.user, &r.system);
+  r.total_cpu_time = 0;
+  r.idle_cpu_time = 0;
+  get_cpu_usage(&r.total_cpu_time, &r.idle_cpu_time);
+  return r;
+}
     
-    TEST(GrpcChannelTest, SparseHostPorts) {
-  GrpcChannelSpec spec;
-  TF_EXPECT_OK(
-      spec.AddHostPortsJob('mnist', {{0, 'a:1'}, {3, 'd:4'}, {4, 'e:5'}}));
-  ChannelCreationFunction channel_func =
-      ConvertToChannelCreationFunction(NewHostPortGrpcChannel);
-  std::unique_ptr<GrpcChannelCache> cc(NewGrpcChannelCache(spec, channel_func));
+      static double Now();
+    
+    int protoc_main(int argc, char* argv[]) {
+  google::protobuf::compiler::CommandLineInterface cli;
+  cli.AllowPlugins('protoc-');
     }
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    Status AsinhGrad(const AttrSlice& attrs, FunctionDef* g) {
-  // clang-format off
-  return GradForUnaryCwise(g, {
-      {{'y'}, 'Asinh', {'x'}},
-      {{'cosh'}, 'Cosh', {'y'}},
-      {{'dx'}, 'Mul', {'dy', 'cosh'}},  // dy * cosh(y)
-  });
-  // clang-format on
-}
-REGISTER_OP_GRADIENT('Asinh', AsinhGrad);
-    
-    
-    {    cvReleaseMat( &(*state)->preFilteredImg0 );
-    cvReleaseMat( &(*state)->preFilteredImg1 );
-    cvReleaseMat( &(*state)->slidingSumBuf );
-    cvReleaseMat( &(*state)->disp );
-    cvReleaseMat( &(*state)->cost );
-    cvFree( state );
+    bool WakeUpLock::IsLocking() {
+    return ::wakeupLock_IsLocking(object_);
 }
     
+    struct ServiceRegister;
     
-    {}
-#endif //0
+        void* This() const {return m_this;}
+    
+    class Test_Spy_Sample {
+  public:
+    Test_Spy_Sample();
+    ~Test_Spy_Sample();
+    }
+    
+    #include 'comm/debugger/testspy.h'
+    
+    #endif
 
     
-            // Core Extension: ARB_map_buffer_range
-        MAP_READ_BIT                     = 0x0001,
-        MAP_WRITE_BIT                    = 0x0002,
-        MAP_INVALIDATE_RANGE_BIT         = 0x0004,
-        MAP_INVALIDATE_BUFFER_BIT        = 0x0008,
-        MAP_FLUSH_EXPLICIT_BIT           = 0x0010,
-        MAP_UNSYNCHRONIZED_BIT           = 0x0020,
-    
-      string model_file   = argv[1];
-  string trained_file = argv[2];
-  string mean_file    = argv[3];
-  string label_file   = argv[4];
-  Classifier classifier(model_file, trained_file, mean_file, label_file);
-    
-     protected:
-   /**
-   * @brief Generates a random integer from Uniform({0, 1, ..., n-1}).
-   *
-   * @param n
-   *    The upperbound (exclusive) value of the random number.
-   * @return
-   *    A uniformly random integer value from ({0, 1, ..., n-1}).
-   */
-  virtual int Rand(int n);
-    
-    /**
- * @brief Compute the index of the @f$ K @f$ max values for each datum across
- *        all dimensions @f$ (C \times H \times W) @f$.
- *
- * Intended for use after a classification layer to produce a prediction.
- * If parameter out_max_val is set to true, output is a vector of pairs
- * (max_ind, max_val) for each image. The axis parameter specifies an axis
- * along which to maximise.
- *
- * NOTE: does not implement Backwards operation.
- */
-template <typename Dtype>
-class ArgMaxLayer : public Layer<Dtype> {
- public:
-  /**
-   * @param param provides ArgMaxParameter argmax_param,
-   *     with ArgMaxLayer options:
-   *   - top_k (\b optional uint, default 1).
-   *     the number @f$ K @f$ of maximal items to output.
-   *   - out_max_val (\b optional bool, default false).
-   *     if set, output a vector of pairs (max_ind, max_val) unless axis is set then
-   *     output max_val along the specified axis.
-   *   - axis (\b optional int).
-   *     if set, maximise along the specified axis else maximise the flattened
-   *     trailing dimensions for each index of the first / num dimension.
-   */
-  explicit ArgMaxLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-      virtual inline int ExactNumBottomBlobs() const { return 3; }
-  virtual inline const char* type() const { return 'ContrastiveLoss'; }
-  /**
-   * Unlike most loss layers, in the ContrastiveLossLayer we can backpropagate
-   * to the first two inputs.
-   */
-  virtual inline bool AllowForceBackward(const int bottom_index) const {
-    return bottom_index != 2;
-  }
-    
-    
-    {}  // namespace caffe
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    /**
- * @brief Compute elementwise operations, such as product and sum,
- *        along multiple input Blobs.
- *
- * TODO(dox): thorough documentation for Forward, Backward, and proto params.
- */
-template <typename Dtype>
-class EltwiseLayer : public Layer<Dtype> {
- public:
-  explicit EltwiseLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    #include <vector>
-    
-     protected:
-  /**
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the computed outputs @f$
-   *        y = \gamma ^ {\alpha x + \beta}
-   *      @f$
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-      /**
-   * @brief Computes the error gradient w.r.t. the forwarded inputs.
-   *
-   * @param top output Blob vector (length 1+), providing the error gradient with
-   *        respect to the outputs
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2+), into which the top error
-   *        gradient is copied
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    // Tests that a unicharset that contains double-letter ligatures (eg ff) has
-// no null char in the encoding at all.
-TEST_F(UnicharcompressTest, DoesLigaturesWithDoubles) {
-  LOG(INFO) << 'Testing por with ligatures';
-  LoadUnicharset('por.unicharset');
-  ExpectCorrect('por');
-  // Check that any unichar-id that is encoded with multiple codes has the
-  // correct encoded_nulll_char_ in between.
-  for (int u = 0; u <= unicharset_.size(); ++u) {
-    RecodedCharID code;
-    int len = compressed_.EncodeUnichar(u, &code);
-    if (len > 1) {
-      // The should not be any null char in the code.
-      for (int i = 0; i < len; ++i) {
-        EXPECT_NE(encoded_null_char_, code(i));
-      }
-    }
-  }
-}
-    
-      // Updates the weights using the given learning rate, momentum and adam_beta.
-  // num_samples is used in the adam computation iff use_adam_ is true.
-  void Update(float learning_rate, float momentum, float adam_beta,
-              int num_samples) override;
-  // Sums the products of weight updates in *this and other, splitting into
-  // positive (same direction) in *same and negative (different direction) in
-  // *changed.
-  void CountAlternators(const Network& other, double* same,
-                        double* changed) const override;
-    
-    
-    {}  // namespace tesseract.
-    
-      // Check the density for a seed candidate (part) using its math density and
-  // italic density, returns true if the check passed.
-  bool CheckSeedDensity(const float math_density_high,
-                        const float math_density_low,
-                        const ColPartition* part) const;
-    
-    class TESS_API PageIterator {
- public:
-  /**
-   * page_res and tesseract come directly from the BaseAPI.
-   * The rectangle parameters are copied indirectly from the Thresholder,
-   * via the BaseAPI. They represent the coordinates of some rectangle in an
-   * original image (in top-left-origin coordinates) and therefore the top-left
-   * needs to be added to any output boxes in order to specify coordinates
-   * in the original image. See TessBaseAPI::SetRectangle.
-   * The scale and scaled_yres are in case the Thresholder scaled the image
-   * rectangle prior to thresholding. Any coordinates in tesseract's image
-   * must be divided by scale before adding (rect_left, rect_top).
-   * The scaled_yres indicates the effective resolution of the binary image
-   * that tesseract has been given by the Thresholder.
-   * After the constructor, Begin has already been called.
-   */
-  PageIterator(PAGE_RES* page_res, Tesseract* tesseract,
-               int scale, int scaled_yres,
-               int rect_left, int rect_top,
-               int rect_width, int rect_height);
-  virtual ~PageIterator();
-    }
-    
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
-    
-      private:
-    void __StartupCreater();
-    void __FirstGetCreater(const std::string& _servicename);
-    void __Creater(std::vector<ServiceRegister>& _vec);
-    
-    // Licensed under the MIT License (the 'License'); you may not use this file except in 
-// compliance with the License. You may obtain a copy of the License at
-// http://opensource.org/licenses/MIT
+    #endif
