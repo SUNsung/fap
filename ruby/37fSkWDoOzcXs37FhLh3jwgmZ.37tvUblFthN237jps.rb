4ead9431fd2d4b19e0a44f53f168b97a3a8cfcb9
SUNsung <weatherius@gmail.com>
2018-05-23 20:11:26 +0300

    Rewrite [37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb] File in: 2018-05-23 20:11:25

--- ruby/37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb
+++ ruby/37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb
@@ -1,42 +1,83 @@
 
         
-                assert_equal(a, b)
-        assert_not_same(a, b)
-        assert_equal(a.frozen?, b.frozen?)
-        assert_equal(a.tainted?, b.tainted?)
+            find_union(segments, Project).includes(:namespace).order_id_desc
+  end
+    
+          def mailer_sender(mapping, sender = :from)
+        default_sender = default_params[sender]
+        if default_sender.present?
+          default_sender.respond_to?(:to_proc) ? instance_eval(&default_sender) : default_sender
+        elsif Devise.mailer_sender.is_a?(Proc)
+          Devise.mailer_sender.call(mapping.name)
+        else
+          Devise.mailer_sender
+        end
+      end
+    
+        def default_used_route(options)
+      singularizer = lambda { |s| s.to_s.singularize.to_sym }
+    
+            if base_url.path == '/spec/'
+          index = css('.subnav li a').to_a.index(at_css('.subnav li a[href='#{result[:path]}']')) + 1
+          name.prepend '#{index}. '
+        end
+    
+        version '2' do
+      self.release = '2.3.0'
+      self.base_url = 'http://coffeescript.org/'
+    
+          options[:only_patterns] = [/\Ad3[\-\w]+\z/, /\Ad3\/blob\/master\/changes\.md\z/i]
+      options[:skip_patterns] = [/3\.x-api-reference/]
+    
+        options[:attribution] = <<-HTML
+      &copy; Joyent, Inc. and other Node contributors<br>
+      Licensed under the MIT License.<br>
+      Node.js is a trademark of Joyent, Inc. and is used with its permission.<br>
+      We are not endorsed by or affiliated with Joyent.
+    HTML
+    
+        def replace(index, name)
+      @filters[assert_index(index)] = filter_const(name)
+    end
+    
+      attributes :id, :type, :name, :updated
+    
+        grouped_codes = codes.reduce([]) do |agg, current|
+      if current[1]
+        agg << [current[0]]
+      else
+        agg.last << current[0]
+        agg
       end
     end
-  end
     
-      it 'decodes past whitespace bytes when passed the '*' modifier' do
-    [ ['a b c',    ['a b c']],
-      ['a\fb c',   ['a\fb c']],
-      ['a\nb c',   ['a\nb c']],
-      ['a\rb c',   ['a\rb c']],
-      ['a\tb c',   ['a\tb c']],
-      ['a\vb c',   ['a\vb c']],
-    ].should be_computed_by(:unpack, unpack_format('*'))
-  end
-end
-    
-      it 'does not decode any items for directives exceeding the input string size' do
-    '\xc2\x80'.unpack('UUUU').should == [0x80]
-  end
-    
-      Car = Struct.new(:make, :model, :year)
-    
-      def self.wakeup_dying_sleeping_thread(kill_method_name=:kill)
-    t = ThreadSpecs.dying_thread_ensures(kill_method_name) { yield }
-    Thread.pass while t.status and t.status != 'sleep'
-    t.wakeup
-    t.join
-  end
-    
-            time.sec.should == 60
-        time.min.should == 59
-        time.hour.should == 23
-        time.day.should == 30
-        time.month.should == 6
+        def parse_symbol_spec(spec, tags)
+      case spec
+      when :clt
+        odisabled ''depends_on :clt''
+      when :tex
+        odisabled ''depends_on :tex''
+      when :libltdl
+        output_disabled(spec, 'libtool')
+      when :apr
+        output_disabled(spec, 'apr-util')
+      when :fortran
+        output_disabled(spec, 'gcc')
+      when :gpg
+        output_disabled(spec, 'gnupg')
+      when :hg
+        output_disabled(spec, 'mercurial')
+      when :mpi
+        output_disabled(spec, 'open-mpi')
+      when :python, :python2
+        output_disabled(spec, 'python@2')
+      when :python3
+        output_disabled(spec, 'python')
+      when :ant, :autoconf, :automake, :bsdmake, :cairo, :emacs, :expat,
+           :fontconfig, :freetype, :libtool, :libpng, :mysql, :perl, :pixman,
+           :postgresql, :rbenv, :ruby
+        output_disabled(spec)
+      else
+        super
       end
     end
-  end
