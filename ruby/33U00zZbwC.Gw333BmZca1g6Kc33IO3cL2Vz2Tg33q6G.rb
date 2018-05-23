a512e220c0a417a7e4d09fc68318eadd5a5b8dfc
SUNsung <weatherius@gmail.com>
2018-05-23 11:11:19 +0300

    Rewrite [33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb] File in: 2018-05-23 11:11:18

--- ruby/33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb
+++ ruby/33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb
@@ -1,113 +1,134 @@
 
         
-        module Homebrew
-  def build_env_keys(env)
-    %w[
-      CC CXX LD OBJC OBJCXX
-      HOMEBREW_CC HOMEBREW_CXX
-      CFLAGS CXXFLAGS CPPFLAGS LDFLAGS SDKROOT MAKEFLAGS
-      CMAKE_PREFIX_PATH CMAKE_INCLUDE_PATH CMAKE_LIBRARY_PATH CMAKE_FRAMEWORK_PATH
-      MACOSX_DEPLOYMENT_TARGET PKG_CONFIG_PATH PKG_CONFIG_LIBDIR
-      HOMEBREW_DEBUG HOMEBREW_MAKE_JOBS HOMEBREW_VERBOSE
-      HOMEBREW_SVN HOMEBREW_GIT
-      HOMEBREW_SDKROOT HOMEBREW_BUILD_FROM_SOURCE
-      MAKE GIT CPP
-      ACLOCAL_PATH PATH CPATH].select { |key| env.key?(key) }
-  end
-    
-        def self.disk_cleanup_size
-      @@disk_cleanup_size
-    end
-    
-        raise SEARCH_ERROR_QUEUE.pop unless SEARCH_ERROR_QUEUE.empty?
-  end
-    
-      def process_bootstrap
-    log_status 'Convert Bootstrap LESS to Sass'
-    puts ' repo   : #@repo_url'
-    puts ' branch : #@branch_sha #@repo_url/tree/#@branch'
-    puts ' save to: #{@save_to.to_json}'
-    puts ' twbs cache: #{@cache_path}'
-    puts '-' * 60
-    
-      # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
-    
-      def participation_count
-    poll_answers.sum('vote_count')
-  end
-end
-
-    
-    module Admin
-  class PodsController < AdminController
-    respond_to :html, :json, :mobile
-    
-            def multiple_assignment?
-          return false unless meta_assignment_node
-          meta_assignment_node.type == MULTIPLE_ASSIGNMENT_TYPE
-        end
-    
-                unless assignment.branch.may_run_incompletely?
-              consumed_branches << assignment.branch
-            end
-          end
-        end
-    
-              lambda do |corrector|
-            corrector.replace(center.source_range, new_center)
-          end
-        end
+            keys.find do |key|
+      if key.to_s.end_with?('_or_later')
+        later_tag = key.to_s[/(\w+)_or_later$/, 1].to_sym
+        MacOS::Version.from_symbol(later_tag) <= tag_version
       end
     end
   end
 end
 
     
-            def on_block(node)
-          on_body_of_reduce(node) do |body|
-            void_next = body.each_node(:next).find do |n|
-              n.children.empty? && parent_block_node(n) == node
-            end
+      def post_superenv_hacks
+    # Only allow Homebrew-approved directories into the PATH, unless
+    # a formula opts-in to allowing the user's path.
+    if formula.env.userpaths? || reqs.any? { |rq| rq.env.userpaths? }
+      ENV.userpaths!
+    end
+  end
     
-            def variables_in_node(node)
-          if node.or_type?
-            node.node_parts
-                .flat_map { |node_part| variables_in_node(node_part) }
-                .uniq
-          else
-            variables_in_simple_node(node)
-          end
-        end
+      def describe_system_ruby
+    s = ''
+    case RUBY_VERSION
+    when /^1\.[89]/, /^2\.0/
+      s << '#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}'
+    else
+      s << RUBY_VERSION
+    end
     
-            end
+          GitHub.open 'https://api.github.com/repos/#{user}/homebrew-#{repo}/git/trees/HEAD?recursive=1' do |json|
+        json['tree'].each do |object|
+          next unless object['type'] == 'blob'
+    
+      def python(_options = {}, &block)
+    opoo 'Formula#python is deprecated and will go away shortly.'
+    block.call if block_given?
+    PythonRequirement.new
+  end
+  alias_method :python2, :python
+  alias_method :python3, :python
+end
+
+    
+      def failure
+    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
+    redirect_to after_omniauth_failure_path_for(resource_name)
+  end
+    
+      protected
+    def after_resetting_password_path_for(resource)
+      Devise.sign_in_after_reset_password ? after_sign_in_path_for(resource) : new_session_path(resource_name)
+    end
+    
+          def mailer_from(mapping)
+        mailer_sender(mapping, :from)
+      end
+    
+          raise 'Could not find a valid mapping for #{obj.inspect}'
+    end
+    
+      def owner=(owner)
+    resource.owner   = owner
+    resource.version = resource.checksum || ERB::Util.url_encode(resource.url)
+  end
+    
+        if !pinned.empty? && !ARGV.include?('--ignore-pinned')
+      ofail 'Not upgrading #{Formatter.pluralize(pinned.length, 'pinned package')}:'
+      puts pinned.map { |f| '#{f.full_specified_name} #{f.pkg_version}' } * ', '
+    end
+    
+        def yielder
+      if instance_variable_defined?(:@satisfied)
+        @satisfied
+      elsif @options[:build_env]
+        require 'extend/ENV'
+        ENV.with_build_environment { yield @proc }
+      else
+        yield @proc
       end
     end
+  end
+    
+          def remove_bad_cookies(request, response)
+        return if bad_cookies.empty?
+        paths = cookie_paths(request.path)
+        bad_cookies.each do |name|
+          paths.each { |path| response.set_cookie name, empty_cookie(request.host, path) }
+        end
+      end
+    
+        it 'copes with nested arrays' do
+      mock_app do |env|
+        request = Rack::Request.new(env)
+        [200, {'Content-Type' => 'text/plain'}, [request.params['foo']['bar']]]
+      end
+      get '/', :foo => {:bar => '<bar>'}
+      expect(body).to eq('&lt;bar&gt;')
+    end
+    
+      it 'should allow changing the protection mode' do
+    # I have no clue what other modes are available
+    mock_app do
+      use Rack::Protection::FrameOptions, :frame_options => :deny
+      run DummyApp
+    end
+    
+      %w(GET HEAD).each do |method|
+    it 'accepts #{method} requests with non-whitelisted Origin' do
+      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://malicious.com')).to be_ok
+    end
+  end
+    
+      it 'accepts a session without changes to tracked parameters' do
+    session = {:foo => :bar}
+    get '/', {}, 'rack.session' => session
+    get '/', {}, 'rack.session' => session
+    expect(session[:foo]).to eq(:bar)
+  end
+    
+        expect(get('/')).to be_ok
   end
 end
 
     
-      context 'called with three widths' do
-    it 'applies second width to left and right' do
-      rule = 'border-width: 4px 5px 6px'
-    
-      context 'called with null values' do
-    it 'writes rules for other three' do
-      ruleset = 'margin-top: 11px; ' +
-                'margin-right: 12px; ' +
-                'margin-left: 13px;'
-      bad_rule = 'margin-bottom: null;'
-    
-      context 'called with one size' do
-    it 'applies same width to all sides' do
-      ruleset = 'position: fixed; ' +
-                'top: 1em; ' +
-                'right: 1em; ' +
-                'bottom: 1em; ' +
-                'left: 1em;'
-    
-          expect('.prefix--webkit').to have_ruleset(rule)
+      def _nt_not_in_operator
+    start_index = index
+    if node_cache[:not_in_operator].has_key?(index)
+      cached = node_cache[:not_in_operator][index]
+      if cached
+        cached = SyntaxNode.new(input, index...(index + 1)) if cached == true
+        @index = cached.interval.end
+      end
+      return cached
     end
-  end
