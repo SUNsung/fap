3277b1dcf2985fbf8a3e87aa809cc41b7418cb4a
SUNsung <weatherius@gmail.com>
2018-05-24 03:37:30 +0300

    Rewrite [29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb] File in: 2018-05-24 03:37:29

--- ruby/29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb
+++ ruby/29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb
@@ -1,40 +1,50 @@
 
         
-          def index
-    if params[:agent_id]
-      @agent = current_user.agents.find(params[:agent_id])
-      @events = @agent.events.page(params[:page])
-    else
-      @events = current_user.events.preload(:agent).page(params[:page])
-    end
+            # @include and @extend from LESS:
+    #  .mixin()             -> @include mixin()
+    #  #scope > .mixin()    -> @include scope-mixin()
+    #  &:extend(.mixin all) -> @include mixin()
+    def replace_mixins(less, mixin_names)
+      mixin_pattern = /(?<=^|\s)((?:[#|\.][\w-]+\s*>\s*)*)\.([\w-]+)\((.*)\)(?!\s\{)/
+    }
     
-            doc
+        def log_http_get_file(url, cached = false)
+      s = '  #{'CACHED ' if cached}GET #{url}...'
+      if cached
+        puts dark green s
+      else
+        puts dark cyan s
       end
     end
+    
+      # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
+    
+    desc 'Compile bootstrap-sass to tmp/ (or first arg)'
+task :compile, :css_path do |t, args|
+  require 'sass'
+  require 'term/ansicolor'
+    
+        it 'Reads referrer from Host header when Referer header is relative' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
+      expect(subject.referrer(env)).to eq('foo.com')
+    end
+    
+      it 'should not override the header if already set' do
+    mock_app with_headers('X-Frame-Options' => 'allow')
+    expect(get('/', {}, 'wants' => 'text/html').headers['X-Frame-Options']).to eq('allow')
   end
 end
 
     
-          def get_type
-        case slug
-        when 'api'
-          'Reference'
-        when 'configuration'
-          'Reference: Configuration'
-        when 'stpl'
-          'Reference: SimpleTemplate'
-        when 'plugindev'
-          'Reference: Plugin'
-        else
-          'Manual'
-        end
-      end
+      # These two settings work together to allow you to limit a spec run
+  # to individual examples or groups you care about by tagging them with
+  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
+  # get run.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
     
-            def log_level?(level, min_level)
-          log_levels[level] >= log_levels[min_level]
-        end
-    
-            Readline::HISTORY << text
-        parse_input(environment, text)
       end
-    end
+end
