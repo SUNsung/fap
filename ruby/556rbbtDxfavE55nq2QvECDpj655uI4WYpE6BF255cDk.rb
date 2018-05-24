1f95061776fca6e54ef7dd84879956a15d800b5e
SUNsung <weatherius@gmail.com>
2018-05-25 00:02:29 +0300

    Rewrite [556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb] File in: 2018-05-25 00:02:29

--- ruby/556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb
+++ ruby/556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb
@@ -1,115 +1,120 @@
 
         
-            it 'should have a way of getting the service configurations' do
-      configs = key.service_configs_for(Spaceship::Portal::Key::MUSIC_KIT_ID)
-      expect(configs).to be_instance_of(Array)
-      expect(configs.sample).to be_instance_of(Hash)
-      expect(configs.first['identifier']).to eq('music.com.snatchev.test')
+              it 'does set the exclude directories' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            cloc(exclude_dir: 'test1,test2,build')
+          end').runner.execute(:test)
+    
+          it 'properly removes new lines of the build number' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          increment_build_number(build_number: '24\n', xcodeproj: '.xcproject')
+        end').runner.execute(:test)
+    
+        options[:attribution] = <<-HTML
+      &copy; Joyent, Inc. and other Node contributors<br>
+      Licensed under the MIT License.<br>
+      Node.js is a trademark of Joyent, Inc. and is used with its permission.<br>
+      We are not endorsed by or affiliated with Joyent.
+    HTML
+    
+        options[:fix_urls] = ->(url) do
+      url.sub! %r{(#{Rust.base_url}.+/)\z}, '\1index.html'
+      url.sub! '/unicode/u_str', '/unicode/str/'
+      url
     end
     
-        context 'Mercurial repository' do
-      before do
-        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_svn?).and_return(false)
-        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_git_svn?).and_return(false)
-        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_git?).and_return(false)
-        expect(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_hg?).and_return(true)
-      end
+        def as_json
+      @pages
+    end
     
-    class NodeMincerTest < Minitest::Test
-  DUMMY_PATH = 'test/dummy_node_mincer'
-    
-            def signal_end_of_output
-          output_pipe.puts(END_OF_OUTPUT_SIGNAL)
-          STDOUT.flush
-        end
-    
-      def send_sinatra_file(path, &missing_file_block)
-    file_path = File.join(File.dirname(__FILE__), 'public',  path)
-    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
-    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
+      def test_truncate_rbuf
+    Tempfile.create('test-truncate') {|f|
+      f.puts 'abc'
+      f.puts 'def'
+      f.rewind
+      assert_equal('abc\n', f.gets)
+      f.truncate(3)
+      assert_equal(nil, f.gets, '[ruby-dev:24197]')
+    }
   end
     
-          rtn = ''
-      (context.environments.first['site'][@array_name] || []).each do |file|
-        if file !~ /^[a-zA-Z0-9_\/\.-]+$/ || file =~ /\.\// || file =~ /\/\./
-          rtn = rtn + 'Include file '#{file}' contains invalid characters or sequences'
-        end
+        set = Set['1',nil,'2',nil,'0','1',false]
+    assert_equal(true, set.include?(nil))
+    assert_equal(true, set.include?(false))
+    assert_equal(true, set.include?('1'))
+    assert_equal(false, set.include?(0))
+    assert_equal(false, set.include?(true))
+  end
     
-        def render(context)
-      code_dir = (context.registers[:site].config['code_dir'].sub(/^\//,'') || 'downloads/code')
-      code_path = (Pathname.new(context.registers[:site].source) + code_dir).expand_path
-      file = code_path + @file
+    describe :string_unpack_unicode, shared: true do
+  it 'decodes Unicode codepoints as ASCII values' do
+    [ ['\x00',      [0]],
+      ['\x01',      [1]],
+      ['\x08',      [8]],
+      ['\x0f',      [15]],
+      ['\x18',      [24]],
+      ['\x1f',      [31]],
+      ['\x7f',      [127]],
+      ['\xc2\x80',  [128]],
+      ['\xc2\x81',  [129]],
+      ['\xc3\xbf',  [255]]
+    ].should be_computed_by(:unpack, 'U')
+  end
     
-              if @address.update_attributes(address_params)
-            respond_with(@address, default_template: :show)
-          else
-            invalid_resource!(@address)
-          end
-        end
-    
-            def create
-          authorize! :create, StockLocation
-          @stock_location = StockLocation.new(stock_location_params)
-          if @stock_location.save
-            respond_with(@stock_location, status: 201, default_template: :show)
-          else
-            invalid_resource!(@stock_location)
-          end
-        end
-    
-            def index
-          authorize! :read, StockMovement
-          @stock_movements = scope.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
-          respond_with(@stock_movements)
-        end
-    
-    When /^(?:|I )go to (.+)$/ do |page_name|
-  visit path_to(page_name)
-end
-    
-        def only_process
-      only_process = @options[:only_process].dup
-      only_process = only_process.call(self) if only_process.respond_to?(:call)
-      only_process.map(&:to_sym)
-    end
-    
-      # Defines the geometry of an image.
-  class Geometry
-    attr_accessor :height, :width, :modifier
-    
-        # Perform the actual interpolation. Takes the pattern to interpolate
-    # and the arguments to pass, which are the attachment and style name.
-    # You can pass a method name on your record as a symbol, which should turn
-    # an interpolation pattern for Paperclip to use.
-    def self.interpolate pattern, *args
-      pattern = args.first.instance.send(pattern) if pattern.kind_of? Symbol
-      result = pattern.dup
-      interpolators_cache.each do |method, token|
-        result.gsub!(token) { send(method, *args) } if result.include?(token)
-      end
-      result
-    end
-    
-            def type_allowed?(type)
-          @subject.send('#{@attachment_name}_content_type=', type)
-          @subject.valid?
-          @subject.errors[:'#{@attachment_name}_content_type'].blank?
-        end
-    
-        Hash.new.tap do |missing_styles|
-      current_styles.each do |klass, attachment_definitions|
-        attachment_definitions.each do |attachment_name, styles|
-          registered = registered_styles[klass][attachment_name] || [] rescue []
-          missed = styles - registered
-          if missed.present?
-            klass_sym = klass.to_s.to_sym
-            missing_styles[klass_sym] ||= Hash.new
-            missing_styles[klass_sym][attachment_name.to_sym] ||= Array.new
-            missing_styles[klass_sym][attachment_name.to_sym].concat(missed.to_a)
-            missing_styles[klass_sym][attachment_name.to_sym].map!(&:to_s).sort!.map!(&:to_sym).uniq!
-          end
-        end
+        respond_to do |format|
+      if @book.save
+        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        format.json { render :show, status: :created, location: @book }
+      else
+        format.html { render :new }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
-end
+    
+            # @param annotated_source [String] string passed to the matchers
+        #
+        # Separates annotation lines from source lines. Tracks the real
+        # source line number that each annotation corresponds to.
+        #
+        # @return [AnnotatedSource]
+        def self.parse(annotated_source)
+          source      = []
+          annotations = []
+    
+                  include Something
+              # Comment
+              def do_something; end
+    
+        execute 'ensure-sidekiq-is-setup-with-monit' do 
+      command %Q{ 
+        monit reload 
+      } 
+    end
+    
+          def create_worker_spec
+        template_file = File.join(
+            'spec/workers',
+            class_path,
+            '#{file_name}_worker_spec.rb'
+        )
+        template 'worker_spec.rb.erb', template_file
+      end
+    
+        module ActionMailer
+      def sidekiq_delay(options={})
+        Proxy.new(DelayedMailer, self, options)
+      end
+      def sidekiq_delay_for(interval, options={})
+        Proxy.new(DelayedMailer, self, options.merge('at' => Time.now.to_f + interval.to_f))
+      end
+      def sidekiq_delay_until(timestamp, options={})
+        Proxy.new(DelayedMailer, self, options.merge('at' => timestamp.to_f))
+      end
+      alias_method :delay, :sidekiq_delay
+      alias_method :delay_for, :sidekiq_delay_for
+      alias_method :delay_until, :sidekiq_delay_until
+    end
+    
+      # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
