ecc20831c468bcd6193d7dfd81770d2362bc0282
SUNsung <weatherius@gmail.com>
2018-05-24 09:49:05 +0300

    Rewrite [58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py] File in: 2018-05-24 09:49:05

--- pyton/58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py
+++ pyton/58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py
@@ -1,50 +1,57 @@
 
         
-        import os
+            def add_options(self, parser):
+        '''
+        Populate option parse with options available for this command
+        '''
+        group = OptionGroup(parser, 'Global Options')
+        group.add_option('--logfile', metavar='FILE',
+            help='log file. if omitted stderr will be used')
+        group.add_option('-L', '--loglevel', metavar='LEVEL', default=None,
+            help='log level (default: %s)' % self.settings['LOG_LEVEL'])
+        group.add_option('--nolog', action='store_true',
+            help='disable logging completely')
+        group.add_option('--profile', metavar='FILE', default=None,
+            help='write python cProfile stats to FILE')
+        group.add_option('--pidfile', metavar='FILE',
+            help='write process ID to FILE')
+        group.add_option('-s', '--set', action='append', default=[], metavar='NAME=VALUE',
+            help='set/override setting (may be repeated)')
+        group.add_option('--pdb', action='store_true', help='enable pdb on failure')
     
-        host = None
-    for matching in ['ipv4', 'ipv6', 'hostname']:
-        m = patterns[matching].match(address)
-        if m:
-            host = address
-            continue
-    
-            # global, resource, entity
-        input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/global/urlMaps/my-url-map'
-        actual = GCPUtils.parse_gcp_url(input_url)
-        self.assertEquals('myproject', actual['project'])
-        self.assertTrue('global' in actual)
-        self.assertTrue(actual['global'])
-        self.assertEquals('v1', actual['api_version'])
-        self.assertEquals('compute', actual['service'])
-    
-    import re
-    
-        classifiers = proj_info['classifiers'],
-    
-    from .theplatform import theplatform_download_by_pid
-    
-            mp3 = match1(content, r'name='filename'\s*value='([^']+\.mp3)'')
-        if mp3: mp3 = 'http://res.infoq.com/downloads/mp3downloads/%s' % mp3
-    
-    def kuwo_download_by_rid(rid, output_dir = '.', merge = True, info_only = False):
-    html=get_content('http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_%s'%rid)
-    title=match1(html,r'<name>(.*)</name>')
-    #to get title
-    #format =aac|mp3 ->to get aac format=mp3 ->to get mp3
-    url=get_content('http://antiserver.kuwo.cn/anti.s?format=mp3&rid=MUSIC_%s&type=convert_url&response=url'%rid)
-    songtype, ext, size = url_info(url)
-    print_info(site_info, title, songtype, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir)
-    
-    from .tudou import tudou_download_by_id
-from .youku import youku_download_by_vid
-from xml.dom.minidom import parseString
-    
-        def prepare(self, **kwargs):
-        # scrape the html
-        content = get_content(self.url)
+        def run(self, args, opts):
+        with _BenchServer():
+            self.crawler_process.crawl(_BenchSpider, total=100000)
+            self.crawler_process.start()
     
     
-if __name__ == '__main__':
+class TextTestResult(_TextTestResult):
+    def printSummary(self, start, stop):
+        write = self.stream.write
+        writeln = self.stream.writeln
+    
+        def _find_template(self, template):
+        template_file = join(self.templates_dir, '%s.tmpl' % template)
+        if exists(template_file):
+            return template_file
+        print('Unable to find template: %s\n' % template)
+        print('Use 'scrapy genspider --list' to see all available templates.')
+    
+        def _adjust_process_count(self):
+        for _ in range(len(self._processes), self._max_workers):
+            p = multiprocessing.Process(
+                    target=_process_worker,
+                    args=(self._call_queue,
+                          self._result_queue))
+            p.start()
+            self._processes.add(p)
+    
+    
+class FutureTests(unittest.TestCase):
+    def test_done_callback_with_result(self):
+        callback_result = [None]
+        def fn(callback_future):
+            callback_result[0] = callback_future.result()
+    
+    from ycm.tests.test_utils import MockVimModule
+MockVimModule()
