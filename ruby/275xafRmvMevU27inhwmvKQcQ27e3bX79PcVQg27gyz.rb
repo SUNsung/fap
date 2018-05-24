43fdda7062c7f3cc1ab6aea909ef530fd86e3664
SUNsung <weatherius@gmail.com>
2018-05-24 22:07:49 +0300

    Rewrite [275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb] File in: 2018-05-24 22:07:49

--- ruby/275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb
+++ ruby/275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb
@@ -1,51 +1,197 @@
 
         
-                css('.note h3', '.warning h3').each do |node|
-          node.before('<p><strong>#{node.inner_html}</strong></p>').remove
-        end
+            if superenv?
+      ENV.keg_only_deps = keg_only_deps
+      ENV.deps = formula_deps
+      ENV.x11 = reqs.any? { |rq| rq.is_a?(X11Requirement) }
+      ENV.setup_build_environment(formula)
+      post_superenv_hacks
+      reqs.each(&:modify_build_environment)
+      deps.each(&:modify_build_environment)
+    else
+      ENV.setup_build_environment(formula)
+      reqs.each(&:modify_build_environment)
+      deps.each(&:modify_build_environment)
     
-          def call
-        title('Gems')
-        table(all_gem_names) do |gem, row|
-          row.yellow if update_available?(gem)
-          row << gem
-          row << installed_gem_version(gem)
-          row << '(update available)' if update_available?(gem)
-        end
+        option_names.any? do |name|
+      if option_defined? 'with-#{name}'
+        include? 'with-#{name}'
+      elsif option_defined? 'without-#{name}'
+        !include? 'without-#{name}'
+      else
+        false
       end
-    
-      desc 'Updated'
-  task :updated do
+    end
   end
     
-          describe 'setting an internal role filter' do
-        subject { dsl.roles(:app) }
-        it 'ignores it' do
-          dsl.set :filter, role: :web
-          expect(subject.map(&:hostname)).to eq(['example3.com', 'example4.com'])
-        end
-      end
-    
-      let(:help_output) do
-    out, _err = capture_io do
-      flags '--help', '-h'
+        if !Language::Python.in_sys_path?('python', homebrew_site_packages)
+      s = <<-EOS.undent
+        Python modules have been installed and Homebrew's site-packages is not
+        in your Python sys.path, so you will not be able to import the modules
+        this formula installed. If you plan to develop with these modules,
+        please run:
+      EOS
+      s += instructions
+    elsif keg.python_pth_files_installed?
+      s = <<-EOS.undent
+        This formula installed .pth files to Homebrew's site-packages and your
+        Python isn't configured to process them, so you will not be able to
+        import the modules this formula installed. If you plan to develop
+        with these modules, please run:
+      EOS
+      s += instructions
     end
-    out
+    s
   end
     
-    __END__
+      # Clean a top-level (bin, sbin, lib) directory, recursively, by fixing file
+  # permissions and removing .la files, unless the files (or parent
+  # directories) are protected by skip_clean.
+  #
+  # bin and sbin should not have any subdirectories; if either do that is
+  # caught as an audit warning
+  #
+  # lib may have a large directory tree (see Erlang for instance), and
+  # clean_dir applies cleaning rules to the entire tree
+  def clean_dir(d)
+    d.find do |path|
+      path.extend(ObserverPathnameExtension)
     
-            if @javascript and not @escaper.respond_to? :escape_javascript
-          fail('Use EscapeUtils for JavaScript escaping.')
-        end
-      end
+    module Homebrew
+  SEARCH_ERROR_QUEUE = Queue.new
     
-        it 'Reads referrer from Host header when Referer header is missing' do
-      env = {'HTTP_HOST' => 'foo.com'}
-      expect(subject.referrer(env)).to eq('foo.com')
+      # Use this method to generate standard caveats.
+  def standard_instructions(home_name, home_value = libexec)
+    <<-EOS.undent
+      Before you can use these tools you must export some variables to your $SHELL.
+    
+      private
+    
+      # Finds the groups of the source user, optionally limited to those visible to
+  # the current user.
+  def execute(current_user = nil)
+    segments = all_groups(current_user)
+    
+        begin
+      response = U2F::RegisterResponse.load_from_json(params[:device_response])
+      registration_data = u2f.register!(challenges, response)
+      registration.update(certificate: registration_data.certificate,
+                          key_handle: registration_data.key_handle,
+                          public_key: registration_data.public_key,
+                          counter: registration_data.counter,
+                          user: user,
+                          name: params[:name])
+    rescue JSON::ParserError, NoMethodError, ArgumentError
+      registration.errors.add(:base, 'Your U2F device did not send a valid JSON response.')
+    rescue U2F::Error => e
+      registration.errors.add(:base, e.message)
     end
     
-      %w(POST PUT DELETE).each do |method|
-    it 'denies #{method} requests with non-whitelisted Origin' do
-      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://malicious.com')).not_to be_ok
+      list = rss_url && !rss_url.empty? ? blogs : unavailable
+  list.push(Struct::Blog.new(name, web_url, rss_url))
+end
+    
+        formulae = ARGV.include?('--installed') ? Formula.installed : Formula
+    recursive = ARGV.flag? '--recursive'
+    only_installed_arg = ARGV.include?('--installed') &&
+                         !ARGV.include?('--include-build') &&
+                         !ARGV.include?('--include-test') &&
+                         !ARGV.include?('--include-optional') &&
+                         !ARGV.include?('--skip-recommended')
+    
+        def prune?(dependent, req, &_block)
+      catch(:prune) do
+        if block_given?
+          yield dependent, req
+        elsif req.optional? || req.recommended?
+          prune unless dependent.build.with?(req)
+        end
+      end
+    end
+    
+      def send_sinatra_file(path, &missing_file_block)
+    file_path = File.join(File.dirname(__FILE__), 'public',  path)
+    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
+    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
+  end
+    
+        def render(context)
+      quote = paragraphize(super)
+      author = '<strong>#{@by.strip}</strong>' if @by
+      if @source
+        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
+        parts = []
+        url.each do |part|
+          if (parts + [part]).join('/').length < 32
+            parts << part
+          end
+        end
+        source = parts.join('/')
+        source << '/&hellip;' unless source == @source
+      end
+      if !@source.nil?
+        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
+      elsif !@title.nil?
+        cite = ' <cite>#{@title}</cite>'
+      end
+      blockquote = if @by.nil?
+        quote
+      elsif cite
+        '#{quote}<footer>#{author + cite}</footer>'
+      else
+        '#{quote}<footer>#{author}</footer>'
+      end
+      '<blockquote>#{blockquote}</blockquote>'
+    end
+    
+    
+  # Jekyll hook - the generate method is called by jekyll, and generates all of the category pages.
+  class GenerateCategories < Generator
+    safe true
+    priority :low
+    
+    class ConfigTag < Liquid::Tag
+  def initialize(tag_name, options, tokens)
+    super
+    options = options.split(' ').map {|i| i.strip }
+    @key = options.slice!(0)
+    @tag = nil
+    @classname = nil
+    options.each do |option|
+      @tag = $1 if option =~ /tag:(\S+)/ 
+      @classname = $1 if option =~ /classname:(\S+)/
+    end
+  end
+    
+    module OctopressFilters
+  def self.pre_filter(page)
+    if page.ext.match('html|textile|markdown|md|haml|slim|xml')
+      input = BacktickCodeBlock::render_code_block(page.content)
+      page.content = input.gsub /(<figure.+?>.+?<\/figure>)/m do
+        TemplateWrapper::safe_wrap($1)
+      end
+    end
+  end
+  def self.post_filter(page)
+    if page.ext.match('html|textile|markdown|md|haml|slim|xml')
+      page.output = TemplateWrapper::unwrap(page.output)
+    end
+  end
+    
+        def render(context)
+      output = super
+      types = {
+        '.mp4' => 'type='video/mp4; codecs=\'avc1.42E01E, mp4a.40.2\''',
+        '.ogv' => 'type='video/ogg; codecs=theora, vorbis'',
+        '.webm' => 'type='video/webm; codecs=vp8, vorbis''
+      }
+      if @videos.size > 0
+        video =  '<video #{sizes} preload='metadata' controls #{poster}>'
+        @videos.each do |v|
+          video << '<source src='#{v}' #{types[File.extname(v)]}>'
+        end
+        video += '</video>'
+      else
+        'Error processing input, expected syntax: {% video url/to/video [url/to/video] [url/to/video] [width height] [url/to/poster] %}'
+      end
     end
