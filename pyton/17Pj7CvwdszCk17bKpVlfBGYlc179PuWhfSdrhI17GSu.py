6dc6471c3bef8ef73c2ba6eff2cd1fabcc7fb639
SUNsung <weatherius@gmail.com>
2018-05-24 10:34:02 +0300

    Rewrite [17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py] File in: 2018-05-24 10:34:01

--- pyton/17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py
+++ pyton/17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py
@@ -1,130 +1,45 @@
 
         
-            Models: `flatpages.flatpages`
-    Templates: Uses the template defined by the ``template_name`` field,
-        or :template:`flatpages/default.html` if template_name is not defined.
-    Context:
-        flatpage
-            `flatpages.flatpages` object
-    '''
-    if not url.startswith('/'):
-        url = '/' + url
-    site_id = get_current_site(request).id
-    try:
-        f = get_object_or_404(FlatPage, url=url, sites=site_id)
-    except Http404:
-        if not url.endswith('/') and settings.APPEND_SLASH:
-            url += '/'
-            f = get_object_or_404(FlatPage, url=url, sites=site_id)
-            return HttpResponsePermanentRedirect('%s/' % request.path)
-        else:
-            raise
-    return render_flatpage(request, f)
+        
+def plot_feature_times(all_times, batch_size, all_components, data):
+    plt.figure()
+    plot_results(all_components, all_times['pca'], label='PCA')
+    plot_results(all_components, all_times['ipca'],
+                 label='IncrementalPCA, bsize=%i' % batch_size)
+    plot_results(all_components, all_times['rpca'], label='RandomizedPCA')
+    plt.legend(loc='upper left')
+    plt.suptitle('Algorithm runtime vs. n_components\n \
+                 LFW, size %i x %i' % data.shape)
+    plt.xlabel('Number of components (out of max %i)' % data.shape[1])
+    plt.ylabel('Time (seconds)')
     
-        def remove(self, key):
-        hash_index = self._hash_function(key)
-        for index, item in enumerate(self.table[hash_index]):
-            if item.key == key:
-                del self.table[hash_index][index]
-                return
-        raise KeyError('Key not found')
+    The outer product of the row and column label vectors shows a
+representation of the checkerboard structure.
     
-        def bfs(self, source, dest):
-        if source is None:
-            return False
-        queue = deque()
-        queue.append(source)
-        source.visit_state = State.visited
-        while queue:
-            node = queue.popleft()
-            print(node)
-            if dest is node:
-                return True
-            for adjacent_node in node.adj_nodes.values():
-                if adjacent_node.visit_state == State.unvisited:
-                    queue.append(adjacent_node)
-                    adjacent_node.visit_state = State.visited
-        return False
+        # View probabilities=
+    probas = classifier.predict_proba(Xfull)
+    n_classes = np.unique(y_pred).size
+    for k in range(n_classes):
+        plt.subplot(n_classifiers, n_classes, index * n_classes + k + 1)
+        plt.title('Class %d' % k)
+        if k == 0:
+            plt.ylabel(name)
+        imshow_handle = plt.imshow(probas[:, k].reshape((100, 100)),
+                                   extent=(3, 9, 1, 5), origin='lower')
+        plt.xticks(())
+        plt.yticks(())
+        idx = (y_pred == k)
+        if idx.any():
+            plt.scatter(X[idx, 0], X[idx, 1], marker='o', c='k')
     
+    import mock
     
-class Cache(object):
-    
-        def remove_vehicle(self):
-        pass
-
-    
-        def receive_friend_request(self, friend_id):
-        pass
-    
-            assert len(self.regex.groupindex) in (0, self.regex.groups), \
-            ('groups in url regexes must either be all named or all '
-             'positional: %r' % self.regex.pattern)
-    
-    
-_re_unescape_pattern = re.compile(r'\\(.)', re.DOTALL)
-    
-        @gen_test
-    def test_put_with_getters(self):
-        q = queues.Queue()
-        get0 = q.get()
-        get1 = q.get()
-        yield q.put(0)
-        self.assertEqual(0, (yield get0))
-        yield q.put(1)
-        self.assertEqual(1, (yield get1))
-    
-        def test_legacy_interface(self):
-        response = self.fetch('/')
-        if not self.http1:
-            self.skipTest('requires HTTP/1.x')
-        self.assertEqual(response.body, b'Hello world')
-
-    
-            InheritBoth is a subclass of both AsyncTestCase and
-        SetUpTearDown, with the ordering so that the super of
-        AsyncTestCase will be SetUpTearDown.
-        '''
-        events = []
-        result = unittest.TestResult()
-    
-            @gen.coroutine
-        def f(index):
-            with (yield sem.acquire()):
-                history.append('acquired %d' % index)
-                yield gen.sleep(0.01)
-                history.append('release %d' % index)
-    
-        @classmethod
-    def send_updates(cls, chat):
-        logging.info('sending message to %d waiters', len(cls.waiters))
-        for waiter in cls.waiters:
-            try:
-                waiter.write_message(chat)
-            except:
-                logging.error('Error sending message', exc_info=True)
-    
-        def finish_tree(self, tree, filename):
-        if self.found_future_import:
-            return
-        if not isinstance(tree, pytree.Node):
-            # Empty files (usually __init__.py) show up as a single Leaf
-            # instead of a Node, so leave them alone
-            return
-        first_stmt = tree.children[0]
-        if is_docstring(first_stmt):
-            # Skip a line and add the import after the docstring
-            tree.insert_child(1, Newline())
-            pos = 2
-        elif first_stmt.prefix:
-            # No docstring, but an initial comment (perhaps a #! line).
-            # Transfer the initial comment to a new blank line.
-            newline = Newline()
-            newline.prefix = first_stmt.prefix
-            first_stmt.prefix = ''
-            tree.insert_child(0, newline)
-            pos = 1
-        else:
-            # No comments or docstring, just insert at the start
-            pos = 0
-        tree.insert_child(pos, self.new_future_import(None))
-        tree.insert_child(pos + 1, Newline())  # terminates the import stmt
+      Args:
+    nesting_state: The _NestingState object that contains info about our state.
+    is_forward_declaration: If the class is a forward declared class.
+  Returns:
+    Whether or not the new block is directly in a namespace.
+  '''
+  if is_forward_declaration:
+    return len(nesting_state.stack) >= 1 and (
+      isinstance(nesting_state.stack[-1], _NamespaceInfo))
