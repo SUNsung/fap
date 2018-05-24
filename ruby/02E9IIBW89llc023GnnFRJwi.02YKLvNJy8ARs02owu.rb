4431bd674f4e6d237c42e31341e5a382f19c20e7
SUNsung <weatherius@gmail.com>
2018-05-25 01:11:15 +0300

    Rewrite [02E9IIBW89llc023GnnFRJwi.02YKLvNJy8ARs02owu.rb] File in: 2018-05-25 01:11:15

--- ruby/02E9IIBW89llc023GnnFRJwi.02YKLvNJy8ARs02owu.rb
+++ ruby/02E9IIBW89llc023GnnFRJwi.02YKLvNJy8ARs02owu.rb
@@ -1,77 +1,92 @@
 
         
-        require 'json'
-require 'open-uri'
-    
-        def no_subcommand(args)
-      unless args.empty? ||
-          args.first !~ %r(!/^--/!) || %w(--help --version).include?(args.first)
-        deprecation_message 'Jekyll now uses subcommands instead of just switches. \
-                          Run `jekyll help` to find out more.'
-        abort
+            context 'minimal configuration' do
+      let(:options) do
+        {
+          name: { 'en-US' => 'Fastlane Demo' },
+          description: { 'en-US' => 'Demo description' }
+        }
       end
+    
+          it 'it increments all targets patch version number' do
+        Fastlane::FastFile.new.parse('lane :test do
+          increment_version_number
+        end').runner.execute(:test)
+    
+        def log_processed(name)
+      puts green '    #{name}'
     end
     
-        # Get or set the priority of this plugin. When called without an
-    # argument it returns the priority. When an argument is given, it will
-    # set the priority.
-    #
-    # priority - The Symbol priority (default: nil). Valid options are:
-    #            :lowest, :low, :normal, :high, :highest
-    #
-    # Returns the Symbol priority.
-    def self.priority(priority = nil)
-      @priority ||= nil
-      if priority && PRIORITIES.key?(priority)
-        @priority = priority
-      end
-      @priority || :normal
-    end
-    
-      def tumblr_consumer_key
-    ENV['TUMBLR_OAUTH_KEY']
+    desc 'Dumps output to a CSS file for testing'
+task :debug do
+  require 'sass'
+  path = Bootstrap.stylesheets_path
+  %w(bootstrap).each do |file|
+    engine = Sass::Engine.for_file('#{path}/#{file}.scss', syntax: :scss, load_paths: [path])
+    File.open('./#{file}.css', 'w') { |f| f.write(engine.render) }
   end
+end
     
-      def reemit
-    @event.reemit!
-    respond_to do |format|
-      format.html { redirect_back event_path(@event), notice: 'Event re-emitted.' }
-    end
-  end
-    
-        respond_to do |format|
-      format.html { redirect_to jobs_path, notice: 'All jobs removed.' }
-      format.json { head :no_content }
-    end
-  end
-    
-      def destroy
-    @services = current_user.services.find(params[:id])
-    @services.destroy
-    
-      # Before we load the schema, define the timestamp_id function.
-  # Idiomatically, we might do this in a migration, but then it
-  # wouldn't end up in schema.rb, so we'd need to figure out a way to
-  # get it in before doing db:setup as well. This is simpler, and
-  # ensures it's always in place.
-  Rake::Task['db:schema:load'].enhance ['db:define_timestamp_id']
-    
-      def type
-    'Emoji'
-  end
-    
-    namespace :emojis do
-  desc 'Generate a unicode to filename mapping'
-  task :generate do
-    source = 'http://www.unicode.org/Public/emoji/5.0/emoji-test.txt'
-    codes  = []
-    dest   = Rails.root.join('app', 'javascript', 'mastodon', 'features', 'emoji', 'emoji_map.json')
-    
-    module LogStash
-  module Api
-    module Commands
-      module System
-        class Plugins < Commands::Base
-          def run
-            { :total => plugins.count, :plugins => plugins }
+            def run
+          UI.puts('$CACHE_ROOT: #{@cache.root}') if @short_output
+          if @pod_name.nil? # Print all
+            @cache.cache_descriptors_per_pod.each do |pod_name, cache_descriptors|
+              print_pod_cache_infos(pod_name, cache_descriptors)
+            end
+          else # Print only for the requested pod
+            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
+            if cache_descriptors.nil?
+              UI.notice('No cache for pod named #{@pod_name} found')
+            else
+              print_pod_cache_infos(@pod_name, cache_descriptors)
+            end
           end
+        end
+    
+            def run
+          print_version
+          signal_end_of_output
+          listen
+        end
+    
+          def core_gem_names
+        %w(capistrano airbrussh rake sshkit net-ssh) & Gem.loaded_specs.keys
+      end
+    
+          # Prints a title in blue with surrounding newlines.
+      def title(text)
+        # Use $stdout directly to bypass the indentation that our `puts` does.
+        $stdout.puts(color.colorize('\n#{text}\n', :blue))
+      end
+    
+        def default_tasks
+      %w{install}
+    end
+    
+      # Read and eval a .rake file in such a way that `self` within the .rake file
+  # refers to this plugin instance. This gives the tasks in the file access to
+  # helper methods defined by the plugin.
+  def eval_rakefile(path)
+    contents = IO.read(path)
+    instance_eval(contents, path, 1)
+  end
+    
+        return captured_stdout.string, captured_stderr.string
+  ensure
+    $stdout = orig_stdout
+    $stderr = orig_stderr
+  end
+end
+
+    
+        worker_count.times do |count|
+      template '/data/#{app}/shared/config/sidekiq_#{count}.yml' do
+        owner node[:owner_name]
+        group node[:owner_name]
+        mode 0644
+        source 'sidekiq.yml.erb'
+        variables({
+          :require => '/data/#{app}/current'
+        })
+      end
+    end
