46ca0eadfc404a5d997d4f395ca06b80a18be607
SUNsung <weatherius@gmail.com>
2018-05-24 14:43:49 +0300

    Rewrite [540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py] File in: 2018-05-24 14:43:48

--- pyton/540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py
+++ pyton/540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py
@@ -1,183 +1,66 @@
 
         
-        
-def register_teardowns(app):
-    @app.teardown_appcontext
-    def close_db(error):
-        '''Closes the database again at the end of the request.'''
-        if hasattr(g, 'sqlite_db'):
-            g.sqlite_db.close()
-
+            # To avoid having to always use the '|safe' filter in flatpage templates,
+    # mark the title and content as already safe (since they are raw HTML
+    # content in the first place).
+    f.title = mark_safe(f.title)
+    f.content = mark_safe(f.content)
     
-            # because special names such as Name.Class, Name.Function, etc.
-        # are not recognized as such later in the parsing, we choose them
-        # to look the same as ordinary variables.
-        Name:                      '#000000',        # class: 'n'
-        Name.Attribute:            '#c4a000',        # class: 'na' - to be revised
-        Name.Builtin:              '#004461',        # class: 'nb'
-        Name.Builtin.Pseudo:       '#3465a4',        # class: 'bp'
-        Name.Class:                '#000000',        # class: 'nc' - to be revised
-        Name.Constant:             '#000000',        # class: 'no' - to be revised
-        Name.Decorator:            '#888',           # class: 'nd' - to be revised
-        Name.Entity:               '#ce5c00',        # class: 'ni'
-        Name.Exception:            'bold #cc0000',   # class: 'ne'
-        Name.Function:             '#000000',        # class: 'nf'
-        Name.Property:             '#000000',        # class: 'py'
-        Name.Label:                '#f57900',        # class: 'nl'
-        Name.Namespace:            '#000000',        # class: 'nn' - to be revised
-        Name.Other:                '#000000',        # class: 'nx'
-        Name.Tag:                  'bold #004461',   # class: 'nt' - like a keyword
-        Name.Variable:             '#000000',        # class: 'nv' - to be revised
-        Name.Variable.Class:       '#000000',        # class: 'vc' - to be revised
-        Name.Variable.Global:      '#000000',        # class: 'vg' - to be revised
-        Name.Variable.Instance:    '#000000',        # class: 'vi' - to be revised
+    from mrjob.job import MRJob
     
+        def mapper(self, _, line):
+        '''Parse each log line, extract and transform relevant lines.
     
-def consume_socket_content(sock, timeout=0.5):
-    chunks = 65536
-    content = b''
+        def __init__(self, floor, total_spots):
+        self.floor = floor
+        self.num_spots = total_spots
+        self.available_spots = 0
+        self.spots = []  # List of ParkingSpots
     
-        authstr = 'Basic ' + to_native_string(
-        b64encode(b':'.join((username, password))).strip()
+            self.assertFalse(self.vhost2.conflicts([self.addr1,
+                                                self.addr_default]))
+    
+        @mock.patch(
+        'certbot_compatibility_test.validator.crypto_util.probe_sni')
+    def test_certificate_error(self, mock_probe_sni):
+        cert = OpenSSL.crypto.X509()
+        mock_probe_sni.side_effect = [acme_errors.Error]
+        self.assertFalse(self.validator.certificate(
+            cert, 'test.com', '127.0.0.1'))
+    
+    js.load_plugin_modules(plugins)
+modules = dict((k, m) for k, m in js.module.iteritems())
+print 'JS_MODULES := ' + ' '.join(modules.iterkeys())
+outputs = []
+for name, module in modules.iteritems():
+    outputs.extend(module.outputs)
+    print 'JS_MODULE_OUTPUTS_%s := %s' % (name, ' '.join(module.outputs))
+    print 'JS_MODULE_DEPS_%s := %s' % (name, ' '.join(module.dependencies))
+    
+        current_ref = get_current_ref()
+    base_ref = get_merge_base()
+    if options.files:
+        files = options.files
+    else:
+        files = get_changed_files(base_ref, current_ref)
+        logging.debug('files changed: %r', files)
+    
+            if 'note' in notes_json:
+            note = notes_json['note'] or ''
+            if not rel_exists:
+                # If this is a newly created friend relationship,
+                # the cache needs to be updated before a note can
+                # be applied
+                c.user.friend_rels_cache(_update=True)
+            c.user.add_friend_note(friend, note)
+        rel_view = FriendTableItem(friend_rel)
+        return self.api_wrapper(FriendTableItemJsonTemplate().data(rel_view))
+    
+        @validate(
+        container_id=VGTMContainerId('id')
     )
+    def GET_jail(self, container_id):
+        return GoogleTagManagerJail(container_id=container_id).render()
     
-    
-class CookieConflictError(RuntimeError):
-    '''There are two cookies that meet the criteria specified in the cookie jar.
-    Use .get and .set and include domain and path args in order to be more specific.
-    '''
-    
-    #: Python 2.x?
-is_py2 = (_ver[0] == 2)
-    
-    class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', 'Arguments to pass into py.test')]
-    
-    
-@pytest.mark.parametrize(
-    'value, expected', (
-        ('example.com/path', 'http://example.com/path'),
-        ('//example.com/path', 'http://example.com/path'),
-    ))
-def test_prepend_scheme_if_needed(value, expected):
-    assert prepend_scheme_if_needed(value, 'http') == expected
-    
-            :param url: URL for the new :class:`Request` object.
-        :param data: (optional) Dictionary, bytes, or file-like object to send in the body of the :class:`Request`.
-        :param json: (optional) json to send in the body of the :class:`Request`.
-        :param \*\*kwargs: Optional arguments that ``request`` takes.
-        :rtype: requests.Response
-        '''
-    
-        assert history.history['acc'] != history.history['weighted_acc']
-    
-    
-@keras_test
-@pytest.mark.skipif(K.backend() == 'theano', reason='Not supported.')
-@pytest.mark.parametrize('merge_mode', ['sum', 'concat', None])
-def test_Bidirectional_dropout(merge_mode):
-    rnn = layers.LSTM
-    samples = 2
-    dim = 5
-    timesteps = 3
-    units = 3
-    X = [np.random.rand(samples, timesteps, dim)]
-    
-    
-def preprocess_image(image_path):
-    img = load_img(image_path, target_size=(img_nrows, img_ncols))
-    img = img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = vgg19.preprocess_input(img)
-    return img
-    
-    from keras.utils.test_utils import keras_test
-from keras.utils.test_utils import layer_test
-from keras.legacy import layers as legacy_layers
-from keras import layers
-from keras import models
-from keras import regularizers
-from keras import constraints
-from keras import backend as K
-import numpy as np
-    
-        x = K.placeholder(ndim=2)
-    f = K.function([x], [activations.softmax(x)])
-    test_values = get_standard_values()
-    
-        # Test with Sequential API
-    model = Sequential([
-        layers.Dense(16, input_shape=(x_train.shape[-1],), activation='relu'),
-        layers.Dense(8),
-        layers.Activation('relu'),
-        layers.Dense(num_classes, activation='softmax')
-    ])
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='rmsprop',
-                  metrics=['accuracy'])
-    model.summary()
-    history = model.fit(x_train, y_train, epochs=15, batch_size=16,
-                        validation_data=(x_test, y_test),
-                        verbose=0)
-    assert(history.history['val_acc'][-1] > 0.8)
-    config = model.get_config()
-    model = Sequential.from_config(config)
-    
-        print()
-    print('Classification performance:')
-    print('===========================')
-    print()
-    print('%s %s %s %s' % ('Classifier  ', 'train-time', 'test-time',
-                           'Accuracy'))
-    print('-' * 44)
-    for name in sorted(accuracy, key=accuracy.get):
-        print('%s %s %s %s' % (name.ljust(16),
-                               ('%.4fs' % train_time[name]).center(10),
-                               ('%.4fs' % test_time[name]).center(10),
-                               ('%.4f' % accuracy[name]).center(10)))
-    
-        for i in range(n_iter):
-    
-        # start time
-    tstart = datetime.now()
-    clf = DecisionTreeClassifier()
-    clf.fit(X, Y).predict(X)
-    delta = (datetime.now() - tstart)
-    # stop time
-    
-    pages = {
-    u'ar': u'http://ar.wikipedia.org/wiki/%D9%88%D9%8A%D9%83%D9%8A%D8%A8%D9%8A%D8%AF%D9%8A%D8%A7',
-    u'de': u'http://de.wikipedia.org/wiki/Wikipedia',
-    u'en': u'https://en.wikipedia.org/wiki/Wikipedia',
-    u'es': u'http://es.wikipedia.org/wiki/Wikipedia',
-    u'fr': u'http://fr.wikipedia.org/wiki/Wikip%C3%A9dia',
-    u'it': u'http://it.wikipedia.org/wiki/Wikipedia',
-    u'ja': u'http://ja.wikipedia.org/wiki/Wikipedia',
-    u'nl': u'http://nl.wikipedia.org/wiki/Wikipedia',
-    u'pl': u'http://pl.wikipedia.org/wiki/Wikipedia',
-    u'pt': u'http://pt.wikipedia.org/wiki/Wikip%C3%A9dia',
-    u'ru': u'http://ru.wikipedia.org/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D0%BF%D0%B5%D0%B4%D0%B8%D1%8F',
-#    u'zh': u'http://zh.wikipedia.org/wiki/Wikipedia',
-}
-    
-    import matplotlib.pyplot as plt
-import numpy as np
-    
-    print 'PLUGIN_I18N_PATHS := ' + ','.join(os.path.relpath(plugin.path)
-                                         for plugin in plugins
-                                         if plugin.needs_translation)
-    
-        def current_oauth_client(self):
-        client = self.stacked_proxy_safe_get(c, 'oauth2_client', None)
-        return getattr(client, '_id', None)
-    
-    from pylons import request
-from pylons import app_globals as g
-from reddit_base import RedditController
-from r2.lib.pages import AdminPage, AdminAwards
-from r2.lib.pages import AdminAwardGive, AdminAwardWinners
-from r2.lib.validator import *
-    
-            An iden is given as the `captcha` field with a `BAD_CAPTCHA`
-        error, you should use this endpoint if you get a
-        `BAD_CAPTCHA` error response.
+    import json
+import os
