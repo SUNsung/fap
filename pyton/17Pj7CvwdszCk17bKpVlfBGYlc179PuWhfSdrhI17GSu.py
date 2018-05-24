991430e7e270765e024cc78701c28a4db6c15378
SUNsung <weatherius@gmail.com>
2018-05-24 14:25:14 +0300

    Rewrite [17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py] File in: 2018-05-24 14:25:13

--- pyton/17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py
+++ pyton/17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py
@@ -1,45 +1,18 @@
 
         
-        
-def plot_feature_times(all_times, batch_size, all_components, data):
-    plt.figure()
-    plot_results(all_components, all_times['pca'], label='PCA')
-    plot_results(all_components, all_times['ipca'],
-                 label='IncrementalPCA, bsize=%i' % batch_size)
-    plot_results(all_components, all_times['rpca'], label='RandomizedPCA')
-    plt.legend(loc='upper left')
-    plt.suptitle('Algorithm runtime vs. n_components\n \
-                 LFW, size %i x %i' % data.shape)
-    plt.xlabel('Number of components (out of max %i)' % data.shape[1])
-    plt.ylabel('Time (seconds)')
+            def __get__(self, obj, type=None):
+        if obj is None:
+            return self
+        rv = obj.config[self.__name__]
+        if self.get_converter is not None:
+            rv = self.get_converter(rv)
+        return rv
     
-    The outer product of the row and column label vectors shows a
-representation of the checkerboard structure.
     
-        # View probabilities=
-    probas = classifier.predict_proba(Xfull)
-    n_classes = np.unique(y_pred).size
-    for k in range(n_classes):
-        plt.subplot(n_classifiers, n_classes, index * n_classes + k + 1)
-        plt.title('Class %d' % k)
-        if k == 0:
-            plt.ylabel(name)
-        imshow_handle = plt.imshow(probas[:, k].reshape((100, 100)),
-                                   extent=(3, 9, 1, 5), origin='lower')
-        plt.xticks(())
-        plt.yticks(())
-        idx = (y_pred == k)
-        if idx.any():
-            plt.scatter(X[idx, 0], X[idx, 1], marker='o', c='k')
+def test_teardown_on_pop(app):
+    buffer = []
     
-    import mock
-    
-      Args:
-    nesting_state: The _NestingState object that contains info about our state.
-    is_forward_declaration: If the class is a forward declared class.
-  Returns:
-    Whether or not the new block is directly in a namespace.
-  '''
-  if is_forward_declaration:
-    return len(nesting_state.stack) >= 1 and (
-      isinstance(nesting_state.stack[-1], _NamespaceInfo))
+        rv = app.test_client().get('/', errors_stream=out)
+    assert rv.status_code == 500
+    assert b'Internal Server Error' in rv.data
+    assert not out.getvalue()
