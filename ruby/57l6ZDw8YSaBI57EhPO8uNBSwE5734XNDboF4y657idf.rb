db3feb031b3a42e09ead9c2106c68ec4ec9c8101
SUNsung <weatherius@gmail.com>
2018-05-24 07:56:28 +0300

    Rewrite [57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb] File in: 2018-05-24 07:56:28

--- ruby/57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb
+++ ruby/57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb
@@ -1,56 +1,98 @@
 
         
-            Or via the Cask:
-      brew cask install ngrok
-    EOS
+              get :redirect_to_new_record
+      assert_equal 'http://test.host/workshops', redirect_to_url
+      assert_redirected_to Workshop.new(nil)
+    end
+  end
+    
+          # Returns constant of subscription adapter specified in config/cable.yml.
+      # If the adapter cannot be found, this will default to the Redis adapter.
+      # Also makes sure proper dependencies are required.
+      def pubsub_adapter
+        adapter = (cable.fetch('adapter') { 'redis' })
+    
+      test 'helpers' do
+    assert_response_code_range 200..299, :successful?
+    assert_response_code_range [404],    :not_found?
+    assert_response_code_range 300..399, :redirection?
+    assert_response_code_range 500..599, :server_error?
+    assert_response_code_range 400..499, :client_error?
+  end
+    
+      def call_controller(klass, action)
+    klass.action(action).call(ActionController::TestRequest::DEFAULT_ENV.dup)
+  end
+    
+      class MyController < ActionController::Metal
+    use BlockMiddleware do |config|
+      config.configurable_message = 'Configured by block.'
+    end
+    use MyMiddleware
+    middleware.insert_before MyMiddleware, ExclaimerMiddleware
+    
+        # Helpers for creating and wrapping delivery behavior, used by DeliveryMethods.
+    module ClassMethods
+      # Provides a list of emails that have been delivered by Mail::TestMailer
+      delegate :deliveries, :deliveries=, to: Mail::TestMailer
+    
+        # Returns +text+ wrapped at +len+ columns and indented +indent+ spaces.
+    # By default column length +len+ equals 72 characters and indent
+    # +indent+ equal two spaces.
+    #
+    #   my_text = 'Here is a sample text with more than 40 characters'
+    #
+    #   format_paragraph(my_text, 25, 4)
+    #   # => '    Here is a sample text with\n    more than 40 characters'
+    def format_paragraph(text, len = 72, indent = 2)
+      sentences = [[]]
+    
+      teardown do
+    I18n.locale = I18n.default_locale
+  end
+    
+    Benchmark.ips do |x|
+  x.report('no body include?') { CONTENT_NOT_CONTAINING.include?('<body') }
+  x.report('no body regexp')   { CONTENT_NOT_CONTAINING =~ /<\s*body/ }
+  x.compare!
+end
+    
+        def defaults_deprecate_type(old, current)
+      Jekyll.logger.warn 'Defaults:', 'The '#{old}' type has become '#{current}'.'
+      Jekyll.logger.warn 'Defaults:', 'Please update your front-matter defaults to use \
+                        'type: #{current}'.'
+    end
   end
 end
 
     
-          keg_only_deps.each do |dep|
-        ENV.prepend_path 'PATH', dep.opt_bin.to_s
-        ENV.prepend_path 'PKG_CONFIG_PATH', '#{dep.opt_lib}/pkgconfig'
-        ENV.prepend_path 'PKG_CONFIG_PATH', '#{dep.opt_share}/pkgconfig'
-        ENV.prepend_path 'ACLOCAL_PATH', '#{dep.opt_share}/aclocal'
-        ENV.prepend_path 'CMAKE_PREFIX_PATH', dep.opt_prefix.to_s
-        ENV.prepend 'LDFLAGS', '-L#{dep.opt_lib}' if dep.opt_lib.directory?
-        ENV.prepend 'CPPFLAGS', '-I#{dep.opt_include}' if dep.opt_include.directory?
-      end
-    end
+    module Jekyll
+  class Layout
+    include Convertible
     
-      class SubclassX
-    attr_reader :key
-    def initialize(*)
-      @key = :value
-      super
-    end
-  end
-end
-
+            Bundler.setup
+        required_gems = Bundler.require(:jekyll_plugins)
+        message = 'Required #{required_gems.map(&:name).join(', ')}'
+        Jekyll.logger.debug('PluginManager:', message)
+        ENV['JEKYLL_NO_BUNDLER_REQUIRE'] = 'true'
     
-      it 'handles fractional usec close to rounding limit' do
-    time = Time.send(@method, 2000, 1, 1, 12, 30, 0, 9999r/10000)
-    
-      # Creates a delayed logger wrapping `inner`.
-  #
-  # @param inner [Sass::Logger::Base] The wrapped logger.
-  def initialize(inner)
-    self.log_level = inner.log_level
-    @inner = inner
-    @messages = []
+        @inner.log_level = log_level
+    Sass.logger = @inner
   end
     
-        # @see \{MediaQuery#to\_a}
-    def to_a
-      res = []
-      res += modifier
-      res << ' ' unless modifier.empty?
-      res += type
-      res << ' and ' unless type.empty? || expressions.empty?
-      res += Sass::Util.intersperse(expressions, ' and ').flatten
-      res
-    end
+        # Starts the read-eval-print loop.
+    def run
+      environment = Environment.new
+      @line = 0
+      loop do
+        @line += 1
+        unless (text = Readline.readline('>> '))
+          puts
+          return
+        end
     
-      if defined? config.symbolize_keys!
-    config.symbolize_keys!
-  end
+            def matches? subject
+          @subject = subject
+          @subject = subject.new if subject.class == Class
+          error_when_not_valid? && no_error_when_valid?
+        end
