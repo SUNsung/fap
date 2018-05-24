18dea1019072811db6c63b5c986a11497f6a166c
SUNsung <weatherius@gmail.com>
2018-05-24 13:41:14 +0300

    Rewrite [06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py] File in: 2018-05-24 13:41:13

--- pyton/06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py
+++ pyton/06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py
@@ -1,15 +1,83 @@
 
         
-        PROJ_NAME = 'you-get'
-PACKAGE_NAME = 'you_get'
+            All keys are expected to be strings. The structure remembers the
+    case of the last key to be set, and ``iter(instance)``,
+    ``keys()``, ``items()``, ``iterkeys()``, and ``iteritems()``
+    will contain case-sensitive keys. However, querying and contains
+    testing is case insensitive::
     
-    from ..common import *
-from ..extractor import VideoExtractor
+    
+class TestCaseInsensitiveDict:
+    
+    if is_py2:
+    from urllib import (
+        quote, unquote, quote_plus, unquote_plus, urlencode, getproxies,
+        proxy_bypass, proxy_bypass_environment, getproxies_environment)
+    from urlparse import urlparse, urlunparse, urljoin, urlsplit, urldefrag
+    from urllib2 import parse_http_list
+    import cookielib
+    from Cookie import Morsel
+    from StringIO import StringIO
+    
+            This method also replaces the Proxy-Authorization header where
+        necessary.
+    
+        :param content: bytestring to extract encodings from.
+    '''
+    warnings.warn((
+        'In requests 3.0, get_encodings_from_content will be removed. For '
+        'more information, please see the discussion on issue #2266. (This'
+        ' warning should only appear once.)'),
+        DeprecationWarning)
+    
+        m = re.match(r'(\d+)\.(\d+)(\.(\d+))?(b(\d+))?', version_str)
+    if m is None:
+        raise ValueError('Bad version string %r' % version_str)
     
     
+    def __str__(self):
+        return 'MismatchedRangeException(%r not in [%r..%r])' % (
+            self.getUnexpectedType(), self.a, self.b
+            )
+    __repr__ = __str__
     
-            # extract raw urls
-        orig_img = match1(content,
-                         r'<meta itemprop='image' content='([^']+/originals/[^']+)'')
-        twit_img = match1(content,
-                          r'<meta property='twitter:image:src' name='twitter:image:src' content='([^']+)'')
+        if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        print('Choose one path as argument one')
+
+    
+    
+class QueueBasicTest(AsyncTestCase):
+    def test_repr_and_str(self):
+        q = queues.Queue(maxsize=1)
+        self.assertIn(hex(id(q)), repr(q))
+        self.assertNotIn(hex(id(q)), str(q))
+        q.get()
+    
+        def test_simple(self):
+        response = self.fetch('/')
+        self.assertEqual(response.body, b'Hello world!')
+    
+                # Generator coroutines and with-statements with non-local
+            # effects interact badly.  Check here for signs of
+            # the stack getting out of sync.
+            # Note that this check comes after restoring _state.context
+            # so that if it fails things are left in a (relatively)
+            # consistent state.
+            if final_contexts is not self.new_contexts:
+                raise StackContextInconsistentError(
+                    'stack_context inconsistency (may be caused by yield '
+                    'within a 'with StackContext' block)')
+    
+    
+# It is impossible to quickly and consistently generate an error in name
+# resolution, so test this case separately, using mocks as needed.
+class _ResolverErrorTestMixin(object):
+    def test_bad_host(self):
+        def handler(exc_typ, exc_val, exc_tb):
+            self.stop(exc_val)
+            return True  # Halt propagation.
+    
+        def write_fileno(self):
+        return self.writer.fileno()
