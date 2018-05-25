861fe364f6459b30bc7f6f12a7ee7068fd51e374
SUNsung <weatherius@gmail.com>
2018-05-26 02:49:29 +0300

    Rewrite [35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py] File in: 2018-05-26 02:49:29

--- pyton/35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py
+++ pyton/35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py
@@ -1,34 +1,96 @@
 
         
-            if include_debug_info:
-        print_debug_info(env)
-        if args == ['--debug']:
-            return ExitStatus.OK
+        tf_proto_library_py(
+    name = 'spec_pb2',
+    srcs = ['spec.proto'],
+)
     
-        This argument can be omitted in which case HTTPie will use POST if there
-    is some data to be sent, otherwise GET:
+      def testDerivedParametersForRuntime(self):
+    '''Test generation of derived parameters for the runtime.'''
+    with tf.Graph().as_default(), self.test_session():
+      master = MockMaster()
+      component = MockComponent(master, _make_biaffine_spec())
     
-        def test_verify_custom_ca_bundle_path(
-            self, httpbin_secure_untrusted):
-        r = http(httpbin_secure_untrusted + '/get', '--verify', CA_BUNDLE)
-        assert HTTP_OK in r
     
-    '''
-__version__ = '1.0.0-dev'
-__author__ = 'Jakub Roztocil'
-__licence__ = 'BSD'
+def fetch_fast_fixed_embeddings(comp,
+                                state,
+                                pad_to_batch=None,
+                                pad_to_steps=None):
+  '''Looks up fixed features with fast, non-differentiable, op.
     
-            If decode_unicode is True, content will be decoded using the best
-        available encoding based on the response.
-        '''
+      # Resets session.
+  session_config = tf.ConfigProto(
+      log_device_placement=False,
+      intra_op_parallelism_threads=10,
+      inter_op_parallelism_threads=10)
     
-            assert server.handler_results[0] == first_request
-        assert server.handler_results[1] == second_request
     
-    elif is_py3:
-    from urllib.parse import urlparse, urlunparse, urljoin, urlsplit, urlencode, quote, unquote, quote_plus, unquote_plus, urldefrag
-    from urllib.request import parse_http_list, getproxies, proxy_bypass, proxy_bypass_environment, getproxies_environment
-    from http import cookiejar as cookielib
-    from http.cookies import Morsel
-    from io import StringIO
-    from collections import OrderedDict
+if __name__ == '__main__':
+  tf.test.main()
+
+    
+    ## All tokens go to the parser (unless skip() is called in that rule)
+# on a particular 'channel'.  The parser tunes to a particular channel
+# so that whitespace etc... can go to the parser on a 'hidden' channel.
+DEFAULT_CHANNEL = 0
+    
+        def test_big_queue_popright(self):
+        d = deque()
+        append, pop = d.appendleft, d.pop
+        for i in range(BIG):
+            append(i)
+        for i in range(BIG):
+            x = pop()
+            if x != i:
+                self.assertEqual(x, i)
+    
+        def unlock(self):
+        '''Unlock the mailbox if it is locked.'''
+        if self._locked:
+            _unlock_file(self._file)
+            _sync_close(self._file)
+            del self._file
+            self._locked = False
+    
+        print('LIBS=', end=' ')
+    for lib in libs: print(''%s'' % (lib), end=' ')
+    print() ; print()
+    
+    
+def pyfunc(arg1, arg2):
+    return [arg1, arg2]
+    
+        1: ('line', 2, 'tfunc_import'),    ('next',),
+    2: ('line', 3, 'tfunc_import'),    ('step',),
+    3: ('call', 5, 'main'),            ('break', ('test_module.py', None, False, None, 'func')),
+    4: ('None', 5, 'main'),            ('continue',),
+    5: ('line', 3, 'func', ({1: 1}, [])), ('step',),
+      BpNum Temp Enb Hits Ignore Where
+      1     no   yes 1    0      at test_module.py:2
+    6: ('return', 3, 'func'),          ('step',),
+    7: ('line', 7, 'main'),            ('step',),
+    8: ('return', 7, 'main'),          ('quit',),
+    
+        top = Toplevel(parent)
+    top.title('Test ColorDelegator')
+    x, y = map(int, parent.geometry().split('+')[1:])
+    top.geometry('700x250+%d+%d' % (x + 20, y + 175))
+    source = (
+        'if True: int ('1') # keyword, builtin, string, comment\n'
+        'elif False: print(0)\n'
+        'else: float(None)\n'
+        'if iF + If + IF: 'keyword matching must respect case'\n'
+        'if'': x or''  # valid string-keyword no-space combinations\n'
+        'async def f(): await g()\n'
+        '# All valid prefixes for unicode and byte strings should be colored.\n'
+        ''x', '''x''', \'x\', \'\'\'x\'\'\'\n'
+        'r'x', u'x', R'x', U'x', f'x', F'x'\n'
+        'fr'x', Fr'x', fR'x', FR'x', rf'x', rF'x', Rf'x', RF'x'\n'
+        'b'x',B'x', br'x',Br'x',bR'x',BR'x', rb'x'.rB'x',Rb'x',RB'x'\n'
+        '# Invalid combinations of legal characters should be half colored.\n'
+        'ur'x', ru'x', uf'x', fu'x', UR'x', ufr'x', rfu'x', xf'x', fx'x'\n'
+        )
+    text = Text(top, background='white')
+    text.pack(expand=1, fill='both')
+    text.insert('insert', source)
+    text.focus_set()
