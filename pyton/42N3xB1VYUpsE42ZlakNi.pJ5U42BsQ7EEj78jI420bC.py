6a9ec22cbd9121791351861f1e17d7c3f256872c
SUNsung <weatherius@gmail.com>
2018-05-25 04:13:33 +0300

    Rewrite [42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py] File in: 2018-05-25 04:13:32

--- pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
+++ pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
@@ -1,104 +1,82 @@
 
         
-        cc_test(
-    name = 'spanning_tree_iterator_test',
-    size = 'small',
-    srcs = ['spanning_tree_iterator_test.cc'],
-    deps = [
-        ':spanning_tree_iterator',
-        '//syntaxnet:test_main',
-        '@org_tensorflow//tensorflow/core:lib',
-        '@org_tensorflow//tensorflow/core:test',
-    ],
-)
+          # Construct the tagger. This is a simple left-to-right LSTM sequence tagger.
+  tagger = spec_builder.ComponentSpecBuilder('tagger')
+  tagger.set_network_unit(
+      name='wrapped_units.LayerNormBasicLSTMNetwork',
+      hidden_layer_sizes='256')
+  tagger.set_transition_system(name='tagger')
+  tagger.add_token_link(source=lookahead, fml='input.focus', embedding_dim=64)
     
-      # All arguments must share the same type.
-  check.Same([
-      weights_arc.dtype.base_dtype, weights_source.dtype.base_dtype,
-      root.dtype.base_dtype, tokens.dtype.base_dtype
-  ], 'dtype mismatch')
+        logits = self.network.get_bulk_predictions(stride, network_tensors)
+    if logits is None:
+      # The network does not produce custom bulk predictions; default to logits.
+      logits = self.network.get_logits(network_tensors)
+      logits = tf.cond(self.locally_normalize,
+                       lambda: tf.nn.log_softmax(logits), lambda: logits)
+      if self._output_as_probabilities:
+        logits = tf.nn.softmax(logits)
+    handle = dragnn_ops.bulk_advance_from_prediction(
+        state.handle, logits, component=self.name)
     
-      Returns:
-    1. None, since the op is not differentiable w.r.t. its |num_nodes| input.
-    2. [B,M,M] tensor where entry b,t,s is a subgradient of the network loss
-       w.r.t. entry b,t,s of the |scores| input, with the same dtype as
-       |d_loss_d_max_scores|.
-  '''
-  dtype = d_loss_d_max_scores.dtype.base_dtype
-  check.NotNone(dtype)
-    
-    
-@mock.patch('httpie.input.AuthCredentials._getpass',
-            new=lambda self, prompt: 'UNEXPECTED_PROMPT_RESPONSE')
-def test_auth_plugin_prompt_password_false(httpbin):
-    
-        def format_headers(self, headers):
-        for p in self.enabled_plugins:
-            headers = p.format_headers(headers)
-        return headers
-    
-        def __init__(self, **kwargs):
-        '''
-        :param env: an class:`Environment` instance
-        :param kwargs: additional keyword argument that some
-                       processor might require.
-    
-    {available}
-    
-        def _migrate_implicit_content_type(self):
-        '''Migrate the removed implicit_content_type config option'''
-        try:
-            implicit_content_type = self.pop('implicit_content_type')
-        except KeyError:
-            self.save()
-        else:
-            if implicit_content_type == 'form':
-                self['default_options'].insert(0, '--form')
-            self.save()
-            self.load()
-
+      roots_shape = tf.shape(roots)
+  arcs_shape = tf.shape(arcs)
+  batch_size = roots_shape[0]
+  num_tokens = roots_shape[1]
+  with tf.control_dependencies([
+      tf.assert_equal(batch_size, arcs_shape[0]),
+      tf.assert_equal(num_tokens, arcs_shape[1]),
+      tf.assert_equal(num_tokens, arcs_shape[2])]):
+    return tf.matrix_set_diag(arcs, roots)
     
     
-def repr_dict_nice(d):
-    def prepare_dict(d):
-        for k, v in d.items():
-            if isinstance(v, dict):
-                v = dict(prepare_dict(v))
-            elif isinstance(v, bytes):
-                v = v.decode('utf8')
-            elif not isinstance(v, (int, str)):
-                v = repr(v)
-            yield k, v
-    return json.dumps(
-        dict(prepare_dict(d)),
-        indent=4, sort_keys=True,
-    )
+def clean_output_paths(stripped_path):
+  '''Ensures that the output path is cleaned and ready to receive a model.'''
+  # If the export path's directory doesn't exist, create it.
+  export_directory = os.path.dirname(stripped_path)
+  if not tf.gfile.Exists(export_directory):
+    tf.logging.info('%s does not exist; creating it.' % export_directory)
+    tf.gfile.MakeDirs(export_directory)
     
-        def __call__(self, value):
-        # Session name can be a path or just a name.
-        if (os.path.sep not in value and
-                not VALID_SESSION_NAME_PATTERN.search(value)):
-            raise ArgumentError(None, self.error_message)
-        return value
-    
-    from httpie.compat import urlsplit
-from httpie.config import BaseConfigDict, DEFAULT_CONFIG_DIR
-from httpie.plugins import plugin_manager
-    
-    # How to display URL addresses: 'footnote', 'no', or 'inline'.
-#texinfo_show_urls = 'footnote'
-    
-    from certbot import constants
-from certbot_compatibility_test import errors
-from certbot_compatibility_test import util
-    
-        @mock.patch('certbot.display.enhancements.util')
-    def test_redirect(self, mock_util):
-        mock_util().menu.return_value = (display_util.OK, 1)
-        self.assertTrue(self._call('redirect'))
-    
-        description = 'Example Installer plugin'
+          target = spec_pb2.TrainTarget()
+      target.name = 'testTraining-all'
+      train = builder.add_training_from_config(target)
+      with self.test_session() as sess:
+        logging.info('Initializing')
+        sess.run(tf.global_variables_initializer())
     
     
-if __name__ == '__main__':
-    cli()
+@csrf_protect
+def render_flatpage(request, f):
+    '''
+    Internal interface to the flat page view.
+    '''
+    # If registration is required for accessing this page, and the user isn't
+    # logged in, redirect to the login page.
+    if f.registration_required and not request.user.is_authenticated:
+        from django.contrib.auth.views import redirect_to_login
+        return redirect_to_login(request.path)
+    if f.template_name:
+        template = loader.select_template((f.template_name, DEFAULT_TEMPLATE))
+    else:
+        template = loader.get_template(DEFAULT_TEMPLATE)
+    
+        return None
+    
+        def test_repr(self):
+        self.assertRegexpMatches(repr(PENDING_FUTURE),
+                                 '<Future at 0x[0-9a-f]+ state=pending>')
+        self.assertRegexpMatches(repr(RUNNING_FUTURE),
+                                 '<Future at 0x[0-9a-f]+ state=running>')
+        self.assertRegexpMatches(repr(CANCELLED_FUTURE),
+                                 '<Future at 0x[0-9a-f]+ state=cancelled>')
+        self.assertRegexpMatches(repr(CANCELLED_AND_NOTIFIED_FUTURE),
+                                 '<Future at 0x[0-9a-f]+ state=cancelled>')
+        self.assertRegexpMatches(
+                repr(EXCEPTION_FUTURE),
+                '<Future at 0x[0-9a-f]+ state=finished raised IOError>')
+        self.assertRegexpMatches(
+                repr(SUCCESSFUL_FUTURE),
+                '<Future at 0x[0-9a-f]+ state=finished returned int>')
+    
+      parsed_args, nosetests_args = parser.parse_known_args()
