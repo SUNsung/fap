26f05d4aae384751b61245b0271ba1a5b76f16ab
SUNsung <weatherius@gmail.com>
2018-05-25 12:49:00 +0300

    Rewrite [20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py] File in: 2018-05-25 12:48:59

--- pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
+++ pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
@@ -1,73 +1,53 @@
 
         
-            with open('README.md', 'w+') as sorted_file:
-        sorted_file.write(final_README)
-    
-        __slots__ = ('serializer',)
-    
-            app.config.from_envvar('YOURAPPLICATION_SETTINGS')
-    
-            if trv is not None:
-            return trv
-        raise TemplateNotFound(template)
+        
+@keras_test
+def test_conv2d_legacy_interface():
+    old_layer = keras.layers.Convolution2D(5, 3, 3, name='conv')
+    new_layer = keras.layers.Conv2D(5, (3, 3), name='conv')
+    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer.get_config())
     
     
-class BadNonceTest(unittest.TestCase):
-    '''Tests for acme.errors.BadNonce.'''
+def test_classify_class_build_fn():
+    class ClassBuildFnClf(object):
     
-        def __exit__(self, typ, value, tb):
-        if value is not None:
-            self.logger.error('Uncaught exception', exc_info=(typ, value, tb))
-            raise _QuietException
     
-        def create_client(self, **kwargs):
-        return CurlAsyncHTTPClient(force_instance=True,
-                                   defaults=dict(allow_ipv6=False),
-                                   **kwargs)
+@keras_test
+def test_model_trainability_switch():
+    # a non-trainable model has no trainable weights
+    x = Input(shape=(1,))
+    y = Dense(2)(x)
+    model = Model(x, y)
+    model.trainable = False
+    assert model.trainable_weights == []
     
-        def test_types(self):
-        response = self.fetch('/hello')
-        self.assertEqual(type(response.body), bytes)
-        self.assertEqual(type(response.headers['Content-Type']), str)
-        self.assertEqual(type(response.code), int)
-        self.assertEqual(type(response.effective_url), str)
+        # Returns
+        Preprocessed array.
+    '''
+    return imagenet_utils.preprocess_input(x, mode='tf')
+
     
-        def test_large_body_buffered(self):
-        with ExpectLog(gen_log, '.*Content-Length too long'):
-            response = self.fetch('/buffered', method='PUT', body=b'a' * 10240)
-        self.assertEqual(response.code, 400)
+        # Raises
+        ValueError: in case of invalid argument for `weights`,
+            or invalid input shape.
+    '''
+    if not (weights in {'imagenet', None} or os.path.exists(weights)):
+        raise ValueError('The `weights` argument should be either '
+                         '`None` (random initialization), `imagenet` '
+                         '(pre-training on ImageNet), '
+                         'or the path to the weights file to be loaded.')
     
-        def test_exception_logging(self):
-        '''Uncaught exceptions get logged by the IOLoop.'''
-        # Use a NullContext to keep the exception from being caught by
-        # AsyncTestCase.
-        with NullContext():
-            self.io_loop.add_callback(lambda: 1 / 0)
-            self.io_loop.add_callback(self.stop)
-            with ExpectLog(app_log, 'Exception in callback'):
-                self.wait()
-    
-            with closing(self.create_client(resolver=TimeoutResolver())) as client:
-            with ignore_deprecation():
-                response = yield client.fetch(self.get_url('/hello'),
-                                              connect_timeout=timeout,
-                                              raise_error=False)
-                self.assertEqual(response.code, 599)
-                self.assertTrue(timeout_min < response.request_time < timeout_max,
-                                response.request_time)
-                self.assertEqual(str(response.error), 'Timeout while connecting')
-    
-            with self.assertRaises(StackContextInconsistentError):
-            f()
-            self.wait()
-        # Cleanup: to avoid GC warnings (which for some reason only seem
-        # to show up on py33-asyncio), invoke the callback (which will do
-        # nothing since the gen.Runner is already finished) and delete it.
-        self.callback()
-        del self.callback
-    
-        def test_failed_write_error(self):
-        with ExpectLog(app_log, 'Uncaught exception'):
-            response = self.fetch('/failed_write_error')
-            self.assertEqual(response.code, 500)
-            self.assertEqual(b'', response.body)
+        for layer_class in [keras.layers.CuDNNGRU, keras.layers.CuDNNLSTM]:
+        model = keras.models.Sequential()
+        model.add(keras.layers.Embedding(10, input_size,
+                                         input_length=timesteps,
+                                         batch_input_shape=(num_samples,
+                                                            timesteps)))
+        layer = layer_class(units,
+                            return_sequences=False,
+                            stateful=True,
+                            weights=None)
+        model.add(layer)
+        model.compile(optimizer='sgd', loss='mse')
+        out1 = model.predict(np.ones((num_samples, timesteps)))
+        assert(out1.shape == (num_samples, units))
