677ef6e2dd14fd425f2d136b62b9483601cd9663
SUNsung <weatherius@gmail.com>
2018-05-26 16:11:15 +0300

    Rewrite [223VfZDu9R5co22y302JzHZIU22JjXIyCQBhg22uUX.rb] File in: 2018-05-26 16:11:15

--- ruby/223VfZDu9R5co22y302JzHZIU22JjXIyCQBhg22uUX.rb
+++ ruby/223VfZDu9R5co22y302JzHZIU22JjXIyCQBhg22uUX.rb
@@ -1,32 +1,59 @@
-    projects
-  end
-end
 
+        
+                entries = []
     
-        if registration
-      u2f.authenticate!(challenges, response, Base64.decode64(registration.public_key), registration.counter)
-      registration.update(counter: response.counter)
-      true
+        version '5' do
+      self.release = '5.0.7'
+      self.base_url = 'http://sinonjs.org/releases/v#{release}/'
     end
-  rescue JSON::ParserError, NoMethodError, ArgumentError, U2F::Error
-    false
-  end
-end
-
     
-        assert_equal(6, e.size)
-    set << 42
-    assert_equal(7, e.size)
+    module Docs
+  class Tensorflow < UrlScraper
+    self.name = 'TensorFlow'
+    self.type = 'tensorflow'
+    self.release = '1.8'
+    self.root_path = 'index.html'
+    self.links = {
+      home: 'https://www.tensorflow.org/',
+      code: 'https://github.com/tensorflow/tensorflow'
+    }
+    
+        def subpath_to(url, options = nil)
+      url = self.class.parse(url)
+      return unless origin == url.origin
+    
+              if argv.length == 2
+            # @deprecated
+            @env.ui.warn('WARNING: The second argument to `vagrant box remove`')
+            @env.ui.warn('is deprecated. Please use the --provider flag. This')
+            @env.ui.warn('feature will stop working in the next version.')
+            options[:provider] = argv[1]
+          end
+    
+              box_name     = argv[0]
+          box_provider = argv[1].to_sym
+          box_version  = argv[2]
+    
+    namespace :db do
+  namespace :migrate do
+    desc 'Setup the db or migrate depending on state of db'
+    task setup: :environment do
+      begin
+        if ActiveRecord::Migrator.current_version.zero?
+          Rake::Task['db:migrate'].invoke
+          Rake::Task['db:seed'].invoke
+        end
+      rescue ActiveRecord::NoDatabaseError
+        Rake::Task['db:setup'].invoke
+      else
+        Rake::Task['db:migrate'].invoke
+      end
+    end
   end
     
-      it 'decodes the number of shorts requested by the count modifier' do
-    'badcfe'.unpack(unpack_format(3)).should == [25185, 25699, 26213]
-  end
-    
-      it 'decodes the number of bytes specified by the count modifier' do
-    'abc'.unpack(unpack_format(3)).should == ['abc']
-  end
-    
-      require 'sass/plugin/rack'
-  class Sass::Plugin::MerbBootLoader < Merb::BootLoader
-    after Merb::BootLoader::RackUpApplication
+        HTTP.get(source).to_s.split('\n').each do |line|
+      next if line.start_with? '#'
+      parts = line.split(';').map(&:strip)
+      next if parts.size < 2
+      codes << [parts[0], parts[1].start_with?('fully-qualified')]
+    end
