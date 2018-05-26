d087719330df6e4844672405cdd7ec9a9d7b69eb
SUNsung <weatherius@gmail.com>
2018-05-26 10:00:43 +0300

    Rewrite [351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb] File in: 2018-05-26 10:00:42

--- ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
+++ ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
@@ -1,45 +1,150 @@
 
         
-          def initialize(strip, &block)
-    @strip    = strip
-    @resource = Resource::PatchResource.new(&block)
-  end
-    
-        if GitHub.api_credentials_type == :none
-      puts <<~EOS
-        You can create a new personal access token:
-         #{GitHub::ALL_SCOPES_URL}
-        and then set the new HOMEBREW_GITHUB_API_TOKEN as the authentication method.
-    
-    class RubyRequirement < Requirement
-  fatal true
-  satisfy do
-    odisabled('RubyRequirement', ''depends_on \'ruby\''')
-  end
+          bottle_ext && bottle_url_ext && bottle_ext != bottle_url_ext
 end
     
-      def send_sinatra_file(path, &missing_file_block)
-    file_path = File.join(File.dirname(__FILE__), 'public',  path)
-    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
-    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
-  end
-    
-          # Create an Atom-feed for each index.
-      feed = CategoryFeed.new(self, self.source, category_dir, category)
-      feed.render(self.layouts, site_payload)
-      feed.write(self.dest)
-      # Record the fact that this page has been added, otherwise Site::cleanup will remove it.
-      self.pages << feed
-    end
-    
-        def render(context)
-      code_dir = (context.registers[:site].config['code_dir'].sub(/^\//,'') || 'downloads/code')
-      code_path = (Pathname.new(context.registers[:site].source) + code_dir).expand_path
-      file = code_path + @file
-    
-      # Returns a url without the protocol (http://)
-  def shorthand_url(input)
-    input.gsub /(https?:\/\/)(\S+)/ do
-      $2
+      def describe_path(path)
+    return 'N/A' if path.nil?
+    realpath = path.realpath
+    if realpath == path
+      path
+    else
+      '#{path} => #{realpath}'
     end
   end
+    
+        def render(options, screenshots)
+      Dir.mktmpdir do |dir|
+        path = generator.render(options, screenshots, dir)
+        return File.read(path)
+      end
+    end
+  end
+    
+          it 'get HG revision number' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          get_build_number_repository(
+            use_hg_revision_number: true
+          )
+        end').runner.execute(:test)
+    
+          it 'pass a custom build number to the tool' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          increment_build_number(build_number: 24, xcodeproj: '.xcproject')
+        end').runner.execute(:test)
+    
+        if test_conf['database']&.present?
+      ActiveRecord::Base.establish_connection(:test)
+      yield
+      ActiveRecord::Base.establish_connection(Rails.env.to_sym)
+    end
+  end
+    
+      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/follow
+  def follow
+    f = Follow.last
+    NotificationMailer.follow(f.target_account, Notification.find_by(activity: f))
+  end
+    
+      attributes :name, :short_name, :description,
+             :icons, :theme_color, :background_color,
+             :display, :start_url, :scope,
+             :share_target
+    
+    require 'rails_helper'
+    
+          def handle_authorization_form(auth)
+        if auth
+          process_authorization_consent('true')
+        else
+          request_authorization_consent_form
+        end
+      end
+    
+          super
+      input = @options[:input]
+      if File.directory?(input)
+        raise 'Error: '#{input.path}' is a directory (did you mean to use --recursive?)'
+      end
+      output = @options[:output]
+      output = input if @options[:in_place]
+      process_file(input, output)
+    end
+    
+    if $PROGRAM_NAME == __FILE__ && !ENV['COCOAPODS_NO_BUNDLER']
+  ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
+  require 'rubygems'
+  require 'bundler/setup'
+  $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+elsif ENV['COCOAPODS_NO_BUNDLER']
+  require 'rubygems'
+  gem 'cocoapods'
+end
+    
+          def initialize(argv)
+        super
+        config.silent = false
+      end
+    
+          def call
+        title('Gems')
+        table(all_gem_names) do |gem, row|
+          row.yellow if update_available?(gem)
+          row << gem
+          row << installed_gem_version(gem)
+          row << '(update available)' if update_available?(gem)
+        end
+      end
+    
+        # @abstract
+    #
+    # Update the clone on the deployment target
+    #
+    # @return void
+    #
+    def update
+      raise NotImplementedError, 'Your SCM strategy module should provide a #update method'
+    end
+    
+      desc 'Publish the release.'
+  task :publishing do
+  end
+    
+    module RuboCop
+  module Cop
+    module Performance
+      # This cop identifies the use of `Regexp#match` or `String#match`, which
+      # returns `#<MatchData>`/`nil`. The return value of `=~` is an integral
+      # index/`nil` and is more performant.
+      #
+      # @example
+      #   # bad
+      #   do_something if str.match(/regex/)
+      #   while regex.match('str')
+      #     do_something
+      #   end
+      #
+      #   # good
+      #   method(str =~ /regex/)
+      #   return value unless regex =~ 'str'
+      class RedundantMatch < Cop
+        MSG = 'Use `=~` in places where the `MatchData` returned by ' \
+              '`#match` will not be used.'.freeze
+    
+            def on_block(node)
+          on_body_of_reduce(node) do |body|
+            void_next = body.each_node(:next).find do |n|
+              n.children.empty? && parent_block_node(n) == node
+            end
+    
+              [optarg_positions, arg_positions]
+        end
+      end
+    end
+  end
+end
+
+    
+        expect(corrected).to eq(''something'.to_sym')
+  end
+end
