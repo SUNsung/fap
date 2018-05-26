6e956ce6f7d4daf2b66a123ca5cb48f486f89693
SUNsung <weatherius@gmail.com>
2018-05-26 22:44:53 +0300

    Rewrite [04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py] File in: 2018-05-26 22:44:52

--- pyton/04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py
+++ pyton/04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py
@@ -1,77 +1,105 @@
 
         
-        
-def test_classify_inherit_class_build_fn():
-    class InheritClassBuildFnClf(KerasClassifier):
+        tf_kernel_library(
+    name = 'dragnn_op_kernels',
+    srcs = [
+        'ops/dragnn_op_kernels.cc',
+        'ops/dragnn_ops.cc',
+    ],
+    hdrs = [
+    ],
+    deps = [
+        ':compute_session',
+        ':compute_session_op',
+        ':compute_session_pool',
+        ':resource_container',
+        ':shape_helpers',
+        '//dragnn/core/util:label',
+        '//dragnn/protos:data_proto_cc',
+        '//dragnn/protos:spec_proto_cc',
+        '//syntaxnet:base',
+        '@org_tensorflow//third_party/eigen3',
+    ],
+)
     
     
-@keras_test
-def test_Bidirectional_with_constants_layer_passing_initial_state():
-    class RNNCellWithConstants(Layer):
-        def __init__(self, units, **kwargs):
-            self.units = units
-            self.state_size = units
-            super(RNNCellWithConstants, self).__init__(**kwargs)
+class MockNetwork(object):
     
-            # check that output changes after states are reset
-        # (even though the model itself didn't change)
-        layer.reset_states()
-        out3 = model.predict(np.ones((num_samples, timesteps)))
-        assert(out2.max() != out3.max())
+                              [[[  8,  19,  37],
+                            [ 18,  43,  85],
+                            [ 28,  67, 133]],
+                           [[ 27,  65, 131],
+                            [ 17,  41,  83],
+                            [  7,  17,  35]]])  # pyformat: disable
     
-        def test_batch_standardize(self):
-        # ImageDataGenerator.standardize should work on batches
-        for test_images in self.all_test_images:
-            img_list = []
-            for im in test_images:
-                img_list.append(image.img_to_array(im)[None, ...])
+      Returns:
+    Dict mapping from shortened resource path to original resource path.
+  '''
+  for component_spec in master_spec.component:
+    for feature_spec in component_spec.fixed_feature:
+      feature_spec.ClearField('pretrained_embedding_matrix')
+      feature_spec.ClearField('vocab')
     
-    def clean_pdf_link(link):
-    if 'arxiv' in link:
-        link = link.replace('abs', 'pdf')   
-        if not(link.endswith('.pdf')):
-            link = '.'.join((link, 'pdf'))
+          # Sanity check outputs.
+      for cell_output in cell_subgraph_spec.output:
+        self.assertGreater(len(cell_output.name), 0)
+        self.assertGreater(len(cell_output.tensor), 0)
+        restored_graph.get_tensor_by_name(cell_output.tensor)  # shouldn't raise
     
-        return difflib.unified_diff(old_report, new_report)
+        with server as (host, port):
+        url = 'http://{0}:{1}/'.format(host, port)
+        r = requests.get(url, auth=auth)
+        # Verify server succeeded in authenticating.
+        assert r.status_code == 200
+        # Verify Authorization was sent in final request.
+        assert 'Authorization' in r.request.headers
+        assert r.request.headers['Authorization'].startswith('Digest ')
+        # Verify redirect happened as we expected.
+        assert r.history[0].status_code == 302
+        close_server.set()
     
-        @require_oauth2_scope('creddits')
-    @validate(
-        VUser(),
-        user=VAccountByName('username'),
-        months=VInt('months', min=1, max=36),
-        timeout=VNotInTimeout(),
-    )
-    @api_doc(
-        api_section.gold,
-        uri='/api/v1/gold/give/{username}',
-    )
-    def POST_give(self, user, months, timeout):
-        self._gift_using_creddits(
-            recipient=user,
-            months=months,
-            proxying_for=request.POST.get('proxying_for'),
-        )
-
+        :rtype: str
+    '''
+    parts = uri.split('%')
+    for i in range(1, len(parts)):
+        h = parts[i][0:2]
+        if len(h) == 2 and h.isalnum():
+            try:
+                c = chr(int(h, 16))
+            except ValueError:
+                raise InvalidURL('Invalid percent-escape sequence: '%s'' % h)
     
-        @require_oauth2_scope('subscribe')
-    @validate(
-        VUser(),
-        friend_rel=VFriendOfMine('username'),
-    )
-    @api_doc(api_section.users, uri='/api/v1/me/friends/{username}')
-    def DELETE_friends(self, friend_rel):
-        '''Stop being friends with a user.'''
-        c.user.remove_friend(friend_rel._thing2)
-        if c.user.gold:
-            c.user.friend_rels_cache(_update=True)
-        response.status = 204
-
+    The multivariate normal or Gaussian distribution is a generalisation
+of the one-dimensional normal distribution to higher dimensions.
     
-        def rendercontent(self, input, fp):
-        soup = BeautifulSoup(input)
+    pc_expand_repr_doc = '''
+: boolean
+    Whether to print out the full DataFrame repr for wide DataFrames across
+    multiple lines, `max_columns` is still respected, but the output will
+    wrap-around across multiple 'pages' if its width exceeds `display.width`.
+'''
     
-        @validate(
-        container_id=VGTMContainerId('id')
-    )
-    def GET_jail(self, container_id):
-        return GoogleTagManagerJail(container_id=container_id).render()
+        def time_read_uint64_neg_values(self):
+        read_csv(self.data2, header=None, names=['foo'])
+    
+            result = cut(arr, 4)
+    
+            print('Test: General case, duplicates')
+        linked_list.insert_to_front(1)
+        linked_list.insert_to_front(1)
+        linked_list.insert_to_front(3)
+        linked_list.insert_to_front(2)
+        linked_list.insert_to_front(3)
+        linked_list.insert_to_front(1)
+        linked_list.insert_to_front(1)
+        linked_list.remove_dupes()
+        assert_equal(linked_list.get_all_data(), [1, 3, 2])
+    
+    
+def main():
+    num_stacks = 3
+    stack_size = 100
+    test = TestStacks()
+    test.test_pop_on_empty(num_stacks, stack_size)
+    test.test_push_on_full(num_stacks, stack_size)
+    test.test_stacks(num_stacks, stack_size)
