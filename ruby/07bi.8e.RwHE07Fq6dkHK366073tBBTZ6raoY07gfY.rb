5c533695d740969290bdeecb8cff4fd2e5cf8b6c
SUNsung <weatherius@gmail.com>
2018-05-26 06:26:24 +0300

    Rewrite [07bi.8e.RwHE07Fq6dkHK366073tBBTZ6raoY07gfY.rb] File in: 2018-05-26 06:26:24

--- ruby/07bi.8e.RwHE07Fq6dkHK366073tBBTZ6raoY07gfY.rb
+++ ruby/07bi.8e.RwHE07Fq6dkHK366073tBBTZ6raoY07gfY.rb
@@ -1,13 +1,75 @@
 
         
-        describe Spaceship::Portal::Key do
-  let(:mock_client) { double('MockClient') }
+              # Topic may be hard deleted due to spam, no point complaining
+      # we would have to look at the topics table id sequence to find cases
+      # where this was called with an invalid id, no point really
+      return unless topic.present?
     
-        export LANG=en_US.UTF-8
-    \e[0m
-    DOC
+        def execute(args)
+    
+        # The path used after resending confirmation instructions.
+    def after_resending_confirmation_instructions_path_for(resource_name)
+      is_navigational_format? ? new_session_path(resource_name) : '/'
+    end
+    
+        def email_changed(record, opts={})
+      devise_mail(record, :email_changed, opts)
+    end
+    
+          protected
+    
+      def inspect
+    '#<#{self.class.name}: #{strip.inspect} #{url.inspect}>'
+  end
+end
+    
+    class GitRequirement < Requirement
+  fatal true
+  satisfy do
+    odisabled('GitRequirement', ''depends_on \'git\''')
+  end
+end
+    
+          respond_with do |format|
+        format.html do
+          gon.preloads[:pods] = pods_json
+          gon.unchecked_count = Pod.unchecked.count
+          gon.version_failed_count = Pod.version_failed.count
+          gon.error_count = Pod.check_failed.count
+    
+      def create_hash(model, opts={})
+    opts[:range] ||= 1.day
+    plural = model.to_s.underscore.pluralize
+    eval(<<DATA
+      @#{plural} = {
+        :day_before => #{model}.where(:created_at => ((Time.now.midnight - #{opts[:range]*2})..Time.now.midnight - #{opts[:range]})).count,
+        :yesterday => #{model}.where(:created_at => ((Time.now.midnight - #{opts[:range]})..Time.now.midnight)).count
+      }
+      @#{plural}[:change] = percent_change(@#{plural}[:yesterday], @#{plural}[:day_before])
+DATA
+    )
   end
     
-          def plugins_string
-        UI::ErrorReport.plugins_string
+          def response_type_as_space_seperated_values
+        [*session[:response_type]].join(' ')
       end
+    
+          rescue_from Rack::OAuth2::Server::Authorize::BadRequest,
+                  JSON::JWT::InvalidFormat, JSON::JWK::UnknownAlgorithm do |e|
+        logger.info e.backtrace[0, 10].join('\n')
+        render json: {error: :invalid_request, error_description: e.message, status: 400}
+      end
+      rescue_from JSON::JWT::VerificationFailed do |e|
+        logger.info e.backtrace[0, 10].join('\n')
+        render json: {error: :invalid_grant, error_description: e.message, status: 400}
+      end
+    end
+  end
+end
+
+    
+            raise t('error.invalid_stage_name', name: invalid, path: stage_config_path.join('#{invalid}.rb'))
+      end
+    end
+  end
+end
