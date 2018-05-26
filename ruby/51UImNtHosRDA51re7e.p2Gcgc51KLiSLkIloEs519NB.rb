f2c36a35af4507e9f2a6cdb26255224366695a09
SUNsung <weatherius@gmail.com>
2018-05-26 13:13:51 +0300

    Create [51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb] File in: 2018-05-26 13:13:51

---
+++ ruby/51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb
@@ -0,0 +1,42 @@
+links.each do |link|
+  href = link.attribute('href').to_s
+  uri = URI.join(BASE_URI, href)
+  if map[uri]
+    dups <<  href
+  end
+  map[uri] = href
+end
+    
+      def dry_run?
+    !!@dry_run
+  end
+    
+      def retry_queued
+    @jobs = Delayed::Job.awaiting_retry.update_all(run_at: Time.zone.now)
+    
+    respond_to do |format|
+      format.html { redirect_to jobs_path, notice: 'Queued jobs getting retried.' }
+      format.json { head :no_content }
+    end
+  end
+    
+          # This returns the keys (or ids) that are in the string.
+      #
+      # @return [<Array<String>]
+      def keys
+        regexp = /^#\s*VAGRANT-BEGIN:\s*(.+?)$\r?\n?(.*)$\r?\n?^#\s*VAGRANT-END:\s(\1)$/m
+        @value.scan(regexp).map do |match|
+          match[0]
+        end
+      end
+    
+      it 'raises an ArgumentError if not passed a block' do
+    lambda {
+      Thread.send(@method)
+    }.should raise_error(ArgumentError)
+  end
+    
+          def initialize(argv)
+        super
+        config.silent = false
+      end
