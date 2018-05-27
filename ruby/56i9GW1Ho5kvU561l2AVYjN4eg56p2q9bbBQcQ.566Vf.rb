70ebd6922e549cc768aeda95d773f945ca8eb9d5
SUNsung <weatherius@gmail.com>
2018-05-27 12:50:45 +0300

    Rewrite [56i9GW1Ho5kvU561l2AVYjN4eg56p2q9bbBQcQ.566Vf.rb] File in: 2018-05-27 12:50:45

--- ruby/56i9GW1Ho5kvU561l2AVYjN4eg56p2q9bbBQcQ.566Vf.rb
+++ ruby/56i9GW1Ho5kvU561l2AVYjN4eg56p2q9bbBQcQ.566Vf.rb
@@ -1,115 +1,208 @@
 
         
-        unless File.exist?(PROF_OUTPUT_FILE)
-  StackProf.run(
-    mode: MODE.to_sym,
-    interval: 100,
-    raw: true,
-    out: PROF_OUTPUT_FILE
-  ) do
-    puts 'GC Stats:', JSON.pretty_generate(GC.stat)
-    GC.disable
+              https://pip.readthedocs.org/en/stable/installing/#install-pip
+    EOS
+  when 'pil' then <<-EOS.undent
+    Instead of PIL, consider `pip install pillow` or `brew install Homebrew/python/pillow`.
+    EOS
+  when 'macruby' then <<-EOS.undent
+    MacRuby works better when you install their package:
+      http://www.macruby.org/
+    EOS
+  when /(lib)?lzma/
+    'lzma is now part of the xz formula.'
+  when 'xcode'
+    if MacOS.version >= :lion
+      <<-EOS.undent
+      Xcode can be installed from the App Store.
+      EOS
+    else
+      <<-EOS.undent
+      Xcode can be installed from https://developer.apple.com/xcode/downloads/
+      EOS
+    end
+  when 'gtest', 'googletest', 'google-test' then <<-EOS.undent
+    Installing gtest system-wide is not recommended; it should be vendored
+    in your projects that use it.
+    EOS
+  when 'gmock', 'googlemock', 'google-mock' then <<-EOS.undent
+    Installing gmock system-wide is not recommended; it should be vendored
+    in your projects that use it.
+    EOS
+  when 'sshpass' then <<-EOS.undent
+    We won't add sshpass because it makes it too easy for novice SSH users to
+    ruin SSH's security.
+    EOS
+  when 'gsutil' then <<-EOS.undent
+    Install gsutil with `pip install gsutil`
+    EOS
+  when 'clojure' then <<-EOS.undent
+    Clojure isn't really a program but a library managed as part of a
+    project and Leiningen is the user interface to that library.
     
-    File.write(config, output)
-
+      # This allows generic Altivec PPC bottles to be supported in some
+  # formulae, while also allowing specific bottles in others; e.g.,
+  # sometimes a formula has just :tiger_altivec, other times it has
+  # :tiger_g4, :tiger_g5, etc.
+  def find_altivec_tag(tag)
+    if tag.to_s =~ /(\w+)_(g4|g4e|g5)$/
+      altivec_tag = '#{$1}_altivec'.to_sym
+      altivec_tag if key?(altivec_tag)
+    end
+  end
     
-          new_theme_name = args.join('_')
-      theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
-      if theme.path.exist?
-        Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.'
+    module Homebrew
+  def build_env_keys(env)
+    %w[
+      CC CXX LD OBJC OBJCXX
+      HOMEBREW_CC HOMEBREW_CXX
+      CFLAGS CXXFLAGS CPPFLAGS LDFLAGS SDKROOT MAKEFLAGS
+      CMAKE_PREFIX_PATH CMAKE_INCLUDE_PATH CMAKE_LIBRARY_PATH CMAKE_FRAMEWORK_PATH
+      MACOSX_DEPLOYMENT_TARGET PKG_CONFIG_PATH PKG_CONFIG_LIBDIR
+      HOMEBREW_DEBUG HOMEBREW_MAKE_JOBS HOMEBREW_VERBOSE
+      HOMEBREW_SVN HOMEBREW_GIT
+      HOMEBREW_SDKROOT HOMEBREW_BUILD_FROM_SOURCE
+      MAKE GIT CPP
+      ACLOCAL_PATH PATH CPATH].select { |key| env.key?(key) }
+  end
+    
+        def self.cleanup_lockfiles
+      return unless HOMEBREW_CACHE_FORMULA.directory?
+      candidates = HOMEBREW_CACHE_FORMULA.children
+      lockfiles  = candidates.select { |f| f.file? && f.extname == '.brewing' }
+      lockfiles.each do |file|
+        next unless file.readable?
+        file.open.flock(File::LOCK_EX | File::LOCK_NB) && file.unlink
+      end
+    end
+    
+        first_warning = true
+    methods.each do |method|
+      unless checks.respond_to?(method)
+        Homebrew.failed = true
+        puts 'No check available by the name: #{method}'
+        next
       end
     
-            parsed_expr = parse_expression(expression)
-        @context.stack do
-          groups = input.group_by do |item|
-            @context[variable] = item
-            parsed_expr.render(@context)
+          To export the needed variables, add them to your dotfiles.
+       * On Bash, add them to `~/.bash_profile`.
+       * On Zsh, add them to `~/.zprofile` instead.
+    
+    def check_link(uri)
+  HTTParty.head(uri, :verify => false).code.to_i.tap do |status|
+    if (400..422).include?(status)
+      if status != 403 && !uri.exclude?('udemy.com')
+        raise 'Request had status #{status}'
+      else
+        putc('S')
+      end
+    end
+  end
+end
+    
+    module Vagrant
+  module Plugin
+    module V2
+      # This class maintains a list of all the registered plugins as well
+      # as provides methods that allow querying all registered components of
+      # those plugins as a single unit.
+      class Manager
+        attr_reader :registered
+    
+          protected
+    
+    module VagrantPlugins
+  module CommandBox
+    module Command
+      class Repackage < Vagrant.plugin('2', :command)
+        def execute
+          opts = OptionParser.new do |o|
+            o.banner = 'Usage: vagrant box repackage <name> <provider> <version>'
           end
-          grouped_array(groups)
+    
+            opts = OptionParser.new do |o|
+          o.banner = 'Usage: vagrant cap [options] TYPE NAME [args]'
+          o.separator ''
+          o.separator 'This is an advanced command. If you don't know what this'
+          o.separator 'does and you aren't explicitly trying to use it, you probably'
+          o.separator 'don't want to use this.'
+          o.separator ''
+          o.separator 'This command checks or executes arbitrary capabilities that'
+          o.separator 'Vagrant has for hosts, guests, and providers.'
+          o.separator ''
+          o.separator 'Options:'
+          o.separator ''
+    
+    lib_path = root.join('lib').to_path
+    
+      fd  = File.open(out, 'a')
+    
+    
+# replace calls, jmps, and read/write handle/filename references
+replaces = []
+asm.each_line { |ln|
+	if ln =~ /call /
+		parts = ln.split(' ')
+		if (parts[0] == 'call' and parts[2] == ';call')
+			old = parts[1]
+			func = parts[3]
+			new = addrs[func]
+			#puts '%32s: %s -> %x' % [func, old, new]
+			replaces << [func, old, new.to_s(16)]
+		end
+	end
+    }
+    
+    classNames = [ 'HelloWorld1', 'HelloWorld2' ]
+    
+        # Remove directories opposite from traversal, so that a subtree with no
+    # actual files gets removed correctly.
+    dirs.reverse_each do |d|
+      if d.children.empty?
+        puts 'rmdir: #{d} (empty)' if ARGV.verbose?
+        d.rmdir
+      end
+    end
+    
+        if formulae_to_install.empty?
+      oh1 'No packages to upgrade'
+    else
+      oh1 'Upgrading #{Formatter.pluralize(formulae_to_install.length, 'outdated package')}, with result:'
+      formulae_upgrades = formulae_to_install.map do |f|
+        if f.optlinked?
+          '#{f.full_specified_name} #{Keg.new(f.opt_prefix).version} -> #{f.pkg_version}'
+        else
+          '#{f.full_specified_name} #{f.pkg_version}'
         end
       end
-    
-      def test_untainted_path
-    bug5374 = '[ruby-core:39745]'
-    cwd = ('./'*40+'.'.taint).dup.untaint
-    in_safe = proc {|safe| $SAFE = safe; File.stat(cwd)}
-    assert_not_send([cwd, :tainted?])
-    (0..1).each do |level|
-      assert_nothing_raised(SecurityError, bug5374) {in_safe[level]}
+      puts formulae_upgrades.join(', ')
     end
-  ensure
-    $SAFE = 0
-  end
     
-      it 'decodes the remaining doubles when passed the '*' modifier after another directive' do
-    array = '@\x15333333@\x22ffffff'.unpack(unpack_format()+unpack_format('*'))
-    array.should == [5.3, 9.2]
-  end
-    
-      it 'decodes the number of bytes specified by the count modifier including whitespace bytes' do
-    [ ['a bc',  ['a b', 'c']],
-      ['a\fbc', ['a\fb', 'c']],
-      ['a\nbc', ['a\nb', 'c']],
-      ['a\rbc', ['a\rb', 'c']],
-      ['a\tbc', ['a\tb', 'c']],
-      ['a\vbc', ['a\vb', 'c']]
-    ].should be_computed_by(:unpack, unpack_format(3)+unpack_format)
-  end
-    
-        t = Thread.new do
-      while true
-        break if exit_loop == true
-        Thread.pass
-      end
-    
-          it 'sets the regeneration marker to expire' do
-        allow(RegenerationWorker).to receive(:perform_async)
-        get :show
-        expect(Redis.current.ttl('account:#{user.account_id}:regeneration')).to be >= 0
-      end
-    
-      attr_reader :resource, :strip
-    
-        fi = FormulaInstaller.new(f)
-    fi.options = options
-    fi.build_bottle = ARGV.build_bottle? || (!f.bottled? && f.build.bottle?)
-    fi.installed_on_request = !ARGV.named.empty?
-    fi.link_keg           ||= keg_was_linked if keg_had_linked_opt
-    if tab
-      fi.installed_as_dependency = tab.installed_as_dependency
-      fi.installed_on_request  ||= tab.installed_on_request
-    end
-    fi.prelude
-    
-          def core_gem_names
-        %w(capistrano airbrussh rake sshkit net-ssh) & Gem.loaded_specs.keys
-      end
-    
-            raise t('error.invalid_stage_name', name: invalid, path: stage_config_path.join('#{invalid}.rb'))
+        def yielder
+      if instance_variable_defined?(:@satisfied)
+        @satisfied
+      elsif @options[:build_env]
+        require 'extend/ENV'
+        ENV.with_build_environment { yield @proc }
+      else
+        yield @proc
       end
     end
   end
-end
-
     
-      def send_sinatra_file(path, &missing_file_block)
-    file_path = File.join(File.dirname(__FILE__), 'public',  path)
-    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
-    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
+        @inner.log_level = log_level
+    Sass.logger = @inner
   end
     
-      if options.respond_to? 'keys'
-    options.each do |k,v|
-      unless v.nil?
-        v = v.join ',' if v.respond_to? 'join'
-        v = v.to_json if v.respond_to? 'keys'
-        output += ' data-#{k.sub'_','-'}='#{v}''
+            {
+          :always_update     => false,
+          :template_location => root + '/public/stylesheets/sass',
+          :css_location      => root + '/public/stylesheets',
+          :cache_location    => root + '/tmp/sass-cache',
+          :always_check      => env != 'production',
+          :quiet             => env != 'production',
+          :full_exception    => env != 'production'
+        }.freeze
       end
     end
-  elsif options.respond_to? 'join'
-    output += ' data-value='#{config[key].join(',')}''
-  else
-    output += ' data-value='#{config[key]}''
   end
-  output += '></#{tag}>'
-end
