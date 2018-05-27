cc64bbd4e2dad0a6f5d5369723b0a6eaa1787835
SUNsung <weatherius@gmail.com>
2018-05-27 18:23:22 +0300

    Rewrite [11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb] File in: 2018-05-27 18:23:21

--- ruby/11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb
+++ ruby/11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb
@@ -1,58 +1,92 @@
-      if new_path.length != old_path.length
-        new_path.length >= old_path.length
-      elsif new_scope.key?('type')
-        true
-      else
-        !old_scope.key? 'type'
-      end
+
+        
+          def evernote_oauth_token
+    service && service.token
+  end
+end
+
+    
+      def show
+    respond_to do |format|
+      format.html
+      format.json { render json: @event }
     end
-    # rubocop: enable PredicateName
+  end
     
-          def parse(content)
-        measure_time do
-          @template = Liquid::Template.parse(content, :line_numbers => true)
-        end
     
-      #forward some requests to status message, because a poll is just attached to a status message and is not sharable itself
-  delegate :author_id, :diaspora_handle, :public?, :subscribers, to: :status_message
-    
-    def config_tag(config, key, tag=nil, classname=nil)
-  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
-  tag       ||= 'div'
-  classname ||= key.sub(/_/, '-').sub(/\./, '-')
-  output      = '<#{tag} class='#{classname}''
-    
-      class ImageTag < Liquid::Tag
-    @img = nil
-    
-        def render(context)
-      output = super
-      types = {
-        '.mp4' => 'type='video/mp4; codecs=\'avc1.42E01E, mp4a.40.2\''',
-        '.ogv' => 'type='video/ogg; codecs=theora, vorbis'',
-        '.webm' => 'type='video/webm; codecs=vp8, vorbis''
-      }
-      if @videos.size > 0
-        video =  '<video #{sizes} preload='metadata' controls #{poster}>'
-        @videos.each do |v|
-          video << '<source src='#{v}' #{types[File.extname(v)]}>'
-        end
-        video += '</video>'
-      else
-        'Error processing input, expected syntax: {% video url/to/video [url/to/video] [url/to/video] [width height] [url/to/poster] %}'
+    {      delta = 1
+      stat = File.stat(path)
+      assert_in_delta tb,   stat.birthtime.to_f, delta
+      assert_in_delta t0+2, stat.mtime.to_f, delta
+      if stat.birthtime != stat.ctime
+        assert_in_delta t0+4, stat.ctime.to_f, delta
       end
+      if /mswin|mingw/ !~ RUBY_PLATFORM && !Bug::File::Fs.noatime?(path)
+        # Windows delays updating atime
+        assert_in_delta t0+6, stat.atime.to_f, delta
+      end
+    }
+  rescue NotImplementedError
+  end
+    
+      def test_select!
+    set = Set.new(1..10)
+    ret = set.select! { |i| i <= 10 }
+    assert_equal(nil, ret)
+    assert_equal(Set.new(1..10), set)
+    
+        def alive?
+      @alive
     end
     
-    module LogStash::Api::AppHelpers
-  # This method handle both of the normal flow *happy path*
-  # and the display or errors, if more custom logic is added here
-  # it will make sense to separate them.
-  #
-  # See `#error` method in the `LogStash::Api::Module::Base`
-  def respond_with(data, options={})
-    as     = options.fetch(:as, :json)
-    filter = options.fetch(:filter, '')
+      # This case occurred in JRuby where native threads are used to provide
+  # the same behavior as MRI green threads. Key to this issue was the fact
+  # that the thread which called #exit in its block was also being explicitly
+  # sent #join from outside the thread. The 100.times provides a certain
+  # probability that the deadlock will occur. It was sufficient to reliably
+  # reproduce the deadlock in JRuby.
+  it 'does not deadlock when called from within the thread while being joined from without' do
+    100.times do
+      t = Thread.new { Thread.stop; Thread.current.send(@method) }
+      Thread.pass while t.status and t.status != 'sleep'
+      t.wakeup.should == t
+      t.join.should == t
+    end
+  end
+end
+
     
-              def logstash_plugin_gem_spec?(spec)
-            spec.metadata && spec.metadata['logstash_plugin'] == 'true'
-          end
+      it 'interprets pre-Gregorian reform dates using Gregorian proleptic calendar' do
+    Time.send(@method, 1582, 10, 4, 12).to_i.should == -12220200000 # 2299150j
+  end
+    
+      def remote_url
+    object.remote_url.presence
+  end
+    
+        existence_maps = grouped_codes.map { |c| c.map { |cc| [cc, File.exist?(Rails.root.join('public', 'emoji', codepoints_to_filename(cc) + '.svg'))] }.to_h }
+    map = {}
+    
+    all_environments = [
+    :development,
+    :production,
+    :test
+]
+    
+      # Extract the form
+  res = '<form'
+  form.attributes.each do |attr|
+    res << ' #{attr[0]}='#{attr[1].gsub(''', '')}''
+  end
+  res << '> '
+    
+            # Remove it form the session objects so freeup
+        sessions.delete(s[:session])
+    
+    sock = TCPSocket.new(ip, port)
+    
+    	SIZE1 = 28
+	SIZE2 = 28 + 4 + 32
+	SIZE3 = 28 + 4 + 32 + 4
+    
+        private
