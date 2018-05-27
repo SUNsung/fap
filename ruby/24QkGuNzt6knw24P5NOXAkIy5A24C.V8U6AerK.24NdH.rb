361b97b70a0c75cfead8091db3ebabcfbf351e39
SUNsung <weatherius@gmail.com>
2018-05-27 04:38:42 +0300

    Rewrite [24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb] File in: 2018-05-27 04:38:42

--- ruby/24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb
+++ ruby/24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb
@@ -1,23 +1,106 @@
 
         
-                return if type.nil?
-        return unless Hbc::Container.from_type(type).nil?
-        raise 'invalid container type: #{type.inspect}'
-      end
+          def app_caveats
+    if keg && keg.app_installed?
+      <<-EOS.undent
+        .app bundles were installed.
+        Run `brew linkapps #{keg.name}` to symlink these to /Applications.
+      EOS
+    end
+  end
     
-    module Homebrew
-  module_function
+        # Many formulae include 'lib/charset.alias', but it is not strictly needed
+    # and will conflict if more than one formula provides it
+    observe_file_removal @f.lib/'charset.alias'
+    
+      def internal_commands
+    find_internal_commands HOMEBREW_LIBRARY_PATH/'cmd'
+  end
+    
+      def llvm
+    @llvm ||= MacOS.llvm_build_version if MacOS.has_apple_developer_tools?
+  end
+    
+          out = checks.send(method)
+      unless out.nil? || out.empty?
+        if first_warning
+          $stderr.puts <<-EOS.undent
+            #{Tty.white}Please note that these warnings are just used to help the Homebrew maintainers
+            with debugging if you file an issue. If everything you use Homebrew for is
+            working fine: please don't worry and just ignore them. Thanks!#{Tty.reset}
+          EOS
+        end
+    
+      def dump_formula_report(key, title)
+    formulae = select_formula(key).sort.map do |name, new_name|
+      # Format list items of renamed formulae
+      if key == :R
+        name = pretty_installed(name) if installed?(name)
+        new_name = pretty_installed(new_name) if installed?(new_name)
+        '#{name} -> #{new_name}'
+      else
+        installed?(name) ? pretty_installed(name) : name
+      end
+    end
+    
+      # Use this method to generate standard caveats.
+  def standard_instructions(home_name, home_value = libexec)
+    <<-EOS.undent
+      Before you can use these tools you must export some variables to your $SHELL.
+    
+          html_filters.push 'coffeescript/clean_html_v1', 'coffeescript/entries_v1', 'title'
+    
+        version '8 LTS' do
+      self.release = '8.11.1'
+      self.base_url = 'https://nodejs.org/dist/latest-v8.x/docs/api/'
+    end
+    
+        options[:max_image_size] = 300_000
+    options[:container] = '.devsite-main-content'
     
         private
     
-          # If the importer is based on files on the local filesystem
-      # this method should return folders which should be watched
-      # for changes.
-      #
-      # @return [Array<String>] List of absolute paths of directories to watch
-      def directories_to_watch
-        []
-      end
+      def test_chmod_m17n
+    bug5671 = '[ruby-dev:44898]'
+    Dir.mktmpdir('test-file-chmod-m17n-') do |tmpdir|
+      file = File.join(tmpdir, '\u3042')
+      File.open(file, 'w'){}
+      assert_equal(File.chmod(0666, file), 1, bug5671)
+    end
+  end
+    
+      def test_eql
+    assert_not_send([@cls[], :eql?, 0])
+    o = Object.new
+    o.instance_variable_set(:@cls, @cls)
+    def o.to_hash; @cls[]; end
+    def o.eql?(x); true; end
+    assert_send([@cls[], :eql?, o])
+    def o.eql?(x); false; end
+    assert_not_send([@cls[], :eql?, o])
+  end
+    
+      it 'implicitly has a count of one when no count is specified' do
+    'abc'.unpack(unpack_format).should == ['a']
+  end
+    
+      it 'creates a time based on given C-style gmtime arguments, interpreted as UTC (GMT)' do
+    time = Time.send(@method, 1, 15, 20, 1, 1, 2000, :ignored, :ignored, :ignored, :ignored)
+    time.inspect.should == '2000-01-01 20:15:01 UTC'
+  end
+    
+    def usage
+  <<-EOS
+Usage: list_login_items_for_app <path.app>
+    
+    def report_apps
+  running = Set.new
+  @app_names.zip(@bundle_ids, @unix_ids).each do |app_name, bundle_id, _unix_id|
+    next if excluded_bundle_id bundle_id
+    next if excluded_app_name app_name
+    bundle_id.gsub!(%r{^(missing value)$}, '<\1>')
+    running.add '#{bundle_id}\t#{app_name}'
+  end
     
       module Sass::Plugin::Configuration
     # Different default options in a m environment.
@@ -31,3 +114,25 @@
           root = Merb.root.to_s
           env  = Merb.environment
         end
+    
+    module Sass
+  # Runs a SassScript read-eval-print loop.
+  # It presents a prompt on the terminal,
+  # reads in SassScript expressions,
+  # evaluates them,
+  # and prints the result.
+  class Repl
+    # @param options [{Symbol => Object}] An options hash.
+    def initialize(options = {})
+      @options = options
+    end
+    
+    # The project root directory
+$root = ::File.dirname(__FILE__)
+    
+      def render(context)
+    config_tag(context.registers[:site].config, @key, @tag, @classname)
+  end
+end
+    
+    Liquid::Template.register_tag('include_array', Jekyll::IncludeArrayTag)
