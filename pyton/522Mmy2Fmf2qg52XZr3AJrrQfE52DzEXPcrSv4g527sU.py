7edbf7c7b2bfc7851681519a8ba3746ce8ea19ed
SUNsung <weatherius@gmail.com>
2018-05-27 21:13:18 +0300

    Rewrite [522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py] File in: 2018-05-27 21:13:17

--- pyton/522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py
+++ pyton/522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py
@@ -1,291 +1,105 @@
 
         
-        with io.open(README_FILE, encoding='utf-8') as f:
-    oldreadme = f.read()
+            def reject_friend_request(self, from_user_id, to_user_id):
+        pass
     
-    import shutil
+    def evalString(s):
+    assert s.startswith(''') or s.startswith('''), repr(s[:1])
+    q = s[0]
+    if s[:3] == q*3:
+        q = q*3
+    assert s.endswith(q), repr(s[-len(q):])
+    assert len(s) >= 2*len(q)
+    s = s[len(q):-len(q)]
+    return re.sub(r'\\(\'|\'|\\|[abfnrtv]|x.{0,2}|[0-7]{1,3})', escape, s)
     
-    from test.helper import (
-    FakeYDL,
-    get_params,
-)
-from youtube_dl.compat import (
-    compat_str,
-    compat_urllib_request,
-)
+        def __setitem__(self, key, message):
+        '''Replace the keyed message; raise KeyError if it doesn't exist.'''
+        old_subpath = self._lookup(key)
+        temp_key = self.add(message)
+        temp_subpath = self._lookup(temp_key)
+        if isinstance(message, MaildirMessage):
+            # temp's subdir and suffix were specified by message.
+            dominant_subpath = temp_subpath
+        else:
+            # temp's subdir and suffix were defaults from add().
+            dominant_subpath = old_subpath
+        subdir = os.path.dirname(dominant_subpath)
+        if self.colon in dominant_subpath:
+            suffix = self.colon + dominant_subpath.split(self.colon)[-1]
+        else:
+            suffix = ''
+        self.discard(key)
+        tmp_path = os.path.join(self._path, temp_subpath)
+        new_path = os.path.join(self._path, subdir, key + suffix)
+        if isinstance(message, MaildirMessage):
+            os.utime(tmp_path,
+                     (os.path.getatime(tmp_path), message.get_date()))
+        # No file modification should be done after the file is moved to its
+        # final position in order to prevent race conditions with changes
+        # from other programs
+        os.rename(tmp_path, new_path)
+    
+        print('$(temp_dir):')
+    print(r'  if not exist $(temp_dir)\. mkdir $(temp_dir)')
+    print()
     
     
-class AudiomackIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?audiomack\.com/song/(?P<id>[\w/-]+)'
-    IE_NAME = 'audiomack'
-    _TESTS = [
-        # hosted on audiomack
-        {
-            'url': 'http://www.audiomack.com/song/roosh-williams/extraordinary',
-            'info_dict':
-            {
-                'id': '310086',
-                'ext': 'mp3',
-                'uploader': 'Roosh Williams',
-                'title': 'Extraordinary'
-            }
-        },
-        # audiomack wrapper around soundcloud song
-        {
-            'add_ie': ['Soundcloud'],
-            'url': 'http://www.audiomack.com/song/hip-hop-daily/black-mamba-freestyle',
-            'info_dict': {
-                'id': '258901379',
-                'ext': 'mp3',
-                'description': 'mamba day freestyle for the legend Kobe Bryant ',
-                'title': 'Black Mamba Freestyle [Prod. By Danny Wolf]',
-                'uploader': 'ILOVEMAKONNEN',
-                'upload_date': '20160414',
-            }
-        },
-    ]
+def triplet_to_brightness(rgbtuple):
+    # return the brightness (grey level) along the scale 0.0==black to
+    # 1.0==white
+    r = 0.299
+    g = 0.587
+    b = 0.114
+    return r*rgbtuple[0] + g*rgbtuple[1] + b*rgbtuple[2]
     
+        def docserver(self, server_name, package_documentation, methods):
+        '''Produce HTML documentation for an XML-RPC server.'''
     
-class ClipRsIE(OnetBaseIE):
-    _VALID_URL = r'https?://(?:www\.)?clip\.rs/(?P<id>[^/]+)/\d+'
-    _TEST = {
-        'url': 'http://www.clip.rs/premijera-frajle-predstavljaju-novi-spot-za-pesmu-moli-me-moli/3732',
-        'md5': 'c412d57815ba07b56f9edc7b5d6a14e5',
-        'info_dict': {
-            'id': '1488842.1399140381',
-            'ext': 'mp4',
-            'title': 'PREMIJERA Frajle predstavljaju novi spot za pesmu Moli me, moli',
-            'description': 'md5:56ce2c3b4ab31c5a2e0b17cb9a453026',
-            'duration': 229,
-            'timestamp': 1459850243,
-            'upload_date': '20160405',
-        }
+    def set_memlimit(limit):
+    global max_memuse
+    global real_max_memuse
+    sizes = {
+        'k': 1024,
+        'm': _1M,
+        'g': _1G,
+        't': 1024*_1G,
     }
+    m = re.match(r'(\d+(\.\d+)?) (K|M|G|T)b?$', limit,
+                 re.IGNORECASE | re.VERBOSE)
+    if m is None:
+        raise ValueError('Invalid memory limit %r' % (limit,))
+    memlimit = int(float(m.group(1)) * sizes[m.group(3).lower()])
+    real_max_memuse = memlimit
+    if memlimit > MAX_Py_ssize_t:
+        memlimit = MAX_Py_ssize_t
+    if memlimit < _2G - 1:
+        raise ValueError('Memory limit %r too low to be useful' % (limit,))
+    max_memuse = memlimit
     
-    import re
+        def test_mkdir_no_parents_file(self):
+        p = self.cls(BASE, 'fileA')
+        self.assertTrue(p.exists())
+        # An exception is raised when the last path component is an existing
+        # regular file, regardless of whether exist_ok is true or not.
+        with self.assertRaises(FileExistsError) as cm:
+            p.mkdir()
+        self.assertEqual(cm.exception.errno, errno.EEXIST)
+        with self.assertRaises(FileExistsError) as cm:
+            p.mkdir(exist_ok=True)
+        self.assertEqual(cm.exception.errno, errno.EEXIST)
     
-    cc_library(
-    name = 'compute_session_impl',
-    srcs = ['compute_session_impl.cc'],
-    hdrs = ['compute_session_impl.h'],
-    deps = [
-        ':compute_session',
-        ':index_translator',
-        ':input_batch_cache',
-        '//dragnn/components/util:bulk_feature_extractor',
-        '//dragnn/core/util:label',
-        '//dragnn/protos:data_proto_cc',
-        '//dragnn/protos:spec_proto_cc',
-        '//dragnn/protos:trace_proto_cc',
-        '//syntaxnet:base',
-        '//syntaxnet:registry',
-    ],
-)
+        def testWriteText(self): pass
     
-      def _feedforward_unit(self, state, arrays, network_states, stride,
-                        during_training):
-    '''Constructs a single instance of a feed-forward cell.
-    
-    '''Tests for digraph ops.'''
-    
-        with tf.Session(graph=restored_graph, config=restoration_config) as sess:
-      tf.saved_model.loader.load(sess, [tf.saved_model.tag_constants.SERVING],
-                                 export_path)
-    
-      def _get_compute_session(self):
-    '''Returns a new ComputeSession handle.'''
-    return dragnn_ops.get_session(
-        self.pool_scope,
-        master_spec=self.spec.SerializeToString(),
-        grid_point=self.hyperparams.SerializeToString(),
-        name='GetSession')
-    
-      def testBuildLexicon(self):
-    empty_input_path = os.path.join(test_flags.temp_dir(), 'empty-input')
-    lexicon_output_path = os.path.join(test_flags.temp_dir(), 'lexicon-output')
-    
-    
-@pytest.fixture
-def httpbin_secure(httpbin_secure):
-    return prepare_url(httpbin_secure)
+        def createDOMInputSource(self):
+        return DOMInputSource()
 
     
-        def run(self):
-        try:
-            self.server_sock = self._create_socket_and_bind()
-            # in case self.port = 0
-            self.port = self.server_sock.getsockname()[1]
-            self.ready_event.set()
-            self._handle_requests()
-    
-        def test_copy(self):
-        copy = self.case_insensitive_dict.copy()
-        assert copy is not self.case_insensitive_dict
-        assert copy == self.case_insensitive_dict
-    
-        def test_request_recovery_with_bigger_timeout(self):
-        '''a biggest timeout can be specified'''
-        server = Server.basic_response_server(request_timeout=3)
-        data = b'bananadine'
-    
-        builtin_str = str
-    str = str
-    bytes = bytes
-    basestring = (str, bytes)
-    numeric_types = (int, float)
-    integer_types = (int,)
-
-    
-    
-class InvalidProxyURL(InvalidURL):
-    '''The proxy URL provided is invalid.'''
-    
-        @pytest.mark.parametrize(
-        'value', (
-            '8.8.8.8',
-            '192.168.1.0/a',
-            '192.168.1.0/128',
-            '192.168.1.0/-1',
-            '192.168.1.999/24',
-        ))
-    def test_invalid(self, value):
-        assert not is_valid_cidr(value)
-    
-    class _WorkItem(object):
-    def __init__(self, future, fn, args, kwargs):
-        self.future = future
-        self.fn = fn
-        self.args = args
-        self.kwargs = kwargs
-    
-    class _WorkItem(object):
-    def __init__(self, future, fn, args, kwargs):
-        self.future = future
-        self.fn = fn
-        self.args = args
-        self.kwargs = kwargs
-    
-    
-  def IsAllowed( self, diagnostic ):
-    # NOTE: a diagnostic IsAllowed() ONLY if NO filters match it
-    for filterMatches in self._filters:
-      if filterMatches( diagnostic ):
-        return False
-    
-    
-def FormatDebugInfoResponse_Completer_ServerRunningWithoutHost_test():
-  response = deepcopy( GENERIC_RESPONSE )
-  response[ 'completer' ][ 'servers' ][ 0 ].update( {
-    'address': None,
-    'port': None
-  } )
-  assert_that(
-    FormatDebugInfoResponse( response ),
-    contains_string(
-      'Completer name completer debug information:\n'
-      '  Server name running\n'
-      '  Server name process ID: 12345\n'
-      '  Server name executable: /path/to/executable\n'
-      '  Server name logfiles:\n'
-      '    /path/to/stdout/logfile\n'
-      '    /path/to/stderr/logfile\n'
-      '  Server name key: value\n'
-      '  Key: value\n'
-    )
-  )
-    
-    
-def KeywordsFromSyntaxListOutput_PythonSyntax_test():
-  expected_keywords = (
-    'bytearray', 'IndexError', 'all', 'help', 'vars', 'SyntaxError', 'global',
-    'elif', 'unicode', 'sorted', 'memoryview', 'isinstance', 'except',
-    'nonlocal', 'NameError', 'finally', 'BytesWarning', 'dict', 'IOError',
-    'pass', 'oct', 'bin', 'SystemExit', 'return', 'StandardError', 'format',
-    'TabError', 'break', 'next', 'not', 'UnicodeDecodeError', 'False',
-    'RuntimeWarning', 'list', 'iter', 'try', 'reload', 'Warning', 'round',
-    'dir', 'cmp', 'set', 'bytes', 'UnicodeTranslateError', 'intern',
-    'issubclass', 'yield', 'Ellipsis', 'hash', 'locals', 'BufferError',
-    'slice', 'for', 'FloatingPointError', 'sum', 'VMSError', 'getattr', 'abs',
-    'print', 'import', 'True', 'FutureWarning', 'ImportWarning', 'None',
-    'EOFError', 'len', 'frozenset', 'ord', 'super', 'raise', 'TypeError',
-    'KeyboardInterrupt', 'UserWarning', 'filter', 'range', 'staticmethod',
-    'SystemError', 'or', 'BaseException', 'pow', 'RuntimeError', 'float',
-    'MemoryError', 'StopIteration', 'globals', 'divmod', 'enumerate', 'apply',
-    'LookupError', 'open', 'basestring', 'from', 'UnicodeError', 'zip', 'hex',
-    'long', 'IndentationError', 'int', 'chr', '__import__', 'type',
-    'Exception', 'continue', 'tuple', 'reduce', 'reversed', 'else', 'assert',
-    'UnicodeEncodeError', 'input', 'with', 'hasattr', 'delattr', 'setattr',
-    'raw_input', 'PendingDeprecationWarning', 'compile', 'ArithmeticError',
-    'while', 'del', 'str', 'property', 'def', 'and', 'GeneratorExit',
-    'ImportError', 'xrange', 'is', 'EnvironmentError', 'KeyError', 'coerce',
-    'SyntaxWarning', 'file', 'in', 'unichr', 'ascii', 'any', 'as', 'if',
-    'OSError', 'DeprecationWarning', 'min', 'UnicodeWarning', 'execfile', 'id',
-    'complex', 'bool', 'ValueError', 'NotImplemented', 'map', 'exec', 'buffer',
-    'max', 'class', 'object', 'repr', 'callable', 'ZeroDivisionError', 'eval',
-    '__debug__', 'ReferenceError', 'AssertionError', 'classmethod',
-    'UnboundLocalError', 'NotImplementedError', 'lambda', 'AttributeError',
-    'OverflowError', 'WindowsError' )
-    
-    
-def AdjustCandidateInsertionText_NothingAfterCursor_test():
-  with MockTextAfterCursor( '' ):
-    eq_( [ 'foofoo',
-           'zobar' ],
-         base.AdjustCandidateInsertionText( [ 'foofoo',
-                                              'zobar' ] ) )
-    
-    
-def OverlapLength( left_string, right_string ):
-  '''Returns the length of the overlap between two strings.
-  Example: 'foo baro' and 'baro zoo' -> 4
-  '''
-  left_string_length = len( left_string )
-  right_string_length = len( right_string )
-    
-    
-def Done_AlwaysTrue_test():
-  request = BuildOmnicompletionRequest( [] )
-    
-      post_vim_message.assert_has_exact_calls( [
-    call( 'On the first day of Christmas, my VimScript gave to me',
-          warning=False,
-          truncate=True ),
-    call( 'A test file in a Command-T', warning=False, truncate=True ),
-    call( 'On the second day of Christmas, my VimScript gave to me',
-          warning=False,
-          truncate=True ),
-    call( 'Two popup menus, and a test file in a Command-T',
-          warning=False,
-          truncate=True ),
-  ] )
-
-    
-        def test_car_adapter_shall_make_very_loud_noise(self):
-        car = Car()
-        car_adapter = Adapter(car, make_noise=car.make_noise)
-        noise = car_adapter.make_noise(10)
-        expected_noise = 'vroom!!!!!!!!!!'
-    
-        def test_sequential_execution(self):
-        self.command_stack[0].execute()
-        output_after_first_execution = os.listdir(self.test_dir)
-        self.assertEqual(output_after_first_execution[0], 'bar.txt')
-        self.command_stack[1].execute()
-        output_after_second_execution = os.listdir(self.test_dir)
-        self.assertEqual(output_after_second_execution[0], 'baz.txt')
-    
-        def test_cat_greek_localization(self):
-        self.assertEqual(self.g.get('cat'), 'γάτα')
-    
-        def test_display_current_time_at_midnight(self):
-        '''
-        Would almost always fail (despite of right at/after midnight) if
-        untestable production code would have been used.
-        '''
-        time_provider_stub = MidnightTimeProvider()
-        class_under_test = TimeDisplay()
-        class_under_test.set_time_provider(time_provider_stub)
-        expected_time = '<span class=\'tinyBoldText\'>24:01</span>'
-        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
-    
-    from dft.constructor_injection import TimeDisplay, MidnightTimeProvider, ProductionCodeTimeProvider, datetime
+        def test_stderr_none(self):
+        rc, stdout, stderr = assert_python_ok('-c',
+            'import sys; sys.stderr = None; '
+            'import warnings; warnings.simplefilter('always'); '
+            'warnings.warn('Warning!')')
+        self.assertEqual(stdout, b'')
+        self.assertNotIn(b'Warning!', stderr)
+        self.assertNotIn(b'Error', stderr)
