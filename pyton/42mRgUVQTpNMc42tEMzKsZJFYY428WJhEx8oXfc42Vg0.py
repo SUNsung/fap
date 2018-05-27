e0dd6eda858f579862254f061679b831fdde37ec
SUNsung <weatherius@gmail.com>
2018-05-27 22:55:58 +0300

    Rewrite [42mRgUVQTpNMc42tEMzKsZJFYY428WJhEx8oXfc42Vg0.py] File in: 2018-05-27 22:55:57

--- pyton/42mRgUVQTpNMc42tEMzKsZJFYY428WJhEx8oXfc42Vg0.py
+++ pyton/42mRgUVQTpNMc42tEMzKsZJFYY428WJhEx8oXfc42Vg0.py
@@ -1,55 +1,101 @@
 
         
-            exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR
-    assert error_msg == (
-        'ConnectionError: '
-        'Connection aborted while doing GET request to URL: '
-        'http://www.google.com')
+            :copyright: © 2010 by the Pallets team.
+    :license: BSD, see LICENSE for more details.
+'''
     
-            for response in responses:
+        class Loader(object):
+        def find_module(self, name, path=None):
+            return self
     
+      The D&M parser uses two MLPs to create two activation vectors for each token,
+  which represent the token when it it used as the source or target of an arc.
+  Arcs are scored using a 'biaffine' function that includes a bilinear and
+  linear term:
     
-def get_filename_max_length(directory):
-    max_len = 255
-    try:
-        pathconf = os.pathconf
-    except AttributeError:
-        pass  # non-posix
-    else:
+          with tf.variable_scope(component.name, reuse=True):
+        self.assertEqual(len(component.network.derived_params), 2)
+    
+        Does nothing if master.build_runtime_graph is False.  Subclasses should call
+    this at the end of build_*_inference().  For details on the runtime hooks,
+    see runtime_support.py.
+    '''
+    if self.master.build_runtime_graph:
+      with tf.variable_scope(self.name, reuse=True):
+        runtime_support.add_hooks(self, self._cell_subgraph_spec)
+      self._cell_subgraph_spec = None  # prevent further exports
+    
+      def testSigmoidCrossEntropyLoss(self):
+    indices = tf.constant([0, 0, 1])
+    gold_labels = tf.constant([0, 1, 2])
+    probs = tf.constant([0.6, 0.7, 0.2])
+    logits = tf.constant([[0.9, -0.3, 0.1], [-0.5, 0.4, 2.0]])
+    cost, correct, total, gold_labels = (
+        component.build_sigmoid_cross_entropy_loss(logits, gold_labels, indices,
+                                                   probs))
+    
+        with tf.Session(graph=restored_graph, config=restoration_config) as sess:
+      tf.saved_model.loader.load(sess, [tf.saved_model.tag_constants.SERVING],
+                                 export_path)
+    
         try:
-            max_len = pathconf(directory, 'PC_NAME_MAX')
-        except OSError as e:
-            if e.errno != errno.EINVAL:
-                raise
-    return max_len
+      with open(FLAGS.expected_file) as expected:
+        content_expected = expected.read()
+    except IOError as e:
+      self.fail('Error opening '%s': %s' % (FLAGS.expected_file, e.strerror))
     
-            Return a ``requests.auth.AuthBase`` subclass instance.
     
-        @pytest.mark.parametrize('verify_value', ['false', 'fALse'])
-    def test_verify_false_OK(self, httpbin_secure, verify_value):
-        r = http(httpbin_secure.url + '/get', '--verify', verify_value)
-        assert HTTP_OK in r
+class Professor(AbstractExpert):
     
-        @property
-    def path(self):
-        '''Return the config file path creating basedir, if needed.'''
-        path = self._get_path()
-        try:
-            os.makedirs(os.path.dirname(path), mode=0o700)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
-        return path
+        def on_switchover(self):
+        raise UnsupportedTransition
     
-        def copy_certs_and_keys(self, cert_path, key_path, chain_path=None):
-        '''Copies certs and keys into the temporary directory'''
-        cert_and_key_dir = os.path.join(self._temp_dir, 'certs_and_keys')
-        if not os.path.isdir(cert_and_key_dir):
-            os.mkdir(cert_and_key_dir)
+        def test_human_adapter_shall_make_noise(self):
+        human = Human()
+        human_adapter = Adapter(human, make_noise=human.speak)
+        noise = human_adapter.make_noise()
+        expected_noise = ''hello''
+        self.assertEqual(noise, expected_noise)
     
-        # Implement all methods from IInstaller, remembering to add
-    # 'self' as first argument, e.g. def get_all_names(self)...
+        def test_sequential_undo(self):
+        self.command_stack = list(reversed(self.command_stack))
+        self.command_stack[0].undo()
+        output_after_first_undo = os.listdir(self.test_dir)
+        self.assertEqual(output_after_first_undo[0], 'bar.txt')
+        self.command_stack[1].undo()
+        output_after_second_undo = os.listdir(self.test_dir)
+        self.assertEqual(output_after_second_undo[0], 'foo.txt')
+    
+        def test_parrot_eng_localization(self):
+        self.assertEqual(self.e.get('parrot'), 'parrot')
+    
+        def test_am_station_overflow_after_scan(self):
+        self.radio.scan()
+        station = self.radio.state.stations[self.radio.state.pos]
+        expected_station = '1250'
+        self.assertEqual(station, expected_station)
+    
+    class TimeDisplay(object):
+    
+            depending on self.param value
+        '''
+        self._static_method_choices[self.param].__get__(None, self.__class__)()
+    
+        def undo(self):
+        self.rename(self.dest, self.src)
+    
+        print(u'Setting Data 1 = 10')
+    data1.data = 10
+    print(u'Setting Data 2 = 15')
+    data2.data = 15
+    print(u'Setting Data 1 = 3')
+    data1.data = 3
+    print(u'Setting Data 2 = 5')
+    data2.data = 5
+    print(u'Detach HexViewer from data1 and data2.')
+    data1.detach(view2)
+    data2.detach(view2)
+    print(u'Setting Data 1 = 10')
+    data1.data = 10
+    print(u'Setting Data 2 = 15')
+    data2.data = 15
