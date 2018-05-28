96d8e89199d10044009b5fecc2203268c91a80d1
SUNsung <weatherius@gmail.com>
2018-05-28 12:25:54 +0300

    Rewrite [56L6S30E224Vw56oLDuUT9rTCQ562FgL6HV0yd656X2I.py] File in: 2018-05-28 12:25:54

--- pyton/56L6S30E224Vw56oLDuUT9rTCQ562FgL6HV0yd656X2I.py
+++ pyton/56L6S30E224Vw56oLDuUT9rTCQ562FgL6HV0yd656X2I.py
@@ -1,190 +1,105 @@
 
         
-        def main():
-    # First, we load the current README into memory as an array of lines
-    with open('README.md', 'r') as read_me_file:
-        read_me = read_me_file.readlines()
+            exec('def reraise(tp, value, tb=None):\n raise tp, value, tb')
     
-    entries = []
-for v in versions:
-    fields = v.split('.')
-    year, month, day = map(int, fields[:3])
-    faked = 0
-    patchlevel = 0
-    while True:
-        try:
-            datetime.date(year, month, day)
-        except ValueError:
-            day -= 1
-            faked += 1
-            assert day > 0
+        #: When data is changed, this is set to ``True``. Only the session
+    #: dictionary itself is tracked; if the session contains mutable
+    #: data (for example a nested dict) then this must be set to
+    #: ``True`` manually when modifying that data. The session cookie
+    #: will only be written to the response if this is ``True``.
+    modified = False
+    
+                # If we have no method at all in there we don't want to add a
+            # method list. This is for instance the case for the base class
+            # or another subclass of a base method view that does not introduce
+            # new methods.
+            if methods:
+                cls.methods = methods
+    
+    
+@pytest.mark.skipif(greenlet is None, reason='greenlet not installed')
+class TestGreenletContextCopying(object):
+    
+        # The directory may already exist when running locally multiple times.
+    if not os.path.exists(lexicon_output_path):
+      os.mkdir(lexicon_output_path)
+    
+            self.assertAlmostEqual(tf.exp(log_partition_functions[0]).eval(), 2.0)
+        self.assertAlmostEqual(
+            tf.exp(log_partition_functions[1]).eval(), 3.0 * 5.0)
+        self.assertAlmostEqual(
+            tf.exp(log_partition_functions[2]).eval(), 7.0 * 11.0 * 13.0)
+    
+          # Add hooks.  This should ignore channel 0 and add hooks for channel 1.
+      with tf.variable_scope(component.name, reuse=True):
+        runtime_support.add_hooks(component, export_pb2.CellSubgraphSpec())
+    
+      def testReadAndProjectivize(self):
+    reader = sentence_io.ConllSentenceReader(
+        self.filepath, self.batch_size, projectivize=True)
+    self.assertParseable(reader, self.batch_size, False)
+    self.assertParseable(reader, self.batch_size, False)
+    self.assertParseable(reader, 14, True)
+    self.assertParseable(reader, 0, True)
+    self.assertParseable(reader, 0, True)
+    
+      Arguments:
+    sess: TF session to use.
+    trainers: List of training ops to use.
+    annotator: Annotation op.
+    evaluator: Function taking two serialized corpora and returning a dict of
+      scalar summaries representing evaluation metrics. The 'eval_metric'
+      summary will be used for early stopping.
+    pretrain_steps: List of the no. of pre-training steps for each train op.
+    train_steps: List of the total no. of steps for each train op.
+    train_corpus: Training corpus to use.
+    eval_corpus: Holdout Corpus for early stoping.
+    eval_gold: Reference of eval_corpus for computing accuracy.
+      eval_corpus and eval_gold are allowed to be the same if eval_corpus
+      already contains gold annotation.
+      Note for segmentation eval_corpus and eval_gold are always different since
+      eval_corpus contains sentences whose tokens are utf8-characters while
+      eval_gold's tokens are gold words.
+    batch_size: How many examples to send to the train op at a time.
+    summary_writer: TF SummaryWriter to use to write summaries.
+    report_every: How often to compute summaries (in steps).
+    saver: TF saver op to save variables.
+    checkpoint_filename: File to save checkpoints to.
+    checkpoint_stats: Stats of checkpoint.
+  '''
+  if not checkpoint_stats:
+    checkpoint_stats = [0] * (len(train_steps) + 1)
+    
+      session_config = tf.ConfigProto(
+      log_device_placement=False,
+      intra_op_parallelism_threads=FLAGS.threads,
+      inter_op_parallelism_threads=FLAGS.threads)
+    
+        base = 'src/sentry/locale'
+    for locale in os.listdir(base):
+        fn = os.path.join(base, locale, 'LC_MESSAGES', 'django.po')
+        if not os.path.isfile(fn):
             continue
-        break
-    if len(fields) >= 4:
-        try:
-            patchlevel = int(fields[3])
-        except ValueError:
-            patchlevel = 1
-    timestamp = '%04d-%02d-%02dT00:%02d:%02dZ' % (year, month, day, faked, patchlevel)
     
-    from youtube_dl.compat import (
-    compat_print,
-    compat_urllib_request,
-)
-from youtube_dl.utils import format_bytes
+        def on_diagnostics_passed(self):
+        super(Suspect, self).send_diagnostics_pass_report()
+        super(Suspect, self).clear_alarm()  # loss of redundancy alarm
+        super(Suspect, self).next_state('standby')
     
+        def test_sequential_execution(self):
+        self.command_stack[0].execute()
+        output_after_first_execution = os.listdir(self.test_dir)
+        self.assertEqual(output_after_first_execution[0], 'bar.txt')
+        self.command_stack[1].execute()
+        output_after_second_execution = os.listdir(self.test_dir)
+        self.assertEqual(output_after_second_execution[0], 'baz.txt')
     
-def _is_empty(d):
-    return not bool(os.listdir(d))
+        def unsubscribe(self, msg, subscriber):
+        self.subscribers[msg].remove(subscriber)
     
-        def _get_ip(self, protocol):
-        if self._SKIP_SOCKS_TEST:
-            return '127.0.0.1'
-    
-    from youtube_dl.swfinterp import SWFInterpreter
-    
-            if check_executable('mplayer', ['-h']):
-            args = [
-                'mplayer', '-really-quiet', '-vo', 'null', '-vc', 'dummy',
-                '-dumpstream', '-dumpfile', tmpfilename, url]
-        elif check_executable('mpv', ['-h']):
-            args = [
-                'mpv', '-really-quiet', '--vo=null', '--stream-dump=' + tmpfilename, url]
-        else:
-            self.report_error('MMS or RTSP download detected but neither 'mplayer' nor 'mpv' could be run. Please install any.')
-            return False
-    
-        def _real_extract(self, url):
-        # URLs end with [uploader name]/[uploader title]
-        # this title is whatever the user types in, and is rarely
-        # the proper song title.  Real metadata is in the api response
-        album_url_tag = self._match_id(url)
-    
-            page = self._download_json(
-            'http://vxml.56.com/json/%s/' % text_id, text_id, 'Downloading video info')
-    
-    
-class CamWithHerIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?camwithher\.tv/view_video\.php\?.*\bviewkey=(?P<id>\w+)'
-    
-    
-class ClipRsIE(OnetBaseIE):
-    _VALID_URL = r'https?://(?:www\.)?clip\.rs/(?P<id>[^/]+)/\d+'
-    _TEST = {
-        'url': 'http://www.clip.rs/premijera-frajle-predstavljaju-novi-spot-za-pesmu-moli-me-moli/3732',
-        'md5': 'c412d57815ba07b56f9edc7b5d6a14e5',
-        'info_dict': {
-            'id': '1488842.1399140381',
-            'ext': 'mp4',
-            'title': 'PREMIJERA Frajle predstavljaju novi spot za pesmu Moli me, moli',
-            'description': 'md5:56ce2c3b4ab31c5a2e0b17cb9a453026',
-            'duration': 229,
-            'timestamp': 1459850243,
-            'upload_date': '20160405',
-        }
-    }
-    
-        @property
-    def body(self):
-        '''Return a `bytes` with the message's body.'''
-        raise NotImplementedError()
-    
-    UNICODE = FILE_CONTENT
-
-    
-    
-def test_unicode_raw_json_item_verbose(httpbin):
-    r = http('--json', 'POST', httpbin.url + '/post',
-             u'test:={ '%s' : [ '%s' ] }' % (UNICODE, UNICODE))
-    assert HTTP_OK in r
-    assert r.json['json'] == {'test': {UNICODE: [UNICODE]}}
-    
-        '''
-    # attachment; filename=jakubroztocil-httpie-0.4.1-20-g40bd8f6.tar.gz
-    
-        def __call__(self, r):
-        '''
-        Override username/password serialization to allow unicode.
-    
-            $ http :3000                    # => http://localhost:3000
-        $ http :/foo                    # => http://localhost/foo
-    
-            '''
-        try:
-            return super(AuthCredentialsArgType, self).__call__(string)
-        except ArgumentTypeError:
-            # No password provided, will prompt for it later.
-            return self.key_value_class(
-                key=string,
-                value=None,
-                sep=SEP_CREDENTIALS,
-                orig=string
-            )
-    
-        digest = hash_func()
-    data = to_bytes(data, errors='surrogate_or_strict')
-    digest.update(data)
-    return digest.hexdigest()
-    
-    
-class GCPUtilsTestCase(unittest.TestCase):
-    params_dict = {
-        'url_map_name': 'foo_url_map_name',
-        'description': 'foo_url_map description',
-        'host_rules': [
-            {
-                'description': 'host rules description',
-                'hosts': [
-                        'www.example.com',
-                        'www2.example.com'
-                ],
-                'path_matcher': 'host_rules_path_matcher'
-            }
-        ],
-        'path_matchers': [
-            {
-                'name': 'path_matcher_one',
-                'description': 'path matcher one',
-                'defaultService': 'bes-pathmatcher-one-default',
-                'pathRules': [
-                        {
-                            'service': 'my-one-bes',
-                            'paths': [
-                                '/',
-                                '/aboutus'
-                            ]
-                        }
-                ]
-            },
-            {
-                'name': 'path_matcher_two',
-                'description': 'path matcher two',
-                'defaultService': 'bes-pathmatcher-two-default',
-                'pathRules': [
-                        {
-                            'service': 'my-two-bes',
-                            'paths': [
-                                '/webapp',
-                                '/graphs'
-                            ]
-                        }
-                ]
-            }
-        ]
-    }
-    
-    
-class TerminalModule(TerminalBase):
-    
-        def on_open_shell(self):
-        try:
-            self._exec_cli_command(b'environment no more')
-        except AnsibleConnectionFailure:
-            raise AnsibleConnectionFailure('unable to set terminal parameters')
-
-    
-        def construct_yaml_seq(self, node):
-        data = AnsibleSequence()
-        yield data
-        data.extend(self.construct_sequence(node))
-        data.ansible_pos = self._node_position_info(node)
+        def test_bunch_launch(self):
+        self.runner.runAll()
+        output = self.out.getvalue().strip()
+        self.assertEqual(output, str(self.average_result_tc1 + '\n\n' +
+                         self.average_result_tc2 + '\n\n' +
+                         self.average_result_tc3))
