9aa08765e9e06d7f9672d05b07f0edfad80d78ff
SUNsung <weatherius@gmail.com>
2018-05-28 09:15:37 +0300

    Rewrite [36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py] File in: 2018-05-28 09:15:36

--- pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
+++ pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
@@ -1,58 +1,11 @@
 
         
-        
-def test_unicode_digest_auth(httpbin):
-    # it doesn't really authenticate us because httpbin
-    # doesn't interpret the utf8-encoded auth
-    http('--auth-type=digest',
-         '--auth', u'test:%s' % UNICODE,
-         httpbin.url + u'/digest-auth/auth/test/' + UNICODE)
-
-    
-            if downloader and exit_status == ExitStatus.OK:
-            # Last response body download.
-            download_stream, download_to = downloader.start(final_response)
-            write_stream(
-                stream=download_stream,
-                outfile=download_to,
-                flush=False,
-            )
-            downloader.finish()
-            if downloader.interrupted:
-                exit_status = ExitStatus.ERROR
-                log_error('Incomplete download: size=%d; downloaded=%d' % (
-                    downloader.status.total_size,
-                    downloader.status.downloaded
-                ))
-        return exit_status
-    
-            '''
-        def check_options(value, option):
-            unknown = set(value) - OUTPUT_OPTIONS
-            if unknown:
-                self.error('Unknown output options: {0}={1}'.format(
-                    option,
-                    ','.join(unknown)
-                ))
-    
-        # Auth
-    def get_auth_plugins(self):
-        return [plugin for plugin in self if issubclass(plugin, AuthPlugin)]
-    
-            # host:port => host_port
-        hostname = hostname.replace(':', '_')
-        path = os.path.join(config_dir,
-                            SESSIONS_DIR_NAME,
-                            hostname,
-                            session_name + '.json')
-    
-        @classmethod
-    def __get_test_directory(self):
-        '''
-        Get the temporary directory for the tests.
-        '''
-        self.test_dir = os.path.join(os.path.dirname(
-            os.path.realpath(__file__)), 'test_command')
+            def test_data_change_shall_notify_all_observers_once(cls):
+        with patch.object(cls.dec_obs, 'update') as mock_dec_obs_update,\
+                patch.object(cls.hex_obs, 'update') as mock_hex_obs_update:
+            cls.sub.data = 10
+            cls.assertEqual(mock_dec_obs_update.call_count, 1)
+            cls.assertEqual(mock_hex_obs_update.call_count, 1)
     
         def test_sales_manager_shall_not_talk_through_proxy_with_delay(cls):
         cls.ntp.busy = 'No'
@@ -67,21 +20,6 @@ This Sales Manager will not talk to you whether he/she is busy or not\n'
         expected_execution_time = 1
         cls.assertEqual(int(execution_time*10), expected_execution_time)
     
-        def __init__(self, name):
-        self.name = name
-    
-        def notify(self, msg):
-        self.msg_queue.append(msg)
-    
-        def setUp(self):
-        self.sample_queue = queue.Queue()
-        self.sample_queue.put('first')
-        self.sample_queue.put('second')
-    
-        print('After subclassing: ')
-    for k in RegistryHolder.REGISTRY:
-        print(k)
-    
     
 class RadioTest(unittest.TestCase):
     '''
@@ -90,7 +28,16 @@ class RadioTest(unittest.TestCase):
     http://stackoverflow.com/questions/5387299/python-unittest-testcase-execution-order
     '''
     
-    
+        def test_display_current_time_at_midnight(self):
+        '''
+        Would almost always fail (despite of right at/after midnight) if
+        untestable production code would have been used.
+        '''
+        time_provider_stub = MidnightTimeProvider()
+        class_under_test = TimeDisplay()
+        class_under_test.set_time_provider(time_provider_stub)
+        expected_time = '<span class=\'tinyBoldText\'>24:01</span>'
+        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
     
     '''
 *TL;DR80
