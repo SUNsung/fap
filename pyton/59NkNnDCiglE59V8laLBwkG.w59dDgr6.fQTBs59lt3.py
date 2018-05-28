58a96462a4293a60f2331a0d05bfd002ac5bfe89
SUNsung <weatherius@gmail.com>
2018-05-28 07:11:30 +0300

    Rewrite [59NkNnDCiglE59V8laLBwkG.w59dDgr6.fQTBs59lt3.py] File in: 2018-05-28 07:11:29

--- pyton/59NkNnDCiglE59V8laLBwkG.w59dDgr6.fQTBs59lt3.py
+++ pyton/59NkNnDCiglE59V8laLBwkG.w59dDgr6.fQTBs59lt3.py
@@ -1,47 +1,179 @@
 
         
-        import datetime
-import io
-import json
-import textwrap
+        flags.DEFINE_string('spec_file', 'parser_spec.textproto',
+                    'Filename to save the spec to.')
     
-            def report_warning(self, message):
-            if re.match(regex, message):
-                return
-            old_report_warning(message)
-        self.report_warning = types.MethodType(report_warning, self)
+    cc_test(
+    name = 'mst_solver_test',
+    size = 'small',
+    srcs = ['mst_solver_test.cc'],
+    deps = [
+        ':mst_solver',
+        '//dragnn/core/test:generic',
+        '//syntaxnet:base',
+        '//syntaxnet:test_main',
+        '@org_tensorflow//tensorflow/core:test',
+    ],
+)
     
-            def _find_spec(key):
-            m = re.search(
-                r'(?m)^//\s*%s:\s*(.*?)\n' % re.escape(key), as_content)
-            if not m:
-                raise ValueError('Cannot find %s in %s' % (key, testfile))
-            return json.loads(m.group(1))
+    py_library(
+    name = 'mst_units',
+    srcs = ['mst_units.py'],
+    deps = [
+        ':mst_ops',
+        ':network_units',
+        '//syntaxnet/util:check',
+        '@org_tensorflow//tensorflow:tensorflow_py',
+    ],
+)
     
-            return self._extract_nuevo(
-            'http://www.anitube.se/nuevo/econfig.php?key=%s' % key, video_id)
-
+        # Get embedding matrix variables.
+    with tf.variable_scope(comp.name, reuse=True):
+      fixed_embedding_matrix = tf.get_variable(
+          network_units.fixed_embeddings_name(0))
     
-                for video_version in json_data.get('video_versions'):
-                video_version_url = video_version.get('download_url') or video_version.get('stream_url')
-                if not video_version_url:
-                    continue
-                f = {
-                    'url': video_version_url,
-                    'width': int_or_none(video_version.get('width')),
-                    'height': int_or_none(video_version.get('height')),
-                    'abr': int_or_none(video_version.get('audio_bitrate')),
-                    'vbr': int_or_none(video_version.get('video_bitrate')),
-                }
-                bitrate = self._search_regex(r'(\d+)k', video_version_url, 'bitrate', default=None)
-                if bitrate:
-                    f.update({
-                        'format_id': 'http-%s' % bitrate,
-                    })
-                formats.append(f)
-            self._sort_formats(formats)
+          # Raises ValueError if not found.
+      comp = component.ComponentBuilderBase.Create(component_type, self,
+                                                   component_spec)
     
-            page = self._download_json(
-            'http://vxml.56.com/json/%s/' % text_id, text_id, 'Downloading video info')
+    import dragnn.python.load_mst_cc_impl
+from dragnn.mst.ops import gen_mst_ops
+from dragnn.python import digraph_ops
+from syntaxnet.util import check
     
-            webpage = self._download_webpage(url, display_id)
+    
+def ismount(path):
+    '''Test whether a path is a mount point
+    clone of os.path.ismount (from cpython Lib/posixpath.py)
+    fixed to solve https://github.com/ansible/ansible-modules-core/issues/2186
+    and workaround non-fixed http://bugs.python.org/issue2466
+    this should be rewritten as soon as python issue 2466 is fixed
+    probably check for python version and use os.path.ismount if fixed
+    
+        'bracketed_hostport': re.compile(
+        r'''^
+            \[(.+)\]                    # [host identifier]
+            :([0-9]+)                   # :port number
+            $
+        ''', re.X
+    ),
+    
+        terminal_stdout_re = [
+        re.compile(r'[\r\n]?<.+>(?:\s*)$'),
+        re.compile(r'[\r\n]?\[.+\](?:\s*)$'),
+    ]
+    
+            print('benchmarking scikit-learn: ')
+        scikit_results.append(bench(ScikitLasso, X, Y, X_test, Y_test, coef_))
+        print('benchmarking glmnet: ')
+        glmnet_results.append(bench(GlmnetLasso, X, Y, X_test, Y_test, coef_))
+    
+        for i, NN in enumerate(Nrange):
+        print('N = %i (%i out of %i)' % (NN, i + 1, len(Nrange)))
+        X = get_data(NN, D, dataset)
+        for algorithm in algorithms:
+            nbrs = neighbors.NearestNeighbors(n_neighbors=min(NN, k),
+                                              algorithm=algorithm,
+                                              leaf_size=leaf_size)
+            t0 = time()
+            nbrs.fit(X)
+            t1 = time()
+            nbrs.kneighbors(X)
+            t2 = time()
+    
+    from sklearn.cluster import AgglomerativeClustering
+    
+        op.add_option('--eps',
+                  dest='eps', default=0.5, type=float,
+                  help='See the documentation of the underlying transformers.')
+    
+    Does two benchmarks
+    
+    
+if not os.path.exists(DATA_FOLDER):
+    
+    # Define 'classifiers' to be used
+classifiers = {
+    'Empirical Covariance': EllipticEnvelope(support_fraction=1.,
+                                             contamination=0.261),
+    'Robust Covariance (Minimum Covariance Determinant)':
+    EllipticEnvelope(contamination=0.261),
+    'OCSVM': OneClassSVM(nu=0.261, gamma=0.05)}
+colors = ['m', 'g', 'b']
+legend1 = {}
+legend2 = {}
+    
+    The dataset is generated using the ``make_biclusters`` function, which
+creates a matrix of small values and implants bicluster with large
+values. The rows and columns are then shuffled and passed to the
+Spectral Co-Clustering algorithm. Rearranging the shuffled matrix to
+make biclusters contiguous shows how accurately the algorithm found
+the biclusters.
+    
+    np.random.seed(0)
+###############################################################################
+n_features = 100
+# simulation covariance matrix (AR(1) process)
+r = 0.1
+real_cov = toeplitz(r ** np.arange(n_features))
+coloring_matrix = cholesky(real_cov)
+    
+    # If true, show URL addresses after external links.
+#man_show_urls = False
+    
+        def url_features(self):
+        return set(request.GET.getall('feature'))
+    
+    from r2.controllers.api_docs import api_doc, api_section
+from r2.controllers.oauth2 import require_oauth2_scope
+from r2.controllers.reddit_base import OAuth2OnlyController
+from r2.lib.jsontemplates import (
+    FriendTableItemJsonTemplate,
+    get_usertrophies,
+    IdentityJsonTemplate,
+    KarmaListJsonTemplate,
+    PrefsJsonTemplate,
+)
+from r2.lib.pages import FriendTableItem
+from r2.lib.validator import (
+    validate,
+    VAccountByName,
+    VFriendOfMine,
+    VLength,
+    VList,
+    VUser,
+    VValidatedJSON,
+)
+from r2.models import Account, Trophy
+import r2.lib.errors as errors
+import r2.lib.validator.preferences as vprefs
+    
+                if links:
+                kw = {}
+                if wrapper:
+                    links = wrap_links(links, wrapper = wrapper)
+                else:
+                    links = wrap_links(links)
+                links = list(links)
+                links = max(links, key = lambda x: x._score) if links else None
+            if not links and wrapper:
+                return wrapper(None)
+            return links
+            # note: even if _by_url successed or a link was passed in,
+            # it is possible link_listing.things is empty if the
+            # link(s) is/are members of a private reddit
+            # return the link with the highest score (if more than 1)
+        except:
+            #we don't want to return 500s in other people's pages.
+            import traceback
+            g.log.debug('FULLPATH: get_link error in buttons code')
+            g.log.debug(traceback.format_exc())
+            if wrapper:
+                return wrapper(None)
+    
+    class CaptchaController(RedditController):
+    @allow_oauth2_access
+    @api_doc(api_section.captcha, uri='/captcha/{iden}')
+    def GET_captchaimg(self, iden):
+        '''
+        Request a CAPTCHA image given an `iden`.
