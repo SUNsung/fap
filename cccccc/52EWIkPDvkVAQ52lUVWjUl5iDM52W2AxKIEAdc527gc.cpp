402603ef68ac7fb6a67a07b3d532d4189e4eff93
SUNsung <weatherius@gmail.com>
2018-05-28 17:08:31 +0300

    Rewrite [52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp] File in: 2018-05-28 17:08:30

--- cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
+++ cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
@@ -1,84 +1,141 @@
 
         
-        #ifndef GRPC_INTERNAL_COMPILER_PYTHON_GENERATOR_H
-#define GRPC_INTERNAL_COMPILER_PYTHON_GENERATOR_H
-    
-    #include <signal.h>
-    
-     private:
-  static Result Sample();
-    
-      void SetComparatorName(const Slice& name) {
-    has_comparator_ = true;
-    comparator_ = name.ToString();
-  }
-  void SetLogNumber(uint64_t num) {
-    has_log_number_ = true;
-    log_number_ = num;
-  }
-  void SetPrevLogNumber(uint64_t num) {
-    has_prev_log_number_ = true;
-    prev_log_number_ = num;
-  }
-  void SetNextFile(uint64_t num) {
-    has_next_file_number_ = true;
-    next_file_number_ = num;
-  }
-  void SetLastSequence(SequenceNumber seq) {
-    has_last_sequence_ = true;
-    last_sequence_ = seq;
-  }
-  void SetCompactPointer(int level, const InternalKey& key) {
-    compact_pointers_.push_back(std::make_pair(level, key));
-  }
-    
-    static Slice Key(int i, char* buffer) {
-  EncodeFixed32(buffer, i);
-  return Slice(buffer, sizeof(uint32_t));
+        
+    {    return S_OK;
 }
     
-      // Pick next gap with average value of config::kReadBytesPeriod.
-  ssize_t RandomPeriod() {
-    return rnd_.Uniform(2*config::kReadBytesPeriod);
+    #ifndef __OPENCV_CORE_OCL_RUNTIME_COMMON_HPP__
+#define __OPENCV_CORE_OCL_RUNTIME_COMMON_HPP__
+    
+    #  define EXPECT_DEBUG_DEATH(statement, regex) \
+  GTEST_EXECUTE_STATEMENT_(statement, regex)
+    
+    namespace internal {
+    }
+    
+    #if !GTEST_OS_SYMBIAN
+# include <utility>
+#endif
+    
+    // Factory interface for death tests.  May be mocked out for testing.
+class DeathTestFactory {
+ public:
+  virtual ~DeathTestFactory() { }
+  virtual bool Create(const char* statement, const RE* regex,
+                      const char* file, int line, DeathTest** test) = 0;
+};
+    
+      // Returns true if pathname describes an absolute path.
+  bool IsAbsolutePath() const;
+    
+    // This is used internally by all instances of linked_ptr<>.  It needs to be
+// a non-template class because different types of linked_ptr<> can refer to
+// the same object (linked_ptr<Superclass>(obj) vs linked_ptr<Subclass>(obj)).
+// So, it needs to be possible for different types of linked_ptr to participate
+// in the same circular linked list, so we need a single class type here.
+//
+// DO NOT USE THIS CLASS DIRECTLY YOURSELF.  Use linked_ptr<T>.
+class linked_ptr_internal {
+ public:
+  // Create a new circle that includes only this instance.
+  void join_new() {
+    next_ = this;
+  }
+    }
+    
+      template <typename T>
+  operator ParamGenerator<T>() const {
+    const T array[] = {static_cast<T>(v1_), static_cast<T>(v2_),
+        static_cast<T>(v3_), static_cast<T>(v4_), static_cast<T>(v5_),
+        static_cast<T>(v6_), static_cast<T>(v7_), static_cast<T>(v8_),
+        static_cast<T>(v9_), static_cast<T>(v10_), static_cast<T>(v11_),
+        static_cast<T>(v12_), static_cast<T>(v13_), static_cast<T>(v14_),
+        static_cast<T>(v15_), static_cast<T>(v16_), static_cast<T>(v17_),
+        static_cast<T>(v18_), static_cast<T>(v19_), static_cast<T>(v20_),
+        static_cast<T>(v21_), static_cast<T>(v22_), static_cast<T>(v23_),
+        static_cast<T>(v24_), static_cast<T>(v25_), static_cast<T>(v26_),
+        static_cast<T>(v27_), static_cast<T>(v28_), static_cast<T>(v29_),
+        static_cast<T>(v30_), static_cast<T>(v31_), static_cast<T>(v32_)};
+    return ValuesIn(array);
   }
     
-    class FormatTest { };
+    template <$for j, [[typename T$j]]>
+class CartesianProductGenerator$i
+    : public ParamGeneratorInterface< ::std::tr1::tuple<$for j, [[T$j]]> > {
+ public:
+  typedef ::std::tr1::tuple<$for j, [[T$j]]> ParamType;
+    }
     
-      // Finish building the block and return a slice that refers to the
-  // block contents.  The returned slice will remain valid for the
-  // lifetime of this builder or until Reset() is called.
-  Slice Finish();
+      virtual int GetNextPrime(int p) const {
+    for (int n = p + 1; n < is_prime_size_; n++) {
+      if (is_prime_[n]) return n;
+    }
+    }
+    
+    	static PoolByteArray get_project_cert_array();
+	static void load_certs_from_memory(const PoolByteArray &p_memory);
+	static bool is_available();
+    
+    	ERR_FAIL_COND_V(context != NULL, FAILED);
     
     
-    {} // namespace A2STR
+    {	if (_is_multiplayer) {
+		// need to wait for ID confirmation...
+	} else {
+		emit_signal('connection_established', p_protocol);
+	}
+}
     
-      virtual bool getInitialOption() const CXX11_OVERRIDE;
+    	virtual void poll() = 0;
+	virtual Error connect_to_host(String p_host, String p_path, uint16_t p_port, bool p_ssl, PoolVector<String> p_protocol = PoolVector<String>()) = 0;
+	virtual void disconnect_from_host() = 0;
+	virtual IP_Address get_connected_host() const = 0;
+	virtual uint16_t get_connected_port() const = 0;
+    
+    	GDCLASS(VehicleBody, RigidBody);
+    
+    
+    {            k += r;
+          }
+          
+          s = JPGD_HUFF_EXTEND(extra_bits, s);
+    
+    #include <stdlib.h>
+#include <stdio.h>
+#include <setjmp.h>
+    
+    	HKEY cmderKey;
+	FAIL_ON_ERROR(RegCreateKeyEx(root, keyBaseName, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &cmderKey, NULL));
+    
+    #include <libkern/libkern.h>
+#include <mach/mach_types.h>
+    
+    #include 'osquery/core/utils.h'
+    
+      struct ConstraintList cl2;
+  cl2.affinity = INTEGER_TYPE;
+  constraint = Constraint(LESS_THAN);
+  constraint.expr = '1000';
+  cl2.add(constraint);
+  constraint = Constraint(GREATER_THAN);
+  constraint.expr = '1';
+  cl2.add(constraint);
     
     
     {
-    {    httpConnection_->sendProxyRequest(std::move(httpRequest));
-  }
-  else {
-    httpConnection_->sendPendingData();
-  }
-  if (httpConnection_->sendBufferIsEmpty()) {
-    getDownloadEngine()->addCommand(getNextCommand());
-    return true;
-  }
-  else {
-    setWriteCheckSocket(getSocket());
-    addCommandSelf();
-    return false;
-  }
+    {  FLAGS_watchdog_delay = delay;
 }
-    
-    class AbstractProxyRequestCommand : public AbstractCommand {
-private:
-  std::shared_ptr<Request> proxyRequest_;
-    }
+} // namespace osquery
+
     
     
-    {} // namespace aria2
-    
-    
-    {} // namespace aria2
+    {  BSTR* pData = nullptr;
+  SafeArrayAccessData(value.parray, (void**)&pData);
+  ret.reserve(count);
+  for (long i = 0; i < count; i++) {
+    ret.push_back(bstrToString(pData[i]));
+  }
+  SafeArrayUnaccessData(value.parray);
+  VariantClear(&value);
+  return Status(0);
+}
