7f945f470b967ef535b8c21901afb9d31c4e6eb1
SUNsung <weatherius@gmail.com>
2018-05-28 15:26:29 +0300

    Rewrite [501MoGbese6Qc50E4ZiPzZpD0Y50hIjm5uBj3kw50edb.cpp] File in: 2018-05-28 15:26:29

--- cccccc/501MoGbese6Qc50E4ZiPzZpD0Y50hIjm5uBj3kw50edb.cpp
+++ cccccc/501MoGbese6Qc50E4ZiPzZpD0Y50hIjm5uBj3kw50edb.cpp
@@ -1,243 +1,212 @@
 
         
-        
-    {}  // namespace tesseract.
+          void compute();
     
-    CCStruct::~CCStruct() {
-}
+      auto &fileMgr = clangCI.getFileManager();
+  bool isSystem = module->isSystemModule();
+  // FIXME: Get real values for the following.
+  StringRef swiftVersion;
+  StringRef sysrootPath = clangCI.getHeaderSearchOpts().Sysroot;
+  std::string indexUnitToken = module->getModuleFilename();
+  // For indexing serialized modules 'debug compilation' is irrelevant, so
+  // set it to true by default.
+  bool isDebugCompilation = true;
     
-    DetLineFit::~DetLineFit() {
-}
+    #include 'swift/Markup/Markup.h'
+#include 'llvm/ADT/Optional.h'
     
-    #ifndef TESSERACT_CCUTIL_QRSEQUENCE_H_
-#define TESSERACT_CCUTIL_QRSEQUENCE_H_
+      /// Treat all warnings as errors
+  bool WarningsAsErrors = false;
     
-      // The UNICHARMAP is represented as a tree whose nodes are of type
-  // UNICHARMAP_NODE.
-  struct UNICHARMAP_NODE {
+    
+    {  StringRef getReceiverUSR() const {
+    for (auto Relation: Relations) {
+      if (Relation.roles & (SymbolRoleSet) SymbolRole::RelationReceivedBy)
+        return Relation.USR;
     }
+    return StringRef();
+  }
+};
+    
+        mBsparse.SwitchToMatrixType(MatrixType::SPARSE, matrixFormatSparseCSR, true);
+    Matrix<float>::ScaleAndAdd(alpha, mAdense, mBsparse);
+    
+        StreamMinibatchInputs inputMatrices;
+    for (auto& node : featureNodes)
+        inputMatrices.AddInput(node->NodeName(), node->ValuePtr(), node->GetMBLayout(), node->GetSampleLayout());
+    
+    // ===========================================================================
+// DoConvertFromDbn() - implements CNTK 'convertdbn' command
+// ===========================================================================
+    
+    // access the parser through one of these functions
+ExpressionPtr ParseConfigDictFromString(wstring text, wstring location, vector<wstring>&& includePaths);          // parses a list of dictionary members, returns a dictionary expression
+// TODO: These rvalue references are no longer adding value, change to const<>&
+//ExpressionPtr ParseConfigDictFromFile(wstring path, vector<wstring> includePaths);              // likewise, but from a file path
+ExpressionPtr ParseConfigExpression(const wstring& sourceText, vector<wstring>&& includePaths); // parses a single expression from sourceText, which is meant to contain an include statement, hence includePaths
     
     
+    {        // no config file, parse as regular argument
+        if (compare)
+        {
+            configString += (msra::strfun::utf8(str) + '\n');
+        }
+        else // One or more config file paths specified in a '+'-separated list.
+        {
+            const std::string filePaths = msra::strfun::utf8(str.substr(configDescriptor.length()));
+            std::vector<std::string> filePathsVec = msra::strfun::split(filePaths, '+');
+            for (auto filePath : filePathsVec)
+            {
+                if (std::find(resolvedConfigFiles.begin(), resolvedConfigFiles.end(), filePath) == resolvedConfigFiles.end())
+                {
+                    // if haven't already read this file, read it
+                    resolvedConfigFiles.push_back(filePath);
+                    configString += config.ReadConfigFile(filePath);
+                    // remember all config directories, for use as include paths by BrainScriptNetworkBuilder
+                    GetBrainScriptNetworkBuilderIncludePaths().push_back(File::DirectoryPathOf(msra::strfun::utf16(filePath)));
+                }
+                else
+                    RuntimeError('Cannot specify same config file multiple times at the command line.');
+            }
+        }
+    }
+    // now, configString is a concatenation of lines, including parameters from the command line, with comments stripped
+    
+    public:
+    // DataWriter Constructor
+    // config - [in] configuration parameters for the datareader
+    template <class ConfigRecordType>
+    DataWriter(const ConfigRecordType& config);
+    // constructor from Scripting
+    DataWriter(const ScriptableObjects::IConfigRecordPtr configp)
+        : DataWriter(*configp)
     {
-}  // namespace routeguide
-    
-    AuthPropertyIterator SecureAuthContext::begin() const {
-  if (ctx_) {
-    grpc_auth_property_iterator iter =
-        grpc_auth_context_property_iterator(ctx_);
-    const grpc_auth_property* property =
-        grpc_auth_property_iterator_next(&iter);
-    return AuthPropertyIterator(property, &iter);
-  } else {
-    return end();
-  }
-}
-    
-    gpr_atm grpc::testing::interop::g_got_sigint;
-    
-    #include 'test/cpp/qps/usage_timer.h'
-    
-    grpc::string DescribeServiceList(std::vector<grpc::string> service_list,
-                                 grpc::protobuf::DescriptorPool& desc_pool) {
-  std::stringstream result;
-  for (auto it = service_list.begin(); it != service_list.end(); it++) {
-    auto const& service = *it;
-    const grpc::protobuf::ServiceDescriptor* service_desc =
-        desc_pool.FindServiceByName(service);
-    if (service_desc != nullptr) {
-      result << DescribeService(service_desc);
     }
-  }
-  return result.str();
-}
-    
-     private:
-  SubProcess(const SubProcess& other);
-  SubProcess& operator=(const SubProcess& other);
-    
-    #include <google/protobuf/compiler/command_line_interface.h>
-#include <google/protobuf/compiler/python/python_generator.h>
-    
-    static int populate_ifaddrs(struct ifaddrs* ifaddr, ifaddrmsg* msg, void* bytes,
-		     size_t len) {
-	if (set_ifname(ifaddr, msg->ifa_index) != 0) {
-		return -1;
-	}
-	if (set_flags(ifaddr) != 0) {
-		return -1;
-	}
-	if (set_addresses(ifaddr, msg, bytes, len) != 0) {
-		return -1;
-	}
-	if (make_prefixes(ifaddr, msg->ifa_family, msg->ifa_prefixlen) != 0) {
-		return -1;
-	}
-	return 0;
-}
-    
-    	virtual int get_available_bytes() const;
+    virtual ~DataWriter();
     
     
-    {	ADD_SIGNAL(MethodInfo('data_received'));
-	ADD_SIGNAL(MethodInfo('connection_established', PropertyInfo(Variant::STRING, 'protocol')));
-	ADD_SIGNAL(MethodInfo('connection_closed'));
-	ADD_SIGNAL(MethodInfo('connection_error'));
-}
-
-    
-    #endif // WEBSOCKET_CLIENT_H
-
-    
-    		MethodBind *bind = create_method_bind(p_method);
-		const Variant *ptr[4] = { &p_def1, &p_def2, &p_def3, &p_def4 };
-    
-    	void _resolve_single_bilateral(PhysicsDirectBodyState *s, const Vector3 &pos1, PhysicsBody *body2, const Vector3 &pos2, const Vector3 &normal, real_t &impulse, real_t p_rollInfluence);
-	real_t _calc_rolling_friction(btVehicleWheelContactPoint &contactPoint);
-    
-    	GodotRestInfoContactResultCallback(btCollisionObject *p_self_object, PhysicsDirectSpaceState::ShapeRestInfo *p_result, const Set<RID> *p_exclude) :
-			m_self_object(p_self_object),
-			m_result(p_result),
-			m_exclude(p_exclude),
-			m_collided(false),
-			m_min_distance(0) {}
-    
-    #include 'editor/create_dialog.h'
-#include 'editor/plugins/script_editor_plugin.h'
-#include 'editor/property_editor.h'
-#include 'editor/property_selector.h'
-#include 'scene/gui/graph_edit.h'
-#include 'visual_script.h'
-class VisualScriptEditorSignalEdit;
-class VisualScriptEditorVariableEdit;
-    
-    /*Modern gcc (4.x) can compile the naive versions of min and max with cmov if
-   given an appropriate architecture, but the branchless bit-twiddling versions
-   are just as fast, and do not require any special target architecture.
-  Earlier gcc versions (3.x) compiled both code to the same assembly
-   instructions, because of the way they represented ((_b)>(_a)) internally.*/
-#define OC_MAXI(_a,_b)      ((_a)-((_a)-(_b)&-((_b)>(_a))))
-#define OC_MINI(_a,_b)      ((_a)+((_b)-(_a)&-((_b)<(_a))))
-/*Clamps an integer into the given range.
-  If _a>_c, then the lower bound _a is respected over the upper bound _c (this
-   behavior is required to meet our documented API behavior).
-  _a: The lower bound.
-  _b: The value to clamp.
-  _c: The upper boud.*/
-#define OC_CLAMPI(_a,_b,_c) (OC_MAXI(_a,OC_MINI(_b,_c)))
-#define OC_CLAMP255(_x)     ((unsigned char)((((_x)<0)-1)&((_x)|-((_x)>255))))
-/*This has a chance of compiling branchless, and is just as fast as the
-   bit-twiddling method, which is slightly less portable, since it relies on a
-   sign-extended rightshift, which is not guaranteed by ANSI (but present on
-   every relevant platform).*/
-#define OC_SIGNI(_a)        (((_a)>0)-((_a)<0))
-/*Slightly more portable than relying on a sign-extended right-shift (which is
-   not guaranteed by ANSI), and just as fast, since gcc (3.x and 4.x both)
-   compile it into the right-shift anyway.*/
-#define OC_SIGNMASK(_a)     (-((_a)<0))
-/*Divides an integer by a power of two, truncating towards 0.
-  _dividend: The integer to divide.
-  _shift:    The non-negative power of two to divide by.
-  _rmask:    (1<<_shift)-1*/
-#define OC_DIV_POW2(_dividend,_shift,_rmask)\
-  ((_dividend)+(OC_SIGNMASK(_dividend)&(_rmask))>>(_shift))
-/*Divides _x by 65536, truncating towards 0.*/
-#define OC_DIV2_16(_x) OC_DIV_POW2(_x,16,0xFFFF)
-/*Divides _x by 2, truncating towards 0.*/
-#define OC_DIV2(_x) OC_DIV_POW2(_x,1,0x1)
-/*Divides _x by 8, truncating towards 0.*/
-#define OC_DIV8(_x) OC_DIV_POW2(_x,3,0x7)
-/*Divides _x by 16, truncating towards 0.*/
-#define OC_DIV16(_x) OC_DIV_POW2(_x,4,0xF)
-/*Right shifts _dividend by _shift, adding _rval, and subtracting one for
-   negative dividends first.
-  When _rval is (1<<_shift-1), this is equivalent to division with rounding
-   ties away from zero.*/
-#define OC_DIV_ROUND_POW2(_dividend,_shift,_rval)\
-  ((_dividend)+OC_SIGNMASK(_dividend)+(_rval)>>(_shift))
-/*Divides a _x by 2, rounding towards even numbers.*/
-#define OC_DIV2_RE(_x) ((_x)+((_x)>>1&1)>>1)
-/*Divides a _x by (1<<(_shift)), rounding towards even numbers.*/
-#define OC_DIV_POW2_RE(_x,_shift) \
-  ((_x)+((_x)>>(_shift)&1)+((1<<(_shift))-1>>1)>>(_shift))
-/*Swaps two integers _a and _b if _a>_b.*/
-#define OC_SORT2I(_a,_b) \
-  do{ \
-    int t__; \
-    t__=((_a)^(_b))&-((_b)<(_a)); \
-    (_a)^=t__; \
-    (_b)^=t__; \
-  } \
-  while(0)
-    
-    
-    {    return init.result.get();
-  }
-    
-    #ifndef BOOST_ASIO_BUFFERED_WRITE_STREAM_FWD_HPP
-#define BOOST_ASIO_BUFFERED_WRITE_STREAM_FWD_HPP
-    
-    namespace boost {
+    {        // compute global progress
+        bool needToPrint = us.m_progressTracingTimer.ElapsedSeconds() > 10;
+        if (needToPrint || isTimerPaced)
+        {
+            double epochProg = ((100.0f * (double) (us.m_currentStepOffset + epochNumber)) / (double) us.m_totalNumberOfSteps);
+            mbProg = (mbProg * 100.0f) / (double) us.m_totalNumberOfSteps;
+            printf('PROGRESS: %.2f%%\n', epochProg + mbProg);
+            us.m_progressTracingTimer.Restart();
+        }
+        return needToPrint;
     }
     
-    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
-# pragma once
-#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+        float4 operator>=(const float4& other) const
+    {
+        return _mm_cmpge_ps(v, other);
+    }
+    float4 operator<=(const float4& other) const
+    {
+        return _mm_cmple_ps(v, other);
+    }
     
-    #endif // BOOST_ASIO_DETAIL_FUNCTION_HPP
-
-    
-    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
-# pragma once
-#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
-    
-    #define BOOST_ASIO_DEFINE_HANDLER_PTR(op) \
-  struct ptr \
-  { \
-    Handler* h; \
-    void* v; \
-    op* p; \
-    ~ptr() \
-    { \
-      reset(); \
-    } \
-    void reset() \
-    { \
-      if (p) \
-      { \
-        p->~op(); \
-        p = 0; \
-      } \
-      if (v) \
-      { \
-        boost_asio_handler_alloc_helpers::deallocate(v, sizeof(op), *h); \
-        v = 0; \
-      } \
-    } \
-  } \
-  /**/
-    
-      // Get an iterator for the end of the map.
-  const_iterator end() const
-  {
-    return values_.end();
+      std::string content;
+  auto status = readFile('/etc/exports', content);
+  if (!status.ok()) {
+    VLOG(1) << 'Error reading /etc/exports: ' << status.toString();
+    return {};
   }
     
-    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
-# pragma once
-#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
     
-    #endif // BOOST_ASIO_DETAIL_IMPL_DEV_POLL_REACTOR_IPP
+    {/**
+ * @brief Write a log line to the OS system log.
+ *
+ * There are occasional needs to log independently of the osquery logging
+ * facilities. This allows a feature (not a table) to bypass all osquery
+ * configuration and log to the OS system log.
+ *
+ * Linux/Darwin: this uses syslog's LOG_NOTICE.
+ * Windows: This will end up inside the Facebook/osquery in the Windows
+ * Event Log.
+ */
+void systemLog(const std::string& line);
+} // namespace osquery
 
     
-    template <typename Time_Traits>
-std::size_t epoll_reactor::cancel_timer(timer_queue<Time_Traits>& queue,
-    typename timer_queue<Time_Traits>::per_timer_data& timer,
-    std::size_t max_cancelled)
-{
-  mutex::scoped_lock lock(mutex_);
-  op_queue<operation> ops;
-  std::size_t n = queue.cancel_timer(timer, ops, max_cancelled);
-  lock.unlock();
-  io_service_.post_deferred_completions(ops);
-  return n;
+    static inline void teardown_locks() {
+  // Release locks and their heap memory.
+  lck_mtx_free(osquery.mtx, osquery.lck_grp);
+    }
+    
+    
+    {  // Cleanup allocations.
+  std::string result(buffer);
+  free(buffer);
+  return result;
 }
+    
+    
+    {  EXPECT_EQ(expected, toAsciiTime(&result));
+}
+    
+    Status WmiResultItem::GetUnsignedShort(const std::string& name,
+                                       unsigned short& ret) const {
+  std::wstring property_name = stringToWstring(name);
+  VARIANT value;
+  HRESULT hr = result_->Get(property_name.c_str(), 0, &value, nullptr, nullptr);
+    }
+    
+        // Create texture
+    int flags = al_get_new_bitmap_flags();
+    int fmt = al_get_new_bitmap_format();
+    al_set_new_bitmap_flags(ALLEGRO_MEMORY_BITMAP|ALLEGRO_MIN_LINEAR|ALLEGRO_MAG_LINEAR);
+    al_set_new_bitmap_format(ALLEGRO_PIXEL_FORMAT_ABGR_8888_LE);
+    ALLEGRO_BITMAP* img = al_create_bitmap(width, height);
+    al_set_new_bitmap_flags(flags);
+    al_set_new_bitmap_format(fmt);
+    if (!img)
+        return false;
+    
+            max_glyph_size.x = ImMax(max_glyph_size.x, font_face.Info.MaxAdvanceWidth);
+        max_glyph_size.y = ImMax(max_glyph_size.y, font_face.Info.Ascender - font_face.Info.Descender);
+    
+        // Setup style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+    
+        IDXGISwapChain1* swapChain1 = NULL;
+    dxgiFactory->CreateSwapChainForHwnd(g_pd3dCommandQueue, hWnd, &sd, NULL, NULL, &swapChain1);
+    swapChain1->QueryInterface(IID_PPV_ARGS(&g_pSwapChain));
+    swapChain1->Release();
+    dxgiFactory->Release();
+    
+    
+    {    switch (msg)
+    {
+    case WM_SIZE:
+        if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
+        {
+            ImGui_ImplDX9_InvalidateDeviceObjects();
+            g_d3dpp.BackBufferWidth  = LOWORD(lParam);
+            g_d3dpp.BackBufferHeight = HIWORD(lParam);
+            HRESULT hr = g_pd3dDevice->Reset(&g_d3dpp);
+            if (hr == D3DERR_INVALIDCALL)
+                IM_ASSERT(0);
+            ImGui_ImplDX9_CreateDeviceObjects();
+        }
+        return 0;
+    case WM_SYSCOMMAND:
+        if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+            return 0;
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+    
+                if (ImGui::Button('Button'))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text('counter = %d', counter);
+    
+    // About GLSL version:
+// The 'glsl_version' initialization parameter defaults to '#version 150' if NULL.
+// Only override if your GL version doesn't handle this GLSL version. Keep NULL if unsure!
