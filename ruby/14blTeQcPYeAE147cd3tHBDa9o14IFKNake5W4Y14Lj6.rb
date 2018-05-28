1adac6261bab9a6d99332cbb49179af946cbd4c0
SUNsung <weatherius@gmail.com>
2018-05-28 18:55:00 +0300

    Rewrite [14blTeQcPYeAE147cd3tHBDa9o14IFKNake5W4Y14Lj6.rb] File in: 2018-05-28 18:55:00

--- ruby/14blTeQcPYeAE147cd3tHBDa9o14IFKNake5W4Y14Lj6.rb
+++ ruby/14blTeQcPYeAE147cd3tHBDa9o14IFKNake5W4Y14Lj6.rb
@@ -1,20 +1,16 @@
 
         
-                # This returns all the config classes for the various providers.
-        #
-        # @return [Hash]
-        def provider_configs
-          Registry.new.tap do |result|
-            @registered.each do |plugin|
-              result.merge!(plugin.components.configs[:provider])
-            end
-          end
-        end
+        if defined?(ActionMailer)
+  class Devise::Mailer < Devise.parent_mailer.constantize
+    include Devise::Mailers::Helpers
     
-            # Registers a SIGINT handler. This typically is called from {busy}.
-        # Callbacks are only registered once, so calling this multiple times
-        # with the same callback has no consequence.
-        def register(sig_callback)
-          @@mutex.synchronize do
-            registered << sig_callback
-            registered.uniq!
+      def test_timegm
+    if negative_time_t?
+      assert_equal(-0x80000000, Time.utc(1901, 12, 13, 20, 45, 52).tv_sec)
+      assert_equal(-2, Time.utc(1969, 12, 31, 23, 59, 58).tv_sec)
+      assert_equal(-1, Time.utc(1969, 12, 31, 23, 59, 59).tv_sec)
+    end
+    
+      def self.counter
+    @@counter
+  end
