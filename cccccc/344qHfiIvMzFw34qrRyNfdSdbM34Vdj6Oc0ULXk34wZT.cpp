7a2440b380b888382568e185b71435aefa2b0f41
SUNsung <weatherius@gmail.com>
2018-05-29 16:57:20 +0300

    Rewrite [344qHfiIvMzFw34qrRyNfdSdbM34Vdj6Oc0ULXk34wZT.cpp] File in: 2018-05-29 16:57:20

--- cccccc/344qHfiIvMzFw34qrRyNfdSdbM34Vdj6Oc0ULXk34wZT.cpp
+++ cccccc/344qHfiIvMzFw34qrRyNfdSdbM34Vdj6Oc0ULXk34wZT.cpp
@@ -1,181 +1,251 @@
 
         
-        
-    {  /// Returns true if \p BB is a dead-end block.
-  bool isDeadEnd(SILBasicBlock *BB) {
-    if (!isComputed) {
-      // Lazily compute the dataflow.
-      compute();
-      isComputed = true;
-    }
-    return ReachableBlocks.count(BB) == 0;
-  }
-};
+        // Build a Table file from the contents of *iter.  The generated file
+// will be named according to meta->number.  On success, the rest of
+// *meta will be filled with metadata about the generated table.
+// If no data is present in *iter, meta->file_size will be set to
+// zero, and no Table file will be produced.
+extern Status BuildTable(const std::string& dbname,
+                         Env* env,
+                         const Options& options,
+                         TableCache* table_cache,
+                         Iterator* iter,
+                         FileMetaData* meta);
     
-    typedef NS_ENUM(NSInteger, ObjectBehaviorAction) {
-    ObjectBehaviorActionRetain,
-    ObjectBehaviorActionCopy,
-    ObjectBehaviorActionMutableCopy
-};
-    
-      void finish() override {
-    for (auto &pair : TrackerByGroup) {
-      StringRef groupName = pair.first();
-      SymbolTracker &tracker = *pair.second;
-      bool cont = onFinish(groupName, tracker);
-      if (!cont)
-        break;
-    }
-  }
-    
-      ArrayRef<const MarkupASTNode *> getChildren() const {
-    return {};
-  }
-    
-    using namespace swift;
-    
-    #include 'llvm/ADT/IntrusiveRefCntPtr.h'
-    
-    // On macOS and iOS, swift_once is implemented using GCD.
-// The compiler emits an inline check matching the barrier-free inline fast
-// path of dispatch_once(). See SwiftTargetInfo.OnceDonePredicateValue.
-    
-    // Each function below returns a Mapping, or a Sequence of descriptors.
-// They all return a new reference.
-    
-    
-    {
-    {}  // namespace
-}  // namespace protobuf
-    
-    #include <string>
-#include <google/protobuf/compiler/code_generator.h>
-    
-      void Generate(io::Printer* printer);
-    
-    class ReflectionClassGenerator : public SourceGeneratorBase {
- public:
-  ReflectionClassGenerator(const FileDescriptor* file, const Options* options);
-  ~ReflectionClassGenerator();
-    }
-    
-    void RepeatedEnumFieldGenerator::GenerateSerializedSizeCode(io::Printer* printer) {  
-  printer->Print(
-    variables_,
-    'size += $name$_.CalculateSize(_repeated_$name$_codec);\n');
+    void AppendInternalKey(std::string* result, const ParsedInternalKey& key) {
+  result->append(key.user_key.data(), key.user_key.size());
+  PutFixed64(result, PackSequenceAndType(key.sequence, key.type));
 }
     
-    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
-//   can only really compare the output against a golden value, which is a
-//   fairly tedious and fragile testing strategy.  If we want to go that route,
-//   it probably makes sense to bite the bullet and write a test that compares
-//   the whole generated output for unittest.proto against a golden value, with
-//   a very simple script that can be run to regenerate it with the latest code.
-//   This would mean that updates to the golden file would have to be included
-//   in any change to the code generator, which would actually be fairly useful
-//   as it allows the reviewer to see clearly how the generated code is
-//   changing.
+      // Return the user key
+  Slice user_key() const { return Slice(kstart_, end_ - kstart_ - 8); }
     
-      // Builder addRepeatedField(Field value)
-  WriteFieldDocComment(printer, descriptor_);
-  PrintNestedBuilderFunction(printer,
-    '$deprecation$public Builder add$capitalized_name$($type$ value)',
-    
-    
-    {}
-    
-    struct IRGS;
-    
-    #if defined(HAVE_UTIMES)
-# include <sys/time.h>
-#elif defined(HAVE_UTIME)
-# if defined(HAVE_SYS_UTIME_H)
-#  include <sys/utime.h>
-# elif defined(HAVE_UTIME_H)
-#  include <utime.h>
-# endif
-#endif
-    
-      Instr imm = Assembler::ImmPCRelAddress(target - this);
-    
-      typedef std::function<void(pid_t)> LostChildHandler;
-  static void SetLostChildHandler(const LostChildHandler& handler);
-    
-    
-    {  while (!got_sigint && !worker.Done()) {
-    gpr_sleep_until(gpr_time_add(gpr_now(GPR_CLOCK_REALTIME),
-                                 gpr_time_from_seconds(5, GPR_TIMESPAN)));
+    // Print contents of a log file. (*func)() is called on every record.
+Status PrintLogContents(Env* env, const std::string& fname,
+                        void (*func)(uint64_t, Slice, WritableFile*),
+                        WritableFile* dst) {
+  SequentialFile* file;
+  Status s = env->NewSequentialFile(fname, &file);
+  if (!s.ok()) {
+    return s;
   }
+  CorruptionReporter reporter;
+  reporter.dst_ = dst;
+  log::Reader reader(file, &reporter, true, 0);
+  Slice record;
+  std::string scratch;
+  while (reader.ReadRecord(&record, &scratch)) {
+    (*func)(reader.LastRecordOffset(), record, dst);
+  }
+  delete file;
+  return Status::OK();
 }
     
-    #endif  // GRPC_COMMON_CPP_ROUTE_GUIDE_HELPER_H_
+    // Return the name of the log file with the specified number
+// in the db named by 'dbname'.  The result will be prefixed with
+// 'dbname'.
+extern std::string LogFileName(const std::string& dbname, uint64_t number);
     
-    namespace {
-    }
+      // If a seek to internal key 'k' in specified file finds an entry,
+  // call (*handle_result)(arg, found_key, found_value).
+  Status Get(const ReadOptions& options,
+             uint64_t file_number,
+             uint64_t file_size,
+             const Slice& k,
+             void* arg,
+             void (*handle_result)(void*, const Slice&, const Slice&));
     
-    int main(int argc, char** argv) {
-  grpc::testing::InitTest(&argc, &argv, true);
-  signal(SIGINT, sigint_handler);
-    }
+      ASSERT_TRUE(! Overlaps('a', 'b'));
+  ASSERT_TRUE(! Overlaps('z1', 'z2'));
+  ASSERT_TRUE(Overlaps('a', 'p'));
+  ASSERT_TRUE(Overlaps('a', 'q'));
+  ASSERT_TRUE(Overlaps('a', 'z'));
+  ASSERT_TRUE(Overlaps('p', 'p1'));
+  ASSERT_TRUE(Overlaps('p', 'q'));
+  ASSERT_TRUE(Overlaps('p', 'z'));
+  ASSERT_TRUE(Overlaps('p1', 'p2'));
+  ASSERT_TRUE(Overlaps('p1', 'z'));
+  ASSERT_TRUE(Overlaps('q', 'q'));
+  ASSERT_TRUE(Overlaps('q', 'q1'));
     
-    #include 'test/cpp/qps/usage_timer.h'
+        sqlite3_stmt *replace_stmt, *begin_trans_stmt, *end_trans_stmt;
+    std::string replace_str = 'REPLACE INTO test (key, value) VALUES (?, ?)';
+    std::string begin_trans_str = 'BEGIN TRANSACTION;';
+    std::string end_trans_str = 'END TRANSACTION;';
     
-    double Node::getComputedWidth(void) const
-{
-    return YGNodeLayoutGetWidth(m_node);
-}
+    #include <google/protobuf/stubs/logging.h>
+#include <google/protobuf/stubs/common.h>
+#include <google/protobuf/descriptor.pb.h>
+#include <google/protobuf/pyext/message.h>
+#include <google/protobuf/pyext/scoped_pyobject_ptr.h>
     
-        method(getComputedWidth);
-    method(getComputedHeight);
+      virtual void WriteHash(io::Printer* printer);
+  virtual void WriteEquals(io::Printer* printer);
+  virtual void WriteToString(io::Printer* printer);
     
     
-    {} // namespace facebook
+    {}  // namespace google
+#endif  // GOOGLE_PROTOBUF_COMPILER_JAVA_EXTENSION_LITE_H__
 
     
-    #define FROM_HERE facebook::ProgramLocation(__FUNCTION__, __FILE__, __LINE__)
+      // implements CodeGenerator ----------------------------------------
+  bool Generate(const FileDescriptor* file,
+                const string& parameter,
+                GeneratorContext* context,
+                string* error) const;
     
-      void PrintFeatureSet() {
-    for (size_t i = 0; i < kFeatureSetSize; i++) {
-      if(size_t Sz = GetFeature(i))
-        Printf('[%zd: id %zd sz%zd] ', i, SmallestElementPerFeature[i], Sz);
-    }
-    Printf('\n\t');
-    for (size_t i = 0; i < Inputs.size(); i++)
-      if (size_t N = Inputs[i]->NumFeatures)
-        Printf(' %zd=>%zd ', i, N);
-    Printf('\n');
-  }
+    #include <google/protobuf/compiler/java/java_generator_factory.h>
     
-    extern 'C' {
-// Declare these symbols as weak to allow them to be optionally defined.
-#define EXT_FUNC(NAME, RETURN_TYPE, FUNC_SIG, WARN)                            \
-  __attribute__((weak)) RETURN_TYPE NAME FUNC_SIG
+    void ImmutableLazyMessageFieldGenerator::
+GenerateBuildingCode(io::Printer* printer) const {
+  printer->Print(variables_,
+      'if ($get_has_field_bit_from_local$) {\n'
+      '  $set_has_field_bit_to_local$;\n'
+      '}\n');
     }
     
-    std::string Base64(const Unit &U) {
-  static const char Table[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-                              'abcdefghijklmnopqrstuvwxyz'
-                              '0123456789+/';
-  std::string Res;
-  size_t i;
-  for (i = 0; i + 2 < U.size(); i += 3) {
-    uint32_t x = (U[i] << 16) + (U[i + 1] << 8) + U[i + 2];
-    Res += Table[(x >> 18) & 63];
-    Res += Table[(x >> 12) & 63];
-    Res += Table[(x >> 6) & 63];
-    Res += Table[x & 63];
+        struct coeff_buf
+    {
+      uint8 *pData;
+      int block_num_x, block_num_y;
+      int block_len_x, block_len_y;
+      int block_size;
+    };
+    
+    # define OC_STATIC_ILOG0(_v) (!!(_v))
+# define OC_STATIC_ILOG1(_v) (((_v)&0x2)?2:OC_STATIC_ILOG0(_v))
+# define OC_STATIC_ILOG2(_v) \
+ (((_v)&0xC)?2+OC_STATIC_ILOG1((_v)>>2):OC_STATIC_ILOG1(_v))
+# define OC_STATIC_ILOG3(_v) \
+ (((_v)&0xF0)?4+OC_STATIC_ILOG2((_v)>>4):OC_STATIC_ILOG2(_v))
+# define OC_STATIC_ILOG4(_v) \
+ (((_v)&0xFF00)?8+OC_STATIC_ILOG3((_v)>>8):OC_STATIC_ILOG3(_v))
+# define OC_STATIC_ILOG5(_v) \
+ (((_v)&0xFFFF0000)?16+OC_STATIC_ILOG4((_v)>>16):OC_STATIC_ILOG4(_v))
+# define OC_STATIC_ILOG6(_v) \
+ (((_v)&0xFFFFFFFF00000000ULL)?32+OC_STATIC_ILOG5((_v)>>32):OC_STATIC_ILOG5(_v))
+/**
+ * OC_STATIC_ILOG_32 - The integer logarithm of an (unsigned, 32-bit) constant.
+ * @_v: A non-negative 32-bit constant.
+ * Returns floor(log2(_v))+1, or 0 if _v==0.
+ * This is the number of bits that would be required to represent _v in two's
+ *  complement notation with all of the leading zeros stripped.
+ * This macro is suitable for evaluation at compile time, but it should not be
+ *  used on values that can change at runtime, as it operates via exhaustive
+ *  search.
+ */
+# define OC_STATIC_ILOG_32(_v) (OC_STATIC_ILOG5((ogg_uint32_t)(_v)))
+/**
+ * OC_STATIC_ILOG_64 - The integer logarithm of an (unsigned, 64-bit) constant.
+ * @_v: A non-negative 64-bit constant.
+ * Returns floor(log2(_v))+1, or 0 if _v==0.
+ * This is the number of bits that would be required to represent _v in two's
+ *  complement notation with all of the leading zeros stripped.
+ * This macro is suitable for evaluation at compile time, but it should not be
+ *  used on values that can change at runtime, as it operates via exhaustive
+ *  search.
+ */
+# define OC_STATIC_ILOG_64(_v) (OC_STATIC_ILOG6((ogg_int64_t)(_v)))
+    
+    /*Accesses one of four (signed) bytes given an index.
+  This can be used to avoid small lookup tables.*/
+#define OC_BYTE_TABLE32(_a,_b,_c,_d,_i) \
+  ((signed char) \
+   (((_a)&0xFF|((_b)&0xFF)<<8|((_c)&0xFF)<<16|((_d)&0xFF)<<24)>>(_i)*8))
+/*Accesses one of eight (unsigned) nibbles given an index.
+  This can be used to avoid small lookup tables.*/
+#define OC_UNIBBLE_TABLE32(_a,_b,_c,_d,_e,_f,_g,_h,_i) \
+  ((((_a)&0xF|((_b)&0xF)<<4|((_c)&0xF)<<8|((_d)&0xF)<<12| \
+   ((_e)&0xF)<<16|((_f)&0xF)<<20|((_g)&0xF)<<24|((_h)&0xF)<<28)>>(_i)*4)&0xF)
+    
+    #include 'x86/x86cpu.h'
+/* We currently support 5 x86 variants:
+ * arch[0] -> non-sse
+ * arch[1] -> sse
+ * arch[2] -> sse2
+ * arch[3] -> sse4.1
+ * arch[4] -> avx
+ */
+#define OPUS_ARCHMASK 7
+int opus_select_arch(void);
+    
+       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+    
+    #define silk_SAT8(a)                        ((a) > silk_int8_MAX ? silk_int8_MAX  :       \
+                                            ((a) < silk_int8_MIN ? silk_int8_MIN  : (a)))
+#define silk_SAT16(a)                       ((a) > silk_int16_MAX ? silk_int16_MAX :      \
+                                            ((a) < silk_int16_MIN ? silk_int16_MIN : (a)))
+#define silk_SAT32(a)                       ((a) > silk_int32_MAX ? silk_int32_MAX :      \
+                                            ((a) < silk_int32_MIN ? silk_int32_MIN : (a)))
+    
+      // Now map the buffer into the user space process as read only.
+  osquery.mm = osquery.md->createMappingInTask(
+      current_task(), NULL, kIOMapAnywhere | kIOMapReadOnly);
+  if (osquery.mm == NULL) {
+    err = -EINVAL;
+    goto error_exit;
   }
-  if (i + 1 == U.size()) {
-    uint32_t x = (U[i] << 16);
-    Res += Table[(x >> 18) & 63];
-    Res += Table[(x >> 12) & 63];
-    Res += '==';
-  } else if (i + 2 == U.size()) {
-    uint32_t x = (U[i] << 16) + (U[i + 1] << 8);
-    Res += Table[(x >> 18) & 63];
-    Res += Table[(x >> 12) & 63];
-    Res += Table[(x >> 6) & 63];
-    Res += '=';
+    
+    
+    {/// Clear decorations for a source when it updates.
+void clearDecorations(const std::string& source);
+}
+
+    
+      if (count > nelms) {
+    return Status(1, 'Failed to strncpy: dst too small');
   }
-  return Res;
+    
+      EXPECT_EQ(FLAGS_test_double_alias, 4.2);
+  FLAGS_test_double_alias = 2.4;
+  EXPECT_EQ(FLAGS_test_double, 2.4);
+  FLAGS_test_double = 22.44;
+  EXPECT_EQ(FLAGS_test_double_alias, 22.44);
+  EXPECT_TRUE(FLAGS_test_double_alias > 0);
+    
+    TEST_F(StatusTests, test_constructor_2) {
+  Status s;
+  EXPECT_EQ(s.getCode(), 0);
+  EXPECT_EQ(s.getMessage(), 'OK');
+}
+    
+    #endif
+
+    
+    void JniCallback::releaseJniEnv(jboolean& attached) const {
+  JniUtil::releaseJniEnv(m_jvm, attached);
+}
+    
+    namespace rocksdb {
+    }
+    
+      // Update counters and flush status after inserting a whole write batch
+  // Used in concurrent memtable inserts.
+  void BatchPostProcess(const MemTablePostProcessInfo& update_counters) {
+    num_entries_.fetch_add(update_counters.num_entries,
+                           std::memory_order_relaxed);
+    data_size_.fetch_add(update_counters.data_size, std::memory_order_relaxed);
+    if (update_counters.num_deletes != 0) {
+      num_deletes_.fetch_add(update_counters.num_deletes,
+                             std::memory_order_relaxed);
+    }
+    UpdateFlushState();
+  }
+    
+    #include 'util/sync_point.h'
+#include 'util/sync_point_impl.h'
+    
+    const char* CompactionFilterFactoryJniCallback::Name() const {
+  return m_name.get();
 }
