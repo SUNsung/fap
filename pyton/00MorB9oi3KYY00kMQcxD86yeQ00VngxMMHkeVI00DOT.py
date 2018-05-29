c9433b6e018ae96e1c188714117309b5c0eadfb6
SUNsung <weatherius@gmail.com>
2018-05-30 02:25:56 +0300

    Rewrite [00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py] File in: 2018-05-30 02:25:56

--- pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
+++ pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
@@ -1,105 +1,180 @@
 
         
-        from sklearn.cluster import AgglomerativeClustering
+          # Lookahead LSTM reads right-to-left to represent the rightmost context of the
+  # words. It gets word embeddings from the char model.
+  lookahead = spec_builder.ComponentSpecBuilder('lookahead')
+  lookahead.set_network_unit(
+      name='wrapped_units.LayerNormBasicLSTMNetwork',
+      hidden_layer_sizes='256')
+  lookahead.set_transition_system(name='shift-only', left_to_right='false')
+  lookahead.add_link(source=char2word, fml='input.last-char-focus',
+                     embedding_dim=64)
     
-        selected_algorithm = opts.selected_algorithm.split(',')
-    for key in selected_algorithm:
-        if key not in default_algorithms.split(','):
-            raise ValueError('Unknown sampling algorithm \'%s\' not in (%s).'
-                             % (key, default_algorithms))
+    py_library(
+    name = 'mst_units',
+    srcs = ['mst_units.py'],
+    deps = [
+        ':mst_ops',
+        ':network_units',
+        '//syntaxnet/util:check',
+        '@org_tensorflow//tensorflow:tensorflow_py',
+    ],
+)
+    
+        self._weights = []
+    self._weights.append(
+        tf.get_variable('weights_arc', [self._source_dim, self._target_dim],
+                        tf.float32, tf.orthogonal_initializer()))
+    self._weights.append(
+        tf.get_variable('weights_source', [self._source_dim], tf.float32,
+                        tf.zeros_initializer()))
+    self._weights.append(
+        tf.get_variable('root', [self._source_dim], tf.float32,
+                        tf.zeros_initializer()))
+    
+        self.setUp()
+    builder = bulk_component.BulkAnnotatorComponentBuilder(
+        self.master, component_spec)
+    builder.network = AssertPreCreateBeforeCreateNetwork(builder, self)
+    builder.build_greedy_training(
+        component.MasterState(['foo', 'bar'], 2), self.network_states)
+    
+      source_tokens_shape = tf.shape(source_tokens)
+  target_tokens_shape = tf.shape(target_tokens)
+  batch_size = source_tokens_shape[0]
+  num_tokens = source_tokens_shape[1]
+  with tf.control_dependencies([
+      tf.assert_equal(batch_size, target_tokens_shape[0]),
+      tf.assert_equal(num_tokens, target_tokens_shape[1])]):
+    # Flatten out the batch dimension so we can use one big multiplication.
+    targets_bnxt = tf.reshape(target_tokens, [-1, num_target_activations])
+    
+      Replaces resource paths in the MasterSpec with shortened paths and builds a
+  mapping from the shortened path to the original path. Note that shortened
+  paths are relative to the 'assets.extra' directory of the SavedModel. Also
+  removes resources from FixedFeatureChannel, since they are not exported.
+    
+      def testEqualFiles(self):
+    content_actual = None
+    content_expected = None
+    
+      # To eliminate overflow, we locally normalize the scores.  Specifically, for
+  # each node we divide its incoming arc scores and root selection score by the
+  # maximum such score.  Since each node in a tree must select exactly one of
+  # these scores (i.e., it is either a root or has exactly one incoming arc),
+  # the local normalization factors are identical for all trees and can thus be
+  # factored out of the sum over trees.
+  #
+  # More concretely, we find the maximum per node, divide all scores for that
+  # node by the maximum, and then find the partition function of the normalized
+  # scores.  Then we recover the un-normalized partition function by multiplying
+  # the per-node maxima back in.  This final step is performed in the log domain
+  # to avoid overflow.
+  #
+  # Note that underflow is still possible, but unlikely as long as the scores
+  # are close to feasible (i.e., there is not too much mass on non-trees).  The
+  # |max_dynamic_range| argument can be used to mitigate this.
+    
+    '''
+requests.hooks
+~~~~~~~~~~~~~~
     
     
-if __name__ == '__main__':
+@pytest.fixture
+def httpbin_secure(httpbin_secure):
+    return prepare_url(httpbin_secure)
+
     
-        if not os.path.exists(ARCHIVE_NAME):
-        print('Downloading dataset from %s (3 MB)' % URL)
-        opener = urlopen(URL)
-        with open(ARCHIVE_NAME, 'wb') as archive:
-            archive.write(opener.read())
+        return out
     
-        if not os.path.exists(ARCHIVE_NAME):
-        print('Downloading dataset from %s (14 MB)' % URL)
-        opener = urlopen(URL)
-        with open(ARCHIVE_NAME, 'wb') as archive:
-            archive.write(opener.read())
+        def __delitem__(self, key):
+        del self._store[key.lower()]
     
-        # Print and plot the confusion matrix
-    cm = metrics.confusion_matrix(y_test, y_predicted)
-    print(cm)
+        def test_copy(self):
+        copy = self.case_insensitive_dict.copy()
+        assert copy is not self.case_insensitive_dict
+        assert copy == self.case_insensitive_dict
     
-    exercise_dir = os.path.dirname(__file__)
-if exercise_dir == '':
-    exercise_dir = '.'
+        def get_new_headers(self):
+        return self._new_headers
     
-    Adjusted for chance measure such as ARI display some random variations
-centered around a mean score of 0.0 for any number of samples and
-clusters.
+        with server as (host, port):
+        url = 'http://{0}:{1}/'.format(host, port)
+        r = requests.get(url, auth=auth)
+        # Verify server didn't receive auth from us.
+        assert r.status_code == 200
+        assert len(r.history) == 0
+        close_server.set()
     
-    A Parser needs a TokenStream as input (which in turn is usually fed by a
-Lexer):
+        Provides a general-case interface for Requests sessions to contact HTTP and
+    HTTPS urls by implementing the Transport Adapter interface. This class will
+    usually be created by the :class:`Session <Session>` class under the
+    covers.
     
-                ret += [v] * n
+        if implementation == 'CPython':
+        implementation_version = platform.python_version()
+    elif implementation == 'PyPy':
+        implementation_version = '%s.%s.%s' % (sys.pypy_version_info.major,
+                                               sys.pypy_version_info.minor,
+                                               sys.pypy_version_info.micro)
+        if sys.pypy_version_info.releaselevel != 'final':
+            implementation_version = ''.join([
+                implementation_version, sys.pypy_version_info.releaselevel
+            ])
+    elif implementation == 'Jython':
+        implementation_version = platform.python_version()  # Complete Guess
+    elif implementation == 'IronPython':
+        implementation_version = platform.python_version()  # Complete Guess
+    else:
+        implementation_version = 'Unknown'
     
-            # and some (genuinely) random tests
-        for _ in range(10000):
-            a_bits = random.randrange(1000)
-            b_bits = random.randrange(1, 1000)
-            x = random.randrange(2**a_bits)
-            y = random.randrange(1, 2**b_bits)
-            self.check_truediv(x, y)
-            self.check_truediv(x, -y)
-            self.check_truediv(-x, y)
-            self.check_truediv(-x, -y)
+        :param method: method for the new :class:`Request` object.
+    :param url: URL for the new :class:`Request` object.
+    :param params: (optional) Dictionary or bytes to be sent in the query string for the :class:`Request`.
+    :param data: (optional) Dictionary or list of tuples ``[(key, value)]`` (will be form-encoded), bytes, or file-like object to send in the body of the :class:`Request`.
+    :param json: (optional) A JSON serializable Python object to send in the body of the :class:`Request`.
+    :param headers: (optional) Dictionary of HTTP Headers to send with the :class:`Request`.
+    :param cookies: (optional) Dict or CookieJar object to send with the :class:`Request`.
+    :param files: (optional) Dictionary of ``'name': file-like-objects`` (or ``{'name': file-tuple}``) for multipart encoding upload.
+        ``file-tuple`` can be a 2-tuple ``('filename', fileobj)``, 3-tuple ``('filename', fileobj, 'content_type')``
+        or a 4-tuple ``('filename', fileobj, 'content_type', custom_headers)``, where ``'content-type'`` is a string
+        defining the content type of the given file and ``custom_headers`` a dict-like object containing additional headers
+        to add for the file.
+    :param auth: (optional) Auth tuple to enable Basic/Digest/Custom HTTP Auth.
+    :param timeout: (optional) How many seconds to wait for the server to send data
+        before giving up, as a float, or a :ref:`(connect timeout, read
+        timeout) <timeouts>` tuple.
+    :type timeout: float or tuple
+    :param allow_redirects: (optional) Boolean. Enable/disable GET/OPTIONS/POST/PUT/PATCH/DELETE/HEAD redirection. Defaults to ``True``.
+    :type allow_redirects: bool
+    :param proxies: (optional) Dictionary mapping protocol to the URL of the proxy.
+    :param verify: (optional) Either a boolean, in which case it controls whether we verify
+            the server's TLS certificate, or a string, in which case it must be a path
+            to a CA bundle to use. Defaults to ``True``.
+    :param stream: (optional) if ``False``, the response content will be immediately downloaded.
+    :param cert: (optional) if String, path to ssl client cert file (.pem). If Tuple, ('cert', 'key') pair.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
     
-        def test_pickle(self):
-        for d in deque(range(200)), deque(range(200), 100):
-            for i in range(pickle.HIGHEST_PROTOCOL + 1):
-                s = pickle.dumps(d, i)
-                e = pickle.loads(s)
-                self.assertNotEqual(id(e), id(d))
-                self.assertEqual(list(e), list(d))
-                self.assertEqual(e.maxlen, d.maxlen)
+        return ip_string[:ip_string_size.value - 1]
     
-    import re
+    class MismatchedSetException(RecognitionException):
+    '''@brief The next token does not match a set of expected types.'''
     
-        def get_sequences(self):
-        '''Return a name-to-key-list dictionary to define each sequence.'''
-        results = {}
-        with open(os.path.join(self._path, '.mh_sequences'), 'r', encoding='ASCII') as f:
-            all_keys = set(self.keys())
-            for line in f:
-                try:
-                    name, contents = line.split(':')
-                    keys = set()
-                    for spec in contents.split():
-                        if spec.isdigit():
-                            keys.add(int(spec))
-                        else:
-                            start, stop = (int(x) for x in spec.split('-'))
-                            keys.update(range(start, stop + 1))
-                    results[name] = [key for key in sorted(keys) \
-                                         if key in all_keys]
-                    if len(results[name]) == 0:
-                        del results[name]
-                except ValueError:
-                    raise FormatError('Invalid sequence specification: %s' %
-                                      line.rstrip())
-        return results
+    api('reltableitem', RelTableItemJsonTemplate)
+api('bannedtableitem', BannedTableItemJsonTemplate)
+api('mutedtableitem', MutedTableItemJsonTemplate)
+api('invitedmodtableitem', InvitedModTableItemJsonTemplate)
+api('friendtableitem', FriendTableItemJsonTemplate)
     
-        objects = []
-    libs = ['shell32.lib', 'comdlg32.lib', 'wsock32.lib', 'user32.lib', 'oleaut32.lib']
-    for moddefn in moddefns:
-        print('# Module', moddefn.name)
-        for file in moddefn.sourceFiles:
-            base = os.path.basename(file)
-            base, ext = os.path.splitext(base)
-            objects.append(base + '.obj')
-            print(r'$(temp_dir)\%s.obj: '%s'' % (base, file))
-            print('\t@$(CC) -c -nologo /Fo$* $(cdl) $(c_debug) /D BUILD_FREEZE', end=' ')
-            print(''-I$(pythonhome)/Include'  '-I$(pythonhome)/PC' \\')
-            print('\t\t$(cflags) $(cdebug) $(cinclude) \\')
-            extra = moddefn.GetCompilerOptions()
-            if extra:
-                print('\t\t%s \\' % (' '.join(extra),))
-            print('\t\t'%s'' % file)
-            print()
+    
+class GoogleTagManagerController(MinimalController):
+    def pre(self):
+        if request.host != g.media_domain:
+            # don't serve up untrusted content except on our
+            # specifically untrusted domain
+            self.abort404()
+    
+        def GET_promohealth(self):
+        response.content_type = 'application/json'
+        return json.dumps(promote.health_check())
