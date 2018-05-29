d0a238567e803b6f7e69a8732c81bd8e8d472ed9
SUNsung <weatherius@gmail.com>
2018-05-29 09:32:05 +0300

    Rewrite [06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py] File in: 2018-05-29 09:32:05

--- pyton/06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py
+++ pyton/06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py
@@ -1,116 +1,187 @@
 
         
-            # Then we cluster the lines together as blocks
-    # Each block represents a collection of lines that should be sorted
-    # This was done by assuming only links ([...](...)) are meant to be sorted
-    # Clustering is done by indentation
-    blocks = []
-    last_indent = None
-    for line in read_me:
-        s_line = line.lstrip()
-        indent = len(line) - len(s_line)
+            try:
+        s1 = os.lstat(path)
+    except OSError:
+        # the OSError should be handled with more care
+        # it could be a 'permission denied' but path is still a mount
+        return False
+    else:
+        # A symlink can never be a mount point
+        if os.path.stat.S_ISLNK(s1.st_mode):
+            return False
     
-    import datetime
-import io
-import json
-import textwrap
+        headers = dict(
+        Authorization='apiToken %s' % args.key,
+    )
     
-        infile, outfile = args
+        'hostname': re.compile(
+        r'''^
+            {label}                     # We must have at least one label
+            (?:\.{label})*              # Followed by zero or more .labels
+            $
+        '''.format(label=label), re.X | re.I | re.UNICODE
+    ),
     
-    import youtube_dl.extractor
-from youtube_dl import YoutubeDL
-from youtube_dl.compat import (
-    compat_os_name,
-    compat_str,
-)
-from youtube_dl.utils import (
-    preferredencoding,
-    write_string,
-)
+        def test_max_delay_none(self):
+        strategy = _exponential_backoff(retries=7, delay=1, backoff=2, max_delay=None)
+        result = list(strategy())
+        self.assertEquals(result, [1, 2, 4, 8, 16, 32, 64])
     
-        def _real_extract(self, url):
-        video_id = self._match_id(url)
+        n_samples = 2000
+    list_n_features = np.linspace(500, 3000, 5).astype(np.int)
+    lasso_results, lars_lasso_results = compute_bench(alpha, [n_samples],
+                                           list_n_features, precompute=False)
+    plt.subplot(212)
+    plt.plot(list_n_features, lasso_results, 'b-', label='Lasso')
+    plt.plot(list_n_features, lars_lasso_results, 'r-', label='LassoLars')
+    plt.title('%d samples, alpha=%s' % (n_samples, alpha))
+    plt.legend(loc='upper left')
+    plt.xlabel('number of features')
+    plt.ylabel('Time (s)')
+    plt.axis('tight')
+    plt.show()
+
     
-    from .common import InfoExtractor
-from ..utils import js_to_json
+        Returns
+    -------
+    array of floats shaped like (metrics, formats, samples, classes, density)
+        Time in seconds.
+    '''
+    metrics = np.atleast_1d(metrics)
+    samples = np.atleast_1d(samples)
+    classes = np.atleast_1d(classes)
+    density = np.atleast_1d(density)
+    formats = np.atleast_1d(formats)
+    out = np.zeros((len(metrics), len(formats), len(samples), len(classes),
+                    len(density)), dtype=float)
+    it = itertools.product(samples, classes, density)
+    for i, (s, c, d) in enumerate(it):
+        _, y_true = make_multilabel_classification(n_samples=s, n_features=1,
+                                                   n_classes=c, n_labels=d * c,
+                                                   random_state=42)
+        _, y_pred = make_multilabel_classification(n_samples=s, n_features=1,
+                                                   n_classes=c, n_labels=d * c,
+                                                   random_state=84)
+        for j, f in enumerate(formats):
+            f_true = f(y_true)
+            f_pred = f(y_pred)
+            for k, metric in enumerate(metrics):
+                t = timeit(partial(metric, f_true, f_pred), number=n_times)
     
     
-class CloserToTruthIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?closertotruth\.com/(?:[^/]+/)*(?P<id>[^/?#&]+)'
-    _TESTS = [{
-        'url': 'http://closertotruth.com/series/solutions-the-mind-body-problem#video-3688',
-        'info_dict': {
-            'id': '0_zof1ktre',
-            'display_id': 'solutions-the-mind-body-problem',
-            'ext': 'mov',
-            'title': 'Solutions to the Mind-Body Problem?',
-            'upload_date': '20140221',
-            'timestamp': 1392956007,
-            'uploader_id': 'CTTXML'
-        },
-        'params': {
-            'skip_download': True,
-        },
-    }, {
-        'url': 'http://closertotruth.com/episodes/how-do-brains-work',
-        'info_dict': {
-            'id': '0_iuxai6g6',
-            'display_id': 'how-do-brains-work',
-            'ext': 'mov',
-            'title': 'How do Brains Work?',
-            'upload_date': '20140221',
-            'timestamp': 1392956024,
-            'uploader_id': 'CTTXML'
-        },
-        'params': {
-            'skip_download': True,
-        },
-    }, {
-        'url': 'http://closertotruth.com/interviews/1725',
-        'info_dict': {
-            'id': '1725',
-            'title': 'AyaFr-002',
-        },
-        'playlist_mincount': 2,
-    }]
+def rbf_kernels(X, n_jobs):
+    return pairwise_kernels(X, metric='rbf', n_jobs=n_jobs, gamma=0.1)
     
-        def test_cert_file_not_found(self, httpbin_secure):
-        r = http(httpbin_secure + '/get',
-                 '--cert', '/__not_found__',
-                 error_exit_ok=True)
-        assert r.exit_status == ExitStatus.ERROR
-        assert 'No such file or directory' in r.stderr
+        This is called by sphinx.ext.linkcode
     
-        def reducer(self, key, values):
-        '''Sum values for each key.
+    # Print the classification report
+print(metrics.classification_report(y_test, y_predicted,
+                                    target_names=dataset.target_names))
     
-        # when accessing non-existent attributes on c, return '' instead of dying
-    config['pylons.strict_tmpl_context'] = False
     
-    from r2.controllers.api_docs import api_doc, api_section
-from r2.controllers.oauth2 import require_oauth2_scope
-from r2.controllers.reddit_base import OAuth2OnlyController
-from r2.lib.jsontemplates import (
-    FriendTableItemJsonTemplate,
-    get_usertrophies,
-    IdentityJsonTemplate,
-    KarmaListJsonTemplate,
-    PrefsJsonTemplate,
-)
-from r2.lib.pages import FriendTableItem
-from r2.lib.validator import (
-    validate,
-    VAccountByName,
-    VFriendOfMine,
-    VLength,
-    VList,
-    VUser,
-    VValidatedJSON,
-)
-from r2.models import Account, Trophy
-import r2.lib.errors as errors
-import r2.lib.validator.preferences as vprefs
+def main(argv):
+    op = get_parser()
+    opts, args = op.parse_args(argv[1:])
+    root = Tk.Tk()
+    model = Model()
+    controller = Controller(model)
+    root.wm_title('Scikit-learn Libsvm GUI')
+    view = View(root, controller)
+    model.add_observer(view)
+    Tk.mainloop()
     
-            return HelpPage(_('help'),
-                        content = Embed(content=output),
-                        show_sidebar = None).render()
+    # Train uncalibrated random forest classifier on whole train and validation
+# data and evaluate on test data
+clf = RandomForestClassifier(n_estimators=25)
+clf.fit(X_train_valid, y_train_valid)
+clf_probs = clf.predict_proba(X_test)
+score = log_loss(y_test, clf_probs)
+    
+    plt.show()
+
+    
+            X, y = generate_data(n_test, n_features)
+        score_clf1 += clf1.score(X, y)
+        score_clf2 += clf2.score(X, y)
+    
+    
+def uniform_labelings_scores(score_func, n_samples, n_clusters_range,
+                             fixed_n_classes=None, n_runs=5, seed=42):
+    '''Compute score for 2 random uniform cluster labelings.
+    
+        def __init__(self, input=None):
+        Exception.__init__(self)
+    
+    
+    def combineFollows(self, exact):
+        followSet = set()
+        for idx, localFollowSet in reversed(list(enumerate(self._state.following))):
+            followSet |= localFollowSet
+            if exact:
+                # can we see end of rule?
+                if EOR_TOKEN_TYPE in localFollowSet:
+                    # Only leave EOR in set if at top (start rule); this lets
+                    # us know if have to include follow(start rule); i.e., EOF
+                    if idx > 0:
+                        followSet.remove(EOR_TOKEN_TYPE)
+                        
+                else:
+                    # can't see end of rule, quit
+                    break
+    
+            Using setter/getter methods is deprecated. Use o.index instead.'''
+    
+    # begin[licence]
+#
+# [The 'BSD licence']
+# Copyright (c) 2005-2008 Terence Parr
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# end[licence]
+    
+        def test_same_server(self):
+        from certbot_apache.obj import VirtualHost
+        no_name1 = VirtualHost(
+            'fp', 'vhp', set([self.addr1]), False, False, None)
+        no_name2 = VirtualHost(
+            'fp', 'vhp', set([self.addr2]), False, False, None)
+        no_name3 = VirtualHost(
+            'fp', 'vhp', set([self.addr_default]),
+            False, False, None)
+        no_name4 = VirtualHost(
+            'fp', 'vhp', set([self.addr2, self.addr_default]),
+            False, False, None)
+    
+    
+# -- Options for manual page output ---------------------------------------
+    
+        def __getattr__(self, name):
+        '''Wraps the configurator methods'''
+        if self._configurator is None:
+            raise AttributeError()
+    
+    # If true, show URL addresses after external links.
+#man_show_urls = False
