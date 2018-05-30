dea0b30e94066dd9bd54b88c8aee91c018762d44
SUNsung <weatherius@gmail.com>
2018-05-31 02:46:34 +0300

    Rewrite [44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp] File in: 2018-05-31 02:46:34

--- cccccc/44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp
+++ cccccc/44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp
@@ -2,230 +2,87 @@
         
         
     {}  // namespace atom
-    
-    namespace atom {
-    }
-    
-      // Sent when an omnibox extension has sent back omnibox suggestions. The
-  // source is the profile, and the details are an
-  // extensions::api::omnibox::SendSuggestions::Params object.
-  NOTIFICATION_EXTENSION_OMNIBOX_SUGGESTIONS_READY,
-    
-     private:
-  // Non-owning pointer.
-  content::BrowserPpapiHost* host_;
-    
-    #endif  // CHROME_BROWSER_UI_VIEWS_COLOR_CHOOSER_DIALOG_H_
 
     
-    #define FPL FILE_PATH_LITERAL
-    
-    
-    {} // namespace swift
-    
-    #include 'swift/AST/SyntaxASTMap.h'
-#include 'swift/AST/Expr.h'
-#include 'swift/AST/Decl.h'
-#include 'swift/AST/Stmt.h'
-#include 'swift/Syntax/Syntax.h'
-    
-    public:
-  Impl(CompletionKind kind, bool hasExpectedTypes);
-    
-    /// Index the given module and store the results to \p indexStorePath.
-///
-/// \param module The module to index.
-///
-/// \param indexUnitTokens A list of unique identifiers for the index units to
-/// be written. This may either be one unit per source file of \p module, or it
-/// may be a single unit, in which case all the index information will be
-/// combined into a single unit.
-///
-/// \param moduleUnitToken A unique identifier for this module unit in the form
-/// of a file path. Only used if \p indexUnitTokens are specified for each
-/// source file, otherwise the single \p indexUnitTokens value is used instead.
-///
-/// \param indexStorePath The location to write the indexing data to.
-///
-/// \param indexSystemModules If true, emit index data for imported serialized
-/// swift system modules.
-///
-/// \param isDebugCompilation true for non-optimized compiler invocation.
-///
-/// \param targetTriple The target for this compilation.
-///
-/// \param dependencyTracker The set of dependencies seen while building.
-bool indexAndRecord(ModuleDecl *module, ArrayRef<std::string> indexUnitTokens,
-                    StringRef moduleUnitToken, StringRef indexStorePath,
-                    bool indexSystemModules, bool isDebugCompilation,
-                    StringRef targetTriple,
-                    const DependencyTracker &dependencyTracker);
-// FIXME: indexUnitTokens could be StringRef, but that creates an impedance
-// mismatch in the caller.
-    
-        // Finish and check for builder errors
-    if (s.ok()) {
-      s = builder->Finish();
-      if (s.ok()) {
-        meta->file_size = builder->FileSize();
-        assert(meta->file_size > 0);
-      }
-    } else {
-      builder->Abandon();
-    }
-    delete builder;
-    
-    #include <stdlib.h>
-#include <unistd.h>
-#include 'leveldb/cache.h'
-#include 'leveldb/comparator.h'
-#include 'leveldb/db.h'
-#include 'leveldb/env.h'
-#include 'leveldb/filter_policy.h'
-#include 'leveldb/iterator.h'
-#include 'leveldb/options.h'
-#include 'leveldb/status.h'
-#include 'leveldb/write_batch.h'
-    
-      for (size_t i = 0; i < compact_pointers_.size(); i++) {
-    PutVarint32(dst, kCompactPointer);
-    PutVarint32(dst, compact_pointers_[i].first);  // level
-    PutLengthPrefixedSlice(dst, compact_pointers_[i].second.Encode());
-  }
-    
-      ASSERT_TRUE(! Overlaps('a', 'b'));
-  ASSERT_TRUE(! Overlaps('z1', 'z2'));
-  ASSERT_TRUE(Overlaps('a', 'p'));
-  ASSERT_TRUE(Overlaps('a', 'q'));
-  ASSERT_TRUE(Overlaps('a', 'z'));
-  ASSERT_TRUE(Overlaps('p', 'p1'));
-  ASSERT_TRUE(Overlaps('p', 'q'));
-  ASSERT_TRUE(Overlaps('p', 'z'));
-  ASSERT_TRUE(Overlaps('p1', 'p2'));
-  ASSERT_TRUE(Overlaps('p1', 'z'));
-  ASSERT_TRUE(Overlaps('q', 'q'));
-  ASSERT_TRUE(Overlaps('q', 'q1'));
-    
-    void WriteBatchInternal::SetSequence(WriteBatch* b, SequenceNumber seq) {
-  EncodeFixed64(&b->rep_[0], seq);
-}
-    
-        const char* benchmarks = FLAGS_benchmarks;
-    while (benchmarks != NULL) {
-      const char* sep = strchr(benchmarks, ',');
-      Slice name;
-      if (sep == NULL) {
-        name = benchmarks;
-        benchmarks = NULL;
-      } else {
-        name = Slice(benchmarks, sep - benchmarks);
-        benchmarks = sep + 1;
-      }
+    struct DraggableRegion {
+  bool draggable;
+  gfx::Rect bounds;
     }
     
-    #ifndef STORAGE_LEVELDB_INCLUDE_FILTER_POLICY_H_
-#define STORAGE_LEVELDB_INCLUDE_FILTER_POLICY_H_
+    #endif  // CHROME_BROWSER_PRINTING_PRINT_VIEW_MANAGER_OBSERVER_H_
+
     
-    For CIDFonts, its a little more complicated. Because CIDFonts can be
-large, using a glyph name as the key is unreasonable (it would also
-lead to unfeasibly large Encoding arrays), so instead we use a 'CID'
-as the key. CIDs are just numbers.
-    
-    
-    {}  // namespace tesseract
-    
-      word_res = word_res_it.data();
-  if (word_res->word->flag(W_REP_CHAR) ||
-      word_res->combination ||
-      word_res->part_of_combo ||
-      !word_res->word->flag(W_DONT_CHOP))
-    return;
-    
-    #include <set>
-    
-    #include 'src/compiler/config.h'
-#include 'src/compiler/generator_helpers.h'
-    
-    
-    {  // Get all comments (leading, leading_detached, trailing) and print them as a
-  // docstring. Any leading space of a line will be removed, but the line
-  // wrapping will not be changed.
-  void PrintAllComments(std::vector<grpc::string> comments,
-                        grpc_generator::Printer* out);
-};
-    
-    std::vector<grpc::string_ref> SecureAuthContext::FindPropertyValues(
-    const grpc::string& name) const {
-  if (!ctx_) {
-    return std::vector<grpc::string_ref>();
-  }
-  grpc_auth_property_iterator iter =
-      grpc_auth_context_find_properties_by_name(ctx_, name.c_str());
-  const grpc_auth_property* property = nullptr;
-  std::vector<grpc::string_ref> values;
-  while ((property = grpc_auth_property_iterator_next(&iter))) {
-    values.push_back(grpc::string_ref(property->value, property->value_length));
-  }
-  return values;
-}
-    
-    #ifndef TEST_QPS_USAGE_TIMER_H
-#define TEST_QPS_USAGE_TIMER_H
-    
-    	while (!stateflag) {
-		int ret = buffer_data();
-		if (ret == 0) break;
-		while (ogg_sync_pageout(&oy, &og) > 0) {
-			ogg_stream_state test;
-    }
+    class DocComment {
+  const Decl *D;
+  const swift::markup::Document *Doc = nullptr;
+  const swift::markup::CommentParts Parts;
     }
     
-    //#define SIMPLE_METHODDEF
+    #ifndef SWIFT_INDEX_INDEXDATACONSUMER_H
+#define SWIFT_INDEX_INDEXDATACONSUMER_H
     
-    
-    {
-    {		property.hint_string = options;
+    static int set_flags(struct ifaddrs* ifaddr) {
+	int fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (fd == -1) {
+		return -1;
 	}
+	ifreq ifr;
+	memset(&ifr, 0, sizeof(ifr));
+	strncpy(ifr.ifr_name, ifaddr->ifa_name, IFNAMSIZ - 1);
+	int rc = ioctl(fd, SIOCGIFFLAGS, &ifr);
+	close(fd);
+	if (rc == -1) {
+		return -1;
+	}
+	ifaddr->ifa_flags = ifr.ifr_flags;
+	return 0;
 }
     
-    
-    {	virtual btScalar addSingleResult(btCollisionWorld::LocalConvexResult &convexResult, bool normalInWorldSpace);
-};
-    
-        FreeType font driver for pcf fonts
-    
-    SEXP XGBoosterEvalOneIter_R(SEXP handle, SEXP iter, SEXP dmats, SEXP evnames) {
-  const char *ret;
-  R_API_BEGIN();
-  CHECK_EQ(length(dmats), length(evnames))
-      << 'dmats and evnams must have same length';
-  int len = length(dmats);
-  std::vector<void*> vec_dmats;
-  std::vector<std::string> vec_names;
-  std::vector<const char*> vec_sptr;
-  for (int i = 0; i < len; ++i) {
-    vec_dmats.push_back(R_ExternalPtrAddr(VECTOR_ELT(dmats, i)));
-    vec_names.push_back(std::string(CHAR(asChar(VECTOR_ELT(evnames, i)))));
-  }
-  for (int i = 0; i < len; ++i) {
-    vec_sptr.push_back(vec_names[i].c_str());
-  }
-  CHECK_CALL(XGBoosterEvalOneIter(R_ExternalPtrAddr(handle),
-                                asInteger(iter),
-                                BeginPtr(vec_dmats),
-                                BeginPtr(vec_sptr),
-                                len, &ret));
-  R_API_END();
-  return mkString(ret);
+    PowerIphone::PowerIphone() :
+		nsecs_left(-1),
+		percent_left(-1),
+		power_state(OS::POWERSTATE_UNKNOWN) {
+	// TODO Auto-generated constructor stub
 }
     
-    TEST(Metric, MAP) {
-  xgboost::Metric * metric = xgboost::Metric::Create('map');
-  ASSERT_STREQ(metric->Name(), 'map');
-  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 1, 1e-10);
-  EXPECT_NEAR(GetMetricEval(metric,
-                            {0.1f, 0.9f, 0.1f, 0.9f},
-                            {  0,   0,   1,   1}),
-              0.5f, 0.001f);
-  EXPECT_NEAR(GetMetricEval(metric, {}, {}), 1, 1e-10);
+    StreamPeerMbedTLS::~StreamPeerMbedTLS() {
+	disconnect_from_stream();
+}
+    
+    	virtual void poll() = 0;
+	virtual Error connect_to_host(String p_host, String p_path, uint16_t p_port, bool p_ssl, PoolVector<String> p_protocol = PoolVector<String>()) = 0;
+	virtual void disconnect_from_host() = 0;
+	virtual IP_Address get_connected_host() const = 0;
+	virtual uint16_t get_connected_port() const = 0;
+    
+    int jpeg_decoder_mem_stream::read(uint8 *pBuf, int max_bytes_to_read, bool *pEOF_flag)
+{
+  *pEOF_flag = false;
     }
+    
+    
+# if defined(OC_CLZ32)
+/**
+ * OC_ILOGNZ_32 - Integer binary logarithm of a non-zero 32-bit value.
+ * @_v: A non-zero 32-bit value.
+ * Returns floor(log2(_v))+1.
+ * This is the number of bits that would be required to represent _v in two's
+ *  complement notation with all of the leading zeros stripped.
+ * If _v is zero, the return value is undefined; use OC_ILOG_32() instead.
+ */
+#  define OC_ILOGNZ_32(_v) (OC_CLZ32_OFFS-OC_CLZ32(_v))
+/**
+ * OC_ILOG_32 - Integer binary logarithm of a 32-bit value.
+ * @_v: A 32-bit value.
+ * Returns floor(log2(_v))+1, or 0 if _v==0.
+ * This is the number of bits that would be required to represent _v in two's
+ *  complement notation with all of the leading zeros stripped.
+ */
+#  define OC_ILOG_32(_v)   (OC_ILOGNZ_32(_v)&-!!(_v))
+# else
+#  define OC_ILOGNZ_32(_v) (oc_ilog32(_v))
+#  define OC_ILOG_32(_v)   (oc_ilog32(_v))
+# endif
+    
+      function:
+    last mod: $Id: ocintrin.h 16503 2009-08-22 18:14:02Z giles $
