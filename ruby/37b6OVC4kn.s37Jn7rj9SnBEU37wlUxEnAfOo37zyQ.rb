79c6ca2e3fd4b8e280933b8d580b90328e074361
SUNsung <weatherius@gmail.com>
2018-05-30 06:07:17 +0300

    Rewrite [37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb] File in: 2018-05-30 06:07:16

--- ruby/37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb
+++ ruby/37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb
@@ -1,62 +1,56 @@
 
         
-        html_readme = '<html>#{Kramdown::Document.new(open('README.md').read).to_html}</html>'
-readme_doctree = REXML::Document.new(html_readme)
-links = REXML::XPath.match(readme_doctree, '//a')
-    
-          t0 = Process.clock_gettime(Process::CLOCK_REALTIME)
-      File.write(path, 'foo')
-      sleep 2
-      File.write(path, 'bar')
-      sleep 2
-      File.read(path)
-      File.chmod(0644, path)
-      sleep 2
-      File.read(path)
-    
-      it 'decodes the remaining doubles when passed the '*' modifier' do
-    array = '333333\x07@ffffff\xf6?ffffff\x20@'.unpack(unpack_format('*'))
-    array.should == [2.9, 1.4, 8.2]
-  end
-    
-      it 'decodes UTF-8 BMP codepoints' do
-    [ ['\xc2\x80',      [0x80]],
-      ['\xdf\xbf',      [0x7ff]],
-      ['\xe0\xa0\x80',  [0x800]],
-      ['\xef\xbf\xbf',  [0xffff]]
-    ].should be_computed_by(:unpack, 'U')
-  end
-    
-        Thread.pass until in_ensure_clause == true
-    10.times { t.send(@method); Thread.pass }
-    exit_loop = true
-    t.join
-    ScratchPad.recorded.should == :after_stop
-  end
-  end
-    
-        time.usec.should == 0
-    time.nsec.should == 999
-  end
-    
-        # Remove directories opposite from traversal, so that a subtree with no
-    # actual files gets removed correctly.
-    dirs.reverse_each do |d|
-      if d.children.empty?
-        puts 'rmdir: #{d} (empty)' if ARGV.verbose?
-        d.rmdir
-      end
+              sets.map do |set|
+        if valid?(set)
+          ensure_time!(update_deprecated_types(set))
+        else
+          Jekyll.logger.warn 'Defaults:', 'An invalid front-matter default set was found:'
+          Jekyll.logger.warn set.to_s
+          nil
+        end
+      end.compact
     end
     
-        if arg == 'help' && !cmd
-      # Command-style help: `help <cmd>` is fine, but `<cmd> help` is not.
-      help_flag = true
-    elsif !cmd && !help_flag_list.include?(arg)
-      cmd = ARGV.delete_at(i)
-    end
-  end
+    unless File.exist?(PROF_OUTPUT_FILE)
+  StackProf.run(
+    mode: MODE.to_sym,
+    interval: 100,
+    raw: true,
+    out: PROF_OUTPUT_FILE
+  ) do
+    puts 'GC Stats:', JSON.pretty_generate(GC.stat)
+    GC.disable
     
-          def eject(mount)
-        # realpath is a failsafe against unusual filenames
-        mountpath = Pathname.new(mount).realpath
-        return unless mountpath.exist?
+    require 'json'
+require 'open-uri'
+    
+        def deprecation_message(message)
+      Jekyll.logger.warn 'Deprecation:', message
+    end
+    
+          def parse(content)
+        measure_time do
+          @template = Liquid::Template.parse(content, :line_numbers => true)
+        end
+    
+        # Spaceship is priority [higher -> lower]
+    #
+    # other - The class to be compared.
+    #
+    # Returns -1, 0, 1.
+    def self.<=>(other)
+      PRIORITIES[other.priority] <=> PRIORITIES[self.priority]
+    end
+    
+    class SinatraStaticServer < Sinatra::Base
+    
+    def config_tag(config, key, tag=nil, classname=nil)
+  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
+  tag       ||= 'div'
+  classname ||= key.sub(/_/, '-').sub(/\./, '-')
+  output      = '<#{tag} class='#{classname}''
+    
+    require 'cgi'
+require 'digest/md5'
+require 'net/https'
+require 'uri'
