defdaeb4e426de1e5a90b76c2c2178b5327941f1
SUNsung <weatherius@gmail.com>
2018-05-30 12:52:56 +0300

    Rewrite [49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb] File in: 2018-05-30 12:52:55

--- ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
+++ ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
@@ -1,41 +1,59 @@
 
         
-          include TestEOF
-  def open_file(content)
-    Tempfile.create('test-eof') {|f|
-      f << content
-      f.rewind
-      yield f
-    }
-  end
-  alias open_file_rw open_file
-    
-        assert_raise(ArgumentError) {
-      set.subset?([2])
-    }
-    
-      it 'handles bad leap seconds by carrying values forward' do
-    with_timezone 'UTC' do
-      time = Time.send(@method, 2017, 7, 5, 23, 59, 60)
-      time.sec.should == 0
-      time.min.should == 0
-      time.hour.should == 0
-      time.day.should == 6
-      time.month.should == 7
+            formula_deps.each do |dep|
+      fixopt(dep) unless dep.opt_prefix.directory?
     end
+    
+    class BuildEnvironment
+  def initialize(*settings)
+    @settings = Set.new(*settings)
   end
     
-    ###
-### dependencies
-###
+      private
     
-        def define
-      define_flush_errors
-      define_getters
-      define_setter
-      define_query
-      register_new_attachment
-      add_active_record_callbacks
-      add_paperclip_callbacks
-      add_required_validations
+        [@f.bin, @f.sbin, @f.lib].each { |d| clean_dir(d) if d.exist? }
+    
+      def self.path(name)
+    Formulary.core_path(name)
+  end
+    
+          it 'pass a custom build number to the tool' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          increment_build_number(build_number: 24, xcodeproj: '.xcproject')
+        end').runner.execute(:test)
+    
+          it 'returns the new version as return value' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          increment_version_number(bump_type: 'major')
+        end').runner.execute(:test)
+    
+          # This gets the value of the block with the given key.
+      def get(key)
+        key    = Regexp.quote(key)
+        regexp = /^#\s*VAGRANT-BEGIN:\s*#{key}$\r?\n?(.*?)\r?\n?^#\s*VAGRANT-END:\s*#{key}$\r?\n?/m
+        match  = regexp.match(@value)
+        return nil if !match
+        match[1]
+      end
+    
+        # Paths
+    def gem_path
+      @gem_path ||= File.expand_path '..', File.dirname(__FILE__)
     end
+    
+            path = File.join save_to, name.sub(/\.less$/, '.scss')
+        path = File.join File.dirname(path), '_' + File.basename(path)
+        save_file(path, file)
+        log_processed File.basename(path)
+      end
+    
+    desc 'Start a dummy (test) Rails app server'
+task :dummy_rails do
+  require 'rack'
+  require 'term/ansicolor'
+  port = ENV['PORT'] || 9292
+  puts %Q(Starting on #{Term::ANSIColor.cyan 'http://localhost:#{port}'})
+  Rack::Server.start(
+    config: 'test/dummy_rails/config.ru',
+    Port: port)
+end
