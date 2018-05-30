11d8197f1d4e193937221a27bc860c8d68b2b2b4
SUNsung <weatherius@gmail.com>
2018-05-30 23:20:31 +0300

    Rewrite [07bi.8e.RwHE07Fq6dkHK366073tBBTZ6raoY07gfY.rb] File in: 2018-05-30 23:20:30

--- ruby/07bi.8e.RwHE07Fq6dkHK366073tBBTZ6raoY07gfY.rb
+++ ruby/07bi.8e.RwHE07Fq6dkHK366073tBBTZ6raoY07gfY.rb
@@ -1,75 +1,119 @@
 
         
-              # Topic may be hard deleted due to spam, no point complaining
-      # we would have to look at the topics table id sequence to find cases
-      # where this was called with an invalid id, no point really
-      return unless topic.present?
-    
-        def execute(args)
-    
-        # The path used after resending confirmation instructions.
-    def after_resending_confirmation_instructions_path_for(resource_name)
-      is_navigational_format? ? new_session_path(resource_name) : '/'
-    end
-    
-        def email_changed(record, opts={})
-      devise_mail(record, :email_changed, opts)
-    end
-    
-          protected
-    
-      def inspect
-    '#<#{self.class.name}: #{strip.inspect} #{url.inspect}>'
-  end
-end
-    
-    class GitRequirement < Requirement
-  fatal true
-  satisfy do
-    odisabled('GitRequirement', ''depends_on \'git\''')
-  end
-end
-    
-          respond_with do |format|
-        format.html do
-          gon.preloads[:pods] = pods_json
-          gon.unchecked_count = Pod.unchecked.count
-          gon.version_failed_count = Pod.version_failed.count
-          gon.error_count = Pod.check_failed.count
-    
-      def create_hash(model, opts={})
-    opts[:range] ||= 1.day
-    plural = model.to_s.underscore.pluralize
-    eval(<<DATA
-      @#{plural} = {
-        :day_before => #{model}.where(:created_at => ((Time.now.midnight - #{opts[:range]*2})..Time.now.midnight - #{opts[:range]})).count,
-        :yesterday => #{model}.where(:created_at => ((Time.now.midnight - #{opts[:range]})..Time.now.midnight)).count
-      }
-      @#{plural}[:change] = percent_change(@#{plural}[:yesterday], @#{plural}[:day_before])
-DATA
-    )
+          def teardown
+    ActiveSupport.escape_html_entities_in_json = @old_escape_html_entities_in_json
   end
     
-          def response_type_as_space_seperated_values
-        [*session[:response_type]].join(' ')
-      end
+    module ActionCable
+  module Server
+    # An instance of this configuration object is available via ActionCable.server.config, which allows you to tweak Action Cable configuration
+    # in a Rails config initializer.
+    class Configuration
+      attr_accessor :logger, :log_tags
+      attr_accessor :connection_class, :worker_pool_size
+      attr_accessor :disable_request_forgery_protection, :allowed_request_origins, :allow_same_origin_as_host
+      attr_accessor :cable, :url, :mount_path
     
-          rescue_from Rack::OAuth2::Server::Authorize::BadRequest,
-                  JSON::JWT::InvalidFormat, JSON::JWK::UnknownAlgorithm do |e|
-        logger.info e.backtrace[0, 10].join('\n')
-        render json: {error: :invalid_request, error_description: e.message, status: 400}
-      end
-      rescue_from JSON::JWT::VerificationFailed do |e|
-        logger.info e.backtrace[0, 10].join('\n')
-        render json: {error: :invalid_grant, error_description: e.message, status: 400}
+                def when_connected(&block)
+              if @raw_client
+                block.call
+              else
+                @when_connected << block
+              end
+            end
+    
+      test 'token_and_options returns correct token with slashes' do
+    token = 'rcHu+\\\\'/896A'
+    actual = ActionController::HttpAuthentication::Token.token_and_options(sample_request(token)).first
+    expected = token
+    assert_equal(expected, actual)
+  end
+    
+    class WebServiceTest < ActionDispatch::IntegrationTest
+  class TestController < ActionController::Base
+    def assign_parameters
+      if params[:full]
+        render plain: dump_params_keys
+      else
+        render plain: (params.keys - ['controller', 'action']).sort.join(', ')
       end
     end
-  end
-end
-
     
-            raise t('error.invalid_stage_name', name: invalid, path: stage_config_path.join('#{invalid}.rb'))
-      end
+    module ActionMailer
+  # The <tt>ActionMailer::DeliveryJob</tt> class is used when you
+  # want to send emails outside of the request-response cycle.
+  #
+  # Exceptions are rescued and handled by the mailer class.
+  class DeliveryJob < ActiveJob::Base # :nodoc:
+    queue_as { ActionMailer::Base.deliver_later_queue_name }
+    
+        def call(env)
+      @stack.call(env)
     end
   end
-end
+    
+        assert_equal(1, @logger.logged(:info).size)
+    assert_match(/Sent mail to system@test\.lindsaar\.net/, @logger.logged(:info).first)
+    
+      protected
+    
+            expect_updated_sign_in_at(user)
+        expect(Redis.current.zcard(FeedManager.instance.key(:home, user.account_id))).to eq 3
+        expect(Redis.current.get('account:#{user.account_id}:regeneration')).to be_nil
+      end
+    end
+    
+    class ManifestSerializer < ActiveModel::Serializer
+  include RoutingHelper
+  include ActionView::Helpers::TextHelper
+    
+      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/follow
+  def follow
+    f = Follow.last
+    NotificationMailer.follow(f.target_account, Notification.find_by(activity: f))
+  end
+    
+            attr_reader :name, :declaration_node, :scope,
+                    :assignments, :references, :captured_by_block
+        alias captured_by_block? captured_by_block
+    
+    module RuboCop
+  module Cop
+    module Layout
+      # Checks for unnecessary additional spaces inside array percent literals
+      # (i.e. %i/%w).
+      #
+      # @example
+      #
+      #   # bad
+      #   %w(foo  bar  baz)
+      #   # good
+      #   %i(foo bar baz)
+      class SpaceInsideArrayPercentLiteral < Cop
+        include MatchRange
+        include PercentLiteral
+    
+            def nested_variable_comparison?(node)
+          return false unless nested_comparison?(node)
+          variables_in_node(node).count == 1
+        end
+    
+            # @param annotated_source [String] string passed to the matchers
+        #
+        # Separates annotation lines from source lines. Tracks the real
+        # source line number that each annotation corresponds to.
+        #
+        # @return [AnnotatedSource]
+        def self.parse(annotated_source)
+          source      = []
+          annotations = []
+    
+          origin = caller[1]
+      if origin =~ /rubygems\/custom_require/
+        origin = caller[3]
+        if origin.nil?
+          STDERR.puts 'Unknown origin'
+          STDERR.puts caller.join('\n')
+        end
+      end
+      origin = origin.gsub(/:[0-9]+:in .*/, '') if origin
