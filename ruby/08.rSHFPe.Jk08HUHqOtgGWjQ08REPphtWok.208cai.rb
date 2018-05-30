45587f85dcb628f627fc5734ec24bc5227059e95
SUNsung <weatherius@gmail.com>
2018-05-30 07:37:25 +0300

    Rewrite [08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb] File in: 2018-05-30 07:37:24

--- ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
+++ ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
@@ -1,126 +1,50 @@
 
         
-          def setup
-    # Increment symbol counter.
-    @symbol = (@@counter ||= 'A0').succ.dup
-    
-        test 'head :switching_protocols (101) does not return a content-type header' do
-      headers = HeadController.action(:switching_protocols).call(Rack::MockRequest.env_for('/')).second
-      assert_nil headers['Content-Type']
-      assert_nil headers['Content-Length']
-    end
-    
-        def show
-      self.response_body = 'show'
-    end
-  end
-    
-          def handle_exception_with_mailer_class(exception)
-        if klass = mailer_class
-          klass.handle_exception exception
-        else
-          raise exception
-        end
-      end
+            Or via the Cask:
+      brew cask install ngrok
+    EOS
   end
 end
 
     
-          text.split.each do |word|
-        if sentences.first.present? && (sentences.last + [word]).join(' ').length > len
-          sentences << [word]
-        else
-          sentences.last << word
-        end
-      end
+    module Homebrew
+  def config
+    dump_verbose_config
+  end
     
-        def mail_with_defaults(&block)
-      mail(to: 'test@localhost', from: 'tester@example.com',
-            subject: 'using helpers', &block)
+        # Exclude cache, logs, and repository, if they are located under the prefix.
+    [HOMEBREW_CACHE, HOMEBREW_LOGS, HOMEBREW_REPOSITORY].each do |dir|
+      dirs.delete dir.relative_path_from(HOMEBREW_PREFIX).to_s
     end
-end
+    dirs.delete 'etc'
+    dirs.delete 'var'
     
-      def regular?
-    !staff?
-  end
+    #
+# Railties
+#
     
-      def self.fragment_cache
-    @cache ||= DistributedCache.new('am_serializer_fragment_cache')
-  end
+    sitelist = ARGV.shift() || usage()
+output   = ARGV.shift() || usage()
     
-      def send_sinatra_file(path, &missing_file_block)
-    file_path = File.join(File.dirname(__FILE__), 'public',  path)
-    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
-    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
-  end
+    #Rjb::load('.', jvmargs=[])
+Rjb::load('#{ENV['JAVA_HOME']}/lib/tools.jar:.',jvmargs=[])
     
-        def initialize(tag_name, markup, tokens)
-      @by = nil
-      @source = nil
-      @title = nil
-      if markup =~ FullCiteWithTitle
-        @by = $1
-        @source = $2 + $3
-        @title = $4.titlecase.strip
-      elsif markup =~ FullCite
-        @by = $1
-        @source = $2 + $3
-      elsif markup =~ AuthorTitle
-        @by = $1
-        @title = $2.titlecase.strip
-      elsif markup =~ Author
-        @by = $1
-      end
-      super
-    end
-    
-    
-===============================================
- Error for category_generator.rb plugin
------------------------------------------------
- No 'category_index.html' in source/_layouts/
- Perhaps you haven't installed a theme yet.
-===============================================
-    
-        def get_web_content(url)
-      raw_uri           = URI.parse url
-      proxy             = ENV['http_proxy']
-      if proxy
-        proxy_uri       = URI.parse(proxy)
-        https           = Net::HTTP::Proxy(proxy_uri.host, proxy_uri.port).new raw_uri.host, raw_uri.port
-      else
-        https           = Net::HTTP.new raw_uri.host, raw_uri.port
-      end
-      https.use_ssl     = true
-      https.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      request           = Net::HTTP::Get.new raw_uri.request_uri
-      data              = https.request request
-    end
-  end
-    
-        def render(context)
-      if @img
-        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
-      else
-        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
+            raise t('error.invalid_stage_name', name: invalid, path: stage_config_path.join('#{invalid}.rb'))
       end
     end
   end
 end
+
     
-      class IncludeCodeTag < Liquid::Tag
-    def initialize(tag_name, markup, tokens)
-      @title = nil
-      @file = nil
-      if markup.strip =~ /\s*lang:(\S+)/i
-        @filetype = $1
-        markup = markup.strip.sub(/lang:\S+/i,'')
-      end
-      if markup.strip =~ /(.*)?(\s+|^)(\/*\S+)/i
-        @title = $1 || nil
-        @file = $3
-      end
-      super
+      desc 'Rollback to previous release.'
+  task :rollback do
+    %w{ starting started
+        reverting reverted
+        publishing published
+        finishing_rollback finished }.each do |task|
+      invoke 'deploy:#{task}'
     end
+  end
+end
     
-    Liquid::Template.register_tag('video', Jekyll::VideoTag)
+      it 'provides a --format option which enables the choice of output formatting'
