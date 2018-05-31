bd555b7f380dde94bcb906cd1687d227c3b8acc5
SUNsung <weatherius@gmail.com>
2018-05-31 05:52:26 +0300

    Rewrite [153Kg81.5Z5wc156Yz0q.ZBvCY15ek5eiuSEXRY15UkL.py] File in: 2018-05-31 05:52:26

--- pyton/153Kg81.5Z5wc156Yz0q.ZBvCY15ek5eiuSEXRY15UkL.py
+++ pyton/153Kg81.5Z5wc156Yz0q.ZBvCY15ek5eiuSEXRY15UkL.py
@@ -1,134 +1,102 @@
 
         
-        '''TensorFlow ops for maximum spanning tree problems.'''
+            digest = hash_func()
+    data = to_bytes(data, errors='surrogate_or_strict')
+    digest.update(data)
+    return digest.hexdigest()
     
-    '''Tests for maximum spanning tree ops.'''
+            if not args.test:
+            with open(path, 'w') as metadata_fd:
+                metadata_fd.write(contents)
     
-      Args:
-    component: Component that the hook node belongs to.
-    variable_name: Variable that the hook node name is based on.
-    suffix: Suffix to append to the variable name.
+        data = dict(
+        globalEnv=['%s=%s' % (kp[0], kp[1]) for kp in args.env or []]
+    )
     
+            # test exceptions
+        no_projects_input_url = 'https://www.googleapis.com/compute/v1/not-projects/myproject/global/backendServices/mybackendservice/getHealth'
+        no_resource_input_url = 'https://www.googleapis.com/compute/v1/not-projects/myproject/global'
     
-import tensorflow as tf
+        terminal_length = os.getenv('ANSIBLE_VYOS_TERMINAL_LENGTH', 10000)
     
-    
-if __name__ == '__main__':
-  googletest.main()
+        def on_open_shell(self):
+        try:
+            self._exec_cli_command(b'environment no more')
+        except AnsibleConnectionFailure:
+            raise AnsibleConnectionFailure('unable to set terminal parameters')
 
     
-      def create(self,
-             fixed_embeddings,
-             linked_embeddings,
-             context_tensor_arrays,
-             attention_tensor,
-             during_training,
-             stride=None):
-    '''See base class.'''
-    # NB: This cell pulls the lstm's h and c vectors from context_tensor_arrays
-    # instead of through linked features.
-    check.Eq(
-        len(context_tensor_arrays), 2 * len(self._hidden_layer_sizes),
-        'require two context tensors per hidden layer')
+        # test flat list inputs
+    with CustomObjectScope(custom_objects):
+        layer = wrappers.Bidirectional.from_config(copy.deepcopy(config))
+    y = layer([x, c])
+    model = Model([x, c], y)
+    model.set_weights(weights)
+    y_np_3 = model.predict([x_np, c_np])
+    assert_allclose(y_np, y_np_3, atol=1e-4)
     
-    flags.DEFINE_string('master_spec', None, 'Path to task context with '
-                    'inputs and parameters for feature extractors.')
-flags.DEFINE_string('params_path', None, 'Path to trained model parameters.')
-flags.DEFINE_string('export_path', '', 'Output path for exported servo model.')
-flags.DEFINE_string('resource_path', '',
-                    'Base directory for resources in the master spec.')
-flags.DEFINE_bool('export_moving_averages', True,
-                  'Whether to export the moving average parameters.')
-    
-        tf.logging.info('Processed %d documents in %.2f seconds.',
-                    len(input_corpus), time.time() - start_time)
-    _, uas, las = evaluation.calculate_parse_metrics(input_corpus, processed)
-    tf.logging.info('UAS: %.2f', uas)
-    tf.logging.info('LAS: %.2f', las)
-    
-      tf.logging.info('Reading documents...')
-  input_corpus = sentence_io.ConllSentenceReader(FLAGS.input_file).corpus()
-  with tf.Session(graph=tf.Graph()) as tmp_session:
-    char_input = gen_parser_ops.char_token_generator(input_corpus)
-    char_corpus = tmp_session.run(char_input)
-  check.Eq(len(input_corpus), len(char_corpus))
-    
-      # Construct the 'lookahead' ComponentSpec. This is a simple right-to-left RNN
-  # sequence model, which encodes the context to the right of each token. It has
-  # no loss except for the downstream components.
-  lookahead = spec_builder.ComponentSpecBuilder('lookahead')
-  lookahead.set_network_unit(
-      name='wrapped_units.LayerNormBasicLSTMNetwork', hidden_layer_sizes='256')
-  lookahead.set_transition_system(name='shift-only', left_to_right='false')
-  lookahead.add_fixed_feature(name='char',
-                              fml='input(-1).char input.char input(1).char',
-                              embedding_dim=32)
-  lookahead.add_fixed_feature(name='char-bigram',
-                              fml='input.char-bigram',
-                              embedding_dim=32)
-  lookahead.fill_from_resources(FLAGS.resource_path, FLAGS.tf_master)
-    
-    from dragnn.python import evaluation
-from dragnn.python import graph_builder
-from dragnn.python import lexicon
-from dragnn.python import spec_builder
-from dragnn.python import trainer_lib
-    
-        .. versionchanged:: 5.0:
-       Unified ``tornado.gen.TimeoutError`` and
-       ``tornado.ioloop.TimeoutError`` as ``tornado.util.TimeoutError``.
-       Both former names remain as aliases.
-    '''
-    
-           This class is deprecated and will be removed in Tornado 6.0.
-       Use Tornado's `.HTTPServer` instead of a WSGI container.
-    '''
-    def __init__(self, application):
-        warnings.warn('WSGIAdapter is deprecated, use Tornado's HTTPServer instead',
-                      DeprecationWarning)
-        if isinstance(application, WSGIApplication):
-            self.application = lambda request: web.Application.__call__(
-                application, request)
-        else:
-            self.application = application
+        # Test single image
+    x = np.random.uniform(0, 255, (10, 10, 3))
+    inputs = Input(shape=x.shape)
+    outputs = Lambda(utils.preprocess_input, output_shape=x.shape)(inputs)
+    model = Model(inputs, outputs)
+    assert model.predict(x[np.newaxis])[0].shape == x.shape
     
     
-class FacebookGraphMixin(OAuth2Mixin):
-    '''Facebook authentication using the new Graph API and OAuth2.'''
-    _OAUTH_ACCESS_TOKEN_URL = 'https://graph.facebook.com/oauth/access_token?'
-    _OAUTH_AUTHORIZE_URL = 'https://www.facebook.com/dialog/oauth?'
-    _OAUTH_NO_CALLBACKS = False
-    _FACEBOOK_BASE_URL = 'https://graph.facebook.com'
+def get_test_values():
+    return [0.1, 0.5, 3, 8, 1e-7]
     
-            .. versionchanged:: 4.0
-           Now passes through ``*args`` and ``**kwargs`` to the callback.
         '''
-        if isinstance(deadline, numbers.Real):
-            return self.call_at(deadline, callback, *args, **kwargs)
-        elif isinstance(deadline, datetime.timedelta):
-            return self.call_at(self.time() + timedelta_to_seconds(deadline),
-                                callback, *args, **kwargs)
-        else:
-            raise TypeError('Unsupported deadline %r' % deadline)
+    model = Sequential()
+    model.add(layers.Embedding(10, 10, mask_zero=True))
+    model.add(layers.Activation('softmax'))
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam')
     
-            self.http_client.fetch(self.get_url('/'), self.stop)
-        response = self.wait()
+        # 4. Anything else is not a valid argument
+    with pytest.raises(ValueError):
+        a = activations.get(6)
+    
+                # train once so that the states change
+            model.train_on_batch(np.ones_like(inputs),
+                                 np.random.random(out1.shape))
+            out2 = model.predict(np.ones_like(inputs))
     
     
-class TwitterClientShowUserHandlerLegacy(TwitterClientHandler):
-    with ignore_deprecation():
-        @asynchronous
-        @gen.engine
-        def get(self):
-            # TODO: would be nice to go through the login flow instead of
-            # cheating with a hard-coded access token.
-            with warnings.catch_warnings():
-                warnings.simplefilter('ignore', DeprecationWarning)
-                response = yield gen.Task(self.twitter_request,
-                                          '/users/show/%s' % self.get_argument('name'),
-                                          access_token=dict(key='hjkl', secret='vbnm'))
-            if response is None:
-                self.set_status(500)
-                self.finish('error from twitter request')
-            else:
-                self.finish(response)
+class AdapterTest(unittest.TestCase):
+    
+    
+class RadioTest(unittest.TestCase):
+    '''
+    Attention: Test case results depend on test case execution. The test cases
+    in this integration test class should be executed in an explicit order:
+    http://stackoverflow.com/questions/5387299/python-unittest-testcase-execution-order
+    '''
+    
+        def setUp(self):
+        self.tc1 = TC1()
+        self.tc2 = TC2()
+        self.tc3 = TC3()
+        self.average_result_tc1 = '###### In Test 1 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc2 = '###### In Test 2 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc3 = '###### In Test 3 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.runner = TestRunner()
+        self.out = StringIO()
+        self.saved_stdout = sys.stdout
+        sys.stdout = self.out
+    
+    class TimeDisplay(object):
+    
+    
