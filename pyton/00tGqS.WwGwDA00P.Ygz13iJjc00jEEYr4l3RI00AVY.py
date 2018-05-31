a83c8b30f2ac974483ca9a74206a232d9b6060a1
SUNsung <weatherius@gmail.com>
2018-05-31 18:55:00 +0300

    Rewrite [00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py] File in: 2018-05-31 18:55:00

--- pyton/00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py
+++ pyton/00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py
@@ -1,116 +1,68 @@
 
-def check_entry(line_num, segments):
-    # START Title
-    title = segments[index_title].upper()
-    if title.endswith(' API'):
-        add_error(line_num, 'Title should not contain 'API'')
-    # END Title
-    # START Description
-    # first character should be capitalized
-    char = segments[index_desc][0]
-    if char.upper() != char:
-        add_error(line_num, 'first character of description is not capitalized')
-    # last character should not punctuation
-    char = segments[index_desc][-1]
-    if char in punctuation:
-        add_error(line_num, 'description should not end with {}'.format(char))
-    desc_length = len(segments[index_desc])
-    if desc_length > 100:
-        add_error(line_num, 'description should not exceed 100 characters (currently {})'.format(desc_length))
-    # END Description
-    # START Auth
-    # values should conform to valid options only
-    auth = segments[index_auth]
-    if auth != 'No' and (not auth.startswith('`') or not auth.endswith('`')):
-        add_error(line_num, 'auth value is not enclosed with `backticks`')
-    if auth.replace('`', '') not in auth_keys:
-        add_error(line_num, '{} is not a valid Auth option'.format(auth))
-    # END Auth
-    # START HTTPS
-    # values should conform to valid options only
-    https = segments[index_https]
-    if https not in https_keys:
-        add_error(line_num, '{} is not a valid HTTPS option'.format(https))
-    # END HTTPS
-    # START CORS
-    # values should conform to valid options only
-    cors = segments[index_cors]
-    if cors not in cors_keys:
-        add_error(line_num, '{} is not a valid CORS option'.format(cors))
-    # END CORS
-    # START Link
-    # url should be wrapped in '[Go!]()' Markdown syntax
-    link = segments[index_link]
-    if not link.startswith('[Go!](http') or not link.endswith(')'):
-        add_error(line_num, 'link syntax should be '[Go!](LINK)'')
-    if link in previous_links:
-        add_error(line_num, 'duplicate link - entries should only be included in one section')
-    else:
-        previous_links.append(link)
-    # END Link
+        
+        
+class ConfigAttribute(object):
+    '''Makes an attribute forward to the config'''
     
-      def testImmutabilityOfArguments(self):
-    '''Tests that training schedule generation does not change its arguments.'''
-    pretrain_steps = [1, 2, 3]
-    train_steps = [5, 5, 5]
-    trainer_lib.generate_target_per_step_schedule(pretrain_steps, train_steps)
-    self.assertEqual(pretrain_steps, [1, 2, 3])
-    self.assertEqual(train_steps, [5, 5, 5])
+        def open_session(self, app, request):
+        s = self.get_signing_serializer(app)
+        if s is None:
+            return None
+        val = request.cookies.get(app.session_cookie_name)
+        if not val:
+            return self.session_class()
+        max_age = total_seconds(app.permanent_session_lifetime)
+        try:
+            data = s.loads(val, max_age=max_age)
+            return self.session_class(data)
+        except BadSignature:
+            return self.session_class()
     
-    py_binary(
-    name = 'build_pip_package',
-    srcs = ['build_pip_package.py'],
-    data = ['oss_setup.py'],
+            if cls.decorators:
+            view.__name__ = name
+            view.__module__ = cls.__module__
+            for decorator in cls.decorators:
+                view = decorator(view)
+    
+    
+def test_uninstalled_package_paths(modules_tmpdir, purge_module):
+    app = modules_tmpdir.mkdir('config_package_app')
+    init = app.join('__init__.py')
+    init.write(
+        'import os\n'
+        'import flask\n'
+        'here = os.path.abspath(os.path.dirname(__file__))\n'
+        'app = flask.Flask(__name__)\n'
+    )
+    purge_module('config_package_app')
+    
+    _gc_lock = threading.Lock()
+    
+      # Lookahead LSTM reads right-to-left to represent the rightmost context of the
+  # words. It gets word embeddings from the char model.
+  lookahead = spec_builder.ComponentSpecBuilder('lookahead')
+  lookahead.set_network_unit(
+      name='wrapped_units.LayerNormBasicLSTMNetwork',
+      hidden_layer_sizes='256')
+  lookahead.set_transition_system(name='shift-only', left_to_right='false')
+  lookahead.add_link(source=char2word, fml='input.last-char-focus',
+                     embedding_dim=64)
+    
+    py_library(
+    name = 'mst_units',
+    srcs = ['mst_units.py'],
     deps = [
-        ':all_in_one_components',
+        ':mst_ops',
+        ':network_units',
+        '//syntaxnet/util:check',
+        '@org_tensorflow//tensorflow:tensorflow_py',
     ],
 )
     
-        target = parser.add_mutually_exclusive_group()
-    
-        def test_get_gcp_resource_from_methodId(self):
-        input_data = 'compute.urlMaps.list'
-        actual = GCPUtils.get_gcp_resource_from_methodId(input_data)
-        self.assertEqual('urlMaps', actual)
-        input_data = None
-        actual = GCPUtils.get_gcp_resource_from_methodId(input_data)
-        self.assertFalse(actual)
-        input_data = 666
-        actual = GCPUtils.get_gcp_resource_from_methodId(input_data)
-        self.assertFalse(actual)
-    
-        return results
-
-    
-    from ansible.plugins.terminal import TerminalBase
-from ansible.errors import AnsibleConnectionFailure
-    
-        elif isinstance( candidate, str ) or isinstance( candidate, bytes ):
-      new_candidates.append(
-        { 'abbr': candidate,
-          'word': NewCandidateInsertionText( candidate, text_after_cursor ) } )
-  return new_candidates
-    
-    from ycm.tests.test_utils import MockVimModule
-MockVimModule()
-    
-    
-if __name__ == '__main__':
-    main()
-
-    
-    
-# TwistedResolver produces consistent errors in our test cases so we
-# could test the regular and error cases in the same class. However,
-# in the error cases it appears that cleanup of socket objects is
-# handled asynchronously and occasionally results in 'unclosed socket'
-# warnings if not given time to shut down (and there is no way to
-# explicitly shut it down). This makes the test flaky, so we do not
-# test error cases here.
-@skipIfNoNetwork
-@unittest.skipIf(twisted is None, 'twisted module not present')
-@unittest.skipIf(getattr(twisted, '__version__', '0.0') < '12.1', 'old version of twisted')
-class TwistedResolverTest(AsyncTestCase, _ResolverTestMixin):
-    def setUp(self):
-        super(TwistedResolverTest, self).setUp()
-        self.resolver = TwistedResolver()
+    def get_extension(link):
+    extension = os.path.splitext(link)[1][1:]
+    if extension in ['pdf', 'html']:
+        return extension
+    if 'pdf' in extension:
+        return 'pdf'    
+    return 'pdf'    
