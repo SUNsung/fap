20d0ba586820c8aa8208b6acf84713261dfc6c1b
SUNsung <weatherius@gmail.com>
2018-05-31 16:23:24 +0300

    Rewrite [241hYD8G8WiHo24FfNh0IJXaGg24zCbluQVaFyc243N3.rb] File in: 2018-05-31 16:23:24

--- ruby/241hYD8G8WiHo24FfNh0IJXaGg24zCbluQVaFyc243N3.rb
+++ ruby/241hYD8G8WiHo24FfNh0IJXaGg24zCbluQVaFyc243N3.rb
@@ -1,92 +1,150 @@
 
         
-            assert_dom_equal '<script>\n//<![CDATA[\nalert('hello')\n//]]>\n</script>',
-      javascript_tag('alert('hello')')
+        gem 'redis', '>= 3', '< 5'
+require 'redis'
     
-      private
-    def dashbord_url(id, message)
-      url_for action: 'dashboard', params: { 'id' => id, 'message' => message }
-    end
-end
-    
-        # Use the logger configured for ActionMailer::Base.
-    def logger
-      ActionMailer::Base.logger
+        def env_for(url)
+      Rack::MockRequest.env_for(url)
     end
   end
-end
     
-    class AMLogSubscriberTest < ActionMailer::TestCase
-  include ActiveSupport::LogSubscriber::TestHelper
+        rescue_from StandardError, with: :handle_exception_with_mailer_class
     
-    # You can also do other things. Examples:
-# https://github.com/tmm1/stackprof/blob/master/bin/stackprof
-report = StackProf::Report.new(Marshal.load(IO.binread(PROF_OUTPUT_FILE)))
-report.print_text(
-  options[:sort],
-  options[:limit],
-  options[:select_files],
-  options[:reject_files],
-  options[:select_names],
-  options[:reject_names]
-)
-    
-          # Group an array of items by an expression
-      #
-      # input - the object array
-      # variable - the variable to assign each item to in the expression
-      # expression -a Liquid comparison expression passed in as a string
-      #
-      # Returns the filtered array of objects
-      def group_by_exp(input, variable, expression)
-        return input unless groupable?(input)
-    
-          private
-    
-        #
-    
-        # Create an instance of this class.
-    #
-    # site - the instance of Jekyll::Site we're concerned with
-    #
-    # Returns nothing
-    def initialize(site)
-      @site = site
+        # Access the message instance.
+    def message
+      @_message
     end
     
-            if b_length > a_length
-          (b_length - a_length).times { a_split.insert(-2, 0) }
-        elsif a_length > b_length
-          (a_length - b_length).times { b_split.insert(-2, 0) }
+      before do
+    Spaceship::Portal::Key.client = mock_client
+  end
+    
+            if params[:default_keychain]
+          # if there is no default keychain - setting the original will fail - silent this error
+          begin
+            Actions.lane_context[Actions::SharedValues::ORIGINAL_DEFAULT_KEYCHAIN] = Fastlane::Actions.sh('security default-keychain', log: false).strip
+          rescue
+          end
+          commands << Fastlane::Actions.sh('security default-keychain -s #{keychain_path}', log: false)
         end
     
-        def relative_path_from(url)
-      self.class.parse(url).relative_path_to(self)
+          it 'renders HTML' do
+        capture = render(options, screenshots)
+        expect(capture).to match(/<html>/)
+        expect(capture).to include('<li>Some</li>')
+        expect(capture).to include('<li>key</li>')
+        expect(capture).to include('<li>words</li>')
+      end
+    end
+    
+          it 'does set the exclude directories' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            cloc(exclude_dir: 'test1,test2,build')
+          end').runner.execute(:test)
+    
+      # Implemented by subclasses to define Rake tasks. Typically a plugin will call
+  # `eval_rakefile` to load Rake tasks from a separate .rake file.
+  #
+  # Example:
+  #
+  #   def define_tasks
+  #     eval_rakefile File.expand_path('../tasks.rake', __FILE__)
+  #   end
+  #
+  # For simple tasks, you can define them inline. No need for a separate file.
+  #
+  #   def define_tasks
+  #     desc 'Do something fantastic.'
+  #     task 'my_plugin:fantastic' do
+  #       ...
+  #     end
+  #   end
+  #
+  def define_tasks; end
+    
+        # @abstract
+    #
+    # Identify the SHA of the commit that will be deployed.  This will most likely involve SshKit's capture method.
+    #
+    # @return void
+    #
+    def fetch_revision
+      raise NotImplementedError, 'Your SCM strategy module should provide a #fetch_revision method'
     end
   end
 end
 
     
-            css('> .section', '#preamble', 'a[href*='dict.html']', 'code var', 'code strong').each do |node|
-          node.before(node.children).remove
+    desc 'Deploy a new release.'
+task :deploy do
+  set(:deploying, true)
+  %w{ starting started
+      updating updated
+      publishing published
+      finishing finished }.each do |task|
+    invoke 'deploy:#{task}'
+  end
+end
+task default: :deploy
+
+    
+      tasks_dir = Pathname.new('lib/capistrano/tasks')
+  config_dir = Pathname.new('config')
+  deploy_dir = config_dir.join('deploy')
+    
+      describe 'role_properties()' do
+    before do
+      dsl.role :redis, %w[example1.com example2.com], redis: { port: 6379, type: :slave }
+      dsl.server 'example1.com', roles: %w{web}, active: true, web: { port: 80 }
+      dsl.server 'example2.com', roles: %w{web redis}, web: { port: 81 }, redis: { type: :master }
+      dsl.server 'example3.com', roles: %w{app}, primary: true
+    end
+    
+      def command_line(*options)
+    options.each { |opt| ARGV << opt }
+    subject.define_singleton_method(:exit) do |*_args|
+      throw(:system_exit, :exit)
+    end
+    subject.run
+    subject.options
+  end
+    
+    module LogStash::Api::AppHelpers
+  # This method handle both of the normal flow *happy path*
+  # and the display or errors, if more custom logic is added here
+  # it will make sense to separate them.
+  #
+  # See `#error` method in the `LogStash::Api::Module::Base`
+  def respond_with(data, options={})
+    as     = options.fetch(:as, :json)
+    filter = options.fetch(:filter, '')
+    
+          elsif download =~ /.tgz/
+        prefix = download.gsub('.tgz', '').gsub('#{target}/', '')
+        untar(download) do |entry|
+          next unless out = eval_file(entry, file['files'], prefix)
+          File.join(target, out)
         end
     
-    def each_schema_load_environment
-  # If we're in development, also run this for the test environment.
-  # This is a somewhat hacky way to do this, so here's why:
-  # 1. We have to define this before we load the schema, or we won't
-  #    have a timestamp_id function when we get to it in the schema.
-  # 2. db:setup calls db:schema:load_if_ruby, which calls
-  #    db:schema:load, which we define above as having a prerequisite
-  #    of this task.
-  # 3. db:schema:load ends up running
-  #    ActiveRecord::Tasks::DatabaseTasks.load_schema_current, which
-  #    calls a private method `each_current_configuration`, which
-  #    explicitly also does the loading for the `test` environment
-  #    if the current environment is `development`, so we end up
-  #    needing to do the same, and we can't even use the same method
-  #    to do it.
+        private
     
-          it 'sets a regeneration marker while regenerating' do
-        allow(RegenerationWorker).to receive(:perform_async)
-        get :show
+            def no_error_when_valid?
+          @file = StringIO.new('.')
+          @subject.send(@attachment_name).assign(@file)
+          @subject.valid?
+          expected_message = [
+            @attachment_name.to_s.titleize,
+            I18n.t(:blank, scope: [:errors, :messages])
+          ].join(' ')
+          @subject.errors.full_messages.exclude?(expected_message)
+        end
+      end
+    end
+  end
+end
+
+    
+            def in range
+          @low, @high = range.first, range.last
+          self
+        end
