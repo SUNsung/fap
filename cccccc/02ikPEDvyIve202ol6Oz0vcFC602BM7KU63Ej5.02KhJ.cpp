e1f13962fed964a0cbf68e7f4a1c91c00168f10e
SUNsung <weatherius@gmail.com>
2018-06-01 01:34:28 +0300

    Rewrite [02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp] File in: 2018-06-01 01:34:27

--- cccccc/02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp
+++ cccccc/02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp
@@ -1,286 +1,230 @@
 
         
-        #if defined(OS_WIN)
-#include 'base/process/process_handle.h'
-#endif
+            D3D_DRIVER_TYPE driverTypes[] =
+    {
+        D3D_DRIVER_TYPE_HARDWARE,
+        D3D_DRIVER_TYPE_WARP,
+        D3D_DRIVER_TYPE_REFERENCE,
+    };
+    UINT numDriverTypes = ARRAYSIZE(driverTypes);
     
+        // construct the multiplication matrix via schur compliment of the Macaulay
+    // matrix
+    Mtilde = M2_1 - M2_5.t()*M2_4;
     
-    { private:
-  DISALLOW_COPY_AND_ASSIGN(WinFrameView);
-};
+    CV_EXPORTS void  EstimateUncertainties(InputArrayOfArrays objectPoints, InputArrayOfArrays imagePoints,
+                           const IntrinsicParams& params, InputArray omc, InputArray Tc,
+                           IntrinsicParams& errors, Vec2d& std_err, double thresh_cond, int check_cond, double& rms);
     
-    #ifndef ATOM_BROWSER_UI_X_EVENT_DISABLER_H_
-#define ATOM_BROWSER_UI_X_EVENT_DISABLER_H_
+    void CV_ModelEstimator2_Test::fill_array( int test_case_idx, int i, int j, Mat& arr )
+{
+    if( i != INPUT )
+    {
+        cvtest::ArrayTest::fill_array( test_case_idx, i, j, arr );
+        return;
+    }
+    }
     
-    namespace content {
-class BrowserPpapiHost;
-}  // namespace content
+            struct smem
+        {
+            enum { TAG_MASK = (1U << ( (sizeof(unsigned int) << 3) - 5U)) - 1U };
+    }
     
-    void TtsPlatformImpl::clear_error() {
-  error_ = std::string();
+            if (lane < 16)
+        {
+            T partial = ptr[tid];
+    }
+    
+    // taken from TH (generic/THTensor.c)
+// with a little fixes done so as to allocate
+// and free memory the way it is done in THDTensor
+static void THDTensor_(_resize)(THDTensor *self, int nDimension, int64_t *size, int64_t *stride) {
+  int nDimension_;
+  ptrdiff_t totalSize;
+  bool hasRequiredSize = true;
+    }
+    
+    void test_allReduce(std::shared_ptr<thd::DataChannel> data_channel, int workers) {
+  _test_allReduce_helper(data_channel, THDReduceOp::THDReduceSUM,
+                         2, 2 + (workers * (workers + 1) / 2));
+  _test_allReduce_helper(data_channel, THDReduceOp::THDReducePRODUCT,
+                         2, 2 * factorial(workers));
+  _test_allReduce_helper(data_channel, THDReduceOp::THDReduceMIN, 10010, 1);
+  _test_allReduce_helper(data_channel, THDReduceOp::THDReduceMAX,
+                         -1, data_channel->getNumProcesses() - 1);
 }
     
-    // filenames
-const base::FilePath::CharType kCacheDirname[] = FPL('Cache');
-const base::FilePath::CharType kChannelIDFilename[] = FPL('Origin Bound Certs');
-const base::FilePath::CharType kCookieFilename[] = FPL('Cookies');
-const base::FilePath::CharType kCRLSetFilename[] =
-    FPL('Certificate Revocation Lists');
-const base::FilePath::CharType kCustomDictionaryFileName[] =
-    FPL('Custom Dictionary.txt');
-const base::FilePath::CharType kExtensionActivityLogFilename[] =
-    FPL('Extension Activity');
-const base::FilePath::CharType kExtensionsCookieFilename[] =
-    FPL('Extension Cookies');
-const base::FilePath::CharType kFirstRunSentinel[] = FPL('First Run');
-const base::FilePath::CharType kGCMStoreDirname[] = FPL('GCM Store');
-const base::FilePath::CharType kLocalStateFilename[] = FPL('Local State');
-const base::FilePath::CharType kLocalStorePoolName[] = FPL('LocalStorePool');
-const base::FilePath::CharType kMediaCacheDirname[] = FPL('Media Cache');
-const base::FilePath::CharType kNetworkPersistentStateFilename[] =
-    FPL('Network Persistent State');
-const base::FilePath::CharType kOfflinePageArchviesDirname[] =
-    FPL('Offline Pages/archives');
-const base::FilePath::CharType kOfflinePageMetadataDirname[] =
-    FPL('Offline Pages/metadata');
-const base::FilePath::CharType kPreferencesFilename[] = FPL('Preferences');
-const base::FilePath::CharType kProtectedPreferencesFilenameDeprecated[] =
-    FPL('Protected Preferences');
-const base::FilePath::CharType kReadmeFilename[] = FPL('README');
-const base::FilePath::CharType kResetPromptMementoFilename[] =
-    FPL('Reset Prompt Memento');
-const base::FilePath::CharType kSafeBrowsingBaseFilename[] =
-    FPL('Safe Browsing');
-const base::FilePath::CharType kSecurePreferencesFilename[] =
-    FPL('Secure Preferences');
-const base::FilePath::CharType kServiceStateFileName[] = FPL('Service State');
-const base::FilePath::CharType kSingletonCookieFilename[] =
-    FPL('SingletonCookie');
-const base::FilePath::CharType kSingletonLockFilename[] = FPL('SingletonLock');
-const base::FilePath::CharType kSingletonSocketFilename[] =
-    FPL('SingletonSocket');
-const base::FilePath::CharType kSupervisedUserSettingsFilename[] =
-    FPL('Managed Mode Settings');
-const base::FilePath::CharType kThemePackFilename[] = FPL('Cached Theme.pak');
-const base::FilePath::CharType kThemePackMaterialDesignFilename[] =
-    FPL('Cached Theme Material Design.pak');
-const base::FilePath::CharType kWebAppDirname[] = FPL('Web Applications');
+    using namespace at;
     
-    #endif  // GTEST_HAS_PARAM_TEST
+    #undef THHostTensor
+#undef THHostTensor_
+#undef THHostStorage
+#undef THHostStorage_
+
+    
+    #endif
+
+    
+    ByteArray::ByteArray()
+  : _data()
+{}
+    
+    template<template<typename> class Trait, typename U>
+struct map_to_ptr {};
     
     
     {
-    {    // Next, recurses (at compile time) with the tail of the type list.
-    return TypeParameterizedTest<Fixture, TestSel, typename Types::Tail>
-        ::Register(prefix, case_name, test_names, index + 1);
-  }
-};
-    
-    // On some platforms, <regex.h> needs someone to define size_t, and
-// won't compile otherwise.  We can #include it here as we already
-// included <stdlib.h>, which is guaranteed to define size_t through
-// <stddef.h>.
-# include <regex.h>  // NOLINT
-    
-    // A sample program demonstrating using Google C++ testing framework.
-//
-// Author: wan@google.com (Zhanyong Wan)
-    
-    
-    {  if (src->size[dimension] == 1 && src->nDimension > 1) {
-    for (std::size_t d = dimension; d < self->nDimension-1; d++) {
-      self->size[d] = self->size[d+1];
-      self->stride[d] = self->stride[d+1];
-    }
-    self->nDimension--;
-  }
+    {// use R's PRNG to replacd
+CustomGlobalRandomEngine::result_type
+CustomGlobalRandomEngine::operator()() {
+  return static_cast<result_type>(
+      std::floor(unif_rand() * CustomGlobalRandomEngine::max()));
 }
-    
-    inline Half::operator double() {
-  return convert<double, Half>(*this);
-}
-    
-        std::shared_ptr<GlooCache::algorithm_type> algo;
-    if (device == DeviceType::CPU) {
-      algo = std::make_shared<::gloo::BroadcastOneToAll<T>>(
-        context,
-        std::initializer_list<T*>{reinterpret_cast<T*>(input_buffer.get())},
-        count,
-        src_rank);
-#ifdef WITH_CUDA
-    } else if (device == DeviceType::CUDA) {
-      auto stream = THCState_getCurrentStream(THDGetCudaState());
-    }
-    
-    
-    {} // namespace thd
+}  // namespace common
+}  // namespace xgboost
 
     
-    void OutputImage::CopyFromJpegData(const JPEGData& jpg) {
-  for (size_t i = 0; i < jpg.components.size(); ++i) {
-    const JPEGComponent& comp = jpg.components[i];
-    assert(jpg.max_h_samp_factor % comp.h_samp_factor == 0);
-    assert(jpg.max_v_samp_factor % comp.v_samp_factor == 0);
-    int factor_x = jpg.max_h_samp_factor / comp.h_samp_factor;
-    int factor_y = jpg.max_v_samp_factor / comp.v_samp_factor;
-    assert(comp.quant_idx < jpg.quant.size());
-    components_[i].CopyFromJpegComponent(comp, factor_x, factor_y,
-                                         &jpg.quant[comp.quant_idx].values[0]);
-  }
-}
+    XGBOOST_REGISTER_SPARSE_PAGE_FORMAT(lz4hc)
+.describe('Apply LZ4 binary data compression(high compression ratio) for ext memory.')
+.set_body([]() {
+    return new SparsePageLZ4Format<bst_uint>(true);
+  });
     
-    namespace guetzli {
-    }
-    
-    #ifndef GUETZLI_FAST_LOG_H_
-#define GUETZLI_FAST_LOG_H_
-    
-    
-    {}  // namespace guetzli
-    
-    bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
-                     const int* quant, JPEGData* jpg) {
-  if (w < 0 || w >= 1 << 16 || h < 0 || h >= 1 << 16 ||
-      rgb.size() != 3 * w * h) {
-    return false;
-  }
-  InitJPEGDataForYUV444(w, h, jpg);
-  AddApp0Data(jpg);
-    }
-    
-    
-// Adds APP0 header data.
-void AddApp0Data(JPEGData* jpg);
-    
-      // Make a local copy of the input bit length histogram.
-  int count[kJpegHuffmanMaxBitLength + 1] = { 0 };
-  int total_count = 0;
-  for (len = 1; len <= kJpegHuffmanMaxBitLength; ++len) {
-    count[len] = count_in[len];
-    total_count += count[len];
-  }
-    
-      EXPECT_CALL(transport, writeChain(_, BufMatches(buf.get()), _));
-    
-    IOBuf::IOBuf(TakeOwnershipOp, void* buf, uint64_t capacity, uint64_t length,
-             FreeFunction freeFn, void* userData,
-             bool freeOnError)
-  : next_(this),
-    prev_(this),
-    data_(static_cast<uint8_t*>(buf)),
-    buf_(static_cast<uint8_t*>(buf)),
-    length_(length),
-    capacity_(capacity),
-    flagsAndSharedInfo_(packFlagsAndSharedInfo(kFlagFreeSharedInfo, nullptr)) {
-  try {
-    setSharedInfo(new SharedInfo(freeFn, userData));
-  } catch (...) {
-    takeOwnershipError(freeOnError, buf, freeFn, userData);
-    throw;
-  }
-}
-    
-      bool add(T v) {
-    auto prev = &head_;
-    locate_lower_bound(v, prev);
-    auto curr = prev->load(std::memory_order_relaxed);
-    if (curr && curr->elem_ == v) {
-      return false;
-    }
-    prev->store(new Node(std::move(v), curr));
-    return true;
-  }
-    
-      std::vector<std::string> otherVec;
-  for (auto& s : vec) {
-    otherVec.push_back(s);
-  }
-  EXPECT_EQ(otherVec.size(), vec.size());
-  if (otherVec.size() == vec.size()) {
-    EXPECT_TRUE(std::equal(otherVec.begin(), otherVec.end(), vec.begin()));
-  }
-    
-    #include <map>
-#include <node.h>
-    
-    It must be started with :
-void* XXH32_init()
-The function returns a pointer which holds the state of calculation.
-    
-    using namespace rocksdb;
-    
-      static Key RandomTarget(Random* rnd) {
-    switch (rnd->Next() % 10) {
-      case 0:
-        // Seek to beginning
-        return MakeKey(0, 0);
-      case 1:
-        // Seek to end
-        return MakeKey(K, 0);
-      default:
-        // Seek to middle
-        return MakeKey(rnd->Next() % K, 0);
-    }
-  }
-    
-    
-    {  // No copying allowed
-  IterKey(const IterKey&) = delete;
-  void operator=(const IterKey&) = delete;
+    /*! \brief namespace of base64 decoding and encoding table */
+namespace base64 {
+const char DecodeTable[] = {
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  62,  // '+'
+  0, 0, 0,
+  63,  // '/'
+  52, 53, 54, 55, 56, 57, 58, 59, 60, 61,  // '0'-'9'
+  0, 0, 0, 0, 0, 0, 0,
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+  13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,  // 'A'-'Z'
+  0, 0, 0, 0, 0, 0,
+  26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+  39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,  // 'a'-'z'
 };
-    
-    
-    {  m_jcreate_compaction_filter_methodid =
-      AbstractCompactionFilterFactoryJni::getCreateCompactionFilterMethodId(env);
-  if(m_jcreate_compaction_filter_methodid == nullptr) {
-    // exception thrown: NoSuchMethodException or OutOfMemoryError
-    return;
+static const char EncodeTable[] =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+}  // namespace base64
+/*! \brief the stream that reads from base64, note we take from file pointers */
+class Base64InStream: public dmlc::Stream {
+ public:
+  explicit Base64InStream(dmlc::Stream *fs) : reader_(256) {
+    reader_.set_stream(fs);
+    num_prev = 0; tmp_ch = 0;
   }
-}
-    
-    #include <jni.h>
-    
-    void DumpCrashStack::Dump(const std::string& _processname) {
-    vecdump_.clear();
+  /*!
+   * \brief initialize the stream position to beginning of next base64 stream
+   * call this function before actually start read
+   */
+  inline void InitPosition(void) {
+    // get a character
+    do {
+      tmp_ch = reader_.GetChar();
+    } while (isspace(tmp_ch));
+  }
+  /*! \brief whether current position is end of a base64 stream */
+  inline bool IsEOF(void) const {
+    return num_prev == 0 && (tmp_ch == EOF || isspace(tmp_ch));
+  }
+  virtual size_t Read(void *ptr, size_t size) {
+    using base64::DecodeTable;
+    if (size == 0) return 0;
+    // use tlen to record left size
+    size_t tlen = size;
+    unsigned char *cptr = static_cast<unsigned char*>(ptr);
+    // if anything left, load from previous buffered result
+    if (num_prev != 0) {
+      if (num_prev == 2) {
+        if (tlen >= 2) {
+          *cptr++ = buf_prev[0];
+          *cptr++ = buf_prev[1];
+          tlen -= 2;
+          num_prev = 0;
+        } else {
+          // assert tlen == 1
+          *cptr++ = buf_prev[0]; --tlen;
+          buf_prev[0] = buf_prev[1];
+          num_prev = 1;
+        }
+      } else {
+        // assert num_prev == 1
+        *cptr++ = buf_prev[0]; --tlen; num_prev = 0;
+      }
+    }
+    if (tlen == 0) return size;
+    int nvalue;
+    // note: everything goes with 4 bytes in Base64
+    // so we process 4 bytes a unit
+    while (tlen && tmp_ch != EOF && !isspace(tmp_ch)) {
+      // first byte
+      nvalue = DecodeTable[tmp_ch] << 18;
+      {
+        // second byte
+        tmp_ch = reader_.GetChar();
+        CHECK(tmp_ch != EOF && !isspace(tmp_ch)) << 'invalid base64 format';
+        nvalue |= DecodeTable[tmp_ch] << 12;
+        *cptr++ = (nvalue >> 16) & 0xFF; --tlen;
+        }
+      {
+        // third byte
+        tmp_ch = reader_.GetChar();
+        CHECK(tmp_ch != EOF && !isspace(tmp_ch)) << 'invalid base64 format';
+        // handle termination
+        if (tmp_ch == '=') {
+          tmp_ch = reader_.GetChar();
+          CHECK(tmp_ch == '=') << 'invalid base64 format';
+          tmp_ch = reader_.GetChar();
+          CHECK(tmp_ch == EOF || isspace(tmp_ch))
+              << 'invalid base64 format';
+          break;
+        }
+        nvalue |= DecodeTable[tmp_ch] << 6;
+        if (tlen) {
+          *cptr++ = (nvalue >> 8) & 0xFF; --tlen;
+        } else {
+          buf_prev[num_prev++] = (nvalue >> 8) & 0xFF;
+        }
+      }
+      {
+        // fourth byte
+        tmp_ch = reader_.GetChar();
+        CHECK(tmp_ch != EOF && !isspace(tmp_ch))
+            << 'invalid base64 format';
+        if (tmp_ch == '=') {
+          tmp_ch = reader_.GetChar();
+          CHECK(tmp_ch == EOF || isspace(tmp_ch))
+              << 'invalid base64 format';
+          break;
+        }
+        nvalue |= DecodeTable[tmp_ch];
+        if (tlen) {
+          *cptr++ = nvalue & 0xFF; --tlen;
+        } else {
+          buf_prev[num_prev ++] = nvalue & 0xFF;
+        }
+      }
+      // get next char
+      tmp_ch = reader_.GetChar();
+    }
+    if (kStrictCheck) {
+      CHECK_EQ(tlen, 0) << 'Base64InStream: read incomplete';
+    }
+    return size - tlen;
+  }
+  virtual void Write(const void *ptr, size_t size) {
+    LOG(FATAL) << 'Base64InStream do not support write';
+  }
     }
     
-    CommFrequencyLimit::~CommFrequencyLimit()
-{}
-    
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
-    
-    #include 'comm/corepattern/service_base.h'
-    
-    int Test_Spy_Sample::__TestFun1(int i)
-{
-    SPY_HOOK_THIS_API(__TestFun1, i);
-    xinfo2(TSF'Test');
-    return i+1;
-}
-
-    
-    
-#define SPY_DEF_CLASS_NAME TSpy
-#define SPY_DEF_XLOGGER_HOOK TSpy::SpyHookLogFunc
-    
-     public: // Setters
-    
-    void Node::setWidthAuto()
-{
-    YGNodeStyleSetWidthAuto(m_node);
-}
-    
-      const char* functionName() const { return m_functionName; }
-  const char* fileName() const { return m_fileName; }
-  int lineNumber() const { return m_lineNumber; }
-    
-    
-    {  T* operator->() const {
-    return m_instance;
-  }
-private:
-  T* m_instance;
-};
+      delete metric;
+  metric = xgboost::Metric::Create('ndcg@2');
+  ASSERT_STREQ(metric->Name(), 'ndcg@2');
+  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 1, 1e-10);
+  EXPECT_NEAR(GetMetricEval(metric,
+                            {0.1f, 0.9f, 0.1f, 0.9f},
+                            {  0,   0,   1,   1}),
+              0.3868f, 0.001f);
