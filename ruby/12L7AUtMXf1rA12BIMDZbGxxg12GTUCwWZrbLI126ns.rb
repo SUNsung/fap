c68a79f354e812dbe3aa89b398f4f89ce7413f94
SUNsung <weatherius@gmail.com>
2018-06-01 23:34:53 +0300

    Rewrite [12L7AUtMXf1rA12BIMDZbGxxg12GTUCwWZrbLI126ns.rb] File in: 2018-06-01 23:34:53

--- ruby/12L7AUtMXf1rA12BIMDZbGxxg12GTUCwWZrbLI126ns.rb
+++ ruby/12L7AUtMXf1rA12BIMDZbGxxg12GTUCwWZrbLI126ns.rb
@@ -1,93 +1,123 @@
 
         
-          def test_helper_proxy_config
-    AllHelpersController.config.my_var = 'smth'
-    
-        test 'head :switching_protocols (101) does not return a content-type header' do
-      headers = HeadController.action(:switching_protocols).call(Rack::MockRequest.env_for('/')).second
-      assert_nil headers['Content-Type']
-      assert_nil headers['Content-Length']
+          # This allows generic Altivec PPC bottles to be supported in some
+  # formulae, while also allowing specific bottles in others; e.g.,
+  # sometimes a formula has just :tiger_altivec, other times it has
+  # :tiger_g4, :tiger_g5, etc.
+  def find_altivec_tag(tag)
+    if tag.to_s =~ /(\w+)_(g4|g4e|g5)$/
+      altivec_tag = '#{$1}_altivec'.to_sym
+      altivec_tag if key?(altivec_tag)
     end
+  end
     
-        # An email was received.
-    def receive(event)
-      info { 'Received mail (#{event.duration.round(1)}ms)' }
-      debug { event.payload[:mail] }
-    end
-    
-          text.split.each do |word|
-        if sentences.first.present? && (sentences.last + [word]).join(' ').length > len
-          sentences << [word]
+          # we readlink because this path probably doesn't exist since caveats
+      # occurs before the link step of installation
+      # Yosemite security measures mildly tighter rules:
+      # https://github.com/Homebrew/homebrew/issues/33815
+      if !plist_path.file? || !plist_path.symlink?
+        if f.plist_startup
+          s << 'To have launchd start #{f.full_name} at startup:'
+          s << '  sudo mkdir -p #{destination}' unless destination_path.directory?
+          s << '  sudo cp -fv #{f.opt_prefix}/*.plist #{destination}'
+          s << '  sudo chown root #{plist_link}'
         else
-          sentences.last << word
+          s << 'To have launchd start #{f.full_name} at login:'
+          s << '  mkdir -p #{destination}' unless destination_path.directory?
+          s << '  ln -sfv #{f.opt_prefix}/*.plist #{destination}'
+        end
+        s << 'Then to load #{f.full_name} now:'
+        if f.plist_startup
+          s << '  sudo launchctl load #{plist_link}'
+        else
+          s << '  launchctl load #{plist_link}'
+        end
+      # For startup plists, we cannot tell whether it's running on launchd,
+      # as it requires for `sudo launchctl list` to get real result.
+      elsif f.plist_startup
+        s << 'To reload #{f.full_name} after an upgrade:'
+        s << '  sudo launchctl unload #{plist_link}'
+        s << '  sudo cp -fv #{f.opt_prefix}/*.plist #{destination}'
+        s << '  sudo chown root #{plist_link}'
+        s << '  sudo launchctl load #{plist_link}'
+      elsif Kernel.system '/bin/launchctl list #{plist_domain} &>/dev/null'
+        s << 'To reload #{f.full_name} after an upgrade:'
+        s << '  launchctl unload #{plist_link}'
+        s << '  launchctl load #{plist_link}'
+      else
+        s << 'To load #{f.full_name}:'
+        s << '  launchctl load #{plist_link}'
+      end
+    
+      def external_commands
+    paths.reduce([]) do |cmds, path|
+      Dir['#{path}/brew-*'].each do |file|
+        next unless File.executable?(file)
+        cmd = File.basename(file, '.rb')[5..-1]
+        cmds << cmd unless cmd.include?('.')
+      end
+      cmds
+    end.sort
+  end
+    
+      def std_cmake_parameters
+    '-DCMAKE_INSTALL_PREFIX='#{prefix}' -DCMAKE_BUILD_TYPE=None -DCMAKE_FIND_FRAMEWORK=LAST -Wno-dev'
+  end
+    
+          html_filters.push 'coffeescript/entries', 'coffeescript/clean_html', 'title'
+    end
+    
+        version '5' do
+      self.release = '5.4.0'
+      self.base_url = 'https://github.com/d3/'
+      self.root_path = 'd3/blob/master/API.md'
+    
+        version '1.11' do
+      self.release = '1.11.9'
+      self.dir = '/Users/Thibaut/DevDocs/Docs/Django111'
+      self.base_url = 'https://docs.djangoproject.com/en/1.11/'
+    end
+    
+        private
+    
+          if dest.end_with? '/'
+        dest_dir.relative_path_from(base_dir).to_s.tap do |result|
+          result << '/' if result != '.'
+        end
+      else
+        dest_dir.parent.relative_path_from(base_dir).join(dest.split('/').last).to_s
+      end
+    end
+    
+      # Uninstalls this logger from \{Sass.logger\}. This should only be called if
+  # the logger was installed using \{#install!}
+  def uninstall!
+    if Sass.logger != self
+      throw Exception.new('Can't uninstall a logger that's not currently installed.')
+    end
+    
+        # @param modifier [Array<String, Sass::Script::Tree::Node>] See \{#modifier}
+    # @param type [Array<String, Sass::Script::Tree::Node>] See \{#type}
+    # @param expressions [Array<Array<String, Sass::Script::Tree::Node>>] See \{#expressions}
+    def initialize(modifier, type, expressions)
+      @modifier = modifier
+      @type = type
+      @expressions = expressions
+    end
+    
+      # GET /books/1/edit
+  def edit
+  end
+    
+            def comparison?(node)
+          simple_comparison?(node) || nested_comparison?(node)
         end
       end
-    
-      class AssertMultipartSelectMailer < ActionMailer::Base
-    def test(options)
-      mail subject: 'Test e-mail', from: 'test@test.host', to: 'test <test@test.host>' do |format|
-        format.text { render plain: options[:text] }
-        format.html { render plain: options[:html] }
-      end
     end
-  end
-    
-        render_response
-  end
-    
-        groups << @user.authorized_groups.visible_to_user(current_user) if current_user
-    groups << @user.authorized_groups.public_to_user(current_user)
-    
-        File.write(dest, Oj.dump(map))
-    puts 'Wrote emojo to destination! (#{dest})'
   end
 end
 
     
-    module Metasploit
-  module Framework
-    class Application < Rails::Application
-      include Metasploit::Framework::CommonEngine
-    
-    unless ENV['BUNDLE_GEMFILE']
-  require 'pathname'
-    
-    
-# replace calls, jmps, and read/write handle/filename references
-replaces = []
-asm.each_line { |ln|
-	if ln =~ /call /
-		parts = ln.split(' ')
-		if (parts[0] == 'call' and parts[2] == ';call')
-			old = parts[1]
-			func = parts[3]
-			new = addrs[func]
-			#puts '%32s: %s -> %x' % [func, old, new]
-			replaces << [func, old, new.to_s(16)]
-		end
-	end
-    }
-    
-    
-signer._invoke('KeyToolMSF','[Ljava.lang.String;',keytoolOpts)
-    
-    class Parser
-  def initialize(filename)
-    @src = Source.new(filename)
-  end
-    
-          def run
-        UI.puts report
-      end
-    
-            def self.options
-          [
-            ['--template-url=URL', 'The URL of the git repo containing a ' \
-                                  'compatible template'],
-          ].concat(super)
+            if expected_annotations.plain_source == source
+          raise 'Use expect_no_offenses to assert that no offenses are found'
         end
-    
-          #-----------------------------------------------------------------------#
-    end
-  end
-end
