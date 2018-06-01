62fea0287f603315efe4f25ff390865e330e5a1e
SUNsung <weatherius@gmail.com>
2018-06-01 23:17:24 +0300

    Rewrite [22YTrKz.K7WbI22jcyf.fkN5Q22CLFVjAQ0bqw22zuh.py] File in: 2018-06-01 23:17:24

--- pyton/22YTrKz.K7WbI22jcyf.fkN5Q22CLFVjAQ0bqw22zuh.py
+++ pyton/22YTrKz.K7WbI22jcyf.fkN5Q22CLFVjAQ0bqw22zuh.py
@@ -1,98 +1,112 @@
 
         
-        if isinstance(helptext, bytes):
-    helptext = helptext.decode('utf-8')
+        versions_info['versions'][version] = new_version
+versions_info['latest'] = version
     
-            duration = parse_duration(self._search_regex(
-            r'<b>Duration:</b> (?:<q itemprop='duration'>)?(\d+:\d+)', webpage, 'duration', fatal=False))
-        view_count = int_or_none(self._html_search_regex(
-            r'<b>Views:</b> (\d+)', webpage, 'view count', fatal=False))
+    entry_template = textwrap.dedent('''
+    <entry>
+        <id>https://yt-dl.org/feed/youtube-dl-updates-feed/youtube-dl-@VERSION@</id>
+        <title>New version @VERSION@</title>
+        <link href='http://rg3.github.io/youtube-dl' />
+        <content type='xhtml'>
+            <div xmlns='http://www.w3.org/1999/xhtml'>
+                Downloads available at <a href='https://yt-dl.org/downloads/@VERSION@/'>https://yt-dl.org/downloads/@VERSION@/</a>
+            </div>
+        </content>
+        <author>
+            <name>The youtube-dl maintainers</name>
+        </author>
+        <updated>@TIMESTAMP@</updated>
+    </entry>
+    ''')
+    
+        @pytest.mark.parametrize(
+        'content', (
+            # HTML5 meta charset attribute
+            '<meta charset='UTF-8'>',
+            # HTML4 pragma directive
+            '<meta http-equiv='Content-type' content='text/html;charset=UTF-8'>',
+            # XHTML 1.x served with text/html MIME type
+            '<meta http-equiv='Content-type' content='text/html;charset=UTF-8' />',
+            # XHTML 1.x served as XML
+            '<?xml version='1.0' encoding='UTF-8'?>',
+        ))
+    def test_pragmas(self, content):
+        encodings = get_encodings_from_content(content)
+        assert len(encodings) == 1
+        assert encodings[0] == 'UTF-8'
+    
+            extract_cookies_to_jar(self.cookies, request, r.raw)
+    
+    # Language to be used for generating the HTML full-text search index.
+# Sphinx supports the following languages:
+#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
+#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
+#html_search_language = 'en'
+    
+    import codecs
+import collections
+import contextlib
+import io
+import os
+import re
+import socket
+import struct
+import sys
+import tempfile
+import warnings
+import zipfile
+    
+            # put all lines in the file into a Python list
+        strings = f.readlines()
+        
+        # above line leaves trailing newline characters; strip them out
+        strings = [x.strip(u'\n') for x in strings]
+        
+        # remove empty-lines and comments
+        strings = [x for x in strings if x and not x.startswith(u'#')]
+        
+        # insert empty string since all are being removed
+        strings.insert(0, u'')
+    
+            for i in link_list:
+            self.stream_types.append({'id': str(i[0])})
+            self.streams[i[0]] = {'url': i[1]}
+    
+        print_info(site_info, title, type, size)
+    if not info_only:
+        download_urls([video_url], title, ext, size, output_dir, merge=merge, headers = fake_headers)
+    
+    from ..common import *
+from json import loads
+from base64 import b64decode
+import re
+import hashlib
+    
+    from ..common import *
+    
+        # mgid%3Auma%3Avideo%3Amtv81.com%3A897974
+    vid = match1(html, r'getTheVideo\('(.*?)'')
+    xml = parseString(
+        get_content('http://intl.esperanto.mtvi.com/www/xml/media/mediaGen.jhtml?uri={}&flashPlayer=LNX%2013,0,0,206&geo=CN&sid=123456'.format(vid)))
+    
+    For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/sensor.pvoutput/
+'''
+import logging
+from collections import namedtuple
+from datetime import timedelta
     
     
-class RewriteOperation(object):
-    '''@brief Internal helper class.'''
+def __feet_to_meters(feet: float) -> float:
+    '''Convert feet to meters.'''
+    return feet * 0.3048
     
-    def __init__(self, stream, index, text):
-        self.stream = stream
-        self.index = index
-        self.text = text
-    
-    class ClassicToken(Token):
-    '''@brief Alternative token implementation.
-    
-    A Token object like we'd use in ANTLR 2.x; has an actual string created
-    and associated with this object.  These objects are needed for imaginary
-    tree nodes that have payload objects.  We need to create a Token object
-    that has a string; the tree node will point at this token.  CommonToken
-    has indexes into a char stream and hence cannot be used to introduce
-    new strings.
-    '''
-    
-    - BaseRecognizer: Base class with common recognizer functionality.
-- Lexer: Base class for lexers.
-- Parser: Base class for parsers.
-- tree.TreeParser: Base class for %tree parser.
-    
-    
-    def getDescription(self):
-        return 'n/a'
-    
-    def get_extension(link):
-    extension = os.path.splitext(link)[1][1:]
-    if extension in ['pdf', 'html']:
-        return extension
-    if 'pdf' in extension:
-        return 'pdf'    
-    return 'pdf'    
-    
-    # Possible future states (for internal use by the futures package).
-PENDING = 'PENDING'
-RUNNING = 'RUNNING'
-# The future was cancelled by the user...
-CANCELLED = 'CANCELLED'
-# ...and _Waiter.add_cancelled() was called by a worker.
-CANCELLED_AND_NOTIFIED = 'CANCELLED_AND_NOTIFIED'
-FINISHED = 'FINISHED'
-    
-    class _WorkItem(object):
-    def __init__(self, future, fn, args, kwargs):
-        self.future = future
-        self.fn = fn
-        self.args = args
-        self.kwargs = kwargs
-    
-    def main():
-    for name, fn in [('sequential', sequential),
-                     ('processes', with_process_pool_executor),
-                     ('threads', with_thread_pool_executor)]:
-        sys.stdout.write('%s: ' % name.ljust(12))
-        start = time.time()
-        if fn() != [True] * len(PRIMES):
-            sys.stdout.write('failed\n')
-        else:
-            sys.stdout.write('%.2f seconds\n' % (time.time() - start))
-    
-    
-class DiagnosticFilter( object ):
-  def __init__( self, config_or_filters ):
-    if isinstance( config_or_filters, list ):
-      self._filters = config_or_filters
-    
-        vim_eval.assert_has_exact_calls( [
-      call( 'setqflist( {0} )'.format( json.dumps( expected_qf_list ) ) )
-    ] )
-    vim_command.assert_has_exact_calls( [
-      call( 'botright copen' ),
-      call( 'au WinLeave <buffer> q' ),
-      call( 'doautocmd User YcmQuickFixOpened' )
-    ] )
-    set_fitting_height.assert_called_once_with()
-    
-    DEFAULT_NAME = 'PVOutput'
-DEFAULT_VERIFY_SSL = True
-    
-        return True
-
-    
-    from homeassistant.components.device_tracker import bt_home_hub_5
-from homeassistant.const import CONF_HOST
+        def test_from_event_to_delete_state(self):
+        '''Test converting deleting state event to db state.'''
+        event = ha.Event(EVENT_STATE_CHANGED, {
+            'entity_id': 'sensor.temperature',
+            'old_state': ha.State('sensor.temperature', '18'),
+            'new_state': None,
+        })
+        db_state = States.from_event(event)
