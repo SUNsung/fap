7b2726394285cb34af310489920f2cd280ec3a40
SUNsung <weatherius@gmail.com>
2018-06-01 19:44:00 +0300

    Rewrite [22YTrKz.K7WbI22jcyf.fkN5Q22CLFVjAQ0bqw22zuh.py] File in: 2018-06-01 19:43:59

--- pyton/22YTrKz.K7WbI22jcyf.fkN5Q22CLFVjAQ0bqw22zuh.py
+++ pyton/22YTrKz.K7WbI22jcyf.fkN5Q22CLFVjAQ0bqw22zuh.py
@@ -1,168 +1,98 @@
 
         
-        
-@csrf_protect
-def render_flatpage(request, f):
+        if isinstance(helptext, bytes):
+    helptext = helptext.decode('utf-8')
+    
+            duration = parse_duration(self._search_regex(
+            r'<b>Duration:</b> (?:<q itemprop='duration'>)?(\d+:\d+)', webpage, 'duration', fatal=False))
+        view_count = int_or_none(self._html_search_regex(
+            r'<b>Views:</b> (\d+)', webpage, 'view count', fatal=False))
+    
+    
+class RewriteOperation(object):
+    '''@brief Internal helper class.'''
+    
+    def __init__(self, stream, index, text):
+        self.stream = stream
+        self.index = index
+        self.text = text
+    
+    class ClassicToken(Token):
+    '''@brief Alternative token implementation.
+    
+    A Token object like we'd use in ANTLR 2.x; has an actual string created
+    and associated with this object.  These objects are needed for imaginary
+    tree nodes that have payload objects.  We need to create a Token object
+    that has a string; the tree node will point at this token.  CommonToken
+    has indexes into a char stream and hence cannot be used to introduce
+    new strings.
     '''
-    Internal interface to the flat page view.
-    '''
-    # If registration is required for accessing this page, and the user isn't
-    # logged in, redirect to the login page.
-    if f.registration_required and not request.user.is_authenticated:
-        from django.contrib.auth.views import redirect_to_login
-        return redirect_to_login(request.path)
-    if f.template_name:
-        template = loader.select_template((f.template_name, DEFAULT_TEMPLATE))
-    else:
-        template = loader.get_template(DEFAULT_TEMPLATE)
     
-        def test_conflicts(self):
-        from certbot_apache.obj import Addr
-        from certbot_apache.obj import VirtualHost
+    - BaseRecognizer: Base class with common recognizer functionality.
+- Lexer: Base class for lexers.
+- Parser: Base class for parsers.
+- tree.TreeParser: Base class for %tree parser.
     
-        def tearDown(self):
-        logging.disable(logging.NOTSET)
     
-        @mock.patch('certbot.notify.smtplib.LMTP')
-    @mock.patch('certbot.notify.subprocess.Popen')
-    def test_smtp_failure(self, mock_popen, mock_lmtp):
-        from certbot.notify import notify
-        lmtp_obj = mock.MagicMock()
-        mock_lmtp.return_value = lmtp_obj
-        lmtp_obj.sendmail.side_effect = socket.error(17)
-        proc = mock.MagicMock()
-        mock_popen.return_value = proc
-        self.assertTrue(notify('Goose', 'auntrhody@example.com',
-                               'The old grey goose is dead.'))
-        self.assertEqual(lmtp_obj.sendmail.call_count, 1)
-        self.assertEqual(proc.communicate.call_count, 1)
+    def getDescription(self):
+        return 'n/a'
     
-        description = proj_info['description'],
-    keywords = proj_info['keywords'],
+    def get_extension(link):
+    extension = os.path.splitext(link)[1][1:]
+    if extension in ['pdf', 'html']:
+        return extension
+    if 'pdf' in extension:
+        return 'pdf'    
+    return 'pdf'    
     
-            # cookie handler
-        ssl_context = request.HTTPSHandler(
-            context=ssl.SSLContext(ssl.PROTOCOL_TLSv1))
-        cookie_handler = request.HTTPCookieProcessor()
-        opener = request.build_opener(ssl_context, cookie_handler)
-        opener.addheaders = [
-            ('Referer', self.url),
-            ('Cookie',
-             'CloudFront-Policy=%s;CloudFront-Signature=%s;CloudFront-Key-Pair-Id=%s' % (scp, scs, sck))
-        ]
-        request.install_opener(opener)
+    # Possible future states (for internal use by the futures package).
+PENDING = 'PENDING'
+RUNNING = 'RUNNING'
+# The future was cancelled by the user...
+CANCELLED = 'CANCELLED'
+# ...and _Waiter.add_cancelled() was called by a worker.
+CANCELLED_AND_NOTIFIED = 'CANCELLED_AND_NOTIFIED'
+FINISHED = 'FINISHED'
     
-    from ..common import *
-import urllib.error
-from json import loads
-from time import time, sleep
+    class _WorkItem(object):
+    def __init__(self, future, fn, args, kwargs):
+        self.future = future
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
     
-        def _format_chunk(self, chunk):
-        if self._expected_content_remaining is not None:
-            self._expected_content_remaining -= len(chunk)
-            if self._expected_content_remaining < 0:
-                # Close the stream now to stop further framing errors.
-                self.stream.close()
-                raise httputil.HTTPOutputError(
-                    'Tried to write more data than Content-Length')
-        if self._chunking_output and chunk:
-            # Don't write out empty chunks because that means END-OF-STREAM
-            # with chunked encoding
-            return utf8('%x' % len(chunk)) + b'\r\n' + chunk + b'\r\n'
+    def main():
+    for name, fn in [('sequential', sequential),
+                     ('processes', with_process_pool_executor),
+                     ('threads', with_thread_pool_executor)]:
+        sys.stdout.write('%s: ' % name.ljust(12))
+        start = time.time()
+        if fn() != [True] * len(PRIMES):
+            sys.stdout.write('failed\n')
         else:
-            return chunk
-    
-        def __exit__(self, type, value, traceback):
-        _state.contexts = self.old_contexts
-    
-        @unittest.skipIf(os.name == 'nt', 'flaky on windows')
-    def test_large_body_streaming_chunked(self):
-        with ExpectLog(gen_log, '.*chunked body too large'):
-            response = self.fetch('/streaming', method='PUT',
-                                  body_producer=lambda write: write(b'a' * 10240))
-        self.assertEqual(response.code, 400)
+            sys.stdout.write('%.2f seconds\n' % (time.time() - start))
     
     
-def try_close(f):
-    # Avoid issue #875 (race condition when using the file in another
-    # thread).
-    for i in range(10):
-        try:
-            f.close()
-        except IOError:
-            # Yield to another thread
-            time.sleep(1e-3)
-        else:
-            break
-    # Try a last time and let raise
-    f.close()
+class DiagnosticFilter( object ):
+  def __init__( self, config_or_filters ):
+    if isinstance( config_or_filters, list ):
+      self._filters = config_or_filters
     
-            future = c.wait()
-        self.assertEqual(102, len(c._waiters))
+        vim_eval.assert_has_exact_calls( [
+      call( 'setqflist( {0} )'.format( json.dumps( expected_qf_list ) ) )
+    ] )
+    vim_command.assert_has_exact_calls( [
+      call( 'botright copen' ),
+      call( 'au WinLeave <buffer> q' ),
+      call( 'doautocmd User YcmQuickFixOpened' )
+    ] )
+    set_fitting_height.assert_called_once_with()
     
-        IoU_lo_thresh = 0.5
-    IoU_hi_thresh = 0.95
-    ind_lo = _get_thr_ind(coco_eval, IoU_lo_thresh)
-    ind_hi = _get_thr_ind(coco_eval, IoU_hi_thresh)
-    # precision has dims (iou, recall, cls, area range, max dets)
-    # area range index 0: all area ranges
-    # max dets index 2: 100 per image
-    precision = coco_eval.eval['precision'][ind_lo:(ind_hi + 1), :, :, 0, 2]
-    ap_default = np.mean(precision[precision > -1])
-    logger.info(
-        '~~~~ Mean and per-category AP @ IoU=[{:.2f},{:.2f}] ~~~~'.format(
-            IoU_lo_thresh, IoU_hi_thresh))
-    logger.info('{:.1f}'.format(100 * ap_default))
-    for cls_ind, cls in enumerate(json_dataset.classes):
-        if cls == '__background__':
-            continue
-        # minus 1 because of __background__
-        precision = coco_eval.eval['precision'][
-            ind_lo:(ind_hi + 1), :, cls_ind - 1, 0, 2]
-        ap = np.mean(precision[precision > -1])
-        logger.info('{:.1f}'.format(100 * ap))
-    logger.info('~~~~ Summary metrics ~~~~')
-    coco_eval.summarize()
+    DEFAULT_NAME = 'PVOutput'
+DEFAULT_VERIFY_SSL = True
     
+        return True
+
     
-# ---------------------------------------------------------------------------- #
-# RPN and Faster R-CNN outputs and losses
-# ---------------------------------------------------------------------------- #
-    
-        # subsample negative labels if we have too many
-    # (samples with replacement, but since the set of bg inds is large most
-    # samples will not have repeats)
-    num_bg = cfg.TRAIN.RPN_BATCH_SIZE_PER_IM - np.sum(labels == 1)
-    bg_inds = np.where(anchor_to_gt_max < cfg.TRAIN.RPN_NEGATIVE_OVERLAP)[0]
-    if len(bg_inds) > num_bg:
-        enable_inds = bg_inds[npr.randint(len(bg_inds), size=num_bg)]
-        labels[enable_inds] = 0
-    bg_inds = np.where(labels == 0)[0]
-    
-        def test_bbox_dataset_to_prediction_roundtrip(self):
-        '''Simulate the process of reading a ground-truth box from a dataset,
-        make predictions from proposals, convert the predictions back to the
-        dataset format, and then use the COCO API to compute IoU overlap between
-        the gt box and the predictions. These should have IoU of 1.
-        '''
-        weights = (5, 5, 10, 10)
-        # 1/ 'read' a box from a dataset in the default (x1, y1, w, h) format
-        gt_xywh_box = [10, 20, 100, 150]
-        # 2/ convert it to our internal (x1, y1, x2, y2) format
-        gt_xyxy_box = box_utils.xywh_to_xyxy(gt_xywh_box)
-        # 3/ consider nearby proposal boxes
-        prop_xyxy_boxes = random_boxes(gt_xyxy_box, 10, 10)
-        # 4/ compute proposal-to-gt transformation deltas
-        deltas = box_utils.bbox_transform_inv(
-            prop_xyxy_boxes, np.array([gt_xyxy_box]), weights=weights
-        )
-        # 5/ use deltas to transform proposals to xyxy predicted box
-        pred_xyxy_boxes = box_utils.bbox_transform(
-            prop_xyxy_boxes, deltas, weights=weights
-        )
-        # 6/ convert xyxy predicted box to xywh predicted box
-        pred_xywh_boxes = box_utils.xyxy_to_xywh(pred_xyxy_boxes)
-        # 7/ use COCO API to compute IoU
-        not_crowd = [int(False)] * pred_xywh_boxes.shape[0]
-        ious = COCOmask.iou(pred_xywh_boxes, np.array([gt_xywh_box]), not_crowd)
-        np.testing.assert_array_almost_equal(ious, np.ones(ious.shape))
+    from homeassistant.components.device_tracker import bt_home_hub_5
+from homeassistant.const import CONF_HOST
