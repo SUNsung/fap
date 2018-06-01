9ae962ee298ef1b9d9689495ce844484fef0406f
SUNsung <weatherius@gmail.com>
2018-06-01 21:53:48 +0300

    Rewrite [35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py] File in: 2018-06-01 21:53:47

--- pyton/35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py
+++ pyton/35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py
@@ -1,96 +1,181 @@
 
         
-        tf_proto_library_py(
+        
+if __name__ == '__main__':
+    unittest.main()
+
+    
+    
+class TestMultipleSocks(unittest.TestCase):
+    @staticmethod
+    def _check_params(attrs):
+        params = get_params()
+        for attr in attrs:
+            if attr not in params:
+                print('Missing %s. Skipping.' % attr)
+                return
+        return params
+    
+    
+class TestUnicodeLiterals(unittest.TestCase):
+    def test_all_files(self):
+        for dirpath, dirnames, filenames in os.walk(rootDir):
+            for ignore_dir in IGNORED_DIRS:
+                if ignore_dir in dirnames:
+                    # If we remove the directory from dirnames os.walk won't
+                    # recurse into it
+                    dirnames.remove(ignore_dir)
+            for basename in filenames:
+                if not basename.endswith('.py'):
+                    continue
+                if basename in IGNORED_FILES:
+                    continue
+    
+    
+class CloserToTruthIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)?closertotruth\.com/(?:[^/]+/)*(?P<id>[^/?#&]+)'
+    _TESTS = [{
+        'url': 'http://closertotruth.com/series/solutions-the-mind-body-problem#video-3688',
+        'info_dict': {
+            'id': '0_zof1ktre',
+            'display_id': 'solutions-the-mind-body-problem',
+            'ext': 'mov',
+            'title': 'Solutions to the Mind-Body Problem?',
+            'upload_date': '20140221',
+            'timestamp': 1392956007,
+            'uploader_id': 'CTTXML'
+        },
+        'params': {
+            'skip_download': True,
+        },
+    }, {
+        'url': 'http://closertotruth.com/episodes/how-do-brains-work',
+        'info_dict': {
+            'id': '0_iuxai6g6',
+            'display_id': 'how-do-brains-work',
+            'ext': 'mov',
+            'title': 'How do Brains Work?',
+            'upload_date': '20140221',
+            'timestamp': 1392956024,
+            'uploader_id': 'CTTXML'
+        },
+        'params': {
+            'skip_download': True,
+        },
+    }, {
+        'url': 'http://closertotruth.com/interviews/1725',
+        'info_dict': {
+            'id': '1725',
+            'title': 'AyaFr-002',
+        },
+        'playlist_mincount': 2,
+    }]
+    
+    tf_proto_library_py(
     name = 'spec_pb2',
     srcs = ['spec.proto'],
 )
     
-      def testDerivedParametersForRuntime(self):
-    '''Test generation of derived parameters for the runtime.'''
-    with tf.Graph().as_default(), self.test_session():
-      master = MockMaster()
-      component = MockComponent(master, _make_biaffine_spec())
+          self.assertAllEqual(arcs.eval(), [[[53, 53, 53],
+                                         [63, 63, 63],
+                                         [73, 73, 73]],
+                                        [[73, 73, 73],
+                                         [63, 63, 63],
+                                         [53, 53, 53]]])  # pyformat: disable
+    
+      Args:
+    master_spec_path: Path to a proto-text master spec.
+    params_path: Path to the parameters file to export.
+    export_path: Path to export the SavedModel to.
+    export_moving_averages: Whether to export the moving average parameters.
+    build_runtime_graph: Whether to build a graph for use by the runtime.
+  '''
+    
+      Returns:
+    Dict mapping from shortened resource path to original resource path.
+  '''
+  for component_spec in master_spec.component:
+    for feature_spec in component_spec.fixed_feature:
+      feature_spec.ClearField('pretrained_embedding_matrix')
+      feature_spec.ClearField('vocab')
+    
+        try:
+      with open(FLAGS.expected_file) as expected:
+        content_expected = expected.read()
+    except IOError as e:
+      self.fail('Error opening '%s': %s' % (FLAGS.expected_file, e.strerror))
+    
+      Computing the sum-of-product in the log domain reduces the chance of overflow
+  or underflow, and ML techniques (e.g., CRF loss functions) typically require
+  the log partition function anyways.  For similar reasons, the scores input is
+  assumed to be specified in the log domain.
     
     
-def fetch_fast_fixed_embeddings(comp,
-                                state,
-                                pad_to_batch=None,
-                                pad_to_steps=None):
-  '''Looks up fixed features with fast, non-differentiable, op.
-    
-      # Resets session.
-  session_config = tf.ConfigProto(
-      log_device_placement=False,
-      intra_op_parallelism_threads=10,
-      inter_op_parallelism_threads=10)
+@pytest.fixture(params=containers)
+def proc(request, spawnu, TIMEOUT):
+    proc = spawnu(*request.param)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(u'thefuck --alias > ~/.config/fish/config.fish')
+    proc.sendline(u'fish')
+    return proc
     
     
-if __name__ == '__main__':
-  tf.test.main()
-
+def test_regression_class_build_fn():
+    class ClassBuildFnReg(object):
     
-    ## All tokens go to the parser (unless skip() is called in that rule)
-# on a particular 'channel'.  The parser tunes to a particular channel
-# so that whitespace etc... can go to the parser on a 'hidden' channel.
-DEFAULT_CHANNEL = 0
-    
-        def test_big_queue_popright(self):
-        d = deque()
-        append, pop = d.appendleft, d.pop
-        for i in range(BIG):
-            append(i)
-        for i in range(BIG):
-            x = pop()
-            if x != i:
-                self.assertEqual(x, i)
-    
-        def unlock(self):
-        '''Unlock the mailbox if it is locked.'''
-        if self._locked:
-            _unlock_file(self._file)
-            _sync_close(self._file)
-            del self._file
-            self._locked = False
-    
-        print('LIBS=', end=' ')
-    for lib in libs: print(''%s'' % (lib), end=' ')
-    print() ; print()
+        if args.weights:
+        vae = vae.load_weights(args.weights)
+    else:
+        # train the autoencoder
+        vae.fit(x_train,
+                epochs=epochs,
+                batch_size=batch_size,
+                validation_data=(x_test, None))
+        vae.save_weights('vae_mlp_mnist.h5')
     
     
-def pyfunc(arg1, arg2):
-    return [arg1, arg2]
+def main():
+    message_center = Provider()
     
-        1: ('line', 2, 'tfunc_import'),    ('next',),
-    2: ('line', 3, 'tfunc_import'),    ('step',),
-    3: ('call', 5, 'main'),            ('break', ('test_module.py', None, False, None, 'func')),
-    4: ('None', 5, 'main'),            ('continue',),
-    5: ('line', 3, 'func', ({1: 1}, [])), ('step',),
-      BpNum Temp Enb Hits Ignore Where
-      1     no   yes 1    0      at test_module.py:2
-    6: ('return', 3, 'func'),          ('step',),
-    7: ('line', 7, 'main'),            ('step',),
-    8: ('return', 7, 'main'),          ('quit',),
+        class ClassRegistree(BaseRegisteredClass):
     
-        top = Toplevel(parent)
-    top.title('Test ColorDelegator')
-    x, y = map(int, parent.geometry().split('+')[1:])
-    top.geometry('700x250+%d+%d' % (x + 20, y + 175))
-    source = (
-        'if True: int ('1') # keyword, builtin, string, comment\n'
-        'elif False: print(0)\n'
-        'else: float(None)\n'
-        'if iF + If + IF: 'keyword matching must respect case'\n'
-        'if'': x or''  # valid string-keyword no-space combinations\n'
-        'async def f(): await g()\n'
-        '# All valid prefixes for unicode and byte strings should be colored.\n'
-        ''x', '''x''', \'x\', \'\'\'x\'\'\'\n'
-        'r'x', u'x', R'x', U'x', f'x', F'x'\n'
-        'fr'x', Fr'x', fR'x', FR'x', rf'x', rF'x', Rf'x', RF'x'\n'
-        'b'x',B'x', br'x',Br'x',bR'x',BR'x', rb'x'.rB'x',Rb'x',RB'x'\n'
-        '# Invalid combinations of legal characters should be half colored.\n'
-        'ur'x', ru'x', uf'x', fu'x', UR'x', ufr'x', rfu'x', xf'x', fx'x'\n'
-        )
-    text = Text(top, background='white')
-    text.pack(expand=1, fill='both')
-    text.insert('insert', source)
-    text.focus_set()
+        def test_parrot_eng_localization(self):
+        self.assertEqual(self.e.get('parrot'), 'parrot')
+    
+        def setUp(self):
+        self.tc1 = TC1()
+        self.tc2 = TC2()
+        self.tc3 = TC3()
+        self.average_result_tc1 = '###### In Test 1 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc2 = '###### In Test 2 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc3 = '###### In Test 3 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.runner = TestRunner()
+        self.out = StringIO()
+        self.saved_stdout = sys.stdout
+        sys.stdout = self.out
+    
+    '''
+http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
+Implementation of the iterator pattern with a generator
+    
+        def __get__(self, obj, T):
+        def transaction(*args, **kwargs):
+            state = memento(obj)
+            try:
+                return self.method(obj, *args, **kwargs)
+            except Exception as e:
+                state()
+                raise e
