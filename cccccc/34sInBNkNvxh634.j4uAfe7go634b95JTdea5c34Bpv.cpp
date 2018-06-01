b54b270f58bd6a36f9ba407e0f0f7d30b5799406
SUNsung <weatherius@gmail.com>
2018-06-02 02:23:25 +0300

    Rewrite [34sInBNkNvxh634.j4uAfe7go634b95JTdea5c34Bpv.cpp] File in: 2018-06-02 02:23:24

--- cccccc/34sInBNkNvxh634.j4uAfe7go634b95JTdea5c34Bpv.cpp
+++ cccccc/34sInBNkNvxh634.j4uAfe7go634b95JTdea5c34Bpv.cpp
@@ -1,117 +1,219 @@
 
         
-        #endif  // GOOGLE_PROTOBUF_COMPILER_CSHARP_MAP_FIELD_H__
+        
+    {      result->AppendString(argv[i]);
+    }
     
-    RepeatedEnumFieldGenerator::RepeatedEnumFieldGenerator(
-    const FieldDescriptor* descriptor, int fieldOrdinal, const Options *options)
-    : FieldGeneratorBase(descriptor, fieldOrdinal, options) {
+    
+#include 'content/nw/src/api/event/event.h'
+#include 'base/values.h'
+#include 'content/nw/src/api/dispatcher_host.h'
+#include 'ui/gfx/screen.h'
+    
+    
+    {  gfx::Image originImage;
+  nw::Package* package = nw::InitNWPackage();
+  if (nw::GetImage(package, base::FilePath::FromUTF8Unsafe(icon), &originImage)) {
+    const gfx::ImageSkia* originImageSkia = originImage.ToImageSkia();
+    gfx::ImageSkia resizedImageSkia = gfx::ImageSkiaOperations::CreateResizedImage(*originImageSkia,
+                                                                                   skia::ImageOperations::RESIZE_GOOD,
+                                                                                   gfx::Size(kIconWidth, kIconHeight));
+    icon_ = gfx::Image(resizedImageSkia);
+  }
 }
     
-      virtual void GenerateCloningCode(io::Printer* printer);
-  virtual void GenerateFreezingCode(io::Printer* printer);
-  virtual void GenerateMembers(io::Printer* printer);
-  virtual void GenerateMergingCode(io::Printer* printer);
-  virtual void GenerateParsingCode(io::Printer* printer);
-  virtual void GenerateSerializationCode(io::Printer* printer);
-  virtual void GenerateSerializedSizeCode(io::Printer* printer);
+    #include 'caffe/layers/loss_layer.hpp'
     
-    
-    {
-    {
-    {
-    {}  // namespace csharp
-}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-    
-    #ifndef GOOGLE_PROTOBUF_COMPILER_JAVA_DOC_COMMENT_H__
-#define GOOGLE_PROTOBUF_COMPILER_JAVA_DOC_COMMENT_H__
-    
-      // Returns an estimate of the number of bytes the printed code will compile to
-  virtual int GenerateNonNestedInitializationCode(io::Printer* printer);
-    
-    namespace google {
-namespace protobuf {
-namespace compiler {
-namespace java {
+    /**
+ * @brief Convolves the input image with a bank of learned filters,
+ *        and (optionally) adds biases.
+ *
+ *   Caffe convolves by reduction to matrix multiplication. This achieves
+ *   high-throughput and generality of input and filter dimensions but comes at
+ *   the cost of memory for matrices. This makes use of efficiency in BLAS.
+ *
+ *   The input is 'im2col' transformed to a channel K' x H x W data matrix
+ *   for multiplication with the N x K' x H x W filter matrix to yield a
+ *   N' x H x W output matrix that is then 'col2im' restored. K' is the
+ *   input channel * kernel height * kernel width dimension of the unrolled
+ *   inputs so that the im2col matrix has a column for each input region to
+ *   be filtered. col2im restores the output spatial structure by rolling up
+ *   the output channel N' columns of the output matrix.
+ */
+template <typename Dtype>
+class ConvolutionLayer : public BaseConvolutionLayer<Dtype> {
+ public:
+  /**
+   * @param param provides ConvolutionParameter convolution_param,
+   *    with ConvolutionLayer options:
+   *  - num_output. The number of filters.
+   *  - kernel_size / kernel_h / kernel_w. The filter dimensions, given by
+   *  kernel_size for square filters or kernel_h and kernel_w for rectangular
+   *  filters.
+   *  - stride / stride_h / stride_w (\b optional, default 1). The filter
+   *  stride, given by stride_size for equal dimensions or stride_h and stride_w
+   *  for different strides. By default the convolution is dense with stride 1.
+   *  - pad / pad_h / pad_w (\b optional, default 0). The zero-padding for
+   *  convolution, given by pad for equal dimensions or pad_h and pad_w for
+   *  different padding. Input padding is computed implicitly instead of
+   *  actually padding.
+   *  - dilation (\b optional, default 1). The filter
+   *  dilation, given by dilation_size for equal dimensions for different
+   *  dilation. By default the convolution has dilation 1.
+   *  - group (\b optional, default 1). The number of filter groups. Group
+   *  convolution is a method for reducing parameterization by selectively
+   *  connecting input and output channels. The input and output channel dimensions must be divisible
+   *  by the number of groups. For group @f$ \geq 1 @f$, the
+   *  convolutional filters' input and output channels are separated s.t. each
+   *  group takes 1 / group of the input channels and makes 1 / group of the
+   *  output channels. Concretely 4 input channels, 8 output channels, and
+   *  2 groups separate input channels 1-2 and output channels 1-4 into the
+   *  first group and input channels 3-4 and output channels 5-8 into the second
+   *  group.
+   *  - bias_term (\b optional, default true). Whether to have a bias.
+   *  - engine: convolution has CAFFE (matrix multiplication) and CUDNN (library
+   *    kernels + stream parallelism) engines.
+   */
+  explicit ConvolutionLayer(const LayerParameter& param)
+      : BaseConvolutionLayer<Dtype>(param) {}
     }
-    }
-    }
-    }
     
-    GeneratorFactory::GeneratorFactory() {}
-GeneratorFactory::~GeneratorFactory() {}
     
-    enum : uint16_t {
-  kHeapMagic = 0xa5a5,
-  // This memory segment contains an IOBuf that is still in use
-  kIOBufInUse = 0x01,
-  // This memory segment contains buffer data that is still in use
-  kDataInUse = 0x02,
+    {  int M_;
+  int K_;
+  int N_;
+  bool bias_term_;
+  Blob<Dtype> bias_multiplier_;
 };
     
-    template <typename C, ordering o, bool ne>
-struct cmp_pred : private C {
-  using C::C;
-    }
+            if (maxSamplesInRAM < SIZE_MAX)
+        {
+            // into how many pieces would we need to break the minibatch?
+            // TODO: The following calculation relies on the ill-devised definition of 'minibatch' of the current truncated BPTT implementation. Adapt this once fixed.
+            size_t numParallelSequences = dataReader->GetNumParallelSequencesForFixingBPTTMode();
+            size_t estimatedMBSize = tunedMBSize * numParallelSequences;
+            return (estimatedMBSize + maxSamplesInRAM - 1) / maxSamplesInRAM;
+        }
     
-    TEST_F(OrderingTest, compare_less) {
-  compare_less<OddCompare<int>> op;
-  EXPECT_FALSE(op(3, 4));
-  EXPECT_FALSE(op(3, 3));
-  EXPECT_TRUE(op(4, 3));
+    
+    {    auto netBuilder = make_shared<SimpleNetworkBuilder<ElemType>>(config);
+    ComputationNetworkPtr net = netBuilder->BuildNetworkFromDbnFile(dbnModelPath);
+    net->Save(modelPath);
 }
     
-      Atom<Node*> head_{nullptr};
-    
-    
-    { private:
-  using Holder = std::shared_ptr<T>;
-  struct Slots : folly::hazptr_obj_base<Slots> {
-    std::array<std::shared_ptr<T>, kNumSlots> slots_;
-  };
-  std::atomic<Slots*> slots_{nullptr};
-};
-    
-    TEST(Traits, bitAndInit) {
-  EXPECT_TRUE (IsTriviallyCopyable<int>::value);
-  EXPECT_FALSE(IsTriviallyCopyable<vector<int>>::value);
-  EXPECT_TRUE (IsZeroInitializable<int>::value);
-  EXPECT_FALSE(IsZeroInitializable<vector<int>>::value);
+    // Same as 'ReadConfigFile' function below, but takes as input string instead of wstring
+std::string ConfigParser::ReadConfigFile(const std::string& filePath)
+{
+    return ReadConfigFile(msra::strfun::utf16(filePath));
 }
     
-    TEST_F(UtilityTest, forward_like) {
-  int x = 0;
-  // just show that it may be invoked, and that it is purely a cast
-  // the real work is done by like_t, in terms of which forward_like is defined
-  EXPECT_EQ(&x, std::addressof(folly::forward_like<char&>(x)));
-  EXPECT_EQ(&x, std::addressof(folly::forward_like<char const>(x)));
-}
+        // SaveData - save data in the file/files
+    // recordStart - Starting record number
+    // matricies - a map of section name (section:subsection) to data pointer. Data sepcifications from config file will be used to determine where and how to save data
+    // numRecords - number of records we are saving, can be zero if not applicable
+    // datasetSize - size of the dataset (in records)
+    // byteVariableSized - for variable sized data, size of current block to be written, zero when not used, or ignored if not variable sized data
+    virtual bool SaveData(size_t recordStart, const std::map<std::wstring, void*, nocase_compare>& matrices, size_t numRecords, size_t datasetSize, size_t byteVariableSized = 0);
     
-        while (-1 != __ParseStack(stream, state, strcache, strstack)) {
-        if (!_processname.empty() && std::string::npos == strstack.find(processname, 0)) {
-            strstack.clear();
-            continue;
+        // emit the trace message for global progress
+    // 'currentStep' will be offset by m_currentStepOffset.
+    // This only prints of enough time (10s) has elapsed since last print, and the return value is 'true' if it did print.
+    static bool TraceProgressPercentage(size_t epochNumber, double mbProg /*0..100*/, bool isTimerPaced)
+    {
+        auto& us = GetStaticInstance();
+        if (!us.m_enabled)
+        {
+            return false;
         }
     }
     
-    class CoreServiceBase: public ServiceBase {
-  protected:
-    CoreServiceBase(const char* _servicename);
-    virtual ~CoreServiceBase();
+    
+    {public:
+    inline array_ref(_T* ptr, size_t size) throw()
+        : data(ptr), n(size)
+    {
+    }
+    inline array_ref() throw()
+        : data(NULL), n(0)
+    {
+    } // in case we have a vector of this
+    inline _T& operator[](size_t i) throw()
+    {
+        check_index(i);
+        return data[i];
+    }
+    inline const _T& operator[](size_t i) const throw()
+    {
+        check_index(i);
+        return data[i];
+    }
+    inline size_t size() const throw()
+    {
+        return n;
+    }
+    inline _T* begin()
+    {
+        return data;
+    }
+    inline _T* end()
+    {
+        return data + n;
+    }
+    inline void resize(size_t sz)
+    {
+        sz;
+        assert(n == sz);
+    } // allow compatibility with some functions
+    // construct from other vector types
+    template <class _V>
+    inline array_ref(_V& v)
+        : data(v.size() > 0 ? &v[0] : NULL), n((size_t) v.size())
+    {
+    }
+};
+    
+    #pragma once
+#include 'rocksdb/merge_operator.h'
+#include 'rocksdb/slice.h'
+    
+        // Append, Flush, Append, Get
+    slists.Append('a', 'x');
+    slists.Append('b', 'y');
+    db->Flush(rocksdb::FlushOptions());
+    slists.Append('a', 't');
+    slists.Append('a', 'r');
+    slists.Append('b', '2');
+    
+      /// Push / Insert value at beginning/end of the list. Return the length.
+  /// May throw RedisListException
+  int PushLeft(const std::string& key, const std::string& value);
+  int PushRight(const std::string& key, const std::string& value);
+    
+      ~Reader();
+    
+      void SetInternalKey(const Slice& key_prefix, const Slice& user_key,
+                      SequenceNumber s,
+                      ValueType value_type = kValueTypeForSeek) {
+    size_t psize = key_prefix.size();
+    size_t usize = user_key.size();
+    EnlargeBufferIfNeeded(psize + usize + sizeof(uint64_t));
+    if (psize > 0) {
+      memcpy(buf_, key_prefix.data(), psize);
+    }
+    memcpy(buf_ + psize, user_key.data(), usize);
+    EncodeFixed64(buf_ + usize + psize, PackSequenceAndType(s, value_type));
     }
     
+      // call once at the beginning of a test to setup the dependency between
+  // sync points and setup markers indicating the successor is only enabled
+  // when it is processed on the same thread as the predecessor.
+  // When adding a marker, it implicitly adds a dependency for the marker pair.
+  void LoadDependencyAndMarkers(const std::vector<SyncPointPair>& dependencies,
+                                const std::vector<SyncPointPair>& markers);
     
-/*
- * ServiceBase.h
- *
- *  Created on: 2013-6-20
- *      Author: yerungui
- */
-    
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
-    
-    #include <jni.h>
+      std::mutex              mutex_;
+  std::condition_variable cv_;
+  // sync points that have been passed through
+  std::unordered_set<std::string> cleared_points_;
+  bool enabled_ = false;
+  int num_callbacks_running_ = 0;
