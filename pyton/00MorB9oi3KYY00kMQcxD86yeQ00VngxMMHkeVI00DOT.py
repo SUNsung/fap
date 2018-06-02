3cc6f963b15e95bfba1845f6f872edce5c572940
SUNsung <weatherius@gmail.com>
2018-06-03 02:53:09 +0300

    Rewrite [00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py] File in: 2018-06-03 02:53:08

--- pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
+++ pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
@@ -1,180 +1,152 @@
 
         
-          # Lookahead LSTM reads right-to-left to represent the rightmost context of the
-  # words. It gets word embeddings from the char model.
-  lookahead = spec_builder.ComponentSpecBuilder('lookahead')
-  lookahead.set_network_unit(
-      name='wrapped_units.LayerNormBasicLSTMNetwork',
-      hidden_layer_sizes='256')
-  lookahead.set_transition_system(name='shift-only', left_to_right='false')
-  lookahead.add_link(source=char2word, fml='input.last-char-focus',
-                     embedding_dim=64)
+            CHUNK_SIZE = 1
     
-    py_library(
-    name = 'mst_units',
-    srcs = ['mst_units.py'],
-    deps = [
-        ':mst_ops',
-        ':network_units',
-        '//syntaxnet/util:check',
-        '@org_tensorflow//tensorflow:tensorflow_py',
-    ],
-)
+    import requests.auth
     
-        self._weights = []
-    self._weights.append(
-        tf.get_variable('weights_arc', [self._source_dim, self._target_dim],
-                        tf.float32, tf.orthogonal_initializer()))
-    self._weights.append(
-        tf.get_variable('weights_source', [self._source_dim], tf.float32,
-                        tf.zeros_initializer()))
-    self._weights.append(
-        tf.get_variable('root', [self._source_dim], tf.float32,
-                        tf.zeros_initializer()))
+    from ansible.plugins.terminal import TerminalBase
+from ansible.errors import AnsibleConnectionFailure
     
-        self.setUp()
-    builder = bulk_component.BulkAnnotatorComponentBuilder(
-        self.master, component_spec)
-    builder.network = AssertPreCreateBeforeCreateNetwork(builder, self)
-    builder.build_greedy_training(
-        component.MasterState(['foo', 'bar'], 2), self.network_states)
-    
-      source_tokens_shape = tf.shape(source_tokens)
-  target_tokens_shape = tf.shape(target_tokens)
-  batch_size = source_tokens_shape[0]
-  num_tokens = source_tokens_shape[1]
-  with tf.control_dependencies([
-      tf.assert_equal(batch_size, target_tokens_shape[0]),
-      tf.assert_equal(num_tokens, target_tokens_shape[1])]):
-    # Flatten out the batch dimension so we can use one big multiplication.
-    targets_bnxt = tf.reshape(target_tokens, [-1, num_target_activations])
-    
-      Replaces resource paths in the MasterSpec with shortened paths and builds a
-  mapping from the shortened path to the original path. Note that shortened
-  paths are relative to the 'assets.extra' directory of the SavedModel. Also
-  removes resources from FixedFeatureChannel, since they are not exported.
-    
-      def testEqualFiles(self):
-    content_actual = None
-    content_expected = None
-    
-      # To eliminate overflow, we locally normalize the scores.  Specifically, for
-  # each node we divide its incoming arc scores and root selection score by the
-  # maximum such score.  Since each node in a tree must select exactly one of
-  # these scores (i.e., it is either a root or has exactly one incoming arc),
-  # the local normalization factors are identical for all trees and can thus be
-  # factored out of the sum over trees.
-  #
-  # More concretely, we find the maximum per node, divide all scores for that
-  # node by the maximum, and then find the partition function of the normalized
-  # scores.  Then we recover the un-normalized partition function by multiplying
-  # the per-node maxima back in.  This final step is performed in the log domain
-  # to avoid overflow.
-  #
-  # Note that underflow is still possible, but unlikely as long as the scores
-  # are close to feasible (i.e., there is not too much mass on non-trees).  The
-  # |max_dynamic_range| argument can be used to mitigate this.
-    
-    '''
-requests.hooks
-~~~~~~~~~~~~~~
-    
-    
-@pytest.fixture
-def httpbin_secure(httpbin_secure):
-    return prepare_url(httpbin_secure)
+        def on_open_shell(self):
+        try:
+            self._exec_cli_command(b'environment no more')
+        except AnsibleConnectionFailure:
+            raise AnsibleConnectionFailure('unable to set terminal parameters')
 
     
-        return out
+        def __init__(self, pages, data_store, reverse_index_queue, doc_index_queue):
+        self.pages = pages
+        self.data_store = data_store
+        self.reverse_index_queue = reverse_index_queue
+        self.doc_index_queue = doc_index_queue
     
-        def __delitem__(self, key):
-        del self._store[key.lower()]
+        def bfs(self, source, dest):
+        if source is None:
+            return False
+        queue = deque()
+        queue.append(source)
+        source.visit_state = State.visited
+        while queue:
+            node = queue.popleft()
+            print(node)
+            if dest is node:
+                return True
+            for adjacent_node in node.adj_nodes.values():
+                if adjacent_node.visit_state == State.unvisited:
+                    queue.append(adjacent_node)
+                    adjacent_node.visit_state = State.visited
+        return False
     
-        def test_copy(self):
-        copy = self.case_insensitive_dict.copy()
-        assert copy is not self.case_insensitive_dict
-        assert copy == self.case_insensitive_dict
-    
-        def get_new_headers(self):
-        return self._new_headers
-    
-        with server as (host, port):
-        url = 'http://{0}:{1}/'.format(host, port)
-        r = requests.get(url, auth=auth)
-        # Verify server didn't receive auth from us.
-        assert r.status_code == 200
-        assert len(r.history) == 0
-        close_server.set()
-    
-        Provides a general-case interface for Requests sessions to contact HTTP and
-    HTTPS urls by implementing the Transport Adapter interface. This class will
-    usually be created by the :class:`Session <Session>` class under the
-    covers.
-    
-        if implementation == 'CPython':
-        implementation_version = platform.python_version()
-    elif implementation == 'PyPy':
-        implementation_version = '%s.%s.%s' % (sys.pypy_version_info.major,
-                                               sys.pypy_version_info.minor,
-                                               sys.pypy_version_info.micro)
-        if sys.pypy_version_info.releaselevel != 'final':
-            implementation_version = ''.join([
-                implementation_version, sys.pypy_version_info.releaselevel
-            ])
-    elif implementation == 'Jython':
-        implementation_version = platform.python_version()  # Complete Guess
-    elif implementation == 'IronPython':
-        implementation_version = platform.python_version()  # Complete Guess
-    else:
-        implementation_version = 'Unknown'
-    
-        :param method: method for the new :class:`Request` object.
-    :param url: URL for the new :class:`Request` object.
-    :param params: (optional) Dictionary or bytes to be sent in the query string for the :class:`Request`.
-    :param data: (optional) Dictionary or list of tuples ``[(key, value)]`` (will be form-encoded), bytes, or file-like object to send in the body of the :class:`Request`.
-    :param json: (optional) A JSON serializable Python object to send in the body of the :class:`Request`.
-    :param headers: (optional) Dictionary of HTTP Headers to send with the :class:`Request`.
-    :param cookies: (optional) Dict or CookieJar object to send with the :class:`Request`.
-    :param files: (optional) Dictionary of ``'name': file-like-objects`` (or ``{'name': file-tuple}``) for multipart encoding upload.
-        ``file-tuple`` can be a 2-tuple ``('filename', fileobj)``, 3-tuple ``('filename', fileobj, 'content_type')``
-        or a 4-tuple ``('filename', fileobj, 'content_type', custom_headers)``, where ``'content-type'`` is a string
-        defining the content type of the given file and ``custom_headers`` a dict-like object containing additional headers
-        to add for the file.
-    :param auth: (optional) Auth tuple to enable Basic/Digest/Custom HTTP Auth.
-    :param timeout: (optional) How many seconds to wait for the server to send data
-        before giving up, as a float, or a :ref:`(connect timeout, read
-        timeout) <timeouts>` tuple.
-    :type timeout: float or tuple
-    :param allow_redirects: (optional) Boolean. Enable/disable GET/OPTIONS/POST/PUT/PATCH/DELETE/HEAD redirection. Defaults to ``True``.
-    :type allow_redirects: bool
-    :param proxies: (optional) Dictionary mapping protocol to the URL of the proxy.
-    :param verify: (optional) Either a boolean, in which case it controls whether we verify
-            the server's TLS certificate, or a string, in which case it must be a path
-            to a CA bundle to use. Defaults to ``True``.
-    :param stream: (optional) if ``False``, the response content will be immediately downloaded.
-    :param cert: (optional) if String, path to ssl client cert file (.pem). If Tuple, ('cert', 'key') pair.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    
-        return ip_string[:ip_string_size.value - 1]
-    
-    class MismatchedSetException(RecognitionException):
-    '''@brief The next token does not match a set of expected types.'''
-    
-    api('reltableitem', RelTableItemJsonTemplate)
-api('bannedtableitem', BannedTableItemJsonTemplate)
-api('mutedtableitem', MutedTableItemJsonTemplate)
-api('invitedmodtableitem', InvitedModTableItemJsonTemplate)
-api('friendtableitem', FriendTableItemJsonTemplate)
+            Emit key value pairs of the form:
     
     
-class GoogleTagManagerController(MinimalController):
-    def pre(self):
-        if request.host != g.media_domain:
-            # don't serve up untrusted content except on our
-            # specifically untrusted domain
-            self.abort404()
+class Vehicle(metaclass=ABCMeta):
     
-        def GET_promohealth(self):
-        response.content_type = 'application/json'
-        return json.dumps(promote.health_check())
+    
+seller_category_map = {}
+seller_category_map['Exxon'] = DefaultCategories.GAS
+seller_category_map['Target'] = DefaultCategories.SHOPPING
+    
+    def get_colordb(file, filetype=None):
+    colordb = None
+    fp = open(file)
+    try:
+        line = fp.readline()
+        if not line:
+            return None
+        # try to determine the type of RGB file it is
+        if filetype is None:
+            filetypes = FILETYPES
+        else:
+            filetypes = [filetype]
+        for typere, class_ in filetypes:
+            mo = typere.search(line)
+            if mo:
+                break
+        else:
+            # no matching type
+            return None
+        # we know the type and the class to grok the type, so suck it in
+        colordb = class_(fp)
+    finally:
+        fp.close()
+    # save a global copy
+    global DEFAULT_DB
+    DEFAULT_DB = colordb
+    return colordb
+    
+    
+    {            ('2.5.2 (63378, Mar 26 2009, 18:03:29)\n[PyPy 1.0.0]',
+             ('PyPy', 'trunk', '63378'), self.save_platform)
+            :
+                ('PyPy', '2.5.2', 'trunk', '63378', ('63378', 'Mar 26 2009'),
+                 '')
+            }
+        for (version_tag, scm, sys_platform), info in \
+                sys_versions.items():
+            sys.version = version_tag
+            if scm is None:
+                if hasattr(sys, '_git'):
+                    del sys._git
+            else:
+                sys._git = scm
+            if sys_platform is not None:
+                sys.platform = sys_platform
+            self.assertEqual(platform.python_implementation(), info[0])
+            self.assertEqual(platform.python_version(), info[1])
+            self.assertEqual(platform.python_branch(), info[2])
+            self.assertEqual(platform.python_revision(), info[3])
+            self.assertEqual(platform.python_build(), info[4])
+            self.assertEqual(platform.python_compiler(), info[5])
+    
+    def color_config(text):  # Called from htest, Editor, and Turtle Demo.
+    '''Set color opitons of Text widget.
+    
+    
+# Stubs to make mypy happy (and later for actual type-checking).
+def raise_exc_info(exc_info):
+    # type: (Tuple[type, BaseException, types.TracebackType]) -> None
+    pass
+    
+    
+def wrap_web_tests_application():
+    result = {}
+    for cls in web_test.wsgi_safe_tests:
+        def class_factory():
+            class WSGIApplicationWrappedTest(cls):  # type: ignore
+                def setUp(self):
+                    self.warning_catcher = ignore_deprecation()
+                    self.warning_catcher.__enter__()
+                    super(WSGIApplicationWrappedTest, self).setUp()
+    
+        The `tornado.web.FallbackHandler` class is often useful for mixing
+    Tornado and WSGI apps in the same server.  See
+    https://github.com/bdarnell/django-tornado-demo for a complete example.
+    '''
+    def __init__(self, wsgi_application):
+        self.wsgi_application = wsgi_application
+    
+        def _on_connection_close(self):
+        # Note that this callback is only registered on the IOStream
+        # when we have finished reading the request and are waiting for
+        # the application to produce its response.
+        if self._close_callback is not None:
+            callback = self._close_callback
+            self._close_callback = None
+            callback()
+        if not self._finish_future.done():
+            future_set_result_unless_cancelled(self._finish_future, None)
+        self._clear_callbacks()
+    
+    
+class TCPServerTest(AsyncTestCase):
+    @gen_test
+    def test_handle_stream_coroutine_logging(self):
+        # handle_stream may be a coroutine and any exception in its
+        # Future will be logged.
+        class TestServer(TCPServer):
+            @gen.coroutine
+            def handle_stream(self, stream, address):
+                yield stream.read_bytes(len(b'hello'))
+                stream.close()
+                1 / 0
