e252f4aeb3999bd6b5e745bd877cc00f14ce08ec
SUNsung <weatherius@gmail.com>
2018-06-02 13:07:29 +0300

    Rewrite [29cB..NWEb2A29TpgSCO6oTs29RGl9e9esdBY29yVx.rb] File in: 2018-06-02 13:07:28

--- ruby/29cB..NWEb2A29TpgSCO6oTs29RGl9e9esdBY29yVx.rb
+++ ruby/29cB..NWEb2A29TpgSCO6oTs29RGl9e9esdBY29yVx.rb
@@ -1,19 +1,16 @@
 
         
-        def kikaku(a)
-  a.collect {|x| x - a[0]}
-end
-def ud(a)
-  kikaku(a.collect {|x| ((x+NP)%ROW)-ROW*((x+NP)/ROW) }.sort)
-end
-def rl(a)
-  kikaku(a.collect {|x| ROW*((x+NP)/ROW)+ROW-((x+NP)%ROW)}.sort)
-end
-def xy(a)
-  kikaku(a.collect {|x| ROW*((x+NP)%ROW) + (x+NP)/ROW }.sort)
-end
+            valid_oauth_providers :evernote
     
-            attr_writer :log_levels
+      included do
+    helper SortableTableHelper
+  end
+    
+      private
+    
+          @account.reset_avatar!
+      @account.reset_header!
+      @account.save!
     
       module Sass::Plugin::Configuration
     # Different default options in a m environment.
@@ -27,23 +24,3 @@ end
           root = Merb.root.to_s
           env  = Merb.environment
         end
-    
-            Readline::HISTORY << text
-        parse_input(environment, text)
-      end
-    end
-    
-        # Converts a script node into a corresponding string interpolation
-    # expression.
-    #
-    # @param node_or_interp [Sass::Script::Tree::Node]
-    # @return [Sass::Script::Tree::StringInterpolation]
-    def to_string_interpolation(node_or_interp)
-      unless node_or_interp.is_a?(Interpolation)
-        node = node_or_interp
-        return string_literal(node.value.to_s) if node.is_a?(Literal)
-        if node.is_a?(StringInterpolation)
-          return concat(string_literal(node.quote), concat(node, string_literal(node.quote)))
-        end
-        return StringInterpolation.new(string_literal(''), node, string_literal(''))
-      end
