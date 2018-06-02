34d666f33b7ac7922c3b04498f80a8f59f06efad
SUNsung <weatherius@gmail.com>
2018-06-02 16:30:39 +0300

    Rewrite [20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp] File in: 2018-06-02 16:30:38

--- cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
+++ cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
@@ -1,198 +1,41 @@
 
         
-            D3D_DRIVER_TYPE driverTypes[] =
-    {
-        D3D_DRIVER_TYPE_HARDWARE,
-        D3D_DRIVER_TYPE_WARP,
-        D3D_DRIVER_TYPE_REFERENCE,
-    };
-    UINT numDriverTypes = ARRAYSIZE(driverTypes);
-    
-        // A\B
-    cv::solve(H, A, A, cv::DECOMP_NORMAL);
-    H.release();
-    
-    void CV_ModelEstimator2_Test::fill_array( int test_case_idx, int i, int j, Mat& arr )
+        int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPTSTR    lpCmdLine,
+	_In_ int       nCmdShow)
 {
-    if( i != INPUT )
-    {
-        cvtest::ArrayTest::fill_array( test_case_idx, i, j, arr );
-        return;
-    }
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(nCmdShow);
     }
     
-                template<typename T>
-            static __device__ __forceinline__ T atomicMin(T* address, T val)
-            {
-#if defined (__CUDA_ARCH__) && (__CUDA_ARCH__ < 120)
-                T count = ::min(*address, val);
-                do
-                {
-                    *address = count;
-                } while (*address > count);
-    }
+    #ifndef BASEEVENT_INTERFACE_BASEEVENT_H_
+#define BASEEVENT_INTERFACE_BASEEVENT_H_
     
-    #endif /* OPENCV_CUDA_WARP_REDUCE_HPP__ */
-
+    #ifdef ANDROID
     
-    CV_EXPORTS_W void setErrorVerbosity(bool verbose);
-    
-    #undef cv_hal_gemm32f
-#define cv_hal_gemm32f lapack_gemm32f
-#undef cv_hal_gemm64f
-#define cv_hal_gemm64f lapack_gemm64f
-#undef cv_hal_gemm32fc
-#define cv_hal_gemm32fc lapack_gemm32fc
-#undef cv_hal_gemm64fc
-#define cv_hal_gemm64fc lapack_gemm64fc
-    
-    inline int64_t factorial(int n) {
-  int64_t a = 1;
-  for (int64_t i = 1; i <= n; ++i) { a *= i; }
-  return a;
-}
-    
-    template<typename To, typename From> To checked_convert(From f, const char* name) {
-  if (overflows<To, From>(f)) {
-    std::string msg = 'value cannot be converted to type ';
-    msg += name;
-    msg += ' without overflow: ';
-    msg += std::to_string(f);
-    throw std::domain_error(std::move(msg));
-  }
-  return convert<To, From>(f);
-}
-    
-    using THDGeneratorTypes = std::tuple<THDGenerator>;
-    
-            // decide start column and end column
-        size_t st = numParallelSequences *  rank      / numProcs;
-        size_t en = numParallelSequences * (rank + 1) / numProcs;
-        assert(rank < numProcs);
-        en = en > numParallelSequences ? numParallelSequences : en; // TODO: why are these two tests necessary? We should rather test rank
-        en = (rank + 1 == numProcs) ? numParallelSequences : en;
-        size_t numNewParallelSequence = en - st;
-    
-    #define REGISTER_BINARY_LOGIC_OPERATOR_SCHEMA(OpName)                                                           \
-    REGISTER_OPERATOR_SCHEMA(OpName)                                                                            \
-        .Description('Returns the tensor resulted from performing the ''#OpName'' logical operation'            \
-            'elementwise on the input tensors A and B. If broadcasting is enabled, the right-hand-side'         \
-            'argument will be broadcasted to match the shape of left-hand-side argument. Refer to Add'          \
-            ' for a detailed description of the broadcasting rules.')                                           \
-        .Input('A', 'First operand.', 'T')                                                                      \
-        .Input('B', 'Second operand. With broadcasting can be of smaller size than A. If broadcasting'          \
-            'is disabled, it should be of the same size.', 'T')                                                 \
-        .Output('C', 'Result, has same dimensions and A and type bool.', 'T')                                   \
-        .TypeConstraint('T', { 'tensor(bool)' }, 'Constrain input and output types to bool tensor.')            \
-        .Attr('axis', 'If set, defines the broadcast dimensions.',                                              \
-            AttrType::AttributeProto_AttributeType_INT)                                                         \
-        .Attr('broadcast', 'Pass 1 to enable broadcasting.',                                                    \
-            AttrType::AttributeProto_AttributeType_INT);
-    
-        REGISTER_OPERATOR_SCHEMA(LpNormalization)
-        .Description('Given a matrix, apply Lp-normalization along the provided axis. '
-            'For RS4 default of p = 2 and it will perform L2 normalization. Divide each '
-            'element by the square root of the sum of squares of all elements in the input tensor.')
-        .Input('input', 'Input tensor of any shape', 'T')
-        .Output('output', 'Output tensor of same shape and type as input X.', 'T')
-        .TypeConstraint('T', { 'tensor(float16)', 'tensor(float)', 'tensor(float)' }, 'Constrain input and output '
-            'types to float tensors.')
-        .Attr('axis', 'Axis along which to perform normalization.',
-            AttrType::AttributeProto_AttributeType_INT)
-        .Attr('p', '(int64, default 2) the order of the normalization, only 1 or 2 are supported.',
-            AttrType::AttributeProto_AttributeType_INT);
-    
-        // Compute gradients to input observations, the weights to the observations, and the class log posterior probabilities
-    virtual void BackpropToNonLooping(size_t inputIndex) override
-    {
-        // Left node must be a scalar
-        if (inputIndex == 0)  //left derivative
-        {
-            BackpropToLeft(*m_logSoftmaxOfRight, InputRef(inputIndex).Gradient(), Gradient());
-        }
-        else if (inputIndex == 1)
-        {
-            FrameRange frameRange(InputRef(0).GetMBLayout());
-            BackpropToRight(*m_softmaxOfRight, InputRef(inputIndex).Gradient(), Gradient(), *m_CTCposterior);
-            InputRef(inputIndex).MaskMissingGradientColumnsToZero(frameRange);
-        }
-        else
-           RuntimeError('ForwardBackwardNode criterion expects only two inputs: labels and network output.');
-    }
-    
-    FunctionPtr CreateGRU(const ONNXIR::Node *node, const std::vector<Variable> &inputs, const std::string &direction,
-    const std::vector<string> &activations, const std::vector<float> &activation_alpha, const std::vector<float> &activation_beta);
-    
-        // assignment
-    ssematrix &operator=(const ssematrix &other)
-    {
-        assign(other);
-        return *this;
-    }
-    ssematrix &operator=(const ssematrixbase &other)
-    {
-        assign(other);
-        return *this;
-    }
-    ssematrix &operator=(ssematrix &&other)
-    {
-        delete_sse(this->p);
-        move(other);
-        return *this;
-    }
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
     
     
-    {
-    {        if (flags & CopyNodeFlags::copyNodeInputLinks)
-        {
-            // copy the children structure but use the new nodes generated
-            for (int i = 0; i < fromNode->GetNumInputs(); i++)
-                toNode->SetInput(i, GetNodeFromName(toNamePrefix + fromNode->GetInputs()[i]->NodeName()));
-        }
-    }
-}
+    {  protected:
+    std::string   m_servicename;
+    TServicesMap m_dependservices;
+};
     
-    void GPUDataTransferer::CopyGPUToCPUAsync(void* gpuBuffer, size_t totalSize, void* cpuBuffer)
+    //
+//  testspy.h
+//  PublicComponent
+//
+//  Created by yerungui on 14-5-13.
+//
+    
+    int TSpy::__TestFun1(int i)
 {
-    m_inner->CopyGPUToCPUAsync(gpuBuffer, 1, totalSize, cpuBuffer);
-    m_inner->RecordGPUToCPUCopy();
+    return reinterpret_cast<Test_Spy_Sample*>(This())->__TestFun1(i);
 }
     
-    void ComputeBlockIDCT(const coeff_t* block, uint8_t* out) {
-  coeff_t colidcts[kDCTBlockSize];
-  const int kColScale = 11;
-  const int kColRound = 1 << (kColScale - 1);
-  for (int x = 0; x < 8; ++x) {
-    int colbuf[8] = { 0 };
-    Compute1dIDCT(&block[x], 8, colbuf);
-    for (int y = 0; y < 8; ++y) {
-      colidcts[8 * y + x] = (colbuf[y] + kColRound) >> kColScale;
-    }
-  }
-  const int kRowScale = 18;
-  const int kRowRound = 257 << (kRowScale - 1);  // includes offset by 128
-  for (int y = 0; y < 8; ++y) {
-    const int rowidx = 8 * y;
-    int rowbuf[8] = { 0 };
-    Compute1dIDCT(&colidcts[rowidx], 1, rowbuf);
-    for (int x = 0; x < 8; ++x) {
-      out[rowidx + x] =
-          std::max(0, std::min(255, (rowbuf[x] + kRowRound) >> kRowScale));
-    }
-  }
-}
-    
-    // Fills in 'result' with the inverse DCT of 'block'.
-// The arguments 'block' and 'result' point to 8x8 arrays that are arranged in
-// a row-by-row memory layout.
-void ComputeBlockIDCT(const coeff_t* block, uint8_t* result);
-    
-    // Creates a JPEG from the rgb pixel data. Returns true on success. The given
-// quantization table must have 3 * kDCTBlockSize values.
-bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
-                     const int* quant, JPEGData* jpg);
-    
-    #include <assert.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+    #include 'comm/debugger/spy.inl'
