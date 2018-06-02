279c8626f3da438448c8fdb3429eac9f448e4d51
SUNsung <weatherius@gmail.com>
2018-06-02 09:58:01 +0300

    Rewrite [48Klp8B4DU2CA48fkFSJzQ7FEQ48VAlfh9USLfY48imc.py] File in: 2018-06-02 09:58:01

--- pyton/48Klp8B4DU2CA48fkFSJzQ7FEQ48VAlfh9USLfY48imc.py
+++ pyton/48Klp8B4DU2CA48fkFSJzQ7FEQ48VAlfh9USLfY48imc.py
@@ -1,141 +1,94 @@
 
-if len(sys.argv) <= 1:
-    print('Specify the version number as parameter')
-    sys.exit()
-version = sys.argv[1]
-    
-        def test_no_duplicates(self):
-        ies = gen_extractors()
-        for tc in gettestcases(include_onlymatching=True):
-            url = tc['url']
-            for ie in ies:
-                if type(ie).__name__ in ('GenericIE', tc['name'] + 'IE'):
-                    self.assertTrue(ie.suitable(url), '%s should match URL %r' % (type(ie).__name__, url))
-                else:
-                    self.assertFalse(
-                        ie.suitable(url),
-                        '%s should not match URL %r . That URL belongs to %s.' % (type(ie).__name__, url, tc['name']))
-    
-    
-def list_extractors(age_limit):
-    '''
-    Return a list of extractors that are suitable for the given age,
-    sorted by extractor ID.
-    '''
-    
-        def _real_extract(self, url):
-        playlist_id = self._match_id(url)
-    
-            return {
-            'id': flv_id,
-            'url': rtmp_url,
-            'ext': 'flv',
-            'no_resume': True,
-            'title': title,
-            'description': description,
-            'duration': duration,
-            'view_count': view_count,
-            'comment_count': comment_count,
-            'uploader': uploader,
-            'upload_date': upload_date,
-        }
-
-    
-        theplatform_download_by_pid(pid, title, output_dir=output_dir, merge=merge, info_only=info_only)
-    
-    
-def huaban_download(url, output_dir='.', **kwargs):
-    if re.match(r'http://huaban\.com/boards/\d+/', url):
-        huaban_download_board(url, output_dir, **kwargs)
-    else:
-        print('Only board (画板) pages are supported currently')
-        print('ex: http://huaban.com/boards/12345678/')
-    
-    
-    
-    define('port', default=8888, help='run on the given port', type=int)
-    
-    
-class WSGIContainerTest(AsyncHTTPTestCase):
-    def wsgi_app(self, environ, start_response):
-        status = '200 OK'
-        response_headers = [('Content-Type', 'text/plain')]
-        start_response(status, response_headers)
-        return [b'Hello world!']
-    
-        def removeReader(self, reader):
-        if reader in self._readers:
-            fd = self._readers.pop(reader)
-            (_, writer) = self._fds[fd]
-            if writer:
-                # We have a writer so we need to update the IOLoop for
-                # write events only.
-                self._fds[fd] = (None, writer)
-                self._io_loop.update_handler(fd, IOLoop.WRITE)
+        
+            def check_division(self, x, y):
+        eq = self.assertEqual
+        with self.subTest(x=x, y=y):
+            q, r = divmod(x, y)
+            q2, r2 = x//y, x%y
+            pab, pba = x*y, y*x
+            eq(pab, pba, 'multiplication does not commute')
+            eq(q, q2, 'divmod returns different quotient than /')
+            eq(r, r2, 'divmod returns different mod than %')
+            eq(x, q*y + r, 'x != q*y + r after divmod')
+            if y > 0:
+                self.assertTrue(0 <= r < y, 'bad mod from divmod')
             else:
-                # Since we have no writer registered, we remove the
-                # entry from _fds and unregister the handler from the
-                # IOLoop
-                del self._fds[fd]
-                self._io_loop.remove_handler(fd)
+                self.assertTrue(y < r <= 0, 'bad mod from divmod')
     
-    * If you want to write something like an exception handler that will
-  persist across asynchronous calls, create a new `StackContext` (or
-  `ExceptionStackContext`), and make your asynchronous calls in a ``with``
-  block that references your `StackContext`.
-    
-    
-def try_close(f):
-    # Avoid issue #875 (race condition when using the file in another
-    # thread).
-    for i in range(10):
-        try:
-            f.close()
-        except IOError:
-            # Yield to another thread
-            time.sleep(1e-3)
+            Otherwise leave dialog open for user to correct entry or cancel.
+        '''
+        entry = self.entry_ok()
+        if entry is not None:
+            self.result = entry
+            self.destroy()
         else:
-            break
-    # Try a last time and let raise
-    f.close()
+            # [Ok] moves focus.  (<Return> does not.)  Move it back.
+            self.entry.focus_set()
     
-            # Wait for release().
-        f1 = sem.acquire()
-        self.assertFalse(f1.done())
-        f2 = sem.acquire()
-        sem.release()
-        self.assertTrue(f1.done())
-        self.assertFalse(f2.done())
-        sem.release()
-        self.assertTrue(f2.done())
+        Use this function to calculate the variance from the entire population.
+    To estimate the variance from a sample, the ``variance`` function is
+    usually a better choice.
     
-        test_mode = False
+        def lock(self):
+        '''Lock the mailbox.'''
+        if not self._locked:
+            self._file = open(os.path.join(self._path, '.mh_sequences'), 'rb+')
+            _lock_file(self._file)
+            self._locked = True
     
-    api('reltableitem', RelTableItemJsonTemplate)
-api('bannedtableitem', BannedTableItemJsonTemplate)
-api('mutedtableitem', MutedTableItemJsonTemplate)
-api('invitedmodtableitem', InvitedModTableItemJsonTemplate)
-api('friendtableitem', FriendTableItemJsonTemplate)
+        def test_oldargs0_0_ext(self):
+        {}.keys(*())
     
-    from pylons import request
-from pylons import app_globals as g
-from reddit_base import RedditController
-from r2.lib.pages import AdminPage, AdminAwards
-from r2.lib.pages import AdminAwardGive, AdminAwardWinners
-from r2.lib.validator import *
+    3. Install an instance with custom dispatch method:
     
-        def GET_widget_demo_page(self):
-        return BoringPage(_('reddit widget'),
-                          show_sidebar = False, 
-                          content=WidgetDemoPanel()).render()
+        *************************************************************************
+    
+        def test_cwd(self):
+        p = self.cls.cwd()
+        self._test_cwd(p)
+    
+    T_CV2 = typing.ClassVar[int]
+T_CV3 = typing.ClassVar
+    
+    @dataclasses.dataclass
+class IV:
+    T_IV4 = dataclasses.InitVar
+    iv0: dataclasses.InitVar[int]
+    iv1: dataclasses.InitVar
+    iv2: T_IV2
+    iv3: T_IV3
+    not_iv4: T_IV4  # When using string annotations, this field is not recognized as an InitVar.
 
     
-            To request a new CAPTCHA,
-        use [/api/new_captcha](#POST_api_new_captcha).
-        '''
-        image = captcha.get_image(iden)
-        f = StringIO.StringIO()
-        image.save(f, 'PNG')
-        response.content_type = 'image/png;'
-        return f.getvalue()
+    import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
     
+            Returns the underlying `.IOStream` object and stops all further
+        HTTP processing.  May only be called during
+        `.HTTPMessageDelegate.headers_received`.  Intended for implementing
+        protocols like websockets that tunnel over an HTTP handshake.
+        '''
+        self._clear_callbacks()
+        stream = self.stream
+        self.stream = None
+        if not self._finish_future.done():
+            future_set_result_unless_cancelled(self._finish_future, None)
+        return stream
+    
+    from tornado.auth import (
+    AuthError, OpenIdMixin, OAuthMixin, OAuth2Mixin,
+    GoogleOAuth2Mixin, FacebookGraphMixin, TwitterMixin,
+)
+from tornado.concurrent import Future
+from tornado.escape import json_decode
+from tornado import gen
+from tornado.httputil import url_concat
+from tornado.log import gen_log
+from tornado.testing import AsyncHTTPTestCase, ExpectLog
+from tornado.test.util import ignore_deprecation
+from tornado.web import RequestHandler, Application, asynchronous, HTTPError
+    
+        def start_tree(self, tree, filename):
+        self.found_future_import = False
