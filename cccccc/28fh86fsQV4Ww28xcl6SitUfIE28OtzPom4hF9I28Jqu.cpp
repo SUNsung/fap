72b4f611839ee47418a4a697d90c9f3499ee0b9c
SUNsung <weatherius@gmail.com>
2018-06-03 06:29:17 +0300

    Rewrite [28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp] File in: 2018-06-03 06:29:16

--- cccccc/28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp
+++ cccccc/28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp
@@ -1,227 +1,204 @@
 
         
-        SlowTimer::SlowTimer(int64_t msThreshold, const char *location, const char *info)
-  : m_timer(Timer::WallTime), m_msThreshold(msThreshold) {
-  if (location) m_location = location;
-  if (info) m_info = info;
+        namespace content {
+class RenderView;
 }
     
-      /**
-   * Get results of a task. This is blocking until task is finished or times
-   * out. The status code is set to -1 in the event of a timeout.
-   */
-  static String TaskResult(const Resource& task,
-                           Array &headers,
-                           int &code,
-                           int64_t timeout_ms);
     
-    
-    {    auto& block = dataBlocks.back();
-    block.data.reset(new uint8_t[size]);
-    block.size = size;
-    block.align = alignof(T);
-    return (T*)block.data.get();
-  }
-    
-    
-    {}
-    
-      /*
-   * We use a fast path translation with a hashtable if none of the
-   * cases are numeric strings and if the input is actually a string.
-   *
-   * Otherwise we do a linear search through the cases calling string
-   * conversion routines.
-   */
-  const bool fastPath =
-    topC(env)->isA(TStr) &&
-    std::none_of(iv.strvec(), iv.strvec() + numCases,
-      [&](const StrVecItem& item) {
-        return curUnit(env)->lookupLitstrId(item.str)->isNumeric();
-      }
-    );
-    
-    /*
- * Returns an IR block corresponding to the given bytecode offset. If the block
- * starts with a DefLabel expecting a StkPtr, this function will return an
- * intermediate block that passes the current sp.
- */
-Block* getBlock(IRGS& env, Offset offset);
-    
-    void OfflineCode::disasm(FILE* file,
-                         TCA fileStartAddr,
-                         TCA codeStartAddr,
-                         uint64_t codeLen,
-                         const PerfEventsMap<TCA>& perfEvents,
-                         BCMappingInfo bcMappingInfo,
-                         bool printAddr,
-                         bool printBinary) {
-    }
-    
-    #include 'hphp/runtime/vm/jit/func-prologue.h'
-    
-    ///////////////////////////////////////////////////////////////////////////////
-    
-    static int set_flags(struct ifaddrs* ifaddr) {
-	int fd = socket(AF_INET, SOCK_DGRAM, 0);
-	if (fd == -1) {
-		return -1;
-	}
-	ifreq ifr;
-	memset(&ifr, 0, sizeof(ifr));
-	strncpy(ifr.ifr_name, ifaddr->ifa_name, IFNAMSIZ - 1);
-	int rc = ioctl(fd, SIOCGIFFLAGS, &ifr);
-	close(fd);
-	if (rc == -1) {
-		return -1;
-	}
-	ifaddr->ifa_flags = ifr.ifr_flags;
-	return 0;
-}
-    
-    	ERR_FAIL_COND_V(!connected, ERR_UNCONFIGURED);
-    
-    	static void _bind_methods();
-    
-    #if defined(MBEDTLS_PLATFORM_TIME_TYPE_MACRO) &&\
-    ( !defined(MBEDTLS_PLATFORM_C) ||\
-        !defined(MBEDTLS_HAVE_TIME) )
-#error 'MBEDTLS_PLATFORM_TIME_TYPE_MACRO defined, but not all prerequisites'
-#endif
-    
-    	GDCLASS(VehicleBody, RigidBody);
-    
-    	GodotClosestConvexResultCallback(const btVector3 &convexFromWorld, const btVector3 &convexToWorld, const Set<RID> *p_exclude) :
-			btCollisionWorld::ClosestConvexResultCallback(convexFromWorld, convexToWorld),
-			m_exclude(p_exclude) {}
-    
-    # ifdef __GNUC_PREREQ
-#  if __GNUC_PREREQ(3,4)
-#   include <limits.h>
-/*Note the casts to (int) below: this prevents OC_CLZ{32|64}_OFFS from
-   'upgrading' the type of an entire expression to an (unsigned) size_t.*/
-#   if INT_MAX>=2147483647
-#    define OC_CLZ32_OFFS ((int)sizeof(unsigned)*CHAR_BIT)
-#    define OC_CLZ32(_x) (__builtin_clz(_x))
-#   elif LONG_MAX>=2147483647L
-#    define OC_CLZ32_OFFS ((int)sizeof(unsigned long)*CHAR_BIT)
-#    define OC_CLZ32(_x) (__builtin_clzl(_x))
-#   endif
-#   if INT_MAX>=9223372036854775807LL
-#    define OC_CLZ64_OFFS ((int)sizeof(unsigned)*CHAR_BIT)
-#    define OC_CLZ64(_x) (__builtin_clz(_x))
-#   elif LONG_MAX>=9223372036854775807LL
-#    define OC_CLZ64_OFFS ((int)sizeof(unsigned long)*CHAR_BIT)
-#    define OC_CLZ64(_x) (__builtin_clzl(_x))
-#   elif LLONG_MAX>=9223372036854775807LL|| \
-     __LONG_LONG_MAX__>=9223372036854775807LL
-#    define OC_CLZ64_OFFS ((int)sizeof(unsigned long long)*CHAR_BIT)
-#    define OC_CLZ64(_x) (__builtin_clzll(_x))
-#   endif
-#  endif
-# endif
-    
-      // Get an integral value, 0 or 1, for whether a syscall table pointer is modified.
-  auto f1 = osquery::readFile(kKernelSyscallAddrModifiedPath, content);
-  if (f1.ok()) {
-    boost::trim(content);
-    syscall_addr_modified = content;
-  } else {
-    VLOG(1) << 'Cannot read file: ' << kKernelSyscallAddrModifiedPath;
-    return results;
-  }
-    
-    extern 'C' {
-extern kern_return_t _start(kmod_info_t *ki, void *data);
-extern kern_return_t _stop(kmod_info_t *ki, void *data);
-}
-    
-    /**
- * @brief Iterate the discovered decorators for a given point type.
- *
- * The configuration maintains various sources, each may contain a set of
- * decorators. The source tracking is abstracted for the decorator iterator.
- *
- * @param point request execution of decorators for this given point.
- * @param time an optional time for points using intervals.
- * @param source restrict run to a specific config source.
- */
-void runDecorators(DecorationPoint point,
-                   size_t time = 0,
-                   const std::string& source = '');
-    
-    
-    {
-    { private:
-  friend class TLSConfigTests;
+    {  DISALLOW_COPY_AND_ASSIGN(Clipboard);
 };
-}
-
     
-      auto file_path = kTestWorkingDirectory + 'permissions-file2';
-    
-    namespace osquery {
+    namespace nw {
     }
     
-      struct tm result;
-  gmtime_r((time_t*)&epoch, &result);
+    class ObjectManager;
     
-      // Sync + corrupt => no change
-  ASSERT_OK(writable_file->Fsync());
-  ASSERT_OK(dynamic_cast<MockEnv*>(env_)->CorruptBuffer(kFileName));
-  result.clear();
-  ASSERT_OK(rand_file->Read(0, kGood.size(), &result, &(scratch[0])));
-  ASSERT_EQ(result.compare(kGood), 0);
-    
-    
-    {} // namespace rocksdb
-
-    
-    
-  // Should release the snapshot and be aware of the new stuff now
-  it.reset(db_->NewIterator(ReadOptions()));
-  first = true;
-  for (it->Seek(k1); it->Valid(); it->Next()) {
-    res = it->value().ToString();
-    if (first) {
-      ASSERT_EQ(res, 'v1,v2,v3,v4');
-      first = false;
-    } else {
-      ASSERT_EQ(res, 'a1,a2,a3,a4');
-    }
+    void Menu::Insert(MenuItem* menu_item, int pos) {
+  std::vector<MenuItem*>::iterator begin = menu_items.begin();
+  menu_items.insert(begin+pos,menu_item);
+  if (GTK_IS_ACCEL_GROUP(gtk_accel_group)){
+    menu_item->UpdateKeys(gtk_accel_group);
   }
-    
-    
-    {
-    {  releaseJniEnv(attached_thread);
-}
-// @lint-ignore TXT4 T25377293 Grandfathered in
-}  // namespace rocksdb
-    
-      pos = fstart;
-  while (pos >= 0 && '/' == fn[pos]) --pos;
-    
-    #ifdef NDEBUG
-// empty in release build
-#define TEST_KILL_RANDOM(kill_point, rocksdb_kill_odds)
-#else
-    
-    inline
-void BlockFetcher::InsertCompressedBlockToPersistentCacheIfNeeded() {
-  if (status_.ok() && read_options_.fill_cache &&
-      cache_options_.persistent_cache &&
-      cache_options_.persistent_cache->IsCompressed()) {
-    // insert to raw cache
-    PersistentCacheHelper::InsertRawPage(cache_options_, handle_, used_buf_,
-                                         block_size_ + kBlockTrailerSize);
-  }
+  gtk_menu_shell_insert(GTK_MENU_SHELL(menu_), menu_item->menu_item_, pos);
 }
     
-    class CompactionFilterFactoryJniCallback : public JniCallback, public CompactionFilterFactory {
+    #include 'content/nw/src/api/menu/menu.h'
+    
+    void MenuItem::SetTooltip(const std::string& tooltip) {
+  is_modified_ = true;
+  tooltip_ = base::UTF8ToUTF16(tooltip);
+  if (menu_)
+    menu_->UpdateStates();
+}
+    
+    class NwAppQuitFunction : public UIThreadExtensionFunction {
  public:
-    CompactionFilterFactoryJniCallback(
-        JNIEnv* env, jobject jcompaction_filter_factory);
-    virtual std::unique_ptr<CompactionFilter> CreateCompactionFilter(
-      const CompactionFilter::Context& context);
-    virtual const char* Name() const;
+  NwAppQuitFunction();
     }
+    
+    namespace extensions {
+    }
+    
+        // Finish and check for builder errors
+    if (s.ok()) {
+      s = builder->Finish();
+      if (s.ok()) {
+        meta->file_size = builder->FileSize();
+        assert(meta->file_size > 0);
+      }
+    } else {
+      builder->Abandon();
+    }
+    delete builder;
+    
+    Status DumpFile(Env* env, const std::string& fname, WritableFile* dst) {
+  FileType ftype;
+  if (!GuessType(fname, &ftype)) {
+    return Status::InvalidArgument(fname + ': unknown file type');
+  }
+  switch (ftype) {
+    case kLogFile:         return DumpLog(env, fname, dst);
+    case kDescriptorFile:  return DumpDescriptor(env, fname, dst);
+    case kTableFile:       return DumpTable(env, fname, dst);
+    default:
+      break;
+  }
+  return Status::InvalidArgument(fname + ': not a dump-able file type');
+}
+    
+    // A utility routine: write 'data' to the named file and Sync() it.
+extern Status WriteStringToFileSync(Env* env, const Slice& data,
+                                    const std::string& fname);
+    
+    // Return the name of the descriptor file for the db named by
+// 'dbname' and the specified incarnation number.  The result will be
+// prefixed with 'dbname'.
+extern std::string DescriptorFileName(const std::string& dbname,
+                                      uint64_t number);
+    
+      Cache::Handle* handle = NULL;
+  Status s = FindTable(file_number, file_size, &handle);
+  if (!s.ok()) {
+    return NewErrorIterator(s);
+  }
+    
+      // Choose a location for the test database if none given with --db=<path>
+  if (FLAGS_db == NULL) {
+      leveldb::Env::Default()->GetTestDirectory(&default_db_path);
+      default_db_path += '/dbbench';
+      FLAGS_db = default_db_path.c_str();
+  }
+    
+    #endif  // STORAGE_LEVELDB_INCLUDE_COMPARATOR_H_
+
+    
+    TEST(VersionEditTest, EncodeDecode) {
+  static const uint64_t kBig = 1ull << 50;
+    }
+    
+    #include <stddef.h>
+#include <stdint.h>
+#include <string>
+#include <vector>
+#include 'leveldb/slice.h'
+#include 'util/hash.h'
+    
+    double Histogram::Percentile(double p) const {
+  double threshold = num_ * (p / 100.0);
+  double sum = 0;
+  for (int b = 0; b < kNumBuckets; b++) {
+    sum += buckets_[b];
+    if (sum >= threshold) {
+      // Scale linearly within this bucket
+      double left_point = (b == 0) ? 0 : kBucketLimit[b-1];
+      double right_point = kBucketLimit[b];
+      double left_sum = sum - buckets_[b];
+      double right_sum = sum;
+      double pos = (threshold - left_sum) / (right_sum - left_sum);
+      double r = left_point + (right_point - left_point) * pos;
+      if (r < min_) r = min_;
+      if (r > max_) r = max_;
+      return r;
+    }
+  }
+  return max_;
+}
+    
+    static void Usage() {
+  fprintf(
+      stderr,
+      'Usage: leveldbutil command...\n'
+      '   dump files...         -- dump contents of specified files\n'
+      );
+}
+    
+    // tress
+#include '../src/tree/tree_model.cc'
+#include '../src/tree/tree_updater.cc'
+#include '../src/tree/updater_colmaker.cc'
+#include '../src/tree/updater_fast_hist.cc'
+#include '../src/tree/updater_prune.cc'
+#include '../src/tree/updater_refresh.cc'
+#include '../src/tree/updater_sync.cc'
+#include '../src/tree/updater_histmaker.cc'
+#include '../src/tree/updater_skmaker.cc'
+    
+      delete metric;
+  metric = xgboost::Metric::Create('ndcg@2');
+  ASSERT_STREQ(metric->Name(), 'ndcg@2');
+  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 1, 1e-10);
+  EXPECT_NEAR(GetMetricEval(metric,
+                            {0.1f, 0.9f, 0.1f, 0.9f},
+                            {  0,   0,   1,   1}),
+              0.3868f, 0.001f);
+    
+    
+    {}  // namespace xgboost
+
+    
+      void PredictLeaf(DMatrix *p_fmat,
+                   std::vector<bst_float> *out_preds,
+                   unsigned ntree_limit) override {
+    LOG(FATAL) << 'gblinear does not support prediction of leaf index';
+  }
+    
+    #include <folly/CPortability.h>
+#include <folly/Optional.h>
+    
+    template <class T>
+class UnboundedBlockingQueue : public BlockingQueue<T> {
+ public:
+  virtual ~UnboundedBlockingQueue() {}
+    }
+    
+      ~AtomicCoreCachedSharedPtr() {
+    auto slots = slots_.load(std::memory_order_acquire);
+    // Delete of AtomicCoreCachedSharedPtr must be synchronized, no
+    // need for stlots->retire().
+    if (slots) {
+      delete slots;
+    }
+  }
+    
+      // Nothrow as long as swap() on the Compare type is nothrow.
+  void swap(sorted_vector_set& o) {
+    using std::swap;  // Allow ADL for swap(); fall back to std::swap().
+    Compare& a = m_;
+    Compare& b = o.m_;
+    swap(a, b);
+    m_.cont_.swap(o.m_.cont_);
+  }
+    
+      EXPECT_TRUE((std::is_same<remove_cvref_t<int volatile const&>, int>::value));
+  EXPECT_TRUE(
+      (std::is_same<remove_cvref<int volatile const&>::type, int>::value));
+    
+    #include <folly/futures/Future.h>
+#include <folly/Likely.h>
+#include <folly/SingletonThreadLocal.h>
+#include <folly/futures/ThreadWheelTimekeeper.h>
+    
+      int res = 0;
+  bool ranLoopCallbacks;
+  bool blocking = !(flags & EVLOOP_NONBLOCK);
+  bool once = (flags & EVLOOP_ONCE);
