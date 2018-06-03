a9ddfc2b4aca5c59a94c3b335face93af57b3fc5
SUNsung <weatherius@gmail.com>
2018-06-03 07:10:33 +0300

    Rewrite [400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py] File in: 2018-06-03 07:10:33

--- pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
+++ pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
@@ -1,36 +1,117 @@
 
         
-            # This matches an IPv6 address, but also permits range expressions.
-    #
-    # This expression looks complex, but it really only spells out the various
-    # combinations in which the basic unit of an IPv6 address (0..ffff) can be
-    # written, from :: to 1:2:3:4:5:6:7:8, plus the IPv4-in-IPv6 variants such
-    # as ::ffff:192.0.2.3.
-    #
-    # Note that we can't just use ipaddress.ip_address() because we also have to
-    # accept ranges in place of each component.
-    
-        def on_open_shell(self):
-        try:
-            for cmd in (b'set terminal length 0', b'set terminal width 512'):
-                self._exec_cli_command(cmd)
-            self._exec_cli_command(b'set terminal length %d' % self.terminal_length)
-        except AnsibleConnectionFailure:
-            raise AnsibleConnectionFailure('unable to set terminal parameters')
+        
+@pytest.fixture
+def httpbin_secure(httpbin_secure):
+    return prepare_url(httpbin_secure)
 
     
-            return mapping
+        @classmethod
+    def basic_response_server(cls, **kwargs):
+        return cls.text_response_server(
+            'HTTP/1.1 200 OK\r\n' +
+            'Content-Length: 0\r\n\r\n',
+            **kwargs
+        )
     
-        def test_max_delay(self):
-        strategy = _exponential_backoff(retries=7, delay=1, backoff=2, max_delay=60)
-        result = list(strategy())
-        self.assertEquals(result, [1, 2, 4, 8, 16, 32, 60])
+    elif is_py3:
+    from urllib.parse import urlparse, urlunparse, urljoin, urlsplit, urlencode, quote, unquote, quote_plus, unquote_plus, urldefrag
+    from urllib.request import parse_http_list, getproxies, proxy_bypass, proxy_bypass_environment, getproxies_environment
+    from http import cookiejar as cookielib
+    from http.cookies import Morsel
+    from io import StringIO
+    from collections import OrderedDict
     
-    import homeassistant.config as config_util
+    import pytest
     
-        if unit_1 == LENGTH_MILES:
-        meters = __miles_to_meters(value)
-    elif unit_1 == LENGTH_FEET:
-        meters = __feet_to_meters(value)
-    elif unit_1 == LENGTH_KILOMETERS:
-        meters = __kilometers_to_meters(value)
+    from . import utils
+from . import packages
+from .models import Request, Response, PreparedRequest
+from .api import request, get, head, post, patch, put, delete, options
+from .sessions import session, Session
+from .status_codes import codes
+from .exceptions import (
+    RequestException, Timeout, URLRequired,
+    TooManyRedirects, HTTPError, ConnectionError,
+    FileModeWarning, ConnectTimeout, ReadTimeout
+)
+    
+        def __init__(self, *args, **kwargs):
+        '''Initialize RequestException with `request` and `response` objects.'''
+        response = kwargs.pop('response', None)
+        self.response = response
+        self.request = kwargs.pop('request', None)
+        if (response is not None and not self.request and
+                hasattr(response, 'request')):
+            self.request = self.response.request
+        super(RequestException, self).__init__(*args, **kwargs)
+    
+        auth = requests.auth.HTTPDigestAuth('user', 'pass')
+    
+    # If true, do not generate a @detailmenu in the 'Top' node's menu.
+#texinfo_no_detailmenu = False
+    
+        @mock.patch('certbot.display.enhancements.util')
+    def test_redirect(self, mock_util):
+        mock_util().menu.return_value = (display_util.OK, 1)
+        self.assertTrue(self._call('redirect'))
+    
+        description = 'Example Installer plugin'
+    
+        >>> _sum([3, 2.25, 4.5, -0.5, 1.0], 0.75)
+    (<class 'float'>, Fraction(11, 1), 5)
+    
+    #
+# NOTE: This is the pi function from the decimal documentation, modified
+# for benchmarking purposes. Since floats do not have a context, the higher
+# intermediate precision from the original is NOT used, so the modified
+# algorithm only gives an approximation to the correctly rounded result.
+# For serious use, refer to the documentation or the appropriate literature.
+#
+def pi_float():
+    '''native float'''
+    lasts, t, s, n, na, d, da = 0, 3.0, 3, 1, 0, 0, 24
+    while s != lasts:
+        lasts = s
+        n, na = n+na, na+8
+        d, da = d+da, da+32
+        t = (t * n) / d
+        s += t
+    return s
+    
+    import re
+    
+        def _pre_message_hook(self, f):
+        '''Called before writing each message to file f.'''
+        f.write(b'\001\001\001\001' + linesep)
+    
+    
+def makemakefile(outfp, vars, files, target):
+    save = sys.stdout
+    try:
+        sys.stdout = outfp
+        realwork(vars, files, target)
+    finally:
+        sys.stdout = save
+    
+        @support.skip_unless_symlink
+    def test_iterdir_symlink(self):
+        # __iter__ on a symlink to a directory
+        P = self.cls
+        p = P(BASE, 'linkB')
+        paths = set(p.iterdir())
+        expected = { P(BASE, 'linkB', q) for q in ['fileB', 'linkD'] }
+        self.assertEqual(paths, expected)
+    
+        def setdelegate(self, delegate):
+        if self.delegate is not None:
+            self.unbind('<<toggle-auto-coloring>>')
+        Delegator.setdelegate(self, delegate)
+        if delegate is not None:
+            self.config_colors()
+            self.bind('<<toggle-auto-coloring>>', self.toggle_colorize_event)
+            self.notify_range('1.0', 'end')
+        else:
+            # No delegate - stop any colorizing
+            self.stop_colorizing = True
+            self.allow_colorizing = False
