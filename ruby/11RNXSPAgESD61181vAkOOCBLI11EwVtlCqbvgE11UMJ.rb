031307cf8e7399ca808490c4399f97efa17029f5
SUNsung <weatherius@gmail.com>
2018-06-03 20:49:57 +0300

    Rewrite [11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb] File in: 2018-06-03 20:49:56

--- ruby/11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb
+++ ruby/11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb
@@ -1,92 +1,54 @@
 
         
-          def evernote_oauth_token
-    service && service.token
-  end
-end
-
+                  def add_channel(channel, on_success)
+            @subscription_lock.synchronize do
+              ensure_listener_running
+              @subscribe_callbacks[channel] << on_success
+              when_connected { send_command('subscribe', channel) }
+            end
+          end
     
-      def show
-    respond_to do |format|
-      format.html
-      format.json { render json: @event }
-    end
-  end
-    
-    
-    {      delta = 1
-      stat = File.stat(path)
-      assert_in_delta tb,   stat.birthtime.to_f, delta
-      assert_in_delta t0+2, stat.mtime.to_f, delta
-      if stat.birthtime != stat.ctime
-        assert_in_delta t0+4, stat.ctime.to_f, delta
-      end
-      if /mswin|mingw/ !~ RUBY_PLATFORM && !Bug::File::Fs.noatime?(path)
-        # Windows delays updating atime
-        assert_in_delta t0+6, stat.atime.to_f, delta
-      end
-    }
-  rescue NotImplementedError
-  end
-    
-      def test_select!
-    set = Set.new(1..10)
-    ret = set.select! { |i| i <= 10 }
-    assert_equal(nil, ret)
-    assert_equal(Set.new(1..10), set)
-    
-        def alive?
-      @alive
+          debug { event.payload[:mail] }
     end
     
-      # This case occurred in JRuby where native threads are used to provide
-  # the same behavior as MRI green threads. Key to this issue was the fact
-  # that the thread which called #exit in its block was also being explicitly
-  # sent #join from outside the thread. The 100.times provides a certain
-  # probability that the deadlock will occur. It was sufficient to reliably
-  # reproduce the deadlock in JRuby.
-  it 'does not deadlock when called from within the thread while being joined from without' do
-    100.times do
-      t = Thread.new { Thread.stop; Thread.current.send(@method) }
-      Thread.pass while t.status and t.status != 'sleep'
-      t.wakeup.should == t
-      t.join.should == t
+          included do
+        setup :clear_test_deliveries
+        teardown :clear_test_deliveries
+      end
+    
+      def test_deliver_is_notified
+    BaseMailer.welcome.deliver_now
+    wait
+    
+            expect(result).to eq('hg parent --template {rev}')
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('hg parent --template {rev}')
+      end
     end
   end
 end
 
     
-      it 'interprets pre-Gregorian reform dates using Gregorian proleptic calendar' do
-    Time.send(@method, 1582, 10, 4, 12).to_i.should == -12220200000 # 2299150j
-  end
+    class Devise::SessionsController < DeviseController
+  prepend_before_action :require_no_authentication, only: [:new, :create]
+  prepend_before_action :allow_params_authentication!, only: :create
+  prepend_before_action :verify_signed_out_user, only: :destroy
+  prepend_before_action(only: [:create, :destroy]) { request.env['devise.skip_timeout'] = true }
     
-      def remote_url
-    object.remote_url.presence
-  end
+    if defined?(ActionMailer)
+  class Devise::Mailer < Devise.parent_mailer.constantize
+    include Devise::Mailers::Helpers
     
-        existence_maps = grouped_codes.map { |c| c.map { |cc| [cc, File.exist?(Rails.root.join('public', 'emoji', codepoints_to_filename(cc) + '.svg'))] }.to_h }
-    map = {}
+    # Each time a record is set we check whether its session has already timed out
+# or not, based on last request time. If so, the record is logged out and
+# redirected to the sign in page. Also, each time the request comes and the
+# record is set, we set the last request time inside its scoped session to
+# verify timeout in the following request.
+Warden::Manager.after_set_user do |record, warden, options|
+  scope = options[:scope]
+  env   = warden.request.env
     
-    all_environments = [
-    :development,
-    :production,
-    :test
-]
+          html_filters.push 'd3/clean_html', 'd3/entries_v4'
     
-      # Extract the form
-  res = '<form'
-  form.attributes.each do |attr|
-    res << ' #{attr[0]}='#{attr[1].gsub(''', '')}''
-  end
-  res << '> '
-    
-            # Remove it form the session objects so freeup
-        sessions.delete(s[:session])
-    
-    sock = TCPSocket.new(ip, port)
-    
-    	SIZE1 = 28
-	SIZE2 = 28 + 4 + 32
-	SIZE3 = 28 + 4 + 32 + 4
-    
-        private
+        def root?
+      path == 'index'
+    end
