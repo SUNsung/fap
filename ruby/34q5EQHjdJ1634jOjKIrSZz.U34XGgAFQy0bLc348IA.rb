313714cd315ebe9be50bf8872cef09225dfa9549
SUNsung <weatherius@gmail.com>
2018-06-04 00:34:53 +0300

    Rewrite [34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb] File in: 2018-06-04 00:34:52

--- ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
+++ ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
@@ -1,185 +1,35 @@
 
         
-          attr_accessor :output_buffer
-  attr_reader :request
+        def bottle_resolve_formula_names(bottle_file)
+  receipt_file_path = bottle_receipt_path bottle_file
+  receipt_file = Utils.popen_read('tar', '-xOzf', bottle_file, receipt_file_path)
+  name = receipt_file_path.split('/').first
+  tap = Tab.from_file_content(receipt_file, '#{bottle_file}/#{receipt_file_path}').tap
     
-          # The instance of ActionView::Base that is used by +render+.
-      def view
-        @view ||= begin
-          view = @controller.view_context
-          view.singleton_class.include(_helpers)
-          view.extend(Locals)
-          view.rendered_views = rendered_views
-          view.output_buffer = output_buffer
-          view
-        end
+          def installed_gem_version(gem_name)
+        Gem.loaded_specs[gem_name].version
       end
-    
-        # Use the logger configured for ActionMailer::Base.
-    def logger
-      ActionMailer::Base.logger
-    end
-  end
-end
-    
-    module ActionMailer
-  # Provides the option to parameterize mailers in order to share instance variable
-  # setup, processing, and common headers.
-  #
-  # Consider this example that does not use parameterization:
-  #
-  #   class InvitationsMailer < ApplicationMailer
-  #     def account_invitation(inviter, invitee)
-  #       @account = inviter.account
-  #       @inviter = inviter
-  #       @invitee = invitee
-  #
-  #       subject = '#{@inviter.name} invited you to their Basecamp (#{@account.name})'
-  #
-  #       mail \
-  #         subject:   subject,
-  #         to:        invitee.email_address,
-  #         from:      common_address(inviter),
-  #         reply_to:  inviter.email_address_with_name
-  #     end
-  #
-  #     def project_invitation(project, inviter, invitee)
-  #       @account = inviter.account
-  #       @project = project
-  #       @inviter = inviter
-  #       @invitee = invitee
-  #       @summarizer = ProjectInvitationSummarizer.new(@project.bucket)
-  #
-  #       subject = '#{@inviter.name.familiar} added you to a project in Basecamp (#{@account.name})'
-  #
-  #       mail \
-  #         subject:   subject,
-  #         to:        invitee.email_address,
-  #         from:      common_address(inviter),
-  #         reply_to:  inviter.email_address_with_name
-  #     end
-  #
-  #     def bulk_project_invitation(projects, inviter, invitee)
-  #       @account  = inviter.account
-  #       @projects = projects.sort_by(&:name)
-  #       @inviter  = inviter
-  #       @invitee  = invitee
-  #
-  #       subject = '#{@inviter.name.familiar} added you to some new stuff in Basecamp (#{@account.name})'
-  #
-  #       mail \
-  #         subject:   subject,
-  #         to:        invitee.email_address,
-  #         from:      common_address(inviter),
-  #         reply_to:  inviter.email_address_with_name
-  #     end
-  #   end
-  #
-  #   InvitationsMailer.account_invitation(person_a, person_b).deliver_later
-  #
-  # Using parameterized mailers, this can be rewritten as:
-  #
-  #   class InvitationsMailer < ApplicationMailer
-  #     before_action { @inviter, @invitee = params[:inviter], params[:invitee] }
-  #     before_action { @account = params[:inviter].account }
-  #
-  #     default to:       -> { @invitee.email_address },
-  #             from:     -> { common_address(@inviter) },
-  #             reply_to: -> { @inviter.email_address_with_name }
-  #
-  #     def account_invitation
-  #       mail subject: '#{@inviter.name} invited you to their Basecamp (#{@account.name})'
-  #     end
-  #
-  #     def project_invitation
-  #       @project    = params[:project]
-  #       @summarizer = ProjectInvitationSummarizer.new(@project.bucket)
-  #
-  #       mail subject: '#{@inviter.name.familiar} added you to a project in Basecamp (#{@account.name})'
-  #     end
-  #
-  #     def bulk_project_invitation
-  #       @projects = params[:projects].sort_by(&:name)
-  #
-  #       mail subject: '#{@inviter.name.familiar} added you to some new stuff in Basecamp (#{@account.name})'
-  #     end
-  #   end
-  #
-  #   InvitationsMailer.with(inviter: person_a, invitee: person_b).account_invitation.deliver_later
-  module Parameterized
-    extend ActiveSupport::Concern
     
           private
     
-        assert_equal(2, @logger.logged(:debug).size)
-    assert_match(/BaseMailer#welcome: processed outbound mail in [\d.]+ms/, @logger.logged(:debug).first)
-    assert_match(/Welcome/, @logger.logged(:debug).second)
-  ensure
-    BaseMailer.deliveries.clear
-  end
+      # Implemented by subclasses to hook into Capistrano's deployment flow using
+  # using the `before` and `after` DSL methods. Note that `register_hooks` will
+  # not be called if the user has opted-out of hooks when installing the plugin.
+  #
+  # Example:
+  #
+  #   def register_hooks
+  #     after 'deploy:updated', 'my_plugin:do_something'
+  #   end
+  #
+  def register_hooks; end
     
-        def mail_with_defaults(&block)
-      mail(to: 'test@localhost', from: 'tester@example.com',
-            subject: 'using helpers', &block)
+        def self.clear
+      instance.clear
     end
-end
     
-      def contents
-    data = ''
-    path.open('rb') do |f|
-      loop do
-        line = f.gets
-        break if line.nil? || line =~ /^__END__$/
-      end
-      while line = f.gets
-        data << line
-      end
-    end
-    data
-  end
-end
-    
-    class MysqlRequirement < Requirement
-  fatal true
-  satisfy do
-    odisabled('MysqlRequirement', ''depends_on \'mysql\''')
-  end
-end
-    
-        def yielder
-      if instance_variable_defined?(:@satisfied)
-        @satisfied
-      elsif @options[:build_env]
-        require 'extend/ENV'
-        ENV.with_build_environment { yield @proc }
-      else
-        yield @proc
-      end
-    end
-  end
-    
-    module Jekyll
-    
-        def render(context)
-      file_dir = (context.registers[:site].source || 'source')
-      file_path = Pathname.new(file_dir).expand_path
-      file = file_path + @file
-    
-      test 'defined job that uses a :path where none is explicitly set' do
-    Whenever.stubs(:path).returns('/my/path')
-    
-      should 'call run w/ all role args for servers w/ >1 role' do
-    @capistrano.stubs(:role_names_for_host).with(@mock_server1).returns([:role1, :role3])
-    @capistrano.stubs(:whenever_servers).returns([@mock_server1])
-    roles = [:role1, :role2, :role3]
-    @capistrano.stubs(:whenever_options).returns({:roles => roles})
-    
-      test 'various days at a various aligned times using a rake task' do
-    output = Whenever.cron \
-    <<-file
-      set :job_template, nil
-      set :path, '/your/path'
-      every 'mon,wed,fri', :at => '5:02am, 3:02pm' do
-        rake 'blah:blah'
-      end
-    file
+            def matches? subject
+          @subject = subject
+          @subject = subject.new if subject.class == Class
+          error_when_not_valid? && no_error_when_valid?
+        end
