da524961a54f9706737360b2cc84178da1be586e
SUNsung <weatherius@gmail.com>
2018-06-04 09:35:25 +0300

    Rewrite [25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py] File in: 2018-06-04 09:35:24

--- pyton/25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py
+++ pyton/25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py
@@ -1,112 +1,56 @@
 
         
-                def get_auth(self, username=None, password=None):
-            assert self.raw_auth is None
-            assert username is None
-            assert password is None
-            return basic_auth()
+        containers = (('thefuck/python3-tcsh',
+               u'''FROM python:3
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'),
+              ('thefuck/python2-tcsh',
+               u'''FROM python:2
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'))
     
     
-def get_info(package_name):
-    api_url = 'https://pypi.python.org/pypi/{}/json'.format(package_name)
-    resp = requests.get(api_url).json()
-    hasher = hashlib.sha256()
-    for release in resp['urls']:
-        download_url = release['url']
-        if download_url.endswith('.tar.gz'):
-            hasher.update(requests.get(download_url).content)
-            return {
-                'name': package_name,
-                'url': download_url,
-                'sha256': hasher.hexdigest(),
-            }
-    else:
-        raise RuntimeError(
-            '{}: download not found: {}'.format(package_name, resp))
+class TestCaseInsensitiveDict:
     
-            status_line = 'HTTP/{version} {status} {reason}'.format(
-            version=version,
-            status=original.status,
-            reason=original.reason
-        )
-        headers = [status_line]
+        expires = None
+    if morsel['max-age']:
         try:
-            # `original.msg` is a `http.client.HTTPMessage` on Python 3
-            # `_headers` is a 2-tuple
-            headers.extend(
-                '%s: %s' % header for header in original.msg._headers)
-        except AttributeError:
-            # and a `httplib.HTTPMessage` on Python 2.x
-            # `headers` is a list of `name: val<CRLF>`.
-            headers.extend(h.strip() for h in original.msg.headers)
+            expires = int(time.time() + int(morsel['max-age']))
+        except ValueError:
+            raise TypeError('max-age: %s must be integer' % morsel['max-age'])
+    elif morsel['expires']:
+        time_template = '%a, %d-%b-%Y %H:%M:%S GMT'
+        expires = calendar.timegm(
+            time.strptime(morsel['expires'], time_template)
+        )
+    return create_cookie(
+        comment=morsel['comment'],
+        comment_url=bool(morsel['comment']),
+        discard=False,
+        domain=morsel['domain'],
+        expires=expires,
+        name=morsel.key,
+        path=morsel['path'],
+        port=None,
+        rest={'HttpOnly': morsel['httponly']},
+        rfc2109=False,
+        secure=bool(morsel['secure']),
+        value=morsel.value,
+        version=morsel['version'] or 0,
+    )
     
-        def __init__(self, msg, with_headers=True, with_body=True,
-                 on_body_chunk_downloaded=None):
-        '''
-        :param msg: a :class:`models.HTTPMessage` subclass
-        :param with_headers: if `True`, headers will be included
-        :param with_body: if `True`, body will be included
+        def doc(code):
+        names = ', '.join('``%s``' % n for n in _codes[code])
+        return '* %d: %s' % (code, names)
     
+            netrc_path = None
     
-def test_max_redirects(httpbin):
-    r = http('--max-redirects=1', '--follow', httpbin.url + '/redirect/3',
-             error_exit_ok=True)
-    assert r.exit_status == ExitStatus.ERROR_TOO_MANY_REDIRECTS
-
+        def test_str(self):
+        self.assertEqual('Invalid nonce ('xxx'): error', str(self.error))
     
-    
-def test_unicode_form_item(httpbin):
-    r = http('--form', 'POST', httpbin.url + '/post', u'test=%s' % UNICODE)
-    assert HTTP_OK in r
-    assert r.json['form'] == {'test': UNICODE}
-    
-    Invocation flow:
-    
-        '''
-    # The value that should be passed to --auth-type
-    # to use this auth plugin. Eg. 'my-auth'
-    auth_type = None
-    
-        code_block_index = 0
-    last_header = ''
-    linenum = 0
-    with io.open(args.sourcefile, 'r') as read_filehandle:
-        with io.open(args.targetfile, 'w') as text_filehandle:
-            for line in read_filehandle:
-                linenum += 1
-                indent_depth = is_code(line)
-                if indent_depth:
-                    (line, linenum) = process_code(read_filehandle,
-                                                    text_filehandle,
-                                                    line, linenum,
-                                                    args.sourcefile, args.codedir,
-                                                    last_header, code_block_index,
-                                                    indent_depth)
-                    code_block_index += 1
-                # reach here either line was not code, or was code
-                # and we dealt with n code lines
-                if indent_depth < 4 or not is_code(line, indent_depth):
-                    # store header id for codeblock
-                    section_id = get_marker(line)
-                    if section_id is not None:
-                        code_block_index = 0
-                        last_header = section_id
-                    sline = stripped(line)
-                    text_filehandle.write(sline)
-    
-    def is_api(subtype = ''):
-    return c.render_style and c.render_style.startswith(api_type(subtype))
-    
-            return user.name in self.stacked_proxy_safe_get(g, 'admins', [])
-    
-    
-class LimitUploadSize(object):
-    '''
-    Middleware for restricting the size of uploaded files (such as
-    image files for the CSS editing capability).
-    '''
-    def __init__(self, app, max_size=1024*500):
-        self.app = app
-        self.max_size = max_size
-    
-    tpm = tp_manager.tp_manager()
+        stream_types = [
+        {'id': '720p HD'},
+        {'id': '360p SD'},
+    ]
