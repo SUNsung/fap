afbeb1330866eda7e379a0300cf523600ee6cbc8
SUNsung <weatherius@gmail.com>
2018-06-04 11:34:03 +0300

    Rewrite [34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb] File in: 2018-06-04 11:34:02

--- ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
+++ ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
@@ -1,35 +1,11 @@
 
         
-        def bottle_resolve_formula_names(bottle_file)
-  receipt_file_path = bottle_receipt_path bottle_file
-  receipt_file = Utils.popen_read('tar', '-xOzf', bottle_file, receipt_file_path)
-  name = receipt_file_path.split('/').first
-  tap = Tab.from_file_content(receipt_file, '#{bottle_file}/#{receipt_file_path}').tap
+        module VagrantPlugins
+  module CommandBox
+    module Command
+      class Remove < Vagrant.plugin('2', :command)
+        def execute
+          options = {}
+          options[:force] = false
     
-          def installed_gem_version(gem_name)
-        Gem.loaded_specs[gem_name].version
-      end
-    
-          private
-    
-      # Implemented by subclasses to hook into Capistrano's deployment flow using
-  # using the `before` and `after` DSL methods. Note that `register_hooks` will
-  # not be called if the user has opted-out of hooks when installing the plugin.
-  #
-  # Example:
-  #
-  #   def register_hooks
-  #     after 'deploy:updated', 'my_plugin:do_something'
-  #   end
-  #
-  def register_hooks; end
-    
-        def self.clear
-      instance.clear
-    end
-    
-            def matches? subject
-          @subject = subject
-          @subject = subject.new if subject.class == Class
-          error_when_not_valid? && no_error_when_valid?
-        end
+      def_delegators :@logger, :log, :log_status, :log_processing, :log_transform, :log_file_info, :log_processed, :log_http_get_file, :log_http_get_files, :silence_log
