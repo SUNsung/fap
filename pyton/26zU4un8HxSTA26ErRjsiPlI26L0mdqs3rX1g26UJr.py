5c918eba21f23cdf54df071fdd465564c64423dd
SUNsung <weatherius@gmail.com>
2018-06-04 05:52:09 +0300

    Rewrite [26zU4un8HxSTA26ErRjsiPlI26L0mdqs3rX1g26UJr.py] File in: 2018-06-04 05:52:09

--- pyton/26zU4un8HxSTA26ErRjsiPlI26L0mdqs3rX1g26UJr.py
+++ pyton/26zU4un8HxSTA26ErRjsiPlI26L0mdqs3rX1g26UJr.py
@@ -1,34 +1,80 @@
-    @property
-    def is_eager_to_contribute(self):
-        return True
+
+        
+        from jinja2 import Markup
+from werkzeug.http import http_date, parse_date
     
     
-class AdapterTest(unittest.TestCase):
+def test_tag_interface():
+    t = JSONTag(None)
+    pytest.raises(NotImplementedError, t.check, None)
+    pytest.raises(NotImplementedError, t.to_json, None)
+    pytest.raises(NotImplementedError, t.to_python, None)
     
-        def __init__(self, name):
-        self.name = name
+        def implements_to_string(cls):
+        cls.__unicode__ = cls.__str__
+        cls.__str__ = lambda x: x.__unicode__().encode('utf-8')
+        return cls
     
-        def update(self):
-        for msg in self.msg_queue:
-            for sub in self.subscribers.get(msg, []):
-                sub.run(msg)
-        self.msg_queue = []
+    signals_available = False
+try:
+    from blinker import Namespace
+    signals_available = True
+except ImportError:
+    class Namespace(object):
+        def signal(self, name, doc=None):
+            return _FakeSignal(name, doc)
     
-        def test_display_current_time_at_current_time(self):
-        '''
-        Just as justification for working example with the time provider used in
-        production. (Will always pass.)
-        '''
-        production_code_time_provider = ProductionCodeTimeProvider()
-        class_under_test = TimeDisplay()
-        class_under_test.set_time_provider(production_code_time_provider)
-        current_time = datetime.datetime.now()
-        expected_time = '<span class=\'tinyBoldText\'>{}:{}</span>'.format(current_time.hour, current_time.minute)
-        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
+    
+def test_uninstalled_module_paths(modules_tmpdir, purge_module):
+    app = modules_tmpdir.join('config_module_app.py').write(
+        'import os\n'
+        'import flask\n'
+        'here = os.path.abspath(os.path.dirname(__file__))\n'
+        'app = flask.Flask(__name__)\n'
+    )
+    purge_module('config_module_app')
+    
+    
+if __name__ == '__main__':
+    HitCounts.run()
 
     
-    class TimeDisplay(object):
+        def remove_from_tail(self):
+        pass
     
-        def is_satisfied_by(self, candidate):
-        return bool(self._one.is_satisfied_by(candidate) and
-                    self._other.is_satisfied_by(candidate))
+            add_accept_handler(listener, accept_callback)
+        self.client_stream = IOStream(socket.socket())
+        self.addCleanup(self.client_stream.close)
+        yield [self.client_stream.connect(('127.0.0.1', port)),
+               event.wait()]
+        self.io_loop.remove_handler(listener)
+        listener.close()
+    
+            yield q.put(2)
+        put = q.put(3)
+        self.assertFalse(put.done())
+        self.assertEqual(1, (yield q.get()))
+        yield put
+        self.assertTrue(q.full())
+    
+            valid_ipv4_list = {'X-Forwarded-For': '127.0.0.1, 4.4.4.4'}
+        self.assertEqual(
+            self.fetch_json('/', headers=valid_ipv4_list)['remote_ip'],
+            '4.4.4.4')
+    
+        @gen_test
+    def test_run_with_stack_context(self):
+        @gen.coroutine
+        def f1():
+            self.assertEqual(self.active_contexts, ['c1'])
+            yield run_with_stack_context(
+                StackContext(functools.partial(self.context, 'c2')),
+                f2)
+            self.assertEqual(self.active_contexts, ['c1'])
+    
+            namespace = exec_test(globals(), locals(), '''
+        class TestServer(TCPServer):
+            async def handle_stream(self, stream, address):
+                stream.write(b'data')
+                stream.close()
+        ''')
