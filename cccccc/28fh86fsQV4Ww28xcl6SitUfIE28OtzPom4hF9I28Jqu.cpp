8147f1a3add928a8760dbdf4c68e078ae32c13c3
SUNsung <weatherius@gmail.com>
2018-06-04 07:47:22 +0300

    Rewrite [28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp] File in: 2018-06-04 07:47:22

--- cccccc/28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp
+++ cccccc/28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp
@@ -1,204 +1,184 @@
 
         
-        namespace content {
-class RenderView;
+        #include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <condition_variable>
+#include <limits>
+#include <memory>
+#include <mutex>
+#include <vector>
+    
+      for (auto s: tensor->sizes())
+    assert(s == 2);
+  for (int i = 0; i < 2; i++)
+    assert(reinterpret_cast<float*>(tensor->data())[i] == 5);
+    
+    #define THC_GENERIC_FILE 'torch/csrc/generic/Storage.h'
+#include <THC/THCGenerateAllTypes.h>
+    
+    #include <cstddef>
+#include <string>
+    
+      EXPECT_THROW({Cursor(iobuf1.get()).pull(buf, 20);},
+               std::out_of_range);
+    
+    TEST_F(OrderingTest, to_ordering) {
+  EXPECT_EQ(ordering::lt, to_ordering(int(ordering::lt)));
+  EXPECT_EQ(ordering::eq, to_ordering(int(ordering::eq)));
+  EXPECT_EQ(ordering::gt, to_ordering(int(ordering::gt)));
+    }
+    
+      size_t size() override {
+    return queue_.size();
+  }
+    
+        Node* next() {
+      return next_;
+    }
+    
+    TEST_F(UtilityTest, to_signed) {
+  {
+    constexpr auto actual = folly::to_signed(int32_t(-12));
+    EXPECT_TRUE(std::is_signed<decltype(actual)>::value);
+    EXPECT_EQ(-12, actual);
+  }
+  {
+    constexpr auto actual = folly::to_signed(uint32_t(-12));
+    EXPECT_TRUE(std::is_signed<decltype(actual)>::value);
+    EXPECT_EQ(-12, actual);
+  }
 }
     
     
-    {  DISALLOW_COPY_AND_ASSIGN(Clipboard);
+    {// Instantiate the most common Future types to save compile time
+template class SemiFuture<Unit>;
+template class SemiFuture<bool>;
+template class SemiFuture<int>;
+template class SemiFuture<int64_t>;
+template class SemiFuture<std::string>;
+template class SemiFuture<double>;
+template class Future<Unit>;
+template class Future<bool>;
+template class Future<int>;
+template class Future<int64_t>;
+template class Future<std::string>;
+template class Future<double>;
+} // namespace folly
+    
+    #pragma once
+    
+    
+    {  std::shared_ptr<ManualExecutor> westExecutor;
+  std::shared_ptr<ManualExecutor> eastExecutor;
+  std::shared_ptr<ManualWaiter> waiter;
+  InlineExecutor inlineExecutor;
+  std::atomic<bool> done;
+  std::thread th;
 };
     
-    namespace nw {
+    
+void DBWrapper::Init(Handle<Object> exports) {
+  Local<FunctionTemplate> tpl = FunctionTemplate::New(New);
+  tpl->SetClassName(String::NewSymbol('DBWrapper'));
+  tpl->InstanceTemplate()->SetInternalFieldCount(8);
+  tpl->PrototypeTemplate()->Set(String::NewSymbol('open'),
+      FunctionTemplate::New(Open)->GetFunction());
+  tpl->PrototypeTemplate()->Set(String::NewSymbol('get'),
+      FunctionTemplate::New(Get)->GetFunction());
+  tpl->PrototypeTemplate()->Set(String::NewSymbol('put'),
+      FunctionTemplate::New(Put)->GetFunction());
+  tpl->PrototypeTemplate()->Set(String::NewSymbol('delete'),
+      FunctionTemplate::New(Delete)->GetFunction());
+  tpl->PrototypeTemplate()->Set(String::NewSymbol('dump'),
+      FunctionTemplate::New(Dump)->GetFunction());
+  tpl->PrototypeTemplate()->Set(String::NewSymbol('createColumnFamily'),
+      FunctionTemplate::New(CreateColumnFamily)->GetFunction());
+  tpl->PrototypeTemplate()->Set(String::NewSymbol('writeBatch'),
+      FunctionTemplate::New(WriteBatch)->GetFunction());
+  tpl->PrototypeTemplate()->Set(String::NewSymbol('compactRange'),
+      FunctionTemplate::New(CompactRange)->GetFunction());
     }
     
-    class ObjectManager;
+      // folder with contents returns relative path to test dir
+  ASSERT_OK(env_->CreateDirIfMissing(test_dir_ + '/niu'));
+  ASSERT_OK(env_->CreateDirIfMissing(test_dir_ + '/you'));
+  ASSERT_OK(env_->CreateDirIfMissing(test_dir_ + '/guo'));
+  ASSERT_OK(env_->GetChildren(test_dir_, &children));
+  ASSERT_OK(env_->GetChildrenFileAttributes(test_dir_, &childAttr));
+  ASSERT_EQ(3U, children.size());
+  ASSERT_EQ(3U, childAttr.size());
+  for (auto each : children) {
+    env_->DeleteDir(test_dir_ + '/' + each);
+  }  // necessary for default POSIX env
     
-    void Menu::Insert(MenuItem* menu_item, int pos) {
-  std::vector<MenuItem*>::iterator begin = menu_items.begin();
-  menu_items.insert(begin+pos,menu_item);
-  if (GTK_IS_ACCEL_GROUP(gtk_accel_group)){
-    menu_item->UpdateKeys(gtk_accel_group);
-  }
-  gtk_menu_shell_insert(GTK_MENU_SHELL(menu_), menu_item->menu_item_, pos);
-}
+    #include 'rocksdb/merge_operator.h'
+#include 'rocksdb/slice.h'
     
-    #include 'content/nw/src/api/menu/menu.h'
-    
-    void MenuItem::SetTooltip(const std::string& tooltip) {
-  is_modified_ = true;
-  tooltip_ = base::UTF8ToUTF16(tooltip);
-  if (menu_)
-    menu_->UpdateStates();
-}
-    
-    class NwAppQuitFunction : public UIThreadExtensionFunction {
- public:
-  NwAppQuitFunction();
-    }
-    
-    namespace extensions {
-    }
-    
-        // Finish and check for builder errors
-    if (s.ok()) {
-      s = builder->Finish();
-      if (s.ok()) {
-        meta->file_size = builder->FileSize();
-        assert(meta->file_size > 0);
-      }
-    } else {
-      builder->Abandon();
-    }
-    delete builder;
-    
-    Status DumpFile(Env* env, const std::string& fname, WritableFile* dst) {
-  FileType ftype;
-  if (!GuessType(fname, &ftype)) {
-    return Status::InvalidArgument(fname + ': unknown file type');
-  }
-  switch (ftype) {
-    case kLogFile:         return DumpLog(env, fname, dst);
-    case kDescriptorFile:  return DumpDescriptor(env, fname, dst);
-    case kTableFile:       return DumpTable(env, fname, dst);
-    default:
-      break;
-  }
-  return Status::InvalidArgument(fname + ': not a dump-able file type');
-}
-    
-    // A utility routine: write 'data' to the named file and Sync() it.
-extern Status WriteStringToFileSync(Env* env, const Slice& data,
-                                    const std::string& fname);
-    
-    // Return the name of the descriptor file for the db named by
-// 'dbname' and the specified incarnation number.  The result will be
-// prefixed with 'dbname'.
-extern std::string DescriptorFileName(const std::string& dbname,
-                                      uint64_t number);
-    
-      Cache::Handle* handle = NULL;
-  Status s = FindTable(file_number, file_size, &handle);
-  if (!s.ok()) {
-    return NewErrorIterator(s);
+      StatisticsJni::StatisticsJni(std::shared_ptr<Statistics> stats,
+      const std::set<uint32_t> ignore_histograms) : StatisticsImpl(stats, false),
+      m_ignore_histograms(ignore_histograms) {
   }
     
-      // Choose a location for the test database if none given with --db=<path>
-  if (FLAGS_db == NULL) {
-      leveldb::Env::Default()->GetTestDirectory(&default_db_path);
-      default_db_path += '/dbbench';
-      FLAGS_db = default_db_path.c_str();
+      if(m_jSliceB != nullptr) {
+    env->DeleteGlobalRef(m_jSliceB);
   }
     
-    #endif  // STORAGE_LEVELDB_INCLUDE_COMPARATOR_H_
+    // Licensed under the MIT License (the 'License'); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+// http://opensource.org/licenses/MIT
+    
+    static const char gs_crash_dump_header[] =
+    '*** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***';
+    
+    #endif /* DUMPCRASHSTACK_H_ */
 
     
-    TEST(VersionEditTest, EncodeDecode) {
-  static const uint64_t kBig = 1ull << 50;
+    
+    {}
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+    class ScopeJEnv {
+  public:
+    ScopeJEnv(JavaVM* jvm, jint _capacity = 16);
+    ~ScopeJEnv();
     }
     
-    #include <stddef.h>
-#include <stdint.h>
-#include <string>
-#include <vector>
-#include 'leveldb/slice.h'
-#include 'util/hash.h'
-    
-    double Histogram::Percentile(double p) const {
-  double threshold = num_ * (p / 100.0);
-  double sum = 0;
-  for (int b = 0; b < kNumBuckets; b++) {
-    sum += buckets_[b];
-    if (sum >= threshold) {
-      // Scale linearly within this bucket
-      double left_point = (b == 0) ? 0 : kBucketLimit[b-1];
-      double right_point = kBucketLimit[b];
-      double left_sum = sum - buckets_[b];
-      double right_sum = sum;
-      double pos = (threshold - left_sum) / (right_sum - left_sum);
-      double r = left_point + (right_point - left_point) * pos;
-      if (r < min_) r = min_;
-      if (r > max_) r = max_;
-      return r;
+        Value(int unit, double value)
+    : unit(unit)
+    , value(value)
+    {
     }
-  }
-  return max_;
+    
+    void Config::setExperimentalFeatureEnabled(int feature, bool enabled)
+{
+    YGConfigSetExperimentalFeatureEnabled(m_config, static_cast<YGExperimentalFeature>(feature), enabled);
 }
     
-    static void Usage() {
-  fprintf(
-      stderr,
-      'Usage: leveldbutil command...\n'
-      '   dump files...         -- dump contents of specified files\n'
-      );
-}
-    
-    // tress
-#include '../src/tree/tree_model.cc'
-#include '../src/tree/tree_updater.cc'
-#include '../src/tree/updater_colmaker.cc'
-#include '../src/tree/updater_fast_hist.cc'
-#include '../src/tree/updater_prune.cc'
-#include '../src/tree/updater_refresh.cc'
-#include '../src/tree/updater_sync.cc'
-#include '../src/tree/updater_histmaker.cc'
-#include '../src/tree/updater_skmaker.cc'
-    
-      delete metric;
-  metric = xgboost::Metric::Create('ndcg@2');
-  ASSERT_STREQ(metric->Name(), 'ndcg@2');
-  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 1, 1e-10);
-  EXPECT_NEAR(GetMetricEval(metric,
-                            {0.1f, 0.9f, 0.1f, 0.9f},
-                            {  0,   0,   1,   1}),
-              0.3868f, 0.001f);
-    
-    
-    {}  // namespace xgboost
-
-    
-      void PredictLeaf(DMatrix *p_fmat,
-                   std::vector<bst_float> *out_preds,
-                   unsigned ntree_limit) override {
-    LOG(FATAL) << 'gblinear does not support prediction of leaf index';
-  }
-    
-    #include <folly/CPortability.h>
-#include <folly/Optional.h>
-    
-    template <class T>
-class UnboundedBlockingQueue : public BlockingQueue<T> {
- public:
-  virtual ~UnboundedBlockingQueue() {}
+    struct Size
+{
+    double width;
+    double height;
     }
     
-      ~AtomicCoreCachedSharedPtr() {
-    auto slots = slots_.load(std::memory_order_acquire);
-    // Delete of AtomicCoreCachedSharedPtr must be synchronized, no
-    // need for stlots->retire().
-    if (slots) {
-      delete slots;
-    }
+        method(getOverflow);
+    method(getDisplay);
+    
+    #ifdef __ANDROID__
+    
+      std::string asFormattedString() const {
+    std::stringstream str;
+    str << 'Function ' << m_functionName << ' in file ' << m_fileName << ':' << m_lineNumber;
+    return str.str();
   }
     
-      // Nothrow as long as swap() on the Compare type is nothrow.
-  void swap(sorted_vector_set& o) {
-    using std::swap;  // Allow ADL for swap(); fall back to std::swap().
-    Compare& a = m_;
-    Compare& b = o.m_;
-    swap(a, b);
-    m_.cont_.swap(o.m_.cont_);
+      RefPtr(RefPtr<T>&& ref) :
+    m_ptr(nullptr)
+  {
+    *this = std::move(ref);
   }
-    
-      EXPECT_TRUE((std::is_same<remove_cvref_t<int volatile const&>, int>::value));
-  EXPECT_TRUE(
-      (std::is_same<remove_cvref<int volatile const&>::type, int>::value));
-    
-    #include <folly/futures/Future.h>
-#include <folly/Likely.h>
-#include <folly/SingletonThreadLocal.h>
-#include <folly/futures/ThreadWheelTimekeeper.h>
-    
-      int res = 0;
-  bool ranLoopCallbacks;
-  bool blocking = !(flags & EVLOOP_NONBLOCK);
-  bool once = (flags & EVLOOP_ONCE);
