49b1333cb7190c3053cd86ec6de6bc7438eb16d6
SUNsung <weatherius@gmail.com>
2018-06-04 06:56:28 +0300

    Rewrite [17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py] File in: 2018-06-04 06:56:27

--- pyton/17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py
+++ pyton/17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py
@@ -1,144 +1,122 @@
 
         
-        import logging
-import sys
-    
-        def get_source(self, environment, template):
-        if self.app.config['EXPLAIN_TEMPLATE_LOADING']:
-            return self._get_source_explained(environment, template)
-        return self._get_source_fast(environment, template)
-    
-                    if change_info:
-                    break
-    
-        Test that certain behavior of flask can be customized by
-    subclasses.
+        with open('update/LATEST_VERSION', 'w') as f:
+    f.write(version)
     
     
-def test_add_template_test_with_name(app):
-    def is_boolean(value):
-        return isinstance(value, bool)
+class TestUnicodeLiterals(unittest.TestCase):
+    def test_all_files(self):
+        for dirpath, dirnames, filenames in os.walk(rootDir):
+            for ignore_dir in IGNORED_DIRS:
+                if ignore_dir in dirnames:
+                    # If we remove the directory from dirnames os.walk won't
+                    # recurse into it
+                    dirnames.remove(ignore_dir)
+            for basename in filenames:
+                if not basename.endswith('.py'):
+                    continue
+                if basename in IGNORED_FILES:
+                    continue
     
-      Returns:
-    Python dictionary holding XML contents.
-  '''
-  if not xml:
-    return {xml.tag: xml.text}
-  result = {}
-  for child in xml:
-    child_result = recursive_parse_xml_to_dict(child)
-    if child.tag != 'object':
-      result[child.tag] = child_result[child.tag]
+            webpage = self._download_webpage(url, video_id)
+    
+    from .common import InfoExtractor
+from ..utils import js_to_json
+    
+            runtime = self._search_regex(
+            r'Runtime\s*:\s*(.+?) \|', webpage, 'duration', default=None)
+        if runtime:
+            runtime = re.sub(r'[\s-]', '', runtime)
+        duration = parse_duration(runtime)
+        view_count = int_or_none(self._search_regex(
+            r'Views\s*:\s*(\d+)', webpage, 'view count', default=None))
+        comment_count = int_or_none(self._search_regex(
+            r'Comments\s*:\s*(\d+)', webpage, 'comment count', default=None))
+    
+    
+@for_app('tar')
+def match(command):
+    return ('-C' not in command.script
+            and _is_tar_extract(command.script)
+            and _tar_file(command.script_parts) is not None)
+    
+            String:                    '#4e9a06',        # class: 's'
+        String.Backtick:           '#4e9a06',        # class: 'sb'
+        String.Char:               '#4e9a06',        # class: 'sc'
+        String.Doc:                'italic #8f5902', # class: 'sd' - like a comment
+        String.Double:             '#4e9a06',        # class: 's2'
+        String.Escape:             '#4e9a06',        # class: 'se'
+        String.Heredoc:            '#4e9a06',        # class: 'sh'
+        String.Interpol:           '#4e9a06',        # class: 'si'
+        String.Other:              '#4e9a06',        # class: 'sx'
+        String.Regex:              '#4e9a06',        # class: 'sr'
+        String.Single:             '#4e9a06',        # class: 's1'
+        String.Symbol:             '#4e9a06',        # class: 'ss'
+    
+    
+def to_native_string(string, encoding='ascii'):
+    '''Given a string object, regardless of type, returns a representation of
+    that string in the native string type, encoding and decoding where
+    necessary. This assumes ASCII unless told otherwise.
+    '''
+    if isinstance(string, builtin_str):
+        out = string
     else:
-      if child.tag not in result:
-        result[child.tag] = []
-      result[child.tag].append(child_result[child.tag])
-  return {xml.tag: result}
-    
-        self._kernel_shapes = []
-    for i, width in enumerate(self._widths):
-      if self._activation == 'glu' and i in self._activation_layers:
-        self._kernel_shapes.append(
-            [width, width, self._depths[i], 2 * self._depths[i + 1]])
-      else:
-        self._kernel_shapes.append(
-            [width, width, self._depths[i], self._depths[i + 1]])
-    
-          # Just check a few items for now, hopefully that's sufficient to ensure
-      # the permutation is okay.
-      self.assertEqual(matrix[0, 0, 0], actual_permuted[0, 0, 0])
-      self.assertEqual(matrix[0, 0, 1], actual_permuted[0, 0, 1])
-      self.assertEqual(matrix[1, 1, 16], actual_permuted[1, 1, 16])
-      self.assertEqual(matrix[2, 0, 4], actual_permuted[2, 0, 8])
-      self.assertEqual(matrix[2, 0, 5], actual_permuted[2, 0, 9])
-      self.assertEqual(matrix[2, 1, 8], actual_permuted[2, 1, 4])
-      self.assertEqual(matrix[2, 1, 8 + 16], actual_permuted[2, 1, 4 + 16])
-    
-    import os
-import tensorflow as tf
-    
-        Parameters used:
-      hidden_layer_sizes: Comma-delimited number of hidden units for each layer.
-      input_dropout_rate (-1.0): Input dropout rate for each layer.  If < 0.0,
-          use the global |dropout_rate| hyperparameter.
-      recurrent_dropout_rate (0.8): Recurrent dropout rate.  If < 0.0, use the
-          global |recurrent_dropout_rate| hyperparameter.
-      layer_norm (True): Whether or not to use layer norm.
-    
-    py_binary(
-    name = 'segmenter_trainer',
-    srcs = ['segmenter_trainer.py'],
-    deps = [
-        '//dragnn/core:dragnn_bulk_ops',
-        '//dragnn/core:dragnn_ops',
-        '//dragnn/protos:spec_pb2_py',
-        '//dragnn/python:evaluation',
-        '//dragnn/python:graph_builder',
-        '//dragnn/python:sentence_io',
-        '//dragnn/python:spec_builder',
-        '//dragnn/python:trainer_lib',
-        '//syntaxnet:parser_ops',
-        '@absl_py//absl/flags',
-        '@org_tensorflow//tensorflow:tensorflow_py',
-        '@org_tensorflow//tensorflow/core:protos_all_py',
-    ],
-)
-    
-        def __init__(self):
-        Resource.__init__(self)
-        self.concurrent = 0
-        self.tail = deque(maxlen=100)
-        self._reset_stats()
-    
-        def run(self, args, opts):
-        if len(args) < 1:
-            raise UsageError()
-        elif len(args) > 1:
-            raise UsageError('running 'scrapy crawl' with more than one spider is no longer supported')
-        spname = args[0]
-    
-            try:
-            spidercls = self.crawler_process.spider_loader.load(name)
-        except KeyError:
-            pass
+        if is_py2:
+            out = string.encode(encoding)
         else:
-            # if spider already exists and not --force then halt
-            if not opts.force:
-                print('Spider %r already exists in module:' % name)
-                print('  %s' % spidercls.__module__)
-                return
-        template_file = self._find_template(opts.template)
-        if template_file:
-            self._genspider(module, name, domain, opts.template, template_file)
-            if opts.edit:
-                self.exitcode = os.system('scrapy edit '%s'' % name)
+            out = string.decode(encoding)
     
-    # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
     
-        return config
+class LookupDict(dict):
+    '''Dictionary lookup object.'''
+    
+    
+class ReadTimeout(Timeout):
+    '''The server did not send any data in the allotted amount of time.'''
+    
+                        for i in request.body:
+                        low_conn.send(hex(len(i))[2:].encode('utf-8'))
+                        low_conn.send(b'\r\n')
+                        low_conn.send(i)
+                        low_conn.send(b'\r\n')
+                    low_conn.send(b'0\r\n\r\n')
+    
+        def contribute(self):
+        self.blackboard.common_state['problems'] += random.randint(1, 2)
+        self.blackboard.common_state['suggestions'] += random.randint(10, 20)
+        self.blackboard.common_state['contributions'] += [self.__class__.__name__]
+        self.blackboard.common_state['progress'] += random.randint(10, 100)
+    
+        @classmethod
+    def __get_test_directory(self):
+        '''
+        Get the temporary directory for the tests.
+        '''
+        self.test_dir = os.path.join(os.path.dirname(
+            os.path.realpath(__file__)), 'test_command')
+    
+        def test_data_change_shall_notify_all_observers_once(cls):
+        with patch.object(cls.dec_obs, 'update') as mock_dec_obs_update,\
+                patch.object(cls.hex_obs, 'update') as mock_hex_obs_update:
+            cls.sub.data = 10
+            cls.assertEqual(mock_dec_obs_update.call_count, 1)
+            cls.assertEqual(mock_hex_obs_update.call_count, 1)
+    
+        def __init__(self, name):
+        self.name = name
+    
+    '''
+Port of the Java example of 'Setter Injection' in
+'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
+(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
+http://xunitpatterns.com/Dependency%20Injection.html.
+    
+        def now(self):
+        current_time_is_always_midnight = '24:01'
+        return current_time_is_always_midnight
 
     
-                note = None
-            buyer = c.user
-            if c.user.name.lower() in g.live_config['proxy_gilding_accounts']:
-                note = 'proxy-%s' % c.user.name
-                if proxying_for:
-                    try:
-                        buyer = Account._by_name(proxying_for)
-                    except NotFound:
-                        pass
-    
-        @require_oauth2_scope('mysubreddits')
-    @validate(
-        VUser(),
-    )
-    @api_doc(
-        section=api_section.account,
-        uri='/api/v1/me/karma',
-    )
-    def GET_karma(self):
-        '''Return a breakdown of subreddit karma.'''
-        karmas = c.oauth_user.all_karmas(include_old=False)
-        resp = KarmaListJsonTemplate().render(karmas)
-        return self.api_wrapper(resp.finalize())
+    *TL;DR80
+Provides the ability to restore an object to its previous state.
+'''
