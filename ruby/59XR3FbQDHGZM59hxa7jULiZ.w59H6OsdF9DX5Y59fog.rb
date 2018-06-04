437158727e9e16d34b4032b79f2307bddf111d72
SUNsung <weatherius@gmail.com>
2018-06-04 23:57:03 +0300

    Rewrite [59XR3FbQDHGZM59hxa7jULiZ.w59H6OsdF9DX5Y59fog.rb] File in: 2018-06-04 23:57:02

--- ruby/59XR3FbQDHGZM59hxa7jULiZ.w59H6OsdF9DX5Y59fog.rb
+++ ruby/59XR3FbQDHGZM59hxa7jULiZ.w59H6OsdF9DX5Y59fog.rb
@@ -1,158 +1,168 @@
 
         
-        class JavaScriptHelperTest < ActionView::TestCase
-  tests ActionView::Helpers::JavaScriptHelper
+        require 'abstract_unit'
     
-    module ActionMailer
-  # The <tt>ActionMailer::DeliveryJob</tt> class is used when you
-  # want to send emails outside of the request-response cycle.
-  #
-  # Exceptions are rescued and handled by the mailer class.
-  class DeliveryJob < ActiveJob::Base # :nodoc:
-    queue_as { ActionMailer::Base.deliver_later_queue_name }
+    class TestResponseTest < ActiveSupport::TestCase
+  def assert_response_code_range(range, predicate)
+    response = ActionDispatch::TestResponse.new
+    (0..599).each do |status|
+      response.status = status
+      assert_equal range.include?(status), response.send(predicate),
+                   'ActionDispatch::TestResponse.new(#{status}).#{predicate}'
+    end
+  end
     
-            case method
-        when NilClass
-          raise 'Delivery method cannot be nil'
-        when Symbol
-          if klass = delivery_methods[method]
-            mail.delivery_method(klass, (send(:'#{method}_settings') || {}).merge(options || {}))
-          else
-            raise 'Invalid delivery method #{method.inspect}'
-          end
-        else
-          mail.delivery_method(method)
-        end
-    
-      test 'delivery method can be customized per instance' do
-    stub_any_instance(Mail::SMTP, instance: Mail::SMTP.new({})) do |instance|
-      assert_called(instance, :deliver!) do
-        email = DeliveryMailer.welcome.deliver_now
-        assert_instance_of Mail::SMTP, email.delivery_method
-        email = DeliveryMailer.welcome(delivery_method: :test).deliver_now
-        assert_instance_of Mail::TestMailer, email.delivery_method
+    class WebServiceTest < ActionDispatch::IntegrationTest
+  class TestController < ActionController::Base
+    def assign_parameters
+      if params[:full]
+        render plain: dump_params_keys
+      else
+        render plain: (params.keys - ['controller', 'action']).sort.join(', ')
       end
     end
-  end
     
-      def failed_strategy
-    request.respond_to?(:get_header) ? request.get_header('omniauth.error.strategy') : request.env['omniauth.error.strategy']
-  end
-    
-    class Devise::SessionsController < DeviseController
-  prepend_before_action :require_no_authentication, only: [:new, :create]
-  prepend_before_action :allow_params_authentication!, only: :create
-  prepend_before_action :verify_signed_out_user, only: :destroy
-  prepend_before_action(only: [:create, :destroy]) { request.env['devise.skip_timeout'] = true }
-    
-      # GET /resource/unlock?unlock_token=abcdef
-  def show
-    self.resource = resource_class.unlock_access_by_token(params[:unlock_token])
-    yield resource if block_given?
-    
-          generate_helpers!(Devise::URL_HELPERS)
-    
-    # Each time a record is set we check whether its session has already timed out
-# or not, based on last request time. If so, the record is logged out and
-# redirected to the sign in page. Also, each time the request comes and the
-# record is set, we set the last request time inside its scoped session to
-# verify timeout in the following request.
-Warden::Manager.after_set_user do |record, warden, options|
-  scope = options[:scope]
-  env   = warden.request.env
-    
-      # Returns true if the set is a proper superset of the given set.
-  def proper_superset?(set)
-    case
-    when set.instance_of?(self.class) && @hash.respond_to?(:>)
-      @hash > set.instance_variable_get(:@hash)
-    when set.is_a?(Set)
-      size > set.size && set.all? { |o| include?(o) }
-    else
-      raise ArgumentError, 'value must be a set'
-    end
-  end
-  alias > proper_superset?
-    
-      it 'implicitly has a count of one when no count is specified' do
-    'abc'.unpack(unpack_format).should == ['a']
-  end
-    
-    describe :string_unpack_unicode, shared: true do
-  it 'decodes Unicode codepoints as ASCII values' do
-    [ ['\x00',      [0]],
-      ['\x01',      [1]],
-      ['\x08',      [8]],
-      ['\x0f',      [15]],
-      ['\x18',      [24]],
-      ['\x1f',      [31]],
-      ['\x7f',      [127]],
-      ['\xc2\x80',  [128]],
-      ['\xc2\x81',  [129]],
-      ['\xc3\xbf',  [255]]
-    ].should be_computed_by(:unpack, 'U')
-  end
-    
-      it 'runs nested ensure clauses' do
-    ScratchPad.record []
-    @outer = Thread.new do
-      begin
-        @inner = Thread.new do
-          begin
-            sleep
-          ensure
-            ScratchPad << :inner_ensure_clause
-          end
-        end
-        sleep
-      ensure
-        ScratchPad << :outer_ensure_clause
-        @inner.send(@method)
-        @inner.join
+        # An email was generated.
+    def process(event)
+      debug do
+        mailer = event.payload[:mailer]
+        action = event.payload[:action]
+        '#{mailer}##{action}: processed outbound mail in #{event.duration.round(1)}ms'
       end
     end
-    Thread.pass while @outer.status and @outer.status != 'sleep'
-    Thread.pass until @inner
-    Thread.pass while @inner.status and @inner.status != 'sleep'
-    @outer.send(@method)
-    @outer.join
-    ScratchPad.recorded.should include(:inner_ensure_clause)
-    ScratchPad.recorded.should include(:outer_ensure_clause)
-  end
     
-    desc 'Start a dummy (test) Rails app server'
-task :dummy_rails do
-  require 'rack'
-  require 'term/ansicolor'
-  port = ENV['PORT'] || 9292
-  puts %Q(Starting on #{Term::ANSIColor.cyan 'http://localhost:#{port}'})
-  Rack::Server.start(
-    config: 'test/dummy_rails/config.ru',
-    Port: port)
+        def with_translation(locale, data)
+      I18n.backend.store_translations(locale, data)
+      yield
+    ensure
+      I18n.backend.reload!
+    end
 end
+
     
-      def enough_poll_answers
-    errors.add(:poll_answers, I18n.t('activerecord.errors.models.poll.attributes.poll_answers.not_enough_poll_answers')) if poll_answers.size < 2
+      class TestMailer < ActionMailer::Base
+    def receive(mail)
+      # Do nothing
+    end
   end
     
-        def initialize(attributes={})
-      assign_attributes(attributes)
-      yield(self) if block_given?
+        log = StringIO.new
+    @dry_run_started_at = Time.zone.now
+    @dry_run_logger = Logger.new(log).tap { |logger|
+      logger.formatter = proc { |severity, datetime, progname, message|
+        elapsed_time = '%02d:%02d:%02d' % 2.times.inject([datetime - @dry_run_started_at]) { |(x, *xs)|
+          [*x.divmod(60), *xs]
+        }
+    }
+    }
+    
+      def tumblr_consumer_secret
+    ENV['TUMBLR_OAUTH_SECRET']
+  end
+    
+        respond_to do |format|
+      format.html { render layout: !request.xhr? }
+      format.json { render json: @jobs }
+    end
+  end
+    
+      def translation_scope
+    'devise.omniauth_callbacks'
+  end
+end
+
+    
+        # The path used after confirmation.
+    def after_confirmation_path_for(resource_name, resource)
+      if signed_in?(resource_name)
+        signed_in_root_path(resource)
+      else
+        new_session_path(resource_name)
+      end
     end
     
-      not_found do
-    send_file(File.join(File.dirname(__FILE__), 'public', '404.html'), {:status => 404})
-  end
+    (allow file-read-metadata)
+(allow file-read*
+  ; This is currenly only added because using `xcodebuild` to build a resource
+  ; bundle target starts a FSEvents stream on `/`. No idea why that would be
+  ; needed, but for now it doesn’t seem like a real problem.
+  (literal '/')
+  (regex
+    ; TODO see if we can restrict this more, but it's going to be hard
+    #'^/Users/[^.]+/*'
+    ;#'^/Users/[^.]+/.netrc'
+    ;#'^/Users/[^.]+/.gemrc'
+    ;#'^/Users/[^.]+/.gem/*'
+    ;#'^/Users/[^.]+/Library/.*'
+    #'^/Library/*'
+    #'^/System/Library/*'
+    #'^/usr/lib/*'
+    #'^/usr/share/*'
+    #'^/private/*'
+    #'^/dev/*'
+    #'^<%= ruby_prefix %>'
+    #'^<%= pod_prefix %>'
+    #'^<%= xcode_app_path %>'
+    #'^<%= Pod::Config.instance.repos_dir %>'
+<% prefixes.each do |prefix| %>
+    #'^<%= prefix %>/*'
+<% end %>
+  )
+)
     
-    module Jekyll
-    
-        def script_url_for(gist_id, filename)
-      url = 'https://gist.github.com/#{gist_id}.js'
-      url = '#{url}?file=#{filename}' unless filename.nil? or filename.empty?
-      url
+          def actual_path
+        $PROGRAM_NAME
+      end
     end
-    
-      # Checks for excerpts (helpful for template conditionals)
-  def has_excerpt(input)
-    input =~ /<!--\s*more\s*-->/i ? true : false
   end
+end
+
+    
+            self.summary = 'The repl listens to commands on standard input'
+        self.description = <<-DESC
+          The repl listens to commands on standard input and prints their
+          result to standard output.
+          It accepts all the other ipc subcommands. The repl will signal the
+          end of output with the the ASCII CR+LF `\\n\\r`.
+        DESC
+    
+            def initialize(node, variable)
+          unless VARIABLE_ASSIGNMENT_TYPES.include?(node.type)
+            raise ArgumentError,
+                  'Node type must be any of #{VARIABLE_ASSIGNMENT_TYPES}, ' \
+                  'passed #{node.type}'
+          end
+    
+                break if !assignment.branch || assignment.branch == reference.branch
+    
+            # 'match' is a fairly generic name, so we don't flag it unless we see
+        # a string or regexp literal on one side or the other
+        def_node_matcher :match_call?, <<-PATTERN
+          {(send {str regexp} :match _)
+           (send !nil? :match {str regexp})}
+        PATTERN
+    
+            def on_case(case_node)
+          case_node.when_branches.each_with_object([]) do |when_node, previous|
+            when_node.each_condition do |condition|
+              next unless repeated_condition?(previous, condition)
+    
+            def on_block(node)
+          on_body_of_reduce(node) do |body|
+            void_next = body.each_node(:next).find do |n|
+              n.children.empty? && parent_block_node(n) == node
+            end
+    
+            def variables_in_simple_node(node)
+          simple_double_comparison?(node) do |var1, var2|
+            return [variable_name(var1), variable_name(var2)]
+          end
+          simple_comparison?(node) do |var|
+            return [variable_name(var)]
+          end
+          []
+        end
+    
+            def on_send(node)
+          return unless node.receiver && node.method?(:freeze) &&
+                        immutable_literal?(node.receiver)
