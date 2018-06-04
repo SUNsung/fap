dd999952f239b6078fde94180b3fed31f610b9a4
SUNsung <weatherius@gmail.com>
2018-06-04 16:55:37 +0300

    Rewrite [23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb] File in: 2018-06-04 16:55:36

--- ruby/23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb
+++ ruby/23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb
@@ -1,97 +1,83 @@
 
-    {
-    {        '[#{elapsed_time}] #{severity} -- #{progname}: #{message}\n'
-      }
-    }
-    @dry_run_results = {
-      events: [],
-    }
-    
-      def present(payload)
-    if payload.is_a?(Hash)
-      payload = ActiveSupport::HashWithIndifferentAccess.new(payload)
-      MAIN_KEYS.each do |key|
-        return { :title => payload[key].to_s, :entries => present_hash(payload, key) } if payload.has_key?(key)
-      end
-    
-      def tumblr
-    Tumblr.configure do |config|
-      config.consumer_key = tumblr_consumer_key
-      config.consumer_secret = tumblr_consumer_secret
-      config.oauth_token = tumblr_oauth_token
-      config.oauth_token_secret = tumblr_oauth_token_secret
-    end
-    
-    Tumblr::Client.new
-  end
-end
-    
-      def set_table_sort(sort_options)
-    valid_sorts = sort_options[:sorts] or raise ArgumentError.new('You must specify :sorts as an array of valid sort attributes.')
-    default = sort_options[:default] || { valid_sorts.first.to_sym => :desc }
-    
-        respond_to do |format|
-      if @user_credential.update_attributes(user_credential_params)
-        format.html { redirect_to user_credentials_path, notice: 'Your credential was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user_credential.errors, status: :unprocessable_entity }
+        
+            # Require each of the runtime_dependencies specified by the theme's gemspec.
+    #
+    # Returns false only if no dependencies have been specified, otherwise nothing.
+    def require_theme_deps
+      return false unless site.theme.runtime_dependencies
+      site.theme.runtime_dependencies.each do |dep|
+        next if dep.name == 'jekyll'
+        External.require_with_graceful_fail(dep.name) if plugin_allowed?(dep.name)
       end
     end
-  end
     
-        attr_reader :filters
+          def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :name,
+                                       env_name: 'KEYCHAIN_NAME',
+                                       description: 'Keychain name',
+                                       conflicting_options: [:path],
+                                       is_string: true,
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :path,
+                                       env_name: 'KEYCHAIN_PATH',
+                                       description: 'Path to keychain',
+                                       is_string: true,
+                                       conflicting_options: [:name],
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :password,
+                                       env_name: 'KEYCHAIN_PASSWORD',
+                                       description: 'Password for the keychain',
+                                       sensitive: true,
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :default_keychain,
+                                       description: 'Should the newly created Keychain be the new system default keychain',
+                                       is_string: false,
+                                       default_value: false),
+          FastlaneCore::ConfigItem.new(key: :unlock,
+                                       description: 'Unlock keychain after create',
+                                       is_string: false,
+                                       default_value: false),
+          FastlaneCore::ConfigItem.new(key: :timeout,
+                                       description: 'timeout interval in seconds. Set `false` if you want to specify 'no time-out'',
+                                       is_string: false,
+                                       default_value: 300),
+          FastlaneCore::ConfigItem.new(key: :lock_when_sleeps,
+                                       description: 'Lock keychain when the system sleeps',
+                                       is_string: false,
+                                       default_value: false),
+          FastlaneCore::ConfigItem.new(key: :lock_after_timeout,
+                                       description: 'Lock keychain after timeout interval',
+                                       is_string: false,
+                                       default_value: false),
+          FastlaneCore::ConfigItem.new(key: :add_to_search_list,
+                                       description: 'Add keychain to search list',
+                                       is_string: false,
+                                       default_value: true)
+        ]
+      end
     
-            doc
+            self.tasks
       end
     end
   end
 end
 
     
-      def meta
-    object.file.meta
+        def initialize(username, domain)
+      @username = username
+      @domain = domain
+    end
+    
+      task :ensure_id_sequences_exist do
+    each_schema_load_environment do
+      Mastodon::Snowflake.ensure_id_sequences_exist
+    end
   end
 end
 
     
-            def create
-          authorize! :create, StockMovement
-          @stock_movement = scope.new(stock_movement_params)
-          if @stock_movement.save
-            respond_with(@stock_movement, status: 201, default_template: :show)
-          else
-            invalid_resource!(@stock_movement)
-          end
-        end
-    
-      node[:applications].each do |app, data|
-    template '/etc/monit.d/sidekiq_#{app}.monitrc' do 
-      owner 'root' 
-      group 'root' 
-      mode 0644 
-      source 'monitrc.conf.erb' 
-      variables({ 
-        :num_workers => worker_count,
-        :app_name => app, 
-        :rails_env => node[:environment][:framework_env] 
-      }) 
-    end
-    
-      <form method='post' action='/msg'>
-    <input type='text' name='msg'>
-    <input type='submit' value='Add Message'>
-  </form>
-    
-    module Sidekiq
-  module Generators # :nodoc:
-    class WorkerGenerator < ::Rails::Generators::NamedBase # :nodoc:
-      desc 'This generator creates a Sidekiq Worker in app/workers and a corresponding test'
-    
-          def insert_after(oldklass, newklass, *args)
-        i = entries.index { |entry| entry.klass == newklass }
-        new_entry = i.nil? ? Entry.new(newklass, *args) : entries.delete_at(i)
-        i = entries.index { |entry| entry.klass == oldklass } || entries.count - 1
-        entries.insert(i+1, new_entry)
-      end
+      def perform(user_id)
+    @user = User.find(user_id)
+    deliver_digest if @user.allows_digest_emails?
+  end
