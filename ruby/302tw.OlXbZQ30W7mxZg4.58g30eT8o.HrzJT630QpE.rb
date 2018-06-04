e01086f8d7001cd53bc816a4416de51979c36634
SUNsung <weatherius@gmail.com>
2018-06-04 19:30:37 +0300

    Rewrite [302tw.OlXbZQ30W7mxZg4.58g30eT8o.HrzJT630QpE.rb] File in: 2018-06-04 19:30:37

--- ruby/302tw.OlXbZQ30W7mxZg4.58g30eT8o.HrzJT630QpE.rb
+++ ruby/302tw.OlXbZQ30W7mxZg4.58g30eT8o.HrzJT630QpE.rb
@@ -1,157 +1,162 @@
 
         
-        def check_link(uri)
-  HTTParty.head(uri, :verify => false).code.to_i.tap do |status|
-    if (400..422).include?(status)
-      if status != 403 && !uri.exclude?('udemy.com')
-        raise 'Request had status #{status}'
-      else
-        putc('S')
+          test 'token_and_options returns correct token' do
+    token = 'rcHu+HzSFw89Ypyhn/896A=='
+    actual = ActionController::HttpAuthentication::Token.token_and_options(sample_request(token)).first
+    expected = token
+    assert_equal(expected, actual)
+  end
+    
+      class ExclaimerMiddleware
+    def initialize(app)
+      @app = app
+    end
+    
+    module ActionMailer
+  # The <tt>ActionMailer::DeliveryJob</tt> class is used when you
+  # want to send emails outside of the request-response cycle.
+  #
+  # Exceptions are rescued and handled by the mailer class.
+  class DeliveryJob < ActiveJob::Base # :nodoc:
+    queue_as { ActionMailer::Base.deliver_later_queue_name }
+    
+        # Access the message instance.
+    def message
+      @_message
+    end
+    
+        class DeliveryJob < ActionMailer::DeliveryJob # :nodoc:
+      def perform(mailer, mail_method, delivery_method, params, *args)
+        mailer.constantize.with(params).public_send(mail_method, *args).send(delivery_method)
       end
     end
-  end
-end
-    
-          def key?(key)
-        super(convert_key(key))
-      end
-    
-            # Get the proper capability host to check
-        cap_host = nil
-        if type == :host
-          cap_host = @env.host
-        else
-          with_target_vms([]) do |vm|
-            cap_host = case type
-                       when :provider
-                         vm.provider
-                       when :guest
-                         vm.guest
-                       else
-                         raise Vagrant::Errors::CLIInvalidUsage,
-                           help: opts.help.chomp
-                       end
-          end
-        end
-    
-      class << self
-    def [](*ary)        # :nodoc:
-      new(ary)
-    end
-    
-      def test_strftime
-    t2000 = get_t2000
-    t = Time.at(946684800).getlocal
-    assert_equal('Sat', t2000.strftime('%a'))
-    assert_equal('Saturday', t2000.strftime('%A'))
-    assert_equal('Jan', t2000.strftime('%b'))
-    assert_equal('January', t2000.strftime('%B'))
-    assert_kind_of(String, t2000.strftime('%c'))
-    assert_equal('01', t2000.strftime('%d'))
-    assert_equal('00', t2000.strftime('%H'))
-    assert_equal('12', t2000.strftime('%I'))
-    assert_equal('001', t2000.strftime('%j'))
-    assert_equal('01', t2000.strftime('%m'))
-    assert_equal('00', t2000.strftime('%M'))
-    assert_equal('AM', t2000.strftime('%p'))
-    assert_equal('00', t2000.strftime('%S'))
-    assert_equal('00', t2000.strftime('%U'))
-    assert_equal('00', t2000.strftime('%W'))
-    assert_equal('6', t2000.strftime('%w'))
-    assert_equal('01/01/00', t2000.strftime('%x'))
-    assert_equal('00:00:00', t2000.strftime('%X'))
-    assert_equal('00', t2000.strftime('%y'))
-    assert_equal('2000', t2000.strftime('%Y'))
-    assert_equal('UTC', t2000.strftime('%Z'))
-    assert_equal('%', t2000.strftime('%%'))
-    assert_equal('0', t2000.strftime('%-S'))
-    assert_equal('12:00:00 AM', t2000.strftime('%r'))
-    assert_equal('Sat 2000-01-01T00:00:00', t2000.strftime('%3a %FT%T'))
-    
-      it 'decodes past whitespace bytes when passed the '*' modifier' do
-    [ ['a b c',    ['a b c']],
-      ['a\fb c',   ['a\fb c']],
-      ['a\nb c',   ['a\nb c']],
-      ['a\rb c',   ['a\rb c']],
-      ['a\tb c',   ['a\tb c']],
-      ['a\vb c',   ['a\vb c']],
-    ].should be_computed_by(:unpack, unpack_format('*'))
-  end
-end
-    
-    describe :string_unpack_unicode, shared: true do
-  it 'decodes Unicode codepoints as ASCII values' do
-    [ ['\x00',      [0]],
-      ['\x01',      [1]],
-      ['\x08',      [8]],
-      ['\x0f',      [15]],
-      ['\x18',      [24]],
-      ['\x1f',      [31]],
-      ['\x7f',      [127]],
-      ['\xc2\x80',  [128]],
-      ['\xc2\x81',  [129]],
-      ['\xc3\xbf',  [255]]
-    ].should be_computed_by(:unpack, 'U')
-  end
-    
-        run.should be_true
-  end
-    
-    def usage
-  <<-EOS
-Usage: list_login_items_for_app <path.app>
-    
-        it 'denies requests with duplicate session cookies' do
-      get '/', {}, 'HTTP_COOKIE' => 'rack.session=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
-      expect(last_response).not_to be_ok
-    end
-    
-        it 'leaves normal params untouched' do
-      mock_app do |env|
-        request = Rack::Request.new(env)
-        [200, {'Content-Type' => 'text/plain'}, [request.params['foo']]]
-      end
-      get '/', :foo => 'bar'
-      expect(body).to eq('bar')
-    end
-    
-      it 'should not override the header if already set X-Content-Type-Options' do
-    mock_app with_headers('X-Content-Type-Options' => 'sniff')
-    expect(get('/', {}, 'wants' => 'text/html').headers['X-Content-Type-Options']).to eq('sniff')
   end
 end
 
     
-            def operator
-          assignment_node = meta_assignment_node || @node
-          assignment_node.loc.operator.source
+      def test_send_mail
+    stub_any_instance(Mail::SMTP, instance: Mail::SMTP.new({})) do |instance|
+      assert_called(instance, :deliver!) do
+        with_translation 'de', email_subject: '[Anmeldung] Willkommen' do
+          get '/test/send_mail'
+          assert_equal 'Mail sent - Subject: [Anmeldung] Willkommen', @response.body
+        end
+      end
+    end
+  end
+    
+        mail_with_defaults do |format|
+      format.html { render(inline: '<%= block_format @text %>') }
+    end
+  end
+    
+        version 'C++' do
+      self.base_url = 'https://api.haxe.org/cpp/'
+    end
+    
+        version '6 LTS' do
+      self.release = '6.13.1'
+      self.base_url = 'https://nodejs.org/dist/latest-v6.x/docs/api/'
+    end
+    
+        process_font_assets
+    process_stylesheet_assets
+    process_javascript_assets
+    store_version
+  end
+    
+        alias log puts
+    
+      def test_image_helper
+    assert_match %r(url\(['']?/assets/apple-touch-icon-144-precomposed.*png['']?\)), @css
+  end
+    
+    namespace :bower do
+    
+      def create_issue(repo, title, body)
+    url = 'https://api.github.com/repos/#{repo}/issues'
+    data = { 'title' => title, 'body' => body }
+    scopes = GitHub::CREATE_ISSUE_FORK_OR_PR_SCOPES
+    GitHub.open_api(url, data: data, scopes: scopes)['html_url']
+  end
+    
+      def env_proc
+    self.class.env_proc
+  end
+    
+          # Get the cache key pair for the given Sass URI.
+      # The URI need not be checked for validity.
+      #
+      # The only strict requirement is that the returned pair of strings
+      # uniquely identify the file at the given URI.
+      # However, the first component generally corresponds roughly to the directory,
+      # and the second to the basename, of the URI.
+      #
+      # Note that keys must be unique *across importers*.
+      # Thus it's probably a good idea to include the importer name
+      # at the beginning of the first component.
+      #
+      # @param uri [String] A URI known to be valid for this importer.
+      # @param options [{Symbol => Object}] Options for the Sass file
+      #   containing the `@import` currently being checked.
+      # @return [(String, String)] The key pair which uniquely identifies
+      #   the file at the given URI.
+      def key(uri, options)
+        Sass::Util.abstract(self)
+      end
+    
+    (allow file-read-metadata)
+(allow file-read*
+  ; This is currenly only added because using `xcodebuild` to build a resource
+  ; bundle target starts a FSEvents stream on `/`. No idea why that would be
+  ; needed, but for now it doesn’t seem like a real problem.
+  (literal '/')
+  (regex
+    ; TODO see if we can restrict this more, but it's going to be hard
+    #'^/Users/[^.]+/*'
+    ;#'^/Users/[^.]+/.netrc'
+    ;#'^/Users/[^.]+/.gemrc'
+    ;#'^/Users/[^.]+/.gem/*'
+    ;#'^/Users/[^.]+/Library/.*'
+    #'^/Library/*'
+    #'^/System/Library/*'
+    #'^/usr/lib/*'
+    #'^/usr/share/*'
+    #'^/private/*'
+    #'^/dev/*'
+    #'^<%= ruby_prefix %>'
+    #'^<%= pod_prefix %>'
+    #'^<%= xcode_app_path %>'
+    #'^<%= Pod::Config.instance.repos_dir %>'
+<% prefixes.each do |prefix| %>
+    #'^<%= prefix %>/*'
+<% end %>
+  )
+)
+    
+            # Runs the template configuration utilities.
+        #
+        # @return [void]
+        #
+        def configure_template
+          UI.section('Configuring #{@name} template.') do
+            Dir.chdir(@name) do
+              if File.exist?('configure')
+                system({ 'COCOAPODS_VERSION' => Pod::VERSION }, './configure', @name, *@additional_args)
+              else
+                UI.warn 'Template does not have a configure file.'
+              end
+            end
+          end
         end
     
-                  add_offense(condition)
-            end
-    
-            # Construct annotated source string (like what we parse)
-        #
-        # Reconstruct a deterministic annotated source string. This is
-        # useful for eliminating semantically irrelevant annotation
-        # ordering differences.
-        #
-        # @example standardization
-        #
-        #     source1 = AnnotatedSource.parse(<<-RUBY)
-        #     line1
-        #     ^ Annotation 1
-        #      ^^ Annotation 2
-        #     RUBY
-        #
-        #     source2 = AnnotatedSource.parse(<<-RUBY)
-        #     line1
-        #      ^^ Annotation 2
-        #     ^ Annotation 1
-        #     RUBY
-        #
-        #     source1.to_s == source2.to_s # => true
-        #
-        # @return [String]
-        def to_s
-          reconstructed = lines.dup
+      entries.each do |entry|
+    if File.exist?(entry[:file])
+      warn '[skip] #{entry[:file]} already exists'
+    else
+      File.open(entry[:file], 'w+') do |f|
+        f.write(ERB.new(File.read(entry[:template])).result(binding))
+        puts I18n.t(:written_file, scope: :capistrano, file: entry[:file])
+      end
+    end
+  end
