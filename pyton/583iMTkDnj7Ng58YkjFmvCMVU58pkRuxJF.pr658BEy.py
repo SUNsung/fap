f236317220129c7b3df63b8848fa0094d614db29
SUNsung <weatherius@gmail.com>
2018-06-04 12:27:35 +0300

    Rewrite [583iMTkDnj7Ng58YkjFmvCMVU58pkRuxJF.pr658BEy.py] File in: 2018-06-04 12:27:34

--- pyton/583iMTkDnj7Ng58YkjFmvCMVU58pkRuxJF.pr658BEy.py
+++ pyton/583iMTkDnj7Ng58YkjFmvCMVU58pkRuxJF.pr658BEy.py
@@ -1,47 +1,164 @@
 
         
-        
-def get_info(package_name):
-    api_url = 'https://pypi.python.org/pypi/{}/json'.format(package_name)
-    resp = requests.get(api_url).json()
-    hasher = hashlib.sha256()
-    for release in resp['urls']:
-        download_url = release['url']
-        if download_url.endswith('.tar.gz'):
-            hasher.update(requests.get(download_url).content)
-            return {
-                'name': package_name,
-                'url': download_url,
-                'sha256': hasher.hexdigest(),
-            }
-    else:
-        raise RuntimeError(
-            '{}: download not found: {}'.format(package_name, resp))
-    
-    
-FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
-FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
-JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
-BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
-    
-        @classmethod
-    def supports(cls, mime):
-        raise NotImplementedError
-    
-    
-# noinspection PyAbstractClass
-class BuiltinAuthPlugin(AuthPlugin):
-    
-        '''
+        from .common import FileDownloader
+from ..utils import (
+    check_executable,
+    encodeFilename,
 )
-output_options.add_argument(
-    '--history-print', '-P',
-    dest='output_options_history',
-    metavar='WHAT',
-    help='''
-    The same as --print, -p but applies only to intermediary requests/responses
-    (such as redirects) when their inclusion is enabled with --all. If this
-    options is not specified, then they are formatted the same way as the final
-    response.
     
-    from pylons import tmpl_context as c
+    from .nuevo import NuevoBaseIE
+    
+            formats = [
+            {
+                'format_id': f['type'],
+                'filesize': int(f['filesize']),
+                'url': f['url']
+            } for f in info['rfiles']
+        ]
+        self._sort_formats(formats)
+    
+    
+class CNNArticleIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:(?:edition|www)\.)?cnn\.com/(?!videos?/)'
+    _TEST = {
+        'url': 'http://www.cnn.com/2014/12/21/politics/obama-north-koreas-hack-not-war-but-cyber-vandalism/',
+        'md5': '689034c2a3d9c6dc4aa72d65a81efd01',
+        'info_dict': {
+            'id': 'bestoftv/2014/12/21/ip-north-korea-obama.cnn',
+            'ext': 'mp4',
+            'title': 'Obama: Cyberattack not an act of war',
+            'description': 'md5:0a802a40d2376f60e6b04c8d5bcebc4b',
+            'upload_date': '20141221',
+        },
+        'expected_warnings': ['Failed to download m3u8 information'],
+        'add_ie': ['CNN'],
+    }
+    
+    ] + _py_files('scrapy/contrib') + _py_files('scrapy/contrib_exp')
+    
+        def start_requests(self):
+        qargs = {'total': self.total, 'show': self.show}
+        url = '{}?{}'.format(self.baseurl, urlencode(qargs, doseq=1))
+        return [scrapy.Request(url, dont_filter=True)]
+    
+        def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
+                          help='set spider argument (may be repeated)')
+        parser.add_option('-o', '--output', metavar='FILE',
+                          help='dump scraped items into FILE (use - for stdout)')
+        parser.add_option('-t', '--output-format', metavar='FORMAT',
+                          help='format to use for dumping items with -o')
+    
+        @mock.patch(
+        'certbot_compatibility_test.validator.crypto_util.probe_sni')
+    def test_certificate_error(self, mock_probe_sni):
+        cert = OpenSSL.crypto.X509()
+        mock_probe_sni.side_effect = [acme_errors.Error]
+        self.assertFalse(self.validator.certificate(
+            cert, 'test.com', '127.0.0.1'))
+    
+    # Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+    'repoze.sphinx.autointerface',
+]
+    
+          # These things do not look like template argument list:
+      #   class Suspect {
+      #   class Suspect x; }
+      if token in ('{', '}', ';'): return False
+    
+        base = 'src/sentry/locale'
+    for locale in os.listdir(base):
+        fn = os.path.join(base, locale, 'LC_MESSAGES', 'django.po')
+        if not os.path.isfile(fn):
+            continue
+    
+        # For pickling to work, the __module__ variable needs to be set to the frame
+    # where the named tuple is created.  Bypass this step in enviroments where
+    # sys._getframe is not defined (Jython for example).
+    if hasattr(_sys, '_getframe'):
+        result.__module__ = _sys._getframe(1).f_globals.get('__name__', '__main__')
+    
+    def _python_exit():
+    global _shutdown
+    _shutdown = True
+    items = list(_threads_queues.items())
+    for t, q in items:
+        q.put(None)
+    for t, q in items:
+        t.join()
+    
+      def FilterLevel( diagnostic ):
+    return diagnostic[ 'kind' ] == expected_kind
+    
+      It could be argued that the user actually wants the final buffer state to be
+  'foo.zoobar|' (the cursor at the end), but that would be much more difficult
+  to implement and is probably not worth doing.
+  '''
+    
+    
+  def _HandleDetailedInfoResponse( self ):
+    vimsupport.WriteToPreviewWindow( self._response[ 'detailed_info' ] )
+    
+        def __init__(self, msg_center):
+        self.provider = msg_center
+    
+        def setUp(self):
+        self.tc1 = TC1()
+        self.tc2 = TC2()
+        self.tc3 = TC3()
+        self.average_result_tc1 = '###### In Test 1 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc2 = '###### In Test 2 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc3 = '###### In Test 3 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.runner = TestRunner()
+        self.out = StringIO()
+        self.saved_stdout = sys.stdout
+        sys.stdout = self.out
+    
+        def test_display_current_time_at_current_time(self):
+        '''
+        Just as justification for working example with the time provider used in
+        production. (Will always pass.)
+        '''
+        production_code_time_provider = ProductionCodeTimeProvider()
+        class_under_test = TimeDisplay()
+        class_under_test.set_time_provider(production_code_time_provider)
+        current_time = datetime.datetime.now()
+        expected_time = '<span class=\'tinyBoldText\'>{}:{}</span>'.format(current_time.hour, current_time.minute)
+        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
+
+    
+    '''
+Port of the Java example of 'Constructor Injection' in
+'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
+(ISBN-10: 0131495054, ISBN-13: 978-0131495050)
+    
+    '''
+A class that uses different static function depending of a parameter passed in
+init. Note the use of a single dictionary instead of multiple conditions
+'''
+    
+    
+class OrSpecification(CompositeSpecification):
+    _one = Specification()
+    _other = Specification()
