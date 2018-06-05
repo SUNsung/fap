25aca3034dcde5890b4e68660764a9b443b47aa5
SUNsung <weatherius@gmail.com>
2018-06-05 21:33:18 +0300

    Rewrite [33RDKAgVxpsek33SB2XUl6tiug33OKP2YXSgoNc33F.J.rb] File in: 2018-06-05 21:33:17

--- ruby/33RDKAgVxpsek33SB2XUl6tiug33OKP2YXSgoNc33F.J.rb
+++ ruby/33RDKAgVxpsek33SB2XUl6tiug33OKP2YXSgoNc33F.J.rb
@@ -1,30 +1,3 @@
 
         
-              # Override `Kernel#puts` to prepend four spaces to each line.
-      def puts(string=nil)
-        $stdout.puts(string.to_s.gsub(/^/, '    '))
-      end
-    
-      desc 'Started'
-  task :started do
-  end
-    
-          it 'filters by roles from the :filter variable' do
-        hosts = dsl.roles(:web)
-        all = dsl.roles(:all)
-        SSHKit::Coordinator.expects(:new).with(hosts).returns(@coordinator)
-        dsl.set :filter, roles: 'web'
-        dsl.on(all)
-      end
-    
-      subject { described_class.new(lambda {}) }
-    
-      # Many RSpec users commonly either run the entire suite or an individual
-  # file, and it's useful to allow more verbose output when running an
-  # individual spec file.
-  if config.files_to_run.one?
-    # Use the documentation formatter for detailed output,
-    # unless a formatter has already been configured
-    # (e.g. via a command-line flag).
-    config.default_formatter = 'doc'
-  end
+        
