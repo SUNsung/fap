c9ee93413684fb8113c3f18443c2b087e51183fe
SUNsung <weatherius@gmail.com>
2018-06-05 21:55:34 +0300

    Rewrite [55G0rkV3XDw8M55G2p2mz9tC4o553DB9xJEPytA55UA1.cpp] File in: 2018-06-05 21:55:33

--- cccccc/55G0rkV3XDw8M55G2p2mz9tC4o553DB9xJEPytA55UA1.cpp
+++ cccccc/55G0rkV3XDw8M55G2p2mz9tC4o553DB9xJEPytA55UA1.cpp
@@ -1,68 +1,8 @@
 
         
-        struct Options;
-struct FileMetaData;
-    
-    static std::string MakeFileName(const std::string& name, uint64_t number,
-                                const char* suffix) {
-  char buf[100];
-  snprintf(buf, sizeof(buf), '/%06llu.%s',
-           static_cast<unsigned long long>(number),
-           suffix);
-  return name + buf;
-}
-    
-    static void UnrefEntry(void* arg1, void* arg2) {
-  Cache* cache = reinterpret_cast<Cache*>(arg1);
-  Cache::Handle* h = reinterpret_cast<Cache::Handle*>(arg2);
-  cache->Release(h);
-}
-    
-    
-    {}  // namespace leveldb
-    
-    TEST(WriteBatchTest, Multiple) {
-  WriteBatch batch;
-  batch.Put(Slice('foo'), Slice('bar'));
-  batch.Delete(Slice('box'));
-  batch.Put(Slice('baz'), Slice('boo'));
-  WriteBatchInternal::SetSequence(&batch, 100);
-  ASSERT_EQ(100, WriteBatchInternal::Sequence(&batch));
-  ASSERT_EQ(3, WriteBatchInternal::Count(&batch));
-  ASSERT_EQ('Put(baz, boo)@102'
-            'Delete(box)@101'
-            'Put(foo, bar)@100',
-            PrintContents(&batch));
-}
-    
-      // Open leveldb
-  leveldb::DB* db;
-  leveldb::Options options;
-  options.create_if_missing = true;
-  options.error_if_exists = true;
-  leveldb::Status status = leveldb::DB::Open(
-      options, db_filename, &db);
-  CHECK(status.ok()) << 'Failed to open leveldb ' << db_filename
-      << '. Is it already existing?';
-    
-    /**
- * @brief Computes @f$ y = |x| @f$
- *
- * @param bottom input Blob vector (length 1)
- *   -# @f$ (N \times C \times H \times W) @f$
- *      the inputs @f$ x @f$
- * @param top output Blob vector (length 1)
- *   -# @f$ (N \times C \times H \times W) @f$
- *      the computed outputs @f$ y = |x| @f$
- */
-template <typename Dtype>
-class AbsValLayer : public NeuronLayer<Dtype> {
- public:
-  explicit AbsValLayer(const LayerParameter& param)
-      : NeuronLayer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
+        #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
     
       /// @brief The spatial dimensions of a filter kernel.
   Blob<int> kernel_shape_;
@@ -80,280 +20,122 @@ class AbsValLayer : public NeuronLayer<Dtype> {
   vector<int> output_shape_;
   const vector<int>* bottom_shape_;
     
-      /**
-   * @brief Computes the error gradient w.r.t. the reordered input.
-   *
-   * @param top output Blob vector (length 1), providing the error gradient
-   *        with respect to the outputs
-   *   -# @f$ (M \times ...) @f$:
-   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
-   *      with respect to concatenated outputs @f$ y @f$
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2):
-   *   - @f$ \frac{\partial E}{\partial y} @f$ is de-indexed (summing where
-   *     required) back to the input x_1
-   *   - This layer cannot backprop to x_2, i.e. propagate_down[1] must be
-   *     false.
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    
-    {  Blob<Dtype> diff_;  // cached for backward pass
-  Blob<Dtype> dist_sq_;  // cached for backward pass
-  Blob<Dtype> diff_sq_;  // tmp storage for gpu forward pass
-  Blob<Dtype> summer_vec_;  // tmp storage for gpu forward pass
-};
-    
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    namespace caffe {
-    }
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
     #ifdef USE_CUDNN
-/**
- * @brief CuDNN acceleration of TanHLayer.
- */
+/*
+ * @brief cuDNN implementation of PoolingLayer.
+ *        Fallback to PoolingLayer for CPU mode.
+*/
 template <typename Dtype>
-class CuDNNTanHLayer : public TanHLayer<Dtype> {
+class CuDNNPoolingLayer : public PoolingLayer<Dtype> {
  public:
-  explicit CuDNNTanHLayer(const LayerParameter& param)
-      : TanHLayer<Dtype>(param), handles_setup_(false) {}
+  explicit CuDNNPoolingLayer(const LayerParameter& param)
+      : PoolingLayer<Dtype>(param), handles_setup_(false) {}
   virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
   virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNTanHLayer();
+  virtual ~CuDNNPoolingLayer();
+  // Currently, cuDNN does not support the extra top blob.
+  virtual inline int MinTopBlobs() const { return -1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
     }
     
-    #endif  // CAFFE_EXP_LAYER_HPP_
-
-    
-    
-    {
-    {}  // namespace testing
-}  // namespace grpc
-    
-    namespace grpc {
-namespace testing {
-    }
+    namespace caffe {
     }
     
-    gpr_atm grpc::testing::interop::g_got_sigint;
+      virtual inline const char* type() const { return 'Eltwise'; }
+  virtual inline int MinBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
     
-    grpc::string DescribeMethod(const grpc::protobuf::MethodDescriptor* method) {
-  std::stringstream result;
-  result << '  rpc ' << method->name()
-         << (method->client_streaming() ? '(stream ' : '(')
-         << method->input_type()->full_name() << ') returns '
-         << (method->server_streaming() ? '(stream ' : '(')
-         << method->output_type()->full_name() << ') {}\n';
-  if (method->options().deprecated()) {
-    result << ' DEPRECATED';
+      // The user space daemon is requesting a new circular queue.
+  // Make sure the requested size is within sane size bounds.
+  if (size > MAX_KMEM || size < MIN_KMEM) {
+    err = -EINVAL;
+    goto error_exit;
   }
-  return result.str();
+    
+    #include <osquery/config.h>
+#include <osquery/database.h>
+    
+     protected:
+  /// Calculate the URL once and cache the result.
+  std::string uri_;
+    
+    TEST_F(ProcessTests, test_constructor) {
+  auto p = PlatformProcess(kInvalidPid);
+  EXPECT_FALSE(p.isValid());
 }
     
-    void jpeg_decoder::decode_start()
-{
-  init_frame();
+     private:
+  ProcessState state_{PROCESS_STILL_ALIVE};
+  int status_{0};
+    
+      /// RocksDB logger instance.
+  std::shared_ptr<GlogRocksDBLogger> logger_{nullptr};
+    
+    TEST_F(PrinterTests, test_generate_header) {
+  std::map<std::string, size_t> lengths;
+  for (const auto& row : q) {
+    computeRowLengths(row, lengths);
+  }
     }
     
+    // set of devices across which HostDeviceVector can be distributed;
+// currently implemented as a range, but can be changed later to something else,
+// e.g. a bitset
+class GPUSet {
+ public:
+  explicit GPUSet(int start = 0, int ndevices = 0)
+    : start_(start), ndevices_(ndevices) {}
+  static GPUSet Empty() { return GPUSet(); }
+  static GPUSet Range(int start, int ndevices) { return GPUSet(start, ndevices); }
+  int Size() const { return ndevices_; }
+  int operator[](int index) const {
+    CHECK(index >= 0 && index < ndevices_);
+    return start_ + index;
+  }
+  bool IsEmpty() const { return ndevices_ <= 0; }
+  int Index(int device) const {
+    CHECK(device >= start_ && device < start_ + ndevices_);
+    return device - start_;
+  }
+  bool Contains(int device) const {
+    return start_ <= device && device < start_ + ndevices_;
+  }
+  friend bool operator==(GPUSet a, GPUSet b) {
+    return a.start_ == b.start_ && a.ndevices_ == b.ndevices_;
+  }
+  friend bool operator!=(GPUSet a, GPUSet b) {
+    return a.start_ != b.start_ || a.ndevices_ != b.ndevices_;
+  }
+    }
     
-# if defined(OC_CLZ32)
-/**
- * OC_ILOGNZ_32 - Integer binary logarithm of a non-zero 32-bit value.
- * @_v: A non-zero 32-bit value.
- * Returns floor(log2(_v))+1.
- * This is the number of bits that would be required to represent _v in two's
- *  complement notation with all of the leading zeros stripped.
- * If _v is zero, the return value is undefined; use OC_ILOG_32() instead.
+    SEXP XGBoosterUpdateOneIter_R(SEXP handle, SEXP iter, SEXP dtrain) {
+  R_API_BEGIN();
+  CHECK_CALL(XGBoosterUpdateOneIter(R_ExternalPtrAddr(handle),
+                                  asInteger(iter),
+                                  R_ExternalPtrAddr(dtrain)));
+  R_API_END();
+  return R_NilValue;
+}
+    
+    TEST(Metric, NDCG) {
+  xgboost::Metric * metric = xgboost::Metric::Create('ndcg');
+  ASSERT_STREQ(metric->Name(), 'ndcg');
+  EXPECT_ANY_THROW(GetMetricEval(metric, {0, 1}, {}));
+  EXPECT_NEAR(GetMetricEval(metric, {}, {}), 1, 1e-10);
+  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 1, 1e-10);
+  EXPECT_NEAR(GetMetricEval(metric,
+                            {0.1f, 0.9f, 0.1f, 0.9f},
+                            {  0,   0,   1,   1}),
+              0.6509f, 0.001f);
+    }
+    
+    /*!
+ * \brief Macro to register linear updater.
  */
-#  define OC_ILOGNZ_32(_v) (OC_CLZ32_OFFS-OC_CLZ32(_v))
-/**
- * OC_ILOG_32 - Integer binary logarithm of a 32-bit value.
- * @_v: A 32-bit value.
- * Returns floor(log2(_v))+1, or 0 if _v==0.
- * This is the number of bits that would be required to represent _v in two's
- *  complement notation with all of the leading zeros stripped.
- */
-#  define OC_ILOG_32(_v)   (OC_ILOGNZ_32(_v)&-!!(_v))
-# else
-#  define OC_ILOGNZ_32(_v) (oc_ilog32(_v))
-#  define OC_ILOG_32(_v)   (oc_ilog32(_v))
-# endif
-    
-    # if defined(OC_COLLECT_METRICS)
-typedef struct oc_mode_metrics oc_mode_metrics;
-# endif
-typedef struct oc_mode_rd      oc_mode_rd;
-    
-     ********************************************************************/
-    
-    #undef MULT16_16_Q15_ADD
-static inline int MULT16_16_Q15_ADD(int a, int b, int c, int d) {
-    int m;
-    asm volatile('MULT $ac1, %0, %1' : : 'r' ((int)a), 'r' ((int)b));
-    asm volatile('madd $ac1, %0, %1' : : 'r' ((int)c), 'r' ((int)d));
-    asm volatile('EXTR.W %0,$ac1, %1' : '=r' (m): 'i' (15));
-    return m;
-}
-    
-    # if !defined(__OPTIMIZE__)
-#  define OP_CVTEPI16_EPI32_M64(x) \
- (_mm_cvtepi16_epi32(_mm_loadl_epi64((__m128i *)(x))))
-# else
-#  define OP_CVTEPI16_EPI32_M64(x) \
- (_mm_cvtepi16_epi32(*(__m128i *)(x)))
-# endif
-    
-    #undef    silk_LSHIFT_uint
-static OPUS_INLINE opus_uint32 silk_LSHIFT_uint(opus_uint32 a, opus_int32 shift){
-    opus_uint32 ret;
-    ops_count += 1;
-    ret = a << shift;
-    return ret;
-}
-    
-    /* Variable order MA prediction error filter. */
-void silk_LPC_analysis_filter(
-    opus_int16                  *out,               /* O    Output signal                                               */
-    const opus_int16            *in,                /* I    Input signal                                                */
-    const opus_int16            *B,                 /* I    MA prediction coefficients, Q12 [order]                     */
-    const opus_int32            len,                /* I    Signal length                                               */
-    const opus_int32            d,                  /* I    Filter order                                                */
-    int                         arch                /* I    Run-time architecture                                       */
-);
-    
-    
-    {}
-
-    
-    template<> inline
-dnnError_t dnnBatchNormalizationCreateForward_v2<float>(
-    dnnPrimitive_t* pBatchNormalization,
-    dnnPrimitiveAttributes_t attributes,
-    const dnnLayout_t dataLayout,
-    float eps,
-    unsigned int flags)
-{
-    return dnnBatchNormalizationCreateForward_v2_F32(
-        pBatchNormalization, attributes, dataLayout, eps, flags);
-}
-    
-    
-    {        if (source.Mask() != nullptr)
-            Mask()->CopyFrom(*source.Mask());
-        else
-        {
-            if (Mask() != nullptr)
-            {
-                // Clear the mask
-                Mask()->Clear();
-            }
-        }
-    }
-    
-        // replace children
-    // This looks for nodes in the network that have the same name as its current inputs, and then relinks its inputs to those.
-    // I.e. this allows to move a node from network to another and reconnect by the names if its inputs.
-    for (int i = 0; i < newNode->GetNumInputs(); ++i)
-    {
-        if (m_nameToNodeMap.find(newNode->GetInputs()[i]->NodeName()) == m_nameToNodeMap.end())
-            RuntimeError('Child node %ls is not part of the network.', newNode->GetInputs()[i]->NodeName().c_str());
-        newNode->SetInput(i, m_nameToNodeMap[newNode->GetInputs()[i]->NodeName()]);
-    }
-    
-    public:
-    template <class ConfigRecordType>
-    void InitFromConfig(const ConfigRecordType&);
-    virtual void Init(const ConfigParameters& config) override
-    {
-        InitFromConfig(config);
-    }
-    virtual void Init(const ScriptableObjects::IConfigRecord& config) override
-    {
-        InitFromConfig(config);
-    }
-    virtual void Destroy();
-    BinaryReader()
-        : m_pMBLayout(make_shared<MBLayout>())
-    {
-        m_pMBLayout->SetUniqueAxisName(L'BinaryReader');
-    }
-    virtual ~BinaryReader();
-    virtual void StartMinibatchLoop(size_t mbSize, size_t epoch, size_t requestedEpochSamples = requestDataSize);
-    virtual bool TryGetMinibatch(StreamMinibatchInputs& matrices);
-    
-    cudaStream_t GPUDataTransferer::s_assignStream = NULL;
-    
-    namespace Microsoft { namespace MSR { namespace BS {
-    }
-    }
-    }
-    
-    struct SourceFile // content of one source file  (only in this header because TextLocation's private member uses it)
-{
-    /*const*/ wstring path;                     // where it came from
-    /*const*/ vector<wstring> lines;            // source code lines
-    SourceFile(wstring location, wstring text); // from string, e.g. command line
-    SourceFile(wstring path);                   // from file
-};
-    
-        float4& operator&=(const float4& other)
-    {
-        v = _mm_and_ps(v, other);
-        return *this;
-    }
-    float4& operator|=(const float4& other)
-    {
-        v = _mm_or_ps(v, other);
-        return *this;
-    }
-    float4& operator+=(const float4& other)
-    {
-        v = _mm_add_ps(v, other);
-        return *this;
-    }
-    float4& operator-=(const float4& other)
-    {
-        v = _mm_sub_ps(v, other);
-        return *this;
-    }
-    float4& operator*=(const float4& other)
-    {
-        v = _mm_mul_ps(v, other);
-        return *this;
-    }
-    float4& operator/=(const float4& other)
-    {
-        v = _mm_div_ps(v, other);
-        return *this;
-    }
-    
-    
-#endif  // STORAGE_LEVELDB_DB_WRITE_BATCH_INTERNAL_H_
-
-    
-    namespace leveldb {
-    }
-    
-    
-    {}  // namespace leveldb
-    
-    // Read through the first n keys repeatedly and check that they get
-// compacted (verified by checking the size of the key space).
-void AutoCompactTest::DoReads(int n) {
-  std::string value(kValueSize, 'x');
-  DBImpl* dbi = reinterpret_cast<DBImpl*>(db_);
-    }
+#define XGBOOST_REGISTER_LINEAR_UPDATER(UniqueId, Name)                        \
+  static DMLC_ATTRIBUTE_UNUSED ::xgboost::LinearUpdaterReg&                    \
+      __make_##LinearUpdaterReg##_##UniqueId##__ =                             \
+          ::dmlc::Registry< ::xgboost::LinearUpdaterReg>::Get()->__REGISTER__( \
+              Name)
