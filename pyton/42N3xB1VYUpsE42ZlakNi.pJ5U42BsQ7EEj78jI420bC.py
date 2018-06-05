2147e466e16c87c1283640eb0dfdcd0c7e204796
SUNsung <weatherius@gmail.com>
2018-06-05 13:13:05 +0300

    Rewrite [42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py] File in: 2018-06-05 13:13:05

--- pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
+++ pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
@@ -1,365 +1,55 @@
 
         
-            NOTE(danielandor) For now this method cannot be called during training.
-    That is to say, unroll_using_oracle for this component must be set to true.
-    This will be fixed by separating train_with_oracle and train_with_inference.
-    
-    
-class MockComponent(object):
-    
-    
-def ArcSourcePotentialsFromTokens(tokens, weights):
-  r'''Returns arc source potentials computed from tokens and weights.
-    
-      def testBuildLexicon(self):
-    empty_input_path = os.path.join(test_flags.temp_dir(), 'empty-input')
-    lexicon_output_path = os.path.join(test_flags.temp_dir(), 'lexicon-output')
-    
-            '''
-        available_plugins = plugin_manager.get_formatters_grouped()
-        self.enabled_plugins = []
-        for group in groups:
-            for cls in available_plugins[group]:
-                p = cls(env=env, **kwargs)
-                if p.enabled:
-                    self.enabled_plugins.append(p)
-    
-    
-@mock.patch('httpie.core.get_response')
-def test_error_traceback(get_response):
-    exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    with raises(ConnectionError):
-        main(['--ignore-stdin', '--traceback', 'www.google.com'])
-    
-                if args.check_status or downloader:
-                exit_status = get_exit_status(
-                    http_status=response.status_code,
-                    follow=args.follow
-                )
-                if not env.stdout_isatty and exit_status != ExitStatus.OK:
-                    log_error(
-                        'HTTP %s %s', response.raw.status, response.raw.reason,
-                        level='warning'
-                    )
-    
-        def get_auth(self, username=None, password=None):
-        '''
-        If `auth_parse` is set to `True`, then `username`
-        and `password` contain the parsed credentials.
-    
-        for factor, suffix in abbrevs:
-        if n >= factor:
-            break
-    
-    import threading
-import socket
-import select
-    
-            assert server.handler_results[0] == first_request
-        assert server.handler_results[1] == second_request
-    
-    This module handles import compatibility issues between Python 2 and
-Python 3.
-'''
-    
-    
-def test_idna_without_version_attribute(mocker):
-    '''Older versions of IDNA don't provide a __version__ attribute, verify
-    that if we have such a package, we don't blow up.
-    '''
-    mocker.patch('requests.help.idna', new=None)
-    assert info()['idna'] == {'version': ''}
-    
-       >>> payload = dict(key1='value1', key2='value2')
-   >>> r = requests.post('http://httpbin.org/post', data=payload)
-   >>> print(r.text)
-   {
-     ...
-     'form': {
-       'key2': 'value2',
-       'key1': 'value1'
-     },
-     ...
-   }
-    
-    '''
-The ``codes`` object defines a mapping from common names for HTTP statuses
-to their numerical codes, accessible either as attributes or as dictionary
-items.
-    
-            :param request: The :class:`PreparedRequest <PreparedRequest>` being sent.
-        :param stream: (optional) Whether to stream the request content.
-        :param timeout: (optional) How long to wait for the server to send
-            data before giving up, as a float, or a :ref:`(connect timeout,
-            read timeout) <timeouts>` tuple.
-        :type timeout: float or tuple or urllib3 Timeout object
-        :param verify: (optional) Either a boolean, in which case it controls whether
-            we verify the server's TLS certificate, or a string, in which case it
-            must be a path to a CA bundle to use
-        :param cert: (optional) Any user-provided SSL certificate to be trusted.
-        :param proxies: (optional) The proxies dictionary to apply to the request.
-        :rtype: requests.Response
-        '''
-    
-        unweighted_metric = history.history['output2_' + loss_full_name][0]
-    weighted_metric = history.history['output2_weighted_' + loss_full_name][0]
-    
-    print('Vectorization...')
-x = np.zeros((len(questions), MAXLEN, len(chars)), dtype=np.bool)
-y = np.zeros((len(questions), DIGITS + 1, len(chars)), dtype=np.bool)
-for i, sentence in enumerate(questions):
-    x[i] = ctable.encode(sentence, MAXLEN)
-for i, sentence in enumerate(expected):
-    y[i] = ctable.encode(sentence, DIGITS + 1)
-    
-    # Reserve 0 for masking via pad_sequences
-vocab_size = len(vocab) + 1
-word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
-story_maxlen = max(map(len, (x for x, _, _ in train + test)))
-query_maxlen = max(map(len, (x for _, x, _ in train + test)))
-    
-    
-@pytest.fixture
-def in_tmpdir(tmpdir):
-    '''Runs a function in a temporary directory.
-    
-            # check if binary classification
-        if probs.shape[1] == 1:
-            # first column is probability of class 0 and second is of class 1
-            probs = np.hstack([1 - probs, probs])
-        return probs
-    
-        data_train = fetch_20newsgroups_vectorized(subset='train')
-    data_test = fetch_20newsgroups_vectorized(subset='test')
-    X_train = check_array(data_train.data, dtype=np.float32,
-                          accept_sparse='csc')
-    X_test = check_array(data_test.data, dtype=np.float32, accept_sparse='csr')
-    y_train = data_train.target
-    y_test = data_test.target
-    
-        n_samples = 2000
-    list_n_features = np.linspace(500, 3000, 5).astype(np.int)
-    lasso_results, lars_lasso_results = compute_bench(alpha, [n_samples],
-                                           list_n_features, precompute=False)
-    plt.subplot(212)
-    plt.plot(list_n_features, lasso_results, 'b-', label='Lasso')
-    plt.plot(list_n_features, lars_lasso_results, 'r-', label='LassoLars')
-    plt.title('%d samples, alpha=%s' % (n_samples, alpha))
-    plt.legend(loc='upper left')
-    plt.xlabel('number of features')
-    plt.ylabel('Time (s)')
-    plt.axis('tight')
-    plt.show()
-
-    
-    # Split the dataset in training and test set:
-docs_train, docs_test, y_train, y_test = train_test_split(
-    dataset.data, dataset.target, test_size=0.5)
-    
-        # TASK: Build a vectorizer / classifier pipeline that filters out tokens
-    # that are too rare or too frequent
-    pipeline = Pipeline([
-        ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
-        ('clf', LinearSVC(C=1000)),
-    ])
-    
-    # Plot modifications of calibrator
-for i in range(prediction.shape[0]):
-    plt.arrow(p[i, 0], p[i, 1],
-              prediction[i, 0] - p[i, 0], prediction[i, 1] - p[i, 1],
-              head_width=1e-2, color=colors[np.argmax(p[i])])
-# Plot boundaries of unit simplex
-plt.plot([0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], 'k', label='Simplex')
-    
-    n_samples_range = np.arange(6, 31, 1)
-repeat = 100
-lw_mse = np.zeros((n_samples_range.size, repeat))
-oa_mse = np.zeros((n_samples_range.size, repeat))
-lw_shrinkage = np.zeros((n_samples_range.size, repeat))
-oa_shrinkage = np.zeros((n_samples_range.size, repeat))
-for i, n_samples in enumerate(n_samples_range):
-    for j in range(repeat):
-        X = np.dot(
-            np.random.normal(size=(n_samples, n_features)), coloring_matrix.T)
-    
-            cert = os.path.join(cert_and_key_dir, 'cert')
-        shutil.copy(cert_path, cert)
-        key = os.path.join(cert_and_key_dir, 'key')
-        shutil.copy(key_path, key)
-        if chain_path:
-            chain = os.path.join(cert_and_key_dir, 'chain')
-            shutil.copy(chain_path, chain)
-        else:
-            chain = None
-    
-        # Get options and arguments.
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], _short_options, _options)
-    except getopt.GetoptError as e:
-        log.wtf('''
-    [Fatal] {}.
-    Try '{} --help' for more options.'''.format(e, script_name))
-    
-        return Board(title, list(map(Pin, pin_list)))
-    
-    def kuwo_download_by_rid(rid, output_dir = '.', merge = True, info_only = False):
-    html=get_content('http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_%s'%rid)
-    title=match1(html,r'<name>(.*)</name>')
-    #to get title
-    #format =aac|mp3 ->to get aac format=mp3 ->to get mp3
-    url=get_content('http://antiserver.kuwo.cn/anti.s?format=mp3&rid=MUSIC_%s&type=convert_url&response=url'%rid)
-    songtype, ext, size = url_info(url)
-    print_info(site_info, title, songtype, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir)
-    
-        stream_types = [
-        {'id': '720p HD'},
-        {'id': '360p SD'},
-    ]
-    
-        # ordered list of supported stream types / qualities on this site
-    # order: high quality -> low quality
-    stream_types = [
-        {'id': 'original'}, # contains an 'id' or 'itag' field at minimum
-        {'id': 'small'},
-    ]
-    
-    # Usage: ../../../python bench.py
-    
-        @classmethod
-    def fromfile(cls, fileobj):
-        if fileobj.read(4).decode() != 'TZif':
-            raise ValueError('not a zoneinfo file')
-        fileobj.seek(20)
-        header = fileobj.read(24)
-        tzh = (tzh_ttisgmtcnt, tzh_ttisstdcnt, tzh_leapcnt,
-               tzh_timecnt, tzh_typecnt, tzh_charcnt) = struct.unpack('>6l', header)
-        transitions = array('i')
-        transitions.fromfile(fileobj, tzh_timecnt)
-        if sys.byteorder != 'big':
-            transitions.byteswap()
-    
-    class AnnotationsFutureTestCase(unittest.TestCase):
-    template = dedent(
-        '''
-        from __future__ import annotations
-        def f() -> {ann}:
-            ...
-        def g(arg: {ann}) -> None:
-            ...
-        var: {ann}
-        var2: {ann} = None
-        '''
-    )
-    
-        def abort(self):
-        # What does it mean to 'clear' a document?  Does the
-        # documentElement disappear?
-        raise NotImplementedError(
-            'haven't figured out what this means yet')
-    
-        module = c_warnings
-    
-        def toggle_colorize_event(self, event):
-        if self.after_id:
-            after_id = self.after_id
-            self.after_id = None
-            if DEBUG: print('cancel scheduled recolorizer')
-            self.after_cancel(after_id)
-        if self.allow_colorizing and self.colorizing:
-            if DEBUG: print('stop colorizing')
-            self.stop_colorizing = True
-        self.allow_colorizing = not self.allow_colorizing
-        if self.allow_colorizing and not self.colorizing:
-            self.after_id = self.after(1, self.recolorize)
-        if DEBUG:
-            print('auto colorizing turned',\
-                  self.allow_colorizing and 'on' or 'off')
-        return 'break'
-    
-    @dataclasses.dataclass
-class IV:
-    T_IV4 = dataclasses.InitVar
-    iv0: dataclasses.InitVar[int]
-    iv1: dataclasses.InitVar
-    iv2: T_IV2
-    iv3: T_IV3
-    not_iv4: T_IV4  # When using string annotations, this field is not recognized as an InitVar.
-
-    
-    # Workers are created as daemon threads and processes. This is done to allow the
-# interpreter to exit when there are still idle processes in a
-# ProcessPoolExecutor's process pool (i.e. shutdown() was not called). However,
-# allowing workers to die with the interpreter has two undesirable properties:
-#   - The workers would still be running during interpretor shutdown,
-#     meaning that they would fail in unpredictable ways.
-#   - The workers could be killed while evaluating a work item, which could
-#     be bad if the callable being evaluated has external side-effects e.g.
-#     writing to a file.
-#
-# To work around this problem, an exit handler is installed which tells the
-# workers to exit when their work queues are empty and then waits until the
-# threads/processes finish.
-    
-    # The language for content autogenerated by Sphinx. Refer to documentation
-# for a list of supported languages.
-#language = None
-    
-        def test_map_timeout(self):
-        results = []
+                status_line = 'HTTP/{version} {status} {reason}'.format(
+            version=version,
+            status=original.status,
+            reason=original.reason
+        )
+        headers = [status_line]
         try:
-            for i in self.executor.map(time.sleep,
-                                       [0, 0, 3],
-                                       timeout=1.5):
-                results.append(i)
-        except futures.TimeoutError:
-            pass
-        else:
-            self.fail('expected TimeoutError')
+            # `original.msg` is a `http.client.HTTPMessage` on Python 3
+            # `_headers` is a 2-tuple
+            headers.extend(
+                '%s: %s' % header for header in original.msg._headers)
+        except AttributeError:
+            # and a `httplib.HTTPMessage` on Python 2.x
+            # `headers` is a list of `name: val<CRLF>`.
+            headers.extend(h.strip() for h in original.msg.headers)
+    
+        def get_headers(self):
+        return self.formatting.format_headers(
+            self.msg.headers).encode(self.output_encoding)
     
     
-def FormatDebugInfoResponse_ExtraConfFoundButNotLoaded_test():
-  response = deepcopy( GENERIC_RESPONSE )
-  response[ 'extra_conf' ].update( {
-    'is_loaded': False,
-    'path': '/path/to/extra/conf'
-  } )
-  assert_that(
-    FormatDebugInfoResponse( response ),
-    contains_string(
-      'Extra configuration file found but not loaded\n'
-      'Extra configuration path: /path/to/extra/conf\n'
-    )
-  )
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
+def rst_filenames():
+    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
+        if '.tox' not in root:
+            for filename in fnmatch.filter(filenames, '*.rst'):
+                yield os.path.join(root, filename)
     
     
-  def All_Fields_test( self ):
-    self._Check( 0, {
-      'insertion_text':  'INSERTION TEXT',
-      'menu_text':       'MENU TEXT',
-      'extra_menu_info': 'EXTRA MENU INFO',
-      'kind':            'K',
-      'detailed_info':   'DETAILED INFO',
-      'extra_data': {
-        'doc_string':    'DOC STRING',
-      },
-    }, {
-      'word'     : 'INSERTION TEXT',
-      'abbr'     : 'MENU TEXT',
-      'menu'     : 'EXTRA MENU INFO',
-      'kind'     : 'k',
-      'info'     : 'DETAILED INFO\nDOC STRING',
-      'dup'      : 1,
-      'empty'    : 1,
-      'user_data': '0',
-    } )
+def test_unicode_raw_json_item_verbose(httpbin):
+    r = http('--json', 'POST', httpbin.url + '/post',
+             u'test:={ '%s' : [ '%s' ] }' % (UNICODE, UNICODE))
+    assert HTTP_OK in r
+    assert r.json['json'] == {'test': {UNICODE: [UNICODE]}}
+    
+    output_options.add_argument(
+    '--continue', '-c',
+    dest='download_resume',
+    action='store_true',
+    default=False,
+    help='''
+    Resume an interrupted download. Note that the --output option needs to be
+    specified as well.
+    
+        def has_password(self):
+        return self.value is not None
+    
+        # Adapters
+    def get_transport_plugins(self):
+        return [plugin for plugin in self
+                if issubclass(plugin, TransportPlugin)]
+
+    
+            return plugin.get_auth(**credentials)
