e5a4febe11c773ae863dba5ab8fbb75a112de228
SUNsung <weatherius@gmail.com>
2018-06-06 11:13:44 +0300

    Rewrite [11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py] File in: 2018-06-06 11:13:43

--- pyton/11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py
+++ pyton/11.ToRUqpO..11Ym5UVbBdpo11AzhBCuAo5MY11fZ7.py
@@ -1,169 +1,39 @@
 
         
-        def sort_blocks():
-    # First, we load the current README into memory
-    with open('README.md', 'r') as read_me_file:
-        read_me = read_me_file.read()
-    
-    from absl import flags
-import tensorflow as tf
-    
-    tf_proto_library_py(
-    name = 'spec_pb2',
-    srcs = ['spec.proto'],
-)
-    
-      def testSigmoidCrossEntropyLoss(self):
-    indices = tf.constant([0, 0, 1])
-    gold_labels = tf.constant([0, 1, 2])
-    probs = tf.constant([0.6, 0.7, 0.2])
-    logits = tf.constant([[0.9, -0.3, 0.1], [-0.5, 0.4, 2.0]])
-    cost, correct, total, gold_labels = (
-        component.build_sigmoid_cross_entropy_loss(logits, gold_labels, indices,
-                                                   probs))
-    
-      Returns:
-    Dict mapping from shortened resource path to original resource path.
-  '''
-  for component_spec in master_spec.component:
-    for feature_spec in component_spec.fixed_feature:
-      feature_spec.ClearField('pretrained_embedding_matrix')
-      feature_spec.ClearField('vocab')
-    
-    
-_EXPECTED_CONTEXT = r'''
-input { name: 'word-map' Part { file_pattern: '/tmp/word-map' } }
-input { name: 'tag-map' Part { file_pattern: '/tmp/tag-map' } }
-input { name: 'tag-to-category' Part { file_pattern: '/tmp/tag-to-category' } }
-input { name: 'lcword-map' Part { file_pattern: '/tmp/lcword-map' } }
-input { name: 'category-map' Part { file_pattern: '/tmp/category-map' } }
-input { name: 'char-map' Part { file_pattern: '/tmp/char-map' } }
-input { name: 'char-ngram-map' Part { file_pattern: '/tmp/char-ngram-map' } }
-input { name: 'label-map' Part { file_pattern: '/tmp/label-map' } }
-input { name: 'prefix-table' Part { file_pattern: '/tmp/prefix-table' } }
-input { name: 'suffix-table' Part { file_pattern: '/tmp/suffix-table' } }
-input { name: 'known-word-map' Part { file_pattern: '/tmp/known-word-map' } }
-'''
-    
-    This module provides the capabilities for the Requests hooks system.
-    
-        def run(self):
-        try:
-            self.server_sock = self._create_socket_and_bind()
-            # in case self.port = 0
-            self.port = self.server_sock.getsockname()[1]
-            self.ready_event.set()
-            self._handle_requests()
-    
-    
-class TestCaseInsensitiveDict:
-    
-    
-    {    # Server Error.
-    500: ('internal_server_error', 'server_error', '/o\\', '✗'),
-    501: ('not_implemented',),
-    502: ('bad_gateway',),
-    503: ('service_unavailable', 'unavailable'),
-    504: ('gateway_timeout',),
-    505: ('http_version_not_supported', 'http_version'),
-    506: ('variant_also_negotiates',),
-    507: ('insufficient_storage',),
-    509: ('bandwidth_limit_exceeded', 'bandwidth'),
-    510: ('not_extended',),
-    511: ('network_authentication_required', 'network_auth', 'network_authentication'),
-}
-    
-            # test exceptions
-        no_projects_input_url = 'https://www.googleapis.com/compute/v1/not-projects/myproject/global/backendServices/mybackendservice/getHealth'
-        no_resource_input_url = 'https://www.googleapis.com/compute/v1/not-projects/myproject/global'
-    
-    
-class TerminalModule(TerminalBase):
-    
-        terminal_stdout_re = [
-        re.compile(br'[\r\n]?[\w+\-\.:\/\[\]]+(?:\([^\)]+\)){,3}(?:>|#) ?$'),
-        re.compile(br'\@[\w\-\.]+:\S+?[>#\$] ?$')
-    ]
-    
-        for n_samples in sample_sizes:
-        X = random_state.rand(n_samples, 300)
-    
-    n_samples = np.logspace(.5, 3, 9)
-n_features = np.logspace(1, 3.5, 7)
-N_samples, N_features = np.meshgrid(n_samples,
-                                    n_features)
-scikits_time = np.zeros(N_samples.shape)
-scipy_time = np.zeros(N_samples.shape)
-    
-        from sklearn.tree import DecisionTreeRegressor
-    
-    fit_data = data[np.argsort(model.row_labels_)]
-fit_data = fit_data[:, np.argsort(model.column_labels_)]
-    
-    # Author: Alexandre Gramfort <alexandre.gramfort@inria.fr>
-# License: BSD 3 clause
-    
-        # add non-discriminative features
-    if n_features > 1:
-        X = np.hstack([X, np.random.randn(n_samples, n_features - 1)])
-    return X, y
-    
-        result = False
-    try:
-        ret = urlopen(url, timeout=2)
-        result = (ret.code == 200)
-    except HTTPError as e:
-        print(e, file=sys.stderr)
-    except URLError as e:
-        print(e, file=sys.stderr)
-    except timeout as e:
-        print(e, file=sys.stderr)
-    except Exception as e:
-        print(e, file=sys.stderr)
-    
-        def contribute(self):
-        self.blackboard.common_state['problems'] += random.randint(1, 2)
-        self.blackboard.common_state['suggestions'] += random.randint(10, 20)
-        self.blackboard.common_state['contributions'] += [self.__class__.__name__]
-        self.blackboard.common_state['progress'] += random.randint(10, 100)
-    
-        def test_cat_adapter_shall_make_noise(self):
-        cat = Cat()
-        cat_adapter = Adapter(cat, make_noise=cat.meow)
-        noise = cat_adapter.make_noise()
-        expected_noise = 'meow!'
-        self.assertEqual(noise, expected_noise)
-    
-        if not sample_queue.empty():
-        print(sample_queue.get())
-    
-        def test_display_current_time_at_midnight(self):
+            def get_cookie_httponly(self, app):
+        '''Returns True if the session cookie should be httponly.  This
+        currently just returns the value of the ``SESSION_COOKIE_HTTPONLY``
+        config var.
         '''
-        Will almost always fail (despite of right at/after midnight).
-        '''
-        time_provider_stub = MidnightTimeProvider()
-        class_under_test = TimeDisplay(time_provider_stub)
-        expected_time = '<span class=\'tinyBoldText\'>24:01</span>'
-        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
+        return app.config['SESSION_COOKIE_HTTPONLY']
     
-        x1 = 'x1'
-    x2 = 'x2'
+                    if change_info:
+                    break
     
-    if __name__ == '__main__':
-    main()
-    
-    '''
-http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
-Implementation of the iterator pattern with a generator
-    
-        def setTM(self, tm):
-        self._tm = tm
+    containers = (('thefuck/python3-tcsh',
+               u'''FROM python:3
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'),
+              ('thefuck/python2-tcsh',
+               u'''FROM python:2
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'))
     
     
-def memento(obj, deep=False):
-    state = deepcopy(obj.__dict__) if deep else copy(obj.__dict__)
+class CallState(Enum):
     
-        @data.setter
-    def data(self, value):
-        self._data = value
-        self.notify()
+        def __init__(self):
+        self.lookup = {}  # key: person_id, value: person_server
+    
+            The shuffle/sort step of MapReduce will then do a
+        distributed sort on the keys, resulting in:
+    
+        def __init__(self, level, row, spot_number, spot_size, vehicle_size):
+        self.level = level
+        self.row = row
+        self.spot_number = spot_number
+        self.spot_size = spot_size
+        self.vehicle_size = vehicle_size
+        self.vehicle = None
