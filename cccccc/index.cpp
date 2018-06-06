8d62e8432afa5457bbe17c58f98db88e69bf1c95
SUNsung <weatherius@gmail.com>
2018-06-06 15:34:07 +0300

    Rewrite [index.cpp] File in: 2018-06-06 15:34:06

--- cccccc/index.cpp
+++ cccccc/index.cpp
@@ -1,430 +1,64 @@
 
         
-        #ifndef ATOM_BROWSER_UI_DRAG_UTIL_H_
-#define ATOM_BROWSER_UI_DRAG_UTIL_H_
+        bool js_cocos2dx_builder_CCBReader_constructor(JSContext *cx, uint32_t argc, jsval *vp);
+void js_cocos2dx_builder_CCBReader_finalize(JSContext *cx, JSObject *obj);
+void js_register_cocos2dx_builder_CCBReader(JSContext *cx, JS::HandleObject global);
+void register_all_cocos2dx_builder(JSContext* cx, JS::HandleObject obj);
+bool js_cocos2dx_builder_CCBReader_getAnimationManager(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_setAnimationManager(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_addOwnerOutletName(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_getOwnerCallbackNames(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_addDocumentCallbackControlEvents(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_setCCBRootPath(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_addOwnerOutletNode(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_getOwnerCallbackNodes(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_readSoundKeyframesForSeq(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_getCCBRootPath(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_getOwnerCallbackControlEvents(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_getOwnerOutletNodes(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_readUTF8(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_addOwnerCallbackControlEvents(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_getOwnerOutletNames(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_readCallbackKeyframesForSeq(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_getAnimationManagersForNodes(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_getNodesWithAnimationManagers(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_setResolutionScale(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_builder_CCBReader_CCBReader(JSContext *cx, uint32_t argc, jsval *vp);
     
-    #include 'base/macros.h'
-#include 'v8/include/v8.h'
     
     
-    {  DISALLOW_COPY_AND_ASSIGN(RemoteCallbackFreer);
-};
+    int register_all_cocos2dx_cocosdenshion(lua_State* tolua_S);
     
-    
-    {}  // namespace chrome
-    
-      // The map of accelerators that have been successfully registered as global
-  // shortcuts and their observer.
-  typedef std::map<ui::Accelerator, Observer*> AcceleratorMap;
-  AcceleratorMap accelerator_map_;
-    
-    
-    {}  // namespace printing
-    
-    class PepperIsolatedFileSystemMessageFilter
-    : public ppapi::host::ResourceMessageFilter {
- public:
-  static PepperIsolatedFileSystemMessageFilter* Create(
-      PP_Instance instance,
-      content::BrowserPpapiHost* host);
-    }
-    
-    namespace views {
-class ColorChooserListener;
-}
-    
-    #endif //CONTENT_NW_SRC_API_EVENT_EVENT_H_
+    #endif // __cocos2dx_csloader_h__
 
     
     
-    {
-    {    if (zoom_controller) {
-      double zoom_factor = content::ZoomLevelToZoomFactor(zoom_controller->GetZoomLevel());
-      if (zoom_factor > content::kMaximumZoomFactor) {
-        zoom_factor = content::kMaximumZoomFactor;
-      }
-      if (zoom_factor < content::kMinimumZoomFactor) {
-        zoom_factor = content::kMinimumZoomFactor;
-      }
-      x *= zoom_factor;
-      y *= zoom_factor;
+    
+    
+    {        ok &= luaval_to_number(tolua_S, 2,&arg0, 'cc.PhysicsShape:setFriction');
+        if(!ok)
+        {
+            tolua_error(tolua_S,'invalid arguments in function 'lua_cocos2dx_physics_PhysicsShape_setFriction'', nullptr);
+            return 0;
+        }
+        cobj->setFriction(arg0);
+        lua_settop(tolua_S, 1);
+        return 1;
     }
+    luaL_error(tolua_S, '%s has wrong number of arguments: %d, was expecting %d \n', 'cc.PhysicsShape:setFriction',argc, 1);
+    return 0;
     
-    Popup(x, y, rvh);
-  } else if (method == 'EnableShowEvent') {
-    arguments.GetBoolean(0, &enable_show_event_);
-  } else {
-    NOTREACHED() << 'Invalid call to Menu method:' << method
-                 << ' arguments:' << arguments;
-  }
-}
+    	static Test* Create()
+	{
+		return new Bridge;
+	}
     
-    bool NwAppGetArgvSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
-    }
+    			edge.Set(b2Vec2(-10.0f, 0.0f), b2Vec2(10.0f, 0.0f));
+			body->CreateFixture(&edge, 0.0f);
     
-    bool NwClipboardClearSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
-  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
-  clipboard->Clear(ui::CLIPBOARD_TYPE_COPY_PASTE);
-  return true;
-}
-    
-    namespace extensions {
-    }
-    
-    TessHOcrRenderer::TessHOcrRenderer(const char *outputbase, bool font_info)
-    : TessResultRenderer(outputbase, 'hocr') {
-    font_info_ = font_info;
-}
-    
-      // Compute the number of unichars in the label.
-  GenericVector<UNICHAR_ID> encoding;
-  if (!unicharset.encode_string(label, true, &encoding, nullptr, nullptr)) {
-    tprintf('Not outputting illegal unichar %s\n', label);
-    return;
-  }
-    
-      // Calculate Histogram on GPU
-  OpenclDevice od;
-  if (od.selectedDeviceIsOpenCL() && (num_channels == 1 || num_channels == 4) &&
-      top == 0 && left == 0) {
-    od.HistogramRectOCL((unsigned char*)pixGetData(src_pix), num_channels,
-                        pixGetWpl(src_pix) * 4, left, top, width, height,
-                        kHistogramSize, histogramAllChannels);
-    }
-    
-      edgept = start;
-  do {
-                                 /*single fixed step */
-    if (edgept->flags[FLAGS] & FIXED && edgept->flags[RUNLENGTH] == 1
-                                 /*and neighours free */
-      && edgept->next->flags[FLAGS] & FIXED && (edgept->prev->flags[FLAGS] & FIXED) == 0
-                                 /*same pair of dirs */
-      && (edgept->next->next->flags[FLAGS] & FIXED) == 0 && edgept->prev->flags[DIR] == edgept->next->flags[DIR] && edgept->prev->prev->flags[DIR] == edgept->next->next->flags[DIR]
-    && ((edgept->prev->flags[DIR] - edgept->flags[DIR] + 1) & 7) < 3) {
-                                 /*unfix it */
-      edgept->flags[FLAGS] &= ~FIXED;
-      edgept->next->flags[FLAGS] &= ~FIXED;
-    }
-    edgept = edgept->next;       /*do all points */
-  }
-  while (edgept != start);       /*until finished */
-    
-    static void RunSynchronousUnaryPingPong() {
-  gpr_log(GPR_INFO, 'Running Synchronous Unary Ping Pong');
-    }
-    
-    #include 'test/core/util/test_config.h'
-#include 'test/cpp/qps/benchmark_config.h'
-#include 'test/cpp/qps/driver.h'
-#include 'test/cpp/qps/report.h'
-#include 'test/cpp/qps/server.h'
-#include 'test/cpp/util/test_config.h'
-#include 'test/cpp/util/test_credentials_provider.h'
-    
-    std::string GetDbFileContent(int argc, char** argv) {
-  std::string db_path;
-  std::string arg_str('--db_path');
-  if (argc > 1) {
-    std::string argv_1 = argv[1];
-    size_t start_position = argv_1.find(arg_str);
-    if (start_position != std::string::npos) {
-      start_position += arg_str.size();
-      if (argv_1[start_position] == ' ' ||
-          argv_1[start_position] == '=') {
-        db_path = argv_1.substr(start_position + 1);
-      }
-    }
-  } else {
-    db_path = 'route_guide_db.json';
-  }
-  std::ifstream db_file(db_path);
-  if (!db_file.is_open()) {
-    std::cout << 'Failed to open ' << db_path << std::endl;
-    return '';
-  }
-  std::stringstream db;
-  db << db_file.rdbuf();
-  return db.str();
-}
-    
-    #include <string>
-#include <vector>
-    
-    inline grpc::string GetJSServiceFilename(const grpc::string& filename) {
-  return grpc_generator::StripProto(filename) + '_grpc_pb.js';
-}
-    
-    TEST_F(CodegenTestMinimal, Build) {}
-    
-    UsageTimer::Result UsageTimer::Sample() {
-  Result r;
-  r.wall = Now();
-  get_resource_usage(&r.user, &r.system);
-  r.total_cpu_time = 0;
-  r.idle_cpu_time = 0;
-  get_cpu_usage(&r.total_cpu_time, &r.idle_cpu_time);
-  return r;
-}
-    
-    TEST(LogTest, AlignedEof) {
-  const int n = kBlockSize - 2*kHeaderSize + 4;
-  Write(BigString('foo', n));
-  ASSERT_EQ(kBlockSize - kHeaderSize + 4, WrittenBytes());
-  ASSERT_EQ(BigString('foo', n), Read());
-  ASSERT_EQ('EOF', Read());
-}
-    
-    static void TestEncodeDecode(const VersionEdit& edit) {
-  std::string encoded, encoded2;
-  edit.EncodeTo(&encoded);
-  VersionEdit parsed;
-  Status s = parsed.DecodeFrom(encoded);
-  ASSERT_TRUE(s.ok()) << s.ToString();
-  parsed.EncodeTo(&encoded2);
-  ASSERT_EQ(encoded, encoded2);
-}
-    
-    int main(int argc, char** argv) {
-  return leveldb::test::RunAllTests();
-}
-
-    
-    void BlockBuilder::Add(const Slice& key, const Slice& value) {
-  Slice last_key_piece(last_key_);
-  assert(!finished_);
-  assert(counter_ <= options_->block_restart_interval);
-  assert(buffer_.empty() // No values yet?
-         || options_->comparator->Compare(key, last_key_piece) > 0);
-  size_t shared = 0;
-  if (counter_ < options_->block_restart_interval) {
-    // See how much sharing to do with previous string
-    const size_t min_length = std::min(last_key_piece.size(), key.size());
-    while ((shared < min_length) && (last_key_piece[shared] == key[shared])) {
-      shared++;
-    }
-  } else {
-    // Restart compression
-    restarts_.push_back(buffer_.size());
-    counter_ = 0;
-  }
-  const size_t non_shared = key.size() - shared;
-    }
-    
-    namespace leveldb {
-    }
-    
-      // Check third filter (empty)
-  ASSERT_TRUE(! reader.KeyMayMatch(4100, 'foo'));
-  ASSERT_TRUE(! reader.KeyMayMatch(4100, 'bar'));
-  ASSERT_TRUE(! reader.KeyMayMatch(4100, 'box'));
-  ASSERT_TRUE(! reader.KeyMayMatch(4100, 'hello'));
-    
-    static int NextLength(int length) {
-  if (length < 10) {
-    length += 1;
-  } else if (length < 100) {
-    length += 10;
-  } else if (length < 1000) {
-    length += 100;
-  } else {
-    length += 1000;
-  }
-  return length;
-}
-    
-    void Histogram::Add(double value) {
-  // Linear search is fast enough for our usage in db_bench
-  int b = 0;
-  while (b < kNumBuckets - 1 && kBucketLimit[b] <= value) {
-    b++;
-  }
-  buckets_[b] += 1.0;
-  if (min_ > value) min_ = value;
-  if (max_ < value) max_ = value;
-  num_++;
-  sum_ += value;
-  sum_squares_ += (value * value);
-}
-    
-    #include <string>
-    
-      if (direction_ == kForward) {  // Switch directions?
-    // iter_ is pointing at the current entry.  Scan backwards until
-    // the key changes so we can use the normal reverse scanning code.
-    assert(iter_->Valid());  // Otherwise valid_ would have been false
-    SaveKey(ExtractUserKey(iter_->key()), &saved_key_);
-    while (true) {
-      iter_->Prev();
-      if (!iter_->Valid()) {
-        valid_ = false;
-        saved_key_.clear();
-        ClearSavedValue();
-        return;
-      }
-      if (user_comparator_->Compare(ExtractUserKey(iter_->key()),
-                                    saved_key_) < 0) {
-        break;
-      }
-    }
-    direction_ = kReverse;
-  }
-    
-    int InternalKeyComparator::Compare(const Slice& akey, const Slice& bkey) const {
-  // Order by:
-  //    increasing user key (according to user-supplied comparator)
-  //    decreasing sequence number
-  //    decreasing type (though sequence# should be enough to disambiguate)
-  int r = user_comparator_->Compare(ExtractUserKey(akey), ExtractUserKey(bkey));
-  if (r == 0) {
-    const uint64_t anum = DecodeFixed64(akey.data() + akey.size() - 8);
-    const uint64_t bnum = DecodeFixed64(bkey.data() + bkey.size() - 8);
-    if (anum > bnum) {
-      r = -1;
-    } else if (anum < bnum) {
-      r = +1;
-    }
-  }
-  return r;
-}
-    
-    TEST(FormatTest, InternalKey_EncodeDecode) {
-  const char* keys[] = { '', 'k', 'hello', 'longggggggggggggggggggggg' };
-  const uint64_t seq[] = {
-    1, 2, 3,
-    (1ull << 8) - 1, 1ull << 8, (1ull << 8) + 1,
-    (1ull << 16) - 1, 1ull << 16, (1ull << 16) + 1,
-    (1ull << 32) - 1, 1ull << 32, (1ull << 32) + 1
-  };
-  for (int k = 0; k < sizeof(keys) / sizeof(keys[0]); k++) {
-    for (int s = 0; s < sizeof(seq) / sizeof(seq[0]); s++) {
-      TestKey(keys[k], seq[s], kTypeValue);
-      TestKey('hello', 1, kTypeDeletion);
-    }
-  }
-}
-    
-    namespace leveldb {
-    }
-    
-    /**
- * \fn  std::shared_ptr<xgboost::DMatrix> CreateDMatrix(int rows, int columns, float sparsity, int seed);
- *
- * \brief Creates dmatrix with uniform random data between 0-1.
- *
- * \param rows      The rows.
- * \param columns   The columns.
- * \param sparsity  The sparsity.
- * \param seed      The seed.
- *
- * \return  The new d matrix.
- */
-    
-      std::vector<T>& HostVector();
-  void Reshard(GPUSet devices);
-  void Resize(size_t new_size, T v = T());
-    
-    /*! \brief training parameters */
-struct GBTreeTrainParam : public dmlc::Parameter<GBTreeTrainParam> {
-  /*!
-   * \brief number of parallel trees constructed each iteration
-   *  use this option to support boosted random forest
-   */
-  int num_parallel_tree;
-  /*! \brief tree updater sequence */
-  std::string updater_seq;
-  /*! \brief type of boosting process to run */
-  int process_type;
-  // flag to print out detailed breakdown of runtime
-  int debug_verbose;
-  std::string predictor;
-  // declare parameters
-  DMLC_DECLARE_PARAMETER(GBTreeTrainParam) {
-    DMLC_DECLARE_FIELD(num_parallel_tree)
-        .set_default(1)
-        .set_lower_bound(1)
-        .describe('Number of parallel trees constructed during each iteration.'\
-                  ' This option is used to support boosted random forest');
-    DMLC_DECLARE_FIELD(updater_seq)
-        .set_default('grow_colmaker,prune')
-        .describe('Tree updater sequence.');
-    DMLC_DECLARE_FIELD(process_type)
-        .set_default(kDefault)
-        .add_enum('default', kDefault)
-        .add_enum('update', kUpdate)
-        .describe('Whether to run the normal boosting process that creates new trees,'\
-                  ' or to update the trees in an existing model.');
-    DMLC_DECLARE_FIELD(debug_verbose)
-        .set_lower_bound(0)
-        .set_default(0)
-        .describe('flag to print out detailed breakdown of runtime');
-    // add alias
-    DMLC_DECLARE_ALIAS(updater_seq, updater);
-    DMLC_DECLARE_FIELD(predictor)
-      .set_default('cpu_predictor')
-      .describe('Predictor algorithm type');
-  }
-};
-    
-    
-    {}  // namespace xgboost
-    
-    namespace xgboost {
-namespace obj {
-    }
-    }
-    
-    TEST(Metric, AMS) {
-  EXPECT_ANY_THROW(xgboost::Metric::Create('ams'));
-  xgboost::Metric * metric = xgboost::Metric::Create('ams@0.5f');
-  ASSERT_STREQ(metric->Name(), 'ams@0.5');
-  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 0.311f, 0.001f);
-  EXPECT_NEAR(GetMetricEval(metric,
-                            {0.1f, 0.9f, 0.1f, 0.9f},
-                            {  0,   0,   1,   1}),
-              0.29710f, 0.001f);
-    }
-    
-    
-    {
-    {XGBOOST_REGISTER_GBM(GBLinear, 'gblinear')
-    .describe('Linear booster, implement generalized linear model.')
-    .set_body([](const std::vector<std::shared_ptr<DMatrix> > &cache,
-                 bst_float base_margin) {
-      return new GBLinear(cache, base_margin);
-    });
-}  // namespace gbm
-}  // namespace xgboost
-
-    
-    // convert return from global_ref<T>
-template <typename T>
-struct Convert<global_ref<T>> {
-  typedef JniType<T> jniType;
-  // No automatic synthesis of global_ref
-  static jniType toJniRet(global_ref<jniType> t) {
-    return t.get();
-  }
-  static jniType toCall(global_ref<jniType> t) {
-    return t.get();
-  }
-};
-    
-    #include <fb/visibility.h>
-    
-    void Config::setPointScaleFactor(float pixelsInPoint)
-{
-    YGConfigSetPointScaleFactor(m_config, pixelsInPoint);
-}
-    
-    void setAssertHandler(AssertHandler assertHandler) {
-    gAssertHandler = assertHandler;
-}
-    
-    #else
-# define ALOGV(...) ((void)0)
-# define ALOGD(...) ((void)0)
-# define ALOGI(...) ((void)0)
-# define ALOGW(...) ((void)0)
-# define ALOGE(...) ((void)0)
-# define ALOGF(...) ((void)0)
-#endif
+    				if (i > 0)
+				{
+					b2Vec2 anchor(-5.0f + 1.0f * i, 5.0f);
+					jd.Initialize(prevBody, body, anchor);
+					m_world->CreateJoint(&jd);
+				}
