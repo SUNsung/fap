b6a8c4efd3cb8e31fc8ddcd86a832e66803d731e
SUNsung <weatherius@gmail.com>
2018-06-06 23:34:58 +0300

    Rewrite [28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp] File in: 2018-06-06 23:34:57

--- cccccc/28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp
+++ cccccc/28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp
@@ -1,258 +1,196 @@
 
         
         
-    {
-    {}  // namespace log
-}  // namespace leveldb
     
-    int main(int argc, char** argv) {
-  return leveldb::test::RunAllTests();
+    Menu::Menu(int id,
+           const base::WeakPtr<ObjectManager>& object_manager,
+           const base::DictionaryValue& option,
+           const std::string& extension_id)
+  : Base(id, object_manager, option, extension_id), enable_show_event_(false)  {
+  Create(option);
 }
+    
+     protected:
+  ~NwClipboardSetListSyncFunction() override;
+    
+    
+    {} // namespace extensions
+#endif
 
     
-    uint32_t Hash(const char* data, size_t n, uint32_t seed) {
-  // Similar to murmur hash
-  const uint32_t m = 0xc6a4a793;
-  const uint32_t r = 24;
-  const char* limit = data + n;
-  uint32_t h = seed ^ (n * m);
-    }
+      image_file.read(reinterpret_cast<char*>(&magic), 4);
+  magic = swap_endian(magic);
+  CHECK_EQ(magic, 2051) << 'Incorrect image file magic.';
+  label_file.read(reinterpret_cast<char*>(&magic), 4);
+  magic = swap_endian(magic);
+  CHECK_EQ(magic, 2049) << 'Incorrect label file magic.';
+  image_file.read(reinterpret_cast<char*>(&num_items), 4);
+  num_items = swap_endian(num_items);
+  label_file.read(reinterpret_cast<char*>(&num_labels), 4);
+  num_labels = swap_endian(num_labels);
+  CHECK_EQ(num_items, num_labels);
+  image_file.read(reinterpret_cast<char*>(&rows), 4);
+  rows = swap_endian(rows);
+  image_file.read(reinterpret_cast<char*>(&cols), 4);
+  cols = swap_endian(cols);
     
-    TEST(HASH, SignedUnsignedIssue) {
-  const unsigned char data1[1] = {0x62};
-  const unsigned char data2[2] = {0xc3, 0x97};
-  const unsigned char data3[3] = {0xe2, 0x99, 0xa5};
-  const unsigned char data4[4] = {0xe1, 0x80, 0xb9, 0x32};
-  const unsigned char data5[48] = {
-    0x01, 0xc0, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00,
-    0x14, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x04, 0x00,
-    0x00, 0x00, 0x00, 0x14,
-    0x00, 0x00, 0x00, 0x18,
-    0x28, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00,
-    0x02, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00,
-  };
-    }
+    #include 'glog/logging.h'
+#include 'google/protobuf/text_format.h'
+#include 'stdint.h'
     
-    class SCOPED_LOCKABLE MutexLock {
+    
+template <typename Dtype>
+class LayerRegisterer {
  public:
-  explicit MutexLock(port::Mutex *mu) EXCLUSIVE_LOCK_FUNCTION(mu)
-      : mu_(mu)  {
-    this->mu_->Lock();
+  LayerRegisterer(const string& type,
+                  shared_ptr<Layer<Dtype> > (*creator)(const LayerParameter&)) {
+    // LOG(INFO) << 'Registering layer type: ' << type;
+    LayerRegistry<Dtype>::AddCreator(type, creator);
   }
-  ~MutexLock() UNLOCK_FUNCTION() { this->mu_->Unlock(); }
-    }
-    
-      // Read until size drops significantly.
-  std::string limit_key = Key(n);
-  for (int read = 0; true; read++) {
-    ASSERT_LT(read, 100) << 'Taking too long to compact';
-    Iterator* iter = db_->NewIterator(ReadOptions());
-    for (iter->SeekToFirst();
-         iter->Valid() && iter->key().ToString() < limit_key;
-         iter->Next()) {
-      // Drop data
-    }
-    delete iter;
-    // Wait a little bit to allow any triggered compactions to complete.
-    Env::Default()->SleepForMicroseconds(1000000);
-    uint64_t size = Size(Key(0), Key(n));
-    fprintf(stderr, 'iter %3d => %7.3f MB [other %7.3f MB]\n',
-            read+1, size/1048576.0, Size(Key(n), Key(kCount))/1048576.0);
-    if (size <= initial_size/10) {
-      break;
-    }
-  }
-    
-    void DBIter::SeekToFirst() {
-  direction_ = kForward;
-  ClearSavedValue();
-  iter_->SeekToFirst();
-  if (iter_->Valid()) {
-    FindNextUserEntry(false, &saved_key_ /* temporary storage */);
-  } else {
-    valid_ = false;
-  }
-}
-    
-    void InternalKeyComparator::FindShortestSeparator(
-      std::string* start,
-      const Slice& limit) const {
-  // Attempt to shorten the user portion of the key
-  Slice user_start = ExtractUserKey(*start);
-  Slice user_limit = ExtractUserKey(limit);
-  std::string tmp(user_start.data(), user_start.size());
-  user_comparator_->FindShortestSeparator(&tmp, user_limit);
-  if (tmp.size() < user_start.size() &&
-      user_comparator_->Compare(user_start, tmp) < 0) {
-    // User key has become shorter physically, but larger logically.
-    // Tack on the earliest possible number to the shortened user key.
-    PutFixed64(&tmp, PackSequenceAndType(kMaxSequenceNumber,kValueTypeForSeek));
-    assert(this->Compare(*start, tmp) < 0);
-    assert(this->Compare(tmp, limit) < 0);
-    start->swap(tmp);
-  }
-}
-    
-      // Create a writer that will append data to '*dest'.
-  // '*dest' must have initial length 'dest_length'.
-  // '*dest' must remain live while this Writer is in use.
-  Writer(WritableFile* dest, uint64_t dest_length);
-    
-    bool SetDepth(int p0, HuffmanTree *pool, uint8_t *depth, int max_depth) {
-  int stack[17];
-  int level = 0;
-  int p = p0;
-  assert(max_depth <= 16);
-  stack[0] = -1;
-  while (true) {
-    if (pool[p].index_left_ >= 0) {
-      level++;
-      if (level > max_depth) return false;
-      stack[level] = pool[p].index_right_or_value_;
-      p = pool[p].index_left_;
-      continue;
-    } else {
-      depth[pool[p].index_right_or_value_] = static_cast<uint8_t>(level);
-    }
-    while (level >= 0 && stack[level] == -1) level--;
-    if (level < 0) return true;
-    p = stack[level];
-    stack[level] = -1;
-  }
-}
-    
-    // Computes the DCT (Discrete Cosine Transform) of the 8x8 array in 'block',
-// scaled up by a factor of 16. The values in 'block' are laid out row-by-row
-// and the result is written to the same memory area.
-void ComputeBlockDCT(coeff_t* block);
-    
-    #ifndef GUETZLI_JPEG_DATA_READER_H_
-#define GUETZLI_JPEG_DATA_READER_H_
-    
-    
-    {}  // namespace guetzli
-    
-    
-    {
-    {
-    {
-    {
-    {
-    {            /*
-             * Writing another thread's ThreadEntry from here is fine;
-             * the only other potential reader is the owning thread --
-             * from onThreadExit (which grabs the lock, so is properly
-             * synchronized with us) or from get(), which also grabs
-             * the lock if it needs to resize the elements vector.
-             *
-             * We can't conflict with reads for a get(id), because
-             * it's illegal to call get on a thread local that's
-             * destructing.
-             */
-            e->elements[id].ptr = nullptr;
-            e->elements[id].deleter1 = nullptr;
-            e->elements[id].ownsDeleter = false;
-          }
-        }
-        meta.freeIds_.push_back(id);
-      }
-    }
-    // Delete elements outside the locks.
-    for (ElementWrapper& elem : elements) {
-      elem.dispose(TLPDestructionMode::ALL_THREADS);
-    }
-  } catch (...) { // Just in case we get a lock error or something anyway...
-    LOG(WARNING) << 'Destructor discarding an exception that was thrown.';
-  }
-}
-    
-    struct ExceptionInfo {
-  const std::type_info* type{nullptr};
-  // The values in frames are IP (instruction pointer) addresses.
-  // They are only filled if the low-level exception tracer library is
-  // linked in or LD_PRELOADed.
-  std::vector<uintptr_t> frames; // front() is top of stack
 };
     
+    #include 'caffe/layers/neuron_layer.hpp'
     
-    {    iobuf1->prependChain(IOBuf::create(10));
-    iobuf1->prependChain(IOBuf::create(10));
-    EXPECT_TRUE(c.isAtEnd());
-    iobuf1->prev()->append(5);
-    EXPECT_FALSE(c.isAtEnd());
-    c.skip(5);
-    EXPECT_TRUE(c.isAtEnd());
-  }
     
-    enum class ordering : int { lt = -1, eq = 0, gt = 1 };
-    
-    // Specifically, this adds support for two things:
-// 1) incrementing/decrementing the shared count by more than 1 at a time
-// 2) Getting the thing the shared_ptr points to, which may be different from
-//    the aliased pointer.
-    
-      /**
-   * Set bit idx to the given value, using the given memory order. Returns
-   * the previous value of the bit.
-   *
-   * Note that the operation is a read-modify-write operation due to the use
-   * of fetch_and or fetch_or.
-   *
-   * Yes, this is an overload of set(), to keep as close to std::bitset's
-   * interface as possible.
+    { protected:
+  /**
+   * @param bottom input Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$
+   * @param top output Blob vector (length 1)
+   *   -# @f$ (N \times 1 \times K) @f$ or, if out_max_val
+   *      @f$ (N \times 2 \times K) @f$ unless axis set than e.g.
+   *      @f$ (N \times K \times H \times W) @f$ if axis == 1
+   *      the computed outputs @f$
+   *       y_n = \arg\max\limits_i x_{ni}
+   *      @f$ (for @f$ K = 1 @f$).
    */
-  bool set(size_t idx,
-           bool value,
-           std::memory_order order = std::memory_order_seq_cst);
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  /// @brief Not implemented (non-differentiable function)
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+    NOT_IMPLEMENTED;
+  }
+  bool out_max_val_;
+  size_t top_k_;
+  bool has_axis_;
+  int axis_;
+};
     
-     private:
-  friend class AtomicHashMap;
-  explicit ahm_iterator(ContT* ahm,
-                        uint32_t subMap,
-                        const SubIt& subIt)
-      : ahm_(ahm)
-      , subMap_(subMap)
-      , subIt_(subIt)
-  {}
+      virtual inline const char* type() const { return 'BNLL'; }
     
-    #ifdef __cplusplus
-extern 'C' {
-#endif
-/*
- * VM initialization functions.
- *
- * Note these are the only symbols exported for JNI by the VM.
- */
-jint JNI_GetDefaultJavaVMInitArgs(void*);
-jint JNI_CreateJavaVM(JavaVM**, JNIEnv**, void*);
-jint JNI_GetCreatedJavaVMs(JavaVM**, jsize, jsize*);
+    
+    {  /// when divided by UINT_MAX, the randomly generated values @f$u\sim U(0,1)@f$
+  Blob<unsigned int> rand_vec_;
+  /// the probability @f$ p @f$ of dropping any input
+  Dtype threshold_;
+  /// the scale for undropped inputs at train time @f$ 1 / (1 - p) @f$
+  Dtype scale_;
+  unsigned int uint_thres_;
+};
+    
+    class MatrixTest : public ::testing::Test {
+ protected:
+  // Fills src_ with data so it can pretend to be a tensor thus:
+  //  dims_=[5, 4, 3, 2]
+  //  array_=[0, 1, 2, ....119]
+  //  tensor=[[[[0, 1][2, 3][4, 5]]
+  //           [[6, 7][8, 9][10, 11]]
+  //           [[12, 13][14, 15][16, 17]]
+  //           [[18, 19][20, 21][22, 23]]]
+  //          [[[24, 25]...
+  MatrixTest() {
+    src_.Resize(1, kInputSize_, 0);
+    for (int i = 0; i < kInputSize_; ++i) {
+      src_.put(0, i, i);
+    }
+    for (int i = 0; i < kNumDims_; ++i) dims_[i] = 5 - i;
+  }
+  // Number of dimensions in src_.
+  static const int kNumDims_ = 4;
+  // Number of elements in src_.
+  static const int kInputSize_ = 120;
+  // Size of each dimension in src_;
+  int dims_[kNumDims_];
+  // Input array filled with [0,kInputSize).
+  GENERIC_2D_ARRAY<int> src_;
+};
+    
+    namespace tesseract {
     }
     
-    #include <fb/visibility.h>
-    
-    #pragma once
-    
-        method(setExperimentalFeatureEnabled);
-    method(setPointScaleFactor);
-    
-    template<typename... ARGS>
-inline void logv(const char* tag, const char* msg, ARGS... args) noexcept {
-  log(ANDROID_LOG_VERBOSE, tag, msg, args...);
-}
-    
-      ProgramLocation(const char* functionName, const char* fileName, int line) :
-      m_functionName(functionName),
-      m_fileName(fileName),
-      m_lineNumber(line)
-    {}
-    
-    #include <fb/visibility.h>
+      // A LTRResultIterator goes strictly left-to-right word order.
+  LTRResultIterator ltr_it(resit);
+  ltr_it.RestartRow();
+  if (ltr_it.Empty(RIL_WORD)) return;
+  do {
+    directions->push_back(ltr_it.WordDirection());
+  } while (ltr_it.Next(RIL_WORD) && !ltr_it.IsAtBeginningOf(RIL_TEXTLINE));
     
     
-    {} // namespace detail
+    { private:
+  // A name for this document.
+  STRING document_name_;
+  // A group of pages that corresponds in some loose way to a document.
+  PointerVector<ImageData> pages_;
+  // Page number of the first index in pages_.
+  int pages_offset_;
+  // Total number of pages in document (may exceed size of pages_.)
+  int total_pages_;
+  // Total of all pix sizes in the document.
+  int64_t memory_used_;
+  // Max memory to use at any time.
+  int64_t max_memory_;
+  // Saved reader from LoadDocument to allow re-caching.
+  FileReader reader_;
+  // Mutex that protects pages_ and pages_offset_ against multiple parallel
+  // loads, and provides a wait for page.
+  SVMutex pages_mutex_;
+  // Mutex that protects other data members that callers want to access without
+  // waiting for a load operation.
+  mutable SVMutex general_mutex_;
+};
+    
+    #else // USE_GCC_FAST_TLS
+    
+    constexpr int Vframe::Top;
+    
+    /*
+ * Vasm constant.
+ *
+ * Either a 1, 4, or 8 byte unsigned value, 8 byte double, or the disp32 part
+ * of a thread-local address of an immutable constant that varies by thread.
+ * Constants may also represent an undefined value, indicated by the isUndef
+ * member.
+ *
+ * Also contains convenience constructors for various pointer and enum types.
+ */
+struct Vconst {
+  enum Kind { Quad, Long, Byte, Double };
+    }
+    
+    #ifdef PHP_WIN32
+#include <shlwapi.h>
+#endif
+    
+    void OfflineCode::disasm(FILE* file,
+                         TCA fileStartAddr,
+                         TCA codeStartAddr,
+                         uint64_t codeLen,
+                         const PerfEventsMap<TCA>& perfEvents,
+                         BCMappingInfo bcMappingInfo,
+                         bool printAddr,
+                         bool printBinary) {
+    }
+    
+    /*
+ * Clean up any /tmp files that we created at process shutdown time.
+ */
+void embedded_data_cleanup();
+    
+      typedef std::function<void(pid_t)> LostChildHandler;
+  static void SetLostChildHandler(const LostChildHandler& handler);
+    
+    namespace HPHP {
+    }
