d54f0a750fd9b0e23838d1cb9234b889d0984ccc
SUNsung <weatherius@gmail.com>
2018-06-06 19:47:31 +0300

    Rewrite [21kQH68umkcKM21iFTQVud299Q21QtMQIjl2iso21kPr.cpp] File in: 2018-06-06 19:47:31

--- cccccc/21kQH68umkcKM21iFTQVud299Q21QtMQIjl2iso21kPr.cpp
+++ cccccc/21kQH68umkcKM21iFTQVud299Q21QtMQIjl2iso21kPr.cpp
@@ -1,188 +1,206 @@
 
         
-          static vector<string> LayerTypeList() {
-    CreatorRegistry& registry = Registry();
-    vector<string> layer_types;
-    for (typename CreatorRegistry::iterator iter = registry.begin();
-         iter != registry.end(); ++iter) {
-      layer_types.push_back(iter->first);
-    }
-    return layer_types;
+            // construct the multiplication matrix via schur compliment of the Macaulay
+    // matrix
+    Mtilde = M2_1 - M2_5.t()*M2_4;
+    
+    
+    {            ptr[tid] = partial = partial + ptr[tid + 16];
+            ptr[tid] = partial = partial + ptr[tid + 8];
+            ptr[tid] = partial = partial + ptr[tid + 4];
+            ptr[tid] = partial = partial + ptr[tid + 2];
+            ptr[tid] = partial = partial + ptr[tid + 1];
+        }
+    
+    #define CUSTOM_FUNCTION_ID 1000
+    
+    
+    {  template <typename Tuple>
+  static void TersePrintPrefixToStrings(const Tuple& t, Strings* strings) {
+    ::std::stringstream ss;
+    UniversalTersePrint(::std::tr1::get<0>(t), &ss);
+    strings->push_back(ss.str());
   }
-    
-    
-    {  /**
-   * @brief Computes the error gradient w.r.t. the absolute value inputs.
-   *
-   * @param top output Blob vector (length 1), providing the error gradient with
-   *      respect to the outputs
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
-   *      with respect to computed outputs @f$ y @f$
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$; Backward fills their diff with
-   *      gradients @f$
-   *        \frac{\partial E}{\partial x} =
-   *            \mathrm{sign}(x) \frac{\partial E}{\partial y}
-   *      @f$ if propagate_down[0]
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 };
     
-    /**
- * @brief Index into the input blob along its first axis.
- *
- * This layer can be used to select, reorder, and even replicate examples in a
- * batch.  The second blob is cast to int and treated as an index into the
- * first axis of the first blob.
- */
-template <typename Dtype>
-class BatchReindexLayer : public Layer<Dtype> {
- public:
-  explicit BatchReindexLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
     
-    #endif  // CAFFE_BNLL_LAYER_HPP_
-
+    {}  // namespace testing
     
-    #include <vector>
+    // Makes sure this header is not included before gtest.h.
+#ifndef GTEST_INCLUDE_GTEST_GTEST_H_
+# error Do not include gtest_pred_impl.h directly.  Include gtest.h instead.
+#endif  // GTEST_INCLUDE_GTEST_GTEST_H_
     
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    // This generic version is used when k is 0.
+template <typename T, typename U>
+inline void CopyArray(const T& from, U* to) { *to = from; }
     
-    #include 'caffe/layers/base_conv_layer.hpp'
-    
-    /**
- * @brief Compute elementwise operations, such as product and sum,
- *        along multiple input Blobs.
- *
- * TODO(dox): thorough documentation for Forward, Backward, and proto params.
- */
-template <typename Dtype>
-class EltwiseLayer : public Layer<Dtype> {
- public:
-  explicit EltwiseLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    namespace caffe {
-    }
-    
-     protected:
-  /**
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the computed outputs @f$
-   *        y = \gamma ^ {\alpha x + \beta}
-   *      @f$
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-    #ifndef GRPC_TEST_CPP_UTIL_BENCHMARK_CONFIG_H
-#define GRPC_TEST_CPP_UTIL_BENCHMARK_CONFIG_H
-    
-    /* Opaque object representing a Bidirectional stream creating engine. Created
- * and configured outside of this API to facilitate sharing with other
- * components */
-typedef struct stream_engine {
-  void* obj;
-  void* annotation;
-} stream_engine;
+    // scripts/fuse_gtest.py depends on gtest's own header being #included
+// *unconditionally*.  Therefore these #includes cannot be moved
+// inside #if GTEST_HAS_PARAM_TEST.
+#include 'gtest/internal/gtest-internal.h'
+#include 'gtest/internal/gtest-linked_ptr.h'
+#include 'gtest/internal/gtest-port.h'
+#include 'gtest/gtest-printers.h'
     
     
-    {  GetReporter()->ReportQPSPerCore(*result);
-  GetReporter()->ReportLatency(*result);
+]]
+    
+    
+    {  EXPECT_FALSE(IsPrime(-1));
+  EXPECT_FALSE(IsPrime(-2));
+  EXPECT_FALSE(IsPrime(INT_MIN));
 }
     
-    namespace routeguide {
-    }
-    
-    std::string GetDbFileContent(int argc, char** argv);
-    
-    #include 'src/compiler/config.h'
-#include 'src/compiler/schema_interface.h'
-    
-      grpc::testing::interop::RunServer(
-      grpc::testing::CreateInteropServerCredentials());
+    	GetModuleFileName(NULL, exePath, sizeof(exePath));
     
     
     {
-    {}  // namespace testing
-}  // namespace grpc
+    {// use R's PRNG to replacd
+CustomGlobalRandomEngine::result_type
+CustomGlobalRandomEngine::operator()() {
+  return static_cast<result_type>(
+      std::floor(unif_rand() * CustomGlobalRandomEngine::max()));
+}
+}  // namespace common
+}  // namespace xgboost
+
     
     
-    {  public:
-    DNS(DNSFunc _dnsfunc=NULL);
-    ~DNS();
-    
-  public:
-    bool GetHostByName(const std::string& _host_name, std::vector<std::string>& ips, long millsec = 2 * 1000, DNSBreaker* _breaker = NULL);
-    void Cancel(const std::string& _host_name = std::string());
-    void Cancel(DNSBreaker& _breaker);
-    
-    void SetMonitorFunc(const boost::function<void (int _key)>& _monitor_func) {
-    	monitor_func_ = _monitor_func;
-    }
-  private:
-    DNSFunc dnsfunc_;
-    boost::function<void (int _key)> monitor_func_;
-    static const int kDNSThreadIDError = 0;
+    { private:
+  /*! \brief the underlying stream */
+  dmlc::Stream *stream_;
+  /*! \brief buffer to hold data */
+  std::string buffer_;
+  /*! \brief length of valid data in buffer */
+  size_t read_len_;
+  /*! \brief pointer in the buffer */
+  size_t read_ptr_;
 };
     
-        while (fgets(_strcache, 2048, file)) {
-        if (strstr(_strcache, gs_crash_dump_header) != NULL) {
-            ASSERT(EXPECTS_CRASH_DUMP_HEADER != _state);
+    SEXP XGDMatrixCreateFromCSC_R(SEXP indptr,
+                              SEXP indices,
+                              SEXP data,
+                              SEXP num_row) {
+  SEXP ret;
+  R_API_BEGIN();
+  const int *p_indptr = INTEGER(indptr);
+  const int *p_indices = INTEGER(indices);
+  const double *p_data = REAL(data);
+  size_t nindptr = static_cast<size_t>(length(indptr));
+  size_t ndata = static_cast<size_t>(length(data));
+  size_t nrow = static_cast<size_t>(INTEGER(num_row)[0]);
+  std::vector<size_t> col_ptr_(nindptr);
+  std::vector<unsigned> indices_(ndata);
+  std::vector<float> data_(ndata);
     }
+    
+    
+    {
+    {
+    {      CHECK_EQ(jenv->GetArrayLength(jindex), max_elem)
+          << 'batch.index.length must equal batch.offset.back()';
+      CHECK_EQ(jenv->GetArrayLength(jvalue), max_elem)
+          << 'batch.index.length must equal batch.offset.back()';
+      // cbatch is ready
+      CHECK_EQ((*set_function)(set_function_handle, cbatch), 0)
+          << XGBGetLastError();
+      // release the elements.
+      jenv->ReleaseLongArrayElements(
+          joffset, reinterpret_cast<jlong *>(cbatch.offset), 0);
+      jenv->DeleteLocalRef(joffset);
+      if (jlabel != nullptr) {
+        jenv->ReleaseFloatArrayElements(jlabel, cbatch.label, 0);
+        jenv->DeleteLocalRef(jlabel);
+      }
+      if (jweight != nullptr) {
+        jenv->ReleaseFloatArrayElements(jweight, cbatch.weight, 0);
+        jenv->DeleteLocalRef(jweight);
+      }
+      jenv->ReleaseIntArrayElements(jindex, (jint*) cbatch.index, 0);
+      jenv->DeleteLocalRef(jindex);
+      jenv->ReleaseFloatArrayElements(jvalue, cbatch.value, 0);
+      jenv->DeleteLocalRef(jvalue);
+      jenv->DeleteLocalRef(batch);
+      jenv->DeleteLocalRef(batchClass);
+      ret_value = 1;
+    } else {
+      ret_value = 0;
+    }
+    jenv->DeleteLocalRef(iterClass);
+    // only detach if it is a async call.
+    if (jni_status == JNI_EDETACHED) {
+      global_jvm->DetachCurrentThread();
+    }
+    return ret_value;
+  } catch(dmlc::Error e) {
+    // only detach if it is a async call.
+    if (jni_status == JNI_EDETACHED) {
+      global_jvm->DetachCurrentThread();
+    }
+    LOG(FATAL) << e.what();
+    return -1;
+  }
+}
+    
+    struct GLFWwindow;
+    
+            ID3D10Texture2D *pTexture = NULL;
+        D3D10_SUBRESOURCE_DATA subResource;
+        subResource.pSysMem = pixels;
+        subResource.SysMemPitch = desc.Width * 4;
+        subResource.SysMemSlicePitch = 0;
+        g_pd3dDevice->CreateTexture2D(&desc, &subResource, &pTexture);
+    
+        // Destroy old Framebuffer:
+    for (uint32_t i = 0; i < g_BackBufferCount; i++)
+        if (g_BackBufferView[i])
+            vkDestroyImageView(g_Device, g_BackBufferView[i], g_Allocator);
+    for (uint32_t i = 0; i < g_BackBufferCount; i++)
+        if (g_Framebuffer[i])
+            vkDestroyFramebuffer(g_Device, g_Framebuffer[i], g_Allocator);
+    if (g_RenderPass)
+        vkDestroyRenderPass(g_Device, g_RenderPass, g_Allocator);
+    
+    // Implemented features:
+//  [X] User texture binding. Cast 'GLuint' OpenGL texture identifier as void*/ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
+//  [X] Gamepad navigation mapping. Enable with 'io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad'.
+    
+    MATCHER_P(BufMatches, expected, '') {
+  folly::IOBufEqualTo eq;
+  return eq(*arg, *expected);
+}
+    
+    #include <folly/ConstexprMath.h>
+#include <folly/Portability.h>
+#include <folly/Range.h>
+#include <folly/Utility.h>
+#include <folly/lang/Exception.h>
+#include <folly/lang/Ordering.h>
+#include <folly/portability/Constexpr.h>
+    
+    TEST_F(OrderingTest, ordering) {
+  EXPECT_EQ(-1, int(ordering::lt));
+  EXPECT_EQ(0, int(ordering::eq));
+  EXPECT_EQ(+1, int(ordering::gt));
+}
+    
+    namespace folly {
     }
     
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
+      T take() override {
+    T item;
+    while (!queue_.try_dequeue(item)) {
+      sem_.wait();
+    }
+    return item;
+  }
     
-    #include 'comm/debugger/test_spy_sample.h'
-#include 'comm/xlogger/xlogger.h'
+    namespace folly {
+    }
     
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
-    
-    class DownloadEngine;
-class SocketCore;
-class HttpServer;
-    
-      virtual bool execute() CXX11_OVERRIDE;
-    
-      bool allTiersFailed() const;
-    
-      void nextEvent();
-    
-    private:
-  void startAsyncFamily(const std::string& hostname, int family,
-                        DownloadEngine* e, Command* command);
-  void setNameResolverCheck(size_t resolverIndex, DownloadEngine* e,
-                            Command* command);
-  void disableNameResolverCheck(size_t index, DownloadEngine* e,
-                                Command* command);
+    // Configure folly to enable INFO+ messages, and everything else to
+// enable WARNING+.
+//
+// Set the default log handler to log asynchronously by default.
+FOLLY_INIT_LOGGING_CONFIG('.=WARNING,folly=INFO; default:async=true');
