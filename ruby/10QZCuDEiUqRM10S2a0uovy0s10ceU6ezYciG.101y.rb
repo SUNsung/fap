e73b9065827f069b1caba0c9f26656a256d3bd25
SUNsung <weatherius@gmail.com>
2018-06-06 14:19:49 +0300

    Rewrite [10QZCuDEiUqRM10S2a0uovy0s10ceU6ezYciG.101y.rb] File in: 2018-06-06 14:19:48

--- ruby/10QZCuDEiUqRM10S2a0uovy0s10ceU6ezYciG.101y.rb
+++ ruby/10QZCuDEiUqRM10S2a0uovy0s10ceU6ezYciG.101y.rb
@@ -1,57 +1,66 @@
 
         
-        def file_content_from_hash(input_hash)
-  matter_hash = input_hash.reject { |k, _v| k == 'content' }
-  matter = matter_hash.map do |k, v|
-    '#{k}: #{v}\n'
+          def revoke_admin!
+    set_permission('admin', false)
   end
     
-        def process(args)
-      arg_is_present? args, '--server', 'The --server command has been replaced by the \
-                          'serve' subcommand.'
-      arg_is_present? args, '--serve', 'The --serve command has been replaced by the \
-                          'serve' subcommand.'
-      arg_is_present? args, '--no-server', 'To build Jekyll without launching a server, \
-                          use the 'build' subcommand.'
-      arg_is_present? args, '--auto', 'The switch '--auto' has been replaced with \
-                          '--watch'.'
-      arg_is_present? args, '--no-auto', 'To disable auto-replication, simply leave off \
-                          the '--watch' switch.'
-      arg_is_present? args, '--pygments', 'The 'pygments'settings has been removed in \
-                          favour of 'highlighter'.'
-      arg_is_present? args, '--paginate', 'The 'paginate' setting can only be set in \
-                          your config files.'
-      arg_is_present? args, '--url', 'The 'url' setting can only be set in your \
-                          config files.'
-      no_subcommand(args)
+      class CachedFragment
+    def initialize(json)
+      @json = json
     end
-    
-        def theme_layout_entries
-      theme_layout_directory ? entries_in(theme_layout_directory) : []
+    def as_json(*_args)
+      @json
     end
-    
-    def list_login_items_for_app(app_path)
-  out, err, status = Open3.capture3(
-    '/usr/bin/osascript', '-e',
-    'tell application \'System Events\' to get the name of every login item ' \
-    'whose path contains \'#{File.basename(app_path)}\''
-  )
-  if status.exitstatus > 0
-    $stderr.puts err
-    exit status.exitstatus
   end
-  puts out.gsub(', ', '\n')
-end
     
-            def signal_end_of_output
-          output_pipe.puts(END_OF_OUTPUT_SIGNAL)
-          STDOUT.flush
+          def stack
+        UI::ErrorReport.stack
+      end
+    
+          # @param  [Xcodeproj::Project] project
+      #         The xcode project to generate a podfile for.
+      #
+      # @return [String] the text of the Podfile for the provided project
+      #
+      def podfile_template(project)
+        podfile = ''
+        podfile << 'project '#{@project_path}'\n\n' if @project_path
+        podfile << <<-PLATFORM.strip_heredoc
+          # Uncomment the next line to define a global platform for your project
+          # platform :ios, '9.0'
+        PLATFORM
+    
+            def self.options
+          [
+            ['--template-url=URL', 'The URL of the git repo containing a ' \
+                                  'compatible template'],
+          ].concat(super)
         end
     
-          def update_if_necessary!
-        if @update && config.verbose?
-          UI.section('\nUpdating Spec Repositories\n'.yellow) do
-            Repo.new(ARGV.new(['update'])).run
-          end
+    module RuboCop
+  module Cop
+    class VariableForce
+      # A Variable represents existence of a local variable.
+      # This holds a variable declaration node,
+      # and some states of the variable.
+      class Variable
+        VARIABLE_DECLARATION_TYPES =
+          (VARIABLE_ASSIGNMENT_TYPES + ARGUMENT_DECLARATION_TYPES).freeze
+    
+            def each_unnecessary_space_match(node, &blk)
+          each_match_range(
+            contents_range(node),
+            MULTIPLE_SPACES_BETWEEN_ITEMS_REGEX,
+            &blk
+          )
         end
       end
+    end
+  end
+end
+
+    
+            def autocorrect(node)
+          # Regexp#match can take a second argument, but this cop doesn't
+          # register an offense in that case
+          return unless node.first_argument.regexp_type?
