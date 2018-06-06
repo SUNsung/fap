a51be4b0cff507aa52f7b8c86bbc78adcab249da
SUNsung <weatherius@gmail.com>
2018-06-06 16:06:32 +0300

    Rewrite [293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb] File in: 2018-06-06 16:06:32

--- ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
+++ ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
@@ -1,24 +1,10 @@
 
         
-          # Implemented by subclasses to provide default values for settings needed by
-  # this plugin. Typically done using the `set_if_empty` Capistrano DSL method.
-  #
-  # Example:
-  #
-  #   def set_defaults
-  #     set_if_empty :my_plugin_option, true
-  #   end
-  #
-  def set_defaults; end
-    
-          it 'filters from ENV[HOSTS]' do
-        hosts = dsl.roles(:db)
-        all = dsl.roles(:all)
-        SSHKit::Coordinator.expects(:new).with(hosts).returns(@coordinator)
-        ENV['HOSTS'] = 'example5.com'
-        dsl.on(all)
+            grouped_codes = codes.reduce([]) do |agg, current|
+      if current[1]
+        agg << [current[0]]
+      else
+        agg.last << current[0]
+        agg
       end
-    
-            def scope
-          @stock_location.stock_movements.accessible_by(current_ability, :read)
-        end
+    end
