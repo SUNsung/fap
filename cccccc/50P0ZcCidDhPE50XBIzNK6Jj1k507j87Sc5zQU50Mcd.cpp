84da12260f56d2103510a08fb1e2a2e36f5bcecf
SUNsung <weatherius@gmail.com>
2018-06-07 18:46:58 +0300

    Rewrite [50P0ZcCidDhPE50XBIzNK6Jj1k507j87Sc5zQU50Mcd.cpp] File in: 2018-06-07 18:46:57

--- cccccc/50P0ZcCidDhPE50XBIzNK6Jj1k507j87Sc5zQU50Mcd.cpp
+++ cccccc/50P0ZcCidDhPE50XBIzNK6Jj1k507j87Sc5zQU50Mcd.cpp
@@ -1,280 +1,253 @@
 
         
-        // Here's what happens when an ASSERT_DEATH* or EXPECT_DEATH* is
-// executed:
-//
-//   1. It generates a warning if there is more than one active
-//   thread.  This is because it's safe to fork() or clone() only
-//   when there is a single thread.
-//
-//   2. The parent process clone()s a sub-process and runs the death
-//   test in it; the sub-process exits with code 0 at the end of the
-//   death test, if it hasn't exited already.
-//
-//   3. The parent process waits for the sub-process to terminate.
-//
-//   4. The parent process checks the exit code and error message of
-//   the sub-process.
-//
-// Examples:
-//
-//   ASSERT_DEATH(server.SendMessage(56, 'Hello'), 'Invalid port number');
-//   for (int i = 0; i < 5; i++) {
-//     EXPECT_DEATH(server.ProcessRequest(i),
-//                  'Invalid request .* in ProcessRequest()')
-//                  << 'Failed to die on request ' << i;
-//   }
-//
-//   ASSERT_EXIT(server.ExitNow(), ::testing::ExitedWithCode(0), 'Exiting');
-//
-//   bool KilledBySIGHUP(int exit_code) {
-//     return WIFSIGNALED(exit_code) && WTERMSIG(exit_code) == SIGHUP;
-//   }
-//
-//   ASSERT_EXIT(client.HangUpServer(), KilledBySIGHUP, 'Hanging up!');
-//
-// On the regular expressions used in death tests:
-//
-//   On POSIX-compliant systems (*nix), we use the <regex.h> library,
-//   which uses the POSIX extended regex syntax.
-//
-//   On other platforms (e.g. Windows), we only support a simple regex
-//   syntax implemented as part of Google Test.  This limited
-//   implementation should be enough most of the time when writing
-//   death tests; though it lacks many features you can find in PCRE
-//   or POSIX extended regex syntax.  For example, we don't support
-//   union ('x|y'), grouping ('(xy)'), brackets ('[xy]'), and
-//   repetition count ('x{5,7}'), among others.
-//
-//   Below is the syntax that we do support.  We chose it to be a
-//   subset of both PCRE and POSIX extended regex, so it's easy to
-//   learn wherever you come from.  In the following: 'A' denotes a
-//   literal character, period (.), or a single \\ escape sequence;
-//   'x' and 'y' denote regular expressions; 'm' and 'n' are for
-//   natural numbers.
-//
-//     c     matches any literal character c
-//     \\d   matches any decimal digit
-//     \\D   matches any character that's not a decimal digit
-//     \\f   matches \f
-//     \\n   matches \n
-//     \\r   matches \r
-//     \\s   matches any ASCII whitespace, including \n
-//     \\S   matches any character that's not a whitespace
-//     \\t   matches \t
-//     \\v   matches \v
-//     \\w   matches any letter, _, or decimal digit
-//     \\W   matches any character that \\w doesn't match
-//     \\c   matches any literal character c, which must be a punctuation
-//     .     matches any single character except \n
-//     A?    matches 0 or 1 occurrences of A
-//     A*    matches 0 or many occurrences of A
-//     A+    matches 1 or many occurrences of A
-//     ^     matches the beginning of a string (not that of each line)
-//     $     matches the end of a string (not that of each line)
-//     xy    matches x followed by y
-//
-//   If you accidentally use PCRE or POSIX extended regex features
-//   not implemented by us, you will get a run-time failure.  In that
-//   case, please try to rewrite your regular expression within the
-//   above syntax.
-//
-//   This implementation is *not* meant to be as highly tuned or robust
-//   as a compiled regex library, but should perform well enough for a
-//   death test, which already incurs significant overhead by launching
-//   a child process.
-//
-// Known caveats:
-//
-//   A 'threadsafe' style death test obtains the path to the test
-//   program from argv[0] and re-executes it in the sub-process.  For
-//   simplicity, the current implementation doesn't search the PATH
-//   when launching the sub-process.  This means that the user must
-//   invoke the test program via a path that contains at least one
-//   path separator (e.g. path/to/foo_test and
-//   /absolute/path/to/bar_test are fine, but foo_test is not).  This
-//   is rarely a problem as people usually don't put the test binary
-//   directory in PATH.
-//
-// TODO(wan@google.com): make thread-safe death tests search the PATH.
+        Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-      // The d'tor restores the previous test part result reporter.
-  virtual ~ScopedFakeTestPartResultReporter();
-    
-    // The 'Types' template argument below must have spaces around it
-// since some compilers may choke on '>>' when passing a template
-// instance (e.g. Types<int>)
-# define INSTANTIATE_TYPED_TEST_CASE_P(Prefix, CaseName, Types) \
-  bool gtest_##Prefix##_##CaseName GTEST_ATTRIBUTE_UNUSED_ = \
-      ::testing::internal::TypeParameterizedTestCase<CaseName, \
-          GTEST_CASE_NAMESPACE_(CaseName)::gtest_AllTests_, \
-          ::testing::internal::TypeList< Types >::type>::Register(\
-              #Prefix, #CaseName, GTEST_REGISTERED_TEST_NAMES_(CaseName))
-    
-      // We have to put the 'public' section after the 'private' section,
-  // or MSVC refuses to compile the code.
+    class SYCLDeviceContext : public DeviceContext {
  public:
-  // MSVC warns about implicitly converting from double to int for
-  // possible loss of data, so we need to temporarily disable the
-  // warning.
-#ifdef _MSC_VER
-# pragma warning(push)          // Saves the current warning state.
-# pragma warning(disable:4244)  // Temporarily disables warning 4244.
-    
-    // The Nokia Symbian and IBM XL C/C++ compilers cannot decide between
-// const T& and const T* in a function template.  These compilers
-// _can_ decide between class template specializations for T and T*,
-// so a tr1::type_traits-like is_pointer works.
-#if defined(__SYMBIAN32__) || defined(__IBMCPP__)
-# define GTEST_NEEDS_IS_POINTER_ 1
-#endif
-    
-      // Compares two wide C strings, ignoring case.  Returns true iff they
-  // have the same content.
-  //
-  // Unlike wcscasecmp(), this function can handle NULL argument(s).
-  // A NULL C string is considered different to any non-NULL wide C string,
-  // including the empty string.
-  // NB: The implementations on different platforms slightly differ.
-  // On windows, this method uses _wcsicmp which compares according to LC_CTYPE
-  // environment variable. On GNU platform this method uses wcscasecmp
-  // which compares according to LC_CTYPE category of the current locale.
-  // On MacOS X, it uses towlower, which also uses LC_CTYPE category of the
-  // current locale.
-  static bool CaseInsensitiveWideCStringEquals(const wchar_t* lhs,
-                                               const wchar_t* rhs);
-    
-    #include <limits.h>
-#include 'sample1.h'
-#include 'gtest/gtest.h'
-    
-    #include <gflags/gflags.h>
-#include <grpc/grpc.h>
-#include <grpc/support/time.h>
-    
-    
-    {  // Get all comments (leading, leading_detached, trailing) and print them as a
-  // docstring. Any leading space of a line will be removed, but the line
-  // wrapping will not be changed.
-  void PrintAllComments(std::vector<grpc::string> comments,
-                        grpc_generator::Printer* out);
-};
-    
-    #include 'test/cpp/qps/histogram.h'
-    
-    grpc::string DescribeServiceList(std::vector<grpc::string> service_list,
-                                 grpc::protobuf::DescriptorPool& desc_pool) {
-  std::stringstream result;
-  for (auto it = service_list.begin(); it != service_list.end(); it++) {
-    auto const& service = *it;
-    const grpc::protobuf::ServiceDescriptor* service_desc =
-        desc_pool.FindServiceByName(service);
-    if (service_desc != nullptr) {
-      result << DescribeService(service_desc);
+  SYCLDeviceContext() {}
     }
+    
+    std::ostream& operator<<(std::ostream& out,
+                         const VersionedComputationHandle& versioned_handle) {
+  out << versioned_handle.ToString();
+  return out;
+}
+    
+    void MPIUtils::InitMPI() {
+  // Initialize the MPI environment if that hasn't been done
+  int flag = 0;
+  MPI_CHECK(MPI_Initialized(&flag));
+  if (!flag) {
+    int proc_id = 0, number_of_procs = 1, len = -1;
+    char my_host_name[max_worker_name_length];
+    // MPI_CHECK(MPI_Init_thread(0, 0, MPI_THREAD_MULTIPLE, &flag));
+    MPI_CHECK(MPI_Init(0, 0));
+    MPI_CHECK(MPI_Comm_rank(MPI_COMM_WORLD, &proc_id));
+    MPI_CHECK(MPI_Comm_size(MPI_COMM_WORLD, &number_of_procs));
+    MPI_CHECK(MPI_Get_processor_name(my_host_name, &len));
+    fprintf(stderr,
+            'MPI Environment initialized. Process id: %d Total processes: %d '
+            '|| Hostname: %s \n',
+            proc_id, number_of_procs, my_host_name);
   }
-  return result.str();
+}
+    
+    Status PowGrad(const AttrSlice& attrs, FunctionDef* g) {
+  // clang-format off
+  std::vector<FDH::Node> nodes = {
+    {{'z'}, 'Pow', {'x', 'y'}},
+    // dz * y * Pow(x, y - 1)
+    FDH::Const('const_zero', 0.0f),
+    FDH::Const('const_one', 1.0f),
+    {{'zero'}, 'Cast', {'const_zero'}, {{'SrcT', DT_FLOAT}, {'DstT', '$T'}}},
+    {{'one'}, 'Cast', {'const_one'}, {{'SrcT', DT_FLOAT}, {'DstT', '$T'}}},
+    {{'t0'}, 'Sub', {'y', 'one'}, {}, {'dz'}},
+    {{'t1'}, 'Pow', {'x', 't0'}},
+    {{'t2'}, 'Mul', {'dz', 'y'}},
+    {{'gx'}, 'Mul', {'t1', 't2'}},
+    {{'unsafe_log'}, 'Log', {'x'}, {}, {'dz'}},
+    {{'zeros'}, 'ZerosLike', {'x'}}};
+  // clang-format on
+  std::vector<FDH::Node> log_x_handling;
+  DataType T;
+  TF_RETURN_IF_ERROR(GetNodeAttr(attrs, 'T', &T));
+  if (T == DT_COMPLEX64 || T == DT_COMPLEX128) {
+    // dz * z * (x != 0 ? Log(x) : 0)
+    // clang-format off
+    log_x_handling = {
+      {{'nz_x'}, 'NotEqual', {'x', 'zero'}},
+      {{'safe_log'}, 'Select', {'nz_x', 'unsafe_log', 'zeros'}}};
+    // clang-format on
+  } else {
+    // dz * z * (x > 0 ? Log(x) : 0)
+    // clang-format off
+    log_x_handling = {
+      {{'pos_x'}, 'Greater', {'x', 'zero'}},
+      {{'safe_log'}, 'Select', {'pos_x', 'unsafe_log', 'zeros'}}};
+    // clang-format on
+  }
+  nodes.insert(nodes.end(), log_x_handling.begin(), log_x_handling.end());
+  nodes.push_back({{'t4'}, 'Mul', {'dz', 'z'}});
+  nodes.push_back({{'gy'}, 'Mul', {'safe_log', 't4'}});
+  return GradForBinaryCwise(g, nodes);
+}
+REGISTER_OP_GRADIENT('Pow', PowGrad);
+    
+    #include <atomic>
+#include <unordered_set>
+    
+    TEST_F(UnicharcompressTest, DoesChinese) {
+  LOG(INFO) << 'Testing chi_tra';
+  LoadUnicharset('chi_tra.unicharset');
+  ExpectCorrect('chi_tra');
+  LOG(INFO) << 'Testing chi_sim';
+  LoadUnicharset('chi_sim.unicharset');
+  ExpectCorrect('chi_sim');
 }
     
     
-    {}  // namespace grpc
-    
-    #endif  // LLVM_FUZZER_CORPUS
-
-    
-    #include 'FuzzerExtFunctions.h'
-#include 'FuzzerIO.h'
-#include <dlfcn.h>
-    
-    #include 'FuzzerExtFunctions.h'
-#include 'FuzzerIO.h'
-    
-    #include 'FuzzerIO.h'
-#include 'FuzzerDefs.h'
-#include 'FuzzerExtFunctions.h'
-#include <algorithm>
-#include <cstdarg>
-#include <fstream>
-#include <iterator>
-#include <sys/stat.h>
-#include <sys/types.h>
-    
-    
-    {}  // namespace fuzzer
-    
-    private:
-  void AlarmCallback();
-  void CrashCallback();
-  void InterruptCallback();
-  void MutateAndTestOne();
-  void ReportNewCoverage(InputInfo *II, const Unit &U);
-  size_t RunOne(const Unit &U) { return RunOne(U.data(), U.size()); }
-  void WriteToOutputCorpus(const Unit &U);
-  void WriteUnitToFileWithPrefix(const Unit &U, const char *Prefix);
-  void PrintStats(const char *Where, const char *End = '\n', size_t Units = 0);
-  void PrintStatusForNewUnit(const Unit &U);
-  void ShuffleCorpus(UnitVector *V);
-  void AddToCorpus(const Unit &U);
-  void CheckExitOnSrcPosOrItem();
-    
-      static const size_t kCmpDictionaryEntriesDequeSize = 16;
-  DictionaryEntry CmpDictionaryEntriesDeque[kCmpDictionaryEntriesDequeSize];
-  size_t CmpDictionaryEntriesDequeIdx = 0;
-    
-    struct FuzzingOptions {
-  int Verbosity = 1;
-  size_t MaxLen = 0;
-  int UnitTimeoutSec = 300;
-  int TimeoutExitCode = 77;
-  int ErrorExitCode = 77;
-  int MaxTotalTimeSec = 0;
-  int RssLimitMb = 0;
-  bool DoCrossOver = true;
-  int MutateDepth = 5;
-  bool UseCounters = false;
-  bool UseIndirCalls = true;
-  bool UseMemcmp = true;
-  bool UseMemmem = true;
-  bool UseCmp = false;
-  bool UseValueProfile = false;
-  bool Shrink = false;
-  int ReloadIntervalSec = 1;
-  bool ShuffleAtStartUp = true;
-  bool PreferSmall = true;
-  size_t MaxNumberOfRuns = -1L;
-  int ReportSlowUnits = 10;
-  bool OnlyASCII = false;
-  std::string OutputCorpus;
-  std::string ArtifactPrefix = './';
-  std::string ExactArtifactPath;
-  std::string ExitOnSrcPos;
-  std::string ExitOnItem;
-  bool SaveArtifacts = true;
-  bool PrintNEW = true; // Print a status line when new units are found;
-  bool OutputCSV = false;
-  bool PrintNewCovPcs = false;
-  bool PrintFinalStats = false;
-  bool PrintCorpusStats = false;
-  bool PrintCoverage = false;
-  bool DumpCoverage = false;
-  bool DetectLeaks = true;
-  int  TraceMalloc = 0;
-  bool HandleAbrt = false;
-  bool HandleBus = false;
-  bool HandleFpe = false;
-  bool HandleIll = false;
-  bool HandleInt = false;
-  bool HandleSegv = false;
-  bool HandleTerm = false;
+    {  // Gate weight arrays of size [na + 1, no].
+  WeightMatrix gate_weights_[WT_COUNT];
+  // Used only if this is a softmax LSTM.
+  FullyConnected* softmax_;
+  // Input padded with previous output of size [width, na].
+  NetworkIO source_;
+  // Internal state used during forward operation, of size [width, ns].
+  NetworkIO state_;
+  // State of the 2-d maxpool, generated during forward, used during backward.
+  GENERIC_2D_ARRAY<int8_t> which_fg_;
+  // Internal state saved from forward, but used only during backward.
+  NetworkIO node_values_[WT_COUNT];
+  // Preserved input stride_map used for Backward when NT_LSTM_SQUASHED.
+  StrideMap input_map_;
+  int input_width_;
 };
     
-    #endif  // LLVM_FUZZER_SHA1_H
-
+    struct OSBestResult {
+  OSBestResult() : orientation_id(0), script_id(0), sconfidence(0.0),
+                   oconfidence(0.0) {}
+  int orientation_id;
+  int script_id;
+  float sconfidence;
+  float oconfidence;
+};
     
-    static std::mutex SignalMutex;
-// Global variables used to keep track of how signal handling should be
-// restored. They should **not** be accessed without holding `SignalMutex`.
-static int ActiveThreadCount = 0;
-static struct sigaction OldSigIntAction;
-static struct sigaction OldSigQuitAction;
-static sigset_t OldBlockedSignalsSet;
+      // Update the other members if the cost is lower.
+  void UpdateIfBetter(int64_t cost, int32_t steps, const DPPoint* prev,
+                      int32_t n, int32_t sig_x, int64_t sig_xsq);
+    
+      // Return whether a given text line could be a first paragraph line according
+  // to this paragraph model.
+  bool ValidFirstLine(int lmargin, int lindent, int rindent, int rmargin) const;
+    
+    // This file contains types that are used both by the API and internally
+// to Tesseract. In order to decouple the API from Tesseract and prevent cyclic
+// dependencies, THIS FILE SHOULD NOT DEPEND ON ANY OTHER PART OF TESSERACT.
+// Restated: It is OK for low-level Tesseract files to include publictypes.h,
+// but not for the low-level tesseract code to include top-level API code.
+// This file should not use other Tesseract types, as that would drag
+// their includes into the API-level.
+// API-level code should include apitypes.h in preference to this file.
+    
+      assert(peekType(msg) == thpp::Type::FLOAT);
+  double arg1 = unpackFloat(msg);
+  assert(arg1 == 1.0);
+    
+    #include <THPP/tensors/THTensor.hpp>
+    
+    #undef THStoragePtr
+#undef THPStoragePtr
+#undef THTensorPtr
+#undef THPTensorPtr
+    
+    #define THPDoubleStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPDoubleStorageClass)
+#define THPFloatStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPFloatStorageClass)
+#define THPHalfStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPFloatStorageClass)
+#define THPLongStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPLongStorageClass)
+#define THPIntStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPIntStorageClass)
+#define THPShortStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPShortStorageClass)
+#define THPCharStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPCharStorageClass)
+#define THPByteStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPByteStorageClass)
+    
+    bool object_exists(const char *name) {
+  int fd = shm_open(name, O_RDONLY, 0);
+  if (fd >= 0) {
+    close(fd);
+    return true;
+  } else {
+    return false;
+  }
+}
+    
+    void THDTensor_(bernoulli_FloatTensor)(THDTensor *self, THDGenerator *_generator,
+                                       THDFloatTensor *p) {
+  masterCommandChannel->sendMessage(
+    packMessage(Functions::tensorBernoulli_FloatTensor, self, _generator, p),
+    THDState::s_current_worker
+  );
+}
+    
+      /**
+   * Construct a new string by replacing the characters denoted by the half-open
+   *   range [`first`,`last`) within this string with the characters from string
+   *   `that` starting at position `that_pos`.
+   * \pre `that_pos <= that.size()`
+   * \note Equivalent to
+   *   <tt>creplace(first - data(), last - first, that, that_pos,
+   *   that.size() - that_pos)</tt>
+   */
+  template <std::size_t M>
+  constexpr BasicFixedString<Char, N + M> creplace(
+      const Char* first,
+      const Char* last,
+      const BasicFixedString<Char, M>& that,
+      std::size_t that_pos = 0u) const noexcept(false) {
+    return creplace(
+        first - data_,
+        last - first,
+        that,
+        that_pos,
+        that.size_ - detail::fixedstring::checkOverflow(that_pos, that.size_));
+  }
+    
+      size_t sizeGuess() const {
+    size_t size = 0;
+    for (auto& q : queues_) {
+      size += q.sizeGuess();
+    }
+    return size;
+  }
+    
+      folly::Optional<T> try_take_for(std::chrono::milliseconds time) override {
+    T item;
+    while (!queue_.try_dequeue(item)) {
+      if (!sem_.try_wait_for(time)) {
+        return folly::none;
+      }
+    }
+    return std::move(item);
+  }
+    
+      size_type erase(const key_type& key) {
+    iterator it = find(key);
+    if (it == end()) {
+      return 0;
+    }
+    m_.cont_.erase(it);
+    return 1;
+  }
+    
+    TEST(SortedVectorTypes, BadHints) {
+  for (int toInsert = -1; toInsert <= 7; ++toInsert) {
+    for (int hintPos = 0; hintPos <= 4; ++hintPos) {
+      sorted_vector_set<int> s;
+      for (int i = 0; i <= 3; ++i) {
+        s.insert(i * 2);
+      }
+      s.insert(s.begin() + hintPos, toInsert);
+      size_t expectedSize = (toInsert % 2) == 0 ? 4 : 5;
+      EXPECT_EQ(s.size(), expectedSize);
+      check_invariant(s);
+    }
+  }
+}
+    
+          idled = false;
+    } else {
+      std::chrono::steady_clock::duration idleTimeout =
+          MemoryIdler::defaultIdleTimeout.load(std::memory_order_acquire);
+    
+    #include <atomic>
