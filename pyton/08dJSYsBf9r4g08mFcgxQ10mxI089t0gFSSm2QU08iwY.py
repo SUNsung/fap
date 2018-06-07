65579a032ab39857be9bbe3539e0e024b6974f03
SUNsung <weatherius@gmail.com>
2018-06-07 22:35:25 +0300

    Rewrite [08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py] File in: 2018-06-07 22:35:25

--- pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
+++ pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
@@ -1,153 +1,213 @@
 
         
-        entry_template = textwrap.dedent('''
-    <entry>
-        <id>https://yt-dl.org/feed/youtube-dl-updates-feed/youtube-dl-@VERSION@</id>
-        <title>New version @VERSION@</title>
-        <link href='http://rg3.github.io/youtube-dl' />
-        <content type='xhtml'>
-            <div xmlns='http://www.w3.org/1999/xhtml'>
-                Downloads available at <a href='https://yt-dl.org/downloads/@VERSION@/'>https://yt-dl.org/downloads/@VERSION@/</a>
-            </div>
-        </content>
-        <author>
-            <name>The youtube-dl maintainers</name>
-        </author>
-        <updated>@TIMESTAMP@</updated>
-    </entry>
-    ''')
+        
+if len(sys.argv) <= 1:
+    print('Specify the version number as parameter')
+    sys.exit()
+version = sys.argv[1]
     
-        with io.open(infile, encoding='utf-8') as inf:
-        issue_template_tmpl = inf.read()
-    
-    from youtube_dl.compat import (
-    compat_print,
-    compat_urllib_request,
-)
-from youtube_dl.utils import format_bytes
-    
-    
-class TestAgeRestriction(unittest.TestCase):
-    def _assert_restricted(self, url, filename, age, old_age=None):
-        self.assertTrue(_download_restricted(url, filename, old_age))
-        self.assertFalse(_download_restricted(url, filename, age))
-    
-        def test_facebook_matching(self):
-        self.assertTrue(FacebookIE.suitable('https://www.facebook.com/Shiniknoh#!/photo.php?v=10153317450565268'))
-        self.assertTrue(FacebookIE.suitable('https://www.facebook.com/cindyweather?fref=ts#!/photo.php?v=10152183998945793'))
-    
-        def tearDown(self):
-        if os.path.exists(self.test_dir):
-            shutil.rmtree(self.test_dir)
-    
-            select = self._search_regex(
-            r'(?s)<select[^>]+id='select-version'[^>]*>(.+?)</select>',
-            webpage, 'select version', default=None)
-        if select:
-            entry_ids = set()
-            entries = []
-            for mobj in re.finditer(
-                    r'<option[^>]+value=(['\'])(?P<id>[0-9a-z_]+)(?:#.+?)?\1[^>]*>(?P<title>[^<]+)',
-                    webpage):
-                entry_id = mobj.group('id')
-                if entry_id in entry_ids:
-                    continue
-                entry_ids.add(entry_id)
-                entries.append({
-                    '_type': 'url_transparent',
-                    'url': 'kaltura:%s:%s' % (partner_id, entry_id),
-                    'ie_key': 'Kaltura',
-                    'title': mobj.group('title'),
-                })
-            if entries:
-                return self.playlist_result(entries, display_id, title)
-    
-            title = metadata['Title']
-        description = metadata.get('Description')
-        duration = float_or_none(metadata.get('Duration'))
-        timestamp = parse_iso8601(metadata.get('DateCreated'))
-    
-    cc_test(
-    name = 'spanning_tree_iterator_test',
-    size = 'small',
-    srcs = ['spanning_tree_iterator_test.cc'],
-    deps = [
-        ':spanning_tree_iterator',
-        '//syntaxnet:test_main',
-        '@org_tensorflow//tensorflow/core:lib',
-        '@org_tensorflow//tensorflow/core:test',
-    ],
-)
-    
-    py_library(
-    name = 'dragnn_ops',
-    srcs = ['dragnn_ops.py'],
-    deps = [
-        ':load_dragnn_cc_impl_py',
-        '//dragnn/core:dragnn_bulk_ops',
-        '//dragnn/core:dragnn_ops',
-        '//syntaxnet:load_parser_ops_py',
-    ],
-)
-    
-      def testSoftmaxCrossEntropyLoss(self):
-    logits = tf.constant([[0.0, 2.0, -1.0],
-                          [-5.0, 1.0, -1.0],
-                          [3.0, 1.0, -2.0]])  # pyformat: disable
-    gold_labels = tf.constant([1, -1, 1])
-    cost, correct, total, logits, gold_labels = (
-        component.build_softmax_cross_entropy_loss(logits, gold_labels))
-    
-      def testLabelPotentialsFromTokens(self):
-    with self.test_session():
-      tokens = tf.constant([[[1, 2],
-                             [3, 4],
-                             [5, 6]],
-                            [[6, 5],
-                             [4, 3],
-                             [2, 1]]], tf.float32)  # pyformat: disable
-    
-    
-def main(unused_argv):
-  # Run the exporter.
-  export(FLAGS.master_spec, FLAGS.params_path, FLAGS.export_path,
-         FLAGS.export_moving_averages, FLAGS.build_runtime_graph)
-  tf.logging.info('Export complete.')
-    
-        try:
-      with open(FLAGS.actual_file) as actual:
-        content_actual = actual.read()
-    except IOError as e:
-      self.fail('Error opening '%s': %s' % (FLAGS.actual_file, e.strerror))
-    
-            print('Iteration %s of %s' % (i, n_iter))
-    
-    
-def variable_batch_size_comparison(data):
-    batch_sizes = [i.astype(int) for i in np.linspace(data.shape[0] // 10,
-                                                      data.shape[0], num=10)]
-    
-    for i, n in enumerate(n_samples):
-    for j, p in enumerate(n_features):
-        X = np.random.normal(size=(n, p))
-        t0 = time.time()
-        ward.fit(X)
-        scikits_time[j, i] = time.time() - t0
-        t0 = time.time()
-        hierarchy.ward(X)
-        scipy_time[j, i] = time.time() - t0
-    
-    import gc
-import sys
+    import io
 import optparse
-from datetime import datetime
-import operator
     
-    acc_clf1, acc_clf2 = [], []
-n_features_range = range(1, n_features_max + 1, step)
-for n_features in n_features_range:
-    score_clf1, score_clf2 = 0, 0
-    for _ in range(n_averages):
-        X, y = generate_data(n_train, n_features)
+        # Are checkable fields missing from the test case definition?
+    test_info_dict = dict((key, value if not isinstance(value, compat_str) or len(value) < 250 else 'md5:' + md5(value))
+                          for key, value in got_dict.items()
+                          if value and key in ('id', 'title', 'description', 'uploader', 'upload_date', 'timestamp', 'uploader_id', 'location', 'age_limit'))
+    missing_keys = set(test_info_dict.keys()) - set(expected_dict.keys())
+    if missing_keys:
+        def _repr(v):
+            if isinstance(v, compat_str):
+                return ''%s'' % v.replace('\\', '\\\\').replace(''', '\\'').replace('\n', '\\n')
+            else:
+                return repr(v)
+        info_dict_str = ''
+        if len(missing_keys) != len(expected_dict):
+            info_dict_str += ''.join(
+                '    %s: %s,\n' % (_repr(k), _repr(v))
+                for k, v in test_info_dict.items() if k not in missing_keys)
     
-    plt.show()
+            self.port = random.randint(20000, 30000)
+        self.server_process = subprocess.Popen([
+            'srelay', '-f', '-i', '127.0.0.1:%d' % self.port],
+            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+            info_dict = self._extract_from_id(mvp_id, webpage)
+        info_dict['display_id'] = display_id
+    
+    
+class CloserToTruthIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)?closertotruth\.com/(?:[^/]+/)*(?P<id>[^/?#&]+)'
+    _TESTS = [{
+        'url': 'http://closertotruth.com/series/solutions-the-mind-body-problem#video-3688',
+        'info_dict': {
+            'id': '0_zof1ktre',
+            'display_id': 'solutions-the-mind-body-problem',
+            'ext': 'mov',
+            'title': 'Solutions to the Mind-Body Problem?',
+            'upload_date': '20140221',
+            'timestamp': 1392956007,
+            'uploader_id': 'CTTXML'
+        },
+        'params': {
+            'skip_download': True,
+        },
+    }, {
+        'url': 'http://closertotruth.com/episodes/how-do-brains-work',
+        'info_dict': {
+            'id': '0_iuxai6g6',
+            'display_id': 'how-do-brains-work',
+            'ext': 'mov',
+            'title': 'How do Brains Work?',
+            'upload_date': '20140221',
+            'timestamp': 1392956024,
+            'uploader_id': 'CTTXML'
+        },
+        'params': {
+            'skip_download': True,
+        },
+    }, {
+        'url': 'http://closertotruth.com/interviews/1725',
+        'info_dict': {
+            'id': '1725',
+            'title': 'AyaFr-002',
+        },
+        'playlist_mincount': 2,
+    }]
+    
+        def to_python(self, value):
+        key = next(iter(value))
+        return {key[:-2]: value[key]}
+    
+            app.config['SESSION_COOKIE_DOMAIN'] = rv
+        return rv
+    
+        def dispatch_request(self, *args, **kwargs):
+        meth = getattr(self, request.method.lower(), None)
+    
+    
+def test_session_lifetime():
+    app = flask.Flask(__name__)
+    app.config['PERMANENT_SESSION_LIFETIME'] = 42
+    assert app.permanent_session_lifetime.seconds == 42
+    
+    import flask
+from flask.sessions import SessionInterface
+    
+    try:
+    import blinker
+except ImportError:
+    blinker = None
+    
+            self.tail.appendleft(delta)
+        self.lasttime = now
+        self.concurrent += 1
+    
+        def __enter__(self):
+        from scrapy.utils.test import get_testenv
+        pargs = [sys.executable, '-u', '-m', 'scrapy.utils.benchserver']
+        self.proc = subprocess.Popen(pargs, stdout=subprocess.PIPE,
+                                     env=get_testenv())
+        self.proc.stdout.readline()
+    
+    __all__ = ['cbs_download']
+    
+    	type, ext, size = url_info(url)
+	print_info(site_info, title, type, size)
+	
+	if not info_only:
+		download_urls([url], title, ext, size, output_dir, merge = merge)
+    
+            if s: self.streams['video'] = {'url': s }
+        if mp3: self.streams['audio'] = { 'url': mp3 }
+        if pdf: self.streams['slides'] = { 'url': pdf }
+    
+    def kugou_download_by_hash(title,hash_val,output_dir = '.', merge = True, info_only = False):
+    #sample
+    #url_sample:http://www.kugou.com/yy/album/single/536957.html
+    #hash ->key  md5(hash+kgcloud')->key  decompile swf
+    #cmd 4 for mp3 cmd 3 for m4a
+    key=hashlib.new('md5',(hash_val+'kgcloud').encode('utf-8')).hexdigest()
+    html=get_html('http://trackercdn.kugou.com/i/?pid=6&key=%s&acceptMp3=1&cmd=4&hash=%s'%(key,hash_val))
+    j=loads(html)
+    url=j['url']
+    songtype, ext, size = url_info(url)
+    print_info(site_info, title, songtype, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir, merge=merge)
+    
+    def kuwo_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+    if 'www.kuwo.cn/yinyue' in url:
+        rid=match1(url,'yinyue/(\d+)')
+        kuwo_download_by_rid(rid,output_dir, merge, info_only)
+    else:
+        kuwo_playlist_download(url,output_dir,merge,info_only)
+    
+    
+#----------------------------------------------------------------------
+def showroom_download(url, output_dir = '.', merge = False, info_only = False, **kwargs):
+    ''''''
+    if re.match( r'(\w+)://www.showroom-live.com/([-\w]+)', url):
+        room_url_key = match1(url, r'\w+://www.showroom-live.com/([-\w]+)')
+        room_id = showroom_get_roomid_by_room_url_key(room_url_key)
+        showroom_download_by_room_id(room_id, output_dir, merge,
+                                    info_only)
+    
+    
+def main():
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([
+        (r'/', MainHandler),
+    ])
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+    
+            add_accept_handler(listener, accept_callback)
+        self.client_stream = IOStream(socket.socket())
+        self.addCleanup(self.client_stream.close)
+        yield [self.client_stream.connect(('127.0.0.1', port)),
+               event.wait()]
+        self.io_loop.remove_handler(listener)
+        listener.close()
+    
+          wait_iterator = gen.WaitIterator(future1, future2)
+      while not wait_iterator.done():
+          try:
+              result = yield wait_iterator.next()
+          except Exception as e:
+              print('Error {} from {}'.format(e, wait_iterator.current_future))
+          else:
+              print('Result {} received from {} at {}'.format(
+                  result, wait_iterator.current_future,
+                  wait_iterator.current_index))
+    
+        def __str__(self):
+        return 'HTTP %d: %s' % (self.code, self.message)
+    
+            Address may be either an IP address or hostname.  If it's a hostname,
+        the server will listen on all IP addresses associated with the
+        name.  Address may be an empty string or None to listen on all
+        available interfaces.  Family may be set to either `socket.AF_INET`
+        or `socket.AF_INET6` to restrict to IPv4 or IPv6 addresses, otherwise
+        both will be used if available.
+    
+        The unittest framework is synchronous, so the test must be
+    complete by the time the test method returns. This means that
+    asynchronous code cannot be used in quite the same way as usual
+    and must be adapted to fit. To write your tests with coroutines,
+    decorate your test methods with `tornado.testing.gen_test` instead
+    of `tornado.gen.coroutine`.
+    
+        .. versionchanged:: 5.0
+       The ``io_loop`` argument (deprecated since version 4.1) has been removed.
+    '''
+    def initialize(self):
+        # partial copy of twisted.names.client.createResolver, which doesn't
+        # allow for a reactor to be passed in.
+        self.reactor = tornado.platform.twisted.TornadoReactor()
+    
+    * If you're writing an asynchronous library that doesn't rely on a
+  stack_context-aware library like `tornado.ioloop` or `tornado.iostream`
+  (for example, if you're writing a thread pool), use
+  `.stack_context.wrap()` before any asynchronous operations to capture the
+  stack context from where the operation was started.
