74c25ca13f3d9353f568274b3c701e8e4646b358
SUNsung <weatherius@gmail.com>
2018-06-07 14:10:34 +0300

    Rewrite [211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb] File in: 2018-06-07 14:10:33

--- ruby/211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb
+++ ruby/211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb
@@ -1,187 +1,51 @@
 
         
-          # True if a {Formula} is being built in 32-bit/x86 mode.
-  # This is needed for some use-cases though we prefer to build Universal
-  # when a 32-bit version is needed.
-  def build_32_bit?
-    include?('32-bit') && option_defined?('32-bit')
-  end
+            version '1' do
+      self.release = '1.12.6'
+      self.base_url = 'http://coffeescript.org/v1/'
     
-      def describe_java
-    java_xml = Utils.popen_read('/usr/libexec/java_home', '--xml', '--failfast')
-    return 'N/A' unless $?.success?
-    javas = []
-    REXML::XPath.each(REXML::Document.new(java_xml), '//key[text()='JVMVersion']/following-sibling::string') do |item|
-      javas << item.text
-    end
-    javas.uniq.join(', ')
-  end
-    
-      MAIN_KEYS = %w[title message text main value]
-    
-      def tumblr_oauth_token
-    service.token
-  end
-    
-        def filter_const(name)
-      if name.is_a? Array
-        name.map &method(:filter_const)
-      else
-        Docs.const_get '#{name}_filter'.camelize
-      end
+        def initial_urls
+      [ 'https://hexdocs.pm/elixir/#{self.class.release}/api-reference.html',
+        'https://hexdocs.pm/eex/#{self.class.release}/EEx.html',
+        'https://hexdocs.pm/ex_unit/#{self.class.release}/ExUnit.html',
+        'https://hexdocs.pm/iex/#{self.class.release}/IEx.html',
+        'https://hexdocs.pm/logger/#{self.class.release}/Logger.html',
+        'https://hexdocs.pm/mix/#{self.class.release}/Mix.html',
+        'https://elixir-lang.org/getting-started/introduction.html' ]
     end
     
-            doc
-      end
+        html_filters.push 'node/clean_html', 'node/entries', 'title'
+    
+        self.name = 'PHP'
+    self.type = 'php'
+    self.release = '7.2.5'
+    self.base_url = 'https://secure.php.net/manual/en/'
+    self.root_path = 'index.html'
+    self.initial_paths = %w(
+      funcref.html
+      langref.html
+      refs.database.html
+      set.mysqlinfo.html
+      language.control-structures.html
+      reference.pcre.pattern.syntax.html
+      reserved.exceptions.html
+      reserved.interfaces.html
+      reserved.variables.html)
+    
+          super
     end
   end
 end
 
     
-    #
-# This script extracts the forms from the main page of each
-# web site in a list. The output of this can be used with
-# Metasploit (and other tools) to obtain the saved form data
-# of these domains.
-#
+        def scoped_accounts
+      Account.unscoped.tap do |scope|
+        scope.merge! with_usernames
+        scope.merge! matching_username
+        scope.merge! matching_domain
+      end
+    end
     
-    # Sniffer class for GET URL's
-class SnifferURL < BaseProtocolParser
-  def register_sigs
-    self.sigs = {
-      :get		=> /^GET\s+([^\n]+)\s+HTTP\/\d\.\d/i,
-      :webhost	=> /^HOST\:\s+([^\n\r]+)/i,
-    }
+      def type
+    'Emoji'
   end
-    
-    
-    {	if ln =~ /;(read|write)_(handle|filename)=/
-		parts = ln.split(' ')
-		if (parts[0] == 'mov')
-			parts2 = parts[2].split('=')
-			label = parts2[0]
-			label.slice!(0,1)
-			old = parts2[1]
-			new = addrs[label]
-			#puts '%32s: %s -> %x' % [label, old, new]
-			replaces << [label, old, new.to_s(16)]
-		end
-	end
-}
-    
-    puts '* Loading Stdapi'
-    
-    
-jarsignerOpts	= ['-keystore', 'msfkeystore', '-storepass', 'msfstorepass',
-		   '-keypass', 'msfkeypass', '-signedJar', 's#{outputJar}',
-		   outputJar, 'signFiles']
-    
-    			if temp[2].length == 2
-				self.block[-1][-1] << temp[2][0].ljust(8)
-				self.block[-1][-1] << temp[2][1]
-			elsif temp[2].length == 3
-				self.block[-1][-1] << temp[2][0].ljust(8)
-				self.block[-1][-1] << temp[2][1]
-				self.block[-1][-1] << ' '
-				self.block[-1][-1] << temp[2][2]
-			else
-				self.block[-1][-1] << temp[2].to_s
-			end
-		end
-    
-          respond_with do |format|
-        format.html do
-          gon.preloads[:pods] = pods_json
-          gon.unchecked_count = Pod.unchecked.count
-          gon.version_failed_count = Pod.version_failed.count
-          gon.error_count = Pod.check_failed.count
-    
-      def weekly_user_stats
-    @created_users_by_week = Hash.new{ |h,k| h[k] = [] }
-    @created_users = User.where('username IS NOT NULL and created_at IS NOT NULL')
-    @created_users.find_each do |u|
-      week = u.created_at.beginning_of_week.strftime('%Y-%m-%d')
-      @created_users_by_week[week] << u.username
-    end
-    
-          def save_request_parameters
-        session[:client_id] = @o_auth_application.client_id
-        session[:response_type] = @response_type
-        session[:redirect_uri] = @redirect_uri
-        session[:scopes] = scopes_as_space_seperated_values
-        session[:state] = params[:state]
-        session[:nonce] = params[:nonce]
-      end
-    
-    module Api
-  module OpenidConnect
-    class ClientsController < ApplicationController
-      skip_before_action :verify_authenticity_token
-    
-          # A string representation of the importer.
-      # Should be overridden by subclasses.
-      #
-      # This is used to help debugging,
-      # and should usually just show the load path encapsulated by this importer.
-      #
-      # @return [String]
-      def to_s
-        Sass::Util.abstract(self)
-      end
-    
-      # Uninstalls this logger from \{Sass.logger\}. This should only be called if
-  # the logger was installed using \{#install!}
-  def uninstall!
-    if Sass.logger != self
-      throw Exception.new('Can't uninstall a logger that's not currently installed.')
-    end
-    
-        # The type of the query (e.g. `'screen'` or `'print'`).
-    #
-    # When parsed as Sass code, this contains strings and SassScript nodes. When
-    # parsed as CSS, it contains a single string (accessible via
-    # \{#resolved_type}).
-    #
-    # @return [Array<String, Sass::Script::Tree::Node>]
-    attr_accessor :type
-    
-        def parse_input(environment, text)
-      case text
-      when Script::MATCH
-        name = $1
-        guarded = !!$3
-        val = Script::Parser.parse($2, @line, text.size - ($3 || '').size - $2.size)
-    
-          if processing_option.respond_to?(:call)
-        processing_option.call(instance)
-      else
-        processing_option
-      end
-    end
-    
-        def register(klass, attachment_name, attachment_options)
-      @attachments ||= {}
-      @attachments[klass] ||= {}
-      @attachments[klass][attachment_name] = attachment_options
-    end
-    
-        # Returns the scaling and cropping geometries (in string-based ImageMagick format)
-    # neccessary to transform this Geometry into the Geometry given. If crop is true,
-    # then it is assumed the destination Geometry will be the exact final resolution.
-    # In this case, the source Geometry is scaled so that an image containing the
-    # destination Geometry would be completely filled by the source image, and any
-    # overhanging image would be cropped. Useful for square thumbnail images. The cropping
-    # is weighted at the center of the Geometry.
-    def transformation_to dst, crop = false
-      if crop
-        ratio = Geometry.new( dst.width / self.width, dst.height / self.height )
-        scale_geometry, scale = scaling(dst, ratio)
-        crop_geometry         = cropping(dst, ratio, scale)
-      else
-        scale_geometry        = dst.to_s
-      end
-    
-        # Returns a sorted list of all interpolations.
-    def self.all
-      self.instance_methods(false).sort!
-    end
