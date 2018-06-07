d1b542a4304cc9a0c02427b9da9b76494e93a954
SUNsung <weatherius@gmail.com>
2018-06-07 05:01:47 +0300

    Rewrite [31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb] File in: 2018-06-07 05:01:47

--- ruby/31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb
+++ ruby/31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb
@@ -1,13 +1,89 @@
 
         
-                    when :user
-              # When the last command was a username login
-              # We might keep track on this one in future
-            when :pass
-              # Perfect we get an +OK after a PASS command this means right password given :-)
+                def server_address(server, options = {})
+          format_url(
+            server.config[:SSLEnable],
+            server.config[:BindAddress],
+            server.config[:Port],
+            options['baseurl']
+          )
+        end
     
-      validate :enough_poll_answers
-  validates :question, presence: true
+          # Generate a JSON representation of the Drop.
+      #
+      # state - the JSON::State object which determines the state of current processing.
+      #
+      # Returns a JSON representation of the Drop in a String.
+      def to_json(state = nil)
+        JSON.generate(hash_for_json(state), state)
+      end
     
-      def stats
-    @popular_tags = ActsAsTaggableOn::Tagging.joins(:tag).limit(50).order('count(taggings.id) DESC').group(:tag).count
+        options[:trailing_slash] = true
+    
+        options[:attribution] = <<-HTML
+      &copy; 2010 The Rust Project Developers<br>
+      Licensed under the Apache License, Version 2.0 or the MIT license, at your option.
+    HTML
+    
+        options[:title] = 'Sinon.JS'
+    options[:container] = '.content .container'
+    
+        def parse(response)
+      unless response.url == root_url || self.class.version == 'Guide'
+        response.body.sub!(/<nav class='devsite-nav-responsive-sidebar.+?<\/nav>/m, '')
+        response.body.gsub!(/<li class='devsite-nav-item'>.+?<\/li>/m, '')
+      end
+    
+        version do
+      self.release = '4.8.3'
+      self.base_url = 'https://webpack.js.org/'
+      self.root_path = 'guides/'
+      self.initial_paths = %w(
+        concepts/
+        guides/
+        api/
+        configuration/
+        loaders/
+        plugins/
+      )
+      self.links = {
+        home: 'https://webpack.js.org/',
+        code: 'https://github.com/webpack/webpack'
+      }
+    
+        def inheritable_copy
+      self.class.new @filters
+    end
+    
+        def merge!(hash)
+      return super unless hash.is_a? Hash
+      hash.assert_valid_keys URI::Generic::COMPONENT
+      hash.each_pair do |key, value|
+        send '#{key}=', value
+      end
+      self
+    end
+    
+        # True if the dimensions represent a square
+    def square?
+      height == width
+    end
+    
+            def failure_message_when_negated
+          'Should not have an attachment named #{@attachment_name}'
+        end
+        alias negative_failure_message failure_message_when_negated
+    
+          if defined?(ActiveRecord)
+        Paperclip.options[:logger] = ActiveRecord::Base.logger
+        ActiveRecord::Base.send(:include, Paperclip::Glue)
+      end
+    end
+  end
+end
+
+    
+        included do
+      extend  HelperMethods
+      include HelperMethods
+    end
