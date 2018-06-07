469d482324ad59a624c8d63932093a3bfef2aeca
SUNsung <weatherius@gmail.com>
2018-06-07 09:34:35 +0300

    Rewrite [16vA.JjW5YRrk16gjWEv39Ngxo16EbronNNJH6o16Crr.cpp] File in: 2018-06-07 09:34:34

--- cccccc/16vA.JjW5YRrk16gjWEv39Ngxo16EbronNNJH6o16Crr.cpp
+++ cccccc/16vA.JjW5YRrk16gjWEv39Ngxo16EbronNNJH6o16Crr.cpp
@@ -1,411 +1,78 @@
 
         
-        namespace atom {
+          void Generate(io::Printer* printer);
+    
+    // TODO(jonskeet): Refactor repeated field support; all the implementations are *really* similar. We
+// should probably have a RepeatedFieldGeneratorBase.
+class RepeatedEnumFieldGenerator : public FieldGeneratorBase {
+ public:
+  RepeatedEnumFieldGenerator(const FieldDescriptor* descriptor,
+                             int fieldOrdinal,
+                             const Options *options);
+  ~RepeatedEnumFieldGenerator();
     }
     
-    #ifndef CHROME_BROWSER_PRINTING_PRINT_VIEW_MANAGER_OBSERVER_H_
-#define CHROME_BROWSER_PRINTING_PRINT_VIEW_MANAGER_OBSERVER_H_
-    
-    #include 'base/macros.h'
-#include 'ppapi/host/host_factory.h'
-    
-      // Not owned by this object.
-  ppapi::host::PpapiHost* ppapi_host_;
-    
-      void OCRTester(const char* imgname, const char* groundtruth, const char* tessdatadir, const char* lang) {
-    //log.info() << tessdatadir << ' for language: ' << lang << std::endl;
-    char *outText;
-    std::locale loc('C'); // You can also use '' for the default system locale
-    std::ifstream file(groundtruth);
-    file.imbue(loc); // Use it for file input
-    std::string gtText((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
-    ASSERT_FALSE(api->Init(tessdatadir, lang)) << 'Could not initialize tesseract.';
-    Pix *image = pixRead(imgname);
-    ASSERT_TRUE(image != nullptr) << 'Failed to read test image.';
-    api->SetImage(image);
-    outText = api->GetUTF8Text();
-    EXPECT_EQ(gtText,outText) << 'Phototest.tif OCR does not match ground truth for ' << ::testing::PrintToString(lang);
-    api->End();
-    delete [] outText;
-    pixDestroy(&image);
-  }
-    
-    bool TessUnlvRenderer::AddImageHandler(TessBaseAPI* api) {
-  const std::unique_ptr<const char[]> unlv(api->GetUNLVText());
-  if (unlv == nullptr) return false;
-    }
-    
-    // Max number of scripts in ICU + 'NULL' + Japanese and Korean + Fraktur
-const int kMaxNumberOfScripts = 116 + 1 + 2 + 1;
-    
-      // only use opencl if compiled w/ OpenCL and selected device is opencl
-#ifdef USE_OPENCL
-  // all of channel 0 then all of channel 1...
-  int* histogramAllChannels = new int[kHistogramSize * num_channels];
-    
-        // insert the node in the network
-    AddNodeToNet(newNode);
-    
-        // CheckOutputNodes - check output nodes
-    // symbolName - name of the computation nodes we are collecting
-    // compNodes - array of computation nodes
-    void CheckOutputNodes(NDLScript<ElemType>* script, std::string symbolName, std::wstring groupTag)
-    {
-        NDLNode<ElemType>* nodeArray = script->FindSymbol(symbolName);
-        bool valid = m_net->FeatureNodes().size() > 0; // see if it's already valid
-        if (!valid && nodeArray)                       // otherwise, see if we found a symbol
-        {
-            NDLType outputType = nodeArray->GetType();
-            // accept either an array of nodes, or a single node
-            valid = (outputType == ndlTypeArray || outputType == ndlTypeFunction || outputType == ndlTypeMacroCall);
-        }
-        if (!valid)
-            RuntimeError('Invalid network node definition for '%s', nonexistant or wrong type', symbolName.c_str());
-        if (nodeArray)
-        {
-            vector<NDLNode<ElemType>*> nodes;
-            if (nodeArray->GetType() == ndlTypeArray)
-                nodes = nodeArray->GetParameters();
-            else
-                nodes.push_back(nodeArray);
-    }
-    }
+      // Write any attributes used to decorate generated function members (methods and properties).
+  // Should not be used to decorate types.
+  void WriteGeneratedCodeAttributes(io::Printer* printer);
     
     
     {
     {
-    {}}}
+    {
+    {
+    {}  // namespace
+}  // namespace java
+}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
 
     
+      virtual MessageGenerator* NewMessageGenerator(
+      const Descriptor* descriptor) const;
     
-    {    // SaveMapping - save a map into the file
-    // saveId - name of the section to save into (section:subsection format)
-    // labelMapping - map we are saving to the file
-    virtual void SaveMapping(std::wstring saveId, const std::map<LabelIdType, LabelType>& labelMapping);
-    virtual bool SupportMultiUtterances() const 
-    {
-        return false;
-    };
-};
-    
-    public:
-    typedef msra::dbn::latticepair latticepair;
-    latticesource(std::pair<std::vector<std::wstring>, std::vector<std::wstring>> latticetocs, const std::unordered_map<std::string, size_t>& modelsymmap, std::wstring RootPathInToc)
-        : numlattices(latticetocs.first, modelsymmap, RootPathInToc), denlattices(latticetocs.second, modelsymmap, RootPathInToc), verbosity(0)
-    {
-    }
-    
-    // Reads the Define Huffman Table (DHT) marker segment and fills in *jpg with
-// the parsed data. Builds the Huffman decoding table in either dc_huff_lut or
-// ac_huff_lut, depending on the type and solt_id of Huffman code being read.
-bool ProcessDHT(const uint8_t* data, const size_t len,
-                JpegReadMode mode,
-                std::vector<HuffmanTableEntry>* dc_huff_lut,
-                std::vector<HuffmanTableEntry>* ac_huff_lut,
-                size_t* pos,
-                JPEGData* jpg) {
-  const size_t start_pos = *pos;
-  VERIFY_LEN(2);
-  size_t marker_len = ReadUint16(data, pos);
-  if (marker_len == 2) {
-    fprintf(stderr, 'DHT marker: no Huffman table found\n');
-    jpg->error = JPEG_EMPTY_DHT;
-    return false;
-  }
-  while (*pos < start_pos + marker_len) {
-    VERIFY_LEN(1 + kJpegHuffmanMaxBitLength);
-    JPEGHuffmanCode huff;
-    huff.slot_id = ReadUint8(data, pos);
-    int huffman_index = huff.slot_id;
-    int is_ac_table = (huff.slot_id & 0x10) != 0;
-    HuffmanTableEntry* huff_lut;
-    if (is_ac_table) {
-      huffman_index -= 0x10;
-      VERIFY_INPUT(huffman_index, 0, 3, HUFFMAN_INDEX);
-      huff_lut = &(*ac_huff_lut)[huffman_index * kJpegHuffmanLutSize];
-    } else {
-      VERIFY_INPUT(huffman_index, 0, 3, HUFFMAN_INDEX);
-      huff_lut = &(*dc_huff_lut)[huffman_index * kJpegHuffmanLutSize];
-    }
-    huff.counts[0] = 0;
-    int total_count = 0;
-    int space = 1 << kJpegHuffmanMaxBitLength;
-    int max_depth = 1;
-    for (int i = 1; i <= kJpegHuffmanMaxBitLength; ++i) {
-      int count = ReadUint8(data, pos);
-      if (count != 0) {
-        max_depth = i;
-      }
-      huff.counts[i] = count;
-      total_count += count;
-      space -= count * (1 << (kJpegHuffmanMaxBitLength - i));
-    }
-    if (is_ac_table) {
-      VERIFY_INPUT(total_count, 0, kJpegHuffmanAlphabetSize, HUFFMAN_CODE);
-    } else {
-      VERIFY_INPUT(total_count, 0, kJpegDCAlphabetSize, HUFFMAN_CODE);
-    }
-    VERIFY_LEN(total_count);
-    std::vector<bool> values_seen(256, false);
-    for (int i = 0; i < total_count; ++i) {
-      uint8_t value = ReadUint8(data, pos);
-      if (!is_ac_table) {
-        VERIFY_INPUT(value, 0, kJpegDCAlphabetSize - 1, HUFFMAN_CODE);
-      }
-      if (values_seen[value]) {
-        fprintf(stderr, 'Duplicate Huffman code value %d\n', value);
-        jpg->error = JPEG_INVALID_HUFFMAN_CODE;
-        return false;
-      }
-      values_seen[value] = true;
-      huff.values[i] = value;
-    }
-    // Add an invalid symbol that will have the all 1 code.
-    ++huff.counts[max_depth];
-    huff.values[total_count] = kJpegHuffmanAlphabetSize;
-    space -= (1 << (kJpegHuffmanMaxBitLength - max_depth));
-    if (space < 0) {
-      fprintf(stderr, 'Invalid Huffman code lengths.\n');
-      jpg->error = JPEG_INVALID_HUFFMAN_CODE;
-      return false;
-    } else if (space > 0 && huff_lut[0].value != 0xffff) {
-      // Re-initialize the values to an invalid symbol so that we can recognize
-      // it when reading the bit stream using a Huffman code with space > 0.
-      for (int i = 0; i < kJpegHuffmanLutSize; ++i) {
-        huff_lut[i].bits = 0;
-        huff_lut[i].value = 0xffff;
-      }
-    }
-    huff.is_last = (*pos == start_pos + marker_len);
-    if (mode == JPEG_READ_ALL &&
-        !BuildJpegHuffmanTable(&huff.counts[0], &huff.values[0], huff_lut)) {
-      fprintf(stderr, 'Failed to build Huffman table.\n');
-      jpg->error = JPEG_INVALID_HUFFMAN_CODE;
-      return false;
-    }
-    jpg->huffman_code.push_back(huff);
-  }
-  VERIFY_MARKER_END();
-  return true;
-}
-    
-    // Computes the DCT (Discrete Cosine Transform) of the 8x8 array in 'block',
-// scaled up by a factor of 16. The values in 'block' are laid out row-by-row
-// and the result is written to the same memory area.
-void ComputeBlockDCT(coeff_t* block);
-    
-      tmp0 = in[5 * stride];
-  tmp1 = kIDCTMatrix[ 5] * tmp0;
-  tmp2 = kIDCTMatrix[13] * tmp0;
-  tmp3 = kIDCTMatrix[21] * tmp0;
-  tmp4 = kIDCTMatrix[29] * tmp0;
-  out[0] += tmp1;
-  out[1] += tmp2;
-  out[2] += tmp3;
-  out[3] += tmp4;
-  out[4] -= tmp4;
-  out[5] -= tmp3;
-  out[6] -= tmp2;
-  out[7] -= tmp1;
-    
-    void AddApp0Data(JPEGData* jpg) {
-  const unsigned char kApp0Data[] = {
-      0xe0, 0x00, 0x10,              // APP0
-      0x4a, 0x46, 0x49, 0x46, 0x00,  // 'JFIF'
-      0x01, 0x01,                    // v1.01
-      0x00, 0x00, 0x01, 0x00, 0x01,  // aspect ratio = 1:1
-      0x00, 0x00                     // thumbnail width/height
-  };
-  jpg->app_data.push_back(
-      std::string(reinterpret_cast<const char*>(kApp0Data),
-                                 sizeof(kApp0Data)));
-}
-    
-    #include 'guetzli/jpeg_data.h'
-    
-    #endif  // GUETZLI_JPEG_ERROR_H_
-
-    
-      // Create a combined IOBuf, then call reserve() to get a larger buffer.
-  // The IOBuf no longer points to the combined data buffer, but the
-  // overall memory segment cannot be deleted until the IOBuf is also
-  // destroyed.
+      // Determine whether the specified owner is on the stack. Returns address of
+  // key if present, 0 otherwise.
+  static Value* contains(Key* k)
   {
-    auto buf = IOBuf::createCombined(256);
-    buf->reserve(0, buf->capacity() + 100);
-  }
-    
-      bool pop(T& val) {
-    hazptr_local<1, Atom> h;
-    hazptr_holder<Atom>& hptr = h[0];
-    Node* node;
-    while (true) {
-      node = hptr.get_protected(head_);
-      if (node == nullptr) {
-        return false;
-      }
-      auto next = node->next();
-      if (cas_head(node, next)) {
-        break;
-      }
-    }
-    hptr.reset();
-    val = node->value();
-    node->retire();
-    return true;
-  }
-    
-    
-    {  bool cas(T& u, T& v) {
-    Node* n = new Node(v);
-    hazptr_holder<Atom> hptr;
-    Node* p;
-    while (true) {
-      p = hptr.get_protected(node_);
-      if (p->val_ != u) {
-        delete n;
-        return false;
-      }
-      if (node_.compare_exchange_weak(
-              p, n, std::memory_order_relaxed, std::memory_order_release)) {
-        break;
-      }
-    }
-    hptr.reset();
-    p->retire();
-    return true;
-  }
-};
-    
-    TEST(small_vector, BasicGuarantee) {
-  for (int prepop = 1; prepop < 30; ++prepop) {
-    (TestBasicGuarantee(prepop))( // parens or a mildly vexing parse :(
-      1,
-      [&] (folly::small_vector<Thrower,3>& v) {
-        v.emplace_back();
-      }
-    );
-    }
-    }
-    
-      XLOG(DBG1, 'log arguments are concatenated: ', 12345, ', ', 92.0);
-  XLOGF(DBG1, 'XLOGF supports {}-style formatting: {:.3f}', 'python', 1.0 / 3);
-  XLOG(DBG2) << 'streaming syntax is also supported: ' << 1234;
-  XLOG(DBG2, 'if you really want, ', 'you can even')
-      << ' mix function-style and streaming syntax: ' << 42;
-  XLOGF(DBG3, 'and {} can mix {} style', 'you', 'format') << ' and streaming';
-    
-      void Set(const uint8_t *B, uint8_t S) {
-    assert(S <= kMaxSize);
-    memcpy(Data, B, S);
-    Size = S;
-  }
-    
-    
+    context* elem = top_;
+    while (elem)
     {
-    {#undef EXT_FUNC
-};
-} // namespace fuzzer
-    
-    using namespace fuzzer;
-    
-    void DupAndCloseStderr();
-    
-    struct FuzzingOptions {
-  int Verbosity = 1;
-  size_t MaxLen = 0;
-  int UnitTimeoutSec = 300;
-  int TimeoutExitCode = 77;
-  int ErrorExitCode = 77;
-  int MaxTotalTimeSec = 0;
-  int RssLimitMb = 0;
-  bool DoCrossOver = true;
-  int MutateDepth = 5;
-  bool UseCounters = false;
-  bool UseIndirCalls = true;
-  bool UseMemcmp = true;
-  bool UseMemmem = true;
-  bool UseCmp = false;
-  bool UseValueProfile = false;
-  bool Shrink = false;
-  int ReloadIntervalSec = 1;
-  bool ShuffleAtStartUp = true;
-  bool PreferSmall = true;
-  size_t MaxNumberOfRuns = -1L;
-  int ReportSlowUnits = 10;
-  bool OnlyASCII = false;
-  std::string OutputCorpus;
-  std::string ArtifactPrefix = './';
-  std::string ExactArtifactPath;
-  std::string ExitOnSrcPos;
-  std::string ExitOnItem;
-  bool SaveArtifacts = true;
-  bool PrintNEW = true; // Print a status line when new units are found;
-  bool OutputCSV = false;
-  bool PrintNewCovPcs = false;
-  bool PrintFinalStats = false;
-  bool PrintCorpusStats = false;
-  bool PrintCoverage = false;
-  bool DumpCoverage = false;
-  bool DetectLeaks = true;
-  int  TraceMalloc = 0;
-  bool HandleAbrt = false;
-  bool HandleBus = false;
-  bool HandleFpe = false;
-  bool HandleIll = false;
-  bool HandleInt = false;
-  bool HandleSegv = false;
-  bool HandleTerm = false;
-};
-    
-    #include 'FuzzerCorpus.h'
-#include 'FuzzerDefs.h'
-#include 'FuzzerDictionary.h'
-#include 'FuzzerExtFunctions.h'
-#include 'FuzzerIO.h'
-#include 'FuzzerTracePC.h'
-#include 'FuzzerValueBitMap.h'
-#include <map>
-#include <sanitizer/coverage_interface.h>
-#include <set>
-#include <sstream>
-    
-    #if LLVM_FUZZER_DEFINES_SANITIZER_WEAK_HOOOKS
-void __sanitizer_weak_hook_memcmp(void *caller_pc, const void *s1,
-                                  const void *s2, size_t n, int result) {
-  fuzzer::TPC.AddValueForMemcmp(caller_pc, s1, s2, n);
-  if (!RecordingMemcmp) return;
-  if (result == 0) return;  // No reason to mutate.
-  if (n <= 1) return;  // Not interesting.
-  TS->TraceMemcmpCallback(n, reinterpret_cast<const uint8_t *>(s1),
-                          reinterpret_cast<const uint8_t *>(s2));
-}
-    
-    std::string Base64(const Unit &U) {
-  static const char Table[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-                              'abcdefghijklmnopqrstuvwxyz'
-                              '0123456789+/';
-  std::string Res;
-  size_t i;
-  for (i = 0; i + 2 < U.size(); i += 3) {
-    uint32_t x = (U[i] << 16) + (U[i + 1] << 8) + U[i + 2];
-    Res += Table[(x >> 18) & 63];
-    Res += Table[(x >> 12) & 63];
-    Res += Table[(x >> 6) & 63];
-    Res += Table[x & 63];
+      if (elem->key_ == k)
+        return elem->value_;
+      elem = elem->next_;
+    }
+    return 0;
   }
-  if (i + 1 == U.size()) {
-    uint32_t x = (U[i] << 16);
-    Res += Table[(x >> 18) & 63];
-    Res += Table[(x >> 12) & 63];
-    Res += '==';
-  } else if (i + 2 == U.size()) {
-    uint32_t x = (U[i] << 16) + (U[i + 1] << 8);
-    Res += Table[(x >> 18) & 63];
-    Res += Table[(x >> 12) & 63];
-    Res += Table[(x >> 6) & 63];
-    Res += '=';
-  }
-  return Res;
-}
+    
+    template <typename ConstBufferSequence, typename Handler>
+class descriptor_write_op
+  : public descriptor_write_op_base<ConstBufferSequence>
+{
+public:
+  BOOST_ASIO_DEFINE_HANDLER_PTR(descriptor_write_op);
+    }
+    
+    #endif // BOOST_ASIO_DETAIL_FD_SET_ADAPTER_HPP
+
+    
+      // Erase an entry from the map.
+  void erase(iterator it)
+  {
+    BOOST_ASIO_ASSERT(it != values_.end());
+    BOOST_ASIO_ASSERT(num_buckets_ != 0);
+    }
+    
+    #include <cstdarg>
+#include <iostream>
+#include <string>
+    
+    #include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <vector>
+    
+    namespace benchmark {
+    }
