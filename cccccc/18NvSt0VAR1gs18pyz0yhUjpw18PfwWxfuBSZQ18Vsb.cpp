7624f6e5ccaa106046e4b93ab6f503100ac37f63
SUNsung <weatherius@gmail.com>
2018-06-07 22:52:25 +0300

    Rewrite [18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp] File in: 2018-06-07 22:52:24

--- cccccc/18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp
+++ cccccc/18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp
@@ -1,156 +1,110 @@
 
         
-        #endif  // TENSORFLOW_COMMON_RUNTIME_SESSION_FACTORY_H_
-
+         protected:
+  explicit GlobalShortcut(v8::Isolate* isolate);
+  ~GlobalShortcut() override;
     
-    #include 'third_party/eigen3/unsupported/Eigen/CXX11/Tensor'
-#include 'tensorflow/core/framework/tensor_types.h'
-#include 'tensorflow/core/framework/types.h'
-#include 'tensorflow/core/kernels/scatter_functor.h'
-    
-    #include 'tensorflow/core/lib/strings/strcat.h'
+    #ifndef ATOM_BROWSER_UNRESPONSIVE_SUPPRESSOR_H_
+#define ATOM_BROWSER_UNRESPONSIVE_SUPPRESSOR_H_
     
     
-    {}  // namespace tensorflow
-
+    { protected:
+  virtual ~WindowListObserver() {}
+};
     
-    REGISTER_OP('EncodeAudioV2')
-    .Input('sampled_audio: float')
-    .Input('file_format: string')
-    .Input('samples_per_second: int32')
-    .Input('bits_per_second: int32')
-    .Output('contents: string')
-    .SetShapeFn(shape_inference::ScalarShape)
-    .Doc(R'doc(
-Processes a `Tensor` containing sampled audio with the number of channels
-and length of the audio specified by the dimensions of the `Tensor`. The
-audio is converted into a string that, when saved to disk, will be equivalent
-to the audio in the specified audio format.
-    
-    #undef REGISTER_GPU
-    
-      ArrayRef<const MarkupASTNode *> getChildren() const {
-    return {getTrailingObjects<MarkupASTNode *>(), NumChildren};
-  }
-    
-      virtual bool enableWarnings() { return false; }
-  virtual bool indexLocals() { return false; }
-    
-    class QuickTest : public testing::Test {
- protected:
-  virtual void SetUp() {
-    start_time_ = time(nullptr);
-  }
-  virtual void TearDown() {
-    const time_t end_time = time(nullptr);
-    EXPECT_TRUE(end_time - start_time_ <=25) << 'The test took too long - ' << ::testing::PrintToString(end_time - start_time_);
-  }
-  time_t start_time_;
-  };
-    
-    namespace tesseract {
-namespace {
-    }
+    class RemoteCallbackFreer : public ObjectLifeMonitor,
+                            public content::WebContentsObserver {
+ public:
+  static void BindTo(v8::Isolate* isolate,
+                     v8::Local<v8::Object> target,
+                     int object_id,
+                     content::WebContents* web_conents);
     }
     
-    TESS_API BOOL TESS_CALL TessResultIteratorSymbolIsSuperscript(const TessResultIterator* handle)
+    
+    {}  // namespace atom
+    
+      const base::ListValue* preferences() const { return preferences_.get(); }
+    
+    #ifndef CHROME_BROWSER_EXTENSIONS_GLOBAL_SHORTCUT_LISTENER_WIN_H_
+#define CHROME_BROWSER_EXTENSIONS_GLOBAL_SHORTCUT_LISTENER_WIN_H_
+    
+      // Sets time interval between updates. By default list of sources and their
+  // thumbnail are updated once per second. If called after StartUpdating() then
+  // it will take effect only after the next update.
+  virtual void SetUpdatePeriod(base::TimeDelta period) = 0;
+    
+    namespace printing {
+    }
+    
+    
+    {  DISALLOW_COPY_AND_ASSIGN(ChromeBrowserPepperHostFactory);
+};
+    
+    
+    {    IntrinsicParams();
+    IntrinsicParams(Vec2d f, Vec2d c, Vec4d k, double alpha = 0);
+    IntrinsicParams operator+(const Mat& a);
+    IntrinsicParams& operator =(const Mat& a);
+    void Init(const cv::Vec2d& f, const cv::Vec2d& c, const cv::Vec4d& k = Vec4d(0,0,0,0), const double& alpha = 0);
+};
+    
+        int pointsCount = cvtest::randInt(rng) % maxPointsCount;
+    usedPointsCount = pointsCount == 0 ? 0 : cvtest::randInt(rng) % pointsCount;
+    
+    //! @endcond
+    
+    namespace cv
 {
-    return handle->SymbolIsSuperscript() ? TRUE : FALSE;
-}
-    
-      // CIDFONTTYPE2
-  n = snprintf(buf, sizeof(buf),
-               '4 0 obj\n'
-               '<<\n'
-               '  /BaseFont /GlyphLessFont\n'
-               '  /CIDToGIDMap %ld 0 R\n'
-               '  /CIDSystemInfo\n'
-               '  <<\n'
-               '     /Ordering (Identity)\n'
-               '     /Registry (Adobe)\n'
-               '     /Supplement 0\n'
-               '  >>\n'
-               '  /FontDescriptor %ld 0 R\n'
-               '  /Subtype /CIDFontType2\n'
-               '  /Type /Font\n'
-               '  /DW %d\n'
-               '>>\n'
-               'endobj\n',
-               5L,         // CIDToGIDMap
-               7L,         // Font descriptor
-               1000 / kCharWidth);
-  if (n >= sizeof(buf)) return false;
-  AppendPDFObject(buf);
-    
-    
-/**********************************************************************
- * UTF8 Text Renderer interface implementation
- **********************************************************************/
-TessTextRenderer::TessTextRenderer(const char *outputbase)
-    : TessResultRenderer(outputbase, 'txt') {
-}
-    
-    #include <ctype.h>
-#include 'reject.h'
-#include 'statistc.h'
-#include 'control.h'
-#include 'fixspace.h'
-#include 'genblob.h'
-#include 'tessvars.h'
-#include 'tessbox.h'
-#include 'globals.h'
-#include 'tesseractclass.h'
-    
-      // Print statistics.
-  void print_scores(void) const;
-  void print_scores(int orientation_id) const;
-    
-      // If the current WERD_RES (it_->word()) is not nullptr, sets the BlamerBundle
-  // of the current word to the given pointer (takes ownership of the pointer)
-  // and returns true.
-  // Can only be used when iterating on the word level.
-  bool SetWordBlamerBundle(BlamerBundle *blamer_bundle);
-    
-    Variant HHVM_FUNCTION(xhprof_network_disable) {
-  return ServerStats::EndNetworkProfile();
-}
-    
-      if ((ms->flags & MAGIC_PRESERVE_ATIME) != 0) {
-    /*
-     * Try to restore access, modification times if read it.
-     * This is really *bad* because it will modify the status
-     * time of the file... And of course this will affect
-     * backup programs
-     */
-#ifdef HAVE_UTIMES
-    struct timeval  utsbuf[2];
-    (void)memset(utsbuf, 0, sizeof(utsbuf));
-    utsbuf[0].tv_sec = sb->st_atime;
-    utsbuf[1].tv_sec = sb->st_mtime;
     }
     
-      void init(Address start, Address dest, size_t sz, const char* name) {
-    init(start, dest, sz, sz, name);
+    //
+// END OF CUSTOM FUNCTIONS HERE
+//
+    
+    #include 'opencv2/core/opencl/runtime/opencl_core.hpp'
+#include 'opencv2/core/opencl/runtime/opencl_clamdfft.hpp'
+    
+    struct DynamicFnEntry
+{
+    const char* fnName; // 'clCreateContext'
+    void** ppFn; // &clCreateContext_pfn
+};
+    
+        const_buffer source_buffer =
+      const_buffer(*source_iter) + source_buffer_offset;
+    
+    template <typename Stream>
+class buffered_read_stream;
+    
+    namespace boost {
+namespace asio {
+    }
+    }
+    
+    
+    {} // namespace boost
+    
+    #include <boost/asio/detail/config.hpp>
+    
+      // Insert an element into the values list by splicing from the spares list,
+  // if a spare is available, and otherwise by inserting a new element.
+  iterator values_insert(iterator it, const value_type& v)
+  {
+    if (spares_.empty())
+    {
+      return values_.insert(it, v);
+    }
+    else
+    {
+      spares_.front() = v;
+      values_.splice(it, spares_, spares_.begin());
+      return --it;
+    }
   }
     
-    // Disassemble the code from the given raw file, whose initial address is given
-// by fileStartAddr, for the address range given by
-// [codeStartAddr, codeStartAddr + codeLen)
     
-        for (auto it : c2arcs) {
-      auto const c = it.second;
-      auto const c2arc = it.first;
-    }
-    
-      /**
-   * Opens a process with given cwd and environment variables.
-   *
-   * The parameters 'created' and 'desired' describe the pipes that need to
-   * be setup for the child process: 'created' contains created fd for child,
-   * and 'desired' contains desired fd in child.
-   *
-   * The parameter env contains strings of the form <name>=<content>.
-   */
-  static pid_t proc_open(const char *cmd, const std::vector<int> &created,
-                         const std::vector<int> &desired,
-                         const char *cwd, const std::vector<std::string> &env);
+    {  if (result == 0)
+    ec = boost::system::error_code();
+  return result;
+}
