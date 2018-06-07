782f6952129fa298e9ad75b06d13fbb9a4091df6
SUNsung <weatherius@gmail.com>
2018-06-07 05:26:18 +0300

    Rewrite [52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp] File in: 2018-06-07 05:26:17

--- cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
+++ cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
@@ -1,141 +1,222 @@
 
         
-        
-    {    return S_OK;
-}
-    
-    #ifndef __OPENCV_CORE_OCL_RUNTIME_COMMON_HPP__
-#define __OPENCV_CORE_OCL_RUNTIME_COMMON_HPP__
-    
-    #  define EXPECT_DEBUG_DEATH(statement, regex) \
-  GTEST_EXECUTE_STATEMENT_(statement, regex)
-    
-    namespace internal {
+        namespace atom {
     }
     
-    #if !GTEST_OS_SYMBIAN
-# include <utility>
-#endif
+      class MatchGroundTruth : public QuickTest ,
+      public ::testing::WithParamInterface<const char*> {
+  };
+  
+  TEST_P(MatchGroundTruth, FastPhototestOCR) {
+    OCRTester(TESTING_DIR '/phototest.tif',
+              TESTING_DIR '/phototest.txt',
+              TESSDATA_DIR '_fast', GetParam());
+  }
+  
+  INSTANTIATE_TEST_CASE_P( EngLatinDevaArabLang, MatchGroundTruth, 
+                        ::testing::Values('eng', 'script/Latin', 'script/Devanagari', 'script/Arabic') );
     
-    // Factory interface for death tests.  May be mocked out for testing.
-class DeathTestFactory {
- public:
-  virtual ~DeathTestFactory() { }
-  virtual bool Create(const char* statement, const RE* regex,
-                      const char* file, int line, DeathTest** test) = 0;
+    
+    {  UnicharCompress compressed_;
+  UNICHARSET unicharset_;
+  int null_char_;
+  // The encoding of the null_char_.
+  int encoded_null_char_;
 };
     
-      // Returns true if pathname describes an absolute path.
-  bool IsAbsolutePath() const;
-    
-    // This is used internally by all instances of linked_ptr<>.  It needs to be
-// a non-template class because different types of linked_ptr<> can refer to
-// the same object (linked_ptr<Superclass>(obj) vs linked_ptr<Subclass>(obj)).
-// So, it needs to be possible for different types of linked_ptr to participate
-// in the same circular linked list, so we need a single class type here.
-//
-// DO NOT USE THIS CLASS DIRECTLY YOURSELF.  Use linked_ptr<T>.
-class linked_ptr_internal {
- public:
-  // Create a new circle that includes only this instance.
-  void join_new() {
-    next_ = this;
-  }
-    }
-    
-      template <typename T>
-  operator ParamGenerator<T>() const {
-    const T array[] = {static_cast<T>(v1_), static_cast<T>(v2_),
-        static_cast<T>(v3_), static_cast<T>(v4_), static_cast<T>(v5_),
-        static_cast<T>(v6_), static_cast<T>(v7_), static_cast<T>(v8_),
-        static_cast<T>(v9_), static_cast<T>(v10_), static_cast<T>(v11_),
-        static_cast<T>(v12_), static_cast<T>(v13_), static_cast<T>(v14_),
-        static_cast<T>(v15_), static_cast<T>(v16_), static_cast<T>(v17_),
-        static_cast<T>(v18_), static_cast<T>(v19_), static_cast<T>(v20_),
-        static_cast<T>(v21_), static_cast<T>(v22_), static_cast<T>(v23_),
-        static_cast<T>(v24_), static_cast<T>(v25_), static_cast<T>(v26_),
-        static_cast<T>(v27_), static_cast<T>(v28_), static_cast<T>(v29_),
-        static_cast<T>(v30_), static_cast<T>(v31_), static_cast<T>(v32_)};
-    return ValuesIn(array);
-  }
-    
-    template <$for j, [[typename T$j]]>
-class CartesianProductGenerator$i
-    : public ParamGeneratorInterface< ::std::tr1::tuple<$for j, [[T$j]]> > {
- public:
-  typedef ::std::tr1::tuple<$for j, [[T$j]]> ParamType;
-    }
-    
-      virtual int GetNextPrime(int p) const {
-    for (int n = p + 1; n < is_prime_size_; n++) {
-      if (is_prime_[n]) return n;
-    }
-    }
-    
-    	static PoolByteArray get_project_cert_array();
-	static void load_certs_from_memory(const PoolByteArray &p_memory);
-	static bool is_available();
-    
-    	ERR_FAIL_COND_V(context != NULL, FAILED);
-    
-    
-    {	if (_is_multiplayer) {
-		// need to wait for ID confirmation...
-	} else {
-		emit_signal('connection_established', p_protocol);
-	}
+    // Returns the number of blanks before the current word.
+int ResultIterator::BlanksBeforeWord() const {
+  if (CurrentParagraphIsLtr()) return LTRResultIterator::BlanksBeforeWord();
+  return IsAtBeginningOf(RIL_TEXTLINE) ? 0 : 1;
 }
-    
-    	virtual void poll() = 0;
-	virtual Error connect_to_host(String p_host, String p_path, uint16_t p_port, bool p_ssl, PoolVector<String> p_protocol = PoolVector<String>()) = 0;
-	virtual void disconnect_from_host() = 0;
-	virtual IP_Address get_connected_host() const = 0;
-	virtual uint16_t get_connected_port() const = 0;
-    
-    	GDCLASS(VehicleBody, RigidBody);
-    
-    
-    {            k += r;
-          }
-          
-          s = JPGD_HUFF_EXTEND(extra_bits, s);
-    
-    #include <stdlib.h>
-#include <stdio.h>
-#include <setjmp.h>
-    
-    	HKEY cmderKey;
-	FAIL_ON_ERROR(RegCreateKeyEx(root, keyBaseName, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &cmderKey, NULL));
-    
-    #include <libkern/libkern.h>
-#include <mach/mach_types.h>
-    
-    #include 'osquery/core/utils.h'
-    
-      struct ConstraintList cl2;
-  cl2.affinity = INTEGER_TYPE;
-  constraint = Constraint(LESS_THAN);
-  constraint.expr = '1000';
-  cl2.add(constraint);
-  constraint = Constraint(GREATER_THAN);
-  constraint.expr = '1';
-  cl2.add(constraint);
     
     
     {
-    {  FLAGS_watchdog_delay = delay;
+    {
+    {        while ((edgept->next->flags[FLAGS] & FIXED)
+          && edgept != loopstart)
+          edgept = edgept->next; /*look for next non-fixed */
+      }
+                                 /*do all the loop */
+      while (edgept != loopstart);
+      edgesum = 0;
+      do {
+        if (edgept->flags[FLAGS] & FIXED)
+          edgesum++;
+        edgept = edgept->next;
+      }
+                                 //count fixed pts
+      while (edgept != loopstart);
+      if (edgesum < 3)
+        area /= 2;               //must have 3 pts
+    }
+    while (edgesum < 3);
+    do {
+      linestart = edgept;
+      do {
+        edgept = edgept->next;
+      }
+      while ((edgept->flags[FLAGS] & FIXED) == 0);
+      linestart->next = edgept;
+      edgept->prev = linestart;
+      linestart->vec.x = edgept->pos.x - linestart->pos.x;
+      linestart->vec.y = edgept->pos.y - linestart->pos.y;
+    }
+    while (edgept != loopstart);
+  }
+  else
+    edgept = startpt;            /*start of loop */
+    
+    std::string stringFromCFNumber(const CFDataRef& cf_number) {
+  return stringFromCFNumber(cf_number, CFNumberGetType((CFNumberRef)cf_number));
 }
-} // namespace osquery
+    
+    /// The following test macros allow pretty test output.
+#define CREATE_DATABASE_TESTS(n)                                               \
+  TEST_F(n, test_plugin_check) {                                               \
+    testPluginCheck();                                                         \
+  }                                                                            \
+  TEST_F(n, test_reset) {                                                      \
+    testReset();                                                               \
+  }                                                                            \
+  TEST_F(n, test_put) {                                                        \
+    testPut();                                                                 \
+  }                                                                            \
+  TEST_F(n, test_get) {                                                        \
+    testGet();                                                                 \
+  }                                                                            \
+  TEST_F(n, test_delete) {                                                     \
+    testDelete();                                                              \
+  }                                                                            \
+  TEST_F(n, test_delete_range) {                                               \
+    testDeleteRange();                                                         \
+  }                                                                            \
+  TEST_F(n, test_scan) {                                                       \
+    testScan();                                                                \
+  }                                                                            \
+  TEST_F(n, test_scan_limit) {                                                 \
+    testScanLimit();                                                           \
+  }
+    
+     private:
+  /// For testing only, ask the event stream to publish events immediately.
+  bool no_defer_{false};
+    
+    // Decodes one 8x8 block of DCT coefficients from the bit stream.
+bool DecodeDCTBlock(const HuffmanTableEntry* dc_huff,
+                    const HuffmanTableEntry* ac_huff,
+                    int Ss, int Se, int Al,
+                    int* eobrun,
+                    BitReaderState* br,
+                    JPEGData* jpg,
+                    coeff_t* last_dc_coeff,
+                    coeff_t* coeffs) {
+  int s;
+  int r;
+  bool eobrun_allowed = Ss > 0;
+  if (Ss == 0) {
+    s = ReadSymbol(dc_huff, br);
+    if (s >= kJpegDCAlphabetSize) {
+      fprintf(stderr, 'Invalid Huffman symbol %d for DC coefficient.\n', s);
+      jpg->error = JPEG_INVALID_SYMBOL;
+      return false;
+    }
+    if (s > 0) {
+      r = br->ReadBits(s);
+      s = HuffExtend(r, s);
+    }
+    s += *last_dc_coeff;
+    const int dc_coeff = SignedLeftshift(s, Al);
+    coeffs[0] = dc_coeff;
+    if (dc_coeff != coeffs[0]) {
+      fprintf(stderr, 'Invalid DC coefficient %d\n', dc_coeff);
+      jpg->error = JPEG_NON_REPRESENTABLE_DC_COEFF;
+      return false;
+    }
+    *last_dc_coeff = s;
+    ++Ss;
+  }
+  if (Ss > Se) {
+    return true;
+  }
+  if (*eobrun > 0) {
+    --(*eobrun);
+    return true;
+  }
+  for (int k = Ss; k <= Se; k++) {
+    s = ReadSymbol(ac_huff, br);
+    if (s >= kJpegHuffmanAlphabetSize) {
+      fprintf(stderr, 'Invalid Huffman symbol %d for AC coefficient %d\n',
+              s, k);
+      jpg->error = JPEG_INVALID_SYMBOL;
+      return false;
+    }
+    r = s >> 4;
+    s &= 15;
+    if (s > 0) {
+      k += r;
+      if (k > Se) {
+        fprintf(stderr, 'Out-of-band coefficient %d band was %d-%d\n',
+                k, Ss, Se);
+        jpg->error = JPEG_OUT_OF_BAND_COEFF;
+        return false;
+      }
+      if (s + Al >= kJpegDCAlphabetSize) {
+        fprintf(stderr, 'Out of range AC coefficient value: s=%d Al=%d k=%d\n',
+                s, Al, k);
+        jpg->error = JPEG_NON_REPRESENTABLE_AC_COEFF;
+        return false;
+      }
+      r = br->ReadBits(s);
+      s = HuffExtend(r, s);
+      coeffs[kJPEGNaturalOrder[k]] = SignedLeftshift(s, Al);
+    } else if (r == 15) {
+      k += 15;
+    } else {
+      *eobrun = 1 << r;
+      if (r > 0) {
+        if (!eobrun_allowed) {
+          fprintf(stderr, 'End-of-block run crossing DC coeff.\n');
+          jpg->error = JPEG_EOB_RUN_TOO_LONG;
+          return false;
+        }
+        *eobrun += br->ReadBits(r);
+      }
+      break;
+    }
+  }
+  --(*eobrun);
+  return true;
+}
+    
+    #endif  // GUETZLI_COLOR_TRANSFORM_H_
 
     
+    // Performs in-place floating point 8x8 DCT on block[0..63].
+// Note that the DCT used here is the DCT-2 with the first term multiplied by
+// 1/sqrt(2) and the result scaled by 1/2.
+void ComputeBlockDCTDouble(double block[64]);
     
-    {  BSTR* pData = nullptr;
-  SafeArrayAccessData(value.parray, (void**)&pData);
-  ret.reserve(count);
-  for (long i = 0; i < count; i++) {
-    ret.push_back(bstrToString(pData[i]));
-  }
-  SafeArrayUnaccessData(value.parray);
-  VariantClear(&value);
-  return Status(0);
+    inline int Log2FloorNonZero(uint32_t n) {
+#ifdef __GNUC__
+  return 31 ^ __builtin_clz(n);
+#else
+  unsigned int result = 0;
+  while (n >>= 1) result++;
+  return result;
+#endif
 }
+    
+    void BuildDCHistograms(const JPEGData& jpg, JpegHistogram* histo);
+void BuildACHistograms(const JPEGData& jpg, JpegHistogram* histo);
+size_t JpegHeaderSize(const JPEGData& jpg, bool strip_metadata);
+size_t EstimateJpegDataSize(const int num_components,
+                            const std::vector<JpegHistogram>& histograms);
+    
+    namespace guetzli {
+    }
+    
+    // Preprocesses the u (1) or v (2) channel of the given YUV image (range 0-255).
+std::vector<std::vector<float>> PreProcessChannel(
+    int w, int h, int channel, float sigma, float amount, bool blur,
+    bool sharpen, const std::vector<std::vector<float>>& image);
