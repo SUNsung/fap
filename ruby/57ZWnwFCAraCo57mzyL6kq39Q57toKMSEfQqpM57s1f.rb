577be3713fadb3e814d29745d10cf4ab35d5758c
SUNsung <weatherius@gmail.com>
2018-06-08 23:13:11 +0300

    Rewrite [57ZWnwFCAraCo57mzyL6kq39Q57toKMSEfQqpM57s1f.rb] File in: 2018-06-08 23:13:11

--- ruby/57ZWnwFCAraCo57mzyL6kq39Q57toKMSEfQqpM57s1f.rb
+++ ruby/57ZWnwFCAraCo57mzyL6kq39Q57toKMSEfQqpM57s1f.rb
@@ -1,76 +1,21 @@
-    can?(current_user, ability_name, resource)
-  end
+
+        
+              def self.authors
+        ['gin0606']
+      end
     
-    def check_link(uri)
-  HTTParty.head(uri, :verify => false).code.to_i.tap do |status|
-    if (400..422).include?(status)
-      if status != 403 && !uri.exclude?('udemy.com')
-        raise 'Request had status #{status}'
-      else
-        putc('S')
+        def render(options, screenshots)
+      Dir.mktmpdir do |dir|
+        path = generator.render(options, screenshots, dir)
+        return File.read(path)
       end
     end
   end
-end
     
-      def index
-    if params[:agent_id]
-      @agent = current_user.agents.find(params[:agent_id])
-      @events = @agent.events.page(params[:page])
-    else
-      @events = current_user.events.preload(:agent).page(params[:page])
-    end
-    
-      def retry_queued
-    @jobs = Delayed::Job.awaiting_retry.update_all(run_at: Time.zone.now)
-    
-    respond_to do |format|
-      format.html { redirect_to jobs_path, notice: 'Queued jobs getting retried.' }
-      format.json { head :no_content }
-    end
-  end
-    
-            def autocorrect(node)
-          # Regexp#match can take a second argument, but this cop doesn't
-          # register an offense in that case
-          return unless node.first_argument.regexp_type?
-    
-    module RuboCop
-  module Cop
-    module Lint
-      # This cop checks that there are no repeated conditions
-      # used in case 'when' expressions.
-      #
-      # @example
-      #
-      #   # bad
-      #
-      #   case x
-      #   when 'first'
-      #     do_something
-      #   when 'first'
-      #     do_something_else
-      #   end
-      #
-      # @example
-      #
-      #   # good
-      #
-      #   case x
-      #   when 'first'
-      #     do_something
-      #   when 'second'
-      #     do_something_else
-      #   end
-      class DuplicateCaseCondition < Cop
-        MSG = 'Duplicate `when` condition detected.'.freeze
-    
-            def each_misplaced_optional_arg(arguments)
-          optarg_positions, arg_positions = argument_positions(arguments)
-          return if optarg_positions.empty? || arg_positions.empty?
-    
-            it 'autocorrects the offenses' do
-          new_source = autocorrect_source(source)
-          expect(new_source).to eq(<<-RUBY.strip_indent)
-            #{type} SomeObject
-              include Something
+        context 'Mercurial repository' do
+      before do
+        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_svn?).and_return(false)
+        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_git_svn?).and_return(false)
+        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_git?).and_return(false)
+        expect(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_hg?).and_return(true)
+      end
