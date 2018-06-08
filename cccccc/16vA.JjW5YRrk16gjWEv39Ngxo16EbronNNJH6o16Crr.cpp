41e8c1248f88888d64e945718de9f92bd127aafa
SUNsung <weatherius@gmail.com>
2018-06-08 16:17:45 +0300

    Rewrite [16vA.JjW5YRrk16gjWEv39Ngxo16EbronNNJH6o16Crr.cpp] File in: 2018-06-08 16:17:45

--- cccccc/16vA.JjW5YRrk16gjWEv39Ngxo16EbronNNJH6o16Crr.cpp
+++ cccccc/16vA.JjW5YRrk16gjWEv39Ngxo16EbronNNJH6o16Crr.cpp
@@ -1,78 +1,329 @@
 
         
-          void Generate(io::Printer* printer);
+        Status BuildTable(const std::string& dbname,
+                  Env* env,
+                  const Options& options,
+                  TableCache* table_cache,
+                  Iterator* iter,
+                  FileMetaData* meta) {
+  Status s;
+  meta->file_size = 0;
+  iter->SeekToFirst();
+    }
     
-    // TODO(jonskeet): Refactor repeated field support; all the implementations are *really* similar. We
-// should probably have a RepeatedFieldGeneratorBase.
-class RepeatedEnumFieldGenerator : public FieldGeneratorBase {
+    
+    {}  // namespace config
+    
+    Status DumpFile(Env* env, const std::string& fname, WritableFile* dst) {
+  FileType ftype;
+  if (!GuessType(fname, &ftype)) {
+    return Status::InvalidArgument(fname + ': unknown file type');
+  }
+  switch (ftype) {
+    case kLogFile:         return DumpLog(env, fname, dst);
+    case kDescriptorFile:  return DumpDescriptor(env, fname, dst);
+    case kTableFile:       return DumpTable(env, fname, dst);
+    default:
+      break;
+  }
+  return Status::InvalidArgument(fname + ': not a dump-able file type');
+}
+    
+    class TableCache {
  public:
-  RepeatedEnumFieldGenerator(const FieldDescriptor* descriptor,
-                             int fieldOrdinal,
-                             const Options *options);
-  ~RepeatedEnumFieldGenerator();
+  TableCache(const std::string& dbname, const Options* options, int entries);
+  ~TableCache();
     }
     
-      // Write any attributes used to decorate generated function members (methods and properties).
-  // Should not be used to decorate types.
-  void WriteGeneratedCodeAttributes(io::Printer* printer);
-    
-    
-    {
-    {
-    {
-    {
-    {}  // namespace
-}  // namespace java
-}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-
-    
-      virtual MessageGenerator* NewMessageGenerator(
-      const Descriptor* descriptor) const;
-    
-      // Determine whether the specified owner is on the stack. Returns address of
-  // key if present, 0 otherwise.
-  static Value* contains(Key* k)
-  {
-    context* elem = top_;
-    while (elem)
-    {
-      if (elem->key_ == k)
-        return elem->value_;
-      elem = elem->next_;
-    }
-    return 0;
+      // Add the specified file at the specified number.
+  // REQUIRES: This version has not been saved (see VersionSet::SaveTo)
+  // REQUIRES: 'smallest' and 'largest' are smallest and largest keys in file
+  void AddFile(int level, uint64_t file,
+               uint64_t file_size,
+               const InternalKey& smallest,
+               const InternalKey& largest) {
+    FileMetaData f;
+    f.number = file;
+    f.file_size = file_size;
+    f.smallest = smallest;
+    f.largest = largest;
+    new_files_.push_back(std::make_pair(level, f));
   }
     
-    template <typename ConstBufferSequence, typename Handler>
-class descriptor_write_op
-  : public descriptor_write_op_base<ConstBufferSequence>
-{
-public:
-  BOOST_ASIO_DEFINE_HANDLER_PTR(descriptor_write_op);
+    static void TestEncodeDecode(const VersionEdit& edit) {
+  std::string encoded, encoded2;
+  edit.EncodeTo(&encoded);
+  VersionEdit parsed;
+  Status s = parsed.DecodeFrom(encoded);
+  ASSERT_TRUE(s.ok()) << s.ToString();
+  parsed.EncodeTo(&encoded2);
+  ASSERT_EQ(encoded, encoded2);
+}
+    
+    TEST(FindFileTest, OverlapSequenceChecks) {
+  Add('200', '200', 5000, 3000);
+  ASSERT_TRUE(! Overlaps('199', '199'));
+  ASSERT_TRUE(! Overlaps('201', '300'));
+  ASSERT_TRUE(Overlaps('200', '200'));
+  ASSERT_TRUE(Overlaps('190', '200'));
+  ASSERT_TRUE(Overlaps('200', '210'));
+}
+    
+    void WriteBatchInternal::SetSequence(WriteBatch* b, SequenceNumber seq) {
+  EncodeFixed64(&b->rep_[0], seq);
+}
+    
+    inline
+static void ExecErrorCheck(int status, char *err_msg) {
+  if (status != SQLITE_OK) {
+    fprintf(stderr, 'SQL error: %s\n', err_msg);
+    sqlite3_free(err_msg);
+    exit(1);
+  }
+}
+    
+    class Benchmark {
+ private:
+  kyotocabinet::TreeDB* db_;
+  int db_num_;
+  int num_;
+  int reads_;
+  double start_;
+  double last_op_finish_;
+  int64_t bytes_;
+  std::string message_;
+  Histogram hist_;
+  RandomGenerator gen_;
+  Random rand_;
+  kyotocabinet::LZOCompressor<kyotocabinet::LZO::RAW> comp_;
     }
     
-    #endif // BOOST_ASIO_DETAIL_FD_SET_ADAPTER_HPP
+    // Find a file by file name.
+bool PyDescriptorDatabase::FindFileByName(const string& filename,
+                                          FileDescriptorProto* output) {
+  ScopedPyObjectPtr py_descriptor(PyObject_CallMethod(
+      py_database_, 'FindFileByName', 's#', filename.c_str(), filename.size()));
+  return GetFileDescriptorProto(py_descriptor.get(), output);
+}
+    
+    #include <Python.h>
+    
+    #include <string>
+    
+    #include <memory>
+    
+    // Generator options (used by csharp_generator.cc):
+struct Options {
+  Options() :
+      file_extension('.cs'),
+      base_namespace(''),
+      base_namespace_specified(false),
+      internal_access(false) {
+  }
+  // Extension of the generated file. Defaults to '.cs'
+  string file_extension;
+  // Base namespace to use to create directory hierarchy. Defaults to ''.
+  // This option allows the simple creation of a conventional C# file layout,
+  // where directories are created relative to a project-specific base
+  // namespace. For example, in a project with a base namespace of PetShop, a
+  // proto of user.proto with a C# namespace of PetShop.Model.Shared would
+  // generate Model/Shared/User.cs underneath the specified --csharp_out
+  // directory.
+  //
+  // If no base namespace is specified, all files are generated in the
+  // --csharp_out directory, with no subdirectories created automatically.
+  string base_namespace;
+  // Whether the base namespace has been explicitly specified by the user.
+  // This is required as the base namespace can be explicitly set to the empty
+  // string, meaning 'create a full directory hierarchy, starting from the first
+  // segment of the namespace.'
+  bool base_namespace_specified;
+  // Whether the generated classes should have accessibility level of 'internal'.
+  // Defaults to false that generates 'public' classes.
+  bool internal_access;
+};
+    
+      void WriteIntroduction(io::Printer* printer);
+  void WriteDescriptor(io::Printer* printer);
+  void WriteGeneratedCodeInfo(const Descriptor* descriptor,
+                              io::Printer* printer,
+                              bool last);
+    
+    namespace protobuf {
+namespace compiler {
+namespace java {
+    }
+    }
+    }
+    
+    // Author: kenton@google.com (Kenton Varda)
+//  Based on original Protocol Buffers design by
+//  Sanjay Ghemawat, Jeff Dean, and others.
+//
+// Generates Java code for a given .proto file.
+    
+    ServiceGenerator* ImmutableGeneratorFactory::NewServiceGenerator(
+    const ServiceDescriptor* descriptor) const {
+  return new ImmutableServiceGenerator(descriptor, context_);
+}
+    
+    // Factory that creates generators for immutable-default messages.
+class ImmutableGeneratorFactory : public GeneratorFactory {
+ public:
+  ImmutableGeneratorFactory(Context* context);
+  virtual ~ImmutableGeneratorFactory();
+    }
+    
+      if (flags & TrackVtsc) {
+    flags |= TrackCPU;
+  }
+    
+    bool Vunit::needsRegAlloc() const {
+  if (next_vr > Vreg::V0) return true;
+    }
+    
+    #ifndef incl_HPHP_JIT_VASM_UNIT_H_
+#define incl_HPHP_JIT_VASM_UNIT_H_
+    
+    /*
+ * Determine the next NUMA node according to state maintained in `curr_node`.
+ */
+int next_numa_node(std::atomic_int& curr_node);
+/*
+ * The number of numa nodes in the system
+ */
+inline int num_numa_nodes() {
+  return use_numa ? numa_num_nodes : 1;
+}
+/*
+ * Enable numa interleaving for the specified address range
+ */
+void numa_interleave(void* start, size_t size);
+/*
+ * Allocate the specified address range on the given node
+ */
+void numa_bind_to(void* start, size_t size, int node);
+/*
+ * Return true if node is part of the allowed set of numa nodes
+ */
+inline bool numa_node_allowed(int node) {
+  if (numa_node_set == 0) return true;
+  return numa_node_set & (1u << node);
+}
+    
+    namespace HPHP { namespace jit {
+    }
+    }
+    
+    #ifndef incl_HPHP_LIGHT_PROCESS_H_
+#define incl_HPHP_LIGHT_PROCESS_H_
+    
+    public:
+    // constructor from old CNTK config. This is a function template that is also used to get the config from Scripting.
+    template <class ConfigRecordType>
+    SGD(const ConfigRecordType& configSGD)
+        : SGDParams(configSGD, sizeof(ElemType)),
+          // TODO: The next few do not belong into SGD any more than the network or reader we operate on. Either move network and reader in here, or move these out.
+          m_modelPath((const wstring&) configSGD(L'modelPath')),
+          m_keepCheckPointFiles(configSGD(L'keepCheckPointFiles', false)),
+          m_saveBestModelPerCriterion(configSGD(L'saveBestModelPerCriterion', false)),
+          m_trainCriterionNodeName((const wstring&) configSGD(L'trainCriterionNodeName', L'')),
+          m_evalCriterionNodeName ((const wstring&) configSGD(L'evalCriterionNodeName', L'')),
+          m_traceNodeNamesReal    (configSGD(L'traceNodeNamesReal',     ConfigRecordType::Array(stringargvector()))),
+          m_traceNodeNamesCategory(configSGD(L'traceNodeNamesCategory', ConfigRecordType::Array(stringargvector()))),
+          m_traceNodeNamesSparse  (configSGD(L'traceNodeNamesSparse',   ConfigRecordType::Array(stringargvector()))),
+          m_prevChosenMinibatchSize(0),
+          m_lastFinishedEpochTrainLoss(0.0),
+          m_distGradAgg(nullptr),
+          m_gradHeader(nullptr)
+    {
+        msra::files::make_intermediate_dirs(m_modelPath);
+    }
+    // note: This must be in the header, as we cannot properly specialize this constructor in the CPP to make sure all versions are generated.
+    
+        // relink the input of those nodes whose child is oldNode to point to the new one instead
+    for (auto nodeIter = m_nameToNodeMap.begin(); nodeIter != m_nameToNodeMap.end(); nodeIter++)
+    {
+        ComputationNodeBasePtr node = nodeIter->second;
+        for (int i = 0; i < node->GetNumInputs(); i++)
+            if (node->GetInputs()[i] == oldNode)
+                node->SetInput(i, newNode);
+    }
+    
+    // understand and execute from the syntactic expression tree
+ConfigValuePtr Evaluate(ExpressionPtr);                               // evaluate the expression tree
+void Do(ExpressionPtr e);                                             // evaluate e.do
+shared_ptr<Object> EvaluateField(ExpressionPtr e, const wstring& id); // for experimental CNTK integration
+    
+    static void TestEncodeDecode(const VersionEdit& edit) {
+  std::string encoded, encoded2;
+  edit.EncodeTo(&encoded);
+  VersionEdit parsed;
+  Status s = parsed.DecodeFrom(encoded);
+  ASSERT_TRUE(s.ok()) << s.ToString();
+  parsed.EncodeTo(&encoded2);
+  ASSERT_EQ(encoded, encoded2);
+}
+    
+    std::string Key2(int i) {
+  return Key1(i) + '_xxx';
+}
+    
+    #include <string.h>
+#include 'util/coding.h'
+#include 'util/hash.h'
+    
+      std::string ToString() const;
+    
+    #include 'port/port.h'
+#include 'port/thread_annotations.h'
+    
+      ValueType value_type = kTypeDeletion;
+  if (iter_->Valid()) {
+    do {
+      ParsedInternalKey ikey;
+      if (ParseKey(&ikey) && ikey.sequence <= sequence_) {
+        if ((value_type != kTypeDeletion) &&
+            user_comparator_->Compare(ikey.user_key, saved_key_) < 0) {
+          // We encountered a non-deleted value in entries for previous keys,
+          break;
+        }
+        value_type = ikey.type;
+        if (value_type == kTypeDeletion) {
+          saved_key_.clear();
+          ClearSavedValue();
+        } else {
+          Slice raw_value = iter_->value();
+          if (saved_value_.capacity() > raw_value.size() + 1048576) {
+            std::string empty;
+            swap(empty, saved_value_);
+          }
+          SaveKey(ExtractUserKey(iter_->key()), &saved_key_);
+          saved_value_.assign(raw_value.data(), raw_value.size());
+        }
+      }
+      iter_->Prev();
+    } while (iter_->Valid());
+  }
+    
+    namespace leveldb {
+    }
+    
+    #ifndef COMM_COMM_DNS_H_
+#define COMM_COMM_DNS_H_
+    
+      private:
+    void __StartupCreater();
+    void __FirstGetCreater(const std::string& _servicename);
+    void __Creater(std::vector<ServiceRegister>& _vec);
+    
+    
+#endif /* defined(__PublicComponent__testspy__) */
 
     
-      // Erase an entry from the map.
-  void erase(iterator it)
-  {
-    BOOST_ASIO_ASSERT(it != values_.end());
-    BOOST_ASIO_ASSERT(num_buckets_ != 0);
-    }
+    // Licensed under the MIT License (the 'License'); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+// http://opensource.org/licenses/MIT
     
-    #include <cstdarg>
-#include <iostream>
-#include <string>
-    
-    #include <algorithm>
-#include <cstdint>
-#include <iostream>
-#include <string>
-#include <tuple>
-#include <vector>
-    
-    namespace benchmark {
-    }
+    #ifndef COMM_HAS_MEMBER_H_
+#define COMM_HAS_MEMBER_H_
