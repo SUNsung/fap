efe639d3c81303ee85b45a59b25959b57f7b5535
SUNsung <weatherius@gmail.com>
2018-06-08 15:49:08 +0300

    Rewrite [04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py] File in: 2018-06-08 15:49:07

--- pyton/04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py
+++ pyton/04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py
@@ -1,105 +1,50 @@
 
         
-        tf_kernel_library(
-    name = 'dragnn_op_kernels',
-    srcs = [
-        'ops/dragnn_op_kernels.cc',
-        'ops/dragnn_ops.cc',
-    ],
-    hdrs = [
-    ],
-    deps = [
-        ':compute_session',
-        ':compute_session_op',
-        ':compute_session_pool',
-        ':resource_container',
-        ':shape_helpers',
-        '//dragnn/core/util:label',
-        '//dragnn/protos:data_proto_cc',
-        '//dragnn/protos:spec_proto_cc',
-        '//syntaxnet:base',
-        '@org_tensorflow//third_party/eigen3',
-    ],
-)
+            def __init__(self, path, factory=None, create=True):
+        '''Initialize a Mailbox instance.'''
+        self._path = os.path.abspath(os.path.expanduser(path))
+        self._factory = factory
     
+        def test_oldargs1_1_kw(self):
+        self.assertRaises(TypeError, [].count, {}, x=2)
     
-class MockNetwork(object):
+        def test_mkdir_exist_ok_root(self):
+        # Issue #25803: A drive root could raise PermissionError on Windows
+        self.cls('/').resolve().mkdir(exist_ok=True)
+        self.cls('/').resolve().mkdir(parents=True, exist_ok=True)
     
-                              [[[  8,  19,  37],
-                            [ 18,  43,  85],
-                            [ 28,  67, 133]],
-                           [[ 27,  65, 131],
-                            [ 17,  41,  83],
-                            [  7,  17,  35]]])  # pyformat: disable
+        def abort(self):
+        # What does it mean to 'clear' a document?  Does the
+        # documentElement disappear?
+        raise NotImplementedError(
+            'haven't figured out what this means yet')
     
-      Returns:
-    Dict mapping from shortened resource path to original resource path.
-  '''
-  for component_spec in master_spec.component:
-    for feature_spec in component_spec.fixed_feature:
-      feature_spec.ClearField('pretrained_embedding_matrix')
-      feature_spec.ClearField('vocab')
+    @dataclass
+class CV:
+    T_CV4 = ClassVar
+    cv0: ClassVar[int] = 20
+    cv1: ClassVar = 30
+    cv2: T_CV2
+    cv3: T_CV3
+    not_cv4: T_CV4  # When using string annotations, this field is not recognized as a ClassVar.
     
-          # Sanity check outputs.
-      for cell_output in cell_subgraph_spec.output:
-        self.assertGreater(len(cell_output.name), 0)
-        self.assertGreater(len(cell_output.tensor), 0)
-        restored_graph.get_tensor_by_name(cell_output.tensor)  # shouldn't raise
+        for filename in filenames:
+        fd = codecs.open(filename, mode='r', encoding='utf-8')
+        for line in fd.readlines():
+            refs = re.findall(r'(?<=<a href=')[^']*', markdown.markdown(line))
+            for ref in refs:
+                if ref not in urls:
+                    urls.append(ref)
     
-        with server as (host, port):
-        url = 'http://{0}:{1}/'.format(host, port)
-        r = requests.get(url, auth=auth)
-        # Verify server succeeded in authenticating.
-        assert r.status_code == 200
-        # Verify Authorization was sent in final request.
-        assert 'Authorization' in r.request.headers
-        assert r.request.headers['Authorization'].startswith('Digest ')
-        # Verify redirect happened as we expected.
-        assert r.history[0].status_code == 302
-        close_server.set()
+        REGISTRY = {}
     
-        :rtype: str
-    '''
-    parts = uri.split('%')
-    for i in range(1, len(parts)):
-        h = parts[i][0:2]
-        if len(h) == 2 and h.isalnum():
-            try:
-                c = chr(int(h, 16))
-            except ValueError:
-                raise InvalidURL('Invalid percent-escape sequence: '%s'' % h)
-    
-    The multivariate normal or Gaussian distribution is a generalisation
-of the one-dimensional normal distribution to higher dimensions.
-    
-    pc_expand_repr_doc = '''
-: boolean
-    Whether to print out the full DataFrame repr for wide DataFrames across
-    multiple lines, `max_columns` is still respected, but the output will
-    wrap-around across multiple 'pages' if its width exceeds `display.width`.
+        def test_display_current_time_at_midnight(self):
+        class_under_test = TimeDisplay()
+        expected_time = '24:01'
+        result = class_under_test.get_current_time_as_as_html_fragment()
+        self.assertEqual(result, expected_time)
 '''
     
-        def time_read_uint64_neg_values(self):
-        read_csv(self.data2, header=None, names=['foo'])
-    
-            result = cut(arr, 4)
-    
-            print('Test: General case, duplicates')
-        linked_list.insert_to_front(1)
-        linked_list.insert_to_front(1)
-        linked_list.insert_to_front(3)
-        linked_list.insert_to_front(2)
-        linked_list.insert_to_front(3)
-        linked_list.insert_to_front(1)
-        linked_list.insert_to_front(1)
-        linked_list.remove_dupes()
-        assert_equal(linked_list.get_all_data(), [1, 3, 2])
-    
-    
-def main():
-    num_stacks = 3
-    stack_size = 100
-    test = TestStacks()
-    test.test_pop_on_empty(num_stacks, stack_size)
-    test.test_push_on_full(num_stacks, stack_size)
-    test.test_stacks(num_stacks, stack_size)
+            # they can be executed later on
+        for cmd in command_stack:
+            cmd.execute()
