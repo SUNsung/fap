e5b9fc7a3c9fb34bb4d2447ae149fb19f04ba569
SUNsung <weatherius@gmail.com>
2018-06-08 09:52:24 +0300

    Rewrite [31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb] File in: 2018-06-08 09:52:23

--- ruby/31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb
+++ ruby/31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb
@@ -1,89 +1,74 @@
 
         
-                def server_address(server, options = {})
-          format_url(
-            server.config[:SSLEnable],
-            server.config[:BindAddress],
-            server.config[:Port],
-            options['baseurl']
-          )
+          # GET /resource/unlock?unlock_token=abcdef
+  def show
+    self.resource = resource_class.unlock_access_by_token(params[:unlock_token])
+    yield resource if block_given?
+    
+            routes.each do |module_name, actions|
+          [:path, :url].each do |path_or_url|
+            actions.each do |action|
+              action = action ? '#{action}_' : ''
+              method = :'#{action}#{module_name}_#{path_or_url}'
+    
+    if profile_filename = ENV['PROFILE']
+  require 'ruby-prof'
+  reporter =
+    case (profile_extname = File.extname(profile_filename))
+    when '.txt'
+      RubyProf::FlatPrinterWithLineNumbers
+    when '.html'
+      RubyProf::GraphHtmlPrinter
+    when '.callgrind'
+      RubyProf::CallTreePrinter
+    else
+      raise 'Unknown profiler format indicated by extension: #{profile_extname}'
+    end
+  File.open(profile_filename, 'w') do |io|
+    reporter.new(RubyProf.profile { Pod::Command.run(ARGV) }).print(io)
+  end
+else
+  Pod::Command.run(ARGV)
+end
+
+    
+          def stack
+        UI::ErrorReport.stack
+      end
+    
+            def execute_repl_command(repl_command)
+          unless repl_command == '\n'
+            repl_commands = repl_command.split
+            subcommand = repl_commands.shift.capitalize
+            arguments = repl_commands
+            subcommand_class = Pod::Command::IPC.const_get(subcommand)
+            subcommand_class.new(CLAide::ARGV.new(arguments)).run
+            signal_end_of_output
+          end
         end
-    
-          # Generate a JSON representation of the Drop.
-      #
-      # state - the JSON::State object which determines the state of current processing.
-      #
-      # Returns a JSON representation of the Drop in a String.
-      def to_json(state = nil)
-        JSON.generate(hash_for_json(state), state)
-      end
-    
-        options[:trailing_slash] = true
-    
-        options[:attribution] = <<-HTML
-      &copy; 2010 The Rust Project Developers<br>
-      Licensed under the Apache License, Version 2.0 or the MIT license, at your option.
-    HTML
-    
-        options[:title] = 'Sinon.JS'
-    options[:container] = '.content .container'
-    
-        def parse(response)
-      unless response.url == root_url || self.class.version == 'Guide'
-        response.body.sub!(/<nav class='devsite-nav-responsive-sidebar.+?<\/nav>/m, '')
-        response.body.gsub!(/<li class='devsite-nav-item'>.+?<\/li>/m, '')
-      end
-    
-        version do
-      self.release = '4.8.3'
-      self.base_url = 'https://webpack.js.org/'
-      self.root_path = 'guides/'
-      self.initial_paths = %w(
-        concepts/
-        guides/
-        api/
-        configuration/
-        loaders/
-        plugins/
-      )
-      self.links = {
-        home: 'https://webpack.js.org/',
-        code: 'https://github.com/webpack/webpack'
-      }
-    
-        def inheritable_copy
-      self.class.new @filters
-    end
-    
-        def merge!(hash)
-      return super unless hash.is_a? Hash
-      hash.assert_valid_keys URI::Generic::COMPONENT
-      hash.each_pair do |key, value|
-        send '#{key}=', value
-      end
-      self
-    end
-    
-        # True if the dimensions represent a square
-    def square?
-      height == width
-    end
-    
-            def failure_message_when_negated
-          'Should not have an attachment named #{@attachment_name}'
-        end
-        alias negative_failure_message failure_message_when_negated
-    
-          if defined?(ActiveRecord)
-        Paperclip.options[:logger] = ActiveRecord::Base.logger
-        ActiveRecord::Base.send(:include, Paperclip::Glue)
       end
     end
   end
 end
 
     
-        included do
-      extend  HelperMethods
-      include HelperMethods
-    end
+            self.description = <<-DESC
+          Creates a scaffold for the development of a new Pod named `NAME`
+          according to the CocoaPods best practices.
+          If a `TEMPLATE_URL`, pointing to a git repo containing a compatible
+          template, is specified, it will be used in place of the default one.
+        DESC
+    
+            private
+    
+            def nested_variable_comparison?(node)
+          return false unless nested_comparison?(node)
+          variables_in_node(node).count == 1
+        end
+    
+            def on_send(node)
+          return unless node.receiver && node.method?(:freeze) &&
+                        immutable_literal?(node.receiver)
+    
+          def expect_no_offenses(source, file = nil)
+        inspect_source(source, file)
