ff0b00a3ba5c30b427b4f910239a8e1d02c6bd52
SUNsung <weatherius@gmail.com>
2018-06-09 06:57:30 +0300

    Rewrite [07mVwIVWuxhG.07tEnGJFxgLXE075t38QtvVw07kpz.rb] File in: 2018-06-09 06:57:30

--- ruby/07mVwIVWuxhG.07tEnGJFxgLXE075t38QtvVw07kpz.rb
+++ ruby/07mVwIVWuxhG.07tEnGJFxgLXE075t38QtvVw07kpz.rb
@@ -1,48 +1,22 @@
-          @registered.each do |plugin|
-            result += plugin.components.action_hooks[Plugin::ALL_ACTIONS]
-            result += plugin.components.action_hooks[hook_name]
-          end
+
+        
+            set :run, Proc.new { File.expand_path($0) == File.expand_path(app_file) }
     
-                # Register the handler if this is our first callback.
-            Signal.trap('INT') { fire_callbacks } if registered.length == 1
-          end
-        end
-    
-                  # An IO::WaitReadable means there may be more IO but this
-              # IO object is not ready to be read from yet. No problem,
-              # we read as much as we can, so we break.
-              breakable = true
-            elsif e.is_a?(Errno::EAGAIN)
-              # Otherwise, we just look for the EAGAIN error which should be
-              # all that IO::WaitReadable does in Ruby 1.9.
-              breakable = true
-            end
-    
-        i0 = index
-    r1 = _nt_bareword
-    if r1
-      r0 = r1
-    else
-      r2 = _nt_string
-      if r2
-        r0 = r2
-      else
-        r3 = _nt_number
-        if r3
-          r0 = r3
-        else
-          r4 = _nt_array
-          if r4
-            r0 = r4
-          else
-            r5 = _nt_hash
-            if r5
-              r0 = r5
-            else
-              @index = i0
-              r0 = nil
-            end
-          end
+          def remove_bad_cookies(request, response)
+        return if bad_cookies.empty?
+        paths = cookie_paths(request.path)
+        bad_cookies.each do |name|
+          paths.each { |path| response.set_cookie name, empty_cookie(request.host, path) }
         end
       end
+    
+      context 'escaping' do
+    before do
+      mock_app { |e| [200, {'Content-Type' => 'text/plain'}, [e['PATH_INFO']]] }
     end
+    
+      it 'should not override the header if already set X-Content-Type-Options' do
+    mock_app with_headers('X-Content-Type-Options' => 'sniff')
+    expect(get('/', {}, 'wants' => 'text/html').headers['X-Content-Type-Options']).to eq('sniff')
+  end
+end
