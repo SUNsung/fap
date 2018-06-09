a2fb38c8ed6147e692f727fb2dec09703ceeced4
SUNsung <weatherius@gmail.com>
2018-06-10 00:07:50 +0300

    Rewrite [153Kg81.5Z5wc156Yz0q.ZBvCY15ek5eiuSEXRY15UkL.py] File in: 2018-06-10 00:07:49

--- pyton/153Kg81.5Z5wc156Yz0q.ZBvCY15ek5eiuSEXRY15UkL.py
+++ pyton/153Kg81.5Z5wc156Yz0q.ZBvCY15ek5eiuSEXRY15UkL.py
@@ -1,94 +1,97 @@
 
         
-        # Tests.
-cc_test(
-    name = 'syntaxnet_component_test',
-    srcs = ['syntaxnet_component_test.cc'],
-    data = [':testdata'],
-    deps = [
-        ':syntaxnet_component',
-        '//dragnn/core:input_batch_cache',
-        '//dragnn/core/test:generic',
-        '//dragnn/core/test:mock_transition_state',
-        '//dragnn/io:sentence_input_batch',
-        '//syntaxnet:base',
-        '//syntaxnet:sentence_proto_cc',
-        '//syntaxnet:test_main',
-    ],
-)
+            # by setting the `trainable` argument, in Model
+    x = Input(shape=(1,))
+    layer = Dense(2)
+    y = layer(x)
+    model = Model(x, y)
+    assert model.trainable_weights == layer.trainable_weights
+    layer.trainable = False
+    assert model.trainable_weights == []
     
-      # Construct the parser.
-  parser = spec_builder.ComponentSpecBuilder('parser')
-  parser.set_network_unit(name='FeedForwardNetwork', hidden_layer_sizes='256',
-                          layer_norm_hidden='true')
-  parser.set_transition_system(name='arc-standard')
-  parser.add_token_link(source=lookahead, fml='input.focus', embedding_dim=64)
-  parser.add_token_link(
-      source=tagger, fml='input.focus stack.focus stack(1).focus',
-      embedding_dim=64)
+        weights = model.weights
+    assert model.trainable_weights == weights
+    assert model.non_trainable_weights == []
     
-    from dragnn.protos import spec_pb2
-from dragnn.python import bulk_component
-from dragnn.python import component
-from dragnn.python import dragnn_ops
-from dragnn.python import network_units
-from syntaxnet import sentence_pb2
+        if mode == 'torch':
+        x /= 255.
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+    else:
+        if data_format == 'channels_first':
+            # 'RGB'->'BGR'
+            if K.ndim(x) == 3:
+                x = x[::-1, ...]
+            else:
+                x = x[:, ::-1, ...]
+        else:
+            # 'RGB'->'BGR'
+            x = x[..., ::-1]
+        mean = [103.939, 116.779, 123.68]
+        std = None
     
-      def testArcSourcePotentialsFromTokens(self):
-    with self.test_session():
-      tokens = tf.constant([[[4, 5, 6],
-                             [5, 6, 7],
-                             [6, 7, 8]],
-                            [[6, 7, 8],
-                             [5, 6, 7],
-                             [4, 5, 6]]], tf.float32)  # pyformat: disable
-      weights = tf.constant([2, 3, 5], tf.float32)
+            assert_allclose(convout1, convout2, atol=1e-5)
     
-      Replaces resource paths in the MasterSpec with shortened paths and builds a
-  mapping from the shortened path to the original path. Note that shortened
-  paths are relative to the 'assets.extra' directory of the SavedModel. Also
-  removes resources from FixedFeatureChannel, since they are not exported.
     
-        Raises:
-      ValueError: If hook nodes cannot be inferred from the |master_spec|.
+@keras_test
+def test_vector_classification_functional():
+    (x_train, y_train), (x_test, y_test) = get_test_data(num_train=500,
+                                                         num_test=200,
+                                                         input_shape=(20,),
+                                                         classification=True,
+                                                         num_classes=num_classes)
+    # Test with functional API
+    inputs = layers.Input(shape=(x_train.shape[-1],))
+    x = layers.Dense(16, activation=keras.activations.relu)(inputs)
+    x = layers.Dense(8)(x)
+    x = layers.Activation('relu')(x)
+    outputs = layers.Dense(num_classes, activation='softmax')(x)
+    model = keras.models.Model(inputs, outputs)
+    model.compile(loss=keras.losses.sparse_categorical_crossentropy,
+                  optimizer=keras.optimizers.RMSprop(),
+                  metrics=['acc'])
+    history = model.fit(x_train, y_train, epochs=15, batch_size=16,
+                        validation_data=(x_test, y_test),
+                        verbose=0)
+    assert(history.history['val_acc'][-1] > 0.8)
+    
+    # We project onto a single unit output layer, and squash it with a sigmoid:
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+    
+        filenames = []
+    for (dirpath, dnames, fnames) in os.walk(path):
+        for fname in fnames:
+            if fname.endswith('.md'):
+                filenames.append(os.sep.join([dirpath, fname]))
+    
+        def test_display_current_time_at_midnight(self):
+        class_under_test = TimeDisplay()
+        expected_time = '24:01'
+        result = class_under_test.get_current_time_as_as_html_fragment()
+        self.assertEqual(result, expected_time)
+'''
+    
+        def now(self):
+        current_time_is_always_midnight = '24:01'
+        return current_time_is_always_midnight
+
+    
+        test = Catalog('param_value_2')
+    test.main_method()
+    
+        def __init__(self):
+        self.handler = ConcreteHandler1(
+            ConcreteHandler3(ConcreteHandler2(DefaultHandler())))
+    
     '''
-    # Find an op name we can use for testing runtime hooks.  Assume that at
-    # least one component has a fixed feature (else what is the model doing?).
-    component_name = None
-    for component_spec in master_spec.component:
-      if component_spec.fixed_feature:
-        component_name = component_spec.name
-        break
+http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
+Implementation of the iterator pattern with a generator
     
-      # The one-hot argmax is a subgradient of max.  Convert the batch of maximal
-  # spanning trees into 0/1 indicators, then scale them by the relevant output
-  # gradients from |d_loss_d_max_scores|.  Note that |d_loss_d_max_scores| must
-  # be reshaped in order for it to broadcast across the batch dimension.
-  indicators_bxmxm = tf.one_hot(argmax_sources_bxm, input_dim, dtype=dtype)
-  d_loss_d_max_scores_bx1 = tf.expand_dims(d_loss_d_max_scores, -1)
-  d_loss_d_max_scores_bx1x1 = tf.expand_dims(d_loss_d_max_scores_bx1, -1)
-  d_loss_d_scores_bxmxm = indicators_bxmxm * d_loss_d_max_scores_bx1x1
-  return None, d_loss_d_scores_bxmxm
+        context['show_on_github_url'] = show_url
+    context['edit_on_github_url'] = edit_url
     
     
-def get_nodes(data):
-    return [node['hostname'] for node in data]
-    
-    
-def jsonify(result, format=False):
-    ''' format JSON output (uncompressed or uncompressed) '''
-    
-        terminal_stderr_re = [
-        re.compile(r'% ?Error: '),
-        re.compile(r'^% \w+', re.M),
-        re.compile(r'% ?Bad secret'),
-        re.compile(r'invalid input', re.I),
-        re.compile(r'(?:incomplete|ambiguous) command', re.I),
-        re.compile(r'connection timed out', re.I),
-        re.compile(r'[^\r\n]+ not found', re.I),
-        re.compile(r''[^']' +returned error code: ?\d+'),
-        re.compile(r'syntax error'),
-        re.compile(r'unknown command'),
-        re.compile(r'Error\[\d+\]: ', re.I),
-        re.compile(r'Error:', re.I)
-    ]
+def __kilometers_to_meters(kilometers: float) -> float:
+    '''Convert kilometers to meters.'''
+    return kilometers * 1000
