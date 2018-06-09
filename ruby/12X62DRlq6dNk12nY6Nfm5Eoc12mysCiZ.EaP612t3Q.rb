8d8d3153202fe3296aff153486aef443070dcd66
SUNsung <weatherius@gmail.com>
2018-06-09 20:33:09 +0300

    Rewrite [12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb] File in: 2018-06-09 20:33:09

--- ruby/12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb
+++ ruby/12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb
@@ -1,127 +1,177 @@
 
         
-        def each_schema_load_environment
-  # If we're in development, also run this for the test environment.
-  # This is a somewhat hacky way to do this, so here's why:
-  # 1. We have to define this before we load the schema, or we won't
-  #    have a timestamp_id function when we get to it in the schema.
-  # 2. db:setup calls db:schema:load_if_ruby, which calls
-  #    db:schema:load, which we define above as having a prerequisite
-  #    of this task.
-  # 3. db:schema:load ends up running
-  #    ActiveRecord::Tasks::DatabaseTasks.load_schema_current, which
-  #    calls a private method `each_current_configuration`, which
-  #    explicitly also does the loading for the `test` environment
-  #    if the current environment is `development`, so we end up
-  #    needing to do the same, and we can't even use the same method
-  #    to do it.
-    
-        it 'tracks when sign in is nil' do
-      user.update(current_sign_in_at: nil)
-      sign_in user, scope: :user
-      get :show
-    
-      def register_sigs
-    self.sigs = {
-      :banner		=> /^(220\s*[^\r\n]+)/i,
-      :user		=> /^USER\s+([^\s]+)/i,
-      :pass		=> /^PASS\s+([^\s]+)/i,
-      :login_pass => /^(230\s*[^\n]+)/i,
-      :login_fail => /^(5\d\d\s*[^\n]+)/i,
-      :bye      => /^221/
-    }
-  end
-    
+                if routes &&
+           (routes.named_routes.route_defined?(selector) ||
+             routes.mounted_helpers.method_defined?(selector))
+          @controller.__send__(selector, *args)
+        else
+          super
+        end
       end
     
-        if run? && ARGV.any?
-      require 'optparse'
-      OptionParser.new { |op|
-        op.on('-p port',   'set the port (default is 4567)')                { |val| set :port, Integer(val) }
-        op.on('-o addr',   'set the host (default is #{bind})')             { |val| set :bind, val }
-        op.on('-e env',    'set the environment (default is development)')  { |val| set :environment, val.to_sym }
-        op.on('-s server', 'specify rack server/handler (default is thin)') { |val| set :server, val }
-        op.on('-q',        'turn on quiet mode (default is off)')           {       set :quiet, true }
-        op.on('-x',        'turn on the mutex lock (default is off)')       {       set :lock, true }
-      }.parse!(ARGV.dup)
-    end
-  end
-    
-      describe '#referrer' do
-    it 'Reads referrer from Referer header' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
-      expect(subject.referrer(env)).to eq('bar.com')
+        test 'response_body value is wrapped in an array when the value is a String' do
+      controller = BareController.new
+      controller.set_request!(ActionDispatch::Request.empty)
+      controller.set_response!(BareController.make_response!(controller.request))
+      controller.index
+      assert_equal ['Hello world'], controller.response_body
     end
     
-        it 'redirects requests with sneaky encoded session cookies' do
-      get '/path', {}, 'HTTP_COOKIE' => 'rack.%73ession=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
-      expect(last_response).to be_redirect
-      expect(last_response.location).to eq('/path')
+        test 'middleware that is 'use'd is called as part of the Rack application' do
+      result = @app.call(env_for('/'))
+      assert_equal ['Hello World'], [].tap { |a| result[2].each { |x| a << x } }
+      assert_equal 'Success', result[1]['Middleware-Test']
     end
-  end
     
-      %w(GET HEAD).each do |method|
-    it 'accepts #{method} requests with non-whitelisted Origin' do
-      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://malicious.com')).to be_ok
-    end
-  end
-    
-      it 'comparison of Accept-Language header is not case sensitive' do
-    session = {:foo => :bar}
-    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
-    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'A'
-    expect(session).not_to be_empty
-  end
-    
-      # rspec-expectations config goes here. You can use an alternate
-  # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you prefer.
-  config.expect_with :rspec do |expectations|
-    # Enable only the newer, non-monkey-patching expect syntax.
-    # For more details, see:
-    #   - http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax
-    expectations.syntax = :expect
-  end
-    
-    def config_tag(config, key, tag=nil, classname=nil)
-  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
-  tag       ||= 'div'
-  classname ||= key.sub(/_/, '-').sub(/\./, '-')
-  output      = '<#{tag} class='#{classname}''
-    
-          Dir.chdir(code_path) do
-        code = file.read
-        @filetype = file.extname.sub('.','') if @filetype.nil?
-        title = @title ? '#{@title} (#{file.basename})' : file.basename
-        url = '/#{code_dir}/#{@file}'
-        source = '<figure class='code'><figcaption><span>#{title}</span> <a href='#{url}'>download</a></figcaption>\n'
-        source += '#{HighlightCode::highlight(code, @filetype)}</figure>'
-        TemplateWrapper::safe_wrap(source)
+          text.split.each do |word|
+        if sentences.first.present? && (sentences.last + [word]).join(' ').length > len
+          sentences << [word]
+        else
+          sentences.last << word
+        end
       end
+    
+    class ActiveSupport::TestCase
+  include ActiveSupport::Testing::MethodCallAssertions
+    
+        def call(env)
+      @stack.call(env)
     end
   end
     
-      SPREE_GEMS.each do |gem_name|
-    rm_f  '#{gem_name}/Gemfile.lock'
-    rm_rf '#{gem_name}/pkg'
-    rm_rf '#{gem_name}/spec/dummy'
-  end
-end
-    
-                  add_offense(condition)
-            end
-    
-              add_offense(node)
-        end
-    
-            def comparison?(node)
-          simple_comparison?(node) || nested_comparison?(node)
-        end
+            expect(result).to eq('/usr/local/bin/cloc  --by-file  --out=build/cloc.txt')
       end
     end
   end
 end
 
     
-            def immutable_literal?(node)
-          return true if node.immutable_literal?
+          protected
+    
+                o.on('-f', '--force', 'Remove without confirmation.') do |f|
+              options[:force] = f
+            end
+    
+          th = Thread.new do
+        data = ''
+        64.times do |i|
+          data << i.to_s
+          f.rewind
+          f.print data
+          f.truncate(data.bytesize)
+          q1.push data.bytesize
+          q2.pop
+        end
+        q1.push nil
+      end
+    
+                def replace(enum)
+              @keys = nil
+              super
+            end
+    
+      def test_delete
+    set = Set[1,2,3]
+    
+      it 'decodes the remaining floats when passed the '*' modifier after another directive' do
+    array = '\x9a\x99\xa9@33\x13A'.unpack(unpack_format()+unpack_format('*'))
+    array.should == [5.300000190734863, 9.199999809265137]
+  end
+    
+      it 'decodes the remaining ints when passed the '*' modifier after another directive' do
+    'dcbahgfe'.unpack(unpack_format()+unpack_format('*')).should == [1684234849, 1751606885]
+  end
+    
+      it 'cannot be rescued' do
+    thread = Thread.new do
+      begin
+        Thread.current.send(@method)
+      rescue Exception
+        ScratchPad.record :in_rescue
+      end
+     ScratchPad.record :end_of_thread_block
+    end
+    
+        t.join
+  end
+    
+        10.times { sleep 0.1 if after_sleep1 != true }
+    10.times { sleep 0.1 if t.status and t.status != 'sleep' }
+    after_sleep2.should == false # t should be blocked on the second sleep
+    t.send(@method)
+    
+        def log_http_get_files(files, from, cached = false)
+      return if files.empty?
+      s = '  #{'CACHED ' if cached}GET #{files.length} files from #{from} #{files * ' '}...'
+      if cached
+        puts dark green s
+      else
+        puts dark cyan s
+      end
+    end
+    
+          if path.extname == '.la'
+        path.unlink
+      elsif path.basename.to_s == 'perllocal.pod'
+        # Both this file & the .packlist one below are completely unnecessary
+        # to package & causes pointless conflict with other formulae. They are
+        # removed by Debian, Arch & MacPorts amongst other packagers as well.
+        # The files are created as part of installing any Perl module.
+        path.unlink
+      elsif path.basename.to_s == '.packlist' # Hidden file, not file extension!
+        path.unlink
+      else
+        # Set permissions for executables and non-executables
+        perms = if executable_path?(path)
+          0555
+        else
+          0444
+        end
+        if ARGV.debug?
+          old_perms = path.stat.mode & 0777
+          if perms != old_perms
+            puts 'Fixing #{path} permissions from #{old_perms.to_s(8)} to #{perms.to_s(8)}'
+          end
+        end
+        path.chmod perms
+      end
+    end
+  end
+end
+    
+        if ARGV.include?('--new-issue') || ARGV.switch?('n')
+      url = create_issue(f.tap, '#{f.name} failed to build on #{MacOS.full_version}', url)
+    end
+    
+      def send_sinatra_file(path, &missing_file_block)
+    file_path = File.join(File.dirname(__FILE__), 'public',  path)
+    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
+    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
+  end
+    
+        # Initializes a new CategoryIndex.
+    #
+    #  +base+         is the String path to the <source>.
+    #  +category_dir+ is the String path between <source> and the category folder.
+    #  +category+     is the category currently being processed.
+    def initialize(site, base, category_dir, category)
+      @site = site
+      @base = base
+      @dir  = category_dir
+      @name = 'index.html'
+      self.process(@name)
+      # Read the YAML data from the layout page.
+      self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
+      self.data['category']    = category
+      # Set the title for this page.
+      title_prefix             = site.config['category_title_prefix'] || 'Category: '
+      self.data['title']       = '#{title_prefix}#{category}'
+      # Set the meta-description for this page.
+      meta_description_prefix  = site.config['category_meta_description_prefix'] || 'Category: '
+      self.data['description'] = '#{meta_description_prefix}#{category}'
+    end
+    
+    Liquid::Template.register_tag('img', Jekyll::ImageTag)
+
+    
+    
