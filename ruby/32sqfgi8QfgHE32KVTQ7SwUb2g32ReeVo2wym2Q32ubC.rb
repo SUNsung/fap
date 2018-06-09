681af4d1d7d639787910b5a696534675c0be1053
SUNsung <weatherius@gmail.com>
2018-06-09 12:04:53 +0300

    Rewrite [32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb] File in: 2018-06-09 12:04:52

--- ruby/32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb
+++ ruby/32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb
@@ -1,34 +1,91 @@
 
         
-                key.revoke!
+            # The directory for this Collection, relative to the site source or the directory
+    # containing the collection.
+    #
+    # Returns a String containing the directory name where the collection
+    #   is stored on the filesystem.
+    def relative_directory
+      @relative_directory ||= '_#{label}'
+    end
+    
+            # Add the ability to tap file.html the same way that Nginx does on our
+        # Docker images (or on GitHub Pages.) The difference is that we might end
+        # up with a different preference on which comes first.
+    
+          # Private: Determine whether a class name is an allowed custom
+      #   markdown class name.
+      #
+      # parser_name - the name of the parser class
+      #
+      # Returns true if the parser name contains only alphanumeric
+      # characters and is defined within Jekyll::Converters::Markdown
+      def custom_class_allowed?(parser_name)
+        parser_name !~ %r![^A-Za-z0-9_]! && self.class.constants.include?(
+          parser_name.to_sym
+        )
       end
     end
   end
 end
 
     
-        render_response
-  end
-    
-      private
-    
-    require 'open3'
-require 'set'
-    
-          when :login_bad
-        report_auth_info(s.merge({:active => false}))
-        print_status('Bad IMAP Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]} (#{s[:banner].strip})')
-    
-        cal.each_pair do |key, ver|
-      ver.each_pair do |key, value|
-        instance_variable_get('@#{key}').concat(value[-1])
-      end
+        def assets_path
+      @assets_path ||= File.join gem_path, 'assets'
     end
     
-            def multiple_assignment?
-          return false unless meta_assignment_node
-          meta_assignment_node.type == MULTIPLE_ASSIGNMENT_TYPE
-        end
+      # Update version.rb file with BOOTSTRAP_SHA
+  def store_version
+    path    = 'lib/bootstrap-sass/version.rb'
+    content = File.read(path).sub(/BOOTSTRAP_SHA\s*=\s*[''][\w]+['']/, 'BOOTSTRAP_SHA = '#@branch_sha'')
+    File.open(path, 'w') { |f| f.write(content) }
+  end
+end
+
     
-            MSG = 'Do not freeze immutable objects, as freezing them has no ' \
-              'effect.'.freeze
+      # Code is not reloaded between requests.
+  config.cache_classes = true
+    
+    namespace :bower do
+    
+    def usage
+  $stderr.puts '#{$0} [site list] [output-dir]'
+  exit(0)
+end
+    
+    #
+# This script extracts the forms from the main page of each
+# web site in a list. The output of this can be used with
+# Metasploit (and other tools) to obtain the saved form data
+# of these domains.
+#
+    
+                when :user
+              # When the last command was a username login
+              # We might keep track on this one in future
+            when :pass
+              # Perfect we get an +OK after a PASS command this means right password given :-)
+    
+    		self.block << Array.new
+		self.block[-1] << 'char #{block_name}[]='
+	end
+    
+            def find_address
+          if @order.bill_address_id == params[:id].to_i
+            @order.bill_address
+          elsif @order.ship_address_id == params[:id].to_i
+            @order.ship_address
+          else
+            raise CanCan::AccessDenied
+          end
+        end
+      end
+    end
+  end
+end
+
+    
+            def stock_location
+          render 'spree/api/v1/shared/stock_location_required', status: 422 and return unless params[:stock_location_id]
+          @stock_location ||= StockLocation.accessible_by(current_ability, :read).find(params[:stock_location_id])
+        end
