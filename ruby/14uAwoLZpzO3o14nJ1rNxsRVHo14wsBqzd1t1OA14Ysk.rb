95d6f48ec0ca8432e4703ece0363e897a9161335
SUNsung <weatherius@gmail.com>
2018-06-09 17:02:49 +0300

    Rewrite [14uAwoLZpzO3o14nJ1rNxsRVHo14wsBqzd1t1OA14Ysk.rb] File in: 2018-06-09 17:02:48

--- ruby/14uAwoLZpzO3o14nJ1rNxsRVHo14wsBqzd1t1OA14Ysk.rb
+++ ruby/14uAwoLZpzO3o14nJ1rNxsRVHo14wsBqzd1t1OA14Ysk.rb
@@ -1,63 +1,204 @@
-        expect(result).to eq('/usr/local/bin/cloc  --by-file --xml  --out=build/cloc.xml')
+
+        
+            We recommend using a MacTeX distribution: https://www.tug.org/mactex/
+    
+            if file_is_stale || ARGV.switch?('s') && !f.installed? || bottle_file_outdated?(f, file)
+          cleanup_path(file) { file.unlink }
+        end
       end
+    end
     
-      def update
-    @notification_setting = current_user.notification_settings.find(params[:id])
-    @saved = @notification_setting.update_attributes(notification_setting_params)
+        if ARGV.named.empty?
+      slow_checks = %w[
+        check_for_broken_symlinks
+        check_missing_deps
+        check_for_outdated_homebrew
+        check_for_linked_keg_only_brews
+      ]
+      methods = (checks.all.sort - slow_checks) + slow_checks
+    else
+      methods = ARGV.named
+    end
     
-      def all_projects(current_user)
-    projects = []
-    
-      it 'decodes UTF-8 max codepoints' do
-    [ ['\xf0\x90\x80\x80', [0x10000]],
-      ['\xf3\xbf\xbf\xbf', [0xfffff]],
-      ['\xf4\x80\x80\x80', [0x100000]],
-      ['\xf4\x8f\xbf\xbf', [0x10ffff]]
-    ].should be_computed_by(:unpack, 'U')
-  end
-    
-        run.should be_true
-  end
-    
-        10.times { sleep 0.1 if t.status and t.status != 'sleep' }
-    after_sleep1.should == false # t should be blocked on the first sleep
-    t.send(@method)
-    
-      it 'creates a time based on given C-style gmtime arguments, interpreted as UTC (GMT)' do
-    time = Time.send(@method, 1, 15, 20, 1, 1, 2000, :ignored, :ignored, :ignored, :ignored)
-    time.inspect.should == '2000-01-01 20:15:01 UTC'
-  end
-    
-          config.paths['log']             = '#{Msf::Config.log_directory}/#{Rails.env}.log'
-      config.paths['config/database'] = [Metasploit::Framework::Database.configurations_pathname.try(:to_path)]
-    
-      def parse(pkt)
-    # We want to return immediantly if	we do not have a packet which is handled by us
-    return unless pkt.is_tcp?
-    return if (pkt.tcp_sport != 80 and pkt.tcp_dport != 80)
-    s = find_session((pkt.tcp_sport == 80) ? get_session_src(pkt) : get_session_dst(pkt))
-    
-    asm = nil
-File.open('h2b.com.dbg.in', 'rb') { |fd|
-	asm = fd.read(fd.stat.size)
-}
-    
-      # DELETE /books/1
-  # DELETE /books/1.json
-  def destroy
-    @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
-      format.json { head :no_content }
+      def filtered_list
+    names = if ARGV.named.empty?
+      Formula.racks
+    else
+      ARGV.named.map { |n| HOMEBREW_CELLAR+n }.select(&:exist?)
+    end
+    if ARGV.include? '--pinned'
+      pinned_versions = {}
+      names.each do |d|
+        keg_pin = (HOMEBREW_LIBRARY/'PinnedKegs'/d.basename.to_s)
+        if keg_pin.exist? || keg_pin.symlink?
+          pinned_versions[d] = keg_pin.readlink.basename.to_s
+        end
+      end
+      pinned_versions.each do |d, version|
+        puts '#{d.basename}'.concat(ARGV.include?('--versions') ? ' #{version}' : '')
+      end
+    else # --versions without --pinned
+      names.each do |d|
+        versions = d.subdirs.map { |pn| pn.basename.to_s }
+        next if ARGV.include?('--multiple') && versions.length < 2
+        puts '#{d.basename} #{versions*' '}'
+      end
     end
   end
+end
     
-            def_node_matcher :only_truthiness_matters?, <<-PATTERN
-          ^({if while until case while_post until_post} equal?(%0) ...)
-        PATTERN
+        if $stdout.tty?
+      metacharacters = %w[\\ | ( ) [ ] { } ^ $ * + ? .]
+      bad_regex = metacharacters.any? do |char|
+        ARGV.any? do |arg|
+          arg.include?(char) && !arg.start_with?('/')
+        end
+      end
+      if ARGV.any? && bad_regex
+        ohai 'Did you mean to perform a regular expression search?'
+        ohai 'Surround your query with /slashes/ to search by regex.'
+      end
+    end
     
-            def on_block(node)
-          on_body_of_reduce(node) do |body|
-            void_next = body.each_node(:next).find do |n|
-              n.children.empty? && parent_block_node(n) == node
-            end
+    html_readme = '<html>#{Kramdown::Document.new(open('README.md').read).to_html}</html>'
+readme_doctree = REXML::Document.new(html_readme)
+links = REXML::XPath.match(readme_doctree, '//a')
+    
+    def check_link(uri)
+  HTTParty.head(uri, :verify => false).code.to_i.tap do |status|
+    if (400..422).include?(status)
+      if status != 403 && !uri.exclude?('udemy.com')
+        raise 'Request had status #{status}'
+      else
+        putc('S')
+      end
+    end
+  end
+end
+    
+    
+#
+# Set implements a collection of unordered values with no duplicates.
+# This is a hybrid of Array's intuitive inter-operation facilities and
+# Hash's fast lookup.
+#
+# Set is easy to use with Enumerable objects (implementing +each+).
+# Most of the initializer methods and binary operators accept generic
+# Enumerable objects besides sets and arrays.  An Enumerable object
+# can be converted to Set using the +to_set+ method.
+#
+# Set uses Hash as storage, so you must note the following points:
+#
+# * Equality of elements is determined according to Object#eql? and
+#   Object#hash.  Use Set#compare_by_identity to make a set compare
+#   its elements by their identity.
+# * Set assumes that the identity of each element does not change
+#   while it is stored.  Modifying an element of a set will render the
+#   set to an unreliable state.
+# * When a string is to be stored, a frozen copy of the string is
+#   stored instead unless the original string is already frozen.
+#
+# == Comparison
+#
+# The comparison operators <, >, <=, and >= are implemented as
+# shorthand for the {proper_,}{subset?,superset?} methods.  However,
+# the <=> operator is intentionally left out because not every pair of
+# sets is comparable ({x, y} vs. {x, z} for example).
+#
+# == Example
+#
+#   require 'set'
+#   s1 = Set[1, 2]                        #=> #<Set: {1, 2}>
+#   s2 = [1, 2].to_set                    #=> #<Set: {1, 2}>
+#   s1 == s2                              #=> true
+#   s1.add('foo')                         #=> #<Set: {1, 2, 'foo'}>
+#   s1.merge([2, 6])                      #=> #<Set: {1, 2, 'foo', 6}>
+#   s1.subset?(s2)                        #=> false
+#   s2.subset?(s1)                        #=> true
+#
+# == Contact
+#
+#   - Akinori MUSHA <knu@iDaemons.org> (current maintainer)
+#
+class Set
+  include Enumerable
+    
+      def test_label_syntax
+    return unless @cls == Hash
+    
+      it 'ignores NULL bytes between directives' do
+    '333333\x07@ffffff\xf6?'.unpack(unpack_format('\000', 2)).should == [2.9, 1.4]
+  end
+    
+      it 'does not set $!' do
+    thread = ThreadSpecs.dying_thread_ensures(@method) { ScratchPad.record $! }
+    thread.join
+    ScratchPad.recorded.should == nil
+  end
+    
+    if $opt_test
+  exit test_app($opt_test)
+else
+  report_apps
+end
+
+    
+          # bootstrap requires minimum precision of 8, see https://github.com/twbs/bootstrap-sass/issues/409
+      ::Sass::Script::Number.precision = [8, ::Sass::Script::Number.precision].max
+    end
+    
+      def initialize(repo: 'twbs/bootstrap', branch: 'master', save_to: {}, cache_path: 'tmp/converter-cache-bootstrap')
+    @logger     = Logger.new
+    @repo       = repo
+    @branch     = branch || 'master'
+    @branch_sha = get_branch_sha
+    @cache_path = cache_path
+    @repo_url   = 'https://github.com/#@repo'
+    @save_to    = {
+        js:    'assets/javascripts/bootstrap',
+        scss:  'assets/stylesheets/bootstrap',
+        fonts: 'assets/fonts/bootstrap'}.merge(save_to)
+  end
+    
+        def silence_log
+      @silence = true
+      yield
+    ensure
+      @silence = false
+    end
+  end
+end
+
+    
+      # Generate digests for assets URLs.
+  config.assets.digest = true
+    
+    load './tasks/bower.rake'
+    
+            on roles(target_roles) do
+          unless test '[ -f #{file.to_s.shellescape} ]'
+            info 'Uploading #{prerequisite_file} to #{file}'
+            upload! File.open(prerequisite_file), file
+          end
+        end
+      end
+    end
+    
+            def operator_assignment_node
+          return nil unless node.parent
+          return nil unless OPERATOR_ASSIGNMENT_TYPES.include?(node.parent.type)
+          return nil unless node.sibling_index.zero?
+          node.parent
+        end
+    
+              @name = name.to_sym
+          @declaration_node = declaration_node
+          @scope = scope
+    
+              FROZEN_STRING_LITERAL_TYPES.include?(node.type) &&
+            frozen_string_literals_enabled?
+        end
+      end
+    end
+  end
+end
