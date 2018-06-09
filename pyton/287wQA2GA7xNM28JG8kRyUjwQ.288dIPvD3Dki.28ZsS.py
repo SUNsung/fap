1d900dd9d83835f62f40b5575f2e2dcbe8eb457f
SUNsung <weatherius@gmail.com>
2018-06-09 12:18:39 +0300

    Rewrite [287wQA2GA7xNM28JG8kRyUjwQ.288dIPvD3Dki.28ZsS.py] File in: 2018-06-09 12:18:38

--- pyton/287wQA2GA7xNM28JG8kRyUjwQ.288dIPvD3Dki.28ZsS.py
+++ pyton/287wQA2GA7xNM28JG8kRyUjwQ.288dIPvD3Dki.28ZsS.py
@@ -1,184 +1,36 @@
 
         
-            # Then we cluster the lines together as blocks
-    # Each block represents a collection of lines that should be sorted
-    # This was done by assuming only links ([...](...)) are meant to be sorted
-    # Clustering is done by indentation
-    blocks = []
-    last_indent = None
-    for line in read_me:
-        s_line = line.lstrip()
-        indent = len(line) - len(s_line)
+                video_id = match1(html, r'data-brightcove-id='(\d+)'')
+        
+        assert account_number, video_id
     
+    __all__ = ['ehow_download']
     
-@mock.patch('httpie.core.get_response')
-def test_error_traceback(get_response):
-    exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    with raises(ConnectionError):
-        main(['--ignore-stdin', '--traceback', 'www.google.com'])
+        def extract(self, **kwargs):
+        for i in self.streams:
+            s = self.streams[i]
+            _, s['container'], s['size'] = url_info(s['url'])
+            s['src'] = [s['url']]
     
+    __all__ = ['kugou_download']
     
-def test_unicode_headers_verbose(httpbin):
-    # httpbin doesn't interpret utf8 headers
-    r = http('--verbose', httpbin.url + '/headers', u'Test:%s' % UNICODE)
-    assert HTTP_OK in r
-    assert UNICODE in r
-    
-        try:
-        parsed_args = parser.parse_args(args=args, env=env)
-    except KeyboardInterrupt:
-        env.stderr.write('\n')
-        if include_traceback:
-            raise
-        exit_status = ExitStatus.ERROR_CTRL_C
-    except SystemExit as e:
-        if e.code != ExitStatus.OK:
-            env.stderr.write('\n')
-            if include_traceback:
-                raise
-            exit_status = ExitStatus.ERROR
-    else:
-        try:
-            exit_status = program(
-                args=parsed_args,
-                env=env,
-                log_error=log_error,
-            )
-        except KeyboardInterrupt:
-            env.stderr.write('\n')
-            if include_traceback:
-                raise
-            exit_status = ExitStatus.ERROR_CTRL_C
-        except SystemExit as e:
-            if e.code != ExitStatus.OK:
-                env.stderr.write('\n')
-                if include_traceback:
-                    raise
-                exit_status = ExitStatus.ERROR
-        except requests.Timeout:
-            exit_status = ExitStatus.ERROR_TIMEOUT
-            log_error('Request timed out (%ss).', parsed_args.timeout)
-        except requests.TooManyRedirects:
-            exit_status = ExitStatus.ERROR_TOO_MANY_REDIRECTS
-            log_error('Too many redirects (--max-redirects=%s).',
-                      parsed_args.max_redirects)
-        except Exception as e:
-            # TODO: Further distinction between expected and unexpected errors.
-            msg = str(e)
-            if hasattr(e, 'request'):
-                request = e.request
-                if hasattr(request, 'url'):
-                    msg += ' while doing %s request to URL: %s' % (
-                        request.method, request.url)
-            log_error('%s: %s', type(e).__name__, msg)
-            if include_traceback:
-                raise
-            exit_status = ExitStatus.ERROR
-    
-    
-def filename_from_url(url, content_type):
-    fn = urlsplit(url).path.rstrip('/')
-    fn = os.path.basename(fn) if fn else 'index'
-    if '.' not in fn and content_type:
-        content_type = content_type.split(';')[0]
-        if content_type == 'text/plain':
-            # mimetypes returns '.ksh'
-            ext = '.txt'
-        else:
-            ext = mimetypes.guess_extension(content_type)
-    
-        '''
-    abbrevs = [
-        (1 << 50, 'PB'),
-        (1 << 40, 'TB'),
-        (1 << 30, 'GB'),
-        (1 << 20, 'MB'),
-        (1 << 10, 'kB'),
-        (1, 'B')
-    ]
-    
-        def unregister(self, plugin):
-        self._plugins.remove(plugin)
-    
-            Operator:                  '#582800',   # class: 'o'
-        Operator.Word:             'bold #004461',   # class: 'ow' - like keywords
-    
-        if cookie_dict is not None:
-        names_from_jar = [cookie.name for cookie in cookiejar]
-        for name in cookie_dict:
-            if overwrite or (name not in names_from_jar):
-                cookiejar.set_cookie(create_cookie(name, cookie_dict[name]))
-    
-        from urllib3.packages.ordered_dict import OrderedDict
-    
-    
-class StreamConsumedError(RequestException, TypeError):
-    '''The content for this response was already consumed'''
-    
-        def rebuild_method(self, prepared_request, response):
-        '''When being redirected we may want to change the method of the request
-        based on certain specs or browser behavior.
-        '''
-        method = prepared_request.method
-    
-        def test_HTTP_307_ALLOW_REDIRECT_POST(self, httpbin):
-        r = requests.post(httpbin('redirect-to'), data='test', params={'url': 'post', 'status_code': 307})
-        assert r.status_code == 200
-        assert r.history[0].status_code == 307
-        assert r.history[0].is_redirect
-        assert r.json()['data'] == 'test'
-    
-        def __init__(self, employee_id, name):
-        super(Operator, self).__init__(employee_id, name, Rank.DIRECTOR)
-    
-        def __init__(self, message_id, message, timestamp):
-        self.message_id = message_id
-        self.message = message
-        self.timestamp = timestamp
-    
-        @property
-    def value(self):
-        if self.is_ace() == 1:
-            return 1
-        elif self.is_face_card():
-            return 10
-        else:
-            return self._value
-    
-    
-def huaban_download(url, output_dir='.', **kwargs):
-    if re.match(r'http://huaban\.com/boards/\d+/', url):
-        huaban_download_board(url, output_dir, **kwargs)
-    else:
-        print('Only board (画板) pages are supported currently')
-        print('ex: http://huaban.com/boards/12345678/')
-    
-    site = Infoq()
-download = site.download_by_url
-download_playlist = site.download_by_url
+    site_info = 'MioMio.tv'
+download = miomio_download
+download_playlist = playlist_not_supported('miomio')
 
     
-    def kuwo_playlist_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    html=get_content(url)
-    matched=set(re.compile('yinyue/(\d+)').findall(html))#reduce duplicated
-    for rid in matched:
-        kuwo_download_by_rid(rid,output_dir,merge,info_only)
+        for i in range(10, 30):
+        url = 'https://stream{i}.mixcloud.com/c/m4a/64{p}.m4a'.format(
+            i = i,
+            p = preview
+        )
+        try:
+            mime, ext, size = url_info(url)
+            break
+        except: continue
     
-    from ..common import *
+        def prepare(self, **kwargs):
+        # scrape the html
+        content = get_content(self.url)
     
-    class MusicPlayOn(VideoExtractor):
-    name = 'MusicPlayOn'
-    
-        def is_user_loggedin(self, user):
-        if not (user or self.current_user()):
-            return False
-        return True
-    
-    api('modaction', ModActionTemplate)
-    
-            # Replace all links to '/wiki/help/...' with '/help/...'
-        for link in output.findAll('a'):
-            if link.has_key('href') and link['href'].startswith('/wiki/help'):
-                link['href'] = link['href'][5:]
+        oslist.sort()
