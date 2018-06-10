4c8f26fce51531c397a7863abdb909e0113e6365
SUNsung <weatherius@gmail.com>
2018-06-10 08:37:53 +0300

    Rewrite [00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py] File in: 2018-06-10 08:37:53

--- pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
+++ pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
@@ -1,152 +1,120 @@
 
         
-            CHUNK_SIZE = 1
+          def testReadFirstSentence(self):
+    reader = sentence_io.ConllSentenceReader(self.filepath, 1)
+    sentences, last = reader.read()
+    self.assertEqual(1, len(sentences))
+    pb = sentence_pb2.Sentence()
+    pb.ParseFromString(sentences[0])
+    self.assertFalse(last)
+    self.assertEqual(
+        u'I knew I could do it properly if given the right kind of support .',
+        pb.text)
     
-    import requests.auth
+      def __init__(self,
+               name,
+               builder='DynamicComponentBuilder',
+               backend='SyntaxNetComponent'):
+    '''Initializes the ComponentSpec with some defaults for SyntaxNet.
     
-    from ansible.plugins.terminal import TerminalBase
-from ansible.errors import AnsibleConnectionFailure
+        if during_training:
+      cells_forward = self._train_cells_forward
+      cells_backward = self._train_cells_backward
+    else:
+      cells_forward = self._inference_cells_forward
+      cells_backward = self._inference_cells_backward
     
-        def on_open_shell(self):
-        try:
-            self._exec_cli_command(b'environment no more')
-        except AnsibleConnectionFailure:
-            raise AnsibleConnectionFailure('unable to set terminal parameters')
-
-    
-        def __init__(self, pages, data_store, reverse_index_queue, doc_index_queue):
-        self.pages = pages
-        self.data_store = data_store
-        self.reverse_index_queue = reverse_index_queue
-        self.doc_index_queue = doc_index_queue
-    
-        def bfs(self, source, dest):
-        if source is None:
-            return False
-        queue = deque()
-        queue.append(source)
-        source.visit_state = State.visited
-        while queue:
-            node = queue.popleft()
-            print(node)
-            if dest is node:
-                return True
-            for adjacent_node in node.adj_nodes.values():
-                if adjacent_node.visit_state == State.unvisited:
-                    queue.append(adjacent_node)
-                    adjacent_node.visit_state = State.visited
-        return False
-    
-            Emit key value pairs of the form:
+      with tf.Session(graph=g, config=session_config) as sess:
+    tf.logging.info('Initializing variables...')
+    sess.run(tf.global_variables_initializer())
     
     
-class Vehicle(metaclass=ABCMeta):
-    
-    
-seller_category_map = {}
-seller_category_map['Exxon'] = DefaultCategories.GAS
-seller_category_map['Target'] = DefaultCategories.SHOPPING
-    
-    def get_colordb(file, filetype=None):
-    colordb = None
-    fp = open(file)
-    try:
-        line = fp.readline()
-        if not line:
-            return None
-        # try to determine the type of RGB file it is
-        if filetype is None:
-            filetypes = FILETYPES
-        else:
-            filetypes = [filetype]
-        for typere, class_ in filetypes:
-            mo = typere.search(line)
-            if mo:
-                break
-        else:
-            # no matching type
-            return None
-        # we know the type and the class to grok the type, so suck it in
-        colordb = class_(fp)
-    finally:
-        fp.close()
-    # save a global copy
-    global DEFAULT_DB
-    DEFAULT_DB = colordb
-    return colordb
-    
-    
-    {            ('2.5.2 (63378, Mar 26 2009, 18:03:29)\n[PyPy 1.0.0]',
-             ('PyPy', 'trunk', '63378'), self.save_platform)
-            :
-                ('PyPy', '2.5.2', 'trunk', '63378', ('63378', 'Mar 26 2009'),
-                 '')
-            }
-        for (version_tag, scm, sys_platform), info in \
-                sys_versions.items():
-            sys.version = version_tag
-            if scm is None:
-                if hasattr(sys, '_git'):
-                    del sys._git
-            else:
-                sys._git = scm
-            if sys_platform is not None:
-                sys.platform = sys_platform
-            self.assertEqual(platform.python_implementation(), info[0])
-            self.assertEqual(platform.python_version(), info[1])
-            self.assertEqual(platform.python_branch(), info[2])
-            self.assertEqual(platform.python_revision(), info[3])
-            self.assertEqual(platform.python_build(), info[4])
-            self.assertEqual(platform.python_compiler(), info[5])
-    
-    def color_config(text):  # Called from htest, Editor, and Turtle Demo.
-    '''Set color opitons of Text widget.
-    
-    
-# Stubs to make mypy happy (and later for actual type-checking).
-def raise_exc_info(exc_info):
-    # type: (Tuple[type, BaseException, types.TracebackType]) -> None
-    pass
-    
-    
-def wrap_web_tests_application():
-    result = {}
-    for cls in web_test.wsgi_safe_tests:
-        def class_factory():
-            class WSGIApplicationWrappedTest(cls):  # type: ignore
-                def setUp(self):
-                    self.warning_catcher = ignore_deprecation()
-                    self.warning_catcher.__enter__()
-                    super(WSGIApplicationWrappedTest, self).setUp()
-    
-        The `tornado.web.FallbackHandler` class is often useful for mixing
-    Tornado and WSGI apps in the same server.  See
-    https://github.com/bdarnell/django-tornado-demo for a complete example.
+@csrf_protect
+def render_flatpage(request, f):
     '''
-    def __init__(self, wsgi_application):
-        self.wsgi_application = wsgi_application
+    Internal interface to the flat page view.
+    '''
+    # If registration is required for accessing this page, and the user isn't
+    # logged in, redirect to the login page.
+    if f.registration_required and not request.user.is_authenticated:
+        from django.contrib.auth.views import redirect_to_login
+        return redirect_to_login(request.path)
+    if f.template_name:
+        template = loader.select_template((f.template_name, DEFAULT_TEMPLATE))
+    else:
+        template = loader.get_template(DEFAULT_TEMPLATE)
     
-        def _on_connection_close(self):
-        # Note that this callback is only registered on the IOStream
-        # when we have finished reading the request and are waiting for
-        # the application to produce its response.
-        if self._close_callback is not None:
-            callback = self._close_callback
-            self._close_callback = None
-            callback()
-        if not self._finish_future.done():
-            future_set_result_unless_cancelled(self._finish_future, None)
-        self._clear_callbacks()
+    # Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
     
+    # Language to be used for generating the HTML full-text search index.
+# Sphinx supports the following languages:
+#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
+#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
+#html_search_language = 'en'
     
-class TCPServerTest(AsyncTestCase):
-    @gen_test
-    def test_handle_stream_coroutine_logging(self):
-        # handle_stream may be a coroutine and any exception in its
-        # Future will be logged.
-        class TestServer(TCPServer):
-            @gen.coroutine
-            def handle_stream(self, stream, address):
-                yield stream.read_bytes(len(b'hello'))
-                stream.close()
-                1 / 0
+        def browse_file(self):
+        filetypes = [
+            ('HTML Files', '*.htm *.html', 'TEXT'),
+            ('PDF Files', '*.pdf', 'TEXT'),
+            ('Windows Help Files', '*.chm'),
+            ('Text Files', '*.txt', 'TEXT'),
+            ('All Files', '*')]
+        path = self.pathvar.get()
+        if path:
+            dir, base = os.path.split(path)
+        else:
+            base = None
+            if platform[:3] == 'win':
+                dir = os.path.join(os.path.dirname(executable), 'Doc')
+                if not os.path.isdir(dir):
+                    dir = os.getcwd()
+            else:
+                dir = os.getcwd()
+        file = self.askfilename(filetypes, dir, base)
+        if file:
+            self.pathvar.set(file)
+    
+    For descriptions of all methods, read the comments in the code below.
+Note that all arguments and return values representing article numbers
+are strings, not numbers, since they are rarely used for calculations.
+'''
+    
+        print('# The following line assumes you have built Python using the standard instructions')
+    print('# Otherwise fix the following line to point to the library.')
+    print('pythonlib = '$(pythonhome)/pcbuild/python%s$(debug_suffix).lib'' % version_suffix)
+    print()
+    
+        def test_oldargs0_2_ext(self):
+        try:
+            {}.keys(*(1, 2))
+        except TypeError:
+            pass
+        else:
+            raise RuntimeError
+    
+        1: ('line', 2, 'tfunc_import'),    ('next',),
+    2: ('line', 3, 'tfunc_import'),    ('step',),
+    3: ('call', 5, 'main'),            ('break', ('test_module.py', None, False, None, 'func')),
+    4: ('None', 5, 'main'),            ('continue',),
+    5: ('line', 3, 'func', ({1: 1}, [])), ('step',),
+      BpNum Temp Enb Hits Ignore Where
+      1     no   yes 1    0      at test_module.py:2
+    6: ('return', 3, 'func'),          ('step',),
+    7: ('line', 7, 'main'),            ('step',),
+    8: ('return', 7, 'main'),          ('quit',),
+    
+                ('2.5 (trunk:6107, Mar 26 2009, 13:02:18) \n[Java HotSpot(TM) Client VM (\'Apple Computer, Inc.\')]',
+            ('Jython', 'trunk', '6107'), 'java1.5.0_16')
+            :
+                ('Jython', '2.5.0', 'trunk', '6107',
+                 ('trunk:6107', 'Mar 26 2009'), 'java1.5.0_16'),
+    
+    @dataclasses.dataclass
+class IV:
+    T_IV4 = dataclasses.InitVar
+    iv0: dataclasses.InitVar[int]
+    iv1: dataclasses.InitVar
+    iv2: T_IV2
+    iv3: T_IV3
+    not_iv4: T_IV4  # When using string annotations, this field is not recognized as an InitVar.
