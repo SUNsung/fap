befabb8a77c51f67717fae7d140c12f2a7d7da01
SUNsung <weatherius@gmail.com>
2018-06-10 19:35:24 +0300

    Rewrite [31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb] File in: 2018-06-10 19:35:23

--- ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
+++ ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
@@ -1,190 +1,186 @@
 
         
-        class JustMeController < ActionController::Base
-  clear_helpers
+                def start_callback(detached)
+          unless detached
+            proc do
+              mutex.synchronize do
+                # Block until EventMachine reactor starts
+                @reload_reactor&.started_event&.wait
+                @running = true
+                Jekyll.logger.info('Server running...', 'press ctrl-c to stop.')
+                @run_cond.broadcast
+              end
+            end
+          end
+        end
     
-        test 'middleware stack accepts block arguments' do
-      result = @app.call(env_for('/'))
-      assert_equal 'Configured by block.', result[1]['Configurable-Message']
-    end
-    
-      def test_assert_select_email_multipart
-    AssertMultipartSelectMailer.test(html: '<div><p>foo</p><p>bar</p></div>', text: 'foo bar').deliver_now
-    assert_select_email do
-      assert_select 'div:root' do
-        assert_select 'p:first-child', 'foo'
-        assert_select 'p:last-child', 'bar'
+          def convert(content)
+        document = Kramdown::Document.new(content, @config)
+        html_output = document.to_html.chomp
+        if @config['show_warnings']
+          document.warnings.each do |warning|
+            Jekyll.logger.warn 'Kramdown warning:', warning.sub(%r!^Warning:\s+!, '')
+          end
+        end
+        html_output
       end
     end
   end
 end
 
     
-    require 'abstract_unit'
-require 'mailers/base_mailer'
-require 'active_support/log_subscriber/test_helper'
-require 'action_mailer/log_subscriber'
-    
-        begin
-      response = U2F::RegisterResponse.load_from_json(params[:device_response])
-      registration_data = u2f.register!(challenges, response)
-      registration.update(certificate: registration_data.certificate,
-                          key_handle: registration_data.key_handle,
-                          public_key: registration_data.public_key,
-                          counter: registration_data.counter,
-                          user: user,
-                          name: params[:name])
-    rescue JSON::ParserError, NoMethodError, ArgumentError
-      registration.errors.add(:base, 'Your U2F device did not send a valid JSON response.')
-    rescue U2F::Error => e
-      registration.errors.add(:base, e.message)
-    end
-    
-          # Forgets the given resource by deleting a cookie
-      def forget_me(resource)
-        scope = Devise::Mapping.find_scope!(resource)
-        resource.forget_me!
-        cookies.delete(remember_key(resource, scope), forget_cookie_values(resource))
-      end
-    
-    def list_login_items_for_app(app_path)
-  out, err, status = Open3.capture3(
-    '/usr/bin/osascript', '-e',
-    'tell application \'System Events\' to get the name of every login item ' \
-    'whose path contains \'#{File.basename(app_path)}\''
-  )
-  if status.exitstatus > 0
-    $stderr.puts err
-    exit status.exitstatus
-  end
-  puts out.gsub(', ', '\n')
-end
-    
-    With optional '-t <bundle-id>', silently test if a given app
-is running, exiting with an error code if not.
-    
-          ::Sass.load_paths << stylesheets_path
-    
-    require_relative 'converter/fonts_conversion'
-require_relative 'converter/less_conversion'
-require_relative 'converter/js_conversion'
-require_relative 'converter/logger'
-require_relative 'converter/network'
-    
-          tmp = ''
-      less.scan(/^(\s*&)(-[\w\[\]]+\s*\{.+})$/) do |ampersand, css|
-        tmp << '.badge#{css}\n'
-      end
-    
-      def test_image_helper
-    assert_match %r(url\(['']?/assets/apple-touch-icon-144-precomposed.*png['']?\)), @css
-  end
-    
-    desc 'Test all Gemfiles from test/*.gemfile'
-task :test_all_gemfiles do
-  require 'term/ansicolor'
-  require 'pty'
-  require 'shellwords'
-  cmd      = 'bundle install --quiet && bundle exec rake --trace'
-  statuses = Dir.glob('./test/gemfiles/*{[!.lock]}').map do |gemfile|
-    env = {'BUNDLE_GEMFILE' => gemfile}
-    cmd_with_env = '  (#{env.map { |k, v| 'export #{k}=#{Shellwords.escape v}' } * ' '}; #{cmd})'
-    $stderr.puts Term::ANSIColor.cyan('Testing\n#{cmd_with_env}')
-    PTY.spawn(env, cmd) do |r, _w, pid|
-      begin
-        r.each_line { |l| puts l }
-      rescue Errno::EIO
-        # Errno:EIO error means that the process has finished giving output.
-      ensure
-        ::Process.wait pid
-      end
-    end
-    [$? && $?.exitstatus == 0, cmd_with_env]
-  end
-  failed_cmds = statuses.reject(&:first).map { |(_status, cmd_with_env)| cmd_with_env }
-  if failed_cmds.empty?
-    $stderr.puts Term::ANSIColor.green('Tests pass with all gemfiles')
-  else
-    $stderr.puts Term::ANSIColor.red('Failing (#{failed_cmds.size} / #{statuses.size})\n#{failed_cmds * '\n'}')
-    exit 1
-  end
-end
-    
-      def register_sigs
-    self.sigs = {
-      :banner		=> /^(\*\s+OK[^\n\r]*)/i,
-      :login		=> /^CAPABILITY\s+LOGIN\s+([^\s]+)\s+([^\n\r]+)/i,
-      :login_pass => /^CAPABILITY\s+OK\s+(Login[^\n\r]*)/i,
-      :login_bad	=> /^CAPABILITY\s+BAD\s+(Login[^\n\r]*)/i,
-      :login_fail => /^CAPABILITY\s+NO\s+(Login[^\n\r]*)/i
-    }
-  end
-    
-                  s[:proto]='pop3'
-              s[:extra]='Failed Login. Banner: #{s[:banner]}'
-              report_auth_info(s)
-              print_status('Invalid POP3 Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]} (#{s[:banner].strip})')
-              s[:pass]=''
-          end
-        when nil
-          # No matches, no saved state
-        else
-          s[:last]=matched
-          sessions[s[:session]].merge!({k => matches})
-      end # end case matched
-    end # end of each_key
-  end # end of parse
-end
-    
-    puts '* System info:'
-    
-    clsJavaCompile 	= Rjb::import('javaCompile.CompileSourceInMemory')
-clsCreateJar	= Rjb::import('javaCompile.CreateJarFile')
-clsFile			= Rjb::import('java.io.File')
-system			= Rjb::import('java.lang.System')
-#clsString	= Rjb::import('java.lang.String')
-    
-    
-    
-          def core_gem_names
-        %w(capistrano airbrussh rake sshkit net-ssh) & Gem.loaded_specs.keys
-      end
-    
-        # @abstract
+        # Determine which converter to use based on this convertible's
+    # extension.
     #
-    # Your implementation should check the existence of a cache repository on
-    # the deployment target
-    #
-    # @return [Boolean]
-    #
-    def test
-      raise NotImplementedError, 'Your SCM strategy module should provide a #test method'
+    # Returns the Converter instance.
+    def converters
+      _renderer.converters
     end
     
-      desc 'Finish the deployment, clean up server(s).'
-  task :finishing do
-  end
-    
-      it 'enables printing all config variables on command line parameter' do
-    capture_io do
-      flags '--print-config-variables', '-p'
+        def populate_categories
+      merge_data!(
+        'categories' => (
+          Array(data['categories']) + Utils.pluralized_array_from_hash(
+            data, 'category', 'categories'
+          )
+        ).map(&:to_s).flatten.uniq
+      )
     end
-    expect(Capistrano::Configuration.fetch(:print_config_variables)).to be true
-  end
     
-            def reference!(node)
-          @references << node
-          @referenced = true
+          # Generate a Hash representation of the Drop by resolving each key's
+      # value. It includes Drop methods, mutations, and the underlying object's
+      # data. See the documentation for Drop#keys for more.
+      #
+      # Returns a Hash with all the keys and values resolved.
+      def to_h
+        keys.each_with_object({}) do |(key, _), result|
+          result[key] = self[key]
         end
+      end
+      alias_method :to_hash, :to_h
     
-            def on_percent_literal(node)
-          each_unnecessary_space_match(node) do |range|
-            add_offense(node, location: range)
+        To install Clojure you should install Leiningen:
+      brew install leiningen
+    and then follow the tutorial:
+      https://github.com/technomancy/leiningen/blob/stable/doc/TUTORIAL.md
+    EOS
+  when 'osmium' then <<-EOS.undent
+    The creator of Osmium requests that it not be packaged and that people
+    use the GitHub master branch instead.
+    EOS
+  when 'gfortran' then <<-EOS.undent
+    GNU Fortran is now provided as part of GCC, and can be installed with:
+      brew install gcc
+    EOS
+  when 'play' then <<-EOS.undent
+    Play 2.3 replaces the play command with activator:
+      brew install typesafe-activator
+    
+      def fixopt(f)
+    path = if f.linked_keg.directory? && f.linked_keg.symlink?
+      f.linked_keg.resolved_path
+    elsif f.prefix.directory?
+      f.prefix
+    elsif (kids = f.rack.children).size == 1 && kids.first.directory?
+      kids.first
+    else
+      raise
+    end
+    Keg.new(path).optlink
+  rescue StandardError
+    raise '#{f.opt_prefix} not present or broken\nPlease reinstall #{f.full_name}. Sorry :('
+  end
+end
+    
+        if ARGV.include?('--pinned') || ARGV.include?('--versions')
+      filtered_list
+    elsif ARGV.named.empty?
+      if ARGV.include? '--full-name'
+        full_names = Formula.installed.map(&:full_name).sort do |a, b|
+          if a.include?('/') && !b.include?('/')
+            1
+          elsif !a.include?('/') && b.include?('/')
+            -1
+          else
+            a <=> b
           end
         end
+        puts_columns full_names
+      else
+        ENV['CLICOLOR'] = nil
+        exec 'ls', *ARGV.options_only << HOMEBREW_CELLAR
+      end
+    elsif ARGV.verbose? || !$stdout.tty?
+      exec 'find', *ARGV.kegs.map(&:to_s) + %w[-not -type d -print]
+    else
+      ARGV.kegs.each { |keg| PrettyListing.new keg }
+    end
+  end
     
-      def transform_r(source, target)
-    Dir.entries(source).each do |entry|
-      next if [ '.', '..' ].include?(entry)
-      source_entry = File.join(source, entry)
-      target_entry = File.join(target, entry)
+      def python(_options = {}, &block)
+    opoo 'Formula#python is deprecated and will go away shortly.'
+    block.call if block_given?
+    PythonRequirement.new
+  end
+  alias_method :python2, :python
+  alias_method :python3, :python
+end
+
+    
+          keys = Spaceship::Portal::Key.all
+      expect(keys.size).to eq(2)
+      expect(keys.sample).to be_instance_of(Spaceship::Portal::Key)
+    end
+  end
+    
+          options[:container] = '.container'
+    end
+  end
+end
+
+    
+        version 'Guide' do
+      self.base_url = 'https://www.tensorflow.org/'
+      self.root_path = 'get_started/get_started'
+      self.initial_paths = %w(
+        programmers_guide/reading_data
+        tutorials/mandelbrot
+        performance/performance_guide
+        deploy/hadoop
+        extend/architecture)
+    
+        def filter_const(name)
+      if name.is_a? Array
+        name.map &method(:filter_const)
+      else
+        Docs.const_get '#{name}_filter'.camelize
+      end
+    end
+    
+          def stages
+        names = Dir[stage_definitions].map { |f| File.basename(f, '.rb') }
+        assert_valid_stage_names(names)
+        names
+      end
+    
+      # Implemented by subclasses to provide default values for settings needed by
+  # this plugin. Typically done using the `set_if_empty` Capistrano DSL method.
+  #
+  # Example:
+  #
+  #   def set_defaults
+  #     set_if_empty :my_plugin_option, true
+  #   end
+  #
+  def set_defaults; end
+    
+      entries.each do |entry|
+    if File.exist?(entry[:file])
+      warn '[skip] #{entry[:file]} already exists'
+    else
+      File.open(entry[:file], 'w+') do |f|
+        f.write(ERB.new(File.read(entry[:template])).result(binding))
+        puts I18n.t(:written_file, scope: :capistrano, file: entry[:file])
+      end
+    end
+  end
