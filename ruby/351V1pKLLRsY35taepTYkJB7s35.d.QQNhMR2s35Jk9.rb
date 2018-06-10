8709a0dffc4955847337bb8d9bceab62dc6150d3
SUNsung <weatherius@gmail.com>
2018-06-10 05:54:59 +0300

    Rewrite [351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb] File in: 2018-06-10 05:54:58

--- ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
+++ ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
@@ -1,143 +1,17 @@
 
         
-          bottle_ext && bottle_url_ext && bottle_ext != bottle_url_ext
-end
+        html_readme = '<html>#{Kramdown::Document.new(open('README.md').read).to_html}</html>'
+readme_doctree = REXML::Document.new(html_readme)
+links = REXML::XPath.match(readme_doctree, '//a')
     
-      def describe_path(path)
-    return 'N/A' if path.nil?
-    realpath = path.realpath
-    if realpath == path
-      path
-    else
-      '#{path} => #{realpath}'
-    end
-  end
-    
-        def render(options, screenshots)
-      Dir.mktmpdir do |dir|
-        path = generator.render(options, screenshots, dir)
-        return File.read(path)
-      end
-    end
-  end
-    
-          it 'get HG revision number' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          get_build_number_repository(
-            use_hg_revision_number: true
-          )
-        end').runner.execute(:test)
-    
-          it 'pass a custom build number to the tool' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          increment_build_number(build_number: 24, xcodeproj: '.xcproject')
-        end').runner.execute(:test)
-    
-        if test_conf['database']&.present?
-      ActiveRecord::Base.establish_connection(:test)
-      yield
-      ActiveRecord::Base.establish_connection(Rails.env.to_sym)
-    end
-  end
-    
-      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/follow
-  def follow
-    f = Follow.last
-    NotificationMailer.follow(f.target_account, Notification.find_by(activity: f))
-  end
-    
-      attributes :name, :short_name, :description,
-             :icons, :theme_color, :background_color,
-             :display, :start_url, :scope,
-             :share_target
-    
-    require 'rails_helper'
-    
-          def handle_authorization_form(auth)
-        if auth
-          process_authorization_consent('true')
-        else
-          request_authorization_consent_form
-        end
-      end
-    
-          super
-      input = @options[:input]
-      if File.directory?(input)
-        raise 'Error: '#{input.path}' is a directory (did you mean to use --recursive?)'
-      end
-      output = @options[:output]
-      output = input if @options[:in_place]
-      process_file(input, output)
-    end
-    
-    if $PROGRAM_NAME == __FILE__ && !ENV['COCOAPODS_NO_BUNDLER']
-  ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
-  require 'rubygems'
-  require 'bundler/setup'
-  $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-elsif ENV['COCOAPODS_NO_BUNDLER']
-  require 'rubygems'
-  gem 'cocoapods'
-end
-    
-          def initialize(argv)
-        super
-        config.silent = false
-      end
-    
-          def call
-        title('Gems')
-        table(all_gem_names) do |gem, row|
-          row.yellow if update_available?(gem)
-          row << gem
-          row << installed_gem_version(gem)
-          row << '(update available)' if update_available?(gem)
-        end
-      end
-    
-        # @abstract
-    #
-    # Update the clone on the deployment target
-    #
-    # @return void
-    #
-    def update
-      raise NotImplementedError, 'Your SCM strategy module should provide a #update method'
-    end
-    
-      desc 'Publish the release.'
-  task :publishing do
-  end
-    
-    module RuboCop
-  module Cop
-    module Performance
-      # This cop identifies the use of `Regexp#match` or `String#match`, which
-      # returns `#<MatchData>`/`nil`. The return value of `=~` is an integral
-      # index/`nil` and is more performant.
-      #
-      # @example
-      #   # bad
-      #   do_something if str.match(/regex/)
-      #   while regex.match('str')
-      #     do_something
-      #   end
-      #
-      #   # good
-      #   method(str =~ /regex/)
-      #   return value unless regex =~ 'str'
-      class RedundantMatch < Cop
-        MSG = 'Use `=~` in places where the `MatchData` returned by ' \
-              '`#match` will not be used.'.freeze
-    
-            def on_block(node)
-          on_body_of_reduce(node) do |body|
-            void_next = body.each_node(:next).find do |n|
-              n.children.empty? && parent_block_node(n) == node
-            end
-    
-              [optarg_positions, arg_positions]
+            def find_address
+          if @order.bill_address_id == params[:id].to_i
+            @order.bill_address
+          elsif @order.ship_address_id == params[:id].to_i
+            @order.ship_address
+          else
+            raise CanCan::AccessDenied
+          end
         end
       end
     end
@@ -145,6 +19,46 @@ end
 end
 
     
-        expect(corrected).to eq(''something'.to_sym')
-  end
-end
+            def update
+          authorize! :update, stock_location
+          if stock_location.update_attributes(stock_location_params)
+            respond_with(stock_location, status: 200, default_template: :show)
+          else
+            invalid_resource!(stock_location)
+          end
+        end
+    
+            def should_be_unused?
+          name.to_s.start_with?('_')
+        end
+    
+            def autocorrect(node)
+          lambda do |corrector|
+            each_unnecessary_space_match(node) do |range|
+              corrector.replace(range, ' ')
+            end
+          end
+        end
+    
+                  add_offense(condition)
+            end
+    
+              annotated_source.each_line do |source_line|
+            if source_line =~ ANNOTATION_PATTERN
+              annotations << [source.size, source_line]
+            else
+              source << source_line
+            end
+          end
+    
+        expect(cop.offenses.size).to eq(1)
+    expect(cop.messages).to eq(['Prefer `to_sym` over `intern`.'])
+    expect(cop.highlights).to eq(%w[intern])
+    
+          it 'autocorrects the offenses' do
+        new_source = autocorrect_source(source)
+        expect(new_source).to eq(<<-RUBY.strip_indent)
+          #{type} Parent
+            #{type} SomeObject
+              URL = %q(http://example.com)
+              WSDL = %q(http://example.com/wsdl)
