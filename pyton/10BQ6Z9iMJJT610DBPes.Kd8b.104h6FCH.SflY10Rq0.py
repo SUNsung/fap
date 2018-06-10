f837b4385cd78322809c3387adfec4b72c9e067b
SUNsung <weatherius@gmail.com>
2018-06-10 17:07:58 +0300

    Rewrite [10BQ6Z9iMJJT610DBPes.Kd8b.104h6FCH.SflY10Rq0.py] File in: 2018-06-10 17:07:58

--- pyton/10BQ6Z9iMJJT610DBPes.Kd8b.104h6FCH.SflY10Rq0.py
+++ pyton/10BQ6Z9iMJJT610DBPes.Kd8b.104h6FCH.SflY10Rq0.py
@@ -1,94 +1,185 @@
 
         
-          pretrain_ckpt = tf.train.get_checkpoint_state(model_dir)
-  if not (pretrain_ckpt and pretrain_ckpt.model_checkpoint_path):
-    raise ValueError(
-        'Asked to restore model from %s but no checkpoint found.' % model_dir)
-  saver_for_restore.restore(sess, pretrain_ckpt.model_checkpoint_path)
-    
-    import word2vec_optimized
-    
-      def test_pass(self):
-    self.assertEqual(coords.from_sgf(''), None)
-    self.assertEqual(coords.from_flat(utils_test.BOARD_SIZE, 81), None)
-    self.assertEqual(coords.from_kgs(utils_test.BOARD_SIZE, 'pass'), None)
-    self.assertEqual(coords.from_pygtp(utils_test.BOARD_SIZE, (0, 0)), None)
-    
-      def test_train(self):
-    with tempfile.TemporaryDirectory() as working_dir, \
-        tempfile.NamedTemporaryFile() as tf_record:
-      preprocessing.make_dataset_from_sgf(
-          utils_test.BOARD_SIZE, 'example_game.sgf', tf_record.name)
-      dualnet.train(
-          working_dir, [tf_record.name], 1, model_params.DummyMiniGoParams())
-    
-    
-TEST_POSITION3 = go.Position(utils_test.BOARD_SIZE)
-for coord in ((0, 0), (0, 1), (0, 2), (0, 3), (1, 1)):
-  TEST_POSITION3.play_move(coord, mutate=True)
-# resulting position should look like this:
-# X.XO.....
-# .X.......
-# .........
-    
-        final = sgf_positions[-1].position.play_move(
-        sgf_positions[-1].next_move)
-    
-      def clear(self):
-    if self.position and len(self.position.recent) > 1:
-      try:
-        sgf = self.to_sgf()
-        with open(datetime.datetime.now().strftime(
-            '%Y-%m-%d-%H:%M.sgf'), 'w') as f:
-          f.write(sgf)
-      except NotImplementedError:
-        pass
-      except:
-        print('Error saving sgf', file=sys.stderr, flush=True)
-    self.position = go.Position(komi=self.komi)
-    self.initialize_game(self.position)
-    
-      # pylint: disable=unused-argument
-  # pylint: disable=unused-variable
-def make_sgf(board_size, move_history, result_string, ruleset='Chinese',
-             komi=7.5, white_name=PROGRAM_IDENTIFIER,
-             black_name=PROGRAM_IDENTIFIER, comments=[]):
-  '''Turn a game into SGF.
-    
-      def test_make_sgf(self):
-    all_pwcs = list(replay_sgf(utils_test.BOARD_SIZE, NO_HANDICAP_SGF))
-    second_last_position, last_move, _ = all_pwcs[-1]
-    last_position = second_last_position.play_move(last_move)
-    
-      def test_ridiculously_parallel_tree_search(self):
-    player = initialize_almost_done_player()
-    # Test that an almost complete game
-    # will tree search with # parallelism > # legal moves.
-    for i in range(10):
-      player.tree_search(num_parallel=50)
-    self.assertNoPendingVirtualLosses(player.root)
-    
-    
-class TestSymmetryOperations(utils_test.MiniGoUnitTest):
-    
+        entries = []
+for v in versions:
+    fields = v.split('.')
+    year, month, day = map(int, fields[:3])
+    faked = 0
+    patchlevel = 0
+    while True:
         try:
-        s1 = os.lstat(path)
-    except OSError:
-        # the OSError should be handled with more care
-        # it could be a 'permission denied' but path is still a mount
-        return False
-    else:
-        # A symlink can never be a mount point
-        if os.path.stat.S_ISLNK(s1.st_mode):
-            return False
-    
-        port = None
-    for matching in ['bracketed_hostport', 'hostport']:
-        m = patterns[matching].match(address)
-        if m:
-            (address, port) = m.groups()
-            port = int(port)
+            datetime.date(year, month, day)
+        except ValueError:
+            day -= 1
+            faked += 1
+            assert day > 0
             continue
+        break
+    if len(fields) >= 4:
+        try:
+            patchlevel = int(fields[3])
+        except ValueError:
+            patchlevel = 1
+    timestamp = '%04d-%02d-%02dT00:%02d:%02dZ' % (year, month, day, faked, patchlevel)
+    
+    header = oldreadme[:oldreadme.index('# OPTIONS')]
+footer = oldreadme[oldreadme.index('# CONFIGURATION'):]
+    
+            self.port = random.randint(20000, 30000)
+        self.server_process = subprocess.Popen([
+            'srelay', '-f', '-i', '127.0.0.1:%d' % self.port],
+            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+                    fn = os.path.join(dirpath, basename)
+                with io.open(fn, encoding='utf-8') as inf:
+                    code = inf.read()
+    
+            retval = subprocess.call(args)
+        if retval == 0:
+            fsize = os.path.getsize(encodeFilename(tmpfilename))
+            self.to_screen('\r[%s] %s bytes' % (args[0], fsize))
+            self.try_rename(tmpfilename, filename)
+            self._hook_progress({
+                'downloaded_bytes': fsize,
+                'total_bytes': fsize,
+                'filename': filename,
+                'status': 'finished',
+            })
+            return True
+        else:
+            self.to_stderr('\n')
+            self.report_error('%s exited with code %d' % (args[0], retval))
+            return False
+
+    
+    from .common import InfoExtractor
+from ..utils import (
+    parse_duration,
+    int_or_none,
+)
+    
+        def test_contains(self):
+        n = 200
     
     
-class TerminalModule(TerminalBase):
+print('\n# ======================================================================')
+print('#                   Calculating pi, 10000 iterations')
+print('# ======================================================================\n')
+    
+    int main( int argc, char **argv)
+{
+    PyImport_FrozenModules = _PyImport_FrozenModules;
+    return PythonService_main(argc, argv);
+}
+'''
+    
+        def nearest(self, red, green, blue):
+        '''Return the name of color nearest (red, green, blue)'''
+        # BAW: should we use Voronoi diagrams, Delaunay triangulation, or
+        # octree for speeding up the locating of nearest point?  Exhaustive
+        # search is inefficient, but seems fast enough.
+        nearest = -1
+        nearest_name = ''
+        for name, aliases in self.__byrgb.values():
+            r, g, b = self.__byname[name.lower()]
+            rdelta = red - r
+            gdelta = green - g
+            bdelta = blue - b
+            distance = rdelta * rdelta + gdelta * gdelta + bdelta * bdelta
+            if nearest == -1 or distance < nearest:
+                nearest = distance
+                nearest_name = name
+        return nearest_name
+    
+    
+class MultiPathXMLRPCServer(SimpleXMLRPCServer):
+    '''Multipath XML-RPC Server
+    This specialization of SimpleXMLRPCServer allows the user to create
+    multiple Dispatcher instances and assign them to different
+    HTTP request paths.  This makes it possible to run two or more
+    'virtual XML-RPC servers' at the same port.
+    Make sure that the requestHandler accepts the paths in question.
+    '''
+    def __init__(self, addr, requestHandler=SimpleXMLRPCRequestHandler,
+                 logRequests=True, allow_none=False, encoding=None,
+                 bind_and_activate=True, use_builtin_types=False):
+    
+        def user_call(self, frame, argument_list):
+        # Adopt the same behavior as pdb and, as a side effect, skip also the
+        # first 'call' event when the Tracer is started with Tracer.runcall()
+        # which may be possibly considered as a bug.
+        if not self.stop_here(frame):
+            return
+        self.process_event('call', frame, argument_list)
+        self.next_set_method()
+    
+    from idlelib.config import idleConf
+from idlelib.delegator import Delegator
+    
+    # dataclass_module_1.py and dataclass_module_1_str.py are identical
+# except only the latter uses string annotations.
+    
+    @dataclass
+class CV:
+    T_CV4 = ClassVar
+    cv0: ClassVar[int] = 20
+    cv1: ClassVar = 30
+    cv2: T_CV2
+    cv3: T_CV3
+    not_cv4: T_CV4  # When using string annotations, this field is not recognized as a ClassVar.
+    
+            self.assertEqual(set([CANCELLED_AND_NOTIFIED_FUTURE,
+                              EXCEPTION_FUTURE,
+                              SUCCESSFUL_FUTURE]),
+                         completed_futures)
+    
+    
+  def _HandleDetailedInfoResponse( self ):
+    vimsupport.WriteToPreviewWindow( self._response[ 'detailed_info' ] )
+    
+    
+def SendEventNotificationAsync( event_name,
+                                buffer_number = None,
+                                extra_data = None ):
+  event = EventNotification( event_name, buffer_number, extra_data )
+  event.Start()
+
+    
+    
+  def Poll( self, diagnostics_handler ):
+    '''This should be called regularly to check for new messages in this buffer.
+    Returns True if Poll should be called again in a while. Returns False when
+    the completer or server indicated that further polling should not be done
+    for the requested file.'''
+    
+    ATTR_URL = 'url'
+ATTR_URL_DEFAULT = 'https://www.google.com'
+    
+    
+def run(args):
+    '''Handle ensure config commandline script.'''
+    parser = argparse.ArgumentParser(
+        description=('Ensure a Home Assistant config exists, '
+                     'creates one if necessary.'))
+    parser.add_argument(
+        '-c', '--config',
+        metavar='path_to_config_dir',
+        default=config_util.get_default_config_dir(),
+        help='Directory that contains the Home Assistant configuration')
+    parser.add_argument(
+        '--script',
+        choices=['ensure_config'])
+    
+        @patch('{}._get_homehub_data'.format(patch_file), new=_get_homehub_data)
+    def test_config_minimal(self):
+        '''Test the setup with minimal configuration.'''
+        config = {
+            'device_tracker': {
+                CONF_HOST: 'foo'
+            }
+        }
+        result = bt_home_hub_5.get_scanner(None, config)
+    
+        def tearDown(self):  # pylint: disable=invalid-name
+        '''Clean up.'''
+        self.session.rollback()
