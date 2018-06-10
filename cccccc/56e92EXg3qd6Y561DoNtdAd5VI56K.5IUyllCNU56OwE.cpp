1ed7719ac57aa598a44b5cc6f37205b826d2bf48
SUNsung <weatherius@gmail.com>
2018-06-10 03:04:52 +0300

    Rewrite [56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp] File in: 2018-06-10 03:04:51

--- cccccc/56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp
+++ cccccc/56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp
@@ -1,182 +1,126 @@
 
         
-                if(is_empty(&cam_points_k))
-        {
-            cv::Mat C_valid = C_est[k], t_valid = t_est[k];
-            double cost_valid = cost[k];
-    }
+        
+    {	verify_ssl = p_verify_ssl;
+}
     
-            // Version: 1.3
-        TEXTURE0                         = 0x84C0,
-        TEXTURE1                         = 0x84C1,
-        TEXTURE2                         = 0x84C2,
-        TEXTURE3                         = 0x84C3,
-        TEXTURE4                         = 0x84C4,
-        TEXTURE5                         = 0x84C5,
-        TEXTURE6                         = 0x84C6,
-        TEXTURE7                         = 0x84C7,
-        TEXTURE8                         = 0x84C8,
-        TEXTURE9                         = 0x84C9,
-        TEXTURE10                        = 0x84CA,
-        TEXTURE11                        = 0x84CB,
-        TEXTURE12                        = 0x84CC,
-        TEXTURE13                        = 0x84CD,
-        TEXTURE14                        = 0x84CE,
-        TEXTURE15                        = 0x84CF,
-        TEXTURE16                        = 0x84D0,
-        TEXTURE17                        = 0x84D1,
-        TEXTURE18                        = 0x84D2,
-        TEXTURE19                        = 0x84D3,
-        TEXTURE20                        = 0x84D4,
-        TEXTURE21                        = 0x84D5,
-        TEXTURE22                        = 0x84D6,
-        TEXTURE23                        = 0x84D7,
-        TEXTURE24                        = 0x84D8,
-        TEXTURE25                        = 0x84D9,
-        TEXTURE26                        = 0x84DA,
-        TEXTURE27                        = 0x84DB,
-        TEXTURE28                        = 0x84DC,
-        TEXTURE29                        = 0x84DD,
-        TEXTURE30                        = 0x84DE,
-        TEXTURE31                        = 0x84DF,
-        ACTIVE_TEXTURE                   = 0x84E0,
-        MULTISAMPLE                      = 0x809D,
-        SAMPLE_ALPHA_TO_COVERAGE         = 0x809E,
-        SAMPLE_ALPHA_TO_ONE              = 0x809F,
-        SAMPLE_COVERAGE                  = 0x80A0,
-        SAMPLE_BUFFERS                   = 0x80A8,
-        SAMPLES                          = 0x80A9,
-        SAMPLE_COVERAGE_VALUE            = 0x80AA,
-        SAMPLE_COVERAGE_INVERT           = 0x80AB,
-        TEXTURE_CUBE_MAP                 = 0x8513,
-        TEXTURE_BINDING_CUBE_MAP         = 0x8514,
-        TEXTURE_CUBE_MAP_POSITIVE_X      = 0x8515,
-        TEXTURE_CUBE_MAP_NEGATIVE_X      = 0x8516,
-        TEXTURE_CUBE_MAP_POSITIVE_Y      = 0x8517,
-        TEXTURE_CUBE_MAP_NEGATIVE_Y      = 0x8518,
-        TEXTURE_CUBE_MAP_POSITIVE_Z      = 0x8519,
-        TEXTURE_CUBE_MAP_NEGATIVE_Z      = 0x851A,
-        PROXY_TEXTURE_CUBE_MAP           = 0x851B,
-        MAX_CUBE_MAP_TEXTURE_SIZE        = 0x851C,
-        COMPRESSED_RGB                   = 0x84ED,
-        COMPRESSED_RGBA                  = 0x84EE,
-        TEXTURE_COMPRESSION_HINT         = 0x84EF,
-        TEXTURE_COMPRESSED_IMAGE_SIZE    = 0x86A0,
-        TEXTURE_COMPRESSED               = 0x86A1,
-        NUM_COMPRESSED_TEXTURE_FORMATS   = 0x86A2,
-        COMPRESSED_TEXTURE_FORMATS       = 0x86A3,
-        CLAMP_TO_BORDER                  = 0x812D,
-    
-    #ifdef HAVE_LAPACK
-    
-        static void* WinGetProcAddress(const char* name)
-    {
-        static HMODULE opencl_module = NULL;
-        if (!opencl_module)
-        {
-            opencl_module = GetModuleHandleA('clAmdBlas.dll');
-            if (!opencl_module)
-            {
-                opencl_module = LoadLibraryA('clAmdBlas.dll');
-                if (!opencl_module)
-                    return NULL;
-            }
-        }
-        return (void*)GetProcAddress(opencl_module, name);
-    }
-    #define CV_CL_GET_PROC_ADDRESS(name) WinGetProcAddress(name)
-#endif // _WIN32
-    
-    template <typename T>
-class TypeWithoutFormatter<T, kConvertibleToInteger> {
- public:
-  // Since T has no << operator or PrintTo() but can be implicitly
-  // converted to BiggestInt, we print it as a BiggestInt.
-  //
-  // Most likely T is an enum type (either named or unnamed), in which
-  // case printing it as an integer is the desired behavior.  In case
-  // T is not an enum, printing it as an integer is the best we can do
-  // given that it has no user-defined printer.
-  static void PrintValue(const T& value, ::std::ostream* os) {
-    const internal::BiggestInt kBigInt = value;
-    *os << kBigInt;
-  }
-};
-    
-      // Returns the TestInfo object for the test that's currently running,
-  // or NULL if no test is running.
-  const TestInfo* current_test_info() const
-      GTEST_LOCK_EXCLUDED_(mutex_);
-    
-    // This macro is for implementing ASSERT/EXPECT_DEBUG_DEATH when compiled in
-// NDEBUG mode. In this case we need the statements to be executed, the regex is
-// ignored, and the macro must accept a streamed message even though the message
-// is never printed.
-# define GTEST_EXECUTE_STATEMENT_(statement, regex) \
-  GTEST_AMBIGUOUS_ELSE_BLOCKER_ \
-  if (::testing::internal::AlwaysTrue()) { \
-     GTEST_SUPPRESS_UNREACHABLE_CODE_WARNING_BELOW_(statement); \
-  } else \
-    ::testing::Message()
-    
-    // In order to catch the mistake of putting tests that use different
-// test fixture classes in the same test case, we need to assign
-// unique IDs to fixture classes and compare them.  The TypeId type is
-// used to hold such IDs.  The user should treat TypeId as an opaque
-// type: the only operation allowed on TypeId values is to compare
-// them for equality using the == operator.
-typedef const void* TypeId;
-    
-      bool check_for_leaks = false;
-  if (argc > 1 && strcmp(argv[1], '--check_for_leaks') == 0 )
-    check_for_leaks = true;
-  else
-    printf('%s\n', 'Run this program with --check_for_leaks to enable '
-           'custom leak checking in the tests.');
-    
-    
-    {    dbg_printf(
-        'IOCTL alloc: size %lu, location %p\n', alloc->size, alloc->buffer);
-    break;
-  default:
-    err = -ENOTTY;
-    goto error_exit;
-    break;
-  }
-    
-    TEST_F(ViewsConfigParserPluginTests, test_add_view) {
-  Config c;
-  auto s = c.update(getTestConfigMap());
-  EXPECT_TRUE(s.ok());
-    }
-    
-        uid_t expected_user = 0U;
-    EXPECT_EQ(dropper->to_user_, expected_user);
-    
-      // If a constraint list does not exist, then all checks will match.
-  // If there is no predicate clause then all results will match.
-  EXPECT_TRUE(cm['not_path'].matches('some'));
-  EXPECT_TRUE(cm['not_path'].notExistsOrMatches('some'));
-  EXPECT_FALSE(cm['not_path'].exists());
-  EXPECT_FALSE(cm['not_path'].existsAndMatches('some'));
-    
-      // Check that all value lengths were maxed.
-  std::map<std::string, size_t> expected = {
-      {'name', 10}, {'age', 4}, {'food', 23}, {'number', 2}};
-  EXPECT_EQ(lengths, expected);
-    
-        int64_t iterations;
-    TimeUnit time_unit;
-    double real_accumulated_time;
-    double cpu_accumulated_time;
-    
-    #if defined(HAVE_STD_REGEX)
-#include <regex>
-#elif defined(HAVE_GNU_POSIX_REGEX)
-#include <gnuregex.h>
-#elif defined(HAVE_POSIX_REGEX)
-#include <regex.h>
-#else
-#error No regular expression backend was found!
+    #if defined(MBEDTLS_RSA_C) && ( !defined(MBEDTLS_BIGNUM_C) ||         \
+    !defined(MBEDTLS_OID_C) )
+#error 'MBEDTLS_RSA_C defined, but not all prerequisites'
 #endif
-#include <string>
+    
+    // Singleton
+class _VisualScriptEditor : public Object {
+	GDCLASS(_VisualScriptEditor, Object);
+    }
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+    
+    
+    {  return symbol;
+}
+    
+       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+    
+      // Open database.  Disable compression since it affects the creation
+  // of layers and the code below is trying to test against a very
+  // specific scenario.
+  leveldb::DB* db;
+  leveldb::Options db_options;
+  db_options.create_if_missing = true;
+  db_options.compression = leveldb::kNoCompression;
+  ASSERT_OK(leveldb::DB::Open(db_options, dbpath, &db));
+    
+    
+    {}
+    
+    
+    {
+    {    // Add hash marks based on percentage; 20 marks for 100%.
+    int marks = static_cast<int>(20*(buckets_[b] / num_) + 0.5);
+    r.append(marks, '#');
+    r.push_back('\n');
+  }
+  return r;
+}
+    
+    // Helper class that locks a mutex on construction and unlocks the mutex when
+// the destructor of the MutexLock object is invoked.
+//
+// Typical usage:
+//
+//   void MyClass::MyMethod() {
+//     MutexLock l(&mu_);       // mu_ is an instance variable
+//     ... some complex code, possibly with multiple return paths ...
+//   }
+    
+    LookupKey::LookupKey(const Slice& user_key, SequenceNumber s) {
+  size_t usize = user_key.size();
+  size_t needed = usize + 13;  // A conservative estimate
+  char* dst;
+  if (needed <= sizeof(space_)) {
+    dst = space_;
+  } else {
+    dst = new char[needed];
+  }
+  start_ = dst;
+  dst = EncodeVarint32(dst, usize + 8);
+  kstart_ = dst;
+  memcpy(dst, user_key.data(), usize);
+  dst += usize;
+  EncodeFixed64(dst, PackSequenceAndType(s, kValueTypeForSeek));
+  dst += 8;
+  end_ = dst;
+}
+    
+    TEST(FormatTest, InternalKeyShortestSuccessor) {
+  ASSERT_EQ(IKey('g', kMaxSequenceNumber, kValueTypeForSeek),
+            ShortSuccessor(IKey('foo', 100, kTypeValue)));
+  ASSERT_EQ(IKey('\xff\xff', 100, kTypeValue),
+            ShortSuccessor(IKey('\xff\xff', 100, kTypeValue)));
+}
+    
+    struct BlockContents;
+class Comparator;
+    
+      FOLLY_CPP14_CONSTEXPR BasicFixedString& operator=(
+      const BasicFixedString&) noexcept = default;
+    
+      // Test reserve from internal buffer, this used to segfault
+  {
+    unique_ptr<IOBuf> iob(IOBuf::create(0));
+    iob->reserve(0, 2000);
+    EXPECT_EQ(0, iob->headroom());
+    EXPECT_LE(2000, iob->tailroom());
+  }
+    
+    
+    {} // namespace folly
+
+    
+    // Licensed under the MIT License (the 'License'); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+// http://opensource.org/licenses/MIT
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
