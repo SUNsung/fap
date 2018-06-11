919be75167c07ff39af3ce8405533923ec7e6d96
SUNsung <weatherius@gmail.com>
2018-06-11 10:16:33 +0300

    Rewrite [499.uIM2vfqN.49Mb7ia7CsFt249aOpuwkuLsQc49WzG.py] File in: 2018-06-11 10:16:32

--- pyton/499.uIM2vfqN.49Mb7ia7CsFt249aOpuwkuLsQc49WzG.py
+++ pyton/499.uIM2vfqN.49Mb7ia7CsFt249aOpuwkuLsQc49WzG.py
@@ -1,103 +1,42 @@
 
         
-            if not opts and not args:
-        # Display help.
-        print(_help)
-        # Enter GUI mode.
-        #from .gui import gui_main
-        #gui_main()
-    else:
-        conf = {}
-        for opt, arg in opts:
-            if opt in ('-h', '--help'):
-                # Display help.
-                print(_help)
-    
-    site_info = 'CBS.com'
-download = cbs_download
-download_playlist = playlist_not_supported('cbs')
-
-    
-    	html = get_html(url)
-	contentid = r1(r'<meta name='contentid' scheme='DMINSTR2' content='([^']+)' />', html)
-	vid = r1(r''demand_ehow_videoid':'([^']+)'', html)
-	assert vid
-    
-    site_info = 'FC2Video'
-download = fc2video_download
-download_playlist = playlist_not_supported('fc2video')
-
+        def sort_blocks():
+    # First, we load the current README into memory
+    with open('README.md', 'r') as read_me_file:
+        read_me = read_me_file.read()
     
     
-download = huaban_download
-download_playlist = playlist_not_supported('huaban')
-
+def main():
+    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error('Expected an input and an output filename')
     
+    import re
     
+            title = self._html_search_regex(r'<title>(.*?)</title>', webpage, 'title')
+        description = self._html_search_regex(
+            r'<div class='description'[^>]*>([^<]+)</div>', webpage, 'description', fatal=False)
+        thumbnail = self._html_search_regex(
+            r'preview_url\s*:\s*\'(.*?)\'', webpage, 'thumbnail', fatal=False)
     
-        stream_types = [
-        {'id': '720p HD'},
-        {'id': '360p SD'},
-    ]
+        def __init__(self, rest, name):
+        '''Initialize a PVOutput sensor.'''
+        self.rest = rest
+        self._name = name
+        self.pvcoutput = None
+        self.status = namedtuple(
+            'status', [ATTR_DATE, ATTR_TIME, ATTR_ENERGY_GENERATION,
+                       ATTR_POWER_GENERATION, ATTR_ENERGY_CONSUMPTION,
+                       ATTR_POWER_CONSUMPTION, ATTR_EFFICIENCY,
+                       ATTR_TEMPERATURE, ATTR_VOLTAGE])
     
-                # log10(value) == exp, so log(value) == log10(value)/log10(e) ==
-            # exp/LOG10E
-            expected = exp / LOG10E
-            log = math.log(value)
-            self.assertAlmostEqual(log, expected)
-    
-        def test_create_entries(self):
-        self.dialog.top = self.root
-        self.dialog.row = 0
-        self.engine.setpat('hello')
-        self.dialog.create_entries()
-        self.assertIn(self.dialog.ent.get(), 'hello')
-    
-            User enters a name for the Help resource and a web url or file
-        name. The user can browse for the file.
-        '''
-        self.filepath = filepath
-        message = 'Name for item on Help menu:'
-        super().__init__(
-                parent, title, message, text0=menuitem,
-                used_names=used_names, _htest=_htest, _utest=_utest)
-    
-            # Handle comparison errors
-        d = deque(['a', 'b', BadCmp(), 'c'])
-        e = deque(d)
-        self.assertRaises(RuntimeError, d.remove, 'c')
-        for x, y in zip(d, e):
-            # verify that original order and values are retained.
-            self.assertTrue(x is y)
-    
-                    if not args:
-                    # args=NULL, nargs=0
-                    result = _testcapi.pyobject_fastcall(func, None)
-                    self.check_result(result, expected)
-    
-    try:
-    import zlib
-except ImportError:
-    zlib = None
-    
-            # unregister an unknown file obj
-        self.assertRaises(KeyError, s.unregister, 999999)
-    
-        except (AttributeError, OSError):
-        return default
-    output = proc.communicate()[0].decode('latin-1')
-    rc = proc.wait()
-    if not output or rc:
-        return default
-    else:
-        return output
-    
-        def test_exec_failure(self):
-        self.create_module('foo', '1/0')
-        try:
-            import foo
-        except ZeroDivisionError as e:
-            tb = e.__traceback__
-        else:
-            self.fail('ZeroDivisionError should have been raised')
-        self.assert_traceback(tb, [__file__, 'foo.py'])
+        @patch('{}._get_homehub_data'.format(patch_file), new=_get_homehub_data)
+    def test_config_minimal(self):
+        '''Test the setup with minimal configuration.'''
+        config = {
+            'device_tracker': {
+                CONF_HOST: 'foo'
+            }
+        }
+        result = bt_home_hub_5.get_scanner(None, config)
