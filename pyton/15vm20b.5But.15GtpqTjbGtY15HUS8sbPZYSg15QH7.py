3c4c047ac1c730cba85beca746a00633082d528d
SUNsung <weatherius@gmail.com>
2018-06-11 15:01:05 +0300

    Rewrite [15vm20b.5But.15GtpqTjbGtY15HUS8sbPZYSg15QH7.py] File in: 2018-06-11 15:01:05

--- pyton/15vm20b.5But.15GtpqTjbGtY15HUS8sbPZYSg15QH7.py
+++ pyton/15vm20b.5But.15GtpqTjbGtY15HUS8sbPZYSg15QH7.py
@@ -1,56 +1,30 @@
 
         
-                if is_json:
-            content = json.dumps(response.json(), sort_keys=True, indent=4)
-        else:
-            content = response.content
+        # Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named 'default.css' will overwrite the builtin 'default.css'.
+html_static_path = ['_static']
     
-        parser.add_argument('--key',
-                        metavar='KEY',
-                        default=api_key,
-                        required=not api_key,
-                        help='Shippable API key')
-    
-        if result is None:
-        return '{}'
-    
-        terminal_stdout_re = [
-        re.compile(br'[\r\n]?[\w+\-\.:\/\[\]]+(?:\([^\)]+\)){,3}(?:>|#) ?$'),
-        re.compile(br'\@[\w\-\.]+:\S+?[>#\$] ?$')
-    ]
+        @classmethod
+    def add_parser_arguments(cls, parser):
+        '''Adds command line arguments needed by the plugin'''
     
     
-def sort_groups(groups):
-    return sorted(groups, key=lambda g: (g.depth, g.priority, g.name))
-    
-        def construct_yaml_seq(self, node):
-        data = AnsibleSequence()
-        yield data
-        data.extend(self.construct_sequence(node))
-        data.ansible_pos = self._node_position_info(node)
-    
-    '''
-@author: Eugene Duboviy <eugene.dubovoy@gmail.com> | github.com/duboviy
-    
-        def test_sequential_execution(self):
-        self.command_stack[0].execute()
-        output_after_first_execution = os.listdir(self.test_dir)
-        self.assertEqual(output_after_first_execution[0], 'bar.txt')
-        self.command_stack[1].execute()
-        output_after_second_execution = os.listdir(self.test_dir)
-        self.assertEqual(output_after_second_execution[0], 'baz.txt')
-    
-    ### OUTPUT ###
-# Jack move 5m then stop
+if __name__ == '__main__':
+    unittest.main()  # pragma: no cover
 
     
-        def test_initial_state(self):
-        state = self.radio.state.name
-        expected_state_name = 'AM'
-        self.assertEqual(state, expected_state_name)
     
-    production code which is untestable:
+# -- Options for HTML output ----------------------------------------------
     
-        def publishReport(self):
-        self._db.update()
-        self._reporter.report()
+        @mock.patch('certbot.display.enhancements.util')
+    def test_secure(self, mock_util):
+        mock_util().menu.return_value = (display_util.OK, 1)
+        self.assertTrue(self._call())
+    
+        description = 'Example Installer plugin'
+    
+    
+def html_page_context(app, pagename, templatename, context, doctree):
+    if templatename != 'page.html':
+        return
