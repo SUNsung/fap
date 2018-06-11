351f1eb862903b4089179cf86e52d49799b57c95
SUNsung <weatherius@gmail.com>
2018-06-12 02:16:24 +0300

    Rewrite [18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp] File in: 2018-06-12 02:16:23

--- cccccc/18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp
+++ cccccc/18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp
@@ -1,110 +1,188 @@
 
         
-         protected:
-  explicit GlobalShortcut(v8::Isolate* isolate);
-  ~GlobalShortcut() override;
+          /* Invoked when initial response headers are received.
+   * Consumer must call bidirectional_stream_read() to start reading.
+   * Consumer may call bidirectional_stream_write() to start writing or
+   * close the stream. Contents of |headers| is valid for duration of the call.
+   */
+  void (*on_response_headers_received)(
+      bidirectional_stream* stream,
+      const bidirectional_stream_header_array* headers,
+      const char* negotiated_protocol);
     
-    #ifndef ATOM_BROWSER_UNRESPONSIVE_SUPPRESSOR_H_
-#define ATOM_BROWSER_UNRESPONSIVE_SUPPRESSOR_H_
+    #ifndef GRPC_COMMON_CPP_ROUTE_GUIDE_HELPER_H_
+#define GRPC_COMMON_CPP_ROUTE_GUIDE_HELPER_H_
     
+      bool generate_in_pb2_grpc;
     
-    { protected:
-  virtual ~WindowListObserver() {}
-};
+    #include 'src/compiler/python_generator.h'
     
-    class RemoteCallbackFreer : public ObjectLifeMonitor,
-                            public content::WebContentsObserver {
- public:
-  static void BindTo(v8::Isolate* isolate,
-                     v8::Local<v8::Object> target,
-                     int object_id,
-                     content::WebContents* web_conents);
+    	PathCombine(userCfgPath, userConfigDirPath, L'user-ConEmu.xml');
+    
+    void OutputImageComponent::ToPixels(int xmin, int ymin, int xsize, int ysize,
+                                    uint8_t* out, int stride) const {
+  assert(xmin >= 0);
+  assert(ymin >= 0);
+  assert(xmin < width_);
+  assert(ymin < height_);
+  const int yend1 = ymin + ysize;
+  const int yend0 = std::min(yend1, height_);
+  int y = ymin;
+  for (; y < yend0; ++y) {
+    const int xend1 = xmin + xsize;
+    const int xend0 = std::min(xend1, width_);
+    int x = xmin;
+    int px = y * width_ + xmin;
+    for (; x < xend0; ++x, ++px, out += stride) {
+      *out = static_cast<uint8_t>((pixels_[px] + 8 - (x & 1)) >> 4);
     }
-    
-    
-    {}  // namespace atom
-    
-      const base::ListValue* preferences() const { return preferences_.get(); }
-    
-    #ifndef CHROME_BROWSER_EXTENSIONS_GLOBAL_SHORTCUT_LISTENER_WIN_H_
-#define CHROME_BROWSER_EXTENSIONS_GLOBAL_SHORTCUT_LISTENER_WIN_H_
-    
-      // Sets time interval between updates. By default list of sources and their
-  // thumbnail are updated once per second. If called after StartUpdating() then
-  // it will take effect only after the next update.
-  virtual void SetUpdatePeriod(base::TimeDelta period) = 0;
-    
-    namespace printing {
-    }
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(ChromeBrowserPepperHostFactory);
-};
-    
-    
-    {    IntrinsicParams();
-    IntrinsicParams(Vec2d f, Vec2d c, Vec4d k, double alpha = 0);
-    IntrinsicParams operator+(const Mat& a);
-    IntrinsicParams& operator =(const Mat& a);
-    void Init(const cv::Vec2d& f, const cv::Vec2d& c, const cv::Vec4d& k = Vec4d(0,0,0,0), const double& alpha = 0);
-};
-    
-        int pointsCount = cvtest::randInt(rng) % maxPointsCount;
-    usedPointsCount = pointsCount == 0 ? 0 : cvtest::randInt(rng) % pointsCount;
-    
-    //! @endcond
-    
-    namespace cv
-{
-    }
-    
-    //
-// END OF CUSTOM FUNCTIONS HERE
-//
-    
-    #include 'opencv2/core/opencl/runtime/opencl_core.hpp'
-#include 'opencv2/core/opencl/runtime/opencl_clamdfft.hpp'
-    
-    struct DynamicFnEntry
-{
-    const char* fnName; // 'clCreateContext'
-    void** ppFn; // &clCreateContext_pfn
-};
-    
-        const_buffer source_buffer =
-      const_buffer(*source_iter) + source_buffer_offset;
-    
-    template <typename Stream>
-class buffered_read_stream;
-    
-    namespace boost {
-namespace asio {
-    }
-    }
-    
-    
-    {} // namespace boost
-    
-    #include <boost/asio/detail/config.hpp>
-    
-      // Insert an element into the values list by splicing from the spares list,
-  // if a spare is available, and otherwise by inserting a new element.
-  iterator values_insert(iterator it, const value_type& v)
-  {
-    if (spares_.empty())
-    {
-      return values_.insert(it, v);
-    }
-    else
-    {
-      spares_.front() = v;
-      values_.splice(it, spares_, spares_.begin());
-      return --it;
+    const int offset = -stride;
+    for (; x < xend1; ++x) {
+      *out = out[offset];
+      out += stride;
     }
   }
-    
-    
-    {  if (result == 0)
-    ec = boost::system::error_code();
-  return result;
+  for (; y < yend1; ++y) {
+    const int offset = -stride * xsize;
+    for (int x = 0; x < xsize; ++x) {
+      *out = out[offset];
+      out += stride;
+    }
+  }
 }
+    
+    static const int kCrToRedTable[256] = {
+  -179, -178, -177, -175, -174, -172, -171, -170, -168, -167, -165, -164,
+  -163, -161, -160, -158, -157, -156, -154, -153, -151, -150, -149, -147,
+  -146, -144, -143, -142, -140, -139, -137, -136, -135, -133, -132, -130,
+  -129, -128, -126, -125, -123, -122, -121, -119, -118, -116, -115, -114,
+  -112, -111, -109, -108, -107, -105, -104, -102, -101, -100,  -98,  -97,
+   -95,  -94,  -93,  -91,  -90,  -88,  -87,  -86,  -84,  -83,  -81,  -80,
+   -79,  -77,  -76,  -74,  -73,  -72,  -70,  -69,  -67,  -66,  -64,  -63,
+   -62,  -60,  -59,  -57,  -56,  -55,  -53,  -52,  -50,  -49,  -48,  -46,
+   -45,  -43,  -42,  -41,  -39,  -38,  -36,  -35,  -34,  -32,  -31,  -29,
+   -28,  -27,  -25,  -24,  -22,  -21,  -20,  -18,  -17,  -15,  -14,  -13,
+   -11,  -10,   -8,   -7,   -6,   -4,   -3,   -1,    0,    1,    3,    4,
+     6,    7,    8,   10,   11,   13,   14,   15,   17,   18,   20,   21,
+    22,   24,   25,   27,   28,   29,   31,   32,   34,   35,   36,   38,
+    39,   41,   42,   43,   45,   46,   48,   49,   50,   52,   53,   55,
+    56,   57,   59,   60,   62,   63,   64,   66,   67,   69,   70,   72,
+    73,   74,   76,   77,   79,   80,   81,   83,   84,   86,   87,   88,
+    90,   91,   93,   94,   95,   97,   98,  100,  101,  102,  104,  105,
+   107,  108,  109,  111,  112,  114,  115,  116,  118,  119,  121,  122,
+   123,  125,  126,  128,  129,  130,  132,  133,  135,  136,  137,  139,
+   140,  142,  143,  144,  146,  147,  149,  150,  151,  153,  154,  156,
+   157,  158,  160,  161,  163,  164,  165,  167,  168,  170,  171,  172,
+   174,  175,  177,  178
+};
+    
+    // Performs in-place floating point 8x8 DCT on block[0..63].
+// Note that the DCT used here is the DCT-2 with the first term multiplied by
+// 1/sqrt(2) and the result scaled by 1/2.
+void ComputeBlockDCTDouble(double block[64]);
+    
+    // Computes the DCT (Discrete Cosine Transform) of the 8x8 array in 'block',
+// scaled up by a factor of 16. The values in 'block' are laid out row-by-row
+// and the result is written to the same memory area.
+void ComputeBlockDCT(coeff_t* block);
+    
+    // kIDCTMatrix[8*x+u] = alpha(u)*cos((2*x+1)*u*M_PI/16)*sqrt(2), with fixed 13
+// bit precision, where alpha(0) = 1/sqrt(2) and alpha(u) = 1 for u > 0.
+// Some coefficients are off by +-1 to mimick libjpeg's behaviour.
+static const int kIDCTMatrix[kDCTBlockSize] = {
+  8192,  11363,  10703,   9633,   8192,   6437,   4433,   2260,
+  8192,   9633,   4433,  -2259,  -8192, -11362, -10704,  -6436,
+  8192,   6437,  -4433, -11362,  -8192,   2261,  10704,   9633,
+  8192,   2260, -10703,  -6436,   8192,   9633,  -4433, -11363,
+  8192,  -2260, -10703,   6436,   8192,  -9633,  -4433,  11363,
+  8192,  -6437,  -4433,  11362,  -8192,  -2261,  10704,  -9633,
+  8192,  -9633,   4433,   2259,  -8192,  11362, -10704,   6436,
+  8192, -11363,  10703,  -9633,   8192,  -6437,   4433,  -2260,
+};
+    
+    // Mimic libjpeg's heuristics to guess jpeg color space.
+// Requires that the jpg has 3 components.
+bool HasYCbCrColorSpace(const JPEGData& jpg) {
+  bool has_Adobe_marker = false;
+  uint8_t Adobe_transform = 0;
+  for (const std::string& app : jpg.app_data) {
+    if (static_cast<uint8_t>(app[0]) == 0xe0) {
+      return true;
+    } else if (static_cast<uint8_t>(app[0]) == 0xee && app.size() >= 15) {
+      has_Adobe_marker = true;
+      Adobe_transform = app[14];
+    }
+  }
+  if (has_Adobe_marker) {
+    return (Adobe_transform != 0);
+  }
+  const int cid0 = jpg.components[0].id;
+  const int cid1 = jpg.components[1].id;
+  const int cid2 = jpg.components[2].id;
+  return (cid0 != 'R' || cid1 != 'G' || cid2 != 'B');
+}
+    
+    
+    {  return true;
+}
+    
+    #ifndef GUETZLI_JPEG_ERROR_H_
+#define GUETZLI_JPEG_ERROR_H_
+    
+    static const int kJpegHuffmanRootTableBits = 8;
+// Maximum huffman lookup table size.
+// According to zlib/examples/enough.c, 758 entries are always enough for
+// an alphabet of 257 symbols (256 + 1 special symbol for the all 1s code) and
+// max bit length 16 if the root table has 8 bits.
+static const int kJpegHuffmanLutSize = 758;
+    
+    // Preprocesses U and V channel for better results after downsampling.
+    
+    
+    {                if (ImGui::Button('OK', ImVec2(120,0))) { ImGui::CloseCurrentPopup(); }
+                ImGui::SetItemDefaultFocus();
+                ImGui::SameLine();
+                if (ImGui::Button('Cancel', ImVec2(120,0))) { ImGui::CloseCurrentPopup(); }
+                ImGui::EndPopup();
+            }
+    
+    struct FrameContext
+{
+    ID3D12CommandAllocator* CommandAllocator;
+    UINT64                  FenceValue;
+};
+    
+        // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them. 
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple. 
+    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Read 'misc/fonts/README.txt' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Roboto-Medium.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Cousine-Regular.ttf', 15.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/DroidSans.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/ProggyTiny.ttf', 10.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF('c:\\Windows\\Fonts\\ArialUni.ttf', 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+    //IM_ASSERT(font != NULL);
+    
+        // items_count:  Use -1 to ignore (you can call Begin later). Use INT_MAX if you don't know how many items you have (in which case the cursor won't be advanced in the final step).
+    // items_height: Use -1.0f to be calculated automatically on first step. Otherwise pass in the distance between your items, typically GetTextLineHeightWithSpacing() or GetFrameHeightWithSpacing().
+    // If you don't specify an items_height, you NEED to call Step(). If you specify items_height you may call the old Begin()/End() api directly, but prefer calling Step().
+    ImGuiListClipper(int items_count = -1, float items_height = -1.0f)  { Begin(items_count, items_height); } // NB: Begin() initialize every fields (as we allow user to call Begin/End multiple times on a same instance if they want).
+    ~ImGuiListClipper()                                                 { IM_ASSERT(ItemsCount == -1); }      // Assert if user forgot to call End() or Step() until false.
+    
+            const uint32_t w = ft_bitmap->bitmap.width;
+        const uint32_t h = ft_bitmap->bitmap.rows;
+        const uint8_t* src = ft_bitmap->bitmap.buffer;
+        const uint32_t src_pitch = ft_bitmap->bitmap.pitch;
+    
+        if (int size = s3eClipboardGetText(NULL, 0))
+    {
+        if (g_ClipboardText)
+            delete[] g_ClipboardText;
+        g_ClipboardText = new char[size];
+        g_ClipboardText[0] = '\0';
+        s3eClipboardGetText(g_ClipboardText, size);
+    }
