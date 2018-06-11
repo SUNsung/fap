ca1ebb5356a4906f9684384fcb8f914ab7e59f48
SUNsung <weatherius@gmail.com>
2018-06-11 03:48:31 +0300

    Rewrite [55uXiG.Zi9qS255Ypq2xUoiEE554wF49PUDjdk55ca8.rb] File in: 2018-06-11 03:48:30

--- ruby/55uXiG.Zi9qS255Ypq2xUoiEE554wF49PUDjdk55ca8.rb
+++ ruby/55uXiG.Zi9qS255Ypq2xUoiEE554wF49PUDjdk55ca8.rb
@@ -1,68 +1,26 @@
 
         
-          def fish_completion_caveats
-    if keg && keg.completion_installed?(:fish) && which('fish') then <<-EOS.undent
-      fish completion has been installed to:
-        #{HOMEBREW_PREFIX}/share/fish/vendor_completions.d
-      EOS
-    end
-  end
+              html_filters.push 'bootstrap/entries_v4', 'bootstrap/clean_html_v4'
     
-        [@f.bin, @f.sbin, @f.lib].each { |d| clean_dir(d) if d.exist? }
+          options[:only_patterns] = [/\Ad3[\-\w]+\z/, /\Ad3\/blob\/master\/changes\.md\z/i]
+      options[:skip_patterns] = [/3\.x-api-reference/]
     
-          update_disk_cleanup_size(path.disk_usage)
+        version '1.9' do
+      self.release = '1.9.13'
+      self.dir = '/Users/Thibaut/DevDocs/Docs/Django19'
+      self.base_url = 'https://docs.djangoproject.com/en/1.9/'
     end
     
-      def validate_type
-    errors.add(:type, 'cannot be changed once an instance has been created') if type_changed? && !new_record?
-    errors.add(:type, 'is not a valid type') unless self.class.valid_type?(type)
-  end
+        version 'Sys' do
+      self.base_url = 'https://api.haxe.org/sys/'
+    end
     
-      def evernote_consumer_secret
-    (config = Devise.omniauth_configs[:evernote]) && config.strategy.consumer_secret
-  end
+        def ==(other)
+      other.name == name && other.path == path && other.type == type
+    end
     
-        def form_configurable_attributes
-      form_configurable_fields.keys
+        def to_json
+      JSON.generate(as_json)
     end
   end
 end
-
-    
-      def destroy
-    @event.destroy
-    
-        version '1' do
-      self.release = '1.12.6'
-      self.base_url = 'http://coffeescript.org/v1/'
-    
-        version '1.3' do
-      self.release = '1.3.4'
-      self.base_urls = [
-        'https://hexdocs.pm/elixir/#{release}/',
-        'https://hexdocs.pm/eex/#{release}/',
-        'https://hexdocs.pm/ex_unit/#{release}/',
-        'https://hexdocs.pm/iex/#{release}/',
-        'https://hexdocs.pm/logger/#{release}/',
-        'https://hexdocs.pm/mix/#{release}/',
-        'https://elixir-lang.org/getting-started/'
-      ]
-    end
-  end
-end
-
-    
-        options[:only] = BOOKS.map { |s| 'book.#{s}.html' }
-    
-        html_filters.push 'rust/entries', 'rust/clean_html'
-    
-          options[:skip] = %w(list-of-tutorials.html examples.html changelog.html ideas.html roadmap.html)
-    
-        def add(path, content)
-      @pages[path] = content
-    end
-    
-        export LANG=en_US.UTF-8
-    \e[0m
-    DOC
-  end
