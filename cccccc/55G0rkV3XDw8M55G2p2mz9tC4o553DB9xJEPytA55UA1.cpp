e349c02510f140796b0f0a5bcd5b22a4ddfc2a6c
SUNsung <weatherius@gmail.com>
2018-06-11 06:02:23 +0300

    Rewrite [55G0rkV3XDw8M55G2p2mz9tC4o553DB9xJEPytA55UA1.cpp] File in: 2018-06-11 06:02:22

--- cccccc/55G0rkV3XDw8M55G2p2mz9tC4o553DB9xJEPytA55UA1.cpp
+++ cccccc/55G0rkV3XDw8M55G2p2mz9tC4o553DB9xJEPytA55UA1.cpp
@@ -1,141 +1,253 @@
 
         
-        #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
+            // passthrough (pointer interface)
+    std::pair<iterator, bool> insert(const value_type& value) { return m.insert(value); }
     
-      /// @brief The spatial dimensions of a filter kernel.
-  Blob<int> kernel_shape_;
-  /// @brief The spatial dimensions of the stride.
-  Blob<int> stride_;
-  /// @brief The spatial dimensions of the padding.
-  Blob<int> pad_;
-  /// @brief The spatial dimensions of the dilation.
-  Blob<int> dilation_;
-  /// @brief The spatial dimensions of the convolution input.
-  Blob<int> conv_input_shape_;
-  /// @brief The spatial dimensions of the col_buffer.
-  vector<int> col_buffer_shape_;
-  /// @brief The spatial dimensions of the output.
-  vector<int> output_shape_;
-  const vector<int>* bottom_shape_;
+      // Get initial measurement of the space we will be reading.
+  const int64_t initial_size = Size(Key(0), Key(n));
+  const int64_t initial_other_size = Size(Key(n), Key(kCount));
     
-    #ifdef USE_CUDNN
-/*
- * @brief cuDNN implementation of PoolingLayer.
- *        Fallback to PoolingLayer for CPU mode.
-*/
-template <typename Dtype>
-class CuDNNPoolingLayer : public PoolingLayer<Dtype> {
- public:
-  explicit CuDNNPoolingLayer(const LayerParameter& param)
-      : PoolingLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNPoolingLayer();
-  // Currently, cuDNN does not support the extra top blob.
-  virtual inline int MinTopBlobs() const { return -1; }
-  virtual inline int ExactNumTopBlobs() const { return 1; }
-    }
+    #include 'db/filename.h'
+#include 'db/dbformat.h'
+#include 'db/table_cache.h'
+#include 'db/version_edit.h'
+#include 'leveldb/db.h'
+#include 'leveldb/env.h'
+#include 'leveldb/iterator.h'
     
-    namespace caffe {
-    }
+    #include <stdint.h>
+#include 'leveldb/db.h'
+#include 'db/dbformat.h'
     
-      virtual inline const char* type() const { return 'Eltwise'; }
-  virtual inline int MinBottomBlobs() const { return 2; }
-  virtual inline int ExactNumTopBlobs() const { return 1; }
-    
-      // The user space daemon is requesting a new circular queue.
-  // Make sure the requested size is within sane size bounds.
-  if (size > MAX_KMEM || size < MIN_KMEM) {
-    err = -EINVAL;
-    goto error_exit;
+      // Successful parses
+  static struct {
+    const char* fname;
+    uint64_t number;
+    FileType type;
+  } cases[] = {
+    { '100.log',            100,   kLogFile },
+    { '0.log',              0,     kLogFile },
+    { '0.sst',              0,     kTableFile },
+    { '0.ldb',              0,     kTableFile },
+    { 'CURRENT',            0,     kCurrentFile },
+    { 'LOCK',               0,     kDBLockFile },
+    { 'MANIFEST-2',         2,     kDescriptorFile },
+    { 'MANIFEST-7',         7,     kDescriptorFile },
+    { 'LOG',                0,     kInfoLogFile },
+    { 'LOG.old',            0,     kInfoLogFile },
+    { '18446744073709551615.log', 18446744073709551615ull, kLogFile },
+  };
+  for (int i = 0; i < sizeof(cases) / sizeof(cases[0]); i++) {
+    std::string f = cases[i].fname;
+    ASSERT_TRUE(ParseFileName(f, &number, &type)) << f;
+    ASSERT_EQ(cases[i].type, type) << f;
+    ASSERT_EQ(cases[i].number, number) << f;
   }
     
-    #include <osquery/config.h>
-#include <osquery/database.h>
+    
+    {}
+    
+    	STARTUPINFO si = { 0 };
+    
+    
+    {
+    {
+    {  for (const auto& share_line : osquery::split(content, '\n')) {
+    genNFSShare(share_line, results);
+  }
+  return results;
+}
+}
+}
+
+    
+    /**
+ * @brief Access the internal storage of the Decorator parser.
+ *
+ * The decoration set is a map of column name to value. It contains the opaque
+ * set of decoration point results.
+ *
+ * Decorations are applied to log items before they are sent to the downstream
+ * logging APIs: logString, logSnapshot, etc.
+ *
+ * @param results the output parameter to write decorations.
+ */
+void getDecorations(std::map<std::string, std::string>& results);
+    
+    
+    {  // Support a signed 64, a double, and treat everything else as a signed int.
+  if (type == kCFNumberSInt64Type) {
+    long long int value;
+    if (CFNumberGetValue((CFNumberRef)cf_number, type, &value)) {
+      return boost::lexical_cast<std::string>(value);
+    }
+  } else if (type == kCFNumberDoubleType) {
+    double value;
+    if (CFNumberGetValue((CFNumberRef)cf_number, type, &value)) {
+      return boost::lexical_cast<std::string>(value);
+    }
+  } else {
+    unsigned int value;
+    if (CFNumberGetValue((CFNumberRef)cf_number, type, &value)) {
+      return boost::lexical_cast<std::string>(value);
+    }
+  }
+  // Cast as a string.
+  return '0';
+}
+    
+      auto status = ::strerror_s(buffer.data(), buffer.size(), errnum);
+  if (status != 0) {
+    return '';
+  }
+    
+      // Write to the file.
+  ASSERT_OK(
+      env_->NewWritableFile(test_dir_ + '/f1', &writable_file, soptions_));
+  ASSERT_OK(writable_file->Append('abc'));
+  ASSERT_OK(writable_file->Close());
+  writable_file.reset();
+  ASSERT_OK(
+      env_->NewWritableFile(test_dir_ + '/f2', &writable_file, soptions_));
+  ASSERT_OK(writable_file->Close());
+  writable_file.reset();
+    
+        // Helper methods
+    static bool HasFamilyNamed(std::string& name, DBWrapper* db);
+    static bool AddToBatch(rocksdb::WriteBatch& batch, bool del,
+        Handle<Array> array);
+    static bool AddToBatch(rocksdb::WriteBatch& batch, bool del,
+        Handle<Array> array, DBWrapper* db_wrapper, std::string cf);
+    static Handle<Value> CompactRangeDefault(const v8::Arguments& args);
+    static Handle<Value> CompactColumnFamily(const Arguments& args);
+    static Handle<Value> CompactOptions(const Arguments& args);
+    static Handle<Value> CompactAll(const Arguments& args);
+    
+      it.reset(db_->NewIterator(ReadOptions()));
+  first = true;
+  std::string k3('k3');
+  for(it->Seek(k2); it->Valid(); it->Next()) {
+    res = it->value().ToString();
+    if (first) {
+      ASSERT_EQ(res, 'a1,a2,a3,a4');
+      first = false;
+    } else {
+      ASSERT_EQ(res, 'g1');
+    }
+  }
+  for(it->Seek(k3); it->Valid(); it->Next()) {
+    res = it->value().ToString();
+    if (first) {
+      // should not be hit
+      ASSERT_EQ(res, 'a1,a2,a3,a4');
+      first = false;
+    } else {
+      ASSERT_EQ(res, 'g1');
+    }
+  }
+    
+    JniCallback::~JniCallback() {
+  jboolean attached_thread = JNI_FALSE;
+  JNIEnv* env = getJniEnv(&attached_thread);
+  assert(env != nullptr);
+    }
+    
+    #include <memory>
+#include <set>
+#include <string>
+#include 'rocksdb/statistics.h'
+#include 'monitoring/statistics.h'
     
      protected:
-  /// Calculate the URL once and cache the result.
-  std::string uri_;
+  // When *key is an internal key concatenated with the value, returns the
+  // user key.
+  virtual Slice UserKey(const char* key) const;
     
-    TEST_F(ProcessTests, test_constructor) {
-  auto p = PlatformProcess(kInvalidPid);
-  EXPECT_FALSE(p.isValid());
+    // Return an iterator that provided the union of the data in
+// children[0,n-1].  Takes ownership of the child iterators and
+// will delete them when the result iterator is deleted.
+//
+// The result does no duplicate suppression.  I.e., if a particular
+// key is present in K child iterators, it will be yielded K times.
+//
+// REQUIRES: n >= 0
+extern InternalIterator* NewMergingIterator(
+    const InternalKeyComparator* comparator, InternalIterator** children, int n,
+    Arena* arena = nullptr, bool prefix_seek_mode = false);
+    
+    std::unique_ptr<CompactionFilter> CompactionFilterFactoryJniCallback::CreateCompactionFilter(
+    const CompactionFilter::Context& context) {
+  jboolean attached_thread = JNI_FALSE;
+  JNIEnv* env = getJniEnv(&attached_thread);
+  assert(env != nullptr);
+    }
+    
+    #ifndef DUMPCRASHSTACK_H_
+#define DUMPCRASHSTACK_H_
+    
+    #ifdef ANDROID
+#include 'comm/android/callstack.h'
+#endif
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+    //============================================================================
+// Name        : has_member.h
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+    
+      void setDefaultCred(std::string user, std::string password);
+    
+      void setWriteCheckSocket(const std::shared_ptr<SocketCore>& socket);
+    
+    AbstractHttpServerResponseCommand::~AbstractHttpServerResponseCommand()
+{
+  if (readCheck_) {
+    e_->deleteSocketForReadCheck(socket_, this);
+  }
+  if (writeCheck_) {
+    e_->deleteSocketForWriteCheck(socket_, this);
+  }
 }
     
-     private:
-  ProcessState state_{PROCESS_STILL_ALIVE};
-  int status_{0};
+    public:
+  AbstractHttpServerResponseCommand(
+      cuid_t cuid, const std::shared_ptr<HttpServer>& httpServer,
+      DownloadEngine* e, const std::shared_ptr<SocketCore>& socket);
     
-      /// RocksDB logger instance.
-  std::shared_ptr<GlogRocksDBLogger> logger_{nullptr};
+      enum Flag {
+    FLAG_HIDDEN = 1,
+    FLAG_ERASE_AFTER_PARSE = 1 << 1,
+    FLAG_INITIAL_OPTION = 1 << 2,
+    FLAG_CHANGE_OPTION = 1 << 3,
+    FLAG_CHANGE_OPTION_FOR_RESERVED = 1 << 4,
+    FLAG_CHANGE_GLOBAL_OPTION = 1 << 5,
+    FLAG_CUMULATIVE = 1 << 6
+  };
     
-    TEST_F(PrinterTests, test_generate_header) {
-  std::map<std::string, size_t> lengths;
-  for (const auto& row : q) {
-    computeRowLengths(row, lengths);
-  }
+    #include <memory>
+#include <deque>
+#include <vector>
+#include <string>
+    
+    class AnnounceTier {
+public:
+  enum AnnounceEvent {
+    STARTED,
+    STARTED_AFTER_COMPLETION,
+    DOWNLOADING,
+    STOPPED,
+    COMPLETED,
+    SEEDING,
+    HALTED
+  };
     }
     
-    // set of devices across which HostDeviceVector can be distributed;
-// currently implemented as a range, but can be changed later to something else,
-// e.g. a bitset
-class GPUSet {
- public:
-  explicit GPUSet(int start = 0, int ndevices = 0)
-    : start_(start), ndevices_(ndevices) {}
-  static GPUSet Empty() { return GPUSet(); }
-  static GPUSet Range(int start, int ndevices) { return GPUSet(start, ndevices); }
-  int Size() const { return ndevices_; }
-  int operator[](int index) const {
-    CHECK(index >= 0 && index < ndevices_);
-    return start_ + index;
-  }
-  bool IsEmpty() const { return ndevices_ <= 0; }
-  int Index(int device) const {
-    CHECK(device >= start_ && device < start_ + ndevices_);
-    return device - start_;
-  }
-  bool Contains(int device) const {
-    return start_ <= device && device < start_ + ndevices_;
-  }
-  friend bool operator==(GPUSet a, GPUSet b) {
-    return a.start_ == b.start_ && a.ndevices_ == b.ndevices_;
-  }
-  friend bool operator!=(GPUSet a, GPUSet b) {
-    return a.start_ != b.start_ || a.ndevices_ != b.ndevices_;
-  }
+    namespace aria2 {
     }
-    
-    SEXP XGBoosterUpdateOneIter_R(SEXP handle, SEXP iter, SEXP dtrain) {
-  R_API_BEGIN();
-  CHECK_CALL(XGBoosterUpdateOneIter(R_ExternalPtrAddr(handle),
-                                  asInteger(iter),
-                                  R_ExternalPtrAddr(dtrain)));
-  R_API_END();
-  return R_NilValue;
-}
-    
-    TEST(Metric, NDCG) {
-  xgboost::Metric * metric = xgboost::Metric::Create('ndcg');
-  ASSERT_STREQ(metric->Name(), 'ndcg');
-  EXPECT_ANY_THROW(GetMetricEval(metric, {0, 1}, {}));
-  EXPECT_NEAR(GetMetricEval(metric, {}, {}), 1, 1e-10);
-  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 1, 1e-10);
-  EXPECT_NEAR(GetMetricEval(metric,
-                            {0.1f, 0.9f, 0.1f, 0.9f},
-                            {  0,   0,   1,   1}),
-              0.6509f, 0.001f);
-    }
-    
-    /*!
- * \brief Macro to register linear updater.
- */
-#define XGBOOST_REGISTER_LINEAR_UPDATER(UniqueId, Name)                        \
-  static DMLC_ATTRIBUTE_UNUSED ::xgboost::LinearUpdaterReg&                    \
-      __make_##LinearUpdaterReg##_##UniqueId##__ =                             \
-          ::dmlc::Registry< ::xgboost::LinearUpdaterReg>::Get()->__REGISTER__( \
-              Name)
