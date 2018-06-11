adec9b238f67a673fd3798dfd7921b213b0074f0
SUNsung <weatherius@gmail.com>
2018-06-11 13:31:51 +0300

    Rewrite [51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp] File in: 2018-06-11 13:31:51

--- cccccc/51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp
+++ cccccc/51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp
@@ -1,131 +1,180 @@
 
         
-        IPC_MESSAGE_ROUTED3(ShellViewHostMsg_Call_Static_Method,
-                    std::string /* type name */,
-                    std::string /* method name */,
-                    base::ListValue /* arguments */)
+        #ifndef GOOGLE_PROTOBUF_COMPILER_CSHARP_OPTIONS_H__
+#define GOOGLE_PROTOBUF_COMPILER_CSHARP_OPTIONS_H__
     
-      bool delay_destruction() { return delay_destruction_; }
-  void set_delay_destruction(bool val) { delay_destruction_ = val; }
-  bool pending_destruction() { return pending_destruction_; }
-  void set_pending_destruction (bool val) { pending_destruction_ = val; }
- protected:
-  int id_;
-  bool delay_destruction_;
-  bool pending_destruction_;
-  base::WeakPtr<ObjectManager> object_manager_;
-    
-    Menu::Menu(int id,
-           const base::WeakPtr<ObjectManager>& object_manager,
-           const base::DictionaryValue& option,
-           const std::string& extension_id)
-  : Base(id, object_manager, option, extension_id), enable_show_event_(false)  {
-  Create(option);
+    void RepeatedEnumFieldGenerator::GenerateCloningCode(io::Printer* printer) {
+  printer->Print(variables_,
+    '$name$_ = other.$name$_.Clone();\n');
 }
     
-       bool IsCommandIdChecked(int command_id) const override;
-   bool IsCommandIdEnabled(int command_id) const override;
+    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
+//   can only really compare the output against a golden value, which is a
+//   fairly tedious and fragile testing strategy.  If we want to go that route,
+//   it probably makes sense to bite the bullet and write a test that compares
+//   the whole generated output for unittest.proto against a golden value, with
+//   a very simple script that can be run to regenerate it with the latest code.
+//   This would mean that updates to the golden file would have to be included
+//   in any change to the code generator, which would actually be fairly useful
+//   as it allows the reviewer to see clearly how the generated code is
+//   changing.
     
-      keyval = GetKeycodeFromText(key);
-  if (keyval == ui::VKEY_UNKNOWN){
-    enable_shortcut_ = false;
-  } else {
-    enable_shortcut_ = true;
-    //only code for ctrl, shift, alt, super and meta modifiers
-    int modifiers_value = ui::EF_NONE;
-    modifiers = base::ToLowerASCII(modifiers);
-    if (modifiers.find('ctrl')!=std::string::npos){
-      modifiers_value |= ui::EF_CONTROL_DOWN;
+    #include <map>
+#include <string>
+    
+            void DoneWithCurrentMinibatch()
+        {
+            for (auto& x : m_cachedGradient)
+            {
+                const wstring& name  = x.first;
+                auto& accumulategrad = m_cachedGradient.GetInputMatrix<ElemType>(name);
     }
-    if (modifiers.find('shift')!=std::string::npos){
-      modifiers_value |= ui::EF_SHIFT_DOWN ;
     }
-    if (modifiers.find('alt')!=std::string::npos){
-      modifiers_value |= ui::EF_ALT_DOWN;
+    
+    // only copy a complete independent tree
+// when node name exists
+void ComputationNetwork::CopySubTree(const ComputationNetwork& fromNet,
+                                     const std::wstring fromName, std::wstring toNamePrefix,
+                                     const CopyNodeFlags flags)
+{
+    InvalidateCompiledNetwork();
     }
-    if (modifiers.find('super')!=std::string::npos
-     || modifiers.find('cmd')!=std::string::npos
-     || modifiers.find('command')!=std::string::npos){
-      modifiers_value |= ui::EF_COMMAND_DOWN;
+    
+        // ProcessNDLScript - Process the NDL script
+    // netNdl - netNDL structure
+    // ndlPassUntil - complete processing through this pass, all passes if ndlPassAll
+    // fullValidate - validate as a complete network? (false if this might be a snippet of a full network)
+    void ProcessNDLScript(NetNdl<ElemType>* netNdl, NDLPass ndlPassUntil = ndlPassAll, bool fullValidate = false)
+    {
+        ProcessNDLScript(netNdl->ndl, ndlPassUntil, netNdl->lastNode, fullValidate);
     }
-    if (modifiers.find('meta')!=std::string::npos){
-      meta_down_flag_ = true;
+    
+    #include 'stdafx.h'
+#include 'Basics.h'
+#include 'Actions.h'
+#include 'ComputationNetwork.h'
+#include 'ComputationNode.h'
+#include 'DataReader.h'
+#include 'DataWriter.h'
+#include 'SimpleNetworkBuilder.h'
+#include 'Config.h'
+#include 'ScriptableObjects.h'
+    
+    
+    {
+    {
+    {            std::vector<std::string> filePathVec = msra::strfun::split(filePaths, '+');
+            for (auto filePath : filePathVec)
+            {
+                // if file hasn't already been resolved (the resolvedPaths vector doesn't contain it), resolve it.
+                if (std::find(resolvedConfigFiles.begin(), resolvedConfigFiles.end(), filePath) == resolvedConfigFiles.end())
+                {
+                    // Recursively resolve the include statements in the included config files.
+                    // Ensure that the same config file isn't included twice, by keeping track of the config
+                    // files that have already been resolved in the resolvedPaths vector.
+                    resolvedConfigFiles.push_back(filePath);
+                    newConfigString += ResolveIncludeStatements(ReadConfigFile(filePath), resolvedConfigFiles);
+                }
+                else
+                {
+                    // We already resolved this path.  Write a warning so that user is aware of this.
+                    // TODO: This message is written to stderr before stderr gets redirected to the specified file.  Fix this.
+                    fprintf(stderr, 'Warning: Config file included multiple times.  Not including config file again: %s', filePath.c_str());
+                }
+            }
+        }
+        else
+        {
+            newConfigString += (line + '\n');
+        }
     }
-    accelerator_ = ui::Accelerator(keyval,modifiers_value);
+    return newConfigString;
+}
+    
+    int register_all_cocos2dx_experimental(lua_State* tolua_S);
+    
+    int register_all_cocos2dx_experimental_video(lua_State* tolua_S);
+    
+    
+    
+    
+    
+    	void Step(Settings* settings)
+	{
+		Test::Step(settings);
+    }
+    
+      // Compares the 8x8 block with offsets (off_x, off_y) within the current
+  // macro-block of the baseline image with the same block of img and returns
+  // the resulting per-block distance. The interpretation of the returned
+  // distance depends on the comparator used.
+  virtual double CompareBlock(const OutputImage& img,
+                              int off_x, int off_y) const = 0;
+    
+    void OutputImageComponent::ToFloatPixels(float* out, int stride) const {
+  assert(factor_x_ == 1);
+  assert(factor_y_ == 1);
+  for (int block_y = 0; block_y < height_in_blocks_; ++block_y) {
+    for (int block_x = 0; block_x < width_in_blocks_; ++block_x) {
+      coeff_t block[kDCTBlockSize];
+      GetCoeffBlock(block_x, block_y, block);
+      double blockd[kDCTBlockSize];
+      for (int k = 0; k < kDCTBlockSize; ++k) {
+        blockd[k] = block[k];
+      }
+      ComputeBlockIDCTDouble(blockd);
+      for (int iy = 0; iy < 8; ++iy) {
+        for (int ix = 0; ix < 8; ++ix) {
+          int y = block_y * 8 + iy;
+          int x = block_x * 8 + ix;
+          if (y >= height_ || x >= width_) continue;
+          out[(y * width_ + x) * stride] = static_cast<float>(blockd[8 * iy + ix] + 128.0);
+        }
+      }
+    }
   }
+}
     
-    #include <vector>
-    
-        bool WriteRTF(ClipboardData& data) {
-      DCHECK(data.type == TYPE_RTF);
-      scw_->WriteRTF(*(data.data));
-      return true;
-    }
-    
-    class NwClipboardReadAvailableTypesFunction : public NWSyncExtensionFunction {
- public:
-  NwClipboardReadAvailableTypesFunction();
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    }
-    
-      LOG(INFO) << 'Writing Training data';
-  for (int fileid = 0; fileid < kCIFARTrainBatches; ++fileid) {
-    // Open files
-    LOG(INFO) << 'Training Batch ' << fileid + 1;
-    string batchFileName = input_folder + '/data_batch_'
-      + caffe::format_int(fileid+1) + '.bin';
-    std::ifstream data_file(batchFileName.c_str(),
-        std::ios::in | std::ios::binary);
-    CHECK(data_file) << 'Unable to open train file #' << fileid + 1;
-    for (int itemid = 0; itemid < kCIFARBatchSize; ++itemid) {
-      read_image(&data_file, &label, str_buffer);
-      datum.set_label(label);
-      datum.set_data(str_buffer, kCIFARImageNBytes);
-      string out;
-      CHECK(datum.SerializeToString(&out));
-      txn->Put(caffe::format_int(fileid * kCIFARBatchSize + itemid, 5), out);
-    }
-  }
-  txn->Commit();
-  train_db->Close();
-    
-      void Transform(const Datum& datum, Dtype* transformed_data);
-  // Tranformation parameters
-  TransformationParameter param_;
-    
-    #include <utility>
-#include <vector>
-    
-    
-    {  int size_;
-  Dtype alpha_, beta_, k_;
+    static const int kCrToRedTable[256] = {
+  -179, -178, -177, -175, -174, -172, -171, -170, -168, -167, -165, -164,
+  -163, -161, -160, -158, -157, -156, -154, -153, -151, -150, -149, -147,
+  -146, -144, -143, -142, -140, -139, -137, -136, -135, -133, -132, -130,
+  -129, -128, -126, -125, -123, -122, -121, -119, -118, -116, -115, -114,
+  -112, -111, -109, -108, -107, -105, -104, -102, -101, -100,  -98,  -97,
+   -95,  -94,  -93,  -91,  -90,  -88,  -87,  -86,  -84,  -83,  -81,  -80,
+   -79,  -77,  -76,  -74,  -73,  -72,  -70,  -69,  -67,  -66,  -64,  -63,
+   -62,  -60,  -59,  -57,  -56,  -55,  -53,  -52,  -50,  -49,  -48,  -46,
+   -45,  -43,  -42,  -41,  -39,  -38,  -36,  -35,  -34,  -32,  -31,  -29,
+   -28,  -27,  -25,  -24,  -22,  -21,  -20,  -18,  -17,  -15,  -14,  -13,
+   -11,  -10,   -8,   -7,   -6,   -4,   -3,   -1,    0,    1,    3,    4,
+     6,    7,    8,   10,   11,   13,   14,   15,   17,   18,   20,   21,
+    22,   24,   25,   27,   28,   29,   31,   32,   34,   35,   36,   38,
+    39,   41,   42,   43,   45,   46,   48,   49,   50,   52,   53,   55,
+    56,   57,   59,   60,   62,   63,   64,   66,   67,   69,   70,   72,
+    73,   74,   76,   77,   79,   80,   81,   83,   84,   86,   87,   88,
+    90,   91,   93,   94,   95,   97,   98,  100,  101,  102,  104,  105,
+   107,  108,  109,  111,  112,  114,  115,  116,  118,  119,  121,  122,
+   123,  125,  126,  128,  129,  130,  132,  133,  135,  136,  137,  139,
+   140,  142,  143,  144,  146,  147,  149,  150,  151,  153,  154,  156,
+   157,  158,  160,  161,  163,  164,  165,  167,  168,  170,  171,  172,
+   174,  175,  177,  178
 };
-#endif
     
-    #include 'caffe/layers/neuron_layer.hpp'
-#include 'caffe/layers/sigmoid_layer.hpp'
+    // Sort the root nodes, least popular first.
+static inline bool SortHuffmanTree(const HuffmanTree& v0,
+                                   const HuffmanTree& v1) {
+  if (v0.total_count_ != v1.total_count_) {
+    return v0.total_count_ < v1.total_count_;
+  }
+  return v0.index_right_or_value_ > v1.index_right_or_value_;
+}
+    
+    #include <stddef.h>
+#include <stdint.h>
+    
+    #include 'guetzli/jpeg_huffman_decode.h'
+    
+      void ApplyGlobalQuantization(const int q[kDCTBlockSize]);
     
     
-    {}  // namespace caffe
-    
-    
-    {}  // namespace caffe
-    
-    #include <vector>
-    
-    /**
- * @brief Compute elementwise operations, such as product and sum,
- *        along multiple input Blobs.
- *
- * TODO(dox): thorough documentation for Forward, Backward, and proto params.
- */
-template <typename Dtype>
-class EltwiseLayer : public Layer<Dtype> {
- public:
-  explicit EltwiseLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
+    {}  // namespace guetzli
