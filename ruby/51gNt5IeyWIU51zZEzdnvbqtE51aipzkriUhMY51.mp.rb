2656267dc313e11240aa3cae2e53b900f03fe44a
SUNsung <weatherius@gmail.com>
2018-06-13 02:46:30 +0300

    Rewrite [51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb] File in: 2018-06-13 02:46:29

--- ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
+++ ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
@@ -1,77 +1,53 @@
 
         
-                key.revoke!
-      end
+          test 'overriding has_many_attached methods works' do
+    # attach blobs before messing with getter, which breaks `#attach`
+    @user.highlights.attach create_blob(filename: 'funky.jpg'), create_blob(filename: 'wonky.jpg')
+    
+      setup do
+    build_app
+    add_to_config 'config.consider_all_requests_local = true'
+    
+    require 'test_helper'
+require 'stubs/test_connection'
+require 'stubs/room'
+    
+      setup do
+    @user = User.new 'lifo'
+    @connection = TestConnection.new(@user)
+  end
+    
+            confirmation = { 'identifier' => '{id: 1}', 'type' => 'confirm_subscription' }
+        connection.transmit(confirmation)
+    
+        def send_async(method, *args)
+      send method, *args
+    end
+  end
+    
+          assert_not connection.connected
+      assert_equal [], @server.connections
+    end
+  end
+    
+      test '#restart shuts down pub/sub adapter' do
+    assert_called(@server.pubsub, :shutdown) do
+      @server.restart
     end
   end
 end
 
     
-            command = 'security set-keychain-settings'
-        command << ' -t #{params[:timeout]}' if params[:timeout]
-        command << ' -l' if params[:lock_when_sleeps]
-        command << ' -u' if params[:lock_after_timeout]
-        command << ' #{keychain_path}'
+    class BroadcastingTest < ActionCable::TestCase
+  test 'fetching a broadcaster converts the broadcasting queue to a string' do
+    broadcasting = :test_queue
+    server = TestServer.new
+    broadcaster = server.broadcaster_for(broadcasting)
     
-            expect(result).to eq('/usr/local/bin/cloc  --by-file --xml  --out=build/cloc.xml')
+          it 'renders HTML' do
+        expect(render(options, screenshots)).to match(/<html>/)
       end
-    
-      def present(payload)
-    if payload.is_a?(Hash)
-      payload = ActiveSupport::HashWithIndifferentAccess.new(payload)
-      MAIN_KEYS.each do |key|
-        return { :title => payload[key].to_s, :entries => present_hash(payload, key) } if payload.has_key?(key)
-      end
-    
-      def tumblr
-    Tumblr.configure do |config|
-      config.consumer_key = tumblr_consumer_key
-      config.consumer_secret = tumblr_consumer_secret
-      config.oauth_token = tumblr_oauth_token
-      config.oauth_token_secret = tumblr_oauth_token_secret
     end
     
-    Tumblr::Client.new
-  end
-end
-    
-    begin
-  require 'bundler/setup'
-rescue LoadError
-  $stderr.puts '[*] Metasploit requires the Bundler gem to be installed'
-  $stderr.puts '    $ gem install bundler'
-  exit(1)
-end
-    
-          when :banner
-        # Because some ftp server send multiple banner we take only the first one and ignore the rest
-        if not (s[:info])
-          s[:info] = matches
-          report_service(s)
-        end
-    
-    
-# replace calls, jmps, and read/write handle/filename references
-replaces = []
-asm.each_line { |ln|
-	if ln =~ /call /
-		parts = ln.split(' ')
-		if (parts[0] == 'call' and parts[2] == ';call')
-			old = parts[1]
-			func = parts[3]
-			new = addrs[func]
-			#puts '%32s: %s -> %x' % [func, old, new]
-			replaces << [func, old, new.to_s(16)]
-		end
-	end
-    }
-    
-    signer._invoke('JarSignerMSF','[Ljava.lang.String;',jarsignerOpts)
-    
-    	def block_end
-		# Insert the block size
-		self.block[-1][0] = block[-1][0].ljust(SIZE1)
-		self.block[-1][0] << '/*  '
-		self.block[-1][0] << '#{block_size} bytes'
-		self.block[-1][0] = block[-1][0].ljust(SIZE2)
-		self.block[-1][0] << '  */'
+          def load_all_tasks
+        self.tasks = []
