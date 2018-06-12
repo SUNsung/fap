9354d408373673670e7d693616dddee673665ebf
SUNsung <weatherius@gmail.com>
2018-06-12 14:26:21 +0300

    Rewrite [55uXiG.Zi9qS255Ypq2xUoiEE554wF49PUDjdk55ca8.rb] File in: 2018-06-12 14:26:21

--- ruby/55uXiG.Zi9qS255Ypq2xUoiEE554wF49PUDjdk55ca8.rb
+++ ruby/55uXiG.Zi9qS255Ypq2xUoiEE554wF49PUDjdk55ca8.rb
@@ -1,26 +1,91 @@
 
         
-              html_filters.push 'bootstrap/entries_v4', 'bootstrap/clean_html_v4'
-    
-          options[:only_patterns] = [/\Ad3[\-\w]+\z/, /\Ad3\/blob\/master\/changes\.md\z/i]
-      options[:skip_patterns] = [/3\.x-api-reference/]
-    
-        version '1.9' do
-      self.release = '1.9.13'
-      self.dir = '/Users/Thibaut/DevDocs/Docs/Django19'
-      self.base_url = 'https://docs.djangoproject.com/en/1.9/'
+              topic.feature_topic_users(args)
     end
     
-        version 'Sys' do
-      self.base_url = 'https://api.haxe.org/sys/'
-    end
+          raise Discourse::InvalidParameters.new(:to_address) unless args[:to_address].present?
     
-        def ==(other)
-      other.name == name && other.path == path && other.type == type
     end
+
     
-        def to_json
-      JSON.generate(as_json)
-    end
+      def self.fragment_cache
+    @cache ||= DistributedCache.new('am_serializer_fragment_cache')
   end
+    
+        version 'Flash' do
+      self.base_url = 'https://api.haxe.org/flash/'
+    end
+    
+        def to_a
+      @filters.dup
+    end
+    
+        def as_json
+      @pages
+    end
+    
+          class << self
+        # Mark a given block of code as a 'busy' block of code, which will
+        # register a SIGINT handler for the duration of the block. When a
+        # SIGINT occurs, the `sig_callback` proc will be called. It is up
+        # to the callback to behave properly and exit the application.
+        def busy(sig_callback)
+          register(sig_callback)
+          return yield
+        ensure
+          unregister(sig_callback)
+        end
+    
+    module Vagrant
+  module Util
+    class IO
+      # The chunk size for reading from subprocess IO.
+      READ_CHUNK_SIZE = 4096
+    
+              opts = OptionParser.new do |o|
+            o.banner = 'Usage: vagrant box remove <name>'
+            o.separator ''
+            o.separator 'Options:'
+            o.separator ''
+    
+          # move bootstrap/_bootstrap.scss to _bootstrap.scss adjusting import paths
+      main_from = '#{save_to}/_bootstrap.scss'
+      main_to   = File.expand_path('#{save_to}/../_bootstrap.scss')
+      save_file main_to, File.read(main_from).gsub(/ '/, ' 'bootstrap/')
+      File.delete(main_from)
+    
+    if profile_filename = ENV['PROFILE']
+  require 'ruby-prof'
+  reporter =
+    case (profile_extname = File.extname(profile_filename))
+    when '.txt'
+      RubyProf::FlatPrinterWithLineNumbers
+    when '.html'
+      RubyProf::GraphHtmlPrinter
+    when '.callgrind'
+      RubyProf::CallTreePrinter
+    else
+      raise 'Unknown profiler format indicated by extension: #{profile_extname}'
+    end
+  File.open(profile_filename, 'w') do |io|
+    reporter.new(RubyProf.profile { Pod::Command.run(ARGV) }).print(io)
+  end
+else
+  Pod::Command.run(ARGV)
 end
+
+    
+          def run
+        @podfile_path.open('w') { |f| f << podfile_template(@xcode_project) }
+      end
+    
+            def run
+          print_version
+          signal_end_of_output
+          listen
+        end
+    
+            sets = config.sources_manager.aggregate.all_sets
+        sets.each { |set| UI.pod(set, :name_and_version) }
+        UI.puts '\n#{sets.count} pods were found'
+      end
