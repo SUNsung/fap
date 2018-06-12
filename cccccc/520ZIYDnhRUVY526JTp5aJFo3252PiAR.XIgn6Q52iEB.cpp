8d2c3664f8ca09ca899c16ffc503c639b08a82d3
SUNsung <weatherius@gmail.com>
2018-06-13 01:51:31 +0300

    Rewrite [520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp] File in: 2018-06-13 01:51:31

--- cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
+++ cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
@@ -1,277 +1,94 @@
 
         
-        void Clipboard::SetText(std::string& text) {
-  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
-  ui::Clipboard::ObjectMap map;
-  map[ui::Clipboard::CBF_TEXT].push_back(
-      std::vector<char>(text.begin(), text.end()));
-  clipboard->WriteObjects(ui::CLIPBOARD_TYPE_COPY_PASTE, map);
-}
-    
-    EventListener::~EventListener() {
-  for (std::map<int, BaseEvent*>::iterator i = listerners_.begin(); i != listerners_.end(); i++) {
-    delete i->second;
+          class MatchGroundTruth : public QuickTest ,
+      public ::testing::WithParamInterface<const char*> {
+  };
+  
+  TEST_P(MatchGroundTruth, FastPhototestOCR) {
+    OCRTester(TESTING_DIR '/phototest.tif',
+              TESTING_DIR '/phototest.txt',
+              TESSDATA_DIR '_fast', GetParam());
   }
-}
+  
+  INSTANTIATE_TEST_CASE_P( EngLatinDevaArabLang, MatchGroundTruth, 
+                        ::testing::Values('eng', 'script/Latin', 'script/Devanagari', 'script/Arabic') );
     
-      template<typename T> T* AddListener() {
-    std::map<int, BaseEvent*>::iterator i = listerners_.find(T::id);
-    if (i==listerners_.end()) {
-      T* listener_object = new T(this);
-      listerners_[T::id] = listener_object;
-      return listener_object;
-    }
-    return NULL;
-  }
+    ScrollView* bln_word_window_handle();  //return handle
+void build_image_window(int width, int height);
+void display_bln_lines(ScrollView window,
+                       ScrollView::Color colour,
+                       float scale_factor,
+                       float y_offset,
+                       float minx,
+                       float maxx);
+                                 //function to call
+void pgeditor_msg(  //message display
+                  const char *msg);
+void pgeditor_show_point(  //display coords
+                         SVEvent *event);
+                                 //put bln word in       box
+void show_point(PAGE_RES* page_res, float x, float y);
     
-    void Menu::Append(MenuItem* menu_item) {
-  if (menu_item->submenu_)
-    menu_model_->AddSubMenu(menu_item->id(), menu_item->label_,
-                            menu_item->submenu_->menu_model_.get());
-  else if (menu_item->type_ == 'normal')
-    menu_model_->AddItem(menu_item->id(), menu_item->label_);
-  else if (menu_item->type_ == 'checkbox')
-    menu_model_->AddCheckItem(menu_item->id(), menu_item->label_);
-  else if (menu_item->type_ == 'separator')
-    menu_model_->AddSeparator(ui::NORMAL_SEPARATOR);
-    }
-    
-      focus_manager_ = NULL;
-  menu_ = NULL;
-    
-    class NwAppClearAppCacheFunction : public NWSyncExtensionFunction {
- public:
-  NwAppClearAppCacheFunction();
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    }
-    
-          // strip off data uri header if raw is set
-      if (!(data.raw.get() && *(data.raw))) {
-        if (data.type == TYPE_PNG && base::StartsWith(content, kPNGDataUriPrefix, base::CompareCase::INSENSITIVE_ASCII)) {
-          content = content.substr(strlen(kPNGDataUriPrefix));
-        } else if (data.type == TYPE_JPEG && base::StartsWith(content, kJPEGDataUriPrefix, base::CompareCase::INSENSITIVE_ASCII)) {
-          content = content.substr(strlen(kJPEGDataUriPrefix));
-        } else {
-          error_ = base::StringPrintf('Invalid data URI. Only \'%s\' or \'%s\' is accepted.', kPNGDataUriPrefix, kJPEGDataUriPrefix);
-          return false;
-        }
-      }
-    
-    class NwClipboardClearSyncFunction : public NWSyncExtensionFunction {
- public:
-  NwClipboardClearSyncFunction();
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    }
-    
-    namespace grpc {
-namespace testing {
-    }
-    }
-    
-      /**
-   * Invoked when there is no data to be read or written and the stream is
-   * closed successfully remotely and locally. Once invoked, no further callback
-   * methods will be invoked.
-   */
-  void (*on_succeded)(bidirectional_stream* stream);
-    
-    
-    {  GetReporter()->ReportQPS(*result);
-  GetReporter()->ReportLatency(*result);
-}
-    
-    
-    {}  // namespace routeguide
-    
-    namespace grpc_node_generator {
-    }
-    
-    std::vector<grpc::string_ref> SecureAuthContext::GetPeerIdentity() const {
-  if (!ctx_) {
-    return std::vector<grpc::string_ref>();
-  }
-  grpc_auth_property_iterator iter = grpc_auth_context_peer_identity(ctx_);
-  std::vector<grpc::string_ref> identity;
-  const grpc_auth_property* property = nullptr;
-  while ((property = grpc_auth_property_iterator_next(&iter))) {
-    identity.push_back(
-        grpc::string_ref(property->value, property->value_length));
-  }
-  return identity;
-}
-    
-    static void sigint_handler(int x) {
-  gpr_atm_no_barrier_store(&grpc::testing::interop::g_got_sigint, true);
-}
-    
-      static double Now();
-    
-      // Create a QpsGauge with name 'name'. is_present is set to true if the Gauge
-  // is already present in the map.
-  // NOTE: CreateQpsGauge can be called anytime (i.e before or after calling
-  // StartServer).
-  std::shared_ptr<QpsGauge> CreateQpsGauge(const grpc::string& name,
-                                           bool* already_present);
-    
-    
-    {
-    {#ifdef DEBUG_ENABLED
-			print_line('Loaded certs from '' + certs_path);
-#endif
-		}
-	}
-    
-    VARIANT_ENUM_CAST(StreamPeerSSL::Status);
-    
-    #ifndef STREAM_PEER_OPEN_SSL_H
-#define STREAM_PEER_OPEN_SSL_H
-    
-    	info.port = CONTEXT_PORT_NO_LISTEN;
-	info.protocols = _lws_ref->lws_structs;
-	info.gid = -1;
-	info.uid = -1;
-	//info.ws_ping_pong_interval = 5;
-	info.user = _lws_ref;
-#if defined(LWS_OPENSSL_SUPPORT)
-	info.options |= LWS_SERVER_OPTION_DO_SSL_GLOBAL_INIT;
-#endif
-	context = lws_create_context(&info);
-    
-    #include 'core/error_list.h'
-#include 'websocket_multiplayer.h'
-#include 'websocket_peer.h'
-    
-    #if defined(MBEDTLS_KEY_EXCHANGE_RSA_ENABLED) &&                       \
-    ( !defined(MBEDTLS_RSA_C) || !defined(MBEDTLS_X509_CRT_PARSE_C) || \
-      !defined(MBEDTLS_PKCS1_V15) )
-#error 'MBEDTLS_KEY_EXCHANGE_RSA_ENABLED defined, but not all prerequisites'
-#endif
-    
-    
-    {	virtual btScalar addSingleResult(btCollisionWorld::LocalRayResult &rayResult, bool normalInWorldSpace) {
-		m_shapeId = rayResult.m_localShapeInfo->m_triangleIndex; // 'm_triangleIndex' Is a odd name but contains the compound shape ID
-		return btCollisionWorld::ClosestRayResultCallback::addSingleResult(rayResult, normalInWorldSpace);
-	}
-};
-    
-    protected:
-	static void _bind_methods();
-	static _VisualScriptEditor *singleton;
-    
-    class GDNativeLibraryResourceLoader;
-class GDNative;
-    
-      public:
-    jpeg_decoder_mem_stream() : m_pSrc_data(NULL), m_ofs(0), m_size(0) { }
-    jpeg_decoder_mem_stream(const uint8 *pSrc_data, uint size) : m_pSrc_data(pSrc_data), m_ofs(0), m_size(size) { }
-    
-    /**
- * oc_ilog32 - Integer binary logarithm of a 32-bit value.
- * @_v: A 32-bit value.
- * Returns floor(log2(_v))+1, or 0 if _v==0.
- * This is the number of bits that would be required to represent _v in two's
- *  complement notation with all of the leading zeros stripped.
- * The OC_ILOG_32() or OC_ILOGNZ_32() macros may be able to use a builtin
- *  function instead, which should be faster.
- */
-int oc_ilog32(ogg_uint32_t _v);
-/**
- * oc_ilog64 - Integer binary logarithm of a 64-bit value.
- * @_v: A 64-bit value.
- * Returns floor(log2(_v))+1, or 0 if _v==0.
- * This is the number of bits that would be required to represent _v in two's
- *  complement notation with all of the leading zeros stripped.
- * The OC_ILOG_64() or OC_ILOGNZ_64() macros may be able to use a builtin
- *  function instead, which should be faster.
- */
-int oc_ilog64(ogg_int64_t _v);
-    
-      /// Get the timer's expiry time relative to now.
-  /**
-   * This function may be used to obtain the timer's current expiry time.
-   * Whether the timer has expired or not does not affect this value.
-   */
-  duration expires_from_now() const
-  {
-    return this->service.expires_from_now(this->implementation);
-  }
-    
-    
-    {
-    {} // namespace asio
-} // namespace boost
-    
-    template <typename Stream>
-class buffered_write_stream;
-    
-    #if !defined(BOOST_ASIO_HAS_THREADS)
-// Nothing to include.
-#elif defined(BOOST_ASIO_HAS_STD_ATOMIC)
-# include <atomic>
-#else // defined(BOOST_ASIO_HAS_STD_ATOMIC)
-# include <boost/detail/atomic_count.hpp>
-#endif // defined(BOOST_ASIO_HAS_STD_ATOMIC)
-    
-    template<class T, class TimeSystem>
-class base_time;
-    
-    #if defined(BOOST_ASIO_HAS_STD_FUNCTION)
-# include <functional>
-#else // defined(BOOST_ASIO_HAS_STD_FUNCTION)
-# include <boost/function.hpp>
-#endif // defined(BOOST_ASIO_HAS_STD_FUNCTION)
-    
-    // Calls to asio_handler_invoke must be made from a namespace that does not
-// contain overloads of this function. The boost_asio_handler_invoke_helpers
-// namespace is defined here for that purpose.
-namespace boost_asio_handler_invoke_helpers {
-    }
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you use this binding you'll need to call 4 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXXX_NewFrame(), ImGui::Render() and ImGui_ImplXXXX_Shutdown().
-// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-    
-    {    printf('DestroyContext()\n');
-    ImGui::DestroyContext();
-    return 0;
-}
+    #endif  // TESSERACT_CCMAIN_OTSUTHR_H_
 
     
-        // Setup Dear ImGui binding
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-    ImGui_ImplGlfwGL2_Init(window, true);
+    void signal_exit(int signal_code);
     
-            // 1. Show a simple window.
-        // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called 'Debug'.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-            ImGui::Text('Hello, world!');                           // Display some text (you can use a format string too)
-            ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
+    extern const char *kUTF8LineSeparator;
+extern const char *kUTF8ParagraphSeparator;
+extern const char *kLRM;  //< Left-to-Right Mark
+extern const char *kRLM;  //< Right-to-Left Mark
+extern const char *kRLE;  //< Right-to-Left Embedding
+extern const char *kPDF;  //< Pop Directional Formatting
+    
+    /**
+ * Return the character normalization feature for a blob.
+ *
+ * The features returned are in a scale where the x-height has been
+ * normalized to live in the region y = [-0.25 .. 0.25].  Example ranges
+ * for English below are based on the Linux font collection on 2009-12-04:
+ *
+ *  - Params[CharNormY]
+ *     - The y coordinate of the grapheme's centroid.
+ *     - English: [-0.27, 0.71]
+ *
+ *  - Params[CharNormLength]
+ *     - The length of the grapheme's outline (tiny segments discarded),
+ *     divided by 10.0=LENGTH_COMPRESSION.
+ *     - English: [0.16, 0.85]
+ *
+ *  - Params[CharNormRx]
+ *     - The radius of gyration about the x axis, as measured from CharNormY.
+ *     - English: [0.011, 0.34]
+ *
+ *  - Params[CharNormRy]
+ *     - The radius of gyration about the y axis, as measured from
+ *     the x center of the grapheme's bounding box.
+ *     - English: [0.011, 0.31]
+ */
+FEATURE_SET ExtractCharNormFeatures(const INT_FX_RESULT_STRUCT& fx_info) {
+  FEATURE_SET feature_set = NewFeatureSet(1);
+  FEATURE feature = NewFeature(&CharNormDesc);
     }
     
-        // Destroy old Framebuffer:
-    for (uint32_t i = 0; i < g_BackBufferCount; i++)
-        if (g_BackBufferView[i])
-            vkDestroyImageView(g_Device, g_BackBufferView[i], g_Allocator);
-    for (uint32_t i = 0; i < g_BackBufferCount; i++)
-        if (g_Framebuffer[i])
-            vkDestroyFramebuffer(g_Device, g_Framebuffer[i], g_Allocator);
-    if (g_RenderPass)
-        vkDestroyRenderPass(g_Device, g_RenderPass, g_Allocator);
+      // Free up any currently unused dawgs.
+  void DeleteUnusedDawgs() {
+    dawgs_.DeleteUnusedObjects();
+  }
     
-    // Render function.
-// (this used to be set in io.RenderDrawListsFn and called by ImGui::Render(), but you can now call this directly from your main loop)
-void ImGui_ImplDX9_RenderDrawData(ImDrawData* draw_data)
-{
-    // Avoid rendering when minimized
-    ImGuiIO& io = ImGui::GetIO();
-    if (io.DisplaySize.x <= 0.0f || io.DisplaySize.y <= 0.0f)
-        return;
-    }
+      // Builds a target using CTC. Slightly improved as follows:
+  // Includes normalizations and clipping for stability.
+  // labels should be pre-padded with nulls wherever desired, but they don't
+  // have to be between all labels. Allows for multi-label codes with no
+  // nulls between.
+  // labels can be longer than the time sequence, but the total number of
+  // essential labels (non-null plus nulls between equal labels) must not exceed
+  // the number of timesteps in outputs.
+  // outputs is the output of the network, and should have already been
+  // normalized with NormalizeProbs.
+  // On return targets is filled with the computed targets.
+  // Returns false if there is insufficient time for the labels.
+  static bool ComputeCTCTargets(const GenericVector<int>& truth_labels,
+                                int null_char,
+                                const GENERIC_2D_ARRAY<float>& outputs,
+                                NetworkIO* targets);
