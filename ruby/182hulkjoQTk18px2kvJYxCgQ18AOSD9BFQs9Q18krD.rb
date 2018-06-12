f11600ebc794a22b3a5d8c805f13c25be32864d4
SUNsung <weatherius@gmail.com>
2018-06-12 03:24:59 +0300

    Rewrite [182hulkjoQTk18px2kvJYxCgQ18AOSD9BFQs9Q18krD.rb] File in: 2018-06-12 03:24:58

--- ruby/182hulkjoQTk18px2kvJYxCgQ18AOSD9BFQs9Q18krD.rb
+++ ruby/182hulkjoQTk18px2kvJYxCgQ18AOSD9BFQs9Q18krD.rb
@@ -1,57 +1,90 @@
 
         
-          def id
-    object.id.to_s
+            keys.find do |key|
+      if key.to_s.end_with?('_or_later')
+        later_tag = key.to_s[/(\w+)_or_later$/, 1].to_sym
+        MacOS::Version.from_symbol(later_tag) <= tag_version
+      end
+    end
   end
-    
-      def perform(user_id)
-    @user = User.find(user_id)
-    deliver_digest if @user.allows_digest_emails?
-  end
-    
-      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/follow
-  def follow
-    f = Follow.last
-    NotificationMailer.follow(f.target_account, Notification.find_by(activity: f))
-  end
-    
-    # Silence warnings about this defaulting to true
-I18n.enforce_available_locales = true
+end
 
     
-    #
-# This script extracts the forms from the main page of each
-# web site in a list. The output of this can be used with
-# Metasploit (and other tools) to obtain the saved form data
-# of these domains.
-#
+          puts_columns Array(result)
+    else
+      query = ARGV.first
+      rx = query_regexp(query)
+      local_results = search_formulae(rx)
+      puts_columns(local_results)
+      tap_results = search_taps(rx)
+      puts_columns(tap_results)
     
-      def register_sigs
-    self.sigs = {
-      :banner		=> /^(220\s*[^\r\n]+)/i,
-      :user		=> /^USER\s+([^\s]+)/i,
-      :pass		=> /^PASS\s+([^\s]+)/i,
-      :login_pass => /^(230\s*[^\n]+)/i,
-      :login_fail => /^(5\d\d\s*[^\n]+)/i,
-      :bye      => /^221/
-    }
+    # This formula serves as the base class for several very similar
+# formulae for Amazon Web Services related tools.
+class AmazonWebServicesFormula < Formula
+  # Use this method to peform a standard install for Java-based tools,
+  # keeping the .jars out of HOMEBREW_PREFIX/lib
+  def install
+    rm Dir['bin/*.cmd'] # Remove Windows versions
+    libexec.install Dir['*']
+    bin.install_symlink Dir['#{libexec}/bin/*'] - ['#{libexec}/bin/service']
   end
+  alias_method :standard_install, :install
     
-    # This is a completely hackish way to do this, and could break with future
-# versions of the JDK.  Need to find a better way to use sun.security.tools.KeyTool
-# and .JarSigner than modifying the source.  These rely on internal APIs that may
-# change.
-signer = Rjb::import('javaCompile.SignJar')
-#clsKeyTool = Rjb::import('sun.security.tools.KeyTool')
-#clsKeyTool = Rjb::import('sun.security.tools.KeyToolMSF')
-#clsJarSigner = Rjb::import('javaCompile.SignJar.JarSignerMSF')
-#clsJarSigner = Rjb::import('sun.security.tools.JarSigner')
-#clsJarSigner = Rjb::import('sun.security.tools.JarSignerMSF')
+      describe 'instance methods' do
+    let(:key_attributes) do # these keys are intentionally strings.
+      {
+        'canDownload' => false,
+        'canRevoke' => true,
+        'keyId' => 'some-key-id',
+        'keyName' => 'fastlane',
+        'servicesCount' => 3,
+        'services' => [
+          {
+            'name' => 'APNS',
+            'id' => 'U27F4V844T',
+            'configurations' => []
+          },
+          {
+            'name' => 'MusicKit',
+            'id' => '6A7HVUVQ3M',
+            'configurations' => [
+              {
+                'name' => 'music id test',
+                'identifier' => 'music.com.snatchev.test',
+                'type' => 'music',
+                'prefix' => 'some-prefix-id',
+                'id' => 'some-music-kit-id'
+              }
+            ]
+          },
+          {
+            'name' => 'DeviceCheck',
+            'id' => 'DQ8HTZ7739',
+            'configurations' => []
+          }
+        ]
+      }
+    end
     
-    task default: :test
+          it 'splits correctly' do
+        expected = [
+          'One',
+          'Two',
+          'Three',
+          'Four Token',
+          'Or',
+          'Newlines',
+          'Everywhere'
+        ]
+        expect(generator.split_keywords(keywords)).to eq(expected)
+      end
+    end
+  end
+end
+
     
-            def index
-          authorize! :read, StockMovement
-          @stock_movements = scope.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
-          respond_with(@stock_movements)
-        end
+          it 'it increments all targets minor version major' do
+        Fastlane::FastFile.new.parse('lane :test do
+          increment_version_number(bump_type: 'major')
+        end').runner.execute(:test)
