64ed55c93e9901b5124db000b3711c64a0c6c5b3
SUNsung <weatherius@gmail.com>
2018-06-12 06:16:56 +0300

    Rewrite [12ASXjxmDDbDk12vZ0bEefHEes12rwRA9zkvEIM12CN0.py] File in: 2018-06-12 06:16:56

--- pyton/12ASXjxmDDbDk12vZ0bEefHEes12rwRA9zkvEIM12CN0.py
+++ pyton/12ASXjxmDDbDk12vZ0bEefHEes12rwRA9zkvEIM12CN0.py
@@ -1,68 +1,145 @@
 
         
-        from datetime import datetime
-from uuid import uuid4
+        cc_test(
+    name = 'disjoint_set_forest_test',
+    size = 'small',
+    srcs = ['disjoint_set_forest_test.cc'],
+    deps = [
+        ':disjoint_set_forest',
+        '//syntaxnet:base',
+        '//syntaxnet:test_main',
+        '@org_tensorflow//tensorflow/core:test',
+    ],
+)
     
-        for idx, (loader, srcobj, triple) in enumerate(attempts):
-        if isinstance(srcobj, Flask):
-            src_info = 'application '%s'' % srcobj.import_name
-        elif isinstance(srcobj, Blueprint):
-            src_info = 'blueprint '%s' (%s)' % (srcobj.name,
-                                                srcobj.import_name)
-        else:
-            src_info = repr(srcobj)
+    tf_proto_library_cc(
+    name = 'cell_trace_proto',
+    srcs = ['cell_trace.proto'],
+    protodeps = [':trace_proto'],
+)
     
-        #: When data is read or written, this is set to ``True``. Used by
-    # :class:`.SecureCookieSessionInterface` to add a ``Vary: Cookie``
-    #: header, which allows caching proxies to cache different pages for
-    #: different users.
-    accessed = False
+      def get_layer_size(self, unused_name):
+    return _TOKEN_DIM
     
-        class _FakeSignal(object):
-        '''If blinker is unavailable, create a fake class with the same
-        interface that allows sending of signals but will fail with an
-        error on anything else.  Instead of doing anything on send, it
-        will just ignore the arguments and do nothing instead.
+          lengths = tf.constant([1, 2, 3], tf.int64)
+      max_length = 4
+      valid_arcs, valid_tokens = digraph_ops.ValidArcAndTokenMasks(
+          lengths, max_length)
+      self.assertAllEqual(valid_arcs.eval(),
+                          [[[1, 0, 0, 0],
+                            [0, 0, 0, 0],
+                            [0, 0, 0, 0],
+                            [0, 0, 0, 0]],
+                           [[1, 1, 0, 0],
+                            [1, 1, 0, 0],
+                            [0, 0, 0, 0],
+                            [0, 0, 0, 0]],
+                           [[1, 1, 1, 0],
+                            [1, 1, 1, 0],
+                            [1, 1, 1, 0],
+                            [0, 0, 0, 0]]])  # pyformat: disable
+      self.assertAllEqual(valid_tokens.eval(),
+                          [[1, 0, 0, 0],
+                           [1, 1, 0, 0],
+                           [1, 1, 1, 0]])  # pyformat: disable
+    
+    flags.DEFINE_string('master_spec', None, 'Path to task context with '
+                    'inputs and parameters for feature extractors.')
+flags.DEFINE_string('params_path', None, 'Path to trained model parameters.')
+flags.DEFINE_string('export_path', '', 'Output path for exported servo model.')
+flags.DEFINE_bool('export_moving_averages', False,
+                  'Whether to export the moving average parameters.')
+flags.DEFINE_bool('build_runtime_graph', False,
+                  'Whether to build a graph for use by the runtime.')
+    
+          saved_model_builder.save()
+
+    
+    
+class DiffTest(tf.test.TestCase):
+    
+          # As should AttachDataReader.
+      path = _find_input_path_to_type(path[-1], 'AttachDataReader')
+      self.assertNotEmpty(path)
+    
+      # Apply the Matrix-Tree theorem.
+  exp_normalized_laplacian_bxmxm = digraph_ops.LaplacianMatrix(
+      num_nodes, scores_bxmxm, forest=forest)
+  log_normalized_partition_function_b = tf.log(
+      tf.matrix_determinant(exp_normalized_laplacian_bxmxm))
+    
+        if s1.st_dev != s2.st_dev:
+        return True     # path/.. on a different device as path
+    if s1.st_ino == s2.st_ino:
+        return True     # path/.. is the same i-node as path, i.e. path=='/'
+    return False
+
+    
+        target.add_argument('--branch',
+                        help='branch name')
+    
+            # no location in URL
+        input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/targetHttpProxies/mytargetproxy/setUrlMap'
+        actual = GCPUtils.parse_gcp_url(input_url)
+        self.assertEquals('compute', actual['service'])
+        self.assertEquals('v1', actual['api_version'])
+        self.assertEquals('myproject', actual['project'])
+        self.assertFalse('global' in actual)
+        self.assertEquals('targetHttpProxies', actual['resource_name'])
+        self.assertEquals('mytargetproxy', actual['entity_name'])
+        self.assertEquals('setUrlMap', actual['method_name'])
+    
+        terminal_stderr_re = [
+        re.compile(br'\n\s*Invalid command:'),
+        re.compile(br'\nCommit failed'),
+        re.compile(br'\n\s+Set failed'),
+    ]
+    
+    
+class TerminalModule(TerminalBase):
+    
+            # in some cases, we may have pre-read the data and then
+        # passed it to the load() call for YAML, in which case we
+        # want to override the default datasource (which would be
+        # '<string>') to the actual filename we read in
+        datasource = self._ansible_file_name or node.start_mark.name
+    
+        def test_max_delay_none(self):
+        strategy = _exponential_backoff(retries=7, delay=1, backoff=2, max_delay=None)
+        result = list(strategy())
+        self.assertEquals(result, [1, 2, 4, 8, 16, 32, 64])
+    
+            if 'latency' in request.args:
+            latency = float(request.args['latency'][0])
+            reactor.callLater(latency, self._finish, request)
+            return NOT_DONE_YET
+    
+        def test_car_adapter_shall_make_very_loud_noise(self):
+        car = Car()
+        car_adapter = Adapter(car, make_noise=car.make_noise)
+        noise = car_adapter.make_noise(10)
+        expected_noise = 'vroom!!!!!!!!!!'
+    
+    
+class Subscriber:
+    
+    
+    
+    class TimeDisplay(object):
+    
+        def test_display_current_time_at_midnight(self):
         '''
+        Would almost always fail (despite of right at/after midnight) if
+        untestable production code would have been used.
+        '''
+        time_provider_stub = MidnightTimeProvider()
+        class_under_test = TimeDisplay()
+        expected_time = '<span class=\'tinyBoldText\'>24:01</span>'
+        self.assertEqual(class_under_test.get_current_time_as_html_fragment(time_provider_stub), expected_time)
+    
+        def __init__(self):
+        self._tm = None
+        self._bProblem = 0
     
     
-def rmse(a, b):
-    return np.sqrt(np.mean((a - b) ** 2))
-    
-    In the second benchmark, we increase the number of dimensions of the
-training set. Then we plot the computation time as function of
-the number of dimensions.
-    
-    # to store the results
-scikit_classifier_results = []
-scikit_regressor_results = []
-    
-        try:
-        fn = inspect.getsourcefile(obj)
-    except Exception:
-        fn = None
-    if not fn:
-        try:
-            fn = inspect.getsourcefile(sys.modules[obj.__module__])
-        except Exception:
-            fn = None
-    if not fn:
-        return
-    
-        in_exercise_region = False
-    
-            controller.coef0 = Tk.StringVar()
-        controller.coef0.set('0')
-        r = Tk.Frame(valbox)
-        Tk.Label(r, text='coef0:', anchor='e', width=7).pack(side=Tk.LEFT)
-        Tk.Entry(r, width=6, textvariable=controller.coef0).pack(side=Tk.LEFT)
-        r.pack()
-        valbox.pack(side=Tk.LEFT)
-    
-    ax = plt.axes([0.15, 0.04, 0.7, 0.05])
-plt.title('Probability')
-plt.colorbar(imshow_handle, cax=ax, orientation='horizontal')
-    
-    Only adjusted measures can hence safely be used as a consensus index
-to evaluate the average stability of clustering algorithms for a given
-value of k on various overlapping sub-samples of the dataset.
+class Specification(object):
