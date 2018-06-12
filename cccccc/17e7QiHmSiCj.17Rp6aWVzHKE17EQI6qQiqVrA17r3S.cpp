72d061c207cf9ce4112eadc2df4e686b5b9db2cd
SUNsung <weatherius@gmail.com>
2018-06-12 07:24:17 +0300

    Rewrite [17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp] File in: 2018-06-12 07:24:17

--- cccccc/17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp
+++ cccccc/17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp
@@ -1,50 +1,303 @@
 
         
-          /// When evaluating an expression in the context of an existing source file,
-  /// we may want to prefer declarations from that source file.
-  /// The DebuggerClient can return a private-discriminator to tell lookup to
-  /// prefer these certain decls.
-  virtual Identifier getPreferredPrivateDiscriminator() = 0;
+        class DebugGraphDecorator : public DebugGraphDecoratorInterface {
+ public:
+  DebugGraphDecorator(const DebugOptions& debug_options)
+      : debug_options_(debug_options) {}
+  virtual ~DebugGraphDecorator() {}
+    }
+    
+    #endif  // TENSORFLOW_COMMON_RUNTIME_SESSION_FACTORY_H_
+
+    
+    // Declare here, so we don't need a public header.
+Status RemoveDevice(const GraphDef& input_graph_def,
+                    const TransformFuncContext& context,
+                    GraphDef* output_graph_def);
+    
+    // TODO: Enable GPU support for angle op after resolving
+// build failures on GPU (See #10643 for context).
+#if 0 && GOOGLE_CUDA
     
     
-llvm::Optional<ASTNode>
-SyntaxASTMap::getNodeForSyntax(syntax::Syntax SyntaxNode) const {
-  auto Found = SyntaxMap.find(SyntaxNode.Root);
-  if (Found == SyntaxMap.end()) {
-    return None;
+    {  // clang-format off
+  std::vector<FDH::Node> nodes = {
+   {{'x_shape'}, 'Shape', {'x'}},
+   {{'x_rank'}, 'Rank', {'x'}},
+   {{'i_shape'}, 'Shape', {'i'}, {{'T', DT_INT32}}},
+   FDH::Const('zero', 0),
+   FDH::Const('one', 1),
+   // stitch_idx0 = Range(0, x_rank, 1)
+   {{'stitch_val1'}, 'Fill', {'i_shape:output:0', 'one:output:0'},
+    {{'T', DT_INT32}}},
+   {{'y_shape'}, 'DynamicStitch',
+    {'stitch_idx0:output:0', 'i',
+     'x_shape:output:0', 'stitch_val1:output:0'},
+    {{'N', 2}, {'T', DT_INT32}}},
+   {{'tile_scaling'}, 'Div', {'x_shape:output:0', 'y_shape:merged:0'},
+    {{'T', DT_INT32}}},
+   {{'di'}, 'ZerosLike', {'i'}, {{'T', DT_INT32}}}
+  };
+  // clang-format on
+  nodes.insert(nodes.end(), body.begin(), body.end());
+  for (auto& n : nodes) {
+    if (n.attr.empty()) {
+      n.attr = {{'T', '$T'}};
+    }
   }
-  return Found->getSecond();
+  // 'Range' doesn't need any attr.
+  nodes.push_back({{'stitch_idx0'},
+                   'Range',
+                   {'zero:output:0', 'x_rank:output:0', 'one:output:0'},
+                   {}});
+  *g = FDH::Create('_',
+                   // Input defs
+                   {'x:T', 'i:int32', 'dy:T'},
+                   // Ret val defs
+                   {'dx:T', 'di:int32'},
+                   // Attr defs
+                   {{'T: {half, float, double}'}},
+                   // Nodes
+                   nodes,
+                   // Return values
+                   {{'dx', 'dx:output:0'}, {'di', 'di:y:0'}});
+  return Status::OK();
 }
     
-    #include 'llvm/ADT/SetVector.h'
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    // NOTE: this class is NOT meant to be used in threaded contexts.
-@interface ObjectBehaviorVerifier : NSObject
-@property (readonly) BOOL wasRetained;
-@property (readonly) BOOL wasCopied;
-@property (readonly) BOOL wasMutableCopied;
-    
-      Symbol *getSymbol(size_t index) {
-    assert(index < symbols.size());
-    return &symbols[index];
-  }
-    
-      size_t NumChildren;
-  unsigned Level;
+    #include 'tensorflow/core/framework/tensor_testutil.h'
+#include 'tensorflow/core/lib/core/status_test_util.h'
+#include 'tensorflow/core/lib/io/path.h'
+#include 'tensorflow/core/lib/strings/str_util.h'
+#include 'tensorflow/core/platform/env.h'
+#include 'tensorflow/core/platform/test.h'
     
     
     {
-    {  bool didErrorOccur() {
-    return DidErrorOccur;
+    {    if (SemanticNode.is<Expr *>()) {
+      SemanticNode.get<Expr *>()->dump(llvm::errs());
+    } else if (SemanticNode.is<Decl *>()) {
+      SemanticNode.get<Decl *>()->dump(llvm::errs());
+    } else if (SemanticNode.is<Expr *>()) {
+      SemanticNode.get<Expr *>()->dump(llvm::errs());
+    } else {
+      llvm_unreachable('ASTNode has pointer to unknown thing!');
+    }
+    llvm::errs() << '\n=====================================================\n';
   }
-};
-  
+}
+
+    
+    
+    {
+    {    for (auto *FU : mod->getFiles()) {
+      switch (FU->getKind()) {
+      case FileUnitKind::Source:
+      case FileUnitKind::Derived:
+      case FileUnitKind::Builtin:
+        break;
+      case FileUnitKind::SerializedAST:
+      case FileUnitKind::ClangModule: {
+        auto *LFU = cast<LoadedFile>(FU);
+        if (auto *F = fileMgr.getFile(LFU->getFilename())) {
+          std::string moduleName = mod->getNameStr();
+          bool withoutUnitName = true;
+          if (FU->getKind() == FileUnitKind::ClangModule) {
+            withoutUnitName = false;
+            auto clangModUnit = cast<ClangModuleUnit>(LFU);
+            if (auto clangMod = clangModUnit->getUnderlyingClangModule()) {
+              moduleName = clangMod->getTopLevelModuleName();
+              // FIXME: clang's -Rremarks do not seem to go through Swift's
+              // diagnostic emitter.
+              clang::index::emitIndexDataForModuleFile(clangMod,
+                                                       clangCI, unitWriter);
+            }
+          } else {
+            // Serialized AST file.
+            // Only index system modules (essentially stdlib and overlays).
+            // We don't officially support binary swift modules, so normally
+            // the index data for user modules would get generated while
+            // building them.
+            if (mod->isSystemModule() && indexSystemModules) {
+              emitDataForSwiftSerializedModule(mod, indexStorePath,
+                                               indexSystemModules,
+                                               targetTriple, clangCI, diags,
+                                               unitWriter);
+              withoutUnitName = false;
+            }
+          }
+          clang::index::writer::OpaqueModule opaqMod =
+              moduleNameScratch.createString(moduleName);
+          unitWriter.addASTFileDependency(F, mod->isSystemModule(), opaqMod,
+                                          withoutUnitName);
+        }
+        break;
+      }
+      }
+    }
+  }
 }
     
-    /// A shorthand to clearly indicate that a value is a reference counted and
-/// heap-allocated.
-template <typename Inner>
-using RC = llvm::IntrusiveRefCntPtr<Inner>;
     
-    class CanType;
-class SILValue;
+    {  // Make vanilla new/delete illegal.
+  void *operator new(size_t Bytes) = delete;
+  void operator delete(void *Data) = delete;
+};
+    
+    
+    {  /// Return a hash code of any components from these options that should
+  /// contribute to a Swift Bridging PCH hash.
+  llvm::hash_code getPCHHashComponents() const {
+    // Nothing here that contributes anything significant when emitting the PCH.
+    return llvm::hash_value(0);
+  }
+};
+    
+    #ifndef SWIFT_INDEX_INDEXSYMBOL_H
+#define SWIFT_INDEX_INDEXSYMBOL_H
+    
+    Variant HHVM_FUNCTION(xhprof_disable) {
+  return s_profiler_factory->stop();
+}
+    
+    namespace HPHP { namespace jit {
+///////////////////////////////////////////////////////////////////////////////
+    }
+    }
+    
+      case GMP_ROUND_PLUSINF:
+    mpz_cdiv_r(gmpReturn, gmpDataA, gmpDataB);
+    break;
+    
+    // Normalize incoming arc weights and compute avgCallOffset for each node.
+void TargetGraph::normalizeArcWeights() {
+  for (TargetId f = 0; f < targets.size(); f++) {
+    auto& func = targets[f];
+    for (auto src : func.preds) {
+      auto& arc = *arcs.find(Arc(src, f));
+      arc.normalizedWeight = arc.weight / func.samples;
+      if (arc.weight == 0) continue;
+      arc.avgCallOffset = arc.avgCallOffset / arc.weight;
+      assert(arc.avgCallOffset < targets[src].size);
+    }
+  }
+}
+    
+    
+    {  return rawbits_to_double(result);
+}
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    
+    #ifdef HAVE_NUMA
+namespace {
+// We support at most 32 NUMA nodes (numa_node_set in 32-bit), so a single
+// unsigned long is more than enough for the mask.  This can be used in jemalloc
+// allocation hooks, so it is wise to avoid calling malloc/free here, even
+// though jemalloc might still be able to handle reentrance correctly.  Thus, we
+// bypass libnuma and do the syscalls directly here.
+struct SavedNumaPolicy {
+  bool needRestore{false};
+  int oldPolicy{0};
+  unsigned long oldMask{0};
+    }
+    }
+    
+    TEST(VersionEditTest, EncodeDecode) {
+  static const uint64_t kBig = 1ull << 50;
+    }
+    
+    int main(int argc, char** argv) {
+  return leveldb::test::RunAllTests();
+}
+
+    
+    
+    {}
+    
+    Status BlockHandle::DecodeFrom(Slice* input) {
+  if (GetVarint64(input, &offset_) &&
+      GetVarint64(input, &size_)) {
+    return Status::OK();
+  } else {
+    return Status::Corruption('bad block handle');
+  }
+}
+    
+    
+    { private:
+  port::Mutex *const mu_;
+  // No copying allowed
+  MutexLock(const MutexLock&);
+  void operator=(const MutexLock&);
+};
+    
+    class DatabasePluginTests : public testing::Test {
+ public:
+  void SetUp() override;
+    }
+    
+        // Widgets: Value() Helpers. Output single value in 'name: value' format (tip: freely declare more in your code to handle your types. you can add functions to the ImGui namespace)
+    IMGUI_API void          Value(const char* prefix, bool b);
+    IMGUI_API void          Value(const char* prefix, int v);
+    IMGUI_API void          Value(const char* prefix, unsigned int v);
+    IMGUI_API void          Value(const char* prefix, float v, const char* float_format = NULL);
+    
+            // Get the function pointer (required for any extensions)
+        auto vkCreateDebugReportCallbackEXT = (PFN_vkCreateDebugReportCallbackEXT)vkGetInstanceProcAddr(g_Instance, 'vkCreateDebugReportCallbackEXT');
+        IM_ASSERT(vkCreateDebugReportCallbackEXT != NULL);
+    
+        // Create shaders
+    const GLchar *vertex_shader =
+        'uniform mat4 ProjMtx;\n'
+        'in vec2 Position;\n'
+        'in vec2 UV;\n'
+        'in vec4 Color;\n'
+        'out vec2 Frag_UV;\n'
+        'out vec4 Frag_Color;\n'
+        'void main()\n'
+        '{\n'
+        '	Frag_UV = UV;\n'
+        '	Frag_Color = Color;\n'
+        '	gl_Position = ProjMtx * vec4(Position.xy,0,1);\n'
+        '}\n';
+    
+    // That gcc wants both of these prototypes seems mysterious. VC, for
+// its part, can't decide which to use (another mystery). Matching of
+// template overloads: the final frontier.
+#ifndef COMPILER_MSVC
+template <typename T, size_t N>
+char (&ArraySizeHelper(const T (&array)[N]))[N];
+#endif
+    
+    #endif  // BENCHMARK_COMMANDLINEFLAGS_H_
+
+    
+    
+    {  // Print user counters
+  for (const auto &ucn : user_counter_names_) {
+    auto it = run.counters.find(ucn);
+    if(it == run.counters.end()) {
+      Out << ',';
+    } else {
+      Out << ',' << it->second;
+    }
+  }
+  Out << '\n';
+}
+    
+    namespace benchmark {
+    }
+    
+    auto Sqr = [](const double dat) { return dat * dat; };
+auto Sqrt = [](const double dat) {
+  // Avoid NaN due to imprecision in the calculations
+  if (dat < 0.0) return 0.0;
+  return std::sqrt(dat);
+};
