dadbdfb226a7a00de9ee5fed72281d501b4312b6
SUNsung <weatherius@gmail.com>
2018-06-12 13:37:43 +0300

    Rewrite [34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb] File in: 2018-06-12 13:37:42

--- ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
+++ ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
@@ -1,11 +1,53 @@
 
         
-        module VagrantPlugins
-  module CommandBox
-    module Command
-      class Remove < Vagrant.plugin('2', :command)
-        def execute
-          options = {}
-          options[:force] = false
+        # No trailing slash
+Benchmark.ips do |x|
+  path = '/some/very/very/long/path/to/a/file/i/like/'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
+end
+
     
-      def_delegators :@logger, :log, :log_status, :log_processing, :log_transform, :log_file_info, :log_processed, :log_http_get_file, :log_http_get_files, :silence_log
+              @new_body.each do |line|
+            if !@livereload_added && line['<head']
+              line.gsub!(HEAD_TAG_REGEX) do |match|
+                %(#{match}#{template.result(binding)})
+              end
+    
+        def safe_load_file(filename)
+      case File.extname(filename)
+      when %r!\.toml!i
+        Jekyll::External.require_with_graceful_fail('tomlrb') unless defined?(Tomlrb)
+        Tomlrb.load_file(filename)
+      when %r!\.ya?ml!i
+        SafeYAML.load_file(filename) || {}
+      else
+        raise ArgumentError, 'No parser for '#{filename}' is available.
+          Use a .y(a)ml or .toml file instead.'
+      end
+    end
+    
+        # Transform the contents based on the content type.
+    #
+    # Returns the transformed contents.
+    def transform
+      _renderer.convert(content)
+    end
+    
+        find_union(segments, Group).order_id_desc
+  end
+    
+      def failed_strategy
+    request.respond_to?(:get_header) ? request.get_header('omniauth.error.strategy') : request.env['omniauth.error.strategy']
+  end
+    
+      # POST /resource/sign_in
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
