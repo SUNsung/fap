e408c620adbed48c59fd6ad3f649695060aa2b75
SUNsung <weatherius@gmail.com>
2018-06-12 23:39:16 +0300

    Rewrite [331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py] File in: 2018-06-12 23:39:15

--- pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
+++ pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
@@ -1,72 +1,151 @@
 
         
-            # noinspection PyProtectedMember
-    @property
-    def headers(self):
-        original = self._orig.raw._original_response
+        print('Enter the PKCS1 private key, followed by a blank line:')
+privkey = b''
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    if line == '':
+        break
+    privkey += line.encode('ascii') + b'\n'
+privkey = rsa.PrivateKey.load_pkcs1(privkey)
     
-            '''
-        available_plugins = plugin_manager.get_formatters_grouped()
-        self.enabled_plugins = []
-        for group in groups:
-            for cls in available_plugins[group]:
-                p = cls(env=env, **kwargs)
-                if p.enabled:
-                    self.enabled_plugins.append(p)
+        infile, outfile = args
     
-        def __call__(self, r):
-        '''
-        Override username/password serialization to allow unicode.
-    
-    # FIXME:
-# We test against a local httpbin instance which uses a self-signed cert.
-# Requests without --verify=<CA_BUNDLE> will fail with a verification error.
-# See: https://github.com/kevin1024/pytest-httpbin#https-support
-CA_BUNDLE = pytest_httpbin.certs.where()
+        def test_keywords(self):
+        self.assertMatch(':ytsubs', ['youtube:subscriptions'])
+        self.assertMatch(':ytsubscriptions', ['youtube:subscriptions'])
+        self.assertMatch(':ythistory', ['youtube:history'])
     
     
-@pytest.mark.parametrize('argument_name', ['--auth-type', '-A'])
-def test_digest_auth(httpbin_both, argument_name):
-    r = http(argument_name + '=digest', '--auth=user:password',
-             'GET', httpbin_both.url + '/digest-auth/auth/user/password')
+def gen_extractors():
+    ''' Return a list of an instance of every supported extractor.
+    The order does matter; the first extractor matched is the one handling the URL.
+    '''
+    return [klass() for klass in gen_extractor_classes()]
+    
+        def _real_extract(self, url):
+        display_id = self._match_id(url)
+    
+    
+def test_auth_plugin_require_auth_false_and_auth_provided(httpbin):
+    
+    
+@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
+@pytest.mark.parametrize('filename', filenames)
+def test_rst_file_syntax(filename):
+    p = subprocess.Popen(
+        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
+    err = p.communicate()[1]
+    assert p.returncode == 0, err.decode('utf8')
+
+    
+    
+class HTTPBasicAuth(requests.auth.HTTPBasicAuth):
+    
+    
+@mock.patch('httpie.input.AuthCredentials._getpass',
+            new=lambda self, prompt: 'password')
+def test_password_prompt(httpbin):
+    r = http('--auth', 'user',
+             'GET', httpbin.url + '/basic-auth/user/password')
     assert HTTP_OK in r
     assert r.json == {'authenticated': True, 'user': 'user'}
     
-        def test_make_entry(self):
-        equal = self.assertEqual
-        self.dialog.row = 0
-        self.dialog.top = self.root
-        entry, label = self.dialog.make_entry('Test:', 'hello')
-        equal(label['text'], 'Test:')
+            if now - self.lastmark >= 3:
+            self.lastmark = now
+            qps = len(self.tail) / sum(self.tail)
+            print('samplesize={0} concurrent={1} qps={2:0.2f}'.format(len(self.tail), self.concurrent, qps))
     
-            User enters a name for the Help resource and a web url or file
-        name. The user can browse for the file.
-        '''
-        self.filepath = filepath
-        message = 'Name for item on Help menu:'
-        super().__init__(
-                parent, title, message, text0=menuitem,
-                used_names=used_names, _htest=_htest, _utest=_utest)
+        def remove(self, key):
+        hash_index = self._hash_function(key)
+        for index, item in enumerate(self.table[hash_index]):
+            if item.key == key:
+                del self.table[hash_index][index]
+                return
+        raise KeyError('Key not found')
     
-    server = SimpleXMLRPCServer(('localhost', 8000))
-server.register_introspection_functions()
-server.register_instance(Math())
-server.serve_forever()
+        def reducer_identity(self, key, value):
+        yield key, value
     
-        def test_badfuture6(self):
-        with self.assertRaises(SyntaxError) as cm:
-            from test import badsyntax_future6
-        self.check_syntax_error(cm.exception, 'badsyntax_future6', 3)
+        def __init__(self, results):
+        self.results = results
+        self.next = next
     
-            rd, wr = self.make_socketpair()
+        def __init__(self, user_id, name, pass_hash):
+        self.user_id = user_id
+        self.name = name
+        self.pass_hash = pass_hash
+        self.friends_by_id = {}  # key: friend id, value: User
+        self.friend_ids_to_private_chats = {}  # key: friend id, value: private chats
+        self.group_chats_by_id = {}  # key: chat id, value: GroupChat
+        self.received_friend_requests_by_friend_id = {}  # key: friend id, value: AddRequest
+        self.sent_friend_requests_by_friend_id = {}  # key: friend id, value: AddRequest
     
-    if __name__ == '__main__':
-    # Default is to print the aliased verbose platform string
-    terse = ('terse' in sys.argv or '--terse' in sys.argv)
-    aliased = (not 'nonaliased' in sys.argv and not '--nonaliased' in sys.argv)
-    print(platform(aliased, terse))
-    sys.exit(0)
-
+    def sequential():
+    return list(map(is_prime, PRIMES))
     
-    from dataclasses import dataclass, InitVar
-from typing import ClassVar
+    
+def OverlapLength( left_string, right_string ):
+  '''Returns the length of the overlap between two strings.
+  Example: 'foo baro' and 'baro zoo' -> 4
+  '''
+  left_string_length = len( left_string )
+  right_string_length = len( right_string )
+    
+    
+  def Response( self ):
+    return {
+      'completions': self._results,
+      'completion_start_column': self.request_data[ 'start_column' ]
+    }
+    
+    import argparse
+    
+    
+  def Just_Doc_String_test( self ):
+    self._Check( 'not_an_int', {
+      'insertion_text':  'INSERTION TEXT',
+      'menu_text':       'MENU TEXT',
+      'extra_menu_info': 'EXTRA MENU INFO',
+      'kind':            'K',
+      'extra_data': {
+        'doc_string':    'DOC STRING',
+      },
+    }, {
+      'word'     : 'INSERTION TEXT',
+      'abbr'     : 'MENU TEXT',
+      'menu'     : 'EXTRA MENU INFO',
+      'kind'     : 'k',
+      'info'     : 'DOC STRING',
+      'dup'      : 1,
+      'empty'    : 1,
+      'user_data': 'not_an_int',
+    } )
+    
+    
+  def Start( self ):
+    request_data = BuildRequestData()
+    request_data.update( { 'filetypes': self.filetypes } )
+    self._response = self.PostDataToHandler( request_data,
+                                             'semantic_completion_available' )
+    
+    
+  def Poll( self, diagnostics_handler ):
+    '''This should be called regularly to check for new messages in this buffer.
+    Returns True if Poll should be called again in a while. Returns False when
+    the completer or server indicated that further polling should not be done
+    for the requested file.'''
+    
+    
+__licence__ = 'BSD (3 clause)'
+    
+    For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/light.zigbee/
+'''
+import voluptuous as vol
