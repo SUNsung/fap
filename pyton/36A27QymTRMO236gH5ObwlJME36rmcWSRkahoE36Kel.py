b7be70bb9ffe299a02d25e433cbeb700cf2d9513
SUNsung <weatherius@gmail.com>
2018-06-12 19:47:49 +0300

    Rewrite [36A27QymTRMO236gH5ObwlJME36rmcWSRkahoE36Kel.py] File in: 2018-06-12 19:47:48

--- pyton/36A27QymTRMO236gH5ObwlJME36rmcWSRkahoE36Kel.py
+++ pyton/36A27QymTRMO236gH5ObwlJME36rmcWSRkahoE36Kel.py
@@ -1,190 +1,192 @@
-  def __init__(self, metagraph, model):
-    self._metagraph = metagraph
-    self.replicate_states(model.initial_state_name)
-    self.replicate_states(model.final_state_name)
-    self.update_snapshot_name('variables')
-    self.update_snapshot_name('trainable_variables')
+
+        
+                Operator:                  '#582800',   # class: 'o'
+        Operator.Word:             'bold #004461',   # class: 'ow' - like keywords
     
-      Returns:
-    feed_dict: A dictionary of input Tensor to numpy array.
-  '''
-  feed_dict = {}
-  for feature, tensor in model.time_series_features.items():
-    feed_dict[tensor] = features['time_series_features'][feature]
-  for feature, tensor in model.aux_features.items():
-    feed_dict[tensor] = features['aux_features'][feature]
+    '''
+requests._internal_utils
+~~~~~~~~~~~~~~
     
-      def __getattr__(self, attribute):
-    try:
-      return self[attribute]
-    except KeyError as e:
-      raise AttributeError(e)
+        # Informational.
+    100: ('continue',),
+    101: ('switching_protocols',),
+    102: ('processing',),
+    103: ('checkpoint',),
+    122: ('uri_too_long', 'request_uri_too_long'),
+    200: ('ok', 'okay', 'all_ok', 'all_okay', 'all_good', '\\o/', '✓'),
+    201: ('created',),
+    202: ('accepted',),
+    203: ('non_authoritative_info', 'non_authoritative_information'),
+    204: ('no_content',),
+    205: ('reset_content', 'reset'),
+    206: ('partial_content', 'partial'),
+    207: ('multi_status', 'multiple_status', 'multi_stati', 'multiple_stati'),
+    208: ('already_reported',),
+    226: ('im_used',),
     
-      # Tiny periods or erroneous time values could make this loop take forever.
-  if (t_max - t_min) / event.period > 10**6:
-    raise ValueError(
-        'Too many transits! Time range is [%.2f, %.2f] and period is %.2e.' %
-        (t_min, t_max, event.period))
+        expected_digest = (b'Authorization: Digest username='user', '
+                       b'realm='me@kennethreitz.com', '
+                       b'nonce='6bf5d6e4da1ce66918800195d6b9130d', uri='/'')
     
-      def test_mnist_model_fn_predict_mode(self):
-    self.mnist_model_fn_helper(tf.estimator.ModeKeys.PREDICT)
+    elif is_py3:
+    from urllib.parse import urlparse, urlunparse, urljoin, urlsplit, urlencode, quote, unquote, quote_plus, unquote_plus, urldefrag
+    from urllib.request import parse_http_list, getproxies, proxy_bypass, proxy_bypass_environment, getproxies_environment
+    from http import cookiejar as cookielib
+    from http.cookies import Morsel
+    from io import StringIO
+    from collections import OrderedDict
+    from collections.abc import Callable, Mapping, MutableMapping
+    
+        :param jar: cookielib.CookieJar (not necessarily a RequestsCookieJar)
+    :param request: our own requests.Request object
+    :param response: urllib3.HTTPResponse object
+    '''
+    if not (hasattr(response, '_original_response') and
+            response._original_response):
+        return
+    # the _original_response field is the wrapped httplib.HTTPResponse object,
+    req = MockRequest(request)
+    # pull out the HTTPMessage with the headers and put it in the mock:
+    res = MockResponse(response._original_response.msg)
+    jar.extract_cookies(res, req)
     
     
-if __name__ == '__main__':
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(argv=[sys.argv[0]] + unparsed)
+class LookupDict(dict):
+    '''Dictionary lookup object.'''
+    
+    seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
+                   padding='same', return_sequences=True))
+seq.add(BatchNormalization())
+    
+    from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Embedding
+from keras.layers import LSTM
+from keras.datasets import imdb
+    
+    
+class Page(object):
+    
+        def spot_freed(self):
+        self.available_spots += 1
+    
+        def __init__(self, seller_category_map, seller_category_overrides_map):
+        self.seller_category_map = seller_category_map
+        self.seller_category_overrides_map = seller_category_overrides_map
+    
+    
+class sockaddr(ctypes.Structure):
+    _fields_ = [('sa_family', ctypes.c_short),
+                ('__pad1', ctypes.c_ushort),
+                ('ipv4_addr', ctypes.c_byte * 4),
+                ('ipv6_addr', ctypes.c_byte * 16),
+                ('__pad2', ctypes.c_ulong)]
+    
+                    if self.accept[s] >= 1:
+                    #print 'accept state for alt %d' % self.accept[s]
+                    return self.accept[s]
+    
+    # Other changes from the 2.x/3.1 nntplib:
+# - automatic querying of capabilities at connect
+# - New method NNTP.getcapabilities()
+# - New method NNTP.over()
+# - New helper function decode_header()
+# - NNTP.post() and NNTP.ihave() accept file objects, bytes-like objects and
+#   arbitrary iterables yielding lines.
+# - An extensive test suite :-)
+    
+        def _extractname(self, mo):
+        return mo.group('name').strip()
+    
+    class CFunctionCalls(unittest.TestCase):
+    
+        def test_unregister_after_socket_close(self):
+        s = self.SELECTOR()
+        self.addCleanup(s.close)
+        rd, wr = self.make_socketpair()
+        s.register(rd, selectors.EVENT_READ)
+        s.register(wr, selectors.EVENT_WRITE)
+        rd.close()
+        wr.close()
+        s.unregister(rd)
+        s.unregister(wr)
+    
+        def acceptNode(self, element):
+        return self.FILTER_ACCEPT
+    
+    @dataclass
+class IV:
+    T_IV4 = InitVar
+    iv0: InitVar[int]
+    iv1: InitVar
+    iv2: T_IV2
+    iv3: T_IV3
+    not_iv4: T_IV4  # When using string annotations, this field is not recognized as an InitVar.
 
     
-    '''Extracts paths that are indicative of each relation.'''
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+    import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
     
-      def Conversion3dTestWithType(self, dtype):
-    original_data = np.arange(24).reshape(2, 3, 4).astype(dtype)
-    serialized = datum_io.SerializeToString(original_data)
-    retrieved_data = datum_io.ParseFromString(serialized)
-    self.assertTrue(np.array_equal(original_data, retrieved_data))
-    
-        inputs = Input(shape=(5,))
-    x = Dense(5)(inputs)
-    output1 = Dense(1, name='output1')(x)
-    output2 = Dense(1, name='output2')(x)
-    
-        # a Sequential inside a Sequential
-    inner_model = Sequential()
-    inner_model.add(Dense(2, input_dim=1))
-    outer_model = Sequential()
-    outer_model.add(inner_model)
-    assert outer_model.trainable_weights == inner_model.trainable_weights
-    inner_model.trainable = False
-    assert outer_model.trainable_weights == []
-    inner_model.trainable = True
-    inner_model.layers[-1].trainable = False
-    assert outer_model.trainable_weights == []
-    
-    
-@keras_test
-def test_locallyconnected_2d():
-    num_samples = 5
-    filters = 3
-    stack_size = 4
-    num_row = 6
-    num_col = 8
-    padding = 'valid'
-    
-    max_words = 1000
-batch_size = 32
-epochs = 5
-    
-        # generate char sequences of length 'sequence_length' out of alphabet and store the next char as label (e.g. 'ab'->'c')
-    sequence_length = 2
-    sentences = [alphabet[i: i + sequence_length] for i in range(len(alphabet) - sequence_length)]
-    next_chars = [alphabet[i + sequence_length] for i in range(len(alphabet) - sequence_length)]
-    
-        # Test with Sequential API
-    model = Sequential([
-        layers.Dense(16, input_shape=(x_train.shape[-1],), activation='relu'),
-        layers.Dense(8),
-        layers.Activation('relu'),
-        layers.Dense(num_classes, activation='softmax')
-    ])
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='rmsprop',
-                  metrics=['accuracy'])
-    model.summary()
-    history = model.fit(x_train, y_train, epochs=15, batch_size=16,
-                        validation_data=(x_test, y_test),
-                        verbose=0)
-    assert(history.history['val_acc'][-1] > 0.8)
-    config = model.get_config()
-    model = Sequential.from_config(config)
-    
-        # Cut to a 40x40 window
-    noisy_movies = noisy_movies[::, ::, 20:60, 20:60, ::]
-    shifted_movies = shifted_movies[::, ::, 20:60, 20:60, ::]
-    noisy_movies[noisy_movies >= 1] = 1
-    shifted_movies[shifted_movies >= 1] = 1
-    return noisy_movies, shifted_movies
-    
-    
-max_features = 20000
-# cut texts after this number of words
-# (among top max_features most common words)
-maxlen = 100
-batch_size = 32
-    
-    
-def IsDecltype(clean_lines, linenum, column):
-  '''Check if the token ending on (linenum, column) is decltype().
-    
-            entrylabel = Label(frame, anchor='w', justify='left',
-                           text=self.message)
-        self.entryvar = StringVar(self, self.text0)
-        self.entry = Entry(frame, width=30, textvariable=self.entryvar)
-        self.entry.focus_set()
-        self.error_font = Font(name='TkCaptionFont',
-                               exists=True, root=self.parent)
-        self.entry_error = Label(frame, text=' ', foreground='red',
-                                 font=self.error_font)
-        self.button_ok = Button(
-                frame, text='OK', default='active', command=self.ok)
-        self.button_cancel = Button(
-                frame, text='Cancel', command=self.cancel)
-    
-    def pi_cdecimal():
-    '''cdecimal'''
-    D = C.Decimal
-    lasts, t, s, n, na, d, da = D(0), D(3), D(3), D(1), D(0), D(0), D(24)
-    while s != lasts:
-        lasts = s
-        n, na = n+na, na+8
-        d, da = d+da, da+32
-        t = (t * n) / d
-        s += t
-    return s
-    
-    if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        zones = TZInfo.zonelist()
-        for z in zones:
-            print(z)
-        sys.exit()
-    filepath = sys.argv[1]
-    if not filepath.startswith('/'):
-        filepath = os.path.join('/usr/share/zoneinfo', filepath)
-    with open(filepath, 'rb') as fileobj:
-        tzi = TZInfo.fromfile(fileobj)
-    tzi.dump(sys.stdout)
-
-    
-    
-class _AsyncDeprecatedProperty:
-    def warn(self, cls):
-        clsname = cls.__name__
-        warnings.warn(
-            '{cls}.async is deprecated; use {cls}.async_'.format(cls=clsname),
-            DeprecationWarning)
-    
-            # See http://xmlrpc.usefulinc.com/doc/sysmethodsig.html
-    
-            def wndproc(hwnd, msg, wParam, lParam):
-            return hwnd + msg + wParam + lParam
-    
-        def test_pass_pointers(self):
-        dll = CDLL(_ctypes_test.__file__)
-        func = dll._testfunc_p_p
-        if sizeof(c_longlong) == sizeof(c_void_p):
-            func.restype = c_longlong
+        def _load_ui_modules(self, modules):
+        if isinstance(modules, types.ModuleType):
+            self._load_ui_modules(dict((n, getattr(modules, n))
+                                       for n in dir(modules)))
+        elif isinstance(modules, list):
+            for m in modules:
+                self._load_ui_modules(m)
         else:
-            func.restype = c_long
+            assert isinstance(modules, dict)
+            for name, cls in modules.items():
+                try:
+                    if issubclass(cls, UIModule):
+                        self.ui_modules[name] = cls
+                except TypeError:
+                    pass
     
-        def test_3(self):
-        class X(Structure):
-            pass
-        class Y(Structure):
-            _fields_ = [('x', X)] # finalizes X
-        self.assertRaises(AttributeError, setattr, X, '_fields_', [])
+            class TestServer(TCPServer):
+            def handle_stream(self, stream, address):
+                test.assertIsNone(stream.socket.cipher())
+                test.io_loop.spawn_callback(self.handle_connection, stream)
     
+        >>> url_concat('http://example.com/foo', dict(c='d'))
+    'http://example.com/foo?c=d'
+    >>> url_concat('http://example.com/foo?a=b', dict(c='d'))
+    'http://example.com/foo?a=b&c=d'
+    >>> url_concat('http://example.com/foo?a=b', [('c', 'd'), ('c', 'd2')])
+    'http://example.com/foo?a=b&c=d&c=d2'
+    '''
+    if args is None:
+        return url
+    parsed_url = urlparse(url)
+    if isinstance(args, dict):
+        parsed_query = parse_qsl(parsed_url.query, keep_blank_values=True)
+        parsed_query.extend(args.items())
+    elif isinstance(args, list) or isinstance(args, tuple):
+        parsed_query = parse_qsl(parsed_url.query, keep_blank_values=True)
+        parsed_query.extend(args)
+    else:
+        err = ''args' parameter should be dict, list or tuple. Not {0}'.format(
+            type(args))
+        raise TypeError(err)
+    final_query = urlencode(parsed_query)
+    url = urlunparse((
+        parsed_url[0],
+        parsed_url[1],
+        parsed_url[2],
+        parsed_url[3],
+        final_query,
+        parsed_url[5]))
+    return url
     
-def main(path):
+                def finish(self):
+                event.set()
+    
+        @gen_test
+    def test_get_with_putters(self):
+        q = queues.Queue(1)
+        q.put_nowait(0)
+        put = q.put(1)
+        self.assertEqual(0, (yield q.get()))
+        self.assertIsNone((yield put))
