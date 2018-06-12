04d88f223f69a4f13b3df38bf154233fda2659bd
SUNsung <weatherius@gmail.com>
2018-06-12 17:02:17 +0300

    Rewrite [03aiY72AceAY.03fvxUicVuafs031mPPfKkpo8.03Vd0.rb] File in: 2018-06-12 17:02:17

--- ruby/03aiY72AceAY.03fvxUicVuafs031mPPfKkpo8.03Vd0.rb
+++ ruby/03aiY72AceAY.03fvxUicVuafs031mPPfKkpo8.03Vd0.rb
@@ -1,170 +1,60 @@
 
         
-                adapter = adapter.camelize
-        adapter = 'PostgreSQL' if adapter == 'Postgresql'
-        'ActionCable::SubscriptionAdapter::#{adapter}'.constantize
-      end
-    end
-  end
-end
-
-    
-                    on.unsubscribe do |chan, count|
-                  if count == 0
-                    @subscription_lock.synchronize do
-                      @raw_client = nil
-                    end
-                  end
-                end
-              end
-            end
-          end
-    
-      test 'helpers' do
-    assert_response_code_range 200..299, :successful?
-    assert_response_code_range [404],    :not_found?
-    assert_response_code_range 300..399, :redirection?
-    assert_response_code_range 500..599, :server_error?
-    assert_response_code_range 400..499, :client_error?
-  end
-    
-          # Calls this block after #reset is called on the instance. Used for resetting external collaborators, like Time.zone.
-      def resets(&block)
-        set_callback :reset, :after, &block
-      end
-    
-          body.each do |part|
-        assert part.is_a?(String), 'Each part of the body must be a String'
-        string << part
-      end
+            options[:trailing_slash] = true
     
         private
-      # 'Deserialize' the mailer class name by hand in case another argument
-      # (like a Global ID reference) raised DeserializationError.
-      def mailer_class
-        if mailer = Array(@serialized_arguments).first || Array(arguments).first
-          mailer.constantize
-        end
-      end
     
-        # Use the logger configured for ActionMailer::Base.
-    def logger
-      ActionMailer::Base.logger
-    end
-  end
-end
+        private
     
-      def app
-    APP
-  end
-    
-          plist_filename = if f.plist
-        f.plist_path.basename
-      else
-        File.basename Dir['#{keg}/*.plist'].first
-      end
-      plist_link = '#{destination}/#{plist_filename}'
-      plist_domain = f.plist_path.basename('.plist')
-      destination_path = Pathname.new File.expand_path destination
-      plist_path = destination_path/plist_filename
-    
-      # Removes any empty directories in the formula's prefix subtree
-  # Keeps any empty directions projected by skip_clean
-  # Removes any unresolved symlinks
-  def prune
-    dirs = []
-    symlinks = []
-    @f.prefix.find do |path|
-      if path == @f.libexec || @f.skip_clean?(path)
-        Find.prune
-      elsif path.symlink?
-        symlinks << path
-      elsif path.directory?
-        dirs << path
-      end
+        def ==(other)
+      other.is_a?(self.class) && filters == other.filters
     end
     
-      def external_commands
-    paths.reduce([]) do |cmds, path|
-      Dir['#{path}/brew-*'].each do |file|
-        next unless File.executable?(file)
-        cmd = File.basename(file, '.rb')[5..-1]
-        cmds << cmd unless cmd.include?('.')
-      end
-      cmds
-    end.sort
-  end
+    module Docs
+  class PageDb
+    attr_reader :pages
     
-        diff.each_line do |line|
-      status, *paths = line.split
-      src = Pathname.new paths.first
-      dst = Pathname.new paths.last
+    # IMPORTANT: The Capistrano::Plugin system is not yet considered a stable,
+# public API, and is subject to change without notice. Eventually it will be
+# officially documented and supported, but for now, use it at your own risk.
+#
+# Base class for Capistrano plugins. Makes building a Capistrano plugin as easy
+# as writing a `Capistrano::Plugin` subclass and overriding any or all of its
+# three template methods:
+#
+# * set_defaults
+# * register_hooks
+# * define_tasks
+#
+# Within the plugin you can use any methods of the Rake or Capistrano DSLs, like
+# `fetch`, `invoke`, etc. In cases when you need to use SSHKit's backend outside
+# of an `on` block, use the `backend` convenience method. E.g. `backend.test`,
+# `backend.execute`, or `backend.capture`.
+#
+# Package up and distribute your plugin class as a gem and you're good to go!
+#
+# To use a plugin, all a user has to do is install it in the Capfile, like this:
+#
+#   # Capfile
+#   require 'capistrano/superfancy'
+#   install_plugin Capistrano::Superfancy
+#
+# Or, to install the plugin without its hooks:
+#
+#   # Capfile
+#   require 'capistrano/superfancy'
+#   install_plugin Capistrano::Superfancy, load_hooks: false
+#
+class Capistrano::Plugin < Rake::TaskLib
+  include Capistrano::DSL
     
-      # Use this method to generate standard caveats.
-  def standard_instructions(home_name, home_value = libexec)
-    <<-EOS.undent
-      Before you can use these tools you must export some variables to your $SHELL.
+      deploy_rb = File.expand_path('../../templates/deploy.rb.erb', __FILE__)
+  stage_rb = File.expand_path('../../templates/stage.rb.erb', __FILE__)
+  capfile = File.expand_path('../../templates/Capfile', __FILE__)
     
-                  # Check if data is actually ready on this IO device.
-              # We have to do this since `readpartial` will actually block
-              # until data is available, which can cause blocking forever
-              # in some cases.
-              results = ::IO.select([io], nil, nil, 1.0)
-              break if !results || results[0].empty?
-    
-              # Repackage the box
-          output_name = @env.vagrantfile.config.package.name || 'package.box'
-          output_path = Pathname.new(File.expand_path(output_name, FileUtils.pwd))
-          box.repackage(output_path)
-    
-    module VagrantPlugins
-  module CommandCap
-    class Command < Vagrant.plugin('2', :command)
-      def self.synopsis
-        'checks and executes capability'
-      end
-    
-      def test_time_time()
-    assert_equal(Time.utc(2000, 3, 21, 3, 30)  \
-                -Time.utc(2000, 3, 21, 0, 30), 3*3600)
-    assert_equal(Time.utc(2000, 3, 21, 0, 30)  \
-                -Time.utc(2000, 3, 21, 3, 30), -3*3600)
-  end
-    
-      it 'decodes the number of characters specified by the count modifier' do
-    [ ['\xc2\x80\xc2\x81\xc2\x82\xc2\x83', 'U1', [0x80]],
-      ['\xc2\x80\xc2\x81\xc2\x82\xc2\x83', 'U2', [0x80, 0x81]],
-      ['\xc2\x80\xc2\x81\xc2\x82\xc2\x83', 'U3', [0x80, 0x81, 0x82]]
-    ].should be_computed_by(:unpack)
-  end
-    
-        10.times { sleep 0.1 if t.status and t.status != 'sleep' }
-    after_sleep1.should == false # t should be blocked on the first sleep
-    t.send(@method)
-    
-        def define_remote_file_task(task, target_roles)
-      Capistrano::UploadTask.define_task(task) do |t|
-        prerequisite_file = t.prerequisites.first
-        file = shared_path.join(t.name)
-    
-    When /^(?:|I )check '([^']*)'$/ do |field|
-  check(field)
-end
-    
-        def self.names_for(klass)
-      instance.names_for(klass)
+      it 'enables printing all config variables on command line parameter' do
+    capture_io do
+      flags '--print-config-variables', '-p'
     end
-    
-        # True if the dimensions represent a horizontal rectangle
-    def horizontal?
-      height < width
-    end
-    
-          def validate_before_processing(validator_class, options)
-        options = options.dup
-        attributes = options.delete(:attributes)
-        attributes.each do |attribute|
-          options[:attributes] = [attribute]
-          create_validating_before_filter(attribute, validator_class, options)
-        end
-      end
+    expect(Capistrano::Configuration.fetch(:print_config_variables)).to be true
+  end
