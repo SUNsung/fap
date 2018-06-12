535d5429478e0df57453f34ee028a2caf2045168
SUNsung <weatherius@gmail.com>
2018-06-12 15:02:18 +0300

    Rewrite [08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py] File in: 2018-06-12 15:02:18

--- pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
+++ pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
@@ -1,213 +1,129 @@
 
         
         
-if len(sys.argv) <= 1:
-    print('Specify the version number as parameter')
-    sys.exit()
-version = sys.argv[1]
+PACKAGES = [
+    'httpie',
+    'requests',
+    'pygments',
+]
     
-    import io
-import optparse
+        def __init__(self, **kwargs):
+        '''
+        Use keyword arguments to overwrite
+        any of the class attributes for this instance.
     
-        # Are checkable fields missing from the test case definition?
-    test_info_dict = dict((key, value if not isinstance(value, compat_str) or len(value) < 250 else 'md5:' + md5(value))
-                          for key, value in got_dict.items()
-                          if value and key in ('id', 'title', 'description', 'uploader', 'upload_date', 'timestamp', 'uploader_id', 'location', 'age_limit'))
-    missing_keys = set(test_info_dict.keys()) - set(expected_dict.keys())
-    if missing_keys:
-        def _repr(v):
-            if isinstance(v, compat_str):
-                return ''%s'' % v.replace('\\', '\\\\').replace(''', '\\'').replace('\n', '\\n')
-            else:
-                return repr(v)
-        info_dict_str = ''
-        if len(missing_keys) != len(expected_dict):
-            info_dict_str += ''.join(
-                '    %s: %s,\n' % (_repr(k), _repr(v))
-                for k, v in test_info_dict.items() if k not in missing_keys)
-    
-            self.port = random.randint(20000, 30000)
-        self.server_process = subprocess.Popen([
-            'srelay', '-f', '-i', '127.0.0.1:%d' % self.port],
-            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
-            info_dict = self._extract_from_id(mvp_id, webpage)
-        info_dict['display_id'] = display_id
+        def get_converter(self, mime):
+        if is_valid_mime(mime):
+            for converter_class in plugin_manager.get_converters():
+                if converter_class.supports(mime):
+                    return converter_class(mime)
     
     
-class CloserToTruthIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?closertotruth\.com/(?:[^/]+/)*(?P<id>[^/?#&]+)'
-    _TESTS = [{
-        'url': 'http://closertotruth.com/series/solutions-the-mind-body-problem#video-3688',
-        'info_dict': {
-            'id': '0_zof1ktre',
-            'display_id': 'solutions-the-mind-body-problem',
-            'ext': 'mov',
-            'title': 'Solutions to the Mind-Body Problem?',
-            'upload_date': '20140221',
-            'timestamp': 1392956007,
-            'uploader_id': 'CTTXML'
-        },
-        'params': {
-            'skip_download': True,
-        },
-    }, {
-        'url': 'http://closertotruth.com/episodes/how-do-brains-work',
-        'info_dict': {
-            'id': '0_iuxai6g6',
-            'display_id': 'how-do-brains-work',
-            'ext': 'mov',
-            'title': 'How do Brains Work?',
-            'upload_date': '20140221',
-            'timestamp': 1392956024,
-            'uploader_id': 'CTTXML'
-        },
-        'params': {
-            'skip_download': True,
-        },
-    }, {
-        'url': 'http://closertotruth.com/interviews/1725',
-        'info_dict': {
-            'id': '1725',
-            'title': 'AyaFr-002',
-        },
-        'playlist_mincount': 2,
-    }]
+def test_follow_redirect_output_options(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=h',
+             '--history-print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 2
+    assert 'HTTP/1.1 302 FOUND' not in r
+    assert HTTP_OK in r
     
-        def to_python(self, value):
-        key = next(iter(value))
-        return {key[:-2]: value[key]}
+        '''.format(
+        default=DEFAULT_STYLE,
+        available='\n'.join(
+            '{0}{1}'.format(8 * ' ', line.strip())
+            for line in wrap(', '.join(sorted(AVAILABLE_STYLES)), 60)
+        ).rstrip(),
+    )
+)
     
-            app.config['SESSION_COOKIE_DOMAIN'] = rv
-        return rv
+        def _getpass(self, prompt):
+        # To allow mocking.
+        return getpass.getpass(str(prompt))
     
-        def dispatch_request(self, *args, **kwargs):
-        meth = getattr(self, request.method.lower(), None)
+        def unregister(self, plugin):
+        self._plugins.remove(plugin)
     
+        requests_session.cookies = session.cookies
     
-def test_session_lifetime():
-    app = flask.Flask(__name__)
-    app.config['PERMANENT_SESSION_LIFETIME'] = 42
-    assert app.permanent_session_lifetime.seconds == 42
+    import os
     
-    import flask
-from flask.sessions import SessionInterface
+        indent = None
+    if format:
+        indent = 4
     
+            # test exceptions
+        no_projects_input_url = 'https://www.googleapis.com/compute/v1/not-projects/myproject/global/backendServices/mybackendservice/getHealth'
+        no_resource_input_url = 'https://www.googleapis.com/compute/v1/not-projects/myproject/global'
+    
+        def on_open_shell(self):
+        try:
+            self._exec_cli_command(b'environment no more')
+        except AnsibleConnectionFailure:
+            raise AnsibleConnectionFailure('unable to set terminal parameters')
+
+    
+    @mainpage
+    
+    EOF = -1
+    
+    	html = get_html(url)
+	contentid = r1(r'<meta name='contentid' scheme='DMINSTR2' content='([^']+)' />', html)
+	vid = r1(r''demand_ehow_videoid':'([^']+)'', html)
+	assert vid
+    
+    __all__ = ['fc2video_download']
+    
+    def mixcloud_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html(url, faker=True)
+    title = r1(r'<meta property='og:title' content='([^']*)'', html)
+    preview_url = r1(r'm-preview=\'([^\']+)\'', html)
+    preview = r1(r'previews(.*)\.mp3$', preview_url)
+    
+        url = sorted(
+        map(lambda x: x.firstChild.nodeValue, xml.getElementsByTagName('src')),
+        key=lambda x: int(match1(x, r'_(\d+?)_')))[-1]
+    
+        #title
+    title = ''
+    profile_api = 'https://www.showroom-live.com/api/room/profile?room_id={room_id}'.format(room_id = room_id)
+    html = loads(get_content(profile_api))
     try:
-    import blinker
-except ImportError:
-    blinker = None
-    
-            self.tail.appendleft(delta)
-        self.lasttime = now
-        self.concurrent += 1
-    
-        def __enter__(self):
-        from scrapy.utils.test import get_testenv
-        pargs = [sys.executable, '-u', '-m', 'scrapy.utils.benchserver']
-        self.proc = subprocess.Popen(pargs, stdout=subprocess.PIPE,
-                                     env=get_testenv())
-        self.proc.stdout.readline()
-    
-    __all__ = ['cbs_download']
-    
-    	type, ext, size = url_info(url)
-	print_info(site_info, title, type, size)
-	
-	if not info_only:
-		download_urls([url], title, ext, size, output_dir, merge = merge)
-    
-            if s: self.streams['video'] = {'url': s }
-        if mp3: self.streams['audio'] = { 'url': mp3 }
-        if pdf: self.streams['slides'] = { 'url': pdf }
-    
-    def kugou_download_by_hash(title,hash_val,output_dir = '.', merge = True, info_only = False):
-    #sample
-    #url_sample:http://www.kugou.com/yy/album/single/536957.html
-    #hash ->key  md5(hash+kgcloud')->key  decompile swf
-    #cmd 4 for mp3 cmd 3 for m4a
-    key=hashlib.new('md5',(hash_val+'kgcloud').encode('utf-8')).hexdigest()
-    html=get_html('http://trackercdn.kugou.com/i/?pid=6&key=%s&acceptMp3=1&cmd=4&hash=%s'%(key,hash_val))
-    j=loads(html)
-    url=j['url']
-    songtype, ext, size = url_info(url)
-    print_info(site_info, title, songtype, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge=merge)
-    
-    def kuwo_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    if 'www.kuwo.cn/yinyue' in url:
-        rid=match1(url,'yinyue/(\d+)')
-        kuwo_download_by_rid(rid,output_dir, merge, info_only)
-    else:
-        kuwo_playlist_download(url,output_dir,merge,info_only)
+        title = html['main_name']
+    except KeyError:
+        title = 'Showroom_{room_id}'.format(room_id = room_id)
     
     
-#----------------------------------------------------------------------
-def showroom_download(url, output_dir = '.', merge = False, info_only = False, **kwargs):
-    ''''''
-    if re.match( r'(\w+)://www.showroom-live.com/([-\w]+)', url):
-        room_url_key = match1(url, r'\w+://www.showroom-live.com/([-\w]+)')
-        room_id = showroom_get_roomid_by_room_url_key(room_url_key)
-        showroom_download_by_room_id(room_id, output_dir, merge,
-                                    info_only)
-    
-    
-def main():
-    tornado.options.parse_command_line()
-    application = tornado.web.Application([
-        (r'/', MainHandler),
-    ])
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(options.port)
-    tornado.ioloop.IOLoop.current().start()
-    
-            add_accept_handler(listener, accept_callback)
-        self.client_stream = IOStream(socket.socket())
-        self.addCleanup(self.client_stream.close)
-        yield [self.client_stream.connect(('127.0.0.1', port)),
-               event.wait()]
-        self.io_loop.remove_handler(listener)
-        listener.close()
-    
-          wait_iterator = gen.WaitIterator(future1, future2)
-      while not wait_iterator.done():
-          try:
-              result = yield wait_iterator.next()
-          except Exception as e:
-              print('Error {} from {}'.format(e, wait_iterator.current_future))
-          else:
-              print('Result {} received from {} at {}'.format(
-                  result, wait_iterator.current_future,
-                  wait_iterator.current_index))
-    
-        def __str__(self):
-        return 'HTTP %d: %s' % (self.code, self.message)
-    
-            Address may be either an IP address or hostname.  If it's a hostname,
-        the server will listen on all IP addresses associated with the
-        name.  Address may be an empty string or None to listen on all
-        available interfaces.  Family may be set to either `socket.AF_INET`
-        or `socket.AF_INET6` to restrict to IPv4 or IPv6 addresses, otherwise
-        both will be used if available.
-    
-        The unittest framework is synchronous, so the test must be
-    complete by the time the test method returns. This means that
-    asynchronous code cannot be used in quite the same way as usual
-    and must be adapted to fit. To write your tests with coroutines,
-    decorate your test methods with `tornado.testing.gen_test` instead
-    of `tornado.gen.coroutine`.
-    
-        .. versionchanged:: 5.0
-       The ``io_loop`` argument (deprecated since version 4.1) has been removed.
+def save_debug_screenshot(ts, im, piece_x, piece_y, board_x, board_y):
     '''
-    def initialize(self):
-        # partial copy of twisted.names.client.createResolver, which doesn't
-        # allow for a reactor to be passed in.
-        self.reactor = tornado.platform.twisted.TornadoReactor()
+    对 debug 图片加上详细的注释
     
-    * If you're writing an asynchronous library that doesn't rely on a
-  stack_context-aware library like `tornado.ioloop` or `tornado.iostream`
-  (for example, if you're writing a thread pool), use
-  `.stack_context.wrap()` before any asynchronous operations to capture the
-  stack context from where the operation was started.
+    '''
+    make_debug_dir(screenshot_backup_dir)
+    draw = ImageDraw.Draw(im)
+    draw.line((piece_x, piece_y) + (board_x, board_y), fill=2, width=3)
+    draw.line((piece_x, 0, piece_x, im.size[1]), fill=(255, 0, 0))
+    draw.line((0, piece_y, im.size[0], piece_y), fill=(255, 0, 0))
+    draw.line((board_x, 0, board_x, im.size[1]), fill=(0, 0, 255))
+    draw.line((0, board_y, im.size[0], board_y), fill=(0, 0, 255))
+    draw.ellipse((piece_x - 10, piece_y - 10, piece_x + 10, piece_y + 10), fill=(255, 0, 0))
+    draw.ellipse((board_x - 10, board_y - 10, board_x + 10, board_y + 10), fill=(0, 0, 255))
+    del draw
+    im.save(os.path.join(os.getcwd(), screenshot_backup_dir,
+                         '#' + str(ts) + '.png'))
+    
+    
+def jump(distance, delta_piece_y):
+    '''
+    跳跃一定的距离
+    '''
+    # 计算程序长度与截图测得的距离的比例
+    scale = 0.945 * 2 / head_diameter
+    actual_distance = distance * scale * (math.sqrt(6) / 2)
+    press_time = (-945 + math.sqrt(945 ** 2 + 4 * 105 *
+                                   36 * actual_distance)) / (2 * 105) * 1000
+    press_time *= press_coefficient
+    press_time = max(press_time, 200)  # 设置 200ms 是最小的按压时间
+    press_time = int(press_time)
