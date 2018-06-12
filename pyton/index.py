cab2053706b865fa731fb42659ac2f0ec2ee4e71
SUNsung <weatherius@gmail.com>
2018-06-12 20:46:21 +0300

    Rewrite [index.py] File in: 2018-06-12 20:46:20

--- pyton/index.py
+++ pyton/index.py
@@ -1,47 +1,113 @@
 
         
-        PROJ_METADATA = '%s.json' % PROJ_NAME
+            # Replacing the non-sorted libraries by the sorted ones and gathering all at the final_README file
+    blocks[0] = inner_blocks
+    final_README = table_of_contents + '- - -' + ''.join(blocks)
     
-            if args:
-            if 'gui' in conf and conf['gui']:
-                # Enter GUI mode.
-                from .gui import gui_main
-                gui_main(*args, **conf)
-            else:
-                # Enter console mode.
-                from .console import console_main
-                console_main(*args, **conf)
-    
-    #----------------------------------------------------------------------
-def fc2video_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    '''wrapper'''
-    #'http://video.fc2.com/en/content/20151021bTVKnbEw'
-    #'http://xiaojiadianvideo.asia/content/20151021bTVKnbEw'
-    #'http://video.fc2.com/ja/content/20151021bTVKnbEw'
-    #'http://video.fc2.com/tw/content/20151021bTVKnbEw'
-    hostname = urlparse(url).hostname
-    if not ('fc2.com' in hostname or 'xiaojiadianvideo.asia' in hostname):
-        return False
-    upid = match1(url, r'.+/content/(\w+)')
-    
-    def mixcloud_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html(url, faker=True)
-    title = r1(r'<meta property='og:title' content='([^']*)'', html)
-    preview_url = r1(r'm-preview=\'([^\']+)\'', html)
-    preview = r1(r'previews(.*)\.mp3$', preview_url)
-    
-    from ..common import *
-    
-    site = MusicPlayOn()
-download = site.download_by_url
-# TBD: implement download_playlist
+        try:
+        return json.dumps(result, sort_keys=True, indent=indent, ensure_ascii=False)
+    except UnicodeDecodeError:
+        return json.dumps(result, sort_keys=True, indent=indent)
 
     
-        def prepare(self, **kwargs):
-        # scrape the html
-        content = get_content(self.url)
+            # no location in URL
+        input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/targetHttpProxies/mytargetproxy/setUrlMap'
+        actual = GCPUtils.parse_gcp_url(input_url)
+        self.assertEquals('compute', actual['service'])
+        self.assertEquals('v1', actual['api_version'])
+        self.assertEquals('myproject', actual['project'])
+        self.assertFalse('global' in actual)
+        self.assertEquals('targetHttpProxies', actual['resource_name'])
+        self.assertEquals('mytargetproxy', actual['entity_name'])
+        self.assertEquals('setUrlMap', actual['method_name'])
     
-        hass.services.register(DOMAIN, SERVICE_VOLUME_MUTE,
-                           lambda service:
-                           keyboard.tap_key(keyboard.volume_mute_key),
-                           schema=TAP_KEY_SCHEMA)
+        READY = 0
+    IN_PROGRESS = 1
+    COMPLETE = 2
+    
+        def insert_crawled_link(self, url, signature):
+        '''Add the given link to `crawled_links`.'''
+        pass
+    
+        def extract_url(self, line):
+        '''Extract the generated url from the log line.'''
+        pass
+    
+    
+class Level(object):
+    
+        def test_mixed_compares(self):
+        eq = self.assertEqual
+    
+    
+def open_text(package: Package,
+              resource: Resource,
+              encoding: str = 'utf-8',
+              errors: str = 'strict') -> TextIO:
+    '''Return a file-like object opened for text reading of the resource.'''
+    resource = _normalize_path(resource)
+    package = _get_package(package)
+    reader = _get_resource_reader(package)
+    if reader is not None:
+        return TextIOWrapper(reader.open_resource(resource), encoding, errors)
+    _check_location(package)
+    absolute_package_path = os.path.abspath(package.__spec__.origin)
+    package_path = os.path.dirname(absolute_package_path)
+    full_path = os.path.join(package_path, resource)
+    try:
+        return open(full_path, mode='r', encoding=encoding, errors=errors)
+    except OSError:
+        # Just assume the loader is a resource loader; all the relevant
+        # importlib.machinery loaders are and an AttributeError for
+        # get_data() will make it clear what is needed from the loader.
+        loader = cast(ResourceLoader, package.__spec__.loader)
+        data = None
+        if hasattr(package.__spec__.loader, 'get_data'):
+            with suppress(OSError):
+                data = loader.get_data(full_path)
+        if data is None:
+            package_name = package.__spec__.name
+            message = '{!r} resource not found in {!r}'.format(
+                resource, package_name)
+            raise FileNotFoundError(message)
+        else:
+            return TextIOWrapper(BytesIO(data), encoding, errors)
+    
+    
+class InheritanceTest(unittest.TestCase):
+    @unittest.skipUnless(hasattr(socket, 'SOCK_CLOEXEC'),
+                         'SOCK_CLOEXEC not defined')
+    @support.requires_linux_version(2, 6, 28)
+    def test_SOCK_CLOEXEC(self):
+        with socket.socket(socket.AF_INET,
+                           socket.SOCK_STREAM | socket.SOCK_CLOEXEC) as s:
+            self.assertEqual(s.type, socket.SOCK_STREAM)
+            self.assertFalse(s.get_inheritable())
+    
+        def _test_home(self, p):
+        q = self.cls(os.path.expanduser('~'))
+        self.assertEqual(p, q)
+        self.assertEqual(str(p), str(q))
+        self.assertIs(type(p), type(q))
+        self.assertTrue(p.is_absolute())
+    
+        result = False
+    try:
+        ret = urlopen(url, timeout=2)
+        result = (ret.code == 200)
+    except HTTPError as e:
+        print(e, file=sys.stderr)
+    except URLError as e:
+        print(e, file=sys.stderr)
+    except timeout as e:
+        print(e, file=sys.stderr)
+    except Exception as e:
+        print(e, file=sys.stderr)
+    
+        #Removing unnecessary intermediate files
+    try:
+        os.remove('htmlout.html')
+        os.remove('title.html')
+        os.remove('dir_title.html')
+    except OSError:
+        print('Error removing temporary file(s)')
