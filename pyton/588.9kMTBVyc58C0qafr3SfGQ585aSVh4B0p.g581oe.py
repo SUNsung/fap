fed7c5a075bb4cdeabc9845c919ffce6afbbdca5
SUNsung <weatherius@gmail.com>
2018-06-13 03:57:39 +0300

    Rewrite [588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py] File in: 2018-06-13 03:57:39

--- pyton/588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py
+++ pyton/588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py
@@ -1,20 +1,34 @@
 
         
-            This could be extended by having nested blocks, sorting them recursively
-    and flattening the end structure into a list of lines. Revision 2 maybe ^.^.
+          # Locally-normalize and optionally clip the scores.
+  max_scores_bxmx1 = tf.reshape(max_scores_bm, [batch_size, max_nodes, 1])
+  scores_bxmxm -= max_scores_bxmx1
+  if max_dynamic_range is not None:
+    # After normalization, the scores are non-positive with max=0, so the
+    # |max_dynamic_range| can be applied directly.
+    #
+    # PyLint thinks '-max_dynamic_range' is invalid because it defaults to None.
+    
+      _add_hook_node(tf.transpose(parameter), _hook_name('/transposed'))
+  for blocked, blocked_suffix in _blocked_and_dtype_transformations(parameter):
+    _add_hook_node(blocked, _hook_name('/matrix' + blocked_suffix))
+    
+      def testLargerBfloat16Permutation(self):
+    with self.test_session() as session:
+      matrix = tf.random_uniform((3, 4, 32))
+      permuted = runtime_support.bfloat16_permutation(matrix)
+      matrix, actual_permuted = session.run([matrix, permuted])
+    
+      def testReadAndProjectivize(self):
+    reader = sentence_io.ConllSentenceReader(
+        self.filepath, self.batch_size, projectivize=True)
+    self.assertParseable(reader, self.batch_size, False)
+    self.assertParseable(reader, self.batch_size, False)
+    self.assertParseable(reader, 14, True)
+    self.assertParseable(reader, 0, True)
+    self.assertParseable(reader, 0, True)
+    
+    This script loads and finishes a CoNLL checkpoint, then exports it as a
+SavedModel. It expects that the CoNLL RNN cells have been updated using the
+RNN update script.
 '''
-    
-    # Test data.
-filegroup(
-    name = 'testdata',
-    data = glob(['testdata/**']),
-)
-    
-        # Add runtime hooks for pre-computed weights.
-    self._derived_params.append(self._get_root_weights)
-    self._derived_params.append(self._get_root_bias)
-    
-    from ansible.utils.vars import combine_vars
-    
-            r = random.Random(seed)
-        expected = [r.randint(0, 2**i) for i in range(0, retries)]
