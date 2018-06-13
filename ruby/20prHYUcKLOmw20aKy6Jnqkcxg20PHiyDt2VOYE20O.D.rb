66253cb5bc6e948028733e78c0f4bbc470663822
SUNsung <weatherius@gmail.com>
2018-06-13 19:38:21 +0300

    Rewrite [20prHYUcKLOmw20aKy6Jnqkcxg20PHiyDt2VOYE20O.D.rb] File in: 2018-06-13 19:38:20

--- ruby/20prHYUcKLOmw20aKy6Jnqkcxg20PHiyDt2VOYE20O.D.rb
+++ ruby/20prHYUcKLOmw20aKy6Jnqkcxg20PHiyDt2VOYE20O.D.rb
@@ -1,69 +1,86 @@
 
         
-              def handle_exception_with_mailer_class(exception)
-        if klass = mailer_class
-          klass.handle_exception exception
-        else
-          raise exception
-        end
+            def create_event(event)
+      return super unless dry_run?
+      if can_create_events?
+        event = build_event(event)
+        @dry_run_results[:events] << event.payload
+        event
+      else
+        error 'This Agent cannot create events!'
       end
+    end
   end
 end
 
     
-          # Adds a new delivery method through the given class using the given
-      # symbol as alias and the default options supplied.
-      #
-      #   add_delivery_method :sendmail, Mail::Sendmail,
-      #     location:  '/usr/sbin/sendmail',
-      #     arguments: '-i'
-      def add_delivery_method(symbol, klass, default_options = {})
-        class_attribute(:'#{symbol}_settings') unless respond_to?(:'#{symbol}_settings')
-        send(:'#{symbol}_settings=', default_options)
-        self.delivery_methods = delivery_methods.merge(symbol.to_sym => klass).freeze
-      end
-    
-      def setup
-    super
-    ActionMailer::LogSubscriber.attach_to :action_mailer
+      def evernote_consumer_key
+    (config = Devise.omniauth_configs[:evernote]) && config.strategy.consumer_key
   end
     
-        if params[:sort].present?
-      attribute, direction = params[:sort].downcase.split('.')
-      unless valid_sorts.include?(attribute)
-        attribute, direction = default.to_a.first
-      end
-    else
-      attribute, direction = default.to_a.first
+      def set_table_sort(sort_options)
+    valid_sorts = sort_options[:sorts] or raise ArgumentError.new('You must specify :sorts as an array of valid sort attributes.')
+    default = sort_options[:default] || { valid_sorts.first.to_sym => :desc }
+    
+        respond_to do |format|
+      format.html { redirect_back events_path, notice: 'Event deleted.' }
+      format.json { head :no_content }
     end
+  end
     
-      def toggle_availability
-    @service = current_user.services.find(params[:id])
-    @service.toggle_availability!
+      def index
+    set_table_sort sorts: %w[provider name global], default: { provider: :asc }
     
-          assert_equal(true, set >= klass[1,2,3], klass.name)
-      assert_equal(true, set >= klass[1,2], klass.name)
+            when :err
+          case s[:last]
+            when :pass
+              # Oops got a -ERR after a pass so its crap ignore the pass
+              # But report it, might be helpfull for guessing :-)
     
-            # Removes the specified cache
-        #
-        # @param [Array<Hash>] cache_descriptors
-        #        An array of caches to remove, each specified with the same
-        #        hash as cache_descriptors_per_pod especially :spec_file and :slug
-        #
-        def remove_caches(cache_descriptors)
-          cache_descriptors.each do |desc|
-            UI.message('Removing spec #{desc[:spec_file]} (v#{desc[:version]})') do
-              FileUtils.rm(desc[:spec_file])
-            end
-            UI.message('Removing cache #{desc[:slug]}') do
-              FileUtils.rm_rf(desc[:slug])
-            end
+          # There is only one pattern per run to test
+      matched = nil
+      matches = nil
+    
+    meterp.core.use('Stdapi')
+    
+            def initialize(name, declaration_node, scope)
+          unless VARIABLE_DECLARATION_TYPES.include?(declaration_node.type)
+            raise ArgumentError,
+                  'Node type must be any of #{VARIABLE_DECLARATION_TYPES}, ' \
+                  'passed #{declaration_node.type}'
+          end
+    
+                previous.push(when_node.conditions)
           end
         end
     
-          def run
-        update_if_necessary!
+              optarg_positions.each do |optarg_position|
+            # there can only be one group of optional arguments
+            break if optarg_position > arg_positions.max
     
-      puts '\n== Removing old logs and tempfiles =='
-  system 'rm -f log/*'
-  system 'rm -rf tmp/cache'
+              annotated_source.each_line do |source_line|
+            if source_line =~ ANNOTATION_PATTERN
+              annotations << [source.size, source_line]
+            else
+              source << source_line
+            end
+          end
+    
+              def plugins
+            @plugins ||= find_plugins_gem_specs.map do |spec|
+              { :name => spec.name, :version => spec.version.to_s }
+            end.sort_by do |spec|
+              spec[:name]
+            end
+          end
+    
+      def _nt_selector
+    start_index = index
+    if node_cache[:selector].has_key?(index)
+      cached = node_cache[:selector][index]
+      if cached
+        cached = SyntaxNode.new(input, index...(index + 1)) if cached == true
+        @index = cached.interval.end
+      end
+      return cached
+    end
