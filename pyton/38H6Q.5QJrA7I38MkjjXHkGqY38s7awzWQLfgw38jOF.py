14bd720cc5d65ced1bfa47d7c1373e7c928fe011
SUNsung <weatherius@gmail.com>
2018-06-14 19:47:49 +0300

    Rewrite [38H6Q.5QJrA7I38MkjjXHkGqY38s7awzWQLfgw38jOF.py] File in: 2018-06-14 19:47:48

--- pyton/38H6Q.5QJrA7I38MkjjXHkGqY38s7awzWQLfgw38jOF.py
+++ pyton/38H6Q.5QJrA7I38MkjjXHkGqY38s7awzWQLfgw38jOF.py
@@ -1,180 +1,273 @@
 
         
-            def test_youtube_extract(self):
-        assertExtractId = lambda url, id: self.assertEqual(YoutubeIE.extract_id(url), id)
-        assertExtractId('http://www.youtube.com/watch?&v=BaW_jenozKc', 'BaW_jenozKc')
-        assertExtractId('https://www.youtube.com/watch?&v=BaW_jenozKc', 'BaW_jenozKc')
-        assertExtractId('https://www.youtube.com/watch?feature=player_embedded&v=BaW_jenozKc', 'BaW_jenozKc')
-        assertExtractId('https://www.youtube.com/watch_popup?v=BaW_jenozKc', 'BaW_jenozKc')
-        assertExtractId('http://www.youtube.com/watch?v=BaW_jenozKcsharePLED17F32AD9753930', 'BaW_jenozKc')
-        assertExtractId('BaW_jenozKc', 'BaW_jenozKc')
+        
+def main(unused_argv):
+  # Left-to-right, character-based LSTM.
+  char2word = spec_builder.ComponentSpecBuilder('char_lstm')
+  char2word.set_network_unit(
+      name='wrapped_units.LayerNormBasicLSTMNetwork',
+      hidden_layer_sizes='256')
+  char2word.set_transition_system(name='char-shift-only', left_to_right='true')
+  char2word.add_fixed_feature(name='chars', fml='char-input.text-char',
+                              embedding_dim=16)
     
-        def test_func(self):
-        as_file = os.path.join(TEST_DIR, testfile)
-        swf_file = os.path.join(TEST_DIR, test_id + '.swf')
-        if ((not os.path.exists(swf_file)) or
-                os.path.getmtime(swf_file) < os.path.getmtime(as_file)):
-            # Recompile
-            try:
-                subprocess.check_call([
-                    'mxmlc', '-output', swf_file,
-                    '-static-link-runtime-shared-libraries', as_file])
-            except OSError as ose:
-                if ose.errno == errno.ENOENT:
-                    print('mxmlc not found! Skipping test.')
-                    return
-                raise
+    tf_proto_library_cc(
+    name = 'spec_proto',
+    srcs = ['spec.proto'],
+)
+    
+    cc_binary(
+    name = 'mst_cc_impl.so',
+    linkopts = select({
+        '//conditions:default': ['-lm'],
+        '@org_tensorflow//tensorflow:darwin': [],
+    }),
+    linkshared = 1,
+    linkstatic = 1,
+    deps = ['//dragnn/mst:mst_ops_cc'],
+)
+    
+      Args:
+    comp: Component whose fixed features we wish to look up.
+    state: live MasterState object for the component.
+    stride: Tensor containing current batch * beam size.
+    during_training: True if this is being called from a training code path.
+      This controls, e.g., the use of feature ID dropout.
+    
+          saved_model_builder.save()
+
+    
+    from syntaxnet import parser_trainer
+from syntaxnet import task_spec_pb2
+from syntaxnet import test_flags
+    
+    '''
+import hashlib
+import requests
+    
+            super(EncodedStream, self).__init__(**kwargs)
+    
+      1. Read, validate and process the input (args, `stdin`).
+  2. Create and send a request.
+  3. Stream, and possibly process and format, the parts
+     of the request-response exchange selected by output options.
+  4. Simultaneously write to `stdout`
+  5. Exit.
+    
+        name = 'Basic HTTP auth'
+    auth_type = 'basic'
     
     
-@pytest.fixture(params=containers)
-def proc(request, spawnu, TIMEOUT):
-    proc = spawnu(*request.param)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(u'tcsh')
-    proc.sendline(u'setenv PYTHONIOENCODING utf8')
-    proc.sendline(u'eval `thefuck --alias`')
-    return proc
+# Various separators used in args
+SEP_HEADERS = ':'
+SEP_HEADERS_EMPTY = ';'
+SEP_CREDENTIALS = ':'
+SEP_PROXY = ':'
+SEP_DATA = '='
+SEP_DATA_RAW_JSON = ':='
+SEP_FILES = '@'
+SEP_DATA_EMBED_FILE = '=@'
+SEP_DATA_EMBED_RAW_JSON_FILE = ':=@'
+SEP_QUERY = '=='
     
-        def test_make_entry(self):
-        equal = self.assertEqual
-        self.dialog.row = 0
-        self.dialog.top = self.root
-        entry, label = self.dialog.make_entry('Test:', 'hello')
-        equal(label['text'], 'Test:')
     
-        @classmethod
-    def zonelist(cls, zonedir='/usr/share/zoneinfo'):
-        zones = []
-        for root, _, files in os.walk(zonedir):
-            for f in files:
-                p = os.path.join(root, f)
-                with open(p, 'rb') as o:
-                    magic =  o.read(4)
-                if magic == b'TZif':
-                    zones.append(p[len(zonedir) + 1:])
-        return zones
+def test_basic_auth(httpbin_both):
+    r = http('--auth=user:password',
+             'GET', httpbin_both + '/basic-auth/user/password')
+    assert HTTP_OK in r
+    assert r.json == {'authenticated': True, 'user': 'user'}
     
-            # RFC 4642 2.2.2: Both the client and the server MUST know if there is
-        # a TLS session active.  A client MUST NOT attempt to start a TLS
-        # session if a TLS session is already active.
-        self.tls_on = False
+        stream_types = [  #this is just a sample. Will make it in prepare()
+        # {'id': '1080'},
+        # {'id': '720'},
+        # {'id': '360'},
+        # {'id': '288'},
+        # {'id': '190'},
+        # {'id': '180'},
+        
+    ]
     
-    from functools import partial
-import unittest
-from test import support
-from textwrap import dedent
-import os
-import re
+    #----------------------------------------------------------------------
+def fc2video_download_by_upid(upid, output_dir = '.', merge = True, info_only = False, **kwargs):
+    ''''''
+    fake_headers = {
+        'DNT': '1',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'en-CA,en;q=0.8,en-US;q=0.6,zh-CN;q=0.4,zh;q=0.2',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.58 Safari/537.36',
+        'Accept': '*/*',
+        'X-Requested-With': 'ShockwaveFlash/19.0.0.245',
+        'Connection': 'keep-alive',
+    }
+    api_base = 'http://video.fc2.com/ginfo.php?upid={upid}&mimi={mimi}'.format(upid = upid, mimi = makeMimi(upid))
+    html = get_content(api_base, headers=fake_headers)
     
-        def test_unregister_after_socket_close(self):
-        s = self.SELECTOR()
-        self.addCleanup(s.close)
-        rd, wr = self.make_socketpair()
-        s.register(rd, selectors.EVENT_READ)
-        s.register(wr, selectors.EVENT_WRITE)
-        rd.close()
-        wr.close()
-        s.unregister(rd)
-        s.unregister(wr)
+    def mixcloud_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html(url, faker=True)
+    title = r1(r'<meta property='og:title' content='([^']*)'', html)
+    preview_url = r1(r'm-preview=\'([^\']+)\'', html)
+    preview = r1(r'previews(.*)\.mp3$', preview_url)
     
-        def test_libc_ver(self):
-        import os
-        if os.path.isdir(sys.executable) and \
-           os.path.exists(sys.executable+'.exe'):
-            # Cygwin horror
-            executable = sys.executable + '.exe'
-        else:
-            executable = sys.executable
-        res = platform.libc_ver(executable)
     
-            # Simple renaming
-        elem = doc.renameNode(elem, xml.dom.EMPTY_NAMESPACE, 'a')
-        self.confirm(elem.tagName == 'a'
-                and elem.nodeName == 'a'
-                and elem.localName is None
-                and elem.namespaceURI == xml.dom.EMPTY_NAMESPACE
-                and elem.prefix is None
-                and elem.ownerDocument.isSameNode(doc))
+def mtv81_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_content(url)
+    title = HTMLParser().unescape(
+        '|'.join(match1(html, r'<title>(.*?)</title>').split('|')[:-2]))
     
-        @skip_if_dont_write_bytecode
-    def test_missing_source(self):
-        # With PEP 3147 cache layout, removing the source but leaving the pyc
-        # file does not satisfy the import.
-        __import__(TESTFN)
-        pyc_file = importlib.util.cache_from_source(self.source)
-        self.assertTrue(os.path.exists(pyc_file))
-        os.remove(self.source)
-        forget(TESTFN)
-        importlib.invalidate_caches()
-        self.assertRaises(ImportError, __import__, TESTFN)
+    ATTR_ENERGY_GENERATION = 'energy_generation'
+ATTR_POWER_GENERATION = 'power_generation'
+ATTR_ENERGY_CONSUMPTION = 'energy_consumption'
+ATTR_POWER_CONSUMPTION = 'power_consumption'
+ATTR_EFFICIENCY = 'efficiency'
     
-        top = Toplevel(parent)
-    top.title('Test ColorDelegator')
-    x, y = map(int, parent.geometry().split('+')[1:])
-    top.geometry('700x250+%d+%d' % (x + 20, y + 175))
-    source = (
-        'if True: int ('1') # keyword, builtin, string, comment\n'
-        'elif False: print(0)\n'
-        'else: float(None)\n'
-        'if iF + If + IF: 'keyword matching must respect case'\n'
-        'if'': x or''  # valid string-keyword no-space combinations\n'
-        'async def f(): await g()\n'
-        '# All valid prefixes for unicode and byte strings should be colored.\n'
-        ''x', '''x''', \'x\', \'\'\'x\'\'\'\n'
-        'r'x', u'x', R'x', U'x', f'x', F'x'\n'
-        'fr'x', Fr'x', fR'x', FR'x', rf'x', rF'x', Rf'x', RF'x'\n'
-        'b'x',B'x', br'x',Br'x',bR'x',BR'x', rb'x'.rB'x',Rb'x',RB'x'\n'
-        '# Invalid combinations of legal characters should be half colored.\n'
-        'ur'x', ru'x', uf'x', fu'x', UR'x', ufr'x', rfu'x', xf'x', fx'x'\n'
+        def test_from_event_to_delete_state(self):
+        '''Test converting deleting state event to db state.'''
+        event = ha.Event(EVENT_STATE_CHANGED, {
+            'entity_id': 'sensor.temperature',
+            'old_state': ha.State('sensor.temperature', '18'),
+            'new_state': None,
+        })
+        db_state = States.from_event(event)
+    
+            basename = os.path.splitext(os.path.basename(im_name))[0]
+        txtname = os.path.join(output_dir, basename + 'pred.txt')
+        with open(txtname, 'w') as fid_txt:
+            if i % 10 == 0:
+                logger.info('i: {}: {}'.format(i, basename))
+            for j in range(1, len(all_segms)):
+                clss = json_dataset.classes[j]
+                clss_id = cityscapes_eval.name2label[clss].id
+                segms = all_segms[j][i]
+                boxes = all_boxes[j][i]
+                if segms == []:
+                    continue
+                masks = mask_util.decode(segms)
+    
+        def _remove_layers(net):
+        for i in reversed(range(len(net.layer))):
+            if net.layer[i].type in remove_types:
+                net.layer.pop(i)
+    
+        Arguments:
+        model (DetectionModelHelper): the detection model to use
+        im (ndarray): color image to test (in BGR order)
+        boxes (ndarray): R x 4 array of object proposals in 0-indexed
+            [x1, y1, x2, y2] format, or None if using RPN
+    
+    
+def test_net_on_dataset(
+    weights_file,
+    dataset_name,
+    proposal_file,
+    output_dir,
+    multi_gpu=False,
+    gpu_id=0
+):
+    '''Run inference on a dataset.'''
+    dataset = JsonDataset(dataset_name)
+    test_timer = Timer()
+    test_timer.tic()
+    if multi_gpu:
+        num_images = len(dataset.get_roidb())
+        all_boxes, all_segms, all_keyps = multi_gpu_test_net_on_dataset(
+            weights_file, dataset_name, proposal_file, num_images, output_dir
         )
-    text = Text(top, background='white')
-    text.pack(expand=1, fill='both')
-    text.insert('insert', source)
-    text.focus_set()
-    
-    T_CV2 = typing.ClassVar[int]
-T_CV3 = typing.ClassVar
-    
-        A configurable interface is an (abstract) class whose constructor
-    acts as a factory function for one of its implementation subclasses.
-    The implementation subclass as well as optional keyword arguments to
-    its initializer can be set globally at runtime with `configure`.
-    
-        @gen_test
-    def test_blocking_get_wait(self):
-        q = queues.Queue()
-        q.put(0)
-        self.io_loop.call_later(0.01, q.put, 1)
-        self.io_loop.call_later(0.02, q.put, 2)
-        self.assertEqual(0, (yield q.get(timeout=timedelta(seconds=1))))
-        self.assertEqual(1, (yield q.get(timeout=timedelta(seconds=1))))
-    
-        def test_types(self):
-        headers = {'Cookie': 'foo=bar'}
-        response = self.fetch('/typecheck?foo=bar', headers=headers)
-        data = json_decode(response.body)
-        self.assertEqual(data, {})
-    
-            This allows the request handler to be garbage collected more
-        quickly in CPython by breaking up reference cycles.
-        '''
-        self._write_callback = None
-        self._write_future = None
-        self._close_callback = None
-        if self.stream is not None:
-            self.stream.set_close_callback(None)
+    else:
+        all_boxes, all_segms, all_keyps = test_net(
+            weights_file, dataset_name, proposal_file, output_dir, gpu_id=gpu_id
+        )
+    test_timer.toc()
+    logger.info('Total inference time: {:.3f}s'.format(test_timer.average_time))
+    results = task_evaluation.evaluate_all(
+        dataset, all_boxes, all_segms, all_keyps, output_dir
+    )
+    return results
     
     
-class TwitterClientLoginGenEngineHandler(TwitterClientHandler):
-    with ignore_deprecation():
-        @asynchronous
-        @gen.engine
-        def get(self):
-            if self.get_argument('oauth_token', None):
-                user = yield self.get_authenticated_user()
-                self.finish(user)
-            else:
-                # Old style: with @gen.engine we can ignore the Future from
-                # authorize_redirect.
-                self.authorize_redirect()
+def _do_keypoint_eval(json_dataset, res_file, output_dir):
+    ann_type = 'keypoints'
+    imgIds = json_dataset.COCO.getImgIds()
+    imgIds.sort()
+    coco_dt = json_dataset.COCO.loadRes(res_file)
+    coco_eval = COCOeval(json_dataset.COCO, coco_dt, ann_type)
+    coco_eval.params.imgIds = imgIds
+    coco_eval.evaluate()
+    coco_eval.accumulate()
+    eval_file = os.path.join(output_dir, 'keypoint_results.pkl')
+    save_object(coco_eval, eval_file)
+    logger.info('Wrote json eval results to: {}'.format(eval_file))
+    coco_eval.summarize()
+    return coco_eval
+
+    
+    
+def _empty_box_results():
+    return OrderedDict({
+        'box':
+        OrderedDict(
+            [
+                ('AP', -1),
+                ('AP50', -1),
+                ('AP75', -1),
+                ('APs', -1),
+                ('APm', -1),
+                ('APl', -1),
+            ]
+        )
+    })
+    
+            outputs[0].reshape(rois.shape)
+        outputs[0].data[...] = rois
+        if len(outputs) > 1:
+            outputs[1].reshape(roi_probs.shape)
+            outputs[1].data[...] = roi_probs
+    
+        @require_oauth2_scope('creddits')
+    @validate(
+        VUser(),
+        target=VByName('fullname'),
+    )
+    @api_doc(
+        api_section.gold,
+        uri='/api/v1/gold/gild/{fullname}',
+    )
+    def POST_gild(self, target):
+        if not isinstance(target, (Comment, Link)):
+            err = RedditError('NO_THING_ID')
+            self.on_validation_error(err)
+    
+    class AwardsController(RedditController):
+    
+                if links:
+                kw = {}
+                if wrapper:
+                    links = wrap_links(links, wrapper = wrapper)
+                else:
+                    links = wrap_links(links)
+                links = list(links)
+                links = max(links, key = lambda x: x._score) if links else None
+            if not links and wrapper:
+                return wrapper(None)
+            return links
+            # note: even if _by_url successed or a link was passed in,
+            # it is possible link_listing.things is empty if the
+            # link(s) is/are members of a private reddit
+            # return the link with the highest score (if more than 1)
+        except:
+            #we don't want to return 500s in other people's pages.
+            import traceback
+            g.log.debug('FULLPATH: get_link error in buttons code')
+            g.log.debug(traceback.format_exc())
+            if wrapper:
+                return wrapper(None)
+    
+    
+    def send400(self):
+        if 'usable_error_content' in request.environ:
+            return request.environ['usable_error_content']
+        else:
+            res = pages.RedditError(
+                title=_('bad request (%(domain)s)') % dict(domain=g.domain),
+                message=_('you sent an invalid request'),
+                explanation=request.GET.get('explanation'))
+            return res.render()
