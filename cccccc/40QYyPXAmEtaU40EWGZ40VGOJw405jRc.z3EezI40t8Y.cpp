ee8d4c38eda40ebadbf507931ee044880adc4825
SUNsung <weatherius@gmail.com>
2018-06-15 01:49:24 +0300

    Rewrite [40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp] File in: 2018-06-15 01:49:24

--- cccccc/40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp
+++ cccccc/40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp
@@ -1,34 +1,130 @@
 
         
-        class AbstractAuthResolver : public AuthResolver {
-public:
-  AbstractAuthResolver();
-    }
+          /// DebuggerClient is consulted at two times during name
+  /// lookup.  This is the first time: after all names in a
+  /// source file have been checked but before external
+  /// Modules are checked.  The results in the ResultVector will
+  /// be consulted first.  Return true if results have been added
+  /// to RV.
+  /// FIXME: I don't think this ever does anything useful.
+  virtual bool lookupOverrides(DeclBaseName Name, DeclContext *DC,
+                               SourceLoc Loc, bool IsTypeLookup,
+                               ResultVector &RV) = 0;
     
-      virtual void setEraseAfterParse(bool f) CXX11_OVERRIDE;
-    
-    #endif // D_ACTIVE_PEER_CONNECTION_COMMAND_H
+    #endif // SWIFT_INDEX_INDEXRECORD_H
 
     
-    class AnnounceTier {
-public:
-  enum AnnounceEvent {
-    STARTED,
-    STARTED_AFTER_COMPLETION,
-    DOWNLOADING,
-    STOPPED,
-    COMPLETED,
-    SEEDING,
-    HALTED
-  };
-    }
+    /// Get a parsed documentation comment for the declaration, if there is one.
+Optional<DocComment *>getSingleDocComment(swift::markup::MarkupContext &Context,
+                                          const Decl *D);
+    
+    SILFunction *SILDebugScope::getParentFunction() const {
+  if (InlinedCallSite)
+    return InlinedCallSite->getParentFunction();
+  if (auto *ParentScope = Parent.dyn_cast<const SILDebugScope *>())
+    return ParentScope->getParentFunction();
+  return Parent.get<SILFunction *>();
+}
+
     
     
-    {} // namespace aria2
     
-    class AsyncNameResolver;
-class DownloadEngine;
-class Command;
-class Option;
+    #endif // SWIFT_INDEX_INDEXDATACONSUMER_H
+
     
-    AuthConfig::AuthConfig() {}
+      static ResultPlanPtr computeResultPlan(SILGenFunction &SGF,
+                                         const CalleeTypeInfo &calleeTypeInfo,
+                                         SILLocation loc,
+                                         SGFContext evalContext);
+    
+    // skip isnan and isinf check for integral types
+template<typename To, typename From>
+typename std::enable_if<std::is_integral<From>::value, bool>::type overflows(From f) {
+  using limit = std::numeric_limits<To>;
+  return f < limit::lowest() || f > limit::max();
+}
+    
+    #ifdef _THP_CORE
+#define THCPStorageType TH_CONCAT_3(THCP,Real,StorageType)
+#define THCPStorageBaseStr TH_CONCAT_STRING_3(Cuda,Real,StorageBase)
+#endif
+    
+    #include 'override_macros.h'
+    
+    #define THPDoubleStorage_CData(obj)  (obj)->cdata
+#define THPFloatStorage_CData(obj)   (obj)->cdata
+#define THPHalfStorage_CData(obj)    (obj)->cdata
+#define THPLongStorage_CData(obj)    (obj)->cdata
+#define THPIntStorage_CData(obj)     (obj)->cdata
+#define THPShortStorage_CData(obj)   (obj)->cdata
+#define THPCharStorage_CData(obj)    (obj)->cdata
+#define THPByteStorage_CData(obj)    (obj)->cdata
+    
+      masterCommandChannel->sendMessage(
+    packMessage(Functions::tensorGesv, rb, ra, b, a),
+    THDState::s_current_worker
+  );
+    
+    PowerIphone::~PowerIphone() {
+	// TODO Auto-generated destructor stub
+}
+
+    
+    	mbedtls_x509_crt_init(&cacert);
+    
+    	GodotAllConvexResultCallback(PhysicsDirectSpaceState::ShapeResult *p_results, int p_resultMax, const Set<RID> *p_exclude) :
+			m_results(p_results),
+			m_exclude(p_exclude),
+			m_resultMax(p_resultMax),
+			count(0) {}
+    
+    	bool initialized;
+    
+        virtual ~jpeg_decoder_mem_stream() { }
+    
+    # if defined(CLZ64)
+/**
+ * OC_ILOGNZ_64 - Integer binary logarithm of a non-zero 64-bit value.
+ * @_v: A non-zero 64-bit value.
+ * Returns floor(log2(_v))+1.
+ * This is the number of bits that would be required to represent _v in two's
+ *  complement notation with all of the leading zeros stripped.
+ * If _v is zero, the return value is undefined; use OC_ILOG_64() instead.
+ */
+#  define OC_ILOGNZ_64(_v) (CLZ64_OFFS-CLZ64(_v))
+/**
+ * OC_ILOG_64 - Integer binary logarithm of a 64-bit value.
+ * @_v: A 64-bit value.
+ * Returns floor(log2(_v))+1, or 0 if _v==0.
+ * This is the number of bits that would be required to represent _v in two's
+ *  complement notation with all of the leading zeros stripped.
+ */
+#  define OC_ILOG_64(_v)   (OC_ILOGNZ_64(_v)&-!!(_v))
+# else
+#  define OC_ILOGNZ_64(_v) (oc_ilog64(_v))
+#  define OC_ILOG_64(_v)   (oc_ilog64(_v))
+# endif
+    
+    #endif
+
+    
+    /*Accesses one of four (signed) bytes given an index.
+  This can be used to avoid small lookup tables.*/
+#define OC_BYTE_TABLE32(_a,_b,_c,_d,_i) \
+  ((signed char) \
+   (((_a)&0xFF|((_b)&0xFF)<<8|((_c)&0xFF)<<16|((_d)&0xFF)<<24)>>(_i)*8))
+/*Accesses one of eight (unsigned) nibbles given an index.
+  This can be used to avoid small lookup tables.*/
+#define OC_UNIBBLE_TABLE32(_a,_b,_c,_d,_e,_f,_g,_h,_i) \
+  ((((_a)&0xF|((_b)&0xF)<<4|((_c)&0xF)<<8|((_d)&0xF)<<12| \
+   ((_e)&0xF)<<16|((_f)&0xF)<<20|((_g)&0xF)<<24|((_h)&0xF)<<28)>>(_i)*4)&0xF)
+    
+    #define opus_fft_free_arch(_st, arch) \
+   ((void)(arch), opus_fft_free_arm_neon(_st))
+    
+    
+/** 16x32 multiply, followed by a 15-bit shift right and 32-bit add.
+    b must fit in 31 bits.
+    Result fits in 32 bits. */
+#undef MAC16_32_Q15
+#define MAC16_32_Q15(c, a, b) ADD32(c, MULT16_32_Q15(a, b))
