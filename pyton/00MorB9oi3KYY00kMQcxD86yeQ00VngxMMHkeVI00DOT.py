4d472b1950559bc8aa3c43317f8b4a335e1ea773
SUNsung <weatherius@gmail.com>
2018-06-15 19:07:56 +0300

    Rewrite [00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py] File in: 2018-06-15 19:07:55

--- pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
+++ pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
@@ -1,120 +1,147 @@
 
         
-          def testReadFirstSentence(self):
-    reader = sentence_io.ConllSentenceReader(self.filepath, 1)
-    sentences, last = reader.read()
-    self.assertEqual(1, len(sentences))
-    pb = sentence_pb2.Sentence()
-    pb.ParseFromString(sentences[0])
-    self.assertFalse(last)
-    self.assertEqual(
-        u'I knew I could do it properly if given the right kind of support .',
-        pb.text)
+            # Arguments
+        x: Input array, 3D or 4D.
+        data_format: Data format of the image array.
+        mode: One of 'caffe', 'tf' or 'torch'.
+            - caffe: will convert the images from RGB to BGR,
+                then will zero-center each color channel with
+                respect to the ImageNet dataset,
+                without scaling.
+            - tf: will scale pixels between -1 and 1,
+                sample-wise.
+            - torch: will scale pixels between 0 and 1 and then
+                will normalize each channel with respect to the
+                ImageNet dataset.
     
-      def __init__(self,
-               name,
-               builder='DynamicComponentBuilder',
-               backend='SyntaxNetComponent'):
-    '''Initializes the ComponentSpec with some defaults for SyntaxNet.
+    # instantiate VAE model
+outputs = decoder(encoder(inputs)[2])
+vae = Model(inputs, outputs, name='vae')
     
-        if during_training:
-      cells_forward = self._train_cells_forward
-      cells_backward = self._train_cells_backward
+    model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+    
+    model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+    
+                # check state initialization
+            layer = convolutional_recurrent.ConvLSTM2D(filters=filters,
+                                                       kernel_size=(num_row, num_col),
+                                                       data_format=data_format,
+                                                       return_sequences=return_sequences)
+            layer.build(inputs.shape)
+            x = Input(batch_shape=inputs.shape)
+            initial_state = layer.get_initial_state(x)
+            y = layer(x, initial_state=initial_state)
+            model = Model(x, y)
+            assert model.predict(inputs).shape == layer.compute_output_shape(inputs.shape)
+    
+    num_classes = 2
+    
+    
+def test_sparse_categorical_crossentropy_4d():
+    y_pred = K.variable(np.array([[[[0.7, 0.1, 0.2],
+                                    [0.0, 0.3, 0.7],
+                                    [0.1, 0.1, 0.8]],
+                                   [[0.3, 0.7, 0.0],
+                                    [0.3, 0.4, 0.3],
+                                    [0.2, 0.5, 0.3]],
+                                   [[0.8, 0.1, 0.1],
+                                    [1.0, 0.0, 0.0],
+                                    [0.4, 0.3, 0.3]]]]))
+    y_true = K.variable(np.array([[[0, 1, 0],
+                                   [2, 1, 0],
+                                   [2, 2, 1]]]))
+    expected_loss = - (np.log(0.7) + np.log(0.3) + np.log(0.1) +
+                       np.log(K.epsilon()) + np.log(0.4) + np.log(0.2) +
+                       np.log(0.1) + np.log(K.epsilon()) + np.log(0.3)) / 9
+    loss = K.eval(losses.sparse_categorical_crossentropy(y_true, y_pred))
+    assert np.isclose(expected_loss, np.mean(loss))
+    
+            html = get_content(self.url)
+    
+    site_info = 'CBS.com'
+download = cbs_download
+download_playlist = playlist_not_supported('cbs')
+
+    
+    	title = video.attributes['title'].value
+	assert title 
+    
+    
+def huaban_download(url, output_dir='.', **kwargs):
+    if re.match(r'http://huaban\.com/boards/\d+/', url):
+        huaban_download_board(url, output_dir, **kwargs)
     else:
-      cells_forward = self._inference_cells_forward
-      cells_backward = self._inference_cells_backward
-    
-      with tf.Session(graph=g, config=session_config) as sess:
-    tf.logging.info('Initializing variables...')
-    sess.run(tf.global_variables_initializer())
+        print('Only board (画板) pages are supported currently')
+        print('ex: http://huaban.com/boards/12345678/')
     
     
-@csrf_protect
-def render_flatpage(request, f):
-    '''
-    Internal interface to the flat page view.
-    '''
-    # If registration is required for accessing this page, and the user isn't
-    # logged in, redirect to the login page.
-    if f.registration_required and not request.user.is_authenticated:
-        from django.contrib.auth.views import redirect_to_login
-        return redirect_to_login(request.path)
-    if f.template_name:
-        template = loader.select_template((f.template_name, DEFAULT_TEMPLATE))
-    else:
-        template = loader.get_template(DEFAULT_TEMPLATE)
+def mtv81_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_content(url)
+    title = HTMLParser().unescape(
+        '|'.join(match1(html, r'<title>(.*?)</title>').split('|')[:-2]))
     
-    # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+    def _worker(executor_reference, work_queue):
+    try:
+        while True:
+            work_item = work_queue.get(block=True)
+            if work_item is not None:
+                work_item.run()
+                continue
+            executor = executor_reference()
+            # Exit if:
+            #   - The interpreter is shutting down OR
+            #   - The executor that owns the worker has been collected OR
+            #   - The executor that owns the worker has been shutdown.
+            if _shutdown or executor is None or executor._shutdown:
+                # Notice other workers
+                work_queue.put(None)
+                return
+            del executor
+    except BaseException:
+        _base.LOGGER.critical('Exception in worker', exc_info=True)
     
-    # Language to be used for generating the HTML full-text search index.
-# Sphinx supports the following languages:
-#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
-#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
-#html_search_language = 'en'
+    # If true, '()' will be appended to :func: etc. cross-reference text.
+#add_function_parentheses = True
     
-        def browse_file(self):
-        filetypes = [
-            ('HTML Files', '*.htm *.html', 'TEXT'),
-            ('PDF Files', '*.pdf', 'TEXT'),
-            ('Windows Help Files', '*.chm'),
-            ('Text Files', '*.txt', 'TEXT'),
-            ('All Files', '*')]
-        path = self.pathvar.get()
-        if path:
-            dir, base = os.path.split(path)
-        else:
-            base = None
-            if platform[:3] == 'win':
-                dir = os.path.join(os.path.dirname(executable), 'Doc')
-                if not os.path.isdir(dir):
-                    dir = os.getcwd()
-            else:
-                dir = os.getcwd()
-        file = self.askfilename(filetypes, dir, base)
-        if file:
-            self.pathvar.set(file)
     
-    For descriptions of all methods, read the comments in the code below.
-Note that all arguments and return values representing article numbers
-are strings, not numbers, since they are rarely used for calculations.
-'''
+  def SubsetForTypes( self, filetypes ):
+    # check cache
+    cache_key = ','.join( filetypes )
+    cached = self._cache.get( cache_key )
+    if cached is not None:
+      return cached
     
-        print('# The following line assumes you have built Python using the standard instructions')
-    print('# Otherwise fix the following line to point to the library.')
-    print('pythonlib = '$(pythonhome)/pcbuild/python%s$(debug_suffix).lib'' % version_suffix)
-    print()
     
-        def test_oldargs0_2_ext(self):
-        try:
-            {}.keys(*(1, 2))
-        except TypeError:
-            pass
-        else:
-            raise RuntimeError
+class OmniCompletionRequest( CompletionRequest ):
+  def __init__( self, omni_completer, request_data ):
+    super( OmniCompletionRequest, self ).__init__( request_data )
+    self._omni_completer = omni_completer
     
-        1: ('line', 2, 'tfunc_import'),    ('next',),
-    2: ('line', 3, 'tfunc_import'),    ('step',),
-    3: ('call', 5, 'main'),            ('break', ('test_module.py', None, False, None, 'func')),
-    4: ('None', 5, 'main'),            ('continue',),
-    5: ('line', 3, 'func', ({1: 1}, [])), ('step',),
-      BpNum Temp Enb Hits Ignore Where
-      1     no   yes 1    0      at test_module.py:2
-    6: ('return', 3, 'func'),          ('step',),
-    7: ('line', 7, 'main'),            ('step',),
-    8: ('return', 7, 'main'),          ('quit',),
     
-                ('2.5 (trunk:6107, Mar 26 2009, 13:02:18) \n[Java HotSpot(TM) Client VM (\'Apple Computer, Inc.\')]',
-            ('Jython', 'trunk', '6107'), 'java1.5.0_16')
-            :
-                ('Jython', '2.5.0', 'trunk', '6107',
-                 ('trunk:6107', 'Mar 26 2009'), 'java1.5.0_16'),
+def ParseArguments():
+  parser = argparse.ArgumentParser()
+  parser.add_argument( '--skip-build', action = 'store_true',
+                       help = 'Do not build ycmd before testing' )
+  parser.add_argument( '--coverage', action = 'store_true',
+                       help = 'Enable coverage report' )
+  parser.add_argument( '--no-flake8', action = 'store_true',
+                       help = 'Do not run flake8' )
     
-    @dataclasses.dataclass
-class IV:
-    T_IV4 = dataclasses.InitVar
-    iv0: dataclasses.InitVar[int]
-    iv1: dataclasses.InitVar
-    iv2: T_IV2
-    iv3: T_IV3
-    not_iv4: T_IV4  # When using string annotations, this field is not recognized as an InitVar.
+    
+def SendCommandRequest( arguments, completer, extra_data = None ):
+  request = CommandRequest( arguments, completer, extra_data )
+  # This is a blocking call.
+  request.Start()
+  request.RunPostCommandActionsIfNeeded()
+  return request.Response()
+    
+    
+def SendCompleterAvailableRequest( filetypes ):
+  request = CompleterAvailableRequest( filetypes )
+  # This is a blocking call.
+  request.Start()
+  return request.Response()
