4d821f8f20ae0ed7c0b80922b85f0092e988ccb1
SUNsung <weatherius@gmail.com>
2018-06-15 04:09:46 +0300

    Rewrite [00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb] File in: 2018-06-15 04:09:45

--- ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
+++ ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
@@ -1,113 +1,201 @@
 
         
-          describe :render do
-    let(:screenshots) { [] }
+            ActiveRecord::Base.clear_all_connections!
+  end
     
-            expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to match(/cd .* && agvtool new-marketing-version 1.1.0/)
-      end
-    
-        # Returns the Sass/SCSS code for the media query.
-    #
-    # @param options [{Symbol => Object}] An options hash (see {Sass::CSS#initialize}).
-    # @return [String]
-    def to_src(options)
-      src = ''
-      src << Sass::Media._interp_to_src(modifier, options)
-      src << ' ' unless modifier.empty?
-      src << Sass::Media._interp_to_src(type, options)
-      src << ' and ' unless type.empty? || expressions.empty?
-      src << expressions.map do |e|
-        Sass::Media._interp_to_src(e, options)
-      end.join(' and ')
-      src
+      def test_assert_called_with_returns
+    assert_called_with(@object, :increment, returns: 1) do
+      @object.increment
     end
+  end
     
-            def clear_cache
-          UI.message('Removing the whole cache dir #{@cache.root}') do
-            FileUtils.rm_rf(@cache.root)
-          end
-        end
+      test 'timer start and stop' do
+    mock = Minitest::Mock.new
+    3.times { mock.expect(:shutdown, nil) }
+    
+        test 'stream_for' do
+      run_in_eventmachine do
+        connection = TestConnection.new
+    
+      def concurrently(enum)
+    enum.map { |*x| Concurrent::Future.execute { yield(*x) } }.map(&:value!)
+  end
+    
+          connection = Connection.new(server, env)
+      assert_called(connection.websocket, :close) do
+        connection.process
       end
     end
   end
 end
 
     
-            # Prints the list of specs & pod cache dirs for a single pod name.
-        #
-        # This output is valid YAML so it can be parsed with 3rd party tools
-        #
-        # @param [Array<Hash>] cache_descriptors
-        #        The various infos about a pod cache. Keys are
-        #        :spec_file, :version, :release and :slug
-        #
-        def print_pod_cache_infos(pod_name, cache_descriptors)
-          UI.puts '#{pod_name}:'
-          cache_descriptors.each do |desc|
-            if @short_output
-              [:spec_file, :slug].each { |k| desc[k] = desc[k].relative_path_from(@cache.root) }
+      test 'processing disconnect message' do
+    run_in_eventmachine do
+      open_connection
+    
+        def send_async(method, *args)
+      send method, *args
+    end
+    
+          # Run Site#process and catch errors
+      #
+      # site - the Jekyll::Site object
+      #
+      # Returns nothing
+      def process_site(site)
+        site.process
+      rescue Jekyll::Errors::FatalException => e
+        Jekyll.logger.error 'ERROR:', 'YOUR SITE COULD NOT BE BUILT:'
+        Jekyll.logger.error '', '------------------------------------'
+        Jekyll.logger.error '', e.message
+        exit(1)
+      end
+    
+                c.action do |_, options|
+              Jekyll::Commands::Doctor.process(options)
             end
-            UI.puts('  - Version: #{desc[:version]}')
-            UI.puts('    Type:    #{pod_type(desc)}')
-            UI.puts('    Spec:    #{desc[:spec_file]}')
-            UI.puts('    Pod:     #{desc[:slug]}')
           end
         end
-      end
-    end
-  end
-end
-
     
-            def self.options
-          [
-            ['--template-url=URL', 'The URL of the git repo containing a ' \
-                                  'compatible template'],
-          ].concat(super)
+            def after_install(path, options = {})
+          unless options['blank'] || options['skip-bundle']
+            begin
+              require 'bundler'
+              bundle_install path
+            rescue LoadError
+              Jekyll.logger.info 'Could not load Bundler. Bundle install skipped.'
+            end
+          end
+    
+            # rubocop:disable Metrics/AbcSize
+        def process(args, opts)
+          if !args || args.empty?
+            raise Jekyll::Errors::InvalidThemeName, 'You must specify a theme name.'
+          end
+    
+              enable_ssl(opts)
+          enable_logging(opts)
+          opts
         end
     
-          def run
-        update_if_necessary!
+    require 'em-websocket'
     
-    end
-    
-      class IncludeCodeTag < Liquid::Tag
-    def initialize(tag_name, markup, tokens)
-      @title = nil
-      @file = nil
-      if markup.strip =~ /\s*lang:(\S+)/i
-        @filetype = $1
-        markup = markup.strip.sub(/lang:\S+/i,'')
+          def valid_processors
+        %w(kramdown) + third_party_processors
       end
-      if markup.strip =~ /(.*)?(\s+|^)(\/*\S+)/i
-        @title = $1 || nil
-        @file = $3
-      end
-      super
-    end
     
-      # Summary is used on the Archive pages to return the first block of content from a post.
-  def summary(input)
-    if input.index(/\n\n/)
-      input.split(/\n\n/)[0]
-    else
-      input
-    end
+          # Generate a Hash for use in generating JSON.
+      # This is useful if fields need to be cleared before the JSON can generate.
+      #
+      # Returns a Hash ready for JSON generation.
+      def hash_for_json(*)
+        to_h
+      end
+    
+            # Unregisters a SIGINT handler.
+        def unregister(sig_callback)
+          @@mutex.synchronize do
+            registered.delete(sig_callback)
+    
+                  # Read!
+              data << io.readpartial(READ_CHUNK_SIZE).encode('UTF-8', Encoding.default_external)
+            else
+              # Do a simple non-blocking read on the IO object
+              data << io.read_nonblock(READ_CHUNK_SIZE)
+            end
+          rescue Exception => e
+            # The catch-all rescue here is to support multiple Ruby versions,
+            # since we use some Ruby 1.9 specific exceptions.
+    
+            # Get the proper capability host to check
+        cap_host = nil
+        if type == :host
+          cap_host = @env.host
+        else
+          with_target_vms([]) do |vm|
+            cap_host = case type
+                       when :provider
+                         vm.provider
+                       when :guest
+                         vm.guest
+                       else
+                         raise Vagrant::Errors::CLIInvalidUsage,
+                           help: opts.help.chomp
+                       end
+          end
+        end
+    
+      def id
+    object.id.to_s
   end
     
-        def render(context)
-      file_dir = (context.registers[:site].source || 'source')
-      file_path = Pathname.new(file_dir).expand_path
-      file = file_path + @file
+    class ManifestSerializer < ActiveModel::Serializer
+  include RoutingHelper
+  include ActionView::Helpers::TextHelper
     
-        # True if the dimensions represent a vertical rectangle
-    def vertical?
-      height > width
+        # Environment detection helpers
+    def sprockets?
+      defined?(::Sprockets)
     end
     
-            def responds?
-          methods = @subject.instance_methods.map(&:to_s)
-          methods.include?('#{@attachment_name}') &&
-            methods.include?('#{@attachment_name}=') &&
-            methods.include?('#{@attachment_name}?')
+      #forward some requests to status message, because a poll is just attached to a status message and is not sharable itself
+  delegate :author_id, :diaspora_handle, :public?, :subscribers, to: :status_message
+    
+          def destroy
+        authorization = Api::OpenidConnect::Authorization.find_by(id: params[:id])
+        if authorization
+          authorization.destroy
+        else
+          flash[:error] = I18n.t('api.openid_connect.authorizations.destroy.fail', id: params[:id])
         end
+        redirect_to api_openid_connect_user_applications_url
+      end
+    
+    Description:
+  Converts between CSS, indented syntax, and SCSS files. For example,
+  this can convert from the indented syntax to SCSS, or from CSS to
+  SCSS (adding appropriate nesting).
+END
+    
+          # Get the publicly-visible URL for an imported file. This URL is used by
+      # source maps to link to the source stylesheet. This may return `nil` to
+      # indicate that no public URL is available; however, this will cause
+      # sourcemap generation to fail if any CSS is generated from files imported
+      # from this importer.
+      #
+      # If an absolute 'file:' URI can be produced for an imported file, that
+      # should be preferred to returning `nil`. However, a URL relative to
+      # `sourcemap_directory` should be preferred over an absolute 'file:' URI.
+      #
+      # @param uri [String] A URI known to be valid for this importer.
+      # @param sourcemap_directory [String, NilClass] The absolute path to a
+      #   directory on disk where the sourcemap will be saved. If uri refers to
+      #   a file on disk that's accessible relative to sourcemap_directory, this
+      #   may return a relative URL. This may be `nil` if the sourcemap's
+      #   eventual location is unknown.
+      # @return [String?] The publicly-visible URL for this file, or `nil`
+      #   indicating that no publicly-visible URL exists. This should be
+      #   appropriately URL-escaped.
+      def public_url(uri, sourcemap_directory)
+        return if @public_url_warning_issued
+        @public_url_warning_issued = true
+        Sass::Util.sass_warn <<WARNING
+WARNING: #{self.class.name} should define the #public_url method.
+WARNING
+        nil
+      end
+    
+            def log_level(name, options = {})
+          if options[:prepend]
+            level = log_levels.values.min
+            level = level.nil? ? 0 : level - 1
+          else
+            level = log_levels.values.max
+            level = level.nil? ? 0 : level + 1
+          end
+          log_levels.update(name => level)
+          define_logger(name)
+        end
+    
+    Liquid::Template.register_tag('img', Jekyll::ImageTag)
