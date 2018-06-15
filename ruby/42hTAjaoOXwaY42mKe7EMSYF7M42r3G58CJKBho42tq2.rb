af89588dd7514e1cbbe0cf19c7eda3ade9b4da30
SUNsung <weatherius@gmail.com>
2018-06-15 10:19:38 +0300

    Rewrite [42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb] File in: 2018-06-15 10:19:38

--- ruby/42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb
+++ ruby/42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb
@@ -1,107 +1,14 @@
 
         
-            # Gets the path to this layout.
-    attr_reader :path
-    
-        def self.catch_inheritance(const)
-      const.define_singleton_method :inherited do |const_|
-        (@children ||= Set.new).add const_
-        if block_given?
-          yield const_
-        end
-      end
-    end
-    
-      def observe_file_removal(path)
-    path.extend(ObserverPathnameExtension).unlink if path.exist?
-  end
-    
-        previous_tag = ARGV.named.first
-    previous_tag ||= Utils.popen_read(
-      'git', '-C', HOMEBREW_REPOSITORY, 'tag', '--list', '--sort=-version:refname'
-    ).lines.first.chomp
-    odie 'Could not find any previous tags!' unless previous_tag
-    
-          def import_key
-        if @cask.gpg.nil?
-          raise CaskError, 'Expected to find gpg public key in formula. Cask '#{@cask}' must add: 'gpg :embedded, key_id: [Public Key ID]' or 'gpg :embedded, key_url: [Public Key URL]''
-        end
-    
-        def render(context)
-      quote = paragraphize(super)
-      author = '<strong>#{@by.strip}</strong>' if @by
-      if @source
-        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
-        parts = []
-        url.each do |part|
-          if (parts + [part]).join('/').length < 32
-            parts << part
-          end
-        end
-        source = parts.join('/')
-        source << '/&hellip;' unless source == @source
-      end
-      if !@source.nil?
-        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
-      elsif !@title.nil?
-        cite = ' <cite>#{@title}</cite>'
-      end
-      blockquote = if @by.nil?
-        quote
-      elsif cite
-        '#{quote}<footer>#{author + cite}</footer>'
-      else
-        '#{quote}<footer>#{author}</footer>'
-      end
-      '<blockquote>#{blockquote}</blockquote>'
-    end
-    
-        # Outputs a list of categories as comma-separated <a> links. This is used
-    # to output the category list for each post on a category page.
-    #
-    #  +categories+ is the list of categories to format.
-    #
-    # Returns string
-    #
-    def category_links(categories)
-      categories.sort.map { |c| category_link c }.join(', ')
-    end
-    
-          locations = Array.new
-      while (data.code.to_i == 301 || data.code.to_i == 302)
-        data = handle_gist_redirecting(data)
-        break if locations.include? data.header['Location']
-        locations << data.header['Location']
-      end
-    
-        def render(context)
-      if @img
-        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
-      else
-        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
-      end
-    end
+          def human?
+    params.has_key?('human') && (params['human'].nil? || as_boolean(params['human']) == true)
   end
 end
+
     
-        def define_class_getter
-      @klass.extend(ClassMethods)
-    end
-    
-            def matches? subject
-          @subject = subject
-          @subject = subject.new if subject.class == Class
-          error_when_not_valid? && no_error_when_valid?
-        end
-    
-        def processor(name) #:nodoc:
-      @known_processors ||= {}
-      if @known_processors[name.to_s]
-        @known_processors[name.to_s]
-      else
-        name = name.to_s.camelize
-        load_processor(name) unless Paperclip.const_defined?(name)
-        processor = Paperclip.const_get(name)
-        @known_processors[name.to_s] = processor
-      end
-    end
+        # Simple callback system. Implements before and after callbacks for
+    # use within the controllers.
+    #
+    # We didn't use the ActiveSupport callbacks because they do not support
+    # passing in any arbitrary object into the callback method (which we
+    # need to do)
