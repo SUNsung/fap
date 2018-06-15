4975bf2fabe0eb7dd1300e40f8394ca295b3e3da
SUNsung <weatherius@gmail.com>
2018-06-15 13:13:57 +0300

    Rewrite [56LzyGKFpsjHM56BDZmQcfbR4k56T8TbIRVGt865639q.py] File in: 2018-06-15 13:13:57

--- pyton/56LzyGKFpsjHM56BDZmQcfbR4k56T8TbIRVGt865639q.py
+++ pyton/56LzyGKFpsjHM56BDZmQcfbR4k56T8TbIRVGt865639q.py
@@ -1,184 +1,174 @@
 
         
-        filenames = {
-    'bin': 'youtube-dl',
-    'exe': 'youtube-dl.exe',
-    'tar': 'youtube-dl-%s.tar.gz' % version}
-build_dir = os.path.join('..', '..', 'build', version)
-for key, filename in filenames.items():
-    url = 'https://yt-dl.org/downloads/%s/%s' % (version, filename)
-    fn = os.path.join(build_dir, filename)
-    with open(fn, 'rb') as f:
-        data = f.read()
-    if not data:
-        raise ValueError('File %s is empty!' % fn)
-    sha256sum = hashlib.sha256(data).hexdigest()
-    new_version[key] = (url, sha256sum)
+        py_binary(
+    name = 'dragnn_model_saver',
+    srcs = ['dragnn_model_saver.py'],
+    deps = [
+        ':dragnn_model_saver_lib',
+        ':spec_builder',
+        '//dragnn/protos:spec_pb2_py',
+        '@absl_py//absl:app',
+        '@absl_py//absl/flags',
+        '@org_tensorflow//tensorflow:tensorflow_py',
+        '@org_tensorflow//tensorflow/core:protos_all_py',
+    ],
+)
     
-    print('Enter the PKCS1 private key, followed by a blank line:')
-privkey = b''
-while True:
-    try:
-        line = input()
-    except EOFError:
-        break
-    if line == '':
-        break
-    privkey += line.encode('ascii') + b'\n'
-privkey = rsa.PrivateKey.load_pkcs1(privkey)
+          # GetHookNames() finds a component with a fixed feature, so at least the
+      # first feature ID should exist.
+      self.assertTrue(
+          any(cell_input.name == 'fixed_channel_0_index_0_ids'
+              for cell_input in cell_subgraph_spec.input))
     
-    entries = []
-for v in versions:
-    fields = v.split('.')
-    year, month, day = map(int, fields[:3])
-    faked = 0
-    patchlevel = 0
-    while True:
+    
+def log_partition_function(num_nodes,
+                           scores,
+                           forest=False,
+                           max_dynamic_range=None):
+  r'''Returns the log of the sum-of-product of spanning trees or forests.
+    
+    from sklearn.dummy import DummyClassifier
+    
+    
+def benchmark(estimator, data):
+    gc.collect()
+    print('Benching %s' % estimator)
+    t0 = time()
+    estimator.fit(data)
+    training_time = time() - t0
+    data_t = estimator.transform(data)
+    data_r = estimator.inverse_transform(data_t)
+    reconstruction_error = np.mean(np.abs(data - data_r))
+    return {'time': training_time, 'error': reconstruction_error}
+    
+        n = 10
+    step = 10000
+    n_samples = 10000
+    dim = 10
+    n_classes = 10
+    for i in range(n):
+        print('============================================')
+        print('Entering iteration %s of %s' % (i, n))
+        print('============================================')
+        n_samples += step
+        X = np.random.randn(n_samples, dim)
+        Y = np.random.randint(0, n_classes, (n_samples,))
+        bench_scikit_tree_classifier(X, Y)
+        Y = np.random.randn(n_samples)
+        bench_scikit_tree_regressor(X, Y)
+    
+        print('Decompressing %s' % ARCHIVE_NAME)
+    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
+        archive.extractall(path='.')
+    os.remove(ARCHIVE_NAME)
+
+    
+        print('Generating skeleton for %s' % f)
+    
+    The dataset is generated using the ``make_biclusters`` function, which
+creates a matrix of small values and implants bicluster with large
+values. The rows and columns are then shuffled and passed to the
+Spectral Co-Clustering algorithm. Rearranging the shuffled matrix to
+make biclusters contiguous shows how accurately the algorithm found
+the biclusters.
+    
+    plt.show()
+
+    
+        parser = argparse.ArgumentParser(description='''\
+        nntplib built-in demo - display the latest articles in a newsgroup''')
+    parser.add_argument('-g', '--group', default='gmane.comp.python.general',
+                        help='group to fetch messages from (default: %(default)s)')
+    parser.add_argument('-s', '--server', default='news.gmane.org',
+                        help='NNTP server hostname (default: %(default)s)')
+    parser.add_argument('-p', '--port', default=-1, type=int,
+                        help='NNTP port number (default: %s / %s)' % (NNTP_PORT, NNTP_SSL_PORT))
+    parser.add_argument('-n', '--nb-articles', default=10, type=int,
+                        help='number of articles to fetch (default: %(default)s)')
+    parser.add_argument('-S', '--ssl', action='store_true', default=False,
+                        help='use NNTP over SSL')
+    args = parser.parse_args()
+    
+            If the optional allow_dotted_names argument is true and the
+        instance does not have a _dispatch method, method names
+        containing dots are supported and resolved, as long as none of
+        the name segments start with an '_'.
+    
+            g = gen()
         try:
-            datetime.date(year, month, day)
-        except ValueError:
-            day -= 1
-            faked += 1
-            assert day > 0
-            continue
-        break
-    if len(fields) >= 4:
-        try:
-            patchlevel = int(fields[3])
-        except ValueError:
-            patchlevel = 1
-    timestamp = '%04d-%02d-%02dT00:%02d:%02dZ' % (year, month, day, faked, patchlevel)
+            raise ValueError
+        except Exception:
+            self.assertEqual(next(g), 'done')
+        self.assertEqual(sys.exc_info(), (None, None, None))
     
-        # Get the version from youtube_dl/version.py without importing the package
-    exec(compile(open('youtube_dl/version.py').read(),
-                 'youtube_dl/version.py', 'exec'))
+        if not fileout and \
+       executable == sys.executable:
+        # 'file' command did not return anything; we'll try to provide
+        # some sensible defaults then...
+        if sys.platform in _default_architecture:
+            b, l = _default_architecture[sys.platform]
+            if b:
+                bits = b
+            if l:
+                linkage = l
+        return bits, linkage
     
-    # Allow direct execution
-import os
-import sys
-import unittest
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+            if platform.uname().system == 'Darwin':
+            # We're on a MacOSX system, check that
+            # the right version information is returned
+            fd = os.popen('sw_vers', 'r')
+            real_ver = None
+            for ln in fd:
+                if ln.startswith('ProductVersion:'):
+                    real_ver = ln.strip().split()[-1]
+                    break
+            fd.close()
+            self.assertFalse(real_ver is None)
+            result_list = res[0].split('.')
+            expect_list = real_ver.split('.')
+            len_diff = len(result_list) - len(expect_list)
+            # On Snow Leopard, sw_vers reports 10.6.0 as 10.6
+            if len_diff > 0:
+                expect_list.extend(['0'] * len_diff)
+            self.assertEqual(result_list, expect_list)
     
-        _ALL_CLASSES = [
-        klass
-        for name, klass in globals().items()
-        if name.endswith('IE') and name != 'GenericIE'
-    ]
-    _ALL_CLASSES.append(GenericIE)
+            with self.assertRaises(SyntaxError):
+            Generic['/T']
     
-            webpage = self._download_webpage(url, video_id)
-        key = self._search_regex(
-            r'src=['\']https?://[^/]+/embed/([A-Za-z0-9_-]+)', webpage, 'key')
-    
-            title = self._html_search_regex(r'<title>(.*?)</title>', webpage, 'title')
-        description = self._html_search_regex(
-            r'<div class='description'[^>]*>([^<]+)</div>', webpage, 'description', fatal=False)
-        thumbnail = self._html_search_regex(
-            r'preview_url\s*:\s*\'(.*?)\'', webpage, 'thumbnail', fatal=False)
-    
-    
-class AudiMediaIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?audi-mediacenter\.com/(?:en|de)/audimediatv/(?P<id>[^/?#]+)'
-    _TEST = {
-        'url': 'https://www.audi-mediacenter.com/en/audimediatv/60-seconds-of-audi-sport-104-2015-wec-bahrain-rookie-test-1467',
-        'md5': '79a8b71c46d49042609795ab59779b66',
-        'info_dict': {
-            'id': '1565',
-            'ext': 'mp4',
-            'title': '60 Seconds of Audi Sport 104/2015 - WEC Bahrain, Rookie Test',
-            'description': 'md5:60e5d30a78ced725f7b8d34370762941',
-            'upload_date': '20151124',
-            'timestamp': 1448354940,
-            'duration': 74022,
-            'view_count': int,
+        def __init__(self):
+        self.experts = []
+        self.common_state = {
+            'problems': 0,
+            'suggestions': 0,
+            'contributions': [],
+            'progress': 0   # percentage, if 100 -> task is finished
         }
-    }
-    # extracted from https://audimedia.tv/assets/embed/embedded-player.js (dataSourceAuthToken)
-    _AUTH_TOKEN = 'e25b42847dba18c6c8816d5d8ce94c326e06823ebf0859ed164b3ba169be97f2'
     
-    import itertools
-import time
+        def test_dog_adapter_shall_make_noise(self):
+        dog = Dog()
+        dog_adapter = Adapter(dog, make_noise=dog.bark)
+        noise = dog_adapter.make_noise()
+        expected_noise = 'woof!'
+        self.assertEqual(noise, expected_noise)
     
-            # Video URL construction algorithm is reverse-engineered from cwhplayer.swf
-        rtmp_url = 'rtmp://camwithher.tv/clipshare/%s' % (
-            ('mp4:%s.mp4' % flv_id) if int(flv_id) > 2010 else flv_id)
+        def test_pool_behavior_with_single_object_inside(self):
+        sample_queue = queue.Queue()
+        sample_queue.put('yam')
+        with ObjectPool(sample_queue) as obj:
+            # print('Inside with: {}'.format(obj))
+            self.assertEqual(obj, 'yam')
+        self.assertFalse(sample_queue.empty())
+        self.assertTrue(sample_queue.get() == 'yam')
+        self.assertTrue(sample_queue.empty())
+        
     
-        def to_python(self, value):
-        return parse_date(value)
+    Test code which will almost always fail (if not exactly 12:01) when untestable
+production code (have a look into constructor_injection.py) is used:
     
-            :param obj: an import name or object
-        '''
-        if isinstance(obj, string_types):
-            obj = import_string(obj)
-        for key in dir(obj):
-            if key.isupper():
-                self[key] = getattr(obj, key)
+    __author__ = 'Ibrahim Diop <ibrahim@sikilabs.com>'
     
-        logger.propagate = False
-    assert not has_level_handler(logger)
-    logger.propagate = True
-    
-    
-def build_feature_placeholders(config):
-  '''Builds tf.Placeholder ops for feeding model features and labels.
-    
-      if not os.path.exists(filepath):
-    def _progress(count, block_size, total_size):
-      sys.stdout.write('\r>> Downloading %s %.1f%%' % (
-          filename, 100.0 * count * block_size / total_size))
-      sys.stdout.flush()
-    
-      Print the performance on the validation set, and update the saved model if
-  its performance is better on the previous ones. If the performance dropped,
-  tell the training to stop.
-    
-    '''Trains the LexNET path-based model.'''
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-    
-      # Print the confusion matrix
-  print('%s confusion matrix:' % set_name)
-  cm = metrics.confusion_matrix(labels, pred, labels=range(len(classes)))
-  cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis] * 100
-  print_cm(cm, labels=classes)
-  return pred
-    
-      Args:
-    data_path: The path to the save directory.
-    data_fname_stem: The filename stem of the file in which to write the data.
-    dataset_dict:  The dictionary of datasets. The keys are strings
-      and the values data dictionaries (str -> numpy arrays) associations.
-    compression (optional): The compression to use for h5py (disabled by
-      default because the library borks on scalars, otherwise try 'gzip').
-  '''
-    
-        def current_loid_obj(self):
-        return self.stacked_proxy_safe_get(c, 'loid')
-    
-    api('modaction', ModActionTemplate)
-    
-        @csrf_exempt
-    @json_validate(
-        signature=VSigned(),
-        user=VThrottledLogin(['user', 'passwd']),
-    )
-    def POST_login(self, responder, user, **kwargs):
-        kwargs.update(dict(
-            controller=self,
-            form=responder('noop'),
-            responder=responder,
-            user=user,
-        ))
-        return handle_login(**kwargs)
-    
-        @validate(VAdmin())
-    def GET_index(self):
-        res = AdminPage(content = AdminAwards(),
-                        title = 'awards').render()
-        return res
-    
-    from reddit_base import RedditController
-import StringIO
-import r2.lib.captcha as captcha
-from pylons import response
+        def prepareReporting(self):
+        rvalue = self._db.insert()
+        if rvalue == -1:
+            self._tc.setProblem(1)
+            self._reporter.prepare()
