b0847a5fa69361c99f1633450db43501501d69db
SUNsung <weatherius@gmail.com>
2018-06-15 20:41:25 +0300

    Rewrite [16vA.JjW5YRrk16gjWEv39Ngxo16EbronNNJH6o16Crr.cpp] File in: 2018-06-15 20:41:24

--- cccccc/16vA.JjW5YRrk16gjWEv39Ngxo16EbronNNJH6o16Crr.cpp
+++ cccccc/16vA.JjW5YRrk16gjWEv39Ngxo16EbronNNJH6o16Crr.cpp
@@ -1,329 +1,186 @@
 
         
-        Status BuildTable(const std::string& dbname,
-                  Env* env,
-                  const Options& options,
-                  TableCache* table_cache,
-                  Iterator* iter,
-                  FileMetaData* meta) {
-  Status s;
-  meta->file_size = 0;
-  iter->SeekToFirst();
+            // check that the points are infront of the center of perspectivity
+    for (int k = 0; k < sols.cols; ++k)
+    {
+        cv::Mat cam_points = C_est[k] * pp + cv::repeat(t_est[k], 1, pp.cols);
+        cv::Mat cam_points_k = cam_points.row(2);
     }
     
+    void compose_motion(InputArray _om1, InputArray _T1, InputArray _om2, InputArray _T2,
+                    Mat& om3, Mat& T3, Mat& dom3dom1, Mat& dom3dT1, Mat& dom3dom2,
+                    Mat& dom3dT2, Mat& dT3dom1, Mat& dT3dT1, Mat& dT3dom2, Mat& dT3dT2);
     
-    {}  // namespace config
+    #ifndef OPENCV_CUDA_EMULATION_HPP_
+#define OPENCV_CUDA_EMULATION_HPP_
     
-    Status DumpFile(Env* env, const std::string& fname, WritableFile* dst) {
-  FileType ftype;
-  if (!GuessType(fname, &ftype)) {
-    return Status::InvalidArgument(fname + ': unknown file type');
-  }
-  switch (ftype) {
-    case kLogFile:         return DumpLog(env, fname, dst);
-    case kDescriptorFile:  return DumpDescriptor(env, fname, dst);
-    case kTableFile:       return DumpTable(env, fname, dst);
-    default:
-      break;
-  }
-  return Status::InvalidArgument(fname + ': not a dump-able file type');
-}
+    #undef cv_hal_gemm32f
+#define cv_hal_gemm32f lapack_gemm32f
+#undef cv_hal_gemm64f
+#define cv_hal_gemm64f lapack_gemm64f
+#undef cv_hal_gemm32fc
+#define cv_hal_gemm32fc lapack_gemm32fc
+#undef cv_hal_gemm64fc
+#define cv_hal_gemm64fc lapack_gemm64fc
     
-    class TableCache {
+    # if !GTEST_OS_WINDOWS
+// Tests that an exit code describes an exit due to termination by a
+// given signal.
+class GTEST_API_ KilledBySignal {
  public:
-  TableCache(const std::string& dbname, const Options* options, int entries);
-  ~TableCache();
-    }
-    
-      // Add the specified file at the specified number.
-  // REQUIRES: This version has not been saved (see VersionSet::SaveTo)
-  // REQUIRES: 'smallest' and 'largest' are smallest and largest keys in file
-  void AddFile(int level, uint64_t file,
-               uint64_t file_size,
-               const InternalKey& smallest,
-               const InternalKey& largest) {
-    FileMetaData f;
-    f.number = file;
-    f.file_size = file_size;
-    f.smallest = smallest;
-    f.largest = largest;
-    new_files_.push_back(std::make_pair(level, f));
-  }
-    
-    static void TestEncodeDecode(const VersionEdit& edit) {
-  std::string encoded, encoded2;
-  edit.EncodeTo(&encoded);
-  VersionEdit parsed;
-  Status s = parsed.DecodeFrom(encoded);
-  ASSERT_TRUE(s.ok()) << s.ToString();
-  parsed.EncodeTo(&encoded2);
-  ASSERT_EQ(encoded, encoded2);
-}
-    
-    TEST(FindFileTest, OverlapSequenceChecks) {
-  Add('200', '200', 5000, 3000);
-  ASSERT_TRUE(! Overlaps('199', '199'));
-  ASSERT_TRUE(! Overlaps('201', '300'));
-  ASSERT_TRUE(Overlaps('200', '200'));
-  ASSERT_TRUE(Overlaps('190', '200'));
-  ASSERT_TRUE(Overlaps('200', '210'));
-}
-    
-    void WriteBatchInternal::SetSequence(WriteBatch* b, SequenceNumber seq) {
-  EncodeFixed64(&b->rep_[0], seq);
-}
-    
-    inline
-static void ExecErrorCheck(int status, char *err_msg) {
-  if (status != SQLITE_OK) {
-    fprintf(stderr, 'SQL error: %s\n', err_msg);
-    sqlite3_free(err_msg);
-    exit(1);
-  }
-}
-    
-    class Benchmark {
+  explicit KilledBySignal(int signum);
+  bool operator()(int exit_status) const;
  private:
-  kyotocabinet::TreeDB* db_;
-  int db_num_;
-  int num_;
-  int reads_;
-  double start_;
-  double last_op_finish_;
-  int64_t bytes_;
-  std::string message_;
-  Histogram hist_;
-  RandomGenerator gen_;
-  Random rand_;
-  kyotocabinet::LZOCompressor<kyotocabinet::LZO::RAW> comp_;
-    }
-    
-    // Find a file by file name.
-bool PyDescriptorDatabase::FindFileByName(const string& filename,
-                                          FileDescriptorProto* output) {
-  ScopedPyObjectPtr py_descriptor(PyObject_CallMethod(
-      py_database_, 'FindFileByName', 's#', filename.c_str(), filename.size()));
-  return GetFileDescriptorProto(py_descriptor.get(), output);
-}
-    
-    #include <Python.h>
-    
-    #include <string>
-    
-    #include <memory>
-    
-    // Generator options (used by csharp_generator.cc):
-struct Options {
-  Options() :
-      file_extension('.cs'),
-      base_namespace(''),
-      base_namespace_specified(false),
-      internal_access(false) {
-  }
-  // Extension of the generated file. Defaults to '.cs'
-  string file_extension;
-  // Base namespace to use to create directory hierarchy. Defaults to ''.
-  // This option allows the simple creation of a conventional C# file layout,
-  // where directories are created relative to a project-specific base
-  // namespace. For example, in a project with a base namespace of PetShop, a
-  // proto of user.proto with a C# namespace of PetShop.Model.Shared would
-  // generate Model/Shared/User.cs underneath the specified --csharp_out
-  // directory.
-  //
-  // If no base namespace is specified, all files are generated in the
-  // --csharp_out directory, with no subdirectories created automatically.
-  string base_namespace;
-  // Whether the base namespace has been explicitly specified by the user.
-  // This is required as the base namespace can be explicitly set to the empty
-  // string, meaning 'create a full directory hierarchy, starting from the first
-  // segment of the namespace.'
-  bool base_namespace_specified;
-  // Whether the generated classes should have accessibility level of 'internal'.
-  // Defaults to false that generates 'public' classes.
-  bool internal_access;
+  const int signum_;
 };
+# endif  // !GTEST_OS_WINDOWS
     
-      void WriteIntroduction(io::Printer* printer);
-  void WriteDescriptor(io::Printer* printer);
-  void WriteGeneratedCodeInfo(const Descriptor* descriptor,
-                              io::Printer* printer,
-                              bool last);
-    
-    namespace protobuf {
-namespace compiler {
-namespace java {
-    }
-    }
-    }
-    
-    // Author: kenton@google.com (Kenton Varda)
-//  Based on original Protocol Buffers design by
-//  Sanjay Ghemawat, Jeff Dean, and others.
+    // When the compiler sees expression IsContainerTest<C>(0), if C is an
+// STL-style container class, the first overload of IsContainerTest
+// will be viable (since both C::iterator* and C::const_iterator* are
+// valid types and NULL can be implicitly converted to them).  It will
+// be picked over the second overload as 'int' is a perfect match for
+// the type of argument 0.  If C::iterator or C::const_iterator is not
+// a valid type, the first overload is not viable, and the second
+// overload will be picked.  Therefore, we can determine whether C is
+// a container class by checking the type of IsContainerTest<C>(0).
+// The value of the expression is insignificant.
 //
-// Generates Java code for a given .proto file.
-    
-    ServiceGenerator* ImmutableGeneratorFactory::NewServiceGenerator(
-    const ServiceDescriptor* descriptor) const {
-  return new ImmutableServiceGenerator(descriptor, context_);
+// Note that we look for both C::iterator and C::const_iterator.  The
+// reason is that C++ injects the name of a class as a member of the
+// class itself (e.g. you can refer to class iterator as either
+// 'iterator' or 'iterator::iterator').  If we look for C::iterator
+// only, for example, we would mistakenly think that a class named
+// iterator is an STL container.
+//
+// Also note that the simpler approach of overloading
+// IsContainerTest(typename C::const_iterator*) and
+// IsContainerTest(...) doesn't work with Visual Age C++ and Sun C++.
+typedef int IsContainer;
+template <class C>
+IsContainer IsContainerTest(int /* dummy */,
+                            typename C::iterator* /* it */ = NULL,
+                            typename C::const_iterator* /* const_it */ = NULL) {
+  return 0;
 }
     
-    // Factory that creates generators for immutable-default messages.
-class ImmutableGeneratorFactory : public GeneratorFactory {
+    
+$for j [[
+  const ParamGenerator<T$j> g$(j)_;
+    
+    template <GTEST_7_TYPENAMES_(T)>
+class GTEST_7_TUPLE_(T) {
  public:
-  ImmutableGeneratorFactory(Context* context);
-  virtual ~ImmutableGeneratorFactory();
+  template <int k> friend class gtest_internal::Get;
     }
     
-      if (flags & TrackVtsc) {
-    flags |= TrackCPU;
+    // AddRef<T>::type is T if T is a reference; otherwise it's T&.  This
+// is the same as tr1::add_reference<T>::type.
+template <typename T>
+struct AddRef { typedef T& type; };  // NOLINT
+template <typename T>
+struct AddRef<T&> { typedef T& type; };  // NOLINT
+    
+      // Returns true iff n is a prime number.
+  virtual bool IsPrime(int n) const = 0;
+    
+    #include <boost/asio/detail/config.hpp>
+#include <cstddef>
+#include <boost/asio/basic_io_object.hpp>
+#include <boost/asio/detail/handler_type_requirements.hpp>
+#include <boost/asio/detail/throw_error.hpp>
+#include <boost/asio/error.hpp>
+#include <boost/asio/wait_traits.hpp>
+#include <boost/asio/waitable_timer_service.hpp>
+    
+    #include <boost/asio/impl/buffered_read_stream.hpp>
+    
+    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+    
+    #include <boost/asio/detail/push_options.hpp>
+    
+    #include <boost/asio/detail/config.hpp>
+    
+    
+    {
+    {
+    {} // namespace detail
+} // namespace asio
+} // namespace boost
+    
+    #endif // defined(__GNUC__) && defined(__arm__)
+    
+    template <typename Function, typename Context>
+inline void invoke(Function& function, Context& context)
+{
+#if !defined(BOOST_ASIO_HAS_HANDLER_HOOKS)
+  Function tmp(function);
+  tmp();
+#else
+  using boost::asio::asio_handler_invoke;
+  asio_handler_invoke(function, boost::asio::detail::addressof(context));
+#endif
+}
+    
+    // Older versions of gcc have difficulty compiling the sizeof expressions where
+// we test the handler type requirements. We'll disable checking of handler type
+// requirements for those compilers, but otherwise enable it by default.
+#if !defined(BOOST_ASIO_DISABLE_HANDLER_TYPE_REQUIREMENTS)
+# if !defined(__GNUC__) || (__GNUC__ >= 4)
+#  define BOOST_ASIO_ENABLE_HANDLER_TYPE_REQUIREMENTS 1
+# endif // !defined(__GNUC__) || (__GNUC__ >= 4)
+#endif // !defined(BOOST_ASIO_DISABLE_HANDLER_TYPE_REQUIREMENTS)
+    
+      // The type of a non-const iterator over the hash map.
+  typedef typename std::list<value_type>::iterator iterator;
+    
+      op_queue_[op_type].enqueue_operation(descriptor, op);
+  ::pollfd& ev = add_pending_event_change(descriptor);
+  ev.events = POLLERR | POLLHUP;
+  switch (op_type)
+  {
+  case read_op: ev.events |= POLLIN; break;
+  case write_op: ev.events |= POLLOUT; break;
+  case except_op: ev.events |= POLLPRI; break;
+  default: break;
+  }
+  interrupter_.interrupt();
+    
+        void ReleaseStringUTFChars(jstring string, const char* utf)
+    { functions->ReleaseStringUTFChars(this, string, utf); }
+    
+    // TODO(t10737622): Improve on-device symbolification
+void getStackTraceSymbols(vector<StackTraceElement>& symbols,
+                          const vector<InstructionPointer>& trace) {
+  symbols.clear();
+  symbols.reserve(trace.size());
+    }
+    
+    int Node::getPositionType(void) const
+{
+    return YGNodeStyleGetPositionType(m_node);
+}
+    
+    template<typename... ARGS>
+inline void logf(const char* tag, const char* msg, ARGS... args) noexcept {
+  log(ANDROID_LOG_FATAL, tag, msg, args...);
+}
+    
+    // Keeps a thread-local reference to the current thread's JNIEnv.
+struct Environment {
+  // May be null if this thread isn't attached to the JVM
+  FBEXPORT static JNIEnv* current();
+  static void initialize(JavaVM* vm);
+    }
+    
+      bool operator==(const ProgramLocation& other) const {
+    // Assumes that the strings are static
+    return (m_functionName == other.m_functionName) && (m_fileName == other.m_fileName) && m_lineNumber == other.m_lineNumber;
   }
     
-    bool Vunit::needsRegAlloc() const {
-  if (next_vr > Vreg::V0) return true;
-    }
-    
-    #ifndef incl_HPHP_JIT_VASM_UNIT_H_
-#define incl_HPHP_JIT_VASM_UNIT_H_
-    
-    /*
- * Determine the next NUMA node according to state maintained in `curr_node`.
- */
-int next_numa_node(std::atomic_int& curr_node);
-/*
- * The number of numa nodes in the system
- */
-inline int num_numa_nodes() {
-  return use_numa ? numa_num_nodes : 1;
+    template <typename T>
+inline bool operator!=(const RefPtr<T>& ref, std::nullptr_t ptr) {
+  return ref.get() != ptr;
 }
-/*
- * Enable numa interleaving for the specified address range
- */
-void numa_interleave(void* start, size_t size);
-/*
- * Allocate the specified address range on the given node
- */
-void numa_bind_to(void* start, size_t size, int node);
-/*
- * Return true if node is part of the allowed set of numa nodes
- */
-inline bool numa_node_allowed(int node) {
-  if (numa_node_set == 0) return true;
-  return numa_node_set & (1u << node);
-}
-    
-    namespace HPHP { namespace jit {
-    }
-    }
-    
-    #ifndef incl_HPHP_LIGHT_PROCESS_H_
-#define incl_HPHP_LIGHT_PROCESS_H_
-    
-    public:
-    // constructor from old CNTK config. This is a function template that is also used to get the config from Scripting.
-    template <class ConfigRecordType>
-    SGD(const ConfigRecordType& configSGD)
-        : SGDParams(configSGD, sizeof(ElemType)),
-          // TODO: The next few do not belong into SGD any more than the network or reader we operate on. Either move network and reader in here, or move these out.
-          m_modelPath((const wstring&) configSGD(L'modelPath')),
-          m_keepCheckPointFiles(configSGD(L'keepCheckPointFiles', false)),
-          m_saveBestModelPerCriterion(configSGD(L'saveBestModelPerCriterion', false)),
-          m_trainCriterionNodeName((const wstring&) configSGD(L'trainCriterionNodeName', L'')),
-          m_evalCriterionNodeName ((const wstring&) configSGD(L'evalCriterionNodeName', L'')),
-          m_traceNodeNamesReal    (configSGD(L'traceNodeNamesReal',     ConfigRecordType::Array(stringargvector()))),
-          m_traceNodeNamesCategory(configSGD(L'traceNodeNamesCategory', ConfigRecordType::Array(stringargvector()))),
-          m_traceNodeNamesSparse  (configSGD(L'traceNodeNamesSparse',   ConfigRecordType::Array(stringargvector()))),
-          m_prevChosenMinibatchSize(0),
-          m_lastFinishedEpochTrainLoss(0.0),
-          m_distGradAgg(nullptr),
-          m_gradHeader(nullptr)
-    {
-        msra::files::make_intermediate_dirs(m_modelPath);
-    }
-    // note: This must be in the header, as we cannot properly specialize this constructor in the CPP to make sure all versions are generated.
-    
-        // relink the input of those nodes whose child is oldNode to point to the new one instead
-    for (auto nodeIter = m_nameToNodeMap.begin(); nodeIter != m_nameToNodeMap.end(); nodeIter++)
-    {
-        ComputationNodeBasePtr node = nodeIter->second;
-        for (int i = 0; i < node->GetNumInputs(); i++)
-            if (node->GetInputs()[i] == oldNode)
-                node->SetInput(i, newNode);
-    }
-    
-    // understand and execute from the syntactic expression tree
-ConfigValuePtr Evaluate(ExpressionPtr);                               // evaluate the expression tree
-void Do(ExpressionPtr e);                                             // evaluate e.do
-shared_ptr<Object> EvaluateField(ExpressionPtr e, const wstring& id); // for experimental CNTK integration
-    
-    static void TestEncodeDecode(const VersionEdit& edit) {
-  std::string encoded, encoded2;
-  edit.EncodeTo(&encoded);
-  VersionEdit parsed;
-  Status s = parsed.DecodeFrom(encoded);
-  ASSERT_TRUE(s.ok()) << s.ToString();
-  parsed.EncodeTo(&encoded2);
-  ASSERT_EQ(encoded, encoded2);
-}
-    
-    std::string Key2(int i) {
-  return Key1(i) + '_xxx';
-}
-    
-    #include <string.h>
-#include 'util/coding.h'
-#include 'util/hash.h'
-    
-      std::string ToString() const;
-    
-    #include 'port/port.h'
-#include 'port/thread_annotations.h'
-    
-      ValueType value_type = kTypeDeletion;
-  if (iter_->Valid()) {
-    do {
-      ParsedInternalKey ikey;
-      if (ParseKey(&ikey) && ikey.sequence <= sequence_) {
-        if ((value_type != kTypeDeletion) &&
-            user_comparator_->Compare(ikey.user_key, saved_key_) < 0) {
-          // We encountered a non-deleted value in entries for previous keys,
-          break;
-        }
-        value_type = ikey.type;
-        if (value_type == kTypeDeletion) {
-          saved_key_.clear();
-          ClearSavedValue();
-        } else {
-          Slice raw_value = iter_->value();
-          if (saved_value_.capacity() > raw_value.size() + 1048576) {
-            std::string empty;
-            swap(empty, saved_value_);
-          }
-          SaveKey(ExtractUserKey(iter_->key()), &saved_key_);
-          saved_value_.assign(raw_value.data(), raw_value.size());
-        }
-      }
-      iter_->Prev();
-    } while (iter_->Valid());
-  }
-    
-    namespace leveldb {
-    }
-    
-    #ifndef COMM_COMM_DNS_H_
-#define COMM_COMM_DNS_H_
-    
-      private:
-    void __StartupCreater();
-    void __FirstGetCreater(const std::string& _servicename);
-    void __Creater(std::vector<ServiceRegister>& _vec);
-    
-    
-#endif /* defined(__PublicComponent__testspy__) */
-
-    
-    // Licensed under the MIT License (the 'License'); you may not use this file except in 
-// compliance with the License. You may obtain a copy of the License at
-// http://opensource.org/licenses/MIT
-    
-    #ifndef COMM_HAS_MEMBER_H_
-#define COMM_HAS_MEMBER_H_
