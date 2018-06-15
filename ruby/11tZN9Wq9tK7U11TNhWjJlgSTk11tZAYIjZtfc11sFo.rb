67a1690e6b375c11383a79039c0b5cff327b4dff
SUNsung <weatherius@gmail.com>
2018-06-15 23:47:41 +0300

    Rewrite [11tZN9Wq9tK7U11TNhWjJlgSTk11tZAYIjZtfc11sFo.rb] File in: 2018-06-15 23:47:40

--- ruby/11tZN9Wq9tK7U11TNhWjJlgSTk11tZAYIjZtfc11sFo.rb
+++ ruby/11tZN9Wq9tK7U11TNhWjJlgSTk11tZAYIjZtfc11sFo.rb
@@ -1,54 +1,136 @@
 
         
-          def translation_scope
-    'devise.omniauth_callbacks'
+            it 'should map attributes to methods' do
+      expect(key.name).to eq('fastlane')
+      expect(key.id).to eq('some-key-id')
+    end
+    
+          it 'prefers a custom version number over a boring version bump' do
+        Fastlane::FastFile.new.parse('lane :test do
+          increment_version_number(version_number: '1.77.3', bump_type: 'major')
+        end').runner.execute(:test)
+    
+        # apply general less to scss conversion
+    def convert_to_scss(file)
+      # get local mixin names before converting the definitions
+      mixins = shared_mixins + read_mixins(file)
+      file   = replace_vars(file)
+      file   = replace_mixin_definitions(file)
+      file   = replace_mixins(file, mixins)
+      file   = extract_mixins_from_selectors(file, CLASSES_TO_MIXINS.inject({}) { |h, cl| h.update('.#{cl}' => cl) })
+      file   = replace_spin(file)
+      file   = replace_fadein(file)
+      file   = replace_image_urls(file)
+      file   = replace_escaping(file)
+      file   = convert_less_ampersand(file)
+      file   = deinterpolate_vararg_mixins(file)
+      file   = replace_calculation_semantics(file)
+      file   = replace_file_imports(file)
+      file   = wrap_at_groups_with_at_root(file)
+      file
+    end
+    
+        def log_processing(name)
+      puts yellow '  #{File.basename(name)}'
+    end
+    
+      # Raise exceptions instead of rendering exception templates.
+  config.action_dispatch.show_exceptions = false
+    
+    desc 'Test all Gemfiles from test/*.gemfile'
+task :test_all_gemfiles do
+  require 'term/ansicolor'
+  require 'pty'
+  require 'shellwords'
+  cmd      = 'bundle install --quiet && bundle exec rake --trace'
+  statuses = Dir.glob('./test/gemfiles/*{[!.lock]}').map do |gemfile|
+    env = {'BUNDLE_GEMFILE' => gemfile}
+    cmd_with_env = '  (#{env.map { |k, v| 'export #{k}=#{Shellwords.escape v}' } * ' '}; #{cmd})'
+    $stderr.puts Term::ANSIColor.cyan('Testing\n#{cmd_with_env}')
+    PTY.spawn(env, cmd) do |r, _w, pid|
+      begin
+        r.each_line { |l| puts l }
+      rescue Errno::EIO
+        # Errno:EIO error means that the process has finished giving output.
+      ensure
+        ::Process.wait pid
+      end
+    end
+    [$? && $?.exitstatus == 0, cmd_with_env]
+  end
+  failed_cmds = statuses.reject(&:first).map { |(_status, cmd_with_env)| cmd_with_env }
+  if failed_cmds.empty?
+    $stderr.puts Term::ANSIColor.green('Tests pass with all gemfiles')
+  else
+    $stderr.puts Term::ANSIColor.red('Failing (#{failed_cmds.size} / #{statuses.size})\n#{failed_cmds * '\n'}')
+    exit 1
   end
 end
-
     
-                  define_method method do |resource_or_scope, *args|
-                scope = Devise::Mapping.find_scope!(resource_or_scope)
-                router_name = Devise.mappings[scope].router_name
-                context = router_name ? send(router_name) : _devise_route_context
-                context.send('#{action}#{scope}_#{module_name}_#{path_or_url}', *args)
-              end
-            end
-          end
-        end
+          spec['main'] =
+          find_files.(File.join(Bootstrap.stylesheets_path, '_bootstrap.scss')) +
+          find_files.(Bootstrap.fonts_path) +
+          %w(assets/javascripts/bootstrap.js)
+    
+          case Rails.env
+      when 'development'
+        config.eager_load = false
+      when 'test'
+        config.eager_load = false
+      when 'production'
+        config.eager_load = true
+      end
+    end
+  end
+end
+    
+    #
+# This script extracts the forms from the main page of each
+# web site in a list. The output of this can be used with
+# Metasploit (and other tools) to obtain the saved form data
+# of these domains.
+#
+    
+    
+# extract label addresses
+addrs = {}
+dtrans.each_line { |ln|
+	if ln =~ /;[^ ].*:/
+		parts = ln.split(' ')
+		label = parts[1]
+		label = label.slice(1,label.index(':')-1)
+		addr = parts[0].split(':')[1].to_i(16)
+		#puts '%s => %x' % [label, addr]
+		one = { label => addr }
+		addrs.merge!(one)
+	end
+}
+#puts addrs.inspect
+    
+    #compileOpts = ['']
+#outputDir		= system.getProperty('java.io.tmpdir')
+outputDir		= 'testoutdir'
+compileOpts 	= [ '-target', '1.3', '-source', '1.3', '-d', outputDir ]
+    
+    module Pod
+  class Command
+    class Env < Command
+      self.summary = 'Display pod environment'
+      self.description = 'Display pod environment.'
+    
+          def initialize(argv)
+        @update = argv.flag?('update')
+        @stats  = argv.flag?('stats')
+        super
       end
     
-    # Each time a record is set we check whether its session has already timed out
-# or not, based on last request time. If so, the record is logged out and
-# redirected to the sign in page. Also, each time the request comes and the
-# record is set, we set the last request time inside its scoped session to
-# verify timeout in the following request.
-Warden::Manager.after_set_user do |record, warden, options|
-  scope = options[:scope]
-  env   = warden.request.env
+          private
     
-        def metadata_timestamped_path(version: self.version, timestamp: :latest, create: false)
-      if create && timestamp == :latest
-        raise CaskError, 'Cannot create metadata path when timestamp is :latest.'
-      end
-    
-          Find.prune if @f.skip_clean? path
-    
-          def restore_request_parameters
-        req = build_rack_request
-        req.update_param('client_id', session[:client_id])
-        req.update_param('redirect_uri', session[:redirect_uri])
-        req.update_param('response_type', response_type_as_space_seperated_values)
-        req.update_param('scope', session[:scopes])
-        req.update_param('state', session[:state])
-        req.update_param('nonce', session[:nonce])
-      end
-    
-          def fetch_public_key(o_auth_app, jwt)
-        public_key = fetch_public_key_from_json(o_auth_app.jwks, jwt)
-        if public_key.empty? && o_auth_app.jwks_uri
-          response = Faraday.get(o_auth_app.jwks_uri)
-          public_key = fetch_public_key_from_json(response.body, jwt)
-        end
-        raise Rack::OAuth2::Server::Authorize::BadRequest(:unauthorized_client) if public_key.empty?
-        public_key
-      end
+      def command_line(*options)
+    options.each { |opt| ARGV << opt }
+    subject.define_singleton_method(:exit) do |*_args|
+      throw(:system_exit, :exit)
+    end
+    subject.run
+    subject.options
+  end
