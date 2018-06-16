014eedc303d939d742413d7b51012d03711361d5
SUNsung <weatherius@gmail.com>
2018-06-17 00:33:04 +0300

    Rewrite [52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp] File in: 2018-06-17 00:33:04

--- cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
+++ cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
@@ -1,222 +1,163 @@
 
         
-        namespace atom {
+        
+    {}  // namespace atom
+    
+    #include 'base/macros.h'
+#include 'v8/include/v8.h'
+    
+    // Wrapper used to keep track of the lifetime of a WebContents.
+// Lives on the UI thread.
+class PrintingUIWebContentsObserver : public content::WebContentsObserver {
+ public:
+  explicit PrintingUIWebContentsObserver(content::WebContents* web_contents);
     }
     
-      class MatchGroundTruth : public QuickTest ,
-      public ::testing::WithParamInterface<const char*> {
-  };
-  
-  TEST_P(MatchGroundTruth, FastPhototestOCR) {
-    OCRTester(TESTING_DIR '/phototest.tif',
-              TESTING_DIR '/phototest.txt',
-              TESSDATA_DIR '_fast', GetParam());
+    
+    {  /// Returns true if \p BB is a dead-end block.
+  bool isDeadEnd(SILBasicBlock *BB) {
+    if (!isComputed) {
+      // Lazily compute the dataflow.
+      compute();
+      isComputed = true;
+    }
+    return ReachableBlocks.count(BB) == 0;
   }
-  
-  INSTANTIATE_TEST_CASE_P( EngLatinDevaArabLang, MatchGroundTruth, 
-                        ::testing::Values('eng', 'script/Latin', 'script/Devanagari', 'script/Arabic') );
-    
-    
-    {  UnicharCompress compressed_;
-  UNICHARSET unicharset_;
-  int null_char_;
-  // The encoding of the null_char_.
-  int encoded_null_char_;
 };
     
-    // Returns the number of blanks before the current word.
-int ResultIterator::BlanksBeforeWord() const {
-  if (CurrentParagraphIsLtr()) return LTRResultIterator::BlanksBeforeWord();
-  return IsAtBeginningOf(RIL_TEXTLINE) ? 0 : 1;
-}
-    
-    
-    {
-    {
-    {        while ((edgept->next->flags[FLAGS] & FIXED)
-          && edgept != loopstart)
-          edgept = edgept->next; /*look for next non-fixed */
-      }
-                                 /*do all the loop */
-      while (edgept != loopstart);
-      edgesum = 0;
-      do {
-        if (edgept->flags[FLAGS] & FIXED)
-          edgesum++;
-        edgept = edgept->next;
-      }
-                                 //count fixed pts
-      while (edgept != loopstart);
-      if (edgesum < 3)
-        area /= 2;               //must have 3 pts
+    namespace swift {
     }
-    while (edgesum < 3);
-    do {
-      linestart = edgept;
-      do {
-        edgept = edgept->next;
-      }
-      while ((edgept->flags[FLAGS] & FIXED) == 0);
-      linestart->next = edgept;
-      edgept->prev = linestart;
-      linestart->vec.x = edgept->pos.x - linestart->pos.x;
-      linestart->vec.y = edgept->pos.y - linestart->pos.y;
-    }
-    while (edgept != loopstart);
-  }
-  else
-    edgept = startpt;            /*start of loop */
     
-    std::string stringFromCFNumber(const CFDataRef& cf_number) {
-  return stringFromCFNumber(cf_number, CFNumberGetType((CFNumberRef)cf_number));
-}
-    
-    /// The following test macros allow pretty test output.
-#define CREATE_DATABASE_TESTS(n)                                               \
-  TEST_F(n, test_plugin_check) {                                               \
-    testPluginCheck();                                                         \
-  }                                                                            \
-  TEST_F(n, test_reset) {                                                      \
-    testReset();                                                               \
-  }                                                                            \
-  TEST_F(n, test_put) {                                                        \
-    testPut();                                                                 \
-  }                                                                            \
-  TEST_F(n, test_get) {                                                        \
-    testGet();                                                                 \
-  }                                                                            \
-  TEST_F(n, test_delete) {                                                     \
-    testDelete();                                                              \
-  }                                                                            \
-  TEST_F(n, test_delete_range) {                                               \
-    testDeleteRange();                                                         \
-  }                                                                            \
-  TEST_F(n, test_scan) {                                                       \
-    testScan();                                                                \
-  }                                                                            \
-  TEST_F(n, test_scan_limit) {                                                 \
-    testScanLimit();                                                           \
-  }
-    
-     private:
-  /// For testing only, ask the event stream to publish events immediately.
-  bool no_defer_{false};
-    
-    // Decodes one 8x8 block of DCT coefficients from the bit stream.
-bool DecodeDCTBlock(const HuffmanTableEntry* dc_huff,
-                    const HuffmanTableEntry* ac_huff,
-                    int Ss, int Se, int Al,
-                    int* eobrun,
-                    BitReaderState* br,
-                    JPEGData* jpg,
-                    coeff_t* last_dc_coeff,
-                    coeff_t* coeffs) {
-  int s;
-  int r;
-  bool eobrun_allowed = Ss > 0;
-  if (Ss == 0) {
-    s = ReadSymbol(dc_huff, br);
-    if (s >= kJpegDCAlphabetSize) {
-      fprintf(stderr, 'Invalid Huffman symbol %d for DC coefficient.\n', s);
-      jpg->error = JPEG_INVALID_SYMBOL;
-      return false;
-    }
-    if (s > 0) {
-      r = br->ReadBits(s);
-      s = HuffExtend(r, s);
-    }
-    s += *last_dc_coeff;
-    const int dc_coeff = SignedLeftshift(s, Al);
-    coeffs[0] = dc_coeff;
-    if (dc_coeff != coeffs[0]) {
-      fprintf(stderr, 'Invalid DC coefficient %d\n', dc_coeff);
-      jpg->error = JPEG_NON_REPRESENTABLE_DC_COEFF;
-      return false;
-    }
-    *last_dc_coeff = s;
-    ++Ss;
-  }
-  if (Ss > Se) {
-    return true;
-  }
-  if (*eobrun > 0) {
-    --(*eobrun);
-    return true;
-  }
-  for (int k = Ss; k <= Se; k++) {
-    s = ReadSymbol(ac_huff, br);
-    if (s >= kJpegHuffmanAlphabetSize) {
-      fprintf(stderr, 'Invalid Huffman symbol %d for AC coefficient %d\n',
-              s, k);
-      jpg->error = JPEG_INVALID_SYMBOL;
-      return false;
-    }
-    r = s >> 4;
-    s &= 15;
-    if (s > 0) {
-      k += r;
-      if (k > Se) {
-        fprintf(stderr, 'Out-of-band coefficient %d band was %d-%d\n',
-                k, Ss, Se);
-        jpg->error = JPEG_OUT_OF_BAND_COEFF;
-        return false;
-      }
-      if (s + Al >= kJpegDCAlphabetSize) {
-        fprintf(stderr, 'Out of range AC coefficient value: s=%d Al=%d k=%d\n',
-                s, Al, k);
-        jpg->error = JPEG_NON_REPRESENTABLE_AC_COEFF;
-        return false;
-      }
-      r = br->ReadBits(s);
-      s = HuffExtend(r, s);
-      coeffs[kJPEGNaturalOrder[k]] = SignedLeftshift(s, Al);
-    } else if (r == 15) {
-      k += 15;
-    } else {
-      *eobrun = 1 << r;
-      if (r > 0) {
-        if (!eobrun_allowed) {
-          fprintf(stderr, 'End-of-block run crossing DC coeff.\n');
-          jpg->error = JPEG_EOB_RUN_TOO_LONG;
-          return false;
-        }
-        *eobrun += br->ReadBits(r);
-      }
-      break;
-    }
-  }
-  --(*eobrun);
-  return true;
-}
-    
-    #endif  // GUETZLI_COLOR_TRANSFORM_H_
-
-    
-    // Performs in-place floating point 8x8 DCT on block[0..63].
-// Note that the DCT used here is the DCT-2 with the first term multiplied by
-// 1/sqrt(2) and the result scaled by 1/2.
-void ComputeBlockDCTDouble(double block[64]);
-    
-    inline int Log2FloorNonZero(uint32_t n) {
-#ifdef __GNUC__
-  return 31 ^ __builtin_clz(n);
+    // On Cygwin, std::once_flag can not be used because it is larger than the
+// platform word.
+typedef uintptr_t swift_once_t;
 #else
-  unsigned int result = 0;
-  while (n >>= 1) result++;
-  return result;
-#endif
-}
     
-    void BuildDCHistograms(const JPEGData& jpg, JpegHistogram* histo);
-void BuildACHistograms(const JPEGData& jpg, JpegHistogram* histo);
-size_t JpegHeaderSize(const JPEGData& jpg, bool strip_metadata);
-size_t EstimateJpegDataSize(const int num_components,
-                            const std::vector<JpegHistogram>& histograms);
+    #include <google/protobuf/descriptor_database.h>
     
-    namespace guetzli {
+    // TODO(jonskeet): Refactor repeated field support; all the implementations are *really* similar. We
+// should probably have a RepeatedFieldGeneratorBase.
+class RepeatedEnumFieldGenerator : public FieldGeneratorBase {
+ public:
+  RepeatedEnumFieldGenerator(const FieldDescriptor* descriptor,
+                             int fieldOrdinal,
+                             const Options *options);
+  ~RepeatedEnumFieldGenerator();
     }
     
-    // Preprocesses the u (1) or v (2) channel of the given YUV image (range 0-255).
-std::vector<std::vector<float>> PreProcessChannel(
-    int w, int h, int channel, float sigma, float amount, bool blur,
-    bool sharpen, const std::vector<std::vector<float>>& image);
+    // Exposed for testing only.
+LIBPROTOC_EXPORT string EscapeJavadoc(const string& input);
+    
+    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
+//   can only really compare the output against a golden value, which is a
+//   fairly tedious and fragile testing strategy.  If we want to go that route,
+//   it probably makes sense to bite the bullet and write a test that compares
+//   the whole generated output for unittest.proto against a golden value, with
+//   a very simple script that can be run to regenerate it with the latest code.
+//   This would mean that updates to the golden file would have to be included
+//   in any change to the code generator, which would actually be fairly useful
+//   as it allows the reviewer to see clearly how the generated code is
+//   changing.
+    
+    #include <google/protobuf/compiler/java/java_context.h>
+#include <google/protobuf/compiler/java/java_enum_field.h>
+#include <google/protobuf/compiler/java/java_extension.h>
+#include <google/protobuf/compiler/java/java_extension_lite.h>
+#include <google/protobuf/compiler/java/java_field.h>
+#include <google/protobuf/compiler/java/java_helpers.h>
+#include <google/protobuf/compiler/java/java_message.h>
+#include <google/protobuf/compiler/java/java_message_lite.h>
+#include <google/protobuf/compiler/java/java_service.h>
+    
+      virtual ExtensionGenerator* NewExtensionGenerator(
+      const FieldDescriptor* descriptor) const;
+    
+    #ifdef WIN32
+/// Suppress a C4244 warning in gtest-printers.h (double -> BiggestInt
+/// conversion)
+#pragma warning(push, 3)
+#pragma warning(disable : 4244)
+#endif
+    
+        auto dropper = DropPrivileges::get();
+    EXPECT_TRUE(dropper->dropToParent(perm_path_ + 'ro'));
+    EXPECT_TRUE(dropper->dropped_);
+    EXPECT_EQ(dropper->to_user_, nobody->pw_uid);
+    
+    #include <osquery/core.h>
+#include <osquery/system.h>
+    
+    bool js_cocos2dx_studio_MovementData_constructor(JSContext *cx, uint32_t argc, jsval *vp);
+void js_cocos2dx_studio_MovementData_finalize(JSContext *cx, JSObject *obj);
+void js_register_cocos2dx_studio_MovementData(JSContext *cx, JS::HandleObject global);
+void register_all_cocos2dx_studio(JSContext* cx, JS::HandleObject obj);
+bool js_cocos2dx_studio_MovementData_getMovementBoneData(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_MovementData_addMovementBoneData(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_MovementData_create(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_MovementData_MovementData(JSContext *cx, uint32_t argc, jsval *vp);
+    
+    
+    
+    
+    
+    
+    
+    		case 's':
+			m_platform->SetType(b2_staticBody);
+			break;
+    
+    class AbstractDiskWriter : public DiskWriter {
+private:
+  std::string filename_;
+    }
+    
+    void AbstractHttpServerResponseCommand::updateReadWriteCheck()
+{
+  if (httpServer_->wantRead()) {
+    if (!readCheck_) {
+      readCheck_ = true;
+      e_->addSocketForReadCheck(socket_, this);
+    }
+  }
+  else if (readCheck_) {
+    readCheck_ = false;
+    e_->deleteSocketForReadCheck(socket_, this);
+  }
+  if (httpServer_->wantWrite()) {
+    if (!writeCheck_) {
+      writeCheck_ = true;
+      e_->addSocketForWriteCheck(socket_, this);
+    }
+  }
+  else if (writeCheck_) {
+    writeCheck_ = false;
+    e_->deleteSocketForWriteCheck(socket_, this);
+  }
+}
+    
+    namespace aria2 {
+    }
+    
+    class ActivePeerConnectionCommand : public Command {
+private:
+  RequestGroup* requestGroup_;
+  std::shared_ptr<BtRuntime> btRuntime_;
+  std::shared_ptr<PieceStorage> pieceStorage_;
+  std::shared_ptr<PeerStorage> peerStorage_;
+  std::shared_ptr<BtAnnounce> btAnnounce_;
+    }
+    
+      int64_t offset_;
+    
+    void AnnounceList::setCurrentTier(
+    std::deque<std::shared_ptr<AnnounceTier>>::iterator itr)
+{
+  if (itr != std::end(tiers_)) {
+    currentTier_ = std::move(itr);
+    currentTracker_ = std::begin((*currentTier_)->urls);
+  }
+}
+    
+    
+    {  void nextEventIfAfterStarted();
+};
