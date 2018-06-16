2ed74fc2538dcd20e2dd6e7a0de86b5e9e942fd8
SUNsung <weatherius@gmail.com>
2018-06-16 05:29:11 +0300

    Rewrite [35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb] File in: 2018-06-16 05:29:11

--- ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
+++ ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
@@ -1,44 +1,60 @@
 
         
-        class NotificationMailerPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/mention
-  def mention
-    m = Mention.last
-    NotificationMailer.mention(m.account, Notification.find_by(activity: m))
+            if registration
+      u2f.authenticate!(challenges, response, Base64.decode64(registration.public_key), registration.counter)
+      registration.update(counter: response.counter)
+      true
+    end
+  rescue JSON::ParserError, NoMethodError, ArgumentError, U2F::Error
+    false
   end
+end
+
     
-          def escape(object)
-        case object
-        when Hash   then escape_hash(object)
-        when Array  then object.map { |o| escape(o) }
-        when String then escape_string(object)
-        when Tempfile then object
-        else nil
+          def merge!(other)
+        other.each do |key, value|
+          self[convert_key(key)] = value
         end
+        self
       end
     
-        it 'Reads referrer from Host header when Referer header is missing' do
-      env = {'HTTP_HOST' => 'foo.com'}
-      expect(subject.referrer(env)).to eq('foo.com')
+        def log_status(status)
+      puts bold status
     end
     
-        it 'redirects requests with sneaky encoded session cookies' do
-      get '/path', {}, 'HTTP_COOKIE' => 'rack.%73ession=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
-      expect(last_response).to be_redirect
-      expect(last_response.location).to eq('/path')
+      # Prepend all log lines with the following tags.
+  # config.log_tags = [ :subdomain, :uuid ]
+    
+      # Disable request forgery protection in test environment.
+  config.action_controller.allow_forgery_protection = false
+    
+    When /^(?:|I )choose '([^']*)'$/ do |field|
+  choose(field)
+end
+    
+        # Extracts the Geometry from a 'WxH,O' string
+    # Where W is the width, H is the height,
+    # and O is the EXIF orientation
+    def self.parse(string)
+      GeometryParser.new(string).make
     end
+    
+            def failure_message
+          'Should have an attachment named #{@attachment_name}'
+        end
+    
+            def failure_message
+          'Attachment #{@attachment_name} should be required'
+        end
+    
+      # Get list of styles saved on previous deploy (running rake paperclip:refresh:missing_styles)
+  def self.get_registered_attachments_styles
+    YAML.load_file(Paperclip.registered_attachments_styles_path)
+  rescue Errno::ENOENT
+    nil
   end
+  private_class_method :get_registered_attachments_styles
     
-      it 'should set the X-Frame-Options' do
-    expect(get('/', {}, 'wants' => 'text/html').headers['X-Frame-Options']).to eq('SAMEORIGIN')
-  end
-    
-        mock_app do
-      use Rack::Head
-      use(Rack::Config) { |e| e['rack.session'] ||= {}}
-      use detector
-      use klass
-      run DummyApp
-    end
-    
-      def fetch(url, sha1, output)
+        module Statements
+      def add_attachment(table_name, *attachment_names)
+        raise ArgumentError, 'Please specify attachment name in your add_attachment call in your migration.' if attachment_names.empty?
