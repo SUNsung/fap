f84646c3f4e0c08e3aa6d842b2f8dd681ab12b13
SUNsung <weatherius@gmail.com>
2018-06-16 03:40:24 +0300

    Rewrite [211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb] File in: 2018-06-16 03:40:24

--- ruby/211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb
+++ ruby/211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb
@@ -1,51 +1,23 @@
 
         
-            version '1' do
-      self.release = '1.12.6'
-      self.base_url = 'http://coffeescript.org/v1/'
+            groups << @user.authorized_groups.visible_to_user(current_user) if current_user
+    groups << @user.authorized_groups.public_to_user(current_user)
     
-        def initial_urls
-      [ 'https://hexdocs.pm/elixir/#{self.class.release}/api-reference.html',
-        'https://hexdocs.pm/eex/#{self.class.release}/EEx.html',
-        'https://hexdocs.pm/ex_unit/#{self.class.release}/ExUnit.html',
-        'https://hexdocs.pm/iex/#{self.class.release}/IEx.html',
-        'https://hexdocs.pm/logger/#{self.class.release}/Logger.html',
-        'https://hexdocs.pm/mix/#{self.class.release}/Mix.html',
-        'https://elixir-lang.org/getting-started/introduction.html' ]
+        def log_processing(name)
+      puts yellow '  #{File.basename(name)}'
     end
     
-        html_filters.push 'node/clean_html', 'node/entries', 'title'
-    
-        self.name = 'PHP'
-    self.type = 'php'
-    self.release = '7.2.5'
-    self.base_url = 'https://secure.php.net/manual/en/'
-    self.root_path = 'index.html'
-    self.initial_paths = %w(
-      funcref.html
-      langref.html
-      refs.database.html
-      set.mysqlinfo.html
-      language.control-structures.html
-      reference.pcre.pattern.syntax.html
-      reserved.exceptions.html
-      reserved.interfaces.html
-      reserved.variables.html)
-    
-          super
+        s = StringIO.new
+    SystemConfig.dump_verbose_config s
+    # Dummy summary file, asciibetically first, to control display title of gist
+    files['# #{f.name} - #{timestamp}.txt'] = { content: brief_build_info(f) }
+    files['00.config.out'] = { content: s.string }
+    files['00.doctor.out'] = { content: Utils.popen_read('#{HOMEBREW_PREFIX}/bin/brew', 'doctor', err: :out) }
+    unless f.core_formula?
+      tap = <<~EOS
+        Formula: #{f.name}
+        Tap: #{f.tap}
+        Path: #{f.path}
+      EOS
+      files['00.tap.out'] = { content: tap }
     end
-  end
-end
-
-    
-        def scoped_accounts
-      Account.unscoped.tap do |scope|
-        scope.merge! with_usernames
-        scope.merge! matching_username
-        scope.merge! matching_domain
-      end
-    end
-    
-      def type
-    'Emoji'
-  end
