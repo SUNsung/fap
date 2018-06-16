ae485e102bb3d05154ba5e574983ab616acb6de2
SUNsung <weatherius@gmail.com>
2018-06-16 12:09:04 +0300

    Rewrite [42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb] File in: 2018-06-16 12:09:04

--- ruby/42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb
+++ ruby/42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb
@@ -1,14 +1,150 @@
 
         
-          def human?
-    params.has_key?('human') && (params['human'].nil? || as_boolean(params['human']) == true)
+          </body>
+</html>
+HTML
+    
+        # All the entries in this collection.
+    #
+    # Returns an Array of file paths to the documents in this collection
+    #   relative to the collection's directory
+    def entries
+      return [] unless exists?
+      @entries ||=
+        Utils.safe_glob(collection_dir, ['**', '*'], File::FNM_DOTMATCH).map do |entry|
+          entry['#{collection_dir}/'] = ''
+          entry
+        end
+    end
+    
+            def stop
+          # There is only one EventMachine instance per Ruby process so stopping
+          # it here will stop the reactor thread we have running.
+          EM.stop if EM.reactor_running?
+          Jekyll.logger.debug 'LiveReload Server:', 'halted'
+        end
+    
+      class FeatureTopicUsers < Jobs::Base
+    
+            def generate_confirmation_token!
+          generate_confirmation_token && save(validate: false)
+        end
+    
+        if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
+  end
+    
+      # Check if there is no signed in user before doing the sign out.
+  #
+  # If there is no signed in user, it will set the flash message and redirect
+  # to the after_sign_out path.
+  def verify_signed_out_user
+    if all_signed_out?
+      set_flash_message! :notice, :already_signed_out
+    
+        def email_changed(record, opts={})
+      devise_mail(record, :email_changed, opts)
+    end
+    
+            if name.start_with?('etc') || name.start_with?('core.stdc.')
+          name.split('.')[0..2].join('.')
+        elsif name.start_with?('ddmd')
+          'ddmd'
+        elsif name.start_with?('rt')
+          'rt'
+        else
+          name.split('.')[0..1].join('.')
+        end
+      end
+    
+          options[:only_patterns] = [/\Agetting-started\//, /\Alayout\//, /\Acontent\//, /\Acomponents\//, /\Autilities\//, /\Amigration\//]
+    end
+    
+        version '1.3' do
+      self.release = '1.3.4'
+      self.base_urls = [
+        'https://hexdocs.pm/elixir/#{release}/',
+        'https://hexdocs.pm/eex/#{release}/',
+        'https://hexdocs.pm/ex_unit/#{release}/',
+        'https://hexdocs.pm/iex/#{release}/',
+        'https://hexdocs.pm/logger/#{release}/',
+        'https://hexdocs.pm/mix/#{release}/',
+        'https://elixir-lang.org/getting-started/'
+      ]
+    end
   end
 end
 
     
-        # Simple callback system. Implements before and after callbacks for
-    # use within the controllers.
-    #
-    # We didn't use the ActiveSupport callbacks because they do not support
-    # passing in any arbitrary object into the callback method (which we
-    # need to do)
+        SPLIT_INTS = /(?<=\d)\.(?=[\s\d])/.freeze
+    
+        def subpath_from(url, options = nil)
+      self.class.parse(url).subpath_to(self, options)
+    end
+    
+        it 'leaves TempFiles untouched' do
+      mock_app do |env|
+        request = Rack::Request.new(env)
+        [200, {'Content-Type' => 'text/plain'}, [request.params['file'][:filename] + '\n' + \
+                                                 request.params['file'][:tempfile].read + '\n' + \
+                                                 request.params['other']]]
+      end
+    
+          it 'should remain unchanged as ASCII-8BIT' do
+        body = @app.call({ 'PATH_INFO' => '/'.encode('ASCII-8BIT') })[2][0]
+        expect(body).to eq('ASCII-8BIT')
+      end
+    end
+  end
+end
+
+    
+      # These two settings work together to allow you to limit a spec run
+  # to individual examples or groups you care about by tagging them with
+  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
+  # get run.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+    
+            def stock_location
+          render 'spree/api/v1/shared/stock_location_required', status: 422 and return unless params[:stock_location_id]
+          @stock_location ||= StockLocation.accessible_by(current_ability, :read).find(params[:stock_location_id])
+        end
+    
+    module LogStash
+  module Api
+    module Commands
+      module System
+        class Plugins < Commands::Base
+          def run
+            { :total => plugins.count, :plugins => plugins }
+          end
+    
+      def _nt_branch
+    start_index = index
+    if node_cache[:branch].has_key?(index)
+      cached = node_cache[:branch][index]
+      if cached
+        cached = SyntaxNode.new(input, index...(index + 1)) if cached == true
+        @index = cached.interval.end
+      end
+      return cached
+    end
+    
+      def file_fetch(url, sha1, target)
+    filename = File.basename( URI(url).path )
+    output = '#{target}/#{filename}'
+    begin
+      actual_sha1 = file_sha1(output)
+      if actual_sha1 != sha1
+        fetch(url, sha1, output)
+      end
+    rescue Errno::ENOENT
+      fetch(url, sha1, output)
+    end
+    return output
+  end
