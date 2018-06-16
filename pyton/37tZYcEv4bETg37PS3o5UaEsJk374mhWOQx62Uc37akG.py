385c9e01335f108477a560434adc169c1ddcd3f9
SUNsung <weatherius@gmail.com>
2018-06-16 13:00:24 +0300

    Rewrite [37tZYcEv4bETg37PS3o5UaEsJk374mhWOQx62Uc37akG.py] File in: 2018-06-16 13:00:23

--- pyton/37tZYcEv4bETg37PS3o5UaEsJk374mhWOQx62Uc37akG.py
+++ pyton/37tZYcEv4bETg37PS3o5UaEsJk374mhWOQx62Uc37akG.py
@@ -1,24 +1,18 @@
 
-def timedelta_to_seconds(td):
-    # type: (datetime.timedelta) -> float
-    '''Equivalent to td.total_seconds() (introduced in python 2.7).'''
-    return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10 ** 6) / float(10 ** 6)
+        
+        
+if __name__ == '__main__':
+    RemoveDuplicateUrls.run()
+
     
-           WSGI is a *synchronous* interface, while Tornado's concurrency model
-       is based on single-threaded asynchronous execution.  This means that
-       running a WSGI app with Tornado's `WSGIContainer` is *less scalable*
-       than running the same app in a multi-threaded WSGI server like
-       ``gunicorn`` or ``uwsgi``.  Use `WSGIContainer` only when there are
-       benefits to combining Tornado and WSGI in the same process that
-       outweigh the reduced scalability.
+        def get(self, query):
+        '''Get the stored query result from the cache.
     
-        with StackContext(die_on_error):
-        # Any exception thrown here *or in callback and its descendants*
-        # will cause the process to exit instead of spinning endlessly
-        # in the ioloop.
-        http_client.fetch(url, callback)
-    ioloop.start()
+        def __init__(self, message_id, message, timestamp):
+        self.message_id = message_id
+        self.message = message
+        self.timestamp = timestamp
     
-        def close(self):
-        self.reader.close()
-        try_close(self.writer)
+        def __init__(self, seller_category_map, seller_category_overrides_map):
+        self.seller_category_map = seller_category_map
+        self.seller_category_overrides_map = seller_category_overrides_map
