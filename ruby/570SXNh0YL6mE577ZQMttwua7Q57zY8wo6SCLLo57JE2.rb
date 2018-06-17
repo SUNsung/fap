6c8b2f2273b4dc8e61272fd49f31957d3481ce4c
SUNsung <weatherius@gmail.com>
2018-06-17 08:10:47 +0300

    Rewrite [570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb] File in: 2018-06-17 08:10:46

--- ruby/570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb
+++ ruby/570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb
@@ -1,114 +1,55 @@
 
         
-            if resource.errors.empty?
-      resource.unlock_access! if unlockable?(resource)
-      if Devise.sign_in_after_reset_password
-        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
-        set_flash_message!(:notice, flash_message)
-        sign_in(resource_name, resource)
-      else
-        set_flash_message!(:notice, :updated_not_active)
-      end
-      respond_with resource, location: after_resetting_password_path_for(resource)
-    else
-      set_minimum_password_length
-      respond_with resource
-    end
+            render_response
   end
     
-    if defined?(ActionMailer)
-  class Devise::Mailer < Devise.parent_mailer.constantize
-    include Devise::Mailers::Helpers
+      # Finds the projects '@user' contributed to, limited to either public projects
+  # or projects visible to the given user.
+  #
+  # current_user - When given the list of the projects is limited to those only
+  #                visible by this user.
+  #
+  # Returns an ActiveRecord::Relation.
+  def execute(current_user = nil)
+    segments = all_projects(current_user)
     
-          protected
+      def validate_email_options
+    errors.add(:base, 'subject and expected_receive_period_in_days are required') unless options['subject'].present? && options['expected_receive_period_in_days'].present?
     
-      if record && record.respond_to?(:timedout?) && warden.authenticated?(scope) &&
-     options[:store] != false && !env['devise.skip_timeoutable']
-    last_request_at = warden.session(scope)['last_request_at']
+      class Worker
+    attr_reader :thread, :id, :agent, :config, :mutex, :scheduler, :restarting
     
-            name
-      end
+      def destroy
+    @user_credential = current_user.user_credentials.find(params[:id])
+    @user_credential.destroy
     
-        options[:fix_urls] = ->(url) do
-      url.sub! %r{(#{Rust.base_url}.+/)\z}, '\1index.html'
-      url.sub! '/unicode/u_str', '/unicode/str/'
-      url
     end
-    
-        version 'C++' do
-      include MultipleBaseUrls
-      self.base_urls = ['https://www.tensorflow.org/api_docs/cc/', 'https://www.tensorflow.org/api_guides/cc/']
-    end
-    
-            css('p > code:first-child:last-child', 'td > code:first-child:last-child').each do |node|
-          next if node.previous.try(:content).present? || node.next.try(:content).present?
-          node.inner_html = node.inner_html.squish.gsub(/<br(\ \/)?>\s*/, '\n')
-          node.content = node.content.strip
-          node.name = 'pre' if node.content =~ /\s/
-          node.parent.before(node.parent.children).remove if node.parent.name == 'p'
-        end
-    
-        def paragraphize(input)
-      '<p>#{input.lstrip.rstrip.gsub(/\n\n/, '</p><p>').gsub(/\n/, '<br/>')}</p>'
-    end
-  end
-end
-    
-        def render(context)
-      if @img
-        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
-      else
-        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
-      end
-    end
-  end
-end
-    
-      public(:input)
-end # class FPM::Package::NPM
 
     
-      # Returns path of a processed template PackageInfo given to 'pkgbuild --info'
-  # note: '--info' is undocumented:
-  # http://managingosx.wordpress.com/2012/07/05/stupid-tricks-with-pkgbuild
-  def pkginfo_template_path
-    pkginfo_template = Tempfile.open('fpm-PackageInfo')
-    pkginfo_data = template('osxpkg.erb').result(binding)
-    pkginfo_template.write(pkginfo_data)
-    pkginfo_template.close
-    pkginfo_template.path
-  end # def write_pkginfo_template
+        self.sigs.each_key do |k|
+      # There is only one pattern per run to test
+      matched = nil
+      matches = nil
     
-      option '--zonetype', 'ZONETYPE',
-    'Set the allowed zone types (global, nonglobal, both)',
-    :default => 'value=global value=nonglobal' do |value|
-      case @value
-      when 'both'
-        value = 'value=global value=nonglobal'
-      else
-        value = 'value=#{value}'
-      end
-    end # value
     
-      # Helper for group lookup
-  def gid2group(gid)
-    begin
-      grent = Etc.getgrgid(gid)
-      return grent.name
-    rescue ArgumentError => e
-      # Invalid user id? No user? Return the uid.
-      logger.warn('Failed to find group for gid #{gid}')
-      return gid.to_s
-    end
-  end # def uid2user
-end # class FPM::Target::Puppet
+    {	if ln =~ /;(read|write)_(handle|filename)=/
+		parts = ln.split(' ')
+		if (parts[0] == 'mov')
+			parts2 = parts[2].split('=')
+			label = parts2[0]
+			label.slice!(0,1)
+			old = parts2[1]
+			new = addrs[label]
+			#puts '%32s: %s -> %x' % [label, old, new]
+			replaces << [label, old, new.to_s(16)]
+		end
+	end
+}
     
-      # Where we keep metadata and post install scripts and such
-  def fpm_meta_path
-    @fpm_meta_path ||= begin
-                         path = File.join(staging_path, '.fpm')
-                         FileUtils.mkdir_p(path)
-                         path
-                       end
-  end
-end
+    signer._invoke('JarSignerMSF','[Ljava.lang.String;',jarsignerOpts)
+    
+            def on_block(node)
+          on_body_of_reduce(node) do |body|
+            void_next = body.each_node(:next).find do |n|
+              n.children.empty? && parent_block_node(n) == node
+            end
