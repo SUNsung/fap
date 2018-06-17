eb863d7333e7e925dda8556b597ea923a1b79c45
SUNsung <weatherius@gmail.com>
2018-06-17 17:53:15 +0300

    Rewrite [21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb] File in: 2018-06-17 17:53:14

--- ruby/21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb
+++ ruby/21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb
@@ -1,78 +1,76 @@
 
         
-          def failure
-    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
-    redirect_to after_omniauth_failure_path_for(resource_name)
+          def test_utc_p
+    assert_predicate(Time.gm(2000), :gmt?)
+    assert_not_predicate(Time.local(2000), :gmt?)
+    assert_not_predicate(Time.at(0), :gmt?)
   end
     
-          protected
-    
-          config.paths['log']             = '#{Msf::Config.log_directory}/#{Rails.env}.log'
-      config.paths['config/database'] = [Metasploit::Framework::Database.configurations_pathname.try(:to_path)]
-    
-      def register_sigs
-    self.sigs = {
-      :banner		=> /^(\*\s+OK[^\n\r]*)/i,
-      :login		=> /^CAPABILITY\s+LOGIN\s+([^\s]+)\s+([^\n\r]+)/i,
-      :login_pass => /^CAPABILITY\s+OK\s+(Login[^\n\r]*)/i,
-      :login_bad	=> /^CAPABILITY\s+BAD\s+(Login[^\n\r]*)/i,
-      :login_fail => /^CAPABILITY\s+NO\s+(Login[^\n\r]*)/i
-    }
+      it 'decodes the remaining ints when passed the '*' modifier after another directive' do
+    'dcbahgfe'.unpack(unpack_format()+unpack_format('*')).should == [1684234849, 1751606885]
   end
     
-          case matched
-      when :webhost
-        sessions[s[:session]].merge!({k => matches})
-        if(s[:get])
-          print_status('HTTP GET: #{s[:session]} http://#{s[:webhost]}#{s[:get]}')
-          sessions.delete(s[:session])
-          return
+      it 'respects Thread subclasses' do
+    c = Class.new(Thread)
+    t = c.send(@method) { }
+    t.should be_kind_of(c)
+    
+    EOS
+end
+    
+        def index
+      pods_json = PodPresenter.as_collection(Pod.all)
+    
+          rescue_from Rack::OAuth2::Server::Authorize::BadRequest,
+                  JSON::JWT::InvalidFormat, JSON::JWK::UnknownAlgorithm do |e|
+        logger.info e.backtrace[0, 10].join('\n')
+        render json: {error: :invalid_request, error_description: e.message, status: 400}
+      end
+      rescue_from JSON::JWT::VerificationFailed do |e|
+        logger.info e.backtrace[0, 10].join('\n')
+        render json: {error: :invalid_grant, error_description: e.message, status: 400}
+      end
+    end
+  end
+end
+
+    
+          # Prints text in yellow.
+      def warning(text)
+        puts color.colorize(text, :yellow)
+      end
+    
+          def stage_set?
+        !!fetch(:stage, false)
+      end
+    
+        # @abstract
+    #
+    # Identify the SHA of the commit that will be deployed.  This will most likely involve SshKit's capture method.
+    #
+    # @return void
+    #
+    def fetch_revision
+      raise NotImplementedError, 'Your SCM strategy module should provide a #fetch_revision method'
+    end
+  end
+end
+
+    
+      desc 'Finished'
+  task :finished do
+  end
+    
+            def argument?
+          ARGUMENT_DECLARATION_TYPES.include?(@declaration_node.type)
         end
-      when nil
-        # No matches, no saved state
-      end # end case matched
-    end # end of each_key
-  end # end of parse
-end # end of URL sniffer
     
-    puts '* Closing socket'
-    
-    classNames.each { |name|
-	codez << %Q^
-public class #{name} {
-	public static void main(String args[]) {
-		System.out.println('This is from #{name}.');
-	}
-}^}
-    
-    signer._invoke('JarSignerMSF','[Ljava.lang.String;',jarsignerOpts)
-    
-            self.summary = 'The repl listens to commands on standard input'
-        self.description = <<-DESC
-          The repl listens to commands on standard input and prints their
-          result to standard output.
-          It accepts all the other ipc subcommands. The repl will signal the
-          end of output with the the ASCII CR+LF `\\n\\r`.
-        DESC
-    
-          describe 'fetching filtered servers by role' do
-        subject { dsl.roles(:app, filter: :active) }
-    
-      # GET /books
-  # GET /books.json
-  def index
-    @books = Book.all
-  end
-    
-            # 'match' is a fairly generic name, so we don't flag it unless we see
-        # a string or regexp literal on one side or the other
-        def_node_matcher :match_call?, <<-PATTERN
-          {(send {str regexp} :match _)
-           (send !nil? :match {str regexp})}
-        PATTERN
-    
-            def repeated_condition?(previous, condition)
-          previous.any? { |c| c.include?(condition) }
+            def each_unnecessary_space_match(node, &blk)
+          each_match_range(
+            contents_range(node),
+            MULTIPLE_SPACES_BETWEEN_ITEMS_REGEX,
+            &blk
+          )
         end
       end
     end
@@ -80,7 +78,24 @@ public class #{name} {
 end
 
     
-          def expect_no_offenses(source, file = nil)
-        inspect_source(source, file)
+            def autocorrect(node)
+          # Regexp#match can take a second argument, but this cop doesn't
+          # register an offense in that case
+          return unless node.first_argument.regexp_type?
     
-            protected
+            def on_block(node)
+          on_body_of_reduce(node) do |body|
+            void_next = body.each_node(:next).find do |n|
+              n.children.empty? && parent_block_node(n) == node
+            end
+    
+        expect(corrected).to eq(''something'.to_sym')
+  end
+end
+
+    
+            it 'registers an offense for #{type} not beginning '\
+          'and ending with a blank line' do
+          inspect_source(source)
+          expect(cop.messages).to eq([missing_begin, missing_end])
+        end
