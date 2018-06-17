698f16726f85782561d8dbb69f988d653f8dbd3c
SUNsung <weatherius@gmail.com>
2018-06-17 11:28:49 +0300

    Rewrite [24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb] File in: 2018-06-17 11:28:48

--- ruby/24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb
+++ ruby/24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb
@@ -1,138 +1,81 @@
 
         
-          def app_caveats
-    if keg && keg.app_installed?
-      <<-EOS.undent
-        .app bundles were installed.
-        Run `brew linkapps #{keg.name}` to symlink these to /Applications.
-      EOS
-    end
-  end
-    
-        # Many formulae include 'lib/charset.alias', but it is not strictly needed
-    # and will conflict if more than one formula provides it
-    observe_file_removal @f.lib/'charset.alias'
-    
-      def internal_commands
-    find_internal_commands HOMEBREW_LIBRARY_PATH/'cmd'
-  end
-    
-      def llvm
-    @llvm ||= MacOS.llvm_build_version if MacOS.has_apple_developer_tools?
-  end
-    
-          out = checks.send(method)
-      unless out.nil? || out.empty?
-        if first_warning
-          $stderr.puts <<-EOS.undent
-            #{Tty.white}Please note that these warnings are just used to help the Homebrew maintainers
-            with debugging if you file an issue. If everything you use Homebrew for is
-            working fine: please don't worry and just ignore them. Thanks!#{Tty.reset}
-          EOS
-        end
-    
-      def dump_formula_report(key, title)
-    formulae = select_formula(key).sort.map do |name, new_name|
-      # Format list items of renamed formulae
-      if key == :R
-        name = pretty_installed(name) if installed?(name)
-        new_name = pretty_installed(new_name) if installed?(new_name)
-        '#{name} -> #{new_name}'
-      else
-        installed?(name) ? pretty_installed(name) : name
-      end
+              topic.feature_topic_users(args)
     end
     
-      # Use this method to generate standard caveats.
-  def standard_instructions(home_name, home_value = libexec)
-    <<-EOS.undent
-      Before you can use these tools you must export some variables to your $SHELL.
-    
-          html_filters.push 'coffeescript/clean_html_v1', 'coffeescript/entries_v1', 'title'
-    
-        version '8 LTS' do
-      self.release = '8.11.1'
-      self.base_url = 'https://nodejs.org/dist/latest-v8.x/docs/api/'
-    end
-    
-        options[:max_image_size] = 300_000
-    options[:container] = '.devsite-main-content'
-    
-        private
-    
-      def test_chmod_m17n
-    bug5671 = '[ruby-dev:44898]'
-    Dir.mktmpdir('test-file-chmod-m17n-') do |tmpdir|
-      file = File.join(tmpdir, '\u3042')
-      File.open(file, 'w'){}
-      assert_equal(File.chmod(0666, file), 1, bug5671)
-    end
+      def set_permission(permission_name, value)
+    self.send('#{permission_name}=', value)
+    save_and_refresh_staff_groups!
   end
     
-      def test_eql
-    assert_not_send([@cls[], :eql?, 0])
-    o = Object.new
-    o.instance_variable_set(:@cls, @cls)
-    def o.to_hash; @cls[]; end
-    def o.eql?(x); true; end
-    assert_send([@cls[], :eql?, o])
-    def o.eql?(x); false; end
-    assert_not_send([@cls[], :eql?, o])
+    class TC_Enumerable < Test::Unit::TestCase
+  def test_to_set
+    ary = [2,5,4,3,2,1,3]
+    
+      def test_strftime_flags
+    t = Time.mktime(2001, 10, 1, 2, 0, 0)
+    assert_equal('01', t.strftime('%d'))
+    assert_equal('01', t.strftime('%0d'))
+    assert_equal(' 1', t.strftime('%_d'))
+    assert_equal(' 1', t.strftime('%e'))
+    assert_equal('01', t.strftime('%0e'))
+    assert_equal(' 1', t.strftime('%_e'))
+    assert_equal('AM', t.strftime('%p'))
+    assert_equal('am', t.strftime('%#p'))
+    assert_equal('am', t.strftime('%P'))
+    assert_equal('AM', t.strftime('%#P'))
+    assert_equal('02', t.strftime('%H'))
+    assert_equal('02', t.strftime('%0H'))
+    assert_equal(' 2', t.strftime('%_H'))
+    assert_equal('02', t.strftime('%I'))
+    assert_equal('02', t.strftime('%0I'))
+    assert_equal(' 2', t.strftime('%_I'))
+    assert_equal(' 2', t.strftime('%k'))
+    assert_equal('02', t.strftime('%0k'))
+    assert_equal(' 2', t.strftime('%_k'))
+    assert_equal(' 2', t.strftime('%l'))
+    assert_equal('02', t.strftime('%0l'))
+    assert_equal(' 2', t.strftime('%_l'))
+    t = Time.mktime(2001, 10, 1, 14, 0, 0)
+    assert_equal('PM', t.strftime('%p'))
+    assert_equal('pm', t.strftime('%#p'))
+    assert_equal('pm', t.strftime('%P'))
+    assert_equal('PM', t.strftime('%#P'))
+    assert_equal('14', t.strftime('%H'))
+    assert_equal('14', t.strftime('%0H'))
+    assert_equal('14', t.strftime('%_H'))
+    assert_equal('02', t.strftime('%I'))
+    assert_equal('02', t.strftime('%0I'))
+    assert_equal(' 2', t.strftime('%_I'))
+    assert_equal('14', t.strftime('%k'))
+    assert_equal('14', t.strftime('%0k'))
+    assert_equal('14', t.strftime('%_k'))
+    assert_equal(' 2', t.strftime('%l'))
+    assert_equal('02', t.strftime('%0l'))
+    assert_equal(' 2', t.strftime('%_l'))
+    assert_equal('MON', t.strftime('%^a'))
+    assert_equal('OCT', t.strftime('%^b'))
+    
+      it 'does not decode an int when fewer bytes than an int remain and the '*' modifier is passed' do
+    'abc'.unpack(unpack_format('*')).should == []
   end
     
-      it 'implicitly has a count of one when no count is specified' do
-    'abc'.unpack(unpack_format).should == ['a']
-  end
-    
-      it 'creates a time based on given C-style gmtime arguments, interpreted as UTC (GMT)' do
-    time = Time.send(@method, 1, 15, 20, 1, 1, 2000, :ignored, :ignored, :ignored, :ignored)
-    time.inspect.should == '2000-01-01 20:15:01 UTC'
-  end
-    
-    def usage
-  <<-EOS
-Usage: list_login_items_for_app <path.app>
-    
-    def report_apps
-  running = Set.new
-  @app_names.zip(@bundle_ids, @unix_ids).each do |app_name, bundle_id, _unix_id|
-    next if excluded_bundle_id bundle_id
-    next if excluded_app_name app_name
-    bundle_id.gsub!(%r{^(missing value)$}, '<\1>')
-    running.add '#{bundle_id}\t#{app_name}'
-  end
-    
-      module Sass::Plugin::Configuration
-    # Different default options in a m environment.
-    def default_options
-      @default_options ||= begin
-        version = Merb::VERSION.split('.').map {|n| n.to_i}
-        if version[0] <= 0 && version[1] < 5
-          root = MERB_ROOT
-          env  = MERB_ENV
-        else
-          root = Merb.root.to_s
-          env  = Merb.environment
-        end
-    
-    module Sass
-  # Runs a SassScript read-eval-print loop.
-  # It presents a prompt on the terminal,
-  # reads in SassScript expressions,
-  # evaluates them,
-  # and prints the result.
-  class Repl
-    # @param options [{Symbol => Object}] An options hash.
-    def initialize(options = {})
-      @options = options
-    end
-    
-    # The project root directory
-$root = ::File.dirname(__FILE__)
-    
-      def render(context)
-    config_tag(context.registers[:site].config, @key, @tag, @classname)
+      it 'ignores spaces between directives' do
+    '\x01\x02'.unpack('U U').should == [1, 2]
   end
 end
+
     
-    Liquid::Template.register_tag('include_array', Jekyll::IncludeArrayTag)
+          sleep
+      after_sleep2 = true
+    end
+    
+            def create
+          authorize! :create, StockLocation
+          @stock_location = StockLocation.new(stock_location_params)
+          if @stock_location.save
+            respond_with(@stock_location, status: 201, default_template: :show)
+          else
+            invalid_resource!(@stock_location)
+          end
+        end
