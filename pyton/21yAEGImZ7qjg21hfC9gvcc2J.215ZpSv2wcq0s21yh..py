b9848a80484bfaa152f35c4bfabad18fd6802dbc
SUNsung <weatherius@gmail.com>
2018-06-17 22:13:36 +0300

    Rewrite [21yAEGImZ7qjg21hfC9gvcc2J.215ZpSv2wcq0s21yh..py] File in: 2018-06-17 22:13:35

--- pyton/21yAEGImZ7qjg21hfC9gvcc2J.215ZpSv2wcq0s21yh..py
+++ pyton/21yAEGImZ7qjg21hfC9gvcc2J.215ZpSv2wcq0s21yh..py
@@ -1,145 +1,141 @@
 
-@csrf_protect
-def render_flatpage(request, f):
-    '''
-    Internal interface to the flat page view.
-    '''
-    # If registration is required for accessing this page, and the user isn't
-    # logged in, redirect to the login page.
-    if f.registration_required and not request.user.is_authenticated:
-        from django.contrib.auth.views import redirect_to_login
-        return redirect_to_login(request.path)
-    if f.template_name:
-        template = loader.select_template((f.template_name, DEFAULT_TEMPLATE))
-    else:
-        template = loader.get_template(DEFAULT_TEMPLATE)
-    
-        for n_samples in sample_sizes:
-        X = random_state.rand(n_samples, 300)
-    
-    for i, n in enumerate(n_samples):
-    for j, p in enumerate(n_features):
-        X = np.random.normal(size=(n, p))
-        t0 = time.time()
-        ward.fit(X)
-        scikits_time[j, i] = time.time() - t0
-        t0 = time.time()
-        hierarchy.ward(X)
-        scipy_time[j, i] = time.time() - t0
-    
-        scikit_classifier_results.append(
-        delta.seconds + delta.microseconds / mu_second)
-    
-        print('Decompressing %s' % ARCHIVE_NAME)
-    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
-        archive.extractall(path='.')
-    os.remove(ARCHIVE_NAME)
+        
+        
+if __name__ == '__main__':
+    main()
 
     
-        output_file.close()
+    if hasattr(ctypes, 'windll'):
+    WSAStringToAddressA = ctypes.windll.ws2_32.WSAStringToAddressA
+    WSAAddressToStringA = ctypes.windll.ws2_32.WSAAddressToStringA
+else:
+    def not_windows():
+        raise SystemError(
+            'Invalid platform. ctypes.windll must be available.'
+        )
+    WSAStringToAddressA = not_windows
+    WSAAddressToStringA = not_windows
+    
+    class MismatchedSetException(RecognitionException):
+    '''@brief The next token does not match a set of expected types.'''
+    
+    class Token(object):
+    '''@brief Abstract token baseclass.'''
+    
+        def test_car_shall_make_very_loud_noise(self):
+        noise = self.car.make_noise(10)
+        expected_noise = 'vroom!!!!!!!!!!'
+        self.assertEqual(noise, expected_noise)
+    
+        @classmethod
+    def tearDownClass(self):
+        '''
+        Remove the temporary directory /test_command and its content.
+        '''
+        shutil.rmtree('tests/test_command')
 
     
-    The outer product of the row and column label vectors shows a
-representation of the checkerboard structure.
+        '''def test_object(queue):
+           queue_object = QueueObject(queue, True)
+           print('Inside func: {}'.format(queue_object.object))'''
     
-    def kugou_download_by_hash(title,hash_val,output_dir = '.', merge = True, info_only = False):
-    #sample
-    #url_sample:http://www.kugou.com/yy/album/single/536957.html
-    #hash ->key  md5(hash+kgcloud')->key  decompile swf
-    #cmd 4 for mp3 cmd 3 for m4a
-    key=hashlib.new('md5',(hash_val+'kgcloud').encode('utf-8')).hexdigest()
-    html=get_html('http://trackercdn.kugou.com/i/?pid=6&key=%s&acceptMp3=1&cmd=4&hash=%s'%(key,hash_val))
-    j=loads(html)
-    url=j['url']
-    songtype, ext, size = url_info(url)
-    print_info(site_info, title, songtype, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge=merge)
+        def test_cat_eng_localization(self):
+        self.assertEqual(self.e.get('cat'), 'cat')
     
-        url = sorted(
-        map(lambda x: x.firstChild.nodeValue, xml.getElementsByTagName('src')),
-        key=lambda x: int(match1(x, r'_(\d+?)_')))[-1]
     
-        def entry_ok(self):  # Example: usually replace.
-        'Return non-blank entry or None.'
-        self.entry_error['text'] = ''
-        entry = self.entry.get().strip()
-        if not entry:
-            self.showerror('blank line.')
-            return None
-        return entry
+class TestRunnerFacilities(unittest.TestCase):
     
-        def test_next_until_return_in_generator(self):
-        # Issue #16596.
-        # Check that set_next(), set_until() and set_return() do not treat the
-        # `yield` and `yield from` statements as if they were returns and stop
-        # instead in the current frame.
-        code = '''
-            def test_gen():
-                yield 0
-                lno = 4
-                return 123
+        def test_display_current_time_at_midnight(self):
+        '''
+        Would almost always fail (despite of right at/after midnight) if
+        untestable production code would have been used.
+        '''
+        time_provider_stub = MidnightTimeProvider()
+        class_under_test = TimeDisplay()
+        class_under_test.set_time_provider(time_provider_stub)
+        expected_time = '<span class=\'tinyBoldText\'>24:01</span>'
+        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
     
-        def __init__(self):
-        self.byteStream = None
-        self.characterStream = None
-        self.stringData = None
-        self.encoding = None
-        self.publicId = None
-        self.systemId = None
-        self.baseURI = None
+        def update(self, subject):
+        print(u'DecimalViewer: Subject %s has data %d' %
+              (subject.name, subject.data))
     
-                for i in range(10):
-                event = threading.Event()
-                threads = [threading.Thread(target=run) for x in range(2)]
-                try:
-                    with test.support.start_threads(threads, event.set):
-                        time.sleep(0)
-                finally:
-                    sys.modules.pop('package', None)
-                    sys.modules.pop('package.submodule', None)
-                if exc is not None:
-                    raise exc
-        finally:
-            del sys.path[0]
+    from tornado.options import define, options
     
-    T_CV2 = ClassVar[int]
-T_CV3 = ClassVar
+        @classmethod
+    def _restore_configuration(cls, saved):
+        # type: (Tuple[type, Dict[str, Any]]) -> None
+        base = cls.configurable_base()
+        base.__impl_class = saved[0]
+        base.__impl_kwargs = saved[1]
     
-        result = False
-    try:
-        ret = urlopen(url, timeout=2)
-        result = (ret.code == 200)
-    except HTTPError as e:
-        print(e, file=sys.stderr)
-    except URLError as e:
-        print(e, file=sys.stderr)
-    except timeout as e:
-        print(e, file=sys.stderr)
-    except Exception as e:
-        print(e, file=sys.stderr)
+        def __init__(self, client, request, release_callback,
+                 final_callback, max_buffer_size, tcp_client,
+                 max_header_size, max_body_size):
+        self.io_loop = IOLoop.current()
+        self.start_time = self.io_loop.time()
+        self.start_wall_time = time.time()
+        self.client = client
+        self.request = request
+        self.release_callback = release_callback
+        self.final_callback = final_callback
+        self.max_buffer_size = max_buffer_size
+        self.tcp_client = tcp_client
+        self.max_header_size = max_header_size
+        self.max_body_size = max_body_size
+        self.code = None
+        self.headers = None
+        self.chunks = []
+        self._decompressor = None
+        # Timeout handle returned by IOLoop.add_timeout
+        self._timeout = None
+        self._sockaddr = None
+        IOLoop.current().add_callback(self.run)
     
-        @property
-    def device_state_attributes(self):
-        '''Return the state attributes of the monitored installation.'''
-        if self.pvcoutput is not None:
-            return {
-                ATTR_ENERGY_GENERATION: self.pvcoutput.energy_generation,
-                ATTR_POWER_GENERATION: self.pvcoutput.power_generation,
-                ATTR_ENERGY_CONSUMPTION: self.pvcoutput.energy_consumption,
-                ATTR_POWER_CONSUMPTION: self.pvcoutput.power_consumption,
-                ATTR_EFFICIENCY: self.pvcoutput.efficiency,
-                ATTR_TEMPERATURE: self.pvcoutput.temperature,
-                ATTR_VOLTAGE: self.pvcoutput.voltage,
-            }
+        def __delitem__(self, name):
+        norm_name = _normalized_headers[name]
+        del self._dict[norm_name]
+        del self._as_list[norm_name]
     
-    SERVICE_BROWSE_URL_SCHEMA = vol.Schema({
-    # pylint: disable=no-value-for-parameter
-    vol.Required(ATTR_URL, default=ATTR_URL_DEFAULT): vol.Url(),
-})
+        @gen_test
+    def asyncSetUp(self):
+        listener, port = bind_unused_port()
+        event = Event()
     
-        hass.services.register(DOMAIN, SERVICE_MEDIA_PREVIOUS_TRACK,
-                           lambda service:
-                           keyboard.tap_key(keyboard.media_prev_track_key),
-                           schema=TAP_KEY_SCHEMA)
-    return True
+        @gen_test
+    def test_put_clears_timed_out_getters(self):
+        q = queues.Queue()
+        getters = [q.get(timedelta(seconds=0.01)) for _ in range(10)]
+        get = q.get()
+        q.get()
+        self.assertEqual(12, len(q._getters))
+        yield gen.sleep(0.02)
+        self.assertEqual(12, len(q._getters))
+        self.assertFalse(get.done())  # Final waiters still active.
+        q.put(0)  # put() clears the waiters.
+        self.assertEqual(1, len(q._getters))
+        self.assertEqual(0, (yield get))
+        for getter in getters:
+            self.assertRaises(TimeoutError, getter.result)
+    
+        Understands both `asyncio.Future` and Tornado's extensions to
+    enable better tracebacks on Python 2.
+    
+        def start(self):
+        if self._running:
+            raise RuntimeError('IOLoop is already running')
+        if os.getpid() != self._pid:
+            raise RuntimeError('Cannot share PollIOLoops across processes')
+        self._setup_logging()
+        if self._stopped:
+            self._stopped = False
+            return
+        old_current = IOLoop.current(instance=False)
+        if old_current is not self:
+            self.make_current()
+        self._thread_ident = thread.get_ident()
+        self._running = True
+    
+        def test_coroutine_unfinished_sequence_handler(self):
+        response = self.fetch('/coroutine_unfinished_sequence')
+        self.assertEqual(response.body, b'123')
