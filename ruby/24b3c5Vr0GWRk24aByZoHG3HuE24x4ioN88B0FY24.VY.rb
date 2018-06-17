7c47ad39dceac57b027ca9807dae798a4ccab59d
SUNsung <weatherius@gmail.com>
2018-06-17 22:31:23 +0300

    Rewrite [24b3c5Vr0GWRk24aByZoHG3HuE24x4ioN88B0FY24.VY.rb] File in: 2018-06-17 22:31:22

--- ruby/24b3c5Vr0GWRk24aByZoHG3HuE24x4ioN88B0FY24.VY.rb
+++ ruby/24b3c5Vr0GWRk24aByZoHG3HuE24x4ioN88B0FY24.VY.rb
@@ -1,118 +1,42 @@
-        expect(result).to eq('svn info | grep Revision | egrep -o '[0-9]+'')
-        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('svn info | grep Revision | egrep -o '[0-9]+'')
-      end
+
+        
+        module ActionCable
+  module Server
+    # An instance of this configuration object is available via ActionCable.server.config, which allows you to tweak Action Cable configuration
+    # in a Rails config initializer.
+    class Configuration
+      attr_accessor :logger, :log_tags
+      attr_accessor :connection_class, :worker_pool_size
+      attr_accessor :disable_request_forgery_protection, :allowed_request_origins, :allow_same_origin_as_host
+      attr_accessor :cable, :url, :mount_path
+    
+      test 'overriding has_many_attached methods works' do
+    # attach blobs before messing with getter, which breaks `#attach`
+    @user.highlights.attach create_blob(filename: 'funky.jpg'), create_blob(filename: 'wonky.jpg')
+    
+            def integer_like_primary_key_type(type, options)
+          type
+        end
     end
     
-          it 'returns the new version as return value' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          increment_version_number(bump_type: 'major')
-        end').runner.execute(:test)
-    
-      def validate_type
-    errors.add(:type, 'cannot be changed once an instance has been created') if type_changed? && !new_record?
-    errors.add(:type, 'is not a valid type') unless self.class.valid_type?(type)
+        require '#{app_path}/config/environment'
   end
     
-        if params[:sort].present?
-      attribute, direction = params[:sort].downcase.split('.')
-      unless valid_sorts.include?(attribute)
-        attribute, direction = default.to_a.first
-      end
-    else
-      attribute, direction = default.to_a.first
-    end
-    
-      def reemit
-    @event.reemit!
-    respond_to do |format|
-      format.html { redirect_back event_path(@event), notice: 'Event re-emitted.' }
-    end
-  end
-    
-        respond_to do |format|
-      if !running? && @job.destroy
-        format.html { redirect_to jobs_path, notice: 'Job deleted.' }
-        format.json { head :no_content }
-      else
-        format.html { redirect_to jobs_path, alert: 'Can not delete a running job.' }
-        format.json { render json: @job.errors, status: :unprocessable_entity }
+          respond_with do |format|
+        format.html { redirect_to admin_pods_path }
+        format.json { render json: PodPresenter.new(pod).as_json }
       end
     end
   end
+end
+
     
-            # Find a user by its confirmation token and try to confirm it.
-        # If no user is found, returns a new user with an error.
-        # If the user is already confirmed, create an error for the user
-        # Options must have the confirmation_token
-        def confirm_by_token(confirmation_token)
-          confirmable = find_first_by_auth_conditions(confirmation_token: confirmation_token)
-          unless confirmable
-            confirmation_digest = Devise.token_generator.digest(self, :confirmation_token, confirmation_token)
-            confirmable = find_or_initialize_with_error_by(:confirmation_token, confirmation_digest)
-          end
-    
-      protected
-    
-        def unlock_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :unlock_instructions, opts)
-    end
-    
-          # Remembers the given resource by setting up a cookie
-      def remember_me(resource)
-        return if request.env['devise.skip_storage']
-        scope = Devise::Mapping.find_scope!(resource)
-        resource.remember_me!
-        cookies.signed[remember_key(resource, scope)] = remember_cookie_values(resource)
+          rescue_from OpenIDConnect::ValidationFailed,
+                  ActiveRecord::RecordInvalid, Api::OpenidConnect::Error::InvalidSectorIdentifierUri do |e|
+        validation_fail_as_json(e)
       end
     
-            # Removes the specified cache
-        #
-        # @param [Array<Hash>] cache_descriptors
-        #        An array of caches to remove, each specified with the same
-        #        hash as cache_descriptors_per_pod especially :spec_file and :slug
-        #
-        def remove_caches(cache_descriptors)
-          cache_descriptors.each do |desc|
-            UI.message('Removing spec #{desc[:spec_file]} (v#{desc[:version]})') do
-              FileUtils.rm(desc[:spec_file])
-            end
-            UI.message('Removing cache #{desc[:slug]}') do
-              FileUtils.rm_rf(desc[:slug])
-            end
-          end
-        end
-    
-          def initialize(argv)
-        @update = argv.flag?('update')
-        @stats  = argv.flag?('stats')
-        super
-      end
-    
-            def responds?
-          methods = @subject.instance_methods.map(&:to_s)
-          methods.include?('#{@attachment_name}') &&
-            methods.include?('#{@attachment_name}=') &&
-            methods.include?('#{@attachment_name}?')
-        end
-    
-            def error_when_not_valid?
-          @subject.send(@attachment_name).assign(nil)
-          @subject.valid?
-          @subject.errors[:'#{@attachment_name}'].present?
-        end
-    
-              @subject.send(@attachment_name).post_processing = false
-          @subject.send(@attachment_name).assign(file)
-          @subject.valid?
-          @subject.errors[:'#{@attachment_name}_file_size'].blank?
-        ensure
-          @subject.send(@attachment_name).post_processing = true
-        end
-    
-            if record.errors.include? attribute
-          record.errors[attribute].each do |error|
-            record.errors.add base_attribute, error
-          end
-        end
-      end
+    module Api
+  module OpenidConnect
+    class TokenEndpointController < ApplicationController
+      skip_before_action :verify_authenticity_token
