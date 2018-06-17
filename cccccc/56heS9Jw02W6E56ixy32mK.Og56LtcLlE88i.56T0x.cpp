bf8e7d5808ef7595060fe0adc117287c37cd1e4d
SUNsung <weatherius@gmail.com>
2018-06-17 06:29:46 +0300

    Rewrite [56heS9Jw02W6E56ixy32mK.Og56LtcLlE88i.56T0x.cpp] File in: 2018-06-17 06:29:44

--- cccccc/56heS9Jw02W6E56ixy32mK.Og56LtcLlE88i.56T0x.cpp
+++ cccccc/56heS9Jw02W6E56ixy32mK.Og56LtcLlE88i.56T0x.cpp
@@ -1,146 +1,291 @@
 
         
-        @interface LocaleBridgingTester : NSObject
-- (NSLocale *)autoupdatingCurrentLocale;
-- (BOOL)verifyAutoupdatingLocale:(NSLocale *)locale;
-@end
+        Base::~Base() {
+}
     
-      llvm::SMDiagnostic GetMessage(SourceLoc Loc, llvm::SourceMgr::DiagKind Kind,
-                                const Twine &Msg,
-                                ArrayRef<llvm::SMRange> Ranges,
-                                ArrayRef<llvm::SMFixIt> FixIts) const;
     
-    using namespace swift;
+    {}
     
-    #ifndef SWIFT_INDEX_INDEX_H
-#define SWIFT_INDEX_INDEX_H
+    #endif //CONTENT_NW_SRC_API_EVENT_EVENT_H_
+
     
-      auto iov = buf->getIov();
-  EXPECT_EQ(count, iov.size());
+    #include 'content/nw/src/api/menu/menu_delegate.h'
     
-    template <typename C>
-struct compare_greater : detail::cmp_pred<C, ordering::gt, 0> {
-  using detail::cmp_pred<C, ordering::gt, 0>::cmp_pred;
+    bool NwMenuModel::HasIcons() const {
+  // Always return false, see the comment about |NwMenuModel|.
+  return false;
+}
+    
+    void MenuItem::CallSync(const std::string& method,
+                        const base::ListValue& arguments,
+                        base::ListValue* result) {
+  if (method == 'GetChecked') {
+    result->AppendBoolean(GetChecked());
+  } else {
+    NOTREACHED() << 'Invalid call to MenuItem method:' << method
+                 << ' arguments:' << arguments;
+  }
+}
+    
+        bool WriteHTML(ClipboardData& data) {
+      DCHECK(data.type == TYPE_HTML);
+      scw_->WriteHTML(base::UTF8ToUTF16(*(data.data)), std::string());
+      return true;
+    }
+    
+    #include 'extensions/browser/extension_function.h'
+    
+    class NwObjAllocateIdFunction : public NWSyncExtensionFunction {
+ public:
+  NwObjAllocateIdFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9, typename T10,
+    typename T11>
+internal::ValueArray11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10,
+    T11> Values(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9,
+    T10 v10, T11 v11) {
+  return internal::ValueArray11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10,
+      T11>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11);
+}
+    
+    
+    {  GTEST_DISALLOW_COPY_AND_ASSIGN_(HasNewFatalFailureHelper);
 };
     
-     public:
-  HazptrLockFreeLIFO() : head_(nullptr) {}
+     private:
+  friend class TestCase;
+  friend class TestInfo;
+  friend class internal::DefaultGlobalTestPartResultReporter;
+  friend class internal::NoExecDeathTest;
+  friend class internal::TestEventListenersAccessor;
+  friend class internal::UnitTestImpl;
     
-    namespace folly {
+    // Returns a newly created InternalRunDeathTestFlag object with fields
+// initialized from the GTEST_FLAG(internal_run_death_test) flag if
+// the flag is specified; otherwise returns NULL.
+InternalRunDeathTestFlag* ParseInternalRunDeathTestFlag();
+    
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9, typename T10,
+    typename T11, typename T12, typename T13, typename T14, typename T15,
+    typename T16, typename T17, typename T18, typename T19, typename T20,
+    typename T21, typename T22, typename T23, typename T24, typename T25,
+    typename T26, typename T27, typename T28, typename T29, typename T30,
+    typename T31, typename T32, typename T33, typename T34, typename T35,
+    typename T36, typename T37, typename T38, typename T39, typename T40,
+    typename T41, typename T42, typename T43, typename T44, typename T45>
+class ValueArray45 {
+ public:
+  ValueArray45(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9,
+      T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15, T16 v16, T17 v17,
+      T18 v18, T19 v19, T20 v20, T21 v21, T22 v22, T23 v23, T24 v24, T25 v25,
+      T26 v26, T27 v27, T28 v28, T29 v29, T30 v30, T31 v31, T32 v32, T33 v33,
+      T34 v34, T35 v35, T36 v36, T37 v37, T38 v38, T39 v39, T40 v40, T41 v41,
+      T42 v42, T43 v43, T44 v44, T45 v45) : v1_(v1), v2_(v2), v3_(v3), v4_(v4),
+      v5_(v5), v6_(v6), v7_(v7), v8_(v8), v9_(v9), v10_(v10), v11_(v11),
+      v12_(v12), v13_(v13), v14_(v14), v15_(v15), v16_(v16), v17_(v17),
+      v18_(v18), v19_(v19), v20_(v20), v21_(v21), v22_(v22), v23_(v23),
+      v24_(v24), v25_(v25), v26_(v26), v27_(v27), v28_(v28), v29_(v29),
+      v30_(v30), v31_(v31), v32_(v32), v33_(v33), v34_(v34), v35_(v35),
+      v36_(v36), v37_(v37), v38_(v38), v39_(v39), v40_(v40), v41_(v41),
+      v42_(v42), v43_(v43), v44_(v44), v45_(v45) {}
     }
     
-    /**
- * Declare *allocx() and mallctl*() as weak symbols. These will be provided by
- * jemalloc if we are using jemalloc, or will be nullptr if we are using another
- * malloc implementation.
+        void ComputeCurrentValue() {
+      if (!AtEnd())
+        current_value_ = ParamType($for j, [[*current$(j)_]]);
+    }
+    bool AtEnd() const {
+      // We must report iterator past the end of the range when either of the
+      // component iterators has reached the end of its range.
+      return
+$for j  || [[
+    }
+    
+      // Converts a wide C string to a String using the UTF-8 encoding.
+  // NULL will be converted to '(null)'.  If an error occurred during
+  // the conversion, '(failed to convert from wide string)' is
+  // returned.
+  static std::string ShowWideCString(const wchar_t* wide_c_str);
+    
+    int main(int argc, char **argv) {
+  InitGoogleTest(&argc, argv);
+    }
+    
+    namespace HPHP {
+    }
+    
+    Variant HHVM_FUNCTION(xhprof_network_disable) {
+  return ServerStats::EndNetworkProfile();
+}
+    
+    int64_t Timer::measure() const {
+  if (m_type == WallTime) {
+    return GetCurrentTimeMicros();
+  }
+    }
+    
+    /*
+ * Timing execution of block of codes.
  */
-extern 'C' void* mallocx(size_t, int)
-__attribute__((__weak__));
-extern 'C' void* rallocx(void*, size_t, int)
-__attribute__((__weak__));
-extern 'C' size_t xallocx(void*, size_t, size_t, int)
-__attribute__((__weak__));
-extern 'C' size_t sallocx(const void*, int)
-__attribute__((__weak__));
-extern 'C' void dallocx(void*, int)
-__attribute__((__weak__));
-extern 'C' void sdallocx(void*, size_t, int)
-__attribute__((__weak__));
-extern 'C' size_t nallocx(size_t, int)
-__attribute__((__weak__));
-extern 'C' int mallctl(const char*, void*, size_t*, void*, size_t)
-__attribute__((__weak__));
-extern 'C' int mallctlnametomib(const char*, size_t*, size_t*)
-__attribute__((__weak__));
-extern 'C' int mallctlbymib(const size_t*, size_t, void*, size_t*, void*,
-                            size_t)
-__attribute__((__weak__));
-    
-    int main(int argc, char* argv[]) {
-  // Using log macros before calling folly::initLogging() will use the default
-  // log settings defined by folly::initializeLoggerDB().  The default behavior
-  // is to log WARNING+ messages to stderr.
-  XLOG(INFO) << 'log messages less than WARNING will be ignored';
-  XLOG(ERR) << 'error messages before initLogging() will be logged to stderr';
+struct Timer {
+  enum Type {
+    WallTime,
+    SystemCPU,
+    UserCPU,
+    TotalCPU,
+  };
+  enum ReportType {
+    Log,
+    Stderr,
+    Trace,
+  };
+  enum Who {
+    Self = RUSAGE_SELF,
+    Children = RUSAGE_CHILDREN,
+#ifdef RUSAGE_THREAD
+    Thread = RUSAGE_THREAD,
+#endif
+  };
     }
     
-        size_t threadID = 0;
-    size_t messageIndex = 0;
-    try {
-      parseMessage(msg, &threadID, &messageIndex);
-    } catch (const std::exception& ex) {
-      ++numUnableToParse_;
-      XLOG(ERR, 'unable to parse log message: ', msg);
-      return;
+      void setAsioEvent(PageletServerTaskEvent *event);
+    
+    #include 'hphp/runtime/vm/jit/vasm-unit.h'
+    
+        mpz_clear(gmpData);
+  } else {
+    if (data.toInt64() < 0) {
+      raise_warning(cs_GMP_INVALID_VALUE_MUST_BE_POSITIVE, 'gmp_fact');
+      return false;
     }
     
-    #include <folly/executors/SerialExecutor.h>
+      /**
+   * The main process is not the (direct) parent of the worker process,
+   * and therefore it has to delegate to the shadow process to do waitpid.
+   * There can be a timeout (in seconds), after which SIGKILL is sent to
+   * the child process.
+   */
+  static pid_t waitpid(pid_t pid, int *stat_loc, int options, int timeout = 0);
     
-      KeepAlive<Executor> parent_;
-  std::mutex mutex_;
-  std::size_t scheduled_{0};
-  std::queue<Func> queue_;
-    
-    void Benchmark::SetName(const char* name) { name_ = name; }
-    
-    #include <cstdarg>
-#include <iostream>
-#include <string>
-    
-      std::string env_var;
-  for (size_t i = 0; i != flag_str.length(); ++i)
-    env_var += static_cast<char>(::toupper(flag_str.c_str()[i]));
-    
-    // Find the coefficient for the high-order term in the running time, by
-// minimizing the sum of squares of relative error, for the fitting curve
-// given by the lambda expresion.
-//   - n             : Vector containing the size of the benchmark tests.
-//   - time          : Vector containing the times for the benchmark tests.
-//   - fitting_curve : lambda expresion (e.g. [](int n) {return n; };).
-    
-      PrintBasicContext(&GetErrorStream(), context);
-    
-    void Increment(UserCounters *l, UserCounters const& r) {
-  // add counters present in both or just in *l
-  for (auto &c : *l) {
-    auto it = r.find(c.first);
-    if (it != r.end()) {
-      c.second.value = c.second + it->second;
+    namespace HPHP {
+/////////////////////////////////////////////////////////////////////////////
     }
-  }
-  // add counters present in r, but not in *l
-  for (auto const &tc : r) {
-    auto it = l->find(tc.first);
-    if (it == l->end()) {
-      (*l)[tc.first] = tc.second;
+    
+      /*
+   * Threads are notified based on their priority. The priority is decided when
+   * calling wait().
+   */
+  enum Priority {
+    Highest,
+    High,
+    Normal,
+    Low
+  };
+  /**
+   * 'id' is an arbitrary number that locates the conditional variable to wait
+   * on.
+   */
+  void wait(int id, int q, Priority pri);
+  bool wait(int id, int q, Priority pri, long seconds); // false if timed out
+  bool wait(int id, int q, Priority pri, long seconds, long long nanosecs);
+  void notify();
+  void notifyAll();
+  void setNumGroups(int num_groups);
+    
+        // Render characters, setup ImFont and glyphs for runtime
+    for (int input_i = 0; input_i < atlas->ConfigData.Size; input_i++)
+    {
+        ImFontConfig& cfg = atlas->ConfigData[input_i];
+        FreeTypeFont& font_face = fonts[input_i];
+        ImFont* dst_font = cfg.DstFont;
+        if (cfg.MergeMode)
+            dst_font->BuildLookupTable();
     }
-  }
-}
+    
+            // Rendering
+        ImGui::Render();
+        IwGxSetColClear(clear_color.x * 255, clear_color.y * 255, clear_color.z * 255, clear_color.w * 255);
+        IwGxClear();
+        ImGui_Marmalade_RenderDrawData(ImGui::GetDrawData());
+        IwGxSwapBuffers();
+    
+    extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+        return true;
+    }
     
     
-    {  ss << std::scientific << std::setprecision(max_fractional_digits10) << value;
-  return ss.str();
-}
+    {            ImGui::Text('Application average %.3f ms/frame (%.1f FPS)', 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        }
     
-          // regerror returns the number of bytes necessary to null terminate
-      // the string, so we move that when assigning to error.
-      CHECK_NE(needed, 0);
-      error->assign(errbuf, needed - 1);
+        io.KeyMap[ImGuiKey_Tab] = ALLEGRO_KEY_TAB;
+    io.KeyMap[ImGuiKey_LeftArrow] = ALLEGRO_KEY_LEFT;
+    io.KeyMap[ImGuiKey_RightArrow] = ALLEGRO_KEY_RIGHT;
+    io.KeyMap[ImGuiKey_UpArrow] = ALLEGRO_KEY_UP;
+    io.KeyMap[ImGuiKey_DownArrow] = ALLEGRO_KEY_DOWN;
+    io.KeyMap[ImGuiKey_PageUp] = ALLEGRO_KEY_PGUP;
+    io.KeyMap[ImGuiKey_PageDown] = ALLEGRO_KEY_PGDN;
+    io.KeyMap[ImGuiKey_Home] = ALLEGRO_KEY_HOME;
+    io.KeyMap[ImGuiKey_End] = ALLEGRO_KEY_END;
+    io.KeyMap[ImGuiKey_Insert] = ALLEGRO_KEY_INSERT;
+    io.KeyMap[ImGuiKey_Delete] = ALLEGRO_KEY_DELETE;
+    io.KeyMap[ImGuiKey_Backspace] = ALLEGRO_KEY_BACKSPACE;
+    io.KeyMap[ImGuiKey_Space] = ALLEGRO_KEY_SPACE;
+    io.KeyMap[ImGuiKey_Enter] = ALLEGRO_KEY_ENTER;
+    io.KeyMap[ImGuiKey_Escape] = ALLEGRO_KEY_ESCAPE;
+    io.KeyMap[ImGuiKey_A] = ALLEGRO_KEY_A;
+    io.KeyMap[ImGuiKey_C] = ALLEGRO_KEY_C;
+    io.KeyMap[ImGuiKey_V] = ALLEGRO_KEY_V;
+    io.KeyMap[ImGuiKey_X] = ALLEGRO_KEY_X;
+    io.KeyMap[ImGuiKey_Y] = ALLEGRO_KEY_Y;
+    io.KeyMap[ImGuiKey_Z] = ALLEGRO_KEY_Z;
     
-    void SleepForMilliseconds(int milliseconds) {
-  SleepForMicroseconds(milliseconds * kNumMicrosPerMilli);
-}
+                ImGui::Text('Color widget:');
+            ImGui::SameLine(); ShowHelpMarker('Click on the colored square to open a color picker.\nCTRL+click on individual component to input value.\n');
+            ImGui::ColorEdit3('MyColor##1', (float*)&color, misc_flags);
     
-    double StatisticsMedian(const std::vector<double>& v) {
-  if (v.size() < 3) return StatisticsMean(v);
-  std::vector<double> partial;
-  // we need roundDown(count/2)+1 slots
-  partial.resize(1 + (v.size() / 2));
-  std::partial_sort_copy(v.begin(), v.end(), partial.begin(), partial.end());
-  // did we have odd number of samples?
-  // if yes, then the last element of partially-sorted vector is the median
-  // it no, then the average of the last two elements is the median
-  if(v.size() % 2 == 1)
-    return partial.back();
-  return (partial[partial.size() - 2] + partial[partial.size() - 1]) / 2.0;
-}
+    
+    {}  // namespace rocksdb
+    
+           * Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+       * Redistributions in binary form must reproduce the above
+   copyright notice, this list of conditions and the following disclaimer
+   in the documentation and/or other materials provided with the
+   distribution.
+    
+      // Reopen the database (the previous changes should persist / be remembered)
+  {
+    auto db = OpenDb('\n');
+    StringLists slists(db);
+    }
+    
+    #include 'db/log_format.h'
+#include 'rocksdb/slice.h'
+#include 'rocksdb/status.h'
+#include 'rocksdb/options.h'
+    
+      // Notify this table rep that it will no longer be added to. By default,
+  // does nothing.  After MarkReadOnly() is called, this table rep will
+  // not be written to (ie No more calls to Allocate(), Insert(),
+  // or any writes done directly to entries accessed through the iterator.)
+  virtual void MarkReadOnly() { }
+    
+    // Parses a string for an Int32 flag, in the form of
+// '--flag=value'.
+//
+// On success, stores the value of the flag in *value, and returns
+// true.  On failure, returns false without changing *value.
+bool ParseInt32Flag(const char* str, const char* flag, int32_t* value);
+    
+      Out << run.GetAdjustedRealTime() << ',';
+  Out << run.GetAdjustedCPUTime() << ',';
+    
+    #define REQUIRES_SHARED(...) \
+  THREAD_ANNOTATION_ATTRIBUTE__(requires_shared_capability(__VA_ARGS__))
