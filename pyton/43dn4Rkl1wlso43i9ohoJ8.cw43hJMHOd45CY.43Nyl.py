76726b339199d8cafaaf0b10c7db0a2e6107f971
SUNsung <weatherius@gmail.com>
2018-06-18 02:19:23 +0300

    Rewrite [43dn4Rkl1wlso43i9ohoJ8.cw43hJMHOd45CY.43Nyl.py] File in: 2018-06-18 02:19:23

--- pyton/43dn4Rkl1wlso43i9ohoJ8.cw43hJMHOd45CY.43Nyl.py
+++ pyton/43dn4Rkl1wlso43i9ohoJ8.cw43hJMHOd45CY.43Nyl.py
@@ -1,104 +1,83 @@
 
         
-            s.register(Tag2, index=None)
-    assert isinstance(s.order[-1], Tag2)
+            return strings
 
     
-        from inspect import getfullargspec as getargspec
-    from io import StringIO
+    def kuwo_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+    if 'www.kuwo.cn/yinyue' in url:
+        rid=match1(url,'yinyue/(\d+)')
+        kuwo_download_by_rid(rid,output_dir, merge, info_only)
+    else:
+        kuwo_playlist_download(url,output_dir,merge,info_only)
+    
+            print_info(site_info, title, type_, size_full)
+        if not info_only:
+            download_urls(url_list, title, ext, total_size=size_full, output_dir=output_dir, merge=merge, headers=fake_headers)
+    else:
+        raise NotImplementedError(flashvars)
+    
+                if point.name == 'p':
+                link = point.find('a')
+                if link is not None:
+                    link = clean_pdf_link(link.attrs['href'])
+                    ext = get_extension(link)
+                    print(ext)
+                    if not ext in forbidden_extensions:
+                        print(shorten_title(point.text) + ' (' + link + ')')
+                        try:
+                            name = clean_text(point.text.split('[' + ext + ']')[0])
+                            fullname = '.'.join((name, ext))
+                            if not os.path.exists('/'.join((current_directory, fullname)) ):
+                                download_pdf(link, current_directory, '.'.join((name, ext)))
+                        except KeyboardInterrupt:
+                            try:
+                                print('Press Ctrl-C in 1 second to quit')
+                                time.sleep(1)
+                            except KeyboardInterrupt:
+                                print('Cancelling..')
+                                break
+                        except:
+                            failures.append(point.text)
+                        
+        point = point.next_sibling          
+    
+    import os
+import json
+import click
     
     
-class HashTable(object):
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    '''Set up the PVOutput sensor.'''
+    name = config.get(CONF_NAME)
+    api_key = config.get(CONF_API_KEY)
+    system_id = config.get(CONF_SYSTEM_ID)
+    method = 'GET'
+    payload = auth = None
+    verify_ssl = DEFAULT_VERIFY_SSL
+    headers = {
+        'X-Pvoutput-Apikey': api_key,
+        'X-Pvoutput-SystemId': system_id,
+    }
     
-        def possible_scores(self):
-        '''Return a list of possible scores, taking Aces into account.'''
-        pass
-    
-            When updating an entry, updates its position to the front of the LRU list.
-        If the entry is new and the cache is at capacity, removes the oldest entry
-        before the new entry is added.
-        '''
-        node = self.map[query]
-        if node is not None:
-            # Key exists in cache, update the value
-            node.results = results
-            self.linked_list.move_to_front(node)
-        else:
-            # Key does not exist in cache
-            if self.size == self.MAX_SIZE:
-                # Remove the oldest entry from the linked list and lookup
-                self.lookup.pop(self.linked_list.tail.query, None)
-                self.linked_list.remove_from_tail()
-            else:
-                self.size += 1
-            # Add the new key and value
-            new_node = Node(query, results)
-            self.linked_list.append_to_front(new_node)
-            self.lookup[query] = new_node
+                payload = {'host': 'http://host:8088/services/collector/event',
+                       'event': body}
+            self.handler_method(event)
+            self.assertEqual(self.mock_post.call_count, 1)
+            self.assertEqual(
+                self.mock_post.call_args,
+                mock.call(
+                    payload['host'], data=json.dumps(payload),
+                    headers={'Authorization': 'Splunk secret'},
+                    timeout=10
+                )
+            )
+            self.mock_post.reset_mock()
 
     
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer),
-            self.mr(mapper=self.mapper_sort,
-                    reducer=self.reducer_identity),
-        ]
     
-        def reject_friend_request(self, friend_id):
-        pass
-    
-            ttis = []
-        for i in range(tzh_typecnt):
-            ttis.append(ttinfo._make(struct.unpack('>lbb', fileobj.read(6))))
-    
-            This server does NOT support system.methodSignature.'''
-    
-    
-class SelectSelectorTestCase(BaseSelectorTestCase):
-    
-        def recolorize(self):
-        self.after_id = None
-        if not self.delegate:
-            if DEBUG: print('no delegate')
-            return
-        if not self.allow_colorizing:
-            if DEBUG: print('auto colorizing is off')
-            return
-        if self.colorizing:
-            if DEBUG: print('already colorizing')
-            return
-        try:
-            self.stop_colorizing = False
-            self.colorizing = True
-            if DEBUG: print('colorizing...')
-            t0 = time.perf_counter()
-            self.recolorize_main()
-            t1 = time.perf_counter()
-            if DEBUG: print('%.3f seconds' % (t1-t0))
-        finally:
-            self.colorizing = False
-        if self.allow_colorizing and self.tag_nextrange('TODO', '1.0'):
-            if DEBUG: print('reschedule colorizing')
-            self.after_id = self.after(1, self.recolorize)
-        if self.close_when_done:
-            top = self.close_when_done
-            self.close_when_done = None
-            top.destroy()
-    
-        parser = argparse.ArgumentParser(description = 'Download all the PDF/HTML links into README.md')
-    parser.add_argument('-d', action='store', dest='directory')
-    parser.add_argument('--no-html', action='store_true', dest='nohtml', default = False)
-    parser.add_argument('--overwrite', action='store_true', default = False)    
-    results = parser.parse_args()
-    
-        if not app.config.edit_on_github_project:
-        warnings.warn('edit_on_github_project not specified')
-        return
-    if not doctree:
-        warnings.warn('doctree is None')
-        return
-    path = os.path.relpath(doctree.get('source'), app.builder.srcdir)
-    show_url = get_github_url(app, 'blob', path)
-    edit_url = get_github_url(app, 'edit', path)
+def get_github_url(app, view, path):
+    github_fmt = 'https://github.com/{}/{}/{}/{}{}'
+    return (
+        github_fmt.format(app.config.edit_on_github_project, view,
+                          app.config.edit_on_github_branch,
+                          app.config.edit_on_github_src_path, path))
