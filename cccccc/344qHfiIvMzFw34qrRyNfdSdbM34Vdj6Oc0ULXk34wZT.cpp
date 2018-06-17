f4285597fee341bd656e6b620038d5f0ea91ad44
SUNsung <weatherius@gmail.com>
2018-06-17 19:47:21 +0300

    Rewrite [344qHfiIvMzFw34qrRyNfdSdbM34Vdj6Oc0ULXk34wZT.cpp] File in: 2018-06-17 19:47:20

--- cccccc/344qHfiIvMzFw34qrRyNfdSdbM34Vdj6Oc0ULXk34wZT.cpp
+++ cccccc/344qHfiIvMzFw34qrRyNfdSdbM34Vdj6Oc0ULXk34wZT.cpp
@@ -1,159 +1,244 @@
 
         
-          ArrayRef<MarkupASTNode *> getChildren() {
-    return {getTrailingObjects<MarkupASTNode *>(), NumChildren};
+        // Get basic type definitions.
+#define IPC_MESSAGE_IMPL
+#include 'content/nw/src/common/common_message_generator.h'
+    
+    #include 'base/basictypes.h'
+#include '../dispatcher_host.h'
+    
+    #endif  // CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
+
+    
+    
+    {}
+    
+    void MenuItem::Call(const std::string& method,
+                    const base::ListValue& arguments,
+                    content::RenderFrameHost* rvh) {
+  if (method == 'SetLabel') {
+    std::string label;
+    arguments.GetString(0, &label);
+    SetLabel(label);
+  } else if (method == 'SetIcon') {
+    std::string icon;
+    arguments.GetString(0, &icon);
+    SetIcon(icon);
+  } else if (method == 'SetIconIsTemplate') {
+    bool isTemplate;
+    arguments.GetBoolean(0, &isTemplate);
+    SetIconIsTemplate(isTemplate);
+  } else if (method == 'SetTooltip') {
+    std::string tooltip;
+    arguments.GetString(0, &tooltip);
+    SetTooltip(tooltip);
+  } else if (method == 'SetEnabled') {
+    bool enabled = true;
+    arguments.GetBoolean(0, &enabled);
+    SetEnabled(enabled);
+  } else if (method == 'SetChecked') {
+    bool checked = false;
+    arguments.GetBoolean(0, &checked);
+    SetChecked(checked);
+  } else if (method == 'SetSubmenu') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    SetSubmenu(object_manager()->GetApiObject<Menu>(object_id));
+#if defined(OS_MACOSX)
+  } else if (method == 'SetKey') {
+    std::string key;
+    arguments.GetString(0, &key);
+    SetKey(key);
+  } else if (method == 'SetModifiers') {
+    std::string mod;
+    arguments.GetString(0, &mod);
+    SetModifiers(mod);
+#endif
+  } else {
+    NOTREACHED() << 'Invalid call to MenuItem method:' << method
+                 << ' arguments:' << arguments;
   }
+}
     
-    #endif
+    #include <vector>
     
-    template <typename Dtype>
-class LayerRegistry {
- public:
-  typedef shared_ptr<Layer<Dtype> > (*Creator)(const LayerParameter&);
-  typedef std::map<string, Creator> CreatorRegistry;
-    }
+     protected:
+  ~NwObjAllocateIdFunction() override;
     
-      /**
-   * @brief Computes the Contrastive error gradient w.r.t. the inputs.
-   *
-   * Computes the gradients with respect to the two input vectors (bottom[0] and
-   * bottom[1]), but not the similarity label (bottom[2]).
-   *
-   * @param top output Blob vector (length 1), providing the error gradient with
-   *      respect to the outputs
-   *   -# @f$ (1 \times 1 \times 1 \times 1) @f$
-   *      This Blob's diff will simply contain the loss_weight* @f$ \lambda @f$,
-   *      as @f$ \lambda @f$ is the coefficient of this layer's output
-   *      @f$\ell_i@f$ in the overall Net loss
-   *      @f$ E = \lambda_i \ell_i + \mbox{other loss terms}@f$; hence
-   *      @f$ \frac{\partial E}{\partial \ell_i} = \lambda_i @f$.
-   *      (*Assuming that this top Blob is not used as a bottom (input) by any
-   *      other layer of the Net.)
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2)
-   *   -# @f$ (N \times C \times 1 \times 1) @f$
-   *      the features @f$a@f$; Backward fills their diff with
-   *      gradients if propagate_down[0]
-   *   -# @f$ (N \times C \times 1 \times 1) @f$
-   *      the features @f$b@f$; Backward fills their diff with gradients if
-   *      propagate_down[1]
+    #include 'caffe/proto/caffe.pb.h'
+#include 'caffe/util/db.hpp'
+#include 'caffe/util/format.hpp'
+    
+    
+    { protected:
+  /**
+   * @param bottom input Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$
+   * @param top output Blob vector (length 1)
+   *   -# @f$ (N \times 1 \times K) @f$ or, if out_max_val
+   *      @f$ (N \times 2 \times K) @f$ unless axis set than e.g.
+   *      @f$ (N \times K \times H \times W) @f$ if axis == 1
+   *      the computed outputs @f$
+   *       y_n = \arg\max\limits_i x_{ni}
+   *      @f$ (for @f$ K = 1 @f$).
    */
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  /// @brief Not implemented (non-differentiable function)
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+    NOT_IMPLEMENTED;
+  }
+  bool out_max_val_;
+  size_t top_k_;
+  bool has_axis_;
+  int axis_;
+};
+    
+    
+    {  int size_;
+  Dtype alpha_, beta_, k_;
+};
+#endif
+    
+    #include 'caffe/layers/neuron_layer.hpp'
+#include 'caffe/layers/relu_layer.hpp'
     
     
     {}  // namespace caffe
     
-    namespace caffe {
+        // ===================================================================
+    // SubminibatchHelpers -- helper for sub-minibatch implementation
+    // TODO: Can this just exist inside SGD.cpp?
+    // ===================================================================
+    
+            size_t varRank = outputVariable.Shape().Rank();
+        size_t maxSequenceLength = 1;
+        size_t numSequences = 1;
+        std::tie(maxSequenceLength, numSequences) = GetNumTimeStepsAndSequences(Shape().SubShape(varRank), outputVariable.DynamicAxes().size());
+    
+        mAsparse.SwitchToMatrixType(MatrixType::SPARSE, matrixFormatSparseCSR, true);
+    mBsparse.SwitchToMatrixType(MatrixType::SPARSE, matrixFormatSparseCSR, true);
+    Matrix<float>::ScaleAndAdd(alpha, mAsparse, mBsparse);
+    
+        // ProcessPassNDLScript - Process a pass of the NDL script
+    // script - NDL Script to process
+    // ndlPass - complete processing for this pass, all passes if ndlPassAll
+    // skipThrough - for iterative processing, skip through this node in the script (used for in-line MEL processing)
+    // fullValidate - validate as a complete network? (false if this might be a snippet of a full network)
+    // returns: last NDL node processed
+    NDLNode<ElemType>* ProcessPassNDLScript(NDLScript<ElemType>* script, NDLPass ndlPass, NDLNode<ElemType>* skipThrough = nullptr, bool fullValidate = false, const std::wstring& dumpFileName = L'')
+    {
+        if (ndlPass == ndlPassFinal)
+        {
+            // make sure to clear the caches so we pick up the new nodes
+            m_net->InvalidateCompiledNetwork();
+            // if requested then dump the nodes
+            // Note: This happens on the invalidated network.
+            if (dumpFileName != L'')
+                m_net->DumpAllNodesToFile(false, true, dumpFileName);
+        }
+        NDLNodeEvaluatorImpl<ElemType> ndlEvaluator(m_net);
+        NDLNode<ElemType>* lastNode = script->Evaluate(ndlEvaluator, L'', ndlPass, skipThrough);
+        if (ndlPass == ndlPassResolve)
+            SetOutputNodes(script);
+        return lastNode;
     }
     
-     protected:
-  /**
-   * @param bottom input Blob vector (length 2+)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs to be filtered @f$ x_1 @f$
-   *   -# ...
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs to be filtered @f$ x_K @f$
-   *   -# @f$ (N \times 1 \times 1 \times 1) @f$
-   *      the selector blob
-   * @param top output Blob vector (length 1+)
-   *   -# @f$ (S \times C \times H \times W) @f$ ()
-   *        the filtered output @f$ x_1 @f$
-   *        where S is the number of items
-   *        that haven't been filtered
-   *      @f$ (S \times C \times H \times W) @f$
-   *        the filtered output @f$ x_K @f$
-   *        where S is the number of items
-   *        that haven't been filtered
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-    const vector<Blob<Dtype>*>& top);
-    
-    namespace irgen {
+    namespace Microsoft { namespace MSR { namespace BS {
     }
-    
-    /*
- * Determine the next NUMA node according to state maintained in `curr_node`.
- */
-int next_numa_node(std::atomic_int& curr_node);
-/*
- * The number of numa nodes in the system
- */
-inline int num_numa_nodes() {
-  return use_numa ? numa_num_nodes : 1;
-}
-/*
- * Enable numa interleaving for the specified address range
- */
-void numa_interleave(void* start, size_t size);
-/*
- * Allocate the specified address range on the given node
- */
-void numa_bind_to(void* start, size_t size, int node);
-/*
- * Return true if node is part of the allowed set of numa nodes
- */
-inline bool numa_node_allowed(int node) {
-  if (numa_node_set == 0) return true;
-  return numa_node_set & (1u << node);
-}
-    
-    void OfflineCode::disasm(FILE* file,
-                         TCA fileStartAddr,
-                         TCA codeStartAddr,
-                         uint64_t codeLen,
-                         const PerfEventsMap<TCA>& perfEvents,
-                         BCMappingInfo bcMappingInfo,
-                         bool printAddr,
-                         bool printBinary) {
-    }
-    
-    CURLcode CurlShareResource::attachToCurlHandle(CURL *cp) {
-  return curl_easy_setopt(cp, CURLOPT_SHARE, m_share);
-}
-    
-    
-    {///////////////////////////////////////////////////////////////////////////////
-}
-
-    
-    #ifndef incl_HPHP_SYNCHRONIZABLE_MULTI_H_
-#define incl_HPHP_SYNCHRONIZABLE_MULTI_H_
-    
-    bool simplify(Env& env, const ldimmq& inst, Vlabel b, size_t i) {
-  return if_inst<Vinstr::lea>(env, b, i + 1, [&] (const lea& ea) {
-    // ldimmq{s, index}; lea{base[index], d} -> lea{base[s],d}
-    if (!(env.use_counts[inst.d] == 1 &&
-          inst.s.q() <= 4095  &&
-          inst.s.q() >= -4095 &&
-          inst.d == ea.s.index &&
-          ea.s.disp == 0 &&
-          ea.s.base.isValid())) return false;
     }
     }
     
-    #endif // LIBFUZZER_WINDOWS
-
+            for (size_t paramNumber = params.size(); paramNumber > numFixedParams; paramNumber--)
+        {
+            // process optional parameter if it exists
+            std::string propName, value;
+            if (OptionalParameter(params[paramNumber - 1], propName, value))
+            {
+                if (EqualInsensitive(propName, 'copyFlag', 'copy'))
+                {
+                    if (EqualInsensitive(value, 'all'))
+                    {
+                        copyFlags = CopyNodeFlags::copyNodeAll;
+                    }
+                    else if (EqualInsensitive(value, 'value'))
+                    {
+                        copyFlags = CopyNodeFlags::copyNodeValue;
+                    }
+                    else
+                    {
+                        RuntimeError('Invalid optional parameter value %s in CopyNode(), valid values are copyFlag=(all|value)', value.c_str());
+                    }
+                }
+                else
+                {
+                    RuntimeError('Invalid optional parameter to Copy, %s\n valid optional parameters: copyFlag=(all|value)', propName.c_str());
+                }
+            }
+        }
     
-    static FILE *OutputFile = stderr;
-    
-    void CopyFileToErr(const std::string &Path);
-    
-    namespace fuzzer {
-static const size_t kMaxUnitSizeToPrint = 256;
+        int argn = 1;
+    bool use_base85_encoding = false;
+    bool use_compression = true;
+    if (argv[argn][0] == '-')
+    {
+        if (strcmp(argv[argn], '-base85') == 0) { use_base85_encoding = true; argn++; }
+        else if (strcmp(argv[argn], '-nocompress') == 0) { use_compression = false; argn++; }
+        else
+        {
+            printf('Unknown argument: '%s'\n', argv[argn]);
+            return 1;
+        }
     }
     
-      // What features are in the initial corpus?
-  for (size_t i = 0; i < NumFilesInFirstCorpus; i++) {
-    auto &Cur = Files[i].Features;
-    AllFeatures.insert(Cur.begin(), Cur.end());
+        // Setup render state: alpha-blending enabled, no face culling, no depth testing, scissor enabled, polygon fill
+    glEnable(GL_BLEND);
+    glBlendEquation(GL_FUNC_ADD);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glDisable(GL_CULL_FACE);
+    glDisable(GL_DEPTH_TEST);
+    glEnable(GL_SCISSOR_TEST);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    
+        IMGUI_API void          PlotEx(ImGuiPlotType plot_type, const char* label, float (*values_getter)(void* data, int idx), void* data, int values_count, int values_offset, const char* overlay_text, float scale_min, float scale_max, ImVec2 graph_size);
+    
+    
+    {        // Create the constant buffer
+        {
+            D3D10_BUFFER_DESC desc;
+            desc.ByteWidth = sizeof(VERTEX_CONSTANT_BUFFER);
+            desc.Usage = D3D10_USAGE_DYNAMIC;
+            desc.BindFlags = D3D10_BIND_CONSTANT_BUFFER;
+            desc.CPUAccessFlags = D3D10_CPU_ACCESS_WRITE;
+            desc.MiscFlags = 0;
+            g_pd3dDevice->CreateBuffer(&desc, NULL, &g_pVertexConstantBuffer);
+        }
+    }
+    
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
+    
+    #include 'imgui.h'
+#include 'imgui_impl_sdl.h'
+#include 'imgui_impl_opengl3.h'
+#include <stdio.h>
+#include <GL/gl3w.h>    // This example is using gl3w to access OpenGL functions. You may freely use any other OpenGL loader such as: glew, glad, glLoadGen, etc.
+//#include <glew.h>
+#include <SDL.h>
+    
+    using namespace v8;
+    
+    namespace rocksdb {
+    }
+    
+      virtual MemTableRep* CreateMemTableRep(const MemTableRep::KeyComparator&,
+                                         Allocator*, const SliceTransform*,
+                                         Logger* logger) = 0;
+  virtual MemTableRep* CreateMemTableRep(
+      const MemTableRep::KeyComparator& key_cmp, Allocator* allocator,
+      const SliceTransform* slice_transform, Logger* logger,
+      uint32_t /* column_family_id */) {
+    return CreateMemTableRep(key_cmp, allocator, slice_transform, logger);
   }
-  size_t InitialNumFeatures = AllFeatures.size();
-    
-    
-    {} // namespace fuzzer
