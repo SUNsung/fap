392726de7b13925f223cdfcffa75cf6a6a8efa38
SUNsung <weatherius@gmail.com>
2018-06-18 13:47:21 +0300

    Rewrite [344qHfiIvMzFw34qrRyNfdSdbM34Vdj6Oc0ULXk34wZT.cpp] File in: 2018-06-18 13:47:20

--- cccccc/344qHfiIvMzFw34qrRyNfdSdbM34Vdj6Oc0ULXk34wZT.cpp
+++ cccccc/344qHfiIvMzFw34qrRyNfdSdbM34Vdj6Oc0ULXk34wZT.cpp
@@ -1,244 +1,229 @@
 
         
-        // Get basic type definitions.
-#define IPC_MESSAGE_IMPL
-#include 'content/nw/src/common/common_message_generator.h'
+        
+#include 'base/basictypes.h'
     
-    #include 'base/basictypes.h'
-#include '../dispatcher_host.h'
+      std::string key;
+  std::string modifiers;
+  option.GetString('key',&key);
+  option.GetString('modifiers',&modifiers);
     
-    #endif  // CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
+    #include 'base/base64.h'
+#include 'base/logging.h'
+#include 'base/strings/stringprintf.h'
+#include 'base/strings/string_util.h'
+#include 'base/strings/utf_string_conversions.h'
+#include 'chrome/browser/devtools/devtools_window.h'
+#include 'chrome/browser/extensions/devtools_util.h'
+#include 'chrome/browser/extensions/extension_service.h'
+#include 'content/nw/src/api/nw_clipboard.h'
+#include 'content/public/browser/render_view_host.h'
+#include 'content/public/browser/web_contents.h'
+#include 'extensions/browser/extension_system.h'
+#include 'extensions/common/error_utils.h'
+#include 'ui/base/clipboard/clipboard.h'
+#include 'ui/base/clipboard/scoped_clipboard_writer.h'
+#include 'ui/gfx/codec/jpeg_codec.h'
+#include 'ui/gfx/codec/png_codec.h'
+#include 'third_party/skia/include/core/SkBitmap.h'
+    
+    class NwMenuGetNSStringFWithFixupFunction : public NWSyncExtensionFunction {
+ public:
+  NwMenuGetNSStringFWithFixupFunction() {}
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    
+ protected:
+  ~NwMenuGetNSStringFWithFixupFunction() override {}
+    
+  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringFWithFixup', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringFWithFixupFunction);
+};
+    
+     protected:
+  ~NwObjCreateFunction() override;
+    
+    #endif //NW_SRC_API_NW_SCREEN_API_H_
 
     
-    
-    {}
-    
-    void MenuItem::Call(const std::string& method,
-                    const base::ListValue& arguments,
-                    content::RenderFrameHost* rvh) {
-  if (method == 'SetLabel') {
-    std::string label;
-    arguments.GetString(0, &label);
-    SetLabel(label);
-  } else if (method == 'SetIcon') {
-    std::string icon;
-    arguments.GetString(0, &icon);
-    SetIcon(icon);
-  } else if (method == 'SetIconIsTemplate') {
-    bool isTemplate;
-    arguments.GetBoolean(0, &isTemplate);
-    SetIconIsTemplate(isTemplate);
-  } else if (method == 'SetTooltip') {
-    std::string tooltip;
-    arguments.GetString(0, &tooltip);
-    SetTooltip(tooltip);
-  } else if (method == 'SetEnabled') {
-    bool enabled = true;
-    arguments.GetBoolean(0, &enabled);
-    SetEnabled(enabled);
-  } else if (method == 'SetChecked') {
-    bool checked = false;
-    arguments.GetBoolean(0, &checked);
-    SetChecked(checked);
-  } else if (method == 'SetSubmenu') {
-    int object_id = 0;
-    arguments.GetInteger(0, &object_id);
-    SetSubmenu(object_manager()->GetApiObject<Menu>(object_id));
-#if defined(OS_MACOSX)
-  } else if (method == 'SetKey') {
-    std::string key;
-    arguments.GetString(0, &key);
-    SetKey(key);
-  } else if (method == 'SetModifiers') {
-    std::string mod;
-    arguments.GetString(0, &mod);
-    SetModifiers(mod);
-#endif
-  } else {
-    NOTREACHED() << 'Invalid call to MenuItem method:' << method
-                 << ' arguments:' << arguments;
+    // Computes a reshaped copy of the weight matrix w. If there are no
+// partial_funcs_, it does nothing.
+void IntSimdMatrix::Init(const GENERIC_2D_ARRAY<int8_t>& w) {
+  if (partial_funcs_.empty()) return;
+  int num_out = w.dim1();
+  int num_in = w.dim2() - 1;
+  // The rounded-up sizes of the reshaped weight matrix, excluding biases.
+  int rounded_num_in = Roundup(num_in, num_inputs_per_group_);
+  int rounded_num_out = RoundOutputs(num_out);
+  // Add the bias and compute the required size.
+  shaped_w_.resize((rounded_num_in + 1) * rounded_num_out, 0);
+  int shaped_index = 0;
+  int output = 0;
+  // Each number of registers needs a different format! Iterates over the
+  // different numbers of registers (each a power of 2).
+  for (int num_registers = max_output_registers_; num_registers >= 1;
+       num_registers /= 2) {
+    // The number of outputs that we will generate with this many registers.
+    int num_outputs_per_register_set =
+        num_registers * num_outputs_per_register_;
+    // Use the max number of registers until we have to go fewer.
+    while (output + num_outputs_per_register_set <= rounded_num_out) {
+      // Accumulating outputs in registers saves iterating over the inputs, so
+      // we only have to do it once per output register set.
+      for (int input = 0; input < num_in; input += num_inputs_per_group_) {
+        // Iterate over the number of outputs in a register set.
+        for (int j = 0; j < num_outputs_per_register_set; ++j) {
+          // Inner-most loop corresponds to the number of inputs in an input
+          // group.
+          for (int i = 0; i < num_inputs_per_group_; ++i) {
+            int8_t weight = 0;
+            if (output + j < num_out && input + i < num_in)
+              weight = w(output + j, input + i);
+            shaped_w_[shaped_index++] = weight;
+          }
+        }
+      }
+      // Append the bias weights for the register set.
+      for (int j = 0; j < num_outputs_per_register_set; ++j) {
+        int8_t weight = 0;
+        if (output + j < num_out) weight = w(output + j, num_in);
+        shaped_w_[shaped_index++] = weight;
+      }
+      output += num_outputs_per_register_set;
+    }
   }
 }
     
-    #include <vector>
-    
      protected:
-  ~NwObjAllocateIdFunction() override;
-    
-    #include 'caffe/proto/caffe.pb.h'
-#include 'caffe/util/db.hpp'
-#include 'caffe/util/format.hpp'
-    
-    
-    { protected:
   /**
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (N \times 1 \times K) @f$ or, if out_max_val
-   *      @f$ (N \times 2 \times K) @f$ unless axis set than e.g.
-   *      @f$ (N \times K \times H \times W) @f$ if axis == 1
-   *      the computed outputs @f$
-   *       y_n = \arg\max\limits_i x_{ni}
-   *      @f$ (for @f$ K = 1 @f$).
+   * Sets up the internal data for iterating the blobs of a new word, then
+   * moves the iterator to the given offset.
    */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  /// @brief Not implemented (non-differentiable function)
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
-    NOT_IMPLEMENTED;
-  }
-  bool out_max_val_;
-  size_t top_k_;
-  bool has_axis_;
-  int axis_;
+  TESS_LOCAL void BeginWord(int offset);
+    
+    
+/**********************************************************************
+ * ROW::plot
+ *
+ * Draw the ROW in the given colour.
+ **********************************************************************/
+    
+    
+    {}  // namespace tesseract.
+
+    
+    /**
+ * The text lines are read in the given sequence.
+ *
+ * In English, the order is top-to-bottom.
+ * In Chinese, vertical text lines are read right-to-left.  Mongolian is
+ * written in vertical columns top to bottom like Chinese, but the lines
+ * order left-to right.
+ *
+ * Note that only some combinations make sense.  For example,
+ * WRITING_DIRECTION_LEFT_TO_RIGHT implies TEXTLINE_ORDER_TOP_TO_BOTTOM
+*/
+enum TextlineOrder {
+  TEXTLINE_ORDER_LEFT_TO_RIGHT = 0,
+  TEXTLINE_ORDER_RIGHT_TO_LEFT = 1,
+  TEXTLINE_ORDER_TOP_TO_BOTTOM = 2,
 };
     
-    
-    {  int size_;
-  Dtype alpha_, beta_, k_;
-};
-#endif
-    
-    #include 'caffe/layers/neuron_layer.hpp'
-#include 'caffe/layers/relu_layer.hpp'
-    
-    
-    {}  // namespace caffe
-    
-        // ===================================================================
-    // SubminibatchHelpers -- helper for sub-minibatch implementation
-    // TODO: Can this just exist inside SGD.cpp?
-    // ===================================================================
-    
-            size_t varRank = outputVariable.Shape().Rank();
-        size_t maxSequenceLength = 1;
-        size_t numSequences = 1;
-        std::tie(maxSequenceLength, numSequences) = GetNumTimeStepsAndSequences(Shape().SubShape(varRank), outputVariable.DynamicAxes().size());
-    
-        mAsparse.SwitchToMatrixType(MatrixType::SPARSE, matrixFormatSparseCSR, true);
-    mBsparse.SwitchToMatrixType(MatrixType::SPARSE, matrixFormatSparseCSR, true);
-    Matrix<float>::ScaleAndAdd(alpha, mAsparse, mBsparse);
-    
-        // ProcessPassNDLScript - Process a pass of the NDL script
-    // script - NDL Script to process
-    // ndlPass - complete processing for this pass, all passes if ndlPassAll
-    // skipThrough - for iterative processing, skip through this node in the script (used for in-line MEL processing)
-    // fullValidate - validate as a complete network? (false if this might be a snippet of a full network)
-    // returns: last NDL node processed
-    NDLNode<ElemType>* ProcessPassNDLScript(NDLScript<ElemType>* script, NDLPass ndlPass, NDLNode<ElemType>* skipThrough = nullptr, bool fullValidate = false, const std::wstring& dumpFileName = L'')
-    {
-        if (ndlPass == ndlPassFinal)
-        {
-            // make sure to clear the caches so we pick up the new nodes
-            m_net->InvalidateCompiledNetwork();
-            // if requested then dump the nodes
-            // Note: This happens on the invalidated network.
-            if (dumpFileName != L'')
-                m_net->DumpAllNodesToFile(false, true, dumpFileName);
-        }
-        NDLNodeEvaluatorImpl<ElemType> ndlEvaluator(m_net);
-        NDLNode<ElemType>* lastNode = script->Evaluate(ndlEvaluator, L'', ndlPass, skipThrough);
-        if (ndlPass == ndlPassResolve)
-            SetOutputNodes(script);
-        return lastNode;
-    }
-    
-    namespace Microsoft { namespace MSR { namespace BS {
-    }
-    }
-    }
-    
-            for (size_t paramNumber = params.size(); paramNumber > numFixedParams; paramNumber--)
-        {
-            // process optional parameter if it exists
-            std::string propName, value;
-            if (OptionalParameter(params[paramNumber - 1], propName, value))
-            {
-                if (EqualInsensitive(propName, 'copyFlag', 'copy'))
-                {
-                    if (EqualInsensitive(value, 'all'))
-                    {
-                        copyFlags = CopyNodeFlags::copyNodeAll;
-                    }
-                    else if (EqualInsensitive(value, 'value'))
-                    {
-                        copyFlags = CopyNodeFlags::copyNodeValue;
-                    }
-                    else
-                    {
-                        RuntimeError('Invalid optional parameter value %s in CopyNode(), valid values are copyFlag=(all|value)', value.c_str());
-                    }
-                }
-                else
-                {
-                    RuntimeError('Invalid optional parameter to Copy, %s\n valid optional parameters: copyFlag=(all|value)', propName.c_str());
-                }
-            }
-        }
-    
-        int argn = 1;
-    bool use_base85_encoding = false;
-    bool use_compression = true;
-    if (argv[argn][0] == '-')
-    {
-        if (strcmp(argv[argn], '-base85') == 0) { use_base85_encoding = true; argn++; }
-        else if (strcmp(argv[argn], '-nocompress') == 0) { use_compression = false; argn++; }
-        else
-        {
-            printf('Unknown argument: '%s'\n', argv[argn]);
-            return 1;
-        }
-    }
-    
-        // Setup render state: alpha-blending enabled, no face culling, no depth testing, scissor enabled, polygon fill
-    glEnable(GL_BLEND);
-    glBlendEquation(GL_FUNC_ADD);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glDisable(GL_CULL_FACE);
-    glDisable(GL_DEPTH_TEST);
-    glEnable(GL_SCISSOR_TEST);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    
-        IMGUI_API void          PlotEx(ImGuiPlotType plot_type, const char* label, float (*values_getter)(void* data, int idx), void* data, int values_count, int values_offset, const char* overlay_text, float scale_min, float scale_max, ImVec2 graph_size);
-    
-    
-    {        // Create the constant buffer
-        {
-            D3D10_BUFFER_DESC desc;
-            desc.ByteWidth = sizeof(VERTEX_CONSTANT_BUFFER);
-            desc.Usage = D3D10_USAGE_DYNAMIC;
-            desc.BindFlags = D3D10_BIND_CONSTANT_BUFFER;
-            desc.CPUAccessFlags = D3D10_CPU_ACCESS_WRITE;
-            desc.MiscFlags = 0;
-            g_pd3dDevice->CreateBuffer(&desc, NULL, &g_pVertexConstantBuffer);
-        }
-    }
-    
-        ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init();
-    
-    #include 'imgui.h'
-#include 'imgui_impl_sdl.h'
-#include 'imgui_impl_opengl3.h'
-#include <stdio.h>
-#include <GL/gl3w.h>    // This example is using gl3w to access OpenGL functions. You may freely use any other OpenGL loader such as: glew, glad, glLoadGen, etc.
-//#include <glew.h>
-#include <SDL.h>
-    
-    using namespace v8;
-    
-    namespace rocksdb {
-    }
-    
-      virtual MemTableRep* CreateMemTableRep(const MemTableRep::KeyComparator&,
-                                         Allocator*, const SliceTransform*,
-                                         Logger* logger) = 0;
-  virtual MemTableRep* CreateMemTableRep(
-      const MemTableRep::KeyComparator& key_cmp, Allocator* allocator,
-      const SliceTransform* slice_transform, Logger* logger,
-      uint32_t /* column_family_id */) {
-    return CreateMemTableRep(key_cmp, allocator, slice_transform, logger);
+    void TransformBlock(double block[64], Transform1d f) {
+  double tmp[64];
+  for (int x = 0; x < 8; ++x) {
+    f(&block[x], 8, &tmp[x]);
   }
+  for (int y = 0; y < 8; ++y) {
+    f(&tmp[8 * y], 1, &block[8 * y]);
+  }
+}
+    
+    #endif  // GUETZLI_FAST_LOG_H_
+
+    
+    #include <inttypes.h>
+    
+    namespace {
+    }
+    
+      virtual ~AbstractAuthResolver();
+    
+    namespace aria2 {
+    }
+    
+    void AbstractOptionHandler::setEraseAfterParse(bool f)
+{
+  updateFlags(FLAG_ERASE_AFTER_PARSE, f);
+}
+    
+    #include 'FileAllocationIterator.h'
+    
+      /**
+   * Returns announce URL.
+   */
+  std::string getAnnounce() const;
+    
+      AnnounceEvent event;
+  std::deque<std::string> urls;
+    
+      TLSVersion getMinTLSVersion() const { return minTLSVer_; }
+    
+    namespace benchmark {
+namespace internal {
+// The arraysize(arr) macro returns the # of elements in an array arr.
+// The expression is a compile-time constant, and therefore can be
+// used in defining new arrays, for example.  If you use arraysize on
+// a pointer by mistake, you will get a compile-time error.
+//
+    }
+    }
+    
+      std::string const& fname = FLAGS_benchmark_out;
+  if (fname.empty() && file_reporter) {
+    Err << 'A custom file reporter was provided but '
+           '--benchmark_out=<file> was not specified.'
+        << std::endl;
+    std::exit(1);
+  }
+  if (!fname.empty()) {
+    output_file.open(fname);
+    if (!output_file.is_open()) {
+      Err << 'invalid file name: '' << fname << std::endl;
+      std::exit(1);
+    }
+    if (!file_reporter) {
+      default_file_reporter = internal::CreateReporter(
+          FLAGS_benchmark_out_format, ConsoleReporter::OO_None);
+      file_reporter = default_file_reporter.get();
+    }
+    file_reporter->SetOutputStream(&output_file);
+    file_reporter->SetErrorStream(&output_file);
+  }
+    
+    // Parses a string for an Int32 flag, in the form of
+// '--flag=value'.
+//
+// On success, stores the value of the flag in *value, and returns
+// true.  On failure, returns false without changing *value.
+bool ParseInt32Flag(const char* str, const char* flag, int32_t* value);
+    
+    
+    {  double coef;
+  double rms;
+  BigO complexity;
+};
+    
+    void JSONReporter::ReportRuns(std::vector<Run> const& reports) {
+  if (reports.empty()) {
+    return;
+  }
+  std::string indent(4, ' ');
+  std::ostream& out = GetOutputStream();
+  if (!first_report_) {
+    out << ',\n';
+  }
+  first_report_ = false;
+    }
+    
+    #include 'benchmark/benchmark.h'
