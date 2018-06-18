2bc6ea8b8d4cb55b85226c73cff4c4b4c077841f
SUNsung <weatherius@gmail.com>
2018-06-18 05:51:34 +0300

    Rewrite [19x0PypItE6h.19Yd6Q1PYvSOI19qv4GZ.ce.So19DeN.py] File in: 2018-06-18 05:51:34

--- pyton/19x0PypItE6h.19Yd6Q1PYvSOI19qv4GZ.ce.So19DeN.py
+++ pyton/19x0PypItE6h.19Yd6Q1PYvSOI19qv4GZ.ce.So19DeN.py
@@ -1,126 +1,14 @@
 
         
-        def main():
-    # First, we load the current README into memory as an array of lines
-    with open('README.md', 'r') as read_me_file:
-        read_me = read_me_file.readlines()
+        DOCUMENTATION = '''
+---
+inventory: openshift
+short_description: Openshift gears external inventory script
+description:
+  - Generates inventory of Openshift gears using the REST interface
+  - this permit to reuse playbook to setup an Openshift gear
+version_added: None
+author: Michael Scherer
+'''
     
-    from tensorflow.python.framework import test_util
-from tensorflow.python.platform import googletest
-from google.protobuf import text_format
-    
-          self.assertAllEqual(potentials.eval(), [[[6, 2, 3],
-                                               [2, 7, 4],
-                                               [3, 4, 8]],
-                                              [[8, 4, 5],
-                                               [2, 7, 4],
-                                               [1, 2, 6]]])  # pyformat: disable
-    
-          signature_map = {
-          signature_name:
-              tf.saved_model.signature_def_utils.build_signature_def(
-                  inputs={
-                      'inputs':
-                          tf.saved_model.utils.build_tensor_info(
-                              annotation['input_batch'])
-                  },
-                  outputs={
-                      'annotations':
-                          tf.saved_model.utils.build_tensor_info(
-                              annotation['annotations'])
-                  },
-                  method_name=tf.saved_model.signature_constants.
-                  PREDICT_METHOD_NAME),
-      }
-    
-          # Check that the non-averaged version does not exist.
-      with self.assertRaises(KeyError):
-        restored_graph.get_operation_by_name(non_averaged_hook_name)
-    
-    PROJ_METADATA = '%s.json' % PROJ_NAME
-    
-            link_list = []
-    
-        url = sorted(
-        map(lambda x: x.firstChild.nodeValue, xml.getElementsByTagName('src')),
-        key=lambda x: int(match1(x, r'_(\d+?)_')))[-1]
-    
-                f = _base.Future()
-            w = _WorkItem(f, fn, args, kwargs)
-    
-    
-def EndsWithPython_Python2Paths_test():
-  python_paths = [
-    'python',
-    'python2',
-    '/usr/bin/python2.7',
-    '/home/user/.pyenv/shims/python2.7',
-    r'C:\Python27\python.exe',
-    '/Contents/MacOS/Python'
-  ]
-    
-    
-@patch( 'ycm.client.messages_request.PostVimMessage',
-        new_callable = ExtendedMock )
-def HandlePollResponse_MultipleMessagesAndDiagnostics_test( post_vim_message ):
-  diagnostics_handler = ExtendedMock()
-  messages = [
-    { 'filepath': 'foo', 'diagnostics': [ 'PLACEHOLDER1' ] },
-    { 'message': 'On the first day of Christmas, my VimScript gave to me' },
-    { 'filepath': 'bar', 'diagnostics': [ 'PLACEHOLDER2' ] },
-    { 'message': 'A test file in a Command-T' },
-    { 'filepath': 'baz', 'diagnostics': [ 'PLACEHOLDER3' ] },
-    { 'message': 'On the second day of Christmas, my VimScript gave to me' },
-    { 'filepath': 'foo', 'diagnostics': [ 'PLACEHOLDER4' ] },
-    { 'message': 'Two popup menus, and a test file in a Command-T' },
-  ]
-  assert_that( _HandlePollResponse( messages, diagnostics_handler ),
-               equal_to( True ) )
-  diagnostics_handler.UpdateWithNewDiagnosticsForFile.assert_has_exact_calls( [
-    call ( 'foo', [ 'PLACEHOLDER1' ] ),
-    call ( 'bar', [ 'PLACEHOLDER2' ] ),
-    call ( 'baz', [ 'PLACEHOLDER3' ] ),
-    call ( 'foo', [ 'PLACEHOLDER4' ] )
-  ] )
-    
-    js.load_plugin_modules(plugins)
-modules = dict((k, m) for k, m in js.module.iteritems())
-print 'JS_MODULES := ' + ' '.join(modules.iterkeys())
-outputs = []
-for name, module in modules.iteritems():
-    outputs.extend(module.outputs)
-    print 'JS_MODULE_OUTPUTS_%s := %s' % (name, ' '.join(module.outputs))
-    print 'JS_MODULE_DEPS_%s := %s' % (name, ' '.join(module.dependencies))
-    
-        @validate(VAdmin(),
-              award = VAwardByCodename('awardcn'),
-              recipient = nop('recipient'),
-              desc = nop('desc'),
-              url = nop('url'),
-              hours = nop('hours'))
-    def GET_give(self, award, recipient, desc, url, hours):
-        if award is None:
-            abort(404, 'page not found')
-    
-            def builder_wrapper(thing = None):
-            kw = {}
-            if not thing:
-                kw['url'] = url
-                kw['title'] = title
-            return ButtonLite(thing,
-                              image = 1 if buttonimage is None else buttonimage,
-                              target = '_new' if newwindow else '_parent',
-                              styled = styled, **kw)
-    
-    from r2.controllers.reddit_base import MinimalController
-from r2.lib.pages import (
-    GoogleTagManagerJail,
-    GoogleTagManager,
-)
-from r2.lib.validator import (
-    validate,
-    VGTMContainerId,
-)
-    
-    import json
-import os
+        args = parser.parse_args()
