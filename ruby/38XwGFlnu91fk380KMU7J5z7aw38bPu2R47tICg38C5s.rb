a2e53dbe49234df828300add94766a3e1c8984c4
SUNsung <weatherius@gmail.com>
2018-06-19 12:40:59 +0300

    Rewrite [38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb] File in: 2018-06-19 12:40:59

--- ruby/38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb
+++ ruby/38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb
@@ -1,37 +1,80 @@
 
         
-          Jekyll::Command.subclasses.each { |c| c.init_with_program(p) }
-    
-            css('p > code:first-child:last-child', 'td > code:first-child:last-child').each do |node|
-          next if node.previous.try(:content).present? || node.next.try(:content).present?
-          node.inner_html = node.inner_html.squish.gsub(/<br(\ \/)?>\s*/, '\n')
-          node.content = node.content.strip
-          node.name = 'pre' if node.content =~ /\s/
-          node.parent.before(node.parent.children).remove if node.parent.name == 'p'
-        end
-    
-          def additional_entries
-        entries = []
-    
-      def test_conflicting_encodings
-    Dir.mktmpdir(__method__.to_s) do |tmpdir|
-      tmp = File.join(tmpdir, 'x')
-      File.open(tmp, 'wb', :encoding => Encoding::EUC_JP) do |x|
-        assert_equal Encoding::EUC_JP, x.external_encoding
-      end
-    end
-  end
-    
-      class SubclassX
-    attr_reader :key
-    def initialize(*)
-      @key = :value
-      super
-    end
+          def meta
+    object.file.meta
   end
 end
 
     
-            def update
-          authorize! :update, @order, order_token
-          @address = find_address
+      def updated
+    object.updated_at.iso8601
+  end
+    
+      private
+    
+    require 'rails_helper'
+    
+    module Metasploit
+  module Framework
+    class Application < Rails::Application
+      include Metasploit::Framework::CommonEngine
+    
+    begin
+  require 'bundler/setup'
+rescue LoadError
+  $stderr.puts '[*] Metasploit requires the Bundler gem to be installed'
+  $stderr.puts '    $ gem install bundler'
+  exit(1)
+end
+    
+        on :fetch_person_for_hcard do |guid|
+      person = Person.where(guid: guid, closed_account: false).where.not(owner: nil).take
+      if person
+        DiasporaFederation::Discovery::HCard.new(
+          guid:             person.guid,
+          nickname:         person.username,
+          full_name:        '#{person.profile.first_name} #{person.profile.last_name}'.strip,
+          url:              AppConfig.pod_uri,
+          photo_large_url:  person.image_url,
+          photo_medium_url: person.image_url(:thumb_medium),
+          photo_small_url:  person.image_url(:thumb_small),
+          public_key:       person.serialized_public_key,
+          searchable:       person.searchable,
+          first_name:       person.profile.first_name,
+          last_name:        person.profile.last_name
+        )
+      end
+    end
+    
+              render 'admins/pods'
+        end
+        format.mobile { render 'admins/pods' }
+        format.json { render json: pods_json }
+      end
+    end
+    
+        case params[:range]
+    when 'week'
+      range = 1.week
+      @segment = t('admins.stats.week')
+    when '2weeks'
+      range = 2.weeks
+      @segment = t('admins.stats.2weeks')
+    when 'month'
+      range = 1.month
+      @segment = t('admins.stats.month')
+    else
+      range = 1.day
+      @segment = t('admins.stats.daily')
+    end
+    
+      private
+    
+    module LogStash
+  module Api
+    module Commands
+      module System
+        class Plugins < Commands::Base
+          def run
+            { :total => plugins.count, :plugins => plugins }
+          end
