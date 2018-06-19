01fee5d3f4f727fea67c11e4460cf283d57a7c59
SUNsung <weatherius@gmail.com>
2018-06-19 11:28:37 +0300

    Rewrite [08SC9jVIN6OPU08ROLgBW.CfM08CqwF68mbrQ08Q2Y.py] File in: 2018-06-19 11:28:36

--- pyton/08SC9jVIN6OPU08ROLgBW.CfM08CqwF68mbrQ08Q2Y.py
+++ pyton/08SC9jVIN6OPU08ROLgBW.CfM08CqwF68mbrQ08Q2Y.py
@@ -1,176 +1,138 @@
-if len(sys.argv) == 2 and sys.argv[1] == '--list':
-    print(json.dumps(result))
-elif len(sys.argv) == 3 and sys.argv[1] == '--host':
-    print(json.dumps({}))
-else:
-    print('Need an argument, either --list or --host <host>')
+
+        
+        
+def check_format(filename):
+    '''
+    validates that each line is formatted correctly,
+    appending to error list as needed
+    '''
+    with open(filename) as fp:
+        lines = list(line.rstrip() for line in fp)
+    check_alphabetical(lines)
+    # START Check Entries
+    num_in_category = min_entries_per_section + 1
+    category = ''
+    category_line = 0
+    for line_num, line in enumerate(lines):
+        if section_title_re.match(line):
+            title_links.append(section_title_re.match(line).group(1))
+        # check each section for the minimum number of entries
+        if line.startswith(anchor):
+            match = anchor_re.match(line)
+            if match:
+                if match.group(1) not in title_links:
+                    add_error(line_num, 'section header ({}) not added as a title link'.format(match.group(1)))
+            else:
+                add_error(line_num, 'section header is not formatted correctly')
+            if num_in_category < min_entries_per_section:
+                add_error(category_line, '{} section does not have the minimum {} entries (only has {})'.format(
+                    category, min_entries_per_section, num_in_category))
+            category = line.split(' ')[1]
+            category_line = line_num
+            num_in_category = 0
+            continue
+        # skips lines that we do not care about
+        if not line.startswith('|') or line.startswith('|---'):
+            continue
+        num_in_category += 1
+        segments = line.split('|')[1:-1]
+        if len(segments) < num_segments:
+            add_error(line_num, 'entry does not have all the required sections (have {}, need {})'.format(
+                len(segments), num_segments))
+            continue
+        # START Global
+        for segment in segments:
+            # every line segment should start and end with exactly 1 space
+            if len(segment) - len(segment.lstrip()) != 1 or len(segment) - len(segment.rstrip()) != 1:
+                add_error(line_num, 'each segment must start and end with exactly 1 space')
+        # END Global
+        segments = [seg.strip() for seg in segments]
+        check_entry(line_num, segments)
+    # END Check Entries
+    
+        from inspect import getargspec
+    from cStringIO import StringIO
+    
+        app = flask.Flask(__name__)
+    app.config.from_mapping([
+        ('SECRET_KEY', 'config'),
+        ('TEST_KEY', 'foo')
+    ])
+    common_object_test(app)
+    
+        plugin_manager.register(Plugin)
+    
+        def format_body(self, content, mime):
+        if is_valid_mime(mime):
+            for p in self.enabled_plugins:
+                content = p.format_body(content, mime)
+        return content
 
     
-        terminal_length = os.getenv('ANSIBLE_VYOS_TERMINAL_LENGTH', 10000)
     
-        def construct_vault_encrypted_unicode(self, node):
-        value = self.construct_scalar(node)
-        b_ciphertext_data = to_bytes(value)
-        # could pass in a key id here to choose the vault to associate with
-        # TODO/FIXME: plugin vault selector
-        vault = self._vaults['default']
-        if vault.secrets is None:
-            raise ConstructorError(context=None, context_mark=None,
-                                   problem='found !vault but no vault password provided',
-                                   problem_mark=node.start_mark,
-                                   note=None)
-        ret = AnsibleVaultEncryptedUnicode(b_ciphertext_data)
-        ret.vault = vault
-        return ret
-    
-        for layer_class in [keras.layers.CuDNNGRU, keras.layers.CuDNNLSTM]:
-        model = keras.models.Sequential()
-        model.add(keras.layers.Embedding(10, input_size,
-                                         input_length=timesteps,
-                                         batch_input_shape=(num_samples,
-                                                            timesteps)))
-        layer = layer_class(units,
-                            return_sequences=False,
-                            stateful=True,
-                            weights=None)
-        model.add(layer)
-        model.compile(optimizer='sgd', loss='mse')
-        out1 = model.predict(np.ones((num_samples, timesteps)))
-        assert(out1.shape == (num_samples, units))
-    
-        filename = os.path.join(model_name, 'vae_mean.png')
-    # display a 2D plot of the digit classes in the latent space
-    z_mean, _, _ = encoder.predict(x_test,
-                                   batch_size=batch_size)
-    plt.figure(figsize=(12, 10))
-    plt.scatter(z_mean[:, 0], z_mean[:, 1], c=y_test)
-    plt.colorbar()
-    plt.xlabel('z[0]')
-    plt.ylabel('z[1]')
-    plt.savefig(filename)
-    plt.show()
-    
-        x = K.placeholder(ndim=2)
-    exp = activations.tanh(x)
-    f = K.function([x], [exp])
+filenames = list(rst_filenames())
+assert filenames
     
     
-def test_categorical_hinge():
-    y_pred = K.variable(np.array([[0.3, 0.2, 0.1],
-                                  [0.1, 0.2, 0.7]]))
-    y_true = K.variable(np.array([[0, 1, 0],
-                                  [1, 0, 0]]))
-    expected_loss = ((0.3 - 0.2 + 1) + (0.7 - 0.1 + 1)) / 2.0
-    loss = K.eval(losses.categorical_hinge(y_true, y_pred))
-    assert np.isclose(expected_loss, np.mean(loss))
+@mock.patch('httpie.core.get_response')
+def test_timeout(get_response):
+    def error(msg, *args, **kwargs):
+        global error_msg
+        error_msg = msg % args
     
-            print('Training %s ... ' % name, end='')
-        t0 = time()
-        clf.fit(X_train, y_train)
-        train_time[name] = time() - t0
-        t0 = time()
-        y_pred = clf.predict(X_test)
-        test_time[name] = time() - t0
-        accuracy[name] = accuracy_score(y_test, y_pred)
-        print('done')
+            '''
+        raise NotImplementedError()
     
-            for k in sorted(results_dict.keys()):
-            all_times[k].append(results_dict[k]['time'])
-            all_errors[k].append(results_dict[k]['error'])
-    
-        op.add_option('--n-samples',
-                  dest='n_samples', default=500, type=int,
-                  help='Number of samples in the benchmarks')
-    
-            text_filename = os.path.join(text_lang_folder,
-                                     '%s_%04d.txt' % (lang, i))
-        print('Writing %s' % text_filename)
-        open(text_filename, 'wb').write(content.encode('utf-8', 'ignore'))
-        i += 1
-    
-    import os
-import tarfile
-from contextlib import closing
-try:
-    from urllib import urlopen
-except ImportError:
-    from urllib.request import urlopen
-    
-        # the training data folder must be passed as first argument
-    movie_reviews_data_folder = sys.argv[1]
-    dataset = load_files(movie_reviews_data_folder, shuffle=False)
-    print('n_samples: %d' % len(dataset.data))
-    
-            if event == 'clear':
-            self.ax.clear()
-            self.ax.set_xticks([])
-            self.ax.set_yticks([])
-            self.contours = []
-            self.c_labels = None
-            self.plot_kernels()
-    
-    data, row_idx, col_idx = sg._shuffle(data, random_state=0)
-plt.matshow(data, cmap=plt.cm.Blues)
-plt.title('Shuffled dataset')
-    
-    
-def sqr(x):
-    return np.sign(np.cos(x))
-    
-            User enters a name for the Help resource and a web url or file
-        name. The user can browse for the file.
         '''
-        self.filepath = filepath
-        message = 'Name for item on Help menu:'
-        super().__init__(
-                parent, title, message, text0=menuitem,
-                used_names=used_names, _htest=_htest, _utest=_utest)
+)
+network.add_argument(
+    '--follow', '-F',
+    default=False,
+    action='store_true',
+    help='''
+    Follow 30x Location redirects.
     
-        # We only ever write one 'entry point' symbol - either
-    # 'main' or 'WinMain'.  Therefore, there is no need to
-    # pass a subsystem switch to the linker as it works it
-    # out all by itself.  However, the subsystem _does_ determine
-    # the file extension and additional linker flags.
-    target_link_flags = ''
-    target_ext = '.exe'
-    if subsystem_details[vars['subsystem']][2]:
-        target_link_flags = '-dll'
-        target_ext = '.dll'
-    
-    class XMLRPCDocGenerator:
-    '''Generates documentation for an XML-RPC server.
-    
-            orig_alrm_handler = signal.signal(signal.SIGALRM, handler)
-        self.addCleanup(signal.signal, signal.SIGALRM, orig_alrm_handler)
-    
-    
-    @unittest.skipUnless(sys.platform == 'darwin', 'OSX only test')
-    def test_mac_ver_with_fork(self):
-        # Issue7895: platform.mac_ver() crashes when using fork without exec
-        #
-        # This test checks that the fix for that issue works.
-        #
-        pid = os.fork()
-        if pid == 0:
-            # child
-            info = platform.mac_ver()
-            os._exit(0)
-    
-    @dataclasses.dataclass
-class IV:
-    T_IV4 = dataclasses.InitVar
-    iv0: dataclasses.InitVar[int]
-    iv1: dataclasses.InitVar
-    iv2: T_IV2
-    iv3: T_IV3
-    not_iv4: T_IV4  # When using string annotations, this field is not recognized as an InitVar.
+        def _migrate_implicit_content_type(self):
+        '''Migrate the removed implicit_content_type config option'''
+        try:
+            implicit_content_type = self.pop('implicit_content_type')
+        except KeyError:
+            self.save()
+        else:
+            if implicit_content_type == 'form':
+                self['default_options'].insert(0, '--form')
+            self.save()
+            self.load()
 
     
-    @dataclass
-class CV:
-    T_CV4 = ClassVar
-    cv0: ClassVar[int] = 20
-    cv1: ClassVar = 30
-    cv2: T_CV2
-    cv3: T_CV3
-    not_cv4: T_CV4  # When using string annotations, this field is not recognized as a ClassVar.
+    
+class ExitStatus:
+    '''Exit status code constants.'''
+    OK = 0
+    ERROR = 1
+    PLUGIN_ERROR = 7
+    
+                if point.name == 'p':
+                link = point.find('a')
+                if link is not None:
+                    link = clean_pdf_link(link.attrs['href'])
+                    ext = get_extension(link)
+                    print(ext)
+                    if not ext in forbidden_extensions:
+                        print(shorten_title(point.text) + ' (' + link + ')')
+                        try:
+                            name = clean_text(point.text.split('[' + ext + ']')[0])
+                            fullname = '.'.join((name, ext))
+                            if not os.path.exists('/'.join((current_directory, fullname)) ):
+                                download_pdf(link, current_directory, '.'.join((name, ext)))
+                        except KeyboardInterrupt:
+                            try:
+                                print('Press Ctrl-C in 1 second to quit')
+                                time.sleep(1)
+                            except KeyboardInterrupt:
+                                print('Cancelling..')
+                                break
+                        except:
+                            failures.append(point.text)
+                        
+        point = point.next_sibling          
