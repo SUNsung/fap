4cbf6d05446456fabf39f60043e8bf1f841ddb3a
SUNsung <weatherius@gmail.com>
2018-06-20 01:11:47 +0300

    Rewrite [india.rb] File in: 2018-06-20 01:11:47

--- ruby/india.rb
+++ ruby/india.rb
@@ -1,141 +1,154 @@
 
         
-        module ActionCable
-  module Server
-    # An instance of this configuration object is available via ActionCable.server.config, which allows you to tweak Action Cable configuration
-    # in a Rails config initializer.
-    class Configuration
-      attr_accessor :logger, :log_tags
-      attr_accessor :connection_class, :worker_pool_size
-      attr_accessor :disable_request_forgery_protection, :allowed_request_origins, :allow_same_origin_as_host
-      attr_accessor :cable, :url, :mount_path
-    
-            def test_url_sub_key_for_sqlite3
-          spec = resolve :production, 'production' => { 'url' => 'sqlite3:foo?encoding=utf8' }
-          assert_equal({
-            'adapter'  => 'sqlite3',
-            'database' => 'foo',
-            'encoding' => 'utf8',
-            'name'     => 'production' }, spec)
-        end
-    
-      def call_controller(klass, action)
-    klass.action(action).call(@request.env)
-  end
+          s.author   = ['Pratik Naik', 'David Heinemeier Hansson']
+  s.email    = ['pratiknaik@gmail.com', 'david@loudthinking.com']
+  s.homepage = 'http://rubyonrails.org'
     
           private
-        def method_missing(method_name, *args)
-          if @mailer.action_methods.include?(method_name.to_s)
-            ActionMailer::Parameterized::MessageDelivery.new(@mailer, method_name, @params, *args)
-          else
-            super
-          end
+        # Called once a consumer has become a subscriber of the channel. Usually the place to setup any streams
+        # you want this channel to be sending to the subscriber.
+        def subscribed # :doc:
+          # Override in subclasses
         end
     
-      it 'decodes the remaining ints when passed the '*' modifier' do
-    'dbcahfge'.unpack(unpack_format('*')).should == [1684169569, 1751541605]
-  end
+            # May be overridden to change the default stream handling behavior
+        # which decodes JSON and transmits to the client.
+        #
+        # TODO: Tests demonstrating this.
+        #
+        # TODO: Room for optimization. Update transmit API to be coder-aware
+        # so we can no-op when pubsub and connection are both JSON-encoded.
+        # Then we can skip decode+encode if we're just proxying messages.
+        def default_stream_handler(broadcasting, coder:)
+          coder ||= ActiveSupport::JSON
+          stream_transmitter stream_decoder(coder: coder), broadcasting: broadcasting
+        end
     
-    describe :string_unpack_Aa, shared: true do
-  it 'decodes the number of bytes specified by the count modifier including NULL bytes' do
-    'a\x00bc'.unpack(unpack_format(3)+unpack_format).should == ['a\x00b', 'c']
-  end
+            def process_internal_message(message)
+          case message['type']
+          when 'disconnect'
+            logger.info 'Removing connection (#{connection_identifier})'
+            websocket.close
+          end
+        rescue Exception => e
+          logger.error 'There was an exception - #{e.class}(#{e.message})'
+          logger.error e.backtrace.join('\n')
     
-      class Apple < Struct; end
-    
-        while t.status
-      begin
-        t.send(@method)
-      rescue ThreadError
-        # The thread might die right after.
-        t.status.should == false
-      end
-      Thread.pass
-    end
-    
-        time.usec.should == 0
-    time.nsec.should == 999
-  end
-    
-      # Creates a delayed logger wrapping `inner`.
-  #
-  # @param inner [Sass::Logger::Base] The wrapped logger.
-  def initialize(inner)
-    self.log_level = inner.log_level
-    @inner = inner
-    @messages = []
-  end
-    
-        def parse_input(environment, text)
-      case text
-      when Script::MATCH
-        name = $1
-        guarded = !!$3
-        val = Script::Parser.parse($2, @line, text.size - ($3 || '').size - $2.size)
-    
-        mkdir_p page_dir
-    file = '#{page_dir}/#{filename}.#{extension}'
-    if File.exist?(file)
-      abort('rake aborted!') if ask('#{file} already exists. Do you want to overwrite?', ['y', 'n']) == 'n'
-    end
-    puts 'Creating new page: #{file}'
-    open(file, 'w') do |page|
-      page.puts '---'
-      page.puts 'layout: page'
-      page.puts 'title: \'#{title}\''
-      page.puts 'date: #{Time.now.strftime('%Y-%m-%d %H:%M')}'
-      page.puts 'comments: true'
-      page.puts 'sharing: true'
-      page.puts 'footer: true'
-      page.puts '---'
-    end
-  else
-    puts 'Syntax error: #{args.filename} contains unsupported characters'
+        exec './child #{parent_pid}'
   end
 end
     
-        def render(context)
-      quote = paragraphize(super)
-      author = '<strong>#{@by.strip}</strong>' if @by
-      if @source
-        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
-        parts = []
-        url.each do |part|
-          if (parts + [part]).join('/').length < 32
-            parts << part
-          end
-        end
-        source = parts.join('/')
-        source << '/&hellip;' unless source == @source
-      end
-      if !@source.nil?
-        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
-      elsif !@title.nil?
-        cite = ' <cite>#{@title}</cite>'
-      end
-      blockquote = if @by.nil?
-        quote
-      elsif cite
-        '#{quote}<footer>#{author + cite}</footer>'
+        batches(BATCH_SIZE) do |offset|
+      results = mysql_query(
+        'SELECT u.uid id, u.username username, u.email email, u.groupid group_id,
+                    su.regdate regdate, su.password password_hash, su.salt salt,
+                    s.regip regip, s.lastip last_visit_ip, s.lastvisit last_visit_time, s.lastpost last_posted_at, s.lastsendmail last_emailed_at,
+                    u.emailstatus email_confirmed, u.avatarstatus avatar_exists,
+                    p.site website, p.address address, p.bio bio, p.realname realname, p.qq qq,
+                    p.resideprovince resideprovince, p.residecity residecity, p.residedist residedist, p.residecommunity residecommunity,
+                    p.resideprovince birthprovince, p.birthcity birthcity, p.birthdist birthdist, p.birthcommunity birthcommunity,
+                    h.spacecss spacecss, h.spacenote spacenote,
+                    f.customstatus customstatus, f.sightml sightml
+               FROM #{user_table} u
+               LEFT JOIN #{sensitive_user_table} su USING(uid)
+               LEFT JOIN #{profile_table} p USING(uid)
+               LEFT JOIN #{status_table} s USING(uid)
+               LEFT JOIN #{forum_table} f USING(uid)
+               LEFT JOIN #{home_table} h USING(uid)
+              ORDER BY u.uid ASC
+              LIMIT #{BATCH_SIZE}
+             OFFSET #{offset};')
+    
+              attrs = {
+            user_id: post_user.id,
+            topic_id: post.topic_id,
+            raw: p[:cooked],
+            cooked: p[:cooked],
+            created_at: Date.parse(p[:created_at])
+          }
+    
+        total_count = @client.query('
+        SELECT COUNT(*) count
+          FROM node n
+         WHERE n.type = 'answer'
+           AND n.status = 1;').first['count']
+    
+      def normalize_raw!(raw)
+    return '<missing>' if raw.blank?
+    raw = raw.dup
+    
+        import_users
+    import_discussions
+    
+          FastlaneCore::PrintTable.print_values(config: Cert.config, hide_keys: [:output_path], title: 'Summary for cert #{Fastlane::VERSION}')
+    
+        # Override Appfile configuration for a specific lane.
+    #
+    # lane_name  - Symbol representing a lane name. (Can be either :name, 'name' or 'platform name')
+    # block - Block to execute to override configuration values.
+    #
+    # Discussion If received lane name does not match the lane name available as environment variable, no changes will
+    #             be applied.
+    def for_lane(lane_name)
+      if lane_name.to_s.split(' ').count > 1
+        # That's the legacy syntax 'platform name'
+        puts('You use deprecated syntax '#{lane_name}' in your Appfile.'.yellow)
+        puts('Please follow the Appfile guide: https://docs.fastlane.tools/advanced/#appfile'.yellow)
+        platform, lane_name = lane_name.split(' ')
+    
+            c.option('--username username', String, 'Username to remove.')
+    
+            expect(CredentialsManager::AppfileConfig.new('credentials_manager/spec/fixtures/Appfile5').data[:apple_id]).to eq('felix@sunapps.net')
+        expect(CredentialsManager::AppfileConfig.new('credentials_manager/spec/fixtures/Appfile5').data[:app_identifier]).to eq('net.sunapps.1')
+        expect(CredentialsManager::AppfileConfig.new('credentials_manager/spec/fixtures/Appfile5').data[:team_id]).to eq('Q2CBPJ58CC')
+    
+          if okay
+        UI.success('HTML file confirmed...') # print this to give feedback to the user immediately
       else
-        '#{quote}<footer>#{author}</footer>'
-      end
-      '<blockquote>#{blockquote}</blockquote>'
-    end
-    
-        def cache(gist, file, data)
-      cache_file = get_cache_file_for gist, file
-      File.open(cache_file, 'w') do |io|
-        io.write data
+        UI.user_error!('Did not upload the metadata, because the HTML file was rejected by the user')
       end
     end
     
-      # Checks for excerpts (helpful for template conditionals)
-  def has_excerpt(input)
-    input =~ /<!--\s*more\s*-->/i ? true : false
+      def report_params
+    params.require(:abuse_report).permit(%i(
+      message
+      user_id
+    ))
   end
     
-      protected
-  def extract_fields(filter_string)
-    (filter_string.empty? ? [] : filter_string.split(',').map { |s| s.strip.to_sym })
+      def update
+    if @broadcast_message.update(broadcast_message_params)
+      redirect_to admin_broadcast_messages_path, notice: 'Broadcast Message was successfully updated.'
+    else
+      render :edit
+    end
   end
+    
+        # @return [String] major.minor ruby version, ex 1.9
+    def ruby_abi_version
+      RUBY_VERSION[/(\d+\.\d+)(\.\d+)*/, 1]
+    end
+    
+            builder.eval_gemfile('bundler file', gemfile.generate())
+        definition = builder.to_definition(lockfile_path, {})
+        definition.lock(lockfile_path)
+        gemfile.save
+      rescue => e
+        # the error should be handled elsewhere but we need to get the original file if we dont
+        # do this logstash will be in an inconsistent state
+        gemfile.restore!
+        raise e
+      end
+    ensure
+      Bundler.settings[:frozen] = '1' if frozen
+    end
+  end
+end
+
+    
+      # Each plugin install for a gemfile create a path with a unique id.
+  # we must clear what is not currently used in the 
+  def remove_unused_locally_installed_gems!
+    used_path = gemfile.locally_installed_gems.collect { |gem| gem.options[:path] }
+    
+      parameter '[PLUGIN]', 'Part of plugin name to search for, leave empty for all plugins'
