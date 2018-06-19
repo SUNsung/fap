f5f27bc8aab087ae52b012cd231d7f4e7860cd03
SUNsung <weatherius@gmail.com>
2018-06-19 07:30:47 +0300

    Rewrite [080GMQN5CTkc08yLvSx7Qw46E080QYae2Y7Znk08HCV.rb] File in: 2018-06-19 07:30:46

--- ruby/080GMQN5CTkc08yLvSx7Qw46E080QYae2Y7Znk08HCV.rb
+++ ruby/080GMQN5CTkc08yLvSx7Qw46E080QYae2Y7Znk08HCV.rb
@@ -1,119 +1,192 @@
 
         
-                # This returns all registered providers.
-        #
-        # @return [Hash]
-        def providers
-          Registry.new.tap do |result|
-            @registered.each do |plugin|
-              result.merge!(plugin.components.providers)
-            end
-          end
+          </body>
+</html>
+HTML
+CONTENT_NOT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+    
+          https://pip.readthedocs.org/en/stable/installing/#install-pip
+    EOS
+  when 'pil' then <<-EOS.undent
+    Instead of PIL, consider `pip install pillow` or `brew install Homebrew/python/pillow`.
+    EOS
+  when 'macruby' then <<-EOS.undent
+    MacRuby works better when you install their package:
+      http://www.macruby.org/
+    EOS
+  when /(lib)?lzma/
+    'lzma is now part of the xz formula.'
+  when 'xcode'
+    if MacOS.version >= :lion
+      <<-EOS.undent
+      Xcode can be installed from the App Store.
+      EOS
+    else
+      <<-EOS.undent
+      Xcode can be installed from https://developer.apple.com/xcode/downloads/
+      EOS
+    end
+  when 'gtest', 'googletest', 'google-test' then <<-EOS.undent
+    Installing gtest system-wide is not recommended; it should be vendored
+    in your projects that use it.
+    EOS
+  when 'gmock', 'googlemock', 'google-mock' then <<-EOS.undent
+    Installing gmock system-wide is not recommended; it should be vendored
+    in your projects that use it.
+    EOS
+  when 'sshpass' then <<-EOS.undent
+    We won't add sshpass because it makes it too easy for novice SSH users to
+    ruin SSH's security.
+    EOS
+  when 'gsutil' then <<-EOS.undent
+    Install gsutil with `pip install gsutil`
+    EOS
+  when 'clojure' then <<-EOS.undent
+    Clojure isn't really a program but a library managed as part of a
+    project and Leiningen is the user interface to that library.
+    
+      def external_commands
+    paths.reduce([]) do |cmds, path|
+      Dir['#{path}/brew-*'].each do |file|
+        next unless File.executable?(file)
+        cmd = File.basename(file, '.rb')[5..-1]
+        cmds << cmd unless cmd.include?('.')
+      end
+      cmds
+    end.sort
+  end
+    
+          export JAVA_HOME='$(/usr/libexec/java_home)'
+      export AWS_ACCESS_KEY='<Your AWS Access ID>'
+      export AWS_SECRET_KEY='<Your AWS Secret Key>'
+      export #{home_name}='#{home_value}'
+    EOS
+  end
+end
+
+    
+        private_class_method
+    def self.git_log_merge_commit_filtering_option(merge_commit_filtering)
+      case merge_commit_filtering
+      when :exclude_merges
+        '--no-merges'
+      when :only_include_merges
+        '--merges'
+      when :include_merges
+        nil
+      end
+    end
+  end
+end
+
+    
+        context 'with keywords' do
+      let(:options) do
+        {
+          name: { 'en-US' => 'Fastlane Demo' },
+          description: { 'en-US' => 'Demo description' },
+          keywords: { 'en-US' => 'Some, key, words' }
+        }
+      end
+    
+          def initialize(title: nil, description: nil)
+        self.title = title
+        self.description = description
+      end
+    end
+    
+            expect(result).to eq('git rev-parse --short HEAD')
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('git rev-parse --short HEAD')
+      end
+    end
+    
+        def email_changed(record, opts={})
+      devise_mail(record, :email_changed, opts)
+    end
+    
+          # Configure default email options
+      def devise_mail(record, action, opts = {}, &block)
+        initialize_from_record(record)
+        mail headers_for(action, opts), &block
+      end
+    
+      yield
+end
+    
+        puts 'Downloading emojos from source... (#{source})'
+    
+        # Returns a deep copy of this query list and all its children.
+    #
+    # @return [QueryList]
+    def deep_copy
+      QueryList.new(queries.map {|q| q.deep_copy})
+    end
+  end
+    
+        def parse_input(environment, text)
+      case text
+      when Script::MATCH
+        name = $1
+        guarded = !!$3
+        val = Script::Parser.parse($2, @line, text.size - ($3 || '').size - $2.size)
+    
+          interp = node_or_interp
+      after_string_or_interp =
+        if interp.after
+          to_string_interpolation(interp.after)
+        else
+          string_literal('')
+        end
+      if interp.after && interp.whitespace_after
+        after_string_or_interp = concat(string_literal(' '), after_string_or_interp)
+      end
+    
+            def stock_location
+          render 'spree/api/v1/shared/stock_location_required', status: 422 and return unless params[:stock_location_id]
+          @stock_location ||= StockLocation.accessible_by(current_ability, :read).find(params[:stock_location_id])
         end
     
-            # Helper method to get access to the class variable. This is mostly
-        # exposed for tests. This shouldn't be mucked with directly, since it's
-        # structure may change at any time.
-        def registered; @@registered; end
-      end
+        def delete(name, **opts)
+      options = opts.merge(value: 'deleted', expires: Time.at(0))
+      self.[]=(name, options)
     end
   end
 end
 
     
-            # Otherwise, call it
-        cap_host.capability(name, *argv)
+            # Execute the given block after validations and coercions, but before
+        # any endpoint code.
+        def after_validation(&block)
+          namespace_stackable(:after_validations, block)
+        end
     
-      def test_realpath_taintedness
-    Dir.mktmpdir('rubytest-realpath') {|tmpdir|
-      dir = File.realpath(tmpdir).untaint
-      File.write(File.join(dir, base = 'test.file'), '')
-      base.taint
-      dir.taint
-      assert_predicate(File.realpath(base, dir), :tainted?)
-      base.untaint
-      dir.taint
-      assert_predicate(File.realpath(base, dir), :tainted?)
-      base.taint
-      dir.untaint
-      assert_predicate(File.realpath(base, dir), :tainted?)
-      base.untaint
-      dir.untaint
-      assert_not_predicate(File.realpath(base, dir), :tainted?)
-      assert_predicate(Dir.chdir(dir) {File.realpath(base)}, :tainted?)
-    }
-  end
+          module ClassMethods
+        # Clears all defined parameters and validations.
+        def reset_validations!
+          unset_namespace_stackable :declared_params
+          unset_namespace_stackable :validations
+          unset_namespace_stackable :params
+          unset_description_field :params
+        end
     
-      def test_memsize
-    # Time objects are common in some code, try to keep them small
-    skip 'Time object size test' if /^(?:i.?86|x86_64)-linux/ !~ RUBY_PLATFORM
-    require 'objspace'
-    t = Time.at(0)
-    size = GC::INTERNAL_CONSTANTS[:RVALUE_SIZE]
-    case size
-    when 20 then expect = 50
-    when 40 then expect = 86
-    else
-      flunk 'Unsupported RVALUE_SIZE=#{size}, update test_memsize'
-    end
-    assert_equal expect, ObjectSpace.memsize_of(t)
-  rescue LoadError => e
-    skip 'failed to load objspace: #{e.message}'
-  end
-end
-
+          def initialize(errors: [], headers: {}, **_options)
+        @errors = {}
+        errors.each do |validation_error|
+          @errors[validation_error.params] ||= []
+          @errors[validation_error.params] << validation_error
+        end
     
-      it 'does not decode a float when fewer bytes than a float remain and the '*' modifier is passed' do
-    [ ['\xff', []],
-      ['\xff\x00', []],
-      ['\xff\x00\xff', []]
-    ].should be_computed_by(:unpack, unpack_format('*'))
-  end
-    
-      it 'decodes the number of bytes specified by the count modifier' do
-    'abc'.unpack(unpack_format(3)).should == ['abc']
-  end
-    
-      def self.join_dying_thread_with_outer_ensure(kill_method_name=:kill)
-    t = dying_thread_with_outer_ensure(kill_method_name) { yield }
-    lambda { t.join }.should raise_error(RuntimeError, 'In dying thread')
-    return t
-  end
-    
-      it 'cannot be rescued' do
-    thread = Thread.new do
-      begin
-        Thread.current.send(@method)
-      rescue Exception
-        ScratchPad.record :in_rescue
-      end
-     ScratchPad.record :end_of_thread_block
-    end
-    
-    def codepoints_to_unicode(codepoints)
-  if codepoints.include?(' ')
-    codepoints.split(' ').map(&:hex).pack('U*')
-  else
-    [codepoints.hex].pack('U')
-  end
-end
-    
-    class NotificationMailerPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/mention
-  def mention
-    m = Mention.last
-    NotificationMailer.mention(m.account, Notification.find_by(activity: m))
-  end
-    
-        def recheck
-      pod = Pod.find(params[:pod_id])
-      pod.test_connection!
-    
-          def delete_authorization_session_variables
-        session.delete(:client_id)
-        session.delete(:response_type)
-        session.delete(:redirect_uri)
-        session.delete(:scopes)
-        session.delete(:state)
-        session.delete(:nonce)
-      end
+            def context
+          env[Grape::Env::API_ENDPOINT]
+        end
