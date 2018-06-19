c94aac38b00ce49a70238d94dc53664b5f332d01
SUNsung <weatherius@gmail.com>
2018-06-19 19:39:59 +0300

    Rewrite [435kO3PoUjRHk430wZQF0OUujw43slTJBTU0i5E43L2P.rb] File in: 2018-06-19 19:39:59

--- ruby/435kO3PoUjRHk430wZQF0OUujw43slTJBTU0i5E43L2P.rb
+++ ruby/435kO3PoUjRHk430wZQF0OUujw43slTJBTU0i5E43L2P.rb
@@ -1,146 +1,42 @@
 
         
-          def <<(o)
-    @settings << o
+          def merge(*args)
+    @settings.merge(*args)
     self
   end
     
-        # Many formulae include 'lib/charset.alias', but it is not strictly needed
-    # and will conflict if more than one formula provides it
-    observe_file_removal @f.lib/'charset.alias'
-    
-          out = checks.send(method)
-      unless out.nil? || out.empty?
-        if first_warning
-          $stderr.puts <<-EOS.undent
-            #{Tty.white}Please note that these warnings are just used to help the Homebrew maintainers
-            with debugging if you file an issue. If everything you use Homebrew for is
-            working fine: please don't worry and just ignore them. Thanks!#{Tty.reset}
-          EOS
-        end
-    
-        if $stdout.tty?
-      metacharacters = %w[\\ | ( ) [ ] { } ^ $ * + ? .]
-      bad_regex = metacharacters.any? do |char|
-        ARGV.any? do |arg|
-          arg.include?(char) && !arg.start_with?('/')
-        end
-      end
-      if ARGV.any? && bad_regex
-        ohai 'Did you mean to perform a regular expression search?'
-        ohai 'Surround your query with /slashes/ to search by regex.'
-      end
-    end
-    
-          it 'splits correctly' do
-        expected = [
-          'One',
-          'Two',
-          'Three',
-          'Four Token',
-          'Or',
-          'Newlines',
-          'Everywhere'
-        ]
-        expect(generator.split_keywords(keywords)).to eq(expected)
-      end
+      def elisp_caveats
+    return if f.keg_only?
+    if keg && keg.elisp_installed?
+      <<-EOS.undent
+        Emacs Lisp files have been installed to:
+          #{HOMEBREW_PREFIX}/share/emacs/site-lisp/#{f.name}
+      EOS
     end
   end
+    
+      def external_commands
+    paths.reduce([]) do |cmds, path|
+      Dir['#{path}/brew-*'].each do |file|
+        next unless File.executable?(file)
+        cmd = File.basename(file, '.rb')[5..-1]
+        cmds << cmd unless cmd.include?('.')
+      end
+      cmds
+    end.sort
+  end
+    
+      def python(_options = {}, &block)
+    opoo 'Formula#python is deprecated and will go away shortly.'
+    block.call if block_given?
+    PythonRequirement.new
+  end
+  alias_method :python2, :python
+  alias_method :python3, :python
 end
 
     
-            def send_confirmation_notification?
-          confirmation_required? && !@skip_confirmation_notification && self.email.present?
-        end
+      mkdir_p tasks_dir
     
-      def passthru
-    render status: 404, plain: 'Not found. Authentication passthru.'
-  end
-    
-    # Each time a record is set we check whether its session has already timed out
-# or not, based on last request time. If so, the record is logged out and
-# redirected to the sign in page. Also, each time the request comes and the
-# record is set, we set the last request time inside its scoped session to
-# verify timeout in the following request.
-Warden::Manager.after_set_user do |record, warden, options|
-  scope = options[:scope]
-  env   = warden.request.env
-    
-        alias_method :insert_before, :insert
-    
-            css('p > code:first-child:last-child', 'td > code:first-child:last-child').each do |node|
-          next if node.previous.try(:content).present? || node.next.try(:content).present?
-          node.inner_html = node.inner_html.squish.gsub(/<br(\ \/)?>\s*/, '\n')
-          node.content = node.content.strip
-          node.name = 'pre' if node.content =~ /\s/
-          node.parent.before(node.parent.children).remove if node.parent.name == 'p'
-        end
-    
-            entries
-      end
-    
-                  # Read!
-              data << io.readpartial(READ_CHUNK_SIZE).encode('UTF-8', Encoding.default_external)
-            else
-              # Do a simple non-blocking read on the IO object
-              data << io.read_nonblock(READ_CHUNK_SIZE)
-            end
-          rescue Exception => e
-            # The catch-all rescue here is to support multiple Ruby versions,
-            # since we use some Ruby 1.9 specific exceptions.
-    
-          # This deletes the block with the given key if it exists.
-      def delete(key)
-        key    = Regexp.quote(key)
-        regexp = /^#\s*VAGRANT-BEGIN:\s*#{key}$.*^#\s*VAGRANT-END:\s*#{key}$\r?\n?/m
-        @value.gsub!(regexp, '')
-      end
-    
-      fd.close
-    
-          when nil
-        # No matches, no saved state
-      else
-        sessions[s[:session]].merge!({k => matches})
-      end # end case matched
-    end # end of each_key
-  end # end of parse
-end
-    
-          opts.on('--unix-newlines', 'Use Unix-style newlines in written files.',
-                                 ('Always true on Unix.' unless Sass::Util.windows?)) do
-        @options[:unix_newlines] = true if Sass::Util.windows?
-      end
-    end
-    
-            {
-          :always_update     => false,
-          :template_location => root + '/public/stylesheets/sass',
-          :css_location      => root + '/public/stylesheets',
-          :cache_location    => root + '/tmp/sass-cache',
-          :always_check      => env != 'production',
-          :quiet             => env != 'production',
-          :full_exception    => env != 'production'
-        }.freeze
-      end
-    end
-  end
-    
-    module Sass
-  # Runs a SassScript read-eval-print loop.
-  # It presents a prompt on the terminal,
-  # reads in SassScript expressions,
-  # evaluates them,
-  # and prints the result.
-  class Repl
-    # @param options [{Symbol => Object}] An options hash.
-    def initialize(options = {})
-      @options = options
-    end
-    
-    module Capistrano
-  module TaskEnhancements
-    def before(task, prerequisite, *args, &block)
-      prerequisite = Rake::Task.define_task(prerequisite, *args, &block) if block_given?
-      Rake::Task[task].enhance [prerequisite]
-    end
+          describe 'fetching all servers' do
+        subject { dsl.roles(:all) }
