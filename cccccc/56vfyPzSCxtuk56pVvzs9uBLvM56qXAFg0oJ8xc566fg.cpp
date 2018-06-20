981c64281c315b14fa11ecabbe12526f0daecce5
SUNsung <weatherius@gmail.com>
2018-06-21 02:32:44 +0300

    Rewrite [56vfyPzSCxtuk56pVvzs9uBLvM56qXAFg0oJ8xc566fg.cpp] File in: 2018-06-21 02:32:44

--- cccccc/56vfyPzSCxtuk56pVvzs9uBLvM56qXAFg0oJ8xc566fg.cpp
+++ cccccc/56vfyPzSCxtuk56pVvzs9uBLvM56qXAFg0oJ8xc566fg.cpp
@@ -1,176 +1,219 @@
 
         
-          DebuggerClient(ASTContext &C) : Ctx(C) { }
-  virtual ~DebuggerClient() = default;
-  
-  // DebuggerClient is consulted at the beginning of the parsing
-  // of various DeclKinds to see whether the decl should be parsed
-  // in the global context rather than the current context.
-  // This question will only be asked if the decl's current context
-  // is a function marked with the LLDBDebuggerFunction attribute.
-  virtual bool shouldGlobalize(Identifier Name, DeclKind kind) = 0;
-  
-  virtual void didGlobalize (Decl *Decl) = 0;
-    
-      if (!module->isStdlibModule()) {
-    std::string recordFile;
-    bool failed = false;
-    auto consumer = makeRecordingConsumer(filename, indexStorePath,
-                                          &diags, &recordFile, &failed);
-    indexModule(module, /*Hash=*/'', *consumer);
+        // Convert a Python object to a FileDescriptorProto pointer.
+// Handles all kinds of Python errors, which are simply logged.
+static bool GetFileDescriptorProto(PyObject* py_descriptor,
+                                   FileDescriptorProto* output) {
+  if (py_descriptor == NULL) {
+    if (PyErr_ExceptionMatches(PyExc_KeyError)) {
+      // Expected error: item was simply not found.
+      PyErr_Clear();
+    } else {
+      GOOGLE_LOG(ERROR) << 'DescriptorDatabase method raised an error';
+      PyErr_Print();
     }
-    
-      BlockQuote(ArrayRef<MarkupASTNode *> Children);
-    
-    SILDebugScope::SILDebugScope(SILLocation Loc, SILFunction *SILFn,
-                             const SILDebugScope *ParentScope ,
-                             const SILDebugScope *InlinedCallSite)
-    : Loc(Loc), InlinedCallSite(InlinedCallSite) {
-  if (ParentScope)
-    Parent = ParentScope;
-  else {
-    assert(SILFn && 'no parent provided');
-    Parent = SILFn;
+    return false;
+  }
+  if (py_descriptor == Py_None) {
+    return false;
+  }
+  const Descriptor* filedescriptor_descriptor =
+      FileDescriptorProto::default_instance().GetDescriptor();
+  CMessage* message = reinterpret_cast<CMessage*>(py_descriptor);
+  if (PyObject_TypeCheck(py_descriptor, &CMessage_Type) &&
+      message->message->GetDescriptor() == filedescriptor_descriptor) {
+    // Fast path: Just use the pointer.
+    FileDescriptorProto* file_proto =
+        static_cast<FileDescriptorProto*>(message->message);
+    *output = *file_proto;
+    return true;
+  } else {
+    // Slow path: serialize the message. This allows to use databases which
+    // use a different implementation of FileDescriptorProto.
+    ScopedPyObjectPtr serialized_pb(
+        PyObject_CallMethod(py_descriptor, 'SerializeToString', NULL));
+    if (serialized_pb == NULL) {
+      GOOGLE_LOG(ERROR)
+          << 'DescriptorDatabase method did not return a FileDescriptorProto';
+      PyErr_Print();
+      return false;
+    }
+    char* str;
+    Py_ssize_t len;
+    if (PyBytes_AsStringAndSize(serialized_pb.get(), &str, &len) < 0) {
+      GOOGLE_LOG(ERROR)
+          << 'DescriptorDatabase method did not return a FileDescriptorProto';
+      PyErr_Print();
+      return false;
+    }
+    FileDescriptorProto file_proto;
+    if (!file_proto.ParseFromArray(str, len)) {
+      GOOGLE_LOG(ERROR)
+          << 'DescriptorDatabase method did not return a FileDescriptorProto';
+      return false;
+    }
+    *output = file_proto;
+    return true;
   }
 }
     
-      virtual void handleDiagnostic(SourceManager &SM, SourceLoc Loc,
-                                DiagnosticKind Kind,
-                                StringRef FormatString,
-                                ArrayRef<DiagnosticArgument> FormatArgs,
-                                const DiagnosticInfo &Info) override;
+    #ifndef GOOGLE_PROTOBUF_COMPILER_CSHARP_OPTIONS_H__
+#define GOOGLE_PROTOBUF_COMPILER_CSHARP_OPTIONS_H__
     
-    
-    {  StringRef getReceiverUSR() const {
-    for (auto Relation: Relations) {
-      if (Relation.roles & (SymbolRoleSet) SymbolRole::RelationReceivedBy)
-        return Relation.USR;
-    }
-    return StringRef();
-  }
-};
-    
-    #ifndef SWIFT_INDEX_INDEX_H
-#define SWIFT_INDEX_INDEX_H
-    
-    template <> struct DenseMapInfo<LSLocation> {
-  static inline LSLocation getEmptyKey() {
-    return LSLocation(LSBase::Empty);
-  }
-  static inline LSLocation getTombstoneKey() {
-    return LSLocation(LSBase::Tombstone);
-  }
-  static inline unsigned getHashValue(const LSLocation &Loc) {
-    return hash_value(Loc);
-  }
-  static bool isEqual(const LSLocation &LHS, const LSLocation &RHS) {
-    return LHS == RHS;
-  }
-};
-    
-    HINSTANCE g_hInst = NULL;
-D3D_DRIVER_TYPE g_driverType = D3D_DRIVER_TYPE_NULL;
-D3D_FEATURE_LEVEL g_featureLevel = D3D_FEATURE_LEVEL_11_0;
-ID3D11Device* g_pd3dDevice = NULL;
-ID3D11DeviceContext* g_pImmediateContext = NULL;
-IDXGISwapChain* g_pSwapChain = NULL;
-    
-        cv::Mat M2_1 = M2(cv::Range(0,27), cv::Range(0,27));
-    cv::Mat M2_2 = M2(cv::Range(0,27), cv::Range(27,120));
-    cv::Mat M2_3 = M2(cv::Range(27,120), cv::Range(27,120));
-    cv::Mat M2_4 = M2(cv::Range(27,120), cv::Range(0,27));
-    M2.release();
-    
-            static __device__ __forceinline__ int syncthreadsOr(int pred)
-        {
-#if defined (__CUDA_ARCH__) && (__CUDA_ARCH__ < 200)
-                // just campilation stab
-                return 0;
-#else
-                return __syncthreads_or(pred);
-#endif
-        }
-    
-    #if defined(__linux__)
-    #include <dlfcn.h>
-    #include <stdio.h>
-    
-    namespace cv { namespace ocl { namespace runtime {
-    }
-    }
-    }
-    
-    template <typename Generator1, typename Generator2, typename Generator3,
-    typename Generator4, typename Generator5>
-internal::CartesianProductHolder5<Generator1, Generator2, Generator3,
-    Generator4, Generator5> Combine(
-    const Generator1& g1, const Generator2& g2, const Generator3& g3,
-        const Generator4& g4, const Generator5& g5) {
-  return internal::CartesianProductHolder5<Generator1, Generator2, Generator3,
-      Generator4, Generator5>(
-      g1, g2, g3, g4, g5);
-}
-    
-      // Gets the summary of the failure message by omitting the stack
-  // trace in it.
-  static std::string ExtractSummary(const char* message);
-    
-      void Normalize();
-    
-    
-$for j [[
-template <GTEST_$(j)_TYPENAMES_(T)>
-struct tuple_size<GTEST_$(j)_TUPLE_(T) > {
-  static const int value = $j;
-};
-    
-    // Step 3. Call RUN_ALL_TESTS() in main().
-//
-// We do this by linking in src/gtest_main.cc file, which consists of
-// a main() function which calls RUN_ALL_TESTS() for us.
-//
-// This runs all the tests you've defined, prints the result, and
-// returns 0 if successful, or 1 otherwise.
-//
-// Did you notice that we didn't register the tests?  The
-// RUN_ALL_TESTS() macro magically knows about all the tests we
-// defined.  Isn't this convenient?
+    #endif  // GOOGLE_PROTOBUF_COMPILER_CSHARP_REFLECTION_CLASS_H__
 
     
-    TEST_F(ViewsConfigParserPluginTests, test_swap_view) {
-  Config c;
-  std::vector<std::string> old_views_vec;
-  scanDatabaseKeys(kQueries, old_views_vec, 'config_views.');
-  EXPECT_EQ(old_views_vec.size(), 1U);
-  old_views_vec.clear();
-  auto s = c.update(getTestConfigMap('view_test.conf'));
-  EXPECT_TRUE(s.ok());
-  scanDatabaseKeys(kQueries, old_views_vec, 'config_views.');
-  EXPECT_EQ(old_views_vec.size(), 1U);
-  EXPECT_EQ(old_views_vec[0], 'config_views.kernel_hashes_new');
+    
+    { private:
+  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(RepeatedPrimitiveFieldGenerator);
+};
+    
+    namespace google {
+namespace protobuf {
+namespace compiler {
+namespace java {
+namespace {
+    }
+    }
+    }
+    }
     }
     
-    class TLSConfigPlugin;
+        D3D_FEATURE_LEVEL featureLevels[] =
+    {
+        D3D_FEATURE_LEVEL_11_0,
+        D3D_FEATURE_LEVEL_10_1,
+        D3D_FEATURE_LEVEL_10_0,
+    };
+    UINT numFeatureLevels = ARRAYSIZE(featureLevels);
     
-    
-    {  runner.start();
-}
-    
-    #include <osquery/logger.h>
-    
-      static CGEventRef eventCallback(CGEventTapProxy proxy,
-                                  CGEventType type,
-                                  CGEventRef event,
-                                  void* refcon);
-    
-    void Config::setPointScaleFactor(float pixelsInPoint)
+    void cvReleaseStereoBMState( CvStereoBMState** state )
 {
-    YGConfigSetPointScaleFactor(m_config, pixelsInPoint);
+    if( !state )
+        CV_Error( CV_StsNullPtr, '' );
+    }
+    
+    
+    {    IntrinsicParams();
+    IntrinsicParams(Vec2d f, Vec2d c, Vec4d k, double alpha = 0);
+    IntrinsicParams operator+(const Mat& a);
+    IntrinsicParams& operator =(const Mat& a);
+    void Init(const cv::Vec2d& f, const cv::Vec2d& c, const cv::Vec4d& k = Vec4d(0,0,0,0), const double& alpha = 0);
+};
+    
+    #if 0
+    
+    #ifndef STORAGE_LEVELDB_DB_LOG_FORMAT_H_
+#define STORAGE_LEVELDB_DB_LOG_FORMAT_H_
+    
+      // Randomly returns true ~'1/n' of the time, and false otherwise.
+  // REQUIRES: n > 0
+  bool OneIn(int n) { return (Next() % n) == 0; }
+    
+      AutoCompactTest() {
+    dbname_ = test::TmpDir() + '/autocompact_test';
+    tiny_cache_ = NewLRUCache(100);
+    options_.block_cache = tiny_cache_;
+    DestroyDB(dbname_, options_);
+    options_.create_if_missing = true;
+    options_.compression = kNoCompression;
+    ASSERT_OK(DB::Open(options_, dbname_, &db_));
+  }
+    
+    std::string InternalKey::DebugString() const {
+  std::string result;
+  ParsedInternalKey parsed;
+  if (ParseInternalKey(rep_, &parsed)) {
+    result = parsed.DebugString();
+  } else {
+    result = '(bad)';
+    result.append(EscapeString(rep_));
+  }
+  return result;
 }
     
-    // JNI's NIO support has some awkward preconditions and error reporting. This
-// class provides much more user-friendly access.
-class FBEXPORT JByteBuffer : public JavaClass<JByteBuffer> {
- public:
-  static constexpr const char* kJavaDescriptor = 'Ljava/nio/ByteBuffer;';
+      const char* data_;
+  size_t size_;
+  uint32_t restart_offset_;     // Offset in data_ of restart array
+  bool owned_;                  // Block owns data_[]
+    
+      // There must be a goodMallocSize between 100 and 1024...
+  EXPECT_LT(1u, queue.front()->countChainElements());
+  const IOBuf* buf = queue.front();
+  do {
+    EXPECT_LE(100u, buf->capacity());
+    buf = buf->next();
+  } while (buf != queue.front());
+    
+    TEST(IOBuf, IOBufCompare) {
+  folly::IOBufCompare op;
+  auto n = std::unique_ptr<IOBuf>{};
+  auto e = IOBuf::create(0);
+  auto hello1 = seq({'hello'});
+  auto hello2 = seq({'hel', 'lo'});
+  auto hello3 = seq({'he', 'll', 'o'});
+  auto hellow = seq({'hellow'});
+  auto hellox = seq({'hellox'});
     }
+    
+      folly::Optional<T> try_take_for(std::chrono::milliseconds time) override {
+    T item;
+    while (!queue_.readIfNotEmpty(item)) {
+      if (!sem_.try_wait_for(time)) {
+        return folly::none;
+      }
+    }
+    return std::move(item);
+  }
+    
+    
+    {  ExampleObject('foo');
+  XLOG(INFO) << 'main returning';
+  return 0;
+}
+
+    
+    #endif
+    
+    #ifdef ANDROID
+#include 'comm/android/callstack.h'
+#endif
+    
+    void CommFrequencyLimit::__DelOlderTouchTime(uint64_t _time) {
+    for (std::list<uint64_t>::iterator iter = touch_times_.begin(); iter != touch_times_.end();) {
+        if ((_time - (*iter)) > time_span_) {
+            iter = touch_times_.erase(iter);
+            continue;
+        }
+    }
+    }
+    
+    
+/*
+ * CoreServiceBase.h
+ *
+ *  Created on: 2013-6-20
+ *      Author: yerungui
+ */
+    
+      /**
+   * If pred == true, calls setReadCheckSocket(socket). Otherwise, calls
+   * disableReadCheckSocket().
+   */
+  void setReadCheckSocketIf(const std::shared_ptr<SocketCore>& socket,
+                            bool pred);
+  /**
+   * If pred == true, calls setWriteCheckSocket(socket). Otherwise, calls
+   * disableWriteCheckSocket().
+   */
+  void setWriteCheckSocketIf(const std::shared_ptr<SocketCore>& socket,
+                             bool pred);
+    
+      // File must be opened before calling this function.
+  virtual void allocate(int64_t offset, int64_t length,
+                        bool sparse) CXX11_OVERRIDE;
+    
+      virtual PrefPtr getPref() const CXX11_OVERRIDE { return pref_; }
+    
+    
+    {} // namespace aria2
