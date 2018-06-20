a11e0b700caff30fa6439e19b5f27033338812f8
SUNsung <weatherius@gmail.com>
2018-06-20 23:47:02 +0300

    Rewrite [57QNI.r0YnPtA574Nq7nlA4Ng57C5M7UEq3LE57tVP.rb] File in: 2018-06-20 23:47:02

--- ruby/57QNI.r0YnPtA574Nq7nlA4Ng57C5M7UEq3LE57tVP.rb
+++ ruby/57QNI.r0YnPtA574Nq7nlA4Ng57C5M7UEq3LE57tVP.rb
@@ -1,91 +1,96 @@
-        render_and_write
+
+        
+        puts 'Validating #{links.size} links...'
     
-    MODE = ARGV.first || 'cpu'
-PROF_OUTPUT_FILE = File.expand_path('../tmp/stackprof-#{MODE}-#{Time.now.strftime('%Y%m%d%H%M')}.dump', __dir__)
+          def validate!
+        super
+        raise Informative, 'Existing Podfile found in directory' unless config.podfile_path_in_dir(Pathname.pwd).nil?
+        if @project_path
+          help! 'Xcode project at #{@project_path} does not exist' unless File.exist? @project_path
+          project_path = @project_path
+        else
+          raise Informative, 'No Xcode project found, please specify one' unless @project_paths.length > 0
+          raise Informative, 'Multiple Xcode projects found, please specify one' unless @project_paths.length == 1
+          project_path = @project_paths.first
+        end
+        @xcode_project = Xcodeproj::Project.open(project_path)
+      end
     
-          def render!(*args)
-        measure_time do
-          measure_bytes do
-            @template.render!(*args)
-          end
+            rows.each do |row|
+          line = row.values.each_with_index.map do |value, col|
+            value.to_s.ljust(col_widths[col])
+          end.join(' ').rstrip
+          line = color.colorize(line, row.color) if row.color
+          puts line
         end
       end
     
-        # Require all .rb files if safe mode is off
-    #
-    # Returns nothing.
-    def require_plugin_files
-      unless site.safe
-        plugins_path.each do |plugin_search_path|
-          plugin_files = Utils.safe_glob(plugin_search_path, File.join('**', '*.rb'))
-          Jekyll::External.require_with_graceful_fail(plugin_files)
+    module Capistrano
+  module TaskEnhancements
+    def before(task, prerequisite, *args, &block)
+      prerequisite = Rake::Task.define_task(prerequisite, *args, &block) if block_given?
+      Rake::Task[task].enhance [prerequisite]
+    end
+    
+      desc 'Published'
+  task :published do
+  end
+    
+      # PATCH/PUT /books/1
+  # PATCH/PUT /books/1.json
+  def update
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+    
+            def referenced?
+          !@references.empty?
+        end
+    
+            def each_unnecessary_space_match(node, &blk)
+          each_match_range(
+            contents_range(node),
+            MULTIPLE_SPACES_BETWEEN_ITEMS_REGEX,
+            &blk
+          )
         end
       end
     end
-    
-        h = @cls[[['a', 100], 'b', ['c', 300]]]
-    assert_equal(100, h['a'])
-    assert_equal(nil, h['b'])
-    assert_equal(300, h['c'])
-  end
-    
-        t = Time.at(10000000000000000000000)
-    assert_equal('<<10000000000000000000000>>', t.strftime('<<%s>>'))
-    assert_equal('<<010000000000000000000000>>', t.strftime('<<%24s>>'))
-    assert_equal('<<010000000000000000000000>>', t.strftime('<<%024s>>'))
-    assert_equal('<< 10000000000000000000000>>', t.strftime('<<%_24s>>'))
-  end
-    
-      it 'decodes the number of ints requested by the count modifier' do
-    'abcedfgh'.unpack(unpack_format(2)).should == [1701012065, 1751606884]
-  end
-    
-      it 'decodes the number of bytes specified by the count modifier including whitespace bytes' do
-    [ ['a bc',  ['a b', 'c']],
-      ['a\fbc', ['a\fb', 'c']],
-      ['a\nbc', ['a\nb', 'c']],
-      ['a\rbc', ['a\rb', 'c']],
-      ['a\tbc', ['a\tb', 'c']],
-      ['a\vbc', ['a\vb', 'c']]
-    ].should be_computed_by(:unpack, unpack_format(3)+unpack_format)
-  end
-    
-    describe :string_unpack_unicode, shared: true do
-  it 'decodes Unicode codepoints as ASCII values' do
-    [ ['\x00',      [0]],
-      ['\x01',      [1]],
-      ['\x08',      [8]],
-      ['\x0f',      [15]],
-      ['\x18',      [24]],
-      ['\x1f',      [31]],
-      ['\x7f',      [127]],
-      ['\xc2\x80',  [128]],
-      ['\xc2\x81',  [129]],
-      ['\xc3\xbf',  [255]]
-    ].should be_computed_by(:unpack, 'U')
-  end
-    
-      def observe_file_removal(path)
-    path.extend(ObserverPathnameExtension).unlink if path.exist?
-  end
-    
-      def gist_logs
-    raise FormulaUnspecifiedError if ARGV.resolved_formulae.length != 1
-    
-    require 'formula'
-    
-    class PostgresqlRequirement < Requirement
-  fatal true
-  satisfy do
-    odisabled('PostgresqlRequirement', ''depends_on \'postgresql\''')
   end
 end
+
     
-        def env(*settings, &block)
-      if block_given?
-        @env_proc = block
-      else
-        super
-      end
-    end
-  end
+    module RuboCop
+  module Cop
+    module Performance
+      # This cop identifies the use of `Regexp#match` or `String#match`, which
+      # returns `#<MatchData>`/`nil`. The return value of `=~` is an integral
+      # index/`nil` and is more performant.
+      #
+      # @example
+      #   # bad
+      #   do_something if str.match(/regex/)
+      #   while regex.match('str')
+      #     do_something
+      #   end
+      #
+      #   # good
+      #   method(str =~ /regex/)
+      #   return value unless regex =~ 'str'
+      class RedundantMatch < Cop
+        MSG = 'Use `=~` in places where the `MatchData` returned by ' \
+              '`#match` will not be used.'.freeze
+    
+            def autocorrect(node)
+          lambda do |corrector|
+            corrector.remove(node.loc.dot)
+            corrector.remove(node.loc.selector)
+          end
+        end
