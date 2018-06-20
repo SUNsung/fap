7b8b967f23de3c0d4dd1007cf022af3680c60d2f
SUNsung <weatherius@gmail.com>
2018-06-20 15:05:38 +0300

    Rewrite [36kUpSIMUB9qk36LGteQrNsQhY36Lw12eTkiuF636RNa.rb] File in: 2018-06-20 15:05:38

--- ruby/36kUpSIMUB9qk36LGteQrNsQhY36Lw12eTkiuF636RNa.rb
+++ ruby/36kUpSIMUB9qk36LGteQrNsQhY36Lw12eTkiuF636RNa.rb
@@ -1,63 +1,34 @@
 
         
-          </body>
-</html>
-HTML
+            context 'only newlines' do
+      let(:keywords) { 'One\nTwo\r\nThree\nFour Token\n' }
     
-            def init_with_program(prog)
-          prog.command(:serve) do |cmd|
-            cmd.description 'Serve your site locally'
-            cmd.syntax 'serve [options]'
-            cmd.alias :server
-            cmd.alias :s
+          it 'does set the output directory' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            cloc(output_directory: '/tmp')
+          end').runner.execute(:test)
     
-        # Checks if the layout specified in the document actually exists
-    #
-    # layout - the layout to check
-    #
-    # Returns true if the layout is invalid, false if otherwise
-    def invalid_layout?(layout)
-      !data['layout'].nil? && layout.nil? && !(is_a? Jekyll::Excerpt)
+          it 'it increments all targets minor version major' do
+        Fastlane::FastFile.new.parse('lane :test do
+          increment_version_number(bump_type: 'major')
+        end').runner.execute(:test)
+    
+        version '2' do
+      self.release = '2.3.0'
+      self.base_url = 'http://coffeescript.org/'
+    
+            return a.casecmp(b) if a_length == 1 && b_length == 1
+        return 1 if a_length == 1
+        return -1 if b_length == 1
+    
+        def root?
+      path == 'index'
     end
     
-        # Returns whether the document is a draft. This is only the case if
-    # the document is in the 'posts' collection but in a different
-    # directory than '_posts'.
-    #
-    # Returns whether the document is a draft.
-    def draft?
-      data['draft'] ||= relative_path.index(collection.relative_directory).nil? &&
-        collection.label == 'posts'
-    end
-    
-      describe '.create' do
-    it 'creates a key with the client' do
-      expected_service_configs = {
-        'U27F4V844T' => [],
-        'DQ8HTZ7739' => [],
-        '6A7HVUVQ3M' => ['some-music-id']
-      }
-      mock_client_response(:create_key!, with: { name: 'New Key', service_configs: expected_service_configs }) do
-        {
-          keyId: 'a-new-key-id'
-        }
-      end
-    
-            expect(result).to eq(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER])
-      end
-    
-        expect(corrected).to eq(''something'.to_sym')
+      # if rss_url already in existing opml file, use that; otherwise, do a lookup
+  rss_url = nil
+  existing_blog = xml.xpath('//outline[@htmlUrl='#{web_url}']').first if xml
+  if existing_blog
+    rss_url = existing_blog.attr('xmlUrl')
+    puts '#{name}: ALREADY HAVE'
   end
-end
-
-    
-        status data.respond_to?(:status_code) ? data.status_code : 200
-    
-            #target = $LOADED_FEATURES.grep(/#{path}/).first
-        #puts path
-        #puts caller.map { |c| '  #{c}' }.join('\n')
-        #fontsize = [10, duration * 48].max
-        puts '#{duration},#{path},#{source}'
-      end
-      #puts caller.map { |c| ' => #{c}' }.join('\n')
-    end
