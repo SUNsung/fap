7e435b6035a5d25a23502ae67b85e3f25fff3620
SUNsung <weatherius@gmail.com>
2018-06-20 14:38:27 +0300

    Rewrite [50UMOII5X8i1c50XTec8ggru250.dtUghjbDuE50rs4.py] File in: 2018-06-20 14:38:26

--- pyton/50UMOII5X8i1c50XTec8ggru250.dtUghjbDuE50rs4.py
+++ pyton/50UMOII5X8i1c50XTec8ggru250.dtUghjbDuE50rs4.py
@@ -1,170 +1,121 @@
-    return inner
+
+        
+        index_title = 0
+index_desc = 1
+index_auth = 2
+index_https = 3
+index_cors = 4
+index_link = 5
+num_segments = 6
     
-        def run(self):
-        try:
-            self.server_sock = self._create_socket_and_bind()
-            # in case self.port = 0
-            self.port = self.server_sock.getsockname()[1]
-            self.ready_event.set()
-            self._handle_requests()
+    FLAGS = flags.FLAGS
     
-        get_item_parameters = pytest.mark.parametrize(
-        'key, value', (
-            ('bad_gateway', 502),
-            ('not_a_key', None)
-        )
-    )
-    
-    # Attempt to enable urllib3's SNI support, if possible
-try:
-    from urllib3.contrib import pyopenssl
-    pyopenssl.inject_into_urllib3()
-    
-    
-@pytest.mark.parametrize(
-    'url, expected', (
-            ('http://192.168.0.1:5000/', True),
-            ('http://192.168.0.1/', True),
-            ('http://172.16.1.1/', True),
-            ('http://172.16.1.1:5000/', True),
-            ('http://localhost.localdomain:5000/v1.0/', True),
-            ('http://google.com:6000/', True),
-            ('http://172.16.1.12/', False),
-            ('http://172.16.1.12:5000/', False),
-            ('http://google.com:5000/v1.0/', False),
-    ))
-def test_should_bypass_proxies(url, expected, monkeypatch):
-    '''Tests for function should_bypass_proxies to check if proxy
-    can be bypassed or not
-    '''
-    monkeypatch.setenv('no_proxy', '192.168.0.0/24,127.0.0.1,localhost.localdomain,172.16.1.1, google.com:6000')
-    monkeypatch.setenv('NO_PROXY', '192.168.0.0/24,127.0.0.1,localhost.localdomain,172.16.1.1, google.com:6000')
-    assert should_bypass_proxies(url, no_proxy=None) == expected
-    
-        expected_digest = (b'Authorization: Digest username='user', '
-                       b'realm='me@kennethreitz.com', '
-                       b'nonce='6bf5d6e4da1ce66918800195d6b9130d', uri='/'')
-    
-        old_layer = keras.layers.MaxPooling3D((2, 2, 2), padding='valid', dim_ordering='tf', name='maxpool3d')
-    new_layer = keras.layers.MaxPool3D(pool_size=(2, 2, 2), padding='valid', data_format='channels_last', name='maxpool3d')
-    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer.get_config())
-    
-            layer = layer_class(units, return_sequences=False,
-                            input_shape=(timesteps, input_size),
-                            activity_regularizer='l2')
-        assert layer.activity_regularizer
-        x = keras.backend.variable(np.ones((num_samples,
-                                            timesteps,
-                                            input_size)))
-        layer(x)
-        assert len(layer.get_losses_for(x)) == 1
-    
-        layer_test(local.LocallyConnected1D,
-               kwargs={'filters': filters,
-                       'kernel_size': filter_length,
-                       'padding': padding,
-                       'kernel_regularizer': 'l2',
-                       'bias_regularizer': 'l2',
-                       'activity_regularizer': 'l2',
-                       'strides': strides},
-               input_shape=(num_samples, num_steps, input_dim))
-    
-    print('Loading data...')
-(x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=max_words,
-                                                         test_split=0.2)
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
-    
-    
-def test_time_distributed_softmax():
-    x = K.placeholder(shape=(1, 1, 5))
-    f = K.function([x], [activations.softmax(x)])
-    test_values = get_standard_values()
-    test_values = np.reshape(test_values, (1, 1, np.size(test_values)))
-    f([test_values])[0]
-    
-                # test for return state:
-            x = Input(batch_shape=inputs.shape)
-            kwargs = {'data_format': data_format,
-                      'return_sequences': return_sequences,
-                      'return_state': True,
-                      'stateful': True,
-                      'filters': filters,
-                      'kernel_size': (num_row, num_col),
-                      'padding': 'valid'}
-            layer = convolutional_recurrent.ConvLSTM2D(**kwargs)
-            layer.build(inputs.shape)
-            outputs = layer(x)
-            output, states = outputs[0], outputs[1:]
-            assert len(states) == 2
-            model = Model(x, states[0])
-            state = model.predict(inputs)
-            np.testing.assert_allclose(
-                K.eval(layer.states[0]), state, atol=1e-4)
-    
-    print('Loading data...')
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
-    
-                gc.collect()
-            print('- benchmarking Lasso')
-            clf = Lasso(alpha=alpha, fit_intercept=False,
-                        precompute=precompute)
-            tstart = time()
-            clf.fit(X, Y)
-            lasso_results.append(time() - tstart)
-    
-        n = 10
-    step = 10000
-    n_samples = 10000
-    dim = 10
-    n_classes = 10
-    for i in range(n):
-        print('============================================')
-        print('Entering iteration %s of %s' % (i, n))
-        print('============================================')
-        n_samples += step
-        X = np.random.randn(n_samples, dim)
-        Y = np.random.randint(0, n_classes, (n_samples,))
-        bench_scikit_tree_classifier(X, Y)
-        Y = np.random.randn(n_samples)
-        bench_scikit_tree_regressor(X, Y)
-    
-    The goal of this exercise is to train a linear classifier on text features
-that represent sequences of up to 3 consecutive characters so as to be
-recognize natural languages by using the frequencies of short character
-sequences as 'fingerprints'.
-    
-    fit_data = data[np.argsort(model.row_labels_)]
-fit_data = fit_data[:, np.argsort(model.column_labels_)]
-    
-    The logistic regression is not a multiclass classifier out of the box. As
-a result it can identify only the first class.
-'''
-print(__doc__)
-    
-        Note that this only works using dt, custom POSTokenizer
-    instances are not supported.
-    '''
-    global dt
-    if jieba.pool is None:
-        for w in dt.cut(sentence, HMM=HMM):
-            yield w
-    else:
-        parts = strdecode(sentence).splitlines(True)
-        if HMM:
-            result = jieba.pool.map(_lcut_internal, parts)
+        for idx, (loader, srcobj, triple) in enumerate(attempts):
+        if isinstance(srcobj, Flask):
+            src_info = 'application '%s'' % srcobj.import_name
+        elif isinstance(srcobj, Blueprint):
+            src_info = 'blueprint '%s' (%s)' % (srcobj.name,
+                                                srcobj.import_name)
         else:
-            result = jieba.pool.map(_lcut_internal_no_hmm, parts)
-        for r in result:
-            for w in r:
-                yield w
+            src_info = repr(srcobj)
     
-    if opt.topK==None:
-    topK=10
-else:
-    topK = int(opt.topK)
+            # if this is not an ip and app is mounted at the root, allow subdomain
+        # matching by adding a '.' prefix
+        if self.get_cookie_path(app) == '/' and not ip:
+            rv = '.' + rv
     
-    log_f = open('1.log','wb')
-log_f.write(words.encode('utf-8'))
+        def dispatch_request(self):
+        '''Subclasses have to override this method to implement the
+        actual view function code.  This method is called with all
+        the arguments from the URL rule.
+        '''
+        raise NotImplementedError()
+    
+        with open(filename) as f:
+        contents = re.sub(
+            r'^(\s*%s\s*=\s*')(.+?)(')' % pattern,
+            inject_version, f.read(),
+            flags=re.DOTALL | re.MULTILINE
+        )
+    
+        parser.add_argument('--env',
+                        nargs=2,
+                        metavar=('KEY', 'VALUE'),
+                        action='append',
+                        help='environment variable to pass')
+    
+        'hostport': re.compile(
+        r'''^
+            ((?:                        # We want to match:
+                [^:\[\]]                # (a non-range character
+                |                       # ...or...
+                \[[^\]]*\]              # a complete bracketed expression)
+            )*)                         # repeated as many times as possible
+            :([0-9]+)                   # followed by a port number
+            $
+        ''', re.X
+    ),
+    
+    
+class TerminalModule(TerminalBase):
+    
+        def test_max_delay_none(self):
+        strategy = _exponential_backoff(retries=7, delay=1, backoff=2, max_delay=None)
+        result = list(strategy())
+        self.assertEquals(result, [1, 2, 4, 8, 16, 32, 64])
+    
+        def __init__(self, blackboard):
+        self.blackboard = blackboard
+    
+        message_center.update()
+    
+        def test_frozen_pool(self):
+        with ObjectPool(self.sample_queue) as pool:
+            self.assertEqual(pool, 'first')
+            self.assertEqual(pool, 'first')
+        self.assertTrue(self.sample_queue.get() == 'second')
+        self.assertFalse(self.sample_queue.empty())
+        self.assertTrue(self.sample_queue.get() == 'first')
+        self.assertTrue(self.sample_queue.empty())
+    
+        def test_2nd_am_station_after_scan(self):
+        self.radio.scan()
+        station = self.radio.state.stations[self.radio.state.pos]
+        expected_station = '1380'
+        self.assertEqual(station, expected_station)
+    
+    
+    
+            # simple test to validate param value
+        if param in self._static_method_choices.keys():
+            self.param = param
+        else:
+            raise ValueError('Invalid Value for Param: {0}'.format(param))
+    
+    # Test the generator
+count_to_two = lambda: count_to(2)
+count_to_five = lambda: count_to(5)
+    
+        def tearDown(self):
+        if not self._bProblem:
+            print('Tearing down')
+            time.sleep(0.1)
+            self._tm.publishReport()
+        else:
+            print('Test not executed. No tear down required.')
+    
+    
+class Transaction(object):
+    '''A transaction guard.
+    
+        context['show_on_github_url'] = show_url
+    context['edit_on_github_url'] = edit_url
+    
+    
+def volume_mute(hass):
+    '''Press the keyboard button for muting volume.'''
+    hass.services.call(DOMAIN, SERVICE_VOLUME_MUTE)
+    
+    import homeassistant.config as config_util
+    
+            self.assertIsNotNone(result)
