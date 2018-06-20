aafcd65d1c7b9192d4a77f85faf7a3c2a6c1fdfe
SUNsung <weatherius@gmail.com>
2018-06-21 00:59:44 +0300

    Rewrite [563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp] File in: 2018-06-21 00:59:43

--- cccccc/563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp
+++ cccccc/563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp
@@ -1,291 +1,136 @@
 
         
-          Status DecorateGraph(Graph* graph, Device* device) override;
-  Status PublishGraph(const Graph& graph, const string& device_name) override;
+        #ifndef ATOM_COMMON_API_REMOTE_CALLBACK_FREER_H_
+#define ATOM_COMMON_API_REMOTE_CALLBACK_FREER_H_
+#include 'atom/common/api/object_life_monitor.h'
+#include 'content/public/browser/web_contents_observer.h'
     
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    #ifndef CHROME_BROWSER_RENDERER_HOST_PEPPER_PEPPER_ISOLATED_FILE_SYSTEM_MESSAGE_FILTER_H_
+#define CHROME_BROWSER_RENDERER_HOST_PEPPER_PEPPER_ISOLATED_FILE_SYSTEM_MESSAGE_FILTER_H_
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+    namespace chrome {
+    }
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+     private:
+  Env* const env_;
+  const std::string dbname_;
+  const Options* options_;
+  Cache* cache_;
     
-    constexpr double kPicosPerMicro = 1000000.0;
+    Status WriteBatch::Iterate(Handler* handler) const {
+  Slice input(rep_);
+  if (input.size() < kHeader) {
+    return Status::Corruption('malformed WriteBatch (too small)');
+  }
+    }
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+    class Benchmark {
+ private:
+  sqlite3* db_;
+  int db_num_;
+  int num_;
+  int reads_;
+  double start_;
+  double last_op_finish_;
+  int64_t bytes_;
+  std::string message_;
+  Histogram hist_;
+  RandomGenerator gen_;
+  Random rand_;
+    }
     
-    // Returns a Boolean value indicating whether the caller is currently
-// executing in the context of the death test child process.  Tools such as
-// Valgrind heap checkers may need this to modify their behavior in death
-// tests.  IMPORTANT: This is an internal utility.  Using it may break the
-// implementation of death tests.  User code MUST NOT use it.
-GTEST_API_ bool InDeathTestChild();
+    // Returns a new environment that stores its data in memory and delegates
+// all non-file-storage tasks to base_env. The caller must delete the result
+// when it is no longer needed.
+// *base_env must remain live while the result is in use.
+Env* NewMemEnv(Env* base_env);
     
-    template <typename Generator1, typename Generator2, typename Generator3,
-    typename Generator4>
-internal::CartesianProductHolder4<Generator1, Generator2, Generator3,
-    Generator4> Combine(
-    const Generator1& g1, const Generator2& g2, const Generator3& g3,
-        const Generator4& g4) {
-  return internal::CartesianProductHolder4<Generator1, Generator2, Generator3,
-      Generator4>(
-      g1, g2, g3, g4);
+    #include <string>
+    
+    struct Options;
+    
+    // Author: kenton@google.com (Kenton Varda)
+//  Based on original Protocol Buffers design by
+//  Sanjay Ghemawat, Jeff Dean, and others.
+//
+// Generates Java code for a given .proto file.
+    
+      // overroads ImmutableMessageFieldGenerator ---------------------------------
+  void GenerateMembers(io::Printer* printer) const;
+  void GenerateBuilderMembers(io::Printer* printer) const;
+  void GenerateInitializationCode(io::Printer* printer) const;
+  void GenerateBuilderClearCode(io::Printer* printer) const;
+  void GenerateMergingCode(io::Printer* printer) const;
+  void GenerateBuildingCode(io::Printer* printer) const;
+  void GenerateParsingCode(io::Printer* printer) const;
+  void GenerateSerializationCode(io::Printer* printer) const;
+  void GenerateSerializedSizeCode(io::Printer* printer) const;
+    
+    void ParseDb(const std::string& db, std::vector<Feature>* feature_list) {
+  feature_list->clear();
+  std::string db_content(db);
+  db_content.erase(
+      std::remove_if(db_content.begin(), db_content.end(), isspace),
+      db_content.end());
+    }
+    
+    AuthPropertyIterator SecureAuthContext::end() const {
+  return AuthPropertyIterator();
 }
     
-      // Given two numbers in the sign-and-magnitude representation,
-  // returns the distance between them as an unsigned number.
-  static Bits DistanceBetweenSignAndMagnitudeNumbers(const Bits &sam1,
-                                                     const Bits &sam2) {
-    const Bits biased1 = SignAndMagnitudeToBiased(sam1);
-    const Bits biased2 = SignAndMagnitudeToBiased(sam2);
-    return (biased1 >= biased2) ? (biased1 - biased2) : (biased2 - biased1);
-  }
+      // gRPC Python
+  grpc_python_generator::GeneratorConfiguration grpc_py_config;
+  grpc_python_generator::PythonGrpcGenerator grpc_py_generator(grpc_py_config);
+  cli.RegisterGenerator('--grpc_python_out', &grpc_py_generator,
+                        'Generate Python source file.');
     
-      template <typename T>
-  operator ParamGenerator<T>() const {
-    const T array[] = {static_cast<T>(v1_), static_cast<T>(v2_),
-        static_cast<T>(v3_), static_cast<T>(v4_), static_cast<T>(v5_),
-        static_cast<T>(v6_), static_cast<T>(v7_), static_cast<T>(v8_),
-        static_cast<T>(v9_), static_cast<T>(v10_), static_cast<T>(v11_),
-        static_cast<T>(v12_), static_cast<T>(v13_), static_cast<T>(v14_),
-        static_cast<T>(v15_), static_cast<T>(v16_), static_cast<T>(v17_),
-        static_cast<T>(v18_), static_cast<T>(v19_), static_cast<T>(v20_),
-        static_cast<T>(v21_), static_cast<T>(v22_), static_cast<T>(v23_),
-        static_cast<T>(v24_), static_cast<T>(v25_), static_cast<T>(v26_),
-        static_cast<T>(v27_), static_cast<T>(v28_), static_cast<T>(v29_),
-        static_cast<T>(v30_), static_cast<T>(v31_), static_cast<T>(v32_),
-        static_cast<T>(v33_), static_cast<T>(v34_), static_cast<T>(v35_),
-        static_cast<T>(v36_), static_cast<T>(v37_), static_cast<T>(v38_),
-        static_cast<T>(v39_), static_cast<T>(v40_), static_cast<T>(v41_),
-        static_cast<T>(v42_), static_cast<T>(v43_), static_cast<T>(v44_),
-        static_cast<T>(v45_), static_cast<T>(v46_), static_cast<T>(v47_),
-        static_cast<T>(v48_)};
-    return ValuesIn(array);
-  }
-    
-      ParamGenerator& operator=(const ParamGenerator& other) {
-    impl_ = other.impl_;
-    return *this;
-  }
-    
-    #include 'gtest/internal/gtest-port.h'
-    
-    
-    {}  // namespace
-    
-            DestType *destData = dest.data();
-        if (elementCount > dest.size())
-            RuntimeError('Copy: The output buffer size (%zu) is smaller than the number (%zu) of source elements to copy.', dest.size(), elementCount);
-    
-    template <class ElemType>
-class NDLUtil
-{
-    typedef shared_ptr<ComputationNode<ElemType>> ComputationNodePtr;
-    }
-    
-    #include <vector>
-#include <memory>
-#include 'latticearchive.h'
-    
-    namespace xgboost {
-    }
-    
-    TEST(Metric, AMS) {
-  EXPECT_ANY_THROW(xgboost::Metric::Create('ams'));
-  xgboost::Metric * metric = xgboost::Metric::Create('ams@0.5f');
-  ASSERT_STREQ(metric->Name(), 'ams@0.5');
-  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 0.311f, 0.001f);
-  EXPECT_NEAR(GetMetricEval(metric,
-                            {0.1f, 0.9f, 0.1f, 0.9f},
-                            {  0,   0,   1,   1}),
-              0.29710f, 0.001f);
-    }
-    
-    class BaseLogger {
- public:
-  BaseLogger() {
-#if XGBOOST_LOG_WITH_TIME
-    log_stream_ << '[' << dmlc::DateLogger().HumanDate() << '] ';
-#endif
-  }
-  std::ostream& stream() { return log_stream_; }  // NOLINT
-    }
-    
-    
+        // change all inputs of this new node to share the old one's inputs
+    for (int i = 0; i < oldNode->GetNumInputs(); i++)
     {
-    {        break;
-    }
-}
-    
-    class CommFrequencyLimit {
-  public:
-    CommFrequencyLimit(size_t _count, uint64_t _time_span);
-    ~CommFrequencyLimit();
+        newNode->SetInput(i, oldNode->GetInputs()[i]); // TODO: use AttachInput()?
+        //oldNode->SetInput(i, nullptr); // BUGBUG: old node should no longer point into the network
     }
     
-    #ifndef PublicComponent_spy_h
-#define PublicComponent_spy_h
-    
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
-    
-    #ifndef _COMM_FUNCTION_H_
-#define _COMM_FUNCTION_H_
-    
-    class ScopeJEnv {
-  public:
-    ScopeJEnv(JavaVM* jvm, jint _capacity = 16);
-    ~ScopeJEnv();
+        // remove all the added BN nodes from evaluation group
+    for (auto& bnNode : bnNodes)
+    {
+        m_net->RemoveFromNodeGroup(L'evaluation', bnNode);
     }
     
-      const char* name_;
+    #include <string>
+#include <chrono>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <memory>
     
-    class FileEntry;
-class RequestGroup;
-class CheckIntegrityEntry;
-class DownloadContext;
-class SegmentMan;
-class PieceStorage;
-class Request;
-class DownloadEngine;
-class Segment;
-class SocketCore;
-class Option;
-class SocketRecvBuffer;
-#ifdef ENABLE_ASYNC_DNS
-class AsyncNameResolver;
-class AsyncNameResolverMan;
-#endif // ENABLE_ASYNC_DNS
-    
-    bool AbstractHttpServerResponseCommand::execute()
-{
-  if (e_->getRequestGroupMan()->downloadFinished() || e_->isHaltRequested()) {
-    return true;
-  }
-  try {
-    ssize_t len = httpServer_->sendResponse();
-    if (len > 0) {
-      timeoutTimer_ = global::wallclock();
-    }
-  }
-  catch (RecoverableException& e) {
-    A2_LOG_INFO_EX(fmt('CUID#%' PRId64
-                       ' - Error occurred while transmitting response body.',
-                       getCuid()),
-                   e);
-    return true;
-  }
-  if (httpServer_->sendBufferIsEmpty()) {
-    A2_LOG_INFO(fmt('CUID#%' PRId64 ' - HttpServer: all response transmitted.',
-                    getCuid()));
-    afterSend(httpServer_, e_);
-    return true;
-  }
-  else {
-    if (timeoutTimer_.difference(global::wallclock()) >= 30_s) {
-      A2_LOG_INFO(fmt('CUID#%' PRId64
-                      ' - HttpServer: Timeout while trasmitting response.',
-                      getCuid()));
-      return true;
-    }
-    else {
-      updateReadWriteCheck();
-      e_->addCommand(std::unique_ptr<Command>(this));
-      return false;
-    }
-  }
-}
-    
-    
-    {} // namespace aria2
-    
-    void AnnounceTier::nextEventIfAfterStarted()
-{
-  switch (event) {
-  case STOPPED:
-    event = HALTED;
-    break;
-  case COMPLETED:
-    event = SEEDING;
-    break;
-  default:
-    break;
-  }
-}
-    
-      CFRef<SecIdentitySearchRef> search;
-  SecIdentitySearchRef raw_search;
-    
-    class AsyncNameResolverMan {
-public:
-  AsyncNameResolverMan();
-  // Destructor does not call disableNameResolverCheck(). Application
-  // must call it before the destruction of this object.
-  ~AsyncNameResolverMan();
-  // Enable IPv4 address lookup. default: true
-  void setIPv4(bool ipv4) { ipv4_ = ipv4; }
-  // Enable IPv6 address lookup. default: true
-  void setIPv6(bool ipv6) { ipv6_ = ipv6; }
-  // Returns true if asynchronous name resolution has been started.
-  bool started() const;
-  // Starts asynchronous name resolution.
-  void startAsync(const std::string& hostname, DownloadEngine* e,
-                  Command* command);
-  // Appends resolved addresses to |res|.
-  void getResolvedAddress(std::vector<std::string>& res) const;
-  // Adds resolvers to DownloadEngine to check event notification.
-  void setNameResolverCheck(DownloadEngine* e, Command* command);
-  // Removes resolvers from DownloadEngine.
-  void disableNameResolverCheck(DownloadEngine* e, Command* command);
-  // Returns true if any of resolvers are added in DownloadEngine.
-  bool resolverChecked() const { return resolverCheck_; }
-  // Returns status value: 0 for inprogress, 1 for success and -1 for
-  // failure.
-  int getStatus() const;
-  // Returns last error string
-  const std::string& getLastError() const;
-  // Resets state. Also removes resolvers from DownloadEngine.
-  void reset(DownloadEngine* e, Command* command);
-    }
-    
-    
-#ifdef LOG_TAG
-# define ALOGV(...) ::facebook::alog::logv(LOG_TAG, __VA_ARGS__)
-# define ALOGD(...) ::facebook::alog::logd(LOG_TAG, __VA_ARGS__)
-# define ALOGI(...) ::facebook::alog::logi(LOG_TAG, __VA_ARGS__)
-# define ALOGW(...) ::facebook::alog::logw(LOG_TAG, __VA_ARGS__)
-# define ALOGE(...) ::facebook::alog::loge(LOG_TAG, __VA_ARGS__)
-# define ALOGF(...) ::facebook::alog::logf(LOG_TAG, __VA_ARGS__)
+        bool empty() const
+    {
+#ifndef NONUMLATTICEMMI // TODO:set NUM lattice to null so as to save memory
+        if (numlattices.empty() ^ denlattices.empty())
+            RuntimeError('latticesource: numerator and denominator lattices must be either both empty or both not empty');
 #endif
+        return denlattices.empty();
+    }
     
-      ProgramLocation(const char* functionName, const char* fileName, int line) :
-      m_functionName(functionName),
-      m_fileName(fileName),
-      m_lineNumber(line)
+        int unit;
+    double value;
+    
+    template<typename... ARGS>
+inline void log(int level, const char* tag, const char* msg, ARGS... args) noexcept {
+  __android_log_print(level, tag, msg, args...);
+}
+    
+    
     {}
+
     
-      bool isTheLastRef() const {
-    FBASSERT(m_ptr);
-    return m_ptr->hasOnlyOneRef();
+    
+    {  T* operator->() const {
+    return m_instance;
   }
-    
-    #include 'CoreClasses.h'
-#include 'References-forward.h'
+private:
+  T* m_instance;
+};
