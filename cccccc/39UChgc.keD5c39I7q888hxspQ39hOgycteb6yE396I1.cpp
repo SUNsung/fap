0f00ddabe207e5437df581cb1b389096f6547a76
SUNsung <weatherius@gmail.com>
2018-06-20 11:07:54 +0300

    Rewrite [39UChgc.keD5c39I7q888hxspQ39hOgycteb6yE396I1.cpp] File in: 2018-06-20 11:07:53

--- cccccc/39UChgc.keD5c39I7q888hxspQ39hOgycteb6yE396I1.cpp
+++ cccccc/39UChgc.keD5c39I7q888hxspQ39hOgycteb6yE396I1.cpp
@@ -1,268 +1,146 @@
 
         
-        // Sent by the renderer when the draggable regions are updated.
-IPC_MESSAGE_ROUTED1(ShellViewHostMsg_UpdateDraggableRegions,
-                    std::vector<extensions::DraggableRegion> /* regions */)
-    
-    #include 'base/logging.h'
-#include 'base/values.h'
-    
-    #endif  // CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
-
-    
-    void Menu::Popup(int x, int y, content::Shell* shell) {
-  GdkEventButton* event = NULL; //FIXME: shell->web_contents()->GetRenderWidgetHostView()->GetLastMouseDown();
-  uint32_t triggering_event_time = event ? event->time : GDK_CURRENT_TIME;
-  gfx::Point point;
-  if (!event) {
-    // gfx::Rect bounds = shell->web_contents()->GetRenderWidgetHostView()->GetViewBounds();
-    // point = gfx::Point(x + bounds.x(), y + bounds.y());
-    DVLOG(1) << 'no last mouse down event';
-    point = gfx::Point(x, y);
-  }else
-    point = gfx::Point(event->x_root, event->y_root);
-    }
-    
-      for(auto* item: menu_items_) {
-    item->RemoveKeys();
-  }
-    
-    
-    {  gtk_widget_show(menu_item_);
-  g_object_ref_sink(G_OBJECT(menu_item_));
-} 
-    
-    ExtensionFunction::ResponseAction
-NwAppQuitFunction::Run() {
-  ExtensionService* service =
-    ExtensionSystem::Get(browser_context())->extension_service();
-  base::MessageLoop::current()->task_runner()->PostTask(
-        FROM_HERE,
-        base::Bind(&ExtensionService::TerminateExtension,
-                   service->AsWeakPtr(),
-                   extension_id()));
-  return RespondNow(NoArguments());
+        PyDescriptorDatabase::PyDescriptorDatabase(PyObject* py_database)
+    : py_database_(py_database) {
+  Py_INCREF(py_database_);
 }
     
-    class NwClipboardSetListSyncFunction : public NWSyncExtensionFunction {
- public:
-  NwClipboardSetListSyncFunction();
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    #include <string>
+    
+    void RepeatedEnumFieldGenerator::WriteEquals(io::Printer* printer) {
+  printer->Print(
+    variables_,
+    'if(!$name$_.Equals(other.$name$_)) return false;\n');
+}
+    
+    namespace google {
+namespace protobuf {
+namespace compiler {
+namespace csharp {
+    }
+    }
+    }
     }
     
-    #include 'extensions/browser/extension_function.h'
+    SourceGeneratorBase::~SourceGeneratorBase() {
+}
     
-      /// Open the acceptor using the specified protocol.
-  /**
-   * This function opens the socket acceptor so that it will use the specified
-   * protocol.
-   *
-   * @param protocol An object specifying which protocol is to be used.
-   *
-   * @throws boost::system::system_error Thrown on failure.
-   *
-   * @par Example
-   * @code
-   * boost::asio::ip::tcp::acceptor acceptor(io_service);
-   * acceptor.open(boost::asio::ip::tcp::v4());
-   * @endcode
-   */
-  void open(const protocol_type& protocol = protocol_type())
-  {
-    boost::system::error_code ec;
-    this->get_service().open(this->get_implementation(), protocol, ec);
-    boost::asio::detail::throw_error(ec, 'open');
-  }
     
-      /// Perform a blocking wait on the timer.
-  /**
-   * This function is used to wait for the timer to expire. This function
-   * blocks and does not return until the timer has expired.
-   *
-   * @throws boost::system::system_error Thrown on failure.
-   */
-  void wait()
-  {
-    boost::system::error_code ec;
-    this->service.wait(this->implementation, ec);
-    boost::asio::detail::throw_error(ec, 'wait');
-  }
-    
-      static bool all_empty(const boost::asio::mutable_buffers_1& buffer_sequence)
-  {
-    return boost::asio::buffer_size(buffer_sequence) == 0;
-  }
-    
-    #endif // BOOST_ASIO_DETAIL_BUFFERED_STREAM_STORAGE_HPP
+    {}  // namespace google
+#endif  // GOOGLE_PROTOBUF_COMPILER_JAVA_EXTENSION_LITE_H__
 
     
+    // Author: kenton@google.com (Kenton Varda)
+//  Based on original Protocol Buffers design by
+//  Sanjay Ghemawat, Jeff Dean, and others.
+//
+// Generates Java code for a given .proto file.
     
-    {private:
-  // The top of the stack of calls for the current thread.
-  static tss_ptr<context> top_;
+    // ===================================================================
+    
+      // overroads RepeatedImmutableMessageFieldGenerator -------------------------
+  void GenerateMembers(io::Printer* printer) const;
+  void GenerateBuilderMembers(io::Printer* printer) const;
+  void GenerateParsingCode(io::Printer* printer) const;
+  void GenerateSerializationCode(io::Printer* printer) const;
+  void GenerateSerializedSizeCode(io::Printer* printer) const;
+    
+    	ClassDB::bind_method(D_METHOD('poll'), &StreamPeerSSL::poll);
+	ClassDB::bind_method(D_METHOD('accept_stream', 'stream'), &StreamPeerSSL::accept_stream);
+	ClassDB::bind_method(D_METHOD('connect_to_stream', 'stream', 'validate_certs', 'for_hostname'), &StreamPeerSSL::connect_to_stream, DEFVAL(false), DEFVAL(String()));
+	ClassDB::bind_method(D_METHOD('get_status'), &StreamPeerSSL::get_status);
+	ClassDB::bind_method(D_METHOD('disconnect_from_stream'), &StreamPeerSSL::disconnect_from_stream);
+    
+    
+    {	if (_is_multiplayer) {
+		// need to wait for ID confirmation...
+	} else {
+		emit_signal('connection_established', p_protocol);
+	}
+}
+    
+    	virtual bool is_server() const;
+	virtual ConnectionStatus get_connection_status() const = 0;
+    
+    	static void add_signal(StringName p_class, const MethodInfo &p_signal);
+	static bool has_signal(StringName p_class, StringName p_signal);
+	static bool get_signal(StringName p_class, StringName p_signal, MethodInfo *r_signal);
+	static void get_signal_list(StringName p_class, List<MethodInfo> *p_signals, bool p_no_inheritance = false);
+    
+    #ifndef VEHICLE_BODY_H
+#define VEHICLE_BODY_H
+    
+    #ifdef ANDROID
+#include <jni.h>
+    
+    #ifndef DUMPCRASHSTACK_H_
+#define DUMPCRASHSTACK_H_
+    
+    // Licensed under the MIT License (the 'License'); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+// http://opensource.org/licenses/MIT
+    
+    // Licensed under the MIT License (the 'License'); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+// http://opensource.org/licenses/MIT
+    
+      protected:
+    ServiceBase(const char* _servicename) : m_servicename(_servicename) {}
+    
+    //
+//  spy.h
+//  PublicComponent
+//
+//  Created by yerungui on 14-5-14.
+//
+    
+    
+    static bool SpyHookLogFunc(struct XLoggerInfo_t& _info, std::string& _log);
+    void TestFun0();
+    int __TestFun1(int i);
+    
+    #define CALL_NONVIRT_TYPE(_jtype, _jname)                                   \
+    CALL_NONVIRT_TYPE_METHOD(_jtype, _jname)                                \
+    CALL_NONVIRT_TYPE_METHODV(_jtype, _jname)                               \
+    CALL_NONVIRT_TYPE_METHODA(_jtype, _jname)
+    
+    template <typename T> struct jni_sig_from_cxx_t;
+template <typename R, typename... Args>
+struct jni_sig_from_cxx_t<R(Args...)> {
+  using JniRet = typename Convert<typename std::decay<R>::type>::jniType;
+  using JniSig = JniRet(typename Convert<typename std::decay<Args>::type>::jniType...);
 };
     
-    template<class T, class TimeSystem>
-class base_time;
-    
-    #if !defined(BOOST_ASIO_HAS_THREADS) \
-  || defined(BOOST_ASIO_DISABLE_FENCED_BLOCK)
-# include <boost/asio/detail/null_fenced_block.hpp>
-#elif defined(__MACH__) && defined(__APPLE__)
-# include <boost/asio/detail/macos_fenced_block.hpp>
-#elif defined(__sun)
-# include <boost/asio/detail/solaris_fenced_block.hpp>
-#elif defined(__GNUC__) && defined(__arm__) \
-  && !defined(__GCC_HAVE_SYNC_COMPARE_AND_SWAP_4)
-# include <boost/asio/detail/gcc_arm_fenced_block.hpp>
-#elif defined(__GNUC__) && (defined(__hppa) || defined(__hppa__))
-# include <boost/asio/detail/gcc_hppa_fenced_block.hpp>
-#elif defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
-# include <boost/asio/detail/gcc_x86_fenced_block.hpp>
-#elif defined(__GNUC__) \
-  && ((__GNUC__ == 4 && __GNUC_MINOR__ >= 1) || (__GNUC__ > 4)) \
-  && !defined(__INTEL_COMPILER) && !defined(__ICL) \
-  && !defined(__ICC) && !defined(__ECC) && !defined(__PATHSCALE__)
-# include <boost/asio/detail/gcc_sync_fenced_block.hpp>
-#elif defined(BOOST_ASIO_WINDOWS) && !defined(UNDER_CE)
-# include <boost/asio/detail/win_fenced_block.hpp>
-#else
-# include <boost/asio/detail/null_fenced_block.hpp>
-#endif
-    
-    namespace boost {
-namespace asio {
-namespace detail {
-    }
-    }
-    }
-    
-    #define BOOST_ASIO_COMPOSED_CONNECT_HANDLER_CHECK( \
-    handler_type, handler, iter_type) \
-  \
-  typedef BOOST_ASIO_HANDLER_TYPE(handler_type, \
-      void(boost::system::error_code, iter_type)) \
-    asio_true_handler_type; \
-  \
-  BOOST_ASIO_HANDLER_TYPE_REQUIREMENTS_ASSERT( \
-      sizeof(boost::asio::detail::two_arg_handler_test( \
-          boost::asio::detail::clvref< \
-            asio_true_handler_type>(), \
-          static_cast<const boost::system::error_code*>(0), \
-          static_cast<const iter_type*>(0))) == 1, \
-      'ComposedConnectHandler type requirements not met') \
-  \
-  typedef boost::asio::detail::handler_type_requirements< \
-      sizeof( \
-        boost::asio::detail::argbyv( \
-          boost::asio::detail::clvref< \
-            asio_true_handler_type>())) + \
-      sizeof( \
-        boost::asio::detail::lvref< \
-          asio_true_handler_type>()( \
-            boost::asio::detail::lvref<const boost::system::error_code>(), \
-            boost::asio::detail::lvref<const iter_type>()), \
-        char(0))> BOOST_ASIO_UNUSED_TYPEDEF
-    
-      // The list of spare nodes waiting to be recycled. Assumes that POD types only
-  // are stored in the hash map.
-  std::list<value_type> spares_;
-    
-      if (shutdown_)
-  {
-    io_service_.post_immediate_completion(op, false);
-    return;
-  }
-    
-    #ifndef BOOST_ASIO_DETAIL_IMPL_EPOLL_REACTOR_HPP
-#define BOOST_ASIO_DETAIL_IMPL_EPOLL_REACTOR_HPP
-    
-    // Used to encapsulate a particular instance of an opened database.
-//
-// This object should not be used directly in C++; it exists solely to provide
-// a mapping from a JavaScript object to a C++ code that can use the RocksDB
-// API.
-class DBWrapper : public node::ObjectWrap {
-  public:
-    static void Init(Handle<Object> exports);
-    }
-    
-    namespace rocksdb {
-    }
-    
-      class StatisticsJni : public StatisticsImpl {
-   public:
-     StatisticsJni(std::shared_ptr<Statistics> stats);
-     StatisticsJni(std::shared_ptr<Statistics> stats,
-         const std::set<uint32_t> ignore_histograms);
-     virtual bool HistEnabledForType(uint32_t type) const override;
-    }
-    
-    #pragma once
-    
-    #include 'rocksdb/compaction_filter.h'
-#include 'rocksjni/jnicallback.h'
-    
-    PlatformColorCode GetPlatformColorCode(LogColor color) {
-#ifdef BENCHMARK_OS_WINDOWS
-  switch (color) {
-    case COLOR_RED:
-      return FOREGROUND_RED;
-    case COLOR_GREEN:
-      return FOREGROUND_GREEN;
-    case COLOR_YELLOW:
-      return FOREGROUND_RED | FOREGROUND_GREEN;
-    case COLOR_BLUE:
-      return FOREGROUND_BLUE;
-    case COLOR_MAGENTA:
-      return FOREGROUND_BLUE | FOREGROUND_RED;
-    case COLOR_CYAN:
-      return FOREGROUND_BLUE | FOREGROUND_GREEN;
-    case COLOR_WHITE:  // fall through to default
-    default:
-      return 0;
-  }
-#else
-  switch (color) {
-    case COLOR_RED:
-      return '1';
-    case COLOR_GREEN:
-      return '2';
-    case COLOR_YELLOW:
-      return '3';
-    case COLOR_BLUE:
-      return '4';
-    case COLOR_MAGENTA:
-      return '5';
-    case COLOR_CYAN:
-      return '6';
-    case COLOR_WHITE:
-      return '7';
-    default:
-      return nullptr;
-  };
-#endif
+    /* static */ void Config::destroy(Config * node)
+{
+    delete node;
 }
     
-    std::string FormatString(const char* msg, va_list args);
-std::string FormatString(const char* msg, ...);
+     public: // Measure func inspectors
     
-    bool SameNames(UserCounters const& l, UserCounters const& r) {
-  if (&l == &r) return true;
-  if (l.size() != r.size()) {
-    return false;
-  }
-  for (auto const& c : l) {
-    if (r.find(c.first) == r.end()) {
-      return false;
+    struct Size
+{
+    double width;
+    double height;
     }
+    
+        method(setFlex);
+    method(setFlexBasis);
+    method(setFlexBasisPercent);
+    method(setFlexGrow);
+    method(setFlexShrink);
+    
+    private:
+  void ref() {
+    ++m_refcount;
   }
-  return true;
-}
+    
+    namespace facebook {
+    }
+    
+    FBEXPORT void assertInternal(const char* formatstr, ...) __attribute__((noreturn));
     
     
-    {#ifndef NDEBUG
-  Out << '***WARNING*** Library was built as DEBUG. Timings may be '
-         'affected.\n';
-#endif
-}
+    {  uint8_t* getDirectBytes() const;
+  size_t getDirectSize() const;
+};
