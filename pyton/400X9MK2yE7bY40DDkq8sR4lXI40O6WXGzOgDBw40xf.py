2ecac5aa323463fc3cec46c5917ccc97584d9285
SUNsung <weatherius@gmail.com>
2018-06-21 23:49:07 +0300

    Rewrite [400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py] File in: 2018-06-21 23:49:07

--- pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
+++ pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
@@ -1,117 +1,105 @@
 
         
-        
-@pytest.fixture
-def httpbin_secure(httpbin_secure):
-    return prepare_url(httpbin_secure)
-
-    
-        @classmethod
-    def basic_response_server(cls, **kwargs):
-        return cls.text_response_server(
-            'HTTP/1.1 200 OK\r\n' +
-            'Content-Length: 0\r\n\r\n',
-            **kwargs
-        )
-    
-    elif is_py3:
-    from urllib.parse import urlparse, urlunparse, urljoin, urlsplit, urlencode, quote, unquote, quote_plus, unquote_plus, urldefrag
-    from urllib.request import parse_http_list, getproxies, proxy_bypass, proxy_bypass_environment, getproxies_environment
-    from http import cookiejar as cookielib
-    from http.cookies import Morsel
-    from io import StringIO
-    from collections import OrderedDict
-    
-    import pytest
-    
-    from . import utils
-from . import packages
-from .models import Request, Response, PreparedRequest
-from .api import request, get, head, post, patch, put, delete, options
-from .sessions import session, Session
-from .status_codes import codes
-from .exceptions import (
-    RequestException, Timeout, URLRequired,
-    TooManyRedirects, HTTPError, ConnectionError,
-    FileModeWarning, ConnectTimeout, ReadTimeout
-)
-    
-        def __init__(self, *args, **kwargs):
-        '''Initialize RequestException with `request` and `response` objects.'''
-        response = kwargs.pop('response', None)
-        self.response = response
-        self.request = kwargs.pop('request', None)
-        if (response is not None and not self.request and
-                hasattr(response, 'request')):
-            self.request = self.response.request
-        super(RequestException, self).__init__(*args, **kwargs)
-    
-        auth = requests.auth.HTTPDigestAuth('user', 'pass')
-    
-    # If true, do not generate a @detailmenu in the 'Top' node's menu.
-#texinfo_no_detailmenu = False
-    
-        @mock.patch('certbot.display.enhancements.util')
-    def test_redirect(self, mock_util):
-        mock_util().menu.return_value = (display_util.OK, 1)
-        self.assertTrue(self._call('redirect'))
-    
-        description = 'Example Installer plugin'
-    
-        >>> _sum([3, 2.25, 4.5, -0.5, 1.0], 0.75)
-    (<class 'float'>, Fraction(11, 1), 5)
-    
-    #
-# NOTE: This is the pi function from the decimal documentation, modified
-# for benchmarking purposes. Since floats do not have a context, the higher
-# intermediate precision from the original is NOT used, so the modified
-# algorithm only gives an approximation to the correctly rounded result.
-# For serious use, refer to the documentation or the appropriate literature.
+        # Incompatible changes from the 2.x nntplib:
+# - all commands are encoded as UTF-8 data (using the 'surrogateescape'
+#   error handler), except for raw message data (POST, IHAVE)
+# - all responses are decoded as UTF-8 data (using the 'surrogateescape'
+#   error handler), except for raw message data (ARTICLE, HEAD, BODY)
+# - the `file` argument to various methods is keyword-only
 #
-def pi_float():
-    '''native float'''
-    lasts, t, s, n, na, d, da = 0, 3.0, 3, 1, 0, 0, 24
-    while s != lasts:
-        lasts = s
-        n, na = n+na, na+8
-        d, da = d+da, da+32
-        t = (t * n) / d
-        s += t
-    return s
+# - NNTP.date() returns a datetime object
+# - NNTP.newgroups() and NNTP.newnews() take a datetime (or date) object,
+#   rather than a pair of (date, time) strings.
+# - NNTP.newgroups() and NNTP.list() return a list of GroupInfo named tuples
+# - NNTP.descriptions() returns a dict mapping group names to descriptions
+# - NNTP.xover() returns a list of dicts mapping field names (header or metadata)
+#   to field values; each dict representing a message overview.
+# - NNTP.article(), NNTP.head() and NNTP.body() return a (response, ArticleInfo)
+#   tuple.
+# - the 'internal' methods have been marked private (they now start with
+#   an underscore)
     
-    import re
-    
-        def _pre_message_hook(self, f):
-        '''Called before writing each message to file f.'''
-        f.write(b'\001\001\001\001' + linesep)
-    
-    
-def makemakefile(outfp, vars, files, target):
-    save = sys.stdout
-    try:
-        sys.stdout = outfp
-        realwork(vars, files, target)
-    finally:
-        sys.stdout = save
-    
-        @support.skip_unless_symlink
-    def test_iterdir_symlink(self):
-        # __iter__ on a symlink to a directory
-        P = self.cls
-        p = P(BASE, 'linkB')
-        paths = set(p.iterdir())
-        expected = { P(BASE, 'linkB', q) for q in ['fileB', 'linkD'] }
-        self.assertEqual(paths, expected)
-    
-        def setdelegate(self, delegate):
-        if self.delegate is not None:
-            self.unbind('<<toggle-auto-coloring>>')
-        Delegator.setdelegate(self, delegate)
-        if delegate is not None:
-            self.config_colors()
-            self.bind('<<toggle-auto-coloring>>', self.toggle_colorize_event)
-            self.notify_range('1.0', 'end')
+        @cpython_only
+    def test_import_bug(self):
+        # We simulate a bug in importlib and check that it's not stripped
+        # away from the traceback.
+        self.create_module('foo', '')
+        importlib = sys.modules['_frozen_importlib_external']
+        if 'load_module' in vars(importlib.SourceLoader):
+            old_exec_module = importlib.SourceLoader.exec_module
         else:
-            # No delegate - stop any colorizing
-            self.stop_colorizing = True
-            self.allow_colorizing = False
+            old_exec_module = None
+        try:
+            def exec_module(*args):
+                1/0
+            importlib.SourceLoader.exec_module = exec_module
+            try:
+                import foo
+            except ZeroDivisionError as e:
+                tb = e.__traceback__
+            else:
+                self.fail('ZeroDivisionError should have been raised')
+            self.assert_traceback(tb, [__file__, '<frozen importlib', __file__])
+        finally:
+            if old_exec_module is None:
+                del importlib.SourceLoader.exec_module
+            else:
+                importlib.SourceLoader.exec_module = old_exec_module
+    
+    @dataclasses.dataclass
+class CV:
+    T_CV4 = typing.ClassVar
+    cv0: typing.ClassVar[int] = 20
+    cv1: typing.ClassVar = 30
+    cv2: T_CV2
+    cv3: T_CV3
+    not_cv4: T_CV4  # When using string annotations, this field is not recognized as a ClassVar.
+    
+    import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+    
+    
+try:
+    from sys import is_finalizing
+except ImportError:
+    # Emulate it
+    def _get_emulated_is_finalizing():
+        L = []
+        atexit.register(lambda: L.append(None))
+    
+        .. deprecated:: 5.0
+       Tornado ``Futures`` have been merged with `asyncio.Future`,
+       so this method is now a no-op.
+    '''
+    return asyncio_future
+    
+        def on_ping(self, data):
+        pass
+    
+        def resolve(self, host, port, family=socket.AF_UNSPEC, *args, **kwargs):
+        if (host, port, family) in self.mapping:
+            host, port = self.mapping[(host, port, family)]
+        elif (host, port) in self.mapping:
+            host, port = self.mapping[(host, port)]
+        elif host in self.mapping:
+            host = self.mapping[host]
+        return self.resolver.resolve(host, port, family, *args, **kwargs)
+    
+        def set(self, value):
+        if self.multiple:
+            if not isinstance(value, list):
+                raise Error('Option %r is required to be a list of %s' %
+                            (self.name, self.type.__name__))
+            for item in value:
+                if item is not None and not isinstance(item, self.type):
+                    raise Error('Option %r is required to be a list of %s' %
+                                (self.name, self.type.__name__))
+        else:
+            if value is not None and not isinstance(value, self.type):
+                raise Error('Option %r is required to be a %s (%s given)' %
+                            (self.name, self.type.__name__, type(value)))
+        self._value = value
+        if self.callback is not None:
+            self.callback(self._value)
