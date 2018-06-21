b2ac3a9c3c9ba4561575998fcf94c6086525b3fa
SUNsung <weatherius@gmail.com>
2018-06-21 03:35:03 +0300

    Rewrite [58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py] File in: 2018-06-21 03:35:03

--- pyton/58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py
+++ pyton/58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py
@@ -1,152 +1,189 @@
 
         
-        
-root = Root()
-factory = Site(root)
-reactor.listenTCP(8880, factory)
-reactor.run()
-
-    
-    if hasattr(ctypes, 'windll'):
-    WSAStringToAddressA = ctypes.windll.ws2_32.WSAStringToAddressA
-    WSAAddressToStringA = ctypes.windll.ws2_32.WSAAddressToStringA
-else:
-    def not_windows():
-        raise SystemError(
-            'Invalid platform. ctypes.windll must be available.'
-        )
-    WSAStringToAddressA = not_windows
-    WSAAddressToStringA = not_windows
-    
-    A tree.RewriteCardinalityException is raised, when the parsers hits a
-cardinality mismatch during AST construction. Although this is basically a
-bug in your grammar, it can only be detected at runtime.
-    
-    ## All tokens go to the parser (unless skip() is called in that rule)
-# on a particular 'channel'.  The parser tunes to a particular channel
-# so that whitespace etc... can go to the parser on a 'hidden' channel.
-DEFAULT_CHANNEL = 0
-    
-            i = self.p
-        n = 1
-        # find k good tokens looking backwards
-        while n <= k:
-            # skip off-channel tokens
-            i = self.skipOffTokenChannelsReverse(i-1) # leave p on valid token
-            n += 1
-    
-    Local worker thread:
-- reads work ids from the 'Work Ids' queue and looks up the corresponding
-  WorkItem from the 'Work Items' dict: if the work item has been cancelled then
-  it is simply removed from the dict, otherwise it is repackaged as a
-  _CallItem and put in the 'Call Q'. New _CallItems are put in the 'Call Q'
-  until 'Call Q' is full. NOTE: the size of the 'Call Q' is kept small because
-  calls placed in the 'Call Q' can no longer be cancelled with Future.cancel().
-- reads _ResultItems from 'Result Q', updates the future stored in the
-  'Work Items' dict and deletes the dict entry
-    
-        def shutdown(self, wait=True):
-        with self._shutdown_lock:
-            self._shutdown = True
-            self._work_queue.put(None)
-        if wait:
-            for t in self._threads:
-                t.join()
-    shutdown.__doc__ = _base.Executor.shutdown.__doc__
-
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
+                '''
+        available_plugins = plugin_manager.get_formatters_grouped()
+        self.enabled_plugins = []
+        for group in groups:
+            for cls in available_plugins[group]:
+                p = cls(env=env, **kwargs)
+                if p.enabled:
+                    self.enabled_plugins.append(p)
     
     
-def EndsWithPython_BadPaths_test():
-  not_python_paths = [
-    None,
-    '',
-    '/opt/local/bin/vim',
-    r'C:\Program Files\Vim\vim74\gvim.exe',
-    '/usr/bin/python2.5',
-    '/home/user/.pyenv/shims/python3.2',
-  ]
-    
-        @gen_test
-    def asyncSetUp(self):
-        listener, port = bind_unused_port()
-        event = Event()
+def patharg(path):
+    '''
+    Back slashes need to be escaped in ITEM args,
+    even in Windows paths.
     
     
-def wrap_web_tests_application():
-    result = {}
-    for cls in web_test.wsgi_safe_tests:
-        def class_factory():
-            class WSGIApplicationWrappedTest(cls):  # type: ignore
-                def setUp(self):
-                    self.warning_catcher = ignore_deprecation()
-                    self.warning_catcher.__enter__()
-                    super(WSGIApplicationWrappedTest, self).setUp()
-    
-        def test_204_invalid_content_length(self):
-        # 204 status with non-zero content length is malformed
-        with ExpectLog(gen_log, '.*Response with code 204 should not have body'):
-            with ignore_deprecation():
-                response = self.fetch('/?error=1')
-            if not self.http1:
-                self.skipTest('requires HTTP/1.x')
-            if self.http_client.configured_class != SimpleAsyncHTTPClient:
-                self.skipTest('curl client accepts invalid headers')
-            self.assertEqual(response.code, 599)
-    
-    API_TYPES = ('api', 'json')
-RSS_TYPES = ('rss', 'xml')
-    
-        def is_employee(self, user):
-        if not user:
-            return False
-        return user.employee
+def test_unicode_form_item_verbose(httpbin):
+    r = http('--verbose', '--form',
+             'POST', httpbin.url + '/post', u'test=%s' % UNICODE)
+    assert HTTP_OK in r
+    assert UNICODE in r
     
     
-class APIv1GoldController(OAuth2OnlyController):
-    def _gift_using_creddits(self, recipient, months=1, thing_fullname=None,
-            proxying_for=None):
-        with creddits_lock(c.user):
-            if not c.user.employee and c.user.gold_creddits < months:
-                err = RedditError('INSUFFICIENT_CREDDITS')
-                self.on_validation_error(err)
+def print_debug_info(env):
+    env.stderr.writelines([
+        'HTTPie %s\n' % httpie_version,
+        'Requests %s\n' % requests_version,
+        'Pygments %s\n' % pygments_version,
+        'Python %s\n%s\n' % (sys.version, sys.executable),
+        '%s %s' % (platform.system(), platform.release()),
+    ])
+    env.stderr.write('\n\n')
+    env.stderr.write(repr(env))
+    env.stderr.write('\n')
     
-        @csrf_exempt
-    @json_validate(
-        VRatelimit(rate_ip=True, prefix='rate_register_'),
-        signature=VSigned(),
-        name=VUname(['user']),
-        email=ValidEmail('email'),
-        password=VPasswordChange(['passwd', 'passwd2']),
-    )
-    def POST_register(self, responder, name, email, password, **kwargs):
-        kwargs.update(dict(
-            controller=self,
-            form=responder('noop'),
-            responder=responder,
-            name=name,
-            email=email,
-            password=password,
-        ))
-        return handle_register(**kwargs)
+        http://docs.python-requests.org/en/latest/user/advanced/#transport-adapters
     
-    class ButtonsController(RedditController):
-    def get_wrapped_link(self, url, link = None, wrapper = None):
+    
+DEFAULT_CONFIG_DIR = str(os.environ.get(
+    'HTTPIE_CONFIG_DIR',
+    os.path.expanduser('~/.httpie') if not is_windows else
+    os.path.expandvars(r'%APPDATA%\\httpie')
+))
+    
+    '''
+__version__ = '1.0.0-dev'
+__author__ = 'Jakub Roztocil'
+__licence__ = 'BSD'
+    
+        strings = []
+    with open(filepath, 'r') as f:
+    
+        def test_karatsuba(self):
+        digits = list(range(1, 5)) + list(range(KARATSUBA_CUTOFF,
+                                                KARATSUBA_CUTOFF + 10))
+        digits.extend([KARATSUBA_CUTOFF * 10, KARATSUBA_CUTOFF * 100])
+    
+    to_benchmark = [pi_float, pi_decimal]
+if C is not None:
+    to_benchmark.insert(1, pi_cdecimal)
+    
+    Usage:
+while True:
+    try:
+        chunk = Chunk(file)
+    except EOFError:
+        break
+    chunktype = chunk.getname()
+    while True:
+        data = chunk.read(nbytes)
+        if not data:
+            pass
+        # do something with data
+    
+            readermode is sometimes necessary if you are connecting to an
+        NNTP server on the local machine and intend to call
+        reader-specific commands, such as `group'.  If you get
+        unexpected NNTPPermanentErrors, you might need to set
+        readermode.
+        '''
+        self.host = host
+        self.file = file
+        self.debugging = 0
+        self.welcome = self._getresp()
+    
+            Interpret all HTTP GET requests as requests for server
+        documentation.
+        '''
+        # Check that the path is legal
+        if not self.is_rpc_path_valid():
+            self.report_404()
+            return
+    
+                env['HOME'] = '/tmp'
+            self.assertEqual(p1.expanduser(), P('/tmp/Documents'))
+            self.assertEqual(p2.expanduser(), P(userhome) / 'Documents')
+            self.assertEqual(p3.expanduser(), P(otherhome) / 'Documents')
+            self.assertEqual(p4.expanduser(), p4)
+            self.assertEqual(p5.expanduser(), p5)
+            self.assertEqual(p6.expanduser(), p6)
+            self.assertRaises(RuntimeError, p7.expanduser)
+    
+        Args:
+        call_queue: A multiprocessing.Queue of _CallItems that will be read and
+            evaluated by the worker.
+        result_queue: A multiprocessing.Queue of _ResultItems that will written
+            to by the worker.
+        shutdown: A multiprocessing.Event that will be set as a signal to the
+            worker that it should exit when call_queue is empty.
+    '''
+    while True:
+        call_item = call_queue.get(block=True)
+        if call_item is None:
+            # Wake up queue management thread
+            result_queue.put(None)
+            return
         try:
-            links = []
-            if link:
-                links = [link]
-            else:
-                sr = None if isinstance(c.site, FakeSubreddit) else c.site
-                try:
-                    links = Link._by_url(url, sr)
-                except NotFound:
-                    pass
+            r = call_item.fn(*call_item.args, **call_item.kwargs)
+        except BaseException:
+            e = sys.exc_info()[1]
+            result_queue.put(_ResultItem(call_item.work_id,
+                                         exception=e))
+        else:
+            result_queue.put(_ResultItem(call_item.work_id,
+                                         result=r))
     
-    from BeautifulSoup import BeautifulSoup, Tag
+    
+def FormatDebugInfoResponse_Completer_ServerNotRunningWithNoLogfiles_test():
+  response = deepcopy( GENERIC_RESPONSE )
+  response[ 'completer' ][ 'servers' ][ 0 ].update( {
+    'is_running': False,
+    'logfiles': []
+  } )
+  assert_that(
+    FormatDebugInfoResponse( response ),
+    contains_string(
+      'Completer name completer debug information:\n'
+      '  Server name not running\n'
+      '  Server name executable: /path/to/executable\n'
+      '  No logfiles available\n'
+      '  Server name key: value\n'
+      '  Key: value\n'
+    )
+  )
+
+    
+    
+class TestSplunk(unittest.TestCase):
+    '''Test the Splunk component.'''
+    
+    
+def volume_mute(hass):
+    '''Press the keyboard button for muting volume.'''
+    hass.services.call(DOMAIN, SERVICE_VOLUME_MUTE)
+    
+    
+def _get_homehub_data(url):
+    '''Return mock homehub data.'''
+    return '''
+    [
+        {
+            'mac': 'AA:BB:CC:DD:EE:FF,
+            'hostname': 'hostname',
+            'ip': '192.168.1.43',
+            'ipv6': '',
+            'name': 'hostname',
+            'activity': '1',
+            'os': 'Unknown',
+            'device': 'Unknown',
+            'time_first_seen': '2016/06/05 11:14:45',
+            'time_last_active': '2016/06/06 11:33:08',
+            'dhcp_option': '39043T90430T9TGK0EKGE5KGE3K904390K45GK054',
+            'port': 'wl0',
+            'ipv6_ll': 'fe80::gd67:ghrr:fuud:4332',
+            'activity_ip': '1',
+            'activity_ipv6_ll': '0',
+            'activity_ipv6': '0',
+            'device_oui': 'NA',
+            'device_serial': 'NA',
+            'device_class': 'NA'
+        }
+    ]
+    '''
+    
+    
+class TestRecorderRuns(unittest.TestCase):
+    '''Test recorder run model.'''
