3f09f7279705d776cee6607cf469a34453e398b4
SUNsung <weatherius@gmail.com>
2018-06-21 05:19:20 +0300

    Rewrite [26ftrQbxMQ8.g26l8dfJ.BEtc26y5EmOr3Q2LA26uBm.cpp] File in: 2018-06-21 05:19:20

--- cccccc/26ftrQbxMQ8.g26l8dfJ.BEtc26y5EmOr3Q2LA26uBm.cpp
+++ cccccc/26ftrQbxMQ8.g26l8dfJ.BEtc26y5EmOr3Q2LA26uBm.cpp
@@ -1,122 +1,181 @@
 
         
-        #endif  // CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
-
+        
+    {PyObject* NewMessageOneofsByName(const Descriptor* descriptor);
+PyObject* NewMessageOneofsSeq(const Descriptor* descriptor);
+}  // namespace message_descriptor
     
+    #endif  // GOOGLE_PROTOBUF_COMPILER_CSHARP_REPEATED_MESSAGE_FIELD_H__
     
-    {  menu_items_.empty();
-}
-    
-    #include 'base/values.h'
-#include 'content/nw/src/api/dispatcher_host.h'
-#include 'content/nw/src/api/menu/menu.h'
-#include 'gdk/gdkkeysyms.h'//to get keyval from name
-    
-          std::string encoded_image_base64;
-      std::string encoded_image_str(encoded_image.data(), encoded_image.data() + encoded_image.size());
-      base::Base64Encode(encoded_image_str, &encoded_image_base64);
-    
-    bool NwShellOpenItemFunction::RunNWSync(base::ListValue* response, std::string* error) {
-  nw::ObjectManager* manager = nw::ObjectManager::Get(browser_context());
-  manager->OnCallStaticMethod(render_frame_host(), 'Shell', 'OpenItem', *args_);
-  return true;
-}
-    
-    namespace leveldb {
-namespace log {
+    namespace google {
+namespace protobuf {
+namespace compiler {
+namespace csharp {
+    }
+    }
     }
     }
     
-      // Store the specified number as the sequence number for the start of
-  // this batch.
-  static void SetSequence(WriteBatch* batch, SequenceNumber seq);
+    void WriteMessageDocComment(io::Printer* printer, const Descriptor* message);
+void WriteFieldDocComment(io::Printer* printer, const FieldDescriptor* field);
+void WriteEnumDocComment(io::Printer* printer, const EnumDescriptor* enum_);
+void WriteEnumValueDocComment(io::Printer* printer,
+                              const EnumValueDescriptor* value);
+void WriteServiceDocComment(io::Printer* printer,
+                            const ServiceDescriptor* service);
+void WriteMethodDocComment(io::Printer* printer,
+                           const MethodDescriptor* method);
     
-      const FilterPolicy* policy_;
-  std::string keys_;              // Flattened key contents
-  std::vector<size_t> start_;     // Starting index in keys_ of each key
-  std::string result_;            // Filter data computed so far
-  std::vector<Slice> tmp_keys_;   // policy_->CreateFilter() argument
-  std::vector<uint32_t> filter_offsets_;
+        HHVM_FE(gmp_abs);
+    HHVM_FE(gmp_add);
+    HHVM_FE(gmp_and);
+    HHVM_FE(gmp_clrbit);
+    HHVM_FE(gmp_cmp);
+    HHVM_FE(gmp_com);
+    HHVM_FE(gmp_div_q);
+    HHVM_FALIAS(gmp_div, gmp_div_q);
+    HHVM_FE(gmp_div_qr);
+    HHVM_FE(gmp_div_r);
+    HHVM_FE(gmp_divexact);
+    HHVM_FE(gmp_fact);
+    HHVM_FE(gmp_gcd);
+    HHVM_FE(gmp_gcdext);
+    HHVM_FE(gmp_hamdist);
+    HHVM_FE(gmp_init);
+    HHVM_FE(gmp_intval);
+    HHVM_FE(gmp_invert);
+    HHVM_FE(gmp_jacobi);
+    HHVM_FE(gmp_legendre);
+    HHVM_FE(gmp_mod);
+    HHVM_FE(gmp_mul);
+    HHVM_FE(gmp_neg);
+    HHVM_FE(gmp_nextprime);
+    HHVM_FE(gmp_or);
+    HHVM_FE(gmp_perfect_square);
+    HHVM_FE(gmp_popcount);
+    HHVM_FE(gmp_pow);
+    HHVM_FE(gmp_powm);
+    HHVM_FE(gmp_prob_prime);
+    HHVM_FE(gmp_random);
+    HHVM_FE(gmp_root);
+    HHVM_FE(gmp_rootrem);
+    HHVM_FE(gmp_scan0);
+    HHVM_FE(gmp_scan1);
+    HHVM_FE(gmp_setbit);
+    HHVM_FE(gmp_sign);
+    HHVM_FE(gmp_sqrt);
+    HHVM_FE(gmp_sqrtrem);
+    HHVM_FE(gmp_strval);
+    HHVM_FE(gmp_sub);
+    HHVM_FE(gmp_testbit);
+    HHVM_FE(gmp_xor);
     
-    static int NextLength(int length) {
-  if (length < 10) {
-    length += 1;
-  } else if (length < 100) {
-    length += 10;
-  } else if (length < 1000) {
-    length += 100;
-  } else {
-    length += 1000;
-  }
-  return length;
+        for (auto src : cg.targets[fid].preds) {
+      auto& arc = *cg.arcs.find(Arc(src, fid));
+      if (bestPred == InvalidId || arc.normalizedWeight > bestProb) {
+        bestPred = arc.src;
+        bestProb = arc.normalizedWeight;
+      }
+    }
+    
+    
+void Instruction::SetPCRelLoadStoreTarget(Instruction* target) {
+  int offset = (target - this) >> kInstructionSizeLog2;
+  Instr imm = Assembler::ImmPCRelLoadStoreAddress(offset);
+  SetInstructionBits(Mask(~ImmLLiteral_mask) | imm);
 }
     
-    const double Histogram::kBucketLimit[kNumBuckets] = {
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20, 25, 30, 35, 40, 45,
-  50, 60, 70, 80, 90, 100, 120, 140, 160, 180, 200, 250, 300, 350, 400, 450,
-  500, 600, 700, 800, 900, 1000, 1200, 1400, 1600, 1800, 2000, 2500, 3000,
-  3500, 4000, 4500, 5000, 6000, 7000, 8000, 9000, 10000, 12000, 14000,
-  16000, 18000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 60000,
-  70000, 80000, 90000, 100000, 120000, 140000, 160000, 180000, 200000,
-  250000, 300000, 350000, 400000, 450000, 500000, 600000, 700000, 800000,
-  900000, 1000000, 1200000, 1400000, 1600000, 1800000, 2000000, 2500000,
-  3000000, 3500000, 4000000, 4500000, 5000000, 6000000, 7000000, 8000000,
-  9000000, 10000000, 12000000, 14000000, 16000000, 18000000, 20000000,
-  25000000, 30000000, 35000000, 40000000, 45000000, 50000000, 60000000,
-  70000000, 80000000, 90000000, 100000000, 120000000, 140000000, 160000000,
-  180000000, 200000000, 250000000, 300000000, 350000000, 400000000,
-  450000000, 500000000, 600000000, 700000000, 800000000, 900000000,
-  1000000000, 1200000000, 1400000000, 1600000000, 1800000000, 2000000000,
-  2500000000.0, 3000000000.0, 3500000000.0, 4000000000.0, 4500000000.0,
-  5000000000.0, 6000000000.0, 7000000000.0, 8000000000.0, 9000000000.0,
-  1e200,
-};
-    
-    
-    {}  // namespace leveldb
-    
-    #include 'leveldb/db.h'
-#include 'db/db_impl.h'
-#include 'leveldb/cache.h'
-#include 'util/testharness.h'
-#include 'util/testutil.h'
-    
-    LookupKey::LookupKey(const Slice& user_key, SequenceNumber s) {
-  size_t usize = user_key.size();
-  size_t needed = usize + 13;  // A conservative estimate
-  char* dst;
-  if (needed <= sizeof(space_)) {
-    dst = space_;
-  } else {
-    dst = new char[needed];
-  }
-  start_ = dst;
-  dst = EncodeVarint32(dst, usize + 8);
-  kstart_ = dst;
-  memcpy(dst, user_key.data(), usize);
-  dst += usize;
-  EncodeFixed64(dst, PackSequenceAndType(s, kValueTypeForSeek));
-  dst += 8;
-  end_ = dst;
-}
-    
-    int main(int argc, char** argv) {
-  leveldb::Env* env = leveldb::Env::Default();
-  bool ok = true;
-  if (argc < 2) {
-    Usage();
-    ok = false;
-  } else {
-    std::string command = argv[1];
-    if (command == 'dump') {
-      ok = leveldb::HandleDumpCommand(env, argv+2, argc-2);
-    } else {
-      Usage();
-      ok = false;
+    void SynchronizableMulti::notifyAll() {
+  for (auto& cond_list : m_cond_list_vec) {
+    while (!cond_list.empty()) {
+      pthread_cond_signal(cond_list.front());
+      cond_list.pop_front();
     }
   }
-  return (ok ? 0 : 1);
+}
+    
+    
+    {
+    {
+    {  for (const auto& share_line : osquery::split(content, '\n')) {
+    genNFSShare(share_line, results);
+  }
+  return results;
+}
+}
 }
 
     
-      ~Writer();
+    class TLSConfigPlugin : public ConfigPlugin,
+                        public std::enable_shared_from_this<TLSConfigPlugin> {
+ public:
+  Status setUp() override;
+  Status genConfig(std::map<std::string, std::string>& config) override;
+    }
+    
+    #include <iomanip>
+    
+    TEST_F(ProcessTests, test_getpid) {
+  int pid = -1;
+    }
+    
+    
+    {  return std::string(buffer.data());
+}
+    
+      hr = WBEM_S_NO_ERROR;
+  while (hr == WBEM_S_NO_ERROR) {
+    IWbemClassObject* result = nullptr;
+    ULONG result_count = 0;
+    }
+    
+      EXPECT_TRUE(pathExists(path_ + '.backup'));
+    
+    /// Show all tables and exit the shell.
+DECLARE_bool(L);
+    
+     private:
+  /// Disk arbitration session.
+  DASessionRef session_{nullptr};
+    
+    using EventTappingEventContextRef = std::shared_ptr<EventTappingEventContext>;
+using EventTappingSubscriptionContextRef =
+    std::shared_ptr<EventTappingSubscriptionContext>;
+    
+     private:
+  /**
+   * @brief The existing configure-time discovered path.
+   *
+   * The FSEvents publisher expects paths from a configuration to contain
+   * filesystem globbing wildcards, as opposed to SQL wildcards. It also expects
+   * paths to be canonicalized up to the first wildcard. To FSEvents a double
+   * wildcard, meaning recursive, is a watch on the base path string. A single
+   * wildcard means the same watch but a preserved globbing pattern, which is
+   * applied at event-fire time to limit subscriber results.
+   *
+   * This backup will allow post-fire subscriptions to match. It will also allow
+   * faster reconfigures by not performing string manipulation twice.
+   */
+  std::string discovered_;
+    
+    
+    {
+    {      // Each event type may use a specific event type structure.
+      KernelEventContextRef ec = nullptr;
+      switch (event_type) {
+      case OSQUERY_PROCESS_EVENT:
+        ec = createEventContextFrom<osquery_process_event_t>(event_type, event);
+        fire(ec);
+        break;
+      case OSQUERY_FILE_EVENT:
+        ec = createEventContextFrom<osquery_file_event_t>(event_type, event);
+        fire(ec);
+        break;
+      default:
+        LOG(WARNING) << 'Unknown kernel event received: ' << event_type;
+        break;
+      }
+      max_before_lock--;
+    }
+    return true;
+  };
