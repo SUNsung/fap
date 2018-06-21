af31c192e15a9407cad3b65eba5f4e2479d987f9
SUNsung <weatherius@gmail.com>
2018-06-21 08:24:31 +0300

    Rewrite [14blTeQcPYeAE147cd3tHBDa9o14IFKNake5W4Y14Lj6.rb] File in: 2018-06-21 08:24:31

--- ruby/14blTeQcPYeAE147cd3tHBDa9o14IFKNake5W4Y14Lj6.rb
+++ ruby/14blTeQcPYeAE147cd3tHBDa9o14IFKNake5W4Y14Lj6.rb
@@ -1,3 +1,21 @@
 
         
-        
+          def present_hash(hash, skip_key = nil)
+    hash.to_a.sort_by {|a| a.first.to_s }.map { |k, v| '#{k}: #{v}' unless k.to_s == skip_key.to_s }.compact
+  end
+end
+
+    
+      included do
+    include Oauthable
+    
+      def validate_option(method)
+    if self.respond_to? 'validate_#{method}'.to_sym
+      self.send('validate_#{method}'.to_sym)
+    else
+      false
+    end
+  end
+    
+      def destroy
+    @event.destroy
