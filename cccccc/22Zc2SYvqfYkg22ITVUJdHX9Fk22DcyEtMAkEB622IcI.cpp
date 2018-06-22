035574877f3fa48f0ab17f24f35518db8401e890
SUNsung <weatherius@gmail.com>
2018-06-22 07:57:56 +0300

    Rewrite [22Zc2SYvqfYkg22ITVUJdHX9Fk22DcyEtMAkEB622IcI.cpp] File in: 2018-06-22 07:57:55

--- cccccc/22Zc2SYvqfYkg22ITVUJdHX9Fk22DcyEtMAkEB622IcI.cpp
+++ cccccc/22Zc2SYvqfYkg22ITVUJdHX9Fk22DcyEtMAkEB622IcI.cpp
@@ -1,303 +1,66 @@
 
         
-        // Like URLRequestAsarJob, but asks the JavaScript handler for file path.
-class URLRequestAsyncAsarJob : public JsAsker<asar::URLRequestAsarJob> {
- public:
-  URLRequestAsyncAsarJob(net::URLRequest*, net::NetworkDelegate*);
+        namespace osquery {
     }
     
-    class UnresponsiveSuppressor {
- public:
-  UnresponsiveSuppressor();
-  ~UnresponsiveSuppressor();
+     protected:
+  /// Calculate the URL once and cache the result.
+  std::string uri_;
+    
+      {
+    auto dropper = DropPrivileges::get();
+    EXPECT_TRUE(dropper->dropTo(nobody->pw_uid, nobody->pw_gid));
+    EXPECT_EQ(geteuid(), nobody->pw_uid);
     }
     
-    namespace mate {
+    TEST_F(ProcessTests, test_launchWorker) {
+  {
+    std::vector<char*> argv;
+    for (size_t i = 0; i < kExpectedWorkerArgsCount; i++) {
+      char* entry = new char[strlen(kExpectedWorkerArgs[i]) + 1];
+      EXPECT_NE(entry, nullptr);
+      memset(entry, '\0', strlen(kExpectedWorkerArgs[i]) + 1);
+      memcpy(entry, kExpectedWorkerArgs[i], strlen(kExpectedWorkerArgs[i]));
+      argv.push_back(entry);
+    }
+    argv.push_back(nullptr);
+    }
     }
     
-    #endif  // CHROME_BROWSER_CHROME_NOTIFICATION_TYPES_H_
-
-    
-      // Starts updating the model. The model is initially empty, so OnSourceAdded()
-  // notifications will be generated for each existing source as it is
-  // enumerated. After the initial enumeration the model will be refreshed based
-  // on the update period, and notifications generated only for changes in the
-  // model.
-  virtual void StartUpdating(DesktopMediaListObserver* observer) = 0;
-    
-      std::unique_ptr<ppapi::host::ResourceHost> CreateResourceHost(
-      ppapi::host::PpapiHost* host,
-      PP_Resource resource,
-      PP_Instance instance,
-      const IPC::Message& message) override;
-    
-    void NwCurrentWindowInternalCapturePageInternalFunction::OnCaptureFailure(FailureReason reason) {
-  const char* reason_description = 'internal error';
-  switch (reason) {
-    case FAILURE_REASON_UNKNOWN:
-      reason_description = 'unknown error';
-      break;
-    case FAILURE_REASON_ENCODING_FAILED:
-      reason_description = 'encoding failed';
-      break;
-    case FAILURE_REASON_VIEW_INVISIBLE:
-      reason_description = 'view is invisible';
-      break;
-  }
-  error_ = ErrorUtils::FormatErrorMessage('Failed to capture tab: *',
-                                          reason_description);
-  Respond(Error(error_));
+    TEST_F(StatusTests, test_constructor) {
+  auto s = Status(5, 'message');
+  EXPECT_EQ(s.getCode(), 5);
+  EXPECT_EQ(s.getMessage(), 'message');
 }
     
+      EXPECT_FALSE(cl3.matches(1001));
+  EXPECT_TRUE(cl3.matches(1000));
     
-    {  for (size_t i = 0; i < windows.size(); ++i) {
-    // Only send close event to browser windows, since devtools windows will
-    // be automatically closed.
-    if (!windows[i]->is_devtools()) {
-      // If there is no js object bound to the window, then just close.
-      if (force || windows[i]->ShouldCloseWindow(quit))
-        // we used to delete the Shell object here
-        // but it should be deleted on native window destruction
-        windows[i]->window()->Close();
+    Status WmiResultItem::GetUnsignedInt32(const std::string& name,
+                                       unsigned int& ret) const {
+  std::wstring property_name = stringToWstring(name);
+  VARIANT value;
+  HRESULT hr = result_->Get(property_name.c_str(), 0, &value, nullptr, nullptr);
     }
+    
+     public:
+  Status setUp() override;
+    
+      /// If list is empty, return false and leave *result unchanged.
+  /// Else, remove the first/last elem, store it in *result, and return true
+  bool PopLeft(const std::string& key, std::string* result);  // First
+  bool PopRight(const std::string& key, std::string* result); // Last
+    
+      StatisticsJni::StatisticsJni(std::shared_ptr<Statistics> stats)
+      : StatisticsImpl(stats, false), m_ignore_histograms() {
   }
-  if (force) {
-    // in a special force close case, since we're going to exit the
-    // main loop soon, we should delete the shell object asap so the
-    // render widget can be closed on the renderer side
-    windows = Shell::windows();
-    for (size_t i = 0; i < windows.size(); ++i) {
-      if (!windows[i]->is_devtools())
-        delete windows[i];
-    }
-  }
-}
-    
-      static void Call(content::Shell* shell,
-                   const std::string& method,
-                   const base::ListValue& arguments,
-                   base::ListValue* result,
-                   DispatcherHost* dispatcher_host);
-    
-    namespace content {
-class RenderFrameHost;
-}
-    
-    
-#ifndef CONTENT_NW_SRC_API_EVENT_EVENT_H_
-#define CONTENT_NW_SRC_API_EVENT_EVENT_H_
     
      private:
-  ObjectManager* object_manager_;
-    
-    void Menu::UpdateKeys(GtkAccelGroup *gtk_accel_group){
-  this->gtk_accel_group = gtk_accel_group;
-  if (!GTK_IS_ACCEL_GROUP(gtk_accel_group)){
-    return ;
-  } else {
-    std::vector<MenuItem*>::iterator menu_item_iterator = menu_items.begin();
-    std::vector<MenuItem*>::iterator menu_item_end = menu_items.end();
-    while (menu_item_iterator != menu_item_end){
-      MenuItem *menu_item = *menu_item_iterator;
-      if (menu_item!=NULL && GTK_IS_MENU_ITEM(menu_item->menu_item_)){
-        menu_item->UpdateKeys(gtk_accel_group);
-      }
-      ++menu_item_iterator;
-    }
-  }
-}
-    
-    
-
-    
-    #include 'content/nw/src/api/menuitem/menuitem.h'
-    
-    class NwAppClearCacheFunction : public NWSyncExtensionFunction, public content::BrowsingDataRemover::Observer {
- public:
-  NwAppClearCacheFunction();
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-  void OnBrowsingDataRemoverDone() override;
-    }
-    
-      void NwDesktopCaptureMonitor::Stop() {
-    started_ = false;
-    media_list_.clear();
-  }
-    
-    class AutoCompactTest {
- public:
-  std::string dbname_;
-  Cache* tiny_cache_;
-  Options options_;
-  DB* db_;
-    }
-    
-    
-    {    if (s.ok()) {
-      // Verify that the table is usable
-      Iterator* it = table_cache->NewIterator(ReadOptions(),
-                                              meta->number,
-                                              meta->file_size);
-      s = it->status();
-      delete it;
-    }
-  }
-    
-    void DBIter::Prev() {
-  assert(valid_);
-    }
-    
-    void InternalFilterPolicy::CreateFilter(const Slice* keys, int n,
-                                        std::string* dst) const {
-  // We rely on the fact that the code in table.cc does not mind us
-  // adjusting keys[].
-  Slice* mkey = const_cast<Slice*>(keys);
-  for (int i = 0; i < n; i++) {
-    mkey[i] = ExtractUserKey(keys[i]);
-    // TODO(sanjay): Suppress dups?
-  }
-  user_policy_->CreateFilter(keys, n, dst);
-}
-    
-    // Called on every item found in a WriteBatch.
-class WriteBatchItemPrinter : public WriteBatch::Handler {
- public:
-  WritableFile* dst_;
-  virtual void Put(const Slice& key, const Slice& value) {
-    std::string r = '  put '';
-    AppendEscapedStringTo(&r, key);
-    r += '' '';
-    AppendEscapedStringTo(&r, value);
-    r += ''\n';
-    dst_->Append(r);
-  }
-  virtual void Delete(const Slice& key) {
-    std::string r = '  del '';
-    AppendEscapedStringTo(&r, key);
-    r += ''\n';
-    dst_->Append(r);
-  }
-};
-    
-    #ifndef STORAGE_LEVELDB_DB_FILENAME_H_
-#define STORAGE_LEVELDB_DB_FILENAME_H_
-    
-    
-    {    // Write to database
-    for (int i = 0; i < num_entries; i++)
-    {
-      const int k = (order == SEQUENTIAL) ? i : (rand_.Next() % num_entries);
-      char key[100];
-      snprintf(key, sizeof(key), '%016d', k);
-      bytes_ += value_size + strlen(key);
-      std::string cpp_key = key;
-      if (!db_->set(cpp_key, gen_.Generate(value_size).ToString())) {
-        fprintf(stderr, 'set error: %s\n', db_->error().name());
-      }
-      FinishedSingleOp();
-    }
-  }
-    
-    #ifndef STORAGE_LEVELDB_HELPERS_MEMENV_MEMENV_H_
-#define STORAGE_LEVELDB_HELPERS_MEMENV_MEMENV_H_
-    
-    // Return a new filter policy that uses a bloom filter with approximately
-// the specified number of bits per key.  A good value for bits_per_key
-// is 10, which yields a filter with ~ 1% false positive rate.
-//
-// Callers must delete the result after any database that is using the
-// result has been closed.
-//
-// Note: if you are using a custom comparator that ignores some parts
-// of the keys being compared, you must not use NewBloomFilterPolicy()
-// and must provide your own FilterPolicy that also ignores the
-// corresponding parts of the keys.  For example, if the comparator
-// ignores trailing spaces, it would be incorrect to use a
-// FilterPolicy (like NewBloomFilterPolicy) that does not ignore
-// trailing spaces in keys.
-extern const FilterPolicy* NewBloomFilterPolicy(int bits_per_key);
-    
-    bool js_cocos2dx_builder_CCBAnimationManager_constructor(JSContext *cx, uint32_t argc, jsval *vp);
-void js_cocos2dx_builder_CCBAnimationManager_finalize(JSContext *cx, JSObject *obj);
-void js_register_cocos2dx_builder_CCBAnimationManager(JSContext *cx, JS::HandleObject global);
-void register_all_cocos2dx_builder(JSContext* cx, JS::HandleObject obj);
-bool js_cocos2dx_builder_CCBAnimationManager_moveAnimationsFromNode(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setAutoPlaySequenceId(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getDocumentCallbackNames(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_actionForSoundChannel(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setBaseValue(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getDocumentOutletNodes(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getLastCompletedSequenceName(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setRootNode(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_runAnimationsForSequenceNamedTweenDuration(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_addDocumentOutletName(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getRootContainerSize(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setDocumentControllerName(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setObject(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getContainerSize(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_actionForCallbackChannel(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getDocumentOutletNames(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_addDocumentCallbackControlEvents(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_init(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getKeyframeCallbacks(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getDocumentCallbackControlEvents(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setRootContainerSize(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_runAnimationsForSequenceIdTweenDuration(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getRunningSequenceName(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getAutoPlaySequenceId(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_addDocumentCallbackName(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getRootNode(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_addDocumentOutletNode(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setDelegate(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getSequenceDuration(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_addDocumentCallbackNode(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_runAnimationsForSequenceNamed(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getSequenceId(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setCallFunc(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getDocumentCallbackNodes(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_setSequences(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_debug(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_getDocumentControllerName(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_builder_CCBAnimationManager_CCBAnimationManager(JSContext *cx, uint32_t argc, jsval *vp);
-    
-    extern JSClass  *jsb_cocos2d_NavMeshAgent_class;
-extern JSObject *jsb_cocos2d_NavMeshAgent_prototype;
-    
-    	void Step(Settings* settings)
-	{
-		// Drive the kinematic body.
-		if (m_platform->GetType() == b2_kinematicBody)
-		{
-			b2Vec2 p = m_platform->GetTransform().p;
-			b2Vec2 v = m_platform->GetLinearVelocity();
-    }
-    }
-    
-    	void Step(Settings* settings)
-	{
-		Test::Step(settings);
-    }
-    
-    		for (int32 i = 0; i < 2; ++i)
-		{
-			b2CircleShape shape;
-			shape.m_radius = 0.5f;
-    }
-    
-    //
-//  boost_exception.cpp
-//  comm
-//
-//  Created by yanguoyue on 16/5/20.
-//
-    
-    #endif
-
-    
-    
-    {  private:
-    JavaVM* vm_;
-    JNIEnv* env_;
-    bool we_attach_;
-    int status_;
-};
+    // used for synchronisation in compare method
+    std::unique_ptr<port::Mutex> mtx_compare;
+    // used for synchronisation in findShortestSeparator method
+    std::unique_ptr<port::Mutex> mtx_findShortestSeparator;
+    std::unique_ptr<const char[]> m_name;
+    jmethodID m_jCompareMethodId;
+    jmethodID m_jFindShortestSeparatorMethodId;
+    jmethodID m_jFindShortSuccessorMethodId;
