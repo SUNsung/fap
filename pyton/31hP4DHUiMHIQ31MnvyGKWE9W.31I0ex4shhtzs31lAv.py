4c966b39425d6addc29d15068a343e3e523ee127
SUNsung <weatherius@gmail.com>
2018-06-22 16:51:00 +0300

    Rewrite [31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py] File in: 2018-06-22 16:50:59

--- pyton/31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py
+++ pyton/31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py
@@ -1,225 +1,236 @@
 
         
-            # The large learning rate should trigger check_numerics.
-    with self.assertRaisesRegexp(tf.errors.InvalidArgumentError,
-                                 'Cost is not finite'):
-      self.RunFullTrainingAndInference(
-          'tagger-parser',
-          'tagger_parser_master_spec.textproto',
-          hyperparam_config=hyperparam_config,
-          component_weights=[0., 1., 1.],
-          unroll_using_oracle=[False, True, True],
-          expected_num_actions=12,
-          expected=_TAGGER_PARSER_EXPECTED_SENTENCES)
+        if isinstance(helptext, bytes):
+    helptext = helptext.decode('utf-8')
     
-            self.assertAlmostEqual(tf.exp(log_partition_functions[0]).eval(), 1.0)
-        self.assertAlmostEqual(tf.exp(log_partition_functions[1]).eval(), 1.0)
-        self.assertAlmostEqual(tf.exp(log_partition_functions[2]).eval(), 1.0)
+        for release in releases:
+        compat_print(release['name'])
+        for asset in release['assets']:
+            asset_name = asset['name']
+            total_bytes += asset['download_count'] * asset['size']
+            if all(not re.match(p, asset_name) for p in (
+                    r'^youtube-dl$',
+                    r'^youtube-dl-\d{4}\.\d{2}\.\d{2}(?:\.\d+)?\.tar\.gz$',
+                    r'^youtube-dl\.exe$')):
+                continue
+            compat_print(
+                ' %s size: %s downloads: %d'
+                % (asset_name, format_size(asset['size']), asset['download_count']))
     
-        return [lengths_b, scores_bxnxn, scores_bnxn, arcs_bnxn]
+        def test_youporn(self):
+        self._assert_restricted(
+            'http://www.youporn.com/watch/505835/sex-ed-is-it-safe-to-masturbate-daily/',
+            '505835.mp4', 2, old_age=25)
     
-    py_binary(
-    name = 'build_pip_package',
-    srcs = ['build_pip_package.py'],
-    data = ['oss_setup.py'],
-    deps = [
-        ':all_in_one_components',
-    ],
-)
+        def setUp(self):
+        if self._SKIP_SOCKS_TEST:
+            return
     
-      # Read in serialized protos from training data.
-  train_corpus = sentence_io.ConllSentenceReader(
-      train_corpus_path, projectivize=projectivize_train_corpus).corpus()
-  tune_corpus = sentence_io.ConllSentenceReader(
-      tune_corpus_path, projectivize=False).corpus()
-  gold_tune_corpus = tune_corpus
     
-      # Convert word-based docs to char-based documents for segmentation training
-  # and evaluation.
-  with tf.Session(graph=tf.Graph()) as tmp_session:
-    char_training_set_op = gen_parser_ops.segmenter_training_data_constructor(
-        training_set)
-    char_dev_set_op = gen_parser_ops.char_token_generator(dev_set)
-    char_training_set = tmp_session.run(char_training_set_op)
-    char_dev_set = tmp_session.run(char_dev_set_op)
+TEST_DIR = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'swftests')
     
-            if isinstance(headers, bytes):
-            # Python < 3
-            headers = headers.decode('utf8')
-        return headers
+            retval = subprocess.call(args)
+        if retval == 0:
+            fsize = os.path.getsize(encodeFilename(tmpfilename))
+            self.to_screen('\r[%s] %s bytes' % (args[0], fsize))
+            self.try_rename(tmpfilename, filename)
+            self._hook_progress({
+                'downloaded_bytes': fsize,
+                'total_bytes': fsize,
+                'filename': filename,
+                'status': 'finished',
+            })
+            return True
+        else:
+            self.to_stderr('\n')
+            self.report_error('%s exited with code %d' % (args[0], retval))
+            return False
+
     
-        exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR
-    assert error_msg == (
-        'ConnectionError: '
-        'Connection aborted while doing GET request to URL: '
-        'http://www.google.com')
+            formats = [
+            {
+                'format_id': f['type'],
+                'filesize': int(f['filesize']),
+                'url': f['url']
+            } for f in info['rfiles']
+        ]
+        self._sort_formats(formats)
     
-        # The name of the plugin, eg. 'My auth'.
-    name = None
+            info_dict = self._extract_from_id(mvp_id, webpage)
+        info_dict['display_id'] = display_id
     
-    # FIXME:
-# We test against a local httpbin instance which uses a self-signed cert.
-# Requests without --verify=<CA_BUNDLE> will fail with a verification error.
-# See: https://github.com/kevin1024/pytest-httpbin#https-support
-CA_BUNDLE = pytest_httpbin.certs.where()
+        def _real_extract(self, url):
+        sub_domain, path, page_title = re.match(self._VALID_URL, url).groups()
+        if sub_domain not in ('money', 'edition'):
+            sub_domain = 'edition'
+        config = self._CONFIG[sub_domain]
+        return self._extract_cvp_info(
+            config['data_src'] % path, page_title, {
+                'default': {
+                    'media_src': config['media_src'],
+                }
+            })
     
+    
+def _is_tar_extract(cmd):
+    if '--extract' in cmd:
+        return True
+    
+    containers = (('thefuck/python3-tcsh',
+               u'''FROM python:3
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'),
+              ('thefuck/python2-tcsh',
+               u'''FROM python:2
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'))
+    
+    
+def test_idna_without_version_attribute(mocker):
+    '''Older versions of IDNA don't provide a __version__ attribute, verify
+    that if we have such a package, we don't blow up.
     '''
-__version__ = '1.0.0-dev'
-__author__ = 'Jakub Roztocil'
-__licence__ = 'BSD'
-    
-        # Stem block: 35 x 35 x 192
-    x = conv2d_bn(img_input, 32, 3, strides=2, padding='valid')
-    x = conv2d_bn(x, 32, 3, padding='valid')
-    x = conv2d_bn(x, 64, 3)
-    x = MaxPooling2D(3, strides=2)(x)
-    x = conv2d_bn(x, 80, 1, padding='valid')
-    x = conv2d_bn(x, 192, 3, padding='valid')
-    x = MaxPooling2D(3, strides=2)(x)
-    
-        # Returns
-        A Keras model instance.
-    
-            def antirectifier_output_shape(input_shape):
-            shape = list(input_shape)
-            assert len(shape) == 2  # only valid for 2D tensors
-            shape[-1] *= 2
-            return tuple(shape)
-    
-        out = model.predict(x)
-    _, fname = tempfile.mkstemp('.h5')
-    with h5py.File(fname, mode='r+') as h5file:
-        save_model(model, h5file)
-        loaded_model = load_model(h5file)
-        out2 = loaded_model.predict(x)
-    assert_allclose(out, out2, atol=1e-05)
-    
-        for i, yi in enumerate(grid_y):
-        for j, xi in enumerate(grid_x):
-            z_sample = np.array([[xi, yi]])
-            x_decoded = decoder.predict(z_sample)
-            digit = x_decoded[0].reshape(digit_size, digit_size)
-            figure[i * digit_size: (i + 1) * digit_size,
-                   j * digit_size: (j + 1) * digit_size] = digit
-    
-        # Arguments:
-        models (tuple): encoder and decoder models
-        data (tuple): test data and label
-        batch_size (int): prediction batch size
-        model_name (string): which model is using this function
-    '''
-    
-            x = np.random.random((1,) + shape)
-    
-    ## All tokens go to the parser (unless skip() is called in that rule)
-# on a particular 'channel'.  The parser tunes to a particular channel
-# so that whitespace etc... can go to the parser on a 'hidden' channel.
-DEFAULT_CHANNEL = 0
+    mocker.patch('requests.help.idna', new=None)
+    assert info()['idna'] == {'version': ''}
     
     
-class RecognitionException(Exception):
-    '''@brief The root of the ANTLR exception hierarchy.
+class ConnectTimeout(ConnectionError, Timeout):
+    '''The request timed out while trying to connect to the remote server.
     
-        unexpectedType = property(getUnexpectedType)
+    import chardet
     
-        def test_wildcard(self):
-        self.assertFalse(self.addr.is_wildcard())
-        self.assertTrue(self.addr1.is_wildcard())
-        self.assertTrue(self.addr2.is_wildcard())
+        def set(self, name, value, **kwargs):
+        '''Dict-like set() that also supports optional domain and path args in
+        order to resolve naming collisions from using one cookie jar over
+        multiple domains.
+        '''
+        # support client code that unsets cookies by assignment of a None value:
+        if value is None:
+            remove_cookie_by_name(self, name, domain=kwargs.get('domain'), path=kwargs.get('path'))
+            return
     
-        def test_sequential_undo(self):
-        self.command_stack = list(reversed(self.command_stack))
-        self.command_stack[0].undo()
-        output_after_first_undo = os.listdir(self.test_dir)
-        self.assertEqual(output_after_first_undo[0], 'bar.txt')
-        self.command_stack[1].undo()
-        output_after_second_undo = os.listdir(self.test_dir)
-        self.assertEqual(output_after_second_undo[0], 'foo.txt')
+            #: Encoding to decode with when accessing r.text.
+        self.encoding = None
     
-        def test_c_observers_shall_be_detachable(cls):
-        cls.s.detach(cls.dec_obs)
-        # hex viewer shall be remaining if dec viewer is detached first
-        cls.assertEqual(isinstance(cls.s._observers[0], HexViewer), True)
-        cls.assertEqual(len(cls.s._observers), 1)
-        cls.s.detach(cls.hex_obs)
-        cls.assertEqual(len(cls.s._observers), 0)
+        def __delitem__(self, key):
+        del self._store[key.lower()]
     
-        def subscribe(self, msg):
-        self.provider.subscribe(msg, self)
+        tried_encodings = []
     
-    '''
-Port of the Java example of 'Constructor Injection' in
-'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
-(ISBN-10: 0131495054, ISBN-13: 978-0131495050)
+        return lasso_results, lars_lasso_results
     
-        def setProblem(self, value):
-        self._bProblem = value
-    
-            for i in range(3):
-            num_obj.increment()
-            print(num_obj)
-        num_obj.value += 'x'  # will fail
-        print(num_obj)
-    except Exception as e:
-        a_transaction.rollback()
-        print('-- rolled back')
-    print(num_obj)
-    
-        # set up the templating system
-    config['pylons.app_globals'].mako_lookup = TemplateLookup(
-        directories=paths['templates'],
-        error_handler=handle_mako_error,
-        module_directory=module_directory,
-        input_encoding='utf-8',
-        default_filters=['conditional_websafe'],
-        filesystem_checks=getattr(g, 'reload_templates', False),
-        imports=[
-            'from r2.lib.filters import websafe, unsafe, conditional_websafe',
-            'from pylons import request',
-            'from pylons import tmpl_context as c',
-            'from pylons import app_globals as g',
-            'from pylons.i18n import _, ungettext',
-        ],
-        modulename_callable=mako_module_path,
-    )
-    
-            hooks.register_hooks()
-        self._hooks_registered = True
-    
-    # class specific overrides
-api('link',          LinkJsonTemplate)
-api('promotedlink',  PromotedLinkJsonTemplate)
-api('message',       MessageJsonTemplate)
-api('subreddit',     SubredditJsonTemplate)
-api('labeledmulti',  LabeledMultiJsonTemplate)
-api('reddit',        RedditJsonTemplate)
-api('panestack',     PanestackJsonTemplate)
-api('htmlpanestack', NullJsonTemplate)
-api('listing',       ListingJsonTemplate)
-api('searchlisting', SearchListingJsonTemplate)
-api('userlisting',   UserListingJsonTemplate)
-api('usertableitem', UserTableItemJsonTemplate)
-api('account',       AccountJsonTemplate)
-    
-            # See if the target is already an existing friend.
-        # If not, create the friend relationship.
-        friend_rel = Account.get_friend(c.user, friend)
-        rel_exists = bool(friend_rel)
-        if not friend_rel:
-            friend_rel = c.user.add_friend(friend)
-            response.status = 201
-    
-            Responds with a 120x50 `image/png` which should be displayed
-        to the user.
+            # Create flat baselines to compare the variation over batch size
+        all_times['pca'].extend([results_dict['pca']['time']] *
+                                len(batch_sizes))
+        all_errors['pca'].extend([results_dict['pca']['error']] *
+                                 len(batch_sizes))
+        all_times['rpca'].extend([results_dict['rpca']['time']] *
+                                 len(batch_sizes))
+        all_errors['rpca'].extend([results_dict['rpca']['error']] *
+                                  len(batch_sizes))
+        for batch_size in batch_sizes:
+            ipca = IncrementalPCA(n_components=n_components,
+                                  batch_size=batch_size)
+            results_dict = {k: benchmark(est, data) for k, est in [('ipca',
+                                                                   ipca)]}
+            all_times['ipca'].append(results_dict['ipca']['time'])
+            all_errors['ipca'].append(results_dict['ipca']['error'])
     
     
-class GoogleTagManagerController(MinimalController):
-    def pre(self):
-        if request.host != g.media_domain:
-            # don't serve up untrusted content except on our
-            # specifically untrusted domain
-            self.abort404()
+def barplot_neighbors(Nrange=2 ** np.arange(1, 11),
+                      Drange=2 ** np.arange(7),
+                      krange=2 ** np.arange(10),
+                      N=1000,
+                      D=64,
+                      k=5,
+                      leaf_size=30,
+                      dataset='digits'):
+    algorithms = ('kd_tree', 'brute', 'ball_tree')
+    fiducial_values = {'N': N,
+                       'D': D,
+                       'k': k}
+    
+        op.add_option('--algorithm',
+                  dest='selected_algorithm',
+                  default=default_algorithms,
+                  type=str,
+                  help='Comma-separated list of transformer to benchmark. '
+                       'Default: %default. \nAvailable: %default')
+    
+        n = 10
+    step = 10000
+    n_samples = 10000
+    dim = 10
+    n_classes = 10
+    for i in range(n):
+        print('============================================')
+        print('Entering iteration %s of %s' % (i, n))
+        print('============================================')
+        n_samples += step
+        X = np.random.randn(n_samples, dim)
+        Y = np.random.randint(0, n_classes, (n_samples,))
+        bench_scikit_tree_classifier(X, Y)
+        Y = np.random.randn(n_samples)
+        bench_scikit_tree_regressor(X, Y)
+    
+    pages = {
+    u'ar': u'http://ar.wikipedia.org/wiki/%D9%88%D9%8A%D9%83%D9%8A%D8%A8%D9%8A%D8%AF%D9%8A%D8%A7',
+    u'de': u'http://de.wikipedia.org/wiki/Wikipedia',
+    u'en': u'https://en.wikipedia.org/wiki/Wikipedia',
+    u'es': u'http://es.wikipedia.org/wiki/Wikipedia',
+    u'fr': u'http://fr.wikipedia.org/wiki/Wikip%C3%A9dia',
+    u'it': u'http://it.wikipedia.org/wiki/Wikipedia',
+    u'ja': u'http://ja.wikipedia.org/wiki/Wikipedia',
+    u'nl': u'http://nl.wikipedia.org/wiki/Wikipedia',
+    u'pl': u'http://pl.wikipedia.org/wiki/Wikipedia',
+    u'pt': u'http://pt.wikipedia.org/wiki/Wikip%C3%A9dia',
+    u'ru': u'http://ru.wikipedia.org/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D0%BF%D0%B5%D0%B4%D0%B8%D1%8F',
+#    u'zh': u'http://zh.wikipedia.org/wiki/Wikipedia',
+}
+    
+    Only adjusted measures can hence safely be used as a consensus index
+to evaluate the average stability of clustering algorithms for a given
+value of k on various overlapping sub-samples of the dataset.
+    
+    # Apply monkey patches to fix issues in external libraries
+from . import _monkeypatches
+del _monkeypatches
+    
+            if opts.pidfile:
+            with open(opts.pidfile, 'w') as f:
+                f.write(str(os.getpid()) + os.linesep)
+    
+                result.printErrors()
+            result.printSummary(start, stop)
+            self.exitcode = int(not result.wasSuccessful())
+    
+    from scrapy.http import Request
+from scrapy.utils.spider import iterate_spider_output
+from scrapy.utils.python import get_spec
+    
+            assert len(self.args) in [1, 2, 3]
+        self.obj_name = self.args[0] or None
+        self.obj_type = self.objects[self.obj_name]
+    
+    from homeassistant.setup import setup_component
+import homeassistant.components.splunk as splunk
+from homeassistant.const import STATE_ON, STATE_OFF, EVENT_STATE_CHANGED
+from homeassistant.helpers import state as state_helper
+import homeassistant.util.dt as dt_util
+    
+    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_ON_STATE, default=DEFAULT_ON_STATE): vol.In(STATES),
+})
+    
+        config_dir = os.path.join(os.getcwd(), args.config)
+    
+        # pylint: disable=no-self-use
+    def test_from_event(self):
+        '''Test converting event to db event.'''
+        event = ha.Event('test_event', {
+            'some_data': 15
+        })
+        assert event == Events.from_event(event).to_native()
