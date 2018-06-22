12dc150cbb43e43d776b41ba832b1a8e77e219f9
SUNsung <weatherius@gmail.com>
2018-06-22 11:31:28 +0300

    Rewrite [25AVTJqV6ODQ22506cuGlsNE.25Xlwv7FxBEPQ25Znx.py] File in: 2018-06-22 11:31:27

--- pyton/25AVTJqV6ODQ22506cuGlsNE.25Xlwv7FxBEPQ25Znx.py
+++ pyton/25AVTJqV6ODQ22506cuGlsNE.25Xlwv7FxBEPQ25Znx.py
@@ -1,114 +1,97 @@
 
         
-            def get(self, key):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                return item.value
-        raise KeyError('Key not found')
+            infile, outfile = args
     
-        def append_to_front(self, node):
-        ...
+        if not releases:
+        break
     
-    
-class sockaddr(ctypes.Structure):
-    _fields_ = [('sa_family', ctypes.c_short),
-                ('__pad1', ctypes.c_ushort),
-                ('ipv4_addr', ctypes.c_byte * 4),
-                ('ipv6_addr', ctypes.c_byte * 16),
-                ('__pad2', ctypes.c_ulong)]
-    
-    A Lexer emits Token objects which are usually buffered by a TokenStream. A
-Parser can build a Tree, if the output=AST option has been set in the grammar.
-    
-    # begin[licence]
-#
-# [The 'BSD licence']
-# Copyright (c) 2005-2008 Terence Parr
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# end[licensc]
+        def test_cache(self):
+        ydl = FakeYDL({
+            'cachedir': self.test_dir,
+        })
+        c = Cache(ydl)
+        obj = {'x': 1, 'y': ['ä', '\\a', True]}
+        self.assertEqual(c.load('test_cache', 'k.'), None)
+        c.store('test_cache', 'k.', obj)
+        self.assertEqual(c.load('test_cache', 'k2'), None)
+        self.assertFalse(_is_empty(self.test_dir))
+        self.assertEqual(c.load('test_cache', 'k.'), obj)
+        self.assertEqual(c.load('test_cache', 'y'), None)
+        self.assertEqual(c.load('test_cache2', 'k.'), None)
+        c.remove()
+        self.assertFalse(os.path.exists(self.test_dir))
+        self.assertEqual(c.load('test_cache', 'k.'), None)
     
     
-############################################################################
-#
-# token implementations
-#
-# Token
-# +- CommonToken
-# \- ClassicToken
-#
-############################################################################
-    
-    A tree.RewriteCardinalityException is raised, when the parsers hits a
-cardinality mismatch during AST construction. Although this is basically a
-bug in your grammar, it can only be detected at runtime.
-    
-        >>> Point = namedtuple('Point', 'x y')
-    >>> Point.__doc__                   # docstring for the new class
-    'Point(x, y)'
-    >>> p = Point(11, y=22)             # instantiate with positional args or keywords
-    >>> p[0] + p[1]                     # indexable like a plain tuple
-    33
-    >>> x, y = p                        # unpack like a regular tuple
-    >>> x, y
-    (11, 22)
-    >>> p.x + p.y                       # fields also accessable by name
-    33
-    >>> d = p._asdict()                 # convert to a dictionary
-    >>> d['x']
-    11
-    >>> Point(**d)                      # convert from a dictionary
-    Point(x=11, y=22)
-    >>> p._replace(x=100)               # _replace() is like str.replace() but targets named fields
-    Point(x=100, y=22)
-    
-        def test_result_with_success(self):
-        # TODO(brian@sweetapp.com): This test is timing dependant.
-        def notification():
-            # Wait until the main thread is waiting for the result.
-            time.sleep(1)
-            f1.set_result(42)
+def get_info_extractor(ie_name):
+    '''Returns the info extractor class with the given ie_name'''
+    return globals()[ie_name + 'IE']
+
     
     
-def FormatDebugInfoResponse_ExtraConfFoundButNotLoaded_test():
+@click.command()
+@click.argument('catalog_file', type=click.Path())
+def cli(catalog_file):
+    # Read the old ones back.  Once we are in, we will never go.
+    with open(catalog_file) as f:
+        rv = json.load(f)['supported_locales']
+    
+    
+def FormatDebugInfoResponse_ExtraConfFoundAndLoaded_test():
   response = deepcopy( GENERIC_RESPONSE )
   response[ 'extra_conf' ].update( {
-    'is_loaded': False,
+    'is_loaded': True,
     'path': '/path/to/extra/conf'
   } )
   assert_that(
     FormatDebugInfoResponse( response ),
     contains_string(
-      'Extra configuration file found but not loaded\n'
+      'Extra configuration file found and loaded\n'
       'Extra configuration path: /path/to/extra/conf\n'
     )
   )
     
-      subprocess.check_call( [ sys.executable, '-m', 'nose' ] + nosetests_args )
+    
+def _ConvertVimDatasToCompletionDatas( response_data ):
+  return [ ConvertVimDataToCompletionData( x )
+           for x in response_data ]
+
+    
+        raise RuntimeError( 'Path in 'g:ycm_server_python_interpreter' option '
+                        'does not point to a valid Python 2.7 or 3.4+.' )
     
     
-  def Response( self ):
-    return self._response
+class FakeFuture( object ):
+  '''A fake version of a future response object, just about suitable for
+  mocking a server response as generated by PostDataToHandlerAsync.
+  Not usually used directly. See MockAsyncServerResponse* methods'''
+  def __init__( self, done, response = None, exception = None ):
+    self._done = done
+    
+    
+  def Extra_Info_No_Doc_String_test( self ):
+    self._Check( 0, {
+      'insertion_text':  'INSERTION TEXT',
+      'menu_text':       'MENU TEXT',
+      'extra_menu_info': 'EXTRA MENU INFO',
+      'kind':            'K',
+      'extra_data': {
+      },
+    }, {
+      'word'     : 'INSERTION TEXT',
+      'abbr'     : 'MENU TEXT',
+      'menu'     : 'EXTRA MENU INFO',
+      'kind'     : 'k',
+      'dup'      : 1,
+      'empty'    : 1,
+      'user_data': '0',
+    } )
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    _logger = logging.getLogger( __name__ )
