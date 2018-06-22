fa35019f8903e2048a24aca6c4933a31b8635ddb
SUNsung <weatherius@gmail.com>
2018-06-22 14:15:22 +0300

    Rewrite [55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp] File in: 2018-06-22 14:15:22

--- cccccc/55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp
+++ cccccc/55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp
@@ -1,174 +1,285 @@
 
         
-        #define MB_TITLE L'Cmder Launcher'
-#define SHELL_MENU_REGISTRY_PATH_BACKGROUND L'Directory\\Background\\shell\\Cmder'
-#define SHELL_MENU_REGISTRY_PATH_LISTITEM L'Directory\\shell\\Cmder'
+        #include 'atom/browser/net/asar/url_request_asar_job.h'
+#include 'atom/browser/net/js_asker.h'
     
-      /// Sets the non-blocking mode of the native acceptor implementation.
-  /**
-   * This function is used to modify the non-blocking mode of the underlying
-   * native acceptor. It has no effect on the behaviour of the acceptor object's
-   * synchronous operations.
-   *
-   * @param mode If @c true, the underlying acceptor is put into non-blocking
-   * mode and direct system calls may fail with boost::asio::error::would_block
-   * (or the equivalent system error).
-   *
-   * @param ec Set to indicate what error occurred, if any. If the @c mode is
-   * @c false, but the current value of @c non_blocking() is @c true, this
-   * function fails with boost::asio::error::invalid_argument, as the
-   * combination does not make sense.
-   */
-  boost::system::error_code native_non_blocking(
-      bool mode, boost::system::error_code& ec)
-  {
-    return this->get_service().native_non_blocking(
-        this->get_implementation(), mode, ec);
-  }
-    
-    
-    {
-    {} // namespace asio
-} // namespace boost
-    
-    #endif // BOOST_ASIO_BUFFERED_WRITE_STREAM_FWD_HPP
+    #endif  // ATOM_BROWSER_WINDOW_LIST_OBSERVER_H_
 
     
-    template<class T, class TimeSystem>
-class base_time;
     
-    
-    {private:
-  int descriptor_;
-  ConstBufferSequence buffers_;
+    {  DISALLOW_COPY_AND_ASSIGN(RemoteCallbackFreer);
 };
     
-    #if !defined(BOOST_ASIO_HAS_THREADS)
-# include <boost/asio/detail/null_event.hpp>
-#elif defined(BOOST_ASIO_WINDOWS)
-# include <boost/asio/detail/win_event.hpp>
-#elif defined(BOOST_ASIO_HAS_PTHREADS)
-# include <boost/asio/detail/posix_event.hpp>
-#elif defined(BOOST_ASIO_HAS_STD_MUTEX_AND_CONDVAR)
-# include <boost/asio/detail/std_event.hpp>
+    #ifndef ATOM_COMMON_DRAGGABLE_REGION_H_
+#define ATOM_COMMON_DRAGGABLE_REGION_H_
+    
+    namespace base {
+class FilePath;
+}
+    
+    class DesktopMediaListObserver;
+    
+     private:
+  // Non-owning pointer.
+  content::BrowserPpapiHost* host_;
+    
+      // ResourceHost override.
+  int32_t OnResourceMessageReceived(
+      const IPC::Message& msg,
+      ppapi::host::HostMessageContext* context) override;
+    
+    
+    {  DISALLOW_COPY_AND_ASSIGN(PepperIsolatedFileSystemMessageFilter);
+};
+    
+    #include 'chrome/browser/speech/tts_platform.h'
+    
+    template<> inline
+dnnError_t dnnConvolutionCreateForwardBias<double>(
+    dnnPrimitive_t* pConvolution,
+    dnnPrimitiveAttributes_t attributes,
+    dnnAlgorithm_t algorithm,
+    size_t dimension,
+    const size_t srcSize[],
+    const size_t dstSize[],
+    const size_t filterSize[],
+    const size_t convolutionStrides[],
+    const int inputOffset[],
+    const dnnBorder_t border_type)
+{
+    return dnnConvolutionCreateForwardBias_F64(
+        pConvolution,
+        attributes,
+        algorithm,
+        dimension,
+        srcSize,
+        dstSize,
+        filterSize,
+        convolutionStrides,
+        inputOffset,
+        border_type);
+}
+    
+        void PackedValue::Unpack() const
+    {
+        if (m_packedDataLayout && (m_packedDataLayout->GetNumTimeSteps() != 1) && (m_packedDataLayout->GetNumSequences() != 1) && Internal::IsAutomaticUnpackingOfPackedValuesDisabled())
+            LogicError('PackedValue::Unpack: Automatic unpacking of PackedValue objects is disabled');
+    }
+    
+    void GPUDataTransferer::CopyGPUToCPUAsync(void* gpuBuffer, size_t totalSize, void* cpuBuffer)
+{
+    m_inner->CopyGPUToCPUAsync(gpuBuffer, 1, totalSize, cpuBuffer);
+    m_inner->RecordGPUToCPUCopy();
+}
+    
+    // ---------------------------------------------------------------------------
+// TextLocation -- holds a pointer into a source file
+// ---------------------------------------------------------------------------
+    
+      /// Gets the non-blocking mode of the native acceptor implementation.
+  /**
+   * This function is used to retrieve the non-blocking mode of the underlying
+   * native acceptor. This mode has no effect on the behaviour of the acceptor
+   * object's synchronous operations.
+   *
+   * @returns @c true if the underlying acceptor is in non-blocking mode and
+   * direct system calls may fail with boost::asio::error::would_block (or the
+   * equivalent system error).
+   *
+   * @note The current non-blocking mode is cached by the acceptor object.
+   * Consequently, the return value may be incorrect if the non-blocking mode
+   * was set directly on the native acceptor.
+   */
+  bool native_non_blocking() const
+  {
+    return this->get_service().native_non_blocking(this->get_implementation());
+  }
+    
+    #include <boost/asio/detail/push_options.hpp>
+    
+    #define BOOST_ASIO_RESOLVE_HANDLER_CHECK( \
+    handler_type, handler, iter_type) \
+  \
+  typedef BOOST_ASIO_HANDLER_TYPE(handler_type, \
+      void(boost::system::error_code, iter_type)) \
+    asio_true_handler_type; \
+  \
+  BOOST_ASIO_HANDLER_TYPE_REQUIREMENTS_ASSERT( \
+      sizeof(boost::asio::detail::two_arg_handler_test( \
+          boost::asio::detail::clvref< \
+            asio_true_handler_type>(), \
+          static_cast<const boost::system::error_code*>(0), \
+          static_cast<const iter_type*>(0))) == 1, \
+      'ResolveHandler type requirements not met') \
+  \
+  typedef boost::asio::detail::handler_type_requirements< \
+      sizeof( \
+        boost::asio::detail::argbyv( \
+          boost::asio::detail::clvref< \
+            asio_true_handler_type>())) + \
+      sizeof( \
+        boost::asio::detail::lvref< \
+          asio_true_handler_type>()( \
+            boost::asio::detail::lvref<const boost::system::error_code>(), \
+            boost::asio::detail::lvref<const iter_type>()), \
+        char(0))> BOOST_ASIO_UNUSED_TYPEDEF
+    
+      // Dispatch the waiting events.
+  for (int i = 0; i < num_events; ++i)
+  {
+    int descriptor = events[i].fd;
+    if (descriptor == interrupter_.read_descriptor())
+    {
+      interrupter_.reset();
+    }
+    else
+    {
+      bool more_reads = false;
+      bool more_writes = false;
+      bool more_except = false;
+    }
+    }
+    
+    
+    {  if (handle) {
+    ::CloseHandle(handle);
+  }
+}
 #else
-# error Only Windows, POSIX and std::condition_variable are supported!
+TEST_F(ProcessTests, test_constructorPosix) {
+  auto p = PlatformProcess(getpid());
+  EXPECT_TRUE(p.isValid());
+  EXPECT_EQ(p.nativeHandle(), getpid());
+}
 #endif
     
     
-    {  edit.SetComparatorName('foo');
-  edit.SetLogNumber(kBig + 100);
-  edit.SetNextFile(kBig + 200);
-  edit.SetLastSequence(kBig + 1000);
-  TestEncodeDecode(edit);
+    {
+    {  return Status(0, 'OK');
 }
-    
-    #ifndef STORAGE_LEVELDB_DB_WRITE_BATCH_INTERNAL_H_
-#define STORAGE_LEVELDB_DB_WRITE_BATCH_INTERNAL_H_
-    
-    size_t BlockBuilder::CurrentSizeEstimate() const {
-  return (buffer_.size() +                        // Raw data buffer
-          restarts_.size() * sizeof(uint32_t) +   // Restart array
-          sizeof(uint32_t));                      // Restart array length
 }
+
     
-    class FilterPolicy;
+    struct OpenBSMEventContext : public EventContext {
+  // The event_id of the OpenBSM log
+  int event_id;
+  // The tokens for the event to pass to the subscriber
+  std::vector<tokenstr_t> tokens;
+  // A smart pointer to the memory returned by OpenBSM
+  std::shared_ptr<unsigned char> buffer;
+};
     
-      ~BloomTest() {
-    delete policy_;
+     public:
+  void configure() override;
+    
+    namespace detail {
+    }
+    
+    // Some queue implementations (for example, LifoSemMPMCQueue or
+// PriorityLifoSemMPMCQueue) support both blocking (BLOCK) and
+// non-blocking (THROW) behaviors.
+enum class QueueBehaviorIfFull { THROW, BLOCK };
+    
+      // Free the control block if we're the last reader at max value.
+  prev = block->valueAndReaderCount.fetch_sub(kReader,
+                                              std::memory_order_acq_rel);
+  if (prev == (kReader | uint64_t(size_))) {
+    freeControlBlock(block);
   }
     
-    namespace leveldb {
+    int main(int argc, char* argv[]) {
+  // Using log macros before calling folly::initLogging() will use the default
+  // log settings defined by folly::initializeLoggerDB().  The default behavior
+  // is to log WARNING+ messages to stderr.
+  XLOG(INFO) << 'log messages less than WARNING will be ignored';
+  XLOG(ERR) << 'error messages before initLogging() will be logged to stderr';
     }
     
     
-    {  // Verify that the size of the key space not touched by the reads
-  // is pretty much unchanged.
-  const int64_t final_other_size = Size(Key(n), Key(kCount));
-  ASSERT_LE(final_other_size, initial_other_size + 1048576);
-  ASSERT_GE(final_other_size, initial_other_size/5 - 1048576);
+    {  return res;
 }
     
-    void AppendInternalKey(std::string* result, const ParsedInternalKey& key) {
-  result->append(key.user_key.data(), key.user_key.size());
-  PutFixed64(result, PackSequenceAndType(key.sequence, key.type));
+    
+    {  // Errors attempting to format the message should not throw
+  FB_LOGC(footest1234, ERR, 'width overflow: %999999999999999999999d', 5);
+  ASSERT_EQ(1, messages.size());
+  EXPECT_EQ(
+      'error formatting printf-style log message: '
+      'width overflow: %999999999999999999999d',
+      messages[0].first.getMessage());
+  messages.clear();
+}
+
+    
+    void YGNode::setDirty(bool isDirty) {
+  if (isDirty == isDirty_) {
+    return;
+  }
+  isDirty_ = isDirty;
+  if (isDirty && dirtied_) {
+    dirtied_(this);
+  }
 }
     
-    #ifndef STORAGE_LEVELDB_DB_LOG_WRITER_H_
-#define STORAGE_LEVELDB_DB_LOG_WRITER_H_
+     public:
+  YGNode();
+  ~YGNode();
+  explicit YGNode(const YGConfigRef newConfig);
+  YGNode(const YGNode& node);
+  YGNode& operator=(const YGNode& node);
+  YGNode(
+      void* context,
+      YGPrintFunc print,
+      bool hasNewLayout,
+      YGNodeType nodeType,
+      YGMeasureFunc measure,
+      YGBaselineFunc baseline,
+      YGDirtiedFunc dirtied,
+      YGStyle style,
+      const YGLayout& layout,
+      uint32_t lineIndex,
+      YGNodeRef owner,
+      const YGVector& children,
+      YGNodeRef nextChild,
+      YGConfigRef config,
+      bool isDirty,
+      std::array<YGValue, 2> resolvedDimensions);
     
-    namespace osquery {
-/**
- * @brief Create the external SQLite implementation wrapper.
- *
- * Anything built with only libosquery and not the 'additional' library will
- * not include a native SQL implementation. This applies to extensions and
- * separate applications built with the osquery SDK.
- *
- * The ExternalSQLPlugin is a wrapper around the SQLite API, which forwards
- * calls to an osquery extension manager (core).
- */
-REGISTER_INTERNAL(ExternalSQLPlugin, 'sql', 'sql');
+        Value(int unit, double value)
+    : unit(unit)
+    , value(value)
+    {
+    }
+    
+    class Config {
     }
     
     
-    {  // Update the value of the flag, and access through the osquery wrapper.
-  FLAGS_test_string_flag = 'NEW TEST STRING';
-  EXPECT_EQ(Flag::getValue('test_string_flag'), 'NEW TEST STRING');
-}
-    
-    #include <osquery/status.h>
-    
-    Status WmiResultItem::GetLong(const std::string& name, long& ret) const {
-  std::wstring property_name = stringToWstring(name);
-  VARIANT value;
-  HRESULT hr = result_->Get(property_name.c_str(), 0, &value, nullptr, nullptr);
-  if (hr != S_OK) {
-    return Status(-1, 'Error retrieving data from WMI query.');
-  }
-  if (value.vt != VT_I4) {
-    VariantClear(&value);
-    return Status(-1, 'Invalid data type returned.');
-  }
-  ret = value.lVal;
-  VariantClear(&value);
-  return Status(0);
-}
-    
-    TEST_F(MockEnvTest, FakeSleeping) {
-  int64_t now = 0;
-  auto s = env_->GetCurrentTime(&now);
-  ASSERT_OK(s);
-  env_->FakeSleepForMicroseconds(3 * 1000 * 1000);
-  int64_t after_sleep = 0;
-  s = env_->GetCurrentTime(&after_sleep);
-  ASSERT_OK(s);
-  auto delta = after_sleep - now;
-  // this will be true unless test runs for 2 seconds
-  ASSERT_TRUE(delta == 3 || delta == 4);
-}
-    
-    #define       XXH32_SIZEOFSTATE 48
-typedef struct { long long ll[(XXH32_SIZEOFSTATE+(sizeof(long long)-1))/sizeof(long long)]; } XXH32_stateSpace_t;
-/*
-These functions allow user application to make its own allocation for state.
-    
-     private:
-  // A version of PartialMerge that actually performs 'partial merging'.
-  // Use this to simulate the exact behaviour of the StringAppendOperator.
-  bool _AssocPartialMergeMulti(const Slice& key,
-                               const std::deque<Slice>& operand_list,
-                               std::string* new_value, Logger* logger) const;
-    
-      Slice GetUserKey() const {
-    if (IsUserKey()) {
-      return Slice(key_, key_size_);
-    } else {
-      assert(key_size_ >= 8);
-      return Slice(key_, key_size_ - 8);
+    {    void toJS(nbind::cbOutput expose) const
+    {
+        expose(left, right, top, bottom, width, height);
     }
-  }
+};
+
     
-    void SyncPoint::ClearAllCallBacks() {
-  impl_->ClearAllCallBacks();
-}
+    #include './Layout.hh'
+#include './Size.hh'
+#include './Value.hh'
+#include './Config.hh'
+    
+      /**
+   * This runs the closure in a scope with fbjni's classloader. This should be
+   * the same classloader as the rest of the application and thus anything
+   * running in the closure will have access to the same classes as in a normal
+   * java-create thread.
+   */
+  static void WithClassLoader(std::function<void()>&& runnable);
+    
+      ProgramLocation(const char* functionName, const char* fileName, int line) :
+      m_functionName(functionName),
+      m_fileName(fileName),
+      m_lineNumber(line)
+    {}
+    
+    
+    {}
