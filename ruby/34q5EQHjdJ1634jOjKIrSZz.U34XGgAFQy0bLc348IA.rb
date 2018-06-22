d8389a06be329486929cbf6fcc07ea04fcbc4de4
SUNsung <weatherius@gmail.com>
2018-06-22 23:23:26 +0300

    Rewrite [34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb] File in: 2018-06-22 23:23:26

--- ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
+++ ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
@@ -1,53 +1,73 @@
 
         
-        # No trailing slash
-Benchmark.ips do |x|
-  path = '/some/very/very/long/path/to/a/file/i/like/'
-  x.report('pre_pr:#{path}')    { pre_pr(path) }
-  x.report('pr:#{path}')        { pr(path) }
-  x.report('envygeeks:#{path}') { pr(path) }
-  x.compare!
+          def fetch_checksum_for(tag)
+    tag = find_matching_tag(tag)
+    return self[tag], tag if tag
+  end
+    
+    module Homebrew
+  def build_env_keys(env)
+    %w[
+      CC CXX LD OBJC OBJCXX
+      HOMEBREW_CC HOMEBREW_CXX
+      CFLAGS CXXFLAGS CPPFLAGS LDFLAGS SDKROOT MAKEFLAGS
+      CMAKE_PREFIX_PATH CMAKE_INCLUDE_PATH CMAKE_LIBRARY_PATH CMAKE_FRAMEWORK_PATH
+      MACOSX_DEPLOYMENT_TARGET PKG_CONFIG_PATH PKG_CONFIG_LIBDIR
+      HOMEBREW_DEBUG HOMEBREW_MAKE_JOBS HOMEBREW_VERBOSE
+      HOMEBREW_SVN HOMEBREW_GIT
+      HOMEBREW_SDKROOT HOMEBREW_BUILD_FROM_SOURCE
+      MAKE GIT CPP
+      ACLOCAL_PATH PATH CPATH].select { |key| env.key?(key) }
+  end
+    
+        current_revision_var = 'HOMEBREW_UPDATE_AFTER#{repo_var}'
+    @current_revision = ENV[current_revision_var].to_s
+    raise ReporterRevisionUnsetError, current_revision_var if @current_revision.empty?
+  end
+    
+    # This formula serves as the base class for several very similar
+# formulae for Amazon Web Services related tools.
+class AmazonWebServicesFormula < Formula
+  # Use this method to peform a standard install for Java-based tools,
+  # keeping the .jars out of HOMEBREW_PREFIX/lib
+  def install
+    rm Dir['bin/*.cmd'] # Remove Windows versions
+    libexec.install Dir['*']
+    bin.install_symlink Dir['#{libexec}/bin/*'] - ['#{libexec}/bin/service']
+  end
+  alias_method :standard_install, :install
+    
+      private
+    
+        groups << @user.authorized_groups.visible_to_user(current_user) if current_user
+    groups << @user.authorized_groups.public_to_user(current_user)
+    
+    class EmacsRequirement < Requirement
+  fatal true
+  satisfy do
+    odisabled('EmacsRequirement', ''depends_on \'emacs\''')
+  end
 end
-
     
-              @new_body.each do |line|
-            if !@livereload_added && line['<head']
-              line.gsub!(HEAD_TAG_REGEX) do |match|
-                %(#{match}#{template.result(binding)})
-              end
-    
-        def safe_load_file(filename)
-      case File.extname(filename)
-      when %r!\.toml!i
-        Jekyll::External.require_with_graceful_fail('tomlrb') unless defined?(Tomlrb)
-        Tomlrb.load_file(filename)
-      when %r!\.ya?ml!i
-        SafeYAML.load_file(filename) || {}
-      else
-        raise ArgumentError, 'No parser for '#{filename}' is available.
-          Use a .y(a)ml or .toml file instead.'
-      end
+        def assets_path
+      @assets_path ||= File.join gem_path, 'assets'
     end
     
-        # Transform the contents based on the content type.
-    #
-    # Returns the transformed contents.
-    def transform
-      _renderer.convert(content)
-    end
+    desc 'Start a dummy (test) Rails app server'
+task :dummy_rails do
+  require 'rack'
+  require 'term/ansicolor'
+  port = ENV['PORT'] || 9292
+  puts %Q(Starting on #{Term::ANSIColor.cyan 'http://localhost:#{port}'})
+  Rack::Server.start(
+    config: 'test/dummy_rails/config.ru',
+    Port: port)
+end
     
-        find_union(segments, Group).order_id_desc
-  end
-    
-      def failed_strategy
-    request.respond_to?(:get_header) ? request.get_header('omniauth.error.strategy') : request.env['omniauth.error.strategy']
-  end
-    
-      # POST /resource/sign_in
-  def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
-  end
+              def plugins
+            @plugins ||= find_plugins_gem_specs.map do |spec|
+              { :name => spec.name, :version => spec.version.to_s }
+            end.sort_by do |spec|
+              spec[:name]
+            end
+          end
