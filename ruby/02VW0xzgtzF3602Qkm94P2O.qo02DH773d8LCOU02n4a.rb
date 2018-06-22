51e50199a03f803847a565e8f8169c9ca5261a77
SUNsung <weatherius@gmail.com>
2018-06-22 12:44:22 +0300

    Rewrite [02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb] File in: 2018-06-22 12:44:21

--- ruby/02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb
+++ ruby/02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb
@@ -1,62 +1,126 @@
 
         
-          def resource
-    @resource ||=
-      if params[:project_id].present?
-        Project.find(params[:project_id])
-      elsif params[:namespace_id].present?
-        Group.find(params[:namespace_id])
-      end
-  end
-    
-          def save_params_and_render_consent_form(endpoint)
-        @o_auth_application = endpoint.o_auth_application
-        @response_type = endpoint.response_type
-        @redirect_uri = endpoint.redirect_uri
-        @scopes = endpoint.scopes
-        save_request_parameters
-        @app = UserApplicationPresenter.new @o_auth_application, @scopes
-        render :new
-      end
-    
-            def log_level(name, options = {})
-          if options[:prepend]
-            level = log_levels.values.min
-            level = level.nil? ? 0 : level - 1
-          else
-            level = log_levels.values.max
-            level = level.nil? ? 0 : level + 1
-          end
-          log_levels.update(name => level)
-          define_logger(name)
+        module ActiveRecord
+  module ConnectionAdapters
+    class ConnectionSpecification
+      class ResolverTest < ActiveRecord::TestCase
+        def resolve(spec, config = {})
+          Resolver.new(config).resolve(spec)
         end
     
-    desc 'Generate jekyll site'
-task :generate do
-  raise '### You haven't set anything up yet. First run `rake install` to set up an Octopress theme.' unless File.directory?(source_dir)
-  puts '## Generating Site with Jekyll'
-  system 'compass compile --css-dir #{source_dir}/stylesheets'
-  system 'jekyll build'
-end
+        # Count should come from sql count() of treasures rather than treasures_count attribute
+    assert_equal ship.treasures.size, 0
     
-    class SinatraStaticServer < Sinatra::Base
+          expected = { 'identifier' => '{id: 1}', 'type' => 'reject_subscription' }
+      assert_equal expected, @connection.last_transmission
+      assert_equal 1, @connection.transmissions.size
     
-    module Jekyll
-  class GistTag < Liquid::Tag
-    def initialize(tag_name, text, token)
-      super
-      @text           = text
-      @cache_disabled = false
-      @cache_folder   = File.expand_path '../.gist-cache', File.dirname(__FILE__)
-      FileUtils.mkdir_p @cache_folder
+          assert_equal [ connection ], @server.connections
+      assert connection.connected
+    end
+  end
+    
+        def send_async(method, *args)
+      send method, *args
     end
     
-            def each_unnecessary_space_match(node, &blk)
-          each_match_range(
-            contents_range(node),
-            MULTIPLE_SPACES_BETWEEN_ITEMS_REGEX,
-            &blk
-          )
+            def deprecated_relative_permalinks(site)
+          if site.config['relative_permalinks']
+            Jekyll::Deprecator.deprecation_message 'Your site still uses relative permalinks,' \
+                                                   ' which was removed in Jekyll v3.0.0.'
+            true
+          end
+        end
+    
+            def print_message(json_message)
+          msg = JSON.parse(json_message)
+          # Not sure what the 'url' command even does in LiveReload.  The spec is silent
+          # on its purpose.
+          Jekyll.logger.info 'LiveReload:', 'Browser URL: #{msg['url']}' if msg['command'] == 'url'
+        end
+    
+        # Public: Read configuration and return merged Hash
+    #
+    # file - the path to the YAML file to be read in
+    #
+    # Returns this configuration, overridden by the values in the file
+    def read_config_file(file)
+      next_config = safe_load_file(file)
+      check_config_is_hash!(next_config, file)
+      Jekyll.logger.info 'Configuration file:', file
+      next_config
+    rescue SystemCallError
+      if @default_config_file ||= nil
+        Jekyll.logger.warn 'Configuration file:', 'none'
+        {}
+      else
+        Jekyll.logger.error 'Fatal:', 'The configuration file '#{file}'
+          could not be found.'
+        raise LoadError, 'The Configuration file '#{file}' could not be found.'
+      end
+    end
+    
+        def defaults_deprecate_type(old, current)
+      Jekyll.logger.warn 'Defaults:', 'The '#{old}' type has become '#{current}'.'
+      Jekyll.logger.warn 'Defaults:', 'Please update your front-matter defaults to use \
+                        'type: #{current}'.'
+    end
+  end
+end
+
+    
+      # Finds the groups of the source user, optionally limited to those visible to
+  # the current user.
+  def execute(current_user = nil)
+    segments = all_groups(current_user)
+    
+        # Validates that {#boundary} is {#valid_ip_or_range? a valid IP address or
+    # IP address range}. Due to this not being tested before it was moved here
+    # from Mdm, the default workspace does not validate. We always validate boundaries
+    # and a workspace may have a blank default boundary.
+    #
+    # @return [void]
+    def boundary_must_be_ip_range
+      unless boundary.blank?
+        begin
+          boundaries = Shellwords.split(boundary)
+        rescue ArgumentError
+          boundaries = []
+        end
+    
+    lib_path = root.join('lib').to_path
+    
+              fd.write(res)
+        end
+      end
+      break
+    rescue ::Timeout::Error
+      $stderr.puts '#{prefix}#{site} timed out'
+    rescue ::Interrupt
+      raise $!
+    rescue ::Exception => e
+      $stderr.puts '#{prefix}#{site} #{e.class} #{e}'
+    end
+  end
+    
+              s[:pass] = ''
+          return
+        end
+    
+          if(pkt.payload =~ self.sigs[k])
+        matched = k
+        matches = $1
+      end
+    
+            def no_error_when_valid?
+          @file = StringIO.new('.')
+          @subject.send(@attachment_name).assign(@file)
+          @subject.valid?
+          expected_message = [
+            @attachment_name.to_s.titleize,
+            I18n.t(:blank, scope: [:errors, :messages])
+          ].join(' ')
+          @subject.errors.full_messages.exclude?(expected_message)
         end
       end
     end
@@ -64,27 +128,48 @@ end
 end
 
     
-            def_node_matcher :multiple_compare?, <<-PATTERN
-          (send (send _ {:< :> :<= :>=} $_) {:< :> :<= :>=} _)
-        PATTERN
+        module Statements
+      def add_attachment(table_name, *attachment_names)
+        raise ArgumentError, 'Please specify attachment name in your add_attachment call in your migration.' if attachment_names.empty?
     
-    module RuboCop
-  module Cop
-    module Style
-      # This cop checks against comparing a variable with multiple items, where
-      # `Array#include?` could be used instead to avoid code repetition.
-      #
-      # @example
-      #   # bad
-      #   a = 'a'
-      #   foo if a == 'a' || a == 'b' || a == 'c'
-      #
-      #   # good
-      #   a = 'a'
-      #   foo if ['a', 'b', 'c'].include?(a)
-      class MultipleComparison < Cop
-        MSG = 'Avoid comparing a variable with multiple items ' \
-          'in a conditional, use `Array#include?` instead.'.freeze
+                if options.has_key?(validator_kind)
+              validator_options = options.delete(validator_kind)
+              validator_options = {} if validator_options == true
+              conditional_options = options.slice(:if, :unless)
+              Array.wrap(validator_options).each do |local_options|
+                method_name = Paperclip::Validators.const_get(constant.to_s).helper_method_name
+                send(method_name, attributes, local_options.merge(conditional_options))
+              end
+            end
+          end
+        end
+      end
     
-    RSpec.describe RuboCop::Cop::Style::StringMethods, :config do
-  subject(:cop) { described_class.new(config) }
+        module HelperMethods
+      # Places ActiveModel validations on the content type of the file
+      # assigned. The possible options are:
+      # * +content_type+: Allowed content types.  Can be a single content type
+      #   or an array.  Each type can be a String or a Regexp. It should be
+      #   noted that Internet Explorer uploads files with content_types that you
+      #   may not expect. For example, JPEG images are given image/pjpeg and
+      #   PNGs are image/x-png, so keep that in mind when determining how you
+      #   match.  Allows all by default.
+      # * +not+: Forbidden content types.
+      # * +message+: The message to display when the uploaded file has an invalid
+      #   content type.
+      # * +if+: A lambda or name of an instance method. Validation will only
+      #   be run is this lambda or method returns true.
+      # * +unless+: Same as +if+ but validates if lambda or method returns false.
+      # NOTE: If you do not specify an [attachment]_content_type field on your
+      # model, content_type validation will work _ONLY upon assignment_ and
+      # re-validation after the instance has been reloaded will always succeed.
+      # You'll still need to have a virtual attribute (created by +attr_accessor+)
+      # name +[attachment]_content_type+ to be able to use this validator.
+      def validates_attachment_content_type(*attr_names)
+        options = _merge_attributes(attr_names)
+        validates_with AttachmentContentTypeValidator, options.dup
+        validate_before_processing AttachmentContentTypeValidator, options.dup
+      end
+    end
+  end
+end
