c241beb4dc9ad162d46f1ad1464c5b304d29d7a0
SUNsung <weatherius@gmail.com>
2018-06-22 08:22:57 +0300

    Rewrite [56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb] File in: 2018-06-22 08:22:57

--- ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
+++ ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
@@ -1,50 +1,34 @@
 
         
-              def self.example_code
-        [
-          'create_keychain(
-            name: 'KeychainName',
-            default_keychain: true,
-            unlock: true,
-            timeout: 3600,
-            lock_when_sleeps: true
-          )'
-        ]
+              raise Discourse::InvalidParameters.new(:to_address) unless args[:to_address].present?
+    
+      private
+    
+            expect_updated_sign_in_at(user)
+        expect(Redis.current.get('account:#{user.account_id}:regeneration')).to eq 'true'
+        expect(RegenerationWorker).to have_received(:perform_async)
       end
     
-        context 'Mercurial repository' do
-      before do
-        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_svn?).and_return(false)
-        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_git_svn?).and_return(false)
-        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_git?).and_return(false)
-        expect(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_hg?).and_return(true)
-      end
+    Bootstrap.load!
+
     
-          it 'increments the build number of the Xcode project' do
-        Fastlane::FastFile.new.parse('lane :test do
-          increment_build_number(xcodeproj: '.xcproject')
-        end').runner.execute(:test)
+        @save_to.each { |_, v| FileUtils.mkdir_p(v) }
     
-      def tumblr_oauth_token_secret
-    service.secret
-  end
+    desc 'Compile bootstrap-sass to tmp/ (or first arg)'
+task :compile, :css_path do |t, args|
+  require 'sass'
+  require 'term/ansicolor'
     
-        def register_rails_engine
-      require 'bootstrap-sass/engine'
-    end
-    
-    class NodeMincerTest < Minitest::Test
-  DUMMY_PATH = 'test/dummy_node_mincer'
-    
-      path = 'assets/stylesheets'
-  css_path = args.with_defaults(css_path: 'tmp')[:css_path]
-  puts Term::ANSIColor.bold 'Compiling SCSS in #{path}'
-  Dir.mkdir(css_path) unless File.directory?(css_path)
-  %w(_bootstrap bootstrap/_theme).each do |file|
-    save_path = '#{css_path}/#{file.sub(/(^|\/)?_+/, '\1').sub('/', '-')}.css'
-    puts Term::ANSIColor.cyan('  #{save_path}') + '...'
-    engine    = Sass::Engine.for_file('#{path}/#{file}.scss', syntax: :scss, load_paths: [path])
-    css       = engine.render
-    File.open(save_path, 'w') { |f| f.write css }
+      def full_plugin_name
+    @full_plugin_name ||= 'logstash-#{type}-#{name.downcase}'
   end
 end
+
+    
+              def plugins
+            @plugins ||= find_plugins_gem_specs.map do |spec|
+              { :name => spec.name, :version => spec.version.to_s }
+            end.sort_by do |spec|
+              spec[:name]
+            end
+          end
