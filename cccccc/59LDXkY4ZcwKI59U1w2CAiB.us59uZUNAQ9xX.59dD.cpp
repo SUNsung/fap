2f428b7472bbbb1ad48e620a67cd79eda7375057
SUNsung <weatherius@gmail.com>
2018-06-23 05:54:17 +0300

    Rewrite [59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp] File in: 2018-06-23 05:54:16

--- cccccc/59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp
+++ cccccc/59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp
@@ -1,277 +1,320 @@
 
         
-        
-    {
-    {      auto h = handle_.AccessTensor(context)->template flat<string>();
-      h(0) = cinfo_.container();
-      h(1) = cinfo_.name();
-      resource_ = resource;
-    }
-    if (context->expected_output_dtype(0) == DT_RESOURCE) {
-      OP_REQUIRES_OK(context, MakeResourceHandleToOutput(
-                                  context, 0, cinfo_.container(), cinfo_.name(),
-                                  MakeTypeIndex<T>()));
-    } else {
-      context->set_output_ref(0, &mu_, handle_.AccessTensor(context));
-    }
-  }
+        #include 'atom/browser/api/trackable_object.h'
+#include 'base/callback.h'
+#include 'chrome/browser/extensions/global_shortcut_listener.h'
+#include 'native_mate/handle.h'
+#include 'ui/base/accelerators/accelerator.h'
     
-    
-    {}  // namespace xla
-    
-    class SessionFactory {
- public:
-  virtual Session* NewSession(const SessionOptions& options) = 0;
-  virtual bool AcceptsOptions(const SessionOptions& options) = 0;
-    }
-    
-    #include 'third_party/eigen3/unsupported/Eigen/CXX11/Tensor'
-#include 'tensorflow/core/framework/tensor_types.h'
-#include 'tensorflow/core/framework/types.h'
-#include 'tensorflow/core/kernels/scatter_functor.h'
-    
-    /** scalar_sigmoid_fast_derivative_op
-  * \ingroup CXX11_NeuralNetworks_Module
-  * \brief Template functor to compute the fast derivative of a sigmoid
-  *
-  * Input should be the backpropagated gradient.
-  *
-  * \sa class CwiseUnaryOp, Cwise::sigmoid_fast_derivative()
-  */
-template <typename T>
-struct scalar_sigmoid_fast_derivative_op {
-  EIGEN_EMPTY_STRUCT_CTOR(scalar_sigmoid_fast_derivative_op)
-  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE T operator()(const T& y) const {
-    const T one = T(1);
-    return (one - y) * y;
-  }
-    }
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    
-    {  DynamicStitchKernel<T>
-      <<<config.block_count, config.thread_per_block, 0, gpu_device.stream()>>>(
-          slice_size, output_size, input_indices, input_ptrs, output);
-}
-    
-    #endif  // TENSORFLOW_FRAMEWORK_TENSOR_SLICE_H_
+    #endif  // ATOM_BROWSER_RELAUNCHER_H_
 
     
-    using caffe::Datum;
-using boost::scoped_ptr;
-using std::string;
-namespace db = caffe::db;
     
-    namespace caffe {
+    { private:
+  DISALLOW_COPY_AND_ASSIGN(WinFrameView);
+};
+    
+    
+    {}  // namespace atom
+    
+      // Returns whether current process is browser process, currently we detect it
+  // by checking whether current has used V8 Lock, but it might be a bad idea.
+  static inline bool IsBrowserProcess() { return v8::Locker::IsActive(); }
+    
+    namespace atom {
     }
     
-    namespace caffe {
-    }
+    #include 'ui/gfx/geometry/rect.h'
+    
+      /* This operation will write the separate BGR planes directly to the
+   * input layer of the network because it is wrapped by the cv::Mat
+   * objects in input_channels. */
+  cv::split(sample_normalized, *input_channels);
+    
+      image_file.read(reinterpret_cast<char*>(&magic), 4);
+  magic = swap_endian(magic);
+  CHECK_EQ(magic, 2051) << 'Incorrect image file magic.';
+  label_file.read(reinterpret_cast<char*>(&magic), 4);
+  magic = swap_endian(magic);
+  CHECK_EQ(magic, 2049) << 'Incorrect label file magic.';
+  image_file.read(reinterpret_cast<char*>(&num_items), 4);
+  num_items = swap_endian(num_items);
+  label_file.read(reinterpret_cast<char*>(&num_labels), 4);
+  num_labels = swap_endian(num_labels);
+  CHECK_EQ(num_items, num_labels);
+  image_file.read(reinterpret_cast<char*>(&rows), 4);
+  rows = swap_endian(rows);
+  image_file.read(reinterpret_cast<char*>(&cols), 4);
+  cols = swap_endian(cols);
+    
+      /**
+   * @brief Applies the transformation defined in the data layer's
+   * transform_param block to a vector of Datum.
+   *
+   * @param datum_vector
+   *    A vector of Datum containing the data to be transformed.
+   * @param transformed_blob
+   *    This is destination blob. It can be part of top blob's data if
+   *    set_cpu_data() is used. See memory_layer.cpp for an example.
+   */
+  void Transform(const vector<Datum> & datum_vector,
+                Blob<Dtype>* transformed_blob);
+    
+    
+    {  Blob<Dtype> col_buffer_;
+  Blob<Dtype> bias_multiplier_;
+};
+    
+      vector<cudnnTensorDescriptor_t> bottom_descs_, top_descs_;
+  cudnnTensorDescriptor_t    bias_desc_;
+  cudnnFilterDescriptor_t      filter_desc_;
+  vector<cudnnConvolutionDescriptor_t> conv_descs_;
+  int bottom_offset_, top_offset_, bias_offset_;
     
      protected:
-  // Helper functions that abstract away the column buffer and gemm arguments.
-  // The last argument in forward_cpu_gemm is so that we can skip the im2col if
-  // we just called weight_cpu_gemm with the same input.
-  void forward_cpu_gemm(const Dtype* input, const Dtype* weights,
-      Dtype* output, bool skip_im2col = false);
-  void forward_cpu_bias(Dtype* output, const Dtype* bias);
-  void backward_cpu_gemm(const Dtype* input, const Dtype* weights,
-      Dtype* output);
-  void weight_cpu_gemm(const Dtype* input, const Dtype* output, Dtype*
-      weights);
-  void backward_cpu_bias(Dtype* bias, const Dtype* input);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
     
-    /**
- * @brief Takes at least two Blob%s and concatenates them along either the num
- *        or channel dimension, outputting the result.
+    namespace caffe {
+    }
+    
+    #include 'caffe/layers/pooling_layer.hpp'
+    
+    #ifdef USE_CUDNN
+/**
+ * @brief CuDNN acceleration of ReLULayer.
  */
 template <typename Dtype>
-class ConcatLayer : public Layer<Dtype> {
+class CuDNNReLULayer : public ReLULayer<Dtype> {
  public:
-  explicit ConcatLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
+  explicit CuDNNReLULayer(const LayerParameter& param)
+      : ReLULayer<Dtype>(param), handles_setup_(false) {}
   virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
   virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNReLULayer();
     }
     
     
-    {  Blob<Dtype> diff_;  // cached for backward pass
-  Blob<Dtype> dist_sq_;  // cached for backward pass
-  Blob<Dtype> diff_sq_;  // tmp storage for gpu forward pass
-  Blob<Dtype> summer_vec_;  // tmp storage for gpu forward pass
-};
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    
-    {}  // namespace caffe
-    
-    
-    {  WorkloadStats (const WorkloadStats&) = delete;
-  WorkloadStats& operator=(const WorkloadStats&) = delete;
-};
-    
-    /////////////////////////////////////////////////////////////////////////////
-    
-    void initNuma() {
-  if (getenv('HHVM_DISABLE_NUMA')) {
-    return;
-  }
-  // When linked dynamically numa_init() is called before JEMallocInitializer()
-  // numa_init is not exported by libnuma.so so it will be NULL
-  // however when linked statically numa_init() is not guaranteed to be called
-  // before JEMallocInitializer(), so call it here.
-  if (&numa_init) {
-    numa_init();
-  }
-  if (numa_available() < 0) return;
-    }
-    
-    struct DataBlockFull : std::runtime_error {
-  std::string name;
-    }
-    
-    bool CurlShareResource::setOption(int option, const Variant& value) {
-  if (m_share == nullptr) {
-    raise_warning('curl_share_setopt():'
-                  ' curl share not created or is already closed');
-    return false;
-  }
-    }
-    
-    #ifndef incl_HPHP_CURL_SHARE_RESOURCE_H
-#define incl_HPHP_CURL_SHARE_RESOURCE_H
-    
-    /**
- * A Synchronizable object that has multiple conditional variables. The benefit
- * is, notify() can choose to wake up a thread that is more favorable (e.g.,
- * one with stack/heap mapped on huge pages, or one that is recently active).
- */
-struct SynchronizableMulti {
-  explicit SynchronizableMulti(int size);
-  virtual ~SynchronizableMulti() {}
-    }
-    
-    TCA genFuncPrologue(TransID transID, TransKind kind, Func* func, int argc,
-                    CodeCache::View code, CGMeta& fixups) {
-  auto context = prologue_context(transID, kind, func,
-                                  func->getEntryForNumArgs(argc));
-  IRUnit unit{context};
-  irgen::IRGS env{unit, nullptr};
-    }
-    
-      /**
-   * @brief A getter for the message property
-   *
-   * @return a string representing arbitrary additional information about the
-   * success or failure of an operation. On successful operations, the idiom
-   * is for the message to be 'OK'
+    {  /**
+   * Invoked if the stream was canceled via
+   * bidirectional_stream_cancel(). Once invoked, no further callback
+   * methods will be invoked.
    */
-  std::string getMessage() const { return message_; }
+  void (*on_canceled)(bidirectional_stream* stream);
+} bidirectional_stream_callback;
     
-    class PermissionsPollRunnable : public PermissionsRunnable {
- public:
-  PermissionsPollRunnable() : PermissionsRunnable('PermissionsPollRunnable') {}
+      ClientConfig client_config;
+  client_config.set_client_type(SYNC_CLIENT);
+  client_config.set_outstanding_rpcs_per_channel(1);
+  client_config.set_client_channels(1);
+  client_config.set_rpc_type(UNARY);
+  client_config.mutable_load_params()->mutable_closed_loop();
+    
+    #include <algorithm>
+    
+    // Tucks all generator state in an anonymous namespace away from
+// PythonGrpcGenerator and the header file, mostly to encourage future changes
+// to not require updates to the grpcio-tools C++ code part. Assumes that it is
+// only ever used from a single thread.
+struct PrivateGenerator {
+  const GeneratorConfiguration& config;
+  const grpc_generator::File* file;
     }
     
+    grpc::string DescribeService(const grpc::protobuf::ServiceDescriptor* service) {
+  grpc::string result;
+  if (service->options().deprecated()) {
+    result.append('DEPRECATED\n');
+  }
+  result.append('filename: ' + service->file()->name() + '\n');
+    }
     
-    {
-    {  // Set the time at now to 2.
-  TablePlugin::kCacheStep = 2;
-  test.testSetCache(TablePlugin::kCacheStep, TablePlugin::kCacheInterval);
-  EXPECT_TRUE(test.testIsCached(5));
-  // Now 6 is within the freshness of 2 + 5.
-  EXPECT_TRUE(test.testIsCached(6));
-  EXPECT_FALSE(test.testIsCached(7));
+    static int set_addresses(struct ifaddrs* ifaddr, ifaddrmsg* msg, void* data,
+		  size_t len) {
+	if (msg->ifa_family == AF_INET) {
+		sockaddr_in* sa = new sockaddr_in;
+		sa->sin_family = AF_INET;
+		memcpy(&sa->sin_addr, data, len);
+		ifaddr->ifa_addr = reinterpret_cast<sockaddr*>(sa);
+	} else if (msg->ifa_family == AF_INET6) {
+		sockaddr_in6* sa = new sockaddr_in6;
+		sa->sin6_family = AF_INET6;
+		sa->sin6_scope_id = msg->ifa_index;
+		memcpy(&sa->sin6_addr, data, len);
+		ifaddr->ifa_addr = reinterpret_cast<sockaddr*>(sa);
+	} else {
+		return -1;
+	}
+	return 0;
 }
+    
+    PowerIphone::~PowerIphone() {
+	// TODO Auto-generated destructor stub
 }
 
     
-    namespace osquery {
+    		FileAccess *f = FileAccess::open(certs_path, FileAccess::READ);
+		if (f) {
+			int flen = f->get_len();
+			out.resize(flen + 1);
+			{
+				PoolByteArray::Write w = out.write();
+				f->get_buffer(w.ptr(), flen);
+				w[flen] = 0; //end f string
+			}
     }
     
-    SEXP XGBoosterDumpModel_R(SEXP handle, SEXP fmap, SEXP with_stats, SEXP dump_format) {
+    
+    {	int sent;
+	Error err = sp->base->put_partial_data((const uint8_t *)buf, len, sent);
+	if (err != OK) {
+		return MBEDTLS_ERR_SSL_INTERNAL_ERROR;
+	}
+	if (sent == 0) {
+		return MBEDTLS_ERR_SSL_WANT_WRITE;
+	}
+	return sent;
+}
+    
+    	static int bio_recv(void *ctx, unsigned char *buf, size_t len);
+	static int bio_send(void *ctx, const unsigned char *buf, size_t len);
+    
+    #endif // WEBSOCKET_CLIENT_H
+
+    
+    		MethodBind *bind = create_method_bind(p_method);
+		const Variant *ptr[2] = { &p_def1, &p_def2 };
+    
+    	bool singleton;
+	bool load_once;
+	String symbol_prefix;
+	bool reloadable;
+    
+        const int tmp0 = (ACCESS_ROW(0) + ACCESS_ROW(4)) << CONST_BITS;
+    const int tmp1 = (ACCESS_ROW(0) - ACCESS_ROW(4)) << CONST_BITS;
+    
+    
+    
+    TEST_F(ViewsConfigParserPluginTests, test_update_view) {
+  Config c;
+  std::vector<std::string> old_views_vec;
+  scanDatabaseKeys(kQueries, old_views_vec, 'config_views.');
+  EXPECT_EQ(old_views_vec.size(), 1U);
+  old_views_vec.clear();
+  auto s = c.update(getTestConfigMap('view_test2.conf'));
+  EXPECT_TRUE(s.ok());
+  scanDatabaseKeys(kQueries, old_views_vec, 'config_views.');
+  EXPECT_EQ(old_views_vec.size(), 1U);
+  std::string query;
+  getDatabaseValue(kQueries, 'config_views.kernel_hashes_new', query);
+  EXPECT_EQ(query,
+            'select hash.path as binary, version, hash.sha256 as SHA256, '
+            'hash.sha1 as SHA1, hash.md5 as MD5 from (select path || '
+            ''/Contents/MacOS/' as directory, name, version from '
+            'kernel_extensions) join hash using (directory)');
+    }
+    
+    class TLSConfigPlugin : public ConfigPlugin,
+                        public std::enable_shared_from_this<TLSConfigPlugin> {
+ public:
+  Status setUp() override;
+  Status genConfig(std::map<std::string, std::string>& config) override;
+    }
+    
+      // Set the multi-thread path, which both threads will write into.
+  auto multi_thread_path = fs::path(kTestWorkingDirectory) / 'threadperms.txt';
+  kMultiThreadPermissionPath = multi_thread_path.string();
+    
+    
+    {  EXPECT_EQ(constraint.op, EQUALS);
+  EXPECT_EQ(constraint.expr, 'none');
+}
+    
+    class TimeTests : public testing::Test {};
+    
+    
+    {  status_ = Status(0);
+}
+    
+    class RocksDBDatabasePluginTests : public DatabasePluginTests {
+ protected:
+  std::string name() override {
+    return 'rocksdb';
+  }
+};
+    
+    
+    {  auto results = generateHeader(lengths, order);
+  auto expected = '| name       | age  | food                    | number |\n';
+  EXPECT_EQ(results, expected);
+}
+    
+    struct SCNetworkSubscriptionContext : public SubscriptionContext {
+  // Target type.
+  SCNetworkSubscriptionType type;
+    }
+    
+    namespace xgboost {
+ConsoleLogger::~ConsoleLogger() {
+  dmlc::CustomLogMessage::Log(log_stream_.str());
+}
+TrackerLogger::~TrackerLogger() {
+  dmlc::CustomLogMessage::Log(log_stream_.str());
+}
+}  // namespace xgboost
+    
+    SEXP XGBoosterGetAttrNames_R(SEXP handle) {
   SEXP out;
   R_API_BEGIN();
-  bst_ulong olen;
+  bst_ulong len;
   const char **res;
-  const char *fmt = CHAR(asChar(dump_format));
-  CHECK_CALL(XGBoosterDumpModelEx(R_ExternalPtrAddr(handle),
-                                CHAR(asChar(fmap)),
-                                asInteger(with_stats),
-                                fmt,
-                                &olen, &res));
-  out = PROTECT(allocVector(STRSXP, olen));
-  if (!strcmp('json', fmt)) {
-    std::stringstream stream;
-    stream <<  '[\n';
-    for (size_t i = 0; i < olen; ++i) {
-      stream << res[i];
-      if (i < olen - 1) {
-        stream << ',\n';
-      } else {
-        stream << '\n';
-      }
+  CHECK_CALL(XGBoosterGetAttrNames(R_ExternalPtrAddr(handle),
+                                   &len, &res));
+  if (len > 0) {
+    out = PROTECT(allocVector(STRSXP, len));
+    for (size_t i = 0; i < len; ++i) {
+      SET_STRING_ELT(out, i, mkChar(res[i]));
     }
-    stream <<  ']';
-    SET_STRING_ELT(out, 0, mkChar(stream.str().c_str()));
   } else {
-    for (size_t i = 0; i < olen; ++i) {
-      std::stringstream stream;
-      stream <<  'booster[' << i <<']\n' << res[i];
-      SET_STRING_ELT(out, i, mkChar(stream.str().c_str()));
-    }
+    out = PROTECT(R_NilValue);
   }
   R_API_END();
   UNPROTECT(1);
   return out;
 }
+
     
-          ASSERT_TRUE(input == output);
-    
-    // logistic loss, but predict un-transformed margin
-struct LogisticRaw : public LogisticRegression {
-  // duplication is necessary, as __device__ specifier
-  // cannot be made conditional on template parameter
-  XGBOOST_DEVICE static bst_float PredTransform(bst_float x) { return x; }
-  XGBOOST_DEVICE static bst_float FirstOrderGradient(bst_float predt, bst_float label) {
-    predt = common::Sigmoid(predt);
-    return predt - label;
-  }
-  XGBOOST_DEVICE static bst_float SecondOrderGradient(bst_float predt, bst_float label) {
-    const float eps = 1e-16f;
-    predt = common::Sigmoid(predt);
-    return fmaxf(predt * (1.0f - predt), eps);
-  }
-  template <typename T>
-    static T PredTransform(T x) { return x; }
-  template <typename T>
-    static T FirstOrderGradient(T predt, T label) {
-    predt = common::Sigmoid(predt);
-    return predt - label;
-  }
-  template <typename T>
-    static T SecondOrderGradient(T predt, T label) {
-    const T eps = T(1e-16f);
-    predt = common::Sigmoid(predt);
-    return std::max(predt * (T(1.0f) - predt), eps);
-  }
-  static const char* DefaultEvalMetric() { return 'auc'; }
-};
-    
-    
-    {
-    {XGBOOST_REGISTER_LINEAR_UPDATER(ShotgunUpdater, 'shotgun')
-    .describe(
-        'Update linear model according to shotgun coordinate descent '
-        'algorithm.')
-    .set_body([]() { return new ShotgunUpdater(); });
-}  // namespace linear
-}  // namespace xgboost
+    /*! \brief interface of objective function */
+class ObjFunction {
+ public:
+  /*! \brief virtual destructor */
+  virtual ~ObjFunction() = default;
+  /*!
+   * \brief set configuration from pair iterators.
+   * \param begin The beginning iterator.
+   * \param end The end iterator.
+   * \tparam PairIter iterator<std::pair<std::string, std::string> >
+   */
+  template<typename PairIter>
+  inline void Configure(PairIter begin, PairIter end);
+  /*!
+   * \brief Configure the objective with the specified parameters.
+   * \param args arguments to the objective function.
+   */
+  virtual void Configure(const std::vector<std::pair<std::string, std::string> >& args) = 0;
+  /*!
+   * \brief Get gradient over each of predictions, given existing information.
+   * \param preds prediction of current round
+   * \param info information about labels, weights, groups in rank
+   * \param iteration current iteration number.
+   * \param out_gpair output of get gradient, saves gradient and second order gradient in
+   */
+  virtual void GetGradient(HostDeviceVector<bst_float>* preds,
+                           const MetaInfo& info,
+                           int iteration,
+                           HostDeviceVector<GradientPair>* out_gpair) = 0;
+    }
