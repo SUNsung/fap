fe93273570af03f27a71066c08853d4e5537f9da
SUNsung <weatherius@gmail.com>
2018-06-23 23:25:37 +0300

    Rewrite [26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb] File in: 2018-06-23 23:25:36

--- ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
+++ ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
@@ -1,83 +1,54 @@
 
         
-          def self.expire_cache_fragment!(name)
-    fragment_cache.delete(name)
-  end
+          test 'permits empty params when received not a hash' do
+    sanitizer = sanitizer({ 'user' => 'string' })
+    sanitized = sanitizer.sanitize(:sign_in)
     
-        def form_configurable_fields
-      self._form_configurable_fields
-    end
-    
-      before_action :upgrade_warning, only: :index
-    
-      private
-    
-      def test_gets_para_extended_file
-    [nil, {:textmode=>true}, {:binmode=>true}].each do |mode|
-      Tempfile.create('test-extended-file', mode) {|f|
-        assert_nil(f.getc)
-        f.print '\na'
-        f.rewind
-        assert_equal('a', f.gets(''), 'mode = <#{mode}>')
-      }
-    end
-  end
-    
-      it 'decodes the remaining doubles when passed the '*' modifier after another directive' do
-    array = '333333\x15@ffffff\x22@'.unpack(unpack_format()+unpack_format('*'))
-    array.should == [5.3, 9.2]
-  end
-    
-        it 'propagates inner exception to Thread.join if there is an outer ensure clause' do
-      thread = ThreadSpecs.dying_thread_with_outer_ensure(@method) { }
-      lambda { thread.join }.should raise_error(RuntimeError, 'In dying thread')
-    end
-    
-        # Merges this query with another. The returned query queries for
-    # the intersection between the two inputs.
-    #
-    # Both queries should be resolved.
-    #
-    # @param other [Query]
-    # @return [Query?] The merged query, or nil if there is no intersection.
-    def merge(other)
-      m1, t1 = resolved_modifier.downcase, resolved_type.downcase
-      m2, t2 = other.resolved_modifier.downcase, other.resolved_type.downcase
-      t1 = t2 if t1.empty?
-      t2 = t1 if t2.empty?
-      if (m1 == 'not') ^ (m2 == 'not')
-        return if t1 == t2
-        type = m1 == 'not' ? t2 : t1
-        mod = m1 == 'not' ? m2 : m1
-      elsif m1 == 'not' && m2 == 'not'
-        # CSS has no way of representing 'neither screen nor print'
-        return unless t1 == t2
-        type = t1
-        mod = 'not'
-      elsif t1 != t2
-        return
-      else # t1 == t2, neither m1 nor m2 are 'not'
-        type = t1
-        mod = m1.empty? ? m2 : m1
-      end
-      Query.new([mod], [type], other.expressions + expressions)
-    end
-    
-      module Sass::Plugin::Configuration
-    # Different default options in a m environment.
-    def default_options
-      @default_options ||= begin
-        version = Merb::VERSION.split('.').map {|n| n.to_i}
-        if version[0] <= 0 && version[1] < 5
-          root = MERB_ROOT
-          env  = MERB_ENV
-        else
-          root = Merb.root.to_s
-          env  = Merb.environment
+            # To not require reconfirmation after creating with #save called in a
+        # callback call skip_create_confirmation!
+        def skip_reconfirmation_in_callback!
+          @skip_reconfirmation_in_callback = true
         end
     
-        def script_url_for(gist_id, filename)
-      url = 'https://gist.github.com/#{gist_id}.js'
-      url = '#{url}?file=#{filename}' unless filename.nil? or filename.empty?
-      url
+      if options.respond_to? 'keys'
+    options.each do |k,v|
+      unless v.nil?
+        v = v.join ',' if v.respond_to? 'join'
+        v = v.to_json if v.respond_to? 'keys'
+        output += ' data-#{k.sub'_','-'}='#{v}''
+      end
     end
+  elsif options.respond_to? 'join'
+    output += ' data-value='#{config[key].join(',')}''
+  else
+    output += ' data-value='#{config[key]}''
+  end
+  output += '></#{tag}>'
+end
+    
+      class RenderPartialTag < Liquid::Tag
+    include OctopressFilters
+    def initialize(tag_name, markup, tokens)
+      @file = nil
+      @raw = false
+      if markup =~ /^(\S+)\s?(\w+)?/
+        @file = $1.strip
+        @raw = $2 == 'raw'
+      end
+      super
+    end
+    
+        def define_getters
+      define_instance_getter
+      define_class_getter
+    end
+    
+      class Railtie
+    def self.insert
+      Paperclip.options[:logger] = Rails.logger
+    
+          def validate_blacklist(record, attribute, value)
+        if forbidden_types.present? && forbidden_types.any? { |type| type === value }
+          mark_invalid record, attribute, forbidden_types
+        end
+      end
