2489b024a29842c4900a3771c62489274d747de1
SUNsung <weatherius@gmail.com>
2018-06-23 06:59:21 +0300

    Rewrite [17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp] File in: 2018-06-23 06:59:20

--- cccccc/17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp
+++ cccccc/17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp
@@ -1,303 +1,132 @@
 
         
-        class DebugGraphDecorator : public DebugGraphDecoratorInterface {
- public:
-  DebugGraphDecorator(const DebugOptions& debug_options)
-      : debug_options_(debug_options) {}
-  virtual ~DebugGraphDecorator() {}
-    }
-    
-    #endif  // TENSORFLOW_COMMON_RUNTIME_SESSION_FACTORY_H_
-
-    
-    // Declare here, so we don't need a public header.
-Status RemoveDevice(const GraphDef& input_graph_def,
-                    const TransformFuncContext& context,
-                    GraphDef* output_graph_def);
-    
-    // TODO: Enable GPU support for angle op after resolving
-// build failures on GPU (See #10643 for context).
-#if 0 && GOOGLE_CUDA
-    
-    
-    {  // clang-format off
-  std::vector<FDH::Node> nodes = {
-   {{'x_shape'}, 'Shape', {'x'}},
-   {{'x_rank'}, 'Rank', {'x'}},
-   {{'i_shape'}, 'Shape', {'i'}, {{'T', DT_INT32}}},
-   FDH::Const('zero', 0),
-   FDH::Const('one', 1),
-   // stitch_idx0 = Range(0, x_rank, 1)
-   {{'stitch_val1'}, 'Fill', {'i_shape:output:0', 'one:output:0'},
-    {{'T', DT_INT32}}},
-   {{'y_shape'}, 'DynamicStitch',
-    {'stitch_idx0:output:0', 'i',
-     'x_shape:output:0', 'stitch_val1:output:0'},
-    {{'N', 2}, {'T', DT_INT32}}},
-   {{'tile_scaling'}, 'Div', {'x_shape:output:0', 'y_shape:merged:0'},
-    {{'T', DT_INT32}}},
-   {{'di'}, 'ZerosLike', {'i'}, {{'T', DT_INT32}}}
-  };
-  // clang-format on
-  nodes.insert(nodes.end(), body.begin(), body.end());
-  for (auto& n : nodes) {
-    if (n.attr.empty()) {
-      n.attr = {{'T', '$T'}};
-    }
-  }
-  // 'Range' doesn't need any attr.
-  nodes.push_back({{'stitch_idx0'},
-                   'Range',
-                   {'zero:output:0', 'x_rank:output:0', 'one:output:0'},
-                   {}});
-  *g = FDH::Create('_',
-                   // Input defs
-                   {'x:T', 'i:int32', 'dy:T'},
-                   // Ret val defs
-                   {'dx:T', 'di:int32'},
-                   // Attr defs
-                   {{'T: {half, float, double}'}},
-                   // Nodes
-                   nodes,
-                   // Return values
-                   {{'dx', 'dx:output:0'}, {'di', 'di:y:0'}});
-  return Status::OK();
-}
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    #include 'tensorflow/core/framework/tensor_testutil.h'
-#include 'tensorflow/core/lib/core/status_test_util.h'
-#include 'tensorflow/core/lib/io/path.h'
-#include 'tensorflow/core/lib/strings/str_util.h'
-#include 'tensorflow/core/platform/env.h'
-#include 'tensorflow/core/platform/test.h'
-    
-    
-    {
-    {    if (SemanticNode.is<Expr *>()) {
-      SemanticNode.get<Expr *>()->dump(llvm::errs());
-    } else if (SemanticNode.is<Decl *>()) {
-      SemanticNode.get<Decl *>()->dump(llvm::errs());
-    } else if (SemanticNode.is<Expr *>()) {
-      SemanticNode.get<Expr *>()->dump(llvm::errs());
-    } else {
-      llvm_unreachable('ASTNode has pointer to unknown thing!');
-    }
-    llvm::errs() << '\n=====================================================\n';
-  }
-}
-
-    
-    
-    {
-    {    for (auto *FU : mod->getFiles()) {
-      switch (FU->getKind()) {
-      case FileUnitKind::Source:
-      case FileUnitKind::Derived:
-      case FileUnitKind::Builtin:
-        break;
-      case FileUnitKind::SerializedAST:
-      case FileUnitKind::ClangModule: {
-        auto *LFU = cast<LoadedFile>(FU);
-        if (auto *F = fileMgr.getFile(LFU->getFilename())) {
-          std::string moduleName = mod->getNameStr();
-          bool withoutUnitName = true;
-          if (FU->getKind() == FileUnitKind::ClangModule) {
-            withoutUnitName = false;
-            auto clangModUnit = cast<ClangModuleUnit>(LFU);
-            if (auto clangMod = clangModUnit->getUnderlyingClangModule()) {
-              moduleName = clangMod->getTopLevelModuleName();
-              // FIXME: clang's -Rremarks do not seem to go through Swift's
-              // diagnostic emitter.
-              clang::index::emitIndexDataForModuleFile(clangMod,
-                                                       clangCI, unitWriter);
-            }
-          } else {
-            // Serialized AST file.
-            // Only index system modules (essentially stdlib and overlays).
-            // We don't officially support binary swift modules, so normally
-            // the index data for user modules would get generated while
-            // building them.
-            if (mod->isSystemModule() && indexSystemModules) {
-              emitDataForSwiftSerializedModule(mod, indexStorePath,
-                                               indexSystemModules,
-                                               targetTriple, clangCI, diags,
-                                               unitWriter);
-              withoutUnitName = false;
-            }
-          }
-          clang::index::writer::OpaqueModule opaqMod =
-              moduleNameScratch.createString(moduleName);
-          unitWriter.addASTFileDependency(F, mod->isSystemModule(), opaqMod,
-                                          withoutUnitName);
-        }
-        break;
-      }
-      }
-    }
-  }
-}
-    
-    
-    {  // Make vanilla new/delete illegal.
-  void *operator new(size_t Bytes) = delete;
-  void operator delete(void *Data) = delete;
-};
-    
-    
-    {  /// Return a hash code of any components from these options that should
-  /// contribute to a Swift Bridging PCH hash.
-  llvm::hash_code getPCHHashComponents() const {
-    // Nothing here that contributes anything significant when emitting the PCH.
-    return llvm::hash_value(0);
-  }
-};
-    
-    #ifndef SWIFT_INDEX_INDEXSYMBOL_H
-#define SWIFT_INDEX_INDEXSYMBOL_H
-    
-    Variant HHVM_FUNCTION(xhprof_disable) {
-  return s_profiler_factory->stop();
-}
-    
-    namespace HPHP { namespace jit {
-///////////////////////////////////////////////////////////////////////////////
-    }
-    }
-    
-      case GMP_ROUND_PLUSINF:
-    mpz_cdiv_r(gmpReturn, gmpDataA, gmpDataB);
-    break;
-    
-    // Normalize incoming arc weights and compute avgCallOffset for each node.
-void TargetGraph::normalizeArcWeights() {
-  for (TargetId f = 0; f < targets.size(); f++) {
-    auto& func = targets[f];
-    for (auto src : func.preds) {
-      auto& arc = *arcs.find(Arc(src, f));
-      arc.normalizedWeight = arc.weight / func.samples;
-      if (arc.weight == 0) continue;
-      arc.avgCallOffset = arc.avgCallOffset / arc.weight;
-      assert(arc.avgCallOffset < targets[src].size);
-    }
-  }
-}
-    
-    
-    {  return rawbits_to_double(result);
-}
-    
-    ///////////////////////////////////////////////////////////////////////////////
-    
-    #ifdef HAVE_NUMA
-namespace {
-// We support at most 32 NUMA nodes (numa_node_set in 32-bit), so a single
-// unsigned long is more than enough for the mask.  This can be used in jemalloc
-// allocation hooks, so it is wise to avoid calling malloc/free here, even
-// though jemalloc might still be able to handle reentrance correctly.  Thus, we
-// bypass libnuma and do the syscalls directly here.
-struct SavedNumaPolicy {
-  bool needRestore{false};
-  int oldPolicy{0};
-  unsigned long oldMask{0};
-    }
-    }
-    
-    TEST(VersionEditTest, EncodeDecode) {
-  static const uint64_t kBig = 1ull << 50;
-    }
-    
-    int main(int argc, char** argv) {
-  return leveldb::test::RunAllTests();
-}
-
-    
-    
-    {}
-    
-    Status BlockHandle::DecodeFrom(Slice* input) {
-  if (GetVarint64(input, &offset_) &&
-      GetVarint64(input, &size_)) {
-    return Status::OK();
-  } else {
-    return Status::Corruption('bad block handle');
-  }
-}
-    
-    
-    { private:
-  port::Mutex *const mu_;
-  // No copying allowed
-  MutexLock(const MutexLock&);
-  void operator=(const MutexLock&);
-};
-    
-    class DatabasePluginTests : public testing::Test {
- public:
-  void SetUp() override;
-    }
-    
-        // Widgets: Value() Helpers. Output single value in 'name: value' format (tip: freely declare more in your code to handle your types. you can add functions to the ImGui namespace)
-    IMGUI_API void          Value(const char* prefix, bool b);
-    IMGUI_API void          Value(const char* prefix, int v);
-    IMGUI_API void          Value(const char* prefix, unsigned int v);
-    IMGUI_API void          Value(const char* prefix, float v, const char* float_format = NULL);
-    
-            // Get the function pointer (required for any extensions)
-        auto vkCreateDebugReportCallbackEXT = (PFN_vkCreateDebugReportCallbackEXT)vkGetInstanceProcAddr(g_Instance, 'vkCreateDebugReportCallbackEXT');
-        IM_ASSERT(vkCreateDebugReportCallbackEXT != NULL);
-    
-        // Create shaders
-    const GLchar *vertex_shader =
-        'uniform mat4 ProjMtx;\n'
-        'in vec2 Position;\n'
-        'in vec2 UV;\n'
-        'in vec4 Color;\n'
-        'out vec2 Frag_UV;\n'
-        'out vec4 Frag_Color;\n'
-        'void main()\n'
-        '{\n'
-        '	Frag_UV = UV;\n'
-        '	Frag_Color = Color;\n'
-        '	gl_Position = ProjMtx * vec4(Position.xy,0,1);\n'
-        '}\n';
-    
-    // That gcc wants both of these prototypes seems mysterious. VC, for
-// its part, can't decide which to use (another mystery). Matching of
-// template overloads: the final frontier.
-#ifndef COMPILER_MSVC
-template <typename T, size_t N>
-char (&ArraySizeHelper(const T (&array)[N]))[N];
+        #ifdef HAVE_EIGEN
+#  if defined __GNUC__ && defined __APPLE__
+#    pragma GCC diagnostic ignored '-Wshadow'
+#  endif
+#  include <Eigen/Core>
+#  include <Eigen/Eigenvalues>
+#  include 'opencv2/core/eigen.hpp'
 #endif
     
-    #endif  // BENCHMARK_COMMANDLINEFLAGS_H_
+    void ComputeExtrinsicRefine(const Mat& imagePoints, const Mat& objectPoints, Mat& rvec,
+                            Mat&  tvec, Mat& J, const int MaxIter,
+                            const IntrinsicParams& param, const double thresh_cond);
+CV_EXPORTS Mat ComputeHomography(Mat m, Mat M);
+    
+    CV_EXPORTS_W void subtract(InputArray src1, Scalar src2, OutputArray dst, InputArray mask=noArray(), int dtype=-1);
+    
+    #ifndef __OPENCV_CORE_OCL_RUNTIME_COMMON_HPP__
+#define __OPENCV_CORE_OCL_RUNTIME_COMMON_HPP__
+    
+    const float g_8x32fTab[] =
+{
+    -128.f, -127.f, -126.f, -125.f, -124.f, -123.f, -122.f, -121.f,
+    -120.f, -119.f, -118.f, -117.f, -116.f, -115.f, -114.f, -113.f,
+    -112.f, -111.f, -110.f, -109.f, -108.f, -107.f, -106.f, -105.f,
+    -104.f, -103.f, -102.f, -101.f, -100.f,  -99.f,  -98.f,  -97.f,
+     -96.f,  -95.f,  -94.f,  -93.f,  -92.f,  -91.f,  -90.f,  -89.f,
+     -88.f,  -87.f,  -86.f,  -85.f,  -84.f,  -83.f,  -82.f,  -81.f,
+     -80.f,  -79.f,  -78.f,  -77.f,  -76.f,  -75.f,  -74.f,  -73.f,
+     -72.f,  -71.f,  -70.f,  -69.f,  -68.f,  -67.f,  -66.f,  -65.f,
+     -64.f,  -63.f,  -62.f,  -61.f,  -60.f,  -59.f,  -58.f,  -57.f,
+     -56.f,  -55.f,  -54.f,  -53.f,  -52.f,  -51.f,  -50.f,  -49.f,
+     -48.f,  -47.f,  -46.f,  -45.f,  -44.f,  -43.f,  -42.f,  -41.f,
+     -40.f,  -39.f,  -38.f,  -37.f,  -36.f,  -35.f,  -34.f,  -33.f,
+     -32.f,  -31.f,  -30.f,  -29.f,  -28.f,  -27.f,  -26.f,  -25.f,
+     -24.f,  -23.f,  -22.f,  -21.f,  -20.f,  -19.f,  -18.f,  -17.f,
+     -16.f,  -15.f,  -14.f,  -13.f,  -12.f,  -11.f,  -10.f,   -9.f,
+      -8.f,   -7.f,   -6.f,   -5.f,   -4.f,   -3.f,   -2.f,   -1.f,
+       0.f,    1.f,    2.f,    3.f,    4.f,    5.f,    6.f,    7.f,
+       8.f,    9.f,   10.f,   11.f,   12.f,   13.f,   14.f,   15.f,
+      16.f,   17.f,   18.f,   19.f,   20.f,   21.f,   22.f,   23.f,
+      24.f,   25.f,   26.f,   27.f,   28.f,   29.f,   30.f,   31.f,
+      32.f,   33.f,   34.f,   35.f,   36.f,   37.f,   38.f,   39.f,
+      40.f,   41.f,   42.f,   43.f,   44.f,   45.f,   46.f,   47.f,
+      48.f,   49.f,   50.f,   51.f,   52.f,   53.f,   54.f,   55.f,
+      56.f,   57.f,   58.f,   59.f,   60.f,   61.f,   62.f,   63.f,
+      64.f,   65.f,   66.f,   67.f,   68.f,   69.f,   70.f,   71.f,
+      72.f,   73.f,   74.f,   75.f,   76.f,   77.f,   78.f,   79.f,
+      80.f,   81.f,   82.f,   83.f,   84.f,   85.f,   86.f,   87.f,
+      88.f,   89.f,   90.f,   91.f,   92.f,   93.f,   94.f,   95.f,
+      96.f,   97.f,   98.f,   99.f,  100.f,  101.f,  102.f,  103.f,
+     104.f,  105.f,  106.f,  107.f,  108.f,  109.f,  110.f,  111.f,
+     112.f,  113.f,  114.f,  115.f,  116.f,  117.f,  118.f,  119.f,
+     120.f,  121.f,  122.f,  123.f,  124.f,  125.f,  126.f,  127.f,
+     128.f,  129.f,  130.f,  131.f,  132.f,  133.f,  134.f,  135.f,
+     136.f,  137.f,  138.f,  139.f,  140.f,  141.f,  142.f,  143.f,
+     144.f,  145.f,  146.f,  147.f,  148.f,  149.f,  150.f,  151.f,
+     152.f,  153.f,  154.f,  155.f,  156.f,  157.f,  158.f,  159.f,
+     160.f,  161.f,  162.f,  163.f,  164.f,  165.f,  166.f,  167.f,
+     168.f,  169.f,  170.f,  171.f,  172.f,  173.f,  174.f,  175.f,
+     176.f,  177.f,  178.f,  179.f,  180.f,  181.f,  182.f,  183.f,
+     184.f,  185.f,  186.f,  187.f,  188.f,  189.f,  190.f,  191.f,
+     192.f,  193.f,  194.f,  195.f,  196.f,  197.f,  198.f,  199.f,
+     200.f,  201.f,  202.f,  203.f,  204.f,  205.f,  206.f,  207.f,
+     208.f,  209.f,  210.f,  211.f,  212.f,  213.f,  214.f,  215.f,
+     216.f,  217.f,  218.f,  219.f,  220.f,  221.f,  222.f,  223.f,
+     224.f,  225.f,  226.f,  227.f,  228.f,  229.f,  230.f,  231.f,
+     232.f,  233.f,  234.f,  235.f,  236.f,  237.f,  238.f,  239.f,
+     240.f,  241.f,  242.f,  243.f,  244.f,  245.f,  246.f,  247.f,
+     248.f,  249.f,  250.f,  251.f,  252.f,  253.f,  254.f,  255.f
+};
+    
+    #ifndef GRPC_TEST_CPP_UTIL_BENCHMARK_CONFIG_H
+#define GRPC_TEST_CPP_UTIL_BENCHMARK_CONFIG_H
+    
+      // Set up security params
+  SecurityParams security;
+  security.set_use_test_ca(true);
+  security.set_server_host_override('foo.test.google.fr');
+  client_config.mutable_security_params()->CopyFrom(security);
+  server_config.mutable_security_params()->CopyFrom(security);
+    
+    static void sigint_handler(int x) { got_sigint = true; }
+    
+    // Tucks all generator state in an anonymous namespace away from
+// PythonGrpcGenerator and the header file, mostly to encourage future changes
+// to not require updates to the grpcio-tools C++ code part. Assumes that it is
+// only ever used from a single thread.
+struct PrivateGenerator {
+  const GeneratorConfiguration& config;
+  const grpc_generator::File* file;
+    }
+    
+    TEST_F(CodegenTestMinimal, Build) {}
+    
+    int main(int argc, char** argv) {
+  grpc::testing::InitTest(&argc, &argv, true);
+  signal(SIGINT, sigint_handler);
+    }
+    
+    void freeifaddrs(struct ifaddrs* addrs) {
+	struct ifaddrs* last = NULL;
+	struct ifaddrs* cursor = addrs;
+	while (cursor) {
+		delete[] cursor->ifa_name;
+		delete cursor->ifa_addr;
+		delete cursor->ifa_netmask;
+		last = cursor;
+		cursor = cursor->ifa_next;
+		delete last;
+	}
+}
 
     
+    	void _on_peer_packet();
+	void _on_connect(String p_protocol);
+	void _on_disconnect();
+	void _on_error();
     
-    {  // Print user counters
-  for (const auto &ucn : user_counter_names_) {
-    auto it = run.counters.find(ucn);
-    if(it == run.counters.end()) {
-      Out << ',';
-    } else {
-      Out << ',' << it->second;
+    
+    {} // namespace boost
+    
+    template <typename MutableBufferSequence, typename Handler>
+class descriptor_read_op
+  : public descriptor_read_op_base<MutableBufferSequence>
+{
+public:
+  BOOST_ASIO_DEFINE_HANDLER_PTR(descriptor_read_op);
     }
-  }
-  Out << '\n';
-}
     
-    namespace benchmark {
-    }
-    
-    auto Sqr = [](const double dat) { return dat * dat; };
-auto Sqrt = [](const double dat) {
-  // Avoid NaN due to imprecision in the calculations
-  if (dat < 0.0) return 0.0;
-  return std::sqrt(dat);
-};
+    #endif // BOOST_ASIO_DETAIL_IMPL_BUFFER_SEQUENCE_ADAPTER_IPP
