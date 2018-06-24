55e4e6074e67731f8a6fc23eb5a377dade724db7
SUNsung <weatherius@gmail.com>
2018-06-24 12:58:58 +0300

    Rewrite [18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp] File in: 2018-06-24 12:58:58

--- cccccc/18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp
+++ cccccc/18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp
@@ -1,188 +1,205 @@
 
         
-          /* Invoked when initial response headers are received.
-   * Consumer must call bidirectional_stream_read() to start reading.
-   * Consumer may call bidirectional_stream_write() to start writing or
-   * close the stream. Contents of |headers| is valid for duration of the call.
-   */
-  void (*on_response_headers_received)(
-      bidirectional_stream* stream,
-      const bidirectional_stream_header_array* headers,
-      const char* negotiated_protocol);
-    
-    #ifndef GRPC_COMMON_CPP_ROUTE_GUIDE_HELPER_H_
-#define GRPC_COMMON_CPP_ROUTE_GUIDE_HELPER_H_
-    
-      bool generate_in_pb2_grpc;
-    
-    #include 'src/compiler/python_generator.h'
-    
-    	PathCombine(userCfgPath, userConfigDirPath, L'user-ConEmu.xml');
-    
-    void OutputImageComponent::ToPixels(int xmin, int ymin, int xsize, int ysize,
-                                    uint8_t* out, int stride) const {
-  assert(xmin >= 0);
-  assert(ymin >= 0);
-  assert(xmin < width_);
-  assert(ymin < height_);
-  const int yend1 = ymin + ysize;
-  const int yend0 = std::min(yend1, height_);
-  int y = ymin;
-  for (; y < yend0; ++y) {
-    const int xend1 = xmin + xsize;
-    const int xend0 = std::min(xend1, width_);
-    int x = xmin;
-    int px = y * width_ + xmin;
-    for (; x < xend0; ++x, ++px, out += stride) {
-      *out = static_cast<uint8_t>((pixels_[px] + 8 - (x & 1)) >> 4);
-    }
-    const int offset = -stride;
-    for (; x < xend1; ++x) {
-      *out = out[offset];
-      out += stride;
-    }
-  }
-  for (; y < yend1; ++y) {
-    const int offset = -stride * xsize;
-    for (int x = 0; x < xsize; ++x) {
-      *out = out[offset];
-      out += stride;
-    }
-  }
-}
-    
-    static const int kCrToRedTable[256] = {
-  -179, -178, -177, -175, -174, -172, -171, -170, -168, -167, -165, -164,
-  -163, -161, -160, -158, -157, -156, -154, -153, -151, -150, -149, -147,
-  -146, -144, -143, -142, -140, -139, -137, -136, -135, -133, -132, -130,
-  -129, -128, -126, -125, -123, -122, -121, -119, -118, -116, -115, -114,
-  -112, -111, -109, -108, -107, -105, -104, -102, -101, -100,  -98,  -97,
-   -95,  -94,  -93,  -91,  -90,  -88,  -87,  -86,  -84,  -83,  -81,  -80,
-   -79,  -77,  -76,  -74,  -73,  -72,  -70,  -69,  -67,  -66,  -64,  -63,
-   -62,  -60,  -59,  -57,  -56,  -55,  -53,  -52,  -50,  -49,  -48,  -46,
-   -45,  -43,  -42,  -41,  -39,  -38,  -36,  -35,  -34,  -32,  -31,  -29,
-   -28,  -27,  -25,  -24,  -22,  -21,  -20,  -18,  -17,  -15,  -14,  -13,
-   -11,  -10,   -8,   -7,   -6,   -4,   -3,   -1,    0,    1,    3,    4,
-     6,    7,    8,   10,   11,   13,   14,   15,   17,   18,   20,   21,
-    22,   24,   25,   27,   28,   29,   31,   32,   34,   35,   36,   38,
-    39,   41,   42,   43,   45,   46,   48,   49,   50,   52,   53,   55,
-    56,   57,   59,   60,   62,   63,   64,   66,   67,   69,   70,   72,
-    73,   74,   76,   77,   79,   80,   81,   83,   84,   86,   87,   88,
-    90,   91,   93,   94,   95,   97,   98,  100,  101,  102,  104,  105,
-   107,  108,  109,  111,  112,  114,  115,  116,  118,  119,  121,  122,
-   123,  125,  126,  128,  129,  130,  132,  133,  135,  136,  137,  139,
-   140,  142,  143,  144,  146,  147,  149,  150,  151,  153,  154,  156,
-   157,  158,  160,  161,  163,  164,  165,  167,  168,  170,  171,  172,
-   174,  175,  177,  178
-};
-    
-    // Performs in-place floating point 8x8 DCT on block[0..63].
-// Note that the DCT used here is the DCT-2 with the first term multiplied by
-// 1/sqrt(2) and the result scaled by 1/2.
-void ComputeBlockDCTDouble(double block[64]);
-    
-    // Computes the DCT (Discrete Cosine Transform) of the 8x8 array in 'block',
-// scaled up by a factor of 16. The values in 'block' are laid out row-by-row
-// and the result is written to the same memory area.
-void ComputeBlockDCT(coeff_t* block);
-    
-    // kIDCTMatrix[8*x+u] = alpha(u)*cos((2*x+1)*u*M_PI/16)*sqrt(2), with fixed 13
-// bit precision, where alpha(0) = 1/sqrt(2) and alpha(u) = 1 for u > 0.
-// Some coefficients are off by +-1 to mimick libjpeg's behaviour.
-static const int kIDCTMatrix[kDCTBlockSize] = {
-  8192,  11363,  10703,   9633,   8192,   6437,   4433,   2260,
-  8192,   9633,   4433,  -2259,  -8192, -11362, -10704,  -6436,
-  8192,   6437,  -4433, -11362,  -8192,   2261,  10704,   9633,
-  8192,   2260, -10703,  -6436,   8192,   9633,  -4433, -11363,
-  8192,  -2260, -10703,   6436,   8192,  -9633,  -4433,  11363,
-  8192,  -6437,  -4433,  11362,  -8192,  -2261,  10704,  -9633,
-  8192,  -9633,   4433,   2259,  -8192,  11362, -10704,   6436,
-  8192, -11363,  10703,  -9633,   8192,  -6437,   4433,  -2260,
-};
-    
-    // Mimic libjpeg's heuristics to guess jpeg color space.
-// Requires that the jpg has 3 components.
-bool HasYCbCrColorSpace(const JPEGData& jpg) {
-  bool has_Adobe_marker = false;
-  uint8_t Adobe_transform = 0;
-  for (const std::string& app : jpg.app_data) {
-    if (static_cast<uint8_t>(app[0]) == 0xe0) {
-      return true;
-    } else if (static_cast<uint8_t>(app[0]) == 0xee && app.size() >= 15) {
-      has_Adobe_marker = true;
-      Adobe_transform = app[14];
-    }
-  }
-  if (has_Adobe_marker) {
-    return (Adobe_transform != 0);
-  }
-  const int cid0 = jpg.components[0].id;
-  const int cid1 = jpg.components[1].id;
-  const int cid2 = jpg.components[2].id;
-  return (cid0 != 'R' || cid1 != 'G' || cid2 != 'B');
-}
+        PyObject* NewMessageEnumsByName(const Descriptor* descriptor);
+PyObject* NewMessageEnumsSeq(const Descriptor* descriptor);
+PyObject* NewMessageEnumValuesByName(const Descriptor* descriptor);
     
     
-    {  return true;
-}
-    
-    #ifndef GUETZLI_JPEG_ERROR_H_
-#define GUETZLI_JPEG_ERROR_H_
-    
-    static const int kJpegHuffmanRootTableBits = 8;
-// Maximum huffman lookup table size.
-// According to zlib/examples/enough.c, 758 entries are always enough for
-// an alphabet of 257 symbols (256 + 1 special symbol for the all 1s code) and
-// max bit length 16 if the root table has 8 bits.
-static const int kJpegHuffmanLutSize = 758;
-    
-    // Preprocesses U and V channel for better results after downsampling.
-    
-    
-    {                if (ImGui::Button('OK', ImVec2(120,0))) { ImGui::CloseCurrentPopup(); }
-                ImGui::SetItemDefaultFocus();
-                ImGui::SameLine();
-                if (ImGui::Button('Cancel', ImVec2(120,0))) { ImGui::CloseCurrentPopup(); }
-                ImGui::EndPopup();
-            }
-    
-    struct FrameContext
-{
-    ID3D12CommandAllocator* CommandAllocator;
-    UINT64                  FenceValue;
-};
-    
-        // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them. 
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple. 
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Read 'misc/fonts/README.txt' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Roboto-Medium.ttf', 16.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Cousine-Regular.ttf', 15.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/DroidSans.ttf', 16.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/ProggyTiny.ttf', 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF('c:\\Windows\\Fonts\\ArialUni.ttf', 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
-    
-        // items_count:  Use -1 to ignore (you can call Begin later). Use INT_MAX if you don't know how many items you have (in which case the cursor won't be advanced in the final step).
-    // items_height: Use -1.0f to be calculated automatically on first step. Otherwise pass in the distance between your items, typically GetTextLineHeightWithSpacing() or GetFrameHeightWithSpacing().
-    // If you don't specify an items_height, you NEED to call Step(). If you specify items_height you may call the old Begin()/End() api directly, but prefer calling Step().
-    ImGuiListClipper(int items_count = -1, float items_height = -1.0f)  { Begin(items_count, items_height); } // NB: Begin() initialize every fields (as we allow user to call Begin/End multiple times on a same instance if they want).
-    ~ImGuiListClipper()                                                 { IM_ASSERT(ItemsCount == -1); }      // Assert if user forgot to call End() or Step() until false.
-    
-            const uint32_t w = ft_bitmap->bitmap.width;
-        const uint32_t h = ft_bitmap->bitmap.rows;
-        const uint8_t* src = ft_bitmap->bitmap.buffer;
-        const uint32_t src_pitch = ft_bitmap->bitmap.pitch;
-    
-        if (int size = s3eClipboardGetText(NULL, 0))
     {
-        if (g_ClipboardText)
-            delete[] g_ClipboardText;
-        g_ClipboardText = new char[size];
-        g_ClipboardText[0] = '\0';
-        s3eClipboardGetText(g_ClipboardText, size);
+    {}  // namespace python
+}  // namespace protobuf
+    
+    
+    {  ASSERT_TRUE(message.ParseFromString(data));
+  EXPECT_TRUE(message.has_any_value());
+  ASSERT_TRUE(message.any_value().UnpackTo(&submessage));
+  EXPECT_EQ(12345, submessage.int32_value());
+}
+    
+    class EnumGenerator : public SourceGeneratorBase {
+ public:
+  EnumGenerator(const EnumDescriptor* descriptor, const Options* options);
+  ~EnumGenerator();
     }
+    
+    
+    
+    #include <google/protobuf/compiler/code_generator.h>
+#include <google/protobuf/compiler/plugin.h>
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/descriptor.pb.h>
+#include <google/protobuf/io/printer.h>
+#include <google/protobuf/io/zero_copy_stream.h>
+#include <google/protobuf/wire_format.h>
+    
+    
+    
+    struct Options;
+    
+    void WriteMessageDocComment(io::Printer* printer, const Descriptor* message);
+void WriteFieldDocComment(io::Printer* printer, const FieldDescriptor* field);
+void WriteEnumDocComment(io::Printer* printer, const EnumDescriptor* enum_);
+void WriteEnumValueDocComment(io::Printer* printer,
+                              const EnumValueDescriptor* value);
+void WriteServiceDocComment(io::Printer* printer,
+                            const ServiceDescriptor* service);
+void WriteMethodDocComment(io::Printer* printer,
+                           const MethodDescriptor* method);
+    
+    class RepeatedImmutableLazyMessageFieldGenerator
+    : public RepeatedImmutableMessageFieldGenerator {
+ public:
+  explicit RepeatedImmutableLazyMessageFieldGenerator(
+      const FieldDescriptor* descriptor, int messageBitIndex,
+      int builderBitIndex, Context* context);
+  ~RepeatedImmutableLazyMessageFieldGenerator();
+    }
+    
+    #include <memory>
+    
+    
+    {
+    {}  // namespace testing
+}  // namespace grpc
+    
+    template<> inline
+dnnError_t dnnGroupsConvolutionCreateBackwardFilter<double>(
+    dnnPrimitive_t* pConvolution,
+    dnnPrimitiveAttributes_t attributes,
+    dnnAlgorithm_t algorithm,
+    size_t groups,
+    size_t dimension,
+    const size_t srcSize[],
+    const size_t dstSize[],
+    const size_t filterSize[],
+    const size_t convolutionStrides[],
+    const int inputOffset[],
+    const dnnBorder_t border_type)
+{
+    return dnnGroupsConvolutionCreateBackwardFilter_F64(
+        pConvolution,
+        attributes,
+        algorithm,
+        groups,
+        dimension,
+        srcSize,
+        dstSize,
+        filterSize,
+        convolutionStrides,
+        inputOffset,
+        border_type);
+}
+    
+                // The assumption here is that a sequence always start at 0 (no invaid mark at the beginning),
+            // and ends at the first invalid mask. 
+            // Therefore, no need to check NDMask again.
+            // And the sequences has been resized to match the number of sequences and the length of each sequence in the Value object.
+    
+    // replace the old node with the current node, assuming the old node is a leaf node
+// need to update those nodes who use oldNode as their child
+// TODO: Can this be called with a node that's already part of the network? This is currently allowed, but should it?
+// BUGBUG: Seems ReplaceNode() also updates node groups. Why doesn't this function?
+// BUGBUG: What if newNode is the one referenced by oldNodeName?
+// BUGBUG: Or what if an unrelated node of the same name exists?
+void ComputationNetwork::ReplaceLeafNode(wstring oldNodeName, ComputationNodeBasePtr newNode)
+{
+    InvalidateCompiledNetwork();
+    }
+    
+    void GranularGPUDataTransferer::WaitForSyncPointOnFetchStreamAsync()
+{
+    PrepareDevice(m_deviceId);
+    cudaStreamWaitEvent(GetFetchStream(), m_syncEvent, 0 /*flags 'must be 0'*/) || 'cudaStreamWaitEvent failed';
+}
+    
+    // Compares two ASCII strings ignoring the case.
+// TODO: Should switch to boost, boost::iequal should be used instead.
+// TODO: we already have EqualCI() in Basics.h which does the same thing.
+template<class TElement>
+inline bool AreEqualIgnoreCase(
+    const std::basic_string<TElement, char_traits<TElement>, allocator<TElement>>& s1,
+    const std::basic_string<TElement, char_traits<TElement>, allocator<TElement> >& s2)
+{
+    if (s1.size() != s2.size())
+    {
+        return false;
+    }
+    }
+    
+    class latticepair : public std::pair<msra::lattices::lattice, msra::lattices::lattice>
+{
+public:
+    // NOTE: we don't check numerator lattice now
+    size_t getnumframes() const
+    {
+        return second.getnumframes();
+    }
+    size_t getnumnodes() const
+    {
+        return second.getnumnodes();
+    }
+    size_t getnumedges() const
+    {
+        return second.getnumedges();
+    }
+    std::wstring getkey() const
+    {
+        return second.getkey();
+    }
+};
+    
+    YGFloatOptional YGNode::getLeadingPaddingAndBorder(
+    const YGFlexDirection& axis,
+    const float& widthSize) const {
+  return getLeadingPadding(axis, widthSize) +
+      YGFloatOptional(getLeadingBorder(axis));
+}
+    
+      // Other methods
+  YGValue marginLeadingValue(const YGFlexDirection axis) const;
+  YGValue marginTrailingValue(const YGFlexDirection axis) const;
+  YGValue resolveFlexBasisPtr() const;
+  void resolveDimension();
+  YGDirection resolveDirection(const YGDirection ownerDirection);
+  void clearChildren();
+  /// Replaces the occurrences of oldChild with newChild
+  void replaceChild(YGNodeRef oldChild, YGNodeRef newChild);
+  void replaceChild(YGNodeRef child, uint32_t index);
+  void insertChild(YGNodeRef child, uint32_t index);
+  /// Removes the first occurrence of child
+  bool removeChild(YGNodeRef child);
+  void removeChild(uint32_t index);
+    
+    #include <yoga/Yoga.h>
+    
+    #include <dlfcn.h>
+#include <unwind.h>
+    
+    TEST_F(YogaTest_HadOverflowTests, spacing_overflow_no_wrap_and_no_flex_children) {
+  const YGNodeRef child0 = YGNodeNewWithConfig(config);
+  YGNodeStyleSetWidth(child0, 80);
+  YGNodeStyleSetHeight(child0, 40);
+  YGNodeStyleSetMargin(child0, YGEdgeTop, 10);
+  YGNodeStyleSetMargin(child0, YGEdgeBottom, 10);
+  YGNodeInsertChild(root, child0, 0);
+  const YGNodeRef child1 = YGNodeNewWithConfig(config);
+  YGNodeStyleSetWidth(child1, 80);
+  YGNodeStyleSetHeight(child1, 40);
+  YGNodeStyleSetMargin(child1, YGEdgeBottom, 5);
+  YGNodeInsertChild(root, child1, 1);
+    }
+    
+        method(setFlex);
+    method(setFlexBasis);
+    method(setFlexBasisPercent);
+    method(setFlexGrow);
+    method(setFlexShrink);
+    
+      void reset(T* other = NULL) {
+    T* old = (T*)pthread_getspecific(m_key);
+    if (old != other) {
+      FBASSERT(m_cleanup);
+      m_cleanup(old);
+      pthread_setspecific(m_key, other);
+    }
+  }
