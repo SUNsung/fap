66fba274a14c0c2789c8eb15f85cc59dfb62bfd2
SUNsung <weatherius@gmail.com>
2018-06-25 11:25:49 +0300

    Rewrite [32AwfiJCQMpcU32SouDb18HJ4I32tFsMP8HJB2632jk.rb] File in: 2018-06-25 11:25:48

--- ruby/32AwfiJCQMpcU32SouDb18HJ4I32tFsMP8HJB2632jk.rb
+++ ruby/32AwfiJCQMpcU32SouDb18HJ4I32tFsMP8HJB2632jk.rb
@@ -1,64 +1,74 @@
-def bottle_receipt_path(bottle_file)
-  Utils.popen_read('/usr/bin/tar', '-tzf', bottle_file, '*/*/INSTALL_RECEIPT.json').chomp
-end
+
+        
+              def remember_me_is_active?(resource)
+        return false unless resource.respond_to?(:remember_me)
+        scope = Devise::Mapping.find_scope!(resource)
+        _, token, generated_at = cookies.signed[remember_key(resource, scope)]
+        resource.remember_me?(token, generated_at)
+      end
     
-    module Homebrew
-  def build_env_keys(env)
-    %w[
-      CC CXX LD OBJC OBJCXX
-      HOMEBREW_CC HOMEBREW_CXX
-      CFLAGS CXXFLAGS CPPFLAGS LDFLAGS SDKROOT MAKEFLAGS
-      CMAKE_PREFIX_PATH CMAKE_INCLUDE_PATH CMAKE_LIBRARY_PATH CMAKE_FRAMEWORK_PATH
-      MACOSX_DEPLOYMENT_TARGET PKG_CONFIG_PATH PKG_CONFIG_LIBDIR
-      HOMEBREW_DEBUG HOMEBREW_MAKE_JOBS HOMEBREW_VERBOSE
-      HOMEBREW_SVN HOMEBREW_GIT
-      HOMEBREW_SDKROOT HOMEBREW_BUILD_FROM_SOURCE
-      MAKE GIT CPP
-      ACLOCAL_PATH PATH CPATH].select { |key| env.key?(key) }
-  end
+    module Devise
+  module Controllers
+    # Create url helpers to be used with resource/scope configuration. Acts as
+    # proxies to the generated routes created by devise.
+    # Resource param can be a string or symbol, a class, or an instance object.
+    # Example using a :user resource:
+    #
+    #   new_session_path(:user)      => new_user_session_path
+    #   session_path(:user)          => user_session_path
+    #   destroy_session_path(:user)  => destroy_user_session_path
+    #
+    #   new_password_path(:user)     => new_user_password_path
+    #   password_path(:user)         => user_password_path
+    #   edit_password_path(:user)    => edit_user_password_path
+    #
+    #   new_confirmation_path(:user) => new_user_confirmation_path
+    #   confirmation_path(:user)     => user_confirmation_path
+    #
+    # Those helpers are included by default to ActionController::Base.
+    #
+    # In case you want to add such helpers to another class, you can do
+    # that as long as this new class includes both url_helpers and
+    # mounted_helpers. Example:
+    #
+    #     include Rails.application.routes.url_helpers
+    #     include Rails.application.routes.mounted_helpers
+    #
+    module UrlHelpers
+      def self.remove_helpers!
+        self.instance_methods.map(&:to_s).grep(/_(url|path)$/).each do |method|
+          remove_method method
+        end
+      end
     
-      # True if a {Formula} is being built in 32-bit/x86 mode.
-  # This is needed for some use-cases though we prefer to build Universal
-  # when a 32-bit version is needed.
-  def build_32_bit?
-    include?('32-bit') && option_defined?('32-bit')
-  end
+          next if path.symlink? || path.directory?
     
-        def self.cleanup_lockfiles
-      return unless HOMEBREW_CACHE_FORMULA.directory?
-      candidates = HOMEBREW_CACHE_FORMULA.children
-      lockfiles  = candidates.select { |f| f.file? && f.extname == '.brewing' }
-      lockfiles.each do |file|
-        next unless file.readable?
-        file.open.flock(File::LOCK_EX | File::LOCK_NB) && file.unlink
+        uses = formulae.select do |f|
+      used_formulae.all? do |ff|
+        begin
+          deps = f.runtime_dependencies if only_installed_arg
+          deps ||= if recursive
+            recursive_includes(Dependency, f, includes, ignores)
+          else
+            reject_ignores(f.deps, ignores, includes)
+          end
+    
+        def yielder
+      if instance_variable_defined?(:@satisfied)
+        @satisfied
+      elsif @options[:build_env]
+        require 'extend/ENV'
+        ENV.with_build_environment { yield @proc }
+      else
+        yield @proc
       end
     end
-    
-      def internal_development_commands
-    find_internal_commands HOMEBREW_LIBRARY_PATH/'dev-cmd'
   end
     
-    module Homebrew
-  def doctor
-    checks = Diagnostic::Checks.new
+        def default_tasks
+      %w{install}
+    end
     
-    module Homebrew
-  def update_report
-    install_core_tap_if_necessary
-    
-          export JAVA_HOME='$(/usr/libexec/java_home)'
-      export AWS_ACCESS_KEY='<Your AWS Access ID>'
-      export AWS_SECRET_KEY='<Your AWS Secret Key>'
-      export #{home_name}='#{home_value}'
-    EOS
-  end
-end
-
-    
-            # Registers a SIGINT handler. This typically is called from {busy}.
-        # Callbacks are only registered once, so calling this multiple times
-        # with the same callback has no consequence.
-        def register(sig_callback)
-          @@mutex.synchronize do
-            registered << sig_callback
-            registered.uniq!
+        it 'sets the default env' do
+      expect(config.default_env).to eq default_env
+    end
