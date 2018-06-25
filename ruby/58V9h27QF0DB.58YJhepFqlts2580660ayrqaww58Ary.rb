9bf6a2a5c0faa232f5c1220dabe304f215f9d1c1
SUNsung <weatherius@gmail.com>
2018-06-25 05:37:26 +0300

    Rewrite [58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb] File in: 2018-06-25 05:37:25

--- ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
+++ ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
@@ -1,80 +1,86 @@
 
         
-                if params[:add_to_search_list]
-          keychains = Action.sh('security list-keychains -d user').shellsplit
-          keychains << File.expand_path(keychain_path)
-          commands << Fastlane::Actions.sh('security list-keychains -s #{keychains.shelljoin}', log: false)
-        end
-    
-          it 'get SVN build number' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            get_build_number_repository
-        end').runner.execute(:test)
-    
-              results
-        end
-    
-    module Vagrant
-  module Util
-    class IO
-      # The chunk size for reading from subprocess IO.
-      READ_CHUNK_SIZE = 4096
-    
-          # This inserts a block with the given key and value.
-      #
-      # @param [String] key
-      # @param [String] value
-      def insert(key, value)
-        # Insert the new block into the value
-        new_block = <<BLOCK
-# VAGRANT-BEGIN: #{key}
-#{value.strip}
-# VAGRANT-END: #{key}
-BLOCK
-    
-      if options.respond_to? 'keys'
-    options.each do |k,v|
-      unless v.nil?
-        v = v.join ',' if v.respond_to? 'join'
-        v = v.to_json if v.respond_to? 'keys'
-        output += ' data-#{k.sub'_','-'}='#{v}''
+            def matching_domain
+      if domain.nil?
+        Account.where(domain: nil)
+      else
+        Account.where(Account.arel_table[:domain].lower.eq domain.to_s.downcase)
       end
     end
-  elsif options.respond_to? 'join'
-    output += ' data-value='#{config[key].join(',')}''
-  else
-    output += ' data-value='#{config[key]}''
   end
-  output += '></#{tag}>'
 end
+
     
-            Dir.chdir(includes_dir) do
-          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
-          if choices.include?(file)
-            source = File.read(file)
-            partial = Liquid::Template.parse(source)
-            context.stack do
-              rtn = rtn + partial.render(context)
-            end
-          else
-            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
-          end
-        end
+      attributes :id, :type, :name, :updated
+    
+        existence_maps.each do |group|
+      existing_one = group.key(true)
+    
+      private
+    
+      def short_name
+    object.site_title
+  end
+    
+      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/follow
+  def follow
+    f = Follow.last
+    NotificationMailer.follow(f.target_account, Notification.find_by(activity: f))
+  end
+    
+        on :save_person_after_webfinger do |person|
+      # find existing person or create a new one
+      person_entity = Person.find_by(diaspora_handle: person.diaspora_id) ||
+        Person.new(diaspora_handle: person.diaspora_id, guid: person.guid,
+                   serialized_public_key: person.exported_key, pod: Pod.find_or_create_by(url: person.url))
+    
+          # Inspired by https://github.com/nov/openid_connect_sample/blob/master/app/controllers/connect/clients_controller.rb#L24
+      def create
+        registrar = OpenIDConnect::Client::Registrar.new(request.url, params)
+        client = Api::OpenidConnect::OAuthApplication.register! registrar
+        render json: client.as_json(root: false)
       end
-      rtn
-    end
-  end
     
-      # Removes trailing forward slash from a string for easily appending url segments
-  def strip_slash(input)
-    if input =~ /(.+)\/$|^\/$/
-      input = $1
+        def after(task, post_task, *args, &block)
+      Rake::Task.define_task(post_task, *args, &block) if block_given?
+      task = Rake::Task[task]
+      task.enhance do
+        post = Rake.application.lookup(post_task, task.scope)
+        raise ArgumentError, 'Task #{post_task.inspect} not found' unless post
+        post.invoke
+      end
     end
-    input
-  end
     
-        # Determines whether the instance responds to this attribute. Used to prevent
-    # calculations on fields we won't even store.
-    def instance_respond_to?(attr)
-      instance.respond_to?(:'#{name}_#{attr}')
-    end
+    # IMPORTANT: The Capistrano::Plugin system is not yet considered a stable,
+# public API, and is subject to change without notice. Eventually it will be
+# officially documented and supported, but for now, use it at your own risk.
+#
+# Base class for Capistrano plugins. Makes building a Capistrano plugin as easy
+# as writing a `Capistrano::Plugin` subclass and overriding any or all of its
+# three template methods:
+#
+# * set_defaults
+# * register_hooks
+# * define_tasks
+#
+# Within the plugin you can use any methods of the Rake or Capistrano DSLs, like
+# `fetch`, `invoke`, etc. In cases when you need to use SSHKit's backend outside
+# of an `on` block, use the `backend` convenience method. E.g. `backend.test`,
+# `backend.execute`, or `backend.capture`.
+#
+# Package up and distribute your plugin class as a gem and you're good to go!
+#
+# To use a plugin, all a user has to do is install it in the Capfile, like this:
+#
+#   # Capfile
+#   require 'capistrano/superfancy'
+#   install_plugin Capistrano::Superfancy
+#
+# Or, to install the plugin without its hooks:
+#
+#   # Capfile
+#   require 'capistrano/superfancy'
+#   install_plugin Capistrano::Superfancy, load_hooks: false
+#
+class Capistrano::Plugin < Rake::TaskLib
+  include Capistrano::DSL
