6caaad53f7b04a403f575f31b391aa03215db1f5
SUNsung <weatherius@gmail.com>
2018-06-25 19:10:57 +0300

    Rewrite [51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp] File in: 2018-06-25 19:10:56

--- cccccc/51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp
+++ cccccc/51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp
@@ -1,135 +1,260 @@
 
         
-        #ifdef JSON_USE_CPPTL
-Value& Value::operator[](const CppTL::ConstString& key) {
-  return resolveReference(key.c_str(), key.end_c_str());
-}
-Value const& Value::operator[](CppTL::ConstString const& key) const
+        /// A utility for finding dead-end blocks.
+///
+/// Dead-end blocks are blocks from which there is no path to the function exit
+/// (either return or throw). These are blocks which end with an unreachable
+/// instruction and blocks from which all paths end in 'unreachable' blocks.
+/// This utility is needed to determine if the a value definition can have a
+/// lack of users ignored along a specific path.
+class DeadEndBlocks {
+  llvm::SetVector<const SILBasicBlock *> ReachableBlocks;
+  const SILFunction *F;
+  bool isComputed = false;
+    }
+    
+    namespace swift {
+class DependencyTracker;
+class ModuleDecl;
+class SourceFile;
+    }
+    
+      ArrayRef<StringRef> getTags() const {
+    return llvm::makeArrayRef(Parts.Tags.begin(), Parts.Tags.end());
+  }
+    
+    #endif // SWIFT_SYNTAX_REFERENCES_H
+
+    
+    #ifndef SWIFT_RUNTIME_ONCE_H
+#define SWIFT_RUNTIME_ONCE_H
+    
+    public:
+  enum Action {Skip, Abort, Continue};
+    
+    namespace Lowering {
+    }
+    
+      /// Assignment operator.
+  LSBase &operator=(const LSBase &RHS) {
+    Base = RHS.Base;
+    Kind = RHS.Kind;
+    Path = RHS.Path;
+    return *this;
+  }
+    
+    #include 'swift/AST/DiagnosticConsumer.h'
+#include 'swift/Migrator/FixitFilter.h'
+#include 'swift/Migrator/Migrator.h'
+#include 'swift/Migrator/Replacement.h'
+#include 'clang/Rewrite/Core/RewriteBuffer.h'
+#include 'llvm/ADT/DenseSet.h'
+    
+            // TODO: encapsulate it into a destructor? Note: Cannot throw exceptions in destructor.
+        void DoneWithCurrentSubMinibatch(size_t iSubminibatch)
+        {
+            // accumulate gradient here
+            for (auto x : m_cachedGradient)
+            {
+                wstring nodename = x.first;
+                if (m_LearnableNodePtr.find(nodename) == m_LearnableNodePtr.end())
+                {
+                    RuntimeError('ERROR: in DoneWithCurrentSubMinibatch: node %ls not found in LeanrableNode', nodename.c_str());
+                }
+                shared_ptr<ComputationNode<ElemType>> pNode = m_LearnableNodePtr[nodename];
+                m_cachedGradient.GetInputMatrix<ElemType>(nodename) += pNode->Gradient();
+                pNode->Gradient().SetValue(0);
+            }
+            // accumulate criterion value
+            if (!m_netCriterionNodes.empty())
+            {
+                Matrix<ElemType>::AddElementToElement(m_netCriterionNodes[0]->Value(), 0, 0,
+                                                      *m_netCriterionAccumulator, 0, 0);
+                m_netCriterionNodes[0]->Value().SetValue(0);
+            }
+            // accumulate evaluation value
+            for (size_t i = 0; i < m_netEvaluationNodes.size(); i++)
+            {
+                Matrix<ElemType>::AddElementToElement(m_netEvaluationNodes[i]->Value(), 0, 0,
+                                                      *m_netEvaluationAccumulator, 0, i);
+                m_netEvaluationNodes[i]->Value().SetValue(0);
+            }
+    }
+    
+    // Inserts a newNode such that the inputNodeName serves as the input to the newNode
+// Prior to this call, inputNodeName should be set as the input to newNode.
+void ComputationNetwork::InsertNode(wstring inputNodeName, ComputationNodeBasePtr newNode, const std::set<std::wstring>& newNodeTags)
 {
-  Value const* found = find(key.c_str(), key.end_c_str());
-  if (!found) return nullRef;
-  return *found;
+    newNode->Validate(false);
+    }
+    
+    
+    {
+    {        // handle the alternate way of specifying nodes, the array of nodes method
+        //                       parameter name    node-group tag
+        CheckOutputNodes(script, 'featureNodes'  , L'feature'   );
+        CheckOutputNodes(script, 'labelNodes'    , L'label'     );
+        CheckOutputNodes(script, 'criterionNodes', L'criterion' );
+        CheckOutputNodes(script, 'evalNodes'     , L'evaluation');
+        CheckOutputNodes(script, 'outputNodes'   , L'output'    );
+        // legacy name:
+        CheckOutputNodes(script, 'criteriaNodes' , L'criterion' );
+    }
+};
+    
+    // GetWriter - get a reader type from the DLL
+// The F version gets the 'float' version, and D gets 'double'.
+extern 'C' DATAWRITER_API void GetWriterF(IDataWriter** pwriter);
+extern 'C' DATAWRITER_API void GetWriterD(IDataWriter** pwriter);
+    
+        template<unsigned int N>
+    static const char* Timestamp(char(&buf)[N])
+    {
+        std::time_t tt = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        if (!std::strftime(buf, _countof(buf), '%m/%d/%Y %H:%M:%S', std::localtime(&tt)))
+            LogicError('Timestamp: Buffer too small.');
+        return buf;
+    }
+    
+    // ---------------------------------------------------------------------------
+// array_ref -- wraps a C pointer to an array together with its size.
+//
+// Called _ref because this is a reference to the array rather than the array
+// itself (since it wraps a pointer). No need to pass an array_ref by reference.
+//
+// operator[] checks index bounds in Debug builds. size() is provided such
+// that this class can be substituted for STL vector in many cases.
+// ---------------------------------------------------------------------------
+    
+    /**
+ * @brief Access the internal storage of the Decorator parser.
+ *
+ * The decoration set is a map of column name to value. It contains the opaque
+ * set of decoration point results.
+ *
+ * Decorations are applied to log items before they are sent to the downstream
+ * logging APIs: logString, logSnapshot, etc.
+ *
+ * @param results the output parameter to write decorations.
+ */
+void getDecorations(std::map<std::string, std::string>& results);
+    
+    class StatusTests : public testing::Test {};
+    
+    TEST_F(TimeTests, test_asciitime) {
+  const size_t epoch = 1491518994;
+  const std::string expected = 'Thu Apr  6 22:49:54 2017 UTC';
+    }
+    
+      void stop() override;
+    
+    #pragma once
+    
+    
+    {  return Status(0, 'OK');
 }
-#endif
+    
+    YGValue YGNode::resolveFlexBasisPtr() const {
+  YGValue flexBasis = style_.flexBasis;
+  if (flexBasis.unit != YGUnitAuto && flexBasis.unit != YGUnitUndefined) {
+    return flexBasis;
+  }
+  if (!style_.flex.isUndefined() && style_.flex.getValue() > 0.0f) {
+    return config_->useWebDefaults ? YGValueAuto : YGValueZero;
+  }
+  return YGValueAuto;
+}
+    
+      // Other methods
+  YGValue marginLeadingValue(const YGFlexDirection axis) const;
+  YGValue marginTrailingValue(const YGFlexDirection axis) const;
+  YGValue resolveFlexBasisPtr() const;
+  void resolveDimension();
+  YGDirection resolveDirection(const YGDirection ownerDirection);
+  void clearChildren();
+  /// Replaces the occurrences of oldChild with newChild
+  void replaceChild(YGNodeRef oldChild, YGNodeRef newChild);
+  void replaceChild(YGNodeRef child, uint32_t index);
+  void insertChild(YGNodeRef child, uint32_t index);
+  /// Removes the first occurrence of child
+  bool removeChild(YGNodeRef child);
+  void removeChild(uint32_t index);
     
     
+    {    void toJS(nbind::cbOutput expose) const
     {
-    {
-    {
-    {}  // namespace csharp
-}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
+        expose(unit, value);
+    }
+};
 
     
-    void RepeatedPrimitiveFieldGenerator::GenerateFreezingCode(io::Printer* printer) {
+      if (state->stackTrace.size() == state->stackTrace.capacity()) {
+    return _URC_END_OF_STACK;
+  }
+    
+    TEST_F(YogaTest_HadOverflowTests, spacing_overflow_no_wrap_and_no_flex_children) {
+  const YGNodeRef child0 = YGNodeNewWithConfig(config);
+  YGNodeStyleSetWidth(child0, 80);
+  YGNodeStyleSetHeight(child0, 40);
+  YGNodeStyleSetMargin(child0, YGEdgeTop, 10);
+  YGNodeStyleSetMargin(child0, YGEdgeBottom, 10);
+  YGNodeInsertChild(root, child0, 0);
+  const YGNodeRef child1 = YGNodeNewWithConfig(config);
+  YGNodeStyleSetWidth(child1, 80);
+  YGNodeStyleSetHeight(child1, 40);
+  YGNodeStyleSetMargin(child1, YGEdgeBottom, 5);
+  YGNodeInsertChild(root, child1, 1);
+    }
+    
+    Config::~Config(void)
+{
+    YGConfigFree(m_config);
 }
     
-    #endif  // GOOGLE_PROTOBUF_COMPILER_CSHARP_REPEATED_PRIMITIVE_FIELD_H__
-    
-    namespace google {
-namespace protobuf {
-namespace compiler {
-namespace csharp {
-    }
-    }
-    }
-    }
+        double width;
+    double height;
     
     
-    {
-    {
-    {
-    {}  // namespace csharp
-}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-    
-    
-    {
-    {
-    {
-    {
-    {}  // namespace
-}  // namespace java
-}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
+    {    method(getComputedMargin);
+    method(getComputedBorder);
+    method(getComputedPadding);
+}
 
     
-    // CodeGenerator implementation which generates Java code.  If you create your
-// own protocol compiler binary and you want it to support Java output, you
-// can do so by registering an instance of this CodeGenerator with the
-// CommandLineInterface in your main() function.
-class LIBPROTOC_EXPORT JavaGenerator : public CodeGenerator {
- public:
-  JavaGenerator();
-  ~JavaGenerator();
+      RefPtr<T>& operator=(const RefPtr<T>& ref) {
+    if (m_ptr != ref.m_ptr) {
+      unrefIfNecessary(m_ptr);
+      m_ptr = ref.m_ptr;
+      refIfNecessary(m_ptr);
     }
+    return *this;
+  }
     
-    #include <google/protobuf/compiler/java/java_context.h>
-#include <google/protobuf/compiler/java/java_enum_field.h>
-#include <google/protobuf/compiler/java/java_extension.h>
-#include <google/protobuf/compiler/java/java_extension_lite.h>
-#include <google/protobuf/compiler/java/java_field.h>
-#include <google/protobuf/compiler/java/java_helpers.h>
-#include <google/protobuf/compiler/java/java_message.h>
-#include <google/protobuf/compiler/java/java_message_lite.h>
-#include <google/protobuf/compiler/java/java_service.h>
-    
-      explicit WorkloadStats(State guardedState);
-  ~WorkloadStats();
-    
-    int64_t Timer::GetRusageMicros(Type t, Who who) {
-  assert(t != WallTime);
+    /**
+ * A thread-local object is a 'global' object within a thread. This is useful
+ * for writing apartment-threaded code, where nothing is actullay shared
+ * between different threads (hence no locking) but those variables are not
+ * on stack in local scope. To use it, just do something like this,
+ *
+ *   ThreadLocal<MyClass> static_object;
+ *     static_object->data_ = ...;
+ *     static_object->doSomething();
+ *
+ *   ThreadLocal<int> static_number;
+ *     int value = *static_number;
+ *
+ * So, syntax-wise it's similar to pointers. T can be primitive types, and if
+ * it's a class, there has to be a default constructor.
+ */
+template<typename T>
+class ThreadLocal {
+public:
+  /**
+   * Constructor that has to be called from a thread-neutral place.
+   */
+  ThreadLocal() :
+    m_key(0),
+    m_cleanup(OnThreadExit) {
+    initialize();
+  }
     }
-    
-    struct SSATmp;
-struct Type;
-    
-      mpz_init(gmpReturn);
-  mpz_neg(gmpReturn, gmpData);
-    
-    
-static uint64_t RotateRight(uint64_t value,
-                            unsigned int rotate,
-                            unsigned int width) {
-  assert(width <= 64);
-  rotate &= 63;
-  return ((value & ((1UL << rotate) - 1UL)) << (width - rotate)) |
-         (value >> rotate);
-}
-    
-    
-    {
-    {}  // namespace log
-}  // namespace leveldb
-    
-    TEST(VersionEditTest, EncodeDecode) {
-  static const uint64_t kBig = 1ull << 50;
-    }
-    
-    // Memtables and sstables that make the DB representation contain
-// (userkey,seq,type) => uservalue entries.  DBIter
-// combines multiple entries for the same userkey found in the DB
-// representation into a single entry while accounting for sequence
-// numbers, deletion markers, overwrites, etc.
-class DBIter: public Iterator {
- public:
-  // Which direction is the iterator currently moving?
-  // (1) When moving forward, the internal iterator is positioned at
-  //     the exact entry that yields this->key(), this->value()
-  // (2) When moving backwards, the internal iterator is positioned
-  //     just before all entries whose user key == this->key().
-  enum Direction {
-    kForward,
-    kReverse
-  };
-    }
-    
-    
-    {  // When limit user key is prefix of start user key
-  ASSERT_EQ(IKey('foobar', 100, kTypeValue),
-            Shorten(IKey('foobar', 100, kTypeValue),
-                    IKey('foo', 200, kTypeValue)));
-}
