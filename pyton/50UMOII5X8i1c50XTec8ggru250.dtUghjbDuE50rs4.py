3bf5427cc7ad3b3fec69d4a298c9c5cb6e235733
SUNsung <weatherius@gmail.com>
2018-06-25 20:54:39 +0300

    Rewrite [50UMOII5X8i1c50XTec8ggru250.dtUghjbDuE50rs4.py] File in: 2018-06-25 20:54:39

--- pyton/50UMOII5X8i1c50XTec8ggru250.dtUghjbDuE50rs4.py
+++ pyton/50UMOII5X8i1c50XTec8ggru250.dtUghjbDuE50rs4.py
@@ -1,121 +1,110 @@
 
         
-        index_title = 0
-index_desc = 1
-index_auth = 2
-index_https = 3
-index_cors = 4
-index_link = 5
-num_segments = 6
+            entry = entry_template.replace('@TIMESTAMP@', timestamp)
+    entry = entry.replace('@VERSION@', v)
+    entries.append(entry)
     
-    FLAGS = flags.FLAGS
-    
-        for idx, (loader, srcobj, triple) in enumerate(attempts):
-        if isinstance(srcobj, Flask):
-            src_info = 'application '%s'' % srcobj.import_name
-        elif isinstance(srcobj, Blueprint):
-            src_info = 'blueprint '%s' (%s)' % (srcobj.name,
-                                                srcobj.import_name)
-        else:
-            src_info = repr(srcobj)
-    
-            # if this is not an ip and app is mounted at the root, allow subdomain
-        # matching by adding a '.' prefix
-        if self.get_cookie_path(app) == '/' and not ip:
-            rv = '.' + rv
-    
-        def dispatch_request(self):
-        '''Subclasses have to override this method to implement the
-        actual view function code.  This method is called with all
-        the arguments from the URL rule.
-        '''
-        raise NotImplementedError()
-    
-        with open(filename) as f:
-        contents = re.sub(
-            r'^(\s*%s\s*=\s*')(.+?)(')' % pattern,
-            inject_version, f.read(),
-            flags=re.DOTALL | re.MULTILINE
-        )
-    
-        parser.add_argument('--env',
-                        nargs=2,
-                        metavar=('KEY', 'VALUE'),
-                        action='append',
-                        help='environment variable to pass')
-    
-        'hostport': re.compile(
-        r'''^
-            ((?:                        # We want to match:
-                [^:\[\]]                # (a non-range character
-                |                       # ...or...
-                \[[^\]]*\]              # a complete bracketed expression)
-            )*)                         # repeated as many times as possible
-            :([0-9]+)                   # followed by a port number
-            $
-        ''', re.X
-    ),
+        def test_no_duplicates(self):
+        ies = gen_extractors()
+        for tc in gettestcases(include_onlymatching=True):
+            url = tc['url']
+            for ie in ies:
+                if type(ie).__name__ in ('GenericIE', tc['name'] + 'IE'):
+                    self.assertTrue(ie.suitable(url), '%s should match URL %r' % (type(ie).__name__, url))
+                else:
+                    self.assertFalse(
+                        ie.suitable(url),
+                        '%s should not match URL %r . That URL belongs to %s.' % (type(ie).__name__, url, tc['name']))
     
     
-class TerminalModule(TerminalBase):
-    
-        def test_max_delay_none(self):
-        strategy = _exponential_backoff(retries=7, delay=1, backoff=2, max_delay=None)
-        result = list(strategy())
-        self.assertEquals(result, [1, 2, 4, 8, 16, 32, 64])
-    
-        def __init__(self, blackboard):
-        self.blackboard = blackboard
-    
-        message_center.update()
-    
-        def test_frozen_pool(self):
-        with ObjectPool(self.sample_queue) as pool:
-            self.assertEqual(pool, 'first')
-            self.assertEqual(pool, 'first')
-        self.assertTrue(self.sample_queue.get() == 'second')
-        self.assertFalse(self.sample_queue.empty())
-        self.assertTrue(self.sample_queue.get() == 'first')
-        self.assertTrue(self.sample_queue.empty())
-    
-        def test_2nd_am_station_after_scan(self):
-        self.radio.scan()
-        station = self.radio.state.stations[self.radio.state.pos]
-        expected_station = '1380'
-        self.assertEqual(station, expected_station)
+class TestCache(unittest.TestCase):
+    def setUp(self):
+        TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+        TESTDATA_DIR = os.path.join(TEST_DIR, 'testdata')
+        _mkdir(TESTDATA_DIR)
+        self.test_dir = os.path.join(TESTDATA_DIR, 'cache_test')
+        self.tearDown()
     
     
-    
-            # simple test to validate param value
-        if param in self._static_method_choices.keys():
-            self.param = param
-        else:
-            raise ValueError('Invalid Value for Param: {0}'.format(param))
-    
-    # Test the generator
-count_to_two = lambda: count_to(2)
-count_to_five = lambda: count_to(5)
-    
-        def tearDown(self):
-        if not self._bProblem:
-            print('Tearing down')
-            time.sleep(0.1)
-            self._tm.publishReport()
-        else:
-            print('Test not executed. No tear down required.')
+for testfile in os.listdir(TEST_DIR):
+    _make_testfunc(testfile)
     
     
-class Transaction(object):
-    '''A transaction guard.
+class TestUnicodeLiterals(unittest.TestCase):
+    def test_all_files(self):
+        for dirpath, dirnames, filenames in os.walk(rootDir):
+            for ignore_dir in IGNORED_DIRS:
+                if ignore_dir in dirnames:
+                    # If we remove the directory from dirnames os.walk won't
+                    # recurse into it
+                    dirnames.remove(ignore_dir)
+            for basename in filenames:
+                if not basename.endswith('.py'):
+                    continue
+                if basename in IGNORED_FILES:
+                    continue
     
-        context['show_on_github_url'] = show_url
-    context['edit_on_github_url'] = edit_url
+            title = metadata['Title']
+        description = metadata.get('Description')
+        duration = float_or_none(metadata.get('Duration'))
+        timestamp = parse_iso8601(metadata.get('DateCreated'))
     
     
-def volume_mute(hass):
-    '''Press the keyboard button for muting volume.'''
-    hass.services.call(DOMAIN, SERVICE_VOLUME_MUTE)
+def write_with_harness(codefile, sourcefile, start_linenum, linebuffer):
+    '''write output with additional lines to make code likely compilable'''
+    # add commonly used headers, so that lines can likely compile.
+    # This is work in progress, the main issue remains handling class
+    # declarations in in-function code differently
+    with io.open(codefile, 'w') as code_filehandle:
+        code_filehandle.write('''\
+#include<stdio.h>      // by md-split
+#include<stdlib.h>     // by md-split
+#include<tuple>        // by md-split
+#include<utility>      // by md-split
+#include<limits>       // by md-split
+#include<functional>   // by md-split
+#include<string>       // by md-split
+#include<map>          // by md-split
+#include<iostream>     // by md-split
+#include<vector>       // by md-split
+#include<algorithm>    // by md-split
+#include<memory>       // by md-split
+using namespace std;   // by md-split
+// %s : %s
+''' % (sourcefile, start_linenum))
+        # TODO: if not toplevel code, wrap inside class
+        for codeline in linebuffer:
+            code_filehandle.write(codeline)
     
-    import homeassistant.config as config_util
     
-            self.assertIsNotNone(result)
+    {  # An else clause should be on the same line as the preceding closing brace.
+  if Match(r'\s*else\b\s*(?:if\b|\{|$)', line):
+    prevline = GetPreviousNonBlankLine(clean_lines, linenum)[0]
+    if Match(r'\s*}\s*$', prevline):
+      error(filename, linenum, 'whitespace/newline', 4,
+            'An else should appear on the same line as the preceding }')
+    
+                if point.name == 'p':
+                link = point.find('a')
+                if link is not None:
+                    link = clean_pdf_link(link.attrs['href'])
+                    ext = get_extension(link)
+                    print(ext)
+                    if not ext in forbidden_extensions:
+                        print(shorten_title(point.text) + ' (' + link + ')')
+                        try:
+                            name = clean_text(point.text.split('[' + ext + ']')[0])
+                            fullname = '.'.join((name, ext))
+                            if not os.path.exists('/'.join((current_directory, fullname)) ):
+                                download_pdf(link, current_directory, '.'.join((name, ext)))
+                        except KeyboardInterrupt:
+                            try:
+                                print('Press Ctrl-C in 1 second to quit')
+                                time.sleep(1)
+                            except KeyboardInterrupt:
+                                print('Cancelling..')
+                                break
+                        except:
+                            failures.append(point.text)
+                        
+        point = point.next_sibling          
