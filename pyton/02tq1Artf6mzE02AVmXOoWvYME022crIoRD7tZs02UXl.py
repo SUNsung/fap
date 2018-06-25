e5717ec044f3e909e1be3cfc8c21bc81edf3b245
SUNsung <weatherius@gmail.com>
2018-06-25 23:53:23 +0300

    Rewrite [02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py] File in: 2018-06-25 23:53:22

--- pyton/02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py
+++ pyton/02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py
@@ -1,3 +1,91 @@
 
         
         
+def test_follow_all_redirects_shown(httpbin):
+    r = http('--follow', '--all', httpbin.url + '/redirect/2')
+    assert r.count('HTTP/1.1') == 3
+    assert r.count('HTTP/1.1 302 FOUND', 2)
+    assert HTTP_OK in r
+    
+        name = 'Basic HTTP auth'
+    auth_type = 'basic'
+    
+    
+ENTRY_POINT_NAMES = [
+    'httpie.plugins.auth.v1',
+    'httpie.plugins.formatter.v1',
+    'httpie.plugins.converter.v1',
+    'httpie.plugins.transport.v1',
+]
+    
+            plugin = plugin_manager.get_auth_plugin(auth['type'])()
+    
+        def test_conflicts(self):
+        # Note: Defined IP is more important than defined port in match
+        self.assertTrue(self.addr.conflicts(self.addr1))
+        self.assertTrue(self.addr.conflicts(self.addr2))
+        self.assertTrue(self.addr.conflicts(self.addr_defined))
+        self.assertFalse(self.addr.conflicts(self.addr_default))
+    
+    
+class RedirectTest(unittest.TestCase):
+    '''Test the redirect_by_default method.'''
+    @classmethod
+    def _call(cls):
+        from certbot.display.enhancements import redirect_by_default
+        return redirect_by_default()
+    
+    import mock
+    
+        description = 'Example Authenticator plugin'
+    
+    from functools import partial
+import unittest
+from test import support
+from textwrap import dedent
+import os
+import re
+    
+            rd, wr = self.make_socketpair()
+    
+            The file is read and scanned in chunks of chunksize bytes.
+    
+    @dataclasses.dataclass
+class CV:
+    T_CV4 = typing.ClassVar
+    cv0: typing.ClassVar[int] = 20
+    cv1: typing.ClassVar = 30
+    cv2: T_CV2
+    cv3: T_CV3
+    not_cv4: T_CV4  # When using string annotations, this field is not recognized as a ClassVar.
+    
+    @dataclasses.dataclass
+class CV:
+    T_CV4 = typing.ClassVar
+    cv0: typing.ClassVar[int] = 20
+    cv1: typing.ClassVar = 30
+    cv2: T_CV2
+    cv3: T_CV3
+    not_cv4: T_CV4  # When using string annotations, this field is not recognized as a ClassVar.
+    
+    @dataclass
+class CV:
+    T_CV4 = ClassVar
+    cv0: ClassVar[int] = 20
+    cv1: ClassVar = 30
+    cv2: T_CV2
+    cv3: T_CV3
+    not_cv4: T_CV4  # When using string annotations, this field is not recognized as a ClassVar.
+    
+        def test_getnewargs(self):
+        self.assertEqual((1+2j).__getnewargs__(), (1.0, 2.0))
+        self.assertEqual((1-2j).__getnewargs__(), (1.0, -2.0))
+        self.assertEqual((2j).__getnewargs__(), (0.0, 2.0))
+        self.assertEqual((-0j).__getnewargs__(), (0.0, -0.0))
+        self.assertEqual(complex(0, INF).__getnewargs__(), (0.0, INF))
+        self.assertEqual(complex(INF, 0).__getnewargs__(), (INF, 0.0))
+    
+        hass.services.register(DOMAIN, SERVICE_BROWSE_URL,
+                           lambda service:
+                           webbrowser.open(service.data[ATTR_URL]),
+                           schema=SERVICE_BROWSE_URL_SCHEMA)
