7a0965865d048e77d0fdd6373e2296a662dbc483
SUNsung <weatherius@gmail.com>
2018-06-25 11:42:34 +0300

    Rewrite [38H6Q.5QJrA7I38MkjjXHkGqY38s7awzWQLfgw38jOF.py] File in: 2018-06-25 11:42:34

--- pyton/38H6Q.5QJrA7I38MkjjXHkGqY38s7awzWQLfgw38jOF.py
+++ pyton/38H6Q.5QJrA7I38MkjjXHkGqY38s7awzWQLfgw38jOF.py
@@ -1,5 +1,8 @@
 
         
         
-if __name__ == '__main__':
-    HitCounts.run()
+def media_prev_track(hass):
+    '''Press the keyboard button for prev track.'''
+    hass.services.call(DOMAIN, SERVICE_MEDIA_PREVIOUS_TRACK)
+    
+        result = meters
