04219d48d0af6c843fee306026d204b9ad3303b2
SUNsung <weatherius@gmail.com>
2018-06-25 04:36:56 +0300

    Rewrite [31ZU2SU8vRrk31iO8RTz6fiKM31jUL6QoDr2aE31117.py] File in: 2018-06-25 04:36:55

--- pyton/31ZU2SU8vRrk31iO8RTz6fiKM31jUL6QoDr2aE31117.py
+++ pyton/31ZU2SU8vRrk31iO8RTz6fiKM31jUL6QoDr2aE31117.py
@@ -1,53 +1,9 @@
 
         
-            (x_train, y_train), _, (sample_weight, _, _) = _get_test_data()
+        __all__ = ['cbs_download']
     
     
-@keras_test
-@skipif_no_tf_gpu
-def test_return_state():
-    input_size = 10
-    timesteps = 6
-    units = 2
-    num_samples = 32
-    
-        if hasattr(model, 'metrics'):
-        for m in model.stateful_metric_functions:
-            m.reset_states()
-        stateful_metric_indices = [
-            i for i, name in enumerate(model.metrics_names)
-            if str(name) in model.stateful_metric_names]
-    else:
-        stateful_metric_indices = []
-    
-        @support.cpython_only
-    @support.bigmemtest(sys.maxsize + 1000, memuse=2/15 * 2, dry_run=False)
-    def test_huge_lshift(self, size):
-        self.assertEqual(1 << (sys.maxsize + 1000), 1 << 1000 << sys.maxsize)
-    
-            self.assertIn(entry.get(), 'hello')
-        egi = entry.grid_info()
-        equal(int(egi['row']), 0)
-        equal(int(egi['column']), 1)
-        equal(int(egi['rowspan']), 1)
-        equal(int(egi['columnspan']), 1)
-        equal(self.dialog.row, 1)
-    
-        def newnews(self, group, date, *, file=None):
-        '''Process a NEWNEWS command.  Arguments:
-        - group: group name or '*'
-        - date: a date or datetime object
-        Return:
-        - resp: server response if successful
-        - list: list of message ids
-        '''
-        if not isinstance(date, (datetime.date, datetime.date)):
-            raise TypeError(
-                'the date parameter must be a date or datetime object, '
-                'not '{:40}''.format(date.__class__.__name__))
-        date_str, time_str = _unparse_datetime(date, self.nntp_version < 2)
-        cmd = 'NEWNEWS {0} {1} {2}'.format(group, date_str, time_str)
-        return self._longcmdstring(cmd, file)
-    
-    def ISEOF(x):
-    return x == ENDMARKER
+def mtv81_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_content(url)
+    title = HTMLParser().unescape(
+        '|'.join(match1(html, r'<title>(.*?)</title>').split('|')[:-2]))
