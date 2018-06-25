5014eba4ec2b52c364665e24b3a3170357ed67c9
SUNsung <weatherius@gmail.com>
2018-06-25 13:08:41 +0300

    Rewrite [india.rb] File in: 2018-06-25 13:08:40

--- ruby/india.rb
+++ ruby/india.rb
@@ -1,154 +1,83 @@
 
         
-          s.author   = ['Pratik Naik', 'David Heinemeier Hansson']
-  s.email    = ['pratiknaik@gmail.com', 'david@loudthinking.com']
-  s.homepage = 'http://rubyonrails.org'
+        # No trailing slash
+Benchmark.ips do |x|
+  x.report('with body include?') { CONTENT_CONTAINING.include?('<body') }
+  x.report('with body regexp')   { CONTENT_CONTAINING =~ /<\s*body/ }
+  x.compare!
+end
+
     
-          private
-        # Called once a consumer has become a subscriber of the channel. Usually the place to setup any streams
-        # you want this channel to be sending to the subscriber.
-        def subscribed # :doc:
-          # Override in subclasses
-        end
-    
-            # May be overridden to change the default stream handling behavior
-        # which decodes JSON and transmits to the client.
-        #
-        # TODO: Tests demonstrating this.
-        #
-        # TODO: Room for optimization. Update transmit API to be coder-aware
-        # so we can no-op when pubsub and connection are both JSON-encoded.
-        # Then we can skip decode+encode if we're just proxying messages.
-        def default_stream_handler(broadcasting, coder:)
-          coder ||= ActiveSupport::JSON
-          stream_transmitter stream_decoder(coder: coder), broadcasting: broadcasting
-        end
-    
-            def process_internal_message(message)
-          case message['type']
-          when 'disconnect'
-            logger.info 'Removing connection (#{connection_identifier})'
-            websocket.close
-          end
-        rescue Exception => e
-          logger.error 'There was an exception - #{e.class}(#{e.message})'
-          logger.error e.backtrace.join('\n')
-    
-        exec './child #{parent_pid}'
-  end
+    def test_dir(*subdirs)
+  File.join(TEST_DIR, *subdirs)
 end
     
-        batches(BATCH_SIZE) do |offset|
-      results = mysql_query(
-        'SELECT u.uid id, u.username username, u.email email, u.groupid group_id,
-                    su.regdate regdate, su.password password_hash, su.salt salt,
-                    s.regip regip, s.lastip last_visit_ip, s.lastvisit last_visit_time, s.lastpost last_posted_at, s.lastsendmail last_emailed_at,
-                    u.emailstatus email_confirmed, u.avatarstatus avatar_exists,
-                    p.site website, p.address address, p.bio bio, p.realname realname, p.qq qq,
-                    p.resideprovince resideprovince, p.residecity residecity, p.residedist residedist, p.residecommunity residecommunity,
-                    p.resideprovince birthprovince, p.birthcity birthcity, p.birthdist birthdist, p.birthcommunity birthcommunity,
-                    h.spacecss spacecss, h.spacenote spacenote,
-                    f.customstatus customstatus, f.sightml sightml
-               FROM #{user_table} u
-               LEFT JOIN #{sensitive_user_table} su USING(uid)
-               LEFT JOIN #{profile_table} p USING(uid)
-               LEFT JOIN #{status_table} s USING(uid)
-               LEFT JOIN #{forum_table} f USING(uid)
-               LEFT JOIN #{home_table} h USING(uid)
-              ORDER BY u.uid ASC
-              LIMIT #{BATCH_SIZE}
-             OFFSET #{offset};')
+    File.write(config, output)
+
     
-              attrs = {
-            user_id: post_user.id,
-            topic_id: post.topic_id,
-            raw: p[:cooked],
-            cooked: p[:cooked],
-            created_at: Date.parse(p[:created_at])
-          }
+    $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
     
-        total_count = @client.query('
-        SELECT COUNT(*) count
-          FROM node n
-         WHERE n.type = 'answer'
-           AND n.status = 1;').first['count']
-    
-      def normalize_raw!(raw)
-    return '<missing>' if raw.blank?
-    raw = raw.dup
-    
-        import_users
-    import_discussions
-    
-          FastlaneCore::PrintTable.print_values(config: Cert.config, hide_keys: [:output_path], title: 'Summary for cert #{Fastlane::VERSION}')
-    
-        # Override Appfile configuration for a specific lane.
+        # Initialize a new Layout.
     #
-    # lane_name  - Symbol representing a lane name. (Can be either :name, 'name' or 'platform name')
-    # block - Block to execute to override configuration values.
-    #
-    # Discussion If received lane name does not match the lane name available as environment variable, no changes will
-    #             be applied.
-    def for_lane(lane_name)
-      if lane_name.to_s.split(' ').count > 1
-        # That's the legacy syntax 'platform name'
-        puts('You use deprecated syntax '#{lane_name}' in your Appfile.'.yellow)
-        puts('Please follow the Appfile guide: https://docs.fastlane.tools/advanced/#appfile'.yellow)
-        platform, lane_name = lane_name.split(' ')
+    # site - The Site.
+    # base - The String path to the source.
+    # name - The String filename of the post file.
+    def initialize(site, base, name)
+      @site = site
+      @base = base
+      @name = name
     
-            c.option('--username username', String, 'Username to remove.')
+        describe '#revoke!' do
+      it 'revokes the key with the client' do
+        mock_client_response(:revoke_key!)
     
-            expect(CredentialsManager::AppfileConfig.new('credentials_manager/spec/fixtures/Appfile5').data[:apple_id]).to eq('felix@sunapps.net')
-        expect(CredentialsManager::AppfileConfig.new('credentials_manager/spec/fixtures/Appfile5').data[:app_identifier]).to eq('net.sunapps.1')
-        expect(CredentialsManager::AppfileConfig.new('credentials_manager/spec/fixtures/Appfile5').data[:team_id]).to eq('Q2CBPJ58CC')
+      def all_projects(current_user)
+    projects = []
     
-          if okay
-        UI.success('HTML file confirmed...') # print this to give feedback to the user immediately
-      else
-        UI.user_error!('Did not upload the metadata, because the HTML file was rejected by the user')
-      end
+        def boolify(value)
+      agent.send(:boolify, value)
     end
     
-      def report_params
-    params.require(:abuse_report).permit(%i(
-      message
-      user_id
-    ))
-  end
+      def destroy
+    @services = current_user.services.find(params[:id])
+    @services.destroy
     
-      def update
-    if @broadcast_message.update(broadcast_message_params)
-      redirect_to admin_broadcast_messages_path, notice: 'Broadcast Message was successfully updated.'
-    else
-      render :edit
-    end
-  end
+        includes, ignores = argv_includes_ignores(ARGV)
     
-        # @return [String] major.minor ruby version, ex 1.9
-    def ruby_abi_version
-      RUBY_VERSION[/(\d+\.\d+)(\.\d+)*/, 1]
-    end
+    class DependencyCollector
+  module Compat
+    # Define the languages that we can handle as external dependencies.
+    LANGUAGE_MODULES = Set[
+      :lua, :lua51, :perl, :python, :python3, :ruby
+    ].freeze
     
-            builder.eval_gemfile('bundler file', gemfile.generate())
-        definition = builder.to_definition(lockfile_path, {})
-        definition.lock(lockfile_path)
-        gemfile.save
-      rescue => e
-        # the error should be handled elsewhere but we need to get the original file if we dont
-        # do this logstash will be in an inconsistent state
-        gemfile.restore!
-        raise e
-      end
-    ensure
-      Bundler.settings[:frozen] = '1' if frozen
+        # Used to prune requirements when calling expand with a block.
+    def prune
+      throw(:prune, true)
     end
   end
 end
 
     
-      # Each plugin install for a gemfile create a path with a unique id.
-  # we must clear what is not currently used in the 
-  def remove_unused_locally_installed_gems!
-    used_path = gemfile.locally_installed_gems.collect { |gem| gem.options[:path] }
+      def verify_download_integrity(_fn)
+    nil
+  end
+end
     
-      parameter '[PLUGIN]', 'Part of plugin name to search for, leave empty for all plugins'
+            def responds?
+          methods = @subject.instance_methods.map(&:to_s)
+          methods.include?('#{@attachment_name}') &&
+            methods.include?('#{@attachment_name}=') &&
+            methods.include?('#{@attachment_name}?')
+        end
+    
+    module Paperclip
+  require 'rails'
+    
+    module Paperclip
+  # Provides helper methods that can be used in migrations.
+  module Schema
+    COLUMNS = {:file_name    => :string,
+               :content_type => :string,
+               :file_size    => :integer,
+               :updated_at   => :datetime}
