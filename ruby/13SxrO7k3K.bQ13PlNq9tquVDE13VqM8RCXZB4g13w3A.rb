00bc43ef307579c8b054142a44da3d464fcfcdf0
SUNsung <weatherius@gmail.com>
2018-06-25 20:07:01 +0300

    Rewrite [13SxrO7k3K.bQ13PlNq9tquVDE13VqM8RCXZB4g13w3A.rb] File in: 2018-06-25 20:07:00

--- ruby/13SxrO7k3K.bQ13PlNq9tquVDE13VqM8RCXZB4g13w3A.rb
+++ ruby/13SxrO7k3K.bQ13PlNq9tquVDE13VqM8RCXZB4g13w3A.rb
@@ -1,116 +1,29 @@
 
         
-              def accessor
-        ActiveRecord::Store::IndifferentHashAccessor
-      end
-    
-      def test_pick_two
-    assert_equal ['David', 'david@loudthinking.com'], Topic.order(:id).pick(:author_name, :author_email_address)
-    assert_nil Topic.none.pick(:author_name, :author_email_address)
-    assert_nil Topic.where('1=0').pick(:author_name, :author_email_address)
-  end
-    
-      def test_calling_one_should_count_instead_of_loading_association
-    firm = companies(:first_firm)
-    assert_queries(1) do
-      firm.clients.one?  # use count query
-    end
-    assert_not_predicate firm.clients, :loaded?
-  end
-    
-      def check_content_mismatch
-    if attribute_present?('title') && attribute_present?('content') && content == 'Mismatch'
-      errors[:title] << 'is Content Mismatch'
-    end
-  end
-    
-          @channel.perform_action 'action' => :get_latest
-      expected_data = { data: 'latest' }
-    
-      setup do
-    @connection = TestConnection.new
-  end
-    
-        @connection.stub(:subscriptions, subscriptions) do
-      @channel = SecretChannel.new @connection, '{id: 1}', id: 1
-      @channel.subscribe_to_channel
-    
-          client = connection.websocket.send(:websocket)
-      event = Concurrent::Event.new
-      client.instance_variable_get('@stream')
-        .instance_variable_get('@rack_hijack_io')
-        .define_singleton_method(:close) { event.set }
-      connection.close
-      event.wait
-    end
-  end
-    
-    def check_link(uri)
-  HTTParty.head(uri, :verify => false).code.to_i.tap do |status|
-    if (400..422).include?(status)
-      if status != 403 && !uri.exclude?('udemy.com')
-        raise 'Request had status #{status}'
-      else
-        putc('S')
-      end
-    end
-  end
-end
-    
-        options[:skip] = %w(
-      contents.html
-      genindex.html
-      py-modindex.html
-      glossary.html
-      search.html
-      intro/whatsnext.html)
-    
-    module Jekyll
-    
-        def render(context)
-      includes_dir = File.join(context.registers[:site].source, '_includes')
-    
-    Liquid::Template.register_tag('render_partial', Jekyll::RenderPartialTag)
-
-    
-      def render(source)
-    template = File.read(source)
-    renderer = ERB.new(template)
-    context  = LogStash::PluginManager::RenderContext.new(options)
-    renderer.result(context.get_binding)
-  end
-    
-      def as_boolean(string)
-    return true   if string == true   || string =~ (/(true|t|yes|y|1)$/i)
-    return false  if string == false  || string.blank? || string =~ (/(false|f|no|n|0)$/i)
-    raise ArgumentError.new('invalid value for Boolean: \'#{string}\'')
-  end
-    
-              def logstash_plugin_gem_spec?(spec)
-            spec.metadata && spec.metadata['logstash_plugin'] == 'true'
+                    # Register the handler if this is our first callback.
+            Signal.trap('INT') { fire_callbacks } if registered.length == 1
           end
-    
-        def add_required_validations
-      options = Paperclip::Attachment.default_options.deep_merge(@options)
-      if options[:validate_media_type] != false
-        name = @name
-        @klass.validates_media_type_spoof_detection name,
-          :if => ->(instance){ instance.send(name).dirty? }
-      end
-    end
-    
-            def has_column?
-          @subject.column_names.include?('#{@attachment_name}_file_name')
         end
-      end
-    end
-  end
-end
-
     
-            required = directories.map do |directory|
-          pathname = File.expand_path(Rails.root.join(directory, filename))
-          file_exists = File.exist?(pathname)
-          require pathname if file_exists
-          file_exists
+          def merge!(other)
+        other.each do |key, value|
+          self[convert_key(key)] = value
         end
+        self
+      end
+    
+          # This deletes the block with the given key if it exists.
+      def delete(key)
+        key    = Regexp.quote(key)
+        regexp = /^#\s*VAGRANT-BEGIN:\s*#{key}$.*^#\s*VAGRANT-END:\s*#{key}$\r?\n?/m
+        @value.gsub!(regexp, '')
+      end
+    
+              # Verify the box exists that we want to repackage
+          box = @env.boxes.find(box_name, box_provider, '= #{box_version}')
+          if !box
+            raise Vagrant::Errors::BoxNotFoundWithProviderAndVersion,
+              name: box_name,
+              provider: box_provider.to_s,
+              version: box_version
+          end
