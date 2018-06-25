67efa315ce1e86d9b4fb4d3aa27b0753a6c1dfae
SUNsung <weatherius@gmail.com>
2018-06-25 09:56:48 +0300

    Create [479NX67xNU806471jgr1loTtI47tb5KEp1DvYM47vps.py] File in: 2018-06-25 09:56:47

---
+++ pyton/479NX67xNU806471jgr1loTtI47tb5KEp1DvYM47vps.py
@@ -0,0 +1,146 @@
+
+def with_metaclass(meta, *bases):
+    '''Create a base class with a metaclass.'''
+    # This requires a bit of explanation: the basic idea is to make a
+    # dummy metaclass for one level of class instantiation that replaces
+    # itself with the actual metaclass.
+    class metaclass(type):
+        def __new__(cls, name, this_bases, d):
+            return meta(name, bases, d)
+    return type.__new__(metaclass, 'temporary_class', (), {})
+    
+        def __get__(self, obj, type=None):
+        if obj is None:
+            return self
+        rv = obj.config[self.__name__]
+        if self.get_converter is not None:
+            rv = self.get_converter(rv)
+        return rv
+    
+    import logging
+import sys
+    
+            return list(result)
+    
+    from .globals import request
+from ._compat import with_metaclass
+    
+    
+def test_config_from_json():
+    app = flask.Flask(__name__)
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    app.config.from_json(os.path.join(current_dir, 'static', 'config.json'))
+    common_object_test(app)
+    
+        flask.got_request_exception.connect(record, app)
+    try:
+        assert app.test_client().get('/').status_code == 500
+        assert len(recorded) == 1
+        assert isinstance(recorded[0], ZeroDivisionError)
+    finally:
+        flask.got_request_exception.disconnect(record, app)
+    
+    
+@pytest.mark.functional
+def test_select_command_with_arrows(proc, TIMEOUT):
+    select_command_with_arrows(proc, TIMEOUT)
+    
+    
+@pytest.fixture(params=containers)
+def proc(request, spawnu, TIMEOUT):
+    proc = spawnu(*request.param)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(u'tcsh')
+    proc.sendline(u'setenv PYTHONIOENCODING utf8')
+    proc.sendline(u'eval `thefuck --alias`')
+    return proc
+    
+    # This view is called from FlatpageFallbackMiddleware.process_response
+# when a 404 is raised, which often means CsrfViewMiddleware.process_view
+# has not been called even if CsrfViewMiddleware is installed. So we need
+# to use @csrf_protect, in case the template needs {% csrf_token %}.
+# However, we can't just wrap this view; if no matching flatpage exists,
+# or a redirect is required for authentication, the 404 needs to be returned
+# without any CSRF checks. Therefore, we only
+# CSRF protect the internal implementation.
+    
+            Keyword:                   'bold #004461',   # class: 'k'
+        Keyword.Constant:          'bold #004461',   # class: 'kc'
+        Keyword.Declaration:       'bold #004461',   # class: 'kd'
+        Keyword.Namespace:         'bold #004461',   # class: 'kn'
+        Keyword.Pseudo:            'bold #004461',   # class: 'kp'
+        Keyword.Reserved:          'bold #004461',   # class: 'kr'
+        Keyword.Type:              'bold #004461',   # class: 'kt'
+    
+    
+def test_idna_without_version_attribute(mocker):
+    '''Older versions of IDNA don't provide a __version__ attribute, verify
+    that if we have such a package, we don't blow up.
+    '''
+    mocker.patch('requests.help.idna', new=None)
+    assert info()['idna'] == {'version': ''}
+    
+    
+@pytest.mark.parametrize(
+    'mask, expected', (
+        (8, '255.0.0.0'),
+        (24, '255.255.255.0'),
+        (25, '255.255.255.128'),
+    ))
+def test_dotted_netmask(mask, expected):
+    assert dotted_netmask(mask) == expected
+    
+    import idna
+import urllib3
+import chardet
+    
+    # -------
+# Pythons
+# -------
+    
+    Data structures that power Requests.
+'''
+    
+    
+@keras_test
+def test_layer_trainability_switch():
+    # with constructor argument, in Sequential
+    model = Sequential()
+    model.add(Dense(2, trainable=False, input_dim=1))
+    assert model.trainable_weights == []
+    
+        # Returns:
+        z (tensor): sampled latent vector
+    '''
+    
+    
+@keras_test
+def test_3d_to_3d():
+    '''
+    Apply a same Dense layer for each element of time dimension of the input
+    and make predictions of the output sequence elements.
+    This does not make use of the temporal structure of the sequence
+    (see TimeDistributedDense for more details)
+    '''
+    np.random.seed(1337)
+    (x_train, y_train), (x_test, y_test) = get_test_data(num_train=100,
+                                                         num_test=20,
+                                                         input_shape=(3, 5),
+                                                         output_shape=(3, 5),
+                                                         classification=False)
+    
+        model.compile(loss='hinge', optimizer='adagrad')
+    history = model.fit(x_train, y_train, epochs=20, batch_size=16,
+                        validation_data=(x_test, y_test), verbose=0)
+    assert (history.history['val_loss'][-1] < 0.9)
+    
+    ] + _py_files('scrapy/contrib') + _py_files('scrapy/contrib_exp')
+    
+      # A single unnamed argument for a function tends to look like old style cast.
+  # If we see those, don't issue warnings for deprecated casts.
+  remainder = line[match.end(0):]
+  if Match(r'^\s*(?:;|const\b|throw\b|final\b|override\b|[=>{),]|->)',
+           remainder):
+    return False
+    }
