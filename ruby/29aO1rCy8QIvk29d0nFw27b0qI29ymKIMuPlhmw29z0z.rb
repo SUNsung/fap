96a4768c5d2a650626e7c0e5f3ddb11772b94602
SUNsung <weatherius@gmail.com>
2018-06-25 21:52:33 +0300

    Rewrite [29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb] File in: 2018-06-25 21:52:33

--- ruby/29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb
+++ ruby/29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb
@@ -1,77 +1,83 @@
 
         
-                if name.start_with?('etc') || name.start_with?('core.stdc.')
-          name.split('.')[0..2].join('.')
-        elsif name.start_with?('ddmd')
-          'ddmd'
-        elsif name.start_with?('rt')
-          'rt'
-        else
-          name.split('.')[0..1].join('.')
+              def rendered_views
+        @_rendered_views ||= RenderedViewsCollection.new
+      end
+    
+            # Require the adapter itself and give useful feedback about
+        #   1. Missing adapter gems and
+        #   2. Adapter gems' missing dependencies.
+        path_to_adapter = 'action_cable/subscription_adapter/#{adapter}'
+        begin
+          require path_to_adapter
+        rescue LoadError => e
+          # We couldn't require the adapter itself. Raise an exception that
+          # points out config typos and missing gems.
+          if e.path == path_to_adapter
+            # We can assume that a non-builtin adapter was specified, so it's
+            # either misspelled or missing from Gemfile.
+            raise e.class, 'Could not load the '#{adapter}' Action Cable pubsub adapter. Ensure that the adapter is spelled correctly in config/cable.yml and that you've added the necessary adapter gem to your Gemfile.', e.backtrace
+    
+          def force_equality?(value)
+        coder.respond_to?(:object_class) && value.is_a?(coder.object_class)
+      end
+    
+          relation = Relation.new(klass)
+      relation.merge!(where: ['foo = ?', 'bar'])
+      assert_equal Relation::WhereClause.new(['foo = bar']), relation.where_clause
+    end
+    
+        def speak(data)
+      @last_action = [ :speak, data ]
+    end
+    
+        def read_message
+      @has_messages.try_acquire(1, WAIT_WHEN_EXPECTING_EVENT)
+    
+          assert_called(connection.subscriptions, :unsubscribe_from_all) do
+        connection.send :handle_close
+      end
+    
+    class ActionCable::Connection::StreamTest < ActionCable::TestCase
+  class Connection < ActionCable::Connection::Base
+    attr_reader :connected, :websocket, :errors
+    
+          assert_called(channel1, :unsubscribe_from_channel) do
+        assert_called(channel2, :unsubscribe_from_channel) do
+          @subscriptions.unsubscribe_from_all
         end
       end
-    
-        options[:attribution] = <<-HTML
-      &copy; 2011&ndash;2018 Twitter, Inc.<br>
-      &copy; 2011&ndash;2018 The Bootstrap Authors<br>
-      Code licensed under the MIT License.<br>
-      Documentation licensed under the Creative Commons Attribution License v3.0.
-    HTML
-    
-        version '2' do
-      self.release = '2.3.0'
-      self.base_url = 'http://coffeescript.org/'
-    
-          options[:fix_urls] = ->(url) do
-        url.sub! %r{/blob/master/readme.md}i, ''
-        url
-      end
-    end
-    
-        def ==(other)
-      other.name == name && other.path == path && other.type == type
-    end
-    
-        def contains?(url, options = nil)
-      !!subpath_to(url, options)
-    end
-    
-        platform_is_not :windows do
-      it 'expands a path when the default external encoding is ASCII-8BIT' do
-        Encoding.default_external = Encoding::ASCII_8BIT
-        path_8bit = [222, 173, 190, 175].pack('C*')
-        File.expand_path( path_8bit, @rootdir).should == '#{@rootdir}' + path_8bit
-      end
-    end
-    
-      after :each do
-    @file.close
-    @readonly_file.close
-    rm_r @filename
-  end
-    
-    ruby_version_is '2.4' do
-  describe 'String#casecmp? independent of case' do
-    it 'returns true when equal to other' do
-      'abc'.casecmp?('abc').should == true
-      'abc'.casecmp?('ABC').should == true
-    end
-    
-      it 'returns nil if no modifications were made' do
-    a = '+++---111222???'
-    a.swapcase!.should == nil
-    a.should == '+++---111222???'
-    
-      ruby_version_is '2.4' do
-    describe 'full Unicode case mapping' do
-      it 'modifies self in place for all of Unicode with no option' do
-        a = 'äöü'
-        a.upcase!
-        a.should == 'ÄÖÜ'
-      end
-    
-      describe 'RSTRING_LEN' do
-    it 'returns the size of the string' do
-      @s.RSTRING_LEN('gumdrops').should == 8
     end
   end
+    
+    module Capistrano
+  module Doctor
+    # Prints table of all Capistrano-related gems and their version numbers. If
+    # there is a newer version of a gem available, call attention to it.
+    class GemsDoctor
+      include Capistrano::Doctor::OutputHelpers
+    
+          # Prints a table for a given array of records. For each record, the block
+      # is yielded two arguments: the record and a Row object. To print values
+      # for that record, add values using `row << 'some value'`. A row can
+      # optionally be highlighted in yellow using `row.yellow`.
+      def table(records, &block)
+        return if records.empty?
+        rows = collect_rows(records, &block)
+        col_widths = calculate_column_widths(rows)
+    
+            on roles(target_roles) do
+          unless test '[ -f #{file.to_s.shellescape} ]'
+            info 'Uploading #{prerequisite_file} to #{file}'
+            upload! File.open(prerequisite_file), file
+          end
+        end
+      end
+    end
+    
+        orig_stdout = $stdout
+    orig_stderr = $stderr
+    captured_stdout = StringIO.new
+    captured_stderr = StringIO.new
+    $stdout = captured_stdout
+    $stderr = captured_stderr
