a07d3547878fddb9eca176fd1311c7b28fdad81c
SUNsung <weatherius@gmail.com>
2018-06-25 16:39:34 +0300

    Rewrite [23pHAgfzyr4o23ev1fqdRiZq.23b79LvE58k2M2395v.cpp] File in: 2018-06-25 16:39:34

--- cccccc/23pHAgfzyr4o23ev1fqdRiZq.23b79LvE58k2M2395v.cpp
+++ cccccc/23pHAgfzyr4o23ev1fqdRiZq.23b79LvE58k2M2395v.cpp
@@ -1,3 +1,127 @@
 
         
-        
+        	// Check for machine-specific then user config source file.
+	PathCombine(cpuCfgPath, userConfigDirPath, L'ConEmu-%COMPUTERNAME%.xml');
+	ExpandEnvironmentStrings(cpuCfgPath, cpuCfgPath, sizeof(cpuCfgPath) / sizeof(cpuCfgPath[0]));
+    
+    TEST(Metric, PoissionNegLogLik) {
+  xgboost::Metric * metric = xgboost::Metric::Create('poisson-nloglik');
+  ASSERT_STREQ(metric->Name(), 'poisson-nloglik');
+  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 0.5f, 1e-10);
+  EXPECT_NEAR(GetMetricEval(metric,
+                            {0.1f, 0.2f, 0.1f, 0.2f},
+                            {  0,   0,   1,   1}),
+              1.1280f, 0.001f);
+}
+
+    
+    SEXP XGDMatrixSliceDMatrix_R(SEXP handle, SEXP idxset) {
+  SEXP ret;
+  R_API_BEGIN();
+  int len = length(idxset);
+  std::vector<int> idxvec(len);
+  for (int i = 0; i < len; ++i) {
+    idxvec[i] = INTEGER(idxset)[i] - 1;
+  }
+  DMatrixHandle res;
+  CHECK_CALL(XGDMatrixSliceDMatrix(R_ExternalPtrAddr(handle),
+                                   BeginPtr(idxvec), len,
+                                   &res));
+  ret = PROTECT(R_MakeExternalPtr(res, R_NilValue, R_NilValue));
+  R_RegisterCFinalizerEx(ret, _DMatrixFinalizer, TRUE);
+  R_API_END();
+  UNPROTECT(1);
+  return ret;
+}
+    
+    /*
+ * Class:     ml_dmlc_xgboost4j_java_XGBoostJNI
+ * Method:    XGDMatrixCreateFromCSREx
+ * Signature: ([J[J[F)J
+ */
+JNIEXPORT jint JNICALL Java_ml_dmlc_xgboost4j_java_XGBoostJNI_XGDMatrixCreateFromCSREx
+  (JNIEnv *jenv, jclass jcls, jlongArray jindptr, jintArray jindices, jfloatArray jdata, jint jcol, jlongArray jout) {
+  DMatrixHandle result;
+  jlong* indptr = jenv->GetLongArrayElements(jindptr, 0);
+  jint* indices = jenv->GetIntArrayElements(jindices, 0);
+  jfloat* data = jenv->GetFloatArrayElements(jdata, 0);
+  bst_ulong nindptr = (bst_ulong)jenv->GetArrayLength(jindptr);
+  bst_ulong nelem = (bst_ulong)jenv->GetArrayLength(jdata);
+  jint ret = (jint) XGDMatrixCreateFromCSREx((size_t const *)indptr, (unsigned int const *)indices, (float const *)data, nindptr, nelem, jcol, &result);
+  setHandle(jenv, jout, result);
+  //Release
+  jenv->ReleaseLongArrayElements(jindptr, indptr, 0);
+  jenv->ReleaseIntArrayElements(jindices, indices, 0);
+  jenv->ReleaseFloatArrayElements(jdata, data, 0);
+  return ret;
+}
+    
+      delete metric;
+  metric = xgboost::Metric::Create('pre@2');
+  ASSERT_STREQ(metric->Name(), 'pre@2');
+  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 0.5f, 1e-7);
+  EXPECT_NEAR(GetMetricEval(metric,
+                            {0.1f, 0.9f, 0.1f, 0.9f},
+                            {  0,   0,   1,   1}),
+              0.5f, 0.001f);
+    
+    
+    {// Enable LOG(CONSOLE) for print messages to console.
+#define LOG_CONSOLE ::xgboost::ConsoleLogger()
+// Enable LOG(TRACKER) for print messages to tracker
+#define LOG_TRACKER ::xgboost::TrackerLogger()
+}  // namespace xgboost.
+#endif  // XGBOOST_LOGGING_H_
+
+    
+      /*! \return the default evaluation metric for the objective */
+  virtual const char* DefaultEvalMetric() const = 0;
+  // the following functions are optional, most of time default implementation is good enough
+  /*!
+   * \brief transform prediction values, this is only called when Prediction is called
+   * \param io_preds prediction values, saves to this vector as well
+   */
+  virtual void PredTransform(HostDeviceVector<bst_float> *io_preds) {}
+    
+    // Define a customized logistic regression objective in C++.
+// Implement the interface.
+class MyLogistic : public ObjFunction {
+ public:
+  void Configure(const std::vector<std::pair<std::string, std::string> >& args) override {
+    param_.InitAllowUnknown(args);
+  }
+  void GetGradient(HostDeviceVector<bst_float> *preds,
+                   const MetaInfo &info,
+                   int iter,
+                   HostDeviceVector<GradientPair> *out_gpair) override {
+    out_gpair->Resize(preds->Size());
+    std::vector<bst_float>& preds_h = preds->HostVector();
+    std::vector<GradientPair>& out_gpair_h = out_gpair->HostVector();
+    for (size_t i = 0; i < preds_h.size(); ++i) {
+      bst_float w = info.GetWeight(i);
+      // scale the negative examples!
+      if (info.labels_[i] == 0.0f) w *= param_.scale_neg_weight;
+      // logistic transformation
+      bst_float p = 1.0f / (1.0f + std::exp(-preds_h[i]));
+      // this is the gradient
+      bst_float grad = (p - info.labels_[i]) * w;
+      // this is the second order gradient
+      bst_float hess = p * (1.0f - p) * w;
+      out_gpair_h.at(i) = GradientPair(grad, hess);
+    }
+  }
+  const char* DefaultEvalMetric() const override {
+    return 'error';
+  }
+  void PredTransform(HostDeviceVector<bst_float> *io_preds) override {
+    // transform margin value to probability.
+    std::vector<bst_float> &preds = io_preds->HostVector();
+    for (size_t i = 0; i < preds.size(); ++i) {
+      preds[i] = 1.0f / (1.0f + std::exp(-preds[i]));
+    }
+  }
+  bst_float ProbToMargin(bst_float base_score) const override {
+    // transform probability to margin value
+    return -std::log(1.0f / base_score - 1.0f);
+  }
+    }
