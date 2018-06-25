91a67ec5a6406a43086a2385f25cddacb7c6a2fc
SUNsung <weatherius@gmail.com>
2018-06-26 00:26:02 +0300

    Rewrite [03MQ2A1dHTXo03cjgLyGIypM03CNUNgxTUirs03OJp.cpp] File in: 2018-06-26 00:26:01

--- cccccc/03MQ2A1dHTXo03cjgLyGIypM03CNUNgxTUirs03OJp.cpp
+++ cccccc/03MQ2A1dHTXo03cjgLyGIypM03CNUNgxTUirs03OJp.cpp
@@ -1,147 +1,150 @@
 
         
-          assert(peekType(msg) == thpp::Type::LONG);
-  int64_t arg2 = unpackInteger(msg);
-  assert(arg2 == 100);
-    
-      for (auto s: tensor->sizes())
-    assert(s == 2);
-  for (int i = 0; i < 2; i++)
-    assert(reinterpret_cast<float*>(tensor->data())[i] == 5);
-    
-    
-namespace thd {
-    }
-    
-      if (b->nDimension == 1) {
-    b = THDTensor_(newWithStorage2d)(b->storage, b->storageOffset, b->size[0],
-            b->stride[0], 1, 0);
-    free_b = true;
-  }
-    
-    
-    {}
-    
-      /**
-   * Create a task. This returns a task handle, or null object
-   * if there are no worker threads.
-   */
-  static Resource TaskStart(
-    const String& url, const Array& headers,
-    const String& remote_host,
-    const String& post_data = null_string,
-    const Array& files = null_array,
-    int timeoutSeconds = -1,
-    PageletServerTaskEvent *event = nullptr
-  );
-    
-    #include <algorithm>
-    
-    /*
- * Vasm constant.
- *
- * Either a 1, 4, or 8 byte unsigned value, 8 byte double, or the disp32 part
- * of a thread-local address of an immutable constant that varies by thread.
- * Constants may also represent an undefined value, indicated by the isUndef
- * member.
- *
- * Also contains convenience constructors for various pointer and enum types.
- */
-struct Vconst {
-  enum Kind { Quad, Long, Byte, Double };
-    }
-    
-      // DataBlock can optionally be growable. The initial expansion of DataBlock
-  // will allocate a new buffer that is owned by the DataBlock, subsequent
-  // expansions will use realloc to expand this block until m_maxGrow has been
-  // reached. Only DataBlocks which have a different m_base from m_destBase may
-  // be grown, as expansion may move the location of m_destBase.
-  struct Deleter final { void operator()(uint8_t* a) const { ::free(a); } };
-  std::unique_ptr<uint8_t, Deleter> m_destBuf{nullptr};
-    
-    
-void Instruction::SetPCRelImmTarget(Instruction* target) {
-  // ADRP is not supported, so 'this' must point to an ADR instruction.
-  assert(Mask(PCRelAddressingMask) == ADR);
-    }
-    
-    bool CurlShareResource::setLongOption(long option, long value) {
-  CURLSHcode error = CURLSHE_OK;
-  error = curl_share_setopt(m_share,
-                            (CURLSHoption)option,
-                            value);
-  return error == CURLSHE_OK;
-}
-    
-    
-    {    CondVarNode() {
-      pthread_cond_init(&m_cond, nullptr);
-    }
-    ~CondVarNode() {
-      pthread_cond_destroy(&m_cond);
-    }
-    /* implicit */ operator pthread_cond_t*() {
-      return &m_cond;
-    }
-    void unlink() {
-      if (m_listHook.is_linked()) m_listHook.unlink();
-    }
-  };
-    
-    YGFloatOptional YGNode::getTrailingPadding(
-    const YGFlexDirection& axis,
-    const float& widthSize) const {
-  if (YGFlexDirectionIsRow(axis) &&
-      style_.padding[YGEdgeEnd].unit != YGUnitUndefined &&
-      !YGResolveValue(style_.padding[YGEdgeEnd], widthSize).isUndefined() &&
-      YGResolveValue(style_.padding[YGEdgeEnd], widthSize).getValue() >= 0.0f) {
-    return YGResolveValue(style_.padding[YGEdgeEnd], widthSize);
-  }
-    }
-    
-      const float leadingPaddingAndBorderCross = YGUnwrapFloatOptional(
-      node->getLeadingPaddingAndBorder(crossAxis, ownerWidth));
-  const float paddingAndBorderAxisMain = YGNodePaddingAndBorderForAxis(node, mainAxis, ownerWidth);
-  const float paddingAndBorderAxisCross =
-      YGNodePaddingAndBorderForAxis(node, crossAxis, ownerWidth);
-    
-    
-    {void captureBacktrace(size_t skip, vector<InstructionPointer>& stackTrace) {
-  // Beware of a bug on some platforms, which makes the trace loop until the
-  // buffer is full when it reaches a noexcept function. It seems to be fixed in
-  // newer versions of gcc. https://gcc.gnu.org/bugzilla/show_bug.cgi?id=56846
-  // TODO(t10738439): Investigate workaround for the stack trace bug
-  BacktraceState state = {skip, stackTrace};
-  _Unwind_Backtrace(unwindCallback, &state);
-}
-}
-    
-    TEST_F(YogaTest_HadOverflowTests, spacing_overflow_no_wrap_and_no_flex_children) {
-  const YGNodeRef child0 = YGNodeNewWithConfig(config);
-  YGNodeStyleSetWidth(child0, 80);
-  YGNodeStyleSetHeight(child0, 40);
-  YGNodeStyleSetMargin(child0, YGEdgeTop, 10);
-  YGNodeStyleSetMargin(child0, YGEdgeBottom, 10);
-  YGNodeInsertChild(root, child0, 0);
-  const YGNodeRef child1 = YGNodeNewWithConfig(config);
-  YGNodeStyleSetWidth(child1, 80);
-  YGNodeStyleSetHeight(child1, 40);
-  YGNodeStyleSetMargin(child1, YGEdgeBottom, 5);
-  YGNodeInsertChild(root, child1, 1);
-    }
-    
-    void Config::setExperimentalFeatureEnabled(int feature, bool enabled)
-{
-    YGConfigSetExperimentalFeatureEnabled(m_config, static_cast<YGExperimentalFeature>(feature), enabled);
-}
-    
-    
-    {    void toJS(nbind::cbOutput expose) const
-    {
-        expose(left, right, top, bottom, width, height);
-    }
-};
+        #endif  // ATOM_BROWSER_UI_X_EVENT_DISABLER_H_
 
     
-        method(getPositionType);
-    method(getPosition);
+      // The background contents associated with a hosted app has changed (either
+  // a new background contents has been created, or an existing background
+  // contents has closed). The source is the parent Profile, and the details
+  // are the BackgroundContentsService.
+  NOTIFICATION_BACKGROUND_CONTENTS_SERVICE_CHANGED,
+    
+    
+    {  DISALLOW_COPY_AND_ASSIGN(GlobalShortcutListener);
+};
+    
+      // Sets time interval between updates. By default list of sources and their
+  // thumbnail are updated once per second. If called after StartUpdating() then
+  // it will take effect only after the next update.
+  virtual void SetUpdatePeriod(base::TimeDelta period) = 0;
+    
+    void TtsPlatformImpl::WillSpeakUtteranceWithVoice(const Utterance* utterance,
+                                                  const VoiceData& voice_data) {
+}
+    
+        llvm::errs() << '\n=====================================================\n';
+    SyntaxNode->dump(llvm::errs());
+    llvm::errs() << '\n\n---- Maps to semantic node: ----\n\n';
+    
+    #include 'llvm/ADT/SetVector.h'
+    
+    @interface ImmutableDataVerifier : NSData {
+    ObjectBehaviorVerifier *_verifier;
+    NSData *_data;
+}
+@property (readonly) ObjectBehaviorVerifier *verifier;
+@end
+    
+      // Only allow allocation using the allocator in MarkupContext or by
+  // placement new.
+  void *operator new(size_t Bytes, swift::markup::MarkupContext &MC,
+                     unsigned Alignment = alignof(DocComment));
+  void *operator new(size_t Bytes, void *Mem) {
+    assert(Mem);
+    return Mem;
+  }
+    
+    
+    {  const SILDebugScope *Scope = this;
+  while (Scope->Parent.is<const SILDebugScope *>())
+    Scope = Scope->Parent.get<const SILDebugScope *>();
+  assert(Scope->Parent.is<SILFunction *>() && 'orphaned scope');
+  return Scope->Parent.get<SILFunction *>();
+}
+    
+    #ifndef SWIFT_BASIC_DIAGNOSTICOPTIONS_H
+#define SWIFT_BASIC_DIAGNOSTICOPTIONS_H
+    
+    #ifndef SWIFT_RUNTIME_ONCE_H
+#define SWIFT_RUNTIME_ONCE_H
+    
+    using namespace swift;
+    
+      virtual ~IndexDataConsumer() {}
+    
+      /// A list of all of the results that we are tracking in reverse order. The
+  /// reason that it is in reverse order is to allow us to simply traverse the
+  /// list by popping values off the back.
+  SmallVector<SILResultInfo, 8> allResults;
+    
+      /// Assignment operator.
+  LSLocation &operator=(const LSLocation &RHS) {
+    LSBase::operator=(RHS);
+    return *this;
+  }
+    
+    struct ExceptionInfo {
+  const std::type_info* type{nullptr};
+  // The values in frames are IP (instruction pointer) addresses.
+  // They are only filled if the low-level exception tracer library is
+  // linked in or LD_PRELOADed.
+  std::vector<uintptr_t> frames; // front() is top of stack
+};
+    
+      folly::Optional<T> try_take_for(std::chrono::milliseconds time) override {
+    T item;
+    while (!queue_.readIfNotEmpty(item)) {
+      if (!sem_.try_wait_for(time)) {
+        return folly::none;
+      }
+    }
+    return std::move(item);
+  }
+    
+      size_t sizeGuess() const {
+    size_t size = 0;
+    for (auto& q : queues_) {
+      size += q.sizeGuess();
+    }
+    return size;
+  }
+    
+    void SerialExecutor::run() {
+  std::unique_lock<std::mutex> lock(mutex_);
+    }
+    
+    FOLLY_ALWAYS_INLINE int __builtin_ffs(int x) {
+  unsigned long index;
+  return int(_BitScanForward(&index, (unsigned long)x) ? index + 1 : 0);
+}
+    
+    static void BM_memcpy(benchmark::State& state) {
+  char* src = new char[state.range(0)]; char* dst = new char[state.range(0)];
+  memset(src, 'x', state.range(0));
+  for (auto _ : state)
+    memcpy(dst, src, state.range(0));
+  state.SetBytesProcessed(int64_t(state.iterations()) *
+                          int64_t(state.range(0)));
+  delete[] src; delete[] dst;
+}
+BENCHMARK(BM_memcpy)->Arg(8)->Arg(64)->Arg(512)->Arg(1<<10)->Arg(8<<10);
+    
+    
+    {
+    {}  // end namespace internal
+}  // end namespace benchmark
+    
+    #endif  // BENCHMARK_COLORPRINT_H_
+
+    
+    // Parses a string for a string flag, in the form of
+// '--flag=value'.
+//
+// On success, stores the value of the flag in *value, and returns
+// true.  On failure, returns false without changing *value.
+bool ParseStringFlag(const char* str, const char* flag, std::string* value);
+    
+    struct LeastSq {
+  LeastSq() : coef(0.0), rms(0.0), complexity(oNone) {}
+    }
+    
+    double Finish(Counter const& c, double cpu_time, double num_threads) {
+  double v = c.value;
+  if (c.flags & Counter::kIsRate) {
+    v /= cpu_time;
+  }
+  if (c.flags & Counter::kAvgThreads) {
+    v /= num_threads;
+  }
+  return v;
+}
