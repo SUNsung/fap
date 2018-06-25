5efe6d8c298699f30e7fc0cb60cbfc830364aeb2
SUNsung <weatherius@gmail.com>
2018-06-26 01:32:14 +0300

    Rewrite [20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp] File in: 2018-06-26 01:32:13

--- cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
+++ cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
@@ -1,41 +1,218 @@
 
         
-        int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPTSTR    lpCmdLine,
-	_In_ int       nCmdShow)
-{
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
-	UNREFERENCED_PARAMETER(nCmdShow);
+        class DebuggerState : public DebuggerStateInterface {
+ public:
+  DebuggerState(const DebugOptions& debug_options);
+  virtual ~DebuggerState();
     }
     
-    #ifndef BASEEVENT_INTERFACE_BASEEVENT_H_
-#define BASEEVENT_INTERFACE_BASEEVENT_H_
+      // During the first Compute(), resource is either created or looked up using
+  // shared_name. In the latter case, the resource found should be verified if
+  // it is compatible with this op's configuration. The verification may fail in
+  // cases such as two graphs asking queues of the same shared name to have
+  // inconsistent capacities.
+  virtual Status VerifyResource(T* resource) { return Status::OK(); }
     
-    #ifdef ANDROID
+        http://www.apache.org/licenses/LICENSE-2.0
     
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
+        http://www.apache.org/licenses/LICENSE-2.0
     
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    {  protected:
-    std::string   m_servicename;
-    TServicesMap m_dependservices;
-};
+    class TFRecordReader : public ReaderBase {
+ public:
+  TFRecordReader(const string& node_name, const string& compression_type,
+                 Env* env)
+      : ReaderBase(strings::StrCat('TFRecordReader '', node_name, ''')),
+        env_(env),
+        offset_(0),
+        compression_type_(compression_type) {}
+    }
     
-    //
-//  testspy.h
-//  PublicComponent
-//
-//  Created by yerungui on 14-5-13.
-//
+      EXPECT_EQ(nullptr, cc->FindWorkerChannel('invalid_target'));
+  EXPECT_EQ(nullptr, cc->FindWorkerChannel('/job:other/replica:0/task:0'));
+  EXPECT_EQ(nullptr, cc->FindWorkerChannel('/job:mnist/replica:0/task:1'));
+  EXPECT_EQ(nullptr, cc->FindWorkerChannel('/job:mnist/replica:0/task:2'));
+  EXPECT_EQ(nullptr, cc->FindWorkerChannel('/job:mnist/replica:0/task:5'));
     
-    int TSpy::__TestFun1(int i)
-{
-    return reinterpret_cast<Test_Spy_Sample*>(This())->__TestFun1(i);
+    // Declare here, so we don't need a public header.
+Status RemoveDevice(const GraphDef& input_graph_def,
+                    const TransformFuncContext& context,
+                    GraphDef* output_graph_def);
+    
+      // Fill *indices and *sizes from *this (so that we can use the slice()
+  // function in eigen tensor). We need a tensor shape in case some of the
+  // slices are full slices.
+  // We allow NDIMS to be greater than dims(), in which case we will pad the
+  // higher dimensions with trivial dimensions.
+  template <int NDIMS>
+  void FillIndicesAndSizes(
+      const TensorShape& shape,
+      Eigen::DSizes<Eigen::DenseIndex, NDIMS>* indices,
+      Eigen::DSizes<Eigen::DenseIndex, NDIMS>* sizes) const;
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+        // Close the debug gRPC stream.
+    Status close_status = DebugIO::CloseDebugURL(server_data_.url);
+    ASSERT_TRUE(close_status.ok());
+    
+    Base::Base(int id,
+           const base::WeakPtr<ObjectManager>& object_manager,
+           const base::DictionaryValue& option,
+	   const std::string& extension_id)
+    : extension_id_(extension_id),
+      id_(id),
+      delay_destruction_(false),
+      pending_destruction_(false),
+      object_manager_(object_manager) {
 }
     
-    #include 'comm/debugger/spy.inl'
+    EventListener::~EventListener() {
+  for (std::map<int, BaseEvent*>::iterator i = listerners_.begin(); i != listerners_.end(); i++) {
+    delete i->second;
+  }
+}
+    
+    
+    {  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
+  if (!item)
+    return false;
+  return item->is_enabled_;
+}
+    
+      GtkRequisition menu_req;
+  gtk_widget_size_request(GTK_WIDGET(menu), &menu_req);
+  GdkScreen* screen;
+  gdk_display_get_pointer(gdk_display_get_default(), &screen, NULL, NULL, NULL);
+  gint monitor = gdk_screen_get_monitor_at_point(screen, *x, *y);
+    
+    class NwClipboardReadAvailableTypesFunction : public NWSyncExtensionFunction {
+ public:
+  NwClipboardReadAvailableTypesFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+      private:
+    int GetPrimaryMonitorIndex();
+    // DesktopMediaListObserver implementation.
+    void OnSourceAdded(DesktopMediaList* list, int index) override;
+    void OnSourceRemoved(DesktopMediaList* list, int index) override;
+    void OnSourceMoved(DesktopMediaList* list, int old_index, int new_index) override;
+    void OnSourceNameChanged(DesktopMediaList* list, int index) override;
+    void OnSourceThumbnailChanged(DesktopMediaList* list, int index) override;
+    
+      // implement nw.Screen.registerStream()
+  class NwScreenRegisterStreamFunction : public NWSyncExtensionFunction {
+  public:
+    NwScreenRegisterStreamFunction();
+    bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+    
+    {                    index = i;
+                    found = true;
+                }
+                else if (*currentp != (ElementType)0)
+                    RuntimeError('CopyDenseToOneHot: Cannot convert to onehot vector; contains value other than 0/1.');
+    
+                if (!wasDataRead) LogicError('DataRead Failure in batch normalization statistics');
+    
+    template class NDLUtil<float>;
+template class NDLUtil<double>;
+    
+        // GenerateNames - Generates a mapping table from original node to destination name
+    //    used for wildcard matches (i.e. L1* = L2*)
+    // symbolIn - symbol(s) to copy from
+    // symbolOut - symbol(s) to copy to
+    // netNdlIn -  netNdl to copy from
+    // netNdlOut - netNdl to copy to
+    // returns - Source nodes and Target names
+    typedef pair<ComputationNodeBasePtr, std::wstring> GenNameValue;
+    vector<GenNameValue> GenerateNames(const std::string& symbolIn, const std::string& symbolOut, NetNdl<ElemType>*& netNdlIn, NetNdl<ElemType>*& netNdlOut)
+    {
+        MapNodes mapInOut;
+        size_t firstStart, firstCount, secondStart, secondCount;
+        netNdlIn = ParseName(symbolIn, firstStart, firstCount, secondStart, secondCount);
+    }
+    
+    namespace Microsoft { namespace MSR { namespace CNTK {
+    }
+    }
+    }
+    
+    template <class _T>
+class const_array_ref
+{
+    const _T* data;
+    size_t n;
+    inline void check_index(size_t i) const
+    {
+        i;
+        assert(i < n);
+    }
+    inline void check_ptr() const
+    {
+        n;
+        data;
+        assert(n == 0 || data != NULL);
+    }
+    }
+    
+    TEST_F(FlagsTests, test_alias_types) {
+  // Test int32 lexical casting both ways.
+  EXPECT_EQ(FLAGS_test_int32_alias, 1);
+  FLAGS_test_int32_alias = 2;
+  EXPECT_EQ(FLAGS_test_int32, 2);
+  FLAGS_test_int32 = 3;
+  EXPECT_EQ(FLAGS_test_int32_alias, 3);
+  EXPECT_TRUE(FLAGS_test_int32_alias > 0);
+    }
+    
+    #include <osquery/status.h>
+    
+      TablePlugin::kCacheInterval = 5;
+  TablePlugin::kCacheStep = 1;
+  EXPECT_FALSE(test.testIsCached(5));
+  // Set the current time to 1, and the interval at 5.
+  test.testSetCache(TablePlugin::kCacheStep, TablePlugin::kCacheInterval);
+  // Time at 1 is cached for an interval of 5, so at time 5 the cache is fresh.
+  EXPECT_TRUE(test.testIsCached(5));
+  // 6 is the end of the cache, it is not fresh.
+  EXPECT_FALSE(test.testIsCached(6));
+  // 7 is past the cache, it is not fresh.
+  EXPECT_FALSE(test.testIsCached(7));
+    
+    Status WmiResultItem::GetUnsignedShort(const std::string& name,
+                                       unsigned short& ret) const {
+  std::wstring property_name = stringToWstring(name);
+  VARIANT value;
+  HRESULT hr = result_->Get(property_name.c_str(), 0, &value, nullptr, nullptr);
+    }
+    
+      // Check that all value lengths were maxed.
+  std::map<std::string, size_t> expected = {
+      {'name', 10}, {'age', 4}, {'food', 23}, {'number', 2}};
+  EXPECT_EQ(lengths, expected);
+    
+    TreeUpdater* TreeUpdater::Create(const std::string& name) {
+  auto *e = ::dmlc::Registry< ::xgboost::TreeUpdaterReg>::Get()->Find(name);
+  if (e == nullptr) {
+    LOG(FATAL) << 'Unknown tree updater ' << name;
+  }
+  return (e->body)();
+}
+    
+    
+    {}  // namespace xgboost
+#endif  // XGBOOST_TREE_UPDATER_H_
