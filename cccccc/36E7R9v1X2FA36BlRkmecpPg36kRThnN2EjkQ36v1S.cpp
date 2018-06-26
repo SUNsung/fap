045f153bf5896ba58f1f6bf44668c6383694493b
SUNsung <weatherius@gmail.com>
2018-06-26 22:47:10 +0300

    Rewrite [36E7R9v1X2FA36BlRkmecpPg36kRThnN2EjkQ36v1S.cpp] File in: 2018-06-26 22:47:09

--- cccccc/36E7R9v1X2FA36BlRkmecpPg36kRThnN2EjkQ36v1S.cpp
+++ cccccc/36E7R9v1X2FA36BlRkmecpPg36kRThnN2EjkQ36v1S.cpp
@@ -1,307 +1,142 @@
 
         
-        void SyntaxASTMap::clearSyntaxMap() {
-  SyntaxMap.shrink_and_clear();
-}
+        Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
     
-      if (!module->isStdlibModule()) {
-    std::string recordFile;
-    bool failed = false;
-    auto consumer = makeRecordingConsumer(filename, indexStorePath,
-                                          &diags, &recordFile, &failed);
-    indexModule(module, /*Hash=*/'', *consumer);
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    int main(int argc, char* argv[]) {
+  string wav = '';
+  string graph = '';
+  string labels = '';
+  string input_name = 'wav_data';
+  string output_name = 'labels_softmax';
+  int32 how_many_labels = 3;
+  std::vector<Flag> flag_list = {
+      Flag('wav', &wav, 'audio file to be identified'),
+      Flag('graph', &graph, 'model to be executed'),
+      Flag('labels', &labels, 'path to file containing labels'),
+      Flag('input_name', &input_name, 'name of input node in model'),
+      Flag('output_name', &output_name, 'name of output node in model'),
+      Flag('how_many_labels', &how_many_labels, 'number of results to show'),
+  };
+  string usage = tensorflow::Flags::Usage(argv[0], flag_list);
+  const bool parse_result = tensorflow::Flags::Parse(&argc, argv, flag_list);
+  if (!parse_result) {
+    LOG(ERROR) << usage;
+    return -1;
+  }
     }
     
-    
-    {/// Extract comments parts from the given Markup node.
-swift::markup::CommentParts
-extractCommentParts(swift::markup::MarkupContext &MC,
-                    swift::markup::MarkupASTNode *Node);
-} // namespace swift
-    
-      virtual void handleDiagnostic(SourceManager &SM, SourceLoc Loc,
-                                DiagnosticKind Kind,
-                                StringRef FormatString,
-                                ArrayRef<DiagnosticArgument> FormatArgs,
-                                const DiagnosticInfo &Info) override;
-    
-    class IndexDataConsumer {
-  virtual void anchor();
-    }
-    
-    void convert_dataset(const char* image_filename, const char* label_filename,
-        const char* db_filename) {
-  // Open files
-  std::ifstream image_file(image_filename, std::ios::in | std::ios::binary);
-  std::ifstream label_file(label_filename, std::ios::in | std::ios::binary);
-  CHECK(image_file) << 'Unable to open file ' << image_filename;
-  CHECK(label_file) << 'Unable to open file ' << label_filename;
-  // Read the magic and the meta data
-  uint32_t magic;
-  uint32_t num_items;
-  uint32_t num_labels;
-  uint32_t rows;
-  uint32_t cols;
-    }
-    
-    /**
- * @brief Applies common transformations to the input data, such as
- * scaling, mirroring, substracting the image mean...
- */
-template <typename Dtype>
-class DataTransformer {
+    // A simple parser for the json db file. It requires the db file to have the
+// exact form of [{'location': { 'latitude': 123, 'longitude': 456}, 'name':
+// 'the name can be empty' }, { ... } ... The spaces will be stripped.
+class Parser {
  public:
-  explicit DataTransformer(const TransformationParameter& param, Phase phase);
-  virtual ~DataTransformer() {}
+  explicit Parser(const std::string& db) : db_(db) {
+    // Remove all spaces.
+    db_.erase(
+        std::remove_if(db_.begin(), db_.end(), isspace),
+        db_.end());
+    if (!Match('[')) {
+      SetFailedAndReturnFalse();
+    }
+  }
     }
     
-    /**
- * @brief Abstract base class that factors out the BLAS code common to
- *        ConvolutionLayer and DeconvolutionLayer.
- */
-template <typename Dtype>
-class BaseConvolutionLayer : public Layer<Dtype> {
- public:
-  explicit BaseConvolutionLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+    #include 'src/compiler/config.h'
+#include 'src/compiler/generator_helpers.h'
+    
+    #include 'src/compiler/config.h'
+#include 'src/compiler/schema_interface.h'
+    
+    #include <gtest/gtest.h>
+    
+    namespace grpc {
+namespace testing {
+    }
     }
     
-    /**
- * @brief Index into the input blob along its first axis.
- *
- * This layer can be used to select, reorder, and even replicate examples in a
- * batch.  The second blob is cast to int and treated as an index into the
- * first axis of the first blob.
- */
-template <typename Dtype>
-class BatchReindexLayer : public Layer<Dtype> {
- public:
-  explicit BatchReindexLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
+    #include 'ifaddrs_android.h'
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/utsname.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <unistd.h>
+#include <errno.h>
+#include <linux/netlink.h>
+#include <linux/rtnetlink.h>
     
-    #include 'caffe/layers/neuron_layer.hpp'
+    	ClassDB::bind_method(D_METHOD('poll'), &StreamPeerSSL::poll);
+	ClassDB::bind_method(D_METHOD('accept_stream', 'stream'), &StreamPeerSSL::accept_stream);
+	ClassDB::bind_method(D_METHOD('connect_to_stream', 'stream', 'validate_certs', 'for_hostname'), &StreamPeerSSL::connect_to_stream, DEFVAL(false), DEFVAL(String()));
+	ClassDB::bind_method(D_METHOD('get_status'), &StreamPeerSSL::get_status);
+	ClassDB::bind_method(D_METHOD('disconnect_from_stream'), &StreamPeerSSL::disconnect_from_stream);
     
-    #endif  // CAFFE_CONTRASTIVE_LOSS_LAYER_HPP_
+    	Error err;
+    
+    	Transform m_worldTransform;
+	Transform local_xform;
+	bool engine_traction;
+	bool steers;
+    
+    
+    
+    #include 'db/version_edit.h'
+#include 'util/testharness.h'
+    
+    
+    {}  // namespace leveldb
+    
+    
+    {}  // namespace leveldb
 
     
-    /**
- * @brief Convolves the input image with a bank of learned filters,
- *        and (optionally) adds biases.
- *
- *   Caffe convolves by reduction to matrix multiplication. This achieves
- *   high-throughput and generality of input and filter dimensions but comes at
- *   the cost of memory for matrices. This makes use of efficiency in BLAS.
- *
- *   The input is 'im2col' transformed to a channel K' x H x W data matrix
- *   for multiplication with the N x K' x H x W filter matrix to yield a
- *   N' x H x W output matrix that is then 'col2im' restored. K' is the
- *   input channel * kernel height * kernel width dimension of the unrolled
- *   inputs so that the im2col matrix has a column for each input region to
- *   be filtered. col2im restores the output spatial structure by rolling up
- *   the output channel N' columns of the output matrix.
- */
-template <typename Dtype>
-class ConvolutionLayer : public BaseConvolutionLayer<Dtype> {
- public:
-  /**
-   * @param param provides ConvolutionParameter convolution_param,
-   *    with ConvolutionLayer options:
-   *  - num_output. The number of filters.
-   *  - kernel_size / kernel_h / kernel_w. The filter dimensions, given by
-   *  kernel_size for square filters or kernel_h and kernel_w for rectangular
-   *  filters.
-   *  - stride / stride_h / stride_w (\b optional, default 1). The filter
-   *  stride, given by stride_size for equal dimensions or stride_h and stride_w
-   *  for different strides. By default the convolution is dense with stride 1.
-   *  - pad / pad_h / pad_w (\b optional, default 0). The zero-padding for
-   *  convolution, given by pad for equal dimensions or pad_h and pad_w for
-   *  different padding. Input padding is computed implicitly instead of
-   *  actually padding.
-   *  - dilation (\b optional, default 1). The filter
-   *  dilation, given by dilation_size for equal dimensions for different
-   *  dilation. By default the convolution has dilation 1.
-   *  - group (\b optional, default 1). The number of filter groups. Group
-   *  convolution is a method for reducing parameterization by selectively
-   *  connecting input and output channels. The input and output channel dimensions must be divisible
-   *  by the number of groups. For group @f$ \geq 1 @f$, the
-   *  convolutional filters' input and output channels are separated s.t. each
-   *  group takes 1 / group of the input channels and makes 1 / group of the
-   *  output channels. Concretely 4 input channels, 8 output channels, and
-   *  2 groups separate input channels 1-2 and output channels 1-4 into the
-   *  first group and input channels 3-4 and output channels 5-8 into the second
-   *  group.
-   *  - bias_term (\b optional, default true). Whether to have a bias.
-   *  - engine: convolution has CAFFE (matrix multiplication) and CUDNN (library
-   *    kernels + stream parallelism) engines.
+    LookupKey::LookupKey(const Slice& user_key, SequenceNumber s) {
+  size_t usize = user_key.size();
+  size_t needed = usize + 13;  // A conservative estimate
+  char* dst;
+  if (needed <= sizeof(space_)) {
+    dst = space_;
+  } else {
+    dst = new char[needed];
+  }
+  start_ = dst;
+  dst = EncodeVarint32(dst, usize + 8);
+  kstart_ = dst;
+  memcpy(dst, user_key.data(), usize);
+  dst += usize;
+  EncodeFixed64(dst, PackSequenceAndType(s, kValueTypeForSeek));
+  dst += 8;
+  end_ = dst;
+}
+    
+    struct Options;
+    
+      virtual bool getChangeOptionForReserved() const CXX11_OVERRIDE;
+    
+      virtual ~ActivePeerConnectionCommand();
+    
+      /**
+   * The internal announce URL pointer points to next URL.
+   * If the current URL is the last element of its group, then the first
+   * element of the next group is pointed.
    */
-  explicit ConvolutionLayer(const LayerParameter& param)
-      : BaseConvolutionLayer<Dtype>(param) {}
-    }
+  void announceFailure();
     
+    #include <string>
+#include <deque>
     
-    {  size_t tempDataSize;
-  void *tempData1, *tempData2;
-};
-#endif
-    
-      // Returns the direction of the fitted line as a unit vector, using the
-  // least mean squared perpendicular distance. The line runs through the
-  // mean_point, i.e. a point p on the line is given by:
-  // p = mean_point() + lambda * vector_fit() for some real number lambda.
-  // Note that the result (0<=x<=1, -1<=y<=-1) is directionally ambiguous
-  // and may be negated without changing its meaning, since a line is only
-  // unique to a range of pi radians.
-  // Modernists prefer to think of this as an Eigenvalue problem, but
-  // Pearson had the simple solution in 1901.
-  //
-  // Note that this is equivalent to returning the Principal Component in PCA,
-  // or the eigenvector corresponding to the largest eigenvalue in the
-  // covariance matrix.
-  FCOORD vector_fit() const;
-    
-    /**********************************************************************
- * DIR128::DIR128
- *
- * Quantize the direction of an FCOORD to make a DIR128.
- **********************************************************************/
-    
-      // ValidFirstLine() and ValidBodyLine() take arguments describing a text line
-  // in a block of text which we are trying to model:
-  //   lmargin, lindent:  these add up to the distance from the leftmost ink
-  //                      in the text line to the surrounding text block's left
-  //                      edge.
-  //   rmargin, rindent:  these add up to the distance from the rightmost ink
-  //                      in the text line to the surrounding text block's right
-  //                      edge.
-  // The caller determines the division between 'margin' and 'indent', which
-  // only actually affect whether we think the line may be centered.
-  //
-  // If the amount of whitespace matches the amount of whitespace expected on
-  // the relevant side of the line (within tolerance_) we say it matches.
-    
-    
-namespace tesseract {
-    }
-    
-        bool TryLoadCheckPointInfo(const size_t epochNumber,
-                               /*out*/ size_t& totalSamplesSeen,
-                               /*out*/ double& learnRatePerSample,
-                               std::list<Matrix<ElemType>>& smoothedGradients,
-                               std::vector<double>& smoothedCounts,
-                               /*out*/ double& prevCriterion,
-                               /*out*/ size_t& minibatchSize);
-    void LoadCheckPointInfo(const size_t epochNumber,
-                            /*out*/ size_t& totalSamplesSeen,
-                            /*out*/ double& learnRatePerSample,
-                            std::list<Matrix<ElemType>>& smoothedGradients,
-                            std::vector<double>& smoothedCounts,
-                            /*out*/ double& prevCriterion,
-                            /*out*/ size_t& minibatchSize);
-    
-                auto sequenceValueVarSubshape = currentSequenceDataShape.SubShape(0, fullyDefinedSampleShape.Rank());
-            if (sequenceValueVarSubshape != fullyDefinedSampleShape)
-            {
-                for (size_t k = 0; k < fullyDefinedSampleShape.Rank(); ++k)
-                {
-                    if (fullyDefinedSampleShape[k] == NDShape::FreeDimension)
-                        fullyDefinedSampleShape[k] = sequenceValueVarSubshape[k];
-                    else if (fullyDefinedSampleShape[k] != sequenceValueVarSubshape[k])
-                        InvalidArgument('Value::Create: The shape '%S' of sequence #%zu is not compatible with the sample shape '%S'.', currentSequenceData->Shape().AsString().c_str(), i, sampleShape.AsString().c_str());
-                }
-            }
-    
-    
-    {        // perform the operation
-        msra::math::float4 one(1.0f);
-        foreach_index (i, us4)
-            us4[i] = us4[i] * h4[i] * (one - h4[i]); // eh(i,t) *= h(i,t) * (1.0f - h(i,t));
-#else
-        auto &us = *this;
-        foreach_coord (i, t, us)
-            us(i, t) *= h(i, t) * (1.0f - h(i, t));
-#endif
-    }
-    
-    void GranularGPUDataTransferer::WaitForSyncPointOnFetchStreamAsync()
-{
-    PrepareDevice(m_deviceId);
-    cudaStreamWaitEvent(GetFetchStream(), m_syncEvent, 0 /*flags 'must be 0'*/) || 'cudaStreamWaitEvent failed';
-}
-    
-    template function<ComputationNetworkPtr(DEVICEID_TYPE)> GetNetworkFactory<ScriptableObjects::IConfigRecord, float>(const ScriptableObjects::IConfigRecord& config);
-template function<ComputationNetworkPtr(DEVICEID_TYPE)> GetNetworkFactory<ScriptableObjects::IConfigRecord, double>(const ScriptableObjects::IConfigRecord& config);
-template function<ComputationNetworkPtr(DEVICEID_TYPE)> GetNetworkFactory<ConfigParameters, float>(const ConfigParameters& config);
-template function<ComputationNetworkPtr(DEVICEID_TYPE)> GetNetworkFactory<ConfigParameters, double>(const ConfigParameters& config);
-template ComputationNetworkPtr GetModelFromConfig<ConfigParameters, float> (const ConfigParameters& config, const wstring&, vector<wstring>& outputNodeNamesVector);
-template ComputationNetworkPtr GetModelFromConfig<ConfigParameters, double>(const ConfigParameters& config, const wstring&, vector<wstring>& outputNodeNamesVector);
-
-    
-        // FindSymbol - Find matching symbols in the symbol table
-    // symbol - symbol to find
-    // netNdl - [out] netNdl associated with this symbol
-    // returns - nodes this symbol references, might be empty
-    vector<ComputationNodeBasePtr> FindSymbols(const std::string& symbol, NetNdl<ElemType>*& netNdl)
-    {
-        size_t firstStart, firstCount, secondStart, secondCount;
-        netNdl = ParseName(symbol, firstStart, firstCount, secondStart, secondCount);
-        // take off the prefix
-        std::string search;
-        if (firstStart == symbol.length())
-        {
-            // this case is just the model label with nothing else, in that case we want the all nodes
-            search = '*';
-        }
-        else
-        {
-            search = symbol.substr(firstStart);
-        }
-    }
-    
-    // GetWriter - get a reader type from the DLL
-// The F version gets the 'float' version, and D gets 'double'.
-extern 'C' DATAWRITER_API void GetWriterF(IDataWriter** pwriter);
-extern 'C' DATAWRITER_API void GetWriterD(IDataWriter** pwriter);
-    
-    // implements rabit error handling.
-extern 'C' {
-  void XGBoostAssert_R(int exp, const char *fmt, ...);
-  void XGBoostCheck_R(int exp, const char *fmt, ...);
-}
-    
-    TEST(Metric, PoissionNegLogLik) {
-  xgboost::Metric * metric = xgboost::Metric::Create('poisson-nloglik');
-  ASSERT_STREQ(metric->Name(), 'poisson-nloglik');
-  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 0.5f, 1e-10);
-  EXPECT_NEAR(GetMetricEval(metric,
-                            {0.1f, 0.2f, 0.1f, 0.2f},
-                            {  0,   0,   1,   1}),
-              1.1280f, 0.001f);
-}
-
-    
-    namespace xgboost {
-namespace tree {
-// List of files that will be force linked in static links.
-DMLC_REGISTRY_LINK_TAG(updater_colmaker);
-DMLC_REGISTRY_LINK_TAG(updater_skmaker);
-DMLC_REGISTRY_LINK_TAG(updater_refresh);
-DMLC_REGISTRY_LINK_TAG(updater_prune);
-DMLC_REGISTRY_LINK_TAG(updater_fast_hist);
-DMLC_REGISTRY_LINK_TAG(updater_histmaker);
-DMLC_REGISTRY_LINK_TAG(updater_sync);
-#ifdef XGBOOST_USE_CUDA
-DMLC_REGISTRY_LINK_TAG(updater_gpu);
-DMLC_REGISTRY_LINK_TAG(updater_gpu_hist);
-#endif
-}  // namespace tree
-}  // namespace xgboost
-
-    
-          cbw.Write(buffer.data(), input.begin(), input.end());
+      // Don't allow copying
+  AuthConfig(const AuthConfig&);
+  AuthConfig& operator=(const AuthConfig&);
