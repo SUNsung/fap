56a7b3865e3873b00bd751c905faeaeb444b471d
SUNsung <weatherius@gmail.com>
2018-06-26 15:01:54 +0300

    Rewrite [540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py] File in: 2018-06-26 15:01:53

--- pyton/540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py
+++ pyton/540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py
@@ -1,66 +1,67 @@
 
         
-            # To avoid having to always use the '|safe' filter in flatpage templates,
-    # mark the title and content as already safe (since they are raw HTML
-    # content in the first place).
-    f.title = mark_safe(f.title)
-    f.content = mark_safe(f.content)
+                        directory = os.path.dirname(path)
     
-    from mrjob.job import MRJob
+        def on_open_shell(self):
+        try:
+            self._exec_cli_command('screen-length 0 temporary')
+        except AnsibleConnectionFailure:
+            raise AnsibleConnectionFailure('unable to set terminal parameters')
+
     
-        def mapper(self, _, line):
-        '''Parse each log line, extract and transform relevant lines.
+            strategy = _full_jitter_backoff(
+            retries=retries, delay=1, _random=random.Random(seed))
+        result = list(strategy())
     
-        def __init__(self, floor, total_spots):
-        self.floor = floor
-        self.num_spots = total_spots
-        self.available_spots = 0
-        self.spots = []  # List of ParkingSpots
+        def __init__(self, cards):
+        super(BlackJackHand, self).__init__(cards)
     
-            self.assertFalse(self.vhost2.conflicts([self.addr1,
-                                                self.addr_default]))
     
-        @mock.patch(
-        'certbot_compatibility_test.validator.crypto_util.probe_sni')
-    def test_certificate_error(self, mock_probe_sni):
-        cert = OpenSSL.crypto.X509()
-        mock_probe_sni.side_effect = [acme_errors.Error]
-        self.assertFalse(self.validator.certificate(
-            cert, 'test.com', '127.0.0.1'))
+def CheckSpacing(filename, clean_lines, linenum, nesting_state, error):
+  '''Checks for the correctness of various spacing issues in the code.
     
-    js.load_plugin_modules(plugins)
-modules = dict((k, m) for k, m in js.module.iteritems())
-print 'JS_MODULES := ' + ' '.join(modules.iterkeys())
-outputs = []
-for name, module in modules.iteritems():
-    outputs.extend(module.outputs)
-    print 'JS_MODULE_OUTPUTS_%s := %s' % (name, ' '.join(module.outputs))
-    print 'JS_MODULE_DEPS_%s := %s' % (name, ' '.join(module.dependencies))
+            If the named argument was not found, ``new_value`` will be added
+        to ``kwargs`` and None will be returned as ``old_value``.
+        '''
+        if self.arg_pos is not None and len(args) > self.arg_pos:
+            # The arg to replace is passed positionally
+            old_value = args[self.arg_pos]
+            args = list(args)  # *args is normally a tuple
+            args[self.arg_pos] = new_value
+        else:
+            # The arg to replace is either omitted or passed by keyword.
+            old_value = kwargs.get(self.name)
+            kwargs[self.name] = new_value
+        return old_value, args, kwargs
     
-        current_ref = get_current_ref()
-    base_ref = get_merge_base()
-    if options.files:
-        files = options.files
-    else:
-        files = get_changed_files(base_ref, current_ref)
-        logging.debug('files changed: %r', files)
+    import asyncio
     
-            if 'note' in notes_json:
-            note = notes_json['note'] or ''
-            if not rel_exists:
-                # If this is a newly created friend relationship,
-                # the cache needs to be updated before a note can
-                # be applied
-                c.user.friend_rels_cache(_update=True)
-            c.user.add_friend_note(friend, note)
-        rel_view = FriendTableItem(friend_rel)
-        return self.api_wrapper(FriendTableItemJsonTemplate().data(rel_view))
+            Waiting for event
+        About to set the event
+        Not waiting this time
+        Done
+    '''
+    def __init__(self):
+        self._value = False
+        self._waiters = set()
     
-        @validate(
-        container_id=VGTMContainerId('id')
-    )
-    def GET_jail(self, container_id):
-        return GoogleTagManagerJail(container_id=container_id).render()
+            .. deprecated:: 5.1
     
-    import json
-import os
+            self.hass.bus.listen = mock.MagicMock()
+        self.assertTrue(setup_component(self.hass, splunk.DOMAIN, config))
+        self.assertTrue(self.hass.bus.listen.called)
+        self.assertEqual(EVENT_STATE_CHANGED,
+                         self.hass.bus.listen.call_args_list[0][0][0])
+    
+        hass.services.register(DOMAIN, SERVICE_VOLUME_MUTE,
+                           lambda service:
+                           keyboard.tap_key(keyboard.volume_mute_key),
+                           schema=TAP_KEY_SCHEMA)
+    
+        # pylint: disable=no-self-use
+    def test_from_event(self):
+        '''Test converting event to db event.'''
+        event = ha.Event('test_event', {
+            'some_data': 15
+        })
+        assert event == Events.from_event(event).to_native()
