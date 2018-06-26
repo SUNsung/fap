4713aba9ed800be64c48968e6e8353a56bac6edb
SUNsung <weatherius@gmail.com>
2018-06-26 18:31:45 +0300

    Rewrite [54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb] File in: 2018-06-26 18:31:45

--- ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
+++ ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
@@ -1,107 +1,43 @@
 
         
-          # Setting the :extname option will control what extension (if any) is appended to the url for assets
-  def test_javascript_include_tag
-    assert_dom_equal '<script src='/foo.js'></script>',  javascript_include_tag('/foo')
-    assert_dom_equal '<script src='/foo'></script>',     javascript_include_tag('/foo', extname: false)
-    assert_dom_equal '<script src='/foo.bar'></script>', javascript_include_tag('/foo', extname: '.bar')
-  end
-    
-      def rescue_errors(e) raise e end
-    
-                        if callbacks = @subscribe_callbacks[chan]
-                      next_callback = callbacks.shift
-                      @event_loop.post(&next_callback) if next_callback
-                      @subscribe_callbacks.delete(chan) if callbacks.empty?
-                    end
-                  end
-                end
-    
-        assert subscriptions.verify
-  end
-    
-              channel.unsubscribe_from_channel
-        end
-    
-        def wait_for_close
-      @closed.wait(WAIT_WHEN_EXPECTING_EVENT)
-    end
-    
-        def on_error(message)
-      @errors << message
-    end
-  end
-    
-    require 'open-uri'
-require 'json'
-require 'strscan'
-require 'forwardable'
-require 'term/ansicolor'
-require 'fileutils'
-    
-      # Set to :debug to see everything in the log.
-  config.log_level = :info
-    
-    desc 'Test all Gemfiles from test/*.gemfile'
-task :test_all_gemfiles do
-  require 'term/ansicolor'
-  require 'pty'
-  require 'shellwords'
-  cmd      = 'bundle install --quiet && bundle exec rake --trace'
-  statuses = Dir.glob('./test/gemfiles/*{[!.lock]}').map do |gemfile|
-    env = {'BUNDLE_GEMFILE' => gemfile}
-    cmd_with_env = '  (#{env.map { |k, v| 'export #{k}=#{Shellwords.escape v}' } * ' '}; #{cmd})'
-    $stderr.puts Term::ANSIColor.cyan('Testing\n#{cmd_with_env}')
-    PTY.spawn(env, cmd) do |r, _w, pid|
-      begin
-        r.each_line { |l| puts l }
-      rescue Errno::EIO
-        # Errno:EIO error means that the process has finished giving output.
-      ensure
-        ::Process.wait pid
+          def recipients(payload = {})
+    emails = interpolated(payload)['recipients']
+    if emails.present?
+      if emails.is_a?(String)
+        [emails]
+      else
+        emails
       end
+    else
+      [user.email]
     end
-    [$? && $?.exitstatus == 0, cmd_with_env]
   end
-  failed_cmds = statuses.reject(&:first).map { |(_status, cmd_with_env)| cmd_with_env }
-  if failed_cmds.empty?
-    $stderr.puts Term::ANSIColor.green('Tests pass with all gemfiles')
-  else
-    $stderr.puts Term::ANSIColor.red('Failing (#{failed_cmds.size} / #{statuses.size})\n#{failed_cmds * '\n'}')
-    exit 1
+    
+      private
+    
+        respond_to do |format|
+      format.html { redirect_back events_path, notice: 'Event deleted.' }
+      format.json { head :no_content }
+    end
   end
-end
     
-            private
-    
-            def_node_matcher :simple_double_comparison?, '(send $lvar :== $lvar)'
-        def_node_matcher :simple_comparison?, <<-PATTERN
-          {(send $lvar :== _)
-           (send _ :== $lvar)}
-        PATTERN
-    
-              def find_plugins_gem_specs
-            @specs ||= ::Gem::Specification.find_all.select{|spec| logstash_plugin_gem_spec?(spec)}
+              # Parse the options
+          argv = parse_options(opts)
+          return if !argv
+          if argv.empty? || argv.length > 2
+            raise Vagrant::Errors::CLIInvalidUsage,
+              help: opts.help.chomp
           end
     
-        def define_instance_getter
-      name = @name
-      options = @options
+            def on_send(node)
+          return unless match_call?(node) &&
+                        (!node.value_used? || only_truthiness_matters?(node)) &&
+                        !(node.parent && node.parent.block_type?)
     
-          if defined?(ActiveRecord)
-        Paperclip.options[:logger] = ActiveRecord::Base.logger
-        ActiveRecord::Base.send(:include, Paperclip::Glue)
-      end
-    end
-  end
-end
-
+            context 'source with blank lines' do
+          let(:source) do
+            <<-RUBY.strip_indent
+              #{type} Parent
     
-          def validate_before_processing(validator_class, options)
-        options = options.dup
-        attributes = options.delete(:attributes)
-        attributes.each do |attribute|
-          options[:attributes] = [attribute]
-          create_validating_before_filter(attribute, validator_class, options)
-        end
-      end
+      context 'same_line style' do
+    let(:cop_config) { { 'EnforcedStyle' => 'same_line' } }
