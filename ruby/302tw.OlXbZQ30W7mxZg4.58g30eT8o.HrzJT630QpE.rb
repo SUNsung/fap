ac3b36bab6f7da47890a0dbc3ea2dd3e92e7d2c5
SUNsung <weatherius@gmail.com>
2018-06-26 09:33:25 +0300

    Rewrite [302tw.OlXbZQ30W7mxZg4.58g30eT8o.HrzJT630QpE.rb] File in: 2018-06-26 09:33:25

--- ruby/302tw.OlXbZQ30W7mxZg4.58g30eT8o.HrzJT630QpE.rb
+++ ruby/302tw.OlXbZQ30W7mxZg4.58g30eT8o.HrzJT630QpE.rb
@@ -1,162 +1,126 @@
 
         
-          test 'token_and_options returns correct token' do
-    token = 'rcHu+HzSFw89Ypyhn/896A=='
-    actual = ActionController::HttpAuthentication::Token.token_and_options(sample_request(token)).first
-    expected = token
-    assert_equal(expected, actual)
+          def relative_url_redirect_with_status_hash
+    redirect_to('/things/stuff', status: 301)
   end
     
-      class ExclaimerMiddleware
-    def initialize(app)
-      @app = app
-    end
+        # inherited only
+    assert_equal 'funky.jpg', @user.avatar.filename.to_s
     
-    module ActionMailer
-  # The <tt>ActionMailer::DeliveryJob</tt> class is used when you
-  # want to send emails outside of the request-response cycle.
-  #
-  # Exceptions are rescued and handled by the mailer class.
-  class DeliveryJob < ActiveJob::Base # :nodoc:
-    queue_as { ActionMailer::Base.deliver_later_queue_name }
+                      pg_conn.wait_for_notify(1) do |chan, pid, message|
+                    broadcast(chan, message)
+                  end
+                end
+              end
+            end
+          end
     
-        # Access the message instance.
-    def message
-      @_message
-    end
+      def test_clear_active_record_connections_adapter_still_works
+    server = ActionCable::Server::Base.new
+    server.config.cable = cable_config.with_indifferent_access
+    server.config.logger = Logger.new(StringIO.new).tap { |l| l.level = Logger::UNKNOWN }
     
-        class DeliveryJob < ActionMailer::DeliveryJob # :nodoc:
-      def perform(mailer, mail_method, delivery_method, params, *args)
-        mailer.constantize.with(params).public_send(mail_method, *args).send(delivery_method)
+            Rails.application
       end
+    ensure
+      ENV['RAILS_ENV'] = old_env
     end
+    
+        @connection.stub(:subscriptions, subscriptions) do
+      @channel = SecretChannel.new @connection, '{id: 1}', id: 1
+      @channel.subscribe_to_channel
+    
+      private
+    def subscribe_to_chat_channel(identifier = @chat_identifier)
+      @subscriptions.execute_command 'command' => 'subscribe', 'identifier' => identifier
+      assert_equal identifier, @subscriptions.identifiers.last
+    
+      class FakeConnection
+    def close
+    end
+  end
+    
+      def cache_fragment(name)
+    ApplicationSerializer.fragment_cache[name] ||= yield
   end
 end
 
     
-      def test_send_mail
-    stub_any_instance(Mail::SMTP, instance: Mail::SMTP.new({})) do |instance|
-      assert_called(instance, :deliver!) do
-        with_translation 'de', email_subject: '[Anmeldung] Willkommen' do
-          get '/test/send_mail'
-          assert_equal 'Mail sent - Subject: [Anmeldung] Willkommen', @response.body
-        end
-      end
-    end
-  end
+        private
     
-        mail_with_defaults do |format|
-      format.html { render(inline: '<%= block_format @text %>') }
-    end
-  end
-    
-        version 'C++' do
-      self.base_url = 'https://api.haxe.org/cpp/'
-    end
-    
-        version '6 LTS' do
-      self.release = '6.13.1'
-      self.base_url = 'https://nodejs.org/dist/latest-v6.x/docs/api/'
-    end
-    
-        process_font_assets
-    process_stylesheet_assets
-    process_javascript_assets
-    store_version
-  end
-    
-        alias log puts
-    
-      def test_image_helper
-    assert_match %r(url\(['']?/assets/apple-touch-icon-144-precomposed.*png['']?\)), @css
-  end
-    
-    namespace :bower do
-    
-      def create_issue(repo, title, body)
-    url = 'https://api.github.com/repos/#{repo}/issues'
-    data = { 'title' => title, 'body' => body }
-    scopes = GitHub::CREATE_ISSUE_FORK_OR_PR_SCOPES
-    GitHub.open_api(url, data: data, scopes: scopes)['html_url']
-  end
-    
-      def env_proc
-    self.class.env_proc
-  end
-    
-          # Get the cache key pair for the given Sass URI.
-      # The URI need not be checked for validity.
-      #
-      # The only strict requirement is that the returned pair of strings
-      # uniquely identify the file at the given URI.
-      # However, the first component generally corresponds roughly to the directory,
-      # and the second to the basename, of the URI.
-      #
-      # Note that keys must be unique *across importers*.
-      # Thus it's probably a good idea to include the importer name
-      # at the beginning of the first component.
-      #
-      # @param uri [String] A URI known to be valid for this importer.
-      # @param options [{Symbol => Object}] Options for the Sass file
-      #   containing the `@import` currently being checked.
-      # @return [(String, String)] The key pair which uniquely identifies
-      #   the file at the given URI.
-      def key(uri, options)
-        Sass::Util.abstract(self)
+        context 'GIT repository' do
+      before do
+        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_svn?).and_return(false)
+        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_git_svn?).and_return(false)
+        expect(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_git?).and_return(true)
+        allow(Fastlane::Actions::GetBuildNumberRepositoryAction).to receive(:is_hg?).and_return(false)
       end
     
-    (allow file-read-metadata)
-(allow file-read*
-  ; This is currenly only added because using `xcodebuild` to build a resource
-  ; bundle target starts a FSEvents stream on `/`. No idea why that would be
-  ; needed, but for now it doesn’t seem like a real problem.
-  (literal '/')
-  (regex
-    ; TODO see if we can restrict this more, but it's going to be hard
-    #'^/Users/[^.]+/*'
-    ;#'^/Users/[^.]+/.netrc'
-    ;#'^/Users/[^.]+/.gemrc'
-    ;#'^/Users/[^.]+/.gem/*'
-    ;#'^/Users/[^.]+/Library/.*'
-    #'^/Library/*'
-    #'^/System/Library/*'
-    #'^/usr/lib/*'
-    #'^/usr/share/*'
-    #'^/private/*'
-    #'^/dev/*'
-    #'^<%= ruby_prefix %>'
-    #'^<%= pod_prefix %>'
-    #'^<%= xcode_app_path %>'
-    #'^<%= Pod::Config.instance.repos_dir %>'
-<% prefixes.each do |prefix| %>
-    #'^<%= prefix %>/*'
-<% end %>
-  )
-)
+      # Check if there is no signed in user before doing the sign out.
+  #
+  # If there is no signed in user, it will set the flash message and redirect
+  # to the after_sign_out path.
+  def verify_signed_out_user
+    if all_signed_out?
+      set_flash_message! :notice, :already_signed_out
     
-            # Runs the template configuration utilities.
-        #
-        # @return [void]
-        #
-        def configure_template
-          UI.section('Configuring #{@name} template.') do
-            Dir.chdir(@name) do
-              if File.exist?('configure')
-                system({ 'COCOAPODS_VERSION' => Pod::VERSION }, './configure', @name, *@additional_args)
-              else
-                UI.warn 'Template does not have a configure file.'
-              end
-            end
-          end
+          def self.generate_helpers!(routes=nil)
+        routes ||= begin
+          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
+          Devise::URL_HELPERS.slice(*mappings)
         end
     
-      entries.each do |entry|
-    if File.exist?(entry[:file])
-      warn '[skip] #{entry[:file]} already exists'
-    else
-      File.open(entry[:file], 'w+') do |f|
-        f.write(ERB.new(File.read(entry[:template])).result(binding))
-        puts I18n.t(:written_file, scope: :capistrano, file: entry[:file])
+    module Homebrew
+  module_function
+    
+      # Overriding #satisfied? is unsupported.
+  # Pass a block or boolean to the satisfy DSL method instead.
+  def satisfied?
+    satisfy = self.class.satisfy
+    return true unless satisfy
+    @satisfied_result = satisfy.yielder { |p| instance_eval(&p) }
+    return false unless @satisfied_result
+    true
+  end
+    
+      def deprecated_option(hash)
+    raise ArgumentError, 'deprecated_option hash must not be empty' if hash.empty?
+    hash.each do |old_options, new_options|
+      Array(old_options).each do |old_option|
+        Array(new_options).each do |new_option|
+          deprecated_option = DeprecatedOption.new(old_option, new_option)
+          deprecated_options << deprecated_option
+    
+        def any_searchfield_present?
+      if %w(username email guid under13).all? { |attr| public_send(attr).blank? }
+        errors.add :base, 'no fields for search set'
       end
     end
-  end
+    
+          def call
+        title('Gems')
+        table(all_gem_names) do |gem, row|
+          row.yellow if update_available?(gem)
+          row << gem
+          row << installed_gem_version(gem)
+          row << '(update available)' if update_available?(gem)
+        end
+      end
+    
+          it 'filters by hosts and roles from the :filter variable' do
+        all = dsl.roles(:all)
+        SSHKit::Coordinator.expects(:new).with([]).returns(@coordinator)
+        dsl.set :filter, roles: 'db', hosts: 'example3.com'
+        dsl.on(all)
+      end
+    
+        orig_stdout = $stdout
+    orig_stderr = $stderr
+    captured_stdout = StringIO.new
+    captured_stderr = StringIO.new
+    $stdout = captured_stdout
+    $stderr = captured_stderr
+    
+          if File.symlink?(code_path)
+        return 'Code directory '#{code_path}' cannot be a symlink'
+      end
