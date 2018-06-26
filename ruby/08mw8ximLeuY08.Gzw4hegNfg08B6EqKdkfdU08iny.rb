ede18b951262b486e47e874b853daa01e30063e1
SUNsung <weatherius@gmail.com>
2018-06-26 09:07:21 +0300

    Rewrite [08mw8ximLeuY08.Gzw4hegNfg08B6EqKdkfdU08iny.rb] File in: 2018-06-26 09:07:21

--- ruby/08mw8ximLeuY08.Gzw4hegNfg08B6EqKdkfdU08iny.rb
+++ ruby/08mw8ximLeuY08.Gzw4hegNfg08B6EqKdkfdU08iny.rb
@@ -1,140 +1,102 @@
 
         
-            # Create a new Document.
-    #
-    # path - the path to the file
-    # relations - a hash with keys :site and :collection, the values of which
-    #             are the Jekyll::Site and Jekyll::Collection to which this
-    #             Document belong.
-    #
-    # Returns nothing.
-    def initialize(path, relations = {})
-      @site = relations[:site]
-      @path = path
-      @extname = File.extname(path)
-      @collection = relations[:collection]
-      @has_yaml_header = nil
+          # Full error reports are disabled and caching is turned on.
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
     
-      def test_freeze
-    m = Module.new do
-      def self.baz; end
-      def bar; end
-    end
-    m.freeze
-    assert_raise(FrozenError) do
-      m.module_eval do
-        def foo; end
+    desc 'Test all Gemfiles from test/*.gemfile'
+task :test_all_gemfiles do
+  require 'term/ansicolor'
+  require 'pty'
+  require 'shellwords'
+  cmd      = 'bundle install --quiet && bundle exec rake --trace'
+  statuses = Dir.glob('./test/gemfiles/*{[!.lock]}').map do |gemfile|
+    env = {'BUNDLE_GEMFILE' => gemfile}
+    cmd_with_env = '  (#{env.map { |k, v| 'export #{k}=#{Shellwords.escape v}' } * ' '}; #{cmd})'
+    $stderr.puts Term::ANSIColor.cyan('Testing\n#{cmd_with_env}')
+    PTY.spawn(env, cmd) do |r, _w, pid|
+      begin
+        r.each_line { |l| puts l }
+      rescue Errno::EIO
+        # Errno:EIO error means that the process has finished giving output.
+      ensure
+        ::Process.wait pid
       end
     end
-    assert_raise(FrozenError) do
-      m.__send__ :private, :bar
-    end
-    assert_raise(FrozenError) do
-      m.private_class_method :baz
-    end
+    [$? && $?.exitstatus == 0, cmd_with_env]
   end
-    
-      def test_shared_eval
-    bug7671 = '[ruby-core:51296]'
-    vs = (1..9).to_a
-    eval('vs.select {|n| if n==2..n==16 then 1 end}')
-    v = eval('vs.select {|n| if n==3..n==6 then 1 end}')
-    assert_equal([*3..6], v, bug7671)
+  failed_cmds = statuses.reject(&:first).map { |(_status, cmd_with_env)| cmd_with_env }
+  if failed_cmds.empty?
+    $stderr.puts Term::ANSIColor.green('Tests pass with all gemfiles')
+  else
+    $stderr.puts Term::ANSIColor.red('Failing (#{failed_cmds.size} / #{statuses.size})\n#{failed_cmds * '\n'}')
+    exit 1
   end
+end
     
-      def caller_lineno(*)
-    caller_locations(1, 1)[0].lineno
+          def accepts?(env)
+        cookie_header = env['HTTP_COOKIE']
+        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
+        cookies.each do |k, v|
+          if k == session_key && Array(v).size > 1
+            bad_cookies << k
+          elsif k != session_key && Rack::Utils.unescape(k) == session_key
+            bad_cookies << k
+          end
+        end
+        bad_cookies.empty?
+      end
+    
+        it 'accepts #{method} requests with whitelisted Origin' do
+      mock_app do
+        use Rack::Protection::HttpOrigin, :origin_whitelist => ['http://www.friend.com']
+        run DummyApp
+      end
+      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://www.friend.com')).to be_ok
+    end
   end
 end
 
     
-    describe 'CApiAllocTypedSpecs (a class with an alloc func defined)' do
-  it 'calls the alloc func' do
-    @s = CApiAllocTypedSpecs.new
-    @s.typed_wrapped_data.should == 42 # not defined in initialize
+      it 'comparison of Accept-Language header is not case sensitive' do
+    session = {:foo => :bar}
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'A'
+    expect(session).not_to be_empty
   end
-end
     
-      def test_gets_para_extended_file
-    [nil, {:textmode=>true}, {:binmode=>true}].each do |mode|
-      Tempfile.create('test-extended-file', mode) {|f|
-        assert_nil(f.getc)
-        f.print '\na'
-        f.rewind
-        assert_equal('a', f.gets(''), 'mode = <#{mode}>')
-      }
+        expect(get('/..', :foo => '<bar>')).to be_ok
+  end
+    
+              @meta_assignment_node
+        end
+    
+              each_misplaced_optional_arg(arguments) do |argument|
+            add_offense(argument)
+          end
+        end
+    
+      context 'same_line style' do
+    let(:cop_config) { { 'EnforcedStyle' => 'same_line' } }
+    
+          # Custom destructuring method. This can be used to normalize
+      # destructuring for different variations of the node.
+      #
+      # In this case, the `def` node destructures into:
+      #
+      #   `method_name, arguments, body`
+      #
+      # while the `defs` node destructures into:
+      #
+      #   `receiver, method_name, arguments, body`
+      #
+      # so we reverse the destructured array to get the optional receiver
+      # at the end, where it can be discarded.
+      #
+      # @return [Array] the different parts of the `def` or `defs` node
+      def node_parts
+        to_a.reverse
+      end
     end
   end
-    
-        ret = set.collect! { |i|
-      case i
-      when Numeric
-        i * 2
-      when String
-        i.upcase
-      else
-        nil
-      end
-    }
-    
-      it 'decodes two doubles for two format characters' do
-    '333333\x07@ffffff\xf6?'.unpack(unpack_format(nil, 2)).should == [2.9, 1.4]
-  end
-    
-      it 'ignores spaces between directives' do
-    array = 'abcdefghabghefcd'.unpack(unpack_format(' ', 2))
-    array.should == [7523094288207667809, 7233738012216484449]
-  end
 end
-    
-          def call
-        title('Gems')
-        table(all_gem_names) do |gem, row|
-          row.yellow if update_available?(gem)
-          row << gem
-          row << installed_gem_version(gem)
-          row << '(update available)' if update_available?(gem)
-        end
-      end
-    
-            it 'returns all servers' do
-          expect(subject.map(&:hostname)).to eq %w{example1.com example2.com example3.com example4.com example5.com}
-        end
-      end
-    
-      it 'displays documentation URL as help banner' do
-    expect(help_output.lines.first).to match(/capistranorb.com/)
-  end
-    
-    class SinatraStaticServer < Sinatra::Base
-    
-    Liquid::Template.register_tag('config_tag', ConfigTag)
-    
-    require './plugins/pygments_code'
-require './plugins/raw'
-require 'pathname'
-    
-      desc 'Build all spree gems'
-  task :build do
-    pkgdir = File.expand_path('../pkg', __FILE__)
-    FileUtils.mkdir_p pkgdir
-    
-            def index
-          authorize! :read, StockMovement
-          @stock_movements = scope.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
-          respond_with(@stock_movements)
-        end
-    
-      def as_boolean(string)
-    return true   if string == true   || string =~ (/(true|t|yes|y|1)$/i)
-    return false  if string == false  || string.blank? || string =~ (/(false|f|no|n|0)$/i)
-    raise ArgumentError.new('invalid value for Boolean: \'#{string}\'')
-  end
-    
-              def plugins
-            @plugins ||= find_plugins_gem_specs.map do |spec|
-              { :name => spec.name, :version => spec.version.to_s }
-            end.sort_by do |spec|
-              spec[:name]
-            end
-          end
