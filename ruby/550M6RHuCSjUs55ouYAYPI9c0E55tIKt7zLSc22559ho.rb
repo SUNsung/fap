6261bd95ff1cc3276c9a2737b2a69dcf0e7649e6
SUNsung <weatherius@gmail.com>
2018-06-26 16:08:01 +0300

    Rewrite [550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb] File in: 2018-06-26 16:08:01

--- ruby/550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb
+++ ruby/550M6RHuCSjUs55ouYAYPI9c0E55tIKt7zLSc22559ho.rb
@@ -1,193 +1,68 @@
 
         
-              should 'have basic attributes defined in it' do
-        regular_page = setup_page('properties.html', :klass => Page)
-        basic_attrs = %w(dir name path url)
-        attrs = {
-          'content'   => 'All the properties.\n',
-          'dir'       => '/',
-          'excerpt'   => nil,
-          'foo'       => 'bar',
-          'layout'    => 'default',
-          'name'      => 'properties.html',
-          'path'      => 'properties.html',
-          'permalink' => '/properties/',
-          'published' => nil,
-          'title'     => 'Properties Page',
-          'url'       => '/properties.html',
-        }
-        attrs.each do |prop, value|
-          # assert the props being accessible in a Jekyll::Page instance
-          assert_equal 'All the properties.\n', regular_page['content']
-          assert_equal 'properties.html', regular_page['name']
-    
-          process(name)
-      read_yaml(base, name)
-    end
-    
-          private
-    
-      # Clean a top-level (bin, sbin, lib) directory, recursively, by fixing file
-  # permissions and removing .la files, unless the files (or parent
-  # directories) are protected by skip_clean.
-  #
-  # bin and sbin should not have any subdirectories; if either do that is
-  # caught as an audit warning
-  #
-  # lib may have a large directory tree (see Erlang for instance), and
-  # clean_dir applies cleaning rules to the entire tree
-  def clean_dir(d)
-    d.find do |path|
-      path.extend(ObserverPathnameExtension)
-    
-      def filtered_list
-    names = if ARGV.named.empty?
-      Formula.racks
-    else
-      ARGV.named.map { |n| HOMEBREW_CELLAR+n }.select(&:exist?)
-    end
-    if ARGV.include? '--pinned'
-      pinned_versions = {}
-      names.each do |d|
-        keg_pin = (HOMEBREW_LIBRARY/'PinnedKegs'/d.basename.to_s)
-        if keg_pin.exist? || keg_pin.symlink?
-          pinned_versions[d] = keg_pin.readlink.basename.to_s
-        end
-      end
-      pinned_versions.each do |d, version|
-        puts '#{d.basename}'.concat(ARGV.include?('--versions') ? ' #{version}' : '')
-      end
-    else # --versions without --pinned
-      names.each do |d|
-        versions = d.subdirs.map { |pn| pn.basename.to_s }
-        next if ARGV.include?('--multiple') && versions.length < 2
-        puts '#{d.basename} #{versions*' '}'
-      end
+          def post_superenv_hacks
+    # Only allow Homebrew-approved directories into the PATH, unless
+    # a formula opts-in to allowing the user's path.
+    if formula.env.userpaths? || reqs.any? { |rq| rq.env.userpaths? }
+      ENV.userpaths!
     end
   end
-end
     
-      def python(_options = {}, &block)
-    opoo 'Formula#python is deprecated and will go away shortly.'
-    block.call if block_given?
-    PythonRequirement.new
+      # True if a {Formula} is being built universally.
+  # e.g. on newer Intel Macs this means a combined x86_64/x86 binary/library.
+  # <pre>args << '--universal-binary' if build.universal?</pre>
+  def universal?
+    include?('universal') && option_defined?('universal')
   end
-  alias_method :python2, :python
-  alias_method :python3, :python
-end
-
     
-    # This formula serves as the base class for several very similar
-# formulae for Amazon Web Services related tools.
-class AmazonWebServicesFormula < Formula
-  # Use this method to peform a standard install for Java-based tools,
-  # keeping the .jars out of HOMEBREW_PREFIX/lib
-  def install
-    rm Dir['bin/*.cmd'] # Remove Windows versions
-    libexec.install Dir['*']
-    bin.install_symlink Dir['#{libexec}/bin/*'] - ['#{libexec}/bin/service']
-  end
-  alias_method :standard_install, :install
-    
-          # Reads data from an IO object while it can, returning the data it reads.
-      # When it encounters a case when it can't read anymore, it returns the
-      # data.
-      #
-      # @return [String]
-      def self.read_until_block(io)
-        data = ''
-    
-          # This returns the keys (or ids) that are in the string.
-      #
-      # @return [<Array<String>]
-      def keys
-        regexp = /^#\s*VAGRANT-BEGIN:\s*(.+?)$\r?\n?(.*)$\r?\n?^#\s*VAGRANT-END:\s(\1)$/m
-        @value.scan(regexp).map do |match|
-          match[0]
-        end
-      end
-    
-        previous_tag = ARGV.named.first
-    previous_tag ||= Utils.popen_read(
-      'git', '-C', HOMEBREW_REPOSITORY, 'tag', '--list', '--sort=-version:refname'
-    ).lines.first.chomp
-    odie 'Could not find any previous tags!' unless previous_tag
-    
-      def dump_build_env(env, f = $stdout)
-    keys = build_env_keys(env)
-    keys -= %w[CC CXX OBJC OBJCXX] if env['CC'] == env['HOMEBREW_CC']
-    
-              # If mounting without agreeing to EULA succeeded, there is none.
-          plist = if without_eula.success?
-            without_eula.plist
-          else
-            @command.run!('/usr/bin/hdiutil', args: ['convert', '-quiet', '-format', 'UDTO', '-o', cdr_path, path])
-    
-          # Find a Sass file, if it exists.
-      #
-      # This is the primary entry point of the Importer.
-      # It corresponds directly to an `@import` statement in Sass.
-      # It should do three basic things:
-      #
-      # * Determine if the URI is in this importer's format.
-      #   If not, return nil.
-      # * Determine if the file indicated by the URI actually exists and is readable.
-      #   If not, return nil.
-      # * Read the file and place the contents in a {Sass::Engine}.
-      #   Return that engine.
-      #
-      # If this importer's format allows for file extensions,
-      # it should treat them the same way as the default {Filesystem} importer.
-      # If the URI explicitly has a `.sass` or `.scss` filename,
-      # the importer should look for that exact file
-      # and import it as the syntax indicated.
-      # If it doesn't exist, the importer should return nil.
-      #
-      # If the URI doesn't have either of these extensions,
-      # the importer should look for files with the extensions.
-      # If no such files exist, it should return nil.
-      #
-      # The {Sass::Engine} to be returned should be passed `options`,
-      # with a few modifications. `:syntax` should be set appropriately,
-      # `:filename` should be set to `uri`,
-      # and `:importer` should be set to this importer.
-      #
-      # @param uri [String] The URI to import.
-      # @param options [{Symbol => Object}] Options for the Sass file
-      #   containing the `@import` that's currently being resolved.
-      #   This is safe for subclasses to modify destructively.
-      #   Callers should only pass in a value they don't mind being destructively modified.
-      # @return [Sass::Engine, nil] An Engine containing the imported file,
-      #   or nil if it couldn't be found or was in the wrong format.
-      def find(uri, options)
-        Sass::Util.abstract(self)
-      end
-    
-            p environment.var(name)
-      else
-        p Script::Parser.parse(text, @line, 0).perform(environment)
-      end
-    rescue Sass::SyntaxError => e
-      puts 'SyntaxError: #{e.message}'
-      if @options[:trace]
-        e.backtrace.each do |line|
-          puts '\tfrom #{line}'
-        end
+        def self.cleanup_cellar
+      Formula.installed.each do |formula|
+        cleanup_formula formula
       end
     end
-  end
-end
-
     
-          def remove_bad_cookies(request, response)
-        return if bad_cookies.empty?
-        paths = cookie_paths(request.path)
-        bad_cookies.each do |name|
-          paths.each { |path| response.set_cookie name, empty_cookie(request.host, path) }
-        end
+      def external_commands
+    paths.reduce([]) do |cmds, path|
+      Dir['#{path}/brew-*'].each do |file|
+        next unless File.executable?(file)
+        cmd = File.basename(file, '.rb')[5..-1]
+        cmds << cmd unless cmd.include?('.')
+      end
+      cmds
+    end.sort
+  end
+    
+        updated_taps = []
+    Tap.each do |tap|
+      next unless tap.git?
+      begin
+        reporter = Reporter.new(tap)
+      rescue Reporter::ReporterRevisionUnsetError => e
+        onoe e if ARGV.homebrew_developer?
+        next
+      end
+      if reporter.updated?
+        updated_taps << tap.name
+        hub.add(reporter)
+      end
+    end
+    
+      def present(payload)
+    if payload.is_a?(Hash)
+      payload = ActiveSupport::HashWithIndifferentAccess.new(payload)
+      MAIN_KEYS.each do |key|
+        return { :title => payload[key].to_s, :entries => present_hash(payload, key) } if payload.has_key?(key)
       end
     
-      context 'escaping' do
-    before do
-      mock_app { |e| [200, {'Content-Type' => 'text/plain'}, [e['PATH_INFO']]] }
+      def validate_evernote_options
+    unless evernote_consumer_key.present? &&
+      evernote_consumer_secret.present? &&
+      evernote_oauth_token.present?
+      errors.add(:base, 'Evernote ENV variables and a Service are required')
     end
+  end
+    
+      if ARGV.include? '--no-ansi'
+    STDERR.puts <<-DOC
+    WARNING: CocoaPods requires your terminal to be using UTF-8 encoding.
+    Consider adding the following to ~/.profile:
