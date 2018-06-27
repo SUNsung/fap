68710faa1b257693160431bc569681083b6a2748
SUNsung <weatherius@gmail.com>
2018-06-28 02:12:15 +0300

    Rewrite [00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py] File in: 2018-06-28 02:12:14

--- pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
+++ pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
@@ -1,32 +1,3 @@
 
         
         
-def _is_tar_extract(cmd):
-    if '--extract' in cmd:
-        return True
-    
-        def get_testable_domain_names(self):
-        '''Returns the set of domain names that can be tested against'''
-        if self._test_names:
-            return self._test_names
-        else:
-            return {'example.com'}
-    
-        @mock.patch(
-        'certbot_compatibility_test.validator.crypto_util.probe_sni')
-    def test_certificate_success(self, mock_probe_sni):
-        cert = OpenSSL.crypto.X509()
-        mock_probe_sni.return_value = cert
-        self.assertTrue(self.validator.certificate(
-            cert, 'test.com', '127.0.0.1'))
-    
-    # Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'certbot-nginx', u'certbot-nginx Documentation',
-     author, 'certbot-nginx', 'One line description of project.',
-     'Miscellaneous'),
-]
-    
-        description = 'Example Installer plugin'
