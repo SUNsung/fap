cfaf51b5c302e60549c73bd84aa1b83a597957a0
SUNsung <weatherius@gmail.com>
2018-06-28 00:39:05 +0300

    Rewrite [18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp] File in: 2018-06-28 00:39:05

--- cccccc/18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp
+++ cccccc/18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp
@@ -1,205 +1,367 @@
 
         
-        PyObject* NewMessageEnumsByName(const Descriptor* descriptor);
-PyObject* NewMessageEnumsSeq(const Descriptor* descriptor);
-PyObject* NewMessageEnumValuesByName(const Descriptor* descriptor);
-    
-    
-    {
-    {}  // namespace python
-}  // namespace protobuf
-    
-    
-    {  ASSERT_TRUE(message.ParseFromString(data));
-  EXPECT_TRUE(message.has_any_value());
-  ASSERT_TRUE(message.any_value().UnpackTo(&submessage));
-  EXPECT_EQ(12345, submessage.int32_value());
-}
-    
-    class EnumGenerator : public SourceGeneratorBase {
- public:
-  EnumGenerator(const EnumDescriptor* descriptor, const Options* options);
-  ~EnumGenerator();
-    }
-    
-    
-    
-    #include <google/protobuf/compiler/code_generator.h>
-#include <google/protobuf/compiler/plugin.h>
-#include <google/protobuf/descriptor.h>
-#include <google/protobuf/descriptor.pb.h>
-#include <google/protobuf/io/printer.h>
-#include <google/protobuf/io/zero_copy_stream.h>
-#include <google/protobuf/wire_format.h>
-    
-    
-    
-    struct Options;
-    
-    void WriteMessageDocComment(io::Printer* printer, const Descriptor* message);
-void WriteFieldDocComment(io::Printer* printer, const FieldDescriptor* field);
-void WriteEnumDocComment(io::Printer* printer, const EnumDescriptor* enum_);
-void WriteEnumValueDocComment(io::Printer* printer,
-                              const EnumValueDescriptor* value);
-void WriteServiceDocComment(io::Printer* printer,
-                            const ServiceDescriptor* service);
-void WriteMethodDocComment(io::Printer* printer,
-                           const MethodDescriptor* method);
-    
-    class RepeatedImmutableLazyMessageFieldGenerator
-    : public RepeatedImmutableMessageFieldGenerator {
- public:
-  explicit RepeatedImmutableLazyMessageFieldGenerator(
-      const FieldDescriptor* descriptor, int messageBitIndex,
-      int builderBitIndex, Context* context);
-  ~RepeatedImmutableLazyMessageFieldGenerator();
-    }
-    
-    #include <memory>
-    
-    
-    {
-    {}  // namespace testing
-}  // namespace grpc
-    
-    template<> inline
-dnnError_t dnnGroupsConvolutionCreateBackwardFilter<double>(
-    dnnPrimitive_t* pConvolution,
-    dnnPrimitiveAttributes_t attributes,
-    dnnAlgorithm_t algorithm,
-    size_t groups,
-    size_t dimension,
-    const size_t srcSize[],
-    const size_t dstSize[],
-    const size_t filterSize[],
-    const size_t convolutionStrides[],
-    const int inputOffset[],
-    const dnnBorder_t border_type)
-{
-    return dnnGroupsConvolutionCreateBackwardFilter_F64(
-        pConvolution,
-        attributes,
-        algorithm,
-        groups,
-        dimension,
-        srcSize,
-        dstSize,
-        filterSize,
-        convolutionStrides,
-        inputOffset,
-        border_type);
-}
-    
-                // The assumption here is that a sequence always start at 0 (no invaid mark at the beginning),
-            // and ends at the first invalid mask. 
-            // Therefore, no need to check NDMask again.
-            // And the sequences has been resized to match the number of sequences and the length of each sequence in the Value object.
-    
-    // replace the old node with the current node, assuming the old node is a leaf node
-// need to update those nodes who use oldNode as their child
-// TODO: Can this be called with a node that's already part of the network? This is currently allowed, but should it?
-// BUGBUG: Seems ReplaceNode() also updates node groups. Why doesn't this function?
-// BUGBUG: What if newNode is the one referenced by oldNodeName?
-// BUGBUG: Or what if an unrelated node of the same name exists?
-void ComputationNetwork::ReplaceLeafNode(wstring oldNodeName, ComputationNodeBasePtr newNode)
-{
-    InvalidateCompiledNetwork();
-    }
-    
-    void GranularGPUDataTransferer::WaitForSyncPointOnFetchStreamAsync()
-{
-    PrepareDevice(m_deviceId);
-    cudaStreamWaitEvent(GetFetchStream(), m_syncEvent, 0 /*flags 'must be 0'*/) || 'cudaStreamWaitEvent failed';
-}
-    
-    // Compares two ASCII strings ignoring the case.
-// TODO: Should switch to boost, boost::iequal should be used instead.
-// TODO: we already have EqualCI() in Basics.h which does the same thing.
-template<class TElement>
-inline bool AreEqualIgnoreCase(
-    const std::basic_string<TElement, char_traits<TElement>, allocator<TElement>>& s1,
-    const std::basic_string<TElement, char_traits<TElement>, allocator<TElement> >& s2)
-{
-    if (s1.size() != s2.size())
-    {
-        return false;
-    }
-    }
-    
-    class latticepair : public std::pair<msra::lattices::lattice, msra::lattices::lattice>
-{
-public:
-    // NOTE: we don't check numerator lattice now
-    size_t getnumframes() const
-    {
-        return second.getnumframes();
-    }
-    size_t getnumnodes() const
-    {
-        return second.getnumnodes();
-    }
-    size_t getnumedges() const
-    {
-        return second.getnumedges();
-    }
-    std::wstring getkey() const
-    {
-        return second.getkey();
-    }
+        
+    {  DISALLOW_COPY_AND_ASSIGN(RemoteCallbackFreer);
 };
     
-    YGFloatOptional YGNode::getLeadingPaddingAndBorder(
-    const YGFlexDirection& axis,
-    const float& widthSize) const {
-  return getLeadingPadding(axis, widthSize) +
-      YGFloatOptional(getLeadingBorder(axis));
+      // Printing ----------------------------------------------------------------
+    
+    
+    {}  // namespace chrome
+    
+    
+    {  virtual int GetSourceCount() const = 0;
+  virtual const Source& GetSource(int index) const = 0;
+  virtual std::vector<Source> GetSources() const = 0;
+};
+    
+      // Return the parent NativeView of the observed WebContents.
+  gfx::NativeView GetParentView();
+    
+    void TtsPlatformImpl::WillSpeakUtteranceWithVoice(const Utterance* utterance,
+                                                  const VoiceData& voice_data) {
 }
     
-      // Other methods
-  YGValue marginLeadingValue(const YGFlexDirection axis) const;
-  YGValue marginTrailingValue(const YGFlexDirection axis) const;
-  YGValue resolveFlexBasisPtr() const;
-  void resolveDimension();
-  YGDirection resolveDirection(const YGDirection ownerDirection);
-  void clearChildren();
-  /// Replaces the occurrences of oldChild with newChild
-  void replaceChild(YGNodeRef oldChild, YGNodeRef newChild);
-  void replaceChild(YGNodeRef child, uint32_t index);
-  void insertChild(YGNodeRef child, uint32_t index);
-  /// Removes the first occurrence of child
-  bool removeChild(YGNodeRef child);
-  void removeChild(uint32_t index);
     
-    #include <yoga/Yoga.h>
-    
-    #include <dlfcn.h>
-#include <unwind.h>
-    
-    TEST_F(YogaTest_HadOverflowTests, spacing_overflow_no_wrap_and_no_flex_children) {
-  const YGNodeRef child0 = YGNodeNewWithConfig(config);
-  YGNodeStyleSetWidth(child0, 80);
-  YGNodeStyleSetHeight(child0, 40);
-  YGNodeStyleSetMargin(child0, YGEdgeTop, 10);
-  YGNodeStyleSetMargin(child0, YGEdgeBottom, 10);
-  YGNodeInsertChild(root, child0, 0);
-  const YGNodeRef child1 = YGNodeNewWithConfig(config);
-  YGNodeStyleSetWidth(child1, 80);
-  YGNodeStyleSetHeight(child1, 40);
-  YGNodeStyleSetMargin(child1, YGEdgeBottom, 5);
-  YGNodeInsertChild(root, child1, 1);
+    {
+    {    if (zoom_controller) {
+      double zoom_factor = content::ZoomLevelToZoomFactor(zoom_controller->GetZoomLevel());
+      if (zoom_factor > content::kMaximumZoomFactor) {
+        zoom_factor = content::kMaximumZoomFactor;
+      }
+      if (zoom_factor < content::kMinimumZoomFactor) {
+        zoom_factor = content::kMinimumZoomFactor;
+      }
+      x *= zoom_factor;
+      y *= zoom_factor;
     }
     
-        method(setFlex);
-    method(setFlexBasis);
-    method(setFlexBasisPercent);
-    method(setFlexGrow);
-    method(setFlexShrink);
+    Popup(x, y, rvh);
+  } else if (method == 'EnableShowEvent') {
+    arguments.GetBoolean(0, &enable_show_event_);
+  } else {
+    NOTREACHED() << 'Invalid call to Menu method:' << method
+                 << ' arguments:' << arguments;
+  }
+}
     
-      void reset(T* other = NULL) {
-    T* old = (T*)pthread_getspecific(m_key);
-    if (old != other) {
-      FBASSERT(m_cleanup);
-      m_cleanup(old);
-      pthread_setspecific(m_key, other);
+    class ObjectManager;
+    
+    
+    {}
+    
+      // Called when the |display|'s bound has changed.
+  void NwScreenDisplayObserver::OnDisplayMetricsChanged(const display::Display& display,
+    uint32_t changed_metrics) {
+    std::unique_ptr<base::ListValue> args = 
+      nwapi::nw__screen::OnDisplayBoundsChanged::Create(*ConvertGfxDisplay(display),
+                                                        changed_metrics);
+    DispatchEvent(
+      events::HistogramValue::UNKNOWN, 
+      nwapi::nw__screen::OnDisplayBoundsChanged::kEventName,
+      std::move(args));
+  }
+    
+    
+    {  LOG(INFO) << 'Writing Testing data';
+  scoped_ptr<db::DB> test_db(db::GetDB(db_type));
+  test_db->Open(output_folder + '/cifar10_test_' + db_type, db::NEW);
+  txn.reset(test_db->NewTransaction());
+  // Open files
+  std::ifstream data_file((input_folder + '/test_batch.bin').c_str(),
+      std::ios::in | std::ios::binary);
+  CHECK(data_file) << 'Unable to open test file.';
+  for (int itemid = 0; itemid < kCIFARBatchSize; ++itemid) {
+    read_image(&data_file, &label, str_buffer);
+    datum.set_label(label);
+    datum.set_data(str_buffer, kCIFARImageNBytes);
+    string out;
+    CHECK(datum.SerializeToString(&out));
+    txn->Put(caffe::format_int(itemid, 5), out);
+  }
+  txn->Commit();
+  test_db->Close();
+}
+    
+    
+    { protected:
+  /**
+   * @param bottom input Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$
+   * @param top output Blob vector (length 1)
+   *   -# @f$ (N \times 1 \times K) @f$ or, if out_max_val
+   *      @f$ (N \times 2 \times K) @f$ unless axis set than e.g.
+   *      @f$ (N \times K \times H \times W) @f$ if axis == 1
+   *      the computed outputs @f$
+   *       y_n = \arg\max\limits_i x_{ni}
+   *      @f$ (for @f$ K = 1 @f$).
+   */
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  /// @brief Not implemented (non-differentiable function)
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+    NOT_IMPLEMENTED;
+  }
+  bool out_max_val_;
+  size_t top_k_;
+  bool has_axis_;
+  int axis_;
+};
+    
+    
+    {}  // namespace caffe
+    
+      vector<cudnnTensorDescriptor_t> bottom_descs_, top_descs_;
+  cudnnTensorDescriptor_t    bias_desc_;
+  cudnnFilterDescriptor_t      filter_desc_;
+  vector<cudnnConvolutionDescriptor_t> conv_descs_;
+  int bottom_offset_, top_offset_, bias_offset_;
+    
+      bool handles_setup_;
+  cudnnHandle_t             handle_;
+  cudnnLRNDescriptor_t norm_desc_;
+  cudnnTensorDescriptor_t bottom_desc_, top_desc_;
+    
+    #include <vector>
+    
+    #include <vector>
+    
+      void allGather(std::vector<at::Tensor>& output,
+                 std::vector<at::Tensor>& input,
+                 THDGroup group_id = THDGroupWORLD) override;
+  void allGather(std::vector<at::Tensor>& output, at::Tensor& input,
+                 THDGroup group_id = THDGroupWORLD) override;
+  void gather(std::vector<at::Tensor>& output, at::Tensor& input,
+              rank_type dst_rank, THDGroup group_id = THDGroupWORLD) override;
+  void scatter(std::vector<at::Tensor>& input, at::Tensor& output,
+               rank_type src_rank, THDGroup group_id = THDGroupWORLD) override;
+  void allReduce(std::vector<at::Tensor>& data,
+                 THDReduceOp operation,
+                 THDGroup group_id = THDGroupWORLD) override;
+  void allReduce(at::Tensor& data, THDReduceOp operation,
+                 THDGroup group_id = THDGroupWORLD) override;
+  void reduce(std::vector<at::Tensor>& data,
+              THDReduceOp operation,
+              rank_type dstRank,
+              THDGroup group_id = THDGroupWORLD) override;
+  void reduce(at::Tensor& data, THDReduceOp operation, rank_type dst_rank,
+              THDGroup group_id = THDGroupWORLD) override;
+  void broadcast(std::vector<at::Tensor>& data,
+                 rank_type srcRank,
+                 THDGroup group_id = THDGroupWORLD) override;
+  void broadcast(at::Tensor& data, rank_type src_id,
+                 THDGroup group_id = THDGroupWORLD) override;
+  void send(Scalar& data, rank_type dst_id) override;
+  void send(at::Tensor& data, rank_type dst_id) override;
+  void receive(Scalar& data, rank_type src_id) override;
+  rank_type receive(at::Tensor& data) override;
+  void receive(at::Tensor& data, rank_type src_id) override;
+  RequestGloo* isend(at::Tensor& data, rank_type dst_rank) override;
+  RequestGloo* ireceive(at::Tensor& data, rank_type src_rank) override;
+    
+    #define THCPStorage TH_CONCAT_3(THCP,Real,Storage)
+#define THCPStorageStr TH_CONCAT_STRING_3(torch.cuda.,Real,Storage)
+#define THCPStorageClass TH_CONCAT_3(THCP,Real,StorageClass)
+#define THCPStorage_(NAME) TH_CONCAT_4(THCP,Real,Storage_,NAME)
+    
+    #define THPDoubleStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPDoubleStorageClass)
+#define THPFloatStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPFloatStorageClass)
+#define THPHalfStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPFloatStorageClass)
+#define THPLongStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPLongStorageClass)
+#define THPIntStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPIntStorageClass)
+#define THPShortStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPShortStorageClass)
+#define THPCharStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPCharStorageClass)
+#define THPByteStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPByteStorageClass)
+    
+      int timeout = -1;
+  std::vector<int> to_add;
+  std::vector<int> to_remove;
+  for (;;) {
+    int nevents;
+    if (client_sessions.size() == 0)
+      timeout = SHUTDOWN_TIMEOUT;
+    SYSCHECK(nevents = poll(pollfds.data(), pollfds.size(), timeout));
+    timeout = -1;
+    if (nevents == 0 && client_sessions.size() == 0)
+      break;
+    }
+    
+    void THP_decodeDoubleBuffer(double* dst, const uint8_t* src, THPByteOrder order, size_t len)
+{
+  for (size_t i = 0; i < len; i++) {
+    union { uint64_t x; double d; };
+    x = (order == THP_BIG_ENDIAN ? decodeUInt64BE(src) : decodeUInt64LE(src));
+    dst[i] = d;
+    src += sizeof(double);
+  }
+}
+    
+    void DCT1d(const double* in, int stride, double* out) {
+  for (int x = 0; x < 8; ++x) {
+    out[x * stride] = 0.0;
+    for (int u = 0; u < 8; ++u) {
+      out[x * stride] += kDCTMatrix[8 * x + u] * in[u * stride];
     }
   }
+}
+    
+    // A node of a Huffman tree.
+struct HuffmanTree {
+  HuffmanTree() {}
+  HuffmanTree(uint32_t count, int16_t left, int16_t right)
+      : total_count_(count),
+        index_left_(left),
+        index_right_or_value_(right) {
+  }
+  uint32_t total_count_;
+  int16_t index_left_;
+  int16_t index_right_or_value_;
+};
+    
+    #include 'guetzli/jpeg_data.h'
+    
+      tmp0 = in[5 * stride];
+  tmp1 = kIDCTMatrix[ 5] * tmp0;
+  tmp2 = kIDCTMatrix[13] * tmp0;
+  tmp3 = kIDCTMatrix[21] * tmp0;
+  tmp4 = kIDCTMatrix[29] * tmp0;
+  out[0] += tmp1;
+  out[1] += tmp2;
+  out[2] += tmp3;
+  out[3] += tmp4;
+  out[4] -= tmp4;
+  out[5] -= tmp3;
+  out[6] -= tmp2;
+  out[7] -= tmp1;
+    
+    void BuildSequentialHuffmanCodes(
+    const JPEGData& jpg, std::vector<HuffmanCodeTable>* dc_huffman_code_tables,
+    std::vector<HuffmanCodeTable>* ac_huffman_code_tables);
+    
+    // Utility function for building a Huffman lookup table for the jpeg decoder.
+    
+    //
+// Exception tracer library.
+    
+    
+    {} // namespace folly
+
+    
+      ~AtomicCoreCachedSharedPtr() {
+    auto slots = slots_.load(std::memory_order_acquire);
+    // Delete of AtomicCoreCachedSharedPtr must be synchronized, no
+    // need for stlots->retire().
+    if (slots) {
+      delete slots;
+    }
+  }
+    
+      sorted_vector_set<int> forward_initialized_set{1, 2};
+  sorted_vector_set<int> backward_initialized_set{2, 1};
+  EXPECT_EQ(2, forward_initialized_set.size());
+  EXPECT_EQ(1, *forward_initialized_set.begin());
+  EXPECT_EQ(2, *forward_initialized_set.rbegin());
+  EXPECT_TRUE(forward_initialized_set == backward_initialized_set);
+    
+    TEST_F(UtilityTest, as_const) {
+  struct S {
+    bool member() {
+      return false;
+    }
+    bool member() const {
+      return true;
+    }
+  };
+  S s;
+  EXPECT_FALSE(s.member());
+  EXPECT_TRUE(folly::as_const(s).member());
+  EXPECT_EQ(&s, &folly::as_const(s));
+  EXPECT_TRUE(noexcept(folly::as_const(s)));
+}
+    
+    public:
+  AbstractDiskWriter(const std::string& filename);
+  virtual ~AbstractDiskWriter();
+    
+    bool AbstractHttpServerResponseCommand::execute()
+{
+  if (e_->getRequestGroupMan()->downloadFinished() || e_->isHaltRequested()) {
+    return true;
+  }
+  try {
+    ssize_t len = httpServer_->sendResponse();
+    if (len > 0) {
+      timeoutTimer_ = global::wallclock();
+    }
+  }
+  catch (RecoverableException& e) {
+    A2_LOG_INFO_EX(fmt('CUID#%' PRId64
+                       ' - Error occurred while transmitting response body.',
+                       getCuid()),
+                   e);
+    return true;
+  }
+  if (httpServer_->sendBufferIsEmpty()) {
+    A2_LOG_INFO(fmt('CUID#%' PRId64 ' - HttpServer: all response transmitted.',
+                    getCuid()));
+    afterSend(httpServer_, e_);
+    return true;
+  }
+  else {
+    if (timeoutTimer_.difference(global::wallclock()) >= 30_s) {
+      A2_LOG_INFO(fmt('CUID#%' PRId64
+                      ' - HttpServer: Timeout while trasmitting response.',
+                      getCuid()));
+      return true;
+    }
+    else {
+      updateReadWriteCheck();
+      e_->addCommand(std::unique_ptr<Command>(this));
+      return false;
+    }
+  }
+}
+    
+      virtual bool execute() CXX11_OVERRIDE;
+    
+    namespace {
+class FindCompletedAllowedTier {
+public:
+  bool operator()(const std::shared_ptr<AnnounceTier>& tier) const
+  {
+    switch (tier->event) {
+    case AnnounceTier::DOWNLOADING:
+    case AnnounceTier::COMPLETED:
+      return true;
+    default:
+      return false;
+    }
+  }
+};
+} // namespace
+    
+    
+    {} // namespace aria2
+
+    
+      virtual void setVerifyPeer(bool verify) CXX11_OVERRIDE
+  {
+    verifyPeer_ = verify;
+  }
+    
+    std::ostream& operator<<(std::ostream& o,
+                         const std::shared_ptr<AuthConfig>& authConfig)
+{
+  o << authConfig->getAuthText();
+  return o;
+}
