496cdc011e9761601a92e456c6a742298203f474
SUNsung <weatherius@gmail.com>
2018-06-27 08:17:02 +0300

    Rewrite [31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb] File in: 2018-06-27 08:17:01

--- ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
+++ ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
@@ -1,186 +1,125 @@
 
         
-                def start_callback(detached)
-          unless detached
-            proc do
-              mutex.synchronize do
-                # Block until EventMachine reactor starts
-                @reload_reactor&.started_event&.wait
-                @running = true
-                Jekyll.logger.info('Server running...', 'press ctrl-c to stop.')
-                @run_cond.broadcast
-              end
-            end
-          end
+                def test_url_memory_db_for_sqlite3
+          spec = resolve 'sqlite3::memory:'
+          assert_equal(':memory:', spec['database'])
         end
     
-          def convert(content)
-        document = Kramdown::Document.new(content, @config)
-        html_output = document.to_html.chomp
-        if @config['show_warnings']
-          document.warnings.each do |warning|
-            Jekyll.logger.warn 'Kramdown warning:', warning.sub(%r!^Warning:\s+!, '')
-          end
-        end
-        html_output
+          def on_delete
+        options[:on_delete]
       end
-    end
-  end
-end
-
     
-        # Determine which converter to use based on this convertible's
-    # extension.
+            class Listener < SubscriberMap
+          def initialize(adapter, event_loop)
+            super()
+    
+      def test_clear_active_record_connections_adapter_still_works
+    server = ActionCable::Server::Base.new
+    server.config.cable = cable_config.with_indifferent_access
+    server.config.logger = Logger.new(StringIO.new).tap { |l| l.level = Logger::UNKNOWN }
+    
+          env = Rack::MockRequest.env_for '/test', 'HTTP_CONNECTION' => 'upgrade', 'HTTP_UPGRADE' => 'websocket',
+        'HTTP_HOST' => 'localhost', 'HTTP_ORIGIN' => 'http://rubyonrails.com'
+    
+    require 'test_helper'
+require 'stubs/test_server'
+    
+          broadcasting = 'test_queue'
+      message = { body: 'test message' }
+      server.broadcast(broadcasting, message)
+    
+      def failure_message
+    exception = request.respond_to?(:get_header) ? request.get_header('omniauth.error') : request.env['omniauth.error']
+    error   = exception.error_reason if exception.respond_to?(:error_reason)
+    error ||= exception.error        if exception.respond_to?(:error)
+    error ||= (request.respond_to?(:get_header) ? request.get_header('omniauth.error.type') : request.env['omniauth.error.type']).to_s
+    error.to_s.humanize if error
+  end
+    
+    class Devise::PasswordsController < DeviseController
+  prepend_before_action :require_no_authentication
+  # Render the #edit only if coming from a reset password email link
+  append_before_action :assert_reset_token_passed, only: :edit
+    
+      def translation_scope
+    'devise.sessions'
+  end
+    
+          @path = (options[:path] || name).to_s
+      @path_prefix = options[:path_prefix]
+    
     #
-    # Returns the Converter instance.
-    def converters
-      _renderer.converters
-    end
+# This script extracts the forms from the main page of each
+# web site in a list. The output of this can be used with
+# Metasploit (and other tools) to obtain the saved form data
+# of these domains.
+#
     
-        def populate_categories
-      merge_data!(
-        'categories' => (
-          Array(data['categories']) + Utils.pluralized_array_from_hash(
-            data, 'category', 'categories'
-          )
-        ).map(&:to_s).flatten.uniq
-      )
-    end
-    
-          # Generate a Hash representation of the Drop by resolving each key's
-      # value. It includes Drop methods, mutations, and the underlying object's
-      # data. See the documentation for Drop#keys for more.
-      #
-      # Returns a Hash with all the keys and values resolved.
-      def to_h
-        keys.each_with_object({}) do |(key, _), result|
-          result[key] = self[key]
+              fd.write(res)
         end
       end
-      alias_method :to_hash, :to_h
-    
-        To install Clojure you should install Leiningen:
-      brew install leiningen
-    and then follow the tutorial:
-      https://github.com/technomancy/leiningen/blob/stable/doc/TUTORIAL.md
-    EOS
-  when 'osmium' then <<-EOS.undent
-    The creator of Osmium requests that it not be packaged and that people
-    use the GitHub master branch instead.
-    EOS
-  when 'gfortran' then <<-EOS.undent
-    GNU Fortran is now provided as part of GCC, and can be installed with:
-      brew install gcc
-    EOS
-  when 'play' then <<-EOS.undent
-    Play 2.3 replaces the play command with activator:
-      brew install typesafe-activator
-    
-      def fixopt(f)
-    path = if f.linked_keg.directory? && f.linked_keg.symlink?
-      f.linked_keg.resolved_path
-    elsif f.prefix.directory?
-      f.prefix
-    elsif (kids = f.rack.children).size == 1 && kids.first.directory?
-      kids.first
-    else
-      raise
+      break
+    rescue ::Timeout::Error
+      $stderr.puts '#{prefix}#{site} timed out'
+    rescue ::Interrupt
+      raise $!
+    rescue ::Exception => e
+      $stderr.puts '#{prefix}#{site} #{e.class} #{e}'
     end
-    Keg.new(path).optlink
-  rescue StandardError
-    raise '#{f.opt_prefix} not present or broken\nPlease reinstall #{f.full_name}. Sorry :('
+  end
+    
+      def parse(pkt)
+    # We want to return immediatly if we do not have a packet which is handled by us
+    return unless pkt.is_tcp?
+    return if (pkt.tcp_sport != 21 and pkt.tcp_dport != 21)
+    s = find_session((pkt.tcp_sport == 21) ? get_session_src(pkt) : get_session_dst(pkt))
+    s[:sname] ||= 'ftp'
+    
+          case matched
+        when :ok
+          # Last command was successful, in addition most servers transmit a banner with the first +OK
+          case s[:last]
+            when nil
+              # Its the first +OK must include the banner, worst case its just +OK
+              s[:info]  = matches
+              s[:proto] = 'tcp'
+              s[:name]  = 'pop3'
+              report_service(s)
+    
+    # Sniffer class for GET URL's
+class SnifferURL < BaseProtocolParser
+  def register_sigs
+    self.sigs = {
+      :get		=> /^GET\s+([^\n]+)\s+HTTP\/\d\.\d/i,
+      :webhost	=> /^HOST\:\s+([^\n\r]+)/i,
+    }
+  end
+    
+    puts '* Initializing Meterpreter'
+    
+    success = clsJavaCompile._invoke('CompileFromMemory','[Ljava.lang.String;[Ljava.lang.String;[Ljava.lang.String;', classNames, codez, compileOpts)
+    
+    signer._invoke('JarSignerMSF','[Ljava.lang.String;',jarsignerOpts)
+    
+    desc 'Runs all tests in all Spree engines'
+task test: :test_app do
+  SPREE_GEMS.each do |gem_name|
+    Dir.chdir('#{File.dirname(__FILE__)}/#{gem_name}') do
+      sh 'rspec'
+    end
   end
 end
     
-        if ARGV.include?('--pinned') || ARGV.include?('--versions')
-      filtered_list
-    elsif ARGV.named.empty?
-      if ARGV.include? '--full-name'
-        full_names = Formula.installed.map(&:full_name).sort do |a, b|
-          if a.include?('/') && !b.include?('/')
-            1
-          elsif !a.include?('/') && b.include?('/')
-            -1
+            def find_order
+          @order = Spree::Order.find_by!(number: order_id)
+        end
+    
+            def create
+          authorize! :create, StockLocation
+          @stock_location = StockLocation.new(stock_location_params)
+          if @stock_location.save
+            respond_with(@stock_location, status: 201, default_template: :show)
           else
-            a <=> b
+            invalid_resource!(@stock_location)
           end
         end
-        puts_columns full_names
-      else
-        ENV['CLICOLOR'] = nil
-        exec 'ls', *ARGV.options_only << HOMEBREW_CELLAR
-      end
-    elsif ARGV.verbose? || !$stdout.tty?
-      exec 'find', *ARGV.kegs.map(&:to_s) + %w[-not -type d -print]
-    else
-      ARGV.kegs.each { |keg| PrettyListing.new keg }
-    end
-  end
-    
-      def python(_options = {}, &block)
-    opoo 'Formula#python is deprecated and will go away shortly.'
-    block.call if block_given?
-    PythonRequirement.new
-  end
-  alias_method :python2, :python
-  alias_method :python3, :python
-end
-
-    
-          keys = Spaceship::Portal::Key.all
-      expect(keys.size).to eq(2)
-      expect(keys.sample).to be_instance_of(Spaceship::Portal::Key)
-    end
-  end
-    
-          options[:container] = '.container'
-    end
-  end
-end
-
-    
-        version 'Guide' do
-      self.base_url = 'https://www.tensorflow.org/'
-      self.root_path = 'get_started/get_started'
-      self.initial_paths = %w(
-        programmers_guide/reading_data
-        tutorials/mandelbrot
-        performance/performance_guide
-        deploy/hadoop
-        extend/architecture)
-    
-        def filter_const(name)
-      if name.is_a? Array
-        name.map &method(:filter_const)
-      else
-        Docs.const_get '#{name}_filter'.camelize
-      end
-    end
-    
-          def stages
-        names = Dir[stage_definitions].map { |f| File.basename(f, '.rb') }
-        assert_valid_stage_names(names)
-        names
-      end
-    
-      # Implemented by subclasses to provide default values for settings needed by
-  # this plugin. Typically done using the `set_if_empty` Capistrano DSL method.
-  #
-  # Example:
-  #
-  #   def set_defaults
-  #     set_if_empty :my_plugin_option, true
-  #   end
-  #
-  def set_defaults; end
-    
-      entries.each do |entry|
-    if File.exist?(entry[:file])
-      warn '[skip] #{entry[:file]} already exists'
-    else
-      File.open(entry[:file], 'w+') do |f|
-        f.write(ERB.new(File.read(entry[:template])).result(binding))
-        puts I18n.t(:written_file, scope: :capistrano, file: entry[:file])
-      end
-    end
-  end
