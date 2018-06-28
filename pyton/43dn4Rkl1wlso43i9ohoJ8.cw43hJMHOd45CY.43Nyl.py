1ac8fa994acb494293fb89939aba6542cb613b53
SUNsung <weatherius@gmail.com>
2018-06-28 08:52:02 +0300

    Rewrite [43dn4Rkl1wlso43i9ohoJ8.cw43hJMHOd45CY.43Nyl.py] File in: 2018-06-28 08:52:01

--- pyton/43dn4Rkl1wlso43i9ohoJ8.cw43hJMHOd45CY.43Nyl.py
+++ pyton/43dn4Rkl1wlso43i9ohoJ8.cw43hJMHOd45CY.43Nyl.py
@@ -1,51 +1,81 @@
 
         
-        Provides utility functions that are consumed internally by Requests
-which depend on extremely few external helpers (such as compat)
-'''
-    
-        @pytest.fixture(autouse=True)
-    def setup(self):
-        '''LookupDict instance with 'bad_gateway' attribute.'''
-        self.lookup_dict = LookupDict('test')
-        self.lookup_dict.bad_gateway = 502
-    
-    
-class StreamConsumedError(RequestException, TypeError):
-    '''The content for this response was already consumed'''
-    
-            self.method = method
-        self.url = url
-        self.headers = headers
-        self.files = files
-        self.data = data
-        self.json = json
-        self.params = params
-        self.auth = auth
-        self.cookies = cookies
-    
-    # TBD
-def main_dev(**kwargs):
-    '''Main entry point.
-    you-get-dev
+        
+def check_alphabetical(lines):
     '''
+    checks if all entries per section are in alphabetical order based in entry title
+    '''
+    sections = {}
+    section_line_num = {}
+    for line_num, line in enumerate(lines):
+        if line.startswith(anchor):
+            category = line.split(anchor)[1].strip()
+            sections[category] = []
+            section_line_num[category] = line_num
+            continue
+        if not line.startswith('|') or line.startswith('|---'):
+            continue
+        title = [x.strip() for x in line.split('|')[1:-1]][0].upper()
+        sections[category].append(title)
     
-        while pin_count > 0:
-        json_data = extract_json_data(url, max=pin_list[-1]['pin_id'],
-                                      limit=LIMIT)
-        pins = json_data['pins']
-        pin_list += pins
-        pin_count -= len(pins)
+        def _genspider(self, module, name, domain, template_name, template_file):
+        '''Generate the spider module, based on the given template'''
+        tvars = {
+            'project_name': self.settings.get('BOT_NAME'),
+            'ProjectName': string_camelcase(self.settings.get('BOT_NAME')),
+            'module': module,
+            'name': name,
+            'domain': domain,
+            'classname': '%sSpider' % ''.join(s.capitalize() \
+                for s in module.split('_'))
+        }
+        if self.settings.get('NEWSPIDER_MODULE'):
+            spiders_module = import_module(self.settings['NEWSPIDER_MODULE'])
+            spiders_dir = abspath(dirname(spiders_module.__file__))
+        else:
+            spiders_module = None
+            spiders_dir = '.'
+        spider_file = '%s.py' % join(spiders_dir, module)
+        shutil.copyfile(template_file, spider_file)
+        render_templatefile(spider_file, **tvars)
+        print('Created spider %r using template %r ' % (name, \
+            template_name), end=('' if spiders_module else '\n'))
+        if spiders_module:
+            print('in module:\n  %s.%s' % (spiders_module.__name__, module))
+    
+                self._pending_work_items[self._queue_count] = w
+            self._work_ids.put(self._queue_count)
+            self._queue_count += 1
+            # Wake up queue management thread
+            self._result_queue.put(None)
+    
+    def download_urls_with_executor(urls, executor, timeout=60):
+    try:
+        url_to_content = {}
+        future_to_url = dict((executor.submit(load_url, url, timeout), url)
+                             for url in urls)
+    
+      for match in regex.finditer( line ):
+    if match.end() == previous_char_index:
+      return True
+  # If the whole line is whitespace, that means the user probably finished an
+  # identifier on the previous line.
+  return line[ : current_column ].isspace()
     
     
-def mtv81_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_content(url)
-    title = HTMLParser().unescape(
-        '|'.join(match1(html, r'<title>(.*?)</title>').split('|')[:-2]))
+def EndsWithPython_BadPaths_test():
+  not_python_paths = [
+    None,
+    '',
+    '/opt/local/bin/vim',
+    r'C:\Program Files\Vim\vim74\gvim.exe',
+    '/usr/bin/python2.5',
+    '/home/user/.pyenv/shims/python3.2',
+  ]
     
-    class MusicPlayOn(VideoExtractor):
-    name = 'MusicPlayOn'
+      return qf_item
+
     
-        def prepare(self, **kwargs):
-        # scrape the html
-        content = get_content(self.url)
+    
+  def PopulateLocationList( self ):
+    return self._diag_interface.PopulateLocationList()
