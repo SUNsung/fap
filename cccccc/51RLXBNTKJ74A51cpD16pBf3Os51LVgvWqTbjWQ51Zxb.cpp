851a3765d1c878ca5d1321d0a9673765fb10d58f
SUNsung <weatherius@gmail.com>
2018-06-28 22:16:25 +0300

    Rewrite [51RLXBNTKJ74A51cpD16pBf3Os51LVgvWqTbjWQ51Zxb.cpp] File in: 2018-06-28 22:16:24

--- cccccc/51RLXBNTKJ74A51cpD16pBf3Os51LVgvWqTbjWQ51Zxb.cpp
+++ cccccc/51RLXBNTKJ74A51cpD16pBf3Os51LVgvWqTbjWQ51Zxb.cpp
@@ -1,29 +1,92 @@
 
         
-        #include 'tensorflow/core/framework/op_kernel.h'
-#include 'tensorflow/core/framework/resource_mgr.h'
-#include 'tensorflow/core/framework/tensor_shape.h'
-#include 'tensorflow/core/platform/logging.h'
-#include 'tensorflow/core/platform/mutex.h'
-#include 'tensorflow/core/platform/thread_annotations.h'
-#include 'tensorflow/core/platform/types.h'
+        void PartialRunMgr::PartialRunDone(int step_id, StatusCallback done,
+                                   const Status& status) {
+  Status callback_status;
+  {
+    mutex_lock l(mu_);
+    auto run_it = step_id_to_partial_run_.find(step_id);
+    if (run_it == step_id_to_partial_run_.end()) {
+      return;
+    }
+    run_it->second->final_status.Update(status);
+    if (!run_it->second->executor_done) {
+      // If we found the partial_run, we set the final callback to call only
+      // when the executor is completely done.
+      run_it->second->final_callback = std::move(done);
+      return;
+    }
+    callback_status = run_it->second->final_status;
+  }
+  // Otherwise we call the callback immediately.
+  done(callback_status);
+  mutex_lock l(mu_);
+  step_id_to_partial_run_.erase(step_id);
+}
+    
+    /** scalar_sigmoid_fast_derivative_op
+  * \ingroup CXX11_NeuralNetworks_Module
+  * \brief Template functor to compute the fast derivative of a sigmoid
+  *
+  * Input should be the backpropagated gradient.
+  *
+  * \sa class CwiseUnaryOp, Cwise::sigmoid_fast_derivative()
+  */
+template <typename T>
+struct scalar_sigmoid_fast_derivative_op {
+  EIGEN_EMPTY_STRUCT_CTOR(scalar_sigmoid_fast_derivative_op)
+  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE T operator()(const T& y) const {
+    const T one = T(1);
+    return (one - y) * y;
+  }
+    }
+    
+    // Prefetching support
+//
+// Defined behavior on some of the uarchs:
+// PREFETCH_HINT_T0:
+//   prefetch to all levels of the hierarchy (except on p4: prefetch to L2)
+// PREFETCH_HINT_NTA:
+//   p4: fetch to L2, but limit to 1 way (out of the 8 ways)
+//   core: skip L2, go directly to L1
+//   k8 rev E and later: skip L2, can go to either of the 2-ways in L1
+enum PrefetchHint {
+  PREFETCH_HINT_T0 = 3,  // More temporal locality
+  PREFETCH_HINT_T1 = 2,
+  PREFETCH_HINT_T2 = 1,  // Less temporal locality
+  PREFETCH_HINT_NTA = 0  // No temporal locality
+};
+template <PrefetchHint hint>
+void prefetch(const void* x);
     
         http://www.apache.org/licenses/LICENSE-2.0
     
-    // TODO(zongheng): this should be a general functor that powers SparseAdd and
-// ScatterNd ops.  It should be moved to its own head file, once the other ops
-// are implemented.
-template <typename Device, typename T, typename Index, int NDIMS,
-          scatter_op::UpdateOp op>
-struct ScatterNdFunctor {
-  // Returns -1 on success or a nonnegative i s.t. indices[i] is a bad index.
-  Index operator()(const Device& d, typename TTypes<Index>::ConstMatrix indices,
-                   typename TTypes<T>::ConstFlat updates,
-                   typename TTypes<T, NDIMS>::Tensor out);
-};
     
-      static RecordWriterOptions CreateRecordWriterOptions(
-      const string& compression_type);
+    {}  // namespace tensorflow
+    
+    class RemoveDeviceTest : public ::testing::Test {
+ protected:
+  void TestRemoveDevice() {
+    GraphDef graph_def;
+    }
+    }
+    
+    
+    {inline void AddTraceEvent(const TraceEvent &event, string *json) {
+  Appendf(json, R'({'pid':%u,'tid':%u,'ts':%.5f,)', event.device_id(),
+          event.resource_id(), event.timestamp_ps() / kPicosPerMicro);
+  AppendEscapedName(json, event.name());
+  StrAppend(json, ',');
+  if (event.duration_ps() > 0) {
+    Appendf(json, R'('ph':'X','dur':%.5f},)',
+            event.duration_ps() / kPicosPerMicro);
+  } else {
+    StrAppend(json, R'('ph':'i','s':'t'},)');
+  }
+}
+    
+      // Size of the buffer used for caching the data read from source file.
+  int64 input_buffer_size = 256 << 10;
     
     Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an 'AS IS' BASIS,
@@ -32,251 +95,99 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
     
-        const string file_format =
-        str_util::Lowercase(file_format_tensor.scalar<string>()());
-    const int32 samples_per_second =
-        samples_per_second_tensor.scalar<int32>()();
-    const int32 bits_per_second = bits_per_second_tensor.scalar<int32>()();
+    // These are all common classes it's handy to reference with no namespace.
+using tensorflow::Flag;
+using tensorflow::Status;
+using tensorflow::Tensor;
+using tensorflow::int32;
+using tensorflow::string;
     
-        NodeDef* add_node2 = graph_def.add_node();
-    add_node2->set_name('add_node2');
-    add_node2->set_op('Add');
-    add_node2->add_input('const_node1');
-    add_node2->add_input('const_node2');
-    add_node2->set_device('//device:GPU:1');
+     private:
+  // Size of padded input to weight matrices = ni_ + no_ for 1-D operation
+  // and ni_ + 2 * no_ for 2-D operation. Note that there is a phantom 1 input
+  // for the bias that makes the weight matrices of size [na + 1][no].
+  int32_t na_;
+  // Number of internal states. Equal to no_ except for a softmax LSTM.
+  // ns_ is NOT serialized, but is calculated from gate_weights_.
+  int32_t ns_;
+  // Number of additional feedback states. The softmax types feed back
+  // additional output information on top of the ns_ internal states.
+  // In the case of a binary-coded (EMBEDDED) softmax, nf_ < no_.
+  int32_t nf_;
+  // Flag indicating 2-D operation.
+  bool is_2d_;
     
-    namespace tensorflow {
-#define REGISTER_COMPLEX(D, R, C)                         \
-  REGISTER_KERNEL_BUILDER(Name('Angle')                   \
-                              .Device(DEVICE_##D)         \
-                              .TypeConstraint<C>('T')     \
-                              .TypeConstraint<R>('Tout'), \
-                          UnaryOp<D##Device, functor::get_angle<C>>);
-    }
-    
-    Status ReadGroundTruthFile(const string& file_name,
-                           std::vector<std::pair<string, int64>>* result) {
-  std::ifstream file(file_name);
-  if (!file) {
-    return tensorflow::errors::NotFound('Ground truth file '', file_name,
-                                        '' not found.');
-  }
-  result->clear();
-  string line;
-  while (std::getline(file, line)) {
-    std::vector<string> pieces = tensorflow::str_util::Split(line, ',');
-    if (pieces.size() != 2) {
-      continue;
-    }
-    float timestamp;
-    if (!tensorflow::strings::safe_strtof(pieces[1].c_str(), &timestamp)) {
-      return tensorflow::errors::InvalidArgument(
-          'Wrong number format at line: ', line);
-    }
-    string label = pieces[0];
-    auto timestamp_int64 = static_cast<int64>(timestamp);
-    result->push_back({label, timestamp_int64});
-  }
-  std::sort(result->begin(), result->end(),
-            [](const std::pair<string, int64>& left,
-               const std::pair<string, int64>& right) {
-              return left.second < right.second;
-            });
-  return Status::OK();
-}
-    
-    #include 'base/values.h'
-#include 'base/strings/utf_string_conversions.h'
-#include 'base/strings/string16.h'
-#include 'content/nw/src/api/dispatcher_host.h'
-#include 'ui/base/clipboard/clipboard.h'
-    
-    class Clipboard : public Base {
- public:
-  Clipboard(int id,
-            const base::WeakPtr<DispatcherHost>& dispatcher_host,
-            const base::DictionaryValue& option);
-  ~Clipboard() override;
-    }
-    
-    
-namespace nwapi {
-    }
-    
-    bool NwShellShowItemInFolderFunction::RunNWSync(base::ListValue* response, std::string* error) {
-  nw::ObjectManager* manager = nw::ObjectManager::Get(browser_context());
-  manager->OnCallStaticMethod(render_frame_host(), 'Shell', 'ShowItemInFolder', *args_);
-  return true;
-}
-    
-    // Ternary predicate assertion macros.
-#define EXPECT_PRED_FORMAT3(pred_format, v1, v2, v3) \
-  GTEST_PRED_FORMAT3_(pred_format, v1, v2, v3, GTEST_NONFATAL_FAILURE_)
-#define EXPECT_PRED3(pred, v1, v2, v3) \
-  GTEST_PRED3_(pred, v1, v2, v3, GTEST_NONFATAL_FAILURE_)
-#define ASSERT_PRED_FORMAT3(pred_format, v1, v2, v3) \
-  GTEST_PRED_FORMAT3_(pred_format, v1, v2, v3, GTEST_FATAL_FAILURE_)
-#define ASSERT_PRED3(pred, v1, v2, v3) \
-  GTEST_PRED3_(pred, v1, v2, v3, GTEST_FATAL_FAILURE_)
-    
-    // A concrete DeathTestFactory implementation for normal use.
-class DefaultDeathTestFactory : public DeathTestFactory {
- public:
-  virtual bool Create(const char* statement, const RE* regex,
-                      const char* file, int line, DeathTest** test);
-};
-    
-    // pthread_key_create() requires DeleteThreadLocalValue() to have
-// C-linkage.  Therefore it cannot be templatized to access
-// ThreadLocal<T>.  Hence the need for class
-// ThreadLocalValueHolderBase.
-class ThreadLocalValueHolderBase {
- public:
-  virtual ~ThreadLocalValueHolderBase() {}
-};
-    
-      // Creates a UTF-16 wide string from the given ANSI string, allocating
-  // memory using new. The caller is responsible for deleting the return
-  // value using delete[]. Returns the wide string, or NULL if the
-  // input is NULL.
-  //
-  // The wide string is created using the ANSI codepage (CP_ACP) to
-  // match the behaviour of the ANSI versions of Win32 calls and the
-  // C runtime.
-  static LPCWSTR AnsiToUtf16(const char* c_str);
-    
-      bool check_for_leaks = false;
-  if (argc > 1 && strcmp(argv[1], '--check_for_leaks') == 0 )
-    check_for_leaks = true;
-  else
-    printf('%s\n', 'Run this program with --check_for_leaks to enable '
-           'custom leak checking in the tests.');
-    
-      rusage ru;
-  memset(&ru, 0, sizeof(ru));
-  auto DEBUG_ONLY ret = getrusage(who, &ru);
-  assert(ret == 0);
-    
-    
-    {  // points to an event with an attached waithandle from a different request
-  PageletServerTaskEvent *m_event;
-};
-    
-    private void close_and_restore(const struct magic_set *, const char *, int,
-    const struct stat *);
-private int unreadable_info(struct magic_set *, mode_t, const char *);
-#if 0
-private const char* get_default_magic(void);
+    // CALL ONLY from a signal handler! Writes a crash image to stderr.
+void signal_exit(int signal_code) {
+  tprintf('Received signal %d!\n', signal_code);
+#ifdef __linux__
+#ifndef ANDROID
+  int thread_id = syscall(SYS_gettid) % kMaxNumThreadPixes;
+#else
+  int thread_id = gettid() % kMaxNumThreadPixes;
 #endif
-private const char *file_or_stream(struct magic_set *, const char *, php_stream *);
-    
-      // DataBlock can optionally be growable. The initial expansion of DataBlock
-  // will allocate a new buffer that is owned by the DataBlock, subsequent
-  // expansions will use realloc to expand this block until m_maxGrow has been
-  // reached. Only DataBlocks which have a different m_base from m_destBase may
-  // be grown, as expansion may move the location of m_destBase.
-  struct Deleter final { void operator()(uint8_t* a) const { ::free(a); } };
-  std::unique_ptr<uint8_t, Deleter> m_destBuf{nullptr};
-    
-    #include 'hphp/tools/tc-print/tc-print.h'
-#include 'hphp/tools/tc-print/offline-trans-data.h'
-    
-    namespace HPHP {
-///////////////////////////////////////////////////////////////////////////////
-    }
-    
-      EXPECT_EQ(FLAGS_test_int64_alias, (int64_t)1 << 34);
-  FLAGS_test_int64_alias = (int64_t)1 << 35;
-  EXPECT_EQ(FLAGS_test_int64, (int64_t)1 << 35);
-  FLAGS_test_int64 = (int64_t)1 << 36;
-  EXPECT_EQ(FLAGS_test_int64_alias, (int64_t)1 << 36);
-  EXPECT_TRUE(FLAGS_test_int64_alias > 0);
-    
-      // Remove the backup and expect another reload to not create one.
-  removePath(path_ + '.backup');
-  ASSERT_FALSE(pathExists(path_ + '.backup'));
-    
-      void TearDown() override;
-    
-    TEST_F(PrinterTests, test_unicode) {
-  Row r = {{'name', 'Àlex Smith'}};
-  std::map<std::string, size_t> lengths;
-  computeRowLengths(r, lengths);
-    }
-    
-    void KernelEventPublisher::stop() {
-  WriteLock lock(mutex_);
-  if (queue_ != nullptr) {
-    delete queue_;
-    queue_ = nullptr;
+  if (global_crash_pixes[thread_id] != nullptr) {
+    fprintf(stderr, 'Crash caused by image with resolution %d\n',
+            pixGetYRes(global_crash_pixes[thread_id]));
+    fprintf(stderr, '<Cut here>\n');
+    pixWriteStreamPng(stderr, global_crash_pixes[thread_id], 0.0);
+    fprintf(stderr, '\n<End cut>\n');
   }
-}
-    
-    namespace osquery {
-    }
-    
-      virtual const char* Name() const override;
-    
-    #ifndef JAVA_ROCKSJNI_STATISTICSJNI_H_
-#define JAVA_ROCKSJNI_STATISTICSJNI_H_
-    
-        virtual DecodedType decode_key(const char* key) const {
-      // The format of key is frozen and can be terated as a part of the API
-      // contract. Refer to MemTable::Add for details.
-      return GetLengthPrefixedSlice(key);
-    }
-    
-      bool pending_exception =
-      AbstractSliceJni::setHandle(env, m_jSliceA, &a, JNI_FALSE);
-  if(pending_exception) {
-    if(env->ExceptionCheck()) {
-      // exception thrown from setHandle or descendant
-      env->ExceptionDescribe(); // print out exception to stderr
-    }
-    releaseJniEnv(attached_thread);
-    return 0;
-  }
-    
-    
-    {    pclose(stream);
-}
-    
-    
-/*
- * WakeUpLock.cpp
- *
- *  Created on: 2012-9-28
- *      Author: yerungui
- */
-    
-    #include <stdlib.h>
-    
-    
-    {    void throw_exception( std::exception const & e ) {
-        xfatal2(TSF'boost exception:%_', e.what());
-        
-#ifdef ANDROID
-        char stack[4096] = {0};
-        android_callstack(stack, sizeof(stack));
-        xfatal2(TSF'%_', stack);
+  // Raise an uncaught signal, so as to get a useful stack trace.
+  raise(SIGILL);
+#else
+  abort();
 #endif
-    }
 }
+    
+      /// Return the id of the T object.
+  /// This method NEEDS a compare_callback to be passed to
+  /// set_compare_callback.
+  int get_id(T object) const;
+    
+    #define YG_NODE_STYLE_PROPERTY(type, name, paramName)                               \
+  WIN_EXPORT void YGNodeStyleSet##name(const YGNodeRef node, const type paramName); \
+  WIN_EXPORT type YGNodeStyleGet##name(const YGNodeRef node);
+    
+    template <typename R, typename... Args>
+struct jmethod_traits_from_cxx<R(Args...)> : jmethod_traits<detail::jni_sig_from_cxx<R(Args...)>> {
+};
+    
+    // TODO(t10737622): Improve on-device symbolification
+void getStackTraceSymbols(vector<StackTraceElement>& symbols,
+                          const vector<InstructionPointer>& trace) {
+  symbols.clear();
+  symbols.reserve(trace.size());
+    }
+    
+    
+    {};
 
     
-    // Licensed under the MIT License (the 'License'); you may not use this file except in 
-// compliance with the License. You may obtain a copy of the License at
-// http://opensource.org/licenses/MIT
+     public: // Dirtiness accessors
     
-    void TSpy::TestFun0()
-{
-    return reinterpret_cast<Test_Spy_Sample*>(This())->TestFun0();
+    void assertInternal(const char* formatstr ...) {
+    va_list va_args;
+    va_start(va_args, formatstr);
+    vsnprintf(sAssertBuf, sizeof(sAssertBuf), formatstr, va_args);
+    va_end(va_args);
+    if (gAssertHandler != NULL) {
+        gAssertHandler(sAssertBuf);
+    }
+    FBLOG(LOG_FATAL, 'fbassert', '%s', sAssertBuf);
+    // crash at this specific address so that we can find our crashes easier
+    *(int*)0xdeadb00c = 0;
+    // let the compiler know we won't reach the end of the function
+     __builtin_unreachable();
 }
     
+      /**
+   * This runs the closure in a scope with fbjni's classloader. This should be
+   * the same classloader as the rest of the application and thus anything
+   * running in the closure will have access to the same classes as in a normal
+   * java-create thread.
+   */
+  static void WithClassLoader(std::function<void()>&& runnable);
     
-    {  private:
-    JavaVM* vm_;
-    JNIEnv* env_;
-    bool we_attach_;
-    int status_;
-};
+    namespace facebook {
+    }
