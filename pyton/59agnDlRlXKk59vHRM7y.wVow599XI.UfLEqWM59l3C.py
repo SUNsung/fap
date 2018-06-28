f0c8b9055f70f0c6868190e34a90e9be580f988b
SUNsung <weatherius@gmail.com>
2018-06-28 18:46:58 +0300

    Rewrite [59agnDlRlXKk59vHRM7y.wVow599XI.UfLEqWM59l3C.py] File in: 2018-06-28 18:46:57

--- pyton/59agnDlRlXKk59vHRM7y.wVow599XI.UfLEqWM59l3C.py
+++ pyton/59agnDlRlXKk59vHRM7y.wVow599XI.UfLEqWM59l3C.py
@@ -1,119 +1,92 @@
 
         
         
-def main(unused_argv):
-  # Run the exporter.
-  export(FLAGS.master_spec, FLAGS.params_path, FLAGS.export_path,
-         FLAGS.export_moving_averages, FLAGS.build_runtime_graph)
-  tf.logging.info('Export complete.')
+def write_stream_with_colors_win_py3(stream, outfile, flush):
+    '''Like `write`, but colorized chunks are written as text
+    directly to `outfile` to ensure it gets processed by colorama.
+    Applies only to Windows with Python 3 and colorized terminal output.
     
-      Returns:
-    Dict mapping from shortened resource path to original resource path.
-  '''
-  for component_spec in master_spec.component:
-    for feature_spec in component_spec.fixed_feature:
-      feature_spec.ClearField('pretrained_embedding_matrix')
-      feature_spec.ClearField('vocab')
+        # Optional short description. Will be be shown in the help
+    # under --auth-type.
+    description = None
     
-        for ns in n_samples:
-        for nf in n_features:
-            it += 1
-            print('==================')
-            print('Iteration %s of %s' % (it, max(len(n_samples),
-                                          len(n_features))))
-            print('==================')
-            n_informative = nf // 10
-            X, Y, coef_ = make_regression(n_samples=ns, n_features=nf,
-                                          n_informative=n_informative,
-                                          noise=0.1, coef=True)
-    
-    plot(euclidean_distances)
-plot(rbf_kernels)
-plt.show()
-
-    
-            text_filename = os.path.join(text_lang_folder,
-                                     '%s_%04d.txt' % (lang, i))
-        print('Writing %s' % text_filename)
-        open(text_filename, 'wb').write(content.encode('utf-8', 'ignore'))
-        i += 1
-    
-    model = SpectralBiclustering(n_clusters=n_clusters, method='log',
-                             random_state=0)
-model.fit(data)
-score = consensus_score(model.biclusters_,
-                        (rows[:, row_idx], columns[:, col_idx]))
-    
-        # not a test, but looks like a test
-    'scrapy/utils/testsite.py',
-    
-        def test_sequential_execution(self):
-        self.command_stack[0].execute()
-        output_after_first_execution = os.listdir(self.test_dir)
-        self.assertEqual(output_after_first_execution[0], 'bar.txt')
-        self.command_stack[1].execute()
-        output_after_second_execution = os.listdir(self.test_dir)
-        self.assertEqual(output_after_second_execution[0], 'baz.txt')
-    
-        def test_sales_manager_shall_respond_through_proxy_with_delay(cls):
-        cls.p.busy = 'Yes'
-        start_time = time()
-        cls.p.talk()
-        end_time = time()
-        execution_time = end_time - start_time
-        print_output = cls.output.getvalue()
-        expected_print_output = 'Proxy checking for Sales Manager availability\n\
-Sales Manager is busy\n'
-        cls.assertEqual(print_output, expected_print_output)
-        expected_execution_time = 1
-        cls.assertEqual(int(execution_time*10), expected_execution_time)
+        # noinspection PyMethodOverriding
+    def get_auth(self, username, password):
+        return HTTPBasicAuth(username, password)
     
     
-class TestNaitivePool(unittest.TestCase):
-    
-        def test_dog_eng_localization(self):
-        self.assertEqual(self.e.get('dog'), 'dog')
-    
-        def now(self):
-        current_time = datetime.datetime.now()
-        current_time_formatted = '{}:{}'.format(current_time.hour, current_time.minute)
-        return current_time_formatted
+def humanize_bytes(n, precision=2):
+    # Author: Doug Latornell
+    # Licence: MIT
+    # URL: http://code.activestate.com/recipes/577081/
+    '''Return a humanized string representation of a number of bytes.
     
     
-class AndSpecification(CompositeSpecification):
-    _one = Specification()
-    _other = Specification()
+def rbf_kernels(X, n_jobs):
+    return pairwise_kernels(X, metric='rbf', n_jobs=n_jobs, gamma=0.1)
     
-    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_API_KEY): cv.string,
-    vol.Required(CONF_SYSTEM_ID): cv.string,
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-})
+        ###########################################################################
+    # Set transformer input
+    ###########################################################################
+    transformers = {}
     
-        if not app.config.edit_on_github_project:
-        warnings.warn('edit_on_github_project not specified')
-        return
-    if not doctree:
-        warnings.warn('doctree is None')
-        return
-    path = os.path.relpath(doctree.get('source'), app.builder.srcdir)
-    show_url = get_github_url(app, 'blob', path)
-    edit_url = get_github_url(app, 'edit', path)
+    import gc
+import sys
+import optparse
+from datetime import datetime
+import operator
     
-        return True
-
+    mu_second = 0.0 + 10 ** 6  # number of microseconds in a second
     
+    # TASK: Build a vectorizer / classifier pipeline using the previous analyzer
+# the pipeline instance should stored in a variable named clf
     
-def run(args):
-    '''Handle ensure config commandline script.'''
-    parser = argparse.ArgumentParser(
-        description=('Ensure a Home Assistant config exists, '
-                     'creates one if necessary.'))
-    parser.add_argument(
-        '-c', '--config',
-        metavar='path_to_config_dir',
-        default=config_util.get_default_config_dir(),
-        help='Directory that contains the Home Assistant configuration')
-    parser.add_argument(
-        '--script',
-        choices=['ensure_config'])
+    solutions = os.listdir(exercise_dir)
+    
+    # Define 'classifiers' to be used
+classifiers = {
+    'Empirical Covariance': EllipticEnvelope(support_fraction=1.,
+                                             contamination=0.261),
+    'Robust Covariance (Minimum Covariance Determinant)':
+    EllipticEnvelope(contamination=0.261),
+    'OCSVM': OneClassSVM(nu=0.261, gamma=0.05)}
+colors = ['m', 'g', 'b']
+legend1 = {}
+legend2 = {}
+    
+    from sklearn.datasets import make_biclusters
+from sklearn.datasets import samples_generator as sg
+from sklearn.cluster.bicluster import SpectralCoclustering
+from sklearn.metrics import consensus_score
+    
+        # View probabilities=
+    probas = classifier.predict_proba(Xfull)
+    n_classes = np.unique(y_pred).size
+    for k in range(n_classes):
+        plt.subplot(n_classifiers, n_classes, index * n_classes + k + 1)
+        plt.title('Class %d' % k)
+        if k == 0:
+            plt.ylabel(name)
+        imshow_handle = plt.imshow(probas[:, k].reshape((100, 100)),
+                                   extent=(3, 9, 1, 5), origin='lower')
+        plt.xticks(())
+        plt.yticks(())
+        idx = (y_pred == k)
+        if idx.any():
+            plt.scatter(X[idx, 0], X[idx, 1], marker='o', c='k')
+    
+            clf1 = LinearDiscriminantAnalysis(solver='lsqr', shrinkage='auto').fit(X, y)
+        clf2 = LinearDiscriminantAnalysis(solver='lsqr', shrinkage=None).fit(X, y)
+    
+        def test_pool_behavior_with_single_object_inside(self):
+        sample_queue = queue.Queue()
+        sample_queue.put('yam')
+        with ObjectPool(sample_queue) as obj:
+            # print('Inside with: {}'.format(obj))
+            self.assertEqual(obj, 'yam')
+        self.assertFalse(sample_queue.empty())
+        self.assertTrue(sample_queue.get() == 'yam')
+        self.assertTrue(sample_queue.empty())
+        
+    
+    class TimeDisplay(object):
