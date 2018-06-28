81946b1423414ca1bbd7ed85bc568f495a209658
SUNsung <weatherius@gmail.com>
2018-06-28 09:18:05 +0300

    Rewrite [23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp] File in: 2018-06-28 09:18:04

--- cccccc/23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp
+++ cccccc/23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp
@@ -1,144 +1,287 @@
 
         
-        #ifndef GRPC_INTERNAL_COMPILER_NODE_GENERATOR_HELPERS_H
-#define GRPC_INTERNAL_COMPILER_NODE_GENERATOR_HELPERS_H
+            GraphemeClusterBreakProperty GCBForC1 =
+        getGraphemeClusterBreakProperty(C[1]);
+    if (isExtendedGraphemeClusterBoundary(GCBForC0, GCBForC1) &&
+        !graphemeBreakOverride(C[0], C[1]))
+      return S.slice(0, C1Offset);
     
-    
-    { private:
-  GeneratorConfiguration config_;
-};
-    
-    std::vector<grpc::string_ref> SecureAuthContext::FindPropertyValues(
-    const grpc::string& name) const {
-  if (!ctx_) {
-    return std::vector<grpc::string_ref>();
+    // Not currently used, see related comments in the call sites.
+#if 0
+static void
+collectFileDependencies(llvm::SetVector<const clang::FileEntry *> &result,
+                        const DependencyTracker &dependencyTracker,
+                        ModuleDecl *module, clang::FileManager &fileMgr) {
+  for (auto *F : module->getFiles()) {
+    if (auto *SF = dyn_cast<SourceFile>(F)) {
+      if (auto *dep = fileMgr.getFile(SF->getFilename())) {
+        result.insert(dep);
+      }
+    }
   }
-  grpc_auth_property_iterator iter =
-      grpc_auth_context_find_properties_by_name(ctx_, name.c_str());
-  const grpc_auth_property* property = nullptr;
-  std::vector<grpc::string_ref> values;
-  while ((property = grpc_auth_property_iterator_next(&iter))) {
-    values.push_back(grpc::string_ref(property->value, property->value_length));
+  for (StringRef filename : dependencyTracker.getDependencies()) {
+    if (auto *F = fileMgr.getFile(filename))
+      result.insert(F);
   }
-  return values;
 }
-    
-    #include 'test/cpp/qps/histogram.h'
-    
-    grpc::string DescribeMethod(const grpc::protobuf::MethodDescriptor* method) {
-  std::stringstream result;
-  result << '  rpc ' << method->name()
-         << (method->client_streaming() ? '(stream ' : '(')
-         << method->input_type()->full_name() << ') returns '
-         << (method->server_streaming() ? '(stream ' : '(')
-         << method->output_type()->full_name() << ') {}\n';
-  if (method->options().deprecated()) {
-    result << ' DEPRECATED';
-  }
-  return result.str();
-}
-    
-    #include 'jsapi.h'
-#include 'jsfriendapi.h'
-    
-    bool js_cocos2dx_studio_Timeline_constructor(JSContext *cx, uint32_t argc, jsval *vp);
-void js_cocos2dx_studio_Timeline_finalize(JSContext *cx, JSObject *obj);
-void js_register_cocos2dx_studio_Timeline(JSContext *cx, JS::HandleObject global);
-void register_all_cocos2dx_studio(JSContext* cx, JS::HandleObject obj);
-bool js_cocos2dx_studio_Timeline_clone(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_Timeline_gotoFrame(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_Timeline_setNode(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_Timeline_getActionTimeline(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_Timeline_insertFrame(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_Timeline_setActionTag(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_Timeline_addFrame(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_Timeline_getFrames(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_Timeline_getActionTag(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_Timeline_getNode(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_Timeline_removeFrame(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_Timeline_setActionTimeline(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_Timeline_stepToFrame(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_Timeline_create(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_Timeline_Timeline(JSContext *cx, uint32_t argc, jsval *vp);
-    
-    
-    
-    
-#if COCOS2D_DEBUG >= 1
-    if (!tolua_isusertype(tolua_S,1,'cc.SimpleAudioEngine',0,&tolua_err)) goto tolua_lerror;
 #endif
     
+    namespace swift {
+namespace syntax {
+    }
+    }
+    
+      /// Returns the buffer ID for the specified *valid* location.
+  ///
+  /// Because a valid source location always corresponds to a source buffer,
+  /// this routine always returns a valid buffer ID.
+  unsigned findBufferContainingLoc(SourceLoc Loc) const;
+    
+    #ifdef __APPLE__
     
     
-    
-    
-    
-    
-    
-    
-    			b2FixtureDef sd;
-			sd.shape = &shape;
-			sd.density = 0.0f;
-			sd.restitution = k_restitution;
-    
-    			b2EdgeShape shape;
-			shape.Set(b2Vec2(-20.0f, 0.0f), b2Vec2(20.0f, 0.0f));
-    
-    	enum
-	{
-		e_count = 30
-	};
-    
-    			m_body = m_world->CreateBody(&bd);
-			m_body->CreateFixture(&box, 1.0f);
-    
-    			b2EdgeShape shape;
-    
-    
-    {  // Returns a heuristic cutoff on block errors in the sense that we won't
-  // consider distortions where a block error is greater than this.
-  virtual float BlockErrorLimit() const = 0;
-  // Given the search direction (+1 for upwards and -1 for downwards) and the
-  // current distance map, fills in *block_weight image with the relative block
-  // error adjustment weights.
-  // The target_mul param has the same semantics as in DistanceOK().
-  // Note that this is essentially a static function in the sense that it does
-  // not depend on the last Compare() call.
-  virtual void ComputeBlockErrorAdjustmentWeights(
-      int direction, int max_block_dist, double target_mul, int factor_x,
-      int factor_y, const std::vector<float>& distmap,
-      std::vector<float>* block_weight) = 0;
+    {  virtual Optional<std::pair<ManagedValue, ManagedValue>>
+  emitForeignErrorArgument(SILGenFunction &SGF, SILLocation loc) {
+    return None;
+  }
 };
     
-    void OutputImage::ApplyGlobalQuantization(const int q[3][kDCTBlockSize]) {
-  for (int c = 0; c < 3; ++c) {
-    components_[c].ApplyGlobalQuantization(&q[c][0]);
-  }
+    CV_EXPORTS_W void multiply(InputArray src1, Scalar src2, OutputArray dst, double scale=1, int dtype=-1);
+    
+    #undef cv_hal_LU32f
+#define cv_hal_LU32f lapack_LU32f
+#undef cv_hal_LU64f
+#define cv_hal_LU64f lapack_LU64f
+    
+    static void* openclamdfft_check_fn(int ID)
+{
+    assert(ID >= 0 && ID < (int)(sizeof(openclamdfft_fn)/sizeof(openclamdfft_fn[0])));
+    const struct DynamicFnEntry* e = openclamdfft_fn[ID];
+    void* func = CV_CL_GET_PROC_ADDRESS(e->fnName);
+    if (!func)
+    {
+        throw cv::Exception(cv::Error::OpenCLApiCallError,
+                cv::format('OpenCL AMD FFT function is not available: [%s]', e->fnName),
+                CV_Func, __FILE__, __LINE__);
+    }
+    *(e->ppFn) = func;
+    return func;
 }
     
-          // The sentinel node becomes the parent node.
-      size_t j_end = 2 * n - k;
-      tree[j_end].total_count_ =
-          tree[left].total_count_ + tree[right].total_count_;
-      tree[j_end].index_left_ = static_cast<int16_t>(left);
-      tree[j_end].index_right_or_value_ = static_cast<int16_t>(right);
-    
-    namespace guetzli {
+    public:
+    // assign a sub-rectangle from a 0-based matrix of the same size
+    void assignpatch(const ssematrixbase &patch, const size_t i0, const size_t i1, const size_t j0, const size_t j1)
+    {
+        auto &us = *this;
+        assert(i1 - i0 == patch.rows() && j1 - j0 == patch.cols());
+        assert(i0 <= i1 && j0 <= j1);
+        assert(i1 <= rows() && j1 <= cols());
     }
     
-    // Computes the DCT (Discrete Cosine Transform) of the 8x8 array in 'block',
-// scaled up by a factor of 16. The values in 'block' are laid out row-by-row
-// and the result is written to the same memory area.
-void ComputeBlockDCT(coeff_t* block);
+    template void DoConvertFromDbn<float>(const ConfigParameters& config);
+template void DoConvertFromDbn<double>(const ConfigParameters& config);
+template void DoExportToDbn<float>(const ConfigParameters& config);
+template void DoExportToDbn<double>(const ConfigParameters& config);
+
     
-    // Decodes the parsed jpeg coefficients into an RGB image.
-// There can be only either 1 or 3 image components, in either case, an RGB
-// output image will be generated.
-// Only YUV420 and YUV444 sampling factors are supported.
-// Vector will be empty if a decoding error occurred.
-std::vector<uint8_t> DecodeJpegToRGB(const JPEGData& jpg);
+    #include <memory> // for shared_ptr
     
-    namespace guetzli {
+    public:
+    // DataWriter Constructor
+    // config - [in] configuration parameters for the datareader
+    template <class ConfigRecordType>
+    DataWriter(const ConfigRecordType& config);
+    // constructor from Scripting
+    DataWriter(const ScriptableObjects::IConfigRecordPtr configp)
+        : DataWriter(*configp)
+    {
+    }
+    virtual ~DataWriter();
+    
+        void getlattices(const std::wstring& key, std::shared_ptr<const latticepair>& L, size_t expectedframes) const
+    {
+        std::shared_ptr<latticepair> LP(new latticepair);
+        denlattices.getlattice(key, LP->second, expectedframes); // this loads the lattice from disk, using the existing L.second object
+        L = LP;
     }
     
-    #endif  // GUETZLI_JPEG_ERROR_H_
+    template <class _T>
+class const_array_ref
+{
+    const _T* data;
+    size_t n;
+    inline void check_index(size_t i) const
+    {
+        i;
+        assert(i < n);
+    }
+    inline void check_ptr() const
+    {
+        n;
+        data;
+        assert(n == 0 || data != NULL);
+    }
+    }
+    
+    enum RecordType {
+  // Zero is reserved for preallocated files
+  kZeroType = 0,
+    }
+    
+    int main(int argc, char** argv) {
+  return leveldb::test::RunAllTests();
+}
+
+    
+      // Check the crc of the type and the block contents
+  const char* data = contents.data();    // Pointer to where Read put the data
+  if (options.verify_checksums) {
+    const uint32_t crc = crc32c::Unmask(DecodeFixed32(data + n + 1));
+    const uint32_t actual = crc32c::Value(data, n + 1);
+    if (actual != crc) {
+      delete[] buf;
+      s = Status::Corruption('block checksum mismatch');
+      return s;
+    }
+  }
+    
+      // Pick up four bytes at a time
+  while (data + 4 <= limit) {
+    uint32_t w = DecodeFixed32(data);
+    data += 4;
+    h += w;
+    h *= m;
+    h ^= (h >> 16);
+  }
+    
+    int main(int argc, char** argv) {
+  return leveldb::test::RunAllTests();
+}
+
+    
+      // Create a writer that will append data to '*dest'.
+  // '*dest' must have initial length 'dest_length'.
+  // '*dest' must remain live while this Writer is in use.
+  Writer(WritableFile* dest, uint64_t dest_length);
+    
+      /// The time point type of the clock.
+  typedef typename clock_type::time_point time_point;
+    
+    namespace boost {
+namespace asio {
+    }
+    }
+    
+    #endif // BOOST_ASIO_BUFFERED_STREAM_FWD_HPP
+
+    
+    
+    {    // Make the upcall if required.
+    if (owner)
+    {
+      fenced_block b(fenced_block::half);
+      BOOST_ASIO_HANDLER_INVOCATION_BEGIN((handler.arg1_, handler.arg2_));
+      boost_asio_handler_invoke_helpers::invoke(handler, handler.handler_);
+      BOOST_ASIO_HANDLER_INVOCATION_END;
+    }
+  }
+    
+    /// Unlike checkChildProcessStatus, this will block until process exits.
+static bool getProcessExitCode(osquery::PlatformProcess& process,
+                               int& exitCode) {
+  if (!process.isValid()) {
+    return false;
+  }
+    }
+    
+      // Now the watch method returns false, indicating the previous worker failed
+  // performance checked and was stopped.
+  EXPECT_CALL(runner, watch(_)).WillOnce(Return(false));
+  // Since the watch failed, the thread should spawn another worker.
+  EXPECT_CALL(runner, createWorker()).Times(1);
+  EXPECT_CALL(runner, isWatcherHealthy(_, _)).WillOnce(Return(Status(0)));
+    
+    Status WmiResultItem::GetLongLong(const std::string& name,
+                                  long long& ret) const {
+  std::wstring property_name = stringToWstring(name);
+  VARIANT value;
+  HRESULT hr = result_->Get(property_name.c_str(), 0, &value, nullptr, nullptr);
+  if (hr != S_OK) {
+    return Status(-1, 'Error retrieving data from WMI query.');
+  }
+  if (value.vt != VT_I8) {
+    VariantClear(&value);
+    return Status(-1, 'Invalid data type returned.');
+  }
+  ret = value.lVal;
+  VariantClear(&value);
+  return Status(0);
+}
+    
+    class RocksDBDatabasePluginTests : public DatabasePluginTests {
+ protected:
+  std::string name() override {
+    return 'rocksdb';
+  }
+};
+    
+     protected:
+  /// Require each plugin tester to implement a set name.
+  virtual std::string name() = 0;
+    
+    
+    {
+    {  std::map<std::string, size_t> expected = {{'name', 10}};
+  EXPECT_EQ(lengths, expected);
+}
+}
+
+    
+    
+    {  std::string path;
+  std::string action;
+};
+    
+      // Other methods
+  YGValue marginLeadingValue(const YGFlexDirection axis) const;
+  YGValue marginTrailingValue(const YGFlexDirection axis) const;
+  YGValue resolveFlexBasisPtr() const;
+  void resolveDimension();
+  YGDirection resolveDirection(const YGDirection ownerDirection);
+  void clearChildren();
+  /// Replaces the occurrences of oldChild with newChild
+  void replaceChild(YGNodeRef oldChild, YGNodeRef newChild);
+  void replaceChild(YGNodeRef child, uint32_t index);
+  void insertChild(YGNodeRef child, uint32_t index);
+  /// Removes the first occurrence of child
+  bool removeChild(YGNodeRef child);
+  void removeChild(uint32_t index);
+    
+    WIN_EXPORT void YGNodeCalculateLayout(const YGNodeRef node,
+                                      const float availableWidth,
+                                      const float availableHeight,
+                                      const YGDirection ownerDirection);
+    
+    #include <yoga/Yoga.h>
+    
+        static Config * create(void);
+    
+        void setFlex(double flex);
+    void setFlexBasis(double flexBasis);
+    void setFlexBasisPercent(double flexBasis);
+    void setFlexBasisAuto();
+    void setFlexGrow(double flexGrow);
+    void setFlexShrink(double flexShrink);
+    
+      std::string asFormattedString() const {
+    std::stringstream str;
+    str << 'Function ' << m_functionName << ' in file ' << m_fileName << ':' << m_lineNumber;
+    return str.str();
+  }
+    
+      T* get() const {
+    return m_ptr;
+  }
+    
+    
+    {}
