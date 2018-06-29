41aae66da071e85ebd3fc365bab284cb3cc09fce
SUNsung <weatherius@gmail.com>
2018-06-29 08:21:08 +0300

    Rewrite [235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb] File in: 2018-06-29 08:21:08

--- ruby/235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb
+++ ruby/235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb
@@ -1,32 +1,78 @@
 
         
-                  if options.fetch('skip_initial_build', false)
-            Jekyll.logger.warn 'Build Warning:', 'Skipping the initial build.' \
-                               ' This may result in an out-of-date site.'
-          else
-            build(site, options)
-          end
+              def initialize
+        @log_tags = []
     
-            # Add SSL to the stack if the user triggers --enable-ssl and they
-        # provide both types of certificates commonly needed.  Raise if they
-        # forget to add one of the certificates.
-        def enable_ssl(opts)
-          cert, key, src =
-            opts[:JekyllOptions].values_at('ssl_cert', 'ssl_key', 'source')
+          register_handler(BasicObject, BasicObjectHandler.new(self))
+      register_handler(Base, BaseHandler.new(self))
+      register_handler(Range, RangeHandler.new(self))
+      register_handler(Relation, RelationHandler.new)
+      register_handler(Array, ArrayHandler.new(self))
+      register_handler(Set, ArrayHandler.new(self))
+    end
     
-          begin
-        self.content = File.read(@path || site.in_source_dir(base, name),
-                                 Utils.merged_file_read_opts(site, opts))
-        if content =~ Document::YAML_FRONT_MATTER_REGEXP
-          self.content = $POSTMATCH
-          self.data = SafeYAML.load(Regexp.last_match(1))
+      def test_restrict_with_exception
+    firm = RestrictedWithExceptionFirm.create!(name: 'restrict')
+    firm.companies.create(name: 'child')
+    
+        # Block arg
+    periodically every: 3 do
+      ping
+    end
+    
+          env = Rack::MockRequest.env_for(
+        '/test',
+        'HTTP_CONNECTION' => 'upgrade', 'HTTP_UPGRADE' => 'websocket',
+          'HTTP_HOST' => 'localhost', 'HTTP_ORIGIN' => 'http://rubyonrails.org', 'rack.hijack' => CallMeMaybe.new
+      )
+    
+            expect(result).to eq('git rev-parse --short HEAD')
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('git rev-parse --short HEAD')
+      end
+    end
+    
+      def evernote_client
+    EvernoteOAuth::Client.new(
+      token:           evernote_oauth_token,
+      consumer_key:    evernote_consumer_key,
+      consumer_secret: evernote_consumer_secret,
+      sandbox:         use_sandbox?
+    )
+  end
+    
+          _form_configurable_fields[name] = options
+    end
+    
+      def tumblr_consumer_secret
+    ENV['TUMBLR_OAUTH_SECRET']
+  end
+    
+    module SortableTable
+  extend ActiveSupport::Concern
+    
+        @services = current_user.services.reorder(table_sort).page(params[:page])
+    
+            entries = []
+    
+        options[:container] = '#bd'
+    
+        def type=(value)
+      @type = value.try :strip
+    end
+    
+        delegate :empty?, :blank?, to: :pages
+    
+          if dest.end_with? '/'
+        dest_dir.relative_path_from(base_dir).to_s.tap do |result|
+          result << '/' if result != '.'
         end
-      rescue Psych::SyntaxError => e
-        Jekyll.logger.warn 'YAML Exception reading #{filename}: #{e.message}'
-        raise e if site.config['strict_front_matter']
-      rescue StandardError => e
-        Jekyll.logger.warn 'Error reading file #{filename}: #{e.message}'
-        raise e if site.config['strict_front_matter']
+      else
+        dest_dir.parent.relative_path_from(base_dir).join(dest.split('/').last).to_s
+      end
+    end
+    
+          def initialize(string)
+        @value = string
       end
     
               # Repackage the box
@@ -34,118 +80,64 @@
           output_path = Pathname.new(File.expand_path(output_name, FileUtils.pwd))
           box.repackage(output_path)
     
-            # Get the proper capability host to check
-        cap_host = nil
-        if type == :host
-          cap_host = @env.host
-        else
-          with_target_vms([]) do |vm|
-            cap_host = case type
-                       when :provider
-                         vm.provider
-                       when :guest
-                         vm.guest
-                       else
-                         raise Vagrant::Errors::CLIInvalidUsage,
-                           help: opts.help.chomp
-                       end
+              o.on('--check', 'Only checks for a capability, does not execute') do |f|
+            options[:check] = f
           end
         end
     
-    def report_apps
-  running = Set.new
-  @app_names.zip(@bundle_ids, @unix_ids).each do |app_name, bundle_id, _unix_id|
-    next if excluded_bundle_id bundle_id
-    next if excluded_app_name app_name
-    bundle_id.gsub!(%r{^(missing value)$}, '<\1>')
-    running.add '#{bundle_id}\t#{app_name}'
-  end
+    if $PROGRAM_NAME == __FILE__ && !ENV['COCOAPODS_NO_BUNDLER']
+  ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
+  require 'rubygems'
+  require 'bundler/setup'
+  $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+elsif ENV['COCOAPODS_NO_BUNDLER']
+  require 'rubygems'
+  gem 'cocoapods'
+end
     
-          def to_s
-        @pairs.inspect
+            # Removes the specified cache
+        #
+        # @param [Array<Hash>] cache_descriptors
+        #        An array of caches to remove, each specified with the same
+        #        hash as cache_descriptors_per_pod especially :spec_file and :slug
+        #
+        def remove_caches(cache_descriptors)
+          cache_descriptors.each do |desc|
+            UI.message('Removing spec #{desc[:spec_file]} (v#{desc[:version]})') do
+              FileUtils.rm(desc[:spec_file])
+            end
+            UI.message('Removing cache #{desc[:slug]}') do
+              FileUtils.rm_rf(desc[:slug])
+            end
+          end
+        end
+    
+            # Runs the template configuration utilities.
+        #
+        # @return [void]
+        #
+        def configure_template
+          UI.section('Configuring #{@name} template.') do
+            Dir.chdir(@name) do
+              if File.exist?('configure')
+                system({ 'COCOAPODS_VERSION' => Pod::VERSION }, './configure', @name, *@additional_args)
+              else
+                UI.warn 'Template does not have a configure file.'
+              end
+            end
+          end
+        end
+    
+            def find_address
+          if @order.bill_address_id == params[:id].to_i
+            @order.bill_address
+          elsif @order.ship_address_id == params[:id].to_i
+            @order.ship_address
+          else
+            raise CanCan::AccessDenied
+          end
+        end
       end
     end
   end
 end
-
-    
-          if path.extname == '.la'
-        path.unlink
-      elsif path.basename.to_s == 'perllocal.pod'
-        # Both this file & the .packlist one below are completely unnecessary
-        # to package & causes pointless conflict with other formulae. They are
-        # removed by Debian, Arch & MacPorts amongst other packagers as well.
-        # The files are created as part of installing any Perl module.
-        path.unlink
-      elsif path.basename.to_s == '.packlist' # Hidden file, not file extension!
-        path.unlink
-      else
-        # Set permissions for executables and non-executables
-        perms = if executable_path?(path)
-          0555
-        else
-          0444
-        end
-        if ARGV.debug?
-          old_perms = path.stat.mode & 0777
-          if perms != old_perms
-            puts 'Fixing #{path} permissions from #{old_perms.to_s(8)} to #{perms.to_s(8)}'
-          end
-        end
-        path.chmod perms
-      end
-    end
-  end
-end
-    
-    class HeadSoftwareSpec < SoftwareSpec
-  def initialize
-    super
-    @resource.version = Version.create('HEAD')
-  end
-    
-    module Sinatra
-  class Application < Base
-    
-          def initialize(*)
-        super
-    
-        it 'copes with nested arrays' do
-      mock_app do |env|
-        request = Rack::Request.new(env)
-        [200, {'Content-Type' => 'text/plain'}, [request.params['foo']['bar']]]
-      end
-      get '/', :foo => {:bar => '<bar>'}
-      expect(body).to eq('&lt;bar&gt;')
-    end
-    
-      it 'should set the X-Frame-Options' do
-    expect(get('/', {}, 'wants' => 'text/html').headers['X-Frame-Options']).to eq('SAMEORIGIN')
-  end
-    
-        expect_any_instance_of(detector).to receive(:call).with(
-      hash_including('foo.bar' => 42)
-    ).and_call_original
-    
-    module LogStash
-  module Api
-    module Commands
-      module System
-        class Plugins < Commands::Base
-          def run
-            { :total => plugins.count, :plugins => plugins }
-          end
-    
-      def file_fetch(url, sha1, target)
-    filename = File.basename( URI(url).path )
-    output = '#{target}/#{filename}'
-    begin
-      actual_sha1 = file_sha1(output)
-      if actual_sha1 != sha1
-        fetch(url, sha1, output)
-      end
-    rescue Errno::ENOENT
-      fetch(url, sha1, output)
-    end
-    return output
-  end
