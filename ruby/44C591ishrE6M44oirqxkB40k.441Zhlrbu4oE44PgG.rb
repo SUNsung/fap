3011fbd31fe54710e6e5f8450aec4541b5b488bc
SUNsung <weatherius@gmail.com>
2018-06-29 03:01:28 +0300

    Rewrite [44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb] File in: 2018-06-29 03:01:28

--- ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
+++ ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
@@ -1,129 +1,62 @@
 
         
-          def after_omniauth_failure_path_for(scope)
-    new_session_path(scope)
-  end
+              # Returns constant of subscription adapter specified in config/cable.yml.
+      # If the adapter cannot be found, this will default to the Redis adapter.
+      # Also makes sure proper dependencies are required.
+      def pubsub_adapter
+        adapter = (cable.fetch('adapter') { 'redis' })
     
-      def respond_to_on_destroy
-    # We actually need to hardcode this as Rails default responder doesn't
-    # support returning empty response on GET request
-    respond_to do |format|
-      format.all { head :no_content }
-      format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
-    end
-  end
-end
-
-    
-    module Devise
-  module Controllers
-    # A module that may be optionally included in a controller in order
-    # to provide remember me behavior. Useful when signing in is done
-    # through a callback, like in OmniAuth.
-    module Rememberable
-      # Return default cookie values retrieved from session options.
-      def self.cookie_values
-        Rails.configuration.session_options.slice(:path, :domain, :secure)
+          def changed_in_place?(raw_old_value, value)
+        return false if value.nil?
+        raw_new_value = encoded(value)
+        raw_old_value.nil? != raw_new_value.nil? ||
+          subtype.changed_in_place?(raw_old_value, raw_new_value)
       end
     
-          # Set up a subject doing an I18n lookup. At first, it attempts to set a subject
-      # based on the current mapping:
-      #
-      #   en:
-      #     devise:
-      #       mailer:
-      #         confirmation_instructions:
-      #           user_subject: '...'
-      #
-      # If one does not exist, it fallbacks to ActionMailer default:
-      #
-      #   en:
-      #     devise:
-      #       mailer:
-      #         confirmation_instructions:
-      #           subject: '...'
-      #
-      def subject_for(key)
-        I18n.t(:'#{devise_mapping.name}_subject', scope: [:devise, :mailer, key],
-          default: [:subject, key.to_s.humanize])
-      end
+        assert_equal 3, @pirate.birds.reload.length
+  ensure
+    Bird.connection.remove_index :birds, column: :name
+  end
+    
+        def private_method
+      'I am Jack's innermost fears and aspirations'
     end
+    
+        private
+      def subscribe_to(connection, identifiers:)
+        receive connection, command: 'subscribe', identifiers: identifiers
+      end
+    
+        def connect
+      @connected = true
+    end
+    
+      def all_groups(current_user)
+    groups = []
+    
+        if registration
+      u2f.authenticate!(challenges, response, Base64.decode64(registration.public_key), registration.counter)
+      registration.update(counter: response.counter)
+      true
+    end
+  rescue JSON::ParserError, NoMethodError, ArgumentError, U2F::Error
+    false
   end
 end
 
     
-        # Create magic predicates for verifying what module is activated by this map.
-    # Example:
-    #
-    #   def confirmable?
-    #     self.modules.include?(:confirmable)
-    #   end
-    #
-    def self.add_module(m)
-      class_eval <<-METHOD, __FILE__, __LINE__ + 1
-        def #{m}?
-          self.modules.include?(:#{m})
-        end
-      METHOD
-    end
-    
-      def parse(pkt)
-    # We want to return immediantly if	we do not have a packet which is handled by us
-    return unless pkt.is_tcp?
-    return if (pkt.tcp_sport != 80 and pkt.tcp_dport != 80)
-    s = find_session((pkt.tcp_sport == 80) ? get_session_src(pkt) : get_session_dst(pkt))
-    
-    fileOutJar 	= clsFile.new_with_sig('Ljava.lang.String;', 'output.jar')
-filesIn		= Array.new
-    
-      end
-    
-        if as == :json
-      if api_error?(data)
-        data = generate_error_hash(data)
-      else
-        selected_fields = extract_fields(filter.to_s.strip)
-        data.select! { |k,v| selected_fields.include?(k) } unless selected_fields.empty?
-        unless options.include?(:exclude_default_metadata)
-          data = data.to_hash
-          if data.values.size == 0 && selected_fields.size > 0
-            raise LogStash::Api::NotFoundError
+                # Register the handler if this is our first callback.
+            Signal.trap('INT') { fire_callbacks } if registered.length == 1
           end
-          data = default_metadata.merge(data)
         end
-      end
     
-      def _nt_number
-    start_index = index
-    if node_cache[:number].has_key?(index)
-      cached = node_cache[:number][index]
-      if cached
-        cached = SyntaxNode.new(input, index...(index + 1)) if cached == true
-        @index = cached.interval.end
-      end
-      return cached
-    end
+    module Vagrant
+  module Util
+    class IO
+      # The chunk size for reading from subprocess IO.
+      READ_CHUNK_SIZE = 4096
     
-      def download(url, output)
-    uri = URI(url)
-    digest = Digest::SHA1.new
-    tmp = '#{output}.tmp'
-    Net::HTTP.start(uri.host, uri.port, :use_ssl => (uri.scheme == 'https')) do |http|
-      request = Net::HTTP::Get.new(uri.path)
-      http.request(request) do |response|
-        fail 'HTTP fetch failed for #{url}. #{response}' if [200, 301].include?(response.code)
-        size = (response['content-length'].to_i || -1).to_f
-        count = 0
-        File.open(tmp, 'w') do |fd|
-          response.read_body do |chunk|
-            fd.write(chunk)
-            digest << chunk
-            if size > 0 && $stdout.tty?
-              count += chunk.bytesize
-              $stdout.write(sprintf('\r%0.2f%%', count/size * 100))
+                o.on('--provider PROVIDER', String,
+                 'The specific provider type for the box to remove') do |p|
+              options[:provider] = p
             end
-          end
-        end
-        $stdout.write('\r      \r') if $stdout.tty?
-      end
-    end
