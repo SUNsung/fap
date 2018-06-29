25e0ce64bd9234a0e3281397c359ad6cd482afc0
SUNsung <weatherius@gmail.com>
2018-06-29 12:49:21 +0300

    Rewrite [02vkU2nDm69A02kUEzm8sXi8Q02R19fXTyxxDE02ax8.cpp] File in: 2018-06-29 12:49:21

--- cccccc/02vkU2nDm69A02kUEzm8sXi8Q02R19fXTyxxDE02ax8.cpp
+++ cccccc/02vkU2nDm69A02kUEzm8sXi8Q02R19fXTyxxDE02ax8.cpp
@@ -1,160 +1,66 @@
 
         
-        // The argument separating arguments intended for the relauncher process from
-// those intended for the relaunched process. '---' is chosen instead of '--'
-// because CommandLine interprets '--' as meaning 'end of switches', but
-// for many purposes, the relauncher process' CommandLine ought to interpret
-// arguments intended for the relaunched process, to get the correct settings
-// for such things as logging and the user-data-dir in case it affects crash
-// reporting.
-extern const CharType* kRelauncherArgSeparator;
+        #include 'atom/browser/unresponsive_suppressor.h'
     
-      // views::NonClientFrameView:
-  gfx::Rect GetWindowBoundsForClientBounds(
-      const gfx::Rect& client_bounds) const override;
-  int NonClientHitTest(const gfx::Point& point) override;
+    namespace mate {
+    }
     
-    bool IsUnresponsiveEventSuppressed() {
-  return g_suppress_level > 0;
-}
+      // Sent when the chromium session of a particular user is started.
+  // If this is a new user on the machine this will not be sent until a profile
+  // picture has been selected, unlike NOTIFICATION_LOGIN_USER_CHANGED which is
+  // sent immediately after the user has logged in. This will be sent again if
+  // the browser crashes and restarts.
+  // The details are a chromeos::User object.
+  NOTIFICATION_SESSION_STARTED,
     
-    
-    {  DISALLOW_COPY_AND_ASSIGN(GlobalShortcutListenerWin);
-};
-    
-    #endif  // CHROME_BROWSER_PRINTING_PRINTING_UI_WEB_CONTENTS_OBSERVER_H_
+    #endif  // CHROME_BROWSER_CHROME_PROCESS_FINDER_WIN_H_
 
     
-    #include 'chrome/common/chrome_constants.h'
-    
-    bool TessResultRenderer::EndDocument() {
-  if (!happy_) return false;
-  bool ok = EndDocumentHandler();
-  if (next_) {
-    ok = next_->EndDocument() && ok;
-  }
-  return ok;
-}
-    
-    bool read_unlv_file(                    //print list of sides
-                     STRING name,        //basename of file
-                     int32_t xsize,        //image size
-                     int32_t ysize,        //image size
-                     BLOCK_LIST *blocks  //output list
-                    ) {
-  FILE *pdfp;                    //file pointer
-  BLOCK *block;                  //current block
-  int x;                         //current top-down coords
-  int y;
-  int width;                     //of current block
-  int height;
-  BLOCK_IT block_it = blocks;    //block iterator
+    // An interface the PrintViewManager uses to notify an observer when the print
+// dialog is shown. Register the observer via PrintViewManager::set_observer.
+class PrintViewManagerObserver {
+ public:
+  // Notifies the observer that the print dialog was shown.
+  virtual void OnPrintDialogShown() = 0;
     }
     
-    
-    {  // A group of pages that corresponds in some loose way to a document.
-  PointerVector<DocumentData> documents_;
-  // Strategy to use for caching and serializing data samples.
-  CachingStrategy cache_strategy_;
-  // Number of pages in the first document, used as a divisor in
-  // GetPageSequential to determine the document index.
-  int num_pages_per_doc_;
-  // Max memory allowed in this cache.
-  int64_t max_memory_;
-};
+    #include 'content/public/browser/web_contents_observer.h'
+#include 'ui/gfx/native_widget_types.h'
     
     
-#endif  // TESSERACT_CCSTRUCT_LINLSQ_H_
+    {  delete metric;
+  metric = xgboost::Metric::Create('error@0.1');
+  ASSERT_STREQ(metric->Name(), 'error@0.1');
+  EXPECT_STREQ(metric->Name(), 'error@0.1');
+  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 0, 1e-10);
+  EXPECT_NEAR(GetMetricEval(metric,
+                            {0.1f, 0.2f, 0.1f, 0.2f},
+                            {  0,   0,   1,   1}),
+              0.5f, 0.001f);
+}
+    
+    /*!
+ * \brief Macro to register linear updater.
+ */
+#define XGBOOST_REGISTER_LINEAR_UPDATER(UniqueId, Name)                        \
+  static DMLC_ATTRIBUTE_UNUSED ::xgboost::LinearUpdaterReg&                    \
+      __make_##LinearUpdaterReg##_##UniqueId##__ =                             \
+          ::dmlc::Registry< ::xgboost::LinearUpdaterReg>::Get()->__REGISTER__( \
+              Name)
+    
+    
+    {// Enable LOG(CONSOLE) for print messages to console.
+#define LOG_CONSOLE ::xgboost::ConsoleLogger()
+// Enable LOG(TRACKER) for print messages to tracker
+#define LOG_TRACKER ::xgboost::TrackerLogger()
+}  // namespace xgboost.
+#endif  // XGBOOST_LOGGING_H_
 
     
-    #endif
-
-    
-    DEFINE_int32(driver_port, 0, 'Port for communication with driver');
-DEFINE_int32(server_port, 0, 'Port for operation as a server');
-DEFINE_string(credential_type, grpc::testing::kInsecureCredentialsType,
-              'Credential type for communication with driver');
-    
-    namespace routeguide {
-class Feature;
-    }
-    
-    // Get leading or trailing comments in a string. Comment lines start with '// '.
-// Leading detached comments are put in in front of leading comments.
-template <typename DescriptorType>
-inline grpc::string GetNodeComments(const DescriptorType* desc, bool leading) {
-  return grpc_generator::GetPrefixedComments(desc, leading, '//');
-}
-    
-    
-    {  // Get all comments (leading, leading_detached, trailing) and print them as a
-  // docstring. Any leading space of a line will be removed, but the line
-  // wrapping will not be changed.
-  void PrintAllComments(std::vector<grpc::string> comments,
-                        grpc_generator::Printer* out);
+    /*!
+ * \brief Registry entry for objective factory functions.
+ */
+struct ObjFunctionReg
+    : public dmlc::FunctionRegEntryBase<ObjFunctionReg,
+                                        std::function<ObjFunction* ()> > {
 };
-    
-    
-    {  const Result start_;
-};
-    
-    
-    {}  // namespace grpc
-    
-    
-    {
-    {}  // namespace log
-}  // namespace leveldb
-    
-    TEST(LogTest, SkipIntoMultiRecord) {
-  // Consider a fragmented record:
-  //    first(R1), middle(R1), last(R1), first(R2)
-  // If initial_offset points to a record after first(R1) but before first(R2)
-  // incomplete fragment errors are not actual errors, and must be suppressed
-  // until a new first or full record is encountered.
-  Write(BigString('foo', 3*kBlockSize));
-  Write('correct');
-  StartReadingAt(kBlockSize);
-    }
-    
-    static void TestEncodeDecode(const VersionEdit& edit) {
-  std::string encoded, encoded2;
-  edit.EncodeTo(&encoded);
-  VersionEdit parsed;
-  Status s = parsed.DecodeFrom(encoded);
-  ASSERT_TRUE(s.ok()) << s.ToString();
-  parsed.EncodeTo(&encoded2);
-  ASSERT_EQ(encoded, encoded2);
-}
-    
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
-    
-    
-    {    ::wakeupLock_delete(object_);
-}
-    
-    void CommFrequencyLimit::__DelOlderTouchTime(uint64_t _time) {
-    for (std::list<uint64_t>::iterator iter = touch_times_.begin(); iter != touch_times_.end();) {
-        if ((_time - (*iter)) > time_span_) {
-            iter = touch_times_.erase(iter);
-            continue;
-        }
-    }
-    }
-    
-    #ifndef COMM_COMM_FREQUENCY_LIMIT_H_
-#define COMM_COMM_FREQUENCY_LIMIT_H_
-    
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
-    
-    
-    {    SPY_DETACH_VARIABLE('TestFun0 i');
-}
-    
-    
