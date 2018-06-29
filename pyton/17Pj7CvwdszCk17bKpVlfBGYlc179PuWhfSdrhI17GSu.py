c07b5083d3e7a35747f26449ffa0796aa8f00762
SUNsung <weatherius@gmail.com>
2018-06-29 04:37:31 +0300

    Rewrite [17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py] File in: 2018-06-29 04:37:30

--- pyton/17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py
+++ pyton/17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py
@@ -1,122 +1,83 @@
 
         
-        with open('update/LATEST_VERSION', 'w') as f:
-    f.write(version)
+          def testFailsOnRecurrentLinkedFeature(self):
+    component_spec = spec_pb2.ComponentSpec()
+    text_format.Parse('''
+        name: 'test'
+        network_unit {
+          registered_name: 'FeedForwardNetwork'
+          parameters {
+            key: 'hidden_layer_sizes' value: '64'
+          }
+        }
+        linked_feature {
+          name: 'features' embedding_dim: -1 size: 1
+          source_translator: 'identity'
+          source_component: 'test'
+          source_layer: 'layer_0'
+        }
+        ''', component_spec)
     
     
-class TestUnicodeLiterals(unittest.TestCase):
-    def test_all_files(self):
-        for dirpath, dirnames, filenames in os.walk(rootDir):
-            for ignore_dir in IGNORED_DIRS:
-                if ignore_dir in dirnames:
-                    # If we remove the directory from dirnames os.walk won't
-                    # recurse into it
-                    dirnames.remove(ignore_dir)
-            for basename in filenames:
-                if not basename.endswith('.py'):
-                    continue
-                if basename in IGNORED_FILES:
-                    continue
+class MockNetworkUnit(object):
     
-            webpage = self._download_webpage(url, video_id)
-    
-    from .common import InfoExtractor
-from ..utils import js_to_json
-    
-            runtime = self._search_regex(
-            r'Runtime\s*:\s*(.+?) \|', webpage, 'duration', default=None)
-        if runtime:
-            runtime = re.sub(r'[\s-]', '', runtime)
-        duration = parse_duration(runtime)
-        view_count = int_or_none(self._search_regex(
-            r'Views\s*:\s*(\d+)', webpage, 'view count', default=None))
-        comment_count = int_or_none(self._search_regex(
-            r'Comments\s*:\s*(\d+)', webpage, 'comment count', default=None))
+      def testArcPotentialsFromTokens(self):
+    with self.test_session():
+      # Batch of two, where the second batch item is the reverse of the first.
+      source_tokens = tf.constant([[[1, 2],
+                                    [2, 3],
+                                    [3, 4]],
+                                   [[3, 4],
+                                    [2, 3],
+                                    [1, 2]]],
+                                  tf.float32)  # pyformat: disable
+      target_tokens = tf.constant([[[4, 5, 6],
+                                    [5, 6, 7],
+                                    [6, 7, 8]],
+                                   [[6, 7, 8],
+                                    [5, 6, 7],
+                                    [4, 5, 6]]],
+                                  tf.float32)  # pyformat: disable
+      weights = tf.constant([[2, 3, 5],
+                             [7, 11, 13]],
+                            tf.float32)  # pyformat: disable
     
     
-@for_app('tar')
-def match(command):
-    return ('-C' not in command.script
-            and _is_tar_extract(command.script)
-            and _tar_file(command.script_parts) is not None)
+def main(unused_argv):
+  # Run the exporter.
+  export(FLAGS.master_spec, FLAGS.params_path, FLAGS.export_path,
+         FLAGS.export_moving_averages, FLAGS.build_runtime_graph)
+  tf.logging.info('Export complete.')
     
-            String:                    '#4e9a06',        # class: 's'
-        String.Backtick:           '#4e9a06',        # class: 'sb'
-        String.Char:               '#4e9a06',        # class: 'sc'
-        String.Doc:                'italic #8f5902', # class: 'sd' - like a comment
-        String.Double:             '#4e9a06',        # class: 's2'
-        String.Escape:             '#4e9a06',        # class: 'se'
-        String.Heredoc:            '#4e9a06',        # class: 'sh'
-        String.Interpol:           '#4e9a06',        # class: 'si'
-        String.Other:              '#4e9a06',        # class: 'sx'
-        String.Regex:              '#4e9a06',        # class: 'sr'
-        String.Single:             '#4e9a06',        # class: 's1'
-        String.Symbol:             '#4e9a06',        # class: 'ss'
+      Args:
+    master_spec: Proto master spec.
+    params_path: Path to the parameters file to export.
+    export_path: Path to export the SavedModel to.
+    external_graph: A tf.Graph() object to build the graph inside.
+    export_moving_averages: Whether to export the moving average parameters.
+    build_runtime_graph: Whether to build a graph for use by the runtime.
+    signature_name: Name of the signature to insert.
+  '''
+  tf.logging.info(
+      'Exporting graph with signature_name '%s' and use_moving_averages = %s' %
+      (signature_name, export_moving_averages))
     
-    
-def to_native_string(string, encoding='ascii'):
-    '''Given a string object, regardless of type, returns a representation of
-    that string in the native string type, encoding and decoding where
-    necessary. This assumes ASCII unless told otherwise.
-    '''
-    if isinstance(string, builtin_str):
-        out = string
-    else:
-        if is_py2:
-            out = string.encode(encoding)
-        else:
-            out = string.decode(encoding)
+          # A similar contract applies to the annotations.
+      self.checkOpOrder('annotations', anno['annotations'],
+                        ['GetSession', 'AttachDataReader', 'ReleaseSession'])
     
     
-class LookupDict(dict):
-    '''Dictionary lookup object.'''
+def test_config_missing_json():
+    app = flask.Flask(__name__)
+    with pytest.raises(IOError) as e:
+        app.config.from_json('missing.json')
+    msg = str(e.value)
+    assert msg.startswith('[Errno 2] Unable to load configuration '
+                          'file (No such file or directory):')
+    assert msg.endswith('missing.json'')
+    assert not app.config.from_json('missing.json', silent=True)
     
     
-class ReadTimeout(Timeout):
-    '''The server did not send any data in the allotted amount of time.'''
-    
-                        for i in request.body:
-                        low_conn.send(hex(len(i))[2:].encode('utf-8'))
-                        low_conn.send(b'\r\n')
-                        low_conn.send(i)
-                        low_conn.send(b'\r\n')
-                    low_conn.send(b'0\r\n\r\n')
-    
-        def contribute(self):
-        self.blackboard.common_state['problems'] += random.randint(1, 2)
-        self.blackboard.common_state['suggestions'] += random.randint(10, 20)
-        self.blackboard.common_state['contributions'] += [self.__class__.__name__]
-        self.blackboard.common_state['progress'] += random.randint(10, 100)
-    
-        @classmethod
-    def __get_test_directory(self):
-        '''
-        Get the temporary directory for the tests.
-        '''
-        self.test_dir = os.path.join(os.path.dirname(
-            os.path.realpath(__file__)), 'test_command')
-    
-        def test_data_change_shall_notify_all_observers_once(cls):
-        with patch.object(cls.dec_obs, 'update') as mock_dec_obs_update,\
-                patch.object(cls.hex_obs, 'update') as mock_hex_obs_update:
-            cls.sub.data = 10
-            cls.assertEqual(mock_dec_obs_update.call_count, 1)
-            cls.assertEqual(mock_hex_obs_update.call_count, 1)
-    
-        def __init__(self, name):
-        self.name = name
-    
-    '''
-Port of the Java example of 'Setter Injection' in
-'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
-(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
-http://xunitpatterns.com/Dependency%20Injection.html.
-    
-        def now(self):
-        current_time_is_always_midnight = '24:01'
-        return current_time_is_always_midnight
-
-    
-    *TL;DR80
-Provides the ability to restore an object to its previous state.
-'''
+def __kilometers_to_meters(kilometers: float) -> float:
+    '''Convert kilometers to meters.'''
+    return kilometers * 1000
