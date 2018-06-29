3fc2eaa965a871a8f1f890a6681591ddd934f9b1
SUNsung <weatherius@gmail.com>
2018-06-29 23:28:51 +0300

    Rewrite [55riqu5Bbq95w55YC4lfYmkM9M55sMfssyFUTyQ5567.rb] File in: 2018-06-29 23:28:50

--- ruby/55riqu5Bbq95w55YC4lfYmkM9M55sMfssyFUTyQ5567.rb
+++ ruby/55riqu5Bbq95w55YC4lfYmkM9M55sMfssyFUTyQ5567.rb
@@ -1,10 +1,114 @@
 
         
-          def destroy
-    @event.destroy
+        CONTENT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
     
-      def prefixes
-    prefixes = ['/bin', '/usr/bin', '/usr/libexec', xcode_app_path]
-    prefixes << `brew --prefix`.strip unless `which brew`.strip.empty?
+        # Read the allowed documents into the collection's array of docs.
+    #
+    # Returns the sorted array of docs.
+    def read
+      filtered_entries.each do |file_path|
+        full_path = collection_dir(file_path)
+        next if File.directory?(full_path)
+        if Utils.has_yaml_header? full_path
+          read_document(full_path)
+        else
+          read_static_file(file_path, full_path)
+        end
+      end
+      docs.sort!
+    end
     
-            private
+            def setup(destination)
+          require_relative 'serve/servlet'
+    
+        def style_to_permalink(permalink_style)
+      case permalink_style.to_sym
+      when :pretty
+        '/:categories/:year/:month/:day/:title/'
+      when :none
+        '/:categories/:title:output_ext'
+      when :date
+        '/:categories/:year/:month/:day/:title:output_ext'
+      when :ordinal
+        '/:categories/:year/:y_day/:title:output_ext'
+      else
+        permalink_style.to_s
+      end
+    end
+    
+          def custom_processor
+        converter_name = @config['markdown']
+        self.class.const_get(converter_name).new(@config) if custom_class_allowed?(converter_name)
+      end
+    
+        def process(args)
+      arg_is_present? args, '--server', 'The --server command has been replaced by the \
+                          'serve' subcommand.'
+      arg_is_present? args, '--serve', 'The --serve command has been replaced by the \
+                          'serve' subcommand.'
+      arg_is_present? args, '--no-server', 'To build Jekyll without launching a server, \
+                          use the 'build' subcommand.'
+      arg_is_present? args, '--auto', 'The switch '--auto' has been replaced with \
+                          '--watch'.'
+      arg_is_present? args, '--no-auto', 'To disable auto-replication, simply leave off \
+                          the '--watch' switch.'
+      arg_is_present? args, '--pygments', 'The 'pygments'settings has been removed in \
+                          favour of 'highlighter'.'
+      arg_is_present? args, '--paginate', 'The 'paginate' setting can only be set in \
+                          your config files.'
+      arg_is_present? args, '--url', 'The 'url' setting can only be set in your \
+                          config files.'
+      no_subcommand(args)
+    end
+    
+      before do
+    Spaceship::Portal::Key.client = mock_client
+  end
+    
+        # Returns the unwrapped subject and body of the last commit
+    # <b>DEPRECATED:</b> Use <tt>last_git_commit_message</tt> instead.
+    def self.last_git_commit
+      UI.important('`last_git_commit` is deprecated. Please use `last_git_commit_message` instead.')
+      last_git_commit_message
+    end
+    
+            expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to match(/cd .* && agvtool new-marketing-version 2.0.0/)
+      end
+    
+    module Sinatra
+  class Application < Base
+    
+          def accepts?(env)
+        cookie_header = env['HTTP_COOKIE']
+        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
+        cookies.each do |k, v|
+          if k == session_key && Array(v).size > 1
+            bad_cookies << k
+          elsif k != session_key && Rack::Utils.unescape(k) == session_key
+            bad_cookies << k
+          end
+        end
+        bad_cookies.empty?
+      end
+    
+      it 'should set the X-Frame-Options' do
+    expect(get('/', {}, 'wants' => 'text/html').headers['X-Frame-Options']).to eq('SAMEORIGIN')
+  end
+    
+      # These two settings work together to allow you to limit a spec run
+  # to individual examples or groups you care about by tagging them with
+  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
+  # get run.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
