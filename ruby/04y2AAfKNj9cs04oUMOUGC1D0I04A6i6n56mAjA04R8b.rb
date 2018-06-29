82686c652810cf241b7822f22e7ac1e70a538b33
SUNsung <weatherius@gmail.com>
2018-06-29 07:11:06 +0300

    Rewrite [04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb] File in: 2018-06-29 07:11:05

--- ruby/04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb
+++ ruby/04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb
@@ -1,52 +1,94 @@
 
         
-              if options[:type] == :array && (options[:values].blank? || !options[:values].is_a?(Array))
-        raise ArgumentError.new('When using :array as :type you need to provide the :values as an Array')
-      end
+            If you wish to use the 2.x release you can install it
+    either via Homebrew:
+      brew install homebrew/binary/ngrok2
     
-      def tumblr_consumer_secret
-    ENV['TUMBLR_OAUTH_SECRET']
+    class BuildEnvironment
+  def initialize(*settings)
+    @settings = Set.new(*settings)
   end
     
-    require 'open3'
+      # True if a {Formula} is being built universally.
+  # e.g. on newer Intel Macs this means a combined x86_64/x86 binary/library.
+  # <pre>args << '--universal-binary' if build.universal?</pre>
+  def universal?
+    include?('universal') && option_defined?('universal')
+  end
     
-    class AdminsController < Admin::AdminController
-  include ApplicationHelper
+        # Exclude cache, logs, and repository, if they are located under the prefix.
+    [HOMEBREW_CACHE, HOMEBREW_LOGS, HOMEBREW_REPOSITORY].each do |dir|
+      dirs.delete dir.relative_path_from(HOMEBREW_PREFIX).to_s
+    end
+    dirs.delete 'etc'
+    dirs.delete 'var'
     
-      # Implemented by subclasses to hook into Capistrano's deployment flow using
-  # using the `before` and `after` DSL methods. Note that `register_hooks` will
-  # not be called if the user has opted-out of hooks when installing the plugin.
-  #
-  # Example:
-  #
-  #   def register_hooks
-  #     after 'deploy:updated', 'my_plugin:do_something'
-  #   end
-  #
-  def register_hooks; end
+            if msg = blacklisted?(query)
+          if count > 0
+            puts
+            puts 'If you meant #{query.inspect} precisely:'
+            puts
+          end
+          puts msg
+        elsif count == 0
+          puts 'No formula found for #{query.inspect}.'
+          begin
+            GitHub.print_pull_requests_matching(query)
+          rescue GitHub::Error => e
+            SEARCH_ERROR_QUEUE << e
+          end
+        end
+      end
+    end
     
-              it 'returns all release servers' do
-            expect(subject.map(&:hostname)).to eq %w{example1.com example2.com example3.com example4.com}
+        unless updated_taps.empty?
+      puts 'Updated #{updated_taps.size} tap#{plural(updated_taps.size)} ' \
+           '(#{updated_taps.join(', ')}).'
+      updated = true
+    end
+    
+      def self.class_s(name)
+    Formulary.class_s(name)
+  end
+    
+            if entries.empty?
+          css('.quickindex[id]').each do |node|
+            name = node['id'].remove(/quickindex\.?/)
+            next if name.empty? || name =~ /\.\d+\z/ || name =~ /\A([^\.]+)\.\1\z/
+            entries << ['#{self.name}.#{name}', name]
           end
         end
     
-    When /^(?:|I )fill in '([^']*)' with '([^']*)'$/ do |field, value|
-  fill_in(field, :with => value)
-end
-    
-        def definitions_for(klass)
-      parent_classes = klass.ancestors.reverse
-      parent_classes.each_with_object({}) do |ancestor, inherited_definitions|
-        inherited_definitions.deep_merge! @attachments[ancestor]
-      end
+          options[:only_patterns] = [/\Agetting-started\//, /\Alayout\//, /\Acontent\//, /\Acomponents\//, /\Autilities\//, /\Amigration\//]
     end
-  end
-end
-
     
-            def rejected_types_rejected?
-          @missing_rejected_types ||= @rejected_types.select { |type| type_allowed?(type) }
-          @missing_rejected_types.none?
+        options[:attribution] = <<-HTML
+      &copy; Django Software Foundation and individual contributors<br>
+      Licensed under the BSD License.
+    HTML
+    
+    module Docs
+  class Tensorflow < UrlScraper
+    self.name = 'TensorFlow'
+    self.type = 'tensorflow'
+    self.release = '1.8'
+    self.root_path = 'index.html'
+    self.links = {
+      home: 'https://www.tensorflow.org/',
+      code: 'https://github.com/tensorflow/tensorflow'
+    }
+    
+        def initialize(name = nil, path = nil, type = nil)
+      self.name = name
+      self.path = path
+      self.type = type
+    
+            def define_logger(name, options = {})
+          class_eval <<-RUBY, __FILE__, __LINE__ + 1
+            def #{name}(message)
+              #{options.fetch(:to, :log)}(#{name.inspect}, message)
+            end
+          RUBY
         end
       end
     end
@@ -54,18 +96,20 @@ end
 end
 
     
-            def failure_message_when_negated
-          'Attachment #{@attachment_name} cannot be between #{@low} and #{@high} bytes'
-        end
-        alias negative_failure_message failure_message_when_negated
+      Sass::Plugin.options.merge!(config)
     
-            raise LoadError, 'Could not find the '#{name}' processor in any of these paths: #{directories.join(', ')}' unless required.any?
+      if options.respond_to? 'keys'
+    options.each do |k,v|
+      unless v.nil?
+        v = v.join ',' if v.respond_to? 'join'
+        v = v.to_json if v.respond_to? 'keys'
+        output += ' data-#{k.sub'_','-'}='#{v}''
       end
     end
-    
-          def check_validity!
-        unless options.has_key?(:content_type) || options.has_key?(:not)
-          raise ArgumentError, 'You must pass in either :content_type or :not to the validator'
-        end
-      end
-    end
+  elsif options.respond_to? 'join'
+    output += ' data-value='#{config[key].join(',')}''
+  else
+    output += ' data-value='#{config[key]}''
+  end
+  output += '></#{tag}>'
+end
