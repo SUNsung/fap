f70dfd47f349b18366d5e1ccb61a5594d4f1d78c
SUNsung <weatherius@gmail.com>
2018-06-30 09:06:56 +0300

    Rewrite [28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp] File in: 2018-06-30 09:06:55

--- cccccc/28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp
+++ cccccc/28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp
@@ -1,143 +1,134 @@
 
         
-        #endif  // ATOM_BROWSER_UNRESPONSIVE_SUPPRESSOR_H_
-
-    
-      // Returns whether current process is browser process, currently we detect it
-  // by checking whether current has used V8 Lock, but it might be a bad idea.
-  static inline bool IsBrowserProcess() { return v8::Locker::IsActive(); }
-    
-    #endif  // ATOM_COMMON_DRAGGABLE_REGION_H_
-
-    
-      // Sent when the Ash session has started. In its current incantation this is
-  // generated when the metro app has connected to the browser IPC channel.
-  // Used only on Windows.
-  NOTIFICATION_ASH_SESSION_STARTED,
-  // Sent when the Ash session ended. Currently this means the metro app exited.
-  // Used only on Windows.
-  NOTIFICATION_ASH_SESSION_ENDED,
-#endif
+        namespace atom {
+    }
     
     #include 'base/macros.h'
-#include 'ui/events/keycodes/keyboard_codes.h'
+#include 'ui/events/event_rewriter.h'
     
-    // File name of the Pepper Flash plugin on different platforms.
-extern const base::FilePath::CharType kPepperFlashPluginFilename[];
+    class UnresponsiveSuppressor {
+ public:
+  UnresponsiveSuppressor();
+  ~UnresponsiveSuppressor();
+    }
     
-    #if GTEST_HAS_STD_WSTRING
-  // Converts the given wide string to a narrow string using the UTF-8
-  // encoding, and streams the result to this Message object.
-  Message& operator <<(const ::std::wstring& wstr);
-#endif  // GTEST_HAS_STD_WSTRING
+    #include 'chrome/common/chrome_constants.h'
     
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12, typename T13, typename T14, typename T15,
-    typename T16, typename T17, typename T18, typename T19, typename T20,
-    typename T21, typename T22, typename T23, typename T24, typename T25,
-    typename T26, typename T27, typename T28, typename T29, typename T30,
-    typename T31, typename T32, typename T33, typename T34, typename T35>
-internal::ValueArray35<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
-    T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
-    T29, T30, T31, T32, T33, T34, T35> Values(T1 v1, T2 v2, T3 v3, T4 v4,
-    T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12, T13 v13,
-    T14 v14, T15 v15, T16 v16, T17 v17, T18 v18, T19 v19, T20 v20, T21 v21,
-    T22 v22, T23 v23, T24 v24, T25 v25, T26 v26, T27 v27, T28 v28, T29 v29,
-    T30 v30, T31 v31, T32 v32, T33 v33, T34 v34, T35 v35) {
-  return internal::ValueArray35<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
-      T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25,
-      T26, T27, T28, T29, T30, T31, T32, T33, T34, T35>(v1, v2, v3, v4, v5, v6,
-      v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21,
-      v22, v23, v24, v25, v26, v27, v28, v29, v30, v31, v32, v33, v34, v35);
+    #if defined(OS_MACOSX)
+// NOTE: if you change the value of kFrameworkName, please don't forget to
+// update components/test/run_all_unittests.cc as well.
+// TODO(tfarina): Remove the comment above, when you fix components to use plist
+// on Mac.
+extern const base::FilePath::CharType kFrameworkName[];
+#endif  // OS_MACOSX
+    
+    namespace content {
+class RenderProcessHost;
+class Shell;
+}
+    
+    void Base::CallSync(const std::string& method,
+                    const base::ListValue& arguments,
+                    base::ListValue* result) {
+  NOTREACHED() << 'Uncatched callAsync in Base'
+               << ' method:' << method
+               << ' arguments:' << arguments;
+}
+    
+    #endif  // CONTENT_NW_SRC_API_BASE_BASE_H_
+
+    
+    namespace {
+RenderView* GetRenderView(v8::Handle<v8::Context> ctx) {
+  WebLocalFrame* frame = WebLocalFrame::frameForContext(ctx);
+  if (!frame || !frame->isNodeJS())
+    return NULL;
+    }
+    }
+    
+    namespace remote {
+    }
+    
+     private:
+  ObjectManager* object_manager_;
+    
+    void Menu::UpdateKeys(views::FocusManager *focus_manager){
+  if (focus_manager == NULL){
+    return ;
+  } else {
+    focus_manager_ = focus_manager;
+    for(auto* item : menu_items_) {
+      item->UpdateKeys(focus_manager);
+    }
+  }
+}
+    
+    void MenuItem::CallSync(const std::string& method,
+                        const base::ListValue& arguments,
+                        base::ListValue* result) {
+  if (method == 'GetChecked') {
+    result->AppendBoolean(GetChecked());
+  } else {
+    NOTREACHED() << 'Invalid call to MenuItem method:' << method
+                 << ' arguments:' << arguments;
+  }
 }
     
     
-    {  // Tersely prints the first N fields of a tuple to a string vector,
-  // one element for each field.
-  template <typename Tuple>
-  static void TersePrintPrefixToStrings(const Tuple& t, Strings* strings) {
-    TuplePrefixPrinter<N - 1>::TersePrintPrefixToStrings(t, strings);
-    ::std::stringstream ss;
-    UniversalTersePrint(::std::tr1::get<N - 1>(t), &ss);
-    strings->push_back(ss.str());
-  }
+    {#endif
+}  // namespace nwapi
+    
+        bool Read(ClipboardData& data) {
+      switch(data.type) {
+        case TYPE_TEXT:
+        return ReadText(data);
+        break;
+        case TYPE_HTML:
+        return ReadHTML(data);
+        break;
+        case TYPE_RTF:
+        return ReadRTF(data);
+        break;
+        case TYPE_PNG:
+        case TYPE_JPEG:
+        return ReadImage(data);
+        break;
+        case TYPE_NONE:
+        NOTREACHED();
+        return false;
+      }
+      NOTREACHED();
+      return false;      
+    }
+    
+    class NwMenuGetNSStringFWithFixupFunction : public NWSyncExtensionFunction {
+ public:
+  NwMenuGetNSStringFWithFixupFunction() {}
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    
+ protected:
+  ~NwMenuGetNSStringFWithFixupFunction() override {}
+    
+  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringFWithFixup', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringFWithFixupFunction);
 };
     
-      // Same as above, but you can choose the interception scope of this object.
-  ScopedFakeTestPartResultReporter(InterceptMode intercept_mode,
-                                   TestPartResultArray* result);
-    
-    // This interface knows how to report a test part result.
-class TestPartResultReporterInterface {
+    class NwObjDestroyFunction : public NWSyncExtensionFunction {
  public:
-  virtual ~TestPartResultReporterInterface() {}
+  NwObjDestroyFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
     }
     
-    $for m [[
-    f$(m)_ = t.f$(m)_;
-    
-    // Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
-int Factorial(int n) {
-  int result = 1;
-  for (int i = 1; i <= n; i++) {
-    result *= i;
-  }
-    }
-    
-    /**
- * Install the terminate / unexpected handlers to dump exceptions.
- */
-void installHandlers();
-    
-     public:
-  HazptrWideCAS() : node_(new Node()) {}
-    
-      size_t size() override {
-    size_t size = 0;
-    for (auto& q : queues_) {
-      size += q.size();
-    }
-    return size;
+      bool NwScreenStartMonitorFunction::RunNWSync(base::ListValue* response, std::string* error) {
+    bool screens, windows;
+    EXTENSION_FUNCTION_VALIDATE(args_->GetBoolean(0, &screens));
+    EXTENSION_FUNCTION_VALIDATE(args_->GetBoolean(1, &windows));
+    NwDesktopCaptureMonitor::GetInstance()->Start(screens, windows);
+    return true;
   }
     
-    namespace folly {
-    }
     
-    
-    {  auto const slack = currentCapacity - currentSize;
-  if (slack * 2 > currentSize) {
-    // Too much slack, malloc-copy-free cycle:
-    auto const result = checkedMalloc(newCapacity);
-    std::memcpy(result, p, currentSize);
-    free(p);
-    return result;
-  }
-  // If there's not too much slack, we realloc in hope of coalescing
-  return checkedRealloc(p, newCapacity);
-}
-    
-      /**
-   * Add one task for execution in the parent executor
-   */
-  void add(Func func) override;
-    
-    FOLLY_ALWAYS_INLINE int __builtin_ffs(int x) {
-  unsigned long index;
-  return int(_BitScanForward(&index, (unsigned long)x) ? index + 1 : 0);
-}
-    
-    off_t lseek(int fh, off_t off, int orig) {
-  return _lseek(fh, off, orig);
-}
-    
-      MockClock::Now += std::chrono::seconds{1};
-    
-      // Actions to run in child.
-  // Note that this runs after vfork(), so tread lightly.
-  // Returns 0 on success, or an errno value on failure.
-  int prepareChild(const Options& options,
-                   const sigset_t* sigmask,
-                   const char* childDir) const;
-  int runChild(const char* executable, char** argv, char** env,
-               const Options& options) const;
+    {  private:
+    DISALLOW_COPY_AND_ASSIGN(NwScreenIsMonitorStartedFunction);
+  };
