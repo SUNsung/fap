93e153f13a7158f0de9fde6a0d7b972b4ac4fc21
SUNsung <weatherius@gmail.com>
2018-06-30 07:04:04 +0300

    Rewrite [28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb] File in: 2018-06-30 07:04:04

--- ruby/28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb
+++ ruby/28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb
@@ -1,87 +1,34 @@
 
         
-        class JavaScriptHelperTest < ActionView::TestCase
-  tests ActionView::Helpers::JavaScriptHelper
-    
-              # Bubbled up from the adapter require. Prefix the exception message
-          # with some guidance about how to address it and reraise.
+                  if @address.update_attributes(address_params)
+            respond_with(@address, default_template: :show)
           else
-            raise e.class, 'Error loading the '#{adapter}' Action Cable pubsub adapter. Missing a gem it depends on? #{e.message}', e.backtrace
+            invalid_resource!(@address)
           end
         end
     
-        # Define how a class is converted to Arel nodes when passed to +where+.
-    # The handler can be any object that responds to +call+, and will be used
-    # for any value that +===+ the class given. For example:
-    #
-    #     MyCustomDateRange = Struct.new(:start, :end)
-    #     handler = proc do |column, range|
-    #       Arel::Nodes::Between.new(column,
-    #         Arel::Nodes::And.new([range.start, range.end])
-    #       )
-    #     end
-    #     ActiveRecord::PredicateBuilder.new('users').register_handler(MyCustomDateRange, handler)
-    def register_handler(klass, handler)
-      @handlers.unshift([klass, handler])
-    end
-    
-    require 'test_helper'
-require 'stubs/test_server'
-    
-    require 'cocoapods'
-    
-    profile = Profile.new
-# puts profile.generate
-command = ['/usr/bin/sandbox-exec', '-p', profile.generate, profile.pod_bin, *ARGV]
-exec(*command)
-
-    
-            def initialize(argv)
-          @pod_name = argv.shift_argument
-          @short_output = argv.flag?('short')
-          super
-        end
-    
-          def run
-        UI.puts report
-      end
-    
-            def execute_repl_command(repl_command)
-          unless repl_command == '\n'
-            repl_commands = repl_command.split
-            subcommand = repl_commands.shift.capitalize
-            arguments = repl_commands
-            subcommand_class = Pod::Command::IPC.const_get(subcommand)
-            subcommand_class.new(CLAide::ARGV.new(arguments)).run
-            signal_end_of_output
-          end
-        end
-      end
-    end
+      def render(source)
+    template = File.read(source)
+    renderer = ERB.new(template)
+    context  = LogStash::PluginManager::RenderContext.new(options)
+    renderer.result(context.get_binding)
   end
-end
-
     
-      def download(url, output)
-    uri = URI(url)
-    digest = Digest::SHA1.new
-    tmp = '#{output}.tmp'
-    Net::HTTP.start(uri.host, uri.port, :use_ssl => (uri.scheme == 'https')) do |http|
-      request = Net::HTTP::Get.new(uri.path)
-      http.request(request) do |response|
-        fail 'HTTP fetch failed for #{url}. #{response}' if [200, 301].include?(response.code)
-        size = (response['content-length'].to_i || -1).to_f
-        count = 0
-        File.open(tmp, 'w') do |fd|
-          response.read_body do |chunk|
-            fd.write(chunk)
-            digest << chunk
-            if size > 0 && $stdout.tty?
-              count += chunk.bytesize
-              $stdout.write(sprintf('\r%0.2f%%', count/size * 100))
-            end
-          end
+    module LogStash::Api::AppHelpers
+  # This method handle both of the normal flow *happy path*
+  # and the display or errors, if more custom logic is added here
+  # it will make sense to separate them.
+  #
+  # See `#error` method in the `LogStash::Api::Module::Base`
+  def respond_with(data, options={})
+    as     = options.fetch(:as, :json)
+    filter = options.fetch(:filter, '')
+    
+          # Only print require() calls that did actual work.
+      # require() returns true on load, false if already loaded.
+      if result
+        source = caller[0]
+        #p source.include?('/lib/polyglot.rb:63:in `require'') => source
+        if source.include?('/lib/polyglot.rb:63:in `require'')
+          source = caller[1]
         end
-        $stdout.write('\r      \r') if $stdout.tty?
-      end
-    end
