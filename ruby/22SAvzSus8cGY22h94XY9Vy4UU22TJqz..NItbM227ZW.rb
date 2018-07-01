480e30dcefb58fc475c9e15f073a0ac0de1f794c
SUNsung <weatherius@gmail.com>
2018-07-01 22:02:08 +0300

    Rewrite [22SAvzSus8cGY22h94XY9Vy4UU22TJqz..NItbM227ZW.rb] File in: 2018-07-01 22:02:07

--- ruby/22SAvzSus8cGY22h94XY9Vy4UU22TJqz..NItbM227ZW.rb
+++ ruby/22SAvzSus8cGY22h94XY9Vy4UU22TJqz..NItbM227ZW.rb
@@ -1,133 +1,128 @@
 
         
-          attr_accessor :output_buffer
-  attr_reader :request
+          def create
+    return render_404 unless can_read?(resource)
     
-            if routes &&
-           (routes.named_routes.route_defined?(selector) ||
-             routes.mounted_helpers.method_defined?(selector))
-          @controller.__send__(selector, *args)
-        else
+        begin
+      response = U2F::RegisterResponse.load_from_json(params[:device_response])
+      registration_data = u2f.register!(challenges, response)
+      registration.update(certificate: registration_data.certificate,
+                          key_handle: registration_data.key_handle,
+                          public_key: registration_data.public_key,
+                          counter: registration_data.counter,
+                          user: user,
+                          name: params[:name])
+    rescue JSON::ParserError, NoMethodError, ArgumentError
+      registration.errors.add(:base, 'Your U2F device did not send a valid JSON response.')
+    rescue U2F::Error => e
+      registration.errors.add(:base, e.message)
+    end
+    
+      module ClassMethods
+    def load_types_in(module_name, my_name = module_name.singularize)
+      const_set(:MODULE_NAME, module_name)
+      const_set(:BASE_CLASS_NAME, my_name)
+      const_set(:TYPES, Dir[Rails.root.join('app', 'models', module_name.underscore, '*.rb')].map { |path| module_name + '::' + File.basename(path, '.rb').camelize })
+    end
+    
+        def form_configurable_attributes
+      form_configurable_fields.keys
+    end
+  end
+end
+
+    
+        respond_to do |format|
+      format.html { redirect_to services_path }
+      format.json { head :no_content }
+    end
+  end
+    
+      def failure_message
+    exception = request.respond_to?(:get_header) ? request.get_header('omniauth.error') : request.env['omniauth.error']
+    error   = exception.error_reason if exception.respond_to?(:error_reason)
+    error ||= exception.error        if exception.respond_to?(:error)
+    error ||= (request.respond_to?(:get_header) ? request.get_header('omniauth.error.type') : request.env['omniauth.error.type']).to_s
+    error.to_s.humanize if error
+  end
+    
+      # POST /resource/confirmation
+  def create
+    self.resource = resource_class.send_confirmation_instructions(resource_params)
+    yield resource if block_given?
+    
+        def unlock_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :unlock_instructions, opts)
+    end
+    
+          # Set up a subject doing an I18n lookup. At first, it attempts to set a subject
+      # based on the current mapping:
+      #
+      #   en:
+      #     devise:
+      #       mailer:
+      #         confirmation_instructions:
+      #           user_subject: '...'
+      #
+      # If one does not exist, it fallbacks to ActionMailer default:
+      #
+      #   en:
+      #     devise:
+      #       mailer:
+      #         confirmation_instructions:
+      #           subject: '...'
+      #
+      def subject_for(key)
+        I18n.t(:'#{devise_mapping.name}_subject', scope: [:devise, :mailer, key],
+          default: [:subject, key.to_s.humanize])
+      end
+    end
+  end
+end
+
+    
+        export LANG=en_US.UTF-8
+    \e[0m
+    DOC
+  end
+    
+          def self.options
+        [
+          ['--update', 'Run `pod repo update` before listing'],
+          ['--stats',  'Show additional stats (like GitHub watchers and forks)'],
+        ].concat(super)
+      end
+    
+    desc 'Generate jekyll site'
+task :generate do
+  raise '### You haven't set anything up yet. First run `rake install` to set up an Octopress theme.' unless File.directory?(source_dir)
+  puts '## Generating Site with Jekyll'
+  system 'compass compile --css-dir #{source_dir}/stylesheets'
+  system 'jekyll build'
+end
+    
+    run SinatraStaticServer
+
+    
+    class ConfigTag < Liquid::Tag
+  def initialize(tag_name, options, tokens)
+    super
+    options = options.split(' ').map {|i| i.strip }
+    @key = options.slice!(0)
+    @tag = nil
+    @classname = nil
+    options.each do |option|
+      @tag = $1 if option =~ /tag:(\S+)/ 
+      @classname = $1 if option =~ /classname:(\S+)/
+    end
+  end
+    
+      class ImageTag < Liquid::Tag
+    @img = nil
+    
           super
-        end
-      end
-    
-      def persisted?
-    id.present?
-  end
-    
-    module ActionMailer
-  # This module handles everything related to mail delivery, from registering
-  # new delivery methods to configuring the mail object to be sent.
-  module DeliveryMethods
-    extend ActiveSupport::Concern
-    
-        # An email was generated.
-    def process(event)
-      debug do
-        mailer = event.payload[:mailer]
-        action = event.payload[:action]
-        '#{mailer}##{action}: processed outbound mail in #{event.duration.round(1)}ms'
-      end
     end
     
-    module ActionMailer
-  class NonInferrableMailerError < ::StandardError
-    def initialize(name)
-      super 'Unable to determine the mailer to test from #{name}. ' \
-        'You'll need to specify it using tests YourMailer in your ' \
-        'test case definition'
-    end
-  end
-    
-      def test_assert_select_email_multipart
-    AssertMultipartSelectMailer.test(html: '<div><p>foo</p><p>bar</p></div>', text: 'foo bar').deliver_now
-    assert_select_email do
-      assert_select 'div:root' do
-        assert_select 'p:first-child', 'foo'
-        assert_select 'p:last-child', 'bar'
-      end
-    end
-  end
-end
-
-    
-    class I18nTestMailer < ActionMailer::Base
-  configure do |c|
-    c.assets_dir = ''
-  end
-    
-    require 'pry'
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-require 'jekyll'
-    
-    #
-    
-    class Jekyll::Commands::NewTheme < Jekyll::Command
-  class << self
-    def init_with_program(prog)
-      prog.command(:'new-theme') do |c|
-        c.syntax 'new-theme NAME'
-        c.description 'Creates a new Jekyll theme scaffold'
-        c.option 'code_of_conduct', \
-          '-c', '--code-of-conduct', \
-          'Include a Code of Conduct. (defaults to false)'
-    
-            # Removes the specified cache
-        #
-        # @param [Array<Hash>] cache_descriptors
-        #        An array of caches to remove, each specified with the same
-        #        hash as cache_descriptors_per_pod especially :spec_file and :slug
-        #
-        def remove_caches(cache_descriptors)
-          cache_descriptors.each do |desc|
-            UI.message('Removing spec #{desc[:spec_file]} (v#{desc[:version]})') do
-              FileUtils.rm(desc[:spec_file])
-            end
-            UI.message('Removing cache #{desc[:slug]}') do
-              FileUtils.rm_rf(desc[:slug])
-            end
-          end
-        end
-    
-          def plugins_string
-        UI::ErrorReport.plugins_string
-      end
-    
-          #-----------------------------------------------------------------------#
-    end
-  end
-end
-
-    
-            def create
-          authorize! :create, StockMovement
-          @stock_movement = scope.new(stock_movement_params)
-          if @stock_movement.save
-            respond_with(@stock_movement, status: 201, default_template: :show)
-          else
-            invalid_resource!(@stock_movement)
-          end
-        end
-    
-        def calculated_type_matches
-      possible_types.select do |content_type|
-        content_type == type_from_file_contents
-      end
-    end
-    
-        module ClassMethods
-      # This method is a shortcut to validator classes that is in
-      # 'Attachment...Validator' format. It is almost the same thing as the
-      # +validates+ method that shipped with Rails, but this is customized to
-      # be using with attachment validators. This is helpful when you're using
-      # multiple attachment validators on a single attachment.
-      #
-      # Example of using the validator:
-      #
-      #   validates_attachment :avatar, :presence => true,
-      #      :content_type => { :content_type => 'image/jpg' },
-      #      :size => { :in => 0..10.kilobytes }
-      #
-      def validates_attachment(*attributes)
-        options = attributes.extract_options!.dup
+    require 'pathname'
+require './plugins/octopress_filters'
