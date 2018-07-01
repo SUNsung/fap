265a6a35c69ac93c0fb7c418a51d8924e4cb0e04
SUNsung <weatherius@gmail.com>
2018-07-01 18:37:47 +0300

    Rewrite [28q87iyREjPRs28970czot68yY288wmPn8boB6.28VJ0.cpp] File in: 2018-07-01 18:37:47

--- cccccc/28q87iyREjPRs28970czot68yY288wmPn8boB6.28VJ0.cpp
+++ cccccc/28q87iyREjPRs28970czot68yY288wmPn8boB6.28VJ0.cpp
@@ -1,95 +1,120 @@
 
         
-        /// Index the given source file and store the results to \p indexStorePath.
-///
-/// \param primarySourceFile The source file to index.
-///
-/// \param indexUnitToken A unique identifier for this translation unit in the
-/// form of a file path.
-///
-/// \param indexStorePath The location to write the indexing data to.
-///
-/// \param indexSystemModules If true, emit index data for imported serialized
-/// swift system modules.
-///
-/// \param isDebugCompilation true for non-optimized compiler invocation.
-///
-/// \param targetTriple The target for this compilation.
-///
-/// \param dependencyTracker The set of dependencies seen while building.
-bool indexAndRecord(SourceFile *primarySourceFile, StringRef indexUnitToken,
-                    StringRef indexStorePath, bool indexSystemModules,
-                    bool isDebugCompilation, StringRef targetTriple,
-                    const DependencyTracker &dependencyTracker);
-    
-    #endif // SWIFT_INDEX_INDEXSYMBOL_H
-
-    
-      /// Returns the line and column represented by the given source location.
-  ///
-  /// If \p BufferID is provided, \p Loc must come from that source buffer.
-  ///
-  /// This respects #line directives.
-  std::pair<unsigned, unsigned>
-  getLineAndColumn(SourceLoc Loc, unsigned BufferID = 0) const {
-    assert(Loc.isValid());
-    int LineOffset = getLineOffset(Loc);
-    int l, c;
-    std::tie(l, c) = LLVMSourceMgr.getLineAndColumn(Loc.Value, BufferID);
-    assert(LineOffset+l > 0 && 'bogus line offset');
-    return { LineOffset + l, c };
+        
+    {  LOG(INFO) << 'Writing Testing data';
+  scoped_ptr<db::DB> test_db(db::GetDB(db_type));
+  test_db->Open(output_folder + '/cifar10_test_' + db_type, db::NEW);
+  txn.reset(test_db->NewTransaction());
+  // Open files
+  std::ifstream data_file((input_folder + '/test_batch.bin').c_str(),
+      std::ios::in | std::ios::binary);
+  CHECK(data_file) << 'Unable to open test file.';
+  for (int itemid = 0; itemid < kCIFARBatchSize; ++itemid) {
+    read_image(&data_file, &label, str_buffer);
+    datum.set_label(label);
+    datum.set_data(str_buffer, kCIFARImageNBytes);
+    string out;
+    CHECK(datum.SerializeToString(&out));
+    txn->Put(caffe::format_int(itemid, 5), out);
   }
-    
-    #ifndef SWIFT_INDEX_INDEX_H
-#define SWIFT_INDEX_INDEX_H
-    
-    TEST(LogTest, ReadThirdOneOff) {
-  CheckInitialOffsetRecord(10008, 2);
+  txn->Commit();
+  test_db->Close();
 }
     
-    namespace leveldb {
+    
+    {}  // namespace caffe
+    
+    #endif  // CAFFE_CUDNN_POOLING_LAYER_HPP_
+
+    
+    
+    { protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual inline bool reverse_dimensions() { return true; }
+  virtual void compute_output_shape();
+};
+    
+    /**
+ * @brief Takes two+ Blobs, interprets last Blob as a selector and
+ *  filter remaining Blobs accordingly with selector data (0 means that
+ * the corresponding item has to be filtered, non-zero means that corresponding
+ * item needs to stay).
+ */
+template <typename Dtype>
+class FilterLayer : public Layer<Dtype> {
+ public:
+  explicit FilterLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
     }
     
-      // Check second filter
-  ASSERT_TRUE(reader.KeyMayMatch(3100, 'box'));
-  ASSERT_TRUE(! reader.KeyMayMatch(3100, 'foo'));
-  ASSERT_TRUE(! reader.KeyMayMatch(3100, 'bar'));
-  ASSERT_TRUE(! reader.KeyMayMatch(3100, 'hello'));
+    namespace {
+///////////////////////////////////////////////////////////////////////////////
+    }
+    
+    #ifndef  lint
+FILE_RCSID('@(#)$File: magic.c,v 1.78 2013/01/07 18:20:19 christos Exp $')
+#endif  /* lint */
+    
+    /*
+ * Helpers for unconditional and conditional jumps.
+ */
+void surpriseCheck(IRGS&);
+void surpriseCheck(IRGS&, Offset);
+void jmpImpl(IRGS&, Offset);
+void implCondJmp(IRGS&, Offset taken, bool negate, SSATmp*);
     
     
-    {}  // namespace leveldb
+    {}
     
-    std::string ParsedInternalKey::DebugString() const {
-  char buf[50];
-  snprintf(buf, sizeof(buf), '' @ %llu : %d',
-           (unsigned long long) sequence,
-           int(type));
-  std::string result = ''';
-  result += EscapeString(user_key.ToString());
-  result += buf;
-  return result;
+    #ifndef incl_HPHP_LIGHT_PROCESS_H_
+#define incl_HPHP_LIGHT_PROCESS_H_
+    
+    bool CurlShareResource::setLongOption(long option, long value) {
+  CURLSHcode error = CURLSHE_OK;
+  error = curl_share_setopt(m_share,
+                            (CURLSHoption)option,
+                            value);
+  return error == CURLSHE_OK;
 }
     
-    #include 'guetzli/jpeg_data.h'
+      cond_list.push(cond, pri);
+    
+    #ifndef incl_HPHP_SYNCHRONIZABLE_MULTI_H_
+#define incl_HPHP_SYNCHRONIZABLE_MULTI_H_
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    
+    bool AbstractOptionHandler::isHidden() const { return flags_ & FLAG_HIDDEN; }
     
     
-    {}  // namespace guetzli
-
+    {} // namespace aria2
     
-      // Make a local copy of the input bit length histogram.
-  int count[kJpegHuffmanMaxBitLength + 1] = { 0 };
-  int total_count = 0;
-  for (len = 1; len <= kJpegHuffmanMaxBitLength; ++len) {
-    count[len] = count_in[len];
-    total_count += count[len];
-  }
+      /**
+   * Returns announce event, such as started, stopped, completed, etc.
+   */
+  const char* getEventString() const;
     
-    // Builds jpeg-style Huffman lookup table from the given symbols.
-// The symbols are in order of increasing bit lengths. The number of symbols
-// with bit length n is given in counts[n] for each n >= 1.
-// Returns the size of the lookup table.
-int BuildJpegHuffmanTable(const int* counts, const int* symbols,
-                          HuffmanTableEntry* lut);
+      AnnounceEvent event;
+  std::deque<std::string> urls;
     
-    namespace guetzli {
+    #include <vector>
+#include <string>
+#include <memory>
+    
+    class AuthConfig {
+private:
+  std::string authScheme_;
+  std::string user_;
+  std::string password_;
     }
