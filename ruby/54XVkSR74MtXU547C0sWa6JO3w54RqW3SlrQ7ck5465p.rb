a608ca565e10da6d74cc4fbeaee1b454959a9f70
SUNsung <weatherius@gmail.com>
2018-07-01 22:45:22 +0300

    Rewrite [54XVkSR74MtXU547C0sWa6JO3w54RqW3SlrQ7ck5465p.rb] File in: 2018-07-01 22:45:21

--- ruby/54XVkSR74MtXU547C0sWa6JO3w54RqW3SlrQ7ck5465p.rb
+++ ruby/54XVkSR74MtXU547C0sWa6JO3w54RqW3SlrQ7ck5465p.rb
@@ -1,23 +1,104 @@
-    def push(*names)
-      @filters.push *filter_const(names)
+
+        
+            # Gets the last git commit information formatted into a String by the provided
+    # pretty format String. See the git-log documentation for valid format placeholders
+    def self.last_git_commit_formatted_with(pretty_format, date_format = nil)
+      command = ['git log -1']
+      command << '--pretty=\'#{pretty_format}\''
+      command << '--date=\'#{date_format}\'' if date_format
+      Actions.sh(command.compact.join(' '), log: false).chomp
+    rescue
+      nil
     end
     
-          unless root?
-        raise Invalid, 'missing name' if !name || name.empty?
-        raise Invalid, 'missing path' if !path || path.empty?
-        raise Invalid, 'missing type' if !type || type.empty?
+          it 'sets the regeneration marker to expire' do
+        allow(RegenerationWorker).to receive(:perform_async)
+        get :show
+        expect(Redis.current.ttl('account:#{user.account_id}:regeneration')).to be >= 0
       end
+    
+      def scope
+    root_url
+  end
+    
+      def contents; end
+    
+        includes, ignores = argv_includes_ignores(ARGV)
+    
+        def parse_string_spec(spec, tags)
+      if (tag = tags.first) && LANGUAGE_MODULES.include?(tag)
+        odisabled ''depends_on ... => #{tag.inspect}''
+      end
+    
+      def display_s
+    name
+  end
+    
+        # Paths
+    def gem_path
+      @gem_path ||= File.expand_path '..', File.dirname(__FILE__)
     end
     
-          def key?(key)
-        super(convert_key(key))
+        it 'denies requests with duplicate session cookies' do
+      get '/', {}, 'HTTP_COOKIE' => 'rack.session=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
+      expect(last_response).not_to be_ok
+    end
+    
+      # These two settings work together to allow you to limit a spec run
+  # to individual examples or groups you care about by tagging them with
+  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
+  # get run.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+    
+        expect_any_instance_of(detector).to receive(:call).with(
+      hash_including('foo.bar' => 42)
+    ).and_call_original
+    
+      class IncludeArrayTag < Liquid::Tag
+    Syntax = /(#{Liquid::QuotedFragment}+)/
+    def initialize(tag_name, markup, tokens)
+      if markup =~ Syntax
+        @array_name = $1
+      else
+        raise SyntaxError.new('Error in tag 'include_array' - Valid syntax: include_array [array from _config.yml]')
       end
     
-              # Verify the box exists that we want to repackage
-          box = @env.boxes.find(box_name, box_provider, '= #{box_version}')
-          if !box
-            raise Vagrant::Errors::BoxNotFoundWithProviderAndVersion,
-              name: box_name,
-              provider: box_provider.to_s,
-              version: box_version
-          end
+        # Returns the width and height in a format suitable to be passed to Geometry.parse
+    def to_s
+      s = ''
+      s << width.to_i.to_s if width > 0
+      s << 'x#{height.to_i}' if height > 0
+      s << modifier.to_s
+      s
+    end
+    
+            def responds?
+          methods = @subject.instance_methods.map(&:to_s)
+          methods.include?('#{@attachment_name}') &&
+            methods.include?('#{@attachment_name}=') &&
+            methods.include?('#{@attachment_name}?')
+        end
+    
+            required = directories.map do |directory|
+          pathname = File.expand_path(Rails.root.join(directory, filename))
+          file_exists = File.exist?(pathname)
+          require pathname if file_exists
+          file_exists
+        end
+    
+      class Railtie < Rails::Railtie
+    initializer 'paperclip.insert_into_active_record' do |app|
+      ActiveSupport.on_load :active_record do
+        Paperclip::Railtie.insert
+      end
+    
+        def self.included(base)
+      ActiveRecord::ConnectionAdapters::Table.send :include, TableDefinition
+      ActiveRecord::ConnectionAdapters::TableDefinition.send :include, TableDefinition
+      ActiveRecord::ConnectionAdapters::AbstractAdapter.send :include, Statements
+      ActiveRecord::Migration::CommandRecorder.send :include, CommandRecorder
+    end
+    
+            validate_whitelist(record, attribute, value)
+        validate_blacklist(record, attribute, value)
