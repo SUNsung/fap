822ffd526e1f14d8ea3530e861539a1dc28ca1fc
SUNsung <weatherius@gmail.com>
2018-07-02 20:37:28 +0300

    Rewrite [49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb] File in: 2018-07-02 20:37:27

--- ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
+++ ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
@@ -1,57 +1,11 @@
 
         
-                # This returns all the registered guest capabilities.
-        #
-        # @return [Hash]
-        def guest_capabilities
-          results = Hash.new { |h, k| h[k] = Registry.new }
-    
-              # Success, exit status 0
-          0
-        end
-      end
-    end
+          def create_gist(files, description)
+    url = 'https://api.github.com/gists'
+    data = { 'public' => true, 'files' => files, 'description' => description }
+    scopes = GitHub::CREATE_GIST_SCOPES
+    GitHub.open_api(url, data: data, scopes: scopes)['html_url']
   end
-end
-
-    
-            # Get the proper capability host to check
-        cap_host = nil
-        if type == :host
-          cap_host = @env.host
-        else
-          with_target_vms([]) do |vm|
-            cap_host = case type
-                       when :provider
-                         vm.provider
-                       when :guest
-                         vm.guest
-                       else
-                         raise Vagrant::Errors::CLIInvalidUsage,
-                           help: opts.help.chomp
-                       end
-          end
-        end
-    
-      # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-    
-      config.active_support.test_order = :random
-    
-      def setup
-    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
-    success = Dir.chdir DUMMY_PATH do
-      silence_stdout_if !ENV['VERBOSE'] do
-        system 'node', 'manifest.js', tmp_dir
-      end
-    end
-    assert success, 'Node.js Mincer compilation failed'
-    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
-    css_name = manifest['assets']['application.css']
-    @css = File.read('#{tmp_dir}/#{css_name}')
-  end
-end
-
     
     desc 'Dumps output to a CSS file for testing'
 task :debug do
@@ -62,3 +16,10 @@ task :debug do
     File.open('./#{file}.css', 'w') { |f| f.write(engine.render) }
   end
 end
+    
+          spec['main'] =
+          find_files.(File.join(Bootstrap.stylesheets_path, '_bootstrap.scss')) +
+          find_files.(Bootstrap.fonts_path) +
+          %w(assets/javascripts/bootstrap.js)
+    
+            private
