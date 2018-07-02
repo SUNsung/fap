1f2707ee42c9a9a63ca16be605cbda5fc3e77746
SUNsung <weatherius@gmail.com>
2018-07-02 10:10:54 +0300

    Rewrite [58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb] File in: 2018-07-02 10:10:54

--- ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
+++ ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
@@ -1,86 +1,23 @@
 
         
-            def matching_domain
-      if domain.nil?
-        Account.where(domain: nil)
-      else
-        Account.where(Account.arel_table[:domain].lower.eq domain.to_s.downcase)
-      end
-    end
-  end
-end
-
-    
-      attributes :id, :type, :name, :updated
-    
-        existence_maps.each do |group|
-      existing_one = group.key(true)
-    
-      private
-    
-      def short_name
-    object.site_title
-  end
-    
-      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/follow
-  def follow
-    f = Follow.last
-    NotificationMailer.follow(f.target_account, Notification.find_by(activity: f))
-  end
-    
-        on :save_person_after_webfinger do |person|
-      # find existing person or create a new one
-      person_entity = Person.find_by(diaspora_handle: person.diaspora_id) ||
-        Person.new(diaspora_handle: person.diaspora_id, guid: person.guid,
-                   serialized_public_key: person.exported_key, pod: Pod.find_or_create_by(url: person.url))
-    
-          # Inspired by https://github.com/nov/openid_connect_sample/blob/master/app/controllers/connect/clients_controller.rb#L24
-      def create
-        registrar = OpenIDConnect::Client::Registrar.new(request.url, params)
-        client = Api::OpenidConnect::OAuthApplication.register! registrar
-        render json: client.as_json(root: false)
-      end
-    
-        def after(task, post_task, *args, &block)
-      Rake::Task.define_task(post_task, *args, &block) if block_given?
-      task = Rake::Task[task]
-      task.enhance do
-        post = Rake.application.lookup(post_task, task.scope)
-        raise ArgumentError, 'Task #{post_task.inspect} not found' unless post
-        post.invoke
-      end
+            def log_processed(name)
+      puts green '    #{name}'
     end
     
-    # IMPORTANT: The Capistrano::Plugin system is not yet considered a stable,
-# public API, and is subject to change without notice. Eventually it will be
-# officially documented and supported, but for now, use it at your own risk.
-#
-# Base class for Capistrano plugins. Makes building a Capistrano plugin as easy
-# as writing a `Capistrano::Plugin` subclass and overriding any or all of its
-# three template methods:
-#
-# * set_defaults
-# * register_hooks
-# * define_tasks
-#
-# Within the plugin you can use any methods of the Rake or Capistrano DSLs, like
-# `fetch`, `invoke`, etc. In cases when you need to use SSHKit's backend outside
-# of an `on` block, use the `backend` convenience method. E.g. `backend.test`,
-# `backend.execute`, or `backend.capture`.
-#
-# Package up and distribute your plugin class as a gem and you're good to go!
-#
-# To use a plugin, all a user has to do is install it in the Capfile, like this:
-#
-#   # Capfile
-#   require 'capistrano/superfancy'
-#   install_plugin Capistrano::Superfancy
-#
-# Or, to install the plugin without its hooks:
-#
-#   # Capfile
-#   require 'capistrano/superfancy'
-#   install_plugin Capistrano::Superfancy, load_hooks: false
-#
-class Capistrano::Plugin < Rake::TaskLib
-  include Capistrano::DSL
+      #forward some requests to status message, because a poll is just attached to a status message and is not sharable itself
+  delegate :author_id, :diaspora_handle, :public?, :subscribers, to: :status_message
+    
+          interp = node_or_interp
+      after_string_or_interp =
+        if interp.after
+          to_string_interpolation(interp.after)
+        else
+          string_literal('')
+        end
+      if interp.after && interp.whitespace_after
+        after_string_or_interp = concat(string_literal(' '), after_string_or_interp)
+      end
+    
+      def api_error?(error)
+    error.is_a?(LogStash::Api::ApiError)
+  end
