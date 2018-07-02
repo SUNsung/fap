f5d9cc7482ccbc7254ee54dc186329219ffe0fb7
SUNsung <weatherius@gmail.com>
2018-07-02 11:51:57 +0300

    Rewrite [44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb] File in: 2018-07-02 11:51:56

--- ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
+++ ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
@@ -1,62 +1,49 @@
 
         
-              # Returns constant of subscription adapter specified in config/cable.yml.
-      # If the adapter cannot be found, this will default to the Redis adapter.
-      # Also makes sure proper dependencies are required.
-      def pubsub_adapter
-        adapter = (cable.fetch('adapter') { 'redis' })
+        #
+# This script extracts the forms from the main page of each
+# web site in a list. The output of this can be used with
+# Metasploit (and other tools) to obtain the saved form data
+# of these domains.
+#
     
-          def changed_in_place?(raw_old_value, value)
-        return false if value.nil?
-        raw_new_value = encoded(value)
-        raw_old_value.nil? != raw_new_value.nil? ||
-          subtype.changed_in_place?(raw_old_value, raw_new_value)
+    
+    
+    #certCN cannot contain commas
+certCN 		= 'Metasploit Inc.'
+#keytoolOpts 	= '-genkey -alias signFiles -keystore msfkeystore ' +
+#		  '-storepass msfstorepass -dname \'cn=#{certCN}\' ' +
+#		  '-keypass msfkeypass'
+    
+              If there is multiple cache for various versions of the requested pod,
+          you will be asked which one to clean. Use `--all` to clean them all.
+    
+            # Prints the list of specs & pod cache dirs for a single pod name.
+        #
+        # This output is valid YAML so it can be parsed with 3rd party tools
+        #
+        # @param [Array<Hash>] cache_descriptors
+        #        The various infos about a pod cache. Keys are
+        #        :spec_file, :version, :release and :slug
+        #
+        def print_pod_cache_infos(pod_name, cache_descriptors)
+          UI.puts '#{pod_name}:'
+          cache_descriptors.each do |desc|
+            if @short_output
+              [:spec_file, :slug].each { |k| desc[k] = desc[k].relative_path_from(@cache.root) }
+            end
+            UI.puts('  - Version: #{desc[:version]}')
+            UI.puts('    Type:    #{pod_type(desc)}')
+            UI.puts('    Spec:    #{desc[:spec_file]}')
+            UI.puts('    Pod:     #{desc[:slug]}')
+          end
+        end
       end
-    
-        assert_equal 3, @pirate.birds.reload.length
-  ensure
-    Bird.connection.remove_index :birds, column: :name
-  end
-    
-        def private_method
-      'I am Jack's innermost fears and aspirations'
     end
-    
-        private
-      def subscribe_to(connection, identifiers:)
-        receive connection, command: 'subscribe', identifiers: identifiers
-      end
-    
-        def connect
-      @connected = true
-    end
-    
-      def all_groups(current_user)
-    groups = []
-    
-        if registration
-      u2f.authenticate!(challenges, response, Base64.decode64(registration.public_key), registration.counter)
-      registration.update(counter: response.counter)
-      true
-    end
-  rescue JSON::ParserError, NoMethodError, ArgumentError, U2F::Error
-    false
   end
 end
 
     
-                # Register the handler if this is our first callback.
-            Signal.trap('INT') { fire_callbacks } if registered.length == 1
-          end
-        end
-    
-    module Vagrant
-  module Util
-    class IO
-      # The chunk size for reading from subprocess IO.
-      READ_CHUNK_SIZE = 4096
-    
-                o.on('--provider PROVIDER', String,
-                 'The specific provider type for the box to remove') do |p|
-              options[:provider] = p
-            end
+          def stack
+        UI::ErrorReport.stack
+      end
