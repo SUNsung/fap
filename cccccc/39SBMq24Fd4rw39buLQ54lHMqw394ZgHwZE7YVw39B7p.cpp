c8a3b8d29f7bb262a4f7f3ddd868de3c985be618
SUNsung <weatherius@gmail.com>
2018-07-03 01:11:14 +0300

    Rewrite [39SBMq24Fd4rw39buLQ54lHMqw394ZgHwZE7YVw39B7p.cpp] File in: 2018-07-03 01:11:14

--- cccccc/39SBMq24Fd4rw39buLQ54lHMqw394ZgHwZE7YVw39B7p.cpp
+++ cccccc/39SBMq24Fd4rw39buLQ54lHMqw394ZgHwZE7YVw39B7p.cpp
@@ -1,161 +1,157 @@
 
         
-        
-    {
-    {}  // namespace
-}  // namespace tesseract
-
-    
-    class IntSimdMatrixTest : public ::testing::Test {
- protected:
-  // Makes a random weights matrix of the given size.
-  GENERIC_2D_ARRAY<int8_t> InitRandom(int no, int ni) {
-    GENERIC_2D_ARRAY<int8_t> a(no, ni, 0);
-    for (int i = 0; i < no; ++i) {
-      for (int j = 0; j < ni; ++j) {
-        a(i, j) = static_cast<int8_t>(random_.SignedRand(INT8_MAX));
-      }
-    }
-    return a;
-  }
-  // Makes a random input vector of the given size, with rounding up.
-  std::vector<int8_t> RandomVector(int size, const IntSimdMatrix& matrix) {
-    int rounded_size = matrix.RoundInputs(size);
-    std::vector<int8_t> v(rounded_size, 0);
-    for (int i = 0; i < size; ++i) {
-      v[i] = static_cast<int8_t>(random_.SignedRand(INT8_MAX));
-    }
-    return v;
-  }
-  // Makes a random scales vector of the given size.
-  GenericVector<double> RandomScales(int size) {
-    GenericVector<double> v(size, 0.0);
-    for (int i = 0; i < size; ++i) {
-      v[i] = 1.0 + random_.SignedRand(1.0);
-    }
-    return v;
-  }
-  // Tests a range of sizes and compares the results against the base_ version.
-  void ExpectEqualResults(IntSimdMatrix* matrix) {
-    for (int num_out = 1; num_out < 130; ++num_out) {
-      for (int num_in = 1; num_in < 130; ++num_in) {
-        GENERIC_2D_ARRAY<int8_t> w = InitRandom(num_out, num_in + 1);
-        matrix->Init(w);
-        std::vector<int8_t> u = RandomVector(num_in, *matrix);
-        GenericVector<double> scales = RandomScales(num_out);
-        std::vector<double> base_result(num_out);
-        base_.MatrixDotVector(w, scales, u.data(), base_result.data());
-        std::vector<double> test_result(num_out);
-        matrix->MatrixDotVector(w, scales, u.data(), test_result.data());
-        for (int i = 0; i < num_out; ++i)
-          EXPECT_FLOAT_EQ(base_result[i], test_result[i]) << 'i=' << i;
-      }
-    }
-  }
-    }
-    
-     private:
-  // Resizes forward data to cope with an input image of the given width.
-  void ResizeForward(const NetworkIO& input);
-    
-     private:
-  // Saves the given Pix as a PNG-encoded string and destroys it.
-  static void SetPixInternal(Pix* pix, GenericVector<char>* image_data);
-  // Returns the Pix image for the image_data. Must be pixDestroyed after use.
-  static Pix* GetPixInternal(const GenericVector<char>& image_data);
-  // Parses the text string as a box file and adds any discovered boxes that
-  // match the page number. Returns false on error.
-  bool AddBoxes(const char* box_text);
-    
-      // Returns the average sum of squared perpendicular error from a line
-  // through mean_point() in the direction dir.
-  double rms_orth(const FCOORD &dir) const;
-    
-    /* Cancels the stream. Can be called at any time after
- * bidirectional_stream_start(). The on_canceled() method of
- * bidirectional_stream_callback will be invoked when cancelation
- * is complete and no further callback methods will be invoked. If the
- * stream has completed or has not started, calling
- * bidirectional_stream_cancel() has no effect and on_canceled() will not
- * be invoked. At most one callback method may be invoked after
- * bidirectional_stream_cancel() has completed.
- */
-GRPC_SUPPORT_EXPORT
-void bidirectional_stream_cancel(bidirectional_stream* stream);
-    
-    #endif  // GRPC_INTERNAL_COMPILER_NODE_GENERATOR_HELPERS_H
-
-    
-    #include <grpc/grpc_security.h>
-    
-    class CodegenTestMinimal : public ::testing::Test {};
-    
-      static double Now();
-    
-      // Initialize the internal timer and reset the query count to 0
-  void Reset();
-    
-    namespace grpc {
-namespace testing {
-    }
-    }
+        #endif  // CONTENT_NW_SRC_API_APP_APP_H_
     
     
-    {  EXPECT_FALSE(buf->isShared());
+    {  RenderThread::Get()->Send(new ShellViewHostMsg_Allocate_Object(
+      routing_id,
+      object_id,
+      type,
+      *static_cast<base::DictionaryValue*>(value_option.get())));
+  return v8::Undefined(isolate);
 }
     
-      static_assert(
-      !std::is_reference<Value>::value,
-      'Expected may not be used with reference types');
-  static_assert(
-      !std::is_abstract<Value>::value,
-      'Expected may not be used with abstract types');
+       void ExecuteCommand(int command_id, int event_flags) override;
     
-      auto p = promises(block);
-  uint32_t i = 0;
-  try {
-    for (i = 0; i < size_; ++i) {
-      new (p + i) BoolPromise();
+    bool NwShellShowItemInFolderFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  nw::ObjectManager* manager = nw::ObjectManager::Get(browser_context());
+  manager->OnCallStaticMethod(render_frame_host(), 'Shell', 'ShowItemInFolder', *args_);
+  return true;
+}
+    
+    // customize random engine.
+void CustomGlobalRandomEngine::seed(CustomGlobalRandomEngine::result_type val) {
+  // ignore the seed
+}
+    
+      auto iter = p_fmat->RowIterator();
+  iter->BeforeFirst();
+  while (iter->Next()) {
+     auto batch = iter->Value();
+    #pragma omp parallel num_threads(nthread)
+    {
+      CHECK_EQ(nthread, omp_get_num_threads());
+      auto tid = static_cast<unsigned>(omp_get_thread_num());
+      unsigned begin = std::min(nstep * tid, ncol);
+      unsigned end = std::min(nstep * (tid + 1), ncol);
+      for (size_t i = 0; i < batch.Size(); ++i) { // NOLINT(*)
+        size_t ridx = batch.base_rowid + i;
+        SparsePage::Inst inst = batch[i];
+        for (bst_uint j = 0; j < inst.length; ++j) {
+          if (inst[j].index >= begin && inst[j].index < end) {
+            sketchs[inst[j].index].Push(inst[j].fvalue, info.GetWeight(ridx));
+          }
+        }
+      }
     }
-  } catch (...) {
-    for (; i != 0; --i) {
-      p[i - 1].~BoolPromise();
-    }
-    throw;
   }
     
-      // Log a bunch of messages to the writer
-  size_t numMessages = 100;
-  {
-    AsyncFileWriter writer{folly::File{fds[1], true}};
-    for (size_t n = 0; n < numMessages; ++n) {
-      writer.writeMessage(folly::to<std::string>('message ', n, '\n'));
-      sched_yield();
+    #include <xgboost/base.h>
+#include <xgboost/data.h>
+#include <dmlc/threadediter.h>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include '../common/common.h'
+#include './sparse_page_writer.h'
+    
+    SparsePageWriter::SparsePageWriter(
+    const std::vector<std::string>& name_shards,
+    const std::vector<std::string>& format_shards,
+    size_t extra_buffer_capacity)
+    : num_free_buffer_(extra_buffer_capacity + name_shards.size()),
+      clock_ptr_(0),
+      workers_(name_shards.size()),
+      qworkers_(name_shards.size()) {
+  CHECK_EQ(name_shards.size(), format_shards.size());
+  // start writer threads
+  for (size_t i = 0; i < name_shards.size(); ++i) {
+    std::string name_shard = name_shards[i];
+    std::string format_shard = format_shards[i];
+    auto* wqueue = &qworkers_[i];
+    workers_[i].reset(new std::thread(
+        [this, name_shard, format_shard, wqueue] () {
+          std::unique_ptr<dmlc::Stream> fo(
+              dmlc::Stream::Create(name_shard.c_str(), 'w'));
+          std::unique_ptr<SparsePageFormat> fmt(
+              SparsePageFormat::Create(format_shard));
+          fo->Write(format_shard);
+          std::shared_ptr<SparsePage> page;
+          while (wqueue->Pop(&page)) {
+            if (page == nullptr) break;
+            fmt->Write(*page, fo.get());
+            qrecycle_.Push(std::move(page));
+          }
+          fo.reset(nullptr);
+          LOG(CONSOLE) << 'SparsePage::Writer Finished writing to ' << name_shard;
+        }));
+  }
+}
+    
+    std::vector<uint8_t> DecodeJpegToRGB(const JPEGData& jpg) {
+  if (jpg.components.size() == 1 ||
+      (jpg.components.size() == 3 &&
+       HasYCbCrColorSpace(jpg) && (jpg.Is420() || jpg.Is444()))) {
+    OutputImage img(jpg.width, jpg.height);
+    img.CopyFromJpegData(jpg);
+    return img.ToSRGB();
+  }
+  return std::vector<uint8_t>();
+}
+    
+    
+    {}  // namespace
+    
+    #ifndef GUETZLI_JPEG_DATA_READER_H_
+#define GUETZLI_JPEG_DATA_READER_H_
+    
+    #include 'guetzli/quality.h'
+    
+      StatisticsJni::StatisticsJni(std::shared_ptr<Statistics> stats)
+      : StatisticsImpl(stats, false), m_ignore_histograms() {
+  }
+    
+      uint64_t GetID() const { return id_; }
+    
+     private:
+    // used for synchronisation in compare method
+    std::unique_ptr<port::Mutex> mtx_compare;
+    // used for synchronisation in findShortestSeparator method
+    std::unique_ptr<port::Mutex> mtx_findShortestSeparator;
+    std::unique_ptr<const char[]> m_name;
+    jmethodID m_jCompareMethodId;
+    jmethodID m_jFindShortestSeparatorMethodId;
+    jmethodID m_jFindShortSuccessorMethodId;
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+    
+/*
+ * CoreServiceBase.h
+ *
+ *  Created on: 2013-6-20
+ *      Author: yerungui
+ */
+    
+    #endif
+
+    
+    ScopeJEnv::ScopeJEnv(JavaVM* jvm, jint _capacity)
+    : vm_(jvm), env_(NULL), we_attach_(false), status_(0) {
+    ASSERT(jvm);
+    do {
+        env_ = (JNIEnv*)pthread_getspecific(g_env_key);
+        
+        if (NULL != env_) {
+            break;
+        }
+        
+        status_ = vm_->GetEnv((void**) &env_, JNI_VERSION_1_6);
     }
-  }
+    }
     
     
-    
-      virtual const char* Name() const override;
-    
-      typedef std::shared_ptr<DB> (* OpenFuncPtr)(char);
-    
-     public: // Delete / Remove / Pop / Trim
-  /// Trim (list: key) so that it will only contain the indices from start..stop
-  /// Returns true on success
-  /// May throw RedisListException
-  bool Trim(const std::string& key, int32_t start, int32_t stop);
-    
-    // @lint-ignore TXT4 T25377293 Grandfathered in
-#endif  // JAVA_ROCKSJNI_STATISTICSJNI_H_
-    
-      // Returns the sequence number that is guaranteed to be smaller than or equal
-  // to the sequence number of any key that could be inserted into this
-  // memtable. It can then be assumed that any write with a larger(or equal)
-  // sequence number will be present in this memtable or a later memtable.
-  //
-  // If the earliest sequence number could not be determined,
-  // kMaxSequenceNumber will be returned.
-  SequenceNumber GetEarliestSequenceNumber() {
-    return earliest_seqno_.load(std::memory_order_relaxed);
-  }
