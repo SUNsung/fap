00bb1acccea238292eae7d25de1e6fa02f2d820d
SUNsung <weatherius@gmail.com>
2018-07-02 07:22:00 +0300

    Rewrite [49InRrhCrP0M649crFe61w0ji.49FDh1Swbl2S.49O9e.py] File in: 2018-07-02 07:22:00

--- pyton/49InRrhCrP0M649crFe61w0ji.49FDh1Swbl2S.49O9e.py
+++ pyton/49InRrhCrP0M649crFe61w0ji.49FDh1Swbl2S.49O9e.py
@@ -1,189 +1,114 @@
 
         
-              # A similar contract applies to the annotations.
-      self.checkOpOrder('annotations', anno['annotations'],
-                        ['GetSession', 'AttachDataReader', 'ReleaseSession'])
-    
-    import tensorflow as tf
-    
-      def testIdentityInitializerNonSquareRank2FirstDimSmaller(self):
-    divisor = 2.
-    std = 1e-3
-    shape = (2, 4)
-    m = divisor / shape[-1]
-    expected = [[m, m, -9.95908980e-04, 6.93598529e-04],
-                [-4.18301526e-04, -1.58457726e-03, m, m]]
-    self.IdentityInitializerHelper(shape, expected, divisor, std)
-    
-        tagger = spec_builder.ComponentSpecBuilder('tagger')
-    tagger.set_network_unit(name='FeedForwardNetwork', hidden_layer_sizes='256')
-    tagger.set_transition_system(name='tagger')
-    tagger.add_fixed_feature(name='words', fml='input.word', embedding_dim=64)
-    tagger.add_rnn_link(embedding_dim=-1)
-    tagger.fill_from_resources(lexicon_dir)
-    
-      # Constructs lexical resources for SyntaxNet in the given resource path, from
-  # the training data.
-  if FLAGS.compute_lexicon:
-    logging.info('Computing lexicon...')
-    lexicon.build_lexicon(FLAGS.resource_path, FLAGS.training_corpus_path)
-    
-    flags.DEFINE_integer('batch_size', 4, 'Batch size.')
-flags.DEFINE_integer('report_every', 200,
-                     'Report cost and training accuracy every this many steps.')
-flags.DEFINE_integer('job_id', 0, 'The trainer will clear checkpoints if the '
-                     'saved job id is less than the id this flag. If you want '
-                     'training to start over, increment this id.')
-    
-    cc_library(
-    name = 'whole_sentence_features',
-    srcs = ['whole_sentence_features.cc'],
-    hdrs = ['whole_sentence_features.h'],
-    deps = [
-        ':base',
-        ':feature_extractor',
-        ':registry',
-        ':task_context',
-        ':workspace',
-    ],
-    alwayslink = 1,
-)
-    
-        # To avoid having to always use the '|safe' filter in flatpage templates,
-    # mark the title and content as already safe (since they are raw HTML
-    # content in the first place).
-    f.title = mark_safe(f.title)
-    f.content = mark_safe(f.content)
-    
-        def __init__(self, args):
-        '''Initializes the plugin with the given command line args'''
-        self._temp_dir = tempfile.mkdtemp()
-        self.le_config = util.create_le_config(self._temp_dir)
-        config_dir = util.extract_configs(args.configs, self._temp_dir)
-        self._configs = [
-            os.path.join(config_dir, config)
-            for config in os.listdir(config_dir)]
-    
-        @mock.patch(
-        'certbot_compatibility_test.validator.crypto_util.probe_sni')
-    def test_certificate_failure(self, mock_probe_sni):
-        cert = OpenSSL.crypto.X509()
-        cert.set_serial_number(1337)
-        mock_probe_sni.return_value = OpenSSL.crypto.X509()
-        self.assertFalse(self.validator.certificate(
-            cert, 'test.com', '127.0.0.1'))
-    
-    autodoc_member_order = 'bysource'
-autodoc_default_flags = ['show-inheritance', 'private-members']
-    
-        # Get options and arguments.
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], _short_options, _options)
-    except getopt.GetoptError as e:
-        log.wtf('''
-    [Fatal] {}.
-    Try '{} --help' for more options.'''.format(e, script_name))
-    
-        stream_types = [  #this is just a sample. Will make it in prepare()
-        # {'id': '1080'},
-        # {'id': '720'},
-        # {'id': '360'},
-        # {'id': '288'},
-        # {'id': '190'},
-        # {'id': '180'},
         
-    ]
+def main(unused_argv):
+  # Run the exporter.
+  export(FLAGS.master_spec, FLAGS.params_path, FLAGS.export_path,
+         FLAGS.export_moving_averages, FLAGS.build_runtime_graph)
+  tf.logging.info('Export complete.')
     
-        while pin_count > 0:
-        json_data = extract_json_data(url, max=pin_list[-1]['pin_id'],
-                                      limit=LIMIT)
-        pins = json_data['pins']
-        pin_list += pins
-        pin_count -= len(pins)
-    
-        title = r1(r'<meta name='description' content='([^']*)'', html)
-    flashvars = r1(r'flashvars='(type=[^']*)'', html)
-    
-            # extract title
-        self.title = match1(content,
-                            r'<meta property='og:description' name='og:description' content='([^']+)'')
-    
-    #----------------------------------------------------------------------
-def showroom_get_roomid_by_room_url_key(room_url_key):
-    '''str->str'''
-    fake_headers_mobile = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Charset': 'UTF-8,*;q=0.5',
-        'Accept-Encoding': 'gzip,deflate,sdch',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'
-    }
-    webpage_url = 'https://www.showroom-live.com/' + room_url_key
-    html = get_content(webpage_url, headers = fake_headers_mobile)
-    roomid = match1(html, r'room\?room_id\=(\d+)')
-    assert roomid
-    return roomid
-    
-        def test_sales_manager_shall_talk_through_proxy_with_delay(cls):
-        cls.p.busy = 'No'
-        start_time = time()
-        cls.p.talk()
-        end_time = time()
-        execution_time = end_time - start_time
-        print_output = cls.output.getvalue()
-        expected_print_output = 'Proxy checking for Sales Manager availability\n\
-Sales Manager ready to talk\n'
-        cls.assertEqual(print_output, expected_print_output)
-        expected_execution_time = 1
-        cls.assertEqual(int(execution_time*10), expected_execution_time)
-    
-        move = Action('move')
-    person = Person('Jack', move)
-    person.do_action().amount('5m').stop()
-    
-        def test_frozen_pool(self):
-        with ObjectPool(self.sample_queue) as pool:
-            self.assertEqual(pool, 'first')
-            self.assertEqual(pool, 'first')
-        self.assertTrue(self.sample_queue.get() == 'second')
-        self.assertFalse(self.sample_queue.empty())
-        self.assertTrue(self.sample_queue.get() == 'first')
-        self.assertTrue(self.sample_queue.empty())
-    
-        def __new__(cls, name, bases, attrs):
-        new_cls = type.__new__(cls, name, bases, attrs)
-        '''
-            Here the name of the class is used as key but it could be any class
-            parameter.
-        '''
-        cls.REGISTRY[new_cls.__name__] = new_cls
-        return new_cls
-    
-    '''
-Port of the Java example of 'Constructor Injection' in
-'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
-(ISBN-10: 0131495054, ISBN-13: 978-0131495050)
-    
-        def get_current_time_as_html_fragment(self, time_provider):
-        current_time = time_provider.now()
-        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
-        return current_time_as_html_fragment
+        try:
+      with open(FLAGS.expected_file) as expected:
+        content_expected = expected.read()
+    except IOError as e:
+      self.fail('Error opening '%s': %s' % (FLAGS.expected_file, e.strerror))
     
     
-class NumObj(object):
+_EXPECTED_CONTEXT = r'''
+input { name: 'word-map' Part { file_pattern: '/tmp/word-map' } }
+input { name: 'tag-map' Part { file_pattern: '/tmp/tag-map' } }
+input { name: 'tag-to-category' Part { file_pattern: '/tmp/tag-to-category' } }
+input { name: 'lcword-map' Part { file_pattern: '/tmp/lcword-map' } }
+input { name: 'category-map' Part { file_pattern: '/tmp/category-map' } }
+input { name: 'char-map' Part { file_pattern: '/tmp/char-map' } }
+input { name: 'char-ngram-map' Part { file_pattern: '/tmp/char-ngram-map' } }
+input { name: 'label-map' Part { file_pattern: '/tmp/label-map' } }
+input { name: 'prefix-table' Part { file_pattern: '/tmp/prefix-table' } }
+input { name: 'suffix-table' Part { file_pattern: '/tmp/suffix-table' } }
+input { name: 'known-word-map' Part { file_pattern: '/tmp/known-word-map' } }
+'''
     
-        def attach(self, observer):
-        if observer not in self._observers:
-            self._observers.append(observer)
     
-    from __future__ import print_function
+if __name__ == '__main__':
+    main()
+
     
-        context['show_on_github_url'] = show_url
-    context['edit_on_github_url'] = edit_url
+        def _handle_events(self, fd, events):
+        fileobj, handler_func = self.handlers[fd]
+        handler_func(fileobj, events)
     
-    DOMAIN = 'browser'
-SERVICE_BROWSE_URL = 'browse_url'
+            async def worker(worker_id):
+            await sem.acquire()
+            try:
+                print('Worker %d is working' % worker_id)
+                await use_some_resource()
+            finally:
+                print('Worker %d is done' % worker_id)
+                sem.release()
     
-    REQUIREMENTS = ['pyuserinput==0.1.11']
+    try:
+    import tornado.websocket  # noqa
+    from tornado.util import _websocket_mask_python
+except ImportError:
+    # The unittest module presents misleading errors on ImportError
+    # (it acts as if websocket_test could not be found, hiding the underlying
+    # error).  If we get an ImportError here (which could happen due to
+    # TORNADO_EXTENSION=1), print some extra information before failing.
+    traceback.print_exc()
+    raise
     
-    patch_file = 'homeassistant.components.device_tracker.bt_home_hub_5'
+            If a socket with the given name already exists, it will be deleted.
+        If any other file with that name exists, an exception will be
+        raised.
+    
+                    # If @asynchronous is used with @gen.coroutine, (but
+                # not @gen.engine), we can automatically finish the
+                # request when the future resolves.  Additionally,
+                # the Future will swallow any exceptions so we need
+                # to throw them back out to the stack context to finish
+                # the request.
+                def future_complete(f):
+                    f.result()
+                    if not self._finished:
+                        self.finish()
+                IOLoop.current().add_future(result, future_complete)
+                # Once we have done this, hide the Future from our
+                # caller (i.e. RequestHandler._when_complete), which
+                # would otherwise set up its own callback and
+                # exception handler (resulting in exceptions being
+                # logged twice).
+                return None
+            return result
+    return wrapper
+    
+    
+class TestIOStreamStartTLS(AsyncTestCase):
+    def setUp(self):
+        try:
+            super(TestIOStreamStartTLS, self).setUp()
+            self.listener, self.port = bind_unused_port()
+            self.server_stream = None
+            self.server_accepted = Future()
+            netutil.add_accept_handler(self.listener, self.accept)
+            self.client_stream = IOStream(socket.socket())
+            self.io_loop.add_future(self.client_stream.connect(
+                ('127.0.0.1', self.port)), self.stop)
+            self.wait()
+            self.io_loop.add_future(self.server_accepted, self.stop)
+            self.wait()
+        except Exception as e:
+            print(e)
+            raise
+    
+        Return the main content-type and a dictionary of options.
+    
+            namespace = exec_test(globals(), locals(), '''
+        async def f():
+            results = []
+            async for i in q:
+                results.append(i)
+                if i == 4:
+                    return results
+        ''')
+        results = yield namespace['f']()
+        self.assertEqual(results, list(range(5)))
