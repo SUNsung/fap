5a2dd4555e4f3e8c4804177e243bf7d568a1923b
SUNsung <weatherius@gmail.com>
2018-07-02 09:47:22 +0300

    Rewrite [46f99pPmYsVu6469Z4R4DGyJPU46lKLoiJZ1RE463Tu.py] File in: 2018-07-02 09:47:22

--- pyton/46f99pPmYsVu6469Z4R4DGyJPU46lKLoiJZ1RE463Tu.py
+++ pyton/46f99pPmYsVu6469Z4R4DGyJPU46lKLoiJZ1RE463Tu.py
@@ -1,60 +1,207 @@
 
         
-        
+        entry_template = textwrap.dedent('''
+    <entry>
+        <id>https://yt-dl.org/feed/youtube-dl-updates-feed/youtube-dl-@VERSION@</id>
+        <title>New version @VERSION@</title>
+        <link href='http://rg3.github.io/youtube-dl' />
+        <content type='xhtml'>
+            <div xmlns='http://www.w3.org/1999/xhtml'>
+                Downloads available at <a href='https://yt-dl.org/downloads/@VERSION@/'>https://yt-dl.org/downloads/@VERSION@/</a>
+            </div>
+        </content>
+        <author>
+            <name>The youtube-dl maintainers</name>
+        </author>
+        <updated>@TIMESTAMP@</updated>
+    </entry>
+    ''')
+    
+    options = helptext[helptext.index('  General Options:') + 19:]
+options = re.sub(r'(?m)^  (\w.+)$', r'## \1', options)
+options = '# OPTIONS\n' + options + '\n'
+    
+    
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    unittest.main()
 
     
-    import re
+    from __future__ import unicode_literals
     
-            self.assertEquals(result, expected)
+    from test.helper import assertRegexpMatches
+    
+            self._debug_cmd(args)
+    
+        _ALL_CLASSES = [
+        klass
+        for name, klass in globals().items()
+        if name.endswith('IE') and name != 'GenericIE'
+    ]
+    _ALL_CLASSES.append(GenericIE)
+    
+            return {
+            'id': video_id,
+            'url': video_url,
+            'ext': 'mp4',
+            'title': title,
+            'description': description,
+            'thumbnail': thumbnail,
+            'categories': categories,
+            'duration': duration,
+            'view_count': view_count,
+            'age_limit': 18,
+        }
 
     
-    
-print('\n# ======================================================================')
-print('#                   Calculating pi, 10000 iterations')
-print('# ======================================================================\n')
-    
-    class TZInfo:
-    def __init__(self, transitions, type_indices, ttis, abbrs):
-        self.transitions = transitions
-        self.type_indices = type_indices
-        self.ttis = ttis
-        self.abbrs = abbrs
+            runtime = self._search_regex(
+            r'Runtime\s*:\s*(.+?) \|', webpage, 'duration', default=None)
+        if runtime:
+            runtime = re.sub(r'[\s-]', '', runtime)
+        duration = parse_duration(runtime)
+        view_count = int_or_none(self._search_regex(
+            r'Views\s*:\s*(\d+)', webpage, 'view count', default=None))
+        comment_count = int_or_none(self._search_regex(
+            r'Comments\s*:\s*(\d+)', webpage, 'comment count', default=None))
     
     
-def triplet_to_brightness(rgbtuple):
-    # return the brightness (grey level) along the scale 0.0==black to
-    # 1.0==white
-    r = 0.299
-    g = 0.587
-    b = 0.114
-    return r*rgbtuple[0] + g*rgbtuple[1] + b*rgbtuple[2]
+class ClipRsIE(OnetBaseIE):
+    _VALID_URL = r'https?://(?:www\.)?clip\.rs/(?P<id>[^/]+)/\d+'
+    _TEST = {
+        'url': 'http://www.clip.rs/premijera-frajle-predstavljaju-novi-spot-za-pesmu-moli-me-moli/3732',
+        'md5': 'c412d57815ba07b56f9edc7b5d6a14e5',
+        'info_dict': {
+            'id': '1488842.1399140381',
+            'ext': 'mp4',
+            'title': 'PREMIJERA Frajle predstavljaju novi spot za pesmu Moli me, moli',
+            'description': 'md5:56ce2c3b4ab31c5a2e0b17cb9a453026',
+            'duration': 229,
+            'timestamp': 1459850243,
+            'upload_date': '20160405',
+        }
+    }
     
-            with patch as m:
-            m.return_value.modify = unittest.mock.Mock(
-                side_effect=ZeroDivisionError)
-            s = self.SELECTOR()
-            self.addCleanup(s.close)
-            rd, wr = self.make_socketpair()
-            s.register(rd, selectors.EVENT_READ)
-            self.assertEqual(len(s._map), 1)
-            with self.assertRaises(ZeroDivisionError):
-                s.modify(rd, selectors.EVENT_WRITE)
-            self.assertEqual(len(s._map), 0)
+            # Also run the annotation graph with tracing enabled.
+        annotations_with_trace, traces = sess.run(
+            [trace['annotations'], trace['traces']],
+            feed_dict={trace['input_batch']: test_reader_strings})
     
-        def _guess_media_encoding(self, source):
-        info = source.byteStream.info()
-        if 'Content-Type' in info:
-            for param in info.getplist():
-                if param.startswith('charset='):
-                    return param.split('=', 1)[1].lower()
+          self.assertAllEqual(network_tensors[0].eval(), [3])
+      self.assertAllEqual(network_tensors[1].eval(),
+                          [[[1.0, 0.5, 0.5],
+                            [2.0, 0.5, 0.5],
+                            [0.5, 3.0, 0.5]]])  # pyformat: disable
+      self.assertAllEqual(network_tensors[2].eval(),
+                          [[1.0, 0.5, 0.5],
+                           [2.0, 0.5, 0.5],
+                           [0.5, 3.0, 0.5]])  # pyformat: disable
+      self.assertAllEqual(network_tensors[3].eval(),
+                          [[1.0, 0.0, 0.0],
+                           [1.0, 0.0, 0.0],
+                           [0.0, 1.0, 0.0]])  # pyformat: disable
     
-    @dataclasses.dataclass
-class IV:
-    T_IV4 = dataclasses.InitVar
-    iv0: dataclasses.InitVar[int]
-    iv1: dataclasses.InitVar
-    iv2: T_IV2
-    iv3: T_IV3
-    not_iv4: T_IV4  # When using string annotations, this field is not recognized as an InitVar.
+      Given a MasterSpec that defines the DRAGNN architecture, completes the spec so
+  that it can be used to build a DRAGNN graph and run training/inference.
+    
+      Returns:
+    List of created variables.
+  '''
+  # Use a dict to dedupe captured variables.
+  created_vars = {}
+    
+    from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+    
+    
+if __name__ == '__main__':
+  tf.app.run()
+
+    
+                app.config['IMAGE_STORE_TYPE'] = 'fs'
+            app.config['IMAGE_STORE_PATH'] = '/var/app/images'
+            app.config['IMAGE_STORE_BASE_URL'] = 'http://img.website.com'
+            image_store_config = app.config.get_namespace('IMAGE_STORE_')
+    
+        def _fail(self, *args, **kwargs):
+        raise RuntimeError('The session is unavailable because no secret '
+                           'key was set.  Set the secret_key on the '
+                           'application to something unique and secret.')
+    __setitem__ = __delitem__ = clear = pop = popitem = \
+        update = setdefault = _fail
+    del _fail
+    
+        class Test(Base):
+        SECRET_KEY = 'config'
+    
+    
+def test_installed_package_paths(limit_loader, modules_tmpdir,
+                                 modules_tmpdir_prefix, purge_module,
+                                 monkeypatch):
+    installed_path = modules_tmpdir.mkdir('path')
+    monkeypatch.syspath_prepend(installed_path)
+    
+        flask.before_render_template.connect(record, app)
+    try:
+        rv = app.test_client().get('/')
+        assert len(recorded) == 1
+        template, context = recorded[0]
+        assert template.name == 'simple_template.html'
+        assert context['whiskey'] == 43
+        assert rv.data == b'<h1>43</h1>'
+    finally:
+        flask.before_render_template.disconnect(record, app)
+    
+        def test_binary_put(self):
+        request = requests.Request('PUT', 'http://example.com',
+                                   data=u'ööö'.encode('utf-8')).prepare()
+        assert isinstance(request.body, bytes)
+    
+        text_302 = (b'HTTP/1.1 302 FOUND\r\n'
+                b'Content-Length: 0\r\n'
+                b'Location: /\r\n\r\n')
+    
+    # Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+html_theme_options = {
+    'show_powered_by': False,
+    'github_user': 'requests',
+    'github_repo': 'requests',
+    'github_banner': True,
+    'show_related': False,
+    'note_bg': '#FFF59C'
+}
+    
+            return r
+    
+            cid = CaseInsensitiveDict()
+        cid['Accept'] = 'application/json'
+        cid['aCCEPT'] == 'application/json'  # True
+        list(cid) == ['Accept']  # True
+    
+    _init()
+
+    
+        def test_setup_config_defaults(self):
+        '''Test setup with defaults.'''
+        config = {
+            'splunk': {
+                'host': 'host',
+                'token': 'secret',
+            }
+        }
+    
+        hass.services.register(DOMAIN, SERVICE_VOLUME_MUTE,
+                           lambda service:
+                           keyboard.tap_key(keyboard.volume_mute_key),
+                           schema=TAP_KEY_SCHEMA)
+    
+        @patch('{}._get_homehub_data'.format(patch_file), new=_get_homehub_data)
+    def test_config_minimal(self):
+        '''Test the setup with minimal configuration.'''
+        config = {
+            'device_tracker': {
+                CONF_HOST: 'foo'
+            }
+        }
+        result = bt_home_hub_5.get_scanner(None, config)
