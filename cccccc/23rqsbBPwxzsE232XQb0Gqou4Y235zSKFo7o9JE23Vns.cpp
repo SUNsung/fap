928a433085792a9cc769b292afaccce8eed08522
SUNsung <weatherius@gmail.com>
2018-07-02 18:29:56 +0300

    Rewrite [23rqsbBPwxzsE232XQb0Gqou4Y235zSKFo7o9JE23Vns.cpp] File in: 2018-07-02 18:29:55

--- cccccc/23rqsbBPwxzsE232XQb0Gqou4Y235zSKFo7o9JE23Vns.cpp
+++ cccccc/23rqsbBPwxzsE232XQb0Gqou4Y235zSKFo7o9JE23Vns.cpp
@@ -1,215 +1,221 @@
 
         
-        #ifndef TENSORFLOW_KERNELS_SPARSE_TENSOR_DENSE_ADD_OP_H_
-#define TENSORFLOW_KERNELS_SPARSE_TENSOR_DENSE_ADD_OP_H_
-    
-    class RecordWriter {
+        class App {
  public:
-  // Create a writer that will append data to '*dest'.
-  // '*dest' must be initially empty.
-  // '*dest' must remain live while this Writer is in use.
-  RecordWriter(WritableFile* dest,
-               const RecordWriterOptions& options = RecordWriterOptions());
+  static void Call(const std::string& method,
+                   const base::ListValue& arguments);
     }
     
-    namespace internal {
-template <typename T>
-struct functor_traits<scalar_sigmoid_fast_derivative_op<T> > {
-  enum {
-    Cost = NumTraits<T>::AddCost * 2 + NumTraits<T>::MulCost,
-    PacketAccess = packet_traits<T>::HasAdd && packet_traits<T>::HasMul &&
-                   packet_traits<T>::HasNegate
-  };
-};
-}  // namespace internal
+    // Call method of an object in browser and return the result.
+// function CallObjectMethod(id, type, method, args);
+v8::Handle<v8::Value> CallObjectMethodSync(int routing_id,
+                                           int object_id,
+                                           const std::string& type,
+                                           const std::string& method,
+                                           v8::Handle<v8::Value> args);
     
-    #include 'tensorflow/core/platform/platform.h'
+    public:
+  EventListener(int id,
+                const base::WeakPtr<DispatcherHost>& dispatcher_host,
+                const base::DictionaryValue& option);
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
     
-      Status OnWorkStartedLocked() override {
-    offset_ = 0;
-    TF_RETURN_IF_ERROR(env_->NewRandomAccessFile(current_work(), &file_));
+    {    }
+    
+    
+double CV_ModelEstimator2_Test::get_success_error_level( int /*test_case_idx*/, int /*i*/, int /*j*/ )
+{
+    return 0;
+}
+    
+    #define CUSTOM_FUNCTION_ID 1000
+    
+    /// Copies bytes from a source buffer sequence to a target buffer sequence.
+/**
+ * @param target A modifiable buffer sequence representing the memory regions to
+ * which the bytes will be copied.
+ *
+ * @param source A non-modifiable buffer sequence representing the memory
+ * regions from which the bytes will be copied.
+ *
+ * @returns The number of bytes copied.
+ *
+ * @note The number of bytes copied is the lesser of:
+ *
+ * @li @c buffer_size(target)
+ *
+ * @li @c buffer_size(source)
+ *
+ * This function is implemented in terms of @c memcpy, and consequently it
+ * cannot be used to copy between overlapping memory regions.
+ */
+template <typename MutableBufferSequence, typename ConstBufferSequence>
+std::size_t buffer_copy(const MutableBufferSequence& target,
+    const ConstBufferSequence& source)
+{
+  std::size_t total_bytes_copied = 0;
     }
     
-    sampled_audio: A rank 2 tensor containing all tracks of the audio. Dimension 0
-    is time and dimension 1 is the channel.
-contents: The binary audio file contents.
-file_format: A string describing the audio file format. This must be 'wav'.
-samples_per_second: The number of samples per second that the audio should have.
-bits_per_second: The approximate bitrate of the encoded audio file. This is
-    ignored by the 'wav' file format.
-)doc');
-    
-    MPIUtils::MPIUtils(const std::string& worker_name) {
-  InitMPI();
-  // Connect the MPI process IDs to the worker names that are used by TF.
-  // Gather the names of all the active processes (name can't be longer than
-  // 128 bytes)
-  int proc_id = 0, number_of_procs = 1;
-  char my_name[max_worker_name_length];
-  MPI_CHECK(MPI_Comm_rank(MPI_COMM_WORLD, &proc_id));
-  MPI_CHECK(MPI_Comm_size(MPI_COMM_WORLD, &number_of_procs));
-    }
-    
-    // Comparison ops.
-REGISTER_OP_NO_GRADIENT('Less');
-REGISTER_OP_NO_GRADIENT('LessEqual');
-REGISTER_OP_NO_GRADIENT('Greater');
-REGISTER_OP_NO_GRADIENT('GreaterEqual');
-REGISTER_OP_NO_GRADIENT('Equal');
-REGISTER_OP_NO_GRADIENT('NotEqual');
-    
-     private:
-  const EnumDescriptor* descriptor_;
-    
-    #endif  // GOOGLE_PROTOBUF_COMPILER_CSHARP_MAP_FIELD_H__
-    
-    #include <google/protobuf/compiler/csharp/csharp_doc_comment.h>
-#include <google/protobuf/compiler/csharp/csharp_helpers.h>
-#include <google/protobuf/compiler/csharp/csharp_repeated_enum_field.h>
-    
-      // overroads RepeatedImmutableMessageFieldGenerator -------------------------
-  void GenerateMembers(io::Printer* printer) const;
-  void GenerateBuilderMembers(io::Printer* printer) const;
-  void GenerateParsingCode(io::Printer* printer) const;
-  void GenerateSerializationCode(io::Printer* printer) const;
-  void GenerateSerializedSizeCode(io::Printer* printer) const;
-    
-    #define ASSERT_HOST(x) if (!(x))                                        \
-  {                                                                     \
-    ASSERT_FAILED.error(#x, ABORT, 'in file %s, line %d',               \
-                        __FILE__, __LINE__);                            \
+      /// Construct, passing the specified argument to initialise the next layer.
+  template <typename Arg>
+  explicit buffered_read_stream(Arg& a)
+    : next_layer_(a),
+      storage_(default_buffer_size)
+  {
   }
     
-    void FreeMFOutline(void *agr);  //MFOUTLINE                             Outline);
     
-    extern double TanhTable[];
-extern double LogisticTable[];
+    {
+    {} // namespace asio
+} // namespace boost
     
-    // Include automatically generated configuration file if running autoconf.
-#ifdef HAVE_CONFIG_H
-#include 'config_auto.h'
-#endif
+    template<class T, class TimeSystem>
+class base_time;
     
-      // Sets the neighbours and good_stroke_neighbours members of the blob by
-  // searching close on all 4 sides.
-  // When finding leader dots/dashes, there is a slightly different rule for
-  // what makes a good neighbour.
-  // If activate_line_trap, then line-like objects are found and isolated.
-  void SetNeighbours(bool leaders, bool activate_line_trap, BLOBNBOX* blob);
+    #include <boost/asio/detail/pop_options.hpp>
     
-    #endif
-
+    #endif // !defined(BOOST_ASIO_WINDOWS) && !defined(__CYGWIN__)
+    
+    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+    
+    // Calls to asio_handler_invoke must be made from a namespace that does not
+// contain overloads of this function. The boost_asio_handler_invoke_helpers
+// namespace is defined here for that purpose.
+namespace boost_asio_handler_invoke_helpers {
+    }
+    
+    #define BOOST_ASIO_COMPOSED_CONNECT_HANDLER_CHECK( \
+    handler_type, handler, iter_type) \
+  \
+  typedef BOOST_ASIO_HANDLER_TYPE(handler_type, \
+      void(boost::system::error_code, iter_type)) \
+    asio_true_handler_type; \
+  \
+  BOOST_ASIO_HANDLER_TYPE_REQUIREMENTS_ASSERT( \
+      sizeof(boost::asio::detail::two_arg_handler_test( \
+          boost::asio::detail::clvref< \
+            asio_true_handler_type>(), \
+          static_cast<const boost::system::error_code*>(0), \
+          static_cast<const iter_type*>(0))) == 1, \
+      'ComposedConnectHandler type requirements not met') \
+  \
+  typedef boost::asio::detail::handler_type_requirements< \
+      sizeof( \
+        boost::asio::detail::argbyv( \
+          boost::asio::detail::clvref< \
+            asio_true_handler_type>())) + \
+      sizeof( \
+        boost::asio::detail::lvref< \
+          asio_true_handler_type>()( \
+            boost::asio::detail::lvref<const boost::system::error_code>(), \
+            boost::asio::detail::lvref<const iter_type>()), \
+        char(0))> BOOST_ASIO_UNUSED_TYPEDEF
+    
+      // Start looping through starting at the first options
+  // (so skip the exports)
+  for (auto iter = line.begin() + options_index; iter != line.end(); ++iter) {
+    if (iter->compare('-ro') == 0 || iter->compare('-o') == 0) {
+      readonly = 1;
+    }
+  }
+    
+    TEST_F(FlagsTests, test_aliases) {
+  EXPECT_TRUE(FLAGS_shell_only_alias);
+  FLAGS_shell_only = false;
+  EXPECT_FALSE(FLAGS_shell_only);
+  EXPECT_FALSE(FLAGS_shell_only_alias);
+}
+    
+    class PermissionsRunnable : public InternalRunnable {
+ public:
+  PermissionsRunnable() : InternalRunnable('PermissionsRunnable') {}
+  PermissionsRunnable(const std::string& name) : InternalRunnable(name) {}
+    }
+    
+    TEST_F(RocksDBDatabasePluginTests, test_rocksdb_loglevel) {
+  // Make sure a log file was created.
+  EXPECT_FALSE(pathExists(path_ + '/LOG'));
+    }
+    
+     protected:
+  /// Require each plugin tester to implement a set name.
+  virtual std::string name() = 0;
+    
+      static CGEventRef eventCallback(CGEventTapProxy proxy,
+                                  CGEventType type,
+                                  CGEventRef event,
+                                  void* refcon);
+    
+    /**
+ * @brief An osquery EventPublisher for the Apple SCNetwork Reachability API.
+ *
+ * This exposes a lightweight network change monitoring capability.
+ *
+ */
+class SCNetworkEventPublisher
+    : public EventPublisher<SCNetworkSubscriptionContext,
+                            SCNetworkEventContext> {
+  DECLARE_PUBLISHER('scnetwork');
+    }
+    
+      // Regardless of the status of the kernel extension, if the device node does
+  // not exist then the kernel publisher will silently shutdown.
+  // This is not considered an error, and does not emit an error log.
+  if (!isWritable(kKernelDevice)) {
+    return Status(2, 'Cannot access ' + kKernelDevice);
+  }
+    
+    std::vector<float> YUVToRGB(const std::vector<std::vector<float> >& yuv) {
+  std::vector<float> rgb(3 * yuv[0].size());
+  for (size_t i = 0, p = 0; p < rgb.size(); ++i, p += 3) {
+    const float y = yuv[0][i];
+    const float u = yuv[1][i];
+    const float v = yuv[2][i];
+    rgb[p + 0] = Clip(YUVToR(y, u, v));
+    rgb[p + 1] = Clip(YUVToG(y, u, v));
+    rgb[p + 2] = Clip(YUVToB(y, u, v));
+  }
+  return rgb;
+}
     
     namespace guetzli {
     }
     
-    static const int kCrToRedTable[256] = {
-  -179, -178, -177, -175, -174, -172, -171, -170, -168, -167, -165, -164,
-  -163, -161, -160, -158, -157, -156, -154, -153, -151, -150, -149, -147,
-  -146, -144, -143, -142, -140, -139, -137, -136, -135, -133, -132, -130,
-  -129, -128, -126, -125, -123, -122, -121, -119, -118, -116, -115, -114,
-  -112, -111, -109, -108, -107, -105, -104, -102, -101, -100,  -98,  -97,
-   -95,  -94,  -93,  -91,  -90,  -88,  -87,  -86,  -84,  -83,  -81,  -80,
-   -79,  -77,  -76,  -74,  -73,  -72,  -70,  -69,  -67,  -66,  -64,  -63,
-   -62,  -60,  -59,  -57,  -56,  -55,  -53,  -52,  -50,  -49,  -48,  -46,
-   -45,  -43,  -42,  -41,  -39,  -38,  -36,  -35,  -34,  -32,  -31,  -29,
-   -28,  -27,  -25,  -24,  -22,  -21,  -20,  -18,  -17,  -15,  -14,  -13,
-   -11,  -10,   -8,   -7,   -6,   -4,   -3,   -1,    0,    1,    3,    4,
-     6,    7,    8,   10,   11,   13,   14,   15,   17,   18,   20,   21,
-    22,   24,   25,   27,   28,   29,   31,   32,   34,   35,   36,   38,
-    39,   41,   42,   43,   45,   46,   48,   49,   50,   52,   53,   55,
-    56,   57,   59,   60,   62,   63,   64,   66,   67,   69,   70,   72,
-    73,   74,   76,   77,   79,   80,   81,   83,   84,   86,   87,   88,
-    90,   91,   93,   94,   95,   97,   98,  100,  101,  102,  104,  105,
-   107,  108,  109,  111,  112,  114,  115,  116,  118,  119,  121,  122,
-   123,  125,  126,  128,  129,  130,  132,  133,  135,  136,  137,  139,
-   140,  142,  143,  144,  146,  147,  149,  150,  151,  153,  154,  156,
-   157,  158,  160,  161,  163,  164,  165,  167,  168,  170,  171,  172,
-   174,  175,  177,  178
-};
-    
-    namespace guetzli {
-    }
-    
-    std::vector<uint8_t> DecodeJpegToRGB(const JPEGData& jpg) {
-  if (jpg.components.size() == 1 ||
-      (jpg.components.size() == 3 &&
-       HasYCbCrColorSpace(jpg) && (jpg.Is420() || jpg.Is444()))) {
-    OutputImage img(jpg.width, jpg.height);
-    img.CopyFromJpegData(jpg);
-    return img.ToSRGB();
-  }
-  return std::vector<uint8_t>();
+    const double* Srgb8ToLinearTable() {
+  static const double* const kSrgb8ToLinearTable = NewSrgb8ToLinearTable();
+  return kSrgb8ToLinearTable;
 }
     
-    #include <stdint.h>
+    // Licensed under the MIT License (the 'License'); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+// http://opensource.org/licenses/MIT
     
-      typedef std::unordered_map<detail::TypeDescriptor,
-                             detail::SingletonHolderBase*,
-                             detail::TypeDescriptorHasher> SingletonMap;
-  Synchronized<SingletonMap> singletons_;
-  Synchronized<std::unordered_set<detail::SingletonHolderBase*>>
-      eagerInitSingletons_;
-  Synchronized<std::vector<detail::TypeDescriptor>> creationOrder_;
-    
-    #pragma once
-    
-    int main(int argc, char* argv[]) {
-  // Using log macros before calling folly::initLogging() will use the default
-  // log settings defined by folly::initializeLoggerDB().  The default behavior
-  // is to log WARNING+ messages to stderr.
-  XLOG(INFO) << 'log messages less than WARNING will be ignored';
-  XLOG(ERR) << 'error messages before initLogging() will be logged to stderr';
+    class WakeUpLock {
+  public:
+    WakeUpLock();
+    ~WakeUpLock();
     }
     
+    //
+//  boost_exception.cpp
+//  comm
+//
+//  Created by yanguoyue on 16/5/20.
+//
     
-    {  const char* value_{nullptr};
-};
+    CommFrequencyLimit::~CommFrequencyLimit()
+{}
     
-      /**
-   * Add one task for execution in the parent executor, and use the given
-   * priority for one task submission to parent executor.
-   *
-   * Since in-order execution of tasks submitted to SerialExecutor is
-   * guaranteed, the priority given here does not necessarily reflect the
-   * execution priority of the task submitted with this call to
-   * `addWithPriority`. The given priority is passed on to the parent executor
-   * for the execution of one of the SerialExecutor's tasks.
-   */
-  void addWithPriority(Func func, int8_t priority) override;
-  uint8_t getNumPriorities() const override {
-    return parent_->getNumPriorities();
-  }
-    
-    template <typename T>
-FOLLY_ALWAYS_INLINE T* hazptr_holder::get_protected(
-    const std::atomic<T*>& src) noexcept {
-  return get_protected(src, [](T* t) { return t; });
+    Test_Spy_Sample::~Test_Spy_Sample()
+{
+    SPY_DETACH_CLASS();
 }
     
-      // Disabled log messages should not evaluate their arguments
-  bool argumentEvaluated = false;
-  auto getValue = [&] {
-    argumentEvaluated = true;
-    return 5;
-  };
-  FB_LOGC(foobar, DBG3, 'discarded message: %d', getValue());
-  EXPECT_FALSE(argumentEvaluated);
     
-    
-    {} // namespace folly
-
-    
-        /**
-     * Shortcut to change the action for standard error.
-     * Note that stderr(1) will redirect the standard error to the same
-     * file descriptor as standard output; the equivalent of bash's '2>&1'
-     */
-    Options& stderrFd(int action) { return fd(STDERR_FILENO, action); }
+bool TSpy::SpyHookLogFunc(XLoggerInfo_t& _info, std::string& _log)
+{
+    __attribute__((unused)) int i = 0;
+    return true;
+}
