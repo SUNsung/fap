d5d61b68eb326c5507c1d92abf9168722d653309
SUNsung <weatherius@gmail.com>
2018-07-03 23:32:04 +0300

    Rewrite [48Eb4SwqE2vE48fXTNPcX9PLg48xyY75TobXdU48YrZ.cpp] File in: 2018-07-03 23:32:03

--- cccccc/48Eb4SwqE2vE48fXTNPcX9PLg48xyY75TobXdU48YrZ.cpp
+++ cccccc/48Eb4SwqE2vE48fXTNPcX9PLg48xyY75TobXdU48YrZ.cpp
@@ -1,185 +1,230 @@
 
         
-        int PowerIphone::get_power_seconds_left() {
-	if (UpdatePowerInfo()) {
-		return nsecs_left;
-	} else {
-		return -1;
-	}
-}
-    
-    	base = p_base;
-	int ret = 0;
-	int authmode = p_validate_certs ? MBEDTLS_SSL_VERIFY_REQUIRED : MBEDTLS_SSL_VERIFY_NONE;
-    
-    public:
-	Error connect_to_url(String p_url, PoolVector<String> p_protocols = PoolVector<String>(), bool gd_mp_api = false);
-    
-    	static RWLock *lock;
-	static HashMap<StringName, ClassInfo, StringNameHasher> classes;
-	static HashMap<StringName, StringName, StringNameHasher> resource_base_extensions;
-	static HashMap<StringName, StringName, StringNameHasher> compat_classes;
-    
-    void VideoPlayer::_notification(int p_notification) {
-    }
-    
-    	AcceptDialog *edit_variable_dialog;
-	PropertyEditor *edit_variable_edit;
-    
-      // Chroma IDCT, with upsampling
-	jpgd_block_t temp_block[64];
-    
-    PrefetchGPUDataTransferer::PrefetchGPUDataTransferer(int deviceId) : GranularGPUDataTransferer(deviceId, nullptr, nullptr, true)
-{
-     cudaStreamCreateWithFlags(&m_stream, cudaStreamNonBlocking) || 'cudaStreamCreateWithFlags failed (PrefetchGPUDataTransferer ctor)';
-}
-    
-        if (useDistributedMBReading)
-        dataReader->StartDistributedMinibatchLoop(mbSize, 0, m_mpi->CurrentNodeRank(), m_mpi->NumNodesInUse(), inputMatrices.GetStreamDescriptions(), totalEpochSize);
-    else
-        dataReader->StartMinibatchLoop(mbSize, 0, inputMatrices.GetStreamDescriptions(), totalEpochSize);
-    
-    
-    {    wstring GetError(const std::wstring& linePrefix) const override
-    {
-        return TextLocation::CreateIssueMessage(locations, linePrefix.c_str(), kind(), msra::strfun::utf16(what()).c_str());
-    }
-    // pretty-print this as an error message
-    void /*ScriptingException::*/ PrintError(const std::wstring& linePrefix) const override
-    {
-        TextLocation::PrintIssue(locations, linePrefix.c_str(), kind(), msra::strfun::utf16(what()).c_str());
-    }
-    void AddLocation(TextLocation where)
-    {
-        locations.push_back(where);
-    }
+        
+    { private:
+  DebugOptions debug_options_;
 };
     
-    // ---------------------------------------------------------------------------
-// array_ref -- wraps a C pointer to an array together with its size.
-//
-// Called _ref because this is a reference to the array rather than the array
-// itself (since it wraps a pointer). No need to pass an array_ref by reference.
-//
-// operator[] checks index bounds in Debug builds. size() is provided such
-// that this class can be substituted for STL vector in many cases.
-// ---------------------------------------------------------------------------
-    
-    
-#endif  // STORAGE_LEVELDB_DB_WRITE_BATCH_INTERNAL_H_
-
-    
-      // create first key range
-  leveldb::WriteBatch batch;
-  for (size_t i = 0; i < kNumKeys; i++) {
-    batch.Put(Key1(i), 'value for range 1 key');
-  }
-  ASSERT_OK(db->Write(leveldb::WriteOptions(), &batch));
-    
-    int main(int argc, char** argv) {
-  return leveldb::test::RunAllTests();
-}
-
-    
-    Status Footer::DecodeFrom(Slice* input) {
-  const char* magic_ptr = input->data() + kEncodedLength - 8;
-  const uint32_t magic_lo = DecodeFixed32(magic_ptr);
-  const uint32_t magic_hi = DecodeFixed32(magic_ptr + 4);
-  const uint64_t magic = ((static_cast<uint64_t>(magic_hi) << 32) |
-                          (static_cast<uint64_t>(magic_lo)));
-  if (magic != kTableMagicNumber) {
-    return Status::Corruption('not an sstable (bad magic number)');
-  }
+    namespace tensorflow {
     }
     
-    class HASH { };
-    
-    #ifndef STORAGE_LEVELDB_UTIL_HISTOGRAM_H_
-#define STORAGE_LEVELDB_UTIL_HISTOGRAM_H_
-    
-    
-    {}  // namespace leveldb
-    
-    bool InternalFilterPolicy::KeyMayMatch(const Slice& key, const Slice& f) const {
-  return user_policy_->KeyMayMatch(ExtractUserKey(key), f);
-}
-    
-    TEST(FormatTest, InternalKeyShortestSuccessor) {
-  ASSERT_EQ(IKey('g', kMaxSequenceNumber, kValueTypeForSeek),
-            ShortSuccessor(IKey('foo', 100, kTypeValue)));
-  ASSERT_EQ(IKey('\xff\xff', 100, kTypeValue),
-            ShortSuccessor(IKey('\xff\xff', 100, kTypeValue)));
-}
-    
-    
-    {// Remove registry-helper macros from the SDK.
-#undef REGISTER
-#define REGISTER 'Do not REGISTER in the osquery SDK'
-#undef REGISTER_INTERNAL
-#define REGISTER_INTERNAL 'Do not REGISTER_INTERNAL in the osquery SDK'
-#undef CREATE_REGISTRY
-#define CREATE_REGISTRY 'Do not CREATE_REGISTRY in the osquery SDK'
-#undef CREATE_LAZY_REGISTRY
-#define CREATE_LAZY_REGISTRY 'Do not CREATE_LAZY_REGISTRY in the osquery SDK'
-}
-
-    
-    TEST_F(ViewsConfigParserPluginTests, test_swap_view) {
-  Config c;
-  std::vector<std::string> old_views_vec;
-  scanDatabaseKeys(kQueries, old_views_vec, 'config_views.');
-  EXPECT_EQ(old_views_vec.size(), 1U);
-  old_views_vec.clear();
-  auto s = c.update(getTestConfigMap('view_test.conf'));
-  EXPECT_TRUE(s.ok());
-  scanDatabaseKeys(kQueries, old_views_vec, 'config_views.');
-  EXPECT_EQ(old_views_vec.size(), 1U);
-  EXPECT_EQ(old_views_vec[0], 'config_views.kernel_hashes_new');
+    class RecordWriter {
+ public:
+  // Create a writer that will append data to '*dest'.
+  // '*dest' must be initially empty.
+  // '*dest' must remain live while this Writer is in use.
+  RecordWriter(WritableFile* dest,
+               const RecordWriterOptions& options = RecordWriterOptions());
     }
     
-    
-    {  auto all_flags = Flag::flags();
-  EXPECT_TRUE(all_flags['shell_only'].detail.shell);
-  EXPECT_TRUE(all_flags['extension_only'].detail.external);
+    void SYCLDeviceContext::CopyCPUTensorToDevice(const Tensor *cpu_tensor,
+                                              Device *device,
+                                              Tensor *device_tensor,
+                                              StatusCallback done) const {
+  const int64 total_bytes = cpu_tensor->TotalBytes();
+  if (total_bytes > 0) {
+    const void *src_ptr = DMAHelper::base(cpu_tensor);
+    void *dst_ptr = DMAHelper::base(device_tensor);
+    switch (cpu_tensor->dtype()) {
+      case DT_FLOAT:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<float *>(dst_ptr), static_cast<const float *>(src_ptr),
+            total_bytes);
+        break;
+      case DT_DOUBLE:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<double *>(dst_ptr),
+            static_cast<const double *>(src_ptr), total_bytes);
+        break;
+      case DT_INT32:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<int32 *>(dst_ptr), static_cast<const int32 *>(src_ptr),
+            total_bytes);
+        break;
+      case DT_INT64:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<int64 *>(dst_ptr), static_cast<const int64 *>(src_ptr),
+            total_bytes);
+        break;
+      case DT_HALF:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<Eigen::half *>(dst_ptr),
+            static_cast<const Eigen::half *>(src_ptr), total_bytes);
+        break;
+      case DT_COMPLEX64:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<std::complex<float> *>(dst_ptr),
+            static_cast<const std::complex<float> *>(src_ptr), total_bytes);
+        break;
+      case DT_COMPLEX128:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<std::complex<double> *>(dst_ptr),
+            static_cast<const std::complex<double> *>(src_ptr), total_bytes);
+        break;
+      case DT_INT8:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<int8 *>(dst_ptr), static_cast<const int8 *>(src_ptr),
+            total_bytes);
+        break;
+      case DT_INT16:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<int16 *>(dst_ptr), static_cast<const int16 *>(src_ptr),
+            total_bytes);
+        break;
+      case DT_UINT8:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<uint8 *>(dst_ptr), static_cast<const uint8 *>(src_ptr),
+            total_bytes);
+        break;
+      case DT_UINT16:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<uint16 *>(dst_ptr),
+            static_cast<const uint16 *>(src_ptr), total_bytes);
+        break;
+      case DT_BOOL:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<bool *>(dst_ptr), static_cast<const bool *>(src_ptr),
+            total_bytes);
+        break;
+      default:
+        assert(false && 'unsupported type');
+    }
+  }
+  device->eigen_sycl_device()->synchronize();
+  done(Status::OK());
 }
     
-    #include <osquery/status.h>
-    
-    #include <gtest/gtest.h>
-    
-    #pragma once
+    #include 'tensorflow/core/kernels/cwise_ops_common.h'
     
     
-/*
- * DumpCrashStack.h
- *
- *  Created on: 2012-9-28
- *      Author: yerungui
- */
+    {  // From the zlib manual (http://www.zlib.net/manual.html):
+  // The strategy parameter is used to tune the compression algorithm. Use the
+  // value Z_DEFAULT_STRATEGY for normal data, Z_FILTERED for data produced by
+  // a filter (or predictor), Z_HUFFMAN_ONLY to force Huffman encoding only
+  // (no string match), or Z_RLE to limit match distances to one
+  // (run-length encoding). Filtered data consists mostly of small values with
+  // a somewhat random distribution. In this case, the compression algorithm is
+  // tuned to compress them better. The effect of Z_FILTERED is to force more
+  // Huffman coding and less string matching; it is somewhat intermediate
+  // between Z_DEFAULT_STRATEGY and Z_HUFFMAN_ONLY. Z_RLE is designed to be
+  // almost as fast as Z_HUFFMAN_ONLY, but give better compression for
+  // PNG image data. The strategy parameter only affects the compression ratio
+  // but not the correctness of the compressed output even if it is not set
+  // appropriately. Z_FIXED prevents the use of dynamic Huffman codes, allowing
+  // for a simpler decoder for special applications.
+  int8 compression_strategy = Z_DEFAULT_STRATEGY;
+};
     
+      // If there are too few results, assume the result will be unreliable and
+  // bail.
+  const int64 how_many_results = previous_results_.size();
+  const int64 earliest_time = previous_results_.front().first;
+  const int64 samples_duration = current_time_ms - earliest_time;
+  if ((how_many_results < minimum_count_) ||
+      (samples_duration < (average_window_duration_ms_ / 4))) {
+    *found_command = previous_top_label_;
+    *score = 0.0f;
+    *is_new_command = false;
+    return Status::OK();
+  }
     
-    {}
-#endif
-
+    namespace {
+    }
+    
+    #include <math.h>
+    
+    namespace guetzli {
+    }
+    
+    namespace guetzli {
+    }
+    
+    enum JpegReadMode {
+  JPEG_READ_HEADER,   // only basic headers
+  JPEG_READ_TABLES,   // headers and tables (quant, Huffman, ...)
+  JPEG_READ_ALL,      // everything
+};
+    
+    int BuildJpegHuffmanTable(const int* count_in, const int* symbols,
+                          HuffmanTableEntry* lut) {
+  HuffmanTableEntry code;    // current table entry
+  HuffmanTableEntry* table;  // next available space in table
+  int len;         // current code length
+  int idx;         // symbol index
+  int key;         // prefix code
+  int reps;        // number of replicate key values in current table
+  int low;         // low bits for current root entry
+  int table_bits;  // key length of current table
+  int table_size;  // size of current table
+  int total_size;  // sum of root table size and 2nd level table sizes
+    }
+    
+      OutputImageComponent& component(int c) { return components_[c]; }
+  const OutputImageComponent& component(int c) const { return components_[c]; }
+    
+    	if (!xlogger_IsEnabledFor((TLogLevel)_level)) {
+		return;
+	}
+    
+    bool WakeUpLock::IsLocking() {
+    return ::wakeupLock_IsLocking(object_);
+}
     
     
     {  private:
-//    int m_t;
+    size_t count_;
+    uint64_t time_span_;
+    std::list<uint64_t> touch_times_;
 };
     
-      virtual ~AbstractOptionHandler();
+    // Licensed under the MIT License (the 'License'); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+// http://opensource.org/licenses/MIT
     
-    class HttpConnection;
-class SocketCore;
+    // Licensed under the MIT License (the 'License'); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+// http://opensource.org/licenses/MIT
     
-    
-    {} // namespace aria2
-    
-      int64_t offset_;
-    
-    #include 'common.h'
-    
-    namespace aria2 {
+    // CheckHandler is the class constructed by failing CHECK macros. CheckHandler
+// will log information about the failures and abort when it is destructed.
+class CheckHandler {
+ public:
+  CheckHandler(const char* check, const char* file, const char* func, int line)
+      : log_(GetErrorLogInstance()) {
+    log_ << file << ':' << line << ': ' << func << ': Check `' << check
+         << '' failed. ';
+  }
     }
     
-      const std::string& getPassword() const { return password_; }
+    // Macros for declaring flags.
+#define DECLARE_bool(name) extern bool FLAG(name)
+#define DECLARE_int32(name) extern int32_t FLAG(name)
+#define DECLARE_int64(name) extern int64_t FLAG(name)
+#define DECLARE_double(name) extern double FLAG(name)
+#define DECLARE_string(name) extern std::string FLAG(name)
+    
+    
+    {  // Print user counters
+  for (const auto &ucn : user_counter_names_) {
+    auto it = run.counters.find(ucn);
+    if(it == run.counters.end()) {
+      Out << ',';
+    } else {
+      Out << ',' << it->second;
+    }
+  }
+  Out << '\n';
+}
+    
+    #ifdef BENCHMARK_OS_EMSCRIPTEN
+#include <emscripten.h>
+#endif
+    
+    #include <cstdlib>
+    
+    
+    {void SleepForSeconds(double seconds) {
+  SleepForMicroseconds(static_cast<int>(seconds * kNumMicrosPerSecond));
+}
+#endif  // BENCHMARK_OS_WINDOWS
+}  // end namespace benchmark
