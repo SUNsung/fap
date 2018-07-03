7acfd15308efdfdd71de520312ab02a07b1e613d
SUNsung <weatherius@gmail.com>
2018-07-03 12:56:18 +0300

    Rewrite [39SBMq24Fd4rw39buLQ54lHMqw394ZgHwZE7YVw39B7p.cpp] File in: 2018-07-03 12:56:18

--- cccccc/39SBMq24Fd4rw39buLQ54lHMqw394ZgHwZE7YVw39B7p.cpp
+++ cccccc/39SBMq24Fd4rw39buLQ54lHMqw394ZgHwZE7YVw39B7p.cpp
@@ -1,157 +1,153 @@
 
         
-        #endif  // CONTENT_NW_SRC_API_APP_APP_H_
+        
+    {}  // namespace nwapi
     
     
-    {  RenderThread::Get()->Send(new ShellViewHostMsg_Allocate_Object(
-      routing_id,
-      object_id,
-      type,
-      *static_cast<base::DictionaryValue*>(value_option.get())));
-  return v8::Undefined(isolate);
+    {}  // namespace nwapi
+    
+    #include 'base/compiler_specific.h'
+#include 'content/nw/src/api/base/base.h'
+    
+    Menu::~Menu() {
+  Destroy();
 }
     
-       void ExecuteCommand(int command_id, int event_flags) override;
-    
-    bool NwShellShowItemInFolderFunction::RunNWSync(base::ListValue* response, std::string* error) {
-  nw::ObjectManager* manager = nw::ObjectManager::Get(browser_context());
-  manager->OnCallStaticMethod(render_frame_host(), 'Shell', 'ShowItemInFolder', *args_);
-  return true;
-}
-    
-    // customize random engine.
-void CustomGlobalRandomEngine::seed(CustomGlobalRandomEngine::result_type val) {
-  // ignore the seed
-}
-    
-      auto iter = p_fmat->RowIterator();
-  iter->BeforeFirst();
-  while (iter->Next()) {
-     auto batch = iter->Value();
-    #pragma omp parallel num_threads(nthread)
-    {
-      CHECK_EQ(nthread, omp_get_num_threads());
-      auto tid = static_cast<unsigned>(omp_get_thread_num());
-      unsigned begin = std::min(nstep * tid, ncol);
-      unsigned end = std::min(nstep * (tid + 1), ncol);
-      for (size_t i = 0; i < batch.Size(); ++i) { // NOLINT(*)
-        size_t ridx = batch.base_rowid + i;
-        SparsePage::Inst inst = batch[i];
-        for (bst_uint j = 0; j < inst.length; ++j) {
-          if (inst[j].index >= begin && inst[j].index < end) {
-            sketchs[inst[j].index].Push(inst[j].fvalue, info.GetWeight(ridx));
-          }
-        }
+        bool Write(ClipboardData& data) {
+      switch(data.type) {
+        case TYPE_TEXT:
+        return WriteText(data);
+        break;
+        case TYPE_HTML:
+        return WriteHTML(data);
+        break;
+        case TYPE_RTF:
+        return WriteRTF(data);
+        break;
+        case TYPE_PNG:
+        case TYPE_JPEG:
+        return WriteImage(data);
+        break;
+        case TYPE_NONE:
+        NOTREACHED();
+        return false;
       }
+      NOTREACHED();
+      return false;
     }
-  }
     
-    #include <xgboost/base.h>
-#include <xgboost/data.h>
-#include <dmlc/threadediter.h>
-#include <utility>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include '../common/common.h'
-#include './sparse_page_writer.h'
-    
-    SparsePageWriter::SparsePageWriter(
-    const std::vector<std::string>& name_shards,
-    const std::vector<std::string>& format_shards,
-    size_t extra_buffer_capacity)
-    : num_free_buffer_(extra_buffer_capacity + name_shards.size()),
-      clock_ptr_(0),
-      workers_(name_shards.size()),
-      qworkers_(name_shards.size()) {
-  CHECK_EQ(name_shards.size(), format_shards.size());
-  // start writer threads
-  for (size_t i = 0; i < name_shards.size(); ++i) {
-    std::string name_shard = name_shards[i];
-    std::string format_shard = format_shards[i];
-    auto* wqueue = &qworkers_[i];
-    workers_[i].reset(new std::thread(
-        [this, name_shard, format_shard, wqueue] () {
-          std::unique_ptr<dmlc::Stream> fo(
-              dmlc::Stream::Create(name_shard.c_str(), 'w'));
-          std::unique_ptr<SparsePageFormat> fmt(
-              SparsePageFormat::Create(format_shard));
-          fo->Write(format_shard);
-          std::shared_ptr<SparsePage> page;
-          while (wqueue->Pop(&page)) {
-            if (page == nullptr) break;
-            fmt->Write(*page, fo.get());
-            qrecycle_.Push(std::move(page));
-          }
-          fo.reset(nullptr);
-          LOG(CONSOLE) << 'SparsePage::Writer Finished writing to ' << name_shard;
-        }));
-  }
+    void leveldb_compact_range(
+    leveldb_t* db,
+    const char* start_key, size_t start_key_len,
+    const char* limit_key, size_t limit_key_len) {
+  Slice a, b;
+  db->rep->CompactRange(
+      // Pass NULL Slice if corresponding 'const char*' is NULL
+      (start_key ? (a = Slice(start_key, start_key_len), &a) : NULL),
+      (limit_key ? (b = Slice(limit_key, limit_key_len), &b) : NULL));
 }
     
-    std::vector<uint8_t> DecodeJpegToRGB(const JPEGData& jpg) {
-  if (jpg.components.size() == 1 ||
-      (jpg.components.size() == 3 &&
-       HasYCbCrColorSpace(jpg) && (jpg.Is420() || jpg.Is444()))) {
-    OutputImage img(jpg.width, jpg.height);
-    img.CopyFromJpegData(jpg);
-    return img.ToSRGB();
+    void InternalFilterPolicy::CreateFilter(const Slice* keys, int n,
+                                        std::string* dst) const {
+  // We rely on the fact that the code in table.cc does not mind us
+  // adjusting keys[].
+  Slice* mkey = const_cast<Slice*>(keys);
+  for (int i = 0; i < n; i++) {
+    mkey[i] = ExtractUserKey(keys[i]);
+    // TODO(sanjay): Suppress dups?
   }
-  return std::vector<uint8_t>();
+  user_policy_->CreateFilter(keys, n, dst);
 }
     
+      FindFileTest() : disjoint_sorted_files_(true) { }
     
-    {}  // namespace
+        // FLAGS_page_size is defaulted to 1024
+    if (FLAGS_page_size != 1024) {
+      char page_size[100];
+      snprintf(page_size, sizeof(page_size), 'PRAGMA page_size = %d',
+               FLAGS_page_size);
+      status = sqlite3_exec(db_, page_size, NULL, NULL, &err_msg);
+      ExecErrorCheck(status, err_msg);
+    }
     
-    #ifndef GUETZLI_JPEG_DATA_READER_H_
-#define GUETZLI_JPEG_DATA_READER_H_
-    
-    #include 'guetzli/quality.h'
-    
-      StatisticsJni::StatisticsJni(std::shared_ptr<Statistics> stats)
-      : StatisticsImpl(stats, false), m_ignore_histograms() {
-  }
-    
-      uint64_t GetID() const { return id_; }
-    
-     private:
-    // used for synchronisation in compare method
-    std::unique_ptr<port::Mutex> mtx_compare;
-    // used for synchronisation in findShortestSeparator method
-    std::unique_ptr<port::Mutex> mtx_findShortestSeparator;
-    std::unique_ptr<const char[]> m_name;
-    jmethodID m_jCompareMethodId;
-    jmethodID m_jFindShortestSeparatorMethodId;
-    jmethodID m_jFindShortSuccessorMethodId;
-    
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
+    	ClassDB::bind_method(D_METHOD('poll'), &StreamPeerSSL::poll);
+	ClassDB::bind_method(D_METHOD('accept_stream', 'stream'), &StreamPeerSSL::accept_stream);
+	ClassDB::bind_method(D_METHOD('connect_to_stream', 'stream', 'validate_certs', 'for_hostname'), &StreamPeerSSL::connect_to_stream, DEFVAL(false), DEFVAL(String()));
+	ClassDB::bind_method(D_METHOD('get_status'), &StreamPeerSSL::get_status);
+	ClassDB::bind_method(D_METHOD('disconnect_from_stream'), &StreamPeerSSL::disconnect_from_stream);
     
     
-/*
- * CoreServiceBase.h
- *
- *  Created on: 2013-6-20
- *      Author: yerungui
+    {	return false;
+}
+    
+    	virtual bool needsCollision(btBroadphaseProxy *proxy0) const;
+    
+    
+    
+    /*Note that we do not provide a macro for abs(), because it is provided as a
+   library function, which we assume is translated into an intrinsic to avoid
+   the function call overhead and then implemented in the smartest way for the
+   target platform.
+  With modern gcc (4.x), this is true: it uses cmov instructions if the
+   architecture supports it and branchless bit-twiddling if it does not (the
+   speed difference between the two approaches is not measurable).
+  Interestingly, the bit-twiddling method was patented in 2000 (US 6,073,150)
+   by Sun Microsystems, despite prior art dating back to at least 1996:
+   http://web.archive.org/web/19961201174141/www.x86.org/ftp/articles/pentopt/PENTOPT.TXT
+  On gcc 3.x, however, our assumption is not true, as abs() is translated to a
+   conditional jump, which is horrible on deeply piplined architectures (e.g.,
+   all consumer architectures for the past decade or more).
+  Also be warned that -C*abs(x) where C is a constant is mis-optimized as
+   abs(C*x) on every gcc release before 4.2.3.
+  See bug http://gcc.gnu.org/bugzilla/show_bug.cgi?id=34130 */
+    
+    /*============================================================================
+**      On Intel Pentium processors (especially PIII and probably P4), converting
+**      from float to int is very slow. To meet the C specs, the code produced by
+**      most C compilers targeting Pentium needs to change the FPU rounding mode
+**      before the float to int conversion is performed.
+**
+**      Changing the FPU rounding mode causes the FPU pipeline to be flushed. It
+**      is this flushing of the pipeline which is so slow.
+**
+**      Fortunately the ISO C99 specifications define the functions lrint, lrintf,
+**      llrint and llrintf which fix this problem as a side effect.
+**
+**      On Unix-like systems, the configure process should have detected the
+**      presence of these functions. If they weren't found we have to replace them
+**      here with a standard C cast.
+*/
+    
+    /**
+ * Install the terminate / unexpected handlers to dump exceptions.
  */
+void installHandlers();
     
-    #endif
-
+    // Some queue implementations (for example, LifoSemMPMCQueue or
+// PriorityLifoSemMPMCQueue) support both blocking (BLOCK) and
+// non-blocking (THROW) behaviors.
+enum class QueueBehaviorIfFull { THROW, BLOCK };
     
-    ScopeJEnv::ScopeJEnv(JavaVM* jvm, jint _capacity)
-    : vm_(jvm), env_(NULL), we_attach_(false), status_(0) {
-    ASSERT(jvm);
-    do {
-        env_ = (JNIEnv*)pthread_getspecific(g_env_key);
-        
-        if (NULL != env_) {
-            break;
-        }
-        
-        status_ = vm_->GetEnv((void**) &env_, JNI_VERSION_1_6);
+      Atom<Node*> head_;
+    
+      ~HazptrWideCAS() {
+    delete node_.load(std::memory_order_relaxed);
+  }
+    
+    
+    {  return instance_ptr_;
+}
+    
+    #include <fcntl.h>
+    
+    DEFINE_FIND_STATIC_METHOD(KXlog_logWrite2, KXlog, 'logWrite2', '(ILjava/lang/String;Ljava/lang/String;Ljava/lang/String;IIJJLjava/lang/String;)V')
+JNIEXPORT void JNICALL Java_com_tencent_mars_xlog_Xlog_logWrite2
+  (JNIEnv *env, jclass, int _level, jstring _tag, jstring _filename,
+		  jstring _funcname, jint _line, jint _pid, jlong _tid, jlong _maintid, jstring _log) {
     }
-    }
     
+        if (st.url_length + st.head_length > _rawlen) return LONGLINKPACK_CONTINUE_HEAD;
     
+    // Licensed under the MIT License (the 'License'); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+// http://opensource.org/licenses/MIT
