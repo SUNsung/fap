79ac74e90783ea11d1952eed25784cb14462685f
SUNsung <weatherius@gmail.com>
2018-07-03 16:08:37 +0300

    Rewrite [04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py] File in: 2018-07-03 16:08:37

--- pyton/04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py
+++ pyton/04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py
@@ -1,62 +1,214 @@
 
         
         
-@pytest.mark.functional
-def test_without_confirmation(proc, TIMEOUT):
-    without_confirmation(proc, TIMEOUT)
+def run_training_step(sess, trainer, train_corpus, batch_size):
+  '''Runs a single iteration of train_op on a randomly sampled batch.'''
+  batch = random.sample(train_corpus, batch_size)
+  sess.run(trainer['run'], feed_dict={trainer['input_batch']: batch})
     
-    
-class Scientist(AbstractExpert):
-    
-        def test_sequential_execution(self):
-        self.command_stack[0].execute()
-        output_after_first_execution = os.listdir(self.test_dir)
-        self.assertEqual(output_after_first_execution[0], 'bar.txt')
-        self.command_stack[1].execute()
-        output_after_second_execution = os.listdir(self.test_dir)
-        self.assertEqual(output_after_second_execution[0], 'baz.txt')
-    
-    ### OUTPUT ###
-# Jack move 5m then stop
+    # This should include just about everything, suitable for Jupyter notebooks or
+# building a pip package.
+py_library(
+    name = 'all_in_one_components',
+    deps = [
+        ':components',
+        '//dragnn/python:components',
+        '//dragnn/python:dragnn_ops',
+        '//dragnn/python:evaluation',
+        '//dragnn/python:lexicon',
+        '//dragnn/python:render_parse_tree_graphviz',
+        '//dragnn/python:render_spec_with_graphviz',
+        '//dragnn/python:spec_builder',
+        '//dragnn/python:trainer_lib',
+        '//dragnn/python:visualization',
+        '//syntaxnet:task_spec_pb2_py',
+    ],
+)
 
     
-    if __name__ == '__main__':
-    print('Before subclassing: ')
-    for k in RegistryHolder.REGISTRY:
-        print(k)
+      Args:
+    master_spec_path: Path to a proto-text master spec.
+    params_path: Path to the parameters file to export.
+    resource_path: Path to resources in the master spec.
+    export_path: Path to export the SavedModel to.
+    export_moving_averages: Whether to export the moving average parameters.
+  '''
+  # Old CoNLL checkpoints did not need a known-word-map. Create a temporary if
+  # that file is missing.
+  if not tf.gfile.Exists(os.path.join(resource_path, 'known-word-map')):
+    with tf.gfile.FastGFile(os.path.join(resource_path, 'known-word-map'),
+                            'w') as out_file:
+      out_file.write('This file intentionally left blank.')
     
-        def test_cat_eng_localization(self):
-        self.assertEqual(self.e.get('cat'), 'cat')
+        if FLAGS.output_file:
+      with gfile.GFile(FLAGS.output_file, 'w') as f:
+        for serialized_sentence in processed:
+          sentence = sentence_pb2.Sentence()
+          sentence.ParseFromString(serialized_sentence)
+          f.write(text_format.MessageToString(sentence) + '\n\n')
     
-        def test_3rd_am_station_after_scan(self):
+    flags.DEFINE_integer('num_epochs', 10, 'Number of epochs to train for.')
+flags.DEFINE_integer('batch_size', 4, 'Batch size.')
+flags.DEFINE_integer('report_every', 500,
+                     'Report cost and training accuracy every this many steps.')
+flags.DEFINE_string('hyperparams',
+                    'decay_steps:32000 dropout_rate:0.8 gradient_clip_norm:1 '
+                    'learning_method:'momentum' learning_rate:0.1 seed:1 '
+                    'momentum:0.95 use_moving_average:true',
+                    'Hyperparameters of the model to train, either in ProtoBuf'
+                    'text format or base64-encoded ProtoBuf text format.')
+    
+    import pytest
+from requests.compat import urljoin
+    
+    Provides utility functions that are consumed internally by Requests
+which depend on extremely few external helpers (such as compat)
+'''
+    
+    from requests.help import info
+    
+        def test_response_context_manager(self, httpbin):
+        with requests.get(httpbin('stream/4'), stream=True) as response:
+            assert isinstance(response, requests.Response)
+    
+    
+@pytest.mark.parametrize(
+    'url, expected', (
+            ('http://192.168.0.1:5000/', True),
+            ('http://192.168.0.1/', True),
+            ('http://172.16.1.1/', True),
+            ('http://172.16.1.1:5000/', True),
+            ('http://localhost.localdomain:5000/v1.0/', True),
+            ('http://google.com:6000/', True),
+            ('http://172.16.1.12/', False),
+            ('http://172.16.1.12:5000/', False),
+            ('http://google.com:5000/v1.0/', False),
+    ))
+def test_should_bypass_proxies(url, expected, monkeypatch):
+    '''Tests for function should_bypass_proxies to check if proxy
+    can be bypassed or not
+    '''
+    monkeypatch.setenv('no_proxy', '192.168.0.0/24,127.0.0.1,localhost.localdomain,172.16.1.1, google.com:6000')
+    monkeypatch.setenv('NO_PROXY', '192.168.0.0/24,127.0.0.1,localhost.localdomain,172.16.1.1, google.com:6000')
+    assert should_bypass_proxies(url, no_proxy=None) == expected
+    
+            # Verify we haven't overwritten the location with our previous fragment.
+        assert r.history[1].request.url == 'http://{0}:{1}/get#relevant-section'.format(host, port)
+        # Verify previous fragment is used and not the original.
+        assert r.url == 'http://{0}:{1}/final-url/#relevant-section'.format(host, port)
+    
+        def build_digest_header(self, method, url):
+        '''
+        :rtype: str
+        '''
+    
+    # ---------
+# Specifics
+# ---------
+    
+            .. seealso:: itervalues() and iteritems().
+        '''
+        for cookie in iter(self):
+            yield cookie.name
+    
+    
+def _check_cryptography(cryptography_version):
+    # cryptography < 1.3.4
+    try:
+        cryptography_version = list(map(int, cryptography_version.split('.')))
+    except ValueError:
+        return
+    
+        def get(self, key):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                return item.value
+        raise KeyError('Key not found')
+    
+        def __init__(self):
+        self.head = None
+        self.tail = None
+    
+            When updating an entry, updates its position to the front of the LRU list.
+        If the entry is new and the cache is at capacity, removes the oldest entry
+        before the new entry is added.
+        '''
+        node = self.lookup.get(query)
+        if node is not None:
+            # Key exists in cache, update the value
+            node.results = results
+            self.linked_list.move_to_front(node)
+        else:
+            # Key does not exist in cache
+            if self.size == self.MAX_SIZE:
+                # Remove the oldest entry from the linked list and lookup
+                self.lookup.pop(self.linked_list.tail.query, None)
+                self.linked_list.remove_from_tail()
+            else:
+                self.size += 1
+            # Add the new key and value
+            new_node = Node(results)
+            self.linked_list.append_to_front(new_node)
+            self.lookup[query] = new_node
+
+    
+    
+class AddRequest(object):
+    
+        def test_2nd_am_station_after_scan(self):
         self.radio.scan()
         station = self.radio.state.stations[self.radio.state.pos]
-        expected_station = '1510'
+        expected_station = '1380'
         self.assertEqual(station, expected_station)
     
-        def test_bunch_launch(self):
-        self.runner.runAll()
-        output = self.out.getvalue().strip()
-        self.assertEqual(output, str(self.average_result_tc1 + '\n\n' +
-                         self.average_result_tc2 + '\n\n' +
-                         self.average_result_tc3))
-
+            # simple test to validate param value
+        if param in self._static_method_choices.keys():
+            self.param = param
+        else:
+            raise ValueError('Invalid Value for Param: {0}'.format(param))
     
-        def get_current_time_as_html_fragment(self, time_provider):
-        current_time = time_provider.now()
-        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
-        return current_time_as_html_fragment
+    print()
     
-            # dictionary that will be used to determine which static method is
-        # to be executed but that will be also used to store possible param
-        # value
-        self._static_method_choices = {'param_value_1': self._static_method_1,
-                                       'param_value_2': self._static_method_2}
     
-        def __init__(self, src, dest):
-        self.src = src
-        self.dest = dest
+if __name__ == '__main__':
+    reporter = Reporter()
+    db = DB()
+    tm = TestManager()
+    tm.setReporter(reporter)
+    tm.setDB(db)
+    reporter.setTM(tm)
+    db.setTM(tm)
+    # For simplification we are looping on the same test.
+    # Practically, it could be about various unique test classes and their
+    # objects
+    for i in range(3):
+        tc = TC()
+        tc.setTM(tm)
+        tm.setTC(tc)
+        tc.setup()
+        tc.execute()
+        tc.tearDown()
     
-    *TL;DR80
-Provides recombination business logic by chaining together using boolean logic.
-'''
+            Calling `.wait` once the flag is set will not block.
+        '''
+        if not self._value:
+            self._value = True
+    
+        def test_malformed_first_line_log(self):
+        with ExpectLog(gen_log, '.*Malformed HTTP request line'):
+            self.stream.write(b'asdf\r\n\r\n')
+            # TODO: need an async version of ExpectLog so we don't need
+            # hard-coded timeouts here.
+            self.io_loop.add_timeout(datetime.timedelta(seconds=0.05),
+                                     self.stop)
+            self.wait()
+    
+        @gen.coroutine
+    def server_send_line(self, line):
+        self.server_stream.write(line)
+        recv_line = yield self.client_stream.read_until(b'\r\n')
+        self.assertEqual(line, recv_line)
+    
+        def test_wait_timeout(self):
+        time = self.io_loop.time
