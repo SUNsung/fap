efd5b50f262853bbf1c5b519dd4d04e280a5ff03
SUNsung <weatherius@gmail.com>
2018-07-04 02:01:30 +0300

    Rewrite [12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb] File in: 2018-07-04 02:01:29

--- ruby/12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb
+++ ruby/12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb
@@ -1,155 +1,107 @@
 
         
-              # Returns constant of subscription adapter specified in config/cable.yml.
-      # If the adapter cannot be found, this will default to the Redis adapter.
-      # Also makes sure proper dependencies are required.
-      def pubsub_adapter
-        adapter = (cable.fetch('adapter') { 'redis' })
-    
-      class MyController < ActionController::Metal
-    use BlockMiddleware do |config|
-      config.configurable_message = 'Configured by block.'
-    end
-    use MyMiddleware
-    middleware.insert_before MyMiddleware, ExclaimerMiddleware
-    
-            def enqueue_delivery(delivery_method, options = {})
-          if processed?
-            super
-          else
-            args = @mailer_class.name, @action.to_s, delivery_method.to_s, @params, *@args
-            ActionMailer::Parameterized::DeliveryJob.set(options).perform_later(*args)
-          end
-        end
-    end
-    
-    class DefaultsDeliveryMethodsTest < ActiveSupport::TestCase
-  test 'default smtp settings' do
-    settings = { address:              'localhost',
-                 port:                 25,
-                 domain:               'localhost.localdomain',
-                 user_name:            nil,
-                 password:             nil,
-                 authentication:       nil,
-                 enable_starttls_auto: true }
-    assert_equal settings, ActionMailer::Base.smtp_settings
+          def test_capture2e_stdin_data_io
+    IO.pipe {|r, w|
+      w.write 'i'
+      w.close
+      oe, s = Open3.capture2e(RUBY, '-e', 'i=STDIN.read; print i+'o'; STDOUT.flush; STDERR.print i+'e'', :stdin_data=>r)
+      assert_equal('ioie', oe)
+      assert(s.success?)
+    }
   end
     
-    class ActionMailerI18nWithControllerTest < ActionDispatch::IntegrationTest
-  Routes = ActionDispatch::Routing::RouteSet.new
-  Routes.draw do
-    ActiveSupport::Deprecation.silence do
-      get ':controller(/:action(/:id))'
+    def grammar(f, out)
+  while line = f.gets
+    case line
+    when %r</\*% *ripper(?:\[(.*?)\])?: *(.*?) *%\*/>
+      out << DSL.new($2, ($1 || '').split(',')).generate << $/
+    when %r</\*%%%\*/>
+      out << '#if 0' << $/
+    when %r</\*%>
+      out << '#endif' << $/
+    when %r<%\*/>
+      out << $/
+    when /\A%%/
+      out << '%%' << $/
+      return
+    else
+      out << line
     end
   end
+end
     
-      def revoke_moderation!
-    set_permission('moderator', false)
+      it 'returns result of then-body evaluation if expression is true' do
+    if true
+      123
+    end.should == 123
   end
     
-        find_union(segments, Project).includes(:namespace).order_id_desc
+      def assert_constant_reassignment_toplevel(preset, op, expected, err = [], bug = '[Bug #5449]')
+    [
+     ['p ', ''],                # no-pop
+     ['', 'p ::Bar'],           # pop
+    ].each do |p1, p2|
+      src = <<-EOM.gsub(/^\s*\n/, '')
+      #{'Bar = ' + preset if preset}
+      class Foo
+        #{p1}::Bar #{op}= 42
+        #{p2}
+      end
+      EOM
+      msg = '\# #{bug}\n#{src}'
+      assert_valid_syntax(src, caller_locations(1, 1)[0].path, msg)
+      assert_in_out_err([], src, expected, err, msg)
+    end
   end
     
-          html_filters.push 'd3/clean_html', 'd3/entries_v3', 'title'
+        # test4; recursion
+    set2 = Set[]
+    set1 = Set[1, set2]
+    set2.add(set1)
     
-        self.name = 'PHP'
-    self.type = 'php'
-    self.release = '7.2.5'
-    self.base_url = 'https://secure.php.net/manual/en/'
-    self.root_path = 'index.html'
-    self.initial_paths = %w(
-      funcref.html
-      langref.html
-      refs.database.html
-      set.mysqlinfo.html
-      language.control-structures.html
-      reference.pcre.pattern.syntax.html
-      reserved.exceptions.html
-      reserved.interfaces.html
-      reserved.variables.html)
+      Ruby = Struct.new(:version, :platform)
     
-        options[:attribution] = <<-HTML
-      &copy; 2010 The Rust Project Developers<br>
-      Licensed under the Apache License, Version 2.0 or the MIT license, at your option.
-    HTML
+        def replace_asset_url(rule, type)
+      replace_all rule, /url\((.*?)\)/, 'url(if($bootstrap-sass-asset-helper, twbs-#{type}-path(\\1), \\1))'
+    end
     
-        options[:attribution] = <<-HTML
-      &copy; 2010&ndash;2018 Christian Johansen<br>
-      Licensed under the BSD License.
-    HTML
+        def log_processed(name)
+      puts green '    #{name}'
+    end
     
-        def as_json
-      { name: name, path: path, type: type }
+      # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+    
+      def setup
+    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
+    success = Dir.chdir DUMMY_PATH do
+      silence_stdout_if !ENV['VERBOSE'] do
+        system 'node', 'manifest.js', tmp_dir
+      end
+    end
+    assert success, 'Node.js Mincer compilation failed'
+    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
+    css_name = manifest['assets']['application.css']
+    @css = File.read('#{tmp_dir}/#{css_name}')
+  end
+end
+
+    
+          # If this importer is based on files on the local filesystem This method
+      # should return true if the file, when changed, should trigger a
+      # recompile.
+      #
+      # It is acceptable for non-sass files to be watched and trigger a recompile.
+      #
+      # @param filename [String] The absolute filename for a file that has changed.
+      # @return [Boolean] When the file changed should cause a recompile.
+      def watched_file?(filename)
+        false
+      end
     end
   end
 end
 
     
-    module Docs
-  class PageDb
-    attr_reader :pages
-    
-        def self.join(*args)
-      PARSER.join(*args)
+          concat(before_string_or_interp, concat(mid_string_or_interp, after_string_or_interp))
     end
-    
-        existence_maps.each do |group|
-      existing_one = group.key(true)
-    
-      def icons
-    [
-      {
-        src: '/android-chrome-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-    ]
-  end
-    
-    def load_apps
-  out, err, status = Open3.capture3('/usr/bin/osascript', '-e', 'tell application 'System Events' to get (name, bundle identifier, unix id) of every process')
-  if status.exitstatus > 0
-    puts err
-    exit status.exitstatus
-  end
-  out = out.split(', ')
-  one_third   = out.length / 3
-  @app_names  = out.shift(one_third)
-  @bundle_ids = out.shift(one_third)
-  @unix_ids   = out.shift(one_third)
-end
-    
-          spec['version'] = Bootstrap::VERSION
-    
-          opts.on('--unix-newlines', 'Use Unix-style newlines in written files.',
-                                 ('Always true on Unix.' unless Sass::Util.windows?)) do
-        @options[:unix_newlines] = true if Sass::Util.windows?
-      end
-    end
-    
-          module ClassMethods
-        def inherited(subclass)
-          subclass.log_levels = subclass.superclass.log_levels.dup
-        end
-    
-          res << val.to_s(:quote => :none)
-      res << ' ' if @after && @whitespace_after
-      res << @after.perform(environment).to_s if @after
-      str = Sass::Script::Value::String.new(
-        res, :identifier,
-        (to_quoted_equivalent.to_sass if deprecation == :potential))
-      str.source_range = source_range
-      opts(str)
-    end
-    
-      # Read and eval a .rake file in such a way that `self` within the .rake file
-  # refers to this plugin instance. This gives the tasks in the file access to
-  # helper methods defined by the plugin.
-  def eval_rakefile(path)
-    contents = IO.read(path)
-    instance_eval(contents, path, 1)
-  end
-    
-      desc 'Reverted'
-  task :reverted do
-  end
