858e3220237d518a14e480d9d17932827d4d7f7a
SUNsung <weatherius@gmail.com>
2018-07-03 19:15:24 +0300

    Rewrite [35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py] File in: 2018-07-03 19:15:23

--- pyton/35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py
+++ pyton/35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py
@@ -1,69 +1,49 @@
 
         
-        atom_template = atom_template.replace('@TIMESTAMP@', now_iso)
+        cc_library(
+    name = 'beam',
+    hdrs = ['beam.h'],
+    deps = [
+        '//dragnn/core/interfaces:cloneable_transition_state',
+        '//dragnn/core/interfaces:transition_state',
+        '//syntaxnet:base',
+    ],
+)
+    
+      def testFailsOnFixedFeature(self):
+    component_spec = spec_pb2.ComponentSpec()
+    text_format.Parse('''
+        name: 'annotate'
+        network_unit {
+          registered_name: 'IdentityNetwork'
+        }
+        fixed_feature {
+          name: 'fixed' embedding_dim: 32 size: 1
+        }
+        ''', component_spec)
+    with tf.Graph().as_default():
+      comp = bulk_component.BulkAnnotatorComponentBuilder(
+          self.master, component_spec)
     
     
-md5 = lambda s: hashlib.md5(s.encode('utf-8')).hexdigest()
-    
-            self.port = random.randint(20000, 30000)
-        self.server_process = subprocess.Popen([
-            'srelay', '-f', '-i', '127.0.0.1:%d' % self.port],
-            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
-    IGNORED_DIRS = [
-    '.git',
-    '.tox',
-]
+@registry.RegisteredClass
+class ComponentBuilderBase(object):
+  '''Utility to build a single Component in a DRAGNN stack of models.
     
     
-def gen_extractor_classes():
-    ''' Return a list of supported extractors.
-    The order does matter; the first extractor matched is the one handling the URL.
-    '''
-    return _ALL_CLASSES
+def main(unused_argv):
+  # Run the exporter.
+  export(FLAGS.master_spec, FLAGS.params_path, FLAGS.export_path,
+         FLAGS.export_moving_averages, FLAGS.build_runtime_graph)
+  tf.logging.info('Export complete.')
     
-    
-class C56IE(InfoExtractor):
-    _VALID_URL = r'https?://(?:(?:www|player)\.)?56\.com/(?:.+?/)?(?:v_|(?:play_album.+-))(?P<textid>.+?)\.(?:html|swf)'
-    IE_NAME = '56.com'
-    _TESTS = [{
-        'url': 'http://www.56.com/u39/v_OTM0NDA3MTY.html',
-        'md5': 'e59995ac63d0457783ea05f93f12a866',
-        'info_dict': {
-            'id': '93440716',
-            'ext': 'flv',
-            'title': '网事知多少 第32期：车怒',
-            'duration': 283.813,
-        },
-    }, {
-        'url': 'http://www.56.com/u47/v_MTM5NjQ5ODc2.html',
-        'md5': '',
-        'info_dict': {
-            'id': '82247482',
-            'title': '爱的诅咒之杜鹃花开',
-        },
-        'playlist_count': 7,
-        'add_ie': ['Sohu'],
-    }]
-    
-            # Video URL construction algorithm is reverse-engineered from cwhplayer.swf
-        rtmp_url = 'rtmp://camwithher.tv/clipshare/%s' % (
-            ('mp4:%s.mp4' % flv_id) if int(flv_id) > 2010 else flv_id)
-    
-        def _real_extract(self, url):
-        display_id = self._match_id(url)
-    
-    USERNAME = 'user'
-PASSWORD = 'password'
-# Basic auth encoded `USERNAME` and `PASSWORD`
-# noinspection SpellCheckingInspection
-BASIC_AUTH_HEADER_VALUE = 'Basic dXNlcjpwYXNzd29yZA=='
-BASIC_AUTH_URL = '/basic-auth/{0}/{1}'.format(USERNAME, PASSWORD)
-AUTH_OK = {'authenticated': True, 'user': USERNAME}
-    
-    https://github.com/Homebrew/homebrew-core/blob/master/Formula/httpie.rb
-    
-            return '\r\n'.join(headers)
+    # The second sentence has different length, to test the effect of
+# mixed-length batches.
+_DUMMY_GOLD_SENTENCE_2 = '''
+token {
+  word: 'sentence' start: 0 end: 7 tag: 'NN' category: 'NOUN' label: 'ROOT'
+}
+'''
     
         exc = ConnectionError('Connection aborted')
     exc.request = Request(method='GET', url='http://www.google.com')
@@ -75,121 +55,63 @@ AUTH_OK = {'authenticated': True, 'user': USERNAME}
         'Connection aborted while doing GET request to URL: '
         'http://www.google.com')
     
-    '''
-import sys
-import errno
-import platform
-    
-    
-class TransportPlugin(BasePlugin):
-    '''
-    
-    
-# noinspection PyAbstractClass
-class BuiltinAuthPlugin(AuthPlugin):
-    
-        def _get_path(self):
-        '''Return the config file path without side-effects.'''
-        raise NotImplementedError()
-    
-        >>> humanize_bytes(1)
-    '1 B'
-    >>> humanize_bytes(1024, precision=1)
-    '1.0 kB'
-    >>> humanize_bytes(1024 * 123, precision=1)
-    '123.0 kB'
-    >>> humanize_bytes(1024 * 12342, precision=1)
-    '12.1 MB'
-    >>> humanize_bytes(1024 * 12342, precision=2)
-    '12.05 MB'
-    >>> humanize_bytes(1024 * 1234, precision=2)
-    '1.21 MB'
-    >>> humanize_bytes(1024 * 1234 * 1111, precision=2)
-    '1.31 GB'
-    >>> humanize_bytes(1024 * 1234 * 1111, precision=1)
-    '1.3 GB'
-    
-    import os, json, imp
-here = os.path.abspath(os.path.dirname(__file__))
-proj_info = json.loads(open(os.path.join(here, PROJ_METADATA), encoding='utf-8').read())
-try:
-    README = open(os.path.join(here, 'README.rst'), encoding='utf-8').read()
-except:
-    README = ''
-CHANGELOG = open(os.path.join(here, 'CHANGELOG.rst'), encoding='utf-8').read()
-VERSION = imp.load_source('version', os.path.join(here, 'src/%s/version.py' % PACKAGE_NAME)).__version__
-    
-            for i in html_json['sources']:
-            if 'src' in i:  #to avoid KeyError
-                if i['src'].startswith('https'):
-                    link_list.append((str(i['height']), i['src']))
-    
-    def kuwo_download_by_rid(rid, output_dir = '.', merge = True, info_only = False):
-    html=get_content('http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_%s'%rid)
-    title=match1(html,r'<name>(.*)</name>')
-    #to get title
-    #format =aac|mp3 ->to get aac format=mp3 ->to get mp3
-    url=get_content('http://antiserver.kuwo.cn/anti.s?format=mp3&rid=MUSIC_%s&type=convert_url&response=url'%rid)
-    songtype, ext, size = url_info(url)
-    print_info(site_info, title, songtype, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir)
-    
-            if self.closed:
-            raise ValueError('I/O operation on closed file')
-        if not self.seekable:
-            raise OSError('cannot seek')
-        if whence == 1:
-            pos = pos + self.size_read
-        elif whence == 2:
-            pos = pos + self.chunksize
-        if pos < 0 or pos > self.chunksize:
-            raise RuntimeError
-        self.file.seek(self.offset + pos, 0)
-        self.size_read = pos
-    
-    '''Safely evaluate Python string literals without using eval().'''
-    
-    def get_colordb(file, filetype=None):
-    colordb = None
-    fp = open(file)
-    try:
-        line = fp.readline()
-        if not line:
-            return None
-        # try to determine the type of RGB file it is
-        if filetype is None:
-            filetypes = FILETYPES
-        else:
-            filetypes = [filetype]
-        for typere, class_ in filetypes:
-            mo = typere.search(line)
-            if mo:
-                break
-        else:
-            # no matching type
-            return None
-        # we know the type and the class to grok the type, so suck it in
-        colordb = class_(fp)
-    finally:
-        fp.close()
-    # save a global copy
-    global DEFAULT_DB
-    DEFAULT_DB = colordb
-    return colordb
-    
-        def test_oldargs0_1(self):
-        self.assertRaises(TypeError, {}.keys, 0)
-    
-    
-class DOMInputSource(object):
-    __slots__ = ('byteStream', 'characterStream', 'stringData',
-                 'encoding', 'publicId', 'systemId', 'baseURI')
-    
-    T_IV2 = dataclasses.InitVar[int]
-T_IV3 = dataclasses.InitVar
-    
-        return True
+            self.output.write(SUMMARY.format(
+            downloaded=humanize_bytes(actually_downloaded),
+            total=(self.status.total_size and
+                   humanize_bytes(self.status.total_size)),
+            speed=humanize_bytes(speed),
+            time=time_taken,
+        ))
+        self.output.flush()
 
     
-            self.assertIsNotNone(result)
+                if 'Content-Type' not in self.args.headers:
+                content_type = get_content_type(fn)
+                if content_type:
+                    self.args.headers['Content-Type'] = content_type
+    
+            X = np.random.randn(n_samples, n_features)
+        Y = np.random.randn(n_samples)
+    
+        ###########################################################################
+    # Perform benchmark
+    ###########################################################################
+    time_fit = collections.defaultdict(list)
+    time_transform = collections.defaultdict(list)
+    
+    plt.figure(2)
+    
+    We add observation noise to these waveforms. We generate very sparse
+noise: only 6% of the time points contain noise. As a result, the
+l1 norm of this noise (ie 'cityblock' distance) is much smaller than it's
+l2 norm ('euclidean' distance). This can be seen on the inter-class
+distance matrices: the values on the diagonal, that characterize the
+spread of the class, are much bigger for the Euclidean distance than for
+the cityblock distance.
+    
+    	type, ext, size = url_info(url)
+	print_info(site_info, title, type, size)
+	
+	if not info_only:
+		download_urls([url], title, ext, size, output_dir, merge = merge)
+    
+        type_, ext, size = url_info(stream_url)
+    print_info(site_info, title, type_, size)
+    if not info_only:
+        download_url_ffmpeg(url=stream_url, title=title, ext= 'mp4', output_dir=output_dir)
+    
+        def _send_diagnostics_request(self):
+        return 'send diagnostic request'
+    
+        def __init__(self, name, msg_center):
+        self.name = name
+        self.provider = msg_center
+    
+        def __new__(cls, name, bases, attrs):
+        new_cls = type.__new__(cls, name, bases, attrs)
+        '''
+            Here the name of the class is used as key but it could be any class
+            parameter.
+        '''
+        cls.REGISTRY[new_cls.__name__] = new_cls
+        return new_cls
