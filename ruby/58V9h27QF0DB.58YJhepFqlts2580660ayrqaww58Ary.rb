4d630c30a1ae5828a1fa699d91ecc90c2ecbc9d6
SUNsung <weatherius@gmail.com>
2018-07-04 13:40:58 +0300

    Rewrite [58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb] File in: 2018-07-04 13:40:57

--- ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
+++ ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
@@ -1,23 +1,92 @@
 
         
-            def log_processed(name)
-      puts green '    #{name}'
-    end
+          fd.close
     
-      #forward some requests to status message, because a poll is just attached to a status message and is not sharable itself
-  delegate :author_id, :diaspora_handle, :public?, :subscribers, to: :status_message
+            when :err
+          case s[:last]
+            when :pass
+              # Oops got a -ERR after a pass so its crap ignore the pass
+              # But report it, might be helpfull for guessing :-)
     
-          interp = node_or_interp
-      after_string_or_interp =
-        if interp.after
-          to_string_interpolation(interp.after)
-        else
-          string_literal('')
-        end
-      if interp.after && interp.whitespace_after
-        after_string_or_interp = concat(string_literal(' '), after_string_or_interp)
-      end
+    ip   = ARGV.shift() || exit
+port = ARGV.shift() || 31337
     
-      def api_error?(error)
-    error.is_a?(LogStash::Api::ApiError)
+      def participation_answer(user)
+    poll_participations.find_by(author_id: user.person.id)
   end
+    
+          respond_with do |format|
+        format.html { redirect_to admin_pods_path }
+        format.json { render json: PodPresenter.new(pod).as_json }
+      end
+    end
+  end
+end
+
+    
+        unless user
+      EmailInviter.new(email, inviter).send!
+      flash[:notice] = 'invitation sent to #{email}'
+    else
+      flash[:notice]= 'error sending invite to #{email}'
+    end
+    redirect_to user_search_path, :notice => flash[:notice]
+  end
+    
+            # This is a convenient way to check whether the variable is used
+        # in its entire variable lifetime.
+        # For more precise usage check, refer Assignment#used?.
+        #
+        # Once the variable is captured by a block, we have no idea
+        # when, where and how many times the block would be invoked
+        # and it means we cannot track the usage of the variable.
+        # So we consider it's used to suppress false positive offenses.
+        def used?
+          @captured_by_block || referenced?
+        end
+    
+    module RuboCop
+  module Cop
+    module Performance
+      # This cop identifies the use of `Regexp#match` or `String#match`, which
+      # returns `#<MatchData>`/`nil`. The return value of `=~` is an integral
+      # index/`nil` and is more performant.
+      #
+      # @example
+      #   # bad
+      #   do_something if str.match(/regex/)
+      #   while regex.match('str')
+      #     do_something
+      #   end
+      #
+      #   # good
+      #   method(str =~ /regex/)
+      #   return value unless regex =~ 'str'
+      class RedundantMatch < Cop
+        MSG = 'Use `=~` in places where the `MatchData` returned by ' \
+              '`#match` will not be used.'.freeze
+    
+    module RuboCop
+  module Cop
+    module Lint
+      # Don't omit the accumulator when calling `next` in a `reduce` block.
+      #
+      # @example
+      #
+      #   # bad
+      #
+      #   result = (1..4).reduce(0) do |acc, i|
+      #     next if i.odd?
+      #     acc + i
+      #   end
+      #
+      # @example
+      #
+      #   # good
+      #
+      #   result = (1..4).reduce(0) do |acc, i|
+      #     next acc if i.odd?
+      #     acc + i
+      #   end
+      class NextWithoutAccumulator < Cop
+        MSG = 'Use `next` with an accumulator argument in a `reduce`.'.freeze
