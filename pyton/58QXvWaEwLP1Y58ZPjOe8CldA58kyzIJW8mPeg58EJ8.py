0f3b5263ddffaf7b86938935b6ffb00f6ef47a88
SUNsung <weatherius@gmail.com>
2018-07-04 14:46:55 +0300

    Rewrite [58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py] File in: 2018-07-04 14:46:54

--- pyton/58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py
+++ pyton/58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py
@@ -1,139 +1,157 @@
 
         
-            app = flask.Flask(__name__)
-    with pytest.raises(TypeError):
-        app.config.from_mapping(
-            {}, {}
+        
+def basic_auth(header=BASIC_AUTH_HEADER_VALUE):
+    
+    
+class Environment(object):
+    '''
+    Information about the execution context
+    (standard streams, config directory, etc).
+    
+    from httpie.plugins import plugin_manager
+from httpie.context import Environment
+    
+        def __init__(self, chunk_size=CHUNK_SIZE, **kwargs):
+        super(RawStream, self).__init__(**kwargs)
+        self.chunk_size = chunk_size
+    
+    
+@pytest.mark.parametrize('ssl_version', SSL_VERSION_ARG_MAPPING.keys())
+def test_ssl_version(httpbin_secure, ssl_version):
+    try:
+        r = http(
+            '--ssl', ssl_version,
+            httpbin_secure + '/get'
         )
-    
-        @app.teardown_request
-    def end_of_request(exception):
-        buffer.append(exception)
-    
-        for n_components in [i.astype(int) for i in
-                         np.linspace(data.shape[1] // 10,
-                                     data.shape[1], num=4)]:
-        all_times = defaultdict(list)
-        all_errors = defaultdict(list)
-        pca = PCA(n_components=n_components)
-        rpca = RandomizedPCA(n_components=n_components, random_state=1999)
-        results_dict = {k: benchmark(est, data) for k, est in [('pca', pca),
-                                                               ('rpca', rpca)]}
-    
-        # start time
-    tstart = datetime.now()
-    clf = DecisionTreeClassifier()
-    clf.fit(X, Y).predict(X)
-    delta = (datetime.now() - tstart)
-    # stop time
-    
-        # split the dataset in training and test set:
-    docs_train, docs_test, y_train, y_test = train_test_split(
-        dataset.data, dataset.target, test_size=0.25, random_state=None)
-    
-        # TASK: Build a grid search to find out whether unigrams or bigrams are
-    # more useful.
-    # Fit the pipeline on the training set using grid search for the parameters
-    parameters = {
-        'vect__ngram_range': [(1, 1), (1, 2)],
-    }
-    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1)
-    grid_search.fit(docs_train, y_train)
-    
-            controller.gamma = Tk.StringVar()
-        controller.gamma.set('0.01')
-        g = Tk.Frame(valbox)
-        Tk.Label(g, text='gamma:', anchor='e', width=7).pack(side=Tk.LEFT)
-        Tk.Entry(g, width=6, textvariable=controller.gamma).pack(side=Tk.LEFT)
-        g.pack()
-    
-    print('consensus score: {:.1f}'.format(score))
-    
-    calibrated_classifier = sig_clf.calibrated_classifiers_[0]
-prediction = np.vstack([calibrator.predict(this_p)
-                        for calibrator, this_p in
-                        zip(calibrated_classifier.calibrators_, p.T)]).T
-prediction /= prediction.sum(axis=1)[:, None]
-    
-    # 2 independent random clusterings with equal cluster number
-    
-    from concurrent.futures import (as_completed, ThreadPoolExecutor,
-                                ProcessPoolExecutor)
-    
-    if __name__ == '__main__':
-    main()
-
+        assert HTTP_OK in r
+    except SSLError as e:
+        if ssl_version == 'ssl3':
+            # pytest-httpbin doesn't support ssl3
+            assert 'SSLV3_ALERT_HANDSHAKE_FAILURE' in str(e)
+        else:
+            raise
     
     
-GENERIC_RESPONSE = {
-  'clang': {
-    'has_support': True,
-    'version': 'Clang version'
-  },
-  'completer': {
-    'items': [
-      {
-        'key': 'key',
-        'value': 'value'
-      }
-    ],
-    'name': 'Completer name',
-    'servers': [
-      {
-        'address': '127.0.0.1',
-        'executable': '/path/to/executable',
-        'extras': [
-          {
-            'key': 'key',
-            'value': 'value'
-          }
-        ],
-        'is_running': True,
-        'logfiles': [
-          '/path/to/stdout/logfile',
-          '/path/to/stderr/logfile'
-        ],
-        'name': 'Server name',
-        'pid': 12345,
-        'port': 1234
-      }
-    ]
-  },
-  'extra_conf': {
-    'is_loaded': False,
-    'path': '/path/to/extra/conf'
-  },
-  'python': {
-    'executable': '/path/to/python/interpreter',
-    'version': 'Python version'
-  }
-}
+def prepare_url(value):
+    # Issue #1483: Make sure the URL always has a trailing slash
+    httpbin_url = value.url.rstrip('/') + '/'
     
     
-def RegexSingleList_test():
-  opts = _JavaFilter( { 'regex' : [ 'taco' ] }  )
-  f = _CreateFilterForTypes( opts, [ 'java' ] )
+def to_native_string(string, encoding='ascii'):
+    '''Given a string object, regardless of type, returns a representation of
+    that string in the native string type, encoding and decoding where
+    necessary. This assumes ASCII unless told otherwise.
+    '''
+    if isinstance(string, builtin_str):
+        out = string
+    else:
+        if is_py2:
+            out = string.encode(encoding)
+        else:
+            out = string.decode(encoding)
+    
+            :param request: The :class:`PreparedRequest <PreparedRequest>` being sent.
+        :param stream: (optional) Whether to stream the request content.
+        :param timeout: (optional) How long to wait for the server to send
+            data before giving up, as a float, or a :ref:`(connect timeout,
+            read timeout) <timeouts>` tuple.
+        :type timeout: float or tuple
+        :param verify: (optional) Either a boolean, in which case it controls whether we verify
+            the server's TLS certificate, or a string, in which case it must be a path
+            to a CA bundle to use
+        :param cert: (optional) Any user-provided SSL certificate to be trusted.
+        :param proxies: (optional) The proxies dictionary to apply to the request.
+        '''
+        raise NotImplementedError
+    
+        def test_long_authinfo_in_url(self):
+        url = 'http://{0}:{1}@{2}:9000/path?query#frag'.format(
+            'E8A3BE87-9E3F-4620-8858-95478E385B5B',
+            'EA770032-DA4D-4D84-8CE9-29C6D910BF1E',
+            'exactly-------------sixty-----------three------------characters',
+        )
+        r = requests.Request('GET', url).prepare()
+        assert r.url == url
+    
+        def digest_response_handler(sock):
+        # Respond to GET with a 200 containing www-authenticate header.
+        request_content = consume_socket_content(sock, timeout=0.5)
+        assert request_content.startswith(b'GET / HTTP/1.1')
+        sock.send(text_200_chal)
+    
+        def __nonzero__(self):
+        '''Returns True if :attr:`status_code` is less than 400.
+    
+        def __init__(self, data=None, **kwargs):
+        self._store = OrderedDict()
+        if data is None:
+            data = {}
+        self.update(data, **kwargs)
+    
+        @test_util.patch_get_utility()
+    def test_deploy_certificate_restart_failure2(self, mock_get_utility):
+        installer = mock.MagicMock()
+        installer.restart.side_effect = errors.PluginError
+        installer.rollback_checkpoints.side_effect = errors.ReverterError
+        self.client.installer = installer
+    
+        @mock.patch('certbot_compatibility_test.validator.requests.get')
+    def test_redirect_wrong_status_code(self, mock_get_request):
+        mock_get_request.return_value = create_response(
+            201, {'location': 'https://test.com'})
+        self.assertFalse(self.validator.redirect('test.com'))
+    
+    # Language to be used for generating the HTML full-text search index.
+# Sphinx supports the following languages:
+#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
+#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
+#html_search_language = 'en'
+    
+            try:
+            signal.alarm(1)
+    
+        def __init__(self):
+        self._options = Options()
+    
+    T_CV2 = typing.ClassVar[int]
+T_CV3 = typing.ClassVar
+    
+    @dataclass
+class CV:
+    T_CV4 = ClassVar
+    cv0: ClassVar[int] = 20
+    cv1: ClassVar = 30
+    cv2: T_CV2
+    cv3: T_CV3
+    not_cv4: T_CV4  # When using string annotations, this field is not recognized as a ClassVar.
     
     
-def AdjustCandidateInsertionText_MultipleStrings_test():
-  with MockTextAfterCursor( 'bar' ):
-    eq_( [ { 'abbr': 'foobar', 'word': 'foo' },
-           { 'abbr': 'zobar', 'word': 'zo' },
-           { 'abbr': 'qbar', 'word': 'q' },
-           { 'abbr': 'bar', 'word': '' }, ],
-         base.AdjustCandidateInsertionText( [ 'foobar',
-                                              'zobar',
-                                              'qbar',
-                                              'bar' ] ) )
+def read_binary(package: Package, resource: Resource) -> bytes:
+    '''Return the binary contents of the resource.'''
+    resource = _normalize_path(resource)
+    package = _get_package(package)
+    with open_binary(package, resource) as fp:
+        return fp.read()
     
-    YCM_VAR_PREFIX = 'ycm_'
     
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
+class DefaultEntryTest(BaseRequestRateTest, unittest.TestCase):
+    robots_txt = '''\
+User-agent: *
+Crawl-delay: 1
+Request-rate: 3/15
+Disallow: /cyberworld/map/
+    '''
+    request_rate = urllib.robotparser.RequestRate(3, 15)
+    crawl_delay = 1
+    good = ['/', '/test.html']
+    bad = ['/cyberworld/map/index.html']
     
-    from nose.tools import ok_
-from ycm.paths import _EndsWithPython
+            self.assertEqual(format(1+3j, 'g'), '1+3j')
+        self.assertEqual(format(3j, 'g'), '0+3j')
+        self.assertEqual(format(1.5+3.5j, 'g'), '1.5+3.5j')
+    
+    def clean_text(text, replacements = {':': '_', ' ': '_', '/': '_', '.': '', ''': ''}):
+    for key, rep in replacements.items():
+        text = text.replace(key, rep)
+    return text    
