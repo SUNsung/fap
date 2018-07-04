d32549b2cc82d5027527c936c5f8e929076250f6
SUNsung <weatherius@gmail.com>
2018-07-05 00:04:54 +0300

    Rewrite [55pAvbSC41m2551UGjz3tKqHk55cqpTvl6SZgQ55ahc.rb] File in: 2018-07-05 00:04:53

--- ruby/55pAvbSC41m2551UGjz3tKqHk55cqpTvl6SZgQ55ahc.rb
+++ ruby/55pAvbSC41m2551UGjz3tKqHk55cqpTvl6SZgQ55ahc.rb
@@ -1,15 +1,73 @@
-  included do
-    scope :admins, -> { where(admin: true) }
-    scope :moderators, -> { where(moderator: true) }
-    scope :staff, -> { where('moderator or admin ') }
+
+        
+        invalids = []
+Parallel.each(links, in_threads: 4) do |link|
+  href = link.attribute('href').to_s
+  begin
+    case check_link(URI.join(BASE_URI, href))
+    when (200...300)
+      putc('.')
+    when (300..302)
+      putc('w')
+    end
+  rescue => e
+    putc('F')
+    invalids << '#{href} (reason: #{e.message})'
   end
+end
     
-      def cache_fragment(name)
-    ApplicationSerializer.fragment_cache[name] ||= yield
+      yield
+end
+    
+        HTTP.get(source).to_s.split('\n').each do |line|
+      next if line.start_with? '#'
+      parts = line.split(';').map(&:strip)
+      next if parts.size < 2
+      codes << [parts[0], parts[1].start_with?('fully-qualified')]
+    end
+    
+      private
+    
+      def share_target
+    { url_template: 'share?title={title}&text={text}&url={url}' }
   end
 end
 
     
-        def log_status(status)
-      puts bold status
+    # create and write to opml file
+xml = Builder::XmlMarkup.new(indent: 2)
+xml.instruct! :xml, version: '1.0', encoding: 'UTF-8'
+xml.tag!('opml', version: '1.0') do
+  # head
+  xml.tag!('head') do
+    xml.title TITLE
+  end
+    
+        self.sigs.each_key do |k|
+      # There is only one pattern per run to test
+      matched = nil
+      matches = nil
+    
+      def parse(pkt)
+    # We want to return immediantly if	we do not have a packet which is handled by us
+    return unless pkt.is_tcp?
+    return if (pkt.tcp_sport != 80 and pkt.tcp_dport != 80)
+    s = find_session((pkt.tcp_sport == 80) ? get_session_src(pkt) : get_session_dst(pkt))
+    
+          private
+    
+    @@ chat
+<pre id='chat'></pre>
+<form>
+  <input id='msg' placeholder='type message here...' />
+</form>
+    
+          def initialize(*)
+        super
+    
+        it 'Returns nil when Referer header is invalid' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/bad|uri'}
+      expect(subject.referrer(env)).to be_nil
     end
+  end
+end
