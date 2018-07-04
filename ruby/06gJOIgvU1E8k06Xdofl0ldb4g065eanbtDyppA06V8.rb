1bcf4e68c1e9599a2257b99ec9f26fa09eee1761
SUNsung <weatherius@gmail.com>
2018-07-04 15:49:25 +0300

    Rewrite [06gJOIgvU1E8k06Xdofl0ldb4g065eanbtDyppA06V8.rb] File in: 2018-07-04 15:49:25

--- ruby/06gJOIgvU1E8k06Xdofl0ldb4g065eanbtDyppA06V8.rb
+++ ruby/06gJOIgvU1E8k06Xdofl0ldb4g065eanbtDyppA06V8.rb
@@ -1,99 +1,82 @@
 
         
-              def self.available_options
-        [
-          FastlaneCore::ConfigItem.new(key: :name,
-                                       env_name: 'KEYCHAIN_NAME',
-                                       description: 'Keychain name',
-                                       conflicting_options: [:path],
-                                       is_string: true,
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :path,
-                                       env_name: 'KEYCHAIN_PATH',
-                                       description: 'Path to keychain',
-                                       is_string: true,
-                                       conflicting_options: [:name],
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :password,
-                                       env_name: 'KEYCHAIN_PASSWORD',
-                                       description: 'Password for the keychain',
-                                       sensitive: true,
-                                       optional: false),
-          FastlaneCore::ConfigItem.new(key: :default_keychain,
-                                       description: 'Should the newly created Keychain be the new system default keychain',
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :unlock,
-                                       description: 'Unlock keychain after create',
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :timeout,
-                                       description: 'timeout interval in seconds. Set `false` if you want to specify 'no time-out'',
-                                       is_string: false,
-                                       default_value: 300),
-          FastlaneCore::ConfigItem.new(key: :lock_when_sleeps,
-                                       description: 'Lock keychain when the system sleeps',
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :lock_after_timeout,
-                                       description: 'Lock keychain after timeout interval',
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :add_to_search_list,
-                                       description: 'Add keychain to search list',
-                                       is_string: false,
-                                       default_value: true)
-        ]
-      end
-    
-        def render(options, screenshots)
-      Dir.mktmpdir do |dir|
-        path = generator.render(options, screenshots, dir)
-        return File.read(path)
+                expect(result).to eq('git rev-parse --short HEAD')
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('git rev-parse --short HEAD')
       end
     end
-  end
     
-        find_union(segments, Project).includes(:namespace).order_id_desc
-  end
+          def initialize(*args, &block)
+        @bypass_confirmation_postpone = false
+        @skip_reconfirmation_in_callback = false
+        @reconfirmation_required = false
+        @skip_confirmation_notification = false
+        @raw_confirmation_token = nil
+        super
+      end
     
-        find_union(segments, Group).order_id_desc
-  end
+      # GET /resource/confirmation?confirmation_token=abcdef
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
     
-            def define_logger(name, options = {})
-          class_eval <<-RUBY, __FILE__, __LINE__ + 1
-            def #{name}(message)
-              #{options.fetch(:to, :log)}(#{name.inspect}, message)
-            end
-          RUBY
+      # POST /resource/unlock
+  def create
+    self.resource = resource_class.send_unlock_instructions(resource_params)
+    yield resource if block_given?
+    
+        def email_changed(record, opts={})
+      devise_mail(record, :email_changed, opts)
+    end
+    
+    module Devise
+  module Controllers
+    # Create url helpers to be used with resource/scope configuration. Acts as
+    # proxies to the generated routes created by devise.
+    # Resource param can be a string or symbol, a class, or an instance object.
+    # Example using a :user resource:
+    #
+    #   new_session_path(:user)      => new_user_session_path
+    #   session_path(:user)          => user_session_path
+    #   destroy_session_path(:user)  => destroy_user_session_path
+    #
+    #   new_password_path(:user)     => new_user_password_path
+    #   password_path(:user)         => user_password_path
+    #   edit_password_path(:user)    => edit_user_password_path
+    #
+    #   new_confirmation_path(:user) => new_user_confirmation_path
+    #   confirmation_path(:user)     => user_confirmation_path
+    #
+    # Those helpers are included by default to ActionController::Base.
+    #
+    # In case you want to add such helpers to another class, you can do
+    # that as long as this new class includes both url_helpers and
+    # mounted_helpers. Example:
+    #
+    #     include Rails.application.routes.url_helpers
+    #     include Rails.application.routes.mounted_helpers
+    #
+    module UrlHelpers
+      def self.remove_helpers!
+        self.instance_methods.map(&:to_s).grep(/_(url|path)$/).each do |method|
+          remove_method method
         end
       end
-    end
-  end
-end
-
     
-        # Returns the Sass/SCSS code for the media query.
-    #
-    # @param options [{Symbol => Object}] An options hash (see {Sass::CSS#initialize}).
-    # @return [String]
-    def to_src(options)
-      src = ''
-      src << Sass::Media._interp_to_src(modifier, options)
-      src << ' ' unless modifier.empty?
-      src << Sass::Media._interp_to_src(type, options)
-      src << ' and ' unless type.empty? || expressions.empty?
-      src << expressions.map do |e|
-        Sass::Media._interp_to_src(e, options)
-      end.join(' and ')
-      src
-    end
+            @email = headers[:to]
+        headers
+      end
     
-    def config_tag(config, key, tag=nil, classname=nil)
-  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
-  tag       ||= 'div'
-  classname ||= key.sub(/_/, '-').sub(/\./, '-')
-  output      = '<#{tag} class='#{classname}''
+          def call
+        title('Gems')
+        table(all_gem_names) do |gem, row|
+          row.yellow if update_available?(gem)
+          row << gem
+          row << installed_gem_version(gem)
+          row << '(update available)' if update_available?(gem)
+        end
+      end
     
-        def render(context)
-      includes_dir = File.join(context.registers[:site].source, '_includes')
+              it 'returns all release servers' do
+            expect(subject.map(&:hostname)).to eq %w{example1.com example2.com example3.com example4.com}
+          end
+        end
