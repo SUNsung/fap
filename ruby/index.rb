e3d9facfaccb50c8391d7095e1dee29966a3a551
SUNsung <weatherius@gmail.com>
2018-07-04 19:04:24 +0300

    Rewrite [index.rb] File in: 2018-07-04 19:04:23

--- ruby/index.rb
+++ ruby/index.rb
@@ -1,155 +1,34 @@
 
         
-        # No trailing slash
-Benchmark.ips do |x|
-  path = '/some/very/very/long/path/to/a/file/i/like'
-  x.report('pre_pr:#{path}')    { pre_pr(path) }
-  x.report('pr:#{path}')        { pr(path) }
-  x.report('envygeeks:#{path}') { pr(path) }
-  x.compare!
-end
-    
-        # The entry filter for this collection.
-    # Creates an instance of Jekyll::EntryFilter.
-    #
-    # Returns the instance of Jekyll::EntryFilter for this collection.
-    def entry_filter
-      @entry_filter ||= Jekyll::EntryFilter.new(site, relative_directory)
-    end
-    
-                c.option 'force', '--force', 'Force creation even if PATH already exists'
-            c.option 'blank', '--blank', 'Creates scaffolding but with empty files'
-            c.option 'skip-bundle', '--skip-bundle', 'Skip 'bundle install''
-    
-    module Jekyll
-  module Commands
-    class NewTheme < Jekyll::Command
-      class << self
-        def init_with_program(prog)
-          prog.command(:'new-theme') do |c|
-            c.syntax 'new-theme NAME'
-            c.description 'Creates a new Jekyll theme scaffold'
-            c.option 'code_of_conduct', \
-                     '-c', '--code-of-conduct', \
-                     'Include a Code of Conduct. (defaults to false)'
-    
-        # Public: Read configuration and return merged Hash
-    #
-    # file - the path to the YAML file to be read in
-    #
-    # Returns this configuration, overridden by the values in the file
-    def read_config_file(file)
-      next_config = safe_load_file(file)
-      check_config_is_hash!(next_config, file)
-      Jekyll.logger.info 'Configuration file:', file
-      next_config
-    rescue SystemCallError
-      if @default_config_file ||= nil
-        Jekyll.logger.warn 'Configuration file:', 'none'
-        {}
-      else
-        Jekyll.logger.error 'Fatal:', 'The configuration file '#{file}'
-          could not be found.'
-        raise LoadError, 'The Configuration file '#{file}' could not be found.'
-      end
-    end
-    
-        # Determine whether this document should be written.
-    # Based on the Collection to which it belongs.
-    #
-    # True if the document has a collection and if that collection's #write?
-    # method returns true, and if the site's Publisher will publish the document.
-    # False otherwise.
-    def write?
-      collection&.write? && site.publisher.publish?(self)
-    end
-    
-          NON_CONTENT_METHODS = [:fallback_data, :collapse_document].freeze
-    
-        def paragraphize(input)
-      '<p>#{input.lstrip.rstrip.gsub(/\n\n/, '</p><p>').gsub(/\n/, '<br/>')}</p>'
-    end
-  end
-end
-    
-          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
-        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
-        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
-          @img['title']  = title
-          @img['alt']    = alt
-        else
-          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
-        end
-        @img['class'].gsub!(/'/, '') if @img['class']
-      end
-      super
-    end
-    
-      # Improved version of Liquid's truncatewords:
-  # - Uses typographically correct ellipsis (…) insted of '...'
-  def truncatewords(input, length)
-    truncate = input.split(' ')
-    if truncate.length > length
-      truncate[0..length-1].join(' ').strip + ' &hellip;'
-    else
-      input
-    end
+            render_response
   end
     
-      class RenderPartialTag < Liquid::Tag
-    include OctopressFilters
-    def initialize(tag_name, markup, tokens)
-      @file = nil
-      @raw = false
-      if markup =~ /^(\S+)\s?(\w+)?/
-        @file = $1.strip
-        @raw = $2 == 'raw'
-      end
-      super
+        sign_in_as_user do
+      fill_in 'email', with: 'foo@bar.com'
     end
     
-        def blank_name?
-      @filepath.nil? || @filepath.empty?
+        def translation_scope
+      'devise.unlocks'
     end
-    
-        private
-    
-            def responds?
-          methods = @subject.instance_methods.map(&:to_s)
-          methods.include?('#{@attachment_name}') &&
-            methods.include?('#{@attachment_name}=') &&
-            methods.include?('#{@attachment_name}?')
-        end
-    
-            def rejected_types_rejected?
-          @missing_rejected_types ||= @rejected_types.select { |type| type_allowed?(type) }
-          @missing_rejected_types.none?
-        end
-      end
-    end
-  end
 end
 
     
-      # Get list of styles saved on previous deploy (running rake paperclip:refresh:missing_styles)
-  def self.get_registered_attachments_styles
-    YAML.load_file(Paperclip.registered_attachments_styles_path)
-  rescue Errno::ENOENT
-    nil
-  end
-  private_class_method :get_registered_attachments_styles
+          def remember_me_is_active?(resource)
+        return false unless resource.respond_to?(:remember_me)
+        scope = Devise::Mapping.find_scope!(resource)
+        _, token, generated_at = cookies.signed[remember_key(resource, scope)]
+        resource.remember_me?(token, generated_at)
+      end
     
-        # You can add your own processor via the Paperclip configuration. Normally
-    # Paperclip will load all processors from the
-    # Rails.root/lib/paperclip_processors directory, but here you can add any
-    # existing class using this mechanism.
-    #
-    #   Paperclip.configure do |c|
-    #     c.register_processor :watermarker, WatermarkingProcessor.new
-    #   end
-    def register_processor(name, processor)
-      @known_processors ||= {}
-      @known_processors[name.to_s] = processor
-    end
-  end
-end
+                  define_method method do |resource_or_scope, *args|
+                scope = Devise::Mapping.find_scope!(resource_or_scope)
+                router_name = Devise.mappings[scope].router_name
+                context = router_name ? send(router_name) : _devise_route_context
+                context.send('#{action}#{scope}_#{module_name}_#{path_or_url}', *args)
+              end
+            end
+          end
+        end
+      end
+    
+        private
