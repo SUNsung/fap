ca515e702a38e48b3f78edbc794c8c13fb3008bf
SUNsung <weatherius@gmail.com>
2018-07-04 09:17:25 +0300

    Rewrite [30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py] File in: 2018-07-04 09:17:24

--- pyton/30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py
+++ pyton/30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py
@@ -1,41 +1,79 @@
 
         
-        
-@pytest.mark.functional
-def test_without_confirmation(proc, TIMEOUT):
-    without_confirmation(proc, TIMEOUT)
+            # a Model inside a Sequential
+    x = Input(shape=(1,))
+    y = Dense(2)(x)
+    inner_model = Model(x, y)
+    outer_model = Sequential()
+    outer_model.add(inner_model)
+    assert outer_model.trainable_weights == inner_model.trainable_weights
+    inner_model.trainable = False
+    assert outer_model.trainable_weights == []
+    inner_model.trainable = True
+    inner_model.layers[-1].trainable = False
+    assert outer_model.trainable_weights == []
+    
+            inputs = np.random.random((num_samples, timesteps, input_size))
+        state = model.predict(inputs)
+        np.testing.assert_allclose(
+            keras.backend.eval(layer.states[0]), state, atol=1e-4)
+    
+        indices_for_conversion_to_dense = []
+    for i in range(len(model._feed_inputs)):
+        if issparse(ins[i]) and not K.is_sparse(model._feed_inputs[i]):
+            indices_for_conversion_to_dense.append(i)
+    
+    for i, input_text in enumerate(input_texts):
+    for t, char in enumerate(input_text):
+        encoder_input_data[i, t, input_token_index[char]] = 1.
     
     
-@pytest.mark.functional
-def test_refuse_with_confirmation(proc, TIMEOUT):
-    refuse_with_confirmation(proc, TIMEOUT)
+def top_k_categorical_accuracy(y_true, y_pred, k=5):
+    return K.mean(K.in_top_k(y_pred, K.argmax(y_true, axis=-1), k), axis=-1)
     
-        def test_text_response(self):
-        '''the text_response_server sends the given text'''
-        server = Server.text_response_server(
-            'HTTP/1.1 200 OK\r\n' +
-            'Content-Length: 6\r\n' +
-            '\r\nroflol'
-        )
+        # a more explicit example
+    norm_instance = constraints.max_norm(2.0)
+    x = np.array([[0, 0, 0], [1.0, 0, 0], [3, 0, 0], [3, 3, 3]]).T
+    x_normed_target = np.array([[0, 0, 0], [1.0, 0, 0],
+                                [2.0, 0, 0],
+                                [2. / np.sqrt(3),
+                                 2. / np.sqrt(3),
+                                 2. / np.sqrt(3)]]).T
+    x_normed_actual = K.eval(norm_instance(K.variable(x)))
+    assert_allclose(x_normed_actual, x_normed_target, rtol=1e-05)
     
-    Compatibility code to be able to use `cookielib.CookieJar` with requests.
+    from sklearn.datasets import make_multilabel_classification
+from sklearn.metrics import (f1_score, accuracy_score, hamming_loss,
+                             jaccard_similarity_score)
+from sklearn.utils.testing import ignore_warnings
     
-    # HTML files that should be inserted before the pages created by sphinx.
-# The format is a list of tuples containing the path and title.
-#epub_pre_files = []
+    import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import ticker
     
-        kwargs.setdefault('allow_redirects', True)
-    return request('options', url, **kwargs)
+        op.add_option('--density',
+                  dest='density', default=1 / 3,
+                  help='Density used by the sparse random projection.'
+                       ' ('auto' or float (0.0, 1.0]')
     
-        def __init__(self):
-        Resource.__init__(self)
-        self.concurrent = 0
-        self.tail = deque(maxlen=100)
-        self._reset_stats()
     
-    # Apply monkey patches to fix issues in external libraries
-from . import _monkeypatches
-del _monkeypatches
+# TASK: Build a vectorizer that splits strings into sequence of 1 to 3
+# characters instead of word tokens
     
-        if results.overwrite and os.path.exists(output_directory):
-        shutil.rmtree(output_directory)
+    iris = datasets.load_iris()
+X = iris.data[:, 0:2]  # we only take the first two features for visualization
+y = iris.target
+    
+    n_samples = 100
+n_clusters_range = np.linspace(2, n_samples, 10).astype(np.int)
+    
+        def test_eq(self):
+        self.assertTrue(self.vhost1b == self.vhost1)
+        self.assertFalse(self.vhost1 == self.vhost2)
+        self.assertEqual(str(self.vhost1b), str(self.vhost1))
+        self.assertFalse(self.vhost1b == 1234)
+    
+        # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
+    
+    patch_file = 'homeassistant.components.device_tracker.bt_home_hub_5'
