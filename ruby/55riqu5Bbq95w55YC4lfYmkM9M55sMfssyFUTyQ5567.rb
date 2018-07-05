f171fac070dfb67b356a1b55f5fa2c084791025d
SUNsung <weatherius@gmail.com>
2018-07-06 00:58:53 +0300

    Rewrite [55riqu5Bbq95w55YC4lfYmkM9M55sMfssyFUTyQ5567.rb] File in: 2018-07-06 00:58:53

--- ruby/55riqu5Bbq95w55YC4lfYmkM9M55sMfssyFUTyQ5567.rb
+++ ruby/55riqu5Bbq95w55YC4lfYmkM9M55sMfssyFUTyQ5567.rb
@@ -1,107 +1,93 @@
 
         
-            def terminate_thread!
-      if thread
-        thread.instance_eval { ActiveRecord::Base.connection_pool.release_connection }
-        thread.wakeup if thread.status == 'sleep'
-        thread.terminate
+            # Extract options for this collection from the site configuration.
+    #
+    # Returns the metadata for this collection
+    def extract_metadata
+      if site.config['collections'].is_a?(Hash)
+        site.config['collections'][label] || {}
+      else
+        {}
       end
     end
+    
+              options = configuration_from_options(options)
+          site = Jekyll::Site.new(options)
+    
+            def preserve_source_location?(path, options)
+          !options['force'] && !Dir['#{path}/**/*'].empty?
+        end
+    
+    module Jekyll
+  module Converters
+    class Markdown < Converter
+      highlighter_prefix '\n'
+      highlighter_suffix '\n'
+      safe true
+    
+          Jekyll.logger.debug 'Post-Render Hooks:', relative_path
+      Jekyll::Hooks.trigger hook_owner, :post_render, self
+    ensure
+      @_renderer = nil # this will allow the modifications above to disappear
+    end
+    
+          # `{{ site.related_posts }}` is how posts can get posts related to
+      # them, either through LSI if it's enabled, or through the most
+      # recent posts.
+      # We should remove this in 4.0 and switch to `{{ post.related_posts }}`.
+      def related_posts
+        return nil unless @current_document.is_a?(Jekyll::Document)
+        @current_document.related_posts
+      end
+      attr_writer :current_document
+    
+        def types
+      const_get(:TYPES).map(&:constantize)
+    end
+    
+      module ClassMethods
+    def setup_worker
+      active.map do |agent|
+        next unless agent.start_worker?
+        self::Worker.new(id: agent.worker_id, agent: agent)
+      end.compact
+    end
+  end
     
       def tumblr_oauth_token_secret
     service.secret
   end
     
-          # Skips sending the confirmation/reconfirmation notification email after_create/after_update. Unlike
-      # #skip_confirmation!, record still requires confirmation.
-      def skip_confirmation_notification!
-        @skip_confirmation_notification = true
-      end
+          attr_accessor(*VALID_KEYS)
+      attr_accessor :pairs
     
-          def remember_me_is_active?(resource)
-        return false unless resource.respond_to?(:remember_me)
-        scope = Devise::Mapping.find_scope!(resource)
-        _, token, generated_at = cookies.signed[remember_key(resource, scope)]
-        resource.remember_me?(token, generated_at)
-      end
-    
-            routes.each do |module_name, actions|
-          [:path, :url].each do |path_or_url|
-            actions.each do |action|
-              action = action ? '#{action}_' : ''
-              method = :'#{action}#{module_name}_#{path_or_url}'
-    
-      validate :enough_poll_answers
-  validates :question, presence: true
-    
-      def options
-    git_data = get_git_info
-    @options ||= {
-      :plugin_name => name,
-      :author => git_data.author,
-      :email  => git_data.email,
-      :min_version => '2.0',
-    }
-  end
-    
-      def as_boolean(string)
-    return true   if string == true   || string =~ (/(true|t|yes|y|1)$/i)
-    return false  if string == false  || string.blank? || string =~ (/(false|f|no|n|0)$/i)
-    raise ArgumentError.new('invalid value for Boolean: \'#{string}\'')
-  end
-    
-    module LogStash
-  module Api
-    module Commands
-      module System
-        class Plugins < Commands::Base
-          def run
-            { :total => plugins.count, :plugins => plugins }
-          end
-    
-      def eval_file(entry, files, prefix)
-    return false if entry.full_name =~ /PaxHeaders/
-    if !files.nil?
-      if files.is_a?(Array)
-        return false unless files.include?(entry.full_name.gsub(prefix, ''))
-        entry.full_name.split('/').last
-      elsif files.is_a?(String)
-        return false unless entry.full_name =~ Regexp.new(files)
-        entry.full_name.split('/').last
-      end
-    else
-      entry.full_name.gsub(prefix, '')
+      def initialize(tags = [])
+    @cask ||= self.class.cask
+    @download ||= self.class.download
+    tags.each do |tag|
+      next unless tag.is_a? Hash
+      @cask ||= tag[:cask]
+      @download ||= tag[:download]
     end
+    @tags = tags
+    @tags << :build if self.class.build
+    @name ||= infer_name
   end
     
-        # True if the dimensions represent a horizontal rectangle
-    def horizontal?
-      height < width
+      it 'should not override the header if already set' do
+    mock_app with_headers('X-Frame-Options' => 'allow')
+    expect(get('/', {}, 'wants' => 'text/html').headers['X-Frame-Options']).to eq('allow')
+  end
+end
+
+    
+      it 'should allow changing the protection mode' do
+    # I have no clue what other modes are available
+    mock_app do
+      use Rack::Protection::XSSHeader, :xss_mode => :foo
+      run DummyApp
     end
     
-            protected
-    
-            def expected_attachment
-          'Expected #{@attachment_name}:\n'
-        end
-    
-            required = directories.map do |directory|
-          pathname = File.expand_path(Rails.root.join(directory, filename))
-          file_exists = File.exist?(pathname)
-          require pathname if file_exists
-          file_exists
-        end
-    
-      class Railtie < Rails::Railtie
-    initializer 'paperclip.insert_into_active_record' do |app|
-      ActiveSupport.on_load :active_record do
-        Paperclip::Railtie.insert
-      end
-    
-          def create_validating_before_filter(attribute, validator_class, options)
-        if_clause = options.delete(:if)
-        unless_clause = options.delete(:unless)
-        send(:'before_#{attribute}_post_process', :if => if_clause, :unless => unless_clause) do |*args|
-          validator_class.new(options.dup).validate(self)
-        end
-      end
+      it 'should not interfere with normal head requests' do
+    expect(head('/')).to be_ok
+  end
