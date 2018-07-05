41aaefb609949980ed1227c78ec0bb186212a6b1
SUNsung <weatherius@gmail.com>
2018-07-05 22:22:01 +0300

    Rewrite [09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp] File in: 2018-07-05 22:22:00

--- cccccc/09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp
+++ cccccc/09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp
@@ -1,294 +1,49 @@
 
         
-        int g_suppress_level = 0;
-    
-      // The state of a web resource has been changed. A resource may have been
-  // added, removed, or altered. Source is WebResourceService, and the
-  // details are NoDetails.
-  NOTIFICATION_PROMO_RESOURCE_STATE_CHANGED,
-    
-      // Called on the UI thread when a color chooser is closed.  |chose_color| is
-  // true if the user actually chose a color, in which case |color| is the
-  // chosen color.  Calls back to the |listener_| (if applicable) to notify it
-  // of the results, and copies the modified array of |custom_colors_| back to
-  // |g_custom_colors| so future dialogs will see the changes.
-  void DidCloseDialog(bool chose_color, SkColor color, RunState run_state);
-    
-      virtual void handleDiagnostic(SourceManager &SM, SourceLoc Loc,
-                                DiagnosticKind Kind,
-                                StringRef FormatString,
-                                ArrayRef<DiagnosticArgument> FormatArgs,
-                                const DiagnosticInfo &Info) override;
-    
-      /// Associates buffer identifiers to buffer IDs.
-  llvm::StringMap<unsigned> BufIdentIDMap;
-    
-    namespace index {
-    }
-    
-    
-    {
-    {} // end namespace index
-} // end namespace swift
-    
-    void leveldb_readoptions_set_snapshot(
-    leveldb_readoptions_t* opt,
-    const leveldb_snapshot_t* snap) {
-  opt->rep.snapshot = (snap ? snap->rep : NULL);
-}
-    
-    
-    {  // When limit user key is prefix of start user key
-  ASSERT_EQ(IKey('foobar', 100, kTypeValue),
-            Shorten(IKey('foobar', 100, kTypeValue),
-                    IKey('foo', 200, kTypeValue)));
-}
-    
-      fname = DescriptorFileName('bar', 100);
-  ASSERT_EQ('bar/', std::string(fname.data(), 4));
-  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
-  ASSERT_EQ(100, number);
-  ASSERT_EQ(kDescriptorFile, type);
-    
-    #endif  // STORAGE_LEVELDB_DB_VERSION_EDIT_H_
-
-    
-    
-    {}  // namespace leveldb
-
-    
-    #ifndef STORAGE_LEVELDB_HELPERS_MEMENV_MEMENV_H_
-#define STORAGE_LEVELDB_HELPERS_MEMENV_MEMENV_H_
-    
-    namespace leveldb {
-    }
-    
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12, typename T13, typename T14, typename T15,
-    typename T16, typename T17, typename T18, typename T19, typename T20,
-    typename T21, typename T22, typename T23, typename T24, typename T25,
-    typename T26, typename T27, typename T28, typename T29, typename T30,
-    typename T31, typename T32, typename T33, typename T34, typename T35,
-    typename T36, typename T37, typename T38, typename T39, typename T40,
-    typename T41, typename T42>
-internal::ValueArray42<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
-    T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
-    T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39, T40, T41,
-    T42> Values(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9,
-    T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15, T16 v16, T17 v17,
-    T18 v18, T19 v19, T20 v20, T21 v21, T22 v22, T23 v23, T24 v24, T25 v25,
-    T26 v26, T27 v27, T28 v28, T29 v29, T30 v30, T31 v31, T32 v32, T33 v33,
-    T34 v34, T35 v35, T36 v36, T37 v37, T38 v38, T39 v39, T40 v40, T41 v41,
-    T42 v42) {
-  return internal::ValueArray42<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
-      T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25,
-      T26, T27, T28, T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39,
-      T40, T41, T42>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13,
-      v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27,
-      v28, v29, v30, v31, v32, v33, v34, v35, v36, v37, v38, v39, v40, v41,
-      v42);
-}
-    
-    TYPED_TEST_P(FooTest, HasPropertyA) { ... }
-    
-    // Binary predicate assertion macros.
-#define EXPECT_PRED_FORMAT2(pred_format, v1, v2) \
-  GTEST_PRED_FORMAT2_(pred_format, v1, v2, GTEST_NONFATAL_FAILURE_)
-#define EXPECT_PRED2(pred, v1, v2) \
-  GTEST_PRED2_(pred, v1, v2, GTEST_NONFATAL_FAILURE_)
-#define ASSERT_PRED_FORMAT2(pred_format, v1, v2) \
-  GTEST_PRED_FORMAT2_(pred_format, v1, v2, GTEST_FATAL_FAILURE_)
-#define ASSERT_PRED2(pred, v1, v2) \
-  GTEST_PRED2_(pred, v1, v2, GTEST_FATAL_FAILURE_)
-    
-      // Returns true if FilePath ends with a path separator, which indicates that
-  // it is intended to represent a directory. Returns false otherwise.
-  // This does NOT check that a directory (or file) actually exists.
-  bool IsDirectory() const;
-    
-      template <typename T>
-  operator ParamGenerator<T>() const {
-    const T array[] = {static_cast<T>(v1_), static_cast<T>(v2_),
-        static_cast<T>(v3_), static_cast<T>(v4_), static_cast<T>(v5_),
-        static_cast<T>(v6_), static_cast<T>(v7_), static_cast<T>(v8_),
-        static_cast<T>(v9_), static_cast<T>(v10_), static_cast<T>(v11_),
-        static_cast<T>(v12_), static_cast<T>(v13_), static_cast<T>(v14_),
-        static_cast<T>(v15_), static_cast<T>(v16_), static_cast<T>(v17_),
-        static_cast<T>(v18_), static_cast<T>(v19_), static_cast<T>(v20_),
-        static_cast<T>(v21_), static_cast<T>(v22_), static_cast<T>(v23_)};
-    return ValuesIn(array);
+          /*! \brief constructor */
+  SparsePage() {
+    this->Clear();
+  }
+  /*! \return number of instance in the page */
+  inline size_t Size() const {
+    return offset.size() - 1;
+  }
+  /*! \return estimation of memory cost of this page */
+  inline size_t MemCostBytes() const {
+    return offset.size() * sizeof(size_t) + data.size() * sizeof(Entry);
+  }
+  /*! \brief clear the page */
+  inline void Clear() {
+    base_rowid = 0;
+    offset.clear();
+    offset.push_back(0);
+    data.clear();
   }
     
-    inline tuple<> make_tuple() { return tuple<>(); }
-    
-    namespace torch { namespace utils {
-    }
+    namespace xgboost {
     }
     
-    #define THDPStorage TH_CONCAT_3(THDP,Real,Storage)
-#define THDPStorageStr TH_CONCAT_STRING_3(torch.cuda.,Real,Storage)
-#define THDPStorageClass TH_CONCAT_3(THDP,Real,StorageClass)
-#define THDPStorage_(NAME) TH_CONCAT_4(THDP,Real,Storage_,NAME)
-    
-    #include 'generic/Storage.h'
-#include <TH/THGenerateAllTypes.h>
-    
-    template<typename T>
-struct or_trait<T> : T {};
-    
-    int getifaddrs(struct ifaddrs** result) {
-	int fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
-	if (fd < 0) {
-		return -1;
-	}
-	netlinkrequest ifaddr_request;
-	memset(&ifaddr_request, 0, sizeof(ifaddr_request));
-	ifaddr_request.header.nlmsg_flags = NLM_F_ROOT | NLM_F_REQUEST;
-	ifaddr_request.header.nlmsg_type = RTM_GETADDR;
-	ifaddr_request.header.nlmsg_len = NLMSG_LENGTH(sizeof(ifaddrmsg));
-	ssize_t count = send(fd, &ifaddr_request, ifaddr_request.header.nlmsg_len, 0);
-	if (static_cast<size_t>(count) != ifaddr_request.header.nlmsg_len) {
-		close(fd);
-		return -1;
-	}
-	struct ifaddrs* start = NULL;
-	struct ifaddrs* current = NULL;
-	char buf[kMaxReadSize];
-	ssize_t amount_read = recv(fd, &buf, kMaxReadSize, 0);
-	while (amount_read > 0) {
-		nlmsghdr* header = reinterpret_cast<nlmsghdr*>(&buf[0]);
-		size_t header_size = static_cast<size_t>(amount_read);
-		for ( ; NLMSG_OK(header, header_size);
-		      header = NLMSG_NEXT(header, header_size)) {
-			switch (header->nlmsg_type) {
-			case NLMSG_DONE:
-				// Success. Return.
-				*result = start;
-				close(fd);
-				return 0;
-			case NLMSG_ERROR:
-				close(fd);
-				freeifaddrs(start);
-				return -1;
-			case RTM_NEWADDR: {
-				ifaddrmsg* address_msg =
-						reinterpret_cast<ifaddrmsg*>(NLMSG_DATA(header));
-				rtattr* rta = IFA_RTA(address_msg);
-				ssize_t payload_len = IFA_PAYLOAD(header);
-				while (RTA_OK(rta, payload_len)) {
-					if (rta->rta_type == IFA_ADDRESS) {
-						int family = address_msg->ifa_family;
-						if (family == AF_INET || family == AF_INET6) {
-							ifaddrs* newest = new ifaddrs;
-							memset(newest, 0, sizeof(ifaddrs));
-							if (current) {
-								current->ifa_next = newest;
-							} else {
-								start = newest;
-							}
-							if (populate_ifaddrs(newest, address_msg, RTA_DATA(rta),
-									     RTA_PAYLOAD(rta)) != 0) {
-								freeifaddrs(start);
-								*result = NULL;
-								return -1;
-							}
-							current = newest;
-						}
-					}
-					rta = RTA_NEXT(rta, payload_len);
-				}
-				break;
-			}
-			}
-		}
-		amount_read = recv(fd, &buf, kMaxReadSize, 0);
-	}
-	close(fd);
-	freeifaddrs(start);
-	return -1;
-}
-    
-    
-    {  switch (c)
-  {
-    case M_SOF2:
-      m_progressive_flag = JPGD_TRUE;
-    case M_SOF0:  /* baseline DCT */
-    case M_SOF1:  /* extended sequential DCT */
-    {
-      read_sof_marker();
-      break;
-    }
-    case M_SOF9:  /* Arithmitic coding */
-    {
-      stop_decoding(JPGD_NO_ARITHMITIC_SUPPORT);
-      break;
-    }
-    default:
-    {
-      stop_decoding(JPGD_UNSUPPORTED_MARKER);
-      break;
-    }
+    SEXP XGDMatrixSliceDMatrix_R(SEXP handle, SEXP idxset) {
+  SEXP ret;
+  R_API_BEGIN();
+  int len = length(idxset);
+  std::vector<int> idxvec(len);
+  for (int i = 0; i < len; ++i) {
+    idxvec[i] = INTEGER(idxset)[i] - 1;
   }
+  DMatrixHandle res;
+  CHECK_CALL(XGDMatrixSliceDMatrix(R_ExternalPtrAddr(handle),
+                                   BeginPtr(idxvec), len,
+                                   &res));
+  ret = PROTECT(R_MakeExternalPtr(res, R_NilValue, R_NilValue));
+  R_RegisterCFinalizerEx(ret, _DMatrixFinalizer, TRUE);
+  R_API_END();
+  UNPROTECT(1);
+  return ret;
 }
     
-    # if defined(OC_COLLECT_METRICS)
-struct oc_mode_metrics{
-  double fragw;
-  double satd;
-  double rate;
-  double rmse;
-  double satd2;
-  double satdrate;
-  double rate2;
-  double satdrmse;
-  double rmse2;
-};
+    #include <yoga/Yoga.h>
     
-       - Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-    
-    #endif
-
-    
-    
-    {  // close database
-  delete db;
-  DestroyDB(dbpath, leveldb::Options());
-}
-    
-    Status BlockHandle::DecodeFrom(Slice* input) {
-  if (GetVarint64(input, &offset_) &&
-      GetVarint64(input, &size_)) {
-    return Status::OK();
-  } else {
-    return Status::Corruption('bad block handle');
-  }
-}
-    
-    
-    {  // Pick up remaining bytes
-  switch (limit - data) {
-    case 3:
-      h += static_cast<unsigned char>(data[2]) << 16;
-      FALLTHROUGH_INTENDED;
-    case 2:
-      h += static_cast<unsigned char>(data[1]) << 8;
-      FALLTHROUGH_INTENDED;
-    case 1:
-      h += static_cast<unsigned char>(data[0]);
-      h *= m;
-      h ^= (h >> r);
-      break;
-  }
-  return h;
-}
-    
-    #ifndef STORAGE_LEVELDB_DB_LOG_WRITER_H_
-#define STORAGE_LEVELDB_DB_LOG_WRITER_H_
-    
-      // REQUIRES: Finish() has not been called since the last call to Reset().
-  // REQUIRES: key is larger than any previously added key
-  void Add(const Slice& key, const Slice& value);
+    #define ASSERT_BUF_SIZE 4096
+static char sAssertBuf[ASSERT_BUF_SIZE];
+static AssertHandler gAssertHandler;
