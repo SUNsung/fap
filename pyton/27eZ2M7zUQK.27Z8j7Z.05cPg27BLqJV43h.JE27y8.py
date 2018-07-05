ccffffe3f5bc85b5fc7bd42fd8fa690ce074860f
SUNsung <weatherius@gmail.com>
2018-07-05 14:33:36 +0300

    Rewrite [27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py] File in: 2018-07-05 14:33:35

--- pyton/27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py
+++ pyton/27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py
@@ -1,19 +1,55 @@
 
         
-                def get_auth(self, username=None, password=None):
-            assert self.raw_auth == USERNAME
-            assert username == USERNAME
-            assert password is None
-            return basic_auth()
+            def __set__(self, obj, value):
+        obj.config[self.__name__] = value
     
-    class BaseRecognizer(object):
-    '''
-    @brief Common recognizer functionality.
     
-    A generic recognizer that can handle recognizers generated from
-    lexer, parser, and tree grammars.  This is all the parsing
-    support code essentially; most of it is error recovery stuff and
-    backtracking.
-    '''
+def create_logger(app):
+    '''Get the ``'flask.app'`` logger and configure it if needed.
     
-    __version__ = '3.1.1'
+            # chop off the port which is usually not supported by browsers
+        # remove any leading '.' since we'll add that later
+        rv = rv.rsplit(':', 1)[0].lstrip('.')
+    
+            if cls.decorators:
+            view.__name__ = name
+            view.__module__ = cls.__module__
+            for decorator in cls.decorators:
+                view = decorator(view)
+    
+        try:
+        with app.test_request_context('/', environ_overrides={'HTTP_HOST': 'localhost'}):
+            pass
+    except ValueError as e:
+        assert str(e) == (
+            'the server name provided '
+            '('localhost.localdomain:5000') does not match the '
+            'server name from the WSGI environment ('localhost')'
+        )
+    
+    
+def write_with_harness(codefile, sourcefile, start_linenum, linebuffer):
+    '''write output with additional lines to make code likely compilable'''
+    # add commonly used headers, so that lines can likely compile.
+    # This is work in progress, the main issue remains handling class
+    # declarations in in-function code differently
+    with io.open(codefile, 'w') as code_filehandle:
+        code_filehandle.write('''\
+#include<stdio.h>      // by md-split
+#include<stdlib.h>     // by md-split
+#include<tuple>        // by md-split
+#include<utility>      // by md-split
+#include<limits>       // by md-split
+#include<functional>   // by md-split
+#include<string>       // by md-split
+#include<map>          // by md-split
+#include<iostream>     // by md-split
+#include<vector>       // by md-split
+#include<algorithm>    // by md-split
+#include<memory>       // by md-split
+using namespace std;   // by md-split
+// %s : %s
+''' % (sourcefile, start_linenum))
+        # TODO: if not toplevel code, wrap inside class
+        for codeline in linebuffer:
+            code_filehandle.write(codeline)
