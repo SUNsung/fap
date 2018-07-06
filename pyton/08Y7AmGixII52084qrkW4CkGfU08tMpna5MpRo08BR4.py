68c5fa101bb2f3dfa91c7e067f1fadff5e0d6763
SUNsung <weatherius@gmail.com>
2018-07-07 01:21:57 +0300

    Rewrite [08Y7AmGixII52084qrkW4CkGfU08tMpna5MpRo08BR4.py] File in: 2018-07-07 01:21:57

--- pyton/08Y7AmGixII52084qrkW4CkGfU08tMpna5MpRo08BR4.py
+++ pyton/08Y7AmGixII52084qrkW4CkGfU08tMpna5MpRo08BR4.py
@@ -1,43 +1,71 @@
-def cbs_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    '''Downloads CBS videos by URL.
-    '''
-    
-        type, ext, size = url_info(video_url, headers=fake_headers)
-    
-            # cookie handler
-        ssl_context = request.HTTPSHandler(
-            context=ssl.SSLContext(ssl.PROTOCOL_TLSv1))
-        cookie_handler = request.HTTPCookieProcessor()
-        opener = request.build_opener(ssl_context, cookie_handler)
-        opener.addheaders = [
-            ('Referer', self.url),
-            ('Cookie',
-             'CloudFront-Policy=%s;CloudFront-Signature=%s;CloudFront-Key-Pair-Id=%s' % (scp, scs, sck))
-        ]
-        request.install_opener(opener)
-    
-            # extract raw urls
-        orig_img = match1(content,
-                         r'<meta itemprop='image' content='([^']+/originals/[^']+)'')
-        twit_img = match1(content,
-                          r'<meta property='twitter:image:src' name='twitter:image:src' content='([^']+)'')
-    
-    def clean_text(text, replacements = {':': '_', ' ': '_', '/': '_', '.': '', ''': ''}):
-    for key, rep in replacements.items():
-        text = text.replace(key, rep)
-    return text    
-    
-            #Creating directory title page for current directory
-        with open('dir_title.html', 'w') as os_html:
-            os_html.write(header + dir_title)
+
         
-        group.append(HTML('dir_title.html').render())
+                String:                    '#4e9a06',        # class: 's'
+        String.Backtick:           '#4e9a06',        # class: 'sb'
+        String.Char:               '#4e9a06',        # class: 'sc'
+        String.Doc:                'italic #8f5902', # class: 'sd' - like a comment
+        String.Double:             '#4e9a06',        # class: 's2'
+        String.Escape:             '#4e9a06',        # class: 'se'
+        String.Heredoc:            '#4e9a06',        # class: 'sh'
+        String.Interpol:           '#4e9a06',        # class: 'si'
+        String.Other:              '#4e9a06',        # class: 'sx'
+        String.Regex:              '#4e9a06',        # class: 'sr'
+        String.Single:             '#4e9a06',        # class: 's1'
+        String.Symbol:             '#4e9a06',        # class: 'ss'
     
+    DEFAULT_CA_BUNDLE_PATH = certs.where()
     
-def is_translated(msg):
-    if isinstance(msg.string, basestring):
-        return bool(msg.string)
-    for item in msg.string:
-        if not item:
-            return False
-    return True
+        def doc(code):
+        names = ', '.join('``%s``' % n for n in _codes[code])
+        return '* %d: %s' % (code, names)
+    
+            @wraps(cb)
+        def cb_wrapper(response):
+            try:
+                output = cb(response)
+                output = list(iterate_spider_output(output))
+            except:
+                case = _create_testcase(method, 'callback')
+                results.addError(case, sys.exc_info())
+    
+        name = 'url'
+    
+        def _add_middleware(self, mw):
+        super(SpiderMiddlewareManager, self)._add_middleware(mw)
+        if hasattr(mw, 'process_spider_input'):
+            self.methods['process_spider_input'].append(mw.process_spider_input)
+        if hasattr(mw, 'process_spider_output'):
+            self.methods['process_spider_output'].insert(0, mw.process_spider_output)
+        if hasattr(mw, 'process_spider_exception'):
+            self.methods['process_spider_exception'].insert(0, mw.process_spider_exception)
+        if hasattr(mw, 'process_start_requests'):
+            self.methods['process_start_requests'].insert(0, mw.process_start_requests)
+    
+            # extract cookies from Set-Cookie and drop invalid/expired cookies
+        cookiejarkey = request.meta.get('cookiejar')
+        jar = self.jars[cookiejarkey]
+        jar.extract_cookies(response, request)
+        self._debug_set_cookie(response, spider)
+    
+    from scrapy import signals
+    
+        def process_request(self, request, spider):
+        if self.user_agent:
+            request.headers.setdefault(b'User-Agent', self.user_agent)
+
+    
+    class UsageError(Exception):
+    '''To indicate a command-line usage error'''
+    def __init__(self, *a, **kw):
+        self.print_help = kw.pop('print_help', True)
+        super(UsageError, self).__init__(*a, **kw)
+    
+            if self.close_on.get('errorcount'):
+            crawler.signals.connect(self.error_count, signal=signals.spider_error)
+        if self.close_on.get('pagecount'):
+            crawler.signals.connect(self.page_count, signal=signals.response_received)
+        if self.close_on.get('timeout'):
+            crawler.signals.connect(self.spider_opened, signal=signals.spider_opened)
+        if self.close_on.get('itemcount'):
+            crawler.signals.connect(self.item_scraped, signal=signals.item_scraped)
+        crawler.signals.connect(self.spider_closed, signal=signals.spider_closed)
