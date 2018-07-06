1ba2c64569ae002b685af63fe0b9d08afed79aa0
SUNsung <weatherius@gmail.com>
2018-07-07 02:18:31 +0300

    Rewrite [08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb] File in: 2018-07-07 02:18:30

--- ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
+++ ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
@@ -1,84 +1,104 @@
 
         
-        TEST_DIR = File.expand_path('../test', __dir__)
-    
-    # You can also do other things. Examples:
-# https://github.com/tmm1/stackprof/blob/master/bin/stackprof
-report = StackProf::Report.new(Marshal.load(IO.binread(PROF_OUTPUT_FILE)))
-report.print_text(
-  options[:sort],
-  options[:limit],
-  options[:select_files],
-  options[:reject_files],
-  options[:select_names],
-  options[:reject_names]
-)
-    
-    require 'json'
-require 'open-uri'
-    
-              # TODO: replace above lines with
-          # confirmable = find_or_initialize_with_error_by(:confirmation_token, confirmation_token)
-          # after enough time has passed that Devise clients do not use digested tokens
-    
-      # GET /resource/password/edit?reset_password_token=abcdef
-  def edit
-    self.resource = resource_class.new
-    set_minimum_password_length
-    resource.reset_password_token = params[:reset_password_token]
-  end
-    
-      def respond_to_on_destroy
-    # We actually need to hardcode this as Rails default responder doesn't
-    # support returning empty response on GET request
-    respond_to do |format|
-      format.all { head :no_content }
-      format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
-    end
-  end
-end
-
-    
-          def remember_me_is_active?(resource)
-        return false unless resource.respond_to?(:remember_me)
-        scope = Devise::Mapping.find_scope!(resource)
-        _, token, generated_at = cookies.signed[remember_key(resource, scope)]
-        resource.remember_me?(token, generated_at)
+          describe '.all' do
+    it 'uses the client to fetch all keys' do
+      mock_client_response(:list_keys, with: no_args) do
+        [
+          {
+            canDownload: false,
+            canRevoke: true,
+            keyId: 'some-key-id',
+            keyName: 'Test Key via fastlane',
+            servicesCount: 2
+          },
+          {
+            canDownload: true,
+            canRevoke: true,
+            keyId: 'B92NE4F7RG',
+            keyName: 'Test Key via browser',
+            servicesCount: 2
+          }
+        ]
       end
     
-          describe 'setting an internal hosts filter' do
-        subject { dsl.roles(:app) }
-        it 'is ignored' do
-          dsl.set :filter, hosts: 'example3.com'
-          expect(subject.map(&:hostname)).to eq(['example3.com', 'example4.com'])
+            expect(result).to eq('/usr/local/bin/cloc  --by-file --xml  --out=build/cloc.xml')
+      end
+    
+          it 'it increments all targets minor version number' do
+        Fastlane::FastFile.new.parse('lane :test do
+          increment_version_number(bump_type: 'minor')
+        end').runner.execute(:test)
+    
+        find_union(segments, Group).order_id_desc
+  end
+    
+        # The path used after sending reset password instructions
+    def after_sending_reset_password_instructions_path_for(resource_name)
+      new_session_path(resource_name) if is_navigational_format?
+    end
+    
+                # Remove the signal trap if no more registered callbacks exist
+            Signal.trap('INT', 'DEFAULT') if registered.empty?
+          end
         end
-      end
     
-    task default: :test
+    def report_apps
+  running = Set.new
+  @app_names.zip(@bundle_ids, @unix_ids).each do |app_name, bundle_id, _unix_id|
+    next if excluded_bundle_id bundle_id
+    next if excluded_app_name app_name
+    bundle_id.gsub!(%r{^(missing value)$}, '<\1>')
+    running.add '#{bundle_id}\t#{app_name}'
+  end
     
-            #target = $LOADED_FEATURES.grep(/#{path}/).first
-        #puts path
-        #puts caller.map { |c| '  #{c}' }.join('\n')
-        #fontsize = [10, duration * 48].max
-        puts '#{duration},#{path},#{source}'
-      end
-      #puts caller.map { |c| ' => #{c}' }.join('\n')
-    end
+          def initialize(pairs = {})
+        @pairs = pairs
+        pairs.each do |key, value|
+          raise 'invalid container key: '#{key.inspect}'' unless VALID_KEYS.include?(key)
+          send(:'#{key}=', value)
+        end
     
-        def definitions_for(klass)
-      parent_classes = klass.ancestors.reverse
-      parent_classes.each_with_object({}) do |ancestor, inherited_definitions|
-        inherited_definitions.deep_merge! @attachments[ancestor]
-      end
+        formulae = ARGV.include?('--installed') ? Formula.installed : Formula
+    recursive = ARGV.flag? '--recursive'
+    only_installed_arg = ARGV.include?('--installed') &&
+                         !ARGV.include?('--include-build') &&
+                         !ARGV.include?('--include-test') &&
+                         !ARGV.include?('--include-optional') &&
+                         !ARGV.include?('--skip-recommended')
+    
+    #
+# Project
+#
+    
+        if extension_pathname.readable?
+      ENV['BUNDLE_GEMFILE'] = extension_pathname.to_path
+      break
     end
   end
 end
+    
+    $stdout.puts res
 
     
-        def empty_file?
-      File.exist?(@filepath) && File.size(@filepath) == 0
-    end
+      def register_sigs
+    self.sigs = {
+      :banner		=> /^(\*\s+OK[^\n\r]*)/i,
+      :login		=> /^CAPABILITY\s+LOGIN\s+([^\s]+)\s+([^\n\r]+)/i,
+      :login_pass => /^CAPABILITY\s+OK\s+(Login[^\n\r]*)/i,
+      :login_bad	=> /^CAPABILITY\s+BAD\s+(Login[^\n\r]*)/i,
+      :login_fail => /^CAPABILITY\s+NO\s+(Login[^\n\r]*)/i
+    }
+  end
     
-            Paperclip::Validators.constants.each do |constant|
-          if constant.to_s =~ /\AAttachment(.+)Validator\z/
-            validator_kind = $1.underscore.to_sym
+                  s[:proto] = 'tcp'
+              s[:name]  = 'pop3'
+              s[:extra] = 'Successful Login. Banner: #{s[:banner]}'
+              report_auth_info(s)
+              print_status('Successful POP3 Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]} (#{s[:banner].strip})')
+    
+    module Capistrano
+  module TaskEnhancements
+    def before(task, prerequisite, *args, &block)
+      prerequisite = Rake::Task.define_task(prerequisite, *args, &block) if block_given?
+      Rake::Task[task].enhance [prerequisite]
+    end
