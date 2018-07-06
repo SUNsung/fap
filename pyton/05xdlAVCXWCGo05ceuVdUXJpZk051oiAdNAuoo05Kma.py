1f883fa5bb814c76719e7fc5cf1daef7330570ba
SUNsung <weatherius@gmail.com>
2018-07-06 20:22:30 +0300

    Rewrite [05xdlAVCXWCGo05ceuVdUXJpZk051oiAdNAuoo05Kma.py] File in: 2018-07-06 20:22:29

--- pyton/05xdlAVCXWCGo05ceuVdUXJpZk051oiAdNAuoo05Kma.py
+++ pyton/05xdlAVCXWCGo05ceuVdUXJpZk051oiAdNAuoo05Kma.py
@@ -1,113 +1,82 @@
 
         
-            def __init__(self, rank):
-        self.state = CallState.READY
-        self.rank = rank
-        self.employee = None
+            @property
+    def encoding(self):
+        return 'utf8'
     
-        def bfs(self, source, dest):
-        if source is None:
-            return False
-        queue = deque()
-        queue.append(source)
-        source.visit_state = State.visited
-        while queue:
-            node = queue.popleft()
-            print(node)
-            if dest is node:
-                return True
-            for adjacent_node in node.adj_nodes.values():
-                if adjacent_node.visit_state == State.unvisited:
-                    queue.append(adjacent_node)
-                    adjacent_node.visit_state = State.visited
-        return False
+        def format_body(self, content, mime):
+        if is_valid_mime(mime):
+            for p in self.enabled_plugins:
+                content = p.format_body(content, mime)
+        return content
+
     
-        def __init__(self, MAX_SIZE):
-        self.MAX_SIZE = MAX_SIZE
-        self.size = 0
-        self.lookup = {}  # key: query, value: node
-        self.linked_list = LinkedList()
+        '''
+    .format(OUT_RESP_HEAD)
+)
+output_options.add_argument(
+    '--body', '-b',
+    dest='output_options',
+    action='store_const',
+    const=OUT_RESP_BODY,
+    help='''
+    Print only the response body. Shortcut for --print={0}.
     
-        def spot_freed(self):
-        self.available_spots += 1
+        def update_headers(self, request_headers):
+        '''
+        Update the session headers with the request ones while ignoring
+        certain name prefixes.
     
-        def __init__(self, template_categories_to_budget_map):
-        self.categories_to_budget_map = template_categories_to_budget_map
-    
-        To avoid English-only error messages and to generally make things
-    as flexible as possible, these exceptions are not created with strings,
-    but rather the information necessary to generate an error.  Then
-    the various reporting methods in Parser and Lexer can be overridden
-    to generate a localized error message.  For example, MismatchedToken
-    exceptions are built with the expected token type.
-    So, don't expect getMessage() to return anything.
-    
-              stat -> expr -> atom
-    
-        def test_sequential_undo(self):
-        self.command_stack = list(reversed(self.command_stack))
-        self.command_stack[0].undo()
-        output_after_first_undo = os.listdir(self.test_dir)
-        self.assertEqual(output_after_first_undo[0], 'bar.txt')
-        self.command_stack[1].undo()
-        output_after_second_undo = os.listdir(self.test_dir)
-        self.assertEqual(output_after_second_undo[0], 'foo.txt')
-    
-        def test_c_observers_shall_be_detachable(cls):
-        cls.s.detach(cls.dec_obs)
-        # hex viewer shall be remaining if dec viewer is detached first
-        cls.assertEqual(isinstance(cls.s._observers[0], HexViewer), True)
-        cls.assertEqual(len(cls.s._observers), 1)
-        cls.s.detach(cls.hex_obs)
-        cls.assertEqual(len(cls.s._observers), 0)
-    
-        def test_sales_manager_shall_not_talk_through_proxy_with_delay(cls):
-        cls.ntp.busy = 'No'
-        start_time = time()
-        cls.ntp.talk()
-        end_time = time()
-        execution_time = end_time - start_time
-        print_output = cls.output.getvalue()
-        expected_print_output = 'Proxy checking for Sales Manager availability\n\
-This Sales Manager will not talk to you whether he/she is busy or not\n'
-        cls.assertEqual(print_output, expected_print_output)
-        expected_execution_time = 1
-        cls.assertEqual(int(execution_time*10), expected_execution_time)
-    
-        jim = Subscriber('jim', message_center)
-    jim.subscribe('cartoon')
-    jack = Subscriber('jack', message_center)
-    jack.subscribe('music')
-    gee = Subscriber('gee', message_center)
-    gee.subscribe('movie')
-    vani = Subscriber('vani', message_center)
-    vani.subscribe('movie')
-    vani.unsubscribe('movie')
-    
-        def is_satisfied_by(self, candidate):
-        return getattr(candidate, 'super_user', False)
+    This module provides the capabilities for the Requests hooks system.
     
     
-class State(object):
+def prepare_url(value):
+    # Issue #1483: Make sure the URL always has a trailing slash
+    httpbin_url = value.url.rstrip('/') + '/'
     
-    STATES = ['high', 'low']
     
-        if unit_1 == LENGTH_MILES:
-        meters = __miles_to_meters(value)
-    elif unit_1 == LENGTH_FEET:
-        meters = __feet_to_meters(value)
-    elif unit_1 == LENGTH_KILOMETERS:
-        meters = __kilometers_to_meters(value)
+def test_idna_without_version_attribute(mocker):
+    '''Older versions of IDNA don't provide a __version__ attribute, verify
+    that if we have such a package, we don't blow up.
+    '''
+    mocker.patch('requests.help.idna', new=None)
+    assert info()['idna'] == {'version': ''}
     
-            self.session.add(States(
-            entity_id='sensor.temperature',
-            state='20',
-            last_changed=before_run,
-            last_updated=before_run,
-        ))
-        self.session.add(States(
-            entity_id='sensor.sound',
-            state='10',
-            last_changed=after_run,
-            last_updated=after_run,
-        ))
+        def request_url(self, request, proxies):
+        '''Obtain the url to use when making the final request.
+    
+            # Verify PreparedRequest can be pickled
+        r = pickle.loads(pickle.dumps(p))
+        assert r.url == p.url
+        assert r.headers == p.headers
+        assert r.body == p.body
+        assert r.hooks == p.hooks
+    
+        close_server = threading.Event()
+    server = Server(digest_failed_response_handler, wait_to_close_event=close_server)
+    
+        def __call__(self, r):
+        # Initialize per-thread state, if needed
+        self.init_per_thread_state()
+        # If we have a saved nonce, skip the 401
+        if self._thread_local.last_nonce:
+            r.headers['Authorization'] = self.build_digest_header(r.method, r.url)
+        try:
+            self._thread_local.pos = r.body.tell()
+        except AttributeError:
+            # In the case of HTTPDigestAuth being reused and the body of
+            # the previous request was a file-like object, pos has the
+            # file position of the previous body. Ensure it's set to
+            # None.
+            self._thread_local.pos = None
+        r.register_hook('response', self.handle_401)
+        r.register_hook('response', self.handle_redirect)
+        self._thread_local.num_401_calls = 1
+    
+    try:
+    import simplejson as json
+except ImportError:
+    import json
+    
+        def __getitem__(self, key):
+        return self._store[key.lower()][1]
