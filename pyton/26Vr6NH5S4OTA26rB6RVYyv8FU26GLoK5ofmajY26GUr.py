b3697b79c5754d6101338e48655857c34f6f064d
SUNsung <weatherius@gmail.com>
2018-07-06 05:04:30 +0300

    Rewrite [26Vr6NH5S4OTA26rB6RVYyv8FU26GLoK5ofmajY26GUr.py] File in: 2018-07-06 05:04:30

--- pyton/26Vr6NH5S4OTA26rB6RVYyv8FU26GLoK5ofmajY26GUr.py
+++ pyton/26Vr6NH5S4OTA26rB6RVYyv8FU26GLoK5ofmajY26GUr.py
@@ -1,136 +1,96 @@
 
         
-                    self.tags[key] = tag
-    
-    
-def with_metaclass(meta, *bases):
-    '''Create a base class with a metaclass.'''
-    # This requires a bit of explanation: the basic idea is to make a
-    # dummy metaclass for one level of class instantiation that replaces
-    # itself with the actual metaclass.
-    class metaclass(type):
-        def __new__(cls, name, this_bases, d):
-            return meta(name, bases, d)
-    return type.__new__(metaclass, 'temporary_class', (), {})
-    
-            :param namespace: a configuration namespace
-        :param lowercase: a flag indicating if the keys of the resulting
-                          dictionary should be lowercase
-        :param trim_namespace: a flag indicating if the keys of the resulting
-                          dictionary should not include the namespace
-    
-    
-def create_logger(app):
-    '''Get the ``'flask.app'`` logger and configure it if needed.
-    
-        #: When data is changed, this is set to ``True``. Only the session
-    #: dictionary itself is tracked; if the session contains mutable
-    #: data (for example a nested dict) then this must be set to
-    #: ``True`` manually when modifying that data. The session cookie
-    #: will only be written to the response if this is ``True``.
-    modified = False
-    
-    
-def info(message, *args):
-    print(message % args, file=sys.stderr)
-    
-        app = flask.Flask(__name__)
-    with pytest.raises(TypeError):
-        app.config.from_mapping(
-            {}, {}
+            plugin_manager.register(Plugin)
+    try:
+        r = http(
+            httpbin + BASIC_AUTH_URL,
+            '--auth-type',
+            Plugin.auth_type,
+            '--auth',
+            BASIC_AUTH_HEADER_VALUE,
         )
+        assert HTTP_OK in r
+        assert r.json == AUTH_OK
+    finally:
+        plugin_manager.unregister(Plugin)
+    
+        # noinspection PyProtectedMember
+    @property
+    def headers(self):
+        original = self._orig.raw._original_response
     
     
-def test_main_module_paths(modules_tmpdir, purge_module):
-    app = modules_tmpdir.join('main_app.py')
-    app.write('import flask\n\napp = flask.Flask('__main__')')
-    purge_module('main_app')
+BINARY_SUPPRESSED_NOTICE = (
+    b'\n'
+    b'+-----------------------------------------+\n'
+    b'| NOTE: binary data not shown in terminal |\n'
+    b'+-----------------------------------------+'
+)
+    
+    
+def get_exit_status(http_status, follow=False):
+    '''Translate HTTP status code to exit status code.'''
+    if 300 <= http_status <= 399 and not follow:
+        # Redirect
+        return ExitStatus.ERROR_HTTP_3XX
+    elif 400 <= http_status <= 499:
+        # Client Error
+        return ExitStatus.ERROR_HTTP_4XX
+    elif 500 <= http_status <= 599:
+        # Server Error
+        return ExitStatus.ERROR_HTTP_5XX
+    else:
+        return ExitStatus.OK
+    
+        def test_self_signed_server_cert_by_default_raises_ssl_error(
+            self,
+            httpbin_secure_untrusted):
+        with pytest.raises(SSLError):
+            http(httpbin_secure_untrusted.url + '/get')
+    
+        name = None
+    helpurl = None
+    about = None
     
     import pytest
-from werkzeug.exceptions import NotFound
+from requests.compat import urljoin
     
-        @app.route('/meh')
-    def meh():
-        return flask.request.url
     
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer)
-        ]
+class Server(threading.Thread):
+    '''Dummy server using for unit testing'''
+    WAIT_EVENT_TIMEOUT = 5
     
-        @value.setter
-    def value(self, new_value):
-        if 1 <= new_value <= 13:
-            self._value = new_value
-        else:
-            raise ValueError('Invalid card value: {}'.format(new_value))
+    '''
+requests._internal_utils
+~~~~~~~~~~~~~~
     
-        def take_spot(self, spot):
-        self.spots_taken.append(spot)
+        get_item_parameters = pytest.mark.parametrize(
+        'key, value', (
+            ('bad_gateway', 502),
+            ('not_a_key', None)
+        )
+    )
     
-            (2016-01, shopping), 25
-        (2016-01, shopping), 100
-        (2016-01, gas), 50
-        '''
-        timestamp, category, amount = line.split('\t')
-        period = self. extract_year_month(timestamp)
-        if period == self.current_year_month():
-            yield (period, category), amount
     
-        def __init__(self):
-        self._active_state = Active(self)  # Unit.Inservice.Active()
-        self._standby_state = Standby(self)  # Unit.Inservice.Standby()
-        self._suspect_state = Suspect(self)  # Unit.OutOfService.Suspect()
-        self._failed_state = Failed(self)  # Unit.OutOfService.Failed()
-        self._current_state = self._standby_state
-        self.states = {'active': self._active_state,
-                       'standby': self._standby_state,
-                       'suspect': self._suspect_state,
-                       'failed': self._failed_state}
-        self.message_types = {'fault trigger': self._current_state.on_fault_trigger,
-                              'switchover': self._current_state.on_switchover,
-                              'diagnostics passed': self._current_state.on_diagnostics_passed,
-                              'diagnostics failed': self._current_state.on_diagnostics_failed,
-                              'operator inservice': self._current_state.on_operator_inservice}
+def _implementation():
+    '''Return a dict with the Python implementation and version.
     
-        def test_sales_manager_shall_talk_through_proxy_with_delay(cls):
-        cls.p.busy = 'No'
-        start_time = time()
-        cls.p.talk()
-        end_time = time()
-        execution_time = end_time - start_time
-        print_output = cls.output.getvalue()
-        expected_print_output = 'Proxy checking for Sales Manager availability\n\
-Sales Manager ready to talk\n'
-        cls.assertEqual(print_output, expected_print_output)
-        expected_execution_time = 1
-        cls.assertEqual(int(execution_time*10), expected_execution_time)
-    
-        def __init__(self, name):
-        self.name = name
-    
-        def test_bear_eng_localization(self):
-        self.assertEqual(self.e.get('bear'), 'bear')
-    
-        def test_am_station_overflow_after_scan(self):
-        self.radio.scan()
-        station = self.radio.state.stations[self.radio.state.pos]
-        expected_station = '1250'
-        self.assertEqual(station, expected_station)
-    
-        def test_display_current_time_at_midnight(self):
-        class_under_test = TimeDisplay()
-        expected_time = '24:01'
-        result = class_under_test.get_current_time_as_as_html_fragment()
-        self.assertEqual(result, expected_time)
+    This module handles import compatibility issues between Python 2 and
+Python 3.
 '''
     
-    production code which is untestable:
-    
-        parameter
+        :rtype: bool
     '''
+    ipaddr = struct.unpack('=L', socket.inet_aton(ip))[0]
+    netaddr, bits = net.split('/')
+    netmask = struct.unpack('=L', socket.inet_aton(dotted_netmask(int(bits))))[0]
+    network = struct.unpack('=L', socket.inet_aton(netaddr))[0] & netmask
+    return (ipaddr & netmask) == (network & netmask)
     
-        def commit(self):
-        self.states = [memento(target, self.deep) for target in self.targets]
+        for i, yi in enumerate(grid_y):
+        for j, xi in enumerate(grid_x):
+            z_sample = np.array([[xi, yi]])
+            x_decoded = decoder.predict(z_sample)
+            digit = x_decoded[0].reshape(digit_size, digit_size)
+            figure[i * digit_size: (i + 1) * digit_size,
+                   j * digit_size: (j + 1) * digit_size] = digit
