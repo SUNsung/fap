b41c0e9633bed97309adfb1c2d8bc6587a69e76b
SUNsung <weatherius@gmail.com>
2018-07-07 11:13:28 +0300

    Rewrite [59QbtwwpN4kJg590nVZKhFtYyQ59urjQgG6UeCI59iKr.py] File in: 2018-07-07 11:13:27

--- pyton/59QbtwwpN4kJg590nVZKhFtYyQ59urjQgG6UeCI59iKr.py
+++ pyton/59QbtwwpN4kJg590nVZKhFtYyQ59urjQgG6UeCI59iKr.py
@@ -1,139 +1,151 @@
-  # Construct the parser.
-  parser = spec_builder.ComponentSpecBuilder('parser')
-  parser.set_network_unit(name='FeedForwardNetwork', hidden_layer_sizes='256',
-                          layer_norm_hidden='true')
-  parser.set_transition_system(name='arc-standard')
-  parser.add_token_link(source=lookahead, fml='input.focus', embedding_dim=64)
-  parser.add_token_link(
-      source=tagger, fml='input.focus stack.focus stack(1).focus',
-      embedding_dim=64)
+
+        
+            # noinspection PyProtectedMember
+    @property
+    def headers(self):
+        original = self._orig.raw._original_response
     
-    py_library(
-    name = 'mst_units',
-    srcs = ['mst_units.py'],
-    deps = [
-        ':mst_ops',
-        ':network_units',
-        '//syntaxnet/util:check',
-        '@org_tensorflow//tensorflow:tensorflow_py',
-    ],
-)
-    
-      def __init__(self):
-    pass
-    
-      def testFailsOnNonIdentityTranslator(self):
-    component_spec = spec_pb2.ComponentSpec()
-    text_format.Parse('''
-        name: 'test'
-        network_unit {
-          registered_name: 'IdentityNetwork'
-        }
-        linked_feature {
-          name: 'features' embedding_dim: -1 size: 1
-          source_translator: 'history'
-          source_component: 'mock'
-        }
-        ''', component_spec)
-    
-        def test_create_other_buttons(self):
-        for state in (False, True):
-            var = self.engine.backvar
-            var.set(state)
-            frame, others = self.btn_test_setup(
-                self.dialog.create_other_buttons)
-            buttons = frame.pack_slaves()
-            for spec, button in zip(others, buttons):
-                val, label = spec
-                self.assertEqual(button['text'], label)
-                if val == state:
-                    # hit other button, then this one
-                    # indexes depend on button order
-                    self.assertEqual(var.get(), state)
-    
-        @classmethod
-    def zonelist(cls, zonedir='/usr/share/zoneinfo'):
-        zones = []
-        for root, _, files in os.walk(zonedir):
-            for f in files:
-                p = os.path.join(root, f)
-                with open(p, 'rb') as o:
-                    magic =  o.read(4)
-                if magic == b'TZif':
-                    zones.append(p[len(zonedir) + 1:])
-        return zones
-    
-        def date(self):
-        '''Process the DATE command.
-        Returns:
-        - resp: server response if successful
-        - date: datetime object
-        '''
-        resp = self._shortcmd('DATE')
-        if not resp.startswith('111'):
-            raise NNTPReplyError(resp)
-        elem = resp.split()
-        if len(elem) != 2:
-            raise NNTPDataError(resp)
-        date = elem[1]
-        if len(date) != 14:
-            raise NNTPDataError(resp)
-        return resp, _parse_datetime(date, None)
-    
-        def test_is_dir(self):
-        P = self.cls(BASE)
-        self.assertTrue((P / 'dirA').is_dir())
-        self.assertFalse((P / 'fileA').is_dir())
-        self.assertFalse((P / 'non-existing').is_dir())
-        self.assertFalse((P / 'fileA' / 'bah').is_dir())
-        if support.can_symlink():
-            self.assertFalse((P / 'linkA').is_dir())
-            self.assertTrue((P / 'linkB').is_dir())
-            self.assertFalse((P/ 'brokenLink').is_dir())
-    
-    # dataclass_module_1.py and dataclass_module_1_str.py are identical
-# except only the latter uses string annotations.
-    
-    T_CV2 = ClassVar[int]
-T_CV3 = ClassVar
-    
-            self.assertEqual('1.0.1.0', g(b'\x01\x00\x01\x00'))
-        self.assertEqual('170.85.170.85', g(b'\xaa\x55\xaa\x55'))
-        self.assertEqual('255.255.255.255', g(b'\xff\xff\xff\xff'))
-        assertInvalid(g, b'\x00' * 3)
-        assertInvalid(g, b'\x00' * 5)
-        assertInvalid(g, b'\x00' * 16)
-        self.assertEqual('170.85.170.85', g(bytearray(b'\xaa\x55\xaa\x55')))
+            '''
+        available_plugins = plugin_manager.get_formatters_grouped()
+        self.enabled_plugins = []
+        for group in groups:
+            for cls in available_plugins[group]:
+                p = cls(env=env, **kwargs)
+                if p.enabled:
+                    self.enabled_plugins.append(p)
     
     
-def read_text(package: Package,
-              resource: Resource,
-              encoding: str = 'utf-8',
-              errors: str = 'strict') -> str:
-    '''Return the decoded string of the resource.
+FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
+FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
+JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
+BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
     
-        def _init_board(self):
-        succs = self.succs
-        del succs[:]
-        m, n = self.m, self.n
-        c2i = self.coords2index
     
-    ATTR_URL = 'url'
-ATTR_URL_DEFAULT = 'https://www.google.com'
+def test_unicode_digest_auth(httpbin):
+    # it doesn't really authenticate us because httpbin
+    # doesn't interpret the utf8-encoded auth
+    http('--auth-type=digest',
+         '--auth', u'test:%s' % UNICODE,
+         httpbin.url + u'/digest-auth/auth/test/' + UNICODE)
+
     
-        hass.services.register(DOMAIN, SERVICE_MEDIA_PLAY_PAUSE,
-                           lambda service:
-                           keyboard.tap_key(keyboard.media_play_pause_key),
-                           schema=TAP_KEY_SCHEMA)
+            if now - self._prev_time >= self._update_interval:
+            downloaded = self.status.downloaded
+            try:
+                speed = ((downloaded - self._prev_bytes) /
+                         (now - self._prev_time))
+            except ZeroDivisionError:
+                speed = 0
     
-    patch_file = 'homeassistant.components.device_tracker.bt_home_hub_5'
+            '''
+        return headers
     
-        def test_from_event(self):
-        '''Test converting event to db state.'''
-        state = ha.State('sensor.temperature', '18')
-        event = ha.Event(EVENT_STATE_CHANGED, {
-            'entity_id': 'sensor.temperature',
-            'old_state': None,
-            'new_state': state,
-        })
-        assert state == States.from_event(event).to_native()
+        Assumes `from __future__ import division`.
+    
+    
+class ExitStatus:
+    '''Exit status code constants.'''
+    OK = 0
+    ERROR = 1
+    PLUGIN_ERROR = 7
+    
+            '''
+        self.args.output_file_specified = bool(self.args.output_file)
+        if self.args.download:
+            # FIXME: Come up with a cleaner solution.
+            if not self.args.output_file and not self.env.stdout_isatty:
+                # Use stdout as the download output file.
+                self.args.output_file = self.env.stdout
+            # With `--download`, we write everything that would normally go to
+            # `stdout` to `stderr` instead. Let's replace the stream so that
+            # we don't have to use many `if`s throughout the codebase.
+            # The response body will be treated separately.
+            self.env.stdout = self.env.stderr
+            self.env.stdout_isatty = self.env.stderr_isatty
+        elif self.args.output_file:
+            # When not `--download`ing, then `--output` simply replaces
+            # `stdout`. The file is opened for appending, which isn't what
+            # we want in this case.
+            self.args.output_file.seek(0)
+            try:
+                self.args.output_file.truncate()
+            except IOError as e:
+                if e.errno == errno.EINVAL:
+                    # E.g. /dev/null on Linux.
+                    pass
+                else:
+                    raise
+            self.env.stdout = self.args.output_file
+            self.env.stdout_isatty = False
+    
+        xx = np.arange(100, 100 + n * step, step)
+    plt.figure('scikit-learn vs. glmnet benchmark results')
+    plt.title('Regression in high dimensional spaces (%d samples)' % n_samples)
+    plt.plot(xx, scikit_results, 'b-', label='scikit-learn')
+    plt.plot(xx, glmnet_results, 'r-', label='glmnet')
+    plt.legend()
+    plt.xlabel('number of features')
+    plt.ylabel('Time (s)')
+    plt.axis('tight')
+    plt.show()
+
+    
+    
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('metrics', nargs='*', default=sorted(METRICS),
+                    help='Specifies metrics to benchmark, defaults to all. '
+                         'Choices are: {}'.format(sorted(METRICS)))
+    ap.add_argument('--formats', nargs='+', choices=sorted(FORMATS),
+                    help='Specifies multilabel formats to benchmark '
+                         '(defaults to all).')
+    ap.add_argument('--samples', type=int, default=1000,
+                    help='The number of samples to generate')
+    ap.add_argument('--classes', type=int, default=10,
+                    help='The number of classes')
+    ap.add_argument('--density', type=float, default=.2,
+                    help='The average density of labels per sample')
+    ap.add_argument('--plot', choices=['classes', 'density', 'samples'],
+                    default=None,
+                    help='Plot time with respect to this parameter varying '
+                         'up to the specified value')
+    ap.add_argument('--n-steps', default=10, type=int,
+                    help='Plot this many points for each metric')
+    ap.add_argument('--n-times',
+                    default=5, type=int,
+                    help='Time performance over n_times trials')
+    args = ap.parse_args()
+    
+    import time
+    
+        class_name = info['fullname'].split('.')[0]
+    if type(class_name) != str:
+        # Python 2 only
+        class_name = class_name.encode('utf-8')
+    module = __import__(info['module'], fromlist=[class_name])
+    obj = attrgetter(info['fullname'])(module)
+    
+    try:
+    from urllib import urlopen
+except ImportError:
+    from urllib.request import urlopen
+    
+    import numpy as np
+from sklearn.covariance import EllipticEnvelope
+from sklearn.svm import OneClassSVM
+import matplotlib.pyplot as plt
+import matplotlib.font_manager
+from sklearn.datasets import load_boston
+    
+    model = SpectralBiclustering(n_clusters=n_clusters, method='log',
+                             random_state=0)
+model.fit(data)
+score = consensus_score(model.biclusters_,
+                        (rows[:, row_idx], columns[:, col_idx]))
+    
+    acc_clf1, acc_clf2 = [], []
+n_features_range = range(1, n_features_max + 1, step)
+for n_features in n_features_range:
+    score_clf1, score_clf2 = 0, 0
+    for _ in range(n_averages):
+        X, y = generate_data(n_train, n_features)
