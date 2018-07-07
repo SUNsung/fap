173431d20a03cb46c679e9221d956dee7c698ba0
SUNsung <weatherius@gmail.com>
2018-07-07 19:50:55 +0300

    Rewrite [33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb] File in: 2018-07-07 19:50:55

--- ruby/33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb
+++ ruby/33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb
@@ -1,3 +1,60 @@
 
         
-        
+          private
+    
+      def describe_system_ruby
+    s = ''
+    case RUBY_VERSION
+    when /^1\.[89]/, /^2\.0/
+      s << '#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL}'
+    else
+      s << RUBY_VERSION
+    end
+    
+            if msg = blacklisted?(query)
+          if count > 0
+            puts
+            puts 'If you meant #{query.inspect} precisely:'
+            puts
+          end
+          puts msg
+        elsif count == 0
+          puts 'No formula found for #{query.inspect}.'
+          begin
+            GitHub.print_pull_requests_matching(query)
+          rescue GitHub::Error => e
+            SEARCH_ERROR_QUEUE << e
+          end
+        end
+      end
+    end
+    
+      def self.bottle_sha1(*)
+  end
+    
+          it 'renders HTML' do
+        expect(render(options, screenshots)).to match(/<html>/)
+      end
+    end
+    
+    puts 'Deduping #{links.size} links...'
+    
+    ###
+### globals
+###
+    
+    desc 'Dumps output to a CSS file for testing'
+task :debug do
+  require 'sass'
+  path = Bootstrap.stylesheets_path
+  %w(bootstrap).each do |file|
+    engine = Sass::Engine.for_file('#{path}/#{file}.scss', syntax: :scss, load_paths: [path])
+    File.open('./#{file}.css', 'w') { |f| f.write(engine.render) }
+  end
+end
+    
+      desc 'Finished'
+  task :finished do
+  end
+    
+      mkdir_p deploy_dir
