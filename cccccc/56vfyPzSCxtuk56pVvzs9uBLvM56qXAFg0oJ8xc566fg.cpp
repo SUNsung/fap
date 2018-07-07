da8c194dce166bc3f827e0dd0a4923b01456642b
SUNsung <weatherius@gmail.com>
2018-07-07 05:22:54 +0300

    Rewrite [56vfyPzSCxtuk56pVvzs9uBLvM56qXAFg0oJ8xc566fg.cpp] File in: 2018-07-07 05:22:53

--- cccccc/56vfyPzSCxtuk56pVvzs9uBLvM56qXAFg0oJ8xc566fg.cpp
+++ cccccc/56vfyPzSCxtuk56pVvzs9uBLvM56qXAFg0oJ8xc566fg.cpp
@@ -1,156 +1,279 @@
 
         
-        void Timer::GetRealtimeTime(timespec &ts) {
-  gettime(CLOCK_REALTIME, &ts);
-}
-    
-    template<typename T>
-inline void
-emitTLSLoad(Vout& v, TLSDatum<ThreadLocalNoCheck<T>> datum, Vreg d) {
-  // We don't know for sure what's live.
-  PhysRegSaver(v, abi().gpUnreserved - abi().calleeSaved);
+        TEST(AnyTest, TestPackAndUnpack) {
+  protobuf_unittest::TestAny submessage;
+  submessage.set_int32_value(12345);
+  protobuf_unittest::TestAny message;
+  message.mutable_any_value()->PackFrom(submessage);
     }
     
-    Vlabel Vunit::makeScratchBlock() {
-  return makeBlock(AreaIndex::Main, 1);
+    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
+//   can only really compare the output against a golden value, which is a
+//   fairly tedious and fragile testing strategy.  If we want to go that route,
+//   it probably makes sense to bite the bullet and write a test that compares
+//   the whole generated output for unittest.proto against a golden value, with
+//   a very simple script that can be run to regenerate it with the latest code.
+//   This would mean that updates to the golden file would have to be included
+//   in any change to the code generator, which would actually be fairly useful
+//   as it allows the reviewer to see clearly how the generated code is
+//   changing.
+    
+        state->preFilterType = CV_STEREO_BM_XSOBEL; //CV_STEREO_BM_NORMALIZED_RESPONSE;
+    state->preFilterSize = 9;
+    state->preFilterCap = 31;
+    state->SADWindowSize = 15;
+    state->minDisparity = 0;
+    state->numberOfDisparities = numberOfDisparities > 0 ? numberOfDisparities : 64;
+    state->textureThreshold = 10;
+    state->uniquenessRatio = 15;
+    state->speckleRange = state->speckleWindowSize = 0;
+    state->trySmallerWindows = 0;
+    state->roi1 = state->roi2 = cvRect(0,0,0,0);
+    state->disp12MaxDiff = -1;
+    
+    protected:
+    void get_test_array_types_and_sizes( int test_case_idx, vector<vector<Size> >& sizes, vector<vector<int> >& types );
+    void fill_array( int test_case_idx, int i, int j, Mat& arr );
+    double get_success_error_level( int test_case_idx, int i, int j );
+    void run_func();
+    void prepare_to_validation( int test_case_idx );
+    
+            // Version: 3.0
+        COMPARE_REF_TO_TEXTURE           = 0x884E,
+        CLIP_DISTANCE0                   = 0x3000,
+        CLIP_DISTANCE1                   = 0x3001,
+        CLIP_DISTANCE2                   = 0x3002,
+        CLIP_DISTANCE3                   = 0x3003,
+        CLIP_DISTANCE4                   = 0x3004,
+        CLIP_DISTANCE5                   = 0x3005,
+        CLIP_DISTANCE6                   = 0x3006,
+        CLIP_DISTANCE7                   = 0x3007,
+        MAX_CLIP_DISTANCES               = 0x0D32,
+        MAJOR_VERSION                    = 0x821B,
+        MINOR_VERSION                    = 0x821C,
+        NUM_EXTENSIONS                   = 0x821D,
+        CONTEXT_FLAGS                    = 0x821E,
+        COMPRESSED_RED                   = 0x8225,
+        COMPRESSED_RG                    = 0x8226,
+        CONTEXT_FLAG_FORWARD_COMPATIBLE_BIT = 0x0001,
+        RGBA32F                          = 0x8814,
+        RGB32F                           = 0x8815,
+        RGBA16F                          = 0x881A,
+        RGB16F                           = 0x881B,
+        VERTEX_ATTRIB_ARRAY_INTEGER      = 0x88FD,
+        MAX_ARRAY_TEXTURE_LAYERS         = 0x88FF,
+        MIN_PROGRAM_TEXEL_OFFSET         = 0x8904,
+        MAX_PROGRAM_TEXEL_OFFSET         = 0x8905,
+        CLAMP_READ_COLOR                 = 0x891C,
+        FIXED_ONLY                       = 0x891D,
+        TEXTURE_1D_ARRAY                 = 0x8C18,
+        PROXY_TEXTURE_1D_ARRAY           = 0x8C19,
+        TEXTURE_2D_ARRAY                 = 0x8C1A,
+        PROXY_TEXTURE_2D_ARRAY           = 0x8C1B,
+        TEXTURE_BINDING_1D_ARRAY         = 0x8C1C,
+        TEXTURE_BINDING_2D_ARRAY         = 0x8C1D,
+        R11F_G11F_B10F                   = 0x8C3A,
+        UNSIGNED_INT_10F_11F_11F_REV     = 0x8C3B,
+        RGB9_E5                          = 0x8C3D,
+        UNSIGNED_INT_5_9_9_9_REV         = 0x8C3E,
+        TEXTURE_SHARED_SIZE              = 0x8C3F,
+        TRANSFORM_FEEDBACK_VARYING_MAX_LENGTH = 0x8C76,
+        TRANSFORM_FEEDBACK_BUFFER_MODE   = 0x8C7F,
+        MAX_TRANSFORM_FEEDBACK_SEPARATE_COMPONENTS = 0x8C80,
+        TRANSFORM_FEEDBACK_VARYINGS      = 0x8C83,
+        TRANSFORM_FEEDBACK_BUFFER_START  = 0x8C84,
+        TRANSFORM_FEEDBACK_BUFFER_SIZE   = 0x8C85,
+        PRIMITIVES_GENERATED             = 0x8C87,
+        TRANSFORM_FEEDBACK_PRIMITIVES_WRITTEN = 0x8C88,
+        RASTERIZER_DISCARD               = 0x8C89,
+        MAX_TRANSFORM_FEEDBACK_INTERLEAVED_COMPONENTS = 0x8C8A,
+        MAX_TRANSFORM_FEEDBACK_SEPARATE_ATTRIBS = 0x8C8B,
+        INTERLEAVED_ATTRIBS              = 0x8C8C,
+        SEPARATE_ATTRIBS                 = 0x8C8D,
+        TRANSFORM_FEEDBACK_BUFFER        = 0x8C8E,
+        TRANSFORM_FEEDBACK_BUFFER_BINDING = 0x8C8F,
+        RGBA32UI                         = 0x8D70,
+        RGB32UI                          = 0x8D71,
+        RGBA16UI                         = 0x8D76,
+        RGB16UI                          = 0x8D77,
+        RGBA8UI                          = 0x8D7C,
+        RGB8UI                           = 0x8D7D,
+        RGBA32I                          = 0x8D82,
+        RGB32I                           = 0x8D83,
+        RGBA16I                          = 0x8D88,
+        RGB16I                           = 0x8D89,
+        RGBA8I                           = 0x8D8E,
+        RGB8I                            = 0x8D8F,
+        RED_INTEGER                      = 0x8D94,
+        GREEN_INTEGER                    = 0x8D95,
+        BLUE_INTEGER                     = 0x8D96,
+        RGB_INTEGER                      = 0x8D98,
+        RGBA_INTEGER                     = 0x8D99,
+        BGR_INTEGER                      = 0x8D9A,
+        BGRA_INTEGER                     = 0x8D9B,
+        SAMPLER_1D_ARRAY                 = 0x8DC0,
+        SAMPLER_2D_ARRAY                 = 0x8DC1,
+        SAMPLER_1D_ARRAY_SHADOW          = 0x8DC3,
+        SAMPLER_2D_ARRAY_SHADOW          = 0x8DC4,
+        SAMPLER_CUBE_SHADOW              = 0x8DC5,
+        UNSIGNED_INT_VEC2                = 0x8DC6,
+        UNSIGNED_INT_VEC3                = 0x8DC7,
+        UNSIGNED_INT_VEC4                = 0x8DC8,
+        INT_SAMPLER_1D                   = 0x8DC9,
+        INT_SAMPLER_2D                   = 0x8DCA,
+        INT_SAMPLER_3D                   = 0x8DCB,
+        INT_SAMPLER_CUBE                 = 0x8DCC,
+        INT_SAMPLER_1D_ARRAY             = 0x8DCE,
+        INT_SAMPLER_2D_ARRAY             = 0x8DCF,
+        UNSIGNED_INT_SAMPLER_1D          = 0x8DD1,
+        UNSIGNED_INT_SAMPLER_2D          = 0x8DD2,
+        UNSIGNED_INT_SAMPLER_3D          = 0x8DD3,
+        UNSIGNED_INT_SAMPLER_CUBE        = 0x8DD4,
+        UNSIGNED_INT_SAMPLER_1D_ARRAY    = 0x8DD6,
+        UNSIGNED_INT_SAMPLER_2D_ARRAY    = 0x8DD7,
+        QUERY_WAIT                       = 0x8E13,
+        QUERY_NO_WAIT                    = 0x8E14,
+        QUERY_BY_REGION_WAIT             = 0x8E15,
+        QUERY_BY_REGION_NO_WAIT          = 0x8E16,
+        BUFFER_ACCESS_FLAGS              = 0x911F,
+        BUFFER_MAP_LENGTH                = 0x9120,
+        BUFFER_MAP_OFFSET                = 0x9121,
+    
+    static void* openclamdblas_check_fn(int ID)
+{
+    assert(ID >= 0 && ID < (int)(sizeof(openclamdblas_fn)/sizeof(openclamdblas_fn[0])));
+    const struct DynamicFnEntry* e = openclamdblas_fn[ID];
+    void* func = CV_CL_GET_PROC_ADDRESS(e->fnName);
+    if (!func)
+    {
+        throw cv::Exception(cv::Error::OpenCLApiCallError,
+                cv::format('OpenCL AMD BLAS function is not available: [%s]', e->fnName),
+                CV_Func, __FILE__, __LINE__);
+    }
+    *(e->ppFn) = func;
+    return func;
 }
     
-    void initNuma();
-    
-      void setFrontier(Address addr) {
-    assertx(m_base <= addr && addr <= (m_base + m_size));
-    m_frontier = addr;
+      /// (Deprecated: Use native_handle().) Get the native acceptor representation.
+  /**
+   * This function may be used to obtain the underlying representation of the
+   * acceptor. This is intended to allow access to native acceptor functionality
+   * that is not otherwise provided.
+   */
+  native_type native()
+  {
+    return this->get_service().native_handle(this->get_implementation());
   }
     
-    
-void Instruction::SetImmPCOffsetTarget(Instruction* target,
-                                       Instruction* from) {
-  auto adjusted_target = !from ? target : target + (int64_t)(this - from);
-  if (IsPCRelAddressing()) {
-    SetPCRelImmTarget(adjusted_target);
-  } else if (IsLoadOrStore()) {
-    SetPCRelLoadStoreTarget(adjusted_target);
-  } else {
-    SetBranchImmTarget(adjusted_target);
+    private:
+  /// Copy data out of the internal buffer to the specified target buffer.
+  /// Returns the number of bytes copied.
+  template <typename MutableBufferSequence>
+  std::size_t copy(const MutableBufferSequence& buffers)
+  {
+    std::size_t bytes_copied = boost::asio::buffer_copy(
+        buffers, storage_.data(), storage_.size());
+    storage_.consume(bytes_copied);
+    return bytes_copied;
   }
-}
     
-    /*!
- * \brief Registry entry for tree updater.
- */
-struct GradientBoosterReg
-    : public dmlc::FunctionRegEntryBase<
-  GradientBoosterReg,
-  std::function<GradientBooster* (const std::vector<std::shared_ptr<DMatrix> > &cached_mats,
-                                  bst_float base_margin)> > {
-};
+    #endif // BOOST_ASIO_BUFFERED_WRITE_STREAM_FWD_HPP
+
     
-    // implementing configure.
-template<typename PairIter>
-inline void Learner::Configure(PairIter begin, PairIter end) {
-  std::vector<std::pair<std::string, std::string> > vec(begin, end);
-  this->Configure(vec);
-}
+    // Standard library components can't be forward declared, so we'll have to
+// include the array header. Fortunately, it's fairly lightweight and doesn't
+// add significantly to the compile time.
+#if defined(BOOST_ASIO_HAS_STD_ARRAY)
+# include <array>
+#endif // defined(BOOST_ASIO_HAS_STD_ARRAY)
     
-      // extend the unique path
-  PathElement *unique_path = parent_unique_path + unique_depth + 1;
-  std::copy(parent_unique_path, parent_unique_path + unique_depth + 1, unique_path);
+    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
     
-    // try to load group information from file, if exists
-inline bool MetaTryLoadGroup(const std::string& fname,
-                             std::vector<unsigned>* group) {
-  std::unique_ptr<dmlc::Stream> fi(dmlc::Stream::Create(fname.c_str(), 'r', true));
-  if (fi == nullptr) return false;
-  dmlc::istream is(fi.get());
-  group->clear();
-  group->push_back(0);
-  unsigned nline = 0;
-  while (is >> nline) {
-    group->push_back(group->back() + nline);
-  }
-  return true;
-}
+    #include <boost/asio/detail/config.hpp>
+#include <boost/asio/completion_condition.hpp>
     
-    class SparsePageDMatrix : public DMatrix {
- public:
-  explicit SparsePageDMatrix(std::unique_ptr<DataSource>&& source,
-                             std::string  cache_info)
-      : source_(std::move(source)), cache_info_(std::move(cache_info)) {
-  }
+    // Helper class to determine whether or not the current thread is inside an
+// invocation of io_service::run() for a specified io_service object.
+template <typename Key, typename Value = unsigned char>
+class call_stack
+{
+public:
+  // Context class automatically pushes the key/value pair on to the stack.
+  class context
+    : private noncopyable
+  {
+  public:
+    // Push the key on to the stack.
+    explicit context(Key* k)
+      : key_(k),
+        next_(call_stack<Key, Value>::top_)
+    {
+      value_ = reinterpret_cast<unsigned char*>(this);
+      call_stack<Key, Value>::top_ = this;
+    }
+    }
     }
     
-        while (iter->Next()) {
-      page->Push(iter->Value());
-      if (page->MemCostBytes() >= kPageSize) {
-        bytes_write += page->MemCostBytes();
-        writer.PushWrite(std::move(page));
-        writer.Alloc(&page);
-        page->Clear();
-        double tdiff = dmlc::GetTime() - tstart;
-        LOG(CONSOLE) << 'Writing to ' << cache_info << ' in '
-                     << ((bytes_write >> 20UL) / tdiff) << ' MB/s, '
-                     << (bytes_write >> 20UL) << ' written';
+    #if !defined(BOOST_ASIO_HAS_THREADS)
+typedef null_event event;
+#elif defined(BOOST_ASIO_WINDOWS)
+typedef win_event event;
+#elif defined(BOOST_ASIO_HAS_PTHREADS)
+typedef posix_event event;
+#elif defined(BOOST_ASIO_HAS_STD_MUTEX_AND_CONDVAR)
+typedef std_event event;
+#endif
+    
+      // Find an entry in the map.
+  iterator find(const K& k)
+  {
+    if (num_buckets_)
+    {
+      size_t bucket = calculate_hash_value(k) % num_buckets_;
+      iterator it = buckets_[bucket].first;
+      if (it == values_.end())
+        return values_.end();
+      iterator end_it = buckets_[bucket].last;
+      ++end_it;
+      while (it != end_it)
+      {
+        if (it->first == k)
+          return it;
+        ++it;
       }
     }
-    if (page->data.size() != 0) {
-      writer.PushWrite(std::move(page));
-    }
-    
-      virtual const char* Name() const override;
-    
-    int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  // Run with regular database
-  int result;
-  {
-    fprintf(stderr, 'Running tests with regular db and operator.\n');
-    StringAppendOperatorTest::SetOpenDbFunction(&OpenNormalDb);
-    result = RUN_ALL_TESTS();
+    return values_.end();
   }
-    }
     
-      // Create a reader that will return log records from '*file'.
-  // '*file' must remain live while this Reader is in use.
-  //
-  // If 'reporter' is non-nullptr, it is notified whenever some data is
-  // dropped due to a detected corruption.  '*reporter' must remain
-  // live while this Reader is in use.
-  //
-  // If 'checksum' is true, verify checksums if available.
-  //
-  // The Reader will start reading at the first record located at physical
-  // position >= initial_offset within the file.
-  Reader(std::shared_ptr<Logger> info_log,
-  // @lint-ignore TXT2 T25377293 Grandfathered in
-	 unique_ptr<SequentialFileReader>&& file,
-         Reporter* reporter, bool checksum, uint64_t initial_offset,
-         uint64_t log_num);
-    
-    void JniCallback::releaseJniEnv(jboolean& attached) const {
-  JniUtil::releaseJniEnv(m_jvm, attached);
+    void buffer_sequence_adapter_base::init_native_buffer(
+    buffer_sequence_adapter_base::native_buffer_type& buf,
+    const boost::asio::mutable_buffer& buffer)
+{
+  std::memset(&buf, 0, sizeof(native_buffer_type));
+  Microsoft::WRL::ComPtr<IInspectable> insp
+    = Microsoft::WRL::Make<winrt_buffer_impl>(buffer);
+  buf = reinterpret_cast<Windows::Storage::Streams::IBuffer^>(insp.Get());
 }
     
-      bool IsKeyPinned() const { return (key_ != buf_); }
+    template <typename Time_Traits>
+void epoll_reactor::remove_timer_queue(timer_queue<Time_Traits>& queue)
+{
+  do_remove_timer_queue(queue);
+}
     
-      virtual uint64_t ApproximateNumEntries(const Slice& /*start_ikey*/,
-                                         const Slice& /*end_key*/) {
-    return 0;
-  }
+    #include <vector>
+#include <string>
     
-      jstring jname =
-      (jstring)env->CallObjectMethod(m_jcallback_obj, jname_method_id);
-  if(env->ExceptionCheck()) {
-    // exception thrown
-    return;
-  }
-  jboolean has_exception = JNI_FALSE;
-  m_name = JniUtil::copyString(env, jname, &has_exception);  // also releases jname
-  if (has_exception == JNI_TRUE) {
-    // exception thrown
-    return;
-  }
+    WakeUpLock::WakeUpLock() {
+    object_ = ::wakeupLock_new();
+    ASSERT(object_);
+    xinfo2('new wakeuplock:%p', object_);
+}
     
-    #include 'rocksdb/compaction_filter.h'
-#include 'rocksjni/jnicallback.h'
+    #endif /* WAKEUPLOCK_H_ */
+
     
-    #endif  // JAVA_ROCKSJNI_LOGGERJNICALLBACK_H_
+    #ifndef SCOPEDJSTRING_H_
+#define SCOPEDJSTRING_H_
