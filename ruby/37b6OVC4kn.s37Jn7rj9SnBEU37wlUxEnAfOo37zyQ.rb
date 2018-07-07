e17bd7fafe7c499e3f34362efbaf14ebd6d2f49d
SUNsung <weatherius@gmail.com>
2018-07-07 15:42:55 +0300

    Rewrite [37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb] File in: 2018-07-07 15:42:54

--- ruby/37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb
+++ ruby/37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb
@@ -1,56 +1,18 @@
 
         
-              sets.map do |set|
-        if valid?(set)
-          ensure_time!(update_deprecated_types(set))
-        else
-          Jekyll.logger.warn 'Defaults:', 'An invalid front-matter default set was found:'
-          Jekyll.logger.warn set.to_s
-          nil
-        end
-      end.compact
+            respond_to do |format|
+      if @book.save
+        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        format.json { render :show, status: :created, location: @book }
+      else
+        format.html { render :new }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
     end
+  end
     
-    unless File.exist?(PROF_OUTPUT_FILE)
-  StackProf.run(
-    mode: MODE.to_sym,
-    interval: 100,
-    raw: true,
-    out: PROF_OUTPUT_FILE
-  ) do
-    puts 'GC Stats:', JSON.pretty_generate(GC.stat)
-    GC.disable
-    
-    require 'json'
-require 'open-uri'
-    
-        def deprecation_message(message)
-      Jekyll.logger.warn 'Deprecation:', message
-    end
-    
-          def parse(content)
-        measure_time do
-          @template = Liquid::Template.parse(content, :line_numbers => true)
+            def used?
+          @variable.captured_by_block? || @referenced
         end
     
-        # Spaceship is priority [higher -> lower]
-    #
-    # other - The class to be compared.
-    #
-    # Returns -1, 0, 1.
-    def self.<=>(other)
-      PRIORITIES[other.priority] <=> PRIORITIES[self.priority]
-    end
-    
-    class SinatraStaticServer < Sinatra::Base
-    
-    def config_tag(config, key, tag=nil, classname=nil)
-  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
-  tag       ||= 'div'
-  classname ||= key.sub(/_/, '-').sub(/\./, '-')
-  output      = '<#{tag} class='#{classname}''
-    
-    require 'cgi'
-require 'digest/md5'
-require 'net/https'
-require 'uri'
+            private
