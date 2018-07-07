afa32af522490934c73acc0c333e8809e503f354
SUNsung <weatherius@gmail.com>
2018-07-07 03:37:26 +0300

    Rewrite [00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp] File in: 2018-07-07 03:37:25

--- cccccc/00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp
+++ cccccc/00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp
@@ -1,5 +1,181 @@
 
         
-        
-    {	return 0;
+        template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9, typename T10,
+    typename T11, typename T12>
+internal::ValueArray12<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
+    T12> Values(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9,
+    T10 v10, T11 v11, T12 v12) {
+  return internal::ValueArray12<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
+      T12>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12);
+}
+    
+    // Internal macro for implementing {EXPECT|ASSERT}_PRED2.  Don't use
+// this in your code.
+#define GTEST_PRED2_(pred, v1, v2, on_failure)\
+  GTEST_ASSERT_(::testing::AssertPred2Helper(#pred, \
+                                             #v1, \
+                                             #v2, \
+                                             pred, \
+                                             v1, \
+                                             v2), on_failure)
+    
+      // Returns the current working directory, or '' if unsuccessful.
+  static FilePath GetCurrentDir();
+    
+    
+    {    const ParamGeneratorInterface<T>* const base_;
+    typename ContainerType::const_iterator iterator_;
+    // A cached value of *iterator_. We keep it here to allow access by
+    // pointer in the wrapping iterator's operator->().
+    // value_ needs to be mutable to be accessed in Current().
+    // Use of scoped_ptr helps manage cached value's lifetime,
+    // which is bound by the lifespan of the iterator itself.
+    mutable scoped_ptr<const T> value_;
+  };  // class ValuesInIteratorRangeGenerator::Iterator
+    
+    
+    {
+    {      // Marks all multiples of i (except i itself) as non-prime.
+      for (int j = 2*i; j <= max; j += i) {
+        is_prime_[j] = false;
+      }
+    }
+  }
+    
+    int getifaddrs(struct ifaddrs** result) {
+	int fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
+	if (fd < 0) {
+		return -1;
+	}
+	netlinkrequest ifaddr_request;
+	memset(&ifaddr_request, 0, sizeof(ifaddr_request));
+	ifaddr_request.header.nlmsg_flags = NLM_F_ROOT | NLM_F_REQUEST;
+	ifaddr_request.header.nlmsg_type = RTM_GETADDR;
+	ifaddr_request.header.nlmsg_len = NLMSG_LENGTH(sizeof(ifaddrmsg));
+	ssize_t count = send(fd, &ifaddr_request, ifaddr_request.header.nlmsg_len, 0);
+	if (static_cast<size_t>(count) != ifaddr_request.header.nlmsg_len) {
+		close(fd);
+		return -1;
+	}
+	struct ifaddrs* start = NULL;
+	struct ifaddrs* current = NULL;
+	char buf[kMaxReadSize];
+	ssize_t amount_read = recv(fd, &buf, kMaxReadSize, 0);
+	while (amount_read > 0) {
+		nlmsghdr* header = reinterpret_cast<nlmsghdr*>(&buf[0]);
+		size_t header_size = static_cast<size_t>(amount_read);
+		for ( ; NLMSG_OK(header, header_size);
+		      header = NLMSG_NEXT(header, header_size)) {
+			switch (header->nlmsg_type) {
+			case NLMSG_DONE:
+				// Success. Return.
+				*result = start;
+				close(fd);
+				return 0;
+			case NLMSG_ERROR:
+				close(fd);
+				freeifaddrs(start);
+				return -1;
+			case RTM_NEWADDR: {
+				ifaddrmsg* address_msg =
+						reinterpret_cast<ifaddrmsg*>(NLMSG_DATA(header));
+				rtattr* rta = IFA_RTA(address_msg);
+				ssize_t payload_len = IFA_PAYLOAD(header);
+				while (RTA_OK(rta, payload_len)) {
+					if (rta->rta_type == IFA_ADDRESS) {
+						int family = address_msg->ifa_family;
+						if (family == AF_INET || family == AF_INET6) {
+							ifaddrs* newest = new ifaddrs;
+							memset(newest, 0, sizeof(ifaddrs));
+							if (current) {
+								current->ifa_next = newest;
+							} else {
+								start = newest;
+							}
+							if (populate_ifaddrs(newest, address_msg, RTA_DATA(rta),
+									     RTA_PAYLOAD(rta)) != 0) {
+								freeifaddrs(start);
+								*result = NULL;
+								return -1;
+							}
+							current = newest;
+						}
+					}
+					rta = RTA_NEXT(rta, payload_len);
+				}
+				break;
+			}
+			}
+		}
+		amount_read = recv(fd, &buf, kMaxReadSize, 0);
+	}
+	close(fd);
+	freeifaddrs(start);
+	return -1;
+}
+    
+      if (m_comps_in_scan == 1)
+    m_block_y_mcu[m_comp_list[0]]++;
+  else
+  {
+    for (component_num = 0; component_num < m_comps_in_scan; component_num++)
+    {
+      component_id = m_comp_list[component_num];
+    }
+    }
+    
+    /** 16x32 multiplication, followed by a 16-bit shift right. Results fits in 32 bits */
+#undef MULT16_32_Q16
+static OPUS_INLINE opus_val32 MULT16_32_Q16_armv4(opus_val16 a, opus_val32 b)
+{
+  unsigned rd_lo;
+  int rd_hi;
+  __asm__(
+      '#MULT16_32_Q16\n\t'
+      'smull %0, %1, %2, %3\n\t'
+      : '=&r'(rd_lo), '=&r'(rd_hi)
+      : '%r'(b),'r'(a<<16)
+  );
+  return rd_hi;
+}
+#define MULT16_32_Q16(a, b) (MULT16_32_Q16_armv4(a, b))
+    
+    #elif (defined(OPUS_X86_MAY_HAVE_SSE) && !defined(OPUS_X86_PRESUME_SSE)) || \
+  (defined(OPUS_X86_MAY_HAVE_SSE2) && !defined(OPUS_X86_PRESUME_SSE2)) || \
+  (defined(OPUS_X86_MAY_HAVE_SSE4_1) && !defined(OPUS_X86_PRESUME_SSE4_1)) || \
+  (defined(OPUS_X86_MAY_HAVE_AVX) && !defined(OPUS_X86_PRESUME_AVX))
+    
+    	wchar_t commandStr[MAX_PATH + 20] = { 0 };
+	swprintf_s(commandStr, L'\'%s\' \'%%V\'', exePath);
+    
+    
+    {
+    {
+    {  inline void PutChar(char ch) {
+    out_buf += ch;
+    if (out_buf.length() >= kBufferSize) Flush();
+  }
+  inline void Flush(void) {
+    if (out_buf.length() != 0) {
+      fp->Write(&out_buf[0], out_buf.length());
+      out_buf.clear();
+    }
+  }
+};
+}  // namespace common
+}  // namespace xgboost
+#endif  // XGBOOST_COMMON_BASE64_H_
+
+    
+    
+    {  delete metric;
+  metric = xgboost::Metric::Create('error@0.1');
+  ASSERT_STREQ(metric->Name(), 'error@0.1');
+  EXPECT_STREQ(metric->Name(), 'error@0.1');
+  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 0, 1e-10);
+  EXPECT_NEAR(GetMetricEval(metric,
+                            {0.1f, 0.2f, 0.1f, 0.2f},
+                            {  0,   0,   1,   1}),
+              0.5f, 0.001f);
 }
