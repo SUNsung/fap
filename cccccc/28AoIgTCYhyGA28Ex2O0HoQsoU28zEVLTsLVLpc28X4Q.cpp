98b3702985cdd4a561127f6137a3a77634b18921
SUNsung <weatherius@gmail.com>
2018-07-07 10:10:35 +0300

    Rewrite [28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp] File in: 2018-07-07 10:10:34

--- cccccc/28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp
+++ cccccc/28AoIgTCYhyGA28Ex2O0HoQsoU28zEVLTsLVLpc28X4Q.cpp
@@ -1,113 +1,140 @@
 
         
-          llvm::Optional<CommentParts> getParts() const {
-    return Parts;
+        #if defined(USE_LEVELDB) && defined(USE_LMDB)
+#include <leveldb/db.h>
+#include <leveldb/write_batch.h>
+#include <lmdb.h>
+#endif
+    
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
+    
+      /**
+   * @brief Computes the error gradient w.r.t. the concatenate inputs.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *        respect to the outputs
+   *   -# @f$ (KN \times C \times H \times W) @f$ if axis == 0, or
+   *      @f$ (N \times KC \times H \times W) @f$ if axis == 1:
+   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
+   *      with respect to concatenated outputs @f$ y @f$
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length K), into which the top gradient
+   *        @f$ \frac{\partial E}{\partial y} @f$ is deconcatenated back to the
+   *        inputs @f$
+   *        \left[ \begin{array}{cccc}
+   *          \frac{\partial E}{\partial x_1} &
+   *          \frac{\partial E}{\partial x_2} &
+   *          ... &
+   *          \frac{\partial E}{\partial x_K}
+   *        \end{array} \right] =
+   *        \frac{\partial E}{\partial y}
+   *        @f$
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    
+    {  bool handles_setup_;
+  cudnnHandle_t             handle_;
+  cudnnTensorDescriptor_t bottom_desc_;
+  cudnnTensorDescriptor_t top_desc_;
+  cudnnActivationDescriptor_t activ_desc_;
+};
+#endif
+    
+    namespace caffe {
+    }
+    
+    #ifndef GTEST_INCLUDE_GTEST_GTEST_SPI_H_
+#define GTEST_INCLUDE_GTEST_GTEST_SPI_H_
+    
+    // Internal macro for implementing {EXPECT|ASSERT}_PRED_FORMAT3.
+// Don't use this in your code.
+#define GTEST_PRED_FORMAT3_(pred_format, v1, v2, v3, on_failure)\
+  GTEST_ASSERT_(pred_format(#v1, #v2, #v3, v1, v2, v3), \
+                on_failure)
+    
+    // In order to catch the mistake of putting tests that use different
+// test fixture classes in the same test case, we need to assign
+// unique IDs to fixture classes and compare them.  The TypeId type is
+// used to hold such IDs.  The user should treat TypeId as an opaque
+// type: the only operation allowed on TypeId values is to compare
+// them for equality using the == operator.
+typedef const void* TypeId;
+    
+    // Type lists of length 1, 2, 3, and so on.
+    
+    
+    {
+    {}  // namespace log
+}  // namespace leveldb
+    
+    TEST(LogTest, AlignedEof) {
+  const int n = kBlockSize - 2*kHeaderSize + 4;
+  Write(BigString('foo', n));
+  ASSERT_EQ(kBlockSize - kHeaderSize + 4, WrittenBytes());
+  ASSERT_EQ(BigString('foo', n), Read());
+  ASSERT_EQ('EOF', Read());
+}
+    
+      for (size_t i = 0; i < 3; ++i) {
+    std::string res;
+    ASSERT_OK(db->Get(ReadOptions(), keys[i], &res));
+    ASSERT_TRUE(res == vals[i]);
   }
     
-    namespace swift {
-namespace index {
-    }
+    class Issue178 { };
+    
+        // All added keys must match
+    for (int i = 0; i < length; i++) {
+      ASSERT_TRUE(Matches(Key(i, buffer)))
+          << 'Length ' << length << '; key ' << i;
     }
     
-    /// LSLocation and LSValue are used in DenseMap.
-namespace llvm {
-using swift::LSBase;
-using swift::LSLocation;
-using swift::LSValue;
-    }
     
-        D3D_FEATURE_LEVEL featureLevels[] =
-    {
-        D3D_FEATURE_LEVEL_11_0,
-        D3D_FEATURE_LEVEL_10_1,
-        D3D_FEATURE_LEVEL_10_0,
-    };
-    UINT numFeatureLevels = ARRAYSIZE(featureLevels);
-    
-    static void findLinesCrossPoint(Point2f origin1, Point2f dir1, Point2f origin2, Point2f dir2, Point2f& cross_point)
-{
-    float det = dir2.x*dir1.y - dir2.y*dir1.x;
-    Point2f offset = origin2 - origin1;
-    }
-    
-    namespace cv { namespace cuda { namespace device
-{
-    template <class T>
-    __device__ __forceinline__ T warp_reduce(volatile T *ptr , const unsigned int tid = threadIdx.x)
-    {
-        const unsigned int lane = tid & 31; // index of thread in warp (0..31)
-    }
-    }
-    }
-    }
-    
-    //
-// END OF CUSTOM FUNCTIONS HERE
-//
-    
-    void OutputImageComponent::UpdatePixelsForBlock(
-    int block_x, int block_y, const uint8_t idct[kDCTBlockSize]) {
-  if (factor_x_ == 1 && factor_y_ == 1) {
-    for (int iy = 0; iy < 8; ++iy) {
-      for (int ix = 0; ix < 8; ++ix) {
-        int x = 8 * block_x + ix;
-        int y = 8 * block_y + iy;
-        if (x >= width_ || y >= height_) continue;
-        int p = y * width_ + x;
-        pixels_[p] = idct[8 * iy + ix] << 4;
-      }
-    }
-  } else if (factor_x_ == 2 && factor_y_ == 2) {
-    // Fill in the 10x10 pixel area in the subsampled image that will be the
-    // basis of the upsampling. This area is enough to hold the 3x3 kernel of
-    // the fancy upsampler around each pixel.
-    static const int kSubsampledEdgeSize = 10;
-    uint16_t subsampled[kSubsampledEdgeSize * kSubsampledEdgeSize];
-    for (int j = 0; j < kSubsampledEdgeSize; ++j) {
-      // The order we fill in the rows is:
-      //   8 rows intersecting the block, row below, row above
-      const int y0 = block_y * 16 + (j < 9 ? j * 2 : -2);
-      for (int i = 0; i < kSubsampledEdgeSize; ++i) {
-        // The order we fill in each row is:
-        //   8 pixels within the block, left edge, right edge
-        const int ix = ((j < 9 ? (j + 1) * kSubsampledEdgeSize : 0) +
-                        (i < 9 ? i + 1 : 0));
-        const int x0 = block_x * 16 + (i < 9 ? i * 2 : -2);
-        if (x0 < 0) {
-          subsampled[ix] = subsampled[ix + 1];
-        } else if (y0 < 0) {
-          subsampled[ix] = subsampled[ix + kSubsampledEdgeSize];
-        } else if (x0 >= width_) {
-          subsampled[ix] = subsampled[ix - 1];
-        } else if (y0 >= height_) {
-          subsampled[ix] = subsampled[ix - kSubsampledEdgeSize];
-        } else if (i < 8 && j < 8) {
-          subsampled[ix] = idct[j * 8 + i] << 4;
-        } else {
-          // Reconstruct the subsampled pixels around the edge of the current
-          // block by computing the inverse of the fancy upsampler.
-          const int y1 = std::max(y0 - 1, 0);
-          const int x1 = std::max(x0 - 1, 0);
-          subsampled[ix] = (pixels_[y0 * width_ + x0] * 9 +
-                            pixels_[y1 * width_ + x1] +
-                            pixels_[y0 * width_ + x1] * -3 +
-                            pixels_[y1 * width_ + x0] * -3) >> 2;
-        }
-      }
-    }
-    }
-    }
-    
-    #ifndef GUETZLI_ENTROPY_ENCODE_H_
-#define GUETZLI_ENTROPY_ENCODE_H_
-    
-    
-    {}  // namespace guetzli
-    
-    #endif  // GUETZLI_JPEG_HUFFMAN_DECODE_H_
+#endif  // STORAGE_LEVELDB_UTIL_MUTEXLOCK_H_
 
     
-    // Preprocesses the u (1) or v (2) channel of the given YUV image (range 0-255).
-std::vector<std::vector<float>> PreProcessChannel(
-    int w, int h, int channel, float sigma, float amount, bool blur,
-    bool sharpen, const std::vector<std::vector<float>>& image);
+    LookupKey::LookupKey(const Slice& user_key, SequenceNumber s) {
+  size_t usize = user_key.size();
+  size_t needed = usize + 13;  // A conservative estimate
+  char* dst;
+  if (needed <= sizeof(space_)) {
+    dst = space_;
+  } else {
+    dst = new char[needed];
+  }
+  start_ = dst;
+  dst = EncodeVarint32(dst, usize + 8);
+  kstart_ = dst;
+  memcpy(dst, user_key.data(), usize);
+  dst += usize;
+  EncodeFixed64(dst, PackSequenceAndType(s, kValueTypeForSeek));
+  dst += 8;
+  end_ = dst;
+}
+    
+    class StdoutPrinter : public WritableFile {
+ public:
+  virtual Status Append(const Slice& data) {
+    fwrite(data.data(), 1, data.size(), stdout);
+    return Status::OK();
+  }
+  virtual Status Close() { return Status::OK(); }
+  virtual Status Flush() { return Status::OK(); }
+  virtual Status Sync() { return Status::OK(); }
+};
+    
+      Status AddRecord(const Slice& slice);
+    
+    struct Options;
