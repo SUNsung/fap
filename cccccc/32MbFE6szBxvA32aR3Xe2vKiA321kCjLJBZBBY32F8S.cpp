d1995fef06485cf143edf5e01f016d0af078d748
SUNsung <weatherius@gmail.com>
2018-07-07 22:26:42 +0300

    Rewrite [32MbFE6szBxvA32aR3Xe2vKiA321kCjLJBZBBY32F8S.cpp] File in: 2018-07-07 22:26:41

--- cccccc/32MbFE6szBxvA32aR3Xe2vKiA321kCjLJBZBBY32F8S.cpp
+++ cccccc/32MbFE6szBxvA32aR3Xe2vKiA321kCjLJBZBBY32F8S.cpp
@@ -1,9 +1,60 @@
 
         
-          void set_status(const Status& status) {
-    status_ = status;
-    invoked_.Notify();
+        struct StatusTestParam {
+  Status executor_status;
+  Status partial_run_status;
+  Status expected_status;
+};
+    
+    class SmoothHingeLossUpdater : public DualLossUpdater {
+ public:
+  // Computes the updated dual variable (corresponding) to a single example. The
+  // updated dual value maximizes the objective function of the dual
+  // optimization problem associated with smooth hinge loss. The computations
+  // are detailed in readme.md.
+  double ComputeUpdatedDual(const int num_partitions, const double label,
+                            const double example_weight,
+                            const double current_dual, const double wx,
+                            const double weighted_example_norm) const final {
+    // Intutitvely there are 3 cases:
+    // a. new optimal value of the dual variable falls within the admissible
+    // range [0, 1]. In this case we set new dual to this value.
+    // b. new optimal value is < 0. Then, because of convexity, the optimal
+    // valid value for new dual = 0
+    // c. new optimal value > 1.0. Then new optimal value should be set to 1.0.
+    const double candidate_optimal_dual =
+        current_dual +
+        (label - wx - gamma * current_dual) /
+            (num_partitions * example_weight * weighted_example_norm + gamma);
+    if (label * candidate_optimal_dual < 0) {
+      return 0.0;
+    }
+    if (label * candidate_optimal_dual > 1.0) {
+      return label;
+    }
+    return candidate_optimal_dual;
   }
+    }
+    
+    /**
+ * \ingroup CXX11_NeuralNetworks_Module
+ * \brief Template functor to clip the magnitude of the first scalar.
+ *
+ * \sa class CwiseBinaryOp, MatrixBase::Clip
+ */
+template <typename Scalar>
+struct scalar_clip_op {
+  EIGEN_EMPTY_STRUCT_CTOR(scalar_clip_op)
+  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE const Scalar
+  operator()(const Scalar& a, const Scalar& b) const {
+    return numext::mini(numext::maxi(a, -b), b);
+  }
+  template <typename Packet>
+  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE const Packet
+  packetOp(const Packet& a, const Packet& b) const {
+    return internal::pmin(internal::pmax(a, internal::pnegate(b)), b);
+  }
+};
     
     Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an 'AS IS' BASIS,
@@ -12,214 +63,199 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
     
-      // Abort and close all existing sessions, disconnecting their resources from
-  // future sessions.
-  //
-  // Reset() allows misbehaving or slow sessions to be aborted and closed, and
-  // causes their resources eventually to be released.  Reset() does not wait
-  // for the computations in old sessions to cease; it merely starts the
-  // process of tearing them down.  However, if a new session is started after
-  // a Reset(), the new session is isolated from changes that old sessions
-  // (started prior to the Reset()) may continue to make to resources, provided
-  // all those resources are in containers listed in 'containers'.
-  //
-  // Old sessions may continue to have side-effects on resources not in
-  // containers listed in 'containers', and thus may affect future
-  // sessions' results in ways that are hard to predict.  Thus, if well-defined
-  // behavior is desired, is it recommended that all containers be listed in
-  // 'containers'.
-  //
-  // If the 'containers' vector is empty, the default container is assumed.
-  // If the 'containers' vector is non-empty, the default container should be
-  // listed explicitly.
-  //
-  // Sessions that support resource containers should override this function.
-  virtual Status Reset(const SessionOptions& options,
-                       const std::vector<string>& containers) {
-    return errors::Unimplemented('Reset()');
-  }
+    #include 'tensorflow/core/framework/register_types.h'
+#include 'tensorflow/core/framework/tensor_types.h'
+#include 'tensorflow/core/kernels/cuda_device_array_gpu.h'
+#include 'tensorflow/core/util/cuda_kernel_helper.h'
     
-    #ifndef TENSORFLOW_COMMON_RUNTIME_SYCL_SYCL_DEVICE_CONTEXT_H_
-#define TENSORFLOW_COMMON_RUNTIME_SYCL_SYCL_DEVICE_CONTEXT_H_
+    MPIUtils::MPIUtils(const std::string& worker_name) {
+  InitMPI();
+  // Connect the MPI process IDs to the worker names that are used by TF.
+  // Gather the names of all the active processes (name can't be longer than
+  // 128 bytes)
+  int proc_id = 0, number_of_procs = 1;
+  char my_name[max_worker_name_length];
+  MPI_CHECK(MPI_Comm_rank(MPI_COMM_WORLD, &proc_id));
+  MPI_CHECK(MPI_Comm_size(MPI_COMM_WORLD, &number_of_procs));
+    }
     
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    // TODO: Enable GPU support for angle op after resolving
+// build failures on GPU (See #10643 for context).
+#if 0 && GOOGLE_CUDA
+    
+    TEST_F(GrpcDebugTest, SendDebugTensorWithLargeStringAtIndex1ViaGrpcTest) {
+  Tensor tensor(DT_STRING, TensorShape({1, 2}));
+  tensor.flat<string>()(0) = 'A';
+  tensor.flat<string>()(1) = string(5000 * 1024, 'A');
+  const DebugNodeKey kDebugNodeKey('/job:localhost/replica:0/task:0/cpu:0',
+                                   'foo_tensor', 0, 'DebugIdentity');
+  const Status status = DebugIO::PublishDebugTensor(
+      kDebugNodeKey, tensor, Env::Default()->NowMicros(), {server_data_.url});
+  ASSERT_FALSE(status.ok());
+  ASSERT_NE(status.error_message().find('string value at index 1 from debug '
+                                        'node foo_tensor:0:DebugIdentity does '
+                                        'not fit gRPC message size limit'),
+            string::npos);
+  TF_ASSERT_OK(DebugIO::CloseDebugURL(server_data_.url));
+}
+    
+    TEST(AccuracyUtilsTest, ReadGroundTruthFile) {
+  string file_name = tensorflow::io::JoinPath(tensorflow::testing::TmpDir(),
+                                              'ground_truth.txt');
+  string file_data = 'a,10\nb,12\n';
+  TF_ASSERT_OK(WriteStringToFile(Env::Default(), file_name, file_data));
+    }
     
     
-    {  // Invalid names.
-  EXPECT_FALSE(IsSameAddrSp('random_invalid_target', 'random_invalid_target'));
-  EXPECT_FALSE(IsSameAddrSp('/job:/replica:10/task:10/cpu:0',
-                            '/job:/replica:10/task:10/cpu:1'));
-  EXPECT_FALSE(IsSameAddrSp('/job:mnist/replica:xx/task:10/cpu:0',
-                            '/job:mnist/replica:xx/task:10/cpu:1'));
-  EXPECT_FALSE(IsSameAddrSp('/job:mnist/replica:10/task:yy/cpu:0',
-                            '/job:mnist/replica:10/task:yy/cpu:1'));
+    {  /* Copy the output layer to a std::vector */
+  Blob<float>* output_layer = net_->output_blobs()[0];
+  const float* begin = output_layer->cpu_data();
+  const float* end = begin + output_layer->channels();
+  return std::vector<float>(begin, end);
 }
     
     
-    {  if (proc_id == 0) LOG(INFO) << 'MPI process-ID to gRPC server name map: \n';
-  for (int i = 0; i < number_of_procs; i++) {
-    name_to_id_[std::string(&worker_names[i * 128])] = i;
-    if (proc_id == 0)
-      LOG(INFO) << 'Process: ' << i
-                << '\tgRPC-name: ' << std::string(&worker_names[i * 128])
-                << std::endl;
+    {  if (argc != 4) {
+    gflags::ShowUsageWithFlagsRestrict(argv[0],
+        'examples/mnist/convert_mnist_data');
+  } else {
+    google::InitGoogleLogging(argv[0]);
+    convert_dataset(argv[1], argv[2], argv[3], db_backend);
   }
+  return 0;
 }
+#else
+int main(int argc, char** argv) {
+  LOG(FATAL) << 'This example requires LevelDB and LMDB; ' <<
+  'compile with USE_LEVELDB and USE_LMDB.';
+}
+#endif  // USE_LEVELDB and USE_LMDB
+
     
-      // Given slice 'sub' where 'sub' is fully contained in *this,
-  // (meaning that the intersection of 'sub' and *this equals 'sub'), computes
-  // the 'relative' slice of 'sub' with respect to *this.
-  //
-  // In other words, if we use A>S to denote slicing a shape S with a slice A,
-  // then the function is computing a slice X such that:
-  //   X > (this > S) = sub > S
-  // for any shape S.
-  //
-  // In general, along every dimension, the start of the relative slice is the
-  // start of the 'sub' slice minus the start of *this; the length of the
-  // relative slice is the length of the 'sub' slice.
-  //
-  // For example, say we have a shape of {3, 4, 5}, 'this' is 0,2:-:1,2, and
-  // 'sub' is 1,1:2:2,1,2, then the related slice is 1,1:2,2:0,2.
-  //
-  // The caller needs to make sure that 'sub' is indeed a sub-slice of *this;
-  // otherwise the result is undefined.
-  void ComputeRelative(const TensorSlice& sub, TensorSlice* relative) const;
-    
-    namespace tensorflow {
-#define REGISTER_COMPLEX(D, R, C)                         \
-  REGISTER_KERNEL_BUILDER(Name('Angle')                   \
-                              .Device(DEVICE_##D)         \
-                              .TypeConstraint<C>('T')     \
-                              .TypeConstraint<R>('Tout'), \
-                          UnaryOp<D##Device, functor::get_angle<C>>);
-    }
-    
-    #include 'tensorflow/core/kernels/cwise_ops_gpu_common.cu.h'
-    
-    // Adds resource events for a single device.
-void AddResourceMetadata(uint32 device_id,
-                         const std::map<uint32, const Resource *> &resources,
-                         string *json) {
-  for (const auto &pair : resources) {
-    uint32 resource_id = pair.first;
-    const Resource &resource = *pair.second;
-    if (!resource.name().empty()) {
-      Appendf(json,
-              R'({'ph':'M','pid':%u,'tid':%u,)'
-              R'('name':'thread_name','args':{)',
-              device_id, resource_id);
-      AppendEscapedName(json, resource.name());
-      Appendf(json, '}},');
-    }
-    Appendf(json,
-            R'({'ph':'M','pid':%u,'tid':%u,)'
-            R'('name':'thread_sort_index','args':{'sort_index':%u}},)',
-            device_id, resource_id, resource_id);
+      /// @brief The spatial dimensions of the input.
+  inline int input_shape(int i) {
+    return (*bottom_shape_)[channel_axis_ + i];
   }
-}
+  // reverse_dimensions should return true iff we are implementing deconv, so
+  // that conv helpers know which dimensions are which.
+  virtual bool reverse_dimensions() = 0;
+  // Compute height_out_ and width_out_ from other parameters.
+  virtual void compute_output_shape() = 0;
     
-    std::vector<float> Classifier::Predict(const cv::Mat& img) {
-  Blob<float>* input_layer = net_->input_blobs()[0];
-  input_layer->Reshape(1, num_channels_,
-                       input_geometry_.height, input_geometry_.width);
-  /* Forward dimension change to all layers. */
-  net_->Reshape();
-    }
+    #include <vector>
     
-      // Open leveldb
-  leveldb::DB* db;
-  leveldb::Options options;
-  options.create_if_missing = true;
-  options.error_if_exists = true;
-  leveldb::Status status = leveldb::DB::Open(
-      options, db_filename, &db);
-  CHECK(status.ok()) << 'Failed to open leveldb ' << db_filename
-      << '. Is it already existing?';
+    #include <vector>
     
-    #include 'caffe/layers/neuron_layer.hpp'
-    
-    /**
- * @brief Abstract base class that factors out the BLAS code common to
- *        ConvolutionLayer and DeconvolutionLayer.
- */
-template <typename Dtype>
-class BaseConvolutionLayer : public Layer<Dtype> {
- public:
-  explicit BaseConvolutionLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
     
-    #ifdef USE_CUDNN
-/**
- * @brief CuDNN acceleration of ReLULayer.
- */
-template <typename Dtype>
-class CuDNNReLULayer : public ReLULayer<Dtype> {
- public:
-  explicit CuDNNReLULayer(const LayerParameter& param)
-      : ReLULayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNReLULayer();
-    }
+    #include <vector>
     
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
+      explicit WorkloadStats(State guardedState);
+  ~WorkloadStats();
     
-      private:
-    explicit DBWrapper();
-    ~DBWrapper();
-    
-    /* Notice extracted from xxHash homepage :
-    
-      // Offset of the file position indicator within the last block when an
-  // EOF was detected.
-  size_t eof_offset_;
+    #include <cassert>
     
     
     {
-    {  releaseJniEnv(attached_thread);
-}
-// @lint-ignore TXT4 T25377293 Grandfathered in
-}  // namespace rocksdb
-    
-         using Logger::SetInfoLogLevel;
-     using Logger::GetInfoLogLevel;
-     // Write an entry to the log file with the specified format.
-     virtual void Logv(const char* format, va_list ap);
-     // Write an entry to the log file with the specified log level
-     // and format.  Any log with level under the internal log level
-     // of *this (see @SetInfoLogLevel and @GetInfoLogLevel) will not be
-     // printed.
-     virtual void Logv(const InfoLogLevel log_level,
-         const char* format, va_list ap);
-    
-    DEFINE_FIND_STATIC_METHOD(KXlog_setAppenderMode, KXlog, 'setAppenderMode', '(I)V')
-JNIEXPORT void JNICALL Java_com_tencent_mars_xlog_Xlog_setAppenderMode
-  (JNIEnv *, jclass, jint _mode) {
-	appender_setmode((TAppenderMode)_mode);
-}
-    
-    #endif /* BASEEVENT_INTERFACE_BASEEVENT_H_ */
+    {///////////////////////////////////////////////////////////////////////////////
+}}
 
     
+    struct IRGS;
+    
+      struct alignas(64) CondVarNode {
+    pthread_cond_t m_cond;
+    folly::IntrusiveListHook m_listHook;
+    }
+    
+    namespace {
+    }
+    
+    TEST(Issue200, Test) {
+  // Get rid of any state from an old run.
+  std::string dbpath = test::TmpDir() + '/leveldb_issue200_test';
+  DestroyDB(dbpath, Options());
+    }
     
     
-    
-Test_Spy_Sample::Test_Spy_Sample()
-{
-    SPY_ATTACH_CLASS(NULL);
+    {  // Check last filter
+  ASSERT_TRUE(reader.KeyMayMatch(9000, 'box'));
+  ASSERT_TRUE(reader.KeyMayMatch(9000, 'hello'));
+  ASSERT_TRUE(! reader.KeyMayMatch(9000, 'foo'));
+  ASSERT_TRUE(! reader.KeyMayMatch(9000, 'bar'));
 }
+    
+    // Different bits-per-byte
+    
+    double Histogram::Average() const {
+  if (num_ == 0.0) return 0;
+  return sum_ / num_;
+}
+    
+    class SCOPED_LOCKABLE MutexLock {
+ public:
+  explicit MutexLock(port::Mutex *mu) EXCLUSIVE_LOCK_FUNCTION(mu)
+      : mu_(mu)  {
+    this->mu_->Lock();
+  }
+  ~MutexLock() UNLOCK_FUNCTION() { this->mu_->Unlock(); }
+    }
+    
+      // Compute the crc of the record type and the payload.
+  uint32_t crc = crc32c::Extend(type_crc_[t], ptr, n);
+  crc = crc32c::Mask(crc);                 // Adjust for storage
+  EncodeFixed32(buf, crc);
+    
+      size_t size() const { return size_; }
+  Iterator* NewIterator(const Comparator* comparator);
+    
+    bool js_cocos2dx_studio_InputDelegate_constructor(JSContext *cx, uint32_t argc, jsval *vp);
+void js_cocos2dx_studio_InputDelegate_finalize(JSContext *cx, JSObject *obj);
+void js_register_cocos2dx_studio_InputDelegate(JSContext *cx, JS::HandleObject global);
+void register_all_cocos2dx_studio(JSContext* cx, JS::HandleObject obj);
+bool js_cocos2dx_studio_InputDelegate_isAccelerometerEnabled(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_InputDelegate_setKeypadEnabled(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_InputDelegate_getTouchMode(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_InputDelegate_setAccelerometerEnabled(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_InputDelegate_isKeypadEnabled(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_InputDelegate_isTouchEnabled(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_InputDelegate_setTouchPriority(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_InputDelegate_getTouchPriority(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_InputDelegate_setTouchEnabled(JSContext *cx, uint32_t argc, jsval *vp);
+bool js_cocos2dx_studio_InputDelegate_setTouchMode(JSContext *cx, uint32_t argc, jsval *vp);
+    
+    #ifdef __cplusplus
+extern 'C' {
+#endif
+#include 'tolua++.h'
+#ifdef __cplusplus
+}
+#endif
+    
+    GLESDebugDraw::GLESDebugDraw( float32 ratio )
+    : mRatio( ratio )
+{
+    this->initShader();
+}
+    
+        virtual void DrawPolygon(const b2Vec2* vertices, int vertexCount, const b2Color& color);
+    
+    #ifndef AddPair_H
+#define AddPair_H
+    
+    class DumpCrashStack {
+  public:
+    DumpCrashStack() {}
+    ~DumpCrashStack() {}
+    }
+    
+        void* This() const {return m_this;}
+    
+    #include 'comm/debugger/spy.inl'
     
     // Unless required by applicable law or agreed to in writing, software distributed under the License is
 // distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
