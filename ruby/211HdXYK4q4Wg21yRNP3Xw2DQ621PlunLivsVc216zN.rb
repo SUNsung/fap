0f52697b2150e57a523dec56ae3921ccbdd64a13
SUNsung <weatherius@gmail.com>
2018-07-08 06:00:06 +0300

    Rewrite [211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb] File in: 2018-07-08 06:00:06

--- ruby/211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb
+++ ruby/211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb
@@ -1,29 +1,3 @@
 
         
-        module RuboCop
-  module Cop
-    module Lint
-      # Don't omit the accumulator when calling `next` in a `reduce` block.
-      #
-      # @example
-      #
-      #   # bad
-      #
-      #   result = (1..4).reduce(0) do |acc, i|
-      #     next if i.odd?
-      #     acc + i
-      #   end
-      #
-      # @example
-      #
-      #   # good
-      #
-      #   result = (1..4).reduce(0) do |acc, i|
-      #     next acc if i.odd?
-      #     acc + i
-      #   end
-      class NextWithoutAccumulator < Cop
-        MSG = 'Use `next` with an accumulator argument in a `reduce`.'.freeze
-    
-            def immutable_literal?(node)
-          return true if node.immutable_literal?
+        
