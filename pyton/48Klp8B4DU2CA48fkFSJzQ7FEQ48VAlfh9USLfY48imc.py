dadc8631db80362d5d8a4b1edf6dd1f0bbfb310e
SUNsung <weatherius@gmail.com>
2018-07-08 07:05:14 +0300

    Rewrite [48Klp8B4DU2CA48fkFSJzQ7FEQ48VAlfh9USLfY48imc.py] File in: 2018-07-08 07:05:13

--- pyton/48Klp8B4DU2CA48fkFSJzQ7FEQ48VAlfh9USLfY48imc.py
+++ pyton/48Klp8B4DU2CA48fkFSJzQ7FEQ48VAlfh9USLfY48imc.py
@@ -1,68 +1,176 @@
 
         
-            def shutdown(self, wait=True):
-        with self._shutdown_lock:
-            self._shutdown_thread = True
-        if self._queue_management_thread:
-            # Wake up queue management thread
-            self._result_queue.put(None)
-            if wait:
-                self._queue_management_thread.join()
-        # To reduce the risk of openning too many files, remove references to
-        # objects that use file descriptors.
-        self._queue_management_thread = None
-        self._call_queue = None
-        self._result_queue = None
-        self._processes = None
-    shutdown.__doc__ = _base.Executor.shutdown.__doc__
-    
-    # Workers are created as daemon threads. This is done to allow the interpreter
-# to exit when there are still idle threads in a ThreadPoolExecutor's thread
-# pool (i.e. shutdown() was not called). However, allowing workers to die with
-# the interpreter has two undesirable properties:
-#   - The workers would still be running during interpretor shutdown,
-#     meaning that they would fail in unpredictable ways.
-#   - The workers could be killed while evaluating a work item, which could
-#     be bad if the callable being evaluated has external side-effects e.g.
-#     writing to a file.
-#
-# To work around this problem, an exit handler is installed which tells the
-# workers to exit when their work queues are empty and then waits until the
-# threads finish.
-    
-      try:
-    filepath = os.path.join( DIR_OF_YCMD, 'PYTHON_USED_DURING_BUILDING' )
-    return utils.ReadFile( filepath ).strip()
-  # We need to check for IOError for Python2 and OSError for Python3
-  except ( IOError, OSError ):
-    return None
+            with io.open(infile, encoding='utf-8') as inf:
+        issue_template_tmpl = inf.read()
     
     
-  def No_Insertion_Text_test( self ):
-    self._Check( 0, {
-      'menu_text':       'MENU TEXT',
-      'extra_menu_info': 'EXTRA MENU INFO',
-      'kind':            'K',
-      'detailed_info':   'DETAILED INFO',
-      'extra_data': {
-        'doc_string':    'DOC STRING',
-      },
-    }, {
-      'word'     : '',
-      'abbr'     : 'MENU TEXT',
-      'menu'     : 'EXTRA MENU INFO',
-      'kind'     : 'k',
-      'info'     : 'DETAILED INFO\nDOC STRING',
-      'dup'      : 1,
-      'empty'    : 1,
-      'user_data': '0'
-    } )
+total_bytes = 0
+    
+            webpage = self._download_webpage(url, video_id)
+        key = self._search_regex(
+            r'src=['\']https?://[^/]+/embed/([A-Za-z0-9_-]+)', webpage, 'key')
+    
+            formats = []
+        for secure in ('', 'Secure'):
+            for ext in ('Ogg', 'Mp3'):
+                format_id = '%s%s' % (secure, ext)
+                format_url = metadata.get('%sUrl' % format_id)
+                if format_url:
+                    formats.append({
+                        'url': format_url,
+                        'format_id': format_id,
+                        'vcodec': 'none',
+                    })
+        self._sort_formats(formats)
+    
+    cc_library(
+    name = 'mst_solver',
+    hdrs = ['mst_solver.h'],
+    deps = [
+        ':disjoint_set_forest',
+        '//syntaxnet:base',
+        '@org_tensorflow//tensorflow/core:lib',
+    ],
+)
+    
+        # Extract the source and target token activations.  Use |stride| to collapse
+    # batch and beam into a single dimension.
+    sources = network_units.lookup_named_tensor('sources', linked_embeddings)
+    targets = network_units.lookup_named_tensor('targets', linked_embeddings)
+    source_tokens_bxnxs = tf.reshape(sources.tensor,
+                                     [stride, -1, self._source_dim])
+    target_tokens_bxnxt = tf.reshape(targets.tensor,
+                                     [stride, -1, self._target_dim])
+    num_tokens = tf.shape(source_tokens_bxnxs)[1]
+    
+          with tf.variable_scope(component.name, reuse=True):
+        sources = network_units.NamedTensor(
+            tf.zeros([_BATCH_SIZE * _NUM_TOKENS, _TOKEN_DIM]), 'sources')
+        targets = network_units.NamedTensor(
+            tf.zeros([_BATCH_SIZE * _NUM_TOKENS, _TOKEN_DIM]), 'targets')
+    
+      def __init__(self):
+    self.spec = spec_pb2.MasterSpec()
+    self.hyperparams = spec_pb2.GridPoint()
+    self.lookup_component = {'mock': MockComponent()}
+    self.build_runtime_graph = False
+    
+    
+if __name__ == '__main__':
+  app.run(main)
 
     
-      def __init__( self, bufnr, user_options, async_diags ):
-    self.number = bufnr
-    self._parse_tick = 0
-    self._handled_tick = 0
-    self._parse_request = None
-    self._async_diags = async_diags
-    self._diag_interface = DiagnosticInterface( bufnr, user_options )
+        # Validate that the assets are all in the exported directory.
+    path_set = self.ValidateAssetExistence(master_spec, export_path)
+    
+      def testEqualFiles(self):
+    content_actual = None
+    content_expected = None
+    
+            # ensure server thread doesn't get stuck waiting for connections
+        self._close_server_sock_ignore_errors()
+        self.join()
+        return False # allow exceptions to propagate
+
+    
+    from .compat import is_py2, builtin_str, str
+    
+        @pytest.mark.parametrize('method', ('POST', 'PUT', 'PATCH', 'OPTIONS'))
+    def test_empty_content_length(self, httpbin, method):
+        req = requests.Request(method, httpbin(method.lower()), data='').prepare()
+        assert req.headers['Content-Length'] == '0'
+    
+        pyopenssl_info = {
+        'version': None,
+        'openssl_version': '',
+    }
+    if OpenSSL:
+        pyopenssl_info = {
+            'version': OpenSSL.__version__,
+            'openssl_version': '%x' % OpenSSL.SSL.OPENSSL_VERSION_NUMBER,
+        }
+    cryptography_info = {
+        'version': getattr(cryptography, '__version__', ''),
+    }
+    idna_info = {
+        'version': getattr(idna, '__version__', ''),
+    }
+    
+    
+def get(url, params=None, **kwargs):
+    r'''Sends a GET request.
+    
+        builtin_str = str
+    str = str
+    bytes = bytes
+    basestring = (str, bytes)
+    numeric_types = (int, float)
+    integer_types = (int,)
+
+    
+            crawler.signals.connect(self._close, signals.engine_stopped)
+    
+        def process_request(self, request, spider):
+        if self.user_agent:
+            request.headers.setdefault(b'User-Agent', self.user_agent)
+
+    
+        for operating_sys in oslist:
+    
+    class _AllCompletedWaiter(_Waiter):
+    '''Used by wait(return_when=FIRST_EXCEPTION and ALL_COMPLETED).'''
+    
+            Args:
+            max_workers: The maximum number of threads that can be used to
+                execute the given calls.
+        '''
+        self._max_workers = max_workers
+        self._work_queue = queue.Queue()
+        self._threads = set()
+        self._shutdown = False
+        self._shutdown_lock = threading.Lock()
+    
+      assert_that( syntax_parse._KeywordsFromSyntaxListOutput(
+                   ContentsOfTestFile( 'python_syntax' ) ),
+               contains_inanyorder( *expected_keywords ) )
+    
+      # On UNIX platforms, we use sys.executable as the Python interpreter path.
+  # We cannot use sys.executable on Windows because for unknown reasons, it
+  # returns the Vim executable. Instead, we use sys.exec_prefix to deduce the
+  # interpreter path.
+  python_interpreter = ( WIN_PYTHON_PATH if utils.OnWindows() else
+                         sys.executable )
+  if _EndsWithPython( python_interpreter ):
+    return python_interpreter
+    
+    DIR_OF_THIS_SCRIPT = p.dirname( p.abspath( __file__ ) )
+DIR_OF_THIRD_PARTY = p.join( DIR_OF_THIS_SCRIPT, 'third_party' )
+DIR_OF_YCMD_THIRD_PARTY = p.join( DIR_OF_THIRD_PARTY, 'ycmd', 'third_party' )
+    
+    
+  def _HandleFixitResponse( self ):
+    if not len( self._response[ 'fixits' ] ):
+      vimsupport.PostVimMessage( 'No fixits found for current line',
+                                 warning = False )
+    else:
+      try:
+        fixit_index = 0
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+        if self._response_future is None:
+      # First poll
+      self._SendRequest()
+      return True
