feb7400bbafc901a9c65a8db74662362b189e259
SUNsung <weatherius@gmail.com>
2018-07-08 20:07:00 +0300

    Rewrite [22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb] File in: 2018-07-08 20:07:00

--- ruby/22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb
+++ ruby/22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb
@@ -1,93 +1,26 @@
 
         
-            groups << @user.authorized_groups.visible_to_user(current_user) if current_user
-    groups << @user.authorized_groups.public_to_user(current_user)
+        #
     
-        # Tells optparse how to parse the arguments.
-    #
-    # @param opts [OptionParser]
-    def set_opts(opts)
-      opts.banner = <<END
-Usage: sass-convert [options] [INPUT] [OUTPUT]
+    require 'bundler/setup'
+require 'json'
+require 'stackprof'
+require File.expand_path('../lib/jekyll', __dir__)
     
-          val = @mid.perform(environment)
-      if @warn_for_color && val.is_a?(Sass::Script::Value::Color) && val.name
-        alternative = Operation.new(Sass::Script::Value::String.new('', :string), @mid, :plus)
-        Sass::Util.sass_warn <<MESSAGE
-WARNING on line #{line}, column #{source_range.start_pos.offset}#{' of #{filename}' if filename}:
-You probably don't mean to use the color value `#{val}' in interpolation here.
-It may end up represented as #{val.inspect}, which will likely produce invalid CSS.
-Always quote color names when using them as strings (for example, '#{val}').
-If you really want to use the color value here, use `#{alternative.to_sass}'.
-MESSAGE
-      end
+    # Create an array of vendored mimetype => [extensions]
+mimes = {}
+json = open('https://raw.githubusercontent.com/jshttp/mime-db/master/db.json').read
+data = JSON.parse(json)
+data.reject! { |mime, meta| meta['extensions'].nil? || meta['extensions'].empty? }
+data.each do |mime, meta|
+  # Normalize extensions and mime-types
+  mime = mime.downcase.strip
+  extensions = meta['extensions'].map { |e| e.downcase.strip }.compact
     
-      def developer_prefix
-    `xcode-select --print-path`.strip
+      def remote_url
+    object.remote_url.presence
   end
     
-            # Removes the specified cache
-        #
-        # @param [Array<Hash>] cache_descriptors
-        #        An array of caches to remove, each specified with the same
-        #        hash as cache_descriptors_per_pod especially :spec_file and :slug
-        #
-        def remove_caches(cache_descriptors)
-          cache_descriptors.each do |desc|
-            UI.message('Removing spec #{desc[:spec_file]} (v#{desc[:version]})') do
-              FileUtils.rm(desc[:spec_file])
-            end
-            UI.message('Removing cache #{desc[:slug]}') do
-              FileUtils.rm_rf(desc[:slug])
-            end
-          end
-        end
-    
-          def actual_path
-        $PROGRAM_NAME
-      end
-    end
-  end
+    def codepoints_to_filename(codepoints)
+  codepoints.downcase.gsub(/\A[0]+/, '').tr(' ', '-')
 end
-
-    
-            app_targets.each do |app_target|
-          test_targets_for_app = test_targets.select do |target|
-            target.name.downcase.start_with?(app_target.name.downcase)
-          end
-          podfile << target_module(app_target, test_targets_for_app)
-        end
-    
-            def execute_repl_command(repl_command)
-          unless repl_command == '\n'
-            repl_commands = repl_command.split
-            subcommand = repl_commands.shift.capitalize
-            arguments = repl_commands
-            subcommand_class = Pod::Command::IPC.const_get(subcommand)
-            subcommand_class.new(CLAide::ARGV.new(arguments)).run
-            signal_end_of_output
-          end
-        end
-      end
-    end
-  end
-end
-
-    
-            # Checks if a template URL is given else returns the TEMPLATE_REPO URL
-        #
-        # @return String
-        #
-        def template_repo_url
-          @template_url || TEMPLATE_REPO
-        end
-      end
-    end
-  end
-end
-
-    
-            sets = config.sources_manager.aggregate.all_sets
-        sets.each { |set| UI.pod(set, :name_and_version) }
-        UI.puts '\n#{sets.count} pods were found'
-      end
