beee990336e7e7d42f2901b8757fd8ee0eeae030
SUNsung <weatherius@gmail.com>
2018-07-08 12:22:57 +0300

    Rewrite [26sUWxogB.Vc626S62PPP1Lpjc26VhlMtZMTxOg26rFi.py] File in: 2018-07-08 12:22:56

--- pyton/26sUWxogB.Vc626S62PPP1Lpjc26VhlMtZMTxOg26rFi.py
+++ pyton/26sUWxogB.Vc626S62PPP1Lpjc26VhlMtZMTxOg26rFi.py
@@ -1,197 +1,132 @@
-    if blueprint is not None and seems_fishy:
-        info.append('  The template was looked up from an endpoint that '
-                    'belongs to the blueprint '%s'.' % blueprint)
-        info.append('  Maybe you did not place a template in the right folder?')
-        info.append('  See http://flask.pocoo.org/docs/blueprints/#templates')
-    
-            rv = app.config['SERVER_NAME']
-    
-        Implements signals based on blinker if available, otherwise
-    falls silently back to a noop.
-    
-    
-def test_teardown_with_previous_exception(app):
-    buffer = []
-    
-        rv = app.test_client().get('/', errors_stream=out)
-    assert rv.status_code == 500
-    assert b'Internal Server Error' in rv.data
-    assert not out.getvalue()
 
+        
+            # Then we cluster the lines together as blocks
+    # Each block represents a collection of lines that should be sorted
+    # This was done by assuming only links ([...](...)) are meant to be sorted
+    # Clustering is done by indentation
+    blocks = []
+    last_indent = None
+    for line in read_me:
+        s_line = line.lstrip()
+        indent = len(line) - len(s_line)
+    
+    cc_library(
+    name = 'syntaxnet_link_feature_extractor',
+    srcs = ['syntaxnet_link_feature_extractor.cc'],
+    hdrs = ['syntaxnet_link_feature_extractor.h'],
+    deps = [
+        '//dragnn/protos:spec_proto_cc',
+        '//syntaxnet:base',
+        '//syntaxnet:embedding_feature_extractor',
+        '//syntaxnet:parser_transitions',
+        '//syntaxnet:task_context',
+    ],
+)
+    
+    from tensorflow.python.platform import gfile
+    
+        # Compute the pair, source, and target potentials.
+    pairs_bxnxl = digraph_ops.LabelPotentialsFromTokenPairs(sources_bxnxs,
+                                                            targets_bxnxt,
+                                                            weights_pair)
+    sources_bxnxl = digraph_ops.LabelPotentialsFromTokens(sources_bxnxs,
+                                                          weights_source)
+    targets_bxnxl = digraph_ops.LabelPotentialsFromTokens(targets_bxnxt,
+                                                          weights_target)
+    
+    from dragnn.python import component
+from dragnn.python import dragnn_ops
+from dragnn.python import network_units
+from syntaxnet.util import check
+    
+        # Check that the loss op is present.
+    op_names = [op.name for op in tf.get_default_graph().get_operations()]
+    self.assertTrue('train_test/compute_loss/'
+                    'sigmoid_cross_entropy_with_logits' in op_names)
+    
+      This method exports a master_spec and associated files into the SavedModel's
+  'assets.extra' directory (which is unmanaged). All resources are added to the
+  'assets.extra' directory using sanitized paths. The master spec itself is
+  located at the base of the assets.extra directory.
+    
+        # Export the graph via SavedModel. (Here, we maintain a handle to the graph
+    # for comparison, but that's usually not necessary.)  Note that the export
+    # path must not already exist.
+    export_path = os.path.join(test_flags.temp_dir(), 'export2')
+    dragnn_model_saver_lib.clean_output_paths(export_path)
+    saver_graph = tf.Graph()
+    
+    numeric_range = r'''
+    \[
+        (?:[0-9]+:[0-9]+)               # numeric begin:end
+        (?::[0-9]+)?                    # numeric :step (optional)
+    \]
+'''
+    
+        terminal_stdout_re = [
+        re.compile(r'[\r\n]?<.+>(?:\s*)$'),
+        re.compile(r'[\r\n]?\[.+\](?:\s*)$'),
+    ]
+    
+            shuge = '12345' * 120
+        huge = 1 << 30000
+        mhuge = -huge
+        namespace = {'huge': huge, 'mhuge': mhuge, 'shuge': shuge, 'math': math}
+        for test in ['float(huge)', 'float(mhuge)',
+                     'complex(huge)', 'complex(mhuge)',
+                     'complex(huge, 1)', 'complex(mhuge, 1)',
+                     'complex(1, huge)', 'complex(1, mhuge)',
+                     '1. + huge', 'huge + 1.', '1. + mhuge', 'mhuge + 1.',
+                     '1. - huge', 'huge - 1.', '1. - mhuge', 'mhuge - 1.',
+                     '1. * huge', 'huge * 1.', '1. * mhuge', 'mhuge * 1.',
+                     '1. // huge', 'huge // 1.', '1. // mhuge', 'mhuge // 1.',
+                     '1. / huge', 'huge / 1.', '1. / mhuge', 'mhuge / 1.',
+                     '1. ** huge', 'huge ** 1.', '1. ** mhuge', 'mhuge ** 1.',
+                     'math.sin(huge)', 'math.sin(mhuge)',
+                     'math.sqrt(huge)', 'math.sqrt(mhuge)', # should do better
+                     # math.floor() of an int returns an int now
+                     ##'math.floor(huge)', 'math.floor(mhuge)',
+                     ]:
+    
+        print('n = %d\n' % n)
+    
+        objects = []
+    libs = ['shell32.lib', 'comdlg32.lib', 'wsock32.lib', 'user32.lib', 'oleaut32.lib']
+    for moddefn in moddefns:
+        print('# Module', moddefn.name)
+        for file in moddefn.sourceFiles:
+            base = os.path.basename(file)
+            base, ext = os.path.splitext(base)
+            objects.append(base + '.obj')
+            print(r'$(temp_dir)\%s.obj: '%s'' % (base, file))
+            print('\t@$(CC) -c -nologo /Fo$* $(cdl) $(c_debug) /D BUILD_FREEZE', end=' ')
+            print(''-I$(pythonhome)/Include'  '-I$(pythonhome)/PC' \\')
+            print('\t\t$(cflags) $(cdebug) $(cinclude) \\')
+            extra = moddefn.GetCompilerOptions()
+            if extra:
+                print('\t\t%s \\' % (' '.join(extra),))
+            print('\t\t'%s'' % file)
+            print()
+    
+    class IssuesTestCase(BaseTestCase):
+    '''Test fixed bdb issues.'''
+    
+    @dataclass
+class CV:
+    T_CV4 = ClassVar
+    cv0: ClassVar[int] = 20
+    cv1: ClassVar = 30
+    cv2: T_CV2
+    cv3: T_CV3
+    not_cv4: T_CV4  # When using string annotations, this field is not recognized as a ClassVar.
     
     
-@keras_test
-def test_dropout_legacy_interface():
-    old_layer = keras.layers.Dropout(p=3, name='drop')
-    new_layer_1 = keras.layers.Dropout(rate=3, name='drop')
-    new_layer_2 = keras.layers.Dropout(3, name='drop')
-    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer_1.get_config())
-    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer_2.get_config())
+Package = Union[str, ModuleType]
+Resource = Union[str, os.PathLike]
     
-        callbacks.set_model(callback_model)
-    callbacks.set_params({
-        'batch_size': batch_size,
-        'epochs': epochs,
-        'steps': steps_per_epoch,
-        'samples': num_train_samples,
-        'verbose': verbose,
-        'do_validation': do_validation,
-        'metrics': callback_metrics or [],
-    })
-    callbacks.on_train_begin()
-    callback_model.stop_training = False
-    for cbk in callbacks:
-        cbk.validation_data = val_ins
-    
-    
-@keras_test
-def test_dropout():
-    layer_test(layers.Dropout,
-               kwargs={'rate': 0.5},
-               input_shape=(3, 2))
-    
-            generated = ''
-        sentence = text[start_index: start_index + maxlen]
-        generated += sentence
-        print('----- Generating with seed: '' + sentence + ''')
-        sys.stdout.write(generated)
-    
-        >>> from fractions import Fraction as F
-    >>> mean([F(3, 7), F(1, 21), F(5, 3), F(1, 3)])
-    Fraction(13, 21)
-    
-    def pi_decimal():
-    '''decimal'''
-    D = P.Decimal
-    lasts, t, s, n, na, d, da = D(0), D(3), D(3), D(1), D(0), D(0), D(24)
-    while s != lasts:
-        lasts = s
-        n, na = n+na, na+8
-        d, da = d+da, da+32
-        t = (t * n) / d
-        s += t
-    return s
-    
-        def seek(self, pos, whence=0):
-        '''Seek to specified position into the chunk.
-        Default position is 0 (start of chunk).
-        If the file is not seekable, this will result in an error.
-        '''
-    
-    def _sync_flush(f):
-    '''Ensure changes to file f are physically on disk.'''
-    f.flush()
-    if hasattr(os, 'fsync'):
-        os.fsync(f.fileno())
-    
-    
-
-# generic class
-class ColorDB:
-    def __init__(self, fp):
-        lineno = 2
-        self.__name = fp.name
-        # Maintain several dictionaries for indexing into the color database.
-        # Note that while Tk supports RGB intensities of 4, 8, 12, or 16 bits,
-        # for now we only support 8 bit intensities.  At least on OpenWindows,
-        # all intensities in the /usr/openwin/lib/rgb.txt file are 8-bit
-        #
-        # key is (red, green, blue) tuple, value is (name, [aliases])
-        self.__byrgb = {}
-        # key is name, value is (red, green, blue)
-        self.__byname = {}
-        # all unique names (non-aliases).  built-on demand
-        self.__allnames = None
-        for line in fp:
-            # get this compiled regular expression from derived class
-            mo = self._re.match(line)
-            if not mo:
-                print('Error in', fp.name, ' line', lineno, file=sys.stderr)
-                lineno += 1
-                continue
-            # extract the red, green, blue, and name
-            red, green, blue = self._extractrgb(mo)
-            name = self._extractname(mo)
-            keyname = name.lower()
-            # BAW: for now the `name' is just the first named color with the
-            # rgb values we find.  Later, we might want to make the two word
-            # version the `name', or the CapitalizedVersion, etc.
-            key = (red, green, blue)
-            foundname, aliases = self.__byrgb.get(key, (name, []))
-            if foundname != name and foundname not in aliases:
-                aliases.append(name)
-            self.__byrgb[key] = (foundname, aliases)
-            # add to byname lookup
-            self.__byname[keyname] = key
-            lineno = lineno + 1
-    
-            try:
-            # Get arguments by reading body of request.
-            # We read this in chunks to avoid straining
-            # socket.read(); around the 10 or 15Mb mark, some platforms
-            # begin to have problems (bug #792570).
-            max_chunk_size = 10*1024*1024
-            size_remaining = int(self.headers['content-length'])
-            L = []
-            while size_remaining:
-                chunk_size = min(size_remaining, max_chunk_size)
-                chunk = self.rfile.read(chunk_size)
-                if not chunk:
-                    break
-                L.append(chunk)
-                size_remaining -= len(L[-1])
-            data = b''.join(L)
-    
-    # Examples of VER command output:
-#
-#   Windows 2000:  Microsoft Windows 2000 [Version 5.00.2195]
-#   Windows XP:    Microsoft Windows XP [Version 5.1.2600]
-#   Windows Vista: Microsoft Windows [Version 6.0.6002]
-#
-# Note that the 'Version' string gets localized on different
-# Windows versions.
-    
-            # import x.y.z as w binds z as w
-        import test.support as y
-        self.assertIs(y, test.support, y.__name__)
-    
-        def is_resource(self, name):
-        # Maybe we could do better, but if we can get the data, it's a
-        # resource.  Otherwise it isn't.
-        fullname_as_path = self.fullname.replace('.', '/')
-        path = f'{fullname_as_path}/{name}'
+            g = gen()
         try:
-            self.zipimporter.get_data(path)
-        except OSError:
-            return False
-        return True
-    
-            with (yield semaphore.acquire()):
-            pass
-    
-        @gen_test
-    def test_read_until_regex_max_bytes_ignores_extra(self):
-        rs, ws = yield self.make_iostream_pair()
-        closed = Event()
-        rs.set_close_callback(closed.set)
-        try:
-            # Even though data that matches arrives the same packet that
-            # puts us over the limit, we fail the request because it was not
-            # found within the limit.
-            ws.write(b'abcdef')
-            with ExpectLog(gen_log, 'Unsatisfiable read'):
-                rs.read_until_regex(b'def', max_bytes=5)
-                yield closed.wait()
-        finally:
-            ws.close()
-            rs.close()
-    
-    # Verify that the SSL module has all the modern upgrades. Check for several
-# names individually since they were introduced at different versions,
-# although they should all be present by Python 3.4 or 2.7.9.
-if (not hasattr(ssl, 'SSLContext') or
-        not hasattr(ssl, 'create_default_context') or
-        not hasattr(ssl, 'match_hostname')):
-    raise ImportError('Tornado requires an up-to-date SSL module. This means '
-                      'Python 2.7.9+ or 3.4+ (although some distributions have '
-                      'backported the necessary changes to older versions).')
+            raise ValueError
+        except Exception:
+            self.assertEqual(next(g), 'done')
+        self.assertEqual(sys.exc_info(), (None, None, None))
