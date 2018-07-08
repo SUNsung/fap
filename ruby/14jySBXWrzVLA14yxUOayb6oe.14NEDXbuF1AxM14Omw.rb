adf15550edfa7d9867d75c78dbf7fccdbc13cfc4
SUNsung <weatherius@gmail.com>
2018-07-08 18:58:57 +0300

    Rewrite [14jySBXWrzVLA14yxUOayb6oe.14NEDXbuF1AxM14Omw.rb] File in: 2018-07-08 18:58:57

--- ruby/14jySBXWrzVLA14yxUOayb6oe.14NEDXbuF1AxM14Omw.rb
+++ ruby/14jySBXWrzVLA14yxUOayb6oe.14NEDXbuF1AxM14Omw.rb
@@ -1,63 +1,68 @@
 
         
-              # Topic may be hard deleted due to spam, no point complaining
-      # we would have to look at the topics table id sequence to find cases
-      # where this was called with an invalid id, no point really
-      return unless topic.present?
-    
-      def set_permission(permission_name, value)
-    self.send('#{permission_name}=', value)
-    save_and_refresh_staff_groups!
-  end
-    
-          html_filters.push 'coffeescript/clean_html_v1', 'coffeescript/entries_v1', 'title'
-    
-        html_filters.push 'sinon/clean_html', 'sinon/entries'
-    
-        def parse(response)
-      unless response.url == root_url || self.class.version == 'Guide'
-        response.body.sub!(/<nav class='devsite-nav-responsive-sidebar.+?<\/nav>/m, '')
-        response.body.gsub!(/<li class='devsite-nav-item'>.+?<\/li>/m, '')
-      end
-    
-            a_split <=> b_split
-      else
-        a.casecmp(b)
-      end
+          def validate_option(method)
+    if self.respond_to? 'validate_#{method}'.to_sym
+      self.send('validate_#{method}'.to_sym)
+    else
+      false
     end
   end
+    
+        it 'returns Rational(1) when self is Rational(-1) and the exponent is positive and even' do
+      (Rational(-1) ** bignum_value(0)).should eql(Rational(1))
+      (Rational(-1) ** bignum_value(2)).should eql(Rational(1))
+    end
+    
+          each { |u|
+        dig[u] = a = []
+        each{ |v| func.call(u, v) and a << v }
+      }
+    
+        ret = set.delete(2)
+    assert_equal(set, ret)
+    assert_equal(Set[1,3], set)
+    
+    describe :string_unpack_32bit_le_signed, shared: true do
+  it 'decodes an int with most significant bit set as a negative number' do
+    '\x00\xaa\x00\xff'.unpack(unpack_format()).should == [-16733696]
+  end
 end
-
     
-      # Returns true if the set and the given set have no element in
-  # common.  This method is the opposite of +intersect?+.
-  #
-  #   Set[1, 2, 3].disjoint? Set[3, 4]   #=> false
-  #   Set[1, 2, 3].disjoint? Set[4, 5]   #=> true
-  def disjoint?(set)
-    !intersect?(set)
-  end
+        it 'returns related entity for an existing public post' do
+      post = FactoryGirl.create(:status_message, author: local_person, public: true)
+      entity = DiasporaFederation.callbacks.trigger(:fetch_related_entity, 'Post', post.guid)
+      expect(entity.author).to eq(post.diaspora_handle)
+      expect(entity.local).to be_truthy
+      expect(entity.public).to be_truthy
+      expect(entity.parent).to be_nil
+    end
     
-      def self.main_thread2(critical_thread)
-    Thread.pass # The join below seems to cause a deadlock with CRuby unless Thread.pass is called first
-    critical_thread.join
-    Thread.critical.should == false
-  end
+        def index
+      pods_json = PodPresenter.as_collection(Pod.all)
     
-        run.should be_true
-  end
-    
-          if tags.include?(:run)
-        odeprecated ''depends_on ... => :run''
+          def restore_request_parameters
+        req = build_rack_request
+        req.update_param('client_id', session[:client_id])
+        req.update_param('redirect_uri', session[:redirect_uri])
+        req.update_param('response_type', response_type_as_space_seperated_values)
+        req.update_param('scope', session[:scopes])
+        req.update_param('state', session[:state])
+        req.update_param('nonce', session[:nonce])
       end
     
-            def_node_matcher :multiple_compare?, <<-PATTERN
-          (send (send _ {:< :> :<= :>=} $_) {:< :> :<= :>=} _)
-        PATTERN
+        # @abstract
+    #
+    # Update the clone on the deployment target
+    #
+    # @return void
+    #
+    def update
+      raise NotImplementedError, 'Your SCM strategy module should provide a #update method'
+    end
     
-            def_node_matcher :on_body_of_reduce, <<-PATTERN
-          (block (send _recv {:reduce :inject} !sym) _blockargs $(begin ...))
-        PATTERN
-    
-              reconstructed.join
-        end
+      let(:help_output) do
+    out, _err = capture_io do
+      flags '--help', '-h'
+    end
+    out
+  end
