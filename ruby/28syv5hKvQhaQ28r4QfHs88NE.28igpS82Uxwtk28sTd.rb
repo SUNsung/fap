bb7b60e6bfaadc170ce7b91cc53794c208e634aa
SUNsung <weatherius@gmail.com>
2018-07-09 01:33:56 +0300

    Rewrite [28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb] File in: 2018-07-09 01:33:56

--- ruby/28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb
+++ ruby/28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb
@@ -1,47 +1,26 @@
 
         
-          test 'overriding has_many_attached methods works' do
-    # attach blobs before messing with getter, which breaks `#attach`
-    @user.highlights.attach create_blob(filename: 'funky.jpg'), create_blob(filename: 'wonky.jpg')
-    
-          # remove the column +name+ from the table.
-      #   remove_column(:account_id)
-      def remove_column(name)
-        @columns_hash.delete name.to_s
-      end
-    
-            def test_create_table_with_array_column
-          connection.create_table :testings do |t|
-            t.string :foo, array: true
-          end
-    
-    require 'test_helper'
-require 'minitest/mock'
-require 'stubs/test_connection'
-require 'stubs/room'
-    
-        thread = server.run
-    
-          connection = Connection.new(server, env)
-      assert_called(connection.websocket, :close) do
-        connection.process
-      end
-    end
+          def evernote_consumer_key
+    (config = Devise.omniauth_configs[:evernote]) && config.strategy.consumer_key
   end
-end
-
     
-        def send_async(method, *args)
-      send method, *args
+        def run!
+      @thread = Thread.new do
+        Thread.current[:name] = '#{id}-#{Time.now}'
+        begin
+          run
+        rescue SignalException, SystemExit
+          stop!
+        rescue StandardError => e
+          message = '#{id} Exception #{e.message}:\n#{e.backtrace.first(10).join('\n')}'
+          AgentRunner.with_connection do
+            agent.error(message)
+          end
+        end
+      end
     end
     
-        version '4' do
-      self.release = '4.1.1'
-      self.base_url = 'https://getbootstrap.com/docs/4.1/'
-      self.root_path = 'getting-started/introduction/'
+      def destroy
+    @event.destroy
     
-          html_filters.push 'd3/clean_html', 'd3/entries_v3', 'title'
-    
-        def root?
-      path == 'index'
-    end
+      before_action :upgrade_warning, only: :index
