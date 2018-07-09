1c8deaa39e1cc2b9c07d7076ee55d60620785fda
SUNsung <weatherius@gmail.com>
2018-07-09 03:10:26 +0300

    Rewrite [35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb] File in: 2018-07-09 03:10:26

--- ruby/35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb
+++ ruby/35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb
@@ -1,189 +1,85 @@
 
         
-          def url
-    if object.needs_redownload?
-      media_proxy_url(object.id, :original)
+          def recipients(payload = {})
+    emails = interpolated(payload)['recipients']
+    if emails.present?
+      if emails.is_a?(String)
+        [emails]
+      else
+        emails
+      end
     else
-      full_asset_url(object.file.url(:original))
+      [user.email]
     end
   end
     
-        def expect_updated_sign_in_at(user)
-      expect(user.reload.current_sign_in_at).to be_within(1.0).of(Time.now.utc)
-    end
+      def evernote_consumer_secret
+    (config = Devise.omniauth_configs[:evernote]) && config.strategy.consumer_secret
   end
+    
+        def translation_scope
+      'devise.confirmations'
+    end
 end
 
     
-        formulae = ARGV.include?('--installed') ? Formula.installed : Formula
-    recursive = ARGV.flag? '--recursive'
-    only_installed_arg = ARGV.include?('--installed') &&
-                         !ARGV.include?('--include-build') &&
-                         !ARGV.include?('--include-test') &&
-                         !ARGV.include?('--include-optional') &&
-                         !ARGV.include?('--skip-recommended')
+    class Devise::SessionsController < DeviseController
+  prepend_before_action :require_no_authentication, only: [:new, :create]
+  prepend_before_action :allow_params_authentication!, only: :create
+  prepend_before_action :verify_signed_out_user, only: :destroy
+  prepend_before_action(only: [:create, :destroy]) { request.env['devise.skip_timeout'] = true }
     
-    class Python3Requirement < Requirement
-  fatal true
-  satisfy do
-    odisabled('Python3Requirement', ''depends_on \'python\''')
+        if resource.errors.empty?
+      set_flash_message! :notice, :unlocked
+      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
   end
-end
     
-    Bundler.require(
-    *Rails.groups(
-        coverage: [:test],
-        db: all_environments,
-        pcap: all_environments
-    )
+          def remember_me_is_active?(resource)
+        return false unless resource.respond_to?(:remember_me)
+        scope = Devise::Mapping.find_scope!(resource)
+        _, token, generated_at = cookies.signed[remember_key(resource, scope)]
+        resource.remember_me?(token, generated_at)
+      end
+    
+        if record.timedout?(last_request_at) &&
+        !env['devise.skip_timeout'] &&
+        !proxy.remember_me_is_active?(record)
+      Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
+      throw :warden, scope: scope, message: :timeout
+    end
+    
+          def template_paths
+        template_path = _prefixes.dup
+        template_path.unshift '#{@devise_mapping.scoped_path}/mailer' if self.class.scoped_views?
+        template_path
+      end
+    
+        private
+    
+    (allow process-exec
+  (literal
+    '<%= pod_bin %>'
+    '<%= ruby_bin %>'
+  )
+  (regex
+<% prefixes.each do |prefix| %>
+    #'^<%= prefix %>/*'
+<% end %>
+  )
 )
     
-    GEMFILE_EXTENSIONS = [
-    '.local',
-    ''
-]
-    
-    end
-
-    
-          when :login_pass
-    
-    #compileOpts = ['']
-#outputDir		= system.getProperty('java.io.tmpdir')
-outputDir		= 'testoutdir'
-compileOpts 	= [ '-target', '1.3', '-source', '1.3', '-d', outputDir ]
-    
-    #certCN cannot contain commas
-certCN 		= 'Metasploit Inc.'
-#keytoolOpts 	= '-genkey -alias signFiles -keystore msfkeystore ' +
-#		  '-storepass msfstorepass -dname \'cn=#{certCN}\' ' +
-#		  '-keypass msfkeypass'
-    
-      when '6.1.2'
-    __NR_execve      = 7
-    __NR_getpeername = 205
-    __NR_accept      = 232
-    __NR_listen      = 235
-    __NR_bind        = 237
-    __NR_socket      = 238
-    __NR_connect     = 239
-    __NR_close       = 272
-    __NR_kfcntl      = 635
-    
-          opts.on('-v', '--version', 'Print the Sass version.') do
-        puts('Sass #{Sass.version[:string]}')
-        exit
-      end
-    end
-    
-          # Get the publicly-visible URL for an imported file. This URL is used by
-      # source maps to link to the source stylesheet. This may return `nil` to
-      # indicate that no public URL is available; however, this will cause
-      # sourcemap generation to fail if any CSS is generated from files imported
-      # from this importer.
+          # @param  [Xcodeproj::Project] project
+      #         The xcode project to generate a podfile for.
       #
-      # If an absolute 'file:' URI can be produced for an imported file, that
-      # should be preferred to returning `nil`. However, a URL relative to
-      # `sourcemap_directory` should be preferred over an absolute 'file:' URI.
+      # @return [String] the text of the Podfile for the provided project
       #
-      # @param uri [String] A URI known to be valid for this importer.
-      # @param sourcemap_directory [String, NilClass] The absolute path to a
-      #   directory on disk where the sourcemap will be saved. If uri refers to
-      #   a file on disk that's accessible relative to sourcemap_directory, this
-      #   may return a relative URL. This may be `nil` if the sourcemap's
-      #   eventual location is unknown.
-      # @return [String?] The publicly-visible URL for this file, or `nil`
-      #   indicating that no publicly-visible URL exists. This should be
-      #   appropriately URL-escaped.
-      def public_url(uri, sourcemap_directory)
-        return if @public_url_warning_issued
-        @public_url_warning_issued = true
-        Sass::Util.sass_warn <<WARNING
-WARNING: #{self.class.name} should define the #public_url method.
-WARNING
-        nil
-      end
-    
-    # A logger that delays messages until they're explicitly flushed to an inner
-# logger.
-#
-# This can be installed around the current logger by calling \{#install!}, and
-# the original logger can be replaced by calling \{#uninstall!}. The log
-# messages can be flushed by calling \{#flush}.
-class Sass::Logger::Delayed < Sass::Logger::Base
-  # Installs a new delayed logger as the current Sass logger, wrapping the
-  # original logger.
-  #
-  # This can be undone by calling \{#uninstall!}.
-  #
-  # @return [Sass::Logger::Delayed] The newly-created logger.
-  def self.install!
-    logger = Sass::Logger::Delayed.new(Sass.logger)
-    Sass.logger = logger
-    logger
-  end
-    
-        # Starts the read-eval-print loop.
-    def run
-      environment = Environment.new
-      @line = 0
-      loop do
-        @line += 1
-        unless (text = Readline.readline('>> '))
-          puts
-          return
-        end
-    
-      end
-    
-    Liquid::Template.register_tag('include_array', Jekyll::IncludeArrayTag)
-
-    
-      class IncludeCodeTag < Liquid::Tag
-    def initialize(tag_name, markup, tokens)
-      @title = nil
-      @file = nil
-      if markup.strip =~ /\s*lang:(\S+)/i
-        @filetype = $1
-        markup = markup.strip.sub(/lang:\S+/i,'')
-      end
-      if markup.strip =~ /(.*)?(\s+|^)(\/*\S+)/i
-        @title = $1 || nil
-        @file = $3
-      end
-      super
-    end
-    
-      # PATCH/PUT /books/1
-  # PATCH/PUT /books/1.json
-  def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
-        format.json { render :show, status: :ok, location: @book }
-      else
-        format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-    
-            def on_block(node)
-          on_body_of_reduce(node) do |body|
-            void_next = body.each_node(:next).find do |n|
-              n.children.empty? && parent_block_node(n) == node
-            end
-    
-            def_node_matcher :simple_double_comparison?, '(send $lvar :== $lvar)'
-        def_node_matcher :simple_comparison?, <<-PATTERN
-          {(send $lvar :== _)
-           (send _ :== $lvar)}
-        PATTERN
-    
-              FROZEN_STRING_LITERAL_TYPES.include?(node.type) &&
-            frozen_string_literals_enabled?
-        end
-      end
-    end
-  end
-end
+      def podfile_template(project)
+        podfile = ''
+        podfile << 'project '#{@project_path}'\n\n' if @project_path
+        podfile << <<-PLATFORM.strip_heredoc
+          # Uncomment the next line to define a global platform for your project
+          # platform :ios, '9.0'
+        PLATFORM
