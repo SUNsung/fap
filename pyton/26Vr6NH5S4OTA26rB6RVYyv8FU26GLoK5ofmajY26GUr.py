b1523fa900296c83b81144a8d13611649e270b47
SUNsung <weatherius@gmail.com>
2018-07-09 06:29:47 +0300

    Rewrite [26Vr6NH5S4OTA26rB6RVYyv8FU26GLoK5ofmajY26GUr.py] File in: 2018-07-09 06:29:47

--- pyton/26Vr6NH5S4OTA26rB6RVYyv8FU26GLoK5ofmajY26GUr.py
+++ pyton/26Vr6NH5S4OTA26rB6RVYyv8FU26GLoK5ofmajY26GUr.py
@@ -1,96 +1,158 @@
 
         
-            plugin_manager.register(Plugin)
-    try:
-        r = http(
-            httpbin + BASIC_AUTH_URL,
-            '--auth-type',
-            Plugin.auth_type,
-            '--auth',
-            BASIC_AUTH_HEADER_VALUE,
-        )
-        assert HTTP_OK in r
-        assert r.json == AUTH_OK
-    finally:
-        plugin_manager.unregister(Plugin)
-    
-        # noinspection PyProtectedMember
-    @property
-    def headers(self):
-        original = self._orig.raw._original_response
-    
-    
-BINARY_SUPPRESSED_NOTICE = (
-    b'\n'
-    b'+-----------------------------------------+\n'
-    b'| NOTE: binary data not shown in terminal |\n'
-    b'+-----------------------------------------+'
-)
-    
-    
-def get_exit_status(http_status, follow=False):
-    '''Translate HTTP status code to exit status code.'''
-    if 300 <= http_status <= 399 and not follow:
-        # Redirect
-        return ExitStatus.ERROR_HTTP_3XX
-    elif 400 <= http_status <= 499:
-        # Client Error
-        return ExitStatus.ERROR_HTTP_4XX
-    elif 500 <= http_status <= 599:
-        # Server Error
-        return ExitStatus.ERROR_HTTP_5XX
-    else:
-        return ExitStatus.OK
-    
-        def test_self_signed_server_cert_by_default_raises_ssl_error(
-            self,
-            httpbin_secure_untrusted):
-        with pytest.raises(SSLError):
-            http(httpbin_secure_untrusted.url + '/get')
-    
-        name = None
-    helpurl = None
-    about = None
+            styles = {
+        # No corresponding class for the following:
+        #Text:                     '', # class:  ''
+        Whitespace:                'underline #f8f8f8',      # class: 'w'
+        Error:                     '#a40000 border:#ef2929', # class: 'err'
+        Other:                     '#000000',                # class 'x'
+    }
     
     import pytest
-from requests.compat import urljoin
+    
+    # Custom sidebar templates, maps document names to template names.
+html_sidebars = {
+    'index':    ['sidebarintro.html', 'sourcelink.html', 'searchbox.html',
+                 'hacks.html'],
+    '**':       ['sidebarlogo.html', 'localtoc.html', 'relations.html',
+                 'sourcelink.html', 'searchbox.html', 'hacks.html']
+}
     
     
-class Server(threading.Thread):
-    '''Dummy server using for unit testing'''
-    WAIT_EVENT_TIMEOUT = 5
+def patch(url, data=None, **kwargs):
+    r'''Sends a PATCH request.
     
-    '''
-requests._internal_utils
-~~~~~~~~~~~~~~
+        For example, ``headers['content-encoding']`` will return the
+    value of a ``'Content-Encoding'`` response header, regardless
+    of how the header name was originally stored.
     
-        get_item_parameters = pytest.mark.parametrize(
-        'key, value', (
-            ('bad_gateway', 502),
-            ('not_a_key', None)
-        )
+        temporal_sample_weight = np.reshape(sample_weight, (len(sample_weight), 1))
+    temporal_sample_weight = np.repeat(temporal_sample_weight, timesteps, axis=1)
+    
+        # i have not idea what I'm doing: garbage as inputs/outputs
+    j = Input(shape=(32,), name='input_j')
+    k = Input(shape=(32,), name='input_k')
+    m, n = model([j, k])
+    with pytest.raises(ValueError):
+        Model([j, k], [m, n, 0])
+    
+    print('Train...')
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, y_test))
+score, acc = model.evaluate(x_test, y_test, batch_size=batch_size)
+print('Test score:', score)
+print('Test accuracy:', acc)
+
+    
+    # add the match matrix with the second input vector sequence
+response = add([match, input_encoded_c])  # (samples, story_maxlen, query_maxlen)
+response = Permute((2, 1))(response)  # (samples, query_maxlen, story_maxlen)
+    
+    
+def ismount(path):
+    '''Test whether a path is a mount point
+    clone of os.path.ismount (from cpython Lib/posixpath.py)
+    fixed to solve https://github.com/ansible/ansible-modules-core/issues/2186
+    and workaround non-fixed http://bugs.python.org/issue2466
+    this should be rewritten as soon as python issue 2466 is fixed
+    probably check for python version and use os.path.ismount if fixed
+    
+        if not os.path.exists(to_bytes(filename, errors='surrogate_or_strict')) or os.path.isdir(to_bytes(filename, errors='strict')):
+        return None
+    digest = hash_func()
+    blocksize = 64 * 1024
+    try:
+        infile = open(to_bytes(filename, errors='surrogate_or_strict'), 'rb')
+        block = infile.read(blocksize)
+        while block:
+            digest.update(block)
+            block = infile.read(blocksize)
+        infile.close()
+    except IOError as e:
+        raise AnsibleError('error while accessing the file %s, error was: %s' % (filename, e))
+    return digest.hexdigest()
+    
+        parser.add_argument('--key',
+                        dest='api_key',
+                        default=api_key,
+                        required=api_key is None,
+                        help='api key for accessing Shippable')
+    
+    # Components that match a 16-bit portion of an IPv6 address in hexadecimal
+# notation (0..ffff) or an 8-bit portion of an IPv4 address in decimal notation
+# (0..255) or an [x:y(:z)] numeric range.
+    
+            # global URL, resource, entity, method_name
+        input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/global/backendServices/mybackendservice/getHealth'
+        actual = GCPUtils.parse_gcp_url(input_url)
+        self.assertEquals('compute', actual['service'])
+        self.assertEquals('v1', actual['api_version'])
+        self.assertEquals('myproject', actual['project'])
+        self.assertTrue('global' in actual)
+        self.assertTrue(actual['global'])
+        self.assertEquals('backendServices', actual['resource_name'])
+        self.assertEquals('mybackendservice', actual['entity_name'])
+        self.assertEquals('getHealth', actual['method_name'])
+    
     )
+STAGING_URI = 'https://acme-staging-v02.api.letsencrypt.org/directory'
+    
+            '''
+        try:
+            orderr = self.acme.new_order(csr_pem)
+        except acme_errors.WildcardUnsupportedError:
+            raise errors.Error('The currently selected ACME CA endpoint does'
+                               ' not support issuing wildcard certificates.')
+        authzr = self.auth_handler.handle_authorizations(orderr, best_effort)
+        return orderr.update(authorizations=authzr)
+    
+            self.assertTrue(self.vhost1.conflicts([self.addr2]))
+        self.assertFalse(self.vhost1.conflicts([self.addr_default]))
+    
+        @mock.patch('certbot_compatibility_test.validator.requests.get')
+    def test_redirect_with_headers(self, mock_get_request):
+        mock_get_request.return_value = create_response(
+            301, {'location': 'https://test.com'})
+        self.assertTrue(self.validator.redirect(
+            'test.com', headers={'Host': 'test.com'}))
+    
+        # ensure folder exists
+    if not os.path.exists(args.codedir):
+        os.makedirs(args.codedir)
+    
+    # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
+# using the given strftime format.
+#html_last_updated_fmt = '%b %d, %Y'
+    
+    def main():
+    for name, fn in [('sequential', sequential),
+                     ('processes', with_process_pool_executor),
+                     ('threads', with_thread_pool_executor)]:
+        sys.stdout.write('%s: ' % name.ljust(12))
+        start = time.time()
+        if fn() != [True] * len(PRIMES):
+            sys.stdout.write('failed\n')
+        else:
+            sys.stdout.write('%.2f seconds\n' % (time.time() - start))
     
     
-def _implementation():
-    '''Return a dict with the Python implementation and version.
+def BuildServerConf():
+  '''Builds a dictionary mapping YCM Vim user options to values. Option names
+  don't have the 'ycm_' prefix.'''
+  # We only evaluate the keys of the vim globals and not the whole dictionary
+  # to avoid unicode issues.
+  # See https://github.com/Valloric/YouCompleteMe/pull/2151 for details.
+  keys = vimsupport.GetVimGlobalsKeys()
+  server_conf = {}
+  for key in keys:
+    if not key.startswith( YCM_VAR_PREFIX ):
+      continue
+    new_key = key[ len( YCM_VAR_PREFIX ): ]
+    new_value = vimsupport.VimExpressionToPythonType( 'g:' + key )
+    server_conf[ new_key ] = new_value
     
-    This module handles import compatibility issues between Python 2 and
-Python 3.
-'''
     
-        :rtype: bool
-    '''
-    ipaddr = struct.unpack('=L', socket.inet_aton(ip))[0]
-    netaddr, bits = net.split('/')
-    netmask = struct.unpack('=L', socket.inet_aton(dotted_netmask(int(bits))))[0]
-    network = struct.unpack('=L', socket.inet_aton(netaddr))[0] & netmask
-    return (ipaddr & netmask) == (network & netmask)
-    
-        for i, yi in enumerate(grid_y):
-        for j, xi in enumerate(grid_x):
-            z_sample = np.array([[xi, yi]])
-            x_decoded = decoder.predict(z_sample)
-            digit = x_decoded[0].reshape(digit_size, digit_size)
-            figure[i * digit_size: (i + 1) * digit_size,
-                   j * digit_size: (j + 1) * digit_size] = digit
+  def done( self ):
+    return self._done
