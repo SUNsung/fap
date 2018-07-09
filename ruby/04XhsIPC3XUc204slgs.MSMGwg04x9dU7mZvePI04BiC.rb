814f6d610e2c7484717033343f1b3bd588f215ba
SUNsung <weatherius@gmail.com>
2018-07-10 00:02:18 +0300

    Rewrite [04XhsIPC3XUc204slgs.MSMGwg04x9dU7mZvePI04BiC.rb] File in: 2018-07-10 00:02:17

--- ruby/04XhsIPC3XUc204slgs.MSMGwg04x9dU7mZvePI04BiC.rb
+++ ruby/04XhsIPC3XUc204slgs.MSMGwg04x9dU7mZvePI04BiC.rb
@@ -1,213 +1,137 @@
 
         
-        module ActionCable
-  module Server
-    # An instance of this configuration object is available via ActionCable.server.config, which allows you to tweak Action Cable configuration
-    # in a Rails config initializer.
-    class Configuration
-      attr_accessor :logger, :log_tags
-      attr_accessor :connection_class, :worker_pool_size
-      attr_accessor :disable_request_forgery_protection, :allowed_request_origins, :allow_same_origin_as_host
-      attr_accessor :cable, :url, :mount_path
-    
-            def test_url_host_db
-          spec = resolve 'abstract://foo/bar?encoding=utf8'
-          assert_equal({
-            'adapter'  => 'abstract',
-            'database' => 'bar',
-            'host'     => 'foo',
-            'encoding' => 'utf8' }, spec)
-        end
-    
-        def initialize(table)
-      @table = table
-      @handlers = []
-    
-                  if node.reflection.scope &&
-                  node.reflection.scope_for(node.base_klass.unscoped).readonly_value
-                model.readonly!
-              end
-    
-            case ids.size
-        when 0
-          error_message = 'Couldn't find #{model_name} without an ID'
-          raise RecordNotFound.new(error_message, model_name, primary_key)
-        when 1
-          result = find_one(ids.first)
-          expects_array ? [ result ] : result
-        else
-          find_some(ids)
-        end
-      rescue ::RangeError
-        error_message = 'Couldn't find #{model_name} with an out of range ID'
-        raise RecordNotFound.new(error_message, model_name, primary_key, ids)
-      end
-    
-      def test_should_order_by_grouped_field
-    c = Account.group(:firm_id).order('firm_id').sum(:credit_limit)
-    assert_equal [1, 2, 6, 9], c.keys.compact
-  end
-    
-        def secret_action
-    end
-  end
-    
-            channel = ChatChannel.new connection, 'test_channel'
-        channel.send_confirmation
-        channel.send_confirmation
-    
-          # move bootstrap/_bootstrap.scss to _bootstrap.scss adjusting import paths
-      main_from = '#{save_to}/_bootstrap.scss'
-      main_to   = File.expand_path('#{save_to}/../_bootstrap.scss')
-      save_file main_to, File.read(main_from).gsub(/ '/, ' 'bootstrap/')
-      File.delete(main_from)
-    
-      def setup
-    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
-    success = Dir.chdir DUMMY_PATH do
-      silence_stdout_if !ENV['VERBOSE'] do
-        system 'node', 'manifest.js', tmp_dir
-      end
-    end
-    assert success, 'Node.js Mincer compilation failed'
-    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
-    css_name = manifest['assets']['application.css']
-    @css = File.read('#{tmp_dir}/#{css_name}')
-  end
-end
-
-    
-          File.open('bower.json', 'w') do |f|
-        f.puts JSON.pretty_generate(spec)
+            private_class_method
+    def self.git_log_merge_commit_filtering_option(merge_commit_filtering)
+      case merge_commit_filtering
+      when :exclude_merges
+        '--no-merges'
+      when :only_include_merges
+        '--merges'
+      when :include_merges
+        nil
       end
     end
   end
 end
 
     
-        # Many formulae include 'lib/charset.alias', but it is not strictly needed
-    # and will conflict if more than one formula provides it
-    observe_file_removal @f.lib/'charset.alias'
+            expect(result).to eq('svn info | grep Revision | egrep -o '[0-9]+'')
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('svn info | grep Revision | egrep -o '[0-9]+'')
+      end
+    end
     
-      def self.normalize_legacy_patches(list)
-    patches = []
-    
-            def run
-          UI.puts('$CACHE_ROOT: #{@cache.root}') if @short_output
-          if @pod_name.nil? # Print all
-            @cache.cache_descriptors_per_pod.each do |pod_name, cache_descriptors|
-              print_pod_cache_infos(pod_name, cache_descriptors)
-            end
-          else # Print only for the requested pod
-            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
-            if cache_descriptors.nil?
-              UI.notice('No cache for pod named #{@pod_name} found')
-            else
-              print_pod_cache_infos(@pod_name, cache_descriptors)
-            end
-          end
-        end
-    
-        # we assume that the first file that requires 'sinatra' is the
-    # app_file. all other path related options are calculated based
-    # on this path by default.
-    set :app_file, caller_files.first || $0
-    
-      before(:each) do
-    mock_app do
-      use Rack::Protection::HttpOrigin
-      run DummyApp
+          case Rails.env
+      when 'development'
+        config.eager_load = false
+      when 'test'
+        config.eager_load = false
+      when 'production'
+        config.eager_load = true
+      end
     end
   end
+end
+    
+                  s[:proto] = 'tcp'
+              s[:name]  = 'pop3'
+              s[:extra] = 'Successful Login. Banner: #{s[:banner]}'
+              report_auth_info(s)
+              print_status('Successful POP3 Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]} (#{s[:banner].strip})')
+    
+      def parse(pkt)
+    # We want to return immediantly if	we do not have a packet which is handled by us
+    return unless pkt.is_tcp?
+    return if (pkt.tcp_sport != 80 and pkt.tcp_dport != 80)
+    s = find_session((pkt.tcp_sport == 80) ? get_session_src(pkt) : get_session_dst(pkt))
+    
+    classNames.each { |name|
+	codez << %Q^
+public class #{name} {
+	public static void main(String args[]) {
+		System.out.println('This is from #{name}.');
+	}
+}^}
+    
+    # This is a completely hackish way to do this, and could break with future
+# versions of the JDK.  Need to find a better way to use sun.security.tools.KeyTool
+# and .JarSigner than modifying the source.  These rely on internal APIs that may
+# change.
+signer = Rjb::import('javaCompile.SignJar')
+#clsKeyTool = Rjb::import('sun.security.tools.KeyTool')
+#clsKeyTool = Rjb::import('sun.security.tools.KeyToolMSF')
+#clsJarSigner = Rjb::import('javaCompile.SignJar.JarSignerMSF')
+#clsJarSigner = Rjb::import('sun.security.tools.JarSigner')
+#clsJarSigner = Rjb::import('sun.security.tools.JarSignerMSF')
+    
+      when '6.1.0'
+    __NR_execve      = 6
+    __NR_getpeername = 203
+    __NR_accept      = 229
+    __NR_listen      = 232
+    __NR_bind        = 234
+    __NR_socket      = 235
+    __NR_connect     = 236
+    __NR_close       = 269
+    __NR_kfcntl      = 617
+    
+      def as_json(options={})
+    {
+      poll_id:             id,
+      post_id:             status_message.id,
+      question:            question,
+      poll_answers:        poll_answers,
+      participation_count: participation_count
+    }
+  end
+    
+          # Inspired by https://github.com/nov/openid_connect_sample/blob/master/app/controllers/connect/clients_controller.rb#L24
+      def create
+        registrar = OpenIDConnect::Client::Registrar.new(request.url, params)
+        client = Api::OpenidConnect::OAuthApplication.register! registrar
+        render json: client.as_json(root: false)
+      end
+    
+          def bad_cookies
+        @bad_cookies ||= []
+      end
+    
+    begin
+  require 'escape_utils'
+rescue LoadError
+end
     
         mock_app do
       use Rack::Head
       use(Rack::Config) { |e| e['rack.session'] ||= {}}
-      use changer
-      use klass
       use detector
+      use klass
       run DummyApp
     end
     
-        i0 = index
-    i1, s1 = index, []
-    if has_terminal?('(', false, index)
-      r2 = instantiate_node(SyntaxNode,input, index...(index + 1))
-      @index += 1
-    else
-      terminal_parse_failure('(')
-      r2 = nil
-    end
-    s1 << r2
-    if r2
-      r3 = _nt__
-      s1 << r3
-      if r3
-        r4 = _nt_condition
-        s1 << r4
-        if r4
-          r5 = _nt__
-          s1 << r5
-          if r5
-            if has_terminal?(')', false, index)
-              r6 = instantiate_node(SyntaxNode,input, index...(index + 1))
-              @index += 1
-            else
-              terminal_parse_failure(')')
-              r6 = nil
-            end
-            s1 << r6
-          end
+    When /^(?:|I )attach the file '([^']*)' to '([^']*)'$/ do |path, field|
+  attach_file(field, File.expand_path(path))
+end
+    
+            if attachment.nil?
+          attachment = Attachment.new(name, self, options)
+          instance_variable_set(ivar, attachment)
         end
-      end
-    end
-    if s1.last
-      r1 = instantiate_node(SyntaxNode,input, i1...index, s1)
-      r1.extend(Expression0)
-    else
-      @index = i1
-      r1 = nil
-    end
-    if r1
-      r0 = r1
-      r0.extend(LogStash::Config::AST::Expression)
-    else
-      r7 = _nt_negative_expression
-      if r7
-        r0 = r7
-        r0.extend(LogStash::Config::AST::Expression)
+    
+        def processor(name) #:nodoc:
+      @known_processors ||= {}
+      if @known_processors[name.to_s]
+        @known_processors[name.to_s]
       else
-        r8 = _nt_in_expression
-        if r8
-          r0 = r8
-          r0.extend(LogStash::Config::AST::Expression)
-        else
-          r9 = _nt_not_in_expression
-          if r9
-            r0 = r9
-            r0.extend(LogStash::Config::AST::Expression)
-          else
-            r10 = _nt_compare_expression
-            if r10
-              r0 = r10
-              r0.extend(LogStash::Config::AST::Expression)
-            else
-              r11 = _nt_regexp_expression
-              if r11
-                r0 = r11
-                r0.extend(LogStash::Config::AST::Expression)
-              else
-                r12 = _nt_rvalue
-                if r12
-                  r0 = r12
-                  r0.extend(LogStash::Config::AST::Expression)
-                else
-                  @index = i0
-                  r0 = nil
-                end
-              end
-            end
+        name = name.to_s.camelize
+        load_processor(name) unless Paperclip.const_defined?(name)
+        processor = Paperclip.const_get(name)
+        @known_processors[name.to_s] = processor
+      end
+    end
+    
+            attachment_names.each do |attachment_name|
+          COLUMNS.keys.each do |column_name|
+            remove_column(table_name, '#{attachment_name}_#{column_name}')
           end
         end
       end
-    end
