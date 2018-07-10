675110aaa0810cf24ca6c5577e93c9a33ebc8668
SUNsung <weatherius@gmail.com>
2018-07-10 15:44:16 +0300

    Rewrite [02vkU2nDm69A02kUEzm8sXi8Q02R19fXTyxxDE02ax8.cpp] File in: 2018-07-10 15:44:16

--- cccccc/02vkU2nDm69A02kUEzm8sXi8Q02R19fXTyxxDE02ax8.cpp
+++ cccccc/02vkU2nDm69A02kUEzm8sXi8Q02R19fXTyxxDE02ax8.cpp
@@ -1,66 +1,176 @@
 
         
-        #include 'atom/browser/unresponsive_suppressor.h'
+        #include 'base/values.h'
+#include 'extensions/common/draggable_region.h'
+#include 'content/public/common/common_param_traits.h'
+#include 'ipc/ipc_message_macros.h'
+#include 'ui/gfx/ipc/gfx_param_traits.h'
     
-    namespace mate {
+    #ifndef CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
+#define CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
+    
+    
+#include 'content/nw/src/api/event/event.h'
+#include 'base/values.h'
+#include 'content/nw/src/api/dispatcher_host.h'
+#include 'ui/gfx/screen.h'
+    
+    class BaseEvent {
+  friend class EventListener;
+  DISALLOW_COPY_AND_ASSIGN(BaseEvent);
     }
     
-      // Sent when the chromium session of a particular user is started.
-  // If this is a new user on the machine this will not be sent until a profile
-  // picture has been selected, unlike NOTIFICATION_LOGIN_USER_CHANGED which is
-  // sent immediately after the user has logged in. This will be sent again if
-  // the browser crashes and restarts.
-  // The details are a chromeos::User object.
-  NOTIFICATION_SESSION_STARTED,
+      if (!item->enable_shortcut_)
+    return false;
     
-    #endif  // CHROME_BROWSER_CHROME_PROCESS_FINDER_WIN_H_
+    ui::KeyboardCode GetKeycodeFromText(std::string text){
+  ui::KeyboardCode retval = ui::VKEY_UNKNOWN;
+  if (text.size() != 0){
+    std::string upperText = base::ToUpperASCII(text);
+    std::string keyName = text;
+    bool found = false;
+    if (upperText.size() == 1){
+      char key = upperText[0];
+      if (key>='0' && key<='9'){//handle digital
+        keyName = 'Digit' + upperText;
+        found = true;
+      } else if (key>='A'&&key<='Z'){//handle alphabet
+        keyName = 'Key' + upperText;
+        found = true;
+      }
+    }
+    }
+    }
+    
+      int NwDesktopCaptureMonitor::GetPrimaryMonitorIndex() {
+  #ifdef _WIN32 
+    int count=0;
+    for (int i = 0;; ++i) {
+      DISPLAY_DEVICE device;
+      device.cb = sizeof(device);
+      BOOL ret = EnumDisplayDevices(NULL, i, &device, 0);
+      if(!ret)
+        break;
+      if (device.StateFlags & DISPLAY_DEVICE_ACTIVE){
+        if (device.StateFlags & DISPLAY_DEVICE_PRIMARY_DEVICE){
+          return count;
+        }
+        count++;
+      }
+    }
+  #endif
+    return -1;
+  }
+    
+    
+    {  delete tensor;
+  delete tensor2;
+  cout << 'OK' << endl;
+  return 0;
+}
 
     
-    // An interface the PrintViewManager uses to notify an observer when the print
-// dialog is shown. Register the observer via PrintViewManager::set_observer.
-class PrintViewManagerObserver {
- public:
-  // Notifies the observer that the print dialog was shown.
-  virtual void OnPrintDialogShown() = 0;
-    }
+        virtual bool isCompleted() override;
+    virtual void wait() override;
     
-    #include 'content/public/browser/web_contents_observer.h'
-#include 'ui/gfx/native_widget_types.h'
-    
-    
-    {  delete metric;
-  metric = xgboost::Metric::Create('error@0.1');
-  ASSERT_STREQ(metric->Name(), 'error@0.1');
-  EXPECT_STREQ(metric->Name(), 'error@0.1');
-  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 0, 1e-10);
-  EXPECT_NEAR(GetMetricEval(metric,
-                            {0.1f, 0.2f, 0.1f, 0.2f},
-                            {  0,   0,   1,   1}),
-              0.5f, 0.001f);
+    static PyObject* recursive_to_list(
+    char* data, IntList sizes, IntList strides, int64_t dim,
+    ScalarType scalarType, int64_t elementSize)
+{
+  int64_t ndim = sizes.size();
+  if (dim == ndim) {
+    return torch::utils::load_scalar(data, scalarType);
+  }
+  auto n = sizes[dim];
+  auto list = THPObjectPtr(PyList_New(n));
+  if (!list) throw python_error();
+  for (int64_t i = 0; i < n; i++) {
+    PyObject* obj = recursive_to_list(data, sizes, strides, dim + 1, scalarType, elementSize);
+    if (!obj) throw python_error();
+    PyList_SET_ITEM(list.get(), i, obj);
+    data += strides[dim] * elementSize;
+  }
+  return list.release();
 }
     
-    /*!
- * \brief Macro to register linear updater.
- */
-#define XGBOOST_REGISTER_LINEAR_UPDATER(UniqueId, Name)                        \
-  static DMLC_ATTRIBUTE_UNUSED ::xgboost::LinearUpdaterReg&                    \
-      __make_##LinearUpdaterReg##_##UniqueId##__ =                             \
-          ::dmlc::Registry< ::xgboost::LinearUpdaterReg>::Get()->__REGISTER__( \
-              Name)
+    void THDTensor_(multinomial)(THDLongTensor *self, THDGenerator *_generator,
+                             THDTensor *prob_dist, int n_sample,
+                             int with_replacement) {
+  int start_dim = THDTensor_(nDimension)(prob_dist);
+  if (start_dim == 1) {
+    THDTensor_(resize2d)(prob_dist, 1, THDTensor_(size)(prob_dist, 0));
+  }
+    }
+    
+    extern THDTensorDescriptor* THDPModule_makeDescriptor(PyObject *obj);
+template <typename TensorSrc>
+void THDPInsertCopyFunctionFromWorker(
+  THPCopyList& copyList,
+  void (*copyFunc)(THDTensorDescriptor* x, TensorSrc *z))
+{
+  auto wrapper = [copyFunc](PyObject* dst_, PyObject* src_) {
+    TensorSrc* src = THPTypeInfo<TensorSrc>::cdata(src_);
+    }
+    }
+    
+    #include 'osquery/core/conversions.h'
+    
+      {
+    auto dropper = DropPrivileges::get();
+    EXPECT_TRUE(dropper->dropTo(nobody->pw_uid, nobody->pw_gid));
+    EXPECT_EQ(geteuid(), nobody->pw_uid);
+    }
     
     
-    {// Enable LOG(CONSOLE) for print messages to console.
-#define LOG_CONSOLE ::xgboost::ConsoleLogger()
-// Enable LOG(TRACKER) for print messages to tracker
-#define LOG_TRACKER ::xgboost::TrackerLogger()
-}  // namespace xgboost.
-#endif  // XGBOOST_LOGGING_H_
-
+    {  if (hr != S_OK) {
+    return Status(-1, 'Error retrieving data from WMI query.');
+  }
+  if (value.vt != VT_BOOL) {
+    VariantClear(&value);
+    return Status(-1, 'Invalid data type returned.');
+  }
+  ret = value.boolVal == VARIANT_TRUE ? true : false;
+  VariantClear(&value);
+  return Status(0);
+}
     
-    /*!
- * \brief Registry entry for objective factory functions.
- */
-struct ObjFunctionReg
-    : public dmlc::FunctionRegEntryBase<ObjFunctionReg,
-                                        std::function<ObjFunction* ()> > {
-};
+      // Check that all value lengths were maxed.
+  std::map<std::string, size_t> expected = {
+      {'name', 10}, {'age', 4}, {'food', 23}, {'number', 2}};
+  EXPECT_EQ(lengths, expected);
+    
+      static void DiskDisappearedCallback(DADiskRef disk, void* context);
+    
+      EventTappingEventPublisher() : EventPublisher() {}
+    
+      // Other methods
+  YGValue marginLeadingValue(const YGFlexDirection axis) const;
+  YGValue marginTrailingValue(const YGFlexDirection axis) const;
+  YGValue resolveFlexBasisPtr() const;
+  void resolveDimension();
+  YGDirection resolveDirection(const YGDirection ownerDirection);
+  void clearChildren();
+  /// Replaces the occurrences of oldChild with newChild
+  void replaceChild(YGNodeRef oldChild, YGNodeRef newChild);
+  void replaceChild(YGNodeRef child, uint32_t index);
+  void insertChild(YGNodeRef child, uint32_t index);
+  /// Removes the first occurrence of child
+  bool removeChild(YGNodeRef child);
+  void removeChild(uint32_t index);
+    
+        int unit;
+    double value;
+    
+      auto i = 0;
+  out << 'Backtrace:\n';
+  for (auto& elm : trace) {
+    out << '    #' << dec << setfill('0') << setw(2) << i++ << ' ' << elm << '\n';
+  }
+    
+        method(setExperimentalFeatureEnabled);
+    method(setPointScaleFactor);
+    
+    template<typename... ARGS>
+inline void logw(const char* tag, const char* msg, ARGS... args) noexcept {
+  log(ANDROID_LOG_WARN, tag, msg, args...);
+}
