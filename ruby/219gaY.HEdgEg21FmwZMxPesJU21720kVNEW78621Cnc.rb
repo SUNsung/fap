847afbbffc03cf8b5ee6c8e028377c41fbd19fd2
SUNsung <weatherius@gmail.com>
2018-07-10 19:56:02 +0300

    Rewrite [219gaY.HEdgEg21FmwZMxPesJU21720kVNEW78621Cnc.rb] File in: 2018-07-10 19:56:01

--- ruby/219gaY.HEdgEg21FmwZMxPesJU21720kVNEW78621Cnc.rb
+++ ruby/219gaY.HEdgEg21FmwZMxPesJU21720kVNEW78621Cnc.rb
@@ -1,36 +1,47 @@
 
         
-        require 'test_helper'
-require 'database/setup'
+            options[:only_patterns] = [
+      /\Alanguage\./,
+      /\Aclass\./,
+      /\Afunctions?\./,
+      /\Acontrol-structures/,
+      /\Aregexp\./,
+      /\Areserved\.exceptions/,
+      /\Areserved\.interfaces/,
+      /\Areserved\.variables/]
     
-                        case action
-                    when :listen
-                      pg_conn.exec('LISTEN #{pg_conn.escape_identifier channel}')
-                      @event_loop.post(&callback) if callback
-                    when :unlisten
-                      pg_conn.exec('UNLISTEN #{pg_conn.escape_identifier channel}')
-                    when :shutdown
-                      throw :shutdown
-                    end
-                  end
-    
-        def connect
-      @connected = true
+        def initialize
+      @pages = {}
     end
     
-      def validate_evernote_options
-    unless evernote_consumer_key.present? &&
-      evernote_consumer_secret.present? &&
-      evernote_oauth_token.present?
-      errors.add(:base, 'Evernote ENV variables and a Service are required')
-    end
-  end
-    
-          # This gets the value of the block with the given key.
-      def get(key)
-        key    = Regexp.quote(key)
-        regexp = /^#\s*VAGRANT-BEGIN:\s*#{key}$\r?\n?(.*?)\r?\n?^#\s*VAGRANT-END:\s*#{key}$\r?\n?/m
-        match  = regexp.match(@value)
-        return nil if !match
-        match[1]
+        def scoped_accounts
+      Account.unscoped.tap do |scope|
+        scope.merge! with_usernames
+        scope.merge! matching_username
+        scope.merge! matching_domain
       end
+    end
+    
+    class ActivityPub::EmojiSerializer < ActiveModel::Serializer
+  include RoutingHelper
+    
+    def codepoints_to_unicode(codepoints)
+  if codepoints.include?(' ')
+    codepoints.split(' ').map(&:hex).pack('U*')
+  else
+    [codepoints.hex].pack('U')
+  end
+end
+    
+            user.update(last_sign_in_at: 'Tue, 04 Jul 2017 14:45:56 UTC +00:00', current_sign_in_at: 'Wed, 05 Jul 2017 22:10:52 UTC +00:00')
+    
+    desc 'Start a dummy (test) Rails app server'
+task :dummy_rails do
+  require 'rack'
+  require 'term/ansicolor'
+  port = ENV['PORT'] || 9292
+  puts %Q(Starting on #{Term::ANSIColor.cyan 'http://localhost:#{port}'})
+  Rack::Server.start(
+    config: 'test/dummy_rails/config.ru',
+    Port: port)
+end
