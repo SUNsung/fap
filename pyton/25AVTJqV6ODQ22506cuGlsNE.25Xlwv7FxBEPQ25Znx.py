dc6a33e3b420a777f90c41017bb4f4b15ade1e65
SUNsung <weatherius@gmail.com>
2018-07-11 02:37:00 +0300

    Rewrite [25AVTJqV6ODQ22506cuGlsNE.25Xlwv7FxBEPQ25Znx.py] File in: 2018-07-11 02:36:59

--- pyton/25AVTJqV6ODQ22506cuGlsNE.25Xlwv7FxBEPQ25Znx.py
+++ pyton/25AVTJqV6ODQ22506cuGlsNE.25Xlwv7FxBEPQ25Znx.py
@@ -1,97 +1,85 @@
 
         
-            infile, outfile = args
+            alpha = 0.01  # regularization parameter
     
-        if not releases:
-        break
+        plt.figure('scikit-learn parallel %s benchmark results' % func.__name__)
+    plt.plot(sample_sizes, one_core, label='one core')
+    plt.plot(sample_sizes, multi_core, label='multi core')
+    plt.xlabel('n_samples')
+    plt.ylabel('Time (s)')
+    plt.title('Parallel %s' % func.__name__)
+    plt.legend()
     
-        def test_cache(self):
-        ydl = FakeYDL({
-            'cachedir': self.test_dir,
-        })
-        c = Cache(ydl)
-        obj = {'x': 1, 'y': ['ä', '\\a', True]}
-        self.assertEqual(c.load('test_cache', 'k.'), None)
-        c.store('test_cache', 'k.', obj)
-        self.assertEqual(c.load('test_cache', 'k2'), None)
-        self.assertFalse(_is_empty(self.test_dir))
-        self.assertEqual(c.load('test_cache', 'k.'), obj)
-        self.assertEqual(c.load('test_cache', 'y'), None)
-        self.assertEqual(c.load('test_cache2', 'k.'), None)
-        c.remove()
-        self.assertFalse(os.path.exists(self.test_dir))
-        self.assertEqual(c.load('test_cache', 'k.'), None)
+        op.add_option('--algorithm',
+                  dest='selected_algorithm',
+                  default=default_algorithms,
+                  type=str,
+                  help='Comma-separated list of transformer to benchmark. '
+                       'Default: %default. \nAvailable: %default')
+    
+    mu_second = 0.0 + 10 ** 6  # number of microseconds in a second
     
     
-def get_info_extractor(ie_name):
-    '''Returns the info extractor class with the given ie_name'''
-    return globals()[ie_name + 'IE']
-
+URL = ('http://www.cs.cornell.edu/people/pabo/'
+       'movie-review-data/review_polarity.tar.gz')
+    
+    URL = ('http://people.csail.mit.edu/jrennie/'
+       '20Newsgroups/20news-bydate.tar.gz')
+    
+        # TASK: Build a grid search to find out whether unigrams or bigrams are
+    # more useful.
+    # Fit the pipeline on the training set using grid search for the parameters
+    
+        def plot_support_vectors(self, support_vectors):
+        '''Plot the support vectors by placing circles over the
+        corresponding data points and adds the circle collection
+        to the contours list.'''
+        cs = self.ax.scatter(support_vectors[:, 0], support_vectors[:, 1],
+                             s=80, edgecolors='k', facecolors='none')
+        self.contours.append(cs)
+    
+    # Author: Kemal Eren <kemal@kemaleren.com>
+# License: BSD 3 clause
+    
+    # Plot changes in predicted probabilities via arrows
+plt.figure(0)
+colors = ['r', 'g', 'b']
+for i in range(clf_probs.shape[0]):
+    plt.arrow(clf_probs[i, 0], clf_probs[i, 1],
+              sig_clf_probs[i, 0] - clf_probs[i, 0],
+              sig_clf_probs[i, 1] - clf_probs[i, 1],
+              color=colors[y_test[i]], head_width=1e-2)
+    
+    np.random.seed(0)
+###############################################################################
+n_features = 100
+# simulation covariance matrix (AR(1) process)
+r = 0.1
+real_cov = toeplitz(r ** np.arange(n_features))
+coloring_matrix = cholesky(real_cov)
+    
+    from tornado.options import define, options
+    
+            elif callable(target):
+            return _CallableAdapter(
+                partial(target, **target_params), request.connection
+            )
     
     
-@click.command()
-@click.argument('catalog_file', type=click.Path())
-def cli(catalog_file):
-    # Read the old ones back.  Once we are in, we will never go.
-    with open(catalog_file) as f:
-        rv = json.load(f)['supported_locales']
+def _resolve_addr(host, port, family=socket.AF_UNSPEC):
+    # On Solaris, getaddrinfo fails if the given port is not found
+    # in /etc/services and no socket type is given, so we must pass
+    # one here.  The socket type used here doesn't seem to actually
+    # matter (we discard the one we get back in the results),
+    # so the addresses we return should still be usable with SOCK_DGRAM.
+    addrinfo = socket.getaddrinfo(host, port, family, socket.SOCK_STREAM)
+    results = []
+    for family, socktype, proto, canonname, address in addrinfo:
+        results.append((family, address))
+    return results
     
-    
-def FormatDebugInfoResponse_ExtraConfFoundAndLoaded_test():
-  response = deepcopy( GENERIC_RESPONSE )
-  response[ 'extra_conf' ].update( {
-    'is_loaded': True,
-    'path': '/path/to/extra/conf'
-  } )
-  assert_that(
-    FormatDebugInfoResponse( response ),
-    contains_string(
-      'Extra configuration file found and loaded\n'
-      'Extra configuration path: /path/to/extra/conf\n'
-    )
-  )
-    
-    
-def _ConvertVimDatasToCompletionDatas( response_data ):
-  return [ ConvertVimDataToCompletionData( x )
-           for x in response_data ]
-
-    
-        raise RuntimeError( 'Path in 'g:ycm_server_python_interpreter' option '
-                        'does not point to a valid Python 2.7 or 3.4+.' )
-    
-    
-class FakeFuture( object ):
-  '''A fake version of a future response object, just about suitable for
-  mocking a server response as generated by PostDataToHandlerAsync.
-  Not usually used directly. See MockAsyncServerResponse* methods'''
-  def __init__( self, done, response = None, exception = None ):
-    self._done = done
-    
-    
-  def Extra_Info_No_Doc_String_test( self ):
-    self._Check( 0, {
-      'insertion_text':  'INSERTION TEXT',
-      'menu_text':       'MENU TEXT',
-      'extra_menu_info': 'EXTRA MENU INFO',
-      'kind':            'K',
-      'extra_data': {
-      },
-    }, {
-      'word'     : 'INSERTION TEXT',
-      'abbr'     : 'MENU TEXT',
-      'menu'     : 'EXTRA MENU INFO',
-      'kind'     : 'k',
-      'dup'      : 1,
-      'empty'    : 1,
-      'user_data': '0',
-    } )
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    _logger = logging.getLogger( __name__ )
+        def get_app(self):
+        class App(HTTPServerConnectionDelegate):
+            def start_request(self, server_conn, request_conn):
+                return StreamingChunkSizeTest.MessageDelegate(request_conn)
+        return App()
