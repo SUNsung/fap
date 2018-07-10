bade733f8e0004b5a276cceebb61240c668e23a3
SUNsung <weatherius@gmail.com>
2018-07-10 13:24:04 +0300

    Rewrite [30Iz9hTHvadrw30E9WKiPVQgoU303rrAkujjJPU30i5S.py] File in: 2018-07-10 13:24:03

--- pyton/30Iz9hTHvadrw30E9WKiPVQgoU303rrAkujjJPU30i5S.py
+++ pyton/30Iz9hTHvadrw30E9WKiPVQgoU303rrAkujjJPU30i5S.py
@@ -1,34 +1,207 @@
-    def run(self, args, opts):
-        # load contracts
-        contracts = build_component_list(self.settings.getwithbase('SPIDER_CONTRACTS'))
-        conman = ContractsManager(load_object(c) for c in contracts)
-        runner = TextTestRunner(verbosity=2 if opts.verbose else 1)
-        result = TextTestResult(runner.stream, runner.descriptions, runner.verbosity)
+
+        
+            if s1.st_dev != s2.st_dev:
+        return True     # path/.. on a different device as path
+    if s1.st_ino == s2.st_ino:
+        return True     # path/.. is the same i-node as path, i.e. path=='/'
+    return False
+
     
-            assertion = (self.min_bound <= occurrences <= self.max_bound)
+        if os.path.exists(path):
+        return
     
-    import os
-import warnings
-    
-        hass.services.register(DOMAIN, SERVICE_BROWSE_URL,
-                           lambda service:
-                           webbrowser.open(service.data[ATTR_URL]),
-                           schema=SERVICE_BROWSE_URL_SCHEMA)
+        if result is None:
+        return '{}'
     
     
-def __kilometers_to_meters(kilometers: float) -> float:
-    '''Convert kilometers to meters.'''
-    return kilometers * 1000
+class TerminalModule(TerminalBase):
     
-        @patch('{}._get_homehub_data'.format(patch_file), new=_get_homehub_data)
-    def test_config_minimal(self):
-        '''Test the setup with minimal configuration.'''
-        config = {
-            'device_tracker': {
-                CONF_HOST: 'foo'
-            }
-        }
-        result = bt_home_hub_5.get_scanner(None, config)
+        def construct_yaml_map(self, node):
+        data = AnsibleMapping()
+        yield data
+        value = self.construct_mapping(node)
+        data.update(value)
+        data.ansible_pos = self._node_position_info(node)
     
-            assert sorted(run.entity_ids()) == ['sensor.humidity', 'sensor.lux']
-        assert run.entity_ids(in_run2) == ['sensor.humidity']
+    
+class HashTable(object):
+    
+        def is_face_card(self):
+        '''Jack = 11, Queen = 12, King = 13'''
+        return True if 10 < self._value <= 13 else False
+    
+        def can_fit_vehicle(self, vehicle):
+        if self.vehicle is not None:
+            return False
+        return vehicle.can_fit_in_spot(self)
+    
+            (2016-01, shopping), 125
+        (2016-01, gas), 50
+        '''
+        total = sum(values)
+        self.handle_budget_notifications(key, total)
+        yield key, sum(values)
+    
+                if isinstance(self.input, TokenStream):
+                self.token = self.input.LT(1)
+                self.line = self.token.line
+                self.charPositionInLine = self.token.charPositionInLine
+    
+    
+    def test_conversion(self):
+    
+    def evalString(s):
+    assert s.startswith(''') or s.startswith('''), repr(s[:1])
+    q = s[0]
+    if s[:3] == q*3:
+        q = q*3
+    assert s.endswith(q), repr(s[-len(q):])
+    assert len(s) >= 2*len(q)
+    s = s[len(q):-len(q)]
+    return re.sub(r'\\(\'|\'|\\|[abfnrtv]|x.{0,2}|[0-7]{1,3})', escape, s)
+    
+        def check_syntax_error(self, err, basename, lineno, offset=0):
+        self.assertIn('%s.py, line %d' % (basename, lineno), str(err))
+        self.assertEqual(os.path.basename(err.filename), basename + '.py')
+        self.assertEqual(err.lineno, lineno)
+        self.assertEqual(err.offset, offset)
+    
+        def _get_byteStream(self):
+        return self.byteStream
+    def _set_byteStream(self, byteStream):
+        self.byteStream = byteStream
+    
+    T_CV2 = typing.ClassVar[int]
+T_CV3 = typing.ClassVar
+    
+    @dataclasses.dataclass
+class IV:
+    T_IV4 = dataclasses.InitVar
+    iv0: dataclasses.InitVar[int]
+    iv1: dataclasses.InitVar
+    iv2: T_IV2
+    iv3: T_IV3
+    not_iv4: T_IV4  # When using string annotations, this field is not recognized as an InitVar.
+
+    
+    def _worker(executor_reference, work_queue):
+    try:
+        while True:
+            work_item = work_queue.get(block=True)
+            if work_item is not None:
+                work_item.run()
+                continue
+            executor = executor_reference()
+            # Exit if:
+            #   - The interpreter is shutting down OR
+            #   - The executor that owns the worker has been collected OR
+            #   - The executor that owns the worker has been shutdown.
+            if _shutdown or executor is None or executor._shutdown:
+                # Notice other workers
+                work_queue.put(None)
+                return
+            del executor
+    except BaseException:
+        _base.LOGGER.critical('Exception in worker', exc_info=True)
+    
+    def download_urls_sequential(urls, timeout=60):
+    url_to_content = {}
+    for url in urls:
+        try:
+            url_to_content[url] = load_url(url, timeout=timeout)
+        except:
+            pass
+    return url_to_content
+    
+    
+def KeywordsFromSyntaxListOutput_CppSyntax_test():
+  expected_keywords = (
+    'int_fast32_t', 'FILE', 'size_t', 'bitor', 'typedef', 'const', 'struct',
+    'uint8_t', 'fpos_t', 'thread_local', 'unsigned', 'uint_least16_t', 'do',
+    'intptr_t', 'uint_least64_t', 'return', 'auto', 'void', '_Complex',
+    'break', '_Alignof', 'not', 'using', '_Static_assert', '_Thread_local',
+    'public', 'uint_fast16_t', 'this', 'continue', 'char32_t', 'int16_t',
+    'intmax_t', 'static', 'clock_t', 'sizeof', 'int_fast64_t', 'mbstate_t',
+    'try', 'xor', 'uint_fast32_t', 'int_least8_t', 'div_t', 'volatile',
+    'template', 'char16_t', 'new', 'ldiv_t', 'int_least16_t', 'va_list',
+    'uint_least8_t', 'goto', 'noreturn', 'enum', 'static_assert', 'bitand',
+    'compl', 'imaginary', 'jmp_buf', 'throw', 'asm', 'ptrdiff_t', 'uint16_t',
+    'or', 'uint_fast8_t', '_Bool', 'int32_t', 'float', 'private', 'restrict',
+    'wint_t', 'operator', 'not_eq', '_Imaginary', 'alignas', 'union', 'long',
+    'uint_least32_t', 'int_least64_t', 'friend', 'uintptr_t', 'int8_t', 'else',
+    'export', 'int_fast8_t', 'catch', 'true', 'case', 'default', 'double',
+    '_Noreturn', 'signed', 'typename', 'while', 'protected', 'wchar_t',
+    'wctrans_t', 'uint64_t', 'delete', 'and', 'register', 'false', 'int',
+    'uintmax_t', 'off_t', 'char', 'int64_t', 'int_fast16_t', 'DIR', '_Atomic',
+    'time_t', 'xor_eq', 'namespace', 'virtual', 'complex', 'bool', 'mutable',
+    'if', 'int_least32_t', 'sig_atomic_t', 'and_eq', 'ssize_t', 'alignof',
+    '_Alignas', '_Generic', 'extern', 'class', 'typeid', 'short', 'for',
+    'uint_fast64_t', 'wctype_t', 'explicit', 'or_eq', 'switch', 'uint32_t',
+    'inline' )
+    
+      new_candidates = []
+  for candidate in candidates:
+    if isinstance( candidate, dict ):
+      new_candidate = candidate.copy()
+    
+      python_interpreter = vim.eval( 'g:ycm_server_python_interpreter' )
+  if python_interpreter:
+    python_interpreter = utils.FindExecutable( python_interpreter )
+    if python_interpreter:
+      return python_interpreter
+    
+        try:
+      eq_( expected_vim_data, vim_data )
+    except Exception:
+      print( 'Expected:\n'{0}'\nwhen parsing:\n'{1}'\nBut found:\n'{2}''.format(
+          expected_vim_data,
+          completion_data,
+          vim_data ) )
+      raise
+    
+        def test_sales_manager_shall_talk_through_proxy_with_delay(cls):
+        cls.p.busy = 'No'
+        start_time = time()
+        cls.p.talk()
+        end_time = time()
+        execution_time = end_time - start_time
+        print_output = cls.output.getvalue()
+        expected_print_output = 'Proxy checking for Sales Manager availability\n\
+Sales Manager ready to talk\n'
+        cls.assertEqual(print_output, expected_print_output)
+        expected_execution_time = 1
+        cls.assertEqual(int(execution_time*10), expected_execution_time)
+    
+        move = Action('move')
+    person = Person('Jack', move)
+    person.do_action().amount('5m').stop()
+    
+        def test_frozen_pool(self):
+        with ObjectPool(self.sample_queue) as pool:
+            self.assertEqual(pool, 'first')
+            self.assertEqual(pool, 'first')
+        self.assertTrue(self.sample_queue.get() == 'second')
+        self.assertFalse(self.sample_queue.empty())
+        self.assertTrue(self.sample_queue.get() == 'first')
+        self.assertTrue(self.sample_queue.empty())
+    
+        def now(self):
+        current_time_is_always_midnight = '24:01'
+        return current_time_is_always_midnight
+
+    
+    
+def count_to(count):
+    '''Counts by word numbers, up to a maximum of five'''
+    numbers = ['one', 'two', 'three', 'four', 'five']
+    for number in numbers[:count]:
+        yield number
+    
+        def prepareReporting(self):
+        rvalue = self._db.insert()
+        if rvalue == -1:
+            self._tc.setProblem(1)
+            self._reporter.prepare()
+    
+        def scan(self):
+        self.state.scan()
