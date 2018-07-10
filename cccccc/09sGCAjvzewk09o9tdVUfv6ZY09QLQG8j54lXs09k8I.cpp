06a61fefb94dea1f00a598f1a5936ffe2b5ab77f
SUNsung <weatherius@gmail.com>
2018-07-10 15:25:52 +0300

    Rewrite [09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp] File in: 2018-07-10 15:25:51

--- cccccc/09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp
+++ cccccc/09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp
@@ -1,49 +1,186 @@
 
         
-          /*! \brief constructor */
-  SparsePage() {
-    this->Clear();
-  }
-  /*! \return number of instance in the page */
-  inline size_t Size() const {
-    return offset.size() - 1;
-  }
-  /*! \return estimation of memory cost of this page */
-  inline size_t MemCostBytes() const {
-    return offset.size() * sizeof(size_t) + data.size() * sizeof(Entry);
-  }
-  /*! \brief clear the page */
-  inline void Clear() {
-    base_rowid = 0;
-    offset.clear();
-    offset.push_back(0);
-    data.clear();
-  }
+        
+    {  DraggableRegion();
+};
     
-    namespace xgboost {
+      // Sent when a global error has changed and the error UI should update it
+  // self. The source is a Source<Profile> containing the profile for the
+  // error. The detail is a GlobalError object that has changed or NULL if
+  // all error UIs should update.
+  NOTIFICATION_GLOBAL_ERRORS_CHANGED,
+    
+    #include 'content/public/browser/web_contents_observer.h'
+#include 'ui/gfx/native_widget_types.h'
+    
+    #include 'chrome/common/chrome_constants.h'
+    
+    std::unique_ptr<ScenarioResult> RunScenario(
+    const grpc::testing::ClientConfig& client_config, size_t num_clients,
+    const grpc::testing::ServerConfig& server_config, size_t num_servers,
+    int warmup_seconds, int benchmark_seconds, int spawn_local_worker_count,
+    const grpc::string& qps_server_target_override,
+    const grpc::string& credential_type, bool run_inproc);
+    
+    #include <grpc/support/log.h>
+    
+    // Tucks all generator state in an anonymous namespace away from
+// PythonGrpcGenerator and the header file, mostly to encourage future changes
+// to not require updates to the grpcio-tools C++ code part. Assumes that it is
+// only ever used from a single thread.
+struct PrivateGenerator {
+  const GeneratorConfiguration& config;
+  const grpc_generator::File* file;
     }
     
-    SEXP XGDMatrixSliceDMatrix_R(SEXP handle, SEXP idxset) {
-  SEXP ret;
-  R_API_BEGIN();
-  int len = length(idxset);
-  std::vector<int> idxvec(len);
-  for (int i = 0; i < len; ++i) {
-    idxvec[i] = INTEGER(idxset)[i] - 1;
-  }
-  DMatrixHandle res;
-  CHECK_CALL(XGDMatrixSliceDMatrix(R_ExternalPtrAddr(handle),
-                                   BeginPtr(idxvec), len,
-                                   &res));
-  ret = PROTECT(R_MakeExternalPtr(res, R_NilValue, R_NilValue));
-  R_RegisterCFinalizerEx(ret, _DMatrixFinalizer, TRUE);
-  R_API_END();
-  UNPROTECT(1);
-  return ret;
+    #ifndef TEST_QPS_USAGE_TIMER_H
+#define TEST_QPS_USAGE_TIMER_H
+    
+    TEST(LogTest, MarginalTrailer) {
+  // Make a trailer that is exactly the same length as an empty record.
+  const int n = kBlockSize - 2*kHeaderSize;
+  Write(BigString('foo', n));
+  ASSERT_EQ(kBlockSize - kHeaderSize, WrittenBytes());
+  Write('');
+  Write('bar');
+  ASSERT_EQ(BigString('foo', n), Read());
+  ASSERT_EQ('', Read());
+  ASSERT_EQ('bar', Read());
+  ASSERT_EQ('EOF', Read());
 }
     
-    #include <yoga/Yoga.h>
+      VersionEdit edit;
+  for (int i = 0; i < 4; i++) {
+    TestEncodeDecode(edit);
+    edit.AddFile(3, kBig + 300 + i, kBig + 400 + i,
+                 InternalKey('foo', kBig + 500 + i, kTypeValue),
+                 InternalKey('zoo', kBig + 600 + i, kTypeDeletion));
+    edit.DeleteFile(4, kBig + 700 + i);
+    edit.SetCompactPointer(i, InternalKey('x', kBig + 900 + i, kTypeValue));
+  }
     
-    #define ASSERT_BUF_SIZE 4096
-static char sAssertBuf[ASSERT_BUF_SIZE];
-static AssertHandler gAssertHandler;
+    
+    {  // Pick up remaining bytes
+  switch (limit - data) {
+    case 3:
+      h += static_cast<unsigned char>(data[2]) << 16;
+      FALLTHROUGH_INTENDED;
+    case 2:
+      h += static_cast<unsigned char>(data[1]) << 8;
+      FALLTHROUGH_INTENDED;
+    case 1:
+      h += static_cast<unsigned char>(data[0]);
+      h *= m;
+      h ^= (h >> r);
+      break;
+  }
+  return h;
+}
+    
+      // Read until size drops significantly.
+  std::string limit_key = Key(n);
+  for (int read = 0; true; read++) {
+    ASSERT_LT(read, 100) << 'Taking too long to compact';
+    Iterator* iter = db_->NewIterator(ReadOptions());
+    for (iter->SeekToFirst();
+         iter->Valid() && iter->key().ToString() < limit_key;
+         iter->Next()) {
+      // Drop data
+    }
+    delete iter;
+    // Wait a little bit to allow any triggered compactions to complete.
+    Env::Default()->SleepForMicroseconds(1000000);
+    uint64_t size = Size(Key(0), Key(n));
+    fprintf(stderr, 'iter %3d => %7.3f MB [other %7.3f MB]\n',
+            read+1, size/1048576.0, Size(Key(n), Key(kCount))/1048576.0);
+    if (size <= initial_size/10) {
+      break;
+    }
+  }
+    
+    
+    {  // No copying allowed
+  Writer(const Writer&);
+  void operator=(const Writer&);
+};
+    
+      // Returns an estimate of the current (uncompressed) size of the block
+  // we are building.
+  size_t CurrentSizeEstimate() const;
+    
+    bool ReadJpeg(const uint8_t* data, const size_t len, JpegReadMode mode,
+              JPEGData* jpg) {
+  size_t pos = 0;
+  // Check SOI marker.
+  EXPECT_MARKER();
+  int marker = data[pos + 1];
+  pos += 2;
+  if (marker != 0xd8) {
+    fprintf(stderr, 'Did not find expected SOI marker, actual=%d\n', marker);
+    jpg->error = JPEG_SOI_NOT_FOUND;
+    return false;
+  }
+  int lut_size = kMaxHuffmanTables * kJpegHuffmanLutSize;
+  std::vector<HuffmanTableEntry> dc_huff_lut(lut_size);
+  std::vector<HuffmanTableEntry> ac_huff_lut(lut_size);
+  bool found_sof = false;
+  uint16_t scan_progression[kMaxComponents][kDCTBlockSize] = { { 0 } };
+    }
+    
+    void OutputImage::Downsample(const DownsampleConfig& cfg) {
+  if (components_[1].IsAllZero() && components_[2].IsAllZero()) {
+    // If the image is already grayscale, nothing to do.
+    return;
+  }
+  if (cfg.use_silver_screen &&
+      cfg.u_factor_x == 2 && cfg.u_factor_y == 2 &&
+      cfg.v_factor_x == 2 && cfg.v_factor_y == 2) {
+    std::vector<uint8_t> rgb = ToSRGB();
+    std::vector<std::vector<float> > yuv = RGBToYUV420(rgb, width_, height_);
+    SetDownsampledCoefficients(yuv[0], 1, 1, &components_[0]);
+    SetDownsampledCoefficients(yuv[1], 2, 2, &components_[1]);
+    SetDownsampledCoefficients(yuv[2], 2, 2, &components_[2]);
+    return;
+  }
+  // Get the floating-point precision YUV array represented by the set of
+  // DCT coefficients.
+  std::vector<std::vector<float> > yuv(3, std::vector<float>(width_ * height_));
+  for (int c = 0; c < 3; ++c) {
+    components_[c].ToFloatPixels(&yuv[c][0], 1);
+  }
+    }
+    
+    // Performs in-place floating point 8x8 DCT on block[0..63].
+// Note that the DCT used here is the DCT-2 with the first term multiplied by
+// 1/sqrt(2) and the result scaled by 1/2.
+void ComputeBlockDCTDouble(double block[64]);
+    
+    #include <stddef.h>
+#include <stdint.h>
+    
+    #include 'guetzli/jpeg_data.h'
+    
+    #include 'guetzli/jpeg_data.h'
+    
+    #include 'guetzli/jpeg_data_encoder.h'
+    
+    
+    {  const int width_;
+  const int height_;
+  int factor_x_;
+  int factor_y_;
+  int width_in_blocks_;
+  int height_in_blocks_;
+  int num_blocks_;
+  std::vector<coeff_t> coeffs_;
+  std::vector<uint16_t> pixels_;
+  // Same as last argument of ApplyGlobalQuantization() (default is all 1s).
+  int quant_[kDCTBlockSize];
+};
+    
+    // Preprocesses the u (1) or v (2) channel of the given YUV image (range 0-255).
+std::vector<std::vector<float>> PreProcessChannel(
+    int w, int h, int channel, float sigma, float amount, bool blur,
+    bool sharpen, const std::vector<std::vector<float>>& image);
+    
+    #include 'guetzli/quality.h'
