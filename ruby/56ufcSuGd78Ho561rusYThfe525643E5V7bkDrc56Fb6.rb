a3f03d24086e3dff2bd4d2fa711edca6b990f940
SUNsung <weatherius@gmail.com>
2018-07-10 22:41:16 +0300

    Rewrite [56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb] File in: 2018-07-10 22:41:15

--- ruby/56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb
+++ ruby/56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb
@@ -1,45 +1,49 @@
-  # any user that is either a moderator or an admin
-  def staff?
-    admin || moderator
-  end
-    
-    class ApplicationSerializer < ActiveModel::Serializer
-  embed :ids, include: true
-    
-      def can_read?(resource)
-    ability_name = resource.class.name.downcase
-    ability_name = 'read_#{ability_name}'.to_sym
-    
-      private
-    
-        find_union(segments, Project).includes(:namespace).order_id_desc
-  end
-    
-        if registration
-      u2f.authenticate!(challenges, response, Base64.decode64(registration.public_key), registration.counter)
-      registration.update(counter: response.counter)
-      true
-    end
-  rescue JSON::ParserError, NoMethodError, ArgumentError, U2F::Error
-    false
-  end
-end
 
-    
-    puts 'Deduping #{links.size} links...'
-    
-        def log_processed(name)
-      puts green '    #{name}'
+        
+              topic.feature_topic_users(args)
     end
     
-      # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+      def grant_admin!
+    set_permission('admin', true)
+  end
     
-          spec['main'] =
-          find_files.(File.join(Bootstrap.stylesheets_path, '_bootstrap.scss')) +
-          find_files.(Bootstrap.fonts_path) +
-          %w(assets/javascripts/bootstrap.js)
+          key = Spaceship::Portal::Key.create(name: 'New Key', apns: true, device_check: true, music_id: 'some-music-id')
+      expect(key).to be_instance_of(Spaceship::Portal::Key)
+      expect(key.id).to eq('a-new-key-id')
+    end
+  end
     
-      tasks_dir = Pathname.new('lib/capistrano/tasks')
-  config_dir = Pathname.new('config')
-  deploy_dir = config_dir.join('deploy')
+        options[:attribution] = <<-HTML
+      &copy; 2009&ndash;2018 Jeremy Ashkenas<br>
+      Licensed under the MIT License.
+    HTML
+    
+        alias_method :blank?, :empty?
+    
+    module RuboCop
+  module Cop
+    module Lint
+      # In math and Python, we can use `x < y < z` style comparison to compare
+      # multiple value. However, we can't use the comparison in Ruby. However,
+      # the comparison is not syntax error. This cop checks the bad usage of
+      # comparison operators.
+      #
+      # @example
+      #
+      #   # bad
+      #
+      #   x < y < z
+      #   10 <= x <= 20
+      #
+      # @example
+      #
+      #   # good
+      #
+      #   x < y && y < z
+      #   10 <= x && x <= 20
+      class MultipleCompare < Cop
+        MSG = 'Use the `&&` operator to compare multiple values.'.freeze
+    
+            def_node_matcher :on_body_of_reduce, <<-PATTERN
+          (block (send _recv {:reduce :inject} !sym) _blockargs $(begin ...))
+        PATTERN
