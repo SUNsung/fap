31a7e70fe142beb45740102de2133bbcbde154cd
SUNsung <weatherius@gmail.com>
2018-07-11 11:49:49 +0300

    Rewrite [02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp] File in: 2018-07-11 11:49:48

--- cccccc/02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp
+++ cccccc/02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp
@@ -1,208 +1,114 @@
 
         
-        void PartialRunMgr::PartialRunDone(int step_id, StatusCallback done,
-                                   const Status& status) {
-  Status callback_status;
-  {
-    mutex_lock l(mu_);
-    auto run_it = step_id_to_partial_run_.find(step_id);
-    if (run_it == step_id_to_partial_run_.end()) {
-      return;
-    }
-    run_it->second->final_status.Update(status);
-    if (!run_it->second->executor_done) {
-      // If we found the partial_run, we set the final callback to call only
-      // when the executor is completely done.
-      run_it->second->final_callback = std::move(done);
-      return;
-    }
-    callback_status = run_it->second->final_status;
-  }
-  // Otherwise we call the callback immediately.
-  done(callback_status);
-  mutex_lock l(mu_);
-  step_id_to_partial_run_.erase(step_id);
+        
+void Base::Call(const std::string& method, const base::ListValue& arguments,
+                content::RenderFrameHost* rvh) {
+  NOTREACHED() << 'Uncatched call in Base'
+               << ' method:' << method
+               << ' arguments:' << arguments;
 }
     
-    #include <string>
+    namespace extensions {
+    }
     
-    // TODO(zongheng): this should be a general functor that powers SparseAdd and
-// ScatterNd ops.  It should be moved to its own head file, once the other ops
-// are implemented.
-template <typename Device, typename T, typename Index, int NDIMS,
-          scatter_op::UpdateOp op>
-struct ScatterNdFunctor {
-  // Returns -1 on success or a nonnegative i s.t. indices[i] is a bad index.
-  Index operator()(const Device& d, typename TTypes<Index>::ConstMatrix indices,
-                   typename TTypes<T>::ConstFlat updates,
-                   typename TTypes<T, NDIMS>::Tensor out);
+      protected:
+    ~NwScreenRegisterStreamFunction() override {}
+    DECLARE_EXTENSION_FUNCTION('nw.Screen.registerStream', UNKNOWN)
+    
+    NwShellOpenItemFunction::NwShellOpenItemFunction() {
+}
+    
+    std::string stringFromCFNumber(const CFDataRef& cf_number) {
+  return stringFromCFNumber(cf_number, CFNumberGetType((CFNumberRef)cf_number));
+}
+    
+    /// The following test macros allow pretty test output.
+#define CREATE_DATABASE_TESTS(n)                                               \
+  TEST_F(n, test_plugin_check) {                                               \
+    testPluginCheck();                                                         \
+  }                                                                            \
+  TEST_F(n, test_reset) {                                                      \
+    testReset();                                                               \
+  }                                                                            \
+  TEST_F(n, test_put) {                                                        \
+    testPut();                                                                 \
+  }                                                                            \
+  TEST_F(n, test_get) {                                                        \
+    testGet();                                                                 \
+  }                                                                            \
+  TEST_F(n, test_delete) {                                                     \
+    testDelete();                                                              \
+  }                                                                            \
+  TEST_F(n, test_delete_range) {                                               \
+    testDeleteRange();                                                         \
+  }                                                                            \
+  TEST_F(n, test_scan) {                                                       \
+    testScan();                                                                \
+  }                                                                            \
+  TEST_F(n, test_scan_limit) {                                                 \
+    testScanLimit();                                                           \
+  }
+    
+    TEST_F(PrinterTests, test_generate_row) {
+  std::map<std::string, size_t> lengths;
+  for (const auto& row : q) {
+    computeRowLengths(row, lengths);
+  }
+    }
+    
+    struct DiskArbitrationEventContext : public EventContext {
+  std::string action;
+  std::string path;
+  std::string device_path;
+  std::string name;
+  std::string device;
+  std::string uuid;
+  std::string size;
+  std::string ejectable;
+  std::string mountable;
+  std::string writable;
+  std::string content;
+  std::string media_name;
+  std::string vendor;
+  std::string filesystem;
+  std::string disk_appearance_time;
+  std::string checksum;
 };
     
-    namespace internal {
-template <typename T>
-struct functor_traits<scalar_tanh_fast_derivative_op<T> > {
-  enum {
-    Cost = NumTraits<T>::AddCost * 2 + NumTraits<T>::MulCost * 1,
-    PacketAccess = packet_traits<T>::HasAdd && packet_traits<T>::HasMul &&
-                   packet_traits<T>::HasNegate
-  };
-};
-}  // namespace internal
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    RecognizeCommands::RecognizeCommands(const std::vector<string>& labels,
-                                     int32 average_window_duration_ms,
-                                     float detection_threshold,
-                                     int32 suppression_ms, int32 minimum_count)
-    : labels_(labels),
-      average_window_duration_ms_(average_window_duration_ms),
-      detection_threshold_(detection_threshold),
-      suppression_ms_(suppression_ms),
-      minimum_count_(minimum_count) {
-  labels_count_ = labels.size();
-  previous_top_label_ = '_silence_';
-  previous_top_label_time_ = std::numeric_limits<int64>::min();
-}
-    
-    #ifndef STORAGE_LEVELDB_DB_BUILDER_H_
-#define STORAGE_LEVELDB_DB_BUILDER_H_
-    
-    #ifndef STORAGE_LEVELDB_DB_DB_ITER_H_
-#define STORAGE_LEVELDB_DB_DB_ITER_H_
-    
-    TEST(FormatTest, InternalKey_EncodeDecode) {
-  const char* keys[] = { '', 'k', 'hello', 'longggggggggggggggggggggg' };
-  const uint64_t seq[] = {
-    1, 2, 3,
-    (1ull << 8) - 1, 1ull << 8, (1ull << 8) + 1,
-    (1ull << 16) - 1, 1ull << 16, (1ull << 16) + 1,
-    (1ull << 32) - 1, 1ull << 32, (1ull << 32) + 1
-  };
-  for (int k = 0; k < sizeof(keys) / sizeof(keys[0]); k++) {
-    for (int s = 0; s < sizeof(seq) / sizeof(seq[0]); s++) {
-      TestKey(keys[k], seq[s], kTypeValue);
-      TestKey('hello', 1, kTypeDeletion);
-    }
-  }
-}
-    
-          case kLastSequence:
-        if (GetVarint64(&input, &last_sequence_)) {
-          has_last_sequence_ = true;
-        } else {
-          msg = 'last sequence number';
-        }
-        break;
-    
-    #endif  // STORAGE_LEVELDB_INCLUDE_COMPARATOR_H_
-
-    
-    class Slice;
-    
-      /// Get the timer's expiry time as an absolute time.
-  /**
-   * This function may be used to obtain the timer's current expiry time.
-   * Whether the timer has expired or not does not affect this value.
+      /**
+   * @brief Counter used to shut down thread when too many errors occur.
+   *
+   * This counter is incremented when an error occurs, and decremented when a
+   * log line is processed successfully. If it goes over kErrorThreshold, the
+   * thread will return a nonzero status and stop, preventing us from flooding
+   * the logs when things are in a bad state.
    */
-  time_point expires_at() const
-  {
-    return this->service.expires_at(this->implementation);
-  }
-    
-    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
-# pragma once
-#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
-    
-    #ifndef BOOST_ASIO_BUFFERED_STREAM_FWD_HPP
-#define BOOST_ASIO_BUFFERED_STREAM_FWD_HPP
-    
-    #include <boost/asio/detail/pop_options.hpp>
-    
-    #ifndef BOOST_ASIO_DETAIL_FD_SET_ADAPTER_HPP
-#define BOOST_ASIO_DETAIL_FD_SET_ADAPTER_HPP
+  size_t errorCount_;
     
     
-    {
-    {
-    {} // namespace detail
-} // namespace asio
-} // namespace boost
-    
-    template <typename Function, typename Context>
-inline void invoke(Function& function, Context& context)
-{
-#if !defined(BOOST_ASIO_HAS_HANDLER_HOOKS)
-  Function tmp(function);
-  tmp();
-#else
-  using boost::asio::asio_handler_invoke;
-  asio_handler_invoke(function, boost::asio::detail::addressof(context));
-#endif
+    {  // Too many fields
+  bad_line = R'('2016-03-22T21:17:01.701882+00:00','','6','','','','')';
+  ec = pub.createEventContext();
+  status = pub.populateEventContext(bad_line, ec);
+  ASSERT_FALSE(status.ok());
+  ASSERT_NE(std::string::npos, status.getMessage().find('more'));
 }
     
-    #define BOOST_ASIO_READ_HANDLER_CHECK( \
-    handler_type, handler) \
-  \
-  typedef BOOST_ASIO_HANDLER_TYPE(handler_type, \
-      void(boost::system::error_code, std::size_t)) \
-    asio_true_handler_type; \
-  \
-  BOOST_ASIO_HANDLER_TYPE_REQUIREMENTS_ASSERT( \
-      sizeof(boost::asio::detail::two_arg_handler_test( \
-          boost::asio::detail::clvref< \
-            asio_true_handler_type>(), \
-          static_cast<const boost::system::error_code*>(0), \
-          static_cast<const std::size_t*>(0))) == 1, \
-      'ReadHandler type requirements not met') \
-  \
-  typedef boost::asio::detail::handler_type_requirements< \
-      sizeof( \
-        boost::asio::detail::argbyv( \
-          boost::asio::detail::clvref< \
-            asio_true_handler_type>())) + \
-      sizeof( \
-        boost::asio::detail::lvref< \
-          asio_true_handler_type>()( \
-            boost::asio::detail::lvref<const boost::system::error_code>(), \
-            boost::asio::detail::lvref<const std::size_t>()), \
-        char(0))> BOOST_ASIO_UNUSED_TYPEDEF
     
+    {  return new (zone)
+      CallDescriptor(CallDescriptor::kCallCodeObject,  // kind
+                     MachineType::AnyTagged(),         // target MachineType
+                     LinkageLocation::ForAnyRegister(
+                         MachineType::AnyTagged()),  // target location
+                     locations.Build(),              // location_sig
+                     stack_param_count,              // stack_parameter_count
+                     Operator::kNoProperties,        // properties
+                     kNoCalleeSaved,                 // callee-saved registers
+                     kNoCalleeSaved,                 // callee-saved fp
+                     CallDescriptor::kNoFlags);      // flags
+}
     
-/*
- * DumpCrashStack.h
- *
- *  Created on: 2012-9-28
- *      Author: yerungui
- */
+    using compiler::Node;
     
-    class WakeUpLock {
-  public:
-    WakeUpLock();
-    ~WakeUpLock();
-    }
-    
-    
-    {  private:
-    size_t count_;
-    uint64_t time_span_;
-    std::list<uint64_t> touch_times_;
-};
-    
-      public:
-    template<typename T>
-    T* Service() {
-        if (m_publicservices.end() != m_publicservices.find(T::ServiceName()))
-            return (T*)m_publicservices[T::ServiceName()];
-    }
-    
-    
-    {  private:
-//    int m_t;
-};
-    
-    
-/**
- * created on : 2012-07-16
- * author    : yanguoyue
- */
+    #include 'src/ast/scopes.h'
+#include 'src/ast/variables.h'
