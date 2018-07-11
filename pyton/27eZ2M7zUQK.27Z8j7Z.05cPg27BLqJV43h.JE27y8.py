0fcb09c9c09fbc4ebb76751fce7258b47d763b4f
SUNsung <weatherius@gmail.com>
2018-07-11 20:45:36 +0300

    Rewrite [27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py] File in: 2018-07-11 20:45:36

--- pyton/27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py
+++ pyton/27eZ2M7zUQK.27Z8j7Z.05cPg27BLqJV43h.JE27y8.py
@@ -1,55 +1,43 @@
 
         
-            def __set__(self, obj, value):
-        obj.config[self.__name__] = value
+            def from_envvar(self, variable_name, silent=False):
+        '''Loads a configuration from an environment variable pointing to
+        a configuration file.  This is basically just a shortcut with nicer
+        error messages for this line of code::
+    
+        for idx, (loader, srcobj, triple) in enumerate(attempts):
+        if isinstance(srcobj, Flask):
+            src_info = 'application '%s'' % srcobj.import_name
+        elif isinstance(srcobj, Blueprint):
+            src_info = 'blueprint '%s' (%s)' % (srcobj.name,
+                                                srcobj.import_name)
+        else:
+            src_info = repr(srcobj)
+    
+        def make_null_session(self, app):
+        '''Creates a null session which acts as a replacement object if the
+        real session support could not be loaded due to a configuration
+        error.  This mainly aids the user experience because the job of the
+        null session is to still support lookup without complaining but
+        modifications are answered with a helpful error message of what
+        failed.
+    
+        rv = parse_changelog()
+    
+        class Test(Base):
+        SECRET_KEY = 'config'
     
     
-def create_logger(app):
-    '''Get the ``'flask.app'`` logger and configure it if needed.
-    
-            # chop off the port which is usually not supported by browsers
-        # remove any leading '.' since we'll add that later
-        rv = rv.rsplit(':', 1)[0].lstrip('.')
-    
-            if cls.decorators:
-            view.__name__ = name
-            view.__module__ = cls.__module__
-            for decorator in cls.decorators:
-                view = decorator(view)
-    
-        try:
-        with app.test_request_context('/', environ_overrides={'HTTP_HOST': 'localhost'}):
-            pass
-    except ValueError as e:
-        assert str(e) == (
-            'the server name provided '
-            '('localhost.localdomain:5000') does not match the '
-            'server name from the WSGI environment ('localhost')'
-        )
-    
-    
-def write_with_harness(codefile, sourcefile, start_linenum, linebuffer):
-    '''write output with additional lines to make code likely compilable'''
-    # add commonly used headers, so that lines can likely compile.
-    # This is work in progress, the main issue remains handling class
-    # declarations in in-function code differently
-    with io.open(codefile, 'w') as code_filehandle:
-        code_filehandle.write('''\
-#include<stdio.h>      // by md-split
-#include<stdlib.h>     // by md-split
-#include<tuple>        // by md-split
-#include<utility>      // by md-split
-#include<limits>       // by md-split
-#include<functional>   // by md-split
-#include<string>       // by md-split
-#include<map>          // by md-split
-#include<iostream>     // by md-split
-#include<vector>       // by md-split
-#include<algorithm>    // by md-split
-#include<memory>       // by md-split
-using namespace std;   // by md-split
-// %s : %s
-''' % (sourcefile, start_linenum))
-        # TODO: if not toplevel code, wrap inside class
-        for codeline in linebuffer:
-            code_filehandle.write(codeline)
+def test_egg_installed_paths(install_egg, modules_tmpdir,
+                             modules_tmpdir_prefix):
+    modules_tmpdir.mkdir('site_egg').join('__init__.py').write(
+        'import flask\n\napp = flask.Flask(__name__)'
+    )
+    install_egg('site_egg')
+    try:
+        import site_egg
+        assert site_egg.app.instance_path == \
+            str(modules_tmpdir.join('var/').join('site_egg-instance'))
+    finally:
+        if 'site_egg' in sys.modules:
+            del sys.modules['site_egg']
