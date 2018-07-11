d08ad41b1f4b32552013f4ba0679965c41d18663
SUNsung <weatherius@gmail.com>
2018-07-11 22:27:34 +0300

    Rewrite [25MPd3hN3i30U25lIK0wa0muZ.25quzdMCjiS7s25YgO.py] File in: 2018-07-11 22:27:33

--- pyton/25MPd3hN3i30U25lIK0wa0muZ.25quzdMCjiS7s25YgO.py
+++ pyton/25MPd3hN3i30U25lIK0wa0muZ.25quzdMCjiS7s25YgO.py
@@ -1,37 +1,23 @@
 
         
-        anchor = '###'
-min_entries_per_section = 3
-auth_keys = ['apiKey', 'OAuth', 'X-Mashape-Key', 'No']
-punctuation = ['.', '?', '!']
-https_keys = ['Yes', 'No']
-cors_keys = ['Yes', 'No', 'Unknown']
+            def test_subsequent_wait_calls(self):
+        '''
+        This test makes sure that a second call to wait()
+        clears the first timeout.
+        '''
+        self.io_loop.add_timeout(self.io_loop.time() + 0.00, self.stop)
+        self.wait(timeout=0.02)
+        self.io_loop.add_timeout(self.io_loop.time() + 0.03, self.stop)
+        self.wait(timeout=0.15)
     
-            self.assertTrue(self.addr_default.conflicts(self.addr))
-        self.assertTrue(self.addr_default.conflicts(self.addr1))
-        self.assertTrue(self.addr_default.conflicts(self.addr_defined))
-    
-    # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
-    
-        @mock.patch('certbot_compatibility_test.validator.requests.get')
-    def test_redirect_missing_location(self, mock_get_request):
-        mock_get_request.return_value = create_response(301)
-        self.assertFalse(self.validator.redirect('test.com'))
-    
-    # One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'certbot-compatibility-test',
-     u'certbot-compatibility-test Documentation',
-     [author], 1)
-]
-    
-    # The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
-# The short X.Y version.
-version = '0'
-# The full version, including alpha/beta/rc tags.
-release = '0'
+        def finish(self):
+        if self._decompressor is not None:
+            tail = self._decompressor.flush()
+            if tail:
+                # I believe the tail will always be empty (i.e.
+                # decompress will return all it can).  The purpose
+                # of the flush call is to detect errors such
+                # as truncated input.  But in case it ever returns
+                # anything, treat it as an extra chunk
+                self._delegate.data_received(tail)
+        return self._delegate.finish()
