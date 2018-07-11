efd1b53492ce6325750590f7c12ad02fccd8c6b5
SUNsung <weatherius@gmail.com>
2018-07-11 18:22:33 +0300

    Rewrite [28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py] File in: 2018-07-11 18:22:33

--- pyton/28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py
+++ pyton/28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py
@@ -1,74 +1,132 @@
 
         
-        filenames = {
-    'bin': 'youtube-dl',
-    'exe': 'youtube-dl.exe',
-    'tar': 'youtube-dl-%s.tar.gz' % version}
-build_dir = os.path.join('..', '..', 'build', version)
-for key, filename in filenames.items():
-    url = 'https://yt-dl.org/downloads/%s/%s' % (version, filename)
-    fn = os.path.join(build_dir, filename)
-    with open(fn, 'rb') as f:
-        data = f.read()
-    if not data:
-        raise ValueError('File %s is empty!' % fn)
-    sha256sum = hashlib.sha256(data).hexdigest()
-    new_version[key] = (url, sha256sum)
+            if len(result) != 1:
+        raise Exception(
+            'Received %d items instead of 1 looking for %s in:\n%s' % (
+                len(result),
+                args.project,
+                json.dumps(result, indent=4, sort_keys=True)))
     
-        def test_youtube_show_matching(self):
-        self.assertMatch('http://www.youtube.com/show/airdisasters', ['youtube:show'])
+        # This matches a hostname or host pattern including [x:y(:z)] ranges.
+    #
+    # We roughly follow DNS rules here, but also allow ranges (and underscores).
+    # In the past, no systematic rules were enforced about inventory hostnames,
+    # but the parsing context (e.g. shlex.split(), fnmatch.fnmatch()) excluded
+    # various metacharacters anyway.
+    #
+    # We don't enforce DNS length restrictions here (63 characters per label,
+    # 253 characters total) or make any attempt to process IDNs.
     
-        def test_cache(self):
-        ydl = FakeYDL({
-            'cachedir': self.test_dir,
-        })
-        c = Cache(ydl)
-        obj = {'x': 1, 'y': ['ä', '\\a', True]}
-        self.assertEqual(c.load('test_cache', 'k.'), None)
-        c.store('test_cache', 'k.', obj)
-        self.assertEqual(c.load('test_cache', 'k2'), None)
-        self.assertFalse(_is_empty(self.test_dir))
-        self.assertEqual(c.load('test_cache', 'k.'), obj)
-        self.assertEqual(c.load('test_cache', 'y'), None)
-        self.assertEqual(c.load('test_cache2', 'k.'), None)
-        c.remove()
-        self.assertFalse(os.path.exists(self.test_dir))
-        self.assertEqual(c.load('test_cache', 'k.'), None)
+        results = {}
+    for group in sort_groups(groups):
+        results = combine_vars(results, group.get_vars())
     
     
-def list_extractors(age_limit):
-    '''
-    Return a list of extractors that are suitable for the given age,
-    sorted by extractor ID.
-    '''
+class FullJitterBackoffStrategyTestCase(unittest.TestCase):
+    def test_no_retries(self):
+        strategy = _full_jitter_backoff(retries=0)
+        result = list(strategy())
+        self.assertEquals(result, [], 'list should be empty')
     
-            page = self._download_json(
-            'http://vxml.56.com/json/%s/' % text_id, text_id, 'Downloading video info')
+    In the second benchmark, we increase the number of dimensions of the
+training set. Then we plot the computation time as function of
+the number of dimensions.
     
     
-def _py_files(folder):
-    return glob.glob(folder + '/*.py') + glob.glob(folder + '/*/*.py')
+def get_data(N, D, dataset='dense'):
+    if dataset == 'dense':
+        np.random.seed(0)
+        return np.random.random((N, D))
+    elif dataset == 'digits':
+        X = datasets.load_digits().data
+        i = np.argsort(X[0])[::-1]
+        X = X[:, i]
+        return X[:N, :D]
+    else:
+        raise ValueError('invalid dataset: %s' % dataset)
     
-            self.tail.appendleft(delta)
-        self.lasttime = now
-        self.concurrent += 1
+        for n_samples in sample_sizes:
+        X = random_state.rand(n_samples, 300)
     
-        def long_desc(self):
-        '''A long description of the command. Return short description when not
-        available. It cannot contain newlines, since contents will be formatted
-        by optparser which removes newlines and wraps text.
-        '''
-        return self.short_desc()
+        print('Decompressing %s' % ARCHIVE_NAME)
+    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
+        archive.extractall(path='.')
+    os.remove(ARCHIVE_NAME)
+
     
-        def run(self, args, opts):
-        if opts.list:
-            self._list_templates()
-            return
-        if opts.dump:
-            template_file = self._find_template(opts.dump)
-            if template_file:
-                with open(template_file, 'r') as f:
-                    print(f.read())
-            return
-        if len(args) != 2:
-            raise UsageError()
+        # the training data folder must be passed as first argument
+    movie_reviews_data_folder = sys.argv[1]
+    dataset = load_files(movie_reviews_data_folder, shuffle=False)
+    print('n_samples: %d' % len(dataset.data))
+    
+    model = SpectralBiclustering(n_clusters=n_clusters, method='log',
+                             random_state=0)
+model.fit(data)
+score = consensus_score(model.biclusters_,
+                        (rows[:, row_idx], columns[:, col_idx]))
+    
+    # Annotate points on the simplex
+plt.annotate(r'($\frac{1}{3}$, $\frac{1}{3}$, $\frac{1}{3}$)',
+             xy=(1.0/3, 1.0/3), xytext=(1.0/3, .23), xycoords='data',
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             horizontalalignment='center', verticalalignment='center')
+plt.plot([1.0/3], [1.0/3], 'ko', ms=5)
+plt.annotate(r'($\frac{1}{2}$, $0$, $\frac{1}{2}$)',
+             xy=(.5, .0), xytext=(.5, .1), xycoords='data',
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             horizontalalignment='center', verticalalignment='center')
+plt.annotate(r'($0$, $\frac{1}{2}$, $\frac{1}{2}$)',
+             xy=(.0, .5), xytext=(.1, .5), xycoords='data',
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             horizontalalignment='center', verticalalignment='center')
+plt.annotate(r'($\frac{1}{2}$, $\frac{1}{2}$, $0$)',
+             xy=(.5, .5), xytext=(.6, .6), xycoords='data',
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             horizontalalignment='center', verticalalignment='center')
+plt.annotate(r'($0$, $0$, $1$)',
+             xy=(0, 0), xytext=(.1, .1), xycoords='data',
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             horizontalalignment='center', verticalalignment='center')
+plt.annotate(r'($1$, $0$, $0$)',
+             xy=(1, 0), xytext=(1, .1), xycoords='data',
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             horizontalalignment='center', verticalalignment='center')
+plt.annotate(r'($0$, $1$, $0$)',
+             xy=(0, 1), xytext=(.1, 1), xycoords='data',
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             horizontalalignment='center', verticalalignment='center')
+# Add grid
+plt.grid('off')
+for x in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
+    plt.plot([0, x], [x, 0], 'k', alpha=0.2)
+    plt.plot([0, 0 + (1-x)/2], [x, x + (1-x)/2], 'k', alpha=0.2)
+    plt.plot([x, x + (1-x)/2], [0, 0 + (1-x)/2], 'k', alpha=0.2)
+    
+        test_suite = 'tests',
+    
+            account_number = match1(html, r'data-account='(\d+)'')
+    
+    site_info = 'ehow.com'
+download = ehow_download
+download_playlist = playlist_not_supported('ehow')
+
+    
+            # construct available streams
+        if orig_img: self.streams['original'] = {'url': orig_img}
+        if twit_img: self.streams['small'] = {'url': twit_img}
+    
+    #----------------------------------------------------------------------
+def showroom_get_roomid_by_room_url_key(room_url_key):
+    '''str->str'''
+    fake_headers_mobile = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'UTF-8,*;q=0.5',
+        'Accept-Encoding': 'gzip,deflate,sdch',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'
+    }
+    webpage_url = 'https://www.showroom-live.com/' + room_url_key
+    html = get_content(webpage_url, headers = fake_headers_mobile)
+    roomid = match1(html, r'room\?room_id\=(\d+)')
+    assert roomid
+    return roomid
