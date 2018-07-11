4e332d3a68d19f1f6a88ef76d909adc7b4b36fcc
SUNsung <weatherius@gmail.com>
2018-07-12 01:49:29 +0300

    Rewrite [27gCkfuvGM8.27oHAo2.J6drI27FAQV9iScj..27DpC.rb] File in: 2018-07-12 01:49:29

--- ruby/27gCkfuvGM8.27oHAo2.J6drI27FAQV9iScj..27DpC.rb
+++ ruby/27gCkfuvGM8.27oHAo2.J6drI27FAQV9iScj..27DpC.rb
@@ -1,44 +1,59 @@
 
         
-                    raise SystemExit unless process.success?
-          end
-        end
-      end
-    end
-  end
+          url = File.dirname(url)
+  url == FORWARD_SLASH ? url : '#{url}/'
 end
-
     
-          # Run a certain action
-      def trigger(task: nil, flags: nil, serial: nil, print_command: true, print_command_output: true)
-        android_serial = (serial != '') ? 'ANDROID_SERIAL=#{serial}' : nil
-        command = [android_serial, escaped_gradle_path, task, flags].compact.join(' ')
-        Action.sh(command, print_command: print_command, print_command_output: print_command_output)
-      end
+            def process(args, options = {})
+          raise ArgumentError, 'You must specify a path.' if args.empty?
     
-            expect(result).to eq('hg parent --template '{node|short}'')
-        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('hg parent --template '{node|short}'')
-      end
+              new_theme_name = args.join('_')
+          theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
+          Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.' if theme.path.exist?
     
-      def validate_email_options
-    errors.add(:base, 'subject and expected_receive_period_in_days are required') unless options['subject'].present? && options['expected_receive_period_in_days'].present?
+    require 'webrick'
     
-          _form_configurable_fields[name] = options
+      def set_table_sort(sort_options)
+    valid_sorts = sort_options[:sorts] or raise ArgumentError.new('You must specify :sorts as an array of valid sort attributes.')
+    default = sort_options[:default] || { valid_sorts.first.to_sym => :desc }
+    
+        # we assume that the first file that requires 'sinatra' is the
+    # app_file. all other path related options are calculated based
+    # on this path by default.
+    set :app_file, caller_files.first || $0
+    
+            modes       = Array options[:escape]
+        @escaper    = options[:escaper]
+        @html       = modes.include? :html
+        @javascript = modes.include? :javascript
+        @url        = modes.include? :url
+    
+      describe '#referrer' do
+    it 'Reads referrer from Referer header' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
+      expect(subject.referrer(env)).to eq('bar.com')
     end
     
-      def tumblr_consumer_secret
-    ENV['TUMBLR_OAUTH_SECRET']
+      it 'accepts requests with the same Accept-Language header' do
+    session = {:foo => :bar}
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
+    expect(session).not_to be_empty
   end
     
-      def new
-    @user_credential = current_user.user_credentials.build
+      it 'should allow changing the protection mode' do
+    # I have no clue what other modes are available
+    mock_app do
+      use Rack::Protection::XSSHeader, :xss_mode => :foo
+      run DummyApp
+    end
     
-          origin = caller[1]
-      if origin =~ /rubygems\/custom_require/
-        origin = caller[3]
-        if origin.nil?
-          STDERR.puts 'Unknown origin'
-          STDERR.puts caller.join('\n')
-        end
-      end
-      origin = origin.gsub(/:[0-9]+:in .*/, '') if origin
+      # Many RSpec users commonly either run the entire suite or an individual
+  # file, and it's useful to allow more verbose output when running an
+  # individual spec file.
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured
+    # (e.g. via a command-line flag).
+    config.default_formatter = 'doc'
+  end
