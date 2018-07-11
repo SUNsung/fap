a8c3b08571f54ab3a24eea6d977cf44f2812b647
SUNsung <weatherius@gmail.com>
2018-07-11 21:28:32 +0300

    Rewrite [42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb] File in: 2018-07-11 21:28:31

--- ruby/42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb
+++ ruby/42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb
@@ -1,95 +1,29 @@
 
         
-                def generate_confirmation_token!
-          generate_confirmation_token && save(validate: false)
-        end
-    
-        # The path used after confirmation.
-    def after_confirmation_path_for(resource_name, resource)
-      if signed_in?(resource_name)
-        signed_in_root_path(resource)
-      else
-        new_session_path(resource_name)
-      end
-    end
-    
-        def confirmation_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :confirmation_instructions, opts)
-    end
-    
-    def grammar(f, out)
-  while line = f.gets
-    case line
-    when %r</\*% *ripper(?:\[(.*?)\])?: *(.*?) *%\*/>
-      out << DSL.new($2, ($1 || '').split(',')).generate << $/
-    when %r</\*%%%\*/>
-      out << '#if 0' << $/
-    when %r</\*%>
-      out << '#endif' << $/
-    when %r<%\*/>
-      out << $/
-    when /\A%%/
-      out << '%%' << $/
-      return
+        def process_args
+  until ARGV.empty?
+    if ARGV.first =~ %r{^-+t(?:est)?$} && ARGV.length > 1
+      ARGV.shift
+      $opt_test = ARGV.shift
+    elsif ARGV.first =~ %r{^-+h(?:elp)?$}
+      puts usage
+      exit 0
     else
-      out << line
+      puts usage
+      exit 1
     end
   end
 end
     
-    # TODO: Resolve these two tables with actual specs. As the comment at the
-# top suggests, these specs need to be reorganized into a single describe
-# block for each operator. The describe block should include an example
-# for associativity (if relevant), an example for any short circuit behavior
-# (e.g. &&, ||, etc.) and an example block for each operator over which the
-# instant operator has immediately higher precedence.
-    
-    #
-# SortedSet implements a Set that guarantees that its elements are
-# yielded in sorted order (according to the return values of their
-# #<=> methods) when iterating over them.
-#
-# All elements that are added to a SortedSet must respond to the <=>
-# method for comparison.
-#
-# Also, all elements must be <em>mutually comparable</em>: <tt>el1 <=>
-# el2</tt> must not return <tt>nil</tt> for any elements <tt>el1</tt>
-# and <tt>el2</tt>, else an ArgumentError will be raised when
-# iterating over the SortedSet.
-#
-# == Example
-#
-#   require 'set'
-#
-#   set = SortedSet.new([2, 1, 5, 6, 4, 5, 3, 3, 3])
-#   ary = []
-#
-#   set.each do |obj|
-#     ary << obj
-#   end
-#
-#   p ary # => [1, 2, 3, 4, 5, 6]
-#
-#   set2 = SortedSet.new([1, 2, '3'])
-#   set2.each { |obj| } # => raises ArgumentError: comparison of Fixnum with String failed
-#
-class SortedSet < Set
-  @@setup = false
-  @@mutex = Mutex.new
-    
-      Car = Struct.new(:make, :model, :year)
-    
-            def block_argument?
-          argument? && @scope.node.block_type?
-        end
-    
-            def variables_in_node(node)
-          if node.or_type?
-            node.node_parts
-                .flat_map { |node_part| variables_in_node(node_part) }
-                .uniq
-          else
-            variables_in_simple_node(node)
-          end
-        end
+      def brief_build_info(f)
+    build_time_str = f.logs.ctime.strftime('%Y-%m-%d %H:%M:%S')
+    s = <<~EOS
+      Homebrew build logs for #{f.full_name} on #{OS_VERSION}
+    EOS
+    if ARGV.include?('--with-hostname')
+      hostname = Socket.gethostname
+      s << 'Host: #{hostname}\n'
+    end
+    s << 'Build date: #{build_time_str}\n'
+    s
+  end
