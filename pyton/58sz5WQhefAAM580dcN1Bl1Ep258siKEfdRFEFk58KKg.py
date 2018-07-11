e90203c2bc551949f66b90da110682bd6987ef6a
SUNsung <weatherius@gmail.com>
2018-07-11 15:20:07 +0300

    Rewrite [58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py] File in: 2018-07-11 15:20:07

--- pyton/58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py
+++ pyton/58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py
@@ -1,99 +1,125 @@
 
         
-            def test_youtube_playlist_matching(self):
-        assertPlaylist = lambda url: self.assertMatch(url, ['youtube:playlist'])
-        assertPlaylist('ECUl4u3cNGP61MdtwGTqZA0MreSaDybji8')
-        assertPlaylist('UUBABnxM4Ar9ten8Mdjj1j0Q')  # 585
-        assertPlaylist('PL63F0C78739B09958')
-        assertPlaylist('https://www.youtube.com/playlist?list=UUBABnxM4Ar9ten8Mdjj1j0Q')
-        assertPlaylist('https://www.youtube.com/course?list=ECUl4u3cNGP61MdtwGTqZA0MreSaDybji8')
-        assertPlaylist('https://www.youtube.com/playlist?list=PLwP_SiAcdui0KVebT0mU9Apz359a4ubsC')
-        assertPlaylist('https://www.youtube.com/watch?v=AV6J6_AeFEQ&playnext=1&list=PL4023E734DA416012')  # 668
-        self.assertFalse('youtube:playlist' in self.matching_ies('PLtS2H6bU1M'))
-        # Top tracks
-        assertPlaylist('https://www.youtube.com/playlist?list=MCUS.20142101')
-    
-            self.port = random.randint(20000, 30000)
-        self.server_process = subprocess.Popen([
-            'srelay', '-f', '-i', '127.0.0.1:%d' % self.port],
-            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
-            webpage = self._download_webpage(url, video_id)
-        key = self._search_regex(
-            r'src=['\']https?://[^/]+/embed/([A-Za-z0-9_-]+)', webpage, 'key')
-    
-        @app.errorhandler(Foo)
-    def handle_foo(e):
-        return str(e.whatever)
-    
-        data_train = fetch_20newsgroups_vectorized(subset='train')
-    data_test = fetch_20newsgroups_vectorized(subset='test')
-    X_train = check_array(data_train.data, dtype=np.float32,
-                          accept_sparse='csc')
-    X_test = check_array(data_test.data, dtype=np.float32, accept_sparse='csr')
-    y_train = data_train.target
-    y_test = data_test.target
-    
-        plt.figlegend((c_bar, q_bar), ('construction', 'N-point query'),
-                  'upper right')
-    
-    
-def rbf_kernels(X, n_jobs):
-    return pairwise_kernels(X, metric='rbf', n_jobs=n_jobs, gamma=0.1)
-    
-        url_fmt is along the lines of ('https://github.com/USER/PROJECT/'
-                                   'blob/{revision}/{package}/'
-                                   '{path}#L{lineno}')
+        
+def check_alphabetical(lines):
     '''
-    revision = _get_git_revision()
-    return partial(_linkcode_resolve, revision=revision, package=package,
-                   url_fmt=url_fmt)
-
-    
-    
-# TASK: Build a vectorizer that splits strings into sequence of 1 to 3
-# characters instead of word tokens
-    
-    
-def generate_data(n_samples, n_features):
-    '''Generate random blob-ish data with noisy features.
-    
-    We generate data from three groups of waveforms. Two of the waveforms
-(waveform 1 and waveform 2) are proportional one to the other. The cosine
-distance is invariant to a scaling of the data, as a result, it cannot
-distinguish these two waveforms. Thus even with no noise, clustering
-using this distance will not separate out waveform 1 and 2.
-    
-        #print(len(urls))
-    
-    
-class AbstractExpert(object):
-    
-            self.assertEqual(noise, expected_noise)
-
-    
-        def test_items_recoil(self):
-        with ObjectPool(self.sample_queue, True) as pool:
-            self.assertEqual(pool, 'first')
-        self.assertTrue(self.sample_queue.get() == 'second')
-        self.assertFalse(self.sample_queue.empty())
-        self.assertTrue(self.sample_queue.get() == 'first')
-        self.assertTrue(self.sample_queue.empty())
-    
-        def test_bunch_launch(self):
-        self.runner.runAll()
-        output = self.out.getvalue().strip()
-        self.assertEqual(output, str(self.average_result_tc1 + '\n\n' +
-                         self.average_result_tc2 + '\n\n' +
-                         self.average_result_tc3))
-
-    
+    checks if all entries per section are in alphabetical order based in entry title
     '''
-Port of the Java example of 'Setter Injection' in
-'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
-(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
-http://xunitpatterns.com/Dependency%20Injection.html.
+    sections = {}
+    section_line_num = {}
+    for line_num, line in enumerate(lines):
+        if line.startswith(anchor):
+            category = line.split(anchor)[1].strip()
+            sections[category] = []
+            section_line_num[category] = line_num
+            continue
+        if not line.startswith('|') or line.startswith('|---'):
+            continue
+        title = [x.strip() for x in line.split('|')[1:-1]][0].upper()
+        sections[category].append(title)
     
-    class ConstructorInjectionTest(unittest.TestCase):
+        iterkeys = lambda d: iter(d.keys())
+    itervalues = lambda d: iter(d.values())
+    iteritems = lambda d: iter(d.items())
     
-        def __init__(self, param):
+    from werkzeug.utils import import_string
+from ._compat import string_types, iteritems
+from . import json
+    
+        with app.test_request_context('/'):
+        assert flask.url_for('sub', _external=True) == \
+               'http://foo.localhost.localdomain:5000/'
+    
+        # a Model inside a Model
+    x = Input(shape=(1,))
+    y = Dense(2)(x)
+    inner_model = Model(x, y)
+    x = Input(shape=(1,))
+    y = inner_model(x)
+    outer_model = Model(x, y)
+    assert outer_model.trainable_weights == inner_model.trainable_weights
+    inner_model.trainable = False
+    assert outer_model.trainable_weights == []
+    inner_model.trainable = True
+    inner_model.layers[-1].trainable = False
+    assert outer_model.trainable_weights == []
+    
+    from keras import backend as K
+from keras import constraints
+from keras.utils.test_utils import keras_test
+    
+    model1 = create_network(num_classes=num_classes, **network1)
+history_model1 = model1.fit(x_train,
+                            y_train,
+                            batch_size=batch_size,
+                            epochs=epochs,
+                            verbose=1,
+                            validation_split=0.1)
+    
+        def steps(self):
+        '''Run the map and reduce steps.'''
+        return [
+            self.mr(mapper=self.mapper,
+                    reducer=self.reducer)
+        ]
+    
+        def add_card(self, card):
+        self.cards.append(card)
+    
+    
+if __name__ == '__main__':
+    SalesRanker.run()
+
+    
+        def mapper(self, _, line):
+        '''Parse each log line, extract and transform relevant lines.
+    
+        long_description = README,
+    
+    
+def construct_url(url, **params):
+    param_str = urlparse.urlencode(params)
+    return url + '?' + param_str
+    
+    def kugou_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    if url.lower().find('5sing')!=-1:
+        #for 5sing.kugou.com
+        html=get_html(url)
+        ticket=r1(r''ticket':\s*'(.*)'',html)
+        j=loads(str(b64decode(ticket),encoding='utf-8'))
+        url=j['file']
+        title=j['songName']
+        songtype, ext, size = url_info(url)
+        print_info(site_info, title, songtype, size)
+        if not info_only:
+            download_urls([url], title, ext, size, output_dir, merge=merge)
+    else:
+        #for the www.kugou.com/
+        return kugou_download_playlist(url, output_dir=output_dir, merge=merge, info_only=info_only)
+        # raise NotImplementedError(url)       
+    
+    __all__ = ['kuwo_download']
+    
+    def mixcloud_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html(url, faker=True)
+    title = r1(r'<meta property='og:title' content='([^']*)'', html)
+    preview_url = r1(r'm-preview=\'([^\']+)\'', html)
+    preview = r1(r'previews(.*)\.mp3$', preview_url)
+    
+    
+def AdjustCandidateInsertionText( candidates ):
+  '''This function adjusts the candidate insertion text to take into account the
+  text that's currently in front of the cursor.
+    
+      if extra_nosetests_args:
+    nosetests_args.extend( extra_nosetests_args )
+  else:
+    nosetests_args.append( p.join( DIR_OF_THIS_SCRIPT, 'python' ) )
+    
+    
+def MockAsyncServerResponseInProgress():
+  '''Return a fake future object that is incomplete. Suitable for mocking a
+  response future within a client request. For example:
+    
+        self._response_future = self.PostDataToHandlerAsync( request_data,
+                                                         'event_notification' )
