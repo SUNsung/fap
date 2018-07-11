106e27c45396a73c644dd1416604e0f0af15d3b2
SUNsung <weatherius@gmail.com>
2018-07-11 06:30:14 +0300

    Rewrite [51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb] File in: 2018-07-11 06:30:14

--- ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
+++ ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
@@ -1,63 +1,176 @@
 
         
-                msg = ' Please append `--trace` to the `#{cmd.name}` command '
-        dashes = '-' * msg.length
-        Jekyll.logger.error '', dashes
-        Jekyll.logger.error 'Jekyll #{Jekyll::VERSION} ', msg
-        Jekyll.logger.error '', ' for any additional information or backtrace. '
-        Jekyll.logger.abort_with '', dashes
+          # Removes any empty directories in the formula's prefix subtree
+  # Keeps any empty directions projected by skip_clean
+  # Removes any unresolved symlinks
+  def prune
+    dirs = []
+    symlinks = []
+    @f.prefix.find do |path|
+      if path == @f.libexec || @f.skip_clean?(path)
+        Find.prune
+      elsif path.symlink?
+        symlinks << path
+      elsif path.directory?
+        dirs << path
       end
-      # rubocop: enable RescueException
+    end
+    
+      def search_taps(rx)
+    SEARCHABLE_TAPS.map do |user, repo|
+      Thread.new { search_tap(user, repo, rx) }
+    end.inject([]) do |results, t|
+      results.concat(t.value)
+    end
+  end
+    
+        initial_revision = ENV['HOMEBREW_UPDATE_BEFORE'].to_s
+    current_revision = ENV['HOMEBREW_UPDATE_AFTER'].to_s
+    if initial_revision.empty? || current_revision.empty?
+      odie 'update-report should not be called directly!'
+    end
+    
+    end
+
+    
+      def grant_moderation!
+    set_permission('moderator', true)
+  end
+    
+      protected
+    
+        # Returns the current git branch - can be replaced using the environment variable `GIT_BRANCH`
+    def self.git_branch
+      return ENV['GIT_BRANCH'] if ENV['GIT_BRANCH'].to_s.length > 0 # set by Jenkins
+      s = Actions.sh('git rev-parse --abbrev-ref HEAD', log: false).chomp
+      return s.to_s.strip if s.to_s.length > 0
+      nil
+    rescue
+      nil
+    end
+    
+          it 'renders HTML' do
+        capture = render(options, screenshots)
+        expect(capture).to match(/<html>/)
+        expect(capture).to include('<li>Some</li>')
+        expect(capture).to include('<li>key</li>')
+        expect(capture).to include('<li>words</li>')
+      end
+    end
+    
+            command = [escaped_gradle_path, 'tasks', '--console=plain'].join(' ')
+        output = Action.sh(command, print_command: false, print_command_output: false)
+        output.split('\n').each do |line|
+          if (result = line.match(/(\w+)\s\-\s([\w\s]+)/))
+            self.tasks << GradleTask.new(title: result[1], description: result[2])
+          end
+        end
+    
+      protected
+    
+        if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      if Devise.sign_in_after_reset_password
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message!(:notice, flash_message)
+        sign_in(resource_name, resource)
+      else
+        set_flash_message!(:notice, :updated_not_active)
+      end
+      respond_with resource, location: after_resetting_password_path_for(resource)
+    else
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+    
+      def respond_to_on_destroy
+    # We actually need to hardcode this as Rails default responder doesn't
+    # support returning empty response on GET request
+    respond_to do |format|
+      format.all { head :no_content }
+      format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
     end
   end
 end
 
     
-            def deprecated_relative_permalinks(site)
-          if site.config['relative_permalinks']
-            Jekyll::Deprecator.deprecation_message 'Your site still uses relative permalinks,' \
-                                                   ' which was removed in Jekyll v3.0.0.'
-            true
-          end
-        end
+    class Devise::UnlocksController < DeviseController
+  prepend_before_action :require_no_authentication
     
-                c.action do |args, opts|
-              Jekyll::Commands::NewTheme.process(args, opts)
+                  define_method method do |resource_or_scope, *args|
+                scope = Devise::Mapping.find_scope!(resource_or_scope)
+                router_name = Devise.mappings[scope].router_name
+                context = router_name ? send(router_name) : _devise_route_context
+                context.send('#{action}#{scope}_#{module_name}_#{path_or_url}', *args)
+              end
             end
           end
         end
+      end
     
-            def bad_browser?
-          BAD_USER_AGENTS.any? { |pattern| @request['User-Agent'] =~ pattern }
+      it 'wraps with Data_Wrap_Struct and Data_Get_Struct returns data' do
+    a = @s.wrap_struct(1024)
+    @s.get_struct(a).should == 1024
+  end
+    
+      describe 'with an argument that responds to #to_r' do
+    it 'coerces using #to_r' do
+      o = mock_numeric('rational')
+      o.should_receive(:to_r).and_return(Rational(7201, 2))
+      t = Time.gm(2007, 1, 9, 12, 0, 0)
+      t.localtime(o)
+      t.should == Time.new(2007, 1, 9, 13, 0, Rational(1, 2), Rational(7201, 2))
+      t.utc_offset.should eql(Rational(7201, 2))
+    end
+  end
+    
+      def test_vendor_missing
+    orig_vendordir = RbConfig::CONFIG['vendordir']
+    RbConfig::CONFIG.delete 'vendordir'
+    
+      def test_verify_certificate_message_INVALID_CA
+    error_number = OpenSSL::X509::V_ERR_INVALID_CA
+    
+      def test_asia_singapore
+    with_tz(tz='Asia/Singapore') {
+      assert_time_constructor(tz, '1981-12-31 23:59:59 +0730', :local, [1981,12,31,23,59,59])
+      assert_time_constructor(tz, '1982-01-01 00:30:00 +0800', :local, [1982,1,1,0,0,0])
+      assert_time_constructor(tz, '1982-01-01 00:59:59 +0800', :local, [1982,1,1,0,29,59])
+      assert_time_constructor(tz, '1982-01-01 00:30:00 +0800', :local, [1982,1,1,0,30,0])
+    }
+  end
+    
+          it 'sets the regeneration marker to expire' do
+        allow(RegenerationWorker).to receive(:perform_async)
+        get :show
+        expect(Redis.current.ttl('account:#{user.account_id}:regeneration')).to be >= 0
+      end
+    
+        def empty_file?
+      File.exist?(@filepath) && File.size(@filepath) == 0
+    end
+    
+            def no_error_when_valid?
+          @file = StringIO.new('.')
+          @subject.send(@attachment_name).assign(@file)
+          @subject.valid?
+          expected_message = [
+            @attachment_name.to_s.titleize,
+            I18n.t(:blank, scope: [:errors, :messages])
+          ].join(' ')
+          @subject.errors.full_messages.exclude?(expected_message)
         end
-    
-        # Public: Generate list of configuration files from the override
-    #
-    # override - the command-line options hash
-    #
-    # Returns an Array of config files
-    def config_files(override)
-      # Adjust verbosity quickly
-      Jekyll.logger.adjust_verbosity(
-        :quiet   => quiet?(override),
-        :verbose => verbose?(override)
-      )
-    
-      def deliver_digest
-    NotificationMailer.digest(user.account).deliver_now!
-    user.touch(:last_emailed_at)
+      end
+    end
   end
 end
 
     
-        # Get rid of any info 'dir' files, so they don't conflict at the link stage
-    info_dir_file = @f.info + 'dir'
-    if info_dir_file.file? && !@f.skip_clean?(info_dir_file)
-      observe_file_removal info_dir_file
+    module Paperclip
+  class << self
+    attr_writer :registered_attachments_styles_path
+    def registered_attachments_styles_path
+      @registered_attachments_styles_path ||= Rails.root.join('public/system/paperclip_attachments.yml').to_s
     end
-    
-    module Homebrew
-  module_function
-    
-        def initialize(tag_name, markup, tokens)
-      attributes = ['class', 'src', 'width', 'height', 'title']
+  end
