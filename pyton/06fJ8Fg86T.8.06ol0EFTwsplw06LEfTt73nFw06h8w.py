50c7d56a39a132e21fc249984708d4040d4865a6
SUNsung <weatherius@gmail.com>
2018-07-12 23:19:26 +0300

    Rewrite [06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py] File in: 2018-07-12 23:19:25

--- pyton/06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py
+++ pyton/06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py
@@ -1,38 +1,127 @@
 
         
-                n_samples, n_features = 10 * i + 3, 10 * i + 3
+                String:                    '#4e9a06',        # class: 's'
+        String.Backtick:           '#4e9a06',        # class: 'sb'
+        String.Char:               '#4e9a06',        # class: 'sc'
+        String.Doc:                'italic #8f5902', # class: 'sd' - like a comment
+        String.Double:             '#4e9a06',        # class: 's2'
+        String.Escape:             '#4e9a06',        # class: 'se'
+        String.Heredoc:            '#4e9a06',        # class: 'sh'
+        String.Interpol:           '#4e9a06',        # class: 'si'
+        String.Other:              '#4e9a06',        # class: 'sx'
+        String.Regex:              '#4e9a06',        # class: 'sr'
+        String.Single:             '#4e9a06',        # class: 's1'
+        String.Symbol:             '#4e9a06',        # class: 'ss'
     
-                c_bar = plt.bar(xvals, build_time[alg] - bottom,
-                            width, bottom, color='r')
-            q_bar = plt.bar(xvals, query_time[alg],
-                            width, build_time[alg], color='b')
+        def run(self):
+        try:
+            self.server_sock = self._create_socket_and_bind()
+            # in case self.port = 0
+            self.port = self.server_sock.getsockname()[1]
+            self.ready_event.set()
+            self._handle_requests()
+    
+            cookie = list(jar)[0]
+        assert cookie.secure == secure
+        assert cookie.domain == domain
+        assert cookie._rest['HttpOnly'] == rest['HttpOnly']
     
     
-def type_auto_or_int(val):
-    if val == 'auto':
-        return 'auto'
+@pytest.mark.parametrize(
+    'value, length', (
+        ('', 0),
+        ('T', 1),
+        ('Test', 4),
+        ('Cont', 0),
+        ('Other', -5),
+        ('Content', None),
+    ))
+def test_iter_slices(value, length):
+    if length is None or (length <= 0 and len(value) > 0):
+        # Reads all content at once
+        assert len(list(iter_slices(value, length))) == 1
     else:
-        return int(val)
+        assert len(list(iter_slices(value, 1))) == length
     
-    # Create different classifiers. The logistic regression cannot do
-# multiclass out of the box.
-classifiers = {'L1 logistic': LogisticRegression(C=C, penalty='l1'),
-               'L2 logistic (OvR)': LogisticRegression(C=C, penalty='l2'),
-               'Linear SVC': SVC(kernel='linear', C=C, probability=True,
-                                 random_state=0),
-               'L2 logistic (Multinomial)': LogisticRegression(
-                C=C, solver='lbfgs', multi_class='multinomial'),
-               'GPC': GaussianProcessClassifier(kernel)
-               }
+    # Syntax sugar.
+_ver = sys.version_info
     
-    plt.figure(1)
+            raise KeyError('name=%r, domain=%r, path=%r' % (name, domain, path))
     
-            oa = OAS(store_precision=False, assume_centered=True)
-        oa.fit(X)
-        oa_mse[i, j] = oa.error_norm(real_cov, scaling=False)
-        oa_shrinkage[i, j] = oa.shrinkage_
+    from . import utils
+from . import packages
+from .models import Request, Response, PreparedRequest
+from .api import request, get, head, post, patch, put, delete, options
+from .sessions import session, Session
+from .status_codes import codes
+from .exceptions import (
+    RequestException, Timeout, URLRequired,
+    TooManyRedirects, HTTPError, ConnectionError,
+    FileModeWarning, ConnectTimeout, ReadTimeout
+)
+    
+        it = 0
+    
+        for i, kk in enumerate(krange):
+        print('k = %i (%i out of %i)' % (kk, i + 1, len(krange)))
+        for algorithm in algorithms:
+            nbrs = neighbors.NearestNeighbors(n_neighbors=kk,
+                                              algorithm=algorithm,
+                                              leaf_size=leaf_size)
+            t0 = time()
+            nbrs.fit(X)
+            t1 = time()
+            nbrs.kneighbors(X)
+            t2 = time()
+    
+        transformers['SparseRandomProjection'] = \
+        SparseRandomProjection(**sparse_matrix_params)
+    
+      * scikit-learn
     
     
-class BlackJackHand(Hand):
+def _get_git_revision():
+    try:
+        revision = subprocess.check_output(REVISION_CMD.split()).strip()
+    except (subprocess.CalledProcessError, OSError):
+        print('Failed to execute git to get revision')
+        return None
+    return revision.decode('utf-8')
     
-            Emit key value pairs of the form:
+    try:
+    from urllib import urlopen
+except ImportError:
+    from urllib.request import urlopen
+    
+    plt.show()
+
+    
+    np.random.seed(0)
+###############################################################################
+n_features = 100
+# simulation covariance matrix (AR(1) process)
+r = 0.1
+real_cov = toeplitz(r ** np.arange(n_features))
+coloring_matrix = cholesky(real_cov)
+    
+        def setUp(cls):
+        ''' Function/test case scope setup. '''
+        cls.output = StringIO()
+        cls.saved_stdout = sys.stdout
+        sys.stdout = cls.output
+    
+        def __init__(self, name):
+        self.name = name
+    
+        parameter
+    '''
+    
+            # and can also be undone at will
+        for cmd in reversed(command_stack):
+            cmd.undo()
+    finally:
+        os.unlink('foo.txt')
+    
+    *TL;DR80
+Traverses a container and accesses the container's elements.
+'''
