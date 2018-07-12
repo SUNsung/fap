3d2a39c4973c99f14261417700d23561e3d64526
SUNsung <weatherius@gmail.com>
2018-07-13 02:25:08 +0300

    Rewrite [28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py] File in: 2018-07-13 02:25:07

--- pyton/28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py
+++ pyton/28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py
@@ -1,132 +1,37 @@
 
         
-            if len(result) != 1:
-        raise Exception(
-            'Received %d items instead of 1 looking for %s in:\n%s' % (
-                len(result),
-                args.project,
-                json.dumps(result, indent=4, sort_keys=True)))
+        
+atom_template = textwrap.dedent('''\
+    <?xml version='1.0' encoding='utf-8'?>
+    <feed xmlns='http://www.w3.org/2005/Atom'>
+        <link rel='self' href='http://rg3.github.io/youtube-dl/update/releases.atom' />
+        <title>youtube-dl releases</title>
+        <id>https://yt-dl.org/feed/youtube-dl-updates-feed</id>
+        <updated>@TIMESTAMP@</updated>
+        @ENTRIES@
+    </feed>''')
     
-        # This matches a hostname or host pattern including [x:y(:z)] ranges.
-    #
-    # We roughly follow DNS rules here, but also allow ranges (and underscores).
-    # In the past, no systematic rules were enforced about inventory hostnames,
-    # but the parsing context (e.g. shlex.split(), fnmatch.fnmatch()) excluded
-    # various metacharacters anyway.
-    #
-    # We don't enforce DNS length restrictions here (63 characters per label,
-    # 253 characters total) or make any attempt to process IDNs.
+    options = helptext[helptext.index('  General Options:') + 19:]
+options = re.sub(r'(?m)^  (\w.+)$', r'## \1', options)
+options = '# OPTIONS\n' + options + '\n'
     
-        results = {}
-    for group in sort_groups(groups):
-        results = combine_vars(results, group.get_vars())
-    
-    
-class FullJitterBackoffStrategyTestCase(unittest.TestCase):
-    def test_no_retries(self):
-        strategy = _full_jitter_backoff(retries=0)
-        result = list(strategy())
-        self.assertEquals(result, [], 'list should be empty')
-    
-    In the second benchmark, we increase the number of dimensions of the
-training set. Then we plot the computation time as function of
-the number of dimensions.
+        def test_pbs(self):
+        # https://github.com/rg3/youtube-dl/issues/2350
+        self.assertMatch('http://video.pbs.org/viralplayer/2365173446/', ['pbs'])
+        self.assertMatch('http://video.pbs.org/widget/partnerplayer/980042464/', ['pbs'])
     
     
-def get_data(N, D, dataset='dense'):
-    if dataset == 'dense':
-        np.random.seed(0)
-        return np.random.random((N, D))
-    elif dataset == 'digits':
-        X = datasets.load_digits().data
-        i = np.argsort(X[0])[::-1]
-        X = X[:, i]
-        return X[:N, :D]
-    else:
-        raise ValueError('invalid dataset: %s' % dataset)
+@mock.patch('httpie.core.get_response')
+def test_error_traceback(get_response):
+    exc = ConnectionError('Connection aborted')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    with raises(ConnectionError):
+        main(['--ignore-stdin', '--traceback', 'www.google.com'])
     
-        for n_samples in sample_sizes:
-        X = random_state.rand(n_samples, 300)
+            Use `self.raw_auth` to access the raw value passed through
+        `--auth, -a`.
     
-        print('Decompressing %s' % ARCHIVE_NAME)
-    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
-        archive.extractall(path='.')
-    os.remove(ARCHIVE_NAME)
-
-    
-        # the training data folder must be passed as first argument
-    movie_reviews_data_folder = sys.argv[1]
-    dataset = load_files(movie_reviews_data_folder, shuffle=False)
-    print('n_samples: %d' % len(dataset.data))
-    
-    model = SpectralBiclustering(n_clusters=n_clusters, method='log',
-                             random_state=0)
-model.fit(data)
-score = consensus_score(model.biclusters_,
-                        (rows[:, row_idx], columns[:, col_idx]))
-    
-    # Annotate points on the simplex
-plt.annotate(r'($\frac{1}{3}$, $\frac{1}{3}$, $\frac{1}{3}$)',
-             xy=(1.0/3, 1.0/3), xytext=(1.0/3, .23), xycoords='data',
-             arrowprops=dict(facecolor='black', shrink=0.05),
-             horizontalalignment='center', verticalalignment='center')
-plt.plot([1.0/3], [1.0/3], 'ko', ms=5)
-plt.annotate(r'($\frac{1}{2}$, $0$, $\frac{1}{2}$)',
-             xy=(.5, .0), xytext=(.5, .1), xycoords='data',
-             arrowprops=dict(facecolor='black', shrink=0.05),
-             horizontalalignment='center', verticalalignment='center')
-plt.annotate(r'($0$, $\frac{1}{2}$, $\frac{1}{2}$)',
-             xy=(.0, .5), xytext=(.1, .5), xycoords='data',
-             arrowprops=dict(facecolor='black', shrink=0.05),
-             horizontalalignment='center', verticalalignment='center')
-plt.annotate(r'($\frac{1}{2}$, $\frac{1}{2}$, $0$)',
-             xy=(.5, .5), xytext=(.6, .6), xycoords='data',
-             arrowprops=dict(facecolor='black', shrink=0.05),
-             horizontalalignment='center', verticalalignment='center')
-plt.annotate(r'($0$, $0$, $1$)',
-             xy=(0, 0), xytext=(.1, .1), xycoords='data',
-             arrowprops=dict(facecolor='black', shrink=0.05),
-             horizontalalignment='center', verticalalignment='center')
-plt.annotate(r'($1$, $0$, $0$)',
-             xy=(1, 0), xytext=(1, .1), xycoords='data',
-             arrowprops=dict(facecolor='black', shrink=0.05),
-             horizontalalignment='center', verticalalignment='center')
-plt.annotate(r'($0$, $1$, $0$)',
-             xy=(0, 1), xytext=(.1, 1), xycoords='data',
-             arrowprops=dict(facecolor='black', shrink=0.05),
-             horizontalalignment='center', verticalalignment='center')
-# Add grid
-plt.grid('off')
-for x in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    plt.plot([0, x], [x, 0], 'k', alpha=0.2)
-    plt.plot([0, 0 + (1-x)/2], [x, x + (1-x)/2], 'k', alpha=0.2)
-    plt.plot([x, x + (1-x)/2], [0, 0 + (1-x)/2], 'k', alpha=0.2)
-    
-        test_suite = 'tests',
-    
-            account_number = match1(html, r'data-account='(\d+)'')
-    
-    site_info = 'ehow.com'
-download = ehow_download
-download_playlist = playlist_not_supported('ehow')
-
-    
-            # construct available streams
-        if orig_img: self.streams['original'] = {'url': orig_img}
-        if twit_img: self.streams['small'] = {'url': twit_img}
-    
-    #----------------------------------------------------------------------
-def showroom_get_roomid_by_room_url_key(room_url_key):
-    '''str->str'''
-    fake_headers_mobile = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Charset': 'UTF-8,*;q=0.5',
-        'Accept-Encoding': 'gzip,deflate,sdch',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'
-    }
-    webpage_url = 'https://www.showroom-live.com/' + room_url_key
-    html = get_content(webpage_url, headers = fake_headers_mobile)
-    roomid = match1(html, r'room\?room_id\=(\d+)')
-    assert roomid
-    return roomid
+        def test_CRLF_ugly_request(self, httpbin):
+        r = http('--pretty=none', '--print=HB', 'GET', httpbin.url + '/get')
+        self._validate_crlf(r)
