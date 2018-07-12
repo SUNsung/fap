e587d6c079a90674f8656187b652e06a2235e0e9
SUNsung <weatherius@gmail.com>
2018-07-12 21:12:21 +0300

    Rewrite [24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb] File in: 2018-07-12 21:12:21

--- ruby/24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb
+++ ruby/24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb
@@ -1,187 +1,115 @@
 
         
-              def setup_with_controller
-        @controller = ActionView::TestCase::TestController.new
-        @request = @controller.request
-        @view_flow = ActionView::OutputFlow.new
-        # empty string ensures buffer has UTF-8 encoding as
-        # new without arguments returns ASCII-8BIT encoded buffer like String#new
-        @output_buffer = ActiveSupport::SafeBuffer.new ''
-        @rendered = ''.dup
-    
-            def test_url_sub_key
-          spec = resolve :production, 'production' => { 'url' => 'abstract://foo?encoding=utf8' }
-          assert_equal({
-            'adapter'  => 'abstract',
-            'host'     => 'foo',
-            'encoding' => 'utf8',
-            'name'     => 'production' }, spec)
-        end
-    
-              attributes[table_name] = attributes[table_name].merge(column_name => value)
-        end
-    
-          def assert_valid_value(value)
-        if coder.respond_to?(:assert_valid_value)
-          coder.assert_valid_value(value, action: 'serialize')
-        end
-      end
-    
-        # All the threads should retrieve the value knowing it is JSON, and
-    # thus decode it. If this fails, some threads will instead see the
-    # raw string ('1'), or raise an exception.
-    assert_equal [1] * threads.size, threads.map(&:value)
-  end
+        # No trailing slash
+Benchmark.ips do |x|
+  x.report('with body include?') { CONTENT_CONTAINING.include?('<body') }
+  x.report('with body regexp')   { CONTENT_CONTAINING =~ /<\s*body/ }
+  x.compare!
 end
 
     
-      class RaisedOnDestroy < RuntimeError; end
-  attr_accessor :raise_on_destroy
-  before_destroy do
-    raise RaisedOnDestroy if raise_on_destroy
-  end
-    
-      test 'do not send subscription confirmation on initialize' do
-    assert_nil @connection.last_transmission
-  end
-    
-        private
-      def ping
+          def convert(content)
+        document = Kramdown::Document.new(content, @config)
+        html_output = document.to_html.chomp
+        if @config['show_warnings']
+          document.warnings.each do |warning|
+            Jekyll.logger.warn 'Kramdown warning:', warning.sub(%r!^Warning:\s+!, '')
+          end
+        end
+        html_output
       end
-  end
-    
-      test 'closes hijacked i/o socket at shutdown' do
-    run_in_eventmachine do
-      connection = open_connection
-    
-      describe 'instance methods' do
-    let(:key_attributes) do # these keys are intentionally strings.
-      {
-        'canDownload' => false,
-        'canRevoke' => true,
-        'keyId' => 'some-key-id',
-        'keyName' => 'fastlane',
-        'servicesCount' => 3,
-        'services' => [
-          {
-            'name' => 'APNS',
-            'id' => 'U27F4V844T',
-            'configurations' => []
-          },
-          {
-            'name' => 'MusicKit',
-            'id' => '6A7HVUVQ3M',
-            'configurations' => [
-              {
-                'name' => 'music id test',
-                'identifier' => 'music.com.snatchev.test',
-                'type' => 'music',
-                'prefix' => 'some-prefix-id',
-                'id' => 'some-music-kit-id'
-              }
-            ]
-          },
-          {
-            'name' => 'DeviceCheck',
-            'id' => 'DQ8HTZ7739',
-            'configurations' => []
-          }
-        ]
-      }
-    end
-    
-        version '1.3' do
-      self.release = '1.3.4'
-      self.base_urls = [
-        'https://hexdocs.pm/elixir/#{release}/',
-        'https://hexdocs.pm/eex/#{release}/',
-        'https://hexdocs.pm/ex_unit/#{release}/',
-        'https://hexdocs.pm/iex/#{release}/',
-        'https://hexdocs.pm/logger/#{release}/',
-        'https://hexdocs.pm/mix/#{release}/',
-        'https://elixir-lang.org/getting-started/'
-      ]
     end
   end
 end
 
     
-        html_filters.push 'haxe/clean_html', 'haxe/entries'
+    module Jekyll
+  module Deprecator
+    extend self
     
-        # Downloaded from php.net/download-docs.php
-    self.dir = '/Users/Thibaut/DevDocs/Docs/PHP'
-    
-        def inheritable_copy
-      self.class.new @filters
+          options[:only_patterns] = [/\Agetting-started\//, /\Alayout\//, /\Acontent\//, /\Acomponents\//, /\Autilities\//, /\Amigration\//]
     end
     
-      def unsuspend?
-    staff?
+          options[:container] = '.container'
+    end
+  end
+end
+
+    
+        version 'Java' do
+      self.base_url = 'https://api.haxe.org/java/'
+    end
+    
+      def test_hidden_key
+    bug6899 = '[ruby-core:47253]'
+    foo = 'foor'
+    bar = 'bar'
+    assert_nothing_raised(NotImplementedError, bug6899) do
+      2000.times {eval %[(foo..bar) ? 1 : 2]}
+    end
+    foo = bar
   end
     
-      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/follow_request
-  def follow_request
-    f = Follow.last
-    NotificationMailer.follow_request(f.target_account, Notification.find_by(activity: f))
-  end
-    
-      def parse(pkt)
-    # We want to return immediatly if we do not have a packet which is handled by us
-    return unless pkt.is_tcp?
-    return if (pkt.tcp_sport != 21 and pkt.tcp_dport != 21)
-    s = find_session((pkt.tcp_sport == 21) ? get_session_src(pkt) : get_session_dst(pkt))
-    s[:sname] ||= 'ftp'
-    
-        self.sigs.each_key do |k|
-    
-    
-# replace calls, jmps, and read/write handle/filename references
-replaces = []
-asm.each_line { |ln|
-	if ln =~ /call /
-		parts = ln.split(' ')
-		if (parts[0] == 'call' and parts[2] == ';call')
-			old = parts[1]
-			func = parts[3]
-			new = addrs[func]
-			#puts '%32s: %s -> %x' % [func, old, new]
-			replaces << [func, old, new.to_s(16)]
-		end
-	end
-    }
-    
-    puts '* Loading Stdapi'
-    
-    clsCreateJar._invoke('createJarArchive', 'Ljava.io.File;[Ljava.io.File;', fileOutJar, filesIn)
-    
-    			if temp[2].length == 2
-				self.block[-1][-1] << temp[2][0].ljust(8)
-				self.block[-1][-1] << temp[2][1]
-			elsif temp[2].length == 3
-				self.block[-1][-1] << temp[2][0].ljust(8)
-				self.block[-1][-1] << temp[2][1]
-				self.block[-1][-1] << ' '
-				self.block[-1][-1] << temp[2][2]
-			else
-				self.block[-1][-1] << temp[2].to_s
-			end
-		end
-    
-          if rails?
-        register_rails_engine
-      elsif lotus?
-        register_lotus
-      elsif sprockets?
-        register_sprockets
+        line = __LINE__+5
+    e = assert_raise(NoMethodError) do
+      1.upto 0 do
       end
+        .
+        foo(
+          1,
+          2,
+        )
+    end
+    assert_equal(line, e.backtrace_locations[0].lineno)
+  end
     
-        def log_status(status)
-      puts bold status
+      describe 'RDATA()' do
+    it 'returns the struct data' do
+      a = @s.wrap_struct(1024)
+      @s.get_struct_rdata(a).should == 1024
     end
     
-          File.open('bower.json', 'w') do |f|
-        f.puts JSON.pretty_generate(spec)
+      def add_file(name, mode) # :yields: io
+    check_closed
+    
+      def test_implicit_upper_case_proxy
+    use_ui @stub_ui do
+      ENV['HTTP_PROXY'] = @proxy_uri
+      fetcher = Gem::RemoteFetcher.new nil
+      @fetcher = fetcher
+      assert_data_from_proxy fetcher.fetch_path(@server_uri)
+    end
+  end
+    
+      it 'decodes the remaining shorts when passed the '*' modifier' do
+    'abcd'.unpack(unpack_format('*')).should == [25185, 25699]
+  end
+    
+    module Jekyll
+    
+        def render(context)
+      if @img
+        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
+      else
+        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
       end
     end
   end
 end
+    
+    end
+    
+      class IncludeCodeTag < Liquid::Tag
+    def initialize(tag_name, markup, tokens)
+      @title = nil
+      @file = nil
+      if markup.strip =~ /\s*lang:(\S+)/i
+        @filetype = $1
+        markup = markup.strip.sub(/lang:\S+/i,'')
+      end
+      if markup.strip =~ /(.*)?(\s+|^)(\/*\S+)/i
+        @title = $1 || nil
+        @file = $3
+      end
+      super
+    end
