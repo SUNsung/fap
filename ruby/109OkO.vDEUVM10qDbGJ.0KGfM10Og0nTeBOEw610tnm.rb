cd90ff41a9c7520e00cb434338a7e1cb0b912cf0
SUNsung <weatherius@gmail.com>
2018-07-12 22:16:56 +0300

    Rewrite [109OkO.vDEUVM10qDbGJ.0KGfM10Og0nTeBOEw610tnm.rb] File in: 2018-07-12 22:16:55

--- ruby/109OkO.vDEUVM10qDbGJ.0KGfM10Og0nTeBOEw610tnm.rb
+++ ruby/109OkO.vDEUVM10qDbGJ.0KGfM10Og0nTeBOEw610tnm.rb
@@ -1,90 +1,44 @@
 
         
-            context 'with keywords' do
-      let(:options) do
-        {
-          name: { 'en-US' => 'Fastlane Demo' },
-          description: { 'en-US' => 'Demo description' },
-          keywords: { 'en-US' => 'Some, key, words' }
-        }
-      end
+          private
     
-          attr_accessor :description
-    
-    class Devise::OmniauthCallbacksController < DeviseController
-  prepend_before_action { request.env['devise.skip_timeout'] = true }
-    
-      def respond_to_on_destroy
-    # We actually need to hardcode this as Rails default responder doesn't
-    # support returning empty response on GET request
-    respond_to do |format|
-      format.all { head :no_content }
-      format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
+      def find_internal_commands(directory)
+    directory.children.reduce([]) do |cmds, f|
+      cmds << f.basename.to_s.sub(/\.(?:rb|sh)$/, '') if f.file?
+      cmds
     end
   end
 end
 
     
-    class Devise::UnlocksController < DeviseController
-  prepend_before_action :require_no_authentication
+        current_revision_var = 'HOMEBREW_UPDATE_AFTER#{repo_var}'
+    @current_revision = ENV[current_revision_var].to_s
+    raise ReporterRevisionUnsetError, current_revision_var if @current_revision.empty?
+  end
     
-      if record && record.respond_to?(:timedout?) && warden.authenticated?(scope) &&
-     options[:store] != false && !env['devise.skip_timeoutable']
-    last_request_at = warden.session(scope)['last_request_at']
+      def self.class_s(name)
+    Formulary.class_s(name)
+  end
     
-          if options[:skip_helpers] == true
-        @used_helpers = @used_routes
-      elsif skip = options[:skip_helpers]
-        @used_helpers = self.routes - Array(skip).map(&singularizer)
-      else
-        @used_helpers = self.routes
+      def resource
+    @resource ||=
+      if params[:project_id].present?
+        Project.find(params[:project_id])
+      elsif params[:namespace_id].present?
+        Group.find(params[:namespace_id])
       end
+  end
+    
+      def self.authenticate(user, app_id, json_response, challenges)
+    response = U2F::SignResponse.load_from_json(json_response)
+    registration = user.u2f_registrations.find_by_key_handle(response.key_handle)
+    u2f = U2F::U2F.new(app_id)
+    
+        it 'copes with nested arrays' do
+      mock_app do |env|
+        request = Rack::Request.new(env)
+        [200, {'Content-Type' => 'text/plain'}, [request.params['foo']['bar']]]
+      end
+      get '/', :foo => {:bar => '<bar>'}
+      expect(body).to eq('&lt;bar&gt;')
     end
-  end
-end
-
-    
-      def with_reopen(io, arg)
-    old = io.dup
-    io.reopen(arg)
-    yield old
-  ensure
-    io.reopen(old)
-    old.close
-  end
-    
-      it 'returns nil if then-body is empty, expression is true and else part is empty' do
-    if true
-    else
-    end.should == nil
-  end
-    
-      def test_compile_insn_opt_length_size
-    assert_compile_once('#{<<~'begin;'}\n#{<<~'end;'}', result_inspect: '4', insns: %i[opt_length opt_size])
-    begin;
-      array = [1, 2]
-      array.length + array.size
-    end;
-  end
-    
-      def test_keyword_splat
-    assert_valid_syntax('foo(**h)', __FILE__)
-    o = KW2.new
-    h = {k1: 11, k2: 12}
-    assert_equal([11, 12], o.kw(**h))
-    assert_equal([11, 12], o.kw(k2: 22, **h))
-    assert_equal([11, 22], o.kw(**h, **{k2: 22}))
-    assert_equal([11, 12], o.kw(**{k2: 22}, **h))
-  end
-    
-        fmt = %w(Y m d).map { |x| '%#{x}' }.join('-') # defeats optimization
-    t = Time.at(0).getutc
-    ObjectSpace.count_objects(res = {}) # creates strings on first call
-    before = ObjectSpace.count_objects(res)[:T_STRING]
-    val = t.strftime(fmt)
-    after = ObjectSpace.count_objects(res)[:T_STRING]
-    assert_equal before + 1, after, 'only new string is the created one'
-    assert_equal '1970-01-01', val
-  end
-    
-            private
