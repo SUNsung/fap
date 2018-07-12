2079674175334fe13c155aa38541c036ecf6da69
SUNsung <weatherius@gmail.com>
2018-07-12 19:42:25 +0300

    Rewrite [48Eb4SwqE2vE48fXTNPcX9PLg48xyY75TobXdU48YrZ.cpp] File in: 2018-07-12 19:42:24

--- cccccc/48Eb4SwqE2vE48fXTNPcX9PLg48xyY75TobXdU48YrZ.cpp
+++ cccccc/48Eb4SwqE2vE48fXTNPcX9PLg48xyY75TobXdU48YrZ.cpp
@@ -1,230 +1,309 @@
 
         
-        
+        #ifndef SWIFT_PRINTINGDIAGNOSTICCONSUMER_H
+#define SWIFT_PRINTINGDIAGNOSTICCONSUMER_H
+    
+    #ifndef SWIFT_RUNTIME_ONCE_H
+#define SWIFT_RUNTIME_ONCE_H
+    
+      /// return true if that the two objects have the same base but access different
+  /// fields of the base object.
+  bool hasNonEmptySymmetricPathDifference(const LSBase &RHS) const {
+    const ProjectionPath &P = RHS.Path.getValue();
+    return Path.getValue().hasNonEmptySymmetricDifference(P);
+  }
+    
+    #endif // SWIFT_MIGRATOR_FIXITAPPLYDIAGNOSTICCONSUMER_H
+
+    
+    
+    {}  // namespace nwapi
+    
+    void Menu::Call(const std::string& method,
+                const base::ListValue& arguments,
+                content::RenderFrameHost* rvh) {
+  if (method == 'Append') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    Append(object_manager()->GetApiObject<MenuItem>(object_id));
+  } else if (method == 'Insert') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    int pos = 0;
+    arguments.GetInteger(1, &pos);
+    Insert(object_manager()->GetApiObject<MenuItem>(object_id), pos);
+  } else if (method == 'Remove') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    int pos = 0;
+    arguments.GetInteger(1, &pos);
+    Remove(object_manager()->GetApiObject<MenuItem>(object_id), pos);
+  } else if (method == 'Popup') {
+    int x = 0;
+    arguments.GetInteger(0, &x);
+    int y = 0;
+    arguments.GetInteger(1, &y);
+    content::WebContents* web_contents = content::WebContents::FromRenderFrameHost(rvh);
+    DCHECK(web_contents);
+    zoom::ZoomController* zoom_controller = zoom::ZoomController::FromWebContents(web_contents);
+    }
+    }
+    
+    void Menu::UpdateKeys(views::FocusManager *focus_manager){
+  if (focus_manager == NULL){
+    return ;
+  } else {
+    focus_manager_ = focus_manager;
+    for(auto* item : menu_items_) {
+      item->UpdateKeys(focus_manager);
+    }
+  }
+}
+    
+    NwObjCallObjectMethodSyncFunction::~NwObjCallObjectMethodSyncFunction() {
+}
+    
+    
+    {    std::unique_ptr<base::ListValue> args = nwapi::nw__screen::OnSourceThumbnailChanged::Create(
+      src.id.ToString(),
+      base64);
+    DispatchEvent(
+      events::HistogramValue::UNKNOWN, 
+      nwapi::nw__screen::OnSourceThumbnailChanged::kEventName,
+      std::move(args));
+  }
+    
+      // implement nw.Screen.isMonitorStarted()
+  class NwScreenIsMonitorStartedFunction : public NWSyncExtensionFunction {
+  public:
+    NwScreenIsMonitorStartedFunction();
+    bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+        UINT createDeviceFlags = 0;
+    
+        // A\B
+    cv::solve(H, A, A, cv::DECOMP_NORMAL);
+    H.release();
+    
+    //
+// BEGIN OF CUSTOM FUNCTIONS
+//
+    
+    static void* openclamdfft_check_fn(int ID)
+{
+    assert(ID >= 0 && ID < (int)(sizeof(openclamdfft_fn)/sizeof(openclamdfft_fn[0])));
+    const struct DynamicFnEntry* e = openclamdfft_fn[ID];
+    void* func = CV_CL_GET_PROC_ADDRESS(e->fnName);
+    if (!func)
+    {
+        throw cv::Exception(cv::Error::OpenCLApiCallError,
+                cv::format('OpenCL AMD FFT function is not available: [%s]', e->fnName),
+                CV_Func, __FILE__, __LINE__);
+    }
+    *(e->ppFn) = func;
+    return func;
+}
+    
+      template<typename T>
+  void allReduceT(at::Tensor& data, THDReduceOp operation,
+                  THDGroup group_id = THDGroupWORLD);
+    
+    #define THCPDoubleStorage_CData(obj)  (obj)->cdata
+#define THCPFloatStorage_CData(obj)   (obj)->cdata
+#define THCPLongStorage_CData(obj)    (obj)->cdata
+#define THCPIntStorage_CData(obj)     (obj)->cdata
+#define THCPShortStorage_CData(obj)   (obj)->cdata
+#define THCPCharStorage_CData(obj)    (obj)->cdata
+#define THCPByteStorage_CData(obj)    (obj)->cdata
+    
+    #define THD_GENERIC_FILE 'torch/csrc/generic/Storage.h'
+#include <THD/base/THDGenerateAllTypes.h>
+    
+    
+void register_fd(int fd) {
+  struct pollfd pfd = {0};
+  pfd.fd = fd;
+  pfd.events = POLLIN;
+  pollfds.push_back(pfd);
+}
+    
+    static void RunSynchronousUnaryPingPong() {
+  gpr_log(GPR_INFO, 'Running Synchronous Unary Ping Pong');
+    }
+    
+    
+    
+    
     { private:
-  DebugOptions debug_options_;
+  GeneratorConfiguration config_;
 };
     
-    namespace tensorflow {
+      std::pair<bool, grpc::string> GetGrpcServices();
+    
+    namespace grpc {
+namespace testing {
+    }
     }
     
-    class RecordWriter {
- public:
-  // Create a writer that will append data to '*dest'.
-  // '*dest' must be initially empty.
-  // '*dest' must remain live while this Writer is in use.
-  RecordWriter(WritableFile* dest,
-               const RecordWriterOptions& options = RecordWriterOptions());
-    }
+      struct Result {
+    double wall;
+    double user;
+    double system;
+    unsigned long long total_cpu_time;
+    unsigned long long idle_cpu_time;
+  };
     
-    void SYCLDeviceContext::CopyCPUTensorToDevice(const Tensor *cpu_tensor,
-                                              Device *device,
-                                              Tensor *device_tensor,
-                                              StatusCallback done) const {
-  const int64 total_bytes = cpu_tensor->TotalBytes();
-  if (total_bytes > 0) {
-    const void *src_ptr = DMAHelper::base(cpu_tensor);
-    void *dst_ptr = DMAHelper::base(device_tensor);
-    switch (cpu_tensor->dtype()) {
-      case DT_FLOAT:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<float *>(dst_ptr), static_cast<const float *>(src_ptr),
-            total_bytes);
-        break;
-      case DT_DOUBLE:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<double *>(dst_ptr),
-            static_cast<const double *>(src_ptr), total_bytes);
-        break;
-      case DT_INT32:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<int32 *>(dst_ptr), static_cast<const int32 *>(src_ptr),
-            total_bytes);
-        break;
-      case DT_INT64:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<int64 *>(dst_ptr), static_cast<const int64 *>(src_ptr),
-            total_bytes);
-        break;
-      case DT_HALF:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<Eigen::half *>(dst_ptr),
-            static_cast<const Eigen::half *>(src_ptr), total_bytes);
-        break;
-      case DT_COMPLEX64:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<std::complex<float> *>(dst_ptr),
-            static_cast<const std::complex<float> *>(src_ptr), total_bytes);
-        break;
-      case DT_COMPLEX128:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<std::complex<double> *>(dst_ptr),
-            static_cast<const std::complex<double> *>(src_ptr), total_bytes);
-        break;
-      case DT_INT8:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<int8 *>(dst_ptr), static_cast<const int8 *>(src_ptr),
-            total_bytes);
-        break;
-      case DT_INT16:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<int16 *>(dst_ptr), static_cast<const int16 *>(src_ptr),
-            total_bytes);
-        break;
-      case DT_UINT8:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<uint8 *>(dst_ptr), static_cast<const uint8 *>(src_ptr),
-            total_bytes);
-        break;
-      case DT_UINT16:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<uint16 *>(dst_ptr),
-            static_cast<const uint16 *>(src_ptr), total_bytes);
-        break;
-      case DT_BOOL:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<bool *>(dst_ptr), static_cast<const bool *>(src_ptr),
-            total_bytes);
-        break;
-      default:
-        assert(false && 'unsupported type');
-    }
+    #include 'src/proto/grpc/testing/metrics.grpc.pb.h'
+#include 'src/proto/grpc/testing/metrics.pb.h'
+    
+    grpc::string DescribeMethod(const grpc::protobuf::MethodDescriptor* method) {
+  std::stringstream result;
+  result << '  rpc ' << method->name()
+         << (method->client_streaming() ? '(stream ' : '(')
+         << method->input_type()->full_name() << ') returns '
+         << (method->server_streaming() ? '(stream ' : '(')
+         << method->output_type()->full_name() << ') {}\n';
+  if (method->options().deprecated()) {
+    result << ' DEPRECATED';
   }
-  device->eigen_sycl_device()->synchronize();
-  done(Status::OK());
+  return result.str();
 }
     
-    #include 'tensorflow/core/kernels/cwise_ops_common.h'
+      // gRPC Python
+  grpc_python_generator::GeneratorConfiguration grpc_py_config;
+  grpc_python_generator::PythonGrpcGenerator grpc_py_generator(grpc_py_config);
+  cli.RegisterGenerator('--grpc_python_out', &grpc_py_generator,
+                        'Generate Python source file.');
     
+    	virtual Error put_data(const uint8_t *p_data, int p_bytes);
+	virtual Error put_partial_data(const uint8_t *p_data, int p_bytes, int &r_sent);
     
-    {  // From the zlib manual (http://www.zlib.net/manual.html):
-  // The strategy parameter is used to tune the compression algorithm. Use the
-  // value Z_DEFAULT_STRATEGY for normal data, Z_FILTERED for data produced by
-  // a filter (or predictor), Z_HUFFMAN_ONLY to force Huffman encoding only
-  // (no string match), or Z_RLE to limit match distances to one
-  // (run-length encoding). Filtered data consists mostly of small values with
-  // a somewhat random distribution. In this case, the compression algorithm is
-  // tuned to compress them better. The effect of Z_FILTERED is to force more
-  // Huffman coding and less string matching; it is somewhat intermediate
-  // between Z_DEFAULT_STRATEGY and Z_HUFFMAN_ONLY. Z_RLE is designed to be
-  // almost as fast as Z_HUFFMAN_ONLY, but give better compression for
-  // PNG image data. The strategy parameter only affects the compression ratio
-  // but not the correctness of the compressed output even if it is not set
-  // appropriately. Z_FIXED prevents the use of dynamic Huffman codes, allowing
-  // for a simpler decoder for special applications.
-  int8 compression_strategy = Z_DEFAULT_STRATEGY;
-};
-    
-      // If there are too few results, assume the result will be unreliable and
-  // bail.
-  const int64 how_many_results = previous_results_.size();
-  const int64 earliest_time = previous_results_.front().first;
-  const int64 samples_duration = current_time_ms - earliest_time;
-  if ((how_many_results < minimum_count_) ||
-      (samples_duration < (average_window_duration_ms_ / 4))) {
-    *found_command = previous_top_label_;
-    *score = 0.0f;
-    *is_new_command = false;
-    return Status::OK();
-  }
-    
-    namespace {
+    void WebSocketClient::_on_error() {
     }
     
-    #include <math.h>
+    	GodotAllConvexResultCallback(PhysicsDirectSpaceState::ShapeResult *p_results, int p_resultMax, const Set<RID> *p_exclude) :
+			m_results(p_results),
+			m_exclude(p_exclude),
+			m_resultMax(p_resultMax),
+			count(0) {}
     
-    namespace guetzli {
+    The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+    
+    
+    {  create_look_ups();
+}
+    
+    namespace jpgd
+{
+  typedef unsigned char  uint8;
+  typedef   signed short int16;
+  typedef unsigned short uint16;
+  typedef unsigned int   uint;
+  typedef   signed int   int32;
     }
     
-    namespace guetzli {
-    }
+    /*Modern gcc (4.x) can compile the naive versions of min and max with cmov if
+   given an appropriate architecture, but the branchless bit-twiddling versions
+   are just as fast, and do not require any special target architecture.
+  Earlier gcc versions (3.x) compiled both code to the same assembly
+   instructions, because of the way they represented ((_b)>(_a)) internally.*/
+#define OC_MAXI(_a,_b)      ((_a)-((_a)-(_b)&-((_b)>(_a))))
+#define OC_MINI(_a,_b)      ((_a)+((_b)-(_a)&-((_b)<(_a))))
+/*Clamps an integer into the given range.
+  If _a>_c, then the lower bound _a is respected over the upper bound _c (this
+   behavior is required to meet our documented API behavior).
+  _a: The lower bound.
+  _b: The value to clamp.
+  _c: The upper boud.*/
+#define OC_CLAMPI(_a,_b,_c) (OC_MAXI(_a,OC_MINI(_b,_c)))
+#define OC_CLAMP255(_x)     ((unsigned char)((((_x)<0)-1)&((_x)|-((_x)>255))))
+/*This has a chance of compiling branchless, and is just as fast as the
+   bit-twiddling method, which is slightly less portable, since it relies on a
+   sign-extended rightshift, which is not guaranteed by ANSI (but present on
+   every relevant platform).*/
+#define OC_SIGNI(_a)        (((_a)>0)-((_a)<0))
+/*Slightly more portable than relying on a sign-extended right-shift (which is
+   not guaranteed by ANSI), and just as fast, since gcc (3.x and 4.x both)
+   compile it into the right-shift anyway.*/
+#define OC_SIGNMASK(_a)     (-((_a)<0))
+/*Divides an integer by a power of two, truncating towards 0.
+  _dividend: The integer to divide.
+  _shift:    The non-negative power of two to divide by.
+  _rmask:    (1<<_shift)-1*/
+#define OC_DIV_POW2(_dividend,_shift,_rmask)\
+  ((_dividend)+(OC_SIGNMASK(_dividend)&(_rmask))>>(_shift))
+/*Divides _x by 65536, truncating towards 0.*/
+#define OC_DIV2_16(_x) OC_DIV_POW2(_x,16,0xFFFF)
+/*Divides _x by 2, truncating towards 0.*/
+#define OC_DIV2(_x) OC_DIV_POW2(_x,1,0x1)
+/*Divides _x by 8, truncating towards 0.*/
+#define OC_DIV8(_x) OC_DIV_POW2(_x,3,0x7)
+/*Divides _x by 16, truncating towards 0.*/
+#define OC_DIV16(_x) OC_DIV_POW2(_x,4,0xF)
+/*Right shifts _dividend by _shift, adding _rval, and subtracting one for
+   negative dividends first.
+  When _rval is (1<<_shift-1), this is equivalent to division with rounding
+   ties away from zero.*/
+#define OC_DIV_ROUND_POW2(_dividend,_shift,_rval)\
+  ((_dividend)+OC_SIGNMASK(_dividend)+(_rval)>>(_shift))
+/*Divides a _x by 2, rounding towards even numbers.*/
+#define OC_DIV2_RE(_x) ((_x)+((_x)>>1&1)>>1)
+/*Divides a _x by (1<<(_shift)), rounding towards even numbers.*/
+#define OC_DIV_POW2_RE(_x,_shift) \
+  ((_x)+((_x)>>(_shift)&1)+((1<<(_shift))-1>>1)>>(_shift))
+/*Swaps two integers _a and _b if _a>_b.*/
+#define OC_SORT2I(_a,_b) \
+  do{ \
+    int t__; \
+    t__=((_a)^(_b))&-((_b)<(_a)); \
+    (_a)^=t__; \
+    (_b)^=t__; \
+  } \
+  while(0)
     
-    enum JpegReadMode {
-  JPEG_READ_HEADER,   // only basic headers
-  JPEG_READ_TABLES,   // headers and tables (quant, Huffman, ...)
-  JPEG_READ_ALL,      // everything
-};
+    #endif
+
     
-    int BuildJpegHuffmanTable(const int* count_in, const int* symbols,
-                          HuffmanTableEntry* lut) {
-  HuffmanTableEntry code;    // current table entry
-  HuffmanTableEntry* table;  // next available space in table
-  int len;         // current code length
-  int idx;         // symbol index
-  int key;         // prefix code
-  int reps;        // number of replicate key values in current table
-  int low;         // low bits for current root entry
-  int table_bits;  // key length of current table
-  int table_size;  // size of current table
-  int total_size;  // sum of root table size and 2nd level table sizes
-    }
+       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
     
-      OutputImageComponent& component(int c) { return components_[c]; }
-  const OutputImageComponent& component(int c) const { return components_[c]; }
-    
-    	if (!xlogger_IsEnabledFor((TLogLevel)_level)) {
-		return;
+    	PathCombine(configDirPath, exeDir, L'config');
+	if (wcscmp(userConfigDirPath, L'') == 0)
+	{
+		PathCombine(userConfigDirPath, exeDir, L'config');
 	}
+	else
+	{
+		PathCombine(userBinDirPath, userConfigDirPath, L'bin');
+		SHCreateDirectoryEx(0, userBinDirPath, 0);
+    }
     
-    bool WakeUpLock::IsLocking() {
-    return ::wakeupLock_IsLocking(object_);
-}
+    #include 'osquery/core/conversions.h'
     
+    #include <osquery/logger.h>
     
-    {  private:
-    size_t count_;
-    uint64_t time_span_;
-    std::list<uint64_t> touch_times_;
-};
-    
-    // Licensed under the MIT License (the 'License'); you may not use this file except in 
-// compliance with the License. You may obtain a copy of the License at
-// http://opensource.org/licenses/MIT
-    
-    // Licensed under the MIT License (the 'License'); you may not use this file except in 
-// compliance with the License. You may obtain a copy of the License at
-// http://opensource.org/licenses/MIT
-    
-    // CheckHandler is the class constructed by failing CHECK macros. CheckHandler
-// will log information about the failures and abort when it is destructed.
-class CheckHandler {
+    struct FSEventsSubscriptionContext : public SubscriptionContext {
  public:
-  CheckHandler(const char* check, const char* file, const char* func, int line)
-      : log_(GetErrorLogInstance()) {
-    log_ << file << ':' << line << ': ' << func << ': Check `' << check
-         << '' failed. ';
-  }
+  /// Subscription the following filesystem path.
+  std::string path;
     }
     
-    // Macros for declaring flags.
-#define DECLARE_bool(name) extern bool FLAG(name)
-#define DECLARE_int32(name) extern int32_t FLAG(name)
-#define DECLARE_int64(name) extern int64_t FLAG(name)
-#define DECLARE_double(name) extern double FLAG(name)
-#define DECLARE_string(name) extern std::string FLAG(name)
-    
-    
-    {  // Print user counters
-  for (const auto &ucn : user_counter_names_) {
-    auto it = run.counters.find(ucn);
-    if(it == run.counters.end()) {
-      Out << ',';
-    } else {
-      Out << ',' << it->second;
+    TEST_F(SyslogTests, test_populate_event_context) {
+  std::string line =
+      R'|('2016-03-22T21:17:01.701882+00:00','vagrant-ubuntu-trusty-64','6','cron','CRON[16538]:',' (root) CMD (   cd / && run-parts --report /etc/cron.hourly)')|';
+  SyslogEventPublisher pub;
+  auto ec = pub.createEventContext();
+  Status status = pub.populateEventContext(line, ec);
     }
-  }
-  Out << '\n';
-}
-    
-    #ifdef BENCHMARK_OS_EMSCRIPTEN
-#include <emscripten.h>
-#endif
-    
-    #include <cstdlib>
-    
-    
-    {void SleepForSeconds(double seconds) {
-  SleepForMicroseconds(static_cast<int>(seconds * kNumMicrosPerSecond));
-}
-#endif  // BENCHMARK_OS_WINDOWS
-}  // end namespace benchmark
