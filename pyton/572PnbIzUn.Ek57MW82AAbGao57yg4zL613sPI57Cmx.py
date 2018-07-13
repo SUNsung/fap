9e2ac2661cffdfba5e99aed060c043486442abbd
SUNsung <weatherius@gmail.com>
2018-07-14 01:55:54 +0300

    Rewrite [572PnbIzUn.Ek57MW82AAbGao57yg4zL613sPI57Cmx.py] File in: 2018-07-14 01:55:53

--- pyton/572PnbIzUn.Ek57MW82AAbGao57yg4zL613sPI57Cmx.py
+++ pyton/572PnbIzUn.Ek57MW82AAbGao57yg4zL613sPI57Cmx.py
@@ -1,59 +1,162 @@
 
         
-        
-class HitCounts(MRJob):
-    
-            (2016-01, shopping), 25
-        (2016-01, shopping), 100
-        (2016-01, gas), 50
-        '''
-        timestamp, category, amount = line.split('\t')
-        period = self. extract_year_month(timestamp)
-        if period == self.current_year_month():
-            yield (period, category), amount
-    
-        def test_first_exception(self):
-        future1 = self.executor.submit(mul, 2, 21)
-        future2 = self.executor.submit(sleep_and_raise, 1.5)
-        future3 = self.executor.submit(time.sleep, 3)
+            params = {
+        'age_limit': age,
+        'skip_download': True,
+        'writeinfojson': True,
+        'outtmpl': '%(id)s.%(ext)s',
+    }
+    ydl = YoutubeDL(params)
+    ydl.add_default_info_extractors()
+    json_filename = os.path.splitext(filename)[0] + '.info.json'
+    try_rm(json_filename)
+    ydl.download([url])
+    res = os.path.exists(json_filename)
+    try_rm(json_filename)
+    return res
     
     
-def FormatDebugInfoResponse_ExtraConfFoundButNotLoaded_test():
-  response = deepcopy( GENERIC_RESPONSE )
-  response[ 'extra_conf' ].update( {
-    'is_loaded': False,
-    'path': '/path/to/extra/conf'
-  } )
-  assert_that(
-    FormatDebugInfoResponse( response ),
-    contains_string(
-      'Extra configuration file found but not loaded\n'
-      'Extra configuration path: /path/to/extra/conf\n'
-    )
-  )
+class RtspFD(FileDownloader):
+    def real_download(self, filename, info_dict):
+        url = info_dict['url']
+        self.report_destination(filename)
+        tmpfilename = self.temp_name(filename)
     
+            webpage = self._download_webpage(url, video_id)
+        key = self._search_regex(
+            r'src=['\']https?://[^/]+/embed/([A-Za-z0-9_-]+)', webpage, 'key')
     
-def LoadJsonDefaultsIntoVim():
-  defaults = user_options_store.DefaultOptions()
-  for key, value in iteritems( defaults ):
-    new_key = 'g:ycm_' + key
-    if not vimsupport.VariableExists( new_key ):
-      vimsupport.SetVariableValue( new_key, value )
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    
-def SendEventNotificationAsync( event_name,
-                                buffer_number = None,
-                                extra_data = None ):
-  event = EventNotification( event_name, buffer_number, extra_data )
-  event.Start()
+            return {
+            'id': info['vid'],
+            'title': info['Subject'],
+            'duration': int(info['duration']) / 1000.0,
+            'formats': formats,
+            'thumbnail': info.get('bimg') or info.get('img'),
+        }
 
+    
+            title = metadata['Title']
+        description = metadata.get('Description')
+        duration = float_or_none(metadata.get('Duration'))
+        timestamp = parse_iso8601(metadata.get('DateCreated'))
+    
+        strings = []
+    with open(filepath, 'r') as f:
+    
+        test_suite = 'tests',
+    
+        # Get options and arguments.
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], _short_options, _options)
+    except getopt.GetoptError as e:
+        log.wtf('''
+    [Fatal] {}.
+    Try '{} --help' for more options.'''.format(e, script_name))
+    
+    __all__ = ['cbs_download']
+    
+    def kugou_download_playlist(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+    html=get_html(url)
+    pattern=re.compile('title='(.*?)'.* data='(\w*)\|.*?'')
+    pairs=pattern.findall(html)
+    for title,hash_val in pairs:
+        kugou_download_by_hash(title,hash_val,output_dir,merge,info_only)
+    
+    def kuwo_download_by_rid(rid, output_dir = '.', merge = True, info_only = False):
+    html=get_content('http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_%s'%rid)
+    title=match1(html,r'<name>(.*)</name>')
+    #to get title
+    #format =aac|mp3 ->to get aac format=mp3 ->to get mp3
+    url=get_content('http://antiserver.kuwo.cn/anti.s?format=mp3&rid=MUSIC_%s&type=convert_url&response=url'%rid)
+    songtype, ext, size = url_info(url)
+    print_info(site_info, title, songtype, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir)
+    
+            size_full = 0
+        for url in url_list:
+            type_, ext, size = url_info(url, headers=fake_headers)
+            size_full += size
+    
+        # For pickling to work, the __module__ variable needs to be set to the frame
+    # where the named tuple is created.  Bypass this step in enviroments where
+    # sys._getframe is not defined (Jython for example).
+    if hasattr(_sys, '_getframe'):
+        result.__module__ = _sys._getframe(1).f_globals.get('__name__', '__main__')
+    
+    def with_thread_pool_executor():
+    with ThreadPoolExecutor(10) as executor:
+        return list(executor.map(is_prime, PRIMES))
+    
+        def test_repr(self):
+        self.assertRegexpMatches(repr(PENDING_FUTURE),
+                                 '<Future at 0x[0-9a-f]+ state=pending>')
+        self.assertRegexpMatches(repr(RUNNING_FUTURE),
+                                 '<Future at 0x[0-9a-f]+ state=running>')
+        self.assertRegexpMatches(repr(CANCELLED_FUTURE),
+                                 '<Future at 0x[0-9a-f]+ state=cancelled>')
+        self.assertRegexpMatches(repr(CANCELLED_AND_NOTIFIED_FUTURE),
+                                 '<Future at 0x[0-9a-f]+ state=cancelled>')
+        self.assertRegexpMatches(
+                repr(EXCEPTION_FUTURE),
+                '<Future at 0x[0-9a-f]+ state=finished raised IOError>')
+        self.assertRegexpMatches(
+                repr(SUCCESSFUL_FUTURE),
+                '<Future at 0x[0-9a-f]+ state=finished returned int>')
+    
+    
+def ExtractKeywordsFromGroup_KeywordMiddle_test():
+  assert_that( syntax_parse._ExtractKeywordsFromGroup(
+                 syntax_parse.SyntaxGroup( '', [
+                   'foo contained bar',
+                   'zoo goo'
+                 ] ) ),
+               contains_inanyorder( 'foo', 'contained', 'bar', 'zoo', 'goo' ) )
+    
+    
+  def Just_Detailed_Info_test( self ):
+    self._Check( 9999999999, {
+      'insertion_text':  'INSERTION TEXT',
+      'menu_text':       'MENU TEXT',
+      'extra_menu_info': 'EXTRA MENU INFO',
+      'kind':            'K',
+      'detailed_info':   'DETAILED INFO',
+    }, {
+      'word'     : 'INSERTION TEXT',
+      'abbr'     : 'MENU TEXT',
+      'menu'     : 'EXTRA MENU INFO',
+      'kind'     : 'k',
+      'info'     : 'DETAILED INFO',
+      'dup'      : 1,
+      'empty'    : 1,
+      'user_data': '9999999999',
+    } )
+    
+    from ycm.client.base_request import BaseRequest, BuildRequestData
+    
+    from ycm.client.base_request import BaseRequest, BuildRequestData
+    
+    
+def _HandlePollResponse( response, diagnostics_handler ):
+  if isinstance( response, list ):
+    for notification in response:
+      if 'message' in notification:
+        PostVimMessage( notification[ 'message' ],
+                        warning = False,
+                        truncate = True )
+      elif 'diagnostics' in notification:
+        diagnostics_handler.UpdateWithNewDiagnosticsForFile(
+          notification[ 'filepath' ],
+          notification[ 'diagnostics' ] )
+  elif response is False:
+    # Don't keep polling for this file
+    return False
+  # else any truthy response means 'nothing to see here; poll again in a
+  # while'
+    
+    
+  def MarkResponseHandled( self ):
+    self._handled_tick = self._parse_tick
     
                 payload = {'host': 'http://host:8088/services/collector/event',
                        'event': body}
@@ -68,22 +171,3 @@ def SendEventNotificationAsync( event_name,
                 )
             )
             self.mock_post.reset_mock()
-
-    
-    For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/light.zigbee/
-'''
-import voluptuous as vol
-    
-        if unit_2 == LENGTH_MILES:
-        result = __meters_to_miles(meters)
-    elif unit_2 == LENGTH_FEET:
-        result = __meters_to_feet(meters)
-    elif unit_2 == LENGTH_KILOMETERS:
-        result = __meters_to_kilometers(meters)
-    
-    import homeassistant.core as ha
-from homeassistant.const import EVENT_STATE_CHANGED
-from homeassistant.util import dt
-from homeassistant.components.recorder.models import (
-    Base, Events, States, RecorderRuns)
