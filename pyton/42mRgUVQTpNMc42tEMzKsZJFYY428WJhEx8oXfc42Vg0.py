933779b1fdbdd34d0f56276482a39612a0c7e351
SUNsung <weatherius@gmail.com>
2018-07-14 02:32:20 +0300

    Rewrite [42mRgUVQTpNMc42tEMzKsZJFYY428WJhEx8oXfc42Vg0.py] File in: 2018-07-14 02:32:19

--- pyton/42mRgUVQTpNMc42tEMzKsZJFYY428WJhEx8oXfc42Vg0.py
+++ pyton/42mRgUVQTpNMc42tEMzKsZJFYY428WJhEx8oXfc42Vg0.py
@@ -1,45 +1,125 @@
 
         
-            for ns in n_samples:
-        for nf in n_features:
-            it += 1
-            print('==================')
-            print('Iteration %s of %s' % (it, max(len(n_samples),
-                                          len(n_features))))
-            print('==================')
-            n_informative = nf // 10
-            X, Y, coef_ = make_regression(n_samples=ns, n_features=nf,
-                                          n_informative=n_informative,
-                                          noise=0.1, coef=True)
-    
-                    out[k, j].flat[i] = t
-    return out
-    
-    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
-DATA_FOLDER = 'txt_sentoken'
-    
-    for f in solutions:
-    if not f.endswith('.py'):
-        continue
-    
-    The dataset is generated using the ``make_biclusters`` function, which
-creates a matrix of small values and implants bicluster with large
-values. The rows and columns are then shuffled and passed to the
-Spectral Co-Clustering algorithm. Rearranging the shuffled matrix to
-make biclusters contiguous shows how accurately the algorithm found
-the biclusters.
-    
-    # Train random forest classifier, calibrate on validation data and evaluate
-# on test data
-clf = RandomForestClassifier(n_estimators=25)
-clf.fit(X_train, y_train)
-clf_probs = clf.predict_proba(X_test)
-sig_clf = CalibratedClassifierCV(clf, method='sigmoid', cv='prefit')
-sig_clf.fit(X_valid, y_valid)
-sig_clf_probs = sig_clf.predict_proba(X_test)
-sig_score = log_loss(y_test, sig_clf_probs)
+            # Then we cluster the lines together as blocks
+    # Each block represents a collection of lines that should be sorted
+    # This was done by assuming only links ([...](...)) are meant to be sorted
+    # Clustering is done by indentation
+    blocks = []
+    last_indent = None
+    for line in read_me:
+        s_line = line.lstrip()
+        indent = len(line) - len(s_line)
     
     
-# Code source: Gaël Varoquaux
-# Modified for documentation by Jaques Grobler
-# License: BSD 3 clause
+def check_alphabetical(lines):
+    '''
+    checks if all entries per section are in alphabetical order based in entry title
+    '''
+    sections = {}
+    section_line_num = {}
+    for line_num, line in enumerate(lines):
+        if line.startswith(anchor):
+            category = line.split(anchor)[1].strip()
+            sections[category] = []
+            section_line_num[category] = line_num
+            continue
+        if not line.startswith('|') or line.startswith('|---'):
+            continue
+        title = [x.strip() for x in line.split('|')[1:-1]][0].upper()
+        sections[category].append(title)
+    
+        revision is a git commit reference (hash or name)
+    
+    from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import Perceptron
+from sklearn.pipeline import Pipeline
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+    
+        # split the dataset in training and test set:
+    docs_train, docs_test, y_train, y_test = train_test_split(
+        dataset.data, dataset.target, test_size=0.25, random_state=None)
+    
+            if not in_exercise_region or linestrip.startswith('#'):
+            output_file.write(line)
+    
+            C = float(self.complexity.get())
+        gamma = float(self.gamma.get())
+        coef0 = float(self.coef0.get())
+        degree = int(self.degree.get())
+        kernel_map = {0: 'linear', 1: 'rbf', 2: 'poly'}
+        if len(np.unique(y)) == 1:
+            clf = svm.OneClassSVM(kernel=kernel_map[self.kernel.get()],
+                                  gamma=gamma, coef0=coef0, degree=degree)
+            clf.fit(X)
+        else:
+            clf = svm.SVC(kernel=kernel_map[self.kernel.get()], C=C,
+                          gamma=gamma, coef0=coef0, degree=degree)
+            clf.fit(X, y)
+        if hasattr(clf, 'score'):
+            print('Accuracy:', clf.score(X, y) * 100)
+        X1, X2, Z = self.decision_surface(clf)
+        self.model.clf = clf
+        self.model.set_surface((X1, X2, Z))
+        self.model.surface_type = self.surface_type.get()
+        self.fitted = True
+        self.model.changed('surface')
+    
+    np.random.seed(0)
+    
+        def _perform(self, domain, validation_name, validation):
+        self._get_digitalocean_client().add_txt_record(domain, validation_name, validation)
+    
+    # If false, no module index is generated.
+#html_domain_indices = True
+    
+        @mock.patch('certbot_compatibility_test.validator.requests.get')
+    def test_hsts_expire(self, mock_get_request):
+        mock_get_request.return_value = create_response(
+            headers={'strict-transport-security': 'max-age=3600'})
+        self.assertFalse(self.validator.hsts('test.com'))
+    
+    
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/', None),
+    'acme': ('https://acme-python.readthedocs.org/en/latest/', None),
+    'certbot': ('https://certbot.eff.org/docs/', None),
+}
+
+    
+        def test_sequential_execution(self):
+        self.command_stack[0].execute()
+        output_after_first_execution = os.listdir(self.test_dir)
+        self.assertEqual(output_after_first_execution[0], 'bar.txt')
+        self.command_stack[1].execute()
+        output_after_second_execution = os.listdir(self.test_dir)
+        self.assertEqual(output_after_second_execution[0], 'baz.txt')
+    
+            def __init__(self, *args, **kwargs):
+            pass
+    
+    from dft.setter_injection import TimeDisplay, MidnightTimeProvider, ProductionCodeTimeProvider, datetime
+    
+        def __init__(self):
+        pass
+    
+    if __name__ == '__main__':
+    
+    
+class Transactional(object):
+    '''Adds transactional semantics to methods. Methods decorated  with
+    
+    
+class OrSpecification(CompositeSpecification):
+    _one = Specification()
+    _other = Specification()
+    
+        def _clear_current_hook(self):
+        if self.is_current:
+            asyncio.set_event_loop(self.old_asyncio)
+            self.is_current = False
+    
+            async def runner():
+            # Wait for waiter() and notifier() in parallel
+            await gen.multi([waiter(), notifier()])
