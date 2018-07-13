b0446b1c03c9f34469425fd4139146d95a16bdd6
SUNsung <weatherius@gmail.com>
2018-07-13 15:52:51 +0300

    Rewrite [252SLGQIo84jk25XjyVCJrWJdo25qlscaAdyTc.25Plb.py] File in: 2018-07-13 15:52:50

--- pyton/252SLGQIo84jk25XjyVCJrWJdo25qlscaAdyTc.25Plb.py
+++ pyton/252SLGQIo84jk25XjyVCJrWJdo25qlscaAdyTc.25Plb.py
@@ -1,87 +1,168 @@
 
         
-            dimensions = 500 * np.arange(1, n_iter + 1)
+        if len(sys.argv) == 2 and sys.argv[1] == '--list':
+    print(json.dumps(result))
+elif len(sys.argv) == 3 and sys.argv[1] == '--host':
+    print(json.dumps({}))
+else:
+    print('Need an argument, either --list or --host <host>')
+
     
-        n_features = 10
-    list_n_samples = np.linspace(100, 1000000, 5).astype(np.int)
-    lasso_results, lars_lasso_results = compute_bench(alpha, list_n_samples,
-                                            [n_features], precompute=True)
+        parent = os.path.join(path, os.path.pardir)
+    parent = os.path.realpath(parent)
+    
+        target = parser.add_mutually_exclusive_group()
+    
+        try:
+        return json.dumps(result, sort_keys=True, indent=indent, ensure_ascii=False)
+    except UnicodeDecodeError:
+        return json.dumps(result, sort_keys=True, indent=indent)
+
+    
+    AnsibleConstructor.add_constructor(
+    u'tag:yaml.org,2002:seq',
+    AnsibleConstructor.construct_yaml_seq)
+    
+    from sklearn.datasets import make_multilabel_classification
+from sklearn.metrics import (f1_score, accuracy_score, hamming_loss,
+                             jaccard_similarity_score)
+from sklearn.utils.testing import ignore_warnings
+    
+    
+if __name__ == '__main__':
+    ###########################################################################
+    # Option parser
+    ###########################################################################
+    op = optparse.OptionParser()
+    op.add_option('--n-times',
+                  dest='n_times', default=5, type=int,
+                  help='Benchmark results are average over n_times experiments')
     
     try:
     from urllib import urlopen
 except ImportError:
     from urllib.request import urlopen
     
-    import sys
+    Sentiment analysis can be casted as a binary text classification problem,
+that is fitting a linear classifier on features extracted from the text
+of the user messages so as to guess wether the opinion of the author is
+positive or negative.
     
-    plt.show()
-
+    exercise_dir = os.path.dirname(__file__)
+if exercise_dir == '':
+    exercise_dir = '.'
     
-    from sklearn.datasets import make_blobs
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+    plt.figure(2)  # 'banana' shape
+plt.title('Outlier detection on a real data set (boston housing)')
+plt.scatter(X2[:, 0], X2[:, 1], color='black')
+plt.xlim((xx2.min(), xx2.max()))
+plt.ylim((yy2.min(), yy2.max()))
+plt.legend((legend2_values_list[0].collections[0],
+            legend2_values_list[1].collections[0],
+            legend2_values_list[2].collections[0]),
+           (legend2_keys_list[0], legend2_keys_list[1], legend2_keys_list[2]),
+           loc='upper center',
+           prop=matplotlib.font_manager.FontProperties(size=12))
+plt.ylabel('% lower status of the population')
+plt.xlabel('average number of rooms per dwelling')
     
-    plt.title('Clustering measures for 2 random uniform labelings\n'
-          'with equal number of clusters')
-plt.xlabel('Number of clusters (Number of samples is fixed to %d)' % n_samples)
-plt.ylabel('Score value')
-plt.legend(plots, names)
-plt.ylim(ymin=-0.05, ymax=1.05)
+    # Illustrate calibrator
+plt.figure(1)
+# generate grid over 2-simplex
+p1d = np.linspace(0, 1, 20)
+p0, p1 = np.meshgrid(p1d, p1d)
+p2 = 1 - p0 - p1
+p = np.c_[p0.ravel(), p1.ravel(), p2.ravel()]
+p = p[p[:, 2] >= 0]
     
-    This example, inspired from Chen's publication [1], shows a comparison
-of the estimated MSE of the LW and OAS methods, using Gaussian
-distributed data.
+    Plot the classification probability for different classifiers. We use a 3
+class dataset, and we classify it with a Support Vector classifier, L1
+and L2 penalized logistic regression with either a One-Vs-Rest or multinomial
+setting, and Gaussian process classification.
     
+    plt.plot(features_samples_ratio, acc_clf1, linewidth=2,
+         label='Linear Discriminant Analysis with shrinkage', color='navy')
+plt.plot(features_samples_ratio, acc_clf2, linewidth=2,
+         label='Linear Discriminant Analysis', color='gold')
     
-def extract_board_data(url):
-    json_data = extract_json_data(url, limit=LIMIT)
-    pin_list = json_data['pins']
-    title = json_data['title']
-    pin_count = json_data['pin_count']
-    pin_count -= len(pin_list)
+        def __init__(self, crawler):
+        self._crawler = crawler
+        self._schemes = {}  # stores acceptable schemes on instancing
+        self._handlers = {}  # stores instanced handlers for schemes
+        self._notconfigured = {}  # remembers failed handlers
+        handlers = without_none_values(
+            crawler.settings.getwithbase('DOWNLOAD_HANDLERS'))
+        for scheme, clspath in six.iteritems(handlers):
+            self._schemes[scheme] = clspath
     
-    site_info = 'Mixcloud.com'
-download = mixcloud_download
-download_playlist = playlist_not_supported('mixcloud')
-
+            # Method command
+        self.sendCommand(self.factory.method, self.factory.path)
+        # Headers
+        for key, values in self.factory.headers.items():
+            for value in values:
+                self.sendHeader(key, value)
+        self.endHeaders()
+        # Body
+        if self.factory.body is not None:
+            self.transport.write(self.factory.body)
     
-    if (platform.python_implementation() == 'CPython' and
-        os.environ.get('TORNADO_EXTENSION') != '0'):
-    # This extension builds and works on pypy as well, although pypy's jit
-    # produces equivalent performance.
-    kwargs['ext_modules'] = [
-        Extension('tornado.speedups',
-                  sources=['tornado/speedups.c']),
-    ]
+        def scrape_response(self, scrape_func, response, request, spider):
+        fname = lambda f:'%s.%s' % (
+                six.get_method_self(f).__class__.__name__,
+                six.get_method_function(f).__name__)
     
-        def supports_http_1_1(self):
-        '''Returns True if this request supports HTTP/1.1 semantics.
+        def _has_ajax_crawlable_variant(self, response):
+        '''
+        Return True if a page without hash fragment could be 'AJAX crawlable'
+        according to https://developers.google.com/webmasters/ajax-crawling/docs/getting-started.
+        '''
+        body = response.text[:self.lookup_bytes]
+        return _has_ajaxcrawlable_meta(body)
     
-            event = Event()
-        test = self
-        body = []
+        def _get_request_cookies(self, jar, request):
+        if isinstance(request.cookies, dict):
+            cookie_list = [{'name': k, 'value': v} for k, v in \
+                    six.iteritems(request.cookies)]
+        else:
+            cookie_list = request.cookies
     
-           Use a regular `.Application` and wrap it in `WSGIAdapter` instead.
-       This class will be removed in Tornado 6.0.
+    from twisted.internet import reactor
+    
+        def test_cat_shall_meow(self):
+        noise = self.cat.meow()
+        expected_noise = 'meow!'
+        self.assertEqual(noise, expected_noise)
+    
+        def test_dog_greek_localization(self):
+        self.assertEqual(self.g.get('dog'), 'σκύλος')
+    
+        def test_3rd_am_station_after_scan(self):
+        self.radio.scan()
+        station = self.radio.state.stations[self.radio.state.pos]
+        expected_station = '1510'
+        self.assertEqual(station, expected_station)
+    
+        def test_tc1_output(self):
+        self.tc1.run()
+        output = self.out.getvalue().strip()
+        self.assertEqual(output, self.average_result_tc1)
+    
     '''
-    def __call__(self, environ, start_response):
-        return WSGIAdapter(self)(environ, start_response)
+*TL;DR80
+Encapsulates all information needed to perform an action or trigger an event.
+'''
     
-            fut = http.fetch(self._OAUTH_ACCESS_TOKEN_URL,
-                         method='POST',
-                         headers={'Content-Type': 'application/x-www-form-urlencoded'},
-                         body=body)
-        fut.add_done_callback(functools.partial(self._on_access_token, callback))
+        def commit(self):
+        self.states = [memento(target, self.deep) for target in self.targets]
     
-        def activate(self):
-        exc_info = self.exc_info
-        if exc_info is not None:
-            self.exc_info = None
-            self.formatted_tb = traceback.format_exception(*exc_info)
     
-            fut = Future()
-        fut.cancel()
-        is_cancelled = fut.cancelled()
-        future_set_result_unless_cancelled(fut, 42)
-        self.assertEqual(fut.cancelled(), is_cancelled)
-        if not is_cancelled:
-            self.assertEqual(fut.result(), 42)
+# Example usage...
+def main():
+    data1 = Data('Data 1')
+    data2 = Data('Data 2')
+    view1 = DecimalViewer()
+    view2 = HexViewer()
+    data1.attach(view1)
+    data1.attach(view2)
+    data2.attach(view2)
+    data2.attach(view1)
