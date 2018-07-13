9ff21d50c537593777b0b8b1f75811045c0be58f
SUNsung <weatherius@gmail.com>
2018-07-13 12:28:05 +0300

    Rewrite [0057HFUisG5Q005I8s9KtpUfA00POte1SVpshw003DE.py] File in: 2018-07-13 12:28:04

--- pyton/0057HFUisG5Q005I8s9KtpUfA00POte1SVpshw003DE.py
+++ pyton/0057HFUisG5Q005I8s9KtpUfA00POte1SVpshw003DE.py
@@ -1,10 +1,43 @@
 
         
-        - tree.RewriteCardinalityException
-  - tree.RewriteEarlyExitException
-  - tree.RewriteEmptyStreamException
-  .
-.
+            def from_envvar(self, variable_name, silent=False):
+        '''Loads a configuration from an environment variable pointing to
+        a configuration file.  This is basically just a shortcut with nicer
+        error messages for this line of code::
     
-    # Predefined token types
-EOR_TOKEN_TYPE = 1
+        If you configure your own :class:`logging.StreamHandler`, you may want to
+    use this for the stream. If you are using file or dict configuration and
+    can't import this directly, you can refer to it as
+    ``ext://flask.logging.wsgi_errors_stream``.
+    '''
+    return request.environ['wsgi.errors'] if request else sys.stderr
+    
+            rv = app.config['SERVER_NAME']
+    
+    signals_available = False
+try:
+    from blinker import Namespace
+    signals_available = True
+except ImportError:
+    class Namespace(object):
+        def signal(self, name, doc=None):
+            return _FakeSignal(name, doc)
+    
+    
+def common_object_test(app):
+    assert app.secret_key == 'config'
+    assert app.config['TEST_KEY'] == 'foo'
+    assert 'TestConfig' not in app.config
+    
+        logger.propagate = False
+    assert not has_level_handler(logger)
+    logger.propagate = True
+    
+    def shorten_title(title):
+    m1 = re.search('[[0-9]*]', title)
+    m2 = re.search(''.*'', title)
+    if m1:
+        title = m1.group(0)
+    if m2:
+        title = ' '.join((title, m2.group(0)))   
+    return title[:50] + ' [...]'    
