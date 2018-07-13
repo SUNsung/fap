3914efdefa29647d5f8e6dce8848b2f03a2d518e
SUNsung <weatherius@gmail.com>
2018-07-13 22:37:58 +0300

    Rewrite [56i9GW1Ho5kvU561l2AVYjN4eg56p2q9bbBQcQ.566Vf.rb] File in: 2018-07-13 22:37:57

--- ruby/56i9GW1Ho5kvU561l2AVYjN4eg56p2q9bbBQcQ.566Vf.rb
+++ ruby/56i9GW1Ho5kvU561l2AVYjN4eg56p2q9bbBQcQ.566Vf.rb
@@ -1,101 +1,13 @@
 
         
-                def test_url_missing_scheme
-          spec = resolve 'foo'
-          assert_equal({
-            'database' => 'foo' }, spec)
+          def background_color
+    '#191b22'
+  end
+    
+          def update_if_necessary!
+        if @update && config.verbose?
+          UI.section('\nUpdating Spec Repositories\n'.yellow) do
+            Repo.new(ARGV.new(['update'])).run
+          end
         end
-    
-    class WrongReply < Reply
-  validate :errors_on_empty_content
-  validate :title_is_wrong_create, on: :create
-    
-      setup do
-    @connection = TestConnection.new
-  end
-    
-      test 'disallow negative and zero periods' do
-    [ 0, 0.0, 0.seconds, -1, -1.seconds, 'foo', :foo, Object.new ].each do |invalid|
-      e = assert_raise ArgumentError do
-        ChatChannel.periodically :send_updates, every: invalid
       end
-      assert_match(/Expected every:/, e.message)
-    end
-  end
-    
-        def secret_action
-    end
-  end
-    
-    class ActionCable::Connection::IdentifierTest < ActionCable::TestCase
-  class Connection < ActionCable::Connection::Base
-    identified_by :current_user
-    attr_reader :websocket
-    
-      # GET /resource/sign_in
-  def new
-    self.resource = resource_class.new(sign_in_params)
-    clean_up_passwords(resource)
-    yield resource if block_given?
-    respond_with(resource, serialize_options(resource))
-  end
-    
-          if rails?
-        register_rails_engine
-      elsif lotus?
-        register_lotus
-      elsif sprockets?
-        register_sprockets
-      end
-    
-      # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
-    
-        it 'returns related entity for an existing remote post' do
-      post = FactoryGirl.create(:status_message, author: remote_person)
-      entity = DiasporaFederation.callbacks.trigger(:fetch_related_entity, 'Post', post.guid)
-      expect(entity.author).to eq(post.diaspora_handle)
-      expect(entity.local).to be_falsey
-      expect(entity.public).to be_falsey
-      expect(entity.parent).to be_nil
-    end
-    
-          def redirect_prompt_error_display(error, error_description)
-        redirect_params_hash = {error: error, error_description: error_description, state: params[:state]}
-        redirect_fragment = redirect_params_hash.compact.map {|key, value| key.to_s + '=' + value }.join('&')
-        redirect_to params[:redirect_uri] + '?' + redirect_fragment
-      end
-    
-        # Merges this query list with another. The returned query list
-    # queries for the intersection between the two inputs.
-    #
-    # Both query lists should be resolved.
-    #
-    # @param other [QueryList]
-    # @return [QueryList?] The merged list, or nil if there is no intersection.
-    def merge(other)
-      new_queries = queries.map {|q1| other.queries.map {|q2| q1.merge(q2)}}.flatten.compact
-      return if new_queries.empty?
-      QueryList.new(new_queries)
-    end
-    
-      at_exit { Application.run! if $!.nil? && Application.run? }
-end
-    
-          def call(env)
-        request  = Request.new(env)
-        get_was  = handle(request.GET)
-        post_was = handle(request.POST) rescue nil
-        app.call env
-      ensure
-        request.GET.replace  get_was  if get_was
-        request.POST.replace post_was if post_was
-      end
-    
-      # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = :random
