8aeaacdf97ebded743fe572189d64466e7b36610
SUNsung <weatherius@gmail.com>
2018-07-14 09:40:21 +0300

    Rewrite [08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb] File in: 2018-07-14 09:40:21

--- ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
+++ ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
@@ -1,104 +1,44 @@
 
         
-          describe '.all' do
-    it 'uses the client to fetch all keys' do
-      mock_client_response(:list_keys, with: no_args) do
-        [
-          {
-            canDownload: false,
-            canRevoke: true,
-            keyId: 'some-key-id',
-            keyName: 'Test Key via fastlane',
-            servicesCount: 2
-          },
-          {
-            canDownload: true,
-            canRevoke: true,
-            keyId: 'B92NE4F7RG',
-            keyName: 'Test Key via browser',
-            servicesCount: 2
-          }
-        ]
+        module Devise
+  module Controllers
+    # A module that may be optionally included in a controller in order
+    # to provide remember me behavior. Useful when signing in is done
+    # through a callback, like in OmniAuth.
+    module Rememberable
+      # Return default cookie values retrieved from session options.
+      def self.cookie_values
+        Rails.configuration.session_options.slice(:path, :domain, :secure)
       end
     
-            expect(result).to eq('/usr/local/bin/cloc  --by-file --xml  --out=build/cloc.xml')
+    module Jekyll
+    
+        # Loops through the list of category pages and processes each one.
+    def write_category_indexes
+      if self.layouts.key? 'category_index'
+        dir = self.config['category_dir'] || 'categories'
+        self.categories.keys.each do |category|
+          self.write_category_index(File.join(dir, category.to_url), category)
+        end
+    
+      class VideoTag < Liquid::Tag
+    @video = nil
+    @poster = ''
+    @height = ''
+    @width = ''
+    
+            end
       end
-    
-          it 'it increments all targets minor version number' do
-        Fastlane::FastFile.new.parse('lane :test do
-          increment_version_number(bump_type: 'minor')
-        end').runner.execute(:test)
-    
-        find_union(segments, Group).order_id_desc
-  end
-    
-        # The path used after sending reset password instructions
-    def after_sending_reset_password_instructions_path_for(resource_name)
-      new_session_path(resource_name) if is_navigational_format?
-    end
-    
-                # Remove the signal trap if no more registered callbacks exist
-            Signal.trap('INT', 'DEFAULT') if registered.empty?
-          end
-        end
-    
-    def report_apps
-  running = Set.new
-  @app_names.zip(@bundle_ids, @unix_ids).each do |app_name, bundle_id, _unix_id|
-    next if excluded_bundle_id bundle_id
-    next if excluded_app_name app_name
-    bundle_id.gsub!(%r{^(missing value)$}, '<\1>')
-    running.add '#{bundle_id}\t#{app_name}'
-  end
-    
-          def initialize(pairs = {})
-        @pairs = pairs
-        pairs.each do |key, value|
-          raise 'invalid container key: '#{key.inspect}'' unless VALID_KEYS.include?(key)
-          send(:'#{key}=', value)
-        end
-    
-        formulae = ARGV.include?('--installed') ? Formula.installed : Formula
-    recursive = ARGV.flag? '--recursive'
-    only_installed_arg = ARGV.include?('--installed') &&
-                         !ARGV.include?('--include-build') &&
-                         !ARGV.include?('--include-test') &&
-                         !ARGV.include?('--include-optional') &&
-                         !ARGV.include?('--skip-recommended')
-    
-    #
-# Project
-#
-    
-        if extension_pathname.readable?
-      ENV['BUNDLE_GEMFILE'] = extension_pathname.to_path
-      break
     end
   end
 end
-    
-    $stdout.puts res
 
     
-      def register_sigs
-    self.sigs = {
-      :banner		=> /^(\*\s+OK[^\n\r]*)/i,
-      :login		=> /^CAPABILITY\s+LOGIN\s+([^\s]+)\s+([^\n\r]+)/i,
-      :login_pass => /^CAPABILITY\s+OK\s+(Login[^\n\r]*)/i,
-      :login_bad	=> /^CAPABILITY\s+BAD\s+(Login[^\n\r]*)/i,
-      :login_fail => /^CAPABILITY\s+NO\s+(Login[^\n\r]*)/i
-    }
-  end
-    
-                  s[:proto] = 'tcp'
-              s[:name]  = 'pop3'
-              s[:extra] = 'Successful Login. Banner: #{s[:banner]}'
-              report_auth_info(s)
-              print_status('Successful POP3 Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]} (#{s[:banner].strip})')
-    
-    module Capistrano
-  module TaskEnhancements
-    def before(task, prerequisite, *args, &block)
-      prerequisite = Rake::Task.define_task(prerequisite, *args, &block) if block_given?
-      Rake::Task[task].enhance [prerequisite]
-    end
+          # Only print require() calls that did actual work.
+      # require() returns true on load, false if already loaded.
+      if result
+        source = caller[0]
+        #p source.include?('/lib/polyglot.rb:63:in `require'') => source
+        if source.include?('/lib/polyglot.rb:63:in `require'')
+          source = caller[1]
+        end
