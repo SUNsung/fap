cba1c658102843a9e1adbfd875c8bea7faf8a24b
SUNsung <weatherius@gmail.com>
2018-07-14 17:01:09 +0300

    Rewrite [02bIE8nQMRkgU02s22SVLjCKqA021ZEvK.6phRE02KDU.py] File in: 2018-07-14 17:01:09

--- pyton/02bIE8nQMRkgU02s22SVLjCKqA021ZEvK.6phRE02KDU.py
+++ pyton/02bIE8nQMRkgU02s22SVLjCKqA021ZEvK.6phRE02KDU.py
@@ -1,139 +1,77 @@
 
         
-        
-# Make some random data with uniformly located non zero entries with
-# Gaussian distributed values
-def make_sparse_random_data(n_samples, n_features, n_nonzeros,
-                            random_state=None):
-    rng = np.random.RandomState(random_state)
-    data_coo = sp.coo_matrix(
-        (rng.randn(n_nonzeros),
-        (rng.randint(n_samples, size=n_nonzeros),
-         rng.randint(n_features, size=n_nonzeros))),
-        shape=(n_samples, n_features))
-    return data_coo.toarray(), data_coo.tocsr()
-    
-        print('Decompressing %s' % ARCHIVE_NAME)
-    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
-        archive.extractall(path='.')
-    os.remove(ARCHIVE_NAME)
-
-    
-        if not os.path.exists(ARCHIVE_NAME):
-        print('Downloading dataset from %s (14 MB)' % URL)
-        opener = urlopen(URL)
-        with open(ARCHIVE_NAME, 'wb') as archive:
-            archive.write(opener.read())
-    
-        print('Generating skeleton for %s' % f)
-    
-            cmap_group.pack(side=Tk.LEFT)
-    
-    plt.matshow(data, cmap=plt.cm.Blues)
-plt.title('Original dataset')
-    
-        t0 = time()
-    scores = uniform_labelings_scores(score_func, n_samples, n_clusters_range)
-    print('done in %0.3fs' % (time() - t0))
-    plots.append(plt.errorbar(
-        n_clusters_range, np.median(scores, axis=1), scores.std(axis=1))[0])
-    names.append(score_func.__name__)
-    
-    import matplotlib.pyplot as plt
+            # Then we call the sorting method
+    sort_blocks()
     
         def _finish(self, request):
         self.concurrent -= 1
         if not request.finished and not request._disconnected:
             request.finish()
     
-        def __enter__(self):
-        from scrapy.utils.test import get_testenv
-        pargs = [sys.executable, '-u', '-m', 'scrapy.utils.benchserver']
-        self.proc = subprocess.Popen(pargs, stdout=subprocess.PIPE,
-                                     env=get_testenv())
-        self.proc.stdout.readline()
+        def help(self):
+        '''An extensive help for the command. It will be shown when using the
+        'help' command. It can contain newlines, since not post-formatting will
+        be applied to its contents.
+        '''
+        return self.long_desc()
     
-        def __init__(self, *args, **kwargs):
-        super(ReturnsContract, self).__init__(*args, **kwargs)
+        def _genspider(self, module, name, domain, template_name, template_file):
+        '''Generate the spider module, based on the given template'''
+        tvars = {
+            'project_name': self.settings.get('BOT_NAME'),
+            'ProjectName': string_camelcase(self.settings.get('BOT_NAME')),
+            'module': module,
+            'name': name,
+            'domain': domain,
+            'classname': '%sSpider' % ''.join(s.capitalize() \
+                for s in module.split('_'))
+        }
+        if self.settings.get('NEWSPIDER_MODULE'):
+            spiders_module = import_module(self.settings['NEWSPIDER_MODULE'])
+            spiders_dir = abspath(dirname(spiders_module.__file__))
+        else:
+            spiders_module = None
+            spiders_dir = '.'
+        spider_file = '%s.py' % join(spiders_dir, module)
+        shutil.copyfile(template_file, spider_file)
+        render_templatefile(spider_file, **tvars)
+        print('Created spider %r using template %r ' % (name, \
+            template_name), end=('' if spiders_module else '\n'))
+        if spiders_module:
+            print('in module:\n  %s.%s' % (spiders_module.__name__, module))
     
-        def _is_tar(self, response):
-        archive = BytesIO(response.body)
+        def adjust_request_args(self, args):
+        args['url'] = self.args[0]
+        return args
+    
         try:
-            tar_file = tarfile.open(name=mktemp(), fileobj=archive)
-        except tarfile.ReadError:
-            return
-    
-        @classmethod
-    def from_crawler(cls, crawler):
-        o = cls()
-        crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
-        return o
-    
-        def log(self, request, spider):  # log that a request has been filtered
-        pass
-    
-        def error_count(self, failure, response, spider):
-        self.counter['errorcount'] += 1
-        if self.counter['errorcount'] == self.close_on['errorcount']:
-            self.crawler.engine.close_spider(spider, 'closespider_errorcount')
-    
-        if address_family == socket.AF_INET:
-        if len(packed_ip) != ctypes.sizeof(addr.ipv4_addr):
-            raise socket.error('packed IP wrong length for inet_ntoa')
-        ctypes.memmove(addr.ipv4_addr, packed_ip, 4)
-    elif address_family == socket.AF_INET6:
-        if len(packed_ip) != ctypes.sizeof(addr.ipv6_addr):
-            raise socket.error('packed IP wrong length for inet_ntoa')
-        ctypes.memmove(addr.ipv6_addr, packed_ip, 16)
+        import boto.auth
+    except ImportError:
+        _S3Connection = _v19_S3Connection
     else:
+        _S3Connection = _v20_S3Connection
+    
+        def _format_cookie(self, cookie):
+        # build cookie string
+        cookie_str = '%s=%s' % (cookie['name'], cookie['value'])
+    
+        def spider_opened(self, spider):
+        usr = getattr(spider, 'http_user', '')
+        pwd = getattr(spider, 'http_pass', '')
+        if usr or pwd:
+            self.auth = basic_auth_header(usr, pwd)
+    
+    from scrapy.utils.job import job_dir
+from scrapy.utils.request import request_fingerprint
+    
+    class DropItem(Exception):
+    '''Drop item from the item pipeline'''
+    pass
+    
         raise socket.error('unknown address family')
     
+    MIN_TOKEN_TYPE = UP+1
     
-    def getDescription(self):
-        return 'n/a'
+        output_directory = 'pdfs' if results.directory is None else results.directory
     
-                 => ID '=' '(' INT ')' ('+' atom)* ';'
-                                ^
-        match() will see that ';' doesn't match ')' and report a
-        mismatched token error.  To recover, it sees that LA(1)==';'
-        is in the set of tokens that can follow the ')' token
-        reference in rule atom.  It can assume that you forgot the ')'.
-        '''
-    
-            if i < 0:
-            i += 1 # e.g., translate LA(-1) to use offset i=0; then data[p+0-1]
-    
-    # begin[licence]
-#
-# [The 'BSD licence']
-# Copyright (c) 2005-2008 Terence Parr
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# end[licence]
-    
-    For more details about this component, please refer to the documentation at
-https://home-assistant.io/components/keyboard/
-'''
-import voluptuous as vol
+    from weasyprint import HTML
