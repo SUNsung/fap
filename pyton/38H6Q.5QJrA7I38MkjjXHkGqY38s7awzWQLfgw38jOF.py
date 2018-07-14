67497379e1f68998a74faf94e41b3b36f6f2b066
SUNsung <weatherius@gmail.com>
2018-07-15 01:49:52 +0300

    Rewrite [38H6Q.5QJrA7I38MkjjXHkGqY38s7awzWQLfgw38jOF.py] File in: 2018-07-15 01:49:51

--- pyton/38H6Q.5QJrA7I38MkjjXHkGqY38s7awzWQLfgw38jOF.py
+++ pyton/38H6Q.5QJrA7I38MkjjXHkGqY38s7awzWQLfgw38jOF.py
@@ -1,50 +1,119 @@
 
         
-        if (twisted_version.major, twisted_version.minor, twisted_version.micro) >= (15, 5, 0):
-    collect_ignore += _py_files('scrapy/xlib/tx')
+        
+def check_alphabetical(lines):
+    '''
+    checks if all entries per section are in alphabetical order based in entry title
+    '''
+    sections = {}
+    section_line_num = {}
+    for line_num, line in enumerate(lines):
+        if line.startswith(anchor):
+            category = line.split(anchor)[1].strip()
+            sections[category] = []
+            section_line_num[category] = line_num
+            continue
+        if not line.startswith('|') or line.startswith('|---'):
+            continue
+        title = [x.strip() for x in line.split('|')[1:-1]][0].upper()
+        sections[category].append(title)
     
-        def long_desc(self):
-        '''A long description of the command. Return short description when not
-        available. It cannot contain newlines, since contents will be formatted
-        by optparser which removes newlines and wraps text.
-        '''
-        return self.short_desc()
+    # This view is called from FlatpageFallbackMiddleware.process_response
+# when a 404 is raised, which often means CsrfViewMiddleware.process_view
+# has not been called even if CsrfViewMiddleware is installed. So we need
+# to use @csrf_protect, in case the template needs {% csrf_token %}.
+# However, we can't just wrap this view; if no matching flatpage exists,
+# or a redirect is required for authentication, the 404 needs to be returned
+# without any CSRF checks. Therefore, we only
+# CSRF protect the internal implementation.
     
-        @property
-    def templates_dir(self):
-        _templates_base_dir = self.settings['TEMPLATES_DIR'] or \
-            join(scrapy.__path__[0], 'templates')
-        return join(_templates_base_dir, 'spiders')
-
+                batch_outs = f(ins_batch)
+            if not isinstance(batch_outs, list):
+                batch_outs = [batch_outs]
+            if batch_index == 0:
+                # Pre-allocate the results arrays.
+                for batch_out in batch_outs:
+                    shape = (num_samples,) + batch_out.shape[1:]
+                    outs.append(np.zeros(shape, dtype=batch_out.dtype))
+            for i, batch_out in enumerate(batch_outs):
+                outs[i][batch_start:batch_end] = batch_out
+            if verbose == 1:
+                progbar.update(batch_end)
+        if len(outs) == 1:
+            return outs[0]
+        return outs
     
-            if not assertion:
-            if self.min_bound == self.max_bound:
-                expected = self.min_bound
-            else:
-                expected = '%s..%s' % (self.min_bound, self.max_bound)
-    
-        def test_str(self):
-        self.assertEqual('Invalid nonce ('xxx'): error', str(self.error))
+    print('Loading data...')
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
     
     
-print('\n# ======================================================================')
-print('#                   Calculating pi, 10000 iterations')
-print('# ======================================================================\n')
+@keras_test
+def test_unit_norm():
+    unit_norm_instance = constraints.unit_norm()
+    normalized = unit_norm_instance(K.variable(get_example_array()))
+    norm_of_normalized = np.sqrt(np.sum(K.eval(normalized) ** 2, axis=0))
+    # In the unit norm constraint, it should be equal to 1.
+    difference = norm_of_normalized - 1.
+    largest_difference = np.max(np.abs(difference))
+    assert(np.abs(largest_difference) < 10e-5)
     
-    The __init__ method has one required argument, a file-like object
-(including a chunk instance), and one optional argument, a flag which
-specifies whether or not chunks are aligned on 2-byte boundaries.  The
-default is 1, i.e. aligned.
+    
+def test_softmax_valid():
+    '''Test using a reference implementation of softmax.
+    '''
+    def softmax(values):
+        m = np.max(values)
+        e = np.exp(values - m)
+        return e / np.sum(e)
+    
+    Output after 4 epochs on CPU: ~0.8146
+Time per epoch on CPU (Core i7): ~150s.
 '''
     
-        def test_multiple_features(self):
-        with support.CleanImport('test.test_future5'):
-            from test import test_future5
     
-            self.assertEqual(format(-1.5-3.5e-20j, 'g'), '-1.5-3.5e-20j')
-        self.assertEqual(format(-1.5-3.5j, 'f'), '-1.500000-3.500000j')
-        self.assertEqual(format(-1.5-3.5j, 'F'), '-1.500000-3.500000j')
-        self.assertEqual(format(-1.5-3.5j, 'e'), '-1.500000e+00-3.500000e+00j')
-        self.assertEqual(format(-1.5-3.5j, '.2e'), '-1.50e+00-3.50e+00j')
-        self.assertEqual(format(-1.5-3.5j, '.2E'), '-1.50E+00-3.50E+00j')
-        self.assertEqual(format(-1.5e10-3.5e5j, '.2G'), '-1.5E+10-3.5E+05j')
+class ChatSocketHandler(tornado.websocket.WebSocketHandler):
+    waiters = set()
+    cache = []
+    cache_size = 200
+    
+        def test_404(self):
+        self.check_url('/404', expected_status=404)
+    
+        # Start workers, then wait for the work queue to be empty.
+    workers = gen.multi([worker() for _ in range(concurrency)])
+    await q.join(timeout=timedelta(seconds=300))
+    assert fetching == fetched
+    print('Done in %d seconds, fetched %s URLs.' % (
+        time.time() - start, len(fetched)))
+    
+    
+def run():
+    io_loop = IOLoop(make_current=True)
+    app = Application([('/', RootHandler)])
+    port = random.randrange(options.min_port, options.max_port)
+    app.listen(port, address='127.0.0.1')
+    signal.signal(signal.SIGCHLD, handle_sigchld)
+    args = ['ab']
+    args.extend(['-n', str(options.n)])
+    args.extend(['-c', str(options.c)])
+    if options.keepalive:
+        args.append('-k')
+    if options.quiet:
+        # just stops the progress messages printed to stderr
+        args.append('-q')
+    args.append('http://127.0.0.1:%d/' % port)
+    subprocess.Popen(args)
+    io_loop.start()
+    io_loop.close()
+    io_loop.clear_current()
+    
+    from tornado import gen
+from tornado.options import options, define, parse_command_line
+    
+    autodoc_member_order = 'bysource'
+autoclass_content = 'both'
+autodoc_inherit_docstrings = False
+    
+        args = parser.parse_args()
