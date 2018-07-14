51b59e3e060ec76b08aa17806e06150759d663bd
SUNsung <weatherius@gmail.com>
2018-07-14 21:19:01 +0300

    Rewrite [563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp] File in: 2018-07-14 21:19:00

--- cccccc/563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp
+++ cccccc/563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp
@@ -1,226 +1,68 @@
 
         
-          /// Resize the array to size elements.
-  /// New elements are initialized to null.
-  /// May only be called on nullValue or arrayValue.
-  /// \pre type() is arrayValue or nullValue
-  /// \post type() is arrayValue
-  void resize(ArrayIndex size);
+        int g_suppress_level = 0;
     
-     private:
-  const EnumDescriptor* descriptor_;
-    
-    
-    {
-    {
-    {}  // namespace csharp
-}  // namespace compiler
-}  // namespace protobuf
-    
-    #include <google/protobuf/compiler/csharp/csharp_doc_comment.h>
-#include <google/protobuf/compiler/csharp/csharp_helpers.h>
-#include <google/protobuf/compiler/csharp/csharp_repeated_enum_field.h>
-    
-    // TODO(jonskeet): Refactor repeated field support; all the implementations are *really* similar. We
-// should probably have a RepeatedFieldGeneratorBase.
-class RepeatedEnumFieldGenerator : public FieldGeneratorBase {
- public:
-  RepeatedEnumFieldGenerator(const FieldDescriptor* descriptor,
-                             int fieldOrdinal,
-                             const Options *options);
-  ~RepeatedEnumFieldGenerator();
+    namespace atom {
     }
     
-    #include <google/protobuf/compiler/code_generator.h>
-#include <google/protobuf/compiler/csharp/csharp_field_base.h>
     
-    #include <google/protobuf/compiler/java/java_doc_comment.h>
+    {  DraggableRegion();
+};
     
-      virtual void Generate(io::Printer* printer);
+      // Called by platform specific implementations of this class whenever a key
+  // is struck. Only called for keys that have an observer registered.
+  void NotifyKeyPressed(const ui::Accelerator& accelerator);
     
-    namespace google {
-namespace protobuf {
-namespace compiler {
-namespace java {
-    }
-    }
-    }
-    }
+    // Generate param traits write methods.
+#include 'ipc/param_traits_write_macros.h'
+namespace IPC {
+#include 'content/nw/src/common/common_message_generator.h'
+}  // namespace IPC
     
-      // Returns the distance map between the baseline image and the image in the
-  // last Compare() call (or the baseline image, if Compare() was not called
-  // yet).
-  // The dimensions of the distance map are the same as the baseline image.
-  // The interpretation of the distance values depend on the comparator used.
-  virtual const std::vector<float> distmap() const = 0;
-    
-    void OutputImageComponent::ToPixels(int xmin, int ymin, int xsize, int ysize,
-                                    uint8_t* out, int stride) const {
-  assert(xmin >= 0);
-  assert(ymin >= 0);
-  assert(xmin < width_);
-  assert(ymin < height_);
-  const int yend1 = ymin + ysize;
-  const int yend0 = std::min(yend1, height_);
-  int y = ymin;
-  for (; y < yend0; ++y) {
-    const int xend1 = xmin + xsize;
-    const int xend0 = std::min(xend1, width_);
-    int x = xmin;
-    int px = y * width_ + xmin;
-    for (; x < xend0; ++x, ++px, out += stride) {
-      *out = static_cast<uint8_t>((pixels_[px] + 8 - (x & 1)) >> 4);
-    }
-    const int offset = -stride;
-    for (; x < xend1; ++x) {
-      *out = out[offset];
-      out += stride;
-    }
-  }
-  for (; y < yend1; ++y) {
-    const int offset = -stride * xsize;
-    for (int x = 0; x < xsize; ++x) {
-      *out = out[offset];
-      out += stride;
+    void GetRenderProcessHosts(std::set<RenderProcessHost*>& rphs) {
+  RenderProcessHost* render_process_host = NULL;
+  std::vector<Shell*> windows = Shell::windows();
+  for (size_t i = 0; i < windows.size(); ++i) {
+    if (!windows[i]->is_devtools()) {
+      render_process_host = windows[i]->web_contents()->GetRenderProcessHost();
+      rphs.insert(render_process_host);
     }
   }
 }
     
-      // Map of areas where the image is red enough (blue in case of u channel).
-  std::vector<bool> redmap(image[0].size(), false);
-  for (int y = 0; y < h; y++) {
-    for (int x = 0; x < w; x++) {
-      size_t index = y * w + x;
-      float u = yuv[1][index];
-      float v = yuv[2][index];
-    }
-    }
+    #ifndef CONTENT_NW_SRC_API_APP_APP_H_
+#define CONTENT_NW_SRC_API_APP_APP_H_
     
-    #include <stddef.h>
-#include <stdint.h>
-    
-    inline int Log2Floor(uint32_t n) {
-  return n == 0 ? -1 : Log2FloorNonZero(n);
+    void Base::CallSync(const std::string& method,
+                    const base::ListValue& arguments,
+                    base::ListValue* result) {
+  NOTREACHED() << 'Uncatched callAsync in Base'
+               << ' method:' << method
+               << ' arguments:' << arguments;
 }
     
-    // Creates a JPEG from the rgb pixel data. Returns true on success. The given
-// quantization table must have 3 * kDCTBlockSize values.
-bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
-                     const int* quant, JPEGData* jpg);
+    // Call method of an object in browser and return the result.
+// function CallObjectMethod(id, type, method, args);
+v8::Handle<v8::Value> CallObjectMethodSync(int routing_id,
+                                           int object_id,
+                                           const std::string& type,
+                                           const std::string& method,
+                                           v8::Handle<v8::Value> args);
     
-    /**
- * Get current exceptions being handled.  front() is the most recent exception.
- * There should be at most one unless rethrowing.
- */
-std::vector<ExceptionInfo> getCurrentExceptions();
-    
-    // See portability/Unistd.h for why these need to be in a namespace
-// rather then extern 'C'.
-namespace folly {
-namespace portability {
-namespace fcntl {
-int creat(char const* fn, int pm);
-int fcntl(int fd, int cmd, ...);
-int posix_fallocate(int fd, off_t offset, off_t len);
-int open(char const* fn, int of, int pm = 0);
-}
-}
+    void Menu::Remove(MenuItem* menu_item, int pos) {
+  std::vector<MenuItem*>::iterator begin = menu_items.begin();
+  menu_items.erase(begin+pos);
+  gtk_container_remove(GTK_CONTAINER(menu_), menu_item->menu_item_);
 }
     
-      folly::Optional<T> try_take_for(std::chrono::milliseconds time) override {
-    T item;
-    while (!queue_.readIfNotEmpty(item)) {
-      if (!sem_.try_wait_for(time)) {
-        return folly::none;
-      }
+    ExtensionFunction::ResponseAction
+NwObjCallObjectMethodAsyncFunction::Run() {
+  EXTENSION_FUNCTION_VALIDATE(args_);
+  base::ListValue* arguments = nullptr;
+  int id = 0;
+  std::string type, method;
+  EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(0, &id));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(1, &type));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(2, &method));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetList(3, &arguments));
     }
-    return std::move(item);
-  }
-    
-    
-    {
-    {      argumentEvaluated = false;
-      XLOGC_IF(
-          DBG1, conds[i] || conds[j], 'testing conditional %d', getValue());
-      EXPECT_EQ((conds[i] || conds[j]) ? 1 : 0, messages.size());
-      messages.clear();
-      if (conds[i] || conds[j]) {
-        EXPECT_TRUE(argumentEvaluated);
-      } else {
-        EXPECT_FALSE(argumentEvaluated);
-      }
-    }
-  }
-    
-    MockClock::time_point MockClock::Now = MockClock::time_point{};
-    
-        /**
-     * Change action for file descriptor fd.
-     *
-     * 'action' may be another file descriptor number (dup2()ed before the
-     * child execs), or one of CLOSE, PIPE_IN, and PIPE_OUT.
-     *
-     * CLOSE: close the file descriptor in the child
-     * PIPE_IN: open a pipe *from* the child
-     * PIPE_OUT: open a pipe *to* the child
-     *
-     * PIPE is a shortcut; same as PIPE_IN for stdin (fd 0), same as
-     * PIPE_OUT for stdout (fd 1) or stderr (fd 2), and an error for
-     * other file descriptors.
-     */
-    Options& fd(int fd, int action);
-    
-    #include 'AuthResolver.h'
-    
-    void AbstractBtMessage::setBtMessageFactory(BtMessageFactory* factory)
-{
-  messageFactory_ = factory;
-}
-    
-      BtMessageDispatcher* dispatcher_;
-    
-    void AbstractHttpServerResponseCommand::updateReadWriteCheck()
-{
-  if (httpServer_->wantRead()) {
-    if (!readCheck_) {
-      readCheck_ = true;
-      e_->addSocketForReadCheck(socket_, this);
-    }
-  }
-  else if (readCheck_) {
-    readCheck_ = false;
-    e_->deleteSocketForReadCheck(socket_, this);
-  }
-  if (httpServer_->wantWrite()) {
-    if (!writeCheck_) {
-      writeCheck_ = true;
-      e_->addSocketForWriteCheck(socket_, this);
-    }
-  }
-  else if (writeCheck_) {
-    writeCheck_ = false;
-    e_->deleteSocketForWriteCheck(socket_, this);
-  }
-}
-    
-    bool AbstractProxyRequestCommand::executeInternal()
-{
-  // socket->setBlockingMode();
-  if (httpConnection_->sendBufferIsEmpty()) {
-    auto httpRequest = make_unique<HttpRequest>();
-    httpRequest->setUserAgent(getOption()->get(PREF_USER_AGENT));
-    httpRequest->setRequest(getRequest());
-    httpRequest->setProxyRequest(proxyRequest_);
-    }
-    }
-    
-    protected:
-  virtual bool executeInternal() CXX11_OVERRIDE;
-    
-    #endif // D_ANON_DISK_WRITER_FACTORY_H
-
-    
-    #endif // DOWNLOAD_EVENT_LISTENER_H
-
-    
-      virtual ~AppleTLSContext();
