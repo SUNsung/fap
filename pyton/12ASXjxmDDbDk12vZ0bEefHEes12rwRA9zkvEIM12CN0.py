4b0b60f11227fdbb9b98f618c3455fd734205dfa
SUNsung <weatherius@gmail.com>
2018-07-16 02:43:51 +0300

    Rewrite [12ASXjxmDDbDk12vZ0bEefHEes12rwRA9zkvEIM12CN0.py] File in: 2018-07-16 02:43:51

--- pyton/12ASXjxmDDbDk12vZ0bEefHEes12rwRA9zkvEIM12CN0.py
+++ pyton/12ASXjxmDDbDk12vZ0bEefHEes12rwRA9zkvEIM12CN0.py
@@ -1,145 +1,198 @@
 
         
-        cc_test(
-    name = 'disjoint_set_forest_test',
-    size = 'small',
-    srcs = ['disjoint_set_forest_test.cc'],
+        entry_template = textwrap.dedent('''
+    <entry>
+        <id>https://yt-dl.org/feed/youtube-dl-updates-feed/youtube-dl-@VERSION@</id>
+        <title>New version @VERSION@</title>
+        <link href='http://rg3.github.io/youtube-dl' />
+        <content type='xhtml'>
+            <div xmlns='http://www.w3.org/1999/xhtml'>
+                Downloads available at <a href='https://yt-dl.org/downloads/@VERSION@/'>https://yt-dl.org/downloads/@VERSION@/</a>
+            </div>
+        </content>
+        <author>
+            <name>The youtube-dl maintainers</name>
+        </author>
+        <updated>@TIMESTAMP@</updated>
+    </entry>
+    ''')
+    
+    import io
+import sys
+import re
+    
+    
+def format_size(bytes):
+    return '%s (%d bytes)' % (format_bytes(bytes), bytes)
+    
+    
+class CloserToTruthIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)?closertotruth\.com/(?:[^/]+/)*(?P<id>[^/?#&]+)'
+    _TESTS = [{
+        'url': 'http://closertotruth.com/series/solutions-the-mind-body-problem#video-3688',
+        'info_dict': {
+            'id': '0_zof1ktre',
+            'display_id': 'solutions-the-mind-body-problem',
+            'ext': 'mov',
+            'title': 'Solutions to the Mind-Body Problem?',
+            'upload_date': '20140221',
+            'timestamp': 1392956007,
+            'uploader_id': 'CTTXML'
+        },
+        'params': {
+            'skip_download': True,
+        },
+    }, {
+        'url': 'http://closertotruth.com/episodes/how-do-brains-work',
+        'info_dict': {
+            'id': '0_iuxai6g6',
+            'display_id': 'how-do-brains-work',
+            'ext': 'mov',
+            'title': 'How do Brains Work?',
+            'upload_date': '20140221',
+            'timestamp': 1392956024,
+            'uploader_id': 'CTTXML'
+        },
+        'params': {
+            'skip_download': True,
+        },
+    }, {
+        'url': 'http://closertotruth.com/interviews/1725',
+        'info_dict': {
+            'id': '1725',
+            'title': 'AyaFr-002',
+        },
+        'playlist_mincount': 2,
+    }]
+    
+    cc_library(
+    name = 'shape_helpers',
+    hdrs = ['ops/shape_helpers.h'],
     deps = [
-        ':disjoint_set_forest',
-        '//syntaxnet:base',
+        '//syntaxnet:shape_helpers',
+        '@org_tensorflow//tensorflow/core:framework_headers_lib',
+    ],
+)
+    
+    cc_test(
+    name = 'spanning_tree_iterator_test',
+    size = 'small',
+    srcs = ['spanning_tree_iterator_test.cc'],
+    deps = [
+        ':spanning_tree_iterator',
         '//syntaxnet:test_main',
+        '@org_tensorflow//tensorflow/core:lib',
         '@org_tensorflow//tensorflow/core:test',
     ],
 )
     
-    tf_proto_library_cc(
-    name = 'cell_trace_proto',
-    srcs = ['cell_trace.proto'],
-    protodeps = [':trace_proto'],
+    py_library(
+    name = 'biaffine_units',
+    srcs = ['biaffine_units.py'],
+    deps = [
+        ':digraph_ops',
+        ':network_units',
+        '//syntaxnet/util:check',
+        '@org_tensorflow//tensorflow:tensorflow_py',
+    ],
 )
     
-      def get_layer_size(self, unused_name):
-    return _TOKEN_DIM
+    from google.protobuf import text_format
+from dragnn.protos import spec_pb2
+from dragnn.python import dragnn_model_saver_lib as saver_lib
     
-          lengths = tf.constant([1, 2, 3], tf.int64)
-      max_length = 4
-      valid_arcs, valid_tokens = digraph_ops.ValidArcAndTokenMasks(
-          lengths, max_length)
-      self.assertAllEqual(valid_arcs.eval(),
-                          [[[1, 0, 0, 0],
-                            [0, 0, 0, 0],
-                            [0, 0, 0, 0],
-                            [0, 0, 0, 0]],
-                           [[1, 1, 0, 0],
-                            [1, 1, 0, 0],
-                            [0, 0, 0, 0],
-                            [0, 0, 0, 0]],
-                           [[1, 1, 1, 0],
-                            [1, 1, 1, 0],
-                            [1, 1, 1, 0],
-                            [0, 0, 0, 0]]])  # pyformat: disable
-      self.assertAllEqual(valid_tokens.eval(),
-                          [[1, 0, 0, 0],
-                           [1, 1, 0, 0],
-                           [1, 1, 1, 0]])  # pyformat: disable
     
-    flags.DEFINE_string('master_spec', None, 'Path to task context with '
-                    'inputs and parameters for feature extractors.')
-flags.DEFINE_string('params_path', None, 'Path to trained model parameters.')
-flags.DEFINE_string('export_path', '', 'Output path for exported servo model.')
-flags.DEFINE_bool('export_moving_averages', False,
-                  'Whether to export the moving average parameters.')
-flags.DEFINE_bool('build_runtime_graph', False,
-                  'Whether to build a graph for use by the runtime.')
-    
-          saved_model_builder.save()
+if __name__ == '__main__':
+  tf.test.main()
 
     
+          grads_and_vars = self.optimizer.compute_gradients(
+          cost, var_list=params_to_train)
+      clipped_gradients = [
+          (self._clip_gradients(g), v) for g, v in grads_and_vars
+      ]
+      gradient_norm = tf.global_norm(list(zip(*clipped_gradients))[0])
     
-class DiffTest(tf.test.TestCase):
-    
-          # As should AttachDataReader.
-      path = _find_input_path_to_type(path[-1], 'AttachDataReader')
-      self.assertNotEmpty(path)
-    
-      # Apply the Matrix-Tree theorem.
-  exp_normalized_laplacian_bxmxm = digraph_ops.LaplacianMatrix(
-      num_nodes, scores_bxmxm, forest=forest)
-  log_normalized_partition_function_b = tf.log(
-      tf.matrix_determinant(exp_normalized_laplacian_bxmxm))
-    
-        if s1.st_dev != s2.st_dev:
-        return True     # path/.. on a different device as path
-    if s1.st_ino == s2.st_ino:
-        return True     # path/.. is the same i-node as path, i.e. path=='/'
-    return False
-
-    
-        target.add_argument('--branch',
-                        help='branch name')
-    
-            # no location in URL
-        input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/targetHttpProxies/mytargetproxy/setUrlMap'
-        actual = GCPUtils.parse_gcp_url(input_url)
-        self.assertEquals('compute', actual['service'])
-        self.assertEquals('v1', actual['api_version'])
-        self.assertEquals('myproject', actual['project'])
-        self.assertFalse('global' in actual)
-        self.assertEquals('targetHttpProxies', actual['resource_name'])
-        self.assertEquals('mytargetproxy', actual['entity_name'])
-        self.assertEquals('setUrlMap', actual['method_name'])
-    
-        terminal_stderr_re = [
-        re.compile(br'\n\s*Invalid command:'),
-        re.compile(br'\nCommit failed'),
-        re.compile(br'\n\s+Set failed'),
-    ]
+      def testAttachDataReader(self):
+    '''Checks that train['run'] and 'annotations' call AttachDataReader.'''
+    test_name = 'attach-data-reader'
     
     
-class TerminalModule(TerminalBase):
+@pytest.mark.functional
+def test_without_confirmation(proc, TIMEOUT):
+    without_confirmation(proc, TIMEOUT)
     
-            # in some cases, we may have pre-read the data and then
-        # passed it to the load() call for YAML, in which case we
-        # want to override the default datasource (which would be
-        # '<string>') to the actual filename we read in
-        datasource = self._ansible_file_name or node.start_mark.name
+    URLS = ['http://www.google.com/',
+        'http://www.apple.com/',
+        'http://www.ibm.com',
+        'http://www.thisurlprobablydoesnotexist.com',
+        'http://www.slashdot.org/',
+        'http://www.python.org/',
+        'http://www.bing.com/',
+        'http://www.facebook.com/',
+        'http://www.yahoo.com/',
+        'http://www.youtube.com/',
+        'http://www.blogger.com/']
     
-        def test_max_delay_none(self):
-        strategy = _exponential_backoff(retries=7, delay=1, backoff=2, max_delay=None)
-        result = list(strategy())
-        self.assertEquals(result, [1, 2, 4, 8, 16, 32, 64])
+    # The suffix of source filenames.
+source_suffix = '.rst'
     
-            if 'latency' in request.args:
-            latency = float(request.args['latency'][0])
-            reactor.callLater(latency, self._finish, request)
-            return NOT_DONE_YET
+      if 'word' in vim_data:
+    completion_data[ 'insertion_text' ] = vim_data[ 'word' ]
+  if 'abbr' in vim_data:
+    completion_data[ 'menu_text' ] = vim_data[ 'abbr' ]
+  if 'menu' in vim_data:
+    completion_data[ 'extra_menu_info' ] = vim_data[ 'menu' ]
+  if 'kind' in vim_data:
+    completion_data[ 'kind' ] = [ vim_data[ 'kind' ] ]
+  if 'info' in vim_data:
+    completion_data[ 'detailed_info' ] = vim_data[ 'info' ]
     
-        def test_car_adapter_shall_make_very_loud_noise(self):
-        car = Car()
-        car_adapter = Adapter(car, make_noise=car.make_noise)
-        noise = car_adapter.make_noise(10)
-        expected_noise = 'vroom!!!!!!!!!!'
-    
-    
-class Subscriber:
-    
-    
-    
-    class TimeDisplay(object):
-    
-        def test_display_current_time_at_midnight(self):
-        '''
-        Would almost always fail (despite of right at/after midnight) if
-        untestable production code would have been used.
-        '''
-        time_provider_stub = MidnightTimeProvider()
-        class_under_test = TimeDisplay()
-        expected_time = '<span class=\'tinyBoldText\'>24:01</span>'
-        self.assertEqual(class_under_test.get_current_time_as_html_fragment(time_provider_stub), expected_time)
-    
-        def __init__(self):
-        self._tm = None
-        self._bProblem = 0
+    from ycm.tests.test_utils import MockVimModule
+MockVimModule()
     
     
-class Specification(object):
+def ParseArguments():
+  parser = argparse.ArgumentParser()
+  parser.add_argument( '--skip-build', action = 'store_true',
+                       help = 'Do not build ycmd before testing' )
+  parser.add_argument( '--coverage', action = 'store_true',
+                       help = 'Enable coverage report' )
+  parser.add_argument( '--no-flake8', action = 'store_true',
+                       help = 'Do not run flake8' )
+    
+    
+  def Empty_Insertion_Text_test( self ):
+    self._Check( 0, {
+      'insertion_text':  '',
+      'menu_text':       'MENU TEXT',
+      'extra_menu_info': 'EXTRA MENU INFO',
+      'kind':            'K',
+      'detailed_info':   'DETAILED INFO',
+      'extra_data': {
+        'doc_string':    'DOC STRING',
+      },
+    }, {
+      'word'     : '',
+      'abbr'     : 'MENU TEXT',
+      'menu'     : 'EXTRA MENU INFO',
+      'kind'     : 'k',
+      'info'     : 'DETAILED INFO\nDOC STRING',
+      'dup'      : 1,
+      'empty'    : 1,
+      'user_data': '0',
+    } )
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
