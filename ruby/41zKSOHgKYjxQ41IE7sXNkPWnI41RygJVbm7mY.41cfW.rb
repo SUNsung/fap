e73796f25dc93569700224beb16393ac08b160e2
SUNsung <weatherius@gmail.com>
2018-07-15 18:59:08 +0300

    Rewrite [41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb] File in: 2018-07-15 18:59:08

--- ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
+++ ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
@@ -1,201 +1,86 @@
 
         
-              it 'splits correctly' do
-        expected = ['One', 'Two', 'Three', 'Four Token']
-        expect(generator.split_keywords(keywords)).to eq(expected)
-      end
+            def self.git_log_between(pretty_format, from, to, merge_commit_filtering, date_format = nil, ancestry_path)
+      command = ['git log']
+      command << '--pretty=\'#{pretty_format}\''
+      command << '--date=\'#{date_format}\'' if date_format
+      command << '--ancestry-path' if ancestry_path
+      command << '#{from.shellescape}...#{to.shellescape}'
+      command << git_log_merge_commit_filtering_option(merge_commit_filtering)
+      Actions.sh(command.compact.join(' '), log: false).chomp
+    rescue
+      nil
     end
     
-          it 'prefers a custom version number over a boring version bump' do
-        Fastlane::FastFile.new.parse('lane :test do
-          increment_version_number(version_number: '1.77.3', bump_type: 'major')
-        end').runner.execute(:test)
+    describe Deliver::HtmlGenerator do
+  let(:generator) { Deliver::HtmlGenerator.new }
     
-        if registration
-      u2f.authenticate!(challenges, response, Base64.decode64(registration.public_key), registration.counter)
-      registration.update(counter: response.counter)
-      true
-    end
-  rescue JSON::ParserError, NoMethodError, ArgumentError, U2F::Error
-    false
-  end
-end
-
-    
-      # Check if there is no signed in user before doing the sign out.
-  #
-  # If there is no signed in user, it will set the flash message and redirect
-  # to the after_sign_out path.
-  def verify_signed_out_user
-    if all_signed_out?
-      set_flash_message! :notice, :already_signed_out
-    
-    if defined?(ActionMailer)
-  class Devise::Mailer < Devise.parent_mailer.constantize
-    include Devise::Mailers::Helpers
-    
-          def remember_me_is_active?(resource)
-        return false unless resource.respond_to?(:remember_me)
-        scope = Devise::Mapping.find_scope!(resource)
-        _, token, generated_at = cookies.signed[remember_key(resource, scope)]
-        resource.remember_me?(token, generated_at)
-      end
-    
-          private
-    
-        # Returns a deep copy of this query list and all its children.
-    #
-    # @return [QueryList]
-    def deep_copy
-      QueryList.new(queries.map {|q| q.deep_copy})
-    end
-  end
-    
-        def self.run
-      # Apparently there's no better way than this to add Sass
-      # to Merb's Rack stack.
-      Merb::Config[:app] = Sass::Plugin::Rack.new(Merb::Config[:app])
-    end
-  end
-end
-
-    
-        # @see Node#deep_copy
-    def deep_copy
-      node = dup
-      node.instance_variable_set('@before', @before.deep_copy) if @before
-      node.instance_variable_set('@mid', @mid.deep_copy)
-      node.instance_variable_set('@after', @after.deep_copy) if @after
-      node
-    end
-    
-            def on_array(node)
-          process(node, '%i', '%I', '%w', '%W')
-        end
-    
-            private
-    
-                      def do_something
-                  end
-    
-              expect(new_source)
-            .to eq('#{prefix}#{open}#{a}, # a\n#{b},#{close} # b\n#{suffix}')
-        end
-      end
-    end
-  end
-end
-
-    
-          # The body of the method definition.
-      #
-      # @note this can be either a `begin` node, if the method body contains
-      #       multiple expressions, or any other node, if it contains a single
-      #       expression.
-      #
-      # @return [Node] the body of the method definition
-      def body
-        node_parts[0]
-      end
-    
-        if as == :json
-      if api_error?(data)
-        data = generate_error_hash(data)
-      else
-        selected_fields = extract_fields(filter.to_s.strip)
-        data.select! { |k,v| selected_fields.include?(k) } unless selected_fields.empty?
-        unless options.include?(:exclude_default_metadata)
-          data = data.to_hash
-          if data.values.size == 0 && selected_fields.size > 0
-            raise LogStash::Api::NotFoundError
+                # Remove the signal trap if no more registered callbacks exist
+            Signal.trap('INT', 'DEFAULT') if registered.empty?
           end
-          data = default_metadata.merge(data)
         end
+    
+          def [](key)
+        super(convert_key(key))
       end
     
-        i0, s0 = index, []
-    if has_terminal?('-', false, index)
-      r2 = instantiate_node(SyntaxNode,input, index...(index + 1))
-      @index += 1
-    else
-      terminal_parse_failure('-')
-      r2 = nil
-    end
-    if r2
-      r1 = r2
-    else
-      r1 = instantiate_node(SyntaxNode,input, index...index)
-    end
-    s0 << r1
-    if r1
-      s3, i3 = [], index
-      loop do
-        if has_terminal?('\G[0-9]', true, index)
-          r4 = true
-          @index += 1
-        else
-          r4 = nil
+            # If we're just checking, then just return exit codes
+        if options[:check]
+          return 0 if cap_host.capability?(name)
+          return 1
         end
-        if r4
-          s3 << r4
-        else
-          break
-        end
-      end
-      if s3.empty?
-        @index = i3
-        r3 = nil
-      else
-        r3 = instantiate_node(SyntaxNode,input, i3...index, s3)
-      end
-      s0 << r3
-      if r3
-        i6, s6 = index, []
-        if has_terminal?('.', false, index)
-          r7 = instantiate_node(SyntaxNode,input, index...(index + 1))
-          @index += 1
-        else
-          terminal_parse_failure('.')
-          r7 = nil
-        end
-        s6 << r7
-        if r7
-          s8, i8 = [], index
-          loop do
-            if has_terminal?('\G[0-9]', true, index)
-              r9 = true
-              @index += 1
-            else
-              r9 = nil
-            end
-            if r9
-              s8 << r9
-            else
-              break
-            end
-          end
-          r8 = instantiate_node(SyntaxNode,input, i8...index, s8)
-          s6 << r8
-        end
-        if s6.last
-          r6 = instantiate_node(SyntaxNode,input, i6...index, s6)
-          r6.extend(Number0)
-        else
-          @index = i6
-          r6 = nil
-        end
-        if r6
-          r5 = r6
-        else
-          r5 = instantiate_node(SyntaxNode,input, index...index)
-        end
-        s0 << r5
+    
+      def initialize(repo: 'twbs/bootstrap', branch: 'master', save_to: {}, cache_path: 'tmp/converter-cache-bootstrap')
+    @logger     = Logger.new
+    @repo       = repo
+    @branch     = branch || 'master'
+    @branch_sha = get_branch_sha
+    @cache_path = cache_path
+    @repo_url   = 'https://github.com/#@repo'
+    @save_to    = {
+        js:    'assets/javascripts/bootstrap',
+        scss:  'assets/stylesheets/bootstrap',
+        fonts: 'assets/fonts/bootstrap'}.merge(save_to)
+  end
+    
+      # Specifies the header that your server uses for sending files.
+  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+    
+      # The test environment is used exclusively to run your application's
+  # test suite. You never need to work with it otherwise. Remember that
+  # your test database is 'scratch space' for the test suite and is wiped
+  # and recreated between test runs. Don't rely on the data there!
+  config.cache_classes = true
+    
+      desc 'update main and version in bower.json'
+  task :generate do
+    require 'bootstrap-sass'
+    Dir.chdir Bootstrap.gem_path do
+      spec       = JSON.parse(File.read 'bower.json')
+    
+      trap('INT') {
+    [jekyllPid, compassPid, rackupPid].each { |pid| Process.kill(9, pid) rescue Errno::ESRCH }
+    exit 0
+  }
+    
+      if options.respond_to? 'keys'
+    options.each do |k,v|
+      unless v.nil?
+        v = v.join ',' if v.respond_to? 'join'
+        v = v.to_json if v.respond_to? 'keys'
+        output += ' data-#{k.sub'_','-'}='#{v}''
       end
     end
-    if s0.last
-      r0 = instantiate_node(LogStash::Config::AST::Number,input, i0...index, s0)
-      r0.extend(Number1)
-    else
-      @index = i0
-      r0 = nil
-    end
+  elsif options.respond_to? 'join'
+    output += ' data-value='#{config[key].join(',')}''
+  else
+    output += ' data-value='#{config[key]}''
+  end
+  output += '></#{tag}>'
+end
+    
+      class ImageTag < Liquid::Tag
+    @img = nil
+    
+    
