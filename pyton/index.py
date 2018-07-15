9a73e10c95e379c66ea07e81bd9292cc89a67ba9
SUNsung <weatherius@gmail.com>
2018-07-15 07:16:39 +0300

    Rewrite [index.py] File in: 2018-07-15 07:16:38

--- pyton/index.py
+++ pyton/index.py
@@ -1,170 +1,42 @@
 
         
         
-def test_unicode_url(httpbin):
-    r = http(httpbin.url + u'/get?test=' + UNICODE)
-    assert HTTP_OK in r
-    assert r.json['args'] == {'test': UNICODE}
+class HitCounts(MRJob):
     
-    
-def trim_filename(filename, max_len):
-    if len(filename) > max_len:
-        trim_by = len(filename) - max_len
-        name, ext = os.path.splitext(filename)
-        if trim_by >= len(name):
-            filename = filename[:-trim_by]
-        else:
-            filename = name[:-trim_by] + ext
-    return filename
-    
-    import requests.auth
-    
-        >>> humanize_bytes(1)
-    '1 B'
-    >>> humanize_bytes(1024, precision=1)
-    '1.0 kB'
-    >>> humanize_bytes(1024 * 123, precision=1)
-    '123.0 kB'
-    >>> humanize_bytes(1024 * 12342, precision=1)
-    '12.1 MB'
-    >>> humanize_bytes(1024 * 12342, precision=2)
-    '12.05 MB'
-    >>> humanize_bytes(1024 * 1234, precision=2)
-    '1.21 MB'
-    >>> humanize_bytes(1024 * 1234 * 1111, precision=2)
-    '1.31 GB'
-    >>> humanize_bytes(1024 * 1234 * 1111, precision=1)
-    '1.3 GB'
-    
-        if is_pypy and is_py27:
-        # Manually set keys when initialized with an iterable as PyPy
-        # doesn't call __setitem__ in such case (pypy3 does).
-        def __init__(self, *args, **kwargs):
-            if len(args) == 1 and isinstance(args[0], Iterable):
-                super(RequestItemsDict, self).__init__(**kwargs)
-                for k, v in args[0]:
-                    self[k] = v
-            else:
-                super(RequestItemsDict, self).__init__(*args, **kwargs)
-    
-            credentials = {'username': None, 'password': None}
-        try:
-            # New style
-            plugin.raw_auth = auth['raw_auth']
-        except KeyError:
-            # Old style
-            credentials = {
-                'username': auth['username'],
-                'password': auth['password'],
-            }
-        else:
-            if plugin.auth_parse:
-                from httpie.input import parse_auth
-                parsed = parse_auth(plugin.raw_auth)
-                credentials = {
-                    'username': parsed.key,
-                    'password': parsed.value,
-                }
-    
-    
-class TestLookupDict:
-    
-            if proxy:
-            proxy = prepend_scheme_if_needed(proxy, 'http')
-            proxy_url = parse_url(proxy)
-            if not proxy_url.host:
-                raise InvalidProxyURL('Please check proxy URL. It is malformed'
-                                      ' and could be missing the host.')
-            proxy_manager = self.proxy_manager_for(proxy)
-            conn = proxy_manager.connection_from_url(url)
-        else:
-            # Only scheme should be lower case
-            parsed = urlparse(url)
-            url = parsed.geturl()
-            conn = self.poolmanager.connection_from_url(url)
-    
-        @pytest.mark.parametrize(
-        'url', (
-            'http://192.168.0.1:5000/',
-            'http://192.168.0.1/',
-            'http://172.16.1.1/',
-            'http://172.16.1.1:5000/',
-            'http://localhost.localdomain:5000/v1.0/',
-        ))
-    def test_not_bypass_no_proxy_keyword(self, url, monkeypatch):
-        # This is testing that the 'no_proxy' argument overrides the
-        # environment variable 'no_proxy'
-        monkeypatch.setenv('http_proxy', 'http://proxy.example.com:3128/')
-        no_proxy = '192.168.1.1,requests.com'
-        assert get_environ_proxies(url, no_proxy=no_proxy) != {}
-    
-        if implementation == 'CPython':
-        implementation_version = platform.python_version()
-    elif implementation == 'PyPy':
-        implementation_version = '%s.%s.%s' % (sys.pypy_version_info.major,
-                                               sys.pypy_version_info.minor,
-                                               sys.pypy_version_info.micro)
-        if sys.pypy_version_info.releaselevel != 'final':
-            implementation_version = ''.join([
-                implementation_version, sys.pypy_version_info.releaselevel
-            ])
-    elif implementation == 'Jython':
-        implementation_version = platform.python_version()  # Complete Guess
-    elif implementation == 'IronPython':
-        implementation_version = platform.python_version()  # Complete Guess
-    else:
-        implementation_version = 'Unknown'
-    
-    This module handles import compatibility issues between Python 2 and
-Python 3.
-'''
-    
-        def _find(self, name, domain=None, path=None):
-        '''Requests uses this method internally to get cookie values.
-    
-            *Note: Should not normally need to be called explicitly.*
+            (foo, p1), 2
+        (bar, p1), 3
+        (foo, p2), 3
+        (bar, p3), 10
+        (foo, p4), 1
         '''
-        if not self._content_consumed:
-            self.raw.close()
+        yield key, sum(values)
     
-        def getChild(self, request, name):
-        return self
     
-                request.callback = wrapper
-    
-            assert len(self.args) in [1, 2, 3]
-        self.obj_name = self.args[0] or None
-        self.obj_type = self.objects[self.obj_name]
-    
-        def process_request(self, request, spider):
-        if self._timeout:
-            request.meta.setdefault('download_timeout', self._timeout)
+if __name__ == '__main__':
+    SpendingByCategory.run()
 
     
-    class ContractFail(AssertionError):
-    '''Error raised in case of a failing contract'''
-    pass
-
+            video_id = match1(html, r'data-brightcove-id='(\d+)'')
+        
+        assert account_number, video_id
     
-        def spider_opened(self, spider):
-        self.task = reactor.callLater(self.close_on['timeout'], \
-            self.crawler.engine.close_spider, spider, \
-            reason='closespider_timeout')
+    from .theplatform import theplatform_download_by_pid
     
+        video_url = match1(html, r'filepath=(.+)&sec')
+    video_url = video_url.replace('&mid', '?mid')
     
-    
-                    else:
-                    self.c = self.input.LA(1)
-    
-    
-##     def specialTransition(self, state, symbol):
-##         return 0
-    
-    def shorten_title(title):
-    m1 = re.search('[[0-9]*]', title)
-    m2 = re.search(''.*'', title)
-    if m1:
-        title = m1.group(0)
-    if m2:
-        title = ' '.join((title, m2.group(0)))   
-    return title[:50] + ' [...]'    
+    #----------------------------------------------------------------------
+def showroom_get_roomid_by_room_url_key(room_url_key):
+    '''str->str'''
+    fake_headers_mobile = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'UTF-8,*;q=0.5',
+        'Accept-Encoding': 'gzip,deflate,sdch',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'
+    }
+    webpage_url = 'https://www.showroom-live.com/' + room_url_key
+    html = get_content(webpage_url, headers = fake_headers_mobile)
+    roomid = match1(html, r'room\?room_id\=(\d+)')
+    assert roomid
+    return roomid
