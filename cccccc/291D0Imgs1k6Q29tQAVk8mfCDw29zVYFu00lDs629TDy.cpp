91797c6aa419a63567ab56e5b27946b9c39cbd35
SUNsung <weatherius@gmail.com>
2018-07-15 04:31:26 +0300

    Rewrite [291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp] File in: 2018-07-15 04:31:25

--- cccccc/291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp
+++ cccccc/291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp
@@ -1,87 +1,53 @@
-	// Path
-	p_len = host.find('/');
-	if (p_len != -1) {
-		path = host.substr(p_len, host.length() - p_len);
-		host = host.substr(0, p_len);
-	}
-    
-    #if defined(MBEDTLS_PLATFORM_SNPRINTF_ALT) && !defined(MBEDTLS_PLATFORM_C)
-#error 'MBEDTLS_PLATFORM_SNPRINTF_ALT defined, but not all prerequisites'
-#endif
-    
-    	void set_damping_compression(float p_value);
-	float get_damping_compression() const;
-    
-    	GodotClosestConvexResultCallback(const btVector3 &convexFromWorld, const btVector3 &convexToWorld, const Set<RID> *p_exclude) :
-			btCollisionWorld::ClosestConvexResultCallback(convexFromWorld, convexToWorld),
-			m_exclude(p_exclude) {}
-    
-      Copyright 2003 by
-  Francesco Zappa Nardelli
-    
-    
-int             oc_has_mode_metrics;
-oc_mode_metrics OC_MODE_METRICS[64][3][2][OC_SAD_BINS];
-# endif
-    
-    #define __USE_ISOC9X 1
-#define __USE_ISOC99 1
-    
-    
-    {
-    {      // Parameters tuned to avoid sharpening in too bright areas, where the
-      // effect makes it worse instead of better.
-      if (channel == 2 && g < 0.85 && b < 0.85 && r < 0.9) {
-        darkmap[index] = true;
-      }
-      if (channel == 1 && r < 0.85 && g < 0.85 && b < 0.9) {
-        darkmap[index] = true;
-      }
-    }
-  }
-    
-    void DCT1d(const double* in, int stride, double* out) {
-  for (int x = 0; x < 8; ++x) {
-    out[x * stride] = 0.0;
-    for (int u = 0; u < 8; ++u) {
-      out[x * stride] += kDCTMatrix[8 * x + u] * in[u * stride];
-    }
-  }
-}
-    
-    // Sort the root nodes, least popular first.
-static inline bool SortHuffmanTree(const HuffmanTree& v0,
-                                   const HuffmanTree& v1) {
-  if (v0.total_count_ != v1.total_count_) {
-    return v0.total_count_ < v1.total_count_;
-  }
-  return v0.index_right_or_value_ > v1.index_right_or_value_;
-}
-    
-    // Computes the DCT (Discrete Cosine Transform) of the 8x8 array in 'block',
-// scaled up by a factor of 16. The values in 'block' are laid out row-by-row
-// and the result is written to the same memory area.
-void ComputeBlockDCT(coeff_t* block);
-    
-    #include 'guetzli/gamma_correct.h'
-    
-    namespace guetzli {
-    }
-    
-      int iquant[3 * kDCTBlockSize];
-  int idx = 0;
-  for (int i = 0; i < 3; ++i) {
-    for (int j = 0; j < kDCTBlockSize; ++j) {
-      int v = quant[idx];
-      jpg->quant[i].values[j] = v;
-      iquant[idx++] = ((1 << kIQuantBits) + 1) / v;
-    }
-  }
-    
-    #include 'guetzli/jpeg_data.h'
-    
-    #endif  // GUETZLI_JPEG_DATA_READER_H_
 
+        
+        // Relaunches the application using the helper application associated with the
+// currently running instance of Chrome in the parent browser process as the
+// executable for the relauncher process. |args| is an argv-style vector of
+// command line arguments of the form normally passed to execv. args[0] is
+// also the path to the relaunched process. Because the relauncher process
+// will ultimately launch the relaunched process via Launch Services, args[0]
+// may be either a pathname to an executable file or a pathname to an .app
+// bundle directory. The caller should exit soon after RelaunchApp returns
+// successfully. Returns true on success, although some failures can occur
+// after this function returns true if, for example, they occur within the
+// relauncher process. Returns false when the relaunch definitely failed.
+bool RelaunchApp(const StringVector& argv);
     
-    #ifndef GUETZLI_JPEG_HUFFMAN_DECODE_H_
-#define GUETZLI_JPEG_HUFFMAN_DECODE_H_
+    #include <memory>
+    
+    // filenames
+extern const base::FilePath::CharType kCacheDirname[];
+extern const base::FilePath::CharType kChannelIDFilename[];
+extern const base::FilePath::CharType kCookieFilename[];
+extern const base::FilePath::CharType kCRLSetFilename[];
+extern const base::FilePath::CharType kCustomDictionaryFileName[];
+extern const base::FilePath::CharType kExtensionActivityLogFilename[];
+extern const base::FilePath::CharType kExtensionsCookieFilename[];
+extern const base::FilePath::CharType kFirstRunSentinel[];
+extern const base::FilePath::CharType kGCMStoreDirname[];
+extern const base::FilePath::CharType kLocalStateFilename[];
+extern const base::FilePath::CharType kLocalStorePoolName[];
+extern const base::FilePath::CharType kMediaCacheDirname[];
+extern const base::FilePath::CharType kNetworkPersistentStateFilename[];
+extern const base::FilePath::CharType kOfflinePageArchviesDirname[];
+extern const base::FilePath::CharType kOfflinePageMetadataDirname[];
+extern const base::FilePath::CharType kPreferencesFilename[];
+extern const base::FilePath::CharType kProtectedPreferencesFilenameDeprecated[];
+extern const base::FilePath::CharType kReadmeFilename[];
+extern const base::FilePath::CharType kResetPromptMementoFilename[];
+extern const base::FilePath::CharType kSafeBrowsingBaseFilename[];
+extern const base::FilePath::CharType kSecurePreferencesFilename[];
+extern const base::FilePath::CharType kServiceStateFileName[];
+extern const base::FilePath::CharType kSingletonCookieFilename[];
+extern const base::FilePath::CharType kSingletonLockFilename[];
+extern const base::FilePath::CharType kSingletonSocketFilename[];
+extern const base::FilePath::CharType kSupervisedUserSettingsFilename[];
+extern const base::FilePath::CharType kThemePackFilename[];
+extern const base::FilePath::CharType kThemePackMaterialDesignFilename[];
+extern const base::FilePath::CharType kWebAppDirname[];
+    
+    #include 'rocksdb/env.h'
+#include 'util/testharness.h'
+    
+     private:
+  char delim_;         // The delimiter is inserted between elements
