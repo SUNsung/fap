aca0d56c3fc55cc50bcd43e7379844f821e5a0c7
SUNsung <weatherius@gmail.com>
2018-07-16 09:20:23 +0300

    Rewrite [28ANJLv.zj2ZU28waX0cv.x3lw28PO4Q1hMcdmo28YcZ.cpp] File in: 2018-07-16 09:20:22

--- cccccc/28ANJLv.zj2ZU28waX0cv.x3lw28PO4Q1hMcdmo28YcZ.cpp
+++ cccccc/28ANJLv.zj2ZU28waX0cv.x3lw28PO4Q1hMcdmo28YcZ.cpp
@@ -1,234 +1,216 @@
 
         
-        PyObject* NewMessageExtensionsByName(const Descriptor* descriptor);
-PyObject* NewMessageExtensionsSeq(const Descriptor* descriptor);
-    
-    
-    
-    void RepeatedEnumFieldGenerator::GenerateSerializationCode(io::Printer* printer) {
-  printer->Print(
-    variables_,
-    '$name$_.WriteTo(output, _repeated_$name$_codec);\n');
-}
-    
-      virtual void GenerateCloningCode(io::Printer* printer);
-  virtual void GenerateFreezingCode(io::Printer* printer);
-  virtual void GenerateMembers(io::Printer* printer);
-  virtual void GenerateMergingCode(io::Printer* printer);
-  virtual void GenerateParsingCode(io::Printer* printer);
-  virtual void GenerateSerializationCode(io::Printer* printer);
-  virtual void GenerateSerializedSizeCode(io::Printer* printer);
-    
-    void freeifaddrs(struct ifaddrs* addrs) {
-	struct ifaddrs* last = NULL;
-	struct ifaddrs* cursor = addrs;
-	while (cursor) {
-		delete[] cursor->ifa_name;
-		delete cursor->ifa_addr;
-		delete cursor->ifa_netmask;
-		last = cursor;
-		cursor = cursor->ifa_next;
-		delete last;
-	}
-}
+        
+    {} // namespace nwapi
 
     
-    #include 'io/stream_peer.h'
-    
-    	void _on_peer_packet();
-	void _on_connect(String p_protocol);
-	void _on_disconnect();
-	void _on_error();
-    
-    void VideoPlayer::set_expand(bool p_expand) {
-    }
-    
-    #endif /* PCFREAD_H_ */
-    
-    
-    {    // The read() method is called when the internal input buffer is empty.
-    // Parameters:
-    // pBuf - input buffer
-    // max_bytes_to_read - maximum bytes that can be written to pBuf
-    // pEOF_flag - set this to true if at end of stream (no more bytes remaining)
-    // Returns -1 on error, otherwise return the number of bytes actually written to the buffer (which may be 0).
-    // Notes: This method will be called in a loop until you set *pEOF_flag to true or the internal buffer is full.
-    virtual int read(uint8 *pBuf, int max_bytes_to_read, bool *pEOF_flag) = 0;
-  };
-    
-    // WorkloadStats is used to track per request timing for different states
-// of the VM.  At the entrypoint to a change of vm state a WorkloadStats object
-// should be made to guard the state change with appropriate timers and
-// counters.
-//
-// The states tracked are:
-//  - In a request (this is a superset of the interpreter state)
-//  - In the interpreter through Dispatch, or DispatchBB (interpOne disregarded)
-//  - In the JIT (currently tracks time inside the translate routine)
-//
-// Note the time in the TC is not tracked.  This is roughly:
-//   Time in request - Time in interp
-//
-// This gives us the relative interp time formula of:
-//   Relative interp time = Time in interp / Time in request
-struct WorkloadStats final {
-  enum State {
-    InRequest,
-    // -> InInterp   Okay (entering Dispatch loop)
-    // -> InTrans    Okay (entering translate)
-    InInterp,
-    // -> InRequest  Okay (leaving the dispatch loop)
-    // -> InTrans    Okay (entering translate)
-    InTrans,
-    // -> InRequest  Okay (leaving translate)
-    // -> InInterp   Okay (leaving translate)
-  };
-    }
-    
-    //////////////////////////////////////////////////////////////////////
-    
-    
-    {  CurlShareResource();
-  ~CurlShareResource() { close(); }
-  void close();
-  bool setOption(int option, const Variant& value);
-  static bool isLongOption(long option);
-  bool setLongOption(long option, long value);
-  CURLcode attachToCurlHandle(CURL *cp);
- private:
-  CURLSH* m_share;
-  // CURLSH is a typedef to void
-  TYPE_SCAN_IGNORE_FIELD(m_share);
-};
-    
-    void SynchronizableMulti::notifyAll() {
-  for (auto& cond_list : m_cond_list_vec) {
-    while (!cond_list.empty()) {
-      pthread_cond_signal(cond_list.front());
-      cond_list.pop_front();
-    }
-  }
+    MenuDelegate::MenuDelegate(ObjectManager* object_manager)
+    : object_manager_(object_manager) {
 }
     
-    inline void* mmap_1g_impl(void* addr) {
-  void* ret = MAP_FAILED;
-  if (s_hugePath[0] != 0) {
-    int fd = -1;
-    size_t dirNameLen = strlen(s_hugePath);
-    assert(dirNameLen > 0 && s_hugePath[dirNameLen - 1] == '/');
-    for (char i = '0'; i <= '9'; ++i) {
-      s_hugePath[dirNameLen] = i;
-      // We don't put code on 1G huge pages, so no execute permission.
-      fd = open(s_hugePath, O_CREAT | O_EXCL | O_RDWR, 0666);
-      // Retry a few times if the file already exists.
-      if (fd < 0) {
-        if (errno == EEXIST) {
-          errno = 0;
-          continue;
-        } else {
-          snprintf(s_errorMsg, maxErrorMsgLen,
-                   'Failed to create hugetlbfs file %s: ', s_hugePath);
-          record_err_msg();
-          s_hugePath[dirNameLen] = 0;
-          return nullptr;
-        }
-      } else {
-        unlink(s_hugePath);
-      }
+      if (menu_item->submenu_)
+    menu_model_->InsertSubMenuAt(pos, menu_item->id(), menu_item->label_,
+                                 menu_item->submenu_->menu_model_.get());
+  else if (menu_item->type_ == 'normal')
+    menu_model_->InsertItemAt(pos, menu_item->id(), menu_item->label_);
+  else if (menu_item->type_ == 'checkbox')
+    menu_model_->InsertCheckItemAt(pos, menu_item->id(), menu_item->label_);
+  else if (menu_item->type_ == 'separator')
+    menu_model_->InsertSeparatorAt(pos, ui::NORMAL_SEPARATOR);
+    
+    namespace extensions {
+    }
+    
+        // pass address (value interface)
+    iterator find(const K& key)                     { return m.find(&key); }
+    const_iterator find(const K& key) const         { return m.find(&key); }
+    iterator lower_bound(const K& key)              { return m.lower_bound(&key); }
+    const_iterator lower_bound(const K& key) const  { return m.lower_bound(&key); }
+    size_type erase(const K& key)                   { return m.erase(&key); }
+    size_type count(const K& key) const             { return m.count(&key); }
+    
+      // Read until size drops significantly.
+  std::string limit_key = Key(n);
+  for (int read = 0; true; read++) {
+    ASSERT_LT(read, 100) << 'Taking too long to compact';
+    Iterator* iter = db_->NewIterator(ReadOptions());
+    for (iter->SeekToFirst();
+         iter->Valid() && iter->key().ToString() < limit_key;
+         iter->Next()) {
+      // Drop data
+    }
+    delete iter;
+    // Wait a little bit to allow any triggered compactions to complete.
+    Env::Default()->SleepForMicroseconds(1000000);
+    uint64_t size = Size(Key(0), Key(n));
+    fprintf(stderr, 'iter %3d => %7.3f MB [other %7.3f MB]\n',
+            read+1, size/1048576.0, Size(Key(n), Key(kCount))/1048576.0);
+    if (size <= initial_size/10) {
       break;
     }
-    }
-    }
-    
-    // Call mprotect() on all 1G huge pages.  Useful in limiting access in child
-// processes.
-int mprotect_1g_pages(int prot);
-    
-    #include 'hphp/runtime/vm/jit/annotation.h'
-    
-    #include <map>
-#include <functional>
-    
-    TEST_F(ViewsConfigParserPluginTests, test_add_view) {
-  Config c;
-  auto s = c.update(getTestConfigMap());
-  EXPECT_TRUE(s.ok());
-    }
-    
-      char* buffer = (char*)malloc(range.length + 1);
-  if (buffer == nullptr) {
-    return '';
   }
-  memset(buffer, 0, range.length + 1);
     
-    
-    {
-    { protected:
-  void testPluginCheck();
-  void testReset();
-  void testPut();
-  void testGet();
-  void testDelete();
-  void testDeleteRange();
-  void testScan();
-  void testScanLimit();
-};
+    void leveldb_iter_get_error(const leveldb_iterator_t* iter, char** errptr) {
+  SaveError(errptr, iter->rep->status());
 }
+    
+    TableCache::TableCache(const std::string& dbname,
+                       const Options* options,
+                       int entries)
+    : env_(options->env),
+      dbname_(dbname),
+      options_(options),
+      cache_(NewLRUCache(entries)) {
+}
+    
+    
+    {}  // namespace leveldb
+    
+        // Initialize db_
+    db_ = new kyotocabinet::TreeDB();
+    char file_name[100];
+    db_num_++;
+    std::string test_dir;
+    Env::Default()->GetTestDirectory(&test_dir);
+    snprintf(file_name, sizeof(file_name),
+             '%s/dbbench_polyDB-%d.kct',
+             test_dir.c_str(),
+             db_num_);
+    
+      // Three-way comparison.  Returns value:
+  //   <  0 iff '*this' <  'b',
+  //   == 0 iff '*this' == 'b',
+  //   >  0 iff '*this' >  'b'
+  int compare(const Slice& b) const;
+    
+    /*
+ * A Vunit contains all the assets that make up a vasm compilation unit.  It is
+ * responsible for allocating new blocks, Vregs, and tuples.
+ */
+struct Vunit {
+  /*
+   * Create a new block in the given area and weight, returning its id.
+   */
+  Vlabel makeBlock(AreaIndex area, uint64_t weight);
+    }
+    
+    namespace HPHP {
+    }
+    
+    #ifdef PHP_WIN32
+# undef S_IFLNK
+# undef S_IFIFO
+#endif
+    
+    ALWAYS_INLINE
+APCObject::APCObject(ClassOrName cls, uint32_t propCount)
+  : m_handle(APCKind::SharedObject)
+  , m_cls{cls}
+  , m_propCount{propCount}
+  , m_persistent{0}
+  , m_no_wakeup{0}
+  , m_fast_init{0}
+{}
+    
+      // make an empty string with cap reserve bytes, plus 1 for '\0'
+  String(size_t cap, ReserveStringMode /*mode*/)
+      : m_str(StringData::Make(cap), NoIncRef{}) {}
+    
+      struct ObjectInfo {
+    String objClass;
+    int    objId;
+    char   objCode;
+    String rsrcName;
+    int    rsrcId;
+  };
+  req::vector<ObjectInfo> m_objectInfos;
+    
+    namespace guetzli {
+    }
+    
+    #ifndef GUETZLI_COLOR_TRANSFORM_H_
+#define GUETZLI_COLOR_TRANSFORM_H_
+    
+    
+    {}  // namespace guetzli
+    
+    inline int Log2Floor(uint32_t n) {
+  return n == 0 ? -1 : Log2FloorNonZero(n);
+}
+    
+    // kIDCTMatrix[8*x+u] = alpha(u)*cos((2*x+1)*u*M_PI/16)*sqrt(2), with fixed 13
+// bit precision, where alpha(0) = 1/sqrt(2) and alpha(u) = 1 for u > 0.
+// Some coefficients are off by +-1 to mimick libjpeg's behaviour.
+static const int kIDCTMatrix[kDCTBlockSize] = {
+  8192,  11363,  10703,   9633,   8192,   6437,   4433,   2260,
+  8192,   9633,   4433,  -2259,  -8192, -11362, -10704,  -6436,
+  8192,   6437,  -4433, -11362,  -8192,   2261,  10704,   9633,
+  8192,   2260, -10703,  -6436,   8192,   9633,  -4433, -11363,
+  8192,  -2260, -10703,   6436,   8192,  -9633,  -4433,  11363,
+  8192,  -6437,  -4433,  11362,  -8192,  -2261,  10704,  -9633,
+  8192,  -9633,   4433,   2259,  -8192,  11362, -10704,   6436,
+  8192, -11363,  10703,  -9633,   8192,  -6437,   4433,  -2260,
+};
+    
+    #endif  // GUETZLI_IDCT_H_
 
     
-      /// Limit the FSEvents actions to the subscriptioned mask (if not 0).
-  FSEventStreamEventFlags mask{0};
+    // Single pixel rgb to 16-bit yuv conversion.
+// The returned yuv values are signed integers in the
+// range [-128, 127] inclusive.
+inline static void RGBToYUV16(const uint8_t* const rgb,
+                              coeff_t *out) {
+  enum { FRAC = 16, HALF = 1 << (FRAC - 1) };
+  const int r = rgb[0];
+  const int g = rgb[1];
+  const int b = rgb[2];
+  out[0] = (19595 * r  + 38469 * g +  7471 * b - (128 << 16) + HALF) >> FRAC;
+  out[64] = (-11059 * r - 21709 * g + 32768 * b + HALF - 1) >> FRAC;
+  out[128] = (32768 * r  - 27439 * g -  5329 * b + HALF - 1) >> FRAC;
+}
     
-    /// Kernel shared buffer size in bytes.
-static const size_t kKernelQueueSize = (20 * (1 << 20));
-    
-      // Copy constructor if different value type - use getters and setters to
-  // perform conversion
-  template <typename T2>
-  XGBOOST_DEVICE explicit GradientPairInternal(const GradientPairInternal<T2> &g) {
-    SetGrad(g.GetGrad());
-    SetHess(g.GetHess());
+    // Returns the table width of the next 2nd level table, count is the histogram
+// of bit lengths for the remaining symbols, len is the code length of the next
+// processed symbol.
+static inline int NextTableBitSize(const int* count, int len) {
+  int left = 1 << (len - kJpegHuffmanRootTableBits);
+  while (len < kJpegHuffmanMaxBitLength) {
+    left -= count[len];
+    if (left <= 0) break;
+    ++len;
+    left <<= 1;
   }
+  return len - kJpegHuffmanRootTableBits;
+}
     
-    // Define a customized logistic regression objective in C++.
-// Implement the interface.
-class MyLogistic : public ObjFunction {
+      // Fills in block[] with the 8x8 coefficient block with block coordinates
+  // (block_x, block_y).
+  // NOTE: If the component is 2x2 subsampled, this corresponds to the 16x16
+  // pixel area with upper-left corner (16 * block_x, 16 * block_y).
+  void GetCoeffBlock(int block_x, int block_y,
+                     coeff_t block[kDCTBlockSize]) const;
+    
+    
+    {}  // namespace
+    
+    Finally, you can end the calculation anytime, by using XXH32_digest().
+This function returns the final 32-bits hash.
+You must provide the same 'void* state' parameter created by XXH32_init().
+Memory will be freed by XXH32_digest().
+*/
+    
+    // This is the base class for all factories that are used by RocksDB to create
+// new MemTableRep objects
+class MemTableRepFactory {
  public:
-  void Configure(const std::vector<std::pair<std::string, std::string> >& args) override {
-    param_.InitAllowUnknown(args);
-  }
-  void GetGradient(HostDeviceVector<bst_float> *preds,
-                   const MetaInfo &info,
-                   int iter,
-                   HostDeviceVector<GradientPair> *out_gpair) override {
-    out_gpair->Resize(preds->Size());
-    std::vector<bst_float>& preds_h = preds->HostVector();
-    std::vector<GradientPair>& out_gpair_h = out_gpair->HostVector();
-    for (size_t i = 0; i < preds_h.size(); ++i) {
-      bst_float w = info.GetWeight(i);
-      // scale the negative examples!
-      if (info.labels_[i] == 0.0f) w *= param_.scale_neg_weight;
-      // logistic transformation
-      bst_float p = 1.0f / (1.0f + std::exp(-preds_h[i]));
-      // this is the gradient
-      bst_float grad = (p - info.labels_[i]) * w;
-      // this is the second order gradient
-      bst_float hess = p * (1.0f - p) * w;
-      out_gpair_h.at(i) = GradientPair(grad, hess);
+  virtual ~MemTableRepFactory() {}
     }
-  }
-  const char* DefaultEvalMetric() const override {
-    return 'error';
-  }
-  void PredTransform(HostDeviceVector<bst_float> *io_preds) override {
-    // transform margin value to probability.
-    std::vector<bst_float> &preds = io_preds->HostVector();
-    for (size_t i = 0; i < preds.size(); ++i) {
-      preds[i] = 1.0f / (1.0f + std::exp(-preds[i]));
+    
+    #ifndef NDEBUG
+namespace rocksdb {
     }
-  }
-  bst_float ProbToMargin(bst_float base_score) const override {
-    // transform probability to margin value
-    return -std::log(1.0f / base_score - 1.0f);
-  }
-    }
+    
+      // # of memtable hits.
+  MEMTABLE_HIT,
+  // # of memtable misses.
+  MEMTABLE_MISS,
