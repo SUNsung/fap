642ca197cddafb86ade4bc0304d0115c0f9eb95d
SUNsung <weatherius@gmail.com>
2018-07-17 00:01:09 +0300

    Rewrite [53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py] File in: 2018-07-17 00:01:08

--- pyton/53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py
+++ pyton/53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py
@@ -1,98 +1,107 @@
 
         
-            s = TaggedJSONSerializer()
+        
+class FlaskyStyle(Style):
+    background_color = '#f8f8f8'
+    default_style = ''
+    
+    # TODO: response is the only one
+    
+        return out
     
     
-def test_uninstalled_package_paths(modules_tmpdir, purge_module):
-    app = modules_tmpdir.mkdir('config_package_app')
-    init = app.join('__init__.py')
-    init.write(
-        'import os\n'
-        'import flask\n'
-        'here = os.path.abspath(os.path.dirname(__file__))\n'
-        'app = flask.Flask(__name__)\n'
-    )
-    purge_module('config_package_app')
+@pytest.mark.skipif(sys.version_info < (2,7), reason='Only run on Python 2.7+')
+def test_system_ssl():
+    '''Verify we're actually setting system_ssl when it should be available.'''
+    assert info()['system_ssl']['version'] != ''
     
-        flask.template_rendered.connect(record, app)
-    try:
-        client.get('/')
-        assert len(recorded) == 1
-        template, context = recorded[0]
-        assert template.name == 'simple_template.html'
-        assert context['whiskey'] == 42
-    finally:
-        flask.template_rendered.disconnect(record, app)
+        @pytest.fixture(autouse=True, params=['no_proxy', 'NO_PROXY'])
+    def no_proxy(self, request, monkeypatch):
+        monkeypatch.setenv(request.param, '192.168.0.0/24,127.0.0.1,localhost.localdomain,172.16.1.1')
     
-        for i in range(1, n + 1):
-        print('==================')
-        print('Iteration %02d of %02d' % (i, n))
-        print('==================')
-        n_features = i * step
-        n_informative = n_features / 10
+    # List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = ['_build']
     
-        op.add_option('--transformers',
-                  dest='selected_transformers',
-                  default='GaussianRandomProjection,SparseRandomProjection',
-                  type=str,
-                  help='Comma-separated list of transformer to benchmark. '
-                       'Default: %default. Available: '
-                       'GaussianRandomProjection,SparseRandomProjection')
+        # By using the 'with' statement we are sure the session is closed, thus we
+    # avoid leaving sockets open which can trigger a ResourceWarning in some
+    # cases, and look like a memory leak in others.
+    with sessions.Session() as session:
+        return session.request(method=method, url=url, **kwargs)
     
-    In this examples we will use a movie review dataset.
+        builtin_str = str
+    bytes = str
+    str = unicode
+    basestring = basestring
+    numeric_types = (int, long, float)
+    integer_types = (int, long)
     
-    The data is generated with the ``make_checkerboard`` function, then
-shuffled and passed to the Spectral Biclustering algorithm. The rows
-and columns of the shuffled matrix are rearranged to show the
-biclusters found by the algorithm.
+    from . import utils
+from . import packages
+from .models import Request, Response, PreparedRequest
+from .api import request, get, head, post, patch, put, delete, options
+from .sessions import session, Session
+from .status_codes import codes
+from .exceptions import (
+    RequestException, Timeout, URLRequired,
+    TooManyRedirects, HTTPError, ConnectionError,
+    FileModeWarning, ConnectTimeout, ReadTimeout
+)
     
-    C = 1.0
-kernel = 1.0 * RBF([1.0, 1.0])  # for GPC
+    import logging
+from twisted.internet import defer
+import six
+from scrapy.exceptions import NotSupported, NotConfigured
+from scrapy.utils.httpobj import urlparse_cached
+from scrapy.utils.misc import load_object
+from scrapy.utils.python import without_none_values
+from scrapy import signals
     
-    plt.plot(features_samples_ratio, acc_clf1, linewidth=2,
-         label='Linear Discriminant Analysis with shrinkage', color='navy')
-plt.plot(features_samples_ratio, acc_clf2, linewidth=2,
-         label='Linear Discriminant Analysis', color='gold')
-    
-        def process_query(self, query):
-        query = self.parse_query(query)
-        results = self.memory_cache.get(query)
-        if results is None:
-            results = self.reverse_index_cluster.process_search(query)
-            self.memory_cache.set(query, results)
-        return results
-    
-        def categorize(self, transaction):
-        if transaction.seller in self.seller_category_map:
-            return self.seller_category_map[transaction.seller]
-        if transaction.seller in self.seller_category_overrides_map:
-            seller_category_map[transaction.seller] = \
-                self.manual_overrides[transaction.seller].peek_min()
-            return self.seller_category_map[transaction.seller]
-        return None
-    
-        def reducer(self, key, values):
-        '''Sum values for each key.
-    
-    
-from constants import *
-from dfa import *
-from exceptions import *
-from recognizers import *
-from streams import *
-from tokens import *
+        def download_request(self, request, spider):
+        p = urlparse_cached(request)
+        scheme = 'https' if request.meta.get('is_secure') else 'http'
+        bucket = p.hostname
+        path = p.path + '?' + p.query if p.query else p.path
+        url = '%s://%s.s3.amazonaws.com%s' % (scheme, bucket, path)
+        if self.anon:
+            request = request.replace(url=url)
+        elif self._signer is not None:
+            import botocore.awsrequest
+            awsrequest = botocore.awsrequest.AWSRequest(
+                method=request.method,
+                url='%s://s3.amazonaws.com/%s%s' % (scheme, bucket, path),
+                headers=request.headers.to_unicode_dict(),
+                data=request.body)
+            self._signer.add_auth(awsrequest)
+            request = request.replace(
+                url=url, headers=awsrequest.headers.items())
+        else:
+            signed_headers = self.conn.make_request(
+                    method=request.method,
+                    bucket=bucket,
+                    key=unquote(p.path),
+                    query_args=unquote(p.query),
+                    headers=request.headers,
+                    data=request.body)
+            request = request.replace(url=url, headers=signed_headers)
+        return self._download_http(request, spider)
 
     
-            return m
+        text = html.remove_tags_with_content(text, ('script', 'noscript'))
+    text = html.replace_entities(text)
+    text = html.remove_comments(text)
+    return _ajax_crawlable_re.search(text) is not None
+
     
-            raise NotImplementedError
+            body = tar_file.extractfile(tar_file.members[0]).read()
+        respcls = responsetypes.from_args(filename=tar_file.members[0].name, body=body)
+        return response.replace(body=body, cls=respcls)
     
+    from scrapy.utils.python import without_none_values
     
-    def getCharPositionInLine(self):
-        '''@brief Get the column of the tokens first character,
-        
-        Columns are numbered 0..n-1
-        
-        Using setter/getter methods is deprecated. Use o.charPositionInLine instead.'''
+        def process_request(self, request, spider):
+        if self.user_agent:
+            request.headers.setdefault(b'User-Agent', self.user_agent)
+
     
-    Tree objects are wrapper for Token objects.
+        return result
