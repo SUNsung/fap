5e65266905e4e65bfeea972020368249262a8383
SUNsung <weatherius@gmail.com>
2018-07-16 15:12:56 +0300

    Rewrite [01mVk.4MhfM5Y01SnBvBGt078A01h3AO3stKdjM01pp8.cpp] File in: 2018-07-16 15:12:56

--- cccccc/01mVk.4MhfM5Y01SnBvBGt078A01h3AO3stKdjM01pp8.cpp
+++ cccccc/01mVk.4MhfM5Y01SnBvBGt078A01h3AO3stKdjM01pp8.cpp
@@ -1,97 +1,42 @@
-class Env;
-class Iterator;
-class TableCache;
-class VersionEdit;
+
+        
+        #ifndef ATOM_BROWSER_UI_X_EVENT_DISABLER_H_
+#define ATOM_BROWSER_UI_X_EVENT_DISABLER_H_
     
-      inline void ClearSavedValue() {
-    if (saved_value_.capacity() > 1048576) {
-      std::string empty;
-      swap(empty, saved_value_);
-    } else {
-      saved_value_.clear();
-    }
-  }
-    
-    
-    {}  // namespace leveldb
-    
-    // Return the name of the sstable with the specified number
-// in the db named by 'dbname'.  The result will be prefixed with
-// 'dbname'.
-extern std::string TableFileName(const std::string& dbname, uint64_t number);
-    
-    TableCache::~TableCache() {
-  delete cache_;
+    bool IsUnresponsiveEventSuppressed() {
+  return g_suppress_level > 0;
 }
     
-    class TableCache {
- public:
-  TableCache(const std::string& dbname, const Options* options, int entries);
-  ~TableCache();
-    }
-    
-    TEST(WriteBatchTest, Corruption) {
-  WriteBatch batch;
-  batch.Put(Slice('foo'), Slice('bar'));
-  batch.Delete(Slice('box'));
-  WriteBatchInternal::SetSequence(&batch, 200);
-  Slice contents = WriteBatchInternal::Contents(&batch);
-  WriteBatchInternal::SetContents(&batch,
-                                  Slice(contents.data(),contents.size()-1));
-  ASSERT_EQ('Put(foo, bar)@200'
-            'ParseError()',
-            PrintContents(&batch));
-}
-    
-    #endif  // STORAGE_LEVELDB_INCLUDE_DUMPFILE_H_
+    #endif  // ATOM_COMMON_DRAGGABLE_REGION_H_
 
     
-    
-    {  // Intentionally copyable
-};
-    
-    HINSTANCE g_hInst = NULL;
-D3D_DRIVER_TYPE g_driverType = D3D_DRIVER_TYPE_NULL;
-D3D_FEATURE_LEVEL g_featureLevel = D3D_FEATURE_LEVEL_11_0;
-ID3D11Device* g_pd3dDevice = NULL;
-ID3D11DeviceContext* g_pImmediateContext = NULL;
-IDXGISwapChain* g_pSwapChain = NULL;
-    
-    
-    {    if(start_x == -1)
-    {
-        return 0;
+    namespace chrome {
     }
-    else
-    {
-        low_thresh = cvRound(max_start_x + 0.25*(max_end_x - max_start_x));
-        high_thresh = cvRound(max_start_x + 0.75*(max_end_x - max_start_x));
-        return 1;
+    
+      // Returns the bounds with its size clamped to the min/max size.
+  gfx::Size ClampSize(gfx::Size size) const;
+    
+    #endif  // ATOM_COMMON_COLOR_UTIL_H_
+
+    
+    void projectPoints(cv::InputArray objectPoints, cv::OutputArray imagePoints,
+                   cv::InputArray _rvec,cv::InputArray _tvec,
+                   const IntrinsicParams& param, cv::OutputArray jacobian);
+    
+                template<typename T>
+            static __device__ __forceinline__ T atomicMin(T* address, T val)
+            {
+#if defined (__CUDA_ARCH__) && (__CUDA_ARCH__ < 120)
+                T count = ::min(*address, val);
+                do
+                {
+                    *address = count;
+                } while (*address > count);
     }
-}
+    
+    /** @file
+ * @deprecated Use @ref cudev instead.
+ */
     
     
-    {            int tid = threadIdx.x;
-            cta_buffer[tid] = predicate ? (1 << (tid & 31)) : 0;
-            return warp_reduce(cta_buffer);
-#endif
-        }
-    
-    
-    {    // Legacy
-    extern void (CODEGEN_FUNCPTR *EnableClientState)(GLenum cap);
-    extern void (CODEGEN_FUNCPTR *DisableClientState)(GLenum cap);
-    extern void (CODEGEN_FUNCPTR *VertexPointer)(GLint size, GLenum type, GLsizei stride, const GLvoid *ptr);
-    extern void (CODEGEN_FUNCPTR *NormalPointer)(GLenum type, GLsizei stride, const GLvoid *ptr);
-    extern void (CODEGEN_FUNCPTR *ColorPointer)(GLint size, GLenum type, GLsizei stride, const GLvoid *ptr);
-    extern void (CODEGEN_FUNCPTR *TexCoordPointer)(GLint size, GLenum type, GLsizei stride, const GLvoid *ptr);
-    extern void (CODEGEN_FUNCPTR *TexEnvi)(GLenum target, GLenum pname, GLint param);
-    extern void (CODEGEN_FUNCPTR *MatrixMode)(GLenum mode);
-    extern void (CODEGEN_FUNCPTR *LoadIdentity)(void);
-    extern void (CODEGEN_FUNCPTR *Ortho)(GLdouble left, GLdouble right, GLdouble bottom, GLdouble top, GLdouble near_val, GLdouble far_val);
-    extern void (CODEGEN_FUNCPTR *Color3d)(GLdouble red, GLdouble green, GLdouble blue);
-}
-    
-    namespace cv
-{
-    }
+    {}
