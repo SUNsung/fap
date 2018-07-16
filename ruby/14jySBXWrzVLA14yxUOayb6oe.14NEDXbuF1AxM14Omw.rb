bdc1399820c7739820175d26e62fa09f8e5f38bc
SUNsung <weatherius@gmail.com>
2018-07-16 18:56:20 +0300

    Rewrite [14jySBXWrzVLA14yxUOayb6oe.14NEDXbuF1AxM14Omw.rb] File in: 2018-07-16 18:56:19

--- ruby/14jySBXWrzVLA14yxUOayb6oe.14NEDXbuF1AxM14Omw.rb
+++ ruby/14jySBXWrzVLA14yxUOayb6oe.14NEDXbuF1AxM14Omw.rb
@@ -1,68 +1,52 @@
 
         
-          def validate_option(method)
-    if self.respond_to? 'validate_#{method}'.to_sym
-      self.send('validate_#{method}'.to_sym)
-    else
-      false
-    end
+            brew cask install mactex
+    EOS
+  when 'pip' then <<-EOS.undent
+    Homebrew provides pip via: `brew install python`. However you will then
+    have two Pythons installed on your Mac, so alternatively you can install
+    pip via the instructions at:
+    
+      def self.canonical_name(name)
+    Formulary.canonical_name(name)
   end
     
-        it 'returns Rational(1) when self is Rational(-1) and the exponent is positive and even' do
-      (Rational(-1) ** bignum_value(0)).should eql(Rational(1))
-      (Rational(-1) ** bignum_value(2)).should eql(Rational(1))
-    end
+      no_protocol_file_contents = %r{https?:\\?/\\?/}
+  details[:file] = details[:file].gsub(no_protocol_file_contents, '/')
+  secure_details[:file] = secure_details[:file].gsub(no_protocol_file_contents, '/')
     
-          each { |u|
-        dig[u] = a = []
-        each{ |v| func.call(u, v) and a << v }
-      }
-    
-        ret = set.delete(2)
-    assert_equal(set, ret)
-    assert_equal(Set[1,3], set)
-    
-    describe :string_unpack_32bit_le_signed, shared: true do
-  it 'decodes an int with most significant bit set as a negative number' do
-    '\x00\xaa\x00\xff'.unpack(unpack_format()).should == [-16733696]
-  end
-end
-    
-        it 'returns related entity for an existing public post' do
-      post = FactoryGirl.create(:status_message, author: local_person, public: true)
-      entity = DiasporaFederation.callbacks.trigger(:fetch_related_entity, 'Post', post.guid)
-      expect(entity.author).to eq(post.diaspora_handle)
-      expect(entity.local).to be_truthy
-      expect(entity.public).to be_truthy
-      expect(entity.parent).to be_nil
-    end
-    
-        def index
-      pods_json = PodPresenter.as_collection(Pod.all)
-    
-          def restore_request_parameters
-        req = build_rack_request
-        req.update_param('client_id', session[:client_id])
-        req.update_param('redirect_uri', session[:redirect_uri])
-        req.update_param('response_type', response_type_as_space_seperated_values)
-        req.update_param('scope', session[:scopes])
-        req.update_param('state', session[:state])
-        req.update_param('nonce', session[:nonce])
+          def all_gem_names
+        core_gem_names + plugin_gem_names
       end
     
-        # @abstract
-    #
-    # Update the clone on the deployment target
-    #
-    # @return void
-    #
-    def update
-      raise NotImplementedError, 'Your SCM strategy module should provide a #update method'
-    end
+          def stage_definitions
+        stage_config_path.join('*.rb')
+      end
     
-      let(:help_output) do
-    out, _err = capture_io do
-      flags '--help', '-h'
-    end
-    out
+    class Rack::Builder
+  include Sinatra::Delegator
+end
+
+    
+          def call(env)
+        request  = Request.new(env)
+        get_was  = handle(request.GET)
+        post_was = handle(request.POST) rescue nil
+        app.call env
+      ensure
+        request.GET.replace  get_was  if get_was
+        request.POST.replace post_was if post_was
+      end
+    
+      it 'accepts requests with the same Accept-Language header' do
+    session = {:foo => :bar}
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
+    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_LANGUAGE' => 'a'
+    expect(session).not_to be_empty
   end
+    
+      it 'should not override the header if already set X-Content-Type-Options' do
+    mock_app with_headers('X-Content-Type-Options' => 'sniff')
+    expect(get('/', {}, 'wants' => 'text/html').headers['X-Content-Type-Options']).to eq('sniff')
+  end
+end
