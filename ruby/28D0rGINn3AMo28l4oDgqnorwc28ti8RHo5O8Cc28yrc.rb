684126676b88228c9d8e6bc41543c18c8ba1aa66
SUNsung <weatherius@gmail.com>
2018-07-17 03:16:30 +0300

    Rewrite [28D0rGINn3AMo28l4oDgqnorwc28ti8RHo5O8Cc28yrc.rb] File in: 2018-07-17 03:16:29

--- ruby/28D0rGINn3AMo28l4oDgqnorwc28ti8RHo5O8Cc28yrc.rb
+++ ruby/28D0rGINn3AMo28l4oDgqnorwc28ti8RHo5O8Cc28yrc.rb
@@ -1,156 +1,193 @@
 
         
-              logger.log(sev, message)
-    end
+                def test_encoded_password
+          password = 'am@z1ng_p@ssw0rd#!'
+          encoded_password = URI.encode_www_form_component(password)
+          spec = resolve 'abstract://foo:#{encoded_password}@localhost/bar'
+          assert_equal password, spec['password']
+        end
     
-      def validate_evernote_options
-    unless evernote_consumer_key.present? &&
-      evernote_consumer_secret.present? &&
-      evernote_oauth_token.present?
-      errors.add(:base, 'Evernote ENV variables and a Service are required')
-    end
-  end
-    
-        respond_to do |format|
-      format.html { redirect_to user_credentials_path }
-      format.json { head :no_content }
-    end
-  end
-    
-        options[:attribution] = <<-HTML
-      &copy; 2011&ndash;2018 Twitter, Inc.<br>
-      &copy; 2011&ndash;2018 The Bootstrap Authors<br>
-      Code licensed under the MIT License.<br>
-      Documentation licensed under the Creative Commons Attribution License v3.0.
-    HTML
-    
-        options[:container] = '#bd'
-    
-        def initial_urls
-      [ 'https://hexdocs.pm/elixir/#{self.class.release}/api-reference.html',
-        'https://hexdocs.pm/eex/#{self.class.release}/EEx.html',
-        'https://hexdocs.pm/ex_unit/#{self.class.release}/ExUnit.html',
-        'https://hexdocs.pm/iex/#{self.class.release}/IEx.html',
-        'https://hexdocs.pm/logger/#{self.class.release}/Logger.html',
-        'https://hexdocs.pm/mix/#{self.class.release}/Mix.html',
-        'https://elixir-lang.org/getting-started/introduction.html' ]
-    end
-    
-        version 'Neko' do
-      self.base_url = 'https://api.haxe.org/neko/'
-    end
-    
-    def usercode(f, out)
-  require 'erb'
-  compiler = ERB::Compiler.new('%-')
-  compiler.put_cmd = compiler.insert_cmd = 'out.<<'
-  lineno = f.lineno
-  src, = compiler.compile(f.read)
-  eval(src, binding, f.path, lineno)
-end
-    
-      it 'uses '-e' in $0' do
-    system(*ruby_exe, '-e', 'exit $0 == '-e'').should == true
-  end
-    
-      def test_lambda_longjmp
-    assert_eval_with_jit('#{<<~'begin;'}\n#{<<~'end;'}', stdout: '5', success_count: 1)
-    begin;
-      fib = lambda do |x|
-        return x if x == 0 || x == 1
-        fib.call(x-1) + fib.call(x-2)
+                klass ||= AssociationQueryValue
+            queries = klass.new(associated_table, value).queries.map do |query|
+              expand_from_hash(query).reduce(&:and)
+            end
+            queries.reduce(&:or)
+          elsif table.aggregated_with?(key)
+            mapping = table.reflect_on_aggregation(key).mapping
+            queries = Array.wrap(value).map do |object|
+              mapping.map do |field_attr, aggregate_attr|
+                if mapping.size == 1 && !object.respond_to?(aggregate_attr)
+                  build(table.arel_attribute(field_attr), object)
+                else
+                  build(table.arel_attribute(field_attr), object.send(aggregate_attr))
+                end
+              end.reduce(&:and)
+            end
+            queries.reduce(&:or)
+          else
+            build(table.arel_attribute(key), value)
+          end
+        end
       end
-      print fib.call(5)
-    end;
+    
+        assert_same car, new_bulb.car
   end
     
-      def test_chmod_m17n
-    bug5671 = '[ruby-dev:44898]'
-    Dir.mktmpdir('test-file-chmod-m17n-') do |tmpdir|
-      file = File.join(tmpdir, '\u3042')
-      File.open(file, 'w'){}
-      assert_equal(File.chmod(0666, file), 1, bug5671)
+    class WrongReply < Reply
+  validate :errors_on_empty_content
+  validate :title_is_wrong_create, on: :create
+    
+      has_many :replies, dependent: :destroy, foreign_key: 'parent_id', autosave: true
+  has_many :approved_replies, -> { approved }, class_name: 'Reply', foreign_key: 'parent_id', counter_cache: 'replies_count'
+    
+      test 'broadcasting_for with a string' do
+    assert_equal 'hello', ChatChannel.broadcasting_for('hello')
+  end
+end
+
+    
+      test 'disallow block and arg together' do
+    e = assert_raise ArgumentError do
+      ChatChannel.periodically(:send_updates, every: 1) { ping }
+    end
+    assert_match(/not both/, e.message)
+  end
+    
+      test 'websocket connection' do
+    run_in_eventmachine do
+      connection = open_connection
+      connection.process
+    
+          assert_kind_of ChatChannel, channel
+      assert_equal 1, channel.room.id
     end
   end
     
-        a = []
-    a << a
+      test 'broadcast generates notification' do
+    begin
+      server = TestServer.new
     
-      def test_asctime
-    t2000 = get_t2000
-    assert_equal('Sat Jan  1 00:00:00 2000', t2000.asctime)
-    assert_equal(Encoding::US_ASCII, t2000.asctime.encoding)
-    assert_kind_of(String, Time.at(0).asctime)
-  end
+      def all_groups(current_user)
+    groups = []
     
-    describe :string_unpack_32bit_le_unsigned, shared: true do
-  it 'decodes an int with most significant bit set as a positive number' do
-    '\x00\xaa\x00\xff'.unpack(unpack_format()).should == [4278233600]
-  end
-end
+      # POST /resource/confirmation
+  def create
+    self.resource = resource_class.send_confirmation_instructions(resource_params)
+    yield resource if block_given?
     
-      class SubclassX
-    attr_reader :key
-    def initialize(*)
-      @key = :value
-      super
+        def email_changed(record, opts={})
+      devise_mail(record, :email_changed, opts)
+    end
+    
+          def remember_me_is_active?(resource)
+        return false unless resource.respond_to?(:remember_me)
+        scope = Devise::Mapping.find_scope!(resource)
+        _, token, generated_at = cookies.signed[remember_key(resource, scope)]
+        resource.remember_me?(token, generated_at)
+      end
+    
+          if options[:skip_helpers] == true
+        @used_helpers = @used_routes
+      elsif skip = options[:skip_helpers]
+        @used_helpers = self.routes - Array(skip).map(&singularizer)
+      else
+        @used_helpers = self.routes
+      end
     end
   end
 end
 
     
-            self.description = <<-DESC
-          Shows the content of the pods cache as a YAML tree output, organized by pod.
-          If `NAME` is given, only the caches for that pod will be included in the output.
-        DESC
+      def deliver_digest
+    NotificationMailer.digest(user.account).deliver_now!
+    user.touch(:last_emailed_at)
+  end
+end
+
     
-    module Pod
-  class Command
-    class Init < Command
-      self.summary = 'Generate a Podfile for the current directory'
-      self.description = <<-DESC
-        Creates a Podfile for the current directory if none currently exists. If
-        an `XCODEPROJ` project file is specified or if there is only a single
-        project file in the current directory, targets will be automatically
-        generated based on targets defined in the project.
-    
-            self.summary = 'The repl listens to commands on standard input'
-        self.description = <<-DESC
-          The repl listens to commands on standard input and prints their
-          result to standard output.
-          It accepts all the other ipc subcommands. The repl will signal the
-          end of output with the the ASCII CR+LF `\\n\\r`.
-        DESC
-    
-          def redirect(env)
-        request = Request.new(env)
-        warn env, 'attack prevented by #{self.class}'
-        [302, {'Content-Type' => 'text/html', 'Location' => request.path}, []]
-      end
-    
-        it 'accepts requests with a single session cookie' do
-      get '/', {}, 'HTTP_COOKIE' => 'rack.session=SESSION_TOKEN'
-      expect(last_response).to be_ok
+          expect_updated_sign_in_at(user)
     end
     
-        it 'copes with nested arrays' do
-      mock_app do |env|
-        request = Rack::Request.new(env)
-        [200, {'Content-Type' => 'text/plain'}, [request.params['foo']['bar']]]
-      end
-      get '/', :foo => {:bar => '<bar>'}
-      expect(body).to eq('&lt;bar&gt;')
+      def start_url
+    '/web/timelines/home'
+  end
+    
+    # Copyright (C) 2008 Rapid7, Inc.
+    
+    
+    # immediate selector of css at pos
+    def selector_for_pos(css, pos, depth = -1)
+      css[css_def_pos(css, pos, depth)].dup.strip
     end
+    
+        def log_processing(name)
+      puts yellow '  #{File.basename(name)}'
+    end
+    
+    class NodeMincerTest < Minitest::Test
+  DUMMY_PATH = 'test/dummy_node_mincer'
+    
+        def common_options(opts)
+      opts.separator ''
+      opts.separator 'Common Options:'
+    
+            def log_level(name, options = {})
+          if options[:prepend]
+            level = log_levels.values.min
+            level = level.nil? ? 0 : level - 1
+          else
+            level = log_levels.values.max
+            level = level.nil? ? 0 : level + 1
+          end
+          log_levels.update(name => level)
+          define_logger(name)
+        end
+    
+      entries.each do |entry|
+    if File.exist?(entry[:file])
+      warn '[skip] #{entry[:file]} already exists'
+    else
+      File.open(entry[:file], 'w+') do |f|
+        f.write(ERB.new(File.read(entry[:template])).result(binding))
+        puts I18n.t(:written_file, scope: :capistrano, file: entry[:file])
+      end
+    end
+  end
+    
+        it 'Returns nil when Referer header is invalid' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/bad|uri'}
+      expect(subject.referrer(env)).to be_nil
+    end
+  end
+end
+
+    
+            post '/', :file => Rack::Test::UploadedFile.new(temp_file.path), :other => '<bar>'
+        expect(body).to eq('_escaped_params_tmp_file\nhello world\n&lt;bar&gt;')
+      ensure
+        File.unlink(temp_file.path)
+      end
+    end
+  end
+end
+
+    
+      it 'should not override the header if already set' do
+    mock_app with_headers('X-Frame-Options' => 'allow')
+    expect(get('/', {}, 'wants' => 'text/html').headers['X-Frame-Options']).to eq('allow')
+  end
+end
+
     
       it 'should allow changing the protection mode' do
     # I have no clue what other modes are available
     mock_app do
-      use Rack::Protection::FrameOptions, :frame_options => :deny
+      use Rack::Protection::XSSHeader, :xss_mode => :foo
       run DummyApp
     end
     
-          rtn = ''
-      (context.environments.first['site'][@array_name] || []).each do |file|
-        if file !~ /^[a-zA-Z0-9_\/\.-]+$/ || file =~ /\.\// || file =~ /\/\./
-          rtn = rtn + 'Include file '#{file}' contains invalid characters or sequences'
-        end
+      # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = :random
