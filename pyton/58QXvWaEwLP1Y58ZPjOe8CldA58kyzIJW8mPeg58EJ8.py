b83c4429f7c2e46dbe9a6c6283f40db5ca61942f
SUNsung <weatherius@gmail.com>
2018-07-17 03:38:15 +0300

    Rewrite [58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py] File in: 2018-07-17 03:38:15

--- pyton/58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py
+++ pyton/58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py
@@ -1,157 +1,161 @@
 
         
-        
-def basic_auth(header=BASIC_AUTH_HEADER_VALUE):
+        cc_library(
+    name = 'syntaxnet_transition_state',
+    srcs = ['syntaxnet_transition_state.cc'],
+    hdrs = ['syntaxnet_transition_state.h'],
+    deps = [
+        '//dragnn/core/interfaces:cloneable_transition_state',
+        '//dragnn/core/interfaces:transition_state',
+        '//dragnn/io:syntaxnet_sentence',
+        '//dragnn/protos:trace_proto_cc',
+        '//syntaxnet:base',
+        '//syntaxnet:parser_transitions',
+    ],
+)
     
+    cc_library(
+    name = 'mst_ops_cc',
+    srcs = [
+        'ops/mst_op_kernels.cc',
+        'ops/mst_ops.cc',
+    ],
+    deps = [
+        ':mst_solver',
+        '//syntaxnet:base',
+        '@org_tensorflow//tensorflow/core:framework_headers_lib',
+        '@org_tensorflow//tensorflow/core:lib',
+    ],
+    alwayslink = 1,
+)
+
     
-class Environment(object):
+        # Extract the source and target token activations.  Use |stride| to collapse
+    # batch and beam into a single dimension.
+    sources = network_units.lookup_named_tensor('sources', linked_embeddings)
+    targets = network_units.lookup_named_tensor('targets', linked_embeddings)
+    source_tokens_bxnxs = tf.reshape(sources.tensor,
+                                     [stride, -1, self._source_dim])
+    target_tokens_bxnxt = tf.reshape(targets.tensor,
+                                     [stride, -1, self._target_dim])
+    num_tokens = tf.shape(source_tokens_bxnxs)[1]
+    
+        Raises:
+      RuntimeError: if fixed features are configured.
     '''
-    Information about the execution context
-    (standard streams, config directory, etc).
+    logging.info('Building component: %s', self.spec.name)
+    if self.spec.fixed_feature:
+      raise RuntimeError(
+          'Fixed features are not compatible with bulk annotation. '
+          'Use the 'bulk-features' component instead.')
+    linked_embeddings = [
+        fetch_linked_embedding(self, network_states, spec)
+        for spec in self.spec.linked_feature
+    ]
     
-    from httpie.plugins import plugin_manager
-from httpie.context import Environment
+          # The cost corresponding to the three entries is, respectively,
+      # 0.7012, 0.7644, and 1.7269. Each of them is computed using the formula
+      # -prob_i * log(sigmoid(logit_i)) - (1-prob_i) * log(1-sigmoid(logit_i))
+      self.assertAlmostEqual(cost, 3.1924, 4)
+      self.assertEqual(correct, 1)
+      self.assertEqual(total, 3)
+      self.assertAllEqual(gold_labels, [0, 1, 2])
     
-        def __init__(self, chunk_size=CHUNK_SIZE, **kwargs):
-        super(RawStream, self).__init__(**kwargs)
-        self.chunk_size = chunk_size
+    '''Converter for DRAGNN checkpoint+master-spec files to TF SavedModels.
+    
+          tf_out = sess.run(
+          'annotation/annotations:0',
+          feed_dict={'annotation/ComputeSession/InputBatch:0': test_inputs})
+    
+        Args:
+      handle: Handle tensor for the ComputeSession.
+      use_moving_average: Whether or not to read from the moving
+        average variables instead of the true parameters. Note: it is not
+        possible to make gradient updates when this is True.
+      build_runtime_graph: Whether to build a graph for use by the runtime.
+    
+        #: Some implementations can detect changes to the session and set
+    #: this when that happens. The mixin default is hard coded to
+    #: ``True``.
+    modified = True
+    
+    import flask
+    
+            Keyword:                   'bold #004461',   # class: 'k'
+        Keyword.Constant:          'bold #004461',   # class: 'kc'
+        Keyword.Declaration:       'bold #004461',   # class: 'kd'
+        Keyword.Namespace:         'bold #004461',   # class: 'kn'
+        Keyword.Pseudo:            'bold #004461',   # class: 'kp'
+        Keyword.Reserved:          'bold #004461',   # class: 'kr'
+        Keyword.Type:              'bold #004461',   # class: 'kt'
+    
+    This module provides the capabilities for the Requests hooks system.
     
     
-@pytest.mark.parametrize('ssl_version', SSL_VERSION_ARG_MAPPING.keys())
-def test_ssl_version(httpbin_secure, ssl_version):
-    try:
-        r = http(
-            '--ssl', ssl_version,
-            httpbin_secure + '/get'
-        )
-        assert HTTP_OK in r
-    except SSLError as e:
-        if ssl_version == 'ssl3':
-            # pytest-httpbin doesn't support ssl3
-            assert 'SSLV3_ALERT_HANDSHAKE_FAILURE' in str(e)
-        else:
-            raise
+@pytest.fixture
+def httpbin(httpbin):
+    return prepare_url(httpbin)
     
-    
-def prepare_url(value):
-    # Issue #1483: Make sure the URL always has a trailing slash
-    httpbin_url = value.url.rstrip('/') + '/'
-    
-    
-def to_native_string(string, encoding='ascii'):
-    '''Given a string object, regardless of type, returns a representation of
-    that string in the native string type, encoding and decoding where
-    necessary. This assumes ASCII unless told otherwise.
+        :param url: URL for the new :class:`Request` object.
+    :param data: (optional) Dictionary (will be form-encoded), bytes, or file-like object to send in the body of the :class:`Request`.
+    :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
     '''
-    if isinstance(string, builtin_str):
-        out = string
-    else:
-        if is_py2:
-            out = string.encode(encoding)
-        else:
-            out = string.decode(encoding)
     
-            :param request: The :class:`PreparedRequest <PreparedRequest>` being sent.
-        :param stream: (optional) Whether to stream the request content.
-        :param timeout: (optional) How long to wait for the server to send
-            data before giving up, as a float, or a :ref:`(connect timeout,
-            read timeout) <timeouts>` tuple.
-        :type timeout: float or tuple
-        :param verify: (optional) Either a boolean, in which case it controls whether we verify
-            the server's TLS certificate, or a string, in which case it must be a path
-            to a CA bundle to use
-        :param cert: (optional) Any user-provided SSL certificate to be trusted.
-        :param proxies: (optional) The proxies dictionary to apply to the request.
-        '''
-        raise NotImplementedError
+            # Strip port numbers from netloc. This weird `if...encode`` dance is
+        # used for Python 3.2, which doesn't support unicode literals.
+        splitstr = b':'
+        if isinstance(url, str):
+            splitstr = splitstr.decode('ascii')
+        host = ri.netloc.split(splitstr)[0]
     
-        def test_long_authinfo_in_url(self):
-        url = 'http://{0}:{1}@{2}:9000/path?query#frag'.format(
-            'E8A3BE87-9E3F-4620-8858-95478E385B5B',
-            'EA770032-DA4D-4D84-8CE9-29C6D910BF1E',
-            'exactly-------------sixty-----------three------------characters',
-        )
-        r = requests.Request('GET', url).prepare()
-        assert r.url == url
+    import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
     
-        def digest_response_handler(sock):
-        # Respond to GET with a 200 containing www-authenticate header.
-        request_content = consume_socket_content(sock, timeout=0.5)
-        assert request_content.startswith(b'GET / HTTP/1.1')
-        sock.send(text_200_chal)
+            ChatSocketHandler.update_cache(chat)
+        ChatSocketHandler.send_updates(chat)
     
-        def __nonzero__(self):
-        '''Returns True if :attr:`status_code` is less than 400.
-    
-        def __init__(self, data=None, **kwargs):
-        self._store = OrderedDict()
-        if data is None:
-            data = {}
-        self.update(data, **kwargs)
-    
-        @test_util.patch_get_utility()
-    def test_deploy_certificate_restart_failure2(self, mock_get_utility):
-        installer = mock.MagicMock()
-        installer.restart.side_effect = errors.PluginError
-        installer.rollback_checkpoints.side_effect = errors.ReverterError
-        self.client.installer = installer
-    
-        @mock.patch('certbot_compatibility_test.validator.requests.get')
-    def test_redirect_wrong_status_code(self, mock_get_request):
-        mock_get_request.return_value = create_response(
-            201, {'location': 'https://test.com'})
-        self.assertFalse(self.validator.redirect('test.com'))
-    
-    # Language to be used for generating the HTML full-text search index.
-# Sphinx supports the following languages:
-#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
-#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
-#html_search_language = 'en'
-    
-            try:
-            signal.alarm(1)
-    
-        def __init__(self):
-        self._options = Options()
-    
-    T_CV2 = typing.ClassVar[int]
-T_CV3 = typing.ClassVar
-    
-    @dataclass
-class CV:
-    T_CV4 = ClassVar
-    cv0: ClassVar[int] = 20
-    cv1: ClassVar = 30
-    cv2: T_CV2
-    cv3: T_CV3
-    not_cv4: T_CV4  # When using string annotations, this field is not recognized as a ClassVar.
+        def start_tree(self, tree, filename):
+        self.found_future_import = False
     
     
-def read_binary(package: Package, resource: Resource) -> bytes:
-    '''Return the binary contents of the resource.'''
-    resource = _normalize_path(resource)
-    package = _get_package(package)
-    with open_binary(package, resource) as fp:
-        return fp.read()
+def run():
+    io_loop = IOLoop(make_current=True)
+    app = Application([('/', RootHandler)])
+    port = random.randrange(options.min_port, options.max_port)
+    app.listen(port, address='127.0.0.1')
+    signal.signal(signal.SIGCHLD, handle_sigchld)
+    args = ['ab']
+    args.extend(['-n', str(options.n)])
+    args.extend(['-c', str(options.c)])
+    if options.keepalive:
+        args.append('-k')
+    if options.quiet:
+        # just stops the progress messages printed to stderr
+        args.append('-q')
+    args.append('http://127.0.0.1:%d/' % port)
+    subprocess.Popen(args)
+    io_loop.start()
+    io_loop.close()
+    io_loop.clear_current()
     
+    define('port', default=8888, help='run on the given port', type=int)
+define('db_host', default='127.0.0.1', help='blog database host')
+define('db_port', default=5432, help='blog database port')
+define('db_database', default='blog', help='blog database name')
+define('db_user', default='blog', help='blog database user')
+define('db_password', default='blog', help='blog database password')
     
-class DefaultEntryTest(BaseRequestRateTest, unittest.TestCase):
-    robots_txt = '''\
-User-agent: *
-Crawl-delay: 1
-Request-rate: 3/15
-Disallow: /cyberworld/map/
-    '''
-    request_rate = urllib.robotparser.RequestRate(3, 15)
-    crawl_delay = 1
-    good = ['/', '/test.html']
-    bad = ['/cyberworld/map/index.html']
+                stack.append(item)
+            stack_ids.add(item_id)
+            inner(gc.get_referents(item))
+            stack.pop()
+            stack_ids.remove(item_id)
+            visited_ids.add(item_id)
     
-            self.assertEqual(format(1+3j, 'g'), '1+3j')
-        self.assertEqual(format(3j, 'g'), '0+3j')
-        self.assertEqual(format(1.5+3.5j, 'g'), '1.5+3.5j')
-    
-    def clean_text(text, replacements = {':': '_', ' ': '_', '/': '_', '.': '', ''': ''}):
-    for key, rep in replacements.items():
-        text = text.replace(key, rep)
-    return text    
+    ********************************************************************
+'''
