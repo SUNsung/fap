93c6532876fa77cf689cd945fb69f22dbcd41811
SUNsung <weatherius@gmail.com>
2018-07-17 22:11:59 +0300

    Rewrite [16d8t4NGZs5Jk16Xw1rGe1HZU16bK5G.eUoQWk16KYa.cpp] File in: 2018-07-17 22:11:59

--- cccccc/16d8t4NGZs5Jk16Xw1rGe1HZU16bK5G.eUoQWk16KYa.cpp
+++ cccccc/16d8t4NGZs5Jk16Xw1rGe1HZU16bK5G.eUoQWk16KYa.cpp
@@ -1,228 +1,251 @@
 
         
-        // Generate param traits log methods.
-#include 'ipc/param_traits_log_macros.h'
-namespace IPC {
-#include 'content/nw/src/common/common_message_generator.h'
-}  // namespace IPC
+        
+    {  typedef std::unordered_map<string, DeviceAttributes> StatusMap;
+  StatusMap device_status_cache_ GUARDED_BY(mu_);
+};
+    
+    // Functor used by ReverseOp to do the computations.
+template <typename Device, typename T, int Dims>
+struct Reverse {
+  void operator()(const Device& d, typename TTypes<T, Dims>::ConstTensor input,
+                  const Eigen::array<bool, Dims>& reverse_dims,
+                  typename TTypes<T, Dims>::Tensor output) {
+    output.device(d) = input.reverse(reverse_dims);
+  }
+};
+    
+      // sum(Grad[Y] * (X - E[X])).
+  auto sum_grad_output_times_activiation_minus_mean =
+      add(HloInstruction::CreateReduce(
+          feature_shape, grad_output_times_activiation_minus_mean, zero,
+          dimensions_without_feature, add_reduce_computation));
+    
+    
+    {  EXPECT_THAT(model.GetOutput(), ElementsAreArray({1, 0}));
+  EXPECT_THAT(model.GetOutputShape(), ElementsAreArray({1, 1, 2, 1}));
+}
+    
+    #endif  // TENSORFLOW_KERNELS_SPARSE_TENSOR_DENSE_ADD_OP_H_
 
     
-        base::FilePath shortcutPath(path);
-    result->AppendBoolean(base::win::CreateOrUpdateShortcutLink(shortcutPath, props, 
-      base::PathExists(shortcutPath) ? base::win::SHORTCUT_UPDATE_EXISTING : base::win::SHORTCUT_CREATE_ALWAYS));
+    #endif  // TENSORFLOW_LIB_IO_RECORD_WRITER_H_
+
+    
+    namespace leveldb {
+    }
+    
+      VersionEdit edit;
+  for (int i = 0; i < 4; i++) {
+    TestEncodeDecode(edit);
+    edit.AddFile(3, kBig + 300 + i, kBig + 400 + i,
+                 InternalKey('foo', kBig + 500 + i, kTypeValue),
+                 InternalKey('zoo', kBig + 600 + i, kTypeDeletion));
+    edit.DeleteFile(4, kBig + 700 + i);
+    edit.SetCompactPointer(i, InternalKey('x', kBig + 900 + i, kTypeValue));
+  }
+    
+    TEST(FindFileTest, MultipleNullBoundaries) {
+  Add('150', '200');
+  Add('200', '250');
+  Add('300', '350');
+  Add('400', '450');
+  ASSERT_TRUE(! Overlaps(NULL, '149'));
+  ASSERT_TRUE(! Overlaps('451', NULL));
+  ASSERT_TRUE(Overlaps(NULL, NULL));
+  ASSERT_TRUE(Overlaps(NULL, '150'));
+  ASSERT_TRUE(Overlaps(NULL, '199'));
+  ASSERT_TRUE(Overlaps(NULL, '200'));
+  ASSERT_TRUE(Overlaps(NULL, '201'));
+  ASSERT_TRUE(Overlaps(NULL, '400'));
+  ASSERT_TRUE(Overlaps(NULL, '800'));
+  ASSERT_TRUE(Overlaps('100', NULL));
+  ASSERT_TRUE(Overlaps('200', NULL));
+  ASSERT_TRUE(Overlaps('449', NULL));
+  ASSERT_TRUE(Overlaps('450', NULL));
+}
+    
+    #ifndef CPPTL_JSON_ASSERTIONS_H_INCLUDED
+#define CPPTL_JSON_ASSERTIONS_H_INCLUDED
+    
+    // Version constant.
+// This is either 0 for python, 1 for CPP V1, 2 for CPP V2.
+//
+// 0 is default and is equivalent to
+//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+//
+// 1 is set with -DPYTHON_PROTO2_CPP_IMPL_V1 and is equivalent to
+//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
+// and
+//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=1
+//
+// 2 is set with -DPYTHON_PROTO2_CPP_IMPL_V2 and is equivalent to
+//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
+// and
+//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=2
+#ifdef PYTHON_PROTO2_CPP_IMPL_V1
+#error 'PYTHON_PROTO2_CPP_IMPL_V1 is no longer supported.'
 #else
-    result->AppendBoolean(false);
-#endif
-    return;
-  } else if (method == 'GetPackage') {
-    result->AppendString(shell->GetPackage()->package_string());
-    return;
-  } else if (method == 'SetCrashDumpDir') {
-    std::string path;
-    arguments.GetString(0, &path);
-    //FIXME: result->AppendBoolean(SetCrashDumpPath(path.c_str()));
-    return;
-  } else if (method == 'RegisterGlobalHotKey') {
-    int object_id = -1;
-    arguments.GetInteger(0, &object_id);
-    Shortcut* shortcut =
-        static_cast<Shortcut*>(DispatcherHost::GetApiObject(object_id));
-    bool success = GlobalShortcutListener::GetInstance()->RegisterAccelerator(
-                       shortcut->GetAccelerator(), shortcut);
-    if (!success)
-      shortcut->OnFailed('Register global desktop keyboard shortcut failed.');
+#ifdef PYTHON_PROTO2_CPP_IMPL_V2
+static int kImplVersion = 2;
+#else
+#ifdef PYTHON_PROTO2_PYTHON_IMPL
+static int kImplVersion = 0;
+#else
     
-    namespace content {
-class RenderFrameHost;
+    // Find the file that declares the given fully-qualified symbol name.
+bool PyDescriptorDatabase::FindFileContainingSymbol(
+    const string& symbol_name, FileDescriptorProto* output) {
+  ScopedPyObjectPtr py_descriptor(
+      PyObject_CallMethod(py_database_, 'FindFileContainingSymbol', 's#',
+                          symbol_name.c_str(), symbol_name.size()));
+  return GetFileDescriptorProto(py_descriptor.get(), output);
 }
     
+      // Find the file which defines an extension extending the given message type
+  // with the given field number.
+  // Containing_type must be a fully-qualified type name.
+  // Python objects are not required to implement this method.
+  bool FindFileContainingExtension(const string& containing_type,
+                                   int field_number,
+                                   FileDescriptorProto* output);
     
-    {  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
-  if (!item)
-    return;
-  item->OnClick();
-}
+    #include <string>
     
-       bool IsItemForCommandIdDynamic(int command_id) const override;
-   base::string16 GetLabelForCommandId(int command_id) const override;
-   bool GetIconForCommandId(int command_id,
-                                   gfx::Image* icon) const override;
+      void Generate(io::Printer* printer);
     
-    NwMenuModel::NwMenuModel(Delegate* delegate) : SimpleMenuModel(delegate) {
-}
+    #include <sstream>
     
-     protected:
-  ~NwClipboardGetListSyncFunction() override;
-    
-    
-    {    private:
-      DISALLOW_COPY_AND_ASSIGN(NwScreenInitEventListenersFunction);      
-  };
-    
-    #endif // BITCOIN_INDIRECTMAP_H
-
-    
-    // Called on every log record (each one of which is a WriteBatch)
-// found in a kDescriptorFile.
-static void VersionEditPrinter(uint64_t pos, Slice record, WritableFile* dst) {
-  std::string r = '--- offset ';
-  AppendNumberTo(&r, pos);
-  r += '; ';
-  VersionEdit edit;
-  Status s = edit.DecodeFrom(record);
-  if (!s.ok()) {
-    r += s.ToString();
-    r.push_back('\n');
-  } else {
-    r += edit.DebugString();
-  }
-  dst->Append(r);
-}
-    
-    Status TableCache::Get(const ReadOptions& options,
-                       uint64_t file_number,
-                       uint64_t file_size,
-                       const Slice& k,
-                       void* arg,
-                       void (*saver)(void*, const Slice&, const Slice&)) {
-  Cache::Handle* handle = NULL;
-  Status s = FindTable(file_number, file_size, &handle);
-  if (s.ok()) {
-    Table* t = reinterpret_cast<TableAndFile*>(cache_->Value(handle))->table;
-    s = t->InternalGet(options, k, arg, saver);
-    cache_->Release(handle);
-  }
-  return s;
-}
-    
-    #ifndef STORAGE_LEVELDB_DB_TABLE_CACHE_H_
-#define STORAGE_LEVELDB_DB_TABLE_CACHE_H_
-    
-    
-    {    done_++;
-    if (done_ >= next_report_) {
-      if      (next_report_ < 1000)   next_report_ += 100;
-      else if (next_report_ < 5000)   next_report_ += 500;
-      else if (next_report_ < 10000)  next_report_ += 1000;
-      else if (next_report_ < 50000)  next_report_ += 5000;
-      else if (next_report_ < 100000) next_report_ += 10000;
-      else if (next_report_ < 500000) next_report_ += 50000;
-      else                            next_report_ += 100000;
-      fprintf(stderr, '... finished %d ops%30s\r', done_, '');
-      fflush(stderr);
+    namespace google {
+namespace protobuf {
+namespace compiler {
+namespace csharp {
     }
-  }
+    }
+    }
+    }
     
-      // The name of the comparator.  Used to check for comparator
-  // mismatches (i.e., a DB created with one comparator is
-  // accessed using a different comparator.
-  //
-  // The client of this package should switch to a new name whenever
-  // the comparator implementation changes in a way that will cause
-  // the relative ordering of any two keys to change.
-  //
-  // Names starting with 'leveldb.' are reserved and should not be used
-  // by any clients of this package.
-  virtual const char* Name() const = 0;
+    struct Options;
     
-      // Return a pointer to the beginning of the referenced data
-  const char* data() const { return data_; }
+     private:
+  const FieldDescriptor* descriptor_;
+  Context* context_;
+  ClassNameResolver* name_resolver_;
+  string scope_;
     
-     protected:
-  /**
-   * @param bottom input Blob vector (length 2+)
-   *   -# @f$ (N \times ...) @f$
-   *      the inputs @f$ x_1 @f$
-   *   -# @f$ (M) @f$
-   *      the inputs @f$ x_2 @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (M \times ...) @f$:
-   *      the reindexed array @f$
-   *        y = x_1[x_2]
-   *      @f$
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-      /**
-   * @brief Computes the Contrastive error gradient w.r.t. the inputs.
-   *
-   * Computes the gradients with respect to the two input vectors (bottom[0] and
-   * bottom[1]), but not the similarity label (bottom[2]).
-   *
-   * @param top output Blob vector (length 1), providing the error gradient with
-   *      respect to the outputs
-   *   -# @f$ (1 \times 1 \times 1 \times 1) @f$
-   *      This Blob's diff will simply contain the loss_weight* @f$ \lambda @f$,
-   *      as @f$ \lambda @f$ is the coefficient of this layer's output
-   *      @f$\ell_i@f$ in the overall Net loss
-   *      @f$ E = \lambda_i \ell_i + \mbox{other loss terms}@f$; hence
-   *      @f$ \frac{\partial E}{\partial \ell_i} = \lambda_i @f$.
-   *      (*Assuming that this top Blob is not used as a bottom (input) by any
-   *      other layer of the Net.)
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2)
-   *   -# @f$ (N \times C \times 1 \times 1) @f$
-   *      the features @f$a@f$; Backward fills their diff with
-   *      gradients if propagate_down[0]
-   *   -# @f$ (N \times C \times 1 \times 1) @f$
-   *      the features @f$b@f$; Backward fills their diff with gradients if
-   *      propagate_down[1]
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    #include <vector>
-    
-    #ifdef USE_CUDNN
-template <typename Dtype>
-class CuDNNLCNLayer : public LRNLayer<Dtype> {
+    // CodeGenerator implementation which generates Java code.  If you create your
+// own protocol compiler binary and you want it to support Java output, you
+// can do so by registering an instance of this CodeGenerator with the
+// CommandLineInterface in your main() function.
+class LIBPROTOC_EXPORT JavaGenerator : public CodeGenerator {
  public:
-  explicit CuDNNLCNLayer(const LayerParameter& param)
-      : LRNLayer<Dtype>(param), handles_setup_(false), tempDataSize(0),
-        tempData1(NULL), tempData2(NULL) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNLCNLayer();
+  JavaGenerator();
+  ~JavaGenerator();
     }
     
-    
-    {}  // namespace caffe
-    
-     protected:
-  /**
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the computed outputs @f$
-   *        y = \gamma ^ {\alpha x + \beta}
-   *      @f$
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-    #include <xgboost/logging.h>
-#include 'src/common/random.h'
-#include './xgboost_R.h'
-    
-      float float2[2] = {1.0f, 2.0f};
-  EXPECT_EQ(info.GetWeight(1), 1.0f)
-    << 'When no weights are given, was expecting default value 1';
-  info.SetInfo('weight', float2, xgboost::kFloat32, 2);
-  EXPECT_EQ(info.GetWeight(1), 2.0f);
-    
-    bool SimpleDMatrix::ColBatchIter::Next() {
-  if (data_ >= 1) return false;
-  data_ += 1;
-  return true;
+    static int set_flags(struct ifaddrs* ifaddr) {
+	int fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (fd == -1) {
+		return -1;
+	}
+	ifreq ifr;
+	memset(&ifr, 0, sizeof(ifr));
+	strncpy(ifr.ifr_name, ifaddr->ifa_name, IFNAMSIZ - 1);
+	int rc = ioctl(fd, SIOCGIFFLAGS, &ifr);
+	close(fd);
+	if (rc == -1) {
+		return -1;
+	}
+	ifaddr->ifa_flags = ifr.ifr_flags;
+	return 0;
 }
+    
+    
+    {  return pImage_data;
+}
+    
+    /* get number of leading zeros and fractional part (the bits right after the leading one */
+static OPUS_INLINE void silk_CLZ_FRAC(
+    opus_int32 in,            /* I  input                               */
+    opus_int32 *lz,           /* O  number of leading zeros             */
+    opus_int32 *frac_Q7       /* O  the 7 bits right after the leading one */
+)
+{
+    opus_int32 lzeros = silk_CLZ32(in);
+    }
+    
+    int64_t gettime_ns(clockid_t clock) {
+  if (clock != CLOCK_THREAD_CPUTIME_ID) {
+    return folly::chrono::clock_gettime_ns(clock);
+  }
+    }
+    
+    inline const MemoSlot* ObjectData::memoSlotNativeData(
+  Slot slot, size_t ndiSize
+) const {
+  assertx(slot < m_cls->numMemoSlots());
+  return reinterpret_cast<const MemoSlot*>(
+    reinterpret_cast<const char*>(this) - alignTypedValue(ndiSize)
+  ) - slot - 1;
+}
+    
+      unsigned num1GPages = RuntimeOption::EvalNum1GPagesForSlabs;
+  unsigned num2MPages = RuntimeOption::EvalNum2MPagesForSlabs;
+  if (numNodes > 1) {
+    auto perNode1G = num1GPages / numNodes;
+    if (auto const excessive = num1GPages % numNodes) {
+      Logger::Warning('uneven distribution of %u 1G huge pages '
+                      '(Eval.Num1GPagesForSlabs) to %u NUMA nodes, '
+                      'excessive %u pages not used',
+                      num1GPages, numNodes, excessive);
+    }
+    num1GPages = perNode1G;
+    auto perNode2M = num2MPages / numNodes;
+    if (auto const excessive = num2MPages % numNodes) {
+      Logger::Warning('uneven distribution of %u 2M huge pages '
+                      '(Eval.Num1MPagesForSlabs) to %u NUMA nodes, '
+                      'excessive %u pages not used',
+                      num2MPages, numNodes, excessive);
+    }
+    num2MPages = perNode2M;
+  }
+    
+            for (int cmd_i = 0; cmd_i < cmd_list->CmdBuffer.Size; cmd_i++)
+        {
+            const ImDrawCmd* pcmd = &cmd_list->CmdBuffer[cmd_i];
+            if (pcmd->UserCallback)
+            {
+                // User callback (registered via ImDrawList::AddCallback)
+                pcmd->UserCallback(cmd_list, pcmd);
+            }
+            else
+            {
+                ImVec4 clip_rect = ImVec4(pcmd->ClipRect.x - pos.x, pcmd->ClipRect.y - pos.y, pcmd->ClipRect.z - pos.x, pcmd->ClipRect.w - pos.y);
+                if (clip_rect.x < fb_width && clip_rect.y < fb_height && clip_rect.z >= 0.0f && clip_rect.w >= 0.0f)
+                {
+                    // Apply scissor/clipping rectangle
+                    glScissor((int)clip_rect.x, (int)(fb_height - clip_rect.w), (int)(clip_rect.z - clip_rect.x), (int)(clip_rect.w - clip_rect.y));
+    }
+    }
+    }
+    
+        const GLchar* vertex_shader_glsl_120 =
+        'uniform mat4 ProjMtx;\n'
+        'attribute vec2 Position;\n'
+        'attribute vec2 UV;\n'
+        'attribute vec4 Color;\n'
+        'varying vec2 Frag_UV;\n'
+        'varying vec4 Frag_Color;\n'
+        'void main()\n'
+        '{\n'
+        '    Frag_UV = UV;\n'
+        '    Frag_Color = Color;\n'
+        '    gl_Position = ProjMtx * vec4(Position.xy,0,1);\n'
+        '}\n';
+    
+        // Render
+    ImDrawData              DrawData;                           // Main ImDrawData instance to pass render information to the user
+    ImDrawDataBuilder       DrawDataBuilder;
+    float                   DimBgRatio;                         // 0.0..1.0 animation when fading in a dimming background (for modal window and CTRL+TAB list)
+    ImDrawList              OverlayDrawList;                    // Optional software render of mouse cursors, if io.MouseDrawCursor is set + a few debug overlays
+    ImGuiMouseCursor        MouseCursor;
