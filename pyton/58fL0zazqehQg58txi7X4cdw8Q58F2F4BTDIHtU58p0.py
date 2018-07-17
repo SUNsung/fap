d11bf5e4dee99f6c43260fcd338fc000ca0715c0
SUNsung <weatherius@gmail.com>
2018-07-17 08:20:03 +0300

    Rewrite [58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py] File in: 2018-07-17 08:20:02

--- pyton/58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py
+++ pyton/58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py
@@ -1,117 +1,199 @@
 
         
-            params = {
-        'age_limit': age,
-        'skip_download': True,
-        'writeinfojson': True,
-        'outtmpl': '%(id)s.%(ext)s',
+            # Then we cluster the lines together as blocks
+    # Each block represents a collection of lines that should be sorted
+    # This was done by assuming only links ([...](...)) are meant to be sorted
+    # Clustering is done by indentation
+    blocks = []
+    last_indent = None
+    for line in read_me:
+        s_line = line.lstrip()
+        indent = len(line) - len(s_line)
+    
+    tf_gen_op_libs(
+    op_lib_names = ['mst_ops'],
+)
+    
+    py_test(
+    name = 'sentence_io_test',
+    srcs = ['sentence_io_test.py'],
+    data = ['//syntaxnet:testdata'],
+    deps = [
+        ':dragnn_ops',
+        ':sentence_io',
+        '//syntaxnet:load_parser_ops_py',
+        '//syntaxnet:parser_ops',
+        '//syntaxnet:sentence_pb2_py',
+        '//syntaxnet:test_flags',
+        '@org_tensorflow//tensorflow:tensorflow_py',
+        '@org_tensorflow//tensorflow/core:protos_all_py',
+    ],
+)
+    
+      num_labels = weights.get_shape().as_list()[0]
+  num_source_activations = weights.get_shape().as_list()[1]
+  num_target_activations = weights.get_shape().as_list()[2]
+  check.NotNone(num_labels, 'unknown number of labels')
+  check.NotNone(num_source_activations, 'unknown source activation dimension')
+  check.NotNone(num_target_activations, 'unknown target activation dimension')
+  check.Eq(sources.get_shape().as_list()[2], num_source_activations,
+           'activation mismatch between weights and source tokens')
+  check.Eq(targets.get_shape().as_list()[2], num_target_activations,
+           'activation mismatch between weights and target tokens')
+    
+          roots = digraph_ops.RootPotentialsFromTokens(root, tokens, weights_arc,
+                                                   weights_source)
+    
+      short_to_original = saver_lib.shorten_resource_paths(master_spec)
+  saver_lib.export_master_spec(master_spec, graph)
+  saver_lib.export_to_graph(master_spec, params_path, stripped_path, graph,
+                            export_moving_averages, build_runtime_graph)
+  saver_lib.export_assets(master_spec, short_to_original, stripped_path)
+    
+        shortened_to_original = dragnn_model_saver_lib.shorten_resource_paths(
+        master_spec)
+    
+      The partition function is caluclated via application of the Matrix-Tree
+  theorem; see the following for details:
+    https://en.wikipedia.org/wiki/Kirchhoff%27s_theorem
+    http://www.aclweb.org/anthology/D/D07/D07-1015.pdf
+    
+        styles = {
+        # No corresponding class for the following:
+        #Text:                     '', # class:  ''
+        Whitespace:                'underline #f8f8f8',      # class: 'w'
+        Error:                     '#a40000 border:#ef2929', # class: 'err'
+        Other:                     '#000000',                # class 'x'
     }
-    ydl = YoutubeDL(params)
-    ydl.add_default_info_extractors()
-    json_filename = os.path.splitext(filename)[0] + '.info.json'
-    try_rm(json_filename)
-    ydl.download([url])
-    res = os.path.exists(json_filename)
-    try_rm(json_filename)
-    return res
     
-        def test_youtube_channel_matching(self):
-        assertChannel = lambda url: self.assertMatch(url, ['youtube:channel'])
-        assertChannel('https://www.youtube.com/channel/HCtnHdj3df7iM')
-        assertChannel('https://www.youtube.com/channel/HCtnHdj3df7iM?feature=gb_ch_rec')
-        assertChannel('https://www.youtube.com/channel/HCtnHdj3df7iM/videos')
+    Provides utility functions that are consumed internally by Requests
+which depend on extremely few external helpers (such as compat)
+'''
     
+        @pytest.mark.parametrize(
+        'other, result', (
+            ({'AccePT': 'application/json'}, True),
+            ({}, False),
+            (None, False)
+        )
+    )
+    def test_instance_equality(self, other, result):
+        assert (self.case_insensitive_dict == other) is result
     
-class TestCache(unittest.TestCase):
-    def setUp(self):
-        TEST_DIR = os.path.dirname(os.path.abspath(__file__))
-        TESTDATA_DIR = os.path.join(TEST_DIR, 'testdata')
-        _mkdir(TESTDATA_DIR)
-        self.test_dir = os.path.join(TESTDATA_DIR, 'cache_test')
-        self.tearDown()
+            # Give the Response some context.
+        response.request = req
+        response.connection = self
     
+            req = requests.Request('GET', httpbin('headers'))
+        assert not req.auth
     
-TEST_DIR = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 'swftests')
+        text_200 = (b'HTTP/1.1 200 OK\r\n'
+                b'Content-Length: 0\r\n\r\n')
     
-            return self._extract_nuevo(
-            'http://www.anitube.se/nuevo/econfig.php?key=%s' % key, video_id)
+    if len(sys.argv) == 2 and sys.argv[1] == '--list':
+    print(json.dumps(result))
+elif len(sys.argv) == 3 and sys.argv[1] == '--host':
+    print(json.dumps({}))
+else:
+    print('Need an argument, either --list or --host <host>')
 
     
-            title = self._html_search_regex(r'<title>(.*?)</title>', webpage, 'title')
-        description = self._html_search_regex(
-            r'<div class='description'[^>]*>([^<]+)</div>', webpage, 'description', fatal=False)
-        thumbnail = self._html_search_regex(
-            r'preview_url\s*:\s*\'(.*?)\'', webpage, 'thumbnail', fatal=False)
     
-    
-class CloserToTruthIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?closertotruth\.com/(?:[^/]+/)*(?P<id>[^/?#&]+)'
-    _TESTS = [{
-        'url': 'http://closertotruth.com/series/solutions-the-mind-body-problem#video-3688',
-        'info_dict': {
-            'id': '0_zof1ktre',
-            'display_id': 'solutions-the-mind-body-problem',
-            'ext': 'mov',
-            'title': 'Solutions to the Mind-Body Problem?',
-            'upload_date': '20140221',
-            'timestamp': 1392956007,
-            'uploader_id': 'CTTXML'
-        },
-        'params': {
-            'skip_download': True,
-        },
-    }, {
-        'url': 'http://closertotruth.com/episodes/how-do-brains-work',
-        'info_dict': {
-            'id': '0_iuxai6g6',
-            'display_id': 'how-do-brains-work',
-            'ext': 'mov',
-            'title': 'How do Brains Work?',
-            'upload_date': '20140221',
-            'timestamp': 1392956024,
-            'uploader_id': 'CTTXML'
-        },
-        'params': {
-            'skip_download': True,
-        },
-    }, {
-        'url': 'http://closertotruth.com/interviews/1725',
-        'info_dict': {
-            'id': '1725',
-            'title': 'AyaFr-002',
-        },
-        'playlist_mincount': 2,
-    }]
-    
-        return out
-    
-    
-def test_idna_with_version_attribute(mocker):
-    '''Verify we're actually setting idna version when it should be available.'''
-    mocker.patch('requests.help.idna', new=VersionedPackage('2.6'))
-    assert info()['idna'] == {'version': '2.6'}
+def md5(filename):
+    if not _md5:
+        raise ValueError('MD5 not available.  Possibly running in FIPS mode')
+    return secure_hash(filename, _md5)
 
     
-    # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+    try:
+    import argcomplete
+except ImportError:
+    argcomplete = None
     
-        :param url: URL for the new :class:`Request` object.
-    :param data: (optional) Dictionary (will be form-encoded), bytes, or file-like object to send in the body of the :class:`Request`.
-    :param json: (optional) json data to send in the body of the :class:`Request`.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
+        def construct_mapping(self, node, deep=False):
+        # Most of this is from yaml.constructor.SafeConstructor.  We replicate
+        # it here so that we can warn users when they have duplicate dict keys
+        # (pyyaml silently allows overwriting keys)
+        if not isinstance(node, MappingNode):
+            raise ConstructorError(None, None,
+                                   'expected a mapping node, but found %s' % node.id,
+                                   node.start_mark)
+        self.flatten_mapping(node)
+        mapping = AnsibleMapping()
+    
+        Coercion rules are currently an implementation detail. See the CoerceTest
+    test class in test_statistics for details.
     '''
+    # See http://bugs.python.org/issue24068.
+    assert T is not bool, 'initial type T is bool'
+    # If the types are the same, no need to coerce anything. Put this
+    # first, so that the usual case (no coercion needed) happens as soon
+    # as possible.
+    if T is S:  return T
+    # Mixed int & other coerce to the other type.
+    if S is int or S is bool:  return T
+    if T is int:  return S
+    # If one is a (strict) subclass of the other, coerce to the subclass.
+    if issubclass(S, T):  return S
+    if issubclass(T, S):  return T
+    # Ints coerce to the other type.
+    if issubclass(T, int):  return S
+    if issubclass(S, int):  return T
+    # Mixed fraction & float coerces to float (or float subclass).
+    if issubclass(T, Fraction) and issubclass(S, float):
+        return S
+    if issubclass(T, float) and issubclass(S, Fraction):
+        return T
+    # Any other combination is disallowed.
+    msg = 'don't know how to coerce %s and %s'
+    raise TypeError(msg % (T.__name__, S.__name__))
     
-    # Syntax sugar.
-_ver = sys.version_info
+        def test_big_queue_popright(self):
+        d = deque()
+        append, pop = d.appendleft, d.pop
+        for i in range(BIG):
+            append(i)
+        for i in range(BIG):
+            x = pop()
+            if x != i:
+                self.assertEqual(x, i)
     
-        def has_header(self, name):
-        return name in self._r.headers or name in self._new_headers
+        def close(self):
+        if not self.closed:
+            try:
+                self.skip()
+            finally:
+                self.closed = True
     
-        def __repr__(self):
-        return str(dict(self.items()))
+        def _get_filter(self):
+        return self.filter
+    def _set_filter(self, filter):
+        self.filter = filter
     
-        if hasattr(d, 'items'):
-        d = d.items()
+    @dataclass
+class CV:
+    T_CV4 = ClassVar
+    cv0: ClassVar[int] = 20
+    cv1: ClassVar = 30
+    cv2: T_CV2
+    cv3: T_CV3
+    not_cv4: T_CV4  # When using string annotations, this field is not recognized as a ClassVar.
+    
+        def test_bad_urls(self):
+        for url in self.bad:
+            agent, url = self.get_agent_and_url(url)
+            with self.subTest(url=url, agent=agent):
+                self.assertFalse(self.parser.can_fetch(agent, url))
+    
+        def test_bad_type_arg(self):
+        # The type argument must be a ctypes pointer type.
+        array_type = c_byte * sizeof(c_int)
+        array = array_type()
+        self.assertRaises(TypeError, cast, array, None)
+        self.assertRaises(TypeError, cast, array, array_type)
+        class Struct(Structure):
+            _fields_ = [('a', c_int)]
+        self.assertRaises(TypeError, cast, array, Struct)
+        class MyUnion(Union):
+            _fields_ = [('a', c_int)]
+        self.assertRaises(TypeError, cast, array, MyUnion)
