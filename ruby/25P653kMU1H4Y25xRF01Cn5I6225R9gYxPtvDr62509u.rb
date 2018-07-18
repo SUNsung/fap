d55e4d96df5b37da0a207489ca137d21a20ffa4a
SUNsung <weatherius@gmail.com>
2018-07-18 06:23:37 +0300

    Rewrite [25P653kMU1H4Y25xRF01Cn5I6225R9gYxPtvDr62509u.rb] File in: 2018-07-18 06:23:36

--- ruby/25P653kMU1H4Y25xRF01Cn5I6225R9gYxPtvDr62509u.rb
+++ ruby/25P653kMU1H4Y25xRF01Cn5I6225R9gYxPtvDr62509u.rb
@@ -1,88 +1,66 @@
 
         
-          # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/follow_request
-  def follow_request
-    f = Follow.last
-    NotificationMailer.follow_request(f.target_account, Notification.find_by(activity: f))
+          class CachedFragment
+    def initialize(json)
+      @json = json
+    end
+    def as_json(*_args)
+      @json
+    end
   end
     
-          expect_updated_sign_in_at(user)
-    end
+        REDIRECT_RGX = /http-equiv='refresh'/i
+    NOT_FOUND_RGX = /<title>Not Found<\/title>/
     
-        def compass?
-      defined?(::Compass::Frameworks)
-    end
+        version 'Guide' do
+      self.base_url = 'https://www.tensorflow.org/'
+      self.root_path = 'get_started/get_started'
+      self.initial_paths = %w(
+        programmers_guide/reading_data
+        tutorials/mandelbrot
+        performance/performance_guide
+        deploy/hadoop
+        extend/architecture)
     
-        # .btn { ... } -> @mixin btn { ... }; .btn { @include btn }
-    def extract_mixins_from_selectors(file, selectors_to_mixins)
-      selectors_to_mixins.each do |selector, mixin|
-        file = replace_rules file, Regexp.escape(selector), prefix: false do |selector_css|
-          log_transform '#{selector} { ... } -> @mixin #{mixin} { ... }; #{selector} { @include #{mixin} } ', from: 'extract_mixins_from_selectors'
-          <<-SCSS
-// [converter] extracted from `#{selector}` for libsass compatibility
-@mixin #{mixin} {#{unwrap_rule_block(selector_css)}
-}
-// [converter] extracted as `@mixin #{mixin}` for libsass compatibility
-#{selector} {
-  @include #{mixin};
-}
-          SCSS
-        end
-      end
-      file
-    end
-    
-      # PATCH/PUT /books/1
-  # PATCH/PUT /books/1.json
-  def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
-        format.json { render :show, status: :ok, location: @book }
+            a_split <=> b_split
       else
-        format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-    
-            def repeated_condition?(previous, condition)
-          previous.any? { |c| c.include?(condition) }
-        end
+        a.casecmp(b)
       end
     end
   end
 end
 
     
-            def on_block(node)
-          on_body_of_reduce(node) do |body|
-            void_next = body.each_node(:next).find do |n|
-              n.children.empty? && parent_block_node(n) == node
-            end
+        def to_a
+      @filters.dup
+    end
     
-            # Construct annotated source string (like what we parse)
-        #
-        # Reconstruct a deterministic annotated source string. This is
-        # useful for eliminating semantically irrelevant annotation
-        # ordering differences.
-        #
-        # @example standardization
-        #
-        #     source1 = AnnotatedSource.parse(<<-RUBY)
-        #     line1
-        #     ^ Annotation 1
-        #      ^^ Annotation 2
-        #     RUBY
-        #
-        #     source2 = AnnotatedSource.parse(<<-RUBY)
-        #     line1
-        #      ^^ Annotation 2
-        #     ^ Annotation 1
-        #     RUBY
-        #
-        #     source1.to_s == source2.to_s # => true
-        #
-        # @return [String]
-        def to_s
-          reconstructed = lines.dup
+      it 'does not copy constants defined in the singleton class' do
+    class << @obj
+      CLONE = :clone
+    end
+    
+        # Returns the Sass/SCSS code for the media query list.
+    #
+    # @param options [{Symbol => Object}] An options hash (see {Sass::CSS#initialize}).
+    # @return [String]
+    def to_src(options)
+      queries.map {|q| q.to_src(options)}.join(', ')
+    end
+    
+    class SinatraStaticServer < Sinatra::Base
+    
+        def generate(site)
+      site.write_category_indexes
+    end
+    
+      # Improved version of Liquid's truncate:
+  # - Doesn't cut in the middle of a word.
+  # - Uses typographically correct ellipsis (…) insted of '...'
+  def truncate(input, length)
+    if input.length > length && input[0..(length-1)] =~ /(.+)\b.+$/im
+      $1.strip + ' &hellip;'
+    else
+      input
+    end
+  end
