4c0367972054aa7c65d9efca171673d8f943e634
SUNsung <weatherius@gmail.com>
2018-07-19 01:30:43 +0300

    Rewrite [52.bqbKMhvfOM52N7aY8IXUfb652o3BctdN94k526.0.cpp] File in: 2018-07-19 01:30:42

--- cccccc/52.bqbKMhvfOM52N7aY8IXUfb652o3BctdN94k526.0.cpp
+++ cccccc/52.bqbKMhvfOM52N7aY8IXUfb652o3BctdN94k526.0.cpp
@@ -1,47 +1,420 @@
 
         
-        #include <google/protobuf/compiler/code_generator.h>
-#include <google/protobuf/compiler/csharp/csharp_source_generator_base.h>
+        #include 'atom/browser/net/asar/url_request_asar_job.h'
+#include 'atom/browser/net/js_asker.h'
     
-    SourceGeneratorBase::~SourceGeneratorBase() {
+      // ui::EventRewriter:
+  ui::EventRewriteStatus RewriteEvent(
+      const ui::Event& event,
+      std::unique_ptr<ui::Event>* rewritten_event) override;
+  ui::EventRewriteStatus NextDispatchEvent(
+      const ui::Event& last_event,
+      std::unique_ptr<ui::Event>* new_event) override;
+    
+    #include 'atom/browser/unresponsive_suppressor.h'
+    
+     private:
+  // content::RenderThreadObserver:
+  bool OnControlMessageReceived(const IPC::Message& message) override;
+    
+    bool TtsPlatformImpl::LoadBuiltInTtsExtension(
+    content::BrowserContext* browser_context) {
+  return false;
 }
     
-    TEST(JavaDocCommentTest, Escaping) {
-  EXPECT_EQ('foo /&#42; bar *&#47; baz', EscapeJavadoc('foo /* bar */ baz'));
-  EXPECT_EQ('foo /&#42;&#47; baz', EscapeJavadoc('foo /*/ baz'));
-  EXPECT_EQ('{&#64;foo}', EscapeJavadoc('{@foo}'));
-  EXPECT_EQ('&lt;i&gt;&amp;&lt;/i&gt;', EscapeJavadoc('<i>&</i>'));
-  EXPECT_EQ('foo&#92;u1234bar', EscapeJavadoc('foo\\u1234bar'));
-  EXPECT_EQ('&#64;deprecated', EscapeJavadoc('@deprecated'));
+    #include 'chrome/common/chrome_constants.h'
+    
+    // Generate param traits write methods.
+#include 'ipc/param_traits_write_macros.h'
+namespace IPC {
+#include 'content/nw/src/common/common_message_generator.h'
+}  // namespace IPC
+    
+    void Base::CallSync(const std::string& method,
+                    const base::ListValue& arguments,
+                    base::ListValue* result) {
+  NOTREACHED() << 'Uncatched callAsync in Base'
+               << ' method:' << method
+               << ' arguments:' << arguments;
 }
     
-    ServiceGenerator* ImmutableGeneratorFactory::NewServiceGenerator(
-    const ServiceDescriptor* descriptor) const {
-  return new ImmutableServiceGenerator(descriptor, context_);
+    void Clipboard::Call(const std::string& method,
+                     const base::ListValue& arguments) {
+  if (method == 'Set') {
+    std::string text, type;
+    arguments.GetString(0, &text);
+    arguments.GetString(1, &type);
+    SetText(text);
+  } else if (method == 'Clear') {
+    Clear();
+  } else {
+    NOTREACHED() << 'Invalid call to Clipboard method:' << method
+                 << ' arguments:' << arguments;
+  }
 }
     
-    #include <google/protobuf/compiler/java/java_field.h>
-#include <google/protobuf/compiler/java/java_message_field.h>
+    
+    {  gtk_widget_show(menu_);
+  g_object_ref_sink(G_OBJECT(menu_));
+}
+    
+    void MenuItem::SetTooltip(const std::string& tooltip) {
+  is_modified_ = true;
+  tooltip_ = base::UTF8ToUTF16(tooltip);
+  if (menu_)
+    menu_->UpdateStates();
+}
+    
+    NwAppSetProxyConfigFunction::~NwAppSetProxyConfigFunction() {
+}
+    
+     protected:
+  ~NwAppGetArgvSyncFunction() override;
+    
+    NwClipboardClearSyncFunction::NwClipboardClearSyncFunction() {
+    }
+    
+    namespace extensions {
+    }
     
     
-/*
- * WakeUpLock.h
- *
- *  Created on: 2012-9-28
- *      Author: yerungui
+    {    for (size_t i=0; i<displays.size(); i++) {
+      response->Append(ConvertGfxDisplay(displays[i])->ToValue());
+    }
+    
+    return true;
+  }
+    
+    bool WebSocketClient::is_verify_ssl_enabled() const {
+    }
+    
+    #if defined(MBEDTLS_KEY_EXCHANGE_RSA_PSK_ENABLED) &&                   \
+    ( !defined(MBEDTLS_RSA_C) || !defined(MBEDTLS_X509_CRT_PARSE_C) || \
+      !defined(MBEDTLS_PKCS1_V15) )
+#error 'MBEDTLS_KEY_EXCHANGE_RSA_PSK_ENABLED defined, but not all prerequisites'
+#endif
+    
+    	struct btVehicleWheelContactPoint {
+		PhysicsDirectBodyState *m_s;
+		PhysicsBody *m_body1;
+		Vector3 m_frictionPositionWorld;
+		Vector3 m_frictionDirectionWorld;
+		real_t m_jacDiagABInv;
+		real_t m_maxImpulse;
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    
+      String getResults(
+    Array &headers,
+    int &code,
+    int64_t timeout_ms
+  );
+    
+    constexpr int Vframe::Top;
+    
+    #include <atomic>
+#include 'stddef.h'
+    
+    
+    {    auto const need = nBytes - available();
+    auto const amt = std::min(std::max(m_size + need, 2 * m_size), m_maxGrow);
+    if (amt < m_size + need) return false;
+    if (!m_destBuf) {
+      m_destBuf.reset((Address)::malloc(amt));
+      ::memcpy(m_destBuf.get(), m_destBase, used());
+    } else {
+      m_destBuf.reset((Address)::realloc(m_destBuf.release(), amt));
+    }
+    if (!m_destBuf) reportMallocError(amt);
+    m_destBase = m_destBuf.get();
+    m_size = amt;
+    return true;
+  }
+    
+      if (src.equivLocals.size() < dst.equivLocals.size()) {
+    for (auto i = src.equivLocals.size(); i < dst.equivLocals.size(); ++i) {
+      if (dst.equivLocals[i] != NoLocalId) {
+        killLocEquiv(dst, i);
+        changed = true;
+      }
+    }
+    dst.equivLocals.resize(src.equivLocals.size());
+  }
+    
+    
+    {    switch (mkey.mcode) {
+      case MEL: case MPL:
+        folly::toAppend(':', local_string(func, mkey.local), &ret);
+        break;
+      case MEC: case MPC:
+        folly::toAppend(':', mkey.idx, &ret);
+        break;
+      case MEI:
+        folly::toAppend(':', mkey.int64, &ret);
+        break;
+      case MET: case MPT: case MQT:
+        folly::toAppend(
+          ':\'', escapeStringForCPP(mkey.litstr->data(), mkey.litstr->size()),
+          ''', &ret
+        );
+        break;
+      case MW:
+        break;
+    }
+  };
+    
+    
+    {  return false;
+}
+    
+    IniSettingMap IniSetting::FromStringAsMap(const std::string& ini,
+                                          const std::string& filename) {
+  Lock lock(s_mutex); // ini parser is not thread-safe
+  // We are parsing something new, so reset this flag
+  s_config_is_a_constant = false;
+  SystemParserCallback cb;
+  Variant parsed;
+  zend_parse_ini_string(ini, filename, NormalScanner, cb, &parsed);
+  if (parsed.isNull()) {
+    return uninit_null();
+  }
+  // We have the final values for our ini settings.
+  // Unbox everything so that we have no more references in the map since we do
+  // things that might require us not to have references
+  // (e.g. calling Variant::SetEvalScalar(), which will assert if an
+  // arraydata's elements are KindOfRef)
+  std::set<ArrayData*> seen;
+  bool use_defaults = false;
+  Variant ret = Unbox(parsed, seen, use_defaults, empty_string());
+  if (use_defaults) {
+    return uninit_null();
+  }
+  return ret;
+}
+    
+        template <typename ElementType>
+    void Value::CopyVariableValueToCSCSparse(size_t sequenceLength, std::vector<SparseIndexType>& colStarts, std::vector<SparseIndexType>& rowIndices, std::vector<ElementType>& nonZeroValues, size_t& numNonZeroValues)
+    {
+        // All sanity check has been done in ValidateSparseCSCAndGetIndexSizes().
+        NDArrayViewPtr cpuView;
+        if (Device().Type() == DeviceKind::GPU)
+        {
+            // Todo: GPUSparseMatrix to CPUSparseMatrix is not implemented in matrix, as a workaround the dense matrix is used as intermediate presentation.
+            // However, it is possible that data value very close to 0 could treated as 0 after transformation between dense and sparse.
+            auto cpuDenseView = MakeSharedObject<NDArrayView>(GetDataType(), StorageFormat::Dense, Shape(), DeviceDescriptor::CPUDevice());
+            cpuDenseView->CopyFrom(*Data());
+            cpuView = MakeSharedObject<NDArrayView>(GetDataType(), GetStorageFormat(), Shape(), DeviceDescriptor::CPUDevice());
+            cpuView->CopyFrom(*cpuDenseView);
+        }
+        else
+            cpuView = Data();
+    }
+    
+        // insert the node in the network
+    AddNodeToNet(newNode);
+    
+    template <class ElemType>
+class BinaryReader : public DataReaderBase
+{
+    size_t m_mbSize;           // size of minibatch requested
+    size_t m_mbStartSample;    // starting sample # of the next minibatch
+    size_t m_epochSize;        // size of an epoch
+    size_t m_epoch;            // which epoch are we on
+    size_t m_epochStartSample; // the starting sample for the epoch
+    size_t m_totalSamples;     // number of samples in the dataset
+    bool m_partialMinibatch;   // a partial minibatch is allowed
+    MBLayoutPtr m_pMBLayout;
+    }
+    
+    
+    {    // SaveMapping - save a map into the file
+    // saveId - name of the section to save into (section:subsection format)
+    // labelMapping - map we are saving to the file
+    virtual void SaveMapping(std::wstring saveId, const std::map<LabelIdType, LabelType>& labelMapping);
+    virtual bool SupportMultiUtterances() const 
+    {
+        return false;
+    };
+};
+    
+    // ===========================================================================
+// float4 -- wrapper around the rather ugly SSE intrinsics for float[4]
+//
+// Do not use the intrinsics outside anymore; instead add all you need into this class.
+//
+// MSDN links:
+// basic: http://msdn.microsoft.com/en-us/library/x5c07e2a%28v=VS.80%29.aspx
+// load/store: (add this)
+// newer ones: (seems no single list available)
+// ===========================================================================
+    
+    vector<wstring> /*IConfigRecord::*/ ComputationNodeBase::GetMemberIds() const
+{
+    return vector<wstring>{ L'name', L'operation', L'dim', L'dims', /*L'tag', */L'inputs', OperationName() + L'Args' };
+}
+    
+    extern JSClass  *jsb_cocos2d_Physics3DConeTwistConstraint_class;
+extern JSObject *jsb_cocos2d_Physics3DConeTwistConstraint_prototype;
+    
+    
+    
+    
+    {
+    {			float minX = -6.0f;
+			float maxX = 0.0f;
+			float minY = 4.0f;
+			float maxY = 6.0f;
+			
+			for (int32 i = 0; i < 400; ++i)
+			{
+				b2BodyDef bd;
+				bd.type = b2_dynamicBody;
+				bd.position = b2Vec2(RandomFloat(minX,maxX),RandomFloat(minY,maxY));
+				b2Body* body = m_world->CreateBody(&bd);
+				body->CreateFixture(&shape, 0.01f);
+			}
+		}
+		
+		{
+			b2PolygonShape shape;
+			shape.SetAsBox(1.5f, 1.5f);
+			b2BodyDef bd;
+			bd.type = b2_dynamicBody;
+			bd.position.Set(-40.0f,5.0f);
+			bd.bullet = true;
+			b2Body* body = m_world->CreateBody(&bd);
+			body->CreateFixture(&shape, 1.0f);
+			body->SetLinearVelocity(b2Vec2(150.0f, 0.0f));
+		}
+	}
+    
+    			b2FixtureDef fd;
+			fd.shape = &shape;
+			fd.density = 20.0f;
+			fd.friction = 0.2f;
+    
+    struct ExceptionInfo {
+  const std::type_info* type{nullptr};
+  // The values in frames are IP (instruction pointer) addresses.
+  // They are only filled if the low-level exception tracer library is
+  // linked in or LD_PRELOADed.
+  std::vector<uintptr_t> frames; // front() is top of stack
+};
+    
+      // Not yet built.  Is it currently in progress?
+  if (creating_thread_.load(std::memory_order_acquire) != std::thread::id()) {
+    return true;
+  }
+    
+    
+    {  bool cas(T& u, T& v) {
+    Node* n = new Node(v);
+    hazptr_holder<Atom> hptr;
+    Node* p;
+    while (true) {
+      p = hptr.get_protected(node_);
+      if (p->val_ != u) {
+        delete n;
+        return false;
+      }
+      if (node_.compare_exchange_weak(
+              p, n, std::memory_order_relaxed, std::memory_order_release)) {
+        break;
+      }
+    }
+    hptr.reset();
+    p->retire();
+    return true;
+  }
+};
+    
+    
+    {  auto const slack = currentCapacity - currentSize;
+  if (slack * 2 > currentSize) {
+    // Too much slack, malloc-copy-free cycle:
+    auto const result = checkedMalloc(newCapacity);
+    std::memcpy(result, p, currentSize);
+    free(p);
+    return result;
+  }
+  // If there's not too much slack, we realloc in hope of coalescing
+  return checkedRealloc(p, newCapacity);
+}
+    
+    FOLLY_ALWAYS_INLINE int __builtin_ctzll(unsigned long long x) {
+  unsigned long index;
+  return int(_BitScanForward64(&index, x) ? index : 64);
+}
+    
+      argumentEvaluated = false;
+  XLOGC_IF(DBG1, true, 'xlog format string %d', getValue());
+  ASSERT_EQ(1, messages.size());
+  EXPECT_TRUE(argumentEvaluated);
+  messages.clear();
+    
+    /*
+ * A QuantileEstimator that buffers writes for 1 second.
  */
+template <typename ClockT = std::chrono::steady_clock>
+class SimpleQuantileEstimator {
+ public:
+  using TimePoint = typename ClockT::time_point;
+    }
     
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
+    struct MockClock {
+ public:
+  using duration = std::chrono::steady_clock::duration;
+  using time_point = std::chrono::steady_clock::time_point;
+    }
     
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
+    #include <cmath>
+#include <utility>
+#include 'Eigen/Dense'
+#include 'Eigen/SVD'
     
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
+    #include 'modules/common/math/box2d.h'
+#include 'modules/planning/common/obstacle.h'
+#include 'modules/planning/common/reference_line_info.h'
+#include 'modules/planning/common/trajectory/discretized_trajectory.h'
+#include 'modules/planning/lattice/behavior/path_time_graph.h'
+    
+    void Box2d::InitCorners() {
+  const double dx1 = cos_heading_ * half_length_;
+  const double dy1 = sin_heading_ * half_length_;
+  const double dx2 = sin_heading_ * half_width_;
+  const double dy2 = -cos_heading_ * half_width_;
+  corners_.clear();
+  corners_.emplace_back(center_.x() + dx1 + dx2, center_.y() + dy1 + dy2);
+  corners_.emplace_back(center_.x() + dx1 - dx2, center_.y() + dy1 - dy2);
+  corners_.emplace_back(center_.x() - dx1 - dx2, center_.y() - dy1 - dy2);
+  corners_.emplace_back(center_.x() - dx1 + dx2, center_.y() - dy1 + dy2);
+    }
+    
+    void Polygon2d::ExtremePoints(const double heading, Vec2d *const first,
+                              Vec2d *const last) const {
+  CHECK_GE(points_.size(), 3);
+  CHECK_NOTNULL(first);
+  CHECK_NOTNULL(last);
+    }
+    
+      /**
+   * @brief Check if a line segment has overlap with this polygon.
+   * @param line_segment The target line segment. To check if it has
+   *        overlap with this polygon.
+   * @return Whether the target line segment has overlap with this
+   *         polygon or not.
+   */
+  bool HasOverlap(const LineSegment2d &line_segment) const;
+    
+    
+    {  std::unordered_map<std::string, PathTimeObstacle> path_time_obstacle_map_;
+  std::vector<PathTimeObstacle> path_time_obstacles_;
+  std::vector<SLBoundary> static_obs_sl_boundaries_;
+};
+    
+     private:
+  template <typename SensorMsgsImage>
+  void OnImage(const SensorMsgsImage &image);
+    
+    #include 'modules/perception/proto/traffic_light/recognizer_config.pb.h'
