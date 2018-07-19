338830b00326e2a9f630a4309f4efcc618cecd2f
SUNsung <weatherius@gmail.com>
2018-07-19 15:43:33 +0300

    Rewrite [27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp] File in: 2018-07-19 15:43:33

--- cccccc/27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp
+++ cccccc/27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp
@@ -1,170 +1,108 @@
 
         
-          taskbar->SetOverlayIcon(hWnd, icon.get(), L'Status');
-#elif defined(OS_LINUX)
-  views::LinuxUI* linuxUI = views::LinuxUI::instance();
-  if (linuxUI == NULL) {
-    error_ = 'LinuxUI::instance() is NULL';
-    return RespondNow(Error(error_));
-  }
-  SetDeskopEnvironment();
-  linuxUI->SetDownloadCount(atoi(badge.c_str()));
-#else
-  error_ = 'NwCurrentWindowInternalSetBadgeLabelFunction NOT Implemented'
-  NOTIMPLEMENTED() << error_;
-  return RespondNow(Error(error_));
+        
+    {#if GOOGLE_CUDA
+// A special GPU kernel for int32.
+// TODO(b/25387198): Also enable int32 in device memory. This kernel
+// registration requires all int32 inputs and outputs to be in host memory.
+REGISTER_KERNEL_BUILDER(Name('Mod')
+                            .Device(DEVICE_GPU)
+                            .HostMemory('x')
+                            .HostMemory('y')
+                            .HostMemory('z')
+                            .TypeConstraint<int32>('T'),
+                        BinaryOp<CPUDevice, functor::safe_mod<int32>>);
+REGISTER_KERNEL_BUILDER(Name('TruncateMod')
+                            .Device(DEVICE_GPU)
+                            .HostMemory('x')
+                            .HostMemory('y')
+                            .HostMemory('z')
+                            .TypeConstraint<int32>('T'),
+                        BinaryOp<CPUDevice, functor::safe_mod<int32>>);
 #endif
-  return RespondNow(NoArguments());
-}
-  
-ExtensionFunction::ResponseAction
-NwCurrentWindowInternalRequestAttentionInternalFunction::Run() {
-  EXTENSION_FUNCTION_VALIDATE(args_);
-  int count;
-  EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(0, &count));
-#if defined(OS_WIN)
-  FLASHWINFO fwi;
-  fwi.cbSize = sizeof(fwi);
-  fwi.hwnd = getHWND(getAppWindow(this));
-  if (fwi.hwnd == NULL) {
-    error_ = kNoAssociatedAppWindow;
-    LOG(ERROR) << error_;
-    return RespondNow(Error(error_));
-  }
-  if (count != 0) {
-    fwi.dwFlags = FLASHW_ALL;
-    fwi.uCount = count < 0 ? 4 : count;
-    fwi.dwTimeout = 0;
-  }
-  else {
-    fwi.dwFlags = FLASHW_STOP;
-  }
-  FlashWindowEx(&fwi);
-#elif defined(OS_LINUX) || defined(OS_MACOSX)
-  AppWindow* window = getAppWindow(this);
-  if (!window) {
-    error_ = kNoAssociatedAppWindow;
-    return RespondNow(Error(error_));
-  }
-  window->GetBaseWindow()->FlashFrame(count);
-#endif
-  return RespondNow(NoArguments());
-}
-  
-ExtensionFunction::ResponseAction
-NwCurrentWindowInternalSetProgressBarFunction::Run() {
-  EXTENSION_FUNCTION_VALIDATE(args_);
-  double progress;
-  EXTENSION_FUNCTION_VALIDATE(args_->GetDouble(0, &progress));
-#if defined(OS_WIN)
-  Microsoft::WRL::ComPtr<ITaskbarList3> taskbar;
-  HRESULT result = ::CoCreateInstance(CLSID_TaskbarList, nullptr,
-    CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&taskbar));
-    
-    // Generate destructors.
-#include 'ipc/struct_destructor_macros.h'
-#include 'content/nw/src/common/common_message_generator.h'
-    
-    
-    {}  // namespace remote
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(Clipboard);
-};
-    
-    #endif  // CONTENT_NW_SRC_API_MENU_MENU_DELEGATE_H_
+}  // namespace tensorflow
 
     
-    #include 'content/nw/src/api/menuitem/menuitem.h'
+      // TODO(josh11b): Implement serializing and restoring the state.
     
-    void MenuItem::OnClick(GtkWidget* widget) {
-  if (block_active_)
-    return;
-    }
+    #include 'tensorflow/cc/ops/const_op.h'
+#include 'tensorflow/cc/ops/image_ops.h'
+#include 'tensorflow/cc/ops/nn_ops.h'
+#include 'tensorflow/cc/ops/sendrecv_ops.h'
+#include 'tensorflow/cc/ops/standard_ops.h'
+#include 'tensorflow/core/framework/tensor_testutil.h'
+#include 'tensorflow/core/lib/core/status_test_util.h'
+#include 'tensorflow/core/platform/test.h'
+#include 'tensorflow/core/platform/test_benchmark.h'
+#include 'tensorflow/core/public/session.h'
+#include 'tensorflow/tools/graph_transforms/transform_utils.h'
     
-        protected:
-      ~NwScreenInitEventListenersFunction() override {}
-      DECLARE_EXTENSION_FUNCTION('nw.Screen.initEventListeners', UNKNOWN)
+    #define THPDoubleStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPDoubleStorageClass)
+#define THPFloatStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPFloatStorageClass)
+#define THPHalfStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPFloatStorageClass)
+#define THPLongStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPLongStorageClass)
+#define THPIntStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPIntStorageClass)
+#define THPShortStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPShortStorageClass)
+#define THPCharStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPCharStorageClass)
+#define THPByteStorage_Check(obj) \
+    PyObject_IsInstance(obj, THPByteStorageClass)
     
-                shared_ptr<ComputationNode<ElemType>> runMeanNode = static_pointer_cast<ComputationNode<ElemType>>(runMeanParameterPtr);
-            shared_ptr<ComputationNode<ElemType>> runStdNode  = static_pointer_cast<ComputationNode<ElemType>>(runStdParameterPtr);
+    bool object_exists(const char *name) {
+  int fd = shm_open(name, O_RDONLY, 0);
+  if (fd >= 0) {
+    close(fd);
+    return true;
+  } else {
+    return false;
+  }
+}
     
-                for (size_t i = 0; i < nodes.size(); i++)
-            {
-                // get the computation node
-                auto cnNode = ComputationNode<ElemType>::FromVoidPtr(nodes[i]->GetEvalValue());
-    }
     
-    // helper to remove all existing Output nodes and replace them by a new given set
-static void PatchOutputNodes(const ComputationNetworkPtr& net, const ConfigArray& outputNodeNames, vector<wstring>& outputNodeNamesVector)
+    {  if (free_b) THDTensor_(free)(b);
+}
+    
+    	XLoggerInfo xlog_info;
+	gettimeofday(&xlog_info.timeval, NULL);
+	xlog_info.level = (TLogLevel)_level;
+	xlog_info.line = (int)_line;
+	xlog_info.pid = (int)_pid;
+	xlog_info.tid = LONGTHREADID2INT(_tid);
+	xlog_info.maintid = LONGTHREADID2INT(_maintid);
+    
+        _packlen = ntoh(*(T*)_rawbuf);
+    
+    #include <stdint.h>
+#include <cstddef>
+#include <list>
+    
+    
+Test_Spy_Sample::Test_Spy_Sample()
 {
-    // clear out current list of outputNodes
-    while (!net->OutputNodes().empty())
-        net->RemoveFromNodeGroup(L'output', net->OutputNodes().back());
-    // and insert the desired nodes instead
-    for (wstring name : outputNodeNames)
-    {
-        if (!net->NodeNameExists(name))
-        {
-            fprintf(stderr, 'PatchOutputNodes: No node named '%ls'; skipping\n', name.c_str());
-            continue;
-        }
-        outputNodeNamesVector.push_back (name);
-        let& node = net->GetNodeFromName(name);
-        net->AddToNodeGroup(L'output', node);
-    }
+    SPY_ATTACH_CLASS(NULL);
 }
     
-    namespace boost {
-namespace asio {
-namespace detail {
+    //============================================================================
+// Name        : has_member.h
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+    class ScopedJstring {
+  public:
+    ScopedJstring(JNIEnv* _env, jstring _jstr);
+    ScopedJstring(JNIEnv* _env, const char* _char);
     }
-    }
-    }
-    
-    #ifndef BOOST_ASIO_DETAIL_HANDLER_ALLOC_HELPERS_HPP
-#define BOOST_ASIO_DETAIL_HANDLER_ALLOC_HELPERS_HPP
-    
-      STDMETHODIMP Buffer(byte** value)
-  {
-    *value = bytes_;
-    return S_OK;
-  }
-    
-      folly::Optional<T> try_take_for(std::chrono::milliseconds time) override {
-    T item;
-    while (true) {
-      if (nonBlockingTake(item)) {
-        return std::move(item);
-      }
-      if (!sem_.try_wait_for(time)) {
-        return folly::none;
-      }
-    }
-  }
-    
-      T take() override {
-    T item;
-    while (!queue_.try_dequeue(item)) {
-      sem_.wait();
-    }
-    return item;
-  }
-    
-    namespace folly {
-    }
-    
-      void timeoutExpired() noexcept override {
-    // Don't need Promise anymore, break the circular reference
-    auto promise = stealPromise();
-    if (!promise.isFulfilled()) {
-      promise.setValue();
-    }
-  }
-    
-    void IOThreadPoolExecutor::add(Func func) {
-  add(std::move(func), std::chrono::milliseconds(0));
-}
-    
-    /* using override */ using namespace folly::portability::fcntl;
-#endif
