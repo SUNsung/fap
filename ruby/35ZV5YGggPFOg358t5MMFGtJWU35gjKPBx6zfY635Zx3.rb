958dfcb3a948f37c05d71b34661a8c00e67d1a1c
SUNsung <weatherius@gmail.com>
2018-07-19 05:32:23 +0300

    Rewrite [35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb] File in: 2018-07-19 05:32:22

--- ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
+++ ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
@@ -1,69 +1,38 @@
 
         
-              when :login_fail
-    
-          case matched
-      when :webhost
-        sessions[s[:session]].merge!({k => matches})
-        if(s[:get])
-          print_status('HTTP GET: #{s[:session]} http://#{s[:webhost]}#{s[:get]}')
-          sessions.delete(s[:session])
-          return
-        end
-      when nil
-        # No matches, no saved state
-      end # end case matched
-    end # end of each_key
-  end # end of parse
-end # end of URL sniffer
-    
-    
-    {	if ln =~ /;(read|write)_(handle|filename)=/
-		parts = ln.split(' ')
-		if (parts[0] == 'mov')
-			parts2 = parts[2].split('=')
-			label = parts2[0]
-			label.slice!(0,1)
-			old = parts2[1]
-			new = addrs[label]
-			#puts '%32s: %s -> %x' % [label, old, new]
-			replaces << [label, old, new.to_s(16)]
-		end
-	end
-}
-    
-    classNames = [ 'HelloWorld1', 'HelloWorld2' ]
-    
-            post '/', :file => Rack::Test::UploadedFile.new(temp_file.path), :other => '<bar>'
-        expect(body).to eq('_escaped_params_tmp_file\nhello world\n&lt;bar&gt;')
-      ensure
-        File.unlink(temp_file.path)
+          def resource
+    @resource ||=
+      if params[:project_id].present?
+        Project.find(params[:project_id])
+      elsif params[:namespace_id].present?
+        Group.find(params[:namespace_id])
       end
-    end
   end
-end
-
     
-          it 'should remain unchanged as ASCII-8BIT' do
-        body = @app.call({ 'PATH_INFO' => '/'.encode('ASCII-8BIT') })[2][0]
-        expect(body).to eq('ASCII-8BIT')
-      end
-    end
+    if ARGV.count < 2
+  abort 'Usage: #$0 pidfile command [args...]'
+end
+    
+        @services = current_user.services.reorder(table_sort).page(params[:page])
+    
+      # POST /resource/confirmation
+  def create
+    self.resource = resource_class.send_confirmation_instructions(resource_params)
+    yield resource if block_given?
+    
+      # POST /resource/unlock
+  def create
+    self.resource = resource_class.send_unlock_instructions(resource_params)
+    yield resource if block_given?
+    
+          html_filters.push 'coffeescript/clean_html_v1', 'coffeescript/entries_v1', 'title'
+    
+        options[:only] = BOOKS.map { |s| 'book.#{s}.html' }
+    
+      def text_url
+    object.local? ? medium_url(object) : nil
   end
-end
-
     
-      it 'accepts requests with a changing Version header'do
-    session = {:foo => :bar}
-    get '/', {}, 'rack.session' => session, 'HTTP_VERSION' => '1.0'
-    get '/', {}, 'rack.session' => session, 'HTTP_VERSION' => '1.1'
-    expect(session[:foo]).to eq(:bar)
+      def updated
+    object.updated_at.iso8601
   end
-end
-
-    
-      # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = :random
