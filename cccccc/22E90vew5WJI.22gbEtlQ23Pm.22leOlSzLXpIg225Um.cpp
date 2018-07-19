ac75b97edd902f1f7dcd820e20f4978e3bd75ad0
SUNsung <weatherius@gmail.com>
2018-07-19 06:41:16 +0300

    Rewrite [22E90vew5WJI.22gbEtlQ23Pm.22leOlSzLXpIg225Um.cpp] File in: 2018-07-19 06:41:15

--- cccccc/22E90vew5WJI.22gbEtlQ23Pm.22leOlSzLXpIg225Um.cpp
+++ cccccc/22E90vew5WJI.22gbEtlQ23Pm.22leOlSzLXpIg225Um.cpp
@@ -1,265 +1,239 @@
 
         
-        class SILDebuggerClient;
+          bool Register(const ui::Accelerator& accelerator,
+                const base::Closure& callback);
+  bool IsRegistered(const ui::Accelerator& accelerator);
+  void Unregister(const ui::Accelerator& accelerator);
+  void UnregisterAll();
     
-    #pragma mark - NSData verification
+      // URLRequestJob:
+  void GetResponseInfo(net::HttpResponseInfo* info) override;
     
-    /// Index the given module and store the results to \p indexStorePath.
-///
-/// \param module The module to index.
-///
-/// \param indexUnitTokens A list of unique identifiers for the index units to
-/// be written. This may either be one unit per source file of \p module, or it
-/// may be a single unit, in which case all the index information will be
-/// combined into a single unit.
-///
-/// \param moduleUnitToken A unique identifier for this module unit in the form
-/// of a file path. Only used if \p indexUnitTokens are specified for each
-/// source file, otherwise the single \p indexUnitTokens value is used instead.
-///
-/// \param indexStorePath The location to write the indexing data to.
-///
-/// \param indexSystemModules If true, emit index data for imported serialized
-/// swift system modules.
-///
-/// \param isDebugCompilation true for non-optimized compiler invocation.
-///
-/// \param targetTriple The target for this compilation.
-///
-/// \param dependencyTracker The set of dependencies seen while building.
-bool indexAndRecord(ModuleDecl *module, ArrayRef<std::string> indexUnitTokens,
-                    StringRef moduleUnitToken, StringRef indexStorePath,
-                    bool indexSystemModules, bool isDebugCompilation,
-                    StringRef targetTriple,
-                    const DependencyTracker &dependencyTracker);
-// FIXME: indexUnitTokens could be StringRef, but that creates an impedance
-// mismatch in the caller.
-    
-      Optional<const swift::markup::Paragraph *> getBrief() const {
-    return Parts.Brief;
-  }
-    
-    #include 'swift/Basic/LLVM.h'
-#include 'clang/Index/IndexSymbol.h'
-#include 'llvm/ADT/SmallString.h'
-    
-      /// Returns the buffer ID for the specified *valid* location.
-  ///
-  /// Because a valid source location always corresponds to a source buffer,
-  /// this routine always returns a valid buffer ID.
-  unsigned findBufferContainingLoc(SourceLoc Loc) const;
-    
-    // On macOS and iOS, swift_once is implemented using GCD.
-// The compiler emits an inline check matching the barrier-free inline fast
-// path of dispatch_once(). See SwiftTargetInfo.OnceDonePredicateValue.
-    
-    void indexDeclContext(DeclContext *DC, IndexDataConsumer &consumer);
-void indexSourceFile(SourceFile *SF, StringRef hash,
-                     IndexDataConsumer &consumer);
-void indexModule(ModuleDecl *module, StringRef hash,
-                 IndexDataConsumer &consumer);
-    
-    class AbstractionPattern;
-class Initialization;
-class RValue;
-class SILGenFunction;
-class SGFContext;
-class CalleeTypeInfo;
-    
-      /// Copy constructor.
-  LSBase(const LSBase &RHS) {
-    Base = RHS.Base;
-    Kind = RHS.Kind;
-    Path = RHS.Path;
-  }
-    
-    #include 'llvm/ADT/ArrayRef.h'
-    
-    inline void RowSet::Clear() {
-  rows_.clear(); size_ = 0;
-}
-    
-    DMatrix* DMatrix::Create(dmlc::Parser<uint32_t>* parser,
-                         const std::string& cache_prefix) {
-  if (cache_prefix.length() == 0) {
-    std::unique_ptr<data::SimpleCSRSource> source(new data::SimpleCSRSource());
-    source->CopyFrom(parser);
-    return DMatrix::Create(std::move(source), cache_prefix);
-  } else {
-#if DMLC_ENABLE_STD_THREAD
-    if (!data::SparsePageSource::CacheExist(cache_prefix)) {
-      data::SparsePageSource::Create(parser, cache_prefix);
+    namespace atom {
     }
-    std::unique_ptr<data::SparsePageSource> source(new data::SparsePageSource(cache_prefix));
-    return DMatrix::Create(std::move(source), cache_prefix);
-#else
-    LOG(FATAL) << 'External memory is not enabled in mingw';
-    return nullptr;
-#endif
+    
+      // Returns whether current process is browser process, currently we detect it
+  // by checking whether current has used V8 Lock, but it might be a bad idea.
+  static inline bool IsBrowserProcess() { return v8::Locker::IsActive(); }
+    
+    #include 'ui/gfx/geometry/rect.h'
+    
+      // This message is sent before a tab has been closed.  The source is a
+  // Source<NavigationController> with a pointer to the controller for the
+  // closed tab.  No details are expected.
+  //
+  // See also content::NOTIFICATION_WEB_CONTENTS_DESTROYED, which is sent when
+  // the WebContents containing the NavigationController is destroyed.
+  NOTIFICATION_TAB_CLOSING,
+    
+    #endif  // CHROME_BROWSER_CHROME_PROCESS_FINDER_WIN_H_
+
+    
+      virtual ~DesktopMediaList() {}
+    
+    
+    {}  // namespace extensions
+    
+    #endif  // ATOM_COMMON_COLOR_UTIL_H_
+
+    
+    // Generate param traits read methods.
+#include 'ipc/param_traits_read_macros.h'
+namespace IPC {
+#include 'content/nw/src/common/common_message_generator.h'
+}  // namespace IPC
+    
+    EventListener::~EventListener() {
+  for (std::map<int, BaseEvent*>::iterator i = listerners_.begin(); i != listerners_.end(); i++) {
+    delete i->second;
   }
 }
     
-          jclass batchClass = jenv->GetObjectClass(batch);
-      jlongArray joffset = (jlongArray)jenv->GetObjectField(
-          batch, jenv->GetFieldID(batchClass, 'rowOffset', '[J'));
-      jfloatArray jlabel = (jfloatArray)jenv->GetObjectField(
-          batch, jenv->GetFieldID(batchClass, 'label', '[F'));
-      jfloatArray jweight = (jfloatArray)jenv->GetObjectField(
-          batch, jenv->GetFieldID(batchClass, 'weight', '[F'));
-      jintArray jindex = (jintArray)jenv->GetObjectField(
-          batch, jenv->GetFieldID(batchClass, 'featureIndex', '[I'));
-      jfloatArray jvalue = (jfloatArray)jenv->GetObjectField(
-        batch, jenv->GetFieldID(batchClass, 'featureValue', '[F'));
-      XGBoostBatchCSR cbatch;
-      cbatch.size = jenv->GetArrayLength(joffset) - 1;
-      cbatch.offset = reinterpret_cast<jlong *>(
-          jenv->GetLongArrayElements(joffset, 0));
-      if (jlabel != nullptr) {
-        cbatch.label = jenv->GetFloatArrayElements(jlabel, 0);
-        CHECK_EQ(jenv->GetArrayLength(jlabel), static_cast<long>(cbatch.size))
-            << 'batch.label.length must equal batch.numRows()';
-      } else {
-        cbatch.label = nullptr;
-      }
-      if (jweight != nullptr) {
-        cbatch.weight = jenv->GetFloatArrayElements(jweight, 0);
-        CHECK_EQ(jenv->GetArrayLength(jweight), static_cast<long>(cbatch.size))
-            << 'batch.weight.length must equal batch.numRows()';
-      } else {
-        cbatch.weight = nullptr;
-      }
-      long max_elem = cbatch.offset[cbatch.size];
-      cbatch.index = (int*) jenv->GetIntArrayElements(jindex, 0);
-      cbatch.value = jenv->GetFloatArrayElements(jvalue, 0);
     
-      bool Read(SparsePage* page,
-            dmlc::SeekStream* fi,
-            const std::vector<bst_uint>& sorted_index_set) override {
-    if (!fi->Read(&disk_offset_)) return false;
-    // setup the offset
-    page->offset.clear();
-    page->offset.push_back(0);
-    for (unsigned int fid : sorted_index_set) {
-      CHECK_LT(fid + 1, disk_offset_.size());
-      size_t size = disk_offset_[fid + 1] - disk_offset_[fid];
-      page->offset.push_back(page->offset.back() + size);
-    }
-    page->data.resize(page->offset.back());
-    // read in the data
-    size_t begin = fi->Tell();
-    size_t curr_offset = 0;
-    for (size_t i = 0; i < sorted_index_set.size();) {
-      bst_uint fid = sorted_index_set[i];
-      if (disk_offset_[fid] != curr_offset) {
-        CHECK_GT(disk_offset_[fid], curr_offset);
-        fi->Seek(begin + disk_offset_[fid] * sizeof(Entry));
-        curr_offset = disk_offset_[fid];
-      }
-      size_t j, size_to_read = 0;
-      for (j = i; j < sorted_index_set.size(); ++j) {
-        if (disk_offset_[sorted_index_set[j]] == disk_offset_[fid] + size_to_read) {
-          size_to_read += page->offset[j + 1] - page->offset[j];
-        } else {
-          break;
-        }
-      }
-    }
-    }
-    
-    void SparsePageWriter::PushWrite(std::shared_ptr<SparsePage>&& page) {
-  qworkers_[clock_ptr_].Push(std::move(page));
-  clock_ptr_ = (clock_ptr_ + 1) % workers_.size();
-}
-    
-    /*!
- * \brief Registry entry for sparse page format.
- */
-struct SparsePageFormatReg
-    : public dmlc::FunctionRegEntryBase<SparsePageFormatReg,
-                                        std::function<SparsePageFormat* ()> > {
+    {protected:
+  BaseEvent(){}
+  virtual ~BaseEvent(){}
 };
     
-      // Compute Huffman codes for each histograms.
-  for (int i = 0; i < num_histo; ++i) {
-    const bool is_dc = static_cast<size_t>(i) < num_dc_histo;
-    const int idx = is_dc ? i : i - num_dc_histo;
-    int counts[kJpegHuffmanMaxBitLength + 1] = { 0 };
-    int values[JpegHistogram::kSize] = { 0 };
-    BuildHuffmanCode(&depths[i * JpegHistogram::kSize], counts, values);
-    HuffmanCodeTable table;
-    for (int j = 0; j < 256; ++j) table.depth[j] = 255;
-    BuildHuffmanCodeTable(counts, values, &table);
-    for (int c = 0; c < ncomps; ++c) {
-      if (is_dc) {
-        if (dc_histo_indexes[c] == idx) (*dc_huff_tables)[c] = table;
-      } else {
-        if (ac_histo_indexes[c] == idx) (*ac_huff_tables)[c] = table;
-      }
+    #include 'base/values.h'
+#include 'components/zoom/zoom_controller.h'
+#include 'content/nw/src/api/object_manager.h'
+#include 'content/nw/src/api/menuitem/menuitem.h'
+#include 'content/public/browser/web_contents.h'
+#include 'content/public/common/page_zoom.h'
+#include 'ui/views/controls/menu/menu_runner.h'
+    
+       bool IsItemForCommandIdDynamic(int command_id) const override;
+   base::string16 GetLabelForCommandId(int command_id) const override;
+   bool GetIconForCommandId(int command_id,
+                                   gfx::Image* icon) const override;
+    
+      if (type == 'separator') {
+    menu_item_ = gtk_separator_menu_item_new();
+  } else {
+    if (type == 'checkbox') {
+      menu_item_ = gtk_check_menu_item_new();
+      bool checked;
+      if (option.GetBoolean('checked', &checked))
+        SetChecked(checked);
+    } else {
+      menu_item_ = gtk_image_menu_item_new();
+      std::string icon;
+      if (option.GetString('icon', &icon))
+        SetIcon(icon);
     }
-    int max_length = kJpegHuffmanMaxBitLength;
-    while (max_length > 0 && counts[max_length] == 0) --max_length;
-    --counts[max_length];
-    int total_count = 0;
-    for (int j = 0; j <= max_length; ++j) total_count += counts[j];
-    data[pos++] = is_dc ? i : static_cast<uint8_t>(i - num_dc_histo + 0x10);
-    for (size_t j = 1; j <= kJpegHuffmanMaxBitLength; ++j) {
-      data[pos++] = counts[j];
     }
-    for (int j = 0; j < total_count; ++j) {
-      data[pos++] = values[j];
+    
+     protected:
+  ~NwAppQuitFunction() override;
+    
+    bool NwClipboardClearSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
+  clipboard->Clear(ui::CLIPBOARD_TYPE_COPY_PASTE);
+  return true;
+}
+    
+      protected:
+    ~NwScreenRegisterStreamFunction() override {}
+    DECLARE_EXTENSION_FUNCTION('nw.Screen.registerStream', UNKNOWN)
+    
+    static HRESULT InitDevice()
+{
+    HRESULT hr = S_OK;
+    }
+    
+    void projectPoints(cv::InputArray objectPoints, cv::OutputArray imagePoints,
+                   cv::InputArray _rvec,cv::InputArray _tvec,
+                   const IntrinsicParams& param, cv::OutputArray jacobian);
+    
+    #if 0
+    
+    
+    {
+    {
+    {  inline void PutChar(char ch) {
+    out_buf += ch;
+    if (out_buf.length() >= kBufferSize) Flush();
+  }
+  inline void Flush(void) {
+    if (out_buf.length() != 0) {
+      fp->Write(&out_buf[0], out_buf.length());
+      out_buf.clear();
     }
   }
-    
-    
-    {}  // namespace
-    
-    #include 'guetzli/jpeg_data.h'
-    
-    #include <stdint.h>
-#include <vector>
-    
-    	if (NULL != _funcname) {
-		funcname_cstr = env->GetStringUTFChars(_funcname, NULL);
-	}
-    
-    
-    {    void throw_exception( std::exception const & e ) {
-        xfatal2(TSF'boost exception:%_', e.what());
-        
-#ifdef ANDROID
-        char stack[4096] = {0};
-        android_callstack(stack, sizeof(stack));
-        xfatal2(TSF'%_', stack);
-#endif
-    }
-}
+};
+}  // namespace common
+}  // namespace xgboost
+#endif  // XGBOOST_COMMON_BASE64_H_
 
     
     
-/*
- * CoreServiceBase.h
- *
- *  Created on: 2013-6-20
- *      Author: yerungui
- */
-    
-    
-    {}
-    
-    
-/*
- * scop_jenv.cpp
- *
- *  Created on: 2012-8-21
- *      Author: yanguoyue
- */
-    
-    
-    {} // namespace aria2
-    
-    void AbstractBtMessage::setPeerConnection(PeerConnection* peerConnection)
-{
-  peerConnection_ = peerConnection;
+    {  if (pos != std::string::npos) {
+    std::string fmt = cache_prefix.substr(pos + 5, cache_prefix.length());
+    size_t cpos = fmt.rfind('-');
+    if (cpos != std::string::npos) {
+      return std::make_pair(fmt.substr(0, cpos), fmt.substr(cpos + 1, fmt.length()));
+    } else {
+      return std::make_pair(fmt, fmt);
+    }
+  } else {
+    std::string raw = 'raw';
+    return std::make_pair(raw, raw);
+  }
 }
     
-      virtual ~AdaptiveFileAllocationIterator();
+    using LogCallbackRegistryStore = dmlc::ThreadLocalStore<LogCallbackRegistry>;
+    
+    TEST(Metric, MAE) {
+  xgboost::Metric * metric = xgboost::Metric::Create('mae');
+  ASSERT_STREQ(metric->Name(), 'mae');
+  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 0, 1e-10);
+  EXPECT_NEAR(GetMetricEval(metric,
+                            {0.1f, 0.9f, 0.1f, 0.9f},
+                            {  0,   0,   1,   1}),
+              0.5f, 0.001f);
+}
+    
+    namespace xgboost {
+namespace tree {
+// List of files that will be force linked in static links.
+DMLC_REGISTRY_LINK_TAG(updater_colmaker);
+DMLC_REGISTRY_LINK_TAG(updater_skmaker);
+DMLC_REGISTRY_LINK_TAG(updater_refresh);
+DMLC_REGISTRY_LINK_TAG(updater_prune);
+DMLC_REGISTRY_LINK_TAG(updater_fast_hist);
+DMLC_REGISTRY_LINK_TAG(updater_histmaker);
+DMLC_REGISTRY_LINK_TAG(updater_sync);
+#ifdef XGBOOST_USE_CUDA
+DMLC_REGISTRY_LINK_TAG(updater_gpu);
+DMLC_REGISTRY_LINK_TAG(updater_gpu_hist);
+#endif
+}  // namespace tree
+}  // namespace xgboost
+
+    
+    SEXP XGDMatrixCreateFromCSC_R(SEXP indptr,
+                              SEXP indices,
+                              SEXP data,
+                              SEXP num_row) {
+  SEXP ret;
+  R_API_BEGIN();
+  const int *p_indptr = INTEGER(indptr);
+  const int *p_indices = INTEGER(indices);
+  const double *p_data = REAL(data);
+  size_t nindptr = static_cast<size_t>(length(indptr));
+  size_t ndata = static_cast<size_t>(length(data));
+  size_t nrow = static_cast<size_t>(INTEGER(num_row)[0]);
+  std::vector<size_t> col_ptr_(nindptr);
+  std::vector<unsigned> indices_(ndata);
+  std::vector<float> data_(ndata);
+    }
+    
+    /*!
+ * \brief Tag function as usable by device
+ */
+#ifdef __NVCC__
+#define XGBOOST_DEVICE __host__ __device__
+#else
+#define XGBOOST_DEVICE
+#endif
+    
+    	ScopedJstring tag_jstr(env, tag);
+	ScopedJstring filename_jstr(env, filename);
+	ScopedJstring funcname_jstr(env, funcname);
+	ScopedJstring log_jst(env, _log);
+    
+    	extern jobject getLoadLibraries(JNIEnv *_env);
+    
+    #endif
+
+    
+    // true pass, false limit
+bool CommFrequencyLimit::Check() {
+    uint64_t now = ::gettickcount();
+    if (!touch_times_.empty() && (now<touch_times_.front()) ) { //if user modify the time, amend it
+    	xwarn2(TSF'Must be modified time.now=%_', now);
+    	size_t size = touch_times_.size();
+    	touch_times_.clear();
+    	for (size_t i=0; i<size; ++i) {
+    		touch_times_.push_back(now-1);
+    	}
+    }
+    }
+    
+      private:
+    void __StartupCreater();
+    void __FirstGetCreater(const std::string& _servicename);
+    void __Creater(std::vector<ServiceRegister>& _vec);
+    
+    #include <string>
+#include <set>
+#include <map>
+    
+    //============================================================================
+// Name        : has_member.h
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
