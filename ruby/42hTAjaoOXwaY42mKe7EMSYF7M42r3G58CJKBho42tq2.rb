04a240de7eda65742d593e5889cbf84b8e76a3e2
SUNsung <weatherius@gmail.com>
2018-07-19 18:58:53 +0300

    Rewrite [42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb] File in: 2018-07-19 18:58:52

--- ruby/42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb
+++ ruby/42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb
@@ -1,72 +1,49 @@
 
         
-              if ARGV.git?
-        system 'git', 'init'
-        system 'git', 'add', '-A'
-      end
-      if ARGV.interactive?
-        ohai 'Entering interactive mode'
-        puts 'Type `exit' to return and finalize the installation'
-        puts 'Install to this prefix: #{formula.prefix}'
-    
-      def merge(*args)
-    @settings.merge(*args)
-    self
-  end
-    
-            next unless HOMEBREW_CELLAR.directory?
-    
-      def internal_development_commands
-    find_internal_commands HOMEBREW_LIBRARY_PATH/'dev-cmd'
-  end
-    
-      def search_taps(rx)
-    SEARCHABLE_TAPS.map do |user, repo|
-      Thread.new { search_tap(user, repo, rx) }
-    end.inject([]) do |results, t|
-      results.concat(t.value)
-    end
-  end
-    
-          def initialize(gradle_path: nil)
-        self.gradle_path = gradle_path
-      end
-    
-            expect(result).to eq('git rev-parse --short HEAD')
-        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('git rev-parse --short HEAD')
-      end
-    end
-    
-            expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to end_with('&& agvtool new-marketing-version 1.77.3')
-      end
-    
-        projects
-  end
-end
+        end
 
     
-        options[:attribution] = <<-HTML
-      &copy; 2011&ndash;2018 Twitter, Inc.<br>
-      &copy; 2011&ndash;2018 The Bootstrap Authors<br>
-      Code licensed under the MIT License.<br>
-      Documentation licensed under the Creative Commons Attribution License v3.0.
-    HTML
+            command = [escaped_gradle_path, 'tasks', '--console=plain'].join(' ')
+        output = Action.sh(command, print_command: false, print_command_output: false)
+        output.split('\n').each do |line|
+          if (result = line.match(/(\w+)\s\-\s([\w\s]+)/))
+            self.tasks << GradleTask.new(title: result[1], description: result[2])
+          end
+        end
     
-        options[:title] = false
-    options[:root_title] = 'PHP: Hypertext Preprocessor'
-    options[:skip_links] = ->(filter) { !filter.initial_page? }
+            if Devise.activerecord51?
+          def postpone_email_change_until_confirmation_and_regenerate_confirmation_token
+            @reconfirmation_required = true
+            self.unconfirmed_email = self.email
+            self.email = self.email_in_database
+            self.confirmation_token = nil
+            generate_confirmation_token
+          end
+        else
+          def postpone_email_change_until_confirmation_and_regenerate_confirmation_token
+            @reconfirmation_required = true
+            self.unconfirmed_email = self.email
+            self.email = self.email_was
+            self.confirmation_token = nil
+            generate_confirmation_token
+          end
+        end
     
-          options[:only_patterns] = [
-        /\Aget_started/,
-        /\Aprogrammers_guide/,
-        /\Atutorials/,
-        /\Aperformance/,
-        /\Adeploy/,
-        /\Aextend/]
-    end
+          def self.generate_helpers!(routes=nil)
+        routes ||= begin
+          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
+          Devise::URL_HELPERS.slice(*mappings)
+        end
     
-        for_each_gem do |gem_path|
-      sh 'gem push '#{gem_path}''
-    end
+    def load_apps
+  out, err, status = Open3.capture3('/usr/bin/osascript', '-e', 'tell application 'System Events' to get (name, bundle identifier, unix id) of every process')
+  if status.exitstatus > 0
+    puts err
+    exit status.exitstatus
   end
+  out = out.split(', ')
+  one_third   = out.length / 3
+  @app_names  = out.shift(one_third)
+  @bundle_ids = out.shift(one_third)
+  @unix_ids   = out.shift(one_third)
 end
