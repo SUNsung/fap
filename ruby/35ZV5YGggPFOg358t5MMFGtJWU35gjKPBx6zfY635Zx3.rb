7b8bc201d7c2aa5a526a7e21782095f30ff19d8b
SUNsung <weatherius@gmail.com>
2018-07-19 07:48:34 +0300

    Rewrite [35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb] File in: 2018-07-19 07:48:33

--- ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
+++ ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
@@ -1,38 +1,14 @@
 
         
-          def resource
-    @resource ||=
-      if params[:project_id].present?
-        Project.find(params[:project_id])
-      elsif params[:namespace_id].present?
-        Group.find(params[:namespace_id])
-      end
+          def deliver_digest
+    NotificationMailer.digest(user.account).deliver_now!
+    user.touch(:last_emailed_at)
   end
-    
-    if ARGV.count < 2
-  abort 'Usage: #$0 pidfile command [args...]'
 end
+
     
-        @services = current_user.services.reorder(table_sort).page(params[:page])
-    
-      # POST /resource/confirmation
-  def create
-    self.resource = resource_class.send_confirmation_instructions(resource_params)
-    yield resource if block_given?
-    
-      # POST /resource/unlock
-  def create
-    self.resource = resource_class.send_unlock_instructions(resource_params)
-    yield resource if block_given?
-    
-          html_filters.push 'coffeescript/clean_html_v1', 'coffeescript/entries_v1', 'title'
-    
-        options[:only] = BOOKS.map { |s| 'book.#{s}.html' }
-    
-      def text_url
-    object.local? ? medium_url(object) : nil
-  end
-    
-      def updated
-    object.updated_at.iso8601
+      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/follow
+  def follow
+    f = Follow.last
+    NotificationMailer.follow(f.target_account, Notification.find_by(activity: f))
   end
