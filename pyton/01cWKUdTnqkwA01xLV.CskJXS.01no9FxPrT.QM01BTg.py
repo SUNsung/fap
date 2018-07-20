a470af9688b96a24769207b4afaa108d678a6f5b
SUNsung <weatherius@gmail.com>
2018-07-20 15:04:50 +0300

    Rewrite [01cWKUdTnqkwA01xLV.CskJXS.01no9FxPrT.QM01BTg.py] File in: 2018-07-20 15:04:49

--- pyton/01cWKUdTnqkwA01xLV.CskJXS.01no9FxPrT.QM01BTg.py
+++ pyton/01cWKUdTnqkwA01xLV.CskJXS.01no9FxPrT.QM01BTg.py
@@ -1,127 +1,116 @@
 
-def prepare_url(value):
-    # Issue #1483: Make sure the URL always has a trailing slash
-    httpbin_url = value.url.rstrip('/') + '/'
+        
+                    if self.wait_to_close_event:
+                self.wait_to_close_event.wait(self.WAIT_EVENT_TIMEOUT)
+        finally:
+            self.ready_event.set() # just in case of exception
+            self._close_server_sock_ignore_errors()
+            self.stop_event.set()
     
-        def test_stream_with_auth_does_not_set_transfer_encoding_header(self, httpbin):
-        '''Ensure that a byte stream with size > 0 will not set both a Content-Length
-        and Transfer-Encoding header.
+        def redirect_resp_handler(sock):
+        consume_socket_content(sock, timeout=0.5)
+        location = u'//{0}:{1}/{2}'.format(host, port, path)
+        sock.send(
+            b'HTTP/1.1 301 Moved Permanently\r\n'
+            b'Content-Length: 0\r\n'
+            b'Location: ' + location.encode('utf8') + b'\r\n'
+            b'\r\n'
+        )
+        redirect_request.append(consume_socket_content(sock, timeout=0.5))
+        sock.send(b'HTTP/1.1 200 OK\r\n\r\n')
+    
+    # -- Options for LaTeX output ---------------------------------------------
+    
+        if isinstance(password, str):
+        password = password.encode('latin1')
+    
+        def _find(self, name, domain=None, path=None):
+        '''Requests uses this method internally to get cookie values.
+    
+        :param url: URL for the new :class:`Request` object.
+    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
+        object to send in the body of the :class:`Request`.
+    :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    '''
+    
+        def _genspider(self, module, name, domain, template_name, template_file):
+        '''Generate the spider module, based on the given template'''
+        tvars = {
+            'project_name': self.settings.get('BOT_NAME'),
+            'ProjectName': string_camelcase(self.settings.get('BOT_NAME')),
+            'module': module,
+            'name': name,
+            'domain': domain,
+            'classname': '%sSpider' % ''.join(s.capitalize() \
+                for s in module.split('_'))
+        }
+        if self.settings.get('NEWSPIDER_MODULE'):
+            spiders_module = import_module(self.settings['NEWSPIDER_MODULE'])
+            spiders_dir = abspath(dirname(spiders_module.__file__))
+        else:
+            spiders_module = None
+            spiders_dir = '.'
+        spider_file = '%s.py' % join(spiders_dir, module)
+        shutil.copyfile(template_file, spider_file)
+        render_templatefile(spider_file, **tvars)
+        print('Created spider %r using template %r ' % (name, \
+            template_name), end=('' if spiders_module else '\n'))
+        if spiders_module:
+            print('in module:\n  %s.%s' % (spiders_module.__name__, module))
+    
+        @classmethod
+    def from_crawler(cls, crawler):
+        o = cls(crawler.settings.getfloat('DOWNLOAD_TIMEOUT'))
+        crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
+        return o
+    
+    
+class UserAgentMiddleware(object):
+    '''This middleware allows spiders to override the user_agent'''
+    
+    class CloseSpider(Exception):
+    '''Raise this from callbacks to request the spider to be closed'''
+    
         '''
-        auth = ('user', 'pass')
-        url = httpbin('post')
-        file_obj = io.BytesIO(b'test data')
-        r = requests.Request('POST', url, auth=auth, data=file_obj)
-        prepared_request = r.prepare()
-        assert 'Transfer-Encoding' not in prepared_request.headers
-        assert 'Content-Length' in prepared_request.headers
     
+    # A shorter title for the navigation bar.  Default is the same as html_title.
+#html_short_title = None
     
-if __name__ == '__main__':
-    main()
-
+    YCM_VAR_PREFIX = 'ycm_'
     
-            #: A CookieJar of Cookies the server sent back.
-        self.cookies = cookiejar_from_dict({})
+    from ycm.tests.test_utils import MockVimModule
+MockVimModule()
     
+        poll_again = _HandlePollResponse( response, diagnostics_handler )
+    if poll_again:
+      self._SendRequest()
+      return True
     
-class LookupDict(dict):
-    '''Dictionary lookup object.'''
+        def test_wildcard_etag(self):
+        computed_etag = ''xyzzy''
+        etags = '*'
+        self.check_url(
+            '/cache/' + computed_etag, method='GET',
+            headers=[('If-None-Match', etags)],
+            expected_status=304,
+            allowed_warnings=[rs.MISSING_HDRS_304])
     
-                self._start_queue_management_thread()
-            self._adjust_process_count()
-            return f
-    submit.__doc__ = _base.Executor.submit.__doc__
+        for i in range(1, num_tests + 1):
+        logging.info('running test case %d', i)
+        url = options.url + '/runCase?case=%d&agent=%s' % (i, options.name)
+        test_ws = yield websocket_connect(url, None, compression_options={})
+        while True:
+            message = yield test_ws.read_message()
+            if message is None:
+                break
+            test_ws.write_message(message, binary=isinstance(message, bytes))
     
-    def main():
-    for name, fn in [('sequential',
-                      functools.partial(download_urls_sequential, URLS)),
-                     ('processes',
-                      functools.partial(download_urls_with_executor,
-                                        URLS,
-                                        ProcessPoolExecutor(10))),
-                     ('threads',
-                      functools.partial(download_urls_with_executor,
-                                        URLS,
-                                        ThreadPoolExecutor(10)))]:
-        sys.stdout.write('%s: ' % name.ljust(12))
-        start = time.time()
-        url_map = fn()
-        sys.stdout.write('%.2f seconds (%d of %d downloaded)\n' %
-                         (time.time() - start, len(url_map), len(URLS)))
+    # These benchmarks are delicate.  They hit various fast-paths in the gen
+# machinery in order to stay synchronous so we don't need an IOLoop.
+# This removes noise from the results, but it's easy to change things
+# in a way that completely invalidates the results.
     
-    # List of directories, relative to source directory, that shouldn't be searched
-# for source files.
-exclude_trees = ['_build']
-    
-    
-class ThreadPoolWaitTests(ThreadPoolMixin, WaitTests):
-    
-        # build a new DiagnosticFilter merging all filters
-    #  for the provided filetypes
-    spec = []
-    for filetype in filetypes:
-      type_specific = self._all_filters.get( filetype, [] )
-      spec.extend( type_specific )
-    
-    
-def SyntaxGroupsFromOutput_Basic_test():
-  assert_that( syntax_parse._SyntaxGroupsFromOutput( '''
-foogroup xxx foo bar
-             zoo goo
-             links to Statement''' ),
-               has_item( 'foogroup' ) )
-    
-    
-  def Start( self ):
-    request_data = BuildRequestData()
-    request_data.update( { 'filetypes': self.filetypes } )
-    self._response = self.PostDataToHandler( request_data,
-                                             'semantic_completion_available' )
-    
-    
-  def Response( self ):
-    if self._cached_response:
-      return self._cached_response
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-      def __init__( self, bufnr, user_options, async_diags ):
-    self.number = bufnr
-    self._parse_tick = 0
-    self._handled_tick = 0
-    self._parse_request = None
-    self._async_diags = async_diags
-    self._diag_interface = DiagnosticInterface( bufnr, user_options )
-    
-            For example: Given the url pattern /([0-9]{4})/([a-z-]+)/, this method
-        would return ('/%s/%s/', 2).
-        '''
-        pattern = self.regex.pattern
-        if pattern.startswith('^'):
-            pattern = pattern[1:]
-        if pattern.endswith('$'):
-            pattern = pattern[:-1]
-    
-    
-class CloseReasonHandler(TestWebSocketHandler):
-    def open(self):
-        self.on_close_called = False
-        self.close(1001, 'goodbye')
-    
-    
-def _decode_fields_v2(value):
-    def _consume_field(s):
-        length, _, rest = s.partition(b':')
-        n = int(length)
-        field_value = rest[:n]
-        # In python 3, indexing bytes returns small integers; we must
-        # use a slice to get a byte string as in python 2.
-        if rest[n:n + 1] != b'|':
-            raise ValueError('malformed v2 signed value field')
-        rest = rest[n + 1:]
-        return field_value, rest
+            Typical usage::
