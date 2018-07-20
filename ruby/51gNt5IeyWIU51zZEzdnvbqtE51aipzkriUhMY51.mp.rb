366e30462f946a9a6e2180c6b1863994b1820bd8
SUNsung <weatherius@gmail.com>
2018-07-20 23:36:31 +0300

    Rewrite [51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb] File in: 2018-07-20 23:36:31

--- ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
+++ ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
@@ -1,67 +1,29 @@
 
         
-            def execute(args)
-      topic_id = args[:topic_id]
-      raise Discourse::InvalidParameters.new(:topic_id) unless topic_id.present?
-    
-    end
-
-    
-            if entries.empty?
-          css('.quickindex[id]').each do |node|
-            name = node['id'].remove(/quickindex\.?/)
-            next if name.empty? || name =~ /\.\d+\z/ || name =~ /\A([^\.]+)\.\1\z/
-            entries << ['#{self.name}.#{name}', name]
-          end
-        end
-    
-          html_filters.push 'coffeescript/clean_html_v1', 'coffeescript/entries_v1', 'title'
-    
-        version do
-      self.links = {
-        home: 'https://haxe.org',
-        code: 'https://github.com/HaxeFoundation/haxe'
-      }
-    
-        options[:skip] = %w(book/first-edition/README.html)
-    options[:skip_patterns] = [/(?<!\.html)\z/]
-    
-          unless root?
-        raise Invalid, 'missing name' if !name || name.empty?
-        raise Invalid, 'missing path' if !path || path.empty?
-        raise Invalid, 'missing type' if !type || type.empty?
+              # Get the publicly-visible URL for an imported file. This URL is used by
+      # source maps to link to the source stylesheet. This may return `nil` to
+      # indicate that no public URL is available; however, this will cause
+      # sourcemap generation to fail if any CSS is generated from files imported
+      # from this importer.
+      #
+      # If an absolute 'file:' URI can be produced for an imported file, that
+      # should be preferred to returning `nil`. However, a URL relative to
+      # `sourcemap_directory` should be preferred over an absolute 'file:' URI.
+      #
+      # @param uri [String] A URI known to be valid for this importer.
+      # @param sourcemap_directory [String, NilClass] The absolute path to a
+      #   directory on disk where the sourcemap will be saved. If uri refers to
+      #   a file on disk that's accessible relative to sourcemap_directory, this
+      #   may return a relative URL. This may be `nil` if the sourcemap's
+      #   eventual location is unknown.
+      # @return [String?] The publicly-visible URL for this file, or `nil`
+      #   indicating that no publicly-visible URL exists. This should be
+      #   appropriately URL-escaped.
+      def public_url(uri, sourcemap_directory)
+        return if @public_url_warning_issued
+        @public_url_warning_issued = true
+        Sass::Util.sass_warn <<WARNING
+WARNING: #{self.class.name} should define the #public_url method.
+WARNING
+        nil
       end
-    end
-    
-          base_dir = Pathname.new(normalized_path)
-      base_dir = base_dir.parent unless path.end_with? '/'
-    
-    if $PROGRAM_NAME == __FILE__ && !ENV['COCOAPODS_NO_BUNDLER']
-  ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
-  require 'rubygems'
-  require 'bundler/setup'
-  $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-elsif ENV['COCOAPODS_NO_BUNDLER']
-  require 'rubygems'
-  gem 'cocoapods'
-end
-    
-            def run
-          UI.puts('$CACHE_ROOT: #{@cache.root}') if @short_output
-          if @pod_name.nil? # Print all
-            @cache.cache_descriptors_per_pod.each do |pod_name, cache_descriptors|
-              print_pod_cache_infos(pod_name, cache_descriptors)
-            end
-          else # Print only for the requested pod
-            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
-            if cache_descriptors.nil?
-              UI.notice('No cache for pod named #{@pod_name} found')
-            else
-              print_pod_cache_infos(@pod_name, cache_descriptors)
-            end
-          end
-        end
-    
-    end
-    
-    module Jekyll
