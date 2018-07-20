6c72e2a9428794f33f71693c96d8d83139e798aa
SUNsung <weatherius@gmail.com>
2018-07-20 03:13:30 +0300

    Rewrite [01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py] File in: 2018-07-20 03:13:29

--- pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
+++ pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
@@ -1,22 +1,50 @@
 
         
-                total_count = 0
-        translated_count = 0
-        with open(fn) as f:
-            catalog = read_po(f)
-            for msg in catalog:
-                total_count += 1
-                if is_translated(msg):
-                    translated_count += 1
-        pct = translated_count / float(total_count) * 100
-        click.echo('% -7s % 2d%%' % (
-            locale,
-            pct,
-        ), err=True)
-        if pct >= MINIMUM and locale not in rv:
-            rv.append(locale)
-    with open(catalog_file, 'w') as f:
-        json.dump({
-            'supported_locales': sorted(rv)
-        }, f, indent=2)
-        f.write('\n')
+        import pytest
+    
+        This session backend will set the :attr:`modified` and
+    :attr:`accessed` attributes. It cannot reliably track whether a
+    session is new (vs. empty), so :attr:`new` remains hard coded to
+    ``False``.
+    '''
+    
+        :param source: the source code of the template to be
+                   rendered
+    :param context: the variables that should be available in the
+                    context of the template.
+    '''
+    ctx = _app_ctx_stack.top
+    ctx.app.update_template_context(context)
+    return _render(ctx.app.jinja_env.from_string(source),
+                   context, ctx.app)
+
+    
+            os.environ = {'FOO_SETTINGS': __file__.rsplit('.', 1)[0] + '.py'}
+        assert app.config.from_envvar('FOO_SETTINGS')
+        common_object_test(app)
+    finally:
+        os.environ = env
+    
+    
+class POSTHandler(tornado.web.RequestHandler):
+    def post(self):
+        for field_name, files in self.request.files.items():
+            for info in files:
+                filename, content_type = info['filename'], info['content_type']
+                body = info['body']
+                logging.info('POST '%s' '%s' %d bytes',
+                             filename, content_type, len(body))
+    
+    from tornado.auth import TwitterMixin
+from tornado.escape import json_decode, json_encode
+from tornado.ioloop import IOLoop
+from tornado import gen
+from tornado.options import define, options, parse_command_line, parse_config_file
+from tornado.web import Application, RequestHandler, authenticated
+    
+        IOLoop.instance().start()
+    
+    # Repeat the entire benchmark this many times (on different ports)
+# This gives JITs time to warm up, etc.  Pypy needs 3-5 runs at
+# --n=15000 for its JIT to reach full effectiveness
+define('num_runs', type=int, default=1)
