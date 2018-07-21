4c6f5d6546f546e8d0021545a3259f9cebf5f4c6
SUNsung <weatherius@gmail.com>
2018-07-21 15:31:20 +0300

    Rewrite [32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py] File in: 2018-07-21 15:31:19

--- pyton/32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py
+++ pyton/32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py
@@ -1,80 +1,96 @@
 
         
-            Enabling `trace_only` effectively creates a graph that is a 'dry run'.
-    There will be no side affects. In addition, the gradients won't be computed
-    and the model parameters will not be updated.
+            def reducer(self, key, values):
+        total = sum(values)
+        if total == 1:
+            yield key, total
     
-        move = Action('move')
-    person = Person('Jack', move)
-    person.do_action().amount('5m').stop()
+        def _find_available_spot(self, vehicle):
+        '''Find an available spot where vehicle can fit, or return None'''
+        pass
     
-        def test_dog_greek_localization(self):
-        self.assertEqual(self.g.get('dog'), 'σκύλος')
+        def message_group(self, group_id, message):
+        pass
     
-        a_transaction = Transaction(True, num_obj)
-    try:
-        for i in range(3):
-            num_obj.increment()
-            print(num_obj)
-        a_transaction.commit()
-        print('-- committed')
+    from mrjob.job import MRJob
     
-    '''
-http://code.activestate.com/recipes/131499-observer-pattern/
-    
-    import tornado.httpserver
-import tornado.ioloop
-import tornado.options
-import tornado.web
-    
-    
-def main():
-    parse_command_line(final=False)
-    parse_config_file(options.config_file)
-    
-        def get_allowed_warnings(self):
-        return [
-            # We can't set a non-heuristic freshness at the framework level,
-            # so just ignore this warning
-            rs.FRESHNESS_HEURISTIC,
-            # For our small test responses the Content-Encoding header
-            # wipes out any gains from compression
-            rs.CONNEG_GZIP_BAD,
-        ]
+            # Keyword arguments > stream.encoding > default utf8
+        if self.stdin_encoding is None:
+            self.stdin_encoding = getattr(
+                self.stdin, 'encoding', None) or 'utf8'
+        if self.stdout_encoding is None:
+            actual_stdout = self.stdout
+            if is_windows:
+                # noinspection PyUnresolvedReferences
+                from colorama import AnsiToWin32
+                if isinstance(self.stdout, AnsiToWin32):
+                    actual_stdout = self.stdout.wrapped
+            self.stdout_encoding = getattr(
+                actual_stdout, 'encoding', None) or 'utf8'
     
     
-def main():
-    parse_command_line()
+FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
+FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
+JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
+BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
     
-        async def fetch_url(current_url):
-        if current_url in fetching:
-            return
+    import pytest
     
-    # Repeat the entire benchmark this many times (on different ports)
-# This gives JITs time to warm up, etc.  Pypy needs 3-5 runs at
-# --n=15000 for its JIT to reach full effectiveness
-define('num_runs', type=int, default=1)
-    
-    # These benchmarks are delicate.  They hit various fast-paths in the gen
-# machinery in order to stay synchronous so we don't need an IOLoop.
-# This removes noise from the results, but it's easy to change things
-# in a way that completely invalidates the results.
+        exc = ConnectionError('Connection aborted')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
+    assert ret == ExitStatus.ERROR
+    assert error_msg == (
+        'ConnectionError: '
+        'Connection aborted while doing GET request to URL: '
+        'http://www.google.com')
     
     
-@gen.coroutine
-def raw_producer(filename, write):
-    with open(filename, 'rb') as f:
-        while True:
-            # 16K at a time.
-            chunk = f.read(16 * 1024)
-            if not chunk:
-                # Complete.
-                break
+def test_follow_all_output_options_used_for_redirects(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 3
+    assert HTTP_OK not in r
     
-    1) Install virtualbox guest additions (from the device menu in virtualbox)
-2) Set up a shared folder to the root of your tornado repo.  It must be a
-   read-write mount to use tox, although the tests can be run directly
-   in a read-only mount.  This will probably assign drive letter E:.
-3) Install Python 2.7 from python.org.
-4) Run this script by double-clicking it, or running
-   'c:\python27\python.exe bootstrap.py' in a shell.
+        # If both `auth_parse` and `prompt_password` are set to `True`,
+    # and the value of `-a` lacks the password part,
+    # then the user will be prompted to type the password in.
+    prompt_password = True
+    
+    
+CLIENT_CERT = os.path.join(TESTS_ROOT, 'client_certs', 'client.crt')
+CLIENT_KEY = os.path.join(TESTS_ROOT, 'client_certs', 'client.key')
+CLIENT_PEM = os.path.join(TESTS_ROOT, 'client_certs', 'client.pem')
+    
+        # 128+2 SIGINT <http://www.tldp.org/LDP/abs/html/exitcodes.html>
+    ERROR_CTRL_C = 130
+    
+    
+def test_credentials_in_url(httpbin_both):
+    url = add_auth(httpbin_both.url + '/basic-auth/user/password',
+                   auth='user:password')
+    r = http('GET', url)
+    assert HTTP_OK in r
+    assert r.json == {'authenticated': True, 'user': 'user'}
+    
+        def test_CRLF_ugly_response(self, httpbin):
+        r = http('--pretty=none', 'GET', httpbin.url + '/get')
+        self._validate_crlf(r)
+    
+        Each code snipped should get additional C++ code around it to help compile the line in context, with
+    some heuristic guessing of what is needed around. The wrapping code should have a token in each line allowing
+    other tools to filter out these lines
+    
+      if Search(r'\busing namespace\b', line):
+    if Search(r'\bliterals\b', line):
+      error(filename, linenum, 'build/namespaces_literals', 5,
+            'Do not use namespace using-directives.  '
+            'Use using-declarations instead.')
+    else:
+      error(filename, linenum, 'build/namespaces', 5,
+            'Do not use namespace using-directives.  '
+            'Use using-declarations instead.')
