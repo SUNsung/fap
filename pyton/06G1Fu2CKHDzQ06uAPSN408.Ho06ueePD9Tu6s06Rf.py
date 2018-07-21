19b924b399c8b07adcac80d6c0a58f2d89388cd1
SUNsung <weatherius@gmail.com>
2018-07-21 12:25:47 +0300

    Rewrite [06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py] File in: 2018-07-21 12:25:46

--- pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
+++ pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
@@ -1,75 +1,161 @@
 
         
         
-@pytest.mark.functional
-def test_without_confirmation(proc, TIMEOUT):
-    without_confirmation(proc, TIMEOUT)
+if __name__ == '__main__':
+    pytest.main([__file__])
+
     
-        def test_maxlen(self):
-        self.assertRaises(ValueError, deque, 'abc', -1)
-        self.assertRaises(ValueError, deque, 'abc', -2)
-        it = iter(range(10))
-        d = deque(it, maxlen=3)
-        self.assertEqual(list(it), [])
-        self.assertEqual(repr(d), 'deque([7, 8, 9], maxlen=3)')
-        self.assertEqual(list(d), [7, 8, 9])
-        self.assertEqual(d, deque(range(10), 3))
-        d.append(10)
-        self.assertEqual(list(d), [8, 9, 10])
-        d.appendleft(7)
-        self.assertEqual(list(d), [7, 8, 9])
-        d.extend([10, 11])
-        self.assertEqual(list(d), [9, 10, 11])
-        d.extendleft([8, 7])
-        self.assertEqual(list(d), [7, 8, 9])
-        d = deque(range(200), maxlen=10)
-        d.append(d)
-        support.unlink(support.TESTFN)
-        fo = open(support.TESTFN, 'w')
-        try:
-            fo.write(str(d))
-            fo.close()
-            fo = open(support.TESTFN, 'r')
-            self.assertEqual(fo.read(), repr(d))
-        finally:
-            fo.close()
-            support.unlink(support.TESTFN)
+    print('Train...')
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, y_test))
+score, acc = model.evaluate(x_test, y_test, batch_size=batch_size)
+print('Test score:', score)
+print('Test accuracy:', acc)
+
     
-        def _getlongresp(self, file=None):
-        '''Internal: get a response plus following text from the server.
-        Raise various errors if the response indicates an error.
     
-    def ISNONTERMINAL(x):
-    return x >= NT_OFFSET
+@keras_test
+def test_unit_norm():
+    unit_norm_instance = constraints.unit_norm()
+    normalized = unit_norm_instance(K.variable(get_example_array()))
+    norm_of_normalized = np.sqrt(np.sum(K.eval(normalized) ** 2, axis=0))
+    # In the unit norm constraint, it should be equal to 1.
+    difference = norm_of_normalized - 1.
+    largest_difference = np.max(np.abs(difference))
+    assert(np.abs(largest_difference) < 10e-5)
     
-        def test_oldargs1_2_ext(self):
-        try:
-            [].count(*(1, 2))
-        except TypeError:
-            pass
+    # Notes
+    
+        # Arguments
+        n_dense: int > 0. Number of dense layers.
+        dense_units: int > 0. Number of dense units per layer.
+        dropout: keras.layers.Layer. A dropout layer to apply.
+        dropout_rate: 0 <= float <= 1. The rate of dropout.
+        kernel_initializer: str. The initializer for the weights.
+        optimizer: str/keras.optimizers.Optimizer. The optimizer to use.
+        num_classes: int > 0. The number of classes to predict.
+        max_words: int > 0. The maximum number of words per data point.
+    
+        # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+    '''
+    assert 0 <= test_split < 1
+    path = get_file(path,
+                    origin='https://s3.amazonaws.com/keras-datasets/boston_housing.npz',
+                    file_hash='f553886a1f8d56431e820c5b82552d9d95cfcb96d1e678153f8839538947dff5')
+    f = np.load(path)
+    x = f['x']
+    y = f['y']
+    f.close()
+    
+            # start checks
+        if opts.list:
+            for spider, methods in sorted(contract_reqs.items()):
+                if not methods and not opts.verbose:
+                    continue
+                print(spider)
+                for method in sorted(methods):
+                    print('  * %s' % method)
         else:
-            raise RuntimeError
+            start = time.time()
+            self.crawler_process.start()
+            stop = time.time()
     
-        def resource_path(self, resource):
-        # All resources are in the zip file, so there is no path to the file.
-        # Raising FileNotFoundError tells the higher level API to extract the
-        # binary data and create a temporary file.
-        raise FileNotFoundError
+    from scrapy.http import Request
+from scrapy.utils.spider import iterate_spider_output
+from scrapy.utils.python import get_spec
     
-        # Block until the barrier is ready for us, or raise an exception
-    # if it is broken.
-    def _enter(self):
-        while self._state in (-1, 1):
-            # It is draining or resetting, wait until done
-            self._cond.wait()
-        #see if the barrier is in a broken state
-        if self._state < 0:
-            raise BrokenBarrierError
-        assert self._state == 0
+            self.factory.noPage(\
+                defer.TimeoutError('Getting %s took longer than %s seconds.' % \
+                (self.factory.url, self.factory.timeout)))
     
-            class complex1(complex):
-            '''Test usage of __complex__() with a __new__() method'''
-            def __new__(self, value=0j):
-                return complex.__new__(self, 2*value)
-            def __complex__(self):
-                return self
+        def _get_request_cookies(self, jar, request):
+        if isinstance(request.cookies, dict):
+            cookie_list = [{'name': k, 'value': v} for k, v in \
+                    six.iteritems(request.cookies)]
+        else:
+            cookie_list = request.cookies
+    
+    from scrapy.utils.python import without_none_values
+    
+        def process_request(self, request, spider):
+        auth = getattr(self, 'auth', None)
+        if auth and b'Authorization' not in request.headers:
+            request.headers[b'Authorization'] = auth
+
+    
+        Decimals and Fractions are supported:
+    
+            d = deque(s)
+        d.rotate(-1)            # verify rot(-1)
+        self.assertEqual(''.join(d), 'bcdea')
+        d.rotate()              # check default to 1
+        self.assertEqual(tuple(d), s)
+    
+    def evalString(s):
+    assert s.startswith(''') or s.startswith('''), repr(s[:1])
+    q = s[0]
+    if s[:3] == q*3:
+        q = q*3
+    assert s.endswith(q), repr(s[-len(q):])
+    assert len(s) >= 2*len(q)
+    s = s[len(q):-len(q)]
+    return re.sub(r'\\(\'|\'|\\|[abfnrtv]|x.{0,2}|[0-7]{1,3})', escape, s)
+    
+        def test_return(self):
+        self.expect_set = [
+            ('line', 2, 'tfunc_main'),    ('step', ),
+            ('line', 3, 'tfunc_main'),    ('step', ),
+            ('call', 1, 'tfunc_first'),   ('step', ),
+            ('line', 2, 'tfunc_first'),   ('return', ),
+            ('return', 4, 'tfunc_first'), ('step', ),
+            ('line', 4, 'tfunc_main'),    ('quit', ),
+        ]
+        with TracerRun(self) as tracer:
+            tracer.runcall(tfunc_main)
+    
+                # test that the path returns unchanged
+            p1 = P('~/My Documents')
+            p2 = P('~alice/My Documents')
+            p3 = P('~bob/My Documents')
+            p4 = P('/~/My Documents')
+            p5 = P('d:~/My Documents')
+            p6 = P('')
+            self.assertRaises(RuntimeError, p1.expanduser)
+            self.assertRaises(RuntimeError, p2.expanduser)
+            self.assertRaises(RuntimeError, p3.expanduser)
+            self.assertEqual(p4.expanduser(), p4)
+            self.assertEqual(p5.expanduser(), p5)
+            self.assertEqual(p6.expanduser(), p6)
+    
+            for i in range(NUM_FDS // 2):
+            try:
+                rd, wr = self.make_socketpair()
+            except OSError:
+                # too many FDs, skip - note that we should only catch EMFILE
+                # here, but apparently *BSD and Solaris can fail upon connect()
+                # or bind() with EADDRNOTAVAIL, so let's be safe
+                self.skipTest('FD limit reached')
+    
+        def is_resource(self, name):
+        # Maybe we could do better, but if we can get the data, it's a
+        # resource.  Otherwise it isn't.
+        fullname_as_path = self.fullname.replace('.', '/')
+        path = f'{fullname_as_path}/{name}'
+        try:
+            self.zipimporter.get_data(path)
+        except OSError:
+            return False
+        return True
+    
+        def log_message(self, format, *args):
+        pass
+    
+        def test_types(self):
+        # Non-integer objects *equal* to any of the range's items are supposed
+        # to be contained in the range.
+        self.assertIn(1.0, range(3))
+        self.assertIn(True, range(3))
+        self.assertIn(1+0j, range(3))
