3120813a39546144a359dd8dc86027fc869ee975
SUNsung <weatherius@gmail.com>
2018-07-21 04:16:50 +0300

    Rewrite [04lBi4pN.Tpp604X6JrFzb8uuw04H9ArEYFXtQo04ptx.py] File in: 2018-07-21 04:16:49

--- pyton/04lBi4pN.Tpp604X6JrFzb8uuw04H9ArEYFXtQo04ptx.py
+++ pyton/04lBi4pN.Tpp604X6JrFzb8uuw04H9ArEYFXtQo04ptx.py
@@ -1,165 +1,83 @@
 
         
-                def check(self, value):
-            return isinstance(value, Foo)
+            @classmethod
+    def setUpClass(cls):
+        support.requires('network')
+        with support.transient_internet(cls.base_url):
+            cls.parser = urllib.robotparser.RobotFileParser(cls.robots_txt)
+            cls.parser.read()
     
-            export YOURAPPLICATION_SETTINGS='/path/to/config/file'
+                elif event == 'exception':
+                if not isinstance(self.exc_info[1], self.expect[3]):
+                    self.raise_not_expected(
+                        'Wrong exception at expect_set item %d, got '%s'' %
+                        (self.expect_set_no, self.exc_info))
     
-        #: Some implementations can detect whether a session is newly
-    #: created, but that is not guaranteed. Use with caution. The mixin
-    # default is hard-coded ``False``.
-    new = False
+            * Some platforms report ASCII as ANSI_X3.4-1968
+        * Some platforms report ASCII as US-ASCII
+        * Some platforms report UTF-8 instead of utf-8
+        '''
+        data = data.replace(b'ANSI_X3.4-1968', b'ascii')
+        data = data.replace(b'US-ASCII', b'ascii')
+        data = data.lower()
+        return data
     
+            Args:
+            max_workers: The maximum number of threads that can be used to
+                execute the given calls.
+        '''
+        self._max_workers = max_workers
+        self._work_queue = queue.Queue()
+        self._threads = set()
+        self._shutdown = False
+        self._shutdown_lock = threading.Lock()
     
-def test_config_from_json():
-    app = flask.Flask(__name__)
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    app.config.from_json(os.path.join(current_dir, 'static', 'config.json'))
-    common_object_test(app)
+    def download_urls_sequential(urls, timeout=60):
+    url_to_content = {}
+    for url in urls:
+        try:
+            url_to_content[url] = load_url(url, timeout=timeout)
+        except:
+            pass
+    return url_to_content
     
-        stream = StringIO()
-    client.get('/', errors_stream=stream)
-    assert 'ERROR in test_logging: test' in stream.getvalue()
+    # The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+version = '2.1.3'
+# The full version, including alpha/beta/rc tags.
+release = '2.1.3'
     
-        def __enter__(self):
-        gc.disable()
-        _gc_lock.acquire()
-        loc = flask._request_ctx_stack._local
-    
-        def record_teardown(sender, **kwargs):
-        recorded.append(('tear_down', kwargs))
-    
-        h = history.history
-    assert_array_almost_equal(h['loss'], h['weighted_' + loss_full_name], decimal=decimal)
-    
-        assert g._keras_shape == c._keras_shape
-    assert h._keras_shape == d._keras_shape
-    
-        filename = os.path.join(model_name, 'vae_mean.png')
-    # display a 2D plot of the digit classes in the latent space
-    z_mean, _, _ = encoder.predict(x_test,
-                                   batch_size=batch_size)
-    plt.figure(figsize=(12, 10))
-    plt.scatter(z_mean[:, 0], z_mean[:, 1], c=y_test)
-    plt.colorbar()
-    plt.xlabel('z[0]')
-    plt.ylabel('z[1]')
-    plt.savefig(filename)
-    plt.show()
-    
-    model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-    
-    model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
-    
-            model1 = get_model(shape, data_format)
-        model2 = get_model(target_shape, target_data_format)
-        conv = K.function([model1.input], [model1.layers[0].output])
-    
-    
-def test_sigmoid():
-    '''Test using a numerically stable reference sigmoid implementation.
-    '''
-    def ref_sigmoid(x):
-        if x >= 0:
-            return 1 / (1 + np.exp(-x))
+    def main():
+    for name, fn in [('sequential', sequential),
+                     ('processes', with_process_pool_executor),
+                     ('threads', with_thread_pool_executor)]:
+        sys.stdout.write('%s: ' % name.ljust(12))
+        start = time.time()
+        if fn() != [True] * len(PRIMES):
+            sys.stdout.write('failed\n')
         else:
-            z = np.exp(x)
-            return z / (1 + z)
-    sigmoid = np.vectorize(ref_sigmoid)
+            sys.stdout.write('%.2f seconds\n' % (time.time() - start))
     
-                # if the state is not reset, output should be different
-            assert(out1.max() != out2.max())
-    
-    plt.plot(range(epochs),
-         history_model1.history['val_loss'],
-         'g-',
-         label='Network 1 Val Loss')
-plt.plot(range(epochs),
-         history_model2.history['val_loss'],
-         'r-',
-         label='Network 2 Val Loss')
-plt.plot(range(epochs),
-         history_model1.history['loss'],
-         'g--',
-         label='Network 1 Loss')
-plt.plot(range(epochs),
-         history_model2.history['loss'],
-         'r--',
-         label='Network 2 Loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-plt.savefig('comparison_of_networks.png')
+        pil_image.show()
 
     
-    from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
+    # Initialize some variables
+face_locations = []
+face_encodings = []
     
-    plot(euclidean_distances)
-plot(rbf_kernels)
-plt.show()
-
+        # Find all the faces and face enqcodings in the frame of video
+    face_locations = face_recognition.face_locations(rgb_frame)
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
     
-    try:
-    from urllib import urlopen
-except ImportError:
-    from urllib.request import urlopen
+        # Multi-core processing only supported on Python 3.4 or greater
+    if (sys.version_info < (3, 4)) and cpus != 1:
+        click.echo('WARNING: Multi-processing support requires Python 3.4 or greater. Falling back to single-threaded processing!')
+        cpus = 1
     
-    Sentiment analysis can be casted as a binary text classification problem,
-that is fitting a linear classifier on features extracted from the text
-of the user messages so as to guess wether the opinion of the author is
-positive or negative.
-    
-    In both examples below, the main result is that the empirical covariance
-estimate, as a non-robust one, is highly influenced by the heterogeneous
-structure of the observations. Although the robust covariance estimate is
-able to focus on the main mode of the data distribution, it sticks to the
-assumption that the data should be Gaussian distributed, yielding some biased
-estimation of the data structure, but yet accurate to some extent.
-The One-Class SVM does not assume any parametric form of the data distribution
-and can therefore model the complex shape of the data much better.
-    
-    iris = datasets.load_iris()
-X = iris.data[:, 0:2]  # we only take the first two features for visualization
-y = iris.target
-    
-    
-# Code source: Gaël Varoquaux
-# Modified for documentation by Jaques Grobler
-# License: BSD 3 clause
-    
-            ## The token type for the current token
-        self.type = None
-    
-    	xml = get_html('http://www.ehow.com/services/video/series.xml?demand_ehow_videoid=%s' % vid)
-    
-	from xml.dom.minidom import parseString
-	doc = parseString(xml)
-	tab = doc.getElementsByTagName('related')[0].firstChild
-    
-        mediatype, ext, size = 'mp4', 'mp4', 0
-    print_info(site_info, title, mediatype, size)
-    #
-    # rtmpdump  -r 'rtmpe://cp30865.edgefcs.net/ondemand/mtviestor/_!/intlod/MTVInternational/MBUS/GeoLocals/00JP/VIAMTVI/PYC/201304/7122HVAQ4/00JPVIAMTVIPYC7122HVAQ4_640x_360_1200_m30.mp4' -o 'title.mp4' --swfVfy http://media.mtvnservices.com/player/prime/mediaplayerprime.1.10.8.swf
-    #
-    # because rtmpdump is unstable,may try serveral times
-    #
-    if not info_only:
-        # import pdb
-        # pdb.set_trace()
-        download_rtmp_url(url=url, title=title, ext=ext, params={
-                          '--swfVfy': 'http://media.mtvnservices.com/player/prime/mediaplayerprime.1.10.8.swf'}, output_dir=output_dir)
-    
-            # extract title
-        self.title = match1(content,
-                            r'<meta property='og:description' name='og:description' content='([^']+)'')
-    
-    def print_title(title, pattern = '-'):
-    print('\n'.join(('', title, pattern * len(title)))) 
+            # Draw a label with a name below the face
+        text_width, text_height = draw.textsize(name)
+        draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
+        draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
