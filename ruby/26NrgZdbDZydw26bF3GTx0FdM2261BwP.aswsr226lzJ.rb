0b90ba31acce9e5800fc830773d71190bda641c1
SUNsung <weatherius@gmail.com>
2018-07-21 11:16:00 +0300

    Rewrite [26NrgZdbDZydw26bF3GTx0FdM2261BwP.aswsr226lzJ.rb] File in: 2018-07-21 11:15:59

--- ruby/26NrgZdbDZydw26bF3GTx0FdM2261BwP.aswsr226lzJ.rb
+++ ruby/26NrgZdbDZydw26bF3GTx0FdM2261BwP.aswsr226lzJ.rb
@@ -1,49 +1,96 @@
-  # Before we load the schema, define the timestamp_id function.
-  # Idiomatically, we might do this in a migration, but then it
-  # wouldn't end up in schema.rb, so we'd need to figure out a way to
-  # get it in before doing db:setup as well. This is simpler, and
-  # ensures it's always in place.
-  Rake::Task['db:schema:load'].enhance ['db:define_timestamp_id']
+
+        
+              # Returns constant of subscription adapter specified in config/cable.yml.
+      # If the adapter cannot be found, this will default to the Redis adapter.
+      # Also makes sure proper dependencies are required.
+      def pubsub_adapter
+        adapter = (cable.fetch('adapter') { 'redis' })
     
-      attributes :name, :short_name, :description,
-             :icons, :theme_color, :background_color,
-             :display, :start_url, :scope,
-             :share_target
-    
-          it 'sets the regeneration marker to expire' do
-        allow(RegenerationWorker).to receive(:perform_async)
-        get :show
-        expect(Redis.current.ttl('account:#{user.account_id}:regeneration')).to be >= 0
-      end
-    
-      # Creates a delayed logger wrapping `inner`.
-  #
-  # @param inner [Sass::Logger::Base] The wrapped logger.
-  def initialize(inner)
-    self.log_level = inner.log_level
-    @inner = inner
-    @messages = []
-  end
-    
-          def stage_set?
-        !!fetch(:stage, false)
-      end
-    
-          describe 'setting an internal host filter' do
-        subject { dsl.roles(:app) }
-        it 'is ignored' do
-          dsl.set :filter, host: 'example3.com'
-          expect(subject.map(&:hostname)).to eq(['example3.com', 'example4.com'])
+            def test_spec_name_with_inline_config
+          spec = spec('adapter' => 'sqlite3')
+          assert_equal 'primary', spec.name, 'should default to primary id'
         end
       end
-    
-            def initialize(node, variable)
-          unless VARIABLE_ASSIGNMENT_TYPES.include?(node.type)
-            raise ArgumentError,
-                  'Node type must be any of #{VARIABLE_ASSIGNMENT_TYPES}, ' \
-                  'passed #{node.type}'
-          end
-    
-        def _2
-      elements[3]
     end
+  end
+end
+
+    
+        # Abstract representation of a column definition. Instances of this type
+    # are typically created by methods in TableDefinition, and added to the
+    # +columns+ attribute of said TableDefinition object, in order to be used
+    # for generating a number of table creation or table changing SQL statements.
+    ColumnDefinition = Struct.new(:name, :type, :options, :sql_type) do # :nodoc:
+      def primary_key?
+        options[:primary_key]
+      end
+    
+        def get_latest
+      transmit data: 'latest'
+    end
+    
+            assert_equal 1, connection.transmissions.size
+      end
+    end
+  end
+    
+      def wait_for_async
+    wait_for_executor Concurrent.global_io_executor
+  end
+    
+        def test_quit
+      Timeout.timeout(TIMEOUT) do
+        @worker_in.puts 'quit'
+        assert_match(/^bye$/m,@worker_out.read)
+      end
+    end
+  end
+    
+      def test_close_in_block
+    PTY.open {|master, slave|
+      slave.close
+      master.close
+      assert(slave.closed?)
+      assert(master.closed?)
+    }
+  rescue RuntimeError
+    skip $!
+  else
+    assert_nothing_raised {
+      PTY.open {|master, slave|
+        slave.close
+        master.close
+      }
+    }
+  end
+    
+        it 'expand_path for commoms unix path  give a full path' do
+      File.expand_path('/tmp/').should =='/tmp'
+      File.expand_path('/tmp/../../../tmp').should == '/tmp'
+      File.expand_path('').should == Dir.pwd
+      File.expand_path('./////').should == Dir.pwd
+      File.expand_path('.').should == Dir.pwd
+      File.expand_path(Dir.pwd).should == Dir.pwd
+      File.expand_path('~/').should == @home
+      File.expand_path('~/..badfilename').should == '#{@home}/..badfilename'
+      File.expand_path('~/a','~/b').should == '#{@home}/a'
+      File.expand_path('..').should == File.dirname(Dir.pwd)
+    end
+    
+          rm_r filename
+    end
+  end
+    
+          after :each do
+        rm_r @fifo
+      end
+    
+      describe 'rb_cv_set' do
+    it 'sets a class variable' do
+      o = CApiClassSpecs::CVars.new
+      o.new_cv.should be_nil
+      @s.rb_cv_set(CApiClassSpecs::CVars, '@@new_cv', 1)
+      o.new_cv.should == 1
+      CApiClassSpecs::CVars.remove_class_variable :@@new_cv
+    end
+  end
