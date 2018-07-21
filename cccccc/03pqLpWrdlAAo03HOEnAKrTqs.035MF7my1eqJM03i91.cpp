1c37ecde63d3787cceb0d5ec8f4b5b3f5581e949
SUNsung <weatherius@gmail.com>
2018-07-21 14:04:48 +0300

    Rewrite [03pqLpWrdlAAo03HOEnAKrTqs.035MF7my1eqJM03i91.cpp] File in: 2018-07-21 14:04:48

--- cccccc/03pqLpWrdlAAo03HOEnAKrTqs.035MF7my1eqJM03i91.cpp
+++ cccccc/03pqLpWrdlAAo03HOEnAKrTqs.035MF7my1eqJM03i91.cpp
@@ -1,327 +1,148 @@
 
         
-        class RecordWriterOptions {
+        // Like URLRequestAsarJob, but asks the JavaScript handler for file path.
+class URLRequestAsyncAsarJob : public JsAsker<asar::URLRequestAsarJob> {
  public:
-  enum CompressionType { NONE = 0, ZLIB_COMPRESSION = 1 };
-  CompressionType compression_type = NONE;
+  URLRequestAsyncAsarJob(net::URLRequest*, net::NetworkDelegate*);
     }
     
-        auto d_4_1 = cc->FindWorkerChannel('/job:mnist/replica:0/task:3');
-    auto d_4_2 = cc->FindWorkerChannel('/job:mnist/replica:0/task:3');
+    #endif  // ATOM_BROWSER_UNRESPONSIVE_SUPPRESSOR_H_
+
     
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-      // The window_bits parameter is the base two logarithm of the window size
-  // (the size of the history buffer). Larger values of buffer size result in
-  // better compression at the expense of memory usage.
-  //
-  // Accepted values:
-  //
-  // 8..15:
-  // Normal deflate with zlib header and checksum.
-  //
-  // -8..-15:
-  // Negative values can be used for raw deflate/inflate. In this case,
-  // -window_bits determines the window size. deflate() will then generate raw
-  // deflate data  with no zlib header or trailer, and will not compute an
-  // adler32 check value. inflate() will then process raw deflate data, not
-  // looking for a zlib or gzip header, not generating a check value, and not
-  // looking for any check values for comparison at the end of the stream.
-  //
-  // 16 + [8..15]:
-  // window_bits can also be greater than 15 for optional gzip encoding. Add 16
-  // to window_bits to write a simple gzip header and trailer around the
-  // compressed data instead of a zlib wrapper. The gzip header will have no
-  // file name, no extra data, no comment, no modification time (set to zero),
-  // no header crc, and the operating system will be set to 255 (unknown). If a
-  // gzip stream is being written, strm->adler is a crc32 instead of an adler32.
-  //
-  // 0:
-  // window_bits can also be zero to request that inflate use the window size
-  // in the zlib header of the compressed stream.
-  //
-  // While inflating, window_bits must be greater than or equal to the
-  // window_bits value provided used while compressing. If a compressed stream
-  // with a larger window size is given as input, inflate() will return with the
-  // error code Z_DATA_ERROR instead of trying to allocate a larger window.
-  int8 window_bits = MAX_WBITS;
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-            auto numOfColsInMatrix = GetMatrixDimensions(cpuView->Shape()).second + 1;
-        const ElementType* rawNonZeroValues;
-        const SparseIndexType* rawColStarts;
-        const SparseIndexType* rawRowIndices;
-    
-        static ProgressTracing& GetStaticInstance()
-    {
-        static ProgressTracing us;
-        return us;
-    } // wrap static state in an accessor, so we won't need a CPP file
-    
-    class latticepair : public std::pair<msra::lattices::lattice, msra::lattices::lattice>
-{
-public:
-    // NOTE: we don't check numerator lattice now
-    size_t getnumframes() const
-    {
-        return second.getnumframes();
+    // Wrapper used to keep track of the lifetime of a WebContents.
+// Lives on the UI thread.
+class PrintingUIWebContentsObserver : public content::WebContentsObserver {
+ public:
+  explicit PrintingUIWebContentsObserver(content::WebContents* web_contents);
     }
-    size_t getnumnodes() const
-    {
-        return second.getnumnodes();
-    }
-    size_t getnumedges() const
-    {
-        return second.getnumedges();
-    }
-    std::wstring getkey() const
-    {
-        return second.getkey();
-    }
+    
+    #if defined(OS_MACOSX)
+// NOTE: if you change the value of kFrameworkName, please don't forget to
+// update components/test/run_all_unittests.cc as well.
+// TODO(tfarina): Remove the comment above, when you fix components to use plist
+// on Mac.
+extern const base::FilePath::CharType kFrameworkName[];
+#endif  // OS_MACOSX
+    
+    static void TestEncodeDecode(const VersionEdit& edit) {
+  std::string encoded, encoded2;
+  edit.EncodeTo(&encoded);
+  VersionEdit parsed;
+  Status s = parsed.DecodeFrom(encoded);
+  ASSERT_TRUE(s.ok()) << s.ToString();
+  parsed.EncodeTo(&encoded2);
+  ASSERT_EQ(encoded, encoded2);
+}
+    
+    TEST(BloomTest, EmptyFilter) {
+  ASSERT_TRUE(! Matches('hello'));
+  ASSERT_TRUE(! Matches('world'));
+}
+    
+      // crc32c values for all supported record types.  These are
+  // pre-computed to reduce the overhead of computing the crc of the
+  // record type stored in the header.
+  uint32_t type_crc_[kMaxRecordType + 1];
+    
+    struct BlockContents;
+class Comparator;
+    
+    template <typename Stream>
+class buffered_write_stream;
+    
+      BOOST_ASIO_DECL static void init_native_buffer(
+      native_buffer_type& buf,
+      const boost::asio::mutable_buffer& buffer);
+    
+        // The key associated with the context.
+    Key* key_;
+    
+    
+    {private:
+  int descriptor_;
+  MutableBufferSequence buffers_;
 };
     
-    public:
-    float4()
-    {
-    } // uninitialized
-    float4(const float4& f4)
-        : v(f4.v)
-    {
-    }
-    float4& operator=(const float4& other)
-    {
-        v = other.v;
-        return *this;
-    }
+    #include <boost/asio/detail/posix_fd_set_adapter.hpp>
+#include <boost/asio/detail/win_fd_set_adapter.hpp>
     
-        // Destructor - free up the matrix values we allocated
-    virtual ~EvalWriter()
-    {
-    }
-    
-    enum RecordType {
-  // Zero is reserved for preallocated files
-  kZeroType = 0,
-    }
-    
-    #endif  // STORAGE_LEVELDB_TABLE_FILTER_BLOCK_H_
-
-    
-    void DBIter::FindPrevUserEntry() {
-  assert(direction_ == kReverse);
-    }
-    
-    int InternalKeyComparator::Compare(const Slice& akey, const Slice& bkey) const {
-  // Order by:
-  //    increasing user key (according to user-supplied comparator)
-  //    decreasing sequence number
-  //    decreasing type (though sequence# should be enough to disambiguate)
-  int r = user_comparator_->Compare(ExtractUserKey(akey), ExtractUserKey(bkey));
-  if (r == 0) {
-    const uint64_t anum = DecodeFixed64(akey.data() + akey.size() - 8);
-    const uint64_t bnum = DecodeFixed64(bkey.data() + bkey.size() - 8);
-    if (anum > bnum) {
-      r = -1;
-    } else if (anum < bnum) {
-      r = +1;
-    }
+      // Destructor.
+  ~gcc_arm_fenced_block()
+  {
+    barrier();
   }
-  return r;
-}
     
-    bool HandleDumpCommand(Env* env, char** files, int num) {
-  StdoutPrinter printer;
-  bool ok = true;
-  for (int i = 0; i < num; i++) {
-    Status s = DumpFile(env, files[i], &printer);
-    if (!s.ok()) {
-      fprintf(stderr, '%s\n', s.ToString().c_str());
-      ok = false;
-    }
-  }
-  return ok;
-}
-    
-        // Invariant: we never leave < kHeaderSize bytes in a block.
-    assert(kBlockSize - block_offset_ - kHeaderSize >= 0);
-    
-    std::vector<float> YUVToRGB(const std::vector<std::vector<float> >& yuv) {
-  std::vector<float> rgb(3 * yuv[0].size());
-  for (size_t i = 0, p = 0; p < rgb.size(); ++i, p += 3) {
-    const float y = yuv[0][i];
-    const float u = yuv[1][i];
-    const float v = yuv[2][i];
-    rgb[p + 0] = Clip(YUVToR(y, u, v));
-    rgb[p + 1] = Clip(YUVToG(y, u, v));
-    rgb[p + 2] = Clip(YUVToB(y, u, v));
-  }
-  return rgb;
-}
-    
-    // This function will create a Huffman tree.
-//
-// The catch here is that the tree cannot be arbitrarily deep.
-// Brotli specifies a maximum depth of 15 bits for 'code trees'
-// and 7 bits for 'code length code trees.'
-//
-// count_limit is the value that is to be faked as the minimum value
-// and this minimum value is raised until the tree matches the
-// maximum length requirement.
-//
-// This algorithm is not of excellent performance for very long data blocks,
-// especially when population counts are longer than 2**tree_limit, but
-// we are not planning to use this with extremely long blocks.
-//
-// See http://en.wikipedia.org/wiki/Huffman_coding
-void CreateHuffmanTree(const uint32_t *data,
-                       const size_t length,
-                       const int tree_limit,
-                       HuffmanTree* tree,
-                       uint8_t *depth) {
-  // For block sizes below 64 kB, we never need to do a second iteration
-  // of this loop. Probably all of our block sizes will be smaller than
-  // that, so this loop is mostly of academic interest. If we actually
-  // would need this, we would be better off with the Katajainen algorithm.
-  for (uint32_t count_limit = 1; ; count_limit *= 2) {
-    size_t n = 0;
-    for (size_t i = length; i != 0;) {
-      --i;
-      if (data[i]) {
-        const uint32_t count = std::max<uint32_t>(data[i], count_limit);
-        tree[n++] = HuffmanTree(count, -1, static_cast<int16_t>(i));
-      }
-    }
-    }
-    }
-    
-    inline int Log2Floor(uint32_t n) {
-  return n == 0 ? -1 : Log2FloorNonZero(n);
-}
-    
-    const double* NewSrgb8ToLinearTable() {
-  double* table = new double[256];
-  int i = 0;
-  for (; i < 11; ++i) {
-    table[i] = i / 12.92;
-  }
-  for (; i < 256; ++i) {
-    table[i] = 255.0 * std::pow(((i / 255.0) + 0.055) / 1.055, 2.4);
-  }
-  return table;
-}
-    
-      tmp0 = in[4 * stride];
-  tmp1 = kIDCTMatrix[ 4] * tmp0;
-  out[0] += tmp1;
-  out[1] -= tmp1;
-  out[2] -= tmp1;
-  out[3] += tmp1;
-  out[4] += tmp1;
-  out[5] -= tmp1;
-  out[6] -= tmp1;
-  out[7] += tmp1;
-    
-    // Creates a JPEG from the rgb pixel data. Returns true on success. The given
-// quantization table must have 3 * kDCTBlockSize values.
-bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
-                     const int* quant, JPEGData* jpg);
-    
-    #ifndef GUETZLI_JPEG_DATA_WRITER_H_
-#define GUETZLI_JPEG_DATA_WRITER_H_
-    
-    #include 'guetzli/quality.h'
-    
-    void*         XXH32_init   (unsigned int seed);
-XXH_errorcode XXH32_update (void* state, const void* input, int len);
-unsigned int  XXH32_digest (void* state);
-    
-    
-    {
-    {    // Append, Flush, Compact, Get
-    slists.Append('b', 'afcg');
-    db->Flush(rocksdb::FlushOptions());
-    db->CompactRange(CompactRangeOptions(), nullptr, nullptr);
-    slists.Get('b', &b);
-    ASSERT_EQ(b, 'y\n2\nmonkey\ndf\nl;\nafcg');
-  }
-}
-    
-    /**
- * @brief create a new write file handler
- * @details it will check the existence of fname
- *
- * @param fname [description]
- * @param result [description]
- * @param options [description]
- * @return [description]
- */
-Status EnvLibrados::NewWritableFile(
-  const std::string& fname,
-  std::unique_ptr<WritableFile>* result,
-  const EnvOptions& options)
+    template <typename Handler>
+inline void* allocate(std::size_t s, Handler& h)
 {
-  LOG_DEBUG('[IN]%s\n', fname.c_str());
-  std::string dir, file, fid;
-  split(fname, &dir, &file);
-  Status s;
-  std::string fpath = dir + '/' + file;
-    }
-    
-      // If memtable contains a value for key, store it in *value and return true.
-  // If memtable contains a deletion for key, store a NotFound() error
-  // in *status and return true.
-  // If memtable contains Merge operation as the most recent entry for a key,
-  //   and the merge process does not stop (not reaching a value or delete),
-  //   prepend the current merge operand to *operands.
-  //   store MergeInProgress in s, and return false.
-  // Else, return false.
-  // If any operation was found, its most recent sequence number
-  // will be stored in *seq on success (regardless of whether true/false is
-  // returned).  Otherwise, *seq will be set to kMaxSequenceNumber.
-  // On success, *s may be set to OK, NotFound, or MergeInProgress.  Any other
-  // status returned indicates a corruption or other unexpected error.
-  bool Get(const LookupKey& key, std::string* value, Status* s,
-           MergeContext* merge_context, RangeDelAggregator* range_del_agg,
-           SequenceNumber* seq, const ReadOptions& read_opts,
-           ReadCallback* callback = nullptr, bool* is_blob_index = nullptr);
-    
-      virtual const char* Name() const = 0;
-    
-    #endif  // JAVA_ROCKSJNI_COMPACTION_FILTER_FACTORY_JNICALLBACK_H_
-
-    
-      if (jsResultStart != nullptr) {
-    // update start with result
-    jboolean has_exception = JNI_FALSE;
-    std::unique_ptr<const char[]> result_start = JniUtil::copyString(env, jsResultStart,
-        &has_exception);  // also releases jsResultStart
-    if (has_exception == JNI_TRUE) {
-      if (env->ExceptionCheck()) {
-        env->ExceptionDescribe();  // print out exception to stderr
-      }
-      releaseJniEnv(attached_thread);
-      return;
-    }
-    }
-    
-    
-    {  assert(odds > 0);
-  if (odds % 7 == 0) {
-    // class Random uses multiplier 16807, which is 7^5. If odds are
-    // multiplier of 7, there might be limited values generated.
-    odds++;
-  }
-  auto* r = Random::GetTLSInstance();
-  bool crash = r->OneIn(odds);
-  if (crash) {
-    port::Crash(srcfile, srcline);
-  }
+#if !defined(BOOST_ASIO_HAS_HANDLER_HOOKS)
+  return ::operator new(s);
+#else
+  using boost::asio::asio_handler_allocate;
+  return asio_handler_allocate(s, boost::asio::detail::addressof(h));
+#endif
 }
+    
+      // Find an entry in the map.
+  const_iterator find(const K& k) const
+  {
+    if (num_buckets_)
+    {
+      size_t bucket = calculate_hash_value(k) % num_buckets_;
+      const_iterator it = buckets_[bucket].first;
+      if (it == values_.end())
+        return it;
+      const_iterator end_it = buckets_[bucket].last;
+      ++end_it;
+      while (it != end_it)
+      {
+        if (it->first == k)
+          return it;
+        ++it;
+      }
+    }
+    return values_.end();
+  }
+    
+    #ifndef BOOST_ASIO_DETAIL_IMPL_DEV_POLL_REACTOR_HPP
+#define BOOST_ASIO_DETAIL_IMPL_DEV_POLL_REACTOR_HPP
+    
+    #include <memory>
+#include <string>
+#include <vector>
+    
+    CollisionChecker::CollisionChecker(
+    const std::vector<const Obstacle*>& obstacles,
+    const double ego_vehicle_s,
+    const double ego_vehicle_d,
+    const std::vector<PathPoint>& discretized_reference_line,
+    const ReferenceLineInfo* ptr_reference_line_info,
+    const std::shared_ptr<PathTimeGraph>& ptr_path_time_graph) {
+  ptr_reference_line_info_ = ptr_reference_line_info;
+  ptr_path_time_graph_ = ptr_path_time_graph;
+  BuildPredictedEnvironment(obstacles, ego_vehicle_s, ego_vehicle_d,
+                            discretized_reference_line);
+}
+    
+    void Box2d::GetAllCorners(std::vector<Vec2d> *const corners) const {
+  if (corners == nullptr) {
+    return;
+  }
+  *corners = corners_;
+}
+    
+      /**
+   * @brief Rotate from center.
+   * @param rotate_angle Angle to rotate.
+   */
+  void RotateFromCenter(const double rotate_angle);
+    
+      std::vector<PathTimePoint> GetObstacleSurroundingPoints(
+      const std::string& obstacle_id, const double s_dist,
+      const double t_density) const;
+    
+    #include 'modules/perception/traffic_light/interface/base_projection.h'
+    
+    
+    {  std::vector<common::TrajectoryPoint> complete_rtk_trajectory_;
+};
