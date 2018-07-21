0ef8b24660233c2c01ef5903fd8b03935b9e5285
SUNsung <weatherius@gmail.com>
2018-07-21 05:03:41 +0300

    Rewrite [435kO3PoUjRHk430wZQF0OUujw43slTJBTU0i5E43L2P.rb] File in: 2018-07-21 05:03:40

--- ruby/435kO3PoUjRHk430wZQF0OUujw43slTJBTU0i5E43L2P.rb
+++ ruby/435kO3PoUjRHk430wZQF0OUujw43slTJBTU0i5E43L2P.rb
@@ -1,110 +1,131 @@
 
         
-        describe Deliver::HtmlGenerator do
-  let(:generator) { Deliver::HtmlGenerator.new }
+        end
+
     
-          def task_available?(task)
-        load_all_tasks
-        return tasks.collect(&:title).include?(task)
-      end
-    
-          it 'does set the output directory' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            cloc(output_directory: '/tmp')
-          end').runner.execute(:test)
-    
-              If there is multiple cache for various versions of the requested pod,
-          you will be asked which one to clean. Use `--all` to clean them all.
-    
-    @@ chat
-<pre id='chat'></pre>
-<form>
-  <input id='msg' placeholder='type message here...' />
-</form>
-    
-        it 'redirects requests with duplicate session cookies' do
-      get '/', {}, 'HTTP_COOKIE' => 'rack.session=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
-      expect(last_response).to be_redirect
-      expect(last_response.location).to eq('/')
+        def unlock_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :unlock_instructions, opts)
     end
     
-      context 'escaping' do
-    it 'escapes html entities' do
-      mock_app do |env|
-        request = Rack::Request.new(env)
-        [200, {'Content-Type' => 'text/plain'}, [request.params['foo']]]
-      end
-      get '/', :foo => '<bar>'
-      expect(body).to eq('&lt;bar&gt;')
+        if record.timedout?(last_request_at) &&
+        !env['devise.skip_timeout'] &&
+        !proxy.remember_me_is_active?(record)
+      Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
+      throw :warden, scope: scope, message: :timeout
     end
     
-      it 'should set the X-Frame-Options' do
-    expect(get('/', {}, 'wants' => 'text/html').headers['X-Frame-Options']).to eq('SAMEORIGIN')
-  end
+          protected
     
-        mock_app do
-      use Rack::Head
-      use(Rack::Config) { |e| e['rack.session'] ||= {}}
-      use detector
-      use klass
-      run DummyApp
+        def default_defaults(options)
+      @defaults = Hash.new
+      @defaults.merge!(options[:defaults]) if options[:defaults]
     end
     
-              if @address.update_attributes(address_params)
-            respond_with(@address, default_template: :show)
-          else
-            invalid_resource!(@address)
-          end
-        end
+          html_filters.push 'coffeescript/clean_html_v1', 'coffeescript/entries_v1', 'title'
     
-            def create
-          authorize! :create, StockMovement
-          @stock_movement = scope.new(stock_movement_params)
-          if @stock_movement.save
-            respond_with(@stock_movement, status: 201, default_template: :show)
-          else
-            invalid_resource!(@stock_movement)
-          end
-        end
+        version do
+      self.links = {
+        home: 'https://haxe.org',
+        code: 'https://github.com/HaxeFoundation/haxe'
+      }
     
-            def each_unnecessary_space_match(node, &blk)
-          each_match_range(
-            contents_range(node),
-            MULTIPLE_SPACES_BETWEEN_ITEMS_REGEX,
-            &blk
-          )
-        end
+        def parse(response)
+      unless response.url == root_url || self.class.version == 'Guide'
+        response.body.sub!(/<nav class='devsite-nav-responsive-sidebar.+?<\/nav>/m, '')
+        response.body.gsub!(/<li class='devsite-nav-item'>.+?<\/li>/m, '')
       end
+    
+        def as_json
+      { name: name, path: path, type: type }
     end
   end
 end
 
     
-    module RuboCop
-  module Cop
-    module Performance
-      # This cop identifies the use of `Regexp#match` or `String#match`, which
-      # returns `#<MatchData>`/`nil`. The return value of `=~` is an integral
-      # index/`nil` and is more performant.
-      #
-      # @example
-      #   # bad
-      #   do_something if str.match(/regex/)
-      #   while regex.match('str')
-      #     do_something
-      #   end
-      #
-      #   # good
-      #   method(str =~ /regex/)
-      #   return value unless regex =~ 'str'
-      class RedundantMatch < Cop
-        MSG = 'Use `=~` in places where the `MatchData` returned by ' \
-              '`#match` will not be used.'.freeze
+        def add(path, content)
+      @pages[path] = content
+    end
     
-        context 'opening brace on same line as first element' do
-      context 'last element has a trailing comma' do
-        it 'autocorrects closing brace on different line from last element' do
-          new_source = autocorrect_source(['#{prefix}#{open}#{a}, # a',
-                                           '#{b}, # b',
-                                           close,
-                                           suffix])
+        def origin
+      if scheme && host
+        origin = '#{scheme}://#{host}'
+        origin.downcase!
+        origin << ':#{port}' if port
+        origin
+      else
+        nil
+      end
+    end
+    
+    load_extension('typed_data')
+    
+      it 'adds the directory at the front of $LOAD_PATH' do
+    dir = tmp('rubylib/incl_front')
+    ENV['RUBYLIB'] = @pre + dir
+    paths = ruby_exe('puts $LOAD_PATH').lines.map(&:chomp)
+    if PlatformGuard.implementation? :ruby
+      # In a MRI checkout, $PWD and some extra -I entries end up as
+      # the first entries in $LOAD_PATH. So just assert that it's not last.
+      idx = paths.index(dir)
+      idx.should < paths.size-1
+    else
+      paths[0].should == dir
+    end
+  end
+    
+      DIGEST_ALGORITHM =
+    if defined?(OpenSSL::Digest::SHA256) then
+      OpenSSL::Digest::SHA256
+    elsif defined?(OpenSSL::Digest::SHA1) then
+      OpenSSL::Digest::SHA1
+    else
+      require 'digest'
+      Digest::SHA512
+    end
+    
+        File.open gemspec_file, 'w' do |gs|
+      gs.write @gem.to_ruby
+    end
+    
+      def test_document
+    @cmd.handle_options %w[--document]
+    
+      # I don't know Ruby's spec about 'unlink-before-close' exactly.
+  # This test asserts current behaviour.
+  def test_unlink_before_close
+    Dir.mktmpdir('rubytest-file') {|tmpdir|
+      filename = tmpdir + '/' + File.basename(__FILE__) + '.#{$$}'
+      w = File.open(filename, 'w')
+      w << 'foo'
+      w.close
+      r = File.open(filename, 'r')
+      begin
+        if /(mswin|bccwin|mingw|emx)/ =~ RUBY_PLATFORM
+          assert_raise(Errno::EACCES) {File.unlink(filename)}
+        else
+          assert_nothing_raised {File.unlink(filename)}
+        end
+      ensure
+        r.close
+        File.unlink(filename) if File.exist?(filename)
+      end
+    }
+  end
+    
+    Given an Application (app) bundle directory on disk, find all
+login items associated with that app, which you can use in a
+Cask uninstall stanza, eg
+    
+    ; TODO make this stricter if possible
+(allow network-outbound)
+    
+          def run
+        UI.puts report
+      end
+    
+            def self.options
+          [
+            ['--only-errors', 'Lint presents only the errors'],
+          ].concat(super)
+        end
