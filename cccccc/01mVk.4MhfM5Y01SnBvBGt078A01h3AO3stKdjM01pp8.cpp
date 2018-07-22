9cba6473e254dcc4a6d0434db1e28560d4eb2ff2
SUNsung <weatherius@gmail.com>
2018-07-22 15:22:49 +0300

    Rewrite [01mVk.4MhfM5Y01SnBvBGt078A01h3AO3stKdjM01pp8.cpp] File in: 2018-07-22 15:22:48

--- cccccc/01mVk.4MhfM5Y01SnBvBGt078A01h3AO3stKdjM01pp8.cpp
+++ cccccc/01mVk.4MhfM5Y01SnBvBGt078A01h3AO3stKdjM01pp8.cpp
@@ -1,42 +1,140 @@
 
         
-        #ifndef ATOM_BROWSER_UI_X_EVENT_DISABLER_H_
-#define ATOM_BROWSER_UI_X_EVENT_DISABLER_H_
+        #include 'base/logging.h'
+#include 'base/values.h'
     
-    bool IsUnresponsiveEventSuppressed() {
-  return g_suppress_level > 0;
+    namespace nw {
+    }
+    
+    v8::Handle<v8::Value> AllocateId(int routing_id);
+    
+    EventListener::EventListener(int id,
+  const base::WeakPtr<DispatcherHost>& dispatcher_host,
+  const base::DictionaryValue& option) : Base(id, dispatcher_host, option) {
+    }
+    
+       bool IsItemForCommandIdDynamic(int command_id) const override;
+   base::string16 GetLabelForCommandId(int command_id) const override;
+   bool GetIconForCommandId(int command_id,
+                                   gfx::Image* icon) const override;
+    
+    namespace nwapi {
+    }
+    
+    class NwAppClearCacheFunction : public NWSyncExtensionFunction, public content::BrowsingDataRemover::Observer {
+ public:
+  NwAppClearCacheFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+  void OnBrowsingDataRemoverDone() override;
+    }
+    
+    namespace extensions {
+    }
+    
+    
+    {  DECLARE_EXTENSION_FUNCTION('nw.Clipboard.readAvailableTypes', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwClipboardReadAvailableTypesFunction);
+};
+    
+    
+    {    std::unique_ptr<base::ListValue> args = nwapi::nw__screen::OnSourceThumbnailChanged::Create(
+      src.id.ToString(),
+      base64);
+    DispatchEvent(
+      events::HistogramValue::UNKNOWN, 
+      nwapi::nw__screen::OnSourceThumbnailChanged::kEventName,
+      std::move(args));
+  }
+    
+    template<> inline
+dnnError_t dnnInnerProductCreateForwardBias<float>(
+    dnnPrimitive_t* pInnerProduct,
+    dnnPrimitiveAttributes_t attributes,
+    size_t dimensions,
+    const size_t srcSize[],
+    size_t outputChannels)
+{
+    return dnnInnerProductCreateForwardBias_F32(
+        pInnerProduct, attributes, dimensions, srcSize, outputChannels);
 }
     
-    #endif  // ATOM_COMMON_DRAGGABLE_REGION_H_
-
+            DestType *destData = dest.data();
+        if (elementCount > dest.size())
+            RuntimeError('Copy: The output buffer size (%zu) is smaller than the number (%zu) of source elements to copy.', dest.size(), elementCount);
     
-    namespace chrome {
+    // print model stats
+// Returns a pair (model params, non-null model params) for aggregate statistics printing.
+template <class M>
+std::pair<unsigned int, unsigned int> printmatvaluedistributionf(const char *name, const M &m)
+{
+    const unsigned int num = (unsigned int) (m.rows() * m.cols());
+    if (num == 0)
+        return std::make_pair(0UL, 0UL);
+    fprintf(stderr, '\n###### absolute weight value distribution %s (%d, %d) ######\n', name, m.rows(), m.cols());
     }
     
-      // Returns the bounds with its size clamped to the min/max size.
-  gfx::Size ClampSize(gfx::Size size) const;
+        if (useDistributedMBReading)
+        dataReader->StartDistributedMinibatchLoop(mbSize, 0, m_mpi->CurrentNodeRank(), m_mpi->NumNodesInUse(), inputMatrices.GetStreamDescriptions(), totalEpochSize);
+    else
+        dataReader->StartMinibatchLoop(mbSize, 0, inputMatrices.GetStreamDescriptions(), totalEpochSize);
     
-    #endif  // ATOM_COMMON_COLOR_UTIL_H_
-
-    
-    void projectPoints(cv::InputArray objectPoints, cv::OutputArray imagePoints,
-                   cv::InputArray _rvec,cv::InputArray _tvec,
-                   const IntrinsicParams& param, cv::OutputArray jacobian);
-    
-                template<typename T>
-            static __device__ __forceinline__ T atomicMin(T* address, T val)
-            {
-#if defined (__CUDA_ARCH__) && (__CUDA_ARCH__ < 120)
-                T count = ::min(*address, val);
-                do
-                {
-                    *address = count;
-                } while (*address > count);
+    class latticesource
+{
+    const msra::lattices::archive numlattices, denlattices;
+    int verbosity;
     }
     
-    /** @file
- * @deprecated Use @ref cudev instead.
- */
+    private:
+    static VariableLayout ToVariableLayout(const ComputationNodeBasePtr n);
+    std::vector<ComputationNodeBasePtr> m_outputNodes;
+    std::shared_ptr<ScopedNetworkOperationMode> m_scopedNetworkOperationMode;
+    std::vector<ComputationNodeBasePtr> m_inputNodes;
+    StreamMinibatchInputs m_inputMatrices;
+    bool m_started;
+    
+      // Threshold for where considered an edge.
+  const double threshold = (channel == 2 ? 0.02 : 1.0) * 127.5;
+    
+    #ifndef GUETZLI_COLOR_TRANSFORM_H_
+#define GUETZLI_COLOR_TRANSFORM_H_
+    
+    #include <assert.h>
+#include <algorithm>
     
     
-    {}
+    {}  // namespace guetzli
+    
+    std::vector<uint8_t> DecodeJpegToRGB(const JPEGData& jpg) {
+  if (jpg.components.size() == 1 ||
+      (jpg.components.size() == 3 &&
+       HasYCbCrColorSpace(jpg) && (jpg.Is420() || jpg.Is444()))) {
+    OutputImage img(jpg.width, jpg.height);
+    img.CopyFromJpegData(jpg);
+    return img.ToSRGB();
+  }
+  return std::vector<uint8_t>();
+}
+    
+    
+    {  return true;
+}
+    
+    size_t HistogramEntropyCost(const JpegHistogram& histo,
+                            const uint8_t depths[256]);
+size_t HistogramHeaderCost(const JpegHistogram& histo);
+    
+    // Definition of error codes for parsing jpeg files.
+    
+      // Special case code with only one value.
+  if (total_count == 1) {
+    code.bits = 0;
+    code.value = symbols[0];
+    for (key = 0; key < total_size; ++key) {
+      table[key] = code;
+    }
+    return total_size;
+  }
+    
+    
+    {}  // namespace guetzli
