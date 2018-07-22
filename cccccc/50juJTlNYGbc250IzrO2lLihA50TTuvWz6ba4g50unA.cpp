0eb111b8a34b282fd18d3a657288c16a297698be
SUNsung <weatherius@gmail.com>
2018-07-22 10:16:57 +0300

    Rewrite [50juJTlNYGbc250IzrO2lLihA50TTuvWz6ba4g50unA.cpp] File in: 2018-07-22 10:16:56

--- cccccc/50juJTlNYGbc250IzrO2lLihA50TTuvWz6ba4g50unA.cpp
+++ cccccc/50juJTlNYGbc250IzrO2lLihA50TTuvWz6ba4g50unA.cpp
@@ -1,109 +1,213 @@
 
         
-        static void RunSynchronousUnaryPingPong() {
-  gpr_log(GPR_INFO, 'Running Synchronous Unary Ping Pong');
+        namespace leveldb {
     }
     
-    int main(int argc, char** argv) {
-  grpc::testing::InitTest(&argc, &argv, true);
+    namespace leveldb {
     }
     
-    #include <string>
-#include <vector>
+    struct FileMetaData {
+  int refs;
+  int allowed_seeks;          // Seeks allowed until compaction
+  uint64_t number;
+  uint64_t file_size;         // File size in bytes
+  InternalKey smallest;       // Smallest internal key served by table
+  InternalKey largest;        // Largest internal key served by table
+    }
     
-    // Get leading or trailing comments in a string. Comment lines start with '// '.
-// Leading detached comments are put in in front of leading comments.
-template <typename DescriptorType>
-inline grpc::string GetNodeComments(const DescriptorType* desc, bool leading) {
-  return grpc_generator::GetPrefixedComments(desc, leading, '//');
+    TEST(WriteBatchTest, Append) {
+  WriteBatch b1, b2;
+  WriteBatchInternal::SetSequence(&b1, 200);
+  WriteBatchInternal::SetSequence(&b2, 300);
+  WriteBatchInternal::Append(&b1, &b2);
+  ASSERT_EQ('',
+            PrintContents(&b1));
+  b2.Put('a', 'va');
+  WriteBatchInternal::Append(&b1, &b2);
+  ASSERT_EQ('Put(a, va)@200',
+            PrintContents(&b1));
+  b2.Clear();
+  b2.Put('b', 'vb');
+  WriteBatchInternal::Append(&b1, &b2);
+  ASSERT_EQ('Put(a, va)@200'
+            'Put(b, vb)@201',
+            PrintContents(&b1));
+  b2.Delete('foo');
+  WriteBatchInternal::Append(&b1, &b2);
+  ASSERT_EQ('Put(a, va)@200'
+            'Put(b, vb)@202'
+            'Put(b, vb)@201'
+            'Delete(foo)@203',
+            PrintContents(&b1));
 }
     
-    #include <iostream>
-#include <vector>
-    
-      Result Mark() const;
-    
-    #include 'src/proto/grpc/testing/metrics.grpc.pb.h'
-#include 'src/proto/grpc/testing/metrics.pb.h'
-    
-      // Compares img with the baseline image and saves the resulting distance map
-  // inside the object. The provided image must have the same dimensions as the
-  // baseline image.
-  virtual void Compare(const OutputImage& img) = 0;
-    
-    OutputImageComponent::OutputImageComponent(int w, int h)
-    : width_(w), height_(h) {
-  Reset(1, 1);
-}
-    
-    #endif  // GUETZLI_COLOR_TRANSFORM_H_
-
-    
-    
-    {
-    {
-    {      // Add back the last sentinel node.
-      tree[j_end + 1] = sentinel;
-    }
-    if (SetDepth(static_cast<int>(2 * n - 1), &tree[0], depth, tree_limit)) {
-      /* We need to pack the Huffman tree in tree_limit bits. If this was not
-         successful, add fake entities to the lowest values and retry. */
-      break;
-    }
+    static Slice TrimSpace(Slice s) {
+  int start = 0;
+  while (start < s.size() && isspace(s[start])) {
+    start++;
   }
+  int limit = s.size();
+  while (limit > start && isspace(s[limit-1])) {
+    limit--;
+  }
+  return Slice(s.data() + start, limit - start);
 }
     
-    #endif  // GUETZLI_FAST_LOG_H_
-
     
-    const double* Srgb8ToLinearTable() {
-  static const double* const kSrgb8ToLinearTable = NewSrgb8ToLinearTable();
-  return kSrgb8ToLinearTable;
-}
+    {}  // namespace leveldb
     
-    enum JPEGReadError {
-  JPEG_OK = 0,
-  JPEG_SOI_NOT_FOUND,
-  JPEG_SOF_NOT_FOUND,
-  JPEG_UNEXPECTED_EOF,
-  JPEG_MARKER_BYTE_NOT_FOUND,
-  JPEG_UNSUPPORTED_MARKER,
-  JPEG_WRONG_MARKER_SIZE,
-  JPEG_INVALID_PRECISION,
-  JPEG_INVALID_WIDTH,
-  JPEG_INVALID_HEIGHT,
-  JPEG_INVALID_NUMCOMP,
-  JPEG_INVALID_SAMP_FACTOR,
-  JPEG_INVALID_START_OF_SCAN,
-  JPEG_INVALID_END_OF_SCAN,
-  JPEG_INVALID_SCAN_BIT_POSITION,
-  JPEG_INVALID_COMPS_IN_SCAN,
-  JPEG_INVALID_HUFFMAN_INDEX,
-  JPEG_INVALID_QUANT_TBL_INDEX,
-  JPEG_INVALID_QUANT_VAL,
-  JPEG_INVALID_MARKER_LEN,
-  JPEG_INVALID_SAMPLING_FACTORS,
-  JPEG_INVALID_HUFFMAN_CODE,
-  JPEG_INVALID_SYMBOL,
-  JPEG_NON_REPRESENTABLE_DC_COEFF,
-  JPEG_NON_REPRESENTABLE_AC_COEFF,
-  JPEG_INVALID_SCAN,
-  JPEG_OVERLAPPING_SCANS,
-  JPEG_INVALID_SCAN_ORDER,
-  JPEG_EXTRA_ZERO_RUN,
-  JPEG_DUPLICATE_DRI,
-  JPEG_DUPLICATE_SOF,
-  JPEG_WRONG_RESTART_MARKER,
-  JPEG_DUPLICATE_COMPONENT_ID,
-  JPEG_COMPONENT_NOT_FOUND,
-  JPEG_HUFFMAN_TABLE_NOT_FOUND,
-  JPEG_HUFFMAN_TABLE_ERROR,
-  JPEG_QUANT_TABLE_NOT_FOUND,
-  JPEG_EMPTY_DHT,
-  JPEG_EMPTY_DQT,
-  JPEG_OUT_OF_BAND_COEFF,
-  JPEG_EOB_RUN_TOO_LONG,
-  JPEG_IMAGE_TOO_LARGE,
+    #ifndef STORAGE_LEVELDB_INCLUDE_SLICE_H_
+#define STORAGE_LEVELDB_INCLUDE_SLICE_H_
+    
+    #include <map>
+#include <string>
+#include <vector>
+    
+      virtual inline const char* type() const { return 'Convolution'; }
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #ifdef USE_CUDNN
+/*
+ * @brief cuDNN implementation of PoolingLayer.
+ *        Fallback to PoolingLayer for CPU mode.
+*/
+template <typename Dtype>
+class CuDNNPoolingLayer : public PoolingLayer<Dtype> {
+ public:
+  explicit CuDNNPoolingLayer(const LayerParameter& param)
+      : PoolingLayer<Dtype>(param), handles_setup_(false) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNPoolingLayer();
+  // Currently, cuDNN does not support the extra top blob.
+  virtual inline int MinTopBlobs() const { return -1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+    }
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+      /**
+   * @brief Computes the error gradient w.r.t. the exp inputs.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *      respect to the outputs
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
+   *      with respect to computed outputs @f$ y @f$
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$; Backward fills their diff with
+   *      gradients @f$
+   *        \frac{\partial E}{\partial x} =
+   *            \frac{\partial E}{\partial y} y \alpha \log_e(gamma)
+   *      @f$ if propagate_down[0]
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    const char *kHyphenLikeUTF8[] = {
+  '-',       // ASCII hyphen-minus
+  '\u05BE',  // word hyphen in hybrew
+  '\u2010',  // hyphen
+  '\u2011',  // non-breaking hyphen
+  '\u2012',  // a hyphen the same width as digits
+  '\u2013',  // en dash
+  '\u2014',  // em dash
+  '\u2015',  // horizontal bar
+  '\u2212',  // arithmetic minus sign
+  '\uFE58',  // small em dash
+  '\uFE63',  // small hyphen-minus
+  '\uFF0D',  // fullwidth hyphen-minus
+  nullptr,      // end of our list
 };
     
-    // Preprocesses U and V channel for better results after downsampling.
+     private:
+  // Computes matrix.vector v = Wu.
+  // u is of size starts.back()+extents.back() and the output v is of size
+  // starts.size().
+  // The weight matrix w, is of size starts.size()xMAX(extents)+add_bias_fwd.
+  // If add_bias_fwd, an extra element at the end of w[i] is the bias weight
+  // and is added to v[i].
+  static void MatrixDotVectorInternal(const GENERIC_2D_ARRAY<double>& w,
+                                      bool add_bias_fwd, bool skip_bias_back,
+                                      const double* u, double* v);
+    
+      // Clears the memory held by this object.
+  void Clear();
+    
+    
+    {  edgept = start;
+  realstart = start;
+  botleft = topright = ICOORD (edgept->pos.x (), edgept->pos.y ());
+  leftmost = edgept->pos.x ();
+  length = 0;                    //coutn length
+  do {
+    edgept = edgept->next;
+    if (edgept->pos.x () < botleft.x ())
+                                 //get bounding box
+      botleft.set_x (edgept->pos.x ());
+    else if (edgept->pos.x () > topright.x ())
+      topright.set_x (edgept->pos.x ());
+    if (edgept->pos.y () < botleft.y ())
+                                 //get bounding box
+      botleft.set_y (edgept->pos.y ());
+    else if (edgept->pos.y () > topright.y ()) {
+      realstart = edgept;
+      leftmost = edgept->pos.x ();
+      topright.set_y (edgept->pos.y ());
+    }
+    else if (edgept->pos.y () == topright.y ()
+    && edgept->pos.x () < leftmost) {
+                                 //leftmost on line
+      leftmost = edgept->pos.x ();
+      realstart = edgept;
+    }
+    length++;                    //count elements
+  }
+  while (edgept != start);
+  start = realstart;             //shift it to topleft
+  return length;
+}
+
+    
+    #include          'params.h'
+#include          'scrollview.h'
+#include          'pdblock.h'
+#include          'crakedge.h'
+    
+      /// Search the list for the (index)'th item (0-based) in (list:key)
+  /// A negative index indicates: 'from end-of-list'
+  /// If index is within range: return true, and return the value in *result.
+  /// If (index < -length OR index>=length), then index is out of range:
+  ///   return false (and *result is left unchanged)
+  /// May throw RedisListException
+  bool Index(const std::string& key, int32_t index,
+             std::string* result);
+    
+    // @lint-ignore TXT4 T25377293 Grandfathered in
+#endif  // JAVA_ROCKSJNI_STATISTICSJNI_H_
+    
+    
+    { private:
+  MergingIterator* merge_iter;
+  InternalIterator* first_iter;
+  bool use_merging_iter;
+  Arena* arena;
+};
+    
+    namespace port {
+    }
