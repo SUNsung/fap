7ef331e79c8e4daa525fb822ba68d03ef671aff0
SUNsung <weatherius@gmail.com>
2018-07-22 23:49:56 +0300

    Rewrite [56B65N8.lxiY.56KNeM6b.0lw56a85tOinFHUs56o6y.py] File in: 2018-07-22 23:49:56

--- pyton/56B65N8.lxiY.56KNeM6b.0lw56a85tOinFHUs56o6y.py
+++ pyton/56B65N8.lxiY.56KNeM6b.0lw56a85tOinFHUs56o6y.py
@@ -1,39 +1,62 @@
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write('Hello, world')
+        
+            implements_to_string = _identity
     
-        @skipBefore35
-    @gen_test
-    def test_native_coroutine(self):
-        ws = yield self.ws_connect('/native')
-        # Send both messages immediately, coroutine must process one at a time.
-        yield ws.write_message('hello1')
-        yield ws.write_message('hello2')
-        res = yield ws.read_message()
-        self.assertEqual(res, 'hello1')
-        res = yield ws.read_message()
-        self.assertEqual(res, 'hello2')
+        When :attr:`~flask.Flask.debug` is enabled, set the logger level to
+    :data:`logging.DEBUG` if it is not set.
     
-        @gen_test
-    def test_pipe_iostream_big_write(self):
-        rs, ws = yield self.make_iostream_pair()
+        if version in tags:
+        fail('Version '%s' is already tagged', version)
     
-        def test_undecorated_generator_with_skip(self):
-        class Test(AsyncTestCase):
-            @unittest.skip('don't run this')
-            def test_gen(self):
-                yield
-        test = Test('test_gen')
-        result = unittest.TestResult()
-        test.run(result)
-        self.assertEqual(len(result.errors), 0)
-        self.assertEqual(len(result.skipped), 1)
     
-    `tornado.options.options` is a singleton instance of `OptionParser`, and
-the top-level functions in this module (`define`, `parse_command_line`, etc)
-simply call methods on it.  You may create additional `OptionParser`
-instances to define isolated sets of options, such as for subcommands.
+def test_config_from_envvar_missing():
+    env = os.environ
+    try:
+        os.environ = {'FOO_SETTINGS': 'missing.cfg'}
+        with pytest.raises(IOError) as e:
+            app = flask.Flask(__name__)
+            app.config.from_envvar('FOO_SETTINGS')
+        msg = str(e.value)
+        assert msg.startswith('[Errno 2] Unable to load configuration '
+                              'file (No such file or directory):')
+        assert msg.endswith('missing.cfg'')
+        assert not app.config.from_envvar('FOO_SETTINGS', silent=True)
+    finally:
+        os.environ = env
+    
+            # Force Python to track this dictionary at all times.
+        # This is necessary since Python only starts tracking
+        # dicts if they contain mutable objects.  It's a horrible,
+        # horrible hack but makes this kinda testable.
+        loc.__storage__['FOOO'] = [1, 2, 3]
+    
+        def test_greenlet_context_copying_api(self, app, client):
+        greenlets = []
+    
+        def steps(self):
+        '''Run the map and reduce steps.'''
+        return [
+            self.mr(mapper=self.mapper,
+                    reducer=self.reducer)
+        ]
+    
+        def _hash_function(self, key):
+        return key % self.size
+    
+        def setUp(self):
+        from acme.errors import BadNonce
+        self.error = BadNonce(nonce='xxx', error='error')
+    
+        @mock.patch('certbot.display.enhancements.util')
+    def test_secure(self, mock_util):
+        mock_util().menu.return_value = (display_util.OK, 1)
+        self.assertTrue(self._call())
+    
+    
+@zope.interface.implementer(interfaces.IInstaller)
+@zope.interface.provider(interfaces.IPluginFactory)
+class Installer(common.Plugin):
+    '''Example Installer.'''
     
         def test_sequential_undo(self):
         self.command_stack = list(reversed(self.command_stack))
@@ -44,9 +67,53 @@ instances to define isolated sets of options, such as for subcommands.
         output_after_second_undo = os.listdir(self.test_dir)
         self.assertEqual(output_after_second_undo[0], 'foo.txt')
     
-    '''
-http://web.archive.org/web/20120309135549/http://dpip.testingperspective.com/?p=28
+        def test_pool_behavior_with_single_object_inside(self):
+        sample_queue = queue.Queue()
+        sample_queue.put('yam')
+        with ObjectPool(sample_queue) as obj:
+            # print('Inside with: {}'.format(obj))
+            self.assertEqual(obj, 'yam')
+        self.assertFalse(sample_queue.empty())
+        self.assertTrue(sample_queue.get() == 'yam')
+        self.assertTrue(sample_queue.empty())
+        
     
-        def toggle_amfm(self):
-        print(u'Switching to AM')
-        self.radio.state = self.radio.amstate
+        def __new__(cls, name, bases, attrs):
+        new_cls = type.__new__(cls, name, bases, attrs)
+        '''
+            Here the name of the class is used as key but it could be any class
+            parameter.
+        '''
+        cls.REGISTRY[new_cls.__name__] = new_cls
+        return new_cls
+    
+        def test_shall_toggle_from_am_to_fm(self):
+        self.radio.toggle_amfm()
+        state = self.radio.state.name
+        expected_state_name = 'FM'
+        self.assertEqual(state, expected_state_name)
+    
+    class MidnightTimeProvider(object):
+    '''
+    Class implemented as hard-coded stub (in contrast to configurable stub).
+    '''
+    
+        def test_display_current_time_at_midnight(self):
+        class_under_test = TimeDisplay()
+        expected_time = '24:01'
+        result = class_under_test.get_current_time_as_as_html_fragment()
+        self.assertEqual(result, expected_time)
+'''
+    
+        a_transaction = Transaction(True, num_obj)
+    try:
+        for i in range(3):
+            num_obj.increment()
+            print(num_obj)
+        a_transaction.commit()
+        print('-- committed')
+    
+    *TL;DR80
+Implements state as a derived class of the state pattern interface.
+Implements state transitions by invoking methods from the pattern's superclass.
+'''
