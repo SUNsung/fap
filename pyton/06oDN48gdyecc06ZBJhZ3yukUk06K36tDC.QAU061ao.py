29d46914b4a5a2c2d89a99dc1077620cc7bd800e
SUNsung <weatherius@gmail.com>
2018-07-23 04:48:23 +0300

    Rewrite [06oDN48gdyecc06ZBJhZ3yukUk06K36tDC.QAU061ao.py] File in: 2018-07-23 04:48:22

--- pyton/06oDN48gdyecc06ZBJhZ3yukUk06K36tDC.QAU061ao.py
+++ pyton/06oDN48gdyecc06ZBJhZ3yukUk06K36tDC.QAU061ao.py
@@ -1,60 +1,151 @@
-        # put all lines in the file into a Python list
-        strings = f.readlines()
+
         
-        # above line leaves trailing newline characters; strip them out
-        strings = [x.strip(u'\n') for x in strings]
         
-        # remove empty-lines and comments
-        strings = [x for x in strings if x and not x.startswith(u'#')]
-        
-        # insert empty string since all are being removed
-        strings.insert(0, u'')
+def assertGreaterEqual(self, got, expected, msg=None):
+    if not (got >= expected):
+        if msg is None:
+            msg = '%r not greater than or equal to %r' % (got, expected)
+        self.assertTrue(got >= expected, msg)
     
     
-    def endResync(self):
+def _download_restricted(url, filename, age):
+    ''' Returns true if the file has been downloaded '''
+    
+    
+if __name__ == '__main__':
+    unittest.main()
+
+    
+        _TEST = {
+        'url': 'http://www.anitube.se/video/36621',
+        'md5': '59d0eeae28ea0bc8c05e7af429998d43',
+        'info_dict': {
+            'id': '36621',
+            'ext': 'mp4',
+            'title': 'Recorder to Randoseru 01',
+            'duration': 180.19,
+        },
+        'skip': 'Blocked in the US',
+    }
+    
+        def _real_extract(self, url):
+        webpage = self._download_webpage(url, url_basename(url))
+        cnn_url = self._html_search_regex(r'data-url='(.+?)'', webpage, 'cnn url')
+        return {
+            '_type': 'url',
+            'url': cnn_url,
+            'ie_key': CNNIE.ie_key(),
+        }
+    
+    from mrjob.job import MRJob
+    
+        def set(self, key, value):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                item.value = value
+                return
+        self.table[hash_index].append(Item(key, value))
+    
+    
+class PagesDataStore(object):
+    
+        class Plugin(AuthPlugin):
+        auth_type = 'test-require-false'
+        auth_require = False
+    
+    
+with codecs.open(FILE_PATH, encoding='utf8') as f:
+    # Strip because we don't want new lines in the data so that we can
+    # easily count occurrences also when embedded in JSON (where the new
+    # line would be escaped).
+    FILE_CONTENT = f.read().strip()
+    
         '''
-        A hook to listen in on the token consumption during error recovery.
-        The DebugParser subclasses this to fire events to the listenter.
-        '''
+)
+
     
-        In general, the recognition exceptions can track where in a grammar a
-    problem occurred and/or what was the expected input.  While the parser
-    knows its state (such as current input symbol and line info) that
-    state can change before the exception is reported so current token index
-    is computed and stored at exception time.  From this info, you can
-    perhaps print an entire line of input not just a single token, for example.
-    Better to just say the recognizer had a problem and then let the parser
-    figure out a fancy report.
+        config['implicit_content_type'] = 'json'
+    config.save()
+    config.load()
+    assert 'implicit_content_type' not in config
+    assert not config['default_options']
     
-    def shorten_title(title):
-    m1 = re.search('[[0-9]*]', title)
-    m2 = re.search(''.*'', title)
-    if m1:
-        title = m1.group(0)
-    if m2:
-        title = ' '.join((title, m2.group(0)))   
-    return title[:50] + ' [...]'    
+        def test_verbose_implies_all(self, httpbin):
+        r = http('--verbose', '--follow', httpbin + '/redirect/1')
+        assert 'GET /redirect/1 HTTP/1.1' in r
+        assert 'HTTP/1.1 302 FOUND' in r
+        assert 'GET /get HTTP/1.1' in r
+        assert HTTP_OK in r
     
-        A configurable interface is an (abstract) class whose constructor
-    acts as a factory function for one of its implementation subclasses.
-    The implementation subclass as well as optional keyword arguments to
-    its initializer can be set globally at runtime with `configure`.
+        The host identifier may be a hostname (qualified or not), an IPv4 address,
+    or an IPv6 address. If allow_ranges is True, then any of those may contain
+    [x:y] range specifications, e.g. foo[1:3] or foo[0:5]-bar[x-z].
     
-            async def worker(worker_id):
-            await sem.acquire()
-            try:
-                print('Worker %d is working' % worker_id)
-                await use_some_resource()
-            finally:
-                print('Worker %d is done' % worker_id)
-                sem.release()
+        def on_open_shell(self):
+        try:
+            self._exec_cli_command('screen-length 0 temporary')
+        except AnsibleConnectionFailure:
+            raise AnsibleConnectionFailure('unable to set terminal parameters')
+
     
-        def test_subsequent_wait_calls(self):
-        '''
-        This test makes sure that a second call to wait()
-        clears the first timeout.
-        '''
-        self.io_loop.add_timeout(self.io_loop.time() + 0.00, self.stop)
-        self.wait(timeout=0.02)
-        self.io_loop.add_timeout(self.io_loop.time() + 0.03, self.stop)
-        self.wait(timeout=0.15)
+        text = html.remove_tags_with_content(text, ('script', 'noscript'))
+    text = html.replace_entities(text)
+    text = html.remove_comments(text)
+    return _ajax_crawlable_re.search(text) is not None
+
+    
+    
+class CookiesMiddleware(object):
+    '''This middleware enables working with sites that need cookies'''
+    
+        def process_request(self, request, spider):
+        for k, v in self._headers:
+            request.headers.setdefault(k, v)
+
+    
+        def __init__(self, path=None, debug=False):
+        self.file = None
+        self.fingerprints = set()
+        self.logdupes = True
+        self.debug = debug
+        self.logger = logging.getLogger(__name__)
+        if path:
+            self.file = open(os.path.join(path, 'requests.seen'), 'a+')
+            self.file.seek(0)
+            self.fingerprints.update(x.rstrip() for x in self.file)
+    
+        if results.overwrite and os.path.exists(output_directory):
+        shutil.rmtree(output_directory)
+    
+    
+  def IsAllowed( self, diagnostic ):
+    # NOTE: a diagnostic IsAllowed() ONLY if NO filters match it
+    for filterMatches in self._filters:
+      if filterMatches( diagnostic ):
+        return False
+    
+    
+def RunFlake8():
+  print( 'Running flake8' )
+  subprocess.check_call( [
+    sys.executable, '-m', 'flake8', p.join( DIR_OF_THIS_SCRIPT, 'python' )
+  ] )
+    
+        with patch.object( ycm._message_poll_request,
+                       '_response_future',
+                       new = MockAsyncServerResponseInProgress() ):
+      ycm.OnPeriodicTick() # Uses ycm._message_poll_request ...
+  '''
+  return mock.MagicMock( wraps = FakeFuture( False ) )
+    
+    
+class CommandRequest( BaseRequest ):
+  def __init__( self, arguments, completer_target = None, extra_data = None ):
+    super( CommandRequest, self ).__init__()
+    self._arguments = _EnsureBackwardsCompatibility( arguments )
+    self._command = arguments and arguments[ 0 ]
+    self._completer_target = ( completer_target if completer_target
+                               else 'filetype_default' )
+    self._extra_data = extra_data
+    self._response = None
