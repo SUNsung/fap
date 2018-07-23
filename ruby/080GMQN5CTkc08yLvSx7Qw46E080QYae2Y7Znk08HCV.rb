5b69cebd7408d150b8821f6c39dc62ebc1ba1c90
SUNsung <weatherius@gmail.com>
2018-07-23 12:35:18 +0300

    Rewrite [080GMQN5CTkc08yLvSx7Qw46E080QYae2Y7Znk08HCV.rb] File in: 2018-07-23 12:35:18

--- ruby/080GMQN5CTkc08yLvSx7Qw46E080QYae2Y7Znk08HCV.rb
+++ ruby/080GMQN5CTkc08yLvSx7Qw46E080QYae2Y7Znk08HCV.rb
@@ -1,77 +1,102 @@
 
         
-          def short_type
-    @short_type ||= type.split('::').pop
+          def test_should_merge_errors_on_the_associated_models_onto_the_parent_even_if_it_is_not_valid
+    @pirate.ship.name   = nil
+    @pirate.catchphrase = nil
+    assert_predicate @pirate, :invalid?
+    assert_predicate @pirate.errors[:'ship.name'], :any?
+    assert_predicate @pirate.errors[:catchphrase], :any?
   end
     
-      def recipients(payload = {})
-    emails = interpolated(payload)['recipients']
-    if emails.present?
-      if emails.is_a?(String)
-        [emails]
-      else
-        emails
+      before_destroy :overwrite_to_raise
+    
+      validate :check_empty_title
+  validate :check_content_mismatch, on: :create
+  validate :check_wrong_update, on: :update
+  validate :check_author_name_is_secret, on: :special_case
+    
+        private
+      def ping
       end
-    else
-      [user.email]
+  end
+    
+        def connect
+      @connected = true
+    end
+    
+      def save_and_refresh_staff_groups!
+    transaction do
+      self.save!
+      Group.refresh_automatic_groups!(:admins, :moderators, :staff)
     end
   end
     
-          _form_configurable_fields[name] = options
-    end
-    
-        # Optional
-    #   Use this method the gracefully stop your agent but make sure the run method return, or
-    #   terminate the thread.
-    def stop; end
+      def self.fragment_cache
+    @cache ||= DistributedCache.new('am_serializer_fragment_cache')
   end
-end
-=end
-module LongRunnable
-  extend ActiveSupport::Concern
     
-          html_filters.push 'bootstrap/entries_v4', 'bootstrap/clean_html_v4'
-    
-        version '1.5' do
-      self.release = '1.5.3'
-      self.base_urls = [
-        'https://hexdocs.pm/elixir/#{release}/',
-        'https://hexdocs.pm/eex/#{release}/',
-        'https://hexdocs.pm/ex_unit/#{release}/',
-        'https://hexdocs.pm/iex/#{release}/',
-        'https://hexdocs.pm/logger/#{release}/',
-        'https://hexdocs.pm/mix/#{release}/',
-        'https://elixir-lang.org/getting-started/'
-      ]
+        def entries_as_json
+      @entries.sort! { |a, b| sort_fn(a.name, b.name) }.map(&:as_json)
     end
     
-        def parse(response)
-      unless response.url == root_url || self.class.version == 'Guide'
-        response.body.sub!(/<nav class='devsite-nav-responsive-sidebar.+?<\/nav>/m, '')
-        response.body.gsub!(/<li class='devsite-nav-item'>.+?<\/li>/m, '')
-      end
-    
-        def to_a
-      @filters.dup
-    end
-    
-    module Docs
-  class PageDb
-    attr_reader :pages
-    
-                # Register the handler if this is our first callback.
-            Signal.trap('INT') { fire_callbacks } if registered.length == 1
-          end
+            hash.each do |key, value|
+          self[convert_key(key)] = value
         end
+      end
     
-            # Success, exit status 0
-        0
+            def each_unnecessary_space_match(node, &blk)
+          each_match_range(
+            contents_range(node),
+            MULTIPLE_SPACES_BETWEEN_ITEMS_REGEX,
+            &blk
+          )
+        end
       end
     end
   end
 end
 
     
-      def start_url
-    '/web/timelines/home'
-  end
+    module RuboCop
+  module Cop
+    module Lint
+      # Don't omit the accumulator when calling `next` in a `reduce` block.
+      #
+      # @example
+      #
+      #   # bad
+      #
+      #   result = (1..4).reduce(0) do |acc, i|
+      #     next if i.odd?
+      #     acc + i
+      #   end
+      #
+      # @example
+      #
+      #   # good
+      #
+      #   result = (1..4).reduce(0) do |acc, i|
+      #     next acc if i.odd?
+      #     acc + i
+      #   end
+      class NextWithoutAccumulator < Cop
+        MSG = 'Use `next` with an accumulator argument in a `reduce`.'.freeze
+    
+            # @param annotated_source [String] string passed to the matchers
+        #
+        # Separates annotation lines from source lines. Tracks the real
+        # source line number that each annotation corresponds to.
+        #
+        # @return [AnnotatedSource]
+        def self.parse(annotated_source)
+          source      = []
+          annotations = []
+    
+          context 'source without blank lines' do
+        let(:source) do
+          <<-RUBY.strip_indent
+            #{type} SomeObject
+              def do_something; end
+            end
+          RUBY
+        end
