e109851e300412ebebbf12843fd7170f5e13e0f5
SUNsung <weatherius@gmail.com>
2018-07-24 02:47:49 +0300

    Rewrite [31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb] File in: 2018-07-24 02:47:48

--- ruby/31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb
+++ ruby/31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb
@@ -1,164 +1,124 @@
 
         
-          </body>
-</html>
-HTML
-CONTENT_NOT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+          attributes :id, :type, :url, :preview_url,
+             :remote_url, :text_url, :meta,
+             :description
     
-        # The entry filter for this collection.
-    # Creates an instance of Jekyll::EntryFilter.
-    #
-    # Returns the instance of Jekyll::EntryFilter for this collection.
-    def entry_filter
-      @entry_filter ||= Jekyll::EntryFilter.new(site, relative_directory)
+        it 'tracks when sign in is older than one day' do
+      user.update(current_sign_in_at: 2.days.ago)
+      sign_in user, scope: :user
+      get :show
+    
+      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/follow
+  def follow
+    f = Follow.last
+    NotificationMailer.follow(f.target_account, Notification.find_by(activity: f))
+  end
+    
+      tasks_dir = Pathname.new('lib/capistrano/tasks')
+  config_dir = Pathname.new('config')
+  deploy_dir = config_dir.join('deploy')
+    
+    def config_tag(config, key, tag=nil, classname=nil)
+  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
+  tag       ||= 'div'
+  classname ||= key.sub(/_/, '-').sub(/\./, '-')
+  output      = '<#{tag} class='#{classname}''
+    
+        def render(context)
+      if @img
+        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
+      else
+        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
+      end
+    end
+  end
+end
+    
+      class RenderPartialTag < Liquid::Tag
+    include OctopressFilters
+    def initialize(tag_name, markup, tokens)
+      @file = nil
+      @raw = false
+      if markup =~ /^(\S+)\s?(\w+)?/
+        @file = $1.strip
+        @raw = $2 == 'raw'
+      end
+      super
     end
     
-    module Jekyll
-  module Commands
-    class Build < Command
-      class << self
-        # Create the Mercenary command for the Jekyll CLI for this Command
-        def init_with_program(prog)
-          prog.command(:build) do |c|
-            c.syntax      'build [options]'
-            c.description 'Build your site'
-            c.alias :b
+        status data.respond_to?(:status_code) ? data.status_code : 200
     
-            private
-    
-          def valid_processors
-        %w(kramdown) + third_party_processors
+          # Skip this file if the output file is the same size
+      if entry.directory?
+        FileUtils.mkdir_p(path) unless File.directory?(path)
+      else
+        entry_mode = entry.instance_eval { @mode } & 0777
+        if File.exists?(path)
+          stat = File.stat(path)
+          # TODO(sissel): Submit a patch to archive-tar-minitar upstream to
+          # expose headers in the entry.
+          entry_size = entry.instance_eval { @size }
+          # If file sizes are same, skip writing.
+          next if stat.size == entry_size && (stat.mode & 0777) == entry_mode
+        end
+        puts 'Extracting #{entry.full_name} from #{tarball} #{entry_mode.to_s(8)}'
+        File.open(path, 'w') do |fd|
+          # eof? check lets us skip empty files. Necessary because the API provided by
+          # Archive::Tar::Minitar::Reader::EntryStream only mostly acts like an
+          # IO object. Something about empty files in this EntryStream causes
+          # IO.copy_stream to throw 'can't convert nil into String' on JRuby
+          # TODO(sissel): File a bug about this.
+          while !entry.eof?
+            chunk = entry.read(16384)
+            fd.write(chunk)
+          end
+            #IO.copy_stream(entry, fd)
+        end
+        File.chmod(entry_mode, path)
       end
+    end
+    tar.close
+    File.unlink(tarball) if File.file?(tarball)
+  end # def untar
     
-          def convert(content)
-        document = Kramdown::Document.new(content, @config)
-        html_output = document.to_html.chomp
-        if @config['show_warnings']
-          document.warnings.each do |warning|
-            Jekyll.logger.warn 'Kramdown warning:', warning.sub(%r!^Warning:\s+!, '')
+    # Use this to fill in an entire form with data from a table. Example:
+#
+#   When I fill in the following:
+#     | Account Number | 5002       |
+#     | Expiry date    | 2009-11-01 |
+#     | Note           | Nice guy   |
+#     | Wants Email?   |            |
+#
+# TODO: Add support for checkbox, select og option
+# based on naming conventions.
+#
+When /^(?:|I )fill in the following:$/ do |fields|
+  fields.rows_hash.each do |name, value|
+    When %{I fill in '#{name}' with '#{value}'}
+  end
+end
+    
+        def type_from_file_contents
+      type_from_mime_magic || type_from_file_command
+    rescue Errno::ENOENT => e
+      Paperclip.log('Error while determining content type: #{e}')
+      SENSIBLE_DEFAULT
+    end
+    
+            def failure_message
+          '#{expected_attachment}\n'.tap do |message|
+            message << accepted_types_and_failures.to_s
+            message << '\n\n' if @allowed_types.present? && @rejected_types.present?
+            message << rejected_types_and_failures.to_s
           end
         end
-        html_output
+    
+          def validate_before_processing(validator_class, options)
+        options = options.dup
+        attributes = options.delete(:attributes)
+        attributes.each do |attribute|
+          options[:attributes] = [attribute]
+          create_validating_before_filter(attribute, validator_class, options)
+        end
       end
-    end
-  end
-end
-
-    
-          def fallback_data
-        @fallback_data ||= {}
-      end
-    end
-  end
-end
-
-    
-          def short_month
-        @obj.date.strftime('%b')
-      end
-    
-              if entry.end_with?('/')
-            entry_path.in_path?(
-              item
-            )
-    
-      # SecureRandom.hex generates a random hexadecimal string.
-  #
-  # The argument _n_ specifies the length, in bytes, of the random number to be generated.
-  # The length of the resulting hexadecimal string is twice of _n_.
-  #
-  # If _n_ is not specified or is nil, 16 is assumed.
-  # It may be larger in the future.
-  #
-  # The result may contain 0-9 and a-f.
-  #
-  #   require 'securerandom'
-  #
-  #   SecureRandom.hex #=> 'eb693ec8252cd630102fd0d0fb7c3485'
-  #   SecureRandom.hex #=> '91dc3bfb4de5b11d029d376634589b61'
-  #
-  # If a secure random number generator is not available,
-  # +NotImplementedError+ is raised.
-  def hex(n=nil)
-    random_bytes(n).unpack('H*')[0]
-  end
-    
-      def test_pty_check_raise
-    bug2642 = '[ruby-dev:44600]'
-    st1 = st2 = pid = nil
-    PTY.spawn('cat') do |r,w,id|
-      pid = id
-      assert_nothing_raised(PTY::ChildExited, bug2642) {st1 = PTY.check(pid, true)}
-      w.close
-      r.close
-      sleep(0.1)
-      st2 = assert_raise(PTY::ChildExited, bug2642) {PTY.check(pid, true)}.status
-    end
-  rescue RuntimeError
-    skip $!
-  else
-    assert_nil(st1)
-    assert_equal(pid, st2.pid)
-  end
-    
-          it 'does not keep buffer content if it is longer than offset + result' do
-        n = [ 65, 66, 67 ]
-        buffer = '1234567890'
-        n.pack('@3ccc', buffer: buffer).should == '123ABC'
-      end
-    end
-  end
-end
-
-    
-      it 'does not modify a HOME string argument' do
-    str = '~/a'
-    File.expand_path(str).should == '#{Dir.home}/a'
-    str.should == '~/a'
-  end
-    
-      # For this test we delete the file first to reset the perms
-  it 'opens the file when passed mode, num, permissions and block' do
-    rm_r @file
-    File.umask(0022)
-    File.open(@file, 'w', 0755){ |fh| }
-    platform_is_not :windows do
-      File.stat(@file).mode.to_s(8).should == '100755'
-    end
-    File.exist?(@file).should == true
-  end
-    
-        File.pipe?(filename).should == false
-    
-    describe 'String#upcase' do
-  it 'returns a copy of self with all lowercase letters upcased' do
-    'Hello'.upcase.should == 'HELLO'
-    'hello'.upcase.should == 'HELLO'
-  end
-    
-        res = Net::HTTPServerError.new('1.0', '5xx', 'test response')
-    res.error_type.should == Net::HTTPFatalError
-  end
-end
-
-    
-      def icon
-    object.image
-  end
-    
-      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/favourite
-  def favourite
-    f = Favourite.last
-    NotificationMailer.favourite(f.status.account, Notification.find_by(activity: f))
-  end
