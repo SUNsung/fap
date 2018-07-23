9d02c4e351d7181726518bf68b60292a0b888844
SUNsung <weatherius@gmail.com>
2018-07-23 11:01:48 +0300

    Rewrite [10r6hJCpWiLM10qczV9HMozHg10lVKYy6oDri.10nki.rb] File in: 2018-07-23 11:01:47

--- ruby/10r6hJCpWiLM10qczV9HMozHg10lVKYy6oDri.10nki.rb
+++ ruby/10r6hJCpWiLM10qczV9HMozHg10lVKYy6oDri.10nki.rb
@@ -1,46 +1,54 @@
 
         
-                begin
-          f = Formulary.from_rack(HOMEBREW_CELLAR/name)
-        rescue FormulaUnavailableError, TapFormulaAmbiguityError, TapFormulaWithOldnameAmbiguityError
-          next
-        end
+            log = StringIO.new
+    @dry_run_started_at = Time.zone.now
+    @dry_run_logger = Logger.new(log).tap { |logger|
+      logger.formatter = proc { |severity, datetime, progname, message|
+        elapsed_time = '%02d:%02d:%02d' % 2.times.inject([datetime - @dry_run_started_at]) { |(x, *xs)|
+          [*x.divmod(60), *xs]
+        }
+    }
+    }
     
-      def external_commands
-    paths.reduce([]) do |cmds, path|
-      Dir['#{path}/brew-*'].each do |file|
-        next unless File.executable?(file)
-        cmd = File.basename(file, '.rb')[5..-1]
-        cmds << cmd unless cmd.include?('.')
+          if status.is_a? Symbol
+        pod.status = Pod::CURL_ERROR_MAP.fetch(status, :unknown_error)
+        pod.error = 'FederationError: #{status}'
+      elsif status >= 200 && status < 300
+        pod.status = :no_errors unless Pod.statuses[pod.status] == Pod.statuses[:version_failed]
+      else
+        pod.status = :http_failed
+        pod.error = 'FederationError: HTTP status code was: #{status}'
       end
-      cmds
-    end.sort
-  end
+      pod.update_offline_since
     
-    class Reporter
-  class ReporterRevisionUnsetError < RuntimeError
-    def initialize(var_name)
-      super '#{var_name} is unset!'
+    describe Capistrano::Application do
+  it 'provides a --trace option which enables SSHKit/NetSSH trace output'
+    
+        i0 = index
+    r1 = _nt_double_quoted_string
+    if r1
+      r0 = r1
+    else
+      r2 = _nt_single_quoted_string
+      if r2
+        r0 = r2
+      else
+        @index = i0
+        r0 = nil
+      end
     end
-  end
     
-    # This formula serves as the base class for several very similar
-# formulae for Amazon Web Services related tools.
-class AmazonWebServicesFormula < Formula
-  # Use this method to peform a standard install for Java-based tools,
-  # keeping the .jars out of HOMEBREW_PREFIX/lib
-  def install
-    rm Dir['bin/*.cmd'] # Remove Windows versions
-    libexec.install Dir['*']
-    bin.install_symlink Dir['#{libexec}/bin/*'] - ['#{libexec}/bin/service']
+      def file_sha1(path)
+    digest = Digest::SHA1.new
+    fd = File.new(path, 'r')
+    while true
+      begin
+        digest << fd.sysread(16384)
+      rescue EOFError
+        break
+      end
+    end
+    return digest.hexdigest
+  ensure
+    fd.close if fd
   end
-  alias_method :standard_install, :install
-    
-      def type
-    'Emoji'
-  end
-    
-      attributes :name, :short_name, :description,
-             :icons, :theme_color, :background_color,
-             :display, :start_url, :scope,
-             :share_target
