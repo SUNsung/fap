896cee51b8a98a2c28a244b8062be6a93a7e4a52
SUNsung <weatherius@gmail.com>
2018-07-24 13:43:24 +0300

    Rewrite [51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb] File in: 2018-07-24 13:43:23

--- ruby/51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb
+++ ruby/51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb
@@ -1,20 +1,65 @@
 
         
-        
-    
-      describe '#referrer' do
-    it 'Reads referrer from Referer header' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
-      expect(subject.referrer(env)).to eq('bar.com')
-    end
-    
-        it 'denies requests with sneaky encoded session cookies' do
-      get '/', {}, 'HTTP_COOKIE' => 'rack.session=EVIL_SESSION_TOKEN; rack.%73ession=SESSION_TOKEN'
-      expect(last_response).not_to be_ok
-    end
-    
-      if ''.respond_to?(:encoding)  # Ruby 1.9+ M17N
-    context 'PATH_INFO's encoding' do
-      before do
-        @app = Rack::Protection::PathTraversal.new(proc { |e| [200, {'Content-Type' => 'text/plain'}, [e['PATH_INFO'].encoding.to_s]] })
+              def force_equality?(value)
+        coder.respond_to?(:object_class) && value.is_a?(coder.object_class)
       end
+    
+        firm.reload
+    assert_not_predicate firm.clients, :loaded?
+    assert_queries(1) do
+      assert_equal true, firm.clients.include?(client)
+    end
+    assert_not_predicate firm.clients, :loaded?
+  end
+    
+        def private_method
+      'I am Jack's innermost fears and aspirations'
+    end
+    
+    require 'test_helper'
+require 'stubs/test_connection'
+require 'stubs/room'
+    
+          assert_equal [ -1, {}, [] ], response
+    end
+  end
+    
+          assert_kind_of ChatChannel, channel
+      assert_equal 1, channel.room.id
+    end
+  end
+    
+          events = []
+      ActiveSupport::Notifications.subscribe 'broadcast.action_cable' do |*args|
+        events << ActiveSupport::Notifications::Event.new(*args)
+      end
+    
+    # No trailing slash
+Benchmark.ips do |x|
+  x.report('with body include?') { CONTENT_CONTAINING.include?('<body') }
+  x.report('with body regexp')   { CONTENT_CONTAINING =~ /<\s*body/ }
+  x.compare!
+end
+
+    
+          # `{{ site.related_posts }}` is how posts can get posts related to
+      # them, either through LSI if it's enabled, or through the most
+      # recent posts.
+      # We should remove this in 4.0 and switch to `{{ post.related_posts }}`.
+      def related_posts
+        return nil unless @current_document.is_a?(Jekyll::Document)
+        @current_document.related_posts
+      end
+      attr_writer :current_document
+    
+            if exp.is_a?(Regexp)
+          entry_path =~ exp
+    
+      def delete_from_facebook(url, body)
+    Faraday.delete(url, body)
+  end
+end
+
+    
+        def index
+      pods_json = PodPresenter.as_collection(Pod.all)
