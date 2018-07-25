11cc4baed920e586b8fa87cef00831b6b0c4a72c
SUNsung <weatherius@gmail.com>
2018-07-25 04:40:31 +0300

    Create [30Ll.RqfXta8E30YS1oMHia6yE30DcG6KlFWCDQ30HyN.rb] File in: 2018-07-25 04:40:30

---
+++ ruby/30Ll.RqfXta8E30YS1oMHia6yE30DcG6KlFWCDQ30HyN.rb
@@ -0,0 +1,49 @@
+  # GET /resource/password/edit?reset_password_token=abcdef
+  def edit
+    self.resource = resource_class.new
+    set_minimum_password_length
+    resource.reset_password_token = params[:reset_password_token]
+  end
+    
+    module Devise
+  module Controllers
+    # A module that may be optionally included in a controller in order
+    # to provide remember me behavior. Useful when signing in is done
+    # through a callback, like in OmniAuth.
+    module Rememberable
+      # Return default cookie values retrieved from session options.
+      def self.cookie_values
+        Rails.configuration.session_options.slice(:path, :domain, :secure)
+      end
+    
+    module Devise
+  module Mailers
+    module Helpers
+      extend ActiveSupport::Concern
+    
+    GEMFILE_EXTENSIONS = [
+    '.local',
+    ''
+]
+    
+    res = ''
+doc = Hpricot(File.open(input))
+doc.search('//form').each do |form|
+    
+          when :login_fail
+        if(s[:user] and s[:pass])
+          report_auth_info(s.merge({:active => false}))
+          print_status('Failed FTP Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]}')
+    
+    meterp = Rex::Post::Meterpreter::Client.new(sock)
+    
+    # This is a completely hackish way to do this, and could break with future
+# versions of the JDK.  Need to find a better way to use sun.security.tools.KeyTool
+# and .JarSigner than modifying the source.  These rely on internal APIs that may
+# change.
+signer = Rjb::import('javaCompile.SignJar')
+#clsKeyTool = Rjb::import('sun.security.tools.KeyTool')
+#clsKeyTool = Rjb::import('sun.security.tools.KeyToolMSF')
+#clsJarSigner = Rjb::import('javaCompile.SignJar.JarSignerMSF')
+#clsJarSigner = Rjb::import('sun.security.tools.JarSigner')
+#clsJarSigner = Rjb::import('sun.security.tools.JarSignerMSF')
