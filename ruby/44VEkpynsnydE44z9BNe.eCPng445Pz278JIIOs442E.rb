434f67a97f65482f5c4ee5248fe316d023badc7d
SUNsung <weatherius@gmail.com>
2018-07-25 16:50:20 +0300

    Rewrite [44VEkpynsnydE44z9BNe.eCPng445Pz278JIIOs442E.rb] File in: 2018-07-25 16:50:19

--- ruby/44VEkpynsnydE44z9BNe.eCPng445Pz278JIIOs442E.rb
+++ ruby/44VEkpynsnydE44z9BNe.eCPng445Pz278JIIOs442E.rb
@@ -1,80 +1,148 @@
 
         
-          protected
-    
-        def unlock_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :unlock_instructions, opts)
-    end
-    
-          def remember_cookie_values(resource)
-        options = { httponly: true }
-        options.merge!(forget_cookie_values(resource))
-        options.merge!(
-          value: resource.class.serialize_into_cookie(resource),
-          expires: resource.remember_expires_at
-        )
-      end
-    
-          @router_name = options[:router_name]
-    
-      it 'uses '-e' as file' do
-    ruby_exe('puts __FILE__', escape: false).chomp.should == '-e'
-  end
-    
-      describe 'RTYPEDATA' do
-    it 'returns the struct data' do
-      a = @s.typed_wrap_struct(1024)
-      @s.typed_get_struct_rdata(a).should == 1024
-    end
-    
-        add_file name, mode do |io|
-      Gem::Package::DigestIO.wrap io, digests do |digest_io|
-        yield digest_io
+            # Extract options for this collection from the site configuration.
+    #
+    # Returns the metadata for this collection
+    def extract_metadata
+      if site.config['collections'].is_a?(Hash)
+        site.config['collections'][label] || {}
+      else
+        {}
       end
     end
     
-        gemspec_file = File.join(@tempdir, @gem.spec_name)
+          # Add common options to a command for building configuration
+      #
+      # cmd - the Jekyll::Command to add these options to
+      #
+      # Returns nothing
+      # rubocop:disable Metrics/MethodLength
+      def add_build_options(cmd)
+        cmd.option 'config', '--config CONFIG_FILE[,CONFIG_FILE2,...]',
+                   Array, 'Custom configuration file'
+        cmd.option 'destination', '-d', '--destination DESTINATION',
+                   'The current folder will be generated into DESTINATION'
+        cmd.option 'source', '-s', '--source SOURCE', 'Custom source directory'
+        cmd.option 'future', '--future', 'Publishes posts with a future date'
+        cmd.option 'limit_posts', '--limit_posts MAX_POSTS', Integer,
+                   'Limits the number of posts to parse and publish'
+        cmd.option 'watch', '-w', '--[no-]watch', 'Watch for changes and rebuild'
+        cmd.option 'baseurl', '-b', '--baseurl URL',
+                   'Serve the website from the given base URL'
+        cmd.option 'force_polling', '--force_polling', 'Force watch to use polling'
+        cmd.option 'lsi', '--lsi', 'Use LSI for improved related posts'
+        cmd.option 'show_drafts', '-D', '--drafts', 'Render posts in the _drafts folder'
+        cmd.option 'unpublished', '--unpublished',
+                   'Render posts that were marked as unpublished'
+        cmd.option 'quiet', '-q', '--quiet', 'Silence output.'
+        cmd.option 'verbose', '-V', '--verbose', 'Print verbose output.'
+        cmd.option 'incremental', '-I', '--incremental', 'Enable incremental rebuild.'
+        cmd.option 'strict_front_matter', '--strict_front_matter',
+                   'Fail if errors are present in front matter'
+      end
+      # rubocop:enable Metrics/MethodLength
     
-      def test_ssl_connection
-    ssl_server = self.class.start_ssl_server
-    temp_ca_cert = File.join(DIR, 'ca_cert.pem')
-    with_configured_fetcher(':ssl_ca_cert: #{temp_ca_cert}') do |fetcher|
-      fetcher.fetch_path('https://localhost:#{ssl_server.config[:Port]}/yaml')
+    module Jekyll
+  class Configuration < Hash
+    # Default options. Overridden by values in _config.yml.
+    # Strings rather than symbols are used for compatibility with YAML.
+    DEFAULTS = Configuration[{
+      # Where things are
+      'source'              => Dir.pwd,
+      'destination'         => File.join(Dir.pwd, '_site'),
+      'collections_dir'     => '',
+      'plugins_dir'         => '_plugins',
+      'layouts_dir'         => '_layouts',
+      'data_dir'            => '_data',
+      'includes_dir'        => '_includes',
+      'collections'         => {},
+    }
+    
+    module Jekyll
+  module Deprecator
+    extend self
+    
+          def i_day
+        @obj.date.strftime('%-d')
+      end
+    
+        def excluded?(entry)
+      glob_include?(site.exclude, relative_to_source(entry)).tap do |excluded|
+        if excluded
+          Jekyll.logger.debug(
+            'EntryFilter:',
+            'excluded #{relative_to_source(entry)}'
+          )
+        end
+      end
+    end
+    
+      it 'unwraps data for a parent type' do
+    a = @s.typed_wrap_struct(1024)
+    @s.typed_get_struct_parent(a).should == 1024
+  end
+    
+          with_timezone('Europe/Amsterdam') do
+        time.localtime
+      end
+    
+      it 'adds the directory at the front of $LOAD_PATH' do
+    dir = tmp('rubylib/incl_front')
+    ENV['RUBYLIB'] = @pre + dir
+    paths = ruby_exe('puts $LOAD_PATH').lines.map(&:chomp)
+    if PlatformGuard.implementation? :ruby
+      # In a MRI checkout, $PWD and some extra -I entries end up as
+      # the first entries in $LOAD_PATH. So just assert that it's not last.
+      idx = paths.index(dir)
+      idx.should < paths.size-1
+    else
+      paths[0].should == dir
     end
   end
     
-      def meta
-    object.file.meta
+        def stop_servers
+      if @normal_server
+        @normal_server.kill.join
+        @normal_server = nil
+      end
+      if @proxy_server
+        @proxy_server.kill.join
+        @proxy_server = nil
+      end
+      if @ssl_server
+        @ssl_server.stop
+        @ssl_server = nil
+      end
+      if @ssl_server_thread
+        @ssl_server_thread.kill.join
+        @ssl_server_thread = nil
+      end
+      utils = WEBrick::Utils    # TimeoutHandler is since 1.9
+      utils::TimeoutHandler.terminate if defined?(utils::TimeoutHandler.terminate)
+    end
+    
+      it 'does not decode any items for directives exceeding the input string size' do
+    '\xc2\x80'.unpack('UUUU').should == [0x80]
   end
-end
-
     
-      def deliver_digest
-    NotificationMailer.digest(user.account).deliver_now!
-    user.touch(:last_emailed_at)
+        alias log puts
+    
+      # The test environment is used exclusively to run your application's
+  # test suite. You never need to work with it otherwise. Remember that
+  # your test database is 'scratch space' for the test suite and is wiped
+  # and recreated between test runs. Don't rely on the data there!
+  config.cache_classes = true
+    
+      def test_font_helper_with_suffix_question
+    assert_match %r(url\(['']?/assets/.*eot\?.*['']?\)), @css
   end
-end
-
     
-        it 'tracks when sign in is older than one day' do
-      user.update(current_sign_in_at: 2.days.ago)
-      sign_in user, scope: :user
-      get :show
+      find_files = ->(path) {
+    Find.find(Pathname.new(path).relative_path_from(Pathname.new Dir.pwd).to_s).map do |path|
+      path if File.file?(path)
+    end.compact
+  }
     
-              if @address.update_attributes(address_params)
-            respond_with(@address, default_template: :show)
-          else
-            invalid_resource!(@address)
-          end
-        end
-    
-            def stock_location
-          @stock_location ||= StockLocation.accessible_by(current_ability, :read).find(params[:id])
-        end
-    
-            def index
-          authorize! :read, StockMovement
-          @stock_movements = scope.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
-          respond_with(@stock_movements)
+            def log_level?(level, min_level)
+          log_levels[level] >= log_levels[min_level]
         end
