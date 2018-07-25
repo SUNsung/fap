9418328170960aedfd7783e4fb3d5bcde8ce3014
SUNsung <weatherius@gmail.com>
2018-07-25 19:07:30 +0300

    Rewrite [16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb] File in: 2018-07-25 19:07:29

--- ruby/16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb
+++ ruby/16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb
@@ -1,49 +1,92 @@
 
         
-              # Remembers the given resource by setting up a cookie
-      def remember_me(resource)
-        return if request.env['devise.skip_storage']
-        scope = Devise::Mapping.find_scope!(resource)
-        resource.remember_me!
-        cookies.signed[remember_key(resource, scope)] = remember_cookie_values(resource)
-      end
+        module Homebrew
+  def build_env_keys(env)
+    %w[
+      CC CXX LD OBJC OBJCXX
+      HOMEBREW_CC HOMEBREW_CXX
+      CFLAGS CXXFLAGS CPPFLAGS LDFLAGS SDKROOT MAKEFLAGS
+      CMAKE_PREFIX_PATH CMAKE_INCLUDE_PATH CMAKE_LIBRARY_PATH CMAKE_FRAMEWORK_PATH
+      MACOSX_DEPLOYMENT_TARGET PKG_CONFIG_PATH PKG_CONFIG_LIBDIR
+      HOMEBREW_DEBUG HOMEBREW_MAKE_JOBS HOMEBREW_VERBOSE
+      HOMEBREW_SVN HOMEBREW_GIT
+      HOMEBREW_SDKROOT HOMEBREW_BUILD_FROM_SOURCE
+      MAKE GIT CPP
+      ACLOCAL_PATH PATH CPATH].select { |key| env.key?(key) }
+  end
     
-          @sign_out_via = options[:sign_out_via] || Devise.sign_out_via
-      @format = options[:format]
+      private
     
-          def key?(key)
-        super(convert_key(key))
-      end
+      def list_unbrewed
+    dirs  = HOMEBREW_PREFIX.subdirs.map { |dir| dir.basename.to_s }
+    dirs -= %w[Library Cellar .git]
     
-            opts = OptionParser.new do |o|
-          o.banner = 'Usage: vagrant cap [options] TYPE NAME [args]'
-          o.separator ''
-          o.separator 'This is an advanced command. If you don't know what this'
-          o.separator 'does and you aren't explicitly trying to use it, you probably'
-          o.separator 'don't want to use this.'
-          o.separator ''
-          o.separator 'This command checks or executes arbitrary capabilities that'
-          o.separator 'Vagrant has for hosts, guests, and providers.'
-          o.separator ''
-          o.separator 'Options:'
-          o.separator ''
-    
-        def type_from_mime_magic
-      @type_from_mime_magic ||= File.open(@filepath) do |file|
-        MimeMagic.by_magic(file).try(:type)
+            if msg = blacklisted?(query)
+          if count > 0
+            puts
+            puts 'If you meant #{query.inspect} precisely:'
+            puts
+          end
+          puts msg
+        elsif count == 0
+          puts 'No formula found for #{query.inspect}.'
+          begin
+            GitHub.print_pull_requests_matching(query)
+          rescue GitHub::Error => e
+            SEARCH_ERROR_QUEUE << e
+          end
+        end
       end
     end
     
-            def responds?
-          methods = @subject.instance_methods.map(&:to_s)
-          methods.include?('#{@attachment_name}') &&
-            methods.include?('#{@attachment_name}=') &&
-            methods.include?('#{@attachment_name}?')
+      # Prepend all log lines with the following tags.
+  # config.log_tags = [ :subdomain, :uuid ]
+    
+        # resize to a new geometry
+    # @param geometry [String] the Paperclip geometry definition to resize to
+    # @example
+    #   Paperclip::Geometry.new(150, 150).resize_to('50x50!')
+    #   #=> Paperclip::Geometry(50, 50)
+    def resize_to(geometry)
+      new_geometry = Paperclip::Geometry.parse geometry
+      case new_geometry.modifier
+      when '!', '#'
+        new_geometry
+      when '>'
+        if new_geometry.width >= self.width && new_geometry.height >= self.height
+          self
+        else
+          scale_to new_geometry
+        end
+      when '<'
+        if new_geometry.width <= self.width || new_geometry.height <= self.height
+          self
+        else
+          scale_to new_geometry
+        end
+      else
+        scale_to new_geometry
+      end
+    end
+    
+            def description
+          'have an attachment named #{@attachment_name}'
         end
     
-        def self.included(base)
-      ActiveRecord::ConnectionAdapters::Table.send :include, TableDefinition
-      ActiveRecord::ConnectionAdapters::TableDefinition.send :include, TableDefinition
-      ActiveRecord::ConnectionAdapters::AbstractAdapter.send :include, Statements
-      ActiveRecord::Migration::CommandRecorder.send :include, CommandRecorder
+        def processor(name) #:nodoc:
+      @known_processors ||= {}
+      if @known_processors[name.to_s]
+        @known_processors[name.to_s]
+      else
+        name = name.to_s.camelize
+        load_processor(name) unless Paperclip.const_defined?(name)
+        processor = Paperclip.const_get(name)
+        @known_processors[name.to_s] = processor
+      end
     end
+    
+      class Railtie < Rails::Railtie
+    initializer 'paperclip.insert_into_active_record' do |app|
+      ActiveSupport.on_load :active_record do
+        Paperclip::Railtie.insert
+      end
