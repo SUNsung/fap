b20b21bee7a3fd5dec2a0a4e63ced8551c44818c
SUNsung <weatherius@gmail.com>
2018-07-26 00:46:52 +0300

    Create [522sNkiPBCGcM52wYKIF0ahSo52fwtanFEN5kk52q7..cpp] File in: 2018-07-26 00:46:52

---
+++ cccccc/522sNkiPBCGcM52wYKIF0ahSo52fwtanFEN5kk52q7..cpp
@@ -0,0 +1,147 @@
+#endif  // GOOGLE_CUDA
+
+    
+    namespace tensorflow {
+    }
+    
+    class WritableFile;
+    
+    namespace internal {
+template <typename Scalar>
+struct functor_traits<scalar_clip_op<Scalar> > {
+  enum {
+    Cost = NumTraits<Scalar>::AddCost * 3,
+    PacketAccess = packet_traits<Scalar>::HasMax &&
+                   packet_traits<Scalar>::HasMin &&
+                   packet_traits<Scalar>::HasNegate
+  };
+};
+}  // namespace internal
+    
+    void SYCLDeviceContext::CopyCPUTensorToDevice(const Tensor *cpu_tensor,
+                                              Device *device,
+                                              Tensor *device_tensor,
+                                              StatusCallback done) const {
+  const int64 total_bytes = cpu_tensor->TotalBytes();
+  if (total_bytes > 0) {
+    const void *src_ptr = DMAHelper::base(cpu_tensor);
+    void *dst_ptr = DMAHelper::base(device_tensor);
+    switch (cpu_tensor->dtype()) {
+      case DT_FLOAT:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<float *>(dst_ptr), static_cast<const float *>(src_ptr),
+            total_bytes);
+        break;
+      case DT_DOUBLE:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<double *>(dst_ptr),
+            static_cast<const double *>(src_ptr), total_bytes);
+        break;
+      case DT_INT32:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<int32 *>(dst_ptr), static_cast<const int32 *>(src_ptr),
+            total_bytes);
+        break;
+      case DT_INT64:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<int64 *>(dst_ptr), static_cast<const int64 *>(src_ptr),
+            total_bytes);
+        break;
+      case DT_HALF:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<Eigen::half *>(dst_ptr),
+            static_cast<const Eigen::half *>(src_ptr), total_bytes);
+        break;
+      case DT_COMPLEX64:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<std::complex<float> *>(dst_ptr),
+            static_cast<const std::complex<float> *>(src_ptr), total_bytes);
+        break;
+      case DT_COMPLEX128:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<std::complex<double> *>(dst_ptr),
+            static_cast<const std::complex<double> *>(src_ptr), total_bytes);
+        break;
+      case DT_INT8:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<int8 *>(dst_ptr), static_cast<const int8 *>(src_ptr),
+            total_bytes);
+        break;
+      case DT_INT16:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<int16 *>(dst_ptr), static_cast<const int16 *>(src_ptr),
+            total_bytes);
+        break;
+      case DT_UINT8:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<uint8 *>(dst_ptr), static_cast<const uint8 *>(src_ptr),
+            total_bytes);
+        break;
+      case DT_UINT16:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<uint16 *>(dst_ptr),
+            static_cast<const uint16 *>(src_ptr), total_bytes);
+        break;
+      case DT_BOOL:
+        device->eigen_sycl_device()->memcpyHostToDevice(
+            static_cast<bool *>(dst_ptr), static_cast<const bool *>(src_ptr),
+            total_bytes);
+        break;
+      default:
+        assert(false && 'unsupported type');
+    }
+  }
+  device->eigen_sycl_device()->synchronize();
+  done(Status::OK());
+}
+    
+    class SYCLDeviceContext : public DeviceContext {
+ public:
+  SYCLDeviceContext() {}
+    }
+    
+    #ifdef TENSORFLOW_USE_MPI
+    
+    
+    {#undef REGISTER_COMPLEX
+}  // namespace tensorflow
+
+    
+    #ifndef ATOM_BROWSER_API_ATOM_API_GLOBAL_SHORTCUT_H_
+#define ATOM_BROWSER_API_ATOM_API_GLOBAL_SHORTCUT_H_
+    
+    
+    {}  // namespace atom
+    
+    class PreferencesManager : public content::RenderThreadObserver {
+ public:
+  PreferencesManager();
+  ~PreferencesManager() override;
+    }
+    
+    // Platform-neutral implementation of a class that keeps track of observers and
+// monitors keystrokes. It relays messages to the appropriate observer when a
+// global shortcut has been struck by the user.
+class GlobalShortcutListener {
+ public:
+  class Observer {
+   public:
+    // Called when your global shortcut (|accelerator|) is struck.
+    virtual void OnKeyPressed(const ui::Accelerator& accelerator) = 0;
+  };
+    }
+    
+     protected:
+  /// Calculate the URL once and cache the result.
+  std::string uri_;
+    
+      std::string bad_line =
+      R'('2016-03-22T21:17:01.701882+00:00','vagrant-ubuntu-trusty-64','6','cron',)';
+  ec = pub.createEventContext();
+  status = pub.populateEventContext(bad_line, ec);
+  ASSERT_FALSE(status.ok());
+  ASSERT_NE(std::string::npos, status.getMessage().find('fewer'));
+    
+    #include <boost/filesystem.hpp>
+    
+    FLAG(bool, disable_memory, false, 'Disable physical memory reads');
