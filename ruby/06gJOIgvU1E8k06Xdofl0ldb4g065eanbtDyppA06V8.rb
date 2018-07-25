315808f53529ec69418bb0cacc546f11fb49a2cd
SUNsung <weatherius@gmail.com>
2018-07-25 18:29:01 +0300

    Rewrite [06gJOIgvU1E8k06Xdofl0ldb4g065eanbtDyppA06V8.rb] File in: 2018-07-25 18:29:00

--- ruby/06gJOIgvU1E8k06Xdofl0ldb4g065eanbtDyppA06V8.rb
+++ ruby/06gJOIgvU1E8k06Xdofl0ldb4g065eanbtDyppA06V8.rb
@@ -1,122 +1,59 @@
 
         
-            class CreateKeychainAction < Action
-      def self.run(params)
-        escaped_password = params[:password].shellescape
+          def tumblr_consumer_key
+    ENV['TUMBLR_OAUTH_KEY']
+  end
     
-        options[:title] = 'CoffeeScript'
-    options[:skip_links] = true
+      def set_table_sort(sort_options)
+    valid_sorts = sort_options[:sorts] or raise ArgumentError.new('You must specify :sorts as an array of valid sort attributes.')
+    default = sort_options[:default] || { valid_sorts.first.to_sym => :desc }
     
-          html_filters.push 'd3/clean_html', 'd3/entries_v3', 'title'
+      def text_url
+    object.local? ? medium_url(object) : nil
+  end
     
-        self.name = 'PHP'
-    self.type = 'php'
-    self.release = '7.2.5'
-    self.base_url = 'https://secure.php.net/manual/en/'
-    self.root_path = 'index.html'
-    self.initial_paths = %w(
-      funcref.html
-      langref.html
-      refs.database.html
-      set.mysqlinfo.html
-      language.control-structures.html
-      reference.pcre.pattern.syntax.html
-      reserved.exceptions.html
-      reserved.interfaces.html
-      reserved.variables.html)
+        existence_maps = grouped_codes.map { |c| c.map { |cc| [cc, File.exist?(Rails.root.join('public', 'emoji', codepoints_to_filename(cc) + '.svg'))] }.to_h }
+    map = {}
     
-        def root?
-      path == 'index'
+        # Description formatted to work well as page title when viewing gist
+    if f.core_formula?
+      descr = '#{f.name} on #{OS_VERSION} - Homebrew build logs'
+    else
+      descr = '#{f.name} (#{f.full_name}) on #{OS_VERSION} - Homebrew build logs'
+    end
+    url = create_gist(files, descr)
+    
+    require 'formula'
+    
+              # Check for git:// GitHub repo urls, https:// is preferred.
+          git_gh_pattern = %r{^git://[^/]*github\.com/}
+          audit_urls(urls, git_gh_pattern) do |_, url|
+            problem 'Please use https:// for #{url}'
+          end
+    
+        def define_setter
+      name = @name
+      @klass.send :define_method, '#{@name}=' do |file|
+        send(name).assign(file)
+      end
     end
     
-              results
+            def rejected_types_rejected?
+          @missing_rejected_types ||= @rejected_types.select { |type| type_allowed?(type) }
+          @missing_rejected_types.none?
         end
-    
-          # This inserts a block with the given key and value.
-      #
-      # @param [String] key
-      # @param [String] value
-      def insert(key, value)
-        # Insert the new block into the value
-        new_block = <<BLOCK
-# VAGRANT-BEGIN: #{key}
-#{value.strip}
-# VAGRANT-END: #{key}
-BLOCK
-    
-      if Rails.env == 'development'
-    test_conf = ActiveRecord::Base.configurations['test']
-    
-        describe 'feed regeneration' do
-      before do
-        alice = Fabricate(:account)
-        bob   = Fabricate(:account)
-    
-      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/favourite
-  def favourite
-    f = Favourite.last
-    NotificationMailer.favourite(f.status.account, Notification.find_by(activity: f))
-  end
-    
-          out =
-        Sass::Util.silence_sass_warnings do
-          if @options[:from] == :css
-            require 'sass/css'
-            Sass::CSS.new(read(input), @options[:for_tree]).render(@options[:to])
-          else
-            if input_path
-              Sass::Engine.for_file(input_path, @options[:for_engine])
-            else
-              Sass::Engine.new(read(input), @options[:for_engine])
-            end.to_tree.send('to_#{@options[:to]}', @options[:for_tree])
-          end
-        end
-    
-      if defined? config.symbolize_keys!
-    config.symbolize_keys!
-  end
-    
-        # Converts a script node into a corresponding string interpolation
-    # expression.
-    #
-    # @param node_or_interp [Sass::Script::Tree::Node]
-    # @return [Sass::Script::Tree::StringInterpolation]
-    def to_string_interpolation(node_or_interp)
-      unless node_or_interp.is_a?(Interpolation)
-        node = node_or_interp
-        return string_literal(node.value.to_s) if node.is_a?(Literal)
-        if node.is_a?(StringInterpolation)
-          return concat(string_literal(node.quote), concat(node, string_literal(node.quote)))
-        end
-        return StringInterpolation.new(string_literal(''), node, string_literal(''))
       end
+    end
+  end
+end
+
     
-    module RuboCop
-  module Cop
-    module Lint
-      # In math and Python, we can use `x < y < z` style comparison to compare
-      # multiple value. However, we can't use the comparison in Ruby. However,
-      # the comparison is not syntax error. This cop checks the bad usage of
-      # comparison operators.
-      #
-      # @example
-      #
-      #   # bad
-      #
-      #   x < y < z
-      #   10 <= x <= 20
-      #
-      # @example
-      #
-      #   # good
-      #
-      #   x < y && y < z
-      #   10 <= x && x <= 20
-      class MultipleCompare < Cop
-        MSG = 'Use the `&&` operator to compare multiple values.'.freeze
+            def matches? subject
+          @subject = subject
+          @subject = subject.new if subject.class == Class
+          error_when_not_valid? && no_error_when_valid?
+        end
     
-            def on_block(node)
-          on_body_of_reduce(node) do |body|
-            void_next = body.each_node(:next).find do |n|
-              n.children.empty? && parent_block_node(n) == node
-            end
+          def mark_invalid(record, attribute, types)
+        record.errors.add attribute, :invalid, options.merge(:types => types.join(', '))
+      end
