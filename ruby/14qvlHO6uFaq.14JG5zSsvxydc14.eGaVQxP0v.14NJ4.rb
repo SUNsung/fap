f55a001fa4f7e3536ece21b39b90a37a07e18670
SUNsung <weatherius@gmail.com>
2018-07-25 03:55:48 +0300

    Rewrite [14qvlHO6uFaq.14JG5zSsvxydc14.eGaVQxP0v.14NJ4.rb] File in: 2018-07-25 03:55:47

--- ruby/14qvlHO6uFaq.14JG5zSsvxydc14.eGaVQxP0v.14NJ4.rb
+++ ruby/14qvlHO6uFaq.14JG5zSsvxydc14.eGaVQxP0v.14NJ4.rb
@@ -1,87 +1,29 @@
 
         
-          describe :render do
-    let(:screenshots) { [] }
-    
-          if valid_type?(type)
-        type.constantize.new(attributes).tap do |instance|
-          instance.user = user if instance.respond_to?(:user=)
-        end
-      else
-        const_get(:BASE_CLASS_NAME).constantize.new(attributes).tap do |instance|
-          instance.type = type
-          instance.user = user if instance.respond_to?(:user=)
-        end
-      end
-    end
+            projects
   end
 end
+
     
-        valid_oauth_providers :tumblr
-  end
+    describe ApplicationController, type: :controller do
+  controller do
+    include UserTrackingConcern
     
-        if params[:sort].present?
-      attribute, direction = params[:sort].downcase.split('.')
-      unless valid_sorts.include?(attribute)
-        attribute, direction = default.to_a.first
-      end
-    else
-      attribute, direction = default.to_a.first
-    end
+    run SinatraStaticServer
+
     
-      def toggle_availability
-    @service = current_user.services.find(params[:id])
-    @service.toggle_availability!
-    
-    def usage
-  <<-EOS
-Usage: list_login_items_for_app <path.app>
-    
-    def process_args
-  until ARGV.empty?
-    if ARGV.first =~ %r{^-+t(?:est)?$} && ARGV.length > 1
-      ARGV.shift
-      $opt_test = ARGV.shift
-    elsif ARGV.first =~ %r{^-+h(?:elp)?$}
-      puts usage
-      exit 0
-    else
-      puts usage
-      exit 1
-    end
-  end
-end
-    
-            def <<(value)
-          values << value
-        end
-    
-      desc 'Publish the release.'
-  task :publishing do
-  end
-    
-          it 'sets the input as the variable' do
-        expect(dsl.fetch(:scm)).to eq 'git'
+      if options.respond_to? 'keys'
+    options.each do |k,v|
+      unless v.nil?
+        v = v.join ',' if v.respond_to? 'join'
+        v = v.to_json if v.respond_to? 'keys'
+        output += ' data-#{k.sub'_','-'}='#{v}''
       end
     end
-    
-    post '/' do
-  connections.each { |out| out << 'data: #{params[:msg]}\n\n' }
-  204 # response without entity body
+  elsif options.respond_to? 'join'
+    output += ' data-value='#{config[key].join(',')}''
+  else
+    output += ' data-value='#{config[key]}''
+  end
+  output += '></#{tag}>'
 end
-    
-    module Rack
-  module Protection
-    ##
-    # Prevented attack::   Cookie Tossing
-    # Supported browsers:: all
-    # More infos::         https://github.com/blog/1466-yummy-cookies-across-domains
-    #
-    # Does not accept HTTP requests if the HTTP_COOKIE header contains more than one
-    # session cookie. This does not protect against a cookie overflow attack.
-    #
-    # Options:
-    #
-    # session_key:: The name of the session cookie (default: 'rack.session')
-    class CookieTossing < Base
-      default_reaction :deny
