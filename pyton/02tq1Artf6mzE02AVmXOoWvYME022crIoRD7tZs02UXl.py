7a6f2e22988b6537d23c8ee03edcb3883888383f
SUNsung <weatherius@gmail.com>
2018-07-25 21:18:36 +0300

    Rewrite [02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py] File in: 2018-07-25 21:18:35

--- pyton/02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py
+++ pyton/02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py
@@ -1,168 +1,43 @@
 
         
-        
-def consume_socket_content(sock, timeout=0.5):
-    chunks = 65536
-    content = b''
+                # because special names such as Name.Class, Name.Function, etc.
+        # are not recognized as such later in the parsing, we choose them
+        # to look the same as ordinary variables.
+        Name:                      '#000000',        # class: 'n'
+        Name.Attribute:            '#c4a000',        # class: 'na' - to be revised
+        Name.Builtin:              '#004461',        # class: 'nb'
+        Name.Builtin.Pseudo:       '#3465a4',        # class: 'bp'
+        Name.Class:                '#000000',        # class: 'nc' - to be revised
+        Name.Constant:             '#000000',        # class: 'no' - to be revised
+        Name.Decorator:            '#888',           # class: 'nd' - to be revised
+        Name.Entity:               '#ce5c00',        # class: 'ni'
+        Name.Exception:            'bold #cc0000',   # class: 'ne'
+        Name.Function:             '#000000',        # class: 'nf'
+        Name.Property:             '#000000',        # class: 'py'
+        Name.Label:                '#f57900',        # class: 'nl'
+        Name.Namespace:            '#000000',        # class: 'nn' - to be revised
+        Name.Other:                '#000000',        # class: 'nx'
+        Name.Tag:                  'bold #004461',   # class: 'nt' - like a keyword
+        Name.Variable:             '#000000',        # class: 'nv' - to be revised
+        Name.Variable.Class:       '#000000',        # class: 'vc' - to be revised
+        Name.Variable.Global:      '#000000',        # class: 'vg' - to be revised
+        Name.Variable.Instance:    '#000000',        # class: 'vi' - to be revised
     
-        :param str u_string: unicode string to check. Must be unicode
-        and not Python 2 `str`.
-    :rtype: bool
-    '''
-    assert isinstance(u_string, str)
-    try:
-        u_string.encode('ascii')
-        return True
-    except UnicodeEncodeError:
-        return False
-
-    
-    try:
-    from urllib3.contrib import pyopenssl
-except ImportError:
-    pyopenssl = None
-    OpenSSL = None
-    cryptography = None
-else:
-    import OpenSSL
-    import cryptography
-    
-    import copy
-import time
-import calendar
-    
-        def get(self, key, default=None):
-        return self.__dict__.get(key, default)
-
-    
-        :param url: URL for the new :class:`Request` object.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    '''
-    
-    # begin[licence]
-#
-# [The 'BSD licence']
-# Copyright (c) 2005-2008 Terence Parr
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# end[licence]
-    
-                    # not in range and not EOF/EOT, must be invalid symbol
-                self.noViableAlt(s, input)
-                return 0
-    
-            else:
-            return self.c
-    
-        # overridden by generated subclasses
-    tokenNames = None
-    
-    INVALID_TOKEN_TYPE = 0
-    
-    for prec in [9, 19]:
-    print('\nPrecision: %d decimal digits\n' % prec)
-    for func in to_benchmark:
-        start = time.time()
-        if C is not None:
-            C.getcontext().prec = prec
-        P.getcontext().prec = prec
-        for i in range(10000):
-            x = func()
-        print('%s:' % func.__name__.replace('pi_', ''))
-        print('result: %s' % str(x))
-        print('time: %fs\n' % (time.time()-start))
-    
-    if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        zones = TZInfo.zonelist()
-        for z in zones:
-            print(z)
-        sys.exit()
-    filepath = sys.argv[1]
-    if not filepath.startswith('/'):
-        filepath = os.path.join('/usr/share/zoneinfo', filepath)
-    with open(filepath, 'rb') as fileobj:
-        tzi = TZInfo.fromfile(fileobj)
-    tzi.dump(sys.stdout)
-
-    
-    
-class DifferentAgentTest(CrawlDelayAndRequestRateTest):
-    agent = 'FigTree Robot libwww-perl/5.04'
-    # these are not actually tested, but we still need to parse it
-    # in order to accommodate the input parameters
-    request_rate = None
-    crawl_delay = None
-    
-        The func will be passed to sys.setprofile() for each thread, before its
-    run() method is called.
-    
-        '''
-    def gen(a, b):
+        def run(self):
         try:
-            while 1:
-                x = next(a)
-                y = next(b)
-                yield x
-                yield y
-        except StopIteration:
-            return
+            self.server_sock = self._create_socket_and_bind()
+            # in case self.port = 0
+            self.port = self.server_sock.getsockname()[1]
+            self.ready_event.set()
+            self._handle_requests()
     
-        def test_various___class___pathologies(self):
-        # See issue #12370
-        class X(A):
-            def f(self):
-                return super().f()
-            __class__ = 413
-        x = X()
-        self.assertEqual(x.f(), 'A')
-        self.assertEqual(x.__class__, 413)
-        class X:
-            x = __class__
-            def f():
-                __class__
-        self.assertIs(X.x, type(self))
-        with self.assertRaises(NameError) as e:
-            exec('''class X:
-                __class__
-                def f():
-                    __class__''', globals(), {})
-        self.assertIs(type(e.exception), NameError) # Not UnboundLocalError
-        class X:
-            global __class__
-            __class__ = 42
-            def f():
-                __class__
-        self.assertEqual(globals()['__class__'], 42)
-        del globals()['__class__']
-        self.assertNotIn('__class__', X.__dict__)
-        class X:
-            nonlocal __class__
-            __class__ = 42
-            def f():
-                __class__
-        self.assertEqual(__class__, 42)
+    
+def test_idna_with_version_attribute(mocker):
+    '''Verify we're actually setting idna version when it should be available.'''
+    mocker.patch('requests.help.idna', new=VersionedPackage('2.6'))
+    assert info()['idna'] == {'version': '2.6'}
+
+    
+    
+class URLRequired(RequestException):
+    '''A valid URL is required to make a request.'''
