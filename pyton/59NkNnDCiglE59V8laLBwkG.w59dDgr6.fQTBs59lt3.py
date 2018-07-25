65e51558c14ac71d89c8e4015adc7f62a7db3fe2
SUNsung <weatherius@gmail.com>
2018-07-26 00:08:39 +0300

    Rewrite [59NkNnDCiglE59V8laLBwkG.w59dDgr6.fQTBs59lt3.py] File in: 2018-07-26 00:08:38

--- pyton/59NkNnDCiglE59V8laLBwkG.w59dDgr6.fQTBs59lt3.py
+++ pyton/59NkNnDCiglE59V8laLBwkG.w59dDgr6.fQTBs59lt3.py
@@ -1,173 +1,92 @@
 
         
-                def check(self, value):
-            return isinstance(value, Foo)
+            tag = to_json
     
     
-# Core signals.  For usage examples grep the source code or consult
-# the API documentation in docs/api.rst as well as docs/signals.rst
-template_rendered = _signals.signal('template-rendered')
-before_render_template = _signals.signal('before-render-template')
-request_started = _signals.signal('request-started')
-request_finished = _signals.signal('request-finished')
-request_tearing_down = _signals.signal('request-tearing-down')
-got_request_exception = _signals.signal('got-request-exception')
-appcontext_tearing_down = _signals.signal('appcontext-tearing-down')
-appcontext_pushed = _signals.signal('appcontext-pushed')
-appcontext_popped = _signals.signal('appcontext-popped')
-message_flashed = _signals.signal('message-flashed')
+def test_get_namespace():
+    app = flask.Flask(__name__)
+    app.config['FOO_OPTION_1'] = 'foo option 1'
+    app.config['FOO_OPTION_2'] = 'foo option 2'
+    app.config['BAR_STUFF_1'] = 'bar stuff 1'
+    app.config['BAR_STUFF_2'] = 'bar stuff 2'
+    foo_options = app.config.get_namespace('FOO_')
+    assert 2 == len(foo_options)
+    assert 'foo option 1' == foo_options['option_1']
+    assert 'foo option 2' == foo_options['option_2']
+    bar_options = app.config.get_namespace('BAR_', lowercase=False)
+    assert 2 == len(bar_options)
+    assert 'bar stuff 1' == bar_options['STUFF_1']
+    assert 'bar stuff 2' == bar_options['STUFF_2']
+    foo_options = app.config.get_namespace('FOO_', trim_namespace=False)
+    assert 2 == len(foo_options)
+    assert 'foo option 1' == foo_options['foo_option_1']
+    assert 'foo option 2' == foo_options['foo_option_2']
+    bar_options = app.config.get_namespace('BAR_', lowercase=False, trim_namespace=False)
+    assert 2 == len(bar_options)
+    assert 'bar stuff 1' == bar_options['BAR_STUFF_1']
+    assert 'bar stuff 2' == bar_options['BAR_STUFF_2']
+    
+            (2016-01, shopping), 25
+        (2016-01, shopping), 100
+        (2016-01, gas), 50
+        '''
+        timestamp, category, amount = line.split('\t')
+        period = self. extract_year_month(timestamp)
+        if period == self.current_year_month():
+            yield (period, category), amount
+    
+    
+def rst_filenames():
+    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
+        if '.tox' not in root:
+            for filename in fnmatch.filter(filenames, '*.rst'):
+                yield os.path.join(root, filename)
+    
+    
+class BaseConfigDict(dict):
+    
+    
+def test_current_version():
+    version = Environment().config['__meta__']['httpie']
+    assert version == __version__
 
     
-        @app.route('/')
-    def index():
-        return None
+        def test_format_option(self, httpbin):
+        env = MockEnvironment(colors=256)
+        r = http('--print=B', '--pretty=format',
+                 'GET', httpbin.url + '/get', 'a=b',
+                 env=env)
+        # Tests that the JSON data is formatted.
+        assert r.strip().count('\n') == 2
+        assert COLOR not in r
     
-        flask.message_flashed.connect(record, app)
-    try:
-        client = app.test_client()
-        with client.session_transaction():
-            client.get('/')
-            assert len(recorded) == 1
-            message, category = recorded[0]
-            assert message == 'This is a flash message'
-            assert category == 'notice'
-    finally:
-        flask.message_flashed.disconnect(record, app)
+            if args.coverage_results:
+            path = os.path.join(output_dir, '%s/coverage.json' % job_number)
+            url = 'https://api.shippable.com/jobs/%s/jobCoverageReports' % job_id
+            download(args, headers, path, url)
+            extract_contents(args, path, os.path.join(output_dir, '%s/coverage' % job_number))
     
-        def crawl(self):
-        while True:
-            page = self.data_store.extract_max_priority_page()
-            if page is None:
-                break
-            if self.data_store.crawled_similar(page.signature):
-                self.data_store.reduce_priority_link_to_crawl(page.url)
-            else:
-                self.crawl_page(page)
-            page = self.data_store.extract_max_priority_page()
-
-    
-        def extract_year_month(self, timestamp):
-        '''Return the year and month portions of the timestamp.'''
-        ...
-    
-    # The ## imports above & following could help make some tests gui-free.
-# However, they currently make radiobutton tests fail.
-##def setUpModule():
-##    # Replace tk objects used to initialize se.SearchEngine.
-##    se.BooleanVar = Var
-##    se.StringVar = Var
-##
-##def tearDownModule():
-##    se.BooleanVar = BooleanVar
-##    se.StringVar = StringVar
-    
-    simple_escapes = {'a': '\a',
-                  'b': '\b',
-                  'f': '\f',
-                  'n': '\n',
-                  'r': '\r',
-                  't': '\t',
-                  'v': '\v',
-                  ''': ''',
-                  ''': ''',
-                  '\\': '\\'}
-    
-            # Add .lib files this module needs
-        for modlib in moddefn.GetLinkerLibs():
-            if modlib not in libs:
-                libs.append(modlib)
-    
-            try:
-            # call the matching registered function
-            func = self.funcs[method]
-        except KeyError:
-            pass
-        else:
-            if func is not None:
-                return func(*params)
-            raise Exception('method '%s' is not supported' % method)
+            input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/targetHttpProxies'
+        actual = GCPUtils.parse_gcp_url(input_url)
+        self.assertEquals('compute', actual['service'])
+        self.assertEquals('v1', actual['api_version'])
+        self.assertEquals('myproject', actual['project'])
+        self.assertFalse('global' in actual)
+        self.assertEquals('targetHttpProxies', actual['resource_name'])
     
     
-def open_binary(package: Package, resource: Resource) -> BinaryIO:
-    '''Return a file-like object opened for binary reading of the resource.'''
-    resource = _normalize_path(resource)
-    package = _get_package(package)
-    reader = _get_resource_reader(package)
-    if reader is not None:
-        return reader.open_resource(resource)
-    _check_location(package)
-    absolute_package_path = os.path.abspath(package.__spec__.origin)
-    package_path = os.path.dirname(absolute_package_path)
-    full_path = os.path.join(package_path, resource)
-    try:
-        return open(full_path, mode='rb')
-    except OSError:
-        # Just assume the loader is a resource loader; all the relevant
-        # importlib.machinery loaders are and an AttributeError for
-        # get_data() will make it clear what is needed from the loader.
-        loader = cast(ResourceLoader, package.__spec__.loader)
-        data = None
-        if hasattr(package.__spec__.loader, 'get_data'):
-            with suppress(OSError):
-                data = loader.get_data(full_path)
-        if data is None:
-            package_name = package.__spec__.name
-            message = '{!r} resource not found in {!r}'.format(
-                resource, package_name)
-            raise FileNotFoundError(message)
-        else:
-            return BytesIO(data)
+class Controller(object):
     
-    try:
-    # compatible for python2
-    from urllib2 import urlopen
-    from urllib2 import HTTPError
-    from urllib2 import URLError
-except ImportError:
-    # compatible for python3
-    from urllib.request import urlopen
-    from urllib.error import HTTPError
-    from urllib.error import URLError
+    - single source 'message type' for state transition changes
+- message type considered, messages (comment) not considered to avoid complexity
+'''
     
-    class _AcquireFutures(object):
-    '''A context manager that does an ordered acquire of Future conditions.'''
+    class ParameterInjectionTest(unittest.TestCase):
     
-    Local worker thread:
-- reads work ids from the 'Work Ids' queue and looks up the corresponding
-  WorkItem from the 'Work Items' dict: if the work item has been cancelled then
-  it is simply removed from the dict, otherwise it is repackaged as a
-  _CallItem and put in the 'Call Q'. New _CallItems are put in the 'Call Q'
-  until 'Call Q' is full. NOTE: the size of the 'Call Q' is kept small because
-  calls placed in the 'Call Q' can no longer be cancelled with Future.cancel().
-- reads _ResultItems from 'Result Q', updates the future stored in the
-  'Work Items' dict and deletes the dict entry
+    class ParameterInjectionTest(unittest.TestCase):
     
-        sqrt_n = int(math.floor(math.sqrt(n)))
-    for i in range(3, sqrt_n + 1, 2):
-        if n % i == 0:
-            return False
-    return True
-    
-    
-FILTER_COMPILERS = { 'regex' : CompileRegex,
-                     'level' : CompileLevel }
-    
-    
-def ExtractKeywordsFromGroup_KeywordMiddle_test():
-  assert_that( syntax_parse._ExtractKeywordsFromGroup(
-                 syntax_parse.SyntaxGroup( '', [
-                   'foo contained bar',
-                   'zoo goo'
-                 ] ) ),
-               contains_inanyorder( 'foo', 'contained', 'bar', 'zoo', 'goo' ) )
-    
-      if 'word' in vim_data:
-    completion_data[ 'insertion_text' ] = vim_data[ 'word' ]
-  if 'abbr' in vim_data:
-    completion_data[ 'menu_text' ] = vim_data[ 'abbr' ]
-  if 'menu' in vim_data:
-    completion_data[ 'extra_menu_info' ] = vim_data[ 'menu' ]
-  if 'kind' in vim_data:
-    completion_data[ 'kind' ] = [ vim_data[ 'kind' ] ]
-  if 'info' in vim_data:
-    completion_data[ 'detailed_info' ] = vim_data[ 'info' ]
+    ### OUTPUT ###
+# Counting to two...
+# one two
+# Counting to five...
+# one two three four five
