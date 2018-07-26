3d20c2a15afa876e6d28ade9ffade9d0ac56364d
SUNsung <weatherius@gmail.com>
2018-07-26 17:24:33 +0300

    Rewrite [0057HFUisG5Q005I8s9KtpUfA00POte1SVpshw003DE.py] File in: 2018-07-26 17:24:32

--- pyton/0057HFUisG5Q005I8s9KtpUfA00POte1SVpshw003DE.py
+++ pyton/0057HFUisG5Q005I8s9KtpUfA00POte1SVpshw003DE.py
@@ -1,43 +1,22 @@
 
         
-            def from_envvar(self, variable_name, silent=False):
-        '''Loads a configuration from an environment variable pointing to
-        a configuration file.  This is basically just a shortcut with nicer
-        error messages for this line of code::
+                def __init__(self, name, doc=None):
+            self.name = name
+            self.__doc__ = doc
+        def _fail(self, *args, **kwargs):
+            raise RuntimeError('signalling support is unavailable '
+                               'because the blinker library is '
+                               'not installed.')
+        send = lambda *a, **kw: None
+        connect = disconnect = has_receivers_for = receivers_for = \
+            temporarily_connected_to = connected_to = _fail
+        del _fail
     
-        If you configure your own :class:`logging.StreamHandler`, you may want to
-    use this for the stream. If you are using file or dict configuration and
-    can't import this directly, you can refer to it as
-    ``ext://flask.logging.wsgi_errors_stream``.
+    
+class Environment(BaseEnvironment):
+    '''Works like a regular Jinja2 environment but has some additional
+    knowledge of how Flask's blueprint works so that it can prepend the
+    name of the blueprint to referenced templates if necessary.
     '''
-    return request.environ['wsgi.errors'] if request else sys.stderr
     
-            rv = app.config['SERVER_NAME']
-    
-    signals_available = False
-try:
-    from blinker import Namespace
-    signals_available = True
-except ImportError:
-    class Namespace(object):
-        def signal(self, name, doc=None):
-            return _FakeSignal(name, doc)
-    
-    
-def common_object_test(app):
-    assert app.secret_key == 'config'
-    assert app.config['TEST_KEY'] == 'foo'
-    assert 'TestConfig' not in app.config
-    
-        logger.propagate = False
-    assert not has_level_handler(logger)
-    logger.propagate = True
-    
-    def shorten_title(title):
-    m1 = re.search('[[0-9]*]', title)
-    m2 = re.search(''.*'', title)
-    if m1:
-        title = m1.group(0)
-    if m2:
-        title = ' '.join((title, m2.group(0)))   
-    return title[:50] + ' [...]'    
+        yield
