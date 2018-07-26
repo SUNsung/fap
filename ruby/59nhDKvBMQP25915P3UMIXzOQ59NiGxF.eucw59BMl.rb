7b2c15664710fea99a1862c838132742dc9e138a
SUNsung <weatherius@gmail.com>
2018-07-27 02:28:52 +0300

    Rewrite [59nhDKvBMQP25915P3UMIXzOQ59NiGxF.eucw59BMl.rb] File in: 2018-07-27 02:28:51

--- ruby/59nhDKvBMQP25915P3UMIXzOQ59NiGxF.eucw59BMl.rb
+++ ruby/59nhDKvBMQP25915P3UMIXzOQ59NiGxF.eucw59BMl.rb
@@ -1,82 +1,53 @@
 
         
-          def failure
-    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
-    redirect_to after_omniauth_failure_path_for(resource_name)
+        module ActionCable
+  module Server
+    # An instance of this configuration object is available via ActionCable.server.config, which allows you to tweak Action Cable configuration
+    # in a Rails config initializer.
+    class Configuration
+      attr_accessor :logger, :log_tags
+      attr_accessor :connection_class, :worker_pool_size
+      attr_accessor :disable_request_forgery_protection, :allowed_request_origins, :allow_same_origin_as_host
+      attr_accessor :cable, :url, :mount_path
+    
+            def test_encoded_password
+          password = 'am@z1ng_p@ssw0rd#!'
+          encoded_password = URI.encode_www_form_component(password)
+          spec = resolve 'abstract://foo:#{encoded_password}@localhost/bar'
+          assert_equal password, spec['password']
+        end
+    
+      def title_is_wrong_create
+    errors[:title] << 'is Wrong Create' if attribute_present?('title') && title == 'Wrong Create'
   end
     
-        if resource.errors.empty?
-      set_flash_message!(:notice, :confirmed)
-      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+        def connect
+      reject_unauthorized_connection
     end
+    
+      test 'rack response' do
+    run_in_eventmachine do
+      connection = open_connection
+      response = connection.process
+    
+      test 'unsubscribe from all' do
+    run_in_eventmachine do
+      setup_connection
+    
+        render_response
   end
     
-        # Check if a reset_password_token is provided in the request
-    def assert_reset_token_passed
-      if params[:reset_password_token].blank?
-        set_flash_message(:alert, :no_token)
-        redirect_to new_session_path(resource_name)
+      # Finds the groups of the source user, optionally limited to those visible to
+  # the current user.
+  def execute(current_user = nil)
+    segments = all_groups(current_user)
+    
+          def fetch_public_key(o_auth_app, jwt)
+        public_key = fetch_public_key_from_json(o_auth_app.jwks, jwt)
+        if public_key.empty? && o_auth_app.jwks_uri
+          response = Faraday.get(o_auth_app.jwks_uri)
+          public_key = fetch_public_key_from_json(response.body, jwt)
+        end
+        raise Rack::OAuth2::Server::Authorize::BadRequest(:unauthorized_client) if public_key.empty?
+        public_key
       end
-    end
-    
-        def password_change(record, opts={})
-      devise_mail(record, :password_change, opts)
-    end
-  end
-end
-
-    
-        mkdir_p page_dir
-    file = '#{page_dir}/#{filename}.#{extension}'
-    if File.exist?(file)
-      abort('rake aborted!') if ask('#{file} already exists. Do you want to overwrite?', ['y', 'n']) == 'n'
-    end
-    puts 'Creating new page: #{file}'
-    open(file, 'w') do |page|
-      page.puts '---'
-      page.puts 'layout: page'
-      page.puts 'title: \'#{title}\''
-      page.puts 'date: #{Time.now.strftime('%Y-%m-%d %H:%M')}'
-      page.puts 'comments: true'
-      page.puts 'sharing: true'
-      page.puts 'footer: true'
-      page.puts '---'
-    end
-  else
-    puts 'Syntax error: #{args.filename} contains unsupported characters'
-  end
-end
-    
-      def send_sinatra_file(path, &missing_file_block)
-    file_path = File.join(File.dirname(__FILE__), 'public',  path)
-    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
-    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
-  end
-    
-    module Jekyll
-    
-      class IncludeArrayTag < Liquid::Tag
-    Syntax = /(#{Liquid::QuotedFragment}+)/
-    def initialize(tag_name, markup, tokens)
-      if markup =~ Syntax
-        @array_name = $1
-      else
-        raise SyntaxError.new('Error in tag 'include_array' - Valid syntax: include_array [array from _config.yml]')
-      end
-    
-      class IncludeCodeTag < Liquid::Tag
-    def initialize(tag_name, markup, tokens)
-      @title = nil
-      @file = nil
-      if markup.strip =~ /\s*lang:(\S+)/i
-        @filetype = $1
-        markup = markup.strip.sub(/lang:\S+/i,'')
-      end
-      if markup.strip =~ /(.*)?(\s+|^)(\/*\S+)/i
-        @title = $1 || nil
-        @file = $3
-      end
-      super
-    end
