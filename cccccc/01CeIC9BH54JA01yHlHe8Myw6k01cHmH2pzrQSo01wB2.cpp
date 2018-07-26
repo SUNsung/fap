34be4a357190276cb78d99ac93cd01edc8b6d303
SUNsung <weatherius@gmail.com>
2018-07-26 18:52:31 +0300

    Rewrite [01CeIC9BH54JA01yHlHe8Myw6k01cHmH2pzrQSo01wB2.cpp] File in: 2018-07-26 18:52:30

--- cccccc/01CeIC9BH54JA01yHlHe8Myw6k01cHmH2pzrQSo01wB2.cpp
+++ cccccc/01CeIC9BH54JA01yHlHe8Myw6k01cHmH2pzrQSo01wB2.cpp
@@ -1,38 +1,112 @@
 
         
-        namespace A2STR {
+        TEST_CASE('Optim/XORConvergence/Adagrad') {
+  REQUIRE(test_optimizer_xor<Adagrad>(
+      AdagradOptions(1.0).weight_decay(1e-6).lr_decay(1e-3)));
+}
+    
+    Caffe2Ops Caffe2Backend::CreateReciprocal(
+    OnnxNode* onnx_node,
+    int /*opset_version*/) {
+  const auto& node = onnx_node->node;
+  if (node.input_size() != 1 || node.output_size() != 1) {
+    CAFFE_THROW('Caffe2 Reciprocal should have 1 input and 1 output');
+  }
     }
     
-      virtual void truncate(int64_t length) CXX11_OVERRIDE;
+      CAFFE_ENFORCE_EQ(heatmaps_in.ndim(), 4);
+  const int N = heatmaps_in.dim32(0);
+  CAFFE_ENFORCE_EQ(heatmaps_in.dim32(0), N);
+  const int keypoint_count = heatmaps_in.dim32(1);
+  const int heatmap_size = heatmaps_in.dim32(2);
+  CAFFE_ENFORCE_GE(heatmap_size, 2); // at least 2x2 for approx
+  CAFFE_ENFORCE_EQ(heatmaps_in.dim32(2), heatmaps_in.dim32(3));
     
-    void AbstractOptionHandler::updateFlags(int flag, bool val)
-{
-  if (val) {
-    flags_ |= flag;
+      bool RunOnDevice() override {
+    const auto& val = Input(0);
+    const auto& key = Input(1);
+    const auto& seg = Input(2);
+    const auto& weight = Input(3);
+    }
+    
+      bool RunOnDevice() override {
+    return DispatchHelper<TensorTypes<int32_t, int64_t>>::call(
+        this, Input(INDICES));
   }
-  else {
-    flags_ &= ~flag;
-  }
+    
+    
+    {    RemoveNodeFromNet(node);        // take it out remporarily
+    node->SetNodeName(newNodeName); // change the name
+    AddNodeToNet(node);             // and put it back
 }
     
-      virtual void setInitialOption(bool f) CXX11_OVERRIDE;
     
-    AbstractProxyRequestCommand::~AbstractProxyRequestCommand() = default;
-    
-      virtual ~AbstractProxyRequestCommand();
-    
-    #include <memory>
-    
-    size_t AnnounceList::countStoppedAllowedTier() const
-{
-  return count_if(std::begin(tiers_), std::end(tiers_),
-                  FindStoppedAllowedTier());
+    {    // write dbn file
+    net->SaveToDbnFile<ElemType>(net, dbnModelPath);
 }
     
-    #include 'TLSContext.h'
-#include 'DlAbortEx.h'
+    
+    {        // compute global progress
+        bool needToPrint = us.m_progressTracingTimer.ElapsedSeconds() > 10;
+        if (needToPrint || isTimerPaced)
+        {
+            double epochProg = ((100.0f * (double) (us.m_currentStepOffset + epochNumber)) / (double) us.m_totalNumberOfSteps);
+            mbProg = (mbProg * 100.0f) / (double) us.m_totalNumberOfSteps;
+            printf('PROGRESS: %.2f%%\n', epochProg + mbProg);
+            us.m_progressTracingTimer.Restart();
+        }
+        return needToPrint;
+    }
     
     
-    {  virtual std::unique_ptr<AuthConfig>
-  resolveAuthConfig(const std::string& hostname) = 0;
-};
+    {
+    {}  // namespace log
+}  // namespace leveldb
+    
+    namespace leveldb {
+    }
+    
+    TEST(CRC, Mask) {
+  uint32_t crc = Value('foo', 3);
+  ASSERT_NE(crc, Mask(crc));
+  ASSERT_NE(crc, Mask(Mask(crc)));
+  ASSERT_EQ(crc, Unmask(Mask(crc)));
+  ASSERT_EQ(crc, Unmask(Unmask(Mask(Mask(crc)))));
+}
+    
+    std::string Histogram::ToString() const {
+  std::string r;
+  char buf[200];
+  snprintf(buf, sizeof(buf),
+           'Count: %.0f  Average: %.4f  StdDev: %.2f\n',
+           num_, Average(), StandardDeviation());
+  r.append(buf);
+  snprintf(buf, sizeof(buf),
+           'Min: %.4f  Median: %.4f  Max: %.4f\n',
+           (num_ == 0.0 ? 0.0 : min_), Median(), max_);
+  r.append(buf);
+  r.append('------------------------------------------------------\n');
+  const double mult = 100.0 / num_;
+  double sum = 0;
+  for (int b = 0; b < kNumBuckets; b++) {
+    if (buckets_[b] <= 0.0) continue;
+    sum += buckets_[b];
+    snprintf(buf, sizeof(buf),
+             '[ %7.0f, %7.0f ) %7.0f %7.3f%% %7.3f%% ',
+             ((b == 0) ? 0.0 : kBucketLimit[b-1]),      // left
+             kBucketLimit[b],                           // right
+             buckets_[b],                               // count
+             mult * buckets_[b],                        // percentage
+             mult * sum);                               // cumulative percentage
+    r.append(buf);
+    }
+    }
+    
+    // Read through the first n keys repeatedly and check that they get
+// compacted (verified by checking the size of the key space).
+void AutoCompactTest::DoReads(int n) {
+  std::string value(kValueSize, 'x');
+  DBImpl* dbi = reinterpret_cast<DBImpl*>(db_);
+    }
+    
+      Status AddRecord(const Slice& slice);
