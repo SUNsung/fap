104485f0561ac621e0d9956ca8086fe2e77469bf
SUNsung <weatherius@gmail.com>
2018-07-26 23:59:54 +0300

    Rewrite [20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py] File in: 2018-07-26 23:59:53

--- pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
+++ pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
@@ -1,34 +1,274 @@
 
         
-            def start_requests(self):
-        qargs = {'total': self.total, 'show': self.show}
-        url = '{}?{}'.format(self.baseurl, urlencode(qargs, doseq=1))
-        return [scrapy.Request(url, dont_filter=True)]
+        
+def side_effect(old_cmd, command):
+    with tarfile.TarFile(_tar_file(old_cmd.script_parts)[0]) as archive:
+        for file in archive.getnames():
+            try:
+                os.remove(file)
+            except OSError:
+                # does not try to remove directories as we cannot know if they
+                # already existed before
+                pass
+
     
-            return requests
+        Models: `flatpages.flatpages`
+    Templates: Uses the template defined by the ``template_name`` field,
+        or :template:`flatpages/default.html` if template_name is not defined.
+    Context:
+        flatpage
+            `flatpages.flatpages` object
+    '''
+    if not url.startswith('/'):
+        url = '/' + url
+    site_id = get_current_site(request).id
+    try:
+        f = get_object_or_404(FlatPage, url=url, sites=site_id)
+    except Http404:
+        if not url.endswith('/') and settings.APPEND_SLASH:
+            url += '/'
+            f = get_object_or_404(FlatPage, url=url, sites=site_id)
+            return HttpResponsePermanentRedirect('%s/' % request.path)
+        else:
+            raise
+    return render_flatpage(request, f)
+    
+            print('Training %s ... ' % name, end='')
+        t0 = time()
+        clf.fit(X_train, y_train)
+        train_time[name] = time() - t0
+        t0 = time()
+        y_pred = clf.predict(X_test)
+        test_time[name] = time() - t0
+        accuracy[name] = accuracy_score(y_test, y_pred)
+        print('done')
+    
+            start = datetime.now()
+        ridge = linear_model.Ridge(alpha=1.)
+        ridge.fit(X, Y)
+        time_ridge[i] = total_seconds(datetime.now() - start)
+    
+        Parameters
+    ----------
+    metrics : array-like of callables (1d or 0d)
+        The metric functions to time.
+    
+    import matplotlib.pyplot as plt
+    
+        fn = os.path.relpath(fn,
+                         start=os.path.dirname(__import__(package).__file__))
+    try:
+        lineno = inspect.getsourcelines(obj)[1]
+    except Exception:
+        lineno = ''
+    return url_fmt.format(revision=revision, package=package,
+                          path=fn, lineno=lineno)
+    
+    from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import Perceptron
+from sklearn.pipeline import Pipeline
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
     
     
-class DownloadHandlers(object):
+def main(argv):
+    op = get_parser()
+    opts, args = op.parse_args(argv[1:])
+    root = Tk.Tk()
+    model = Model()
+    controller = Controller(model)
+    root.wm_title('Scikit-learn Libsvm GUI')
+    view = View(root, controller)
+    model.add_observer(view)
+    Tk.mainloop()
     
-            cookiejarkey = request.meta.get('cookiejar')
-        jar = self.jars[cookiejarkey]
-        cookies = self._get_request_cookies(jar, request)
-        for cookie in cookies:
-            jar.set_cookie_if_ok(cookie, request)
+    import numpy as np
+import matplotlib.pyplot as plt
+from time import time
+from sklearn import metrics
     
-        def _is_bzip2(self, response):
-        try:
-            body = bz2.decompress(response.body)
-        except IOError:
-            return
+    from sklearn.covariance import LedoitWolf, OAS
     
-        def __init__(self, headers):
-        self._headers = headers
+    print('The data was generated from (random_state=%d):' % RANDOM_SEED)
+print('Class', 'P(C)', 'P(w0|C)', 'P(w1|C)', sep='\t')
+for k, p, p_w in zip(['red', 'blue', 'yellow'], p_c, p_w_c.T):
+    print('%s\t%0.2f\t%0.2f\t%0.2f' % (k, p, p_w[0], p_w[1]))
+
     
-        def __init__(self, timeout=180):
-        self._timeout = timeout
+        # Loop over each face found in the frame to see if it's someone we know.
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
+        name = '<Unknown Person>'
     
-        def process_request(self, request, spider):
-        auth = getattr(self, 'auth', None)
-        if auth and b'Authorization' not in request.headers:
-            request.headers[b'Authorization'] = auth
+    # Open the input movie file
+input_movie = cv2.VideoCapture('hamilton_clip.mp4')
+length = int(input_movie.get(cv2.CAP_PROP_FRAME_COUNT))
+    
+        # Loop through each face in this frame of video
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+    
+    
+def detect_faces_in_image(file_stream):
+    # Pre-calculated face encoding of Obama generated with face_recognition.face_encodings(img)
+    known_face_encoding = [-0.09634063,  0.12095481, -0.00436332, -0.07643753,  0.0080383,
+                            0.01902981, -0.07184699, -0.09383309,  0.18518871, -0.09588896,
+                            0.23951106,  0.0986533 , -0.22114635, -0.1363683 ,  0.04405268,
+                            0.11574756, -0.19899382, -0.09597053, -0.11969153, -0.12277931,
+                            0.03416885, -0.00267565,  0.09203379,  0.04713435, -0.12731361,
+                           -0.35371891, -0.0503444 , -0.17841317, -0.00310897, -0.09844551,
+                           -0.06910533, -0.00503746, -0.18466514, -0.09851682,  0.02903969,
+                           -0.02174894,  0.02261871,  0.0032102 ,  0.20312519,  0.02999607,
+                           -0.11646006,  0.09432904,  0.02774341,  0.22102901,  0.26725179,
+                            0.06896867, -0.00490024, -0.09441824,  0.11115381, -0.22592428,
+                            0.06230862,  0.16559327,  0.06232892,  0.03458837,  0.09459756,
+                           -0.18777156,  0.00654241,  0.08582542, -0.13578284,  0.0150229 ,
+                            0.00670836, -0.08195844, -0.04346499,  0.03347827,  0.20310158,
+                            0.09987706, -0.12370517, -0.06683611,  0.12704916, -0.02160804,
+                            0.00984683,  0.00766284, -0.18980607, -0.19641446, -0.22800779,
+                            0.09010898,  0.39178532,  0.18818057, -0.20875394,  0.03097027,
+                           -0.21300618,  0.02532415,  0.07938635,  0.01000703, -0.07719778,
+                           -0.12651891, -0.04318593,  0.06219772,  0.09163868,  0.05039065,
+                           -0.04922386,  0.21839413, -0.02394437,  0.06173781,  0.0292527 ,
+                            0.06160797, -0.15553983, -0.02440624, -0.17509389, -0.0630486 ,
+                            0.01428208, -0.03637431,  0.03971229,  0.13983178, -0.23006812,
+                            0.04999552,  0.0108454 , -0.03970895,  0.02501768,  0.08157793,
+                           -0.03224047, -0.04502571,  0.0556995 , -0.24374914,  0.25514284,
+                            0.24795187,  0.04060191,  0.17597422,  0.07966681,  0.01920104,
+                           -0.01194376, -0.02300822, -0.17204897, -0.0596558 ,  0.05307484,
+                            0.07417042,  0.07126575,  0.00209804]
+    
+    Usage:
+    
+        # macOS will crash due to a bug in libdispatch if you don't use 'forkserver'
+    context = multiprocessing
+    if 'forkserver' in multiprocessing.get_all_start_methods():
+        context = multiprocessing.get_context('forkserver')
+    
+    print('I found {} face(s) in this photograph.'.format(len(face_landmarks_list)))
+    
+        # For a definition of each point index, see https://cdn-images-1.medium.com/max/1600/1*AbEg31EgkbXSQehuNJBlWg.png
+    if model == 'large':
+        return [{
+            'chin': points[0:17],
+            'left_eyebrow': points[17:22],
+            'right_eyebrow': points[22:27],
+            'nose_bridge': points[27:31],
+            'nose_tip': points[31:36],
+            'left_eye': points[36:42],
+            'right_eye': points[42:48],
+            'top_lip': points[48:55] + [points[64]] + [points[63]] + [points[62]] + [points[61]] + [points[60]],
+            'bottom_lip': points[54:60] + [points[48]] + [points[60]] + [points[67]] + [points[66]] + [points[65]] + [points[64]]
+        } for points in landmarks_as_tuples]
+    elif model == 'small':
+        return [{
+            'nose_tip': [points[4]],
+            'left_eye': points[2:4],
+            'right_eye': points[0:2],
+        } for points in landmarks_as_tuples]
+    else:
+        raise ValueError('Invalid landmarks model type. Supported models are ['small', 'large'].')
+    
+        # Invert the detected boxes
+    boxes_inv = box_utils.aspect_ratio(boxes_ar, 1.0 / aspect_ratio)
+    
+    
+def _create_cell_anchors():
+    '''
+    Generate all types of anchors for all fpn levels/scales/aspect ratios.
+    This function is called only once at the beginning of inference.
+    '''
+    k_max, k_min = cfg.FPN.RPN_MAX_LEVEL, cfg.FPN.RPN_MIN_LEVEL
+    scales_per_octave = cfg.RETINANET.SCALES_PER_OCTAVE
+    aspect_ratios = cfg.RETINANET.ASPECT_RATIOS
+    anchor_scale = cfg.RETINANET.ANCHOR_SCALE
+    A = scales_per_octave * len(aspect_ratios)
+    anchors = {}
+    for lvl in range(k_min, k_max + 1):
+        # create cell anchors array
+        stride = 2. ** lvl
+        cell_anchors = np.zeros((A, 4))
+        a = 0
+        for octave in range(scales_per_octave):
+            octave_scale = 2 ** (octave / float(scales_per_octave))
+            for aspect in aspect_ratios:
+                anchor_sizes = (stride * octave_scale * anchor_scale, )
+                anchor_aspect_ratios = (aspect, )
+                cell_anchors[a, :] = generate_anchors(
+                    stride=stride, sizes=anchor_sizes,
+                    aspect_ratios=anchor_aspect_ratios)
+                a += 1
+        anchors[lvl] = cell_anchors
+    return anchors
+    
+    
+def _whctrs(anchor):
+    '''Return width, height, x center, and y center for an anchor (window).'''
+    w = anchor[2] - anchor[0] + 1
+    h = anchor[3] - anchor[1] + 1
+    x_ctr = anchor[0] + 0.5 * (w - 1)
+    y_ctr = anchor[1] + 0.5 * (h - 1)
+    return w, h, x_ctr, y_ctr
+    
+    from detectron.core.config import cfg
+from detectron.utils.c2 import const_fill
+from detectron.utils.c2 import gauss_fill
+import detectron.modeling.ResNet as ResNet
+import detectron.utils.blob as blob_utils
+    
+        def __init__(self, anchors, spatial_scale, train):
+        self._anchors = anchors
+        self._num_anchors = self._anchors.shape[0]
+        self._feat_stride = 1. / spatial_scale
+        self._train = train
+    
+        if len(data.shape) == 1:
+        ret = np.empty((count, ), dtype=data.dtype)
+        ret.fill(fill)
+        ret[inds] = data
+    else:
+        ret = np.empty((count, ) + data.shape[1:], dtype=data.dtype)
+        ret.fill(fill)
+        ret[inds, :] = data
+    return ret
+    
+        blobs['retnet_fg_num'], blobs['retnet_bg_num'] = 0.0, 0.0
+    for im_i, entry in enumerate(roidb):
+        scale = im_scales[im_i]
+        im_height = np.round(entry['height'] * scale)
+        im_width = np.round(entry['width'] * scale)
+        gt_inds = np.where(
+            (entry['gt_classes'] > 0) & (entry['is_crowd'] == 0))[0]
+        assert len(gt_inds) > 0, \
+            'Empty ground truth empty for image is not allowed. Please check.'
+    
+    from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+    
+            (Initialized proxies ALWAYS return a value, either a set value
+        or an empty string)
+    
+        @validate(buttonimage = VInt('i', 0, 14),
+              title = nop('title'),
+              url = VSanitizedUrl('url'),
+              newwindow = VBoolean('newwindow', default = False),
+              styled = VBoolean('styled', default=True))
+    def GET_button_lite(self, buttonimage, title, url, styled, newwindow):
+        c.user = UnloggedUser([c.lang])
+        c.user_is_loggedin = False
+        c.render_style = 'js'
+    
+    from r2.controllers.reddit_base import MinimalController
+from r2.lib.pages import (
+    GoogleTagManagerJail,
+    GoogleTagManager,
+)
+from r2.lib.validator import (
+    validate,
+    VGTMContainerId,
+)
