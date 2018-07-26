c612172eacc9046904b3070cab2ab34041e93691
SUNsung <weatherius@gmail.com>
2018-07-26 16:29:05 +0300

    Rewrite [11qgYwUbyfJ.w11m9XAkgb2e.Q11xDJTtmlAVNw11jfo.rb] File in: 2018-07-26 16:29:04

--- ruby/11qgYwUbyfJ.w11m9XAkgb2e.Q11xDJTtmlAVNw11jfo.rb
+++ ruby/11qgYwUbyfJ.w11m9XAkgb2e.Q11xDJTtmlAVNw11jfo.rb
@@ -1,131 +1,63 @@
-          # Verify the box exists that we want to repackage
-          box = @env.boxes.find(box_name, box_provider, '= #{box_version}')
-          if !box
-            raise Vagrant::Errors::BoxNotFoundWithProviderAndVersion,
-              name: box_name,
-              provider: box_provider.to_s,
-              version: box_version
-          end
+
+        
+        require 'pry'
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+require 'jekyll'
     
-    module VagrantPlugins
-  module CommandCap
-    class Command < Vagrant.plugin('2', :command)
-      def self.synopsis
-        'checks and executes capability'
-      end
+    STDOUT.sync = true
     
-        it 'raises TypeError exception if buffer is not String' do
-      lambda { [65].pack('ccc', buffer: []) }.should raise_error(
-        TypeError, 'buffer must be String, not Array')
+        # Initialize a new Layout.
+    #
+    # site - The Site.
+    # base - The String path to the source.
+    # name - The String filename of the post file.
+    def initialize(site, base, name)
+      @site = site
+      @base = base
+      @name = name
+    
     end
+
     
-        def initialize(message = nil)
-      super
-      @ivar = 1
-    end
     
-      def self.block_device
-    raise 'Could not find a block device' unless @block
-    yield @block
-  end
+    {
+    {        '[#{elapsed_time}] #{severity} -- #{progname}: #{message}\n'
+      }
+    }
+    @dry_run_results = {
+      events: [],
+    }
     
-    describe 'File.pipe?' do
-  it 'returns false if file does not exist' do
-    File.pipe?('I_am_a_bogus_file').should == false
-  end
-    
-    describe 'String#downcase!' do
-  it 'modifies self in place' do
-    a = 'HeLlO'
-    a.downcase!.should equal(a)
-    a.should == 'hello'
-  end
-    
-        it 'returns nil if the class has no superclass' do
-      @s.rb_class_superclass(BasicObject).should be_nil
-    end
-  end
-    
-      class SubSub < Sub
-    def call_super_method
-      :subsubclass_method
-    end
-  end
-    
-      def apply
-    dir = Pathname.pwd
-    resource.unpack do
-      patch_dir = Pathname.pwd
-      if patch_files.empty?
-        children = patch_dir.children
-        if children.length != 1 || !children.first.file?
-          raise MissingApplyError, <<~EOS
-            There should be exactly one patch file in the staging directory unless
-            the 'apply' method was used one or more times in the patch-do block.
-          EOS
-        end
-        patch_files << children.first.basename
-      end
-      dir.cd do
-        patch_files.each do |patch_file|
-          ohai 'Applying #{patch_file}'
-          patch_file = patch_dir/patch_file
-          safe_system 'patch', '-g', '0', '-f', '-#{strip}', '-i', patch_file
-        end
+        if options['recipients'].present?
+      emails = options['recipients']
+      emails = [emails] if emails.is_a?(String)
+      unless emails.all? { |email| email =~ Devise.email_regexp || email =~ /\{/ }
+        errors.add(:base, ''when provided, 'recipients' should be an email address or an array of email addresses')
       end
     end
   end
+    }
     
-      def load_logs(dir)
-    logs = {}
-    if dir.exist?
-      dir.children.sort.each do |file|
-        contents = file.size? ? file.read : 'empty log'
-        # small enough to avoid GitHub 'unicorn' page-load-timeout errors
-        max_file_size = 1_000_000
-        contents = truncate_text_to_approximate_size(contents, max_file_size, front_weight: 0.2)
-        logs[file.basename.to_s] = { content: contents }
-      end
+      def validate_evernote_options
+    unless evernote_consumer_key.present? &&
+      evernote_consumer_secret.present? &&
+      evernote_oauth_token.present?
+      errors.add(:base, 'Evernote ENV variables and a Service are required')
     end
-    raise 'No logs.' if logs.empty?
-    logs
   end
     
-        def parse_symbol_spec(spec, tags)
-      case spec
-      when :clt
-        odisabled ''depends_on :clt''
-      when :tex
-        odisabled ''depends_on :tex''
-      when :libltdl
-        output_disabled(spec, 'libtool')
-      when :apr
-        output_disabled(spec, 'apr-util')
-      when :fortran
-        output_disabled(spec, 'gcc')
-      when :gpg
-        output_disabled(spec, 'gnupg')
-      when :hg
-        output_disabled(spec, 'mercurial')
-      when :mpi
-        output_disabled(spec, 'open-mpi')
-      when :python, :python2
-        output_disabled(spec, 'python@2')
-      when :python3
-        output_disabled(spec, 'python')
-      when :ant, :autoconf, :automake, :bsdmake, :cairo, :emacs, :expat,
-           :fontconfig, :freetype, :libtool, :libpng, :mysql, :perl, :pixman,
-           :postgresql, :rbenv, :ruby
-        output_disabled(spec)
-      else
-        super
-      end
-    end
+      def load_event
+    @event = current_user.events.find(params[:id])
+  end
+end
+
     
-      context 'with default reaction' do
-    before(:each) do
-      mock_app do
-        use Rack::Protection::CookieTossing
-        run DummyApp
-      end
-    end
+      #forward some requests to status message, because a poll is just attached to a status message and is not sharable itself
+  delegate :author_id, :diaspora_handle, :public?, :subscribers, to: :status_message
+    
+          respond_with do |format|
+        format.html do
+          gon.preloads[:pods] = pods_json
+          gon.unchecked_count = Pod.unchecked.count
+          gon.version_failed_count = Pod.version_failed.count
+          gon.error_count = Pod.check_failed.count
