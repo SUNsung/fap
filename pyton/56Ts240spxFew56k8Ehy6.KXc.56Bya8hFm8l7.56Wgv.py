3622511af84b0d68bd2335a70f12491f6094461e
SUNsung <weatherius@gmail.com>
2018-07-26 20:02:00 +0300

    Rewrite [56Ts240spxFew56k8Ehy6.KXc.56Bya8hFm8l7.56Wgv.py] File in: 2018-07-26 20:02:00

--- pyton/56Ts240spxFew56k8Ehy6.KXc.56Bya8hFm8l7.56Wgv.py
+++ pyton/56Ts240spxFew56k8Ehy6.KXc.56Bya8hFm8l7.56Wgv.py
@@ -1,73 +1,92 @@
 
         
         py_test(
-    name = 'runtime_support_test',
-    srcs = ['runtime_support_test.py'],
+    name = 'visualization_test',
+    srcs = ['visualization_test.py'],
     deps = [
-        ':network_units',
-        ':runtime_support',
-        '//dragnn/protos:export_pb2_py',
+        ':visualization',
         '//dragnn/protos:spec_pb2_py',
+        '//dragnn/protos:trace_pb2_py',
         '@org_tensorflow//tensorflow:tensorflow_py',
     ],
 )
     
-        # As well as annotation:
-    self.setUp()
-    comp = bulk_component.BulkAnnotatorComponentBuilder(self.master,
-                                                        component_spec)
+        This loop repeatedly evaluates the network and computes the loss, but it
+    does not advance using the predictions of the network. Instead, it advances
+    using the oracle defined in the underlying transition system. The final
+    state will always correspond to the gold annotation.
     
-          # Cost = -2 + ln(1 + exp(2) + exp(-1))
-      #        -1 + ln(exp(3) + exp(1) + exp(-2))
-      self.assertAlmostEqual(cost, 2.3027, 4)
-      self.assertEqual(correct, 1)
-      self.assertEqual(total, 2)
+            with tf.control_dependencies([increment_target_step]):
+          handle = tf.identity(handle)
     
     
-def main(unused_argv):
-  # Run the exporter.
-  export(FLAGS.master_spec, FLAGS.params_path, FLAGS.export_path,
-         FLAGS.export_moving_averages, FLAGS.build_runtime_graph)
-  tf.logging.info('Export complete.')
+@tf.RegisterGradient('MaximumSpanningTree')
+def maximum_spanning_tree_gradient(mst_op, d_loss_d_max_scores, *_):
+  '''Returns a subgradient of the MaximumSpanningTree op.
     
-        cmd = cmd.split()
     
-    from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
+@for_app('tar')
+def match(command):
+    return ('-C' not in command.script
+            and _is_tar_extract(command.script)
+            and _tar_file(command.script_parts) is not None)
     
-    mu_second = 0.0 + 10 ** 6  # number of microseconds in a second
     
-        # the training data folder must be passed as first argument
-    movie_reviews_data_folder = sys.argv[1]
-    dataset = load_files(movie_reviews_data_folder, shuffle=False)
-    print('n_samples: %d' % len(dataset.data))
+@keras_test
+def test_sequential_class_weights():
+    model = create_sequential_model()
+    model.compile(loss=loss, optimizer='rmsprop')
     
+    
+@keras_test
+def test_global_maxpooling2d_legacy_interface():
+    old_layer = keras.layers.GlobalMaxPooling2D(dim_ordering='tf', name='global_maxpool2d')
+    new_layer = keras.layers.GlobalMaxPool2D(data_format='channels_last', name='global_maxpool2d')
+    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer.get_config())
+    
+        x = Input(shape=(1,))
+    y = inner_model(x)
+    outer_model = Model(x, y)
+    assert outer_model.trainable_weights == inner_model.trainable_weights
+    inner_model.trainable = False
+    assert outer_model.trainable_weights == []
+    inner_model.trainable = True
+    inner_model.layers[-1].trainable = False
+    assert outer_model.trainable_weights == []
+    
+        for i, yi in enumerate(grid_y):
+        for j, xi in enumerate(grid_x):
+            z_sample = np.array([[xi, yi]])
+            x_decoded = decoder.predict(z_sample)
+            digit = x_decoded[0].reshape(digit_size, digit_size)
+            figure[i * digit_size: (i + 1) * digit_size,
+                   j * digit_size: (j + 1) * digit_size] = digit
+    
+        # learn the alphabet with stacked LSTM
+    model = Sequential([
+        layers.LSTM(16, return_sequences=True, input_shape=(sequence_length, number_of_chars)),
+        layers.LSTM(16, return_sequences=False),
+        layers.Dense(number_of_chars, activation='softmax')
+    ])
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    model.fit(x, y, batch_size=1, epochs=60, verbose=1)
+    
+    for j in range(16):
+    new_pos = seq.predict(track[np.newaxis, ::, ::, ::, ::])
+    new = new_pos[::, -1, ::, ::, ::]
+    track = np.concatenate((track, new), axis=0)
+    
+        def download_delay(self):
+        if self.randomize_delay:
+            return random.uniform(0.5 * self.delay, 1.5 * self.delay)
+        return self.delay
+    
+            if request.method != 'GET':
+            # other HTTP methods are either not safe or don't have a body
+            return response
+    
+    class ScrapyDeprecationWarning(Warning):
+    '''Warning category for deprecated features, since the default
+    DeprecationWarning is silenced on Python 2.7+
     '''
-=========================================================
-The Digit Dataset
-=========================================================
-    
-    class Bigthink(VideoExtractor):
-    name = 'Bigthink'
-    
-    from ..common import *
-    
-    def kuwo_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    if 'www.kuwo.cn/yinyue' in url:
-        rid=match1(url,'yinyue/(\d+)')
-        kuwo_download_by_rid(rid,output_dir, merge, info_only)
-    else:
-        kuwo_playlist_download(url,output_dir,merge,info_only)
-    
-    from ..common import *
-    
-            for s in self.stream_types:
-            quality = s['id']
-            src = match1(content,
-                         r'src: '([^']+)', 'data-res': '%s'' % quality)
-            if src is not None:
-                url = 'http://en.musicplayon.com%s' % src
-                self.streams[quality] = {'url': url}
+    pass
