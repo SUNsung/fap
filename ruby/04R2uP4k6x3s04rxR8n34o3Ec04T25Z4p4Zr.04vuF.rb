1e4fdbe751584470866785eae1c320cbd023f7cd
SUNsung <weatherius@gmail.com>
2018-07-27 21:55:21 +0300

    Rewrite [04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb] File in: 2018-07-27 21:55:21

--- ruby/04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb
+++ ruby/04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb
@@ -1,16 +1,76 @@
 
         
-            # The path used after sending unlock password instructions
-    def after_sending_unlock_instructions_path_for(resource)
-      new_session_path(resource) if is_navigational_format?
+        class ApplicationSerializer < ActiveModel::Serializer
+  embed :ids, include: true
+    
+      describe '#post' do
+    it 'posts a status message to facebook' do
+      stub_request(:post, 'https://graph.facebook.com/me/feed').
+          to_return(:status => 200, :body => '{'id': '12345'}', :headers => {})
+      @service.post(@post)
     end
     
-          rescue_from OpenIDConnect::ValidationFailed,
-                  ActiveRecord::RecordInvalid, Api::OpenidConnect::Error::InvalidSectorIdentifierUri do |e|
-        validation_fail_as_json(e)
+      def create_hash(model, opts={})
+    opts[:range] ||= 1.day
+    plural = model.to_s.underscore.pluralize
+    eval(<<DATA
+      @#{plural} = {
+        :day_before => #{model}.where(:created_at => ((Time.now.midnight - #{opts[:range]*2})..Time.now.midnight - #{opts[:range]})).count,
+        :yesterday => #{model}.where(:created_at => ((Time.now.midnight - #{opts[:range]})..Time.now.midnight)).count
+      }
+      @#{plural}[:change] = percent_change(@#{plural}[:yesterday], @#{plural}[:day_before])
+DATA
+    )
+  end
+    
+          def process_authorization_consent(approved_string)
+        endpoint = Api::OpenidConnect::AuthorizationPoint::EndpointConfirmationPoint.new(
+          current_user, to_boolean(approved_string))
+        handle_confirmation_endpoint_response(endpoint)
       end
     
-        alias_method :load_debug, :load
+        def initialize(tag_name, markup, tokens)
+      @by = nil
+      @source = nil
+      @title = nil
+      if markup =~ FullCiteWithTitle
+        @by = $1
+        @source = $2 + $3
+        @title = $4.titlecase.strip
+      elsif markup =~ FullCite
+        @by = $1
+        @source = $2 + $3
+      elsif markup =~ AuthorTitle
+        @by = $1
+        @title = $2.titlecase.strip
+      elsif markup =~ Author
+        @by = $1
+      end
+      super
+    end
     
-        puts 'Downloading #{url}'
-    actual_sha1 = download(url, output)
+          Dir.chdir(code_path) do
+        code = file.read
+        @filetype = file.extname.sub('.','') if @filetype.nil?
+        title = @title ? '#{@title} (#{file.basename})' : file.basename
+        url = '/#{code_dir}/#{@file}'
+        source = '<figure class='code'><figcaption><span>#{title}</span> <a href='#{url}'>download</a></figcaption>\n'
+        source += '#{HighlightCode::highlight(code, @filetype)}</figure>'
+        TemplateWrapper::safe_wrap(source)
+      end
+    end
+  end
+    
+      class RenderPartialTag < Liquid::Tag
+    include OctopressFilters
+    def initialize(tag_name, markup, tokens)
+      @file = nil
+      @raw = false
+      if markup =~ /^(\S+)\s?(\w+)?/
+        @file = $1.strip
+        @raw = $2 == 'raw'
+      end
+      super
+    end
+    
+    module Jekyll
