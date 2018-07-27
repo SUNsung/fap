4bddd49c269a92ab52e4abfa24ba5ce8c966d795
SUNsung <weatherius@gmail.com>
2018-07-27 23:44:16 +0300

    Rewrite [15Scu0k12vo5g15umZVD20pNhI15irDkSzP2OQg158fR.rb] File in: 2018-07-27 23:44:16

--- ruby/15Scu0k12vo5g15umZVD20pNhI15irDkSzP2OQg158fR.rb
+++ ruby/15Scu0k12vo5g15umZVD20pNhI15irDkSzP2OQg158fR.rb
@@ -1,38 +1,124 @@
 
         
-            def execute(args)
+          url = File.dirname(url)
+  url == FORWARD_SLASH ? url : '#{url}/'
+end
     
-      def revoke_admin!
-    set_permission('admin', false)
+    def site_configuration(overrides = {})
+  build_configs({
+    'source'      => source_dir,
+    'destination' => dest_dir
+  }, build_configs(overrides))
+end
+    
+    class Devise::ConfirmationsController < DeviseController
+  # GET /resource/confirmation/new
+  def new
+    self.resource = resource_class.new
   end
     
-    class ApplicationSerializer < ActiveModel::Serializer
-  embed :ids, include: true
+      def serialize_options(resource)
+    methods = resource_class.authentication_keys.dup
+    methods = methods.keys if methods.is_a?(Hash)
+    methods << :password if resource.respond_to?(:password)
+    { methods: methods, only: [:password] }
+  end
     
-      # Update version.rb file with BOOTSTRAP_SHA
-  def store_version
-    path    = 'lib/bootstrap-sass/version.rb'
-    content = File.read(path).sub(/BOOTSTRAP_SHA\s*=\s*[''][\w]+['']/, 'BOOTSTRAP_SHA = '#@branch_sha'')
-    File.open(path, 'w') { |f| f.write(content) }
+        def password_change(record, opts={})
+      devise_mail(record, :password_change, opts)
+    end
   end
 end
 
     
-      # Set to :debug to see everything in the log.
-  config.log_level = :info
+      if record && record.respond_to?(:timedout?) && warden.authenticated?(scope) &&
+     options[:store] != false && !env['devise.skip_timeoutable']
+    last_request_at = warden.session(scope)['last_request_at']
     
-      # The test environment is used exclusively to run your application's
-  # test suite. You never need to work with it otherwise. Remember that
-  # your test database is 'scratch space' for the test suite and is wiped
-  # and recreated between test runs. Don't rely on the data there!
-  config.cache_classes = true
+          def headers_for(action, opts)
+        headers = {
+          subject: subject_for(action),
+          to: resource.email,
+          from: mailer_sender(devise_mapping),
+          reply_to: mailer_reply_to(devise_mapping),
+          template_path: template_paths,
+          template_name: action
+        }.merge(opts)
     
-    require 'rake/testtask'
-Rake::TestTask.new do |t|
-  t.libs << 'test'
-  t.test_files = FileList['test/**/*_test.rb']
-  t.verbose = true
+    puts '\nUnable to find an RSS feed for the following blogs:'
+puts '==================================================='
+unavailable.each do |b|
+  puts '#{b.name} | #{b.web_url}'
 end
+puts '==================================================='
+
     
-        def index
-      pods_json = PodPresenter.as_collection(Pod.all)
+          def plugin_gem_names
+        (Gem.loaded_specs.keys - ['capistrano']).grep(/capistrano/).sort
+      end
+    end
+  end
+end
+
+    
+      # Implemented by subclasses to define Rake tasks. Typically a plugin will call
+  # `eval_rakefile` to load Rake tasks from a separate .rake file.
+  #
+  # Example:
+  #
+  #   def define_tasks
+  #     eval_rakefile File.expand_path('../tasks.rake', __FILE__)
+  #   end
+  #
+  # For simple tasks, you can define them inline. No need for a separate file.
+  #
+  #   def define_tasks
+  #     desc 'Do something fantastic.'
+  #     task 'my_plugin:fantastic' do
+  #       ...
+  #     end
+  #   end
+  #
+  def define_tasks; end
+    
+        # @abstract
+    #
+    # Create a (new) clone of the remote-repository on the deployment target
+    #
+    # @return void
+    #
+    def clone
+      raise NotImplementedError, 'Your SCM strategy module should provide a #clone method'
+    end
+    
+        describe 'when passed server literal names' do
+      before do
+        ENV.delete 'ROLES'
+        ENV.delete 'HOSTS'
+        @coordinator = mock('coordinator')
+        @coordinator.expects(:each).returns(nil)
+      end
+    
+      def command_line(*options)
+    options.each { |opt| ARGV << opt }
+    subject.define_singleton_method(:exit) do |*_args|
+      throw(:system_exit, :exit)
+    end
+    subject.run
+    subject.options
+  end
+    
+            def on_array(node)
+          process(node, '%i', '%I', '%w', '%W')
+        end
+    
+              add_offense(node)
+        end
+    
+        context 'when first child is NOT a method' do
+      it 'does not require blank line at the beginning of #{type} body '\
+        'but requires blank line before first def definition '\
+        'and requires blank line at the end of #{type} body' do
+        inspect_source(<<-RUBY.strip_indent)
+          #{type} SomeObject
+            include Something
