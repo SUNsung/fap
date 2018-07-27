a9851262b1287f00974c5d5782984c822e847e31
SUNsung <weatherius@gmail.com>
2018-07-27 21:02:25 +0300

    Rewrite [572PnbIzUn.Ek57MW82AAbGao57yg4zL613sPI57Cmx.py] File in: 2018-07-27 21:02:25

--- pyton/572PnbIzUn.Ek57MW82AAbGao57yg4zL613sPI57Cmx.py
+++ pyton/572PnbIzUn.Ek57MW82AAbGao57yg4zL613sPI57Cmx.py
@@ -1,173 +1,105 @@
 
         
-            params = {
-        'age_limit': age,
-        'skip_download': True,
-        'writeinfojson': True,
-        'outtmpl': '%(id)s.%(ext)s',
-    }
-    ydl = YoutubeDL(params)
-    ydl.add_default_info_extractors()
-    json_filename = os.path.splitext(filename)[0] + '.info.json'
-    try_rm(json_filename)
-    ydl.download([url])
-    res = os.path.exists(json_filename)
-    try_rm(json_filename)
-    return res
-    
-    
-class RtspFD(FileDownloader):
-    def real_download(self, filename, info_dict):
-        url = info_dict['url']
-        self.report_destination(filename)
-        tmpfilename = self.temp_name(filename)
-    
-            webpage = self._download_webpage(url, video_id)
-        key = self._search_regex(
-            r'src=['\']https?://[^/]+/embed/([A-Za-z0-9_-]+)', webpage, 'key')
-    
-            return {
-            'id': info['vid'],
-            'title': info['Subject'],
-            'duration': int(info['duration']) / 1000.0,
-            'formats': formats,
-            'thumbnail': info.get('bimg') or info.get('img'),
-        }
+        
+def side_effect(old_cmd, command):
+    with tarfile.TarFile(_tar_file(old_cmd.script_parts)[0]) as archive:
+        for file in archive.getnames():
+            try:
+                os.remove(file)
+            except OSError:
+                # does not try to remove directories as we cannot know if they
+                # already existed before
+                pass
 
     
-            title = metadata['Title']
-        description = metadata.get('Description')
-        duration = float_or_none(metadata.get('Duration'))
-        timestamp = parse_iso8601(metadata.get('DateCreated'))
     
-        strings = []
-    with open(filepath, 'r') as f:
+@pytest.fixture(params=containers)
+def proc(request, spawnu, TIMEOUT):
+    proc = spawnu(*request.param)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(u'thefuck --alias > ~/.config/fish/config.fish')
+    proc.sendline(u'fish')
+    return proc
     
-        test_suite = 'tests',
-    
-        # Get options and arguments.
+        plugin_manager.register(Plugin)
     try:
-        opts, args = getopt.getopt(sys.argv[1:], _short_options, _options)
-    except getopt.GetoptError as e:
-        log.wtf('''
-    [Fatal] {}.
-    Try '{} --help' for more options.'''.format(e, script_name))
-    
-    __all__ = ['cbs_download']
-    
-    def kugou_download_playlist(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    html=get_html(url)
-    pattern=re.compile('title='(.*?)'.* data='(\w*)\|.*?'')
-    pairs=pattern.findall(html)
-    for title,hash_val in pairs:
-        kugou_download_by_hash(title,hash_val,output_dir,merge,info_only)
-    
-    def kuwo_download_by_rid(rid, output_dir = '.', merge = True, info_only = False):
-    html=get_content('http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_%s'%rid)
-    title=match1(html,r'<name>(.*)</name>')
-    #to get title
-    #format =aac|mp3 ->to get aac format=mp3 ->to get mp3
-    url=get_content('http://antiserver.kuwo.cn/anti.s?format=mp3&rid=MUSIC_%s&type=convert_url&response=url'%rid)
-    songtype, ext, size = url_info(url)
-    print_info(site_info, title, songtype, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir)
-    
-            size_full = 0
-        for url in url_list:
-            type_, ext, size = url_info(url, headers=fake_headers)
-            size_full += size
-    
-        # For pickling to work, the __module__ variable needs to be set to the frame
-    # where the named tuple is created.  Bypass this step in enviroments where
-    # sys._getframe is not defined (Jython for example).
-    if hasattr(_sys, '_getframe'):
-        result.__module__ = _sys._getframe(1).f_globals.get('__name__', '__main__')
-    
-    def with_thread_pool_executor():
-    with ThreadPoolExecutor(10) as executor:
-        return list(executor.map(is_prime, PRIMES))
-    
-        def test_repr(self):
-        self.assertRegexpMatches(repr(PENDING_FUTURE),
-                                 '<Future at 0x[0-9a-f]+ state=pending>')
-        self.assertRegexpMatches(repr(RUNNING_FUTURE),
-                                 '<Future at 0x[0-9a-f]+ state=running>')
-        self.assertRegexpMatches(repr(CANCELLED_FUTURE),
-                                 '<Future at 0x[0-9a-f]+ state=cancelled>')
-        self.assertRegexpMatches(repr(CANCELLED_AND_NOTIFIED_FUTURE),
-                                 '<Future at 0x[0-9a-f]+ state=cancelled>')
-        self.assertRegexpMatches(
-                repr(EXCEPTION_FUTURE),
-                '<Future at 0x[0-9a-f]+ state=finished raised IOError>')
-        self.assertRegexpMatches(
-                repr(SUCCESSFUL_FUTURE),
-                '<Future at 0x[0-9a-f]+ state=finished returned int>')
+        r = http(
+            httpbin + BASIC_AUTH_URL,
+            '--auth-type',
+            Plugin.auth_type,
+        )
+        assert HTTP_OK in r
+        assert r.json == AUTH_OK
+    finally:
+        plugin_manager.unregister(Plugin)
     
     
-def ExtractKeywordsFromGroup_KeywordMiddle_test():
-  assert_that( syntax_parse._ExtractKeywordsFromGroup(
-                 syntax_parse.SyntaxGroup( '', [
-                   'foo contained bar',
-                   'zoo goo'
-                 ] ) ),
-               contains_inanyorder( 'foo', 'contained', 'bar', 'zoo', 'goo' ) )
+def has_docutils():
+    try:
+        # noinspection PyUnresolvedReferences
+        import docutils
+        return True
+    except ImportError:
+        return False
+    
+        See <https://github.com/httpie/httpie-ntlm> for an example auth plugin.
+    
+            with open(self.path, 'w') as f:
+            json.dump(self, f, indent=4, sort_keys=True, ensure_ascii=True)
+            f.write('\n')
     
     
-  def Just_Detailed_Info_test( self ):
-    self._Check( 9999999999, {
-      'insertion_text':  'INSERTION TEXT',
-      'menu_text':       'MENU TEXT',
-      'extra_menu_info': 'EXTRA MENU INFO',
-      'kind':            'K',
-      'detailed_info':   'DETAILED INFO',
-    }, {
-      'word'     : 'INSERTION TEXT',
-      'abbr'     : 'MENU TEXT',
-      'menu'     : 'EXTRA MENU INFO',
-      'kind'     : 'k',
-      'info'     : 'DETAILED INFO',
-      'dup'      : 1,
-      'empty'    : 1,
-      'user_data': '9999999999',
-    } )
+class TestVerboseFlag:
+    def test_verbose(self, httpbin):
+        r = http('--verbose',
+                 'GET', httpbin.url + '/get', 'test-header:__test__')
+        assert HTTP_OK in r
+        assert r.count('__test__') == 2
     
-    from ycm.client.base_request import BaseRequest, BuildRequestData
+            if args.job_metadata:
+            path = os.path.join(output_dir, '%s/job.json' % job_number)
+            contents = json.dumps(j, sort_keys=True, indent=4)
+    
+        return results
+
+    
+        def construct_yaml_map(self, node):
+        data = AnsibleMapping()
+        yield data
+        value = self.construct_mapping(node)
+        data.update(value)
+        data.ansible_pos = self._node_position_info(node)
+    
+    
+class FullJitterBackoffStrategyTestCase(unittest.TestCase):
+    def test_no_retries(self):
+        strategy = _full_jitter_backoff(retries=0)
+        result = list(strategy())
+        self.assertEquals(result, [], 'list should be empty')
+    
+            '''
+        if self.installer is None:
+            logger.warning('No installer is specified, there isn't any '
+                           'configuration to enhance.')
+            raise errors.Error('No installer available')
+    
+    # Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+    
+    
+def ConvertVimDataToCompletionData( vim_data ):
+  # see :h complete-items for a description of the dictionary fields
+  completion_data = {}
     
     from ycm.client.base_request import BaseRequest, BuildRequestData
     
     
-def _HandlePollResponse( response, diagnostics_handler ):
-  if isinstance( response, list ):
-    for notification in response:
-      if 'message' in notification:
-        PostVimMessage( notification[ 'message' ],
-                        warning = False,
-                        truncate = True )
-      elif 'diagnostics' in notification:
-        diagnostics_handler.UpdateWithNewDiagnosticsForFile(
-          notification[ 'filepath' ],
-          notification[ 'diagnostics' ] )
-  elif response is False:
-    # Don't keep polling for this file
-    return False
-  # else any truthy response means 'nothing to see here; poll again in a
-  # while'
-    
-    
-  def MarkResponseHandled( self ):
-    self._handled_tick = self._parse_tick
-    
-                payload = {'host': 'http://host:8088/services/collector/event',
-                       'event': body}
-            self.handler_method(event)
-            self.assertEqual(self.mock_post.call_count, 1)
-            self.assertEqual(
-                self.mock_post.call_args,
-                mock.call(
-                    payload['host'], data=json.dumps(payload),
-                    headers={'Authorization': 'Splunk secret'},
-                    timeout=10
-                )
-            )
-            self.mock_post.reset_mock()
+# This class can be used to keep the ycmd server alive for the duration of the
+# life of the client. By default, ycmd shuts down if it doesn't see a request in
+# a while.
+class YcmdKeepalive( object ):
+  def __init__( self, ping_interval_seconds = 60 * 10 ):
+    self._keepalive_thread = Thread( target = self._ThreadMain )
+    self._keepalive_thread.daemon = True
+    self._ping_interval_seconds = ping_interval_seconds
