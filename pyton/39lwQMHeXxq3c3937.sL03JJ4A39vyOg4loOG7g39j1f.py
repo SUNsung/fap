754e5a9e13bfcb982ff3193e0b74bb5372855519
SUNsung <weatherius@gmail.com>
2018-07-27 07:22:11 +0300

    Rewrite [39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py] File in: 2018-07-27 07:22:11

--- pyton/39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py
+++ pyton/39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py
@@ -1,8 +1,142 @@
 
         
-            #: Setting this disables or force-enables the automatic options handling.
-    provide_automatic_options = None
+        signature = hexlify(rsa.pkcs1.sign(json.dumps(versions_info, sort_keys=True).encode('utf-8'), privkey, 'SHA-256')).decode()
+print('signature: ' + signature)
     
-        app.config.update(SERVER_NAME='localhost:80')
-    with app.test_request_context('/', environ_overrides={'SERVER_NAME': 'localhost:80'}):
+    for page in itertools.count(1):
+    releases = json.loads(compat_urllib_request.urlopen(
+        'https://api.github.com/repos/rg3/youtube-dl/releases?page=%s' % page
+    ).read().decode('utf-8'))
+    
+        def test_proxy_https(self):
+        params = self._check_params(['primary_proxy', 'primary_server_ip'])
+        if params is None:
+            return
+        ydl = FakeYDL({
+            'proxy': params['primary_proxy']
+        })
+        self.assertEqual(
+            ydl.urlopen('https://yt-dl.org/ip').read().decode('utf-8'),
+            params['primary_server_ip'])
+    
+        _ALL_CLASSES = [
+        klass
+        for name, klass in globals().items()
+        if name.endswith('IE') and name != 'GenericIE'
+    ]
+    _ALL_CLASSES.append(GenericIE)
+    
+    
+class AnySexIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)?anysex\.com/(?P<id>\d+)'
+    _TEST = {
+        'url': 'http://anysex.com/156592/',
+        'md5': '023e9fbb7f7987f5529a394c34ad3d3d',
+        'info_dict': {
+            'id': '156592',
+            'ext': 'mp4',
+            'title': 'Busty and sexy blondie in her bikini strips for you',
+            'description': 'md5:de9e418178e2931c10b62966474e1383',
+            'categories': ['Erotic'],
+            'duration': 270,
+            'age_limit': 18,
+        }
+    }
+    
+            return {
+            'id': info['vid'],
+            'title': info['Subject'],
+            'duration': int(info['duration']) / 1000.0,
+            'formats': formats,
+            'thumbnail': info.get('bimg') or info.get('img'),
+        }
+
+    
+        def reducer(self, key, values):
+        total = sum(values)
+        if total == 1:
+            yield key, total
+    
+        @abstractmethod
+    def escalate_call(self):
         pass
+    
+        def __init__(self, results):
+        self.results = results
+        self.next = next
+    
+        def can_fit_in_spot(self, spot):
+        return True
+    
+        def get_person(self, person_id):
+        person_server = self.lookup[person_id]
+        return person_server.people[person_id]
+    
+    # (c) 2015, Marc Abramowitz <marca@surveymonkey.com>
+#
+# This file is part of Ansible.
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible. If not, see <http://www.gnu.org/licenses/>.
+    
+            with open(path, 'w') as content_fd:
+            content_fd.write(content)
+    
+    
+class GCPUtilsTestCase(unittest.TestCase):
+    params_dict = {
+        'url_map_name': 'foo_url_map_name',
+        'description': 'foo_url_map description',
+        'host_rules': [
+            {
+                'description': 'host rules description',
+                'hosts': [
+                        'www.example.com',
+                        'www2.example.com'
+                ],
+                'path_matcher': 'host_rules_path_matcher'
+            }
+        ],
+        'path_matchers': [
+            {
+                'name': 'path_matcher_one',
+                'description': 'path matcher one',
+                'defaultService': 'bes-pathmatcher-one-default',
+                'pathRules': [
+                        {
+                            'service': 'my-one-bes',
+                            'paths': [
+                                '/',
+                                '/aboutus'
+                            ]
+                        }
+                ]
+            },
+            {
+                'name': 'path_matcher_two',
+                'description': 'path matcher two',
+                'defaultService': 'bes-pathmatcher-two-default',
+                'pathRules': [
+                        {
+                            'service': 'my-two-bes',
+                            'paths': [
+                                '/webapp',
+                                '/graphs'
+                            ]
+                        }
+                ]
+            }
+        ]
+    }
+    
+    import re
