6d723d715eec460d96ca2051894c20008ad4c593
SUNsung <weatherius@gmail.com>
2018-07-28 01:34:51 +0300

    Rewrite [5254VjhLSOw5231cQgWUpjc.52Xd6jqFjxhIA52P58.rb] File in: 2018-07-28 01:34:50

--- ruby/5254VjhLSOw5231cQgWUpjc.52Xd6jqFjxhIA52P58.rb
+++ ruby/5254VjhLSOw5231cQgWUpjc.52Xd6jqFjxhIA52P58.rb
@@ -1,181 +1,80 @@
 
         
-          included do
-    scope :admins, -> { where(admin: true) }
-    scope :moderators, -> { where(moderator: true) }
-    scope :staff, -> { where('moderator or admin ') }
+          it 'does not evaluate else-body if expression is true' do
+    a = []
+    if true
+      a << 123
+    else
+      a << 456
+    end
+    a.should == [123]
   end
     
-      protected
-    
-          def self.available_options
-        [
-          FastlaneCore::ConfigItem.new(key: :name,
-                                       env_name: 'KEYCHAIN_NAME',
-                                       description: 'Keychain name',
-                                       conflicting_options: [:path],
-                                       is_string: true,
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :path,
-                                       env_name: 'KEYCHAIN_PATH',
-                                       description: 'Path to keychain',
-                                       is_string: true,
-                                       conflicting_options: [:name],
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :password,
-                                       env_name: 'KEYCHAIN_PASSWORD',
-                                       description: 'Password for the keychain',
-                                       sensitive: true,
-                                       optional: false),
-          FastlaneCore::ConfigItem.new(key: :default_keychain,
-                                       description: 'Should the newly created Keychain be the new system default keychain',
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :unlock,
-                                       description: 'Unlock keychain after create',
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :timeout,
-                                       description: 'timeout interval in seconds. Set `false` if you want to specify 'no time-out'',
-                                       is_string: false,
-                                       default_value: 300),
-          FastlaneCore::ConfigItem.new(key: :lock_when_sleeps,
-                                       description: 'Lock keychain when the system sleeps',
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :lock_after_timeout,
-                                       description: 'Lock keychain after timeout interval',
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :add_to_search_list,
-                                       description: 'Add keychain to search list',
-                                       is_string: false,
-                                       default_value: true)
-        ]
-      end
-    
-            command = [escaped_gradle_path, 'tasks', '--console=plain'].join(' ')
-        output = Action.sh(command, print_command: false, print_command_output: false)
-        output.split('\n').each do |line|
-          if (result = line.match(/(\w+)\s\-\s([\w\s]+)/))
-            self.tasks << GradleTask.new(title: result[1], description: result[2])
-          end
-        end
-    
-      def test_capture3_flip
-    o, e, s = Open3.capture3(RUBY, '-e', 'STDOUT.sync=true; 1000.times { print 'o'*1000; STDERR.print 'e'*1000 }')
-    assert_equal('o'*1000000, o)
-    assert_equal('e'*1000000, e)
-    assert(s.success?)
+      describe 'DATA_PTR' do
+    it 'returns the struct data' do
+      a = @s.wrap_struct(1024)
+      @s.get_struct_data_ptr(a).should == 1024
+    end
   end
+end
+
     
-      it 'throws an exception for a wrong type' do
-    a = @s.typed_wrap_struct(1024)
-    lambda { @s.typed_get_struct_other(a) }.should raise_error(TypeError)
-  end
+      ##
+  # Adds +name+ with permissions +mode+ to the tar, yielding +io+ for writing
+  # the file.  The +digest_algorithm+ is written to a read-only +name+.sum
+  # file following the given file contents containing the digest name and
+  # hexdigest separated by a tab.
+  #
+  # The created digest object is returned.
     
-      # Equivalent to Set#flatten, but replaces the receiver with the
-  # result in place.  Returns nil if no modifications were made.
-  def flatten!
-    replace(flatten()) if any? { |e| e.is_a?(Set) }
-  end
-    
-    require_relative 'converter/fonts_conversion'
-require_relative 'converter/less_conversion'
-require_relative 'converter/js_conversion'
-require_relative 'converter/logger'
-require_relative 'converter/network'
-    
-      # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
-    
-      def test_font_helper_with_suffix_question
-    assert_match %r(url\(['']?/assets/.*eot\?.*['']?\)), @css
-  end
-    
-          def call(env)
-        status, headers, body = super
-        response = Rack::Response.new(body, status, headers)
-        request = Rack::Request.new(env)
-        remove_bad_cookies(request, response)
-        response.finish
-      end
-    
-      it 'accepts requests with a changing Accept-Encoding header' do
-    # this is tested because previously it led to clearing the session
-    session = {:foo => :bar}
-    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_ENCODING' => 'a'
-    get '/', {}, 'rack.session' => session, 'HTTP_ACCEPT_ENCODING' => 'b'
-    expect(session).not_to be_empty
-  end
-    
-          # Parsed representation of code annotated with the `^^^ Message` style
-      class AnnotatedSource
-        ANNOTATION_PATTERN = /\A\s*\^+ /
-    
-        def clear
-      @attachments = Hash.new { |h,k| h[k] = {} }
+      DIGEST_NAME = # :nodoc:
+    if DIGEST_ALGORITHM.method_defined? :name then
+      DIGEST_ALGORITHM.new.name
+    else
+      DIGEST_ALGORITHM.name[/::([^:]+)\z/, 1]
     end
     
-            if args.length > 0
-          attachment.to_s(args.first)
-        else
-          attachment
-        end
+        assert_predicate set2, :frozen?
+    assert_raise(FrozenError) {
+      set2.add 5
+    }
+  end
+end
+    
+      it 'ignores spaces between directives' do
+    '\x01\x02'.unpack('U U').should == [1, 2]
+  end
+end
+
+    
+          case entity
+      when DiasporaFederation::Entities::AccountDeletion
+        Diaspora::Federation::Receive.account_deletion(entity)
+      when DiasporaFederation::Entities::Retraction
+        Diaspora::Federation::Receive.retraction(entity, recipient_id)
+      else
+        persisted = Diaspora::Federation::Receive.perform(entity)
+        Workers::ReceiveLocal.perform_async(persisted.class.to_s, persisted.id, [recipient_id].compact) if persisted
       end
     end
     
-            def accepted_types_and_failures
-          if @allowed_types.present?
-            'Accept content types: #{@allowed_types.join(', ')}\n'.tap do |message|
-              if @missing_allowed_types.present?
-                message << '  #{@missing_allowed_types.join(', ')} were rejected.'
-              else
-                message << '  All were accepted successfully.'
-              end
-            end
+              lambda do |corrector|
+            corrector.replace(center.source_range, new_center)
           end
-        end
-        def rejected_types_and_failures
-          if @rejected_types.present?
-            'Reject content types: #{@rejected_types.join(', ')}\n'.tap do |message|
-              if @missing_rejected_types.present?
-                message << '  #{@missing_rejected_types.join(', ')} were accepted.'
-              else
-                message << '  All were rejected successfully.'
-              end
-            end
-          end
-        end
-    
-    
-    {
-    {  # Returns hash with styles for all classes using Paperclip.
-  # Unfortunately current version does not work with lambda styles:(
-  #   {
-  #     :User => {:avatar => [:small, :big]},
-  #     :Book => {
-  #       :cover => [:thumb, :croppable]},
-  #       :sample => [:thumb, :big]},
-  #     }
-  #   }
-  def self.current_attachments_styles
-    Hash.new.tap do |current_styles|
-      Paperclip::AttachmentRegistry.each_definition do |klass, attachment_name, attachment_attributes|
-        # TODO: is it even possible to take into account Procs?
-        next if attachment_attributes[:styles].kind_of?(Proc)
-        attachment_attributes[:styles].try(:keys).try(:each) do |style_name|
-          klass_sym = klass.to_s.to_sym
-          current_styles[klass_sym] ||= Hash.new
-          current_styles[klass_sym][attachment_name.to_sym] ||= Array.new
-          current_styles[klass_sym][attachment_name.to_sym] << style_name.to_sym
-          current_styles[klass_sym][attachment_name.to_sym].map!(&:to_s).sort!.map!(&:to_sym).uniq!
         end
       end
     end
   end
-  private_class_method :current_attachments_styles
+end
+
     
-          def remove_attachment(table_name, *attachment_names)
-        raise ArgumentError, 'Please specify attachment name in your remove_attachment call in your migration.' if attachment_names.empty?
+            MSG = 'Do not freeze immutable objects, as freezing them has no ' \
+              'effect.'.freeze
+    
+        context 'opening brace on same line as first element' do
+      context 'last element has a trailing comma' do
+        it 'autocorrects closing brace on different line from last element' do
+          new_source = autocorrect_source(['#{prefix}#{open}#{a}, # a',
+                                           '#{b}, # b',
+                                           close,
+                                           suffix])
