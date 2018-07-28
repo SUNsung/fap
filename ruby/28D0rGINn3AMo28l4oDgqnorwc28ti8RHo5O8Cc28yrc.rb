241801413848194419f3115dd6c865415f1a6204
SUNsung <weatherius@gmail.com>
2018-07-28 20:49:56 +0300

    Rewrite [28D0rGINn3AMo28l4oDgqnorwc28ti8RHo5O8Cc28yrc.rb] File in: 2018-07-28 20:49:55

--- ruby/28D0rGINn3AMo28l4oDgqnorwc28ti8RHo5O8Cc28yrc.rb
+++ ruby/28D0rGINn3AMo28l4oDgqnorwc28ti8RHo5O8Cc28yrc.rb
@@ -1,74 +1,66 @@
 
         
-            You can read more about this change at:
-      https://www.playframework.com/documentation/2.3.x/Migration23
-      https://www.playframework.com/documentation/2.3.x/Highlights23
-    EOS
-  when 'haskell-platform' then <<-EOS.undent
-    We no longer package haskell-platform. Consider installing ghc
-    and cabal-install instead:
-      brew install ghc cabal-install
+          class FeatureTopicUsers < Jobs::Base
     
-          keg_only_deps.each do |dep|
-        ENV.prepend_path 'PATH', dep.opt_bin.to_s
-        ENV.prepend_path 'PKG_CONFIG_PATH', '#{dep.opt_lib}/pkgconfig'
-        ENV.prepend_path 'PKG_CONFIG_PATH', '#{dep.opt_share}/pkgconfig'
-        ENV.prepend_path 'ACLOCAL_PATH', '#{dep.opt_share}/aclocal'
-        ENV.prepend_path 'CMAKE_PREFIX_PATH', dep.opt_prefix.to_s
-        ENV.prepend 'LDFLAGS', '-L#{dep.opt_lib}' if dep.opt_lib.directory?
-        ENV.prepend 'CPPFLAGS', '-I#{dep.opt_include}' if dep.opt_include.directory?
-      end
-    end
-    
-      def zsh_completion_caveats
-    if keg && keg.completion_installed?(:zsh) then <<-EOS.undent
-      zsh completion has been installed to:
-        #{HOMEBREW_PREFIX}/share/zsh/site-functions
-      EOS
+        def form_configurable_attributes
+      form_configurable_fields.keys
     end
   end
-    
-        dirs.each do |d|
-      files = []
-      d.find { |pn| files << pn unless pn.directory? }
-      print_remaining_files files, d
-    end
-    
-        if $stdout.tty?
-      metacharacters = %w[\\ | ( ) [ ] { } ^ $ * + ? .]
-      bad_regex = metacharacters.any? do |char|
-        ARGV.any? do |arg|
-          arg.include?(char) && !arg.start_with?('/')
-        end
-      end
-      if ARGV.any? && bad_regex
-        ohai 'Did you mean to perform a regular expression search?'
-        ohai 'Surround your query with /slashes/ to search by regex.'
-      end
-    end
-    
-      def self.factory(name)
-    Formulary.factory(name)
-  end
-    
-    end
+end
 
     
-        def javascripts_path
-      File.join assets_path, 'javascripts'
-    end
-    
-      # Configure static asset server for tests with Cache-Control for performance.
-  if config.respond_to?(:serve_static_files)
-    # rails >= 4.2
-    config.serve_static_files = true
-  elsif config.respond_to?(:serve_static_assets)
-    # rails < 4.2
-    config.serve_static_assets = true
+      def tumblr_oauth_token_secret
+    service.secret
   end
-  config.static_cache_control = 'public, max-age=3600'
     
-          spec['main'] =
-          find_files.(File.join(Bootstrap.stylesheets_path, '_bootstrap.scss')) +
-          find_files.(Bootstrap.fonts_path) +
-          %w(assets/javascripts/bootstrap.js)
+      it 'evaluates elsif-body in containing scope' do
+    a = 123
+    if false
+      b = a+1
+    elsif false
+      b = a+2
+    elsif true
+      b = a+3
+    else
+      b = a+4
+    end
+    b.should == 126
+  end
+end
+
+    
+      it 'returns a Time with a UTC offset specified as -HH:MM' do
+    t = Time.gm(2007, 1, 9, 12, 0, 0)
+    t.localtime('-01:00')
+    t.should == Time.new(2007, 1, 9, 11, 0, 0, -3600)
+    t.utc_offset.should == -3600
+  end
+    
+        self
+  end
+    
+        Tempfile.create(name.to_s) {|f|
+      f.sync = true
+      expected = ''
+      result = nil
+      bytes[0...-1].each do |x|
+        f.write x
+        f.write ' '
+        f.pos -= 1
+        expected << x
+        assert_nothing_raised(bug6487) {result = File.read(f.path, mode: 'rb:bom|utf-8')}
+        assert_equal('#{expected} '.force_encoding('utf-8'), result)
+      end
+      f.write bytes[-1]
+      assert_nothing_raised(bug6487) {result = File.read(f.path, mode: 'rb:bom|utf-8')}
+      assert_equal '', result, 'valid bom'
+    }
+  end
+    
+      it 'decodes the remaining doubles when passed the '*' modifier' do
+    array = '@\x07333333?\xf6ffffff@\x20ffffff'.unpack(unpack_format('*'))
+    array.should == [2.9, 1.4, 8.2]
+  end
+    
+    fileOutJar 	= clsFile.new_with_sig('Ljava.lang.String;', 'output.jar')
+filesIn		= Array.new
