b11d8339261f00d16bfecd76e9ffcfc7991c013a
SUNsung <weatherius@gmail.com>
2018-07-29 02:01:54 +0300

    Rewrite [23pHAgfzyr4o23ev1fqdRiZq.23b79LvE58k2M2395v.cpp] File in: 2018-07-29 02:01:53

--- cccccc/23pHAgfzyr4o23ev1fqdRiZq.23b79LvE58k2M2395v.cpp
+++ cccccc/23pHAgfzyr4o23ev1fqdRiZq.23b79LvE58k2M2395v.cpp
@@ -1,190 +1,196 @@
 
         
-        #if defined(OS_WIN)
-StringType GetWaitEventName(base::ProcessId pid);
+                const float max_dist = 10.0f;
+        if(black_order[0].second > max_dist || black_order[1].second > max_dist ||
+           white_order[0].second > max_dist || white_order[1].second > max_dist)
+        {
+            continue; // there will be no improvement in this corner position
+        }
     
-    namespace atom {
-    }
-    
-    class NativeWindow;
-    
-    namespace atom {
-    }
-    
-      // Called by platform specific implementations of this class whenever a key
-  // is struck. Only called for keys that have an observer registered.
-  void NotifyKeyPressed(const ui::Accelerator& accelerator);
-    
-    #ifndef CHROME_BROWSER_PRINTING_PRINTING_UI_WEB_CONTENTS_OBSERVER_H_
-#define CHROME_BROWSER_PRINTING_PRINTING_UI_WEB_CONTENTS_OBSERVER_H_
-    
-        SECTION('operator[key] throws when given an invalid key') {
-      REQUIRE_THROWS_WITH(cursor['foo'], StartsWith('No such key: 'foo''));
-      REQUIRE_THROWS_WITH(cursor['bar'], StartsWith('No such key: 'bar''));
-    }
-    
-      CursorBase() = default;
-    
-    /// Optimizer that defines a required `step()` method that takes no arguments
-/// and produces no values. The only side effect is that parameters are updated
-/// according to the concrete optimization algorithm.
-class Optimizer : public detail::OptimizerBase {
- public:
-  using detail::OptimizerBase::OptimizerBase;
-  virtual void step() = 0;
-};
-    
-    void THPDevice_init(PyObject *module)
-{
-  if (PyType_Ready(&THPDeviceType) < 0) {
-    throw python_error();
-  }
-  Py_INCREF(&THPDeviceType);
-  if (PyModule_AddObject(module, 'device', (PyObject *)&THPDeviceType) != 0) {
-    throw python_error();
-  }
-}
-
-    
-      EArrXi maxIndices(N * keypoint_count);
-  // finding max value first (only maxCoeff() is vectorized, not
-  // maxCoeff(&index)), then find the index (equalness check is also fast)
-  EArrXf maxScores = heatmaps.rowwise().maxCoeff();
-  for (int r = 0; r < N * keypoint_count; r++) {
-    float maxScore = maxScores[r];
-    for (int c = 0; c < heatmap_size * heatmap_size; c++) {
-      if (heatmaps(r, c) == maxScore) {
-        maxIndices[r] = c;
-        break;
+    template<typename Op>
+folly::Optional<Type> key_type_or_fixup(ISS& env, Op op) {
+  auto fixup = [&] (Type ty, bool isProp) -> folly::Optional<Type> {
+    if (auto const val = tv(ty)) {
+      if (isStringType(val->m_type)) {
+        op.mkey.mcode = isProp ? MPT : MET;
+        op.mkey.litstr = val->m_data.pstr;
+        reduce(env, op);
+        return folly::none;
+      }
+      if (!isProp && val->m_type == KindOfInt64) {
+        op.mkey.mcode = MEI;
+        op.mkey.int64 = val->m_data.num;
+        reduce(env, op);
+        return folly::none;
       }
     }
+    return std::move(ty);
+  };
+  switch (op.mkey.mcode) {
+    case MEC: case MPC:
+      return fixup(topC(env, op.mkey.idx), op.mkey.mcode == MPC);
+    case MEL: case MPL:
+      return fixup(locAsCell(env, op.mkey.local), op.mkey.mcode == MPL);
+    case MW:
+      return TBottom;
+    case MEI:
+      return ival(op.mkey.int64);
+    case MET: case MPT: case MQT:
+      return sval(op.mkey.litstr);
   }
+  not_reached();
+}
     
-        const auto* weight_data = weight.template data<T>();
-    const auto* alpha_data = adaptive_ ? Input(4).template data<T>() : 0;
-    const auto* val_data = val.template data<T>();
-    const auto* key_data = key.template data<TIndex>();
+      /**
+   * StreamContext settings for this connection
+   */
+  void setStreamContextOptions(const Array &opts) {
+    m_stream_context_options = opts;
+  }
+  /**
+   * require SLS/TLS
+   * (default) CURLUSESSL_NONE, CURLUSESSL_TRY, CURLUSESSL_CONTROL,
+   *           CURLUSESSL_ALL
+   */
+    
+    /*!
+ * \brief Check whether the value is simple parameter
+ * \param val The value to check.
+ */
+inline bool isSimple(const Rcpp::RObject& val) {
+  switch (TYPEOF(val)) {
+    case STRSXP:
+    case INTSXP:
+    case REALSXP:
+    case LGLSXP: return true;
+    default: return false;
+  }
+}
+    
+    void ArrayDataIter::Convert(const Rcpp::NumericVector& src,
+                            const std::vector<size_t>& order,
+                            size_t batch_size,
+                            std::vector<NDArray> *out) {
+  Rcpp::RObject dim = src.attr('dim');
+  Rcpp::Dimension rshape(dim);
+  size_t ndim = rshape.size();
+  std::vector<mx_float> temp(src.size()), batch;
+  std::copy(src.begin(), src.end(), temp.begin());
+  out->clear();
+  out->reserve(rshape[ndim - 1] / batch_size + 1);
+  size_t line_size = 1;
+  for (size_t i = 0; i < rshape.size() - 1; ++i) {
+    line_size *= rshape[i];
+  }
+  rshape[ndim - 1] = batch_size;
+  batch.resize(batch_size * line_size, 0.0f);
+    }
     
     
-    {  // RPN_PRE_NMS_TOP_N
-  int rpn_pre_nms_topN_{6000};
-  // RPN_POST_NMS_TOP_N
-  int rpn_post_nms_topN_{300};
-  // RPN_NMS_THRESH
-  float rpn_nms_thresh_{0.7};
-  // RPN_MIN_SIZE
-  float rpn_min_size_{16};
-  // Correct bounding box transform coordates, see bbox_transform() in boxes.py
-  // Set to true to match the detectron code, set to false for backward
-  // compatibility
-  bool correct_transform_coords_{false};
-  // If set, for rotated boxes in RRPN, output angles are normalized to be
-  // within [angle_bound_lo, angle_bound_hi].
-  bool angle_bound_on_{true};
-  int angle_bound_lo_{-90};
-  int angle_bound_hi_{90};
-  // For RRPN, clip almost horizontal boxes within this threshold of
-  // tolerance for backward compatibility. Set to negative value for
-  // no clipping.
-  float clip_angle_thresh_{1.0};
+    { private:
+  // make constructor private
+  explicit NDArrayFunction(OpHandle handle, std::string name);
+  /*! \brief internal functioon handle. */
+  OpHandle handle_;
+  // name of the function
+  std::string name_;
+  // keyword arguments.
+  std::string key_var_num_args_;
+  // name of arguments
+  std::vector<std::string> arg_names_;
+  // check
+  std::vector<bool> arg_nd_array_;
+  // ther formals of arguments
+  Rcpp::List formals_;
 };
     
-      // Convert inputs and figure out weights
-  std::unordered_set<std::string> weights;
-  const std::vector<string>& ws_blobs = ws->Blobs();
-  for (const auto& s : ws_blobs) {
-    VLOG(2) << 'Add weights: ' << s;
-    weights.emplace(s);
+    /*!
+ * \brief All the possible information needed by Operator.Forward and Backward
+ *  This is the superset of RunContext.
+ *  We use this data structure to bookkeep everything needed by Forward and Backward.
+ * \sa Resource
+ */
+struct OpContext {
+  /*! \brief whether there is a backward phase to compute gradients. */
+  bool need_grad;
+  /*! \brief whether it is training phase */
+  bool is_train;
+  /*! \brief RunContext related resources */
+  RunContext run_ctx;
+  /*! \brief the callback when operation completes, used by asynchronize ops */
+  engine::CallbackOnComplete async_on_complete;
+  /*! \brief Resources requested by the operator */
+  std::vector<Resource> requested;
+  /*!
+   * \brief get mshadow stream from Context
+   * \return the mshadow stream
+   * \tparam xpu the device type of the stream
+   */
+  template<typename xpu>
+  inline mshadow::Stream<xpu>* get_stream() const {
+    return run_ctx.get_stream<xpu>();
   }
+};
     
-    static PyObject* THPWrapperClass = NULL;
     
-            NotifyChangedNodes<ElemType>(net, inputMatrices);
-    
-    template<> inline
-dnnError_t dnnInnerProductCreateBackwardData<float>(
-    dnnPrimitive_t* pInnerProduct,
-    dnnPrimitiveAttributes_t attributes,
-    size_t dimensions,
-    const size_t srcSize[],
-    size_t outputChannels)
-{
-    return dnnInnerProductCreateBackwardData_F32(
-        pInnerProduct, attributes, dimensions, srcSize, outputChannels);
-}
-template<> inline
-dnnError_t dnnInnerProductCreateBackwardData<double>(
-    dnnPrimitive_t* pInnerProduct,
-    dnnPrimitiveAttributes_t attributes,
-    size_t dimensions,
-    const size_t srcSize[],
-    size_t outputChannels)
-{
-    return dnnInnerProductCreateBackwardData_F64(
-        pInnerProduct, attributes, dimensions, srcSize, outputChannels);
+    {  if (map_key_var_args.count(op) != 0) {
+    *key_var_num_args = map_key_var_args[op].c_str();
+  } else {
+    *key_var_num_args = ret->ret_str.c_str();
+  }
+  return NNGetOpInfo(
+      creator, name, description,
+      num_args, arg_names, arg_type_infos,
+      arg_descriptions, return_type);
 }
     
-        // This prints a PROGRESS message with a percentage value of 0 to prevent timeouts on Philly
-    // when executing long running non-training operations like PreCompute, CV, Eval, and Write
-    static size_t TraceFakeProgress(size_t numIterationsBeforePrintingProgress, size_t numItersSinceLastPrintOfProgress)
-    {
-        size_t newNumItersSinceLastPrintOfProgress = numItersSinceLastPrintOfProgress;
-        if (GetTracingFlag())
-        {
-            newNumItersSinceLastPrintOfProgress++;
-            if (newNumItersSinceLastPrintOfProgress >= numIterationsBeforePrintingProgress)
-            {
-                printf('PROGRESS: %.2f%%\n', 0.0f);
-                newNumItersSinceLastPrintOfProgress = 0;
-            }
+    nnvm::NodeEntry AggregateGradient(std::vector<nnvm::NodeEntry>&& v);
+    
+    void ElementwiseSumDnsCsrDnsImpl(mshadow::Stream<cpu>* s,
+                                 const Resource& rsc,
+                                 const std::vector<NDArray>& nds,
+                                 NDArray* out) {
+  using namespace mxnet::op;
+  using namespace mxnet::op::mxnet_op;
+  const TBlob& out_data = out->data();
+  MSHADOW_TYPE_SWITCH(out->dtype(), DType, {  // data type
+    Kernel<Sum, cpu>::Launch(
+      s, out_data.Size(), out_data.dptr<DType>(), kWriteTo, nds[0].data().dptr<DType>(),
+      nds[2].data().dptr<DType>());
+    const TBlob& csr_data = nds[1].data();
+    const TBlob& csr_indices = nds[1].aux_data(csr::kIdx);
+    const TBlob& csr_indptr = nds[1].aux_data(csr::kIndPtr);
+    const nnvm::dim_t num_rows = nds[1].shape()[0];
+    const nnvm::dim_t num_cols = nds[1].shape()[1];
+    MSHADOW_IDX_TYPE_SWITCH(csr_indices.type_flag_, IType, {  // indices type
+      MSHADOW_IDX_TYPE_SWITCH(csr_indptr.type_flag_, CType, {  // indptr type
+        if (nds[1].storage_initialized()) {
+          Kernel<ElemwiseDnsCsrDnsKernel<kWriteTo, mshadow_op::plus>, cpu>::Launch(
+            s, num_rows, out_data.dptr<DType>(), out_data.dptr<DType>(),
+            csr_data.dptr<DType>(), csr_indices.dptr<IType>(),
+            csr_indptr.dptr<CType>(), num_rows, num_cols);
         }
-    }
-    
-    // Writes len bytes from buf, using the out callback.
-inline bool JPEGWrite(JPEGOutput out, const uint8_t* buf, size_t len) {
-  static const size_t kBlockSize = 1u << 30;
-  size_t pos = 0;
-  while (len - pos > kBlockSize) {
-    if (!out.Write(buf + pos, kBlockSize)) {
-      return false;
-    }
-    pos += kBlockSize;
-  }
-  return out.Write(buf + pos, len - pos);
+      });
+    });
+  });
 }
     
-    #include <math.h>
-    
-      tmp0 = in[stride];
-  tmp1 = kIDCTMatrix[ 1] * tmp0;
-  tmp2 = kIDCTMatrix[ 9] * tmp0;
-  tmp3 = kIDCTMatrix[17] * tmp0;
-  tmp4 = kIDCTMatrix[25] * tmp0;
-  out[0] += tmp1;
-  out[1] += tmp2;
-  out[2] += tmp3;
-  out[3] += tmp4;
-  out[4] -= tmp4;
-  out[5] -= tmp3;
-  out[6] -= tmp2;
-  out[7] -= tmp1;
-    
-    #include 'guetzli/jpeg_data.h'
-    
-    std::vector<uint8_t> DecodeJpegToRGB(const JPEGData& jpg) {
-  if (jpg.components.size() == 1 ||
-      (jpg.components.size() == 3 &&
-       HasYCbCrColorSpace(jpg) && (jpg.Is420() || jpg.Is444()))) {
-    OutputImage img(jpg.width, jpg.height);
-    img.CopyFromJpegData(jpg);
-    return img.ToSRGB();
+    bool ElementWiseSumForwardInferStorageType(const nnvm::NodeAttrs& attrs,
+                                           const int dev_mask,
+                                           DispatchMode* dispatch_mode,
+                                           std::vector<int> *in_attrs,
+                                           std::vector<int> *out_attrs) {
+  CHECK(!in_attrs->empty());
+  CHECK_EQ(out_attrs->size(), 1U);
+  bool ret = ElemwiseStorageAttr<false, true, false>(attrs, dev_mask, dispatch_mode,
+                                                     in_attrs, out_attrs);
+#if MXNET_USE_MKLDNN == 1
+  // We should always use FComputeEx.
+  if (dev_mask == mshadow::cpu::kDevMask
+      && common::ContainsOnlyStorage(*in_attrs, kDefaultStorage)
+      && out_attrs->at(0) == kDefaultStorage) {
+    *dispatch_mode = DispatchMode::kFComputeEx;
   }
-  return std::vector<uint8_t>();
-}
-    
-    #endif  // GUETZLI_JPEG_DATA_DECODER_H_
-
-    
-    
-    {  return true;
+#endif
+  return ret;
 }
