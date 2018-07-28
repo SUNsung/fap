cef2dd36efa20693847bb3a606ee4c21b346848f
SUNsung <weatherius@gmail.com>
2018-07-28 23:32:22 +0300

    Rewrite [56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py] File in: 2018-07-28 23:32:21

--- pyton/56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py
+++ pyton/56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py
@@ -1,278 +1,147 @@
 
         
-          # Recurrent connection for the arc-standard parser. For both tokens on the
-  # stack, we connect to the last time step to either SHIFT or REDUCE that
-  # token. This allows the parser to build up compositional representations of
-  # phrases.
-  parser.add_link(
-      source=parser,  # recurrent connection
-      name='rnn-stack',  # unique identifier
-      fml='stack.focus stack(1).focus',  # look for both stack tokens
-      source_translator='shift-reduce-step',  # maps token indices -> step
-      embedding_dim=64)  # project down to 64 dims
-    
-    cc_test(
-    name = 'index_translator_test',
-    srcs = ['index_translator_test.cc'],
-    deps = [
-        ':index_translator',
-        '//dragnn/core/test:mock_component',
-        '//dragnn/core/test:mock_transition_state',
-        '//syntaxnet:base',
-        '//syntaxnet:test_main',
-    ],
-)
-    
-        self._biases = []
-    self._biases.append(tf.get_variable(
-        'biases', [self._num_labels], tf.float32,
-        tf.random_normal_initializer(stddev=1e-4)))
-    
-    from google.protobuf import text_format
-    
-                              [[[  8,  19,  37],
-                            [ 18,  43,  85],
-                            [ 28,  67, 133]],
-                           [[ 27,  65, 131],
-                            [ 17,  41,  83],
-                            [  7,  17,  35]]])  # pyformat: disable
-    
-        self.assertTrue(content_actual == content_expected)
-    
-        Args:
-      name_scope: Scope for the annotation pipeline.
-      enable_tracing: Enabling this will result in two things:
-          1. Tracing will be enabled during inference.
-          2. A 'traces' node will be added to the outputs.
-      build_runtime_graph: Whether to build a graph for use by the runtime.
-    
-        with open(empty_input_path, 'w'):
-      pass
-    
-      log_normalization_factor_b1 = tf.unsorted_segment_sum(
-      max_scores_bm, batch_ids_bm, batch_size + 1)
-  log_normalization_factor_b = log_normalization_factor_b1[1:]
-    
-        return inner
+        
+if not PY2:
+    text_type = str
+    string_types = (str,)
+    integer_types = (int,)
     
     
-@pytest.mark.parametrize(
-    'url, expected', (
-            ('http://192.168.0.1:5000/', True),
-            ('http://192.168.0.1/', True),
-            ('http://172.16.1.1/', True),
-            ('http://172.16.1.1:5000/', True),
-            ('http://localhost.localdomain:5000/v1.0/', True),
-            ('http://172.16.1.12/', False),
-            ('http://172.16.1.12:5000/', False),
-            ('http://google.com:5000/v1.0/', False),
-    ))
-def test_should_bypass_proxies_no_proxy(
-        url, expected, monkeypatch):
-    '''Tests for function should_bypass_proxies to check if proxy
-    can be bypassed or not using the 'no_proxy' argument
-    '''
-    no_proxy = '192.168.0.0/24,127.0.0.1,localhost.localdomain,172.16.1.1'
-    # Test 'no_proxy' argument
-    assert should_bypass_proxies(url, no_proxy=no_proxy) == expected
-    
-    elif is_py3:
-    from urllib.parse import urlparse, urlunparse, urljoin, urlsplit, urlencode, quote, unquote, quote_plus, unquote_plus, urldefrag
-    from urllib.request import parse_http_list, getproxies, proxy_bypass, proxy_bypass_environment, getproxies_environment
-    from http import cookiejar as cookielib
-    from http.cookies import Morsel
-    from io import StringIO
-    from collections import OrderedDict
-    from collections.abc import Callable, Mapping, MutableMapping
-    
-            # Check if file, fo, generator, iterator.
-        # If not, run through normal process.
-    
-        elif hasattr(o, 'fileno'):
-        try:
-            fileno = o.fileno()
-        except io.UnsupportedOperation:
-            pass
-        else:
-            total_length = os.fstat(fileno).st_size
-    
-       >>> payload = dict(key1='value1', key2='value2')
-   >>> r = requests.post('http://httpbin.org/post', data=payload)
-   >>> print(r.text)
-   {
-     ...
-     'form': {
-       'key2': 'value2',
-       'key1': 'value1'
-     },
-     ...
-   }
-    
-        try:
-        s1 = os.lstat(path)
-    except OSError:
-        # the OSError should be handled with more care
-        # it could be a 'permission denied' but path is still a mount
-        return False
-    else:
-        # A symlink can never be a mount point
-        if os.path.stat.S_ISLNK(s1.st_mode):
-            return False
-    
-    # Backwards compat only
-try:
-    from hashlib import md5 as _md5
-except ImportError:
-    try:
-        from md5 import md5 as _md5
-    except ImportError:
-        # Assume we're running in FIPS mode here
-        _md5 = None
-    
-        target.add_argument('--run',
-                        metavar='ID',
-                        help='Shippable run ID')
-    
-            expected = {
-            'description': 'foo_url_map description',
-            'hostRules': [
-                {
-                    'description': 'host rules description',
-                    'hosts': [
-                        'www.example.com',
-                        'www2.example.com'
-                    ],
-                    'pathMatcher': 'host_rules_path_matcher'
-                }
-            ],
-            'name': 'foo_url_map_name',
-            'pathMatchers': [
-                {
-                    'defaultService': 'bes-pathmatcher-one-default',
-                    'description': 'path matcher one',
-                    'name': 'path_matcher_one',
-                    'pathRules': [
-                        {
-                            'paths': [
-                                '/',
-                                '/aboutus'
-                            ],
-                            'service': 'my-one-bes'
-                        }
-                    ]
-                },
-                {
-                    'defaultService': 'bes-pathmatcher-two-default',
-                    'description': 'path matcher two',
-                    'name': 'path_matcher_two',
-                    'pathRules': [
-                        {
-                            'paths': [
-                                '/webapp',
-                                '/graphs'
-                            ],
-                            'service': 'my-two-bes'
-                        }
-                    ]
-                }
-            ]
-        }
-    
-        def set(self, key, value):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                item.value = value
-                return
-        self.table[hash_index].append(Item(key, value))
-    
-        def crawled_similar(self, signature):
-        '''Determine if we've already crawled a page matching the given signature'''
-        pass
-    
-        def __init__(self, id, name):
-        self.id = id
-        self.name = name
-        self.friend_ids = []
-    
-        def current_year_month(self):
-        '''Return the current year and month.'''
-        ...
-    
-    # Usage: ../../../python bench.py
-    
-        def _get_publicId(self):
-        return self.publicId
-    def _set_publicId(self, publicId):
-        self.publicId = publicId
-    
-    @dataclasses.dataclass
-class CV:
-    T_CV4 = typing.ClassVar
-    cv0: typing.ClassVar[int] = 20
-    cv1: typing.ClassVar = 30
-    cv2: T_CV2
-    cv3: T_CV3
-    not_cv4: T_CV4  # When using string annotations, this field is not recognized as a ClassVar.
-    
-    # dataclass_module_1.py and dataclass_module_1_str.py are identical
-# except only the latter uses string annotations.
-    
-    
-class DefaultEntryTest(BaseRequestRateTest, unittest.TestCase):
-    robots_txt = '''\
-User-agent: *
-Crawl-delay: 1
-Request-rate: 3/15
-Disallow: /cyberworld/map/
-    '''
-    request_rate = urllib.robotparser.RequestRate(3, 15)
-    crawl_delay = 1
-    good = ['/', '/test.html']
-    bad = ['/cyberworld/map/index.html']
-    
-    curl_log = logging.getLogger('tornado.curl_httpclient')
-    
-        def __repr__(self):
-        attrs = ('protocol', 'host', 'method', 'uri', 'version', 'remote_ip')
-        args = ', '.join(['%s=%r' % (n, getattr(self, n)) for n in attrs])
-        return '%s(%s)' % (self.__class__.__name__, args)
-    
-        @gen_test
-    def test_put_timeout(self):
-        q = queues.Queue(1)
-        q.put_nowait(0)  # Now it's full.
-        put_timeout = q.put(1, timeout=timedelta(seconds=0.01))
-        put = q.put(2)
-        with self.assertRaises(TimeoutError):
-            yield put_timeout
-    
-    from tornado.concurrent import Future, is_future, chain_future, future_set_exc_info, future_add_done_callback  # noqa: E501
-from tornado.log import app_log, gen_log
-from tornado.platform.auto import set_close_exec, Waker
-from tornado import stack_context
-from tornado.util import (
-    PY3, Configurable, errno_from_exception, timedelta_to_seconds,
-    TimeoutError, unicode_type, import_object,
-)
-    
-            Raises `ValueError` if called more times than `.put`.
-        '''
-        if self._unfinished_tasks <= 0:
-            raise ValueError('task_done() called too many times')
-        self._unfinished_tasks -= 1
-        if self._unfinished_tasks == 0:
-            self._finished.set()
-    
-        def test_async_raise_return(self):
-        @gen.coroutine
-        def f():
-            yield gen.moment
-            raise gen.Return()
-    
-        pass
+# Core signals.  For usage examples grep the source code or consult
+# the API documentation in docs/api.rst as well as docs/signals.rst
+template_rendered = _signals.signal('template-rendered')
+before_render_template = _signals.signal('before-render-template')
+request_started = _signals.signal('request-started')
+request_finished = _signals.signal('request-finished')
+request_tearing_down = _signals.signal('request-tearing-down')
+got_request_exception = _signals.signal('got-request-exception')
+appcontext_tearing_down = _signals.signal('appcontext-tearing-down')
+appcontext_pushed = _signals.signal('appcontext-pushed')
+appcontext_popped = _signals.signal('appcontext-popped')
+message_flashed = _signals.signal('message-flashed')
 
     
-        config_path = config_util.ensure_config_exists(config_dir)
-    print('Configuration file:', config_path)
-    return 0
+        The decorators stored in the decorators list are applied one after another
+    when the view function is created.  Note that you can *not* use the class
+    based decorators since those would decorate the view class and not the
+    generated view function!
+    '''
+    
+        app = flask.Flask(__name__)
+    with pytest.raises(TypeError):
+        app.config.from_mapping(
+            {}, {}
+        )
+    
+    
+def test_uninstalled_package_paths(modules_tmpdir, purge_module):
+    app = modules_tmpdir.mkdir('config_package_app')
+    init = app.join('__init__.py')
+    init.write(
+        'import os\n'
+        'import flask\n'
+        'here = os.path.abspath(os.path.dirname(__file__))\n'
+        'app = flask.Flask(__name__)\n'
+    )
+    purge_module('config_package_app')
+    
+        app.testing = False
+    stream = StringIO()
+    rv = client.get('/', errors_stream=stream)
+    assert rv.status_code == 500
+    assert rv.data
+    err = stream.getvalue()
+    assert 'Exception on / [GET]' in err
+    assert 'Exception: test' in err
+
+    
+    
+def test_aborting(app):
+    class Foo(Exception):
+        whatever = 42
+    
+    import flask
+    
+    
+tar_extensions = ('.tar', '.tar.Z', '.tar.bz2', '.tar.gz', '.tar.lz',
+                  '.tar.lzma', '.tar.xz', '.taz', '.tb2', '.tbz', '.tbz2',
+                  '.tgz', '.tlz', '.txz', '.tz')
+    
+        @property
+    def encoding(self):
+        return self._orig.encoding or 'utf8'
+    
+    
+MIME_RE = re.compile(r'^[^/]+/[^/]+$')
+    
+    
+FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
+FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
+JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
+BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
+    
+    
+def test_unicode_digest_auth(httpbin):
+    # it doesn't really authenticate us because httpbin
+    # doesn't interpret the utf8-encoded auth
+    http('--auth-type=digest',
+         '--auth', u'test:%s' % UNICODE,
+         httpbin.url + u'/digest-auth/auth/test/' + UNICODE)
+
+    
+        def test_self_signed_server_cert_by_default_raises_ssl_error(
+            self,
+            httpbin_secure_untrusted):
+        with pytest.raises(SSLError):
+            http(httpbin_secure_untrusted.url + '/get')
+    
+    
+class BaseConfigDict(dict):
+    
+    
+def test_credentials_in_url(httpbin_both):
+    url = add_auth(httpbin_both.url + '/basic-auth/user/password',
+                   auth='user:password')
+    r = http('GET', url)
+    assert HTTP_OK in r
+    assert r.json == {'authenticated': True, 'user': 'user'}
+    
+    
+def get_config(env_var, config_var):
+    result = os.getenv(env_var)
+    if not result:
+        result = get_from_rhc_config(config_var)
+    if not result:
+        sys.exit('failed=True msg='missing %s'' % env_var)
+    return result
+    
+        def on_open_shell(self):
+        try:
+            self._exec_cli_command('screen-length 0 temporary')
+        except AnsibleConnectionFailure:
+            raise AnsibleConnectionFailure('unable to set terminal parameters')
+
+    
+        def on_open_shell(self):
+        try:
+            for cmd in (b'set terminal length 0', b'set terminal width 512'):
+                self._exec_cli_command(cmd)
+            self._exec_cli_command(b'set terminal length %d' % self.terminal_length)
+        except AnsibleConnectionFailure:
+            raise AnsibleConnectionFailure('unable to set terminal parameters')
+
+    
+        def construct_vault_encrypted_unicode(self, node):
+        value = self.construct_scalar(node)
+        b_ciphertext_data = to_bytes(value)
+        # could pass in a key id here to choose the vault to associate with
+        # TODO/FIXME: plugin vault selector
+        vault = self._vaults['default']
+        if vault.secrets is None:
+            raise ConstructorError(context=None, context_mark=None,
+                                   problem='found !vault but no vault password provided',
+                                   problem_mark=node.start_mark,
+                                   note=None)
+        ret = AnsibleVaultEncryptedUnicode(b_ciphertext_data)
+        ret.vault = vault
+        return ret
