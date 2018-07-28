a1b3bd3108c9bdf771683834f8ec9a9f173155a1
SUNsung <weatherius@gmail.com>
2018-07-28 05:16:49 +0300

    Rewrite [37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb] File in: 2018-07-28 05:16:49

--- ruby/37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb
+++ ruby/37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb
@@ -1,45 +1,92 @@
 
         
-          def remote_url
-    object.remote_url.presence
-  end
-    
-    module Hbc
-  class DSL
-    class Container
-      VALID_KEYS = Set.new [
-        :type,
-        :nested,
-      ]
-    
-        used_formulae_missing = false
-    used_formulae = begin
-      ARGV.formulae
-    rescue FormulaUnavailableError => e
-      opoo e
-      used_formulae_missing = true
-      # If the formula doesn't exist: fake the needed formula object name.
-      ARGV.named.map { |name| OpenStruct.new name: name, full_name: name }
+              it 'renders HTML' do
+        capture = render(options, screenshots)
+        expect(capture).to match(/<html>/)
+        expect(capture).to include('<li>Some</li>')
+        expect(capture).to include('<li>key</li>')
+        expect(capture).to include('<li>words</li>')
+      end
     end
     
-      raise
+            expect(result).to eq('hg parent --template {rev}')
+        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('hg parent --template {rev}')
+      end
+    end
+  end
+end
+
+    
+          it 'prefers a custom version number over a boring version bump' do
+        Fastlane::FastFile.new.parse('lane :test do
+          increment_version_number(version_number: '1.77.3', bump_type: 'major')
+        end').runner.execute(:test)
+    
+    def check_link(uri)
+  HTTParty.head(uri, :verify => false).code.to_i.tap do |status|
+    if (400..422).include?(status)
+      if status != 403 && !uri.exclude?('udemy.com')
+        raise 'Request had status #{status}'
+      else
+        putc('S')
+      end
+    end
+  end
 end
     
-        validate :any_searchfield_present?
-    
-        def path_for(file)
-      return file.path if file.is_a?(File)
-      return file if file.is_a?(String)
+          options[:only_patterns] = [/\Agetting-started\//, /\Alayout\//, /\Acontent\//, /\Acomponents\//, /\Autilities\//, /\Amigration\//]
     end
     
-    module Sass
-  # Runs a SassScript read-eval-print loop.
-  # It presents a prompt on the terminal,
-  # reads in SassScript expressions,
-  # evaluates them,
-  # and prints the result.
-  class Repl
-    # @param options [{Symbol => Object}] An options hash.
-    def initialize(options = {})
-      @options = options
+        version 'C#' do
+      self.base_url = 'https://api.haxe.org/cs/'
     end
+    
+        options[:only_patterns] = [
+      /\Alanguage\./,
+      /\Aclass\./,
+      /\Afunctions?\./,
+      /\Acontrol-structures/,
+      /\Aregexp\./,
+      /\Areserved\.exceptions/,
+      /\Areserved\.interfaces/,
+      /\Areserved\.variables/]
+    
+    module Docs
+  class EntryIndex
+    attr_reader :entries, :types
+    
+        def type=(value)
+      @type = value.try :strip
+    end
+    
+        delegate :empty?, :blank?, to: :pages
+    
+            def find_order
+          @order = Spree::Order.find_by!(number: order_id)
+        end
+    
+        def add_required_validations
+      options = Paperclip::Attachment.default_options.deep_merge(@options)
+      if options[:validate_media_type] != false
+        name = @name
+        @klass.validates_media_type_spoof_detection name,
+          :if => ->(instance){ instance.send(name).dirty? }
+      end
+    end
+    
+            def responds?
+          methods = @subject.instance_methods.map(&:to_s)
+          methods.include?('#{@attachment_name}') &&
+            methods.include?('#{@attachment_name}=') &&
+            methods.include?('#{@attachment_name}?')
+        end
+    
+      class Railtie
+    def self.insert
+      Paperclip.options[:logger] = Rails.logger
+    
+          def validate_blacklist(record, attribute, value)
+        if forbidden.present? && forbidden.any? { |type| type === value }
+          mark_invalid record, attribute, forbidden
+        end
+      end
