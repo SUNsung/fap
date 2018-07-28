7156fcd36d11d209f9ff0aa433bc98fee8dbd05c
SUNsung <weatherius@gmail.com>
2018-07-28 09:50:47 +0300

    Rewrite [520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp] File in: 2018-07-28 09:50:47

--- cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
+++ cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
@@ -1,94 +1,323 @@
 
         
-          class MatchGroundTruth : public QuickTest ,
-      public ::testing::WithParamInterface<const char*> {
-  };
-  
-  TEST_P(MatchGroundTruth, FastPhototestOCR) {
-    OCRTester(TESTING_DIR '/phototest.tif',
-              TESTING_DIR '/phototest.txt',
-              TESSDATA_DIR '_fast', GetParam());
-  }
-  
-  INSTANTIATE_TEST_CASE_P( EngLatinDevaArabLang, MatchGroundTruth, 
-                        ::testing::Values('eng', 'script/Latin', 'script/Devanagari', 'script/Arabic') );
-    
-    ScrollView* bln_word_window_handle();  //return handle
-void build_image_window(int width, int height);
-void display_bln_lines(ScrollView window,
-                       ScrollView::Color colour,
-                       float scale_factor,
-                       float y_offset,
-                       float minx,
-                       float maxx);
-                                 //function to call
-void pgeditor_msg(  //message display
-                  const char *msg);
-void pgeditor_show_point(  //display coords
-                         SVEvent *event);
-                                 //put bln word in       box
-void show_point(PAGE_RES* page_res, float x, float y);
-    
-    #endif  // TESSERACT_CCMAIN_OTSUTHR_H_
-
-    
-    void signal_exit(int signal_code);
-    
-    extern const char *kUTF8LineSeparator;
-extern const char *kUTF8ParagraphSeparator;
-extern const char *kLRM;  //< Left-to-Right Mark
-extern const char *kRLM;  //< Right-to-Left Mark
-extern const char *kRLE;  //< Right-to-Left Embedding
-extern const char *kPDF;  //< Pop Directional Formatting
-    
-    /**
- * Return the character normalization feature for a blob.
- *
- * The features returned are in a scale where the x-height has been
- * normalized to live in the region y = [-0.25 .. 0.25].  Example ranges
- * for English below are based on the Linux font collection on 2009-12-04:
- *
- *  - Params[CharNormY]
- *     - The y coordinate of the grapheme's centroid.
- *     - English: [-0.27, 0.71]
- *
- *  - Params[CharNormLength]
- *     - The length of the grapheme's outline (tiny segments discarded),
- *     divided by 10.0=LENGTH_COMPRESSION.
- *     - English: [0.16, 0.85]
- *
- *  - Params[CharNormRx]
- *     - The radius of gyration about the x axis, as measured from CharNormY.
- *     - English: [0.011, 0.34]
- *
- *  - Params[CharNormRy]
- *     - The radius of gyration about the y axis, as measured from
- *     the x center of the grapheme's bounding box.
- *     - English: [0.011, 0.31]
- */
-FEATURE_SET ExtractCharNormFeatures(const INT_FX_RESULT_STRUCT& fx_info) {
-  FEATURE_SET feature_set = NewFeatureSet(1);
-  FEATURE feature = NewFeature(&CharNormDesc);
+          GraphemeClusterBreakProperty GCBForC0 = getGraphemeClusterBreakProperty(C[0]);
+  while (true) {
+    if (isExtendedGraphemeClusterBoundaryAfter(GCBForC0))
+      return S.slice(0, SourceNext - SourceStart);
     }
     
-      // Free up any currently unused dawgs.
-  void DeleteUnusedDawgs() {
-    dawgs_.DeleteUnusedObjects();
+    /// Index the given source file and store the results to \p indexStorePath.
+///
+/// \param primarySourceFile The source file to index.
+///
+/// \param indexUnitToken A unique identifier for this translation unit in the
+/// form of a file path.
+///
+/// \param indexStorePath The location to write the indexing data to.
+///
+/// \param indexSystemModules If true, emit index data for imported serialized
+/// swift system modules.
+///
+/// \param isDebugCompilation true for non-optimized compiler invocation.
+///
+/// \param targetTriple The target for this compilation.
+///
+/// \param dependencyTracker The set of dependencies seen while building.
+bool indexAndRecord(SourceFile *primarySourceFile, StringRef indexUnitToken,
+                    StringRef indexStorePath, bool indexSystemModules,
+                    bool isDebugCompilation, StringRef targetTriple,
+                    const DependencyTracker &dependencyTracker);
+    
+      ArrayRef<const MarkupASTNode *> getChildren() const {
+    return {getTrailingObjects<MarkupASTNode *>(), NumChildren};
   }
     
-      // Builds a target using CTC. Slightly improved as follows:
-  // Includes normalizations and clipping for stability.
-  // labels should be pre-padded with nulls wherever desired, but they don't
-  // have to be between all labels. Allows for multi-label codes with no
-  // nulls between.
-  // labels can be longer than the time sequence, but the total number of
-  // essential labels (non-null plus nulls between equal labels) must not exceed
-  // the number of timesteps in outputs.
-  // outputs is the output of the network, and should have already been
-  // normalized with NormalizeProbs.
-  // On return targets is filled with the computed targets.
-  // Returns false if there is insufficient time for the labels.
-  static bool ComputeCTCTargets(const GenericVector<int>& truth_labels,
-                                int null_char,
-                                const GENERIC_2D_ARRAY<float>& outputs,
-                                NetworkIO* targets);
+      /// Creates a copy of a \c MemoryBuffer and adds it to the \c SourceManager,
+  /// taking ownership of the copy.
+  unsigned addMemBufferCopy(llvm::MemoryBuffer *Buffer);
+    
+    #ifndef SWIFT_SYNTAX_REFERENCES_H
+#define SWIFT_SYNTAX_REFERENCES_H
+    
+    /// Runs the given function with the given context argument exactly once.
+/// The predicate argument must point to a global or static variable of static
+/// extent of type swift_once_t.
+SWIFT_RUNTIME_EXPORT
+void swift_once(swift_once_t *predicate, void (*fn)(void *), void *context);
+    
+    // The compiler generates the swift_once_t values as word-sized zero-initialized
+// variables, so we want to make sure swift_once_t isn't larger than the
+// platform word or the function below might overwrite something it shouldn't.
+static_assert(sizeof(swift_once_t) <= sizeof(void*),
+              'swift_once_t must be no larger than the platform word');
+    
+    #ifndef SWIFT_INDEX_INDEX_H
+#define SWIFT_INDEX_INDEX_H
+    
+      ResultPlanBuilder(SILGenFunction &SGF, SILLocation loc,
+                    const CalleeTypeInfo &calleeTypeInfo)
+      : SGF(SGF), loc(loc), calleeTypeInfo(calleeTypeInfo),
+        // We reverse the order so we can pop values off the back.
+        allResults(reversed(calleeTypeInfo.substFnType->getResults())) {}
+    
+    
+    {    auto I = std::lower_bound(Children.begin(), Children.end(), Idx,
+                              [](IndexTrieNode *a, unsigned i) {
+                                return a->Index < i;
+                              });
+    if (I != Children.end() && (*I)->Index == Idx)
+      return *I;
+    auto *N = new IndexTrieNode(Idx, this);
+    Children.insert(I, N);
+    return N;
+  }
+    
+    
+    {} // end namespace swift
+    
+    
+    {  RenderThread::Get()->Send(new ShellViewHostMsg_Call_Object_Method(
+      routing_id,
+      object_id,
+      type,
+      method,
+      *static_cast<base::ListValue*>(value_args.get())));
+  return v8::Undefined(isolate);
+}
+    
+     private:
+  void SetText(std::string& text);
+  std::string GetText();
+  void Clear();
+    
+    #endif //CONTENT_NW_SRC_API_EVENT_EVENT_H_
+
+    
+    typedef std::map<std::string,std::string> KeyMap;
+    
+    bool NwObjCallObjectMethodFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  base::ListValue* arguments = nullptr;
+  int id = 0;
+  std::string type, method;
+  EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(0, &id));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(1, &type));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(2, &method));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetList(3, &arguments));
+    }
+    
+    
+    {  DECLARE_EXTENSION_FUNCTION('nw.Obj.callObjectMethodAsync', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwObjCallObjectMethodAsyncFunction);
+};
+    
+        NwDesktopCaptureMonitor();
+    void Start(bool screens, bool windows);
+    void Stop();
+    bool IsStarted();
+    
+    #include <memory>
+    
+    
+    {  return 0;
+}
+
+    
+    static void RunSynchronousUnaryPingPong() {
+  gpr_log(GPR_INFO, 'Running Synchronous Unary Ping Pong');
+    }
+    
+    // A simple parser for the json db file. It requires the db file to have the
+// exact form of [{'location': { 'latitude': 123, 'longitude': 456}, 'name':
+// 'the name can be empty' }, { ... } ... The spaces will be stripped.
+class Parser {
+ public:
+  explicit Parser(const std::string& db) : db_(db) {
+    // Remove all spaces.
+    db_.erase(
+        std::remove_if(db_.begin(), db_.end(), isspace),
+        db_.end());
+    if (!Match('[')) {
+      SetFailedAndReturnFalse();
+    }
+  }
+    }
+    
+    #include 'src/compiler/python_generator.h'
+#include 'src/compiler/schema_interface.h'
+    
+      static double Now();
+    
+    TEST_F(MockEnvTest, FakeSleeping) {
+  int64_t now = 0;
+  auto s = env_->GetCurrentTime(&now);
+  ASSERT_OK(s);
+  env_->FakeSleepForMicroseconds(3 * 1000 * 1000);
+  int64_t after_sleep = 0;
+  s = env_->GetCurrentTime(&after_sleep);
+  ASSERT_OK(s);
+  auto delta = after_sleep - now;
+  // this will be true unless test runs for 2 seconds
+  ASSERT_TRUE(delta == 3 || delta == 4);
+}
+    
+      virtual bool Merge(const Slice& key,
+                     const Slice* existing_value,
+                     const Slice& value,
+                     std::string* new_value,
+                     Logger* logger) const override;
+    
+    JNIEnv* JniCallback::getJniEnv(jboolean* attached) const {
+  return JniUtil::getJniEnv(m_jvm, attached);
+}
+    
+    void SyncPoint::ClearAllCallBacks() {
+  impl_->ClearAllCallBacks();
+}
+    
+      // Insert key into the collection. (The caller will pack key and value into a
+  // single buffer and pass that in as the parameter to Insert).
+  // REQUIRES: nothing that compares equal to key is currently in the
+  // collection, and no concurrent modifications to the table in progress
+  virtual void Insert(KeyHandle handle) = 0;
+    
+      std::mutex              mutex_;
+  std::condition_variable cv_;
+  // sync points that have been passed through
+  std::unordered_set<std::string> cleared_points_;
+  std::atomic<bool> enabled_;
+  int num_callbacks_running_ = 0;
+    
+      pthread_key_t k = TlsAlloc();
+  if (TLS_OUT_OF_INDEXES == k) {
+    return ENOMEM;
+  }
+    
+      // BlobDB specific stats
+  // # of Put/PutTTL/PutUntil to BlobDB.
+  BLOB_DB_NUM_PUT,
+  // # of Write to BlobDB.
+  BLOB_DB_NUM_WRITE,
+  // # of Get to BlobDB.
+  BLOB_DB_NUM_GET,
+  // # of MultiGet to BlobDB.
+  BLOB_DB_NUM_MULTIGET,
+  // # of Seek/SeekToFirst/SeekToLast/SeekForPrev to BlobDB iterator.
+  BLOB_DB_NUM_SEEK,
+  // # of Next to BlobDB iterator.
+  BLOB_DB_NUM_NEXT,
+  // # of Prev to BlobDB iterator.
+  BLOB_DB_NUM_PREV,
+  // # of keys written to BlobDB.
+  BLOB_DB_NUM_KEYS_WRITTEN,
+  // # of keys read from BlobDB.
+  BLOB_DB_NUM_KEYS_READ,
+  // # of bytes (key + value) written to BlobDB.
+  BLOB_DB_BYTES_WRITTEN,
+  // # of bytes (keys + value) read from BlobDB.
+  BLOB_DB_BYTES_READ,
+  // # of keys written by BlobDB as non-TTL inlined value.
+  BLOB_DB_WRITE_INLINED,
+  // # of keys written by BlobDB as TTL inlined value.
+  BLOB_DB_WRITE_INLINED_TTL,
+  // # of keys written by BlobDB as non-TTL blob value.
+  BLOB_DB_WRITE_BLOB,
+  // # of keys written by BlobDB as TTL blob value.
+  BLOB_DB_WRITE_BLOB_TTL,
+  // # of bytes written to blob file.
+  BLOB_DB_BLOB_FILE_BYTES_WRITTEN,
+  // # of bytes read from blob file.
+  BLOB_DB_BLOB_FILE_BYTES_READ,
+  // # of times a blob files being synced.
+  BLOB_DB_BLOB_FILE_SYNCED,
+  // # of blob index evicted from base DB by BlobDB compaction filter because
+  // of expiration.
+  BLOB_DB_BLOB_INDEX_EXPIRED_COUNT,
+  // size of blob index evicted from base DB by BlobDB compaction filter
+  // because of expiration.
+  BLOB_DB_BLOB_INDEX_EXPIRED_SIZE,
+  // # of blob index evicted from base DB by BlobDB compaction filter because
+  // of corresponding file deleted.
+  BLOB_DB_BLOB_INDEX_EVICTED_COUNT,
+  // size of blob index evicted from base DB by BlobDB compaction filter
+  // because of corresponding file deleted.
+  BLOB_DB_BLOB_INDEX_EVICTED_SIZE,
+  // # of blob files being garbage collected.
+  BLOB_DB_GC_NUM_FILES,
+  // # of blob files generated by garbage collection.
+  BLOB_DB_GC_NUM_NEW_FILES,
+  // # of BlobDB garbage collection failures.
+  BLOB_DB_GC_FAILURES,
+  // # of keys drop by BlobDB garbage collection because they had been
+  // overwritten.
+  BLOB_DB_GC_NUM_KEYS_OVERWRITTEN,
+  // # of keys drop by BlobDB garbage collection because of expiration.
+  BLOB_DB_GC_NUM_KEYS_EXPIRED,
+  // # of keys relocated to new blob file by garbage collection.
+  BLOB_DB_GC_NUM_KEYS_RELOCATED,
+  // # of bytes drop by BlobDB garbage collection because they had been
+  // overwritten.
+  BLOB_DB_GC_BYTES_OVERWRITTEN,
+  // # of bytes drop by BlobDB garbage collection because of expiration.
+  BLOB_DB_GC_BYTES_EXPIRED,
+  // # of bytes relocated to new blob file by garbage collection.
+  BLOB_DB_GC_BYTES_RELOCATED,
+  // # of blob files evicted because of BlobDB is full.
+  BLOB_DB_FIFO_NUM_FILES_EVICTED,
+  // # of keys in the blob files evicted because of BlobDB is full.
+  BLOB_DB_FIFO_NUM_KEYS_EVICTED,
+  // # of bytes in the blob files evicted because of BlobDB is full.
+  BLOB_DB_FIFO_BYTES_EVICTED,
+    
+    size_t HistogramBucketMapper::IndexForValue(const uint64_t value) const {
+  if (value >= maxBucketValue_) {
+    return bucketValues_.size() - 1;
+  } else if ( value >= minBucketValue_ ) {
+    std::map<uint64_t, uint64_t>::const_iterator lowerBound =
+      valueIndexMap_.lower_bound(value);
+    if (lowerBound != valueIndexMap_.end()) {
+      return static_cast<size_t>(lowerBound->second);
+    } else {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
+}
+    
+    // See portability/Unistd.h for why these need to be in a namespace
+// rather then extern 'C'.
+namespace folly {
+namespace portability {
+namespace fcntl {
+int creat(char const* fn, int pm);
+int fcntl(int fd, int cmd, ...);
+int posix_fallocate(int fd, off_t offset, off_t len);
+int open(char const* fn, int of, int pm = 0);
+}
+}
+}
+    
+      folly::Optional<T> try_take_for(std::chrono::milliseconds time) override {
+    T item;
+    while (true) {
+      if (nonBlockingTake(item)) {
+        return std::move(item);
+      }
+      if (!sem_.try_wait_for(time)) {
+        return folly::none;
+      }
+    }
+  }
+    
+    namespace folly {
+    }
+    
+      /**
+   * Are notifications for one pipe to/from child enabled?  Throws if the
+   * childFd is not known.
+   */
+  bool notificationsEnabled(int childFd) const;
+    
+      ControlBlock* allocateControlBlock();
+  void freeControlBlock(ControlBlock* b);
