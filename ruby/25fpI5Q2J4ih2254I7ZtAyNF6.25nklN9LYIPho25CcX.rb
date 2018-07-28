da966c6ba96795d4a15dfa7bc609895ca775006d
SUNsung <weatherius@gmail.com>
2018-07-28 11:00:55 +0300

    Rewrite [25fpI5Q2J4ih2254I7ZtAyNF6.25nklN9LYIPho25CcX.rb] File in: 2018-07-28 11:00:55

--- ruby/25fpI5Q2J4ih2254I7ZtAyNF6.25nklN9LYIPho25CcX.rb
+++ ruby/25fpI5Q2J4ih2254I7ZtAyNF6.25nklN9LYIPho25CcX.rb
@@ -1,139 +1,43 @@
 
         
-              def self.available_options
-        [
-          FastlaneCore::ConfigItem.new(key: :name,
-                                       env_name: 'KEYCHAIN_NAME',
-                                       description: 'Keychain name',
-                                       conflicting_options: [:path],
-                                       is_string: true,
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :path,
-                                       env_name: 'KEYCHAIN_PATH',
-                                       description: 'Path to keychain',
-                                       is_string: true,
-                                       conflicting_options: [:name],
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :password,
-                                       env_name: 'KEYCHAIN_PASSWORD',
-                                       description: 'Password for the keychain',
-                                       sensitive: true,
-                                       optional: false),
-          FastlaneCore::ConfigItem.new(key: :default_keychain,
-                                       description: 'Should the newly created Keychain be the new system default keychain',
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :unlock,
-                                       description: 'Unlock keychain after create',
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :timeout,
-                                       description: 'timeout interval in seconds. Set `false` if you want to specify 'no time-out'',
-                                       is_string: false,
-                                       default_value: 300),
-          FastlaneCore::ConfigItem.new(key: :lock_when_sleeps,
-                                       description: 'Lock keychain when the system sleeps',
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :lock_after_timeout,
-                                       description: 'Lock keychain after timeout interval',
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :add_to_search_list,
-                                       description: 'Add keychain to search list',
-                                       is_string: false,
-                                       default_value: true)
-        ]
-      end
+            used_formulae_missing = false
+    used_formulae = begin
+      ARGV.formulae
+    rescue FormulaUnavailableError => e
+      opoo e
+      used_formulae_missing = true
+      # If the formula doesn't exist: fake the needed formula object name.
+      ARGV.named.map { |name| OpenStruct.new name: name, full_name: name }
+    end
     
-            expect(result).to eq('/usr/local/bin/cloc --exclude-dir=test1,test2,build --by-file --xml  --out=build/cloc.xml')
-      end
-    
-      private
-    
-    if $PROGRAM_NAME == __FILE__ && !ENV['COCOAPODS_NO_BUNDLER']
-  ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
-  require 'rubygems'
-  require 'bundler/setup'
-  $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-elsif ENV['COCOAPODS_NO_BUNDLER']
-  require 'rubygems'
-  gem 'cocoapods'
+      lenratio = (100 * secure_details[:file].length / details[:file].length).to_i
+  return unless (90..110).cover?(lenratio)
+  'The URL #{url} may be able to use HTTPS rather than HTTP. Please verify it in a browser.'
 end
     
-          def run
-        UI.puts report
-      end
-    
-        def register(klass, attachment_name, attachment_options)
-      @attachments ||= {}
-      @attachments[klass] ||= {}
-      @attachments[klass][attachment_name] = attachment_options
-    end
-    
-            def matches? subject
-          @subject = subject
-          @subject = @subject.new if @subject.class == Class
-          lower_than_low? && higher_than_low? && lower_than_high? && higher_than_high?
+              desc_length = '#{@formula_name}: #{string_content(desc)}'.length
+          max_desc_length = 80
+          return if desc_length <= max_desc_length
+          problem 'Description is too long. \'name: desc\' should be less than #{max_desc_length} characters. ' \
+                  'Length is calculated as #{@formula_name} + desc. (currently #{desc_length})'
         end
-    
-    
-    {  # Returns hash with styles missing from recent run of rake paperclip:refresh:missing_styles
-  #   {
-  #     :User => {:avatar => [:big]},
-  #     :Book => {
-  #       :cover => [:croppable]},
-  #     }
-  #   }
-  def self.missing_attachments_styles
-    current_styles = current_attachments_styles
-    registered_styles = get_registered_attachments_styles
-    
-        def self.included(base)
-      ActiveRecord::ConnectionAdapters::Table.send :include, TableDefinition
-      ActiveRecord::ConnectionAdapters::TableDefinition.send :include, TableDefinition
-      ActiveRecord::ConnectionAdapters::AbstractAdapter.send :include, Statements
-      ActiveRecord::Migration::CommandRecorder.send :include, CommandRecorder
-    end
-    
       end
-    
-    # Support for self extracting sh files (.sh files)
-#
-# This class only supports output of packages.
-#
-# The sh package is a single sh file with a tar payload concatenated to the end.
-# The script can unpack the tarball to install it and call optional post install scripts.
-class FPM::Package::Sh < FPM::Package
-    
-      def default_output
-    v = version
-    v = '#{epoch}:#{v}' if epoch
-    if iteration
-      '#{name}_#{v}-#{iteration}_#{architecture}.#{type}'
-    else
-      '#{name}_#{v}_#{architecture}.#{type}'
     end
-  end # def default_output
-end # class FPM::Deb
     
-        task(options.name) do |_, task_args|
-      block.call(*[options, task_args].first(block.arity)) if block_given?
-      abort('Must specify args') unless options.respond_to?(:args)
-      @args = options.delete_field(:args)
-      run_cli
+        on :update_pod do |url, status|
+      pod = Pod.find_or_create_by(url: url)
+    
+        it 'removes text formatting markdown from post text' do
+      message = double(urls: [])
+      expect(message).to receive(:plain_text_without_markdown).and_return('')
+      post = double(message: message, photos: [])
+      post_params = @service.create_post_params(post)
     end
-  end
     
-          original = FPM::Package::Sh.new
-      yield original if block_given?
-      original.output(target)
-    
-      subject do
-    source = FPM::Package::Gem.new
-    prefix = source.attributes[:gem_package_name_prefix ] = 'rubygem19'
-    name = source.name = 'whatever'
-    version = source.version = '1.0'
-    source.provides << '#{prefix}-#{name} = #{version}'
-    source.convert(FPM::Package::RPM)
-  end
+          def handle_params_error(error, error_description)
+        if params[:client_id] && params[:redirect_uri]
+          handle_params_error_when_client_id_and_redirect_uri_exists(error, error_description)
+        else
+          render_error I18n.t('api.openid_connect.error_page.could_not_authorize'), error_description
+        end
+      end
