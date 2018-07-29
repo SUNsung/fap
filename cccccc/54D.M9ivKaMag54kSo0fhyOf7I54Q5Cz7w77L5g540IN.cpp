07643904478984ffb69b9c88f94cc3f818e35c0a
SUNsung <weatherius@gmail.com>
2018-07-29 07:37:23 +0300

    Rewrite [54D.M9ivKaMag54kSo0fhyOf7I54Q5Cz7w77L5g540IN.cpp] File in: 2018-07-29 07:37:22

--- cccccc/54D.M9ivKaMag54kSo0fhyOf7I54Q5Cz7w77L5g540IN.cpp
+++ cccccc/54D.M9ivKaMag54kSo0fhyOf7I54Q5Cz7w77L5g540IN.cpp
@@ -1,257 +1,124 @@
-#include <vector>
+
+        
+          DebuggerClient(ASTContext &C) : Ctx(C) { }
+  virtual ~DebuggerClient() = default;
+  
+  // DebuggerClient is consulted at the beginning of the parsing
+  // of various DeclKinds to see whether the decl should be parsed
+  // in the global context rather than the current context.
+  // This question will only be asked if the decl's current context
+  // is a function marked with the LLDBDebuggerFunction attribute.
+  virtual bool shouldGlobalize(Identifier Name, DeclKind kind) = 0;
+  
+  virtual void didGlobalize (Decl *Decl) = 0;
     
-    /**
- * @brief Index into the input blob along its first axis.
- *
- * This layer can be used to select, reorder, and even replicate examples in a
- * batch.  The second blob is cast to int and treated as an index into the
- * first axis of the first blob.
- */
-template <typename Dtype>
-class BatchReindexLayer : public Layer<Dtype> {
- public:
-  explicit BatchReindexLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
+    #pragma mark - NSCalendar verification
+    
+      size_t NumChildren;
+  bool Ordered;
     
     
-    {  size_t *workspace_fwd_sizes_;
-  size_t *workspace_bwd_data_sizes_;
-  size_t *workspace_bwd_filter_sizes_;
-  size_t workspaceSizeInBytes;  // size of underlying storage
-  void *workspaceData;  // underlying storage
-  void **workspace;  // aliases into workspaceData
+    {  /// Return a hash code of any components from these options that should
+  /// contribute to a Swift Bridging PCH hash.
+  llvm::hash_code getPCHHashComponents() const {
+    // Nothing here that contributes anything significant when emitting the PCH.
+    return llvm::hash_value(0);
+  }
 };
+    
+    /// Runs the given function with the given context argument exactly once.
+/// The predicate argument must point to a global or static variable of static
+/// extent of type swift_once_t.
+void swift::swift_once(swift_once_t *predicate, void (*fn)(void *),
+                       void *context) {
+#if defined(__APPLE__)
+  dispatch_once_f(predicate, context, fn);
+#elif defined(__CYGWIN__)
+  _swift_once_f(predicate, context, fn);
+#else
+  std::call_once(*predicate, [fn, context]() { fn(context); });
 #endif
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    #endif  // CAFFE_DECONV_LAYER_HPP_
+}
 
     
-     protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    namespace caffe {
+    namespace index {
     }
     
-    /**
- * @brief A layer for learning 'embeddings' of one-hot vector input.
- *        Equivalent to an InnerProductLayer with one-hot vectors as input, but
- *        for efficiency the input is the 'hot' index of each column itself.
- *
- * TODO(dox): thorough documentation for Forward, Backward, and proto params.
- */
-template <typename Dtype>
-class EmbedLayer : public Layer<Dtype> {
+      explicit IndexTrieNode(unsigned V, IndexTrieNode *P): Index(V), Parent(P) {}
+    
+      /// Compare payload only, not comments etc.
+  bool operator<(const Value& other) const;
+  bool operator<=(const Value& other) const;
+  bool operator>=(const Value& other) const;
+  bool operator>(const Value& other) const;
+  bool operator==(const Value& other) const;
+  bool operator!=(const Value& other) const;
+  int compare(const Value& other) const;
+    
+    #include <google/protobuf/compiler/command_line_interface.h>
+#include <google/protobuf/compiler/csharp/csharp_helpers.h>
+#include <google/protobuf/io/zero_copy_stream.h>
+#include <google/protobuf/io/printer.h>
+    
+    RepeatedEnumFieldGenerator::RepeatedEnumFieldGenerator(
+    const FieldDescriptor* descriptor, int fieldOrdinal, const Options *options)
+    : FieldGeneratorBase(descriptor, fieldOrdinal, options) {
+}
+    
+      virtual void GenerateCloningCode(io::Printer* printer);
+  virtual void GenerateFreezingCode(io::Printer* printer);
+  virtual void GenerateMembers(io::Printer* printer);
+  virtual void GenerateMergingCode(io::Printer* printer);
+  virtual void GenerateParsingCode(io::Printer* printer);
+  virtual void GenerateSerializationCode(io::Printer* printer);
+  virtual void GenerateSerializedSizeCode(io::Printer* printer);
+    
+    
+    
+    
+    {  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(SourceGeneratorBase);
+};
+    
+      // Returns an estimate of the number of bytes the printed code will compile to
+  virtual int GenerateNonNestedInitializationCode(io::Printer* printer);
+    
+      virtual MessageGenerator* NewMessageGenerator(
+      const Descriptor* descriptor) const;
+    
+    
+    {  // Pick up remaining bytes
+  switch (limit - data) {
+    case 3:
+      h += static_cast<unsigned char>(data[2]) << 16;
+      FALLTHROUGH_INTENDED;
+    case 2:
+      h += static_cast<unsigned char>(data[1]) << 8;
+      FALLTHROUGH_INTENDED;
+    case 1:
+      h += static_cast<unsigned char>(data[0]);
+      h *= m;
+      h ^= (h >> r);
+      break;
+  }
+  return h;
+}
+    
+    class SCOPED_LOCKABLE MutexLock {
  public:
-  explicit EmbedLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-      vector<int64_t> sizes = {2, 2};
-  tensor2->resize(sizes);
-  tensor2->fill(4);
-  tensor->add(*tensor2, 1);
-  assert(tensor->nDim() == 2);
-    
-      bool init() override;
-  void destroy() override;
-    
-    static PyObject* recursive_to_list(
-    char* data, IntList sizes, IntList strides, int64_t dim,
-    ScalarType scalarType, int64_t elementSize)
-{
-  int64_t ndim = sizes.size();
-  if (dim == ndim) {
-    return torch::utils::load_scalar(data, scalarType);
+  explicit MutexLock(port::Mutex *mu) EXCLUSIVE_LOCK_FUNCTION(mu)
+      : mu_(mu)  {
+    this->mu_->Lock();
   }
-  auto n = sizes[dim];
-  auto list = THPObjectPtr(PyList_New(n));
-  if (!list) throw python_error();
-  for (int64_t i = 0; i < n; i++) {
-    PyObject* obj = recursive_to_list(data, sizes, strides, dim + 1, scalarType, elementSize);
-    if (!obj) throw python_error();
-    PyList_SET_ITEM(list.get(), i, obj);
-    data += strides[dim] * elementSize;
+  ~MutexLock() UNLOCK_FUNCTION() { this->mu_->Unlock(); }
+    }
+    
+      // Delete everything
+  for (int i = 0; i < kCount; i++) {
+    ASSERT_OK(db_->Delete(WriteOptions(), Key(i)));
   }
-  return list.release();
-}
+  ASSERT_OK(dbi->TEST_CompactMemTable());
     
-    #include 'generic/Storage.h'
-#include <TH/THGenerateAllTypes.h>
-    
-    using THDGeneratorTypes = std::tuple<THDGenerator>;
-    
-      THDTensor *ra_ = THDTensor_(cloneColumnMajor)(ra, a);
-  THDIntTensor_resize1d(rpiv, n);
-    
-    void THDTensor_(bernoulli)(THDTensor *self, THDGenerator *_generator, double p) {
-  masterCommandChannel->sendMessage(
-    packMessage(Functions::tensorBernoulli, self, _generator, p),
-    THDState::s_current_worker
-  );
-}
-    
-    void THP_decodeDoubleBuffer(double* dst, const uint8_t* src, THPByteOrder order, size_t len)
-{
-  for (size_t i = 0; i < len; i++) {
-    union { uint64_t x; double d; };
-    x = (order == THP_BIG_ENDIAN ? decodeUInt64BE(src) : decodeUInt64LE(src));
-    dst[i] = d;
-    src += sizeof(double);
-  }
-}
-    
-    
-    {
-}  // namespace routeguide
-    
-    std::string GetDbFileContent(int argc, char** argv);
-    
-    // Get leading or trailing comments in a string. Comment lines start with '// '.
-// Leading detached comments are put in in front of leading comments.
-template <typename DescriptorType>
-inline grpc::string GetNodeComments(const DescriptorType* desc, bool leading) {
-  return grpc_generator::GetPrefixedComments(desc, leading, '//');
-}
-    
-    namespace grpc {
-namespace {
-    }
-    }
-    
-    namespace grpc {
-namespace testing {
-    }
-    }
-    
-    	ClassDB::bind_method(D_METHOD('poll'), &StreamPeerSSL::poll);
-	ClassDB::bind_method(D_METHOD('accept_stream', 'stream'), &StreamPeerSSL::accept_stream);
-	ClassDB::bind_method(D_METHOD('connect_to_stream', 'stream', 'validate_certs', 'for_hostname'), &StreamPeerSSL::connect_to_stream, DEFVAL(false), DEFVAL(String()));
-	ClassDB::bind_method(D_METHOD('get_status'), &StreamPeerSSL::get_status);
-	ClassDB::bind_method(D_METHOD('disconnect_from_stream'), &StreamPeerSSL::disconnect_from_stream);
-    
-    class StreamPeerSSL : public StreamPeer {
-	GDCLASS(StreamPeerSSL, StreamPeer);
-    }
-    
-    #endif // STREAM_PEER_SSL_H
+    #endif  // STORAGE_LEVELDB_DB_LOG_WRITER_H_
 
     
-    void WebSocketClient::_bind_methods() {
-	ClassDB::bind_method(D_METHOD('connect_to_url', 'url', 'protocols', 'gd_mp_api'), &WebSocketClient::connect_to_url, DEFVAL(PoolVector<String>()), DEFVAL(false));
-	ClassDB::bind_method(D_METHOD('disconnect_from_host'), &WebSocketClient::disconnect_from_host);
-	ClassDB::bind_method(D_METHOD('set_verify_ssl_enabled', 'enabled'), &WebSocketClient::set_verify_ssl_enabled);
-	ClassDB::bind_method(D_METHOD('is_verify_ssl_enabled'), &WebSocketClient::is_verify_ssl_enabled);
-    }
-    
-    	_FORCE_INLINE_ void set_load_once(bool p_load_once) {
-		load_once = p_load_once;
-	}
-	_FORCE_INLINE_ void set_singleton(bool p_singleton) {
-		singleton = p_singleton;
-	}
-	_FORCE_INLINE_ void set_symbol_prefix(String p_symbol_prefix) {
-		symbol_prefix = p_symbol_prefix;
-	}
-    
-    // ---------------------------------------------------------------------------
-// SGDParams -- parameters for SGD
-//
-// TODO: This should keep everything that is configured by the config.
-//       Currently it does not store which matrices are used.
-// ---------------------------------------------------------------------------
-    
-        // insert the node in the network
-    AddNodeToNet(newNode);
-    
-    
-    {    // Save the data into this section,
-    virtual bool SaveData(size_t recordStart, const std::map<std::wstring, void*, nocase_compare>& matrices, size_t numRecords, size_t datasetSize, size_t byteVariableSized);
-};
-    
-    
-    {    cudaMemcpyAsync(cpuBuffer, gpuBuffer, numElements * elementSize, cudaMemcpyDeviceToHost, GetFetchStream()) || 'cudaMemcpyAsync failed';
-}
-    
-        virtual void GetSections(std::map<std::wstring, SectionType, nocase_compare>& sections);
-    
-    // If the Tracing flag is set, print out a timestamp with no new line at the end
-#define PREPENDTS(stream) \
-    do \
-    { \
-        if (ProgressTracing::GetTimestampingFlag()) \
-        { \
-            char mbstr[30]; \
-            fprintf(stream, '%s: ', ProgressTracing::Timestamp(mbstr));  \
-        } \
-    } while(0)
-    
-        // prefetch a float4 from an address
-    static void prefetch(const float4* p)
-    {
-        _mm_prefetch((const char*) const_cast<float4*>(p), _MM_HINT_T0);
-    }
-    
-    #include 'ComputationNode.h'
-#include 'InputAndParamNodes.h'
-#include 'ComputationNetworkBuilder.h' // TODO: We should only pull in NewComputationNodeFromConfig(). Nodes should not know about network at large.
-#include 'TensorShape.h'
-    
-    
-    {  bool cas(T& u, T& v) {
-    Node* n = new Node(v);
-    hazptr_holder<Atom> hptr;
-    Node* p;
-    while (true) {
-      p = hptr.get_protected(node_);
-      if (p->val_ != u) {
-        delete n;
-        return false;
-      }
-      if (node_.compare_exchange_weak(
-              p, n, std::memory_order_relaxed, std::memory_order_release)) {
-        break;
-      }
-    }
-    hptr.reset();
-    p->retire();
-    return true;
-  }
-};
-    
-    #include <fcntl.h>
-    
-      folly::Optional<T> try_take_for(std::chrono::milliseconds time) override {
-    T item;
-    while (!queue_.try_dequeue(item)) {
-      if (!sem_.try_wait_for(time)) {
-        return folly::none;
-      }
-    }
-    return std::move(item);
-  }
-    
-       private:
-    friend class SerialExecutor;
-    explicit Deleter(std::shared_ptr<Executor> parent)
-        : parent_(std::move(parent)) {}
-    
-      EXPECT_EQ(5050, estimates.sum);
-  EXPECT_EQ(100, estimates.count);
+      ~Block();
