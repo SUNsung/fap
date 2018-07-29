a646890a3daf6810c7cc4c7f71985375f18289a9
SUNsung <weatherius@gmail.com>
2018-07-29 11:22:51 +0300

    Rewrite [04yHUL5UdTEwA04YAK7VC2XmuY04Iy3dgE1vcc04xn..py] File in: 2018-07-29 11:22:50

--- pyton/04yHUL5UdTEwA04YAK7VC2XmuY04Iy3dgE1vcc04xn..py
+++ pyton/04yHUL5UdTEwA04YAK7VC2XmuY04Iy3dgE1vcc04xn..py
@@ -1,54 +1,128 @@
 
         
-        from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
+            def __init__(self):
+        Resource.__init__(self)
+        self.concurrent = 0
+        self.tail = deque(maxlen=100)
+        self._reset_stats()
     
-        results = benchmark([METRICS[k] for k in args.metrics],
-                        [FORMATS[k] for k in args.formats],
-                        args.samples, args.classes, args.density,
-                        args.n_times)
+    from twisted import version as _txv
+twisted_version = (_txv.major, _txv.minor, _txv.micro)
     
-        plt.figure('scikit-learn parallel %s benchmark results' % func.__name__)
-    plt.plot(sample_sizes, one_core, label='one core')
-    plt.plot(sample_sizes, multi_core, label='multi core')
-    plt.xlabel('n_samples')
-    plt.ylabel('Time (s)')
-    plt.title('Parallel %s' % func.__name__)
-    plt.legend()
     
-        print('Dataset statics')
-    print('===========================')
-    print('n_samples \t= %s' % opts.n_samples)
-    print('n_features \t= %s' % opts.n_features)
-    if opts.n_components == 'auto':
-        print('n_components \t= %s (auto)' %
-              johnson_lindenstrauss_min_dim(n_samples=opts.n_samples,
-                                            eps=opts.eps))
-    else:
-        print('n_components \t= %s' % opts.n_components)
-    print('n_elements \t= %s' % (opts.n_features * opts.n_samples))
-    print('n_nonzeros \t= %s per feature' % n_nonzeros)
-    print('ratio_nonzeros \t= %s' % opts.ratio_nonzeros)
-    print('')
-    
+# contracts
+class UrlContract(Contract):
+    ''' Contract to set the url of the request (mandatory)
+        @url http://scrapy.org
     '''
-# Author: Olivier Grisel <olivier.grisel@ensta.org>
-# License: Simplified BSD
     
-    # Author: Kemal Eren <kemal@kemaleren.com>
-# License: BSD 3 clause
+        class _v19_S3Connection(S3Connection):
+        '''A dummy S3Connection wrapper that doesn't do any synchronous download'''
+        def _mexe(self, method, bucket, key, headers, *args, **kwargs):
+            return headers
     
-    # Author: Kemal Eren <kemal@kemaleren.com>
-# License: BSD 3 clause
+        def process_request(self, request, spider):
+        auth = getattr(self, 'auth', None)
+        if auth and b'Authorization' not in request.headers:
+            request.headers[b'Authorization'] = auth
+
     
-    ax = plt.axes([0.15, 0.04, 0.7, 0.05])
-plt.title('Probability')
-plt.colorbar(imshow_handle, cax=ax, orientation='horizontal')
+            # put all lines in the file into a Python list
+        strings = f.readlines()
+        
+        # above line leaves trailing newline characters; strip them out
+        strings = [x.strip(u'\n') for x in strings]
+        
+        # remove empty-lines and comments
+        strings = [x for x in strings if x and not x.startswith(u'#')]
+        
+        # insert empty string since all are being removed
+        strings.insert(0, u'')
     
-        Only one feature contains discriminative information, the other features
-    contain only noise.
-    '''
-    X, y = make_blobs(n_samples=n_samples, n_features=1, centers=[[-2], [2]])
+            ret = []
+        for i in range(len(string) / 2):
+            (n, v) = ord(string[i*2]), ord(string[i*2+1])
+    
+                if point.name == 'p':
+                link = point.find('a')
+                if link is not None:
+                    link = clean_pdf_link(link.attrs['href'])
+                    ext = get_extension(link)
+                    print(ext)
+                    if not ext in forbidden_extensions:
+                        print(shorten_title(point.text) + ' (' + link + ')')
+                        try:
+                            name = clean_text(point.text.split('[' + ext + ']')[0])
+                            fullname = '.'.join((name, ext))
+                            if not os.path.exists('/'.join((current_directory, fullname)) ):
+                                download_pdf(link, current_directory, '.'.join((name, ext)))
+                        except KeyboardInterrupt:
+                            try:
+                                print('Press Ctrl-C in 1 second to quit')
+                                time.sleep(1)
+                            except KeyboardInterrupt:
+                                print('Cancelling..')
+                                break
+                        except:
+                            failures.append(point.text)
+                        
+        point = point.next_sibling          
+    
+            for future in as_completed(future_to_url):
+            try:
+                url_to_content[future_to_url[future]] = future.result()
+            except:
+                pass
+        return url_to_content
+    finally:
+        executor.shutdown()
+    
+    def sequential():
+    return list(map(is_prime, PRIMES))
+    
+        vim_eval.assert_has_exact_calls( [
+      call( 'setqflist( {0} )'.format( json.dumps( expected_qf_list ) ) )
+    ] )
+    vim_command.assert_has_exact_calls( [
+      call( 'botright copen' ),
+      call( 'au WinLeave <buffer> q' ),
+      call( 'doautocmd User YcmQuickFixOpened' )
+    ] )
+    set_fitting_height.assert_called_once_with()
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    
+  @staticmethod
+  def CreateFromOptions( user_options ):
+    all_filters = dict( user_options.get( 'filter_diagnostics', {} ) )
+    compiled_by_type = {}
+    for type_spec, filter_value in iteritems( dict( all_filters ) ):
+      filetypes = [ type_spec ]
+      if type_spec.find( ',' ) != -1:
+        filetypes = type_spec.split( ',' )
+      for filetype in filetypes:
+        compiled_by_type[ filetype ] = _CompileFilters( filter_value )
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+      if 'word' in vim_data:
+    completion_data[ 'insertion_text' ] = vim_data[ 'word' ]
+  if 'abbr' in vim_data:
+    completion_data[ 'menu_text' ] = vim_data[ 'abbr' ]
+  if 'menu' in vim_data:
+    completion_data[ 'extra_menu_info' ] = vim_data[ 'menu' ]
+  if 'kind' in vim_data:
+    completion_data[ 'kind' ] = [ vim_data[ 'kind' ] ]
+  if 'info' in vim_data:
+    completion_data[ 'detailed_info' ] = vim_data[ 'info' ]
