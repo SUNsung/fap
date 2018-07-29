c5b53b7a6365f4c701dbb00775ff7d74b12341f1
SUNsung <weatherius@gmail.com>
2018-07-29 20:59:15 +0300

    Rewrite [44XO8jvXCh3Rw44XS0ptbIsNp244aLyaQc23Bek44WvB.cpp] File in: 2018-07-29 20:59:15

--- cccccc/44XO8jvXCh3Rw44XS0ptbIsNp244aLyaQc23Bek44WvB.cpp
+++ cccccc/44XO8jvXCh3Rw44XS0ptbIsNp244aLyaQc23Bek44WvB.cpp
@@ -1,70 +1,229 @@
 
         
-        template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6>
-internal::ValueArray6<T1, T2, T3, T4, T5, T6> Values(T1 v1, T2 v2, T3 v3,
-    T4 v4, T5 v5, T6 v6) {
-  return internal::ValueArray6<T1, T2, T3, T4, T5, T6>(v1, v2, v3, v4, v5, v6);
-}
+        #ifndef SWIFT_DEBUGGERCLIENT_H
+#define SWIFT_DEBUGGERCLIENT_H
     
-    namespace testing {
+    
+    {
+    {    if (SemanticNode.is<Expr *>()) {
+      SemanticNode.get<Expr *>()->dump(llvm::errs());
+    } else if (SemanticNode.is<Decl *>()) {
+      SemanticNode.get<Decl *>()->dump(llvm::errs());
+    } else if (SemanticNode.is<Expr *>()) {
+      SemanticNode.get<Expr *>()->dump(llvm::errs());
+    } else {
+      llvm_unreachable('ASTNode has pointer to unknown thing!');
+    }
+    llvm::errs() << '\n=====================================================\n';
+  }
+}
+
+    
+    namespace swift {
+namespace syntax {
+    }
     }
     
-    // A helper class for implementing EXPECT_FATAL_FAILURE() and
-// EXPECT_NONFATAL_FAILURE().  Its destructor verifies that the given
-// TestPartResultArray contains exactly one failure that has the given
-// type and contains the given substring.  If that's not the case, a
-// non-fatal failure will be generated.
-class GTEST_API_ SingleFailureChecker {
- public:
-  // The constructor remembers the arguments.
-  SingleFailureChecker(const TestPartResultArray* results,
-                       TestPartResult::Type type,
-                       const string& substr);
-  ~SingleFailureChecker();
- private:
-  const TestPartResultArray* const results_;
-  const TestPartResult::Type type_;
-  const string substr_;
-    }
+    #ifndef SWIFT_PRINTINGDIAGNOSTICCONSUMER_H
+#define SWIFT_PRINTINGDIAGNOSTICCONSUMER_H
     
-    #include <iosfwd>
-#include <vector>
-#include 'gtest/internal/gtest-internal.h'
-#include 'gtest/internal/gtest-string.h'
+    #ifndef SWIFT_RUNTIME_ONCE_H
+#define SWIFT_RUNTIME_ONCE_H
     
-    // Then, use TYPED_TEST_P() to define as many type-parameterized tests
-// for this type-parameterized test case as you want.
-TYPED_TEST_P(FooTest, DoesBlah) {
-  // Inside a test, refer to TypeParam to get the type parameter.
-  TypeParam n = 0;
-  ...
+    public:
+  enum Action {Skip, Abort, Continue};
+    
+    #ifndef SWIFT_SILGEN_RESULTPLAN_H
+#define SWIFT_SILGEN_RESULTPLAN_H
+    
+    
+    {  ASSERT_TRUE(message.ParseFromString(data));
+  EXPECT_TRUE(message.has_any_value());
+  ASSERT_TRUE(message.any_value().UnpackTo(&any));
+  ASSERT_TRUE(any.UnpackTo(&submessage));
+  EXPECT_EQ(12345, submessage.int32_value());
 }
     
-      template <typename T>
-  operator ParamGenerator<T>() const {
-    const T array[] = {static_cast<T>(v1_), static_cast<T>(v2_),
-        static_cast<T>(v3_), static_cast<T>(v4_), static_cast<T>(v5_),
-        static_cast<T>(v6_), static_cast<T>(v7_), static_cast<T>(v8_),
-        static_cast<T>(v9_), static_cast<T>(v10_), static_cast<T>(v11_),
-        static_cast<T>(v12_), static_cast<T>(v13_), static_cast<T>(v14_),
-        static_cast<T>(v15_), static_cast<T>(v16_), static_cast<T>(v17_),
-        static_cast<T>(v18_), static_cast<T>(v19_), static_cast<T>(v20_),
-        static_cast<T>(v21_), static_cast<T>(v22_), static_cast<T>(v23_),
-        static_cast<T>(v24_), static_cast<T>(v25_), static_cast<T>(v26_),
-        static_cast<T>(v27_), static_cast<T>(v28_), static_cast<T>(v29_),
-        static_cast<T>(v30_)};
-    return ValuesIn(array);
+    namespace google {
+namespace protobuf {
+namespace compiler {
+namespace cpp {
+    }
+    }
+    }
+    }
+    
+    #include <string>
+    
+    #include <google/protobuf/compiler/command_line_interface.h>
+#include <google/protobuf/compiler/csharp/csharp_helpers.h>
+#include <google/protobuf/io/zero_copy_stream.h>
+#include <google/protobuf/io/printer.h>
+    
+    #include <google/protobuf/compiler/code_generator.h>
+#include <google/protobuf/compiler/csharp/csharp_field_base.h>
+    
+    
+    {
+}  // namespace google
+#endif  // GOOGLE_PROTOBUF_COMPILER_CSHARP_OPTIONS_H__
+
+    
+    #ifndef GOOGLE_PROTOBUF_COMPILER_CSHARP_REPEATED_ENUM_FIELD_H__
+#define GOOGLE_PROTOBUF_COMPILER_CSHARP_REPEATED_ENUM_FIELD_H__
+    
+    // Exposed for testing only.
+LIBPROTOC_EXPORT string EscapeJavadoc(const string& input);
+    
+    
+    {}  // namespace google
+#endif  // GOOGLE_PROTOBUF_COMPILER_JAVA_GENERATOR_FACTORY_H__
+
+    
+    
+    {  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(ImmutableMessageGenerator);
+};
+    
+    #undef cv_hal_Cholesky32f
+#define cv_hal_Cholesky32f lapack_Cholesky32f
+#undef cv_hal_Cholesky64f
+#define cv_hal_Cholesky64f lapack_Cholesky64f
+    
+    #ifdef HAVE_CLAMDBLAS
+    
+    
+    {} // namespace mgpu
+
+    
+    
+    {    return ret;
+  }
+  /* \brief Get engine variable associated with this state */
+  engine::VarHandle get_var() const {
+    return ptr_->var;
+  }
+  /* \brief Get state of type T */
+  template<typename T>
+  T& get_state() const {
+    return *reinterpret_cast<T*>(ptr_->state);
+  }
+  /* \brief clear state */
+  void reset() {
+    ptr_.reset();
+  }
+  /* \brief checks whether the managed object is managed only by the current
+            OpStatePtr instance */
+  bool unique() const {
+    return ptr_.unique();
+  }
+  /* \brief Whether state is empty */
+  explicit operator bool() const {
+    return ptr_ ? true : false;
   }
     
-    // A sample program demonstrating using Google C++ testing framework.
-//
-// Author: wan@google.com (Zhanyong Wan)
     
-    // Tests positive input.
-TEST(IsPrimeTest, Positive) {
-  EXPECT_FALSE(IsPrime(4));
-  EXPECT_TRUE(IsPrime(5));
-  EXPECT_FALSE(IsPrime(6));
-  EXPECT_TRUE(IsPrime(23));
+    {
+    {}  // namespace exec
+}  // namespace mxnet
+#endif  // MXNET_EXECUTOR_GRAPH_EXECUTOR_H_
+
+    
+    /*
+ * Given a computation graph, this function finds the input nodes of the graph
+ * and create symbols for the input nodes. It returns the input symbols.
+ */
+std::vector<nnvm::Symbol *> GetInputSymbols(const nnvm::Symbol &sym) {
+  nnvm::Graph g;
+  std::vector<nnvm::Symbol *> input_syms;
+  g.outputs = sym.outputs;
+  const nnvm::IndexedGraph& idx = g.indexed_graph();
+  // Go through all nodes and return the ones representing variables.
+  for (size_t i = 0; i < idx.num_nodes(); i++) {
+    const nnvm::Node &n = *idx[i].source;
+    for (const nnvm::NodeEntry &e : n.inputs) {
+      auto p = e.node;
+      if (p->is_variable()) {
+        nnvm::Symbol *s = new nnvm::Symbol();
+        s->outputs.push_back(e);
+        input_syms.push_back(s);
+      }
+    }
+  }
+  return input_syms;
 }
+    
+    /*! \brief CSR -op- CSR binary operator for non-canonical NDArray */
+template<typename OP>
+void ElemwiseBinaryOp::CsrCsrOp(mshadow::Stream<gpu> *s,
+                                const nnvm::NodeAttrs &attrs,
+                                const OpContext &ctx,
+                                const NDArray &lhs,
+                                const NDArray &rhs,
+                                const OpReqType req,
+                                const NDArray &output) {
+  LOG(FATAL) << 'GPU not supported for CsrCsrOp';
+}
+    
+    /*!
+ * Copyright (c) 2015 by Contributors
+ * \file elemwise_sum.cc
+ * \brief elementwise sum operator
+*/
+#include './elemwise_sum.h'
+#include '../../ndarray/ndarray_function.h'
+#include '../nn/mkldnn/mkldnn_ops-inl.h'
+#include '../nn/mkldnn/mkldnn_base-inl.h'
+#include '../../common/utils.h'
+    
+    
+namespace mxnet {
+namespace op {
+    }
+    }
+    
+      class StringSource : public SequentialFile {
+   public:
+    Slice contents_;
+    bool force_error_;
+    bool returned_partial_;
+    StringSource() : force_error_(false), returned_partial_(false) { }
+    }
+    
+    class Issue178 { };
+    
+    BlockBuilder::BlockBuilder(const Options* options)
+    : options_(options),
+      restarts_(),
+      counter_(0),
+      finished_(false) {
+  assert(options->block_restart_interval >= 1);
+  restarts_.push_back(0);       // First restart point is at offset 0
+}
+    
+    // The FALLTHROUGH_INTENDED macro can be used to annotate implicit fall-through
+// between switch labels. The real definition should be provided externally.
+// This one is a fallback version for unsupported compilers.
+#ifndef FALLTHROUGH_INTENDED
+#define FALLTHROUGH_INTENDED do { } while (0)
+#endif
+    
+    #include <string>
+    
+    // Read through the first n keys repeatedly and check that they get
+// compacted (verified by checking the size of the key space).
+void AutoCompactTest::DoReads(int n) {
+  std::string value(kValueSize, 'x');
+  DBImpl* dbi = reinterpret_cast<DBImpl*>(db_);
+    }
+    
+        RecordType type;
+    const bool end = (left == fragment_length);
+    if (begin && end) {
+      type = kFullType;
+    } else if (begin) {
+      type = kFirstType;
+    } else if (end) {
+      type = kLastType;
+    } else {
+      type = kMiddleType;
+    }
