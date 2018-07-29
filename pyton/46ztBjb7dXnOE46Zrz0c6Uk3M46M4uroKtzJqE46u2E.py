bc18b7ba7094485e8142b79abd668391f855c4b4
SUNsung <weatherius@gmail.com>
2018-07-29 18:49:01 +0300

    Rewrite [46ztBjb7dXnOE46Zrz0c6Uk3M46M4uroKtzJqE46u2E.py] File in: 2018-07-29 18:49:00

--- pyton/46ztBjb7dXnOE46Zrz0c6Uk3M46M4uroKtzJqE46u2E.py
+++ pyton/46ztBjb7dXnOE46Zrz0c6Uk3M46M4uroKtzJqE46u2E.py
@@ -1,60 +1,39 @@
 
         
-        index_title = 0
-index_desc = 1
-index_auth = 2
-index_https = 3
-index_cors = 4
-index_link = 5
-num_segments = 6
+        def sequential():
+    return list(map(is_prime, PRIMES))
     
-        def test_multiple_requests(self):
-        '''multiple requests can be served'''
-        requests_to_handle = 5
     
-        def init_per_thread_state(self):
-        # Ensure state is initialized just once per-thread
-        if not hasattr(self._thread_local, 'init'):
-            self._thread_local.init = True
-            self._thread_local.last_nonce = ''
-            self._thread_local.nonce_count = 0
-            self._thread_local.chal = {}
-            self._thread_local.pos = None
-            self._thread_local.num_401_calls = None
+def PresentDialog_Confirm_Call( message ):
+  '''Return a mock.call object for a call to vimsupport.PresentDialog, as called
+  why vimsupport.Confirm with the supplied confirmation message'''
+  return call( message, [ 'Ok', 'Cancel' ] )
     
-    # Set default logging handler to avoid 'No handler found' warnings.
-import logging
-try:  # Python 2.7+
-    from logging import NullHandler
-except ImportError:
-    class NullHandler(logging.Handler):
-        def emit(self, record):
-            pass
+        if 'message' in self._response:
+      return self._HandleMessageResponse()
     
-            # reset stats on high iter-request times caused by client restarts
-        if delta > 3: # seconds
-            self._reset_stats()
-            return ''
+      AssertBuffersAreEqualAsBytes( [ 'aAa',
+                                  'aEb',
+                                  'bFb',
+                                  'GBa',
+                                  'aCa' ], result_buffer )
     
-            parser.add_option_group(group)
+      if config_entry is None:
+    return []
     
-        def __init__(self, request, timeout=180):
-        self._url = urldefrag(request.url)[0]
-        # converting to bytes to comply to Twisted interface
-        self.url = to_bytes(self._url, encoding='ascii')
-        self.method = to_bytes(request.method, encoding='ascii')
-        self.body = request.body or None
-        self.headers = Headers(request.headers)
-        self.response_headers = None
-        self.timeout = request.meta.get('download_timeout') or timeout
-        self.start_time = time()
-        self.deferred = defer.Deferred().addCallback(self._build_response, request)
     
-        def _debug_set_cookie(self, response, spider):
-        if self.debug:
-            cl = [to_native_str(c, errors='replace')
-                  for c in response.headers.getlist('Set-Cookie')]
-            if cl:
-                cookies = '\n'.join('Set-Cookie: {}\n'.format(c) for c in cl)
-                msg = 'Received cookies from: {}\n{}'.format(response, cookies)
-                logger.debug(msg, extra={'spider': spider})
+def _EndsWithPython( path ):
+  '''Check if given path ends with a python 2.7 or 3.4+ name.'''
+  return path and PYTHON_BINARY_REGEX.search( path ) is not None
+    
+    
+def make_app():
+    return tornado.web.Application([
+        (r'/post', POSTHandler),
+        (r'/(.*)', PUTHandler),
+    ])
+    
+        IOLoop.instance().start()
+    
+    from tornado.options import options, define, parse_command_line
+from tornado.template import Template
