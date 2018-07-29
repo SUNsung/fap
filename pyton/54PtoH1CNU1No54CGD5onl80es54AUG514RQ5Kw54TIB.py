187e06aa4db1dcf0d39625b7ef4b4d594df8b7d1
SUNsung <weatherius@gmail.com>
2018-07-29 23:48:59 +0300

    Rewrite [54PtoH1CNU1No54CGD5onl80es54AUG514RQ5Kw54TIB.py] File in: 2018-07-29 23:48:59

--- pyton/54PtoH1CNU1No54CGD5onl80es54AUG514RQ5Kw54TIB.py
+++ pyton/54PtoH1CNU1No54CGD5onl80es54AUG514RQ5Kw54TIB.py
@@ -1,12 +1,29 @@
 
         
-        from twisted import version as _txv
-twisted_version = (_txv.major, _txv.minor, _txv.micro)
+        containers = (('thefuck/python3-fish',
+               u'''FROM python:3
+                   # Use jessie-backports since it has the fish package. See here for details:
+                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
+                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
+                   RUN apt-get update
+                   RUN apt-get install -yy fish''',
+               u'fish'),
+              ('thefuck/python2-fish',
+               u'''FROM python:2
+                   # Use jessie-backports since it has the fish package. See here for details:
+                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
+                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
+                   RUN apt-get update
+                   RUN apt-get install -yy fish''',
+               u'fish'))
     
     
-    
-        def handleEndHeaders(self):
-        self.factory.gotHeaders(self.headers)
-    
-            ajax_crawl_request.meta['ajax_crawlable'] = True
-        return ajax_crawl_request
+@pytest.fixture(params=containers)
+def proc(request, spawnu, TIMEOUT):
+    proc = spawnu(*request.param)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(u'tcsh')
+    proc.sendline(u'setenv PYTHONIOENCODING utf8')
+    proc.sendline(u'eval `thefuck --alias`')
+    return proc
