3a787cd199ca947d0b1d2a6e9ea2f07e247288cd
SUNsung <weatherius@gmail.com>
2018-07-30 20:25:24 +0300

    Rewrite [12DT0BT..tu5Q12L4hECP9xYeU12GO4u19dJvM612IIr.py] File in: 2018-07-30 20:25:24

--- pyton/12DT0BT..tu5Q12L4hECP9xYeU12GO4u19dJvM612IIr.py
+++ pyton/12DT0BT..tu5Q12L4hECP9xYeU12GO4u19dJvM612IIr.py
@@ -1,134 +1,110 @@
 
         
-                if any([s_line.startswith(s) for s in ['* [', '- [']]):
-            if indent == last_indent:
-                blocks[-1].append(line)
-            else:
-                blocks.append([line])
-            last_indent = indent
-        else:
-            blocks.append([line])
-            last_indent = None
-    
-        def add_cancelled(self, future):
-        super(_AllCompletedWaiter, self).add_cancelled(future)
-        self._decrement_pending_calls()
-    
-    try:
-    import queue
-except ImportError:
-    import Queue as queue
+        README_FILE = 'README.md'
+helptext = sys.stdin.read()
     
     
-PENDING_FUTURE = create_future(state=PENDING)
-RUNNING_FUTURE = create_future(state=RUNNING)
-CANCELLED_FUTURE = create_future(state=CANCELLED)
-CANCELLED_AND_NOTIFIED_FUTURE = create_future(state=CANCELLED_AND_NOTIFIED)
-EXCEPTION_FUTURE = create_future(state=FINISHED, exception=IOError())
-SUCCESSFUL_FUTURE = create_future(state=FINISHED, result=42)
+def _download_restricted(url, filename, age):
+    ''' Returns true if the file has been downloaded '''
     
-    
-def ExtractKeywordsFromGroup_ContainedSyntaxArgAllowed_test():
-  assert_that( syntax_parse._ExtractKeywordsFromGroup(
-                 syntax_parse.SyntaxGroup( '', [
-                   'contained foo zoq',
-                   'contained bar goo',
-                   'far'
-                 ] ) ),
-               contains_inanyorder( 'foo', 'zoq', 'bar', 'goo', 'far' ) )
-
-    
-      return completion_data
-    
-        if not done:
-      self._result = None
-    else:
-      self._result = FakeResponse( response, exception )
-    
-        response = self.HandleFuture( self._response_future,
-                                  display_message = False )
-    if response is None:
-      # Server returned an exception.
-      return False
-    
-            # Pass matched groups to the handler.  Since
-        # match.groups() includes both named and
-        # unnamed groups, we want to use either groups
-        # or groupdict but not both.
-        if self.regex.groupindex:
-            path_kwargs = dict(
-                (str(k), _unquote_or_none(v))
-                for (k, v) in match.groupdict().items())
-        else:
-            path_args = [_unquote_or_none(s) for s in match.groups()]
-    
-    
-if (os.environ.get('TORNADO_NO_EXTENSION') or
-        os.environ.get('TORNADO_EXTENSION') == '0'):
-    # These environment variables exist to make it easier to do performance
-    # comparisons; they are not guaranteed to remain supported in the future.
-    _websocket_mask = _websocket_mask_python
-else:
-    try:
-        from tornado.speedups import websocket_mask as _websocket_mask
-    except ImportError:
-        if os.environ.get('TORNADO_EXTENSION') == '1':
-            raise
-        _websocket_mask = _websocket_mask_python
-    
-            See `.BaseIOStream.set_nodelay` for additional details.
-    
-        The implementations of this interface included with Tornado are
-    
-    setup(
-    name='tornado',
-    version=version,
-    packages=['tornado', 'tornado.test', 'tornado.platform'],
-    package_data={
-        # data files need to be listed both here (which determines what gets
-        # installed) and in MANIFEST.in (which determines what gets included
-        # in the sdist tarball)
-        'tornado.test': [
-            'README',
-            'csv_translations/fr_FR.csv',
-            'gettext_translations/fr_FR/LC_MESSAGES/tornado_test.mo',
-            'gettext_translations/fr_FR/LC_MESSAGES/tornado_test.po',
-            'options_test.cfg',
-            'options_test_types.cfg',
-            'options_test_types_str.cfg',
-            'static/robots.txt',
-            'static/sample.xml',
-            'static/sample.xml.gz',
-            'static/sample.xml.bz2',
-            'static/dir/index.html',
-            'static_foo.txt',
-            'templates/utf8.html',
-            'test.crt',
-            'test.key',
-        ],
-    },
-    author='Facebook',
-    author_email='python-tornado@googlegroups.com',
-    url='http://www.tornadoweb.org/',
-    license='http://www.apache.org/licenses/LICENSE-2.0',
-    description=('Tornado is a Python web framework and asynchronous networking library,'
-                 ' originally developed at FriendFeed.'),
-    classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ],
-    **kwargs
+    from .common import InfoExtractor
+from ..utils import (
+    parse_duration,
+    int_or_none,
 )
-
     
-        @gen_test
-    def test_maxsize(self):
-        self.assertRaises(TypeError, queues.Queue, maxsize=None)
-        self.assertRaises(ValueError, queues.Queue, maxsize=-1)
+        _TESTS = [{
+        'url': 'http://camwithher.tv/view_video.php?viewkey=6e9a24e2c0e842e1f177&page=&viewtype=&category=',
+        'info_dict': {
+            'id': '5644',
+            'ext': 'flv',
+            'title': 'Periscope Tease',
+            'description': 'In the clouds teasing on periscope to my favorite song',
+            'duration': 240,
+            'view_count': int,
+            'comment_count': int,
+            'uploader': 'MileenaK',
+            'upload_date': '20160322',
+        },
+        'params': {
+            'skip_download': True,
+        }
+    }, {
+        'url': 'http://camwithher.tv/view_video.php?viewkey=6dfd8b7c97531a459937',
+        'only_matching': True,
+    }, {
+        'url': 'http://camwithher.tv/view_video.php?page=&viewkey=6e9a24e2c0e842e1f177&viewtype=&category=',
+        'only_matching': True,
+    }, {
+        'url': 'http://camwithher.tv/view_video.php?viewkey=b6c3b5bea9515d1a1fc4&page=&viewtype=&category=mv',
+        'only_matching': True,
+    }]
+    
+    
+class ClipRsIE(OnetBaseIE):
+    _VALID_URL = r'https?://(?:www\.)?clip\.rs/(?P<id>[^/]+)/\d+'
+    _TEST = {
+        'url': 'http://www.clip.rs/premijera-frajle-predstavljaju-novi-spot-za-pesmu-moli-me-moli/3732',
+        'md5': 'c412d57815ba07b56f9edc7b5d6a14e5',
+        'info_dict': {
+            'id': '1488842.1399140381',
+            'ext': 'mp4',
+            'title': 'PREMIJERA Frajle predstavljaju novi spot za pesmu Moli me, moli',
+            'description': 'md5:56ce2c3b4ab31c5a2e0b17cb9a453026',
+            'duration': 229,
+            'timestamp': 1459850243,
+            'upload_date': '20160405',
+        }
+    }
+    
+            formats = []
+        for secure in ('', 'Secure'):
+            for ext in ('Ogg', 'Mp3'):
+                format_id = '%s%s' % (secure, ext)
+                format_url = metadata.get('%sUrl' % format_id)
+                if format_url:
+                    formats.append({
+                        'url': format_url,
+                        'format_id': format_id,
+                        'vcodec': 'none',
+                    })
+        self._sort_formats(formats)
+    
+        '''
+)
+    
+        @pytest.mark.parametrize('verify_value', ['false', 'fALse'])
+    def test_verify_false_OK(self, httpbin_secure, verify_value):
+        r = http(httpbin_secure.url + '/get', '--verify', verify_value)
+        assert HTTP_OK in r
+    
+        for factor, suffix in abbrevs:
+        if n >= factor:
+            break
+    
+        ERROR_TIMEOUT = 2
+    ERROR_TOO_MANY_REDIRECTS = 6
+    
+        def test_pretty_enabled_by_default_unless_stdout_redirected(self, httpbin):
+        r = http('GET', httpbin.url + '/get')
+        assert COLOR not in r
+    
+        long_description = README,
+    
+    def kuwo_download_by_rid(rid, output_dir = '.', merge = True, info_only = False):
+    html=get_content('http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_%s'%rid)
+    title=match1(html,r'<name>(.*)</name>')
+    #to get title
+    #format =aac|mp3 ->to get aac format=mp3 ->to get mp3
+    url=get_content('http://antiserver.kuwo.cn/anti.s?format=mp3&rid=MUSIC_%s&type=convert_url&response=url'%rid)
+    songtype, ext, size = url_info(url)
+    print_info(site_info, title, songtype, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir)
+    
+        # ordered list of supported stream types / qualities on this site
+    # order: high quality -> low quality
+    stream_types = [
+        {'id': 'original'}, # contains an 'id' or 'itag' field at minimum
+        {'id': 'small'},
+    ]
