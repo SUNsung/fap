dd6b9cf2186a2bb5943551f9f5039fd0c290005e
SUNsung <weatherius@gmail.com>
2018-07-30 11:42:13 +0300

    Rewrite [25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py] File in: 2018-07-30 11:42:12

--- pyton/25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py
+++ pyton/25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py
@@ -1,134 +1,210 @@
 
         
-                # reset stats on high iter-request times caused by client restarts
-        if delta > 3: # seconds
-            self._reset_stats()
-            return ''
+            # Sorting the libraries
+    inner_blocks = sorted(blocks[0].split('##'))
+    for i in range(1 , len(inner_blocks)):
+        if inner_blocks[i][0] != '#':
+            inner_blocks[i] = '##' + inner_blocks[i]
+    inner_blocks=''.join(inner_blocks)
     
-    from importlib import import_module
-from os.path import join, dirname, abspath, exists, splitext
+    from tensorflow.python.platform import gfile
     
-            self._signer = None
-        if is_botocore():
-            import botocore.auth
-            import botocore.credentials
-            kw.pop('anon', None)
-            if kw:
-                raise TypeError('Unexpected keyword arguments: %s' % kw)
-            if not self.anon:
-                SignerCls = botocore.auth.AUTH_TYPE_MAPS['s3']
-                self._signer = SignerCls(botocore.credentials.Credentials(
-                    aws_access_key_id, aws_secret_access_key))
-        else:
-            _S3Connection = _get_boto_connection()
-            try:
-                self.conn = _S3Connection(
-                    aws_access_key_id, aws_secret_access_key, **kw)
-            except Exception as ex:
-                raise NotConfigured(str(ex))
+    cc_library(
+    name = 'compute_session_impl',
+    srcs = ['compute_session_impl.cc'],
+    hdrs = ['compute_session_impl.h'],
+    deps = [
+        ':compute_session',
+        ':index_translator',
+        ':input_batch_cache',
+        '//dragnn/components/util:bulk_feature_extractor',
+        '//dragnn/core/util:label',
+        '//dragnn/protos:data_proto_cc',
+        '//dragnn/protos:spec_proto_cc',
+        '//dragnn/protos:trace_proto_cc',
+        '//syntaxnet:base',
+        '//syntaxnet:registry',
+    ],
+)
     
-        def __init__(self, timeout=180):
-        self._timeout = timeout
+    py_library(
+    name = 'composite_optimizer',
+    srcs = ['composite_optimizer.py'],
+    deps = [
+        '@org_tensorflow//tensorflow:tensorflow_py',
+        '@org_tensorflow//tensorflow/core:protos_all_py',
+    ],
+)
     
+    import tensorflow as tf
     
-class HttpAuthMiddleware(object):
-    '''Set Basic HTTP Authorization header
-    (http_user and http_pass spider class attributes)'''
+          # Expect feature extraction to generate a runtime error due to the
+      # fixed feature.
+      with self.assertRaises(RuntimeError):
+        comp.build_greedy_training(self.master_state, self.network_states)
     
-    from mrjob.job import MRJob
+      def get_variable(self, var_name=None, var_params=None):
+    '''Returns either the original or averaged version of a given variable.
     
+          # The cost corresponding to the three entries is, respectively,
+      # 0.7012, 0.7644, and 1.7269. Each of them is computed using the formula
+      # -prob_i * log(sigmoid(logit_i)) - (1-prob_i) * log(1-sigmoid(logit_i))
+      self.assertAlmostEqual(cost, 3.1924, 4)
+      self.assertEqual(correct, 1)
+      self.assertEqual(total, 3)
+      self.assertAllEqual(gold_labels, [0, 1, 2])
     
-class SalesRanker(MRJob):
+        try:
+      with open(FLAGS.expected_file) as expected:
+        content_expected = expected.read()
+    except IOError as e:
+      self.fail('Error opening '%s': %s' % (FLAGS.expected_file, e.strerror))
     
-        def __init__(self, from_user_id, to_user_id, request_status, timestamp):
-        self.from_user_id = from_user_id
-        self.to_user_id = to_user_id
-        self.request_status = request_status
-        self.timestamp = timestamp
+    # Imported for FLAGS.tf_master, which is used in the lexicon module.
     
-        def extract_year_month(self, timestamp):
-        '''Return the year and month portions of the timestamp.'''
-        ...
-    
-    def get_marker(line):
-    matchlist = TAG_REGEX.findall(line)
-    if matchlist:
-        namematch = NAMED_A_TAG_REGEX.match(line)
-        if namematch:
-            return namematch.group(1) # group 0 is full match
-    
-        def test_sequential_execution(self):
-        self.command_stack[0].execute()
-        output_after_first_execution = os.listdir(self.test_dir)
-        self.assertEqual(output_after_first_execution[0], 'bar.txt')
-        self.command_stack[1].execute()
-        output_after_second_execution = os.listdir(self.test_dir)
-        self.assertEqual(output_after_second_execution[0], 'baz.txt')
-    
-        def test_c_observers_shall_be_detachable(cls):
-        cls.s.detach(cls.dec_obs)
-        # hex viewer shall be remaining if dec viewer is detached first
-        cls.assertEqual(isinstance(cls.s._observers[0], HexViewer), True)
-        cls.assertEqual(len(cls.s._observers), 1)
-        cls.s.detach(cls.hex_obs)
-        cls.assertEqual(len(cls.s._observers), 0)
-    
-        def setUp(cls):
-        ''' Function/test case scope setup. '''
-        cls.output = StringIO()
-        cls.saved_stdout = sys.stdout
-        sys.stdout = cls.output
+      TODO(googleuser): Try improving the condition number of the Laplacian matrix
+  directly, instead of using the indirect approach above.  For example, one
+  could add c*I to the Laplacian (i.e., Tikhonov regularization).
     
     
-class Action(object):
+# Core signals.  For usage examples grep the source code or consult
+# the API documentation in docs/api.rst as well as docs/signals.rst
+template_rendered = _signals.signal('template-rendered')
+before_render_template = _signals.signal('before-render-template')
+request_started = _signals.signal('request-started')
+request_finished = _signals.signal('request-finished')
+request_tearing_down = _signals.signal('request-tearing-down')
+got_request_exception = _signals.signal('got-request-exception')
+appcontext_tearing_down = _signals.signal('appcontext-tearing-down')
+appcontext_pushed = _signals.signal('appcontext-pushed')
+appcontext_popped = _signals.signal('appcontext-popped')
+message_flashed = _signals.signal('message-flashed')
+
     
-        def test_cat_greek_localization(self):
-        self.assertEqual(self.g.get('cat'), 'γάτα')
+    
+def test_existing_handler(app):
+    logging.root.addHandler(logging.StreamHandler())
+    assert app.logger.level == logging.NOTSET
+    assert not app.logger.handlers
+    
+        with app.test_request_context('/?name=World'):
+        assert index() == 'Hello World!'
+    with app.test_request_context('/meh'):
+        assert meh() == 'http://localhost/meh'
+    assert flask._request_ctx_stack.top is None
+    
+    from flask._compat import StringIO
+    
+            This should not be called from user code, and is only exposed for use
+        when subclassing the
+        :class:`HTTPAdapter <requests.adapters.HTTPAdapter>`.
     
     
-class TestRunnerFacilities(unittest.TestCase):
+class ChunkedEncodingError(RequestException):
+    '''The server declared chunked encoding but sent an invalid chunk.'''
     
-    class TimeDisplay(object):
+    # Language to be used for generating the HTML full-text search index.
+# Sphinx supports the following languages:
+#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
+#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
+#html_search_language = 'en'
     
-    from dft.parameter_injection import TimeDisplay, MidnightTimeProvider, ProductionCodeTimeProvider, datetime
+    from . import utils
+from . import packages
+from .models import Request, Response, PreparedRequest
+from .api import request, get, head, post, patch, put, delete, options
+from .sessions import session, Session
+from .status_codes import codes
+from .exceptions import (
+    RequestException, Timeout, URLRequired,
+    TooManyRedirects, HTTPError, ConnectionError,
+    FileModeWarning, ConnectTimeout, ReadTimeout
+)
     
-            # simple test to validate param value
-        if param in self._static_method_choices.keys():
-            self.param = param
-        else:
-            raise ValueError('Invalid Value for Param: {0}'.format(param))
-    
-    from __future__ import print_function
-import os
-from os.path import lexists
-    
-            def callback(future):
-            self.future = future
-            raise Exception('callback')
-    
-        def open(self, *args, **kwargs):
-        '''Invoked when a new WebSocket is opened.
-    
-    try:
-    import ssl
-except ImportError:
-    # ssl is not available on Google App Engine
-    ssl = None
-    
-        def render_string(self, path, **kwargs):
-        '''Renders a template and returns it as a string.'''
-        return self.handler.render_string(path, **kwargs)
-    
-        See `OptionParser.print_help`.
+          >>> s = requests.Session()
+      >>> s.send(r)
+      <Response [200]>
     '''
-    return options.print_help(file)
     
-        # IReactorThreads
-    def callFromThread(self, f, *args, **kw):
-        assert callable(f), '%s is not callable' % f
-        with NullContext():
-            # This NullContext is mainly for an edge case when running
-            # TwistedIOLoop on top of a TornadoReactor.
-            # TwistedIOLoop.add_callback uses reactor.callFromThread and
-            # should not pick up additional StackContexts along the way.
-            self._io_loop.add_callback(f, *args, **kw)
+    This module provides a Session object to manage and persist settings across
+requests (cookies, auth, proxies).
+'''
+import os
+import sys
+import time
+from datetime import timedelta
+    
+            expected = {
+            'name': 'myhealthcheck',
+            'checkIntervalSec': 5,
+            'port': 443,
+            'unhealthyThreshold': 2,
+            'healthyThreshold': 2,
+            'host': '',
+            'timeoutSec': 5,
+            'requestPath': '/'}
+    
+        html = get_content(url)
+    pid = match1(html, r'video\.settings\.pid\s*=\s*\'([^\']+)\'')
+    title = match1(html, r'video\.settings\.title\s*=\s*\'([^\']+)\'')
+    
+    class MusicPlayOn(VideoExtractor):
+    name = 'MusicPlayOn'
+    
+        #title
+    title = ''
+    profile_api = 'https://www.showroom-live.com/api/room/profile?room_id={room_id}'.format(room_id = room_id)
+    html = loads(get_content(profile_api))
+    try:
+        title = html['main_name']
+    except KeyError:
+        title = 'Showroom_{room_id}'.format(room_id = room_id)
+    
+    # line 11
+def eggs(x, y):
+    'A docstring.'
+    global fr, st
+    fr = inspect.currentframe()
+    st = inspect.stack()
+    p = x
+    q = y / 0
+    
+        group = parser.add_argument_group('Selecting tests')
+    group.add_argument('-r', '--randomize', action='store_true',
+                       help='randomize test execution order.' + more_details)
+    group.add_argument('--randseed', metavar='SEED',
+                       dest='random_seed', type=int,
+                       help='pass a random seed to reproduce a previous '
+                            'random run')
+    group.add_argument('-f', '--fromfile', metavar='FILE',
+                       help='read names of tests to run from a file.' +
+                            more_details)
+    group.add_argument('-x', '--exclude', action='store_true',
+                       help='arguments are tests to *exclude*')
+    group.add_argument('-s', '--single', action='store_true',
+                       help='single step through a set of tests.' +
+                            more_details)
+    group.add_argument('-m', '--match', metavar='PAT',
+                       dest='match_tests', action='append',
+                       help='match test cases and methods with glob pattern PAT')
+    group.add_argument('--matchfile', metavar='FILENAME',
+                       dest='match_filename',
+                       help='similar to --match but get patterns from a '
+                            'text file, one pattern per line')
+    group.add_argument('-G', '--failfast', action='store_true',
+                       help='fail as soon as a test fails (only with -v or -W)')
+    group.add_argument('-u', '--use', metavar='RES1,RES2,...',
+                       action='append', type=resources_list,
+                       help='specify which special resource intensive tests '
+                            'to run.' + more_details)
+    group.add_argument('-M', '--memlimit', metavar='LIMIT',
+                       help='run very large memory-consuming tests.' +
+                            more_details)
+    group.add_argument('--testdir', metavar='DIR',
+                       type=relative_filename,
+                       help='execute test files in the specified directory '
+                            '(instead of the Python stdlib test suite)')
+    
+    
+def runtest_inner(ns, test, display_failure=True):
+    support.unload(test)
