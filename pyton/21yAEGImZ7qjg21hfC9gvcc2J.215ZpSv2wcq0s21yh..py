7be105fc2a2be1415e6face6a7f2e5b26e8aaca0
SUNsung <weatherius@gmail.com>
2018-07-30 10:17:24 +0300

    Rewrite [21yAEGImZ7qjg21hfC9gvcc2J.215ZpSv2wcq0s21yh..py] File in: 2018-07-30 10:17:23

--- pyton/21yAEGImZ7qjg21hfC9gvcc2J.215ZpSv2wcq0s21yh..py
+++ pyton/21yAEGImZ7qjg21hfC9gvcc2J.215ZpSv2wcq0s21yh..py
@@ -1,117 +1,96 @@
 
         
-          def testConvertNetworkStateTensorarray(self):
-    with self.test_session() as session:
-      ta = tf.TensorArray(
-          dtype=tf.float32,
-          size=0,
-          dynamic_size=True,
-          clear_after_read=False,
-          infer_shape=False)
-      # Create a 3-step x 2-stride x 2-feature-dim source array.
-      ta = ta.write(0, [[0., 0.]] * 2)  # The zeroth step will be removed.
-      ta = ta.write(1, [[1., 10.]] * 2)
-      ta = ta.write(2, [[2., 20.]] * 2)
-      ta = ta.write(3, [[3., 30.]] * 2)
-      tensor = network_units.convert_network_state_tensorarray(ta)
-      actual = session.run(tensor)
-      self.assertEqual(actual.shape, (6, 2))
+            def test_facebook_matching(self):
+        self.assertTrue(FacebookIE.suitable('https://www.facebook.com/Shiniknoh#!/photo.php?v=10153317450565268'))
+        self.assertTrue(FacebookIE.suitable('https://www.facebook.com/cindyweather?fref=ts#!/photo.php?v=10152183998945793'))
     
-      def get_variable(self, var_name=None, var_params=None):
-    if var_name:
-      return tf.get_variable(var_name)
-    else:
-      return var_params
+        def test_secondary_proxy_http(self):
+        params = self._check_params(['secondary_proxy', 'secondary_server_ip'])
+        if params is None:
+            return
+        ydl = FakeYDL()
+        req = compat_urllib_request.Request('http://yt-dl.org/ip')
+        req.add_header('Ytdl-request-proxy', params['secondary_proxy'])
+        self.assertEqual(
+            ydl.urlopen(req).read().decode('utf-8'),
+            params['secondary_server_ip'])
     
-    import os.path
-import tempfile
+            info_dict = self._extract_from_id(mvp_id, webpage)
+        info_dict['display_id'] = display_id
     
-      def testImmutabilityOfArguments(self):
-    '''Tests that training schedule generation does not change its arguments.'''
-    pretrain_steps = [1, 2, 3]
-    train_steps = [5, 5, 5]
-    trainer_lib.generate_target_per_step_schedule(pretrain_steps, train_steps)
-    self.assertEqual(pretrain_steps, [1, 2, 3])
-    self.assertEqual(train_steps, [5, 5, 5])
-    
-      # Parse the flags containing lists, using regular expressions.
-  # This matches and extracts key=value pairs.
-  component_beam_sizes = re.findall(r'([^=,]+)=(\d+)',
-                                    FLAGS.inference_beam_size)
-  # This matches strings separated by a comma. Does not return any empty
-  # strings.
-  components_to_locally_normalize = re.findall(r'[^,]+',
-                                               FLAGS.locally_normalize)
-    
-    flags.DEFINE_string('input_file', '',
-                    'File of CoNLL-formatted sentences to read from.')
-flags.DEFINE_string('output_file', '',
-                    'File path to write annotated sentences to.')
-flags.DEFINE_bool('text_format', False, '')
-    
-      pretrain_steps = [0]
-  train_steps = [FLAGS.num_epochs * len(training_set)]
-    
-        plugin_manager.register(Plugin)
-    try:
-        r = http(
-            httpbin + BASIC_AUTH_URL,
-            '--auth-type',
-            Plugin.auth_type,
-            '--auth',
-            USERNAME + SEP_CREDENTIALS + PASSWORD,
-        )
-        assert HTTP_OK in r
-        assert r.json == AUTH_OK
-    finally:
-        plugin_manager.unregister(Plugin)
-    
-            # Keyword arguments > stream.encoding > default utf8
-        if self.stdin_encoding is None:
-            self.stdin_encoding = getattr(
-                self.stdin, 'encoding', None) or 'utf8'
-        if self.stdout_encoding is None:
-            actual_stdout = self.stdout
-            if is_windows:
-                # noinspection PyUnresolvedReferences
-                from colorama import AnsiToWin32
-                if isinstance(self.stdout, AnsiToWin32):
-                    actual_stdout = self.stdout.wrapped
-            self.stdout_encoding = getattr(
-                actual_stdout, 'encoding', None) or 'utf8'
+            # XXX not implemented yet
+        entdig = None
+        p_parsed = urlparse(url)
+        #: path is request-uri defined in RFC 2616 which should not be empty
+        path = p_parsed.path or '/'
+        if p_parsed.query:
+            path += '?' + p_parsed.query
     
     
-class FormatterPlugin(object):
+class CookieConflictError(RuntimeError):
+    '''There are two cookies that meet the criteria specified in the cookie jar.
+    Use .get and .set and include domain and path args in order to be more specific.
+    '''
     
-        @property
-    def is_eager_to_contribute(self):
-        return random.randint(0, 1)
+        def get(self, url, **kwargs):
+        r'''Sends a GET request. Returns :class:`Response` object.
     
-        def on_switchover(self):
-        super(Standby, self).on_switchover() #message ignored
-        self._hsm._next_state('active')
+        def _advance(self):
+        ''' Return the value of the current token and read the next one into
+            self.cur_token.
+        '''
+        cur_val = None if self.cur_token is None else self.cur_token.value
+        try:
+            self.cur_token = next(self._tokenizer)
+        except StopIteration:
+            self.cur_token = None
+        return cur_val
     
-        def test_sales_manager_shall_not_talk_through_proxy_with_delay(cls):
-        cls.ntp.busy = 'No'
-        start_time = time()
-        cls.ntp.talk()
-        end_time = time()
-        execution_time = end_time - start_time
-        print_output = cls.output.getvalue()
-        expected_print_output = 'Proxy checking for Sales Manager availability\n\
-This Sales Manager will not talk to you whether he/she is busy or not\n'
-        cls.assertEqual(print_output, expected_print_output)
-        expected_execution_time = 1
-        cls.assertEqual(int(execution_time*10), expected_execution_time)
     
-        def get_current_time_as_html_fragment(self):
-        current_time = self.time_provider.now()
-        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
-        return current_time_as_html_fragment
+def main(root=ROOT_DIR, filename=GLOBALS_FILE,
+         filters=None, columns=COLUMN_NAMES, sort=None, group=None,
+         verbosity=VERBOSITY, rc=1):
+    
+    def run_latency_tests(max_threads):
+    for task in latency_tasks:
+        print('Background CPU task:', task.__doc__)
+        print()
+        func, args = task()
+        nthreads = 0
+        while nthreads <= max_threads:
+            results = run_latency_test(func, args, nthreads)
+            n = len(results)
+            # We print out milliseconds
+            lats = [1000 * (t2 - t1) for (t1, t2) in results]
+            #print(list(map(int, lats)))
+            avg = sum(lats) / n
+            dev = (sum((x - avg) ** 2 for x in lats) / n) ** 0.5
+            print('CPU threads=%d: %d ms. (std dev: %d ms.)' % (nthreads, avg, dev), end='')
+            print()
+            #print('    [... from %d samples]' % n)
+            nthreads += 1
+        print()
+    
+    By Guido van Rossum, demystified after a version by Fredrik Lundh.
 '''
     
-    '''
-Port of the Java example of 'Parameter Injection' in
-'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
-(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
-http://xunitpatterns.com/Dependency%20Injection.html.
+            total_count = 0
+        translated_count = 0
+        with open(fn) as f:
+            catalog = read_po(f)
+            for msg in catalog:
+                total_count += 1
+                if is_translated(msg):
+                    translated_count += 1
+        pct = translated_count / float(total_count) * 100
+        click.echo('% -7s % 2d%%' % (
+            locale,
+            pct,
+        ), err=True)
+        if pct >= MINIMUM and locale not in rv:
+            rv.append(locale)
+    with open(catalog_file, 'w') as f:
+        json.dump({
+            'supported_locales': sorted(rv)
+        }, f, indent=2)
+        f.write('\n')
