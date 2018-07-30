953caa03c7737cb9965dec3bab4befa0a7ba859e
SUNsung <weatherius@gmail.com>
2018-07-30 15:01:29 +0300

    Rewrite [10r6hJCpWiLM10qczV9HMozHg10lVKYy6oDri.10nki.rb] File in: 2018-07-30 15:01:28

--- ruby/10r6hJCpWiLM10qczV9HMozHg10lVKYy6oDri.10nki.rb
+++ ruby/10r6hJCpWiLM10qczV9HMozHg10lVKYy6oDri.10nki.rb
@@ -1,54 +1,26 @@
 
         
-            log = StringIO.new
-    @dry_run_started_at = Time.zone.now
-    @dry_run_logger = Logger.new(log).tap { |logger|
-      logger.formatter = proc { |severity, datetime, progname, message|
-        elapsed_time = '%02d:%02d:%02d' % 2.times.inject([datetime - @dry_run_started_at]) { |(x, *xs)|
-          [*x.divmod(60), *xs]
-        }
-    }
-    }
-    
-          if status.is_a? Symbol
-        pod.status = Pod::CURL_ERROR_MAP.fetch(status, :unknown_error)
-        pod.error = 'FederationError: #{status}'
-      elsif status >= 200 && status < 300
-        pod.status = :no_errors unless Pod.statuses[pod.status] == Pod.statuses[:version_failed]
-      else
-        pod.status = :http_failed
-        pod.error = 'FederationError: HTTP status code was: #{status}'
-      end
-      pod.update_offline_since
-    
-    describe Capistrano::Application do
-  it 'provides a --trace option which enables SSHKit/NetSSH trace output'
-    
-        i0 = index
-    r1 = _nt_double_quoted_string
-    if r1
-      r0 = r1
-    else
-      r2 = _nt_single_quoted_string
-      if r2
-        r0 = r2
-      else
-        @index = i0
-        r0 = nil
-      end
+            it 'should have a way of getting the service configurations' do
+      configs = key.service_configs_for(Spaceship::Portal::Key::MUSIC_KIT_ID)
+      expect(configs).to be_instance_of(Array)
+      expect(configs.sample).to be_instance_of(Hash)
+      expect(configs.first['identifier']).to eq('music.com.snatchev.test')
     end
     
-      def file_sha1(path)
-    digest = Digest::SHA1.new
-    fd = File.new(path, 'r')
-    while true
-      begin
-        digest << fd.sysread(16384)
-      rescue EOFError
-        break
-      end
+          it 'does set the output directory' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            cloc(output_directory: '/tmp')
+          end').runner.execute(:test)
+    
+    Struct.new('Blog', :name, :web_url, :rss_url)
+blogs = []
+    
+    class FPM::Package::P5P < FPM::Package
+    
+        args = [ '-B', build_path('build-info'), '-c', build_path('comment'), '-d', build_path('description'), '-f', build_path('packlist'), '-I', '/opt/local', '-p', staging_path,  '-U', '#{cwd}/#{name}-#{self.version}-#{iteration}.tgz' ]
+    safesystem('pkg_create', *args)
+    
+        template = template('solaris.erb')
+    File.open('#{build_path}/pkginfo', 'w') do |pkginfo|
+      pkginfo.puts template.result(binding)
     end
-    return digest.hexdigest
-  ensure
-    fd.close if fd
-  end
