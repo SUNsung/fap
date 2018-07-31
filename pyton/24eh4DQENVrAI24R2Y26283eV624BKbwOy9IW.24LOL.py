931fc22d228c1faf0a8cb7ee0f517881fc6f8a9e
SUNsung <weatherius@gmail.com>
2018-07-31 11:26:21 +0300

    Rewrite [24eh4DQENVrAI24R2Y26283eV624BKbwOy9IW.24LOL.py] File in: 2018-07-31 11:26:21

--- pyton/24eh4DQENVrAI24R2Y26283eV624BKbwOy9IW.24LOL.py
+++ pyton/24eh4DQENVrAI24R2Y26283eV624BKbwOy9IW.24LOL.py
@@ -1,74 +1,90 @@
-cc_library(
-    name = 'dragnn_ops_cc',
-    srcs = [
-        'ops/dragnn_op_kernels.cc',
-        'ops/dragnn_ops.cc',
-    ],
-    deps = [
-        ':compute_session',
-        ':compute_session_op',
-        ':compute_session_pool',
-        ':resource_container',
-        ':shape_helpers',
-        '//dragnn/core/util:label',
-        '//dragnn/protos:data_proto_cc',
-        '//dragnn/protos:spec_proto_cc',
-        '//syntaxnet:base',
-        '@org_tensorflow//third_party/eigen3',
-    ],
-    alwayslink = 1,
-)
+
+        
+            def __str__(self):
+        return self.msg
     
-        Returns:
-      setup_op - An op that, when run, guarantees all setup ops will run.
-    '''
-    logging.info('Building restore hook for component: %s', self.spec.name)
-    with tf.variable_scope(self.name):
-      if callable(getattr(self.network, 'build_post_restore_hook', None)):
-        return [self.network.build_post_restore_hook()]
-      else:
-        return []
+    from __future__ import absolute_import
     
-    _DUMMY_TEST_SENTENCE = '''
-token {
-  word: 'sentence' start: 0 end: 7 break_level: NO_BREAK
-}
-token {
-  word: '0' start: 9 end: 9 break_level: SPACE_BREAK
-}
-token {
-  word: '.' start: 10 end: 10 break_level: NO_BREAK
-}
-'''
+        if release_date.date() != date.today():
+        fail(
+            'Release date is not today (%s != %s)',
+            release_date.date(), date.today()
+        )
     
-    from syntaxnet import parser_trainer
-from syntaxnet import task_spec_pb2
-from syntaxnet import test_flags
+        @app.route('/')
+    def index():
+        return None
     
-      Args:
-    mst_op: The MaximumSpanningTree op being differentiated.
-    d_loss_d_max_scores: [B] vector where entry b is the gradient of the network
-                         loss w.r.t. entry b of the |max_scores| output of the
-                         |mst_op|.
-    *_: The gradients w.r.t. the other outputs; ignored.
-    
-    from httpie.compat import is_windows
-from httpie.config import DEFAULT_CONFIG_DIR, Config
-    
-        '''
-    return path.replace('\\', '\\\\\\')
+        def record(sender, template, context):
+        context['whiskey'] = 43
+        recorded.append((template, context))
     
     
-def rst_filenames():
-    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
-        if '.tox' not in root:
-            for filename in fnmatch.filter(filenames, '*.rst'):
-                yield os.path.join(root, filename)
+@pytest.mark.functional
+def test_select_command_with_arrows(proc, TIMEOUT):
+    select_command_with_arrows(proc, TIMEOUT)
     
-        ERROR_TIMEOUT = 2
-    ERROR_TOO_MANY_REDIRECTS = 6
+        gc.collect()
     
+    # Split the dataset in training and test set:
+docs_train, docs_test, y_train, y_test = train_test_split(
+    dataset.data, dataset.target, test_size=0.5)
     
-def test_headers_empty_value_with_value_gives_error(httpbin):
-    with pytest.raises(ParseError):
-        http('GET', httpbin + '/headers', 'Accept;SYNTAX_ERROR')
+    # Plot the results (= shape of the data points cloud)
+plt.figure(1)  # two clusters
+plt.title('Outlier detection on a real data set (boston housing)')
+plt.scatter(X1[:, 0], X1[:, 1], color='black')
+bbox_args = dict(boxstyle='round', fc='0.8')
+arrow_args = dict(arrowstyle='->')
+plt.annotate('several confounded points', xy=(24, 19),
+             xycoords='data', textcoords='data',
+             xytext=(13, 10), bbox=bbox_args, arrowprops=arrow_args)
+plt.xlim((xx1.min(), xx1.max()))
+plt.ylim((yy1.min(), yy1.max()))
+plt.legend((legend1_values_list[0].collections[0],
+            legend1_values_list[1].collections[0],
+            legend1_values_list[2].collections[0]),
+           (legend1_keys_list[0], legend1_keys_list[1], legend1_keys_list[2]),
+           loc='upper center',
+           prop=matplotlib.font_manager.FontProperties(size=12))
+plt.ylabel('accessibility to radial highways')
+plt.xlabel('pupil-teacher ratio by town')
+    
+    from ..common import *
+from ..extractor import VideoExtractor
+    
+    def kuwo_download_by_rid(rid, output_dir = '.', merge = True, info_only = False):
+    html=get_content('http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_%s'%rid)
+    title=match1(html,r'<name>(.*)</name>')
+    #to get title
+    #format =aac|mp3 ->to get aac format=mp3 ->to get mp3
+    url=get_content('http://antiserver.kuwo.cn/anti.s?format=mp3&rid=MUSIC_%s&type=convert_url&response=url'%rid)
+    songtype, ext, size = url_info(url)
+    print_info(site_info, title, songtype, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir)
+    
+        t = r1(r'type=(\w+)', flashvars)
+    id = r1(r'vid=([^']+)', flashvars)
+    if t == 'youku':
+        youku_download_by_vid(id, title=title, output_dir=output_dir, merge=merge, info_only=info_only)
+    elif t == 'tudou':
+        tudou_download_by_id(id, title, output_dir=output_dir, merge=merge, info_only=info_only)
+    elif t == 'sina' or t == 'video':
+        fake_headers['Referer'] = url
+        url = 'http://www.miomio.tv/mioplayer/mioplayerconfigfiles/sina.php?vid=' + id
+        xml_data = get_content(url, headers=fake_headers, decoded=True)
+        url_list = sina_xml_to_url_list(xml_data)
+    
+        mediatype, ext, size = 'mp4', 'mp4', 0
+    print_info(site_info, title, mediatype, size)
+    #
+    # rtmpdump  -r 'rtmpe://cp30865.edgefcs.net/ondemand/mtviestor/_!/intlod/MTVInternational/MBUS/GeoLocals/00JP/VIAMTVI/PYC/201304/7122HVAQ4/00JPVIAMTVIPYC7122HVAQ4_640x_360_1200_m30.mp4' -o 'title.mp4' --swfVfy http://media.mtvnservices.com/player/prime/mediaplayerprime.1.10.8.swf
+    #
+    # because rtmpdump is unstable,may try serveral times
+    #
+    if not info_only:
+        # import pdb
+        # pdb.set_trace()
+        download_rtmp_url(url=url, title=title, ext=ext, params={
+                          '--swfVfy': 'http://media.mtvnservices.com/player/prime/mediaplayerprime.1.10.8.swf'}, output_dir=output_dir)
