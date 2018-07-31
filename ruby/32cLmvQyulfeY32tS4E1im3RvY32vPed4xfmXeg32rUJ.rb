6a190b39e5022eb37230c3493ccc0f3fc371da9a
SUNsung <weatherius@gmail.com>
2018-07-31 22:09:38 +0300

    Rewrite [32cLmvQyulfeY32tS4E1im3RvY32vPed4xfmXeg32rUJ.rb] File in: 2018-07-31 22:09:37

--- ruby/32cLmvQyulfeY32tS4E1im3RvY32vPed4xfmXeg32rUJ.rb
+++ ruby/32cLmvQyulfeY32tS4E1im3RvY32vPed4xfmXeg32rUJ.rb
@@ -1,68 +1,18 @@
 
         
-              it 'does set the exclude directories' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            cloc(exclude_dir: 'test1,test2,build')
-          end').runner.execute(:test)
-    
-            expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to match(/cd .* && agvtool new-marketing-version 1.4.3/)
-      end
-    
-        groups << @user.authorized_groups.visible_to_user(current_user) if current_user
-    groups << @user.authorized_groups.public_to_user(current_user)
-    
-    unless invalids.empty?
-  puts '\n\nFailed links:'
-  invalids.each do |link|
-    puts '- #{link}'
-  end
-  puts 'Done with errors.'
-  exit(1)
-end
-    
-        # @abstract
-    #
-    # Identify the SHA of the commit that will be deployed.  This will most likely involve SshKit's capture method.
-    #
-    # @return void
-    #
-    def fetch_revision
-      raise NotImplementedError, 'Your SCM strategy module should provide a #fetch_revision method'
-    end
+          def human?
+    params.has_key?('human') && (params['human'].nil? || as_boolean(params['human']) == true)
   end
 end
 
     
-      desc 'Publish the release.'
-  task :publishing do
-  end
-    
-      it 'provides a --format option which enables the choice of output formatting'
-    
-      subject { described_class.new(lambda {}) }
-    
-        it 'leaves cache-breaker params untouched' do
-      mock_app do |env|
-        [200, {'Content-Type' => 'text/plain'}, ['hi']]
+      def _nt_number
+    start_index = index
+    if node_cache[:number].has_key?(index)
+      cached = node_cache[:number][index]
+      if cached
+        cached = SyntaxNode.new(input, index...(index + 1)) if cached == true
+        @index = cached.interval.end
       end
-    
-      it 'should allow changing the protection mode' do
-    # I have no clue what other modes are available
-    mock_app do
-      use Rack::Protection::FrameOptions, :frame_options => :deny
-      run DummyApp
+      return cached
     end
-    
-        { # yes, this is ugly, feel free to change that
-      '/..' => '/', '/a/../b' => '/b', '/a/../b/' => '/b/', '/a/.' => '/a/',
-      '/%2e.' => '/', '/a/%2E%2e/b' => '/b', '/a%2f%2E%2e%2Fb/' => '/b/',
-      '//' => '/', '/%2fetc%2Fpasswd' => '/etc/passwd'
-    }.each do |a, b|
-      it('replaces #{a.inspect} with #{b.inspect}') { expect(get(a).body).to eq(b) }
-    end
-    
-    
-  # Adds some extra filters used during the category creation process.
-  module Filters
-    
-            private
