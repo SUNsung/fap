ed4a696eb137ff0b536a2c20fe0bcdef4b59b83c
SUNsung <weatherius@gmail.com>
2018-07-31 09:25:52 +0300

    Rewrite [55G0rkV3XDw8M55G2p2mz9tC4o553DB9xJEPytA55UA1.cpp] File in: 2018-07-31 09:25:52

--- cccccc/55G0rkV3XDw8M55G2p2mz9tC4o553DB9xJEPytA55UA1.cpp
+++ cccccc/55G0rkV3XDw8M55G2p2mz9tC4o553DB9xJEPytA55UA1.cpp
@@ -1,274 +1,217 @@
 
         
-        #include 'db/builder.h'
+          AcceleratorCallbackMap accelerator_callback_map_;
     
-    #include <stdint.h>
-#include 'leveldb/db.h'
-#include 'db/dbformat.h'
-    
-    namespace leveldb {
+    namespace atom {
     }
-    
-    int main(int argc, char** argv) {
-  return leveldb::test::RunAllTests();
-}
-
-    
-      void Add(const char* smallest, const char* largest,
-           SequenceNumber smallest_seq = 100,
-           SequenceNumber largest_seq = 100) {
-    FileMetaData* f = new FileMetaData;
-    f->number = files_.size() + 1;
-    f->smallest = InternalKey(smallest, smallest_seq, kTypeValue);
-    f->largest = InternalKey(largest, largest_seq, kTypeValue);
-    files_.push_back(f);
-  }
-    
-    class Env;
-    
-    
-    {  // At this point, letter_types is entirely U_LTR or U_RTL.
-  for (int i = word_length_ - 1; i >= 0;) {
-    if (letter_types[i] == U_RTL) {
-      blob_indices->push_back(i);
-      i--;
-    } else {
-      // left to right sequence.  scan to the beginning.
-      int j = i - 1;
-      for (; j >= 0 && letter_types[j] != U_RTL; j--) { }  // pass
-      // Now (j, i] is LTR
-      for (int k = j + 1; k <= i; k++) blob_indices->push_back(k);
-      i = j;
-    }
-  }
-  ASSERT_HOST(blob_indices->size() == word_length_);
-}
-    
-    // Get a terminated UTF8 string: Must delete[] it after use.
-char* UNICHAR::utf8_str() const {
-  int len = utf8_len();
-  char* str = new char[len + 1];
-  memcpy(str, chars, len);
-  str[len] = 0;
-  return str;
-}
     
      private:
-  NormalizationMode norm_mode_;
-  // Character set we are training for.
-  UNICHARSET unicharset_;
-  // Original feature space. Subspace mapping is contained in feature_map_.
-  IntFeatureSpace feature_space_;
-  TrainingSampleSet samples_;
-  TrainingSampleSet junk_samples_;
-  TrainingSampleSet verify_samples_;
-  // Master shape table defines what fonts stay together until the leaves.
-  ShapeTable master_shapes_;
-  // Flat shape table has each unichar/font id pair in a separate shape.
-  ShapeTable flat_shapes_;
-  // Font metrics gathered from multiple files.
-  FontInfoTable fontinfo_table_;
-  // Array of xheights indexed by font ids in fontinfo_table_;
-  GenericVector<int32_t> xheights_;
+  void* operator new(size_t size);
+  void operator delete(void*, size_t);
     
-    
-    {  if ((chainsum != 4 && chainsum != -4)
-  || edgept != start || length < MINEDGELENGTH) {
-    if (edgept != start) {
-     return ScrollView::YELLOW;
-    } else if (length < MINEDGELENGTH) {
-     return ScrollView::MAGENTA;
-    } else {
-      ED_ILLEGAL_SUM.error ('check_path_legal', TESSLOG, 'chainsum=%d',
-        chainsum);
-      return ScrollView::GREEN;
-    }
-  }
-                                 //colour on inside
-  return chainsum < 0 ? ScrollView::BLUE : ScrollView::RED;
-}
-    
-    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
-# pragma once
-#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
-    
-    #include <boost/asio/detail/push_options.hpp>
-    
-    template <typename Stream>
-class buffered_read_stream;
-    
-    namespace boost {
-namespace asio {
-    }
-    }
-    
-      descriptor_write_op(int descriptor,
-      const ConstBufferSequence& buffers, Handler& handler)
-    : descriptor_write_op_base<ConstBufferSequence>(
-        descriptor, buffers, &descriptor_write_op::do_complete),
-      handler_(BOOST_ASIO_MOVE_CAST(Handler)(handler))
-  {
-  }
-    
-    namespace boost {
-namespace asio {
-namespace detail {
-    }
-    }
-    }
-    
-    
-    {
-    {
-    {} // namespace detail
-} // namespace asio
-} // namespace boost
-    
-    template <typename Handler>
-inline void* allocate(std::size_t s, Handler& h)
-{
-#if !defined(BOOST_ASIO_HAS_HANDLER_HOOKS)
-  return ::operator new(s);
-#else
-  using boost::asio::asio_handler_allocate;
-  return asio_handler_allocate(s, boost::asio::detail::addressof(h));
-#endif
+    namespace ui {
+class Accelerator;
 }
     
     
-    {
-    {
-    {} // namespace detail
-} // namespace asio
-} // namespace boost
-    
-    class winrt_buffer_impl :
-  public Microsoft::WRL::RuntimeClass<
-    Microsoft::WRL::RuntimeClassFlags<
-      Microsoft::WRL::RuntimeClassType::WinRtClassicComMix>,
-    ABI::Windows::Storage::Streams::IBuffer,
-    Windows::Storage::Streams::IBufferByteAccess>
-{
-public:
-  explicit winrt_buffer_impl(const boost::asio::const_buffer& b)
-  {
-    bytes_ = const_cast<byte*>(boost::asio::buffer_cast<const byte*>(b));
-    length_ = boost::asio::buffer_size(b);
-    capacity_ = boost::asio::buffer_size(b);
-  }
-    }
-    
-    
-    {  errno = 0;
-  int result = error_wrapper(::fcntl(d, cmd), ec);
-  if (result != -1)
-    ec = boost::system::error_code();
-  return result;
-}
-    
-    #endif // defined(BOOST_ASIO_HAS_DEV_POLL)
-    
-    #ifndef BOOST_ASIO_DETAIL_IMPL_EPOLL_REACTOR_HPP
-#define BOOST_ASIO_DETAIL_IMPL_EPOLL_REACTOR_HPP
-    
-    namespace xgboost {
-namespace tree {
-// List of files that will be force linked in static links.
-DMLC_REGISTRY_LINK_TAG(updater_colmaker);
-DMLC_REGISTRY_LINK_TAG(updater_skmaker);
-DMLC_REGISTRY_LINK_TAG(updater_refresh);
-DMLC_REGISTRY_LINK_TAG(updater_prune);
-DMLC_REGISTRY_LINK_TAG(updater_fast_hist);
-DMLC_REGISTRY_LINK_TAG(updater_histmaker);
-DMLC_REGISTRY_LINK_TAG(updater_sync);
-#ifdef XGBOOST_USE_CUDA
-DMLC_REGISTRY_LINK_TAG(updater_gpu);
-DMLC_REGISTRY_LINK_TAG(updater_gpu_hist);
-#endif
-}  // namespace tree
-}  // namespace xgboost
-
-    
-    /*!
- * \brief string flag for R library, to leave hooks when needed.
- */
-#ifndef XGBOOST_STRICT_R_MODE
-#define XGBOOST_STRICT_R_MODE 0
-#endif
-    
-    #pragma once
-    
-    template <typename T>
-constexpr ordering to_ordering(T c) {
-  return c < T(0) ? ordering::lt : c > T(0) ? ordering::gt : ordering::eq;
-}
-    
-    TEST_F(OrderingTest, to_ordering) {
-  EXPECT_EQ(ordering::lt, to_ordering(int(ordering::lt)));
-  EXPECT_EQ(ordering::eq, to_ordering(int(ordering::eq)));
-  EXPECT_EQ(ordering::gt, to_ordering(int(ordering::gt)));
-    }
-    
-    /** Wide CAS.
- */
-template <typename T, template <typename> class Atom = std::atomic>
-class HazptrWideCAS {
-  struct Node : public hazptr_obj_base<Node, Atom> {
-    T val_;
-    explicit Node(T v = {}) : val_(v) {}
-  };
-    }
-    
-    #include <boost/container/flat_map.hpp>
-#include <boost/operators.hpp>
-    
-    
-    {  return block;
-}
-    
-    	if (!xlogger_IsEnabledFor((TLogLevel)_level)) {
-		return;
-	}
-    
-    namespace mars_boost {} namespace boost = mars_boost; namespace mars_boost {
-    }
-    
-    
-    
-    // Licensed under the MIT License (the 'License'); you may not use this file except in 
-// compliance with the License. You may obtain a copy of the License at
-// http://opensource.org/licenses/MIT
-    
-    ScopeJEnv::ScopeJEnv(JavaVM* jvm, jint _capacity)
-    : vm_(jvm), env_(NULL), we_attach_(false), status_(0) {
-    ASSERT(jvm);
-    do {
-        env_ = (JNIEnv*)pthread_getspecific(g_env_key);
-        
-        if (NULL != env_) {
-            break;
-        }
-        
-        status_ = vm_->GetEnv((void**) &env_, JNI_VERSION_1_6);
-    }
-    }
-    
-    
-#endif /* SCOP_JENV_H_ */
-
-    
-    // This is needed for return conversion
-template <>
-struct Convert<void> {
-  typedef void jniType;
+    {  virtual int GetSourceCount() const = 0;
+  virtual const Source& GetSource(int index) const = 0;
+  virtual std::vector<Source> GetSources() const = 0;
 };
     
-    #include <cstdarg>
-#include <stdio.h>
-    
-    namespace facebook {
+    // An interface the PrintViewManager uses to notify an observer when the print
+// dialog is shown. Register the observer via PrintViewManager::set_observer.
+class PrintViewManagerObserver {
+ public:
+  // Notifies the observer that the print dialog was shown.
+  virtual void OnPrintDialogShown() = 0;
     }
     
-    inline local_ref<jobject> autobox(alias_ref<jobject> val) {
-  return make_local(val);
+    #include 'chrome/browser/speech/tts_platform.h'
+    
+    #include <string>
+    
+    #ifndef TEST_QPS_USAGE_TIMER_H
+#define TEST_QPS_USAGE_TIMER_H
+    
+    void ProtoToCoreStats(const grpc::core::Stats& proto, grpc_stats_data* core) {
+  memset(core, 0, sizeof(*core));
+  for (const auto& m : proto.metrics()) {
+    switch (m.value_case()) {
+      case Metric::VALUE_NOT_SET:
+        break;
+      case Metric::kCount:
+        for (int i = 0; i < GRPC_STATS_COUNTER_COUNT; i++) {
+          if (m.name() == grpc_stats_counter_name[i]) {
+            core->counters[i] = m.count();
+            break;
+          }
+        }
+        break;
+      case Metric::kHistogram:
+        for (int i = 0; i < GRPC_STATS_HISTOGRAM_COUNT; i++) {
+          if (m.name() == grpc_stats_histogram_name[i]) {
+            const auto& h = m.histogram();
+            bool valid = true;
+            if (grpc_stats_histo_buckets[i] != h.buckets_size()) valid = false;
+            for (int j = 0; valid && j < h.buckets_size(); j++) {
+              if (grpc_stats_histo_bucket_boundaries[i][j] !=
+                  h.buckets(j).start()) {
+                valid = false;
+              }
+            }
+            if (!valid) {
+              gpr_log(GPR_ERROR,
+                      'Found histogram %s but shape is different from proto',
+                      m.name().c_str());
+            }
+            for (int j = 0; valid && j < h.buckets_size(); j++) {
+              core->histograms[grpc_stats_histo_start[i] + j] =
+                  h.buckets(j).count();
+            }
+          }
+        }
+        break;
+    }
+  }
 }
+    
+    // Decodes one 8x8 block of DCT coefficients from the bit stream.
+bool DecodeDCTBlock(const HuffmanTableEntry* dc_huff,
+                    const HuffmanTableEntry* ac_huff,
+                    int Ss, int Se, int Al,
+                    int* eobrun,
+                    BitReaderState* br,
+                    JPEGData* jpg,
+                    coeff_t* last_dc_coeff,
+                    coeff_t* coeffs) {
+  int s;
+  int r;
+  bool eobrun_allowed = Ss > 0;
+  if (Ss == 0) {
+    s = ReadSymbol(dc_huff, br);
+    if (s >= kJpegDCAlphabetSize) {
+      fprintf(stderr, 'Invalid Huffman symbol %d for DC coefficient.\n', s);
+      jpg->error = JPEG_INVALID_SYMBOL;
+      return false;
+    }
+    if (s > 0) {
+      r = br->ReadBits(s);
+      s = HuffExtend(r, s);
+    }
+    s += *last_dc_coeff;
+    const int dc_coeff = SignedLeftshift(s, Al);
+    coeffs[0] = dc_coeff;
+    if (dc_coeff != coeffs[0]) {
+      fprintf(stderr, 'Invalid DC coefficient %d\n', dc_coeff);
+      jpg->error = JPEG_NON_REPRESENTABLE_DC_COEFF;
+      return false;
+    }
+    *last_dc_coeff = s;
+    ++Ss;
+  }
+  if (Ss > Se) {
+    return true;
+  }
+  if (*eobrun > 0) {
+    --(*eobrun);
+    return true;
+  }
+  for (int k = Ss; k <= Se; k++) {
+    s = ReadSymbol(ac_huff, br);
+    if (s >= kJpegHuffmanAlphabetSize) {
+      fprintf(stderr, 'Invalid Huffman symbol %d for AC coefficient %d\n',
+              s, k);
+      jpg->error = JPEG_INVALID_SYMBOL;
+      return false;
+    }
+    r = s >> 4;
+    s &= 15;
+    if (s > 0) {
+      k += r;
+      if (k > Se) {
+        fprintf(stderr, 'Out-of-band coefficient %d band was %d-%d\n',
+                k, Ss, Se);
+        jpg->error = JPEG_OUT_OF_BAND_COEFF;
+        return false;
+      }
+      if (s + Al >= kJpegDCAlphabetSize) {
+        fprintf(stderr, 'Out of range AC coefficient value: s=%d Al=%d k=%d\n',
+                s, Al, k);
+        jpg->error = JPEG_NON_REPRESENTABLE_AC_COEFF;
+        return false;
+      }
+      r = br->ReadBits(s);
+      s = HuffExtend(r, s);
+      coeffs[kJPEGNaturalOrder[k]] = SignedLeftshift(s, Al);
+    } else if (r == 15) {
+      k += 15;
+    } else {
+      *eobrun = 1 << r;
+      if (r > 0) {
+        if (!eobrun_allowed) {
+          fprintf(stderr, 'End-of-block run crossing DC coeff.\n');
+          jpg->error = JPEG_EOB_RUN_TOO_LONG;
+          return false;
+        }
+        *eobrun += br->ReadBits(r);
+      }
+      break;
+    }
+  }
+  --(*eobrun);
+  return true;
+}
+    
+    using std::size_t;
+    
+    inline void ColorTransformYCbCrToRGB(uint8_t* pixel) {
+  int y  = pixel[0];
+  int cb = pixel[1];
+  int cr = pixel[2];
+  pixel[0] = kRangeLimit[y + kCrToRedTable[cr]];
+  pixel[1] = kRangeLimit[y +
+                         ((kCrToGreenTable[cr] + kCbToGreenTable[cb]) >> 16)];
+  pixel[2] = kRangeLimit[y + kCbToBlueTable[cb]];
+}
+    
+    // Computes the DCT (Discrete Cosine Transform) of the 8x8 array in 'block',
+// scaled up by a factor of 16. The values in 'block' are laid out row-by-row
+// and the result is written to the same memory area.
+void ComputeBlockDCT(coeff_t* block);
+    
+    #include 'guetzli/gamma_correct.h'
+    
+    std::vector<uint8_t> DecodeJpegToRGB(const JPEGData& jpg) {
+  if (jpg.components.size() == 1 ||
+      (jpg.components.size() == 3 &&
+       HasYCbCrColorSpace(jpg) && (jpg.Is420() || jpg.Is444()))) {
+    OutputImage img(jpg.width, jpg.height);
+    img.CopyFromJpegData(jpg);
+    return img.ToSRGB();
+  }
+  return std::vector<uint8_t>();
+}
+    
+    void UpdateACHistogramForDCTBlock(const coeff_t* coeffs,
+                                  JpegHistogram* ac_histogram);
+    
+      void SaveToJpegData(JPEGData* jpg) const;
+    
+     private:
+  std::unordered_map<std::string, const Obstacle*> id_obstacle_map_;
+    
+      /**
+   * @brief: recognize light status
+   * @param  const Recognize&: recognize options
+   * @param  const Image&: input image
+   * @param  std::vector<Light>*: recognized light status
+   * @return  bool
+   */
+  bool RecognizeStatus(const Image &image, const RecognizeOption &option,
+                       std::vector<LightPtr> *lights) override;
+    
+    #include 'modules/prediction/container/obstacles/obstacle.h'
+#include 'modules/prediction/evaluator/evaluator.h'
+#include 'modules/prediction/proto/fnn_vehicle_model.pb.h'
+#include 'modules/prediction/proto/lane_graph.pb.h'
