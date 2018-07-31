7cd443462a198aa6f0166bdafd34a22a9c35e698
SUNsung <weatherius@gmail.com>
2018-07-31 09:58:55 +0300

    Rewrite [55FO0IDHlgh655EO20IJX9V7255whzIPP7gKbw55qQH.rb] File in: 2018-07-31 09:58:55

--- ruby/55FO0IDHlgh655EO20IJX9V7255whzIPP7gKbw55qQH.rb
+++ ruby/55FO0IDHlgh655EO20IJX9V7255whzIPP7gKbw55qQH.rb
@@ -1,130 +1,82 @@
 
         
-            describe '#revoke!' do
-      it 'revokes the key with the client' do
-        mock_client_response(:revoke_key!)
-    
-          it 'renders HTML' do
-        capture = render(options, screenshots)
-        expect(capture).to match(/<html>/)
-        expect(capture).to include('<li>Some</li>')
-        expect(capture).to include('<li>key</li>')
-        expect(capture).to include('<li>words</li>')
-      end
+          module ClassMethods
+    def load_types_in(module_name, my_name = module_name.singularize)
+      const_set(:MODULE_NAME, module_name)
+      const_set(:BASE_CLASS_NAME, my_name)
+      const_set(:TYPES, Dir[Rails.root.join('app', 'models', module_name.underscore, '*.rb')].map { |path| module_name + '::' + File.basename(path, '.rb').camelize })
     end
     
-            expect(result).to eq('hg parent --template '{node|short}'')
-        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('hg parent --template '{node|short}'')
-      end
+        validate :validate_evernote_options
     
-        #
-    # @param ips [String] IP range(s)
-    # @return [true] if actions on ips are allowed.
-    # @return [false] if actions are not allowed on ips.
-    def allow_actions_on?(ips)
-      return true unless limit_to_network
-      return true unless boundary
-      return true if boundary.empty?
-      boundaries = Shellwords.split(boundary)
-      return true if boundaries.empty? # It's okay if there is no boundary range after all
-      given_range = Rex::Socket::RangeWalker.new(ips)
-      return false unless given_range # Can't do things to nonexistant IPs
-      allowed = false
-      boundaries.each do |boundary_range|
-        ok_range = Rex::Socket::RangeWalker.new(boundary)
-        allowed  = true if ok_range.include_range? given_range
-      end
-      return allowed
-    end
+      module ClassMethods
+    def form_configurable(name, *args)
+      options = args.extract_options!.reverse_merge(roles: [], type: :string)
     
-          case Rails.env
-      when 'development'
-        config.eager_load = false
-      when 'test'
-        config.eager_load = false
-      when 'production'
-        config.eager_load = true
-      end
-    end
-  end
+      def index
+    set_table_sort sorts: %w[provider name global], default: { provider: :asc }
+    
+    # For compatibility with jquery-rails (and other engines that need action_view) in pro
+require 'action_controller/railtie'
+require 'action_view/railtie'
+    
+          when nil
+        # No matches, no saved state
+      else
+        sessions[s[:session]].merge!({k => matches})
+      end # end case matched
+    end # end of each_key
+  end # end of parse
 end
     
-      def parse(pkt)
-    # We want to return immediantly if	we do not have a packet which is handled by us
-    return unless pkt.is_tcp?
-    return if (pkt.tcp_sport != 80 and pkt.tcp_dport != 80)
-    s = find_session((pkt.tcp_sport == 80) ? get_session_src(pkt) : get_session_dst(pkt))
     
-    		elsif line =~ /\w+:\t/
-			block_do(line)
+    {	if ln =~ /;(read|write)_(handle|filename)=/
+		parts = ln.split(' ')
+		if (parts[0] == 'mov')
+			parts2 = parts[2].split('=')
+			label = parts2[0]
+			label.slice!(0,1)
+			old = parts2[1]
+			new = addrs[label]
+			#puts '%32s: %s -> %x' % [label, old, new]
+			replaces << [label, old, new.to_s(16)]
+		end
+	end
+}
     
-      describe ':queue_private_receive' do
-    let(:data) { '<diaspora/>' }
+    clsCreateJar._invoke('createJarArchive', 'Ljava.io.File;[Ljava.io.File;', fileOutJar, filesIn)
     
-          private
+    (allow file-read-metadata)
+(allow file-read*
+  ; This is currenly only added because using `xcodebuild` to build a resource
+  ; bundle target starts a FSEvents stream on `/`. No idea why that would be
+  ; needed, but for now it doesn’t seem like a real problem.
+  (literal '/')
+  (regex
+    ; TODO see if we can restrict this more, but it's going to be hard
+    #'^/Users/[^.]+/*'
+    ;#'^/Users/[^.]+/.netrc'
+    ;#'^/Users/[^.]+/.gemrc'
+    ;#'^/Users/[^.]+/.gem/*'
+    ;#'^/Users/[^.]+/Library/.*'
+    #'^/Library/*'
+    #'^/System/Library/*'
+    #'^/usr/lib/*'
+    #'^/usr/share/*'
+    #'^/private/*'
+    #'^/dev/*'
+    #'^<%= ruby_prefix %>'
+    #'^<%= pod_prefix %>'
+    #'^<%= xcode_app_path %>'
+    #'^<%= Pod::Config.instance.repos_dir %>'
+<% prefixes.each do |prefix| %>
+    #'^<%= prefix %>/*'
+<% end %>
+  )
+)
     
-      %w(quiet silent verbose).each do |switch|
-    it 'doesn't include --#{switch} in help' do
-      expect(help_output).not_to match(/--#{switch}/)
-    end
-  end
-    
-    module RuboCop
-  module Cop
-    class VariableForce
-      # A Variable represents existence of a local variable.
-      # This holds a variable declaration node,
-      # and some states of the variable.
-      class Variable
-        VARIABLE_DECLARATION_TYPES =
-          (VARIABLE_ASSIGNMENT_TYPES + ARGUMENT_DECLARATION_TYPES).freeze
-    
-            def autocorrect(node)
-          lambda do |corrector|
-            each_unnecessary_space_match(node) do |range|
-              corrector.replace(range, ' ')
-            end
-          end
+            def initialize(argv)
+          @pod_name = argv.shift_argument
+          @short_output = argv.flag?('short')
+          super
         end
-    
-            def on_block(node)
-          on_body_of_reduce(node) do |body|
-            void_next = body.each_node(:next).find do |n|
-              n.children.empty? && parent_block_node(n) == node
-            end
-    
-    module RuboCop
-  module Cop
-    module Style
-      # This cop checks for optional arguments to methods
-      # that do not come at the end of the argument list
-      #
-      # @example
-      #   # bad
-      #   def foo(a = 1, b, c)
-      #   end
-      #
-      #   # good
-      #   def baz(a, b, c = 1)
-      #   end
-      #
-      #   def foobar(a = 1, b = 2, c = 3)
-      #   end
-      class OptionalArguments < Cop
-        MSG = 'Optional arguments should appear at the end ' \
-              'of the argument list.'.freeze
-    
-          # Checks whether this node body is a void context.
-      #
-      # @return [Boolean] whether the `def` node body is a void context
-      def void_context?
-        method?(:initialize) || assignment_method?
-      end
-    
-              def plugins
-            @plugins ||= find_plugins_gem_specs.map do |spec|
-              { :name => spec.name, :version => spec.version.to_s }
-            end.sort_by do |spec|
-              spec[:name]
-            end
-          end
