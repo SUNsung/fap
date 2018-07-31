712265b45c6a35b9b64df0f390cf2ca8254c9933
SUNsung <weatherius@gmail.com>
2018-07-31 08:59:16 +0300

    Rewrite [57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb] File in: 2018-07-31 08:59:16

--- ruby/57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb
+++ ruby/57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb
@@ -1,71 +1,31 @@
 
         
-        if profile_filename = ENV['PROFILE']
-  require 'ruby-prof'
-  reporter =
-    case (profile_extname = File.extname(profile_filename))
-    when '.txt'
-      RubyProf::FlatPrinterWithLineNumbers
-    when '.html'
-      RubyProf::GraphHtmlPrinter
-    when '.callgrind'
-      RubyProf::CallTreePrinter
-    else
-      raise 'Unknown profiler format indicated by extension: #{profile_extname}'
-    end
-  File.open(profile_filename, 'w') do |io|
-    reporter.new(RubyProf.profile { Pod::Command.run(ARGV) }).print(io)
-  end
-else
-  Pod::Command.run(ARGV)
-end
-
-    
-        it 'leaves TempFiles untouched' do
-      mock_app do |env|
-        request = Rack::Request.new(env)
-        [200, {'Content-Type' => 'text/plain'}, [request.params['file'][:filename] + '\n' + \
-                                                 request.params['file'][:tempfile].read + '\n' + \
-                                                 request.params['other']]]
-      end
-    
-      it 'should not override the header if already set' do
-    mock_app with_headers('X-Frame-Options' => 'allow')
-    expect(get('/', {}, 'wants' => 'text/html').headers['X-Frame-Options']).to eq('allow')
-  end
-end
-
-    
-        { # yes, this is ugly, feel free to change that
-      '/..' => '/', '/a/../b' => '/b', '/a/../b/' => '/b/', '/a/.' => '/a/',
-      '/%2e.' => '/', '/a/%2E%2e/b' => '/b', '/a%2f%2E%2e%2Fb/' => '/b/',
-      '//' => '/', '/%2fetc%2Fpasswd' => '/etc/passwd'
-    }.each do |a, b|
-      it('replaces #{a.inspect} with #{b.inspect}') { expect(get(a).body).to eq(b) }
-    end
-    
-                yield arguments[optarg_position]
+                  # Check if invalid uppercase words are at the start of a
+          # formula's desc
+          if !VALID_LOWERCASE_WORDS.include?(string_content(desc).split.first) &&
+             regex_match_group(desc, /^[a-z]/)
+            problem 'Description should start with a capital letter'
           end
-        end
     
-          # Custom destructuring method. This can be used to normalize
-      # destructuring for different variations of the node.
-      #
-      # In this case, the `def` node destructures into:
-      #
-      #   `method_name, arguments, body`
-      #
-      # while the `defs` node destructures into:
-      #
-      #   `receiver, method_name, arguments, body`
-      #
-      # so we reverse the destructured array to get the optional receiver
-      # at the end, where it can be discarded.
-      #
-      # @return [Array] the different parts of the `def` or `defs` node
-      def node_parts
-        to_a.reverse
+        export LANG=en_US.UTF-8
+    DOC
+  else
+    STDERR.puts <<-DOC
+    \e[33mWARNING: CocoaPods requires your terminal to be using UTF-8 encoding.
+    Consider adding the following to ~/.profile:
+    
+              If you don't give a pod `NAME`, you need to specify the `--all`
+          flag (this is to avoid cleaning all the cache by mistake).
+        DESC
+    
+            self.description = <<-DESC
+          Shows the content of the pods cache as a YAML tree output, organized by pod.
+          If `NAME` is given, only the caches for that pod will be included in the output.
+        DESC
+    
+    ```
+#{plugins_string}
+```
+#{markdown_podfile}
+EOS
       end
-    end
-  end
-end
