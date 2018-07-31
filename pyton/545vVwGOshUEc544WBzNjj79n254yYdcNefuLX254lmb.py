2fdb646f4c0dbcc1c9d6ebf8a32f23d638ea7e46
SUNsung <weatherius@gmail.com>
2018-07-31 20:19:03 +0300

    Rewrite [545vVwGOshUEc544WBzNjj79n254yYdcNefuLX254lmb.py] File in: 2018-07-31 20:19:02

--- pyton/545vVwGOshUEc544WBzNjj79n254yYdcNefuLX254lmb.py
+++ pyton/545vVwGOshUEc544WBzNjj79n254yYdcNefuLX254lmb.py
@@ -1,98 +1,43 @@
 
         
-                # Keyword arguments > stream.encoding > default utf8
-        if self.stdin_encoding is None:
-            self.stdin_encoding = getattr(
-                self.stdin, 'encoding', None) or 'utf8'
-        if self.stdout_encoding is None:
-            actual_stdout = self.stdout
-            if is_windows:
-                # noinspection PyUnresolvedReferences
-                from colorama import AnsiToWin32
-                if isinstance(self.stdout, AnsiToWin32):
-                    actual_stdout = self.stdout.wrapped
-            self.stdout_encoding = getattr(
-                actual_stdout, 'encoding', None) or 'utf8'
+        
+class PollErrorTest(unittest.TestCase):
+    '''Tests for acme.errors.PollError.'''
     
-        def __init__(self, env=Environment(), **kwargs):
     
-        exc = Timeout('Request timed out')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR_TIMEOUT
-    assert error_msg == 'Request timed out (30s).'
+def create_response(status_code=200, headers=None):
+    '''Creates a requests.Response object for testing'''
+    response = requests.Response()
+    response.status_code = status_code
+    
+    
+if __name__ == '__main__':
+    main()
 
     
+        def test_weak_etag_not_match(self):
+        computed_etag = ''xyzzy2''
+        etags = 'W/'xyzzy1''
+        self.check_url(
+            '/cache/' + computed_etag, method='GET',
+            headers=[('If-None-Match', etags)],
+            expected_status=200)
     
-def load_json_preserve_order(s):
-    return json.loads(s, object_pairs_hook=OrderedDict)
+        url = options.url + '/updateReports?agent=%s' % options.name
+    update_ws = yield websocket_connect(url, None)
+    msg = yield update_ws.read_message()
+    assert msg is None
+    IOLoop.instance().stop()
     
     
-@mock.patch('httpie.input.AuthCredentials._getpass',
-            new=lambda self, prompt: 'password')
-def test_password_prompt(httpbin):
-    r = http('--auth', 'user',
-             'GET', httpbin.url + '/basic-auth/user/password')
-    assert HTTP_OK in r
-    assert r.json == {'authenticated': True, 'user': 'user'}
+def main():
+    tornado.options.parse_command_line()
+    if not (options.facebook_api_key and options.facebook_secret):
+        print('--facebook_api_key and --facebook_secret must be set')
+        return
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
     
-        def test_binary_included_and_correct_when_suitable(self):
-        env = MockEnvironment(stdin_isatty=True, stdout_isatty=False)
-        r = http('GET', self.url, env=env)
-        assert r == self.bindata
-
-    
-        @pytest.mark.parametrize('header, expected_filename', [
-        ('attachment; filename=hello-WORLD_123.txt', 'hello-WORLD_123.txt'),
-        ('attachment; filename='.hello-WORLD_123.txt'', 'hello-WORLD_123.txt'),
-        ('attachment; filename='white space.txt'', 'white space.txt'),
-        (r'attachment; filename='\'quotes\'.txt'', ''quotes'.txt'),
-        ('attachment; filename=/etc/hosts', 'hosts'),
-        ('attachment; filename=', None)
-    ])
-    def test_Content_Disposition_parsing(self, header, expected_filename):
-        assert filename_from_content_disposition(header) == expected_filename
-    
-    from scrapy.commands import ScrapyCommand
-from scrapy.contracts import ContractsManager
-from scrapy.utils.misc import load_object
-from scrapy.utils.conf import build_component_list
-    
-            if not assertion:
-            if self.min_bound == self.max_bound:
-                expected = self.min_bound
-            else:
-                expected = '%s..%s' % (self.min_bound, self.max_bound)
-    
-        def __init__(self, headers):
-        self._headers = headers
-    
-        def process_request(self, request, spider):
-        auth = getattr(self, 'auth', None)
-        if auth and b'Authorization' not in request.headers:
-            request.headers[b'Authorization'] = auth
-
-    
-        @classmethod
-    def from_crawler(cls, crawler):
-        o = cls(crawler.settings['USER_AGENT'])
-        crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
-        return o
-    
-        def close(self, reason):
-        if self.file:
-            self.file.close()
-    
-        failures = []
-    while point is not None:
-        if point.name:
-            if re.search('h[1-2]', point.name):
-                if point.name == 'h1':
-                    h1_directory = os.path.join(output_directory, clean_text(point.text))
-                    current_directory = h1_directory
-                elif point.name == 'h2':
-                    current_directory = os.path.join(h1_directory, clean_text(point.text))  
-                if not os.path.exists(current_directory):
-                    os.makedirs(current_directory)
-                print_title(point.text)
+    import time
+from datetime import timedelta
