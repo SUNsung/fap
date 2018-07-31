572e937bedd7b32d40107a353f98c052fb453001
SUNsung <weatherius@gmail.com>
2018-07-31 13:43:18 +0300

    Rewrite [56mOEBmaE6br256IHGaM4Kh5Us56xp0cFRW.uvw56Sc.py] File in: 2018-07-31 13:43:18

--- pyton/56mOEBmaE6br256IHGaM4Kh5Us56xp0cFRW.uvw56Sc.py
+++ pyton/56mOEBmaE6br256IHGaM4Kh5Us56xp0cFRW.uvw56Sc.py
@@ -1,61 +1,172 @@
 
         
-        
-def set_best_server(is_local=False):
-    return 'Developing'
-
+            This could be extended by having nested blocks, sorting them recursively
+    and flattening the end structure into a list of lines. Revision 2 maybe ^.^.
+'''
     
-        def reset(self):
-        self.buffer_list = []
-        self.string_len = 0
+    import os
+import os.path
     
-        def _create_ssl_connection(self, ip):
-        if not self.check_local_network.is_ok(ip):
-            self.logger.debug('connect %s blocked: network fail', ip)
-            self.ip_manager.ssl_closed(ip, 'network fail')
-            time.sleep(10)
-            return
+      # Create a tensor of 'target IDs'.  In each row of each sub-matrix, the
+  # positions of valid source tokens are filled with the 1-origin index of that
+  # row in the entire batch, and zero elsewhere.  For example, given a batch
+  # with num_nodes=[2, 3] we might have
+  #   [[[1, 1, 0],
+  #     [2, 2, 0],
+  #     [3, 3, 0]],
+  #    [[4, 4, 4],
+  #     [5, 5, 5],
+  #     [6, 6, 6]]]
+  #
+  # TODO(googleuser): The dynamic masking is pretty awkward.  Find an op that does
+  # this (I looked, but maybe not hard enough), or write a custom op for this.
+  valid_tokens_bx1xm = tf.expand_dims(valid_tokens_bxm, 1)
+  valid_sources_bxmxm = tf.tile(valid_tokens_bx1xm, [1, max_nodes, 1])
+  sequence_bm = 1 + tf.range(total_nodes, dtype=tf.int32)
+  sequence_bxmx1 = tf.reshape(sequence_bm, [batch_size, max_nodes, 1])
+  target_ids_bxmxm = valid_sources_bxmxm * sequence_bxmx1
+    
+      test_spec_linked = '''
+      component {
+        name: 'bi_lstm'
+        backend { registered_name: 'TestComponent' }
+        fixed_feature {
+          name: 'words'
+          fml: 'words'
+          size: 1
+          embedding_dim: 32
+          vocabulary_size: 1079813,
+        }
+        linked_feature {
+          name: 'lstm_h'
+          fml: 'bias(0)'
+          embedding_dim: -1
+          size: 1
+          source_component: 'bi_lstm'
+          source_translator: 'history'
+          source_layer: 'lstm_h'
+        }
+        linked_feature {
+          name: 'lstm_c'
+          fml: 'bias(0)'
+          embedding_dim: -1
+          size: 1
+          source_component: 'bi_lstm'
+          source_translator: 'history'
+          source_layer: 'lstm_c'
+        }
+        network_unit {
+          registered_name: 'LSTMNetwork'
+          parameters {
+            key: 'hidden_layer_sizes'
+            value: '128'
+          }
+        }
+      }
+    '''
+    
+      def testReadFirstSentence(self):
+    reader = sentence_io.ConllSentenceReader(self.filepath, 1)
+    sentences, last = reader.read()
+    self.assertEqual(1, len(sentences))
+    pb = sentence_pb2.Sentence()
+    pb.ParseFromString(sentences[0])
+    self.assertFalse(last)
+    self.assertEqual(
+        u'I knew I could do it properly if given the right kind of support .',
+        pb.text)
+    
+    py_binary(
+    name = 'legacy_parse_to_conll',
+    srcs = ['legacy_parse_to_conll.py'],
+    tags = [
+        'notap',
+        'optonly',
+    ],
+    deps = [
+        ':components',
+        '//dragnn/python:dragnn_ops',
+        '//dragnn/python:evaluation',
+        '//dragnn/python:spec_builder',
+        '@absl_py//absl/flags',
+    ],
+)
+    
+      # Graph building.
+  tf.logging.info('Building the graph')
+  g = tf.Graph()
+  with g.as_default(), tf.device('/device:CPU:0'):
+    hyperparam_config = spec_pb2.GridPoint()
+    hyperparam_config.use_moving_average = True
+    builder = graph_builder.MasterBuilder(master_spec, hyperparam_config)
+    annotator = builder.add_annotation()
+    builder.add_saver()
+    
+    from dragnn.protos import spec_pb2
+from dragnn.python import evaluation
+from dragnn.python import graph_builder
+from dragnn.python import sentence_io
+from dragnn.python import spec_builder
+from dragnn.python import trainer_lib
+from syntaxnet.ops import gen_parser_ops
+from syntaxnet.util import check
+    
+    from syntaxnet.ops import gen_parser_ops
+from syntaxnet.util import check
+    
+    from syntaxnet.ops import gen_parser_ops
+from syntaxnet import task_spec_pb2
+from syntaxnet import sentence_pb2
+    
+      tf.logging.info('Loading MasterSpec...')
+  master_spec = spec_pb2.MasterSpec()
+  with gfile.FastGFile(FLAGS.dragnn_spec, 'r') as fin:
+    text_format.Parse(fin.read(), master_spec)
+  spec_builder.complete_master_spec(master_spec, None, FLAGS.resource_path)
+  logging.info('Constructed master spec: %s', str(master_spec))
+  hyperparam_config = spec_pb2.GridPoint()
+    
+            #Required string to create directory title pages
+        dir_title = '<h2 class=titledir>' + operating_sys.capitalize() + '</h2></body></html>'
+    
+            Args:
+            fn: A callable that will be called with this future as its only
+                argument when the future completes or is cancelled. The callable
+                will always be called by a thread in the same process in which
+                it was added. If the future has already completed or been
+                cancelled then the callable will be called immediately. These
+                callables are called in the order that they were added.
+        '''
+        with self._condition:
+            if self._state not in [CANCELLED, CANCELLED_AND_NOTIFIED, FINISHED]:
+                self._done_callbacks.append(fn)
+                return
+        fn(self)
+    
+        sqrt_n = int(math.floor(math.sqrt(n)))
+    for i in range(3, sqrt_n + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+    
+        try:
+        yield logging_stream
+    finally:
+        logging.root.removeHandler(handler)
     
     
-    
-    # begin[licence]
-#
-# [The 'BSD licence']
-# Copyright (c) 2005-2008 Terence Parr
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# end[licensc]
+  def IsResponseHandled( self ):
+    return self._handled_tick == self._parse_tick
     
     
-    def __str__(self):
-        return 'NoViableAltException(%r!=[%r])' % (
-            self.unexpectedType, self.grammarDecisionDescription
-            )
-    __repr__ = __str__
-    
-    
-    EOF_TOKEN = CommonToken(type=EOF)
-	
-INVALID_TOKEN = CommonToken(type=INVALID_TOKEN_TYPE)
+class CompletionRequest( BaseRequest ):
+  def __init__( self, request_data ):
+    super( CompletionRequest, self ).__init__()
+    self.request_data = request_data
+    self._response_future = None
+    self._complete_done_hooks = {
+      'cs': self._OnCompleteDone_Csharp,
+      'java': self._OnCompleteDone_FixIt,
+      'javascript': self._OnCompleteDone_FixIt,
+      'typescript': self._OnCompleteDone_FixIt,
+    }
