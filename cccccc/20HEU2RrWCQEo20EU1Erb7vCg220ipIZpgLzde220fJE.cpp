b891f829d06056d8ac956ea17a6a3929cb7438dc
SUNsung <weatherius@gmail.com>
2018-07-31 15:37:02 +0300

    Rewrite [20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp] File in: 2018-07-31 15:37:02

--- cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
+++ cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
@@ -1,177 +1,63 @@
 
         
-        namespace base {
-class FilePath;
-}
+          std::string toStyledString() const;
+    
+      virtual void WriteHash(io::Printer* printer);
+  virtual void WriteEquals(io::Printer* printer);
+  virtual void WriteToString(io::Printer* printer);
     
     
-    {}  // namespace chrome
-    
-    void TtsPlatformImpl::clear_error() {
-  error_ = std::string();
-}
-    
-    
-    {}  // namespace chrome
-    
-    #ifndef ATOM_COMMON_COLOR_UTIL_H_
-#define ATOM_COMMON_COLOR_UTIL_H_
-    
-      DebuggerClient(ASTContext &C) : Ctx(C) { }
-  virtual ~DebuggerClient() = default;
-  
-  // DebuggerClient is consulted at the beginning of the parsing
-  // of various DeclKinds to see whether the decl should be parsed
-  // in the global context rather than the current context.
-  // This question will only be asked if the decl's current context
-  // is a function marked with the LLDBDebuggerFunction attribute.
-  virtual bool shouldGlobalize(Identifier Name, DeclKind kind) = 0;
-  
-  virtual void didGlobalize (Decl *Decl) = 0;
-    
-    /// Runs the given function with the given context argument exactly once.
-/// The predicate argument must point to a global or static variable of static
-/// extent of type swift_once_t.
-SWIFT_RUNTIME_EXPORT
-void swift_once(swift_once_t *predicate, void (*fn)(void *), void *context);
-    
-    
-    {
-    {} // end namespace index
-} // end namespace swift
-    
-      /// Initialize a location with a new set of base, projectionpath and kind.
-  void init(SILValue B, const Optional<ProjectionPath> &P, KeyKind K= Normal) {
-    Base = B;
-    Path = P;
-    Kind = K;
-  }
-    
-    
-    {} // end namespace swift
-    
-    
-    {  return 0;
-}
+    {}  // namespace google
+#endif  // GOOGLE_PROTOBUF_COMPILER_JAVA_MESSAGE_H__
 
     
-    static void RunQPS() {
-  gpr_log(GPR_INFO, 'Running QPS test, open-loop');
+      std::vector<rocksdb::ColumnFamilyDescriptor> families;
+    
+    namespace rocksdb {
     }
     
-    namespace grpc {
-    }
+    #include 'util/sync_point.h'
+#include 'util/sync_point_impl.h'
     
-    	String host = p_url;
-	String path = '/';
-	int p_len = -1;
-	int port = 80;
-	bool ssl = false;
-	if (host.begins_with('wss://')) {
-		ssl = true; // we should implement this
-		host = host.substr(6, host.length() - 6);
-		port = 443;
-	} else {
-		ssl = false;
-		if (host.begins_with('ws://'))
-			host = host.substr(5, host.length() - 5);
-	}
+      // Timestamp of oldest key
+  std::atomic<uint64_t> oldest_key_time_;
     
-    public:
-	Error connect_to_url(String p_url, PoolVector<String> p_protocols = PoolVector<String>(), bool gd_mp_api = false);
-    
-    /*
- * We assume CHAR_BIT is 8 in many places. In practice, this is true on our
- * target platforms, so not an issue, but let's just be extra sure.
- */
-#include <limits.h>
-#if CHAR_BIT != 8
-#error 'mbed TLS requires a platform with 8-bit chars'
-#endif
-    
-    	bool initialized;
-    
-      Copyright 2003 by
-  Francesco Zappa Nardelli
-    
-        // Returns the next scan line.
-    // For grayscale images, pScan_line will point to a buffer containing 8-bit pixels (get_bytes_per_pixel() will return 1). 
-    // Otherwise, it will always point to a buffer containing 32-bit RGBA pixels (A will always be 255, and get_bytes_per_pixel() will return 4).
-    // Returns JPGD_SUCCESS if a scan line has been returned.
-    // Returns JPGD_DONE if all scan lines have been returned.
-    // Returns JPGD_FAILED if an error occurred. Call get_error_code() for a more info.
-    int decode(const void** pScan_line, uint* pScan_line_len);
-    
-    inline jpgd_status get_error_code() const { return m_error_code; }
-    
-    #include 'x86/x86cpu.h'
-/* We currently support 5 x86 variants:
- * arch[0] -> non-sse
- * arch[1] -> sse
- * arch[2] -> sse2
- * arch[3] -> sse4.1
- * arch[4] -> avx
- */
-#define OPUS_ARCHMASK 7
-int opus_select_arch(void);
-    
-       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
-   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-    
-       - Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
-    
-       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
-   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-    
-    namespace leveldb {
-    }
-    
-      // count the keys
-  leveldb::Iterator* iter = db->NewIterator(leveldb::ReadOptions());
-  size_t num_keys = 0;
-  for (iter->SeekToFirst(); iter->Valid(); iter->Next()) {
-    num_keys++;
-  }
-  delete iter;
-  ASSERT_EQ(kNumKeys, num_keys) << 'Bad number of keys';
-    
-    // A FilterBlockBuilder is used to construct all of the filters for a
-// particular Table.  It generates a single string which is stored as
-// a special block in the Table.
-//
-// The sequence of calls to FilterBlockBuilder must match the regexp:
-//      (StartBlock AddKey*)* Finish
-class FilterBlockBuilder {
+    class CompactionFilterFactoryJniCallback : public JniCallback, public CompactionFilterFactory {
  public:
-  explicit FilterBlockBuilder(const FilterPolicy*);
+    CompactionFilterFactoryJniCallback(
+        JNIEnv* env, jobject jcompaction_filter_factory);
+    virtual std::unique_ptr<CompactionFilter> CreateCompactionFilter(
+      const CompactionFilter::Context& context);
+    virtual const char* Name() const;
     }
     
-    int main(int argc, char** argv) {
-  return leveldb::test::RunAllTests();
-}
-
+    struct ComparatorJniCallbackOptions {
+  // Use adaptive mutex, which spins in the user space before resorting
+  // to kernel. This could reduce context switch when the mutex is not
+  // heavily contended. However, if the mutex is hot, we could end up
+  // wasting spin time.
+  // Default: false
+  bool use_adaptive_mutex;
+    }
     
-    #include <stddef.h>
-#include <stdint.h>
-#include 'leveldb/iterator.h'
+      std::mutex              mutex_;
+  std::condition_variable cv_;
+  // sync points that have been passed through
+  std::unordered_set<std::string> cleared_points_;
+  std::atomic<bool> enabled_;
+  int num_callbacks_running_ = 0;
+    
+      pthread_key_t k = TlsAlloc();
+  if (TLS_OUT_OF_INDEXES == k) {
+    return ENOMEM;
+  }
+    
+    void HistogramStat::Add(uint64_t value) {
+  // This function is designed to be lock free, as it's in the critical path
+  // of any operation. Each individual value is atomic and the order of updates
+  // by concurrent threads is tolerable.
+  const size_t index = bucketMapper.IndexForValue(value);
+  assert(index < num_buckets_);
+  buckets_[index].store(buckets_[index].load(std::memory_order_relaxed) + 1,
+                        std::memory_order_relaxed);
+    }
