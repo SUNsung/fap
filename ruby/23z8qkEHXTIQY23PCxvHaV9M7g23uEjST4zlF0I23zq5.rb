3582ae23062434227dd5a02ad53dc94a9c1940cb
SUNsung <weatherius@gmail.com>
2018-08-01 11:33:14 +0300

    Rewrite [23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb] File in: 2018-08-01 11:33:13

--- ruby/23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb
+++ ruby/23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb
@@ -1,90 +1,121 @@
 
         
-              # Generate a Hash which breaks the recursive chain.
-      # Certain fields which are normally available are omitted.
+                  @env.action_runner.run(Vagrant::Action.action_box_remove, {
+            box_name:     argv[0],
+            box_provider: options[:provider],
+            box_version:  options[:version],
+            force_confirm_box_remove: options[:force],
+            box_remove_all_versions: options[:all],
+          })
+    
+    	SIZE1 = 28
+	SIZE2 = 28 + 4 + 32
+	SIZE3 = 28 + 4 + 32 + 4
+    
+          opts.on('--unix-newlines', 'Use Unix-style newlines in written files.',
+                                 ('Always true on Unix.' unless Sass::Util.windows?)) do
+        @options[:unix_newlines] = true if Sass::Util.windows?
+      end
+    end
+    
+          # Returns the time the given Sass file was last modified.
       #
-      # Returns a Hash with only non-recursive fields present.
-      def collapse_document(doc)
-        doc.keys.each_with_object({}) do |(key, _), result|
-          result[key] = doc[key] unless NESTED_OBJECT_FIELD_BLACKLIST.include?(key)
-        end
+      # If the given file has been deleted or the time can't be accessed
+      # for some other reason, this should return nil.
+      #
+      # @param uri [String] The URI of the file to check.
+      #   Comes from a `:filename` option set on an engine returned by this importer.
+      # @param options [{Symbol => Object}] Options for the Sass file
+      #   containing the `@import` currently being checked.
+      # @return [Time, nil]
+      def mtime(uri, options)
+        Sass::Util.abstract(self)
       end
-    end
-  end
+    
+      module Sass::Plugin::Configuration
+    # Different default options in a m environment.
+    def default_options
+      @default_options ||= begin
+        version = Merb::VERSION.split('.').map {|n| n.to_i}
+        if version[0] <= 0 && version[1] < 5
+          root = MERB_ROOT
+          env  = MERB_ENV
+        else
+          root = Merb.root.to_s
+          env  = Merb.environment
+        end
+    
+    @@ layout
+<html>
+  <head>
+    <title>Super Simple Chat with Sinatra</title>
+    <meta charset='utf-8' />
+    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
+  </head>
+  <body><%= yield %></body>
+</html>
+    
+    class Rack::Builder
+  include Sinatra::Delegator
 end
 
     
-        Or via the Cask:
-      brew cask install ngrok
-    EOS
-  end
-end
-
+        it 'Reads referrer from Host header when Referer header is missing' do
+      env = {'HTTP_HOST' => 'foo.com'}
+      expect(subject.referrer(env)).to eq('foo.com')
+    end
     
-      def keys
-    @checksums.keys
-  end
+        it 'denies requests with duplicate session cookies' do
+      get '/', {}, 'HTTP_COOKIE' => 'rack.session=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
+      expect(last_response).not_to be_ok
+    end
     
-    class BuildEnvironment
-  def initialize(*settings)
-    @settings = Set.new(*settings)
-  end
-    
-      # True if a {Formula} is being built with {Formula.devel} instead of {Formula.stable}.
-  # <pre>args << '--some-beta' if build.devel?</pre>
-  def devel?
-    include? 'devel'
-  end
-    
-          if f.plist_manual
-        s << 'Or, if you don't want/need launchctl, you can just run:'
-        s << '  #{f.plist_manual}'
+      %w(GET HEAD POST PUT DELETE).each do |method|
+    it 'accepts #{method} requests when allow_if is true' do
+      mock_app do
+        use Rack::Protection::HttpOrigin, :allow_if => lambda{|env| env.has_key?('HTTP_ORIGIN') }
+        run DummyApp
       end
-    
-      def self.canonical_name(name)
-    Formulary.canonical_name(name)
-  end
-    
-      def index
-    return redirect_to path('/login') if SiteSetting.login_required? && current_user.nil?
-    
-      def create
-    color_scheme = ColorScheme.create(color_scheme_params)
-    if color_scheme.valid?
-      render json: color_scheme, root: false
-    else
-      render_json_error(color_scheme)
+      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://any.domain.com')).to be_ok
     end
   end
     
-      def index
-    url = params[:filter]
-    permalinks = Permalink.filter_by(url)
-    render_serialized(permalinks, PermalinkSerializer)
-  end
+        %w[/foo/bar /foo/bar/ / /.f /a.x].each do |path|
+      it('does not touch #{path.inspect}') { expect(get(path).body).to eq(path) }
+    end
     
-          def to_h
-        directives.to_h
+      desc 'Release all gems to rubygems'
+  task release: :build do
+    sh 'git tag -a -m \'Version #{version}\' v#{version}'
+    
+        # Returns a String describing the file's content type
+    def detect
+      if blank_name?
+        SENSIBLE_DEFAULT
+      elsif empty_file?
+        EMPTY_TYPE
+      elsif calculated_type_matches.any?
+        calculated_type_matches.first
+      else
+        type_from_file_contents || SENSIBLE_DEFAULT
+      end.to_s
+    end
+    
+            def type_allowed?(type)
+          @subject.send('#{@attachment_name}_content_type=', type)
+          @subject.valid?
+          @subject.errors[:'#{@attachment_name}_content_type'].blank?
+        end
+    
+            def matches? subject
+          @subject = subject
+          @subject = @subject.new if @subject.class == Class
+          lower_than_low? && higher_than_low? && lower_than_high? && higher_than_high?
+        end
+    
+            if record.errors.include? attribute
+          record.errors[attribute].each do |error|
+            record.errors.add base_attribute, error
+          end
+        end
       end
-    
-          def run_installer(command: nil, verbose: false, **_options)
-        ohai 'Running installer for #{cask}; your password may be necessary.'
-        ohai 'Package installers may write to any location; options such as --appdir are ignored.'
-        unless path.exist?
-          raise CaskError, 'pkg source file not found: '#{path.relative_path_from(cask.staged_path)}''
-        end
-        args = [
-          '-pkg',    path,
-          '-target', '/'
-        ]
-        args << '-verboseR' if verbose
-        if stanza_options.fetch(:allow_untrusted, false)
-          args << '-allowUntrusted'
-        end
-        with_choices_file do |choices_path|
-          args << '-applyChoiceChangesXML' << choices_path if choices_path
-          command.run!('/usr/sbin/installer', sudo: true, args: args, print_stdout: true)
-        end
-      end
-    
-          private
