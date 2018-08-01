91d61372a8205cac1e22a25c534e055c83d812b8
SUNsung <weatherius@gmail.com>
2018-08-01 12:41:23 +0300

    Rewrite [49InRrhCrP0M649crFe61w0ji.49FDh1Swbl2S.49O9e.py] File in: 2018-08-01 12:41:22

--- pyton/49InRrhCrP0M649crFe61w0ji.49FDh1Swbl2S.49O9e.py
+++ pyton/49InRrhCrP0M649crFe61w0ji.49FDh1Swbl2S.49O9e.py
@@ -1,3 +1,182 @@
 
         
         
+FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
+FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
+JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
+BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
+    
+    
+def test_unicode_url_query_arg_item_verbose(httpbin):
+    r = http('--verbose', httpbin.url + '/get', u'test==%s' % UNICODE)
+    assert HTTP_OK in r
+    assert UNICODE in r
+    
+    
+class TestClientCert:
+    
+        def __init__(self, directory=DEFAULT_CONFIG_DIR):
+        super(Config, self).__init__()
+        self.update(self.DEFAULTS)
+        self.directory = directory
+    
+    
+def test_credentials_in_url(httpbin_both):
+    url = add_auth(httpbin_both.url + '/basic-auth/user/password',
+                   auth='user:password')
+    r = http('GET', url)
+    assert HTTP_OK in r
+    assert r.json == {'authenticated': True, 'user': 'user'}
+    
+    from scrapy.commands import ScrapyCommand
+from scrapy.contracts import ContractsManager
+from scrapy.utils.misc import load_object
+from scrapy.utils.conf import build_component_list
+    
+            assert len(self.args) in [1, 2, 3]
+        self.obj_name = self.args[0] or None
+        self.obj_type = self.objects[self.obj_name]
+    
+        @classmethod
+    def _get_mwlist_from_settings(cls, settings):
+        return build_component_list(
+            settings.getwithbase('DOWNLOADER_MIDDLEWARES'))
+    
+        def scrape_response(self, scrape_func, response, request, spider):
+        fname = lambda f:'%s.%s' % (
+                six.get_method_self(f).__class__.__name__,
+                six.get_method_function(f).__name__)
+    
+        def spider_opened(self, spider):
+        self._timeout = getattr(spider, 'download_timeout', self._timeout)
+    
+        def __init__(self, path=None, debug=False):
+        self.file = None
+        self.fingerprints = set()
+        self.logdupes = True
+        self.debug = debug
+        self.logger = logging.getLogger(__name__)
+        if path:
+            self.file = open(os.path.join(path, 'requests.seen'), 'a+')
+            self.file.seek(0)
+            self.fingerprints.update(x.rstrip() for x in self.file)
+    
+        long_description = README,
+    
+    class Bigthink(VideoExtractor):
+    name = 'Bigthink'
+    
+    site_info = 'CBS.com'
+download = cbs_download
+download_playlist = playlist_not_supported('cbs')
+
+    
+    	xml = get_html('http://www.ehow.com/services/video/series.xml?demand_ehow_videoid=%s' % vid)
+    
+	from xml.dom.minidom import parseString
+	doc = parseString(xml)
+	tab = doc.getElementsByTagName('related')[0].firstChild
+    
+    
+def huaban_download(url, output_dir='.', **kwargs):
+    if re.match(r'http://huaban\.com/boards/\d+/', url):
+        huaban_download_board(url, output_dir, **kwargs)
+    else:
+        print('Only board (画板) pages are supported currently')
+        print('ex: http://huaban.com/boards/12345678/')
+    
+    __all__ = ['mixcloud_download']
+    
+        # For pickling to work, the __module__ variable needs to be set to the frame
+    # where the named tuple is created.  Bypass this step in enviroments where
+    # sys._getframe is not defined (Jython for example).
+    if hasattr(_sys, '_getframe'):
+        result.__module__ = _sys._getframe(1).f_globals.get('__name__', '__main__')
+    
+    +----------+     +----------+       +--------+     +-----------+    +---------+
+|          |  => | Work Ids |    => |        |  => | Call Q    | => |         |
+|          |     +----------+       |        |     +-----------+    |         |
+|          |     | ...      |       |        |     | ...       |    |         |
+|          |     | 6        |       |        |     | 5, call() |    |         |
+|          |     | 7        |       |        |     | ...       |    |         |
+| Process  |     | ...      |       | Local  |     +-----------+    | Process |
+|  Pool    |     +----------+       | Worker |                      |  #1..n  |
+| Executor |                        | Thread |                      |         |
+|          |     +----------- +     |        |     +-----------+    |         |
+|          | <=> | Work Items | <=> |        | <=  | Result Q  | <= |         |
+|          |     +------------+     |        |     +-----------+    |         |
+|          |     | 6: call()  |     |        |     | ...       |    |         |
+|          |     |    future  |     |        |     | 4, result |    |         |
+|          |     | ...        |     |        |     | 3, except |    |         |
++----------+     +------------+     +--------+     +-----------+    +---------+
+    
+        def test_del_shutdown(self):
+        executor = futures.ThreadPoolExecutor(max_workers=5)
+        executor.map(abs, range(-5, 5))
+        threads = executor._threads
+        del executor
+    
+    
+DIAGNOSTIC_UI_FILETYPES = { 'cpp', 'cs', 'c', 'objc', 'objcpp', 'cuda',
+                            'javascript', 'typescript' }
+DIAGNOSTIC_UI_ASYNC_FILETYPES = { 'java' }
+    
+      # This test validates the behaviour of YouCompleteMe.HandleFileParseRequest
+  # in combination with YouCompleteMe.OnFileReadyToParse when the completer
+  # raises an exception handling FileReadyToParse event notification
+  ERROR_TEXT = 'Some completer response text'
+    
+        def DetailedInfoTest( command, info ):
+      with patch( 'ycm.vimsupport.WriteToPreviewWindow' ) as write_to_preview:
+        request = CommandRequest( [ command ] )
+        request._response = { 'detailed_info': info }
+        request.RunPostCommandActionsIfNeeded( 'topleft' )
+        write_to_preview.assert_called_with( info )
+    
+      vim_buffer[ start_line : end_line + 1 ] = replacement_lines[ : ]
+    
+        def on_switchover(self):
+        super(Standby, self).on_switchover() #message ignored
+        self._hsm._next_state('active')
+    
+        def test_car_adapter_shall_make_very_loud_noise(self):
+        car = Car()
+        car_adapter = Adapter(car, make_noise=car.make_noise)
+        noise = car_adapter.make_noise(10)
+        expected_noise = 'vroom!!!!!!!!!!'
+    
+        def test_c_observers_shall_be_detachable(cls):
+        cls.s.detach(cls.dec_obs)
+        # hex viewer shall be remaining if dec viewer is detached first
+        cls.assertEqual(isinstance(cls.s._observers[0], HexViewer), True)
+        cls.assertEqual(len(cls.s._observers), 1)
+        cls.s.detach(cls.hex_obs)
+        cls.assertEqual(len(cls.s._observers), 0)
+    
+    ###  OUTPUT ###
+# Before subclassing:
+# BaseRegisteredClass
+# After subclassing:
+# BaseRegisteredClass
+# ClassRegistree
+
+    
+        def test_bunch_launch(self):
+        self.runner.runAll()
+        output = self.out.getvalue().strip()
+        self.assertEqual(output, str(self.average_result_tc1 + '\n\n' +
+                         self.average_result_tc2 + '\n\n' +
+                         self.average_result_tc3))
+
+    
+        def now(self):
+        current_time = datetime.datetime.now()
+        current_time_formatted = '{}:{}'.format(current_time.hour, current_time.minute)
+        return current_time_formatted
+    
+    
+class DB:
+    
+        def is_satisfied_by(self, candidate):
+        return bool(self._one.is_satisfied_by(candidate) and
+                    self._other.is_satisfied_by(candidate))
