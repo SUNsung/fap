0d477a9dd6f09fbe1b3b0043bbdad0e1f7469153
SUNsung <weatherius@gmail.com>
2018-08-01 18:32:31 +0300

    Rewrite [58I6pfbcMDyu658YJBPnmI4zqw58CUqVCUlbnLU58Jek.cpp] File in: 2018-08-01 18:32:30

--- cccccc/58I6pfbcMDyu658YJBPnmI4zqw58CUqVCUlbnLU58Jek.cpp
+++ cccccc/58I6pfbcMDyu658YJBPnmI4zqw58CUqVCUlbnLU58Jek.cpp
@@ -1,25 +1,235 @@
 
         
-        @interface LocaleBridgingTester : NSObject
-- (NSLocale *)autoupdatingCurrentLocale;
-- (BOOL)verifyAutoupdatingLocale:(NSLocale *)locale;
-@end
-    
-    
-    {/// Extract comments parts from the given Markup node.
-swift::markup::CommentParts
-extractCommentParts(swift::markup::MarkupContext &MC,
-                    swift::markup::MarkupASTNode *Node);
-} // namespace swift
-    
-    /// A shorthand to clearly indicate that a value is a reference counted and
-/// heap-allocated.
-template <typename Inner>
-using RC = llvm::IntrusiveRefCntPtr<Inner>;
-    
-    #include 'swift/Index/IndexDataConsumer.h'
-    
-      virtual ~IndexDataConsumer() {}
-    
-    namespace swift {
+        
+    {
+    {    if (SemanticNode.is<Expr *>()) {
+      SemanticNode.get<Expr *>()->dump(llvm::errs());
+    } else if (SemanticNode.is<Decl *>()) {
+      SemanticNode.get<Decl *>()->dump(llvm::errs());
+    } else if (SemanticNode.is<Expr *>()) {
+      SemanticNode.get<Expr *>()->dump(llvm::errs());
+    } else {
+      llvm_unreachable('ASTNode has pointer to unknown thing!');
     }
+    llvm::errs() << '\n=====================================================\n';
+  }
+}
+
+    
+    #include 'swift/Basic/Unicode.h'
+#include 'llvm/ADT/SmallVector.h'
+#include 'llvm/Support/ConvertUTF.h'
+    
+      Optional<const markup::LocalizationKeyField *>
+  getLocalizationKeyField() const {
+    return Parts.LocalizationKeyField;
+  }
+    
+    #ifndef SWIFT_PRINTINGDIAGNOSTICCONSUMER_H
+#define SWIFT_PRINTINGDIAGNOSTICCONSUMER_H
+    
+      void handleDiagnostic(SourceManager &SM, SourceLoc Loc,
+                        DiagnosticKind Kind,
+                        StringRef FormatString,
+                        ArrayRef<DiagnosticArgument> FormatArgs,
+                        const DiagnosticInfo &Info) override;
+    
+    #include 'llvm/ADT/ArrayRef.h'
+    
+    
+    {  NotificationCenter &getNotificationCenter() { return *NotificationCtr; }
+};
+    
+    namespace HPHP {
+    }
+    
+    Timer::Counter Timer::CounterValue(Timer::Name name) {
+  return s_counters[name];
+}
+    
+    inline bool ObjectData::hasInstanceDtor() const {
+  return HPHP::hasInstanceDtor(m_kind);
+}
+    
+    inline void RowSet::Clear() {
+  rows_.clear(); size_ = 0;
+}
+    
+      std::string tmp_file = TempFileName();
+  dmlc::Stream * fs = dmlc::Stream::Create(tmp_file.c_str(), 'w');
+  info.SaveBinary(fs);
+  delete fs;
+    
+      for (auto alphabet_size : test_cases) {
+    for (int i = 0; i < repetitions; i++) {
+      std::vector<int> input(num_elements);
+      std::generate(input.begin(), input.end(),
+        [=]() { return rand() % alphabet_size; });
+      CompressedBufferWriter cbw(alphabet_size);
+    }
+    }
+    
+    namespace detail {
+/*! \brief Implementation of gradient statistics pair. Template specialisation
+ * may be used to overload different gradients types e.g. low precision, high
+ * precision, integer, floating point. */
+template <typename T>
+class GradientPairInternal {
+  /*! \brief gradient statistics */
+  T grad_;
+  /*! \brief second order gradient statistics */
+  T hess_;
+    }
+    }
+    
+    
+    {  JPEGScanInfo scan_info;
+  scan_info.components.resize(comps_in_scan);
+  VERIFY_LEN(2 * comps_in_scan);
+  std::vector<bool> ids_seen(256, false);
+  for (int i = 0; i < comps_in_scan; ++i) {
+    int id = ReadUint8(data, pos);
+    if (ids_seen[id]) {   // (cf. section B.2.3, regarding CSj)
+      fprintf(stderr, 'Duplicate ID %d in SOS.\n', id);
+      jpg->error = JPEG_DUPLICATE_COMPONENT_ID;
+      return false;
+    }
+    ids_seen[id] = true;
+    bool found_index = false;
+    for (size_t j = 0; j < jpg->components.size(); ++j) {
+      if (jpg->components[j].id == id) {
+        scan_info.components[i].comp_idx = j;
+        found_index = true;
+      }
+    }
+    if (!found_index) {
+      fprintf(stderr, 'SOS marker: Could not find component with id %d\n', id);
+      jpg->error = JPEG_COMPONENT_NOT_FOUND;
+      return false;
+    }
+    int c = ReadUint8(data, pos);
+    int dc_tbl_idx = c >> 4;
+    int ac_tbl_idx = c & 0xf;
+    VERIFY_INPUT(dc_tbl_idx, 0, 3, HUFFMAN_INDEX);
+    VERIFY_INPUT(ac_tbl_idx, 0, 3, HUFFMAN_INDEX);
+    scan_info.components[i].dc_tbl_idx = dc_tbl_idx;
+    scan_info.components[i].ac_tbl_idx = ac_tbl_idx;
+  }
+  VERIFY_LEN(3);
+  scan_info.Ss = ReadUint8(data, pos);
+  scan_info.Se = ReadUint8(data, pos);
+  VERIFY_INPUT(scan_info.Ss, 0, 63, START_OF_SCAN);
+  VERIFY_INPUT(scan_info.Se, scan_info.Ss, 63, END_OF_SCAN);
+  int c = ReadUint8(data, pos);
+  scan_info.Ah = c >> 4;
+  scan_info.Al = c & 0xf;
+  // Check that all the Huffman tables needed for this scan are defined.
+  for (int i = 0; i < comps_in_scan; ++i) {
+    bool found_dc_table = false;
+    bool found_ac_table = false;
+    for (size_t j = 0; j < jpg->huffman_code.size(); ++j) {
+      int slot_id = jpg->huffman_code[j].slot_id;
+      if (slot_id == scan_info.components[i].dc_tbl_idx) {
+        found_dc_table = true;
+      } else if (slot_id == scan_info.components[i].ac_tbl_idx + 16) {
+        found_ac_table = true;
+      }
+    }
+    if (scan_info.Ss == 0 && !found_dc_table) {
+      fprintf(stderr, 'SOS marker: Could not find DC Huffman table with index '
+              '%d\n', scan_info.components[i].dc_tbl_idx);
+      jpg->error = JPEG_HUFFMAN_TABLE_NOT_FOUND;
+      return false;
+    }
+    if (scan_info.Se > 0 && !found_ac_table) {
+      fprintf(stderr, 'SOS marker: Could not find AC Huffman table with index '
+              '%d\n', scan_info.components[i].ac_tbl_idx);
+      jpg->error = JPEG_HUFFMAN_TABLE_NOT_FOUND;
+      return false;
+    }
+  }
+  jpg->scan_info.push_back(scan_info);
+  VERIFY_MARKER_END();
+  return true;
+}
+    
+    void OutputImageComponent::SetCoeffBlock(int block_x, int block_y,
+                                         const coeff_t block[kDCTBlockSize]) {
+  assert(block_x < width_in_blocks_);
+  assert(block_y < height_in_blocks_);
+  int offset = (block_y * width_in_blocks_ + block_x) * kDCTBlockSize;
+  memcpy(&coeffs_[offset], block, kDCTBlockSize * sizeof(coeffs_[0]));
+  uint8_t idct[kDCTBlockSize];
+  ComputeBlockIDCT(&coeffs_[offset], idct);
+  UpdatePixelsForBlock(block_x, block_y, idct);
+}
+    
+    void UpdateGuess(const std::vector<float>& target,
+                 const std::vector<float>& reconstructed,
+                 std::vector<float>* guess) {
+  assert(reconstructed.size() == guess->size());
+  assert(target.size() == guess->size());
+  for (size_t i = 0; i < guess->size(); ++i) {
+    // TODO(user): Evaluate using a decaying constant here.
+    (*guess)[i] = Clip((*guess)[i] - (reconstructed[i] - target[i]));
+  }
+}
+    
+    // Definition of error codes for parsing jpeg files.
+    
+    // Builds jpeg-style Huffman lookup table from the given symbols.
+// The symbols are in order of increasing bit lengths. The number of symbols
+// with bit length n is given in counts[n] for each n >= 1.
+// Returns the size of the lookup table.
+int BuildJpegHuffmanTable(const int* counts, const int* symbols,
+                          HuffmanTableEntry* lut);
+    
+      double v = matched_it->v();
+  double theta = matched_it->path_point().theta();
+  double v_x = v * std::cos(theta);
+  double v_y = v * std::sin(theta);
+    
+      std::vector<const Obstacle*> obstacles_;
+    
+    #include <array>
+#include <memory>
+#include <vector>
+    
+    
+    {  const Vec2d direction_vec = Vec2d::CreateUnitVec2d(heading);
+  double min_proj = std::numeric_limits<double>::infinity();
+  double max_proj = -std::numeric_limits<double>::infinity();
+  for (const auto &pt : points_) {
+    const double proj = pt.InnerProd(direction_vec);
+    if (proj < min_proj) {
+      min_proj = proj;
+      *first = pt;
+    }
+    if (proj > max_proj) {
+      max_proj = proj;
+      *last = pt;
+    }
+  }
+}
+    
+    #include 'modules/common/util/file.h'
+#include 'modules/perception/traffic_light/base/tl_shared_data.h'
+    
+      virtual ~MultiCamerasProjection() = default;
+  virtual bool Init();
+  virtual bool Project(const CarPose &pose, const ProjectOption &option,
+                       Light *light) const;
+  std::string name() const { return 'TLPreprocessor'; }
+    
+    #include 'modules/dreamview/backend/handlers/image_handler.h'
+#include 'modules/dreamview/backend/handlers/websocket_handler.h'
+#include 'modules/dreamview/backend/hmi/hmi.h'
+#include 'modules/dreamview/backend/map/map_service.h'
+#include 'modules/dreamview/backend/point_cloud/point_cloud_updater.h'
+#include 'modules/dreamview/backend/sim_control/sim_control.h'
+#include 'modules/dreamview/backend/simulation_world/simulation_world_updater.h'
+    
+    #include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
