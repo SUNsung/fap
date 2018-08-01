cf276801d378bd7016dd002fd62b59a9d4520a73
SUNsung <weatherius@gmail.com>
2018-08-01 13:03:45 +0300

    Rewrite [02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb] File in: 2018-08-01 13:03:44

--- ruby/02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb
+++ ruby/02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb
@@ -1,175 +1,236 @@
 
         
-        module ActiveRecord
-  module ConnectionAdapters
-    class ConnectionSpecification
-      class ResolverTest < ActiveRecord::TestCase
-        def resolve(spec, config = {})
-          Resolver.new(config).resolve(spec)
-        end
+        FORWARD_SLASH = '/'.freeze
     
-        # Count should come from sql count() of treasures rather than treasures_count attribute
-    assert_equal ship.treasures.size, 0
+      </body>
+</html>
+HTML
+CONTENT_NOT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
     
-          expected = { 'identifier' => '{id: 1}', 'type' => 'reject_subscription' }
-      assert_equal expected, @connection.last_transmission
-      assert_equal 1, @connection.transmissions.size
-    
-          assert_equal [ connection ], @server.connections
-      assert connection.connected
+            msg = ' Please append `--trace` to the `#{cmd.name}` command '
+        dashes = '-' * msg.length
+        Jekyll.logger.error '', dashes
+        Jekyll.logger.error 'Jekyll #{Jekyll::VERSION} ', msg
+        Jekyll.logger.error '', ' for any additional information or backtrace. '
+        Jekyll.logger.abort_with '', dashes
+      end
+      # rubocop: enable RescueException
     end
   end
+end
+
     
-        def send_async(method, *args)
-      send method, *args
-    end
-    
-            def deprecated_relative_permalinks(site)
-          if site.config['relative_permalinks']
-            Jekyll::Deprecator.deprecation_message 'Your site still uses relative permalinks,' \
-                                                   ' which was removed in Jekyll v3.0.0.'
-            true
+              External.require_with_graceful_fail 'jekyll-watch'
+          watch_method = Jekyll::Watcher.method(:watch)
+          if watch_method.parameters.size == 1
+            watch_method.call(
+              options
+            )
+          else
+            watch_method.call(
+              options, site
+            )
           end
         end
-    
-            def print_message(json_message)
-          msg = JSON.parse(json_message)
-          # Not sure what the 'url' command even does in LiveReload.  The spec is silent
-          # on its purpose.
-          Jekyll.logger.info 'LiveReload:', 'Browser URL: #{msg['url']}' if msg['command'] == 'url'
-        end
-    
-        # Public: Read configuration and return merged Hash
-    #
-    # file - the path to the YAML file to be read in
-    #
-    # Returns this configuration, overridden by the values in the file
-    def read_config_file(file)
-      next_config = safe_load_file(file)
-      check_config_is_hash!(next_config, file)
-      Jekyll.logger.info 'Configuration file:', file
-      next_config
-    rescue SystemCallError
-      if @default_config_file ||= nil
-        Jekyll.logger.warn 'Configuration file:', 'none'
-        {}
-      else
-        Jekyll.logger.error 'Fatal:', 'The configuration file '#{file}'
-          could not be found.'
-        raise LoadError, 'The Configuration file '#{file}' could not be found.'
-      end
-    end
-    
-        def defaults_deprecate_type(old, current)
-      Jekyll.logger.warn 'Defaults:', 'The '#{old}' type has become '#{current}'.'
-      Jekyll.logger.warn 'Defaults:', 'Please update your front-matter defaults to use \
-                        'type: #{current}'.'
-    end
-  end
-end
-
-    
-      # Finds the groups of the source user, optionally limited to those visible to
-  # the current user.
-  def execute(current_user = nil)
-    segments = all_groups(current_user)
-    
-        # Validates that {#boundary} is {#valid_ip_or_range? a valid IP address or
-    # IP address range}. Due to this not being tested before it was moved here
-    # from Mdm, the default workspace does not validate. We always validate boundaries
-    # and a workspace may have a blank default boundary.
-    #
-    # @return [void]
-    def boundary_must_be_ip_range
-      unless boundary.blank?
-        begin
-          boundaries = Shellwords.split(boundary)
-        rescue ArgumentError
-          boundaries = []
-        end
-    
-    lib_path = root.join('lib').to_path
-    
-              fd.write(res)
-        end
-      end
-      break
-    rescue ::Timeout::Error
-      $stderr.puts '#{prefix}#{site} timed out'
-    rescue ::Interrupt
-      raise $!
-    rescue ::Exception => e
-      $stderr.puts '#{prefix}#{site} #{e.class} #{e}'
-    end
-  end
-    
-              s[:pass] = ''
-          return
-        end
-    
-          if(pkt.payload =~ self.sigs[k])
-        matched = k
-        matches = $1
-      end
-    
-            def no_error_when_valid?
-          @file = StringIO.new('.')
-          @subject.send(@attachment_name).assign(@file)
-          @subject.valid?
-          expected_message = [
-            @attachment_name.to_s.titleize,
-            I18n.t(:blank, scope: [:errors, :messages])
-          ].join(' ')
-          @subject.errors.full_messages.exclude?(expected_message)
-        end
       end
     end
   end
 end
 
     
-        module Statements
-      def add_attachment(table_name, *attachment_names)
-        raise ArgumentError, 'Please specify attachment name in your add_attachment call in your migration.' if attachment_names.empty?
+          def matches(ext)
+        extname_list.include?(ext.downcase)
+      end
     
-                if options.has_key?(validator_kind)
-              validator_options = options.delete(validator_kind)
-              validator_options = {} if validator_options == true
-              conditional_options = options.slice(:if, :unless)
-              Array.wrap(validator_options).each do |local_options|
-                method_name = Paperclip::Validators.const_get(constant.to_s).helper_method_name
-                send(method_name, attributes, local_options.merge(conditional_options))
-              end
+          def initialize(config)
+        Jekyll::External.require_with_graceful_fail 'kramdown' unless defined?(Kramdown)
+        @config = config['kramdown'].dup || {}
+        @config[:input] = :SmartyPants
+      end
+    
+          html_filters.push 'bootstrap/entries_v3', 'bootstrap/clean_html_v3'
+    
+        def assert_index(index)
+      i = index.is_a?(Integer) ? index : @filters.index(filter_const(index))
+      raise 'No such filter to insert: #{index}' unless i
+      i
+    end
+  end
+end
+
+    
+          unless root?
+        raise Invalid, 'missing name' if !name || name.empty?
+        raise Invalid, 'missing path' if !path || path.empty?
+        raise Invalid, 'missing type' if !type || type.empty?
+      end
+    end
+    
+        delegate :empty?, :blank?, to: :pages
+    
+            rows.each do |row|
+          line = row.values.each_with_index.map do |value, col|
+            value.to_s.ljust(col_widths[col])
+          end.join(' ').rstrip
+          line = color.colorize(line, row.color) if row.color
+          puts line
+        end
+      end
+    
+      it 'enables printing all config variables on command line parameter' do
+    capture_io do
+      flags '--print-config-variables', '-p'
+    end
+    expect(Capistrano::Configuration.fetch(:print_config_variables)).to be true
+  end
+    
+            def autocorrect(node)
+          lambda do |corrector|
+            each_unnecessary_space_match(node) do |range|
+              corrector.replace(range, ' ')
             end
           end
         end
-      end
     
-        module HelperMethods
-      # Places ActiveModel validations on the content type of the file
-      # assigned. The possible options are:
-      # * +content_type+: Allowed content types.  Can be a single content type
-      #   or an array.  Each type can be a String or a Regexp. It should be
-      #   noted that Internet Explorer uploads files with content_types that you
-      #   may not expect. For example, JPEG images are given image/pjpeg and
-      #   PNGs are image/x-png, so keep that in mind when determining how you
-      #   match.  Allows all by default.
-      # * +not+: Forbidden content types.
-      # * +message+: The message to display when the uploaded file has an invalid
-      #   content type.
-      # * +if+: A lambda or name of an instance method. Validation will only
-      #   be run is this lambda or method returns true.
-      # * +unless+: Same as +if+ but validates if lambda or method returns false.
-      # NOTE: If you do not specify an [attachment]_content_type field on your
-      # model, content_type validation will work _ONLY upon assignment_ and
-      # re-validation after the instance has been reloaded will always succeed.
-      # You'll still need to have a virtual attribute (created by +attr_accessor+)
-      # name +[attachment]_content_type+ to be able to use this validator.
-      def validates_attachment_content_type(*attr_names)
-        options = _merge_attributes(attr_names)
-        validates_with AttachmentContentTypeValidator, options.dup
-        validate_before_processing AttachmentContentTypeValidator, options.dup
+            # @param lines [Array<String>]
+        # @param annotations [Array<(Integer, String)>]
+        #   each entry is the annotated line number and the annotation text
+        #
+        # @note annotations are sorted so that reconstructing the annotation
+        #   text via {#to_s} is deterministic
+        def initialize(lines, annotations)
+          @lines       = lines.freeze
+          @annotations = annotations.sort.freeze
+        end
+    
+          # Skip this file if the output file is the same size
+      if entry.directory?
+        FileUtils.mkdir_p(path) unless File.directory?(path)
+      else
+        entry_mode = entry.instance_eval { @mode } & 0777
+        if File.exists?(path)
+          stat = File.stat(path)
+          # TODO(sissel): Submit a patch to archive-tar-minitar upstream to
+          # expose headers in the entry.
+          entry_size = entry.instance_eval { @size }
+          # If file sizes are same, skip writing.
+          next if stat.size == entry_size && (stat.mode & 0777) == entry_mode
+        end
+        puts 'Extracting #{entry.full_name} from #{tarball} #{entry_mode.to_s(8)}'
+        File.open(path, 'w') do |fd|
+          # eof? check lets us skip empty files. Necessary because the API provided by
+          # Archive::Tar::Minitar::Reader::EntryStream only mostly acts like an
+          # IO object. Something about empty files in this EntryStream causes
+          # IO.copy_stream to throw 'can't convert nil into String' on JRuby
+          # TODO(sissel): File a bug about this.
+          while !entry.eof?
+            chunk = entry.read(16384)
+            fd.write(chunk)
+          end
+            #IO.copy_stream(entry, fd)
+        end
+        File.chmod(entry_mode, path)
       end
     end
+    tar.close
+    File.unlink(tarball) if File.file?(tarball)
+  end # def untar
+    
+            def responds?
+          methods = @subject.instance_methods.map(&:to_s)
+          methods.include?('#{@attachment_name}') &&
+            methods.include?('#{@attachment_name}=') &&
+            methods.include?('#{@attachment_name}?')
+        end
+    
+        module ClassMethods
+      # This method is a shortcut to validator classes that is in
+      # 'Attachment...Validator' format. It is almost the same thing as the
+      # +validates+ method that shipped with Rails, but this is customized to
+      # be using with attachment validators. This is helpful when you're using
+      # multiple attachment validators on a single attachment.
+      #
+      # Example of using the validator:
+      #
+      #   validates_attachment :avatar, :presence => true,
+      #      :content_type => { :content_type => 'image/jpg' },
+      #      :size => { :in => 0..10.kilobytes }
+      #
+      def validates_attachment(*attributes)
+        options = attributes.extract_options!.dup
+    
+            return if (value.nil? && options[:allow_nil]) || (value.blank? && options[:allow_blank])
+    
+            if record.errors.include? attribute
+          record.errors[attribute].each do |error|
+            record.errors.add base_attribute, error
+          end
+        end
+      end
+    
+      option '--lint' , :flag, 'Check manifest with pkglint',
+    :default => true
+    
+        # do channel-update if requested
+    if attributes[:pear_channel_update?]
+      channel = attributes[:pear_channel] || 'pear'
+      logger.info('Updating the channel', :channel => channel)
+      safesystem('pear', '-c', config, 'channel-update', channel)
+    end
+    
+      def build!(params)
+    # TODO(sissel): Support these somehow, perhaps with execs and files.
+    self.scripts.each do |name, path|
+      case name
+        when 'pre-install'
+        when 'post-install'
+        when 'pre-uninstall'
+        when 'post-uninstall'
+      end # case name
+    end # self.scripts.each
+    
+      def specfile(builddir)
+    '#{builddir}/pkginfo'
   end
-end
+    
+        before do
+      $stdout = null
+    end
+    
+          it 'should have the correct 'after_target_uninstall' trigger script' do
+        insist { @rpm.tags[:triggername][3] } == 'test'
+        insist { @rpm.tags[:triggerversion][3] } == ''
+        insist { @rpm.tags[:triggerflags][3] & (1 << 18)} == ( 1 << 18) # See FPM::Package::RPM#rpm_get_trigger_type
+        insist { @rpm.tags[:triggerindex][3] } == 3
+        insist { @rpm.tags[:triggerscriptprog][3] } == '/bin/sh'
+        insist { @rpm.tags[:triggerscripts][3] } == '#!/bin/sh\necho after_target_uninstall trigger executed'
+      end
+    
+    describe FPM::Package::Sh do
+  describe '#output', :if => shell_is_bash do
+    def make_sh_package
+      # output a package, use it as the input, set the subject to that input
+      # package. This helps ensure that we can write and read packages
+      # properly.
+      tmpfile = Tempfile.new('fpm-test-sh')
+      target = tmpfile.path
+      # The target file must not exist.
+      tmpfile.unlink
+    
+        it 'executes FPM::Command with the appropriate arguments' do
+      command = instance_double(FPM::Command)
+      expected = %W(-t tar -s dir -C #{Dir.tmpdir} --cpan-mirror-only
+                    --no-cpan-test --config-files foo --config-files bar
+                    --name awesome --cpan-mirror-only --url http://example.com
+                    bin/)
