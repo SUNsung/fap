6d14f19fc25c0e6d1498f439a2083b48cbfff84d
SUNsung <weatherius@gmail.com>
2018-08-01 19:48:37 +0300

    Rewrite [21Al8W.xEv5go21LmCe5kKynAQ21IX6TN8c1nA.21Ax..py] File in: 2018-08-01 19:48:37

--- pyton/21Al8W.xEv5go21LmCe5kKynAQ21IX6TN8c1nA.21Ax..py
+++ pyton/21Al8W.xEv5go21LmCe5kKynAQ21IX6TN8c1nA.21Ax..py
@@ -1,185 +1,25 @@
 
         
-        import pytest
-    
-    
-def with_metaclass(meta, *bases):
-    '''Create a base class with a metaclass.'''
-    # This requires a bit of explanation: the basic idea is to make a
-    # dummy metaclass for one level of class instantiation that replaces
-    # itself with the actual metaclass.
-    class metaclass(type):
-        def __new__(cls, name, this_bases, d):
-            return meta(name, bases, d)
-    return type.__new__(metaclass, 'temporary_class', (), {})
-    
-        def dispatch_request(self, *args, **kwargs):
-        meth = getattr(self, request.method.lower(), None)
-    
-    
-def test_config_from_json():
-    app = flask.Flask(__name__)
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    app.config.from_json(os.path.join(current_dir, 'static', 'config.json'))
-    common_object_test(app)
-    
-        def record(sender, message, category):
-        recorded.append((message, category))
-    
-    try:
-    import json
-except ImportError:
-    import simplejson as json
-    
-    from ansible.plugins.terminal import TerminalBase
-from ansible.errors import AnsibleConnectionFailure
-    
-        def test_max_delay_none(self):
-        strategy = _exponential_backoff(retries=7, delay=1, backoff=2, max_delay=None)
-        result = list(strategy())
-        self.assertEquals(result, [1, 2, 4, 8, 16, 32, 64])
-    
-        Returns
-    -------
-    array of floats shaped like (metrics, formats, samples, classes, density)
-        Time in seconds.
-    '''
-    metrics = np.atleast_1d(metrics)
-    samples = np.atleast_1d(samples)
-    classes = np.atleast_1d(classes)
-    density = np.atleast_1d(density)
-    formats = np.atleast_1d(formats)
-    out = np.zeros((len(metrics), len(formats), len(samples), len(classes),
-                    len(density)), dtype=float)
-    it = itertools.product(samples, classes, density)
-    for i, (s, c, d) in enumerate(it):
-        _, y_true = make_multilabel_classification(n_samples=s, n_features=1,
-                                                   n_classes=c, n_labels=d * c,
-                                                   random_state=42)
-        _, y_pred = make_multilabel_classification(n_samples=s, n_features=1,
-                                                   n_classes=c, n_labels=d * c,
-                                                   random_state=84)
-        for j, f in enumerate(formats):
-            f_true = f(y_true)
-            f_pred = f(y_pred)
-            for k, metric in enumerate(metrics):
-                t = timeit(partial(metric, f_true, f_pred), number=n_times)
-    
-    
-if __name__ == '__main__':
-    ###########################################################################
-    # Option parser
-    ###########################################################################
-    op = optparse.OptionParser()
-    op.add_option('--n-times',
-                  dest='n_times', default=5, type=int,
-                  help='Benchmark results are average over n_times experiments')
-    
-        selected_algorithm = opts.selected_algorithm.split(',')
-    for key in selected_algorithm:
-        if key not in default_algorithms.split(','):
-            raise ValueError('Unknown sampling algorithm \'%s\' not in (%s).'
-                             % (key, default_algorithms))
-    
-    URL = ('http://people.csail.mit.edu/jrennie/'
-       '20Newsgroups/20news-bydate.tar.gz')
-    
-        # Print the classification report
-    print(metrics.classification_report(y_test, y_predicted,
-                                        target_names=dataset.target_names))
-    
-    # Author: Virgile Fritsch <virgile.fritsch@inria.fr>
-# License: BSD 3 clause
-    
-    Note that this two-dimensional example is very degenerate:
-generally the number of features would be much greater than the
-'document length', while here we have much larger documents than vocabulary.
-Similarly, with ``n_classes > n_features``, it is much less likely that a
-feature distinguishes a particular class.
-'''
-    
-        def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('-l', '--list', dest='list', action='store_true',
-            help='List available templates')
-        parser.add_option('-e', '--edit', dest='edit', action='store_true',
-            help='Edit spider after creating it')
-        parser.add_option('-d', '--dump', dest='dump', metavar='TEMPLATE',
-            help='Dump template to standard output')
-        parser.add_option('-t', '--template', dest='template', default='basic',
-            help='Uses a custom template.')
-        parser.add_option('--force', dest='force', action='store_true',
-            help='If the spider already exists, overwrite it with the template')
-    
-        def _get_slot(self, request, spider):
-        key = self._get_slot_key(request, spider)
-        if key not in self.slots:
-            conc = self.ip_concurrency if self.ip_concurrency else self.domain_concurrency
-            conc, delay = _get_concurrency_delay(conc, spider, self.settings)
-            self.slots[key] = Slot(conc, delay, self.randomize_delay)
-    
-    from scrapy.http import Request, Response
-from scrapy.middleware import MiddlewareManager
-from scrapy.utils.defer import mustbe_deferred
-from scrapy.utils.conf import build_component_list
-    
-            if not isinstance(response, HtmlResponse) or response.status != 200:
-            return response
-    
-            for fmt, func in six.iteritems(self._formats):
-            new_response = func(response)
-            if new_response:
-                logger.debug('Decompressed response with format: %(responsefmt)s',
-                             {'responsefmt': fmt}, extra={'spider': spider})
-                return new_response
-        return response
+            @classmethod
+    def tearDownClass(self):
+        '''
+        Remove the temporary directory /test_command and its content.
+        '''
+        shutil.rmtree('tests/test_command')
 
     
+        def __init__(self, name, action):
+        self.name = name
+        self.action = action
     
-def main():
-    args = parse_args()
-    zip_path = fetch_zip(
-        args.tag,
-        args.externals_dir / 'zips',
-        org=args.organization,
-        binary=args.binary,
-        verbose=args.verbose,
-    )
-    final_name = args.externals_dir / args.tag
-    extract_zip(args.externals_dir, zip_path).replace(final_name)
+    # Test the generator
+count_to_two = lambda: count_to(2)
+count_to_five = lambda: count_to(5)
     
-        @staticmethod
-    def calculate_indent(line):
-        return len(line) - len(line.strip())
-    
-        def test_disallowed_grouping__empty_group_on_right(self):
-        self.parse_function_should_fail('''
-module foo
-foo.empty_group
-    param: int
-    [
-    [
-    ]
-    group2 : int
-    ]
-            ''')
-    
-    
-def receiver(group):
-    # Look up multicast group address in name server and find out IP version
-    addrinfo = socket.getaddrinfo(group, None)[0]
-    
-        def display(self):
-        self.nfound = self.nfound + 1
-        if self.silent:
-            return
-        print('+-' + '--'*self.n + '+')
-        for y in range(self.n-1, -1, -1):
-            print('|', end=' ')
-            for x in range(self.n):
-                if self.y[x] == y:
-                    print('Q', end=' ')
-                else:
-                    print('.', end=' ')
-            print('|')
-        print('+-' + '--'*self.n + '+')
+        a_transaction = Transaction(True, num_obj)
+    try:
+        for i in range(3):
+            num_obj.increment()
+            print(num_obj)
+        a_transaction.commit()
+        print('-- committed')
