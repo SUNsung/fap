372ba0bc1b2200bf1f5f5e0e1dce0c22a053171c
SUNsung <weatherius@gmail.com>
2018-08-01 22:16:26 +0300

    Rewrite [12yrVrqyqZUo12dST15xW01GE12UeHkej5Naj212tA2.rb] File in: 2018-08-01 22:16:25

--- ruby/12yrVrqyqZUo12dST15xW01GE12UeHkej5Naj212tA2.rb
+++ ruby/12yrVrqyqZUo12dST15xW01GE12UeHkej5Naj212tA2.rb
@@ -1,76 +1,73 @@
-module Jekyll
-  module Converters
-    class SmartyPants < Converter
-      safe true
-      priority :low
+
+        
+          def after_omniauth_failure_path_for(scope)
+    new_session_path(scope)
+  end
+    
+        if successfully_sent?(resource)
+      respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
+    else
+      respond_with(resource)
+    end
+  end
+    
+    class Devise::UnlocksController < DeviseController
+  prepend_before_action :require_no_authentication
+    
+        unless env['devise.skip_trackable']
+      warden.session(scope)['last_request_at'] = Time.now.utc.to_i
+    end
+  end
+end
+
+    
+          def headers_for(action, opts)
+        headers = {
+          subject: subject_for(action),
+          to: resource.email,
+          from: mailer_sender(devise_mapping),
+          reply_to: mailer_reply_to(devise_mapping),
+          template_path: template_paths,
+          template_name: action
+        }.merge(opts)
     
           options[:only_patterns] = [/\Ad3[\-\w]+\z/, /\Ad3\/blob\/master\/changes\.md\z/i]
       options[:skip_patterns] = [/3\.x-api-reference/]
     
-        options[:attribution] = <<-HTML
-      &copy; 2018 The TensorFlow Authors. All rights reserved.<br>
-      Licensed under the Creative Commons Attribution License 3.0.<br>
-      Code samples licensed under the Apache 2.0 License.
-    HTML
-    
-        def initialize
-      @pages = {}
+        def initial_urls
+      [ 'https://hexdocs.pm/elixir/#{self.class.release}/api-reference.html',
+        'https://hexdocs.pm/eex/#{self.class.release}/EEx.html',
+        'https://hexdocs.pm/ex_unit/#{self.class.release}/ExUnit.html',
+        'https://hexdocs.pm/iex/#{self.class.release}/IEx.html',
+        'https://hexdocs.pm/logger/#{self.class.release}/Logger.html',
+        'https://hexdocs.pm/mix/#{self.class.release}/Mix.html',
+        'https://elixir-lang.org/getting-started/introduction.html' ]
     end
     
-      def report_disk_usage
-    disk_space = disk_usage_readable(Cleanup.disk_cleanup_size)
-    if ARGV.dry_run?
-      ohai 'This operation would free approximately #{disk_space} of disk space.'
-    else
-      ohai 'This operation has freed approximately #{disk_space} of disk space.'
+        version 'JavaScript' do
+      self.base_url = 'https://api.haxe.org/js/'
+    end
+    
+        def inheritable_copy
+      self.class.new @filters
+    end
+    
+        def join(*args)
+      self.class.join self, *args
+    end
+    
+            doc
+      end
     end
   end
+end
+
     
-            def find_order
-          @order = Spree::Order.find_by!(number: order_id)
-        end
-    
-            def stock_location
-          @stock_location ||= StockLocation.accessible_by(current_ability, :read).find(params[:id])
-        end
-    
-            def on_percent_literal(node)
-          each_unnecessary_space_match(node) do |range|
-            add_offense(node, location: range)
-          end
-        end
-    
-            def on_send(node)
-          return unless match_call?(node) &&
-                        (!node.value_used? || only_truthiness_matters?(node)) &&
-                        !(node.parent && node.parent.block_type?)
-    
-            # Construct annotated source string (like what we parse)
-        #
-        # Reconstruct a deterministic annotated source string. This is
-        # useful for eliminating semantically irrelevant annotation
-        # ordering differences.
-        #
-        # @example standardization
-        #
-        #     source1 = AnnotatedSource.parse(<<-RUBY)
-        #     line1
-        #     ^ Annotation 1
-        #      ^^ Annotation 2
-        #     RUBY
-        #
-        #     source2 = AnnotatedSource.parse(<<-RUBY)
-        #     line1
-        #      ^^ Annotation 2
-        #     ^ Annotation 1
-        #     RUBY
-        #
-        #     source1.to_s == source2.to_s # => true
-        #
-        # @return [String]
-        def to_s
-          reconstructed = lines.dup
-    
-            def failure_message
-          'Attachment #{@attachment_name} should be required'
-        end
+      def command_line(*options)
+    options.each { |opt| ARGV << opt }
+    subject.define_singleton_method(:exit) do |*_args|
+      throw(:system_exit, :exit)
+    end
+    subject.run
+    subject.options
+  end
