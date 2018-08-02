248071d274db6f0dd143a315ec6dba268adeda47
SUNsung <weatherius@gmail.com>
2018-08-02 21:31:27 +0300

    Rewrite [23tBSaulI6yvc23965VM05QshI23jMdeyuHjCdM2397.rb] File in: 2018-08-02 21:31:26

--- ruby/23tBSaulI6yvc23965VM05QshI23jMdeyuHjCdM2397.rb
+++ ruby/23tBSaulI6yvc23965VM05QshI23jMdeyuHjCdM2397.rb
@@ -1,171 +1,81 @@
 
         
-              def say_no_to_protect_against_forgery!
-        _helpers.module_eval do
-          silence_redefinition_of_method :protect_against_forgery?
-          def protect_against_forgery?
-            false
+                    Thread.pass while @thread.alive?
+          end
+    
+            attributes.flat_map do |key, value|
+          if value.is_a?(Hash) && !table.has_column?(key)
+            associated_predicate_builder(key).expand_from_hash(value)
+          elsif table.associated_with?(key)
+            # Find the foreign key when using queries such as:
+            # Post.where(author: author)
+            #
+            # For polymorphic relationships, find the foreign key and type:
+            # PriceEstimate.where(estimate_of: treasure)
+            associated_table = table.associated_table(key)
+            if associated_table.polymorphic_association?
+              case value.is_a?(Array) ? value.first : value
+              when Base, Relation
+                value = [value] unless value.is_a?(Array)
+                klass = PolymorphicArrayValue
+              end
+            end
+    
+        # This isn't strictly necessary for the test, but a little bit of
+    # knowledge of internals allows us to make failures far more likely.
+    model.define_singleton_method(:define_attribute) do |*args|
+      Thread.pass
+      super(*args)
+    end
+    
+        def speak(data)
+      @last_action = [ :speak, data ]
+    end
+    
+      test 'disallow block and arg together' do
+    e = assert_raise ArgumentError do
+      ChatChannel.periodically(:send_updates, every: 1) { ping }
+    end
+    assert_match(/not both/, e.message)
+  end
+    
+          expected = { 'identifier' => '{id: 1}', 'type' => 'reject_subscription' }
+      assert_equal expected, @connection.last_transmission
+    end
+    
+            Connection.new(@server, env).tap do |connection|
+          connection.process
+          assert_predicate connection.websocket, :possible?
+    
+        def close
+      sleep WAIT_WHEN_NOT_EXPECTING_EVENT
+    
+            assert_equal 'action_cable/User#lifo', pubsub_call[:channel]
+        assert_instance_of Proc, pubsub_call[:callback]
+      end
+    end
+  end
+    
+            # Internal hax = :(
+        client = connection.websocket.send(:websocket)
+        rack_hijack_io = client.instance_variable_get('@stream').instance_variable_get('@rack_hijack_io')
+        rack_hijack_io.stub(:write, proc { raise(closed_exception, 'foo') }) do
+          assert_called(client, :client_gone) do
+            client.write('boo')
           end
         end
+        assert_equal [], connection.errors
       end
-    
-    require 'cases/helper'
-    
-      has_many :developers_with_select, -> { select('id, name, first_name') }, class_name: 'Developer'
-    
-        def connect
-      reject_unauthorized_connection
-    end
-    
-        def send_async(method, *args)
-      send method, *args
-    end
-    
-        # Fetch the Documents in this collection.
-    # Defaults to an empty array if no documents have been read in.
-    #
-    # Returns an array of Jekyll::Document objects.
-    def docs
-      @docs ||= []
-    end
-    
-    module Jekyll
-  module Commands
-    class NewTheme < Jekyll::Command
-      class << self
-        def init_with_program(prog)
-          prog.command(:'new-theme') do |c|
-            c.syntax 'new-theme NAME'
-            c.description 'Creates a new Jekyll theme scaffold'
-            c.option 'code_of_conduct', \
-                     '-c', '--code-of-conduct', \
-                     'Include a Code of Conduct. (defaults to false)'
-    
-              opts[:DirectoryIndex] = [] if opts[:JekyllOptions]['show_dir_listing']
-    
-          # Filtering Content
-      'show_drafts'         => nil,
-      'limit_posts'         => 0,
-      'future'              => false,
-      'unpublished'         => false,
-    
-    module Kramdown
-  module Parser
-    class SmartyPants < Kramdown::Parser::Kramdown
-      def initialize(source, options)
-        super
-        @block_parsers = [:block_html, :content]
-        @span_parsers =  [:smart_quotes, :html_entity, :typographic_syms, :span_html]
-      end
-    
-        def defaults_deprecate_type(old, current)
-      Jekyll.logger.warn 'Defaults:', 'The '#{old}' type has become '#{current}'.'
-      Jekyll.logger.warn 'Defaults:', 'Please update your front-matter defaults to use \
-                        'type: #{current}'.'
-    end
-  end
-end
-
-    
-          # Create a new Drop
-      #
-      # obj - the Jekyll Site, Collection, or Document required by the
-      # drop.
-      #
-      # Returns nothing
-      def initialize(obj)
-        @obj = obj
-        @mutations = {} # only if mutable: true
-      end
-    
-        # Gets the last git commit information formatted into a String by the provided
-    # pretty format String. See the git-log documentation for valid format placeholders
-    def self.last_git_commit_formatted_with(pretty_format, date_format = nil)
-      command = ['git log -1']
-      command << '--pretty=\'#{pretty_format}\''
-      command << '--date=\'#{date_format}\'' if date_format
-      Actions.sh(command.compact.join(' '), log: false).chomp
-    rescue
-      nil
-    end
-    
-            commands = []
-        commands << Fastlane::Actions.sh('security create-keychain -p #{escaped_password} #{keychain_path}', log: false)
-    
-        context 'with keywords' do
-      let(:options) do
-        {
-          name: { 'en-US' => 'Fastlane Demo' },
-          description: { 'en-US' => 'Demo description' },
-          keywords: { 'en-US' => 'Some, key, words' }
-        }
-      end
-    
-            command = [escaped_gradle_path, 'tasks', '--console=plain'].join(' ')
-        output = Action.sh(command, print_command: false, print_command_output: false)
-        output.split('\n').each do |line|
-          if (result = line.match(/(\w+)\s\-\s([\w\s]+)/))
-            self.tasks << GradleTask.new(title: result[1], description: result[2])
-          end
-        end
-    
-        if registration
-      u2f.authenticate!(challenges, response, Base64.decode64(registration.public_key), registration.counter)
-      registration.update(counter: response.counter)
-      true
-    end
-  rescue JSON::ParserError, NoMethodError, ArgumentError, U2F::Error
-    false
-  end
-end
-
-    
-      def dry_run!(event = nil)
-    @dry_run = true
-    
-      def recipients(payload = {})
-    emails = interpolated(payload)['recipients']
-    if emails.present?
-      if emails.is_a?(String)
-        [emails]
-      else
-        emails
-      end
-    else
-      [user.email]
     end
   end
     
-    
-# replace calls, jmps, and read/write handle/filename references
-replaces = []
-asm.each_line { |ln|
-	if ln =~ /call /
-		parts = ln.split(' ')
-		if (parts[0] == 'call' and parts[2] == ';call')
-			old = parts[1]
-			func = parts[3]
-			new = addrs[func]
-			#puts '%32s: %s -> %x' % [func, old, new]
-			replaces << [func, old, new.to_s(16)]
-		end
-	end
-    }
-    
-    meterp.core.use('Stdapi')
-    
-    classNames = [ 'HelloWorld1', 'HelloWorld2' ]
-    
-    Rjb::load(ENV['JAVA_HOME'] + '/lib/tools.jar:.',jvmargs=[])
-    
-      # TODO: raise SAFE level (0) to 4 if possible.
-  def generate
-    ERB.new(PROFILE_ERB_TEMPLATE, 0, '>').result(binding)
+      def evernote_client
+    EvernoteOAuth::Client.new(
+      token:           evernote_oauth_token,
+      consumer_key:    evernote_consumer_key,
+      consumer_secret: evernote_consumer_secret,
+      sandbox:         use_sandbox?
+    )
   end
-end
     
-    module Pod
-  class Command
-    class Env < Command
-      self.summary = 'Display pod environment'
-      self.description = 'Display pod environment.'
+      before_action :upgrade_warning, only: :index
