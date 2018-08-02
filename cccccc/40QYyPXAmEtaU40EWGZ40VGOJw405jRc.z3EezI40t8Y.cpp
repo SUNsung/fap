526d05a2032d4733f6cdfcdb8843de7135797029
SUNsung <weatherius@gmail.com>
2018-08-02 03:54:56 +0300

    Rewrite [40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp] File in: 2018-08-02 03:54:56

--- cccccc/40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp
+++ cccccc/40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp
@@ -1,173 +1,101 @@
 
         
-        /// Define a map of decoration points to their expected configuration key.
-extern const std::map<DecorationPoint, std::string> kDecorationPointKeys;
+        struct Options;
+struct FileMetaData;
     
-    /**
- * @brief Subscription details for KernelEventPublisher events.
- */
-struct KernelSubscriptionContext : public SubscriptionContext {
-  /// The kernel event subscription type.
-  osquery_event_t event_type;
+    void leveldb_writebatch_iterate(
+    leveldb_writebatch_t* b,
+    void* state,
+    void (*put)(void*, const char* k, size_t klen, const char* v, size_t vlen),
+    void (*deleted)(void*, const char* k, size_t klen)) {
+  class H : public WriteBatch::Handler {
+   public:
+    void* state_;
+    void (*put_)(void*, const char* k, size_t klen, const char* v, size_t vlen);
+    void (*deleted_)(void*, const char* k, size_t klen);
+    virtual void Put(const Slice& key, const Slice& value) {
+      (*put_)(state_, key.data(), key.size(), value.data(), value.size());
     }
-    
-    TEST_F(SyslogTests, test_populate_event_context) {
-  std::string line =
-      R'|('2016-03-22T21:17:01.701882+00:00','vagrant-ubuntu-trusty-64','6','cron','CRON[16538]:',' (root) CMD (   cd / && run-parts --report /etc/cron.hourly)')|';
-  SyslogEventPublisher pub;
-  auto ec = pub.createEventContext();
-  Status status = pub.populateEventContext(line, ec);
+    virtual void Delete(const Slice& key) {
+      (*deleted_)(state_, key.data(), key.size());
     }
-    
-    // try to load weight information from file, if exists
-inline bool MetaTryLoadFloatInfo(const std::string& fname,
-                                 std::vector<bst_float>* data) {
-  std::unique_ptr<dmlc::Stream> fi(dmlc::Stream::Create(fname.c_str(), 'r', true));
-  if (fi == nullptr) return false;
-  dmlc::istream is(fi.get());
-  data->clear();
-  bst_float value;
-  while (is >> value) {
-    data->push_back(value);
-  }
-  return true;
+  };
+  H handler;
+  handler.state_ = state;
+  handler.put_ = put;
+  handler.deleted_ = deleted;
+  b->rep.Iterate(&handler);
 }
     
-      uint64_t uint64_t2[2] = {1U, 2U};
-  EXPECT_EQ(info.group_ptr_.size(), 0);
-  info.SetInfo('group', uint64_t2, xgboost::kUInt64, 2);
-  ASSERT_EQ(info.group_ptr_.size(), 3);
-  EXPECT_EQ(info.group_ptr_[2], 3);
     
-      EXPECT_ANY_THROW(xgboost::Metric::Create('error@abc'));
-  delete metric;
-  metric = xgboost::Metric::Create('error@0.5f');
-  EXPECT_STREQ(metric->Name(), 'error');
+    {}  // namespace leveldb
     
+    bool GuessType(const std::string& fname, FileType* type) {
+  size_t pos = fname.rfind('/');
+  std::string basename;
+  if (pos == std::string::npos) {
+    basename = fname;
+  } else {
+    basename = std::string(fname.data() + pos + 1, fname.size() - pos - 1);
+  }
+  uint64_t ignored;
+  return ParseFileName(basename, &ignored, type);
+}
     
-    {
-    {
-    {      // Test write Symbol
-      std::vector<unsigned char> buffer2(
-        CompressedBufferWriter::CalculateBufferSize(input.size(),
-          alphabet_size));
-      for (int i = 0; i < input.size(); i++) {
-        cbw.WriteSymbol(buffer2.data(), input[i], i);
+      fname = DescriptorFileName('bar', 100);
+  ASSERT_EQ('bar/', std::string(fname.data(), 4));
+  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
+  ASSERT_EQ(100, number);
+  ASSERT_EQ(kDescriptorFile, type);
+    
+        const char* benchmarks = FLAGS_benchmarks;
+    while (benchmarks != NULL) {
+      const char* sep = strchr(benchmarks, ',');
+      Slice name;
+      if (sep == NULL) {
+        name = benchmarks;
+        benchmarks = NULL;
+      } else {
+        name = Slice(benchmarks, sep - benchmarks);
+        benchmarks = sep + 1;
       }
-      CompressedIterator<int> ci2(buffer.data(), alphabet_size);
-      std::vector<int> output2(input.size());
-      for (int i = 0; i < input.size(); i++) {
-        output2[i] = ci2[i];
-      }
-      ASSERT_TRUE(input == output2);
-    }
-  }
-}
-    
-    // common regressions
-// linear regression
-struct LinearSquareLoss {
-  // duplication is necessary, as __device__ specifier
-  // cannot be made conditional on template parameter
-  XGBOOST_DEVICE static bst_float PredTransform(bst_float x) { return x; }
-  XGBOOST_DEVICE static bool CheckLabel(bst_float x) { return true; }
-  XGBOOST_DEVICE static bst_float FirstOrderGradient(bst_float predt, bst_float label) {
-    return predt - label;
-  }
-  XGBOOST_DEVICE static bst_float SecondOrderGradient(bst_float predt, bst_float label) {
-    return 1.0f;
-  }
-  template <typename T>
-  static T PredTransform(T x) { return x; }
-  template <typename T>
-  static T FirstOrderGradient(T predt, T label) { return predt - label; }
-  template <typename T>
-  static T SecondOrderGradient(T predt, T label) { return T(1.0f); }
-  static bst_float ProbToMargin(bst_float base_score) { return base_score; }
-  static const char* LabelErrorMsg() { return ''; }
-  static const char* DefaultEvalMetric() { return 'rmse'; }
-};
-    
-    TEST(Metric, AUCPR) {
-  xgboost::Metric *metric = xgboost::Metric::Create('aucpr');
-  ASSERT_STREQ(metric->Name(), 'aucpr');
-  EXPECT_NEAR(GetMetricEval(metric, {0, 0, 1, 1}, {0, 0, 1, 1}), 1, 1e-10);
-  EXPECT_NEAR(GetMetricEval(metric, {0.1f, 0.9f, 0.1f, 0.9f}, {0, 0, 1, 1}),
-              0.5f, 0.001f);
-  EXPECT_NEAR(
-      GetMetricEval(metric,
-                    {0.4f, 0.2f, 0.9f, 0.1f, 0.2f, 0.4f, 0.1f, 0.1f, 0.2f, 0.1f},
-                    {0, 0, 0, 0, 0, 1, 0, 0, 1, 1}),
-      0.2908445f, 0.001f);
-  EXPECT_NEAR(GetMetricEval(
-                  metric, {0.87f, 0.31f, 0.40f, 0.42f, 0.25f, 0.66f, 0.95f,
-                           0.09f, 0.10f, 0.97f, 0.76f, 0.69f, 0.15f, 0.20f,
-                           0.30f, 0.14f, 0.07f, 0.58f, 0.61f, 0.08f},
-                  {0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1}),
-              0.2769199f, 0.001f);
-  EXPECT_ANY_THROW(GetMetricEval(metric, {0, 1}, {}));
-  EXPECT_ANY_THROW(GetMetricEval(metric, {0, 0}, {0, 0}));
-}
-    
-    template <typename C>
-struct compare_not_equal_to : detail::cmp_pred<C, ordering::eq, 1> {
-  using detail::cmp_pred<C, ordering::eq, 1>::cmp_pred;
-};
-    
-    TEST_F(OrderingTest, compare_not_equal_to) {
-  compare_not_equal_to<OddCompare<int>> op;
-  EXPECT_TRUE(op(3, 4));
-  EXPECT_FALSE(op(3, 3));
-  EXPECT_TRUE(op(4, 3));
-}
-    
-    template <typename T>
-bool SingletonHolder<T>::hasLiveInstance() {
-  return !instance_weak_.expired();
-}
-    
-        Node* next() {
-      return next_;
     }
     
-    #include <exception>
-#include <stdexcept>
     
-    /**
- * This function tries to reallocate a buffer of which only the first
- * currentSize bytes are used. The problem with using realloc is that
- * if currentSize is relatively small _and_ if realloc decides it
- * needs to move the memory chunk to a new buffer, then realloc ends
- * up copying data that is not used. It's generally not a win to try
- * to hook in to realloc() behavior to avoid copies - at least in
- * jemalloc, realloc() almost always ends up doing a copy, because
- * there is little fragmentation / slack space to take advantage of.
- */
-FOLLY_MALLOC_CHECKED_MALLOC FOLLY_MALLOC_NOINLINE inline void* smartRealloc(
-    void* p,
-    const size_t currentSize,
-    const size_t currentCapacity,
-    const size_t newCapacity) {
-  assert(p);
-  assert(currentSize <= currentCapacity &&
-         currentCapacity < newCapacity);
+    {    done_++;
+    if (done_ >= next_report_) {
+      if      (next_report_ < 1000)   next_report_ += 100;
+      else if (next_report_ < 5000)   next_report_ += 500;
+      else if (next_report_ < 10000)  next_report_ += 1000;
+      else if (next_report_ < 50000)  next_report_ += 5000;
+      else if (next_report_ < 100000) next_report_ += 10000;
+      else if (next_report_ < 500000) next_report_ += 50000;
+      else                            next_report_ += 100000;
+      fprintf(stderr, '... finished %d ops%30s\r', done_, '');
+      fflush(stderr);
+    }
+  }
+    
+    // A Comparator object provides a total order across slices that are
+// used as keys in an sstable or a database.  A Comparator implementation
+// must be thread-safe since leveldb may invoke its methods concurrently
+// from multiple threads.
+class Comparator {
+ public:
+  virtual ~Comparator();
     }
     
-    #include <glog/logging.h>
     
-    #include <atomic>
-#include <memory>
-#include <mutex>
-#include <queue>
+    {  return false;
+}
     
-      // Call the helpers function in XlogFile1.cpp and XlogFile2.cpp and makes
-  // sure their categories are reported correctly.
-  testXlogFile1Dbg1('foobar 1234');
-  ASSERT_EQ(1, messages.size());
-  EXPECT_EQ('file1: foobar 1234', messages[0].first.getMessage());
-  EXPECT_EQ(
-      'folly.logging.test.XlogFile1.cpp',
-      messages[0].first.getCategory()->getName());
-  messages.clear();
-    
-    #pragma once
+    Type typeAddO(Type t1, Type t2) {
+  if (auto t = eval_const(t1, t2, cellAddO))          return *t;
+  if (t1.subtypeOf(BInt) && t2.subtypeOf(BInt))       return TNum;
+  if (auto t = usual_arith_conversions(t1, t2))       return *t;
+  if (t1.subtypeOf(BArr) && t2.subtypeOf(BArr))       return TArr;
+  if (t1.subtypeOf(BVec) && t2.subtypeOf(BVec))       return TVec;
+  if (t1.subtypeOf(BDict) && t2.subtypeOf(BDict))     return TDict;
+  if (t1.subtypeOf(BKeyset) && t2.subtypeOf(BKeyset)) return TKeyset;
+  return TInitCell;
+}
