03336fee719380564af454e305c3100cbe237a7b
SUNsung <weatherius@gmail.com>
2018-08-02 13:54:17 +0300

    Rewrite [219gaY.HEdgEg21FmwZMxPesJU21720kVNEW78621Cnc.rb] File in: 2018-08-02 13:54:16

--- ruby/219gaY.HEdgEg21FmwZMxPesJU21720kVNEW78621Cnc.rb
+++ ruby/219gaY.HEdgEg21FmwZMxPesJU21720kVNEW78621Cnc.rb
@@ -1,47 +1,10 @@
 
         
-            options[:only_patterns] = [
-      /\Alanguage\./,
-      /\Aclass\./,
-      /\Afunctions?\./,
-      /\Acontrol-structures/,
-      /\Aregexp\./,
-      /\Areserved\.exceptions/,
-      /\Areserved\.interfaces/,
-      /\Areserved\.variables/]
-    
-        def initialize
-      @pages = {}
-    end
-    
-        def scoped_accounts
-      Account.unscoped.tap do |scope|
-        scope.merge! with_usernames
-        scope.merge! matching_username
-        scope.merge! matching_domain
-      end
-    end
-    
-    class ActivityPub::EmojiSerializer < ActiveModel::Serializer
-  include RoutingHelper
-    
-    def codepoints_to_unicode(codepoints)
-  if codepoints.include?(' ')
-    codepoints.split(' ').map(&:hex).pack('U*')
-  else
-    [codepoints.hex].pack('U')
+        links.each do |link|
+  href = link.attribute('href').to_s
+  uri = URI.join(BASE_URI, href)
+  if map[uri]
+    dups <<  href
   end
-end
-    
-            user.update(last_sign_in_at: 'Tue, 04 Jul 2017 14:45:56 UTC +00:00', current_sign_in_at: 'Wed, 05 Jul 2017 22:10:52 UTC +00:00')
-    
-    desc 'Start a dummy (test) Rails app server'
-task :dummy_rails do
-  require 'rack'
-  require 'term/ansicolor'
-  port = ENV['PORT'] || 9292
-  puts %Q(Starting on #{Term::ANSIColor.cyan 'http://localhost:#{port}'})
-  Rack::Server.start(
-    config: 'test/dummy_rails/config.ru',
-    Port: port)
+  map[uri] = href
 end
