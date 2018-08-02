f9cdb2248c29442f3a64419d6bdf461cc8d671f0
SUNsung <weatherius@gmail.com>
2018-08-02 18:54:38 +0300

    Rewrite [02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp] File in: 2018-08-02 18:54:37

--- cccccc/02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp
+++ cccccc/02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp
@@ -1,114 +1,19 @@
 
         
-        
-void Base::Call(const std::string& method, const base::ListValue& arguments,
-                content::RenderFrameHost* rvh) {
-  NOTREACHED() << 'Uncatched call in Base'
-               << ' method:' << method
-               << ' arguments:' << arguments;
-}
-    
-    namespace extensions {
-    }
-    
-      protected:
-    ~NwScreenRegisterStreamFunction() override {}
-    DECLARE_EXTENSION_FUNCTION('nw.Screen.registerStream', UNKNOWN)
-    
-    NwShellOpenItemFunction::NwShellOpenItemFunction() {
-}
-    
-    std::string stringFromCFNumber(const CFDataRef& cf_number) {
-  return stringFromCFNumber(cf_number, CFNumberGetType((CFNumberRef)cf_number));
-}
-    
-    /// The following test macros allow pretty test output.
-#define CREATE_DATABASE_TESTS(n)                                               \
-  TEST_F(n, test_plugin_check) {                                               \
-    testPluginCheck();                                                         \
-  }                                                                            \
-  TEST_F(n, test_reset) {                                                      \
-    testReset();                                                               \
-  }                                                                            \
-  TEST_F(n, test_put) {                                                        \
-    testPut();                                                                 \
-  }                                                                            \
-  TEST_F(n, test_get) {                                                        \
-    testGet();                                                                 \
-  }                                                                            \
-  TEST_F(n, test_delete) {                                                     \
-    testDelete();                                                              \
-  }                                                                            \
-  TEST_F(n, test_delete_range) {                                               \
-    testDeleteRange();                                                         \
-  }                                                                            \
-  TEST_F(n, test_scan) {                                                       \
-    testScan();                                                                \
-  }                                                                            \
-  TEST_F(n, test_scan_limit) {                                                 \
-    testScanLimit();                                                           \
+        class RocksDBDatabasePluginTests : public DatabasePluginTests {
+ protected:
+  std::string name() override {
+    return 'rocksdb';
   }
-    
-    TEST_F(PrinterTests, test_generate_row) {
-  std::map<std::string, size_t> lengths;
-  for (const auto& row : q) {
-    computeRowLengths(row, lengths);
-  }
-    }
-    
-    struct DiskArbitrationEventContext : public EventContext {
-  std::string action;
-  std::string path;
-  std::string device_path;
-  std::string name;
-  std::string device;
-  std::string uuid;
-  std::string size;
-  std::string ejectable;
-  std::string mountable;
-  std::string writable;
-  std::string content;
-  std::string media_name;
-  std::string vendor;
-  std::string filesystem;
-  std::string disk_appearance_time;
-  std::string checksum;
 };
     
-      /**
-   * @brief Counter used to shut down thread when too many errors occur.
-   *
-   * This counter is incremented when an error occurs, and decremented when a
-   * log line is processed successfully. If it goes over kErrorThreshold, the
-   * thread will return a nonzero status and stop, preventing us from flooding
-   * the logs when things are in a bad state.
-   */
-  size_t errorCount_;
+      /// Check whether the subscription matches the event.
+  bool shouldFire(const KernelSubscriptionContextRef &sc,
+                  const KernelEventContextRef &ec) const override;
     
+     public:
+  virtual ~UdevEventPublisher() {
+    tearDown();
+  }
     
-    {  // Too many fields
-  bad_line = R'('2016-03-22T21:17:01.701882+00:00','','6','','','','')';
-  ec = pub.createEventContext();
-  status = pub.populateEventContext(bad_line, ec);
-  ASSERT_FALSE(status.ok());
-  ASSERT_NE(std::string::npos, status.getMessage().find('more'));
-}
-    
-    
-    {  return new (zone)
-      CallDescriptor(CallDescriptor::kCallCodeObject,  // kind
-                     MachineType::AnyTagged(),         // target MachineType
-                     LinkageLocation::ForAnyRegister(
-                         MachineType::AnyTagged()),  // target location
-                     locations.Build(),              // location_sig
-                     stack_param_count,              // stack_parameter_count
-                     Operator::kNoProperties,        // properties
-                     kNoCalleeSaved,                 // callee-saved registers
-                     kNoCalleeSaved,                 // callee-saved fp
-                     CallDescriptor::kNoFlags);      // flags
-}
-    
-    using compiler::Node;
-    
-    #include 'src/ast/scopes.h'
-#include 'src/ast/variables.h'
+    #include <boost/filesystem.hpp>
