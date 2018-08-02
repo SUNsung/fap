2998fef7e8fa471948852a477221bd9c8619f4d8
SUNsung <weatherius@gmail.com>
2018-08-02 21:04:30 +0300

    Rewrite [356ZPHHGg6Hcg35n7KcaVyjdoE35ivb34pJGRWw35FIq.rb] File in: 2018-08-02 21:04:30

--- ruby/356ZPHHGg6Hcg35n7KcaVyjdoE35ivb34pJGRWw35FIq.rb
+++ ruby/356ZPHHGg6Hcg35n7KcaVyjdoE35ivb34pJGRWw35FIq.rb
@@ -1,129 +1,101 @@
 
         
-                # This unregisters a plugin so that its components will no longer
-        # be used. Note that this should only be used for testing purposes.
-        def unregister(plugin)
-          if @registered.include?(plugin)
-            @logger.info('Unregistered: #{plugin.name}')
-            @registered.delete(plugin)
-          end
-        end
+          def test_module_redirect
+    get :module_redirect
+    assert_response :redirect
+    assert_redirected_to 'http://test.host/module_test/module_redirect/hello_world'
+  end
+    
+            # The abstract adapter is used simply to bypass the bit of code that
+        # checks that the adapter file can be required in.
+    
+          def initialize(subtype, coder)
+        @subtype = subtype
+        @coder = coder
+        super(subtype)
       end
+    
+    class TestAutosaveAssociationValidationsOnABelongsToAssociation < ActiveRecord::TestCase
+  self.use_transactional_tests = false unless supports_savepoints?
+    
+      before_destroy do |client|
+    if client.firm
+      Client.destroyed_client_ids[client.firm.id] << client.id
     end
+    true
+  end
+    
+      test 'broadcasting_for with a string' do
+    assert_equal 'hello', ChatChannel.broadcasting_for('hello')
   end
 end
 
     
-        end
-  end
-end
-
+        # Block arg
+    periodically every: 3 do
+      ping
+    end
     
-        # Validates that {#boundary} is {#valid_ip_or_range? a valid IP address or
-    # IP address range}. Due to this not being tested before it was moved here
-    # from Mdm, the default workspace does not validate. We always validate boundaries
-    # and a workspace may have a blank default boundary.
+      test 'subscription rejection' do
+    subscriptions = Minitest::Mock.new
+    subscriptions.expect(:remove_subscription, SecretChannel, [SecretChannel])
+    
+            wait_for_async
+    
+          rescue IOError
+        # Work around https://bugs.ruby-lang.org/issues/13405
+        #
+        # Puma's sometimes raising while shutting down, when it closes
+        # its internal pipe. We can safely ignore that, but we do need
+        # to do the step skipped by the exception:
+        server.binder.close
+    
+      [ EOFError, Errno::ECONNRESET ].each do |closed_exception|
+    test 'closes socket on #{closed_exception}' do
+      run_in_eventmachine do
+        connection = open_connection
+    
+    # Copyright (C) 2008 Rapid7, Inc.
+    
+    vers.each do |ver|
+  case ver
+  when '6.1.4'
+    __NR_execve      = 7
+    __NR_getpeername = 211
+    __NR_accept      = 237
+    __NR_listen      = 240
+    __NR_bind        = 242
+    __NR_socket      = 243
+    __NR_connect     = 244
+    __NR_close       = 278
+    __NR_kfcntl      = 658
+    
+        # See \{#modifier}.
+    # @return [String]
+    def resolved_modifier
+      # modifier should contain only a single string
+      modifier.first || ''
+    end
+    
+        # Interpolation in a property is of the form `before #{mid} after`.
     #
-    # @return [void]
-    def boundary_must_be_ip_range
-      unless boundary.blank?
-        begin
-          boundaries = Shellwords.split(boundary)
-        rescue ArgumentError
-          boundaries = []
-        end
-    
-    classNames.each { |name|
-	filesIn << clsFile.new_with_sig('Ljava.lang.String;', '#{outputDir}/#{name}.class')
-}
-    
-    parser = Parser.new(filename)
-parser.parse
-print parser.get_result
-    
-    	def parse_line(line)
-		if line =~ /\w+ <[\.\w]+>:/
-			# End a previous block
-			unless block_size == 0
-				block_end
-			end
-			block_begin(line)
-    
-      validate :enough_poll_answers
-  validates :question, presence: true
-    
-      def post(post, url='')
-    logger.debug 'event=post_to_service type=facebook sender_id=#{user_id} post=#{post.guid}'
-    response = post_to_facebook('https://graph.facebook.com/me/feed', create_post_params(post).to_param)
-    response = JSON.parse response.body
-    post.facebook_id = response['id']
-    post.save
-  end
-    
-          def handle_params_error(error, error_description)
-        if params[:client_id] && params[:redirect_uri]
-          handle_params_error_when_client_id_and_redirect_uri_exists(error, error_description)
-        else
-          render_error I18n.t('api.openid_connect.error_page.could_not_authorize'), error_description
-        end
-      end
-    
-        if run? && ARGV.any?
-      require 'optparse'
-      OptionParser.new { |op|
-        op.on('-p port',   'set the port (default is 4567)')                { |val| set :port, Integer(val) }
-        op.on('-o addr',   'set the host (default is #{bind})')             { |val| set :bind, val }
-        op.on('-e env',    'set the environment (default is development)')  { |val| set :environment, val.to_sym }
-        op.on('-s server', 'specify rack server/handler (default is thin)') { |val| set :server, val }
-        op.on('-q',        'turn on quiet mode (default is off)')           {       set :quiet, true }
-        op.on('-x',        'turn on the mutex lock (default is off)')       {       set :lock, true }
-      }.parse!(ARGV.dup)
-    end
-  end
-    
-        { # yes, this is ugly, feel free to change that
-      '/..' => '/', '/a/../b' => '/b', '/a/../b/' => '/b/', '/a/.' => '/a/',
-      '/%2e.' => '/', '/a/%2E%2e/b' => '/b', '/a%2f%2E%2e%2Fb/' => '/b/',
-      '//' => '/', '/%2fetc%2Fpasswd' => '/etc/passwd'
-    }.each do |a, b|
-      it('replaces #{a.inspect} with #{b.inspect}') { expect(get(a).body).to eq(b) }
-    end
-    
-        expect(get('/')).to be_ok
-  end
-end
-
-    
-          locations = Array.new
-      while (data.code.to_i == 301 || data.code.to_i == 302)
-        data = handle_gist_redirecting(data)
-        break if locations.include? data.header['Location']
-        locations << data.header['Location']
-      end
-    
-      class IncludeCodeTag < Liquid::Tag
-    def initialize(tag_name, markup, tokens)
-      @title = nil
-      @file = nil
-      if markup.strip =~ /\s*lang:(\S+)/i
-        @filetype = $1
-        markup = markup.strip.sub(/lang:\S+/i,'')
-      end
-      if markup.strip =~ /(.*)?(\s+|^)(\/*\S+)/i
-        @title = $1 || nil
-        @file = $3
-      end
-      super
-    end
-    
-      class RenderPartialTag < Liquid::Tag
-    include OctopressFilters
-    def initialize(tag_name, markup, tokens)
-      @file = nil
-      @raw = false
-      if markup =~ /^(\S+)\s?(\w+)?/
-        @file = $1.strip
-        @raw = $2 == 'raw'
-      end
-      super
+    # @param before [Node] See {Interpolation#before}
+    # @param mid [Node] See {Interpolation#mid}
+    # @param after [Node] See {Interpolation#after}
+    # @param wb [Boolean] See {Interpolation#whitespace_before}
+    # @param wa [Boolean] See {Interpolation#whitespace_after}
+    # @param originally_text [Boolean] See {Interpolation#originally_text}
+    # @param warn_for_color [Boolean] See {Interpolation#warn_for_color}
+    # @comment
+    #   rubocop:disable ParameterLists
+    def initialize(before, mid, after, wb, wa, opts = {})
+      # rubocop:enable ParameterLists
+      @before = before
+      @mid = mid
+      @after = after
+      @whitespace_before = wb
+      @whitespace_after = wa
+      @originally_text = opts[:originally_text] || false
+      @warn_for_color = opts[:warn_for_color] || false
+      @deprecation = opts[:deprecation] || :none
     end
