24be0a6bcb1c93c7e7bc888343f83bf32ec160fd
SUNsung <weatherius@gmail.com>
2018-08-03 23:41:27 +0300

    Rewrite [16K8pSkFV8H3216W91WgJk6J5U16OaWchH2DsKc16tvG.cpp] File in: 2018-08-03 23:41:27

--- cccccc/16K8pSkFV8H3216W91WgJk6J5U16OaWchH2DsKc16tvG.cpp
+++ cccccc/16K8pSkFV8H3216W91WgJk6J5U16OaWchH2DsKc16tvG.cpp
@@ -1,91 +1,243 @@
 
         
-        /// Attempt to get a doc comment from the declaration, or other inherited
-/// sources, like from base classes or protocols.
-Optional<DocComment *> getCascadingDocComment(swift::markup::MarkupContext &MC,
-                                             const Decl *D);
+        
+    { private:
+  DISALLOW_COPY_AND_ASSIGN(URLRequestAsyncAsarJob);
+};
     
-    #ifndef SWIFT_SYNTAX_REFERENCES_H
-#define SWIFT_SYNTAX_REFERENCES_H
+    #ifndef ATOM_RENDERER_PREFERENCES_MANAGER_H_
+#define ATOM_RENDERER_PREFERENCES_MANAGER_H_
     
-    #ifdef __APPLE__
+    // Attempts to send the current command line to an already running instance of
+// Chrome via a WM_COPYDATA message.
+// Returns true if a running Chrome is found and successfully notified.
+// |fast_start| is true when this is being called on the window fast start path.
+NotifyChromeResult AttemptToNotifyRunningChrome(HWND remote_window,
+                                                bool fast_start);
+    
+    namespace extensions {
+    }
+    
+    
+    { private:
+  DISALLOW_COPY_AND_ASSIGN(Net);
+};
+    
+    template <class T>
+struct DereferencingComparator { bool operator()(const T a, const T b) const { return *a < *b; } };
+    
+    
+    {}  // anonymous namespace
+    
+      // When start user key is prefix of limit user key
+  ASSERT_EQ(IKey('foo', 100, kTypeValue),
+            Shorten(IKey('foo', 100, kTypeValue),
+                    IKey('foobar', 200, kTypeValue)));
+    
+    Status SetCurrentFile(Env* env, const std::string& dbname,
+                      uint64_t descriptor_number) {
+  // Remove leading 'dbname/' and add newline to manifest file name
+  std::string manifest = DescriptorFileName(dbname, descriptor_number);
+  Slice contents = manifest;
+  assert(contents.starts_with(dbname + '/'));
+  contents.remove_prefix(dbname.size() + 1);
+  std::string tmp = TempFileName(dbname, descriptor_number);
+  Status s = WriteStringToFileSync(env, contents.ToString() + '\n', tmp);
+  if (s.ok()) {
+    s = env->RenameFile(tmp, CurrentFileName(dbname));
+  }
+  if (!s.ok()) {
+    env->DeleteFile(tmp);
+  }
+  return s;
+}
+    
+      // Return an iterator for the specified file number (the corresponding
+  // file length must be exactly 'file_size' bytes).  If 'tableptr' is
+  // non-NULL, also sets '*tableptr' to point to the Table object
+  // underlying the returned iterator, or NULL if no Table object underlies
+  // the returned iterator.  The returned '*tableptr' object is owned by
+  // the cache and should not be deleted, and is valid for as long as the
+  // returned iterator is live.
+  Iterator* NewIterator(const ReadOptions& options,
+                        uint64_t file_number,
+                        uint64_t file_size,
+                        Table** tableptr = NULL);
+    
+    static Slice TrimSpace(Slice s) {
+  int start = 0;
+  while (start < s.size() && isspace(s[start])) {
+    start++;
+  }
+  int limit = s.size();
+  while (limit > start && isspace(s[limit-1])) {
+    limit--;
+  }
+  return Slice(s.data() + start, limit - start);
+}
+    
+      // Advanced functions: these are used to reduce the space requirements
+  // for internal data structures like index blocks.
+    
+    	std::copy(cfgRoot.begin(), cfgRoot.end(), userConfigDirPath);
+	userConfigDirPath[cfgRoot.length()] = 0;
+    
+      // Check the crc of the type and the block contents
+  const char* data = contents.data();    // Pointer to where Read put the data
+  if (options.verify_checksums) {
+    const uint32_t crc = crc32c::Unmask(DecodeFixed32(data + n + 1));
+    const uint32_t actual = crc32c::Value(data, n + 1);
+    if (actual != crc) {
+      delete[] buf;
+      s = Status::Corruption('block checksum mismatch');
+      return s;
+    }
+  }
+    
+    
+    {  unsigned char data[48] = {
+    0x01, 0xc0, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0x14, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x04, 0x00,
+    0x00, 0x00, 0x00, 0x14,
+    0x00, 0x00, 0x00, 0x18,
+    0x28, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0x02, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+  };
+  ASSERT_EQ(0xd9963a56, Value(reinterpret_cast<char*>(data), sizeof(data)));
+}
+    
+    TEST(HASH, SignedUnsignedIssue) {
+  const unsigned char data1[1] = {0x62};
+  const unsigned char data2[2] = {0xc3, 0x97};
+  const unsigned char data3[3] = {0xe2, 0x99, 0xa5};
+  const unsigned char data4[4] = {0xe1, 0x80, 0xb9, 0x32};
+  const unsigned char data5[48] = {
+    0x01, 0xc0, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0x14, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x04, 0x00,
+    0x00, 0x00, 0x00, 0x14,
+    0x00, 0x00, 0x00, 0x18,
+    0x28, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0x02, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+  };
+    }
+    
+    namespace leveldb {
+    }
+    
+    #include <stdint.h>
+    
+    static std::string Shorten(const std::string& s, const std::string& l) {
+  std::string result = s;
+  InternalKeyComparator(BytewiseComparator()).FindShortestSeparator(&result, l);
+  return result;
+}
     
     
     {
-    {} // end namespace Lowering
-} // end namespace swift
-    
-    /// This represents one part of a #if block.  If the condition field is
-/// non-null, then this represents a #if or a #elseif, otherwise it represents
-/// an #else block.
-struct IfConfigClause {
-  /// The location of the #if, #elseif, or #else keyword.
-  SourceLoc Loc;
-  
-  /// The condition guarding this #if or #elseif block.  If this is null, this
-  /// is a #else clause.
-  Expr *Cond;
-  
-  /// Elements inside the clause
-  ArrayRef<ASTNode> Elements;
-    }
-    
-    #include 'test/cpp/qps/report.h'
-    
-    #include <set>
-    
-      bool PrintBetaServicer(const grpc_generator::Service* service,
-                         grpc_generator::Printer* out);
-  bool PrintBetaServerFactory(
-      const grpc::string& package_qualified_service_name,
-      const grpc_generator::Service* service, grpc_generator::Printer* out);
-  bool PrintBetaStub(const grpc_generator::Service* service,
-                     grpc_generator::Printer* out);
-  bool PrintBetaStubFactory(const grpc::string& package_qualified_service_name,
-                            const grpc_generator::Service* service,
-                            grpc_generator::Printer* out);
-    
-    static void sigint_handler(int x) {
-  gpr_atm_no_barrier_store(&grpc::testing::interop::g_got_sigint, true);
-}
-    
-    #include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-    
-    
-    {}  // namespace grpc
-    
-    
-    {  return cli.Run(argc, argv);
-}
+    {}  // namespace log
+}  // namespace leveldb
 
     
-    namespace grpc {
-    }
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
     
-    void AbstractBtMessage::setPeer(const std::shared_ptr<Peer>& peer)
-{
-  peer_ = peer;
+    // Licensed under the MIT License (the 'License'); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+// http://opensource.org/licenses/MIT
+    
+    
+    {    __DelOlderTouchTime(now);
+    touch_times_.push_back(now);
+    return true;
 }
     
-      BtMessageFactory* getBtMessageFactory() const { return messageFactory_; }
     
-    public:
-  AbstractCommand(
-      cuid_t cuid, const std::shared_ptr<Request>& req,
-      const std::shared_ptr<FileEntry>& fileEntry, RequestGroup* requestGroup,
-      DownloadEngine* e, const std::shared_ptr<SocketCore>& s = nullptr,
-      const std::shared_ptr<SocketRecvBuffer>& socketRecvBuffer = nullptr,
-      bool incNumConnection = true);
-    
-      virtual ~AdaptiveFileAllocationIterator();
-    
-      AnnounceTier(std::deque<std::string> urls);
+    {        return NULL;
+    }
     
     
-    {} // namespace aria2
+    {  protected:
+    std::string   m_servicename;
+    TServicesMap m_dependservices;
+};
     
-    #include <string>
-#include <memory>
+    // Licensed under the MIT License (the 'License'); you may not use this file except in 
+// compliance with the License. You may obtain a copy of the License at
+// http://opensource.org/licenses/MIT
+    
+    
+    static bool SpyHookLogFunc(struct XLoggerInfo_t& _info, std::string& _log);
+    void TestFun0();
+    int __TestFun1(int i);
+    
+    
+/*
+ * scop_jenv.h
+ *
+ *  Created on: 2012-8-21
+ *      Author: yanguoyue
+ */
+    
+        jint        (*RegisterNatives)(JNIEnv*, jclass, const JNINativeMethod*,
+                        jint);
+    jint        (*UnregisterNatives)(JNIEnv*, jclass);
+    jint        (*MonitorEnter)(JNIEnv*, jobject);
+    jint        (*MonitorExit)(JNIEnv*, jobject);
+    jint        (*GetJavaVM)(JNIEnv*, JavaVM**);
+    
+    
+/**
+ * Captures and symbolicates a stack trace
+ *
+ * Beware of a bug on some platforms, which makes the trace loop until the
+ * buffer is full when it reaches a noexpr function. It seems to be fixed in
+ * newer versions of gcc. https://gcc.gnu.org/bugzilla/show_bug.cgi?id=56846
+ *
+ * @param skip The number of frames before capturing the trace
+ *
+ * @param limit The maximum number of frames captured
+ */
+FBEXPORT inline std::vector<StackTraceElement> getStackTraceSymbols(
+    size_t skip = 0,
+    size_t limit = kDefaultLimit) {
+  return getStackTraceSymbols(getStackTrace(skip + 1, limit));
+}
+    
+      // TODO? Create reusable interface for Allocatable classes and use it to
+  // strengthen type-checking (and possibly provide a default
+  // implementation of allocate().)
+  template <typename... Args>
+  static local_ref<jhybridobject> allocateWithCxxArgs(Args&&... args) {
+    auto hybridData = makeCxxInstance(std::forward<Args>(args)...);
+    static auto allocateMethod =
+        javaClassStatic()->template getStaticMethod<jhybridobject(jhybriddata)>('allocate');
+    return allocateMethod(javaClassStatic(), hybridData.get());
+  }
+    
+      bool hasOnlyOneRef() const {
+    return m_refcount == 1;
+  }
+    
+      // Only allow implicit upcasts. A downcast will result in a compile error
+  // unless you use static_cast (which will end up invoking the explicit
+  // operator below).
+  template <typename U>
+  RefPtr(RefPtr<U>&& ref, typename std::enable_if<std::is_base_of<T,U>::value, U>::type* = nullptr) :
+    m_ptr(nullptr)
+  {
+    *this = std::move(ref);
+  }
