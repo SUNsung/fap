555f0f84bd78426b7d9e03c68ca996435025178b
SUNsung <weatherius@gmail.com>
2018-08-03 04:22:02 +0300

    Rewrite [331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py] File in: 2018-08-03 04:22:01

--- pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
+++ pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
@@ -1,151 +1,63 @@
 
         
-        print('Enter the PKCS1 private key, followed by a blank line:')
-privkey = b''
-while True:
-    try:
-        line = input()
-    except EOFError:
-        break
-    if line == '':
-        break
-    privkey += line.encode('ascii') + b'\n'
-privkey = rsa.PrivateKey.load_pkcs1(privkey)
+          def testCombineArcAndRootPotentials(self):
+    with self.test_session():
+      arcs = tf.constant([[[1, 2, 3],
+                           [2, 3, 4],
+                           [3, 4, 5]],
+                          [[3, 4, 5],
+                           [2, 3, 4],
+                           [1, 2, 3]]], tf.float32)  # pyformat: disable
+      roots = tf.constant([[6, 7, 8],
+                           [8, 7, 6]], tf.float32)  # pyformat: disable
     
-        infile, outfile = args
+      This method exports a master_spec and associated files into the SavedModel's
+  'assets.extra' directory (which is unmanaged). All resources are added to the
+  'assets.extra' directory using sanitized paths. The master spec itself is
+  located at the base of the assets.extra directory.
     
-        def test_keywords(self):
-        self.assertMatch(':ytsubs', ['youtube:subscriptions'])
-        self.assertMatch(':ytsubscriptions', ['youtube:subscriptions'])
-        self.assertMatch(':ythistory', ['youtube:history'])
-    
-    
-def gen_extractors():
-    ''' Return a list of an instance of every supported extractor.
-    The order does matter; the first extractor matched is the one handling the URL.
-    '''
-    return [klass() for klass in gen_extractor_classes()]
-    
-        def _real_extract(self, url):
-        display_id = self._match_id(url)
-    
-    
-def test_auth_plugin_require_auth_false_and_auth_provided(httpbin):
-    
-    
-@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
-@pytest.mark.parametrize('filename', filenames)
-def test_rst_file_syntax(filename):
-    p = subprocess.Popen(
-        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE
-    )
-    err = p.communicate()[1]
-    assert p.returncode == 0, err.decode('utf8')
-
-    
-    
-class HTTPBasicAuth(requests.auth.HTTPBasicAuth):
-    
-    
-@mock.patch('httpie.input.AuthCredentials._getpass',
-            new=lambda self, prompt: 'password')
-def test_password_prompt(httpbin):
-    r = http('--auth', 'user',
-             'GET', httpbin.url + '/basic-auth/user/password')
-    assert HTTP_OK in r
-    assert r.json == {'authenticated': True, 'user': 'user'}
-    
-            if now - self.lastmark >= 3:
-            self.lastmark = now
-            qps = len(self.tail) / sum(self.tail)
-            print('samplesize={0} concurrent={1} qps={2:0.2f}'.format(len(self.tail), self.concurrent, qps))
-    
-        def remove(self, key):
-        hash_index = self._hash_function(key)
-        for index, item in enumerate(self.table[hash_index]):
-            if item.key == key:
-                del self.table[hash_index][index]
-                return
-        raise KeyError('Key not found')
-    
-        def reducer_identity(self, key, value):
-        yield key, value
-    
-        def __init__(self, results):
-        self.results = results
-        self.next = next
-    
-        def __init__(self, user_id, name, pass_hash):
-        self.user_id = user_id
-        self.name = name
-        self.pass_hash = pass_hash
-        self.friends_by_id = {}  # key: friend id, value: User
-        self.friend_ids_to_private_chats = {}  # key: friend id, value: private chats
-        self.group_chats_by_id = {}  # key: chat id, value: GroupChat
-        self.received_friend_requests_by_friend_id = {}  # key: friend id, value: AddRequest
-        self.sent_friend_requests_by_friend_id = {}  # key: friend id, value: AddRequest
-    
-    def sequential():
-    return list(map(is_prime, PRIMES))
-    
-    
-def OverlapLength( left_string, right_string ):
-  '''Returns the length of the overlap between two strings.
-  Example: 'foo baro' and 'baro zoo' -> 4
-  '''
-  left_string_length = len( left_string )
-  right_string_length = len( right_string )
-    
-    
-  def Response( self ):
-    return {
-      'completions': self._results,
-      'completion_start_column': self.request_data[ 'start_column' ]
-    }
-    
-    import argparse
-    
-    
-  def Just_Doc_String_test( self ):
-    self._Check( 'not_an_int', {
-      'insertion_text':  'INSERTION TEXT',
-      'menu_text':       'MENU TEXT',
-      'extra_menu_info': 'EXTRA MENU INFO',
-      'kind':            'K',
-      'extra_data': {
-        'doc_string':    'DOC STRING',
-      },
-    }, {
-      'word'     : 'INSERTION TEXT',
-      'abbr'     : 'MENU TEXT',
-      'menu'     : 'EXTRA MENU INFO',
-      'kind'     : 'k',
-      'info'     : 'DOC STRING',
-      'dup'      : 1,
-      'empty'    : 1,
-      'user_data': 'not_an_int',
-    } )
-    
-    
-  def Start( self ):
-    request_data = BuildRequestData()
-    request_data.update( { 'filetypes': self.filetypes } )
-    self._response = self.PostDataToHandler( request_data,
-                                             'semantic_completion_available' )
-    
-    
-  def Poll( self, diagnostics_handler ):
-    '''This should be called regularly to check for new messages in this buffer.
-    Returns True if Poll should be called again in a while. Returns False when
-    the completer or server indicated that further polling should not be done
-    for the requested file.'''
-    
-    
-__licence__ = 'BSD (3 clause)'
-    
-    For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/light.zigbee/
+    _DUMMY_TEST_SENTENCE = '''
+token {
+  word: 'sentence' start: 0 end: 7 break_level: NO_BREAK
+}
+token {
+  word: '0' start: 9 end: 9 break_level: SPACE_BREAK
+}
+token {
+  word: '.' start: 10 end: 10 break_level: NO_BREAK
+}
 '''
-import voluptuous as vol
+    
+        try:
+      with open(FLAGS.actual_file) as actual:
+        content_actual = actual.read()
+    except IOError as e:
+      self.fail('Error opening '%s': %s' % (FLAGS.actual_file, e.strerror))
+    
+    
+def default_hooks():
+    return dict((event, []) for event in HOOKS)
+    
+    
+class VersionedPackage(object):
+    def __init__(self, version):
+        self.__version__ = version
+    
+        Provide both the name and the version of the Python implementation
+    currently running. For example, on CPython 2.7.5 it will return
+    {'name': 'CPython', 'version': '2.7.5'}.
+    
+    # ---------
+# Specifics
+# ---------
+    
+        # Sometimes, urllib3 only reports its version as 16.1.
+    if len(urllib3_version) == 2:
+        urllib3_version.append('0')
+    
+        return request('post', url, data=data, json=json, **kwargs)
+    
+            # It's possible that users might accidentally send a Request object.
+        # Guard against that specific failure case.
+        if isinstance(request, Request):
+            raise ValueError('You can only send PreparedRequests.')
