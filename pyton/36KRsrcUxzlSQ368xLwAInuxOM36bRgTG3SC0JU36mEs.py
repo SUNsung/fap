8813a02d9e10ac43083c940682d5c1f0b836128f
SUNsung <weatherius@gmail.com>
2018-08-03 18:31:06 +0300

    Rewrite [36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py] File in: 2018-08-03 18:31:06

--- pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
+++ pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
@@ -1,94 +1,14 @@
 
         
-                        if self.accept[s] >= 1:
-                    #print 'accept state for alt %d' % self.accept[s]
-                    return self.accept[s]
+          def testNoTrainSteps(self):
+    '''Edge case, 1 target, only pretrain.'''
+    generated_schedule = trainer_lib.generate_target_per_step_schedule([10],
+                                                                       [0])
+    expected_schedule = [0] * 10
+    self.assertEqual(generated_schedule, expected_schedule)
     
-        def tearDown(cls):
-        ''' Function/test case scope teardown. '''
-        cls.output.close()
-        sys.stdout = cls.saved_stdout
-    
-        def __init__(self, name, action):
-        self.name = name
-        self.action = action
-    
-        def test_items_recoil(self):
-        with ObjectPool(self.sample_queue, True) as pool:
-            self.assertEqual(pool, 'first')
-        self.assertTrue(self.sample_queue.get() == 'second')
-        self.assertFalse(self.sample_queue.empty())
-        self.assertTrue(self.sample_queue.get() == 'first')
-        self.assertTrue(self.sample_queue.empty())
-    
-        def setUp(self):
-        self.tc1 = TC1()
-        self.tc2 = TC2()
-        self.tc3 = TC3()
-        self.average_result_tc1 = '###### In Test 1 ######\n' + \
-                                  'Setting up\n' + \
-                                  'Running test\n' + \
-                                  'Tearing down\n' + \
-                                  'Test Finished'
-        self.average_result_tc2 = '###### In Test 2 ######\n' + \
-                                  'Setting up\n' + \
-                                  'Running test\n' + \
-                                  'Tearing down\n' + \
-                                  'Test Finished'
-        self.average_result_tc3 = '###### In Test 3 ######\n' + \
-                                  'Setting up\n' + \
-                                  'Running test\n' + \
-                                  'Tearing down\n' + \
-                                  'Test Finished'
-        self.runner = TestRunner()
-        self.out = StringIO()
-        self.saved_stdout = sys.stdout
-        sys.stdout = self.out
-    
-    
-    
-    from __future__ import print_function
-    
-    ### OUTPUT ###
-# Setting Data 1 = 10
-# DecimalViewer: Subject Data 1 has data 10
-# HexViewer: Subject Data 1 has data 0xa
-# Setting Data 2 = 15
-# HexViewer: Subject Data 2 has data 0xf
-# DecimalViewer: Subject Data 2 has data 15
-# Setting Data 1 = 3
-# DecimalViewer: Subject Data 1 has data 3
-# HexViewer: Subject Data 1 has data 0x3
-# Setting Data 2 = 5
-# HexViewer: Subject Data 2 has data 0x5
-# DecimalViewer: Subject Data 2 has data 5
-# Detach HexViewer from data1 and data2.
-# Setting Data 1 = 10
-# DecimalViewer: Subject Data 1 has data 10
-# Setting Data 2 = 15
-# DecimalViewer: Subject Data 2 has data 15
-
-    
-        def is_satisfied_by(self, candidate):
-        return bool(not self._wrapped.is_satisfied_by(candidate))
-    
-    import tornado.ioloop
-import tornado.web
-from tornado import options
-    
-    
-class MainHandler(BaseHandler, TwitterMixin):
-    @authenticated
-    @gen.coroutine
-    def get(self):
-        timeline = yield self.twitter_request(
-            '/statuses/home_timeline',
-            access_token=self.current_user['access_token'])
-        self.render('home.html', timeline=timeline)
-    
-        Returned links have had the fragment after `#` removed, and have been made
-    absolute so, e.g. the URL 'gen.html#tornado.gen.coroutine' becomes
-    'http://www.tornadoweb.org/en/stable/gen.html'.
-    '''
-    response = await httpclient.AsyncHTTPClient().fetch(url)
-    print('fetched %s' % url)
+        base = 'src/sentry/locale'
+    for locale in os.listdir(base):
+        fn = os.path.join(base, locale, 'LC_MESSAGES', 'django.po')
+        if not os.path.isfile(fn):
+            continue
