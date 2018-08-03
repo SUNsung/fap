662749f3834a2102b5ba9e4ed4bfd9f4ac262d14
SUNsung <weatherius@gmail.com>
2018-08-03 05:20:48 +0300

    Rewrite [23NkwOQ1NHMtI238iUmm.CoGL.23.iEz6MR787w23hFg.py] File in: 2018-08-03 05:20:47

--- pyton/23NkwOQ1NHMtI238iUmm.CoGL.23.iEz6MR787w23hFg.py
+++ pyton/23NkwOQ1NHMtI238iUmm.CoGL.23.iEz6MR787w23hFg.py
@@ -1,40 +1,230 @@
 
         
-                self.poolmanager = PoolManager(num_pools=connections, maxsize=maxsize,
-                                       block=block, strict=True, **pool_kwargs)
+        README_FILE = 'README.md'
+helptext = sys.stdin.read()
     
     
-@pytest.mark.parametrize(
-    'value, expected', (
-                (u'test', True),
-                (u'æíöû', False),
-                (u'ジェーピーニック', False),
-    )
-)
-def test_unicode_is_ascii(value, expected):
-    assert unicode_is_ascii(value) is expected
+from youtube_dl import YoutubeDL
     
-            return 'Digest %s' % (base)
+        def test_secondary_proxy_http(self):
+        params = self._check_params(['secondary_proxy', 'secondary_server_ip'])
+        if params is None:
+            return
+        ydl = FakeYDL()
+        req = compat_urllib_request.Request('http://yt-dl.org/ip')
+        req.add_header('Ytdl-request-proxy', params['secondary_proxy'])
+        self.assertEqual(
+            ydl.urlopen(req).read().decode('utf-8'),
+            params['secondary_server_ip'])
     
-        elif hasattr(o, 'fileno'):
-        try:
-            fileno = o.fileno()
-        except io.UnsupportedOperation:
-            pass
-        else:
-            total_length = os.fstat(fileno).st_size
+            def _find_spec(key):
+            m = re.search(
+                r'(?m)^//\s*%s:\s*(.*?)\n' % re.escape(key), as_content)
+            if not m:
+                raise ValueError('Cannot find %s in %s' % (key, testfile))
+            return json.loads(m.group(1))
     
-        REGISTRY = {}
+                    m = re.search(r'(?<=\s)u[\''](?!\)|,|$)', code)
+                if m is not None:
+                    self.assertTrue(
+                        m is None,
+                        'u present in %s, around %s' % (
+                            fn, code[m.start() - 10:m.end() + 10]))
     
-        def test_shall_toggle_from_fm_to_am(self):
-        self.radio.toggle_amfm()
-        state = self.radio.state.name
-        expected_state_name = 'AM'
-        self.assertEqual(state, expected_state_name)
+            return {
+            'id': flv_id,
+            'url': rtmp_url,
+            'ext': 'flv',
+            'no_resume': True,
+            'title': title,
+            'description': description,
+            'duration': duration,
+            'view_count': view_count,
+            'comment_count': comment_count,
+            'uploader': uploader,
+            'upload_date': upload_date,
+        }
 
     
-        def undo(self):
-        self.rename(self.dest, self.src)
+            title = self._search_regex(
+            r'<title>(.+?)\s*\|\s*.+?</title>', webpage, 'video title')
     
-        def is_satisfied_by(self, candidate):
-        return bool(not self._wrapped.is_satisfied_by(candidate))
+    containers = (('thefuck/python3-tcsh',
+               u'''FROM python:3
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'),
+              ('thefuck/python2-tcsh',
+               u'''FROM python:2
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'))
+    
+    Provides utility functions that are consumed internally by Requests
+which depend on extremely few external helpers (such as compat)
+'''
+    
+                        for i in request.body:
+                        low_conn.send(hex(len(i))[2:].encode('utf-8'))
+                        low_conn.send(b'\r\n')
+                        low_conn.send(i)
+                        low_conn.send(b'\r\n')
+                    low_conn.send(b'0\r\n\r\n')
+    
+            #: A list of :class:`Response <Response>` objects from
+        #: the history of the Request. Any redirect responses will end
+        #: up here. The list is sorted from the oldest to the most recent request.
+        self.history = []
+    
+        global __doc__
+    __doc__ = (__doc__ + '\n' +
+               '\n'.join(doc(code) for code in sorted(_codes))
+               if __doc__ is not None else None)
+    
+    with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+with open('HISTORY.rst', 'r', 'utf-8') as f:
+    history = f.read()
+    
+    if hasattr(ctypes, 'windll'):
+    WSAStringToAddressA = ctypes.windll.ws2_32.WSAStringToAddressA
+    WSAAddressToStringA = ctypes.windll.ws2_32.WSAAddressToStringA
+else:
+    def not_windows():
+        raise SystemError(
+            'Invalid platform. ctypes.windll must be available.'
+        )
+    WSAStringToAddressA = not_windows
+    WSAAddressToStringA = not_windows
+    
+    
+    def getCharPositionInLine(self):
+        return self.charPositionInLine
+    
+    def setCharPositionInLine(self, pos):
+        self.charPositionInLine = pos
+    
+    INVALID_TOKEN_TYPE = 0
+    
+        def setUp(cls):
+        ''' Function/test case scope setup. '''
+        cls.output = StringIO()
+        cls.saved_stdout = sys.stdout
+        sys.stdout = cls.output
+    
+    
+class RegistryHolder(type):
+    
+        def test_bear_greek_localization(self):
+        self.assertEqual(self.g.get('bear'), 'bear')
+
+    
+        def tearDown(self):
+        self.out.close()
+        sys.stdout = self.saved_stdout
+    
+            # simple test to validate param value
+        if param in self._static_method_choices.keys():
+            self.param = param
+        else:
+            raise ValueError('Invalid Value for Param: {0}'.format(param))
+    
+    print()
+    
+        def setProblem(self, value):
+        self._bProblem = value
+    
+    
+### OUTPUT ###
+# <NumObj: -1>
+# <NumObj: 0>
+# <NumObj: 1>
+# <NumObj: 2>
+# -- committed
+# <NumObj: 3>
+# <NumObj: 4>
+# <NumObj: 5>
+# -- rolled back
+# <NumObj: 2>
+# -- now doing stuff ...
+# -> doing stuff failed!
+# Traceback (most recent call last):
+# File 'memento.py', line 97, in <module>
+#     num_obj.do_stuff()
+#   File 'memento.py', line 52, in transaction
+#     raise e
+#   File 'memento.py', line 49, in transaction
+#     return self.method(obj, *args, **kwargs)
+#   File 'memento.py', line 70, in do_stuff
+#     self.increment()     # <- will fail and rollback
+#   File 'memento.py', line 65, in increment
+#     self.value += 1
+# TypeError: Can't convert 'int' object to str implicitly
+# <NumObj: 2>
+
+    
+    *TL;DR80
+Provides recombination business logic by chaining together using boolean logic.
+'''
+    
+        app = Application(
+        [
+            ('/', MainHandler),
+            ('/login', LoginHandler),
+            ('/logout', LogoutHandler),
+        ],
+        login_url='/login',
+        **options.group_dict('application'))
+    app.listen(options.port)
+    
+    define('port', default=8888, help='run on the given port', type=int)
+    
+            ``cursor`` should be the ``id`` of the last message received.
+        '''
+        results = []
+        for msg in reversed(self.cache):
+            if msg['id'] == cursor:
+                break
+            results.append(msg)
+        results.reverse()
+        return results
+    
+    define('port', default=8888, help='run on the given port', type=int)
+define('facebook_api_key', help='your Facebook application API key', type=str)
+define('facebook_secret', help='your Facebook application secret', type=str)
+    
+        logging.warning('Starting fetch with curl client')
+    curl_client = CurlAsyncHTTPClient()
+    curl_client.fetch('http://localhost:%d/' % options.port,
+                      callback=callback)
+    IOLoop.current().start()
+    
+    # These benchmarks are delicate.  They hit various fast-paths in the gen
+# machinery in order to stay synchronous so we don't need an IOLoop.
+# This removes noise from the results, but it's easy to change things
+# in a way that completely invalidates the results.
+    
+    
+def start(port, root_directory, bucket_depth):
+    '''Starts the mock S3 server on the given port at the given path.'''
+    application = S3Application(root_directory, bucket_depth)
+    http_server = httpserver.HTTPServer(application)
+    http_server.listen(port)
+    ioloop.IOLoop.current().start()
+    
+            yield write(b'\r\n')
+    
+    extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+]
+    
+    Circular references are not leaks per se, because they will eventually
+be GC'd. However, on CPython, they prevent the reference-counting fast
+path from being used and instead rely on the slower full GC. This
+increases memory footprint and CPU overhead, so we try to eliminate
+circular references created by normal operation.
+'''
