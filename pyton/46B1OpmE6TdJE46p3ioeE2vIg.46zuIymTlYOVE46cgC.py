c57742f8de2daf34e3a76e1bf7d3cfab8a1f45e0
SUNsung <weatherius@gmail.com>
2018-08-04 13:49:57 +0300

    Rewrite [46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py] File in: 2018-08-04 13:49:56

--- pyton/46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py
+++ pyton/46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py
@@ -1,165 +1,206 @@
 
         
-            def finish(self):
-        assert not self.finished
-        self.finished = True
-        self.status.finished()
+                def to_json(self, value):
+            return [[k, self.serializer.tag(v)] for k, v in iteritems(value)]
     
-    from httpie.plugins.base import AuthPlugin
+        exec('def reraise(tp, value, tb=None):\n raise tp, value, tb')
     
-        '''
-)
-network.add_argument(
-    '--check-status',
-    default=False,
-    action='store_true',
-    help='''
-    By default, HTTPie exits with 0 when no network or other fatal errors
-    occur. This flag instructs HTTPie to also check the HTTP status code and
-    exit with an error if the status indicates one.
+        def __init__(self, root_path, defaults=None):
+        dict.__init__(self, defaults or {})
+        self.root_path = root_path
     
-        def test_cert_file_not_found(self, httpbin_secure):
-        r = http(httpbin_secure + '/get',
-                 '--cert', '/__not_found__',
-                 error_exit_ok=True)
-        assert r.exit_status == ExitStatus.ERROR
-        assert 'No such file or directory' in r.stderr
+            # In case just a slash was appended we can be extra helpful
+        if request.base_url + '/' == exc.new_url.split('?')[0]:
+            buf.append('  The URL was defined with a trailing slash so '
+                       'Flask will automatically redirect to the URL '
+                       'with the trailing slash if it was accessed '
+                       'without one.')
     
-        def get_auth_plugin(self, auth_type):
-        return self.get_auth_plugin_mapping()[auth_type]
+            def __init__(self, name, doc=None):
+            self.name = name
+            self.__doc__ = doc
+        def _fail(self, *args, **kwargs):
+            raise RuntimeError('signalling support is unavailable '
+                               'because the blinker library is '
+                               'not installed.')
+        send = lambda *a, **kw: None
+        connect = disconnect = has_receivers_for = receivers_for = \
+            temporarily_connected_to = connected_to = _fail
+        del _fail
     
-        if args.auth_plugin:
-        session.auth = {
-            'type': args.auth_plugin.auth_type,
-            'raw_auth': args.auth_plugin.raw_auth,
-        }
-    elif session.auth:
-        kwargs['auth'] = session.auth
+        When you want to decorate a pluggable view you will have to either do that
+    when the view function is created (by wrapping the return value of
+    :meth:`as_view`) or you can use the :attr:`decorators` attribute::
     
-        print()
-    print('Classification performance:')
-    print('===========================')
-    print()
-    print('%s %s %s %s' % ('Classifier  ', 'train-time', 'test-time',
-                           'Accuracy'))
-    print('-' * 44)
-    for name in sorted(accuracy, key=accuracy.get):
-        print('%s %s %s %s' % (name.ljust(16),
-                               ('%.4fs' % train_time[name]).center(10),
-                               ('%.4fs' % test_time[name]).center(10),
-                               ('%.4f' % accuracy[name]).center(10)))
+        info(
+        'Releasing %s (codename %s, release date %s)',
+        version, codename, release_date.strftime('%d/%m/%Y')
+    )
+    tags = get_git_tags()
     
-        ###########################################################################
-    # Set SparseRandomProjection input
-    sparse_matrix_params = {
-        'n_components': opts.n_components,
-        'random_state': opts.random_seed,
-        'density': opts.density,
-        'eps': opts.eps,
-    }
+        if logging_plugin:
+        pytestconfig.pluginmanager.register(logging_plugin, 'logging-plugin')
     
-        # the training data folder must be passed as first argument
-    movie_reviews_data_folder = sys.argv[1]
-    dataset = load_files(movie_reviews_data_folder, shuffle=False)
-    print('n_samples: %d' % len(dataset.data))
+        Tests regressions.
+    
+            Number:                    '#990000',        # class: 'm'
     
     
-if __name__ == '__main__':
-    # NOTE: we put the following in a 'if __name__ == '__main__'' protected
-    # block to be able to use a multi-core grid search that also works under
-    # Windows, see: http://docs.python.org/library/multiprocessing.html#windows
-    # The multiprocessing module is used as the backend of joblib.Parallel
-    # that is used when n_jobs != 1 in GridSearchCV
-    
-    # Plot perfect predictions
-plt.plot([1.0], [0.0], 'ro', ms=20, label='Class 1')
-plt.plot([0.0], [1.0], 'go', ms=20, label='Class 2')
-plt.plot([0.0], [0.0], 'bo', ms=20, label='Class 3')
-    
-    
-def generate_data(n_samples, n_features):
-    '''Generate random blob-ish data with noisy features.
-    
-    n_samples = 100
-n_clusters_range = np.linspace(2, n_samples, 10).astype(np.int)
-    
-    X = list()
-y = list()
-for i, (phi, a) in enumerate([(.5, .15), (.5, .6), (.3, .2)]):
-    for _ in range(30):
-        phase_noise = .01 * np.random.normal()
-        amplitude_noise = .04 * np.random.normal()
-        additional_noise = 1 - 2 * np.random.rand(n_features)
-        # Make the noise sparse
-        additional_noise[np.abs(additional_noise) < .997] = 0
-    
-        return strings
-
-    
-    def kuwo_download_by_rid(rid, output_dir = '.', merge = True, info_only = False):
-    html=get_content('http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_%s'%rid)
-    title=match1(html,r'<name>(.*)</name>')
-    #to get title
-    #format =aac|mp3 ->to get aac format=mp3 ->to get mp3
-    url=get_content('http://antiserver.kuwo.cn/anti.s?format=mp3&rid=MUSIC_%s&type=convert_url&response=url'%rid)
-    songtype, ext, size = url_info(url)
-    print_info(site_info, title, songtype, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir)
-    
-            print_info(site_info, title, type_, size_full)
-        if not info_only:
-            download_urls(url_list, title, ext, total_size=size_full, output_dir=output_dir, merge=merge, headers=fake_headers)
-    else:
-        raise NotImplementedError(flashvars)
-    
-    site_info = 'Mixcloud.com'
-download = mixcloud_download
-download_playlist = playlist_not_supported('mixcloud')
-
-    
-    site = Pinterest()
-download = site.download_by_url
-# TBD: implement download_playlist
+def dispatch_hook(key, hooks, hook_data, **kwargs):
+    '''Dispatches a hook dictionary on a given piece of data.'''
+    hooks = hooks or dict()
+    hooks = hooks.get(key)
+    if hooks:
+        if hasattr(hooks, '__call__'):
+            hooks = [hooks]
+        for hook in hooks:
+            _hook_data = hook(hook_data, **kwargs)
+            if _hook_data is not None:
+                hook_data = _hook_data
+    return hook_data
 
     
     
-def ExtractKeywordsFromGroup_Basic_test():
-  assert_that( syntax_parse._ExtractKeywordsFromGroup(
-                 syntax_parse.SyntaxGroup( '', [
-                   'foo bar',
-                   'zoo goo',
-                 ] ) ),
-               contains_inanyorder( 'foo', 'bar', 'zoo', 'goo' ) )
+def consume_socket_content(sock, timeout=0.5):
+    chunks = 65536
+    content = b''
     
-      python_interpreter = _PathToPythonUsedDuringBuild()
-  if python_interpreter and utils.GetExecutable( python_interpreter ):
-    return python_interpreter
+    def test_fragment_not_sent_with_request():
+    '''Verify that the fragment portion of a URI isn't sent to the server.'''
+    def response_handler(sock):
+        req = consume_socket_content(sock, timeout=0.5)
+        sock.send(
+            b'HTTP/1.1 200 OK\r\n'
+            b'Content-Length: '+bytes(len(req))+b'\r\n'
+            b'\r\n'+req
+        )
+    
+    import json
+import platform
+import sys
+import ssl
+    
+    # Add any extra paths that contain custom files (such as robots.txt or
+# .htaccess) here, relative to this directory. These files are copied
+# directly to the root of the documentation.
+#html_extra_path = []
+    
+        def lower_items(self):
+        '''Like iteritems(), but with all lowercase keys.'''
+        return (
+            (lowerkey, keyval[1])
+            for (lowerkey, keyval)
+            in self._store.items()
+        )
+    
+    Some codes have multiple names, and both upper- and lower-case versions of
+the names are allowed. For example, ``codes.ok``, ``codes.OK``, and
+``codes.okay`` all correspond to the HTTP status code 200.
+'''
     
     
-  def result( self ):
-    return self._result
+def request(method, url, **kwargs):
+    '''Constructs and sends a :class:`Request <Request>`.
     
+    import datetime
+import sys
     
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write('Hello, world')
+        time_ridge = np.empty(n_iter)
+    time_ols = np.empty(n_iter)
+    time_lasso = np.empty(n_iter)
     
-            with ignore_deprecation():
-            with ExceptionStackContext(error_handler):
-                self.fetch('/chunk', header_callback=header_callback)
-        self.assertEqual(len(exc_info), 1)
-        self.assertIs(exc_info[0][0], ZeroDivisionError)
+        xx = np.arange(100, 100 + n * step, step)
+    plt.figure('scikit-learn vs. glmnet benchmark results')
+    plt.title('Regression in high dimensional spaces (%d samples)' % n_samples)
+    plt.plot(xx, scikit_results, 'b-', label='scikit-learn')
+    plt.plot(xx, glmnet_results, 'r-', label='glmnet')
+    plt.legend()
+    plt.xlabel('number of features')
+    plt.ylabel('Time (s)')
+    plt.axis('tight')
+    plt.show()
+
     
+    import time
     
-Multi = multi
+            if event == 'clear':
+            self.ax.clear()
+            self.ax.set_xticks([])
+            self.ax.set_yticks([])
+            self.contours = []
+            self.c_labels = None
+            self.plot_kernels()
     
-            .. versionchanged:: 4.4
-           Added the ``reuse_port`` argument.
-        '''
-        sockets = bind_sockets(port, address=address, family=family,
-                               backlog=backlog, reuse_port=reuse_port)
-        if self._started:
-            self.add_sockets(sockets)
-        else:
-            self._pending_sockets.extend(sockets)
+    from sklearn.datasets import make_checkerboard
+from sklearn.datasets import samples_generator as sg
+from sklearn.cluster.bicluster import SpectralBiclustering
+from sklearn.metrics import consensus_score
+    
+    # Annotate points on the simplex
+plt.annotate(r'($\frac{1}{3}$, $\frac{1}{3}$, $\frac{1}{3}$)',
+             xy=(1.0/3, 1.0/3), xytext=(1.0/3, .23), xycoords='data',
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             horizontalalignment='center', verticalalignment='center')
+plt.plot([1.0/3], [1.0/3], 'ko', ms=5)
+plt.annotate(r'($\frac{1}{2}$, $0$, $\frac{1}{2}$)',
+             xy=(.5, .0), xytext=(.5, .1), xycoords='data',
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             horizontalalignment='center', verticalalignment='center')
+plt.annotate(r'($0$, $\frac{1}{2}$, $\frac{1}{2}$)',
+             xy=(.0, .5), xytext=(.1, .5), xycoords='data',
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             horizontalalignment='center', verticalalignment='center')
+plt.annotate(r'($\frac{1}{2}$, $\frac{1}{2}$, $0$)',
+             xy=(.5, .5), xytext=(.6, .6), xycoords='data',
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             horizontalalignment='center', verticalalignment='center')
+plt.annotate(r'($0$, $0$, $1$)',
+             xy=(0, 0), xytext=(.1, .1), xycoords='data',
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             horizontalalignment='center', verticalalignment='center')
+plt.annotate(r'($1$, $0$, $0$)',
+             xy=(1, 0), xytext=(1, .1), xycoords='data',
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             horizontalalignment='center', verticalalignment='center')
+plt.annotate(r'($0$, $1$, $0$)',
+             xy=(0, 1), xytext=(.1, 1), xycoords='data',
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             horizontalalignment='center', verticalalignment='center')
+# Add grid
+plt.grid('off')
+for x in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
+    plt.plot([0, x], [x, 0], 'k', alpha=0.2)
+    plt.plot([0, 0 + (1-x)/2], [x, x + (1-x)/2], 'k', alpha=0.2)
+    plt.plot([x, x + (1-x)/2], [0, 0 + (1-x)/2], 'k', alpha=0.2)
+    
+    Shows how shrinkage improves classification.
+'''
+    
+    The usual covariance maximum likelihood estimate can be regularized
+using shrinkage. Ledoit and Wolf proposed a close formula to compute
+the asymptotically optimal shrinkage parameter (minimizing a MSE
+criterion), yielding the Ledoit-Wolf covariance estimate.
+    
+    #Load the digits dataset
+digits = datasets.load_digits()
+    
+        # Gloss the lips
+    d.polygon(face_landmarks['top_lip'], fill=(150, 0, 0, 128))
+    d.polygon(face_landmarks['bottom_lip'], fill=(150, 0, 0, 128))
+    d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=8)
+    d.line(face_landmarks['bottom_lip'], fill=(150, 0, 0, 64), width=8)
+    
+        # Write the resulting image to the output video file
+    print('Writing frame {} / {}'.format(frame_number, length))
+    output_movie.write(frame)
+    
+    # Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
+    
+    # Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
+
+    
+    # If false, no index is generated.
+#html_use_index = True
