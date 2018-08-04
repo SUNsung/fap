14f43727b4eee70e00b5eb5d425d8b7e3304806e
SUNsung <weatherius@gmail.com>
2018-08-04 09:55:49 +0300

    Rewrite [522sNkiPBCGcM52wYKIF0ahSo52fwtanFEN5kk52q7..cpp] File in: 2018-08-04 09:55:48

--- cccccc/522sNkiPBCGcM52wYKIF0ahSo52fwtanFEN5kk52q7..cpp
+++ cccccc/522sNkiPBCGcM52wYKIF0ahSo52fwtanFEN5kk52q7..cpp
@@ -1,147 +1,202 @@
-#endif  // GOOGLE_CUDA
 
-    
-    namespace tensorflow {
-    }
-    
-    class WritableFile;
-    
-    namespace internal {
-template <typename Scalar>
-struct functor_traits<scalar_clip_op<Scalar> > {
-  enum {
-    Cost = NumTraits<Scalar>::AddCost * 3,
-    PacketAccess = packet_traits<Scalar>::HasMax &&
-                   packet_traits<Scalar>::HasMin &&
-                   packet_traits<Scalar>::HasNegate
-  };
-};
-}  // namespace internal
-    
-    void SYCLDeviceContext::CopyCPUTensorToDevice(const Tensor *cpu_tensor,
-                                              Device *device,
-                                              Tensor *device_tensor,
-                                              StatusCallback done) const {
-  const int64 total_bytes = cpu_tensor->TotalBytes();
-  if (total_bytes > 0) {
-    const void *src_ptr = DMAHelper::base(cpu_tensor);
-    void *dst_ptr = DMAHelper::base(device_tensor);
-    switch (cpu_tensor->dtype()) {
-      case DT_FLOAT:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<float *>(dst_ptr), static_cast<const float *>(src_ptr),
-            total_bytes);
-        break;
-      case DT_DOUBLE:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<double *>(dst_ptr),
-            static_cast<const double *>(src_ptr), total_bytes);
-        break;
-      case DT_INT32:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<int32 *>(dst_ptr), static_cast<const int32 *>(src_ptr),
-            total_bytes);
-        break;
-      case DT_INT64:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<int64 *>(dst_ptr), static_cast<const int64 *>(src_ptr),
-            total_bytes);
-        break;
-      case DT_HALF:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<Eigen::half *>(dst_ptr),
-            static_cast<const Eigen::half *>(src_ptr), total_bytes);
-        break;
-      case DT_COMPLEX64:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<std::complex<float> *>(dst_ptr),
-            static_cast<const std::complex<float> *>(src_ptr), total_bytes);
-        break;
-      case DT_COMPLEX128:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<std::complex<double> *>(dst_ptr),
-            static_cast<const std::complex<double> *>(src_ptr), total_bytes);
-        break;
-      case DT_INT8:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<int8 *>(dst_ptr), static_cast<const int8 *>(src_ptr),
-            total_bytes);
-        break;
-      case DT_INT16:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<int16 *>(dst_ptr), static_cast<const int16 *>(src_ptr),
-            total_bytes);
-        break;
-      case DT_UINT8:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<uint8 *>(dst_ptr), static_cast<const uint8 *>(src_ptr),
-            total_bytes);
-        break;
-      case DT_UINT16:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<uint16 *>(dst_ptr),
-            static_cast<const uint16 *>(src_ptr), total_bytes);
-        break;
-      case DT_BOOL:
-        device->eigen_sycl_device()->memcpyHostToDevice(
-            static_cast<bool *>(dst_ptr), static_cast<const bool *>(src_ptr),
-            total_bytes);
-        break;
-      default:
-        assert(false && 'unsupported type');
-    }
+        
+        
+    {
+    {    rph->Send(new ShellViewMsg_Open(path));
   }
-  device->eigen_sycl_device()->synchronize();
-  done(Status::OK());
 }
     
-    class SYCLDeviceContext : public DeviceContext {
- public:
-  SYCLDeviceContext() {}
+    Menu::Menu(int id,
+           const base::WeakPtr<ObjectManager>& object_manager,
+           const base::DictionaryValue& option,
+           const std::string& extension_id)
+  : Base(id, object_manager, option, extension_id), enable_show_event_(false)  {
+  Create(option);
+}
+    
+    void Menu::Remove(MenuItem* menu_item, int pos) {
+  if (pos < 0 || pos >= (int)menu_items_.size()) return;
     }
     
-    #ifdef TENSORFLOW_USE_MPI
+          std::unique_ptr<SkBitmap> bitmap(new SkBitmap());
+      if (data.type == TYPE_PNG &&
+        !gfx::PNGCodec::Decode(reinterpret_cast<const unsigned char*>(decoded_str.c_str()), decoded_str.size(), bitmap.get())) {
+        error_ = 'Failed to decode as PNG';
+        return false;
+      } else if (data.type == TYPE_JPEG) {
+        std::unique_ptr<SkBitmap> tmp_bitmap = gfx::JPEGCodec::Decode(reinterpret_cast<const unsigned char*>(decoded_str.c_str()), decoded_str.size());
+        if (tmp_bitmap == NULL) {
+          error_ = 'Failed to decode as JPEG';
+          return false;
+        }
+        bitmap = std::move(tmp_bitmap);
+      }
+    
+    NwObjCallObjectMethodFunction::~NwObjCallObjectMethodFunction() {
+}
+    
+        SECTION('operator[key] throws when given an invalid key') {
+      REQUIRE_THROWS_WITH(cursor['foo'], StartsWith('No such key: 'foo''));
+      REQUIRE_THROWS_WITH(cursor['bar'], StartsWith('No such key: 'bar''));
+    }
+    
+    template <typename Parameters>
+void assign_parameter(
+    const Parameters& parameters,
+    const char* name,
+    torch::Tensor new_tensor) {
+  auto parameter = parameters.at(name);
+  parameter.set_requires_grad(false);
+  parameter.flatten().copy_(new_tensor);
+  parameter.set_requires_grad(true);
+}
+    
+      bool RunOnDevice() override;
+    
+      if (dim_tensor1 == 1 && dim_tensor2 == 1) {
+    return has_out ? at::native::dot_out(out, tensor1, tensor2) : tensor1.dot(tensor2);
+  } else if (dim_tensor1 == 2 && dim_tensor2 == 1) {
+    return has_out ? at::native::mv_out(out, tensor1, tensor2) : tensor1.mv(tensor2);
+  } else if (dim_tensor1 == 1 && dim_tensor2 == 2) {
+    return has_out ? at::native::mm_out(out, tensor1.unsqueeze(0), tensor2).squeeze_(0)
+                   : tensor1.unsqueeze(0).mm(tensor2).squeeze_(0);
+  } else if (dim_tensor1 == 2 && dim_tensor2 == 2) {
+    return has_out ? at::native::mm_out(out, tensor1, tensor2) : tensor1.mm(tensor2);
+  } else if (dim_tensor1 >= 3 && (dim_tensor2 == 1 || dim_tensor2 == 2)) {
+    // optimization: use mm instead of bmm by folding tensor1's batch into
+    // its leading matrix dimension.
+    }
+    
+    #include 'caffe2/core/context.h'
+#include 'caffe2/core/logging.h'
+#include 'caffe2/core/operator.h'
+#include 'caffe2/utils/math.h'
+    
+      EMatXf result_gt(5, 4);
+  result_gt << 206.94953073, -30.71519157, 298.3876512, 245.44846569,
+      143.8712194, -83.34289038, 291.50227513, 122.05339902, 177.43029521,
+      198.66623633, 197.29527254, 229.70308414, 152.25190373, 145.43156421,
+      388.21547899, 275.59425266, 5.06242193, 11.04094661, 67.32890274,
+      270.68622005;
+    
+    namespace rpc {
+    }
+    
+    namespace leveldb {
+    }
     
     
-    {#undef REGISTER_COMPLEX
-}  // namespace tensorflow
+    {  size_t read = 0;
+  std::string read_data;
+  while (read < kWriteSize) {
+    ASSERT_OK(seq_file->Read(kWriteSize - read, &result, scratch));
+    read_data.append(result.data(), result.size());
+    read += result.size();
+  }
+  ASSERT_TRUE(write_data == read_data);
+  delete seq_file;
+  delete [] scratch;
+}
+    
+    class FilterBlockReader {
+ public:
+ // REQUIRES: 'contents' and *policy must stay live while *this is live.
+  FilterBlockReader(const FilterPolicy* policy, const Slice& contents);
+  bool KeyMayMatch(uint64_t block_offset, const Slice& key);
+    }
+    
+      // Check third filter (empty)
+  ASSERT_TRUE(! reader.KeyMayMatch(4100, 'foo'));
+  ASSERT_TRUE(! reader.KeyMayMatch(4100, 'bar'));
+  ASSERT_TRUE(! reader.KeyMayMatch(4100, 'box'));
+  ASSERT_TRUE(! reader.KeyMayMatch(4100, 'hello'));
+    
+    
+    {  // Pick up remaining bytes
+  switch (limit - data) {
+    case 3:
+      h += static_cast<unsigned char>(data[2]) << 16;
+      FALLTHROUGH_INTENDED;
+    case 2:
+      h += static_cast<unsigned char>(data[1]) << 8;
+      FALLTHROUGH_INTENDED;
+    case 1:
+      h += static_cast<unsigned char>(data[0]);
+      h *= m;
+      h ^= (h >> r);
+      break;
+  }
+  return h;
+}
+    
+    void Histogram::Merge(const Histogram& other) {
+  if (other.min_ < min_) min_ = other.min_;
+  if (other.max_ > max_) max_ = other.max_;
+  num_ += other.num_;
+  sum_ += other.sum_;
+  sum_squares_ += other.sum_squares_;
+  for (int b = 0; b < kNumBuckets; b++) {
+    buckets_[b] += other.buckets_[b];
+  }
+}
+    
+    #include 'port/port.h'
+#include 'port/thread_annotations.h'
+    
+    
+    {}  // namespace leveldb
+    
+    int main(int argc, char** argv) {
+  return leveldb::test::RunAllTests();
+}
 
     
-    #ifndef ATOM_BROWSER_API_ATOM_API_GLOBAL_SHORTCUT_H_
-#define ATOM_BROWSER_API_ATOM_API_GLOBAL_SHORTCUT_H_
+    class TLSConfigPlugin;
     
-    
-    {}  // namespace atom
-    
-    class PreferencesManager : public content::RenderThreadObserver {
- public:
-  PreferencesManager();
-  ~PreferencesManager() override;
+    /// Unlike checkChildProcessStatus, this will block until process exits.
+static bool getProcessExitCode(osquery::PlatformProcess& process,
+                               int& exitCode) {
+  if (!process.isValid()) {
+    return false;
+  }
     }
     
-    // Platform-neutral implementation of a class that keeps track of observers and
-// monitors keystrokes. It relays messages to the appropriate observer when a
-// global shortcut has been struck by the user.
-class GlobalShortcutListener {
- public:
-  class Observer {
-   public:
-    // Called when your global shortcut (|accelerator|) is struck.
-    virtual void OnKeyPressed(const ui::Accelerator& accelerator) = 0;
-  };
+    TEST_F(TablesTests, test_constraint_matching) {
+  struct ConstraintList cl;
+  // An empty constraint list has expectations.
+  EXPECT_FALSE(cl.exists());
+  EXPECT_FALSE(cl.exists(GREATER_THAN));
+  EXPECT_TRUE(cl.notExistsOrMatches('some'));
     }
     
-     protected:
-  /// Calculate the URL once and cache the result.
-  std::string uri_;
     
-      std::string bad_line =
-      R'('2016-03-22T21:17:01.701882+00:00','vagrant-ubuntu-trusty-64','6','cron',)';
-  ec = pub.createEventContext();
-  status = pub.populateEventContext(bad_line, ec);
-  ASSERT_FALSE(status.ok());
-  ASSERT_NE(std::string::npos, status.getMessage().find('fewer'));
+    {TEST_F(SyslogTests, test_csv_separator) {
+  ASSERT_EQ(std::vector<std::string>({'', '', '', '', ''}), splitCsv(',,,,'));
+  ASSERT_EQ(std::vector<std::string>({' ', ' ', ' ', ' ', ' '}),
+            splitCsv(' , , , , '));
+  ASSERT_EQ(std::vector<std::string>({'foo', 'bar', 'baz'}),
+            splitCsv('foo,bar,baz'));
+  ASSERT_EQ(std::vector<std::string>({'foo', 'bar', 'baz'}),
+            splitCsv('\'foo\',\'bar\',\'baz\''));
+  ASSERT_EQ(std::vector<std::string>({',foo,', ',bar', 'baz,'}),
+            splitCsv('\',foo,\',\',bar\',\'baz,\''));
+  ASSERT_EQ(std::vector<std::string>({',f\\oo,', ',ba\\'r', 'baz\\,'}),
+            splitCsv('\',f\\oo,\',\',ba\\'r\',\'baz\\,\''));
+  ASSERT_EQ(std::vector<std::string>({'\',f\\o\'o,', '\',ba\\'r', 'baz\\,\''}),
+            splitCsv('\'\'\',f\\o\'\'o,\',\'\'\',ba\\'r\',\'baz\\,\'\'\''));
+  ASSERT_EQ(std::vector<std::string>({'\',f\\ø\'o,', '\',bá\\'r', 'baz\\,\''}),
+            splitCsv('\'\'\',f\\ø\'\'o,\',\'\'\',bá\\'r\',\'baz\\,\'\'\''));
+}
+}
+
     
-    #include <boost/filesystem.hpp>
+    Status FirehoseLogForwarder::internalSetup() {
+  if (FLAGS_aws_firehose_stream.empty()) {
+    return Status(1,
+                  'Stream name must be specified with --aws_firehose_stream');
+  }
+    }
     
-    FLAG(bool, disable_memory, false, 'Disable physical memory reads');
+    Status KinesisLoggerPlugin::logString(const std::string& s) {
+  return forwarder_->logString(s);
+}
