089f39bd27f52ca7748d86d053e0211dd9091738
SUNsung <weatherius@gmail.com>
2018-08-04 04:19:49 +0300

    Rewrite [14qvlHO6uFaq.14JG5zSsvxydc14.eGaVQxP0v.14NJ4.rb] File in: 2018-08-04 04:19:49

--- ruby/14qvlHO6uFaq.14JG5zSsvxydc14.eGaVQxP0v.14NJ4.rb
+++ ruby/14qvlHO6uFaq.14JG5zSsvxydc14.eGaVQxP0v.14NJ4.rb
@@ -1,29 +1,79 @@
 
         
-            projects
+        end
+
+    
+      def set_permission(permission_name, value)
+    self.send('#{permission_name}=', value)
+    save_and_refresh_staff_groups!
+  end
+    
+      delegate :form_configurable_attributes, to: :class
+  delegate :form_configurable_fields, to: :class
+    
+      included do
+    include Oauthable
+    
+      module SortableTableHelper
+    # :call-seq:
+    #   sortable_column(attribute, default_direction = 'desc', name: attribute.humanize)
+    def sortable_column(attribute, default_direction = nil, options = nil)
+      if options.nil? && (options = Hash.try_convert(default_direction))
+        default_direction = nil
+      end
+      default_direction ||= 'desc'
+      options ||= {}
+      name = options[:name] || attribute.humanize
+      selected = @table_sort_info[:attribute].to_s == attribute
+      if selected
+        direction = @table_sort_info[:direction]
+        new_direction = direction.to_s == 'desc' ? 'asc' : 'desc'
+        classes = 'selected #{direction}'
+      else
+        classes = ''
+        new_direction = default_direction
+      end
+      link_to(name, url_for(sort: '#{attribute}.#{new_direction}'), class: classes)
+    end
   end
 end
 
     
-    describe ApplicationController, type: :controller do
-  controller do
-    include UserTrackingConcern
+        @services = current_user.services.reorder(table_sort).page(params[:page])
     
-    run SinatraStaticServer
-
-    
-      if options.respond_to? 'keys'
-    options.each do |k,v|
-      unless v.nil?
-        v = v.join ',' if v.respond_to? 'join'
-        v = v.to_json if v.respond_to? 'keys'
-        output += ' data-#{k.sub'_','-'}='#{v}''
+        # The path used after confirmation.
+    def after_confirmation_path_for(resource_name, resource)
+      if signed_in?(resource_name)
+        signed_in_root_path(resource)
+      else
+        new_session_path(resource_name)
       end
     end
-  elsif options.respond_to? 'join'
-    output += ' data-value='#{config[key].join(',')}''
-  else
-    output += ' data-value='#{config[key]}''
+    
+          def _devise_route_context
+        @_devise_route_context ||= send(Devise.available_router_name)
+      end
+    end
   end
-  output += '></#{tag}>'
 end
+
+    
+    ```
+#{plugins_string}
+```
+#{markdown_podfile}
+EOS
+      end
+    
+            def initialize(argv)
+          @name = argv.shift_argument
+          @template_url = argv.option('template-url', TEMPLATE_REPO)
+          super
+          @additional_args = argv.remainder!
+        end
+    
+                report.pods_by_error.each do |message, versions_by_name|
+              UI.puts '-> #{message}'.red
+              versions_by_name.each { |name, versions| UI.puts '  - #{name} (#{versions * ', '})' }
+              UI.puts
+            end
