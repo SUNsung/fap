8a9fe4f3ae1f66dc6ab65ee160da42cd96665937
SUNsung <weatherius@gmail.com>
2018-08-04 19:58:28 +0300

    Rewrite [03aiY72AceAY.03fvxUicVuafs031mPPfKkpo8.03Vd0.rb] File in: 2018-08-04 19:58:27

--- ruby/03aiY72AceAY.03fvxUicVuafs031mPPfKkpo8.03Vd0.rb
+++ ruby/03aiY72AceAY.03fvxUicVuafs031mPPfKkpo8.03Vd0.rb
@@ -1,98 +1,30 @@
 
         
-        class Bintray
-  def self.package(formula_name)
-    formula_name.to_s.tr('+', 'x')
-  end
-    
-        keys.each do |key|
-      value = env[key]
-      s = '#{key}: #{value}'
-      case key
-      when 'CC', 'CXX', 'LD'
-        s << ' => #{Pathname.new(value).realpath}' if File.symlink?(value)
-      end
-      f.puts s
-    end
-  end
-end
-
-    
-            if Pathname::BOTTLE_EXTNAME_RX === file.to_s
-          version = bottle_resolve_version(file) rescue file.version
-        else
-          version = file.version
+                def test_url_sub_key_for_sqlite3
+          spec = resolve :production, 'production' => { 'url' => 'sqlite3:foo?encoding=utf8' }
+          assert_equal({
+            'adapter'  => 'sqlite3',
+            'database' => 'foo',
+            'encoding' => 'utf8',
+            'name'     => 'production' }, spec)
         end
-        next unless version
-        next unless (name = file.basename.to_s[/(.*)-(?:#{Regexp.escape(version)})/, 1])
     
-      def python(_options = {}, &block)
-    opoo 'Formula#python is deprecated and will go away shortly.'
-    block.call if block_given?
-    PythonRequirement.new
-  end
-  alias_method :python2, :python
-  alias_method :python3, :python
-end
-
-    
-    puts 'Deduping #{links.size} links...'
-    
-    # puts '\nDone.'
-
-    
-    class Devise::OmniauthCallbacksController < DeviseController
-  prepend_before_action { request.env['devise.skip_timeout'] = true }
-    
-        # Check if a reset_password_token is provided in the request
-    def assert_reset_token_passed
-      if params[:reset_password_token].blank?
-        set_flash_message(:alert, :no_token)
-        redirect_to new_session_path(resource_name)
-      end
-    end
-    
-            routes.each do |module_name, actions|
-          [:path, :url].each do |path_or_url|
-            actions.each do |action|
-              action = action ? '#{action}_' : ''
-              method = :'#{action}#{module_name}_#{path_or_url}'
-    
-        def log_file_info(s)
-      puts '    #{magenta s}'
-    end
-    
-      def test_font_helper_without_suffix
-    assert_match %r(url\(['']?/assets/.*eot['']?\)), @css
-  end
-    
-      path = 'assets/stylesheets'
-  css_path = args.with_defaults(css_path: 'tmp')[:css_path]
-  puts Term::ANSIColor.bold 'Compiling SCSS in #{path}'
-  Dir.mkdir(css_path) unless File.directory?(css_path)
-  %w(_bootstrap bootstrap/_theme).each do |file|
-    save_path = '#{css_path}/#{file.sub(/(^|\/)?_+/, '\1').sub('/', '-')}.css'
-    puts Term::ANSIColor.cyan('  #{save_path}') + '...'
-    engine    = Sass::Engine.for_file('#{path}/#{file}.scss', syntax: :scss, load_paths: [path])
-    css       = engine.render
-    File.open(save_path, 'w') { |f| f.write css }
-  end
-end
-    
-            def yellow
-          @color = :yellow
-        end
+          def inspect
+        Kernel.instance_method(:inspect).bind(self).call
       end
     
-            on roles(target_roles) do
-          unless test '[ -f #{file.to_s.shellescape} ]'
-            info 'Uploading #{prerequisite_file} to #{file}'
-            upload! File.open(prerequisite_file), file
-          end
-        end
-      end
-    end
+      private
+    def subscribe_to_chat_channel(identifier = @chat_identifier)
+      @subscriptions.execute_command 'command' => 'subscribe', 'identifier' => identifier
+      assert_equal identifier, @subscriptions.identifiers.last
     
-      desc 'Finish the deployment, clean up server(s).'
-  task :finishing do
+    class BroadcastingTest < ActionCable::TestCase
+  test 'fetching a broadcaster converts the broadcasting queue to a string' do
+    broadcasting = :test_queue
+    server = TestServer.new
+    broadcaster = server.broadcaster_for(broadcasting)
+    
+      def validate_type
+    errors.add(:type, 'cannot be changed once an instance has been created') if type_changed? && !new_record?
+    errors.add(:type, 'is not a valid type') unless self.class.valid_type?(type)
   end
