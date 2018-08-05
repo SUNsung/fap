d4ba4f52f74a53783f8a8e32911a65fa5d3be769
SUNsung <weatherius@gmail.com>
2018-08-05 10:01:26 +0300

    Rewrite [352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp] File in: 2018-08-05 10:01:25

--- cccccc/352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp
+++ cccccc/352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp
@@ -1,342 +1,167 @@
 
         
-        v8::Handle<v8::Value> CallObjectMethod(int routing_id,
-                                       int object_id,
-                                       const std::string& type,
-                                       const std::string& method,
-                                       v8::Handle<v8::Value> args) {
-  v8::Isolate* isolate = v8::Isolate::GetCurrent();
-  scoped_ptr<V8ValueConverter> converter(V8ValueConverter::create());
+        #endif // SWIFT_INDEX_INDEX_H
+
+    
+    #endif
+
+    
+    
+    {}  // namespace nw
+
+    
+    #endif  // CONTENT_NW_SRC_API_BINDINGS_COMMON_H_
+
+    
+    #endif  // CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
+
+    
+      template<typename T> T* AddListener() {
+    std::map<int, BaseEvent*>::iterator i = listerners_.find(T::id);
+    if (i==listerners_.end()) {
+      T* listener_object = new T(this);
+      listerners_[T::id] = listener_object;
+      return listener_object;
+    }
+    return NULL;
+  }
+    
+    class NwClipboardClearSyncFunction : public NWSyncExtensionFunction {
+ public:
+  NwClipboardClearSyncFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+    bool NwObjCreateFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  base::DictionaryValue* options = nullptr;
+  int id = 0;
+  std::string type;
+  EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(0, &id));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(1, &type));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetDictionary(2, &options));
+    }
+    
+    
+    {  DECLARE_EXTENSION_FUNCTION('nw.Obj.callObjectMethodAsync', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwObjCallObjectMethodAsyncFunction);
+};
+    
+    class UnicharcompressTest : public ::testing::Test {
+ protected:
+  // Loads and compresses the given unicharset.
+  void LoadUnicharset(const string& unicharset_name) {
+    string radical_stroke_file =
+        file::JoinPath(FLAGS_test_srcdir,
+                       'langdata/radical-stroke.txt');
+    string unicharset_file = file::JoinPath(
+        FLAGS_test_srcdir, 'testdata',
+        unicharset_name);
+    string uni_data;
+    CHECK_OK(file::GetContents(unicharset_file, &uni_data, file::Defaults()));
+    string radical_data;
+    CHECK_OK(file::GetContents(radical_stroke_file, &radical_data,
+                               file::Defaults()));
+    CHECK(
+        unicharset_.load_from_inmemory_file(uni_data.data(), uni_data.size()));
+    STRING radical_str(radical_data.c_str());
+    null_char_ =
+        unicharset_.has_special_codes() ? UNICHAR_BROKEN : unicharset_.size();
+    compressed_.ComputeEncoding(unicharset_, null_char_, &radical_str);
+    // Get the encoding of the null char.
+    RecodedCharID code;
+    compressed_.EncodeUnichar(null_char_, &code);
+    encoded_null_char_ = code(0);
+    string output_name = file::JoinPath(
+        FLAGS_test_tmpdir, absl::StrCat(unicharset_name, '.encoding.txt'));
+    STRING encoding = compressed_.GetEncodingAsString(unicharset_);
+    string encoding_str(&encoding[0], encoding.size());
+    CHECK_OK(file::SetContents(output_name, encoding_str, file::Defaults()));
+    LOG(INFO) << 'Wrote encoding to:' << output_name;
+  }
+  // Serializes and de-serializes compressed_ over itself.
+  void SerializeAndUndo() {
+    GenericVector<char> data;
+    TFile wfp;
+    wfp.OpenWrite(&data);
+    EXPECT_TRUE(compressed_.Serialize(&wfp));
+    TFile rfp;
+    rfp.Open(&data[0], data.size());
+    EXPECT_TRUE(compressed_.DeSerialize(&rfp));
+  }
+  // Returns true if the lang is in CJK.
+  bool IsCJKLang(const string& lang) {
+    return lang == 'chi_sim' || lang == 'chi_tra' || lang == 'kor' ||
+           lang == 'jpn';
+  }
+  // Returns true if the lang is Indic.
+  bool IsIndicLang(const string& lang) {
+    return lang == 'asm' || lang == 'ben' || lang == 'bih' || lang == 'hin' ||
+           lang == 'mar' || lang == 'nep' || lang == 'san' || lang == 'bod' ||
+           lang == 'dzo' || lang == 'guj' || lang == 'kan' || lang == 'mal' ||
+           lang == 'ori' || lang == 'pan' || lang == 'sin' || lang == 'tam' ||
+           lang == 'tel';
+  }
+    }
+    
+    
+    {  // Does this paragraph begin with a drop cap?
+  bool has_drop_cap;
+};
+    
+    template <bool del, class P1, class P2, class A1, class A2, class A3, class A4>
+class _TessFunctionResultCallback_2_4<del, void, P1, P2, A1, A2, A3, A4> : public TessCallback4<A1,A2,A3,A4> {
+ public:
+  typedef TessCallback4<A1,A2,A3,A4> base;
+  typedef void (*FunctionSignature)(P1,P2,A1,A2,A3,A4);
     }
     
      private:
-  void SetText(std::string& text);
-  std::string GetText();
-  void Clear();
+  // Helper returns the current UnicharAndFont shape_entry.
+  const UnicharAndFonts* GetShapeEntry() const;
     
-    
-    {}
-    
-      if (!item->enable_shortcut_)
-    return false;
-    
-    
-    {  focus_manager_ = NULL;
-}
-    
-    static KeyMap keymap = {
-  {'`'    , 'Backquote'},
-  {'\\'   , 'Backslash'},
-  {'['    , 'BracketLeft'},
-  {']'    , 'BracketRight'},
-  {','    , 'Comma'},
-  {'='    , 'Equal'},
-  {'-'    , 'Minus'},
-  {'.'    , 'Period'},
-  {'''    , 'Quote'},
-  {';'    , 'Semicolon'},
-  {'/'    , 'Slash'},
-  {'\n'   , 'Enter'},
-  {'\t'   , 'Tab'},
-  {'UP'   , 'ArrowUp'},
-  {'DOWN' , 'ArrowDown'},
-  {'LEFT' , 'ArrowLeft'},
-  {'RIGHT', 'ArrowRight'},
-  {'ESC'  , 'Escape'},
-  {'MEDIANEXTTRACK', 'MediaTrackNext'},
-  {'MEDIAPREVTRACK', 'MediaTrackPrevious'}
-};
-    
-    
-    {  nw::ObjectManager* manager = nw::ObjectManager::Get(browser_context());
-  manager->OnAllocateObject(id, type, *options, extension_id());
-  return true;
-}
-    
-      // Read until size drops significantly.
-  std::string limit_key = Key(n);
-  for (int read = 0; true; read++) {
-    ASSERT_LT(read, 100) << 'Taking too long to compact';
-    Iterator* iter = db_->NewIterator(ReadOptions());
-    for (iter->SeekToFirst();
-         iter->Valid() && iter->key().ToString() < limit_key;
-         iter->Next()) {
-      // Drop data
-    }
-    delete iter;
-    // Wait a little bit to allow any triggered compactions to complete.
-    Env::Default()->SleepForMicroseconds(1000000);
-    uint64_t size = Size(Key(0), Key(n));
-    fprintf(stderr, 'iter %3d => %7.3f MB [other %7.3f MB]\n',
-            read+1, size/1048576.0, Size(Key(n), Key(kCount))/1048576.0);
-    if (size <= initial_size/10) {
-      break;
-    }
+      // Returns a mutable pointer to the sample with the given index.
+  TrainingSample* mutable_sample(int index) {
+    return samples_[index];
+  }
+  // Gets ownership of the sample with the given index, removing it from this.
+  TrainingSample* extract_sample(int index) {
+    TrainingSample* sample = samples_[index];
+    samples_[index] = nullptr;
+    return sample;
   }
     
-    // Return the name of the current file.  This file contains the name
-// of the current manifest file.  The result will be prefixed with
-// 'dbname'.
-extern std::string CurrentFileName(const std::string& dbname);
-    
-    // Tag numbers for serialized VersionEdit.  These numbers are written to
-// disk and should not be changed.
-enum Tag {
-  kComparator           = 1,
-  kLogNumber            = 2,
-  kNextFileNumber       = 3,
-  kLastSequence         = 4,
-  kCompactPointer       = 5,
-  kDeletedFile          = 6,
-  kNewFile              = 7,
-  // 8 was used for large value refs
-  kPrevLogNumber        = 9
-};
-    
-    
-    {  FileMetaData() : refs(0), allowed_seeks(1 << 30), file_size(0) { }
-};
-    
-    #endif  // CAFFE_LAYER_FACTORY_H_
-
-    
-     protected:
-  /// @copydoc AbsValLayer
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-    #include <vector>
-    
-    namespace caffe {
-    }
-    
-      /**
-   * @brief Computes the error gradient w.r.t. the forwarded inputs.
-   *
-   * @param top output Blob vector (length 1+), providing the error gradient with
-   *        respect to the outputs
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2+), into which the top error
-   *        gradient is copied
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    #ifndef incl_HPHP_WORKLOAD_STATS_H_
-#define incl_HPHP_WORKLOAD_STATS_H_
-    
-    private:
-  std::atomic<int> m_refCount;
-  int m_timeoutSeconds;
-    
-    struct RepoAuthType;
-struct StringData;
-    
-    ///////////////////////////////////////////////////////////////////////////////
-    
-    
-    {    m_base = m_frontier = start;
-    m_destBase = dest;
-    m_size = sz;
-    m_maxGrow = maxGrow;
-    m_name = name;
-  }
-    
-    Type baseLocNameType(const Base& b) {
-  return b.locName ? sval(b.locName) : TInitGen;
-}
-    
-      if (src.unreachable) {
-    // If we're coming from unreachable code and the dst is already
-    // initialized, it doesn't change the dst (whether it is reachable or not).
-    return false;
-  }
-  if (dst.unreachable) {
-    // If we're going to code currently believed to be unreachable, take the
-    // src state, and consider the dest state changed only if the source state
-    // was reachable.
-    dst = src;
-    return !src.unreachable;
-  }
-    
-      bool isCompact() const {
-    return
-      RuntimeOption::EvalEnableCompactBacktrace &&
-      !m_skipTop &&
-      !m_skipInlined &&
-      !m_withSelf &&
-      !m_withThis &&
-      !m_withMetadata &&
-      !m_withPseudoMain &&
-      (!RuntimeOption::EnableArgsInBacktraces || !m_withArgValues) &&
-      !m_withArgNames &&
-      !m_limit &&
-      !m_parserFrame &&
-      !m_fromWaitHandle;
-  }
-    
-    void addRootNode(HeapGraph& g, const PtrMap<const HeapObject*>& blocks,
-                 type_scan::Scanner& scanner,
-                 const void* h, size_t size, type_scan::Index ty) {
-  auto from = g.nodes.size();
-  g.nodes.push_back(
-    HeapGraph::Node{h, size, true, ty, -1, -1}
-  );
-  g.root_nodes.push_back(from);
-  scanner.scanByIndex(ty, h, size);
-  scanner.finish(
-    [&](const void* p, std::size_t size) {
-      conservativeScan(p, size, [&](const void** addr, const void* ptr) {
-        if (auto r = blocks.region(ptr)) {
-          auto to = blocks.index(r);
-          auto offset = uintptr_t(addr) - uintptr_t(h);
-          auto e = addPtr(g, from, to, HeapGraph::Ambiguous, offset);
-          g.root_ptrs.push_back(e);
-        }
-      });
-    },
-    [&](const void** addr) {
-      if (auto r = blocks.region(*addr)) {
-        auto to = blocks.index(r);
-        auto offset = uintptr_t(addr) - uintptr_t(h);
-        auto e = addPtr(g, from, to, HeapGraph::Counted, offset);
-        g.root_ptrs.push_back(e);
-      }
-    },
-    [&](const void* p) {
-      auto weak = static_cast<const WeakRefDataHandle*>(p);
-      auto addr = &(weak->wr_data->pointee.m_data.pobj);
-      if (auto r = blocks.region(*addr)) {
-        auto to = blocks.index(r);
-        // Note that offset is going to be meaningless because weak->wr_data is
-        // a shared_ptr, so &pointee.m_data.pobj will be inside the shared_ptr's
-        // internal node, allocated separately.
-        addPtr(g, from, to, HeapGraph::Weak, 0);
-      }
-    }
-  );
-}
-    
-      static size_t curl_write(char *data, size_t size, size_t nmemb, void *ctx);
-  static size_t curl_header(char *data, size_t size, size_t nmemb, void *ctx);
-    
-    /*
- * Initialize PCRE cache.
- */
-void pcre_init();
-    
-    void DataIter::InitRcppModule() {
-  using namespace Rcpp;  // NOLINT(*)
-  class_<DataIter>('MXDataIter')
-      .method('iter.next', &DataIter::Next)
-      .method('reset', &DataIter::Reset)
-      .method('value', &DataIter::Value)
-      .method('num.pad', &DataIter::NumPad);
-    }
-    
-    /*!
- * \brief MXNet's internal data iterator.
- */
-class MXDataIter : public DataIter {
+    // The CCNonTextDetect class contains grid-based operations on blobs to create
+// a full-resolution image mask analogous yet complementary to
+// pixGenHalftoneMask as it is better at line-drawings, graphs and charts.
+class CCNonTextDetect : public BlobGrid {
  public:
-  /*! \return typename from R side. */
-  inline static const char* TypeName() {
-    return 'MXNativeDataIter';
-  }
-  // implement the interface
-  virtual void Reset();
-  virtual bool Next();
-  virtual int NumPad() const;
-  virtual Rcpp::List Value() const;
-  virtual ~MXDataIter() {
-    MX_CALL(MXDataIterFree(handle_));
-  }
+  CCNonTextDetect(int gridsize, const ICOORD& bleft, const ICOORD& tright);
+  virtual ~CCNonTextDetect();
     }
     
-    #include <mshadow/tensor.h>
-#include <nnvm/op_attr_types.h>
-    
-    static inline bool BilinearSampleOpStorageType(const nnvm::NodeAttrs &attrs,
-                                               const int dev_mask,
-                                               DispatchMode *dispatch_mode,
-                                               std::vector<int> *in_attrs,
-                                               std::vector<int> *out_attrs) {
-  CHECK_EQ(in_attrs->size(), 1);
-  CHECK_EQ(out_attrs->size(), 1);
-  *dispatch_mode = DispatchMode::kFCompute;
-  for (int& v : *in_attrs) {
-    if (v == - 1) v = kDefaultStorage;
-  }
-  for (size_t i = 0; i < out_attrs->size(); i++) {
-    (*out_attrs)[i] = kDefaultStorage;
-  }
-  return true;
-}
-    
-    #include <cstdint>
-#include <iosfwd>
-#include <typeinfo>
-#include <vector>
-    
-    [[noreturn]] void singletonWarnLeakyDoubleRegistrationAndAbort(
-    const TypeDescriptor& type);
-    
-      bool pop(T& val) {
-    hazptr_local<1, Atom> h;
-    hazptr_holder<Atom>& hptr = h[0];
-    Node* node;
-    while (true) {
-      node = hptr.get_protected(head_);
-      if (node == nullptr) {
-        return false;
-      }
-      auto next = node->next();
-      if (cas_head(node, next)) {
-        break;
-      }
-    }
-    hptr.reset();
-    val = node->value();
-    node->retire();
-    return true;
-  }
-    
-    #endif
-    
-    FOLLY_ALWAYS_INLINE int __builtin_clzll(unsigned long long x) {
-  unsigned long index;
-  return int(_BitScanReverse64(&index, x) ? 63 - index : 64);
-}
-    
-    
-    { private:
-  detail::BufferedSlidingWindow<TDigest, ClockT> bufferedSlidingWindow_;
-};
-    
-    #if __cpp_coroutines >= 201703L && FOLLY_HAS_INCLUDE(<experimental/coroutine>)
-#define FOLLY_HAS_COROUTINES 1
-#elif _MSC_VER && _RESUMABLE_FUNCTIONS_SUPPORTED
-#define FOLLY_HAS_COROUTINES 1
-#endif
-    
-    ///////////////////////////////////
-#endif // FOLLY_F14_VECTOR_INTRINSICS_AVAILABLE
-///////////////////////////////////
-
-    
-    #endif /* BASEEVENT_INTERFACE_BASEEVENT_H_ */
-
-    
-    
-/*
- * DumpCrashStack.h
- *
- *  Created on: 2012-9-28
- *      Author: yerungui
- */
-    
-            std::map<const std::string, Spy*>::iterator it;
+      // Hoovers up all un-owned blobs and deletes them.
+  // The rest get released from the block so the ColPartitions can pass
+  // ownership to the output blocks.
+  void ReleaseBlobsAndCleanupUnused(TO_BLOCK* block);
+  // Splits partitions that cross columns where they have nothing in the gap.
+  void GridSplitPartitions();
+  // Merges partitions where there is vertical overlap, within a single column,
+  // and the horizontal gap is small enough.
+  void GridMergePartitions();
+  // Inserts remaining noise blobs into the most applicable partition if any.
+  // If there is no applicable partition, then the blobs are deleted.
+  void InsertRemainingNoise(TO_BLOCK* block);
+  // Remove partitions that come from horizontal lines that look like
+  // underlines, but are not part of a table.
+  void GridRemoveUnderlinePartitions();
+  // Add horizontal line separators as partitions.
+  void GridInsertHLinePartitions();
+  // Add vertical line separators as partitions.
+  void GridInsertVLinePartitions();
+  // For every ColPartition in the grid, sets its type based on position
+  // in the columns.
+  void SetPartitionTypes();
+  // Only images remain with multiple types in a run of partners.
+  // Sets the type of all in the group to the maximum of the group.
+  void SmoothPartnerRuns();
