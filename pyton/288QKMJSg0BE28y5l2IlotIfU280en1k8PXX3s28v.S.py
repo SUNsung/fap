45f3fcfa235c16d4e2edcad1169534b06e47e2fc
SUNsung <weatherius@gmail.com>
2018-08-05 19:31:47 +0300

    Rewrite [288QKMJSg0BE28y5l2IlotIfU280en1k8PXX3s28v.S.py] File in: 2018-08-05 19:31:47

--- pyton/288QKMJSg0BE28y5l2IlotIfU280en1k8PXX3s28v.S.py
+++ pyton/288QKMJSg0BE28y5l2IlotIfU280en1k8PXX3s28v.S.py
@@ -1,66 +1,172 @@
 
         
-        containers = (('thefuck/python3-fish',
-               u'''FROM python:3
-                   # Use jessie-backports since it has the fish package. See here for details:
-                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
-                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
-                   RUN apt-get update
-                   RUN apt-get install -yy fish''',
-               u'fish'),
-              ('thefuck/python2-fish',
-               u'''FROM python:2
-                   # Use jessie-backports since it has the fish package. See here for details:
-                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
-                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
-                   RUN apt-get update
-                   RUN apt-get install -yy fish''',
-               u'fish'))
+        cc_test(
+    name = 'mst_solver_random_comparison_test',
+    size = 'small',
+    timeout = 'long',
+    srcs = ['mst_solver_random_comparison_test.cc'],
+    tags = [
+        'manual',  # exclude from :all, since this is expensive
+    ],
+    deps = [
+        ':mst_solver',
+        ':spanning_tree_iterator',
+        '//syntaxnet:base',
+        '//syntaxnet:test_main',
+        '@org_tensorflow//tensorflow/core:lib',
+        '@org_tensorflow//tensorflow/core:test',
+    ],
+)
+    
+    tf_proto_library_cc(
+    name = 'trace_proto',
+    srcs = ['trace.proto'],
+    protodeps = [':data_proto'],
+)
+    
+    py_library(
+    name = 'dragnn_model_saver_lib',
+    srcs = ['dragnn_model_saver_lib.py'],
+    deps = [
+        ':dragnn_ops',
+        ':graph_builder',
+        ':load_dragnn_cc_impl_py',
+        ':network_units',
+        '//dragnn/protos:spec_pb2_py',
+        '//syntaxnet:load_parser_ops_py',
+        '//syntaxnet:sentence_pb2_py',
+        '@org_tensorflow//tensorflow:tensorflow_py',
+        '@org_tensorflow//tensorflow/core:protos_all_py',
+    ],
+)
     
     
-@pytest.mark.functional
-def test_refuse_with_confirmation(proc, TIMEOUT):
-    refuse_with_confirmation(proc, TIMEOUT)
+      weights = tf.constant([[[ 2,  3],
+                              [ 5,  7]],
+                             [[11, 13],
+                              [17, 19]],
+                             [[23, 29],
+                              [31, 37]]], tf.float32)  # pyformat: disable
+    
+      Args:
+    mst_op: The MaximumSpanningTree op being differentiated.
+    d_loss_d_max_scores: [B] vector where entry b is the gradient of the network
+                         loss w.r.t. entry b of the |max_scores| output of the
+                         |mst_op|.
+    *_: The gradients w.r.t. the other outputs; ignored.
     
     
-class EnvironmentConfig(object):
-    uri = os.getenv('LANDSCAPE_API_URI')
-    access_key = os.getenv('LANDSCAPE_API_KEY')
-    secret_key = os.getenv('LANDSCAPE_API_SECRET')
-    ssl_ca_file = os.getenv('LANDSCAPE_API_SSL_CA_FILE')
-    
-                if key in mapping:
-                display.warning(u'While constructing a mapping from {1}, line {2}, column {3}, found a duplicate dict key ({0}).'
-                                u' Using last defined value only.'.format(key, *mapping.ansible_pos))
-    
-    
-class TestData(unittest.TestCase):
-    
-        def stop(self):
-        print('then stop')
-    
-        def test_frozen_pool(self):
-        with ObjectPool(self.sample_queue) as pool:
-            self.assertEqual(pool, 'first')
-            self.assertEqual(pool, 'first')
-        self.assertTrue(self.sample_queue.get() == 'second')
-        self.assertFalse(self.sample_queue.empty())
-        self.assertTrue(self.sample_queue.get() == 'first')
-        self.assertTrue(self.sample_queue.empty())
+def barplot_neighbors(Nrange=2 ** np.arange(1, 11),
+                      Drange=2 ** np.arange(7),
+                      krange=2 ** np.arange(10),
+                      N=1000,
+                      D=64,
+                      k=5,
+                      leaf_size=30,
+                      dataset='digits'):
+    algorithms = ('kd_tree', 'brute', 'ball_tree')
+    fiducial_values = {'N': N,
+                       'D': D,
+                       'k': k}
     
     
-class TestRunnerFacilities(unittest.TestCase):
+def rbf_kernels(X, n_jobs):
+    return pairwise_kernels(X, metric='rbf', n_jobs=n_jobs, gamma=0.1)
     
-    class MidnightTimeProvider(object):
+        op.add_option('--density',
+                  dest='density', default=1 / 3,
+                  help='Density used by the sparse random projection.'
+                       ' ('auto' or float (0.0, 1.0]')
+    
+        ###########################################################################
+    # Set Python core input
+    sampling_algorithm['python-core-sample'] = \
+        lambda n_population, n_sample: \
+            random.sample(xrange(n_population), n_sample)
+    
+    
+if __name__ == '__main__':
+    # NOTE: we put the following in a 'if __name__ == '__main__'' protected
+    # block to be able to use a multi-core grid search that also works under
+    # Windows, see: http://docs.python.org/library/multiprocessing.html#windows
+    # The multiprocessing module is used as the backend of joblib.Parallel
+    # that is used when n_jobs != 1 in GridSearchCV
+    
+    
+def generate_data(n_samples, n_features):
+    '''Generate random blob-ish data with noisy features.
+    
     '''
-    Class implemented as hard-coded stub (in contrast to configurable stub).
-    '''
+print(__doc__)
     
-        def __init__(self):
-        self._tm = None
+    This dataset is made up of 1797 8x8 images. Each image,
+like the one shown below, is of a hand-written digit.
+In order to utilize an 8x8 figure like this, we'd have to
+first transform it into a feature vector with length 64.
     
-    *TL;DR80
-Provides the ability to restore an object to its previous state.
+    __author__ = 'Brian Quinlan (brian@sweetapp.com)'
+    
+    def download_urls_with_executor(urls, executor, timeout=60):
+    try:
+        url_to_content = {}
+        future_to_url = dict((executor.submit(load_url, url, timeout), url)
+                             for url in urls)
+    
+    # Output file base name for HTML help builder.
+htmlhelp_basename = 'futuresdoc'
+    
+    
+class CommandRequest( BaseRequest ):
+  def __init__( self, arguments, completer_target = None, extra_data = None ):
+    super( CommandRequest, self ).__init__()
+    self._arguments = _EnsureBackwardsCompatibility( arguments )
+    self._command = arguments and arguments[ 0 ]
+    self._completer_target = ( completer_target if completer_target
+                               else 'filetype_default' )
+    self._extra_data = extra_data
+    self._response = None
+    
+    
+  def IsAllowed( self, diagnostic ):
+    # NOTE: in this class's implementation, we ask vimsupport for
+    #  the current filetypes and delegate automatically; it is probably,
+    #  more efficient, however, to call SubsetForTypes() and reuse
+    #  the returned DiagnosticFilter if it will be checked repeatedly.
+    filetypes = vimsupport.CurrentFiletypes()
+    return self.SubsetForTypes( filetypes ).IsAllowed( diagnostic )
+    
+    
+  def Response( self ):
+    return {
+      'completions': self._results,
+      'completion_start_column': self.request_data[ 'start_column' ]
+    }
+    
+    image = face_recognition.load_image_file('{}')
 '''
     
-    from abc import abstractmethod
+        # You can access the actual face itself like this:
+    face_image = image[top:bottom, left:right]
+    pil_image = Image.fromarray(face_image)
+    pil_image.show()
+
+    
+    # Load a second sample picture and learn how to recognize it.
+biden_image = face_recognition.load_image_file('biden.jpg')
+biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+    
+    requirements = [
+    'face_recognition_models>=0.3.0',
+    'Click>=6.0',
+    'dlib>=19.7',
+    'numpy',
+    'Pillow'
+]
+    
+    # Load the jpg file into a numpy array
+image = face_recognition.load_image_file('biden.jpg')
+    
+    
+def _rect_to_css(rect):
+    '''
+    Convert a dlib 'rect' object to a plain tuple in (top, right, bottom, left) order
