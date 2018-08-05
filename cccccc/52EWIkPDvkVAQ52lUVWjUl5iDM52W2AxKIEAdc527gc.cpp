f7218bd2a4e09214bd19af4eee86ddc3f27686ce
SUNsung <weatherius@gmail.com>
2018-08-05 05:53:19 +0300

    Rewrite [52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp] File in: 2018-08-05 05:53:18

--- cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
+++ cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
@@ -1,163 +1,328 @@
 
         
-        
-    {}  // namespace atom
+        #include 'tensorflow/core/distributed_runtime/worker_cache.h'
+#include 'tensorflow/core/platform/mutex.h'
+#include 'tensorflow/core/platform/thread_annotations.h'
+#include 'tensorflow/core/platform/types.h'
+#include 'tensorflow/core/protobuf/worker.pb.h'
     
-    #include 'base/macros.h'
-#include 'v8/include/v8.h'
-    
-    // Wrapper used to keep track of the lifetime of a WebContents.
-// Lives on the UI thread.
-class PrintingUIWebContentsObserver : public content::WebContentsObserver {
- public:
-  explicit PrintingUIWebContentsObserver(content::WebContents* web_contents);
-    }
+    #endif  // GOOGLE_CUDA
+
     
     
-    {  /// Returns true if \p BB is a dead-end block.
-  bool isDeadEnd(SILBasicBlock *BB) {
-    if (!isComputed) {
-      // Lazily compute the dataflow.
-      compute();
-      isComputed = true;
-    }
-    return ReachableBlocks.count(BB) == 0;
-  }
-};
-    
-    namespace swift {
-    }
-    
-    // On Cygwin, std::once_flag can not be used because it is larger than the
-// platform word.
-typedef uintptr_t swift_once_t;
-#else
-    
-    #include <google/protobuf/descriptor_database.h>
-    
-    // TODO(jonskeet): Refactor repeated field support; all the implementations are *really* similar. We
-// should probably have a RepeatedFieldGeneratorBase.
-class RepeatedEnumFieldGenerator : public FieldGeneratorBase {
- public:
-  RepeatedEnumFieldGenerator(const FieldDescriptor* descriptor,
-                             int fieldOrdinal,
-                             const Options *options);
-  ~RepeatedEnumFieldGenerator();
-    }
-    
-    // Exposed for testing only.
-LIBPROTOC_EXPORT string EscapeJavadoc(const string& input);
-    
-    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
-//   can only really compare the output against a golden value, which is a
-//   fairly tedious and fragile testing strategy.  If we want to go that route,
-//   it probably makes sense to bite the bullet and write a test that compares
-//   the whole generated output for unittest.proto against a golden value, with
-//   a very simple script that can be run to regenerate it with the latest code.
-//   This would mean that updates to the golden file would have to be included
-//   in any change to the code generator, which would actually be fairly useful
-//   as it allows the reviewer to see clearly how the generated code is
-//   changing.
-    
-    #include <google/protobuf/compiler/java/java_context.h>
-#include <google/protobuf/compiler/java/java_enum_field.h>
-#include <google/protobuf/compiler/java/java_extension.h>
-#include <google/protobuf/compiler/java/java_extension_lite.h>
-#include <google/protobuf/compiler/java/java_field.h>
-#include <google/protobuf/compiler/java/java_helpers.h>
-#include <google/protobuf/compiler/java/java_message.h>
-#include <google/protobuf/compiler/java/java_message_lite.h>
-#include <google/protobuf/compiler/java/java_service.h>
-    
-      virtual ExtensionGenerator* NewExtensionGenerator(
-      const FieldDescriptor* descriptor) const;
-    
-    #ifdef WIN32
-/// Suppress a C4244 warning in gtest-printers.h (double -> BiggestInt
-/// conversion)
-#pragma warning(push, 3)
-#pragma warning(disable : 4244)
+    {#if GOOGLE_CUDA
+// A special GPU kernel for int32.
+// TODO(b/25387198): Also enable int32 in device memory. This kernel
+// registration requires all int32 inputs and outputs to be in host memory.
+REGISTER_KERNEL_BUILDER(Name('Mod')
+                            .Device(DEVICE_GPU)
+                            .HostMemory('x')
+                            .HostMemory('y')
+                            .HostMemory('z')
+                            .TypeConstraint<int32>('T'),
+                        BinaryOp<CPUDevice, functor::safe_mod<int32>>);
+REGISTER_KERNEL_BUILDER(Name('TruncateMod')
+                            .Device(DEVICE_GPU)
+                            .HostMemory('x')
+                            .HostMemory('y')
+                            .HostMemory('z')
+                            .TypeConstraint<int32>('T'),
+                        BinaryOp<CPUDevice, functor::safe_mod<int32>>);
 #endif
-    
-        auto dropper = DropPrivileges::get();
-    EXPECT_TRUE(dropper->dropToParent(perm_path_ + 'ro'));
-    EXPECT_TRUE(dropper->dropped_);
-    EXPECT_EQ(dropper->to_user_, nobody->pw_uid);
-    
-    #include <osquery/core.h>
-#include <osquery/system.h>
-    
-    bool js_cocos2dx_studio_MovementData_constructor(JSContext *cx, uint32_t argc, jsval *vp);
-void js_cocos2dx_studio_MovementData_finalize(JSContext *cx, JSObject *obj);
-void js_register_cocos2dx_studio_MovementData(JSContext *cx, JS::HandleObject global);
-void register_all_cocos2dx_studio(JSContext* cx, JS::HandleObject obj);
-bool js_cocos2dx_studio_MovementData_getMovementBoneData(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_MovementData_addMovementBoneData(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_MovementData_create(JSContext *cx, uint32_t argc, jsval *vp);
-bool js_cocos2dx_studio_MovementData_MovementData(JSContext *cx, uint32_t argc, jsval *vp);
+}  // namespace tensorflow
+
     
     
-    
-    
-    
-    
-    
-    		case 's':
-			m_platform->SetType(b2_staticBody);
-			break;
-    
-    class AbstractDiskWriter : public DiskWriter {
-private:
-  std::string filename_;
-    }
-    
-    void AbstractHttpServerResponseCommand::updateReadWriteCheck()
-{
-  if (httpServer_->wantRead()) {
-    if (!readCheck_) {
-      readCheck_ = true;
-      e_->addSocketForReadCheck(socket_, this);
-    }
-  }
-  else if (readCheck_) {
-    readCheck_ = false;
-    e_->deleteSocketForReadCheck(socket_, this);
-  }
-  if (httpServer_->wantWrite()) {
-    if (!writeCheck_) {
-      writeCheck_ = true;
-      e_->addSocketForWriteCheck(socket_, this);
-    }
-  }
-  else if (writeCheck_) {
-    writeCheck_ = false;
-    e_->deleteSocketForWriteCheck(socket_, this);
-  }
+    {  return kTfLiteOk;
 }
+    
+      Status DecorateGraph(Graph* graph, Device* device) override;
+  Status PublishGraph(const Graph& graph, const string& device_name) override;
+    
+    // Prefetching support
+//
+// Defined behavior on some of the uarchs:
+// PREFETCH_HINT_T0:
+//   prefetch to all levels of the hierarchy (except on p4: prefetch to L2)
+// PREFETCH_HINT_NTA:
+//   p4: fetch to L2, but limit to 1 way (out of the 8 ways)
+//   core: skip L2, go directly to L1
+//   k8 rev E and later: skip L2, can go to either of the 2-ways in L1
+enum PrefetchHint {
+  PREFETCH_HINT_T0 = 3,  // More temporal locality
+  PREFETCH_HINT_T1 = 2,
+  PREFETCH_HINT_T2 = 1,  // Less temporal locality
+  PREFETCH_HINT_NTA = 0  // No temporal locality
+};
+template <PrefetchHint hint>
+void prefetch(const void* x);
+    
+    
+    {}  // namespace tensorflow
+#endif  // TENSORFLOW_USE_SYCL
+
+    
+    REGISTER_OP('EncodeAudio')
+    .Input('sampled_audio: float')
+    .Output('contents: string')
+    .Attr('file_format: string')
+    .Attr('samples_per_second: int')
+    .Attr('bits_per_second: int = 192000')
+    .SetShapeFn(shape_inference::ScalarShape)
+    .Doc(R'doc(
+Processes a `Tensor` containing sampled audio with the number of channels
+and length of the audio specified by the dimensions of the `Tensor`. The
+audio is converted into a string that, when saved to disk, will be equivalent
+to the audio in the specified audio format.
+    
+    
+    {    return state;
+}
+    
+    #endif
+
+    
+    void CV_ModelEstimator2_Test::get_test_array_types_and_sizes( int /*test_case_idx*/,
+                                                              vector<vector<Size> > &sizes, vector<vector<int> > &types )
+{
+    RNG &rng = ts->get_rng();
+    checkPartialSubsets = (cvtest::randInt(rng) % 2 == 0);
+    }
+    
+    CV_EXPORTS_W void max(InputArray src1, Scalar src2, OutputArray dst);
+    
+    #undef cv_hal_LU32f
+#define cv_hal_LU32f lapack_LU32f
+#undef cv_hal_LU64f
+#define cv_hal_LU64f lapack_LU64f
+    
+      static Slice Contents(const WriteBatch* batch) {
+    return Slice(batch->rep_);
+  }
+    
+      // Check for expected size.
+  ASSERT_OK(env_->GetFileSize('/dir/f', &file_size));
+  ASSERT_EQ(8, file_size);
+    
+      // create first key range
+  leveldb::WriteBatch batch;
+  for (size_t i = 0; i < kNumKeys; i++) {
+    batch.Put(Key1(i), 'value for range 1 key');
+  }
+  ASSERT_OK(db->Write(leveldb::WriteOptions(), &batch));
+    
+    // A FilterBlockBuilder is used to construct all of the filters for a
+// particular Table.  It generates a single string which is stored as
+// a special block in the Table.
+//
+// The sequence of calls to FilterBlockBuilder must match the regexp:
+//      (StartBlock AddKey*)* Finish
+class FilterBlockBuilder {
+ public:
+  explicit FilterBlockBuilder(const FilterPolicy*);
+    }
+    
+    std::string Histogram::ToString() const {
+  std::string r;
+  char buf[200];
+  snprintf(buf, sizeof(buf),
+           'Count: %.0f  Average: %.4f  StdDev: %.2f\n',
+           num_, Average(), StandardDeviation());
+  r.append(buf);
+  snprintf(buf, sizeof(buf),
+           'Min: %.4f  Median: %.4f  Max: %.4f\n',
+           (num_ == 0.0 ? 0.0 : min_), Median(), max_);
+  r.append(buf);
+  r.append('------------------------------------------------------\n');
+  const double mult = 100.0 / num_;
+  double sum = 0;
+  for (int b = 0; b < kNumBuckets; b++) {
+    if (buckets_[b] <= 0.0) continue;
+    sum += buckets_[b];
+    snprintf(buf, sizeof(buf),
+             '[ %7.0f, %7.0f ) %7.0f %7.3f%% %7.3f%% ',
+             ((b == 0) ? 0.0 : kBucketLimit[b-1]),      // left
+             kBucketLimit[b],                           // right
+             buckets_[b],                               // count
+             mult * buckets_[b],                        // percentage
+             mult * sum);                               // cumulative percentage
+    r.append(buf);
+    }
+    }
+    
+    namespace leveldb {
+    }
+    
+    // A very simple random number generator.  Not especially good at
+// generating truly random bits, but good enough for our needs in this
+// package.
+class Random {
+ private:
+  uint32_t seed_;
+ public:
+  explicit Random(uint32_t s) : seed_(s & 0x7fffffffu) {
+    // Avoid bad seeds.
+    if (seed_ == 0 || seed_ == 2147483647L) {
+      seed_ = 1;
+    }
+  }
+  uint32_t Next() {
+    static const uint32_t M = 2147483647L;   // 2^31-1
+    static const uint64_t A = 16807;  // bits 14, 8, 7, 5, 2, 1, 0
+    // We are computing
+    //       seed_ = (seed_ * A) % M,    where M = 2^31-1
+    //
+    // seed_ must not be zero or M, or else all subsequent computed values
+    // will be zero or M respectively.  For all other values, seed_ will end
+    // up cycling through every number in [1,M-1]
+    uint64_t product = seed_ * A;
+    }
+    }
+    
+      inline void ClearSavedValue() {
+    if (saved_value_.capacity() > 1048576) {
+      std::string empty;
+      swap(empty, saved_value_);
+    } else {
+      saved_value_.clear();
+    }
+  }
+    
+    LookupKey::LookupKey(const Slice& user_key, SequenceNumber s) {
+  size_t usize = user_key.size();
+  size_t needed = usize + 13;  // A conservative estimate
+  char* dst;
+  if (needed <= sizeof(space_)) {
+    dst = space_;
+  } else {
+    dst = new char[needed];
+  }
+  start_ = dst;
+  dst = EncodeVarint32(dst, usize + 8);
+  kstart_ = dst;
+  memcpy(dst, user_key.data(), usize);
+  dst += usize;
+  EncodeFixed64(dst, PackSequenceAndType(s, kValueTypeForSeek));
+  dst += 8;
+  end_ = dst;
+}
+    
+        RecordType type;
+    const bool end = (left == fragment_length);
+    if (begin && end) {
+      type = kFullType;
+    } else if (begin) {
+      type = kFirstType;
+    } else if (end) {
+      type = kLastType;
+    } else {
+      type = kMiddleType;
+    }
+    
+    extern JSClass  *jsb_cocos2d_Physics3DPointToPointConstraint_class;
+extern JSObject *jsb_cocos2d_Physics3DPointToPointConstraint_prototype;
+    
+    
+    
+    
+    
+    int register_all_cocos2dx_experimental_video(lua_State* tolua_S);
+    
+    
+    
+    
+    
+    // This class implements debug drawing callbacks that are invoked
+// inside b2World::Step.
+class GLESDebugDraw : public b2Draw
+{
+    float32 mRatio;
+    cocos2d::GLProgram* mShaderProgram;
+    GLint        mColorLocation;
+    }
+    
+    
+    {
+    {			float minX = -6.0f;
+			float maxX = 0.0f;
+			float minY = 4.0f;
+			float maxY = 6.0f;
+			
+			for (int32 i = 0; i < 400; ++i)
+			{
+				b2BodyDef bd;
+				bd.type = b2_dynamicBody;
+				bd.position = b2Vec2(RandomFloat(minX,maxX),RandomFloat(minY,maxY));
+				b2Body* body = m_world->CreateBody(&bd);
+				body->CreateFixture(&shape, 0.01f);
+			}
+		}
+		
+		{
+			b2PolygonShape shape;
+			shape.SetAsBox(1.5f, 1.5f);
+			b2BodyDef bd;
+			bd.type = b2_dynamicBody;
+			bd.position.Set(-40.0f,5.0f);
+			bd.bullet = true;
+			b2Body* body = m_world->CreateBody(&bd);
+			body->CreateFixture(&shape, 1.0f);
+			body->SetLinearVelocity(b2Vec2(150.0f, 0.0f));
+		}
+	}
+    
+      void setDefaultCred(std::string user, std::string password);
+    
+      virtual void onCancelSendingPieceEvent(const BtCancelSendingPieceEvent& event)
+      CXX11_OVERRIDE
+  {
+  }
+    
+    AbstractHttpServerResponseCommand::AbstractHttpServerResponseCommand(
+    cuid_t cuid, const std::shared_ptr<HttpServer>& httpServer,
+    DownloadEngine* e, const std::shared_ptr<SocketCore>& socket)
+    : Command(cuid),
+      e_(e),
+      socket_(socket),
+      httpServer_(httpServer),
+      readCheck_(false),
+      writeCheck_(true)
+{
+  setStatus(Command::STATUS_ONESHOT_REALTIME);
+  e_->addSocketForWriteCheck(socket_, this);
+}
+    
+      virtual bool getChangeOption() const CXX11_OVERRIDE;
     
     namespace aria2 {
     }
     
-    class ActivePeerConnectionCommand : public Command {
-private:
-  RequestGroup* requestGroup_;
-  std::shared_ptr<BtRuntime> btRuntime_;
-  std::shared_ptr<PieceStorage> pieceStorage_;
-  std::shared_ptr<PeerStorage> peerStorage_;
-  std::shared_ptr<BtAnnounce> btAnnounce_;
-    }
     
-      int64_t offset_;
+    {  virtual int64_t getTotalLength() CXX11_OVERRIDE;
+};
     
-    void AnnounceList::setCurrentTier(
-    std::deque<std::shared_ptr<AnnounceTier>>::iterator itr)
+    void AnnounceTier::nextEvent()
 {
-  if (itr != std::end(tiers_)) {
-    currentTier_ = std::move(itr);
-    currentTracker_ = std::begin((*currentTier_)->urls);
+  switch (event) {
+  case STARTED:
+    event = DOWNLOADING;
+    break;
+  case STARTED_AFTER_COMPLETION:
+    event = SEEDING;
+    break;
+  case STOPPED:
+    event = HALTED;
+    break;
+  case COMPLETED:
+    event = SEEDING;
+    break;
+  default:
+    break;
   }
 }
     
+      // Don't allow copying
+  AnnounceTier(const AnnounceTier&) = delete;
+  AnnounceTier& operator=(const AnnounceTier&) = delete;
     
-    {  void nextEventIfAfterStarted();
-};
+    class AuthConfig;
