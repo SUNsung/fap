16b19fedab335b3147dc4f0a63efb89b06ba85ee
SUNsung <weatherius@gmail.com>
2018-08-05 06:43:36 +0300

    Rewrite [53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py] File in: 2018-08-05 06:43:35

--- pyton/53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py
+++ pyton/53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py
@@ -1,73 +1,188 @@
 
         
-        from mrjob.job import MRJob
+            # Get the version from youtube_dl/version.py without importing the package
+    exec(compile(open('youtube_dl/version.py').read(),
+                 'youtube_dl/version.py', 'exec'))
     
-            (category1, 1), product4
-        (category1, 2), product1
-        (category1, 3), product2
-        (category2, 3), product1
-        (category2, 7), product3
+    with io.open(README_FILE, encoding='utf-8') as f:
+    oldreadme = f.read()
+    
+        for release in releases:
+        compat_print(release['name'])
+        for asset in release['assets']:
+            asset_name = asset['name']
+            total_bytes += asset['download_count'] * asset['size']
+            if all(not re.match(p, asset_name) for p in (
+                    r'^youtube-dl$',
+                    r'^youtube-dl-\d{4}\.\d{2}\.\d{2}(?:\.\d+)?\.tar\.gz$',
+                    r'^youtube-dl\.exe$')):
+                continue
+            compat_print(
+                ' %s size: %s downloads: %d'
+                % (asset_name, format_size(asset['size']), asset['download_count']))
+    
+    
+def expect_value(self, got, expected, field):
+    if isinstance(expected, compat_str) and expected.startswith('re:'):
+        match_str = expected[len('re:'):]
+        match_rex = re.compile(match_str)
+    
+        _TESTS = [{
+        'url': 'http://camwithher.tv/view_video.php?viewkey=6e9a24e2c0e842e1f177&page=&viewtype=&category=',
+        'info_dict': {
+            'id': '5644',
+            'ext': 'flv',
+            'title': 'Periscope Tease',
+            'description': 'In the clouds teasing on periscope to my favorite song',
+            'duration': 240,
+            'view_count': int,
+            'comment_count': int,
+            'uploader': 'MileenaK',
+            'upload_date': '20160322',
+        },
+        'params': {
+            'skip_download': True,
+        }
+    }, {
+        'url': 'http://camwithher.tv/view_video.php?viewkey=6dfd8b7c97531a459937',
+        'only_matching': True,
+    }, {
+        'url': 'http://camwithher.tv/view_video.php?page=&viewkey=6e9a24e2c0e842e1f177&viewtype=&category=',
+        'only_matching': True,
+    }, {
+        'url': 'http://camwithher.tv/view_video.php?viewkey=b6c3b5bea9515d1a1fc4&page=&viewtype=&category=mv',
+        'only_matching': True,
+    }]
+    
+        _CONFIG = {
+        # http://edition.cnn.com/.element/apps/cvp/3.0/cfg/spider/cnn/expansion/config.xml
+        'edition': {
+            'data_src': 'http://edition.cnn.com/video/data/3.0/video/%s/index.xml',
+            'media_src': 'http://pmd.cdn.turner.com/cnn/big',
+        },
+        # http://money.cnn.com/.element/apps/cvp2/cfg/config.xml
+        'money': {
+            'data_src': 'http://money.cnn.com/video/data/4.0/video/%s.xml',
+            'media_src': 'http://ht3.cdn.turner.com/money/big',
+        },
+    }
+    
+        @property
+    @abstractmethod
+    def value(self):
+        pass
+    
+            Accessing a node updates its position to the front of the LRU list.
         '''
-        category, product_id = key
-        quantity = value
-        yield (category, quantity), product_id
+        node = self.lookup.get(query)
+        if node is None:
+            return None
+        self.linked_list.move_to_front(node)
+        return node.results
     
-    html_folder = u'html'
-text_folder = u'paragraphs'
-short_text_folder = u'short_paragraphs'
-n_words_per_short_text = 5
+        def remove(self, key):
+        hash_index = self._hash_function(key)
+        for index, item in enumerate(self.table[hash_index]):
+            if item.key == key:
+                del self.table[hash_index][index]
+                return
+        raise KeyError('Key not found')
+
     
-    '''
-# Author: Olivier Grisel <olivier.grisel@ensta.org>
-# License: Simplified BSD
+            Accessing a node updates its position to the front of the LRU list.
+        '''
+        node = self.lookup[query]
+        if node is None:
+            return None
+        self.linked_list.move_to_front(node)
+        return node.results
     
-    import sys
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import LinearSVC
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV
-from sklearn.datasets import load_files
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
+        def crawl(self):
+        while True:
+            page = self.data_store.extract_max_priority_page()
+            if page is None:
+                break
+            if self.data_store.crawled_similar(page.signature):
+                self.data_store.reduce_priority_link_to_crawl(page.url)
+            else:
+                self.crawl_page(page)
+            page = self.data_store.extract_max_priority_page()
+
     
-    n_clusters = (4, 3)
-data, rows, columns = make_checkerboard(
-    shape=(300, 300), n_clusters=n_clusters, noise=10,
-    shuffle=False, random_state=0)
+            run = response.json()
     
-    The dataset is generated using the ``make_biclusters`` function, which
-creates a matrix of small values and implants bicluster with large
-values. The rows and columns are then shuffled and passed to the
-Spectral Co-Clustering algorithm. Rearranging the shuffled matrix to
-make biclusters contiguous shows how accurately the algorithm found
-the biclusters.
+    # A hostname label, e.g. 'foo' in 'foo.example.com'. Consists of alphanumeric
+# characters plus dashes (and underscores) or valid ranges. The label may not
+# start or end with a hyphen or an underscore. This is interpolated into the
+# hostname pattern below. We don't try to enforce the 63-char length limit.
     
-    # Plot changes in predicted probabilities via arrows
-plt.figure(0)
-colors = ['r', 'g', 'b']
-for i in range(clf_probs.shape[0]):
-    plt.arrow(clf_probs[i, 0], clf_probs[i, 1],
-              sig_clf_probs[i, 0] - clf_probs[i, 0],
-              sig_clf_probs[i, 1] - clf_probs[i, 1],
-              color=colors[y_test[i]], head_width=1e-2)
-    
-        ax.scatter(X[:, 0], X[:, 1], color=COLORS.take((Y * [1, 2, 4]
-                                                    ).sum(axis=1)),
-               marker='.')
-    ax.scatter(p_w_c[0] * length, p_w_c[1] * length,
-               marker='*', linewidth=.5, edgecolor='black',
-               s=20 + 1500 * p_c ** 2,
-               color=COLORS.take([1, 2, 4]))
-    ax.set_xlabel('Feature 0 count')
-    return p_c, p_w_c
+            # global URL, resource, entity, method_name
+        input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/global/backendServices/mybackendservice/getHealth'
+        actual = GCPUtils.parse_gcp_url(input_url)
+        self.assertEquals('compute', actual['service'])
+        self.assertEquals('v1', actual['api_version'])
+        self.assertEquals('myproject', actual['project'])
+        self.assertTrue('global' in actual)
+        self.assertTrue(actual['global'])
+        self.assertEquals('backendServices', actual['resource_name'])
+        self.assertEquals('mybackendservice', actual['entity_name'])
+        self.assertEquals('getHealth', actual['method_name'])
     
     
-class Pin:
-    host = 'http://img.hb.aicdn.com/'
+def get_group_vars(groups):
     
-        # ordered list of supported stream types / qualities on this site
-    # order: high quality -> low quality
-    stream_types = [
-        {'id': 'original'}, # contains an 'id' or 'itag' field at minimum
-        {'id': 'small'},
-    ]
+        def test_max_delay(self):
+        strategy = _exponential_backoff(retries=7, delay=1, backoff=2, max_delay=60)
+        result = list(strategy())
+        self.assertEquals(result, [1, 2, 4, 8, 16, 32, 60])
+    
+        def _dicts(self, ajvars):
+        print(ajvars)
+        res28 = self._dict_jinja28(ajvars)
+        res29 = self._dict_jinja29(ajvars)
+        # res28_other = self._dict_jinja28(ajvars, {'other_key': 'other_value'})
+        # other = {'other_key': 'other_value'}
+        # res29_other = self._dict_jinja29(ajvars, *other)
+        print('res28: %s' % res28)
+        print('res29: %s' % res29)
+        # print('res28_other: %s' % res28_other)
+        # print('res29_other: %s' % res29_other)
+        # return (res28, res29, res28_other, res29_other)
+        # assert ajvars == res28
+        # assert ajvars == res29
+        return (res28, res29)
+    
+    In Blackboard pattern several specialised sub-systems (knowledge sources)
+assemble their knowledge to build a possibly partial or approximate solution.
+In this way, the sub-systems work together to solve the problem,
+where the solution is the sum of its parts.
+    
+        @classmethod
+    def setUpClass(self):
+        '''
+        - Create a temporary directory and file
+        /test_command
+           /foo.txt
+        - get the temporary test directory
+        - and initializes the command stack.
+        '''
+        os.mkdir('tests/test_command')
+        open('tests/test_command/foo.txt', 'w').close()
+        self.__get_test_directory()
+        self.command_stack = []
+        self.command_stack.append(MoveFileCommand(os.path.join(
+            self.test_dir, 'foo.txt'), os.path.join(self.test_dir, 'bar.txt')))
+        self.command_stack.append(MoveFileCommand(os.path.join(
+            self.test_dir, 'bar.txt'), os.path.join(self.test_dir, 'baz.txt')))
+    
+        def test_am_station_overflow_after_scan(self):
+        self.radio.scan()
+        station = self.radio.state.stations[self.radio.state.pos]
+        expected_station = '1250'
+        self.assertEqual(station, expected_station)
+    
+        def execute(self):
+        self.rename(self.src, self.dest)
+    
+    import random
+import time
