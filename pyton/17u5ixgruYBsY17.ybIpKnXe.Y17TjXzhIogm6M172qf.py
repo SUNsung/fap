b87f4d228797d43f3da7d36a5f29294e2b6a1ad1
SUNsung <weatherius@gmail.com>
2018-08-05 05:01:55 +0300

    Rewrite [17u5ixgruYBsY17.ybIpKnXe.Y17TjXzhIogm6M172qf.py] File in: 2018-08-05 05:01:55

--- pyton/17u5ixgruYBsY17.ybIpKnXe.Y17TjXzhIogm6M172qf.py
+++ pyton/17u5ixgruYBsY17.ybIpKnXe.Y17TjXzhIogm6M172qf.py
@@ -1,61 +1,87 @@
 
         
-            def _slot_gc(self, age=60):
-        mintime = time() - age
-        for key, slot in list(self.slots.items()):
-            if not slot.active and slot.lastseen + slot.delay < mintime:
-                self.slots.pop(key).close()
-
+                .. versionadded:: 0.11
+        '''
+        rv = {}
+        for k, v in iteritems(self):
+            if not k.startswith(namespace):
+                continue
+            if trim_namespace:
+                key = k[len(namespace):]
+            else:
+                key = k
+            if lowercase:
+                key = key.lower()
+            rv[key] = v
+        return rv
     
+        def __init__(self, request, key):
+        form_matches = request.form.getlist(key)
+        buf = ['You tried to access the file '%s' in the request.files '
+               'dictionary but it does not exist.  The mimetype for the request '
+               'is '%s' instead of 'multipart/form-data' which means that no '
+               'file contents were transmitted.  To fix this error you should '
+               'provide enctype='multipart/form-data' in your form.' %
+               (key, request.mimetype)]
+        if form_matches:
+            buf.append('\n\nThe browser instead transmitted some file names. '
+                       'This was submitted: %s' % ', '.join(''%s'' % x
+                            for x in form_matches))
+        self.msg = ''.join(buf)
     
-def _parsed_url_args(parsed):
-    # Assume parsed is urlparse-d from Request.url,
-    # which was passed via safe_url_string and is ascii-only.
-    b = lambda s: to_bytes(s, encoding='ascii')
-    path = urlunparse(('', '', parsed.path or '/', parsed.params, parsed.query, ''))
-    path = b(path)
-    host = b(parsed.hostname)
-    port = parsed.port
-    scheme = b(parsed.scheme)
-    netloc = b(parsed.netloc)
-    if port is None:
-        port = 443 if scheme == b'https' else 80
-    return scheme, netloc, host, port, path
+        def make_null_session(self, app):
+        '''Creates a null session which acts as a replacement object if the
+        real session support could not be loaded due to a configuration
+        error.  This mainly aids the user experience because the job of the
+        null session is to still support lookup without complaining but
+        modifications are answered with a helpful error message of what
+        failed.
     
-        def process_start_requests(self, start_requests, spider):
-        return self._process_chain('process_start_requests', start_requests, spider)
-
+        if version in tags:
+        fail('Version '%s' is already tagged', version)
     
-            if not self._has_ajax_crawlable_variant(response):
-            return response
+    from flask._compat import StringIO
+from flask.logging import default_handler, has_level_handler, \
+    wsgi_errors_stream
     
-        def _debug_set_cookie(self, response, spider):
-        if self.debug:
-            cl = [to_native_str(c, errors='replace')
-                  for c in response.headers.getlist('Set-Cookie')]
-            if cl:
-                cookies = '\n'.join('Set-Cookie: {}\n'.format(c) for c in cl)
-                msg = 'Received cookies from: {}\n{}'.format(response, cookies)
-                logger.debug(msg, extra={'spider': spider})
+        try:
+        rv = app.test_client().get('/')
+        assert rv.data == b'stuff'
+    
+        def process_options(self, args, opts):
+        try:
+            self.settings.setdict(arglist_to_dict(opts.set),
+                                  priority='cmdline')
+        except ValueError:
+            raise UsageError('Invalid -s value, use -s NAME=VALUE', print_help=False)
+    
+    from twisted.internet import defer
+    
+        def _add_middleware(self, mw):
+        super(SpiderMiddlewareManager, self)._add_middleware(mw)
+        if hasattr(mw, 'process_spider_input'):
+            self.methods['process_spider_input'].append(mw.process_spider_input)
+        if hasattr(mw, 'process_spider_output'):
+            self.methods['process_spider_output'].insert(0, mw.process_spider_output)
+        if hasattr(mw, 'process_spider_exception'):
+            self.methods['process_spider_exception'].insert(0, mw.process_spider_exception)
+        if hasattr(mw, 'process_start_requests'):
+            self.methods['process_start_requests'].insert(0, mw.process_start_requests)
+    
+    define('port', default=8888, help='run on the given port', type=int)
+    
+        def on_close(self):
+        ChatSocketHandler.waiters.remove(self)
+    
+        def transform(self, node, results):
+        self.found_future_import = True
+        return self.new_future_import(node)
     
     try:
-    from cStringIO import StringIO as BytesIO
+    from urllib.parse import unquote
 except ImportError:
-    from io import BytesIO
-    
-        def __init__(self, timeout=180):
-        self._timeout = timeout
-    
-        @classmethod
-    def from_crawler(cls, crawler):
-        o = cls(crawler.settings['USER_AGENT'])
-        crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
-        return o
-    
-    # HTTP and crawling
-    
-        def data_received(self, chunk):
-        self.bytes_read += len(chunk)
+    # Python 2.
+    from urllib import unquote
     
     
 class Application(tornado.web.Application):
@@ -79,6 +105,27 @@ class Application(tornado.web.Application):
         )
         tornado.web.Application.__init__(self, handlers, **settings)
     
-        if twisted is not None:
-        from tornado.platform.twisted import TwistedResolver
-        resolvers.append(TwistedResolver())
+    import logging
+from tornado.curl_httpclient import CurlAsyncHTTPClient
+from tornado.simple_httpclient import SimpleAsyncHTTPClient
+from tornado.ioloop import IOLoop
+from tornado.options import define, options, parse_command_line
+from tornado.web import RequestHandler, Application
+    
+    
+def main():
+    parse_command_line()
+    if options.dump:
+        print(tmpl.code)
+        sys.exit(0)
+    t = Timer(render)
+    results = t.timeit(options.num) / options.num
+    print('%0.3f ms per iteration' % (results * 1000))
+    
+    # On RTD we can't import sphinx_rtd_theme, but it will be applied by
+# default anyway.  This block will use the same theme when building locally
+# as on RTD.
+if not on_rtd:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
