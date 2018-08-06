778a2ada27fa2321f45ccff5baba6801a8a5e83b
SUNsung <weatherius@gmail.com>
2018-08-06 14:03:35 +0300

    Rewrite [25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py] File in: 2018-08-06 14:03:34

--- pyton/25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py
+++ pyton/25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py
@@ -1,210 +1,63 @@
 
         
-            # Sorting the libraries
-    inner_blocks = sorted(blocks[0].split('##'))
-    for i in range(1 , len(inner_blocks)):
-        if inner_blocks[i][0] != '#':
-            inner_blocks[i] = '##' + inner_blocks[i]
-    inner_blocks=''.join(inner_blocks)
-    
-    from tensorflow.python.platform import gfile
-    
-    cc_library(
-    name = 'compute_session_impl',
-    srcs = ['compute_session_impl.cc'],
-    hdrs = ['compute_session_impl.h'],
-    deps = [
-        ':compute_session',
-        ':index_translator',
-        ':input_batch_cache',
-        '//dragnn/components/util:bulk_feature_extractor',
-        '//dragnn/core/util:label',
-        '//dragnn/protos:data_proto_cc',
-        '//dragnn/protos:spec_proto_cc',
-        '//dragnn/protos:trace_proto_cc',
-        '//syntaxnet:base',
-        '//syntaxnet:registry',
-    ],
-)
-    
-    py_library(
-    name = 'composite_optimizer',
-    srcs = ['composite_optimizer.py'],
-    deps = [
-        '@org_tensorflow//tensorflow:tensorflow_py',
-        '@org_tensorflow//tensorflow/core:protos_all_py',
-    ],
-)
-    
-    import tensorflow as tf
-    
-          # Expect feature extraction to generate a runtime error due to the
-      # fixed feature.
-      with self.assertRaises(RuntimeError):
-        comp.build_greedy_training(self.master_state, self.network_states)
-    
-      def get_variable(self, var_name=None, var_params=None):
-    '''Returns either the original or averaged version of a given variable.
-    
-          # The cost corresponding to the three entries is, respectively,
-      # 0.7012, 0.7644, and 1.7269. Each of them is computed using the formula
-      # -prob_i * log(sigmoid(logit_i)) - (1-prob_i) * log(1-sigmoid(logit_i))
-      self.assertAlmostEqual(cost, 3.1924, 4)
-      self.assertEqual(correct, 1)
-      self.assertEqual(total, 3)
-      self.assertAllEqual(gold_labels, [0, 1, 2])
-    
+        def download_urls_sequential(urls, timeout=60):
+    url_to_content = {}
+    for url in urls:
         try:
-      with open(FLAGS.expected_file) as expected:
-        content_expected = expected.read()
-    except IOError as e:
-      self.fail('Error opening '%s': %s' % (FLAGS.expected_file, e.strerror))
+            url_to_content[url] = load_url(url, timeout=timeout)
+        except:
+            pass
+    return url_to_content
     
-    # Imported for FLAGS.tf_master, which is used in the lexicon module.
+    # If true, the index is split into individual pages for each letter.
+#html_split_index = False
     
-      TODO(googleuser): Try improving the condition number of the Laplacian matrix
-  directly, instead of using the indirect approach above.  For example, one
-  could add c*I to the Laplacian (i.e., Tikhonov regularization).
+        def test_result_with_cancel(self):
+        # TODO(brian@sweetapp.com): This test is timing dependant.
+        def notification():
+            # Wait until the main thread is waiting for the result.
+            time.sleep(1)
+            f1.cancel()
     
-    
-# Core signals.  For usage examples grep the source code or consult
-# the API documentation in docs/api.rst as well as docs/signals.rst
-template_rendered = _signals.signal('template-rendered')
-before_render_template = _signals.signal('before-render-template')
-request_started = _signals.signal('request-started')
-request_finished = _signals.signal('request-finished')
-request_tearing_down = _signals.signal('request-tearing-down')
-got_request_exception = _signals.signal('got-request-exception')
-appcontext_tearing_down = _signals.signal('appcontext-tearing-down')
-appcontext_pushed = _signals.signal('appcontext-pushed')
-appcontext_popped = _signals.signal('appcontext-popped')
-message_flashed = _signals.signal('message-flashed')
+        for test in tests:
+      yield test[ 0 ], test[ 1 ], test[ 2 ]
 
     
     
-def test_existing_handler(app):
-    logging.root.addHandler(logging.StreamHandler())
-    assert app.logger.level == logging.NOTSET
-    assert not app.logger.handlers
-    
-        with app.test_request_context('/?name=World'):
-        assert index() == 'Hello World!'
-    with app.test_request_context('/meh'):
-        assert meh() == 'http://localhost/meh'
-    assert flask._request_ctx_stack.top is None
-    
-    from flask._compat import StringIO
-    
-            This should not be called from user code, and is only exposed for use
-        when subclassing the
-        :class:`HTTPAdapter <requests.adapters.HTTPAdapter>`.
-    
-    
-class ChunkedEncodingError(RequestException):
-    '''The server declared chunked encoding but sent an invalid chunk.'''
-    
-    # Language to be used for generating the HTML full-text search index.
-# Sphinx supports the following languages:
-#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
-#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
-#html_search_language = 'en'
-    
-    from . import utils
-from . import packages
-from .models import Request, Response, PreparedRequest
-from .api import request, get, head, post, patch, put, delete, options
-from .sessions import session, Session
-from .status_codes import codes
-from .exceptions import (
-    RequestException, Timeout, URLRequired,
-    TooManyRedirects, HTTPError, ConnectionError,
-    FileModeWarning, ConnectTimeout, ReadTimeout
-)
-    
-          >>> s = requests.Session()
-      >>> s.send(r)
-      <Response [200]>
-    '''
-    
-    This module provides a Session object to manage and persist settings across
-requests (cookies, auth, proxies).
-'''
-import os
-import sys
-import time
-from datetime import timedelta
-    
-            expected = {
-            'name': 'myhealthcheck',
-            'checkIntervalSec': 5,
-            'port': 443,
-            'unhealthyThreshold': 2,
-            'healthyThreshold': 2,
-            'host': '',
-            'timeoutSec': 5,
-            'requestPath': '/'}
-    
-        html = get_content(url)
-    pid = match1(html, r'video\.settings\.pid\s*=\s*\'([^\']+)\'')
-    title = match1(html, r'video\.settings\.title\s*=\s*\'([^\']+)\'')
-    
-    class MusicPlayOn(VideoExtractor):
-    name = 'MusicPlayOn'
-    
-        #title
-    title = ''
-    profile_api = 'https://www.showroom-live.com/api/room/profile?room_id={room_id}'.format(room_id = room_id)
-    html = loads(get_content(profile_api))
-    try:
-        title = html['main_name']
-    except KeyError:
-        title = 'Showroom_{room_id}'.format(room_id = room_id)
-    
-    # line 11
-def eggs(x, y):
-    'A docstring.'
-    global fr, st
-    fr = inspect.currentframe()
-    st = inspect.stack()
-    p = x
-    q = y / 0
-    
-        group = parser.add_argument_group('Selecting tests')
-    group.add_argument('-r', '--randomize', action='store_true',
-                       help='randomize test execution order.' + more_details)
-    group.add_argument('--randseed', metavar='SEED',
-                       dest='random_seed', type=int,
-                       help='pass a random seed to reproduce a previous '
-                            'random run')
-    group.add_argument('-f', '--fromfile', metavar='FILE',
-                       help='read names of tests to run from a file.' +
-                            more_details)
-    group.add_argument('-x', '--exclude', action='store_true',
-                       help='arguments are tests to *exclude*')
-    group.add_argument('-s', '--single', action='store_true',
-                       help='single step through a set of tests.' +
-                            more_details)
-    group.add_argument('-m', '--match', metavar='PAT',
-                       dest='match_tests', action='append',
-                       help='match test cases and methods with glob pattern PAT')
-    group.add_argument('--matchfile', metavar='FILENAME',
-                       dest='match_filename',
-                       help='similar to --match but get patterns from a '
-                            'text file, one pattern per line')
-    group.add_argument('-G', '--failfast', action='store_true',
-                       help='fail as soon as a test fails (only with -v or -W)')
-    group.add_argument('-u', '--use', metavar='RES1,RES2,...',
-                       action='append', type=resources_list,
-                       help='specify which special resource intensive tests '
-                            'to run.' + more_details)
-    group.add_argument('-M', '--memlimit', metavar='LIMIT',
-                       help='run very large memory-consuming tests.' +
-                            more_details)
-    group.add_argument('--testdir', metavar='DIR',
-                       type=relative_filename,
-                       help='execute test files in the specified directory '
-                            '(instead of the Python stdlib test suite)')
+@YouCompleteMeInstance()
+def SendCommandRequest_BuildRange_VisualMarks_test( ycm, *args ):
+  current_buffer = VimBuffer( 'buffer',
+                              contents = [ 'first line',
+                                           'second line' ],
+                              visual_start = [ 1, 4 ],
+                              visual_end = [ 2, 8 ] )
+  with MockVimBuffers( [ current_buffer ], [ current_buffer ] ):
+    with patch( 'ycm.youcompleteme.SendCommandRequest' ) as send_request:
+      ycm.SendCommandRequest( [ 'GoTo' ], 'python', 'tab', True, 1, 2 )
+      send_request.assert_called_once_with(
+        [ 'GoTo' ],
+        'python',
+        'tab',
+        {
+          'options': {
+            'tab_size': 2,
+            'insert_spaces': True
+          },
+          'range': {
+            'start': {
+              'line_num': 1,
+              'column_num': 5
+            },
+            'end': {
+              'line_num': 2,
+              'column_num': 9
+            }
+          }
+        }
+      )
+
     
     
-def runtest_inner(ns, test, display_failure=True):
-    support.unload(test)
+def ConvertVimDataToCompletionData( vim_data ):
+  # see :h complete-items for a description of the dictionary fields
+  completion_data = {}
