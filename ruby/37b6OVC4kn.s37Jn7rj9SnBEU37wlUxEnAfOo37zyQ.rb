23a8b5e9f62c6b1bd26b21c6958c6b799196d4f2
SUNsung <weatherius@gmail.com>
2018-08-07 02:21:23 +0300

    Rewrite [37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb] File in: 2018-08-07 02:21:22

--- ruby/37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb
+++ ruby/37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb
@@ -1,130 +1,74 @@
 
         
-          test 'broadcasting_for with a string' do
-    assert_equal 'hello', ChatChannel.broadcasting_for('hello')
-  end
-end
-
-    
-    require 'test_helper'
-require 'stubs/test_server'
-    
-          assert_called(@connection.websocket, :close) do
-        @connection.process_internal_message 'type' => 'disconnect'
-      end
+            # Get rid of any info 'dir' files, so they don't conflict at the link stage
+    info_dir_file = @f.info + 'dir'
+    if info_dir_file.file? && !@f.skip_clean?(info_dir_file)
+      observe_file_removal info_dir_file
     end
+    
+          # Find commands in Homebrew/dev-cmd
+      if ARGV.homebrew_developer?
+        puts
+        puts 'Built-in development commands'
+        puts_columns internal_development_commands
+      end
+    
+      def updated?
+    initial_revision != current_revision
   end
     
-    require 'test_helper'
-require 'stubs/test_server'
-require 'active_support/core_ext/hash/indifferent_access'
+          it 'get HG revision number' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          get_build_number_repository(
+            use_hg_revision_number: true
+          )
+        end').runner.execute(:test)
     
-        To install Clojure you should install Leiningen:
-      brew install leiningen
-    and then follow the tutorial:
-      https://github.com/technomancy/leiningen/blob/stable/doc/TUTORIAL.md
-    EOS
-  when 'osmium' then <<-EOS.undent
-    The creator of Osmium requests that it not be packaged and that people
-    use the GitHub master branch instead.
-    EOS
-  when 'gfortran' then <<-EOS.undent
-    GNU Fortran is now provided as part of GCC, and can be installed with:
-      brew install gcc
-    EOS
-  when 'play' then <<-EOS.undent
-    Play 2.3 replaces the play command with activator:
-      brew install typesafe-activator
-    
-      def userpaths?
-    @settings.include? :userpaths
-  end
-end
-    
-          if path.symlink? || path.directory?
-        next
-      elsif path.extname == '.la'
-        path.unlink
+        def matching_domain
+      if domain.nil?
+        Account.where(domain: nil)
       else
-        # Set permissions for executables and non-executables
-        perms = if path.mach_o_executable? || path.text_executable?
-          0555
-        else
-          0444
+        Account.where(Account.arel_table[:domain].lower.eq domain.to_s.downcase)
+      end
+    end
+  end
+end
+
+    
+    def each_schema_load_environment
+  # If we're in development, also run this for the test environment.
+  # This is a somewhat hacky way to do this, so here's why:
+  # 1. We have to define this before we load the schema, or we won't
+  #    have a timestamp_id function when we get to it in the schema.
+  # 2. db:setup calls db:schema:load_if_ruby, which calls
+  #    db:schema:load, which we define above as having a prerequisite
+  #    of this task.
+  # 3. db:schema:load ends up running
+  #    ActiveRecord::Tasks::DatabaseTasks.load_schema_current, which
+  #    calls a private method `each_current_configuration`, which
+  #    explicitly also does the loading for the `test` environment
+  #    if the current environment is `development`, so we end up
+  #    needing to do the same, and we can't even use the same method
+  #    to do it.
+    
+          it 'sets the regeneration marker to expire' do
+        allow(RegenerationWorker).to receive(:perform_async)
+        get :show
+        expect(Redis.current.ttl('account:#{user.account_id}:regeneration')).to be >= 0
+      end
+    
+      def provider
+    'facebook'
+  end
+    
+            def failure_message_when_negated
+          'Should not have an attachment named #{@attachment_name}'
         end
-        if ARGV.debug?
-          old_perms = path.stat.mode & 0777
-          if perms != old_perms
-            puts 'Fixing #{path} permissions from #{old_perms.to_s(8)} to #{perms.to_s(8)}'
-          end
+        alias negative_failure_message failure_message_when_negated
+    
+            required = directories.map do |directory|
+          pathname = File.expand_path(Rails.root.join(directory, filename))
+          file_exists = File.exist?(pathname)
+          require pathname if file_exists
+          file_exists
         end
-        path.chmod perms
-      end
-    end
-  end
-end
-
-    
-        if ARGV.include? '--list-checks'
-      puts checks.all.sort
-      exit
-    end
-    
-        results.map do |name|
-      begin
-        formula = Formulary.factory(name)
-        canonical_name = formula.name
-        canonical_full_name = formula.full_name
-      rescue
-        canonical_name = canonical_full_name = name
-      end
-      # Ignore aliases from results when the full name was also found
-      if aliases.include?(name) && results.include?(canonical_full_name)
-        next
-      elsif (HOMEBREW_CELLAR/canonical_name).directory?
-        pretty_installed(name)
-      else
-        name
-      end
-    end.compact
-  end
-end
-
-    
-          export JAVA_HOME='$(/usr/libexec/java_home)'
-      export AWS_ACCESS_KEY='<Your AWS Access ID>'
-      export AWS_SECRET_KEY='<Your AWS Secret Key>'
-      export #{home_name}='#{home_value}'
-    EOS
-  end
-end
-
-    
-      included do
-    scope :admins, -> { where(admin: true) }
-    scope :moderators, -> { where(moderator: true) }
-    scope :staff, -> { where('moderator or admin ') }
-  end
-    
-      def id
-    object.id.to_s
-  end
-    
-        existence_maps = grouped_codes.map { |c| c.map { |cc| [cc, File.exist?(Rails.root.join('public', 'emoji', codepoints_to_filename(cc) + '.svg'))] }.to_h }
-    map = {}
-    
-    module Sinatra
-  class Application < Base
-    
-      %w(GET HEAD).each do |method|
-    it 'accepts #{method} requests with non-whitelisted Origin' do
-      expect(send(method.downcase, '/', {}, 'HTTP_ORIGIN' => 'http://malicious.com')).to be_ok
-    end
-  end
-    
-      it 'denies requests with a changing User-Agent header' do
-    session = {:foo => :bar}
-    get '/', {}, 'rack.session' => session, 'HTTP_USER_AGENT' => 'a'
-    get '/', {}, 'rack.session' => session, 'HTTP_USER_AGENT' => 'b'
-    expect(session).to be_empty
-  end
