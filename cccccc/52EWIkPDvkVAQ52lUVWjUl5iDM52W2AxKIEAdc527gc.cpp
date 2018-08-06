8f8a5fbfc72e4dcf8b35ee48ccb0e6a06debe45c
SUNsung <weatherius@gmail.com>
2018-08-06 19:04:32 +0300

    Rewrite [52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp] File in: 2018-08-06 19:04:31

--- cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
+++ cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
@@ -1,328 +1,151 @@
 
         
-        #include 'tensorflow/core/distributed_runtime/worker_cache.h'
-#include 'tensorflow/core/platform/mutex.h'
-#include 'tensorflow/core/platform/thread_annotations.h'
-#include 'tensorflow/core/platform/types.h'
-#include 'tensorflow/core/protobuf/worker.pb.h'
+        #include 'core/os/memory.h'
+#include 'core/print_string.h'
     
-    #endif  // GOOGLE_CUDA
-
-    
-    
-    {#if GOOGLE_CUDA
-// A special GPU kernel for int32.
-// TODO(b/25387198): Also enable int32 in device memory. This kernel
-// registration requires all int32 inputs and outputs to be in host memory.
-REGISTER_KERNEL_BUILDER(Name('Mod')
-                            .Device(DEVICE_GPU)
-                            .HostMemory('x')
-                            .HostMemory('y')
-                            .HostMemory('z')
-                            .TypeConstraint<int32>('T'),
-                        BinaryOp<CPUDevice, functor::safe_mod<int32>>);
-REGISTER_KERNEL_BUILDER(Name('TruncateMod')
-                            .Device(DEVICE_GPU)
-                            .HostMemory('x')
-                            .HostMemory('y')
-                            .HostMemory('z')
-                            .TypeConstraint<int32>('T'),
-                        BinaryOp<CPUDevice, functor::safe_mod<int32>>);
-#endif
-}  // namespace tensorflow
-
-    
-    
-    {  return kTfLiteOk;
-}
-    
-      Status DecorateGraph(Graph* graph, Device* device) override;
-  Status PublishGraph(const Graph& graph, const string& device_name) override;
-    
-    // Prefetching support
-//
-// Defined behavior on some of the uarchs:
-// PREFETCH_HINT_T0:
-//   prefetch to all levels of the hierarchy (except on p4: prefetch to L2)
-// PREFETCH_HINT_NTA:
-//   p4: fetch to L2, but limit to 1 way (out of the 8 ways)
-//   core: skip L2, go directly to L1
-//   k8 rev E and later: skip L2, can go to either of the 2-ways in L1
-enum PrefetchHint {
-  PREFETCH_HINT_T0 = 3,  // More temporal locality
-  PREFETCH_HINT_T1 = 2,
-  PREFETCH_HINT_T2 = 1,  // Less temporal locality
-  PREFETCH_HINT_NTA = 0  // No temporal locality
-};
-template <PrefetchHint hint>
-void prefetch(const void* x);
-    
-    
-    {}  // namespace tensorflow
-#endif  // TENSORFLOW_USE_SYCL
-
-    
-    REGISTER_OP('EncodeAudio')
-    .Input('sampled_audio: float')
-    .Output('contents: string')
-    .Attr('file_format: string')
-    .Attr('samples_per_second: int')
-    .Attr('bits_per_second: int = 192000')
-    .SetShapeFn(shape_inference::ScalarShape)
-    .Doc(R'doc(
-Processes a `Tensor` containing sampled audio with the number of channels
-and length of the audio specified by the dimensions of the `Tensor`. The
-audio is converted into a string that, when saved to disk, will be equivalent
-to the audio in the specified audio format.
-    
-    
-    {    return state;
-}
-    
-    #endif
-
-    
-    void CV_ModelEstimator2_Test::get_test_array_types_and_sizes( int /*test_case_idx*/,
-                                                              vector<vector<Size> > &sizes, vector<vector<int> > &types )
-{
-    RNG &rng = ts->get_rng();
-    checkPartialSubsets = (cvtest::randInt(rng) % 2 == 0);
+    void SoftBody::update_physics_server() {
     }
     
-    CV_EXPORTS_W void max(InputArray src1, Scalar src2, OutputArray dst);
+    	GDCLASS(Skeleton, Spatial);
     
-    #undef cv_hal_LU32f
-#define cv_hal_LU32f lapack_LU32f
-#undef cv_hal_LU64f
-#define cv_hal_LU64f lapack_LU64f
-    
-      static Slice Contents(const WriteBatch* batch) {
-    return Slice(batch->rep_);
-  }
-    
-      // Check for expected size.
-  ASSERT_OK(env_->GetFileSize('/dir/f', &file_size));
-  ASSERT_EQ(8, file_size);
-    
-      // create first key range
-  leveldb::WriteBatch batch;
-  for (size_t i = 0; i < kNumKeys; i++) {
-    batch.Put(Key1(i), 'value for range 1 key');
-  }
-  ASSERT_OK(db->Write(leveldb::WriteOptions(), &batch));
-    
-    // A FilterBlockBuilder is used to construct all of the filters for a
-// particular Table.  It generates a single string which is stored as
-// a special block in the Table.
-//
-// The sequence of calls to FilterBlockBuilder must match the regexp:
-//      (StartBlock AddKey*)* Finish
-class FilterBlockBuilder {
- public:
-  explicit FilterBlockBuilder(const FilterPolicy*);
-    }
-    
-    std::string Histogram::ToString() const {
-  std::string r;
-  char buf[200];
-  snprintf(buf, sizeof(buf),
-           'Count: %.0f  Average: %.4f  StdDev: %.2f\n',
-           num_, Average(), StandardDeviation());
-  r.append(buf);
-  snprintf(buf, sizeof(buf),
-           'Min: %.4f  Median: %.4f  Max: %.4f\n',
-           (num_ == 0.0 ? 0.0 : min_), Median(), max_);
-  r.append(buf);
-  r.append('------------------------------------------------------\n');
-  const double mult = 100.0 / num_;
-  double sum = 0;
-  for (int b = 0; b < kNumBuckets; b++) {
-    if (buckets_[b] <= 0.0) continue;
-    sum += buckets_[b];
-    snprintf(buf, sizeof(buf),
-             '[ %7.0f, %7.0f ) %7.0f %7.3f%% %7.3f%% ',
-             ((b == 0) ? 0.0 : kBucketLimit[b-1]),      // left
-             kBucketLimit[b],                           // right
-             buckets_[b],                               // count
-             mult * buckets_[b],                        // percentage
-             mult * sum);                               // cumulative percentage
-    r.append(buf);
-    }
-    }
-    
-    namespace leveldb {
-    }
-    
-    // A very simple random number generator.  Not especially good at
-// generating truly random bits, but good enough for our needs in this
-// package.
-class Random {
- private:
-  uint32_t seed_;
- public:
-  explicit Random(uint32_t s) : seed_(s & 0x7fffffffu) {
-    // Avoid bad seeds.
-    if (seed_ == 0 || seed_ == 2147483647L) {
-      seed_ = 1;
-    }
-  }
-  uint32_t Next() {
-    static const uint32_t M = 2147483647L;   // 2^31-1
-    static const uint64_t A = 16807;  // bits 14, 8, 7, 5, 2, 1, 0
-    // We are computing
-    //       seed_ = (seed_ * A) % M,    where M = 2^31-1
-    //
-    // seed_ must not be zero or M, or else all subsequent computed values
-    // will be zero or M respectively.  For all other values, seed_ will end
-    // up cycling through every number in [1,M-1]
-    uint64_t product = seed_ * A;
-    }
-    }
-    
-      inline void ClearSavedValue() {
-    if (saved_value_.capacity() > 1048576) {
-      std::string empty;
-      swap(empty, saved_value_);
-    } else {
-      saved_value_.clear();
-    }
-  }
-    
-    LookupKey::LookupKey(const Slice& user_key, SequenceNumber s) {
-  size_t usize = user_key.size();
-  size_t needed = usize + 13;  // A conservative estimate
-  char* dst;
-  if (needed <= sizeof(space_)) {
-    dst = space_;
-  } else {
-    dst = new char[needed];
-  }
-  start_ = dst;
-  dst = EncodeVarint32(dst, usize + 8);
-  kstart_ = dst;
-  memcpy(dst, user_key.data(), usize);
-  dst += usize;
-  EncodeFixed64(dst, PackSequenceAndType(s, kValueTypeForSeek));
-  dst += 8;
-  end_ = dst;
-}
-    
-        RecordType type;
-    const bool end = (left == fragment_length);
-    if (begin && end) {
-      type = kFullType;
-    } else if (begin) {
-      type = kFirstType;
-    } else if (end) {
-      type = kLastType;
-    } else {
-      type = kMiddleType;
-    }
-    
-    extern JSClass  *jsb_cocos2d_Physics3DPointToPointConstraint_class;
-extern JSObject *jsb_cocos2d_Physics3DPointToPointConstraint_prototype;
-    
-    
-    
-    
-    
-    int register_all_cocos2dx_experimental_video(lua_State* tolua_S);
-    
-    
-    
-    
-    
-    // This class implements debug drawing callbacks that are invoked
-// inside b2World::Step.
-class GLESDebugDraw : public b2Draw
-{
-    float32 mRatio;
-    cocos2d::GLProgram* mShaderProgram;
-    GLint        mColorLocation;
-    }
-    
-    
-    {
-    {			float minX = -6.0f;
-			float maxX = 0.0f;
-			float minY = 4.0f;
-			float maxY = 6.0f;
-			
-			for (int32 i = 0; i < 400; ++i)
-			{
-				b2BodyDef bd;
-				bd.type = b2_dynamicBody;
-				bd.position = b2Vec2(RandomFloat(minX,maxX),RandomFloat(minY,maxY));
-				b2Body* body = m_world->CreateBody(&bd);
-				body->CreateFixture(&shape, 0.01f);
-			}
+    static int make_prefixes(struct ifaddrs* ifaddr, int family, int prefixlen) {
+	char* prefix = NULL;
+	if (family == AF_INET) {
+		sockaddr_in* mask = new sockaddr_in;
+		mask->sin_family = AF_INET;
+		memset(&mask->sin_addr, 0, sizeof(in_addr));
+		ifaddr->ifa_netmask = reinterpret_cast<sockaddr*>(mask);
+		if (prefixlen > 32) {
+			prefixlen = 32;
 		}
-		
-		{
-			b2PolygonShape shape;
-			shape.SetAsBox(1.5f, 1.5f);
-			b2BodyDef bd;
-			bd.type = b2_dynamicBody;
-			bd.position.Set(-40.0f,5.0f);
-			bd.bullet = true;
-			b2Body* body = m_world->CreateBody(&bd);
-			body->CreateFixture(&shape, 1.0f);
-			body->SetLinearVelocity(b2Vec2(150.0f, 0.0f));
+		prefix = reinterpret_cast<char*>(&mask->sin_addr);
+	} else if (family == AF_INET6) {
+		sockaddr_in6* mask = new sockaddr_in6;
+		mask->sin6_family = AF_INET6;
+		memset(&mask->sin6_addr, 0, sizeof(in6_addr));
+		ifaddr->ifa_netmask = reinterpret_cast<sockaddr*>(mask);
+		if (prefixlen > 128) {
+			prefixlen = 128;
 		}
+		prefix = reinterpret_cast<char*>(&mask->sin6_addr);
+	} else {
+		return -1;
 	}
-    
-      void setDefaultCred(std::string user, std::string password);
-    
-      virtual void onCancelSendingPieceEvent(const BtCancelSendingPieceEvent& event)
-      CXX11_OVERRIDE
-  {
-  }
-    
-    AbstractHttpServerResponseCommand::AbstractHttpServerResponseCommand(
-    cuid_t cuid, const std::shared_ptr<HttpServer>& httpServer,
-    DownloadEngine* e, const std::shared_ptr<SocketCore>& socket)
-    : Command(cuid),
-      e_(e),
-      socket_(socket),
-      httpServer_(httpServer),
-      readCheck_(false),
-      writeCheck_(true)
-{
-  setStatus(Command::STATUS_ONESHOT_REALTIME);
-  e_->addSocketForWriteCheck(socket_, this);
+	for (int i = 0; i < (prefixlen / 8); i++) {
+		*prefix++ = 0xFF;
+	}
+	char remainder = 0xff;
+	remainder <<= (8 - prefixlen % 8);
+	*prefix = remainder;
+	return 0;
 }
     
-      virtual bool getChangeOption() const CXX11_OVERRIDE;
+            for (size_t i = 0; i < numSequences; ++i)
+        {
+            MaskKind firstMaskEntry = maskBuffer[i * maxNumTimeSteps];
+            if (firstMaskEntry == MaskKind::SequenceBegin)
+                sequenceBeginIndices[i] = 0;
+            else if (firstMaskEntry == MaskKind::Valid)
+                sequenceBeginIndices[i] = Microsoft::MSR::CNTK::SentinelValueIndicatingUnspecifedSequenceBeginIdx;
+            else
+                LogicError('The first entry of a Value mask must be Valid or SequenceBegin');
+    }
     
-    namespace aria2 {
+                m_gradHeader->numSamples = actualMBSize ? 1 : actualMBSize;
+            distGradAgg.AggregateGradients(learnParamsValues, m_gradHeader.get(), 0);
+    
+        static void SetTimestampingFlag()
+    {
+        auto& us = GetStaticInstance();
+        us.m_timestampFlag = true;
     }
     
     
-    {  virtual int64_t getTotalLength() CXX11_OVERRIDE;
-};
-    
-    void AnnounceTier::nextEvent()
-{
-  switch (event) {
-  case STARTED:
-    event = DOWNLOADING;
-    break;
-  case STARTED_AFTER_COMPLETION:
-    event = SEEDING;
-    break;
-  case STOPPED:
-    event = HALTED;
-    break;
-  case COMPLETED:
-    event = SEEDING;
-    break;
-  default:
-    break;
-  }
+    {  return scope.Close(Boolean::New(true));
 }
     
-      // Don't allow copying
-  AnnounceTier(const AnnounceTier&) = delete;
-  AnnounceTier& operator=(const AnnounceTier&) = delete;
+        // Helper methods
+    static bool HasFamilyNamed(std::string& name, DBWrapper* db);
+    static bool AddToBatch(rocksdb::WriteBatch& batch, bool del,
+        Handle<Array> array);
+    static bool AddToBatch(rocksdb::WriteBatch& batch, bool del,
+        Handle<Array> array, DBWrapper* db_wrapper, std::string cf);
+    static Handle<Value> CompactRangeDefault(const v8::Arguments& args);
+    static Handle<Value> CompactColumnFamily(const Arguments& args);
+    static Handle<Value> CompactOptions(const Arguments& args);
+    static Handle<Value> CompactAll(const Arguments& args);
     
-    class AuthConfig;
+    class StringAppendTESTOperator : public MergeOperator {
+ public:
+  // Constructor with delimiter
+  explicit StringAppendTESTOperator(char delim_char);
+    }
+    
+      // Extend record types with the following special values
+  enum {
+    kEof = kMaxRecordType + 1,
+    // Returned whenever we find an invalid physical record.
+    // Currently there are three situations in which this happens:
+    // * The record has an invalid CRC (ReadPhysicalRecord reports a drop)
+    // * The record is a 0-length record (No drop is reported)
+    // * The record is below constructor's initial_offset (No drop is reported)
+    kBadRecord = kMaxRecordType + 2,
+    // Returned when we fail to read a valid header.
+    kBadHeader = kMaxRecordType + 3,
+    // Returned when we read an old record from a previous user of the log.
+    kOldRecord = kMaxRecordType + 4,
+    // Returned when we get a bad record length
+    kBadRecordLen = kMaxRecordType + 5,
+    // Returned when we get a bad record checksum
+    kBadRecordChecksum = kMaxRecordType + 6,
+  };
+    
+    void SyncPoint::ClearTrace() {
+  impl_->ClearTrace();
+}
+    
+      const char* startUtf = start->c_str();
+  jstring jsStart = env->NewStringUTF(startUtf);
+  if(jsStart == nullptr) {
+    // unable to construct string
+    if(env->ExceptionCheck()) {
+      env->ExceptionDescribe(); // print out exception to stderr
+    }
+    releaseJniEnv(attached_thread);
+    return;
+  }
+  if(env->ExceptionCheck()) {
+    // exception thrown: OutOfMemoryError
+    env->ExceptionDescribe(); // print out exception to stderr
+    env->DeleteLocalRef(jsStart);
+    releaseJniEnv(attached_thread);
+    return;
+  }
+    
+    #include <jni.h>
+#include <memory>
+#include <string>
+#include 'rocksjni/jnicallback.h'
+#include 'rocksdb/comparator.h'
+#include 'rocksdb/slice.h'
+#include 'port/port.h'
+    
+    
+void SyncPoint::Data::LoadDependency(const std::vector<SyncPointPair>& dependencies) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  successors_.clear();
+  predecessors_.clear();
+  cleared_points_.clear();
+  for (const auto& dependency : dependencies) {
+    successors_[dependency.predecessor].push_back(dependency.successor);
+    predecessors_[dependency.successor].push_back(dependency.predecessor);
+  }
+  cv_.notify_all();
+}
+    
+      std::mutex              mutex_;
+  std::condition_variable cv_;
+  // sync points that have been passed through
+  std::unordered_set<std::string> cleared_points_;
+  std::atomic<bool> enabled_;
+  int num_callbacks_running_ = 0;
