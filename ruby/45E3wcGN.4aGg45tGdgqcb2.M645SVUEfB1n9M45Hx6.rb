00f2a20f4ce05fa511341c28dac6c630b1629fd1
SUNsung <weatherius@gmail.com>
2018-08-06 06:26:19 +0300

    Rewrite [45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb] File in: 2018-08-06 06:26:18

--- ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
+++ ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
@@ -1,64 +1,50 @@
 
         
-          if Rails.env == 'development'
-    test_conf = ActiveRecord::Base.configurations['test']
-    
-      def text_url
-    object.local? ? medium_url(object) : nil
+        unless dups.empty?
+  puts '\nDuplicate links:'
+  dups.each do |link|
+    puts '- #{link}'
+    puts `grep -nr '#{link}' README.md`
   end
+  puts '\nDone with errors.'
+  exit(1)
+end
     
-    class ActivityPub::EmojiSerializer < ActiveModel::Serializer
-  include RoutingHelper
+        private
     
-        # @param modifier [Array<String, Sass::Script::Tree::Node>] See \{#modifier}
-    # @param type [Array<String, Sass::Script::Tree::Node>] See \{#type}
-    # @param expressions [Array<Array<String, Sass::Script::Tree::Node>>] See \{#expressions}
-    def initialize(modifier, type, expressions)
-      @modifier = modifier
-      @type = type
-      @expressions = expressions
-    end
+      # Configure static asset server for tests with Cache-Control for performance.
+  if config.respond_to?(:serve_static_files)
+    # rails >= 4.2
+    config.serve_static_files = true
+  elsif config.respond_to?(:serve_static_assets)
+    # rails < 4.2
+    config.serve_static_assets = true
+  end
+  config.static_cache_control = 'public, max-age=3600'
     
-            def find_address
-          if @order.bill_address_id == params[:id].to_i
-            @order.bill_address
-          elsif @order.ship_address_id == params[:id].to_i
-            @order.ship_address
-          else
-            raise CanCan::AccessDenied
+    Then /^(?:|I )should see '([^']*)'$/ do |text|
+  expect(page).to have_content(text)
+end
+
+    
+            def matches? subject
+          @subject = subject
+          @subject = @subject.class unless Class === @subject
+          responds? && has_column?
+        end
+    
+            def failure_message
+          '#{expected_attachment}\n'.tap do |message|
+            message << accepted_types_and_failures.to_s
+            message << '\n\n' if @allowed_types.present? && @rejected_types.present?
+            message << rejected_types_and_failures.to_s
           end
         end
-      end
-    end
-  end
-end
-
     
-            protected
-    
-          class ValidateAttachmentContentTypeMatcher
-        def initialize attachment_name
-          @attachment_name = attachment_name
-          @allowed_types = []
-          @rejected_types = []
-        end
-    
-            def no_error_when_valid?
-          @file = StringIO.new('.')
-          @subject.send(@attachment_name).assign(@file)
+              @subject.send(@attachment_name).post_processing = false
+          @subject.send(@attachment_name).assign(file)
           @subject.valid?
-          expected_message = [
-            @attachment_name.to_s.titleize,
-            I18n.t(:blank, scope: [:errors, :messages])
-          ].join(' ')
-          @subject.errors.full_messages.exclude?(expected_message)
-        end
-      end
-    end
-  end
-end
-
-    
-            def lower_than_low?
-          @low.nil? || !passes_validation_with_size(@low - 1)
+          @subject.errors[:'#{@attachment_name}_file_size'].blank?
+        ensure
+          @subject.send(@attachment_name).post_processing = true
         end
