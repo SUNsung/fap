785f72521d35e6bb280d46a47c675e3e3e2e5281
SUNsung <weatherius@gmail.com>
2018-08-06 15:07:58 +0300

    Rewrite [261K.XZMFeaQ26K07zpEz0nQ265cu5RgcDxIs26gQa.cpp] File in: 2018-08-06 15:07:58

--- cccccc/261K.XZMFeaQ26K07zpEz0nQ265cu5RgcDxIs26gQa.cpp
+++ cccccc/261K.XZMFeaQ26K07zpEz0nQ265cu5RgcDxIs26gQa.cpp
@@ -1,143 +1,266 @@
 
         
-        #include 'base/basictypes.h'
-#include '../dispatcher_host.h'
-    
-      int id() const { return id_; }
-  std::string extension_id_;
-  ObjectManager* object_manager() const { return object_manager_.get(); }
-    
-    using content::RenderView;
-using content::RenderThread;
-using content::V8ValueConverter;
-using blink::WebFrame;
-using blink::WebLocalFrame;
-using blink::WebView;
-    
-    #ifndef CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
-#define CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
-    
-    void MenuItem::SetIconIsTemplate(bool isTemplate) {
+        base::StringPiece GetStringResource(int resource_id) {
+  return ResourceBundle::GetSharedInstance().GetRawDataResource(resource_id);
 }
     
-    bool NwClipboardSetListSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
-  std::unique_ptr<SetListSync::Params> params(SetListSync::Params::Create(*args_));
-  EXTENSION_FUNCTION_VALIDATE(params.get());
-  std::unique_ptr<ClipboardWriter> writer(new ClipboardWriter());
+    std::string Clipboard::GetText() {
+  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
+  base::string16 text;
+  clipboard->ReadText(ui::CLIPBOARD_TYPE_COPY_PASTE, &text);
+  return base::UTF16ToUTF8(text);
+}
+    
+    #include 'base/compiler_specific.h'
+#include 'content/nw/src/api/base/base.h'
+    
+    
+bool MenuDelegate::GetAcceleratorForCommandId(
+      int command_id,
+      ui::Accelerator* accelerator) const {
+  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
+  if (!item)
+    return false;
     }
     
-      // implement nw.Screen.registerStream()
-  class NwScreenRegisterStreamFunction : public NWSyncExtensionFunction {
-  public:
-    NwScreenRegisterStreamFunction();
-    bool RunNWSync(base::ListValue* response, std::string* error) override;
+      // implement nw.Screen.initEventListeners()
+  class NwScreenInitEventListenersFunction: public NWSyncExtensionFunction {
+    public:
+      NwScreenInitEventListenersFunction();
+      bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+        // allocate memory for all bnNodes evalOrder
+    m_net->AllocateAllMatrices(bnNodes, std::vector<ComputationNodeBasePtr>(), nullptr);
+    
+    // helper to remove all existing Output nodes and replace them by a new given set
+static void PatchOutputNodes(const ComputationNetworkPtr& net, const ConfigArray& outputNodeNames, vector<wstring>& outputNodeNamesVector)
+{
+    // clear out current list of outputNodes
+    while (!net->OutputNodes().empty())
+        net->RemoveFromNodeGroup(L'output', net->OutputNodes().back());
+    // and insert the desired nodes instead
+    for (wstring name : outputNodeNames)
+    {
+        if (!net->NodeNameExists(name))
+        {
+            fprintf(stderr, 'PatchOutputNodes: No node named '%ls'; skipping\n', name.c_str());
+            continue;
+        }
+        outputNodeNamesVector.push_back (name);
+        let& node = net->GetNodeFromName(name);
+        net->AddToNodeGroup(L'output', node);
+    }
+}
+    
+        static void SetTimestampingFlag()
+    {
+        auto& us = GetStaticInstance();
+        us.m_timestampFlag = true;
     }
     
     
-    {    if (s.ok()) {
-      // Verify that the table is usable
-      Iterator* it = table_cache->NewIterator(ReadOptions(),
-                                              meta->number,
-                                              meta->file_size);
-      s = it->status();
-      delete it;
+    {public:
+    inline array_ref(_T* ptr, size_t size) throw()
+        : data(ptr), n(size)
+    {
     }
-  }
-    
-    
-    {}  // namespace leveldb
-    
-     private:
-  // We construct a char array of the form:
-  //    klength  varint32               <-- start_
-  //    userkey  char[klength]          <-- kstart_
-  //    tag      uint64
-  //                                    <-- end_
-  // The array is a suitable MemTable key.
-  // The suffix starting with 'userkey' can be used as an InternalKey.
-  const char* start_;
-  const char* kstart_;
-  const char* end_;
-  char space_[200];      // Avoid allocation for short keys
-    
-    #endif  // STORAGE_LEVELDB_DB_TABLE_CACHE_H_
-
-    
-        if (bytes_ > 0) {
-      char rate[100];
-      snprintf(rate, sizeof(rate), '%6.1f MB/s',
-               (bytes_ / 1048576.0) / (finish - start_));
-      if (!message_.empty()) {
-        message_  = std::string(rate) + ' ' + message_;
-      } else {
-        message_ = rate;
-      }
+    inline array_ref() throw()
+        : data(NULL), n(0)
+    {
+    } // in case we have a vector of this
+    inline _T& operator[](size_t i) throw()
+    {
+        check_index(i);
+        return data[i];
     }
-    
-    #include <poll.h>
-#include <pwd.h>
-    
-    class RocksDBDatabasePluginTests : public DatabasePluginTests {
- protected:
-  std::string name() override {
-    return 'rocksdb';
-  }
+    inline const _T& operator[](size_t i) const throw()
+    {
+        check_index(i);
+        return data[i];
+    }
+    inline size_t size() const throw()
+    {
+        return n;
+    }
+    inline _T* begin()
+    {
+        return data;
+    }
+    inline _T* end()
+    {
+        return data + n;
+    }
+    inline void resize(size_t sz)
+    {
+        sz;
+        assert(n == sz);
+    } // allow compatibility with some functions
+    // construct from other vector types
+    template <class _V>
+    inline array_ref(_V& v)
+        : data(v.size() > 0 ? &v[0] : NULL), n((size_t) v.size())
+    {
+    }
 };
     
-    /**
- * @brief Generate the separator string for query results
- *
- * @param lengths The data returned from computeQueryDataLengths
- * @param columns The order of the keys (since maps are unordered)
- *
- * @return A string, with a newline, representing your separator
- */
-std::string generateToken(const std::map<std::string, size_t>& lengths,
-                          const std::vector<std::string>& columns);
-    
-    
-    {  return Status(0, 'OK');
-}
-    
-    #include <chrono>
-#include <memory>
-#include <vector>
-    
-    #include <string>
-    
-    void AbstractBtMessage::setBtRequestFactory(BtRequestFactory* factory)
-{
-  requestFactory_ = factory;
-}
-    
-      BtMessageFactory* messageFactory_;
-    
-    namespace aria2 {
+        // transpose a 4x4 matrix
+    // Passing input as const ref to ensure aligned-ness
+    static void transpose(const float4& col0, const float4& col1, const float4& col2, const float4& col3,
+                          float4& row0, float4& row1, float4& row2, float4& row3)
+    { // note: the temp variable here gets completely eliminated by optimization
+        float4 m0 = col0;
+        float4 m1 = col1;
+        float4 m2 = col2;
+        float4 m3 = col3;
+        _MM_TRANSPOSE4_PS(m0, m1, m2, m3); // 8 instructions for 16 elements
+        row0 = m0;
+        row1 = m1;
+        row2 = m2;
+        row3 = m3;
     }
     
-    #include <memory>
-    
-      virtual std::string toTagString() const CXX11_OVERRIDE;
-    
-    #include 'AbstractCommand.h'
-    
-    
-    {} // namespace aria2
-    
-    AnnounceList::AnnounceList() : currentTrackerInitialized_(false) {}
-    
-    namespace aria2 {
+        virtual void GetSections(std::map<std::wstring, SectionType, nocase_compare>& /*sections*/)
+    {
+        assert(false);
+        NOT_IMPLEMENTED;
+    }
+    virtual bool SaveData(size_t /*recordStart*/, const std::map<std::wstring, void*, nocase_compare>& matrices, size_t numRecords, size_t /*datasetSize*/, size_t /*byteVariableSized*/)
+    {
+        // loop through all the output vectors to copy the data over
+        for (auto iter = m_outputs->begin(); iter != m_outputs->end(); ++iter)
+        {
+            // figure out the dimension of the data
+            std::wstring val = iter->first;
+            size_t rows = (*m_dimensions)[val];
+            // size_t count = rows*numRecords;
+    }
     }
     
-    class ApiCallbackDownloadEventListener : public DownloadEventListener {
-public:
-  ApiCallbackDownloadEventListener(Session* session,
-                                   DownloadEventCallback callback,
-                                   void* userData);
-  virtual ~ApiCallbackDownloadEventListener();
-  virtual void onEvent(DownloadEvent event,
-                       const RequestGroup* group) CXX11_OVERRIDE;
+      /// Cancels one asynchronous operation that is waiting on the timer.
+  /**
+   * This function forces the completion of one pending asynchronous wait
+   * operation against the timer. Handlers are cancelled in FIFO order. The
+   * handler for the cancelled operation will be invoked with the
+   * boost::asio::error::operation_aborted error code.
+   *
+   * Cancelling the timer does not change the expiry time.
+   *
+   * @param ec Set to indicate what error occurred, if any.
+   *
+   * @return The number of asynchronous operations that were cancelled. That is,
+   * either 0 or 1.
+   *
+   * @note If the timer has already expired when cancel_one() is called, then
+   * the handlers for asynchronous wait operations will:
+   *
+   * @li have already been invoked; or
+   *
+   * @li have been queued for invocation in the near future.
+   *
+   * These handlers can no longer be cancelled, and therefore are passed an
+   * error code that indicates the successful completion of the wait operation.
+   */
+  std::size_t cancel_one(boost::system::error_code& ec)
+  {
+    return this->service.cancel_one(this->implementation, ec);
+  }
+    
+    #ifndef BOOST_ASIO_BUFFERED_READ_STREAM_HPP
+#define BOOST_ASIO_BUFFERED_READ_STREAM_HPP
+    
+    template <typename Stream>
+class buffered_stream;
+    
+    namespace boost {
+    }
+    
+    #include <boost/asio/detail/config.hpp>
+    
+    #include <boost/asio/detail/push_options.hpp>
+    
+    namespace boost {
+namespace asio {
+namespace detail {
+    }
+    }
     }
     
     
-    {} // namespace aria2
+    {
+    {
+    {} // namespace detail
+} // namespace asio
+} // namespace boost
+    
+    #if !defined(BOOST_ASIO_HAS_THREADS) \
+  || defined(BOOST_ASIO_DISABLE_FENCED_BLOCK)
+typedef null_fenced_block fenced_block;
+#elif defined(__MACH__) && defined(__APPLE__)
+typedef macos_fenced_block fenced_block;
+#elif defined(__sun)
+typedef solaris_fenced_block fenced_block;
+#elif defined(__GNUC__) && defined(__arm__) \
+  && !defined(__GCC_HAVE_SYNC_COMPARE_AND_SWAP_4)
+typedef gcc_arm_fenced_block fenced_block;
+#elif defined(__GNUC__) && (defined(__hppa) || defined(__hppa__))
+typedef gcc_hppa_fenced_block fenced_block;
+#elif defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
+typedef gcc_x86_fenced_block fenced_block;
+#elif defined(__GNUC__) \
+  && ((__GNUC__ == 4 && __GNUC_MINOR__ >= 1) || (__GNUC__ > 4)) \
+  && !defined(__INTEL_COMPILER) && !defined(__ICL) \
+  && !defined(__ICC) && !defined(__ECC) && !defined(__PATHSCALE__)
+typedef gcc_sync_fenced_block fenced_block;
+#elif defined(BOOST_ASIO_WINDOWS) && !defined(UNDER_CE)
+typedef win_fenced_block fenced_block;
+#else
+typedef null_fenced_block fenced_block;
+#endif
+    
+      /*
+  bool predict_velocity(
+      const cv::Mat &frame, const std::vector<std::shared_ptr<VisualObject>>
+  &objects,
+      double timestamp, const CameraTrackerOptions &options,
+      std::vector<std::shared_ptr<VisualObject>> *tracked_objects) override {
+    *tracked_objects = objects;
+    this->trans_object_to_world(options, tracked_objects);
+    return true;
+  }
+    
+    // The base class of transforming camera space objects into other defined
+// 3D spaces, like world space or ego-car space
+    
+    #ifndef MODULES_COMMON_MATH_MATRIX_OPERATIONS_H_
+#define MODULES_COMMON_MATH_MATRIX_OPERATIONS_H_
+    
+      Eigen::Matrix<float, 1, 2> m_c = Eigen::MatrixXf::Ones(1, 2);
+    
+      auto matched_it = std::lower_bound(trajectory.trajectory_point().begin(),
+      trajectory.trajectory_point().end(), t,
+      [](const common::TrajectoryPoint& p, const double t)
+      { return p.relative_time() < t; });
+    
+    class PredictionQuerier {
+ public:
+  explicit PredictionQuerier(
+      const std::vector<const Obstacle*>& obstacles,
+      const std::shared_ptr<std::vector<common::PathPoint>>&
+      ptr_reference_line);
+    }
+    
+    namespace apollo {
+namespace perception {
+namespace traffic_light {
+    }
+    }
+    }
+    
+    #include 'modules/perception/onboard/common_shared_data.h'
+#include 'modules/perception/onboard/subnode.h'
+#include 'modules/perception/traffic_light/interface/base_recognizer.h'
+#include 'modules/perception/traffic_light/interface/base_rectifier.h'
+#include 'modules/perception/traffic_light/interface/base_reviser.h'
+#include 'modules/perception/traffic_light/interface/green_interface.h'
+#include 'modules/perception/traffic_light/projection/multi_camera_projection.h'
