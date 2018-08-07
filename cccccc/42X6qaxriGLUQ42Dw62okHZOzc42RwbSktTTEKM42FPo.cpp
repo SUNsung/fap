733234ce7ae65d3c568019f23db0fb1e25cf2395
SUNsung <weatherius@gmail.com>
2018-08-08 02:59:15 +0300

    Rewrite [42X6qaxriGLUQ42Dw62okHZOzc42RwbSktTTEKM42FPo.cpp] File in: 2018-08-08 02:59:14

--- cccccc/42X6qaxriGLUQ42Dw62okHZOzc42RwbSktTTEKM42FPo.cpp
+++ cccccc/42X6qaxriGLUQ42Dw62okHZOzc42RwbSktTTEKM42FPo.cpp
@@ -1,97 +1,203 @@
 
         
-        
-    {}
+          // JsAsker:
+  void StartAsync(std::unique_ptr<base::Value> options) override;
     
-      if (flags & TrackVtsc) {
-    flags |= TrackCPU;
+    class WindowListObserver {
+ public:
+  // Called immediately after a window is added to the list.
+  virtual void OnWindowAdded(NativeWindow* window) {}
+    }
+    
+    #include 'atom/browser/api/event_emitter.h'
+    
+        for (UINT driverTypeIndex = 0; driverTypeIndex < numDriverTypes; driverTypeIndex++)
+    {
+        g_driverType = driverTypes[driverTypeIndex];
+        hr = D3D11CreateDeviceAndSwapChain(NULL, g_driverType, NULL, createDeviceFlags, featureLevels, numFeatureLevels,
+                D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &g_featureLevel, &g_pImmediateContext);
+        if (SUCCEEDED(hr))
+            break;
+    }
+    if (FAILED(hr))
+        return hr;
+    
+    
+    {    IntrinsicParams();
+    IntrinsicParams(Vec2d f, Vec2d c, Vec4d k, double alpha = 0);
+    IntrinsicParams operator+(const Mat& a);
+    IntrinsicParams& operator =(const Mat& a);
+    void Init(const cv::Vec2d& f, const cv::Vec2d& c, const cv::Vec4d& k = Vec4d(0,0,0,0), const double& alpha = 0);
+};
+    
+    namespace cv { namespace ocl { namespace runtime {
+    }
+    }
+    }
+    
+      Datum datum;
+  datum.set_channels(1);
+  datum.set_height(rows);
+  datum.set_width(cols);
+  LOG(INFO) << 'A total of ' << num_items << ' items.';
+  LOG(INFO) << 'Rows: ' << rows << ' Cols: ' << cols;
+  for (int item_id = 0; item_id < num_items; ++item_id) {
+    image_file.read(pixels, rows * cols);
+    label_file.read(&label, 1);
+    datum.set_data(pixels, rows*cols);
+    datum.set_label(label);
+    string key_str = caffe::format_int(item_id, 8);
+    datum.SerializeToString(&value);
+    }
+    
+    
+    {  static string LayerTypeListString() {
+    vector<string> layer_types = LayerTypeList();
+    string layer_types_str;
+    for (vector<string>::iterator iter = layer_types.begin();
+         iter != layer_types.end(); ++iter) {
+      if (iter != layer_types.begin()) {
+        layer_types_str += ', ';
+      }
+      layer_types_str += *iter;
+    }
+    return layer_types_str;
   }
+};
     
-    SlowTimer::SlowTimer(int64_t msThreshold, const char *location, const char *info)
-  : m_timer(Timer::WallTime), m_msThreshold(msThreshold) {
-  if (location) m_location = location;
-  if (info) m_info = info;
+    #include <vector>
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    
+    {}  // namespace caffe
+    
+     protected:
+  /**
+   * @param bottom input Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$
+   * @param top output Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the computed outputs @f$
+   *        y = \left\{
+   *        \begin{array}{lr}
+   *            x                  & \mathrm{if} \; x > 0 \\
+   *            \alpha (\exp(x)-1) & \mathrm{if} \; x \le 0
+   *        \end{array} \right.
+   *      @f$.  
+   */
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    }
+    
+    namespace caffe {
+    }
+    
+    // Tests that the RotatingTranspose function does the right thing for various
+// transformations.
+// dims=[5, 4, 3, 2]->[5, 2, 4, 3]
+TEST_F(MatrixTest, RotatingTranspose_3_1) {
+  GENERIC_2D_ARRAY<int> m;
+  src_.RotatingTranspose(dims_, kNumDims_, 3, 1, &m);
+  m.ResizeNoInit(kInputSize_ / 3, 3);
+  // Verify that the result is:
+  // output tensor=[[[[0, 2, 4][6, 8, 10][12, 14, 16][18, 20, 22]]
+  //                 [[1, 3, 5][7, 9, 11][13, 15, 17][19, 21, 23]]]
+  //                [[[24, 26, 28]...
+  EXPECT_EQ(0, m(0, 0));
+  EXPECT_EQ(2, m(0, 1));
+  EXPECT_EQ(4, m(0, 2));
+  EXPECT_EQ(6, m(1, 0));
+  EXPECT_EQ(1, m(4, 0));
+  EXPECT_EQ(24, m(8, 0));
+  EXPECT_EQ(26, m(8, 1));
+  EXPECT_EQ(25, m(12, 0));
 }
     
-    ///////////////////////////////////////////////////////////////////////////////
+    #ifndef TESSERACT_CCUTIL_UNICODES_H_
+#define TESSERACT_CCUTIL_UNICODES_H_
     
-    //////////////////////////////////////////////////////////////////////
+    /**----------------------------------------------------------------------------
+          Include Files and Type Defines
+----------------------------------------------------------------------------**/
+#include 'strngs.h'
     
-    namespace HPHP {
-    }
-    
-    struct StringDataHashCompare {
-  bool equal(const StringData *s1, const StringData *s2) const {
-    assertx(s1 && s2);
-    return s1->same(s2);
-  }
-  size_t hash(const StringData *s) const {
-    assertx(s);
-    return s->hash();
-  }
-};
-    
-      auto map_entry = weakmap->find((uintptr_t)obj.get());
-  if (map_entry != weakmap->end()) {
-    wr_data = map_entry->second.lock();
-  } else {
-    wr_data = req::make_shared<WeakRefData>(make_tv<KindOfObject>(obj.get()));
-    }
-    
-    
-    {  static constexpr Native::PropAccessorMap& map =
-    reflection_extension_accessorsMap;
-};
-    
-      unsigned num1GPages = RuntimeOption::EvalNum1GPagesForSlabs;
-  unsigned num2MPages = RuntimeOption::EvalNum2MPagesForSlabs;
-  if (numNodes > 1) {
-    auto perNode1G = num1GPages / numNodes;
-    if (auto const excessive = num1GPages % numNodes) {
-      Logger::Warning('uneven distribution of %u 1G huge pages '
-                      '(Eval.Num1GPagesForSlabs) to %u NUMA nodes, '
-                      'excessive %u pages not used',
-                      num1GPages, numNodes, excessive);
-    }
-    num1GPages = perNode1G;
-    auto perNode2M = num2MPages / numNodes;
-    if (auto const excessive = num2MPages % numNodes) {
-      Logger::Warning('uneven distribution of %u 2M huge pages '
-                      '(Eval.Num1MPagesForSlabs) to %u NUMA nodes, '
-                      'excessive %u pages not used',
-                      num2MPages, numNodes, excessive);
-    }
-    num2MPages = perNode2M;
+      // If we manage the given dawg, decrement its count,
+  // and possibly delete it if the count reaches zero.
+  // If dawg is unknown to us, return false.
+  bool FreeDawg(Dawg *dawg) {
+    return dawgs_.Free(dawg);
   }
     
-    private:
-  std::string userDefinedUser_;
-  std::string userDefinedPassword_;
+    // Non-linearity (sigmoid) functions with cache tables and clipping.
+inline double Tanh(double x) {
+  if (x < 0.0) return -Tanh(-x);
+  if (x >= (kTableSize - 1) / kScaleFactor) return 1.0;
+  x *= kScaleFactor;
+  int index = static_cast<int>(floor(x));
+  if (TanhTable[index] == 0.0 && index > 0) {
+    // Generate the entry.
+    TanhTable[index] = tanh(index / kScaleFactor);
+  }
+  if (index == kTableSize - 1) return TanhTable[kTableSize - 1];
+  if (TanhTable[index + 1] == 0.0) {
+    // Generate the entry.
+    TanhTable[index + 1] = tanh((index + 1) / kScaleFactor);
+  }
+  double offset = x - index;
+  return TanhTable[index] * (1.0 - offset) + TanhTable[index + 1] * offset;
+}
+    
+      // The constraint list is a simple struct.
+  cl.add(constraint);
+  EXPECT_EQ(cl.constraints_.size(), 1U);
+    
+    /// Select all from a table an exit the shell.
+DECLARE_string(A);
+    
+    TEST_F(PrinterTests, test_unicode) {
+  Row r = {{'name', 'Àlex Smith'}};
+  std::map<std::string, size_t> lengths;
+  computeRowLengths(r, lengths);
+    }
     
     
-    {} // namespace aria2
+    {BENCHMARK(PLIST_parse_file);
+}
 
     
-      enum Flag {
-    FLAG_HIDDEN = 1,
-    FLAG_ERASE_AFTER_PARSE = 1 << 1,
-    FLAG_INITIAL_OPTION = 1 << 2,
-    FLAG_CHANGE_OPTION = 1 << 3,
-    FLAG_CHANGE_OPTION_FOR_RESERVED = 1 << 4,
-    FLAG_CHANGE_GLOBAL_OPTION = 1 << 5,
-    FLAG_CUMULATIVE = 1 << 6
-  };
+      // Use memmap for maximum portability over read().
+  auto map = mmap(0, offset + length, PROT_READ, MAP_SHARED, fd, base - offset);
+  if (map == MAP_FAILED) {
+    // Could fallback to a lseek/read.
+    if (!readMem(fd, base, length, (uint8_t*)*buffer).ok()) {
+      close(fd);
+      free(*buffer);
+      *buffer = nullptr;
+      return Status(1, 'Cannot memory map or seek/read memory');
+    }
+  } else {
+    // Memory map succeeded, copy and unmap.
+    memcpy(*buffer, (uint8_t*)map + offset, length);
+    if (munmap(map, offset + length) == -1) {
+      LOG(WARNING) << 'Unable to unmap raw memory';
+    }
+  }
     
-    AbstractProxyRequestCommand::~AbstractProxyRequestCommand() = default;
+    size_t FirehoseLogForwarder::getMaxRecordsPerBatch() const {
+  return 500U;
+}
     
-    class RequestGroup;
-class DownloadEngine;
-class Peer;
-class BtRuntime;
-class PieceStorage;
-class PeerStorage;
-class BtAnnounce;
+    namespace osquery {
+    }
     
-      virtual bool finished() CXX11_OVERRIDE;
-    
-    #include 'Notifier.h'
+      bool usesLogStatus() override {
+    return true;
+  }
