51986af5e5fe122173a4e2516c4e5e2951ccad09
SUNsung <weatherius@gmail.com>
2018-08-07 08:46:32 +0300

    Rewrite [288QKMJSg0BE28y5l2IlotIfU280en1k8PXX3s28v.S.py] File in: 2018-08-07 08:46:31

--- pyton/288QKMJSg0BE28y5l2IlotIfU280en1k8PXX3s28v.S.py
+++ pyton/288QKMJSg0BE28y5l2IlotIfU280en1k8PXX3s28v.S.py
@@ -1,149 +1,79 @@
 
         
         
-username = get_config('ANSIBLE_OPENSHIFT_USERNAME', 'default_rhlogin')
-password = get_config('ANSIBLE_OPENSHIFT_PASSWORD', 'password')
-broker_url = 'https://%s/broker/rest/' % get_config('ANSIBLE_OPENSHIFT_BROKER', 'libra_server')
+class MethodView(with_metaclass(MethodViewType, View)):
+    '''A class-based view that dispatches request methods to the corresponding
+    class methods. For example, if you implement a ``get`` method, it will be
+    used to handle ``GET`` requests. ::
     
-                for item in items:
-                contents = item['contents'].encode('utf-8')
-                path = output_dir + '/' + re.sub('^/*', '', item['path'])
+    from httpie.compat import is_windows
+from httpie.config import DEFAULT_CONFIG_DIR, Config
     
-        def construct_yaml_unsafe(self, node):
-        return self.construct_yaml_str(node, unsafe=True)
-    
-    from ansible.compat.tests import unittest
-from ansible.compat.tests.mock import MagicMock
-    
-    n_samples = np.logspace(.5, 3, 9)
-n_features = np.logspace(1, 3.5, 7)
-N_samples, N_features = np.meshgrid(n_samples,
-                                    n_features)
-scikits_time = np.zeros(N_samples.shape)
-scipy_time = np.zeros(N_samples.shape)
-    
-        op.add_option('--n-population',
-                  dest='n_population', default=100000, type=int,
-                  help='Size of the population to sample from.')
-    
-    import codecs
+        def __init__(self, chunk_size=CHUNK_SIZE, **kwargs):
+        super(RawStream, self).__init__(**kwargs)
+        self.chunk_size = chunk_size
     
     
-plt.show()
-
+def test_follow_all_redirects_shown(httpbin):
+    r = http('--follow', '--all', httpbin.url + '/redirect/2')
+    assert r.count('HTTP/1.1') == 3
+    assert r.count('HTTP/1.1 302 FOUND', 2)
+    assert HTTP_OK in r
     
-    COLORS = np.array(['!',
-                   '#FF3333',  # red
-                   '#0198E1',  # blue
-                   '#BF5FFF',  # purple
-                   '#FCD116',  # yellow
-                   '#FF7216',  # orange
-                   '#4DBD33',  # green
-                   '#87421F'   # brown
-                   ])
+    # def test_unicode_url_verbose(self):
+#     r = http(httpbin.url + '--verbose', u'/get?test=' + UNICODE)
+#     assert HTTP_OK in r
     
-        def test_get_group_cfws_only(self):
-        group = self._test_get_x(parser.get_group,
-            'Monty Python: (hidden);',
-            'Monty Python: (hidden);',
-            'Monty Python: ;',
-            [],
-            '')
-        self.assertEqual(group.token_type, 'group')
-        self.assertEqual(group.display_name, 'Monty Python')
-        self.assertEqual(len(group.mailboxes), 0)
-        self.assertEqual(group.mailboxes,
-                         group.all_mailboxes)
+        >>> humanize_bytes(1)
+    '1 B'
+    >>> humanize_bytes(1024, precision=1)
+    '1.0 kB'
+    >>> humanize_bytes(1024 * 123, precision=1)
+    '123.0 kB'
+    >>> humanize_bytes(1024 * 12342, precision=1)
+    '12.1 MB'
+    >>> humanize_bytes(1024 * 12342, precision=2)
+    '12.05 MB'
+    >>> humanize_bytes(1024 * 1234, precision=2)
+    '1.21 MB'
+    >>> humanize_bytes(1024 * 1234 * 1111, precision=2)
+    '1.31 GB'
+    >>> humanize_bytes(1024 * 1234 * 1111, precision=1)
+    '1.3 GB'
     
-                self.assertTrue(ntpath.ismount('\\\\localhost\\c$'))
-            self.assertTrue(ntpath.ismount('\\\\localhost\\c$\\'))
+        def test_binary_suppresses_when_not_terminal_but_pretty(self):
+        env = MockEnvironment(stdin_isatty=True, stdout_isatty=False)
+        r = http('--pretty=all', 'GET', self.url,
+                 env=env)
+        assert BINARY_SUPPRESSED_NOTICE.decode() in r
     
-        def testFabs(self):
-        self.assertRaises(TypeError, math.fabs)
-        self.ftest('fabs(-1)', math.fabs(-1), 1)
-        self.ftest('fabs(0)', math.fabs(0), 0)
-        self.ftest('fabs(1)', math.fabs(1), 1)
+        config['implicit_content_type'] = 'json'
+    config.save()
+    config.load()
+    assert 'implicit_content_type' not in config
+    assert not config['default_options']
     
-    def does_tree_import(package, name, node):
-    ''' Returns true if name is imported from package at the
-        top level of the tree which node belongs to.
-        To cover the case of an import like 'import foo', use
-        None for the package and 'foo' for the name. '''
-    binding = find_binding(name, find_root(node), package)
-    return bool(binding)
+        def test_colors_option(self, httpbin):
+        env = MockEnvironment(colors=256)
+        r = http('--print=B', '--pretty=colors',
+                 'GET', httpbin.url + '/get', 'a=b',
+                 env=env)
+        # Tests that the JSON data isn't formatted.
+        assert not r.strip().count('\n')
+        assert COLOR in r
     
-        PATTERN = '''
-    power< head=any+
-         trailer< '.' method=('keys'|'items'|'values'|
-                              'iterkeys'|'iteritems'|'itervalues'|
-                              'viewkeys'|'viewitems'|'viewvalues') >
-         parens=trailer< '(' ')' >
-         tail=any*
-    >
-    '''
+        for test in tests:
+      yield MessageTest, test[ 0 ], test[ 1 ]
     
-        def test_get_builtin_constructor(self):
-        get_builtin_constructor = getattr(hashlib,
-                                          '__get_builtin_constructor')
-        builtin_constructor_cache = getattr(hashlib,
-                                            '__builtin_constructor_cache')
-        self.assertRaises(ValueError, get_builtin_constructor, 'test')
-        try:
-            import _md5
-        except ImportError:
-            self.skipTest('_md5 module not available')
-        # This forces an ImportError for 'import _md5' statements
-        sys.modules['_md5'] = None
-        # clear the cache
-        builtin_constructor_cache.clear()
-        try:
-            self.assertRaises(ValueError, get_builtin_constructor, 'md5')
-        finally:
-            if '_md5' in locals():
-                sys.modules['_md5'] = _md5
-            else:
-                del sys.modules['_md5']
-        self.assertRaises(TypeError, get_builtin_constructor, 3)
-        constructor = get_builtin_constructor('md5')
-        self.assertIs(constructor, _md5.md5)
-        self.assertEqual(sorted(builtin_constructor_cache), ['MD5', 'md5'])
+      AssertBuffersAreEqualAsBytes( [ 'aEb',
+                                  'bFb',
+                                  'bGbcccc',
+                                  'aBa',
+                                  'aCa' ], result_buffer )
     
-    
-class Action(object):
-    
-        def __init__(self):
-        self.msg_queue = []
-        self.subscribers = {}
-    
-    
-@gen.engine
-def run_tests():
-    url = options.url + '/getCaseCount'
-    control_ws = yield websocket_connect(url, None)
-    num_tests = int((yield control_ws.read_message()))
-    logging.info('running %d cases', num_tests)
-    msg = yield control_ws.read_message()
-    assert msg is None
-    
-    # Repeat the entire benchmark this many times (on different ports)
-# This gives JITs time to warm up, etc.  Pypy needs 3-5 runs at
-# --n=15000 for its JIT to reach full effectiveness
-define('num_runs', type=int, default=1)
-    
-    from tornado.options import options, define, parse_command_line
-from tornado.template import Template
-    
-    latex_documents = [
-    ('index', 'tornado.tex', 'Tornado Documentation', 'The Tornado Authors', 'manual', False),
-]
-    
-        def run(self):
-        try:
-            build_ext.run(self)
-        except Exception:
-            e = sys.exc_info()[1]
-            sys.stdout.write('%s\n' % str(e))
-            warnings.warn(self.warning_message % ('Extension modules',
-                                                  'There was an issue with '
-                                                  'your platform configuration'
-                                                  ' - see above.'))
+      try:
+    filepath = os.path.join( DIR_OF_YCMD, 'PYTHON_USED_DURING_BUILDING' )
+    return utils.ReadFile( filepath ).strip()
+  # We need to check for IOError for Python2 and OSError for Python3
+  except ( IOError, OSError ):
+    return None
