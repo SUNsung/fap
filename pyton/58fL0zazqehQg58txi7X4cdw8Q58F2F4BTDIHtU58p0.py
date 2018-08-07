1ea5a23f5b90fe26ec8799d81443ff4b3ee4b3f4
SUNsung <weatherius@gmail.com>
2018-08-08 00:29:06 +0300

    Rewrite [58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py] File in: 2018-08-08 00:29:06

--- pyton/58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py
+++ pyton/58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py
@@ -1,199 +1,87 @@
 
         
-            # Then we cluster the lines together as blocks
-    # Each block represents a collection of lines that should be sorted
-    # This was done by assuming only links ([...](...)) are meant to be sorted
-    # Clustering is done by indentation
-    blocks = []
-    last_indent = None
-    for line in read_me:
-        s_line = line.lstrip()
-        indent = len(line) - len(s_line)
+            def setUp(self):
+        if self._SKIP_SOCKS_TEST:
+            return
     
-    tf_gen_op_libs(
-    op_lib_names = ['mst_ops'],
-)
-    
-    py_test(
-    name = 'sentence_io_test',
-    srcs = ['sentence_io_test.py'],
-    data = ['//syntaxnet:testdata'],
-    deps = [
-        ':dragnn_ops',
-        ':sentence_io',
-        '//syntaxnet:load_parser_ops_py',
-        '//syntaxnet:parser_ops',
-        '//syntaxnet:sentence_pb2_py',
-        '//syntaxnet:test_flags',
-        '@org_tensorflow//tensorflow:tensorflow_py',
-        '@org_tensorflow//tensorflow/core:protos_all_py',
-    ],
-)
-    
-      num_labels = weights.get_shape().as_list()[0]
-  num_source_activations = weights.get_shape().as_list()[1]
-  num_target_activations = weights.get_shape().as_list()[2]
-  check.NotNone(num_labels, 'unknown number of labels')
-  check.NotNone(num_source_activations, 'unknown source activation dimension')
-  check.NotNone(num_target_activations, 'unknown target activation dimension')
-  check.Eq(sources.get_shape().as_list()[2], num_source_activations,
-           'activation mismatch between weights and source tokens')
-  check.Eq(targets.get_shape().as_list()[2], num_target_activations,
-           'activation mismatch between weights and target tokens')
-    
-          roots = digraph_ops.RootPotentialsFromTokens(root, tokens, weights_arc,
-                                                   weights_source)
-    
-      short_to_original = saver_lib.shorten_resource_paths(master_spec)
-  saver_lib.export_master_spec(master_spec, graph)
-  saver_lib.export_to_graph(master_spec, params_path, stripped_path, graph,
-                            export_moving_averages, build_runtime_graph)
-  saver_lib.export_assets(master_spec, short_to_original, stripped_path)
-    
-        shortened_to_original = dragnn_model_saver_lib.shorten_resource_paths(
-        master_spec)
-    
-      The partition function is caluclated via application of the Matrix-Tree
-  theorem; see the following for details:
-    https://en.wikipedia.org/wiki/Kirchhoff%27s_theorem
-    http://www.aclweb.org/anthology/D/D07/D07-1015.pdf
-    
-        styles = {
-        # No corresponding class for the following:
-        #Text:                     '', # class:  ''
-        Whitespace:                'underline #f8f8f8',      # class: 'w'
-        Error:                     '#a40000 border:#ef2929', # class: 'err'
-        Other:                     '#000000',                # class 'x'
-    }
-    
-    Provides utility functions that are consumed internally by Requests
-which depend on extremely few external helpers (such as compat)
-'''
-    
-        @pytest.mark.parametrize(
-        'other, result', (
-            ({'AccePT': 'application/json'}, True),
-            ({}, False),
-            (None, False)
-        )
-    )
-    def test_instance_equality(self, other, result):
-        assert (self.case_insensitive_dict == other) is result
-    
-            # Give the Response some context.
-        response.request = req
-        response.connection = self
-    
-            req = requests.Request('GET', httpbin('headers'))
-        assert not req.auth
-    
-        text_200 = (b'HTTP/1.1 200 OK\r\n'
-                b'Content-Length: 0\r\n\r\n')
-    
-    if len(sys.argv) == 2 and sys.argv[1] == '--list':
-    print(json.dumps(result))
-elif len(sys.argv) == 3 and sys.argv[1] == '--host':
-    print(json.dumps({}))
-else:
-    print('Need an argument, either --list or --host <host>')
-
+                    fn = os.path.join(dirpath, basename)
+                with io.open(fn, encoding='utf-8') as inf:
+                    code = inf.read()
     
     
-def md5(filename):
-    if not _md5:
-        raise ValueError('MD5 not available.  Possibly running in FIPS mode')
-    return secure_hash(filename, _md5)
-
+class RtspFD(FileDownloader):
+    def real_download(self, filename, info_dict):
+        url = info_dict['url']
+        self.report_destination(filename)
+        tmpfilename = self.temp_name(filename)
     
     try:
-    import argcomplete
+    from .lazy_extractors import *
+    from .lazy_extractors import _ALL_CLASSES
+    _LAZY_LOADER = True
 except ImportError:
-    argcomplete = None
+    _LAZY_LOADER = False
+    from .extractors import *
     
-        def construct_mapping(self, node, deep=False):
-        # Most of this is from yaml.constructor.SafeConstructor.  We replicate
-        # it here so that we can warn users when they have duplicate dict keys
-        # (pyyaml silently allows overwriting keys)
-        if not isinstance(node, MappingNode):
-            raise ConstructorError(None, None,
-                                   'expected a mapping node, but found %s' % node.id,
-                                   node.start_mark)
-        self.flatten_mapping(node)
-        mapping = AnsibleMapping()
+    from .onet import OnetBaseIE
     
-        Coercion rules are currently an implementation detail. See the CoerceTest
-    test class in test_statistics for details.
-    '''
-    # See http://bugs.python.org/issue24068.
-    assert T is not bool, 'initial type T is bool'
-    # If the types are the same, no need to coerce anything. Put this
-    # first, so that the usual case (no coercion needed) happens as soon
-    # as possible.
-    if T is S:  return T
-    # Mixed int & other coerce to the other type.
-    if S is int or S is bool:  return T
-    if T is int:  return S
-    # If one is a (strict) subclass of the other, coerce to the subclass.
-    if issubclass(S, T):  return S
-    if issubclass(T, S):  return T
-    # Ints coerce to the other type.
-    if issubclass(T, int):  return S
-    if issubclass(S, int):  return T
-    # Mixed fraction & float coerces to float (or float subclass).
-    if issubclass(T, Fraction) and issubclass(S, float):
-        return S
-    if issubclass(T, float) and issubclass(S, Fraction):
-        return T
-    # Any other combination is disallowed.
-    msg = 'don't know how to coerce %s and %s'
-    raise TypeError(msg % (T.__name__, S.__name__))
+    from .common import InfoExtractor
+from .turner import TurnerBaseIE
+from ..utils import url_basename
     
-        def test_big_queue_popright(self):
-        d = deque()
-        append, pop = d.appendleft, d.pop
-        for i in range(BIG):
-            append(i)
-        for i in range(BIG):
-            x = pop()
-            if x != i:
-                self.assertEqual(x, i)
+        You can insert stuff, replace, and delete chunks.  Note that the
+    operations are done lazily--only if you convert the buffer to a
+    String.  This is very efficient because you are not moving data around
+    all the time.  As the buffer of tokens is converted to strings, the
+    toString() method(s) check to see if there is an operation at the
+    current index.  If so, the operation is done and then normal String
+    rendering continues on the buffer.  This is like having multiple Turing
+    machine instruction streams (programs) operating on a single input tape. :)
     
-        def close(self):
-        if not self.closed:
-            try:
-                self.skip()
-            finally:
-                self.closed = True
+        def pushbutton(self, name, x, y, w, h, attr, text, next):
+        return self.control(name, 'PushButton', x, y, w, h, attr, None, text, next, None)
     
-        def _get_filter(self):
-        return self.filter
-    def _set_filter(self, filter):
-        self.filter = filter
+        token_type = 'header'
     
-    @dataclass
-class CV:
-    T_CV4 = ClassVar
-    cv0: ClassVar[int] = 20
-    cv1: ClassVar = 30
-    cv2: T_CV2
-    cv3: T_CV3
-    not_cv4: T_CV4  # When using string annotations, this field is not recognized as a ClassVar.
+        def test_default_exc_handler_coro(self):
+        self.loop._process_events = mock.Mock()
     
-        def test_bad_urls(self):
-        for url in self.bad:
-            agent, url = self.get_agent_and_url(url)
-            with self.subTest(url=url, agent=agent):
-                self.assertFalse(self.parser.can_fetch(agent, url))
+        def testSin(self):
+        self.assertRaises(TypeError, math.sin)
+        self.ftest('sin(0)', math.sin(0), 0)
+        self.ftest('sin(pi/2)', math.sin(math.pi/2), 1)
+        self.ftest('sin(-pi/2)', math.sin(-math.pi/2), -1)
+        try:
+            self.assertTrue(math.isnan(math.sin(INF)))
+            self.assertTrue(math.isnan(math.sin(NINF)))
+        except ValueError:
+            self.assertRaises(ValueError, math.sin, INF)
+            self.assertRaises(ValueError, math.sin, NINF)
+        self.assertTrue(math.isnan(math.sin(NAN)))
     
-        def test_bad_type_arg(self):
-        # The type argument must be a ctypes pointer type.
-        array_type = c_byte * sizeof(c_int)
-        array = array_type()
-        self.assertRaises(TypeError, cast, array, None)
-        self.assertRaises(TypeError, cast, array, array_type)
-        class Struct(Structure):
-            _fields_ = [('a', c_int)]
-        self.assertRaises(TypeError, cast, array, Struct)
-        class MyUnion(Union):
-            _fields_ = [('a', c_int)]
-        self.assertRaises(TypeError, cast, array, MyUnion)
+    
+class FixXrange(fixer_base.BaseFix):
+    BM_compatible = True
+    PATTERN = '''
+              power<
+                 (name='range'|name='xrange') trailer< '(' args=any ')' >
+              rest=any* >
+              '''
+    
+    
+class TestLLTrace(unittest.TestCase):
+    
+            ffi_inc = [sysconfig.get_config_var('LIBFFI_INCLUDEDIR')]
+        if not ffi_inc or ffi_inc[0] == '':
+            ffi_inc = find_file('ffi.h', [], inc_dirs)
+        if ffi_inc is not None:
+            ffi_h = ffi_inc[0] + '/ffi.h'
+            if not os.path.exists(ffi_h):
+                ffi_inc = None
+                print('Header file {} does not exist'.format(ffi_h))
+        ffi_lib = None
+        if ffi_inc is not None:
+            for lib_name in ('ffi', 'ffi_pic'):
+                if (self.compiler.find_library_file(lib_dirs, lib_name)):
+                    ffi_lib = lib_name
+                    break
