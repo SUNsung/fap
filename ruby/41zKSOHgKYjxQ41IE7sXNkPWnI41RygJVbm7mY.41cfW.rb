44b966dce6f103486174254ea295729b0749edc5
SUNsung <weatherius@gmail.com>
2018-08-07 04:01:31 +0300

    Rewrite [41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb] File in: 2018-08-07 04:01:31

--- ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
+++ ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
@@ -1,86 +1,123 @@
 
         
-            def self.git_log_between(pretty_format, from, to, merge_commit_filtering, date_format = nil, ancestry_path)
-      command = ['git log']
-      command << '--pretty=\'#{pretty_format}\''
-      command << '--date=\'#{date_format}\'' if date_format
-      command << '--ancestry-path' if ancestry_path
-      command << '#{from.shellescape}...#{to.shellescape}'
-      command << git_log_merge_commit_filtering_option(merge_commit_filtering)
-      Actions.sh(command.compact.join(' '), log: false).chomp
-    rescue
-      nil
-    end
+          # POST /resource/confirmation
+  def create
+    self.resource = resource_class.send_confirmation_instructions(resource_params)
+    yield resource if block_given?
     
-    describe Deliver::HtmlGenerator do
-  let(:generator) { Deliver::HtmlGenerator.new }
+          def remember_cookie_values(resource)
+        options = { httponly: true }
+        options.merge!(forget_cookie_values(resource))
+        options.merge!(
+          value: resource.class.serialize_into_cookie(resource),
+          expires: resource.remember_expires_at
+        )
+      end
     
-                # Remove the signal trap if no more registered callbacks exist
-            Signal.trap('INT', 'DEFAULT') if registered.empty?
+                  define_method method do |resource_or_scope, *args|
+                scope = Devise::Mapping.find_scope!(resource_or_scope)
+                router_name = Devise.mappings[scope].router_name
+                context = router_name ? send(router_name) : _devise_route_context
+                context.send('#{action}#{scope}_#{module_name}_#{path_or_url}', *args)
+              end
+            end
           end
         end
-    
-          def [](key)
-        super(convert_key(key))
       end
     
-            # If we're just checking, then just return exit codes
-        if options[:check]
-          return 0 if cap_host.capability?(name)
-          return 1
-        end
+      describe 'DATA_PTR' do
+    it 'returns the struct data' do
+      a = @s.wrap_struct(1024)
+      @s.get_struct_data_ptr(a).should == 1024
+    end
+  end
+end
+
     
-      def initialize(repo: 'twbs/bootstrap', branch: 'master', save_to: {}, cache_path: 'tmp/converter-cache-bootstrap')
-    @logger     = Logger.new
-    @repo       = repo
-    @branch     = branch || 'master'
-    @branch_sha = get_branch_sha
-    @cache_path = cache_path
-    @repo_url   = 'https://github.com/#@repo'
-    @save_to    = {
-        js:    'assets/javascripts/bootstrap',
-        scss:  'assets/stylesheets/bootstrap',
-        fonts: 'assets/fonts/bootstrap'}.merge(save_to)
+      it 'raises an Errno::ECHILD if there are no child processes' do
+    lambda { Process.wait }.should raise_error(Errno::ECHILD)
   end
     
-      # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+    require 'mkmf'
+require File.expand_path('../deprecation', __FILE__)
     
-      # The test environment is used exclusively to run your application's
-  # test suite. You never need to work with it otherwise. Remember that
-  # your test database is 'scratch space' for the test suite and is wiped
-  # and recreated between test runs. Don't rely on the data there!
-  config.cache_classes = true
+    if !Dir.respond_to?(:mktmpdir)
+  # copied from lib/tmpdir.rb
+  def Dir.mktmpdir(prefix_suffix=nil, tmpdir=nil)
+    case prefix_suffix
+    when nil
+      prefix = 'd'
+      suffix = ''
+    when String
+      prefix = prefix_suffix
+      suffix = ''
+    when Array
+      prefix = prefix_suffix[0]
+      suffix = prefix_suffix[1]
+    else
+      raise ArgumentError, 'unexpected prefix_suffix: #{prefix_suffix.inspect}'
+    end
+    tmpdir ||= Dir.tmpdir
+    t = Time.now.strftime('%Y%m%d')
+    n = nil
+    begin
+      path = '#{tmpdir}/#{prefix}#{t}-#{$$}-#{rand(0x100000000).to_s(36)}'
+      path << '-#{n}' if n
+      path << suffix
+      Dir.mkdir(path, 0700)
+    rescue Errno::EEXIST
+      n ||= 0
+      n += 1
+      retry
+    end
     
-      desc 'update main and version in bower.json'
-  task :generate do
-    require 'bootstrap-sass'
-    Dir.chdir Bootstrap.gem_path do
-      spec       = JSON.parse(File.read 'bower.json')
-    
-      trap('INT') {
-    [jekyllPid, compassPid, rackupPid].each { |pid| Process.kill(9, pid) rescue Errno::ESRCH }
-    exit 0
-  }
-    
-      if options.respond_to? 'keys'
-    options.each do |k,v|
-      unless v.nil?
-        v = v.join ',' if v.respond_to? 'join'
-        v = v.to_json if v.respond_to? 'keys'
-        output += ' data-#{k.sub'_','-'}='#{v}''
+        nr_threads.times do |i|
+      threads[i] = Thread.new do
+        mutex.synchronize do
+          result << 'C1'
+          condvar.wait mutex
+          result << 'C2'
+        end
       end
     end
-  elsif options.respond_to? 'join'
-    output += ' data-value='#{config[key].join(',')}''
-  else
-    output += ' data-value='#{config[key]}''
+    sleep 0.1
+    mutex.synchronize do
+      result << 'P1'
+      condvar.broadcast
+      result << 'P2'
+    end
+    Timeout.timeout(5) do
+      nr_threads.times do |i|
+        threads[i].join
+      end
+    end
+    
+      # test that waiting producers are woken up on close
+  def close_wakeup( num_items, num_threads, &qcreate )
+    raise 'This test won't work with num_items(#{num_items}) >= num_threads(#{num_threads})' if num_items >= num_threads
+    
+        assert_equal(tester.sync_waiting.uniq, tester.sync_waiting)
+    assert_equal(tester.sync_waiting, [])
   end
-  output += '></#{tag}>'
 end
+
     
-      class ImageTag < Liquid::Tag
-    @img = nil
+        # Never trust parameters from the scary internet, only allow the white list through.
+    def book_params
+      params.require(:book).permit(:name)
+    end
+end
+
     
+            def on_send(node)
+          return unless match_call?(node) &&
+                        (!node.value_used? || only_truthiness_matters?(node)) &&
+                        !(node.parent && node.parent.block_type?)
     
+            def on_block(node)
+          on_body_of_reduce(node) do |body|
+            void_next = body.each_node(:next).find do |n|
+              n.children.empty? && parent_block_node(n) == node
+            end
+    
+            private
