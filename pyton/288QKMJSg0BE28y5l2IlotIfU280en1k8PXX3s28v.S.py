3a66085043761a010df8ce26505fd9fff3b265c9
SUNsung <weatherius@gmail.com>
2018-08-07 05:05:32 +0300

    Rewrite [288QKMJSg0BE28y5l2IlotIfU280en1k8PXX3s28v.S.py] File in: 2018-08-07 05:05:32

--- pyton/288QKMJSg0BE28y5l2IlotIfU280en1k8PXX3s28v.S.py
+++ pyton/288QKMJSg0BE28y5l2IlotIfU280en1k8PXX3s28v.S.py
@@ -1,172 +1,149 @@
 
         
-        cc_test(
-    name = 'mst_solver_random_comparison_test',
-    size = 'small',
-    timeout = 'long',
-    srcs = ['mst_solver_random_comparison_test.cc'],
-    tags = [
-        'manual',  # exclude from :all, since this is expensive
-    ],
-    deps = [
-        ':mst_solver',
-        ':spanning_tree_iterator',
-        '//syntaxnet:base',
-        '//syntaxnet:test_main',
-        '@org_tensorflow//tensorflow/core:lib',
-        '@org_tensorflow//tensorflow/core:test',
-    ],
-)
+        
+username = get_config('ANSIBLE_OPENSHIFT_USERNAME', 'default_rhlogin')
+password = get_config('ANSIBLE_OPENSHIFT_PASSWORD', 'password')
+broker_url = 'https://%s/broker/rest/' % get_config('ANSIBLE_OPENSHIFT_BROKER', 'libra_server')
     
-    tf_proto_library_cc(
-    name = 'trace_proto',
-    srcs = ['trace.proto'],
-    protodeps = [':data_proto'],
-)
+                for item in items:
+                contents = item['contents'].encode('utf-8')
+                path = output_dir + '/' + re.sub('^/*', '', item['path'])
     
-    py_library(
-    name = 'dragnn_model_saver_lib',
-    srcs = ['dragnn_model_saver_lib.py'],
-    deps = [
-        ':dragnn_ops',
-        ':graph_builder',
-        ':load_dragnn_cc_impl_py',
-        ':network_units',
-        '//dragnn/protos:spec_pb2_py',
-        '//syntaxnet:load_parser_ops_py',
-        '//syntaxnet:sentence_pb2_py',
-        '@org_tensorflow//tensorflow:tensorflow_py',
-        '@org_tensorflow//tensorflow/core:protos_all_py',
-    ],
-)
+        def construct_yaml_unsafe(self, node):
+        return self.construct_yaml_str(node, unsafe=True)
+    
+    from ansible.compat.tests import unittest
+from ansible.compat.tests.mock import MagicMock
+    
+    n_samples = np.logspace(.5, 3, 9)
+n_features = np.logspace(1, 3.5, 7)
+N_samples, N_features = np.meshgrid(n_samples,
+                                    n_features)
+scikits_time = np.zeros(N_samples.shape)
+scipy_time = np.zeros(N_samples.shape)
+    
+        op.add_option('--n-population',
+                  dest='n_population', default=100000, type=int,
+                  help='Size of the population to sample from.')
+    
+    import codecs
     
     
-      weights = tf.constant([[[ 2,  3],
-                              [ 5,  7]],
-                             [[11, 13],
-                              [17, 19]],
-                             [[23, 29],
-                              [31, 37]]], tf.float32)  # pyformat: disable
-    
-      Args:
-    mst_op: The MaximumSpanningTree op being differentiated.
-    d_loss_d_max_scores: [B] vector where entry b is the gradient of the network
-                         loss w.r.t. entry b of the |max_scores| output of the
-                         |mst_op|.
-    *_: The gradients w.r.t. the other outputs; ignored.
-    
-    
-def barplot_neighbors(Nrange=2 ** np.arange(1, 11),
-                      Drange=2 ** np.arange(7),
-                      krange=2 ** np.arange(10),
-                      N=1000,
-                      D=64,
-                      k=5,
-                      leaf_size=30,
-                      dataset='digits'):
-    algorithms = ('kd_tree', 'brute', 'ball_tree')
-    fiducial_values = {'N': N,
-                       'D': D,
-                       'k': k}
-    
-    
-def rbf_kernels(X, n_jobs):
-    return pairwise_kernels(X, metric='rbf', n_jobs=n_jobs, gamma=0.1)
-    
-        op.add_option('--density',
-                  dest='density', default=1 / 3,
-                  help='Density used by the sparse random projection.'
-                       ' ('auto' or float (0.0, 1.0]')
-    
-        ###########################################################################
-    # Set Python core input
-    sampling_algorithm['python-core-sample'] = \
-        lambda n_population, n_sample: \
-            random.sample(xrange(n_population), n_sample)
-    
-    
-if __name__ == '__main__':
-    # NOTE: we put the following in a 'if __name__ == '__main__'' protected
-    # block to be able to use a multi-core grid search that also works under
-    # Windows, see: http://docs.python.org/library/multiprocessing.html#windows
-    # The multiprocessing module is used as the backend of joblib.Parallel
-    # that is used when n_jobs != 1 in GridSearchCV
-    
-    
-def generate_data(n_samples, n_features):
-    '''Generate random blob-ish data with noisy features.
-    
-    '''
-print(__doc__)
-    
-    This dataset is made up of 1797 8x8 images. Each image,
-like the one shown below, is of a hand-written digit.
-In order to utilize an 8x8 figure like this, we'd have to
-first transform it into a feature vector with length 64.
-    
-    __author__ = 'Brian Quinlan (brian@sweetapp.com)'
-    
-    def download_urls_with_executor(urls, executor, timeout=60):
-    try:
-        url_to_content = {}
-        future_to_url = dict((executor.submit(load_url, url, timeout), url)
-                             for url in urls)
-    
-    # Output file base name for HTML help builder.
-htmlhelp_basename = 'futuresdoc'
-    
-    
-class CommandRequest( BaseRequest ):
-  def __init__( self, arguments, completer_target = None, extra_data = None ):
-    super( CommandRequest, self ).__init__()
-    self._arguments = _EnsureBackwardsCompatibility( arguments )
-    self._command = arguments and arguments[ 0 ]
-    self._completer_target = ( completer_target if completer_target
-                               else 'filetype_default' )
-    self._extra_data = extra_data
-    self._response = None
-    
-    
-  def IsAllowed( self, diagnostic ):
-    # NOTE: in this class's implementation, we ask vimsupport for
-    #  the current filetypes and delegate automatically; it is probably,
-    #  more efficient, however, to call SubsetForTypes() and reuse
-    #  the returned DiagnosticFilter if it will be checked repeatedly.
-    filetypes = vimsupport.CurrentFiletypes()
-    return self.SubsetForTypes( filetypes ).IsAllowed( diagnostic )
-    
-    
-  def Response( self ):
-    return {
-      'completions': self._results,
-      'completion_start_column': self.request_data[ 'start_column' ]
-    }
-    
-    image = face_recognition.load_image_file('{}')
-'''
-    
-        # You can access the actual face itself like this:
-    face_image = image[top:bottom, left:right]
-    pil_image = Image.fromarray(face_image)
-    pil_image.show()
+plt.show()
 
     
-    # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file('biden.jpg')
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+    COLORS = np.array(['!',
+                   '#FF3333',  # red
+                   '#0198E1',  # blue
+                   '#BF5FFF',  # purple
+                   '#FCD116',  # yellow
+                   '#FF7216',  # orange
+                   '#4DBD33',  # green
+                   '#87421F'   # brown
+                   ])
     
-    requirements = [
-    'face_recognition_models>=0.3.0',
-    'Click>=6.0',
-    'dlib>=19.7',
-    'numpy',
-    'Pillow'
+        def test_get_group_cfws_only(self):
+        group = self._test_get_x(parser.get_group,
+            'Monty Python: (hidden);',
+            'Monty Python: (hidden);',
+            'Monty Python: ;',
+            [],
+            '')
+        self.assertEqual(group.token_type, 'group')
+        self.assertEqual(group.display_name, 'Monty Python')
+        self.assertEqual(len(group.mailboxes), 0)
+        self.assertEqual(group.mailboxes,
+                         group.all_mailboxes)
+    
+                self.assertTrue(ntpath.ismount('\\\\localhost\\c$'))
+            self.assertTrue(ntpath.ismount('\\\\localhost\\c$\\'))
+    
+        def testFabs(self):
+        self.assertRaises(TypeError, math.fabs)
+        self.ftest('fabs(-1)', math.fabs(-1), 1)
+        self.ftest('fabs(0)', math.fabs(0), 0)
+        self.ftest('fabs(1)', math.fabs(1), 1)
+    
+    def does_tree_import(package, name, node):
+    ''' Returns true if name is imported from package at the
+        top level of the tree which node belongs to.
+        To cover the case of an import like 'import foo', use
+        None for the package and 'foo' for the name. '''
+    binding = find_binding(name, find_root(node), package)
+    return bool(binding)
+    
+        PATTERN = '''
+    power< head=any+
+         trailer< '.' method=('keys'|'items'|'values'|
+                              'iterkeys'|'iteritems'|'itervalues'|
+                              'viewkeys'|'viewitems'|'viewvalues') >
+         parens=trailer< '(' ')' >
+         tail=any*
+    >
+    '''
+    
+        def test_get_builtin_constructor(self):
+        get_builtin_constructor = getattr(hashlib,
+                                          '__get_builtin_constructor')
+        builtin_constructor_cache = getattr(hashlib,
+                                            '__builtin_constructor_cache')
+        self.assertRaises(ValueError, get_builtin_constructor, 'test')
+        try:
+            import _md5
+        except ImportError:
+            self.skipTest('_md5 module not available')
+        # This forces an ImportError for 'import _md5' statements
+        sys.modules['_md5'] = None
+        # clear the cache
+        builtin_constructor_cache.clear()
+        try:
+            self.assertRaises(ValueError, get_builtin_constructor, 'md5')
+        finally:
+            if '_md5' in locals():
+                sys.modules['_md5'] = _md5
+            else:
+                del sys.modules['_md5']
+        self.assertRaises(TypeError, get_builtin_constructor, 3)
+        constructor = get_builtin_constructor('md5')
+        self.assertIs(constructor, _md5.md5)
+        self.assertEqual(sorted(builtin_constructor_cache), ['MD5', 'md5'])
+    
+    
+class Action(object):
+    
+        def __init__(self):
+        self.msg_queue = []
+        self.subscribers = {}
+    
+    
+@gen.engine
+def run_tests():
+    url = options.url + '/getCaseCount'
+    control_ws = yield websocket_connect(url, None)
+    num_tests = int((yield control_ws.read_message()))
+    logging.info('running %d cases', num_tests)
+    msg = yield control_ws.read_message()
+    assert msg is None
+    
+    # Repeat the entire benchmark this many times (on different ports)
+# This gives JITs time to warm up, etc.  Pypy needs 3-5 runs at
+# --n=15000 for its JIT to reach full effectiveness
+define('num_runs', type=int, default=1)
+    
+    from tornado.options import options, define, parse_command_line
+from tornado.template import Template
+    
+    latex_documents = [
+    ('index', 'tornado.tex', 'Tornado Documentation', 'The Tornado Authors', 'manual', False),
 ]
     
-    # Load the jpg file into a numpy array
-image = face_recognition.load_image_file('biden.jpg')
-    
-    
-def _rect_to_css(rect):
-    '''
-    Convert a dlib 'rect' object to a plain tuple in (top, right, bottom, left) order
+        def run(self):
+        try:
+            build_ext.run(self)
+        except Exception:
+            e = sys.exc_info()[1]
+            sys.stdout.write('%s\n' % str(e))
+            warnings.warn(self.warning_message % ('Extension modules',
+                                                  'There was an issue with '
+                                                  'your platform configuration'
+                                                  ' - see above.'))
