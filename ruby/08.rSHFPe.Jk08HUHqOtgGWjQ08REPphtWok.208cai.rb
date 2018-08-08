fd5033e280f7c1ca3bbf6d2b821cf46c1220f494
SUNsung <weatherius@gmail.com>
2018-08-08 11:19:06 +0300

    Rewrite [08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb] File in: 2018-08-08 11:19:06

--- ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
+++ ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
@@ -1,90 +1,45 @@
 
         
-              def get_type
-        return 'Reference' if base_url.path == '/spec/'
+          def failure_message
+    exception = request.respond_to?(:get_header) ? request.get_header('omniauth.error') : request.env['omniauth.error']
+    error   = exception.error_reason if exception.respond_to?(:error_reason)
+    error ||= exception.error        if exception.respond_to?(:error)
+    error ||= (request.respond_to?(:get_header) ? request.get_header('omniauth.error.type') : request.env['omniauth.error.type']).to_s
+    error.to_s.humanize if error
+  end
     
-        options[:attribution] = <<-HTML
-      &copy; 2009&ndash;2018 Jeremy Ashkenas<br>
-      Licensed under the MIT License.
-    HTML
-    
-          options[:only_patterns] = [/\Ad3[\-\w]+\z/, /\Ad3\/blob\/master\/changes\.md\z/i]
-      options[:skip_patterns] = [/3\.x-api-reference/]
-    
-        html_filters.push 'tensorflow/entries', 'tensorflow/clean_html'
-    
-            if b_length > a_length
-          (b_length - a_length).times { a_split.insert(-2, 0) }
-        elsif a_length > b_length
-          (a_length - b_length).times { b_split.insert(-2, 0) }
-        end
-    
-        def initialize
-      @pages = {}
-    end
-    
-      GEMFILE_EXTENSIONS.each do |extension|
-    extension_pathname = root.join('Gemfile#{extension}')
-    
-        end
-    
-      def parse(pkt)
-    # We want to return immediatly if we do not have a packet which is handled by us
-    return unless pkt.is_tcp?
-    return if (pkt.tcp_sport != 21 and pkt.tcp_dport != 21)
-    s = find_session((pkt.tcp_sport == 21) ? get_session_src(pkt) : get_session_dst(pkt))
-    s[:sname] ||= 'ftp'
-    
-    classNames = [ 'HelloWorld1', 'HelloWorld2' ]
-    
-    
-signer._invoke('KeyToolMSF','[Ljava.lang.String;',keytoolOpts)
-    
-    unless STDIN.tty?
-	p = ::Parser.new('')
-	p.parse
-	p.dump_all
-else
-	print 'Tested with:\n'
-	print '\tGNU objdump 2.9-aix51-020209\n'
-	print '\tGNU objdump 2.15.92.0.2 20040927\n'
-	print 'Usage: objdump -dM suffix <file(s)> | ruby objdumptoc.rb\n'
-end
-
-    
-            def run
-          if @pod_name.nil?
-            # Note: at that point, @wipe_all is always true (thanks to `validate!`)
-            # Remove all
-            clear_cache
-          else
-            # Remove only cache for this pod
-            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
-            if cache_descriptors.nil?
-              UI.notice('No cache for pod named #{@pod_name} found')
-            elsif cache_descriptors.count > 1 && !@wipe_all
-              # Ask which to remove
-              choices = cache_descriptors.map { |c| '#{@pod_name} v#{c[:version]} (#{pod_type(c)})' }
-              index = UI.choose_from_array(choices, 'Which pod cache do you want to remove?')
-              remove_caches([cache_descriptors[index]])
-            else
-              # Remove all found cache of this pod
-              remove_caches(cache_descriptors)
-            end
-          end
-        end
-    
-            def execute_repl_command(repl_command)
-          unless repl_command == '\n'
-            repl_commands = repl_command.split
-            subcommand = repl_commands.shift.capitalize
-            arguments = repl_commands
-            subcommand_class = Pod::Command::IPC.const_get(subcommand)
-            subcommand_class.new(CLAide::ARGV.new(arguments)).run
-            signal_end_of_output
-          end
-        end
+        if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      if Devise.sign_in_after_reset_password
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message!(:notice, flash_message)
+        sign_in(resource_name, resource)
+      else
+        set_flash_message!(:notice, :updated_not_active)
       end
+      respond_with resource, location: after_resetting_password_path_for(resource)
+    else
+      set_minimum_password_length
+      respond_with resource
     end
   end
-end
+    
+    require 'metasploit/framework/common_engine'
+require 'metasploit/framework/database'
+    
+    #
+# This script extracts the forms from the main page of each
+# web site in a list. The output of this can be used with
+# Metasploit (and other tools) to obtain the saved form data
+# of these domains.
+#
+    
+      def register_sigs
+    self.sigs = {
+      :banner		=> /^(\*\s+OK[^\n\r]*)/i,
+      :login		=> /^CAPABILITY\s+LOGIN\s+([^\s]+)\s+([^\n\r]+)/i,
+      :login_pass => /^CAPABILITY\s+OK\s+(Login[^\n\r]*)/i,
+      :login_bad	=> /^CAPABILITY\s+BAD\s+(Login[^\n\r]*)/i,
+      :login_fail => /^CAPABILITY\s+NO\s+(Login[^\n\r]*)/i
+    }
+  end
