10b2d26eb5afef5d6aeed17bcec03a91fe89f6b8
SUNsung <weatherius@gmail.com>
2018-08-08 09:24:26 +0300

    Rewrite [51XE1w6QQIVIs51wNOVn3Ah69k51VT4Z5QE4hUQ513I2.rb] File in: 2018-08-08 09:24:25

--- ruby/51XE1w6QQIVIs51wNOVn3Ah69k51VT4Z5QE4hUQ513I2.rb
+++ ruby/51XE1w6QQIVIs51wNOVn3Ah69k51VT4Z5QE4hUQ513I2.rb
@@ -1,113 +1,39 @@
 
         
-                # Require the adapter itself and give useful feedback about
-        #   1. Missing adapter gems and
-        #   2. Adapter gems' missing dependencies.
-        path_to_adapter = 'action_cable/subscription_adapter/#{adapter}'
-        begin
-          require path_to_adapter
-        rescue LoadError => e
-          # We couldn't require the adapter itself. Raise an exception that
-          # points out config typos and missing gems.
-          if e.path == path_to_adapter
-            # We can assume that a non-builtin adapter was specified, so it's
-            # either misspelled or missing from Gemfile.
-            raise e.class, 'Could not load the '#{adapter}' Action Cable pubsub adapter. Ensure that the adapter is spelled correctly in config/cable.yml and that you've added the necessary adapter gem to your Gemfile.', e.backtrace
+            # Loops through the list of category pages and processes each one.
+    def write_category_indexes
+      if self.layouts.key? 'category_index'
+        dir = self.config['category_dir'] || 'categories'
+        self.categories.keys.each do |category|
+          self.write_category_index(File.join(dir, category.to_url), category)
+        end
     
-      def test_nil_is_not_changed_when_serialized_with_a_class
-    Topic.serialize(:content, Array)
-    
-      setup do
-    @connection = TestConnection.new
-  end
-    
-      test 'disallow block and arg together' do
-    e = assert_raise ArgumentError do
-      ChatChannel.periodically(:send_updates, every: 1) { ping }
-    end
-    assert_match(/not both/, e.message)
-  end
-    
-        assert subscriptions.verify
-  end
-end
-
-    
-      test 'broadcast generates notification' do
-    begin
-      server = TestServer.new
-    
-      def test_commandline
-    commandline = 'echo quux\n'
-    Open3.popen3(commandline) {|i,o,e,t|
-      assert_equal('quux\n', o.read)
-    }
-  end
-    
-      it 'returns nil if else-body is empty, expression is false and then-body is empty' do
-    if false
-    else
-    end.should == nil
-  end
-    
-        it 'allows changing the wrapped struct' do
-      a = @s.wrap_struct(1024)
-      @s.change_struct(a, 100)
-      @s.get_struct(a).should == 100
+        def post_render(page)
+      OctopressFilters::post_filter(page)
     end
   end
     
-        # This spec is probably system-dependent.
-    it 'waits for a child whose process group ID is that of the calling process' do
-      pid1 = Process.spawn(ruby_cmd('exit'), pgroup: true)
-      pid2 = Process.spawn(ruby_cmd('exit'))
-    
-          ##
-      # Used by UserDB to create a basic password entry
-    
-          klass = nil
-      mesg = nil
-      begin
-        mutex.lock
-        cv.wait mutex
-        mutex.unlock
-      rescue Exception => e
-        klass = e.class
-        mesg = e.message
-      end
-      puts klass
-      print mesg
-INPUT
-  end
-    
-      # test that waiting producers are woken up on close
-  def close_wakeup( num_items, num_threads, &qcreate )
-    raise 'This test won't work with num_items(#{num_items}) >= num_threads(#{num_threads})' if num_items >= num_threads
-    
-      # Update version.rb file with BOOTSTRAP_SHA
-  def store_version
-    path    = 'lib/bootstrap-sass/version.rb'
-    content = File.read(path).sub(/BOOTSTRAP_SHA\s*=\s*[''][\w]+['']/, 'BOOTSTRAP_SHA = '#@branch_sha'')
-    File.open(path, 'w') { |f| f.write(content) }
-  end
-end
-
-    
-      # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
-    
-        if as == :json
-      if api_error?(data)
-        data = generate_error_hash(data)
-      else
-        selected_fields = extract_fields(filter.to_s.strip)
-        data.select! { |k,v| selected_fields.include?(k) } unless selected_fields.empty?
-        unless options.include?(:exclude_default_metadata)
-          data = data.to_hash
-          if data.values.size == 0 && selected_fields.size > 0
-            raise LogStash::Api::NotFoundError
+          Dir.chdir(file_path) do
+        contents = file.read
+        if contents =~ /\A-{3}.+[^\A]-{3}\n(.+)/m
+          contents = $1.lstrip
+        end
+        contents = pre_filter(contents)
+        if @raw
+          contents
+        else
+          partial = Liquid::Template.parse(contents)
+          context.stack do
+            partial.render(context)
           end
-          data = default_metadata.merge(data)
         end
       end
+    end
+  end
+end
+    
+      class VideoTag < Liquid::Tag
+    @video = nil
+    @poster = ''
+    @height = ''
+    @width = ''
