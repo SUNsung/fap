c01052c08b30f74460b30d96eaf4d25815406bd0
SUNsung <weatherius@gmail.com>
2018-08-08 21:40:01 +0300

    Rewrite [54PtoH1CNU1No54CGD5onl80es54AUG514RQ5Kw54TIB.py] File in: 2018-08-08 21:40:00

--- pyton/54PtoH1CNU1No54CGD5onl80es54AUG514RQ5Kw54TIB.py
+++ pyton/54PtoH1CNU1No54CGD5onl80es54AUG514RQ5Kw54TIB.py
@@ -1,145 +1,68 @@
 
         
-        cc_library(
-    name = 'syntaxnet_link_feature_extractor',
-    srcs = ['syntaxnet_link_feature_extractor.cc'],
-    hdrs = ['syntaxnet_link_feature_extractor.h'],
-    deps = [
-        '//dragnn/protos:spec_proto_cc',
-        '//syntaxnet:base',
-        '//syntaxnet:embedding_feature_extractor',
-        '//syntaxnet:parser_transitions',
-        '//syntaxnet:task_context',
-    ],
-)
-    
-      def testFailsOnFixedFeature(self):
-    component_spec = spec_pb2.ComponentSpec()
-    text_format.Parse('''
-        name: 'annotate'
-        network_unit {
-          registered_name: 'IdentityNetwork'
-        }
-        fixed_feature {
-          name: 'fixed' embedding_dim: 32 size: 1
-        }
-        ''', component_spec)
-    with tf.Graph().as_default():
-      comp = bulk_component.BulkAnnotatorComponentBuilder(
-          self.master, component_spec)
-    
-        # Saves completed arrays and returns final state.
-    state.handle = output[0]
-    arrays = output[1:]
-    network_state = network_states[self.name]
-    with tf.name_scope(self.name + '/stored_act'):
-      for index, layer in enumerate(self.network.layers):
-        network_state.activations[layer.name] = network_units.StoredActivations(
-            array=arrays[index])
-    self._add_runtime_hooks()
-    with tf.control_dependencies([x.flow for x in arrays]):
-      return tf.identity(state.handle)
-    
-    FLAGS = flags.FLAGS
-    
-    
-        '''
-        msg = ('We were unable to set up enhancement %s for your server, '
-               'however, we successfully installed your certificate.'
-               % (enhancement))
-        with error_handler.ErrorHandler(self._recovery_routine_with_msg, msg):
-            for dom in domains:
-                try:
-                    self.installer.enhance(dom, enhancement, options)
-                except errors.PluginEnhancementAlreadyPresent:
-                    if enhancement == 'ensure-http-header':
-                        logger.warning('Enhancement %s was already set.',
-                                options)
-                    else:
-                        logger.warning('Enhancement %s was already set.',
-                                enhancement)
-                except errors.PluginError:
-                    logger.warning('Unable to set enhancement %s for %s',
-                            enhancement, dom)
-                    raise
-    
-        def test_str(self):
-        self.assertTrue('FOO' in str(self.error))
-        self.assertTrue('{}' in str(self.error))
-    
-        #Parsing the arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('dir_path', help = 'Path to tldr 'pages' directory')
-    parser.add_argument('-c', choices=['solarized-light', 'solarized-dark'], help='Color scheme of the PDF')
-    args = parser.parse_args()
-    
-    loc = args.dir_path
-    if args.c == 'solarized-light' or args.c == 'solarized-dark':
-        colorscheme = args.c
         
-    main(loc, colorscheme)
-    
-            Returns True if the future was cancelled, False otherwise. A future
-        cannot be cancelled if it is running or has already completed.
-        '''
-        with self._condition:
-            if self._state in [RUNNING, FINISHED]:
-                return False
-    
-            for future in as_completed(future_to_url):
-            try:
-                url_to_content[future_to_url[future]] = future.result()
-            except:
-                pass
-        return url_to_content
-    finally:
-        executor.shutdown()
-    
-    # Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title, author, documentclass [howto/manual]).
-latex_documents = [
-  ('index', 'futures.tex', u'futures Documentation',
-   u'Brian Quinlan', 'manual'),
-]
-    
-    
-@YouCompleteMeInstance( { 'g:ycm_extra_conf_vim_data': [ 'undefined_value' ] } )
-def SendCommandRequest_ExtraConfData_UndefinedValue_test( ycm ):
-  current_buffer = VimBuffer( 'buffer' )
-  with MockVimBuffers( [ current_buffer ], [ current_buffer ] ):
-    with patch( 'ycm.youcompleteme.SendCommandRequest' ) as send_request:
-      ycm.SendCommandRequest( [ 'GoTo' ], 'python', 'belowright', False, 1, 1 )
-      assert_that(
-        # Positional arguments passed to SendCommandRequest.
-        send_request.call_args[ 0 ],
-        contains(
-          contains( 'GoTo' ),
-          'python',
-          'belowright',
-          has_entries( {
-            'options': has_entries( {
-              'tab_size': 2,
-              'insert_spaces': True,
-            } )
-          } )
-        )
-      )
-    
-    for i, face_distance in enumerate(face_distances):
-    print('The test image has a distance of {:.2} from known image #{}'.format(face_distance, i))
-    print('- With a normal cutoff of 0.6, would the test image match the known image? {}'.format(face_distance < 0.6))
-    print('- With a very strict cutoff of 0.5, would the test image match the known image? {}'.format(face_distance < 0.5))
-    print()
+def dispatch_hook(key, hooks, hook_data, **kwargs):
+    '''Dispatches a hook dictionary on a given piece of data.'''
+    hooks = hooks or dict()
+    hooks = hooks.get(key)
+    if hooks:
+        if hasattr(hooks, '__call__'):
+            hooks = [hooks]
+        for hook in hooks:
+            _hook_data = hook(hook_data, **kwargs)
+            if _hook_data is not None:
+                hook_data = _hook_data
+    return hook_data
 
     
-        # Print the location of each face in this image
-    top, right, bottom, left = face_location
-    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
     
-            # Blur the face image
-        face_image = cv2.GaussianBlur(face_image, (99, 99), 30)
+@pytest.fixture
+def httpbin_secure(httpbin_secure):
+    return prepare_url(httpbin_secure)
+
     
-    # This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
+        possible_keys = pytest.mark.parametrize('key', ('accept', 'ACCEPT', 'aCcEpT', 'Accept'))
+    
+    
+def _basic_auth_str(username, password):
+    '''Returns a Basic Auth string.'''
+    
+    This module handles import compatibility issues between Python 2 and
+Python 3.
+'''
+    
+        def __init__(self):
+        self.experts = []
+        self.common_state = {
+            'problems': 0,
+            'suggestions': 0,
+            'contributions': [],
+            'progress': 0   # percentage, if 100 -> task is finished
+        }
+    
+        move = Action('move')
+    person = Person('Jack', move)
+    person.do_action().amount('5m').stop()
+    
+        def test_items_recoil(self):
+        with ObjectPool(self.sample_queue, True) as pool:
+            self.assertEqual(pool, 'first')
+        self.assertTrue(self.sample_queue.get() == 'second')
+        self.assertFalse(self.sample_queue.empty())
+        self.assertTrue(self.sample_queue.get() == 'first')
+        self.assertTrue(self.sample_queue.empty())
+    
+    '''
+Port of the Java example of 'Setter Injection' in
+'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
+(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
+http://xunitpatterns.com/Dependency%20Injection.html.
+    
+        def test_display_current_time_at_midnight(self):
+        '''
+        Will almost always fail (despite of right at/after midnight).
+        '''
+        time_provider_stub = MidnightTimeProvider()
+        class_under_test = TimeDisplay(time_provider_stub)
+        expected_time = '<span class=\'tinyBoldText\'>24:01</span>'
+        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
