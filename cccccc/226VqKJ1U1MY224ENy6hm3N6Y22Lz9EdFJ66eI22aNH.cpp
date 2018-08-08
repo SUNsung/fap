e9fec0e92a6f1a6b555ee8b9567931702c21d1c5
SUNsung <weatherius@gmail.com>
2018-08-08 19:08:53 +0300

    Rewrite [226VqKJ1U1MY224ENy6hm3N6Y22Lz9EdFJ66eI22aNH.cpp] File in: 2018-08-08 19:08:52

--- cccccc/226VqKJ1U1MY224ENy6hm3N6Y22Lz9EdFJ66eI22aNH.cpp
+++ cccccc/226VqKJ1U1MY224ENy6hm3N6Y22Lz9EdFJ66eI22aNH.cpp
@@ -1,171 +1,468 @@
 
         
-        
-    {
-    {} // end namespace index
-} // end namespace swift
+        #ifndef STORAGE_LEVELDB_DB_DB_ITER_H_
+#define STORAGE_LEVELDB_DB_DB_ITER_H_
     
+    bool InternalFilterPolicy::KeyMayMatch(const Slice& key, const Slice& f) const {
+  return user_policy_->KeyMayMatch(ExtractUserKey(key), f);
+}
     
-    {} // end namespace swift
-    
-    /// Runs the given function with the given context argument exactly once.
-/// The predicate argument must point to a global or static variable of static
-/// extent of type swift_once_t.
-SWIFT_RUNTIME_EXPORT
-void swift_once(swift_once_t *predicate, void (*fn)(void *), void *context);
-    
-    #ifndef SWIFT_INDEX_INDEXDATACONSUMER_H
-#define SWIFT_INDEX_INDEXDATACONSUMER_H
-    
-    
-    {
-    {
-    {  unsigned getNumFixitsApplied() const {
-    return NumFixitsApplied;
+    // Called on every item found in a WriteBatch.
+class WriteBatchItemPrinter : public WriteBatch::Handler {
+ public:
+  WritableFile* dst_;
+  virtual void Put(const Slice& key, const Slice& value) {
+    std::string r = '  put '';
+    AppendEscapedStringTo(&r, key);
+    r += '' '';
+    AppendEscapedStringTo(&r, value);
+    r += ''\n';
+    dst_->Append(r);
   }
-};
-} // end namespace migrator
-} // end namespace swift
-    
-    template<typename T>
-inline void
-emitTLSLoad(Vout& v, TLSDatum<ThreadLocalNoCheck<T>> datum, Vreg d) {
-  auto const off = ThreadLocalNoCheck<T>::node_ptr_offset();
-  v << load{emitTLSAddr(v, datum) + safe_cast<int32_t>(off), d};
-}
-    
-    Type typeAddO(Type t1, Type t2) {
-  if (auto t = eval_const(t1, t2, cellAddO))          return *t;
-  if (t1.subtypeOf(BInt) && t2.subtypeOf(BInt))       return TNum;
-  if (auto t = usual_arith_conversions(t1, t2))       return *t;
-  if (t1.subtypeOf(BArr) && t2.subtypeOf(BArr))       return TArr;
-  if (t1.subtypeOf(BVec) && t2.subtypeOf(BVec))       return TVec;
-  if (t1.subtypeOf(BDict) && t2.subtypeOf(BDict))     return TDict;
-  if (t1.subtypeOf(BKeyset) && t2.subtypeOf(BKeyset)) return TKeyset;
-  return TInitCell;
-}
-    
-      std::string m_proxyHost;
-  int         m_proxyPort;
-  std::string m_proxyUsername;
-  std::string m_proxyPassword;
-    
-    // Set by a .ini file at the start
-static SystemSettings s_system_settings;
-    
-    TEST(LogTest, UnexpectedMiddleType) {
-  Write('foo');
-  SetByte(6, kMiddleType);
-  FixChecksum(0, 3);
-  ASSERT_EQ('EOF', Read());
-  ASSERT_EQ(3, DroppedBytes());
-  ASSERT_EQ('OK', MatchError('missing start'));
-}
-    
-    
-    {  size_t read = 0;
-  std::string read_data;
-  while (read < kWriteSize) {
-    ASSERT_OK(seq_file->Read(kWriteSize - read, &result, scratch));
-    read_data.append(result.data(), result.size());
-    read += result.size();
-  }
-  ASSERT_TRUE(write_data == read_data);
-  delete seq_file;
-  delete [] scratch;
-}
-    
-    
-    {  // Skewed: pick 'base' uniformly from range [0,max_log] and then
-  // return 'base' random bits.  The effect is to pick a number in the
-  // range [0,2^max_log-1] with exponential bias towards smaller numbers.
-  uint32_t Skewed(int max_log) {
-    return Uniform(1 << Uniform(max_log + 1));
+  virtual void Delete(const Slice& key) {
+    std::string r = '  del '';
+    AppendEscapedStringTo(&r, key);
+    r += ''\n';
+    dst_->Append(r);
   }
 };
     
+    // Return the name of the current file.  This file contains the name
+// of the current manifest file.  The result will be prefixed with
+// 'dbname'.
+extern std::string CurrentFileName(const std::string& dbname);
     
-    {  // Verify that the size of the key space not touched by the reads
-  // is pretty much unchanged.
-  const int64_t final_other_size = Size(Key(n), Key(kCount));
-  ASSERT_LE(final_other_size, initial_other_size + 1048576);
-  ASSERT_GE(final_other_size, initial_other_size/5 - 1048576);
+    
+    {  // Errors
+  static const char* errors[] = {
+    '',
+    'foo',
+    'foo-dx-100.log',
+    '.log',
+    '',
+    'manifest',
+    'CURREN',
+    'CURRENTX',
+    'MANIFES',
+    'MANIFEST',
+    'MANIFEST-',
+    'XMANIFEST-3',
+    'MANIFEST-3x',
+    'LOC',
+    'LOCKx',
+    'LO',
+    'LOGx',
+    '18446744073709551616.log',
+    '184467440737095516150.log',
+    '100',
+    '100.',
+    '100.lop'
+  };
+  for (int i = 0; i < sizeof(errors) / sizeof(errors[0]); i++) {
+    std::string f = errors[i];
+    ASSERT_TRUE(!ParseFileName(f, &number, &type)) << f;
+  }
 }
     
+    class Env;
     
-    {  // When limit user key is prefix of start user key
-  ASSERT_EQ(IKey('foobar', 100, kTypeValue),
-            Shorten(IKey('foobar', 100, kTypeValue),
-                    IKey('foo', 200, kTypeValue)));
-}
+      // keys[0,n-1] contains a list of keys (potentially with duplicates)
+  // that are ordered according to the user supplied comparator.
+  // Append a filter that summarizes keys[0,n-1] to *dst.
+  //
+  // Warning: do not change the initial contents of *dst.  Instead,
+  // append the newly constructed filter to *dst.
+  virtual void CreateFilter(const Slice* keys, int n, std::string* dst)
+      const = 0;
     
-    
-    {  // No copying allowed
-  BlockBuilder(const BlockBuilder&);
-  void operator=(const BlockBuilder&);
-};
-    
-    template <typename T>
-bool SingletonHolder<T>::hasLiveInstance() {
-  return !instance_weak_.expired();
-}
-    
-    
-    {  Type type_;
-};
-    
-      void push(T val) {
-    auto node = new Node(val, head());
-    while (!cas_head(node->next_, node)) {
-      /* try again */;
+      LOG(INFO) << 'Writing Training data';
+  for (int fileid = 0; fileid < kCIFARTrainBatches; ++fileid) {
+    // Open files
+    LOG(INFO) << 'Training Batch ' << fileid + 1;
+    string batchFileName = input_folder + '/data_batch_'
+      + caffe::format_int(fileid+1) + '.bin';
+    std::ifstream data_file(batchFileName.c_str(),
+        std::ios::in | std::ios::binary);
+    CHECK(data_file) << 'Unable to open train file #' << fileid + 1;
+    for (int itemid = 0; itemid < kCIFARBatchSize; ++itemid) {
+      read_image(&data_file, &label, str_buffer);
+      datum.set_label(label);
+      datum.set_data(str_buffer, kCIFARImageNBytes);
+      string out;
+      CHECK(datum.SerializeToString(&out));
+      txn->Put(caffe::format_int(fileid * kCIFARBatchSize + itemid, 5), out);
     }
   }
+  txn->Commit();
+  train_db->Close();
     
-    #pragma once
-    
-    
-    { private:
-  folly::LifoSem sem_;
-  folly::MPMCQueue<T> queue_;
-};
-    
-    class alignas(hardware_destructive_interference_size) hazptr_rec {
-  friend class hazptr_domain;
-  friend class hazptr_holder;
-  friend struct hazptr_tc_entry;
-    }
-    
-    #include <folly/portability/GTest.h>
-    
-    void AbstractBtMessage::setBtMessageFactory(BtMessageFactory* factory)
-{
-  messageFactory_ = factory;
+    void read_image(std::ifstream* image_file, std::ifstream* label_file,
+        uint32_t index, uint32_t rows, uint32_t cols,
+        char* pixels, char* label) {
+  image_file->seekg(index * rows * cols + 16);
+  image_file->read(pixels, rows * cols);
+  label_file->seekg(index + 8);
+  label_file->read(label, 1);
 }
     
-    namespace aria2 {
+    #include <map>
+#include <string>
+#include <vector>
+    
+    /**
+ * @brief Abstract base class that factors out the BLAS code common to
+ *        ConvolutionLayer and DeconvolutionLayer.
+ */
+template <typename Dtype>
+class BaseConvolutionLayer : public Layer<Dtype> {
+ public:
+  explicit BaseConvolutionLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    }
+    
+    #include <vector>
+    
+    #endif  // CAFFE_CUDNN_RELU_LAYER_HPP_
+
+    
+    #endif  // CAFFE_DROPOUT_LAYER_HPP_
+
+    
+    /**
+ * @brief Compute elementwise operations, such as product and sum,
+ *        along multiple input Blobs.
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+template <typename Dtype>
+class EltwiseLayer : public Layer<Dtype> {
+ public:
+  explicit EltwiseLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
     }
     
     
-    {} // namespace aria2
-    
-      virtual ssize_t readData(unsigned char* data, size_t len,
-                           int64_t offset) CXX11_OVERRIDE;
-    
-    // DiskwriterFactory class template to create DiskWriter derived
-// object, ignoring filename.
-template <class DiskWriterType>
-class AnonDiskWriterFactory : public DiskWriterFactory {
-public:
-  AnonDiskWriterFactory() = default;
-  virtual ~AnonDiskWriterFactory() = default;
-    }
-    
-    
-    {  std::shared_ptr<AsyncNameResolver> asyncNameResolver_[2];
-  size_t numResolver_;
-  int resolverCheck_;
-  bool ipv4_;
-  bool ipv6_;
+    {  int M_;
+  int K_;
+  int N_;
+  bool bias_term_;
+  Blob<Dtype> bias_multiplier_;
 };
     
-    #include 'common.h'
+      /**
+   * @brief Computes the error gradient w.r.t. the exp inputs.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *      respect to the outputs
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
+   *      with respect to computed outputs @f$ y @f$
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$; Backward fills their diff with
+   *      gradients @f$
+   *        \frac{\partial E}{\partial x} =
+   *            \frac{\partial E}{\partial y} y \alpha \log_e(gamma)
+   *      @f$ if propagate_down[0]
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #include 'test/cpp/interop/server_helper.h'
+#include 'test/cpp/util/test_config.h'
+    
+    static void get_resource_usage(double* utime, double* stime) {
+#ifdef __linux__
+  struct rusage usage;
+  getrusage(RUSAGE_SELF, &usage);
+  *utime = time_double(&usage.ru_utime);
+  *stime = time_double(&usage.ru_stime);
+#else
+  *utime = 0;
+  *stime = 0;
+#endif
+}
+    
+    namespace HPHP {
+    }
+    
+    #include <algorithm>
+    
+      void bytes(size_t n, const uint8_t *bs) {
+    assertCanEmit(n);
+    if (n <= 8 && m_destBase == m_base) {
+      // If it is a modest number of bytes, try executing in one machine
+      // store. This allows control-flow edges, including nop, to be
+      // appear idempotent on other CPUs. If m_destBase != m_base then the
+      // current block is a temporary buffer and this write is neither required
+      // nor safe, as we may override an adjacent buffer or write off the end
+      // of an allocation.
+      union {
+        uint64_t qword;
+        uint8_t bytes[8];
+      } u;
+      u.qword = *(uint64_t*)dest();
+      for (size_t i = 0; i < n; ++i) {
+        u.bytes[i] = bs[i];
+      }
+    }
+    }
+    
+    Type typeToInt(Type ty) {
+  if (auto const v = tv(ty)) return ival(cellToInt(*v));
+  if (ty.subtypeOf(BNull))   return ival(0);
+  return TInt;
+}
+    
+    #endif // incl_HPHP_BACKTRACE_H_
+
+    
+    // mark ambigous pointers in the range [start,start+len). If the start or
+// end is a partial word, don't scan that word.
+template <bool apcgc>
+void FOLLY_DISABLE_ADDRESS_SANITIZER
+Collector::conservativeScan(const void* start, size_t len) {
+  constexpr uintptr_t M{7}; // word size - 1
+  auto s = (char**)((uintptr_t(start) + M) & ~M); // round up
+  auto e = (char**)((uintptr_t(start) + len) & ~M); // round down
+  cscanned_ += uintptr_t(e) - uintptr_t(s);
+  for (; s < e; s++) {
+    checkedEnqueue<apcgc>(
+      // Mask off the upper 16-bits to handle things like
+      // DiscriminatedPtr which stores things up there.
+      (void*)(uintptr_t(*s) & (-1ULL >> 16))
+    );
+  }
+}
+    
+    void addRootNode(HeapGraph& g, const PtrMap<const HeapObject*>& blocks,
+                 type_scan::Scanner& scanner,
+                 const void* h, size_t size, type_scan::Index ty) {
+  auto from = g.nodes.size();
+  g.nodes.push_back(
+    HeapGraph::Node{h, size, true, ty, -1, -1}
+  );
+  g.root_nodes.push_back(from);
+  scanner.scanByIndex(ty, h, size);
+  scanner.finish(
+    [&](const void* p, std::size_t size) {
+      conservativeScan(p, size, [&](const void** addr, const void* ptr) {
+        if (auto r = blocks.region(ptr)) {
+          auto to = blocks.index(r);
+          auto offset = uintptr_t(addr) - uintptr_t(h);
+          auto e = addPtr(g, from, to, HeapGraph::Ambiguous, offset);
+          g.root_ptrs.push_back(e);
+        }
+      });
+    },
+    [&](const void** addr) {
+      if (auto r = blocks.region(*addr)) {
+        auto to = blocks.index(r);
+        auto offset = uintptr_t(addr) - uintptr_t(h);
+        auto e = addPtr(g, from, to, HeapGraph::Counted, offset);
+        g.root_ptrs.push_back(e);
+      }
+    },
+    [&](const void* p) {
+      auto weak = static_cast<const WeakRefDataHandle*>(p);
+      auto addr = &(weak->wr_data->pointee.m_data.pobj);
+      if (auto r = blocks.region(*addr)) {
+        auto to = blocks.index(r);
+        // Note that offset is going to be meaningless because weak->wr_data is
+        // a shared_ptr, so &pointee.m_data.pobj will be inside the shared_ptr's
+        // internal node, allocated separately.
+        addPtr(g, from, to, HeapGraph::Weak, 0);
+      }
+    }
+  );
+}
+    
+    String getWrapperProtocol(const char* url, int* pathIndex = nullptr);
+Wrapper* getWrapper(const String& scheme, bool warn = true);
+Wrapper* getWrapperFromURI(const String& uri,
+                           int* pathIndex = nullptr, bool warn = true);
+    
+     private:
+  StreamBufferReader reader_;
+  int tmp_ch;
+  int num_prev;
+  unsigned char buf_prev[2];
+  // whether we need to do strict check
+  static const bool kStrictCheck = false;
+};
+/*! \brief the stream that write to base64, note we take from file pointers */
+class Base64OutStream: public dmlc::Stream {
+ public:
+  explicit Base64OutStream(dmlc::Stream *fp) : fp(fp) {
+    buf_top = 0;
+  }
+  virtual void Write(const void *ptr, size_t size) {
+    using base64::EncodeTable;
+    size_t tlen = size;
+    const unsigned char *cptr = static_cast<const unsigned char*>(ptr);
+    while (tlen) {
+      while (buf_top < 3  && tlen != 0) {
+        buf[++buf_top] = *cptr++; --tlen;
+      }
+      if (buf_top == 3) {
+        // flush 4 bytes out
+        PutChar(EncodeTable[buf[1] >> 2]);
+        PutChar(EncodeTable[((buf[1] << 4) | (buf[2] >> 4)) & 0x3F]);
+        PutChar(EncodeTable[((buf[2] << 2) | (buf[3] >> 6)) & 0x3F]);
+        PutChar(EncodeTable[buf[3] & 0x3F]);
+        buf_top = 0;
+      }
+    }
+  }
+  virtual size_t Read(void *ptr, size_t size) {
+    LOG(FATAL) << 'Base64OutStream do not support read';
+    return 0;
+  }
+  /*!
+   * \brief finish writing of all current base64 stream, do some post processing
+   * \param endch character to put to end of stream, if it is EOF, then nothing will be done
+   */
+  inline void Finish(char endch = EOF) {
+    using base64::EncodeTable;
+    if (buf_top == 1) {
+      PutChar(EncodeTable[buf[1] >> 2]);
+      PutChar(EncodeTable[(buf[1] << 4) & 0x3F]);
+      PutChar('=');
+      PutChar('=');
+    }
+    if (buf_top == 2) {
+      PutChar(EncodeTable[buf[1] >> 2]);
+      PutChar(EncodeTable[((buf[1] << 4) | (buf[2] >> 4)) & 0x3F]);
+      PutChar(EncodeTable[(buf[2] << 2) & 0x3F]);
+      PutChar('=');
+    }
+    buf_top = 0;
+    if (endch != EOF) PutChar(endch);
+    this->Flush();
+  }
+    
+    
+    {    return label_order_cache_;
+  }
+  /*! \brief clear all the information */
+  void Clear();
+  /*!
+   * \brief Load the Meta info from binary stream.
+   * \param fi The input stream
+   */
+  void LoadBinary(dmlc::Stream* fi);
+  /*!
+   * \brief Save the Meta info to binary stream
+   * \param fo The output stream.
+   */
+  void SaveBinary(dmlc::Stream* fo) const;
+  /*!
+   * \brief Set information in the meta info.
+   * \param key The key of the information.
+   * \param dptr The data pointer of the source array.
+   * \param dtype The type of the source data.
+   * \param num Number of elements in the source array.
+   */
+  void SetInfo(const char* key, const void* dptr, DataType dtype, size_t num);
+    
+    /*!
+ * \brief Whether to customize global PRNG.
+ */
+#ifndef XGBOOST_CUSTOMIZE_GLOBAL_PRNG
+#define XGBOOST_CUSTOMIZE_GLOBAL_PRNG XGBOOST_STRICT_R_MODE
+#endif
+    
+    /*! \brief Applies an elastic net penalty and per-leaf penalty. */
+class ElasticNet final : public SplitEvaluator {
+ public:
+  explicit ElasticNet(std::unique_ptr<SplitEvaluator> inner) {
+    if (inner) {
+      LOG(FATAL) << 'ElasticNet does not accept an inner SplitEvaluator';
+    }
+  }
+  void Init(
+      const std::vector<std::pair<std::string, std::string> >& args) override {
+    params_.InitAllowUnknown(args);
+  }
+    }
+    
+    namespace xgboost {
+namespace tree {
+// List of files that will be force linked in static links.
+DMLC_REGISTRY_LINK_TAG(updater_colmaker);
+DMLC_REGISTRY_LINK_TAG(updater_skmaker);
+DMLC_REGISTRY_LINK_TAG(updater_refresh);
+DMLC_REGISTRY_LINK_TAG(updater_prune);
+DMLC_REGISTRY_LINK_TAG(updater_fast_hist);
+DMLC_REGISTRY_LINK_TAG(updater_histmaker);
+DMLC_REGISTRY_LINK_TAG(updater_sync);
+#ifdef XGBOOST_USE_CUDA
+DMLC_REGISTRY_LINK_TAG(updater_gpu);
+DMLC_REGISTRY_LINK_TAG(updater_gpu_hist);
+#endif
+}  // namespace tree
+}  // namespace xgboost
+
+    
+    SEXP XGDMatrixGetInfo_R(SEXP handle, SEXP field) {
+  SEXP ret;
+  R_API_BEGIN();
+  bst_ulong olen;
+  const float *res;
+  CHECK_CALL(XGDMatrixGetFloatInfo(R_ExternalPtrAddr(handle),
+                                   CHAR(asChar(field)),
+                                 &olen,
+                                 &res));
+  ret = PROTECT(allocVector(REALSXP, olen));
+  for (size_t i = 0; i < olen; ++i) {
+    REAL(ret)[i] = res[i];
+  }
+  R_API_END();
+  UNPROTECT(1);
+  return ret;
+}
+    
+    
+    {/// Clear decorations for a source when it updates.
+void clearDecorations(const std::string& source);
+}
+
+    
+    #include <osquery/core.h>
+#include <osquery/system.h>
+    
+    /**
+ * @brief Generate the separator string for query results
+ *
+ * @param lengths The data returned from computeQueryDataLengths
+ * @param columns The order of the keys (since maps are unordered)
+ *
+ * @return A string, with a newline, representing your separator
+ */
+std::string generateToken(const std::map<std::string, size_t>& lengths,
+                          const std::vector<std::string>& columns);
+    
+    TEST_F(PrinterTests, test_generate_separator) {
+  std::map<std::string, size_t> lengths;
+  for (const auto& row : q) {
+    computeRowLengths(row, lengths);
+  }
+    }
+    
+    
+    {FirehoseLogForwarder::Result FirehoseLogForwarder::getResult(
+    Outcome& outcome) const {
+  return outcome.GetResult().GetRequestResponses();
+}
+}
