844dba2c5020eb8340695901f02a72e0584dd2c5
SUNsung <weatherius@gmail.com>
2018-08-08 14:09:31 +0300

    Rewrite [05MTOjAlE1UnQ05jNTilX8Tf8605eUsAeskfWdE05t4c.rb] File in: 2018-08-08 14:09:31

--- ruby/05MTOjAlE1UnQ05jNTilX8Tf8605eUsAeskfWdE05t4c.rb
+++ ruby/05MTOjAlE1UnQ05jNTilX8Tf8605eUsAeskfWdE05t4c.rb
@@ -1,37 +1,17 @@
 
         
-        # puts '\nDone.'
-
-    
-      def validate_type
-    errors.add(:type, 'cannot be changed once an instance has been created') if type_changed? && !new_record?
-    errors.add(:type, 'is not a valid type') unless self.class.valid_type?(type)
+        def check_link(uri)
+  HTTParty.head(uri, :verify => false).code.to_i.tap do |status|
+    if (400..422).include?(status)
+      if status != 403 && !uri.exclude?('udemy.com')
+        raise 'Request had status #{status}'
+      else
+        putc('S')
+      end
+    end
   end
-    
-    EOS
 end
     
-    With optional '-t <bundle-id>', silently test if a given app
-is running, exiting with an error code if not.
-    
-    require 'formula'
-require 'system_config'
-require 'stringio'
-require 'socket'
-    
-              urls += mirrors
-    
-      def as_json(options={})
-    {
-      poll_id:             id,
-      post_id:             status_message.id,
-      question:            question,
-      poll_answers:        poll_answers,
-      participation_count: participation_count
-    }
-  end
-    
-        def index
-      pods_json = PodPresenter.as_collection(Pod.all)
-    
-            private
+        def poster
+      'poster='#{@poster}'' if @poster
+    end
