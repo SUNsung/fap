29b72e596e3d6925c9d56bb2d6b939076705e9ae
SUNsung <weatherius@gmail.com>
2018-08-08 10:03:06 +0300

    Rewrite [06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py] File in: 2018-08-08 10:03:05

--- pyton/06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py
+++ pyton/06jIlAAhpXsxQ06qDtJ33J7lJk06vQ2.X3MSnZM06rwK.py
@@ -1,167 +1,212 @@
 
         
-            for idx, (loader, srcobj, triple) in enumerate(attempts):
-        if isinstance(srcobj, Flask):
-            src_info = 'application '%s'' % srcobj.import_name
-        elif isinstance(srcobj, Blueprint):
-            src_info = 'blueprint '%s' (%s)' % (srcobj.name,
-                                                srcobj.import_name)
-        else:
-            src_info = repr(srcobj)
-    
-    
-class SecureCookieSession(CallbackDict, SessionMixin):
-    '''Base class for sessions based on signed cookies.
-    
-            # If the request method is HEAD and we don't have a handler for it
-        # retry with GET.
-        if meth is None and request.method == 'HEAD':
-            meth = getattr(self, 'get', None)
-    
-    
-def build():
-    cmd = [sys.executable, 'setup.py', 'sdist', 'bdist_wheel']
-    Popen(cmd).wait()
-    
-    import flask
-from flask.sessions import SessionInterface
-    
-        h = history.history
-    assert_array_almost_equal(h['loss'], h['weighted_' + loss_full_name], decimal=decimal)
-    
-        # a Model inside a Sequential
-    x = Input(shape=(1,))
-    y = Dense(2)(x)
-    inner_model = Model(x, y)
-    outer_model = Sequential()
-    outer_model.add(inner_model)
-    assert outer_model.trainable_weights == inner_model.trainable_weights
-    inner_model.trainable = False
-    assert outer_model.trainable_weights == []
-    inner_model.trainable = True
-    inner_model.layers[-1].trainable = False
-    assert outer_model.trainable_weights == []
-    
-        z_mean, z_log_var = args
-    batch = K.shape(z_mean)[0]
-    dim = K.int_shape(z_mean)[1]
-    # by default, random_normal has mean=0 and std=1.0
-    epsilon = K.random_normal(shape=(batch, dim))
-    return z_mean + K.exp(0.5 * z_log_var) * epsilon
-    
-        reconstruction_loss *= image_size * image_size
-    kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
-    kl_loss = K.sum(kl_loss, axis=-1)
-    kl_loss *= -0.5
-    vae_loss = K.mean(reconstruction_loss + kl_loss)
-    vae.add_loss(vae_loss)
-    vae.compile(optimizer='rmsprop')
-    vae.summary()
-    plot_model(vae, to_file='vae_cnn.png', show_shapes=True)
-    
-    print('Loading data...')
-(x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=max_words,
-                                                         test_split=0.2)
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
-    
-        history = model.fit(x, ys, validation_split=0.05, batch_size=10,
-                        verbose=0, epochs=3)
-    ground_truth = -np.log(0.5)
-    assert(np.abs(history.history['loss'][-1] - ground_truth) < 0.06)
-    
-                # check that output changes after states are reset
-            # (even though the model itself didn't change)
-            layer.reset_states()
-            out3 = model.predict(np.ones_like(inputs))
-            assert(out2.max() != out3.max())
-    
-                    # Make it more robust by adding noise.
-                # The idea is that if during inference,
-                # the value of the pixel is not exactly one,
-                # we need to train the network to be robust and still
-                # consider it as a pixel belonging to a square.
-                if np.random.randint(0, 2):
-                    noise_f = (-1)**np.random.randint(0, 2)
-                    noisy_movies[i, t,
-                                 x_shift - w - 1: x_shift + w + 1,
-                                 y_shift - w - 1: y_shift + w + 1,
-                                 0] += noise_f * 0.1
-    
-    - RNNs are tricky. Choice of batch size is important,
-choice of loss and optimizer is critical, etc.
-Some configurations won't converge.
-    
-            # put all lines in the file into a Python list
-        strings = f.readlines()
         
-        # above line leaves trailing newline characters; strip them out
-        strings = [x.strip(u'\n') for x in strings]
-        
-        # remove empty-lines and comments
-        strings = [x for x in strings if x and not x.startswith(u'#')]
-        
-        # insert empty string since all are being removed
-        strings.insert(0, u'')
+class TestAgeRestriction(unittest.TestCase):
+    def _assert_restricted(self, url, filename, age, old_age=None):
+        self.assertTrue(_download_restricted(url, filename, old_age))
+        self.assertFalse(_download_restricted(url, filename, age))
     
-    def _create_and_install_waiters(fs, return_when):
-    if return_when == _AS_COMPLETED:
-        waiter = _AsCompletedWaiter()
-    elif return_when == FIRST_COMPLETED:
-        waiter = _FirstCompletedWaiter()
-    else:
-        pending_count = sum(
-                f._state not in [CANCELLED_AND_NOTIFIED, FINISHED] for f in fs)
+        _ALL_CLASSES = [
+        klass
+        for name, klass in globals().items()
+        if name.endswith('IE') and name != 'GenericIE'
+    ]
+    _ALL_CLASSES.append(GenericIE)
     
-            f = Future()
-        f.set_exception(Exception('test'))
-        f.add_done_callback(fn)
-        self.assertEqual(('test',), callback_exception[0].args)
+            return info_dict
+
     
-    
-  def tearDown( self ):
-    self._request = None
-    
-    
-@YouCompleteMeInstance()
-def SendCommandRequest_BuildRange_NoVisualMarks_test( ycm, *args ):
-  current_buffer = VimBuffer( 'buffer', contents = [ 'first line',
-                                                     'second line' ] )
-  with MockVimBuffers( [ current_buffer ], [ current_buffer ] ):
-    with patch( 'ycm.youcompleteme.SendCommandRequest' ) as send_request:
-      ycm.SendCommandRequest( [ 'GoTo' ], 'python', '', True, 1, 2 )
-      send_request.assert_called_once_with(
-        [ 'GoTo' ],
-        'python',
-        '',
-        {
-          'options': {
-            'tab_size': 2,
-            'insert_spaces': True
-          },
-          'range': {
-            'start': {
-              'line_num': 1,
-              'column_num': 1
-            },
-            'end': {
-              'line_num': 2,
-              'column_num': 12
-            }
-          }
+        def _real_extract(self, url):
+        webpage = self._download_webpage(url, url_basename(url))
+        cnn_url = self._html_search_regex(r'video:\s*'([^']+)'', webpage, 'cnn url')
+        return {
+            '_type': 'url',
+            'url': 'http://cnn.com/video/?/video/' + cnn_url,
+            'ie_key': CNNIE.ie_key(),
         }
-      )
+
+    
+      def testGetBulkPredictions(self):
+    with self.test_session():
+      master = MockMaster()
+      component = MockComponent(master, master.spec.component[0])
+      component.network = mst_units.MstSolverNetwork(component)
+    
+    import os.path
+import tempfile
+    
+      # Graph building.
+  tf.logging.info('Building the graph')
+  g = tf.Graph()
+  with g.as_default(), tf.device('/device:CPU:0'):
+    hyperparam_config = spec_pb2.GridPoint()
+    hyperparam_config.use_moving_average = True
+    builder = graph_builder.MasterBuilder(master_spec, hyperparam_config)
+    annotator = builder.add_annotation()
+    builder.add_saver()
+    
+        '''
+    is_windows = is_windows
+    config_dir = DEFAULT_CONFIG_DIR
+    stdin = sys.stdin
+    stdin_isatty = stdin.isatty()
+    stdin_encoding = None
+    stdout = sys.stdout
+    stdout_isatty = stdout.isatty()
+    stdout_encoding = None
+    stderr = sys.stderr
+    stderr_isatty = stderr.isatty()
+    colors = 256
+    if not is_windows:
+        if curses:
+            try:
+                curses.setupterm()
+                colors = curses.tigetnum('colors')
+            except curses.error:
+                pass
+    else:
+        # noinspection PyUnresolvedReferences
+        import colorama.initialise
+        stdout = colorama.initialise.wrap_stream(
+            stdout, convert=None, strip=None,
+            autoreset=True, wrap=True
+        )
+        stderr = colorama.initialise.wrap_stream(
+            stderr, convert=None, strip=None,
+            autoreset=True, wrap=True
+        )
+        del colorama
+    
+    UNICODE = FILE_CONTENT
+
+    
+        def test_verify_custom_ca_bundle_invalid_path(self, httpbin_secure):
+        # since 2.14.0 requests raises IOError
+        with pytest.raises((SSLError, IOError)):
+            http(httpbin_secure.url + '/get', '--verify', '/__not_found__')
     
     
-def FormatDebugInfoResponse_NoResponse_test():
-  assert_that(
-    FormatDebugInfoResponse( None ),
-    equal_to( 'Server errored, no debug info from server\n' )
-  )
+def repr_dict_nice(d):
+    def prepare_dict(d):
+        for k, v in d.items():
+            if isinstance(v, dict):
+                v = dict(prepare_dict(v))
+            elif isinstance(v, bytes):
+                v = v.decode('utf8')
+            elif not isinstance(v, (int, str)):
+                v = repr(v)
+            yield k, v
+    return json.dumps(
+        dict(prepare_dict(d)),
+        indent=4, sort_keys=True,
+    )
     
-      try:
-    filepath = os.path.join( DIR_OF_YCMD, 'PYTHON_USED_DURING_BUILDING' )
-    return utils.ReadFile( filepath ).strip()
-  # We need to check for IOError for Python2 and OSError for Python3
-  except ( IOError, OSError ):
-    return None
+    
+def test_default_options_overwrite(httpbin):
+    env = MockEnvironment()
+    env.config['default_options'] = ['--form']
+    env.config.save()
+    r = http('--json', httpbin.url + '/post', 'foo=bar', env=env)
+    assert r.json['json'] == {'foo': 'bar'}
+    
+            self.concurrent -= 1
+        return ''
+    
+        def __init__(self, settings):
+        if not settings.getbool('AJAXCRAWL_ENABLED'):
+            raise NotConfigured
+    
+        def test_expandvars(self):
+        with support.EnvironmentVarGuard() as env:
+            env.clear()
+            env['foo'] = 'bar'
+            env['{foo'] = 'baz1'
+            env['{foo}'] = 'baz2'
+            tester('ntpath.expandvars('foo')', 'foo')
+            tester('ntpath.expandvars('$foo bar')', 'bar bar')
+            tester('ntpath.expandvars('${foo}bar')', 'barbar')
+            tester('ntpath.expandvars('$[foo]bar')', '$[foo]bar')
+            tester('ntpath.expandvars('$bar bar')', '$bar bar')
+            tester('ntpath.expandvars('$?bar')', '$?bar')
+            tester('ntpath.expandvars('$foo}bar')', 'bar}bar')
+            tester('ntpath.expandvars('${foo')', '${foo')
+            tester('ntpath.expandvars('${{foo}}')', 'baz1}')
+            tester('ntpath.expandvars('$foo$foo')', 'barbar')
+            tester('ntpath.expandvars('$bar$bar')', '$bar$bar')
+            tester('ntpath.expandvars('%foo% bar')', 'bar bar')
+            tester('ntpath.expandvars('%foo%bar')', 'barbar')
+            tester('ntpath.expandvars('%foo%%foo%')', 'barbar')
+            tester('ntpath.expandvars('%%foo%%foo%foo%')', '%foo%foobar')
+            tester('ntpath.expandvars('%?bar%')', '%?bar%')
+            tester('ntpath.expandvars('%foo%%bar')', 'bar%bar')
+            tester('ntpath.expandvars('\'%foo%\'%bar')', '\'%foo%\'%bar')
+            tester('ntpath.expandvars('bar\'%foo%')', 'bar\'%foo%')
+    
+    # output pattern for missing module
+missing_pattern = '''\
+No Python documentation found for %r.
+Use help() to get the interactive help utility.
+Use help(str) for help on the str class.'''.replace('\n', os.linesep)
+    
+        async def sock_accept(self, sock):
+        return await self._proactor.accept(sock)
+    
+        def test_force_close_idempotent(self):
+        tr = self.socket_transport()
+        tr._closing = True
+        tr._force_close(None)
+        test_utils.run_briefly(self.loop)
+        self.assertFalse(self.protocol.connection_lost.called)
+    
+        PATTERN = '''
+    power< head=any+
+         trailer< '.' method=('keys'|'items'|'values'|
+                              'iterkeys'|'iteritems'|'itervalues'|
+                              'viewkeys'|'viewitems'|'viewvalues') >
+         parens=trailer< '(' ')' >
+         tail=any*
+    >
+    '''
+    
+        PATTERN = '''
+        atom< ('[' | '(')
+            (listmaker< any
+                comp_for<
+                    'for' NAME 'in'
+                    target=testlist_safe< any (',' any)+ [',']
+                     >
+                    [any]
+                >
+            >
+            |
+            testlist_gexp< any
+                comp_for<
+                    'for' NAME 'in'
+                    target=testlist_safe< any (',' any)+ [',']
+                     >
+                    [any]
+                >
+            >)
+        (']' | ')') >
+    '''
+    
+        def check_no_unicode(self, algorithm_name):
+        # Unicode objects are not allowed as input.
+        constructors = self.constructors_to_test[algorithm_name]
+        for hash_object_constructor in constructors:
+            self.assertRaises(TypeError, hash_object_constructor, 'spam')
+    
+            for filename in files:
+            if os.path.islink(filename): continue
+            mode = defaultMode
+            if filename.endswith(self.shlib_suffix): mode = sharedLibMode
+            log.info('changing mode of %s to %o', filename, mode)
+            if not self.dry_run: os.chmod(filename, mode)
+    
+    def get_extension(link):
+    extension = os.path.splitext(link)[1][1:]
+    if extension in ['pdf', 'html']:
+        return extension
+    if 'pdf' in extension:
+        return 'pdf'    
+    return 'pdf'    
