01100b2452425d53892ca692fb3855f4c2536fe3
SUNsung <weatherius@gmail.com>
2018-08-08 21:58:43 +0300

    Rewrite [26sUWxogB.Vc626S62PPP1Lpjc26VhlMtZMTxOg26rFi.py] File in: 2018-08-08 21:58:42

--- pyton/26sUWxogB.Vc626S62PPP1Lpjc26VhlMtZMTxOg26rFi.py
+++ pyton/26sUWxogB.Vc626S62PPP1Lpjc26VhlMtZMTxOg26rFi.py
@@ -1,61 +1,124 @@
 
         
-            old_layer = keras.layers.Dense(2, bias=True,
-                                   b_regularizer='l1',
-                                   b_constraint='maxnorm', name='d')
-    new_layer = keras.layers.Dense(2, use_bias=True,
-                                   bias_regularizer='l1',
-                                   bias_constraint='max_norm', name='d')
-    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer.get_config())
+        from mrjob.job import MRJob
     
-        # by setting the `trainable` argument, in Sequential
-    model = Sequential()
-    layer = Dense(2, input_dim=1)
-    model.add(layer)
-    assert model.trainable_weights == layer.trainable_weights
-    layer.trainable = False
-    assert model.trainable_weights == []
+        def __init__(self, from_user_id, to_user_id, request_status, timestamp):
+        self.from_user_id = from_user_id
+        self.to_user_id = to_user_id
+        self.request_status = request_status
+        self.timestamp = timestamp
     
-        # Arguments:
-        models (tuple): encoder and decoder models
-        data (tuple): test data and label
-        batch_size (int): prediction batch size
-        model_name (string): which model is using this function
-    '''
+        def __init__(self, seller_category_map, seller_category_overrides_map):
+        self.seller_category_map = seller_category_map
+        self.seller_category_overrides_map = seller_category_overrides_map
     
-            assert_allclose(out1, out2, atol=1e-5)
+        def crawl(self):
+        while True:
+            page = self.data_store.extract_max_priority_page()
+            if page is None:
+                break
+            if self.data_store.crawled_similar(page.signature):
+                self.data_store.reduce_priority_link_to_crawl(page.url)
+            else:
+                self.crawl_page(page)
+            page = self.data_store.extract_max_priority_page()
+
+    
+        @pytest.fixture(autouse=True)
+    def setup(self):
+        '''CaseInsensitiveDict instance with 'Accept' header.'''
+        self.case_insensitive_dict = CaseInsensitiveDict()
+        self.case_insensitive_dict['Accept'] = 'application/json'
+    
+    This module contains the set of Requests' exceptions.
+'''
+from urllib3.exceptions import HTTPError as BaseHTTPError
+    
+        # Check cryptography version
+    from cryptography import __version__ as cryptography_version
+    _check_cryptography(cryptography_version)
+except ImportError:
+    pass
+    
+                gc.collect()
+            print('- benchmarking LassoLars')
+            clf = LassoLars(alpha=alpha, fit_intercept=False,
+                            normalize=False, precompute=precompute)
+            tstart = time()
+            clf.fit(X, Y)
+            lars_lasso_results.append(time() - tstart)
+    
+        n = 10
+    step = 10000
+    n_samples = 10000
+    dim = 10
+    n_classes = 10
+    for i in range(n):
+        print('============================================')
+        print('Entering iteration %s of %s' % (i, n))
+        print('============================================')
+        n_samples += step
+        X = np.random.randn(n_samples, dim)
+        Y = np.random.randint(0, n_classes, (n_samples,))
+        bench_scikit_tree_classifier(X, Y)
+        Y = np.random.randn(n_samples)
+        bench_scikit_tree_regressor(X, Y)
+    
+            text_filename = os.path.join(text_lang_folder,
+                                     '%s_%04d.txt' % (lang, i))
+        print('Writing %s' % text_filename)
+        open(text_filename, 'wb').write(content.encode('utf-8', 'ignore'))
+        i += 1
+    
+    calibrated_classifier = sig_clf.calibrated_classifiers_[0]
+prediction = np.vstack([calibrator.predict(this_p)
+                        for calibrator, this_p in
+                        zip(calibrated_classifier.calibrators_, p.T)]).T
+prediction /= prediction.sum(axis=1)[:, None]
     
     
-@keras_test
-def test_min_max_norm():
-    array = get_example_array()
-    for m in get_test_values():
-        norm_instance = constraints.min_max_norm(min_value=m, max_value=m * 2)
-        normed = norm_instance(K.variable(array))
-        value = K.eval(normed)
-        l2 = np.sqrt(np.sum(np.square(value), axis=0))
-        assert not l2[l2 < m]
-        assert not l2[l2 > m * 2 + 1e-5]
+n_train = 20  # samples for training
+n_test = 200  # samples for testing
+n_averages = 50  # how often to repeat classification
+n_features_max = 75  # maximum number of features
+step = 4  # step size for the calculation
     
     
-@pytest.mark.skipif(K.backend() != 'tensorflow', reason='Requires TensorFlow backend')
-@keras_test
-def test_embedding_with_clipnorm():
-    model = Sequential()
-    model.add(layers.Embedding(input_dim=1, output_dim=1))
-    model.compile(optimizer=optimizers.SGD(clipnorm=0.1), loss='mse')
-    model.fit(np.array([[0]]), np.array([[[0.5]]]), epochs=1)
+def plot_2d(ax, n_labels=1, n_classes=3, length=50):
+    X, Y, p_c, p_w_c = make_ml_clf(n_samples=150, n_features=2,
+                                   n_classes=n_classes, n_labels=n_labels,
+                                   length=length, allow_unlabeled=False,
+                                   return_distributions=True,
+                                   random_state=RANDOM_SEED)
     
-                    layer = convolutional_recurrent.ConvLSTM2D(**kwargs)
-                layer.build(inputs.shape)
-                assert len(layer.losses) == 3
-                assert layer.activity_regularizer
-                output = layer(K.variable(np.ones(inputs.shape)))
-                assert len(layer.losses) == 4
-                K.eval(output)
     
-    from __future__ import print_function
-import numpy as np
+def main():
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([
+        (r'/', MainHandler),
+    ])
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
     
-        if results.overwrite and os.path.exists(output_directory):
-        shutil.rmtree(output_directory)
+    
+class MainHandler(BaseHandler, TwitterMixin):
+    @authenticated
+    @gen.coroutine
+    def get(self):
+        timeline = yield self.twitter_request(
+            '/statuses/home_timeline',
+            access_token=self.current_user['access_token'])
+        self.render('home.html', timeline=timeline)
+    
+    # These benchmarks are delicate.  They hit various fast-paths in the gen
+# machinery in order to stay synchronous so we don't need an IOLoop.
+# This removes noise from the results, but it's easy to change things
+# in a way that completely invalidates the results.
+    
+    Circular references are not leaks per se, because they will eventually
+be GC'd. However, on CPython, they prevent the reference-counting fast
+path from being used and instead rely on the slower full GC. This
+increases memory footprint and CPU overhead, so we try to eliminate
+circular references created by normal operation.
+'''
