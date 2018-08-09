a4780df4173a9402ab3a7a73e2599bcb1472d5a3
SUNsung <weatherius@gmail.com>
2018-08-09 12:37:57 +0300

    Rewrite [288QKMJSg0BE28y5l2IlotIfU280en1k8PXX3s28v.S.py] File in: 2018-08-09 12:37:56

--- pyton/288QKMJSg0BE28y5l2IlotIfU280en1k8PXX3s28v.S.py
+++ pyton/288QKMJSg0BE28y5l2IlotIfU280en1k8PXX3s28v.S.py
@@ -1,79 +1,136 @@
 
         
-        
-class MethodView(with_metaclass(MethodViewType, View)):
-    '''A class-based view that dispatches request methods to the corresponding
-    class methods. For example, if you implement a ``get`` method, it will be
-    used to handle ``GET`` requests. ::
+        import rsa
+import json
+from binascii import hexlify
     
-    from httpie.compat import is_windows
-from httpie.config import DEFAULT_CONFIG_DIR, Config
+            return self._extract_nuevo(
+            'http://www.anitube.se/nuevo/econfig.php?key=%s' % key, video_id)
+
     
-        def __init__(self, chunk_size=CHUNK_SIZE, **kwargs):
-        super(RawStream, self).__init__(**kwargs)
-        self.chunk_size = chunk_size
+    from .common import InfoExtractor
+    
+      If a FeedForwardNetwork is configured with no hidden units, this component
+  acts as a 'bulk softmax' component.
+  '''
+    
+      Args:
+    master_spec_path: Path to a proto-text master spec.
+    params_path: Path to the parameters file to export.
+    export_path: Path to export the SavedModel to.
+    export_moving_averages: Whether to export the moving average parameters.
+    build_runtime_graph: Whether to build a graph for use by the runtime.
+  '''
+    
+      Returns:
+    [B] vector Z of log partition function values, where
+      Z[b] = log(
+          \sum_{tree spanning batch item b}
+              score(root_of(tree)) \prod_{arc in tree} score(arc))
+  '''
+  orig_dtype = scores.dtype.base_dtype
+  scores_bxmxm = tf.to_double(scores)  # use doubles to reduce under/overflow
+  shape_bxmxm = tf.shape(scores_bxmxm)
+  batch_size = shape_bxmxm[0]
+  max_nodes = shape_bxmxm[1]
+  total_nodes = batch_size * max_nodes
+    
+    https://en.wikipedia.org/wiki/Blackboard_system
+'''
+    
+        def __init__(self, HierachicalStateMachine):
+        self._hsm = HierachicalStateMachine
+    
+        def test_frozen_pool(self):
+        with ObjectPool(self.sample_queue) as pool:
+            self.assertEqual(pool, 'first')
+            self.assertEqual(pool, 'first')
+        self.assertTrue(self.sample_queue.get() == 'second')
+        self.assertFalse(self.sample_queue.empty())
+        self.assertTrue(self.sample_queue.get() == 'first')
+        self.assertTrue(self.sample_queue.empty())
+    
+        def test_bunch_launch(self):
+        self.runner.runAll()
+        output = self.out.getvalue().strip()
+        self.assertEqual(output, str(self.average_result_tc1 + '\n\n' +
+                         self.average_result_tc2 + '\n\n' +
+                         self.average_result_tc3))
+
+    
+    from dft.setter_injection import TimeDisplay, MidnightTimeProvider, ProductionCodeTimeProvider, datetime
+    
+        def test_display_current_time_at_midnight(self):
+        '''
+        Would almost always fail (despite of right at/after midnight) if
+        untestable production code would have been used.
+        '''
+        time_provider_stub = MidnightTimeProvider()
+        class_under_test = TimeDisplay()
+        expected_time = '<span class=\'tinyBoldText\'>24:01</span>'
+        self.assertEqual(class_under_test.get_current_time_as_html_fragment(time_provider_stub), expected_time)
+    
+        def __init__(self, param):
+    
+        def __init__(self, radio):
+        self.radio = radio
+        self.stations = ['81.3', '89.1', '103.9']
+        self.pos = 0
+        self.name = 'FM'
+    
+    define('debug', default=False, group='application',
+       help='run in debug mode (with automatic reloading)')
+# The following settings should probably be defined in secrets.cfg
+define('twitter_consumer_key', type=str, group='application')
+define('twitter_consumer_secret', type=str, group='application')
+define('cookie_secret', type=str, group='application',
+       default='__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE__',
+       help='signing key for secure cookies')
     
     
-def test_follow_all_redirects_shown(httpbin):
-    r = http('--follow', '--all', httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 3
-    assert r.count('HTTP/1.1 302 FOUND', 2)
-    assert HTTP_OK in r
+class AuthLoginHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
+    async def get(self):
+        my_url = (self.request.protocol + '://' + self.request.host +
+                  '/auth/login?next=' +
+                  tornado.escape.url_escape(self.get_argument('next', '/')))
+        if self.get_argument('code', False):
+            user = await self.get_authenticated_user(
+                redirect_uri=my_url,
+                client_id=self.settings['facebook_api_key'],
+                client_secret=self.settings['facebook_secret'],
+                code=self.get_argument('code'))
+            self.set_secure_cookie('fbdemo_user', tornado.escape.json_encode(user))
+            self.redirect(self.get_argument('next', '/'))
+            return
+        self.authorize_redirect(redirect_uri=my_url,
+                                client_id=self.settings['facebook_api_key'],
+                                extra_params={'scope': 'user_posts'})
     
-    # def test_unicode_url_verbose(self):
-#     r = http(httpbin.url + '--verbose', u'/get?test=' + UNICODE)
-#     assert HTTP_OK in r
+    from tornado.ioloop import IOLoop
+from tornado.options import define, options, parse_command_line
+from tornado.web import RequestHandler, Application
     
-        >>> humanize_bytes(1)
-    '1 B'
-    >>> humanize_bytes(1024, precision=1)
-    '1.0 kB'
-    >>> humanize_bytes(1024 * 123, precision=1)
-    '123.0 kB'
-    >>> humanize_bytes(1024 * 12342, precision=1)
-    '12.1 MB'
-    >>> humanize_bytes(1024 * 12342, precision=2)
-    '12.05 MB'
-    >>> humanize_bytes(1024 * 1234, precision=2)
-    '1.21 MB'
-    >>> humanize_bytes(1024 * 1234 * 1111, precision=2)
-    '1.31 GB'
-    >>> humanize_bytes(1024 * 1234 * 1111, precision=1)
-    '1.3 GB'
     
-        def test_binary_suppresses_when_not_terminal_but_pretty(self):
-        env = MockEnvironment(stdin_isatty=True, stdout_isatty=False)
-        r = http('--pretty=all', 'GET', self.url,
-                 env=env)
-        assert BINARY_SUPPRESSED_NOTICE.decode() in r
+# Using HTTP PUT, upload one raw file. This is preferred for large files since
+# the server can stream the data instead of buffering it entirely in memory.
+@gen.coroutine
+def post(filenames):
+    client = httpclient.AsyncHTTPClient()
+    boundary = uuid4().hex
+    headers = {'Content-Type': 'multipart/form-data; boundary=%s' % boundary}
+    producer = partial(multipart_producer, boundary, filenames)
+    response = yield client.fetch('http://localhost:8888/post',
+                                  method='POST',
+                                  headers=headers,
+                                  body_producer=producer)
     
-        config['implicit_content_type'] = 'json'
-    config.save()
-    config.load()
-    assert 'implicit_content_type' not in config
-    assert not config['default_options']
+        # Do a little work. Alternately, could leave this script running and
+    # poke at it with a browser.
+    client = httpclient.AsyncHTTPClient()
+    yield client.fetch('http://127.0.0.1:8888/dummy/')
+    yield client.fetch('http://127.0.0.1:8888/dummyasync/', raise_error=False)
     
-        def test_colors_option(self, httpbin):
-        env = MockEnvironment(colors=256)
-        r = http('--print=B', '--pretty=colors',
-                 'GET', httpbin.url + '/get', 'a=b',
-                 env=env)
-        # Tests that the JSON data isn't formatted.
-        assert not r.strip().count('\n')
-        assert COLOR in r
+    define('family', default='unspec',
+       help='Address family to query: unspec, inet, or inet6')
     
-        for test in tests:
-      yield MessageTest, test[ 0 ], test[ 1 ]
-    
-      AssertBuffersAreEqualAsBytes( [ 'aEb',
-                                  'bFb',
-                                  'bGbcccc',
-                                  'aBa',
-                                  'aCa' ], result_buffer )
-    
-      try:
-    filepath = os.path.join( DIR_OF_YCMD, 'PYTHON_USED_DURING_BUILDING' )
-    return utils.ReadFile( filepath ).strip()
-  # We need to check for IOError for Python2 and OSError for Python3
-  except ( IOError, OSError ):
-    return None
+    TMPDIR = r'c:\tornado_bootstrap'
