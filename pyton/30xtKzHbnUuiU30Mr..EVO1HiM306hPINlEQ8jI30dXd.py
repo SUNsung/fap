0952407ceac6a0fbb413f164b3b98f5a94cc2161
SUNsung <weatherius@gmail.com>
2018-08-09 23:28:35 +0300

    Rewrite [30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py] File in: 2018-08-09 23:28:34

--- pyton/30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py
+++ pyton/30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py
@@ -1,98 +1,79 @@
 
         
-        if len(sys.argv) == 2 and sys.argv[1] == '--list':
-    print(json.dumps(result))
-elif len(sys.argv) == 3 and sys.argv[1] == '--host':
-    print(json.dumps({}))
-else:
-    print('Need an argument, either --list or --host <host>')
+                def to_json(self, value):
+            return [[k, self.serializer.tag(v)] for k, v in iteritems(value)]
+    
+    
+def attach_enctype_error_multidict(request):
+    '''Since Flask 0.8 we're monkeypatching the files object in case a
+    request is detected that does not use multipart form data but the files
+    object is accessed.
+    '''
+    oldcls = request.files.__class__
+    class newcls(oldcls):
+        def __getitem__(self, key):
+            try:
+                return oldcls.__getitem__(self, key)
+            except KeyError:
+                if key not in request.form:
+                    raise
+                raise DebugFilesKeyError(request, key)
+    newcls.__name__ = oldcls.__name__
+    newcls.__module__ = oldcls.__module__
+    request.files.__class__ = newcls
+    
+            This check is usually skipped if the session was deleted.
+    
+            for srcobj, loader in self._iter_loaders(template):
+            try:
+                rv = loader.get_source(environment, template)
+                if trv is None:
+                    trv = rv
+            except TemplateNotFound:
+                rv = None
+            attempts.append((loader, srcobj, rv))
+    
+        with open(filename) as f:
+        contents = re.sub(
+            r'^(\s*%s\s*=\s*')(.+?)(')' % pattern,
+            inject_version, f.read(),
+            flags=re.DOTALL | re.MULTILINE
+        )
+    
+        sys.meta_path.append(Loader())
+    request.addfinalizer(sys.meta_path.pop)
+    
+                greenlets.append(greenlet(g))
+            return 'Hello World!'
+    
+        def __init__(self, expecting, input):
+        RecognitionException.__init__(self, input)
+        
+        self.expecting = expecting
+    
+    '''
+    
+    
+class PostModule(tornado.web.UIModule):
+    def render(self, post):
+        return self.render_string('modules/post.html', post=post)
+    
+    
+def main():
+    parse_command_line()
+    if options.dump:
+        print(tmpl.code)
+        sys.exit(0)
+    t = Timer(render)
+    results = t.timeit(options.num) / options.num
+    print('%0.3f ms per iteration' % (results * 1000))
+    
+    
+if __name__ == '__main__':
+    options.parse_command_line()
+    start(options.port, options.root_directory, options.bucket_depth)
 
     
-        try:
-        s1 = os.lstat(path)
-    except OSError:
-        # the OSError should be handled with more care
-        # it could be a 'permission denied' but path is still a mount
-        return False
-    else:
-        # A symlink can never be a mount point
-        if os.path.stat.S_ISLNK(s1.st_mode):
-            return False
-    
-    
-class GCPUtilsTestCase(unittest.TestCase):
-    params_dict = {
-        'url_map_name': 'foo_url_map_name',
-        'description': 'foo_url_map description',
-        'host_rules': [
-            {
-                'description': 'host rules description',
-                'hosts': [
-                        'www.example.com',
-                        'www2.example.com'
-                ],
-                'path_matcher': 'host_rules_path_matcher'
-            }
-        ],
-        'path_matchers': [
-            {
-                'name': 'path_matcher_one',
-                'description': 'path matcher one',
-                'defaultService': 'bes-pathmatcher-one-default',
-                'pathRules': [
-                        {
-                            'service': 'my-one-bes',
-                            'paths': [
-                                '/',
-                                '/aboutus'
-                            ]
-                        }
-                ]
-            },
-            {
-                'name': 'path_matcher_two',
-                'description': 'path matcher two',
-                'defaultService': 'bes-pathmatcher-two-default',
-                'pathRules': [
-                        {
-                            'service': 'my-two-bes',
-                            'paths': [
-                                '/webapp',
-                                '/graphs'
-                            ]
-                        }
-                ]
-            }
-        ]
-    }
-    
-        terminal_stdout_re = [
-        re.compile(br'[\r\n]?[\w+\-\.:\/\[\]]+(?:\([^\)]+\)){,3}(?:>|#) ?$'),
-        re.compile(br'\@[\w\-\.]+:\S+?[>#\$] ?$')
-    ]
-    
-    #############################################
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
-    
-        description = proj_info['description'],
-    keywords = proj_info['keywords'],
-    
-        html = get_content(url)
-    pid = match1(html, r'video\.settings\.pid\s*=\s*\'([^\']+)\'')
-    title = match1(html, r'video\.settings\.title\s*=\s*\'([^\']+)\'')
-    
-            mp3 = match1(content, r'name='filename'\s*value='([^']+\.mp3)'')
-        if mp3: mp3 = 'http://res.infoq.com/downloads/mp3downloads/%s' % mp3
-    
-    from ..common import *
-    
-    
-#----------------------------------------------------------------------
-def showroom_download(url, output_dir = '.', merge = False, info_only = False, **kwargs):
-    ''''''
-    if re.match( r'(\w+)://www.showroom-live.com/([-\w]+)', url):
-        room_url_key = match1(url, r'\w+://www.showroom-live.com/([-\w]+)')
-        room_id = showroom_get_roomid_by_room_url_key(room_url_key)
-        showroom_download_by_room_id(room_id, output_dir, merge,
-                                    info_only)
+    If you are seeing this message on Linux you probably need to
+install GCC and/or the Python development package for your
+version of Python.
