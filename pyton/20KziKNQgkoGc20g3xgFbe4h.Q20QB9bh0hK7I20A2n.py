7a7499ee2914f1e2865f8038433aa85c943c8f9b
SUNsung <weatherius@gmail.com>
2018-08-09 14:59:01 +0300

    Rewrite [20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py] File in: 2018-08-09 14:59:00

--- pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
+++ pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
@@ -1,99 +1,81 @@
 
         
-        '''
-    The approach taken is explained below. I decided to do it simply.
-    Initially I was considering parsing the data into some sort of
-    structure and then generating an appropriate README. I am still
-    considering doing it - but for now this should work. The only issue
-    I see is that it only sorts the entries at the lowest level, and that
-    the order of the top-level contents do not match the order of the actual
-    entries.
+        print('Enter the PKCS1 private key, followed by a blank line:')
+privkey = b''
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    if line == '':
+        break
+    privkey += line.encode('ascii') + b'\n'
+privkey = rsa.PrivateKey.load_pkcs1(privkey)
     
-    from __future__ import division
-    
-    This example, inspired from Chen's publication [1], shows a comparison
-of the estimated MSE of the LW and OAS methods, using Gaussian
-distributed data.
-    
-    This dataset is made up of 1797 8x8 images. Each image,
-like the one shown below, is of a hand-written digit.
-In order to utilize an 8x8 figure like this, we'd have to
-first transform it into a feature vector with length 64.
-    
-    from six.moves.urllib.parse import urlencode
-    
-        def syntax(self):
-        return '[options] <spider>'
-    
-        def adjust_request_args(self, args):
-        args['url'] = self.args[0]
-        return args
-    
-        def _slot_gc(self, age=60):
-        mintime = time() - age
-        for key, slot in list(self.slots.items()):
-            if not slot.active and slot.lastseen + slot.delay < mintime:
-                self.slots.pop(key).close()
-
-    
-        def download_request(self, request, spider):
-        scheme = urlparse_cached(request).scheme
-        handler = self._get_handler(scheme)
-        if not handler:
-            raise NotSupported('Unsupported URL scheme '%s': %s' %
-                               (scheme, self._notconfigured[scheme]))
-        return handler.download_request(request, spider)
-    
-    See documentation in docs/topics/downloader-middleware.rst
-'''
-    
-        @classmethod
-    def from_crawler(cls, crawler):
-        o = cls(crawler.settings.getfloat('DOWNLOAD_TIMEOUT'))
-        crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
-        return o
-    
-    from scrapy import signals
-    
-    class DontCloseSpider(Exception):
-    '''Request the spider not to be closed yet'''
-    pass
-    
-    from tornado.options import define, options
-    
-        def put(self, filename):
-        filename = unquote(filename)
-        mtype = self.request.headers.get('Content-Type')
-        logging.info('PUT '%s' '%s' %d bytes', filename, mtype, self.bytes_read)
-        self.write('OK')
+    from youtube_dl.extractor import (
+    FacebookIE,
+    gen_extractors,
+    YoutubeIE,
+)
     
     
-if __name__ == '__main__':
-    main()
-
+class TestMultipleSocks(unittest.TestCase):
+    @staticmethod
+    def _check_params(attrs):
+        params = get_params()
+        for attr in attrs:
+            if attr not in params:
+                print('Missing %s. Skipping.' % attr)
+                return
+        return params
     
-        def _log(self):
-        pass
+    rootDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     
-    import logging
-from tornado.curl_httpclient import CurlAsyncHTTPClient
-from tornado.simple_httpclient import SimpleAsyncHTTPClient
-from tornado.ioloop import IOLoop
-from tornado.options import define, options, parse_command_line
-from tornado.web import RequestHandler, Application
+            title = self._html_search_regex(r'<title>(.*?)</title>', webpage, 'title')
+        description = self._html_search_regex(
+            r'<div class='description'[^>]*>([^<]+)</div>', webpage, 'description', fatal=False)
+        thumbnail = self._html_search_regex(
+            r'preview_url\s*:\s*\'(.*?)\'', webpage, 'thumbnail', fatal=False)
     
-    from timeit import Timer
+        _CONFIG = {
+        # http://edition.cnn.com/.element/apps/cvp/3.0/cfg/spider/cnn/expansion/config.xml
+        'edition': {
+            'data_src': 'http://edition.cnn.com/video/data/3.0/video/%s/index.xml',
+            'media_src': 'http://pmd.cdn.turner.com/cnn/big',
+        },
+        # http://money.cnn.com/.element/apps/cvp2/cfg/config.xml
+        'money': {
+            'data_src': 'http://money.cnn.com/video/data/4.0/video/%s.xml',
+            'media_src': 'http://ht3.cdn.turner.com/money/big',
+        },
+    }
     
+            json_url = (
+            'http://static.videos.gouv.fr/brightcovehub/export/json/%s' %
+            video_id)
+        info = self._download_json(json_url, title, 'Downloading JSON config')
+        video_url = info['renditions'][0]['url']
     
-# Using HTTP PUT, upload one raw file. This is preferred for large files since
-# the server can stream the data instead of buffering it entirely in memory.
-@gen.coroutine
-def post(filenames):
-    client = httpclient.AsyncHTTPClient()
-    boundary = uuid4().hex
-    headers = {'Content-Type': 'multipart/form-data; boundary=%s' % boundary}
-    producer = partial(multipart_producer, boundary, filenames)
-    response = yield client.fetch('http://localhost:8888/post',
-                                  method='POST',
-                                  headers=headers,
-                                  body_producer=producer)
+            from certbot_dns_dnsimple.dns_dnsimple import Authenticator
+    
+            raise errors.PluginError('Unable to determine base domain for {0} using names: {1}.'
+                                 .format(domain_name, domain_name_guesses))
+    
+    # If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = True
+    
+        def get_all_names_answer(self):
+        '''Returns the set of domain names that the plugin should find'''
+        if self._all_names:
+            return self._all_names
+        else:
+            raise errors.Error('No configuration file loaded')
+    
+        @mock.patch('certbot_compatibility_test.validator.requests.get')
+    def test_hsts_bad_max_age(self, mock_get_request):
+        mock_get_request.return_value = create_response(
+            headers={'strict-transport-security': 'max-age=not-an-int'})
+        self.assertFalse(self.validator.hsts('test.com'))
+    
+    # If true, 'Created using Sphinx' is shown in the HTML footer. Default is True.
+#html_show_sphinx = True
