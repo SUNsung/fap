e49abbe1c9849e97b71cb391f44e002a3a682412
SUNsung <weatherius@gmail.com>
2018-08-10 00:49:55 +0300

    Rewrite [55wnYL52fYFgg559oSaB7pEn0o55skpMUiWt4Yg55L4X.py] File in: 2018-08-10 00:49:55

--- pyton/55wnYL52fYFgg559oSaB7pEn0o55skpMUiWt4Yg55L4X.py
+++ pyton/55wnYL52fYFgg559oSaB7pEn0o55skpMUiWt4Yg55L4X.py
@@ -1,152 +1,109 @@
 
         
-        import io
-import optparse
+        
+@pytest.fixture(params=containers)
+def proc(request, spawnu, TIMEOUT):
+    proc = spawnu(*request.param)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(u'tcsh')
+    proc.sendline(u'setenv PYTHONIOENCODING utf8')
+    proc.sendline(u'eval `thefuck --alias`')
+    return proc
     
-    if isinstance(helptext, bytes):
-    helptext = helptext.decode('utf-8')
+        def get_all_names_answer(self):
+        '''Returns the set of domain names that the plugin should find'''
+        if self._all_names:
+            return self._all_names
+        else:
+            raise errors.Error('No configuration file loaded')
     
-        params = {
-        'age_limit': age,
-        'skip_download': True,
-        'writeinfojson': True,
-        'outtmpl': '%(id)s.%(ext)s',
-    }
-    ydl = YoutubeDL(params)
-    ydl.add_default_info_extractors()
-    json_filename = os.path.splitext(filename)[0] + '.info.json'
-    try_rm(json_filename)
-    ydl.download([url])
-    res = os.path.exists(json_filename)
-    try_rm(json_filename)
-    return res
+        @mock.patch(
+        'certbot_compatibility_test.validator.crypto_util.probe_sni')
+    def test_certificate_success(self, mock_probe_sni):
+        cert = OpenSSL.crypto.X509()
+        mock_probe_sni.return_value = cert
+        self.assertTrue(self.validator.certificate(
+            cert, 'test.com', '127.0.0.1'))
     
-    from __future__ import unicode_literals
+        def __init__(self):
+        self.experts = []
+        self.common_state = {
+            'problems': 0,
+            'suggestions': 0,
+            'contributions': [],
+            'progress': 0   # percentage, if 100 -> task is finished
+        }
     
     
-def gen_extractors():
-    ''' Return a list of an instance of every supported extractor.
-    The order does matter; the first extractor matched is the one handling the URL.
-    '''
-    return [klass() for klass in gen_extractor_classes()]
+class TestSubject(unittest.TestCase):
     
-            duration = parse_duration(self._search_regex(
-            r'<b>Duration:</b> (?:<q itemprop='duration'>)?(\d+:\d+)', webpage, 'duration', fatal=False))
-        view_count = int_or_none(self._html_search_regex(
-            r'<b>Views:</b> (\d+)', webpage, 'view count', fatal=False))
+        # sample_queue.put('sam')
+    # test_object(sample_queue)
+    # print('Outside func: {}'.format(sample_queue.get()))
     
-    from .common import InfoExtractor
-from ..utils import js_to_json
+        def test_bear_greek_localization(self):
+        self.assertEqual(self.g.get('bear'), 'bear')
+
+    
+        def test_am_station_overflow_after_scan(self):
+        self.radio.scan()
+        station = self.radio.state.stations[self.radio.state.pos]
+        expected_station = '1250'
+        self.assertEqual(station, expected_station)
+    
+        def test_display_current_time_at_midnight(self):
+        '''
+        Will almost always fail (despite of right at/after midnight).
+        '''
+        time_provider_stub = MidnightTimeProvider()
+        class_under_test = TimeDisplay(time_provider_stub)
+        expected_time = '<span class=\'tinyBoldText\'>24:01</span>'
+        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
+    
+    print('Counting to two...')
+for number in count_to_two():
+    print(number, end=' ')
+    
+        def setup(self):
+        print('Setting up the Test')
+        time.sleep(0.1)
+        self._tm.prepareReporting()
+    
+        def __init__(self, super_user=False):
+        self.super_user = super_user
+    
+    from tornado.options import define, options
+    
+    define('port', default=8888, help='run on the given port', type=int)
     
     
 if __name__ == '__main__':
-    SalesRanker.run()
+    # Tornado configures logging.
+    options.parse_command_line()
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
 
     
-        def append_to_front(self, node):
-        pass
     
-        def __init__(self, level, row, spot_number, spot_size, vehicle_size):
-        self.level = level
-        self.row = row
-        self.spot_number = spot_number
-        self.spot_size = spot_size
-        self.vehicle_size = vehicle_size
-        self.vehicle = None
+def main():
+    parse_command_line()
+    t = Timer(e1)
+    results = t.timeit(options.num) / options.num
+    print('engine: %0.3f ms per iteration' % (results * 1000))
+    t = Timer(c1)
+    results = t.timeit(options.num) / options.num
+    print('coroutine: %0.3f ms per iteration' % (results * 1000))
     
-        def get(self, key):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                return item.value
-        raise KeyError('Key not found')
+        garbage = garbage or gc.garbage
+    found = []
+    stack = []
+    stack_ids = set()
+    garbage_ids = set(map(id, garbage))
+    visited_ids = set()
     
-    
-class Node(object):
-    
-        def extract_max_priority_page(self):
-        '''Return the highest priority link in `links_to_crawl`.'''
-        pass
-    
-    
-class ConnectionError(RequestException):
-    '''A Connection error occurred.'''
-    
-        pyopenssl_info = {
-        'version': None,
-        'openssl_version': '',
-    }
-    if OpenSSL:
-        pyopenssl_info = {
-            'version': OpenSSL.__version__,
-            'openssl_version': '%x' % OpenSSL.SSL.OPENSSL_VERSION_NUMBER,
-        }
-    cryptography_info = {
-        'version': getattr(cryptography, '__version__', ''),
-    }
-    idna_info = {
-        'version': getattr(idna, '__version__', ''),
-    }
-    
-    This module implements the Requests API.
-    
-    
-class EnvironmentConfig(object):
-    uri = os.getenv('LANDSCAPE_API_URI')
-    access_key = os.getenv('LANDSCAPE_API_KEY')
-    secret_key = os.getenv('LANDSCAPE_API_SECRET')
-    ssl_ca_file = os.getenv('LANDSCAPE_API_SSL_CA_FILE')
-    
-            expected = {
-            'name': 'myhealthcheck',
-            'checkIntervalSec': 5,
-            'port': 443,
-            'unhealthyThreshold': 2,
-            'healthyThreshold': 2,
-            'host': '',
-            'timeoutSec': 5,
-            'requestPath': '/'}
-    
-        print()
-    print('Classification performance:')
-    print('===========================')
-    print()
-    print('%s %s %s %s' % ('Classifier  ', 'train-time', 'test-time',
-                           'Accuracy'))
-    print('-' * 44)
-    for name in sorted(accuracy, key=accuracy.get):
-        print('%s %s %s %s' % (name.ljust(16),
-                               ('%.4fs' % train_time[name]).center(10),
-                               ('%.4fs' % test_time[name]).center(10),
-                               ('%.4f' % accuracy[name]).center(10)))
-    
-        return lasso_results, lars_lasso_results
-    
-        print('Transformer performance:')
-    print('===========================')
-    print('Results are averaged over %s repetition(s).' % opts.n_times)
-    print('')
-    print('%s | %s | %s' % ('Transformer'.ljust(30),
-                            'fit'.center(12),
-                            'transform'.center(12)))
-    print(31 * '-' + ('|' + '-' * 14) * 2)
-    
-        print('Averaging results...', end='')
-    for name in sampling_algorithm:
-        time[name] = np.mean(time[name], axis=1)
-    print('done\n')
-    
-        if not os.path.exists(ARCHIVE_NAME):
-        print('Downloading dataset from %s (14 MB)' % URL)
-        opener = urlopen(URL)
-        with open(ARCHIVE_NAME, 'wb') as archive:
-            archive.write(opener.read())
-    
-        input_file = open(os.path.join(exercise_dir, f))
-    output_file = open(os.path.join(skeleton_dir, f), 'w')
-    
-    
-def generate_data(n_samples, n_features):
-    '''Generate random blob-ish data with noisy features.
-    
-    from sklearn import datasets
+    try:
+    import twisted
+except ImportError:
+    twisted = None
