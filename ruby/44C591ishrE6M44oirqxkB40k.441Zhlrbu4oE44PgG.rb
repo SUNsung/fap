60f84e27245ec1da255b16ed0d0c80318ba4f0b5
SUNsung <weatherius@gmail.com>
2018-08-09 20:13:55 +0300

    Rewrite [44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb] File in: 2018-08-09 20:13:55

--- ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
+++ ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
@@ -1,39 +1,55 @@
 
         
-            def register_compass_extension
-      ::Compass::Frameworks.register(
-          'bootstrap',
-          :version               => Bootstrap::VERSION,
-          :path                  => gem_path,
-          :stylesheets_directory => stylesheets_path,
-          :templates_directory   => File.join(gem_path, 'templates')
-      )
+          def post_superenv_hacks
+    # Only allow Homebrew-approved directories into the PATH, unless
+    # a formula opts-in to allowing the user's path.
+    if formula.env.userpaths? || reqs.any? { |rq| rq.env.userpaths? }
+      ENV.userpaths!
+    end
+  end
+    
+      def elisp_caveats
+    return if f.keg_only?
+    if keg && keg.elisp_installed?
+      <<-EOS.undent
+        Emacs Lisp files have been installed to:
+          #{HOMEBREW_PREFIX}/share/emacs/site-lisp/#{f.name}
+      EOS
+    end
+  end
+    
+        # Get rid of any info 'dir' files, so they don't conflict at the link stage
+    info_dir_file = @f.info + 'dir'
+    if info_dir_file.file? && !@f.skip_clean?(info_dir_file)
+      observe_file_removal info_dir_file
     end
     
-    module RuboCop
-  module Cop
-    module Performance
-      # This cop identifies the use of `Regexp#match` or `String#match`, which
-      # returns `#<MatchData>`/`nil`. The return value of `=~` is an integral
-      # index/`nil` and is more performant.
-      #
-      # @example
-      #   # bad
-      #   do_something if str.match(/regex/)
-      #   while regex.match('str')
-      #     do_something
-      #   end
-      #
-      #   # good
-      #   method(str =~ /regex/)
-      #   return value unless regex =~ 'str'
-      class RedundantMatch < Cop
-        MSG = 'Use `=~` in places where the `MatchData` returned by ' \
-              '`#match` will not be used.'.freeze
+      def describe_path(path)
+    return 'N/A' if path.nil?
+    realpath = path.realpath
+    if realpath == path
+      path
+    else
+      '#{path} => #{realpath}'
+    end
+  end
     
-          # The name of the defined method as a symbol.
-      #
-      # @return [Symbol] the name of the defined method
-      def method_name
-        node_parts[2]
-      end
+      def updated
+    object.updated_at.iso8601
+  end
+    
+      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/reblog
+  def reblog
+    r = Status.where.not(reblog_of_id: nil).first
+    NotificationMailer.reblog(r.reblog.account, Notification.find_by(activity: r))
+  end
+    
+    Print a list of currently running Applications and associated
+Bundle IDs, which may be useful in a Cask uninstall stanza, eg
+    
+    input = ARGV.shift() || usage()
+    
+    meterp.core.use('Stdapi')
+    
+    require 'rubygems'
+require 'rjb'
