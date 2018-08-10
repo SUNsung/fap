4b1b6bcd8e15572d839a5047d9604e09450b195a
SUNsung <weatherius@gmail.com>
2018-08-10 21:25:33 +0300

    Rewrite [36E7R9v1X2FA36BlRkmecpPg36kRThnN2EjkQ36v1S.cpp] File in: 2018-08-10 21:25:32

--- cccccc/36E7R9v1X2FA36BlRkmecpPg36kRThnN2EjkQ36v1S.cpp
+++ cccccc/36E7R9v1X2FA36BlRkmecpPg36kRThnN2EjkQ36v1S.cpp
@@ -1,87 +1,149 @@
 
         
         
-    {
-    {}  // namespace testing
-}  // namespace grpc
+    { private:
+  DISALLOW_COPY_AND_ASSIGN(URLRequestAsyncAsarJob);
+};
     
-    #include 'test/cpp/qps/benchmark_config.h'
-#include 'test/cpp/qps/driver.h'
-#include 'test/cpp/qps/report.h'
-#include 'test/cpp/qps/server.h'
-#include 'test/cpp/util/test_config.h'
-#include 'test/cpp/util/test_credentials_provider.h'
+    #ifndef ATOM_BROWSER_UNRESPONSIVE_SUPPRESSOR_H_
+#define ATOM_BROWSER_UNRESPONSIVE_SUPPRESSOR_H_
     
-    namespace {
-    }
-    
-    namespace grpc {
-namespace testing {
-    }
-    }
-    
-    #endif  // TEST_QPS_TIMER_H
+    #endif  // ATOM_COMMON_DRAGGABLE_REGION_H_
 
     
-    struct gpr_subprocess;
+    #ifndef ATOM_RENDERER_PREFERENCES_MANAGER_H_
+#define ATOM_RENDERER_PREFERENCES_MANAGER_H_
     
+    // filenames
+const base::FilePath::CharType kCacheDirname[] = FPL('Cache');
+const base::FilePath::CharType kChannelIDFilename[] = FPL('Origin Bound Certs');
+const base::FilePath::CharType kCookieFilename[] = FPL('Cookies');
+const base::FilePath::CharType kCRLSetFilename[] =
+    FPL('Certificate Revocation Lists');
+const base::FilePath::CharType kCustomDictionaryFileName[] =
+    FPL('Custom Dictionary.txt');
+const base::FilePath::CharType kExtensionActivityLogFilename[] =
+    FPL('Extension Activity');
+const base::FilePath::CharType kExtensionsCookieFilename[] =
+    FPL('Extension Cookies');
+const base::FilePath::CharType kFirstRunSentinel[] = FPL('First Run');
+const base::FilePath::CharType kGCMStoreDirname[] = FPL('GCM Store');
+const base::FilePath::CharType kLocalStateFilename[] = FPL('Local State');
+const base::FilePath::CharType kLocalStorePoolName[] = FPL('LocalStorePool');
+const base::FilePath::CharType kMediaCacheDirname[] = FPL('Media Cache');
+const base::FilePath::CharType kNetworkPersistentStateFilename[] =
+    FPL('Network Persistent State');
+const base::FilePath::CharType kOfflinePageArchviesDirname[] =
+    FPL('Offline Pages/archives');
+const base::FilePath::CharType kOfflinePageMetadataDirname[] =
+    FPL('Offline Pages/metadata');
+const base::FilePath::CharType kPreferencesFilename[] = FPL('Preferences');
+const base::FilePath::CharType kProtectedPreferencesFilenameDeprecated[] =
+    FPL('Protected Preferences');
+const base::FilePath::CharType kReadmeFilename[] = FPL('README');
+const base::FilePath::CharType kResetPromptMementoFilename[] =
+    FPL('Reset Prompt Memento');
+const base::FilePath::CharType kSafeBrowsingBaseFilename[] =
+    FPL('Safe Browsing');
+const base::FilePath::CharType kSecurePreferencesFilename[] =
+    FPL('Secure Preferences');
+const base::FilePath::CharType kServiceStateFileName[] = FPL('Service State');
+const base::FilePath::CharType kSingletonCookieFilename[] =
+    FPL('SingletonCookie');
+const base::FilePath::CharType kSingletonLockFilename[] = FPL('SingletonLock');
+const base::FilePath::CharType kSingletonSocketFilename[] =
+    FPL('SingletonSocket');
+const base::FilePath::CharType kSupervisedUserSettingsFilename[] =
+    FPL('Managed Mode Settings');
+const base::FilePath::CharType kThemePackFilename[] = FPL('Cached Theme.pak');
+const base::FilePath::CharType kThemePackMaterialDesignFilename[] =
+    FPL('Cached Theme Material Design.pak');
+const base::FilePath::CharType kWebAppDirname[] = FPL('Web Applications');
     
-    {
-    {// use R's PRNG to replacd
-CustomGlobalRandomEngine::result_type
-CustomGlobalRandomEngine::operator()() {
-  return static_cast<result_type>(
-      std::floor(unif_rand() * CustomGlobalRandomEngine::max()));
+    // This flag controls the style of death tests.  Valid values are 'threadsafe',
+// meaning that the death test child process will re-execute the test binary
+// from the start, running only a single death test, or 'fast',
+// meaning that the child process will execute the test logic immediately
+// after forking.
+GTEST_DECLARE_string_(death_test_style);
+    
+    // Prints the given number of elements in an array, without printing
+// the curly braces.
+template <typename T>
+void PrintRawArrayTo(const T a[], size_t count, ::std::ostream* os) {
+  UniversalPrint(a[0], os);
+  for (size_t i = 1; i != count; i++) {
+    *os << ', ';
+    UniversalPrint(a[i], os);
+  }
 }
-}  // namespace common
-}  // namespace xgboost
-
     
-    #include <cstdint>
-#include <xgboost/c_api.h>
-#include <xgboost/base.h>
-#include <xgboost/logging.h>
-#include './xgboost4j.h'
-#include <cstring>
-#include <vector>
-#include <string>
+    // An Environment object is capable of setting up and tearing down an
+// environment.  The user should subclass this to define his own
+// environment(s).
+//
+// An Environment object does the set-up and tear-down in virtual
+// methods SetUp() and TearDown() instead of the constructor and the
+// destructor, as:
+//
+//   1. You cannot safely throw from a destructor.  This is a problem
+//      as in some cases Google Test is used where exceptions are enabled, and
+//      we may want to implement ASSERT_* using exceptions where they are
+//      available.
+//   2. You cannot use ASSERT_* directly in a constructor or
+//      destructor.
+class Environment {
+ public:
+  // The d'tor is virtual as we need to subclass Environment.
+  virtual ~Environment() {}
+    }
     
+    #endif  // GTEST_HAS_DEATH_TEST
     
-    {  delete metric;
-  metric = xgboost::Metric::Create('ndcg@2-');
-  ASSERT_STREQ(metric->Name(), 'ndcg@2-');
-  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 1, 1e-10);
+    /*
+ * Class:     ml_dmlc_xgboost4j_java_XGBoostJNI
+ * Method:    XGBoosterFree
+ * Signature: (J)V
+ */
+JNIEXPORT jint JNICALL Java_ml_dmlc_xgboost4j_java_XGBoostJNI_XGBoosterFree
+  (JNIEnv *jenv, jclass jcls, jlong jhandle) {
+    BoosterHandle handle = (BoosterHandle) jhandle;
+    return XGBoosterFree(handle);
+}
+    
+    // macro to dispatch according to specified pointer types
+#define DISPATCH_CONST_PTR(dtype, old_ptr, cast_ptr, proc)              \
+  switch (dtype) {                                                      \
+    case kFloat32: {                                                    \
+      auto cast_ptr = reinterpret_cast<const float*>(old_ptr); proc; break; \
+    }                                                                   \
+    case kDouble: {                                                     \
+      auto cast_ptr = reinterpret_cast<const double*>(old_ptr); proc; break; \
+    }                                                                   \
+    case kUInt32: {                                                     \
+      auto cast_ptr = reinterpret_cast<const uint32_t*>(old_ptr); proc; break; \
+    }                                                                   \
+    case kUInt64: {                                                     \
+      auto cast_ptr = reinterpret_cast<const uint64_t*>(old_ptr); proc; break; \
+    }                                                                   \
+    default: LOG(FATAL) << 'Unknown data type' << dtype;                \
+  }                                                                     \
+    
+    // prediction
+#include '../src/predictor/predictor.cc'
+#include '../src/predictor/cpu_predictor.cc'
+    
+    #include <dmlc/base.h>
+#include <dmlc/omp.h>
+#include <cmath>
+#include <iostream>
+    
+    TEST(Metric, MAE) {
+  xgboost::Metric * metric = xgboost::Metric::Create('mae');
+  ASSERT_STREQ(metric->Name(), 'mae');
+  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 0, 1e-10);
   EXPECT_NEAR(GetMetricEval(metric,
                             {0.1f, 0.9f, 0.1f, 0.9f},
                             {  0,   0,   1,   1}),
-              0.3868f, 0.001f);
+              0.5f, 0.001f);
 }
-    
-    DEFINE_FIND_STATIC_METHOD(KXlog_setConsoleLogOpen, KXlog, 'setConsoleLogOpen', '(Z)V')
-JNIEXPORT void JNICALL Java_com_tencent_mars_xlog_Xlog_setConsoleLogOpen
-  (JNIEnv *env, jclass, jboolean _is_open) {
-	appender_set_console_log((bool)_is_open);
-}
-    
-    namespace mars {
-namespace baseevent {
-    }
-    }
-    
-    #include <vector>
-    
-    #ifndef SERVICEBASE_H_
-#define SERVICEBASE_H_
-    
-    // Licensed under the MIT License (the 'License'); you may not use this file except in 
-// compliance with the License. You may obtain a copy of the License at
-// http://opensource.org/licenses/MIT
-    
-      const std::shared_ptr<Request>& getProxyRequest() const
-  {
-    return proxyRequest_;
-  }
-    
-      virtual bool good() const CXX11_OVERRIDE { return true; }
-    
-    #include 'common.h'
