2520c2196d81bb1853aaca8f8ef9588be9b756a3
SUNsung <weatherius@gmail.com>
2018-08-10 22:03:53 +0300

    Rewrite [02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp] File in: 2018-08-10 22:03:52

--- cccccc/02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp
+++ cccccc/02ikPEDvyIve202ol6Oz0vcFC602BM7KU63Ej5.02KhJ.cpp
@@ -1,19 +1,371 @@
 
         
-        class RocksDBDatabasePluginTests : public DatabasePluginTests {
+        using base::MessageLoop;
+using base::CommandLine;
+using content::BrowserThread;
+using content::Shell;
+using content::ShellBrowserContext;
+using content::RenderProcessHost;
+    
+      bool delay_destruction() { return delay_destruction_; }
+  void set_delay_destruction(bool val) { delay_destruction_ = val; }
+  bool pending_destruction() { return pending_destruction_; }
+  void set_pending_destruction (bool val) { pending_destruction_ = val; }
  protected:
-  std::string name() override {
-    return 'rocksdb';
+  int id_;
+  bool delay_destruction_;
+  bool pending_destruction_;
+  base::WeakPtr<ObjectManager> object_manager_;
+    
+    // Call method of an object in browser.
+// function CallObjectMethod(id, type, method, args);
+v8::Handle<v8::Value> CallObjectMethod(int routing_id,
+                                       int object_id,
+                                       const std::string& type,
+                                       const std::string& method,
+                                       v8::Handle<v8::Value> args);
+    
+    namespace nw {
+    }
+    
+    void Menu::Create(const base::DictionaryValue& option) {
+  gtk_accel_group = NULL;
+  std::string type;
+  if (option.GetString('type', &type) && type == 'menubar')
+    menu_ = gtk_menu_bar_new();
+  else
+    menu_ = gtk_menu_new();
+    }
+    
+        bool Write(ClipboardData& data) {
+      switch(data.type) {
+        case TYPE_TEXT:
+        return WriteText(data);
+        break;
+        case TYPE_HTML:
+        return WriteHTML(data);
+        break;
+        case TYPE_RTF:
+        return WriteRTF(data);
+        break;
+        case TYPE_PNG:
+        case TYPE_JPEG:
+        return WriteImage(data);
+        break;
+        case TYPE_NONE:
+        NOTREACHED();
+        return false;
+      }
+      NOTREACHED();
+      return false;
+    }
+    
+        TableBuilder* builder = new TableBuilder(options, file);
+    meta->smallest.DecodeFrom(iter->key());
+    for (; iter->Valid(); iter->Next()) {
+      Slice key = iter->key();
+      meta->largest.DecodeFrom(key);
+      builder->Add(key, iter->value());
+    }
+    
+    Status DumpDescriptor(Env* env, const std::string& fname, WritableFile* dst) {
+  return PrintLogContents(env, fname, VersionEditPrinter, dst);
+}
+    
+          case kPrevLogNumber:
+        if (GetVarint64(&input, &prev_log_number_)) {
+          has_prev_log_number_ = true;
+        } else {
+          msg = 'previous log number';
+        }
+        break;
+    
+        // Initialize db_
+    db_ = new kyotocabinet::TreeDB();
+    char file_name[100];
+    db_num_++;
+    std::string test_dir;
+    Env::Default()->GetTestDirectory(&test_dir);
+    snprintf(file_name, sizeof(file_name),
+             '%s/dbbench_polyDB-%d.kct',
+             test_dir.c_str(),
+             db_num_);
+    
+    
+    {}  // namespace leveldb
+    
+    struct TraceInfo;                      // Information about a trace point.
+class ScopedTrace;                     // Implements scoped trace.
+class TestInfoImpl;                    // Opaque implementation of TestInfo
+class UnitTestImpl;                    // Opaque implementation of UnitTest
+    
+        virtual const ParamGeneratorInterface<ParamType>* BaseGenerator() const {
+      return base_;
+    }
+    // Advance should not be called on beyond-of-range iterators
+    // so no component iterators must be beyond end of range, either.
+    virtual void Advance() {
+      assert(!AtEnd());
+      ++current$(i)_;
+    }
+    
+    // The user can define the following macros in the build script to
+// control Google Test's behavior.  If the user doesn't define a macro
+// in this list, Google Test will define it.
+//
+//   GTEST_HAS_CLONE          - Define it to 1/0 to indicate that clone(2)
+//                              is/isn't available.
+//   GTEST_HAS_EXCEPTIONS     - Define it to 1/0 to indicate that exceptions
+//                              are enabled.
+//   GTEST_HAS_GLOBAL_STRING  - Define it to 1/0 to indicate that ::string
+//                              is/isn't available (some systems define
+//                              ::string, which is different to std::string).
+//   GTEST_HAS_GLOBAL_WSTRING - Define it to 1/0 to indicate that ::string
+//                              is/isn't available (some systems define
+//                              ::wstring, which is different to std::wstring).
+//   GTEST_HAS_POSIX_RE       - Define it to 1/0 to indicate that POSIX regular
+//                              expressions are/aren't available.
+//   GTEST_HAS_PTHREAD        - Define it to 1/0 to indicate that <pthread.h>
+//                              is/isn't available.
+//   GTEST_HAS_RTTI           - Define it to 1/0 to indicate that RTTI is/isn't
+//                              enabled.
+//   GTEST_HAS_STD_WSTRING    - Define it to 1/0 to indicate that
+//                              std::wstring does/doesn't work (Google Test can
+//                              be used where std::wstring is unavailable).
+//   GTEST_HAS_TR1_TUPLE      - Define it to 1/0 to indicate tr1::tuple
+//                              is/isn't available.
+//   GTEST_HAS_SEH            - Define it to 1/0 to indicate whether the
+//                              compiler supports Microsoft's 'Structured
+//                              Exception Handling'.
+//   GTEST_HAS_STREAM_REDIRECTION
+//                            - Define it to 1/0 to indicate whether the
+//                              platform supports I/O stream redirection using
+//                              dup() and dup2().
+//   GTEST_USE_OWN_TR1_TUPLE  - Define it to 1/0 to indicate whether Google
+//                              Test's own tr1 tuple implementation should be
+//                              used.  Unused when the user sets
+//                              GTEST_HAS_TR1_TUPLE to 0.
+//   GTEST_LANG_CXX11         - Define it to 1/0 to indicate that Google Test
+//                              is building in C++11/C++98 mode.
+//   GTEST_LINKED_AS_SHARED_LIBRARY
+//                            - Define to 1 when compiling tests that use
+//                              Google Test as a shared library (known as
+//                              DLL on Windows).
+//   GTEST_CREATE_SHARED_LIBRARY
+//                            - Define to 1 when compiling Google Test itself
+//                              as a shared library.
+    
+     private:
+  void CalculatePrimesUpTo(int max) {
+    ::std::fill(is_prime_, is_prime_ + is_prime_size_, true);
+    is_prime_[0] = is_prime_[1] = false;
+    }
+    
+    #include 'sample1.h'
+    
+    
+// Step 2. Use the TEST macro to define your tests.
+//
+// TEST has two parameters: the test case name and the test name.
+// After using the macro, you should define your test logic between a
+// pair of braces.  You can use a bunch of macros to indicate the
+// success or failure of a test.  EXPECT_TRUE and EXPECT_EQ are
+// examples of such macros.  For a complete list, see gtest.h.
+//
+// <TechnicalDetails>
+//
+// In Google Test, tests are grouped into test cases.  This is how we
+// keep test code organized.  You should put logically related tests
+// into the same test case.
+//
+// The test case name and the test name should both be valid C++
+// identifiers.  And you should not use underscore (_) in the names.
+//
+// Google Test guarantees that each test you define is run exactly
+// once, but it makes no guarantee on the order the tests are
+// executed.  Therefore, you should write your tests in such a way
+// that their results don't depend on their order.
+//
+// </TechnicalDetails>
+    
+      /**
+   * Returns true if the iterator is at the start of an object at the given
+   * level.
+   *
+   * For instance, suppose an iterator it is pointed to the first symbol of the
+   * first word of the third line of the second paragraph of the first block in
+   * a page, then:
+   *   it.IsAtBeginningOf(RIL_BLOCK) = false
+   *   it.IsAtBeginningOf(RIL_PARA) = false
+   *   it.IsAtBeginningOf(RIL_TEXTLINE) = true
+   *   it.IsAtBeginningOf(RIL_WORD) = true
+   *   it.IsAtBeginningOf(RIL_SYMBOL) = true
+   */
+  virtual bool IsAtBeginningOf(PageIteratorLevel level) const;
+    
+    const ICOORD *dirtab = (ICOORD *) idirtab;
+    
+      const IndexMapBiDi& charset_map() const {
+    return *charset_map_;
   }
+  const ShapeTable* shape_table() const {
+    return shape_table_;
+  }
+  // Sample set operations.
+  const TrainingSampleSet* sample_set() const {
+    return sample_set_;
+  }
+    
+      // Computes and returns the dot product of the two n-vectors u and v.
+  static double DotProduct(const double* u, const double* v, int n);
+  // Utility function converts an array of float to the corresponding array
+  // of double.
+  static void FloatToDouble(const GENERIC_2D_ARRAY<float>& wf,
+                            GENERIC_2D_ARRAY<double>* wd);
+    
+    /** Returns the benchmark Reporter instance.
+ *
+ * The returned instance will take care of generating reports for all the actual
+ * reporters configured via the 'enable_*_reporter' command line flags (see
+ * benchmark_config.cc). */
+std::shared_ptr<Reporter> GetReporter();
+    
+    
+    {  GetReporter()->ReportQPS(*result);
+  GetReporter()->ReportLatency(*result);
+}
+    
+      // Set up security params
+  SecurityParams security;
+  security.set_use_test_ca(true);
+  security.set_server_host_override('foo.test.google.fr');
+  client_config.mutable_security_params()->CopyFrom(security);
+  server_config.mutable_security_params()->CopyFrom(security);
+    
+    // Data pertaining to configuration of the generator with respect to anything
+// that may be used internally at Google.
+struct GeneratorConfiguration {
+  GeneratorConfiguration();
+  grpc::string grpc_package_root;
+  // TODO(https://github.com/grpc/grpc/issues/8622): Drop this.
+  grpc::string beta_package_root;
+  // TODO(https://github.com/google/protobuf/issues/888): Drop this.
+  grpc::string import_prefix;
 };
     
-      /// Check whether the subscription matches the event.
-  bool shouldFire(const KernelSubscriptionContextRef &sc,
-                  const KernelEventContextRef &ec) const override;
+    SecureAuthContext::~SecureAuthContext() {
+  if (take_ownership_) grpc_auth_context_release(ctx_);
+}
     
-     public:
-  virtual ~UdevEventPublisher() {
-    tearDown();
+    /*
+ * Global litstr Id's are all above this mark.
+ */
+constexpr int kGlobalLitstrOffset = 0x40000000;
+    
+    extent_hooks_t BumpExtentAllocator::s_hooks {
+  BumpExtentAllocator::extent_alloc,
+  nullptr,                              // dalloc
+  nullptr,                              // destroy
+  extent_commit,
+  nullptr,                              // decommit
+  extent_purge,                         // purge_lazy
+  nullptr,                              // purge_forced
+  extent_split,
+  extent_merge
+};
+    
+    #include 'hphp/runtime/vm/jit/block.h'
+#include 'hphp/runtime/vm/jit/check.h'
+#include 'hphp/runtime/vm/jit/containers.h'
+#include 'hphp/runtime/vm/jit/ir-instr-table.h'
+#include 'hphp/runtime/vm/jit/ir-opcode.h'
+#include 'hphp/runtime/vm/jit/region-selection.h'
+#include 'hphp/runtime/vm/jit/translator.h'
+    
+      if (num_req == 1) {
+    // Iterating through the counter is correct but a bit inefficient
+    // compared to being able to access the right offset into array data,
+    // but necessary for this code to be agnostic to the array's internal
+    // representation.  Assuming uniform distribution, we'll expect to
+    // iterate through half of the array's data.
+    ssize_t index = HHVM_FN(rand)(0, count-1);
+    ssize_t pos = input->iter_begin();
+    while (index--) {
+      pos = input->iter_advance(pos);
+    }
+    return input->getKey(pos);
   }
     
-    #include <boost/filesystem.hpp>
+    	szArgList = CommandLineToArgvW(GetCommandLine(), &argCount);
+    
+    
+    {        vecdump_.push_back(strstack);
+        strstack.clear();
+    }
+    
+    class CommFrequencyLimit {
+  public:
+    CommFrequencyLimit(size_t _count, uint64_t _time_span);
+    ~CommFrequencyLimit();
+    }
+    
+    
+#define DEFINE_HAS_MEMBER_WITH_TYPE(member_name, member_type) \
+    template <typename T>\
+    class has_##member_name {\
+      private:\
+        struct yes_type { char x[1]; };\
+        struct no_type { char x[2]; };\
+        template <member_type (T::*)> struct tester;\
+        template <typename U> static yes_type test(tester<&U::member_name>*);\
+        template <typename U> static no_type test(...);\
+      public:\
+        static const bool value = (sizeof(test<T>(0)) == sizeof(yes_type));\
+    };
+    
+      private:
+    ScopedJstring();
+    ScopedJstring(const ScopedJstring&);
+    ScopedJstring& operator=(const ScopedJstring&);
+    
+    
+    { private:
+  DISALLOW_COPY_AND_ASSIGN(DummyCameraDetector);
+};
+    
+    /**
+ * @brief Computes the Moore-Penrose pseudo-inverse of a given square matrix,
+ * rounding all eigenvalues with absolute value bounded by epsilon to zero.
+ *
+ * @param m The square matrix to be pseudo-inverted
+ * @param epsilon A small positive real number (optional; default is 1.0e-6).
+ *
+ * @return Moore-Penrose pseudo-inverse of the given matrix.
+ */
+template <typename T, unsigned int N>
+Eigen::Matrix<T, N, N> PseudoInverse(const Eigen::Matrix<T, N, N> &m,
+                                     const double epsilon = 1.0e-6) {
+  Eigen::JacobiSVD<Eigen::Matrix<T, N, N>> svd(
+      m, Eigen::ComputeFullU | Eigen::ComputeFullV);
+  return svd.matrixV() *
+         (svd.singularValues().array().abs() > epsilon)
+             .select(svd.singularValues().array().inverse(), 0)
+             .matrix()
+             .asDiagonal() *
+         svd.matrixU().adjoint();
+}
+    
+      /**
+   * @brief Get all vertices of the polygon
+   * @param All vertices of the polygon
+   */
+  void GetAllVertices(std::vector<Vec2d> *const vertices) const;
+    
+    #include <memory>
+#include <string>
+    
+      std::vector<int> gt_list = {
+      3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
