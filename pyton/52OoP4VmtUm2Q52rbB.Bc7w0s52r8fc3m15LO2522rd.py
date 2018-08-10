d88bea56c21c345294fb043f2c2f0938f83be9a7
SUNsung <weatherius@gmail.com>
2018-08-10 16:25:05 +0300

    Rewrite [52OoP4VmtUm2Q52rbB.Bc7w0s52r8fc3m15LO2522rd.py] File in: 2018-08-10 16:25:04

--- pyton/52OoP4VmtUm2Q52rbB.Bc7w0s52r8fc3m15LO2522rd.py
+++ pyton/52OoP4VmtUm2Q52rbB.Bc7w0s52r8fc3m15LO2522rd.py
@@ -1,86 +1,50 @@
 
         
-        
-###############################################################################
-# Data
+                title_string = 'Varying %s' % quantity
     
+        if not os.path.exists(ARCHIVE_NAME):
+        print('Downloading dataset from %s (3 MB)' % URL)
+        opener = urlopen(URL)
+        with open(ARCHIVE_NAME, 'wb') as archive:
+            archive.write(opener.read())
     
-def bench_sample(sampling, n_population, n_samples):
-    gc.collect()
-    # start time
-    t_start = datetime.now()
-    sampling(n_population, n_samples)
-    delta = (datetime.now() - t_start)
-    # stop time
-    time = compute_time(t_start, delta)
-    return time
+    import os
+import tarfile
+from contextlib import closing
     
+        # the training data folder must be passed as first argument
+    movie_reviews_data_folder = sys.argv[1]
+    dataset = load_files(movie_reviews_data_folder, shuffle=False)
+    print('n_samples: %d' % len(dataset.data))
     
-URL = ('http://www.cs.cornell.edu/people/pabo/'
-       'movie-review-data/review_polarity.tar.gz')
+    import matplotlib
+matplotlib.use('TkAgg')
     
-    #import matplotlib.pyplot as plt
-#plt.matshow(cm, cmap=plt.cm.jet)
-#plt.show()
+    This example demonstrates how to generate a dataset and bicluster it
+using the Spectral Co-Clustering algorithm.
     
-        if f == os.path.basename(__file__):
-        continue
+    # plot shrinkage coefficient
+plt.subplot(2, 1, 2)
+plt.errorbar(n_samples_range, lw_shrinkage.mean(1), yerr=lw_shrinkage.std(1),
+             label='Ledoit-Wolf', color='navy', lw=2)
+plt.errorbar(n_samples_range, oa_shrinkage.mean(1), yerr=oa_shrinkage.std(1),
+             label='OAS', color='darkorange', lw=2)
+plt.xlabel('n_samples')
+plt.ylabel('Shrinkage')
+plt.legend(loc='lower right')
+plt.ylim(plt.ylim()[0], 1. + (plt.ylim()[1] - plt.ylim()[0]) / 10.)
+plt.xlim(5, 31)
     
-    # Author: Kemal Eren <kemal@kemaleren.com>
-# License: BSD 3 clause
-    
-        # add non-discriminative features
-    if n_features > 1:
-        X = np.hstack([X, np.random.randn(n_samples, n_features - 1)])
-    return X, y
-    
-    
-# Code source: Gaël Varoquaux
-# Modified for documentation by Jaques Grobler
-# License: BSD 3 clause
-    
-        def deploy_cert(self, domain, cert_path, key_path, chain_path=None,
-                    fullchain_path=None):
-        '''Installs cert'''
-        cert_path, key_path, chain_path = self.copy_certs_and_keys(
-            cert_path, key_path, chain_path)
-        self._configurator.deploy_cert(
-            domain, cert_path, key_path, chain_path, fullchain_path)
+    print('The data was generated from (random_state=%d):' % RANDOM_SEED)
+print('Class', 'P(C)', 'P(w0|C)', 'P(w1|C)', sep='\t')
+for k, p, p_w in zip(['red', 'blue', 'yellow'], p_c, p_w_c.T):
+    print('%s\t%0.2f\t%0.2f\t%0.2f' % (k, p, p_w[0], p_w[1]))
 
     
-        @mock.patch('certbot.notify.smtplib.LMTP')
-    @mock.patch('certbot.notify.subprocess.Popen')
-    def test_everything_fails(self, mock_popen, mock_lmtp):
-        from certbot.notify import notify
-        lmtp_obj = mock.MagicMock()
-        mock_lmtp.return_value = lmtp_obj
-        lmtp_obj.sendmail.side_effect = socket.error(17)
-        proc = mock.MagicMock()
-        mock_popen.return_value = proc
-        proc.communicate.side_effect = OSError('What we have here is a '
-                                               'failure to communicate.')
-        self.assertFalse(notify('Goose', 'auntrhody@example.com',
-                                'The old grey goose is dead.'))
-        self.assertEqual(lmtp_obj.sendmail.call_count, 1)
-        self.assertEqual(proc.communicate.call_count, 1)
     
-        result = False
-    try:
-        ret = urlopen(url, timeout=2)
-        result = (ret.code == 200)
-    except HTTPError as e:
-        print(e, file=sys.stderr)
-    except URLError as e:
-        print(e, file=sys.stderr)
-    except timeout as e:
-        print(e, file=sys.stderr)
-    except Exception as e:
-        print(e, file=sys.stderr)
-    
-    
-@click.command()
-@click.argument('catalog_file', type=click.Path())
-def cli(catalog_file):
-    # Read the old ones back.  Once we are in, we will never go.
-    with open(catalog_file) as f:
-        rv = json.load(f)['supported_locales']
+def huaban_download(url, output_dir='.', **kwargs):
+    if re.match(r'http://huaban\.com/boards/\d+/', url):
+        huaban_download_board(url, output_dir, **kwargs)
+    else:
+        print('Only board (画板) pages are supported currently')
+        print('ex: http://huaban.com/boards/12345678/')
