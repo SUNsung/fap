3c6118925db39d026ef17d6efd8764cc99cbb52b
SUNsung <weatherius@gmail.com>
2018-08-10 12:23:55 +0300

    Rewrite [25P653kMU1H4Y25xRF01Cn5I6225R9gYxPtvDr62509u.rb] File in: 2018-08-10 12:23:55

--- ruby/25P653kMU1H4Y25xRF01Cn5I6225R9gYxPtvDr62509u.rb
+++ ruby/25P653kMU1H4Y25xRF01Cn5I6225R9gYxPtvDr62509u.rb
@@ -1,66 +1,48 @@
 
         
-          class CachedFragment
-    def initialize(json)
-      @json = json
+            def execute(args)
+      topic_id = args[:topic_id]
+      raise Discourse::InvalidParameters.new(:topic_id) unless topic_id.present?
+    
+      def revoke_moderation!
+    set_permission('moderator', false)
+  end
+    
+      exec(*cmd)
+end
+    
+    Note that you will likely need to have opened the app at least
+once for any login items to be present.
+    
+        export LANG=en_US.UTF-8
+    \e[0m
+    DOC
+  end
+    
+          def self.options
+        [
+          ['--update', 'Run `pod repo update` before listing'],
+          ['--stats',  'Show additional stats (like GitHub watchers and forks)'],
+        ].concat(super)
+      end
+    
+        def possible_types
+      MIME::Types.type_for(@filepath).collect(&:content_type)
     end
-    def as_json(*_args)
-      @json
+    
+        # Extracts the Geometry from a 'WxH,O' string
+    # Where W is the width, H is the height,
+    # and O is the EXIF orientation
+    def self.parse(string)
+      GeometryParser.new(string).make
+    end
+    
+      def self.save_current_attachments_styles!
+    File.open(Paperclip.registered_attachments_styles_path, 'w') do |f|
+      YAML.dump(current_attachments_styles, f)
     end
   end
     
-        REDIRECT_RGX = /http-equiv='refresh'/i
-    NOT_FOUND_RGX = /<title>Not Found<\/title>/
-    
-        version 'Guide' do
-      self.base_url = 'https://www.tensorflow.org/'
-      self.root_path = 'get_started/get_started'
-      self.initial_paths = %w(
-        programmers_guide/reading_data
-        tutorials/mandelbrot
-        performance/performance_guide
-        deploy/hadoop
-        extend/architecture)
-    
-            a_split <=> b_split
-      else
-        a.casecmp(b)
+            raise LoadError, 'Could not find the '#{name}' processor in any of these paths: #{directories.join(', ')}' unless required.any?
       end
     end
-  end
-end
-
-    
-        def to_a
-      @filters.dup
-    end
-    
-      it 'does not copy constants defined in the singleton class' do
-    class << @obj
-      CLONE = :clone
-    end
-    
-        # Returns the Sass/SCSS code for the media query list.
-    #
-    # @param options [{Symbol => Object}] An options hash (see {Sass::CSS#initialize}).
-    # @return [String]
-    def to_src(options)
-      queries.map {|q| q.to_src(options)}.join(', ')
-    end
-    
-    class SinatraStaticServer < Sinatra::Base
-    
-        def generate(site)
-      site.write_category_indexes
-    end
-    
-      # Improved version of Liquid's truncate:
-  # - Doesn't cut in the middle of a word.
-  # - Uses typographically correct ellipsis (…) insted of '...'
-  def truncate(input, length)
-    if input.length > length && input[0..(length-1)] =~ /(.+)\b.+$/im
-      $1.strip + ' &hellip;'
-    else
-      input
-    end
-  end
