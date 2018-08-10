111a019f1f4629ff92ff94a6a14b991d9c790d0f
SUNsung <weatherius@gmail.com>
2018-08-10 05:57:30 +0300

    Rewrite [29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp] File in: 2018-08-10 05:57:30

--- cccccc/29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp
+++ cccccc/29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp
@@ -1,94 +1,219 @@
 
         
-        static const char* kModuleName = '_api_implementation';
-static const char kModuleDocstring[] =
-'_api_implementation is a module that exposes compile-time constants that\n'
-'determine the default API implementation to use for Python proto2.\n'
-'\n'
-'It complements api_implementation.py by setting defaults using compile-time\n'
-'constants defined in C, such that one can set defaults at compilation\n'
-'(e.g. with blaze flag --copt=-DPYTHON_PROTO2_CPP_IMPL_V2).';
+        // The 'Types' template argument below must have spaces around it
+// since some compilers may choke on '>>' when passing a template
+// instance (e.g. Types<int>)
+# define TYPED_TEST_CASE(CaseName, Types) \
+  typedef ::testing::internal::TypeList< Types >::type \
+      GTEST_TYPE_PARAMS_(CaseName)
     
-      // implements CodeGenerator ----------------------------------------
-  bool Generate(const FileDescriptor* file,
-                const string& parameter,
-                GeneratorContext* generator_context,
-                string* error) const;
-    
-    #ifndef GOOGLE_PROTOBUF_COMPILER_CSHARP_OPTIONS_H__
-#define GOOGLE_PROTOBUF_COMPILER_CSHARP_OPTIONS_H__
-    
-    namespace google {
-namespace protobuf {
-namespace compiler {
-namespace csharp {
+        void ComputeCurrentValue() {
+      if (!AtEnd())
+        current_value_ = ParamType(*current1_, *current2_, *current3_,
+            *current4_, *current5_, *current6_, *current7_, *current8_,
+            *current9_, *current10_);
     }
-    }
-    }
-    }
-    
-    
-    
-    namespace google {
-namespace protobuf {
-namespace compiler {
-namespace java {
-    }
-    }
-    }
+    bool AtEnd() const {
+      // We must report iterator past the end of the range when either of the
+      // component iterators has reached the end of its range.
+      return
+          current1_ == end1_ ||
+          current2_ == end2_ ||
+          current3_ == end3_ ||
+          current4_ == end4_ ||
+          current5_ == end5_ ||
+          current6_ == end6_ ||
+          current7_ == end7_ ||
+          current8_ == end8_ ||
+          current9_ == end9_ ||
+          current10_ == end10_;
     }
     
+      CartesianProductGenerator$i($for j, [[const ParamGenerator<T$j>& g$j]])
+      : $for j, [[g$(j)_(g$j)]] {}
+  virtual ~CartesianProductGenerator$i() {}
     
-    {
-    {
-    {}  // namespace java
-}  // namespace compiler
-}  // namespace protobuf
-    
-    ExtensionGenerator* ImmutableGeneratorFactory::NewExtensionGenerator(
-    const FieldDescriptor* descriptor) const {
-  if (HasDescriptorMethods(descriptor->file(), context_->EnforceLite())) {
-    return new ImmutableExtensionGenerator(descriptor, context_);
-  } else {
-    return new ImmutableExtensionLiteGenerator(descriptor, context_);
+    // Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
+int Factorial(int n) {
+  int result = 1;
+  for (int i = 1; i <= n; i++) {
+    result *= i;
   }
+    }
+    
+      bool check_for_leaks = false;
+  if (argc > 1 && strcmp(argv[1], '--check_for_leaks') == 0 )
+    check_for_leaks = true;
+  else
+    printf('%s\n', 'Run this program with --check_for_leaks to enable '
+           'custom leak checking in the tests.');
+    
+      bool thrown = false;
+  try {
+    thpp::IntTensor &a = dynamic_cast<thpp::IntTensor&>(*tensor);
+  } catch(std::bad_cast &e) {
+    thrown = true;
+  }
+  assert(thrown);
+    
+    Tensor fft(const Tensor& self, const int64_t signal_ndim, const bool normalized) {
+  return _fft(self, signal_ndim, /* complex_input */ true,
+              /* complex_output */ true, /* inverse */ false, {}, normalized,
+              /* onesided */ false);
 }
     
-    #ifndef GOOGLE_PROTOBUF_COMPILER_JAVA_GENERATOR_FACTORY_H__
-#define GOOGLE_PROTOBUF_COMPILER_JAVA_GENERATOR_FACTORY_H__
+    #ifdef USE_CUDA
+std::vector <THCStream*> THPUtils_PySequence_to_THCStreamList(PyObject *obj) {
+  if (!PySequence_Check(obj)) {
+    throw std::runtime_error('Expected a sequence in THPUtils_PySequence_to_THCStreamList');
+  }
+  THPObjectPtr seq = THPObjectPtr(PySequence_Fast(obj, NULL));
+  if (seq.get() == NULL) {
+    throw std::runtime_error('expected PySequence, but got ' + std::string(THPUtils_typename(obj)));
+  }
+    }
     
-    CV_EXPORTS Mat NormalizePixels(const Mat& imagePoints, const IntrinsicParams& param);
+    #ifndef HEATMAP_MAX_KEYPOINT_OP_H_
+#define HEATMAP_MAX_KEYPOINT_OP_H_
     
-    #if 0
-#include '_modelest.h'
+    Tensor mv(const Tensor& self, const Tensor& vec) {
+  check_1d(vec, 'vec', 'mv');
+  return at::_mv(self, vec);
+}
     
-                template<typename T>
-            static __device__ __forceinline__ T atomicInc(T* address, T val)
-            {
-#if defined (__CUDA_ARCH__) && (__CUDA_ARCH__ < 120)
-                T count;
-                unsigned int tag = threadIdx.x << ( (sizeof(unsigned int) << 3) - 5U);
-                do
-                {
-                    count = *address & TAG_MASK;
-                    count = tag | (count + 1);
-                    *address = count;
-                } while (*address != count);
+    #include 'RPC-inl.hpp'
+
+    
+    	GDCLASS(PhysicsMaterial, Resource);
+	OBJ_SAVE_TYPE(PhysicsMaterial);
+	RES_BASE_EXTENSION('PhyMat');
+    
+    	virtual void set_active(bool p_active);
+	virtual void init();
+	virtual void step(real_t p_step);
+	virtual void sync();
+	virtual void flush_queries();
+	virtual void end_sync();
+	virtual void finish();
+    
+    public:
+	static const String setting_property_name;
+    
+    				if (ep) {
+					//set all this before the control gets the ENTER_TREE notification
+					ep->object = object;
     }
     
     
-    {} // namespace detail
+    {		if (!_ptr)
+			return NULL;
+		return reinterpret_cast<T *>(_ptr);
+	}
     
-        Node* next() {
-      return next_;
+    
+    {    // Internal fields
+    rocksdb::Options options_;
+    rocksdb::Status status_;
+    rocksdb::DB* db_;
+    std::unordered_map<std::string, rocksdb::ColumnFamilyHandle*>
+        columnFamilies_;
+};
+    
+      virtual bool Merge(const Slice& key,
+                     const Slice* existing_value,
+                     const Slice& value,
+                     std::string* new_value,
+                     Logger* logger) const override;
+    
+      std::mutex _mutex;                 // used to protect modification of all following variables
+  librados::bufferlist _buffer;      // write buffer
+  uint64_t _buffer_size;             // write buffer size
+  uint64_t _file_size;               // this file size doesn't include buffer size
+    
+    void SyncPoint::LoadDependencyAndMarkers(
+  const std::vector<SyncPointPair>& dependencies,
+  const std::vector<SyncPointPair>& markers) {
+  impl_->LoadDependencyAndMarkers(dependencies, markers);
+}
+    
+    #ifndef ROCKSDB_LITE
+#include 'db/compacted_db_impl.h'
+#include 'db/db_impl.h'
+#include 'db/version_set.h'
+#include 'table/get_context.h'
+    
+    namespace folly {
     }
     
-    /*
- * A QuantileEstimator that keeps values for nWindows * windowDuration (see
- * constructor). Values are buffered for windowDuration.
+      folly::Optional<T> try_take_for(std::chrono::milliseconds time) override {
+    T item;
+    while (true) {
+      if (nonBlockingTake(item)) {
+        return std::move(item);
+      }
+      if (!sem_.try_wait_for(time)) {
+        return folly::none;
+      }
+    }
+  }
+    
+    
+    {  auto node = new hazptr_retire_node(obj, std::move(reclaim));
+  node->reclaim_ = [](hazptr_obj* p) {
+    delete static_cast<hazptr_retire_node*>(p);
+  };
+  objRetire(node);
+}
+    
+    /**
+ * Type that encapsulates the current pair of (to<string>(value), reason)
  */
-template <typename ClockT = std::chrono::steady_clock>
-class SlidingWindowQuantileEstimator {
- public:
-  using TimePoint = typename ClockT::time_point;
+using SettingsInfo = std::pair<std::string, std::string>;
+/**
+ * @return If the setting exists, the current setting information.
+ *         Empty Optional otherwise.
+ */
+folly::Optional<SettingsInfo> getAsString(folly::StringPiece settingName);
+    
+    
+    {
+    {}
+}
+    
+    template <class T>
+inline int SimpleUnpack(const void* _rawbuf, size_t _rawlen, size_t& _packlen, AutoBuffer& _data) {
+    if (sizeof(T) > _rawlen) return SIMPLE_CONTINUE;
     }
+    
+    
+    {
+    {        break;
+    }
+}
+    
+    #include 'comm/debugger/test_spy_sample.h'
+#include 'comm/xlogger/xlogger.h'
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+    // Unless required by applicable law or agreed to in writing, software distributed under the License is
+// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions and
+// limitations under the License.
+    
+    /**
+ * @file
+ **/
+    
+    namespace apollo {
+namespace planning {
+    }
+    }
+    
+    #include 'modules/planning/common/frame.h'
+#include 'modules/planning/common/reference_line_info.h'
+#include 'modules/planning/planner/planner.h'
+#include 'modules/planning/proto/planning_config.pb.h'
