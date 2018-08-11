5c55f1e96774febc3c7eacd282ca23a4af403c9c
SUNsung <weatherius@gmail.com>
2018-08-12 00:01:06 +0300

    Rewrite [46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py] File in: 2018-08-12 00:01:06

--- pyton/46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py
+++ pyton/46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py
@@ -1,206 +1,103 @@
 
         
-                def to_json(self, value):
-            return [[k, self.serializer.tag(v)] for k, v in iteritems(value)]
+        
+def has_docutils():
+    try:
+        # noinspection PyUnresolvedReferences
+        import docutils
+        return True
+    except ImportError:
+        return False
     
-        exec('def reraise(tp, value, tb=None):\n raise tp, value, tb')
-    
-        def __init__(self, root_path, defaults=None):
-        dict.__init__(self, defaults or {})
-        self.root_path = root_path
-    
-            # In case just a slash was appended we can be extra helpful
-        if request.base_url + '/' == exc.new_url.split('?')[0]:
-            buf.append('  The URL was defined with a trailing slash so '
-                       'Flask will automatically redirect to the URL '
-                       'with the trailing slash if it was accessed '
-                       'without one.')
-    
-            def __init__(self, name, doc=None):
-            self.name = name
-            self.__doc__ = doc
-        def _fail(self, *args, **kwargs):
-            raise RuntimeError('signalling support is unavailable '
-                               'because the blinker library is '
-                               'not installed.')
-        send = lambda *a, **kw: None
-        connect = disconnect = has_receivers_for = receivers_for = \
-            temporarily_connected_to = connected_to = _fail
-        del _fail
-    
-        When you want to decorate a pluggable view you will have to either do that
-    when the view function is created (by wrapping the return value of
-    :meth:`as_view`) or you can use the :attr:`decorators` attribute::
-    
-        info(
-        'Releasing %s (codename %s, release date %s)',
-        version, codename, release_date.strftime('%d/%m/%Y')
-    )
-    tags = get_git_tags()
-    
-        if logging_plugin:
-        pytestconfig.pluginmanager.register(logging_plugin, 'logging-plugin')
-    
-        Tests regressions.
-    
-            Number:                    '#990000',        # class: 'm'
-    
-    
-def dispatch_hook(key, hooks, hook_data, **kwargs):
-    '''Dispatches a hook dictionary on a given piece of data.'''
-    hooks = hooks or dict()
-    hooks = hooks.get(key)
-    if hooks:
-        if hasattr(hooks, '__call__'):
-            hooks = [hooks]
-        for hook in hooks:
-            _hook_data = hook(hook_data, **kwargs)
-            if _hook_data is not None:
-                hook_data = _hook_data
-    return hook_data
+        exc = Timeout('Request timed out')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
+    assert ret == ExitStatus.ERROR_TIMEOUT
+    assert error_msg == 'Request timed out (30s).'
 
     
-    
-def consume_socket_content(sock, timeout=0.5):
-    chunks = 65536
-    content = b''
-    
-    def test_fragment_not_sent_with_request():
-    '''Verify that the fragment portion of a URI isn't sent to the server.'''
-    def response_handler(sock):
-        req = consume_socket_content(sock, timeout=0.5)
-        sock.send(
-            b'HTTP/1.1 200 OK\r\n'
-            b'Content-Length: '+bytes(len(req))+b'\r\n'
-            b'\r\n'+req
-        )
-    
-    import json
-import platform
-import sys
-import ssl
-    
-    # Add any extra paths that contain custom files (such as robots.txt or
-# .htaccess) here, relative to this directory. These files are copied
-# directly to the root of the documentation.
-#html_extra_path = []
-    
-        def lower_items(self):
-        '''Like iteritems(), but with all lowercase keys.'''
-        return (
-            (lowerkey, keyval[1])
-            for (lowerkey, keyval)
-            in self._store.items()
-        )
-    
-    Some codes have multiple names, and both upper- and lower-case versions of
-the names are allowed. For example, ``codes.ok``, ``codes.OK``, and
-``codes.okay`` all correspond to the HTTP status code 200.
-'''
+        # Used only when requested with --check-status:
+    ERROR_HTTP_3XX = 3
+    ERROR_HTTP_4XX = 4
+    ERROR_HTTP_5XX = 5
     
     
-def request(method, url, **kwargs):
-    '''Constructs and sends a :class:`Request <Request>`.
+def test_default_options(httpbin):
+    env = MockEnvironment()
+    env.config['default_options'] = ['--form']
+    env.config.save()
+    r = http(httpbin.url + '/post', 'foo=bar', env=env)
+    assert r.json['form'] == {'foo': 'bar'}
     
-    import datetime
-import sys
     
-        time_ridge = np.empty(n_iter)
-    time_ols = np.empty(n_iter)
-    time_lasso = np.empty(n_iter)
+class TestDownloads:
+    # TODO: more tests
     
-        xx = np.arange(100, 100 + n * step, step)
-    plt.figure('scikit-learn vs. glmnet benchmark results')
-    plt.title('Regression in high dimensional spaces (%d samples)' % n_samples)
-    plt.plot(xx, scikit_results, 'b-', label='scikit-learn')
-    plt.plot(xx, glmnet_results, 'r-', label='glmnet')
-    plt.legend()
-    plt.xlabel('number of features')
-    plt.ylabel('Time (s)')
-    plt.axis('tight')
-    plt.show()
-
     
-    import time
+def test_headers_unset(httpbin_both):
+    r = http('GET', httpbin_both + '/headers')
+    assert 'Accept' in r.json['headers']  # default Accept present
     
-            if event == 'clear':
-            self.ax.clear()
-            self.ax.set_xticks([])
-            self.ax.set_yticks([])
-            self.contours = []
-            self.c_labels = None
-            self.plot_kernels()
+    \note Please be warned that the line numbers in the API documentation do not
+match the real locations in the source code of the package. This is an
+unintended artifact of doxygen, which I could only convince to use the
+correct module names by concatenating all files from the package into a single
+module file...
     
-    from sklearn.datasets import make_checkerboard
-from sklearn.datasets import samples_generator as sg
-from sklearn.cluster.bicluster import SpectralBiclustering
-from sklearn.metrics import consensus_score
+            self.expecting = expecting
+        
     
-    # Annotate points on the simplex
-plt.annotate(r'($\frac{1}{3}$, $\frac{1}{3}$, $\frac{1}{3}$)',
-             xy=(1.0/3, 1.0/3), xytext=(1.0/3, .23), xycoords='data',
-             arrowprops=dict(facecolor='black', shrink=0.05),
-             horizontalalignment='center', verticalalignment='center')
-plt.plot([1.0/3], [1.0/3], 'ko', ms=5)
-plt.annotate(r'($\frac{1}{2}$, $0$, $\frac{1}{2}$)',
-             xy=(.5, .0), xytext=(.5, .1), xycoords='data',
-             arrowprops=dict(facecolor='black', shrink=0.05),
-             horizontalalignment='center', verticalalignment='center')
-plt.annotate(r'($0$, $\frac{1}{2}$, $\frac{1}{2}$)',
-             xy=(.0, .5), xytext=(.1, .5), xycoords='data',
-             arrowprops=dict(facecolor='black', shrink=0.05),
-             horizontalalignment='center', verticalalignment='center')
-plt.annotate(r'($\frac{1}{2}$, $\frac{1}{2}$, $0$)',
-             xy=(.5, .5), xytext=(.6, .6), xycoords='data',
-             arrowprops=dict(facecolor='black', shrink=0.05),
-             horizontalalignment='center', verticalalignment='center')
-plt.annotate(r'($0$, $0$, $1$)',
-             xy=(0, 0), xytext=(.1, .1), xycoords='data',
-             arrowprops=dict(facecolor='black', shrink=0.05),
-             horizontalalignment='center', verticalalignment='center')
-plt.annotate(r'($1$, $0$, $0$)',
-             xy=(1, 0), xytext=(1, .1), xycoords='data',
-             arrowprops=dict(facecolor='black', shrink=0.05),
-             horizontalalignment='center', verticalalignment='center')
-plt.annotate(r'($0$, $1$, $0$)',
-             xy=(0, 1), xytext=(.1, 1), xycoords='data',
-             arrowprops=dict(facecolor='black', shrink=0.05),
-             horizontalalignment='center', verticalalignment='center')
-# Add grid
-plt.grid('off')
-for x in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    plt.plot([0, x], [x, 0], 'k', alpha=0.2)
-    plt.plot([0, 0 + (1-x)/2], [x, x + (1-x)/2], 'k', alpha=0.2)
-    plt.plot([x, x + (1-x)/2], [0, 0 + (1-x)/2], 'k', alpha=0.2)
+                t = self.tokenSource.nextToken()
+       
+        # leave p pointing at first token on channel
+        self.p = 0
+        self.p = self.skipOffTokenChannels(self.p)
     
-    Shows how shrinkage improves classification.
-'''
+        '''
     
-    The usual covariance maximum likelihood estimate can be regularized
-using shrinkage. Ledoit and Wolf proposed a close formula to compute
-the asymptotically optimal shrinkage parameter (minimizing a MSE
-criterion), yielding the Ledoit-Wolf covariance estimate.
+    def __init__(self, type=None, channel=DEFAULT_CHANNEL, text=None,
+                 input=None, start=None, stop=None, oldToken=None):
+        Token.__init__(self)
+        
+        if oldToken is not None:
+            self.type = oldToken.type
+            self.line = oldToken.line
+            self.charPositionInLine = oldToken.charPositionInLine
+            self.channel = oldToken.channel
+            self.index = oldToken.index
+            self._text = oldToken._text
+            if isinstance(oldToken, CommonToken):
+                self.input = oldToken.input
+                self.start = oldToken.start
+                self.stop = oldToken.stop
+            
+        else:
+            self.type = type
+            self.input = input
+            self.charPositionInLine = -1 # set to invalid position
+            self.line = 0
+            self.channel = channel
+            
+	    #What token number is this from 0..n-1 tokens; < 0 implies invalid index
+            self.index = -1
+            
+            # We need to be able to change the text once in a while.  If
+            # this is non-null, then getText should return this.  Note that
+            # start/stop are not affected by changing this.
+            self._text = text
     
-    #Load the digits dataset
-digits = datasets.load_digits()
+    - CommonTokenStream: A basic and most commonly used TokenStream
+  implementation.
+- TokenRewriteStream: A modification of CommonTokenStream that allows the
+  stream to be altered (by the Parser). See the 'tweak' example for a usecase.
     
-        # Gloss the lips
-    d.polygon(face_landmarks['top_lip'], fill=(150, 0, 0, 128))
-    d.polygon(face_landmarks['bottom_lip'], fill=(150, 0, 0, 128))
-    d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=8)
-    d.line(face_landmarks['bottom_lip'], fill=(150, 0, 0, 64), width=8)
+    	# Track the line at which the error occurred in case this is
+	# generated from a lexer.  We need to track this since the
+        # unexpected char doesn't carry the line info.
+        self.line = None
     
-        # Write the resulting image to the output video file
-    print('Writing frame {} / {}'.format(frame_number, length))
-    output_movie.write(frame)
+    def is_inside_code(line, indent_depth):
+    return is_code(line, indent_depth) > 0 or line.strip() == ''
     
-    # Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
-    
-    # Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
-
-    
-    # If false, no index is generated.
-#html_use_index = True
+      cppvar = GetHeaderGuardCPPVariable(filename)
