0f58eb529ffd126dcb06ce6b6673a69fb725fb44
SUNsung <weatherius@gmail.com>
2018-08-11 10:18:59 +0300

    Rewrite [53v.ecNqy6m6653wXClxLYAm0o53qqMcCHzQziI53yMe.cpp] File in: 2018-08-11 10:18:59

--- cccccc/53v.ecNqy6m6653wXClxLYAm0o53qqMcCHzQziI53yMe.cpp
+++ cccccc/53v.ecNqy6m6653wXClxLYAm0o53qqMcCHzQziI53yMe.cpp
@@ -1,309 +1,114 @@
 
         
-        // The argument separating arguments intended for the relauncher process from
-// those intended for the relaunched process. '---' is chosen instead of '--'
-// because CommandLine interprets '--' as meaning 'end of switches', but
-// for many purposes, the relauncher process' CommandLine ought to interpret
-// arguments intended for the relaunched process, to get the correct settings
-// for such things as logging and the user-data-dir in case it affects crash
-// reporting.
-extern const CharType* kRelauncherArgSeparator;
+        #define THC_GENERIC_FILE 'torch/csrc/generic/utils.cpp'
+#include <THC/THCGenerateAllTypes.h>
     
-    #include <vector>
+    PyObject * THPUtils_dispatchStateless(
+    PyObject *tensor, const char *name, PyObject *args, PyObject *kwargs)
+{
+  THPObjectPtr methods(PyObject_GetAttrString(tensor, THP_STATELESS_ATTRIBUTE_NAME));
+  if (!methods) {
+    return PyErr_Format(
+        PyExc_TypeError,
+        'Type %s doesn't implement stateless methods',
+        classOrTypename(tensor));
+  }
+  THPObjectPtr method(PyObject_GetAttrString(methods, name));
+  if (!method) {
+    return PyErr_Format(
+        PyExc_TypeError,
+        'Type %s doesn't implement stateless method %s',
+        classOrTypename(tensor),
+        name);
+  }
+  return PyObject_Call(method.get(), args, kwargs);
+}
     
-    namespace atom {
-    }
+        auto* output_data = output->template mutable_data<float>();
+    size_t block_size = input.size_from_dim(1);
+    size_t n_blocks = input.dim(0);
+    
+    PyObject * THPWrapper_New(void *data, void (*destructor)(void*))
+{
+  PyObject *args = PyTuple_New(0);
+  if (!args) {
+    return NULL;
+  }
+  PyObject *result = PyObject_Call(THPWrapperClass, args, NULL);
+  if (result) {
+    THPWrapper* wrapper = (THPWrapper*) result;
+    wrapper->data = data;
+    wrapper->destructor = destructor;
+  }
+  Py_DECREF(args);
+  return result;
+}
     
     
-    {}  // namespace printing
-    
-    
-    { private:
-  DISALLOW_COPY_AND_ASSIGN(Net);
+    {    {Functions::exit, exitWorker}
 };
     
-    int main(int /*argc*/, char** /*argv*/)
-{
-    InitDevice();
-    return 0;
+    #pragma once
+    
+      ~HazptrLockFreeLIFO() {
+    Node* next;
+    for (auto node = head(); node; node = next) {
+      next = node->next();
+      node->retire();
+    }
+    hazptr_cleanup<Atom>();
+  }
+    
+    /** Wide CAS.
+ */
+template <typename T, template <typename> class Atom = std::atomic>
+class HazptrWideCAS {
+  struct Node : public hazptr_obj_base<Node, Atom> {
+    T val_;
+    explicit Node(T v = {}) : val_(v) {}
+  };
+    }
+    
+     public:
+  WTCallback(PrivateConstructorTag, EventBase* base) : base_(base) {}
+    
+    /**
+ * Declare *allocx() and mallctl*() as weak symbols. These will be provided by
+ * jemalloc if we are using jemalloc, or will be nullptr if we are using another
+ * malloc implementation.
+ */
+extern 'C' void* mallocx(size_t, int)
+__attribute__((__weak__));
+extern 'C' void* rallocx(void*, size_t, int)
+__attribute__((__weak__));
+extern 'C' size_t xallocx(void*, size_t, size_t, int)
+__attribute__((__weak__));
+extern 'C' size_t sallocx(const void*, int)
+__attribute__((__weak__));
+extern 'C' void dallocx(void*, int)
+__attribute__((__weak__));
+extern 'C' void sdallocx(void*, size_t, int)
+__attribute__((__weak__));
+extern 'C' size_t nallocx(size_t, int)
+__attribute__((__weak__));
+extern 'C' int mallctl(const char*, void*, size_t*, void*, size_t)
+__attribute__((__weak__));
+extern 'C' int mallctlnametomib(const char*, size_t*, size_t*)
+__attribute__((__weak__));
+extern 'C' int mallctlbymib(const size_t*, size_t, void*, size_t*, void*,
+                            size_t)
+__attribute__((__weak__));
+    
+    FOLLY_ALWAYS_INLINE int __builtin_popcount(unsigned int x) {
+  return int(__popcnt(x));
+}
+    
+    
+    {  EXPECT_EQ(0, set->size());
+  EXPECT_THROW(std::move(f).get(), std::runtime_error);
 }
 
     
-        CV_Assert( state != 0 );
-    
-    void ComputeExtrinsicRefine(const Mat& imagePoints, const Mat& objectPoints, Mat& rvec,
-                            Mat&  tvec, Mat& J, const int MaxIter,
-                            const IntrinsicParams& param, const double thresh_cond);
-CV_EXPORTS Mat ComputeHomography(Mat m, Mat M);
-    
-        Mat img = _img.getMat(), cornersM = _corners.getMat();
-    int ncorners = cornersM.checkVector(2, CV_32F);
-    CV_Assert( ncorners >= 0 );
-    Point2f* corners = cornersM.ptr<Point2f>();
-    const int nbins = 256;
-    float ranges[] = {0, 256};
-    const float* _ranges = ranges;
-    Mat hist;
-    
-    /** @file
- * @deprecated Use @ref cudev instead.
- */
-    
-            // Core Extension: ARB_uniform_buffer_object
-        UNIFORM_BUFFER                   = 0x8A11,
-        UNIFORM_BUFFER_BINDING           = 0x8A28,
-        UNIFORM_BUFFER_START             = 0x8A29,
-        UNIFORM_BUFFER_SIZE              = 0x8A2A,
-        MAX_VERTEX_UNIFORM_BLOCKS        = 0x8A2B,
-        MAX_FRAGMENT_UNIFORM_BLOCKS      = 0x8A2D,
-        MAX_COMBINED_UNIFORM_BLOCKS      = 0x8A2E,
-        MAX_UNIFORM_BUFFER_BINDINGS      = 0x8A2F,
-        MAX_UNIFORM_BLOCK_SIZE           = 0x8A30,
-        MAX_COMBINED_VERTEX_UNIFORM_COMPONENTS = 0x8A31,
-        MAX_COMBINED_FRAGMENT_UNIFORM_COMPONENTS = 0x8A33,
-        UNIFORM_BUFFER_OFFSET_ALIGNMENT  = 0x8A34,
-        ACTIVE_UNIFORM_BLOCK_MAX_NAME_LENGTH = 0x8A35,
-        ACTIVE_UNIFORM_BLOCKS            = 0x8A36,
-        UNIFORM_TYPE                     = 0x8A37,
-        UNIFORM_SIZE                     = 0x8A38,
-        UNIFORM_NAME_LENGTH              = 0x8A39,
-        UNIFORM_BLOCK_INDEX              = 0x8A3A,
-        UNIFORM_OFFSET                   = 0x8A3B,
-        UNIFORM_ARRAY_STRIDE             = 0x8A3C,
-        UNIFORM_MATRIX_STRIDE            = 0x8A3D,
-        UNIFORM_IS_ROW_MAJOR             = 0x8A3E,
-        UNIFORM_BLOCK_BINDING            = 0x8A3F,
-        UNIFORM_BLOCK_DATA_SIZE          = 0x8A40,
-        UNIFORM_BLOCK_NAME_LENGTH        = 0x8A41,
-        UNIFORM_BLOCK_ACTIVE_UNIFORMS    = 0x8A42,
-        UNIFORM_BLOCK_ACTIVE_UNIFORM_INDICES = 0x8A43,
-        UNIFORM_BLOCK_REFERENCED_BY_VERTEX_SHADER = 0x8A44,
-        UNIFORM_BLOCK_REFERENCED_BY_FRAGMENT_SHADER = 0x8A46,
-        INVALID_INDEX                    = 0xFFFFFFFF,
-        MAX_GEOMETRY_UNIFORM_BLOCKS      = 0x8A2C,
-        MAX_COMBINED_GEOMETRY_UNIFORM_COMPONENTS = 0x8A32,
-        UNIFORM_BLOCK_REFERENCED_BY_GEOMETRY_SHADER = 0x8A45,
-    
-    using namespace cv::ocl::runtime;
-    
-    const float g_8x32fTab[] =
-{
-    -128.f, -127.f, -126.f, -125.f, -124.f, -123.f, -122.f, -121.f,
-    -120.f, -119.f, -118.f, -117.f, -116.f, -115.f, -114.f, -113.f,
-    -112.f, -111.f, -110.f, -109.f, -108.f, -107.f, -106.f, -105.f,
-    -104.f, -103.f, -102.f, -101.f, -100.f,  -99.f,  -98.f,  -97.f,
-     -96.f,  -95.f,  -94.f,  -93.f,  -92.f,  -91.f,  -90.f,  -89.f,
-     -88.f,  -87.f,  -86.f,  -85.f,  -84.f,  -83.f,  -82.f,  -81.f,
-     -80.f,  -79.f,  -78.f,  -77.f,  -76.f,  -75.f,  -74.f,  -73.f,
-     -72.f,  -71.f,  -70.f,  -69.f,  -68.f,  -67.f,  -66.f,  -65.f,
-     -64.f,  -63.f,  -62.f,  -61.f,  -60.f,  -59.f,  -58.f,  -57.f,
-     -56.f,  -55.f,  -54.f,  -53.f,  -52.f,  -51.f,  -50.f,  -49.f,
-     -48.f,  -47.f,  -46.f,  -45.f,  -44.f,  -43.f,  -42.f,  -41.f,
-     -40.f,  -39.f,  -38.f,  -37.f,  -36.f,  -35.f,  -34.f,  -33.f,
-     -32.f,  -31.f,  -30.f,  -29.f,  -28.f,  -27.f,  -26.f,  -25.f,
-     -24.f,  -23.f,  -22.f,  -21.f,  -20.f,  -19.f,  -18.f,  -17.f,
-     -16.f,  -15.f,  -14.f,  -13.f,  -12.f,  -11.f,  -10.f,   -9.f,
-      -8.f,   -7.f,   -6.f,   -5.f,   -4.f,   -3.f,   -2.f,   -1.f,
-       0.f,    1.f,    2.f,    3.f,    4.f,    5.f,    6.f,    7.f,
-       8.f,    9.f,   10.f,   11.f,   12.f,   13.f,   14.f,   15.f,
-      16.f,   17.f,   18.f,   19.f,   20.f,   21.f,   22.f,   23.f,
-      24.f,   25.f,   26.f,   27.f,   28.f,   29.f,   30.f,   31.f,
-      32.f,   33.f,   34.f,   35.f,   36.f,   37.f,   38.f,   39.f,
-      40.f,   41.f,   42.f,   43.f,   44.f,   45.f,   46.f,   47.f,
-      48.f,   49.f,   50.f,   51.f,   52.f,   53.f,   54.f,   55.f,
-      56.f,   57.f,   58.f,   59.f,   60.f,   61.f,   62.f,   63.f,
-      64.f,   65.f,   66.f,   67.f,   68.f,   69.f,   70.f,   71.f,
-      72.f,   73.f,   74.f,   75.f,   76.f,   77.f,   78.f,   79.f,
-      80.f,   81.f,   82.f,   83.f,   84.f,   85.f,   86.f,   87.f,
-      88.f,   89.f,   90.f,   91.f,   92.f,   93.f,   94.f,   95.f,
-      96.f,   97.f,   98.f,   99.f,  100.f,  101.f,  102.f,  103.f,
-     104.f,  105.f,  106.f,  107.f,  108.f,  109.f,  110.f,  111.f,
-     112.f,  113.f,  114.f,  115.f,  116.f,  117.f,  118.f,  119.f,
-     120.f,  121.f,  122.f,  123.f,  124.f,  125.f,  126.f,  127.f,
-     128.f,  129.f,  130.f,  131.f,  132.f,  133.f,  134.f,  135.f,
-     136.f,  137.f,  138.f,  139.f,  140.f,  141.f,  142.f,  143.f,
-     144.f,  145.f,  146.f,  147.f,  148.f,  149.f,  150.f,  151.f,
-     152.f,  153.f,  154.f,  155.f,  156.f,  157.f,  158.f,  159.f,
-     160.f,  161.f,  162.f,  163.f,  164.f,  165.f,  166.f,  167.f,
-     168.f,  169.f,  170.f,  171.f,  172.f,  173.f,  174.f,  175.f,
-     176.f,  177.f,  178.f,  179.f,  180.f,  181.f,  182.f,  183.f,
-     184.f,  185.f,  186.f,  187.f,  188.f,  189.f,  190.f,  191.f,
-     192.f,  193.f,  194.f,  195.f,  196.f,  197.f,  198.f,  199.f,
-     200.f,  201.f,  202.f,  203.f,  204.f,  205.f,  206.f,  207.f,
-     208.f,  209.f,  210.f,  211.f,  212.f,  213.f,  214.f,  215.f,
-     216.f,  217.f,  218.f,  219.f,  220.f,  221.f,  222.f,  223.f,
-     224.f,  225.f,  226.f,  227.f,  228.f,  229.f,  230.f,  231.f,
-     232.f,  233.f,  234.f,  235.f,  236.f,  237.f,  238.f,  239.f,
-     240.f,  241.f,  242.f,  243.f,  244.f,  245.f,  246.f,  247.f,
-     248.f,  249.f,  250.f,  251.f,  252.f,  253.f,  254.f,  255.f
-};
-    
-      static vector<string> LayerTypeList() {
-    CreatorRegistry& registry = Registry();
-    vector<string> layer_types;
-    for (typename CreatorRegistry::iterator iter = registry.begin();
-         iter != registry.end(); ++iter) {
-      layer_types.push_back(iter->first);
-    }
-    return layer_types;
-  }
-    
-    /**
- * @brief Abstract base class that factors out the BLAS code common to
- *        ConvolutionLayer and DeconvolutionLayer.
- */
-template <typename Dtype>
-class BaseConvolutionLayer : public Layer<Dtype> {
- public:
-  explicit BaseConvolutionLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    
-    {}  // namespace caffe
-    
-      virtual inline const char* type() const { return 'Concat'; }
-  virtual inline int MinBottomBlobs() const { return 1; }
-  virtual inline int ExactNumTopBlobs() const { return 1; }
-    
-      bool handles_setup_;
-  cudnnHandle_t             handle_;
-  cudnnLRNDescriptor_t norm_desc_;
-  cudnnTensorDescriptor_t bottom_desc_, top_desc_;
-    
-    
-    {}  // namespace caffe
-    
-    /**
- * @brief A layer for learning 'embeddings' of one-hot vector input.
- *        Equivalent to an InnerProductLayer with one-hot vectors as input, but
- *        for efficiency the input is the 'hot' index of each column itself.
- *
- * TODO(dox): thorough documentation for Forward, Backward, and proto params.
- */
-template <typename Dtype>
-class EmbedLayer : public Layer<Dtype> {
- public:
-  explicit EmbedLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    TEST_F(MockEnvTest, FakeSleeping) {
-  int64_t now = 0;
-  auto s = env_->GetCurrentTime(&now);
-  ASSERT_OK(s);
-  env_->FakeSleepForMicroseconds(3 * 1000 * 1000);
-  int64_t after_sleep = 0;
-  s = env_->GetCurrentTime(&after_sleep);
-  ASSERT_OK(s);
-  auto delta = after_sleep - now;
-  // this will be true unless test runs for 2 seconds
-  ASSERT_TRUE(delta == 3 || delta == 4);
-}
-    
-      // Reports dropped bytes to the reporter.
-  // buffer_ must be updated to remove the dropped bytes prior to invocation.
-  void ReportCorruption(size_t bytes, const char* reason);
-  void ReportDrop(size_t bytes, const Status& reason);
-    
-      jboolean attached_thread = JNI_FALSE;
-  JNIEnv* env = getJniEnv(&attached_thread);
-  assert(env != nullptr);
-    
-     private:
-    // used for synchronisation in compare method
-    std::unique_ptr<port::Mutex> mtx_compare;
-    // used for synchronisation in findShortestSeparator method
-    std::unique_ptr<port::Mutex> mtx_findShortestSeparator;
-    std::unique_ptr<const char[]> m_name;
-    jmethodID m_jCompareMethodId;
-    jmethodID m_jFindShortestSeparatorMethodId;
-    jmethodID m_jFindShortSuccessorMethodId;
-    
-    template <typename C, ordering o, bool ne>
-struct cmp_pred : private C {
-  using C::C;
-    }
-    
-      static Entry& entryInstance() {
-    /* library-local */ static auto entry = detail::createGlobal<Entry, Tag>();
-    return *entry;
-  }
-    
-    #include <atomic>
-    
-    
-    {    queues_.reserve(capacities.size());
-    for (auto capacity : capacities) {
-      queues_.emplace_back(capacity);
-    }
-  }
-    
-      BlockingQueueAddResult add(T item) override {
-    queue_.enqueue(std::move(item));
-    return sem_.post();
-  }
-    
-      // Move from the registration phase to the 'you can actually instantiate
-  // things now' phase.
-  folly::SingletonVault::singleton()->registrationComplete();
-    
-    template <size_t M>
-FOLLY_ALWAYS_INLINE hazptr_array<M>& hazptr_array<M>::operator=(
-    hazptr_array&& other) noexcept {
-  HAZPTR_DEBUG_PRINT(this << ' ' << M << ' ' << &other);
-  auto h = reinterpret_cast<hazptr_holder*>(&raw_);
-  for (size_t i = 0; i < M; ++i) {
-    h[i] = std::move(other[i]);
-    HAZPTR_DEBUG_PRINT(i << ' ' << &h[i] << ' ' << &other[i]);
-  }
-  empty_ = other.empty_;
-  other.empty_ = true;
-  return *this;
-}
-    
-    
-    {  // vector of {quantile, value}
-  std::vector<std::pair<double, double>> quantiles;
-};
-    
-      static time_point now() {
-    return Now;
-  }
-    
-    template <typename Value, typename T>
-struct AllocatorHasDefaultObjectDestroy<std::allocator<Value>, T>
-    : std::true_type {};
-    
-    
-    {
-    {
-    {} // namespace detail
-} // namespace f14
-} // namespace folly
-    
-    TEST(allocateOverAligned, manualOverCustomAlloc) {
-  // allocates 6 byte with alignment 64 using non-standard allocator, which
-  // will result in an allocation of 64 + alignof(max_align_t) underneath.
-  ExpectingAlloc<short> a(
-      alignof(folly::max_align_t), 64 / alignof(folly::max_align_t) + 1);
-  auto p = folly::allocateOverAligned<decltype(a), 64>(a, 3);
-  EXPECT_EQ((reinterpret_cast<uintptr_t>(p) % 64), 0);
-  folly::deallocateOverAligned<decltype(a), 64>(a, p, 3);
-  EXPECT_EQ(
-      (folly::allocationBytesForOverAligned<decltype(a), 64>(3)),
-      64 + alignof(folly::max_align_t));
-}
-    
-    namespace folly {
-namespace settings {
-    }
+    TEST(NonCopyableLambda, Function) {
+  Promise<int> promise;
     }
