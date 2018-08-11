7e7bc6f88c42d0f8fd81b9dc8e55498bfb6f96fa
SUNsung <weatherius@gmail.com>
2018-08-11 20:42:48 +0300

    Rewrite [01MvdAqlSHeE201GbhSugxz9gQ01gy3YYb2RoT.014xq.py] File in: 2018-08-11 20:42:47

--- pyton/01MvdAqlSHeE201GbhSugxz9gQ01gy3YYb2RoT.014xq.py
+++ pyton/01MvdAqlSHeE201GbhSugxz9gQ01gy3YYb2RoT.014xq.py
@@ -1,28 +1,59 @@
 
         
-        
-setup_locate_faces = '''
-import face_recognition
-    
-    while True:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
-    
-    # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file('obama.jpg')
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-    
-    # Find all the faces and face encodings in the unknown image
-face_locations = face_recognition.face_locations(unknown_image)
-face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+            def take_call(self, call):
+        '''Assume the employee will always successfully take the call.'''
+        self.call = call
+        self.call.employee = self
+        self.call.state = CallState.IN_PROGRESS
     
     
-@click.command()
-@click.argument('image_to_check')
-@click.option('--cpus', default=1, help='number of CPU cores to use in parallel. -1 means 'use all in system'')
-@click.option('--model', default='hog', help='Which face detection model to use. Options are 'hog' or 'cnn'.')
-def main(image_to_check, cpus, model):
-    # Multi-core processing only supported on Python 3.4 or greater
-    if (sys.version_info < (3, 4)) and cpus != 1:
-        click.echo('WARNING: Multi-processing support requires Python 3.4 or greater. Falling back to single-threaded processing!')
-        cpus = 1
+class PersonServer(object):
+    
+        # Are checkable fields missing from the test case definition?
+    test_info_dict = dict((key, value if not isinstance(value, compat_str) or len(value) < 250 else 'md5:' + md5(value))
+                          for key, value in got_dict.items()
+                          if value and key in ('id', 'title', 'description', 'uploader', 'upload_date', 'timestamp', 'uploader_id', 'location', 'age_limit'))
+    missing_keys = set(test_info_dict.keys()) - set(expected_dict.keys())
+    if missing_keys:
+        def _repr(v):
+            if isinstance(v, compat_str):
+                return ''%s'' % v.replace('\\', '\\\\').replace(''', '\\'').replace('\n', '\\n')
+            else:
+                return repr(v)
+        info_dict_str = ''
+        if len(missing_keys) != len(expected_dict):
+            info_dict_str += ''.join(
+                '    %s: %s,\n' % (_repr(k), _repr(v))
+                for k, v in test_info_dict.items() if k not in missing_keys)
+    
+    
+def _is_empty(d):
+    return not bool(os.listdir(d))
+    
+    
+def gen_extractors():
+    ''' Return a list of an instance of every supported extractor.
+    The order does matter; the first extractor matched is the one handling the URL.
+    '''
+    return [klass() for klass in gen_extractor_classes()]
+    
+            webpage = self._download_webpage(url, video_id)
+        key = self._search_regex(
+            r'src=['\']https?://[^/]+/embed/([A-Za-z0-9_-]+)', webpage, 'key')
+    
+    import re
+    
+            info = page['info']
+    
+        def _real_extract(self, url):
+        display_id = self._match_id(url)
+    
+        _TEST = {
+        'url': 'http://www.defense.gouv.fr/layout/set/ligthboxvideo/base-de-medias/webtv/attaque-chimique-syrienne-du-21-aout-2013-1',
+        'md5': '75bba6124da7e63d2d60b5244ec9430c',
+        'info_dict': {
+            'id': '11213',
+            'ext': 'mp4',
+            'title': 'attaque-chimique-syrienne-du-21-aout-2013-1'
+        }
+    }
