621b7fada2d8c7cda84cd46bf1875814c4f0bb2e
SUNsung <weatherius@gmail.com>
2018-08-11 03:10:24 +0300

    Rewrite [12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp] File in: 2018-08-11 03:10:23

--- cccccc/12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp
+++ cccccc/12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp
@@ -1,247 +1,281 @@
 
-    {  /// Returns true if \p BB is a dead-end block.
-  bool isDeadEnd(SILBasicBlock *BB) {
-    if (!isComputed) {
-      // Lazily compute the dataflow.
-      compute();
-      isComputed = true;
-    }
-    return ReachableBlocks.count(BB) == 0;
-  }
-};
+        
+          image_file.read(reinterpret_cast<char*>(&magic), 4);
+  magic = swap_endian(magic);
+  CHECK_EQ(magic, 2051) << 'Incorrect image file magic.';
+  label_file.read(reinterpret_cast<char*>(&magic), 4);
+  magic = swap_endian(magic);
+  CHECK_EQ(magic, 2049) << 'Incorrect label file magic.';
+  image_file.read(reinterpret_cast<char*>(&num_items), 4);
+  num_items = swap_endian(num_items);
+  label_file.read(reinterpret_cast<char*>(&num_labels), 4);
+  num_labels = swap_endian(num_labels);
+  CHECK_EQ(num_items, num_labels);
+  image_file.read(reinterpret_cast<char*>(&rows), 4);
+  rows = swap_endian(rows);
+  image_file.read(reinterpret_cast<char*>(&cols), 4);
+  cols = swap_endian(cols);
     
-    
-    {
-    {    C[0] = C[1];
-    TargetStart = C + 1;
-    GCBForC0 = GCBForC1;
-  }
-}
-    
-    // NOTE: this class is NOT meant to be used in threaded contexts.
-@interface ObjectBehaviorVerifier : NSObject
-@property (readonly) BOOL wasRetained;
-@property (readonly) BOOL wasCopied;
-@property (readonly) BOOL wasMutableCopied;
-    
-    /// A SyntaxRewriter for applying a set of formatting rules to a Syntax tree.
-struct FormatSyntaxRewriter : public SyntaxRewriter {
-  virtual StructDeclSyntax
-  rewriteStructDecl(StructDeclSyntax Struct) override;
-};
-    
-      virtual void handleDiagnostic(SourceManager &SM, SourceLoc Loc,
-                                DiagnosticKind Kind,
-                                StringRef FormatString,
-                                ArrayRef<DiagnosticArgument> FormatArgs,
-                                const DiagnosticInfo &Info) override;
-    
-    #endif
-    
-    #endif // SWIFT_INDEX_INDEX_H
+    #endif  // CAFFE_ARGMAX_LAYER_HPP_
 
     
-    protected:
-  /// The base of the object.
-  SILValue Base;
-  /// Empty key, tombstone key or normal key.
-  KeyKind Kind;
-  /// The path to reach the accessed field of the object.
-  Optional<ProjectionPath> Path;
-    
-    #include 'swift/AST/DiagnosticConsumer.h'
-#include 'swift/Migrator/FixitFilter.h'
-#include 'swift/Migrator/Migrator.h'
-#include 'swift/Migrator/Replacement.h'
-#include 'clang/Rewrite/Core/RewriteBuffer.h'
-#include 'llvm/ADT/DenseSet.h'
-    
-    Vec3d median3d(InputArray m);
-    
-            bool isGeneralPosition;
-        do
-        {
-            ArrayTest::fill_array(test_case_idx, i, j, arr);
+    /**
+ * @brief Computes @f$ y = x + \log(1 + \exp(-x)) @f$ if @f$ x > 0 @f$;
+ *        @f$ y = \log(1 + \exp(x)) @f$ otherwise.
+ *
+ * @param bottom input Blob vector (length 1)
+ *   -# @f$ (N \times C \times H \times W) @f$
+ *      the inputs @f$ x @f$
+ * @param top output Blob vector (length 1)
+ *   -# @f$ (N \times C \times H \times W) @f$
+ *      the computed outputs @f$
+ *      y = \left\{
+ *         \begin{array}{ll}
+ *            x + \log(1 + \exp(-x)) & \mbox{if } x > 0 \\
+ *            \log(1 + \exp(x)) & \mbox{otherwise}
+ *         \end{array} \right.
+ *      @f$
+ */
+template <typename Dtype>
+class BNLLLayer : public NeuronLayer<Dtype> {
+ public:
+  explicit BNLLLayer(const LayerParameter& param)
+      : NeuronLayer<Dtype>(param) {}
+    }
     }
     
-    #ifndef OPENCV_CUDA_WARP_REDUCE_HPP__
-#define OPENCV_CUDA_WARP_REDUCE_HPP__
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
     
-    #include <memory>
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
     
-    static void RunSynchronousUnaryPingPong() {
-  gpr_log(GPR_INFO, 'Running Synchronous Unary Ping Pong');
+    
+    {  bool handles_setup_;
+  cudnnHandle_t             handle_;
+  cudnnTensorDescriptor_t bottom_desc_;
+  cudnnTensorDescriptor_t top_desc_;
+  cudnnActivationDescriptor_t activ_desc_;
+};
+#endif
+    
+    
+    {  bool handles_setup_;
+  cudnnHandle_t             handle_;
+  cudnnTensorDescriptor_t bottom_desc_;
+  cudnnTensorDescriptor_t top_desc_;
+};
+#endif
+    
+    namespace caffe {
     }
     
-    #include <grpc/support/log.h>
+     protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
     
-    std::string GetDbFileContent(int argc, char** argv);
+      /**
+   * @brief Computes the error gradient w.r.t. the exp inputs.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *      respect to the outputs
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
+   *      with respect to computed outputs @f$ y @f$
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$; Backward fills their diff with
+   *      gradients @f$
+   *        \frac{\partial E}{\partial x} =
+   *            \frac{\partial E}{\partial y} y \alpha \log_e(gamma)
+   *      @f$ if propagate_down[0]
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
     
-    bool SecureAuthContext::SetPeerIdentityPropertyName(const grpc::string& name) {
-  if (!ctx_) return false;
-  return grpc_auth_context_set_peer_identity_property_name(ctx_,
-                                                           name.c_str()) != 0;
-}
     
-      // gRPC Python
-  grpc_python_generator::GeneratorConfiguration grpc_py_config;
-  grpc_python_generator::PythonGrpcGenerator grpc_py_generator(grpc_py_config);
-  cli.RegisterGenerator('--grpc_python_out', &grpc_py_generator,
-                        'Generate Python source file.');
+    {}  // namespace caffe
     
-    	GodotKinClosestConvexResultCallback(const btVector3 &convexFromWorld, const btVector3 &convexToWorld, const RigidBodyBullet *p_self_object, bool p_infinite_inertia) :
-			btCollisionWorld::ClosestConvexResultCallback(convexFromWorld, convexToWorld),
-			m_self_object(p_self_object),
-			m_infinite_inertia(p_infinite_inertia) {}
     
-      // Loads a JPEG image from a memory buffer or a file.
-  // req_comps can be 1 (grayscale), 3 (RGB), or 4 (RGBA).
-  // On return, width/height will be set to the image's dimensions, and actual_comps will be set to the either 1 (grayscale) or 3 (RGB).
-  // Notes: For more control over where and how the source data is read, see the decompress_jpeg_image_from_stream() function below, or call the jpeg_decoder class directly.
-  // Requesting a 8 or 32bpp image is currently a little faster than 24bpp because the jpeg_decoder class itself currently always unpacks to either 8 or 32bpp.
-  unsigned char *decompress_jpeg_image_from_memory(const unsigned char *pSrc_data, int src_data_size, int *width, int *height, int *actual_comps, int req_comps);
-  unsigned char *decompress_jpeg_image_from_file(const char *pSrc_filename, int *width, int *height, int *actual_comps, int req_comps);
+    {} // namespace cuda
     
-       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
-   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-    
-    // deletes a node from the network including setting all input links to it to null, and removing it from the node groups
-void ComputationNetwork::DeleteNode(const std::wstring& nodeName)
-{
-    InvalidateCompiledNetwork();
+    struct Handle {
+  cudnnHandle_t handle;
+  Handle() : handle(NULL) {
+    AT_CUDNN_CHECK(cudnnCreate(&handle));
+  }
+  ~Handle() {
+    if (handle) {
+// this is because of something dumb in the ordering of
+// destruction. Sometimes atexit, the cuda context (or something)
+// would already be destroyed by the time this gets destroyed. It
+// happens in fbcode setting. @colesbury and I decided to not destroy
+// the handle as a workaround.
+//   - @soumith
+#ifdef NO_CUDNN_DESTROY_HANDLE
+#else
+      cudnnDestroy(handle);
+#endif
     }
+  }
+};
     
-        // DENSE
-    Matrix<float> mB = Matrix<float>::RandomGaussian(dim2, dim3, c_deviceIdZero, 1.0f, 4.0f, IncrementCounter());
-    Matrix<float> mC = Matrix<float>::RandomGaussian(dim1, dim3, c_deviceIdZero, 1.0f, 2.0f, IncrementCounter());
-    Matrix<float> mD(mC.DeepClone());
-    
-    
-    {            // forward prop till reaching the current bn node
-            m_net->ForwardProp(node);
+        // Checks if input strides can be viewed as embedded.
+    // See NOTE [ cuFFT Embedded Strides ].
+    //
+    // TODO: Figure out why windows fails to compile
+    //         at::optional<std::vector<long long int>> inembed_opt = at::nullopt;
+    //       Then move the following to a helper function.
+    std::vector<long long int> inembed(signal_ndim);
+    if (!clone_input) {
+      auto istrides = input.strides();
+      auto last_istride = istrides[signal_ndim];
+      clone_input = last_istride <= 0;
+      for (auto i = signal_ndim - 1; !clone_input && i > 0 /* inembed[0] doesn't matteer */; i--) {
+        auto istride = istrides[i];
+        if (istride > 0 && istride % last_istride == 0) {
+          inembed[i] = istride / last_istride;
+          last_istride = istride;
+        } else {
+          clone_input = true;
         }
-    
-        virtual void Destroy() override;
-    
-      /// Bind the acceptor to the given local endpoint.
-  /**
-   * This function binds the socket acceptor to the specified endpoint on the
-   * local machine.
-   *
-   * @param endpoint An endpoint on the local machine to which the socket
-   * acceptor will be bound.
-   *
-   * @param ec Set to indicate what error occurred, if any.
-   *
-   * @par Example
-   * @code
-   * boost::asio::ip::tcp::acceptor acceptor(io_service);
-   * boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), 12345);
-   * acceptor.open(endpoint.protocol());
-   * boost::system::error_code ec;
-   * acceptor.bind(endpoint, ec);
-   * if (ec)
-   * {
-   *   // An error occurred.
-   * }
-   * @endcode
-   */
-  boost::system::error_code bind(const endpoint_type& endpoint,
-      boost::system::error_code& ec)
-  {
-    return this->get_service().bind(this->get_implementation(), endpoint, ec);
-  }
-    
-      /// Constructor to set a particular expiry time relative to now.
-  /**
-   * This constructor creates a timer and sets the expiry time.
-   *
-   * @param io_service The io_service object that the timer will use to dispatch
-   * handlers for any asynchronous operations performed on the timer.
-   *
-   * @param expiry_time The expiry time to be used for the timer, relative to
-   * now.
-   */
-  basic_waitable_timer(boost::asio::io_service& io_service,
-      const duration& expiry_time)
-    : basic_io_object<WaitableTimerService>(io_service)
-  {
-    boost::system::error_code ec;
-    this->service.expires_from_now(this->implementation, expiry_time, ec);
-    boost::asio::detail::throw_error(ec, 'expires_from_now');
-  }
-    
-    namespace boost {
-namespace asio {
-    }
+      }
     }
     
-    namespace boost {
-namespace asio {
-    }
-    }
-    
-      // Resize the buffer to the specified length.
-  void resize(size_type length)
-  {
-    BOOST_ASIO_ASSERT(length <= capacity());
-    if (begin_offset_ + length <= capacity())
-    {
-      end_offset_ = begin_offset_ + length;
-    }
-    else
-    {
-      using namespace std; // For memmove.
-      memmove(&buffer_[0], &buffer_[0] + begin_offset_, size());
-      end_offset_ = length;
-      begin_offset_ = 0;
-    }
-  }
-    
-    #include <boost/asio/detail/config.hpp>
-#include <list>
-#include <utility>
-#include <boost/asio/detail/assert.hpp>
-#include <boost/asio/detail/noncopyable.hpp>
-    
-      BtMessageDispatcher* getBtMessageDispatcher() const { return dispatcher_; }
-    
-    bool AbstractOptionHandler::getChangeOptionForReserved() const
+    void setSamplerDescriptor(SpatialTransformerDescriptor& desc,
+                          cudnnDataType_t dataType,
+                          int N, int C, int H, int W)
 {
-  return flags_ & FLAG_CHANGE_OPTION_FOR_RESERVED;
+  int inputSize[4] = {N, C, H, W};
+  desc.set(dataType, 4, inputSize);
 }
     
-    #include <memory>
-    
-    void AnnounceTier::nextEvent()
-{
-  switch (event) {
-  case STARTED:
-    event = DOWNLOADING;
-    break;
-  case STARTED_AFTER_COMPLETION:
-    event = SEEDING;
-    break;
-  case STOPPED:
-    event = HALTED;
-    break;
-  case COMPLETED:
-    event = SEEDING;
-    break;
-  default:
-    break;
+    Tensor expandScale(const Tensor& t, int64_t dim) {
+  std::vector<int64_t> size{ 1, t.numel() };
+  while (static_cast<int64_t>(size.size()) < dim) {
+    size.emplace_back(1);
   }
+  return t.view(size);
 }
     
+    // ---------------------------------------------------------------------
+//
+// Bias addition
+//
+// ---------------------------------------------------------------------
     
-    {} // namespace aria2
+      TensorDescriptor idesc{ *input };  // input descriptor
+  TensorDescriptor odesc{ *grad_output };  // grad_output descriptor
+  TensorDescriptor gdesc{ grad_input_t };  // grad_input descriptor
+  SpatialTransformerDescriptor desc; // sampler descriptor
+    
+        std::set<THCStreamPtr> streams(std::move(block->stream_uses));
+    THAssert(block->stream_uses.empty());
+    for (auto it = streams.begin(); it != streams.end(); ++it) {
+      auto& stream = *it;
+    }
+    
+      // p2pAccessEnabled records if p2p copies are allowed between pairs of
+  // devices. Values include '1' (copy allowed), '0' (copy not allowed), and
+  // '-1' (unknown).
+  // Currently the max number of gpus in P2P group is 8, so if there are more
+  // we enable P2P in groups of 8
+  state->p2pAccessEnabled = (int**) malloc(sizeof(int*) * numDevices);
+  for (int i = 0; i < numDevices; ++i) {
+    state->p2pAccessEnabled[i] = (int*) malloc(sizeof(int) * numDevices);
+    for (int j = 0; j < numDevices; ++j)
+      if (i == j)
+        state->p2pAccessEnabled[i][j] = 1;
+      else if (j / THC_CUDA_MAX_PEER_SIZE != i / THC_CUDA_MAX_PEER_SIZE)
+        state->p2pAccessEnabled[i][j] = 0;
+      else
+        state->p2pAccessEnabled[i][j] = -1;
+  }
+    
+    THC_API int THCStream_device(THCStream* stream) { 
+  return at::cuda::detail::CUDAStream_device(stream);
+}
+    
+    // Create the pybind11 module
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  // Use the same name as the check functions so error messages make sense
+  m.def(cudnn_relu_name, &cudnn_relu, 'CuDNN ReLU');
+}
+
+    
+    // This is a pass-through wrapper function that does the size check and
+// inferences. The actual forward implementation function is called
+// at::_fft_with_size which dispatches to _fft_cufft (CUDA) or _fft_mkl (CPU).
+static inline Tensor _fft(const Tensor &self, const int64_t signal_ndim,
+           const bool complex_input, const bool complex_output,
+           const bool inverse, IntList signal_sizes, const bool normalized,
+           const bool onesided) {
+    }
+    
+      /* storageOffset */
+  if (storageOffset < 0)
+    THError('can't set negative storage offset');
+  self->storageOffset = storageOffset;
+    
+    
+    {} // namespace at
+
+    
+    inline half float16ToHalf(const float16 x) {
+  __half_raw hr;
+  hr.x = x.x;
+  half r(hr);
+  return r;
+}
+    
+      /**
+   * @brief Gets a human-readable description of the box
+   * @return A debug-string
+   */
+  std::string DebugString() const;
+    
+      /**
+   * @brief Get the extreme points along a heading direction.
+   * @param heading The specified heading.
+   * @param first The point on the boundary of this polygon with the minimal
+   *        projection onto the heading direction.
+   * @param last The point on the boundary of this polygon with the maximal
+   *        projection onto the heading direction.
+   */
+  void ExtremePoints(const double heading, Vec2d *const first,
+                     Vec2d *const last) const;
+    
+    
+    {  /**
+   * @brief Compute a trajectory for execution.
+   * @param planning_init_point The trajectory point where planning starts.
+   * @param frame Current planning frame.
+   * @param reference_line_info The computed reference line.
+   * @return OK if planning succeeds; error otherwise.
+   */
+  virtual apollo::common::Status PlanOnReferenceLine(
+      const common::TrajectoryPoint& planning_init_point, Frame* frame,
+      ReferenceLineInfo* reference_line_info) = 0;
+};
+    
+      EXPECT_TRUE(
+      PathObstacle::MergeLongitudinalDecision(decision_follow, decision_ignore)
+          .has_follow());
+  EXPECT_TRUE(PathObstacle::MergeLongitudinalDecision(decision_follow,
+                                                      decision_overtake)
+                  .has_follow());
