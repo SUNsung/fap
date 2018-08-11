f3e967c5be384e9330994c7533a1c682d644caa4
SUNsung <weatherius@gmail.com>
2018-08-11 21:21:37 +0300

    Rewrite [01cWKUdTnqkwA01xLV.CskJXS.01no9FxPrT.QM01BTg.py] File in: 2018-08-11 21:21:36

--- pyton/01cWKUdTnqkwA01xLV.CskJXS.01no9FxPrT.QM01BTg.py
+++ pyton/01cWKUdTnqkwA01xLV.CskJXS.01no9FxPrT.QM01BTg.py
@@ -1,123 +1,14 @@
 
         
-        from werkzeug.local import LocalProxy
+            def setUp(self):
+        from acme.errors import BadNonce
+        self.error = BadNonce(nonce='xxx', error='error')
     
-    from datetime import timedelta
-import os
-import textwrap
+    # Output file base name for HTML help builder.
+htmlhelp_basename = 'certbot-apachedoc'
     
-    
-@pytest.fixture(autouse=True)
-def reset_logging(pytestconfig):
-    root_handlers = logging.root.handlers[:]
-    logging.root.handlers = []
-    root_level = logging.root.level
-    
-                greenlets.append(greenlet(g))
-            return 'Hello World!'
-    
-    
-def main(args_list):
-    args = get_args(args_list)
-    if args.list:
-        print_list()
-    if args.host:
-        print_host(args.host)
-    
-    
-def get_from_rhc_config(variable):
-    global configparser
-    CONF_FILE = os.path.expanduser('~/.openshift/express.conf')
-    if os.path.exists(CONF_FILE):
-        if not configparser:
-            ini_str = '[root]\n' + open(CONF_FILE, 'r').read()
-            configparser = ConfigParser.SafeConfigParser()
-            configparser.readfp(StringIO.StringIO(ini_str))
-        try:
-            return configparser.get('root', variable)
-        except ConfigParser.NoOptionError:
-            return None
-    
-    
-def extract_contents(args, path, output_dir):
-    '''
-    :type args: any
-    :type path: str
-    :type output_dir: str
-    '''
-    if not args.test:
-        if not os.path.exists(path):
-            return
-    
-        parser = argparse.ArgumentParser(description='Start a new Shippable run.')
-    
-        def on_open_shell(self):
-        try:
-            for cmd in (b'set terminal length 0', b'set terminal width 512'):
-                self._exec_cli_command(cmd)
-            self._exec_cli_command(b'set terminal length %d' % self.terminal_length)
-        except AnsibleConnectionFailure:
-            raise AnsibleConnectionFailure('unable to set terminal parameters')
-
-    
-        results = {}
-    for group in sort_groups(groups):
-        results = combine_vars(results, group.get_vars())
-    
-            if opts.pidfile:
-            with open(opts.pidfile, 'w') as f:
-                f.write(str(os.getpid()) + os.linesep)
-    
-        def needs_backout(self):
-        return len(self.active) >= self.total_concurrency
-    
-        def download_request(self, request, spider):
-        scheme = urlparse_cached(request).scheme
-        handler = self._get_handler(scheme)
-        if not handler:
-            raise NotSupported('Unsupported URL scheme '%s': %s' %
-                               (scheme, self._notconfigured[scheme]))
-        return handler.download_request(request, spider)
-    
-        def spider_opened(self, spider):
-        self._timeout = getattr(spider, 'download_timeout', self._timeout)
-    
-    from w3lib.http import basic_auth_header
-    
-    from scrapy import signals
-    
-        def error_count(self, failure, response, spider):
-        self.counter['errorcount'] += 1
-        if self.counter['errorcount'] == self.close_on['errorcount']:
-            self.crawler.engine.close_spider(spider, 'closespider_errorcount')
-    
-            #Conversion of md to HTML
-        for md in allmd:
-    
-    # PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-    
-        :param X_img_path: path to image to be recognized
-    :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.
-    :param model_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.
-    :param distance_threshold: (optional) distance threshold for face classification. the larger it is, the more chance
-           of mis-classifying an unknown person as a known one.
-    :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...].
-        For faces of unrecognized persons, the name 'unknown' will be returned.
-    '''
-    if not os.path.isfile(X_img_path) or os.path.splitext(X_img_path)[1][1:] not in ALLOWED_EXTENSIONS:
-        raise Exception('Invalid image path: {}'.format(X_img_path))
-    
-    
-def test_image(image_to_check, model):
-    unknown_image = face_recognition.load_image_file(image_to_check)
-    face_locations = face_recognition.face_locations(unknown_image, number_of_times_to_upsample=0, model=model)
-    
-    test_requirements = [
-    'tox',
-    'flake8==2.6.0'
-]
-    
-    # Find all facial features in all the faces in the image
-face_landmarks_list = face_recognition.face_landmarks(image)
+        def copy_certs_and_keys(self, cert_path, key_path, chain_path=None):
+        '''Copies certs and keys into the temporary directory'''
+        cert_and_key_dir = os.path.join(self._temp_dir, 'certs_and_keys')
+        if not os.path.isdir(cert_and_key_dir):
+            os.mkdir(cert_and_key_dir)
