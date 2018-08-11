f9d7e3813f2993837c131eb2dbc780f5f1b8289a
SUNsung <weatherius@gmail.com>
2018-08-11 18:35:45 +0300

    Rewrite [58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py] File in: 2018-08-11 18:35:44

--- pyton/58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py
+++ pyton/58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py
@@ -1,170 +1,226 @@
 
         
-            def take_call(self, call):
-        '''Assume the employee will always successfully take the call.'''
-        self.call = call
-        self.call.employee = self
-        self.call.state = CallState.IN_PROGRESS
+        versions_info = json.load(open('update/versions.json'))
+versions = list(versions_info['versions'].keys())
+versions.sort()
     
-            (2016-01, url0), 1
-        (2016-01, url0), 1
-        (2016-01, url1), 1
-        '''
-        url = self.extract_url(line)
-        period = self.extract_year_month(line)
-        yield (period, url), 1
+        def download(self, x):
+        self.result.append(x)
     
-            (foo, p1), 2
-        (bar, p1), 3
-        (foo, p2), 3
-        (bar, p3), 10
-        (foo, p4), 1
-        '''
-        yield key, sum(values)
+        def test_youtube_extract(self):
+        assertExtractId = lambda url, id: self.assertEqual(YoutubeIE.extract_id(url), id)
+        assertExtractId('http://www.youtube.com/watch?&v=BaW_jenozKc', 'BaW_jenozKc')
+        assertExtractId('https://www.youtube.com/watch?&v=BaW_jenozKc', 'BaW_jenozKc')
+        assertExtractId('https://www.youtube.com/watch?feature=player_embedded&v=BaW_jenozKc', 'BaW_jenozKc')
+        assertExtractId('https://www.youtube.com/watch_popup?v=BaW_jenozKc', 'BaW_jenozKc')
+        assertExtractId('http://www.youtube.com/watch?v=BaW_jenozKcsharePLED17F32AD9753930', 'BaW_jenozKc')
+        assertExtractId('BaW_jenozKc', 'BaW_jenozKc')
     
+            uploader = self._search_regex(
+            r'Added by\s*:\s*<a[^>]+>([^<]+)</a>', webpage, 'uploader', default=None)
+        upload_date = unified_strdate(self._search_regex(
+            r'Added on\s*:\s*([\d-]+)', webpage, 'upload date', default=None))
     
-class LinkedList(object):
+        def _real_extract(self, url):
+        display_id = self._match_id(url)
     
-        def bfs(self, source, dest):
-        if source is None:
-            return False
-        queue = deque()
-        queue.append(source)
-        source.visit_state = State.visited
-        while queue:
-            node = queue.popleft()
-            print(node)
-            if dest is node:
-                return True
-            for adjacent_node in node.adj_nodes.values():
-                if adjacent_node.visit_state == State.unvisited:
-                    queue.append(adjacent_node)
-                    adjacent_node.visit_state = State.visited
-        return False
+    print('Building model...')
+model = Sequential()
+model.add(Dense(512, input_shape=(max_words,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
     
+        model.compile(loss='hinge', optimizer='adagrad')
+    history = model.fit(x_train, y_train, epochs=20, batch_size=16,
+                        validation_data=(x_test, y_test), verbose=0)
+    assert (history.history['val_loss'][-1] < 0.9)
     
-with codecs.open(FILE_PATH, encoding='utf8') as f:
-    # Strip because we don't want new lines in the data so that we can
-    # easily count occurrences also when embedded in JSON (where the new
-    # line would be escaped).
-    FILE_CONTENT = f.read().strip()
+                for t in range(n_frames):
+                x_shift = xstart + directionx * t
+                y_shift = ystart + directiony * t
+                noisy_movies[i, t, x_shift - w: x_shift + w,
+                             y_shift - w: y_shift + w, 0] += 1
     
+    - RNNs are tricky. Choice of batch size is important,
+choice of loss and optimizer is critical, etc.
+Some configurations won't converge.
     
-@mock.patch('httpie.core.get_response')
-def test_error_traceback(get_response):
-    exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    with raises(ConnectionError):
-        main(['--ignore-stdin', '--traceback', 'www.google.com'])
+    from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
     
-        # Will be set to the raw value of `-a` (if provided) before
-    # `get_auth()` gets called.
-    raw_auth = None
+        if args.plot is not None:
+        print('Displaying plot', file=sys.stderr)
+        title = ('Multilabel metrics with %s' %
+                 ', '.join('{0}={1}'.format(field, getattr(args, field))
+                           for field in ['samples', 'classes', 'density']
+                           if args.plot != field))
+        _plot(results, args.metrics, args.formats, title, steps, args.plot)
+
     
-        # Used only when requested with --check-status:
-    ERROR_HTTP_3XX = 3
-    ERROR_HTTP_4XX = 4
-    ERROR_HTTP_5XX = 5
+    In this examples we will use a movie review dataset.
     
-        def test_actual_download(self, httpbin_both, httpbin):
-        robots_txt = '/robots.txt'
-        body = urlopen(httpbin + robots_txt).read().decode()
-        env = MockEnvironment(stdin_isatty=True, stdout_isatty=False)
-        r = http('--download', httpbin_both.url + robots_txt, env=env)
-        assert 'Downloading' in r.stderr
-        assert '[K' in r.stderr
-        assert 'Done' in r.stderr
-        assert body == r
-    
-        def test_verbose_implies_all(self, httpbin):
-        r = http('--verbose', '--follow', httpbin + '/redirect/1')
-        assert 'GET /redirect/1 HTTP/1.1' in r
-        assert 'HTTP/1.1 302 FOUND' in r
-        assert 'GET /get HTTP/1.1' in r
-        assert HTTP_OK in r
-    
-        def __enter__(self):
-        from scrapy.utils.test import get_testenv
-        pargs = [sys.executable, '-u', '-m', 'scrapy.utils.benchserver']
-        self.proc = subprocess.Popen(pargs, stdout=subprocess.PIPE,
-                                     env=get_testenv())
-        self.proc.stdout.readline()
-    
-    
-    
-            return requests
-    
-            e.g.:
-        @returns request
-        @returns request 2
-        @returns request 2 10
-        @returns request 0 10
     '''
+# Author: Olivier Grisel <olivier.grisel@ensta.org>
+# License: Simplified BSD
     
-        def _download(self, slot, request, spider):
-        # The order is very important for the following deferreds. Do not change!
+    plt.figure(2)  # 'banana' shape
+plt.title('Outlier detection on a real data set (boston housing)')
+plt.scatter(X2[:, 0], X2[:, 1], color='black')
+plt.xlim((xx2.min(), xx2.max()))
+plt.ylim((yy2.min(), yy2.max()))
+plt.legend((legend2_values_list[0].collections[0],
+            legend2_values_list[1].collections[0],
+            legend2_values_list[2].collections[0]),
+           (legend2_keys_list[0], legend2_keys_list[1], legend2_keys_list[2]),
+           loc='upper center',
+           prop=matplotlib.font_manager.FontProperties(size=12))
+plt.ylabel('% lower status of the population')
+plt.xlabel('average number of rooms per dwelling')
     
-            if not aws_access_key_id:
-            aws_access_key_id = settings['AWS_ACCESS_KEY_ID']
-        if not aws_secret_access_key:
-            aws_secret_access_key = settings['AWS_SECRET_ACCESS_KEY']
+    The dataset is generated using the ``make_biclusters`` function, which
+creates a matrix of small values and implants bicluster with large
+values. The rows and columns are then shuffled and passed to the
+Spectral Co-Clustering algorithm. Rearranging the shuffled matrix to
+make biclusters contiguous shows how accurately the algorithm found
+the biclusters.
     
-        def handleResponse(self, response):
-        if self.factory.method.upper() == b'HEAD':
-            self.factory.page(b'')
-        elif self.length is not None and self.length > 0:
-            self.factory.noPage(self._connection_lost_reason)
+    Only adjusted measures can hence safely be used as a consensus index
+to evaluate the average stability of clustering algorithms for a given
+value of k on various overlapping sub-samples of the dataset.
+    
+        def testSeekBackwardsAcrossStreams(self):
+        self.createTempFile(streams=2)
+        with BZ2File(self.filename) as bz2f:
+            readto = len(self.TEXT) + 100
+            while readto > 0:
+                readto -= len(bz2f.read(readto))
+            bz2f.seek(-150, 1)
+            self.assertEqual(bz2f.read(), self.TEXT[100-150:] + self.TEXT)
+    
+    
+# Return whether a path is absolute.
+# Trivial in Posix, harder on Windows.
+# For Windows it is absolute if it starts with a slash or backslash (current
+# volume), or if a pathname after the volume-letter-and-colon or UNC-resource
+# starts with a slash or backslash.
+    
+        @unittest.skipUnless(nt, 'OS helpers require 'nt' module')
+    def test_nt_helpers(self):
+        # Trivial validation that the helpers do not break, and support both
+        # unicode and bytes (UTF-8) paths
+    
+    <hr>
+Data descriptors defined here:<br>
+<dl><dt><strong>__dict__</strong></dt>
+<dd><tt>%s</tt></dd>
+</dl>
+<dl><dt><strong>__weakref__</strong></dt>
+<dd><tt>%s</tt></dd>
+</dl>
+</td></tr></table> <p>
+<table width='100%%' cellspacing=0 cellpadding=2 border=0 summary='section'>
+<tr bgcolor='#ffc8d8'>
+<td colspan=3 valign=bottom>&nbsp;<br>
+<font color='#000000' face='helvetica, arial'><a name='B'>class <strong>B</strong></a>(<a href='builtins.html#object'>builtins.object</a>)</font></td></tr>
+\x20\x20\x20\x20
+<tr><td bgcolor='#ffc8d8'><tt>&nbsp;&nbsp;&nbsp;</tt></td><td>&nbsp;</td>
+<td width='100%%'>Data descriptors defined here:<br>
+<dl><dt><strong>__dict__</strong></dt>
+<dd><tt>%s</tt></dd>
+</dl>
+<dl><dt><strong>__weakref__</strong></dt>
+<dd><tt>%s</tt></dd>
+</dl>
+<hr>
+Data and other attributes defined here:<br>
+<dl><dt><strong>NO_MEANING</strong> = 'eggs'</dl>
+    
+        def test_dont_pause_writing(self):
+        tr = self.pause_writing_transport(high=4)
+    
+            callLst[:] = []
+        testme // 1
+        self.assertCallStack([('__floordiv__', (testme, 1))])
+    
+            class E(object):
+            @property
+            def foo(self):
+                return self._foo
+            @foo.setter
+            def foo(self, value):
+                raise RuntimeError
+            @foo.setter
+            def foo(self, value):
+                self._foo = abs(value)
+            @foo.deleter
+            def foo(self, value=None):
+                del self._foo
+    
+        def finish_tree(self, tree, filename):
+        self.transformed_xranges = None
+    
+    
+    @unittest.skipUnless(hasattr(c_hashlib, 'scrypt'),
+                     '   test requires OpenSSL > 1.1')
+    def test_scrypt(self):
+        for password, salt, n, r, p, expected in self.scrypt_test_vectors:
+            result = hashlib.scrypt(password, salt=salt, n=n, r=r, p=p)
+            self.assertEqual(result, expected)
+    
+    
+def parse_args():
+    p = argparse.ArgumentParser()
+    p.add_argument('-v', '--verbose', action='store_true')
+    p.add_argument('-b', '--binary', action='store_true',
+                   help='Is the dependency in the binary repo?')
+    p.add_argument('-O', '--organization',
+                   help='Organization owning the deps repos', default='python')
+    p.add_argument('-e', '--externals-dir', type=pathlib.Path,
+                   help='Directory in which to store dependencies',
+                   default=pathlib.Path(__file__).parent.parent / 'externals')
+    p.add_argument('tag',
+                   help='tag of the dependency')
+    return p.parse_args()
+    
+    
+# Find a suitable Perl installation for OpenSSL.
+# cygwin perl does *not* work.  ActivePerl does.
+# Being a Perl dummy, the simplest way I can check is if the 'Win32' package
+# is available.
+def find_working_perl(perls):
+    for perl in perls:
+        try:
+            subprocess.check_output([perl, '-e', 'use Win32;'])
+        except subprocess.CalledProcessError:
+            continue
         else:
-            self.factory.page(response)
-        self.transport.loseConnection()
+            return perl
     
-        def scrape_response(self, scrape_func, response, request, spider):
-        fname = lambda f:'%s.%s' % (
-                six.get_method_self(f).__class__.__name__,
-                six.get_method_function(f).__name__)
+        # Loop over each face found in the frame to see if it's someone we know.
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
+        name = '<Unknown Person>'
     
+    print('Benchmarks (Note: All benchmarks are only using a single CPU core)')
+print()
     
-class AjaxCrawlMiddleware(object):
-    '''
-    Handle 'AJAX crawlable' pages marked as crawlable via meta tag.
-    For more info see https://developers.google.com/webmasters/ajax-crawling/docs/getting-started.
-    '''
+    # Find all the faces in the image using the default HOG-based model.
+# This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
+# See also: find_faces_in_picture_cnn.py
+face_locations = face_recognition.face_locations(image)
     
-        @classmethod
-    def from_crawler(cls, crawler):
-        o = cls(crawler.settings.getfloat('DOWNLOAD_TIMEOUT'))
-        crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
-        return o
+    # Loop through each face found in the unknown image
+for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+    # See if the face is a match for the known face(s)
+    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
     
-        @staticmethod
-    def get_streams_by_id(account_number, video_id):
-        '''
-        int, int->list
-        
-        Get the height of the videos.
-        
-        Since brightcove is using 3 kinds of links: rtmp, http and https,
-        we will be using the HTTPS one to make it secure.
-        
-        If somehow akamaihd.net is blocked by the Great Fucking Wall,
-        change the 'startswith https' to http.
-        '''
-        endpoint = 'https://edge.api.brightcove.com/playback/v1/accounts/{account_number}/videos/{video_id}'.format(account_number = account_number, video_id = video_id)
-        fake_header_id = fake_headers
-        #is this somehow related to the time? Magic....
-        fake_header_id['Accept'] ='application/json;pk=BCpkADawqM1cc6wmJQC2tvoXZt4mrB7bFfi6zGt9QnOzprPZcGLE9OMGJwspQwKfuFYuCjAAJ53JdjI8zGFx1ll4rxhYJ255AXH1BQ10rnm34weknpfG-sippyQ'
-    
-    from ..common import *
-    
-        t = r1(r'type=(\w+)', flashvars)
-    id = r1(r'vid=([^']+)', flashvars)
-    if t == 'youku':
-        youku_download_by_vid(id, title=title, output_dir=output_dir, merge=merge, info_only=info_only)
-    elif t == 'tudou':
-        tudou_download_by_id(id, title, output_dir=output_dir, merge=merge, info_only=info_only)
-    elif t == 'sina' or t == 'video':
-        fake_headers['Referer'] = url
-        url = 'http://www.miomio.tv/mioplayer/mioplayerconfigfiles/sina.php?vid=' + id
-        xml_data = get_content(url, headers=fake_headers, decoded=True)
-        url_list = sina_xml_to_url_list(xml_data)
+    class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
