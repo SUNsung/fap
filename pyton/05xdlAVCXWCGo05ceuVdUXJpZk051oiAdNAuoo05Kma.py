9f2fb9e51f02973dce5b0eac810f971f64d71c32
SUNsung <weatherius@gmail.com>
2018-08-12 16:40:45 +0300

    Rewrite [05xdlAVCXWCGo05ceuVdUXJpZk051oiAdNAuoo05Kma.py] File in: 2018-08-12 16:40:44

--- pyton/05xdlAVCXWCGo05ceuVdUXJpZk051oiAdNAuoo05Kma.py
+++ pyton/05xdlAVCXWCGo05ceuVdUXJpZk051oiAdNAuoo05Kma.py
@@ -1,32 +1,186 @@
 
         
-            def to_json(self, value):
-        # JSON objects may only have string keys, so don't bother tagging the
-        # key here.
-        return dict((k, self.serializer.tag(v)) for k, v in iteritems(value))
+            def steps(self):
+        '''Run the map and reduce steps.'''
+        return [
+            self.mr(mapper=self.mapper,
+                    reducer=self.reducer)
+        ]
     
-            return session.modified or (
-            session.permanent and app.config['SESSION_REFRESH_EACH_REQUEST']
-        )
+        def extract_year_month(self, line):
+        '''Return the year and month portions of the timestamp.'''
+        pass
     
-            def __init__(self, name, doc=None):
-            self.name = name
-            self.__doc__ = doc
-        def _fail(self, *args, **kwargs):
-            raise RuntimeError('signalling support is unavailable '
-                               'because the blinker library is '
-                               'not installed.')
-        send = lambda *a, **kw: None
-        connect = disconnect = has_receivers_for = receivers_for = \
-            temporarily_connected_to = connected_to = _fail
-        del _fail
+        def append_to_front(self, node):
+        pass
     
-        :param source: the source code of the template to be
-                   rendered
-    :param context: the variables that should be available in the
-                    context of the template.
+    
+class LookupService(object):
+    
+            # because special names such as Name.Class, Name.Function, etc.
+        # are not recognized as such later in the parsing, we choose them
+        # to look the same as ordinary variables.
+        Name:                      '#000000',        # class: 'n'
+        Name.Attribute:            '#c4a000',        # class: 'na' - to be revised
+        Name.Builtin:              '#004461',        # class: 'nb'
+        Name.Builtin.Pseudo:       '#3465a4',        # class: 'bp'
+        Name.Class:                '#000000',        # class: 'nc' - to be revised
+        Name.Constant:             '#000000',        # class: 'no' - to be revised
+        Name.Decorator:            '#888',           # class: 'nd' - to be revised
+        Name.Entity:               '#ce5c00',        # class: 'ni'
+        Name.Exception:            'bold #cc0000',   # class: 'ne'
+        Name.Function:             '#000000',        # class: 'nf'
+        Name.Property:             '#000000',        # class: 'py'
+        Name.Label:                '#f57900',        # class: 'nl'
+        Name.Namespace:            '#000000',        # class: 'nn' - to be revised
+        Name.Other:                '#000000',        # class: 'nx'
+        Name.Tag:                  'bold #004461',   # class: 'nt' - like a keyword
+        Name.Variable:             '#000000',        # class: 'nv' - to be revised
+        Name.Variable.Class:       '#000000',        # class: 'vc' - to be revised
+        Name.Variable.Global:      '#000000',        # class: 'vg' - to be revised
+        Name.Variable.Instance:    '#000000',        # class: 'vi' - to be revised
+    
+    This module provides the capabilities for the Requests hooks system.
+    
+        def test_lower_items(self):
+        assert list(self.case_insensitive_dict.lower_items()) == [('accept', 'application/json')]
+    
+    
+@pytest.mark.skipif(sys.version_info < (2,7), reason='Only run on Python 2.7+')
+def test_system_ssl():
+    '''Verify we're actually setting system_ssl when it should be available.'''
+    assert info()['system_ssl']['version'] != ''
+    
+    :copyright: (c) 2012 by Kenneth Reitz.
+:license: Apache2, see LICENSE for more details.
+'''
+    
+            See GH #3579
+        '''
+        cj = cookiejar_from_dict({'foo': 'bar'}, cookielib.CookieJar())
+        s = requests.Session()
+        s.cookies = cookiejar_from_dict({'cookie': 'tasty'})
+    
+        def __str__(self):
+        #return 'MismatchedTokenException('+self.expecting+')'
+        return 'MismatchedTokenException(%r!=%r)' % (
+            self.getUnexpectedType(), self.expecting
+            )
+    __repr__ = __str__
+    
+            if self.p == -1:
+            self.fillBuffer()
+    
     '''
-    ctx = _app_ctx_stack.top
-    ctx.app.update_template_context(context)
-    return _render(ctx.app.jinja_env.from_string(source),
-                   context, ctx.app)
+    
+        def finish_tree(self, tree, filename):
+        if self.found_future_import:
+            return
+        if not isinstance(tree, pytree.Node):
+            # Empty files (usually __init__.py) show up as a single Leaf
+            # instead of a Node, so leave them alone
+            return
+        first_stmt = tree.children[0]
+        if is_docstring(first_stmt):
+            # Skip a line and add the import after the docstring
+            tree.insert_child(1, Newline())
+            pos = 2
+        elif first_stmt.prefix:
+            # No docstring, but an initial comment (perhaps a #! line).
+            # Transfer the initial comment to a new blank line.
+            newline = Newline()
+            newline.prefix = first_stmt.prefix
+            first_stmt.prefix = ''
+            tree.insert_child(0, newline)
+            pos = 1
+        else:
+            # No comments or docstring, just insert at the start
+            pos = 0
+        tree.insert_child(pos, self.new_future_import(None))
+        tree.insert_child(pos + 1, Newline())  # terminates the import stmt
+
+    
+    from tornado.auth import TwitterMixin
+from tornado.escape import json_decode, json_encode
+from tornado.ioloop import IOLoop
+from tornado import gen
+from tornado.options import define, options, parse_command_line, parse_config_file
+from tornado.web import Application, RequestHandler, authenticated
+    
+        def run_redbot(self, url, method, body, headers):
+        red = HttpResource(url, method=method, req_body=body,
+                           req_hdrs=headers)
+    
+        for i in range(1, num_tests + 1):
+        logging.info('running test case %d', i)
+        url = options.url + '/runCase?case=%d&agent=%s' % (i, options.name)
+        test_ws = yield websocket_connect(url, None, compression_options={})
+        while True:
+            message = yield test_ws.read_message()
+            if message is None:
+                break
+            test_ws.write_message(message, binary=isinstance(message, bytes))
+    
+    
+class RootHandler(RequestHandler):
+    def get(self):
+        self.write('Hello, world')
+    
+        def put(self, bucket_name):
+        path = os.path.abspath(os.path.join(
+            self.application.directory, bucket_name))
+        if not path.startswith(self.application.directory) or \
+           os.path.exists(path):
+            raise web.HTTPError(403)
+        os.makedirs(path)
+        self.finish()
+    
+    import os
+import subprocess
+import sys
+import urllib
+    
+    setup(
+    name='tornado',
+    version=version,
+    packages=['tornado', 'tornado.test', 'tornado.platform'],
+    package_data={
+        # data files need to be listed both here (which determines what gets
+        # installed) and in MANIFEST.in (which determines what gets included
+        # in the sdist tarball)
+        'tornado.test': [
+            'README',
+            'csv_translations/fr_FR.csv',
+            'gettext_translations/fr_FR/LC_MESSAGES/tornado_test.mo',
+            'gettext_translations/fr_FR/LC_MESSAGES/tornado_test.po',
+            'options_test.cfg',
+            'options_test_types.cfg',
+            'options_test_types_str.cfg',
+            'static/robots.txt',
+            'static/sample.xml',
+            'static/sample.xml.gz',
+            'static/sample.xml.bz2',
+            'static/dir/index.html',
+            'static_foo.txt',
+            'templates/utf8.html',
+            'test.crt',
+            'test.key',
+        ],
+    },
+    author='Facebook',
+    author_email='python-tornado@googlegroups.com',
+    url='http://www.tornadoweb.org/',
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    description=('Tornado is a Python web framework and asynchronous networking library,'
+                 ' originally developed at FriendFeed.'),
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    **kwargs
+)
