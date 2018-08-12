33db4479390bb9707bf11a49e8508d62de0734c1
SUNsung <weatherius@gmail.com>
2018-08-12 15:23:51 +0300

    Rewrite [080GMQN5CTkc08yLvSx7Qw46E080QYae2Y7Znk08HCV.rb] File in: 2018-08-12 15:23:50

--- ruby/080GMQN5CTkc08yLvSx7Qw46E080QYae2Y7Znk08HCV.rb
+++ ruby/080GMQN5CTkc08yLvSx7Qw46E080QYae2Y7Znk08HCV.rb
@@ -1,102 +1,53 @@
 
         
-          def test_should_merge_errors_on_the_associated_models_onto_the_parent_even_if_it_is_not_valid
-    @pirate.ship.name   = nil
-    @pirate.catchphrase = nil
-    assert_predicate @pirate, :invalid?
-    assert_predicate @pirate.errors[:'ship.name'], :any?
-    assert_predicate @pirate.errors[:catchphrase], :any?
+          private
+    
+      test 'broadcasting_for with an object' do
+    assert_equal 'Room#1-Campfire', ChatChannel.broadcasting_for(Room.new(1))
   end
     
-      before_destroy :overwrite_to_raise
+      test 'processing disconnect message' do
+    run_in_eventmachine do
+      open_connection
     
-      validate :check_empty_title
-  validate :check_content_mismatch, on: :create
-  validate :check_wrong_update, on: :update
-  validate :check_author_name_is_secret, on: :special_case
-    
-        private
-      def ping
-      end
-  end
-    
-        def connect
-      @connected = true
-    end
-    
-      def save_and_refresh_staff_groups!
-    transaction do
-      self.save!
-      Group.refresh_automatic_groups!(:admins, :moderators, :staff)
-    end
-  end
-    
-      def self.fragment_cache
-    @cache ||= DistributedCache.new('am_serializer_fragment_cache')
-  end
-    
-        def entries_as_json
-      @entries.sort! { |a, b| sort_fn(a.name, b.name) }.map(&:as_json)
-    end
-    
-            hash.each do |key, value|
-          self[convert_key(key)] = value
-        end
+      describe '.create' do
+    it 'creates a key with the client' do
+      expected_service_configs = {
+        'U27F4V844T' => [],
+        'DQ8HTZ7739' => [],
+        '6A7HVUVQ3M' => ['some-music-id']
+      }
+      mock_client_response(:create_key!, with: { name: 'New Key', service_configs: expected_service_configs }) do
+        {
+          keyId: 'a-new-key-id'
+        }
       end
     
-            def each_unnecessary_space_match(node, &blk)
-          each_match_range(
-            contents_range(node),
-            MULTIPLE_SPACES_BETWEEN_ITEMS_REGEX,
-            &blk
-          )
-        end
+          it 'splits correctly' do
+        expected = ['One', 'Two', 'Three', 'Four Token']
+        expect(generator.split_keywords(keywords)).to eq(expected)
       end
     end
-  end
-end
-
     
-    module RuboCop
-  module Cop
-    module Lint
-      # Don't omit the accumulator when calling `next` in a `reduce` block.
-      #
-      # @example
-      #
-      #   # bad
-      #
-      #   result = (1..4).reduce(0) do |acc, i|
-      #     next if i.odd?
-      #     acc + i
-      #   end
-      #
-      # @example
-      #
-      #   # good
-      #
-      #   result = (1..4).reduce(0) do |acc, i|
-      #     next acc if i.odd?
-      #     acc + i
-      #   end
-      class NextWithoutAccumulator < Cop
-        MSG = 'Use `next` with an accumulator argument in a `reduce`.'.freeze
+          # Run a certain action
+      def trigger(task: nil, flags: nil, serial: nil, print_command: true, print_command_output: true)
+        android_serial = (serial != '') ? 'ANDROID_SERIAL=#{serial}' : nil
+        command = [android_serial, escaped_gradle_path, task, flags].compact.join(' ')
+        Action.sh(command, print_command: print_command, print_command_output: print_command_output)
+      end
     
-            # @param annotated_source [String] string passed to the matchers
-        #
-        # Separates annotation lines from source lines. Tracks the real
-        # source line number that each annotation corresponds to.
-        #
-        # @return [AnnotatedSource]
-        def self.parse(annotated_source)
-          source      = []
-          annotations = []
+          it 'does set the exclude directories' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            cloc(exclude_dir: 'test1,test2,build')
+          end').runner.execute(:test)
     
-          context 'source without blank lines' do
-        let(:source) do
-          <<-RUBY.strip_indent
-            #{type} SomeObject
-              def do_something; end
-            end
-          RUBY
-        end
+          it 'automatically removes new lines from the version number' do
+        Fastlane::FastFile.new.parse('lane :test do
+          increment_version_number(version_number: '1.77.3\n', bump_type: 'major')
+        end').runner.execute(:test)
+    
+        if GitHub.api_credentials_type == :none
+      puts <<~EOS
+        You can create a new personal access token:
+         #{GitHub::ALL_SCOPES_URL}
+        and then set the new HOMEBREW_GITHUB_API_TOKEN as the authentication method.
