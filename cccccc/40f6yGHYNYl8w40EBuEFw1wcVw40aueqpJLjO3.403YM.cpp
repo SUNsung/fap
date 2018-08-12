9ea0d16acb7812aabb927207e5225e0beb10174a
SUNsung <weatherius@gmail.com>
2018-08-12 04:13:50 +0300

    Rewrite [40f6yGHYNYl8w40EBuEFw1wcVw40aueqpJLjO3.403YM.cpp] File in: 2018-08-12 04:13:50

--- cccccc/40f6yGHYNYl8w40EBuEFw1wcVw40aueqpJLjO3.403YM.cpp
+++ cccccc/40f6yGHYNYl8w40EBuEFw1wcVw40aueqpJLjO3.403YM.cpp
@@ -1,251 +1,117 @@
 
         
-        #ifndef TENSORFLOW_CORE_DISTRIBUTED_RUNTIME_WORKER_CACHE_PARTIAL_H_
-#define TENSORFLOW_CORE_DISTRIBUTED_RUNTIME_WORKER_CACHE_PARTIAL_H_
+        #include 'content/nw/src/api/menu/menu_delegate.h'
     
-    namespace tensorflow {
-namespace functor {
-// TODO(b/32239807) No GPU ops for mod yet.
-}  // namespace functor
-}  // namespace tensorflow
-    
-      int64* scratch = scratch_holder.data();
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-#include <gtest/gtest.h>
-#include 'tensorflow/contrib/lite/interpreter.h'
-#include 'tensorflow/contrib/lite/kernels/register.h'
-#include 'tensorflow/contrib/lite/kernels/test_util.h'
-#include 'tensorflow/contrib/lite/model.h'
-    
-    #include 'tensorflow/core/kernels/loss.h'
-#include 'tensorflow/core/lib/core/errors.h'
-#include 'tensorflow/core/lib/core/status.h'
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    #include <memory>
-#include 'tensorflow/core/framework/reader_base.h'
-#include 'tensorflow/core/framework/reader_op_kernel.h'
-#include 'tensorflow/core/lib/core/errors.h'
-#include 'tensorflow/core/lib/io/inputbuffer.h'
-#include 'tensorflow/core/lib/strings/strcat.h'
-#include 'tensorflow/core/platform/env.h'
-    
-        string compression_type;
-    OP_REQUIRES_OK(context,
-                   context->GetAttr('compression_type', &compression_type));
-    
-    namespace tensorflow {
+    void Menu::Popup(int x, int y, content::Shell* shell) {
+  GdkEventButton* event = NULL; //FIXME: shell->web_contents()->GetRenderWidgetHostView()->GetLastMouseDown();
+  uint32_t triggering_event_time = event ? event->time : GDK_CURRENT_TIME;
+  gfx::Point point;
+  if (!event) {
+    // gfx::Rect bounds = shell->web_contents()->GetRenderWidgetHostView()->GetViewBounds();
+    // point = gfx::Point(x + bounds.x(), y + bounds.y());
+    DVLOG(1) << 'no last mouse down event';
+    point = gfx::Point(x, y);
+  }else
+    point = gfx::Point(event->x_root, event->y_root);
     }
     
     
-    {inline void AddTraceEvent(const TraceEvent &event, string *json) {
-  Appendf(json, R'({'pid':%u,'tid':%u,'ts':%.5f,)', event.device_id(),
-          event.resource_id(), event.timestamp_ps() / kPicosPerMicro);
-  AppendEscapedName(json, event.name());
-  StrAppend(json, ',');
-  if (event.duration_ps() > 0) {
-    Appendf(json, R'('ph':'X','dur':%.5f},)',
-            event.duration_ps() / kPicosPerMicro);
+    {}  // namespace nw
+
+    
+      if (type == 'separator') {
+    menu_item_ = gtk_separator_menu_item_new();
   } else {
-    StrAppend(json, R'('ph':'i','s':'t'},)');
-  }
-}
-    
-    
-    {
-    {    if (SemanticNode.is<Expr *>()) {
-      SemanticNode.get<Expr *>()->dump(llvm::errs());
-    } else if (SemanticNode.is<Decl *>()) {
-      SemanticNode.get<Decl *>()->dump(llvm::errs());
-    } else if (SemanticNode.is<Expr *>()) {
-      SemanticNode.get<Expr *>()->dump(llvm::errs());
+    if (type == 'checkbox') {
+      menu_item_ = gtk_check_menu_item_new();
+      bool checked;
+      if (option.GetBoolean('checked', &checked))
+        SetChecked(checked);
     } else {
-      llvm_unreachable('ASTNode has pointer to unknown thing!');
+      menu_item_ = gtk_image_menu_item_new();
+      std::string icon;
+      if (option.GetString('icon', &icon))
+        SetIcon(icon);
     }
-    llvm::errs() << '\n=====================================================\n';
-  }
-}
-
-    
-      ArrayRef<MarkupASTNode *> getChildren() {
-    return {getTrailingObjects<MarkupASTNode *>(), NumChildren};
-  }
-    
-    #include 'llvm/Support/raw_ostream.h'
-    
-    #endif // SWIFT_SYNTAX_REFERENCES_H
-
-    
-    // On macOS and iOS, swift_once is implemented using GCD.
-// The compiler emits an inline check matching the barrier-free inline fast
-// path of dispatch_once(). See SwiftTargetInfo.OnceDonePredicateValue.
-    
-      virtual bool recordHash(StringRef hash, bool isKnown) = 0;
-  virtual bool startDependency(StringRef name, StringRef path, bool isClangModule,
-                               bool isSystem, StringRef hash) = 0;
-  virtual bool finishDependency(bool isClangModule) = 0;
-  virtual Action startSourceEntity(const IndexSymbol &symbol) = 0;
-  virtual bool finishSourceEntity(SymbolInfo symInfo, SymbolRoleSet roles) = 0;
-    
-      /// True if this is the active clause of the #if block.
-  bool isActive;
-    
-    // Get basic type definitions.
-#define IPC_MESSAGE_IMPL
-#include 'content/nw/src/common/common_message_generator.h'
-    
-    Menu::~Menu() {
-  Destroy();
-}
-    
-     protected:
-  ~NwAppCrashBrowserFunction() override {}
-    
-    
-    {}
-    
-    /**
- * @brief Iterate the discovered decorators for a given point type.
- *
- * The configuration maintains various sources, each may contain a set of
- * decorators. The source tracking is abstracted for the decorator iterator.
- *
- * @param point request execution of decorators for this given point.
- * @param time an optional time for points using intervals.
- * @param source restrict run to a specific config source.
- */
-void runDecorators(DecorationPoint point,
-                   size_t time = 0,
-                   const std::string& source = '');
-    
-    class TLSConfigPlugin : public ConfigPlugin,
-                        public std::enable_shared_from_this<TLSConfigPlugin> {
- public:
-  Status setUp() override;
-  Status genConfig(std::map<std::string, std::string>& config) override;
     }
     
-    #ifdef WIN32
-  pid = (int)::GetCurrentProcessId();
-#else
-  pid = getpid();
-#endif
-    
-    /**
- * @brief Run an interactive SQL query shell.
- *
- * @code{.cpp}
- *   // Copyright 2004-present Facebook. All Rights Reserved.
- *   #include <osquery/core.h>
- *   #include <osquery/devtools.h>
- *
- *   int main(int argc, char *argv[]) {
- *     osquery::initOsquery(argc, argv);
- *     return osquery::launchIntoShell(argc, argv);
- *   }
- * @endcode
- *
- * @param argc the number of elements in argv
- * @param argv the command-line flags
- *
- * @return an int which represents the 'return code'
- */
-int launchIntoShell(int argc, char** argv);
-    
-    class PrinterTests : public testing::Test {
+    class NwMenuGetNSStringWithFixupFunction : public NWSyncExtensionFunction {
  public:
-  QueryData q;
-  std::vector<std::string> order;
-  void SetUp() {
-    order = {'name', 'age', 'food', 'number'};
-    q = {
-        {
-         {'name', 'Mike Jones'},
-         {'age', '39'},
-         {'food', 'mac and cheese'},
-         {'number', '1'},
-        },
-        {
-         {'name', 'John Smith'},
-         {'age', '44'},
-         {'food', 'peanut butter and jelly'},
-         {'number', '2'},
-        },
-        {
-         {'name', 'Doctor Who'},
-         {'age', '2000'},
-         {'food', 'fish sticks and custard'},
-         {'number', '11'},
-        },
-    };
-  }
+  NwMenuGetNSStringWithFixupFunction(){}
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    
+ protected:
+  ~NwMenuGetNSStringWithFixupFunction() override {}
+    
+  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringWithFixup', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringWithFixupFunction);
 };
     
     
-    {  // Too many fields
-  bad_line = R'('2016-03-22T21:17:01.701882+00:00','','6','','','','')';
-  ec = pub.createEventContext();
-  status = pub.populateEventContext(bad_line, ec);
-  ASSERT_FALSE(status.ok());
-  ASSERT_NE(std::string::npos, status.getMessage().find('more'));
+    {            ptr[tid] = partial = partial + ptr[tid + 16];
+            ptr[tid] = partial = partial + ptr[tid + 8];
+            ptr[tid] = partial = partial + ptr[tid + 4];
+            ptr[tid] = partial = partial + ptr[tid + 2];
+            ptr[tid] = partial = partial + ptr[tid + 1];
+        }
+    
+    struct DynamicFnEntry
+{
+    const char* fnName; // 'clCreateContext'
+    void** ppFn; // &clCreateContext_pfn
+};
+    
+            static void call(const GpuMat& src, GpuMat& dst, int flipCode, cudaStream_t stream)
+        {
+            NppStreamHandler h(stream);
+    }
+    
+    inline std::size_t buffer_size_helper(const const_buffer& b)
+{
+  return b.size_;
 }
     
-    size_t FirehoseLogForwarder::getMaxBytesPerBatch() const {
-  return 4000000U;
-}
+    #if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
     
-      Status setUp() override;
+    #endif // BOOST_ASIO_BUFFERED_STREAM_FWD_HPP
+
     
-    // Performs in-place floating point 8x8 DCT on block[0..63].
-// Note that the DCT used here is the DCT-2 with the first term multiplied by
-// 1/sqrt(2) and the result scaled by 1/2.
-void ComputeBlockDCTDouble(double block[64]);
+    #endif // BOOST_ASIO_DETAIL_ARRAY_FWD_HPP
+
     
-    #include <stdint.h>
+    #include <boost/asio/detail/config.hpp>
     
-      // Requires that comp is not downsampled.
-  void CopyFromJpegComponent(const JPEGComponent& comp,
-                             int factor_x, int factor_y,
-                             const int* quant);
+    #endif // BOOST_ASIO_DETAIL_FD_SET_ADAPTER_HPP
+
     
-    // Preprocesses the u (1) or v (2) channel of the given YUV image (range 0-255).
-std::vector<std::vector<float>> PreProcessChannel(
-    int w, int h, int channel, float sigma, float amount, bool blur,
-    bool sharpen, const std::vector<std::vector<float>>& image);
+    #ifndef BOOST_ASIO_DETAIL_FUNCTION_HPP
+#define BOOST_ASIO_DETAIL_FUNCTION_HPP
     
-    void SimpleShortPack(const void* _data, size_t _datalen, AutoBuffer& _outbuf) {
-    SimplePack<uint16_t>(_data, _datalen, _outbuf);
-}
+    namespace boost {
+namespace asio {
+namespace detail {
+    }
+    }
+    }
     
-    // Licensed under the MIT License (the 'License'); you may not use this file except in 
-// compliance with the License. You may obtain a copy of the License at
-// http://opensource.org/licenses/MIT
+      explicit winrt_buffer_impl(const boost::asio::mutable_buffer& b)
+  {
+    bytes_ = const_cast<byte*>(boost::asio::buffer_cast<const byte*>(b));
+    length_ = 0;
+    capacity_ = boost::asio::buffer_size(b);
+  }
     
-        Spy* GetSpy(const void* _this) const
-    { return m_thismap.find(_this)->second; }
+    template <typename Time_Traits>
+void epoll_reactor::schedule_timer(timer_queue<Time_Traits>& queue,
+    const typename Time_Traits::time_type& time,
+    typename timer_queue<Time_Traits>::per_timer_data& timer, wait_op* op)
+{
+  mutex::scoped_lock lock(mutex_);
+    }
     
-    
-#define DEFINE_HAS_MEMBER_WITH_TYPE(member_name, member_type) \
-    template <typename T>\
-    class has_##member_name {\
-      private:\
-        struct yes_type { char x[1]; };\
-        struct no_type { char x[2]; };\
-        template <member_type (T::*)> struct tester;\
-        template <typename U> static yes_type test(tester<&U::member_name>*);\
-        template <typename U> static no_type test(...);\
-      public:\
-        static const bool value = (sizeof(test<T>(0)) == sizeof(yes_type));\
-    };
-    
-    #ifndef _COMM_FUNCTION_H_
-#define _COMM_FUNCTION_H_
+     protected:
+  /// Calculate the URL once and cache the result.
+  std::string uri_;
