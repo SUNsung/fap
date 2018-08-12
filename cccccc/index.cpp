3dd3abc3897a78e7ca0ab13919c04c36f4900744
SUNsung <weatherius@gmail.com>
2018-08-12 10:32:20 +0300

    Rewrite [index.cpp] File in: 2018-08-12 10:32:19

--- cccccc/index.cpp
+++ cccccc/index.cpp
@@ -1,239 +1,217 @@
 
         
-        double median(const Mat& row);
-    
-    //! @cond IGNORED
-    
-    const uchar g_Saturate8u[] =
-{
-      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-      0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,  15,
-     16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,
-     32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,
-     48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,
-     64,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,
-     80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  94,  95,
-     96,  97,  98,  99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111,
-    112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127,
-    128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143,
-    144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159,
-    160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175,
-    176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191,
-    192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207,
-    208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223,
-    224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239,
-    240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255,
-    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-    255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-    255
-};
-    
-      for (auto s: tensor->sizes())
-    assert(s == 2);
-  for (int i = 0; i < 2; i++)
-    assert(reinterpret_cast<float*>(tensor->data())[i] == 5);
+        #include 'atom/browser/net/asar/url_request_asar_job.h'
+#include 'atom/browser/net/js_asker.h'
     
     
-    {  if (src->size[dimension] == 1 && src->nDimension > 1) {
-    for (size_t d = dimension; d < self->nDimension-1; d++) {
-      self->size[d] = self->size[d+1];
-      self->stride[d] = self->stride[d+1];
-    }
-    self->nDimension--;
-  }
-}
+    {}  // namespace atom
     
-    PyObject *THPDevice_New(const at::Device& device)
-{
-  auto type = (PyTypeObject*)&THPDeviceType;
-  auto self = THPObjectPtr{type->tp_alloc(type, 0)};
-  if (!self) throw python_error();
-  auto self_ = reinterpret_cast<THPDevice*>(self.get());
-  self_->device = device;
-  return self.release();
-}
-    
-    static bool backCompatKeepdimWarn = false;
-    
-      CAFFE_ENFORCE_EQ(heatmaps_in.ndim(), 4);
-  const int N = heatmaps_in.dim32(0);
-  CAFFE_ENFORCE_EQ(heatmaps_in.dim32(0), N);
-  const int keypoint_count = heatmaps_in.dim32(1);
-  const int heatmap_size = heatmaps_in.dim32(2);
-  CAFFE_ENFORCE_GE(heatmap_size, 2); // at least 2x2 for approx
-  CAFFE_ENFORCE_EQ(heatmaps_in.dim32(2), heatmaps_in.dim32(3));
-    
-    #ifndef CAFFE2_OPERATORS_BATCH_SPARSE_TO_DENSE_OP_H_
-#define CAFFE2_OPERATORS_BATCH_SPARSE_TO_DENSE_OP_H_
-    
-    template <typename T, class Context>
-class BBoxTransformOp final : public Operator<Context> {
+    // Only lock when lockers are used in current thread.
+class Locker {
  public:
-  BBoxTransformOp(const OperatorDef& operator_def, Workspace* ws)
-      : Operator<Context>(operator_def, ws),
-        weights_(OperatorBase::GetRepeatedArgument<T>(
-            'weights',
-            vector<T>{1.0f, 1.0f, 1.0f, 1.0f})),
-        apply_scale_(
-            OperatorBase::GetSingleArgument<bool>('apply_scale', true)),
-        correct_transform_coords_(OperatorBase::GetSingleArgument<bool>(
-            'correct_transform_coords',
-            false)),
-        rotated_(OperatorBase::GetSingleArgument<bool>('rotated', false)),
-        angle_bound_on_(
-            OperatorBase::GetSingleArgument<bool>('angle_bound_on', true)),
-        angle_bound_lo_(
-            OperatorBase::GetSingleArgument<int>('angle_bound_lo', -90)),
-        angle_bound_hi_(
-            OperatorBase::GetSingleArgument<int>('angle_bound_hi', 90)),
-        clip_angle_thresh_(
-            OperatorBase::GetSingleArgument<float>('clip_angle_thresh', 1.0)) {
-    CAFFE_ENFORCE_EQ(
-        weights_.size(),
-        4,
-        'weights size ' + caffe2::to_string(weights_.size()) + 'must be 4.');
+  explicit Locker(v8::Isolate* isolate);
+  ~Locker();
+    }
+    
+    #ifndef CHROME_BROWSER_PRINTING_PRINTING_UI_WEB_CONTENTS_OBSERVER_H_
+#define CHROME_BROWSER_PRINTING_PRINTING_UI_WEB_CONTENTS_OBSERVER_H_
+    
+    // File name of the Pepper Flash plugin on different platforms.
+const base::FilePath::CharType kPepperFlashPluginFilename[] =
+#if defined(OS_MACOSX)
+    FPL('PepperFlashPlayer.plugin');
+#elif defined(OS_WIN)
+    FPL('pepflashplayer.dll');
+#else  // OS_LINUX, etc.
+    FPL('libpepflashplayer.so');
+#endif
+    
+    // filenames
+extern const base::FilePath::CharType kCacheDirname[];
+extern const base::FilePath::CharType kChannelIDFilename[];
+extern const base::FilePath::CharType kCookieFilename[];
+extern const base::FilePath::CharType kCRLSetFilename[];
+extern const base::FilePath::CharType kCustomDictionaryFileName[];
+extern const base::FilePath::CharType kExtensionActivityLogFilename[];
+extern const base::FilePath::CharType kExtensionsCookieFilename[];
+extern const base::FilePath::CharType kFirstRunSentinel[];
+extern const base::FilePath::CharType kGCMStoreDirname[];
+extern const base::FilePath::CharType kLocalStateFilename[];
+extern const base::FilePath::CharType kLocalStorePoolName[];
+extern const base::FilePath::CharType kMediaCacheDirname[];
+extern const base::FilePath::CharType kNetworkPersistentStateFilename[];
+extern const base::FilePath::CharType kOfflinePageArchviesDirname[];
+extern const base::FilePath::CharType kOfflinePageMetadataDirname[];
+extern const base::FilePath::CharType kPreferencesFilename[];
+extern const base::FilePath::CharType kProtectedPreferencesFilenameDeprecated[];
+extern const base::FilePath::CharType kReadmeFilename[];
+extern const base::FilePath::CharType kResetPromptMementoFilename[];
+extern const base::FilePath::CharType kSafeBrowsingBaseFilename[];
+extern const base::FilePath::CharType kSecurePreferencesFilename[];
+extern const base::FilePath::CharType kServiceStateFileName[];
+extern const base::FilePath::CharType kSingletonCookieFilename[];
+extern const base::FilePath::CharType kSingletonLockFilename[];
+extern const base::FilePath::CharType kSingletonSocketFilename[];
+extern const base::FilePath::CharType kSupervisedUserSettingsFilename[];
+extern const base::FilePath::CharType kThemePackFilename[];
+extern const base::FilePath::CharType kThemePackMaterialDesignFilename[];
+extern const base::FilePath::CharType kWebAppDirname[];
+    
+      // This returns true if all components are specified, and min and max are
+  // equal.
+  bool HasFixedSize() const;
+    
+    namespace api {
+    }
+    
+    namespace atom {
+    }
+    
+    using namespace swift;
+using namespace swift::syntax;
+    
+    @interface LocaleBridgingTester : NSObject
+- (NSLocale *)autoupdatingCurrentLocale;
+- (BOOL)verifyAutoupdatingLocale:(NSLocale *)locale;
+@end
+    
+    
+    {/// Extract comments parts from the given Markup node.
+swift::markup::CommentParts
+extractCommentParts(swift::markup::MarkupContext &MC,
+                    swift::markup::MarkupASTNode *Node);
+} // namespace swift
+    
+    /// Format a Syntax tree with the given rules.
+Syntax format(Syntax Tree);
+// TODO: Representation for formatting rules, etc. This is just a figment
+// for now.
+    
+    #endif // SWIFT_SYNTAX_REFERENCES_H
+
+    
+    // On macOS and iOS, swift_once is implemented using GCD.
+// The compiler emits an inline check matching the barrier-free inline fast
+// path of dispatch_once(). See SwiftTargetInfo.OnceDonePredicateValue.
+    
+    void indexDeclContext(DeclContext *DC, IndexDataConsumer &consumer);
+void indexSourceFile(SourceFile *SF, StringRef hash,
+                     IndexDataConsumer &consumer);
+void indexModule(ModuleDecl *module, StringRef hash,
+                 IndexDataConsumer &consumer);
+    
+      bool isRoot() const { return Index == RootIdx; }
+    
+    class Context {
+  std::string RuntimeLibPath;
+  std::unique_ptr<LangSupport> SwiftLang;
+  std::unique_ptr<NotificationCenter> NotificationCtr;
+    }
+    
+    class MemTable;
+    
+    
+    {      // Ok
+      break;
+    case kSnappyCompression: {
+      size_t ulength = 0;
+      if (!port::Snappy_GetUncompressedLength(data, n, &ulength)) {
+        delete[] buf;
+        return Status::Corruption('corrupted compressed block contents');
+      }
+      char* ubuf = new char[ulength];
+      if (!port::Snappy_Uncompress(data, n, ubuf)) {
+        delete[] buf;
+        delete[] ubuf;
+        return Status::Corruption('corrupted compressed block contents');
+      }
+      delete[] buf;
+      result->data = Slice(ubuf, ulength);
+      result->heap_allocated = true;
+      result->cachable = true;
+      break;
+    }
+    default:
+      delete[] buf;
+      return Status::Corruption('bad block type');
   }
-  USE_OPERATOR_CONTEXT_FUNCTIONS;
-    }
     
-      // Generate bounding box proposals for a given image
-  // im_info: [height, width, im_scale]
-  // all_anchors: (H * W * A, 4)
-  // bbox_deltas_tensor: (4 * A, H, W)
-  // scores_tensor: (A, H, W)
-  // out_boxes: (n, 5)
-  // out_probs: n
-  void ProposalsForOneImage(
-      const Eigen::Array3f& im_info,
-      const Eigen::Map<const ERMatXf>& all_anchors,
-      const utils::ConstTensorView<float>& bbox_deltas_tensor,
-      const utils::ConstTensorView<float>& scores_tensor,
-      ERArrXXf* out_boxes,
-      EArrXf* out_probs) const;
-    
-      EMatXf result_gt(5, 5);
-  result_gt << 252.668, 107.367, 91.4381, 276.165, 0.0, 217.686, 19.3551,
-      147.631, 205.397, 55.0, 187.363, 214.185, 19.865, 31.0368, 100.0, 270.234,
-      210.513, 235.963, 130.163, -50.0, 36.1956, 140.863, 62.2665, 259.645,
-      180.5;
-    
-    
-    
-    
-    
-    
-    
-    
-    {    p2 = p1 + k_axisScale * xf.q.GetYAxis();
-    DrawSegment(p1,p2,b2Color(0,1,0));
-}
-    
-        virtual void DrawSolidCircle(const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& color);
-    
-    		case 'a':
-			{
-				m_body->ApplyTorque(50.0f, true);
-			}
-			break;
-    
-    
-    {	bool m_broke;
-	bool m_break;
-};
-    
-    	Bridge()
-	{
-		b2Body* ground = NULL;
-		{
-			b2BodyDef bd;
-			ground = m_world->CreateBody(&bd);
-    }
-    }
-    
-      // Compares img with the baseline image and saves the resulting distance map
-  // inside the object. The provided image must have the same dimensions as the
-  // baseline image.
-  virtual void Compare(const OutputImage& img) = 0;
-    
-    // TODO(user) Use SRGB->linear conversion and a lookup-table.
-inline float GammaToLinear(float x) {
-  return static_cast<float>(std::pow(x / 255.0f, 2.2));
-}
-    
-    static const int kCbToBlueTable[256] = {
-  -227, -225, -223, -222, -220, -218, -216, -214, -213, -211, -209, -207,
-  -206, -204, -202, -200, -198, -197, -195, -193, -191, -190, -188, -186,
-  -184, -183, -181, -179, -177, -175, -174, -172, -170, -168, -167, -165,
-  -163, -161, -159, -158, -156, -154, -152, -151, -149, -147, -145, -144,
-  -142, -140, -138, -136, -135, -133, -131, -129, -128, -126, -124, -122,
-  -120, -119, -117, -115, -113, -112, -110, -108, -106, -105, -103, -101,
-   -99,  -97,  -96,  -94,  -92,  -90,  -89,  -87,  -85,  -83,  -82,  -80,
-   -78,  -76,  -74,  -73,  -71,  -69,  -67,  -66,  -64,  -62,  -60,  -58,
-   -57,  -55,  -53,  -51,  -50,  -48,  -46,  -44,  -43,  -41,  -39,  -37,
-   -35,  -34,  -32,  -30,  -28,  -27,  -25,  -23,  -21,  -19,  -18,  -16,
-   -14,  -12,  -11,   -9,   -7,   -5,   -4,   -2,    0,    2,    4,    5,
-     7,    9,   11,   12,   14,   16,   18,   19,   21,   23,   25,   27,
-    28,   30,   32,   34,   35,   37,   39,   41,   43,   44,   46,   48,
-    50,   51,   53,   55,   57,   58,   60,   62,   64,   66,   67,   69,
-    71,   73,   74,   76,   78,   80,   82,   83,   85,   87,   89,   90,
-    92,   94,   96,   97,   99,  101,  103,  105,  106,  108,  110,  112,
-   113,  115,  117,  119,  120,  122,  124,  126,  128,  129,  131,  133,
-   135,  136,  138,  140,  142,  144,  145,  147,  149,  151,  152,  154,
-   156,  158,  159,  161,  163,  165,  167,  168,  170,  172,  174,  175,
-   177,  179,  181,  183,  184,  186,  188,  190,  191,  193,  195,  197,
-   198,  200,  202,  204,  206,  207,  209,  211,  213,  214,  216,  218,
-   220,  222,  223,  225,
-};
-    
-    namespace guetzli {
-    }
-    
-    std::vector<uint8_t> DecodeJpegToRGB(const JPEGData& jpg) {
-  if (jpg.components.size() == 1 ||
-      (jpg.components.size() == 3 &&
-       HasYCbCrColorSpace(jpg) && (jpg.Is420() || jpg.Is444()))) {
-    OutputImage img(jpg.width, jpg.height);
-    img.CopyFromJpegData(jpg);
-    return img.ToSRGB();
+      AutoCompactTest() {
+    dbname_ = test::TmpDir() + '/autocompact_test';
+    tiny_cache_ = NewLRUCache(100);
+    options_.block_cache = tiny_cache_;
+    DestroyDB(dbname_, options_);
+    options_.create_if_missing = true;
+    options_.compression = kNoCompression;
+    ASSERT_OK(DB::Open(options_, dbname_, &db_));
   }
-  return std::vector<uint8_t>();
+    
+    inline bool DBIter::ParseKey(ParsedInternalKey* ikey) {
+  Slice k = iter_->key();
+  ssize_t n = k.size() + iter_->value().size();
+  bytes_counter_ -= n;
+  while (bytes_counter_ < 0) {
+    bytes_counter_ += RandomPeriod();
+    db_->RecordReadSample(k);
+  }
+  if (!ParseInternalKey(k, ikey)) {
+    status_ = Status::Corruption('corrupted internal key in DBIter');
+    return false;
+  } else {
+    return true;
+  }
 }
     
+    bool HandleDumpCommand(Env* env, char** files, int num) {
+  StdoutPrinter printer;
+  bool ok = true;
+  for (int i = 0; i < num; i++) {
+    Status s = DumpFile(env, files[i], &printer);
+    if (!s.ok()) {
+      fprintf(stderr, '%s\n', s.ToString().c_str());
+      ok = false;
+    }
+  }
+  return ok;
+}
     
-    {}  // namespace guetzli
+      // Fragment the record if necessary and emit it.  Note that if slice
+  // is empty, we still want to iterate once to emit a single
+  // zero-length record
+  Status s;
+  bool begin = true;
+  do {
+    const int leftover = kBlockSize - block_offset_;
+    assert(leftover >= 0);
+    if (leftover < kHeaderSize) {
+      // Switch to a new block
+      if (leftover > 0) {
+        // Fill the trailer (literal below relies on kHeaderSize being 7)
+        assert(kHeaderSize == 7);
+        dest_->Append(Slice('\x00\x00\x00\x00\x00\x00', leftover));
+      }
+      block_offset_ = 0;
+    }
+    }
     
-    #include <assert.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+    extern JSClass  *jsb_cocos2d_NavMesh_class;
+extern JSObject *jsb_cocos2d_NavMesh_prototype;
     
-      void ToLinearRGB(std::vector<std::vector<float> >* rgb) const;
+    #if COCOS2D_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,'#ferror in function 'lua_cocos2dx_cocosdenshion_SimpleAudioEngine_isBackgroundMusicPlaying'.',&tolua_err);
+#endif
+    
+    #endif // __cocos2dx_csloader_h__
+
+    
+    
+    
+    
+    
+    			b2PolygonShape shape;
+			shape.SetAsBox(0.75f, 0.75f);
+    
+    			b2FixtureDef fd;
+			fd.shape = &shape;
+			fd.density = 1.0f;
