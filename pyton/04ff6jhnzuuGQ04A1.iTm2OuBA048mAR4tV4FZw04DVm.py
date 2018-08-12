126bb660ca1f39334bdeae80ce19ccd07815374b
SUNsung <weatherius@gmail.com>
2018-08-13 01:55:59 +0300

    Rewrite [04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py] File in: 2018-08-13 01:55:59

--- pyton/04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py
+++ pyton/04ff6jhnzuuGQ04A1.iTm2OuBA048mAR4tV4FZw04DVm.py
@@ -1,214 +1,225 @@
 
         
+        - BaseRecognizer: Base class with common recognizer functionality.
+- Lexer: Base class for lexers.
+- Parser: Base class for parsers.
+- tree.TreeParser: Base class for %tree parser.
+    
+    
+    def getErrorMessage(self, e, tokenNames):
+        '''
+        What error message should be generated for the various
+        exception types?
         
-def run_training_step(sess, trainer, train_corpus, batch_size):
-  '''Runs a single iteration of train_op on a randomly sampled batch.'''
-  batch = random.sample(train_corpus, batch_size)
-  sess.run(trainer['run'], feed_dict={trainer['input_batch']: batch})
+        Not very object-oriented code, but I like having all error message
+        generation within one method rather than spread among all of the
+        exception classes. This also makes it much easier for the exception
+        handling because the exception classes do not have to have pointers back
+        to this object to access utility routines and so on. Also, changing
+        the message for an exception type would be difficult because you
+        would have to subclassing exception, but then somehow get ANTLR
+        to make those kinds of exception objects instead of the default.
+        This looks weird, but trust me--it makes the most sense in terms
+        of flexibility.
     
-    # This should include just about everything, suitable for Jupyter notebooks or
-# building a pip package.
-py_library(
-    name = 'all_in_one_components',
-    deps = [
-        ':components',
-        '//dragnn/python:components',
-        '//dragnn/python:dragnn_ops',
-        '//dragnn/python:evaluation',
-        '//dragnn/python:lexicon',
-        '//dragnn/python:render_parse_tree_graphviz',
-        '//dragnn/python:render_spec_with_graphviz',
-        '//dragnn/python:spec_builder',
-        '//dragnn/python:trainer_lib',
-        '//dragnn/python:visualization',
-        '//syntaxnet:task_spec_pb2_py',
-    ],
-)
-
+        def LT(self, i):
+        if i == 0:
+            return 0 # undefined
     
-      Args:
-    master_spec_path: Path to a proto-text master spec.
-    params_path: Path to the parameters file to export.
-    resource_path: Path to resources in the master spec.
-    export_path: Path to export the SavedModel to.
-    export_moving_averages: Whether to export the moving average parameters.
-  '''
-  # Old CoNLL checkpoints did not need a known-word-map. Create a temporary if
-  # that file is missing.
-  if not tf.gfile.Exists(os.path.join(resource_path, 'known-word-map')):
-    with tf.gfile.FastGFile(os.path.join(resource_path, 'known-word-map'),
-                            'w') as out_file:
-      out_file.write('This file intentionally left blank.')
+        def setText(self, text):
+        self.text = text
     
-        if FLAGS.output_file:
-      with gfile.GFile(FLAGS.output_file, 'w') as f:
-        for serialized_sentence in processed:
-          sentence = sentence_pb2.Sentence()
-          sentence.ParseFromString(serialized_sentence)
-          f.write(text_format.MessageToString(sentence) + '\n\n')
+                                s = self.eot[s]
+                            input.consume()
+                            # TODO: I had this as return accept[eot[s]]
+                            # which assumed here that the EOT edge always
+                            # went to an accept...faster to do this, but
+                            # what about predicated edges coming from EOT
+                            # target?
+                            continue
     
-    flags.DEFINE_integer('num_epochs', 10, 'Number of epochs to train for.')
-flags.DEFINE_integer('batch_size', 4, 'Batch size.')
-flags.DEFINE_integer('report_every', 500,
-                     'Report cost and training accuracy every this many steps.')
-flags.DEFINE_string('hyperparams',
-                    'decay_steps:32000 dropout_rate:0.8 gradient_clip_norm:1 '
-                    'learning_method:'momentum' learning_rate:0.1 seed:1 '
-                    'momentum:0.95 use_moving_average:true',
-                    'Hyperparameters of the model to train, either in ProtoBuf'
-                    'text format or base64-encoded ProtoBuf text format.')
-    
-    import pytest
-from requests.compat import urljoin
-    
-    Provides utility functions that are consumed internally by Requests
-which depend on extremely few external helpers (such as compat)
-'''
-    
-    from requests.help import info
-    
-        def test_response_context_manager(self, httpbin):
-        with requests.get(httpbin('stream/4'), stream=True) as response:
-            assert isinstance(response, requests.Response)
+        def _close_self_pipe(self):
+        if self._self_reading_future is not None:
+            self._self_reading_future.cancel()
+            self._self_reading_future = None
+        self._ssock.close()
+        self._ssock = None
+        self._csock.close()
+        self._csock = None
+        self._internal_fds -= 1
     
     
-@pytest.mark.parametrize(
-    'url, expected', (
-            ('http://192.168.0.1:5000/', True),
-            ('http://192.168.0.1/', True),
-            ('http://172.16.1.1/', True),
-            ('http://172.16.1.1:5000/', True),
-            ('http://localhost.localdomain:5000/v1.0/', True),
-            ('http://google.com:6000/', True),
-            ('http://172.16.1.12/', False),
-            ('http://172.16.1.12:5000/', False),
-            ('http://google.com:5000/v1.0/', False),
-    ))
-def test_should_bypass_proxies(url, expected, monkeypatch):
-    '''Tests for function should_bypass_proxies to check if proxy
-    can be bypassed or not
-    '''
-    monkeypatch.setenv('no_proxy', '192.168.0.0/24,127.0.0.1,localhost.localdomain,172.16.1.1, google.com:6000')
-    monkeypatch.setenv('NO_PROXY', '192.168.0.0/24,127.0.0.1,localhost.localdomain,172.16.1.1, google.com:6000')
-    assert should_bypass_proxies(url, no_proxy=None) == expected
+def parse_args():
+    p = argparse.ArgumentParser()
+    p.add_argument('-v', '--verbose', action='store_true')
+    p.add_argument('-b', '--binary', action='store_true',
+                   help='Is the dependency in the binary repo?')
+    p.add_argument('-O', '--organization',
+                   help='Organization owning the deps repos', default='python')
+    p.add_argument('-e', '--externals-dir', type=pathlib.Path,
+                   help='Directory in which to store dependencies',
+                   default=pathlib.Path(__file__).parent.parent / 'externals')
+    p.add_argument('tag',
+                   help='tag of the dependency')
+    return p.parse_args()
     
-            # Verify we haven't overwritten the location with our previous fragment.
-        assert r.history[1].request.url == 'http://{0}:{1}/get#relevant-section'.format(host, port)
-        # Verify previous fragment is used and not the original.
-        assert r.url == 'http://{0}:{1}/final-url/#relevant-section'.format(host, port)
-    
-        def build_digest_header(self, method, url):
-        '''
-        :rtype: str
-        '''
-    
-    # ---------
-# Specifics
-# ---------
-    
-            .. seealso:: itervalues() and iteritems().
-        '''
-        for cookie in iter(self):
-            yield cookie.name
-    
-    
-def _check_cryptography(cryptography_version):
-    # cryptography < 1.3.4
+        print('Creating the makefiles...')
+    sys.stdout.flush()
+    # run configure, copy includes, patch files
+    run_configure(configure, do_script)
+    makefile = makefile_template.format(suffix)
     try:
-        cryptography_version = list(map(int, cryptography_version.split('.')))
-    except ValueError:
-        return
+        os.unlink(makefile)
+    except FileNotFoundError:
+        pass
+    os.rename(generated_makefile, makefile)
+    copy_includes(makefile, suffix)
     
-        def get(self, key):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                return item.value
-        raise KeyError('Key not found')
+        # Print the location of each face in this image
+    top, right, bottom, left = face_location
+    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
     
-        def __init__(self):
-        self.head = None
-        self.tail = None
+        # Loop through each face in this frame of video
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
     
-            When updating an entry, updates its position to the front of the LRU list.
-        If the entry is new and the cache is at capacity, removes the oldest entry
-        before the new entry is added.
-        '''
-        node = self.lookup.get(query)
-        if node is not None:
-            # Key exists in cache, update the value
-            node.results = results
-            self.linked_list.move_to_front(node)
-        else:
-            # Key does not exist in cache
-            if self.size == self.MAX_SIZE:
-                # Remove the oldest entry from the linked list and lookup
-                self.lookup.pop(self.linked_list.tail.query, None)
-                self.linked_list.remove_from_tail()
+    
+@click.command()
+@click.argument('image_to_check')
+@click.option('--cpus', default=1, help='number of CPU cores to use in parallel. -1 means 'use all in system'')
+@click.option('--model', default='hog', help='Which face detection model to use. Options are 'hog' or 'cnn'.')
+def main(image_to_check, cpus, model):
+    # Multi-core processing only supported on Python 3.4 or greater
+    if (sys.version_info < (3, 4)) and cpus != 1:
+        click.echo('WARNING: Multi-processing support requires Python 3.4 or greater. Falling back to single-threaded processing!')
+        cpus = 1
+    
+    face_recognition_model = face_recognition_models.face_recognition_model_location()
+face_encoder = dlib.face_recognition_model_v1(face_recognition_model)
+    
+    
+def pickle_weights(out_file_name, weights):
+    blobs = {
+        normalize_resnet_name(blob.name): utils.Caffe2TensorToNumpyArray(blob)
+        for blob in weights.protos
+    }
+    with open(out_file_name, 'w') as f:
+        pickle.dump(blobs, f, protocol=pickle.HIGHEST_PROTOCOL)
+    print('Wrote blobs:')
+    print(sorted(blobs.keys()))
+    
+    
+def initialize_model_from_cfg(weights_file, gpu_id=0):
+    '''Initialize a model from the global cfg. Loads test-time weights and
+    creates the networks in the Caffe2 workspace.
+    '''
+    model = model_builder.create(cfg.MODEL.TYPE, train=False, gpu_id=gpu_id)
+    net_utils.initialize_gpu_from_weights_file(
+        model, weights_file, gpu_id=gpu_id,
+    )
+    model_builder.add_inference_inputs(model)
+    workspace.CreateNet(model.net)
+    workspace.CreateNet(model.conv_body_net)
+    if cfg.MODEL.MASK_ON:
+        workspace.CreateNet(model.mask_net)
+    if cfg.MODEL.KEYPOINTS_ON:
+        workspace.CreateNet(model.keypoint_net)
+    return model
+    
+                if use_box_score:
+                kps_score = scores[j]
             else:
-                self.size += 1
-            # Add the new key and value
-            new_node = Node(results)
-            self.linked_list.append_to_front(new_node)
-            self.lookup[query] = new_node
-
+                kps_score /= kps_dets[j].shape[1]
+    
+            if BBGT.size > 0:
+            # compute overlaps
+            # intersection
+            ixmin = np.maximum(BBGT[:, 0], bb[0])
+            iymin = np.maximum(BBGT[:, 1], bb[1])
+            ixmax = np.minimum(BBGT[:, 2], bb[2])
+            iymax = np.minimum(BBGT[:, 3], bb[3])
+            iw = np.maximum(ixmax - ixmin + 1., 0.)
+            ih = np.maximum(iymax - iymin + 1., 0.)
+            inters = iw * ih
     
     
-class AddRequest(object):
+# ---------------------------------------------------------------------------- #
+# Helper functions for working with multilevel FPN RoIs
+# ---------------------------------------------------------------------------- #
     
-        def test_2nd_am_station_after_scan(self):
-        self.radio.scan()
-        station = self.radio.state.stations[self.radio.state.pos]
-        expected_station = '1380'
-        self.assertEqual(station, expected_station)
+        for i in range(cfg.KRCNN.NUM_STACKED_CONVS):
+        current = model.Conv(
+            current,
+            'conv_fcn' + str(i + 1),
+            dim_in,
+            hidden_dim,
+            kernel_size,
+            stride=1,
+            pad=pad_size,
+            weight_init=(cfg.KRCNN.CONV_INIT, {'std': 0.01}),
+            bias_init=('ConstantFill', {'value': 0.})
+        )
+        current = model.Relu(current, current)
+        dim_in = hidden_dim
     
-            # simple test to validate param value
-        if param in self._static_method_choices.keys():
-            self.param = param
-        else:
-            raise ValueError('Invalid Value for Param: {0}'.format(param))
     
-    print()
+_RENAME = {
+    # Removed 'ResNet_' from the name because it wasn't relevent
+    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v1up4convs':
+        'mask_rcnn_heads.mask_rcnn_fcn_head_v1up4convs',
+    # Removed 'ResNet_' from the name because it wasn't relevent
+    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v1up':
+        'mask_rcnn_heads.mask_rcnn_fcn_head_v1up',
+    # Removed 'ResNet_' from the name because it wasn't relevent
+    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v0upshare':
+        'mask_rcnn_heads.mask_rcnn_fcn_head_v0upshare',
+    # Removed 'ResNet_' from the name because it wasn't relevent
+    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v0up':
+        'mask_rcnn_heads.mask_rcnn_fcn_head_v0up',
+    # Removed head_builder module in favor of the more specific fast_rcnn name
+    'head_builder.add_roi_2mlp_head':
+        'fast_rcnn_heads.add_roi_2mlp_head',
+}
     
     
-if __name__ == '__main__':
-    reporter = Reporter()
-    db = DB()
-    tm = TestManager()
-    tm.setReporter(reporter)
-    tm.setDB(db)
-    reporter.setTM(tm)
-    db.setTM(tm)
-    # For simplification we are looping on the same test.
-    # Practically, it could be about various unique test classes and their
-    # objects
-    for i in range(3):
-        tc = TC()
-        tc.setTM(tm)
-        tm.setTC(tc)
-        tc.setup()
-        tc.execute()
-        tc.tearDown()
+def build_data_parallel_model(model, single_gpu_build_func):
+    '''Build a data parallel model given a function that builds the model on a
+    single GPU.
+    '''
+    if model.only_build_forward_pass:
+        single_gpu_build_func(model)
+    elif model.train:
+        all_loss_gradients = _build_forward_graph(model, single_gpu_build_func)
+        # Add backward pass on all GPUs
+        model.AddGradientOperators(all_loss_gradients)
+        if cfg.NUM_GPUS > 1:
+            _add_allreduce_graph(model)
+        for gpu_id in range(cfg.NUM_GPUS):
+            # After allreduce, all GPUs perform SGD updates on their identical
+            # params and gradients in parallel
+            with c2_utils.NamedCudaScope(gpu_id):
+                add_single_gpu_param_update_ops(model, gpu_id)
+    else:
+        # Test-time network operates on single GPU
+        # Test-time parallelism is implemented through multiprocessing
+        with c2_utils.NamedCudaScope(model.target_gpu_id):
+            single_gpu_build_func(model)
     
-            Calling `.wait` once the flag is set will not block.
-        '''
-        if not self._value:
-            self._value = True
+            rois = np.empty((0, 5), dtype=np.float32)
+        roi_probs = np.empty((0, 1), dtype=np.float32)
+        for im_i in range(num_images):
+            im_i_boxes, im_i_probs = self.proposals_for_one_image(
+                im_info[im_i, :], all_anchors, bbox_deltas[im_i, :, :, :],
+                scores[im_i, :, :, :]
+            )
+            batch_inds = im_i * np.ones(
+                (im_i_boxes.shape[0], 1), dtype=np.float32
+            )
+            im_i_rois = np.hstack((batch_inds, im_i_boxes))
+            rois = np.append(rois, im_i_rois, axis=0)
+            roi_probs = np.append(roi_probs, im_i_probs, axis=0)
     
-        def test_malformed_first_line_log(self):
-        with ExpectLog(gen_log, '.*Malformed HTTP request line'):
-            self.stream.write(b'asdf\r\n\r\n')
-            # TODO: need an async version of ExpectLog so we don't need
-            # hard-coded timeouts here.
-            self.io_loop.add_timeout(datetime.timedelta(seconds=0.05),
-                                     self.stop)
-            self.wait()
-    
-        @gen.coroutine
-    def server_send_line(self, line):
-        self.server_stream.write(line)
-        recv_line = yield self.client_stream.read_until(b'\r\n')
-        self.assertEqual(line, recv_line)
-    
-        def test_wait_timeout(self):
-        time = self.io_loop.time
+    import detectron.utils.boxes as box_utils
+import detectron.roi_data.data_utils as data_utils
+from detectron.core.config import cfg
