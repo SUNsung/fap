6b96892ae91568a027ddc18ebffa7666a2f14220
SUNsung <weatherius@gmail.com>
2018-08-12 11:46:42 +0300

    Rewrite [506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb] File in: 2018-08-12 11:46:41

--- ruby/506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb
+++ ruby/506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb
@@ -1,3 +1,57 @@
 
         
-        
+          # GET /resource/password/edit?reset_password_token=abcdef
+  def edit
+    self.resource = resource_class.new
+    set_minimum_password_length
+    resource.reset_password_token = params[:reset_password_token]
+  end
+    
+      # GET /resource/sign_in
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    yield resource if block_given?
+    respond_with(resource, serialize_options(resource))
+  end
+    
+        def translation_scope
+      'devise.unlocks'
+    end
+end
+
+    
+          def devise_mapping
+        @devise_mapping ||= Devise.mappings[scope_name]
+      end
+    
+        # Remove unresolved symlinks
+    symlinks.reverse_each do |s|
+      s.unlink unless s.resolved_path_exists?
+    end
+  end
+    
+      def create_gist(files, description)
+    url = 'https://api.github.com/gists'
+    data = { 'public' => true, 'files' => files, 'description' => description }
+    scopes = GitHub::CREATE_GIST_SCOPES
+    GitHub.open_api(url, data: data, scopes: scopes)['html_url']
+  end
+    
+              deps.any? do |dep|
+            begin
+              dep.to_formula.full_name == ff.full_name
+            rescue
+              dep.name == ff.name
+            end
+          end
+        rescue FormulaUnavailableError
+          # Silently ignore this case as we don't care about things used in
+          # taps that aren't currently tapped.
+          next
+        end
+      end
+    end
+    
+        def perform
+      return User.none unless valid?
