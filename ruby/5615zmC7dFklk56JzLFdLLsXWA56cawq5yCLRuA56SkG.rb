5a9c01d2115016b789e924f2ea866ada39e6a8a4
SUNsung <weatherius@gmail.com>
2018-08-12 09:57:17 +0300

    Rewrite [5615zmC7dFklk56JzLFdLLsXWA56cawq5yCLRuA56SkG.rb] File in: 2018-08-12 09:57:16

--- ruby/5615zmC7dFklk56JzLFdLLsXWA56cawq5yCLRuA56SkG.rb
+++ ruby/5615zmC7dFklk56JzLFdLLsXWA56cawq5yCLRuA56SkG.rb
@@ -1,26 +1,79 @@
 
         
-        desc 'Generates a dummy app for testing for every Spree engine'
-task :test_app do
-  SPREE_GEMS.each do |gem_name|
-    Dir.chdir('#{File.dirname(__FILE__)}/#{gem_name}') do
-      sh 'rake test_app'
+        # No trailing slash
+Benchmark.ips do |x|
+  x.report('with body include?') { CONTENT_CONTAINING.include?('<body') }
+  x.report('with body regexp')   { CONTENT_CONTAINING =~ /<\s*body/ }
+  x.compare!
+end
+
+    
+          def site
+        @site_drop ||= SiteDrop.new(@obj)
+      end
+    
+    module Jekyll
+  module External
+    class << self
+      #
+      # Gems that, if installed, should be loaded.
+      # Usually contain subcommands.
+      #
+      def blessed_gems
+        %w(
+          jekyll-compose
+          jekyll-docs
+          jekyll-import
+        )
+      end
+    
+          key = Spaceship::Portal::Key.create(name: 'New Key', apns: true, device_check: true, music_id: 'some-music-id')
+      expect(key).to be_instance_of(Spaceship::Portal::Key)
+      expect(key.id).to eq('a-new-key-id')
+    end
+  end
+    
+        alias :empty? :empty_file?
+    
+        # Extracts the Geometry from a 'WxH,O' string
+    # Where W is the width, H is the height,
+    # and O is the EXIF orientation
+    def self.parse(string)
+      GeometryParser.new(string).make
+    end
+    
+        def define_query
+      name = @name
+      @klass.send :define_method, '#{@name}?' do
+        send(name).file?
+      end
+    end
+    
+            def has_column?
+          @subject.column_names.include?('#{@attachment_name}_file_name')
+        end
+      end
     end
   end
 end
+
     
-            def update
-          authorize! :update, stock_location
-          if stock_location.update_attributes(stock_location_params)
-            respond_with(stock_location, status: 200, default_template: :show)
-          else
-            invalid_resource!(stock_location)
-          end
+            def failure_message_when_negated
+          'Attachment #{@attachment_name} cannot be between #{@low} and #{@high} bytes'
         end
+        alias negative_failure_message failure_message_when_negated
     
-            def responds?
-          methods = @subject.instance_methods.map(&:to_s)
-          methods.include?('#{@attachment_name}') &&
-            methods.include?('#{@attachment_name}=') &&
-            methods.include?('#{@attachment_name}?')
+          if defined?(ActiveRecord)
+        Paperclip.options[:logger] = ActiveRecord::Base.logger
+        ActiveRecord::Base.send(:include, Paperclip::Glue)
+      end
+    end
+  end
+end
+
+    
+          def validate_blacklist(record, attribute, value)
+        if forbidden.present? && forbidden.any? { |type| type === value }
+          mark_invalid record, attribute, forbidden
         end
+      end
