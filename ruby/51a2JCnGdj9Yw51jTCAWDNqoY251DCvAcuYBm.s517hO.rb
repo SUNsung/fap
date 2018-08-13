1a16fe9d8aa4f98d82d9ee27d1241ab56ac7c99f
SUNsung <weatherius@gmail.com>
2018-08-13 22:14:45 +0300

    Rewrite [51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb] File in: 2018-08-13 22:14:45

--- ruby/51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb
+++ ruby/51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb
@@ -1,54 +1,92 @@
 
         
-          describe 'with -n and a Fixnum range' do
-    before :each do
-      @script = '-W0 -ne 'print if %s' #{fixture(__FILE__, 'conditional_range.txt')}'
+            if ARGV.named.empty?
+      slow_checks = %w[
+        check_for_broken_symlinks
+        check_missing_deps
+        check_for_outdated_homebrew
+        check_for_linked_keg_only_brews
+      ]
+      methods = (checks.all.sort - slow_checks) + slow_checks
+    else
+      methods = ARGV.named
     end
     
-    load_extension('typed_data')
+      def set_table_sort(sort_options)
+    valid_sorts = sort_options[:sorts] or raise ArgumentError.new('You must specify :sorts as an array of valid sort attributes.')
+    default = sort_options[:default] || { valid_sorts.first.to_sym => :desc }
     
-          Process.wait(0).should == pid2
-      Process.wait.should == pid1
-    end
+      before_action :upgrade_warning, only: :index
     
-      def test_system_wordsplit
-    with_tmpchdir {|d|
-      write_file('script', <<-'End')
-        File.open('result', 'w') {|t| t << 'haha pid=#{$$} ppid=#{Process.ppid}' }
-        exit 5
-      End
-      str = '#{RUBY} script'
-      ret = system(str)
-      status = $?
-      assert_equal(false, ret)
-      assert_predicate(status, :exited?)
-      assert_equal(5, status.exitstatus)
-      assert_equal('haha pid=#{status.pid} ppid=#{$$}', File.read('result'))
-    }
+    module AccountFinderConcern
+  extend ActiveSupport::Concern
+    
+      # After we load the schema, make sure we have sequences for each
+  # table using timestamp IDs.
+  Rake::Task['db:schema:load'].enhance do
+    Rake::Task['db:ensure_id_sequences_exist'].invoke
   end
     
-          if name.bytesize > 100 or prefix.empty? then
-        raise Gem::Package::TooLongFileName.new('File \'#{prefix}/#{name}\' has a too long name (should be 100 or less)')
-      end
-    
-        @gem = util_spec 'some_gem' do |s|
-      s.rubyforge_project = 'example'
-    end
-    
-      def test_stat_inode
-    assert_not_equal 0, File.stat(__FILE__).ino
-  end
-    
-        assert_equal(hb, h2.update(h1))
-    assert_equal(hb, h2)
-  end
-    
-    describe :string_unpack_32bit_le_signed, shared: true do
-  it 'decodes an int with most significant bit set as a negative number' do
-    '\x00\xaa\x00\xff'.unpack(unpack_format()).should == [-16733696]
+      def deliver_digest
+    NotificationMailer.digest(user.account).deliver_now!
+    user.touch(:last_emailed_at)
   end
 end
+
     
-      it 'implicitly has a count of one when no count modifier is passed' do
-    '\xc2\x80\xc2\x81\xc2\x82\xc2\x83'.unpack('U1').should == [0x80]
+      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/favourite
+  def favourite
+    f = Favourite.last
+    NotificationMailer.favourite(f.status.account, Notification.find_by(activity: f))
   end
+    
+      def participation_count
+    poll_answers.sum('vote_count')
+  end
+end
+
+    
+      def delete_from_facebook(url, body)
+    Faraday.delete(url, body)
+  end
+end
+
+    
+    get '/' do
+  halt erb(:login) unless params[:user]
+  erb :chat, :locals => { :user => params[:user].gsub(/\W/, '') }
+end
+    
+      it 'should not leak changes to env' do
+    klass    = described_class
+    detector = Struct.new(:app) do
+      def call(env)
+        was = env.dup
+        res = app.call(env)
+        was.each do |k,v|
+          next if env[k] == v
+          fail 'env[#{k.inspect}] changed from #{v.inspect} to #{env[k].inspect}'
+        end
+        res
+      end
+    end
+    
+          class ValidateAttachmentPresenceMatcher
+        def initialize attachment_name
+          @attachment_name = attachment_name
+        end
+    
+      class Railtie
+    def self.insert
+      Paperclip.options[:logger] = Rails.logger
+    
+          def validate_each(record, attribute, value)
+        base_attribute = attribute.to_sym
+        attribute = '#{attribute}_content_type'.to_sym
+        value = record.send :read_attribute_for_validation, attribute
+    
+          def validate_whitelist(record, attribute, value)
+        if allowed.present? && allowed.none? { |type| type === value }
+          mark_invalid record, attribute, allowed
+        end
+      end
