ebd211f59f384f18a8540235704eb04a194ee542
SUNsung <weatherius@gmail.com>
2018-08-13 04:54:09 +0300

    Rewrite [568bn5GJOAMmk56kgvhDYo65K656Y4bFhtQDels56wzq.py] File in: 2018-08-13 04:54:09

--- pyton/568bn5GJOAMmk56kgvhDYo65K656Y4bFhtQDels56wzq.py
+++ pyton/568bn5GJOAMmk56kgvhDYo65K656Y4bFhtQDels56wzq.py
@@ -1,3 +1,13 @@
 
         
-        
+            Snippets without code (only comments) or containing lines starting with ??? should not yeld files,
+    but the counter for naming snippets should still increment.
+    '''
+    parser = argparse.ArgumentParser(description='Split md file into plain text and code blocks')
+    parser.add_argument('sourcefile',
+                        help='which file to read')
+    parser.add_argument('targetfile',
+                        help='where to put plain text')
+    parser.add_argument('codedir',
+                        help='where to put codeblocks')
+    args = parser.parse_args()
