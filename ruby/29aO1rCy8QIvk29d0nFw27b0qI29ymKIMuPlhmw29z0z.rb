23c78bb69a4eba04be06746b616e9d98b48dac51
SUNsung <weatherius@gmail.com>
2018-08-13 10:01:46 +0300

    Rewrite [29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb] File in: 2018-08-13 10:01:46

--- ruby/29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb
+++ ruby/29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb
@@ -1,10 +1,21 @@
 
         
-              configure_sass
-    end
+              def handle_prompt_none
+        if params[:prompt] == 'none'
+          if user_signed_in?
+            handle_prompt_with_signed_in_user
+          else
+            handle_params_error('login_required', 'User must already be logged in when `prompt` is `none`')
+          end
+        else
+          handle_params_error('invalid_request', 'The 'none' value cannot be used with any other prompt value')
+        end
+      end
     
-      find_files = ->(path) {
-    Find.find(Pathname.new(path).relative_path_from(Pathname.new Dir.pwd).to_s).map do |path|
-      path if File.file?(path)
-    end.compact
-  }
+        set :run, Proc.new { File.expand_path($0) == File.expand_path(app_file) }
+    
+        it 'Returns nil when Referer header is missing and allow_empty_referrer is false' do
+      env = {'HTTP_HOST' => 'foo.com'}
+      subject.options[:allow_empty_referrer] = false
+      expect(subject.referrer(env)).to be_nil
+    end
