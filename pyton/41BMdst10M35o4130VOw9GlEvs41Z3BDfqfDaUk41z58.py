363ef5b97920c83061c6a5da5f0c62d87dbd9dbd
SUNsung <weatherius@gmail.com>
2018-08-14 07:30:58 +0300

    Rewrite [41BMdst10M35o4130VOw9GlEvs41Z3BDfqfDaUk41z58.py] File in: 2018-08-14 07:30:57

--- pyton/41BMdst10M35o4130VOw9GlEvs41Z3BDfqfDaUk41z58.py
+++ pyton/41BMdst10M35o4130VOw9GlEvs41Z3BDfqfDaUk41z58.py
@@ -1,153 +1,17 @@
 
         
-        from keras import backend as K
-from keras.utils.test_utils import get_test_data
-from keras.models import Sequential, Model
-from keras.layers import Dense, Activation, GRU, TimeDistributed, Input
-from keras.utils import np_utils
-from keras.utils.test_utils import keras_test
-from numpy.testing import assert_almost_equal, assert_array_almost_equal
-    
-    print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
-    
-        In this task, there are variable length inputs of integers from 1-9, and a random
-    subset of unmasked outputs. Each of these outputs has a 50% probability of being
-    the input number unchanged, and a 50% probability of being 2*input%10.
-    
-                # check dropout
-            layer_test(convolutional_recurrent.ConvLSTM2D,
-                       kwargs={'data_format': data_format,
-                               'return_sequences': return_sequences,
-                               'filters': filters,
-                               'kernel_size': (num_row, num_col),
-                               'padding': 'same',
-                               'dropout': 0.1,
-                               'recurrent_dropout': 0.1},
-                       input_shape=inputs.shape)
-    
-    print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
-y_train = np.array(y_train)
-y_test = np.array(y_test)
-    
-    from keras.preprocessing import sequence
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
-from keras.layers import Embedding
-from keras.layers import Conv1D, GlobalMaxPooling1D
-from keras.datasets import imdb
+            cmd = cmd.split()
     
     
-allobj = [losses.mean_squared_error,
-          losses.mean_absolute_error,
-          losses.mean_absolute_percentage_error,
-          losses.mean_squared_logarithmic_error,
-          losses.squared_hinge,
-          losses.hinge,
-          losses.categorical_crossentropy,
-          losses.binary_crossentropy,
-          losses.kullback_leibler_divergence,
-          losses.poisson,
-          losses.cosine_proximity,
-          losses.logcosh,
-          losses.categorical_hinge]
-    
-    from importlib import import_module
-from os.path import join, dirname, abspath, exists, splitext
-    
-        def post_process(self, output):
-        for x in output:
-            if isinstance(x, (BaseItem, dict)):
-                for arg in self.args:
-                    if not arg in x:
-                        raise ContractFail(''%s' field is missing' % arg)
-
-    
-        def _get_slot(self, request, spider):
-        key = self._get_slot_key(request, spider)
-        if key not in self.slots:
-            conc = self.ip_concurrency if self.ip_concurrency else self.domain_concurrency
-            conc, delay = _get_concurrency_delay(conc, spider, self.settings)
-            self.slots[key] = Slot(conc, delay, self.randomize_delay)
-    
-        def request_seen(self, request):
-        fp = self.request_fingerprint(request)
-        if fp in self.fingerprints:
-            return True
-        self.fingerprints.add(fp)
-        if self.file:
-            self.file.write(fp + os.linesep)
-    
-    # There are two options for replacing |today|: either, you set today to some
-# non-false value, then it is used:
-#today = ''
-# Else, today_fmt is used as the format for a strftime call.
-#today_fmt = '%B %d, %Y'
-    
-    def is_prime(n):
-    if n % 2 == 0:
-        return False
+class Command(ScrapyCommand):
     
     
-def KeywordsFromSyntaxListOutput_PhpSyntax_ContainsPreProc_test():
-  assert_that( syntax_parse._KeywordsFromSyntaxListOutput(
-                   ContentsOfTestFile( 'php_syntax' ) ),
-               has_items( 'skip', 'function' ) )
-    
-      for match in regex.finditer( line ):
-    if match.end() == previous_char_index:
-      return True
-  # If the whole line is whitespace, that means the user probably finished an
-  # identifier on the previous line.
-  return line[ : current_column ].isspace()
-    
-      raise RuntimeError( 'Cannot find Python 2.7 or 3.4+. '
-                      'Set the 'g:ycm_server_python_interpreter' option '
-                      'to a Python interpreter path.' )
-    
-        exception = RuntimeError( 'Check client handles exception' )
-    with patch.object( ycm._message_poll_request,
-                       '_response_future',
-                       new = MockAsyncServerResponseException( exception ) ):
-      ycm.OnPeriodicTick() # Uses ycm._message_poll_request ...
-  '''
-  return mock.MagicMock( wraps = FakeFuture( True, None, exception ) )
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-        # Print the location of each face in this image
-    top, right, bottom, left = face_location
-    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
-    
-        # Find all the faces and face encodings in the current frame of video
-    face_locations = face_recognition.face_locations(rgb_frame)
-    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
-    
-        face_found = False
-    is_obama = False
-    
-    
-def process_images_in_process_pool(images_to_check, known_names, known_face_encodings, number_of_cpus, tolerance, show_distance):
-    if number_of_cpus == -1:
-        processes = None
-    else:
-        processes = number_of_cpus
-    
-        :param img_path: path to image to be recognized
-    :param predictions: results of the predict function
-    :return:
+class ScrapesContract(Contract):
+    ''' Contract to check presence of fields in scraped items
+        @scrapes page_name page_body
     '''
-    pil_image = Image.open(img_path).convert('RGB')
-    draw = ImageDraw.Draw(pil_image)
+    
+        def lineReceived(self, line):
+        return HTTPClient.lineReceived(self, line.rstrip())
+    
+    class CoreStats(object):
