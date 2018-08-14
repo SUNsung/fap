836db24a3f6ae8f8ed22668a89976f26c08d0734
SUNsung <weatherius@gmail.com>
2018-08-14 14:39:39 +0300

    Rewrite [58W9RV1uCOsuU58Yvve9fbLa.58tlwPEbUMuvg58S7M.py] File in: 2018-08-14 14:39:38

--- pyton/58W9RV1uCOsuU58Yvve9fbLa.58tlwPEbUMuvg58S7M.py
+++ pyton/58W9RV1uCOsuU58Yvve9fbLa.58tlwPEbUMuvg58S7M.py
@@ -1,51 +1,28 @@
 
         
-            # Then we cluster the lines together as blocks
-    # Each block represents a collection of lines that should be sorted
-    # This was done by assuming only links ([...](...)) are meant to be sorted
-    # Clustering is done by indentation
-    blocks = []
-    last_indent = None
-    for line in read_me:
-        s_line = line.lstrip()
-        indent = len(line) - len(s_line)
-    
-        exc = Timeout('Request timed out')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR_TIMEOUT
-    assert error_msg == 'Request timed out (30s).'
-
-    
-        ERROR_TIMEOUT = 2
-    ERROR_TOO_MANY_REDIRECTS = 6
-    
-    # TODO: response is the only one
-    
-    
-@pytest.fixture
-def httpbin_secure(httpbin_secure):
-    return prepare_url(httpbin_secure)
-
-    
-        @pytest.fixture(autouse=True)
-    def setup(self):
-        '''LookupDict instance with 'bad_gateway' attribute.'''
-        self.lookup_dict = LookupDict('test')
-        self.lookup_dict.bad_gateway = 502
-    
-    
-class URLRequired(RequestException):
-    '''A valid URL is required to make a request.'''
-    
-    
-    {# Latex figure (float) alignment
-#'figure_align': 'htbp',
-}
-    
-        # By using the 'with' statement we are sure the session is closed, thus we
-    # avoid leaving sockets open which can trigger a ResourceWarning in some
-    # cases, and look like a memory leak in others.
-    with sessions.Session() as session:
-        return session.request(method=method, url=url, **kwargs)
+        
+def write_with_harness(codefile, sourcefile, start_linenum, linebuffer):
+    '''write output with additional lines to make code likely compilable'''
+    # add commonly used headers, so that lines can likely compile.
+    # This is work in progress, the main issue remains handling class
+    # declarations in in-function code differently
+    with io.open(codefile, 'w') as code_filehandle:
+        code_filehandle.write('''\
+#include<stdio.h>      // by md-split
+#include<stdlib.h>     // by md-split
+#include<tuple>        // by md-split
+#include<utility>      // by md-split
+#include<limits>       // by md-split
+#include<functional>   // by md-split
+#include<string>       // by md-split
+#include<map>          // by md-split
+#include<iostream>     // by md-split
+#include<vector>       // by md-split
+#include<algorithm>    // by md-split
+#include<memory>       // by md-split
+using namespace std;   // by md-split
+// %s : %s
+''' % (sourcefile, start_linenum))
+        # TODO: if not toplevel code, wrap inside class
+        for codeline in linebuffer:
+            code_filehandle.write(codeline)
