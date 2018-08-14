0b2d852634e494443b0beb3968c0aa9a8c4d402f
SUNsung <weatherius@gmail.com>
2018-08-14 03:16:55 +0300

    Rewrite [14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py] File in: 2018-08-14 03:16:54

--- pyton/14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py
+++ pyton/14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py
@@ -1,10 +1,23 @@
 
         
-            with open('README.md', 'w+') as sorted_file:
-        # Then all of the blocks are sorted individually
-        blocks = [''.join(sorted(block, key=lambda s: s.lower())) for block in blocks]
-        # And the result is written back to README.md
-        sorted_file.write(''.join(blocks))
+            * :class:`dict`
+    * :class:`tuple`
+    * :class:`bytes`
+    * :class:`~flask.Markup`
+    * :class:`~uuid.UUID`
+    * :class:`~datetime.datetime`
+    '''
+    
+        :param template_name_or_list: the name of the template to be
+                                  rendered, or an iterable with template names
+                                  the first one existing will be rendered
+    :param context: the variables that should be available in the
+                    context of the template.
+    '''
+    ctx = _app_ctx_stack.top
+    ctx.app.update_template_context(context)
+    return _render(ctx.app.jinja_env.get_or_select_template(template_name_or_list),
+                   context, ctx.app)
     
         # This test only works on CPython 2.7.
     if sys.version_info >= (2, 7) and \
@@ -13,58 +26,174 @@
             for x in range(10):
                 fire()
     
-        app.config.update(SERVER_NAME='localhost:80')
-    with app.test_request_context('/', environ_overrides={'SERVER_NAME': 'localhost:80'}):
-        pass
-    
-    
-def get_args(args_list):
-    parser = argparse.ArgumentParser(
-        description='ansible inventory script reading from landscape cluster')
-    mutex_group = parser.add_mutually_exclusive_group(required=True)
-    help_list = 'list all hosts from landscape cluster'
-    mutex_group.add_argument('--list', action='store_true', help=help_list)
-    help_host = 'display variables for a host'
-    mutex_group.add_argument('--host', help=help_host)
-    return parser.parse_args(args_list)
-    
-                    if not os.path.exists(path):
-                    with open(path, 'w') as output_fd:
-                        output_fd.write(contents)
-    
-        def test_max_delay(self):
-        strategy = _exponential_backoff(retries=7, delay=1, backoff=2, max_delay=60)
-        result = list(strategy())
-        self.assertEquals(result, [1, 2, 4, 8, 16, 32, 60])
-    
-        def add_message(self, message):
-        self.cache.append(message)
-        if len(self.cache) > self.cache_size:
-            self.cache = self.cache[-self.cache_size:]
-        self.cond.notify_all()
-    
-    # Repeat the entire benchmark this many times (on different ports)
-# This gives JITs time to warm up, etc.  Pypy needs 3-5 runs at
-# --n=15000 for its JIT to reach full effectiveness
-define('num_runs', type=int, default=1)
-    
-    
-async def maybe_create_tables(db):
     try:
-        with (await db.cursor()) as cur:
-            await cur.execute('SELECT COUNT(*) FROM entries LIMIT 1')
-            await cur.fetchone()
-    except psycopg2.ProgrammingError:
-        with open('schema.sql') as f:
-            schema = f.read()
-        with (await db.cursor()) as cur:
-            await cur.execute(schema)
+    from greenlet import greenlet
+except ImportError:
+    greenlet = None
     
-        if not args:
-        args = ['localhost', 'www.google.com',
-                'www.facebook.com', 'www.dropbox.com']
+    pytestmark = pytest.mark.skipif(
+    blinker is None,
+    reason='Signals require the blinker library.'
+)
+    
+        h = history.history
+    assert_array_almost_equal(h['val_loss'], h['val_' + loss_full_name], decimal=decimal)
+    assert_array_almost_equal(h['val_loss'], h['val_weighted_' + loss_full_name], decimal=decimal)
+    
+        times = []
+    for use_cudnn in [True, False]:
+        start_time = time.time()
+        inputs = keras.layers.Input(shape=(None, input_size))
+        if use_cudnn:
+            if rnn_type == 'lstm':
+                layer = keras.layers.CuDNNLSTM(units)
+            else:
+                layer = keras.layers.CuDNNGRU(units)
+        else:
+            if rnn_type == 'lstm':
+                layer = keras.layers.LSTM(units)
+            else:
+                layer = keras.layers.GRU(units)
+        outputs = layer(inputs)
     
     
-if setuptools is not None:
-    python_requires = '>= 3.5'
-    kwargs['python_requires'] = python_requires
+@keras_test
+def test_max_norm():
+    array = get_example_array()
+    for m in get_test_values():
+        norm_instance = constraints.max_norm(m)
+        normed = norm_instance(K.variable(array))
+        assert(np.all(K.eval(normed) < m))
+    
+    
+# Artificial data generation:
+# Generate movies with 3 to 7 moving squares inside.
+# The squares are of shape 1x1 or 2x2 pixels,
+# which move linearly over time.
+# For convenience we first create movies with bigger width and height (80x80)
+# and at the end we select a 40x40 window.
+    
+        x_train = np.array(x[:int(len(x) * (1 - test_split))])
+    y_train = np.array(y[:int(len(x) * (1 - test_split))])
+    x_test = np.array(x[int(len(x) * (1 - test_split)):])
+    y_test = np.array(y[int(len(x) * (1 - test_split)):])
+    return (x_train, y_train), (x_test, y_test)
+
+    
+        from sklearn.tree import DecisionTreeClassifier
+    
+    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
+TRAIN_FOLDER = '20news-bydate-train'
+TEST_FOLDER = '20news-bydate-test'
+    
+    In this examples we will use a movie review dataset.
+    
+    data, row_idx, col_idx = sg._shuffle(data, random_state=0)
+plt.matshow(data, cmap=plt.cm.Blues)
+plt.title('Shuffled dataset')
+    
+    acc_clf1, acc_clf2 = [], []
+n_features_range = range(1, n_features_max + 1, step)
+for n_features in n_features_range:
+    score_clf1, score_clf2 = 0, 0
+    for _ in range(n_averages):
+        X, y = generate_data(n_train, n_features)
+    
+        t0 = time()
+    scores = uniform_labelings_scores(score_func, n_samples, n_clusters_range)
+    print('done in %0.3fs' % (time() - t0))
+    plots.append(plt.errorbar(
+        n_clusters_range, np.median(scores, axis=1), scores.std(axis=1))[0])
+    names.append(score_func.__name__)
+    
+        plt.imshow(avg_dist, interpolation='nearest', cmap=plt.cm.gnuplot2,
+               vmin=0)
+    plt.xticks(range(n_clusters), labels, rotation=45)
+    plt.yticks(range(n_clusters), labels)
+    plt.colorbar()
+    plt.suptitle('Interclass %s distances' % metric, size=18)
+    plt.tight_layout()
+    
+    
+class _BenchSpider(scrapy.Spider):
+    '''A spider that follows all links'''
+    name = 'follow'
+    total = 10000
+    show = 20
+    baseurl = 'http://localhost:8998'
+    link_extractor = LinkExtractor()
+    
+            if infos:
+            writeln(' (%s)' % (', '.join(infos),))
+        else:
+            write('\n')
+    
+    from . import Contract
+    
+    import six
+from twisted.internet import reactor, defer, task
+    
+            path = self._schemes[scheme]
+        try:
+            dhcls = load_object(path)
+            dh = dhcls(self._crawler.settings)
+        except NotConfigured as ex:
+            self._notconfigured[scheme] = str(ex)
+            return None
+        except Exception as ex:
+            logger.error('Loading '%(clspath)s' for scheme '%(scheme)s'',
+                         {'clspath': path, 'scheme': scheme},
+                         exc_info=True,  extra={'crawler': self._crawler})
+            self._notconfigured[scheme] = str(ex)
+            return None
+        else:
+            self._handlers[scheme] = dh
+        return self._handlers[scheme]
+    
+            self._signer = None
+        if is_botocore():
+            import botocore.auth
+            import botocore.credentials
+            kw.pop('anon', None)
+            if kw:
+                raise TypeError('Unexpected keyword arguments: %s' % kw)
+            if not self.anon:
+                SignerCls = botocore.auth.AUTH_TYPE_MAPS['s3']
+                self._signer = SignerCls(botocore.credentials.Credentials(
+                    aws_access_key_id, aws_secret_access_key))
+        else:
+            _S3Connection = _get_boto_connection()
+            try:
+                self.conn = _S3Connection(
+                    aws_access_key_id, aws_secret_access_key, **kw)
+            except Exception as ex:
+                raise NotConfigured(str(ex))
+    
+        def download(self, download_func, request, spider):
+        @defer.inlineCallbacks
+        def process_request(request):
+            for method in self.methods['process_request']:
+                response = yield method(request=request, spider=spider)
+                assert response is None or isinstance(response, (Response, Request)), \
+                        'Middleware %s.process_request must return None, Response or Request, got %s' % \
+                        (six.get_method_self(method).__class__.__name__, response.__class__.__name__)
+                if response:
+                    defer.returnValue(response)
+            defer.returnValue((yield download_func(request=request,spider=spider)))
+    
+        def _set_connection_attributes(self, request):
+        parsed = urlparse_cached(request)
+        self.scheme, self.netloc, self.host, self.port, self.path = _parsed_url_args(parsed)
+        proxy = request.meta.get('proxy')
+        if proxy:
+            self.scheme, _, self.host, self.port, _ = _parse(proxy)
+            self.path = self.url
+    
+            namelist = zip_file.namelist()
+        body = zip_file.read(namelist[0])
+        respcls = responsetypes.from_args(filename=namelist[0], body=body)
+        return response.replace(body=body, cls=respcls)
+    
+    from w3lib.http import basic_auth_header
+    
+    class CoreStats(object):
