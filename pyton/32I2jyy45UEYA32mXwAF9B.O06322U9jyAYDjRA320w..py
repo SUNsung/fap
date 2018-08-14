e2be3ce45adcd2f8741ffdfe6e70dd1cd0a96f93
SUNsung <weatherius@gmail.com>
2018-08-14 08:16:09 +0300

    Rewrite [32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py] File in: 2018-08-14 08:16:09

--- pyton/32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py
+++ pyton/32I2jyy45UEYA32mXwAF9B.O06322U9jyAYDjRA320w..py
@@ -1,94 +1,31 @@
 
         
-            def __init__(self, first_user, second_user):
-        super(PrivateChat, self).__init__()
-        self.users.append(first_user)
-        self.users.append(second_user)
+            with open('README.md', 'w+') as sorted_file:
+        # Then all of the blocks are sorted individually
+        blocks = [''.join(sorted(block, key=lambda s: s.lower())) for block in blocks]
+        # And the result is written back to README.md
+        sorted_file.write(''.join(blocks))
     
+        # for convenience in actions
+    HIDDEN = HIDDEN_CHANNEL
     
-class State(Enum):
-    unvisited = 0
-    visited = 1
-    
-        :param str u_string: unicode string to check. Must be unicode
-        and not Python 2 `str`.
-    :rtype: bool
-    '''
-    assert isinstance(u_string, str)
-    try:
-        u_string.encode('ascii')
-        return True
-    except UnicodeEncodeError:
-        return False
-
-    
-    
-class RequestsWarning(Warning):
-    '''Base warning for Requests.'''
-    pass
-    
-    
-# -- Options for manual page output ---------------------------------------
-    
-    from . import utils
-from . import packages
-from .models import Request, Response, PreparedRequest
-from .api import request, get, head, post, patch, put, delete, options
-from .sessions import session, Session
-from .status_codes import codes
-from .exceptions import (
-    RequestException, Timeout, URLRequired,
-    TooManyRedirects, HTTPError, ConnectionError,
-    FileModeWarning, ConnectTimeout, ReadTimeout
-)
-    
-    
-class AuthenticatorTest(test_util.TempDirTestCase,
-                        dns_test_common_lexicon.BaseLexiconAuthenticatorTest):
-    
-    # The master toctree document.
-master_doc = 'index'
-    
-            chain_file, abs_chain_path =\
-                _open_pem_file('chain_path', chain_path)
-        fullchain_file, abs_fullchain_path =\
-                _open_pem_file('fullchain_path', fullchain_path)
-    
-            self.addr1 = Addr.fromstring('127.0.0.1')
-        self.addr2 = Addr.fromstring('127.0.0.1:*')
-    
-    
-def fetch_zip(commit_hash, zip_dir, *, org='python', binary=False, verbose):
-    repo = f'cpython-{'bin' if binary else 'source'}-deps'
-    url = f'https://github.com/{org}/{repo}/archive/{commit_hash}.zip'
-    reporthook = None
-    if verbose:
-        reporthook = print
-    zip_dir.mkdir(parents=True, exist_ok=True)
-    filename, headers = urlretrieve(
-        url,
-        zip_dir / f'{commit_hash}.zip',
-        reporthook=reporthook,
-    )
-    return filename
-    
-    
-class TypeDefVisitor(EmitVisitor):
-    def visitModule(self, mod):
-        for dfn in mod.dfns:
-            self.visit(dfn)
-    
-        def clear(self):
-        for ta in self._array:
-            ta._text.clear()
-    
-    import sys
-    
-    
-class Tests(unittest.TestCase):
-    
-    N = 8                                   # Default; command line overrides
-    
-        subtracted
-    >>> a - b
-    Vec(-2, 0, 2)
+            total_count = 0
+        translated_count = 0
+        with open(fn) as f:
+            catalog = read_po(f)
+            for msg in catalog:
+                total_count += 1
+                if is_translated(msg):
+                    translated_count += 1
+        pct = translated_count / float(total_count) * 100
+        click.echo('% -7s % 2d%%' % (
+            locale,
+            pct,
+        ), err=True)
+        if pct >= MINIMUM and locale not in rv:
+            rv.append(locale)
+    with open(catalog_file, 'w') as f:
+        json.dump({
+            'supported_locales': sorted(rv)
+        }, f, indent=2)
+        f.write('\n')
