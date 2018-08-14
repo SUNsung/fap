ed34cd818eef3b219fab5957f164302eb1b7ec56
SUNsung <weatherius@gmail.com>
2018-08-14 13:25:04 +0300

    Rewrite [39.fkhe.g2l2c392I34zRYUc5Q39NtYjjngKVXc39CZt.rb] File in: 2018-08-14 13:25:04

--- ruby/39.fkhe.g2l2c392I34zRYUc5Q39NtYjjngKVXc39CZt.rb
+++ ruby/39.fkhe.g2l2c392I34zRYUc5Q39NtYjjngKVXc39CZt.rb
@@ -1,160 +1,60 @@
-    A binary installer is available:
-      https://www.haskell.org/platform/mac.html
-    EOS
-  when 'mysqldump-secure' then <<-EOS.undent
-    The creator of mysqldump-secure tried to game our popularity metrics.
-    EOS
-  when 'ngrok' then <<-EOS.undent
-    Upstream sunsetted 1.x in March 2016 and 2.x is not open-source.
-    
-    def bottle_resolve_version(bottle_file)
-  PkgVersion.parse bottle_receipt_path(bottle_file).split('/')[1]
-end
-    
-      def clang_build
-    @clang_build ||= MacOS.clang_build_version if MacOS.has_apple_developer_tools?
-  end
-    
-      def search_taps(rx)
-    SEARCHABLE_TAPS.map do |user, repo|
-      Thread.new { search_tap(user, repo, rx) }
-    end.inject([]) do |results, t|
-      results.concat(t.value)
-    end
-  end
-    
-    # This formula serves as the base class for several very similar
-# formulae for Amazon Web Services related tools.
-class AmazonWebServicesFormula < Formula
-  # Use this method to peform a standard install for Java-based tools,
-  # keeping the .jars out of HOMEBREW_PREFIX/lib
-  def install
-    rm Dir['bin/*.cmd'] # Remove Windows versions
-    libexec.install Dir['*']
-    bin.install_symlink Dir['#{libexec}/bin/*'] - ['#{libexec}/bin/service']
-  end
-  alias_method :standard_install, :install
-    
-    namespace :db do
-  namespace :migrate do
-    desc 'Setup the db or migrate depending on state of db'
-    task setup: :environment do
-      begin
-        if ActiveRecord::Migrator.current_version.zero?
-          Rake::Task['db:migrate'].invoke
-          Rake::Task['db:seed'].invoke
-        end
-      rescue ActiveRecord::NoDatabaseError
-        Rake::Task['db:setup'].invoke
-      else
-        Rake::Task['db:migrate'].invoke
-      end
-    end
-  end
-    
-      def theme_color
-    '#282c37'
-  end
-    
-        on :fetch_person_for_hcard do |guid|
-      person = Person.where(guid: guid, closed_account: false).where.not(owner: nil).take
-      if person
-        DiasporaFederation::Discovery::HCard.new(
-          guid:             person.guid,
-          nickname:         person.username,
-          full_name:        '#{person.profile.first_name} #{person.profile.last_name}'.strip,
-          url:              AppConfig.pod_uri,
-          photo_large_url:  person.image_url,
-          photo_medium_url: person.image_url(:thumb_medium),
-          photo_small_url:  person.image_url(:thumb_small),
-          public_key:       person.serialized_public_key,
-          searchable:       person.searchable,
-          first_name:       person.profile.first_name,
-          last_name:        person.profile.last_name
-        )
-      end
-    end
-    
-        it 'returns related entity for an existing comment' do
-      post = FactoryGirl.create(:status_message, author: local_person, public: true)
-      comment = FactoryGirl.create(:comment, author: remote_person, parent: post)
-      entity = DiasporaFederation.callbacks.trigger(:fetch_related_entity, 'Comment', comment.guid)
-      expect(entity.author).to eq(comment.diaspora_handle)
-      expect(entity.local).to be_falsey
-      expect(entity.public).to be_truthy
-      expect(entity.parent.author).to eq(post.diaspora_handle)
-      expect(entity.parent.local).to be_truthy
-      expect(entity.parent.public).to be_truthy
-      expect(entity.parent.parent).to be_nil
-    end
-    
-          def handle_params_error(error, error_description)
-        if params[:client_id] && params[:redirect_uri]
-          handle_params_error_when_client_id_and_redirect_uri_exists(error, error_description)
-        else
-          render_error I18n.t('api.openid_connect.error_page.could_not_authorize'), error_description
-        end
+
+        
+          describe '.create' do
+    it 'creates a key with the client' do
+      expected_service_configs = {
+        'U27F4V844T' => [],
+        'DQ8HTZ7739' => [],
+        '6A7HVUVQ3M' => ['some-music-id']
+      }
+      mock_client_response(:create_key!, with: { name: 'New Key', service_configs: expected_service_configs }) do
+        {
+          keyId: 'a-new-key-id'
+        }
       end
     
-    module RuboCop
-  module Cop
-    module Performance
-      # This cop identifies the use of `Regexp#match` or `String#match`, which
-      # returns `#<MatchData>`/`nil`. The return value of `=~` is an integral
-      # index/`nil` and is more performant.
-      #
-      # @example
-      #   # bad
-      #   do_something if str.match(/regex/)
-      #   while regex.match('str')
-      #     do_something
-      #   end
-      #
-      #   # good
-      #   method(str =~ /regex/)
-      #   return value unless regex =~ 'str'
-      class RedundantMatch < Cop
-        MSG = 'Use `=~` in places where the `MatchData` returned by ' \
-              '`#match` will not be used.'.freeze
+          def self.is_supported?(platform)
+        true
+      end
     
-              lambda do |corrector|
-            corrector.replace(center.source_range, new_center)
-          end
-        end
+          it 'splits correctly' do
+        expected = [
+          'One',
+          'Two',
+          'Three',
+          'Four Token',
+          'Or',
+          'Newlines',
+          'Everywhere'
+        ]
+        expect(generator.split_keywords(keywords)).to eq(expected)
       end
     end
   end
 end
 
     
-    module RuboCop
-  module Cop
-    module Lint
-      # Don't omit the accumulator when calling `next` in a `reduce` block.
-      #
-      # @example
-      #
-      #   # bad
-      #
-      #   result = (1..4).reduce(0) do |acc, i|
-      #     next if i.odd?
-      #     acc + i
-      #   end
-      #
-      # @example
-      #
-      #   # good
-      #
-      #   result = (1..4).reduce(0) do |acc, i|
-      #     next acc if i.odd?
-      #     acc + i
-      #   end
-      class NextWithoutAccumulator < Cop
-        MSG = 'Use `next` with an accumulator argument in a `reduce`.'.freeze
+          def load_all_tasks
+        self.tasks = []
     
-              [optarg_positions, arg_positions]
-        end
-      end
-    end
-  end
-end
+          it 'does set the source directory' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            cloc(source_directory: 'MyCoolApp')
+          end').runner.execute(:test)
+    
+          it 'it increments all targets minor version major' do
+        Fastlane::FastFile.new.parse('lane :test do
+          increment_version_number(bump_type: 'major')
+        end').runner.execute(:test)
+    
+    class Devise::OmniauthCallbacksController < DeviseController
+  prepend_before_action { request.env['devise.skip_timeout'] = true }
+    
+    class Devise::PasswordsController < DeviseController
+  prepend_before_action :require_no_authentication
+  # Render the #edit only if coming from a reset password email link
+  append_before_action :assert_reset_token_passed, only: :edit
+    
+      if record && record.respond_to?(:timedout?) && warden.authenticated?(scope) &&
+     options[:store] != false && !env['devise.skip_timeoutable']
+    last_request_at = warden.session(scope)['last_request_at']
