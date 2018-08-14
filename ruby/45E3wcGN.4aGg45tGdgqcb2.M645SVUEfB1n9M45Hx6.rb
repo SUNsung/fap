3188a984a1c12a9c38403c9f643916acaf398fcc
SUNsung <weatherius@gmail.com>
2018-08-14 20:30:39 +0300

    Rewrite [45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb] File in: 2018-08-14 20:30:39

--- ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
+++ ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
@@ -1,50 +1,90 @@
 
         
-        unless dups.empty?
-  puts '\nDuplicate links:'
-  dups.each do |link|
-    puts '- #{link}'
-    puts `grep -nr '#{link}' README.md`
+          def test_capture3_flip
+    o, e, s = Open3.capture3(RUBY, '-e', 'STDOUT.sync=true; 1000.times { print 'o'*1000; STDERR.print 'e'*1000 }')
+    assert_equal('o'*1000000, o)
+    assert_equal('e'*1000000, e)
+    assert(s.success?)
   end
-  puts '\nDone with errors.'
-  exit(1)
+    
+      def test_optional_self_reference
+    bug9593 = '[ruby-core:61299] [Bug #9593]'
+    o = Object.new
+    assert_warn(/circular argument reference - var/) do
+      o.instance_eval('def foo(var = defined?(var)) var end')
+    end
+    assert_equal(42, o.foo(42))
+    assert_equal('local-variable', o.foo, bug9593)
+    
+        it 'allows changing the wrapped struct' do
+      a = @s.wrap_struct(1024)
+      @s.change_struct(a, 100)
+      @s.get_struct(a).should == 100
+    end
+  end
+    
+    def exec_test(pathes)
+  @count = 0
+  @error = 0
+  @errbuf = []
+  @location = nil
+  @columns = 0
+  @width = pathes.map {|path| File.basename(path).size}.max + 2
+  pathes.each do |path|
+    @basename = File.basename(path)
+    $stderr.printf('%s%-*s ', erase(@quiet), @width, @basename)
+    $stderr.flush
+    @columns = @width + 1
+    $stderr.puts if @verbose
+    count = @count
+    error = @error
+    load File.expand_path(path)
+    if @tty
+      if @error == error
+        msg = 'PASS #{@count-count}'
+        @columns += msg.size - 1
+        $stderr.print '#{@progress_bs}#{@passed}#{msg}#{@reset}'
+      else
+        msg = 'FAIL #{@error-error}/#{@count-count}'
+        $stderr.print '#{@progress_bs}#{@failed}#{msg}#{@reset}'
+        @columns = 0
+      end
+    end
+    $stderr.puts unless @quiet and @tty and @error == error
+  end
+  $stderr.print(erase) if @quiet
+  if @error == 0
+    if @count == 0
+      $stderr.puts 'No tests, no problem'
+    else
+      $stderr.puts '#{@passed}PASS#{@reset} all #{@count} tests'
+    end
+    exit true
+  else
+    @errbuf.each do |msg|
+      $stderr.puts msg
+    end
+    $stderr.puts '#{@failed}FAIL#{@reset} #{@error}/#{@count} tests failed'
+    exit false
+  end
 end
     
-        private
+        t = Thread.new do
+      tester.sync_lock(:SH)
+      tester.sync_lock(:EX)
+    end
     
-      # Configure static asset server for tests with Cache-Control for performance.
-  if config.respond_to?(:serve_static_files)
-    # rails >= 4.2
-    config.serve_static_files = true
-  elsif config.respond_to?(:serve_static_assets)
-    # rails < 4.2
-    config.serve_static_assets = true
+      def self.hangul_comp_one(string)
+    length = string.length
+    if length>1 and 0 <= (lead =string[0].ord-LBASE) and lead  < LCOUNT and
+                    0 <= (vowel=string[1].ord-VBASE) and vowel < VCOUNT
+      lead_vowel = SBASE + (lead * VCOUNT + vowel) * TCOUNT
+      if length>2 and 0 < (trail=string[2].ord-TBASE) and trail < TCOUNT
+        (lead_vowel + trail).chr(Encoding::UTF_8) + string[3..-1]
+      else
+        lead_vowel.chr(Encoding::UTF_8) + string[2..-1]
+      end
+    else
+      string
+    end
   end
-  config.static_cache_control = 'public, max-age=3600'
-    
-    Then /^(?:|I )should see '([^']*)'$/ do |text|
-  expect(page).to have_content(text)
-end
-
-    
-            def matches? subject
-          @subject = subject
-          @subject = @subject.class unless Class === @subject
-          responds? && has_column?
-        end
-    
-            def failure_message
-          '#{expected_attachment}\n'.tap do |message|
-            message << accepted_types_and_failures.to_s
-            message << '\n\n' if @allowed_types.present? && @rejected_types.present?
-            message << rejected_types_and_failures.to_s
-          end
-        end
-    
-              @subject.send(@attachment_name).post_processing = false
-          @subject.send(@attachment_name).assign(file)
-          @subject.valid?
-          @subject.errors[:'#{@attachment_name}_file_size'].blank?
-        ensure
-          @subject.send(@attachment_name).post_processing = true
-        end
