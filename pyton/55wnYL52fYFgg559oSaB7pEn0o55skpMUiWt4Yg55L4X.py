0e4a5561245196d6ba7d43a176f405f2e5f1f607
SUNsung <weatherius@gmail.com>
2018-08-15 16:52:33 +0300

    Rewrite [55wnYL52fYFgg559oSaB7pEn0o55skpMUiWt4Yg55L4X.py] File in: 2018-08-15 16:52:33

--- pyton/55wnYL52fYFgg559oSaB7pEn0o55skpMUiWt4Yg55L4X.py
+++ pyton/55wnYL52fYFgg559oSaB7pEn0o55skpMUiWt4Yg55L4X.py
@@ -1,109 +1,154 @@
 
         
-        
-@pytest.fixture(params=containers)
-def proc(request, spawnu, TIMEOUT):
-    proc = spawnu(*request.param)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(u'tcsh')
-    proc.sendline(u'setenv PYTHONIOENCODING utf8')
-    proc.sendline(u'eval `thefuck --alias`')
-    return proc
+        print('Enter the PKCS1 private key, followed by a blank line:')
+privkey = b''
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    if line == '':
+        break
+    privkey += line.encode('ascii') + b'\n'
+privkey = rsa.PrivateKey.load_pkcs1(privkey)
     
-        def get_all_names_answer(self):
-        '''Returns the set of domain names that the plugin should find'''
-        if self._all_names:
-            return self._all_names
-        else:
-            raise errors.Error('No configuration file loaded')
+        params = {
+        'age_limit': age,
+        'skip_download': True,
+        'writeinfojson': True,
+        'outtmpl': '%(id)s.%(ext)s',
+    }
+    ydl = YoutubeDL(params)
+    ydl.add_default_info_extractors()
+    json_filename = os.path.splitext(filename)[0] + '.info.json'
+    try_rm(json_filename)
+    ydl.download([url])
+    res = os.path.exists(json_filename)
+    try_rm(json_filename)
+    return res
     
-        @mock.patch(
-        'certbot_compatibility_test.validator.crypto_util.probe_sni')
-    def test_certificate_success(self, mock_probe_sni):
-        cert = OpenSSL.crypto.X509()
-        mock_probe_sni.return_value = cert
-        self.assertTrue(self.validator.certificate(
-            cert, 'test.com', '127.0.0.1'))
-    
-        def __init__(self):
-        self.experts = []
-        self.common_state = {
-            'problems': 0,
-            'suggestions': 0,
-            'contributions': [],
-            'progress': 0   # percentage, if 100 -> task is finished
-        }
-    
-    
-class TestSubject(unittest.TestCase):
-    
-        # sample_queue.put('sam')
-    # test_object(sample_queue)
-    # print('Outside func: {}'.format(sample_queue.get()))
-    
-        def test_bear_greek_localization(self):
-        self.assertEqual(self.g.get('bear'), 'bear')
-
-    
-        def test_am_station_overflow_after_scan(self):
-        self.radio.scan()
-        station = self.radio.state.stations[self.radio.state.pos]
-        expected_station = '1250'
-        self.assertEqual(station, expected_station)
-    
-        def test_display_current_time_at_midnight(self):
-        '''
-        Will almost always fail (despite of right at/after midnight).
-        '''
-        time_provider_stub = MidnightTimeProvider()
-        class_under_test = TimeDisplay(time_provider_stub)
-        expected_time = '<span class=\'tinyBoldText\'>24:01</span>'
-        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
-    
-    print('Counting to two...')
-for number in count_to_two():
-    print(number, end=' ')
-    
-        def setup(self):
-        print('Setting up the Test')
-        time.sleep(0.1)
-        self._tm.prepareReporting()
-    
-        def __init__(self, super_user=False):
-        self.super_user = super_user
-    
-    from tornado.options import define, options
-    
-    define('port', default=8888, help='run on the given port', type=int)
-    
-    
-if __name__ == '__main__':
-    # Tornado configures logging.
-    options.parse_command_line()
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
-
-    
-    
-def main():
-    parse_command_line()
-    t = Timer(e1)
-    results = t.timeit(options.num) / options.num
-    print('engine: %0.3f ms per iteration' % (results * 1000))
-    t = Timer(c1)
-    results = t.timeit(options.num) / options.num
-    print('coroutine: %0.3f ms per iteration' % (results * 1000))
-    
-        garbage = garbage or gc.garbage
-    found = []
-    stack = []
-    stack_ids = set()
-    garbage_ids = set(map(id, garbage))
-    visited_ids = set()
+        def test_youtube_feeds(self):
+        self.assertMatch('https://www.youtube.com/feed/watch_later', ['youtube:watchlater'])
+        self.assertMatch('https://www.youtube.com/feed/subscriptions', ['youtube:subscriptions'])
+        self.assertMatch('https://www.youtube.com/feed/recommended', ['youtube:recommended'])
+        self.assertMatch('https://www.youtube.com/my_favorites', ['youtube:favorites'])
     
     try:
-    import twisted
+    from .lazy_extractors import *
+    from .lazy_extractors import _ALL_CLASSES
+    _LAZY_LOADER = True
 except ImportError:
-    twisted = None
+    _LAZY_LOADER = False
+    from .extractors import *
+    
+            return {
+            '_type': 'playlist',
+            'id': playlist_id,
+            'title': title,
+            'description': description,
+            'entries': entries,
+        }
+
+    
+    
+def extract_contents(args, path, output_dir):
+    '''
+    :type args: any
+    :type path: str
+    :type output_dir: str
+    '''
+    if not args.test:
+        if not os.path.exists(path):
+            return
+    
+    
+class GCPUtilsTestCase(unittest.TestCase):
+    params_dict = {
+        'url_map_name': 'foo_url_map_name',
+        'description': 'foo_url_map description',
+        'host_rules': [
+            {
+                'description': 'host rules description',
+                'hosts': [
+                        'www.example.com',
+                        'www2.example.com'
+                ],
+                'path_matcher': 'host_rules_path_matcher'
+            }
+        ],
+        'path_matchers': [
+            {
+                'name': 'path_matcher_one',
+                'description': 'path matcher one',
+                'defaultService': 'bes-pathmatcher-one-default',
+                'pathRules': [
+                        {
+                            'service': 'my-one-bes',
+                            'paths': [
+                                '/',
+                                '/aboutus'
+                            ]
+                        }
+                ]
+            },
+            {
+                'name': 'path_matcher_two',
+                'description': 'path matcher two',
+                'defaultService': 'bes-pathmatcher-two-default',
+                'pathRules': [
+                        {
+                            'service': 'my-two-bes',
+                            'paths': [
+                                '/webapp',
+                                '/graphs'
+                            ]
+                        }
+                ]
+            }
+        ]
+    }
+    
+        def test_max_delay_none(self):
+        strategy = _exponential_backoff(retries=7, delay=1, backoff=2, max_delay=None)
+        result = list(strategy())
+        self.assertEquals(result, [1, 2, 4, 8, 16, 32, 64])
+    
+        def _assert_globals(self, res):
+        self.assertIsInstance(res, dict)
+        self.assertIn('foo', res)
+        self.assertEqual(res['foo'], 'bar')
+    
+        ec2_metadata_facts = Ec2Metadata(module).run()
+    ec2_metadata_facts_result = dict(changed=False, ansible_facts=ec2_metadata_facts)
+    
+    
+class WhiteSpaceTokenList(TokenList):
+    
+        def assertEqualCI(self, s1, s2):
+        '''Assert that two strings are equal ignoring case differences.'''
+        self.assertEqual(s1.lower(), s2.lower())
+    
+        @unittest.skipUnless(hasattr(posix, 'O_EXLOCK'),
+                         'test needs posix.O_EXLOCK')
+    def test_osexlock(self):
+        fd = os.open(support.TESTFN,
+                     os.O_WRONLY|os.O_EXLOCK|os.O_CREAT)
+        self.assertRaises(OSError, os.open, support.TESTFN,
+                          os.O_WRONLY|os.O_EXLOCK|os.O_NONBLOCK)
+        os.close(fd)
+    
+        def in_special_context(self, node):
+        if node.parent is None:
+            return False
+        results = {}
+        if (node.parent.parent is not None and
+               self.p1.match(node.parent.parent, results) and
+               results['node'] is node):
+            # list(d.keys()) -> list(d.keys()), etc.
+            return results['func'].value in consuming_calls
+        # for ... in d.iterkeys() -> for ... in d.keys(), etc.
+        return self.p2.match(node.parent, results) and results['node'] is node
+
+    
+        def test_3x_style_invalid_3(self):
+        self.invalid_syntax('raise from E1, E2')
