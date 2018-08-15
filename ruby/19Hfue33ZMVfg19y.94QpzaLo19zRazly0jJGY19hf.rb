bd4044c1849b8236d842552908f5c379a378ebb3
SUNsung <weatherius@gmail.com>
2018-08-15 16:17:25 +0300

    Rewrite [19Hfue33ZMVfg19y.94QpzaLo19zRazly0jJGY19hf.rb] File in: 2018-08-15 16:17:24

--- ruby/19Hfue33ZMVfg19y.94QpzaLo19zRazly0jJGY19hf.rb
+++ ruby/19Hfue33ZMVfg19y.94QpzaLo19zRazly0jJGY19hf.rb
@@ -1,39 +1,77 @@
 
         
-          def deliver_digest
-    NotificationMailer.digest(user.account).deliver_now!
-    user.touch(:last_emailed_at)
-  end
-end
-
+              def initialize
+        @log_tags = []
     
-      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/favourite
-  def favourite
-    f = Favourite.last
-    NotificationMailer.favourite(f.status.account, Notification.find_by(activity: f))
-  end
+            def test_url_memory_db_for_sqlite3
+          spec = resolve 'sqlite3::memory:'
+          assert_equal(':memory:', spec['database'])
+        end
     
-    if profile_filename = ENV['PROFILE']
-  require 'ruby-prof'
-  reporter =
-    case (profile_extname = File.extname(profile_filename))
-    when '.txt'
-      RubyProf::FlatPrinterWithLineNumbers
-    when '.html'
-      RubyProf::GraphHtmlPrinter
-    when '.callgrind'
-      RubyProf::CallTreePrinter
-    else
-      raise 'Unknown profiler format indicated by extension: #{profile_extname}'
+      def test_should_validation_the_associated_models_on_create
+    assert_no_difference('#{ @association_name == :birds ? 'Bird' : 'Parrot' }.count') do
+      2.times { @pirate.send(@association_name).build }
+      @pirate.save
     end
-  File.open(profile_filename, 'w') do |io|
-    reporter.new(RubyProf.profile { Pod::Command.run(ARGV) }).print(io)
   end
-else
-  Pod::Command.run(ARGV)
-end
-
     
-      def ruby_bin
-    File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name'])
+    require 'models/topic'
+    
+    require 'active_support/hash_with_indifferent_access'
+    
+        assert_called(conn, :close) do
+      @server.restart
+    end
+  end
+    
+        def initial_urls
+      [ 'https://hexdocs.pm/elixir/#{self.class.release}/api-reference.html',
+        'https://hexdocs.pm/eex/#{self.class.release}/EEx.html',
+        'https://hexdocs.pm/ex_unit/#{self.class.release}/ExUnit.html',
+        'https://hexdocs.pm/iex/#{self.class.release}/IEx.html',
+        'https://hexdocs.pm/logger/#{self.class.release}/Logger.html',
+        'https://hexdocs.pm/mix/#{self.class.release}/Mix.html',
+        'https://elixir-lang.org/getting-started/introduction.html' ]
+    end
+    
+        options[:only_patterns] = [
+      /\Alanguage\./,
+      /\Aclass\./,
+      /\Afunctions?\./,
+      /\Acontrol-structures/,
+      /\Aregexp\./,
+      /\Areserved\.exceptions/,
+      /\Areserved\.interfaces/,
+      /\Areserved\.variables/]
+    
+            while true
+          begin
+            if Platform.windows?
+              # Windows doesn't support non-blocking reads on
+              # file descriptors or pipes so we have to get
+              # a bit more creative.
+    
+        # @abstract
+    #
+    # Your implementation should check if the specified remote-repository is
+    # available.
+    #
+    # @return [Boolean]
+    #
+    def check
+      raise NotImplementedError, 'Your SCM strategy module should provide a #check method'
+    end
+    
+            it 'returns all servers' do
+          expect(subject.map(&:hostname)).to eq %w{example1.com example2.com example3.com example4.com example5.com}
+        end
+      end
+    
+      def command_line(*options)
+    options.each { |opt| ARGV << opt }
+    subject.define_singleton_method(:exit) do |*_args|
+      throw(:system_exit, :exit)
+    end
+    subject.run
+    subject.options
   end
