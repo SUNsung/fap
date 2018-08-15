a54065a3c9ea5038cd661018953ebe221af0babb
SUNsung <weatherius@gmail.com>
2018-08-15 06:02:42 +0300

    Rewrite [45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb] File in: 2018-08-15 06:02:41

--- ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
+++ ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
@@ -1,90 +1,91 @@
 
         
-          def test_capture3_flip
-    o, e, s = Open3.capture3(RUBY, '-e', 'STDOUT.sync=true; 1000.times { print 'o'*1000; STDERR.print 'e'*1000 }')
-    assert_equal('o'*1000000, o)
-    assert_equal('e'*1000000, e)
-    assert(s.success?)
-  end
+                def scaffold_post_content
+          ERB.new(File.read(File.expand_path(scaffold_path, site_template))).result
+        end
     
-      def test_optional_self_reference
-    bug9593 = '[ruby-core:61299] [Bug #9593]'
-    o = Object.new
-    assert_warn(/circular argument reference - var/) do
-      o.instance_eval('def foo(var = defined?(var)) var end')
-    end
-    assert_equal(42, o.foo(42))
-    assert_equal('local-variable', o.foo, bug9593)
+            def processed?
+          @processed
+        end
     
-        it 'allows changing the wrapped struct' do
-      a = @s.wrap_struct(1024)
-      @s.change_struct(a, 100)
-      @s.get_struct(a).should == 100
-    end
-  end
+          def site
+        @site_drop ||= SiteDrop.new(@obj)
+      end
     
-    def exec_test(pathes)
-  @count = 0
-  @error = 0
-  @errbuf = []
-  @location = nil
-  @columns = 0
-  @width = pathes.map {|path| File.basename(path).size}.max + 2
-  pathes.each do |path|
-    @basename = File.basename(path)
-    $stderr.printf('%s%-*s ', erase(@quiet), @width, @basename)
-    $stderr.flush
-    @columns = @width + 1
-    $stderr.puts if @verbose
-    count = @count
-    error = @error
-    load File.expand_path(path)
-    if @tty
-      if @error == error
-        msg = 'PASS #{@count-count}'
-        @columns += msg.size - 1
-        $stderr.print '#{@progress_bs}#{@passed}#{msg}#{@reset}'
-      else
-        msg = 'FAIL #{@error-error}/#{@count-count}'
-        $stderr.print '#{@progress_bs}#{@failed}#{msg}#{@reset}'
-        @columns = 0
+          def_delegator :@obj, :cleaned_relative_path, :path
+      def_delegator :@obj, :output_ext, :output_ext
+    
+        def excluded?(entry)
+      glob_include?(site.exclude, relative_to_source(entry)).tap do |excluded|
+        if excluded
+          Jekyll.logger.debug(
+            'EntryFilter:',
+            'excluded #{relative_to_source(entry)}'
+          )
+        end
       end
     end
-    $stderr.puts unless @quiet and @tty and @error == error
-  end
-  $stderr.print(erase) if @quiet
-  if @error == 0
-    if @count == 0
-      $stderr.puts 'No tests, no problem'
-    else
-      $stderr.puts '#{@passed}PASS#{@reset} all #{@count} tests'
+    
+        sidekiq_options queue: 'critical'
+    
+        version '5' do
+      self.release = '5.4.0'
+      self.base_url = 'https://github.com/d3/'
+      self.root_path = 'd3/blob/master/API.md'
+    
+        version '1.3' do
+      self.release = '1.3.4'
+      self.base_urls = [
+        'https://hexdocs.pm/elixir/#{release}/',
+        'https://hexdocs.pm/eex/#{release}/',
+        'https://hexdocs.pm/ex_unit/#{release}/',
+        'https://hexdocs.pm/iex/#{release}/',
+        'https://hexdocs.pm/logger/#{release}/',
+        'https://hexdocs.pm/mix/#{release}/',
+        'https://elixir-lang.org/getting-started/'
+      ]
     end
-    exit true
-  else
-    @errbuf.each do |msg|
-      $stderr.puts msg
-    end
-    $stderr.puts '#{@failed}FAIL#{@reset} #{@error}/#{@count} tests failed'
-    exit false
   end
 end
+
     
-        t = Thread.new do
-      tester.sync_lock(:SH)
-      tester.sync_lock(:EX)
-    end
+        private
     
-      def self.hangul_comp_one(string)
-    length = string.length
-    if length>1 and 0 <= (lead =string[0].ord-LBASE) and lead  < LCOUNT and
-                    0 <= (vowel=string[1].ord-VBASE) and vowel < VCOUNT
-      lead_vowel = SBASE + (lead * VCOUNT + vowel) * TCOUNT
-      if length>2 and 0 < (trail=string[2].ord-TBASE) and trail < TCOUNT
-        (lead_vowel + trail).chr(Encoding::UTF_8) + string[3..-1]
-      else
-        lead_vowel.chr(Encoding::UTF_8) + string[2..-1]
-      end
-    else
-      string
+        def as_json
+      { name: name, path: path, type: type }
     end
   end
+end
+
+    
+          users = User.arel_table
+      people = Person.arel_table
+      profiles = Profile.arel_table
+      res = User.joins(person: :profile)
+      res = res.where(users[:username].matches('%#{username}%')) unless username.blank?
+      res = res.where(users[:email].matches('%#{email}%')) unless email.blank?
+      res = res.where(people[:guid].matches('%#{guid}%')) unless guid.blank?
+      res = res.where(profiles[:birthday].gt(Date.today-13.years)) if under13 == '1'
+      res
+    end
+  end
+end
+
+    
+          def stages
+        names = Dir[stage_definitions].map { |f| File.basename(f, '.rb') }
+        assert_valid_stage_names(names)
+        names
+      end
+    
+    desc 'Deploy a new release.'
+task :deploy do
+  set(:deploying, true)
+  %w{ starting started
+      updating updated
+      publishing published
+      finishing finished }.each do |task|
+    invoke 'deploy:#{task}'
+  end
+end
+task default: :deploy
