dba446a67167f8ab96c800d6066dd7a04c5c94ae
SUNsung <weatherius@gmail.com>
2018-08-15 12:05:32 +0300

    Rewrite [23CqzEkB0MoNQ23WIOAo.XOLUw23QT35SxRCcU23.1Y.cpp] File in: 2018-08-15 12:05:31

--- cccccc/23CqzEkB0MoNQ23WIOAo.XOLUw23QT35SxRCcU23.1Y.cpp
+++ cccccc/23CqzEkB0MoNQ23WIOAo.XOLUw23QT35SxRCcU23.1Y.cpp
@@ -1,275 +1,250 @@
 
         
-          // GlobalShortcutListener::Observer implementation.
-  void OnKeyPressed(const ui::Accelerator& accelerator) override;
-    
-    // relauncher implements main browser application relaunches across platforms.
-// When a browser wants to relaunch itself, it can't simply fork off a new
-// process and exec a new browser from within. That leaves open a window
-// during which two browser applications might be running concurrently. If
-// that happens, each will wind up with a distinct Dock icon, which is
-// especially bad if the user expected the Dock icon to be persistent by
-// choosing Keep in Dock from the icon's contextual menu.
-//
-// relauncher approaches this problem by introducing an intermediate
-// process (the 'relauncher') in between the original browser ('parent') and
-// replacement browser ('relaunched'). The helper executable is used for the
-// relauncher process; because it's an LSUIElement, it doesn't get a Dock
-// icon and isn't visible as a running application at all. The parent will
-// start a relauncher process, giving it the 'writer' side of a pipe that it
-// retains the 'reader' end of. When the relauncher starts up, it will
-// establish a kqueue to wait for the parent to exit, and will then write to
-// the pipe. The parent, upon reading from the pipe, is free to exit. When the
-// relauncher is notified via its kqueue that the parent has exited, it
-// proceeds, launching the relaunched process. The handshake to synchronize
-// the parent with the relauncher is necessary to avoid races: the relauncher
-// needs to be sure that it's monitoring the parent and not some other process
-// in light of PID reuse, so the parent must remain alive long enough for the
-// relauncher to set up its kqueue.
-    
-    #endif  // ATOM_BROWSER_UNRESPONSIVE_SUPPRESSOR_H_
-
-    
-    namespace mate {
-    }
-    
-    
-    {  DraggableRegion();
-};
-    
-      // Sets ID of the hosting desktop picker dialog. The window with this ID will
-  // be filtered out from the list of sources.
-  virtual void SetViewDialogWindowId(content::DesktopMediaID::Id dialog_id) = 0;
-    
-      // This returns true if all components are specified, and min and max are
-  // equal.
-  bool HasFixedSize() const;
-    
-    #endif  // ATOM_BROWSER_API_ATOM_API_NET_H_
-
-    
-    // Convert a Python object to a FileDescriptorProto pointer.
-// Handles all kinds of Python errors, which are simply logged.
-static bool GetFileDescriptorProto(PyObject* py_descriptor,
-                                   FileDescriptorProto* output) {
-  if (py_descriptor == NULL) {
-    if (PyErr_ExceptionMatches(PyExc_KeyError)) {
-      // Expected error: item was simply not found.
-      PyErr_Clear();
-    } else {
-      GOOGLE_LOG(ERROR) << 'DescriptorDatabase method raised an error';
-      PyErr_Print();
-    }
-    return false;
-  }
-  if (py_descriptor == Py_None) {
-    return false;
-  }
-  const Descriptor* filedescriptor_descriptor =
-      FileDescriptorProto::default_instance().GetDescriptor();
-  CMessage* message = reinterpret_cast<CMessage*>(py_descriptor);
-  if (PyObject_TypeCheck(py_descriptor, &CMessage_Type) &&
-      message->message->GetDescriptor() == filedescriptor_descriptor) {
-    // Fast path: Just use the pointer.
-    FileDescriptorProto* file_proto =
-        static_cast<FileDescriptorProto*>(message->message);
-    *output = *file_proto;
-    return true;
-  } else {
-    // Slow path: serialize the message. This allows to use databases which
-    // use a different implementation of FileDescriptorProto.
-    ScopedPyObjectPtr serialized_pb(
-        PyObject_CallMethod(py_descriptor, 'SerializeToString', NULL));
-    if (serialized_pb == NULL) {
-      GOOGLE_LOG(ERROR)
-          << 'DescriptorDatabase method did not return a FileDescriptorProto';
-      PyErr_Print();
-      return false;
-    }
-    char* str;
-    Py_ssize_t len;
-    if (PyBytes_AsStringAndSize(serialized_pb.get(), &str, &len) < 0) {
-      GOOGLE_LOG(ERROR)
-          << 'DescriptorDatabase method did not return a FileDescriptorProto';
-      PyErr_Print();
-      return false;
-    }
-    FileDescriptorProto file_proto;
-    if (!file_proto.ParseFromArray(str, len)) {
-      GOOGLE_LOG(ERROR)
-          << 'DescriptorDatabase method did not return a FileDescriptorProto';
-      return false;
-    }
-    *output = file_proto;
-    return true;
-  }
-}
-    
-    #include <google/protobuf/testing/googletest.h>
-#include <gtest/gtest.h>
-#include <google/protobuf/testing/file.h>
-    
-    #include <google/protobuf/compiler/code_generator.h>
-#include <google/protobuf/compiler/plugin.h>
-#include <google/protobuf/descriptor.h>
-#include <google/protobuf/descriptor.pb.h>
-#include <google/protobuf/io/printer.h>
-#include <google/protobuf/io/zero_copy_stream.h>
-#include <google/protobuf/wire_format.h>
-    
-    
-    
-    #include <string>
-    
-    
-    {
-    {
-    {
-    {
-    {}  // namespace
-}  // namespace java
-}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-
-    
-      // All static variables have to be declared at the top-level of the file
-  // so that we can control initialization order, which is important for
-  // DescriptorProto bootstrapping to work.
-  virtual void GenerateStaticVariables(
-      io::Printer* printer, int* bytecode_estimate) = 0;
-    
-    static void PrintScriptDirs(const GenericVector<StrongScriptDirection> &dirs) {
-  for (int i = 0; i < dirs.size(); i++) {
-    switch (dirs[i]) {
-      case DIR_NEUTRAL: tprintf ('N '); break;
-      case DIR_LEFT_TO_RIGHT: tprintf('L '); break;
-      case DIR_RIGHT_TO_LEFT: tprintf('R '); break;
-      case DIR_MIX: tprintf('Z '); break;
-      default: tprintf('? '); break;
-    }
-  }
-  tprintf('\n');
-}
-    
-    
-    {  virtual R Run() {
-    if (!del) {
-      R result = (*function_)(p1_,p2_,p3_,p4_,p5_);
-      return result;
-    } else {
-      R result = (*function_)(p1_,p2_,p3_,p4_,p5_);
-      //  zero out the pointer to ensure segfault if used again
-      function_ = nullptr;
-      delete this;
-      return result;
-    }
-  }
-};
-    
-      void Clear();
-    
-      // Creates and returns a Pix with the same resolution as the original
-  // in which 1 (black) pixels represent likely non text (photo, line drawing)
-  // areas of the page, deleting from the blob_block the blobs that were
-  // determined to be non-text.
-  // The photo_map (binary image mask) is used to bias the decision towards
-  // non-text, rather than supplying a definite decision.
-  // The blob_block is the usual result of connected component analysis,
-  // holding the detected blobs.
-  // The returned Pix should be PixDestroyed after use.
-  Pix* ComputeNonTextMask(bool debug, Pix* photo_map, TO_BLOCK* blob_block);
-    
-                // second, get data from reader, stored it in cache
-            // 1. for each key, allocate the specific matrix on device
-            for (auto& pa : inputMatrices)
-            {
-                const wstring& name = pa.first;
-                const auto& input = pa.second;
-                auto& M = input.GetMatrix<ElemType>();
-                if (m_inputMatricesCache.find(name) == m_inputMatricesCache.end())
-                    m_inputMatricesCache.AddInput(name, make_shared<Matrix<ElemType>>(M, M.GetDeviceId()), input.pMBLayout, input.sampleLayout); // deep copy from M
-                else
-                    m_inputMatricesCache.GetInputMatrix<ElemType>(name).SetValue(M);
-            }
-            // 2. MBlayout
-            m_MBLayoutCache->CopyFrom(net.GetMBLayoutPtrOfNetwork());
-            size_t nParallelSequences = m_MBLayoutCache->GetNumParallelSequences();
-    
-    #include 'Basics.h'
-#include 'ComputationNetwork.h'
-#include 'SimpleEvaluator.h'
-#include 'DataReader.h'
-#include 'ScriptableObjects.h'
-#include 'Criterion.h'
-#include <vector>
-#include <string>
-#include <stdexcept>
-#include 'fileutil.h'
-#include 'Config.h'
-#include <chrono>
-#include <random>
-#include 'Profiler.h'
-#include 'MASGD.h'
-#include 'ASGDHelper.h'
-#include <map>
-using namespace std; // ugh! TODO: get rid of this from .h files!!!
-    
-    template<> inline
-dnnError_t dnnConversionCreate<double>(dnnPrimitive_t* pConversion, const dnnLayout_t from, const dnnLayout_t to)
+        namespace cv { namespace cuda { namespace device
 {
-    return dnnConversionCreate_F64(pConversion, from, to);
-}
-    
-    // understand and execute from the syntactic expression tree
-ConfigValuePtr Evaluate(ExpressionPtr);                               // evaluate the expression tree
-void Do(ExpressionPtr e);                                             // evaluate e.do
-shared_ptr<Object> EvaluateField(ExpressionPtr e, const wstring& id); // for experimental CNTK integration
-    
-    
-    {  // Check that deleting works.
-  ASSERT_TRUE(!env_->DeleteFile('/dir/non_existent').ok());
-  ASSERT_OK(env_->DeleteFile('/dir/g'));
-  ASSERT_TRUE(!env_->FileExists('/dir/g'));
-  ASSERT_OK(env_->GetChildren('/dir', &children));
-  ASSERT_EQ(0, children.size());
-  ASSERT_OK(env_->DeleteDir('/dir'));
-}
-    
-      // create first key range
-  leveldb::WriteBatch batch;
-  for (size_t i = 0; i < kNumKeys; i++) {
-    batch.Put(Key1(i), 'value for range 1 key');
-  }
-  ASSERT_OK(db->Write(leveldb::WriteOptions(), &batch));
-    
-    BlockBuilder::BlockBuilder(const Options* options)
-    : options_(options),
-      restarts_(),
-      counter_(0),
-      finished_(false) {
-  assert(options->block_restart_interval >= 1);
-  restarts_.push_back(0);       // First restart point is at offset 0
-}
-    
-    class FilterPolicy;
-    
-    class Histogram {
- public:
-  Histogram() { }
-  ~Histogram() { }
+    struct Emulation
+    {
+    }
+    }
+    }
     }
     
-    // Helper class that locks a mutex on construction and unlocks the mutex when
-// the destructor of the MutexLock object is invoked.
-//
-// Typical usage:
-//
-//   void MyClass::MyMethod() {
-//     MutexLock l(&mu_);       // mu_ is an instance variable
-//     ... some complex code, possibly with multiple return paths ...
-//   }
+    #undef cv_hal_LU32f
+#define cv_hal_LU32f lapack_LU32f
+#undef cv_hal_LU64f
+#define cv_hal_LU64f lapack_LU64f
     
-    #include <stdint.h>
+        static void* GetProcAddress (const char* name)
+    {
+        static void* h = NULL;
+        if (!h)
+        {
+            h = dlopen('libclAmdFft.Runtime.so', RTLD_LAZY | RTLD_GLOBAL);
+            if (!h)
+                return NULL;
+        }
+    }
     
-      ~Writer();
+    const float g_8x32fTab[] =
+{
+    -128.f, -127.f, -126.f, -125.f, -124.f, -123.f, -122.f, -121.f,
+    -120.f, -119.f, -118.f, -117.f, -116.f, -115.f, -114.f, -113.f,
+    -112.f, -111.f, -110.f, -109.f, -108.f, -107.f, -106.f, -105.f,
+    -104.f, -103.f, -102.f, -101.f, -100.f,  -99.f,  -98.f,  -97.f,
+     -96.f,  -95.f,  -94.f,  -93.f,  -92.f,  -91.f,  -90.f,  -89.f,
+     -88.f,  -87.f,  -86.f,  -85.f,  -84.f,  -83.f,  -82.f,  -81.f,
+     -80.f,  -79.f,  -78.f,  -77.f,  -76.f,  -75.f,  -74.f,  -73.f,
+     -72.f,  -71.f,  -70.f,  -69.f,  -68.f,  -67.f,  -66.f,  -65.f,
+     -64.f,  -63.f,  -62.f,  -61.f,  -60.f,  -59.f,  -58.f,  -57.f,
+     -56.f,  -55.f,  -54.f,  -53.f,  -52.f,  -51.f,  -50.f,  -49.f,
+     -48.f,  -47.f,  -46.f,  -45.f,  -44.f,  -43.f,  -42.f,  -41.f,
+     -40.f,  -39.f,  -38.f,  -37.f,  -36.f,  -35.f,  -34.f,  -33.f,
+     -32.f,  -31.f,  -30.f,  -29.f,  -28.f,  -27.f,  -26.f,  -25.f,
+     -24.f,  -23.f,  -22.f,  -21.f,  -20.f,  -19.f,  -18.f,  -17.f,
+     -16.f,  -15.f,  -14.f,  -13.f,  -12.f,  -11.f,  -10.f,   -9.f,
+      -8.f,   -7.f,   -6.f,   -5.f,   -4.f,   -3.f,   -2.f,   -1.f,
+       0.f,    1.f,    2.f,    3.f,    4.f,    5.f,    6.f,    7.f,
+       8.f,    9.f,   10.f,   11.f,   12.f,   13.f,   14.f,   15.f,
+      16.f,   17.f,   18.f,   19.f,   20.f,   21.f,   22.f,   23.f,
+      24.f,   25.f,   26.f,   27.f,   28.f,   29.f,   30.f,   31.f,
+      32.f,   33.f,   34.f,   35.f,   36.f,   37.f,   38.f,   39.f,
+      40.f,   41.f,   42.f,   43.f,   44.f,   45.f,   46.f,   47.f,
+      48.f,   49.f,   50.f,   51.f,   52.f,   53.f,   54.f,   55.f,
+      56.f,   57.f,   58.f,   59.f,   60.f,   61.f,   62.f,   63.f,
+      64.f,   65.f,   66.f,   67.f,   68.f,   69.f,   70.f,   71.f,
+      72.f,   73.f,   74.f,   75.f,   76.f,   77.f,   78.f,   79.f,
+      80.f,   81.f,   82.f,   83.f,   84.f,   85.f,   86.f,   87.f,
+      88.f,   89.f,   90.f,   91.f,   92.f,   93.f,   94.f,   95.f,
+      96.f,   97.f,   98.f,   99.f,  100.f,  101.f,  102.f,  103.f,
+     104.f,  105.f,  106.f,  107.f,  108.f,  109.f,  110.f,  111.f,
+     112.f,  113.f,  114.f,  115.f,  116.f,  117.f,  118.f,  119.f,
+     120.f,  121.f,  122.f,  123.f,  124.f,  125.f,  126.f,  127.f,
+     128.f,  129.f,  130.f,  131.f,  132.f,  133.f,  134.f,  135.f,
+     136.f,  137.f,  138.f,  139.f,  140.f,  141.f,  142.f,  143.f,
+     144.f,  145.f,  146.f,  147.f,  148.f,  149.f,  150.f,  151.f,
+     152.f,  153.f,  154.f,  155.f,  156.f,  157.f,  158.f,  159.f,
+     160.f,  161.f,  162.f,  163.f,  164.f,  165.f,  166.f,  167.f,
+     168.f,  169.f,  170.f,  171.f,  172.f,  173.f,  174.f,  175.f,
+     176.f,  177.f,  178.f,  179.f,  180.f,  181.f,  182.f,  183.f,
+     184.f,  185.f,  186.f,  187.f,  188.f,  189.f,  190.f,  191.f,
+     192.f,  193.f,  194.f,  195.f,  196.f,  197.f,  198.f,  199.f,
+     200.f,  201.f,  202.f,  203.f,  204.f,  205.f,  206.f,  207.f,
+     208.f,  209.f,  210.f,  211.f,  212.f,  213.f,  214.f,  215.f,
+     216.f,  217.f,  218.f,  219.f,  220.f,  221.f,  222.f,  223.f,
+     224.f,  225.f,  226.f,  227.f,  228.f,  229.f,  230.f,  231.f,
+     232.f,  233.f,  234.f,  235.f,  236.f,  237.f,  238.f,  239.f,
+     240.f,  241.f,  242.f,  243.f,  244.f,  245.f,  246.f,  247.f,
+     248.f,  249.f,  250.f,  251.f,  252.f,  253.f,  254.f,  255.f
+};
+    
+    void cv::cuda::split(InputArray, GpuMat*, Stream&) { throw_no_cuda(); }
+void cv::cuda::split(InputArray, std::vector<GpuMat>&, Stream&) { throw_no_cuda(); }
+    
+      ServerConfig server_config;
+  server_config.set_server_type(SYNC_SERVER);
+    
+    #ifndef GRPC_CORE_LIB_IOMGR_CALL_COMBINER_H
+#define GRPC_CORE_LIB_IOMGR_CALL_COMBINER_H
+    
+    static size_t batch_slot_for_op(grpc_op_type type) {
+  switch (type) {
+    case GRPC_OP_SEND_INITIAL_METADATA:
+      return 0;
+    case GRPC_OP_SEND_MESSAGE:
+      return 1;
+    case GRPC_OP_SEND_CLOSE_FROM_CLIENT:
+    case GRPC_OP_SEND_STATUS_FROM_SERVER:
+      return 2;
+    case GRPC_OP_RECV_INITIAL_METADATA:
+      return 3;
+    case GRPC_OP_RECV_MESSAGE:
+      return 4;
+    case GRPC_OP_RECV_CLOSE_ON_SERVER:
+    case GRPC_OP_RECV_STATUS_ON_CLIENT:
+      return 5;
+  }
+  GPR_UNREACHABLE_CODE(return 123456789);
+}
+    
+    namespace grpc {
+    }
+    
+    	btVector3 localInertia(0, 0, 0);
+    
+    	int ac = areas.size();
+	bool stopped = false;
+	gravity = Vector3(0, 0, 0);
+	area_linear_damp = 0;
+	area_angular_damp = 0;
+	if (ac) {
+		areas.sort();
+		const AreaCMP *aa = &areas[0];
+		// damp_area = aa[ac-1].area;
+		for (int i = ac - 1; i >= 0 && !stopped; i--) {
+			PhysicsServer::AreaSpaceOverrideMode mode = aa[i].area->get_space_override_mode();
+			switch (mode) {
+				case PhysicsServer::AREA_SPACE_OVERRIDE_COMBINE:
+				case PhysicsServer::AREA_SPACE_OVERRIDE_COMBINE_REPLACE: {
+					_compute_area_gravity_and_dampenings(aa[i].area);
+					stopped = mode == PhysicsServer::AREA_SPACE_OVERRIDE_COMBINE_REPLACE;
+				} break;
+				case PhysicsServer::AREA_SPACE_OVERRIDE_REPLACE:
+				case PhysicsServer::AREA_SPACE_OVERRIDE_REPLACE_COMBINE: {
+					gravity = Vector3(0, 0, 0);
+					area_angular_damp = 0;
+					area_linear_damp = 0;
+					_compute_area_gravity_and_dampenings(aa[i].area);
+					stopped = mode == PhysicsServer::AREA_SPACE_OVERRIDE_REPLACE;
+				} break;
+				default: {}
+			}
+		}
+	}
+    
+    	//ray hit something
+    
+    public:
+	virtual Vector3 get_total_gravity() const = 0;
+	virtual float get_total_angular_damp() const = 0;
+	virtual float get_total_linear_damp() const = 0;
+    
+    /** Function used by the error macros */
+    
+    #ifdef FACEBOOK
+  uint64_t time;
+  if (!fb_perf_get_thread_cputime_ns(&time)) {
+    time += s_extra_request_nanoseconds;
+    ts->tv_sec = time / sec_to_ns;
+    ts->tv_nsec = time % sec_to_ns;
+    return 0;
+  }
+#endif
+    
+      void addToPipeline(const std::string &s);
+    
+    
+    {
+    {      // If this address spans cache lines, on x64 this is not an atomic store.
+      // This being the case, use caution when overwriting code that is
+      // reachable by multiple threads: make sure it doesn't span cache lines.
+      *reinterpret_cast<uint64_t*>(dest()) = u.qword;
+    } else {
+      memcpy(dest(), bs, n);
+    }
+    m_frontier += n;
+  }
+    
+    
+    {}
+    
+    template<class Fn> void FOLLY_DISABLE_ADDRESS_SANITIZER
+conservativeScan(const void* start, size_t len, Fn fn) {
+  const uintptr_t M{7}; // word size - 1
+  auto s = (const void**)((uintptr_t(start) + M) & ~M); // round up
+  auto e = (const void**)((uintptr_t(start) + len) & ~M); // round down
+  for (; s < e; s++) {
+    // Mask off the upper 16-bits to handle things like
+    // DiscriminatedPtr which stores things up there.
+    fn(s, (const void*)(uintptr_t(*s) & (-1ULL >> 16)));
+  }
+}
+    
+      // Add the specified file at the specified number.
+  // REQUIRES: This version has not been saved (see VersionSet::SaveTo)
+  // REQUIRES: 'smallest' and 'largest' are smallest and largest keys in file
+  void AddFile(int level, uint64_t file,
+               uint64_t file_size,
+               const InternalKey& smallest,
+               const InternalKey& largest) {
+    FileMetaData f;
+    f.number = file;
+    f.file_size = file_size;
+    f.smallest = smallest;
+    f.largest = largest;
+    new_files_.push_back(std::make_pair(level, f));
+  }
+    
+    static void TestEncodeDecode(const VersionEdit& edit) {
+  std::string encoded, encoded2;
+  edit.EncodeTo(&encoded);
+  VersionEdit parsed;
+  Status s = parsed.DecodeFrom(encoded);
+  ASSERT_TRUE(s.ok()) << s.ToString();
+  parsed.EncodeTo(&encoded2);
+  ASSERT_EQ(encoded, encoded2);
+}
+    
+    #include 'leveldb/db.h'
+#include 'util/testharness.h'
+    
+      // Check second filter
+  ASSERT_TRUE(reader.KeyMayMatch(3100, 'box'));
+  ASSERT_TRUE(! reader.KeyMayMatch(3100, 'foo'));
+  ASSERT_TRUE(! reader.KeyMayMatch(3100, 'bar'));
+  ASSERT_TRUE(! reader.KeyMayMatch(3100, 'hello'));
+    
+    
+    {  unsigned char data[48] = {
+    0x01, 0xc0, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0x14, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x04, 0x00,
+    0x00, 0x00, 0x00, 0x14,
+    0x00, 0x00, 0x00, 0x18,
+    0x28, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+    0x02, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00,
+  };
+  ASSERT_EQ(0xd9963a56, Value(reinterpret_cast<char*>(data), sizeof(data)));
+}
+    
+    int main(int argc, char** argv) {
+  return leveldb::test::RunAllTests();
+}
+
+    
+    
+    {}  // namespace leveldb
