310caebf9c6103531551eeebef32f6c5eb8e2192
SUNsung <weatherius@gmail.com>
2018-08-15 07:22:44 +0300

    Rewrite [05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb] File in: 2018-08-15 07:22:44

--- ruby/05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb
+++ ruby/05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb
@@ -1,40 +1,111 @@
 
         
-                def redis_connection
-          self.class.redis_connector.call(@server.config.cable)
-        end
+                            if callbacks = @subscribe_callbacks[chan]
+                      next_callback = callbacks.shift
+                      @event_loop.post(&next_callback) if next_callback
+                      @subscribe_callbacks.delete(chan) if callbacks.empty?
+                    end
+                  end
+                end
     
-        def build_from_hash(attributes)
-      attributes = convert_dot_notation_to_hash(attributes)
-      expand_from_hash(attributes)
-    end
+    class TestDefaultAutosaveAssociationOnNewRecord < ActiveRecord::TestCase
+  def test_autosave_new_record_on_belongs_to_can_be_disabled_per_relationship
+    new_account = Account.new('credit_limit' => 1000)
+    new_firm = Firm.new('name' => 'some firm')
     
-      def test_should_save_changed_has_one_changed_object_if_child_is_saved
-    @pirate.ship.name = 'NewName'
-    assert @pirate.save
-    assert_equal 'NewName', @pirate.ship.reload.name
+    require 'models/topic'
+    
+      after_touch do
+    self.after_touch_called += 1
   end
     
-      has_many :replies, dependent: :destroy, foreign_key: 'parent_id', autosave: true
-  has_many :approved_replies, -> { approved }, class_name: 'Reply', foreign_key: 'parent_id', counter_cache: 'replies_count'
+      test 'broadcasts_to' do
+    assert_called_with(
+      ActionCable.server,
+      :broadcast,
+      [
+        'action_cable:channel:broadcasting_test:chat:Room#1-Campfire',
+        'Hello World'
+      ]
+    ) do
+      ChatChannel.broadcast_to(Room.new(1), 'Hello World')
+    end
+  end
     
-      test 'on connection close' do
-    run_in_eventmachine do
-      connection = open_connection
-      connection.process
+      setup do
+    @user = User.new 'lifo'
+    @connection = TestConnection.new(@user)
+  end
     
-      private
-    def open_connection
-      env = Rack::MockRequest.env_for '/test',
-        'HTTP_CONNECTION' => 'upgrade', 'HTTP_UPGRADE' => 'websocket',
-        'HTTP_HOST' => 'localhost', 'HTTP_ORIGIN' => 'http://rubyonrails.com'
-      env['rack.hijack'] = -> { env['rack.hijack_io'] = StringIO.new }
+      setup do
+    @server = TestServer.new
+    @server.config.allowed_request_origins = %w( http://rubyonrails.com )
+  end
     
-      test 'broadcaster from broadcaster_for generates notification' do
-    begin
-      server = TestServer.new
+    class RedisAdapterTest::AlternateConfiguration < RedisAdapterTest
+  def cable_config
+    alt_cable_config = super.dup
+    alt_cable_config.delete(:url)
+    alt_cable_config.merge(host: '127.0.0.1', port: 6379, db: 12)
+  end
+end
     
-        private
     
-    		elsif line =~ /\w+:\t/
-			block_do(line)
+# replace calls, jmps, and read/write handle/filename references
+replaces = []
+asm.each_line { |ln|
+	if ln =~ /call /
+		parts = ln.split(' ')
+		if (parts[0] == 'call' and parts[2] == ';call')
+			old = parts[1]
+			func = parts[3]
+			new = addrs[func]
+			#puts '%32s: %s -> %x' % [func, old, new]
+			replaces << [func, old, new.to_s(16)]
+		end
+	end
+    }
+    
+    meterp.sock.close
+
+    
+      def get_result
+    @src.get_result
+  end
+end
+    
+    def config_tag(config, key, tag=nil, classname=nil)
+  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
+  tag       ||= 'div'
+  classname ||= key.sub(/_/, '-').sub(/\./, '-')
+  output      = '<#{tag} class='#{classname}''
+    
+        def render(context)
+      includes_dir = File.join(context.registers[:site].source, '_includes')
+    
+      class IncludeCodeTag < Liquid::Tag
+    def initialize(tag_name, markup, tokens)
+      @title = nil
+      @file = nil
+      if markup.strip =~ /\s*lang:(\S+)/i
+        @filetype = $1
+        markup = markup.strip.sub(/lang:\S+/i,'')
+      end
+      if markup.strip =~ /(.*)?(\s+|^)(\/*\S+)/i
+        @title = $1 || nil
+        @file = $3
+      end
+      super
+    end
+    
+      class PageFilters < Octopress::Hooks::Page
+    def pre_render(page)
+      OctopressFilters::pre_filter(page)
+    end
+    
+              if @address.update_attributes(address_params)
+            respond_with(@address, default_template: :show)
+          else
+            invalid_resource!(@address)
+          end
+        end
