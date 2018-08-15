0a22684f8974f6a694344da07307bc286070b7f3
SUNsung <weatherius@gmail.com>
2018-08-15 08:19:03 +0300

    Rewrite [223VfZDu9R5co22y302JzHZIU22JjXIyCQBhg22uUX.rb] File in: 2018-08-15 08:19:02

--- ruby/223VfZDu9R5co22y302JzHZIU22JjXIyCQBhg22uUX.rb
+++ ruby/223VfZDu9R5co22y302JzHZIU22JjXIyCQBhg22uUX.rb
@@ -1,51 +1,56 @@
 
         
-        # No trailing slash
-Benchmark.ips do |x|
-  x.report('with body include?') { CONTENT_CONTAINING.include?('<body') }
-  x.report('with body regexp')   { CONTENT_CONTAINING =~ /<\s*body/ }
-  x.compare!
-end
-
-    
-    module Jekyll
-  module Converters
-    class Markdown < Converter
-      highlighter_prefix '\n'
-      highlighter_suffix '\n'
-      safe true
-    
-          # return nil for `{{ site.config }}` even if --config was passed via CLI
-      def config; end
-    end
+          it 'considers a non-nil and non-boolean object in expression result as true' do
+    if mock('x')
+      123
+    else
+      456
+    end.should == 123
   end
-end
-
     
-          def fallback_data
-        @fallback_data ||= {}
+      def test_flip_flop
+    eval <<-END
+    assert_equal [4,5], (1..9).select {|n| true if (n==4)..(n==5)}
+    assert_equal [4,5], (1..9).select {|n| true if (n==4)...(n==5)}
+    assert_equal [2], (1..9).select {|n| true if (n==2)..(n%2).zero?}
+    assert_equal [2,3,4], (1..9).select {|n| true if (n==2)...(n%2).zero?}
+    assert_equal [4,5,7,8], (1..9).select {|n| true if (n==4)...(n==5) or (n==7)...(n==8)}
+    assert_equal [nil, 2, 3, 4, nil], (1..5).map {|x| x if (x==2..x==4)}
+    assert_equal [1, nil, nil, nil, 5], (1..5).map {|x| x if !(x==2..x==4)}
+    END
+  end
+    
+      def test_download_to_cache
+    @a2, @a2_gem = util_gem 'a', '2'
+    
+            File.open(@path,'a').close unless File.exist?(@path)
+        reload
+      end
+    
+            def operator_assignment?
+          return false unless meta_assignment_node
+          OPERATOR_ASSIGNMENT_TYPES.include?(meta_assignment_node.type)
+        end
+    
+            def autocorrect(node)
+          center = multiple_compare?(node)
+          new_center = '#{center.source} && #{center.source}'
+    
+              add_offense(node)
+        end
+    
+            attr_reader :lines, :annotations
       end
     end
   end
 end
 
     
-      def initialize(f)
-    @f = f
-  end
-    
-        dirs.each do |d|
-      files = []
-      d.find { |pn| files << pn unless pn.directory? }
-      print_remaining_files files, d
-    end
-    
-      included do
-    scope :admins, -> { where(admin: true) }
-    scope :moderators, -> { where(moderator: true) }
-    scope :staff, -> { where('moderator or admin ') }
-  end
-    
-    ###
-### globals
-###
+    module RuboCop
+  module AST
+    # A node extension for `def` nodes. This will be used in place of a plain
+    # node when the builder constructs the AST, making its methods available
+    # to all `def` nodes within RuboCop.
+    class DefNode < Node
+      include ParameterizedNode
+      include MethodIdentifierPredicates
