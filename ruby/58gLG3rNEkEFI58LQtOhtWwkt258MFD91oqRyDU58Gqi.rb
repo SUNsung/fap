f16db1e865be706177f6df2d0400f0c023345411
SUNsung <weatherius@gmail.com>
2018-08-16 19:40:50 +0300

    Rewrite [58gLG3rNEkEFI58LQtOhtWwkt258MFD91oqRyDU58Gqi.rb] File in: 2018-08-16 19:40:50

--- ruby/58gLG3rNEkEFI58LQtOhtWwkt258MFD91oqRyDU58Gqi.rb
+++ ruby/58gLG3rNEkEFI58LQtOhtWwkt258MFD91oqRyDU58Gqi.rb
@@ -1,96 +1,51 @@
 
         
-        def bottle_resolve_formula_names(bottle_file)
-  receipt_file_path = bottle_receipt_path bottle_file
-  receipt_file = Utils.popen_read('tar', '-xOzf', bottle_file, receipt_file_path)
-  name = receipt_file_path.split('/').first
-  tap = Tab.from_file_content(receipt_file, '#{bottle_file}/#{receipt_file_path}').tap
+          # PUT /resource/password
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+    yield resource if block_given?
     
-      # True if a {Formula} is being built with {Formula.devel} instead of {Formula.stable}.
-  # <pre>args << '--some-beta' if build.devel?</pre>
-  def devel?
-    include? 'devel'
-  end
-    
-        # Remove directories opposite from traversal, so that a subtree with no
-    # actual files gets removed correctly.
-    dirs.reverse_each do |d|
-      if d.children.empty?
-        puts 'rmdir: #{d} (empty)' if ARGV.verbose?
-        d.rmdir
-      end
+        if successfully_sent?(resource)
+      respond_with({}, location: after_sending_unlock_instructions_path_for(resource))
+    else
+      respond_with(resource)
     end
-    
-      private
-    
-      def macports_or_fink
-    @ponk ||= MacOS.macports_or_fink
-    @ponk.join(', ') unless @ponk.empty?
   end
     
-        checks.inject_dump_stats! if ARGV.switch? 'D'
+          included do
+        include Devise::Controllers::ScopedViews
+      end
     
-      def patches
-    {}
-  end
+        # If {#limit_to_network} is disabled, this will always return `true`.
+    # Otherwise, return `true` only if all of the given IPs are within the
+    # project {#boundary boundaries}.
     
-                  s[:proto]='pop3'
-              s[:extra]='Failed Login. Banner: #{s[:banner]}'
-              report_auth_info(s)
-              print_status('Invalid POP3 Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]} (#{s[:banner].strip})')
-              s[:pass]=''
-          end
-        when nil
-          # No matches, no saved state
-        else
-          s[:last]=matched
-          sessions[s[:session]].merge!({k => matches})
-      end # end case matched
-    end # end of each_key
-  end # end of parse
+    begin
+  require 'bundler/setup'
+rescue LoadError
+  $stderr.puts '[*] Metasploit requires the Bundler gem to be installed'
+  $stderr.puts '    $ gem install bundler'
+  exit(1)
 end
     
-    sock = TCPSocket.new(ip, port)
+    $stdout.puts res
+
     
-    keytoolOpts 	= ['-genkey', '-alias', 'signFiles', '-keystore', 'msfkeystore',
-		   '-storepass', 'msfstorepass', '-dname', 'cn=#{certCN}',
-		   '-keypass', 'msfkeypass']
+    success = clsJavaCompile._invoke('CompileFromMemory','[Ljava.lang.String;[Ljava.lang.String;[Ljava.lang.String;', classNames, codez, compileOpts)
     
-        def names_for(klass)
-      @attachments[klass].keys
-    end
+    if $PROGRAM_NAME == __FILE__ && !ENV['COCOAPODS_NO_BUNDLER']
+  ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
+  require 'rubygems'
+  require 'bundler/setup'
+  $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+elsif ENV['COCOAPODS_NO_BUNDLER']
+  require 'rubygems'
+  gem 'cocoapods'
+end
     
-        # Extracts the Geometry from a 'WxH,O' string
-    # Where W is the width, H is the height,
-    # and O is the EXIF orientation
-    def self.parse(string)
-      GeometryParser.new(string).make
-    end
+            private
     
-    module Paperclip
-  class << self
-    attr_writer :registered_attachments_styles_path
-    def registered_attachments_styles_path
-      @registered_attachments_styles_path ||= Rails.root.join('public/system/paperclip_attachments.yml').to_s
-    end
-  end
-    
-        module HelperMethods
-      # Places ActiveModel validations on the name of the file
-      # assigned. The possible options are:
-      # * +matches+: Allowed filename patterns as Regexps. Can be a single one
-      #   or an array.
-      # * +not+: Forbidden file name patterns, specified the same was as +matches+.
-      # * +message+: The message to display when the uploaded file has an invalid
-      #   name.
-      # * +if+: A lambda or name of an instance method. Validation will only
-      #   be run is this lambda or method returns true.
-      # * +unless+: Same as +if+ but validates if lambda or method returns false.
-      def validates_attachment_file_name(*attr_names)
-        options = _merge_attributes(attr_names)
-        validates_with AttachmentFileNameValidator, options.dup
-        validate_before_processing AttachmentFileNameValidator, options.dup
-      end
+          #-----------------------------------------------------------------------#
     end
   end
 end
