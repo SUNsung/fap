8b2517ef3463be75f96fed400af8dc70908bcd96
SUNsung <weatherius@gmail.com>
2018-08-16 05:37:18 +0300

    Rewrite [270Apu.y5CMVE27EqENz6Dt8oE27V6CzyxTS3m627r5V.py] File in: 2018-08-16 05:37:17

--- pyton/270Apu.y5CMVE27EqENz6Dt8oE27V6CzyxTS3m627r5V.py
+++ pyton/270Apu.y5CMVE27EqENz6Dt8oE27V6CzyxTS3m627r5V.py
@@ -1,24 +1,33 @@
 
         
-        site_info = 'CBS.com'
-download = cbs_download
-download_playlist = playlist_not_supported('cbs')
-
+        
+class Cache(object):
     
-    site_info = 'ehow.com'
-download = ehow_download
-download_playlist = playlist_not_supported('ehow')
-
+        def can_fit_in_spot(self, spot):
+        return True
     
-    import json
-import os
-import re
-import math
-import traceback
-import urllib.parse as urlparse
+        def extract_year_month(self, timestamp):
+        '''Return the year and month portions of the timestamp.'''
+        ...
     
-            if s: self.streams['video'] = {'url': s }
-        if mp3: self.streams['audio'] = { 'url': mp3 }
-        if pdf: self.streams['slides'] = { 'url': pdf }
+        def remove_link_to_crawl(self, url):
+        '''Remove the given link from `links_to_crawl`.'''
+        pass
     
+        def __repr__(self):
+        cls_name = self.__class__.__name__
+        return '%s(concurrency=%r, delay=%0.2f, randomize_delay=%r)' % (
+            cls_name, self.concurrency, self.delay, self.randomize_delay)
     
+            self.factory.noPage(\
+                defer.TimeoutError('Getting %s took longer than %s seconds.' % \
+                (self.factory.url, self.factory.timeout)))
+    
+            # XXX: Google parses at least first 100k bytes; scrapy's redirect
+        # middleware parses first 4k. 4k turns out to be insufficient
+        # for this middleware, and parsing 100k could be slow.
+        # We use something in between (32K) by default.
+        self.lookup_bytes = settings.getint('AJAXCRAWL_MAXSIZE', 32768)
+    
+            respcls = responsetypes.from_args(body=body)
+        return response.replace(body=body, cls=respcls)
