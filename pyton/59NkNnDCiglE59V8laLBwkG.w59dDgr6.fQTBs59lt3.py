fdde5b8c54e489e2411a5801a3b67252e984f1c2
SUNsung <weatherius@gmail.com>
2018-08-16 22:47:21 +0300

    Rewrite [59NkNnDCiglE59V8laLBwkG.w59dDgr6.fQTBs59lt3.py] File in: 2018-08-16 22:47:20

--- pyton/59NkNnDCiglE59V8laLBwkG.w59dDgr6.fQTBs59lt3.py
+++ pyton/59NkNnDCiglE59V8laLBwkG.w59dDgr6.fQTBs59lt3.py
@@ -1,92 +1,153 @@
 
         
-            tag = to_json
+                app.config.from_pyfile('yourconfig.cfg')
     
+        if blueprint is not None and seems_fishy:
+        info.append('  The template was looked up from an endpoint that '
+                    'belongs to the blueprint '%s'.' % blueprint)
+        info.append('  Maybe you did not place a template in the right folder?')
+        info.append('  See http://flask.pocoo.org/docs/blueprints/#templates')
     
-def test_get_namespace():
-    app = flask.Flask(__name__)
-    app.config['FOO_OPTION_1'] = 'foo option 1'
-    app.config['FOO_OPTION_2'] = 'foo option 2'
-    app.config['BAR_STUFF_1'] = 'bar stuff 1'
-    app.config['BAR_STUFF_2'] = 'bar stuff 2'
-    foo_options = app.config.get_namespace('FOO_')
-    assert 2 == len(foo_options)
-    assert 'foo option 1' == foo_options['option_1']
-    assert 'foo option 2' == foo_options['option_2']
-    bar_options = app.config.get_namespace('BAR_', lowercase=False)
-    assert 2 == len(bar_options)
-    assert 'bar stuff 1' == bar_options['STUFF_1']
-    assert 'bar stuff 2' == bar_options['STUFF_2']
-    foo_options = app.config.get_namespace('FOO_', trim_namespace=False)
-    assert 2 == len(foo_options)
-    assert 'foo option 1' == foo_options['foo_option_1']
-    assert 'foo option 2' == foo_options['foo_option_2']
-    bar_options = app.config.get_namespace('BAR_', lowercase=False, trim_namespace=False)
-    assert 2 == len(bar_options)
-    assert 'bar stuff 1' == bar_options['BAR_STUFF_1']
-    assert 'bar stuff 2' == bar_options['BAR_STUFF_2']
-    
-            (2016-01, shopping), 25
-        (2016-01, shopping), 100
-        (2016-01, gas), 50
-        '''
-        timestamp, category, amount = line.split('\t')
-        period = self. extract_year_month(timestamp)
-        if period == self.current_year_month():
-            yield (period, category), amount
-    
-    
-def rst_filenames():
-    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
-        if '.tox' not in root:
-            for filename in fnmatch.filter(filenames, '*.rst'):
-                yield os.path.join(root, filename)
-    
-    
-class BaseConfigDict(dict):
-    
-    
-def test_current_version():
-    version = Environment().config['__meta__']['httpie']
-    assert version == __version__
-
-    
-        def test_format_option(self, httpbin):
-        env = MockEnvironment(colors=256)
-        r = http('--print=B', '--pretty=format',
-                 'GET', httpbin.url + '/get', 'a=b',
-                 env=env)
-        # Tests that the JSON data is formatted.
-        assert r.strip().count('\n') == 2
-        assert COLOR not in r
-    
-            if args.coverage_results:
-            path = os.path.join(output_dir, '%s/coverage.json' % job_number)
-            url = 'https://api.shippable.com/jobs/%s/jobCoverageReports' % job_id
-            download(args, headers, path, url)
-            extract_contents(args, path, os.path.join(output_dir, '%s/coverage' % job_number))
-    
-            input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/targetHttpProxies'
-        actual = GCPUtils.parse_gcp_url(input_url)
-        self.assertEquals('compute', actual['service'])
-        self.assertEquals('v1', actual['api_version'])
-        self.assertEquals('myproject', actual['project'])
-        self.assertFalse('global' in actual)
-        self.assertEquals('targetHttpProxies', actual['resource_name'])
-    
-    
-class Controller(object):
-    
-    - single source 'message type' for state transition changes
-- message type considered, messages (comment) not considered to avoid complexity
+    :copyright: © 2010 by the Pallets team.
+:license: BSD, see LICENSE for more details.
 '''
     
-    class ParameterInjectionTest(unittest.TestCase):
     
-    class ParameterInjectionTest(unittest.TestCase):
+class SessionMixin(MutableMapping):
+    '''Expands a basic dictionary with session attributes.'''
     
-    ### OUTPUT ###
-# Counting to two...
-# one two
-# Counting to five...
-# one two three four five
+        def inject_version(match):
+        before, old, after = match.groups()
+        changed.append(True)
+        return before + version_number + after
+    
+    
+@pytest.mark.skipif(not PY2, reason='This only works under Python 2.')
+def test_meta_path_loader_without_is_package(request, modules_tmpdir):
+    app = modules_tmpdir.join('unimportable.py')
+    app.write('import flask\napp = flask.Flask(__name__)')
+    
+        response = HttpResponse(template.render({'flatpage': f}, request))
+    return response
+
+    
+    
+###############################################################################
+# Data
+    
+    from sklearn.utils import check_random_state
+from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.metrics.pairwise import pairwise_kernels
+    
+    import numpy as np
+from scipy.cluster import hierarchy
+import matplotlib.pyplot as plt
+    
+    Benchmarks for random projections.
+    
+    for f in solutions:
+    if not f.endswith('.py'):
+        continue
+    
+    '''
+print(__doc__)
+    
+    acc_clf1, acc_clf2 = [], []
+n_features_range = range(1, n_features_max + 1, step)
+for n_features in n_features_range:
+    score_clf1, score_clf2 = 0, 0
+    for _ in range(n_averages):
+        X, y = generate_data(n_train, n_features)
+    
+        def _list_templates(self):
+        print('Available templates:')
+        for filename in sorted(os.listdir(self.templates_dir)):
+            if filename.endswith('.tmpl'):
+                print('  %s' % splitext(filename)[0])
+    
+        def _add_middleware(self, mw):
+        if hasattr(mw, 'process_request'):
+            self.methods['process_request'].append(mw.process_request)
+        if hasattr(mw, 'process_response'):
+            self.methods['process_response'].insert(0, mw.process_response)
+        if hasattr(mw, 'process_exception'):
+            self.methods['process_exception'].insert(0, mw.process_exception)
+    
+        def log(self, request, spider):
+        if self.debug:
+            msg = 'Filtered duplicate request: %(request)s'
+            self.logger.debug(msg, {'request': request}, extra={'spider': spider})
+        elif self.logdupes:
+            msg = ('Filtered duplicate request: %(request)s'
+                   ' - no more duplicates will be shown'
+                   ' (see DUPEFILTER_DEBUG to show all duplicates)')
+            self.logger.debug(msg, {'request': request}, extra={'spider': spider})
+            self.logdupes = False
+    
+        # Gloss the lips
+    d.polygon(face_landmarks['top_lip'], fill=(150, 0, 0, 128))
+    d.polygon(face_landmarks['bottom_lip'], fill=(150, 0, 0, 128))
+    d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=8)
+    d.line(face_landmarks['bottom_lip'], fill=(150, 0, 0, 64), width=8)
+    
+    print('I found {} face(s) in this photograph.'.format(len(face_locations)))
+    
+        # If no valid image file was uploaded, show the file upload form:
+    return '''
+    <!doctype html>
+    <title>Is this a picture of Obama?</title>
+    <h1>Upload a picture and see if it's a picture of Obama!</h1>
+    <form method='POST' enctype='multipart/form-data'>
+      <input type='file' name='file'>
+      <input type='submit' value='Upload'>
+    </form>
+    '''
+    
+        # Find encodings for faces in the test iamge
+    faces_encodings = face_recognition.face_encodings(X_img, known_face_locations=X_face_locations)
+    
+    setup(
+    name='face_recognition',
+    version='1.2.2',
+    description='Recognize faces from Python or from the command line',
+    long_description=readme + '\n\n' + history,
+    author='Adam Geitgey',
+    author_email='ageitgey@gmail.com',
+    url='https://github.com/ageitgey/face_recognition',
+    packages=[
+        'face_recognition',
+    ],
+    package_dir={'face_recognition': 'face_recognition'},
+    package_data={
+        'face_recognition': ['models/*.dat']
+    },
+    entry_points={
+        'console_scripts': [
+            'face_recognition=face_recognition.face_recognition_cli:main',
+            'face_detection=face_recognition.face_detection_cli:main'
+        ]
+    },
+    install_requires=requirements,
+    license='MIT license',
+    zip_safe=False,
+    keywords='face_recognition',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
+
+    
+            result = runner.invoke(face_recognition_cli.main, args=[image_folder, image_file, '--tolerance', '0.55'])
