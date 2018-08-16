b09afc8a16bb432887ac8c4a0335f5afcaf52418
SUNsung <weatherius@gmail.com>
2018-08-16 11:13:06 +0300

    Rewrite [008247zzLmnOw00508iYMFKcrA00AFWXsj2P2Mc00aQ.py] File in: 2018-08-16 11:13:06

--- pyton/008247zzLmnOw00508iYMFKcrA00AFWXsj2P2Mc00aQ.py
+++ pyton/008247zzLmnOw00508iYMFKcrA00AFWXsj2P2Mc00aQ.py
@@ -1,165 +1,60 @@
 
         
-        header = oldreadme[:oldreadme.index('# OPTIONS')]
-footer = oldreadme[oldreadme.index('# CONFIGURATION'):]
+            def test_write_eof_duplex_pipe(self):
+        tr = _ProactorDuplexPipeTransport(
+            self.loop, self.sock, self.protocol)
+        self.assertFalse(tr.can_write_eof())
+        with self.assertRaises(NotImplementedError):
+            tr.write_eof()
+        close_transport(tr)
     
-        return sorted(
-        filter(lambda ie: ie.is_suitable(age_limit), gen_extractors()),
-        key=lambda ie: ie.IE_NAME.lower())
-    
-            formats = [
-            {
-                'format_id': f['type'],
-                'filesize': int(f['filesize']),
-                'url': f['url']
-            } for f in info['rfiles']
-        ]
-        self._sort_formats(formats)
-    
-    tf_proto_library_cc(
-    name = 'cell_trace_proto',
-    srcs = ['cell_trace.proto'],
-    protodeps = [':trace_proto'],
-)
-    
-        check.Gt(self._num_labels, 0, 'Expected some labels')
-    check.Eq(len(self._fixed_feature_dims.items()), 0,
-             'Expected no fixed features')
-    check.Eq(len(self._linked_feature_dims.items()), 2,
-             'Expected two linked features')
-    
-        Args:
-      state: MasterState from the 'AdvanceMaster' op that advances the
-          underlying master to this component.
-      network_states: dictionary of component NetworkState objects
-      during_training: whether the graph is being constructed during training
+        def test_object_class_assignment_between_heaptypes_and_nonheaptypes(self):
+        class SubType(types.ModuleType):
+            a = 1
     
     
-class BulkComponentTest(test_util.TensorFlowTestCase):
+try:
+    import _hashlib
+    new = __hash_new
+    __get_hash = __get_openssl_constructor
+    algorithms_available = algorithms_available.union(
+            _hashlib.openssl_md_meth_names)
+except ImportError:
+    new = __py_new
+    __get_hash = __get_builtin_constructor
     
+        print('Creating the makefiles...')
+    sys.stdout.flush()
+    # run configure, copy includes, patch files
+    run_configure(configure, do_script)
+    makefile = makefile_template.format(suffix)
+    try:
+        os.unlink(makefile)
+    except FileNotFoundError:
+        pass
+    os.rename(generated_makefile, makefile)
+    copy_includes(makefile, suffix)
     
-if __name__ == '__main__':
-  app.run(main)
-
+        def tearDown(cls):
+        ''' Function/test case scope teardown. '''
+        cls.output.close()
+        sys.stdout = cls.saved_stdout
     
-          # As should AttachDataReader.
-      path = _find_input_path_to_type(path[-1], 'AttachDataReader')
-      self.assertNotEmpty(path)
+        def amount(self, val):
+        print(val, end=' ')
+        return self
     
-      Computing the sum-of-product in the log domain reduces the chance of overflow
-  or underflow, and ML techniques (e.g., CRF loss functions) typically require
-  the log partition function anyways.  For similar reasons, the scores input is
-  assumed to be specified in the log domain.
-    
-            for label in ax.get_xticklabels():
-            label.set_rotation(-90)
-            label.set_fontsize(10)
-    
-    for i, n in enumerate(n_samples):
-    for j, p in enumerate(n_features):
-        X = np.random.normal(size=(n, p))
-        t0 = time.time()
-        ward.fit(X)
-        scikits_time[j, i] = time.time() - t0
-        t0 = time.time()
-        hierarchy.ward(X)
-        scipy_time[j, i] = time.time() - t0
-    
-        # op.add_option('--random-seed',
-    #               dest='random_seed', default=13, type=int,
-    #               help='Seed used by the random number generators.')
-    
-    pages = {
-    u'ar': u'http://ar.wikipedia.org/wiki/%D9%88%D9%8A%D9%83%D9%8A%D8%A8%D9%8A%D8%AF%D9%8A%D8%A7',
-    u'de': u'http://de.wikipedia.org/wiki/Wikipedia',
-    u'en': u'https://en.wikipedia.org/wiki/Wikipedia',
-    u'es': u'http://es.wikipedia.org/wiki/Wikipedia',
-    u'fr': u'http://fr.wikipedia.org/wiki/Wikip%C3%A9dia',
-    u'it': u'http://it.wikipedia.org/wiki/Wikipedia',
-    u'ja': u'http://ja.wikipedia.org/wiki/Wikipedia',
-    u'nl': u'http://nl.wikipedia.org/wiki/Wikipedia',
-    u'pl': u'http://pl.wikipedia.org/wiki/Wikipedia',
-    u'pt': u'http://pt.wikipedia.org/wiki/Wikip%C3%A9dia',
-    u'ru': u'http://ru.wikipedia.org/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D0%BF%D0%B5%D0%B4%D0%B8%D1%8F',
-#    u'zh': u'http://zh.wikipedia.org/wiki/Wikipedia',
-}
-    
-        # the training data folder must be passed as first argument
-    movie_reviews_data_folder = sys.argv[1]
-    dataset = load_files(movie_reviews_data_folder, shuffle=False)
-    print('n_samples: %d' % len(dataset.data))
-    
-        for line in input_file:
-        linestrip = line.strip()
-        if len(linestrip) == 0:
-            in_exercise_region = False
-        elif linestrip.startswith('# TASK:'):
-            in_exercise_region = True
-    
-    print('Log-loss of')
-print(' * uncalibrated classifier trained on 800 datapoints: %.3f '
-      % score)
-print(' * classifier trained on 600 datapoints and calibrated on '
-      '200 datapoint: %.3f' % sig_score)
-    
-    # Generate waveform data
-n_features = 2000
-t = np.pi * np.linspace(0, 1, n_features)
-    
-    import numpy as np
-import matplotlib.pyplot as plt
-from scipy.linalg import toeplitz, cholesky
-    
-    from twisted import version as _txv
-twisted_version = (_txv.major, _txv.minor, _txv.micro)
-    
-            infos = []
-        if not self.wasSuccessful():
-            write('FAILED')
-            failed, errored = map(len, (self.failures, self.errors))
-            if failed:
-                infos.append('failures=%d' % failed)
-            if errored:
-                infos.append('errors=%d' % errored)
-        else:
-            write('OK')
-    
-    
-def sanitize_module_name(module_name):
-    '''Sanitize the given module name, by replacing dashes and points
-    with underscores and prefixing it with a letter if it doesn't start
-    with one
     '''
-    module_name = module_name.replace('-', '_').replace('.', '_')
-    if module_name[0] not in string.ascii_letters:
-        module_name = 'a' + module_name
-    return module_name
+Port of the Java example of 'Parameter Injection' in
+'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
+(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
+http://xunitpatterns.com/Dependency%20Injection.html.
     
-        @classmethod
-    def _get_mwlist_from_settings(cls, settings):
-        return build_component_list(
-            settings.getwithbase('DOWNLOADER_MIDDLEWARES'))
+    '''
+Port of the Java example of 'Parameter Injection' in
+'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
+(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
+http://xunitpatterns.com/Dependency%20Injection.html.
     
-            # extract cookies from Set-Cookie and drop invalid/expired cookies
-        cookiejarkey = request.meta.get('cookiejar')
-        jar = self.jars[cookiejarkey]
-        jar.extract_cookies(response, request)
-        self._debug_set_cookie(response, spider)
-    
-        def contribute(self):
-        self.blackboard.common_state['problems'] += random.randint(1, 10)
-        self.blackboard.common_state['suggestions'] += random.randint(1, 10)
-        self.blackboard.common_state['contributions'] += [self.__class__.__name__]
-        self.blackboard.common_state['progress'] += random.randint(1, 2)
-    
-    
-class Provider:
-    
-        def test_display_current_time_at_midnight(self):
-        class_under_test = TimeDisplay()
-        expected_time = '24:01'
-        result = class_under_test.get_current_time_as_as_html_fragment()
-        self.assertEqual(result, expected_time)
-'''
-    
-            return transaction
+        _static_method_choices = {'param_value_1': _static_method_1,
+                              'param_value_2': _static_method_2}
