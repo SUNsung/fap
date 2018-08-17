9e7b30b972504b18789537dbb1150d487cfce768
SUNsung <weatherius@gmail.com>
2018-08-17 10:31:59 +0300

    Rewrite [35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py] File in: 2018-08-17 10:31:58

--- pyton/35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py
+++ pyton/35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py
@@ -1,206 +1,240 @@
 
         
-            @property
-    def content_type(self):
-        '''Return the message content type.'''
-        ct = self._orig.headers.get('Content-Type', '')
-        if not isinstance(ct, str):
-            ct = ct.decode('utf8')
-        return ct
+                scores = tf.log(scores_raw)
+        init_scores = np.log(np.array(scores_raw))
+        num_nodes = tf.constant([2, 3, 4], tf.int32)
     
-    error_msg = None
+      def _compute_m3n_loss(self, lengths, scores, gold):
+    '''Computes the M3N-style structured hinge loss for a batch.'''
+    # Perform hamming-loss-augmented inference.
+    gold_scores_b = tf.reduce_sum(scores * gold, axis=[1, 2])
+    hamming_loss_bxnxn = 1 - gold
+    scores_bxnxn = scores + hamming_loss_bxnxn
+    max_scores_b, _ = mst_ops.maximum_spanning_tree(
+        num_nodes=lengths, scores=scores_bxnxn, forest=self._attrs['forest'])
+    return max_scores_b - gold_scores_b
     
+          # Don't add a bias term if we're going to apply layer norm, since layer
+      # norm includes a bias already.
+      if index == 0 and self._layer_norm_hidden:
+        acts = self._layer_norm_hidden.normalize(acts)
+      else:
+        acts = tf.nn.bias_add(acts,
+                              self._component.get_variable('bias_%d' % index))
     
-def test_follow_all_redirects_shown(httpbin):
-    r = http('--follow', '--all', httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 3
-    assert r.count('HTTP/1.1 302 FOUND', 2)
-    assert HTTP_OK in r
-    
-    ssl.add_argument(
-    '--cert-key',
-    default=None,
-    type=readable_file_arg,
-    help='''
-    The private key to use with SSL. Only needed if --cert is given and the
-    certificate file does not contain the private key.
-    
-    
-def humanize_bytes(n, precision=2):
-    # Author: Doug Latornell
-    # Licence: MIT
-    # URL: http://code.activestate.com/recipes/577081/
-    '''Return a humanized string representation of a number of bytes.
-    
-        def __init__(self):
-        self._plugins = []
-    
-    
-def test_default_options_overwrite(httpbin):
-    env = MockEnvironment()
-    env.config['default_options'] = ['--form']
-    env.config.save()
-    r = http('--json', httpbin.url + '/post', 'foo=bar', env=env)
-    assert r.json['json'] == {'foo': 'bar'}
-    
-    INVALID_TOKEN_TYPE = 0
-    
-            elif isinstance(self.input, TreeNodeStream):
-            adaptor = self.input.treeAdaptor
-            return adaptor.getType(self.node)
-    
-        def test_get_name_addr_atom_name(self):
-        name_addr = self._test_get_x(parser.get_name_addr,
-            'Dinsdale <dinsdale@example.com>',
-            'Dinsdale <dinsdale@example.com>',
-            'Dinsdale <dinsdale@example.com>',
-            [],
-            '')
-        self.assertEqual(name_addr.token_type, 'name-addr')
-        self.assertEqual(name_addr.display_name, 'Dinsdale')
-        self.assertEqual(name_addr.local_part, 'dinsdale')
-        self.assertEqual(name_addr.domain, 'example.com')
-        self.assertIsNone(name_addr.route)
-        self.assertEqual(name_addr.addr_spec, 'dinsdale@example.com')
-    
-    A proactor is a 'notify-on-completion' multiplexer.  Currently a
-proactor is only implemented on Windows with IOCP.
-'''
-    
-        def transform_xrange(self, node, results):
-        name = results['name']
-        name.replace(Name('range', prefix=name.prefix))
-        # This prevents the new range call from being wrapped in a list later.
-        self.transformed_xranges.add(id(node))
-    
-            while pending:
-            if timeout is None:
-                wait_timeout = None
-            else:
-                wait_timeout = end_time - time.time()
-                if wait_timeout < 0:
-                    raise TimeoutError(
-                            '%d (of %d) futures unfinished' % (
-                            len(pending), len(fs)))
-    
-        Args:
-        call_queue: A multiprocessing.Queue of _CallItems that will be read and
-            evaluated by the worker.
-        result_queue: A multiprocessing.Queue of _ResultItems that will written
-            to by the worker.
-        shutdown: A multiprocessing.Event that will be set as a signal to the
-            worker that it should exit when call_queue is empty.
-    '''
-    while True:
-        call_item = call_queue.get(block=True)
-        if call_item is None:
-            # Wake up queue management thread
-            result_queue.put(None)
-            return
-        try:
-            r = call_item.fn(*call_item.args, **call_item.kwargs)
-        except BaseException:
-            e = sys.exc_info()[1]
-            result_queue.put(_ResultItem(call_item.work_id,
-                                         exception=e))
-        else:
-            result_queue.put(_ResultItem(call_item.work_id,
-                                         result=r))
-    
-            for future in as_completed(future_to_url):
-            try:
-                url_to_content[future_to_url[future]] = future.result()
-            except:
-                pass
-        return url_to_content
-    finally:
-        executor.shutdown()
-    
-    def main():
-    for name, fn in [('sequential', sequential),
-                     ('processes', with_process_pool_executor),
-                     ('threads', with_thread_pool_executor)]:
-        sys.stdout.write('%s: ' % name.ljust(12))
-        start = time.time()
-        if fn() != [True] * len(PRIMES):
-            sys.stdout.write('failed\n')
-        else:
-            sys.stdout.write('%.2f seconds\n' % (time.time() - start))
-    
-        # If not a dictionary or a list, the response is necessarily a
-    # scalar: boolean, number, string, etc. In this case, we print
-    # it to the user.
-    if not isinstance( self._response, ( dict, list ) ):
-      return self._HandleBasicResponse()
-    
-      def BasicResponse_test( self ):
-    def _BasicResponseTest( command, response ):
-      with patch( 'vim.command' ) as vim_command:
-        request = CommandRequest( [ command ] )
-        request._response = response
-        request.RunPostCommandActionsIfNeeded( 'belowright' )
-        vim_command.assert_called_with( 'echo '{0}''.format( response ) )
-    
-    import face_recognition
-import picamera
-import numpy as np
-    
-    for image in TEST_IMAGES:
-    size = image.split('-')[1].split('.')[0]
-    print('Timings at {}:'.format(size))
-    
-    app = Flask(__name__)
+      def add_link(self,
+               source,
+               source_layer=None,
+               source_translator='identity',
+               name=None,
+               **kwargs):
+    '''Add a link using default naming and layers only.'''
+    if source_layer is None:
+      source_layer = source.default_source_layer()
+    if name is None:
+      name = source.spec.name
+    self.spec.linked_feature.add(
+        source_component=source.spec.name,
+        source_layer=source_layer,
+        name=name,
+        source_translator=source_translator,
+        **kwargs)
     
     
-def print_result(filename, location):
-    top, right, bottom, left = location
-    print('{},{},{},{},{}'.format(filename, top, right, bottom, left))
+def get_summary_writer(tensorboard_dir):
+  '''Creates a directory for writing summaries and returns a writer.'''
+  tf.logging.info('TensorBoard directory: %s', tensorboard_dir)
+  tf.logging.info('Deleting prior data if exists...')
+  try:
+    gfile.DeleteRecursively(tensorboard_dir)
+  except errors.OpError as err:
+    tf.logging.error('Directory did not exist? Error: %s', err)
+  tf.logging.info('Deleted! Creating the directory again...')
+  gfile.MakeDirs(tensorboard_dir)
+  tf.logging.info('Created! Instatiating SummaryWriter...')
+  summary_writer = tf.summary.FileWriter(tensorboard_dir)
+  return summary_writer
     
-    setup(
-    name='face_recognition',
-    version='1.2.2',
-    description='Recognize faces from Python or from the command line',
-    long_description=readme + '\n\n' + history,
-    author='Adam Geitgey',
-    author_email='ageitgey@gmail.com',
-    url='https://github.com/ageitgey/face_recognition',
-    packages=[
-        'face_recognition',
-    ],
-    package_dir={'face_recognition': 'face_recognition'},
-    package_data={
-        'face_recognition': ['models/*.dat']
-    },
-    entry_points={
-        'console_scripts': [
-            'face_recognition=face_recognition.face_recognition_cli:main',
-            'face_detection=face_recognition.face_detection_cli:main'
-        ]
-    },
-    install_requires=requirements,
-    license='MIT license',
-    zip_safe=False,
-    keywords='face_recognition',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements
+    
+def main(unused_argv):
+  tf.logging.set_verbosity(tf.logging.INFO)
+    
+      # Build and write master_spec.
+  master_spec = spec_pb2.MasterSpec()
+  master_spec.component.extend([lookahead.spec, segmenter.spec])
+  logging.info('Constructed master spec: %s', str(master_spec))
+  with gfile.GFile(FLAGS.resource_path + '/master_spec', 'w') as f:
+    f.write(str(master_spec).encode('utf-8'))
+    
+    tf_proto_library_cc(
+    name = 'dictionary_proto',
+    srcs = ['dictionary.proto'],
 )
+    
+      def testBatchPathScoresAgree(self):
+    '''Ensures that path scores computed in the beam are same in the net.'''
+    all_path_scores, beam_path_scores = self.PathScores(
+        iterations=1, beam_size=130, max_steps=5, batch_size=22)
+    self.assertArrayNear(all_path_scores[0], beam_path_scores[0], 1e-6)
+    
+        if not has_level_handler(logger):
+        logger.addHandler(default_handler)
+    
+                if match is None:
+                continue
+    
+    
+def test_manual_context_binding(app):
+    @app.route('/')
+    def index():
+        return 'Hello %s!' % flask.request.args['name']
+    
+        flask.request_started.connect(before_request_signal, app)
+    flask.request_finished.connect(after_request_signal, app)
+    
+    outputs = Conv2DTranspose(filters=1,
+                          kernel_size=kernel_size,
+                          activation='sigmoid',
+                          padding='same',
+                          name='decoder_output')(x)
+    
+        # generate char sequences of length 'sequence_length' out of alphabet and store the next char as label (e.g. 'ab'->'c')
+    sequence_length = 2
+    sentences = [alphabet[i: i + sequence_length] for i in range(len(alphabet) - sequence_length)]
+    next_chars = [alphabet[i + sequence_length] for i in range(len(alphabet) - sequence_length)]
+    
+    print('Pad sequences (samples x time)')
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+    
+        for i in range(n_dense - 1):
+        model.add(Dense(dense_units, kernel_initializer=kernel_initializer))
+        model.add(Activation(activation))
+        model.add(dropout(dropout_rate))
+    
+        x_train = np.array(x[:int(len(x) * (1 - test_split))])
+    y_train = np.array(y[:int(len(x) * (1 - test_split))])
+    x_test = np.array(x[int(len(x) * (1 - test_split)):])
+    y_test = np.array(y[int(len(x) * (1 - test_split)):])
+    return (x_train, y_train), (x_test, y_test)
 
     
-        def test_fd_command_line_interface_cnn_model(self):
-        target_string = 'obama.jpg'
-        runner = CliRunner()
-        image_file = os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg')
+    from .cifar import load_batch
+from ..utils.data_utils import get_file
+from .. import backend as K
+import numpy as np
+import os
+    
+        # Parse and validate the field names.  Validation serves two purposes,
+    # generating informative error messages and preventing template injection attacks.
+    if isinstance(field_names, basestring):
+        field_names = field_names.replace(',', ' ').split() # names separated by whitespace and/or commas
+    field_names = tuple(map(str, field_names))
+    for name in (typename,) + field_names:
+        if not all(c.isalnum() or c=='_' for c in name):
+            raise ValueError('Type names and field names can only contain alphanumeric characters and underscores: %r' % name)
+        if _iskeyword(name):
+            raise ValueError('Type names and field names cannot be a keyword: %r' % name)
+        if name[0].isdigit():
+            raise ValueError('Type names and field names cannot start with a number: %r' % name)
+    seen_names = set()
+    for name in field_names:
+        if name.startswith('_'):
+            raise ValueError('Field names cannot start with an underscore: %r' % name)
+        if name in seen_names:
+            raise ValueError('Encountered duplicate field name: %r' % name)
+        seen_names.add(name)
+    
+            try:
+            result = self.fn(*self.args, **self.kwargs)
+        except BaseException:
+            e = sys.exc_info()[1]
+            self.future.set_exception(e)
+        else:
+            self.future.set_result(result)
+    
+    
+def assert_python_ok(*args, **env_vars):
+    '''
+    Assert that running the interpreter with `args` and optional environment
+    variables `env_vars` is ok and return a (return code, stdout, stderr) tuple.
+    '''
+    return _assert_python(True, *args, **env_vars)
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    
+@YouCompleteMeInstance()
+def EventNotification_FileReadyToParse_NonDiagnostic_ConfirmExtraConf_test(
+    ycm ):
+    
+    
+GENERIC_RESPONSE = {
+  'clang': {
+    'has_support': True,
+    'version': 'Clang version'
+  },
+  'completer': {
+    'items': [
+      {
+        'key': 'key',
+        'value': 'value'
+      }
+    ],
+    'name': 'Completer name',
+    'servers': [
+      {
+        'address': '127.0.0.1',
+        'executable': '/path/to/executable',
+        'extras': [
+          {
+            'key': 'key',
+            'value': 'value'
+          }
+        ],
+        'is_running': True,
+        'logfiles': [
+          '/path/to/stdout/logfile',
+          '/path/to/stderr/logfile'
+        ],
+        'name': 'Server name',
+        'pid': 12345,
+        'port': 1234
+      }
+    ]
+  },
+  'extra_conf': {
+    'is_loaded': False,
+    'path': '/path/to/extra/conf'
+  },
+  'python': {
+    'executable': '/path/to/python/interpreter',
+    'version': 'Python version'
+  }
+}
+    
+      # Start by looking for a single character match
+  # and increase length until no match is found.
+  best = 0
+  length = 1
+  while True:
+    pattern = left_string[ -length: ]
+    found = right_string.find( pattern )
+    if found < 0:
+      return best
+    length += found
+    if left_string[ -length: ] == right_string[ :length ]:
+      best = length
+      length += 1
+
+    
+    
+  def Start( self ):
+    self._results = self._omni_completer.ComputeCandidates( self.request_data )
