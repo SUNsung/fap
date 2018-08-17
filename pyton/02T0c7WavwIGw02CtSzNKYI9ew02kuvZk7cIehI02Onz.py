fe46c7b1eda6b03c754491892f323863964da9e6
SUNsung <weatherius@gmail.com>
2018-08-17 13:43:53 +0300

    Rewrite [02T0c7WavwIGw02CtSzNKYI9ew02kuvZk7cIehI02Onz.py] File in: 2018-08-17 13:43:52

--- pyton/02T0c7WavwIGw02CtSzNKYI9ew02kuvZk7cIehI02Onz.py
+++ pyton/02T0c7WavwIGw02CtSzNKYI9ew02kuvZk7cIehI02Onz.py
@@ -1,137 +1,152 @@
 
-with codecs.open(JSON_FILE_PATH, encoding='utf8') as f:
-    JSON_FILE_CONTENT = f.read()
-    
-    
-def test_unicode_basic_auth(httpbin):
-    # it doesn't really authenticate us because httpbin
-    # doesn't interpret the utf8-encoded auth
-    http('--verbose', '--auth', u'test:%s' % UNICODE,
-         httpbin.url + u'/basic-auth/test/' + UNICODE)
-    
-        def get_auth_plugin(self, auth_type):
-        return self.get_auth_plugin_mapping()[auth_type]
-    
-        Models: `flatpages.flatpages`
-    Templates: Uses the template defined by the ``template_name`` field,
-        or :template:`flatpages/default.html` if template_name is not defined.
-    Context:
-        flatpage
-            `flatpages.flatpages` object
-    '''
-    if not url.startswith('/'):
-        url = '/' + url
-    site_id = get_current_site(request).id
-    try:
-        f = get_object_or_404(FlatPage, url=url, sites=site_id)
-    except Http404:
-        if not url.endswith('/') and settings.APPEND_SLASH:
-            url += '/'
-            f = get_object_or_404(FlatPage, url=url, sites=site_id)
-            return HttpResponsePermanentRedirect('%s/' % request.path)
-        else:
-            raise
-    return render_flatpage(request, f)
-    
-                sock1.connect(address)
-            sock1.sendall(first_request)
-            sock1.close()
-    
-            # Verify we receive an Authorization header in response, then redirect.
-        request_content = consume_socket_content(sock, timeout=0.5)
-        assert expected_digest in request_content
-        sock.send(text_302)
-    
-    
-def info():
-    '''Generate information for a bug report.'''
-    try:
-        platform_info = {
-            'system': platform.system(),
-            'release': platform.release(),
-        }
-    except IOError:
-        platform_info = {
-            'system': 'Unknown',
-            'release': 'Unknown',
-        }
-    
-    
-class sockaddr(ctypes.Structure):
-    _fields_ = [('sa_family', ctypes.c_short),
-                ('__pad1', ctypes.c_ushort),
-                ('ipv4_addr', ctypes.c_byte * 4),
-                ('ipv6_addr', ctypes.c_byte * 16),
-                ('__pad2', ctypes.c_ulong)]
-    
-        m = re.match(r'(\d+)\.(\d+)(\.(\d+))?(b(\d+))?', version_str)
-    if m is None:
-        raise ValueError('Bad version string %r' % version_str)
-    
-                # report after consuming so AW sees the token in the exception
-            self.reportError(e)
-    
-        def __init__(self, type=None, text=None, channel=DEFAULT_CHANNEL,
-                 oldToken=None
-                 ):
-        Token.__init__(self)
         
-        if oldToken is not None:
-            self.text = oldToken.text
-            self.type = oldToken.type
-            self.line = oldToken.line
-            self.charPositionInLine = oldToken.charPositionInLine
-            self.channel = oldToken.channel
-            
-        self.text = text
-        self.type = type
-        self.line = None
-        self.charPositionInLine = None
-        self.channel = channel
-        self.index = None
+            model.fit(temporal_x_train, temporal_y_train, batch_size=batch_size,
+              epochs=epochs // 3, verbose=0,
+              sample_weight=temporal_sample_weight)
+    model.fit(temporal_x_train, temporal_y_train, batch_size=batch_size,
+              epochs=epochs // 3, verbose=0,
+              sample_weight=temporal_sample_weight,
+              validation_split=0.1)
+    
+        x = Input(shape=(1,))
+    y = inner_model(x)
+    outer_model = Model(x, y)
+    assert outer_model.trainable_weights == inner_model.trainable_weights
+    inner_model.trainable = False
+    assert outer_model.trainable_weights == []
+    inner_model.trainable = True
+    inner_model.layers[-1].trainable = False
+    assert outer_model.trainable_weights == []
     
     
-def huaban_download_board(url, output_dir, **kwargs):
-    kwargs['merge'] = False
-    board = extract_board_data(url)
-    output_dir = os.path.join(output_dir, board.title)
-    print_info(site_info, board.title, 'jpg', float('Inf'))
-    for pin in board.pins:
-        download_urls([pin.url], pin.id, pin.ext, float('Inf'),
-                      output_dir=output_dir, faker=True, **kwargs)
+@keras_test
+def test_temporal_classification():
+    '''
+    Classify temporal sequences of float numbers
+    of length 3 into 2 classes using
+    single layer of GRU units and softmax applied
+    to the last activations of the units
+    '''
+    np.random.seed(1337)
+    (x_train, y_train), (x_test, y_test) = get_test_data(num_train=200,
+                                                         num_test=20,
+                                                         input_shape=(3, 4),
+                                                         classification=True,
+                                                         num_classes=2)
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
     
-    from ..common import *
-from ..extractor import VideoExtractor
+    # try using different optimizers and different optimizer configs
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
     
-    def kugou_download_playlist(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    html=get_html(url)
-    pattern=re.compile('title='(.*?)'.* data='(\w*)\|.*?'')
-    pairs=pattern.findall(html)
-    for title,hash_val in pairs:
-        kugou_download_by_hash(title,hash_val,output_dir,merge,info_only)
+        num_train_samples = 50000
     
-        stream_types = [
-        {'id': '720p HD'},
-        {'id': '360p SD'},
-    ]
+        def __init__(self,
+                 return_sequences=False,
+                 return_state=False,
+                 go_backwards=False,
+                 stateful=False,
+                 **kwargs):
+        if K.backend() != 'tensorflow':
+            raise RuntimeError('CuDNN RNNs are only available '
+                               'with the TensorFlow backend.')
+        super(RNN, self).__init__(**kwargs)
+        self.return_sequences = return_sequences
+        self.return_state = return_state
+        self.go_backwards = go_backwards
+        self.stateful = stateful
+        self.supports_masking = False
+        self.input_spec = [InputSpec(ndim=3)]
+        if hasattr(self.cell.state_size, '__len__'):
+            state_size = self.cell.state_size
+        else:
+            state_size = [self.cell.state_size]
+        self.state_spec = [InputSpec(shape=(None, dim))
+                           for dim in state_size]
+        self.constants_spec = None
+        self._states = None
+        self._num_constants = None
     
-            .. versionchanged:: 4.2
-           Now accepts positional arguments in addition to keyword arguments.
-        '''
     
-                    def get_app(self):
-                    self.app = WSGIApplication(self.get_handlers(),
-                                               **self.get_app_kwargs())
-                    return WSGIContainer(validator(self.app))
-        result['WSGIApplication_' + cls.__name__] = class_factory()
-    return result
+# split train/test data
+def split_data(x, y, ratio=0.8):
+    to_train = int(input_len * ratio)
+    # tweak to match with batch_size
+    to_train -= to_train % batch_size
     
-        @gen.coroutine
-    def get(self):
-        http_client = AsyncHTTPClient()
-        response1, response2 = yield [http_client.fetch(url1),
-                                      http_client.fetch(url2)]
-        response_dict = yield dict(response3=http_client.fetch(url3),
-                                   response4=http_client.fetch(url4))
-        response3 = response_dict['response3']
-        response4 = response_dict['response4']
+        time_ridge = np.empty(n_iter)
+    time_ols = np.empty(n_iter)
+    time_lasso = np.empty(n_iter)
+    
+        op.add_option('--n-components',
+                  dest='n_components', default='auto',
+                  help='Size of the random subspace.'
+                       ' ('auto' or int > 0)')
+    
+    
+def _get_git_revision():
+    try:
+        revision = subprocess.check_output(REVISION_CMD.split()).strip()
+    except (subprocess.CalledProcessError, OSError):
+        print('Failed to execute git to get revision')
+        return None
+    return revision.decode('utf-8')
+    
+        print('Decompressing %s' % ARCHIVE_NAME)
+    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
+        archive.extractall(path='.')
+    os.remove(ARCHIVE_NAME)
+
+    
+        # TASK: print the mean and std for each candidate along with the parameter
+    # settings for all the candidates explored by grid search.
+    n_candidates = len(grid_search.cv_results_['params'])
+    for i in range(n_candidates):
+        print(i, 'params - %s; mean - %0.2f; std - %0.2f'
+                 % (grid_search.cv_results_['params'][i],
+                    grid_search.cv_results_['mean_test_score'][i],
+                    grid_search.cv_results_['std_test_score'][i]))
+    
+            self.concurrent -= 1
+        return ''
+    
+        def run(self, args, opts):
+        # load contracts
+        contracts = build_component_list(self.settings.getwithbase('SPIDER_CONTRACTS'))
+        conman = ContractsManager(load_object(c) for c in contracts)
+        runner = TextTestRunner(verbosity=2 if opts.verbose else 1)
+        result = TextTestResult(runner.stream, runner.descriptions, runner.verbosity)
+    
+        @property
+    def templates_dir(self):
+        _templates_base_dir = self.settings['TEMPLATES_DIR'] or \
+            join(scrapy.__path__[0], 'templates')
+        return join(_templates_base_dir, 'spiders')
+
+    
+            if not aws_access_key_id:
+            aws_access_key_id = settings['AWS_ACCESS_KEY_ID']
+        if not aws_secret_access_key:
+            aws_secret_access_key = settings['AWS_SECRET_ACCESS_KEY']
+    
+    from scrapy import signals
+    
+            spider.crawler.stats.inc_value('dupefilter/filtered', spider=spider)
+
+    
+        def error_count(self, failure, response, spider):
+        self.counter['errorcount'] += 1
+        if self.counter['errorcount'] == self.close_on['errorcount']:
+            self.crawler.engine.close_spider(spider, 'closespider_errorcount')
+    
+    
+    def _read_image(self):
+        return np.array(Image.open(self.image_dir))
+
+    
+    update = True
+    
+        def adb_path(self):
+        return self.adb_path
