488afc3a35b3f8547c050ae5cc563ebbe680445c
SUNsung <weatherius@gmail.com>
2018-08-18 20:28:55 +0300

    Rewrite [53Ir.BQGxoT5g53Gaojm5jc5E53YMGZ34hPVlo53fNN.cpp] File in: 2018-08-18 20:28:54

--- cccccc/53Ir.BQGxoT5g53Gaojm5jc5E53YMGZ34hPVlo53fNN.cpp
+++ cccccc/53Ir.BQGxoT5g53Gaojm5jc5E53YMGZ34hPVlo53fNN.cpp
@@ -1,251 +1,217 @@
 
         
-        #ifndef GTEST_INCLUDE_GTEST_GTEST_MESSAGE_H_
-#define GTEST_INCLUDE_GTEST_GTEST_MESSAGE_H_
+            http://www.apache.org/licenses/LICENSE-2.0
     
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12, typename T13, typename T14, typename T15,
-    typename T16, typename T17, typename T18, typename T19, typename T20,
-    typename T21, typename T22, typename T23, typename T24, typename T25,
-    typename T26, typename T27, typename T28, typename T29, typename T30>
-internal::ValueArray30<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
-    T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
-    T29, T30> Values(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8,
-    T9 v9, T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15, T16 v16,
-    T17 v17, T18 v18, T19 v19, T20 v20, T21 v21, T22 v22, T23 v23, T24 v24,
-    T25 v25, T26 v26, T27 v27, T28 v28, T29 v29, T30 v30) {
-  return internal::ValueArray30<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
-      T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25,
-      T26, T27, T28, T29, T30>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11,
-      v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25,
-      v26, v27, v28, v29, v30);
-}
-    
-      GTEST_DECLARE_TUPLE_AS_FRIEND_
-    
-    
-    {    // Now, we have i <= n/i < n.
-    // If n is divisible by i, n is not prime.
-    if (n % i == 0) return false;
-  }
-    
-    TEST_F(UnicharcompressTest, DoesChinese) {
-  LOG(INFO) << 'Testing chi_tra';
-  LoadUnicharset('chi_tra.unicharset');
-  ExpectCorrect('chi_tra');
-  LOG(INFO) << 'Testing chi_sim';
-  LoadUnicharset('chi_sim.unicharset');
-  ExpectCorrect('chi_sim');
-}
-    
-    // dims=[5, 4, 3, 2]->[5, 3, 2, 4]
-TEST_F(MatrixTest, RotatingTranspose_1_3) {
-  GENERIC_2D_ARRAY<int> m;
-  src_.RotatingTranspose(dims_, kNumDims_, 1, 3, &m);
-  m.ResizeNoInit(kInputSize_ / 4, 4);
-  // Verify that the result is:
-  // output tensor=[[[[0, 6, 12, 18][1, 7, 13, 19]]
-  //                 [[2, 8, 14, 20][3, 9, 15, 21]]
-  //                 [[4, 10, 16, 22][5, 11, 17, 23]]]
-  //                [[[24, 30, 36, 42]...
-  EXPECT_EQ(0, m(0, 0));
-  EXPECT_EQ(6, m(0, 1));
-  EXPECT_EQ(1, m(1, 0));
-  EXPECT_EQ(2, m(2, 0));
-  EXPECT_EQ(3, m(3, 0));
-  EXPECT_EQ(4, m(4, 0));
-  EXPECT_EQ(5, m(5, 0));
-  EXPECT_EQ(24, m(6, 0));
-  EXPECT_EQ(30, m(6, 1));
-}
-    
-      /// Return true if we are processing the full image.
-  bool IsFullImage() const {
-    return rect_left_ == 0 && rect_top_ == 0 &&
-           rect_width_ == image_width_ && rect_height_ == image_height_;
-  }
-    
-      ParagraphModel()
-      : justification_(tesseract::JUSTIFICATION_UNKNOWN),
-         margin_(0),
-         first_indent_(0),
-         body_indent_(0),
-         tolerance_(0) { }
-    
-    
-    {  virtual R Run(A1 a1,A2 a2) {
-    if (!del) {
-      R result = (object_->*member_)(a1,a2);
-      return result;
-    } else {
-      R result = (object_->*member_)(a1,a2);
-      //  zero out the pointer to ensure segfault if used again
-      member_ = nullptr;
-      delete this;
-      return result;
+    class DebugGraphDecorator : public DebugGraphDecoratorInterface {
+ public:
+  DebugGraphDecorator(const DebugOptions& debug_options)
+      : debug_options_(debug_options) {}
+  virtual ~DebugGraphDecorator() {}
     }
+    
+    void PartialRunMgr::ExecutorDone(int step_id, const Status& executor_status) {
+  StatusCallback done;
+  Status callback_status;
+  {
+    mutex_lock l(mu_);
+    auto run_it = step_id_to_partial_run_.find(step_id);
+    if (run_it == step_id_to_partial_run_.end()) {
+      return;
+    }
+    // If we found the partial_run, we call the final callback, if it
+    // exists.
+    // It is guaranteed that run_it->second->final_callback is left empty
+    // after the std::move call.
+    done = std::move(run_it->second->final_callback);
+    if (!executor_status.ok()) {
+      run_it->second->final_status = executor_status;
+    }
+    callback_status = run_it->second->final_status;
+    run_it->second->executor_done = true;
   }
+  if (done != nullptr) {
+    done(callback_status);
+    mutex_lock l(mu_);
+    step_id_to_partial_run_.erase(step_id);
+  }
+}
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    template <typename T>
+void DynamicStitchGPUImpl(const Eigen::GpuDevice& gpu_device,
+                          const int32 slice_size, const int32 first_dim_size,
+                          const CudaDeviceArrayStruct<int>& input_indices,
+                          const CudaDeviceArrayStruct<const T*>& input_ptrs,
+                          T* output) {
+  const int32 output_size = first_dim_size * slice_size;
+  auto config = GetCudaLaunchConfig(output_size, gpu_device);
+    }
+    
+    // TODO: Enable GPU support for angle op after resolving
+// build failures on GPU (See #10643 for context).
+#if 0 && GOOGLE_CUDA
+REGISTER_COMPLEX(GPU, float, complex64);
+REGISTER_COMPLEX(GPU, double, complex128);
+#endif
+    
+    
+    {}  // namespace leveldb
+
+    
+    
+    {}  // namespace leveldb
+    
+    
+    {  // No copying allowed
+  DBIter(const DBIter&);
+  void operator=(const DBIter&);
 };
     
+    #ifndef STORAGE_LEVELDB_DB_FILENAME_H_
+#define STORAGE_LEVELDB_DB_FILENAME_H_
     
-//#define EXPERIMENT_ON
-#ifdef EXPERIMENT_ON  // This code is commented out as SampleIterator and
-// TrainingSample are not reviewed/checked in yet, but these functions are a
-// useful indicator of how an IntFeatureMap is setup.
+    static void DeleteEntry(const Slice& key, void* value) {
+  TableAndFile* tf = reinterpret_cast<TableAndFile*>(value);
+  delete tf->table;
+  delete tf->file;
+  delete tf;
+}
+    
+    struct FileMetaData {
+  int refs;
+  int allowed_seeks;          // Seeks allowed until compaction
+  uint64_t number;
+  uint64_t file_size;         // File size in bytes
+  InternalKey smallest;       // Smallest internal key served by table
+  InternalKey largest;        // Largest internal key served by table
+    }
     
     
-    {  bool failed_ = false;
-  std::string db_;
-  size_t current_ = 0;
-  const std::string location_ = '\'location\':';
-  const std::string latitude_ = '\'latitude\':';
-  const std::string longitude_ = '\'longitude\':';
-  const std::string name_ = '\'name\':';
+    {}  // namespace leveldb
+    
+    static std::string PrintContents(WriteBatch* b) {
+  InternalKeyComparator cmp(BytewiseComparator());
+  MemTable* mem = new MemTable(cmp);
+  mem->Ref();
+  std::string state;
+  Status s = WriteBatchInternal::InsertInto(b, mem);
+  int count = 0;
+  Iterator* iter = mem->NewIterator();
+  for (iter->SeekToFirst(); iter->Valid(); iter->Next()) {
+    ParsedInternalKey ikey;
+    ASSERT_TRUE(ParseInternalKey(iter->key(), &ikey));
+    switch (ikey.type) {
+      case kTypeValue:
+        state.append('Put(');
+        state.append(ikey.user_key.ToString());
+        state.append(', ');
+        state.append(iter->value().ToString());
+        state.append(')');
+        count++;
+        break;
+      case kTypeDeletion:
+        state.append('Delete(');
+        state.append(ikey.user_key.ToString());
+        state.append(')');
+        count++;
+        break;
+    }
+    state.append('@');
+    state.append(NumberToString(ikey.sequence));
+  }
+  delete iter;
+  if (!s.ok()) {
+    state.append('ParseError()');
+  } else if (count != WriteBatchInternal::Count(b)) {
+    state.append('CountMismatch()');
+  }
+  mem->Unref();
+  return state;
+}
+    
+      void PrintHeader() {
+    const int kKeySize = 16;
+    PrintEnvironment();
+    fprintf(stdout, 'Keys:       %d bytes each\n', kKeySize);
+    fprintf(stdout, 'Values:     %d bytes each\n', FLAGS_value_size);
+    fprintf(stdout, 'Entries:    %d\n', num_);
+    fprintf(stdout, 'RawSize:    %.1f MB (estimated)\n',
+            ((static_cast<int64_t>(kKeySize + FLAGS_value_size) * num_)
+             / 1048576.0));
+    PrintWarnings();
+    fprintf(stdout, '------------------------------------------------\n');
+  }
+    
+    #endif  // STORAGE_LEVELDB_HELPERS_MEMENV_MEMENV_H_
+
+    
+    #endif  // STORAGE_LEVELDB_INCLUDE_DUMPFILE_H_
+
+    
+    #endif // BOOST_ASIO_DETAIL_ARRAY_FWD_HPP
+
+    
+    #include <boost/asio/detail/pop_options.hpp>
+    
+    
+    {private:
+  int descriptor_;
+  ConstBufferSequence buffers_;
 };
     
-    namespace grpc {
-namespace testing {
+    template <typename Handler>
+inline void deallocate(void* p, std::size_t s, Handler& h)
+{
+#if !defined(BOOST_ASIO_HAS_HANDLER_HOOKS)
+  ::operator delete(p);
+#else
+  using boost::asio::asio_handler_deallocate;
+  asio_handler_deallocate(p, s, boost::asio::detail::addressof(h));
+#endif
+}
+    
+    template <typename Function, typename Context>
+inline void invoke(const Function& function, Context& context)
+{
+#if !defined(BOOST_ASIO_HAS_HANDLER_HOOKS)
+  Function tmp(function);
+  tmp();
+#else
+  using boost::asio::asio_handler_invoke;
+  asio_handler_invoke(function, boost::asio::detail::addressof(context));
+#endif
+}
+    
+      {
+    auto nobody = getpwnam('nobody');
+    auto dropper = DropPrivileges::get();
+    dropper->dropTo(nobody->pw_uid, nobody->pw_gid);
+    PlatformFile fd(file_path, PF_OPEN_EXISTING | PF_READ);
+    EXPECT_FALSE(fd.isValid());
+  }
+    
+    
+    {
+    {    int code = 0;
+    EXPECT_TRUE(getProcessExitCode(*process, code));
+    EXPECT_EQ(code, EXTENSION_SUCCESS_CODE);
+  }
+}
+    
+      struct tm result;
+  localtime_r((time_t*)&epoch, &result);
+    
+    TEST_F(RocksDBDatabasePluginTests, test_rocksdb_loglevel) {
+  // Make sure a log file was created.
+  EXPECT_FALSE(pathExists(path_ + '/LOG'));
     }
-    }
     
+      void stop() override;
     
-    {}  // namespace grpc
-    
-    QueryData genNFSShares(QueryContext& context) {
-  QueryData results;
-    }
-    
-      // Test operator upper bounds.
-  EXPECT_FALSE(cl2.matches(1000));
-  EXPECT_FALSE(cl2.matches(1001));
-    
-      /**
-   * @brief Translate event subscribers into kernel subscriptions.
-   *
-   * The kernel component also uses a subscription abstraction. We expect to
-   * register kernel-based callbacks or start kernel threads that publish into
-   * a circular queue. When the queue is initialized it may communicate to each
-   * of these kernel publishers.
-   */
-  void configure() override;
-    
-    TEST_F(SyslogTests, test_populate_event_context) {
-  std::string line =
-      R'|('2016-03-22T21:17:01.701882+00:00','vagrant-ubuntu-trusty-64','6','cron','CRON[16538]:',' (root) CMD (   cd / && run-parts --report /etc/cron.hourly)')|';
-  SyslogEventPublisher pub;
-  auto ec = pub.createEventContext();
-  Status status = pub.populateEventContext(line, ec);
-    }
+    #include <libudev.h>
     
     namespace osquery {
     }
-    
-        std::string content;
-    setDatabaseValue(kPersistentSettings, 'complex_example', '1');
-    if (getDatabaseValue(kPersistentSettings, 'complex_example', content)) {
-      r['database_test'] = content;
-    }
-    
-    class FirehoseLoggerPlugin : public LoggerPlugin {
- public:
-  FirehoseLoggerPlugin() : LoggerPlugin() {}
-    }
-    
-    extern JSClass  *jsb_cocosbuilder_CCBReader_class;
-extern JSObject *jsb_cocosbuilder_CCBReader_prototype;
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    			vertices[0] = b2Mul(xf2, b2Vec2(-1.0f, 0.0f));
-			vertices[1] = b2Mul(xf2, b2Vec2(1.0f, 0.0f));
-			vertices[2] = b2Mul(xf2, b2Vec2(0.0f, 0.5f));
-			
-			b2PolygonShape poly2;
-			poly2.Set(vertices, 3);
-    
-    			b2RevoluteJointDef jd;
-    
-    class OrderingTest : public testing::Test {};
-    
-    template <typename T>
-bool SingletonHolder<T>::hasLiveInstance() {
-  return !instance_weak_.expired();
-}
-    
-    // See portability/Unistd.h for why these need to be in a namespace
-// rather then extern 'C'.
-namespace folly {
-namespace portability {
-namespace fcntl {
-int creat(char const* fn, int pm);
-int fcntl(int fd, int cmd, ...);
-int posix_fallocate(int fd, off_t offset, off_t len);
-int open(char const* fn, int of, int pm = 0);
-}
-}
-}
-    
-      HANDLE h = CreateFileA(
-      path,
-      GENERIC_READ,
-      FILE_SHARE_READ,
-      nullptr,
-      OPEN_EXISTING,
-      FILE_FLAG_BACKUP_SEMANTICS,
-      nullptr);
-  if (h == INVALID_HANDLE_VALUE) {
-    return -1;
-  }
-    
-    #pragma once
-    
-      T* allocate(size_t count) {
-    using lifted = typename detail::lift_void_to_char<T>::type;
-    auto const p = std::malloc(sizeof(lifted) * count);
-    if (!p) {
-      throw_exception<std::bad_alloc>();
-    }
-    return static_cast<T*>(p);
-  }
-  void deallocate(T* p, size_t /* count */) {
-    std::free(p);
-  }
-    
-    /* Primitive types that match up with Java equivalents. */
-typedef uint8_t  jboolean; /* unsigned 8 bits */
-typedef int8_t   jbyte;    /* signed 8 bits */
-typedef uint16_t jchar;    /* unsigned 16 bits */
-typedef int16_t  jshort;   /* signed 16 bits */
-typedef int32_t  jint;     /* signed 32 bits */
-typedef int64_t  jlong;    /* signed 64 bits */
-typedef float    jfloat;   /* 32-bit IEEE 754 */
-typedef double   jdouble;  /* 64-bit IEEE 754 */
-    
-    #include 'Common.h'
-#include 'References.h'
-    
-    /**
- * Symbolicates a stack trace into a given vector
- *
- * @param symbols The vector to receive the output. The vector is cleared and
- * enough room to keep the frames are reserved.
- *
- * @param stackTrace The input stack trace
- */
-FBEXPORT void getStackTraceSymbols(std::vector<StackTraceElement>& symbols,
-                                   const std::vector<InstructionPointer>& trace);
-    
-        YGNodeRef m_node;
