42bd5c061fc67d56ab2097fef578bfc1132a34f3
SUNsung <weatherius@gmail.com>
2018-08-18 13:46:06 +0300

    Rewrite [31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py] File in: 2018-08-18 13:46:05

--- pyton/31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py
+++ pyton/31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py
@@ -1,44 +1,17 @@
 
         
-        
-    def predict(self, input):
-        '''
-        From the input stream, predict what alternative will succeed
-	using this DFA (representing the covering regular approximation
-	to the underlying CFL).  Return an alternative number 1..n.  Throw
-	 an exception upon error.
-	'''
-        mark = input.mark()
-        s = 0 # we always start at s0
-        try:
-            for _ in xrange(50000):
-                #print '***Current state = %d' % s
-                
-                specialState = self.special[s]
-                if specialState >= 0:
-                    #print 'is special'
-                    s = self.specialStateTransition(specialState, input)
-                    if s == -1:
-                        self.noViableAlt(s, input)
-                        return 0
-                    input.consume()
-                    continue
-    
-            
-    def computeContextSensitiveRuleFOLLOW(self):
-        '''
-        Compute the context-sensitive FOLLOW set for current rule.
-        This is set of token types that can follow a specific rule
-        reference given a specific call chain.  You get the set of
-        viable tokens that can possibly come next (lookahead depth 1)
-        given the current call chain.  Contrast this with the
-        definition of plain FOLLOW for rule r:
-    
-    ## All tokens go to the parser (unless skip() is called in that rule)
+        ## All tokens go to the parser (unless skip() is called in that rule)
 # on a particular 'channel'.  The parser tunes to a particular channel
 # so that whitespace etc... can go to the parser on a 'hidden' channel.
 DEFAULT_CHANNEL = 0
     
+        def getUnexpectedToken(self):
+        return self.token
     
-class DFA(object):
-    '''@brief A DFA implemented as a set of transition tables.
+        if version_str == 'HEAD':
+        return (sys.maxint, sys.maxint, sys.maxint, sys.maxint)
+    
+            Terence implemented packed table initializers, because Java has a
+        size restriction on .class files and the lookup tables can grow
+        pretty large. The generated JavaLexer.java of the Java.g example
+        would be about 15MB with uncompressed array initializers.
