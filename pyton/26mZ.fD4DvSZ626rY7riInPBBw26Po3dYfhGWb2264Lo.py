3ab2019c4fc526d8a00600ac4e38f370dd1971fc
SUNsung <weatherius@gmail.com>
2018-08-18 18:07:01 +0300

    Rewrite [26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py] File in: 2018-08-18 18:07:01

--- pyton/26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py
+++ pyton/26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py
@@ -1,67 +1,50 @@
 
         
-            def setType(self, ttype):
-        self.type = ttype
+          Parameters:
+    forest: If true, solve for a spanning forest instead of a spanning tree.
+    loss: The loss function for training.  Select from
+      softmax: Default unstructured softmax (prediction is still structured).
+      m3n: Max-Margin Markov Networks loss.
+    crf_max_dynamic_range: Max dynamic range for the log partition function.
     
-    for i, face_distance in enumerate(face_distances):
-    print('The test image has a distance of {:.2} from known image #{}'.format(face_distance, i))
-    print('- With a normal cutoff of 0.6, would the test image match the known image? {}'.format(face_distance < 0.6))
-    print('- With a very strict cutoff of 0.5, would the test image match the known image? {}'.format(face_distance < 0.5))
-    print()
+          # For the first tree, the gold and scores agree on one arc (that index 1
+      # is a root), and for the second tree, the gold and scores agree on none
+      # of the arcs.  Therefore, we expect +1 and +3 for the first and second
+      # trees in the M3N loss.
+      gold = tf.constant([0, 1, -1, 0, 0, 1], tf.int32)
+      first_gold_score = 0.5 + 2.0
+      second_gold_score = 2.5 + 2.5 + 2.5
+      first_tree_correct = 1
+      second_tree_correct = 0
+      first_tree_loss = 2 * 2.0 + 2 - first_tree_correct - first_gold_score
+      second_tree_loss = 3 * 4.0 + 3 - second_tree_correct - second_gold_score
+    
+      Args:
+    function: Function to apply using captured variables.  The function should
+        take one argument, its enclosing variable scope.
+    scope_name: Variable scope in which the |function| is evaluated.  Must match
+        the scope passed to capture_variables().
+    component: Component from which to extract captured variables.
+    
+    
+if __name__ == '__main__':
+  tf.app.run()
 
     
-    # This is a very simple benchmark to give you an idea of how fast each step of face recognition will run on your system.
-# Notice that face detection gets very slow at large image sizes. So you might consider running face detection on a
-# scaled down version of your image and then running face encodings on the the full size image.
+      # Convert to char-based corpora, if requested.
+  if config['convert_to_char_corpora']:
+    # NB: Do not convert the |gold_tune_corpus|, which should remain word-based
+    # for segmentation evaluation purposes.
+    train_corpus = _convert_to_char_corpus(train_corpus)
+    tune_corpus = _convert_to_char_corpus(tune_corpus)
+    
+    from syntaxnet.ops import gen_parser_ops
+from syntaxnet.util import check
+    
+        parts[-1] += 1
+    return '.'.join(map(str, parts))
     
     
-def detect_faces_in_image(file_stream):
-    # Pre-calculated face encoding of Obama generated with face_recognition.face_encodings(img)
-    known_face_encoding = [-0.09634063,  0.12095481, -0.00436332, -0.07643753,  0.0080383,
-                            0.01902981, -0.07184699, -0.09383309,  0.18518871, -0.09588896,
-                            0.23951106,  0.0986533 , -0.22114635, -0.1363683 ,  0.04405268,
-                            0.11574756, -0.19899382, -0.09597053, -0.11969153, -0.12277931,
-                            0.03416885, -0.00267565,  0.09203379,  0.04713435, -0.12731361,
-                           -0.35371891, -0.0503444 , -0.17841317, -0.00310897, -0.09844551,
-                           -0.06910533, -0.00503746, -0.18466514, -0.09851682,  0.02903969,
-                           -0.02174894,  0.02261871,  0.0032102 ,  0.20312519,  0.02999607,
-                           -0.11646006,  0.09432904,  0.02774341,  0.22102901,  0.26725179,
-                            0.06896867, -0.00490024, -0.09441824,  0.11115381, -0.22592428,
-                            0.06230862,  0.16559327,  0.06232892,  0.03458837,  0.09459756,
-                           -0.18777156,  0.00654241,  0.08582542, -0.13578284,  0.0150229 ,
-                            0.00670836, -0.08195844, -0.04346499,  0.03347827,  0.20310158,
-                            0.09987706, -0.12370517, -0.06683611,  0.12704916, -0.02160804,
-                            0.00984683,  0.00766284, -0.18980607, -0.19641446, -0.22800779,
-                            0.09010898,  0.39178532,  0.18818057, -0.20875394,  0.03097027,
-                           -0.21300618,  0.02532415,  0.07938635,  0.01000703, -0.07719778,
-                           -0.12651891, -0.04318593,  0.06219772,  0.09163868,  0.05039065,
-                           -0.04922386,  0.21839413, -0.02394437,  0.06173781,  0.0292527 ,
-                            0.06160797, -0.15553983, -0.02440624, -0.17509389, -0.0630486 ,
-                            0.01428208, -0.03637431,  0.03971229,  0.13983178, -0.23006812,
-                            0.04999552,  0.0108454 , -0.03970895,  0.02501768,  0.08157793,
-                           -0.03224047, -0.04502571,  0.0556995 , -0.24374914,  0.25514284,
-                            0.24795187,  0.04060191,  0.17597422,  0.07966681,  0.01920104,
-                           -0.01194376, -0.02300822, -0.17204897, -0.0596558 ,  0.05307484,
-                            0.07417042,  0.07126575,  0.00209804]
-    
-    # The name of an image file (within the static path) to use as favicon
-# of the docs.  This file should be a Windows icon file (.ico) being
-# 16x16 or 32x32 pixels large.
-#html_favicon = None
-    
-    
-@click.command()
-@click.argument('known_people_folder')
-@click.argument('image_to_check')
-@click.option('--cpus', default=1, help='number of CPU cores to use in parallel (can speed up processing lots of images). -1 means 'use all in system'')
-@click.option('--tolerance', default=0.6, help='Tolerance for face comparisons. Default is 0.6. Lower this if you get multiple matches for the same person.')
-@click.option('--show-distance', default=False, type=bool, help='Output face distance. Useful for tweaking tolerance setting.')
-def main(known_people_folder, image_to_check, cpus, tolerance, show_distance):
-    known_names, known_face_encodings = scan_known_people(known_people_folder)
-    
-        # STEP 2: Using the trained classifier, make predictions for unknown images
-    for image_file in os.listdir('knn_examples/test'):
-        full_file_path = os.path.join('knn_examples/test', image_file)
-    
-    # Find all facial features in all the faces in the image
-face_landmarks_list = face_recognition.face_landmarks(image)
+def test_add_template_filter(app):
+    def my_reverse(s):
+        return s[::-1]
