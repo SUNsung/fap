a3faf5e72a29a1adf6d327bf561ec9861a2875a9
SUNsung <weatherius@gmail.com>
2018-08-18 21:41:17 +0300

    Rewrite [51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb] File in: 2018-08-18 21:41:17

--- ruby/51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb
+++ ruby/51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb
@@ -1,57 +1,150 @@
 
         
-            def process_response?(response)
-      !(response.body =~ REDIRECT_RGX || response.body =~ NOT_FOUND_RGX || response.body.blank?)
+          def test_popen2e
+    with_pipe {|r, w|
+      with_reopen(STDERR, w) {|old|
+        w.close
+        Open3.popen2e(RUBY, '-e', 's=STDIN.read; STDOUT.print s+'o'; STDOUT.flush; STDERR.print s+'e'') {|i,o,t|
+          assert_kind_of(Thread, t)
+          i.print 'y'
+          i.close
+          STDERR.reopen(old)
+          assert_equal('yoye', o.read)
+          assert_equal('', r.read)
+        }
+      }
+    }
+  end
+    
+      describe 'with -n and a Fixnum range' do
+    before :each do
+      @script = '-W0 -ne 'print if %s' #{fixture(__FILE__, 'conditional_range.txt')}'
+    end
+    
+    describe 'The if expression' do
+  ruby_version_is '2.4' do
+    describe 'accepts multiple assignments in conditional expression' do
+      before(:each) { ScratchPad.record([]) }
+      after(:each)  { ScratchPad.clear }
+    
+      def test_flip_flop
+    eval <<-END
+    assert_equal [4,5], (1..9).select {|n| true if (n==4)..(n==5)}
+    assert_equal [4,5], (1..9).select {|n| true if (n==4)...(n==5)}
+    assert_equal [2], (1..9).select {|n| true if (n==2)..(n%2).zero?}
+    assert_equal [2,3,4], (1..9).select {|n| true if (n==2)...(n%2).zero?}
+    assert_equal [4,5,7,8], (1..9).select {|n| true if (n==4)...(n==5) or (n==7)...(n==8)}
+    assert_equal [nil, 2, 3, 4, nil], (1..5).map {|x| x if (x==2..x==4)}
+    assert_equal [1, nil, nil, nil, 5], (1..5).map {|x| x if !(x==2..x==4)}
+    END
+  end
+    
+      it 'raises an Errno::ECHILD if there are no child processes' do
+    lambda { Process.wait }.should raise_error(Errno::ECHILD)
+  end
+    
+      ##
+  # Length of keys created by KEY_ALGORITHM
+    
+    unless defined?(OpenSSL::SSL) then
+  warn 'Skipping Gem::Request tests.  openssl not found.'
+end
+    
+      def test_dump_insns_with_rflag
+    assert_norun_with_rflag('--dump=insns')
+  end
+    
+        ##
+    # Htpasswd accesses apache-compatible password files.  Passwords are
+    # matched to a realm where they are valid.  For security, the path for a
+    # password database should be stored outside of the paths available to the
+    # HTTP server.
+    #
+    # Htpasswd is intended for use with WEBrick::HTTPAuth::BasicAuth.
+    #
+    # To create an Htpasswd database with a single user:
+    #
+    #   htpasswd = WEBrick::HTTPAuth::Htpasswd.new 'my_password_file'
+    #   htpasswd.set_passwd 'my realm', 'username', 'password'
+    #   htpasswd.flush
+    
+    def assert_not_match(unexpected_pattern, testsrc, message = '')
+  newtest
+  assert_check(testsrc, message) {|result|
+    if unexpected_pattern !~ result
+      nil
+    else
+      desc = '#{unexpected_pattern.inspect} expected to be !~\n#{result.inspect}'
+      pretty(testsrc, desc, result)
+    end
+  }
+end
+    
+      def parse(pkt)
+    # We want to return immediatly if we do not have a packet which is handled by us
+    return unless pkt.is_tcp?
+    return if (pkt.tcp_sport != 21 and pkt.tcp_dport != 21)
+    s = find_session((pkt.tcp_sport == 21) ? get_session_src(pkt) : get_session_dst(pkt))
+    s[:sname] ||= 'ftp'
+    
+    # This is a completely hackish way to do this, and could break with future
+# versions of the JDK.  Need to find a better way to use sun.security.tools.KeyTool
+# and .JarSigner than modifying the source.  These rely on internal APIs that may
+# change.
+signer = Rjb::import('javaCompile.SignJar')
+#clsKeyTool = Rjb::import('sun.security.tools.KeyTool')
+#clsKeyTool = Rjb::import('sun.security.tools.KeyToolMSF')
+#clsJarSigner = Rjb::import('javaCompile.SignJar.JarSignerMSF')
+#clsJarSigner = Rjb::import('sun.security.tools.JarSigner')
+#clsJarSigner = Rjb::import('sun.security.tools.JarSignerMSF')
+    
+      def parse
+vers = [
+  '6.1.4',
+  '6.1.3',
+  '6.1.2',
+  '6.1.1',
+  '6.1.0',
+  '5.3.10',
+  '5.3.9',
+  '5.3.8',
+  '5.3.7',
+]
+    
+    		temp[1].strip!
+		temp[1] = temp[1].scan(/\w+/)
+    
+        def puts(*args)
+      STDERR.puts *args unless @silence
+    end
+    
+      # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # config.force_ssl = true
+    
+    class NodeMincerTest < Minitest::Test
+  DUMMY_PATH = 'test/dummy_node_mincer'
+    
+            raise t('error.invalid_stage_name', name: invalid, path: stage_config_path.join('#{invalid}.rb'))
+      end
     end
   end
 end
 
     
-        version 'C++' do
-      include MultipleBaseUrls
-      self.base_urls = ['https://www.tensorflow.org/api_docs/cc/', 'https://www.tensorflow.org/api_guides/cc/']
-    end
+      mkdir_p deploy_dir
     
-            css('p > code:first-child:last-child', 'td > code:first-child:last-child').each do |node|
-          next if node.previous.try(:content).present? || node.next.try(:content).present?
-          node.inner_html = node.inner_html.squish.gsub(/<br(\ \/)?>\s*/, '\n')
-          node.content = node.content.strip
-          node.name = 'pre' if node.content =~ /\s/
-          node.parent.before(node.parent.children).remove if node.parent.name == 'p'
+            def failure_message
+          'Attachment #{@attachment_name} must be between #{@low} and #{@high} bytes'
         end
     
-      def self.socket
-    require 'socket'
-    name = tmp('ftype_socket.socket')
-    rm_r name
-    socket = UNIXServer.new name
-    begin
-      yield name
-    ensure
-      socket.close
-      rm_r name
-    end
-  end
-end
-
+          def validate_blacklist(record, attribute, value)
+        if forbidden_types.present? && forbidden_types.any? { |type| type === value }
+          mark_invalid record, attribute, forbidden_types
+        end
+      end
     
-      it 'returns subclass instances when called on a subclass' do
-    StringSpecs::MyString.new('').swapcase.should be_an_instance_of(StringSpecs::MyString)
-    StringSpecs::MyString.new('hello').swapcase.should be_an_instance_of(StringSpecs::MyString)
-  end
-end
-    
-      ruby_version_is ''...'2.4' do
-    it 'is locale insensitive (only replaces a-z)' do
-      'äöü'.upcase.should == 'äöü'
-    
-    With optional '-t <bundle-id>', silently test if a given app
-is running, exiting with an error code if not.
-    
-              # Check for Maven Central urls, prefer HTTPS redirector over specific host
-          maven_pattern = %r{https?://(?:central|repo\d+)\.maven\.org/maven2/(.+)$}
-          audit_urls(urls, maven_pattern) do |match, url|
-            problem '#{url} should be `https://search.maven.org/remotecontent?filepath=#{match[1]}`'
-          end
+          def validate_blacklist(record, attribute, value)
+        if forbidden.present? && forbidden.any? { |type| type === value }
+          mark_invalid record, attribute, forbidden
         end
       end
