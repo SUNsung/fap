e6b9e490ff2a8fbb02f47649feb020083d38b5ec
SUNsung <weatherius@gmail.com>
2018-08-19 18:43:17 +0300

    Rewrite [486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb] File in: 2018-08-19 18:43:16

--- ruby/486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb
+++ ruby/486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb
@@ -1,32 +1,56 @@
 
         
-          def userpaths?
-    @settings.include? :userpaths
-  end
-end
+                def test_encoded_password
+          password = 'am@z1ng_p@ssw0rd#!'
+          encoded_password = URI.encode_www_form_component(password)
+          spec = resolve 'abstract://foo:#{encoded_password}@localhost/bar'
+          assert_equal password, spec['password']
+        end
     
-      def bash_completion_caveats
-    if keg && keg.completion_installed?(:bash) then <<-EOS.undent
-      Bash completion has been installed to:
-        #{HOMEBREW_PREFIX}/etc/bash_completion.d
-      EOS
+        line_item = invoice.line_items.build invoice_id: invoice.id + 1
+    assert_equal invoice.id, line_item.invoice_id
+    
+        def connect
+      @connected = true
     end
-  end
     
-      # Clean the keg of formula @f
-  def clean
-    ObserverPathnameExtension.reset_counts!
+      test 'unsubscribe from all' do
+    run_in_eventmachine do
+      setup_connection
     
-        outpath = file.gsub('.gz', '')
-    tgz = Zlib::GzipReader.new(File.open(file))
-    begin
-      File.open(outpath, 'w') do |out|
-        IO::copy_stream(tgz, out)
+          Dir.chdir(code_path) do
+        code = file.read
+        @filetype = file.extname.sub('.','') if @filetype.nil?
+        title = @title ? '#{@title} (#{file.basename})' : file.basename
+        url = '/#{code_dir}/#{@file}'
+        source = '<figure class='code'><figcaption><span>#{title}</span> <a href='#{url}'>download</a></figcaption>\n'
+        source += '#{HighlightCode::highlight(code, @filetype)}</figure>'
+        TemplateWrapper::safe_wrap(source)
       end
-      File.unlink(file)
-    rescue
-      File.unlink(outpath) if File.file?(outpath)
-     raise
     end
-    tgz.close
   end
+    
+    module RuboCop
+  module Cop
+    module Style
+      # This cop check for uses of Object#freeze on immutable objects.
+      #
+      # @example
+      #   # bad
+      #   CONST = 1.freeze
+      #
+      #   # good
+      #   CONST = 1
+      class RedundantFreeze < Cop
+        include FrozenStringLiteral
+    
+          # The body of the method definition.
+      #
+      # @note this can be either a `begin` node, if the method body contains
+      #       multiple expressions, or any other node, if it contains a single
+      #       expression.
+      #
+      # @return [Node] the body of the method definition
+      def body
+        node_parts[0]
+      end
