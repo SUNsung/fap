a72c608a02f963613d967cddead40e172c1db3f6
SUNsung <weatherius@gmail.com>
2018-08-19 07:06:56 +0300

    Rewrite [09gkEXFpszwGA09f4A3iMUXHPI09asuL6jgEdk09WsI.rb] File in: 2018-08-19 07:06:56

--- ruby/09gkEXFpszwGA09f4A3iMUXHPI09asuL6jgEdk09WsI.rb
+++ ruby/09gkEXFpszwGA09f4A3iMUXHPI09asuL6jgEdk09WsI.rb
@@ -1,117 +1,42 @@
 
         
-          end
-    
-      class CachedFragment
-    def initialize(json)
-      @json = json
-    end
-    def as_json(*_args)
-      @json
-    end
-  end
-    
-      def complete_option(method)
-    if self.respond_to? 'complete_#{method}'.to_sym
-      self.send('complete_#{method}'.to_sym)
-    end
-  end
-    
-        if params[:sort].present?
-      attribute, direction = params[:sort].downcase.split('.')
-      unless valid_sorts.include?(attribute)
-        attribute, direction = default.to_a.first
-      end
-    else
-      attribute, direction = default.to_a.first
-    end
-    
-      def show
-    respond_to do |format|
-      format.html
-      format.json { render json: @event }
-    end
-  end
-    
-        grouped_codes = codes.reduce([]) do |agg, current|
-      if current[1]
-        agg << [current[0]]
+            def log(message, options = {})
+      return super unless dry_run?
+      case options[:level] || 3
+      when 0..2
+        sev = Logger::DEBUG
+      when 3
+        sev = Logger::INFO
       else
-        agg.last << current[0]
-        agg
+        sev = Logger::ERROR
       end
+    
+      def validate_email_options
+    errors.add(:base, 'subject and expected_receive_period_in_days are required') unless options['subject'].present? && options['expected_receive_period_in_days'].present?
+    
+      included do
+    include Oauthable
+    
+      def load_event
+    @event = current_user.events.find(params[:id])
+  end
+end
+
+    
+    module Paperclip
+  class << self
+    attr_writer :registered_attachments_styles_path
+    def registered_attachments_styles_path
+      @registered_attachments_styles_path ||= Rails.root.join('public/system/paperclip_attachments.yml').to_s
     end
-    
-          it 'sets the regeneration marker to expire' do
-        allow(RegenerationWorker).to receive(:perform_async)
-        get :show
-        expect(Redis.current.ttl('account:#{user.account_id}:regeneration')).to be >= 0
-      end
-    
-      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/favourite
-  def favourite
-    f = Favourite.last
-    NotificationMailer.favourite(f.status.account, Notification.find_by(activity: f))
   end
     
-        it 'redirects requests with sneaky encoded session cookies' do
-      get '/path', {}, 'HTTP_COOKIE' => 'rack.%73ession=EVIL_SESSION_TOKEN; rack.session=SESSION_TOKEN'
-      expect(last_response).to be_redirect
-      expect(last_response.location).to eq('/path')
-    end
+        rake_tasks { load 'tasks/paperclip.rake' }
   end
     
-        it 'leaves TempFiles untouched' do
-      mock_app do |env|
-        request = Rack::Request.new(env)
-        [200, {'Content-Type' => 'text/plain'}, [request.params['file'][:filename] + '\n' + \
-                                                 request.params['file'][:tempfile].read + '\n' + \
-                                                 request.params['other']]]
+          def check_validity!
+        unless options.has_key?(:matches) || options.has_key?(:not)
+          raise ArgumentError, 'You must pass in either :matches or :not to the validator'
+        end
       end
-    
-    
-  it 'should allow changing the protection mode to a string' do
-    # I have no clue what other modes are available
-    mock_app do
-      use Rack::Protection::FrameOptions, :frame_options => 'ALLOW-FROM foo'
-      run DummyApp
     end
-    
-        { # yes, this is ugly, feel free to change that
-      '/..' => '/', '/a/../b' => '/b', '/a/../b/' => '/b/', '/a/.' => '/a/',
-      '/%2e.' => '/', '/a/%2E%2e/b' => '/b', '/a%2f%2E%2e%2Fb/' => '/b/',
-      '//' => '/', '/%2fetc%2Fpasswd' => '/etc/passwd'
-    }.each do |a, b|
-      it('replaces #{a.inspect} with #{b.inspect}') { expect(get(a).body).to eq(b) }
-    end
-    
-      it 'should allow changing the protection mode' do
-    # I have no clue what other modes are available
-    mock_app do
-      use Rack::Protection::XSSHeader, :xss_mode => :foo
-      run DummyApp
-    end
-    
-            def reference!(node)
-          @references << node
-          @referenced = true
-        end
-    
-              each_misplaced_optional_arg(arguments) do |argument|
-            add_offense(argument)
-          end
-        end
-    
-                  [offense.line, '#{indent}#{carets} #{offense.message}\n']
-            end
-    
-        context 'opening brace on same line as first element' do
-      context 'last element has a trailing comma' do
-        it 'autocorrects closing brace on different line as last element' do
-          new_source = autocorrect_source(['#{prefix}#{open}#{a}, # a',
-                                           '#{b}, # b',
-                                           close,
-                                           suffix])
-    
-        files.each do |file|
-      download = file_fetch(file['url'], file['sha1'],target)
