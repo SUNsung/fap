d22ee206006d2ddaf0ece70a91039361074bc355
SUNsung <weatherius@gmail.com>
2018-08-19 05:16:08 +0300

    Rewrite [48FANjs1aD9zM48Ct3X6NK4KYM48gwcLLkw44o48HIC.cpp] File in: 2018-08-19 05:16:08

--- cccccc/48FANjs1aD9zM48Ct3X6NK4KYM48gwcLLkw44o48HIC.cpp
+++ cccccc/48FANjs1aD9zM48Ct3X6NK4KYM48gwcLLkw44o48HIC.cpp
@@ -1,309 +1,64 @@
 
         
-        
-    {}  // namespace tensorflow
-#endif  // TENSORFLOW_CORE_DISTRIBUTED_RUNTIME_WORKER_CACHE_PARTIAL_H_
-
+        @interface MutableDataVerifier : NSMutableData {
+    ObjectBehaviorVerifier *_verifier;
+    NSMutableData *_data;
+}
+@property (readonly) ObjectBehaviorVerifier *verifier;
+@end
     
-    namespace tensorflow {
-REGISTER2(BinaryOp, CPU, 'Mod', functor::safe_mod, int32, int64);
-REGISTER2(BinaryOp, CPU, 'Mod', functor::fmod, float, double);
-REGISTER2(BinaryOp, CPU, 'TruncateMod', functor::safe_mod, int32, int64);
-REGISTER2(BinaryOp, CPU, 'TruncateMod', functor::fmod, float, double);
-    }
+    #include 'swift/Markup/Markup.h'
+#include 'llvm/ADT/Optional.h'
     
-      auto epsilon_literal = LiteralUtil::CreateR0(batch_norm->epsilon());
-  TF_ASSIGN_OR_RETURN(epsilon_literal, epsilon_literal->Convert(ptype));
-  auto epsilon_scalar =
-      add(HloInstruction::CreateConstant(std::move(epsilon_literal)));
-  auto epsilon_activation = add(
-      HloInstruction::CreateBroadcast(activation_shape, epsilon_scalar, {}));
-  auto epsilon_feature =
-      add(HloInstruction::CreateBroadcast(feature_shape, epsilon_scalar, {}));
     
-    class TextLineReaderOp : public ReaderOpKernel {
- public:
-  explicit TextLineReaderOp(OpKernelConstruction* context)
-      : ReaderOpKernel(context) {
-    int skip_header_lines = -1;
-    OP_REQUIRES_OK(context,
-                   context->GetAttr('skip_header_lines', &skip_header_lines));
-    OP_REQUIRES(context, skip_header_lines >= 0,
-                errors::InvalidArgument('skip_header_lines must be >= 0 not ',
-                                        skip_header_lines));
-    Env* env = context->env();
-    SetReaderFactory([this, skip_header_lines, env]() {
-      return new TextLineReader(name(), skip_header_lines, env);
-    });
+    {  static bool classof(const MarkupASTNode *N) {
+    return N->getKind() >= ASTNodeKind::First_Private &&
+      N->getKind() <= ASTNodeKind::Last_Private;
   }
 };
     
-    TF_CALL_GPU_NUMBER_TYPES(REGISTER_GPU);
-TF_CALL_complex64(REGISTER_GPU);
-TF_CALL_complex128(REGISTER_GPU);
-TF_CALL_int64(REGISTER_GPU);
-TF_CALL_int32(REGISTER_GPU)
+    #ifndef SWIFT_SYNTAX_FORMAT_H
+#define SWIFT_SYNTAX_FORMAT_H
     
-      ASSERT_FALSE(
-      DebugIO::IsDebugNodeGateOpen('foo:1:DebugIdentity', {kGrpcUrl1}));
-  ASSERT_FALSE(
-      DebugIO::IsDebugNodeGateOpen('foo:1:DebugNumericSummary', {kGrpcUrl1}));
-  ASSERT_FALSE(
-      DebugIO::IsDebugNodeGateOpen('qux:0:DebugIdentity', {kGrpcUrl1}));
-  ASSERT_TRUE(DebugIO::IsDebugNodeGateOpen('foo:0:DebugIdentity', {kGrpcUrl1}));
-  ASSERT_TRUE(DebugIO::IsDebugNodeGateOpen('bar:0:DebugIdentity', {kGrpcUrl1}));
     
-            template<int CTA_SIZE>
-        static __forceinline__ __device__ int Ballot(int predicate)
+    {} // end namespace swift
+    
+            size_t numSequences = oneHotSequences.size();
+        NDShape valueDataShape = sampleShape.AppendShape({ maxSequenceLength, numSequences });
+        size_t numCSCCols = valueDataShape.SubShape(1).TotalSize() + 1;
+        std::vector<SparseIndexType> colStarts(numCSCCols);
+        std::vector<ElementType> nonZeroValues;
+        std::vector<SparseIndexType> rowIndices;
+        for (size_t i = 0; i < numSequences; ++i)
         {
-#if defined (__CUDA_ARCH__) && (__CUDA_ARCH__ >= 200)
-            return __ballot(predicate);
-#else
-            __shared__ volatile int cta_buffer[CTA_SIZE];
+            size_t currentSequenceNumCols = oneHotSequences[i].size();
+            size_t j = 0;
+            for (; j < currentSequenceNumCols; ++j)
+            {
+                colStarts[(i * maxSequenceNumCols) + j] = (SparseIndexType)nonZeroValues.size();
+                size_t oneHotIdx = oneHotSequences[i][j];
+                if ((oneHotIdx & OneHotSkip) == OneHotSkip) // note that OneHotSkip used to be (size_t)-1, and later changed to (uint32_t)-1. Both are supported
+                {
+                    nonZeroValues.push_back(0);
+                    rowIndices.push_back(0);
+                }
+                else
+                {
+                    nonZeroValues.push_back(1);
+                    if (oneHotIdx >= dimension)
+                        InvalidArgument('Value::Create: one-hot index value (%zu) exceeds vocabulary size (%zu).', oneHotSequences[i][j], dimension);
+                    rowIndices.push_back((SparseIndexType)(oneHotSequences[i][j]));
+                }
+            }
     }
     
+        ComputationNetworkPtr net = make_shared<ComputationNetwork>(deviceID);
+    net->Load<ElemType>(modelPath);
     
-    {    syncOutput(dst, _dst, stream);
-}
+    #pragma once
     
-    const int kCIFARSize = 32;
-const int kCIFARImageNBytes = 3072;
-const int kCIFARBatchSize = 10000;
-const int kCIFARTrainBatches = 5;
-    
-    
-    {  static string LayerTypeListString() {
-    vector<string> layer_types = LayerTypeList();
-    string layer_types_str;
-    for (vector<string>::iterator iter = layer_types.begin();
-         iter != layer_types.end(); ++iter) {
-      if (iter != layer_types.begin()) {
-        layer_types_str += ', ';
-      }
-      layer_types_str += *iter;
-    }
-    return layer_types_str;
-  }
-};
-    
-    /**
- * @brief Index into the input blob along its first axis.
- *
- * This layer can be used to select, reorder, and even replicate examples in a
- * batch.  The second blob is cast to int and treated as an index into the
- * first axis of the first blob.
- */
-template <typename Dtype>
-class BatchReindexLayer : public Layer<Dtype> {
- public:
-  explicit BatchReindexLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    /**
- * @brief Takes at least two Blob%s and concatenates them along either the num
- *        or channel dimension, outputting the result.
- */
-template <typename Dtype>
-class ConcatLayer : public Layer<Dtype> {
- public:
-  explicit ConcatLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    namespace caffe {
-    }
-    
-     protected:
-  /**
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the computed outputs. At training time, we have @f$
-   *      y_{\mbox{train}} = \left\{
-   *         \begin{array}{ll}
-   *            \frac{x}{1 - p} & \mbox{if } u > p \\
-   *            0 & \mbox{otherwise}
-   *         \end{array} \right.
-   *      @f$, where @f$ u \sim U(0, 1)@f$ is generated independently for each
-   *      input at each iteration. At test time, we simply have
-   *      @f$ y_{\mbox{test}} = \mathbb{E}[y_{\mbox{train}}] = x @f$.
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    }
-    
-    #endif  // CAFFE_EMBED_LAYER_HPP_
-
-    
-    int main(int argc, char** argv) {
-  grpc::testing::InitTest(&argc, &argv, true);
-    }
-    
-    #endif  // GRPC_COMMON_CPP_ROUTE_GUIDE_HELPER_H_
-    
-    #include 'test/cpp/interop/server_helper.h'
-#include 'test/cpp/util/test_config.h'
-    
-    #ifndef TEST_QPS_STATS_UTILS_H
-#define TEST_QPS_STATS_UTILS_H
-    
-      int Join();
-  void Interrupt();
-    
-    
+    public:
+    static bool GetTracingFlag()
     {
-    {
-    {  for (const auto& share_line : osquery::split(content, '\n')) {
-    genNFSShare(share_line, results);
-  }
-  return results;
-}
-}
-}
-
-    
-    namespace osquery {
+        return GetStaticInstance().m_tracingFlag;
     }
-    
-      // Start our permissions thread.
-  auto perms_thread = std::make_shared<PermissionsRunnable>();
-  Dispatcher::addService(perms_thread);
-    
-    size_t KinesisLogForwarder::getInitialRetryDelay() const {
-  return 3000U;
-}
-    
-    void TransformBlock(double block[64], Transform1d f) {
-  double tmp[64];
-  for (int x = 0; x < 8; ++x) {
-    f(&block[x], 8, &tmp[x]);
-  }
-  for (int y = 0; y < 8; ++y) {
-    f(&tmp[8 * y], 1, &block[8 * y]);
-  }
-}
-    
-    // Output callback function with associated data.
-struct JPEGOutput {
-  JPEGOutput(JPEGOutputHook cb, void* data) : cb(cb), data(data) {}
-  bool Write(const uint8_t* buf, size_t len) const {
-    return (len == 0) || (cb(data, buf, len) == len);
-  }
- private:
-  JPEGOutputHook cb;
-  void* data;
-};
-    
-    enum JPEGReadError {
-  JPEG_OK = 0,
-  JPEG_SOI_NOT_FOUND,
-  JPEG_SOF_NOT_FOUND,
-  JPEG_UNEXPECTED_EOF,
-  JPEG_MARKER_BYTE_NOT_FOUND,
-  JPEG_UNSUPPORTED_MARKER,
-  JPEG_WRONG_MARKER_SIZE,
-  JPEG_INVALID_PRECISION,
-  JPEG_INVALID_WIDTH,
-  JPEG_INVALID_HEIGHT,
-  JPEG_INVALID_NUMCOMP,
-  JPEG_INVALID_SAMP_FACTOR,
-  JPEG_INVALID_START_OF_SCAN,
-  JPEG_INVALID_END_OF_SCAN,
-  JPEG_INVALID_SCAN_BIT_POSITION,
-  JPEG_INVALID_COMPS_IN_SCAN,
-  JPEG_INVALID_HUFFMAN_INDEX,
-  JPEG_INVALID_QUANT_TBL_INDEX,
-  JPEG_INVALID_QUANT_VAL,
-  JPEG_INVALID_MARKER_LEN,
-  JPEG_INVALID_SAMPLING_FACTORS,
-  JPEG_INVALID_HUFFMAN_CODE,
-  JPEG_INVALID_SYMBOL,
-  JPEG_NON_REPRESENTABLE_DC_COEFF,
-  JPEG_NON_REPRESENTABLE_AC_COEFF,
-  JPEG_INVALID_SCAN,
-  JPEG_OVERLAPPING_SCANS,
-  JPEG_INVALID_SCAN_ORDER,
-  JPEG_EXTRA_ZERO_RUN,
-  JPEG_DUPLICATE_DRI,
-  JPEG_DUPLICATE_SOF,
-  JPEG_WRONG_RESTART_MARKER,
-  JPEG_DUPLICATE_COMPONENT_ID,
-  JPEG_COMPONENT_NOT_FOUND,
-  JPEG_HUFFMAN_TABLE_NOT_FOUND,
-  JPEG_HUFFMAN_TABLE_ERROR,
-  JPEG_QUANT_TABLE_NOT_FOUND,
-  JPEG_EMPTY_DHT,
-  JPEG_EMPTY_DQT,
-  JPEG_OUT_OF_BAND_COEFF,
-  JPEG_EOB_RUN_TOO_LONG,
-  JPEG_IMAGE_TOO_LARGE,
-};
-    
-    // Butteraugli scores that correspond to JPEG quality levels, starting at
-// kLowestQuality. They were computed by taking median BA scores of JPEGs
-// generated using libjpeg-turbo at given quality from a set of PNGs.
-// The scores above quality level 100 are just linearly decreased so that score
-// for 110 is 90% of the score for 100.
-const double kScoreForQuality[] = {
-  2.810761,  // 70
-  2.729300,
-  2.689687,
-  2.636811,
-  2.547863,
-  2.525400,
-  2.473416,
-  2.366133,
-  2.338078,
-  2.318654,
-  2.201674,  // 80
-  2.145517,
-  2.087322,
-  2.009328,
-  1.945456,
-  1.900112,
-  1.805701,
-  1.750194,
-  1.644175,
-  1.562165,
-  1.473608,  // 90
-  1.382021,
-  1.294298,
-  1.185402,
-  1.066781,
-  0.971769,  // 95
-  0.852901,
-  0.724544,
-  0.611302,
-  0.443185,
-  0.211578,  // 100
-  0.209462,
-  0.207346,
-  0.205230,
-  0.203114,
-  0.200999,  // 105
-  0.198883,
-  0.196767,
-  0.194651,
-  0.192535,
-  0.190420,  // 110
-  0.190420,
-};
