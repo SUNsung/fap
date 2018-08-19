aca2c32e64180350b6ab263f69133082c11747a8
SUNsung <weatherius@gmail.com>
2018-08-19 08:55:00 +0300

    Rewrite [01xpfWrxNbCo01acVXQmrk04Y01NUyJkqdCeBw015vJ.rb] File in: 2018-08-19 08:55:00

--- ruby/01xpfWrxNbCo01acVXQmrk04Y01NUyJkqdCeBw015vJ.rb
+++ ruby/01xpfWrxNbCo01acVXQmrk04Y01NUyJkqdCeBw015vJ.rb
@@ -1,128 +1,44 @@
 
         
-          [name, full_name]
-end
+          when '6.1.0'
+    __NR_execve      = 6
+    __NR_getpeername = 203
+    __NR_accept      = 229
+    __NR_listen      = 232
+    __NR_bind        = 234
+    __NR_socket      = 235
+    __NR_connect     = 236
+    __NR_close       = 269
+    __NR_kfcntl      = 617
     
-      # True if a {Formula} is being built with a specific option.
-  # <pre>args << '--i-want-spam' if build.with? 'spam'
-  #
-  # args << '--qt-gui' if build.with? 'qt' # '--with-qt' ==> build.with? 'qt'
-  #
-  # # If a formula presents a user with a choice, but the choice must be fulfilled:
-  # if build.with? 'example2'
-  #   args << '--with-example2'
-  # else
-  #   args << '--with-example1'
-  # end</pre>
-  def with?(val)
-    option_names = val.respond_to?(:option_names) ? val.option_names : [val]
+        it 'Reads referrer from Host header when Referer header is relative' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
+      expect(subject.referrer(env)).to eq('foo.com')
+    end
     
-        if ARGV.include?('--pinned') || ARGV.include?('--versions')
-      filtered_list
-    elsif ARGV.named.empty?
-      if ARGV.include? '--full-name'
-        full_names = Formula.installed.map(&:full_name).sort do |a, b|
-          if a.include?('/') && !b.include?('/')
-            1
-          elsif !a.include?('/') && b.include?('/')
-            -1
-          else
-            a <=> b
-          end
-        end
-        puts_columns full_names
-      else
-        ENV['CLICOLOR'] = nil
-        exec 'ls', *ARGV.options_only << HOMEBREW_CELLAR
-      end
-    elsif ARGV.verbose? || !$stdout.tty?
-      exec 'find', *ARGV.kegs.map(&:to_s) + %w[-not -type d -print]
-    else
-      ARGV.kegs.each { |keg| PrettyListing.new keg }
+          expected_header = <<-END.chomp
+rack.%2573ession=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.%2573ession=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.%2573ession=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.session=; domain=example.org; path=/; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.session=; domain=example.org; path=/some; expires=Thu, 01 Jan 1970 00:00:00 -0000
+rack.session=; domain=example.org; path=/some/path; expires=Thu, 01 Jan 1970 00:00:00 -0000
+END
+      expect(last_response.headers['Set-Cookie']).to eq(expected_header)
     end
   end
     
-        raise SEARCH_ERROR_QUEUE.pop unless SEARCH_ERROR_QUEUE.empty?
-  end
-    
-          options[:only_patterns] = [/\Agetting-started\//, /\Alayout\//, /\Acontent\//, /\Acomponents\//, /\Autilities\//, /\Amigration\//]
-    end
-    
-          options[:fix_urls] = ->(url) do
-        url.sub! %r{/blob/master/readme.md}i, ''
-        url
-      end
-    end
-    
-        options[:container] = '.span9'
-    
-      def test_capture3_stdin_data_io
-    IO.pipe {|r, w|
-      w.write 'i'
-      w.close
-      o, e, s = Open3.capture3(RUBY, '-e', 'i=STDIN.read; print i+'o'; STDOUT.flush; STDERR.print i+'e'', :stdin_data=>r)
-      assert_equal('io', o)
-      assert_equal('ie', e)
-      assert(s.success?)
-    }
-  end
-    
-    def grammar(f, out)
-  while line = f.gets
-    case line
-    when %r</\*% *ripper(?:\[(.*?)\])?: *(.*?) *%\*/>
-      out << DSL.new($2, ($1 || '').split(',')).generate << $/
-    when %r</\*%%%\*/>
-      out << '#if 0' << $/
-    when %r</\*%>
-      out << '#endif' << $/
-    when %r<%\*/>
-      out << $/
-    when /\A%%/
-      out << '%%' << $/
-      return
-    else
-      out << line
-    end
-  end
-end
-    
-      it 'uses 'main' as self' do
-    ruby_exe('puts self', escape: false).chomp.should == 'main'
-  end
-    
-      it 'wraps and unwraps data' do
-    a = @s.typed_wrap_struct(1024)
-    @s.typed_get_struct(a).should == 1024
-  end
-    
-      it 'adds the directory after directories added by -I' do
-    dash_i_dir = tmp('dash_I_include')
-    rubylib_dir = tmp('rubylib_include')
-    ENV['RUBYLIB'] = @pre + rubylib_dir
-    paths = ruby_exe('puts $LOAD_PATH', options: '-I #{dash_i_dir}').lines.map(&:chomp)
-    paths.should include(dash_i_dir)
-    paths.should include(rubylib_dir)
-    paths.index(dash_i_dir).should < paths.index(rubylib_dir)
-  end
-    
-        assert_equal 'some_gem', spec.name
-    assert_equal 'this is a summary', spec.summary
-  end
-    
-    describe :string_unpack_16bit_le_unsigned, shared: true do
-  it 'decodes a short with most significant bit set as a positive number' do
-    '\x00\xff'.unpack(unpack_format()).should == [65280]
-  end
-end
-    
-        if test_conf['database']&.present?
-      ActiveRecord::Base.establish_connection(:test)
-      yield
-      ActiveRecord::Base.establish_connection(Rails.env.to_sym)
+      before(:each) do
+    mock_app do
+      use Rack::Protection::HttpOrigin
+      run DummyApp
     end
   end
     
-      attributes :id, :type, :url, :preview_url,
-             :remote_url, :text_url, :meta,
-             :description
+        { # yes, this is ugly, feel free to change that
+      '/..' => '/', '/a/../b' => '/b', '/a/../b/' => '/b/', '/a/.' => '/a/',
+      '/%2e.' => '/', '/a/%2E%2e/b' => '/b', '/a%2f%2E%2e%2Fb/' => '/b/',
+      '//' => '/', '/%2fetc%2Fpasswd' => '/etc/passwd'
+    }.each do |a, b|
+      it('replaces #{a.inspect} with #{b.inspect}') { expect(get(a).body).to eq(b) }
+    end
