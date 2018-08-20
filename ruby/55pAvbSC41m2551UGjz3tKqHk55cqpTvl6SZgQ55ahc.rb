e0cb13b79416baa2fafabecdd766eaf65b1cff6e
SUNsung <weatherius@gmail.com>
2018-08-20 14:48:44 +0300

    Rewrite [55pAvbSC41m2551UGjz3tKqHk55cqpTvl6SZgQ55ahc.rb] File in: 2018-08-20 14:48:43

--- ruby/55pAvbSC41m2551UGjz3tKqHk55cqpTvl6SZgQ55ahc.rb
+++ ruby/55pAvbSC41m2551UGjz3tKqHk55cqpTvl6SZgQ55ahc.rb
@@ -1,87 +1,76 @@
 
         
-          def serialize_options(resource)
-    methods = resource_class.authentication_keys.dup
-    methods = methods.keys if methods.is_a?(Hash)
-    methods << :password if resource.respond_to?(:password)
-    { methods: methods, only: [:password] }
+        if ARGV.count < 2
+  abort 'Usage: #$0 pidfile command [args...]'
+end
+    
+      def updated
+    object.updated_at.iso8601
   end
     
-          protected
+        #@posts[:new_public] = Post.where(:type => ['StatusMessage','ActivityStreams::Photo'],
+    #                                 :public => true).order('created_at DESC').limit(15).all
     
-        def default_used_route(options)
-      singularizer = lambda { |s| s.to_s.singularize.to_sym }
-    
-    See CONTRIBUTING.md for more information.
-    
-    require 'open3'
-require 'set'
-    
-    require 'extend/os/cleaner'
+          # If this importer is based on files on the local filesystem This method
+      # should return true if the file, when changed, should trigger a
+      # recompile.
+      #
+      # It is acceptable for non-sass files to be watched and trigger a recompile.
+      #
+      # @param filename [String] The absolute filename for a file that has changed.
+      # @return [Boolean] When the file changed should cause a recompile.
+      def watched_file?(filename)
+        false
+      end
+    end
+  end
+end
 
     
-        it 'should be able to deal with PATH_INFO = nil (fcgi?)' do
-      app = Rack::Protection::PathTraversal.new(proc { 42 })
-      expect(app.call({})).to eq(42)
+            {
+          :always_update     => false,
+          :template_location => root + '/public/stylesheets/sass',
+          :css_location      => root + '/public/stylesheets',
+          :cache_location    => root + '/tmp/sass-cache',
+          :always_check      => env != 'production',
+          :quiet             => env != 'production',
+          :full_exception    => env != 'production'
+        }.freeze
+      end
     end
   end
     
-      it 'allows passing on values in env' do
-    klass    = described_class
-    changer  = Struct.new(:app) do
-      def call(env)
-        env['foo.bar'] = 42
-        app.call(env)
+        # Converts a script node into a corresponding string interpolation
+    # expression.
+    #
+    # @param node_or_interp [Sass::Script::Tree::Node]
+    # @return [Sass::Script::Tree::StringInterpolation]
+    def to_string_interpolation(node_or_interp)
+      unless node_or_interp.is_a?(Interpolation)
+        node = node_or_interp
+        return string_literal(node.value.to_s) if node.is_a?(Literal)
+        if node.is_a?(StringInterpolation)
+          return concat(string_literal(node.quote), concat(node, string_literal(node.quote)))
+        end
+        return StringInterpolation.new(string_literal(''), node, string_literal(''))
       end
-    end
-    detector = Struct.new(:app) do
-      def call(env)
-        app.call(env)
-      end
-    end
     
-    module RuboCop
-  module Cop
-    module Layout
-      # Checks for unnecessary additional spaces inside array percent literals
-      # (i.e. %i/%w).
-      #
-      # @example
-      #
-      #   # bad
-      #   %w(foo  bar  baz)
-      #   # good
-      #   %i(foo bar baz)
-      class SpaceInsideArrayPercentLiteral < Cop
-        include MatchRange
-        include PercentLiteral
+            def initialize(argv)
+          @pod_name = argv.shift_argument
+          @wipe_all = argv.flag?('all')
+          super
+        end
     
-            def autocorrect(node)
-          # Regexp#match can take a second argument, but this cop doesn't
-          # register an offense in that case
-          return unless node.first_argument.regexp_type?
+      # GET /books/new
+  def new
+    @book = Book.new
+  end
     
-                previous.push(when_node.conditions)
+            private
+    
+                yield arguments[optarg_position]
           end
         end
     
-          # The body of the method definition.
-      #
-      # @note this can be either a `begin` node, if the method body contains
-      #       multiple expressions, or any other node, if it contains a single
-      #       expression.
-      #
-      # @return [Node] the body of the method definition
-      def body
-        node_parts[0]
-      end
-    
-          # Checks whether the `if` node has an `else` clause.
-      #
-      # @note This returns `true` for nodes containing an `elsif` clause.
-      #       This is legacy behavior, and many cops rely on it.
-      #
-      # @return [Boolean] whether the node has an `else` clause
-      def else?
-        loc.respond_to?(:else) && loc.else
-      end
+            MSG = 'Do not freeze immutable objects, as freezing them has no ' \
+              'effect.'.freeze
