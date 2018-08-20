907287d435f24734db9f63cbe0366b7693c61417
SUNsung <weatherius@gmail.com>
2018-08-20 18:23:33 +0300

    Rewrite [20L.T6cD9vF7.20bLB.C1xX8bo20KD4OnTBoUAk20w6j.cpp] File in: 2018-08-20 18:23:33

--- cccccc/20L.T6cD9vF7.20bLB.C1xX8bo20KD4OnTBoUAk20w6j.cpp
+++ cccccc/20L.T6cD9vF7.20bLB.C1xX8bo20KD4OnTBoUAk20w6j.cpp
@@ -1,174 +1,168 @@
 
         
-        He, Kaiming, et al. 'Mask R-CNN.' ICCV, 2017
-)code' ADD_FILELINE)
-.set_num_inputs(2)
-.set_num_outputs(1)
-.set_attr<nnvm::FListInputNames>('FListInputNames',
-    [](const NodeAttrs& attrs) {
-  return std::vector<std::string>{'data', 'rois'};
-})
-.set_attr<nnvm::FListOutputNames>('FListOutputNames',
-    [](const NodeAttrs& attrs) {
-  return std::vector<std::string>{'output'};
-})
-.set_attr_parser(ParamParser<ROIAlignParam>)
-.set_attr<nnvm::FInferShape>('FInferShape', [](const nnvm::NodeAttrs& attrs,
-      std::vector<TShape> *in_shape, std::vector<TShape> *out_shape){
-  using namespace mshadow;
-  const ROIAlignParam param = nnvm::get<ROIAlignParam>(attrs.parsed);
-  CHECK_EQ(in_shape->size(), 2) << 'Input:[data, rois]';
-  // data: [batch_size, c, h, w]
-  TShape dshape = in_shape->at(roialign::kData);
-  CHECK_EQ(dshape.ndim(), 4) << 'data should be a 4D tensor';
-  // bbox: [num_rois, 5]
-  TShape bshape = in_shape->at(roialign::kBox);
-  CHECK_EQ(bshape.ndim(), 2) << 'bbox should be a 2D tensor of shape [batch, 5]';
-  CHECK_EQ(bshape[1], 5) << 'bbox should be a 2D tensor of shape [batch, 5]';
-  // out: [num_rois, c, pooled_h, pooled_w]
-  out_shape->clear();
-  out_shape->push_back(
-       Shape4(bshape[0], dshape[1], param.pooled_size[0], param.pooled_size[1]));
-  return true;
-})
-.set_attr<nnvm::FInferType>('FInferType', [](const nnvm::NodeAttrs& attrs,
-      std::vector<int> *in_type, std::vector<int> *out_type) {
-  CHECK_EQ(in_type->size(), 2);
-  int dtype = (*in_type)[0];
-  CHECK_EQ(dtype, (*in_type)[1]);
-  CHECK_NE(dtype, -1) << 'Input must have specified type';
-    }
-    
-    // Transfer only gradient to FGradient function
-struct ElemwiseGradUseNone {
-  const char *op_name;
-  std::vector<nnvm::NodeEntry> operator()(const nnvm::NodePtr& n,
-                                          const std::vector<nnvm::NodeEntry>& ograds) const {
-    return MakeNonlossGradNode(op_name, n, ograds, {}, n->attrs.dict);
+          Optional<const swift::markup::Paragraph *> getBrief() const {
+    return Parts.Brief;
   }
+    
+      /// Keep emitting subsequent diagnostics after a fatal error.
+  bool ShowDiagnosticsAfterFatalError = false;
+    
+    #ifndef SWIFT_INDEX_INDEXSYMBOL_H
+#define SWIFT_INDEX_INDEXSYMBOL_H
+    
+      virtual void failed(StringRef error) = 0;
+  virtual void warning(StringRef warning) {}
+    
+    
+    {private:
+  ResultPlanPtr buildTopLevelResult(Initialization *init, SILLocation loc);
 };
     
-    class LogUniformGenerator {
-private:
-  const int range_max_;
-  const double log_range_max_;
-  std::default_random_engine generator_;
-  std::uniform_real_distribution<double> distribution_;
-public:
-  LogUniformGenerator(const int);
-  std::unordered_set<long> draw(const size_t, int*);
-};
-    
-    const SparsePage& SparsePageSource::Value() const {
-  return *page_;
-}
-    
-    // data
-#include '../src/data/data.cc'
-#include '../src/data/simple_csr_source.cc'
-#include '../src/data/simple_dmatrix.cc'
-#include '../src/data/sparse_page_raw_format.cc'
-    
-    TEST(Metric, RMSE) {
-  xgboost::Metric * metric = xgboost::Metric::Create('rmse');
-  ASSERT_STREQ(metric->Name(), 'rmse');
-  EXPECT_NEAR(GetMetricEval(metric, {0, 1}, {0, 1}), 0, 1e-10);
-  EXPECT_NEAR(GetMetricEval(metric,
-                            {0.1f, 0.9f, 0.1f, 0.9f},
-                            {  0,   0,   1,   1}),
-              0.6403f, 0.001f);
-}
-    
-    namespace xgboost {
-    }
-    
-    
-    {  for (size_t i = 0; i < nindptr; ++i) {
-    col_ptr_[i] = static_cast<size_t>(p_indptr[i]);
+      /// Materialize the SILValue that this LSValue represents.
+  ///
+  /// In the case where we have a single value this can be materialized by
+  /// applying Path to the Base.
+  SILValue materialize(SILInstruction *Inst) {
+    if (CoveringValue)
+      return SILValue();
+    return Path.getValue().createExtract(Base, Inst, true);
   }
-  #pragma omp parallel for schedule(static)
-  for (int64_t i = 0; i < static_cast<int64_t>(ndata); ++i) {
-    indices_[i] = static_cast<unsigned>(p_indices[i]);
-    data_[i] = static_cast<float>(p_data[i]);
+    
+    class FixitApplyDiagnosticConsumer final
+  : public DiagnosticConsumer, public FixitFilter {
+  clang::RewriteBuffer RewriteBuf;
+    }
+    
+    public:
+  Context(StringRef RuntimeLibPath,
+          llvm::function_ref<
+              std::unique_ptr<LangSupport>(Context &)> LangSupportFactoryFn,
+          bool shouldDispatchNotificationsOnMain = true);
+  ~Context();
+    
+        DXGI_SWAP_CHAIN_DESC sd;
+    ZeroMemory( &sd, sizeof( sd ) );
+    sd.BufferCount = 1;
+    sd.BufferDesc.Width = width;
+    sd.BufferDesc.Height = height;
+#ifdef CHECK_NV12
+    sd.BufferDesc.Format = DXGI_FORMAT_NV12;
+#else
+    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+#endif
+    sd.BufferDesc.RefreshRate.Numerator = 60;
+    sd.BufferDesc.RefreshRate.Denominator = 1;
+    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    sd.OutputWindow = NULL; //g_hWnd;
+    sd.SampleDesc.Count = 1;
+    sd.SampleDesc.Quality = 0;
+    sd.Windowed = TRUE;
+    
+            const std::vector<Point>* quads[4] = {&black_contours[black_order[0].first], &black_contours[black_order[1].first],
+                                         &white_contours[white_order[0].first], &white_contours[white_order[1].first]};
+        std::vector<Point2f> quads_approx[4];
+        Point2f quad_corners[4];
+        for(int k = 0; k < 4; k++)
+        {
+            std::vector<Point2f> temp;
+            for(size_t j = 0; j < quads[k]->size(); j++) temp.push_back((*quads[k])[j]);
+            approxPolyDP(Mat(temp), quads_approx[k], 0.5, true);
+    }
+    
+    /** @file
+ * @deprecated Use @ref cudev instead.
+ */
+    
+    // This macro is used for implementing macros such as
+// EXPECT_DEATH_IF_SUPPORTED and ASSERT_DEATH_IF_SUPPORTED on systems where
+// death tests are not supported. Those macros must compile on such systems
+// iff EXPECT_DEATH and ASSERT_DEATH compile with the same parameters on
+// systems that support death tests. This allows one to write such a macro
+// on a system that does not support death tests and be sure that it will
+// compile on a death-test supporting system.
+//
+// Parameters:
+//   statement -  A statement that a macro such as EXPECT_DEATH would test
+//                for program termination. This macro has to make sure this
+//                statement is compiled but not executed, to ensure that
+//                EXPECT_DEATH_IF_SUPPORTED compiles with a certain
+//                parameter iff EXPECT_DEATH compiles with it.
+//   regex     -  A regex that a macro such as EXPECT_DEATH would use to test
+//                the output of statement.  This parameter has to be
+//                compiled but not evaluated by this macro, to ensure that
+//                this macro only accepts expressions that a macro such as
+//                EXPECT_DEATH would accept.
+//   terminator - Must be an empty statement for EXPECT_DEATH_IF_SUPPORTED
+//                and a return statement for ASSERT_DEATH_IF_SUPPORTED.
+//                This ensures that ASSERT_DEATH_IF_SUPPORTED will not
+//                compile inside functions where ASSERT_DEATH doesn't
+//                compile.
+//
+//  The branch that has an always false condition is used to ensure that
+//  statement and regex are compiled (and thus syntactically correct) but
+//  never executed. The unreachable code macro protects the terminator
+//  statement from generating an 'unreachable code' warning in case
+//  statement unconditionally returns or throws. The Message constructor at
+//  the end allows the syntax of streaming additional messages into the
+//  macro, for compilational compatibility with EXPECT_DEATH/ASSERT_DEATH.
+# define GTEST_UNSUPPORTED_DEATH_TEST_(statement, regex, terminator) \
+    GTEST_AMBIGUOUS_ELSE_BLOCKER_ \
+    if (::testing::internal::AlwaysTrue()) { \
+      GTEST_LOG_(WARNING) \
+          << 'Death tests are not supported on this platform.\n' \
+          << 'Statement '' #statement '' cannot be verified.'; \
+    } else if (::testing::internal::AlwaysFalse()) { \
+      ::testing::internal::RE::PartialMatch('.*', (regex)); \
+      GTEST_SUPPRESS_UNREACHABLE_CODE_WARNING_BELOW_(statement); \
+      terminator; \
+    } else \
+      ::testing::Message()
+    
+    // INTERNAL IMPLEMENTATION - DO NOT USE IN USER CODE.
+//
+// Stores a parameter value and later creates tests parameterized with that
+// value.
+template <class TestClass>
+class ParameterizedTestFactory : public TestFactoryBase {
+ public:
+  typedef typename TestClass::ParamType ParamType;
+  explicit ParameterizedTestFactory(ParamType parameter) :
+      parameter_(parameter) {}
+  virtual Test* CreateTest() {
+    TestClass::SetParam(&parameter_);
+    return new TestClass();
   }
-  DMatrixHandle handle;
-  CHECK_CALL(XGDMatrixCreateFromCSCEx(BeginPtr(col_ptr_), BeginPtr(indices_),
-                                      BeginPtr(data_), nindptr, ndata,
-                                      nrow, &handle));
-  ret = PROTECT(R_MakeExternalPtr(handle, R_NilValue, R_NilValue));
-  R_RegisterCFinalizerEx(ret, _DMatrixFinalizer, TRUE);
-  R_API_END();
-  UNPROTECT(1);
-  return ret;
+    }
+    
+      // Formats a byte as '%02X'.
+  static std::string FormatByte(unsigned char value);
+    
+      explicit tuple(GTEST_BY_REF_(T0) f0, GTEST_BY_REF_(T1) f1,
+      GTEST_BY_REF_(T2) f2, GTEST_BY_REF_(T3) f3, GTEST_BY_REF_(T4) f4,
+      GTEST_BY_REF_(T5) f5, GTEST_BY_REF_(T6) f6, GTEST_BY_REF_(T7) f7,
+      GTEST_BY_REF_(T8) f8) : f0_(f0), f1_(f1), f2_(f2), f3_(f3), f4_(f4),
+      f5_(f5), f6_(f6), f7_(f7), f8_(f8) {}
+    
+    // A unique type used as the default value for the arguments of class
+// template Types.  This allows us to simulate variadic templates
+// (e.g. Types<int>, Type<int, double>, and etc), which C++ doesn't
+// support directly.
+struct None {};
+    
+    
+    {  return result;
 }
     
-          cbw.Write(buffer.data(), input.begin(), input.end());
-    
-    // Reads the Start of Scan (SOS) marker segment and fills in *scan_info with the
-// parsed data.
-bool ProcessSOS(const uint8_t* data, const size_t len, size_t* pos,
-                JPEGData* jpg) {
-  const size_t start_pos = *pos;
-  VERIFY_LEN(3);
-  size_t marker_len = ReadUint16(data, pos);
-  int comps_in_scan = ReadUint8(data, pos);
-  VERIFY_INPUT(comps_in_scan, 1, static_cast<int>(jpg->components.size()),
-               COMPS_IN_SCAN);
+      if (options->Has(String::NewSymbol('target_level')) &&
+      options->Get(String::NewSymbol('target_level'))->IsInt32()) {
+    target_level = (int)(options->Get(
+        String::NewSymbol('target_level'))->ToInt32()->Value());
     }
     
-      // Returns true if the argument of the last Compare() call (or the baseline
-  // image, if Compare() was not called yet) meets the image acceptance
-  // criteria. The target_mul modifies the acceptance criteria used in this call
-  // the following way:
-  //    = 1.0 : the original acceptance criteria is used,
-  //    < 1.0 : a more strict acceptance criteria is used,
-  //    > 1.0 : a less strict acceptance criteria is used.
-  virtual bool DistanceOK(double target_mul) const = 0;
-    
-    double Normal(double x, double sigma) {
-  static const double kInvSqrt2Pi = 0.3989422804014327;
-  return std::exp(-x * x / (2 * sigma * sigma)) * kInvSqrt2Pi / sigma;
-}
-    
-    
-    {}  // namespace guetzli
-
-    
-    #endif  // GUETZLI_DCT_DOUBLE_H_
-
-    
-    namespace guetzli {
+    #if defined (__cplusplus)
+namespace rocksdb {
+#endif
     }
-    
-    
-    {}  // namespace guetzli
-    
-    #include 'guetzli/gamma_correct.h'
-    
-      tmp0 = in[3 * stride];
-  tmp1 = kIDCTMatrix[ 3] * tmp0;
-  tmp2 = kIDCTMatrix[11] * tmp0;
-  tmp3 = kIDCTMatrix[19] * tmp0;
-  tmp4 = kIDCTMatrix[27] * tmp0;
-  out[0] += tmp1;
-  out[1] += tmp2;
-  out[2] += tmp3;
-  out[3] += tmp4;
-  out[4] -= tmp4;
-  out[5] -= tmp3;
-  out[6] -= tmp2;
-  out[7] -= tmp1;
-    
-    #include 'guetzli/jpeg_data.h'
-    
-      private:
-    explicit DBWrapper();
-    ~DBWrapper();
-    
-    namespace rocksdb {
-    }
-    
-    #include 'rocksjni/statisticsjni.h'
     
     class CompactionFilterFactoryJniCallback : public JniCallback, public CompactionFilterFactory {
  public:
@@ -179,56 +173,55 @@ bool ProcessSOS(const uint8_t* data, const size_t len, size_t* pos,
     virtual const char* Name() const;
     }
     
-    /**
- * This class acts as a bridge between C++
- * and Java. The methods in this class will be
- * called back from the RocksDB storage engine (C++)
- * we then callback to the appropriate Java method
- * this enables Comparators to be implemented in Java.
- *
- * The design of this Comparator caches the Java Slice
- * objects that are used in the compare and findShortestSeparator
- * method callbacks. Instead of creating new objects for each callback
- * of those functions, by reuse via setHandle we are a lot
- * faster; Unfortunately this means that we have to
- * introduce independent locking in regions of each of those methods
- * via the mutexs mtx_compare and mtx_findShortestSeparator respectively
- */
-class BaseComparatorJniCallback : public JniCallback, public Comparator {
- public:
-    BaseComparatorJniCallback(
-      JNIEnv* env, jobject jComparator,
-      const ComparatorJniCallbackOptions* copt);
-    virtual const char* Name() const;
-    virtual int Compare(const Slice& a, const Slice& b) const;
-    virtual void FindShortestSeparator(
-      std::string* start, const Slice& limit) const;
-    virtual void FindShortSuccessor(std::string* key) const;
-    }
     
-      std::mutex              mutex_;
-  std::condition_variable cv_;
-  // sync points that have been passed through
-  std::unordered_set<std::string> cleared_points_;
-  std::atomic<bool> enabled_;
-  int num_callbacks_running_ = 0;
-    
-    void StatisticsImpl::histogramData(uint32_t histogramType,
-                                   HistogramData* const data) const {
-  MutexLock lock(&aggregate_lock_);
-  getHistogramImplLocked(histogramType)->Data(data);
-}
-    
-    Status CompactedDBImpl::Get(const ReadOptions& options, ColumnFamilyHandle*,
-                            const Slice& key, PinnableSlice* value) {
-  GetContext get_context(user_comparator_, nullptr, nullptr, nullptr,
-                         GetContext::kNotFound, key, value, nullptr, nullptr,
-                         nullptr, nullptr);
-  LookupKey lkey(key, kMaxSequenceNumber);
-  files_.files[FindFile(key)].fd.table_reader->Get(options, lkey.internal_key(),
-                                                   &get_context, nullptr);
-  if (get_context.State() == GetContext::kFound) {
-    return Status::OK();
+    {  assert(odds > 0);
+  if (odds % 7 == 0) {
+    // class Random uses multiplier 16807, which is 7^5. If odds are
+    // multiplier of 7, there might be limited values generated.
+    odds++;
   }
-  return Status::NotFound();
+  auto* r = Random::GetTLSInstance();
+  bool crash = r->OneIn(odds);
+  if (crash) {
+    port::Crash(srcfile, srcline);
+  }
 }
+    
+    #include 'port/port.h'
+#include 'util/random.h'
+    
+    
+    {} // namespace aria2
+
+    
+    AbstractBtMessage::~AbstractBtMessage() = default;
+    
+      bool shouldProcess() const;
+    
+    
+    {  virtual bool execute() CXX11_OVERRIDE;
+};
+    
+    bool AbstractOptionHandler::isHidden() const { return flags_ & FLAG_HIDDEN; }
+    
+    public:
+  AbstractProxyRequestCommand(cuid_t cuid, const std::shared_ptr<Request>& req,
+                              const std::shared_ptr<FileEntry>& fileEntry,
+                              RequestGroup* requestGroup, DownloadEngine* e,
+                              const std::shared_ptr<Request>& proxyRequest,
+                              const std::shared_ptr<SocketCore>& s);
+    
+    
+    {  void setBtAnnounce(const std::shared_ptr<BtAnnounce>& btAnnounce);
+};
+    
+      /**
+   * The internal announce URL pointer points to next URL.
+   * If the current URL is the last element of its group, then the first
+   * element of the next group is pointed.
+   */
+  void announceFailure();
+    
+    #include 'Notifier.h'
+    
+    #include 'common.h'
