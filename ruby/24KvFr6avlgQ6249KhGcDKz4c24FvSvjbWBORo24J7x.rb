d8831c50c4c891e633848dbd223bddf34f2ebd4b
SUNsung <weatherius@gmail.com>
2018-08-20 23:22:02 +0300

    Rewrite [24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb] File in: 2018-08-20 23:22:01

--- ruby/24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb
+++ ruby/24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb
@@ -1,67 +1,49 @@
 
         
-          def present_hash(hash, skip_key = nil)
-    hash.to_a.sort_by {|a| a.first.to_s }.map { |k, v| '#{k}: #{v}' unless k.to_s == skip_key.to_s }.compact
+        module Jekyll
+  binding.pry
+end
+
+    
+    unless File.exist?(PROF_OUTPUT_FILE)
+  StackProf.run(
+    mode: MODE.to_sym,
+    interval: 100,
+    raw: true,
+    out: PROF_OUTPUT_FILE
+  ) do
+    puts 'GC Stats:', JSON.pretty_generate(GC.stat)
+    GC.disable
+    
+    # Create an array of vendored mimetype => [extensions]
+mimes = {}
+json = open('https://raw.githubusercontent.com/jshttp/mime-db/master/db.json').read
+data = JSON.parse(json)
+data.reject! { |mime, meta| meta['extensions'].nil? || meta['extensions'].empty? }
+data.each do |mime, meta|
+  # Normalize extensions and mime-types
+  mime = mime.downcase.strip
+  extensions = meta['extensions'].map { |e| e.downcase.strip }.compact
+    
+        def self.require_from_bundler
+      if !ENV['JEKYLL_NO_BUNDLER_REQUIRE'] && File.file?('Gemfile')
+        require 'bundler'
+    
+            end
+      end
+    end
   end
 end
 
     
-          _form_configurable_fields[name] = options
-    end
-    
-          respond_to do |format|
-        if new_credentials.map(&:save).all?
-          format.html { redirect_to user_credentials_path, notice: 'The file was successfully uploaded.'}
-        else
-          format.html { redirect_to user_credentials_path, notice: 'One or more of the uploaded credentials was not imported due to an error. Perhaps an existing credential had the same name?'}
-        end
-      end
-    else
-      redirect_to user_credentials_path, notice: 'No file was chosen to be uploaded.' 
-    end
-  end
-    
-        mkdir_p page_dir
-    file = '#{page_dir}/#{filename}.#{extension}'
-    if File.exist?(file)
-      abort('rake aborted!') if ask('#{file} already exists. Do you want to overwrite?', ['y', 'n']) == 'n'
-    end
-    puts 'Creating new page: #{file}'
-    open(file, 'w') do |page|
-      page.puts '---'
-      page.puts 'layout: page'
-      page.puts 'title: \'#{title}\''
-      page.puts 'date: #{Time.now.strftime('%Y-%m-%d %H:%M')}'
-      page.puts 'comments: true'
-      page.puts 'sharing: true'
-      page.puts 'footer: true'
-      page.puts '---'
-    end
-  else
-    puts 'Syntax error: #{args.filename} contains unsupported characters'
-  end
-end
-    
-          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
-        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
-        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
-          @img['title']  = title
-          @img['alt']    = alt
-        else
-          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
-        end
-        @img['class'].gsub!(/'/, '') if @img['class']
-      end
-      super
-    end
-    
-            def show
-          authorize! :read, @order, order_token
-          @address = find_address
-          respond_with(@address)
-        end
-    
-      module CompareExpression0
-    def rvalue1
+      module Condition0
+    def _1
       elements[0]
     end
+    
+          if download =~ /.tar.gz/
+        prefix = download.gsub('.tar.gz', '').gsub('#{target}/', '')
+        untar(download) do |entry|
+          next unless out = eval_file(entry, file['files'], prefix)
+          File.join(target, out)
+        end
