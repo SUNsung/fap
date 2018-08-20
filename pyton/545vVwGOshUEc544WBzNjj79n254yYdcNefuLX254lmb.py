ee86ad51a7110a3e5f043d23d23ce15b2480ab77
SUNsung <weatherius@gmail.com>
2018-08-20 20:22:06 +0300

    Rewrite [545vVwGOshUEc544WBzNjj79n254yYdcNefuLX254lmb.py] File in: 2018-08-20 20:22:05

--- pyton/545vVwGOshUEc544WBzNjj79n254yYdcNefuLX254lmb.py
+++ pyton/545vVwGOshUEc544WBzNjj79n254yYdcNefuLX254lmb.py
@@ -1,43 +1,88 @@
 
         
-        
-class PollErrorTest(unittest.TestCase):
-    '''Tests for acme.errors.PollError.'''
+            def reducer(self, key, values):
+        total = sum(values)
+        if total == 1:
+            yield key, total
     
+        def get(self, key):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                return item.value
+        raise KeyError('Key not found')
     
-def create_response(status_code=200, headers=None):
-    '''Creates a requests.Response object for testing'''
-    response = requests.Response()
-    response.status_code = status_code
-    
-    
-if __name__ == '__main__':
-    main()
+            return {
+            'id': flv_id,
+            'url': rtmp_url,
+            'ext': 'flv',
+            'no_resume': True,
+            'title': title,
+            'description': description,
+            'duration': duration,
+            'view_count': view_count,
+            'comment_count': comment_count,
+            'uploader': uploader,
+            'upload_date': upload_date,
+        }
 
     
-        def test_weak_etag_not_match(self):
-        computed_etag = ''xyzzy2''
-        etags = 'W/'xyzzy1''
-        self.check_url(
-            '/cache/' + computed_etag, method='GET',
-            headers=[('If-None-Match', etags)],
-            expected_status=200)
+    from .onet import OnetBaseIE
     
-        url = options.url + '/updateReports?agent=%s' % options.name
-    update_ws = yield websocket_connect(url, None)
-    msg = yield update_ws.read_message()
-    assert msg is None
-    IOLoop.instance().stop()
+                if next(lineiter).count('-') != len(match.group(0)):
+                continue
     
     
-def main():
-    tornado.options.parse_command_line()
-    if not (options.facebook_api_key and options.facebook_secret):
-        print('--facebook_api_key and --facebook_secret must be set')
-        return
-    http_server = tornado.httpserver.HTTPServer(Application())
-    http_server.listen(options.port)
-    tornado.ioloop.IOLoop.current().start()
+@pytest.mark.functional
+def test_without_confirmation(proc, TIMEOUT):
+    without_confirmation(proc, TIMEOUT)
     
-    import time
-from datetime import timedelta
+            if self._task_class == 'true':
+            junit_classname = re.sub(r'\.yml:[0-9]+$', '', task_data.path)
+        else:
+            junit_classname = task_data.path
+    
+            self.appid_manager = AppidManager(self.config, logger)
+    
+    import os
+import sys
+from .common import *
+    
+                self.cmd_queue[seq] = data.get_buf()
+    
+    - tree.RewriteCardinalityException
+  - tree.RewriteEarlyExitException
+  - tree.RewriteEmptyStreamException
+  .
+.
+    
+            self.input.consume()
+    
+        def test_not_equal(self):
+        self.assertFalse(self.addr1 != self.addr2)
+        self.assertTrue(self.addr != self.addr1)
+    
+    import os
+import sys
+import codecs
+import re
+    
+        def run_loop(self):
+        while self.blackboard.common_state['progress'] < 100:
+            for expert in self.blackboard.experts:
+                if expert.is_eager_to_contribute:
+                    expert.contribute()
+        return self.blackboard.common_state['contributions']
+    
+        def setUp(self):
+        self.sample_queue = queue.Queue()
+        self.sample_queue.put('first')
+        self.sample_queue.put('second')
+    
+        def test_tc2_output(self):
+        self.tc2.run()
+        output = self.out.getvalue().strip()
+        self.assertEqual(output, self.average_result_tc2)
+    
+        def setDB(self, db):
+        self._db = db
