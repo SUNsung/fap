8f28f04b0e30d1152ca9fdc475df6eee3bc913d4
SUNsung <weatherius@gmail.com>
2018-08-20 08:01:25 +0300

    Rewrite [22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb] File in: 2018-08-20 08:01:24

--- ruby/22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb
+++ ruby/22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb
@@ -1,74 +1,97 @@
 
         
-          def evernote_client
-    EvernoteOAuth::Client.new(
-      token:           evernote_oauth_token,
-      consumer_key:    evernote_consumer_key,
-      consumer_secret: evernote_consumer_secret,
-      sandbox:         use_sandbox?
-    )
-  end
-    
-        def schedule(method, args, &blk)
-      @scheduler.send(method, *args, tag: id, &blk)
+            it 'returns a complex number if self is negative and the passed argument is not 0' do
+      (Rational(-3, 2) ** 1.5).should be_close(Complex(0.0, -1.8371173070873836), TOLERANCE)
+      (Rational(3, -2) ** 1.5).should be_close(Complex(0.0, -1.8371173070873836), TOLERANCE)
+      (Rational(3, -2) ** -1.5).should be_close(Complex(0.0, 0.5443310539518174), TOLERANCE)
     end
     
-      def show
-    respond_to do |format|
-      format.html
-      format.json { render json: @event }
-    end
-  end
-    
-      def new
-    @user_credential = current_user.user_credentials.build
-    
-          options[:skip_patterns] = [/\A(?:cpp|cs|flash|java|js|neko|php|python|lua|hl|sys)/i]
+        it 'evaluates the second conditions lazily with exclusive-end range' do
+      collector = proc { |i| ScratchPad << i }
+      eval '10.times { |i| i if (i == 4)..collector[i] }'
+      ScratchPad.recorded.should == [4]
     end
     
-        options[:only_patterns] = [
-      /\Abook\/first-edition\//,
-      /\Areference\//,
-      /\Acollections\//,
-      /\Astd\// ]
-    
-        delegate :empty?, :blank?, to: :pages
-    
-      # Update version.rb file with BOOTSTRAP_SHA
-  def store_version
-    path    = 'lib/bootstrap-sass/version.rb'
-    content = File.read(path).sub(/BOOTSTRAP_SHA\s*=\s*[''][\w]+['']/, 'BOOTSTRAP_SHA = '#@branch_sha'')
-    File.open(path, 'w') { |f| f.write(content) }
+    describe 'CApiWrappedTypedStruct' do
+  before :each do
+    @s = CApiWrappedTypedStructSpecs.new
   end
-end
-
     
-      def setup
-    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
-    success = Dir.chdir DUMMY_PATH do
-      silence_stdout_if !ENV['VERBOSE'] do
-        system 'node', 'manifest.js', tmp_dir
+      ##
+  # Gem::Security default exception type
+    
+      def test_fetch_unmodified
+    uri = URI.parse '#{@gem_repo}/specs.#{Gem.marshal_version}'
+    t = Time.utc(2013, 1, 2, 3, 4, 5)
+    conn, response = util_stub_net_http(:body => '', :code => 304) do |c|
+      @request = make_request(uri, Net::HTTP::Get, t, nil)
+      [c, @request.fetch]
+    end
+    
+          Tempfile.create('test_webrick_auth') {|tmpfile|
+        tmpfile.close
+        tmp_pass = WEBrick::HTTPAuth::Htdigest.new(tmpfile.path)
+        tmp_pass.set_passwd(realm, 'webrick', 'supersecretpassword')
+        tmp_pass.set_passwd(realm, 'foo', 'supersecretpassword')
+        tmp_pass.flush
+    }
+    
+          ##
+      # Retrieves a password from the database for +user+ in +realm+.  If
+      # +reload_db+ is true the database will be reloaded first.
+    
+    
+    {        (candidates|ReservedWords).grep(/^#{Regexp.quote(input)}/)
       end
-    end
-    assert success, 'Node.js Mincer compilation failed'
-    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
-    css_name = manifest['assets']['application.css']
-    @css = File.read('#{tmp_dir}/#{css_name}')
-  end
-end
-
+    }
     
-            p environment.var(name)
-      else
-        p Script::Parser.parse(text, @line, 0).perform(environment)
+    class Dir
+  def self.mktmpdir(path)
+    path = File.join(tmpdir, path+'-#{$$}-#{rand(100000)}')
+    begin
+      mkdir(path)
+    rescue Errno::EEXIST
+      path.succ!
+      retry
+    end
+    path
+  end unless respond_to?(:mktmpdir)
+end
+    
+      in_temporary_working_directory(dir) {
+    exec_test pathes
+  }
+end
+    
+        nr_threads.times do |i|
+      if (i != 0)
+        mutex.unlock
       end
-    rescue Sass::SyntaxError => e
-      puts 'SyntaxError: #{e.message}'
-      if @options[:trace]
-        e.backtrace.each do |line|
-          puts '\tfrom #{line}'
+      threads[i] = Thread.new do
+        mutex.synchronize do
+          condvar.wait mutex
         end
       end
+      mutex.lock
     end
-  end
-end
+    
+    GEMFILE_EXTENSIONS = [
+    '.local',
+    ''
+]
+    
+    #
+# This script extracts the forms from the main page of each
+# web site in a list. The output of this can be used with
+# Metasploit (and other tools) to obtain the saved form data
+# of these domains.
+#
+    
+      config.webfinger_http_fallback = Rails.env == 'development'
+    
+    class Services::Facebook < Service
+  include Rails.application.routes.url_helpers
+    
+          post_params = @service.create_post_params(@status_message)
+      expect(post_params[:message]).to include 'http:'
+    end
