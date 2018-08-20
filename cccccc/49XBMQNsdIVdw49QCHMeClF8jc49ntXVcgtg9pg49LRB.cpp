c0499021227f27becaf7aa9345c8607f14d03ed6
SUNsung <weatherius@gmail.com>
2018-08-20 10:34:29 +0300

    Rewrite [49XBMQNsdIVdw49QCHMeClF8jc49ntXVcgtg9pg49LRB.cpp] File in: 2018-08-20 10:34:28

--- cccccc/49XBMQNsdIVdw49QCHMeClF8jc49ntXVcgtg9pg49LRB.cpp
+++ cccccc/49XBMQNsdIVdw49QCHMeClF8jc49ntXVcgtg9pg49LRB.cpp
@@ -1,317 +1,117 @@
 
         
-        
-    {    // passthrough
-    bool empty() const              { return m.empty(); }
-    size_type size() const          { return m.size(); }
-    size_type max_size() const      { return m.max_size(); }
-    void clear()                    { m.clear(); }
-    iterator begin()                { return m.begin(); }
-    iterator end()                  { return m.end(); }
-    const_iterator begin() const    { return m.begin(); }
-    const_iterator end() const      { return m.end(); }
-    const_iterator cbegin() const   { return m.cbegin(); }
-    const_iterator cend() const     { return m.cend(); }
-};
+          /// DebuggerClient is consulted at two times during name
+  /// lookup.  This is the first time: after all names in a
+  /// source file have been checked but before external
+  /// Modules are checked.  The results in the ResultVector will
+  /// be consulted first.  Return true if results have been added
+  /// to RV.
+  /// FIXME: I don't think this ever does anything useful.
+  virtual bool lookupOverrides(DeclBaseName Name, DeclContext *DC,
+                               SourceLoc Loc, bool IsTypeLookup,
+                               ResultVector &RV) = 0;
     
-      // Get initial measurement of the space we will be reading.
-  const int64_t initial_size = Size(Key(0), Key(n));
-  const int64_t initial_other_size = Size(Key(n), Key(kCount));
+    // NOTE: this class is NOT meant to be used in threaded contexts.
+@interface ObjectBehaviorVerifier : NSObject
+@property (readonly) BOOL wasRetained;
+@property (readonly) BOOL wasCopied;
+@property (readonly) BOOL wasMutableCopied;
     
-        TableBuilder* builder = new TableBuilder(options, file);
-    meta->smallest.DecodeFrom(iter->key());
-    for (; iter->Valid(); iter->Next()) {
-      Slice key = iter->key();
-      meta->largest.DecodeFrom(key);
-      builder->Add(key, iter->value());
+    
+    
+    #ifdef __APPLE__
+    
+    namespace swift {
+class ModuleDecl;
+class SourceFile;
+class DeclContext;
     }
     
-    class DBImpl;
-    
-    static std::string IKey(const std::string& user_key,
-                        uint64_t seq,
-                        ValueType vt) {
-  std::string encoded;
-  AppendInternalKey(&encoded, ParsedInternalKey(user_key, seq, vt));
-  return encoded;
-}
-    
-    namespace {
-class MemTableInserter : public WriteBatch::Handler {
- public:
-  SequenceNumber sequence_;
-  MemTable* mem_;
-    }
+    class FixitApplyDiagnosticConsumer final
+  : public DiagnosticConsumer, public FixitFilter {
+  clang::RewriteBuffer RewriteBuf;
     }
     
-    class Slice {
- public:
-  // Create an empty slice.
-  Slice() : data_(''), size_(0) { }
+    #include 'llvm/ADT/ArrayRef.h'
+    
+      StringRef getRuntimeLibPath() const { return RuntimeLibPath; }
+    
+    void convert_dataset(const char* image_filename, const char* label_filename,
+        const char* db_filename) {
+  // Open files
+  std::ifstream image_file(image_filename, std::ios::in | std::ios::binary);
+  std::ifstream label_file(label_filename, std::ios::in | std::ios::binary);
+  CHECK(image_file) << 'Unable to open file ' << image_filename;
+  CHECK(label_file) << 'Unable to open file ' << label_filename;
+  // Read the magic and the meta data
+  uint32_t magic;
+  uint32_t num_items;
+  uint32_t num_labels;
+  uint32_t rows;
+  uint32_t cols;
     }
     
-      caffe::Datum datum;
-  datum.set_channels(2);  // one channel for each image in the pair
-  datum.set_height(rows);
-  datum.set_width(cols);
-  LOG(INFO) << 'A total of ' << num_items << ' items.';
-  LOG(INFO) << 'Rows: ' << rows << ' Cols: ' << cols;
-  for (int itemid = 0; itemid < num_items; ++itemid) {
-    int i = caffe::caffe_rng_rand() % num_items;  // pick a random  pair
-    int j = caffe::caffe_rng_rand() % num_items;
-    read_image(&image_file, &label_file, i, rows, cols,
-        pixels, &label_i);
-    read_image(&image_file, &label_file, j, rows, cols,
-        pixels + (rows * cols), &label_j);
-    datum.set_data(pixels, 2*rows*cols);
-    if (label_i  == label_j) {
-      datum.set_label(1);
-    } else {
-      datum.set_label(0);
-    }
-    datum.SerializeToString(&value);
-    std::string key_str = caffe::format_int(itemid, 8);
-    db->Put(leveldb::WriteOptions(), key_str, value);
-  }
     
-      static vector<string> LayerTypeList() {
-    CreatorRegistry& registry = Registry();
-    vector<string> layer_types;
-    for (typename CreatorRegistry::iterator iter = registry.begin();
-         iter != registry.end(); ++iter) {
-      layer_types.push_back(iter->first);
-    }
-    return layer_types;
-  }
+    {}  // namespace caffe
     
-    /**
- * @brief Compute the index of the @f$ K @f$ max values for each datum across
- *        all dimensions @f$ (C \times H \times W) @f$.
- *
- * Intended for use after a classification layer to produce a prediction.
- * If parameter out_max_val is set to true, output is a vector of pairs
- * (max_ind, max_val) for each image. The axis parameter specifies an axis
- * along which to maximise.
- *
- * NOTE: does not implement Backwards operation.
- */
-template <typename Dtype>
-class ArgMaxLayer : public Layer<Dtype> {
- public:
-  /**
-   * @param param provides ArgMaxParameter argmax_param,
-   *     with ArgMaxLayer options:
-   *   - top_k (\b optional uint, default 1).
-   *     the number @f$ K @f$ of maximal items to output.
-   *   - out_max_val (\b optional bool, default false).
-   *     if set, output a vector of pairs (max_ind, max_val) unless axis is set then
-   *     output max_val along the specified axis.
-   *   - axis (\b optional int).
-   *     if set, maximise along the specified axis else maximise the flattened
-   *     trailing dimensions for each index of the first / num dimension.
-   */
-  explicit ArgMaxLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    /**
- * @brief Index into the input blob along its first axis.
- *
- * This layer can be used to select, reorder, and even replicate examples in a
- * batch.  The second blob is cast to int and treated as an index into the
- * first axis of the first blob.
- */
-template <typename Dtype>
-class BatchReindexLayer : public Layer<Dtype> {
- public:
-  explicit BatchReindexLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
+      virtual inline const char* type() const { return 'ArgMax'; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
     
      protected:
+  /// @copydoc ContrastiveLossLayer
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
   virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #include 'caffe/layers/lrn_layer.hpp'
+#include 'caffe/layers/power_layer.hpp'
+    
+    #include <vector>
     
     #include 'caffe/blob.hpp'
 #include 'caffe/layer.hpp'
 #include 'caffe/proto/caffe.pb.h'
     
-      virtual inline const char* type() const { return 'ELU'; }
+     protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
     
-    
-    {}  // namespace caffe
-    
-    
-    {    return;
-}
-    
-            // inject additional items into the source code
-        // We support two ways of specifying the network in BrainScript:
-        //  - BrainScriptNetworkBuilder = ( any BS expression that evaluates to a ComputationNetwork )
-        //  - BrainScriptNetworkBuilder = { constructor parameters for a ComputationNetwork }
-        // For back-compat, [ ] is allowed and means the same as { }
-        if (sourceOfNetwork[0] == '{' || sourceOfNetwork[0] == '[') // if { } form then we turn it into ComputationNetwork by constructing a ComputationNetwork from it
-            sourceOfNetwork = L'new ComputationNetwork ' + sourceOfNetwork;
-        let sourceOfBS = msra::strfun::wstrprintf(L'include \'cntk.core.bs\'\n' // include our core lib. Note: Using lowercase here to match the Linux name of the CNTK exe.
-            L'deviceId = %d\n'            // deviceId as passed in
-            L'traceLevel = %d\n'
-            L'precision = '%ls'\n'        // 'float' or 'double'
-            L'network = %ls',             // source code of expression that evaluates to a ComputationNetwork
-            (int)deviceId, traceLevel, ElemTypeName<ElemType>(), sourceOfNetwork.c_str());
-        let expr = BS::ParseConfigDictFromString(sourceOfBS, L'BrainScriptNetworkBuilder', move(includePaths));
-    }
-    
-    // ---------------------------------------------------------------------------
-// latticesource -- manages loading of lattices for MMI (in pairs for numer and denom)
-// ---------------------------------------------------------------------------
-    
-        virtual void GetSections(std::map<std::wstring, SectionType, nocase_compare>& /*sections*/)
-    {
-        assert(false);
-        NOT_IMPLEMENTED;
-    }
-    virtual bool SaveData(size_t /*recordStart*/, const std::map<std::wstring, void*, nocase_compare>& matrices, size_t numRecords, size_t /*datasetSize*/, size_t /*byteVariableSized*/)
-    {
-        // loop through all the output vectors to copy the data over
-        for (auto iter = m_outputs->begin(); iter != m_outputs->end(); ++iter)
-        {
-            // figure out the dimension of the data
-            std::wstring val = iter->first;
-            size_t rows = (*m_dimensions)[val];
-            // size_t count = rows*numRecords;
-    }
-    }
-    
-    void AbstractBtMessage::setBtRequestFactory(BtRequestFactory* factory)
-{
-  requestFactory_ = factory;
-}
-    
-    bool AbstractHttpServerResponseCommand::execute()
-{
-  if (e_->getRequestGroupMan()->downloadFinished() || e_->isHaltRequested()) {
-    return true;
-  }
-  try {
-    ssize_t len = httpServer_->sendResponse();
-    if (len > 0) {
-      timeoutTimer_ = global::wallclock();
-    }
-  }
-  catch (RecoverableException& e) {
-    A2_LOG_INFO_EX(fmt('CUID#%' PRId64
-                       ' - Error occurred while transmitting response body.',
-                       getCuid()),
-                   e);
-    return true;
-  }
-  if (httpServer_->sendBufferIsEmpty()) {
-    A2_LOG_INFO(fmt('CUID#%' PRId64 ' - HttpServer: all response transmitted.',
-                    getCuid()));
-    afterSend(httpServer_, e_);
-    return true;
-  }
-  else {
-    if (timeoutTimer_.difference(global::wallclock()) >= 30_s) {
-      A2_LOG_INFO(fmt('CUID#%' PRId64
-                      ' - HttpServer: Timeout while trasmitting response.',
-                      getCuid()));
-      return true;
-    }
-    else {
-      updateReadWriteCheck();
-      e_->addCommand(std::unique_ptr<Command>(this));
-      return false;
-    }
-  }
-}
-    
-    
-    {  bool currentTierAcceptsCompletedEvent() const;
-};
-    
-    // DiskwriterFactory class template to create DiskWriter derived
-// object, ignoring filename.
-template <class DiskWriterType>
-class AnonDiskWriterFactory : public DiskWriterFactory {
+      std::chrono::seconds interval_;
+  DownloadEngine* e_;
+  Timer checkPoint_;
+  int numNewConnection_; // the number of the connection to establish.
 public:
-  AnonDiskWriterFactory() = default;
-  virtual ~AnonDiskWriterFactory() = default;
+  ActivePeerConnectionCommand(cuid_t cuid, RequestGroup* requestGroup,
+                              DownloadEngine* e, std::chrono::seconds interval);
+    
+    #endif // D_ANNOUNCE_LIST_H
+
+    
+    namespace aria2 {
     }
     
-    
-    {} // namespace aria2
-    
-    std::unique_ptr<AuthConfig> AuthConfig::create(std::string user,
-                                               std::string password)
-{
-  if (user.empty()) {
-    return nullptr;
-  }
-  else {
-    return make_unique<AuthConfig>(std::move(user), std::move(password));
-  }
-}
-    
-    
-    {} // namespace aria2
-    
-    template<typename T, typename Alloc>
-inline void PinnedPrimitiveArray<T, Alloc>::release() {
-  releaseImpl(0);
-  clear();
-}
-    
-    public:
-  // Factory method for creating a hybrid object where the arguments
-  // are used to initialize the C++ part directly without passing them
-  // through java.  This method requires the Java part to have a ctor
-  // which takes a HybridData, and for the C++ part to have a ctor
-  // compatible with the arguments passed here.  For safety, the ctor
-  // can be private, and the hybrid declared a friend of its base, so
-  // the hybrid can only be created from here.
-  //
-  // Exception behavior: This can throw an exception if creating the
-  // C++ object fails, or any JNI methods throw.
-  template <typename... Args>
-  static local_ref<JavaPart> newObjectCxxArgs(Args&&... args) {
-    auto hybridData = makeCxxInstance(std::forward<Args>(args)...);
-    return JavaPart::newInstance(hybridData);
-  }
-    
-    template<typename T>
-inline void swap(
-    weak_ref<T>& a,
-    weak_ref<T>& b) noexcept {
-  internal::dbglog('Ref swap a.ref=%p a=%p b.ref=%p b=%p',
-      a.get(), &a, b.get(), &b);
-  a.storage_.swap(b.storage_);
-}
-    
-    #include './Layout.hh'
-#include './Size.hh'
-#include './Value.hh'
-#include './Config.hh'
-    
-    template<typename... ARGS>
-inline void logw(const char* tag, const char* msg, ARGS... args) noexcept {
-  log(ANDROID_LOG_WARN, tag, msg, args...);
-}
-    
-    class Countable : public noncopyable, public nonmovable {
+    class AnnounceTier {
 public:
-  // RefPtr expects refcount to start at 0
-  Countable() : m_refcount(0) {}
-  virtual ~Countable()
-  {
-    FBASSERT(m_refcount == 0);
-  }
+  enum AnnounceEvent {
+    STARTED,
+    STARTED_AFTER_COMPLETION,
+    DOWNLOADING,
+    STOPPED,
+    COMPLETED,
+    SEEDING,
+    HALTED
+  };
     }
+    
+    namespace aria2 {
+    }
+    
+    #endif // D_AUTH_RESOLVER_H
