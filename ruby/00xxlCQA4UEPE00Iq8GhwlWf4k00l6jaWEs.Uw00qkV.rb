7c9d7ac958540b959b9ab43dcfb77ddb54aa1125
SUNsung <weatherius@gmail.com>
2018-08-21 12:46:35 +0300

    Rewrite [00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb] File in: 2018-08-21 12:46:35

--- ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
+++ ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
@@ -1,201 +1,24 @@
 
         
-            ActiveRecord::Base.clear_all_connections!
-  end
-    
-      def test_assert_called_with_returns
-    assert_called_with(@object, :increment, returns: 1) do
-      @object.increment
+            # Check if proper Lockable module methods are present & unlock strategy
+    # allows to unlock resource on password reset
+    def unlockable?(resource)
+      resource.respond_to?(:unlock_access!) &&
+        resource.respond_to?(:unlock_strategy_enabled?) &&
+        resource.unlock_strategy_enabled?(:email)
     end
-  end
     
-      test 'timer start and stop' do
-    mock = Minitest::Mock.new
-    3.times { mock.expect(:shutdown, nil) }
-    
-        test 'stream_for' do
-      run_in_eventmachine do
-        connection = TestConnection.new
-    
-      def concurrently(enum)
-    enum.map { |*x| Concurrent::Future.execute { yield(*x) } }.map(&:value!)
-  end
-    
-          connection = Connection.new(server, env)
-      assert_called(connection.websocket, :close) do
-        connection.process
-      end
+      def respond_to_on_destroy
+    # We actually need to hardcode this as Rails default responder doesn't
+    # support returning empty response on GET request
+    respond_to do |format|
+      format.all { head :no_content }
+      format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
     end
   end
 end
 
     
-      test 'processing disconnect message' do
-    run_in_eventmachine do
-      open_connection
-    
-        def send_async(method, *args)
-      send method, *args
+        def normalized_path
+      path == '' ? '/' : path
     end
-    
-          # Run Site#process and catch errors
-      #
-      # site - the Jekyll::Site object
-      #
-      # Returns nothing
-      def process_site(site)
-        site.process
-      rescue Jekyll::Errors::FatalException => e
-        Jekyll.logger.error 'ERROR:', 'YOUR SITE COULD NOT BE BUILT:'
-        Jekyll.logger.error '', '------------------------------------'
-        Jekyll.logger.error '', e.message
-        exit(1)
-      end
-    
-                c.action do |_, options|
-              Jekyll::Commands::Doctor.process(options)
-            end
-          end
-        end
-    
-            def after_install(path, options = {})
-          unless options['blank'] || options['skip-bundle']
-            begin
-              require 'bundler'
-              bundle_install path
-            rescue LoadError
-              Jekyll.logger.info 'Could not load Bundler. Bundle install skipped.'
-            end
-          end
-    
-            # rubocop:disable Metrics/AbcSize
-        def process(args, opts)
-          if !args || args.empty?
-            raise Jekyll::Errors::InvalidThemeName, 'You must specify a theme name.'
-          end
-    
-              enable_ssl(opts)
-          enable_logging(opts)
-          opts
-        end
-    
-    require 'em-websocket'
-    
-          def valid_processors
-        %w(kramdown) + third_party_processors
-      end
-    
-          # Generate a Hash for use in generating JSON.
-      # This is useful if fields need to be cleared before the JSON can generate.
-      #
-      # Returns a Hash ready for JSON generation.
-      def hash_for_json(*)
-        to_h
-      end
-    
-            # Unregisters a SIGINT handler.
-        def unregister(sig_callback)
-          @@mutex.synchronize do
-            registered.delete(sig_callback)
-    
-                  # Read!
-              data << io.readpartial(READ_CHUNK_SIZE).encode('UTF-8', Encoding.default_external)
-            else
-              # Do a simple non-blocking read on the IO object
-              data << io.read_nonblock(READ_CHUNK_SIZE)
-            end
-          rescue Exception => e
-            # The catch-all rescue here is to support multiple Ruby versions,
-            # since we use some Ruby 1.9 specific exceptions.
-    
-            # Get the proper capability host to check
-        cap_host = nil
-        if type == :host
-          cap_host = @env.host
-        else
-          with_target_vms([]) do |vm|
-            cap_host = case type
-                       when :provider
-                         vm.provider
-                       when :guest
-                         vm.guest
-                       else
-                         raise Vagrant::Errors::CLIInvalidUsage,
-                           help: opts.help.chomp
-                       end
-          end
-        end
-    
-      def id
-    object.id.to_s
-  end
-    
-    class ManifestSerializer < ActiveModel::Serializer
-  include RoutingHelper
-  include ActionView::Helpers::TextHelper
-    
-        # Environment detection helpers
-    def sprockets?
-      defined?(::Sprockets)
-    end
-    
-      #forward some requests to status message, because a poll is just attached to a status message and is not sharable itself
-  delegate :author_id, :diaspora_handle, :public?, :subscribers, to: :status_message
-    
-          def destroy
-        authorization = Api::OpenidConnect::Authorization.find_by(id: params[:id])
-        if authorization
-          authorization.destroy
-        else
-          flash[:error] = I18n.t('api.openid_connect.authorizations.destroy.fail', id: params[:id])
-        end
-        redirect_to api_openid_connect_user_applications_url
-      end
-    
-    Description:
-  Converts between CSS, indented syntax, and SCSS files. For example,
-  this can convert from the indented syntax to SCSS, or from CSS to
-  SCSS (adding appropriate nesting).
-END
-    
-          # Get the publicly-visible URL for an imported file. This URL is used by
-      # source maps to link to the source stylesheet. This may return `nil` to
-      # indicate that no public URL is available; however, this will cause
-      # sourcemap generation to fail if any CSS is generated from files imported
-      # from this importer.
-      #
-      # If an absolute 'file:' URI can be produced for an imported file, that
-      # should be preferred to returning `nil`. However, a URL relative to
-      # `sourcemap_directory` should be preferred over an absolute 'file:' URI.
-      #
-      # @param uri [String] A URI known to be valid for this importer.
-      # @param sourcemap_directory [String, NilClass] The absolute path to a
-      #   directory on disk where the sourcemap will be saved. If uri refers to
-      #   a file on disk that's accessible relative to sourcemap_directory, this
-      #   may return a relative URL. This may be `nil` if the sourcemap's
-      #   eventual location is unknown.
-      # @return [String?] The publicly-visible URL for this file, or `nil`
-      #   indicating that no publicly-visible URL exists. This should be
-      #   appropriately URL-escaped.
-      def public_url(uri, sourcemap_directory)
-        return if @public_url_warning_issued
-        @public_url_warning_issued = true
-        Sass::Util.sass_warn <<WARNING
-WARNING: #{self.class.name} should define the #public_url method.
-WARNING
-        nil
-      end
-    
-            def log_level(name, options = {})
-          if options[:prepend]
-            level = log_levels.values.min
-            level = level.nil? ? 0 : level - 1
-          else
-            level = log_levels.values.max
-            level = level.nil? ? 0 : level + 1
-          end
-          log_levels.update(name => level)
-          define_logger(name)
-        end
-    
-    Liquid::Template.register_tag('img', Jekyll::ImageTag)
