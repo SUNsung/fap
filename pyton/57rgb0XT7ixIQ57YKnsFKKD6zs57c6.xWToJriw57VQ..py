9ce217ebad9f6eebdb5ef8cf3b6a9c68b07bd92e
SUNsung <weatherius@gmail.com>
2018-08-21 04:04:31 +0300

    Rewrite [57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py] File in: 2018-08-21 04:04:30

--- pyton/57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py
+++ pyton/57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py
@@ -1,145 +1,89 @@
 
         
-            # Sorting the libraries
-    inner_blocks = sorted(blocks[0].split('##'))
-    for i in range(1 , len(inner_blocks)):
-        if inner_blocks[i][0] != '#':
-            inner_blocks[i] = '##' + inner_blocks[i]
-    inner_blocks=''.join(inner_blocks)
+        
+class MockMaster(object):
     
     
-def test_config_missing_json():
-    app = flask.Flask(__name__)
-    with pytest.raises(IOError) as e:
-        app.config.from_json('missing.json')
-    msg = str(e.value)
-    assert msg.startswith('[Errno 2] Unable to load configuration '
-                          'file (No such file or directory):')
-    assert msg.endswith('missing.json'')
-    assert not app.config.from_json('missing.json', silent=True)
+class DragnnModelSaverLibTest(test_util.TensorFlowTestCase):
     
-        return len(cmd) > 1 and 'x' in cmd[1]
-    
-    
-@pytest.mark.functional
-def test_refuse_with_confirmation(proc, TIMEOUT):
-    refuse_with_confirmation(proc, TIMEOUT)
-    
-        history = model.fit(x_train, y_train, batch_size=batch_size,
-                        epochs=epochs // 3, verbose=0)
-    
-        old_layer = keras.layers.LSTM(input_shape=[3, 5], output_dim=2, name='d', consume_less='mem')
-    new_layer = keras.layers.LSTM(2, input_shape=[3, 5], name='d', implementation=1)
-    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer.get_config())
-    
-        a_2 = Dense(16, name='dense_1')(a)
-    dp = Dropout(0.5, name='dropout')
-    b_2 = dp(b)
-    
-        _, fname = tempfile.mkstemp('.h5')
-    model.save(fname)
-    
-        for data_format in ['channels_first', 'channels_last']:
-        if data_format == 'channels_first':
-            shape = (3, 5, 5)
-            target_shape = (5, 5, 3)
-            prev_shape = (2, 3, 2)
-            flip = lambda x: np.flip(np.flip(x, axis=2), axis=3)
-            transpose = lambda x: np.transpose(x, (0, 2, 3, 1))
-            target_data_format = 'channels_last'
-        elif data_format == 'channels_last':
-            shape = (5, 5, 3)
-            target_shape = (3, 5, 5)
-            prev_shape = (2, 2, 3)
-            flip = lambda x: np.flip(np.flip(x, axis=1), axis=2)
-            transpose = lambda x: np.transpose(x, (0, 3, 1, 2))
-            target_data_format = 'channels_first'
-    
-    - The task does not traditionally parse the question separately. This likely
-improves accuracy and is a good example of merging two RNNs.
-    
-    
-for seq_index in range(100):
-    # Take one sequence (part of the training set)
-    # for trying out decoding.
-    input_seq = encoder_input_data[seq_index: seq_index + 1]
-    decoded_sentence = decode_sequence(input_seq)
-    print('-')
-    print('Input sentence:', input_texts[seq_index])
-    print('Decoded sentence:', decoded_sentence)
-
-    
-    input_token_index = dict(
-    [(char, i) for i, char in enumerate(input_characters)])
-target_token_index = dict(
-    [(char, i) for i, char in enumerate(target_characters)])
-    
-        # Note: you have to use shuffle='batch' or False with HDF5Matrix
-    model.fit(X_train, y_train, batch_size=32, shuffle='batch', verbose=False)
-    # test that evalutation and prediction don't crash and return reasonable results
-    out_pred = model.predict(X_test, batch_size=32, verbose=False)
-    out_eval = model.evaluate(X_test, y_test, batch_size=32, verbose=False)
-    
-    The one public-facing part of this module is the `Configurable` class
-and its `~Configurable.configure` method, which becomes a part of the
-interface of its subclasses, including `.AsyncHTTPClient`, `.IOLoop`,
-and `.Resolver`.
-'''
-    
-        @gen_test
-    def test_nonblocking_put_with_getters(self):
-        q = queues.Queue()
-        get0 = q.get()
-        get1 = q.get()
-        q.put_nowait(0)
-        # put_nowait does *not* immediately unblock getters.
-        yield gen.moment
-        self.assertEqual(0, (yield get0))
-        q.put_nowait(1)
-        yield gen.moment
-        self.assertEqual(1, (yield get1))
-    
-        def shutdown(self, wait=True):
-        pass
-    
-    .. testoutput::
-   :hide:
-    
-            ``timeout`` may be a number denoting a time (on the same
-        scale as `tornado.ioloop.IOLoop.time`, normally `time.time`), or a
-        `datetime.timedelta` object for a deadline relative to the
-        current time.
-        '''
-        future = Future()
         try:
-            self.put_nowait(item)
-        except QueueFull:
-            self._putters.append((item, future))
-            _set_timeout(future, timeout)
-        else:
-            future.set_result(None)
-        return future
+      with open(FLAGS.expected_file) as expected:
+        content_expected = expected.read()
+    except IOError as e:
+      self.fail('Error opening '%s': %s' % (FLAGS.expected_file, e.strerror))
     
-        def test_none_callback_pos(self):
-        future = self.sync_future(None)
-        self.assertEqual(future.result(), 42)
+        This constructs a separately managed pipeline for a given target:
+    it has its own ComputeSession, InputSpec placeholder, etc. The ops
+    are given standardized names to allow access from the C++ API. It
+    passes the values in target_config to build_training() above.
     
-        @unittest.skipIf(futures is None, 'futures module not present')
-    @gen_test
-    def test_normal_concurrent_future(self):
-        # A conccurrent future that resolves while waiting for the timeout.
-        with futures.ThreadPoolExecutor(1) as executor:
-            yield gen.with_timeout(datetime.timedelta(seconds=3600),
-                                   executor.submit(lambda: time.sleep(0.01)))
+            with Server.basic_response_server(wait_to_close_event=block_server) as (host, port):
+            sock = socket.socket()
+            sock.connect((host, port))
+            sock.sendall(b'send something')
+            time.sleep(2.5)
+            sock.sendall(b'still alive')
+            block_server.set()  # release server block
     
-               `stop` and `wait` are deprecated; use ``@gen_test`` instead.
-        '''
-        assert _arg is None or not kwargs
-        self.__stop_args = kwargs or _arg
-        if self.__running:
-            self.io_loop.stop()
-            self.__running = False
-        self.__stopped = True
     
-        `TwistedIOLoop` implements the Tornado IOLoop interface on top of
-    the Twisted reactor. Recommended usage::
+def test_idna_without_version_attribute(mocker):
+    '''Older versions of IDNA don't provide a __version__ attribute, verify
+    that if we have such a package, we don't blow up.
+    '''
+    mocker.patch('requests.help.idna', new=None)
+    assert info()['idna'] == {'version': ''}
+    
+    
+class UnrewindableBodyError(RequestException):
+    '''Requests encountered an error when trying to rewind a body'''
+    
+    import idna
+import urllib3
+import chardet
+    
+        def iteritems(self):
+        '''Dict-like iteritems() that returns an iterator of name-value tuples
+        from the jar.
+    
+    # Declare top-level shortcuts
+from scrapy.spiders import Spider
+from scrapy.http import Request, FormRequest
+from scrapy.selector import Selector
+from scrapy.item import Item, Field
+    
+            # Delay queue processing if a download_delay is configured
+        now = time()
+        delay = slot.download_delay()
+        if delay:
+            penalty = delay - now + slot.lastseen
+            if penalty > 0:
+                slot.latercall = reactor.callLater(penalty, self._process_queue, spider, slot)
+                return
+    
+    
+def _get_boto_connection():
+    from boto.s3.connection import S3Connection
+    
+            @defer.inlineCallbacks
+        def process_response(response):
+            assert response is not None, 'Received None in process_response'
+            if isinstance(response, Request):
+                defer.returnValue(response)
+    
+        def gotHeaders(self, headers):
+        self.headers_time = time()
+        self.response_headers = headers
+    
+        def process_response(self, request, response, spider):
+    
+        @classmethod
+    def from_crawler(cls, crawler):
+        o = cls(crawler.settings['USER_AGENT'])
+        crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
+        return o
+    
+    class ScrapyDeprecationWarning(Warning):
+    '''Warning category for deprecated features, since the default
+    DeprecationWarning is silenced on Python 2.7+
+    '''
+    pass
