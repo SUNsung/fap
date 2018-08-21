1572d6371514741ebf18d16a7a9dec86cea3172b
SUNsung <weatherius@gmail.com>
2018-08-21 04:57:05 +0300

    Rewrite [51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb] File in: 2018-08-21 04:57:04

--- ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
+++ ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
@@ -1,33 +1,3 @@
 
         
-          validate :enough_poll_answers
-  validates :question, presence: true
-    
-        on :fetch_public_entity do |entity_type, guid|
-      entity = Diaspora::Federation::Mappings.model_class_for(entity_type).all_public.find_by(guid: guid)
-      case entity
-      when Post
-        Diaspora::Federation::Entities.post(entity)
-      when Poll
-        Diaspora::Federation::Entities.status_message(entity.status_message)
-      end
-    end
-    
-            private
-    
-    ```
-#{plugins_string}
-```
-#{markdown_podfile}
-EOS
-      end
-    
-            def self.options
-          [
-            ['--template-url=URL', 'The URL of the git repo containing a ' \
-                                  'compatible template'],
-          ].concat(super)
-        end
-    
-          def run
-        update_if_necessary!
+        
