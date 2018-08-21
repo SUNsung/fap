5c7346d2136c3e4e8c4464e00b5a650fd713e9fa
SUNsung <weatherius@gmail.com>
2018-08-21 10:52:57 +0300

    Rewrite [55gyRwHSHcwjY55ah0.DrNnJM55Q5K6BYYIYa255Zhv.cpp] File in: 2018-08-21 10:52:56

--- cccccc/55gyRwHSHcwjY55ah0.DrNnJM55Q5K6BYYIYa255Zhv.cpp
+++ cccccc/55gyRwHSHcwjY55ah0.DrNnJM55Q5K6BYYIYa255Zhv.cpp
@@ -1,12 +1,56 @@
 
         
-        protected:
-	static mbedtls_x509_crt cacert;
-	mbedtls_entropy_context entropy;
-	mbedtls_ctr_drbg_context ctr_drbg;
-	mbedtls_ssl_context ssl;
-	mbedtls_ssl_config conf;
+        
+    {}  // namespace mate
     
-    Error WebSocketClient::connect_to_url(String p_url, PoolVector<String> p_protocols, bool gd_mp_api) {
-	_is_multiplayer = gd_mp_api;
+      // Sent by extension test javascript code, typically in a browser test. The
+  // sender is a std::string representing the extension id, and the details
+  // are a std::string with some message. This is particularly useful when you
+  // want to have C++ code wait for javascript code to do something.
+  NOTIFICATION_EXTENSION_TEST_MESSAGE,
+    
+    // Wrapper used to keep track of the lifetime of a WebContents.
+// Lives on the UI thread.
+class PrintingUIWebContentsObserver : public content::WebContentsObserver {
+ public:
+  explicit PrintingUIWebContentsObserver(content::WebContents* web_contents);
     }
+    
+    // A handful of resource-like constants related to the Chrome application.
+    
+      static void BuildPrototype(v8::Isolate* isolate,
+                             v8::Local<v8::FunctionTemplate> prototype);
+    
+    
+    {            ptr[tid] = partial = partial + ptr[tid + 16];
+            ptr[tid] = partial = partial + ptr[tid + 8];
+            ptr[tid] = partial = partial + ptr[tid + 4];
+            ptr[tid] = partial = partial + ptr[tid + 2];
+            ptr[tid] = partial = partial + ptr[tid + 1];
+        }
+    
+    #include '../../precomp.hpp'
+    
+    #include 'matrix.h'
+#include 'include_gunit.h'
+#include 'genericvector.h'
+#include 'tprintf.h'
+    
+    const char *kApostropheLikeUTF8[] = {
+  ''',       // ASCII apostrophe
+  '`',       // ASCII backtick
+  '\u2018',  // opening single quote
+  '\u2019',  // closing single quote
+  '\u2032',  // mathematical prime mark
+  nullptr,      // end of our list.
+};
+    
+     private:
+  // Computes and returns the noise_density IntGrid, at the same gridsize as
+  // this by summing the number of small elements in a 3x3 neighbourhood of
+  // each grid cell. good_grid is filled with blobs that are considered most
+  // likely good text, and this is filled with small and medium blobs that are
+  // more likely non-text.
+  // The photo_map is used to bias the decision towards non-text, rather than
+  // supplying definite decision.
+  IntGrid* ComputeNoiseDensity(bool debug, Pix* photo_map, BlobGrid* good_grid);
