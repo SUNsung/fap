5a5e4187006d96cd434daa4f843f8abab4d582be
SUNsung <weatherius@gmail.com>
2018-08-22 21:16:32 +0300

    Rewrite [556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb] File in: 2018-08-22 21:16:32

--- ruby/556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb
+++ ruby/556rbbtDxfavE55nq2QvECDpj655uI4WYpE6BF255cDk.rb
@@ -1,35 +1,44 @@
 
         
-          def find_matching_tag(tag)
-    if key?(tag)
-      tag
-    else
-      find_altivec_tag(tag) || find_or_later_tag(tag)
-    end
+          def validate_type
+    errors.add(:type, 'cannot be changed once an instance has been created') if type_changed? && !new_record?
+    errors.add(:type, 'is not a valid type') unless self.class.valid_type?(type)
   end
     
-        if RUBY_PATH.to_s !~ %r{^/System/Library/Frameworks/Ruby.framework/Versions/[12]\.[089]/usr/bin/ruby}
-      s << ' => #{RUBY_PATH}'
-    end
-    s
+      def present_hash(hash, skip_key = nil)
+    hash.to_a.sort_by {|a| a.first.to_s }.map { |k, v| '#{k}: #{v}' unless k.to_s == skip_key.to_s }.compact
   end
+end
+
     
-        unless formulae.empty?
-      # Dump formula list.
-      ohai title
-      puts_columns(formulae)
-    end
-  end
+    module SortableTable
+  extend ActiveSupport::Concern
     
-      def self.canonical_name(name)
-    Formulary.canonical_name(name)
-  end
+            self.description = <<-DESC
+          Remove the cache for a given pod, or clear the cache completely.
     
-            def update
-          authorize! :update, stock_location
-          if stock_location.update_attributes(stock_location_params)
-            respond_with(stock_location, status: 200, default_template: :show)
-          else
-            invalid_resource!(stock_location)
+            private
+    
+            # Runs the template configuration utilities.
+        #
+        # @return [void]
+        #
+        def configure_template
+          UI.section('Configuring #{@name} template.') do
+            Dir.chdir(@name) do
+              if File.exist?('configure')
+                system({ 'COCOAPODS_VERSION' => Pod::VERSION }, './configure', @name, *@additional_args)
+              else
+                UI.warn 'Template does not have a configure file.'
+              end
+            end
           end
         end
+    
+          def update_if_necessary!
+        if @update && config.verbose?
+          UI.section('\nUpdating Spec Repositories\n'.yellow) do
+            Repo.new(ARGV.new(['update'])).run
+          end
+        end
+      end
