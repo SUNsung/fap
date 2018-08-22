4eb32d7a7513ac0b64330936d4e4820e514a093b
SUNsung <weatherius@gmail.com>
2018-08-22 16:58:34 +0300

    Rewrite [493ZMPQTWkxcw49LgZdQrPLl1249mufhXn15mBw493Sr.cpp] File in: 2018-08-22 16:58:34

--- cccccc/493ZMPQTWkxcw49LgZdQrPLl1249mufhXn15mBw493Sr.cpp
+++ cccccc/493ZMPQTWkxcw49LgZdQrPLl1249mufhXn15mBw493Sr.cpp
@@ -1,221 +1,180 @@
 
         
-        void PartialRunMgr::PartialRunDone(int step_id, StatusCallback done,
-                                   const Status& status) {
-  Status callback_status;
-  {
-    mutex_lock l(mu_);
-    auto run_it = step_id_to_partial_run_.find(step_id);
-    if (run_it == step_id_to_partial_run_.end()) {
-      return;
+        	_FORCE_INLINE_ real_t get_inv_mass() const { return _inv_mass; }
+	_FORCE_INLINE_ Vector3 get_inv_inertia() const { return _inv_inertia; }
+	_FORCE_INLINE_ Basis get_inv_inertia_tensor() const { return _inv_inertia_tensor; }
+	_FORCE_INLINE_ real_t get_friction() const { return friction; }
+	_FORCE_INLINE_ Vector3 get_gravity() const { return gravity; }
+	_FORCE_INLINE_ real_t get_bounce() const { return bounce; }
+    
+    	virtual void free(RID p_rid);
+    
+    #include 'os/memory.h'
+#include 'safe_refcount.h'
+    
+    	if (p_render_target.is_valid()) {
+		RasterizerStorageGLES3::RenderTarget *rt = storage->render_target_owner.getornull(p_render_target);
+		storage->frame.current_rt = rt;
+		ERR_FAIL_COND(!rt);
+		storage->frame.clear_request = false;
     }
-    run_it->second->final_status.Update(status);
-    if (!run_it->second->executor_done) {
-      // If we found the partial_run, we set the final callback to call only
-      // when the executor is completely done.
-      run_it->second->final_callback = std::move(done);
-      return;
+    
+    				case ShaderLanguage::TYPE_SAMPLERCUBE: {
+					// TODO
+				} break;
+    
+    #define FUNC3S(m_type, m_arg1, m_arg2, m_arg3)                                         \
+	virtual void m_type(m_arg1 p1, m_arg2 p2, m_arg3 p3) {                             \
+		if (Thread::get_caller_id() != server_thread) {                                \
+			command_queue.push_and_sync(server_name, &ServerName::m_type, p1, p2, p3); \
+			SYNC_DEBUG                                                                 \
+		} else {                                                                       \
+			server_name->m_type(p1, p2, p3);                                           \
+		}                                                                              \
+	}
+    
+    	virtual void mesh_surface_set_material(RID p_mesh, int p_surface, RID p_material) = 0;
+	virtual RID mesh_surface_get_material(RID p_mesh, int p_surface) const = 0;
+    
+    	struct VarInfo {
     }
-    callback_status = run_it->second->final_status;
+    
+    	SkeletonIK *s = Object::cast_to<SkeletonIK>(p_object);
+	if (!s)
+		return;
+    
+    Timer::CounterVec Timer::Counters() {
+  CounterVec ret;
+  for (auto& pair : s_names) {
+    ret.emplace_back(pair.str, s_counters[pair.name]);
   }
-  // Otherwise we call the callback immediately.
-  done(callback_status);
-  mutex_lock l(mu_);
-  step_id_to_partial_run_.erase(step_id);
+  return ret;
 }
     
-    #define EIGEN_USE_GPU
+    extern __thread int64_t s_extra_request_nanoseconds;
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+    /*
+ * Helpers for unconditional and conditional jumps.
+ */
+void surpriseCheck(IRGS&);
+void surpriseCheck(IRGS&, Offset);
+void jmpImpl(IRGS&, Offset);
+void implCondJmp(IRGS&, Offset taken, bool negate, SSATmp*);
     
-    // To distinguish different instances of the pattern, (yes, you
-// can instantiate it more then once) the first argument to the
-// INSTANTIATE_TEST_CASE_P macro is a prefix that will be added to the
-// actual test case name. Remember to pick unique prefixes for different
-// instantiations. The tests from the instantiation above will have
-// these names:
-//
-//    * InstantiationName/FooTest.DoesBlah/0 for 'meeny'
-//    * InstantiationName/FooTest.DoesBlah/1 for 'miny'
-//    * InstantiationName/FooTest.DoesBlah/2 for 'moe'
-//    * InstantiationName/FooTest.HasBlahBlah/0 for 'meeny'
-//    * InstantiationName/FooTest.HasBlahBlah/1 for 'miny'
-//    * InstantiationName/FooTest.HasBlahBlah/2 for 'moe'
-//
-// You can use these names in --gtest_filter.
-//
-// This statement will instantiate all tests from FooTest again, each
-// with parameter values 'cat' and 'dog':
     
-    // Then, use the TEST_P macro to define as many parameterized tests
-// for this fixture as you want. The _P suffix is for 'parameterized'
-// or 'pattern', whichever you prefer to think.
+    {
+    {}}
     
-    // A helper class for implementing EXPECT_FATAL_FAILURE() and
-// EXPECT_NONFATAL_FAILURE().  Its destructor verifies that the given
-// TestPartResultArray contains exactly one failure that has the given
-// type and contains the given substring.  If that's not the case, a
-// non-fatal failure will be generated.
-class GTEST_API_ SingleFailureChecker {
- public:
-  // The constructor remembers the arguments.
-  SingleFailureChecker(const TestPartResultArray* results,
-                       TestPartResult::Type type,
-                       const string& substr);
-  ~SingleFailureChecker();
- private:
-  const TestPartResultArray* const results_;
-  const TestPartResult::Type type_;
-  const string substr_;
+        memcpy(p, 'free_hugepages', 15);
+    assert(strlen('free_hugepages') == 14); // extra \0 byte
+    free_huge = readNumFrom(fileName);
+    
+      Variant getKey(ssize_t pos) const {
+    if (isPacked()) {
+      assertx(static_cast<size_t>(pos) < m_size);
+      return pos;
     }
+    assertx(static_cast<size_t>(pos) < m.m_num);
+    return buckets()[pos].key->toLocal();
+  }
     
-    // A copyable object representing the result of a test part (i.e. an
-// assertion or an explicit FAIL(), ADD_FAILURE(), or SUCCESS()).
-//
-// Don't inherit from TestPartResult as its destructor is not virtual.
-class GTEST_API_ TestPartResult {
- public:
-  // The possible outcomes of a test part (i.e. an assertion or an
-  // explicit SUCCEED(), FAIL(), or ADD_FAILURE()).
-  enum Type {
-    kSuccess,          // Succeeded.
-    kNonFatalFailure,  // Failed but the test can continue.
-    kFatalFailure      // Failed and the test should be terminated.
-  };
-    }
+    ArrayData* SetArray::Pop(ArrayData* ad, Variant& value) {
+  auto a = asSet(ad);
+  if (a->cowCheck()) a = a->copySet();
+  if (a->m_size) {
+    ssize_t pos = a->getIterLast();
+    cellCopy(a->getElm(pos), *value.asTypedValue());
+    auto const pelm = &a->data()[pos];
+    auto loc = a->findForRemove(pelm->hash(),
+      [pelm] (const Elm& e) { return &e == pelm; }
+    );
+    assertx(loc != Empty);
+    a->erase(loc);
+  } else {
+    value = uninit_null();
+  }
+  /*
+   * To conform to PHP5 behavior, the pop operation resets the array's
+   * internal iterator.
+   */
+  a->m_pos = a->getIterBegin();
+  return a;
+}
     
-    // A concrete DeathTestFactory implementation for normal use.
-class DefaultDeathTestFactory : public DeathTestFactory {
- public:
-  virtual bool Create(const char* statement, const RE* regex,
-                      const char* file, int line, DeathTest** test);
+    /** Read a attribute='value' thing. Leading whitespace is skipped.
+ * Between attribute and '=' no whitespace is allowed. After the '=' it is
+ * permitted.
+ * @param in the istream to read from.
+ * @param attribute string the attribute name is put in
+ * @param value string the value of the attribute is put in
+ * @throws ios_base::failure if something is fishy. E.g. malformed quoting
+ * or missing '='
+ */
+void read_AttributeNValue( ANTLR_USE_NAMESPACE(std)istream& in,
+								   ANTLR_USE_NAMESPACE(std)string& attribute,
+								   ANTLR_USE_NAMESPACE(std)string& value );
+    
+    #ifdef NEEDS_OPERATOR_LESS_THAN
+// RK: apparently needed by MSVC and a SUN CC, up to and including
+// 2.7.2 this was undefined ?
+inline bool operator<( RefAST l, RefAST r )
+{
+	return nullAST == l ? ( nullAST == r ? false : true ) : l->getType() < r->getType();
+}
+#endif
+    
+    	void setText(const ANTLR_USE_NAMESPACE(std)string& text);
+	void setType(int ttype);
+	ANTLR_USE_NAMESPACE(std)string toString() const;
+	ANTLR_USE_NAMESPACE(std)string toStringList() const;
+	ANTLR_USE_NAMESPACE(std)string toStringTree() const;
+    
+    typedef ASTRefCount<AST> RefAST;
+    
+    /* ANTLR Translator Generator
+ * Project led by Terence Parr at http://www.jGuru.com
+ * Software rights: http://www.antlr.org/license.html
+ *
+ * $Id: //depot/code/org.antlr/release/antlr-2.7.7/lib/cpp/antlr/BitSet.hpp#2 $
+ */
+    
+    #include <istream>
+    
+    /* ANTLR Translator Generator
+ * Project led by Terence Parr at http://www.jGuru.com
+ * Software rights: http://www.antlr.org/license.html
+ *
+ * $Id: //depot/code/org.antlr/release/antlr-2.7.7/lib/cpp/antlr/CharStreamException.hpp#2 $
+ */
+    
+    	virtual void initialize( RefAST t )
+	{
+		setType(t->getType());
+		setText(t->getText());
+	}
+	virtual void initialize( RefToken t )
+	{
+		setType(t->getType());
+		setText(t->getText());
+	}
+    
+    
+    {	void setHiddenAfter(RefToken t);
+	void setHiddenBefore(RefToken t);
 };
     
-      // Clones a 0-terminated C string, allocating memory using new.  The
-  // caller is responsible for deleting the return value using
-  // delete[].  Returns the cloned string, or NULL if the input is
-  // NULL.
-  //
-  // This is different from strdup() in string.h, which allocates
-  // memory using malloc().
-  static const char* CloneCString(const char* c_str);
     
-    	virtual void joint_set_solver_priority(RID p_joint, int p_priority);
-	virtual int joint_get_solver_priority(RID p_joint) const;
+    {private:
+	CommonToken(const CommonToken&);
+	const CommonToken& operator=(const CommonToken&);
+};
     
-    	bool read_only;
-	bool checkable;
-	bool checked;
-	bool draw_red;
-	bool keying;
-    
-    #endif // SHADERCOMPILERGLES3_H
-
-    
-    #endif // BOOST_ASIO_BASIC_WAITABLE_TIMER_HPP
-
-    
-    /// Create a new non-modifiable buffer that represents the given POD array.
-/**
- * @returns A const_buffers_1 value equivalent to:
- * @code const_buffers_1(
- *     data.data(),
- *     data.size() * sizeof(PodType)); @endcode
+    /* ANTLR Translator Generator
+ * Project led by Terence Parr at http://www.jGuru.com
+ * Software rights: http://www.antlr.org/license.html
+ *
+ * $Id:$
  */
-template <typename PodType, std::size_t N>
-inline const_buffers_1 buffer(const boost::array<PodType, N>& data)
-{
-  return const_buffers_1(
-      const_buffer(data.data(), data.size() * sizeof(PodType)));
-}
     
-    #ifndef BOOST_ASIO_BUFFERED_READ_STREAM_FWD_HPP
-#define BOOST_ASIO_BUFFERED_READ_STREAM_FWD_HPP
-    
-    #if !defined(BOOST_ASIO_HAS_THREADS)
-typedef long atomic_count;
-inline void increment(atomic_count& a, long b) { a += b; }
-#elif defined(BOOST_ASIO_HAS_STD_ATOMIC)
-typedef std::atomic<long> atomic_count;
-inline void increment(atomic_count& a, long b) { a += b; }
-#else // defined(BOOST_ASIO_HAS_STD_ATOMIC)
-typedef boost::detail::atomic_count atomic_count;
-inline void increment(atomic_count& a, long b) { while (b > 0) ++a, --b; }
-#endif // defined(BOOST_ASIO_HAS_STD_ATOMIC)
-    
-        // Pop the key/value pair from the stack.
-    ~context()
-    {
-      call_stack<Key, Value>::top_ = next_;
-    }
-    
-    
-    {    // Make the upcall if required.
-    if (owner)
-    {
-      fenced_block b(fenced_block::half);
-      BOOST_ASIO_HANDLER_INVOCATION_BEGIN((handler.arg1_, handler.arg2_));
-      boost_asio_handler_invoke_helpers::invoke(handler, handler.handler_);
-      BOOST_ASIO_HANDLER_INVOCATION_END;
-    }
-  }
-    
-    #ifndef BOOST_ASIO_DETAIL_FD_SET_ADAPTER_HPP
-#define BOOST_ASIO_DETAIL_FD_SET_ADAPTER_HPP
-    
-      // Remove all entries from the map.
-  void clear()
-  {
-    // Clear the values.
-    values_.clear();
-    size_ = 0;
-    }
-    
-    #pragma once
-    
-     private:
-  /// udev handle (socket descriptor contained within).
-  struct udev* handle_{nullptr};
-    
-    class ExampleTable : public TablePlugin {
- private:
-  TableColumns columns() const {
-    return {
-        std::make_tuple('example_text', TEXT_TYPE, ColumnOptions::DEFAULT),
-        std::make_tuple(
-            'example_integer', INTEGER_TYPE, ColumnOptions::DEFAULT),
-    };
-  }
-    }
-    
-    Status readRawMem(size_t base, size_t length, void** buffer) {
-  *buffer = 0;
-    }
-    
-    Status FirehoseLoggerPlugin::logStatus(const std::vector<StatusLogLine>& log) {
-  return forwarder_->logStatus(log);
-}
-    
-      Status logString(const std::string& s) override;
-    
-    class KinesisLoggerPlugin : public LoggerPlugin {
- public:
-  KinesisLoggerPlugin() : LoggerPlugin() {}
-    }
-    
-      virtual bool GetAdjustedExtrinsics(
-    Eigen::Matrix<double, 4, 4>* extrinsics) = 0;
-    
-      const double dx1 = cos_heading_ * half_length_;
-  const double dy1 = sin_heading_ * half_length_;
-  const double dx2 = sin_heading_ * half_width_;
-  const double dy2 = -cos_heading_ * half_width_;
-  const double dx3 = box.cos_heading() * box.half_length();
-  const double dy3 = box.sin_heading() * box.half_length();
-  const double dx4 = box.sin_heading() * box.half_width();
-  const double dy4 = -box.cos_heading() * box.half_width();
+    public:
+	MismatchedCharException();
