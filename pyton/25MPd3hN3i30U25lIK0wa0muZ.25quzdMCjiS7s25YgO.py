fb74a689e8333549e48c93fda9be33719dfdc4b8
SUNsung <weatherius@gmail.com>
2018-08-22 19:07:10 +0300

    Rewrite [25MPd3hN3i30U25lIK0wa0muZ.25quzdMCjiS7s25YgO.py] File in: 2018-08-22 19:07:10

--- pyton/25MPd3hN3i30U25lIK0wa0muZ.25quzdMCjiS7s25YgO.py
+++ pyton/25MPd3hN3i30U25lIK0wa0muZ.25quzdMCjiS7s25YgO.py
@@ -1,164 +1,95 @@
 
         
-        with open('update/versions.json', 'w') as jsonf:
-    json.dump(versions_info, jsonf, indent=4, sort_keys=True)
-
+            # The font size ('10pt', '11pt' or '12pt').
+    #
+    # 'pointsize': '10pt',
     
-        def test_no_duplicates(self):
-        ies = gen_extractors()
-        for tc in gettestcases(include_onlymatching=True):
-            url = tc['url']
-            for ie in ies:
-                if type(ie).__name__ in ('GenericIE', tc['name'] + 'IE'):
-                    self.assertTrue(ie.suitable(url), '%s should match URL %r' % (type(ie).__name__, url))
-                else:
-                    self.assertFalse(
-                        ie.suitable(url),
-                        '%s should not match URL %r . That URL belongs to %s.' % (type(ie).__name__, url, tc['name']))
-    
-    
-def _make_testfunc(testfile):
-    m = re.match(r'^(.*)\.(as)$', testfile)
-    if not m:
-        return
-    test_id = m.group(1)
-    
-                    fn = os.path.join(dirpath, basename)
-                with io.open(fn, encoding='utf-8') as inf:
-                    code = inf.read()
-    
-        _TEST = {
-        'url': 'http://www.anitube.se/video/36621',
-        'md5': '59d0eeae28ea0bc8c05e7af429998d43',
-        'info_dict': {
-            'id': '36621',
-            'ext': 'mp4',
-            'title': 'Recorder to Randoseru 01',
-            'duration': 180.19,
-        },
-        'skip': 'Blocked in the US',
-    }
-    
-        def _real_extract(self, url):
-        video_id = self._match_id(url)
-    
-    
-class ClipRsIE(OnetBaseIE):
-    _VALID_URL = r'https?://(?:www\.)?clip\.rs/(?P<id>[^/]+)/\d+'
-    _TEST = {
-        'url': 'http://www.clip.rs/premijera-frajle-predstavljaju-novi-spot-za-pesmu-moli-me-moli/3732',
-        'md5': 'c412d57815ba07b56f9edc7b5d6a14e5',
-        'info_dict': {
-            'id': '1488842.1399140381',
-            'ext': 'mp4',
-            'title': 'PREMIJERA Frajle predstavljaju novi spot za pesmu Moli me, moli',
-            'description': 'md5:56ce2c3b4ab31c5a2e0b17cb9a453026',
-            'duration': 229,
-            'timestamp': 1459850243,
-            'upload_date': '20160405',
-        }
-    }
-    
-    MIN_TOKEN_TYPE = UP+1
-	
-INVALID_TOKEN_TYPE = 0
-    
-                    # EOF Transition to accept state?
-                if c == EOF and self.eof[s] >= 0:
-                    #print 'EOF Transition to accept state %d' \
-                    #  % self.accept[self.eof[s]]
-                    return self.accept[self.eof[s]]
-    
-        def delete(self, *args):
-        self.replace(*(list(args) + [None]))
-    
-    
-from constants import *
-from dfa import *
-from exceptions import *
-from recognizers import *
-from streams import *
-from tokens import *
-
-    
-                            #print 'no viable alt'
-                        self.noViableAlt(s, input)
-                        return 0
-    
-            from certbot.client import Client
-        with mock.patch('certbot.client.acme_client.BackwardsCompatibleClientV2') as acme:
-            self.acme_client = acme
-            self.acme = acme.return_value = mock.MagicMock()
-            self.client = Client(
-                config=self.config, account_=self.account,
-                auth=None, installer=None)
-    
-    
-class VirtualHostTest(unittest.TestCase):
-    '''Test the VirtualHost class.'''
-    
-        @mock.patch('certbot_compatibility_test.validator.requests.get')
-    def test_hsts_expire(self, mock_get_request):
-        mock_get_request.return_value = create_response(
-            headers={'strict-transport-security': 'max-age=3600'})
-        self.assertFalse(self.validator.hsts('test.com'))
-    
-    # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.0'
-    
-        def popitem(self):
-        '''Delete an arbitrary (key, message) pair and return it.'''
-        for key in self.iterkeys():
-            return (key, self.pop(key))     # This is only run once.
-        else:
-            raise KeyError('No messages in mailbox')
-    
-            NOTE: the 'message id' form isn't supported by XOVER
+            :param str domain_name: The domain name for which to find the corresponding Domain.
+        :returns: The Domain, if found.
+        :rtype: `~digitalocean.Domain`
+        :raises certbot.errors.PluginError: if no matching Domain is found.
         '''
-        cmd = 'OVER' if 'OVER' in self._caps else 'XOVER'
-        if isinstance(message_spec, (tuple, list)):
-            start, end = message_spec
-            cmd += ' {0}-{1}'.format(start, end or '')
-        elif message_spec is not None:
-            cmd = cmd + ' ' + message_spec
-        resp, lines = self._longcmdstring(cmd, file)
-        fmt = self._getoverviewfmt()
-        return resp, _parse_overview(lines, fmt)
     
-        # We only ever write one 'entry point' symbol - either
-    # 'main' or 'WinMain'.  Therefore, there is no need to
-    # pass a subsystem switch to the linker as it works it
-    # out all by itself.  However, the subsystem _does_ determine
-    # the file extension and additional linker flags.
-    target_link_flags = ''
-    target_ext = '.exe'
-    if subsystem_details[vars['subsystem']][2]:
-        target_link_flags = '-dll'
-        target_ext = '.dll'
+        eff.handle_subscription(config)
     
-        def getActual(self, annotation):
-        scope = {}
-        exec(self.template.format(ann=annotation), {}, scope)
-        func_ret_ann = scope['f'].__annotations__['return']
-        func_arg_ann = scope['g'].__annotations__['arg']
-        var_ann1 = scope['__annotations__']['var']
-        var_ann2 = scope['__annotations__']['var2']
-        self.assertEqual(func_ret_ann, func_arg_ann)
-        self.assertEqual(func_ret_ann, var_ann1)
-        self.assertEqual(func_ret_ann, var_ann2)
-        return func_ret_ann
+    # The name of a javascript file (relative to the configuration directory) that
+# implements a search results scorer. If empty, the default will be used.
+#html_search_scorer = 'scorer.js'
     
-    '''
-# Emacs turd '
+        def load_config(self):
+        '''Returns the next config directory to be tested'''
+        shutil.rmtree(self.le_config.work_dir, ignore_errors=True)
+        backup = os.path.join(self.le_config.work_dir, constants.BACKUP_DIR)
+        os.makedirs(backup)
+        return self._configs.pop()
     
-        def test_stdin_stdout(self):
-        args = sys.executable, '-m', 'json.tool'
-        with Popen(args, stdin=PIPE, stdout=PIPE, stderr=PIPE) as proc:
-            out, err = proc.communicate(self.data.encode())
-        self.assertEqual(out.splitlines(), self.expect.encode().splitlines())
-        self.assertEqual(err, b'')
+    import sys
+import os
+import shlex
+    
+        def test_sales_manager_shall_not_talk_through_proxy_with_delay(cls):
+        cls.ntp.busy = 'No'
+        start_time = time()
+        cls.ntp.talk()
+        end_time = time()
+        execution_time = end_time - start_time
+        print_output = cls.output.getvalue()
+        expected_print_output = 'Proxy checking for Sales Manager availability\n\
+This Sales Manager will not talk to you whether he/she is busy or not\n'
+        cls.assertEqual(print_output, expected_print_output)
+        expected_execution_time = 1
+        cls.assertEqual(int(execution_time*10), expected_execution_time)
+    
+        fftv.publish('cartoon')
+    fftv.publish('music')
+    fftv.publish('ads')
+    fftv.publish('movie')
+    fftv.publish('cartoon')
+    fftv.publish('cartoon')
+    fftv.publish('movie')
+    fftv.publish('blank')
+    
+        def setUp(self):
+        self.tc1 = TC1()
+        self.tc2 = TC2()
+        self.tc3 = TC3()
+        self.average_result_tc1 = '###### In Test 1 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc2 = '###### In Test 2 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.average_result_tc3 = '###### In Test 3 ######\n' + \
+                                  'Setting up\n' + \
+                                  'Running test\n' + \
+                                  'Tearing down\n' + \
+                                  'Test Finished'
+        self.runner = TestRunner()
+        self.out = StringIO()
+        self.saved_stdout = sys.stdout
+        sys.stdout = self.out
     
     
-<!-- The above is a description of an error in a Python program, formatted
-     for a Web browser because the 'cgitb' module was enabled.  In case you
-     are not reading this in a Web browser, here is the original traceback:
+    
+        def test_display_current_time_at_midnight(self):
+        '''
+        Would almost always fail (despite of right at/after midnight) if
+        untestable production code would have been used.
+        '''
+        time_provider_stub = MidnightTimeProvider()
+        class_under_test = TimeDisplay()
+        expected_time = '<span class=\'tinyBoldText\'>24:01</span>'
+        self.assertEqual(class_under_test.get_current_time_as_html_fragment(time_provider_stub), expected_time)
+    
+            return transaction
+    
+    
+if __name__ == '__main__':
+    print('Specification')
+    andrey = User()
+    ivan = User(super_user=True)
+    vasiliy = 'not User instance'
