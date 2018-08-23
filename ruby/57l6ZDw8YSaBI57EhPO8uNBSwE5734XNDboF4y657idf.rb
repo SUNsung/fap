1db61893a6d6e74f3a0ba483752b2ad6f7096998
SUNsung <weatherius@gmail.com>
2018-08-23 03:42:15 +0300

    Rewrite [57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb] File in: 2018-08-23 03:42:14

--- ruby/57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb
+++ ruby/57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb
@@ -1,31 +1,130 @@
 
         
-                  # Check if invalid uppercase words are at the start of a
-          # formula's desc
-          if !VALID_LOWERCASE_WORDS.include?(string_content(desc).split.first) &&
-             regex_match_group(desc, /^[a-z]/)
-            problem 'Description should start with a capital letter'
+            def create_event(event)
+      return super unless dry_run?
+      if can_create_events?
+        event = build_event(event)
+        @dry_run_results[:events] << event.payload
+        event
+      else
+        error 'This Agent cannot create events!'
+      end
+    end
+  end
+end
+
+    
+          if options[:type] == :array && (options[:values].blank? || !options[:values].is_a?(Array))
+        raise ArgumentError.new('When using :array as :type you need to provide the :values as an Array')
+      end
+    
+        def boolify(value)
+      agent.send(:boolify, value)
+    end
+    
+      def index
+    if params[:agent_id]
+      @agent = current_user.agents.find(params[:agent_id])
+      @events = @agent.events.page(params[:page])
+    else
+      @events = current_user.events.preload(:agent).page(params[:page])
+    end
+    
+          # The message to be shown if the account is inactive.
+      def inactive_message
+        !confirmed? ? :unconfirmed : super
+      end
+    
+          def remember_cookie_values(resource)
+        options = { httponly: true }
+        options.merge!(forget_cookie_values(resource))
+        options.merge!(
+          value: resource.class.serialize_into_cookie(resource),
+          expires: resource.remember_expires_at
+        )
+      end
+    
+      if record && record.respond_to?(:timedout?) && warden.authenticated?(scope) &&
+     options[:store] != false && !env['devise.skip_timeoutable']
+    last_request_at = warden.session(scope)['last_request_at']
+    
+      def create_gist(files, description)
+    url = 'https://api.github.com/gists'
+    data = { 'public' => true, 'files' => files, 'description' => description }
+    scopes = GitHub::CREATE_GIST_SCOPES
+    GitHub.open_api(url, data: data, scopes: scopes)['html_url']
+  end
+    
+        used_formulae_missing = false
+    used_formulae = begin
+      ARGV.formulae
+    rescue FormulaUnavailableError => e
+      opoo e
+      used_formulae_missing = true
+      # If the formula doesn't exist: fake the needed formula object name.
+      ARGV.named.map { |name| OpenStruct.new name: name, full_name: name }
+    end
+    
+            def define_logger(name, options = {})
+          class_eval <<-RUBY, __FILE__, __LINE__ + 1
+            def #{name}(message)
+              #{options.fetch(:to, :log)}(#{name.inspect}, message)
+            end
+          RUBY
+        end
+      end
+    end
+  end
+end
+
+    
+        def parse_input(environment, text)
+      case text
+      when Script::MATCH
+        name = $1
+        guarded = !!$3
+        val = Script::Parser.parse($2, @line, text.size - ($3 || '').size - $2.size)
+    
+      def generate_error_hash(error)
+    {
+      :path => request.path,
+      :status => error.status_code,
+      :error => error.to_hash
+    }
+  end
+    
+    module LogStash
+  module Api
+    module Commands
+      module System
+        class Plugins < Commands::Base
+          def run
+            { :total => plugins.count, :plugins => plugins }
           end
     
-        export LANG=en_US.UTF-8
-    DOC
-  else
-    STDERR.puts <<-DOC
-    \e[33mWARNING: CocoaPods requires your terminal to be using UTF-8 encoding.
-    Consider adding the following to ~/.profile:
+        r0
+  end
     
-              If you don't give a pod `NAME`, you need to specify the `--all`
-          flag (this is to avoid cleaning all the cache by mistake).
-        DESC
+        # Returns the width and height in a format suitable to be passed to Geometry.parse
+    def to_s
+      s = ''
+      s << width.to_i.to_s if width > 0
+      s << 'x#{height.to_i}' if height > 0
+      s << modifier.to_s
+      s
+    end
     
-            self.description = <<-DESC
-          Shows the content of the pods cache as a YAML tree output, organized by pod.
-          If `NAME` is given, only the caches for that pod will be included in the output.
-        DESC
-    
-    ```
-#{plugins_string}
-```
-#{markdown_podfile}
-EOS
+            def no_error_when_valid?
+          @file = StringIO.new('.')
+          @subject.send(@attachment_name).assign(@file)
+          @subject.valid?
+          expected_message = [
+            @attachment_name.to_s.titleize,
+            I18n.t(:blank, scope: [:errors, :messages])
+          ].join(' ')
+          @subject.errors.full_messages.exclude?(expected_message)
+        end
       end
+    end
+  end
+end
