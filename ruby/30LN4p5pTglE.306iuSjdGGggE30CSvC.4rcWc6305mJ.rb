3a933bca03be833f2be286032c1c51abae53c9b8
SUNsung <weatherius@gmail.com>
2018-08-23 14:41:00 +0300

    Rewrite [30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb] File in: 2018-08-23 14:40:59

--- ruby/30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb
+++ ruby/30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb
@@ -1,238 +1,89 @@
 
         
-              if valid_type?(type)
-        type.constantize.new(attributes).tap do |instance|
-          instance.user = user if instance.respond_to?(:user=)
-        end
-      else
-        const_get(:BASE_CLASS_NAME).constantize.new(attributes).tap do |instance|
-          instance.type = type
-          instance.user = user if instance.respond_to?(:user=)
-        end
-      end
-    end
-  end
-end
+              raise Discourse::InvalidParameters.new(:to_address) unless args[:to_address].present?
     
-        def log(message, options = {})
-      return super unless dry_run?
-      case options[:level] || 3
-      when 0..2
-        sev = Logger::DEBUG
-      when 3
-        sev = Logger::INFO
-      else
-        sev = Logger::ERROR
-      end
-    
-      def working?
-    last_receive_at && last_receive_at > options['expected_receive_period_in_days'].to_i.days.ago && !recent_error_logs?
-  end
-    
-      def evernote_client
-    EvernoteOAuth::Client.new(
-      token:           evernote_oauth_token,
-      consumer_key:    evernote_consumer_key,
-      consumer_secret: evernote_consumer_secret,
-      sandbox:         use_sandbox?
+    Bundler.require(
+    *Rails.groups(
+        coverage: [:test],
+        db: all_environments,
+        pcap: all_environments
     )
-  end
+)
     
-      def complete_option(method)
-    if self.respond_to? 'complete_#{method}'.to_sym
-      self.send('complete_#{method}'.to_sym)
-    end
-  end
-    
-        if params[:sort].present?
-      attribute, direction = params[:sort].downcase.split('.')
-      unless valid_sorts.include?(attribute)
-        attribute, direction = default.to_a.first
-      end
-    else
-      attribute, direction = default.to_a.first
-    end
-    
-      def destroy
-    @services = current_user.services.find(params[:id])
-    @services.destroy
-    
-          class << self
-        # Mark a given block of code as a 'busy' block of code, which will
-        # register a SIGINT handler for the duration of the block. When a
-        # SIGINT occurs, the `sig_callback` proc will be called. It is up
-        # to the callback to behave properly and exit the application.
-        def busy(sig_callback)
-          register(sig_callback)
-          return yield
-        ensure
-          unregister(sig_callback)
-        end
-    
-          def []=(key, value)
-        super(convert_key(key), value)
-      end
-    
-                  # Read!
-              data << io.readpartial(READ_CHUNK_SIZE).encode('UTF-8', Encoding.default_external)
-            else
-              # Do a simple non-blocking read on the IO object
-              data << io.read_nonblock(READ_CHUNK_SIZE)
-            end
-          rescue Exception => e
-            # The catch-all rescue here is to support multiple Ruby versions,
-            # since we use some Ruby 1.9 specific exceptions.
-    
-        #
-    # @param ips [String] IP range(s)
-    # @return [true] if actions on ips are allowed.
-    # @return [false] if actions are not allowed on ips.
-    def allow_actions_on?(ips)
-      return true unless limit_to_network
-      return true unless boundary
-      return true if boundary.empty?
-      boundaries = Shellwords.split(boundary)
-      return true if boundaries.empty? # It's okay if there is no boundary range after all
-      given_range = Rex::Socket::RangeWalker.new(ips)
-      return false unless given_range # Can't do things to nonexistant IPs
-      allowed = false
-      boundaries.each do |boundary_range|
-        ok_range = Rex::Socket::RangeWalker.new(boundary)
-        allowed  = true if ok_range.include_range? given_range
-      end
-      return allowed
-    end
-    
-        if extension_pathname.readable?
-      ENV['BUNDLE_GEMFILE'] = extension_pathname.to_path
-      break
-    end
-  end
+                  s[:proto]='pop3'
+              s[:extra]='Failed Login. Banner: #{s[:banner]}'
+              report_auth_info(s)
+              print_status('Invalid POP3 Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]} (#{s[:banner].strip})')
+              s[:pass]=''
+          end
+        when nil
+          # No matches, no saved state
+        else
+          s[:last]=matched
+          sessions[s[:session]].merge!({k => matches})
+      end # end case matched
+    end # end of each_key
+  end # end of parse
 end
     
-    if (ip == nil || port == nil)
-  puts 'Syntax: test.rb <ip> [port]\n'
-  exit
-end
+    keytoolOpts 	= ['-genkey', '-alias', 'signFiles', '-keystore', 'msfkeystore',
+		   '-storepass', 'msfstorepass', '-dname', 'cn=#{certCN}',
+		   '-keypass', 'msfkeypass']
     
-            def log_level?(level, min_level)
-          log_levels[level] >= log_levels[min_level]
-        end
-    
-        # The type of the query (e.g. `'screen'` or `'print'`).
-    #
-    # When parsed as Sass code, this contains strings and SassScript nodes. When
-    # parsed as CSS, it contains a single string (accessible via
-    # \{#resolved_type}).
-    #
-    # @return [Array<String, Sass::Script::Tree::Node>]
-    attr_accessor :type
-    
-          def all_gem_names
-        core_gem_names + plugin_gem_names
+          # Get the publicly-visible URL for an imported file. This URL is used by
+      # source maps to link to the source stylesheet. This may return `nil` to
+      # indicate that no public URL is available; however, this will cause
+      # sourcemap generation to fail if any CSS is generated from files imported
+      # from this importer.
+      #
+      # If an absolute 'file:' URI can be produced for an imported file, that
+      # should be preferred to returning `nil`. However, a URL relative to
+      # `sourcemap_directory` should be preferred over an absolute 'file:' URI.
+      #
+      # @param uri [String] A URI known to be valid for this importer.
+      # @param sourcemap_directory [String, NilClass] The absolute path to a
+      #   directory on disk where the sourcemap will be saved. If uri refers to
+      #   a file on disk that's accessible relative to sourcemap_directory, this
+      #   may return a relative URL. This may be `nil` if the sourcemap's
+      #   eventual location is unknown.
+      # @return [String?] The publicly-visible URL for this file, or `nil`
+      #   indicating that no publicly-visible URL exists. This should be
+      #   appropriately URL-escaped.
+      def public_url(uri, sourcemap_directory)
+        return if @public_url_warning_issued
+        @public_url_warning_issued = true
+        Sass::Util.sass_warn <<WARNING
+WARNING: #{self.class.name} should define the #public_url method.
+WARNING
+        nil
       end
     
-    # IMPORTANT: The Capistrano::Plugin system is not yet considered a stable,
-# public API, and is subject to change without notice. Eventually it will be
-# officially documented and supported, but for now, use it at your own risk.
-#
-# Base class for Capistrano plugins. Makes building a Capistrano plugin as easy
-# as writing a `Capistrano::Plugin` subclass and overriding any or all of its
-# three template methods:
-#
-# * set_defaults
-# * register_hooks
-# * define_tasks
-#
-# Within the plugin you can use any methods of the Rake or Capistrano DSLs, like
-# `fetch`, `invoke`, etc. In cases when you need to use SSHKit's backend outside
-# of an `on` block, use the `backend` convenience method. E.g. `backend.test`,
-# `backend.execute`, or `backend.capture`.
-#
-# Package up and distribute your plugin class as a gem and you're good to go!
-#
-# To use a plugin, all a user has to do is install it in the Capfile, like this:
-#
-#   # Capfile
-#   require 'capistrano/superfancy'
-#   install_plugin Capistrano::Superfancy
-#
-# Or, to install the plugin without its hooks:
-#
-#   # Capfile
-#   require 'capistrano/superfancy'
-#   install_plugin Capistrano::Superfancy, load_hooks: false
-#
-class Capistrano::Plugin < Rake::TaskLib
-  include Capistrano::DSL
-    
-    desc 'Deploy a new release.'
-task :deploy do
-  set(:deploying, true)
-  %w{ starting started
-      updating updated
-      publishing published
-      finishing finished }.each do |task|
-    invoke 'deploy:#{task}'
-  end
-end
-task default: :deploy
-
-    
-        it 'yields the properties for multiple roles' do
-      recipient = mock('recipient')
-      recipient.expects(:doit).with('example1.com', :redis, port: 6379, type: :slave)
-      recipient.expects(:doit).with('example2.com', :redis, port: 6379, type: :master)
-      recipient.expects(:doit).with('example3.com', :app, nil)
-      dsl.role_properties(:redis, :app) do |host, role, props|
-        recipient.doit(host, role, props)
-      end
-    end
-    
-            def stock_location_params
-          params.require(:stock_location).permit(permitted_stock_location_attributes)
-        end
-      end
-    end
+      # Converts an interpolation array to source.
+  #
+  # @param interp [Array<String, Sass::Script::Tree::Node>] The interpolation array to convert.
+  # @param options [{Symbol => Object}] An options hash (see {Sass::CSS#initialize}).
+  # @return [String]
+  def self._interp_to_src(interp, options)
+    interp.map {|r| r.is_a?(String) ? r : r.to_sass(options)}.join
   end
 end
 
     
-            def index
-          authorize! :read, StockMovement
-          @stock_movements = scope.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
-          respond_with(@stock_movements)
+    desc 'Generates a dummy app for testing for every Spree engine'
+task :test_app do
+  SPREE_GEMS.each do |gem_name|
+    Dir.chdir('#{File.dirname(__FILE__)}/#{gem_name}') do
+      sh 'rake test_app'
+    end
+  end
+end
+    
+            def create
+          authorize! :create, StockMovement
+          @stock_movement = scope.new(stock_movement_params)
+          if @stock_movement.save
+            respond_with(@stock_movement, status: 201, default_template: :show)
+          else
+            invalid_resource!(@stock_movement)
+          end
         end
-    
-    module RuboCop
-  module Cop
-    class VariableForce
-      # This class represents each assignment of a variable.
-      class Assignment
-        include Branchable
-    
-            # Construct annotated source string (like what we parse)
-        #
-        # Reconstruct a deterministic annotated source string. This is
-        # useful for eliminating semantically irrelevant annotation
-        # ordering differences.
-        #
-        # @example standardization
-        #
-        #     source1 = AnnotatedSource.parse(<<-RUBY)
-        #     line1
-        #     ^ Annotation 1
-        #      ^^ Annotation 2
-        #     RUBY
-        #
-        #     source2 = AnnotatedSource.parse(<<-RUBY)
-        #     line1
-        #      ^^ Annotation 2
-        #     ^ Annotation 1
-        #     RUBY
-        #
-        #     source1.to_s == source2.to_s # => true
-        #
-        # @return [String]
-        def to_s
-          reconstructed = lines.dup
