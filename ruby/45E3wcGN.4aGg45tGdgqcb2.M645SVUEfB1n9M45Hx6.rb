af33adec1a8f4a216983b76addbbc1da20cd3c7c
SUNsung <weatherius@gmail.com>
2018-08-23 16:01:58 +0300

    Rewrite [45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb] File in: 2018-08-23 16:01:58

--- ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
+++ ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
@@ -1,69 +1,74 @@
 
         
-              def load_all_tasks
-        self.tasks = []
+              topic = Topic.find_by(id: topic_id)
     
-    module Capistrano
-  module TaskEnhancements
-    def before(task, prerequisite, *args, &block)
-      prerequisite = Rake::Task.define_task(prerequisite, *args, &block) if block_given?
-      Rake::Task[task].enhance [prerequisite]
-    end
+      protected
     
-          describe 'setting an internal roles filter' do
-        subject { dsl.roles(:app) }
-        it 'ignores it' do
-          dsl.set :filter, roles: :web
-          expect(subject.map(&:hostname)).to eq(['example3.com', 'example4.com'])
-        end
-      end
+      def icon
+    object.image
+  end
     
-    desc 'Clean out caches: .pygments-cache, .gist-cache, .sass-cache'
-task :clean do
-  rm_rf [Dir.glob('.pygments-cache/**'), Dir.glob('.gist-cache/**'), Dir.glob('.sass-cache/**'), 'source/stylesheets/screen.css']
-end
+      private
     
-      end
-    
-      class IncludeCodeTag < Liquid::Tag
-    def initialize(tag_name, markup, tokens)
-      @title = nil
-      @file = nil
-      if markup.strip =~ /\s*lang:(\S+)/i
-        @filetype = $1
-        markup = markup.strip.sub(/lang:\S+/i,'')
-      end
-      if markup.strip =~ /(.*)?(\s+|^)(\/*\S+)/i
-        @title = $1 || nil
-        @file = $3
-      end
-      super
-    end
-    
-    
-    {
-    {  # Returns hash with styles for all classes using Paperclip.
-  # Unfortunately current version does not work with lambda styles:(
-  #   {
-  #     :User => {:avatar => [:small, :big]},
-  #     :Book => {
-  #       :cover => [:thumb, :croppable]},
-  #       :sample => [:thumb, :big]},
-  #     }
-  #   }
-  def self.current_attachments_styles
-    Hash.new.tap do |current_styles|
-      Paperclip::AttachmentRegistry.each_definition do |klass, attachment_name, attachment_attributes|
-        # TODO: is it even possible to take into account Procs?
-        next if attachment_attributes[:styles].kind_of?(Proc)
-        attachment_attributes[:styles].try(:keys).try(:each) do |style_name|
-          klass_sym = klass.to_s.to_sym
-          current_styles[klass_sym] ||= Hash.new
-          current_styles[klass_sym][attachment_name.to_sym] ||= Array.new
-          current_styles[klass_sym][attachment_name.to_sym] << style_name.to_sym
-          current_styles[klass_sym][attachment_name.to_sym].map!(&:to_s).sort!.map!(&:to_sym).uniq!
-        end
-      end
+        def show
+      render plain: 'show'
     end
   end
-  private_class_method :current_attachments_styles
+    
+    class NotificationMailerPreview < ActionMailer::Preview
+  # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/mention
+  def mention
+    m = Mention.last
+    NotificationMailer.mention(m.account, Notification.find_by(activity: m))
+  end
+    
+        def common_options(opts)
+      opts.separator ''
+      opts.separator 'Common Options:'
+    
+        def self.run
+      # Apparently there's no better way than this to add Sass
+      # to Merb's Rack stack.
+      Merb::Config[:app] = Sass::Plugin::Rack.new(Merb::Config[:app])
+    end
+  end
+end
+
+    
+    When /^(?:|I )select '([^']*)' from '([^']*)'$/ do |value, field|
+  select(value, :from => field)
+end
+    
+        def possible_types
+      MIME::Types.type_for(@filepath).collect(&:content_type)
+    end
+    
+            def accepted_types_and_failures
+          if @allowed_types.present?
+            'Accept content types: #{@allowed_types.join(', ')}\n'.tap do |message|
+              if @missing_allowed_types.present?
+                message << '  #{@missing_allowed_types.join(', ')} were rejected.'
+              else
+                message << '  All were accepted successfully.'
+              end
+            end
+          end
+        end
+        def rejected_types_and_failures
+          if @rejected_types.present?
+            'Reject content types: #{@rejected_types.join(', ')}\n'.tap do |message|
+              if @missing_rejected_types.present?
+                message << '  #{@missing_rejected_types.join(', ')} were accepted.'
+              else
+                message << '  All were rejected successfully.'
+              end
+            end
+          end
+        end
+    
+            required = directories.map do |directory|
+          pathname = File.expand_path(Rails.root.join(directory, filename))
+          file_exists = File.exist?(pathname)
+          require pathname if file_exists
+          file_exists
+        end
