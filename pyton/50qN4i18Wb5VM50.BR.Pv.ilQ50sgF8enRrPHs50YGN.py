33b37000ea2ff296107685dea39409018a0c9888
SUNsung <weatherius@gmail.com>
2018-08-23 22:18:27 +0300

    Rewrite [50qN4i18Wb5VM50.BR.Pv.ilQ50sgF8enRrPHs50YGN.py] File in: 2018-08-23 22:18:26

--- pyton/50qN4i18Wb5VM50.BR.Pv.ilQ50sgF8enRrPHs50YGN.py
+++ pyton/50qN4i18Wb5VM50.BR.Pv.ilQ50sgF8enRrPHs50YGN.py
@@ -1,68 +1,21 @@
 
         
-        
-@pytest.mark.functional
-def test_refuse_with_confirmation(proc, TIMEOUT):
-    refuse_with_confirmation(proc, TIMEOUT)
+        # Often instead of just checking if two faces match or not (True or False), it's helpful to see how similar they are.
+# You can do that by using the face_distance function.
     
-        Models: `flatpages.flatpages`
-    Templates: Uses the template defined by the ``template_name`` field,
-        or :template:`flatpages/default.html` if template_name is not defined.
-    Context:
-        flatpage
-            `flatpages.flatpages` object
-    '''
-    if not url.startswith('/'):
-        url = '/' + url
-    site_id = get_current_site(request).id
-    try:
-        f = get_object_or_404(FlatPage, url=url, sites=site_id)
-    except Http404:
-        if not url.endswith('/') and settings.APPEND_SLASH:
-            url += '/'
-            f = get_object_or_404(FlatPage, url=url, sites=site_id)
-            return HttpResponsePermanentRedirect('%s/' % request.path)
-        else:
-            raise
-    return render_flatpage(request, f)
+    # Load a sample picture and learn how to recognize it.
+print('Loading known face image(s)')
+obama_image = face_recognition.load_image_file('obama_small.jpg')
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
     
-        @defer.inlineCallbacks
-    def _close(self, *_a, **_kw):
-        for dh in self._handlers.values():
-            if hasattr(dh, 'close'):
-                yield dh.close()
-
+    # This is a demo of running face recognition on a video file and saving the results to a new video file.
+#
+# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
     
-        def _is_tar(self, response):
-        archive = BytesIO(response.body)
-        try:
-            tar_file = tarfile.open(name=mktemp(), fileobj=archive)
-        except tarfile.ReadError:
-            return
+    # This is a demo of blurring faces in video.
     
-    
-class UserAgentMiddleware(object):
-    '''This middleware allows spiders to override the user_agent'''
-    
-            self.assertEqual(
-            set(face_landmarks[0].keys()),
-            set(['nose_tip', 'left_eye', 'right_eye']))
-        self.assertEqual(face_landmarks[0]['nose_tip'], [(496, 295)])
-    
-    while True:
-    print('Capturing image.')
-    # Grab a single frame of video from the RPi camera as a numpy array
-    camera.capture(output, format='rgb')
-    
-                for face_location in face_locations:
-                # Print the location of each face in this frame
-                top, right, bottom, left = face_location
-                print(' - A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
-    
-    # If extensions (or modules to document with autodoc) are in another
-# directory, add these directories to sys.path here. If the directory is
-# relative to the documentation root, use os.path.abspath to make it
-# absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
-    
-        pool.starmap(test_image, function_parameters)
+    # PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
