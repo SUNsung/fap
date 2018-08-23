93ecfe3b6a05c1e9bc2e35f54e3cbdca0f1e111a
SUNsung <weatherius@gmail.com>
2018-08-23 18:38:01 +0300

    Rewrite [58KGovPY3zzV.58zkNU58UaOUs58wCzcfeKRKEU58url.rb] File in: 2018-08-23 18:38:00

--- ruby/58KGovPY3zzV.58zkNU58UaOUs58wCzcfeKRKEU58url.rb
+++ ruby/58KGovPY3zzV.58zkNU58UaOUs58wCzcfeKRKEU58url.rb
@@ -1,69 +1,32 @@
 
         
-        def load_apps
-  out, err, status = Open3.capture3('/usr/bin/osascript', '-e', 'tell application 'System Events' to get (name, bundle identifier, unix id) of every process')
-  if status.exitstatus > 0
-    puts err
-    exit status.exitstatus
-  end
-  out = out.split(', ')
-  one_third   = out.length / 3
-  @app_names  = out.shift(one_third)
-  @bundle_ids = out.shift(one_third)
-  @unix_ids   = out.shift(one_third)
-end
-    
-        process_font_assets
-    process_stylesheet_assets
-    process_javascript_assets
-    store_version
-  end
-    
-      # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
-    
-      def setup
-    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
-    success = Dir.chdir DUMMY_PATH do
-      silence_stdout_if !ENV['VERBOSE'] do
-        system 'node', 'manifest.js', tmp_dir
-      end
+            # Exclude cache, logs, and repository, if they are located under the prefix.
+    [HOMEBREW_CACHE, HOMEBREW_LOGS, HOMEBREW_REPOSITORY].each do |dir|
+      dirs.delete dir.relative_path_from(HOMEBREW_PREFIX).to_s
     end
-    assert success, 'Node.js Mincer compilation failed'
-    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
-    css_name = manifest['assets']['application.css']
-    @css = File.read('#{tmp_dir}/#{css_name}')
-  end
-end
-
+    dirs.delete 'etc'
+    dirs.delete 'var'
     
-          spec['main'] =
-          find_files.(File.join(Bootstrap.stylesheets_path, '_bootstrap.scss')) +
-          find_files.(Bootstrap.fonts_path) +
-          %w(assets/javascripts/bootstrap.js)
-    
-            rows.each do |row|
-          line = row.values.each_with_index.map do |value, col|
-            value.to_s.ljust(col_widths[col])
-          end.join(' ').rstrip
-          line = color.colorize(line, row.color) if row.color
-          puts line
+          if valid_type?(type)
+        type.constantize.new(attributes).tap do |instance|
+          instance.user = user if instance.respond_to?(:user=)
+        end
+      else
+        const_get(:BASE_CLASS_NAME).constantize.new(attributes).tap do |instance|
+          instance.type = type
+          instance.user = user if instance.respond_to?(:user=)
         end
       end
-    
-        # @abstract
-    #
-    # Your implementation should check the existence of a cache repository on
-    # the deployment target
-    #
-    # @return [Boolean]
-    #
-    def test
-      raise NotImplementedError, 'Your SCM strategy module should provide a #test method'
     end
+  end
+end
     
-      entries = [{ template: deploy_rb, file: config_dir.join('deploy.rb') }]
-  entries += envs.split(',').map { |stage| { template: stage_rb, file: deploy_dir.join('#{stage}.rb') } }
+        respond_to do |format|
+      format.html { redirect_back events_path, notice: 'Event deleted.' }
+      format.json { head :no_content }
+    end
+  end
     
-      it 'provides a --format option which enables the choice of output formatting'
+      config = Merb::Plugins.config[:sass] || Merb::Plugins.config['sass'] || {}
+    
+            return if (value.nil? && options[:allow_nil]) || (value.blank? && options[:allow_blank])
