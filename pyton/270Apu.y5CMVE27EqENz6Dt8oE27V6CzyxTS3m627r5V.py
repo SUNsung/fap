7eac1fa7c59ad0f869f15f736f5a2cc374334eba
SUNsung <weatherius@gmail.com>
2018-08-24 02:58:10 +0300

    Rewrite [270Apu.y5CMVE27EqENz6Dt8oE27V6CzyxTS3m627r5V.py] File in: 2018-08-24 02:58:10

--- pyton/270Apu.y5CMVE27EqENz6Dt8oE27V6CzyxTS3m627r5V.py
+++ pyton/270Apu.y5CMVE27EqENz6Dt8oE27V6CzyxTS3m627r5V.py
@@ -1,33 +1,94 @@
 
         
         
-class Cache(object):
+class ExitStatus:
+    '''Exit status code constants.'''
+    OK = 0
+    ERROR = 1
+    PLUGIN_ERROR = 7
     
-        def can_fit_in_spot(self, spot):
-        return True
+        def iter_body(self, chunk_size):
+        '''Return an iterator over the body.'''
+        raise NotImplementedError()
     
-        def extract_year_month(self, timestamp):
-        '''Return the year and month portions of the timestamp.'''
-        ...
     
-        def remove_link_to_crawl(self, url):
-        '''Remove the given link from `links_to_crawl`.'''
-        pass
+with codecs.open(JSON_FILE_PATH, encoding='utf8') as f:
+    JSON_FILE_CONTENT = f.read()
     
-        def __repr__(self):
-        cls_name = self.__class__.__name__
-        return '%s(concurrency=%r, delay=%0.2f, randomize_delay=%r)' % (
-            cls_name, self.concurrency, self.delay, self.randomize_delay)
     
-            self.factory.noPage(\
-                defer.TimeoutError('Getting %s took longer than %s seconds.' % \
-                (self.factory.url, self.factory.timeout)))
+def test_default_options_overwrite(httpbin):
+    env = MockEnvironment()
+    env.config['default_options'] = ['--form']
+    env.config.save()
+    r = http('--json', httpbin.url + '/post', 'foo=bar', env=env)
+    assert r.json['json'] == {'foo': 'bar'}
     
-            # XXX: Google parses at least first 100k bytes; scrapy's redirect
-        # middleware parses first 4k. 4k turns out to be insufficient
-        # for this middleware, and parsing 100k could be slow.
-        # We use something in between (32K) by default.
-        self.lookup_bytes = settings.getint('AJAXCRAWL_MAXSIZE', 32768)
+    from httpie.compat import urlopen
+from httpie.downloads import (
+    parse_content_range, filename_from_content_disposition, filename_from_url,
+    get_unique_filename, ContentRangeError, Downloader,
+)
+from utils import http, MockEnvironment
     
-            respcls = responsetypes.from_args(body=body)
-        return response.replace(body=body, cls=respcls)
+        exc = ConnectionError('Connection aborted')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
+    assert ret == ExitStatus.ERROR
+    assert error_msg == (
+        'ConnectionError: '
+        'Connection aborted while doing GET request to URL: '
+        'http://www.google.com')
+    
+        # we start from a gray image with some random noise
+    if K.image_data_format() == 'channels_first':
+        input_img_data = np.random.random((1, 3, img_width, img_height))
+    else:
+        input_img_data = np.random.random((1, img_width, img_height, 3))
+    input_img_data = (input_img_data - 0.5) * 20 + 128
+    
+    from __future__ import print_function
+    
+        for i in range(1, 6):
+        fpath = os.path.join(path, 'data_batch_' + str(i))
+        (x_train[(i - 1) * 10000: i * 10000, :, :, :],
+         y_train[(i - 1) * 10000: i * 10000]) = load_batch(fpath)
+    
+    import six
+from . import backend as K
+from .losses import mean_squared_error
+from .losses import mean_absolute_error
+from .losses import mean_absolute_percentage_error
+from .losses import mean_squared_logarithmic_error
+from .losses import hinge
+from .losses import logcosh
+from .losses import squared_hinge
+from .losses import categorical_crossentropy
+from .losses import sparse_categorical_crossentropy
+from .losses import binary_crossentropy
+from .losses import kullback_leibler_divergence
+from .losses import poisson
+from .losses import cosine_proximity
+from .utils.generic_utils import deserialize_keras_object
+from .utils.generic_utils import serialize_keras_object
+    
+    
+def test_decode_predictions():
+    x = np.zeros((2, 1000))
+    x[0, 372] = 1.0
+    x[1, 549] = 1.0
+    outs = utils.decode_predictions(x, top=1)
+    scores = [out[0][2] for out in outs]
+    assert scores[0] == scores[1]
+    
+            # put all lines in the file into a Python list
+        strings = f.readlines()
+        
+        # above line leaves trailing newline characters; strip them out
+        strings = [x.strip(u'\n') for x in strings]
+        
+        # remove empty-lines and comments
+        strings = [x for x in strings if x and not x.startswith(u'#')]
+        
+        # insert empty string since all are being removed
+        strings.insert(0, u'')
