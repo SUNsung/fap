341b410061a14899ce63d528c16495051a836999
SUNsung <weatherius@gmail.com>
2018-08-23 11:43:57 +0300

    Rewrite [45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py] File in: 2018-08-23 11:43:56

--- pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
+++ pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
@@ -1,122 +1,116 @@
 
         
-            data_train = fetch_20newsgroups_vectorized(subset='train')
-    data_test = fetch_20newsgroups_vectorized(subset='test')
-    X_train = check_array(data_train.data, dtype=np.float32,
-                          accept_sparse='csc')
-    X_test = check_array(data_test.data, dtype=np.float32, accept_sparse='csr')
-    y_train = data_train.target
-    y_test = data_test.target
+          def testCanCreate(self):
+    '''Tests that create() works on a good spec.'''
+    with tf.Graph().as_default(), self.test_session():
+      master = MockMaster()
+      component = MockComponent(master, _make_biaffine_spec())
     
-        alpha = 0.01  # regularization parameter
+          # The cost corresponding to the three entries is, respectively,
+      # 0.7012, 0.7644, and 1.7269. Each of them is computed using the formula
+      # -prob_i * log(sigmoid(logit_i)) - (1-prob_i) * log(1-sigmoid(logit_i))
+      self.assertAlmostEqual(cost, 3.1924, 4)
+      self.assertEqual(correct, 1)
+      self.assertEqual(total, 3)
+      self.assertAllEqual(gold_labels, [0, 1, 2])
     
-            plt.gcf().suptitle('%s data set' % dataset.capitalize(), fontsize=16)
+      NB: The format of the shortened resource paths should be considered an
+  implementation detail and may change.
     
-    ward = AgglomerativeClustering(n_clusters=3, linkage='ward')
+        # The directory may already exist when running locally multiple times.
+    if not os.path.exists(lexicon_output_path):
+      os.mkdir(lexicon_output_path)
     
-            for step in xrange(opts.n_steps):
-            for it in xrange(opts.n_times):
-                time[name][step, it] = bench_sample(sampling_algorithm[name],
-                                                      opts.n_population,
-                                                      n_samples[step])
-    
-    
-if not os.path.exists(DATA_FOLDER):
-    
-    import os
-import tarfile
-from contextlib import closing
-    
-        # Print and plot the confusion matrix
-    cm = metrics.confusion_matrix(y_test, y_predicted)
-    print(cm)
-    
-    import numpy as np
-import matplotlib.pyplot as plt
-    
-    from sklearn.cluster import AgglomerativeClustering
-from sklearn.metrics import pairwise_distances
-    
-            ca_certs = os.path.join(current_path, 'cacert.pem')
-        self.openssl_context = SSLContext(
-            logger, ca_certs=ca_certs, support_http2=False,
-            cipher_suites=['ALL', '!RC4-SHA', '!ECDHE-RSA-RC4-SHA', '!ECDHE-RSA-AES128-GCM-SHA256',
-                           '!AES128-GCM-SHA256', '!ECDHE-RSA-AES128-SHA', '!AES128-SHA']
-        )
-    
-    import os
-import sys
-from .common import *
-    
-    	# Track the line at which the error occurred in case this is
-	# generated from a lexer.  We need to track this since the
-        # unexpected char doesn't carry the line info.
-        self.line = None
-    
-            raise NotImplementedError
-        
-    
-    def LT(self, i):
-        '''
-        Get the ith character of lookahead.  This is the same usually as
-        LA(i).  This will be used for labels in the generated
-        lexer code.  I'd prefer to return a char here type-wise, but it's
-        probably better to be 32-bit clean and be consistent with LA.
-        '''
-    
-        while pin_count > 0:
-        json_data = extract_json_data(url, max=pin_list[-1]['pin_id'],
-                                      limit=LIMIT)
-        pins = json_data['pins']
-        pin_list += pins
-        pin_count -= len(pins)
-    
-    def mixcloud_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html(url, faker=True)
-    title = r1(r'<meta property='og:title' content='([^']*)'', html)
-    preview_url = r1(r'm-preview=\'([^\']+)\'', html)
-    preview = r1(r'previews(.*)\.mp3$', preview_url)
-    
-    from ..common import *
-from ..extractor import VideoExtractor
-    
-        filenames = []
-    for (dirpath, dnames, fnames) in os.walk(path):
-        for fname in fnames:
-            if fname.endswith('.md'):
-                filenames.append(os.sep.join([dirpath, fname]))
-    
-        url = options.url + '/updateReports?agent=%s' % options.name
-    update_ws = yield websocket_connect(url, None)
-    msg = yield update_ws.read_message()
-    assert msg is None
-    IOLoop.instance().stop()
+    model = Sequential()
+model.add(Embedding(max_features, embedding_size, input_length=maxlen))
+model.add(Dropout(0.25))
+model.add(Conv1D(filters,
+                 kernel_size,
+                 padding='valid',
+                 activation='relu',
+                 strides=1))
+model.add(MaxPooling1D(pool_size=pool_size))
+model.add(LSTM(lstm_output_size))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
     
     
-class Application(tornado.web.Application):
-    def __init__(self):
-        handlers = [
-            (r'/', MainHandler),
-            (r'/auth/login', AuthLoginHandler),
-            (r'/auth/logout', AuthLogoutHandler),
-        ]
-        settings = dict(
-            cookie_secret='__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__',
-            login_url='/auth/login',
-            template_path=os.path.join(os.path.dirname(__file__), 'templates'),
-            static_path=os.path.join(os.path.dirname(__file__), 'static'),
-            xsrf_cookies=True,
-            facebook_api_key=options.facebook_api_key,
-            facebook_secret=options.facebook_secret,
-            ui_modules={'Post': PostModule},
-            debug=True,
-            autoescape=None,
-        )
-        tornado.web.Application.__init__(self, handlers, **settings)
+def get_example_array():
+    np.random.seed(3537)
+    example_array = np.random.random((100, 100)) * 100. - 50.
+    example_array[0, 0] = 0.  # 0 could possibly cause trouble
+    return example_array
     
-    try:
-    import pycares
-except ImportError:
-    pycares = None
+        # Test with Sequential API
+    model = Sequential([
+        layers.Dense(16, input_shape=(x_train.shape[-1],), activation='relu'),
+        layers.Dense(8),
+        layers.Activation('relu'),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+    model.summary()
+    history = model.fit(x_train, y_train, epochs=15, batch_size=16,
+                        validation_data=(x_test, y_test),
+                        verbose=0)
+    assert(history.history['val_acc'][-1] > 0.8)
+    config = model.get_config()
+    model = Sequential.from_config(config)
     
-    version = '6.0.dev1'
+    model = Sequential()
+model.add(Embedding(max_features, 128, input_length=maxlen))
+model.add(Bidirectional(LSTM(64)))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='sigmoid'))
+    
+        with custom_object_scope({'MSE_MAE_loss': MSE_MAE_loss}):
+        deserialized = losses.deserialize(serialized)
+    assert isinstance(deserialized, MSE_MAE_loss)
+    assert deserialized.mse_fraction == 0.3
+    
+        with gzip.open(paths[0], 'rb') as lbpath:
+        y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+    
+    model_input = layers.Input(tensor=x_train_batch)
+model_output = cnn_layers(model_input)
+train_model = keras.models.Model(inputs=model_input, outputs=model_output)
+    
+        def _finish(self, request):
+        self.concurrent -= 1
+        if not request.finished and not request._disconnected:
+            request.finish()
+    
+    # Apply monkey patches to fix issues in external libraries
+from . import _monkeypatches
+del _monkeypatches
+    
+    
+class DownloadHandlers(object):
+    
+        def __init__(self, headers):
+        self._headers = headers
+    
+    class IgnoreRequest(Exception):
+    '''Indicates a decision was made not to process a request'''
+    
+    
+class TestData(unittest.TestCase):
+    
+        def unsubscribe(self, msg):
+        self.provider.unsubscribe(msg, self)
+    
+        def test_tc3_output(self):
+        self.tc3.run()
+        output = self.out.getvalue().strip()
+        self.assertEqual(output, self.average_result_tc3)
+    
+    
+# Alternative implementation for different levels of methods
+class CatalogInstance(object):
+    
+    
+class UserSpecification(CompositeSpecification):
+    
+        '''Base state. This is to share functionality'''
