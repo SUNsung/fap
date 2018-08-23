e2e44b55766cf402dbf2aa43428064004560b26d
SUNsung <weatherius@gmail.com>
2018-08-23 17:17:27 +0300

    Rewrite [13qKUbUkofDvo13znpvhSCB3Zw13OGx7OEns7s13PJS.rb] File in: 2018-08-23 17:17:26

--- ruby/13qKUbUkofDvo13znpvhSCB3Zw13OGx7OEns7s13PJS.rb
+++ ruby/13qKUbUkofDvo13znpvhSCB3Zw13OGx7OEns7s13PJS.rb
@@ -1,8 +1,125 @@
 
         
-              def fetch_public_key_from_json(string, jwt)
-        json = JSON.parse(string)
-        keys = json['keys']
-        public_key = get_key_from_kid(keys, jwt.header['kid'])
-        public_key
+              def make_test_case_available_to_view!
+        test_case_instance = self
+        _helpers.module_eval do
+          unless private_method_defined?(:_test_case)
+            define_method(:_test_case) { test_case_instance }
+            private :_test_case
+          end
+        end
       end
+    
+              # Bubbled up from the adapter require. Prefix the exception message
+          # with some guidance about how to address it and reraise.
+          else
+            raise e.class, 'Error loading the '#{adapter}' Action Cable pubsub adapter. Missing a gem it depends on? #{e.message}', e.backtrace
+          end
+        end
+    
+              def add_channel(channel, on_success)
+            @subscription_lock.synchronize do
+              ensure_listener_running
+              @subscribe_callbacks[channel] << on_success
+              when_connected { send_command('subscribe', channel) }
+            end
+          end
+    
+        def build(attribute, value)
+      if table.type(attribute.name).force_equality?(value)
+        bind = build_bind_attribute(attribute.name, value)
+        attribute.eq(bind)
+      else
+        handler_for(value).call(attribute, value)
+      end
+    end
+    
+          def serialize(value)
+        return if value.nil?
+        unless default_value?(value)
+          super coder.dump(value)
+        end
+      end
+    
+      has_many :clients, -> { order 'id' }, dependent: :destroy, before_remove: :log_before_remove, after_remove: :log_after_remove
+  has_many :unsorted_clients, class_name: 'Client'
+  has_many :unsorted_clients_with_symbol, class_name: :Client
+  has_many :clients_sorted_desc, -> { order 'id DESC' }, class_name: 'Client'
+  has_many :clients_of_firm, -> { order 'id' }, foreign_key: 'client_of', class_name: 'Client', inverse_of: :firm
+  has_many :clients_ordered_by_name, -> { order 'name' }, class_name: 'Client'
+  has_many :unvalidated_clients_of_firm, foreign_key: 'client_of', class_name: 'Client', validate: false
+  has_many :dependent_clients_of_firm, -> { order 'id' }, foreign_key: 'client_of', class_name: 'Client', dependent: :destroy
+  has_many :exclusively_dependent_clients_of_firm, -> { order 'id' }, foreign_key: 'client_of', class_name: 'Client', dependent: :delete_all
+  has_many :limited_clients, -> { limit 1 }, class_name: 'Client'
+  has_many :clients_with_interpolated_conditions, ->(firm) { where 'rating > #{firm.rating}' }, class_name: 'Client'
+  has_many :clients_like_ms, -> { where('name = 'Microsoft'').order('id') }, class_name: 'Client'
+  has_many :clients_like_ms_with_hash_conditions, -> { where(name: 'Microsoft').order('id') }, class_name: 'Client'
+  has_many :plain_clients, class_name: 'Client'
+  has_many :clients_using_primary_key, class_name: 'Client',
+           primary_key: 'name', foreign_key: 'firm_name'
+  has_many :clients_using_primary_key_with_delete_all, class_name: 'Client',
+           primary_key: 'name', foreign_key: 'firm_name', dependent: :delete_all
+  has_many :clients_grouped_by_firm_id, -> { group('firm_id').select('firm_id') }, class_name: 'Client'
+  has_many :clients_grouped_by_name, -> { group('name').select('name') }, class_name: 'Client'
+    
+        def unsubscribed
+      @room = nil
+    end
+    
+    class RedisAdapterTest::Hiredis < RedisAdapterTest
+  def cable_config
+    super.merge(driver: 'hiredis')
+  end
+end
+    
+          def with_broadcast_connection(&block) # :nodoc:
+        ActiveRecord::Base.connection_pool.with_connection do |ar_conn|
+          pg_conn = ar_conn.raw_connection
+          verify!(pg_conn)
+          yield pg_conn
+        end
+      end
+    
+      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/follow_request
+  def follow_request
+    f = Follow.last
+    NotificationMailer.follow_request(f.target_account, Notification.find_by(activity: f))
+  end
+    
+      def scope
+    root_url
+  end
+    
+      sidekiq_options queue: 'mailers'
+    
+                  if(inp.attributes[ikey] =~ /^http/i)
+                inp[ikey] = ''
+                next
+              end
+    
+          when :banner
+        # Because some ftp server send multiple banner we take only the first one and ignore the rest
+        if not (s[:info])
+          s[:info] = matches
+          report_service(s)
+        end
+    
+    
+# replace the stuff
+replaces.uniq!
+replaces.each { |arr|
+	#puts '%32s: %s -> %s' % arr
+	asm.gsub!(arr[1], arr[2])
+}
+    
+    outputJar = 'output.jar'
+    
+        @execve      = ''
+    @getpeername = ''
+    @accept      = ''
+    @listen      = ''
+    @bind        = ''
+    @socket      = ''
+    @connect     = ''
+    @close       = ''
+    @kfcntl      = ''
