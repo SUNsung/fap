ca01cac35d2462d4ef3d190d2b97d47d7d4d44e7
SUNsung <weatherius@gmail.com>
2018-08-23 04:31:55 +0300

    Rewrite [50loMLlmci1SM50UNYnY0H9AOI50h.kTTRNPIjY50Gtz.py] File in: 2018-08-23 04:31:55

--- pyton/50loMLlmci1SM50UNYnY0H9AOI50h.kTTRNPIjY50Gtz.py
+++ pyton/50loMLlmci1SM50UNYnY0H9AOI50h.kTTRNPIjY50Gtz.py
@@ -1,71 +1,100 @@
 
         
-        
-def _tar_file(cmd):
-    for c in cmd:
-        for ext in tar_extensions:
-            if c.endswith(ext):
-                return (c, c[0:len(c) - len(ext)])
+                print('benchmarking scikit-learn: ')
+        scikit_results.append(bench(ScikitLasso, X, Y, X_test, Y_test, coef_))
+        print('benchmarking glmnet: ')
+        glmnet_results.append(bench(GlmnetLasso, X, Y, X_test, Y_test, coef_))
     
-            print('Iteration %s of %s' % (i, n_iter))
+        plt.figure('scikit-learn LASSO benchmark results')
+    plt.subplot(211)
+    plt.plot(list_n_samples, lasso_results, 'b-',
+                            label='Lasso')
+    plt.plot(list_n_samples, lars_lasso_results, 'r-',
+                            label='LassoLars')
+    plt.title('precomputed Gram matrix, %d features, alpha=%s' % (n_features,
+                            alpha))
+    plt.legend(loc='upper left')
+    plt.xlabel('number of samples')
+    plt.ylabel('Time (s)')
+    plt.axis('tight')
     
+    pages = {
+    u'ar': u'http://ar.wikipedia.org/wiki/%D9%88%D9%8A%D9%83%D9%8A%D8%A8%D9%8A%D8%AF%D9%8A%D8%A7',
+    u'de': u'http://de.wikipedia.org/wiki/Wikipedia',
+    u'en': u'https://en.wikipedia.org/wiki/Wikipedia',
+    u'es': u'http://es.wikipedia.org/wiki/Wikipedia',
+    u'fr': u'http://fr.wikipedia.org/wiki/Wikip%C3%A9dia',
+    u'it': u'http://it.wikipedia.org/wiki/Wikipedia',
+    u'ja': u'http://ja.wikipedia.org/wiki/Wikipedia',
+    u'nl': u'http://nl.wikipedia.org/wiki/Wikipedia',
+    u'pl': u'http://pl.wikipedia.org/wiki/Wikipedia',
+    u'pt': u'http://pt.wikipedia.org/wiki/Wikip%C3%A9dia',
+    u'ru': u'http://ru.wikipedia.org/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D0%BF%D0%B5%D0%B4%D0%B8%D1%8F',
+#    u'zh': u'http://zh.wikipedia.org/wiki/Wikipedia',
+}
     
-def _tabulate(results, metrics, formats):
-    '''Prints results by metric and format
+        # Print the classification report
+    print(metrics.classification_report(y_test, y_predicted,
+                                        target_names=dataset.target_names))
     
-        op.add_option('--density',
-                  dest='density', default=1 / 3,
-                  help='Density used by the sparse random projection.'
-                       ' ('auto' or float (0.0, 1.0]')
+        def clear_data(self):
+        self.model.data = []
+        self.fitted = False
+        self.model.changed('clear')
     
-    for lang, page in pages.items():
-    
-    import os
-import tarfile
-from contextlib import closing
-    
-        # split the dataset in training and test set:
-    docs_train, docs_test, y_train, y_test = train_test_split(
-        dataset.data, dataset.target, test_size=0.25, random_state=None)
-    
-    We selected two sets of two variables from the Boston housing data set
-as an illustration of what kind of analysis can be done with several
-outlier detection tools. For the purpose of visualization, we are working
-with two-dimensional examples, but one should be aware that things are
-not so trivial in high-dimension, as it will be pointed out.
-    
-    import sys
-import numpy as np
-    
-    plt.matshow(fit_data, cmap=plt.cm.Blues)
-plt.title('After biclustering; rearranged to show biclusters')
-    
-            lw = LedoitWolf(store_precision=False, assume_centered=True)
-        lw.fit(X)
-        lw_mse[i, j] = lw.error_norm(real_cov, scaling=False)
-        lw_shrinkage[i, j] = lw.shrinkage_
-    
-    import matplotlib.pyplot as plt
-    
-    
-# -- Options for manual page output ---------------------------------------
-    
-                record_id = result['domain_record']['id']
-    
-        def test_str(self):
-        self.assertEqual('Invalid nonce ('xxx'): error', str(self.error))
-    
-        def cleanup_from_tests(self):
-        '''Performs any necessary cleanup from running plugin tests'''
-    
-    # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.viewcode',
-    'repoze.sphinx.autointerface',
+    score_funcs = [
+    metrics.adjusted_rand_score,
+    metrics.v_measure_score,
+    metrics.adjusted_mutual_info_score,
+    metrics.mutual_info_score,
 ]
+    
+    # plot shrinkage coefficient
+plt.subplot(2, 1, 2)
+plt.errorbar(n_samples_range, lw_shrinkage.mean(1), yerr=lw_shrinkage.std(1),
+             label='Ledoit-Wolf', color='navy', lw=2)
+plt.errorbar(n_samples_range, oa_shrinkage.mean(1), yerr=oa_shrinkage.std(1),
+             label='OAS', color='darkorange', lw=2)
+plt.xlabel('n_samples')
+plt.ylabel('Shrinkage')
+plt.legend(loc='lower right')
+plt.ylim(plt.ylim()[0], 1. + (plt.ylim()[1] - plt.ylim()[0]) / 10.)
+plt.xlim(5, 31)
+    
+            self.concurrent -= 1
+        return ''
+    
+    
+logger = logging.getLogger(__name__)
+    
+        def _add_middleware(self, mw):
+        if hasattr(mw, 'process_request'):
+            self.methods['process_request'].append(mw.process_request)
+        if hasattr(mw, 'process_response'):
+            self.methods['process_response'].insert(0, mw.process_response)
+        if hasattr(mw, 'process_exception'):
+            self.methods['process_exception'].insert(0, mw.process_exception)
+    
+            if 'ajax_crawlable' in request.meta:  # prevent loops
+            return response
+    
+    logger = logging.getLogger(__name__)
+    
+    from scrapy import signals
+    
+            self.config = mock.MagicMock(nsone_credentials=path,
+                                     nsone_propagation_seconds=0)  # don't wait during tests
+    
+    
+class TestDfs(object):
+    
+        def test_add_reverse(self):
+        print('Test: Empty list(s)')
+        assert_equal(MyLinkedList().add_reverse(None, None), None)
+        assert_equal(MyLinkedList().add_reverse(Node(5), None), None)
+        assert_equal(MyLinkedList().add_reverse(None, Node(10)), None)
+    
+            print('Test: One element list')
+        linked_list.insert_to_front(2)
+        linked_list.remove_dupes()
+        assert_equal(linked_list.get_all_data(), [2])
