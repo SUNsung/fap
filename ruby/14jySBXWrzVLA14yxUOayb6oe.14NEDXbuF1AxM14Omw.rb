8878d48b43db20b267634dd99285d58e119bbe32
SUNsung <weatherius@gmail.com>
2018-08-24 06:11:16 +0300

    Rewrite [14jySBXWrzVLA14yxUOayb6oe.14NEDXbuF1AxM14Omw.rb] File in: 2018-08-24 06:11:15

--- ruby/14jySBXWrzVLA14yxUOayb6oe.14NEDXbuF1AxM14Omw.rb
+++ ruby/14jySBXWrzVLA14yxUOayb6oe.14NEDXbuF1AxM14Omw.rb
@@ -1,57 +1,100 @@
 
         
-            # The entry filter for this collection.
-    # Creates an instance of Jekyll::EntryFilter.
-    #
-    # Returns the instance of Jekyll::EntryFilter for this collection.
-    def entry_filter
-      @entry_filter ||= Jekyll::EntryFilter.new(site, relative_directory)
+          def ==(other)
+    if other.respond_to?(:to_ary)
+      return true if to_ary == other.to_ary
     end
     
-          # Keep a list of subclasses of Jekyll::Command every time it's inherited
-      # Called automatically.
-      #
-      # base - the subclass
-      #
-      # Returns nothing
-      def inherited(base)
-        subclasses << base
-        super(base)
+      # @private
+  def used_options
+    @options & @args
+  end
+    
+          def self.english_description
+        '#{english_name} #{link_type_english_name}s'
       end
     
-            def deprecated_relative_permalinks(site)
-          if site.config['relative_permalinks']
-            Jekyll::Deprecator.deprecation_message 'Your site still uses relative permalinks,' \
-                                                   ' which was removed in Jekyll v3.0.0.'
-            true
-          end
-        end
-    
-        if registration
-      u2f.authenticate!(challenges, response, Base64.decode64(registration.public_key), registration.counter)
-      registration.update(counter: response.counter)
-      true
+        change.down do
+      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
+      Mention.where(mentions_container_type: 'Comment').destroy_all
+      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
     end
-  rescue JSON::ParserError, NoMethodError, ArgumentError, U2F::Error
-    false
   end
 end
 
     
-          protected
+    When /^I (?:sign|log) in manually as '([^']*)' with password '([^']*)'( on the mobile website)?$/ \
+do |username, password, mobile|
+  @me = User.find_by_username(username)
+  @me.password ||= password
+  manual_login
+  confirm_login mobile
+end
     
-              # Repackage the box
-          output_name = @env.vagrantfile.config.package.name || 'package.box'
-          output_path = Pathname.new(File.expand_path(output_name, FileUtils.pwd))
-          box.repackage(output_path)
+    # Ensure we know the appservers port
+Capybara.server_port = AppConfig.pod_uri.port
+Rails.application.routes.default_url_options[:host] = AppConfig.pod_uri.host
+Rails.application.routes.default_url_options[:port] = AppConfig.pod_uri.port
     
-            def signal_end_of_output
-          output_pipe.puts(END_OF_OUTPUT_SIGNAL)
-          STDOUT.flush
-        end
+      class SendPrivate < Base
+    def perform(*_args)
+      # don't federate in cucumber
+    end
+  end
     
-            def initialize(argv)
-          @name = argv.shift_argument
-          @only_errors = argv.flag?('only-errors')
-          super
-        end
+    describe ContactsController, :type => :controller do
+  describe '#index' do
+    before do
+      AppConfig.chat.enabled = true
+      @aspect = bob.aspects.create(:name => 'another aspect')
+      bob.share_with alice.person, @aspect
+      bob.share_with eve.person, @aspect
+      sign_in bob, scope: :user
+    end
+    
+    if Encoding.default_external != Encoding::UTF_8
+    
+        # Returns a new {Installer} parametrized from the {Config}.
+    #
+    # @return [Installer]
+    #
+    def installer_for_config
+      Installer.new(config.sandbox, config.podfile, config.lockfile)
+    end
+    
+            self.description = <<-DESC
+          Remove the cache for a given pod, or clear the cache completely.
+    
+    if RUBY_ENGINE == 'jruby'
+  gem 'nokogiri', '!= 1.5.0'
+  gem 'trinidad'
+end
+    
+          def mask_authenticity_token(session)
+        token = set_token(session)
+        mask_token(token)
+      end
+    
+    module Rack
+  module Protection
+    ##
+    # Prevented attack::   XSS
+    # Supported browsers:: all
+    # More infos::         http://en.wikipedia.org/wiki/Cross-site_scripting
+    #
+    # Automatically escapes Rack::Request#params so they can be embedded in HTML
+    # or JavaScript without any further issues. Calls +html_safe+ on the escaped
+    # strings if defined, to avoid double-escaping in Rails.
+    #
+    # Options:
+    # escape:: What escaping modes to use, should be Symbol or Array of Symbols.
+    #          Available: :html (default), :javascript, :url
+    class EscapedParams < Base
+      extend Rack::Utils
+    
+            if has_vector?(request, headers)
+          warn env, 'attack prevented by #{self.class}'
+    
+      # Defines the geometry of an image.
+  class Geometry
+    attr_accessor :height, :width, :modifier
