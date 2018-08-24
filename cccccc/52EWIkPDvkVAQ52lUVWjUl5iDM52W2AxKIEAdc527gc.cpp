80120959bda03451102cf64ab012f4872cc3c752
SUNsung <weatherius@gmail.com>
2018-08-24 17:48:51 +0300

    Rewrite [52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp] File in: 2018-08-24 17:48:50

--- cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
+++ cccccc/52EWIkPDvkVAQ52lUVWjUl5iDM52W2AxKIEAdc527gc.cpp
@@ -1,151 +1,339 @@
 
         
-        #include 'core/os/memory.h'
-#include 'core/print_string.h'
-    
-    void SoftBody::update_physics_server() {
-    }
-    
-    	GDCLASS(Skeleton, Spatial);
-    
-    static int make_prefixes(struct ifaddrs* ifaddr, int family, int prefixlen) {
-	char* prefix = NULL;
-	if (family == AF_INET) {
-		sockaddr_in* mask = new sockaddr_in;
-		mask->sin_family = AF_INET;
-		memset(&mask->sin_addr, 0, sizeof(in_addr));
-		ifaddr->ifa_netmask = reinterpret_cast<sockaddr*>(mask);
-		if (prefixlen > 32) {
-			prefixlen = 32;
-		}
-		prefix = reinterpret_cast<char*>(&mask->sin_addr);
-	} else if (family == AF_INET6) {
-		sockaddr_in6* mask = new sockaddr_in6;
-		mask->sin6_family = AF_INET6;
-		memset(&mask->sin6_addr, 0, sizeof(in6_addr));
-		ifaddr->ifa_netmask = reinterpret_cast<sockaddr*>(mask);
-		if (prefixlen > 128) {
-			prefixlen = 128;
-		}
-		prefix = reinterpret_cast<char*>(&mask->sin6_addr);
-	} else {
-		return -1;
-	}
-	for (int i = 0; i < (prefixlen / 8); i++) {
-		*prefix++ = 0xFF;
-	}
-	char remainder = 0xff;
-	remainder <<= (8 - prefixlen % 8);
-	*prefix = remainder;
-	return 0;
-}
-    
-            for (size_t i = 0; i < numSequences; ++i)
-        {
-            MaskKind firstMaskEntry = maskBuffer[i * maxNumTimeSteps];
-            if (firstMaskEntry == MaskKind::SequenceBegin)
-                sequenceBeginIndices[i] = 0;
-            else if (firstMaskEntry == MaskKind::Valid)
-                sequenceBeginIndices[i] = Microsoft::MSR::CNTK::SentinelValueIndicatingUnspecifedSequenceBeginIdx;
-            else
-                LogicError('The first entry of a Value mask must be Valid or SequenceBegin');
-    }
-    
-                m_gradHeader->numSamples = actualMBSize ? 1 : actualMBSize;
-            distGradAgg.AggregateGradients(learnParamsValues, m_gradHeader.get(), 0);
-    
-        static void SetTimestampingFlag()
+        
     {
-        auto& us = GetStaticInstance();
-        us.m_timestampFlag = true;
+    {        // Register the memory buffer.
+        SML->registerMemoryBuffer(info.name, std::move(bitstream));
+        foundModules.push_back(info.name);
+      }
+    } else {
+      llvm::dbgs() << 'Unable to load module';
+      if (!info.name.empty())
+        llvm::dbgs() << ' '' << info.name << '\'';
+      llvm::dbgs() << '.\n';
+    }
+    
+    void CacheImpl::removeAll() {
+  cache_remove_all(static_cast<cache_t*>(Impl));
+}
+    
+    
+    {  Task(const char *ExecPath, ArrayRef<const char *> Args,
+       ArrayRef<const char *> Env = llvm::None, void *Context = nullptr)
+      : ExecPath(ExecPath), Args(Args), Env(Env), Context(Context) {}
+};
+    
+    void
+swift::trimLeadingWhitespaceFromLines(StringRef RawText,
+                                      unsigned WhitespaceToTrim,
+                                      SmallVectorImpl<StringRef> &OutLines) {
+  SmallVector<StringRef, 8> Lines;
     }
     
     
-    {  return scope.Close(Boolean::New(true));
+    {
+    {void TaskProcessInformation::provideMapping(json::Output &out) {
+  out.mapRequired('real_pid', OSPid);
+  if (ProcessUsage.hasValue())
+    out.mapRequired('usage', ProcessUsage.getValue());
+}
+}
 }
     
-        // Helper methods
-    static bool HasFamilyNamed(std::string& name, DBWrapper* db);
-    static bool AddToBatch(rocksdb::WriteBatch& batch, bool del,
-        Handle<Array> array);
-    static bool AddToBatch(rocksdb::WriteBatch& batch, bool del,
-        Handle<Array> array, DBWrapper* db_wrapper, std::string cf);
-    static Handle<Value> CompactRangeDefault(const v8::Arguments& args);
-    static Handle<Value> CompactColumnFamily(const Arguments& args);
-    static Handle<Value> CompactOptions(const Arguments& args);
-    static Handle<Value> CompactAll(const Arguments& args);
+      ConvertUTF8toUTF32(&SourceNext, SourceStart + S.size(), &TargetStart, C + 1,
+                     llvm::lenientConversion);
+  if (TargetStart == C) {
+    // The source string contains an ill-formed subsequence at the end.
+    return S;
+  }
     
-    class StringAppendTESTOperator : public MergeOperator {
+    
+    {  return GraphemeClusterBreakProperty::Other;
+}
+    
+    	void clear() {
+    }
+    
+    public:
+	Error compile(VS::ShaderMode p_mode, const String &p_code, IdentifierActions *p_actions, const String &p_path, GeneratedCode &r_gen_code);
+    
+    	static TCP_Server *_create();
+    
+    struct DirAccessWindowsPrivate;
+    
+    
+    {	return OK;
+}
+    
+    #include 'error_macros.h'
+#include 'os/memory.h'
+#include <stdio.h>
+    
+    std::unique_ptr<php::Unit> parse_unit(php::Program& prog,
+                                      std::unique_ptr<UnitEmitter> ue);
+    
+    //////////////////////////////////////////////////////////////////////
+    
+        if (!semi) {
+      // only media type (type/subtype,data)
+      ssize_t media_len = comma - data;
+      meta_len -= media_len;
+      data += media_len;
+    } else if (slash && slash < semi) {
+      // media type + param (type/subtype;param,data)
+      ssize_t media_len = semi - data;
+      meta_len -= media_len;
+      data += media_len;
+    } else {
+      // no media type (;base64,data)
+      if (semi != data // ex. foo;base64,data
+          || meta_len != sizeof(';base64') - 1 // ex. ;something,data
+          || memcmp(data, ';base64',
+                    sizeof(';base64') - 1)) { // ex. ;base65,data
+          raise_warning('rfc2397: invalid meta data');
+          return nullptr;
+        }
+    }
+    
+    inline const req::ptr<StreamContext>& ExecutionContext::getStreamContext() {
+  return m_streamContext;
+}
+    
+      String ret(maxlen, ReserveString);
+  char* path = ret.mutableData();
+    
+    namespace CNTK
+{
+    class CompositeMinibatchSource final : public MinibatchSource
+    {
+        static const std::wstring PositionAttributeName;
+        static const std::wstring DistributedAfterSampleCountAttributeName;
+    }
+    }
+    
+    
+    {
+    {                // Reset the window.
+                m_loss.first = m_loss.second;
+                m_metric.first = m_metric.second;
+                m_samples.first = m_samples.second;
+                m_updates.first = m_updates.second;
+            }
+        }
+    
+    namespace CNTK
+{
+    class UDFUtils
+    {
+    public:
+    }
+    }
+    
+    
+    {
+    {    private:
+        // Disallow copy and move construction and assignment
+        VariableFields(const VariableFields&) = delete; VariableFields& operator=(const VariableFields& other) = delete; VariableFields(VariableFields&&) = delete; VariableFields& operator=(VariableFields&&) = delete;
+    };
+}
+
+    
+    public:
+    ScopeTimer(size_t verbosity, const std::string& message)
+        : m_verbosity(verbosity), m_message(message)
+    {
+        if (m_verbosity > 2)
+        {
+            m_aggregateTimer.Start();
+        }
+    }
+    
+                // replace input if needed
+            let iter = replacements.find(input);
+            if (iter != replacements.end())
+            {
+                assert(input->GetEnvironmentPtr()); // must be in some network if mapped
+                input = iter->second;
+                numRelinked++;
+                node->SetInput(i, input);
+            }
+    
+    #include <iostream>
+    
+      auto obj = data_.getObject();
+  data_.copyFrom(cv->second.doc(), obj);
+  data_.add('views', obj);
+    
+    std::shared_ptr<PlatformProcess> PlatformProcess::launchExtension(
+    const std::string& exec_path,
+    const std::string& extensions_socket,
+    const std::string& extensions_timeout,
+    const std::string& extensions_interval,
+    bool verbose) {
+  auto ext_pid = ::fork();
+  if (ext_pid < 0) {
+    return std::shared_ptr<PlatformProcess>();
+  } else if (ext_pid == 0) {
+    setEnvVar('OSQUERY_EXTENSION', std::to_string(::getpid()).c_str());
+    }
+    }
+    
+    TEST_F(QueryTests, test_query_name_not_found_in_db) {
+  // Try to retrieve results from a query that has not executed.
+  QueryDataSet previous_qd;
+  auto query = getOsqueryScheduledQuery();
+  auto cf = Query('not_a_real_query', query);
+  auto status = cf.getPreviousQueryResults(previous_qd);
+  EXPECT_FALSE(status.ok());
+  EXPECT_TRUE(previous_qd.empty());
+}
+    
+    
+    {/// Get a performance limit by name and optional level.
+size_t getWorkerLimit(WatchdogLimitType limit);
+}
+
+    
+      // This class is different but also uses different types!
+  auto fake_pub = std::make_shared<FakeEventPublisher>();
+  status = EventFactory::registerEventPublisher(fake_pub);
+  EXPECT_TRUE(status.ok());
+    
+        // Use the basic 'force' flag to check implicit SQL usage.
+    auto flags =
+        SQL('select default_value from osquery_flags where name = 'force'');
+    if (flags.rows().size() > 0) {
+      r['flag_test'] = flags.rows().back().at('default_value');
+    }
+    
+      std::vector<std::vector<float> > rgb1(3, std::vector<float>(kDCTBlockSize));
+  img.ToLinearRGB(xmin, ymin, 8, 8, &rgb1);
+  rgb1 = ::butteraugli::PackedFromPlanes(::butteraugli::OpsinDynamicsImage(
+      ::butteraugli::PlanesFromPacked(8, 8, rgb1)));
+    
+    namespace guetzli {
+    }
+    
+    #endif  // GUETZLI_FAST_LOG_H_
+
+    
+    void RowDct(coeff_t* in, const coeff_t* table) {
+  // The Fourier transform is an unitary operator, so we're basically
+  // doing the transpose of RowIdct()
+  const int a0 = in[0] + in[7];
+  const int b0 = in[0] - in[7];
+  const int a1 = in[1] + in[6];
+  const int b1 = in[1] - in[6];
+  const int a2 = in[2] + in[5];
+  const int b2 = in[2] - in[5];
+  const int a3 = in[3] + in[4];
+  const int b3 = in[3] - in[4];
+    }
+    
+    const double* NewSrgb8ToLinearTable() {
+  double* table = new double[256];
+  int i = 0;
+  for (; i < 11; ++i) {
+    table[i] = i / 12.92;
+  }
+  for (; i < 256; ++i) {
+    table[i] = 255.0 * std::pow(((i / 255.0) + 0.055) / 1.055, 2.4);
+  }
+  return table;
+}
+    
+    
+    {}  // namespace guetzli
+    
+      void JumpToByteBoundary() {
+    while (put_bits <= 56) {
+      int c = (put_buffer >> 56) & 0xff;
+      EmitByte(c);
+      put_buffer <<= 8;
+      put_bits += 8;
+    }
+    if (put_bits < 64) {
+      int padmask = 0xff >> (64 - put_bits);
+      int c = ((put_buffer >> 56) & ~padmask) | padmask;
+      EmitByte(c);
+    }
+    put_buffer = 0;
+    put_bits = 64;
+  }
+    
+    
+    {  // Bit length histogram.
+  std::vector<int> counts;
+  // Symbol values sorted by increasing bit lengths.
+  std::vector<int> values;
+  // The index of the Huffman code in the current set of Huffman codes. For AC
+  // component Huffman codes, 0x10 is added to the index.
+  int slot_id;
+  // Set to true if this Huffman code is the last one within its marker segment.
+  bool is_last;
+};
+    
+    /**
+ * @class CanClientFactory
+ * @brief CanClientFactory inherites apollo::common::util::Facotory.
+ */
+class CanClientFactory
+    : public apollo::common::util::Factory<CANCardParameter::CANCardBrand,
+                                           CanClient> {
  public:
-  // Constructor with delimiter
-  explicit StringAppendTESTOperator(char delim_char);
+  /**
+   * @brief Register the CAN clients of all brands. This function call the
+   *        Function apollo::common::util::Factory::Register() for all of the
+   *        CAN clients.
+   */
+  void RegisterCanClients();
     }
     
-      // Extend record types with the following special values
-  enum {
-    kEof = kMaxRecordType + 1,
-    // Returned whenever we find an invalid physical record.
-    // Currently there are three situations in which this happens:
-    // * The record has an invalid CRC (ReadPhysicalRecord reports a drop)
-    // * The record is a 0-length record (No drop is reported)
-    // * The record is below constructor's initial_offset (No drop is reported)
-    kBadRecord = kMaxRecordType + 2,
-    // Returned when we fail to read a valid header.
-    kBadHeader = kMaxRecordType + 3,
-    // Returned when we read an old record from a previous user of the log.
-    kOldRecord = kMaxRecordType + 4,
-    // Returned when we get a bad record length
-    kBadRecordLen = kMaxRecordType + 5,
-    // Returned when we get a bad record checksum
-    kBadRecordChecksum = kMaxRecordType + 6,
-  };
-    
-    void SyncPoint::ClearTrace() {
-  impl_->ClearTrace();
-}
-    
-      const char* startUtf = start->c_str();
-  jstring jsStart = env->NewStringUTF(startUtf);
-  if(jsStart == nullptr) {
-    // unable to construct string
-    if(env->ExceptionCheck()) {
-      env->ExceptionDescribe(); // print out exception to stderr
+      void RecvThreadFunc() {
+    using common::time::Clock;
+    using common::time::AsInt64;
+    using common::time::micros;
+    using common::ErrorCode;
+    AINFO << 'Receive thread starting...';
+    TestCanParam *param = param_ptr();
+    CanClient *client = param->can_client;
+    int64_t start = 0;
+    std::vector<CanFrame> buf;
     }
-    releaseJniEnv(attached_thread);
-    return;
+    
+    /**
+ * @file
+ * @brief Defines the FakeCanClient class which inherites CanClient.
+ */
+    
+    class FakeCanClientTest : public ::testing::Test {
+ public:
+  static const int32_t FRAME_LEN = 10;
+    }
+    
+      /**
+   * @brief Stop the ESD CAN client.
+   */
+    
+    
+    {    // Synchronous transmission of CAN messages
+    int ret = write(dev_handler_, &send_frames_[i], sizeof(send_frames_[i]));
+    if (ret <= 0) {
+      AERROR << 'send message failed, error code: ' << ret;
+      return ErrorCode::CAN_CLIENT_ERROR_BASE;
+    }
   }
-  if(env->ExceptionCheck()) {
-    // exception thrown: OutOfMemoryError
-    env->ExceptionDescribe(); // print out exception to stderr
-    env->DeleteLocalRef(jsStart);
-    releaseJniEnv(attached_thread);
-    return;
+    
+    #include <cstdint>
+    
+      // Init can client
+  auto *can_factory = CanClientFactory::instance();
+  can_factory->RegisterCanClients();
+  can_client_ = can_factory->CreateCANClient(canbus_conf_.can_card_parameter());
+  if (!can_client_) {
+    return OnError('Failed to create can client.');
   }
+  AINFO << 'Can client is successfully created.';
     
-    #include <jni.h>
-#include <memory>
-#include <string>
-#include 'rocksjni/jnicallback.h'
-#include 'rocksdb/comparator.h'
-#include 'rocksdb/slice.h'
-#include 'port/port.h'
-    
-    
-void SyncPoint::Data::LoadDependency(const std::vector<SyncPointPair>& dependencies) {
-  std::lock_guard<std::mutex> lock(mutex_);
-  successors_.clear();
-  predecessors_.clear();
-  cleared_points_.clear();
-  for (const auto& dependency : dependencies) {
-    successors_[dependency.predecessor].push_back(dependency.successor);
-    predecessors_[dependency.successor].push_back(dependency.predecessor);
-  }
-  cv_.notify_all();
-}
-    
-      std::mutex              mutex_;
-  std::condition_variable cv_;
-  // sync points that have been passed through
-  std::unordered_set<std::string> cleared_points_;
-  std::atomic<bool> enabled_;
-  int num_callbacks_running_ = 0;
+    // Sensor gflags
+DECLARE_double(sensor_freq);
