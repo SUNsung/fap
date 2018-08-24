476551064e6af7347e1ac7d78492d6fd51b12d15
SUNsung <weatherius@gmail.com>
2018-08-25 01:58:31 +0300

    Rewrite [53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py] File in: 2018-08-25 01:58:30

--- pyton/53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py
+++ pyton/53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py
@@ -1,188 +1,159 @@
 
         
-            # Get the version from youtube_dl/version.py without importing the package
-    exec(compile(open('youtube_dl/version.py').read(),
-                 'youtube_dl/version.py', 'exec'))
+        flags = tf.app.flags
+flags.DEFINE_string('save_dir', '/tmp/' + DATA_DIR + '/',
+                    'Directory for saving data.')
+flags.DEFINE_string('datafile_name', 'thits_data',
+                    'Name of data file for input case.')
+flags.DEFINE_string('noise_type', 'poisson', 'Noise type for data.')
+flags.DEFINE_integer('synth_data_seed', 5, 'Random seed for RNN generation.')
+flags.DEFINE_float('T', 1.0, 'Time in seconds to generate.')
+flags.DEFINE_integer('C', 100, 'Number of conditions')
+flags.DEFINE_integer('N', 50, 'Number of units for the RNN')
+flags.DEFINE_integer('S', 50, 'Number of sampled units from RNN')
+flags.DEFINE_integer('npcs', 10, 'Number of PCS for multi-session case.')
+flags.DEFINE_float('train_percentage', 4.0/5.0,
+                   'Percentage of train vs validation trials')
+flags.DEFINE_integer('nreplications', 40,
+                     'Number of noise replications of the same underlying rates.')
+flags.DEFINE_float('g', 1.5, 'Complexity of dynamics')
+flags.DEFINE_float('x0_std', 1.0,
+                   'Volume from which to pull initial conditions (affects diversity of dynamics.')
+flags.DEFINE_float('tau', 0.025, 'Time constant of RNN')
+flags.DEFINE_float('dt', 0.010, 'Time bin')
+flags.DEFINE_float('input_magnitude', 20.0,
+                   'For the input case, what is the value of the input?')
+flags.DEFINE_float('max_firing_rate', 30.0, 'Map 1.0 of RNN to a spikes per second')
+FLAGS = flags.FLAGS
     
-    with io.open(README_FILE, encoding='utf-8') as f:
-    oldreadme = f.read()
+      # Now for each dataset, we regress the channel data onto the top
+  # pcs, and this will be our alignment matrix for that dataset.
+  # |B - A*W|^2
+  for name, dataset in datasets.items():
+    cidx_s = channel_idxs[name][0]
+    cidx_f = channel_idxs[name][1]
+    all_data_zm_chxtc = all_data_zm_nxtc[cidx_s:cidx_f,:] # ch for channel
+    W_chxp, _, _, _ = \
+        np.linalg.lstsq(all_data_zm_chxtc.T, all_data_pca_pxtc.T)
+    dataset['alignment_matrix_cxf'] = W_chxp
+    alignment_bias_cx1 = all_data_mean_nx1[cidx_s:cidx_f]
+    dataset['alignment_bias_c'] = np.squeeze(alignment_bias_cx1, axis=1)
     
-        for release in releases:
-        compat_print(release['name'])
-        for asset in release['assets']:
-            asset_name = asset['name']
-            total_bytes += asset['download_count'] * asset['size']
-            if all(not re.match(p, asset_name) for p in (
-                    r'^youtube-dl$',
-                    r'^youtube-dl-\d{4}\.\d{2}\.\d{2}(?:\.\d+)?\.tar\.gz$',
-                    r'^youtube-dl\.exe$')):
-                continue
-            compat_print(
-                ' %s size: %s downloads: %d'
-                % (asset_name, format_size(asset['size']), asset['download_count']))
+      # Note the use of get_variable vs. tf.Variable.  this is because get_variable
+  # does not allow the initialization of the variable with a value.
+  if normalized:
+    w_collections = [tf.GraphKeys.GLOBAL_VARIABLES, 'norm-variables']
+    if collections:
+      w_collections += collections
+    if mat_init_value is not None:
+      w = tf.Variable(mat_init_value, name=wname, collections=w_collections,
+                      trainable=trainable)
+    else:
+      w = tf.get_variable(wname, [in_size, out_size], initializer=mat_init,
+                          collections=w_collections, trainable=trainable)
+    w = tf.nn.l2_normalize(w, dim=0) # x W, so xW_j = \sum_i x_bi W_ij
+  else:
+    w_collections = [tf.GraphKeys.GLOBAL_VARIABLES]
+    if collections:
+      w_collections += collections
+    if mat_init_value is not None:
+      w = tf.Variable(mat_init_value, name=wname, collections=w_collections,
+                      trainable=trainable)
+    else:
+      w = tf.get_variable(wname, [in_size, out_size], initializer=mat_init,
+                          collections=w_collections, trainable=trainable)
+  b = None
+  if do_bias:
+    b_collections = [tf.GraphKeys.GLOBAL_VARIABLES]
+    if collections:
+      b_collections += collections
+    bname = (name + '/b') if name else '/b'
+    if bias_init_value is None:
+      b = tf.get_variable(bname, [1, out_size],
+                          initializer=tf.zeros_initializer(),
+                          collections=b_collections,
+                          trainable=trainable)
+    else:
+      b = tf.Variable(bias_init_value, name=bname,
+                      collections=b_collections,
+                      trainable=trainable)
     
     
-def expect_value(self, got, expected, field):
-    if isinstance(expected, compat_str) and expected.startswith('re:'):
-        match_str = expected[len('re:'):]
-        match_rex = re.compile(match_str)
+def construct_ngrams_dict(ngrams_list):
+  '''Construct a ngram dictionary which maps an ngram tuple to the number
+  of times it appears in the text.'''
+  counts = {}
     
-        _TESTS = [{
-        'url': 'http://camwithher.tv/view_video.php?viewkey=6e9a24e2c0e842e1f177&page=&viewtype=&category=',
-        'info_dict': {
-            'id': '5644',
-            'ext': 'flv',
-            'title': 'Periscope Tease',
-            'description': 'In the clouds teasing on periscope to my favorite song',
-            'duration': 240,
-            'view_count': int,
-            'comment_count': int,
-            'uploader': 'MileenaK',
-            'upload_date': '20160322',
-        },
-        'params': {
-            'skip_download': True,
-        }
-    }, {
-        'url': 'http://camwithher.tv/view_video.php?viewkey=6dfd8b7c97531a459937',
-        'only_matching': True,
-    }, {
-        'url': 'http://camwithher.tv/view_video.php?page=&viewkey=6e9a24e2c0e842e1f177&viewtype=&category=',
-        'only_matching': True,
-    }, {
-        'url': 'http://camwithher.tv/view_video.php?viewkey=b6c3b5bea9515d1a1fc4&page=&viewtype=&category=mv',
-        'only_matching': True,
-    }]
-    
-        _CONFIG = {
-        # http://edition.cnn.com/.element/apps/cvp/3.0/cfg/spider/cnn/expansion/config.xml
-        'edition': {
-            'data_src': 'http://edition.cnn.com/video/data/3.0/video/%s/index.xml',
-            'media_src': 'http://pmd.cdn.turner.com/cnn/big',
-        },
-        # http://money.cnn.com/.element/apps/cvp2/cfg/config.xml
-        'money': {
-            'data_src': 'http://money.cnn.com/video/data/4.0/video/%s.xml',
-            'media_src': 'http://ht3.cdn.turner.com/money/big',
-        },
+      if not FLAGS.seq2seq_share_embedding:
+    variable_mapping = {
+        str(model_str) + '/embedding':
+            encoder_embedding,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/kernel':
+            encoder_lstm_w_0,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/bias':
+            encoder_lstm_b_0,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/kernel':
+            encoder_lstm_w_1,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/bias':
+            encoder_lstm_b_1
     }
+  else:
+    variable_mapping = {
+        str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/kernel':
+            encoder_lstm_w_0,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/bias':
+            encoder_lstm_b_0,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/kernel':
+            encoder_lstm_w_1,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/bias':
+            encoder_lstm_b_1
+    }
+  return variable_mapping
     
-        @property
-    @abstractmethod
-    def value(self):
-        pass
+        def test_server(self):
     
-            Accessing a node updates its position to the front of the LRU list.
-        '''
-        node = self.lookup.get(query)
-        if node is None:
-            return None
-        self.linked_list.move_to_front(node)
-        return node.results
+    # register the Foo class; make `g()` and `_h()` accessible via proxy
+MyManager.register('Foo2', Foo, exposed=('g', '_h'))
     
-        def remove(self, key):
-        hash_index = self._hash_function(key)
-        for index, item in enumerate(self.table[hash_index]):
-            if item.key == key:
-                del self.table[hash_index][index]
-                return
-        raise KeyError('Key not found')
+        def test_chunked(self):
+        self.check_url('/chunked')
+    
+    import tornado.ioloop
+import tornado.web
+from tornado import options
+    
+    import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+    
+        # Start workers, then wait for the work queue to be empty.
+    workers = gen.multi([worker() for _ in range(concurrency)])
+    await q.join(timeout=timedelta(seconds=300))
+    assert fetching == fetched
+    print('Done in %d seconds, fetched %s URLs.' % (
+        time.time() - start, len(fetched)))
+    
+        def callback(response):
+        response.rethrow()
+        assert len(response.body) == (options.num_chunks * options.chunk_size)
+        logging.warning('fetch completed in %s seconds', response.request_time)
+        IOLoop.current().stop()
+    
+    
+if __name__ == '__main__':
+    main()
 
     
-            Accessing a node updates its position to the front of the LRU list.
-        '''
-        node = self.lookup[query]
-        if node is None:
-            return None
-        self.linked_list.move_to_front(node)
-        return node.results
-    
-        def crawl(self):
-        while True:
-            page = self.data_store.extract_max_priority_page()
-            if page is None:
-                break
-            if self.data_store.crawled_similar(page.signature):
-                self.data_store.reduce_priority_link_to_crawl(page.url)
-            else:
-                self.crawl_page(page)
-            page = self.data_store.extract_max_priority_page()
-
-    
-            run = response.json()
-    
-    # A hostname label, e.g. 'foo' in 'foo.example.com'. Consists of alphanumeric
-# characters plus dashes (and underscores) or valid ranges. The label may not
-# start or end with a hyphen or an underscore. This is interpolated into the
-# hostname pattern below. We don't try to enforce the 63-char length limit.
-    
-            # global URL, resource, entity, method_name
-        input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/global/backendServices/mybackendservice/getHealth'
-        actual = GCPUtils.parse_gcp_url(input_url)
-        self.assertEquals('compute', actual['service'])
-        self.assertEquals('v1', actual['api_version'])
-        self.assertEquals('myproject', actual['project'])
-        self.assertTrue('global' in actual)
-        self.assertTrue(actual['global'])
-        self.assertEquals('backendServices', actual['resource_name'])
-        self.assertEquals('mybackendservice', actual['entity_name'])
-        self.assertEquals('getHealth', actual['method_name'])
+    extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+]
     
     
-def get_group_vars(groups):
-    
-        def test_max_delay(self):
-        strategy = _exponential_backoff(retries=7, delay=1, backoff=2, max_delay=60)
-        result = list(strategy())
-        self.assertEquals(result, [1, 2, 4, 8, 16, 32, 60])
-    
-        def _dicts(self, ajvars):
-        print(ajvars)
-        res28 = self._dict_jinja28(ajvars)
-        res29 = self._dict_jinja29(ajvars)
-        # res28_other = self._dict_jinja28(ajvars, {'other_key': 'other_value'})
-        # other = {'other_key': 'other_value'}
-        # res29_other = self._dict_jinja29(ajvars, *other)
-        print('res28: %s' % res28)
-        print('res29: %s' % res29)
-        # print('res28_other: %s' % res28_other)
-        # print('res29_other: %s' % res29_other)
-        # return (res28, res29, res28_other, res29_other)
-        # assert ajvars == res28
-        # assert ajvars == res29
-        return (res28, res29)
-    
-    In Blackboard pattern several specialised sub-systems (knowledge sources)
-assemble their knowledge to build a possibly partial or approximate solution.
-In this way, the sub-systems work together to solve the problem,
-where the solution is the sum of its parts.
-    
-        @classmethod
-    def setUpClass(self):
-        '''
-        - Create a temporary directory and file
-        /test_command
-           /foo.txt
-        - get the temporary test directory
-        - and initializes the command stack.
-        '''
-        os.mkdir('tests/test_command')
-        open('tests/test_command/foo.txt', 'w').close()
-        self.__get_test_directory()
-        self.command_stack = []
-        self.command_stack.append(MoveFileCommand(os.path.join(
-            self.test_dir, 'foo.txt'), os.path.join(self.test_dir, 'bar.txt')))
-        self.command_stack.append(MoveFileCommand(os.path.join(
-            self.test_dir, 'bar.txt'), os.path.join(self.test_dir, 'baz.txt')))
-    
-        def test_am_station_overflow_after_scan(self):
-        self.radio.scan()
-        station = self.radio.state.stations[self.radio.state.pos]
-        expected_station = '1250'
-        self.assertEqual(station, expected_station)
-    
-        def execute(self):
-        self.rename(self.src, self.dest)
-    
-    import random
-import time
+@gen.coroutine
+def main():
+    args = parse_command_line()
