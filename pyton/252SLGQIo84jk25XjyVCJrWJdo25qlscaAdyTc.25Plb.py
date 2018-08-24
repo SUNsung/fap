8d4a201ac45669f11143e99a6184c498bb3b6b9f
SUNsung <weatherius@gmail.com>
2018-08-24 20:22:15 +0300

    Rewrite [252SLGQIo84jk25XjyVCJrWJdo25qlscaAdyTc.25Plb.py] File in: 2018-08-24 20:22:14

--- pyton/252SLGQIo84jk25XjyVCJrWJdo25qlscaAdyTc.25Plb.py
+++ pyton/252SLGQIo84jk25XjyVCJrWJdo25qlscaAdyTc.25Plb.py
@@ -1,222 +1,57 @@
 
         
-        
-def check_alphabetical(lines):
+            # Separating the 'table of contents' from the contents (blocks)
+    table_of_contents = ''.join(read_me.split('- - -')[0])
+    blocks = ''.join(read_me.split('- - -')[1]).split('\n# ')
+    for i in range(len(blocks)):
+        if i == 0:
+            blocks[i] = blocks[i] + '\n'
+        else:
+            blocks[i] = '# ' + blocks[i] + '\n'
+    
+    
+def init_app(app):
+    '''Register database functions with the Flask app. This is called by
+    the application factory.
     '''
-    checks if all entries per section are in alphabetical order based in entry title
-    '''
-    sections = {}
-    section_line_num = {}
-    for line_num, line in enumerate(lines):
-        if line.startswith(anchor):
-            category = line.split(anchor)[1].strip()
-            sections[category] = []
-            section_line_num[category] = line_num
-            continue
-        if not line.startswith('|') or line.startswith('|---'):
-            continue
-        title = [x.strip() for x in line.split('|')[1:-1]][0].upper()
-        sections[category].append(title)
-    
-            self.assertRaises(TypeError, d.rotate, 'x')   # Wrong arg type
-        self.assertRaises(TypeError, d.rotate, 1, 10) # Too many args
-    
-        @classmethod
-    def fromfile(cls, fileobj):
-        if fileobj.read(4).decode() != 'TZif':
-            raise ValueError('not a zoneinfo file')
-        fileobj.seek(20)
-        header = fileobj.read(24)
-        tzh = (tzh_ttisgmtcnt, tzh_ttisstdcnt, tzh_leapcnt,
-               tzh_timecnt, tzh_typecnt, tzh_charcnt) = struct.unpack('>6l', header)
-        transitions = array('i')
-        transitions.fromfile(fileobj, tzh_timecnt)
-        if sys.byteorder != 'big':
-            transitions.byteswap()
-    
-        def find_byrgb(self, rgbtuple):
-        '''Return name for rgbtuple'''
-        try:
-            return self.__byrgb[rgbtuple]
-        except KeyError:
-            raise BadColor(rgbtuple) from None
-    
-        def contents(self):
-        # This is a bit convoluted, because fullname will be a module path,
-        # but _files is a list of file names relative to the top of the
-        # archive's namespace.  We want to compare file paths to find all the
-        # names of things inside the module represented by fullname.  So we
-        # turn the module path of fullname into a file path relative to the
-        # top of the archive, and then we iterate through _files looking for
-        # names inside that 'directory'.
-        fullname_path = Path(self.zipimporter.get_filename(self.fullname))
-        relative_path = fullname_path.relative_to(self.zipimporter.archive)
-        # Don't forget that fullname names a package, so its path will include
-        # __init__.py, which we want to ignore.
-        assert relative_path.name == '__init__.py'
-        package_path = relative_path.parent
-        subdirs_seen = set()
-        for filename in self.zipimporter._files:
-            try:
-                relative = Path(filename).relative_to(package_path)
-            except ValueError:
-                continue
-            # If the path of the file (which is relative to the top of the zip
-            # namespace), relative to the package given when the resource
-            # reader was created, has a parent, then it's a name in a
-            # subdirectory and thus we skip it.
-            parent_name = relative.parent.name
-            if len(parent_name) == 0:
-                yield relative.name
-            elif parent_name not in subdirs_seen:
-                subdirs_seen.add(parent_name)
-                yield parent_name
-    
-            *kwargs* is a dictionary of keyword arguments for the target
-        invocation. Defaults to {}.
-    
-        def test___classcell___missing(self):
-        # See issue #23722
-        # Some metaclasses may not pass the original namespace to type.__new__
-        # We test that case here by forcibly deleting __classcell__
-        class Meta(type):
-            def __new__(cls, name, bases, namespace):
-                namespace.pop('__classcell__', None)
-                return super().__new__(cls, name, bases, namespace)
-    
-            a = 0
-        b = sys.maxsize**10
-        c = 2*sys.maxsize
-        expected_len = 1 + (b - a) // c
-        x = range(a, b, c)
-        self.assertIn(a, x)
-        self.assertNotIn(b, x)
-        self.assertRaises(OverflowError, len, x)
-        self.assertTrue(x)
-        self.assertEqual(_range_len(x), expected_len)
-        self.assertEqual(x[0], a)
-        idx = sys.maxsize+1
-        self.assertEqual(x[idx], a+(idx*c))
-        self.assertEqual(x[idx:idx+1][0], a+(idx*c))
-        with self.assertRaises(IndexError):
-            x[-expected_len-1]
-        with self.assertRaises(IndexError):
-            x[expected_len]
-    
-    def main():
-    for name, fn in [('sequential',
-                      functools.partial(download_urls_sequential, URLS)),
-                     ('processes',
-                      functools.partial(download_urls_with_executor,
-                                        URLS,
-                                        ProcessPoolExecutor(10))),
-                     ('threads',
-                      functools.partial(download_urls_with_executor,
-                                        URLS,
-                                        ThreadPoolExecutor(10)))]:
-        sys.stdout.write('%s: ' % name.ljust(12))
-        start = time.time()
-        url_map = fn()
-        sys.stdout.write('%.2f seconds (%d of %d downloaded)\n' %
-                         (time.time() - start, len(url_map), len(URLS)))
-    
-    
-  @staticmethod
-  def CreateFromOptions( user_options ):
-    all_filters = dict( user_options.get( 'filter_diagnostics', {} ) )
-    compiled_by_type = {}
-    for type_spec, filter_value in iteritems( dict( all_filters ) ):
-      filetypes = [ type_spec ]
-      if type_spec.find( ',' ) != -1:
-        filetypes = type_spec.split( ',' )
-      for filetype in filetypes:
-        compiled_by_type[ filetype ] = _CompileFilters( filter_value )
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    
-# Not caching the result of this function; users shouldn't have to restart Vim
-# after running the install script or setting the
-# `g:ycm_server_python_interpreter` option.
-def PathToPythonInterpreter():
-  # Not calling the Python interpreter to check its version as it significantly
-  # impacts startup time.
-  from ycmd import utils
-    
-      for path in python_paths:
-    yield EndsWithPython_Good, path
-    
-    
-def _EnsureBackwardsCompatibility( arguments ):
-  if arguments and arguments[ 0 ] == 'GoToDefinitionElseDeclaration':
-    arguments[ 0 ] = 'GoTo'
-  return arguments
-    
-    import logging
-    
-        def test_c_observers_shall_be_detachable(cls):
-        cls.s.detach(cls.dec_obs)
-        # hex viewer shall be remaining if dec viewer is detached first
-        cls.assertEqual(isinstance(cls.s._observers[0], HexViewer), True)
-        cls.assertEqual(len(cls.s._observers), 1)
-        cls.s.detach(cls.hex_obs)
-        cls.assertEqual(len(cls.s._observers), 0)
-    
-        def test_sales_manager_shall_not_respond_through_proxy_with_delay(cls):
-        cls.ntp.busy = 'Yes'
-        start_time = time()
-        cls.ntp.talk()
-        end_time = time()
-        execution_time = end_time - start_time
-        print_output = cls.output.getvalue()
-        expected_print_output = 'Proxy checking for Sales Manager availability\n\
-This Sales Manager will not talk to you whether he/she is busy or not\n'
-        cls.assertEqual(print_output, expected_print_output)
-        expected_execution_time = 1
-        cls.assertEqual(int(execution_time*10), expected_execution_time)
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
 
     
-        class ClassRegistree(BaseRegisteredClass):
     
-        def setUp(self):
-        self.tc1 = TC1()
-        self.tc2 = TC2()
-        self.tc3 = TC3()
-        self.average_result_tc1 = '###### In Test 1 ######\n' + \
-                                  'Setting up\n' + \
-                                  'Running test\n' + \
-                                  'Tearing down\n' + \
-                                  'Test Finished'
-        self.average_result_tc2 = '###### In Test 2 ######\n' + \
-                                  'Setting up\n' + \
-                                  'Running test\n' + \
-                                  'Tearing down\n' + \
-                                  'Test Finished'
-        self.average_result_tc3 = '###### In Test 3 ######\n' + \
-                                  'Setting up\n' + \
-                                  'Running test\n' + \
-                                  'Tearing down\n' + \
-                                  'Test Finished'
-        self.runner = TestRunner()
-        self.out = StringIO()
-        self.saved_stdout = sys.stdout
-        sys.stdout = self.out
+@pytest.fixture
+def client(app):
+    '''A test client for the app.'''
+    return app.test_client()
     
-    Test code which will almost always fail (if not exactly 12:01) when untestable
-production code (production code time provider is datetime) is used:
+            -   a string: in this case the object with that name will be imported
+        -   an actual object reference: that object is used directly
     
-    '''
-*TL;DR80
-Encapsulates all information needed to perform an action or trigger an event.
-'''
+        def get_cookie_samesite(self, app):
+        '''Return ``'Strict'`` or ``'Lax'`` if the cookie should use the
+        ``SameSite`` attribute. This currently just returns the value of
+        the :data:`SESSION_COOKIE_SAMESITE` setting.
+        '''
+        return app.config['SESSION_COOKIE_SAMESITE']
     
-    '''
-http://code.activestate.com/recipes/413838-memento-closure/
     
-        def is_satisfied_by(self, candidate):
-        return isinstance(candidate, User)
+class Node(object):
+    pass
+    
+        def get_product_information(self, product):
+        product_info = self.business_logic.product_information(product)
+        if product_info:
+            print('PRODUCT INFORMATION:')
+            print('Name: {0}, Price: {1:.2f}, Quantity: {2:}'.format(
+                product.title(), product_info.get('price', 0),
+                product_info.get('quantity', 0)))
+        else:
+            print('That product '{0}' does not exist in the records'.format(
+                product))
+    
+    
+def main():
+    shapes = (
+        CircleShape(1, 2, 3, DrawingAPI1()),
+        CircleShape(5, 7, 11, DrawingAPI2())
+    )
