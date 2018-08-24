1f68f0742b076a5c8a552b9f38d72bb27f88defa
SUNsung <weatherius@gmail.com>
2018-08-24 03:15:40 +0300

    Rewrite [59QbtwwpN4kJg590nVZKhFtYyQ59urjQgG6UeCI59iKr.py] File in: 2018-08-24 03:15:39

--- pyton/59QbtwwpN4kJg590nVZKhFtYyQ59urjQgG6UeCI59iKr.py
+++ pyton/59QbtwwpN4kJg590nVZKhFtYyQ59urjQgG6UeCI59iKr.py
@@ -1,282 +1,86 @@
 
         
-        Let's seen an example that adds support for :class:`~collections.OrderedDict`.
-Dicts don't have an order in Python or JSON, so to handle this we will dump
-the items as a list of ``[key, value]`` pairs. Subclass :class:`JSONTag` and
-give it the new key ``' od'`` to identify the type. The session serializer
-processes dicts first, so insert the new tag at the front of the order since
-``OrderedDict`` must be processed before ``dict``. ::
-    
-        If there is no handler for the logger's effective level, add a
-    :class:`~logging.StreamHandler` for
-    :func:`~flask.logging.wsgi_errors_stream` with a basic format.
-    '''
-    logger = logging.getLogger('flask.app')
-    
-        This module provides class-based views inspired by the ones in Django.
-    
-        app = flask.Flask(__name__)
-    with pytest.raises(TypeError):
-        app.config.from_mapping(
-            {}, {}
-        )
-    
-        @app.route('/test')
-    def test():
-        raise Foo()
-    
-        @app.teardown_request
-    def end_of_request(exception):
-        buffer.append(exception)
+        
+def test_when_already_configured(usage_tracker_io, shell_pid,
+                                 shell, shell_config, logs):
+    shell.get_history.return_value = ['fuck']
+    shell_pid.return_value = 12
+    _change_tracker(usage_tracker_io, 12)
+    shell_config.read.return_value = 'eval $(thefuck --alias)'
+    main()
+    logs.already_configured.assert_called_once()
     
     
-@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
-@pytest.mark.parametrize('filename', filenames)
-def test_rst_file_syntax(filename):
-    p = subprocess.Popen(
-        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE
-    )
-    err = p.communicate()[1]
-    assert p.returncode == 0, err.decode('utf8')
+@pytest.mark.functional
+def test_how_to_configure_alias(proc, TIMEOUT):
+    proc.sendline('unset -f fuck')
+    how_to_configure(proc, TIMEOUT)
 
     
-    from httpie import ExitStatus
-from httpie.core import main
+    containers = (('thefuck/python3-fish',
+               u'''FROM python:3
+                   # Use jessie-backports since it has the fish package. See here for details:
+                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
+                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
+                   RUN apt-get update
+                   RUN apt-get install -yy fish''',
+               u'fish'),
+              ('thefuck/python2-fish',
+               u'''FROM python:2
+                   # Use jessie-backports since it has the fish package. See here for details:
+                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
+                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
+                   RUN apt-get update
+                   RUN apt-get install -yy fish''',
+               u'fish'))
     
-            '''
-        return headers
+    containers = (('thefuck/python3-tcsh',
+               u'''FROM python:3
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'),
+              ('thefuck/python2-tcsh',
+               u'''FROM python:2
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'))
     
     
-CLIENT_CERT = os.path.join(TESTS_ROOT, 'client_certs', 'client.crt')
-CLIENT_KEY = os.path.join(TESTS_ROOT, 'client_certs', 'client.key')
-CLIENT_PEM = os.path.join(TESTS_ROOT, 'client_certs', 'client.pem')
+init_zshrc = u'''echo '
+export SHELL=/usr/bin/zsh
+export HISTFILE=~/.zsh_history
+echo > $HISTFILE
+export SAVEHIST=100
+export HISTSIZE=100
+eval $(thefuck --alias {})
+setopt INC_APPEND_HISTORY
+echo 'instant mode ready: $THEFUCK_INSTANT_MODE'
+' > ~/.zshrc'''
     
-        Assumes `from __future__ import division`.
     
-    ``response``:
-    The response generated from a Request.
+@pytest.mark.parametrize('command, packages', [
+    (Command('vim', 'vim: command not found'),
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('sudo vim', 'vim: command not found'),
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('vim', 'The program 'vim' is currently not installed. You can install it by typing: sudo apt install vim'),
+     [('vim', 'main'), ('vim-tiny', 'main')])])
+def test_match(mocker, command, packages):
+    mocker.patch('thefuck.rules.apt_get.which', return_value=None)
+    mocker.patch('thefuck.rules.apt_get._get_packages',
+                 create=True, return_value=packages)
+    
+    match_output = '''
+Hit:1 http://us.archive.ubuntu.com/ubuntu zesty InRelease
+Hit:2 http://us.archive.ubuntu.com/ubuntu zesty-updates InRelease
+Get:3 http://us.archive.ubuntu.com/ubuntu zesty-backports InRelease [89.2 kB]
+Hit:4 http://security.ubuntu.com/ubuntu zesty-security InRelease
+Hit:5 http://ppa.launchpad.net/ubuntu-mozilla-daily/ppa/ubuntu zesty InRelease
+Hit:6 https://download.docker.com/linux/ubuntu zesty InRelease
+Hit:7 https://cli-assets.heroku.com/branches/stable/apt ./ InRelease
+Fetched 89.2 kB in 0s (122 kB/s)
+Reading package lists... Done
+Building dependency tree
+Reading state information... Done
+8 packages can be upgraded. Run 'apt list --upgradable' to see them.
 '''
-HOOKS = ['response']
-    
-    
-def prepare_url(value):
-    # Issue #1483: Make sure the URL always has a trailing slash
-    httpbin_url = value.url.rstrip('/') + '/'
-    
-                return request_content
-    
-                if not os.path.isdir(cert_loc):
-                conn.ca_certs = cert_loc
-            else:
-                conn.ca_cert_dir = cert_loc
-        else:
-            conn.cert_reqs = 'CERT_NONE'
-            conn.ca_certs = None
-            conn.ca_cert_dir = None
-    
-    import pytest
-import threading
-import requests
-    
-    # The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
-#html_logo = None
-    
-                return _r
-    
-    
-def get(url, params=None, **kwargs):
-    r'''Sends a GET request.
-    
-        model.train_on_batch(x_train[:32], y_train[:32],
-                         sample_weight=sample_weight[:32])
-    model.test_on_batch(x_train[:32], y_train[:32],
-                        sample_weight=sample_weight[:32])
-    score = model.evaluate(x_test[test_ids, :], y_test[test_ids, :], verbose=0)
-    assert(score < standard_score_sequential)
-    
-    
-@keras_test
-def test_avgpooling2d_legacy_interface():
-    old_layer = keras.layers.AveragePooling2D(pool_size=(2, 2), border_mode='valid', name='avgpooling2d')
-    new_layer = keras.layers.AvgPool2D(pool_size=(2, 2), padding='valid', name='avgpooling2d')
-    assert json.dumps(old_layer.get_config()) == json.dumps(new_layer.get_config())
-    
-        reconstruction_loss *= image_size * image_size
-    kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
-    kl_loss = K.sum(kl_loss, axis=-1)
-    kl_loss *= -0.5
-    vae_loss = K.mean(reconstruction_loss + kl_loss)
-    vae.add_loss(vae_loss)
-    vae.compile(optimizer='rmsprop')
-    vae.summary()
-    plot_model(vae, to_file='vae_cnn.png', show_shapes=True)
-    
-        for strides in [(1, 1), (2, 2)]:
-        layer_test(local.LocallyConnected2D,
-                   kwargs={'filters': filters,
-                           'kernel_size': 3,
-                           'padding': padding,
-                           'kernel_regularizer': 'l2',
-                           'bias_regularizer': 'l2',
-                           'activity_regularizer': 'l2',
-                           'strides': strides,
-                           'data_format': 'channels_last'},
-                   input_shape=(num_samples, num_row, num_col, stack_size))
-    
-                # check state initialization
-            layer = convolutional_recurrent.ConvLSTM2D(filters=filters,
-                                                       kernel_size=(num_row, num_col),
-                                                       data_format=data_format,
-                                                       return_sequences=return_sequences)
-            layer.build(inputs.shape)
-            x = Input(batch_shape=inputs.shape)
-            initial_state = layer.get_initial_state(x)
-            y = layer(x, initial_state=initial_state)
-            model = Model(x, y)
-            assert model.predict(inputs).shape == layer.compute_output_shape(inputs.shape)
-    
-        y_a = K.variable(np.random.randint(0, 7, (6,)))
-    y_b = K.variable(np.random.random((6, 7)))
-    assert K.eval(losses.sparse_categorical_crossentropy(y_a, y_b)).shape == (6,)
-    
-    
-def load_data():
-    '''Loads CIFAR10 dataset.
-    
-        return (x_train, y_train), (x_test, y_test)
-
-    
-        for i, DD in enumerate(Drange):
-        print('D = %i (%i out of %i)' % (DD, i + 1, len(Drange)))
-        X = get_data(N, DD, dataset)
-        for algorithm in algorithms:
-            nbrs = neighbors.NearestNeighbors(n_neighbors=k,
-                                              algorithm=algorithm,
-                                              leaf_size=leaf_size)
-            t0 = time()
-            nbrs.fit(X)
-            t1 = time()
-            nbrs.kneighbors(X)
-            t2 = time()
-    
-    n_samples = np.logspace(.5, 3, 9)
-n_features = np.logspace(1, 3.5, 7)
-N_samples, N_features = np.meshgrid(n_samples,
-                                    n_features)
-scikits_time = np.zeros(N_samples.shape)
-scipy_time = np.zeros(N_samples.shape)
-    
-    import os
-import tarfile
-from contextlib import closing
-    
-        input_file = open(os.path.join(exercise_dir, f))
-    output_file = open(os.path.join(skeleton_dir, f), 'w')
-    
-    model = SpectralCoclustering(n_clusters=5, random_state=0)
-model.fit(data)
-score = consensus_score(model.biclusters_,
-                        (rows[:, row_idx], columns[:, col_idx]))
-    
-    import numpy as np
-import matplotlib.pyplot as plt
-from time import time
-from sklearn import metrics
-    
-    plot_2d(ax2, n_labels=3)
-ax2.set_title('n_labels=3, length=50')
-ax2.set_xlim(left=0, auto=True)
-ax2.set_ylim(bottom=0, auto=True)
-    
-            if not data:
-            self._eof_received()
-            return
-    
-    class DeviceEncodingTests(unittest.TestCase):
-    
-        PATTERN = '''
-        atom< ('[' | '(')
-            (listmaker< any
-                comp_for<
-                    'for' NAME 'in'
-                    target=testlist_safe< any (',' any)+ [',']
-                     >
-                    [any]
-                >
-            >
-            |
-            testlist_gexp< any
-                comp_for<
-                    'for' NAME 'in'
-                    target=testlist_safe< any (',' any)+ [',']
-                     >
-                    [any]
-                >
-            >)
-        (']' | ')') >
-    '''
-    
-        P2 = '''for_stmt< 'for' any 'in' node=any ':' any* >
-            | comp_for< 'for' any 'in' node=any any* >
-            | comparison< any 'in' node=any any*>
-         '''
-    p2 = patcomp.compile_pattern(P2)
-    
-        def test_trailing_commas_lambda_8(self):
-        self.validate('f = lambda a=1, b=2: call(a=1, b=2)')
-        self.validate('f = lambda a=1, b=2,: call(a=1, b=2,)')
-    
-        >>> import hashlib
-    >>> m = hashlib.md5()
-    >>> m.update(b'Nobody inspects')
-    >>> m.update(b' the spammish repetition')
-    >>> m.digest()
-    b'\\xbbd\\x9c\\x83\\xdd\\x1e\\xa5\\xc9\\xd9\\xde\\xc9\\xa1\\x8d\\xf0\\xff\\xe9'
-    
-        1) 'dir' is not already in 'dirlist'
-    2) 'dir' actually exists, and is a directory.
-    '''
-    if dir is None or not os.path.isdir(dir) or dir in dirlist:
-        return
-    for i, path in enumerate(dirlist):
-        if not os.path.isabs(path):
-            dirlist.insert(i + 1, dir)
-            return
-    dirlist.insert(0, dir)
-    
-        @classmethod
-    def __get_test_directory(self):
-        '''
-        Get the temporary directory for the tests.
-        '''
-        self.test_dir = os.path.join(os.path.dirname(
-            os.path.realpath(__file__)), 'test_command')
-    
-        jim = Subscriber('jim', message_center)
-    jim.subscribe('cartoon')
-    jack = Subscriber('jack', message_center)
-    jack.subscribe('music')
-    gee = Subscriber('gee', message_center)
-    gee.subscribe('movie')
-    vani = Subscriber('vani', message_center)
-    vani.subscribe('movie')
-    vani.unsubscribe('movie')
-    
-    
-class TestNaitivePool(unittest.TestCase):
-    
-            def __init__(self, *args, **kwargs):
-            pass
-    
-        def test_display_current_time_at_midnight(self):
-        '''
-        Would almost always fail (despite of right at/after midnight) if
-        untestable production code would have been used.
-        '''
-        time_provider_stub = MidnightTimeProvider()
-        class_under_test = TimeDisplay()
-        expected_time = '<span class=\'tinyBoldText\'>24:01</span>'
-        self.assertEqual(class_under_test.get_current_time_as_html_fragment(time_provider_stub), expected_time)
