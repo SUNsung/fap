b69637e3757691f126b5d20a942c0c33d69b1c88
SUNsung <weatherius@gmail.com>
2018-08-24 04:43:49 +0300

    Rewrite [26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb] File in: 2018-08-24 04:43:48

--- ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
+++ ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
@@ -1,46 +1,116 @@
 
         
-        class REST::MediaAttachmentSerializer < ActiveModel::Serializer
-  include RoutingHelper
-    
-      sidekiq_options queue: 'mailers'
-    
-      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/reblog
-  def reblog
-    r = Status.where.not(reblog_of_id: nil).first
-    NotificationMailer.reblog(r.reblog.account, Notification.find_by(activity: r))
+          def help
+    Helper.instance
   end
     
-      # body
-  xml.tag!('body') do
-    xml.tag!('outline', text: TITLE, title: TITLE) do
-      blogs.each do |blog|
-        xml.tag!('outline', type: 'rss', text: blog.name, title: blog.name,
-          xmlUrl: blog.rss_url, htmlUrl: blog.web_url)
+          def find_sessionless_user
+        find_user_from_access_token || find_user_from_feed_token
+      rescue Gitlab::Auth::AuthenticationError
+        nil
+      end
+    
+            def value_color
+          case @status
+          when 95..100 then STATUS_COLOR[:good]
+          when 90..95 then STATUS_COLOR[:acceptable]
+          when 75..90 then STATUS_COLOR[:medium]
+          when 0..75 then STATUS_COLOR[:low]
+          else
+            STATUS_COLOR[:unknown]
+          end
+        end
       end
     end
   end
 end
+
     
-    require 'formula'
+      def failure_message
+    exception = request.respond_to?(:get_header) ? request.get_header('omniauth.error') : request.env['omniauth.error']
+    error   = exception.error_reason if exception.respond_to?(:error_reason)
+    error ||= exception.error        if exception.respond_to?(:error)
+    error ||= (request.respond_to?(:get_header) ? request.get_header('omniauth.error.type') : request.env['omniauth.error.type']).to_s
+    error.to_s.humanize if error
+  end
     
-          tmp = ''
-      less.scan(/^(\s*&)(-[\w\[\]]+\s*\{.+})$/) do |ampersand, css|
-        tmp << '.badge#{css}\n'
-      end
-    
-        def log_file_info(s)
-      puts '    #{magenta s}'
-    end
-    
-          def escape_hash(hash)
-        hash = hash.dup
-        hash.each { |k,v| hash[k] = escape(v) }
-        hash
-      end
-    
-        it 'should be able to deal with PATH_INFO = nil (fcgi?)' do
-      app = Rack::Protection::PathTraversal.new(proc { 42 })
-      expect(app.call({})).to eq(42)
+      # Sets the flash message with :key, using I18n. By default you are able
+  # to set up your messages using specific resource scope, and if no message is
+  # found we look to the default scope. Set the 'now' options key to a true
+  # value to populate the flash.now hash in lieu of the default flash hash (so
+  # the flash message will be available to the current action instead of the
+  # next action).
+  # Example (i18n locale file):
+  #
+  #   en:
+  #     devise:
+  #       passwords:
+  #         #default_scope_messages - only if resource_scope is not found
+  #         user:
+  #           #resource_scope_messages
+  #
+  # Please refer to README or en.yml locale file to check what messages are
+  # available.
+  def set_flash_message(key, kind, options = {})
+    message = find_message(kind, options)
+    if options[:now]
+      flash.now[key] = message if message.present?
+    else
+      flash[key] = message if message.present?
     end
   end
+    
+    end
+    
+          def remember_cookie_values(resource)
+        options = { httponly: true }
+        options.merge!(forget_cookie_values(resource))
+        options.merge!(
+          value: resource.class.serialize_into_cookie(resource),
+          expires: resource.remember_expires_at
+        )
+      end
+    
+    describe 'Kernel#taint' do
+  it 'returns self' do
+    o = Object.new
+    o.taint.should equal(o)
+  end
+    
+    gem 'configurate', '0.3.1'
+    
+      # Setup the root logger with the Rails log level and the desired set of
+  # appenders. The list of appenders to use should be set in the environment
+  # specific configuration file.
+  #
+  # For example, in a production application you would not want to log to
+  # STDOUT, but you would want to send an email for 'error' and 'fatal'
+  # messages:
+  #
+  # => config/environments/production.rb
+  #
+  #     config.log_to = %w[file email]
+  #
+  # In development you would want to log to STDOUT and possibly to a file:
+  #
+  # => config/environments/development.rb
+  #
+  #     config.log_to = %w[stdout file]
+  #
+  Logging.logger.root.appenders = config.log_to unless config.log_to.empty?
+    
+    When /^I submit the password reset form$/ do
+  submit_password_reset_form
+end
+    
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
+    
+        it 'generates a jasmine fixture', fixture: true do
+      session[:mobile_view] = true
+      get :new, format: :mobile
+      save_fixture(html_for('body'), 'conversations_new_mobile')
+    end
+  end
+end
