dd190058f2b1be3b9ed128a9664bd8d62c941de5
SUNsung <weatherius@gmail.com>
2018-08-24 10:01:35 +0300

    Rewrite [50ITEPnVy7Lhg50lvVU2Ze4kQg50Z66KJZxYsic5082G.cpp] File in: 2018-08-24 10:01:34

--- cccccc/50ITEPnVy7Lhg50lvVU2Ze4kQg50Z66KJZxYsic5082G.cpp
+++ cccccc/50ITEPnVy7Lhg50lvVU2Ze4kQg50Z66KJZxYsic5082G.cpp
@@ -1,272 +1,98 @@
-  Blob<float>* input_layer = net_->input_blobs()[0];
-  num_channels_ = input_layer->channels();
-  CHECK(num_channels_ == 3 || num_channels_ == 1)
-    << 'Input layer should have 1 or 3 channels.';
-  input_geometry_ = cv::Size(input_layer->width(), input_layer->height());
-    
-    #endif  // CAFFE_DATA_TRANSFORMER_HPP_
 
+        
+        #if defined(UNIX_ENABLED) || defined(PTHREAD_ENABLED)
     
+    	if (!is_connection_available()) {
+		return Ref<StreamPeerTCP>();
+	};
     
-    {  bool handles_setup_;
-  cudnnHandle_t             handle_;
-  cudnnTensorDescriptor_t bottom_desc_, top_desc_;
-  cudnnPoolingDescriptor_t  pooling_desc_;
-  cudnnPoolingMode_t        mode_;
-};
+    #include 'thread_posix.h'
+#include 'script_language.h'
+    
+    	p_path = fix_path(p_path);
+    
+    #ifdef WINDOWS_ENABLED
+    
+    #include 'rw_lock_windows.h'
+    
+    void ThreadWindows::make_default() {
+    }
+    
+    /*!
+ * \brief Check if alignas(*) keyword is supported. (g++ 4.8 or higher)
+ */
+#if defined(__GNUC__) && ((__GNUC__ == 4 && __GNUC_MINOR__ >= 8) || __GNUC__ > 4)
+#define XGBOOST_ALIGNAS(X) alignas(X)
+#else
+#define XGBOOST_ALIGNAS(X)
 #endif
     
-     protected:
-  /**
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the computed outputs. At training time, we have @f$
-   *      y_{\mbox{train}} = \left\{
-   *         \begin{array}{ll}
-   *            \frac{x}{1 - p} & \mbox{if } u > p \\
-   *            0 & \mbox{otherwise}
-   *         \end{array} \right.
-   *      @f$, where @f$ u \sim U(0, 1)@f$ is generated independently for each
-   *      input at each iteration. At test time, we simply have
-   *      @f$ y_{\mbox{test}} = \mathbb{E}[y_{\mbox{train}}] = x @f$.
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    }
+      virtual void PredictContribution(DMatrix* dmat,
+                                   std::vector<bst_float>* out_contribs,
+                                   const gbm::GBTreeModel& model,
+                                   unsigned ntree_limit = 0,
+                                   bool approximate = false,
+                                   int condition = 0,
+                                   unsigned condition_feature = 0) = 0;
     
-    /**
- * @brief Takes two+ Blobs, interprets last Blob as a selector and
- *  filter remaining Blobs accordingly with selector data (0 means that
- * the corresponding item has to be filtered, non-zero means that corresponding
- * item needs to stay).
- */
-template <typename Dtype>
-class FilterLayer : public Layer<Dtype> {
- public:
-  explicit FilterLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    // Like ASSERT_EXIT, but continues on to successive tests in the
-// test case, if any:
-# define EXPECT_EXIT(statement, predicate, regex) \
-    GTEST_DEATH_TEST_(statement, predicate, regex, GTEST_NONFATAL_FAILURE_)
-    
-    namespace testing {
-    }
-    
-    
-    {    const ParamGeneratorInterface<T>* const base_;
-    typename ContainerType::const_iterator iterator_;
-    // A cached value of *iterator_. We keep it here to allow access by
-    // pointer in the wrapping iterator's operator->().
-    // value_ needs to be mutable to be accessed in Current().
-    // Use of scoped_ptr helps manage cached value's lifetime,
-    // which is bound by the lifespan of the iterator itself.
-    mutable scoped_ptr<const T> value_;
-  };  // class ValuesInIteratorRangeGenerator::Iterator
-    
-    // Implementation #1 calculates the primes on-the-fly.
-class OnTheFlyPrimeTable : public PrimeTable {
- public:
-  virtual bool IsPrime(int n) const {
-    if (n <= 1) return false;
+    namespace xgboost {
+namespace common {
+using MemoryFixSizeBuffer = rabit::utils::MemoryFixSizeBuffer;
+using MemoryBufferStream = rabit::utils::MemoryBufferStream;
     }
     }
     
+        if (row_indices_.empty()) {  // edge case: empty instance set
+      // assign arbitrary address here, to bypass nullptr check
+      // (nullptr usually indicates a nonexistent rowset, but we want to
+      //  indicate a valid rowset that happens to have zero length and occupies
+      //  the whole instance set)
+      // this is okay, as BuildHist will compute (end-begin) as the set size
+      const size_t* begin = reinterpret_cast<size_t*>(20);
+      const size_t* end = begin;
+      elem_of_each_node_.emplace_back(Elem(begin, end, 0));
+      return;
+    }
     
-    {  return false;
+    void SparsePageWriter::PushWrite(std::shared_ptr<SparsePage>&& page) {
+  qworkers_[clock_ptr_].Push(std::move(page));
+  clock_ptr_ = (clock_ptr_ + 1) % workers_.size();
 }
-    
-    #include 'file.h'
-    
-      size_t numFrees()   const { return m_nfree; }
-  size_t numAllocs()  const { return m_nalloc; }
-  size_t bytesFree()  const { return m_bytesFree; }
-  size_t blocksFree() const { return m_freeRanges.size(); }
-    
-      auto dir = [&]{
-    if (auto const dumpDir = getenv('HHBBC_DUMP_DIR')) {
-      return fs::path(dumpDir);
-    } else {
-      char dirBuf[] = '/tmp/hhbbcXXXXXX';
-      auto const dtmpRet = mkdtemp(dirBuf);
-      if (!dtmpRet) {
-        throw std::runtime_error(
-          std::string('Failed to create temporary directory') +
-          strerror(errno));
-      }
-      return fs::path(dtmpRet);
-    }
-  }();
-  fs::create_directory(dir);
-    
-    void setPropForBase(ISS& env, Type ty) {
-  assert(couldBeInNonSerializedProp(env, env.state.mInstrState.base));
-    }
-    
-      assert(src.initialized);
-  assert(dst.locals.size() == src.locals.size());
-  assert(dst.iters.size() == src.iters.size());
-  assert(dst.clsRefSlots.size() == src.clsRefSlots.size());
-  assert(dst.stack.size() == src.stack.size());
-  assert(dst.fpiStack.size() == src.fpiStack.size());
-    
-    //////////////////////////////////////////////////////////////////////
-    
-      IterNextIndex getHelperIndex() {
-    return m_nextHelperIdx;
-  }
     
       /**
-   * Do not return function arguments for frames on the stack.
+   * @brief Stop the CAN client.
    */
-  BacktraceArgs& ignoreArgs(bool ignoreArgs = true) {
-    m_withArgNames = m_withArgNames && !ignoreArgs;
-    m_withArgValues = m_withArgValues && !ignoreArgs;
-    return *this;
+  virtual void Stop() = 0;
+    
+      if (!is_started_) {
+    AERROR << 'Esd can client has not been initiated! Please init first!';
+    return ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED;
+  }
+  for (size_t i = 0; i < frames.size() && i < MAX_CAN_SEND_FRAME_LEN; ++i) {
+    send_frames_[i].id = frames[i].id;
+    send_frames_[i].len = frames[i].len;
+    std::memcpy(send_frames_[i].data, frames[i].data, frames[i].len);
   }
     
+     private:
+  std::unique_ptr<std::thread> thread_;
+  bool is_running_ = false;
+  // CanClient, MessageManager pointer life is managed by outer program
+  CanClient *can_client_ = nullptr;
+  MessageManager<SensorType> *pt_manager_ = nullptr;
+  bool enable_log_ = false;
+  bool is_init_ = false;
     
-    {    switch (msg)
+    
     {
-    case WM_SIZE:
-        if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
-        {
-            ImGui_ImplDX11_InvalidateDeviceObjects();
-            CleanupRenderTarget();
-            g_pSwapChain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
-            CreateRenderTarget();
-            ImGui_ImplDX11_CreateDeviceObjects();
-        }
-        return 0;
-    case WM_SYSCOMMAND:
-        if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
-            return 0;
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-    }
-    return DefWindowProc(hWnd, msg, wParam, lParam);
-}
+    {
+    {}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+
     
-        // Upload texture to graphics system
-    g_FontTexture = NULL;
-    if (g_pd3dDevice->CreateTexture(width, height, 1, D3DUSAGE_DYNAMIC, D3DFMT_A8R8G8B8, D3DPOOL_DEFAULT, &g_FontTexture, NULL) < 0)
-        return false;
-    D3DLOCKED_RECT tex_locked_rect;
-    if (g_FontTexture->LockRect(0, &tex_locked_rect, NULL, 0) != D3D_OK)
-        return false;
-    for (int y = 0; y < height; y++)
-        memcpy((unsigned char *)tex_locked_rect.pBits + tex_locked_rect.Pitch * y, pixels + (width * bytes_per_pixel) * y, (width * bytes_per_pixel));
-    g_FontTexture->UnlockRect(0);
+     protected:
+  template <class T, bool need_check>
+  void AddRecvProtocolData();
     
-      // Compute DHT and SOS marker data sizes and start emitting DHT marker.
-  int num_histo = num_dc_histo + num_ac_histo;
-  histograms.resize(num_histo);
-  int total_count = 0;
-  for (size_t i = 0; i < histograms.size(); ++i) {
-    total_count += histograms[i].NumSymbols();
-  }
-  const size_t dht_marker_len =
-      2 + num_histo * (kJpegHuffmanMaxBitLength + 1) + total_count;
-  const size_t sos_marker_len = 6 + 2 * ncomps;
-  std::vector<uint8_t> data(dht_marker_len + sos_marker_len + 4);
-  size_t pos = 0;
-  data[pos++] = 0xff;
-  data[pos++] = 0xc4;
-  data[pos++] = static_cast<uint8_t>(dht_marker_len >> 8);
-  data[pos++] = dht_marker_len & 0xff;
-    
-    bool SetDepth(int p0, HuffmanTree *pool, uint8_t *depth, int max_depth) {
-  int stack[17];
-  int level = 0;
-  int p = p0;
-  assert(max_depth <= 16);
-  stack[0] = -1;
-  while (true) {
-    if (pool[p].index_left_ >= 0) {
-      level++;
-      if (level > max_depth) return false;
-      stack[level] = pool[p].index_right_or_value_;
-      p = pool[p].index_left_;
-      continue;
-    } else {
-      depth[pool[p].index_right_or_value_] = static_cast<uint8_t>(level);
-    }
-    while (level >= 0 && stack[level] == -1) level--;
-    if (level < 0) return true;
-    p = stack[level];
-    stack[level] = -1;
-  }
-}
-    
-    #include 'guetzli/gamma_correct.h'
-    
-    // Decodes the parsed jpeg coefficients into an RGB image.
-// There can be only either 1 or 3 image components, in either case, an RGB
-// output image will be generated.
-// Only YUV420 and YUV444 sampling factors are supported.
-// Vector will be empty if a decoding error occurred.
-std::vector<uint8_t> DecodeJpegToRGB(const JPEGData& jpg);
-    
-    void Quantize(coeff_t* v, int iquant) {
-  *v = (*v * iquant + kBias) >> kDCTBits;
-}
-    
-    #ifndef GUETZLI_JPEG_HUFFMAN_DECODE_H_
-#define GUETZLI_JPEG_HUFFMAN_DECODE_H_
-    
-    AbstractBtMessage::AbstractBtMessage(uint8_t id, const char* name)
-    : BtMessage(id),
-      invalidate_(false),
-      uploading_(false),
-      cuid_(0),
-      name_(name),
-      pieceStorage_(nullptr),
-      dispatcher_(nullptr),
-      messageFactory_(nullptr),
-      requestFactory_(nullptr),
-      peerConnection_(nullptr),
-      metadataGetMode_(false)
-{
-}
-    
-    class AbstractBtMessage : public BtMessage {
-private:
-  bool invalidate_;
-  bool uploading_;
-  cuid_t cuid_;
-    }
-    
-    void AbstractOptionHandler::setInitialOption(bool f)
-{
-  updateFlags(FLAG_INITIAL_OPTION, f);
-}
-    
-    #include 'common.h'
-    
-    #include 'Notifier.h'
-    
-    std::ostream& operator<<(std::ostream& o,
-                         const std::shared_ptr<AuthConfig>& authConfig)
-{
-  o << authConfig->getAuthText();
-  return o;
-}
-    
-    #endif // D_AUTH_CONFIG_H
+    #include 'modules/drivers/canbus/common/byte.h'
