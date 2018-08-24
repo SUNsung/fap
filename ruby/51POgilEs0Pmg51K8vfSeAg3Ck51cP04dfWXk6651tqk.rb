93735dd8359756f40f8198660a580440f904a7cd
SUNsung <weatherius@gmail.com>
2018-08-24 13:20:18 +0300

    Rewrite [51POgilEs0Pmg51K8vfSeAg3Ck51cP04dfWXk6651tqk.rb] File in: 2018-08-24 13:20:18

--- ruby/51POgilEs0Pmg51K8vfSeAg3Ck51cP04dfWXk6651tqk.rb
+++ ruby/51POgilEs0Pmg51K8vfSeAg3Ck51cP04dfWXk6651tqk.rb
@@ -1,78 +1,101 @@
 
         
-        def pre_pr(url)
-  url[-1, 1] == FORWARD_SLASH ? url : File.dirname(url)
+        describe GivenDailyLike do
+    
+    SiteSetting.refresh!
+uncat_id = SiteSetting.uncategorized_category_id
+uncat_id = -1 unless Numeric === uncat_id
+    
+          if staff.topic_id.nil?
+        creator = PostCreator.new(Discourse.system_user,
+          raw: I18n.t('staff_category_description'),
+          title: I18n.t('category.topic_prefix', category: staff.name),
+          category: staff.name,
+          archetype: Archetype.default
+        )
+        post = creator.create
+    
+    gem 'activemodel-serializers-xml', github: 'rails/activemodel-serializers-xml'
+    
+        if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
+  end
+    
+      # POST /resource/unlock
+  def create
+    self.resource = resource_class.send_unlock_instructions(resource_params)
+    yield resource if block_given?
+    
+    require 'rack/test'
+require 'action_controller/railtie'
+require 'active_record'
+require 'devise/rails/routes'
+require 'devise/rails/warden_compat'
+    
+          def is_navigational_format?
+        Devise.navigational_formats.include?(request_format)
+      end
+    
+              path
+        end
+      end
+    
+        unless env['devise.skip_trackable']
+      warden.session(scope)['last_request_at'] = Time.now.utc.to_i
+    end
+  end
 end
+
     
-    CONTENT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+          it 'left-justifies the result if specified with $ argument is negative' do
+        format('%1$*2$b', 10, -10).should == '1010      '
+        format('%1$*2$B', 10, -10).should == '1010      '
+        format('%1$*2$d', 112, -10).should == '112       '
+        format('%1$*2$i', 112, -10).should == '112       '
+        format('%1$*2$o', 87, -10).should == '127       '
+        format('%1$*2$u', 112, -10).should == '112       '
+        format('%1$*2$x', 196, -10).should == 'c4        '
+        format('%1$*2$X', 196, -10).should == 'C4        '
     
-              if options.fetch('skip_initial_build', false)
-            Jekyll.logger.warn 'Build Warning:', 'Skipping the initial build.' \
-                               ' This may result in an out-of-date site.'
-          else
-            build(site, options)
+    STDOUT.sync = true if ENV['CP_STDOUT_SYNC'] == 'TRUE'
+    
+      def prefixes
+    prefixes = ['/bin', '/usr/bin', '/usr/libexec', xcode_app_path]
+    prefixes << `brew --prefix`.strip unless `which brew`.strip.empty?
+    
+            def validate!
+          super
+          if @pod_name.nil? && !@wipe_all
+            # Security measure, to avoid removing the pod cache too agressively by mistake
+            help! 'You should either specify a pod name or use the --all flag'
           end
-    
-              after_install(new_blog_path, options)
         end
     
-          def matches(_)
-        false
-      end
+            self.description = <<-DESC
+          Shows the content of the pods cache as a YAML tree output, organized by pod.
+          If `NAME` is given, only the caches for that pod will be included in the output.
+        DESC
     
-          def day
-        @obj.date.strftime('%d')
-      end
-    
-          #
-      # The version constraint required to activate a given gem.
-      # Usually the gem version requirement is '> 0,' because any version
-      # will do. In the case of jekyll-docs, however, we require the exact
-      # same version as Jekyll.
-      #
-      # Returns a String version constraint in a parseable form for
-      # RubyGems.
-      def version_constraint(gem_name)
-        return '= #{Jekyll::VERSION}' if gem_name.to_s.eql?('jekyll-docs')
-        '> 0'
-      end
-    
-        describe '#download' do
-      it 'returns the p8 file' do
-        mock_client_response(:download_key) do
-          %{
------BEGIN PRIVATE KEY-----
-this is the encoded private key contents
------END PRIVATE KEY-----
-          }
+                respond_with(@order, default_template: :show, status: 201)
+          else
+            @order = Spree::Order.create!(user: current_api_user, store: current_store)
+            if Cart::Update.call(order: @order, params: order_params).success?
+              respond_with(@order, default_template: :show, status: 201)
+            else
+              invalid_resource!(@order)
+            end
+          end
         end
-        p8_string = key.download
-        expect(p8_string).to include('PRIVATE KEY')
-      end
-    end
     
-      # spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
-  spec.require_paths = Dir['*/lib']
+            def perform_payment_action(action, *args)
+          authorize! action, Payment
+          @payment.send('#{action}!', *args)
+          respond_with(@payment, default_template: :show)
+        end
     
-        # get the pos of css def at pos (search backwards)
-    def css_def_pos(css, pos, depth = -1)
-      to       = open_brace_pos(css, pos, depth)
-      prev_def = to - (css[0..to].reverse.index(RULE_CLOSE_BRACE_RE_REVERSE) || to) + 1
-      from     = prev_def + 1 + (css[prev_def + 1..-1] =~ %r(^\s*[^\s/]))
-      (from..to - 1)
-    end
-    
-      # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+            def index
+          @states = scope.ransack(params[:q]).result.includes(:country)
