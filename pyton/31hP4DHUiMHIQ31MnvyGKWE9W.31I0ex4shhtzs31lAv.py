65930c7609e0f57e1b33be79d2eed48ca3f23949
SUNsung <weatherius@gmail.com>
2018-08-25 19:18:54 +0300

    Rewrite [31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py] File in: 2018-08-25 19:18:54

--- pyton/31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py
+++ pyton/31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py
@@ -1,17 +1,114 @@
 
         
-        ## All tokens go to the parser (unless skip() is called in that rule)
-# on a particular 'channel'.  The parser tunes to a particular channel
-# so that whitespace etc... can go to the parser on a 'hidden' channel.
-DEFAULT_CHANNEL = 0
+            By default, it represents the actual environment.
+    All of the attributes can be overwritten though, which
+    is used by the test suite to simulate various scenarios.
     
-        def getUnexpectedToken(self):
-        return self.token
+        name = 'Digest HTTP auth'
+    auth_type = 'digest'
     
-        if version_str == 'HEAD':
-        return (sys.maxint, sys.maxint, sys.maxint, sys.maxint)
     
-            Terence implemented packed table initializers, because Java has a
-        size restriction on .class files and the lookup tables can grow
-        pretty large. The generated JavaLexer.java of the Java.g example
-        would be about 15MB with uncompressed array initializers.
+class PluginManager(object):
+    
+        if n == 1:
+        return '1 B'
+    
+    from utils import http, add_auth, HTTP_OK, MockEnvironment
+import httpie.input
+import httpie.cli
+    
+            def get_auth(self, username=None, password=None):
+            assert self.raw_auth is None
+            assert username is None
+            assert password is None
+            return basic_auth()
+    
+        def test_binary_stdin(self, httpbin):
+        with open(BIN_FILE_PATH, 'rb') as stdin:
+            env = MockEnvironment(
+                stdin=stdin,
+                stdin_isatty=False,
+                stdout_isatty=False
+            )
+            r = http('--print=B', 'POST', httpbin.url + '/post', env=env)
+            assert r == BIN_FILE_CONTENT
+    
+    
+class TestImplicitHTTPMethod:
+    def test_implicit_GET(self, httpbin):
+        r = http(httpbin.url + '/get')
+        assert HTTP_OK in r
+    
+    
+@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
+@pytest.mark.parametrize('filename', filenames)
+def test_rst_file_syntax(filename):
+    p = subprocess.Popen(
+        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
+    err = p.communicate()[1]
+    assert p.returncode == 0, err.decode('utf8')
+
+    
+            # syntax error
+        pytest.raises(ContentRangeError, parse, 'beers 100-199/*', 100)
+    
+        exc = ConnectionError('Connection aborted')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
+    assert ret == ExitStatus.ERROR
+    assert error_msg == (
+        'ConnectionError: '
+        'Connection aborted while doing GET request to URL: '
+        'http://www.google.com')
+    
+    print('Counting to five...')
+for number in count_to_five():
+    print(number, end=' ')
+    
+        def prepare(self):
+        print('Reporter Class is preparing to report the results')
+        time.sleep(0.1)
+    
+        @property
+    def data(self):
+        return self._data
+    
+        def __init__(self, radio):
+        self.radio = radio
+        self.stations = ['81.3', '89.1', '103.9']
+        self.pos = 0
+        self.name = 'FM'
+    
+    ingredients = 'spam eggs apple'
+line = '-' * 10
+    
+        def show_pet(self):
+        '''Creates and shows a pet using the abstract factory'''
+    
+    class TimeDisplay(object):
+    
+        def get_current_time_as_html_fragment(self):
+        current_time = self.time_provider.now()
+        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
+        return current_time_as_html_fragment
+'''
+    
+        '''
+    
+        def run_loop(self):
+        while self.blackboard.common_state['progress'] < 100:
+            for expert in self.blackboard.experts:
+                if expert.is_eager_to_contribute:
+                    expert.contribute()
+        return self.blackboard.common_state['contributions']
+    
+        def find_path(self, start, end, path=None):
+        path = path or []
+    
+        for shape in shapes:
+        shape.scale(2.5)
+        shape.draw()
