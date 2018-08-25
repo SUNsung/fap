6701772f8bf494f45bfaae165c9699cb5abb6a1b
SUNsung <weatherius@gmail.com>
2018-08-25 15:22:01 +0300

    Rewrite [35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py] File in: 2018-08-25 15:22:00

--- pyton/35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py
+++ pyton/35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py
@@ -1,240 +1,95 @@
 
         
-                scores = tf.log(scores_raw)
-        init_scores = np.log(np.array(scores_raw))
-        num_nodes = tf.constant([2, 3, 4], tf.int32)
+            def load(self):
+        try:
+            with open(self.path, 'rt') as f:
+                try:
+                    data = json.load(f)
+                except ValueError as e:
+                    raise ValueError(
+                        'Invalid %s JSON: %s [%s]' %
+                        (type(self).__name__, str(e), self.path)
+                    )
+                self.update(data)
+        except IOError as e:
+            if e.errno != errno.ENOENT:
+                raise
     
-      def _compute_m3n_loss(self, lengths, scores, gold):
-    '''Computes the M3N-style structured hinge loss for a batch.'''
-    # Perform hamming-loss-augmented inference.
-    gold_scores_b = tf.reduce_sum(scores * gold, axis=[1, 2])
-    hamming_loss_bxnxn = 1 - gold
-    scores_bxnxn = scores + hamming_loss_bxnxn
-    max_scores_b, _ = mst_ops.maximum_spanning_tree(
-        num_nodes=lengths, scores=scores_bxnxn, forest=self._attrs['forest'])
-    return max_scores_b - gold_scores_b
+        @property
+    def config(self):
+        if not hasattr(self, '_config'):
+            self._config = Config(directory=self.config_dir)
+            if self._config.is_new():
+                self._config.save()
+            else:
+                self._config.load()
+        return self._config
     
-          # Don't add a bias term if we're going to apply layer norm, since layer
-      # norm includes a bias already.
-      if index == 0 and self._layer_norm_hidden:
-        acts = self._layer_norm_hidden.normalize(acts)
-      else:
-        acts = tf.nn.bias_add(acts,
-                              self._component.get_variable('bias_%d' % index))
+        def iter_lines(self, chunk_size):
+        return ((line, b'\n') for line in self._orig.iter_lines(chunk_size))
     
-      def add_link(self,
-               source,
-               source_layer=None,
-               source_translator='identity',
-               name=None,
-               **kwargs):
-    '''Add a link using default naming and layers only.'''
-    if source_layer is None:
-      source_layer = source.default_source_layer()
-    if name is None:
-      name = source.spec.name
-    self.spec.linked_feature.add(
-        source_component=source.spec.name,
-        source_layer=source_layer,
-        name=name,
-        source_translator=source_translator,
-        **kwargs)
+        def __init__(self, groups, env=Environment(), **kwargs):
+        '''
+        :param groups: names of processor groups to be applied
+        :param env: Environment
+        :param kwargs: additional keyword arguments for processors
     
     
-def get_summary_writer(tensorboard_dir):
-  '''Creates a directory for writing summaries and returns a writer.'''
-  tf.logging.info('TensorBoard directory: %s', tensorboard_dir)
-  tf.logging.info('Deleting prior data if exists...')
-  try:
-    gfile.DeleteRecursively(tensorboard_dir)
-  except errors.OpError as err:
-    tf.logging.error('Directory did not exist? Error: %s', err)
-  tf.logging.info('Deleted! Creating the directory again...')
-  gfile.MakeDirs(tensorboard_dir)
-  tf.logging.info('Created! Instatiating SummaryWriter...')
-  summary_writer = tf.summary.FileWriter(tensorboard_dir)
-  return summary_writer
+with codecs.open(FILE_PATH, encoding='utf8') as f:
+    # Strip because we don't want new lines in the data so that we can
+    # easily count occurrences also when embedded in JSON (where the new
+    # line would be escaped).
+    FILE_CONTENT = f.read().strip()
+    
+            # invalid instance-length
+        pytest.raises(ContentRangeError, parse, 'bytes 100-199/199', 100)
+    
+        exc = ConnectionError('Connection aborted')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
+    assert ret == ExitStatus.ERROR
+    assert error_msg == (
+        'ConnectionError: '
+        'Connection aborted while doing GET request to URL: '
+        'http://www.google.com')
     
     
-def main(unused_argv):
-  tf.logging.set_verbosity(tf.logging.INFO)
+@pytest.mark.skip('unimplemented')
+def test_unset_host_header(httpbin_both):
+    r = http('GET', httpbin_both + '/headers')
+    assert 'Host' in r.json['headers']  # default Host present
     
-      # Build and write master_spec.
-  master_spec = spec_pb2.MasterSpec()
-  master_spec.component.extend([lookahead.spec, segmenter.spec])
-  logging.info('Constructed master spec: %s', str(master_spec))
-  with gfile.GFile(FLAGS.resource_path + '/master_spec', 'w') as f:
-    f.write(str(master_spec).encode('utf-8'))
+    print('Counting to two...')
+for number in count_to_two():
+    print(number, end=' ')
     
-    tf_proto_library_cc(
-    name = 'dictionary_proto',
-    srcs = ['dictionary.proto'],
-)
-    
-      def testBatchPathScoresAgree(self):
-    '''Ensures that path scores computed in the beam are same in the net.'''
-    all_path_scores, beam_path_scores = self.PathScores(
-        iterations=1, beam_size=130, max_steps=5, batch_size=22)
-    self.assertArrayNear(all_path_scores[0], beam_path_scores[0], 1e-6)
-    
-        if not has_level_handler(logger):
-        logger.addHandler(default_handler)
-    
-                if match is None:
-                continue
+        REGISTRY = {}
     
     
-def test_manual_context_binding(app):
-    @app.route('/')
-    def index():
-        return 'Hello %s!' % flask.request.args['name']
+def reverse_item(item):
+    print(item[::-1])
     
-        flask.request_started.connect(before_request_signal, app)
-    flask.request_finished.connect(after_request_signal, app)
+        def visit(self, node, *args, **kwargs):
+        meth = None
+        for cls in node.__class__.__mro__:
+            meth_name = 'visit_' + cls.__name__
+            meth = getattr(self, meth_name, None)
+            if meth:
+                break
     
-    outputs = Conv2DTranspose(filters=1,
-                          kernel_size=kernel_size,
-                          activation='sigmoid',
-                          padding='same',
-                          name='decoder_output')(x)
-    
-        # generate char sequences of length 'sequence_length' out of alphabet and store the next char as label (e.g. 'ab'->'c')
-    sequence_length = 2
-    sentences = [alphabet[i: i + sequence_length] for i in range(len(alphabet) - sequence_length)]
-    next_chars = [alphabet[i + sequence_length] for i in range(len(alphabet) - sequence_length)]
-    
-    print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
-    
-        for i in range(n_dense - 1):
-        model.add(Dense(dense_units, kernel_initializer=kernel_initializer))
-        model.add(Activation(activation))
-        model.add(dropout(dropout_rate))
-    
-        x_train = np.array(x[:int(len(x) * (1 - test_split))])
-    y_train = np.array(y[:int(len(x) * (1 - test_split))])
-    x_test = np.array(x[int(len(x) * (1 - test_split)):])
-    y_test = np.array(y[int(len(x) * (1 - test_split)):])
-    return (x_train, y_train), (x_test, y_test)
-
-    
-    from .cifar import load_batch
-from ..utils.data_utils import get_file
-from .. import backend as K
-import numpy as np
-import os
-    
-        # Parse and validate the field names.  Validation serves two purposes,
-    # generating informative error messages and preventing template injection attacks.
-    if isinstance(field_names, basestring):
-        field_names = field_names.replace(',', ' ').split() # names separated by whitespace and/or commas
-    field_names = tuple(map(str, field_names))
-    for name in (typename,) + field_names:
-        if not all(c.isalnum() or c=='_' for c in name):
-            raise ValueError('Type names and field names can only contain alphanumeric characters and underscores: %r' % name)
-        if _iskeyword(name):
-            raise ValueError('Type names and field names cannot be a keyword: %r' % name)
-        if name[0].isdigit():
-            raise ValueError('Type names and field names cannot start with a number: %r' % name)
-    seen_names = set()
-    for name in field_names:
-        if name.startswith('_'):
-            raise ValueError('Field names cannot start with an underscore: %r' % name)
-        if name in seen_names:
-            raise ValueError('Encountered duplicate field name: %r' % name)
-        seen_names.add(name)
-    
-            try:
-            result = self.fn(*self.args, **self.kwargs)
-        except BaseException:
-            e = sys.exc_info()[1]
-            self.future.set_exception(e)
-        else:
-            self.future.set_result(result)
+    *What does this example do?
+In this example queue.Queue is used to create the pool (wrapped in a
+custom ObjectPool object to use with the with statement), and it is
+populated with strings.
+As we can see, the first string object put in 'yam' is USED by the
+with statement. But because it is released back into the pool
+afterwards it is reused by the explicit call to sample_queue.get().
+Same thing happens with 'sam', when the ObjectPool created insided the
+function is deleted (by the GC) and the object is returned.
     
     
-def assert_python_ok(*args, **env_vars):
+class MidnightTimeProvider(object):
     '''
-    Assert that running the interpreter with `args` and optional environment
-    variables `env_vars` is ok and return a (return code, stdout, stderr) tuple.
+    Class implemented as hard-coded stub (in contrast to configurable stub).
     '''
-    return _assert_python(True, *args, **env_vars)
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    
-@YouCompleteMeInstance()
-def EventNotification_FileReadyToParse_NonDiagnostic_ConfirmExtraConf_test(
-    ycm ):
-    
-    
-GENERIC_RESPONSE = {
-  'clang': {
-    'has_support': True,
-    'version': 'Clang version'
-  },
-  'completer': {
-    'items': [
-      {
-        'key': 'key',
-        'value': 'value'
-      }
-    ],
-    'name': 'Completer name',
-    'servers': [
-      {
-        'address': '127.0.0.1',
-        'executable': '/path/to/executable',
-        'extras': [
-          {
-            'key': 'key',
-            'value': 'value'
-          }
-        ],
-        'is_running': True,
-        'logfiles': [
-          '/path/to/stdout/logfile',
-          '/path/to/stderr/logfile'
-        ],
-        'name': 'Server name',
-        'pid': 12345,
-        'port': 1234
-      }
-    ]
-  },
-  'extra_conf': {
-    'is_loaded': False,
-    'path': '/path/to/extra/conf'
-  },
-  'python': {
-    'executable': '/path/to/python/interpreter',
-    'version': 'Python version'
-  }
-}
-    
-      # Start by looking for a single character match
-  # and increase length until no match is found.
-  best = 0
-  length = 1
-  while True:
-    pattern = left_string[ -length: ]
-    found = right_string.find( pattern )
-    if found < 0:
-      return best
-    length += found
-    if left_string[ -length: ] == right_string[ :length ]:
-      best = length
-      length += 1
-
-    
-    
-  def Start( self ):
-    self._results = self._omni_completer.ComputeCandidates( self.request_data )
