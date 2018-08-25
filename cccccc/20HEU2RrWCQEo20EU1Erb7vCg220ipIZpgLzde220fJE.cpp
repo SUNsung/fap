1ae8d8502e1e823c844a42d17c935e0930fbf497
SUNsung <weatherius@gmail.com>
2018-08-26 00:22:28 +0300

    Rewrite [20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp] File in: 2018-08-26 00:22:28

--- cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
+++ cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
@@ -1,63 +1,157 @@
 
         
-          std::string toStyledString() const;
+        // Returns an EagerTensor of dimension [len(`tensors`)] containing
+// the `slice_dim`'th dimension of each tensor in `tensors`. In other words,
+// TFE_Py_TensorShapeSlice takes a slice of dimensions of tensors in
+// `tensors`. For example, if `tensors` contains tensors of with shapes
+// [1, 2, 3], [4, 5], [6, 7, 8, 9], TFE_Py_TensorShapeSlice called with
+// `slice_dim` equal to 1 will return [2, 5, 7].
+// On error, returns nullptr and sets python exception.
+// REQUIRES: `tensors` is a python list/tuple of EagerTensors
+// REQUIRES: `slice_dim` is non-negative and smaller than the rank of all
+//   tensors in `tensors`.
+PyObject* TFE_Py_TensorShapeSlice(PyObject* tensors, int slice_dim);
     
-      virtual void WriteHash(io::Printer* printer);
-  virtual void WriteEquals(io::Printer* printer);
-  virtual void WriteToString(io::Printer* printer);
+    REGISTER_OP('TwoFloatOutputs')
+    .Output('a: float32')
+    .Output('b: float32')
+    .SetShapeFn(shape_inference::UnknownShape);
     
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    {}  // namespace google
-#endif  // GOOGLE_PROTOBUF_COMPILER_JAVA_MESSAGE_H__
-
+    REGISTER_OP('Invalid')
+    .Attr('invalid attr: int32')  // invalid since the name has a space.
+    .Doc(R'doc(
+An op to test that invalid ops do not successfully generate invalid python code.
+)doc');
     
-      std::vector<rocksdb::ColumnFamilyDescriptor> families;
+    // Must be included first.
+#include 'tensorflow/python/lib/core/numpy.h'
     
-    namespace rocksdb {
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    #include 'tensorflow/core/framework/graph.pb.h'
+#include 'tensorflow/core/platform/types.h'
+    
+    #ifndef BITCOIN_QT_PLATFORMSTYLE_H
+#define BITCOIN_QT_PLATFORMSTYLE_H
+    
+    namespace
+{
     }
     
-    #include 'util/sync_point.h'
-#include 'util/sync_point_impl.h'
+    #include <script/script.h>
     
-      // Timestamp of oldest key
-  std::atomic<uint64_t> oldest_key_time_;
-    
-    class CompactionFilterFactoryJniCallback : public JniCallback, public CompactionFilterFactory {
- public:
-    CompactionFilterFactoryJniCallback(
-        JNIEnv* env, jobject jcompaction_filter_factory);
-    virtual std::unique_ptr<CompactionFilter> CreateCompactionFilter(
-      const CompactionFilter::Context& context);
-    virtual const char* Name() const;
+    static void secp256k1_hmac_sha256_initialize(secp256k1_hmac_sha256_t *hash, const unsigned char *key, size_t keylen) {
+    int n;
+    unsigned char rkey[64];
+    if (keylen <= 64) {
+        memcpy(rkey, key, keylen);
+        memset(rkey + keylen, 0, 64 - keylen);
+    } else {
+        secp256k1_sha256_t sha256;
+        secp256k1_sha256_initialize(&sha256);
+        secp256k1_sha256_write(&sha256, key, keylen);
+        secp256k1_sha256_finalize(&sha256, rkey);
+        memset(rkey + 32, 0, 32);
+    }
     }
     
-    struct ComparatorJniCallbackOptions {
-  // Use adaptive mutex, which spins in the user space before resorting
-  // to kernel. This could reduce context switch when the mutex is not
-  // heavily contended. However, if the mutex is hot, we could end up
-  // wasting spin time.
-  // Default: false
-  bool use_adaptive_mutex;
+        s_one[31] = 1;
+    /* Check against pubkey creation when the basepoint is the generator */
+    for (i = 0; i < 100; ++i) {
+        secp256k1_sha256_t sha;
+        unsigned char s_b32[32];
+        unsigned char output_ecdh[32];
+        unsigned char output_ser[32];
+        unsigned char point_ser[33];
+        size_t point_ser_len = sizeof(point_ser);
+        secp256k1_scalar s;
     }
     
-      std::mutex              mutex_;
-  std::condition_variable cv_;
-  // sync points that have been passed through
-  std::unordered_set<std::string> cleared_points_;
-  std::atomic<bool> enabled_;
-  int num_callbacks_running_ = 0;
+        /* Check NULLs for conversion */
+    CHECK(secp256k1_ecdsa_sign(both, &normal_sig, message, privkey, NULL, NULL) == 1);
+    ecount = 0;
+    CHECK(secp256k1_ecdsa_recoverable_signature_convert(both, NULL, &recsig) == 0);
+    CHECK(ecount == 1);
+    CHECK(secp256k1_ecdsa_recoverable_signature_convert(both, &normal_sig, NULL) == 0);
+    CHECK(ecount == 2);
+    CHECK(secp256k1_ecdsa_recoverable_signature_convert(both, &normal_sig, &recsig) == 1);
     
-      pthread_key_t k = TlsAlloc();
-  if (TLS_OUT_OF_INDEXES == k) {
-    return ENOMEM;
+    static bool CaseInsensitiveEqual(const std::string &s1, const std::string &s2)
+{
+    if (s1.size() != s2.size()) return false;
+    for (size_t i = 0; i < s1.size(); ++i) {
+        char c1 = s1[i];
+        if (c1 >= 'A' && c1 <= 'Z') c1 -= ('A' - 'a');
+        char c2 = s2[i];
+        if (c2 >= 'A' && c2 <= 'Z') c2 -= ('A' - 'a');
+        if (c1 != c2) return false;
+    }
+    return true;
+}
+    
+    BOOST_FIXTURE_TEST_SUITE(validation_block_tests, RegtestingSetup)
+    
+    const UniValue& find_value( const UniValue& obj, const std::string& name);
+    
+      /**
+   * @brief Send messages
+   * @param frames The messages to send.
+   * @param frame_num The amount of messages to send.
+   * @return The status of the sending action which is defined by
+   *         apollo::common::ErrorCode.
+   */
+  virtual apollo::common::ErrorCode Send(const std::vector<CanFrame> &frames,
+                                         int32_t *const frame_num) = 0;
+    
+      /**
+   * @brief Stop the fake CAN client.
+   */
+  void Stop() override;
+    
+    
+    {
+    {    if (ret < 0) {
+      AERROR << 'receive message failed, error code: ' << ret;
+      return ErrorCode::CAN_CLIENT_ERROR_BASE;
+    }
+    if (recv_frames_[i].can_dlc != CANBUS_MESSAGE_LENGTH) {
+      AERROR << 'recv_frames_[' << i
+             << '].can_dlc = ' << recv_frames_[i].can_dlc
+             << ', which is not equal to can message data length ('
+             << CANBUS_MESSAGE_LENGTH << ').';
+      return ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED;
+    }
+    cf.id = recv_frames_[i].can_id;
+    cf.len = recv_frames_[i].can_dlc;
+    std::memcpy(cf.data, recv_frames_[i].data, recv_frames_[i].can_dlc);
+    frames->push_back(cf);
   }
+  return ErrorCode::OK;
+}
     
-    void HistogramStat::Add(uint64_t value) {
-  // This function is designed to be lock free, as it's in the critical path
-  // of any operation. Each individual value is atomic and the order of updates
-  // by concurrent threads is tolerable.
-  const size_t index = bucketMapper.IndexForValue(value);
-  assert(index < num_buckets_);
-  buckets_[index].store(buckets_[index].load(std::memory_order_relaxed) + 1,
-                        std::memory_order_relaxed);
-    }
+    template <typename SensorType>
+ErrorCode MessageManager<SensorType>::GetSensorData(
+    SensorType *const sensor_data) {
+  if (sensor_data == nullptr) {
+    AERROR << 'Failed to get sensor_data due to nullptr.';
+    return ErrorCode::CANBUS_ERROR;
+  }
+  std::lock_guard<std::mutex> lock(sensor_data_mutex_);
+  sensor_data->CopyFrom(sensor_data_);
+  return ErrorCode::OK;
+}
+    
+    TEST(ByteTest, ByteToString) {
+  unsigned char value = 0x34;
+  EXPECT_EQ('34', Byte::byte_to_hex(value));
+  EXPECT_EQ('00110100', Byte::byte_to_binary(value));
+  uint32_t int_value = 0xE13A;
+  EXPECT_EQ('E13A', Byte::byte_to_hex(int_value));
+}
