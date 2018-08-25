9d73ad7fabb452bbf3ecd2f9d9b430007931d11a
SUNsung <weatherius@gmail.com>
2018-08-25 16:28:24 +0300

    Rewrite [11tZN9Wq9tK7U11TNhWjJlgSTk11tZAYIjZtfc11sFo.rb] File in: 2018-08-25 16:28:23

--- ruby/11tZN9Wq9tK7U11TNhWjJlgSTk11tZAYIjZtfc11sFo.rb
+++ ruby/11tZN9Wq9tK7U11TNhWjJlgSTk11tZAYIjZtfc11sFo.rb
@@ -1,109 +1,35 @@
 
         
-        module Jekyll
-  module Commands
-    class New < Command
-      class << self
-        def init_with_program(prog)
-          prog.command(:new) do |c|
-            c.syntax 'new PATH'
-            c.description 'Creates a new Jekyll site scaffold in PATH'
+          describe '#scenario_label' do
+    it 'creates a scenario label with the scenario name' do
+      expect(scenario_label(scenario)).to eq(
+        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Scene</span>'
+      )
+    end
     
-              @new_body.each do |line|
-            if !@livereload_added && line['<head']
-              line.gsub!(HEAD_TAG_REGEX) do |match|
-                %(#{match}#{template.result(binding)})
-              end
+    describe Utils do
+  describe '#unindent' do
+    it 'unindents to the level of the greatest consistant indention' do
+      expect(Utils.unindent(<<-MD)).to eq('Hello World')
+        Hello World
+      MD
     
-          def remember_me_is_active?(resource)
-        return false unless resource.respond_to?(:remember_me)
-        scope = Devise::Mapping.find_scope!(resource)
-        _, token, generated_at = cookies.signed[remember_key(resource, scope)]
-        resource.remember_me?(token, generated_at)
-      end
+      it 'raises ArgumentError if no block or proc is provided' do
+    lambda do
+      trace_var :$Kernel_trace_var_global
+    end.should raise_error(ArgumentError)
+  end
+end
+
     
-          def self.generate_helpers!(routes=nil)
-        routes ||= begin
-          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
-          Devise::URL_HELPERS.slice(*mappings)
+    Struct.new('Blog', :name, :web_url, :rss_url)
+blogs = []
+    
+      def append(*paths)
+    @paths = parse(*@paths, *paths)
+    self
+  end
+    
+              Formatter.error(string, label: 'Broken Link')
         end
-    
-          def merge(other)
-        dup.merge!(other)
       end
-    
-            while true
-          begin
-            if Platform.windows?
-              # Windows doesn't support non-blocking reads on
-              # file descriptors or pipes so we have to get
-              # a bit more creative.
-    
-                o.on('--provider PROVIDER', String,
-                 'The specific provider type for the box to remove') do |p|
-              options[:provider] = p
-            end
-    
-      def report_disk_usage
-    disk_space = disk_usage_readable(Cleanup.disk_cleanup_size)
-    if ARGV.dry_run?
-      ohai 'This operation would free approximately #{disk_space} of disk space.'
-    else
-      ohai 'This operation has freed approximately #{disk_space} of disk space.'
-    end
-  end
-    
-      get(/.+/) do
-    send_sinatra_file(request.path) {404}
-  end
-    
-        def initialize(tag_name, markup, tokens)
-      @by = nil
-      @source = nil
-      @title = nil
-      if markup =~ FullCiteWithTitle
-        @by = $1
-        @source = $2 + $3
-        @title = $4.titlecase.strip
-      elsif markup =~ FullCite
-        @by = $1
-        @source = $2 + $3
-      elsif markup =~ AuthorTitle
-        @by = $1
-        @title = $2.titlecase.strip
-      elsif markup =~ Author
-        @by = $1
-      end
-      super
-    end
-    
-        def names_for(klass)
-      @attachments[klass].keys
-    end
-    
-        template '/engineyard/bin/sidekiq' do
-      owner 'root'
-      group 'root' 
-      mode 0755
-      source 'sidekiq.erb' 
-    end
-    
-    module Sidekiq
-  module Generators # :nodoc:
-    class WorkerGenerator < ::Rails::Generators::NamedBase # :nodoc:
-      desc 'This generator creates a Sidekiq Worker in app/workers and a corresponding test'
-    
-        module ActionMailer
-      def sidekiq_delay(options={})
-        Proxy.new(DelayedMailer, self, options)
-      end
-      def sidekiq_delay_for(interval, options={})
-        Proxy.new(DelayedMailer, self, options.merge('at' => Time.now.to_f + interval.to_f))
-      end
-      def sidekiq_delay_until(timestamp, options={})
-        Proxy.new(DelayedMailer, self, options.merge('at' => timestamp.to_f))
-      end
-      alias_method :delay, :sidekiq_delay
-      alias_method :delay_for, :sidekiq_delay_for
-      alias_method :delay_until, :sidekiq_delay_until
-    end
