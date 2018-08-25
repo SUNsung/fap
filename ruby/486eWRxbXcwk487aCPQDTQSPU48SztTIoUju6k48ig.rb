98e0b6858dad66953f9cf3bb636987cc19985f44
SUNsung <weatherius@gmail.com>
2018-08-25 14:57:35 +0300

    Rewrite [486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb] File in: 2018-08-25 14:57:34

--- ruby/486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb
+++ ruby/486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb
@@ -1,56 +1,224 @@
 
         
-                def test_encoded_password
-          password = 'am@z1ng_p@ssw0rd#!'
-          encoded_password = URI.encode_www_form_component(password)
-          spec = resolve 'abstract://foo:#{encoded_password}@localhost/bar'
-          assert_equal password, spec['password']
-        end
-    
-        line_item = invoice.line_items.build invoice_id: invoice.id + 1
-    assert_equal invoice.id, line_item.invoice_id
-    
-        def connect
-      @connected = true
+            it 'opens the dry run modal even when clicking on the refresh icon' do
+      visit edit_agent_path(agent)
+      find('.agent-dry-run-button span.glyphicon').click
+      expect(page).to have_text('Event to send (Optional)')
     end
     
-      test 'unsubscribe from all' do
-    run_in_eventmachine do
-      setup_connection
+      it 'requires a URL or file uplaod' do
+    visit new_scenario_imports_path
+    click_on 'Start Import'
+    expect(page).to have_text('Please provide either a Scenario JSON File or a Public Scenario URL.')
+  end
     
-          Dir.chdir(code_path) do
-        code = file.read
-        @filetype = file.extname.sub('.','') if @filetype.nil?
-        title = @title ? '#{@title} (#{file.basename})' : file.basename
-        url = '/#{code_dir}/#{@file}'
-        source = '<figure class='code'><figcaption><span>#{title}</span> <a href='#{url}'>download</a></figcaption>\n'
-        source += '#{HighlightCode::highlight(code, @filetype)}</figure>'
-        TemplateWrapper::safe_wrap(source)
+        context 'running workers' do
+      before do
+        AgentRunner.class_variable_set(:@@agents, [HuginnScheduler, DelayedJobWorker])
+        stub.instance_of(HuginnScheduler).setup
+        stub.instance_of(DelayedJobWorker).setup
+      end
+    
+        it 'should work with nested hashes' do
+      @agent.options['very'] = {'nested' => '$.value'}
+      LiquidMigrator.convert_all_agent_options(@agent)
+      expect(@agent.reload.options).to eq({'auth_token' => 'token', 'color' => 'yellow', 'very' => {'nested' => '{{value}}'}, 'notify' => false, 'room_name' => 'test', 'username' => '{{username}}', 'message' => '{{message}}'})
+    end
+    
+    describe ConvertWebsiteAgentTemplateForMerge do
+  let :old_extract do
+    {
+      'url' => { 'css' => '#comic img', 'value' => '@src' },
+      'title' => { 'css' => '#comic img', 'value' => '@alt' },
+      'hovertext' => { 'css' => '#comic img', 'value' => '@title' }
+    }
+  end
+    
+        it 'updates Agents' last_error_log_at when an error is logged' do
+      AgentLog.log_for_agent(agents(:jane_website_agent), 'some message', :level => 3, :outbound_event => events(:jane_website_agent_event))
+      expect(agents(:jane_website_agent).reload.last_error_log_at).to be_nil
+    
+        def current_url
+      context[:url]
+    end
+    
+        private
+    
+        def initialize(content)
+      @content = content
+      @html = document? ? parse_as_document : parse_as_fragment
+    end
+    
+        def pipeline
+      @pipeline ||= ::HTML::Pipeline.new(self.class.filters).tap do |pipeline|
+        pipeline.instrumentation_service = Docs
+      end
+    end
+    
+        module MultipleBaseUrls
+      def self.included(base)
+        base.extend ClassMethods
+      end
+    
+            css('pre').each do |node|
+          node.content = node.content.strip
+    
+            css('button.verbose', 'button.verbose + .l-verbose-section', 'a[id=top]', 'a[href='#top']', '.sidebar', 'br').remove
+    
+            doc
+      end
+    
+        s = mock('seed')
+    s.should_receive(:to_int).and_return 0
+    srand(s)
+  end
+    
+        after do
+      rm_r @tmp_file
+    end
+    
+    
+  #
+  # Payload types were copied from xCAT-server source code (IPMI.pm)
+  #
+  RMCP_ERRORS = {
+    1 => 'Insufficient resources to create new session (wait for existing sessions to timeout)',
+    2 => 'Invalid Session ID', #this shouldn't occur...
+    3 => 'Invalid payload type',#shouldn't occur..
+    4 => 'Invalid authentication algorithm', #if this happens, we need to enhance our mechanism for detecting supported auth algorithms
+    5 => 'Invalid integrity algorithm', #same as above
+    6 => 'No matching authentication payload',
+    7 => 'No matching integrity payload',
+    8 => 'Inactive Session ID', #this suggests the session was timed out while trying to negotiate, shouldn't happen
+    9 => 'Invalid role',
+    0xa => 'Unauthorised role or privilege level requested',
+    0xb => 'Insufficient resources to create a session at the requested role',
+    0xc => 'Invalid username length',
+    0xd => 'Unauthorized name',
+    0xe => 'Unauthorized GUID',
+    0xf => 'Invalid integrity check value',
+    0x10 => 'Invalid confidentiality algorithm',
+    0x11 => 'No cipher suite match with proposed security algorithms',
+    0x12 => 'Illegal or unrecognized parameter', #have never observed this, would most likely mean a bug in xCAT or IPMI device
+  }
+    
+              # Decodes a Rex::Proto::Kerberos::Model::EncKdcResponse
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @raise [RuntimeError] if decoding doesn't succeed
+          def decode_asn1(input)
+            input.value[0].value.each do |val|
+              case val.tag
+              when 0
+                self.key = decode_key(val)
+              when 1
+                self.last_req = decode_last_req(val)
+              when 2
+                self.nonce = decode_nonce(val)
+              when 3
+                self.key_expiration = decode_key_expiration(val)
+              when 4
+                self.flags = decode_flags(val)
+              when 5
+                self.auth_time = decode_auth_time(val)
+              when 6
+                self.start_time = decode_start_time(val)
+              when 7
+                self.end_time = decode_end_time(val)
+              when 8
+                self.renew_till = decode_renew_till(val)
+              when 9
+                self.srealm = decode_srealm(val)
+              when 10
+                self.sname = decode_sname(val)
+              else
+                raise ::RuntimeError, 'Failed to decode ENC-KDC-RESPONSE SEQUENCE'
+              end
+            end
+          end
+    
+        require 'lib/sass'
+    
+      # An environment that can write to in-scope global variables, but doesn't
+  # create new variables in the global scope. Useful for top-level control
+  # directives.
+  class SemiGlobalEnvironment < Environment
+    def try_set_var(name, value)
+      @vars ||= {}
+      if @vars.include?(name)
+        @vars[name] = value
+        true
+      elsif @parent
+        @parent.try_set_var(name, value)
+      else
+        false
       end
     end
   end
+end
+
     
-    module RuboCop
-  module Cop
-    module Style
-      # This cop check for uses of Object#freeze on immutable objects.
-      #
-      # @example
-      #   # bad
-      #   CONST = 1.freeze
-      #
-      #   # good
-      #   CONST = 1
-      class RedundantFreeze < Cop
-        include FrozenStringLiteral
+    PROFILE_ERB_TEMPLATE = <<-EOS
+(version 1)
+(debug allow)
     
-          # The body of the method definition.
+      module Generator
+    autoload :Acknowledgements,        'cocoapods/generator/acknowledgements'
+    autoload :Markdown,                'cocoapods/generator/acknowledgements/markdown'
+    autoload :Plist,                   'cocoapods/generator/acknowledgements/plist'
+    autoload :BridgeSupport,           'cocoapods/generator/bridge_support'
+    autoload :Constant,                'cocoapods/generator/constant'
+    autoload :CopyResourcesScript,     'cocoapods/generator/copy_resources_script'
+    autoload :DummySource,             'cocoapods/generator/dummy_source'
+    autoload :EmbedFrameworksScript,   'cocoapods/generator/embed_frameworks_script'
+    autoload :Header,                  'cocoapods/generator/header'
+    autoload :InfoPlistFile,           'cocoapods/generator/info_plist_file'
+    autoload :ModuleMap,               'cocoapods/generator/module_map'
+    autoload :PrefixHeader,            'cocoapods/generator/prefix_header'
+    autoload :UmbrellaHeader,          'cocoapods/generator/umbrella_header'
+    autoload :AppTargetHelper,         'cocoapods/generator/app_target_helper'
+  end
+    
+        #-------------------------------------------------------------------------#
+    
+            def run
+          UI.puts('$CACHE_ROOT: #{@cache.root}') if @short_output
+          if @pod_name.nil? # Print all
+            @cache.cache_descriptors_per_pod.each do |pod_name, cache_descriptors|
+              print_pod_cache_infos(pod_name, cache_descriptors)
+            end
+          else # Print only for the requested pod
+            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
+            if cache_descriptors.nil?
+              UI.notice('No cache for pod named #{@pod_name} found')
+            else
+              print_pod_cache_infos(@pod_name, cache_descriptors)
+            end
+          end
+        end
+    
+            def left_whole_line_range(loc_begin)
+          if range_by_whole_lines(loc_begin).source.strip == '{'
+            range_by_whole_lines(loc_begin, include_final_newline: true)
+          else
+            loc_begin
+          end
+        end
+    }
+    
+        # Run main task(RSpec or RuboCop).
+    def run_main_task
+      if master? || !test? || jruby?
+        sh!('bundle exec rake #{ENV['TASK']}')
+      else
+        sh!('bundle exec rake parallel:#{ENV['TASK']}')
+      end
+    end
+    
+          # Checks whether this node body is a void context.
+      # Always `true` for `for`.
       #
-      # @note this can be either a `begin` node, if the method body contains
-      #       multiple expressions, or any other node, if it contains a single
-      #       expression.
-      #
-      # @return [Node] the body of the method definition
-      def body
-        node_parts[0]
+      # @return [true] whether the `for` node body is a void context
+      def void_context?
+        true
       end
