217654f23ce5c50ccf284c15a75e7872035721dd
SUNsung <weatherius@gmail.com>
2018-08-26 06:13:24 +0300

    Rewrite [14blTeQcPYeAE147cd3tHBDa9o14IFKNake5W4Y14Lj6.rb] File in: 2018-08-26 06:13:23

--- ruby/14blTeQcPYeAE147cd3tHBDa9o14IFKNake5W4Y14Lj6.rb
+++ ruby/14blTeQcPYeAE147cd3tHBDa9o14IFKNake5W4Y14Lj6.rb
@@ -1,23 +1,97 @@
 
         
-              module ClassMethods
-        def inherited(subclass)
-          subclass.log_levels = subclass.superclass.log_levels.dup
+            if version.present?
+      doc = doc.versions.find { |klass| klass.version == version || klass.version_slug == version }
+      raise DocNotFound.new(%(could not find version '#{version}' for doc '#{name}'), name) unless doc
+    elsif version != false
+      doc = doc.versions.first
+    end
+    
+        def length
+      @entries.length
+    end
+    
+        def xpath(*args)
+      doc.xpath(*args)
+    end
+    
+        def insert(index, *names)
+      @filters.insert assert_index(index), *filter_const(names)
+    end
+    
+        def as_json
+      @pages
+    end
+    
+        def url
+      @url ||= URL.parse request.base_url
+    end
+    
+            css('a.is-button > h3').each do |node|
+          node.parent.content = node.content
         end
     
-        private
+      def set_filters
+    @filters = current_account.custom_filters
+  end
     
-        # Converts a script node into a corresponding string interpolation
-    # expression.
-    #
-    # @param node_or_interp [Sass::Script::Tree::Node]
-    # @return [Sass::Script::Tree::StringInterpolation]
-    def to_string_interpolation(node_or_interp)
-      unless node_or_interp.is_a?(Interpolation)
-        node = node_or_interp
-        return string_literal(node.value.to_s) if node.is_a?(Literal)
-        if node.is_a?(StringInterpolation)
-          return concat(string_literal(node.quote), concat(node, string_literal(node.quote)))
-        end
-        return StringInterpolation.new(string_literal(''), node, string_literal(''))
+      def set_size
+    case params[:id]
+    when 'featured'
+      @account.pinned_statuses.count
+    else
+      raise ActiveRecord::NotFound
+    end
+  end
+    
+          @account_moderation_note = current_account.account_moderation_notes.new(resource_params)
+    
+      def process_push_request
+    case hub_mode
+    when 'subscribe'
+      Pubsubhubbub::SubscribeService.new.call(account_from_topic, hub_callback, hub_secret, hub_lease_seconds, verified_domain)
+    when 'unsubscribe'
+      Pubsubhubbub::UnsubscribeService.new.call(account_from_topic, hub_callback)
+    else
+      ['Unknown mode: #{hub_mode}', 422]
+    end
+  end
+    
+    class Api::SubscriptionsController < Api::BaseController
+  before_action :set_account
+  respond_to :txt
+    
+        it 'corrects one hash parameter with braces and whitespace' do
+      corrected = autocorrect_source(['where(  ',
+                                      ' { x: 1 }   )'].join('\n'))
+      expect(corrected).to eq(['where(  ',
+                               ' x: 1   )'].join('\n'))
+    end
+    
+      include_examples 'multiline literal brace layout' do
+    let(:open) { '[' }
+    let(:close) { ']' }
+  end
+    
+    shared_examples_for 'multiline literal brace layout method argument' do
+  include MultilineLiteralBraceHelper
+    
+    module RuboCop
+  # Common methods and behaviors for dealing with paths.
+  module PathUtil
+    module_function
+    
+          # Calls the given block for each `when` node in the `case` statement.
+      # If no block is given, an `Enumerator` is returned.
+      #
+      # @return [self] if a block is given
+      # @return [Enumerator] if no block is given
+      def each_when
+        return when_branches.to_enum(__method__) unless block_given?
+    
+          # Returns the collection the `for` loop is iterating over.
+      #
+      # @return [Node] The collection the `for` loop is iterating over
+      def collection
+        node_parts[1]
       end
