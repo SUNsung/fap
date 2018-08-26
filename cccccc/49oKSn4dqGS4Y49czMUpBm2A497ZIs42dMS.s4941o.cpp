cb3744baa7618292ce06686ea397ee468e6c2cde
SUNsung <weatherius@gmail.com>
2018-08-27 01:51:20 +0300

    Rewrite [49oKSn4dqGS4Y49czMUpBm2A497ZIs42dMS.s4941o.cpp] File in: 2018-08-27 01:51:19

--- cccccc/49oKSn4dqGS4Y49czMUpBm2A497ZIs42dMS.s4941o.cpp
+++ cccccc/49oKSn4dqGS4Y49czMUpBm2A497ZIs42dMS.s4941o.cpp
@@ -1,231 +1,262 @@
 
-    {}  // namespace atom
-    
-    namespace atom {
+        
+        void ProtoFromShapeHandle(tensorflow::shape_inference::ShapeHandle s,
+                          tensorflow::shape_inference::InferenceContext* c,
+                          TensorShapeProto* out) {
+  if (c->RankKnown(s)) {
+    const int32 rank = c->Rank(s);
+    for (int i = 0; i < rank; ++i) {
+      shape_inference::DimensionHandle d = c->Dim(s, i);
+      auto* out_dim = out->add_dim();
+      if (c->ValueKnown(d)) {
+        out_dim->set_size(c->Value(d));
+      } else {
+        out_dim->set_size(-1);
+      }
     }
-    
-      // Sent when the applications in the NTP app launcher have been reordered.
-  // The details, if not NoDetails, is the std::string ID of the extension that
-  // was moved.
-  NOTIFICATION_EXTENSION_LAUNCHER_REORDERED,
-    
-    void TtsPlatformImpl::set_error(const std::string& error) {
-  error_ = error;
-}
-    
-    namespace chrome {
-    }
-    
-    // filenames
-extern const base::FilePath::CharType kCacheDirname[];
-extern const base::FilePath::CharType kChannelIDFilename[];
-extern const base::FilePath::CharType kCookieFilename[];
-extern const base::FilePath::CharType kCRLSetFilename[];
-extern const base::FilePath::CharType kCustomDictionaryFileName[];
-extern const base::FilePath::CharType kExtensionActivityLogFilename[];
-extern const base::FilePath::CharType kExtensionsCookieFilename[];
-extern const base::FilePath::CharType kFirstRunSentinel[];
-extern const base::FilePath::CharType kGCMStoreDirname[];
-extern const base::FilePath::CharType kLocalStateFilename[];
-extern const base::FilePath::CharType kLocalStorePoolName[];
-extern const base::FilePath::CharType kMediaCacheDirname[];
-extern const base::FilePath::CharType kNetworkPersistentStateFilename[];
-extern const base::FilePath::CharType kOfflinePageArchviesDirname[];
-extern const base::FilePath::CharType kOfflinePageMetadataDirname[];
-extern const base::FilePath::CharType kPreferencesFilename[];
-extern const base::FilePath::CharType kProtectedPreferencesFilenameDeprecated[];
-extern const base::FilePath::CharType kReadmeFilename[];
-extern const base::FilePath::CharType kResetPromptMementoFilename[];
-extern const base::FilePath::CharType kSafeBrowsingBaseFilename[];
-extern const base::FilePath::CharType kSecurePreferencesFilename[];
-extern const base::FilePath::CharType kServiceStateFileName[];
-extern const base::FilePath::CharType kSingletonCookieFilename[];
-extern const base::FilePath::CharType kSingletonLockFilename[];
-extern const base::FilePath::CharType kSingletonSocketFilename[];
-extern const base::FilePath::CharType kSupervisedUserSettingsFilename[];
-extern const base::FilePath::CharType kThemePackFilename[];
-extern const base::FilePath::CharType kThemePackMaterialDesignFilename[];
-extern const base::FilePath::CharType kWebAppDirname[];
-    
-    // Convert color to RGB hex value like '#ABCDEF'
-std::string ToRGBHex(SkColor color);
-    
-    
-    {}  // namespace nwapi
-    
-       void Call(const std::string& method,
-                    const base::ListValue& arguments) override;
-   void CallSync(const std::string& method,
-                        const base::ListValue& arguments,
-                        base::ListValue* result) override;
-    
-    
-#ifndef CONTENT_NW_SRC_API_EVENT_EVENT_H_
-#define CONTENT_NW_SRC_API_EVENT_EVENT_H_
-    
-    #include 'ui/base/models/simple_menu_model.h'
-    
-    
-    {  DECLARE_EXTENSION_FUNCTION('nw.Obj.create', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwObjCreateFunction);
-};
-    
-    HINSTANCE g_hInst = NULL;
-D3D_DRIVER_TYPE g_driverType = D3D_DRIVER_TYPE_NULL;
-D3D_FEATURE_LEVEL g_featureLevel = D3D_FEATURE_LEVEL_11_0;
-ID3D11Device* g_pd3dDevice = NULL;
-ID3D11DeviceContext* g_pImmediateContext = NULL;
-IDXGISwapChain* g_pSwapChain = NULL;
-    
-    
-    {            ptr[tid] = partial = partial + ptr[tid + 16];
-            ptr[tid] = partial = partial + ptr[tid + 8];
-            ptr[tid] = partial = partial + ptr[tid + 4];
-            ptr[tid] = partial = partial + ptr[tid + 2];
-            ptr[tid] = partial = partial + ptr[tid + 1];
-        }
-    
-    #ifdef HAVE_CLAMDFFT
-    
-    
-    {  WorkloadStats (const WorkloadStats&) = delete;
-  WorkloadStats& operator=(const WorkloadStats&) = delete;
-};
-    
-        HHVM_FE(fb_setprofile);
-    HHVM_FE(xhprof_frame_begin);
-    HHVM_FE(xhprof_frame_end);
-    HHVM_FE(xhprof_enable);
-    HHVM_FE(xhprof_disable);
-    HHVM_FE(xhprof_network_enable);
-    HHVM_FE(xhprof_network_disable);
-    HHVM_FE(xhprof_sample_enable);
-    HHVM_FE(xhprof_sample_disable);
-    
-      /**
-   * Query if a task is finished. This is non-blocking and can be called as
-   * many times as desired.
-   */
-  static int64_t TaskStatus(const Resource& task);
-    
-    struct Block;
-struct SSATmp;
-    
-    /*
- * If we have non-conservative scanners, we must treat all unknown
- * type-index allocations in the heap as roots. Why? The generated
- * scanners will only report a pointer if it knows the pointer can point
- * to an object on the request heap. It does this by tracking all types
- * which are allocated via the allocation functions via the type-index
- * mechanism. If an allocation has an unknown type-index, then by definition
- * we don't know which type it contains, and therefore the auto generated
- * scanners will never report a pointer to such a type.
- *
- * The only good way to solve this is to treat such allocations as roots
- * and conservative scan them. If we're conservative scanning everything,
- * we need to take no special action, as the above problem only applies to
- * auto generated scanners.
- */
-    
-            // get some additional information when doing sequence training
-        // TODO: This should not need to be called in case of wasDataRead == false, since in that case, returned values are invalid.
-        if ((criterionNode != nullptr) && (criterionNode->OperationName() == L'SequenceWithSoftmax'))
-        {
-            auto node = dynamic_pointer_cast<SequenceWithSoftmaxNode<ElemType>>(criterionNode);
-            auto latticeinput = node->getLatticePtr();
-            auto uids = node->getuidprt();
-            auto boundaries = node->getboundaryprt();
-            auto extrauttmap = node->getextrauttmap();
-    }
-    
-            // inject additional items into the source code
-        // We support two ways of specifying the network in BrainScript:
-        //  - BrainScriptNetworkBuilder = ( any BS expression that evaluates to a ComputationNetwork )
-        //  - BrainScriptNetworkBuilder = { constructor parameters for a ComputationNetwork }
-        // For back-compat, [ ] is allowed and means the same as { }
-        if (sourceOfNetwork[0] == '{' || sourceOfNetwork[0] == '[') // if { } form then we turn it into ComputationNetwork by constructing a ComputationNetwork from it
-            sourceOfNetwork = L'new ComputationNetwork ' + sourceOfNetwork;
-        let sourceOfBS = msra::strfun::wstrprintf(L'include \'cntk.core.bs\'\n' // include our core lib. Note: Using lowercase here to match the Linux name of the CNTK exe.
-            L'deviceId = %d\n'            // deviceId as passed in
-            L'traceLevel = %d\n'
-            L'precision = '%ls'\n'        // 'float' or 'double'
-            L'network = %ls',             // source code of expression that evaluates to a ComputationNetwork
-            (int)deviceId, traceLevel, ElemTypeName<ElemType>(), sourceOfNetwork.c_str());
-        let expr = BS::ParseConfigDictFromString(sourceOfBS, L'BrainScriptNetworkBuilder', move(includePaths));
-    }
-    
-    
-    {    // SaveMapping - save a map into the file
-    // saveId - name of the section to save into (section:subsection format)
-    // labelMapping - map we are saving to the file
-    virtual void SaveMapping(std::wstring saveId, const std::map<LabelIdType, LabelType>& labelMapping);
-    virtual bool SupportMultiUtterances() const 
-    {
-        return false;
-    };
-};
-    
-    
-    {
-    {
-    {    // please add anything else you might need HERE
-};
-};
-};
-
-    
-    void SimpleCSRSource::CopyFrom(DMatrix* src) {
-  this->Clear();
-  this->info = src->Info();
-  auto iter = src->RowIterator();
-  iter->BeforeFirst();
-  while (iter->Next()) {
-    const auto &batch = iter->Value();
-    page_.Push(batch);
+  } else {
+    out->set_unknown_rank(true);
   }
 }
     
-    namespace xgboost {
-namespace tree {
-// List of files that will be force linked in static links.
-DMLC_REGISTRY_LINK_TAG(updater_colmaker);
-DMLC_REGISTRY_LINK_TAG(updater_skmaker);
-DMLC_REGISTRY_LINK_TAG(updater_refresh);
-DMLC_REGISTRY_LINK_TAG(updater_prune);
-DMLC_REGISTRY_LINK_TAG(updater_fast_hist);
-DMLC_REGISTRY_LINK_TAG(updater_histmaker);
-DMLC_REGISTRY_LINK_TAG(updater_sync);
-#ifdef XGBOOST_USE_CUDA
-DMLC_REGISTRY_LINK_TAG(updater_gpu);
-DMLC_REGISTRY_LINK_TAG(updater_gpu_hist);
-#endif
-}  // namespace tree
-}  // namespace xgboost
-
+    #include 'tensorflow/core/platform/env.h'
+#include 'tensorflow/core/platform/null_file_system.h'
     
-    SEXP XGDMatrixNumRow_R(SEXP handle) {
-  bst_ulong nrow;
-  R_API_BEGIN();
-  CHECK_CALL(XGDMatrixNumRow(R_ExternalPtrAddr(handle), &nrow));
-  R_API_END();
-  return ScalarInteger(static_cast<int>(nrow));
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    void CostAnalyzer::PreprocessCosts() {
+  for (int i = 0; i < op_perf_.op_performance_size(); i++) {
+    OpPerformance* perf = op_perf_.mutable_op_performance(i);
+    const OpPerformance& analytical = op_perf_analytical_.op_performance(i);
+    perf->set_compute_time(analytical.compute_time());
+    perf->set_memory_time(analytical.memory_time());
+    double measured_cost = perf->compute_cost();
+    }
+    }
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    // Returns the id number of the bfloat16 numpy type.
+int Bfloat16NumpyType();
+    
+    // Creates a tensor in 'ret' from the input Ndarray.
+Status NdarrayToTensor(PyObject* obj, Tensor* ret);
+    
+    Status ArrayFromMemory(int dim_size, npy_intp* dims, void* data, DataType dtype,
+                       std::function<void()> destructor, PyObject** result) {
+  int size = 1;
+  for (int i = 0; i < dim_size; ++i) {
+    size *= dims[i];
+  }
+  if (dtype == DT_STRING || dtype == DT_RESOURCE || size == 0) {
+    return errors::FailedPrecondition(
+        'Cannot convert strings, resources, or empty Tensors.');
+  }
+    }
+    
+    #ifndef TENSORFLOW_PYTHON_LIB_CORE_PY_FUNC_H_
+#define TENSORFLOW_PYTHON_LIB_CORE_PY_FUNC_H_
+    
+    namespace tensorflow {
+    }
+    
+    void PyRecordReader::Close() {
+  delete reader_;
+  delete file_;
+  file_ = nullptr;
+  reader_ = nullptr;
 }
     
-      for (auto alphabet_size : test_cases) {
-    for (int i = 0; i < repetitions; i++) {
-      std::vector<int> input(num_elements);
-      std::generate(input.begin(), input.end(),
-        [=]() { return rand() % alphabet_size; });
-      CompressedBufferWriter cbw(alphabet_size);
-    }
+    
+    {}  // namespace atom
+    
+    bool UvTaskRunner::PostNonNestableDelayedTask(const base::Location& from_here,
+                                              base::OnceClosure task,
+                                              base::TimeDelta delay) {
+  return PostDelayedTask(from_here, std::move(task), delay);
+}
+    
+    // static
+void AutoUpdater::BuildPrototype(v8::Isolate* isolate,
+                                 v8::Local<v8::FunctionTemplate> prototype) {
+  prototype->SetClassName(mate::StringToV8(isolate, 'AutoUpdater'));
+  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
+      .SetMethod('checkForUpdates', &auto_updater::AutoUpdater::CheckForUpdates)
+      .SetMethod('getFeedURL', &auto_updater::AutoUpdater::GetFeedURL)
+      .SetMethod('setFeedURL', &AutoUpdater::SetFeedURL)
+      .SetMethod('quitAndInstall', &AutoUpdater::QuitAndInstall);
+}
+    
+      static void BuildPrototype(v8::Isolate* isolate,
+                             v8::Local<v8::FunctionTemplate> prototype);
+    
+    NODE_BUILTIN_MODULE_CONTEXT_AWARE(atom_browser_browser_view, Initialize)
+
+    
+      v8::Local<v8::Value> GetWebContents();
+    
+    #include 'atom/browser/api/atom_api_browser_window.h'
+    
+    namespace api {
     }
     
-    /*!
- * \brief Macro to register linear updater.
- */
-#define XGBOOST_REGISTER_LINEAR_UPDATER(UniqueId, Name)                        \
-  static DMLC_ATTRIBUTE_UNUSED ::xgboost::LinearUpdaterReg&                    \
-      __make_##LinearUpdaterReg##_##UniqueId##__ =                             \
-          ::dmlc::Registry< ::xgboost::LinearUpdaterReg>::Get()->__REGISTER__( \
-              Name)
+    #include 'atom/browser/atom_browser_main_parts.h'
+#include 'atom/common/native_mate_converters/callback.h'
+#include 'atom/common/native_mate_converters/value_converter.h'
+#include 'base/json/json_reader.h'
+#include 'base/json/json_writer.h'
+#include 'base/memory/ptr_util.h'
+#include 'content/public/browser/devtools_agent_host.h'
+#include 'content/public/browser/web_contents.h'
+#include 'native_mate/dictionary.h'
+#include 'native_mate/object_template_builder.h'
+    
+    #include <string>
+#include <utility>
+#include <vector>
+    
+    std::string MessageGenerator::full_class_name() {
+  return GetClassName(descriptor_);
+}
+    
+    #endif  // GOOGLE_PROTOBUF_COMPILER_CSHARP_PRIMITIVE_FIELD_H__
+    
+    #ifndef UINT32_MAX
+    #define UINT32_MAX (4294967295U)
+#endif
+    
+    void absDiff(const Size2D &size,
+             const f32 * src0Base, ptrdiff_t src0Stride,
+             const f32 * src1Base, ptrdiff_t src1Stride,
+             f32 * dstBase, ptrdiff_t dstStride)
+{
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    internal::vtransform(size,
+                         src0Base, src0Stride,
+                         src1Base, src1Stride,
+                         dstBase, dstStride, AbsDiff<f32>());
+#else
+    (void)size;
+    (void)src0Base;
+    (void)src0Stride;
+    (void)src1Base;
+    (void)src1Stride;
+    (void)dstBase;
+    (void)dstStride;
+#endif
+}
+    
+    void bitwiseXor(const Size2D &size,
+                const u8 *src0Base, ptrdiff_t src0Stride,
+                const u8 *src1Base, ptrdiff_t src1Stride,
+                u8 *dstBase, ptrdiff_t dstStride)
+{
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    internal::vtransform(size,
+                         src0Base, src0Stride,
+                         src1Base, src1Stride,
+                         dstBase, dstStride, BitwiseXor());
+#else
+    (void)size;
+    (void)src0Base;
+    (void)src0Stride;
+    (void)src1Base;
+    (void)src1Stride;
+    (void)dstBase;
+    (void)dstStride;
+#endif
+}
+    
+    #include <cstddef>
+#include <cstdlib>
+#include <algorithm>
+    
+    
+    {} // namespace CAROTENE_NS
+
+    
+            size_t i = 0;
+        while(i + 4 <= size.width)
+        {
+            size_t lim = std::min(i + DOT_FLOAT_BLOCKSIZE, size.width) - 4;
+            float32x4_t v_sum = vdupq_n_f32(0.0f);
+    }
+    
+    
+    {            u32 buf[8];
+            vst1_u32(buf, vget_low_u32(el8shr01l));
+            vst1_u32(buf+2, el2l);
+            vst1_u32(buf+4, el2hl);
+            vst1_u32(buf+6, el2hh);
+            for(u32 k=0; k < 8; k++)
+                sqsum[j+k] = prev + prevSqSum[j+k] + buf[k];
+            prev += buf[7];
+        }
+    
+    TEST(Dynamic, FormattedIO) {
+  std::ostringstream out;
+  dynamic doubl = 123.33;
+  dynamic dint = 12;
+  out << '0x' << std::hex << ++dint << ' ' << std::setprecision(1)
+      << doubl << '\n';
+  EXPECT_EQ(out.str(), '0xd 1e+02\n');
+    }
+    
+    void BENCHFUN(pushBack)(int iters, int initialSize) {
+  BenchmarkSuspender braces;
+  auto const obj = randomObject<VECTOR::value_type>();
+  VECTOR v(initialSize, obj);
+  braces.dismissing([&]() {
+    FOR_EACH_RANGE (i, 0, iters) { v.push_back(obj); }
+  });
+}
+    
+      EXPECT_TRUE(bool(temp));
+    
+      template <size_t K, class Callback>
+  void doFormatArg(FormatArg& arg, Callback& cb) const {
+    std::string result;
+    auto appender = [&result](StringPiece s) {
+      result.append(s.data(), s.size());
+    };
+    this->template getFormatValue<K>().format(arg, appender);
+    result = sformat('{{{}}}', result);
+    cb(StringPiece(result));
+  }
+    
+      void sendMessage();
+    
+    std::shared_ptr<DHTBucket>
+DHTRoutingTable::getBucketFor(const unsigned char* nodeID) const
+{
+  return dht::findBucketFor(root_.get(), nodeID);
+}
+    
+    
+    {} // namespace aria2
+    
+    DHTRoutingTableDeserializer::~DHTRoutingTableDeserializer() = default;
+    
+        taskFactory->setLocalNode(localNode);
+    taskFactory->setRoutingTable(routingTable.get());
+    taskFactory->setMessageDispatcher(dispatcher.get());
+    taskFactory->setMessageFactory(factory.get());
+    taskFactory->setTaskQueue(taskQueue.get());
+    taskFactory->setTimeout(std::chrono::seconds(messageTimeout));
+    
+    #include 'common.h'
+    
+      size_t getExecutingTaskSize() const { return execTasks_.size(); }
