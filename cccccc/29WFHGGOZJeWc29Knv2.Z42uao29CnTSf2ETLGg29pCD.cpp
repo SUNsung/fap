763cda1823f37037248cacadfd39117a51cd3daa
SUNsung <weatherius@gmail.com>
2018-08-26 18:30:41 +0300

    Rewrite [29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp] File in: 2018-08-26 18:30:41

--- cccccc/29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp
+++ cccccc/29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp
@@ -1,219 +1,33 @@
 
         
-        // The 'Types' template argument below must have spaces around it
-// since some compilers may choke on '>>' when passing a template
-// instance (e.g. Types<int>)
-# define TYPED_TEST_CASE(CaseName, Types) \
-  typedef ::testing::internal::TypeList< Types >::type \
-      GTEST_TYPE_PARAMS_(CaseName)
+           static char_class_type BOOST_REGEX_CALL lookup_classname(const unsigned short* p1, const unsigned short* p2);
+   static string_type BOOST_REGEX_CALL lookup_collatename(const unsigned short* p1, const unsigned short* p2);
     
-        void ComputeCurrentValue() {
-      if (!AtEnd())
-        current_value_ = ParamType(*current1_, *current2_, *current3_,
-            *current4_, *current5_, *current6_, *current7_, *current8_,
-            *current9_, *current10_);
-    }
-    bool AtEnd() const {
-      // We must report iterator past the end of the range when either of the
-      // component iterators has reached the end of its range.
-      return
-          current1_ == end1_ ||
-          current2_ == end2_ ||
-          current3_ == end3_ ||
-          current4_ == end4_ ||
-          current5_ == end5_ ||
-          current6_ == end6_ ||
-          current7_ == end7_ ||
-          current8_ == end8_ ||
-          current9_ == end9_ ||
-          current10_ == end10_;
+    template <class Container, class Match, class Traits>
+struct format_functor_container
+{
+   format_functor_container(const Container& c) : func(c) {}
     }
     
-      CartesianProductGenerator$i($for j, [[const ParamGenerator<T$j>& g$j]])
-      : $for j, [[g$(j)_(g$j)]] {}
-  virtual ~CartesianProductGenerator$i() {}
-    
-    // Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
-int Factorial(int n) {
-  int result = 1;
-  for (int i = 1; i <= n; i++) {
-    result *= i;
-  }
-    }
-    
-      bool check_for_leaks = false;
-  if (argc > 1 && strcmp(argv[1], '--check_for_leaks') == 0 )
-    check_for_leaks = true;
-  else
-    printf('%s\n', 'Run this program with --check_for_leaks to enable '
-           'custom leak checking in the tests.');
-    
-      bool thrown = false;
-  try {
-    thpp::IntTensor &a = dynamic_cast<thpp::IntTensor&>(*tensor);
-  } catch(std::bad_cast &e) {
-    thrown = true;
-  }
-  assert(thrown);
-    
-    Tensor fft(const Tensor& self, const int64_t signal_ndim, const bool normalized) {
-  return _fft(self, signal_ndim, /* complex_input */ true,
-              /* complex_output */ true, /* inverse */ false, {}, normalized,
-              /* onesided */ false);
+    // make_regex_iterator:
+template <class charT, class traits>
+inline regex_iterator<const charT*, charT, traits> make_regex_iterator(const charT* p, const basic_regex<charT, traits>& e, regex_constants::match_flag_type m = regex_constants::match_default)
+{
+   return regex_iterator<const charT*, charT, traits>(p, p+traits::length(p), e, m);
+}
+template <class charT, class traits, class ST, class SA>
+inline regex_iterator<typename std::basic_string<charT, ST, SA>::const_iterator, charT, traits> make_regex_iterator(const std::basic_string<charT, ST, SA>& p, const basic_regex<charT, traits>& e, regex_constants::match_flag_type m = regex_constants::match_default)
+{
+   return regex_iterator<typename std::basic_string<charT, ST, SA>::const_iterator, charT, traits>(p.begin(), p.end(), e, m);
 }
     
-    #ifdef USE_CUDA
-std::vector <THCStream*> THPUtils_PySequence_to_THCStreamList(PyObject *obj) {
-  if (!PySequence_Check(obj)) {
-    throw std::runtime_error('Expected a sequence in THPUtils_PySequence_to_THCStreamList');
-  }
-  THPObjectPtr seq = THPObjectPtr(PySequence_Fast(obj, NULL));
-  if (seq.get() == NULL) {
-    throw std::runtime_error('expected PySequence, but got ' + std::string(THPUtils_typename(obj)));
-  }
-    }
-    
-    #ifndef HEATMAP_MAX_KEYPOINT_OP_H_
-#define HEATMAP_MAX_KEYPOINT_OP_H_
-    
-    Tensor mv(const Tensor& self, const Tensor& vec) {
-  check_1d(vec, 'vec', 'mv');
-  return at::_mv(self, vec);
-}
-    
-    #include 'RPC-inl.hpp'
-
-    
-    	GDCLASS(PhysicsMaterial, Resource);
-	OBJ_SAVE_TYPE(PhysicsMaterial);
-	RES_BASE_EXTENSION('PhyMat');
-    
-    	virtual void set_active(bool p_active);
-	virtual void init();
-	virtual void step(real_t p_step);
-	virtual void sync();
-	virtual void flush_queries();
-	virtual void end_sync();
-	virtual void finish();
-    
-    public:
-	static const String setting_property_name;
-    
-    				if (ep) {
-					//set all this before the control gets the ENTER_TREE notification
-					ep->object = object;
-    }
+     /*
+  *   LOCATION:    see http://www.boost.org for most recent version.
+  *   FILE         regex_raw_buffer.hpp
+  *   VERSION      see <boost/version.hpp>
+  *   DESCRIPTION: Raw character buffer for regex code.
+  *                Note this is an internal header file included
+  *                by regex.hpp, do not include on its own.
+  */
     
     
-    {		if (!_ptr)
-			return NULL;
-		return reinterpret_cast<T *>(_ptr);
-	}
-    
-    
-    {    // Internal fields
-    rocksdb::Options options_;
-    rocksdb::Status status_;
-    rocksdb::DB* db_;
-    std::unordered_map<std::string, rocksdb::ColumnFamilyHandle*>
-        columnFamilies_;
-};
-    
-      virtual bool Merge(const Slice& key,
-                     const Slice* existing_value,
-                     const Slice& value,
-                     std::string* new_value,
-                     Logger* logger) const override;
-    
-      std::mutex _mutex;                 // used to protect modification of all following variables
-  librados::bufferlist _buffer;      // write buffer
-  uint64_t _buffer_size;             // write buffer size
-  uint64_t _file_size;               // this file size doesn't include buffer size
-    
-    void SyncPoint::LoadDependencyAndMarkers(
-  const std::vector<SyncPointPair>& dependencies,
-  const std::vector<SyncPointPair>& markers) {
-  impl_->LoadDependencyAndMarkers(dependencies, markers);
-}
-    
-    #ifndef ROCKSDB_LITE
-#include 'db/compacted_db_impl.h'
-#include 'db/db_impl.h'
-#include 'db/version_set.h'
-#include 'table/get_context.h'
-    
-    namespace folly {
-    }
-    
-      folly::Optional<T> try_take_for(std::chrono::milliseconds time) override {
-    T item;
-    while (true) {
-      if (nonBlockingTake(item)) {
-        return std::move(item);
-      }
-      if (!sem_.try_wait_for(time)) {
-        return folly::none;
-      }
-    }
-  }
-    
-    
-    {  auto node = new hazptr_retire_node(obj, std::move(reclaim));
-  node->reclaim_ = [](hazptr_obj* p) {
-    delete static_cast<hazptr_retire_node*>(p);
-  };
-  objRetire(node);
-}
-    
-    /**
- * Type that encapsulates the current pair of (to<string>(value), reason)
- */
-using SettingsInfo = std::pair<std::string, std::string>;
-/**
- * @return If the setting exists, the current setting information.
- *         Empty Optional otherwise.
- */
-folly::Optional<SettingsInfo> getAsString(folly::StringPiece settingName);
-    
-    
-    {
-    {}
-}
-    
-    template <class T>
-inline int SimpleUnpack(const void* _rawbuf, size_t _rawlen, size_t& _packlen, AutoBuffer& _data) {
-    if (sizeof(T) > _rawlen) return SIMPLE_CONTINUE;
-    }
-    
-    
-    {
-    {        break;
-    }
-}
-    
-    #include 'comm/debugger/test_spy_sample.h'
-#include 'comm/xlogger/xlogger.h'
-    
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
-    
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
-    
-    /**
- * @file
- **/
-    
-    namespace apollo {
-namespace planning {
-    }
-    }
-    
-    #include 'modules/planning/common/frame.h'
-#include 'modules/planning/common/reference_line_info.h'
-#include 'modules/planning/planner/planner.h'
-#include 'modules/planning/proto/planning_config.pb.h'
