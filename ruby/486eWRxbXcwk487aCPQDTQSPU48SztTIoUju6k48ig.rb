73ff4f30956daab4607ffa951198264230eab4f3
SUNsung <weatherius@gmail.com>
2018-08-26 18:56:04 +0300

    Rewrite [486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb] File in: 2018-08-26 18:56:04

--- ruby/486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb
+++ ruby/486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb
@@ -1,224 +1,75 @@
 
         
-            it 'opens the dry run modal even when clicking on the refresh icon' do
-      visit edit_agent_path(agent)
-      find('.agent-dry-run-button span.glyphicon').click
-      expect(page).to have_text('Event to send (Optional)')
-    end
+                # Get the first part of the email address (before @)
+        # In addtion in removes illegal characters
+        def generate_username(email)
+          email.match(/^[^@]*/)[0].mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/, '').to_s
+        end
     
-      it 'requires a URL or file uplaod' do
-    visit new_scenario_imports_path
-    click_on 'Start Import'
-    expect(page).to have_text('Please provide either a Scenario JSON File or a Public Scenario URL.')
-  end
-    
-        context 'running workers' do
-      before do
-        AgentRunner.class_variable_set(:@@agents, [HuginnScheduler, DelayedJobWorker])
-        stub.instance_of(HuginnScheduler).setup
-        stub.instance_of(DelayedJobWorker).setup
+            case AccessTokenValidationService.new(access_token, request: request).validate(scopes: scopes)
+        when AccessTokenValidationService::INSUFFICIENT_SCOPE
+          raise InsufficientScopeError.new(scopes)
+        when AccessTokenValidationService::EXPIRED
+          raise ExpiredError
+        when AccessTokenValidationService::REVOKED
+          raise RevokedError
+        end
       end
     
-        it 'should work with nested hashes' do
-      @agent.options['very'] = {'nested' => '$.value'}
-      LiquidMigrator.convert_all_agent_options(@agent)
-      expect(@agent.reload.options).to eq({'auth_token' => 'token', 'color' => 'yellow', 'very' => {'nested' => '{{value}}'}, 'notify' => false, 'room_name' => 'test', 'username' => '{{username}}', 'message' => '{{message}}'})
-    end
+          def perform(start_id, stop_id)
+        update = '
+          latest_merge_request_diff_id = (
+            SELECT MAX(id)
+            FROM merge_request_diffs
+            WHERE merge_requests.id = merge_request_diffs.merge_request_id
+          )'.squish
     
-    describe ConvertWebsiteAgentTemplateForMerge do
-  let :old_extract do
-    {
-      'url' => { 'css' => '#comic img', 'value' => '@src' },
-      'title' => { 'css' => '#comic img', 'value' => '@alt' },
-      'hovertext' => { 'css' => '#comic img', 'value' => '@title' }
-    }
-  end
-    
-        it 'updates Agents' last_error_log_at when an error is logged' do
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'some message', :level => 3, :outbound_event => events(:jane_website_agent_event))
-      expect(agents(:jane_website_agent).reload.last_error_log_at).to be_nil
-    
-        def current_url
-      context[:url]
-    end
-    
-        private
-    
-        def initialize(content)
-      @content = content
-      @html = document? ? parse_as_document : parse_as_fragment
-    end
-    
-        def pipeline
-      @pipeline ||= ::HTML::Pipeline.new(self.class.filters).tap do |pipeline|
-        pipeline.instrumentation_service = Docs
-      end
-    end
-    
-        module MultipleBaseUrls
-      def self.included(base)
-        base.extend ClassMethods
+          def to_html
+        link_to(image_tag(image_url, alt: title), link_url)
       end
     
-            css('pre').each do |node|
-          node.content = node.content.strip
+            def value_text
+          STATUS_RENAME[@status.to_s] || @status.to_s
+        end
     
-            css('button.verbose', 'button.verbose + .l-verbose-section', 'a[id=top]', 'a[href='#top']', '.sidebar', 'br').remove
-    
-            doc
+          maker.channel.links.new_link do |link|
+        link.rel = 'alternate'
+        link.href = 'https://devdocs.io/'
+        link.type = 'text/html'
       end
     
-        s = mock('seed')
-    s.should_receive(:to_int).and_return 0
-    srand(s)
-  end
-    
-        after do
-      rm_r @tmp_file
+        def as_json
+      { entries: entries_as_json, types: types_as_json }
     end
     
-    
-  #
-  # Payload types were copied from xCAT-server source code (IPMI.pm)
-  #
-  RMCP_ERRORS = {
-    1 => 'Insufficient resources to create new session (wait for existing sessions to timeout)',
-    2 => 'Invalid Session ID', #this shouldn't occur...
-    3 => 'Invalid payload type',#shouldn't occur..
-    4 => 'Invalid authentication algorithm', #if this happens, we need to enhance our mechanism for detecting supported auth algorithms
-    5 => 'Invalid integrity algorithm', #same as above
-    6 => 'No matching authentication payload',
-    7 => 'No matching integrity payload',
-    8 => 'Inactive Session ID', #this suggests the session was timed out while trying to negotiate, shouldn't happen
-    9 => 'Invalid role',
-    0xa => 'Unauthorised role or privilege level requested',
-    0xb => 'Insufficient resources to create a session at the requested role',
-    0xc => 'Invalid username length',
-    0xd => 'Unauthorized name',
-    0xe => 'Unauthorized GUID',
-    0xf => 'Invalid integrity check value',
-    0x10 => 'Invalid confidentiality algorithm',
-    0x11 => 'No cipher suite match with proposed security algorithms',
-    0x12 => 'Illegal or unrecognized parameter', #have never observed this, would most likely mean a bug in xCAT or IPMI device
-  }
-    
-              # Decodes a Rex::Proto::Kerberos::Model::EncKdcResponse
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode_asn1(input)
-            input.value[0].value.each do |val|
-              case val.tag
-              when 0
-                self.key = decode_key(val)
-              when 1
-                self.last_req = decode_last_req(val)
-              when 2
-                self.nonce = decode_nonce(val)
-              when 3
-                self.key_expiration = decode_key_expiration(val)
-              when 4
-                self.flags = decode_flags(val)
-              when 5
-                self.auth_time = decode_auth_time(val)
-              when 6
-                self.start_time = decode_start_time(val)
-              when 7
-                self.end_time = decode_end_time(val)
-              when 8
-                self.renew_till = decode_renew_till(val)
-              when 9
-                self.srealm = decode_srealm(val)
-              when 10
-                self.sname = decode_sname(val)
-              else
-                raise ::RuntimeError, 'Failed to decode ENC-KDC-RESPONSE SEQUENCE'
-              end
-            end
-          end
-    
-        require 'lib/sass'
-    
-      # An environment that can write to in-scope global variables, but doesn't
-  # create new variables in the global scope. Useful for top-level control
-  # directives.
-  class SemiGlobalEnvironment < Environment
-    def try_set_var(name, value)
-      @vars ||= {}
-      if @vars.include?(name)
-        @vars[name] = value
-        true
-      elsif @parent
-        @parent.try_set_var(name, value)
-      else
-        false
-      end
-    end
+      def collection_presenter
+    ActivityPub::CollectionPresenter.new(
+      id: account_collection_url(@account, params[:id]),
+      type: :ordered,
+      size: @size,
+      items: @statuses
+    )
   end
 end
 
     
-    PROFILE_ERB_TEMPLATE = <<-EOS
-(version 1)
-(debug allow)
+        def update
+      authorize @custom_emoji, :update?
     
-      module Generator
-    autoload :Acknowledgements,        'cocoapods/generator/acknowledgements'
-    autoload :Markdown,                'cocoapods/generator/acknowledgements/markdown'
-    autoload :Plist,                   'cocoapods/generator/acknowledgements/plist'
-    autoload :BridgeSupport,           'cocoapods/generator/bridge_support'
-    autoload :Constant,                'cocoapods/generator/constant'
-    autoload :CopyResourcesScript,     'cocoapods/generator/copy_resources_script'
-    autoload :DummySource,             'cocoapods/generator/dummy_source'
-    autoload :EmbedFrameworksScript,   'cocoapods/generator/embed_frameworks_script'
-    autoload :Header,                  'cocoapods/generator/header'
-    autoload :InfoPlistFile,           'cocoapods/generator/info_plist_file'
-    autoload :ModuleMap,               'cocoapods/generator/module_map'
-    autoload :PrefixHeader,            'cocoapods/generator/prefix_header'
-    autoload :UmbrellaHeader,          'cocoapods/generator/umbrella_header'
-    autoload :AppTargetHelper,         'cocoapods/generator/app_target_helper'
+      private
+    
+      def update
+    if verify_payload?
+      process_salmon
+      head 202
+    elsif payload.present?
+      render plain: signature_verification_failure_reason, status: 401
+    else
+      head 400
+    end
   end
     
-        #-------------------------------------------------------------------------#
-    
-            def run
-          UI.puts('$CACHE_ROOT: #{@cache.root}') if @short_output
-          if @pod_name.nil? # Print all
-            @cache.cache_descriptors_per_pod.each do |pod_name, cache_descriptors|
-              print_pod_cache_infos(pod_name, cache_descriptors)
-            end
-          else # Print only for the requested pod
-            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
-            if cache_descriptors.nil?
-              UI.notice('No cache for pod named #{@pod_name} found')
-            else
-              print_pod_cache_infos(@pod_name, cache_descriptors)
-            end
-          end
-        end
-    
-            def left_whole_line_range(loc_begin)
-          if range_by_whole_lines(loc_begin).source.strip == '{'
-            range_by_whole_lines(loc_begin, include_final_newline: true)
-          else
-            loc_begin
-          end
-        end
-    }
-    
-        # Run main task(RSpec or RuboCop).
-    def run_main_task
-      if master? || !test? || jruby?
-        sh!('bundle exec rake #{ENV['TASK']}')
-      else
-        sh!('bundle exec rake parallel:#{ENV['TASK']}')
-      end
-    end
-    
-          # Checks whether this node body is a void context.
-      # Always `true` for `for`.
-      #
-      # @return [true] whether the `for` node body is a void context
-      def void_context?
-        true
-      end
+      def set_account
+    @account = Account.find(params[:id])
+  end
+end
