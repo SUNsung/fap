4de189bd0a6cf5acf48ee5951be623a7623b83fe
SUNsung <weatherius@gmail.com>
2018-08-27 00:19:57 +0300

    Rewrite [28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb] File in: 2018-08-27 00:19:56

--- ruby/28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb
+++ ruby/28uiJyGy92ePU28Yj0MDNLxNQE28c.GsYSoKpeY28s6h.rb
@@ -1,34 +1,107 @@
 
         
-                  if @address.update_attributes(address_params)
-            respond_with(@address, default_template: :show)
-          else
-            invalid_resource!(@address)
-          end
-        end
+        puts('[WARNING] You are calling #{tool_name} directly. Usage of the tool name without the `fastlane` prefix is deprecated in fastlane 2.0'.yellow)
+puts('Please update your scripts to use `fastlane #{tool_name} #{full_params}` instead.'.yellow)
     
-      def render(source)
-    template = File.read(source)
-    renderer = ERB.new(template)
-    context  = LogStash::PluginManager::RenderContext.new(options)
-    renderer.result(context.get_binding)
+        def run
+      program :name, 'cert'
+      program :version, Fastlane::VERSION
+      program :description, 'CLI for \'cert\' - Create new iOS code signing certificates'
+      program :help, 'Author', 'Felix Krause <cert@krausefx.com>'
+      program :help, 'Website', 'https://fastlane.tools'
+      program :help, 'Documentation', 'https://docs.fastlane.tools/actions/cert/'
+      program :help_formatter, :compact
+    
+        context 'with keywords' do
+      let(:options) do
+        {
+          name: { 'en-US' => 'Fastlane Demo' },
+          description: { 'en-US' => 'Demo description' },
+          keywords: { 'en-US' => 'Some, key, words' }
+        }
+      end
+    
+    describe Deliver::Runner do
+  let(:runner) do
+    allow(Spaceship::Tunes).to receive(:login).and_return(true)
+    allow(Spaceship::Tunes).to receive(:select_team).and_return(true)
+    allow_any_instance_of(Deliver::DetectValues).to receive(:run!) { |opt| opt }
+    Deliver::Runner.new(options)
   end
     
-    module LogStash::Api::AppHelpers
-  # This method handle both of the normal flow *happy path*
-  # and the display or errors, if more custom logic is added here
-  # it will make sense to separate them.
-  #
-  # See `#error` method in the `LogStash::Api::Module::Base`
-  def respond_with(data, options={})
-    as     = options.fetch(:as, :json)
-    filter = options.fetch(:filter, '')
+      it 'allows to click on on the agent name in select2 tags' do
+    visit new_agent_path
+    select_agent_type('Website Agent scrapes')
+    select2('SF Weather', from: 'Sources')
+    click_on 'SF Weather'
+    expect(page).to have_content 'Editing your WeatherAgent'
+  end
     
-          # Only print require() calls that did actual work.
-      # require() returns true on load, false if already loaded.
-      if result
-        source = caller[0]
-        #p source.include?('/lib/polyglot.rb:63:in `require'') => source
-        if source.include?('/lib/polyglot.rb:63:in `require'')
-          source = caller[1]
-        end
+      it 'asks to accept conflicts when the scenario was modified' do
+    DefaultScenarioImporter.seed(user)
+    agent = user.agents.where(name: 'Rain Notifier').first
+    agent.options['expected_receive_period_in_days'] = 9001
+    agent.save!
+    visit new_scenario_imports_path
+    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
+    click_on 'Start Import'
+    expect(page).to have_text('This Scenario already exists in your system.')
+    expect(page).to have_text('9001')
+    check('I confirm that I want to import these Agents.')
+    click_on 'Finish Import'
+    expect(page).to have_text('Import successful!')
+  end
+    
+          it 'generates a richer DOT script' do
+        expect(agents_dot(@agents, rich: true)).to match(%r{
+          \A
+          digraph \x20 'Agent \x20 Event \x20 Flow' \{
+            (graph \[ [^\]]+ \];)?
+            node \[ [^\]]+ \];
+            edge \[ [^\]]+ \];
+            (?<foo>\w+) \[label=foo,tooltip='Dot \x20 Foo',URL='#{Regexp.quote(agent_path(@foo))}'\];
+            \k<foo> -> (?<bar1>\w+) \[style=dashed\];
+            \k<foo> -> (?<bar2>\w+) \[color='\#999999'\];
+            \k<bar1> \[label=bar1,tooltip='Dot \x20 Bar',URL='#{Regexp.quote(agent_path(@bar1))}'\];
+            \k<bar2> \[label=bar2,tooltip='Dot \x20 Bar',URL='#{Regexp.quote(agent_path(@bar2))}',style='rounded,dashed',color='\#999999',fontcolor='\#999999'\];
+            \k<bar2> -> (?<bar3>\w+) \[style=dashed,color='\#999999'\];
+            \k<bar3> \[label=bar3,tooltip='Dot \x20 Bar',URL='#{Regexp.quote(agent_path(@bar3))}'\];
+          \}
+          \z
+        }x)
+      end
+    end
+  end
+    
+          expect(data[:agents][guid_order(agent_list, :jane_weather_agent)]).not_to have_key(:propagate_immediately) # can't receive events
+      expect(data[:agents][guid_order(agent_list, :jane_rain_notifier_agent)]).not_to have_key(:schedule) # can't be scheduled
+    end
+    
+          servers.add_role(name, hosts, options)
+    end
+    
+          private
+    
+          def load_built_in_scm
+        require 'capistrano/scm/#{scm_name}'
+        scm_class = Object.const_get(built_in_scm_plugin_class_name)
+        # We use :load_immediately because we are initializing the SCM plugin
+        # late in the load process and therefore can't use the standard
+        # load:defaults technique.
+        install_plugin(scm_class, load_immediately: true)
+      end
+    
+          def self.[](host)
+        host.is_a?(Server) ? host : new(host)
+      end
+    
+          private
+    
+        attr_reader :local_file
+    
+        context 'with a specific plugin' do
+      let(:plugin_name) { 'logstash-input-stdin' }
+      it 'list the plugin and display the plugin name' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list #{plugin_name}')
+        expect(result).to run_successfully_and_output(/^#{plugin_name}$/)
+      end
