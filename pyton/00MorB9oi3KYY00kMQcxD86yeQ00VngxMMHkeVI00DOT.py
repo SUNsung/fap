0b6b2656c9bc98d65e6d937e83cdb92c51dc3cda
SUNsung <weatherius@gmail.com>
2018-08-27 13:45:40 +0300

    Rewrite [00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py] File in: 2018-08-27 13:45:40

--- pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
+++ pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
@@ -1,110 +1,19 @@
 
         
-                start = time.time()
-        func(X, n_jobs=-1)
-        multi_core.append(time.time() - start)
+        
+class PostgresSimpleLookup(Lookup):
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        rhs, rhs_params = self.process_rhs(qn, connection)
+        params = lhs_params + rhs_params
+        return '%s %s %s' % (lhs, self.operator, rhs), params
     
-        op.add_option('--transformers',
-                  dest='selected_transformers',
-                  default='GaussianRandomProjection,SparseRandomProjection',
-                  type=str,
-                  help='Comma-separated list of transformer to benchmark. '
-                       'Default: %default. Available: '
-                       'GaussianRandomProjection,SparseRandomProjection')
-    
-    In the second benchmark, we increase the number of dimensions of the
-training set, classify a sample and plot the time taken as a function
-of the number of dimensions.
-'''
-import numpy as np
-import matplotlib.pyplot as plt
-import gc
-from datetime import datetime
+        def __init__(self, name):
+        self.name = name
     
     
-if not os.path.exists(DATA_FOLDER):
-    
-        # split the dataset in training and test set:
-    docs_train, docs_test, y_train, y_test = train_test_split(
-        dataset.data, dataset.target, test_size=0.25, random_state=None)
-    
-        output_file.close()
-
-    
-    plt.matshow(fit_data, cmap=plt.cm.Blues)
-plt.title('After biclustering; rearranged to show biclusters')
-    
-    We add observation noise to these waveforms. We generate very sparse
-noise: only 6% of the time points contain noise. As a result, the
-l1 norm of this noise (ie 'cityblock' distance) is much smaller than it's
-l2 norm ('euclidean' distance). This can be seen on the inter-class
-distance matrices: the values on the diagonal, that characterize the
-spread of the class, are much bigger for the Euclidean distance than for
-the cityblock distance.
-    
-    The usual covariance maximum likelihood estimate can be regularized
-using shrinkage. Ledoit and Wolf proposed a close formula to compute
-the asymptotically optimal shrinkage parameter (minimizing a MSE
-criterion), yielding the Ledoit-Wolf covariance estimate.
-    
-    from .theplatform import theplatform_download_by_pid
-    
-    def kuwo_playlist_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    html=get_content(url)
-    matched=set(re.compile('yinyue/(\d+)').findall(html))#reduce duplicated
-    for rid in matched:
-        kuwo_download_by_rid(rid,output_dir,merge,info_only)
-    
-    site = MusicPlayOn()
-download = site.download_by_url
-# TBD: implement download_playlist
-
-    
-    class Pinterest(VideoExtractor):
-    # site name
-    name = 'Pinterest'
-    
-        def resume_reading(self):
-        if self._closing or not self._paused:
-            return
-    
-        def test_set_doc(self):
-        class X:
-            'elephant'
-        X.__doc__ = 'banana'
-        self.assertEqual(X.__doc__, 'banana')
-        with self.assertRaises(TypeError) as cm:
-            type(list).__dict__['__doc__'].__set__(list, 'blah')
-        self.assertIn('can't set list.__doc__', str(cm.exception))
-        with self.assertRaises(TypeError) as cm:
-            type(X).__dict__['__doc__'].__delete__(X)
-        self.assertIn('can't delete X.__doc__', str(cm.exception))
-        self.assertEqual(X.__doc__, 'banana')
-    
-            # Use the constructor with a too-short tuple.
-        try:
-            result2 = os.statvfs_result((10,))
-            self.fail('No exception raised')
-        except TypeError:
-            pass
-    
-    # XXX This doesn't support nested for loops like [x for x in 1, 2 for x in 1, 2]
-class FixParen(fixer_base.BaseFix):
-    BM_compatible = True
-    
-    if __name__ == '__main__':
-    unittest.main()
-
-    
-    
-def main():
-    args = parse_args()
-    zip_path = fetch_zip(
-        args.tag,
-        args.externals_dir / 'zips',
-        org=args.organization,
-        binary=args.binary,
-        verbose=args.verbose,
-    )
-    final_name = args.externals_dir / args.tag
-    extract_zip(args.externals_dir, zip_path).replace(final_name)
+class SessionStore(SessionBase):
+    '''
+    A cache-based session store.
+    '''
+    cache_key_prefix = KEY_PREFIX
