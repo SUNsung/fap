2030ff51a1691c13a2b278290c8378e4b09c3e7d
SUNsung <weatherius@gmail.com>
2018-08-27 22:12:23 +0300

    Rewrite [37eHtEHGTVQ37eSZYONXP4vg37E2kJzkIX8Sk37EIY.rb] File in: 2018-08-27 22:12:22

--- ruby/37eHtEHGTVQ37eSZYONXP4vg37E2kJzkIX8Sk37EIY.rb
+++ ruby/37eHtEHGTVQ37eSZYONXP4vg37E2kJzkIX8Sk37EIY.rb
@@ -1,131 +1,225 @@
 
         
-                  External.require_with_graceful_fail 'jekyll-watch'
-          watch_method = Jekyll::Watcher.method(:watch)
-          if watch_method.parameters.size == 1
-            watch_method.call(
-              options
-            )
-          else
-            watch_method.call(
-              options, site
-            )
-          end
-        end
-      end
-    end
-  end
+        def site
+  @site ||= Jekyll::Site.new(
+    Jekyll.configuration('source' => File.expand_path('../docs', __dir__))
+  ).tap(&:reset).tap(&:read)
 end
-
     
-            def deprecated_relative_permalinks(site)
-          if site.config['relative_permalinks']
-            Jekyll::Deprecator.deprecation_message 'Your site still uses relative permalinks,' \
-                                                   ' which was removed in Jekyll v3.0.0.'
-            true
-          end
-        end
-    
-          def site
-        @site_drop ||= SiteDrop.new(@obj)
-      end
-    
-      def regular?
-    !staff?
-  end
-    
-      def cache_fragment(name)
-    ApplicationSerializer.fragment_cache[name] ||= yield
-  end
-end
-
-    
-        it 'sets $? to a Process::Status' do
-      pid = Process.spawn(ruby_cmd('exit'))
-      Process.wait
-      $?.should be_kind_of(Process::Status)
-      $?.pid.should == pid
-    end
-    
-        assert_equal %w[rdoc ri], @cmd.options[:document]
-  end
-    
-          def reload
-        mtime = File::mtime(@path)
-        if mtime > @mtime
-          @passwd.clear
-          File.open(@path){|io|
-            while line = io.gets
-              line.chomp!
-              case line
-              when %r!\A[^:]+:[a-zA-Z0-9./]{13}\z!
-                if @password_hash == :bcrypt
-                  raise StandardError, '.htpasswd file contains crypt password, only bcrypt passwords supported'
-                end
-                user, pass = line.split(':')
-              when %r!\A[^:]+:\$2[aby]\$\d{2}\$.{53}\z!
-                if @password_hash == :crypt
-                  raise StandardError, '.htpasswd file contains bcrypt password, only crypt passwords supported'
-                end
-                user, pass = line.split(':')
-              when /:\$/, /:{SHA}/
-                raise NotImplementedError,
-                      'MD5, SHA1 .htpasswd file not supported'
-              else
-                raise StandardError, 'bad .htpasswd file'
-              end
-              @passwd[user] = pass
-            end
-          }
-          @mtime = mtime
-        end
-      end
-    
-    
-    # Set of reserved words used by Ruby, you should not use these for
-    # constants or variables
-    ReservedWords = %w[
-      BEGIN END
-      alias and
-      begin break
-      case class
-      def defined do
-      else elsif end ensure
-      false for
-      if in
-      module
-      next nil not
-      or
-      redo rescue retry return
-      self super
-      then true
-      undef unless until
-      when while
-      yield
-    ]
-    
-    if profile_filename = ENV['PROFILE']
-  require 'ruby-prof'
-  reporter =
-    case (profile_extname = File.extname(profile_filename))
-    when '.txt'
-      RubyProf::FlatPrinterWithLineNumbers
-    when '.html'
-      RubyProf::GraphHtmlPrinter
-    when '.callgrind'
-      RubyProf::CallTreePrinter
+      p.action do |args, _|
+    if args.empty?
+      Jekyll.logger.error 'A subcommand is required.'
+      puts p
+      abort
     else
-      raise 'Unknown profiler format indicated by extension: #{profile_extname}'
+      subcommand = args.first
+      unless p.has_command? subcommand
+        Jekyll.logger.abort_with 'fatal: 'jekyll #{args.first}' could not' \
+          ' be found. You may need to install the jekyll-#{args.first} gem' \
+          ' or a related gem to be able to use this subcommand.'
+      end
     end
-  File.open(profile_filename, 'w') do |io|
-    reporter.new(RubyProf.profile { Pod::Command.run(ARGV) }).print(io)
   end
-else
-  Pod::Command.run(ARGV)
 end
 
     
-          def initialize(argv)
-        super
-        config.silent = false
+    Given(%r!^I have a configuration file with '([^\']*)' set to:$!) do |key, table|
+  File.open('_config.yml', 'w') do |f|
+    f.write('#{key}:\n')
+    table.hashes.each do |row|
+      f.write('- #{row['value']}\n')
+    end
+  end
+end
+    
+          CHARS = {
+        :failed    => '\u2718'.red,
+        :pending   => '\u203D'.yellow,
+        :undefined => '\u2718'.red,
+        :passed    => '\u2714'.green,
+        :skipped   => '\u203D'.blue,
+      }.freeze
+    
+        # Fetch the Documents in this collection.
+    # Defaults to an empty array if no documents have been read in.
+    #
+    # Returns an array of Jekyll::Document objects.
+    def docs
+      @docs ||= []
+    end
+    
+      # Override prefixes to consider the scoped view.
+  # Notice we need to check for the request due to a bug in
+  # Action Controller tests that forces _prefixes to be
+  # loaded before even having a request object.
+  #
+  # This method should be public as it is in ActionPack
+  # itself. Changing its visibility may break other gems.
+  def _prefixes #:nodoc:
+    @_prefixes ||= if self.class.scoped_views? && request && devise_mapping
+      ['#{devise_mapping.scoped_path}/#{controller_name}'] + super
+    else
+      super
+    end
+  end
+    
+        def confirmation_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :confirmation_instructions, opts)
+    end
+    
+          # The scope root url to be used when they're signed in. By default, it first
+      # tries to find a resource_root_path, otherwise it uses the root_path.
+      def signed_in_root_path(resource_or_scope)
+        scope = Devise::Mapping.find_scope!(resource_or_scope)
+        router_name = Devise.mappings[scope].router_name
+    
+      if record && record.respond_to?(:timedout?) && warden.authenticated?(scope) &&
+     options[:store] != false && !env['devise.skip_timeoutable']
+    last_request_at = warden.session(scope)['last_request_at']
+    
+        def update
+      authorize @user, :change_email?
+    
+    module Admin
+  class ReportNotesController < BaseController
+    before_action :set_report_note, only: [:destroy]
+    
+          spec['main'] =
+          find_files.(File.join(Bootstrap.stylesheets_path, '_bootstrap.scss')) +
+          find_files.(Bootstrap.fonts_path) +
+          %w(assets/javascripts/bootstrap.js)
+    
+      def save_file(path, content, mode='w')
+    dir = File.dirname(path)
+    FileUtils.mkdir_p(dir) unless File.directory?(dir)
+    File.open(path, mode) { |file| file.write(content) }
+  end
+    
+    Then(/^the specified stage files are created$/) do
+  qa = TestApp.test_app_path.join('config/deploy/qa.rb')
+  production = TestApp.test_app_path.join('config/deploy/production.rb')
+  expect(File.exist?(qa)).to be true
+  expect(File.exist?(production)).to be true
+end
+    
+        def print_config_variables
+      ['--print-config-variables', '-p',
+       'Display the defined config variables before starting the deployment tasks.',
+       lambda do |_value|
+         Configuration.env.set(:print_config_variables, true)
+       end]
+    end
+  end
+end
+
+    
+          def with(properties)
+        properties.each { |key, value| add_property(key, value) }
+        self
+      end
+    
+          # rubocop:disable Security/MarshalLoad
+      def add_role(role, hosts, options={})
+        options_deepcopy = Marshal.dump(options.merge(roles: role))
+        Array(hosts).each { |host| add_host(host, Marshal.load(options_deepcopy)) }
+      end
+      # rubocop:enable Security/MarshalLoad
+    
+    post '/' do
+  connections.each { |out| out << 'data: #{params[:msg]}\n\n' }
+  204 # response without entity body
+end
+    
+    
+    {    if (bt.className == 'condensed') {
+      bt.className = 'expanded';
+      toggler.innerHTML = '(condense)';
+    } else {
+      bt.className = 'condensed';
+      toggler.innerHTML = '(expand)';
+    }
+  }
+  //-->
+  </script>
+    
+    module Rack
+  module Protection
+    class Base
+      DEFAULT_OPTIONS = {
+        :reaction    => :default_reaction, :logging   => true,
+        :message     => 'Forbidden',       :encryptor => Digest::SHA1,
+        :session_key => 'rack.session',    :status    => 403,
+        :allow_empty_referrer => true,
+        :report_key           => 'protection.failed',
+        :html_types           => %w[text/html application/xhtml text/xml application/xml]
+      }
+    
+          def accepts?(env)
+        cookie_header = env['HTTP_COOKIE']
+        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
+        cookies.each do |k, v|
+          if k == session_key && Array(v).size > 1
+            bad_cookies << k
+          elsif k != session_key && Rack::Utils.unescape(k) == session_key
+            bad_cookies << k
+          end
+        end
+        bad_cookies.empty?
+      end
+    
+        it 'corrects one hash parameter with braces and extra trailing ' \
+       'whitespace' do
+      corrected = autocorrect_source('where({ x: 1, y: 2   })')
+      expect(corrected).to eq('where(x: 1, y: 2)')
+    end
+    
+      it 'ignores implicit hashes' do
+    expect_no_offenses(<<-RUBY.strip_indent)
+      foo(a: 1,
+      b: 2)
+    RUBY
+  end
+    
+      def make_multi(multi)
+    multi = multi.dup
+    multi[0] = multi_prefix + multi[0]
+    multi
+  end
+    
+        it 'registers an offense for for' do
+      expect_offense(<<-RUBY.strip_indent)
+        def func
+          for n in [1, 2, 3] do
+          ^^^^^^^^^^^^^^^^^^^^^ Prefer `each` over `for`.
+            puts n
+          end
+        end
+      RUBY
+    end
+    
+              return if node.send_node.stabby_lambda?
+          return if node.braces?
+    
+          # Calls the given block for each `when` node in the `case` statement.
+      # If no block is given, an `Enumerator` is returned.
+      #
+      # @return [self] if a block is given
+      # @return [Enumerator] if no block is given
+      def each_when
+        return when_branches.to_enum(__method__) unless block_given?
+    
+          # Checks whether the `for` node has a `do` keyword.
+      #
+      # @return [Boolean] whether the `for` node has a `do` keyword
+      def do?
+        loc.begin && loc.begin.is?('do')
+      end
+    
+          # Checks whether the `if` node has at least one `elsif` branch. Returns
+      # true if this `if` node itself is an `elsif`.
+      #
+      # @return [Boolean] whether the `if` node has at least one `elsif` branch
+      def elsif_conditional?
+        else_branch && else_branch.if_type? && else_branch.elsif?
       end
