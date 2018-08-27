7a305e66df140cc30f6178c4cdd40e675828b67a
SUNsung <weatherius@gmail.com>
2018-08-27 15:22:00 +0300

    Rewrite [545vVwGOshUEc544WBzNjj79n254yYdcNefuLX254lmb.py] File in: 2018-08-27 15:21:59

--- pyton/545vVwGOshUEc544WBzNjj79n254yYdcNefuLX254lmb.py
+++ pyton/545vVwGOshUEc544WBzNjj79n254yYdcNefuLX254lmb.py
@@ -1,103 +1,27 @@
 
         
-            # Sorting the libraries
-    inner_blocks = sorted(blocks[0].split('##'))
-    for i in range(1 , len(inner_blocks)):
-        if inner_blocks[i][0] != '#':
-            inner_blocks[i] = '##' + inner_blocks[i]
-    inner_blocks=''.join(inner_blocks)
+        import smtplib
     
-                tick_vals += list(xvals + 0.5 * width)
-            tick_labels += ['%i' % val for val in vals]
+    def handleSlideTitle(title):
+    print('<h2>%s</h2>' % getText(title.childNodes))
     
-        # decode the payload explicitly as UTF-8 since lxml is confused for some
-    # reason
-    with codecs.open(html_filename,'r','utf-8') as html_file:
-        html_content = html_file.read()
-    tree = ElementTree(lxml.html.document_fromstring(html_content))
-    i = 0
-    j = 0
-    for p in tree.findall('//p'):
-        content = p.text_content()
-        if len(content) < 100:
-            # skip paragraphs that are too short - probably too noisy and not
-            # representative of the actual language
-            continue
+            #
+        # Testing timeouts
+        #
     
+    cur.execute('insert into people (name_last, age) values ('Yeltsin',   72)')
+cur.execute('insert into people (name_last, age) values ('Putin',     51)')
     
-if not os.path.exists(TRAIN_FOLDER) or not os.path.exists(TEST_FOLDER):
+    test_locate_faces = '''
+face_locations = face_recognition.face_locations(image)
+'''
     
-            valbox = Tk.Frame(fm)
-        controller.complexity = Tk.StringVar()
-        controller.complexity.set('1.0')
-        c = Tk.Frame(valbox)
-        Tk.Label(c, text='C:', anchor='e', width=7).pack(side=Tk.LEFT)
-        Tk.Entry(c, width=6, textvariable=controller.complexity).pack(
-            side=Tk.LEFT)
-        c.pack()
+    # PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
     
-    plt.title('Clustering measures for random uniform labeling\n'
-          'against reference assignment with %d classes' % n_classes)
-plt.xlabel('Number of clusters (Number of samples is fixed to %d)' % n_samples)
-plt.ylabel('Score value')
-plt.ylim(ymin=-0.05, ymax=1.05)
-plt.legend(plots, names)
-plt.show()
-
-    
-    We generate data from three groups of waveforms. Two of the waveforms
-(waveform 1 and waveform 2) are proportional one to the other. The cosine
-distance is invariant to a scaling of the data, as a result, it cannot
-distinguish these two waveforms. Thus even with no noise, clustering
-using this distance will not separate out waveform 1 and 2.
-    
-        def _reset_stats(self):
-        self.tail.clear()
-        self.start = self.lastmark = self.lasttime = time()
-    
-    from scrapy.utils.conf import arglist_to_dict
-from scrapy.exceptions import UsageError
-    
-        @property
-    def templates_dir(self):
-        _templates_base_dir = self.settings['TEMPLATES_DIR'] or \
-            join(scrapy.__path__[0], 'templates')
-        return join(_templates_base_dir, 'spiders')
-
-    
-    import logging
-from twisted.internet import defer
-import six
-from scrapy.exceptions import NotSupported, NotConfigured
-from scrapy.utils.httpobj import urlparse_cached
-from scrapy.utils.misc import load_object
-from scrapy.utils.python import without_none_values
-from scrapy import signals
-    
-            if not self._has_ajax_crawlable_variant(response):
-            return response
-    
-    from scrapy import signals
-    
-    
-class BaseDupeFilter(object):
-    
-    class UsageError(Exception):
-    '''To indicate a command-line usage error'''
-    def __init__(self, *a, **kw):
-        self.print_help = kw.pop('print_help', True)
-        super(UsageError, self).__init__(*a, **kw)
-    
-        def get_testable_domain_names(self):
-        '''Returns the set of domain names that can be tested against'''
-        if self._test_names:
-            return self._test_names
-        else:
-            return {'example.com'}
-    
-    # Output file base name for HTML help builder.
-htmlhelp_basename = 'certbot-nginxdoc'
-    
-            #Creating a list of all md files in the current directory
-        for temp in glob.glob(os.path.join(loc, operating_sys, '*.md')):
-            allmd.append(temp)
+        # Gloss the lips
+    d.polygon(face_landmarks['top_lip'], fill=(150, 0, 0, 128))
+    d.polygon(face_landmarks['bottom_lip'], fill=(150, 0, 0, 128))
+    d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=8)
+    d.line(face_landmarks['bottom_lip'], fill=(150, 0, 0, 64), width=8)
