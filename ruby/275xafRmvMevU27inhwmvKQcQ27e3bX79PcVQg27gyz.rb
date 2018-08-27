8b4bd54a4006f56ea6411ca00f94fd577070a476
SUNsung <weatherius@gmail.com>
2018-08-27 07:19:26 +0300

    Rewrite [275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb] File in: 2018-08-27 07:19:25

--- ruby/275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb
+++ ruby/275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb
@@ -1,75 +1,51 @@
 
         
-            def valid_type?(type)
-      const_get(:TYPES).include?(type)
-    end
+                def template
+          @template ||= Coverage::Template.new(self)
+        end
     
-        def form_configurable_attributes
-      form_configurable_fields.keys
-    end
-  end
-end
-
-    
-        def initialize(tag_name, markup, tokens)
-      @by = nil
-      @source = nil
-      @title = nil
-      if markup =~ FullCiteWithTitle
-        @by = $1
-        @source = $2 + $3
-        @title = $4.titlecase.strip
-      elsif markup =~ FullCite
-        @by = $1
-        @source = $2 + $3
-      elsif markup =~ AuthorTitle
-        @by = $1
-        @title = $2.titlecase.strip
-      elsif markup =~ Author
-        @by = $1
+          def image_url
+        raise NotImplementedError
       end
-      super
+    
+        def manifest_asset_urls
+      @@manifest_asset_urls ||= [
+        javascript_path('application', asset_host: false),
+        stylesheet_path('application'),
+        stylesheet_path('application-dark'),
+        image_path('docs-1.png'),
+        image_path('docs-1@2x.png'),
+        image_path('docs-2.png'),
+        image_path('docs-2@2x.png'),
+        asset_path('docs.js')
+      ]
     end
     
-        def post_render(post)
-      OctopressFilters::post_filter(post)
+        def to_json
+      JSON.generate(as_json)
     end
-  end
-end
     
-    module Jekyll
+        def push(*names)
+      @filters.push *filter_const(names)
+    end
     
-            private
-    
-          # Only print require() calls that did actual work.
-      # require() returns true on load, false if already loaded.
-      if result
-        source = caller[0]
-        #p source.include?('/lib/polyglot.rb:63:in `require'') => source
-        if source.include?('/lib/polyglot.rb:63:in `require'')
-          source = caller[1]
+        def request_all(urls, &block)
+      if options[:rate_limit]
+        if @@rate_limiter
+          @@rate_limiter.limit = options[:rate_limit]
+        else
+          @@rate_limiter = RateLimiter.new(options[:rate_limit])
+          Typhoeus.before(&@@rate_limiter.to_proc)
         end
+      end
     
-      def download(url, output)
-    uri = URI(url)
-    digest = Digest::SHA1.new
-    tmp = '#{output}.tmp'
-    Net::HTTP.start(uri.host, uri.port, :use_ssl => (uri.scheme == 'https')) do |http|
-      request = Net::HTTP::Get.new(uri.path)
-      http.request(request) do |response|
-        fail 'HTTP fetch failed for #{url}. #{response}' if [200, 301].include?(response.code)
-        size = (response['content-length'].to_i || -1).to_f
-        count = 0
-        File.open(tmp, 'w') do |fd|
-          response.read_body do |chunk|
-            fd.write(chunk)
-            digest << chunk
-            if size > 0 && $stdout.tty?
-              count += chunk.bytesize
-              $stdout.write(sprintf('\r%0.2f%%', count/size * 100))
-            end
-          end
-        end
-        $stdout.write('\r      \r') if $stdout.tty?
+        def initialize(*args)
+      if args.empty?
+        super(*Array.new(9))
+      elsif args.length == 1 && args.first.is_a?(Hash)
+        args.first.assert_valid_keys URI::Generic::COMPONENT
+        super(*args.first.values_at(*URI::Generic::COMPONENT))
+      else
+        super
       end
     end
