045af9969ce6e3d17a218d03ec0b3d9507857e41
SUNsung <weatherius@gmail.com>
2018-08-27 17:32:27 +0300

    Rewrite [57WFtpl9xoY2U57vj9tpwQ27OA57jzQSVENsDlE57.Aj.cpp] File in: 2018-08-27 17:32:27

--- cccccc/57WFtpl9xoY2U57vj9tpwQ27OA57jzQSVENsDlE57.Aj.cpp
+++ cccccc/57WFtpl9xoY2U57vj9tpwQ27OA57jzQSVENsDlE57.Aj.cpp
@@ -1,201 +1,350 @@
-namespace tensorflow {
-namespace functor {
-// TODO(b/32239807) No GPU ops for mod yet.
-}  // namespace functor
-}  // namespace tensorflow
-    
-    #endif  // TENSORFLOW_LIB_GTL_EDIT_DISTANCE_H_
 
+        
+        
+    {  // Moving from a message on the arena should lead to a copy.
+  message2 = std::move(*message1_on_arena);
+  EXPECT_NE(nested, &message2.optional_nested_message());
+  TestUtil::ExpectAllFieldsSet(*message1_on_arena);
+  TestUtil::ExpectAllFieldsSet(message2);
+}
     
-      // Grad[X] = scale * rsqrt[Var[X] + epsilon] * 1/N * I6.
-  auto grad_activation = add_binary(activation_shape, HloOpcode::kMultiply,
-                                    scale_times_rsqrt_var_add_epsilon, i6);
-  auto tuple =
-      HloInstruction::CreateTuple({grad_activation, grad_scale, grad_beta});
-  if (batch_norm->has_sharding()) {
-    const HloSharding& sharding = batch_norm->sharding();
-    int64 instruction_count_after = computation_->instruction_count();
-    CHECK_EQ(instruction_count_after,
-             instruction_count_before + added_instructions.size());
-    HloSharding activation_sharding =
-        sharding.GetAsShapeTree(batch_norm->shape()).element({0});
-    auto unique_device = batch_norm->sharding_unique_device();
-    HloSharding default_sharding =
-        unique_device.has_value()
-            ? HloSharding::AssignDevice(unique_device.value())
-            : HloSharding::Replicate();
-    for (HloInstruction* inst : added_instructions) {
-      if (ShapeUtil::Equal(inst->shape(), activation_shape)) {
-        inst->set_sharding(activation_sharding);
-      } else {
-        inst->set_sharding(default_sharding);
+    #include <google/protobuf/compiler/code_generator.h>
+#include <google/protobuf/compiler/plugin.h>
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/descriptor.pb.h>
+#include <google/protobuf/io/printer.h>
+#include <google/protobuf/io/zero_copy_stream.h>
+#include <google/protobuf/stubs/strutil.h>
+    
+    #include <google/protobuf/compiler/code_generator.h>
+#include <google/protobuf/compiler/csharp/csharp_primitive_field.h>
+    
+      if (filename.empty()) {
+    *error = filename_error;
+    return false;
+  }
+  std::unique_ptr<io::ZeroCopyOutputStream> output(
+      generator_context->Open(filename));
+  io::Printer printer(output.get(), '$');
+    
+    #endif  // GOOGLE_PROTOBUF_COMPILER_CSHARP_MAP_FIELD_H__
+    
+    class MessageFieldGenerator : public FieldGeneratorBase {
+ public:
+  MessageFieldGenerator(const FieldDescriptor* descriptor,
+                        int fieldOrdinal,
+                        const Options *options);
+  ~MessageFieldGenerator();
+    }
+    
+    void PrimitiveFieldGenerator::GenerateSerializationCode(io::Printer* printer) {
+  printer->Print(
+    variables_,
+    'if ($has_property_check$) {\n'
+    '  output.WriteRawTag($tag_bytes$);\n'
+    '  output.Write$capitalized_type_name$($property_name$);\n'
+    '}\n');
+}
+    
+      virtual void GenerateCloningCode(io::Printer* printer);
+  virtual void GenerateMembers(io::Printer* printer);
+  virtual void GenerateMergingCode(io::Printer* printer);
+  virtual void WriteToString(io::Printer* printer);
+  virtual void GenerateParsingCode(io::Printer* printer);
+    
+    template <class BidiIterator, class Allocator, class traits>
+bool perl_matcher<BidiIterator, Allocator, traits>::match_dot_repeat_slow()
+{
+#ifdef BOOST_MSVC
+#pragma warning(push)
+#pragma warning(disable:4127)
+#endif
+   unsigned count = 0;
+   const re_repeat* rep = static_cast<const re_repeat*>(pstate);
+   re_syntax_base* psingle = rep->next.p;
+   // match compulsary repeats first:
+   while(count < rep->min)
+   {
+      pstate = psingle;
+      if(!match_wild())
+         return false;
+      ++count;
+   }
+   bool greedy = (rep->greedy) && (!(m_match_flags & regex_constants::match_any) || m_independent);   
+   if(greedy)
+   {
+      // normal repeat:
+      while(count < rep->max)
+      {
+         pstate = psingle;
+         if(!match_wild())
+            break;
+         ++count;
       }
+      if((rep->leading) && (count < rep->max))
+         restart = position;
+      pstate = rep;
+      return backtrack_till_match(count - rep->min);
+   }
+   else
+   {
+      // non-greedy, keep trying till we get a match:
+      BidiIterator save_pos;
+      do
+      {
+         if((rep->leading) && (rep->max == UINT_MAX))
+            restart = position;
+         pstate = rep->alt.p;
+         save_pos = position;
+         ++state_count;
+         if(match_all_states())
+            return true;
+         if((count >= rep->max) || !m_can_backtrack)
+            return false;
+         ++count;
+         pstate = psingle;
+         position = save_pos;
+         if(!match_wild())
+            return false;
+      }while(true);
+   }
+#ifdef BOOST_MSVC
+#pragma warning(pop)
+#endif
+}
+    
+    
+    
+     /*
+  *   LOCATION:    see http://www.boost.org for most recent version.
+  *   FILE         regex_grep.hpp
+  *   VERSION      see <boost/version.hpp>
+  *   DESCRIPTION: Provides regex_grep implementation.
+  */
+    
+    
+namespace boost{
     }
-    tuple->set_sharding(sharding);
+    
+    template <class BaseT>
+struct default_wrapper : public BaseT
+{
+   typedef typename BaseT::char_type char_type;
+   std::string error_string(::boost::regex_constants::error_type e)const
+   {
+      return ::boost::BOOST_REGEX_DETAIL_NS::get_default_error_string(e);
+   }
+   ::boost::regex_constants::syntax_type syntax_type(char_type c)const
+   {
+      return ((c & 0x7f) == c) ? get_default_syntax_type(static_cast<char>(c)) : ::boost::regex_constants::syntax_char;
+   }
+   ::boost::regex_constants::escape_syntax_type escape_syntax_type(char_type c)const
+   {
+      return ((c & 0x7f) == c) ? get_default_escape_syntax_type(static_cast<char>(c)) : ::boost::regex_constants::escape_type_identity;
+   }
+   int toi(const char_type*& p1, const char_type* p2, int radix)const
+   {
+      return ::boost::BOOST_REGEX_DETAIL_NS::global_toi(p1, p2, radix, *this);
+   }
+   char_type translate(char_type c, bool icase)const
+   {
+      return (icase ? this->translate_nocase(c) : this->translate(c));
+   }
+   char_type translate(char_type c)const
+   {
+      return BaseT::translate(c);
+   }
+   char_type tolower(char_type c)const
+   {
+      return ::boost::BOOST_REGEX_DETAIL_NS::global_lower(c);
+   }
+   char_type toupper(char_type c)const
+   {
+      return ::boost::BOOST_REGEX_DETAIL_NS::global_upper(c);
+   }
+};
+    
+    inline Float8 operator+(Float8 lhs, const Float8& rhs) {
+  lhs += rhs;
+  return lhs;
+}
+inline Float8 operator-(Float8 lhs, const Float8& rhs) {
+  lhs -= rhs;
+  return lhs;
+}
+inline Float8 operator*(Float8 lhs, const Float8& rhs) {
+  lhs *= rhs;
+  return lhs;
+}
+inline Float8 operator/(Float8 lhs, const Float8& rhs) {
+  lhs /= rhs;
+  return lhs;
+}
+    
+    /*!
+ * \brief perform numerically safe logsum
+ * \param begin The begining iterator.
+ * \param end The end iterator.
+ * \return the iterator point to the maximum value.
+ * \tparam Iterator The type of the iterator.
+ */
+template<typename Iterator>
+inline float LogSum(Iterator begin, Iterator end) {
+  float mx = *begin;
+  for (Iterator it = begin; it != end; ++it) {
+    mx = std::max(mx, *it);
   }
-    
-      // Copy the input dimensions to output except make the last dimension 1.
-  TF_LITE_ENSURE(context, NumDimensions(input) >= 1);
-  TfLiteIntArray* output_size = TfLiteIntArrayCopy(input->dims);
-  output_size->data[NumDimensions(input) - 1] = 1;
-    
-    #endif  // TENSORFLOW_DEBUGGER_STATE_IMPL_H_
-
-    
-      double ComputeDualLoss(const double current_dual, const double example_label,
-                         const double example_weight) const final {
-    // For binary classification, there are 2 conjugate functions, one per
-    // label value (-1 and 1).
-    const double y_alpha = current_dual * example_label;  // y \alpha
-    if (y_alpha < 0 || y_alpha > 1.0) {
-      return std::numeric_limits<double>::max();
-    }
-    return (-y_alpha + 0.5 * gamma * current_dual * current_dual) *
-           example_weight;
+  float sum = 0.0f;
+  for (Iterator it = begin; it != end; ++it) {
+    sum += std::exp(*it - mx);
   }
+  return mx + std::log(sum);
+}
     
-    namespace tensorflow {
-namespace port {
+    namespace xgboost {
+namespace data {
     }
     }
     
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    // TODO: Enable GPU support for angle op after resolving
-// build failures on GPU (See #10643 for context).
-#if 0 && GOOGLE_CUDA
-REGISTER_COMPLEX(GPU, float, complex64);
-REGISTER_COMPLEX(GPU, double, complex128);
+    #if DMLC_ENABLE_STD_THREAD
+#include <dmlc/concurrency.h>
+#include <thread>
 #endif
     
-    static int set_flags(struct ifaddrs* ifaddr) {
-	int fd = socket(AF_INET, SOCK_DGRAM, 0);
-	if (fd == -1) {
-		return -1;
-	}
-	ifreq ifr;
-	memset(&ifr, 0, sizeof(ifr));
-	strncpy(ifr.ifr_name, ifaddr->ifa_name, IFNAMSIZ - 1);
-	int rc = ioctl(fd, SIOCGIFFLAGS, &ifr);
-	close(fd);
-	if (rc == -1) {
-		return -1;
-	}
-	ifaddr->ifa_flags = ifr.ifr_flags;
-	return 0;
-}
+    int16_t i16s[] = {
+    -(static_cast<int16_t>(1) << 8),
+    static_cast<int16_t>(1) << 12,
+    -(static_cast<int16_t>(1) << 14),
+};
     
-    	// Path
-	p_len = host.find('/');
-	if (p_len != -1) {
-		path = host.substr(p_len, host.length() - p_len);
-		host = host.substr(0, p_len);
-	}
-    
-    	GDCLASS(WebSocketClient, WebSocketMultiplayerPeer);
-	GDCICLASS(WebSocketClient);
-    
-    	GodotContactPairContactResultCallback(btCollisionObject *p_self_object, Vector3 *p_results, int p_resultMax, const Set<RID> *p_exclude) :
-			m_self_object(p_self_object),
-			m_results(p_results),
-			m_exclude(p_exclude),
-			m_resultMax(p_resultMax),
-			m_count(0) {}
-    
-    
-/* END */
-
-    
-       - Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-    
-    #undef    silk_SMLAL
-static OPUS_INLINE opus_int64 silk_SMLAL(opus_int64 a64, opus_int32 b32, opus_int32 c32){
-    opus_int64 ret;
-    ops_count += 8;
-    ret = a64 + ((opus_int64)(b32) * /*(opus_int64)*/(c32));
-    return ret;
-}
-#undef    silk_SMLALBB
-static OPUS_INLINE opus_int64 silk_SMLALBB(opus_int64 a64, opus_int16 b16, opus_int16 c16){
-    opus_int64 ret;
-    ops_count += 4;
-    ret = a64 + ((opus_int64)(b16) * /*(opus_int64)*/(c16));
-    return ret;
-}
-    
-    /* (a32 * (opus_int32)((opus_int16)(b32))) >> 16 output have to be 32bit int */
-#undef silk_SMULWB
-static OPUS_INLINE opus_int32 silk_SMULWB_armv4(opus_int32 a, opus_int16 b)
-{
-  unsigned rd_lo;
-  int rd_hi;
-  __asm__(
-      '#silk_SMULWB\n\t'
-      'smull %0, %1, %2, %3\n\t'
-      : '=&r'(rd_lo), '=&r'(rd_hi)
-      : '%r'(a), 'r'(b<<16)
-  );
-  return rd_hi;
-}
-#define silk_SMULWB(a, b) (silk_SMULWB_armv4(a, b))
-    
-    #include 'imgui.h'
-#include 'imgui_impl_freeglut.h'
-#include <GL/freeglut.h>
-    
-                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
-    
-    int main(int, char**)
-{
-    // Create application window
-    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T('ImGui Example'), NULL };
-    RegisterClassEx(&wc);
-    HWND hwnd = CreateWindow(_T('ImGui Example'), _T('Dear ImGui DirectX11 Example'), WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc.hInstance, NULL);
+    BENCHMARK_RELATIVE(exception_wrapper_create_and_throw_concurrent, iters) {
+  std::atomic<bool> go(false);
+  std::vector<std::thread> threads;
+  BENCHMARK_SUSPEND {
+    for (int t = 0; t < FLAGS_num_threads; ++t) {
+      threads.emplace_back([&go, iters] {
+        while (!go) { }
+        std::runtime_error e('payload');
+        for (size_t i = 0; i < iters; ++i) {
+          auto ew = folly::make_exception_wrapper<std::runtime_error>(e);
+          try {
+            ew.throw_exception();
+          } catch (std::runtime_error&) {
+          }
+        }
+      });
     }
-    
-    namespace A2STR {
-    }
-    
-    void AbstractBtMessage::setBtMessageValidator(
-    std::unique_ptr<BtMessageValidator> validator)
-{
-  validator_ = std::move(validator);
-}
-    
-      virtual void doReceivedAction() CXX11_OVERRIDE {}
-    
-    AbstractHttpServerResponseCommand::~AbstractHttpServerResponseCommand()
-{
-  if (readCheck_) {
-    e_->deleteSocketForReadCheck(socket_, this);
   }
-  if (writeCheck_) {
-    e_->deleteSocketForWriteCheck(socket_, this);
+  go.store(true);
+  for (auto& t : threads) {
+    t.join();
   }
 }
     
-    public:
-  AbstractProxyResponseCommand(
-      cuid_t cuid, const std::shared_ptr<Request>& req,
-      const std::shared_ptr<FileEntry>& fileEntry, RequestGroup* requestGroup,
-      const std::shared_ptr<HttpConnection>& httpConnection, DownloadEngine* e,
-      const std::shared_ptr<SocketCore>& s);
+    TEST(ExecutorTest, KeepAliveCopy) {
+  KeepAliveTestExecutor exec;
+    }
     
-      int64_t totalLength_;
+    // this test makes sure that the coroutine is destroyed properly
+TEST(Expected, CoroutineCleanedUp) {
+  int count_dest = 0;
+  auto r = [&]() -> Expected<int, Err> {
+    SCOPE_EXIT {
+      ++count_dest;
+    };
+    auto x = co_await Expected<int, Err>(makeUnexpected(Err::badder()));
+    ADD_FAILURE() << 'Should not be resuming';
+    co_return x;
+  }();
+  EXPECT_FALSE(r.hasValue());
+  EXPECT_EQ(1, count_dest);
+}
     
-      /**
-   * Returns announce URL.
-   */
-  std::string getAnnounce() const;
+    template <class String> void clause11_21_4_7_9_d(String & test) {
+  String s;
+  randomString(&s, maxString);
+  int tristate = test.compare(s.c_str());
+  if (tristate > 0) {
+    tristate = 1;
+  } else if (tristate < 0) {
+    tristate = 2;
+  }
+  Num2String(test, tristate);
+}
+    
+    
+    {  char buf = 'x';
+  int p[2];
+  expectOK(::pipe(p));
+  int flags = ::fcntl(p[0], F_GETFL);
+  expectOK(flags);
+  expectOK(::fcntl(p[0], F_SETFL, flags | O_NONBLOCK));
+  expectWouldBlock(::read(p[0], &buf, 1));
+  {
+    File f(p[1]);
+    EXPECT_EQ(p[1], f.fd());
+  }
+  // Ensure that moving the file doesn't close it
+  {
+    File f(p[1]);
+    EXPECT_EQ(p[1], f.fd());
+    File f1(std::move(f));
+    EXPECT_EQ(-1, f.fd());
+    EXPECT_EQ(p[1], f1.fd());
+  }
+  expectWouldBlock(::read(p[0], &buf, 1));  // not closed
+  {
+    File f(p[1], true);
+    EXPECT_EQ(p[1], f.fd());
+  }
+  ssize_t r = ::read(p[0], &buf, 1);  // eof
+  expectOK(r);
+  EXPECT_EQ(0, r);
+  ::close(p[0]);
+}
+    
+      // writev-like
+  ssize_t operator()(int fd, const iovec* iov, int count);
+    
+    template <bool containerMode, class... Args>
+class TestExtendingFormatter
+    : public BaseFormatter<TestExtendingFormatter<containerMode, Args...>,
+                           containerMode,
+                           Args...> {
+ private:
+  explicit TestExtendingFormatter(StringPiece& str, Args&&... args)
+      : BaseFormatter<TestExtendingFormatter<containerMode, Args...>,
+                      containerMode,
+                      Args...>(str, std::forward<Args>(args)...) {}
+    }
+    
+    #include 'DHTAbstractTask.h'
+#include 'a2time.h'
+    
+    class DHTResponseMessage : public DHTAbstractMessage {
+protected:
+  virtual std::string toStringOptional() const { return A2STR::NIL; }
+    }
+    
+      Time getSerializedTime() const { return serializedTime_; }
+    
+    DHTRoutingTableSerializer::DHTRoutingTableSerializer(int family)
+    : family_(family)
+{
+}
+    
+    class DHTRoutingTableSerializer {
+private:
+  int family_;
+    }
+    
+        routingTable->setTaskQueue(taskQueue.get());
+    routingTable->setTaskFactory(taskFactory.get());
+    
+    #include <vector>
+#include <deque>
+#include <memory>
+    
+    bool DHTUnknownMessage::isReply() const { return false; }
+    
+    DNSCache::CacheEntry::CacheEntry(const CacheEntry& c) = default;
