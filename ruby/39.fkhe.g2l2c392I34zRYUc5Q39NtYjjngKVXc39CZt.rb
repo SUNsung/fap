498f6da17e745c1e668a468878b2a22c83664320
SUNsung <weatherius@gmail.com>
2018-08-29 02:38:37 +0300

    Rewrite [39.fkhe.g2l2c392I34zRYUc5Q39NtYjjngKVXc39CZt.rb] File in: 2018-08-29 02:38:36

--- ruby/39.fkhe.g2l2c392I34zRYUc5Q39NtYjjngKVXc39CZt.rb
+++ ruby/39.fkhe.g2l2c392I34zRYUc5Q39NtYjjngKVXc39CZt.rb
@@ -1,60 +1,113 @@
 
         
-          describe '.create' do
-    it 'creates a key with the client' do
-      expected_service_configs = {
-        'U27F4V844T' => [],
-        'DQ8HTZ7739' => [],
-        '6A7HVUVQ3M' => ['some-music-id']
-      }
-      mock_client_response(:create_key!, with: { name: 'New Key', service_configs: expected_service_configs }) do
-        {
-          keyId: 'a-new-key-id'
-        }
+              # Add common options to a command for building configuration
+      #
+      # cmd - the Jekyll::Command to add these options to
+      #
+      # Returns nothing
+      # rubocop:disable Metrics/MethodLength
+      def add_build_options(cmd)
+        cmd.option 'config', '--config CONFIG_FILE[,CONFIG_FILE2,...]',
+                   Array, 'Custom configuration file'
+        cmd.option 'destination', '-d', '--destination DESTINATION',
+                   'The current folder will be generated into DESTINATION'
+        cmd.option 'source', '-s', '--source SOURCE', 'Custom source directory'
+        cmd.option 'future', '--future', 'Publishes posts with a future date'
+        cmd.option 'limit_posts', '--limit_posts MAX_POSTS', Integer,
+                   'Limits the number of posts to parse and publish'
+        cmd.option 'watch', '-w', '--[no-]watch', 'Watch for changes and rebuild'
+        cmd.option 'baseurl', '-b', '--baseurl URL',
+                   'Serve the website from the given base URL'
+        cmd.option 'force_polling', '--force_polling', 'Force watch to use polling'
+        cmd.option 'lsi', '--lsi', 'Use LSI for improved related posts'
+        cmd.option 'show_drafts', '-D', '--drafts', 'Render posts in the _drafts folder'
+        cmd.option 'unpublished', '--unpublished',
+                   'Render posts that were marked as unpublished'
+        cmd.option 'quiet', '-q', '--quiet', 'Silence output.'
+        cmd.option 'verbose', '-V', '--verbose', 'Print verbose output.'
+        cmd.option 'incremental', '-I', '--incremental', 'Enable incremental rebuild.'
+        cmd.option 'strict_front_matter', '--strict_front_matter',
+                   'Fail if errors are present in front matter'
       end
+      # rubocop:enable Metrics/MethodLength
     
-          def self.is_supported?(platform)
-        true
+        def resend
+      authorize @user, :confirm?
+    
+        private
+    
+        def destroy
+      authorize @email_domain_block, :destroy?
+      @email_domain_block.destroy!
+      log_action :destroy, @email_domain_block
+      redirect_to admin_email_domain_blocks_path, notice: I18n.t('admin.email_domain_blocks.destroyed_msg')
+    end
+    
+            render template: 'admin/reports/show'
       end
+    end
     
-          it 'splits correctly' do
-        expected = [
-          'One',
-          'Two',
-          'Three',
-          'Four Token',
-          'Or',
-          'Newlines',
-          'Everywhere'
-        ]
-        expect(generator.split_keywords(keywords)).to eq(expected)
+      def set_account
+    @account = Account.find(params[:id])
+  end
+    
+      def set_account
+    @account = Account.find(params[:id])
+  end
+end
+
+    
+          File.open('bower.json', 'w') do |f|
+        f.puts JSON.pretty_generate(spec)
       end
     end
   end
 end
 
     
-          def load_all_tasks
-        self.tasks = []
+      # The test environment is used exclusively to run your application's
+  # test suite. You never need to work with it otherwise. Remember that
+  # your test database is 'scratch space' for the test suite and is wiped
+  # and recreated between test runs. Don't rely on the data there!
+  config.cache_classes = true
     
-          it 'does set the source directory' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            cloc(source_directory: 'MyCoolApp')
-          end').runner.execute(:test)
+            def line_items_attributes
+          { line_items_attributes: {
+              id: params[:id],
+              quantity: params[:line_item][:quantity],
+              options: line_item_params[:options] || {}
+          } }
+        end
     
-          it 'it increments all targets minor version major' do
-        Fastlane::FastFile.new.parse('lane :test do
-          increment_version_number(bump_type: 'major')
-        end').runner.execute(:test)
+            def update
+          @option_type = Spree::OptionType.accessible_by(current_ability, :update).find(params[:id])
+          if @option_type.update_attributes(option_type_params)
+            render :show
+          else
+            invalid_resource!(@option_type)
+          end
+        end
     
-    class Devise::OmniauthCallbacksController < DeviseController
-  prepend_before_action { request.env['devise.skip_timeout'] = true }
+            def index
+          authorize! :index, Order
+          @orders = Order.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
+          respond_with(@orders)
+        end
     
-    class Devise::PasswordsController < DeviseController
-  prepend_before_action :require_no_authentication
-  # Render the #edit only if coming from a reset password email link
-  append_before_action :assert_reset_token_passed, only: :edit
+            def index
+          @payments = @order.payments.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
+          respond_with(@payments)
+        end
     
-      if record && record.respond_to?(:timedout?) && warden.authenticated?(scope) &&
-     options[:store] != false && !env['devise.skip_timeoutable']
-    last_request_at = warden.session(scope)['last_request_at']
+              if @product_property.update_attributes(product_property_params)
+            respond_with(@product_property, status: 200, default_template: :show)
+          else
+            invalid_resource!(@product_property)
+          end
+        end
+    
+            def new; end
+    
+            def stock_location
+          @stock_location ||= StockLocation.accessible_by(current_ability, :read).find(params[:id])
+        end
