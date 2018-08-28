917123799257b8778720b63613b4c7e986e80b3f
SUNsung <weatherius@gmail.com>
2018-08-28 17:26:29 +0300

    Rewrite [56t4HHA9l6kQc56rczb2UoTZrY56zYDfnTJPaOw568Hb.rb] File in: 2018-08-28 17:26:28

--- ruby/56t4HHA9l6kQc56rczb2UoTZrY56zYDfnTJPaOw568Hb.rb
+++ ruby/56t4HHA9l6kQc56rczb2UoTZrY56zYDfnTJPaOw568Hb.rb
@@ -1,149 +1,163 @@
 
         
-              # Run ::process method in a given set of Jekyll::Command subclasses and suggest
-      # re-running the associated command with --trace switch to obtain any additional
-      # information or backtrace regarding the encountered Exception.
-      #
-      # cmd     - the Jekyll::Command to be handled
-      # options - configuration overrides
-      # klass   - an array of Jekyll::Command subclasses associated with the command
-      #
-      # Note that all exceptions are rescued..
-      # rubocop: disable RescueException
-      def process_with_graceful_fail(cmd, options, *klass)
-        klass.each { |k| k.process(options) if k.respond_to?(:process) }
-      rescue Exception => e
-        raise e if cmd.trace
+              Fabricate(:post, topic: @topic, user: @topic.user)
+      @target.reload
+      expect(@target.unread_private_messages).to eq(1)
+    end
     
-            def collect_urls(urls, things, destination)
-          things.each do |thing|
-            dest = thing.destination(destination)
-            if urls[dest]
-              urls[dest] << thing.path
-            else
-              urls[dest] = [thing.path]
-            end
-          end
-          urls
-        end
+    Badge.seed do |b|
+  b.id = Badge::FirstReplyByEmail
+  b.name = 'First Reply By Email'
+  b.badge_type_id = BadgeType::Bronze
+  b.multiple_grant = false
+  b.target_posts = true
+  b.show_posts = true
+  b.query = nil
+  b.badge_grouping_id = BadgeGrouping::GettingStarted
+  b.default_badge_grouping_id = BadgeGrouping::GettingStarted
+  b.trigger = Badge::Trigger::None
+  b.system = true
+end
     
-          def html_pages
-        @site_html_pages ||= @obj.pages.select do |page|
-          page.html? || page.url.end_with?('/')
-        end
+          if lounge.topic_id.nil?
+        creator = PostCreator.new(Discourse.system_user,
+          raw: I18n.t('vip_category_description'),
+          title: I18n.t('category.topic_prefix', category: lounge.name),
+          category: lounge.name,
+          archetype: Archetype.default,
+          skip_validations: true
+        )
+        post = creator.create
+    
+        Category.transaction do
+      staff.group_names = ['staff']
+      unless staff.save
+        puts staff.errors.full_messages
+        raise 'Failed to set permissions on the Staff category!'
       end
     
-      included do
-    self.validate :validate_email_options
-  end
+    BASE_URI = ENV['BASE_URI'] || 'https://github.com/jondot/awesome-react-native'
     
-      def destroy
-    @event.destroy
+    class Devise::OmniauthCallbacksController < DeviseController
+  prepend_before_action { request.env['devise.skip_timeout'] = true }
     
-      def user_credential_params
-    params.require(:user_credential).permit(:credential_name, :credential_value, :mode)
-  end
-end
-
+      private
     
-        #
-    # Instance Methods
+      # Stores the token generator
+  mattr_accessor :token_generator
+  @@token_generator = nil
+    
+    module Devise
+  module Controllers
+    # Create url helpers to be used with resource/scope configuration. Acts as
+    # proxies to the generated routes created by devise.
+    # Resource param can be a string or symbol, a class, or an instance object.
+    # Example using a :user resource:
     #
-    
-    # Copyright (C) 2008 Rapid7, Inc.
-    
-                end
-    
-          when :login_fail
-        if(s[:user] and s[:pass])
-          report_auth_info(s.merge({:active => false}))
-          print_status('Failed FTP Login: #{s[:session]} >> #{s[:user]} / #{s[:pass]}')
-    
-    require_relative 'converter/fonts_conversion'
-require_relative 'converter/less_conversion'
-require_relative 'converter/js_conversion'
-require_relative 'converter/logger'
-require_relative 'converter/network'
-    
-        # margin: a -b
-    # LESS: sets 2 values
-    # Sass: sets 1 value (a-b)
-    # This wraps a and -b so they evaluates to 2 values in Sass
-    def replace_calculation_semantics(file)
-      # split_prop_val.call('(@navbar-padding-vertical / 2) -@navbar-padding-horizontal')
-      # #=> ['(navbar-padding-vertical / 2)', '-navbar-padding-horizontal']
-      split_prop_val = proc { |val|
-        s         = CharStringScanner.new(val)
-        r         = []
-        buff      = ''
-        d         = 0
-        prop_char = %r([\$\w\-/\*\+%!])
-        while (token = s.scan_next(/([\)\(]|\s+|#{prop_char}+)/))
-          buff << token
-          case token
-            when '('
-              d += 1
-            when ')'
-              d -= 1
-              if d == 0
-                r << buff
-                buff = ''
-              end
-            when /\s/
-              if d == 0 && !buff.strip.empty?
-                r << buff
-                buff = ''
-              end
-          end
-        end
-        r << buff unless buff.empty?
-        r.map(&:strip)
-      }
-    
-          spec['main'] =
-          find_files.(File.join(Bootstrap.stylesheets_path, '_bootstrap.scss')) +
-          find_files.(Bootstrap.fonts_path) +
-          %w(assets/javascripts/bootstrap.js)
-    
-          private
-    
-            rows.each do |row|
-          line = row.values.each_with_index.map do |value, col|
-            value.to_s.ljust(col_widths[col])
-          end.join(' ').rstrip
-          line = color.colorize(line, row.color) if row.color
-          puts line
+    #   new_session_path(:user)      => new_user_session_path
+    #   session_path(:user)          => user_session_path
+    #   destroy_session_path(:user)  => destroy_user_session_path
+    #
+    #   new_password_path(:user)     => new_user_password_path
+    #   password_path(:user)         => user_password_path
+    #   edit_password_path(:user)    => edit_user_password_path
+    #
+    #   new_confirmation_path(:user) => new_user_confirmation_path
+    #   confirmation_path(:user)     => user_confirmation_path
+    #
+    # Those helpers are included by default to ActionController::Base.
+    #
+    # In case you want to add such helpers to another class, you can do
+    # that as long as this new class includes both url_helpers and
+    # mounted_helpers. Example:
+    #
+    #     include Rails.application.routes.url_helpers
+    #     include Rails.application.routes.mounted_helpers
+    #
+    module UrlHelpers
+      def self.remove_helpers!
+        self.instance_methods.map(&:to_s).grep(/_(url|path)$/).each do |method|
+          remove_method method
         end
       end
     
-      # Implemented by subclasses to define Rake tasks. Typically a plugin will call
-  # `eval_rakefile` to load Rake tasks from a separate .rake file.
-  #
-  # Example:
-  #
-  #   def define_tasks
-  #     eval_rakefile File.expand_path('../tasks.rake', __FILE__)
-  #   end
-  #
-  # For simple tasks, you can define them inline. No need for a separate file.
-  #
-  #   def define_tasks
-  #     desc 'Do something fantastic.'
-  #     task 'my_plugin:fantastic' do
-  #       ...
-  #     end
-  #   end
-  #
-  def define_tasks; end
+      def existing
+    existing_path = select(&File.method(:directory?))
+    # return nil instead of empty PATH, to unset environment variables
+    existing_path unless existing_path.empty?
+  end
     
-    desc 'Deploy a new release.'
-task :deploy do
-  set(:deploying, true)
-  %w{ starting started
-      updating updated
-      publishing published
-      finishing finished }.each do |task|
-    invoke 'deploy:#{task}'
+        def paragraphize(input)
+      '<p>#{input.lstrip.rstrip.gsub(/\n\n/, '</p><p>').gsub(/\n/, '<br/>')}</p>'
+    end
   end
 end
-task default: :deploy
+    
+      end
+    
+      if options.respond_to? 'keys'
+    options.each do |k,v|
+      unless v.nil?
+        v = v.join ',' if v.respond_to? 'join'
+        v = v.to_json if v.respond_to? 'keys'
+        output += ' data-#{k.sub'_','-'}='#{v}''
+      end
+    end
+  elsif options.respond_to? 'join'
+    output += ' data-value='#{config[key].join(',')}''
+  else
+    output += ' data-value='#{config[key]}''
+  end
+  output += '></#{tag}>'
+end
+    
+      # Removes trailing forward slash from a string for easily appending url segments
+  def strip_slash(input)
+    if input =~ /(.+)\/$|^\/$/
+      input = $1
+    end
+    input
+  end
+    
+                return if after_update_attributes
+    
+              if Cart::Update.call(order: @order, params: order_params).success?
+            user_id = params[:order][:user_id]
+            if current_api_user.has_spree_role?('admin') && user_id
+              @order.associate_user!(Spree.user_class.find(user_id))
+            end
+            respond_with(@order, default_template: :show)
+          else
+            invalid_resource!(@order)
+          end
+        end
+    
+            def index
+          @payments = @order.payments.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
+          respond_with(@payments)
+        end
+    
+            private
+    
+              @products = @products.distinct.page(params[:page]).per(params[:per_page])
+          expires_in 15.minutes, public: true
+          headers['Surrogate-Control'] = 'max-age=#{15.minutes}'
+          respond_with(@products)
+        end
+    
+            def cancel
+          @return_authorization = order.return_authorizations.accessible_by(current_ability, :update).find(params[:id])
+          if @return_authorization.cancel
+            respond_with @return_authorization, default_template: :show
+          else
+            invalid_resource!(@return_authorization)
+          end
+        end
+    
+              @original_shipment.transfer_to_location(@variant, @quantity, @stock_location)
+          render json: { success: true, message: Spree.t(:shipment_transfer_success) }, status: 201
+        end
+    
+              if params[:page] || params[:per_page]
+            @states = @states.page(params[:page]).per(params[:per_page])
+          end
