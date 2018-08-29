fac4c98d655ea31608c1c194623ce1f740b39ac7
SUNsung <weatherius@gmail.com>
2018-08-29 05:52:54 +0300

    Rewrite [53Ir.BQGxoT5g53Gaojm5jc5E53YMGZ34hPVlo53fNN.cpp] File in: 2018-08-29 05:52:54

--- cccccc/53Ir.BQGxoT5g53Gaojm5jc5E53YMGZ34hPVlo53fNN.cpp
+++ cccccc/53Ir.BQGxoT5g53Gaojm5jc5E53YMGZ34hPVlo53fNN.cpp
@@ -1,217 +1,179 @@
 
         
-            http://www.apache.org/licenses/LICENSE-2.0
+          /**
+   * @brief Returns the scalar loss associated with a top blob at a given index.
+   */
+  inline Dtype loss(const int top_index) const {
+    return (loss_.size() > top_index) ? loss_[top_index] : Dtype(0);
+  }
     
-    class DebugGraphDecorator : public DebugGraphDecoratorInterface {
+    /**
+ * @brief Provides base for data layers that feed blobs to the Net.
+ *
+ * TODO(dox): thorough documentation for Forward and proto params.
+ */
+template <typename Dtype>
+class BaseDataLayer : public Layer<Dtype> {
  public:
-  DebugGraphDecorator(const DebugOptions& debug_options)
-      : debug_options_(debug_options) {}
-  virtual ~DebugGraphDecorator() {}
+  explicit BaseDataLayer(const LayerParameter& param);
+  // LayerSetUp: implements common data layer setup functionality, and calls
+  // DataLayerSetUp to do special data layer setup for individual layer types.
+  // This method may not be overridden except by the BasePrefetchingDataLayer.
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {}
+  // Data layers have no bottoms, so reshaping is trivial.
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {}
     }
     
-    void PartialRunMgr::ExecutorDone(int step_id, const Status& executor_status) {
-  StatusCallback done;
-  Status callback_status;
-  {
-    mutex_lock l(mu_);
-    auto run_it = step_id_to_partial_run_.find(step_id);
-    if (run_it == step_id_to_partial_run_.end()) {
-      return;
-    }
-    // If we found the partial_run, we call the final callback, if it
-    // exists.
-    // It is guaranteed that run_it->second->final_callback is left empty
-    // after the std::move call.
-    done = std::move(run_it->second->final_callback);
-    if (!executor_status.ok()) {
-      run_it->second->final_status = executor_status;
-    }
-    callback_status = run_it->second->final_status;
-    run_it->second->executor_done = true;
-  }
-  if (done != nullptr) {
-    done(callback_status);
-    mutex_lock l(mu_);
-    step_id_to_partial_run_.erase(step_id);
-  }
-}
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    template <typename T>
-void DynamicStitchGPUImpl(const Eigen::GpuDevice& gpu_device,
-                          const int32 slice_size, const int32 first_dim_size,
-                          const CudaDeviceArrayStruct<int>& input_indices,
-                          const CudaDeviceArrayStruct<const T*>& input_ptrs,
-                          T* output) {
-  const int32 output_size = first_dim_size * slice_size;
-  auto config = GetCudaLaunchConfig(output_size, gpu_device);
-    }
-    
-    // TODO: Enable GPU support for angle op after resolving
-// build failures on GPU (See #10643 for context).
-#if 0 && GOOGLE_CUDA
-REGISTER_COMPLEX(GPU, float, complex64);
-REGISTER_COMPLEX(GPU, double, complex128);
-#endif
-    
-    
-    {}  // namespace leveldb
+    #endif  // CAFFE_CUDNN_DECONV_LAYER_HPP_
 
     
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
     
-    {}  // namespace leveldb
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
     
     
-    {  // No copying allowed
-  DBIter(const DBIter&);
-  void operator=(const DBIter&);
-};
+    {}  // namespace caffe
     
-    #ifndef STORAGE_LEVELDB_DB_FILENAME_H_
-#define STORAGE_LEVELDB_DB_FILENAME_H_
+    #endif
+
     
-    static void DeleteEntry(const Slice& key, void* value) {
-  TableAndFile* tf = reinterpret_cast<TableAndFile*>(value);
-  delete tf->table;
-  delete tf->file;
-  delete tf;
-}
-    
-    struct FileMetaData {
-  int refs;
-  int allowed_seeks;          // Seeks allowed until compaction
-  uint64_t number;
-  uint64_t file_size;         // File size in bytes
-  InternalKey smallest;       // Smallest internal key served by table
-  InternalKey largest;        // Largest internal key served by table
+    struct APCCollection {
+  static APCHandle::Pair Make(const ObjectData*,
+                              APCHandleLevel level,
+                              bool unserializeObj);
+  static void Delete(APCHandle*);
     }
     
+    void Config::ParseIniFile(const std::string &filename, IniSettingMap &ini,
+                          const bool constants_only /* = false */,
+                          const bool is_system /* = true */ ) {
+    std::ifstream ifs(filename);
+    std::string str((std::istreambuf_iterator<char>(ifs)),
+                    std::istreambuf_iterator<char>());
+    std::string with_includes;
+    Config::ReplaceIncludesWithIni(filename, str, with_includes);
+    Config::SetParsedIni(ini, with_includes, filename, constants_only,
+                         is_system);
+}
     
-    {}  // namespace leveldb
     
-    static std::string PrintContents(WriteBatch* b) {
-  InternalKeyComparator cmp(BytewiseComparator());
-  MemTable* mem = new MemTable(cmp);
-  mem->Ref();
-  std::string state;
-  Status s = WriteBatchInternal::InsertInto(b, mem);
-  int count = 0;
-  Iterator* iter = mem->NewIterator();
-  for (iter->SeekToFirst(); iter->Valid(); iter->Next()) {
-    ParsedInternalKey ikey;
-    ASSERT_TRUE(ParseInternalKey(iter->key(), &ikey));
-    switch (ikey.type) {
-      case kTypeValue:
-        state.append('Put(');
-        state.append(ikey.user_key.ToString());
-        state.append(', ');
-        state.append(iter->value().ToString());
-        state.append(')');
-        count++;
+    {    assertx(data == comma || data == semi);
+    // eat parameters, and figure out if we have ';base64'
+    while (semi && (data == semi)) {
+      data++;
+      meta_len--;
+      char* equals = (char*)memchr(data, '=', meta_len);
+      semi = (char*)memchr(data, ';', meta_len);
+      if (!equals || (semi && semi < data)) {
+        // no equals, so either 'base64' or its bad
+        if (meta_len != sizeof('base64') - 1 ||
+            memcmp(data, 'base64', sizeof('base64')-1)) {
+          raise_warning('rfc2396: invalid parameter');
+          return nullptr;
+        }
+        // it's 'base64', we're done
+        base64 = true;
+        meta_len -= sizeof('base64') - 1;
+        data += sizeof('base64') - 1;
         break;
-      case kTypeDeletion:
-        state.append('Delete(');
-        state.append(ikey.user_key.ToString());
-        state.append(')');
-        count++;
-        break;
+      }
+      // there's a parameter
+      if (semi) {
+        meta_len -= semi - data + 1;
+        data = semi;
+      } /* else, we're done with meta */
     }
-    state.append('@');
-    state.append(NumberToString(ikey.sequence));
   }
-  delete iter;
-  if (!s.ok()) {
-    state.append('ParseError()');
-  } else if (count != WriteBatchInternal::Count(b)) {
-    state.append('CountMismatch()');
-  }
-  mem->Unref();
-  return state;
-}
+  data = comma + 1;
+  data_len -= 1;
+  String decoded;
     
-      void PrintHeader() {
-    const int kKeySize = 16;
-    PrintEnvironment();
-    fprintf(stdout, 'Keys:       %d bytes each\n', kKeySize);
-    fprintf(stdout, 'Values:     %d bytes each\n', FLAGS_value_size);
-    fprintf(stdout, 'Entries:    %d\n', num_);
-    fprintf(stdout, 'RawSize:    %.1f MB (estimated)\n',
-            ((static_cast<int64_t>(kKeySize + FLAGS_value_size) * num_)
-             / 1048576.0));
-    PrintWarnings();
-    fprintf(stdout, '------------------------------------------------\n');
-  }
-    
-    #endif  // STORAGE_LEVELDB_HELPERS_MEMENV_MEMENV_H_
-
-    
-    #endif  // STORAGE_LEVELDB_INCLUDE_DUMPFILE_H_
-
-    
-    #endif // BOOST_ASIO_DETAIL_ARRAY_FWD_HPP
-
-    
-    #include <boost/asio/detail/pop_options.hpp>
+      dirent *e;
+  while ((e = readdir(dir))) {
+    char *ename = e->d_name;
+    }
     
     
-    {private:
-  int descriptor_;
-  ConstBufferSequence buffers_;
+    {/// KafkaTopicsConfigParserPlugin extracts, updates, and parses Kafka topic
+/// configurations from Osquery's configurations.
+class KafkaTopicsConfigParserPlugin : public ConfigParserPlugin {
+ public:
+  std::vector<std::string> keys() const override;
+  Status update(const std::string& source, const ParserConfig& config) override;
 };
-    
-    template <typename Handler>
-inline void deallocate(void* p, std::size_t s, Handler& h)
-{
-#if !defined(BOOST_ASIO_HAS_HANDLER_HOOKS)
-  ::operator delete(p);
-#else
-  using boost::asio::asio_handler_deallocate;
-  asio_handler_deallocate(p, s, boost::asio::detail::addressof(h));
-#endif
-}
-    
-    template <typename Function, typename Context>
-inline void invoke(const Function& function, Context& context)
-{
-#if !defined(BOOST_ASIO_HAS_HANDLER_HOOKS)
-  Function tmp(function);
-  tmp();
-#else
-  using boost::asio::asio_handler_invoke;
-  asio_handler_invoke(function, boost::asio::detail::addressof(context));
-#endif
-}
-    
-      {
-    auto nobody = getpwnam('nobody');
-    auto dropper = DropPrivileges::get();
-    dropper->dropTo(nobody->pw_uid, nobody->pw_gid);
-    PlatformFile fd(file_path, PF_OPEN_EXISTING | PF_READ);
-    EXPECT_FALSE(fd.isValid());
-  }
-    
-    
-    {
-    {    int code = 0;
-    EXPECT_TRUE(getProcessExitCode(*process, code));
-    EXPECT_EQ(code, EXTENSION_SUCCESS_CODE);
-  }
-}
-    
-      struct tm result;
-  localtime_r((time_t*)&epoch, &result);
-    
-    TEST_F(RocksDBDatabasePluginTests, test_rocksdb_loglevel) {
-  // Make sure a log file was created.
-  EXPECT_FALSE(pathExists(path_ + '/LOG'));
-    }
-    
-      void stop() override;
-    
-    #include <libudev.h>
+} // namespace osquery
+
     
     namespace osquery {
     }
+    
+    #include <gtest/gtest.h>
+    
+      // Now test inclusive bounds.
+  struct ConstraintList cl3;
+  constraint = Constraint(LESS_THAN_OR_EQUALS);
+  constraint.expr = '1000';
+  cl3.add(constraint);
+  constraint = Constraint(GREATER_THAN_OR_EQUALS);
+  constraint.expr = '1';
+  cl3.add(constraint);
+    
+      /// Paths to autoload extensions.
+  std::vector<std::string> extensions_paths_;
+    
+    #include 'osquery/events/linux/inotify.h'
+    
+      struct audit_reply reply;
+  reply.type = 1;
+  reply.len = message.size();
+  reply.message = (char*)malloc(sizeof(char) * (message.size() + 1));
+  memset((void*)reply.message, 0, message.size() + 1);
+  memcpy((void*)reply.message, message.c_str(), message.size());
+    
+    
+    {  // Remove mock directory structure.
+  tearDownMockFileStructure();
+  EventFactory::deregisterEventPublisher('inotify');
+}
+    
+        std::string content;
+    setDatabaseValue(kPersistentSettings, 'complex_example', '1');
+    if (getDatabaseValue(kPersistentSettings, 'complex_example', content)) {
+      r['database_test'] = content;
+    }
+    
+    void BENCHFUN(fillCtor)(int iters, int size) {
+  FOR_EACH_RANGE (i, 0, iters) {
+    VECTOR v(size_t(size), randomObject<VECTOR::value_type>());
+    doNotOptimizeAway(&v);
+  }
+}
+BENCHMARK_PARAM(BENCHFUN(fillCtor), 16)
+BENCHMARK_PARAM(BENCHFUN(fillCtor), 128)
+BENCHMARK_PARAM(BENCHFUN(fillCtor), 1024)
+    
+    
+    {  constexpr auto z = 'now is the time for all good llamas'_fs64;
+  static_assert(
+      std::is_same<decltype(z), const folly::FixedString<64>>::value, '');
+  static_assert(z.size() == 35u, '');
+  static_assert(z == 'now is the time for all good llamas', '');
+}
+    
+    int main(int argc, char *argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  return RUN_ALL_TESTS();
+}
