36325040f892f65ecd640c4f687124b7ffb9dbe6
SUNsung <weatherius@gmail.com>
2018-08-29 21:02:25 +0300

    Rewrite [49LWFspIchO7U49XHqs9LQ3gw249bkftXqhWEJM491h..rb] File in: 2018-08-29 21:02:24

--- ruby/49LWFspIchO7U49XHqs9LQ3gw249bkftXqhWEJM491h..rb
+++ ruby/49LWFspIchO7U49XHqs9LQ3gw249bkftXqhWEJM491h..rb
@@ -1,54 +1,104 @@
 
         
-              # If you don't want confirmation to be sent on create, neither a code
-      # to be generated, call skip_confirmation!
-      def skip_confirmation!
-        self.confirmed_at = Time.now.utc
+        module ActionView
+  module Helpers
+    module Tags # :nodoc:
+      class TextField < Base # :nodoc:
+        include Placeholderable
+    
+        module ClassMethods
+      def inherited(klass) # :nodoc:
+        super
+        klass._write_layout_method
       end
     
-      # GET /resource/unlock?unlock_token=abcdef
-  def show
-    self.resource = resource_class.unlock_access_by_token(params[:unlock_token])
-    yield resource if block_given?
-    
-      def initialize(repo: 'twbs/bootstrap', branch: 'master', save_to: {}, cache_path: 'tmp/converter-cache-bootstrap')
-    @logger     = Logger.new
-    @repo       = repo
-    @branch     = branch || 'master'
-    @branch_sha = get_branch_sha
-    @cache_path = cache_path
-    @repo_url   = 'https://github.com/#@repo'
-    @save_to    = {
-        js:    'assets/javascripts/bootstrap',
-        scss:  'assets/stylesheets/bootstrap',
-        fonts: 'assets/fonts/bootstrap'}.merge(save_to)
-  end
-    
-      def as_json(options={})
-    {
-      poll_id:             id,
-      post_id:             status_message.id,
-      question:            question,
-      poll_answers:        poll_answers,
-      participation_count: participation_count
-    }
-  end
-    
-        def index
-      pods_json = PodPresenter.as_collection(Pod.all)
-    
-            def_node_matcher :only_truthiness_matters?, <<-PATTERN
-          ^({if while until case while_post until_post} equal?(%0) ...)
-        PATTERN
-    
-            def parent_block_node(node)
-          node.each_ancestor(:block).first
-        end
+        def render_partial(event)
+      info do
+        message = '  Rendered #{from_rails_root(event.payload[:identifier])}'.dup
+        message << ' within #{from_rails_root(event.payload[:layout])}' if event.payload[:layout]
+        message << ' (#{event.duration.round(1)}ms)'
+        message << ' #{cache_message(event.payload)}' unless event.payload[:cache_hit].nil?
+        message
       end
     end
-  end
-end
-
     
-            def immutable_literal?(node)
-          return true if node.immutable_literal?
+    def built_as_bottle?(f)
+  return false unless f.installed?
+  tab = Tab.for_keg(f.installed_prefix)
+  tab.built_as_bottle
+end
+    
+      # True if a {Formula} is being built with {Formula.devel} instead of {Formula.stable}.
+  # <pre>args << '--some-beta' if build.devel?</pre>
+  def devel?
+    include? 'devel'
+  end
+    
+      def plist_caveats
+    s = []
+    if f.plist || (keg && keg.plist_installed?)
+      destination = if f.plist_startup
+        '/Library/LaunchDaemons'
+      else
+        '~/Library/LaunchAgents'
+      end
+    
+            begin
+          f = Formulary.from_rack(HOMEBREW_CELLAR/name)
+        rescue FormulaUnavailableError, TapFormulaAmbiguityError, TapFormulaWithOldnameAmbiguityError
+          next
+        end
+    
+      private
+    
+      def kernel
+    `uname -m`.chomp
+  end
+    
+      # Use this method to generate standard caveats.
+  def standard_instructions(home_name, home_value = libexec)
+    <<-EOS.undent
+      Before you can use these tools you must export some variables to your $SHELL.
+    
+        def register_lotus
+      Lotus::Assets.sources << assets_path
+    end
+    
+      # Generate digests for assets URLs.
+  config.assets.digest = true
+    
+      config.active_support.test_order = :random
+    
+    When /^I (?:sign|log) in manually as '([^']*)' with password '([^']*)'( on the mobile website)?$/ \
+do |username, password, mobile|
+  @me = User.find_by_username(username)
+  @me.password ||= password
+  manual_login
+  confirm_login mobile
+end
+    
+    Then /^I should see an image in the publisher$/ do
+  photo_in_publisher.should be_present
+end
+    
+          @conv2 = Conversation.create(hash)
+      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv2.id)
+             .increase_unread(alice)
+    
+        it 'returns a 404 for a post not visible to the user' do
+      sign_in eve
+      expect {
+        get :index, params: {post_id: @message.id}
+      }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+    
+        # Outputs a list of categories as comma-separated <a> links. This is used
+    # to output the category list for each post on a category page.
+    #
+    #  +categories+ is the list of categories to format.
+    #
+    # Returns string
+    #
+    def category_links(categories)
+      categories.sort.map { |c| category_link c }.join(', ')
+    end
