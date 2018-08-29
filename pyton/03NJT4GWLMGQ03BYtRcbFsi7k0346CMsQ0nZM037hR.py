a750f8bf3e7cea64ec425668e60d344df9019d9b
SUNsung <weatherius@gmail.com>
2018-08-29 09:40:18 +0300

    Rewrite [03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py] File in: 2018-08-29 09:40:17

--- pyton/03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py
+++ pyton/03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py
@@ -1,279 +1,60 @@
 
         
-            '''
-    is_windows = is_windows
-    config_dir = DEFAULT_CONFIG_DIR
-    stdin = sys.stdin
-    stdin_isatty = stdin.isatty()
-    stdin_encoding = None
-    stdout = sys.stdout
-    stdout_isatty = stdout.isatty()
-    stdout_encoding = None
-    stderr = sys.stderr
-    stderr_isatty = stderr.isatty()
-    colors = 256
-    if not is_windows:
-        if curses:
-            try:
-                curses.setupterm()
-                colors = curses.tigetnum('colors')
-            except curses.error:
-                pass
-    else:
-        # noinspection PyUnresolvedReferences
-        import colorama.initialise
-        stdout = colorama.initialise.wrap_stream(
-            stdout, convert=None, strip=None,
-            autoreset=True, wrap=True
-        )
-        stderr = colorama.initialise.wrap_stream(
-            stderr, convert=None, strip=None,
-            autoreset=True, wrap=True
-        )
-        del colorama
+            def start_requests(self):
+        qargs = {'total': self.total, 'show': self.show}
+        url = '{}?{}'.format(self.baseurl, urlencode(qargs, doseq=1))
+        return [scrapy.Request(url, dont_filter=True)]
     
-        if explicit_json and body and (not lexer or isinstance(lexer, TextLexer)):
-        # JSON response with an incorrect Content-Type?
-        try:
-            json.loads(body)  # FIXME: the body also gets parsed in json.py
-        except ValueError:
-            pass  # Nope
-        else:
-            lexer = pygments.lexers.get_lexer_by_name('json')
+        def syntax(self):
+        return '[-v]'
     
-        name = 'Basic HTTP auth'
-    auth_type = 'basic'
+                    # EOF Transition to accept state?
+                if c == EOF and self.eof[s] >= 0:
+                    #print 'EOF Transition to accept state %d' \
+                    #  % self.accept[self.eof[s]]
+                    return self.accept[self.eof[s]]
     
-        def get_formatters_grouped(self):
-        groups = {}
-        for group_name, group in groupby(
-                self.get_formatters(),
-                key=lambda p: getattr(p, 'group_name', 'format')):
-            groups[group_name] = list(group)
-        return groups
+        Note text is not an actual property of the return value, it is computed
+    from start and stop using the input stream's toString() method.  I
+    could add a ctor to this so that we can pass in and store the input
+    stream, but I'm not sure we want to do that.  It would seem to be undefined
+    to get the .text property anyway if the rule matches tokens from multiple
+    input streams.
     
     
-def test_credentials_in_url(httpbin_both):
-    url = add_auth(httpbin_both.url + '/basic-auth/user/password',
-                   auth='user:password')
-    r = http('GET', url)
-    assert HTTP_OK in r
-    assert r.json == {'authenticated': True, 'user': 'user'}
-    
-            # unexpected range
-        pytest.raises(ContentRangeError, parse, 'bytes 100-199/*', 99)
-    
-    
-class HasKey(PostgresSimpleLookup):
-    lookup_name = 'has_key'
-    operator = '?'
-    prepare_rhs = False
-    
-        def __init__(self):
-        self.name = 'hstore'
-    
-            r = None
-        try:
-            r = Redirect.objects.get(site=current_site, old_path=full_path)
-        except Redirect.DoesNotExist:
-            pass
-        if r is None and settings.APPEND_SLASH and not request.path.endswith('/'):
-            try:
-                r = Redirect.objects.get(
-                    site=current_site,
-                    old_path=request.get_full_path(force_append_slash=True),
-                )
-            except Redirect.DoesNotExist:
-                pass
-        if r is not None:
-            if r.new_path == '':
-                return self.response_gone_class()
-            return self.response_redirect_class(r.new_path)
-    
-    from django.contrib.sessions.backends.base import (
-    CreateError, SessionBase, UpdateError,
-)
-from django.core.exceptions import SuspiciousOperation
-from django.db import DatabaseError, IntegrityError, router, transaction
-from django.utils import timezone
-from django.utils.functional import cached_property
-    
-        def cycle_key(self):
+    def setCharPositionInLine(self, pos):
         '''
-        Keep the same data but with a new key. Call save() and it will
-        automatically save a cookie with a new key at the end of the request.
+        Using setter/getter methods is deprecated. Use o.charPositionInLine
+        instead.
         '''
-        self.save()
+        self.charPositionInLine = pos
     
-    
-class Session(AbstractBaseSession):
+    def _save_cheatsheet(topic_name, cheatsheet):
     '''
-    Django provides full support for anonymous sessions. The session
-    framework lets you store and retrieve arbitrary data on a
-    per-site-visitor basis. It stores data on the server side and
-    abstracts the sending and receiving of cookies. Cookies contain a
-    session ID -- not the data itself.
+    Save posted cheat sheet `cheatsheet` with `topic_name`
+    in the spool directory
+    '''
     
-        def _print_response(self, response, opts):
-        if opts.headers:
-            self._print_headers(response.request.headers, b'>')
-            print('>')
-            self._print_headers(response.headers, b'<')
-        else:
-            self._print_bytes(response.body)
+        if 'topic' in request.args:
+        return redirect('/%s' % request.args.get('topic'))
     
-        def run(self, args, opts):
-        if len(args) != 1:
-            raise UsageError()
-        filename = args[0]
-        if not os.path.exists(filename):
-            raise UsageError('File not found: %s\n' % filename)
-        try:
-            module = _import_file(filename)
-        except (ImportError, ValueError) as e:
-            raise UsageError('Unable to load %r: %s\n' % (filename, e))
-        spclasses = list(iter_spider_classes(module))
-        if not spclasses:
-            raise UsageError('No spider found in file: %s\n' % filename)
-        spidercls = spclasses.pop()
+        def read(self, filename):
+        '''
+        Read template from `filename`
+        '''
+        with open(filename) as f:
+            self._mode = 'page'
+            for line in f.readlines():
+                line = line.rstrip('\n')
+                if line.startswith('==[') and line.endswith(']=='):
+                    self._process_line(line[3:-3].strip())
+                    continue
     
-        def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('--verbose', '-v', dest='verbose', action='store_true',
-            help='also display twisted/python/platform info (useful for bug reports)')
-    
-            html = get_content(endpoint, headers= fake_header_id)
-        html_json = json.loads(html)
-    
-    from .theplatform import theplatform_download_by_pid
-    
-    
-class CNTV(VideoExtractor):
-    name = 'CNTV.com'
-    stream_types = [
-        {'id': '1', 'video_profile': '1280x720_2000kb/s', 'map_to': 'chapters4'},
-        {'id': '2', 'video_profile': '1280x720_1200kb/s', 'map_to': 'chapters3'},
-        {'id': '3', 'video_profile': '640x360_850kb/s', 'map_to': 'chapters2'},
-        {'id': '4', 'video_profile': '480x270_450kb/s', 'map_to': 'chapters'},
-        {'id': '5', 'video_profile': '320x180_200kb/s', 'map_to': 'lowChapters'},
-    ]
-    
-    	xml = get_html('http://www.ehow.com/services/video/series.xml?demand_ehow_videoid=%s' % vid)
-    
-	from xml.dom.minidom import parseString
-	doc = parseString(xml)
-	tab = doc.getElementsByTagName('related')[0].firstChild
-    
-    from ..common import *
-import json
-    
-    #----------------------------------------------------------------------
-def fc2video_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    '''wrapper'''
-    #'http://video.fc2.com/en/content/20151021bTVKnbEw'
-    #'http://xiaojiadianvideo.asia/content/20151021bTVKnbEw'
-    #'http://video.fc2.com/ja/content/20151021bTVKnbEw'
-    #'http://video.fc2.com/tw/content/20151021bTVKnbEw'
-    hostname = urlparse(url).hostname
-    if not ('fc2.com' in hostname or 'xiaojiadianvideo.asia' in hostname):
-        return False
-    upid = match1(url, r'.+/content/(\w+)')
-    
-    def get_url_of_largest(info, api_key, size):
-    if info['media'] == 'photo':
-        sizes = size_suffixes
-        if size in sizes:
-            sizes = sizes[sizes.index(size):]
-        for suffix in sizes:
-            if 'url_' + suffix in info:
-                return info['url_' + suffix].replace('\\', '')
-        return None
-    else:
-        return get_orig_video_source(api_key, info['id'], info['secret'])
-    
-    
-if __name__ == '__main__':
-    # Tornado configures logging.
-    options.parse_command_line()
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
-
-    
-        def delete(self, bucket_name):
-        path = os.path.abspath(os.path.join(
-            self.application.directory, bucket_name))
-        if not path.startswith(self.application.directory) or \
-           not os.path.isdir(path):
-            raise web.HTTPError(404)
-        if len(os.listdir(path)) > 0:
-            raise web.HTTPError(403)
-        os.rmdir(path)
-        self.set_status(204)
-        self.finish()
-    
-    
-def main():
-    parse_command_line()
-    app = Application([('/', ChunkHandler)])
-    app.listen(options.port, address='127.0.0.1')
-    
-    tmpl = Template('''\
-<!doctype html>
-<html>
-  <head>
-    <title>{{ page_title }}</title>
-  </head>
-  <body>
-    <div class='header'>
-      <h1>{{ page_title }}</h1>
-    </div>
-    <ul class='navigation'>
-    {% for href, caption in [ \
-        ('index.html', 'Index'), \
-        ('downloads.html', 'Downloads'), \
-        ('products.html', 'Products') \
-      ] %}
-      <li><a href='{{ href }}'>{{ caption }}</a></li>
-    {% end %}
-    </ul>
-    <div class='table'>
-      <table>
-      {% for row in table %}
-        <tr>
-        {% for cell in row %}
-          <td>{{ cell }}</td>
-        {% end %}
-        </tr>
-      {% end %}
-      </table>
-    </div>
-  </body>
-</html>\
-''')
-    
-    coverage_skip_undoc_in_source = True
-coverage_ignore_modules = [
-    'tornado.platform.asyncio',
-    'tornado.platform.caresresolver',
-    'tornado.platform.twisted',
-]
-# I wish this could go in a per-module file...
-coverage_ignore_classes = [
-    # tornado.gen
-    'Runner',
-    
-    
-def main():
-    if not os.path.exists(TMPDIR):
-        os.mkdir(TMPDIR)
-    os.chdir(TMPDIR)
-    for exe, url in PYTHON_VERSIONS:
-        if os.path.exists(exe):
-            print('%s already exists, skipping' % exe)
-            continue
-        print('Installing %s' % url)
-        filename = download_to_cache(url)
-        # http://blog.jaraco.com/2012/01/how-i-install-python-on-windows.html
-        subprocess.check_call(['msiexec', '/i', filename,
-                               'ALLUSERS=1', '/passive'])
+        @staticmethod
+    def _cut_block(block, start_block=False):
+        answer = block[2:-1]
+        if answer[0].split() == '':
+            answer = answer[1:]
+        if answer[-1].split() == '':
+            answer = answer[:1]
+        return answer
