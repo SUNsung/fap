19aa0dd7fe6771818d73a4880bd36bfbc5c4de2e
SUNsung <weatherius@gmail.com>
2018-08-29 08:28:31 +0300

    Rewrite [02T0c7WavwIGw02CtSzNKYI9ew02kuvZk7cIehI02Onz.py] File in: 2018-08-29 08:28:30

--- pyton/02T0c7WavwIGw02CtSzNKYI9ew02kuvZk7cIehI02Onz.py
+++ pyton/02T0c7WavwIGw02CtSzNKYI9ew02kuvZk7cIehI02Onz.py
@@ -1,152 +1,74 @@
 
         
-            model.fit(temporal_x_train, temporal_y_train, batch_size=batch_size,
-              epochs=epochs // 3, verbose=0,
-              sample_weight=temporal_sample_weight)
-    model.fit(temporal_x_train, temporal_y_train, batch_size=batch_size,
-              epochs=epochs // 3, verbose=0,
-              sample_weight=temporal_sample_weight,
-              validation_split=0.1)
+        
+parser = youtube_dl.parseOpts()[0]
+build_completion(parser)
+
     
-        x = Input(shape=(1,))
-    y = inner_model(x)
-    outer_model = Model(x, y)
-    assert outer_model.trainable_weights == inner_model.trainable_weights
-    inner_model.trainable = False
-    assert outer_model.trainable_weights == []
-    inner_model.trainable = True
-    inner_model.layers[-1].trainable = False
-    assert outer_model.trainable_weights == []
+    import rsa
+import json
+from binascii import hexlify
+    
+    import io
+import sys
+import re
+    
+        with io.open(outfile, 'w', encoding='utf-8') as outf:
+        outf.write(out)
+    
+    py2exe_console = [{
+    'script': './youtube_dl/__main__.py',
+    'dest_base': 'youtube-dl',
+    'version': __version__,
+    'description': DESCRIPTION,
+    'comments': LONG_DESCRIPTION,
+    'product_name': 'youtube-dl',
+    'product_version': __version__,
+}]
+    
+    # Check imported dependencies for compatibility.
+try:
+    check_compatibility(urllib3.__version__, chardet.__version__)
+except (AssertionError, ValueError):
+    warnings.warn('urllib3 ({0}) or chardet ({1}) doesn't match a supported '
+                  'version!'.format(urllib3.__version__, chardet.__version__),
+                  RequestsDependencyWarning)
     
     
-@keras_test
-def test_temporal_classification():
+def test_idna_without_version_attribute(mocker):
+    '''Older versions of IDNA don't provide a __version__ attribute, verify
+    that if we have such a package, we don't blow up.
     '''
-    Classify temporal sequences of float numbers
-    of length 3 into 2 classes using
-    single layer of GRU units and softmax applied
-    to the last activations of the units
+    mocker.patch('requests.help.idna', new=None)
+    assert info()['idna'] == {'version': ''}
+    
+            # the data has all been read
+        assert prep.body.read() == b''
+    
+        :param filepath: Optional filepath the the blns.txt file
+    :returns: The list of naughty strings
     '''
-    np.random.seed(1337)
-    (x_train, y_train), (x_test, y_test) = get_test_data(num_train=200,
-                                                         num_test=20,
-                                                         input_shape=(3, 4),
-                                                         classification=True,
-                                                         num_classes=2)
-    y_train = to_categorical(y_train)
-    y_test = to_categorical(y_test)
     
-    # try using different optimizers and different optimizer configs
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+    # If true, keep warnings as 'system message' paragraphs in the built
+# documents.
+#keep_warnings = False
     
-        num_train_samples = 50000
+    for image in TEST_IMAGES:
+    size = image.split('-')[1].split('.')[0]
+    print('Timings at {}:'.format(size))
     
-        def __init__(self,
-                 return_sequences=False,
-                 return_state=False,
-                 go_backwards=False,
-                 stateful=False,
-                 **kwargs):
-        if K.backend() != 'tensorflow':
-            raise RuntimeError('CuDNN RNNs are only available '
-                               'with the TensorFlow backend.')
-        super(RNN, self).__init__(**kwargs)
-        self.return_sequences = return_sequences
-        self.return_state = return_state
-        self.go_backwards = go_backwards
-        self.stateful = stateful
-        self.supports_masking = False
-        self.input_spec = [InputSpec(ndim=3)]
-        if hasattr(self.cell.state_size, '__len__'):
-            state_size = self.cell.state_size
-        else:
-            state_size = [self.cell.state_size]
-        self.state_spec = [InputSpec(shape=(None, dim))
-                           for dim in state_size]
-        self.constants_spec = None
-        self._states = None
-        self._num_constants = None
+        # Determine how many neighbors to use for weighting in the KNN classifier
+    if n_neighbors is None:
+        n_neighbors = int(round(math.sqrt(len(X))))
+        if verbose:
+            print('Chose n_neighbors automatically:', n_neighbors)
     
+            face_names.append(name)
     
-# split train/test data
-def split_data(x, y, ratio=0.8):
-    to_train = int(input_len * ratio)
-    # tweak to match with batch_size
-    to_train -= to_train % batch_size
+    # To run this, you need a Raspberry Pi 2 (or greater) with face_recognition and
+# the picamera[array] module installed.
+# You can follow this installation instructions to get your RPi set up:
+# https://gist.github.com/ageitgey/1ac8dbe8572f3f533df6269dab35df65
     
-        time_ridge = np.empty(n_iter)
-    time_ols = np.empty(n_iter)
-    time_lasso = np.empty(n_iter)
-    
-        op.add_option('--n-components',
-                  dest='n_components', default='auto',
-                  help='Size of the random subspace.'
-                       ' ('auto' or int > 0)')
-    
-    
-def _get_git_revision():
-    try:
-        revision = subprocess.check_output(REVISION_CMD.split()).strip()
-    except (subprocess.CalledProcessError, OSError):
-        print('Failed to execute git to get revision')
-        return None
-    return revision.decode('utf-8')
-    
-        print('Decompressing %s' % ARCHIVE_NAME)
-    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
-        archive.extractall(path='.')
-    os.remove(ARCHIVE_NAME)
-
-    
-        # TASK: print the mean and std for each candidate along with the parameter
-    # settings for all the candidates explored by grid search.
-    n_candidates = len(grid_search.cv_results_['params'])
-    for i in range(n_candidates):
-        print(i, 'params - %s; mean - %0.2f; std - %0.2f'
-                 % (grid_search.cv_results_['params'][i],
-                    grid_search.cv_results_['mean_test_score'][i],
-                    grid_search.cv_results_['std_test_score'][i]))
-    
-            self.concurrent -= 1
-        return ''
-    
-        def run(self, args, opts):
-        # load contracts
-        contracts = build_component_list(self.settings.getwithbase('SPIDER_CONTRACTS'))
-        conman = ContractsManager(load_object(c) for c in contracts)
-        runner = TextTestRunner(verbosity=2 if opts.verbose else 1)
-        result = TextTestResult(runner.stream, runner.descriptions, runner.verbosity)
-    
-        @property
-    def templates_dir(self):
-        _templates_base_dir = self.settings['TEMPLATES_DIR'] or \
-            join(scrapy.__path__[0], 'templates')
-        return join(_templates_base_dir, 'spiders')
-
-    
-            if not aws_access_key_id:
-            aws_access_key_id = settings['AWS_ACCESS_KEY_ID']
-        if not aws_secret_access_key:
-            aws_secret_access_key = settings['AWS_SECRET_ACCESS_KEY']
-    
-    from scrapy import signals
-    
-            spider.crawler.stats.inc_value('dupefilter/filtered', spider=spider)
-
-    
-        def error_count(self, failure, response, spider):
-        self.counter['errorcount'] += 1
-        if self.counter['errorcount'] == self.close_on['errorcount']:
-            self.crawler.engine.close_spider(spider, 'closespider_errorcount')
-    
-    
-    def _read_image(self):
-        return np.array(Image.open(self.image_dir))
-
-    
-    update = True
-    
-        def adb_path(self):
-        return self.adb_path
+            # Clear the frames array to start the next batch
+        frames = []
