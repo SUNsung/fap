7bbed7940672ad6f49e9da548a6bdddf17b4a454
SUNsung <weatherius@gmail.com>
2018-08-29 07:23:12 +0300

    Rewrite [062ntJTQT1O6A065RJmINIknuE06hdngPWcSu6606vEl.py] File in: 2018-08-29 07:23:12

--- pyton/062ntJTQT1O6A065RJmINIknuE06hdngPWcSu6606vEl.py
+++ pyton/062ntJTQT1O6A065RJmINIknuE06hdngPWcSu6606vEl.py
@@ -1,147 +1,171 @@
 
         
-        entry_template = textwrap.dedent('''
-    <entry>
-        <id>https://yt-dl.org/feed/youtube-dl-updates-feed/youtube-dl-@VERSION@</id>
-        <title>New version @VERSION@</title>
-        <link href='http://rg3.github.io/youtube-dl' />
-        <content type='xhtml'>
-            <div xmlns='http://www.w3.org/1999/xhtml'>
-                Downloads available at <a href='https://yt-dl.org/downloads/@VERSION@/'>https://yt-dl.org/downloads/@VERSION@/</a>
-            </div>
-        </content>
-        <author>
-            <name>The youtube-dl maintainers</name>
-        </author>
-        <updated>@TIMESTAMP@</updated>
-    </entry>
-    ''')
+            # Then we cluster the lines together as blocks
+    # Each block represents a collection of lines that should be sorted
+    # This was done by assuming only links ([...](...)) are meant to be sorted
+    # Clustering is done by indentation
+    blocks = []
+    last_indent = None
+    for line in read_me:
+        s_line = line.lstrip()
+        indent = len(line) - len(s_line)
     
-        ie_htmls = []
-    for ie in youtube_dl.list_extractors(age_limit=None):
-        ie_html = '<b>{}</b>'.format(ie.IE_NAME)
-        ie_desc = getattr(ie, 'IE_DESC', None)
-        if ie_desc is False:
-            continue
-        elif ie_desc is not None:
-            ie_html += ': {}'.format(ie.IE_DESC)
-        if not ie.working():
-            ie_html += ' (Currently broken)'
-        ie_htmls.append('<li>{}</li>'.format(ie_html))
+            Tests on MNIST show that Antirectifier allows to train networks
+        with twice less parameters yet with comparable
+        classification accuracy as an equivalent ReLU-based network.
+    '''
     
-        bug_text = re.search(
-        r'(?s)#\s*BUGS\s*[^\n]*\s*(.*?)#\s*COPYRIGHT', readme).group(1)
-    dev_text = re.search(
-        r'(?s)(#\s*DEVELOPER INSTRUCTIONS.*?)#\s*EMBEDDING YOUTUBE-DL',
-        readme).group(1)
-    
-        return ret
-    
-        for release in releases:
-        compat_print(release['name'])
-        for asset in release['assets']:
-            asset_name = asset['name']
-            total_bytes += asset['download_count'] * asset['size']
-            if all(not re.match(p, asset_name) for p in (
-                    r'^youtube-dl$',
-                    r'^youtube-dl-\d{4}\.\d{2}\.\d{2}(?:\.\d+)?\.tar\.gz$',
-                    r'^youtube-dl\.exe$')):
-                continue
-            compat_print(
-                ' %s size: %s downloads: %d'
-                % (asset_name, format_size(asset['size']), asset['download_count']))
-    
-            # from http://www.indiedb.com/games/king-machine/videos
-        expect_dict(
-            self,
-            self.ie._extract_jwplayer_data(r'''
-<script>
-jwplayer('mediaplayer').setup({'abouttext':'Visit Indie DB','aboutlink':'http:\/\/www.indiedb.com\/','displaytitle':false,'autostart':false,'repeat':false,'title':'king machine trailer 1','sharing':{'link':'http:\/\/www.indiedb.com\/games\/king-machine\/videos\/king-machine-trailer-1','code':'<iframe width=\'560\' height=\'315\' src=\'http:\/\/www.indiedb.com\/media\/iframe\/1522983\' frameborder=\'0\' allowfullscreen><\/iframe><br><a href=\'http:\/\/www.indiedb.com\/games\/king-machine\/videos\/king-machine-trailer-1\'>king machine trailer 1 - Indie DB<\/a>'},'related':{'file':'http:\/\/rss.indiedb.com\/media\/recommended\/1522983\/feed\/rss.xml','dimensions':'160x120','onclick':'link'},'sources':[{'file':'http:\/\/cdn.dbolical.com\/cache\/videos\/games\/1\/50\/49678\/encode_mp4\/king-machine-trailer.mp4','label':'360p SD','default':'true'},{'file':'http:\/\/cdn.dbolical.com\/cache\/videos\/games\/1\/50\/49678\/encode720p_mp4\/king-machine-trailer.mp4','label':'720p HD'}],'image':'http:\/\/media.indiedb.com\/cache\/images\/games\/1\/50\/49678\/thumb_620x2000\/king-machine-trailer.mp4.jpg','advertising':{'client':'vast','tag':'http:\/\/ads.intergi.com\/adrawdata\/3.0\/5205\/4251742\/0\/1013\/ADTECH;cors=yes;width=560;height=315;referring_url=http:\/\/www.indiedb.com\/games\/king-machine\/videos\/king-machine-trailer-1;content_url=http:\/\/www.indiedb.com\/games\/king-machine\/videos\/king-machine-trailer-1;media_id=1522983;title=king+machine+trailer+1;device=__DEVICE__;model=__MODEL__;os=Windows+OS;osversion=__OSVERSION__;ua=__UA__;ip=109.171.17.81;uniqueid=1522983;tags=__TAGS__;number=58cac25928151;time=1489683033'},'width':620,'height':349}).once('play', function(event) {
-            videoAnalytics('play');
-}).once('complete', function(event) {
-    videoAnalytics('completed');
-});
-</script>
-                ''', 'dummy'),
-            {
-                'title': 'king machine trailer 1',
-                'thumbnail': 'http://media.indiedb.com/cache/images/games/1/50/49678/thumb_620x2000/king-machine-trailer.mp4.jpg',
-                'formats': [{
-                    'url': 'http://cdn.dbolical.com/cache/videos/games/1/50/49678/encode_mp4/king-machine-trailer.mp4',
-                    'height': 360,
-                    'ext': 'mp4'
-                }, {
-                    'url': 'http://cdn.dbolical.com/cache/videos/games/1/50/49678/encode720p_mp4/king-machine-trailer.mp4',
-                    'height': 720,
-                    'ext': 'mp4'
-                }]
-            })
-    
-        def test_keywords(self):
-        self.assertMatch(':ytsubs', ['youtube:subscriptions'])
-        self.assertMatch(':ytsubscriptions', ['youtube:subscriptions'])
-        self.assertMatch(':ythistory', ['youtube:history'])
+    The VAE has a modular design. The encoder, decoder and VAE
+are 3 models that share weights. After training the VAE model,
+the encoder can be used to  generate latent vectors.
+The decoder can be used to generate MNIST digits by sampling the
+latent vector from a Gaussian distribution with mean=0 and std=1.
     
     
-class CheckIp(front_base.check_ip.CheckIp):
-    def check_response(self, response):
-        server_type = response.headers.get('Server', '')
-        self.logger.debug('status:%d', response.status)
-        self.logger.debug('Server type:%s', server_type)
+# MNIST dataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
     
-    - tree.CommonTreeNodeStream: A basic and most commonly used tree.TreeNodeStream
-  implementation.
-  
-    
-            for i in self.tree.iterfind('video/quality'):
-            quality = i.attrib ['value']
-            url = i[0].attrib['playurl']
-            self.stream_types.append({'id': quality,
-                                      'video_profile': i.attrib ['desp']})
-            self.streams[quality] = {'url': url,
-                                     'video_profile': i.attrib ['desp']}
-            self.streams_sorted = [dict([('id', stream_type['id'])] + list(self.streams[stream_type['id']].items())) for stream_type in self.__class__.stream_types if stream_type['id'] in self.streams]
+        return (x_train, y_train), (x_test, y_test)
+
     
     
-def cntv_download(url, **kwargs):
-    if re.match(r'http://tv\.cntv\.cn/video/(\w+)/(\w+)', url):
-        rid = match1(url, r'http://tv\.cntv\.cn/video/\w+/(\w+)')
-    elif re.match(r'http://tv\.cctv\.com/\d+/\d+/\d+/\w+.shtml', url):
-        rid = r1(r'var guid = '(\w+)'', get_content(url))
-    elif re.match(r'http://\w+\.cntv\.cn/(\w+/\w+/(classpage/video/)?)?\d+/\d+\.shtml', url) or \
-         re.match(r'http://\w+.cntv.cn/(\w+/)*VIDE\d+.shtml', url) or \
-         re.match(r'http://(\w+).cntv.cn/(\w+)/classpage/video/(\d+)/(\d+).shtml', url) or \
-         re.match(r'http://\w+.cctv.com/\d+/\d+/\d+/\w+.shtml', url) or \
-         re.match(r'http://\w+.cntv.cn/\d+/\d+/\d+/\w+.shtml', url): 
-        page = get_content(url)
-        rid = r1(r'videoCenterId','(\w+)'', page)
-        if rid is None:
-            guid = re.search(r'guid\s*=\s*'([0-9a-z]+)'', page).group(1)
-            rid = guid
-    elif re.match(r'http://xiyou.cntv.cn/v-[\w-]+\.html', url):
-        rid = r1(r'http://xiyou.cntv.cn/v-([\w-]+)\.html', url)
+# Aliases
+    
+        Returns
+    -------
+    array of floats shaped like (metrics, formats, samples, classes, density)
+        Time in seconds.
+    '''
+    metrics = np.atleast_1d(metrics)
+    samples = np.atleast_1d(samples)
+    classes = np.atleast_1d(classes)
+    density = np.atleast_1d(density)
+    formats = np.atleast_1d(formats)
+    out = np.zeros((len(metrics), len(formats), len(samples), len(classes),
+                    len(density)), dtype=float)
+    it = itertools.product(samples, classes, density)
+    for i, (s, c, d) in enumerate(it):
+        _, y_true = make_multilabel_classification(n_samples=s, n_features=1,
+                                                   n_classes=c, n_labels=d * c,
+                                                   random_state=42)
+        _, y_pred = make_multilabel_classification(n_samples=s, n_features=1,
+                                                   n_classes=c, n_labels=d * c,
+                                                   random_state=84)
+        for j, f in enumerate(formats):
+            f_true = f(y_true)
+            f_pred = f(y_pred)
+            for k, metric in enumerate(metrics):
+                t = timeit(partial(metric, f_true, f_pred), number=n_times)
+    
+        results = defaultdict(lambda: [])
+    
+    The input data is mostly low rank but is a fat infinite tail.
+'''
+from __future__ import print_function
+    
+    The quality of the approximation is measured by the spectral norm discrepancy
+between the original input matrix and the reconstructed one (by multiplying
+the randomized_svd's outputs). The spectral norm is always equivalent to the
+largest singular value of a matrix. (3) justifies this choice. However, one can
+notice in these experiments that Frobenius and spectral norms behave
+very similarly in a qualitative sense. Therefore, we suggest to run these
+benchmarks with `enable_spectral_norm = False`, as Frobenius' is MUCH faster to
+compute.
+    
+        op.add_option('--n-step',
+                  dest='n_steps', default=5, type=int,
+                  help='Number of step interval between 0 and n_population.')
+    
+    
+def sparsity_ratio(X):
+    return np.count_nonzero(X) / float(n_samples * n_features)
+    
+    def os_constant(key):
+    # XXX TODO: In the future, this could return different constants
+    #           based on what OS we are running under.  To see an
+    #           approach to how to handle different OSes, see the
+    #           apache version of this file.  Currently, we do not
+    #           actually have any OS-specific constants on Nginx.
+    '''
+    Get a constant value for operating system
+    
+        @nonce.decoder
+    def nonce(value):  # pylint: disable=missing-docstring,no-self-argument
+        try:
+            return jose.decode_b64jose(value)
+        except jose.DeserializationError as error:
+            # TODO: custom error
+            raise jose.DeserializationError('Invalid nonce: {0}'.format(error))
+    
+    
+UPDATED_MOD_SSL_CONF_DIGEST = '.updated-options-ssl-apache-conf-digest.txt'
+'''Name of the hash of the updated or informed mod_ssl_conf as saved in `IConfig.config_dir`.'''
+    
+        def tearDown(self):
+        shutil.rmtree(self.config_dir)
+        shutil.rmtree(self.work_dir)
+        shutil.rmtree(self.temp_dir)
+    
+        def _get_config_text(self, achall, ip_addrs):
+        '''Chocolate virtual server configuration text
+    
+        if len(sys.argv) == 2:
+        main(sys.argv[1])
     else:
-        raise NotImplementedError(url)
+        print('Choose one path as argument one')
+
     
-                    ffmpeg.ffmpeg_concat_audio_and_video([loop_file_path, single_file_path], title + '_full', 'mp4')
-                cleanup_files([video_file_path, audio_file_path, loop_file_path])
-            except EnvironmentError as err:
-                print('Error preparing full coub video. {}'.format(err))
-    except Exception as err:
-        print('Error while downloading files. {}'.format(err))
     
-    # (api_key, method, ext, page)
-tmpl_api_call = (
-    'https://api.flickr.com/services/rest?'
-    '&format=json&nojsoncallback=1'
-    # UNCOMMENT FOR TESTING
-    #'&per_page=5'
-    '&per_page=500'
-    # this parameter CANNOT take control of 'flickr.galleries.getPhotos'
-    # though the doc said it should.
-    # it's always considered to be 500
-    '&api_key=%s'
-    '&method=flickr.%s'
-    '&extras=url_sq,url_q,url_t,url_s,url_n,url_m,url_z,url_c,url_l,url_h,url_k,url_o,media'
-    '%s&page=%d'
-)
+@coroutine
+def coroutine2(target):
+    while True:
+        request = yield
+        if 10 < request <= 20:
+            print('request {} handled in coroutine 2'.format(request))
+        else:
+            target.send(request)
+    
+        fftv.publish('cartoon')
+    fftv.publish('music')
+    fftv.publish('ads')
+    fftv.publish('movie')
+    fftv.publish('cartoon')
+    fftv.publish('cartoon')
+    fftv.publish('movie')
+    fftv.publish('blank')
+    
+        print('After subclassing: ')
+    for k in RegistryHolder.REGISTRY:
+        print(k)
+    
+    
+def lazy_property2(fn):
+    attr = '_lazy__' + fn.__name__
+    
+        def get_objects(self):
+        '''Get all objects'''
+        return self._objects
+    
+        def __getattr__(self, name):
+        def wrapper(*args, **kwargs):
+            if hasattr(self.delegate, name):
+                attr = getattr(self.delegate, name)
+                if callable(attr):
+                    return attr(*args, **kwargs)
+        return wrapper
+    
+    
+class Professor(AbstractExpert):
+    
+    # Load some sample pictures and learn how to recognize them.
+lmm_image = face_recognition.load_image_file('lin-manuel-miranda.png')
+lmm_face_encoding = face_recognition.face_encodings(lmm_image)[0]
+    
+                face_names.append(name)
+    
+            # 0.6 is the default face distance match threshold. So we'll spot-check that the numbers returned
+        # are above or below that based on if they should match (since the exact numbers could vary).
+        self.assertEqual(type(distance_results), np.ndarray)
+        self.assertLessEqual(distance_results[0], 0.6)
+        self.assertLessEqual(distance_results[1], 0.6)
+        self.assertGreater(distance_results[2], 0.6)
