f8dcddaaf5e7cea6675b29d1aa9582eddd9ca1b8
SUNsung <weatherius@gmail.com>
2018-08-30 05:32:20 +0300

    Rewrite [49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb] File in: 2018-08-30 05:32:19

--- ruby/49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb
+++ ruby/49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb
@@ -1,136 +1,152 @@
 
         
-            brew cask install mactex
-    EOS
-  when 'pip' then <<-EOS.undent
-    Homebrew provides pip via: `brew install python`. However you will then
-    have two Pythons installed on your Mac, so alternatively you can install
-    pip via the instructions at:
+        # Test if processing content string without any Liquid constructs, via Liquid,
+# is slower than checking whether constructs exist ( using `String#include?` )
+# and return-ing the 'plaintext' content string as is..
+#
+# Ref: https://github.com/jekyll/jekyll/pull/6735
     
-        keys.each do |key|
-      value = env[key]
-      s = '#{key}: #{value}'
-      case key
-      when 'CC', 'CXX', 'LD'
-        s << ' => #{Pathname.new(value).realpath}' if File.symlink?(value)
+    Before do
+  FileUtils.rm_rf(Paths.test_dir) if Paths.test_dir.exist?
+  FileUtils.mkdir_p(Paths.test_dir) unless Paths.test_dir.directory?
+  Dir.chdir(Paths.test_dir)
+  @timezone_before_scenario = ENV['TZ']
+end
+    
+      s.summary       = 'A simple, blog aware, static site generator.'
+  s.description   = 'Jekyll is a simple, blog aware, static site generator.'
+    
+            def url_valid?(url)
+          Addressable::URI.parse(url)
+          true
+        # Addressable::URI#parse only raises a TypeError
+        # https://git.io/vFfbx
+        rescue TypeError
+          Jekyll.logger.warn 'Warning:', 'The site URL does not seem to be valid, '\
+              'check the value of `url` in your config file.'
+          false
+        end
+    
+    def check_link(uri)
+  HTTParty.head(uri, :verify => false).code.to_i.tap do |status|
+    if (400..422).include?(status)
+      if status != 403 && !uri.exclude?('udemy.com')
+        raise 'Request had status #{status}'
+      else
+        putc('S')
       end
-      f.puts s
     end
   end
 end
-
     
-        def self.cleanup_logs
-      return unless HOMEBREW_LOGS.directory?
-      HOMEBREW_LOGS.subdirs.each do |dir|
-        cleanup_path(dir) { dir.rmtree } if prune?(dir, :days_default => 14)
+    Then /^I should see an image in the publisher$/ do
+  photo_in_publisher.should be_present
+end
+    
+    Before do |scenario|
+  Devise.mailer.deliveries = []
+  page.driver.headers = if scenario.source_tag_names.include? '@mobile'
+                          {'User-Agent' => 'Mozilla/5.0 (Mobile; rv:18.0) Gecko/18.0 Firefox/18.0'}
+                        else
+                          {}
+                        end
+    
+      def navigate_to(page_name)
+    path = path_to(page_name)
+    if path.is_a?(Hash)
+      visit(path[:path])
+      await_elem = path[:special_elem]
+      find(await_elem.delete(:selector), await_elem)
+    else
+      visit(path)
+    end
+  end
+    
+        def render(context)
+      if parts = @text.match(/([a-zA-Z\d]*) (.*)/)
+        gist, file = parts[1].strip, parts[2].strip
+      else
+        gist, file = @text.strip, ''
+      end
+      if gist.empty?
+        ''
+      else
+        script_url = script_url_for gist, file
+        code       = get_cached_gist(gist, file) || get_gist_from_web(gist, file)
+        html_output_for script_url, code
       end
     end
     
-      def find_internal_commands(directory)
-    directory.children.reduce([]) do |cmds, f|
-      cmds << f.basename.to_s.sub(/\.(?:rb|sh)$/, '') if f.file?
-      cmds
+    require 'pathname'
+require './plugins/octopress_filters'
+    
+          install_list.concat(dependencies)
     end
   end
-end
-
     
-        first_warning = true
-    methods.each do |method|
-      unless checks.respond_to?(method)
-        Homebrew.failed = true
-        puts 'No check available by the name: #{method}'
-        next
+        private
+    def uncompress(source)
+      temporary_directory = Stud::Temporary.pathname
+      LogStash::Util::Zip.extract(source, temporary_directory, LOGSTASH_PATTERN_RE)
+      temporary_directory
+    rescue Zip::Error => e
+      # OK Zip's handling of file is bit weird, if the file exist but is not a valid zip, it will raise
+      # a `Zip::Error` exception with a file not found message...
+      raise InvalidPackError, 'Cannot uncompress the zip: #{source}'
+    end
+    
+            it 'should raise a configuration error' do
+          expect do
+            plugin_class.new('oneString' => '${NoSuchVariable}')
+          end.to raise_error(LogStash::ConfigurationError)
+        end
       end
     
-        renamed_formulae = []
-    @report[:D].each do |old_full_name|
-      old_name = old_full_name.split('/').last
-      new_name = tap.formula_renames[old_name]
-      next unless new_name
-    
-      private
-    
-        if registration
-      u2f.authenticate!(challenges, response, Base64.decode64(registration.public_key), registration.counter)
-      registration.update(counter: response.counter)
-      true
-    end
-  rescue JSON::ParserError, NoMethodError, ArgumentError, U2F::Error
-    false
-  end
-end
-
-    
-        def run!
-      @thread = Thread.new do
-        Thread.current[:name] = '#{id}-#{Time.now}'
-        begin
-          run
-        rescue SignalException, SystemExit
-          stop!
-        rescue StandardError => e
-          message = '#{id} Exception #{e.message}:\n#{e.backtrace.first(10).join('\n')}'
-          AgentRunner.with_connection do
-            agent.error(message)
+              it 'allow to install a specific version' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify --version 0.1.0 logstash-filter-qatest')
+            expect(command).to install_successfully
+            expect(logstash).to have_installed?('logstash-filter-qatest', '0.1.0')
           end
         end
       end
-    end
     
-      get(/.+/) do
-    send_sinatra_file(request.path) {404}
+        autoload :Cookies
+    autoload :Validations
+    autoload :ErrorFormatter
+    autoload :Formatter
+    autoload :Parser
+    autoload :Request
+    autoload :Env, 'grape/util/env'
+    autoload :Json, 'grape/util/json'
+    autoload :Xml, 'grape/util/xml'
   end
     
-          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
-        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
-        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
-          @img['title']  = title
-          @img['alt']    = alt
+          # (see #cascade?)
+      def cascade(value = nil)
+        if value.nil?
+          inheritable_setting.namespace_inheritable.keys.include?(:cascade) ? !namespace_inheritable(:cascade).nil? : true
         else
-          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
+          namespace_inheritable(:cascade, value)
         end
-        @img['class'].gsub!(/'/, '') if @img['class']
       end
-      super
-    end
     
-      class IncludeCodeTag < Liquid::Tag
-    def initialize(tag_name, markup, tokens)
-      @title = nil
-      @file = nil
-      if markup.strip =~ /\s*lang:(\S+)/i
-        @filetype = $1
-        markup = markup.strip.sub(/lang:\S+/i,'')
-      end
-      if markup.strip =~ /(.*)?(\s+|^)(\/*\S+)/i
-        @title = $1 || nil
-        @file = $3
-      end
-      super
-    end
+            # Execute the given block after validations and coercions, but before
+        # any endpoint code.
+        def after_validation(&block)
+          namespace_stackable(:after_validations, block)
+        end
     
-      class RenderPartialTag < Liquid::Tag
-    include OctopressFilters
-    def initialize(tag_name, markup, tokens)
-      @file = nil
-      @raw = false
-      if markup =~ /^(\S+)\s?(\w+)?/
-        @file = $1.strip
-        @raw = $2 == 'raw'
-      end
-      super
-    end
+              def config_context.success(*args)
+            entity(*args)
+          end
     
-            private
+                  in_setting = app.top_level_setting
     
-            # @param annotated_source [String] string passed to the matchers
-        #
-        # Separates annotation lines from source lines. Tracks the real
-        # source line number that each annotation corresponds to.
-        #
-        # @return [AnnotatedSource]
-        def self.parse(annotated_source)
-          source      = []
-          annotations = []
+            # Opens a root-level ParamsScope, defining parameter coercions and
+        # validations for the endpoint.
+        # @yield instance context of the new scope
+        def params(&block)
+          Grape::Validations::ParamsScope.new(api: self, type: Hash, &block)
+        end
+    
+          validators = validator_factories.map(&:create_validator)
