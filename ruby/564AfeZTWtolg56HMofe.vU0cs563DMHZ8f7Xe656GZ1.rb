c50564699322ff7782cd1184aa90276ea8218d4e
SUNsung <weatherius@gmail.com>
2018-08-30 12:35:27 +0300

    Rewrite [564AfeZTWtolg56HMofe.vU0cs563DMHZ8f7Xe656GZ1.rb] File in: 2018-08-30 12:35:27

--- ruby/564AfeZTWtolg56HMofe.vU0cs563DMHZ8f7Xe656GZ1.rb
+++ ruby/564AfeZTWtolg56HMofe.vU0cs563DMHZ8f7Xe656GZ1.rb
@@ -1,74 +1,47 @@
 
         
-          it 'correctly sets notification level' do
-    moderator = Fabricate(:moderator)
-    
-            FastlaneCore::CommanderGenerator.new.generate(Cert::Options.available_options, command: c)
-    
-            expect(options[:release_notes]['en-US']).to eql('something else')
-        expect(options[:release_notes]['es-MX']).to eql('something else else')
-        expect(options[:release_notes]['en-AU']).to eql('something')
-        expect(options[:release_notes]['en-CA']).to eql('something')
-        expect(options[:release_notes]['en-GB']).to eql('something')
-        expect(options[:release_notes]['de-DE']).to eql('something')
-        expect(options[:release_notes]['el']).to eql('something')
-      end
-    end
-  end
-end
-
-    
-          def self.available_options
-        [
-          FastlaneCore::ConfigItem.new(key: :adb_path,
-                                       env_name: 'FL_ADB_PATH',
-                                       description: 'The path to your `adb` binary (can be left blank if the ANDROID_SDK_ROOT environment variable is set)',
-                                       is_string: true,
-                                       optional: true,
-                                       default_value: 'adb')
-        ]
-      end
-    
-          def self.wait_for_appium_server(params)
-        loop.with_index do |_, count|
-          break if `lsof -i:#{params[:port]}`.to_s.length != 0
-    
-          def self.connect_to_artifactory(params)
-        config_keys = [:endpoint, :username, :password, :ssl_pem_file, :ssl_verify, :proxy_username, :proxy_password, :proxy_address, :proxy_port]
-        config = params.values.select do |key|
-          config_keys.include?(key)
+            # Releases a local lock on a machine. This does not acquire any locks
+    # so make sure to lock around it.
+    #
+    # @param [String] id
+    def unlocked_release(id)
+      lock_file = @machine_locks[id]
+      if lock_file
+        lock_file.close
+        begin
+          File.delete(lock_file.path)
+        rescue Errno::EACCES
+          # Another process is probably opened it, no problem.
         end
-        Artifactory::Client.new(config)
-      end
     
-            # Defines additional command line commands available by key. The key
-        # becomes the subcommand, so if you register a command 'foo' then
-        # 'vagrant foo' becomes available.
+            # Upload a file to the remote machine.
         #
-        # @param [String] name Subcommand key.
-        def self.command(name=UNSET_VALUE, &block)
-          data[:command] ||= Registry.new
+        # @param [String] from Path of the file locally to upload.
+        # @param [String] to Path of where to save the file on the remote
+        #   machine.
+        def upload(from, to)
+        end
     
-            # Download a file from the remote machine to the local machine.
+            # Merge another configuration object into this one. This assumes that
+        # the other object is the same class as this one. This should not
+        # mutate this object, but instead should return a new, merged object.
         #
-        # @param [String] from Path of the file on the remote machine.
-        # @param [String] to Path of where to save the file locally.
-        def download(from, to)
-        end
+        # The default implementation will simply iterate over the instance
+        # variables and merge them together, with this object overriding
+        # any conflicting instance variables of the older object. Instance
+        # variables starting with '__' (double underscores) will be ignored.
+        # This lets you set some sort of instance-specific state on your
+        # configuration keys without them being merged together later.
+        #
+        # @param [Object] other The other configuration object to merge from,
+        #   this must be the same type of object as this one.
+        # @return [Object] The merged object.
+        def merge(other)
+          result = self.class.new
     
-              nil
-        end
-    
-            # This is an internal initialize function that should never be
-        # overridden. It is used to initialize some common internal state
-        # that is used in a provider.
-        def _initialize(name, machine)
-          initialize_capabilities!(
-            name.to_sym,
-            { name.to_sym => [Class.new, nil] },
-            Vagrant.plugin('2').manager.provider_capabilities,
-            machine,
-          )
+            # This is the method called to when the system is being destroyed
+        # and allows the provisioners to engage in any cleanup tasks necessary.
+        def cleanup
         end
       end
     end
@@ -76,28 +49,67 @@ end
 end
 
     
-      def append(*paths)
-    @paths = parse(*@paths, *paths)
-    self
+        # use Feedbag as a backup to Google Feeds Api
+    if rss_url.nil?
+      rss_url = Feedbag.find(web_url).first
+      if rss_url.nil?
+        suggested_paths = ['/rss', '/feed', '/feeds', '/atom.xml', '/feed.xml', '/rss.xml', '.atom']
+        suggested_paths.each do |suggested_path|
+          rss_url = Feedbag.find('#{web_url.chomp('/')}#{suggested_path}').first
+          break if rss_url
+        end
+      end
+    end
   end
     
-          def delete(target, force: false, command: nil, **_)
-        ohai 'Removing #{self.class.english_name} '#{target}'.'
-        raise CaskError, 'Cannot remove undeletable #{self.class.english_name}.' if MacOS.undeletable?(target)
+    class PolymorphicMentions < ActiveRecord::Migration[4.2]
+  def change
+    remove_index :mentions, column: %i(post_id)
+    remove_index :mentions, column: %i(person_id post_id), unique: true
+    rename_column :mentions, :post_id, :mentions_container_id
+    add_column :mentions, :mentions_container_type, :string
+    add_index :mentions,
+              %i(mentions_container_id mentions_container_type),
+              name:   'index_mentions_on_mc_id_and_mc_type',
+              length: {mentions_container_type: 191}
+    add_index :mentions,
+              %i(person_id mentions_container_id mentions_container_type),
+              name:   'index_mentions_on_person_and_mc_id_and_mc_type',
+              length: {mentions_container_type: 191},
+              unique: true
     
-        def add_error(message)
-      errors << message
-    end
+    Then /^I should have (\d+) nsfw posts$/ do |num_posts|
+  page.should have_css('.nsfw-shield', count: num_posts.to_i)
+end
     
-          rtn = ''
-      (context.environments.first['site'][@array_name] || []).each do |file|
-        if file !~ /^[a-zA-Z0-9_\/\.-]+$/ || file =~ /\.\// || file =~ /\/\./
-          rtn = rtn + 'Include file '#{file}' contains invalid characters or sequences'
+    Given /^a user with username '([^']*)' is connected with '([^']*)'$/ do |arg1, arg2|
+  user1 = User.where(:username => arg1).first
+  user2 = User.where(:username => arg2).first
+  connect_users(user1, user1.aspects.where(:name => 'Besties').first, user2, user2.aspects.where(:name => 'Besties').first)
+end
+    
+    if Encoding.default_external != Encoding::UTF_8
+    
+            # Removes the specified cache
+        #
+        # @param [Array<Hash>] cache_descriptors
+        #        An array of caches to remove, each specified with the same
+        #        hash as cache_descriptors_per_pod especially :spec_file and :slug
+        #
+        def remove_caches(cache_descriptors)
+          cache_descriptors.each do |desc|
+            UI.message('Removing spec #{desc[:spec_file]} (v#{desc[:version]})') do
+              FileUtils.rm(desc[:spec_file])
+            end
+            UI.message('Removing cache #{desc[:slug]}') do
+              FileUtils.rm_rf(desc[:slug])
+            end
+          end
         end
     
-        def initialize(tag_name, markup, tokens)
-      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
-      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
-      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
-      super
-    end
+    class PaperclipGenerator < ActiveRecord::Generators::Base
+  desc 'Create a migration to add paperclip-specific fields to your model. ' +
+       'The NAME argument is the name of your model, and the following ' +
+       'arguments are the name of the attachments'
+    
+        alias :empty? :empty_file?
