cd327f26e190b9d39c1a38636aabb6218fc0fc17
SUNsung <weatherius@gmail.com>
2018-08-31 01:34:08 +0300

    Rewrite [23yV.eoeomDbk2322ErKg3snEs23eqSbLlZfhQc23lJF.py] File in: 2018-08-31 01:34:08

--- pyton/23yV.eoeomDbk2322ErKg3snEs23eqSbLlZfhQc23lJF.py
+++ pyton/23yV.eoeomDbk2322ErKg3snEs23eqSbLlZfhQc23lJF.py
@@ -1,160 +1,69 @@
 
         
-        import datetime
-import io
-import json
-import textwrap
+            data_train = fetch_20newsgroups_vectorized(subset='train')
+    data_test = fetch_20newsgroups_vectorized(subset='test')
+    X_train = check_array(data_train.data, dtype=np.float32,
+                          accept_sparse='csc')
+    X_test = check_array(data_test.data, dtype=np.float32, accept_sparse='csr')
+    y_train = data_train.target
+    y_test = data_test.target
     
+    X -= X.mean(axis=0)
+X /= X.std(axis=0)
     
-def main():
-    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
-    options, args = parser.parse_args()
-    if len(args) != 2:
-        parser.error('Expected an input and an output filename')
+        op.add_option('--density',
+                  dest='density', default=1 / 3,
+                  help='Density used by the sparse random projection.'
+                       ' ('auto' or float (0.0, 1.0]')
     
+        def test_default_encoder_naive_fails(self):
+        from acme.fields import RFC3339Field
+        self.assertRaises(
+            ValueError, RFC3339Field.default_encoder, datetime.datetime.now())
     
-def gen_extractors():
-    ''' Return a list of an instance of every supported extractor.
-    The order does matter; the first extractor matched is the one handling the URL.
-    '''
-    return [klass() for klass in gen_extractor_classes()]
-    
-    
-class AcademicEarthCourseIE(InfoExtractor):
-    _VALID_URL = r'^https?://(?:www\.)?academicearth\.org/playlists/(?P<id>[^?#/]+)'
-    IE_NAME = 'AcademicEarth:Course'
-    _TEST = {
-        'url': 'http://academicearth.org/playlists/laws-of-nature/',
-        'info_dict': {
-            'id': 'laws-of-nature',
-            'title': 'Laws of Nature',
-            'description': 'Introduce yourself to the laws of nature with these free online college lectures from Yale, Harvard, and MIT.',
-        },
-        'playlist_count': 3,
-    }
-    
-    cc_library(
-    name = 'syntaxnet_component',
-    srcs = ['syntaxnet_component.cc'],
-    hdrs = ['syntaxnet_component.h'],
-    deps = [
-        ':syntaxnet_link_feature_extractor',
-        ':syntaxnet_transition_state',
-        '//dragnn/components/util:bulk_feature_extractor',
-        '//dragnn/core:beam',
-        '//dragnn/core:component_registry',
-        '//dragnn/core:input_batch_cache',
-        '//dragnn/core/interfaces:component',
-        '//dragnn/core/interfaces:transition_state',
-        '//dragnn/core/util:label',
-        '//dragnn/io:sentence_input_batch',
-        '//dragnn/io:syntaxnet_sentence',
-        '//dragnn/protos:data_proto_cc',
-        '//dragnn/protos:spec_proto_cc',
-        '//dragnn/protos:trace_proto_cc',
-        '//syntaxnet:base',
-        '//syntaxnet:parser_transitions',
-        '//syntaxnet:registry',
-        '//syntaxnet:sparse_proto_cc',
-        '//syntaxnet:task_context',
-        '//syntaxnet:task_spec_proto_cc',
-        '//syntaxnet:utils',
-        '//util/utf8:unicodetext',
-    ],
-    alwayslink = 1,
-)
-    
-        check.Gt(self._num_labels, 0, 'Expected some labels')
-    check.Eq(len(self._fixed_feature_dims.items()), 0,
-             'Expected no fixed features')
-    check.Eq(len(self._linked_feature_dims.items()), 2,
-             'Expected two linked features')
-    
-        Returns:
-      setup_op - An op that, when run, guarantees all setup ops will run.
-    '''
-    logging.info('Building restore hook for component: %s', self.spec.name)
-    with tf.variable_scope(self.name):
-      if callable(getattr(self.network, 'build_post_restore_hook', None)):
-        return [self.network.build_post_restore_hook()]
-      else:
-        return []
-    
-        Returns:
-      Handle to the state once inference is complete for this Component.
-    '''
-    pass
-    
-      def testEqualFiles(self):
-    content_actual = None
-    content_expected = None
-    
-        Raises:
-      IndexError: if max_index is positive but out of bounds.
-    '''
-    check.IsFalse(compute_gradients and use_moving_average,
-                  'It is not possible to make gradient updates when reading '
-                  'from the moving average variables.')
-    
-            # Check that the oracle traces are not empty.
-        for serialized_trace in traces:
-          master_trace = trace_pb2.MasterTrace()
-          master_trace.ParseFromString(serialized_trace)
-          self.assertTrue(master_trace.component_trace)
-          self.assertTrue(master_trace.component_trace[0].step_trace)
-    
-        The port number is an optional :NN suffix on an IPv4 address or host name,
-    or a mandatory :NN suffix on any square-bracketed expression: IPv6 address,
-    IPv4 address, or host name. (This means the only way to specify a port for
-    an IPv6 address is to enclose it in square brackets.)
-    '''
-    
-    
-def sort_groups(groups):
-    return sorted(groups, key=lambda g: (g.depth, g.priority, g.name))
-    
-        def _node_position_info(self, node):
-        # the line number where the previous token has ended (plus empty lines)
-        # Add one so that the first line is line 1 rather than line 0
-        column = node.start_mark.column + 1
-        line = node.start_mark.line + 1
-    
-        def __init__(self, uuid, name, path, play, action):
-        self.uuid = uuid
-        self.name = name
-        self.path = path
-        self.play = play
-        self.start = None
-        self.host_data = OrderedDict()
-        self.start = time.time()
-        self.action = action
-    
-    In Blackboard pattern several specialised sub-systems (knowledge sources)
-assemble their knowledge to build a possibly partial or approximate solution.
-In this way, the sub-systems work together to solve the problem,
-where the solution is the sum of its parts.
-    
-        def now(self):
-        current_time_is_always_midnight = '24:01'
-        return current_time_is_always_midnight
+        @classmethod
+    # pylint: disable=arguments-differ,too-many-arguments
+    def sign(cls, payload, key, alg, nonce, url=None, kid=None):
+        # Per ACME spec, jwk and kid are mutually exclusive, so only include a
+        # jwk field if kid is not provided.
+        include_jwk = kid is None
+        return super(JWS, cls).sign(payload, key=key, alg=alg,
+                                    protect=frozenset(['nonce', 'url', 'kid', 'jwk', 'alg']),
+                                    nonce=nonce, url=url, kid=kid,
+                                    include_jwk=include_jwk)
 
     
     
-if __name__ == '__main__':
-    reporter = Reporter()
-    db = DB()
-    tm = TestManager()
-    tm.setReporter(reporter)
-    tm.setDB(db)
-    reporter.setTM(tm)
-    db.setTM(tm)
-    # For simplification we are looping on the same test.
-    # Practically, it could be about various unique test classes and their
-    # objects
-    for i in range(3):
-        tc = TC()
-        tc.setTM(tm)
-        tm.setTC(tc)
-        tc.setup()
-        tc.execute()
-        tc.tearDown()
+def vector_path(*names):
+    '''Path to a test vector.'''
+    return pkg_resources.resource_filename(
+        __name__, os.path.join('testdata', *names))
+    
+        def ensure_augeas_state(self):
+        '''Makes sure that all Augeas dom changes are written to files to avoid
+        loss of configuration directives when doing additional augeas parsing,
+        causing a possible augeas.load() resulting dom reset
+        '''
+    
+    UIR_ARGS = ['always', 'set', 'Content-Security-Policy',
+            'upgrade-insecure-requests']
+    
+            '''
+        if self._addr_less_specific(addr):
+            return True
+        elif self.get_addr() == addr.get_addr():
+            if self.is_wildcard() or self.get_port() == addr.get_port():
+                return True
+        return False
+    
+    
+class AddrTest(unittest.TestCase):
+    '''Test obj.Addr.'''
+    def setUp(self):
+        from certbot_apache.obj import Addr
+        self.addr = Addr.fromstring('*:443')
+    
+            self.configurator.parser.add_include(
+            self.configurator.parser.loc['default'], self.challenge_conf)
+        self.configurator.reverter.register_file_creation(
+            True, self.challenge_conf)
