4f0f3ef7ff2bca09977aaff3db93fb2b0edf492d
SUNsung <weatherius@gmail.com>
2018-08-30 12:05:00 +0300

    Rewrite [291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp] File in: 2018-08-30 12:04:59

--- cccccc/291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp
+++ cccccc/291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp
@@ -1,53 +1,273 @@
 
         
-        // Relaunches the application using the helper application associated with the
-// currently running instance of Chrome in the parent browser process as the
-// executable for the relauncher process. |args| is an argv-style vector of
-// command line arguments of the form normally passed to execv. args[0] is
-// also the path to the relaunched process. Because the relauncher process
-// will ultimately launch the relaunched process via Launch Services, args[0]
-// may be either a pathname to an executable file or a pathname to an .app
-// bundle directory. The caller should exit soon after RelaunchApp returns
-// successfully. Returns true on success, although some failures can occur
-// after this function returns true if, for example, they occur within the
-// relauncher process. Returns false when the relaunch definitely failed.
-bool RelaunchApp(const StringVector& argv);
+        #include <grpc/grpc_cronet.h>
+#include <grpcpp/channel.h>
+#include <grpcpp/support/channel_arguments.h>
+#include 'src/cpp/client/create_channel_internal.h'
     
-    #include <memory>
+    #include <grpcpp/support/channel_arguments.h>
     
-    // filenames
-extern const base::FilePath::CharType kCacheDirname[];
-extern const base::FilePath::CharType kChannelIDFilename[];
-extern const base::FilePath::CharType kCookieFilename[];
-extern const base::FilePath::CharType kCRLSetFilename[];
-extern const base::FilePath::CharType kCustomDictionaryFileName[];
-extern const base::FilePath::CharType kExtensionActivityLogFilename[];
-extern const base::FilePath::CharType kExtensionsCookieFilename[];
-extern const base::FilePath::CharType kFirstRunSentinel[];
-extern const base::FilePath::CharType kGCMStoreDirname[];
-extern const base::FilePath::CharType kLocalStateFilename[];
-extern const base::FilePath::CharType kLocalStorePoolName[];
-extern const base::FilePath::CharType kMediaCacheDirname[];
-extern const base::FilePath::CharType kNetworkPersistentStateFilename[];
-extern const base::FilePath::CharType kOfflinePageArchviesDirname[];
-extern const base::FilePath::CharType kOfflinePageMetadataDirname[];
-extern const base::FilePath::CharType kPreferencesFilename[];
-extern const base::FilePath::CharType kProtectedPreferencesFilenameDeprecated[];
-extern const base::FilePath::CharType kReadmeFilename[];
-extern const base::FilePath::CharType kResetPromptMementoFilename[];
-extern const base::FilePath::CharType kSafeBrowsingBaseFilename[];
-extern const base::FilePath::CharType kSecurePreferencesFilename[];
-extern const base::FilePath::CharType kServiceStateFileName[];
-extern const base::FilePath::CharType kSingletonCookieFilename[];
-extern const base::FilePath::CharType kSingletonLockFilename[];
-extern const base::FilePath::CharType kSingletonSocketFilename[];
-extern const base::FilePath::CharType kSupervisedUserSettingsFilename[];
-extern const base::FilePath::CharType kThemePackFilename[];
-extern const base::FilePath::CharType kThemePackMaterialDesignFilename[];
-extern const base::FilePath::CharType kWebAppDirname[];
+    #ifndef GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CONTEXT_H
+#define GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CONTEXT_H
     
-    #include 'rocksdb/env.h'
-#include 'util/testharness.h'
     
-     private:
-  char delim_;         // The delimiter is inserted between elements
+    {
+    {			if (szArgList[i + 1] != NULL && szArgList[i + 1][0] != '/') {
+				cmderOptions.cmderCfgRoot = szArgList[i + 1];
+				i++;
+			}
+		}
+		else if (_wcsicmp(L'/start', szArgList[i]) == 0)
+		{
+			if (PathFileExists(szArgList[i + 1]))
+			{
+				cmderOptions.cmderStart = szArgList[i + 1];
+				i++;
+			}
+			else {
+				MessageBox(NULL, szArgList[i + 1], L'/START - Folder doses not exist!', MB_OK);
+			}
+		}
+		else if (_wcsicmp(L'/task', szArgList[i]) == 0)
+		{
+			cmderOptions.cmderTask = szArgList[i + 1];
+			i++;
+		}
+		else if (_wcsicmp(L'/single', szArgList[i]) == 0)
+		{
+			cmderOptions.cmderSingle = true;
+		}
+		else if (_wcsicmp(L'/register', szArgList[i]) == 0)
+		{
+			cmderOptions.registerApp = true;
+			cmderOptions.unRegisterApp = false;
+			if (szArgList[i + 1] != NULL)
+			{
+				if (_wcsicmp(L'all', szArgList[i + 1]) == 0 || _wcsicmp(L'user', szArgList[i + 1]) == 0)
+				{
+					cmderOptions.cmderRegScope = szArgList[i + 1];
+					i++;
+				}
+			}
+		}
+		else if (_wcsicmp(L'/unregister', szArgList[i]) == 0)
+		{
+			cmderOptions.unRegisterApp = true;
+			cmderOptions.registerApp = false;
+			if (szArgList[i + 1] != NULL)
+			{
+				if (_wcsicmp(L'all', szArgList[i + 1]) == 0 || _wcsicmp(L'user', szArgList[i + 1]) == 0)
+				{
+					cmderOptions.cmderRegScope = szArgList[i + 1];
+					i++;
+				}
+			}
+		}
+		else if (cmderOptions.cmderStart == L'' && PathFileExists(szArgList[i]))
+		{
+			cmderOptions.cmderStart = szArgList[i];
+		}
+		else {
+			MessageBox(NULL, L'Unrecognized parameter.\n\nValid options:\n\n    /c [CMDER User Root Path]\n\n    /task [ConEmu Task Name]\n\n    [/start [Start in Path] | [Start in Path]]\n\n    /single\n\nor\n\n    /register [USER | ALL]\n\nor\n\n    /unregister [USER | ALL]\n', MB_TITLE, MB_OK);
+			cmderOptions.error = true;
+		}
+	}
+    
+        // 1st element (which should be 0,0) should be here too
+    
+    p = pReverse->getControlPointAtIndex(pReverse->count()-1);
+    pReverse->removeControlPointAtIndex(pReverse->count()-1);
+    
+    p = -p;
+    pReverse->insertControlPoint(p, 0);
+    
+    for (ssize_t i = 1; i < pReverse->count(); ++i)
+    {
+        Vec2 current = pReverse->getControlPointAtIndex(i);
+        current = -current;
+        Vec2 abs = current + p;
+        pReverse->replaceControlPoint(abs, i);
+        
+        p = abs;
+    }
+    
+        EaseRateAction *easeRateAction = new (std::nothrow) EaseRateAction();
+    if (easeRateAction && easeRateAction->initWithAction(action, rate))
+    {
+        easeRateAction->autorelease();
+        return easeRateAction;
+    }
+    
+        /** 
+    @brief Initializes an action with duration and grid size.
+    @param gridSize Specify the grid size of the FlipX3D action.
+    @param duration Specify the duration of the FlipX3D action. It's a value in seconds.
+    @return If the initialization success, return true; otherwise, return false.
+    */
+    virtual bool initWithSize(const Size& gridSize, float duration);
+    
+        /** Creates a Place action with a position.
+     *
+     * @param pos  A certain position.
+     * @return  An autoreleased Place object.
+     */
+    static Place * create(const Vec2& pos);
+    
+    
+    {
+    {
+    {            if ((action->getFlags() & flags) != 0 && action->getOriginalTarget() == target)
+            {
+                removeActionAtIndex(i, element);
+                --limit;
+            }
+            else
+            {
+                ++i;
+            }
+        }
+    }
+}
+    
+    /** @class ActionManager
+ @brief ActionManager is a singleton that manages all the actions.
+ Normally you won't need to use this singleton directly. 99% of the cases you will use the Node interface,
+ which uses this singleton.
+ But there are some cases where you might need to use this singleton.
+ Examples:
+    - When you want to run an action where the target is different from a Node. 
+    - When you want to pause / resume the actions.
+ 
+ @since v0.8
+ */
+class CC_DLL ActionManager : public Ref
+{
+public:
+    /**
+     * @js ctor
+     */
+    ActionManager();
+    }
+    
+    void ProgressTo::startWithTarget(Node *target)
+{
+    ActionInterval::startWithTarget(target);
+    _from = ((kProgressTimerCast)(target))->getPercentage();
+}
+    
+    The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+    
+    Animation::~Animation(void)
+{
+    CCLOGINFO('deallocing Animation: %p', this);
+}
+    
+        for (const auto& anim : animations)
+    {
+        const ValueMap& animationDict = anim.second.asValueMap();
+        const ValueVector& frameNames = animationDict.at('frames').asValueVector();
+        float delay = animationDict.at('delay').asFloat();
+        Animation* animation = nullptr;
+    }
+    
+        /** Returns a Animation that was previously added.
+     * If the name is not found it will return nil.
+     * You should retain the returned copy if you are going to use it.
+     *
+     * @return A Animation that was previously added. If the name is not found it will return nil.
+     */
+    Animation* getAnimation(const std::string& name);
+    /**
+     * @deprecated. Use getAnimation() instead
+     * @js NA
+     * @lua NA
+     */
+    CC_DEPRECATED_ATTRIBUTE Animation* animationByName(const std::string& name){ return getAnimation(name); }
+    
+    #include <vector>
+    
+      // Returns true if the argument of the last Compare() call (or the baseline
+  // image, if Compare() was not called yet) meets the image acceptance
+  // criteria. The target_mul modifies the acceptance criteria used in this call
+  // the following way:
+  //    = 1.0 : the original acceptance criteria is used,
+  //    < 1.0 : a more strict acceptance criteria is used,
+  //    > 1.0 : a less strict acceptance criteria is used.
+  virtual bool DistanceOK(double target_mul) const = 0;
+    
+    void PrintDebug(ProcessStats* stats, std::string s);
+    
+        size_t i = 0;      // Points to the next leaf node.
+    size_t j = n + 1;  // Points to the next non-leaf node.
+    for (size_t k = n - 1; k != 0; --k) {
+      size_t left, right;
+      if (tree[i].total_count_ <= tree[j].total_count_) {
+        left = i;
+        ++i;
+      } else {
+        left = j;
+        ++j;
+      }
+      if (tree[i].total_count_ <= tree[j].total_count_) {
+        right = i;
+        ++i;
+      } else {
+        right = j;
+        ++j;
+      }
+    }
+    
+      if (fseek(f, 0, SEEK_END) == 0) {
+    buffer_size = std::max<off_t>(ftell(f), 1);
+    if (fseek(f, 0, SEEK_SET) != 0) {
+      perror('fseek');
+      exit(1);
+    }
+  } else if (ferror(f)) {
+    perror('fseek');
+    exit(1);
+  }
+    
+      bool Is420() const;
+  bool Is444() const;
+    
+    // Decodes the parsed jpeg coefficients into an RGB image.
+// There can be only either 1 or 3 image components, in either case, an RGB
+// output image will be generated.
+// Only YUV420 and YUV444 sampling factors are supported.
+// Vector will be empty if a decoding error occurred.
+std::vector<uint8_t> DecodeJpegToRGB(const JPEGData& jpg);
+    
+    void BuildDCHistograms(const JPEGData& jpg, JpegHistogram* histo);
+void BuildACHistograms(const JPEGData& jpg, JpegHistogram* histo);
+size_t JpegHeaderSize(const JPEGData& jpg, bool strip_metadata);
+size_t EstimateJpegDataSize(const int num_components,
+                            const std::vector<JpegHistogram>& histograms);
+    
+    // Synchronous transmission of CAN messages
+ErrorCode EsdCanClient::Send(const std::vector<CanFrame> &frames,
+                             int32_t *const frame_num) {
+  CHECK_NOTNULL(frame_num);
+  CHECK_EQ(frames.size(), static_cast<size_t>(*frame_num));
+    }
+    
+    namespace apollo {
+namespace drivers {
+namespace canbus {
+namespace can {
+    }
+    }
+    }
+    }
+    
+    
+    {
+    {
+    {}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+
+    
+    const int32_t CANBUS_MESSAGE_LENGTH = 8;  // according to ISO-11891-1
+const int32_t MAX_CAN_PORT = 3;
