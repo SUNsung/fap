b804bad5f1baabfd4e9fe0991ceb9e574a0b3d48
SUNsung <weatherius@gmail.com>
2018-08-30 18:52:01 +0300

    Rewrite [09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp] File in: 2018-08-30 18:52:00

--- cccccc/09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp
+++ cccccc/09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp
@@ -1,397 +1,99 @@
 
         
-          if (t_size > s_size) return LevenshteinDistance(t, s, cmp);
-    
-      // E[X].
-  auto mean = add(Mean(elements_per_feature_int64, sum, add));
-    
-    TfLiteStatus ArgMinEval(TfLiteContext* context, TfLiteNode* node) {
-  return Eval(context, node, false);
+        void Clipboard::Call(const std::string& method,
+                     const base::ListValue& arguments) {
+  if (method == 'Set') {
+    std::string text, type;
+    arguments.GetString(0, &text);
+    arguments.GetString(1, &type);
+    SetText(text);
+  } else if (method == 'Clear') {
+    Clear();
+  } else {
+    NOTREACHED() << 'Invalid call to Clipboard method:' << method
+                 << ' arguments:' << arguments;
+  }
 }
     
-    template <typename T>
-class ArgMaxOpModel : public ArgBaseOpModel<T> {
+    class MenuDelegate : public ui::SimpleMenuModel::Delegate {
  public:
-  ArgMaxOpModel(std::initializer_list<int> input_shape, TensorType input_type,
-                TensorType output_type, TensorType index_output_type)
-      : ArgBaseOpModel<T>(input_shape, input_type, output_type,
-                          index_output_type) {
-    ArgBaseOpModel<T>::SetBuiltinOp(
-        BuiltinOperator_ARG_MAX, BuiltinOptions_ArgMaxOptions,
-        CreateArgMaxOptions(ArgBaseOpModel<T>::builder_, index_output_type)
-            .Union());
-    ArgBaseOpModel<T>::BuildInterpreter({input_shape, {1, 1, 1, 1}});
-  }
-};
-    
-      double ComputePrimalLoss(const double wx, const double example_label,
-                           const double example_weight) const final {
-    const double y_wx = example_label * wx;
-    if (y_wx >= 1) return 0;
-    if (y_wx <= 1 - gamma) return (1 - y_wx - gamma / 2) * example_weight;
-    return (1 - y_wx) * (1 - y_wx) * example_weight * 0.5 / gamma;
-  }
-    
-    #include 'third_party/eigen3/unsupported/Eigen/CXX11/Tensor'
-#include 'tensorflow/core/framework/tensor_types.h'
-#include 'tensorflow/core/framework/types.h'
-#include 'tensorflow/core/kernels/scatter_functor.h'
-    
-      // If we have a full slice along dimension 'd'.
-  bool IsFullAt(int d) const {
-    return lengths_[d] == kFullExtent && starts_[d] == 0;
-  }
-    
-    namespace tensorflow {
-#define REGISTER_COMPLEX(D, R, C)                         \
-  REGISTER_KERNEL_BUILDER(Name('Angle')                   \
-                              .Device(DEVICE_##D)         \
-                              .TypeConstraint<C>('T')     \
-                              .TypeConstraint<R>('Tout'), \
-                          UnaryOp<D##Device, functor::get_angle<C>>);
+  MenuDelegate(ObjectManager* object_manager);
+  ~MenuDelegate() override;
     }
     
-    // Adds resource events for a single device.
-void AddResourceMetadata(uint32 device_id,
-                         const std::map<uint32, const Resource *> &resources,
-                         string *json) {
-  for (const auto &pair : resources) {
-    uint32 resource_id = pair.first;
-    const Resource &resource = *pair.second;
-    if (!resource.name().empty()) {
-      Appendf(json,
-              R'({'ph':'M','pid':%u,'tid':%u,)'
-              R'('name':'thread_name','args':{)',
-              device_id, resource_id);
-      AppendEscapedName(json, resource.name());
-      Appendf(json, '}},');
-    }
-    Appendf(json,
-            R'({'ph':'M','pid':%u,'tid':%u,)'
-            R'('name':'thread_sort_index','args':{'sort_index':%u}},)',
-            device_id, resource_id, resource_id);
-  }
+    
+    {  gtk_widget_show(menu_);
+  g_object_ref_sink(G_OBJECT(menu_));
 }
-    
-    #include 'db/builder.h'
-    
-      inline void ClearSavedValue() {
-    if (saved_value_.capacity() > 1048576) {
-      std::string empty;
-      swap(empty, saved_value_);
-    } else {
-      saved_value_.clear();
-    }
-  }
-    
-    class Env;
-    
-      VersionEdit edit;
-  for (int i = 0; i < 4; i++) {
-    TestEncodeDecode(edit);
-    edit.AddFile(3, kBig + 300 + i, kBig + 400 + i,
-                 InternalKey('foo', kBig + 500 + i, kTypeValue),
-                 InternalKey('zoo', kBig + 600 + i, kTypeDeletion));
-    edit.DeleteFile(4, kBig + 700 + i);
-    edit.SetCompactPointer(i, InternalKey('x', kBig + 900 + i, kTypeValue));
-  }
-    
-    void WriteBatch::Delete(const Slice& key) {
-  WriteBatchInternal::SetCount(this, WriteBatchInternal::Count(this) + 1);
-  rep_.push_back(static_cast<char>(kTypeDeletion));
-  PutLengthPrefixedSlice(&rep_, key);
-}
-    
-    #include <stdio.h>
-#include <stdlib.h>
-#include <kcpolydb.h>
-#include 'util/histogram.h'
-#include 'util/random.h'
-#include 'util/testutil.h'
-    
-    namespace leveldb {
-    }
-    
-    inline bool operator!=(const Slice& x, const Slice& y) {
-  return !(x == y);
-}
-    
-    
-    {  delete db;
-  delete [] pixels;
-}
-    
-    /**
- * @brief Applies common transformations to the input data, such as
- * scaling, mirroring, substracting the image mean...
- */
-template <typename Dtype>
-class DataTransformer {
- public:
-  explicit DataTransformer(const TransformationParameter& param, Phase phase);
-  virtual ~DataTransformer() {}
-    }
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-      int num_kernels_im2col_;
-  int num_kernels_col2im_;
-  int conv_out_channels_;
-  int conv_in_channels_;
-  int conv_out_spatial_dim_;
-  int kernel_dim_;
-  int col_offset_;
-  int output_offset_;
-    
-     protected:
-  /**
-   * @param bottom input Blob vector (length 2+)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x_1 @f$
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x_2 @f$
-   *   -# ...
-   *   - K @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x_K @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (KN \times C \times H \times W) @f$ if axis == 0, or
-   *      @f$ (N \times KC \times H \times W) @f$ if axis == 1:
-   *      the concatenated output @f$
-   *        y = [\begin{array}{cccc} x_1 & x_2 & ... & x_K \end{array}]
-   *      @f$
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-    
-    { protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual inline bool reverse_dimensions() { return false; }
-  virtual void compute_output_shape();
-};
-    
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    #endif  // CAFFE_CUDNN_LCN_LAYER_HPP_
-
-    
-    
-    {  int size_;
-  Dtype alpha_, beta_, k_;
-};
-#endif
-    
-    #include <vector>
-    
-     public:
-  // Constructs an empty Message.
-  Message();
-    
-    // Implements printing an array type T[N].
-template <typename T, size_t N>
-class UniversalPrinter<T[N]> {
- public:
-  // Prints the given array, omitting some elements when there are too
-  // many.
-  static void Print(const T (&a)[N], ::std::ostream* os) {
-    UniversalPrintArray(a, N, os);
-  }
-};
-    
-    // Then, use TYPED_TEST_P() to define as many type-parameterized tests
-// for this type-parameterized test case as you want.
-TYPED_TEST_P(FooTest, DoesBlah) {
-  // Inside a test, refer to TypeParam to get the type parameter.
-  TypeParam n = 0;
-  ...
-}
-    
-    
-    {  GTEST_DISALLOW_ASSIGN_(AssertionResult);
-};
-    
-    // Internal macro for implementing {EXPECT|ASSERT}_PRED_FORMAT3.
-// Don't use this in your code.
-#define GTEST_PRED_FORMAT3_(pred_format, v1, v2, v3, on_failure)\
-  GTEST_ASSERT_(pred_format(#v1, #v2, #v3, v1, v2, v3), \
-                on_failure)
-    
-    // Utilities for native arrays.
-    
-      template <GTEST_3_TYPENAMES_(U)>
-  tuple& operator=(const GTEST_3_TUPLE_(U)& t) {
-    return CopyFrom(t);
-  }
-    
-    template <typename Tuple> struct tuple_size;
-    
-    // Type utilities needed for implementing typed and type-parameterized
-// tests.  This file is generated by a SCRIPT.  DO NOT EDIT BY HAND!
-//
-// Currently we support at most 50 types in a list, and at most 50
-// type-parameterized tests in one type-parameterized test case.
-// Please contact googletestframework@googlegroups.com if you need
-// more.
-    
-    namespace {
-    }
-    
-    #include 'test/cpp/qps/benchmark_config.h'
-#include 'test/cpp/qps/driver.h'
-#include 'test/cpp/qps/report.h'
-#include 'test/cpp/qps/server.h'
-#include 'test/cpp/util/test_config.h'
-#include 'test/cpp/util/test_credentials_provider.h'
-    
-    
-    {  bool failed_ = false;
-  std::string db_;
-  size_t current_ = 0;
-  const std::string location_ = '\'location\':';
-  const std::string latitude_ = '\'latitude\':';
-  const std::string longitude_ = '\'longitude\':';
-  const std::string name_ = '\'name\':';
-};
-    
-    
-    
-    #endif  // GRPC_INTERNAL_COMPILER_PYTHON_GENERATOR_H
-
-    
-    #include 'src/compiler/python_generator.h'
-#include 'src/compiler/schema_interface.h'
     
     
     {
-    {}  // namespace
-}  // namespace grpc
+    {    // build keyboard code
+    ui::DomCode domCode = ui::KeycodeConverter::CodeStringToDomCode(keyName.c_str());
+    retval = ui::DomCodeToUsLayoutKeyboardCode(domCode);
+  }
+  return retval;
+}
     
-    class SubProcess {
- public:
-  SubProcess(const std::vector<std::string>& args);
-  ~SubProcess();
+    void MenuItem::UpdateKeys(views::FocusManager *focus_manager){
+  if (focus_manager == NULL){
+    return ;
+  } else {
+    focus_manager_ = focus_manager;
+    if (enable_shortcut_){
+      focus_manager->RegisterAccelerator(
+        accelerator_,
+        ui::AcceleratorManager::kHighPriority,
+        this);
     }
-    
-        // We need a width for the skyline algorithm. Using a dumb heuristic here to decide of width. User can override TexDesiredWidth and TexGlyphPadding if they wish.
-    // Width doesn't really matter much, but some API/GPU have texture size limitations and increasing width can decrease height.
-    atlas->TexWidth = (atlas->TexDesiredWidth > 0) ? atlas->TexDesiredWidth : (total_glyphs_count > 4000) ? 4096 : (total_glyphs_count > 2000) ? 2048 : (total_glyphs_count > 1000) ? 1024 : 512;
-    
-    int main(int argc, char** argv)
-{ 
-    // Create GLUT window
-    glutInit(&argc, argv);
-    glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_MULTISAMPLE);
-    glutInitWindowSize(1280, 720);
-    glutCreateWindow('Dear ImGui FreeGLUT+OpenGL2 Example');
+    if (submenu_ != NULL){
+      submenu_->UpdateKeys(focus_manager);
     }
+  }
+}
     
-            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+     /*
+  *   LOCATION:    see http://www.boost.org for most recent version.
+  *   FILE         match_results.cpp
+  *   VERSION      see <boost/version.hpp>
+  *   DESCRIPTION: Declares template class match_results.
+  */
     
-        // Main loop
-    MSG msg;
-    ZeroMemory(&msg, sizeof(msg));
-    while (msg.message != WM_QUIT)
-    {
-        // Poll and handle messages (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-        if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-            continue;
-        }
-    }
-    
-    
-    {} // namespace detail
-    
-    template <typename Base>
-struct HybridTraits<
-    Base,
-    typename std::enable_if<std::is_base_of<BaseHybridClass, Base>::value>::type> {
- using CxxBase = Base;
- using JavaBase = typename Base::JavaPart;
-};
-    
-    template <typename M, M method, typename C, typename... Args>
-inline NativeMethodWrapper* exceptionWrapJNIMethod(
-    void (C::*method0)(Args... args)) {
-  (void)method0;
-  struct funcWrapper {
-    JNI_ENTRY_POINT static void call(JNIEnv* env, jobject obj,
-                                     typename Convert<typename std::decay<Args>::type>::jniType... args) {
-      try {
-        try {
-          auto aref = wrap_alias(static_cast<typename C::jhybridobject>(obj));
-          // This is usually a noop, but if the hybrid object is a
-          // base class of other classes which register JNI methods,
-          // this will get the right type for the registered method.
-          auto cobj = static_cast<C*>(facebook::jni::cthis(aref));
-          (cobj->*method)(Convert<typename std::decay<Args>::type>::fromJni(args)...);
-        } catch (const std::exception& ex) {
-          C::mapException(ex);
-          throw;
-        }
-      } catch (...) {
-        translatePendingCppExceptionToJavaException();
-      }
-    }
-  };
-    }
-    
-    WIN_EXPORT bool YGNodeCanUseCachedMeasurement(
-    const YGMeasureMode widthMode,
-    const float width,
-    const YGMeasureMode heightMode,
-    const float height,
-    const YGMeasureMode lastWidthMode,
-    const float lastWidth,
-    const YGMeasureMode lastHeightMode,
-    const float lastHeight,
-    const float lastComputedWidth,
-    const float lastComputedHeight,
-    const float marginRow,
-    const float marginColumn,
-    const YGConfigRef config);
-    
-    bool Config::isExperimentalFeatureEnabled(int feature) const
+    template <class T>
+class concrete_protected_call
+   : public abstract_protected_call
 {
-    return YGConfigIsExperimentalFeatureEnabled(m_config, static_cast<YGExperimentalFeature>(feature));
-}
+public:
+   typedef bool (T::*proc_type)();
+   concrete_protected_call(T* o, proc_type p)
+      : obj(o), proc(p) {}
+private:
+   virtual bool call()const;
+   T* obj;
+   proc_type proc;
+};
+    
+    
+    
+    
+    {#ifdef BOOST_NO_STD_ITERATOR
+   typedef std::ptrdiff_t difference_type;
+   typedef typename S::value_type value_type;
+   typedef value_type* pointer;
+   typedef value_type& reference;
+   typedef std::output_iterator_tag iterator_category;
+#endif
+};
+    
+     /*
+  *   LOCATION:    see http://www.boost.org for most recent version.
+  *   FILE         regex_fwd.cpp
+  *   VERSION      see <boost/version.hpp>
+  *   DESCRIPTION: Forward declares boost::basic_regex<> and
+  *                associated typedefs.
+  */
+    
+    
 
     
-        method(setAlignContent);
-    method(setAlignItems);
-    method(setAlignSelf);
-    method(setFlexDirection);
-    method(setFlexWrap);
-    method(setJustifyContent);
-    
-    namespace facebook {
-    }
-    
-    namespace facebook {
-namespace alog {
-    }
-    }
+    #endif  // BOOST_REGEX_V4_REGEX_REPLACE_HPP
