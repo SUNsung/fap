e2be7c062b0b453df0da1b3cd47daa0ce4ae599c
SUNsung <weatherius@gmail.com>
2018-08-31 03:57:17 +0300

    Rewrite [53v.ecNqy6m6653wXClxLYAm0o53qqMcCHzQziI53yMe.cpp] File in: 2018-08-31 03:57:16

--- cccccc/53v.ecNqy6m6653wXClxLYAm0o53qqMcCHzQziI53yMe.cpp
+++ cccccc/53v.ecNqy6m6653wXClxLYAm0o53qqMcCHzQziI53yMe.cpp
@@ -1,114 +1,383 @@
 
         
-        #define THC_GENERIC_FILE 'torch/csrc/generic/utils.cpp'
-#include <THC/THCGenerateAllTypes.h>
+        Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
+#define TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
     
-    PyObject * THPUtils_dispatchStateless(
-    PyObject *tensor, const char *name, PyObject *args, PyObject *kwargs)
-{
-  THPObjectPtr methods(PyObject_GetAttrString(tensor, THP_STATELESS_ATTRIBUTE_NAME));
-  if (!methods) {
-    return PyErr_Format(
-        PyExc_TypeError,
-        'Type %s doesn't implement stateless methods',
-        classOrTypename(tensor));
-  }
-  THPObjectPtr method(PyObject_GetAttrString(methods, name));
-  if (!method) {
-    return PyErr_Format(
-        PyExc_TypeError,
-        'Type %s doesn't implement stateless method %s',
-        classOrTypename(tensor),
-        name);
-  }
-  return PyObject_Call(method.get(), args, kwargs);
-}
+    // Add a _ to the end of s if necessary to avoid a Python keyword or built-in.
+string AvoidPythonReserved(const string& s);
     
-        auto* output_data = output->template mutable_data<float>();
-    size_t block_size = input.size_from_dim(1);
-    size_t n_blocks = input.dim(0);
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    PyObject * THPWrapper_New(void *data, void (*destructor)(void*))
-{
-  PyObject *args = PyTuple_New(0);
-  if (!args) {
-    return NULL;
-  }
-  PyObject *result = PyObject_Call(THPWrapperClass, args, NULL);
-  if (result) {
-    THPWrapper* wrapper = (THPWrapper*) result;
-    wrapper->data = data;
-    wrapper->destructor = destructor;
-  }
-  Py_DECREF(args);
-  return result;
-}
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
     
     
-    {    {Functions::exit, exitWorker}
-};
-    
-    #pragma once
-    
-      ~HazptrLockFreeLIFO() {
-    Node* next;
-    for (auto node = head(); node; node = next) {
-      next = node->next();
-      node->retire();
-    }
-    hazptr_cleanup<Atom>();
-  }
-    
-    /** Wide CAS.
- */
-template <typename T, template <typename> class Atom = std::atomic>
-class HazptrWideCAS {
-  struct Node : public hazptr_obj_base<Node, Atom> {
-    T val_;
-    explicit Node(T v = {}) : val_(v) {}
-  };
-    }
-    
-     public:
-  WTCallback(PrivateConstructorTag, EventBase* base) : base_(base) {}
-    
-    /**
- * Declare *allocx() and mallctl*() as weak symbols. These will be provided by
- * jemalloc if we are using jemalloc, or will be nullptr if we are using another
- * malloc implementation.
- */
-extern 'C' void* mallocx(size_t, int)
-__attribute__((__weak__));
-extern 'C' void* rallocx(void*, size_t, int)
-__attribute__((__weak__));
-extern 'C' size_t xallocx(void*, size_t, size_t, int)
-__attribute__((__weak__));
-extern 'C' size_t sallocx(const void*, int)
-__attribute__((__weak__));
-extern 'C' void dallocx(void*, int)
-__attribute__((__weak__));
-extern 'C' void sdallocx(void*, size_t, int)
-__attribute__((__weak__));
-extern 'C' size_t nallocx(size_t, int)
-__attribute__((__weak__));
-extern 'C' int mallctl(const char*, void*, size_t*, void*, size_t)
-__attribute__((__weak__));
-extern 'C' int mallctlnametomib(const char*, size_t*, size_t*)
-__attribute__((__weak__));
-extern 'C' int mallctlbymib(const size_t*, size_t, void*, size_t*, void*,
-                            size_t)
-__attribute__((__weak__));
-    
-    FOLLY_ALWAYS_INLINE int __builtin_popcount(unsigned int x) {
-  return int(__popcnt(x));
-}
-    
-    
-    {  EXPECT_EQ(0, set->size());
-  EXPECT_THROW(std::move(f).get(), std::runtime_error);
-}
+    {}  // namespace tensorflow
 
     
-    TEST(NonCopyableLambda, Function) {
-  Promise<int> promise;
+    Status TF_DataType_to_PyArray_TYPE(TF_DataType tf_datatype,
+                                   int* out_pyarray_type) {
+  switch (tf_datatype) {
+    case TF_HALF:
+      *out_pyarray_type = NPY_FLOAT16;
+      break;
+    case TF_FLOAT:
+      *out_pyarray_type = NPY_FLOAT32;
+      break;
+    case TF_DOUBLE:
+      *out_pyarray_type = NPY_FLOAT64;
+      break;
+    case TF_INT32:
+      *out_pyarray_type = NPY_INT32;
+      break;
+    case TF_UINT32:
+      *out_pyarray_type = NPY_UINT32;
+      break;
+    case TF_UINT8:
+      *out_pyarray_type = NPY_UINT8;
+      break;
+    case TF_UINT16:
+      *out_pyarray_type = NPY_UINT16;
+      break;
+    case TF_INT8:
+      *out_pyarray_type = NPY_INT8;
+      break;
+    case TF_INT16:
+      *out_pyarray_type = NPY_INT16;
+      break;
+    case TF_INT64:
+      *out_pyarray_type = NPY_INT64;
+      break;
+    case TF_UINT64:
+      *out_pyarray_type = NPY_UINT64;
+      break;
+    case TF_BOOL:
+      *out_pyarray_type = NPY_BOOL;
+      break;
+    case TF_COMPLEX64:
+      *out_pyarray_type = NPY_COMPLEX64;
+      break;
+    case TF_COMPLEX128:
+      *out_pyarray_type = NPY_COMPLEX128;
+      break;
+    case TF_STRING:
+      *out_pyarray_type = NPY_OBJECT;
+      break;
+    case TF_RESOURCE:
+      *out_pyarray_type = NPY_VOID;
+      break;
+    // TODO(keveman): These should be changed to NPY_VOID, and the type used for
+    // the resulting numpy array should be the custom struct types that we
+    // expect for quantized types.
+    case TF_QINT8:
+      *out_pyarray_type = NPY_INT8;
+      break;
+    case TF_QUINT8:
+      *out_pyarray_type = NPY_UINT8;
+      break;
+    case TF_QINT16:
+      *out_pyarray_type = NPY_INT16;
+      break;
+    case TF_QUINT16:
+      *out_pyarray_type = NPY_UINT16;
+      break;
+    case TF_QINT32:
+      *out_pyarray_type = NPY_INT32;
+      break;
+    case TF_BFLOAT16:
+      *out_pyarray_type = Bfloat16NumpyType();
+      break;
+    default:
+      return errors::Internal('Tensorflow type ', tf_datatype,
+                              ' not convertible to numpy dtype.');
+  }
+  return Status::OK();
+}
+    
+    // Must be included first
+#include 'tensorflow/python/lib/core/numpy.h'
+    
+    
+    {}  // namespace tensorflow
+
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+     private:
+  PyRecordReader();
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    #endif  // ATOM_APP_NODE_MAIN_H_
+
+    
+    // static
+void AutoUpdater::BuildPrototype(v8::Isolate* isolate,
+                                 v8::Local<v8::FunctionTemplate> prototype) {
+  prototype->SetClassName(mate::StringToV8(isolate, 'AutoUpdater'));
+  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
+      .SetMethod('checkForUpdates', &auto_updater::AutoUpdater::CheckForUpdates)
+      .SetMethod('getFeedURL', &auto_updater::AutoUpdater::GetFeedURL)
+      .SetMethod('setFeedURL', &AutoUpdater::SetFeedURL)
+      .SetMethod('quitAndInstall', &AutoUpdater::QuitAndInstall);
+}
+    
+    
+    {}  // namespace api
+    
+    using CompletionCallback = base::Callback<void(const base::FilePath&)>;
+    
+    
+    {
+    {    base::DictionaryValue* result_body = nullptr;
+    base::DictionaryValue result;
+    if (dict->GetDictionary('result', &result_body))
+      result.Swap(result_body);
+    send_command_callback.Run(error, result);
+  }
+}
+    
+      // mate::TrackableObject:
+  static void BuildPrototype(v8::Isolate* isolate,
+                             v8::Local<v8::FunctionTemplate> prototype);
+    
+    namespace api {
     }
+    
+    namespace nw {
+    }
+    
+      focus_manager_ = NULL;
+  menu_ = NULL;
+    
+    NwAppSetProxyConfigFunction::NwAppSetProxyConfigFunction() {
+}
+    
+      base::RunLoop run_loop_;
+    
+    bool NwClipboardClearSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
+  clipboard->Clear(ui::CLIPBOARD_TYPE_COPY_PASTE);
+  return true;
+}
+    
+    
+    {  private:
+    DISALLOW_COPY_AND_ASSIGN(NwScreenStopMonitorFunction);
+  };
+    
+      // Open leveldb
+  leveldb::DB* db;
+  leveldb::Options options;
+  options.create_if_missing = true;
+  options.error_if_exists = true;
+  leveldb::Status status = leveldb::DB::Open(
+      options, db_filename, &db);
+  CHECK(status.ok()) << 'Failed to open leveldb ' << db_filename
+      << '. Is it already existing?';
+    
+     protected:
+  /// @copydoc AbsValLayer
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    
+      virtual inline const char* type() const { return 'ArgMax'; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+    
+    namespace caffe {
+    }
+    
+    #endif  // CAFFE_BATCHNORM_LAYER_HPP_
+
+    
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
+    
+    #include <vector>
+    
+      int size_, pre_pad_;
+  Dtype alpha_, beta_, k_;
+    
+    #ifdef USE_CUDNN
+/*
+ * @brief cuDNN implementation of PoolingLayer.
+ *        Fallback to PoolingLayer for CPU mode.
+*/
+template <typename Dtype>
+class CuDNNPoolingLayer : public PoolingLayer<Dtype> {
+ public:
+  explicit CuDNNPoolingLayer(const LayerParameter& param)
+      : PoolingLayer<Dtype>(param), handles_setup_(false) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNPoolingLayer();
+  // Currently, cuDNN does not support the extra top blob.
+  virtual inline int MinTopBlobs() const { return -1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+    }
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #ifdef USE_CUDNN
+/**
+ * @brief CuDNN acceleration of SigmoidLayer.
+ */
+template <typename Dtype>
+class CuDNNSigmoidLayer : public SigmoidLayer<Dtype> {
+ public:
+  explicit CuDNNSigmoidLayer(const LayerParameter& param)
+      : SigmoidLayer<Dtype>(param), handles_setup_(false) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNSigmoidLayer();
+    }
+    
+    namespace caffe2 {
+    }
+    
+    template <typename T, class Context>
+class FlexibleTopKOp : public Operator<Context> {
+ public:
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+    }
+    
+    REGISTER_CPU_OPERATOR(Floor, FloorOp<float, CPUContext>);
+    
+    namespace caffe2 {
+    }
+    
+    #define MAKE_TEMPLATE_TYPE_INFO(m_template, m_type, m_var_type) \
+	template <>                                                 \
+	struct GetTypeInfo<m_template<m_type> > {                   \
+		static const Variant::Type VARIANT_TYPE = m_var_type;   \
+		static inline PropertyInfo get_class_info() {           \
+			return PropertyInfo(VARIANT_TYPE, String());        \
+		}                                                       \
+	};                                                          \
+	template <>                                                 \
+	struct GetTypeInfo<const m_template<m_type> &> {            \
+		static const Variant::Type VARIANT_TYPE = m_var_type;   \
+		static inline PropertyInfo get_class_info() {           \
+			return PropertyInfo(VARIANT_TYPE, String());        \
+		}                                                       \
+	};
+    
+    		} else if (read == 0) {
+    
+    
+    {	vsyslog(p_err ? LOG_ERR : LOG_INFO, p_format, p_list);
+}
+    
+    	DWORD fileAttr;
+    
+    	virtual Error make_dir(String p_dir);
+    
+    	static Mutex *create_func_windows(bool p_recursive);
+    
+    #endif
+
+    
+            static float f = 0.0f;
+        ImGui::Text('Hello, world!');
+        ImGui::SliderFloat('float', &f, 0.0f, 1.0f);
+        ImGui::Text('Application average %.3f ms/frame (%.1f FPS)', 1000.0f / io.Framerate, io.Framerate);
+        ImGui::ShowDemoWindow(NULL);
+    
+        // Setup inputs
+    ALLEGRO_KEYBOARD_STATE keys;
+    al_get_keyboard_state(&keys);
+    io.KeyCtrl = al_key_down(&keys, ALLEGRO_KEY_LCTRL) || al_key_down(&keys, ALLEGRO_KEY_RCTRL);
+    io.KeyShift = al_key_down(&keys, ALLEGRO_KEY_LSHIFT) || al_key_down(&keys, ALLEGRO_KEY_RSHIFT);
+    io.KeyAlt = al_key_down(&keys, ALLEGRO_KEY_ALT) || al_key_down(&keys, ALLEGRO_KEY_ALTGR);
+    io.KeySuper = al_key_down(&keys, ALLEGRO_KEY_LWIN) || al_key_down(&keys, ALLEGRO_KEY_RWIN);
+    
+            // Start the Dear ImGui frame
+        ImGui_Marmalade_NewFrame();
+        ImGui::NewFrame();
+    
+        // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them. 
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple. 
+    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Read 'misc/fonts/README.txt' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Roboto-Medium.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Cousine-Regular.ttf', 15.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/DroidSans.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/ProggyTiny.ttf', 10.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF('c:\\Windows\\Fonts\\ArialUni.ttf', 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+    //IM_ASSERT(font != NULL);
+    
+        // Setup Dear ImGui binding
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+    
+    extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+        return true;
+    }
+    
+    //---- Define attributes of all API symbols declarations, e.g. for DLL under Windows.
+//#define IMGUI_API __declspec( dllexport )
+//#define IMGUI_API __declspec( dllimport )
+    
+    // Data
+static SDL_Window*  g_Window = NULL;
+static Uint64       g_Time = 0;
+static bool         g_MousePressed[3] = { false, false, false };
+static SDL_Cursor*  g_MouseCursors[ImGuiMouseCursor_COUNT] = { 0 };
+static char*        g_ClipboardTextData = NULL;
+    
+    static void ImGui_ImplDX11_CreateFontsTexture()
+{
+    // Build texture atlas
+    ImGuiIO& io = ImGui::GetIO();
+    unsigned char* pixels;
+    int width, height;
+    io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height);
+    }
+    
+            D3D12_RESOURCE_BARRIER barrier = {};
+        barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+        barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+        barrier.Transition.pResource   = pTexture;
+        barrier.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+        barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_COPY_DEST;
+        barrier.Transition.StateAfter  = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
