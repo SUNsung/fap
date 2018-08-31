3ce7379bcbb9b2bfd909f6cfcbe536a299e109c1
SUNsung <weatherius@gmail.com>
2018-08-31 20:35:27 +0300

    Rewrite [32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb] File in: 2018-08-31 20:35:27

--- ruby/32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb
+++ ruby/32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb
@@ -1,96 +1,129 @@
 
         
-          # True if a {Formula} is being built without a specific option.
-  # <pre>args << '--no-spam-plz' if build.without? 'spam'
-  def without?(val)
-    !with?(val)
+            group = Group.create!(name: 'bob')
+    group.add(moderator)
+    group.save
+    
+          if staff.topic_id.nil?
+        creator = PostCreator.new(Discourse.system_user,
+          raw: I18n.t('staff_category_description'),
+          title: I18n.t('category.topic_prefix', category: staff.name),
+          category: staff.name,
+          archetype: Archetype.default
+        )
+        post = creator.create
+    
+      def after_omniauth_failure_path_for(scope)
+    new_session_path(scope)
   end
     
-            begin
-          f = Formulary.from_rack(HOMEBREW_CELLAR/name)
-        rescue FormulaUnavailableError, TapFormulaAmbiguityError, TapFormulaWithOldnameAmbiguityError
-          next
+      # Proxy to devise map name
+  def resource_name
+    devise_mapping.name
+  end
+  alias :scope_name :resource_name
+    
+    class TestController < ApplicationController
+  include Rails.application.routes.url_helpers
+    
+              def #{mapping}_session
+            current_#{mapping} && warden.session(:#{mapping})
+          end
+        METHODS
+    
+          def self.generate_helpers!(routes=nil)
+        routes ||= begin
+          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
+          Devise::URL_HELPERS.slice(*mappings)
         end
     
-        version 'JavaScript' do
-      self.base_url = 'https://api.haxe.org/js/'
+    $stderr.puts <<DEPRECATION
+WARNING: Ruby Sass's Git repository is moving, and the old repository will be
+deled on 26 March 2019! Please update your Git URLs to point to the new
+repository at https://github.com/sass/ruby-sass.
+    
+          # Returns the time the given Sass file was last modified.
+      #
+      # If the given file has been deleted or the time can't be accessed
+      # for some other reason, this should return nil.
+      #
+      # @param uri [String] The URI of the file to check.
+      #   Comes from a `:filename` option set on an engine returned by this importer.
+      # @param options [{Symbol => Object}] Options for the Sass file
+      #   containing the `@import` currently being checked.
+      # @return [Time, nil]
+      def mtime(uri, options)
+        Sass::Util.abstract(self)
+      end
+    
+            found = possible_files(remove_root(name)).map do |f, s|
+          path = if dir == '.' || Sass::Util.pathname(f).absolute?
+                   f
+                 else
+                   '#{escape_glob_characters(dir)}/#{f}'
+                 end
+          Dir[path].map do |full_path|
+            full_path.gsub!(REDUNDANT_DIRECTORY, File::SEPARATOR)
+            [Sass::Util.cleanpath(full_path).to_s, s]
+          end
+        end.flatten(1)
+        return if found.empty?
+    
+            def destroy
+          @option_value = scope.accessible_by(current_ability, :destroy).find(params[:id])
+          @option_value.destroy
+          render plain: nil, status: 204
+        end
+    
+            def index
+          if params[:ids]
+            @products = product_scope.where(id: params[:ids].split(',').flatten)
+          else
+            @products = product_scope.ransack(params[:q]).result
+          end
+    
+            def remove
+          quantity = if params.key?(:quantity)
+                       params[:quantity].to_i
+                     else
+                       @shipment.inventory_units_for(variant).sum(:quantity)
+                     end
+    
+    shared_examples_for 'multiline literal brace layout method argument' do
+  include MultilineLiteralBraceHelper
+    
+          # Custom destructuring method. This can be used to normalize
+      # destructuring for different variations of the node.
+      #
+      # In this case, the `def` node destructures into:
+      #
+      #   `method_name, arguments, body`
+      #
+      # while the `defs` node destructures into:
+      #
+      #   `receiver, method_name, arguments, body`
+      #
+      # so we reverse the destructured array to get the optional receiver
+      # at the end, where it can be discarded.
+      #
+      # @return [Array] the different parts of the `def` or `defs` node
+      def node_parts
+        to_a.reverse
+      end
     end
-    
-        options[:attribution] = <<-HTML
-      &copy; 2010 The Rust Project Developers<br>
-      Licensed under the Apache License, Version 2.0 or the MIT license, at your option.
-    HTML
-    
-        def initialize(name = nil, path = nil, type = nil)
-      self.name = name
-      self.path = path
-      self.type = type
-    
-      it 'considers a zero integer in expression result as true' do
-    if 0
-      123
-    else
-      456
-    end.should == 123
-  end
-    
-      def test_input_line_number_range
-    bug12947 = '[ruby-core:78162] [Bug #12947]'
-    ary = b1 = b2 = nil
-    EnvUtil.suppress_warning do
-      b1 = eval('proc {|i| i if 2..4}')
-      b2 = eval('proc {|i| if 2..4; i; end}')
-    end
-    IO.pipe {|r, w|
-      th = Thread.start {(1..5).each {|i| w.puts i};w.close}
-      ary = r.map {|i| b1.call(i.chomp)}
-      th.join
-    }
-    assert_equal([nil, '2', '3', '4', nil], ary, bug12947)
-    IO.pipe {|r, w|
-      th = Thread.start {(1..5).each {|i| w.puts i};w.close}
-      ary = r.map {|i| b2.call(i.chomp)}
-      th.join
-    }
-    assert_equal([nil, '2', '3', '4', nil], ary, bug12947)
   end
 end
 
     
-      it 'adds the directory after directories added by -I within RUBYOPT' do
-    rubyopt_dir = tmp('rubyopt_include')
-    rubylib_dir = tmp('rubylib_include')
-    ENV['RUBYLIB'] = @pre + rubylib_dir
-    paths = ruby_exe('puts $LOAD_PATH', env: { 'RUBYOPT' => '-I#{rubyopt_dir}' }).lines.map(&:chomp)
-    paths.should include(rubyopt_dir)
-    paths.should include(rubylib_dir)
-    paths.index(rubyopt_dir).should < paths.index(rubylib_dir)
-  end
-    
-      end
-    
-      def test_ri
-    @cmd.handle_options %w[--no-ri]
-    
-      def create_hash(model, opts={})
-    opts[:range] ||= 1.day
-    plural = model.to_s.underscore.pluralize
-    eval(<<DATA
-      @#{plural} = {
-        :day_before => #{model}.where(:created_at => ((Time.now.midnight - #{opts[:range]*2})..Time.now.midnight - #{opts[:range]})).count,
-        :yesterday => #{model}.where(:created_at => ((Time.now.midnight - #{opts[:range]})..Time.now.midnight)).count
-      }
-      @#{plural}[:change] = percent_change(@#{plural}[:yesterday], @#{plural}[:day_before])
-DATA
-    )
-  end
-    
-          def create
-        req = Rack::Request.new(request.env)
-        if req['client_assertion_type'] == 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
-          handle_jwt_bearer(req)
-        end
-        self.status, headers, self.response_body = Api::OpenidConnect::TokenEndpoint.new.call(request.env)
-        headers.each {|name, value| response.headers[name] = value }
-        nil
+    module RuboCop
+  module AST
+    # A node extension for `for` nodes. This will be used in place of a plain
+    # node when the builder constructs the AST, making its methods available
+    # to all `for` nodes within RuboCop.
+    class ForNode < Node
+      # Returns the keyword of the `for` statement as a string.
+      #
+      # @return [String] the keyword of the `until` statement
+      def keyword
+        'for'
       end
