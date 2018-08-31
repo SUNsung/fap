910d5499efd7a2b60dd627380224fb19e7d9eb42
SUNsung <weatherius@gmail.com>
2018-08-31 06:56:44 +0300

    Rewrite [32MbFE6szBxvA32aR3Xe2vKiA321kCjLJBZBBY32F8S.cpp] File in: 2018-08-31 06:56:43

--- cccccc/32MbFE6szBxvA32aR3Xe2vKiA321kCjLJBZBBY32F8S.cpp
+++ cccccc/32MbFE6szBxvA32aR3Xe2vKiA321kCjLJBZBBY32F8S.cpp
@@ -1,203 +1,209 @@
 
         
-          // Deletes the current owned object, if any.
-  // Then takes ownership of a new object without incrementing the reference
-  // count.
-  // This function must be called with a reference that you own.
-  //   this->reset(this->get()) is wrong!
-  //   this->reset(this->release()) is OK.
-  PyObjectStruct* reset(PyObjectStruct* p = NULL) {
-    Py_XDECREF(ptr_);
-    ptr_ = p;
-    return ptr_;
-  }
+        namespace {
+    }
     
-    // Returns the Pascal-cased last part of the proto file. For example,
-// input of 'google/protobuf/foo_bar.proto' would result in 'FooBar'.
-std::string GetFileNameBase(const FileDescriptor* descriptor) {
-    std::string proto_file = descriptor->name();
-    int lastslash = proto_file.find_last_of('/');
-    std::string base = proto_file.substr(lastslash + 1);
-    return UnderscoresToPascalCase(StripDotProto(base));
+    // static
+void UvTaskRunner::OnClose(uv_handle_t* handle) {
+  delete reinterpret_cast<uv_timer_t*>(handle);
 }
     
+    // static
+mate::WrappableBase* BoxLayout::New(mate::Arguments* args,
+                                    views::BoxLayout::Orientation orientation) {
+  auto* layout = new BoxLayout(orientation);
+  layout->InitWith(args->isolate(), args->GetThis());
+  return layout;
+}
     
-    {/*! \brief typedef the factory function of data iterator */
-typedef std::function<IIterator<DataBatch> *()> DataIteratorFactory;
-/*!
- * \brief Registry entry for DataIterator factory functions.
- */
-struct DataIteratorReg
-    : public dmlc::FunctionRegEntryBase<DataIteratorReg,
-                                        DataIteratorFactory> {
+    class BrowserView : public mate::TrackableObject<BrowserView> {
+ public:
+  static mate::WrappableBase* New(mate::Arguments* args);
+    }
+    
+      content::WebContents* web_contents_;  // Weak Reference.
+  scoped_refptr<content::DevToolsAgentHost> agent_host_;
+    
+    NODE_BUILTIN_MODULE_CONTEXT_AWARE(atom_browser_dialog, Initialize)
+
+    
+    // static
+void GlobalShortcut::BuildPrototype(v8::Isolate* isolate,
+                                    v8::Local<v8::FunctionTemplate> prototype) {
+  prototype->SetClassName(mate::StringToV8(isolate, 'GlobalShortcut'));
+  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
+      .SetMethod('register', &GlobalShortcut::Register)
+      .SetMethod('isRegistered', &GlobalShortcut::IsRegistered)
+      .SetMethod('unregister', &GlobalShortcut::Unregister)
+      .SetMethod('unregisterAll', &GlobalShortcut::UnregisterAll);
+}
+    
+    		Hand &h = hands[hand];
+    
+    	void add_message(const StringName &p_src_text, const StringName &p_xlated_text);
+	virtual StringName get_message(const StringName &p_src_text) const; //overridable for other implementations
+	void erase_message(const StringName &p_src_text);
+    
+    
+    {	static bool const value = false;
 };
-//--------------------------------------------------------------
-// The following part are API Registration of Iterators
-//--------------------------------------------------------------
-/*!
- * \brief Macro to register Iterators
- *
- * \code
- * // example of registering a mnist iterator
- * REGISTER_IO_ITE(MNISTIter)
- * .describe('Mnist data iterator')
- * .set_body([]() {
- *     return new PrefetcherIter(new MNISTIter());
- *   });
- * \endcode
- */
-#define MXNET_REGISTER_IO_ITER(name)                                    \
-  DMLC_REGISTRY_REGISTER(::mxnet::DataIteratorReg, DataIteratorReg, name)
-}  // namespace mxnet
-#endif  // MXNET_IO_H_
-
     
-    #endif  // PLUGIN_CAFFE_CAFFE_BLOB_H_
-
+    			p_store_string_func(p_store_string_ud, 'PoolVector2Array( ');
+			PoolVector<Vector2> data = p_variant;
+			int len = data.size();
+			PoolVector<Vector2>::Read r = data.read();
+			const Vector2 *ptr = r.ptr();
+    
+    class RasterizerCanvasGLES3 : public RasterizerCanvas {
+public:
+	struct CanvasItemUBO {
+    }
+    }
     
     
-    {
-    {}  // namespace op
-}  // namespace mxnet
-
+    {	sem_destroy(&sem);
+}
+    
+    	const char *err_type = '**ERROR**';
+	switch (p_type) {
+		case ERR_ERROR: err_type = '**ERROR**'; break;
+		case ERR_WARNING: err_type = '**WARNING**'; break;
+		case ERR_SCRIPT: err_type = '**SCRIPT ERROR**'; break;
+		case ERR_SHADER: err_type = '**SHADER ERROR**'; break;
+		default: ERR_PRINT('Unknown error type'); break;
+	}
+    
+    	_cisdir = (p->fu.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY);
+	_cishidden = (p->fu.dwFileAttributes & FILE_ATTRIBUTE_HIDDEN);
+    
+    #ifdef WINDOWS_ENABLED
+    
+    public:
+	virtual void read_lock();
+	virtual void read_unlock();
+	virtual Error read_try_lock();
+    
+    #include 'stream_peer_tcp_winsock.h'
     
     /*!
- * \brief Operator used in ThreadedEngine.
+ * \file graph_attr_types.h
+ * \brief Data structures that can appear in graph attributes.
  */
-struct ThreadedOpr final : public Opr,
-                           public common::ObjectPoolAllocatable<ThreadedOpr> {
-  /*! \brief The function to be invoked each time. */
-  Engine::AsyncFn fn;
-  /*! \brief The variable this operation will read from. */
-  std::vector<ThreadedVar*> const_vars;
-  /*! \brief The variable this operation will mutate. */
-  std::vector<ThreadedVar*> mutable_vars;
-  /*! \brief The property of the operator */
-  FnProperty prop;
-  /*! \brief The name of the operator */
-  const char* opr_name{nullptr};
-  /*!
-   * \brief Whether this is an temporary operator
-   *        that can be deleted right after the operation completed.
-   */
-  bool temporary{false};
-  /*!
-   * \brief Whether this is a WaitForVar operation
-   */
-  bool wait{false};
-  /*!
-   * \brief Cast a Opr pointer to ThreadedOpr pointer
-   * \param ptr pointer from base.
-   * \return a casted pointer.
-   */
-  inline static ThreadedOpr* CastFromBase(Opr* ptr) {
-    return ptr->Cast<ThreadedOpr>();
-  }
-  // define possible debug information
-  DEFINE_ENGINE_DEBUG_INFO(ThreadedOpr);
-  /*! \brief exception_ptr associated with the ThreadedOpr */
-  std::shared_ptr<std::exception_ptr> opr_exception;
-};  // struct ThreadedOpr
+#ifndef MXNET_GRAPH_ATTR_TYPES_H_
+#define MXNET_GRAPH_ATTR_TYPES_H_
     
-      // get the shape hints
-  std::string shape_hints_key = std::string(attr_name) + '_hints';
-  if (ret.attrs.count(shape_hints_key)) {
-    nnvm::NodeEntryMap<AttrType> shape_hints =
-      ret.GetAttr<nnvm::NodeEntryMap<AttrType>>(shape_hints_key);
-    for (const auto& kv : shape_hints) {
-      nnvm::NodeEntry e = kv.first;
-      if (idx.exist(e.node.get())) {
-        rshape[idx.entry_id(kv.first)] = kv.second;
+    /*! \brief a single data instance */
+struct DataInst {
+  /*! \brief unique id for instance */
+  unsigned index;
+  /*! \brief content of data */
+  std::vector<TBlob> data;
+  /*! \brief extra data to be fed to the network */
+  std::string extra_data;
+};  // struct DataInst
+    
+    namespace mxnet {
+namespace op {
+    }
+    }
+    
+    // specialize define for Layer Parameter
+template<>
+class FieldEntry<caffe::LayerParameter>
+    : public FieldEntryBase<FieldEntry<caffe::LayerParameter>, caffe::LayerParameter> {
+ public:
+  // parent class
+  typedef FieldEntryBase<FieldEntry<caffe::LayerParameter>, caffe::LayerParameter> Parent;
+    }
+    
+    // Decalre Factory function, used for dispatch specialization
+template<typename xpu>
+Operator* CreateOp(CaffeLossParam param, int);
+    
+    OpenMP *OpenMP::Get() {
+  static OpenMP openMP;
+  return &openMP;
+}
+    
+    template <std::size_t kNumGpus, std::size_t kStreams>
+StreamManager<kNumGpus, kStreams>::StreamManager() {
+#if MXNET_USE_CUDA
+  for (std::size_t i = 0; i < kNumGpus; ++i) {
+    gpu_cnt_.at(i) = -1;
+  }
+  for (auto&& i : gpu_io_streams_) {
+    i = nullptr;
+  }
+#endif  // MXNET_USE_CUDA
+}
+    
+    /*!
+ * \brief Thread pool.
+ */
+class ThreadPool {
+ public:
+  /*! \brief Signal event upon destruction, even for exceptions (RAII) */
+  struct SetReadyOnDestroy {
+    explicit inline SetReadyOnDestroy(const std::shared_ptr<dmlc::ManualEvent>& event)
+      : event_(event) {
+    }
+    inline ~SetReadyOnDestroy() {
+      if (event_) {
+        event_->signal();
       }
     }
+    std::shared_ptr<dmlc::ManualEvent>  event_;
+  };
+    }
+    
+    /// Root key to retrieve Kafka topic configurations.
+extern const std::string kKafkaTopicParserRootKey;
+    
+      ASSERT_TRUE(data.doc().HasMember('events'));
+  ASSERT_TRUE(data.doc()['events'].HasMember('environment_variables'));
+  ASSERT_TRUE(data.doc()['events']['environment_variables'].IsArray());
+  for (const auto& var :
+       data.doc()['events']['environment_variables'].GetArray()) {
+    std::string value = var.GetString();
+    EXPECT_TRUE(value == 'foo' || value == 'bar');
   }
     
-    /*!
- * \brief Registry entry for linear updater.
+      fpack.platform_ = 'bad_value';
+  EXPECT_FALSE(fpack.checkPlatform());
+    
+    
+    {/**
+ * @brief Compute a hash digest from the contents of a buffer.
+ *
+ * @param hash_type The osquery-supported hash algorithm.
+ * @param buffer A caller-controlled buffer (already allocated).
+ * @param size The length of buffer in bytes.
+ * @return A string (hex) representation of the hash digest.
  */
-struct LinearUpdaterReg
-    : public dmlc::FunctionRegEntryBase<LinearUpdaterReg,
-                                        std::function<LinearUpdater*()> > {};
+std::string hashFromBuffer(HashType hash_type, const void* buffer, size_t size);
+} // namespace osquery
+
     
     
-namespace xgboost {
-    }
-    
-    DMLC_REGISTRY_FILE_TAG(sparse_page_lz4_format);
-    
-    
-    {    elem_of_each_node_[left_node_id] = Elem(begin, split_pt, left_node_id);
-    elem_of_each_node_[right_node_id] = Elem(split_pt, e.end, right_node_id);
-    elem_of_each_node_[node_id] = Elem(nullptr, nullptr, -1);
+    {    // Code should never reach this point
+    VLOG(1) << 'Could not start extension process: ' << exec_path;
+    Initializer::shutdown(EXIT_FAILURE);
+    return std::shared_ptr<PlatformProcess>();
   }
     
-    class SparsePageDMatrix : public DMatrix {
- public:
-  explicit SparsePageDMatrix(std::unique_ptr<DataSource>&& source,
-                             std::string  cache_info)
-      : source_(std::move(source)), cache_info_(std::move(cache_info)) {
-  }
+    #include <osquery/database.h>
+#include <osquery/dispatcher.h>
+#include <osquery/flags.h>
+    
+    #include <boost/filesystem.hpp>
+    
+    
+    {        return value;
     }
     
-    int main(int, char**)
-{
-    IwGxInit();
-    }
     
-            // Rendering
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        //glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound, but prefer using the GL3+ code.
-        ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
-    
-    HRESULT CreateDeviceD3D(HWND hWnd)
-{
-    // Setup swap chain
-    DXGI_SWAP_CHAIN_DESC1 sd;
     {
-        ZeroMemory(&sd, sizeof(sd));
-        sd.BufferCount = NUM_BACK_BUFFERS;
-        sd.Width = 0;
-        sd.Height = 0;
-        sd.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-        sd.Flags = DXGI_SWAP_CHAIN_FLAG_FRAME_LATENCY_WAITABLE_OBJECT;
-        sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-        sd.SampleDesc.Count = 1;
-        sd.SampleDesc.Quality = 0;
-        sd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
-        sd.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
-        sd.Scaling = DXGI_SCALING_STRETCH;
-        sd.Stereo = FALSE;
-    }
-    }
-    
-    
-    {    ImGuiMouseCursor imgui_cursor = ImGui::GetMouseCursor();
-    if (imgui_cursor == ImGuiMouseCursor_None || io.MouseDrawCursor)
-    {
-        // Hide OS mouse cursor if imgui is drawing it or if it wants no cursor
-        glfwSetInputMode(g_Window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
-    }
-    else
-    {
-        // Show OS mouse cursor
-        // FIXME-PLATFORM: Unfocused windows seems to fail changing the mouse cursor with GLFW 3.2, but 3.3 works here.
-        glfwSetCursor(g_Window, g_MouseCursors[imgui_cursor] ? g_MouseCursors[imgui_cursor] : g_MouseCursors[ImGuiMouseCursor_Arrow]);
-        glfwSetInputMode(g_Window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-    }
-}
-    
-            D3D12_ROOT_SIGNATURE_DESC desc = {};
-        desc.NumParameters = _countof(param);
-        desc.pParameters = param;
-        desc.NumStaticSamplers = 1;
-        desc.pStaticSamplers = &staticSampler;
-        desc.Flags =
-            D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT |
-            D3D12_ROOT_SIGNATURE_FLAG_DENY_HULL_SHADER_ROOT_ACCESS |
-            D3D12_ROOT_SIGNATURE_FLAG_DENY_DOMAIN_SHADER_ROOT_ACCESS |
-            D3D12_ROOT_SIGNATURE_FLAG_DENY_GEOMETRY_SHADER_ROOT_ACCESS;
+    {        BOOST_DEFAULTED_FUNCTION(aligned(), {})
+        BOOST_FORCEINLINE BOOST_CONSTEXPR explicit aligned(type v) BOOST_NOEXCEPT : value(v) {}
+    };
+};
