2c1df7ad42d3debaef66a33880b037d3aab15b5b
SUNsung <weatherius@gmail.com>
2018-09-02 00:31:04 +0300

    Rewrite [31nUraG77L8PI31pFPLGk19vnM31yVuPUV6Moy.31wiK.py] File in: 2018-09-02 00:31:03

--- pyton/31nUraG77L8PI31pFPLGk19vnM31yVuPUV6Moy.31wiK.py
+++ pyton/31nUraG77L8PI31pFPLGk19vnM31yVuPUV6Moy.31wiK.py
@@ -1,131 +1,95 @@
 
         
-                # because special names such as Name.Class, Name.Function, etc.
-        # are not recognized as such later in the parsing, we choose them
-        # to look the same as ordinary variables.
-        Name:                      '#000000',        # class: 'n'
-        Name.Attribute:            '#c4a000',        # class: 'na' - to be revised
-        Name.Builtin:              '#004461',        # class: 'nb'
-        Name.Builtin.Pseudo:       '#3465a4',        # class: 'bp'
-        Name.Class:                '#000000',        # class: 'nc' - to be revised
-        Name.Constant:             '#000000',        # class: 'no' - to be revised
-        Name.Decorator:            '#888',           # class: 'nd' - to be revised
-        Name.Entity:               '#ce5c00',        # class: 'ni'
-        Name.Exception:            'bold #cc0000',   # class: 'ne'
-        Name.Function:             '#000000',        # class: 'nf'
-        Name.Property:             '#000000',        # class: 'py'
-        Name.Label:                '#f57900',        # class: 'nl'
-        Name.Namespace:            '#000000',        # class: 'nn' - to be revised
-        Name.Other:                '#000000',        # class: 'nx'
-        Name.Tag:                  'bold #004461',   # class: 'nt' - like a keyword
-        Name.Variable:             '#000000',        # class: 'nv' - to be revised
-        Name.Variable.Class:       '#000000',        # class: 'vc' - to be revised
-        Name.Variable.Global:      '#000000',        # class: 'vg' - to be revised
-        Name.Variable.Instance:    '#000000',        # class: 'vi' - to be revised
+            def _migrate_implicit_content_type(self):
+        '''Migrate the removed implicit_content_type config option'''
+        try:
+            implicit_content_type = self.pop('implicit_content_type')
+        except KeyError:
+            self.save()
+        else:
+            if implicit_content_type == 'form':
+                self['default_options'].insert(0, '--form')
+            self.save()
+            self.load()
+
     
-        @classmethod
-    def text_response_server(cls, text, request_timeout=0.5, **kwargs):
-        def text_response_handler(sock):
-            request_content = consume_socket_content(sock, timeout=request_timeout)
-            sock.send(text.encode('utf-8'))
+        def __repr__(self):
+        return '<{0} {1}>'.format(type(self).__name__, str(self))
+
     
-    elif is_py3:
-    from urllib.parse import urlparse, urlunparse, urljoin, urlsplit, urlencode, quote, unquote, quote_plus, unquote_plus, urldefrag
-    from urllib.request import parse_http_list, getproxies, proxy_bypass, proxy_bypass_environment, getproxies_environment
-    from http import cookiejar as cookielib
-    from http.cookies import Morsel
-    from io import StringIO
-    from collections import OrderedDict
-    from collections.abc import Callable, Mapping, MutableMapping
+        def unregister(self, plugin):
+        self._plugins.remove(plugin)
     
     
-def patch(url, data=None, **kwargs):
-    r'''Sends a PATCH request.
+@mock.patch('httpie.input.AuthCredentials._getpass',
+            new=lambda self, prompt: 'password')
+def test_password_prompt(httpbin):
+    r = http('--auth', 'user',
+             'GET', httpbin.url + '/basic-auth/user/password')
+    assert HTTP_OK in r
+    assert r.json == {'authenticated': True, 'user': 'user'}
     
-    import io
-import requests
-import pytest
-from requests.adapters import HTTPAdapter
-from requests.auth import HTTPDigestAuth, _basic_auth_str
-from requests.compat import (
-    Morsel, cookielib, getproxies, str, urlparse,
-    builtin_str, OrderedDict)
-from requests.cookies import (
-    cookiejar_from_dict, morsel_to_cookie)
-from requests.exceptions import (
-    ConnectionError, ConnectTimeout, InvalidSchema, InvalidURL,
-    MissingSchema, ReadTimeout, Timeout, RetryError, TooManyRedirects,
-    ProxyError, InvalidHeader, UnrewindableBodyError, SSLError, InvalidProxyURL)
-from requests.models import PreparedRequest
-from requests.structures import CaseInsensitiveDict
-from requests.sessions import SessionRedirectMixin
-from requests.models import urlencode
-from requests.hooks import default_hooks
-from requests.compat import MutableMapping
+            def get_auth(self, username=None, password=None):
+            assert self.raw_auth is None
+            assert username is None
+            assert password is None
+            return basic_auth()
     
-            expected = {
-            'description': 'foo_url_map description',
-            'hostRules': [
-                {
-                    'description': 'host rules description',
-                    'hosts': [
-                        'www.example.com',
-                        'www2.example.com'
-                    ],
-                    'pathMatcher': 'host_rules_path_matcher'
-                }
-            ],
-            'name': 'foo_url_map_name',
-            'pathMatchers': [
-                {
-                    'defaultService': 'bes-pathmatcher-one-default',
-                    'description': 'path matcher one',
-                    'name': 'path_matcher_one',
-                    'pathRules': [
-                        {
-                            'paths': [
-                                '/',
-                                '/aboutus'
-                            ],
-                            'service': 'my-one-bes'
-                        }
-                    ]
-                },
-                {
-                    'defaultService': 'bes-pathmatcher-two-default',
-                    'description': 'path matcher two',
-                    'name': 'path_matcher_two',
-                    'pathRules': [
-                        {
-                            'paths': [
-                                '/webapp',
-                                '/graphs'
-                            ],
-                            'service': 'my-two-bes'
-                        }
-                    ]
-                }
-            ]
-        }
+        def test_binary_stdin(self, httpbin):
+        with open(BIN_FILE_PATH, 'rb') as stdin:
+            env = MockEnvironment(
+                stdin=stdin,
+                stdin_isatty=False,
+                stdout_isatty=False
+            )
+            r = http('--print=B', 'POST', httpbin.url + '/post', env=env)
+            assert r == BIN_FILE_CONTENT
     
-        def construct_yaml_map(self, node):
-        data = AnsibleMapping()
-        yield data
-        value = self.construct_mapping(node)
-        data.update(value)
-        data.ansible_pos = self._node_position_info(node)
+        # Arguments
+        path: path where to cache the dataset locally
+            (relative to ~/.keras/datasets).
+        test_split: fraction of the data to reserve as test set.
+        seed: Random seed for shuffling the data
+            before computing the test split.
+    
+        for i in range(1, 6):
+        fpath = os.path.join(path, 'data_batch_' + str(i))
+        (x_train[(i - 1) * 10000: i * 10000, :, :, :],
+         y_train[(i - 1) * 10000: i * 10000]) = load_batch(fpath)
+    
+    from .cifar import load_batch
+from ..utils.data_utils import get_file
+from .. import backend as K
+import numpy as np
+import os
+    
+            self.kernel_i = self.kernel[:, :self.units]
+        self.kernel_f = self.kernel[:, self.units: self.units * 2]
+        self.kernel_c = self.kernel[:, self.units * 2: self.units * 3]
+        self.kernel_o = self.kernel[:, self.units * 3:]
     
     
-class TestVars(unittest.TestCase):
-    def setUp(self):
-        self.mock_templar = MagicMock(name='mock_templar')
+def embedding_kwargs_preprocessor(args, kwargs):
+    converted = []
+    if 'dropout' in kwargs:
+        kwargs.pop('dropout')
+        warnings.warn('The `dropout` argument is no longer support in `Embedding`. '
+                      'You can apply a `keras.layers.SpatialDropout1D` layer '
+                      'right after the `Embedding` layer to get the same behavior.', stacklevel=3)
+    return args, kwargs, converted
     
-        # uninstalled ---------------------------- {{{
-    def _uninstall_current_package(self):
-        if not self.valid_package(self.current_package):
-            self.failed = True
-            self.message = 'Invalid package: {0}.'.format(self.current_package)
-            raise HomebrewException(self.message)
-    }
-    }
-    }
+    
+def sparse_categorical_accuracy(y_true, y_pred):
+    return K.cast(K.equal(K.max(y_true, axis=-1),
+                          K.cast(K.argmax(y_pred, axis=-1), K.floatx())),
+                  K.floatx())
+    
+    from keras.utils.test_utils import get_test_data, keras_test
+from keras.models import Sequential
+from keras import layers
+import keras
+from keras.utils.np_utils import to_categorical
+    
+    
+def get_test_values():
+    return [0.1, 0.5, 3, 8, 1e-7]
