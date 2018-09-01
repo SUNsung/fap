663481c8c6571a47d89655ec522ad668c8f5f34c
SUNsung <weatherius@gmail.com>
2018-09-01 20:40:29 +0300

    Rewrite [3148Q51Y4SP4231th5Wpk57v6231lEx2MGd2ATI31UhV.py] File in: 2018-09-01 20:40:28

--- pyton/3148Q51Y4SP4231th5Wpk57v6231lEx2MGd2ATI31UhV.py
+++ pyton/3148Q51Y4SP4231th5Wpk57v6231lEx2MGd2ATI31UhV.py
@@ -1,125 +1,65 @@
 
         
         
-def main():
-    if len(sys.argv) < 2:
-        print('No file passed (file should contain Markdown table syntax)')
-        sys.exit(1)
-    check_format(sys.argv[1])
-    if len(errors) > 0:
-        for err in errors:
-            print(err)
-        sys.exit(1)
+def cut_module_meta(app, what, name, obj, options, lines):
+    '''Remove metadata from autodoc output.'''
+    if what != 'module':
+        return
     
-    tf_proto_library_py(
-    name = 'trace_pb2',
-    srcs = ['trace.proto'],
-    protodeps = [':data_pb2'],
-)
+        # test that the user was inserted into the database
+    with app.app_context():
+        assert get_db().execute(
+            'select * from user where username = 'a'',
+        ).fetchone() is not None
     
-    py_test(
-    name = 'component_test',
-    srcs = [
-        'component_test.py',
-    ],
-    deps = [
-        ':components',
-        '//dragnn/protos:spec_pb2_py',
-        '@org_tensorflow//tensorflow:tensorflow_py',
-    ],
-)
+        :param root_path: path to which files are read relative from.  When the
+                      config object is created by the application, this is
+                      the application's :attr:`~flask.Flask.root_path`.
+    :param defaults: an optional dictionary of default values
+    '''
     
-      def testCombineArcAndRootPotentials(self):
-    with self.test_session():
-      arcs = tf.constant([[[1, 2, 3],
-                           [2, 3, 4],
-                           [3, 4, 5]],
-                          [[3, 4, 5],
-                           [2, 3, 4],
-                           [1, 2, 3]]], tf.float32)  # pyformat: disable
-      roots = tf.constant([[6, 7, 8],
-                           [8, 7, 6]], tf.float32)  # pyformat: disable
+        def get(self, name, default=None):
+        '''Get an attribute by name, or a default value. Like
+        :meth:`dict.get`.
     
+        If there is no handler for the logger's effective level, add a
+    :class:`~logging.StreamHandler` for
+    :func:`~flask.logging.wsgi_errors_stream` with a basic format.
+    '''
+    logger = logging.getLogger('flask.app')
     
-def export_to_graph(master_spec,
-                    params_path,
-                    export_path,
-                    external_graph,
-                    export_moving_averages,
-                    build_runtime_graph,
-                    signature_name='model'):
-  '''Restores a model and exports it in SavedModel form.
+    signals_available = False
+try:
+    from blinker import Namespace
+    signals_available = True
+except ImportError:
+    class Namespace(object):
+        def signal(self, name, doc=None):
+            return _FakeSignal(name, doc)
     
+        pyopenssl_info = {
+        'version': None,
+        'openssl_version': '',
+    }
+    if OpenSSL:
+        pyopenssl_info = {
+            'version': OpenSSL.__version__,
+            'openssl_version': '%x' % OpenSSL.SSL.OPENSSL_VERSION_NUMBER,
+        }
+    cryptography_info = {
+        'version': getattr(cryptography, '__version__', ''),
+    }
+    idna_info = {
+        'version': getattr(idna, '__version__', ''),
+    }
     
-if __name__ == '__main__':
-  tf.test.main()
-
+        def inner(*suffix):
+        return urljoin(httpbin_url, '/'.join(suffix))
     
-        # Construct optimizer.
-    self.optimizer = _create_optimizer(self.hyperparams,
-                                       self.master_vars['learning_rate'],
-                                       self.master_vars['step'])
+        possible_keys = pytest.mark.parametrize('key', ('accept', 'ACCEPT', 'aCcEpT', 'Accept'))
     
+                self.handler_results.append(handler_result)
     
-if __name__ == '__main__':
-  tf.test.main()
-
+    This module implements the Requests API.
     
-      # Locally-normalize and optionally clip the scores.
-  max_scores_bxmx1 = tf.reshape(max_scores_bm, [batch_size, max_nodes, 1])
-  scores_bxmxm -= max_scores_bxmx1
-  if max_dynamic_range is not None:
-    # After normalization, the scores are non-positive with max=0, so the
-    # |max_dynamic_range| can be applied directly.
-    #
-    # PyLint thinks '-max_dynamic_range' is invalid because it defaults to None.
-    
-    
-@pytest.mark.functional
-def test_refuse_with_confirmation(proc, TIMEOUT):
-    refuse_with_confirmation(proc, TIMEOUT)
-    
-            if job_number:
-            if args.job_number:
-                exit('ERROR: job number found in url and specified with --job-number')
-    
-        parser.add_argument('--key',
-                        metavar='KEY',
-                        default=api_key,
-                        required=not api_key,
-                        help='Shippable API key')
-    
-    # Make coding more python3-ish
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
-    
-                if self.desired_state in self.enabled_values:
-                if not is_enabled:
-                    if self.module.check_mode:
-                        self.module.exit_json(changed=True)
-    
-                record_id = result['domain_record']['id']
-    
-            :param domains: domains in certificate request
-        :type domains: `list` of `str`
-        :param certname: requested name of lineage
-        :type certname: `str` or `None`
-    
-        def test_no_tos(self):
-        with mock.patch('certbot.client.acme_client.BackwardsCompatibleClientV2') as mock_client:
-            mock_client.new_account_and_tos().terms_of_service = 'http://tos'
-            with mock.patch('certbot.eff.handle_subscription') as mock_handle:
-                with mock.patch('certbot.account.report_new_account'):
-                    mock_client().new_account_and_tos.side_effect = errors.Error
-                    self.assertRaises(errors.Error, self._call)
-                    self.assertFalse(mock_handle.called)
-    
-            self.assertTrue(self.addr_default.conflicts(self.addr))
-        self.assertTrue(self.addr_default.conflicts(self.addr1))
-        self.assertTrue(self.addr_default.conflicts(self.addr_defined))
-    
-        @mock.patch('certbot_compatibility_test.validator.requests.get')
-    def test_hsts_malformed(self, mock_get_request):
-        mock_get_request.return_value = create_response(
-            headers={'strict-transport-security': 'sdfal'})
-        self.assertFalse(self.validator.hsts('test.com'))
+    '''Unpack a MIME message into a directory of files.'''
