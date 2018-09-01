7dec90a333c369b2fb1baf8e81cc16458bc2a1c8
SUNsung <weatherius@gmail.com>
2018-09-01 15:43:52 +0300

    Rewrite [51hXMkdt8WGAA51JJR8NrWODcQ51yhTJ94Lf9yk51oFf.cpp] File in: 2018-09-01 15:43:52

--- cccccc/51hXMkdt8WGAA51JJR8NrWODcQ51yhTJ94Lf9yk51oFf.cpp
+++ cccccc/51hXMkdt8WGAA51JJR8NrWODcQ51yhTJ94Lf9yk51oFf.cpp
@@ -1,182 +1,197 @@
 
         
-        
-    {  // The name of the source file where the test part took place, or
-  // '' if the source file is unknown.
-  std::string file_name_;
-  // The line in the source file where the test part took place, or -1
-  // if the line number is unknown.
-  int line_number_;
-  std::string summary_;  // The test failure summary.
-  std::string message_;  // The test failure message.
-};
+        #include <set>
+#include <string>
+#include <vector>
     
-    #endif  // GTEST_HAS_DEATH_TEST
-    
-      // Take over ownership of a raw pointer.  This should happen as soon as
-  // possible after the object is created.
-  explicit linked_ptr(T* ptr = NULL) { capture(ptr); }
-  ~linked_ptr() { depart(); }
-    
-    template <class Generator1, class Generator2, class Generator3,
-    class Generator4>
-class CartesianProductHolder4 {
- public:
-CartesianProductHolder4(const Generator1& g1, const Generator2& g2,
-    const Generator3& g3, const Generator4& g4)
-      : g1_(g1), g2_(g2), g3_(g3), g4_(g4) {}
-  template <typename T1, typename T2, typename T3, typename T4>
-  operator ParamGenerator< ::std::tr1::tuple<T1, T2, T3, T4> >() const {
-    return ParamGenerator< ::std::tr1::tuple<T1, T2, T3, T4> >(
-        new CartesianProductGenerator4<T1, T2, T3, T4>(
-        static_cast<ParamGenerator<T1> >(g1_),
-        static_cast<ParamGenerator<T2> >(g2_),
-        static_cast<ParamGenerator<T3> >(g3_),
-        static_cast<ParamGenerator<T4> >(g4_)));
-  }
-    }
-    
-    #endif  // GTEST_INCLUDE_GTEST_INTERNAL_GTEST_PARAM_UTIL_H_
-
-    
-    // Implementation #1 calculates the primes on-the-fly.
-class OnTheFlyPrimeTable : public PrimeTable {
- public:
-  virtual bool IsPrime(int n) const {
-    if (n <= 1) return false;
-    }
-    }
-    
-            void DecimateLattices(
-            LatticePtr decimatedLattices,         /* output: lattices after decimation*/
-            BoundariesPtr decimatedBoundaryPtr,   /* output: boundary after decimation*/
-            ExtrauttMapPtr decimatedExtraMapPtr,  /* output: extramap after decimation*/
-            UidPtr decimatedUidPtr,               /* output: Uid after decimation*/
-            const Lattice lattices,               /* input: lattices to be decimated */
-            const Boundaries boundaries,          /* input: boundary to be decimated */
-            const ExtrauttMap extraMaps,          /* input: extra map to be decimated */
-            const Uid uids,                       /* input: uid to be decimated*/
-            pair<size_t, size_t> parallelSeqRange /* input: what parallel sequence range we are looking at */
-            )
-        {
-            size_t parallelSeqStId = parallelSeqRange.first;
-            size_t parallelSeqEnId = parallelSeqRange.second;
-    }
-    
-        // fetch entire object into the cache
-    // Does this really make sense?? Should be rather done during computation.
-    void prefetch() const
-    {
-        const msra::math::float4 *p2 = (msra::math::float4 *) this->p;
-        size_t numfloat4s = cols() * colstride / 4;
-        const msra::math::float4 *q = p2 + numfloat4s;
-        const size_t cacherowbytes = 64; // or what?
-        const size_t cacherowfloat4s = cacherowbytes / sizeof(*p2);
-        for (; p2 < q; p2 += cacherowfloat4s)
-            msra::math::float4::prefetch(p2);
-    }
-    
-    // If the Tracing flag is set, print out a timestamp with no new line at the end
-#define PREPENDTS(stream) \
-    do \
-    { \
-        if (ProgressTracing::GetTimestampingFlag()) \
-        { \
-            char mbstr[30]; \
-            fprintf(stream, '%s: ', ProgressTracing::Timestamp(mbstr));  \
-        } \
-    } while(0)
-    
-        bool haslattice(std::wstring key) const
-    {
-#ifdef NONUMLATTICEMMI
-        return denlattices.haslattice(key);
-#else
-        return numlattices.haslattice(key) && denlattices.haslattice(key);
+      // brightray::MainDelegate:
+  std::unique_ptr<brightray::ContentClient> CreateContentClient() override;
+#if defined(OS_MACOSX)
+  void OverrideChildProcessPath() override;
+  void OverrideFrameworkBundlePath() override;
 #endif
-    }
     
-            static float f = 0.0f;
-        ImGui::Text('Hello, world!');
-        ImGui::SliderFloat('float', &f, 0.0f, 1.0f);
-        ImGui::Text('Application average %.3f ms/frame (%.1f FPS)', 1000.0f / io.Framerate, io.Framerate);
-        ImGui::ShowDemoWindow(NULL);
+    #include 'base/command_line.h'
     
-    void ImGui_ImplFreeGLUT_InstallFuncs()
-{
-    glutReshapeFunc(ImGui_ImplFreeGLUT_ReshapeFunc);
-    glutMotionFunc(ImGui_ImplFreeGLUT_MotionFunc);
-    glutPassiveMotionFunc(ImGui_ImplFreeGLUT_MotionFunc);
-    glutMouseFunc(ImGui_ImplFreeGLUT_MouseFunc);
-    glutMouseWheelFunc(ImGui_ImplFreeGLUT_MouseWheelFunc);
-    glutKeyboardFunc(ImGui_ImplFreeGLUT_KeyboardFunc);
-    glutKeyboardUpFunc(ImGui_ImplFreeGLUT_KeyboardUpFunc);
-    glutSpecialFunc(ImGui_ImplFreeGLUT_SpecialFunc);
-    glutSpecialUpFunc(ImGui_ImplFreeGLUT_SpecialUpFunc);
+      // Delegate implementations.
+  void OnError(const std::string& error) override;
+  void OnError(const std::string& message,
+               const int code,
+               const std::string& domain) override;
+  void OnCheckingForUpdate() override;
+  void OnUpdateAvailable() override;
+  void OnUpdateNotAvailable() override;
+  void OnUpdateDownloaded(const std::string& release_notes,
+                          const std::string& release_name,
+                          const base::Time& release_date,
+                          const std::string& update_url) override;
+    
+    BoxLayout::~BoxLayout() {}
+    
+    void BrowserWindow::UpdateDraggableRegions(
+    content::RenderFrameHost* rfh,
+    const std::vector<DraggableRegion>& regions) {
+  if (window_->has_frame())
+    return;
+  static_cast<NativeWindowViews*>(window_.get())
+      ->UpdateDraggableRegions(DraggableRegionsToSkRegion(regions));
 }
     
-            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+      // content::DevToolsAgentHostClient:
+  void AgentHostClosed(content::DevToolsAgentHost* agent_host) override;
+  void DispatchProtocolMessage(content::DevToolsAgentHost* agent_host,
+                               const std::string& message) override;
     
-    //---- Don't implement some functions to reduce linkage requirements.
-//#define IMGUI_DISABLE_WIN32_DEFAULT_CLIPBOARD_FUNCTIONS   // [Win32] Don't implement default clipboard handler. Won't use and link with OpenClipboard/GetClipboardData/CloseClipboard etc.
-//#define IMGUI_DISABLE_WIN32_DEFAULT_IME_FUNCTIONS         // [Win32] Don't implement default IME handler. Won't use and link with ImmGetContext/ImmSetCompositionWindow.
-//#define IMGUI_DISABLE_FORMAT_STRING_FUNCTIONS             // Don't implement ImFormatString/ImFormatStringV so you can implement them yourself if you don't want to link with vsnprintf.
-//#define IMGUI_DISABLE_MATH_FUNCTIONS                      // Don't implement ImFabs/ImSqrt/ImPow/ImFmod/ImCos/ImSin/ImAcos/ImAtan2 wrapper so you can implement them yourself. Declare your prototypes in imconfig.h.
-//#define IMGUI_DISABLE_DEFAULT_ALLOCATORS                  // Don't implement default allocators calling malloc()/free() to avoid linking with them. You will need to call ImGui::SetAllocatorFunctions().
+      AcceleratorCallbackMap accelerator_callback_map_;
     
-    static void ImGui_ImplSDL2_UpdateMouseCursor()
+    #ifdef DEBUG_LOG
+#define COLOR '\033[31;1m'
+#define RESET '\033[0m'
+#define __DEBUG(msg, ...) fprintf(stderr, COLOR msg '%c' RESET, __VA_ARGS__);
+#define DEBUG(...) __DEBUG(__VA_ARGS__, '\n')
+#else
+#define DEBUG(...) (void)0
+#endif
+    
+    
+    {} // namespace caffe2
+
+    
+      auto axis = helper.GetSingleArgument<int32_t>('axis', 1);
+  const auto canonical_axis = canonical_axis_index_(axis, in[0].dims().size());
+  auto axis_w = helper.GetSingleArgument<int32_t>('axis_w', 1);
+  const int canonical_axis_w =
+      canonical_axis_index_(axis_w, in[1].dims().size());
+  const int N = pretransposed_weight
+      ? size_from_dim_(canonical_axis_w, GetDimsVector(in[1]))
+      : size_to_dim_(canonical_axis_w, GetDimsVector(in[1]));
+    
+    #include 'caffe2/core/context.h'
+#include 'caffe2/core/operator.h'
+#include 'caffe2/core/tensor.h'
+    
+    
+    {} // namespace caffe2
+
+    
+    
+    {} // namespace caffe2
+    
+    
+    {} // namespace caffe2
+
+    
+    	PathRemoveFileSpec(exeDir);
+    
+    Follow* Follow::create(Node *followedNode, const Rect& rect/* = Rect::ZERO*/)
 {
-    ImGuiIO& io = ImGui::GetIO();
-    if (io.ConfigFlags & ImGuiConfigFlags_NoMouseCursorChange)
+    return createWithOffset(followedNode, 0.0, 0.0,rect);
+}
+    
+    #include '2d/CCActionCamera.h'
+#include '2d/CCNode.h'
+#include 'platform/CCStdC.h'
+    
+    
+    {    return ret;
+}
+    
+    void ActionManager::resumeTargets(const Vector<Node*>& targetsToResume)
+{
+    for(const auto &node : targetsToResume)
+    {
+        this->resumeTarget(node);
+    }
+}
+    
+    NS_CC_BEGIN
+    
+    void ProgressFromTo::update(float time)
+{
+    ((kProgressTimerCast)(_target))->setPercentage(_from + (_to - _from) * time);
+}
+    
+    /**
+ * @addtogroup actions
+ * @{
+ */
+    }
+    
+    Animation* Animation::createWithSpriteFrames(const Vector<SpriteFrame*>& frames, float delay/* = 0.0f*/, unsigned int loops/* = 1*/)
+{
+    Animation *animation = new (std::nothrow) Animation();
+    animation->initWithSpriteFrames(frames, delay, loops);
+    animation->autorelease();
+    }
+    
+    /** Read an NSDictionary from a plist file and parse it automatically for animations */
+void AnimationCache::addAnimationsWithFile(const std::string& plist)
+{
+    CCASSERT(!plist.empty(), 'Invalid texture file name');
+    if (plist.empty()) {
+        log('%s error:file name is empty!', __FUNCTION__);
         return;
     }
     
-    #include <stddef.h>
-#include <stdint.h>
-    
-    #ifndef GUETZLI_FAST_LOG_H_
-#define GUETZLI_FAST_LOG_H_
-    
-    #include 'guetzli/output_image.h'
-    
-    namespace guetzli {
+    ValueMap dict = FileUtils::getInstance()->getValueMapFromFile(plist);
     }
     
-    #pragma once
+    /** @brief AtlasNode is a subclass of Node that implements the RGBAProtocol and TextureProtocol protocol.
+ * It knows how to render a TextureAtlas object.
+ * If you are going to render a TextureAtlas consider subclassing AtlasNode (or a subclass of AtlasNode).
+ * All features from Node are valid, plus the following features:
+ * - opacity and RGB colors.
+ */
+class CC_DLL AtlasNode : public Node, public TextureProtocol
+{    
+public:
+	/** creates a AtlasNode  with an Atlas file the width and height of each item and the quantity of items to render.
+     *
+     * @param filename The path of Atlas file.
+     * @param tileWidth The width of the item.
+     * @param tileHeight The height of the item.
+     * @param itemsToRender The quantity of items to render.
+     */
+	static AtlasNode * create(const std::string& filename, int tileWidth, int tileHeight, int itemsToRender);
+    }
     
-      BlockingQueueAddResult add(T item) override {
-    switch (kBehavior) { // static
-      case QueueBehaviorIfFull::THROW:
-        if (!queue_.write(std::move(item))) {
-          throw QueueFullException('LifoSemMPMCQueue full, can't add item');
+    
+    {        ImGui::Render();
+    }
+    
+            // Allegro's implementation of al_draw_indexed_prim() for DX9 is completely broken. Unindex our buffers ourselves.
+        // FIXME-OPT: Unfortunately Allegro doesn't support 32-bits packed colors so we have to convert them to 4 float as well..
+        static ImVector<ImDrawVertAllegro> vertices;
+        vertices.resize(cmd_list->IdxBuffer.Size);
+        for (int i = 0; i < cmd_list->IdxBuffer.Size; i++)
+        {
+            const ImDrawVert* src_v = &cmd_list->VtxBuffer[cmd_list->IdxBuffer[i]];
+            ImDrawVertAllegro* dst_v = &vertices[i];
+            dst_v->pos = src_v->pos;
+            dst_v->uv = src_v->uv;
+            unsigned char* c = (unsigned char*)&src_v->col;
+            dst_v->col = al_map_rgba(c[0], c[1], c[2], c[3]);
         }
-        break;
-      case QueueBehaviorIfFull::BLOCK:
-        queue_.blockingWrite(std::move(item));
-        break;
-    }
-    return sem_.post();
-  }
     
-      if (UNLIKELY(!tk)) {
-    return makeFuture<Unit>(FutureNoTimekeeper());
-  }
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
     
-    FOLLY_ALWAYS_INLINE int __builtin_ffsll(long long x) {
-  unsigned long index;
-  return int(_BitScanForward64(&index, (unsigned long long)x) ? index + 1 : 0);
+            if (ImGui::Button('Button'))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text('counter = %d', counter);
+    
+    static void FramePresent(ImGui_ImplVulkanH_WindowData* wd)
+{
+    ImGui_ImplVulkanH_FrameData* fd = &wd->Frames[wd->FrameIndex];
+    VkPresentInfoKHR info = {};
+    info.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+    info.waitSemaphoreCount = 1;
+    info.pWaitSemaphores = &fd->RenderCompleteSemaphore;
+    info.swapchainCount = 1;
+    info.pSwapchains = &wd->Swapchain;
+	info.pImageIndices = &wd->FrameIndex;
+	VkResult err = vkQueuePresentKHR(g_Queue, &info);
+    check_vk_result(err);
 }
-    
-    TEST(SysAllocator, vector) {
-  using Alloc = SysAllocator<float>;
-  Alloc const alloc;
-  std::vector<float, Alloc> nums(alloc);
-  nums.push_back(3.);
-  nums.push_back(5.);
-  EXPECT_THAT(nums, testing::ElementsAreArray({3., 5.}));
-}
-    
-    #include <folly/futures/Future.h>
-#include <folly/portability/GTest.h>
