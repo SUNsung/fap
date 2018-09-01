dceceb36f4c2e3b2741fcd8942a242847188d8c1
SUNsung <weatherius@gmail.com>
2018-09-01 17:27:33 +0300

    Rewrite [262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py] File in: 2018-09-01 17:27:32

--- pyton/262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py
+++ pyton/262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py
@@ -1,148 +1,55 @@
 
         
-        if __name__ == '__main__':
-    main()
+            # 128+2 SIGINT <http://www.tldp.org/LDP/abs/html/exitcodes.html>
+    ERROR_CTRL_C = 130
+    
+        @property
+    def config(self):
+        if not hasattr(self, '_config'):
+            self._config = Config(directory=self.config_dir)
+            if self._config.is_new():
+                self._config.save()
+            else:
+                self._config.load()
+        return self._config
+    
+        # The URL prefix the adapter should be mount to.
+    prefix = None
+    
+    
+def humanize_bytes(n, precision=2):
+    # Author: Doug Latornell
+    # Licence: MIT
+    # URL: http://code.activestate.com/recipes/577081/
+    '''Return a humanized string representation of a number of bytes.
+    
+    
+def patharg(path):
+    '''
+    Back slashes need to be escaped in ITEM args,
+    even in Windows paths.
+    
+        def test_CRLF_formatted_request(self, httpbin):
+        r = http('--pretty=format', '--print=HB', 'GET', httpbin.url + '/get')
+        self._validate_crlf(r)
 
     
-    
-total_bytes = 0
-    
-    from test.helper import try_rm
-    
-        def tearDown(self):
-        if os.path.exists(self.test_dir):
-            shutil.rmtree(self.test_dir)
-    
-    # Allow direct execution
-import os
-import sys
-import unittest
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-    
-        def _real_extract(self, url):
-        playlist_id = self._match_id(url)
-    
-    from .nuevo import NuevoBaseIE
-    
-            formats = [
-            {
-                'format_id': f['type'],
-                'filesize': int(f['filesize']),
-                'url': f['url']
-            } for f in info['rfiles']
-        ]
-        self._sort_formats(formats)
-    
-            return {
-            'id': flv_id,
-            'url': rtmp_url,
-            'ext': 'flv',
-            'no_resume': True,
-            'title': title,
-            'description': description,
-            'duration': duration,
-            'view_count': view_count,
-            'comment_count': comment_count,
-            'uploader': uploader,
-            'upload_date': upload_date,
-        }
-
-    
-    
-def deserialize(config, custom_objects=None):
-    return deserialize_keras_object(config,
-                                    module_objects=globals(),
-                                    custom_objects=custom_objects,
-                                    printable_module_name='metric function')
-    
-    # Notes
-    
-    import numpy as np
-import matplotlib.pyplot as plt
-import keras
-from keras.datasets import reuters
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
-from keras.layers.noise import AlphaDropout
-from keras.preprocessing.text import Tokenizer
-    
-                    contracts.append(self.contracts[name](method, *args))
-    
-            assert len(self.args) in [1, 2, 3]
-        self.obj_name = self.args[0] or None
-        self.obj_type = self.objects[self.obj_name]
-    
-        # Stripping scripts and comments is slow (about 20x slower than
-    # just checking if a string is in text); this is a quick fail-fast
-    # path that should work for most pages.
-    if 'fragment' not in text:
-        return False
-    if 'content' not in text:
-        return False
-    
-        By default this will get the strings from the blns.txt file
-    
-    
-def main():
-    tornado.options.parse_command_line()
-    application = tornado.web.Application([
-        (r'/', MainHandler),
-    ])
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(options.port)
-    tornado.ioloop.IOLoop.current().start()
-    
-    import logging
-    
-        def get_allowed_errors(self):
-        return super(GzipHTTPTest, self).get_allowed_errors() + [
-            # TODO: The Etag is supposed to change when Content-Encoding is
-            # used.  This should be fixed, but it's difficult to do with the
-            # way GZipContentEncoding fits into the pipeline, and in practice
-            # it doesn't seem likely to cause any problems as long as we're
-            # using the correct Vary header.
-            rs.VARY_ETAG_DOESNT_CHANGE,
-        ]
-    
-    
-@gen.coroutine
-def put(filenames):
-    client = httpclient.AsyncHTTPClient()
-    for filename in filenames:
-        mtype = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
-        headers = {'Content-Type': mtype}
-        producer = partial(raw_producer, filename)
-        url_path = quote(os.path.basename(filename))
-        response = yield client.fetch('http://localhost:8888/%s' % url_path,
-                                      method='PUT',
-                                      headers=headers,
-                                      body_producer=producer)
-        print(response)
-    
-                stack.append(item)
-            stack_ids.add(item_id)
-            inner(gc.get_referents(item))
-            stack.pop()
-            stack_ids.remove(item_id)
-            visited_ids.add(item_id)
-    
-    
-if __name__ == '__main__':
-    main()
-
-    
-    
-class Test_face_recognition(unittest.TestCase):
-    
-                # If a match was found in known_face_encodings, just use the first one.
-            if True in matches:
-                first_match_index = matches.index(True)
-                name = known_face_names[first_match_index]
-    
-        # Draw a box around the face using the Pillow module
-    draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
-    
-    # If true, an OpenSearch description file will be output, and all pages
-# will contain a <link> tag referring to it.  The value of this option
-# must be the base URL from which the finished HTML is served.
-#html_use_opensearch = ''
+        if Search(r'\s:\s*\w+[({]', line):
+      # A lone colon tend to indicate the start of a constructor
+      # initializer list.  It could also be a ternary operator, which
+      # also tend to appear in constructor initializer lists as
+      # opposed to parameter lists.
+      return True
+    if Search(r'\}\s*,\s*$', line):
+      # A closing brace followed by a comma is probably the end of a
+      # brace-initialized member in constructor initializer list.
+      return True
+    if Search(r'[{};]\s*$', line):
+      # Found one of the following:
+      # - A closing brace or semicolon, probably the end of the previous
+      #   function.
+      # - An opening brace, probably the start of current class or namespace.
+      #
+      # Current line is probably not inside an initializer list since
+      # we saw one of those things without seeing the starting colon.
+      return False
