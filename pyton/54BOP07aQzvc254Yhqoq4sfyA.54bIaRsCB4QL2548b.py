e993689a65333be8e36e65b88f896c5710d2e7b4
SUNsung <weatherius@gmail.com>
2018-09-01 21:46:44 +0300

    Rewrite [54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py] File in: 2018-09-01 21:46:43

--- pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
+++ pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
@@ -1,358 +1,71 @@
 
         
-        errors = []
-title_links = []
-previous_links = []
-anchor_re = re.compile(anchor + '\s(.+)')
-section_title_re = re.compile('\*\s\[(.*)\]')
+            def process_lhs(self, qn, connection):
+        if not isinstance(self.lhs.output_field, SearchVectorField):
+            self.lhs = SearchVector(self.lhs)
+        lhs, lhs_params = super().process_lhs(qn, connection)
+        return lhs, lhs_params
     
+            # No redirect was found. Return the response.
+        return response
+
     
-def main(args=None):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--install',
-                        action='store_const', dest='action', const='install',
-                        help='Launch at Windows startup')
-    parser.add_argument('-u', '--uninstall',
-                        action='store_const', dest='action', const='uninstall',
-                        help='Remove Windows service')
-    parser.add_argument('-s', '--service',
-                        action='store_const', dest='action', const='service',
-                        help='Run as a Windows service')
-    parser.add_argument('-b', '--bind', metavar='<host:port>',
-                        action='store', default='0.0.0.0:8142',
-                        help='Bind to host:port (default %default)')
-    options = parser.parse_args(args=args)
-    
-        with open(FISH_COMPLETION_TEMPLATE) as f:
-        template = f.read()
-    filled_template = template.replace('{{commands}}', '\n'.join(commands))
-    with open(FISH_COMPLETION_FILE, 'w') as f:
-        f.write(filled_template)
-    
-    
-def main():
-    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
-    options, args = parser.parse_args()
-    if len(args) != 2:
-        parser.error('Expected an input and an output filename')
-    
-        def gen_ies_md(ies):
-        for ie in ies:
-            ie_md = '**{0}**'.format(ie.IE_NAME)
-            ie_desc = getattr(ie, 'IE_DESC', None)
-            if ie_desc is False:
-                continue
-            if ie_desc is not None:
-                ie_md += ': {0}'.format(ie.IE_DESC)
-            if not ie.working():
-                ie_md += ' (Currently broken)'
-            yield ie_md
-    
-    
-def build_completion(opt_parser):
-    opts = [opt for group in opt_parser.option_groups
-            for opt in group.option_list]
-    opts_file = [opt for opt in opts if opt.metavar == 'FILE']
-    opts_dir = [opt for opt in opts if opt.metavar == 'DIR']
-    
-    # -- Options for HTML output ----------------------------------------------
-    
-        def test_opengraph(self):
-        ie = self.ie
-        html = '''
-            <meta name='og:title' content='Foo'/>
-            <meta content='Some video's description ' name='og:description'/>
-            <meta property='og:image' content='http://domain.com/pic.jpg?key1=val1&amp;key2=val2'/>
-            <meta content='application/x-shockwave-flash' property='og:video:type'>
-            <meta content='Foo' property=og:foobar>
-            <meta name='og:test1' content='foo > < bar'/>
-            <meta name='og:test2' content='foo >//< bar'/>
-            '''
-        self.assertEqual(ie._og_search_title(html), 'Foo')
-        self.assertEqual(ie._og_search_description(html), 'Some video\'s description ')
-        self.assertEqual(ie._og_search_thumbnail(html), 'http://domain.com/pic.jpg?key1=val1&key2=val2')
-        self.assertEqual(ie._og_search_video_url(html, default=None), None)
-        self.assertEqual(ie._og_search_property('foobar', html), 'Foo')
-        self.assertEqual(ie._og_search_property('test1', html), 'foo > < bar')
-        self.assertEqual(ie._og_search_property('test2', html), 'foo >//< bar')
-        self.assertEqual(ie._og_search_property(('test0', 'test1'), html), 'foo > < bar')
-        self.assertRaises(RegexNotFoundError, ie._og_search_property, 'test0', html, None, fatal=True)
-        self.assertRaises(RegexNotFoundError, ie._og_search_property, ('test0', 'test00'), html, None, fatal=True)
-    
-        '''
-    # Serialize JSON data, if needed.
-    data = args.data
-    auto_json = data and not args.form
-    if (args.json or auto_json) and isinstance(data, dict):
-        if data:
-            data = json.dumps(data)
-        else:
-            # We need to set data to an empty string to prevent requests
-            # from assigning an empty list to `response.request.data`.
-            data = ''
-    
-        def _get_path(self):
-        '''Return the config file path without side-effects.'''
-        raise NotImplementedError()
-    
-        if explicit_json and body and (not lexer or isinstance(lexer, TextLexer)):
-        # JSON response with an incorrect Content-Type?
-        try:
-            json.loads(body)  # FIXME: the body also gets parsed in json.py
-        except ValueError:
-            pass  # Nope
-        else:
-            lexer = pygments.lexers.get_lexer_by_name('json')
-    
-        def __init__(self, mime):
-        self.mime = mime
-    
-    import requests.auth
-    
-        >>> humanize_bytes(1)
-    '1 B'
-    >>> humanize_bytes(1024, precision=1)
-    '1.0 kB'
-    >>> humanize_bytes(1024 * 123, precision=1)
-    '123.0 kB'
-    >>> humanize_bytes(1024 * 12342, precision=1)
-    '12.1 MB'
-    >>> humanize_bytes(1024 * 12342, precision=2)
-    '12.05 MB'
-    >>> humanize_bytes(1024 * 1234, precision=2)
-    '1.21 MB'
-    >>> humanize_bytes(1024 * 1234 * 1111, precision=2)
-    '1.31 GB'
-    >>> humanize_bytes(1024 * 1234 * 1111, precision=1)
-    '1.3 GB'
-    
-    
-setup(
-    name='httpie',
-    version=httpie.__version__,
-    description=httpie.__doc__.strip(),
-    long_description=long_description(),
-    url='http://httpie.org/',
-    download_url='https://github.com/jakubroztocil/httpie',
-    author=httpie.__author__,
-    author_email='jakub@roztocil.co',
-    license=httpie.__licence__,
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'http = httpie.__main__:main',
-        ],
-    },
-    extras_require=extras_require,
-    install_requires=install_requires,
-    tests_require=tests_require,
-    cmdclass={'test': PyTest},
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: BSD License',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development',
-        'Topic :: System :: Networking',
-        'Topic :: Terminals',
-        'Topic :: Text Processing',
-        'Topic :: Utilities'
-    ],
+    from django.contrib.sessions.backends.base import (
+    CreateError, SessionBase, UpdateError,
 )
+from django.core.exceptions import SuspiciousOperation
+from django.db import DatabaseError, IntegrityError, router, transaction
+from django.utils import timezone
+from django.utils.functional import cached_property
+    
+    
+class Session(AbstractBaseSession):
+    '''
+    Django provides full support for anonymous sessions. The session
+    framework lets you store and retrieve arbitrary data on a
+    per-site-visitor basis. It stores data on the server side and
+    abstracts the sending and receiving of cookies. Cookies contain a
+    session ID -- not the data itself.
+    
+    import os, json, imp
+here = os.path.abspath(os.path.dirname(__file__))
+proj_info = json.loads(open(os.path.join(here, PROJ_METADATA), encoding='utf-8').read())
+try:
+    README = open(os.path.join(here, 'README.rst'), encoding='utf-8').read()
+except:
+    README = ''
+CHANGELOG = open(os.path.join(here, 'CHANGELOG.rst'), encoding='utf-8').read()
+VERSION = imp.load_source('version', os.path.join(here, 'src/%s/version.py' % PACKAGE_NAME)).__version__
+    
+    site_info = 'baomihua.com'
+download = baomihua_download
+download_playlist = playlist_not_supported('baomihua')
 
     
+        if 'tvId' not in q or 'channelId' not in q:
+        raise Exception('No enough arguments!')
     
-with open(BIN_FILE_PATH, 'rb') as f:
-    BIN_FILE_CONTENT = f.read()
+            self.assertEqual(pydoc.describe(C), 'class C')
+        self.assertEqual(pydoc.describe(c), 'C')
+        expected = 'C in module %s object' % __name__
+        self.assertIn(expected, pydoc.render_doc(c))
     
+        with open(args.msgfile, 'rb') as fp:
+        msg = email.message_from_binary_file(fp, policy=default)
     
-def test_credentials_in_url(httpbin_both):
-    url = add_auth(httpbin_both.url + '/basic-auth/user/password',
-                   auth='user:password')
-    r = http('GET', url)
-    assert HTTP_OK in r
-    assert r.json == {'authenticated': True, 'user': 'user'}
+    def handleToc(slides):
+    for slide in slides:
+        title = slide.getElementsByTagName('title')[0]
+        print('<p>%s</p>' % getText(title.childNodes))
     
+    con = sqlite3.connect(DB_FILE)
+cur = con.cursor()
+cur.execute('''
+        create table people
+        (
+          name_last      varchar(20),
+          age            integer
+        )
+        ''')
     
-@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
-@pytest.mark.parametrize('filename', filenames)
-def test_rst_file_syntax(filename):
-    p = subprocess.Popen(
-        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE
-    )
-    err = p.communicate()[1]
-    assert p.returncode == 0, err.decode('utf8')
-
-    
-    
-@pytest.mark.skip('unimplemented')
-def test_unset_host_header(httpbin_both):
-    r = http('GET', httpbin_both + '/headers')
-    assert 'Host' in r.json['headers']  # default Host present
-    
-        def load(self):
-        '''
-        Load the data from the key itself instead of fetching from some
-        external data store. Opposite of _get_session_key(), raise BadSignature
-        if signature fails.
-        '''
-        try:
-            return signing.loads(
-                self.session_key,
-                serializer=self.serializer,
-                # This doesn't handle non-default expiry dates, see #19201
-                max_age=settings.SESSION_COOKIE_AGE,
-                salt='django.contrib.sessions.backends.signed_cookies',
-            )
-        except Exception:
-            # BadSignature, ValueError, or unpickling exceptions. If any of
-            # these happen, reset the session.
-            self.create()
-        return {}
-    
-        class Meta:
-        abstract = True
-        verbose_name = _('session')
-        verbose_name_plural = _('sessions')
-    
-        class Meta(AbstractBaseSession.Meta):
-        db_table = 'django_session'
-
-    
-                prep.headers['Authorization'] = self.build_digest_header(
-                prep.method, prep.url)
-            _r = r.connection.send(prep, **kwargs)
-            _r.history.append(r)
-            _r.request = prep
-    
-        pyopenssl_info = {
-        'version': None,
-        'openssl_version': '',
-    }
-    if OpenSSL:
-        pyopenssl_info = {
-            'version': OpenSSL.__version__,
-            'openssl_version': '%x' % OpenSSL.SSL.OPENSSL_VERSION_NUMBER,
-        }
-    cryptography_info = {
-        'version': getattr(cryptography, '__version__', ''),
-    }
-    idna_info = {
-        'version': getattr(idna, '__version__', ''),
-    }
-    
-    ``response``:
-    The response generated from a Request.
-'''
-HOOKS = ['response']
-    
-            # because special names such as Name.Class, Name.Function, etc.
-        # are not recognized as such later in the parsing, we choose them
-        # to look the same as ordinary variables.
-        Name:                      '#000000',        # class: 'n'
-        Name.Attribute:            '#c4a000',        # class: 'na' - to be revised
-        Name.Builtin:              '#004461',        # class: 'nb'
-        Name.Builtin.Pseudo:       '#3465a4',        # class: 'bp'
-        Name.Class:                '#000000',        # class: 'nc' - to be revised
-        Name.Constant:             '#000000',        # class: 'no' - to be revised
-        Name.Decorator:            '#888',           # class: 'nd' - to be revised
-        Name.Entity:               '#ce5c00',        # class: 'ni'
-        Name.Exception:            'bold #cc0000',   # class: 'ne'
-        Name.Function:             '#000000',        # class: 'nf'
-        Name.Property:             '#000000',        # class: 'py'
-        Name.Label:                '#f57900',        # class: 'nl'
-        Name.Namespace:            '#000000',        # class: 'nn' - to be revised
-        Name.Other:                '#000000',        # class: 'nx'
-        Name.Tag:                  'bold #004461',   # class: 'nt' - like a keyword
-        Name.Variable:             '#000000',        # class: 'nv' - to be revised
-        Name.Variable.Class:       '#000000',        # class: 'vc' - to be revised
-        Name.Variable.Global:      '#000000',        # class: 'vg' - to be revised
-        Name.Variable.Instance:    '#000000',        # class: 'vi' - to be revised
-    
-                        # Receive the response from the server
-                    try:
-                        # For Python 2.7+ versions, use buffering of HTTP
-                        # responses
-                        r = low_conn.getresponse(buffering=True)
-                    except TypeError:
-                        # For compatibility with Python 2.6 versions and back
-                        r = low_conn.getresponse()
-    
-    Some codes have multiple names, and both upper- and lower-case versions of
-the names are allowed. For example, ``codes.ok``, ``codes.OK``, and
-``codes.okay`` all correspond to the HTTP status code 200.
-'''
-    
-    # convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-    
-        dirname = 'cifar-100-python'
-    origin = 'https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz'
-    path = get_file(dirname, origin=origin, untar=True)
-    
-            self.kernel_initializer = initializers.get(kernel_initializer)
-        self.recurrent_initializer = initializers.get(recurrent_initializer)
-        self.bias_initializer = initializers.get(bias_initializer)
-    
-    legacy_separable_conv2d_support = generate_legacy_interface(
-    allowed_positional_args=['filters', 'kernel_size'],
-    conversions=[('nb_filter', 'filters'),
-                 ('subsample', 'strides'),
-                 ('border_mode', 'padding'),
-                 ('dim_ordering', 'data_format'),
-                 ('b_regularizer', 'bias_regularizer'),
-                 ('b_constraint', 'bias_constraint'),
-                 ('bias', 'use_bias')],
-    value_conversions={'dim_ordering': {'tf': 'channels_last',
-                                        'th': 'channels_first',
-                                        'default': None}},
-    preprocessor=separable_conv2d_args_preprocessor)
-    
-    
-def test_imdb():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = imdb.load_data()
-        (x_train, y_train), (x_test, y_test) = imdb.load_data(maxlen=40)
-        assert len(x_train) == len(y_train)
-        assert len(x_test) == len(y_test)
-        word_index = imdb.get_word_index()
-        assert isinstance(word_index, dict)
-    
-    
-def main():
-    parse_command_line()
-    app = tornado.web.Application(
-        [
-            (r'/', MainHandler),
-            (r'/a/message/new', MessageNewHandler),
-            (r'/a/message/updates', MessageUpdatesHandler),
-        ],
-        cookie_secret='__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__',
-        template_path=os.path.join(os.path.dirname(__file__), 'templates'),
-        static_path=os.path.join(os.path.dirname(__file__), 'static'),
-        xsrf_cookies=True,
-        debug=options.debug,
-    )
-    app.listen(options.port)
-    tornado.ioloop.IOLoop.current().start()
-    
-    latex_documents = [
-    ('index', 'tornado.tex', 'Tornado Documentation', 'The Tornado Authors', 'manual', False),
-]
+    Usage:
