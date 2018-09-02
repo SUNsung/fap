1731b209851b256bc02775b673ec3efe13e4582e
SUNsung <weatherius@gmail.com>
2018-09-02 03:01:41 +0300

    Rewrite [36A6WJCDcXGdI36kaPJ4qDvkKA36.gW9qMyLCx.36f5p.py] File in: 2018-09-02 03:01:40

--- pyton/36A6WJCDcXGdI36kaPJ4qDvkKA36.gW9qMyLCx.36f5p.py
+++ pyton/36A6WJCDcXGdI36kaPJ4qDvkKA36.gW9qMyLCx.36f5p.py
@@ -1,3 +1,170 @@
 
         
-        
+            def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.friend_ids = []
+    
+    MOD_SSL_CONF_SRC = pkg_resources.resource_filename(
+    'certbot_nginx', 'options-ssl-nginx.conf')
+'''Path to the nginx mod_ssl config file found in the Certbot
+distribution.'''
+    
+        def test_timeout(self):
+        self.assertTrue(self.timeout.timeout)
+        self.assertFalse(self.invalid.timeout)
+    
+    ALL_SSL_OPTIONS_HASHES = [
+    '2086bca02db48daf93468332543c60ac6acdb6f0b58c7bfdf578a5d47092f82a',
+    '4844d36c9a0f587172d9fa10f4f1c9518e3bcfa1947379f155e16a70a728c21a',
+    '5a922826719981c0a234b1fbcd495f3213e49d2519e845ea0748ba513044b65b',
+    '4066b90268c03c9ba0201068eaa39abbc02acf9558bb45a788b630eb85dadf27',
+    'f175e2e7c673bd88d0aff8220735f385f916142c44aa83b09f1df88dd4767a88',
+    'cfdd7c18d2025836ea3307399f509cfb1ebf2612c87dd600a65da2a8e2f2797b',
+    '80720bd171ccdc2e6b917ded340defae66919e4624962396b992b7218a561791',
+    'c0c022ea6b8a51ecc8f1003d0a04af6c3f2bc1c3ce506b3c2dfc1f11ef931082',
+]
+'''SHA256 hashes of the contents of previous versions of all versions of MOD_SSL_CONF_SRC'''
+    
+            with mock.patch('certbot.util.get_os_info') as mock_info:
+            for distro in entrypoint.OVERRIDE_CLASSES.keys():
+                mock_info.return_value = (distro, 'whatever')
+                self.assertEqual(entrypoint.get_configurator(),
+                                 entrypoint.OVERRIDE_CLASSES[distro])
+    
+      CheckCall( [ sys.executable, build_file ] + sys.argv[ 1: ] )
+    
+    
+  def Start( self ):
+    request_data = BuildRequestData()
+    request_data.update( { 'filetypes': self.filetypes } )
+    self._response = self.PostDataToHandler( request_data,
+                                             'semantic_completion_available' )
+    
+    
+def CompileLevel( level ):
+  # valid kinds are WARNING and ERROR;
+  #  expected input levels are `warning` and `error`
+  # NOTE: we don't validate the input...
+  expected_kind = level.upper()
+    
+          if line[ 0 ] == ' ' or line[ 0 ] == '\t':
+        current_group.lines.append( line.strip() )
+    
+    
+GENERIC_RESPONSE = {
+  'clang': {
+    'has_support': True,
+    'version': 'Clang version'
+  },
+  'completer': {
+    'items': [
+      {
+        'key': 'key',
+        'value': 'value'
+      }
+    ],
+    'name': 'Completer name',
+    'servers': [
+      {
+        'address': '127.0.0.1',
+        'executable': '/path/to/executable',
+        'extras': [
+          {
+            'key': 'key',
+            'value': 'value'
+          }
+        ],
+        'is_running': True,
+        'logfiles': [
+          '/path/to/stdout/logfile',
+          '/path/to/stderr/logfile'
+        ],
+        'name': 'Server name',
+        'pid': 12345,
+        'port': 1234
+      }
+    ]
+  },
+  'extra_conf': {
+    'is_loaded': False,
+    'path': '/path/to/extra/conf'
+  },
+  'python': {
+    'executable': '/path/to/python/interpreter',
+    'version': 'Python version'
+  }
+}
+    
+    
+def MockAsyncServerResponseDone( response ):
+  '''Return a fake future object that is complete with the supplied response
+  message. Suitable for mocking a response future within a client request. For
+  example:
+    
+    
+# This file provides an UnsafeThreadPoolExecutor, which operates exactly like
+# the upstream Python version of ThreadPoolExecutor with one exception: it
+# doesn't wait for worker threads to finish before shutting down the Python
+# interpreter.
+#
+# This is dangerous for many workloads, but fine for some (like when threads
+# only send network requests). The YCM workload is one of those workloads where
+# it's safe (the aforementioned network requests case).
+    
+    ### OUTPUT ###
+# Counting to two...
+# one two
+# Counting to five...
+# one two three four five
+
+    
+        def __init__(self, msg_center):
+        self.provider = msg_center
+    
+        def __new__(cls, name, bases, attrs):
+        new_cls = type.__new__(cls, name, bases, attrs)
+        '''
+            Here the name of the class is used as key but it could be any class
+            parameter.
+        '''
+        cls.REGISTRY[new_cls.__name__] = new_cls
+        return new_cls
+    
+    
+# Getters
+def get_list():
+    return ingredients.split()
+    
+            if not meth:
+            meth = self.generic_visit
+        return meth(node, *args, **kwargs)
+    
+        def __str__(self):
+        return 'Cat'
+    
+    *Where can the pattern be used practically?
+The Factory Method can be seen in the popular web framework Django:
+http://django.wikispaces.asu.edu/*NEW*+Django+Design+Patterns For
+example, in a contact form of a web page, the subject and the message
+fields are created using the same form factory (CharField()), even
+though they have different implementations according to their
+purposes.
+    
+        def register_object(self, name, obj):
+        '''Register an object'''
+        self._objects[name] = obj
+    
+        def __init__(self, blackboard):
+        self.blackboard = blackboard
+    
+    ### OUTPUT ###
+# ['A', 'B', 'C', 'D']
+# [['A', 'B', 'C', 'D'], ['A', 'B', 'D'], ['A', 'C', 'D']]
+# ['A', 'B', 'D']
+
+    
+        def on_operator_inservice(self):
+        self._hsm.on_switchover()  # message ignored
+        self._hsm.send_operator_inservice_response()
+        self._hsm.next_state('suspect')
