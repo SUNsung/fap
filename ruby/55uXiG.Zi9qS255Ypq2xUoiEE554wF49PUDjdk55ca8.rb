e5425e2fc3fad6254392c945e9a15b88e5122959
SUNsung <weatherius@gmail.com>
2018-09-03 02:17:23 +0300

    Rewrite [55uXiG.Zi9qS255Ypq2xUoiEE554wF49PUDjdk55ca8.rb] File in: 2018-09-03 02:17:22

--- ruby/55uXiG.Zi9qS255Ypq2xUoiEE554wF49PUDjdk55ca8.rb
+++ ruby/55uXiG.Zi9qS255Ypq2xUoiEE554wF49PUDjdk55ca8.rb
@@ -1,47 +1,146 @@
 
         
-        $LOAD_PATH.unshift File.expand_path('lib', __dir__)
-require 'jekyll/version'
+                  method_tag = \
+            case method
+            when 'get'
+              html_options['method'] = 'get'
+              ''
+            when 'post', ''
+              html_options['method'] = 'post'
+              token_tag(authenticity_token, form_options: {
+                action: html_options['action'],
+                method: 'post'
+              })
+            else
+              html_options['method'] = 'post'
+              method_tag(method) + token_tag(authenticity_token, form_options: {
+                action: html_options['action'],
+                method: method
+              })
+            end
     
-      p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
-  p.option 'destination', '-d', '--destination [DIR]',
-    'Destination directory (defaults to ./_site)'
-  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
-  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
-    'Plugins directory (defaults to ./_plugins)'
-  p.option 'layouts_dir', '--layouts DIR', String,
-    'Layouts directory (defaults to ./_layouts)'
-  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
-    
-      matter = matter.join.chomp
-  content = \
-    if !input_hash['input'] || !input_hash['filter']
-      then input_hash['content']
-    else '{{ #{input_hash['input']} | ' \
-        '#{input_hash['filter']} }}'
-    end
-    
-        def limit_reached_for(user_id, date)
-      GivenDailyLike.find_for(user_id, date).pluck(:limit_reached)[0] || false
-    end
-    
-      it 'correctly sets notification level' do
-    moderator = Fabricate(:moderator)
-    
-      smoke_user = User.seed do |u|
-    u.id = 0
-    u.name = 'smoke_user'
-    u.username = 'smoke_user'
-    u.username_lower = 'smoke_user'
-    u.password = 'P4ssw0rd'
-    u.active = true
-    u.approved = true
-    u.approved_at = Time.now
-    u.trust_level = TrustLevel[3]
-  end.first
-    
-            unless post && post.id
-          puts post.errors.full_messages if post
-          puts creator.errors.inspect
-          raise 'Failed to create description for Staff category!'
+            # Mounts a shared folder via NFS. This assumes that the exports
+        # via the host are already done.
+        def mount_nfs(ip, folders)
+          raise BaseError, _key: :unsupported_nfs
         end
+    
+                # If we were told this is an upgrade safe configuration class
+            # then we add it to the set.
+            if upgrade_safe
+              data[:config_upgrade_safe] ||= Set.new
+              data[:config_upgrade_safe].add(name.to_sym)
+            end
+          end
+    
+            # This is the method called to when the system is being destroyed
+        # and allows the provisioners to engage in any cleanup tasks necessary.
+        def cleanup
+        end
+      end
+    end
+  end
+end
+
+    
+            # Execute a command on the remote machine. The exact semantics
+        # of this method are up to the implementor, but in general the
+        # users of this class will expect this to be a shell.
+        #
+        # This method gives you no way to write data back to the remote
+        # machine, so only execute commands that don't expect input.
+        #
+        # @param [String] command Command to execute.
+        # @yield [type, data] Realtime output of the command being executed.
+        # @yieldparam [String] type Type of the output. This can be
+        #   `:stdout`, `:stderr`, etc. The exact types are up to the
+        #   implementor.
+        # @yieldparam [String] data Data for the given output.
+        # @return [Integer] Exit code of the command.
+        def execute(command, opts=nil)
+        end
+    
+            # This contains all the registered host capabilities.
+        #
+        # @return [Hash<Symbol, Registry>]
+        attr_reader :host_capabilities
+    
+              # Persist through the set of invalid methods
+          this_invalid  = @__invalid_methods || Set.new
+          other_invalid = other.instance_variable_get(:'@__invalid_methods') || Set.new
+          result.instance_variable_set(:'@__invalid_methods', this_invalid + other_invalid)
+    
+          if @account_moderation_note.save
+        redirect_to admin_account_path(@account_moderation_note.target_account_id), notice: I18n.t('admin.account_moderation_notes.created_msg')
+      else
+        @account          = @account_moderation_note.target_account
+        @moderation_notes = @account.targeted_moderation_notes.latest
+    
+            render template: 'admin/reports/show'
+      end
+    end
+    
+      def account_from_topic
+    if hub_topic.present? && local_domain? && account_feed_path?
+      Account.find_local(hub_topic_params[:username])
+    end
+  end
+    
+      def show
+    if subscription.valid?(params['hub.topic'])
+      @account.update(subscription_expires_at: future_expires)
+      render plain: encoded_challenge, status: 200
+    else
+      head 404
+    end
+  end
+    
+    desc 'Test the paperclip plugin under all supported Rails versions.'
+task :all do |t|
+  if ENV['BUNDLE_GEMFILE']
+    exec('rake spec && cucumber')
+  else
+    exec('rm -f gemfiles/*.lock')
+    Rake::Task['appraisal:gemfiles'].execute
+    Rake::Task['appraisal:install'].execute
+    exec('rake appraisal')
+  end
+end
+    
+    When /^(?:|I )attach the file '([^']*)' to '([^']*)'$/ do |path, field|
+  attach_file(field, File.expand_path(path))
+end
+    
+    After do
+  ORIGINAL_BUNDLE_VARS.each_pair do |key, value|
+    ENV[key] = value
+  end
+end
+    
+      # Provides configurability to Paperclip. The options available are:
+  # * whiny: Will raise an error if Paperclip cannot process thumbnails of
+  #   an uploaded image. Defaults to true.
+  # * log: Logs progress to the Rails log. Uses ActiveRecord's logger, so honors
+  #   log levels, etc. Defaults to true.
+  # * command_path: Defines the path at which to find the command line
+  #   programs if they are not visible to Rails the system's search path. Defaults to
+  #   nil, which uses the first executable found in the user's search path.
+  # * use_exif_orientation: Whether to inspect EXIF data to determine an
+  #   image's orientation. Defaults to true.
+  def self.options
+    @options ||= {
+      command_path: nil,
+      content_type_mappings: {},
+      log: true,
+      log_command: true,
+      read_timeout: nil,
+      swallow_stderr: true,
+      use_exif_orientation: true,
+      whiny: true,
+      is_windows: Gem.win_platform?
+    }
+  end
+    
+        def define_instance_getter
+      name = @name
+      options = @options
