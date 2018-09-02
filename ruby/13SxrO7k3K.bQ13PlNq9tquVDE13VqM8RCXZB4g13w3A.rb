0d29bed9ffd38e97689c49b4c63764a53106aa90
SUNsung <weatherius@gmail.com>
2018-09-02 12:35:36 +0300

    Rewrite [13SxrO7k3K.bQ13PlNq9tquVDE13VqM8RCXZB4g13w3A.rb] File in: 2018-09-02 12:35:35

--- ruby/13SxrO7k3K.bQ13PlNq9tquVDE13VqM8RCXZB4g13w3A.rb
+++ ruby/13SxrO7k3K.bQ13PlNq9tquVDE13VqM8RCXZB4g13w3A.rb
@@ -1,110 +1,133 @@
 
         
-          def find_internal_commands(directory)
-    directory.children.reduce([]) do |cmds, f|
-      cmds << f.basename.to_s.sub(/\.(?:rb|sh)$/, '') if f.file?
-      cmds
+          it 'imports a scenario that does not exist yet' do
+    visit new_scenario_imports_path
+    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
+    click_on 'Start Import'
+    expect(page).to have_text('This scenario has a few agents to get you started. Feel free to change them or delete them as you see fit!')
+    expect(page).not_to have_text('This Scenario already exists in your system.')
+    check('I confirm that I want to import these Agents.')
+    click_on 'Finish Import'
+    expect(page).to have_text('Import successful!')
+  end
+    
+        it 'works for queued jobs' do
+      expect(status(job)).to eq('<span class='label label-warning'>queued</span>')
     end
+  end
+    
+        it 'is turned off for existing instances of Huginn' do
+      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { nil }
+      DefaultScenarioImporter.import(user)
+    end
+    
+                scenario_import.merges = {
+              '0' => {
+                'name' => 'a new name',
+                'schedule' => '6pm',
+                'keep_events_for' => 2.days.to_i.to_s,
+                'disabled' => 'true',
+                'options' => weather_agent_options.merge('api_key' => 'foo').to_json
+              }
+            }
+    
+            agent_runner.stop
+      end
+    end
+    
+      def guid_order(agent_list, agent_name)
+    agent_list.map{|a|a.guid}.sort.find_index(agents(agent_name).guid)
   end
 end
 
     
-    module Homebrew
-  SEARCH_ERROR_QUEUE = Queue.new
+        it 'work with set FAILED_JOBS_TO_KEEP env variable' do
+      expect { @scheduler.send(:cleanup_failed_jobs!) }.to change(Delayed::Job, :count).by(-1)
+      expect { @scheduler.send(:cleanup_failed_jobs!) }.to change(Delayed::Job, :count).by(0)
+      expect(@keep.id).to eq(Delayed::Job.order(:failed_at)[0].id)
+    end
     
-          case status
-      when 'A', 'D'
-        @report[status.to_sym] << tap.formula_file_to_name(src)
-      when 'M'
-        begin
-          formula = Formulary.factory(tap.path/src)
-          new_version = formula.pkg_version
-          old_version = FormulaVersions.new(formula).formula_at_revision(@initial_revision, &:pkg_version)
-          next if new_version == old_version
-        rescue Exception => e
-          onoe e if ARGV.homebrew_developer?
+          @log.level = -1
+      expect(@log).not_to be_valid
+      expect(@log).to have(1).error_on(:level)
+    
+        def log_transform(*args, from: caller[1][/`.*'/][1..-2].sub(/^block in /, ''))
+      puts '    #{cyan from}#{cyan ': #{args * ', '}' unless args.empty?}'
+    end
+    
+      # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
+    
+      not_found do
+    send_file(File.join(File.dirname(__FILE__), 'public', '404.html'), {:status => 404})
+  end
+    
+            Dir.chdir(includes_dir) do
+          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
+          if choices.include?(file)
+            source = File.read(file)
+            partial = Liquid::Template.parse(source)
+            context.stack do
+              rtn = rtn + partial.render(context)
+            end
+          else
+            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
+          end
         end
-        @report[:M] << tap.formula_file_to_name(src)
-      when /^R\d{0,3}/
-        @report[:D] << tap.formula_file_to_name(src) if tap.formula_file?(src)
-        @report[:A] << tap.formula_file_to_name(dst) if tap.formula_file?(dst)
       end
-    end
-    
-        def add_entry(entry)
-      if @index.add?(entry.as_json.to_s)
-        @entries << entry.dup
-        @types[entry.type].count += 1 if entry.type
-      end
-    end
-    
-        def push(*names)
-      @filters.push *filter_const(names)
-    end
-    
-    module Docs
-  class PageDb
-    attr_reader :pages
-    
-        def merge!(hash)
-      return super unless hash.is_a? Hash
-      hash.assert_valid_keys URI::Generic::COMPONENT
-      hash.each_pair do |key, value|
-        send '#{key}=', value
-      end
-      self
-    end
-    
-      it 'does copy the backtrace' do
-    begin
-      # Explicitly raise so a backtrace is associated with the exception.
-      # It's tempting to call `set_backtrace` instead, but that complicates
-      # the test because it might affect other state (e.g., instance variables)
-      # on some implementations.
-      raise ExceptionSpecs::InitializeException.new('my exception')
-    rescue => e
-      @obj = e
-    end
-    
-          rm_r filename
+      rtn
     end
   end
     
-      it 'writes binary data if no encoding is given' do
-    IO.write(@filename, 'Hëllö'.encode('ISO-8859-1'))
-    xEB = [235].pack('C*')
-    xF6 = [246].pack('C*')
-    File.binread(@filename).should == ('H' + xEB + 'll' + xF6).force_encoding(Encoding::ASCII_8BIT)
-  end
-    
-      it 'returns subclass instances when called on a subclass' do
-    StringSpecs::MyString.new('hello').capitalize.should be_an_instance_of(StringSpecs::MyString)
-    StringSpecs::MyString.new('Hello').capitalize.should be_an_instance_of(StringSpecs::MyString)
+        def sizes
+      attrs = 'width='#{@sizes[0]}'' if @sizes[0]
+      attrs += ' height='#{@sizes[1]}'' if @sizes[1]
+      attrs
+    end
   end
 end
     
-    describe 'Invoking a method' do
-  describe 'with required args after the rest arguments' do
-    it 'binds the required arguments first' do
-      specs.fooM0RQ1(1).should == [[], 1]
-      specs.fooM0RQ1(1,2).should == [[1], 2]
-      specs.fooM0RQ1(1,2,3).should == [[1,2], 3]
+          # users should be able to set price when importing orders via api
+      def permitted_line_item_attributes
+        if @current_user_roles.include?('admin')
+          super + [:price, :variant_id, :sku]
+        else
+          super
+        end
+      end
     
-        it 'calls #inherited on the superclass' do
-      CApiClassSpecs::Super.should_receive(:inherited)
-      @s.rb_define_class_under(CApiClassSpecs, 'ClassUnder4', CApiClassSpecs::Super)
-      CApiClassSpecs.send(:remove_const, :ClassUnder4)
+            def order
+          @order ||= Spree::Order.find_by!(number: order_id)
+          authorize! :read, @order
+        end
+    
+              state = @states.last
+          respond_with(@states) if stale?(state)
+        end
+    
+            def stock_location
+          render 'spree/api/v1/shared/stock_location_required', status: 422 and return unless params[:stock_location_id]
+          @stock_location ||= StockLocation.accessible_by(current_ability, :read).find(params[:stock_location_id])
+        end
+    
+        def raise_if_blank_file
+      if path.blank?
+        raise Errors::NotIdentifiedByImageMagickError.new('Cannot find the geometry of a file with a blank name')
+      end
     end
     
-        it 'enqueues a ReceivePrivate job' do
-      expect(Workers::ReceivePrivate).to receive(:perform_async).with(alice.id, data, true)
-    
-        def register(klass, attachment_name, attachment_options)
-      @attachments ||= {}
-      @attachments[klass] ||= {}
-      @attachments[klass][attachment_name] = attachment_options
-    end
-    
-            raise LoadError, 'Could not find the '#{name}' processor in any of these paths: #{directories.join(', ')}' unless required.any?
+        def add_active_record_callbacks
+      name = @name
+      @klass.send(:after_save) { send(name).send(:save) }
+      @klass.send(:before_destroy) { send(name).send(:queue_all_for_delete) }
+      if @klass.respond_to?(:after_commit)
+        @klass.send(:after_commit, on: :destroy) do
+          send(name).send(:flush_deletes)
+        end
+      else
+        @klass.send(:after_destroy) { send(name).send(:flush_deletes) }
       end
     end
