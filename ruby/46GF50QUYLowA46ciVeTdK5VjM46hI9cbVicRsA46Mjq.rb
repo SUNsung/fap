30a0e17237c30e69454c6b8ae151c4f865f2e35c
SUNsung <weatherius@gmail.com>
2018-09-02 15:52:27 +0300

    Rewrite [46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb] File in: 2018-09-02 15:52:27

--- ruby/46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb
+++ ruby/46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb
@@ -1,66 +1,36 @@
 
         
-        class DigestMailerWorker
-  include Sidekiq::Worker
-    
-      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/follow
-  def follow
-    f = Follow.last
-    NotificationMailer.follow(f.target_account, Notification.find_by(activity: f))
+          module Generator
+    autoload :Acknowledgements,        'cocoapods/generator/acknowledgements'
+    autoload :Markdown,                'cocoapods/generator/acknowledgements/markdown'
+    autoload :Plist,                   'cocoapods/generator/acknowledgements/plist'
+    autoload :BridgeSupport,           'cocoapods/generator/bridge_support'
+    autoload :Constant,                'cocoapods/generator/constant'
+    autoload :CopyResourcesScript,     'cocoapods/generator/copy_resources_script'
+    autoload :DummySource,             'cocoapods/generator/dummy_source'
+    autoload :EmbedFrameworksScript,   'cocoapods/generator/embed_frameworks_script'
+    autoload :Header,                  'cocoapods/generator/header'
+    autoload :InfoPlistFile,           'cocoapods/generator/info_plist_file'
+    autoload :ModuleMap,               'cocoapods/generator/module_map'
+    autoload :PrefixHeader,            'cocoapods/generator/prefix_header'
+    autoload :UmbrellaHeader,          'cocoapods/generator/umbrella_header'
+    autoload :AppTargetHelper,         'cocoapods/generator/app_target_helper'
   end
     
-    require_relative 'converter/fonts_conversion'
-require_relative 'converter/less_conversion'
-require_relative 'converter/js_conversion'
-require_relative 'converter/logger'
-require_relative 'converter/network'
+          def plugins_string
+        UI::ErrorReport.plugins_string
+      end
     
-      # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protection = false
+      class RenderPartialTag < Liquid::Tag
+    include OctopressFilters
+    def initialize(tag_name, markup, tokens)
+      @file = nil
+      @raw = false
+      if markup =~ /^(\S+)\s?(\w+)?/
+        @file = $1.strip
+        @raw = $2 == 'raw'
+      end
+      super
+    end
     
-      desc 'update main and version in bower.json'
-  task :generate do
-    require 'bootstrap-sass'
-    Dir.chdir Bootstrap.gem_path do
-      spec       = JSON.parse(File.read 'bower.json')
-    
-    res = ''
-doc = Hpricot(File.open(input))
-doc.search('//form').each do |form|
-    
-    sitelist = ARGV.shift() || usage()
-output   = ARGV.shift() || usage()
-    
-    signer._invoke('JarSignerMSF','[Ljava.lang.String;',jarsignerOpts)
-    
-      @src.__NR_execve      = __NR_execve
-  @src.__NR_getpeername = __NR_getpeername
-  @src.__NR_accept      = __NR_accept
-  @src.__NR_listen      = __NR_listen
-  @src.__NR_bind        = __NR_bind
-  @src.__NR_socket      = __NR_socket
-  @src.__NR_connect     = __NR_connect
-  @src.__NR_close       = __NR_close
-  @src.__NR_kfcntl      = __NR_kfcntl
-    
-    task default: :test
-    
-            def update
-          authorize! :update, @order, order_token
-          @address = find_address
-    
-    module RuboCop
-  module Cop
-    module Layout
-      # Checks for unnecessary additional spaces inside array percent literals
-      # (i.e. %i/%w).
-      #
-      # @example
-      #
-      #   # bad
-      #   %w(foo  bar  baz)
-      #   # good
-      #   %i(foo bar baz)
-      class SpaceInsideArrayPercentLiteral < Cop
-        include MatchRange
-        include PercentLiteral
+    Liquid::Template.register_tag('video', Jekyll::VideoTag)
