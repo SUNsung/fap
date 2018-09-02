3ca148f0662e0df264115d6a1f240d694905a608
SUNsung <weatherius@gmail.com>
2018-09-02 22:43:56 +0300

    Rewrite [21OOl9YZRco.21vLyiMxEJkw21RsvYJ6XOcO221ed3.py] File in: 2018-09-02 22:43:56

--- pyton/21OOl9YZRco.21vLyiMxEJkw21RsvYJ6XOcO221ed3.py
+++ pyton/21OOl9YZRco.21vLyiMxEJkw21RsvYJ6XOcO221ed3.py
@@ -1,248 +1,266 @@
 
         
-        
-def hex_str(int_list):
-    return codecs.encode(intlist_to_bytes(int_list), 'hex')
+          # Print the confusion matrix
+  print('%s confusion matrix:' % set_name)
+  cm = metrics.confusion_matrix(labels, pred, labels=range(len(classes)))
+  cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis] * 100
+  print_cm(cm, labels=classes)
+  return pred
+    
+    class Poisson(object):
+  '''Poisson distributon
+    
+      Returns:
+    dictionary of hyper parameters (ignoring other flag types).
+  '''
+  d = {}
+  # Data
+  d['output_dist'] = flags.output_dist
+  d['data_dir'] = flags.data_dir
+  d['lfads_save_dir'] = flags.lfads_save_dir
+  d['checkpoint_pb_load_name'] = flags.checkpoint_pb_load_name
+  d['checkpoint_name'] = flags.checkpoint_name
+  d['output_filename_stem'] = flags.output_filename_stem
+  d['max_ckpt_to_keep'] = flags.max_ckpt_to_keep
+  d['max_ckpt_to_keep_lve'] = flags.max_ckpt_to_keep_lve
+  d['ps_nexamples_to_process'] = flags.ps_nexamples_to_process
+  d['ext_input_dim'] = flags.ext_input_dim
+  d['data_filename_stem'] = flags.data_filename_stem
+  d['device'] = flags.device
+  d['csv_log'] = flags.csv_log
+  d['num_steps_for_gen_ic'] = flags.num_steps_for_gen_ic
+  d['inject_ext_input_to_gen'] = flags.inject_ext_input_to_gen
+  # Cell
+  d['cell_weight_scale'] = flags.cell_weight_scale
+  # Generation
+  d['ic_dim'] = flags.ic_dim
+  d['factors_dim'] = flags.factors_dim
+  d['ic_enc_dim'] = flags.ic_enc_dim
+  d['gen_dim'] = flags.gen_dim
+  d['gen_cell_input_weight_scale'] = flags.gen_cell_input_weight_scale
+  d['gen_cell_rec_weight_scale'] = flags.gen_cell_rec_weight_scale
+  # KL distributions
+  d['ic_prior_var_min'] = flags.ic_prior_var_min
+  d['ic_prior_var_scale'] = flags.ic_prior_var_scale
+  d['ic_prior_var_max'] = flags.ic_prior_var_max
+  d['ic_post_var_min'] = flags.ic_post_var_min
+  d['co_prior_var_scale'] = flags.co_prior_var_scale
+  d['prior_ar_atau'] = flags.prior_ar_atau
+  d['prior_ar_nvar'] =  flags.prior_ar_nvar
+  d['do_train_prior_ar_atau'] = flags.do_train_prior_ar_atau
+  d['do_train_prior_ar_nvar'] = flags.do_train_prior_ar_nvar
+  # Controller
+  d['do_causal_controller'] = flags.do_causal_controller
+  d['controller_input_lag'] = flags.controller_input_lag
+  d['do_feed_factors_to_controller'] = flags.do_feed_factors_to_controller
+  d['feedback_factors_or_rates'] = flags.feedback_factors_or_rates
+  d['co_dim'] = flags.co_dim
+  d['ci_enc_dim'] = flags.ci_enc_dim
+  d['con_dim'] = flags.con_dim
+  d['co_mean_corr_scale'] = flags.co_mean_corr_scale
+  # Optimization
+  d['batch_size'] = flags.batch_size
+  d['learning_rate_init'] = flags.learning_rate_init
+  d['learning_rate_decay_factor'] = flags.learning_rate_decay_factor
+  d['learning_rate_stop'] = flags.learning_rate_stop
+  d['learning_rate_n_to_compare'] = flags.learning_rate_n_to_compare
+  d['max_grad_norm'] = flags.max_grad_norm
+  d['cell_clip_value'] = flags.cell_clip_value
+  d['do_train_io_only'] = flags.do_train_io_only
+  d['do_train_encoder_only'] = flags.do_train_encoder_only
+  d['do_reset_learning_rate'] = flags.do_reset_learning_rate
+  d['do_train_readin'] = flags.do_train_readin
+    
+    
+def linear(x, out_size, do_bias=True, alpha=1.0, identity_if_possible=False,
+           normalized=False, name=None, collections=None):
+  '''Linear (affine) transformation, y = x W + b, for a variety of
+  configurations.
+    
+        for i, word in enumerate(self._id_to_word):
+      self._word_char_ids[i] = self._convert_word_to_char_ids(word)
+    
+      prefix = [vocab.word_to_id(w) for w in prefix_words.split()]
+  prefix_char_ids = [vocab.word_to_char_ids(w) for w in prefix_words.split()]
+  for _ in xrange(FLAGS.num_samples):
+    inputs = np.zeros([BATCH_SIZE, NUM_TIMESTEPS], np.int32)
+    char_ids_inputs = np.zeros(
+        [BATCH_SIZE, NUM_TIMESTEPS, vocab.max_word_length], np.int32)
+    samples = prefix[:]
+    char_ids_samples = prefix_char_ids[:]
+    sent = ''
+    while True:
+      inputs[0, 0] = samples[0]
+      char_ids_inputs[0, 0, :] = char_ids_samples[0]
+      samples = samples[1:]
+      char_ids_samples = char_ids_samples[1:]
+    
+    
+def imdb_raw_data(data_path=None):
+  '''Load IMDB raw data from data directory 'data_path'.
+  Reads IMDB tf record files containing integer ids,
+  and performs mini-batching of the inputs.
+  Args:
+    data_path: string path to the directory where simple-examples.tgz has
+      been extracted.
+  Returns:
+    tuple (train_data, valid_data)
+    where each of the data objects can be passed to IMDBIterator.
+  '''
+    
+    
+@pytest.fixture(autouse=True)
+def no_cache(monkeypatch):
+    monkeypatch.setattr('thefuck.utils.cache.disabled', True)
+    
+    
+@pytest.mark.parametrize('command, new_command, packages', [
+    (Command('vim', ''), 'sudo apt-get install vim && vim',
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('convert', ''), 'sudo apt-get install imagemagick && convert',
+     [('imagemagick', 'main'),
+      ('graphicsmagick-imagemagick-compat', 'universe')]),
+    (Command('sudo vim', ''), 'sudo apt-get install vim && sudo vim',
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('sudo convert', ''), 'sudo apt-get install imagemagick && sudo convert',
+     [('imagemagick', 'main'),
+      ('graphicsmagick-imagemagick-compat', 'universe')])])
+def test_get_new_command(mocker, command, new_command, packages):
+    mocker.patch('thefuck.rules.apt_get._get_packages',
+                 create=True, return_value=packages)
+    
+    
+def test_get_new_command():
+    new_command = get_new_command(Command('apt list --upgradable', match_output))
+    assert new_command == 'apt upgrade'
+    
+      aws help
+  aws <command> help
+  aws <command> <subcommand> help
+aws: error: argument operation: Invalid choice, valid choices are:
+    
+        def _get_obj_info(self, obj, depth=99, seen=None):
+        '''
+        Recursively build a data structure for the given pSphere object (depth
+        only applies to ManagedObject instances).
+        '''
+        seen = seen or set()
+        if isinstance(obj, ManagedObject):
+            try:
+                obj_unicode = text_type(getattr(obj, 'name'))
+            except AttributeError:
+                obj_unicode = ()
+            if obj in seen:
+                return obj_unicode
+            seen.add(obj)
+            if depth <= 0:
+                return obj_unicode
+            d = {}
+            for attr in dir(obj):
+                if attr.startswith('_'):
+                    continue
+                try:
+                    val = getattr(obj, attr)
+                    obj_info = self._get_obj_info(val, depth - 1, seen)
+                    if obj_info != ():
+                        d[attr] = obj_info
+                except Exception as e:
+                    pass
+            return d
+        elif isinstance(obj, SudsObject):
+            d = {}
+            for key, val in iter(obj):
+                obj_info = self._get_obj_info(val, depth, seen)
+                if obj_info != ():
+                    d[key] = obj_info
+            return d
+        elif isinstance(obj, (list, tuple)):
+            l = []
+            for val in iter(obj):
+                obj_info = self._get_obj_info(val, depth, seen)
+                if obj_info != ():
+                    l.append(obj_info)
+            return l
+        elif isinstance(obj, (type(None), bool, float) + string_types + integer_types):
+            return obj
+        else:
+            return ()
+    
+        # build ordered list to loop over and dict with attributes
+    clist.append(name)
+    oblist[name] = dict((x, aobj.__dict__['_attributes'][x]) for x in aobj.__dict__['_attributes'] if 'private' not in x or not x.private)
+    
+                if record[12] == 'core':
+                supported_by = 'core'
+            elif record[12] == 'curated':
+                supported_by = 'curated'
+            elif record[12] == 'community':
+                supported_by = 'community'
+            else:
+                print('Module %s has no supported_by field.  Using community' % record[0])
+                supported_by = 'community'
+                supported_by = DEFAULT_METADATA['supported_by']
+    
+        integration_target_aliases_rows = [dict(
+        target=target.name,
+        alias=alias,
+    ) for target in targets for alias in target.aliases]
     
     try:
-    input = raw_input
-except NameError:
-    pass
+    ansible_out = subprocess.check_output(
+        ['ansible', 'localhost', '-m', 'setup'])
+except subprocess.CalledProcessError as e:
+    print('ERROR: ansible run failed, output was: \n')
+    print(e.output)
+    sys.exit(e.returncode)
     
-    if __name__ == '__main__':
-    main()
+            # set host pattern to default if not supplied
+        if len(self.args) > 0:
+            self.options.pattern = self.args[0]
+        else:
+            self.options.pattern = 'all'
+    
+    GITHUB_PATH = '{}/{}'.format(
+    PROJECT_GITHUB_USERNAME, PROJECT_GITHUB_REPOSITORY)
+GITHUB_URL = 'https://github.com/{}'.format(GITHUB_PATH)
+    
+        @callback
+    def async_remove():
+        '''Remove state listeners async.'''
+        unsub()
+        for async_remove in unsub_track_same.values():
+            async_remove()
+        unsub_track_same.clear()
+    
+        if event == SUN_EVENT_SUNRISE:
+        return async_track_sunrise(hass, call_action, offset)
+    return async_track_sunset(hass, call_action, offset)
 
     
-    if isinstance(helptext, bytes):
-    helptext = helptext.decode('utf-8')
     
-        with io.open(outfile, 'w', encoding='utf-8') as outf:
-        outf.write(readme)
+@asyncio.coroutine
+def async_trigger(hass, config, action):
+    '''Listen for state changes based on configuration.'''
+    value_template = config.get(CONF_VALUE_TEMPLATE)
+    value_template.hass = hass
     
-    
-def format_size(bytes):
-    return '%s (%d bytes)' % (format_bytes(bytes), bytes)
-    
-        with open(ZSH_COMPLETION_TEMPLATE) as f:
-        template = f.read()
-    
-    try:
-    # This will create an exe that needs Microsoft Visual C++ 2008
-    # Redistributable Package
-    import py2exe
-except ImportError:
-    if len(sys.argv) >= 2 and sys.argv[1] == 'py2exe':
-        print('Cannot import py2exe', file=sys.stderr)
-        exit(1)
-    
-        def test_format_note(self):
-        ydl = YoutubeDL()
-        self.assertEqual(ydl._format_note({}), '')
-        assertRegexpMatches(self, ydl._format_note({
-            'vbr': 10,
-        }), r'^\s*10k$')
-        assertRegexpMatches(self, ydl._format_note({
-            'fps': 30,
-        }), r'^30fps$')
-    
-        def test_cbc_decrypt(self):
-        data = bytes_to_intlist(
-            b'\x97\x92+\xe5\x0b\xc3\x18\x91ky9m&\xb3\xb5@\xe6'\xc2\x96.\xc8u\x88\xab9-[\x9e|\xf1\xcd'
-        )
-        decrypted = intlist_to_bytes(aes_cbc_decrypt(data, self.key, self.iv))
-        self.assertEqual(decrypted.rstrip(b'\x08'), self.secret_msg)
-    
-        def test_youtube_matching(self):
-        self.assertTrue(YoutubeIE.suitable('PLtS2H6bU1M'))
-        self.assertFalse(YoutubeIE.suitable('https://www.youtube.com/watch?v=AV6J6_AeFEQ&playnext=1&list=PL4023E734DA416012'))  # 668
-        self.assertMatch('http://youtu.be/BaW_jenozKc', ['youtube'])
-        self.assertMatch('http://www.youtube.com/v/BaW_jenozKc', ['youtube'])
-        self.assertMatch('https://youtube.googleapis.com/v/BaW_jenozKc', ['youtube'])
-        self.assertMatch('http://www.cleanvideosearch.com/media/action/yt/watch?videoId=8v_4O44sfjM', ['youtube'])
-    
-        # 128+2 SIGINT <http://www.tldp.org/LDP/abs/html/exitcodes.html>
-    ERROR_CTRL_C = 130
-    
-        def init_poolmanager(self, *args, **kwargs):
-        kwargs['ssl_version'] = self._ssl_version
-        super(HTTPieHTTPAdapter, self).init_poolmanager(*args, **kwargs)
-    
-        def iter_lines(self, chunk_size):
-        yield self.body, b''
-    
-    
-def repr_dict_nice(d):
-    def prepare_dict(d):
-        for k, v in d.items():
-            if isinstance(v, dict):
-                v = dict(prepare_dict(v))
-            elif isinstance(v, bytes):
-                v = v.decode('utf8')
-            elif not isinstance(v, (int, str)):
-                v = repr(v)
-            yield k, v
-    return json.dumps(
-        dict(prepare_dict(d)),
-        indent=4, sort_keys=True,
-    )
+        return async_track_state_change(hass, entity_id, zone_automation_listener,
+                                    MATCH_ALL, MATCH_ALL)
+
     
         @property
-    def bindata(self):
-        if not hasattr(self, '_bindata'):
-            self._bindata = urlopen(self.url).read()
-        return self._bindata
+    def device_state_attributes(self):
+        '''Return device specific state attributes.'''
+        return {'address': self.address}
     
+        url = '/api/cloud/login'
+    name = 'api:cloud:login'
     
-@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
-@pytest.mark.parametrize('filename', filenames)
-def test_rst_file_syntax(filename):
-    p = subprocess.Popen(
-        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE
-    )
-    err = p.communicate()[1]
-    assert p.returncode == 0, err.decode('utf8')
-
-    
-        r = http('GET', httpbin_both + '/headers', 'Accept;')
-    assert r.json['headers']['Accept'] == ''   # Accept has no value
-    
-        def test_CRLF_headers_only(self, httpbin):
-        r = http('--headers', 'GET', httpbin.url + '/get')
-        body = self._validate_crlf(r)
-        assert not body, 'Garbage after headers: %r' % r
-    
-    
-def load_vector(*names):
-    '''Load contents of a test vector.'''
-    # luckily, resource_string opens file in binary mode
-    return pkg_resources.resource_string(
-        __name__, os.path.join('testdata', *names))
-    
-            return responses
-    
-    
-def BuildServerConf():
-  '''Builds a dictionary mapping YCM Vim user options to values. Option names
-  don't have the 'ycm_' prefix.'''
-  # We only evaluate the keys of the vim globals and not the whole dictionary
-  # to avoid unicode issues.
-  # See https://github.com/Valloric/YouCompleteMe/pull/2151 for details.
-  keys = vimsupport.GetVimGlobalsKeys()
-  server_conf = {}
-  for key in keys:
-    if not key.startswith( YCM_VAR_PREFIX ):
-      continue
-    new_key = key[ len( YCM_VAR_PREFIX ): ]
-    new_value = vimsupport.VimExpressionToPythonType( 'g:' + key )
-    server_conf[ new_key ] = new_value
-    
-    
-@patch( 'ycm.client.messages_request.PostVimMessage',
-        new_callable = ExtendedMock )
-def HandlePollResponse_MultipleMessages_test( post_vim_message ):
-  assert_that( _HandlePollResponse( [ { 'message': 'this is a message' },
-                                      { 'message': 'this is another one' } ] ,
-                                    None ),
-               equal_to( True ) )
-    
-    
-def RegexNotFiltered_test():
-  opts = _JavaFilter( { 'regex' : 'taco' } )
-  f = _CreateFilterForTypes( opts, [ 'cs' ] )
-    
-        server_message = {
-      'message': 'this message came from the server'
-    }
-    
-    
-@patch( 'ycm.vimsupport.CurrentFiletypes', return_value = [ 'java' ] )
-def GetCompleteDoneHooks_ResultOnJava_test( *args ):
-  request = CompletionRequest( None )
-  result = list( request._GetCompleteDoneHooks() )
-  eq_( result, [ request._OnCompleteDone_FixIt ] )
-    
-    
-def ExtractKeywordsFromGroup_ContainedSyntaxArgAllowed_test():
-  assert_that( syntax_parse._ExtractKeywordsFromGroup(
-                 syntax_parse.SyntaxGroup( '', [
-                   'contained foo zoq',
-                   'contained bar goo',
-                   'far'
-                 ] ) ),
-               contains_inanyorder( 'foo', 'zoq', 'bar', 'goo', 'far' ) )
-
-    
-        def attach(self, observer):
-        if observer not in self._observers:
-            self._observers.append(observer)
-    
-        def __init__(self, radio):
-        self.radio = radio
-        self.stations = ['1250', '1380', '1510']
-        self.pos = 0
-        self.name = 'AM'
-    
-        @property
-    def _lazy_property(self):
-        if not hasattr(self, attr):
-            setattr(self, attr, fn(self))
-        return getattr(self, attr)
-    return _lazy_property
-    
-    class MidnightTimeProvider(object):
-    '''
-    Class implemented as hard-coded stub (in contrast to configurable stub).
-    '''
-    
-    # Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
-    
-    # Load the jpg file into a numpy array
-image = face_recognition.load_image_file('biden.jpg')
-    
-        # Return the result as json
-    result = {
-        'face_found_in_image': face_found,
-        'is_picture_of_obama': is_obama
-    }
-    return jsonify(result)
-    
-        # For a definition of each point index, see https://cdn-images-1.medium.com/max/1600/1*AbEg31EgkbXSQehuNJBlWg.png
-    if model == 'large':
-        return [{
-            'chin': points[0:17],
-            'left_eyebrow': points[17:22],
-            'right_eyebrow': points[22:27],
-            'nose_bridge': points[27:31],
-            'nose_tip': points[31:36],
-            'left_eye': points[36:42],
-            'right_eye': points[42:48],
-            'top_lip': points[48:55] + [points[64]] + [points[63]] + [points[62]] + [points[61]] + [points[60]],
-            'bottom_lip': points[54:60] + [points[48]] + [points[60]] + [points[67]] + [points[66]] + [points[65]] + [points[64]]
-        } for points in landmarks_as_tuples]
-    elif model == 'small':
-        return [{
-            'nose_tip': [points[4]],
-            'left_eye': points[2:4],
-            'right_eye': points[0:2],
-        } for points in landmarks_as_tuples]
-    else:
-        raise ValueError('Invalid landmarks model type. Supported models are ['small', 'large'].')
-    
-    # Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
-    
-    # Loop through each face found in the unknown image
-for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-    # See if the face is a match for the known face(s)
-    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-    
-    requirements = [
-    'face_recognition_models>=0.3.0',
-    'Click>=6.0',
-    'dlib>=19.7',
-    'numpy',
-    'Pillow'
-]
+        def get_device_name(self, device):
+        '''Return the name of the given device or None if we don't know.'''
+        if not self.last_results:
+            return None
+        for client in self.last_results:
+            if client.mac == device:
+                return client.ip
+        return None
