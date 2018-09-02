e90bafeefe15d67e8259e22ccec49b628cbb3750
SUNsung <weatherius@gmail.com>
2018-09-02 04:56:41 +0300

    Rewrite [14uAwoLZpzO3o14nJ1rNxsRVHo14wsBqzd1t1OA14Ysk.rb] File in: 2018-09-02 04:56:40

--- ruby/14uAwoLZpzO3o14nJ1rNxsRVHo14wsBqzd1t1OA14Ysk.rb
+++ ruby/14uAwoLZpzO3o14nJ1rNxsRVHo14wsBqzd1t1OA14Ysk.rb
@@ -1,96 +1,195 @@
 
         
-                  theme.create!
-          Jekyll.logger.info 'Your new Jekyll theme, #{theme.name.cyan},' \
-                             ' is ready for you in #{theme.path.to_s.cyan}!'
-          Jekyll.logger.info 'For help getting started, read #{theme.path}/README.md.'
-        end
-        # rubocop:enable Metrics/AbcSize
+            context 'updating existing users' do
+      it 'follows the edit link' do
+        visit admin_users_path
+        click_on('bob')
+        expect(page).to have_text('Edit User')
+      end
+    
+    describe 'Dry running an Agent', js: true do
+  let(:agent)   { agents(:bob_website_agent) }
+  let(:formatting_agent) { agents(:bob_formatting_agent) }
+  let(:user)    { users(:bob) }
+  let(:emitter) { agents(:bob_weather_agent) }
+    
+      before do
+    login_as(user)
+  end
+    
+      describe '#style_colors' do
+    it 'returns a css style-formated version of the scenario foreground and background colors' do
+      expect(style_colors(scenario)).to eq('color:#AAAAAA;background-color:#000000')
+    end
+    
+        it 'is turned off for existing instances of Huginn' do
+      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { nil }
+      DefaultScenarioImporter.import(user)
+    end
+    
+              valid_parsed_weather_agent_data.each do |key, value|
+            if key == :type
+              value = value.split('::').last
+            end
+            expect(weather_agent_diff).to respond_to(key)
+            field = weather_agent_diff.send(key)
+            expect(field).to be_a(ScenarioImport::AgentDiff::FieldDiff)
+            expect(field.incoming).to eq(value)
+            expect(field.updated).to eq(value)
+            expect(field.current).to be_nil
+          end
+          expect(weather_agent_diff).not_to respond_to(:propagate_immediately)
+    
+      it 'converts values to Float' do
+    expect(location.lat).to be_a Float
+    expect(location.lat).to eq 2.0
+    expect(location.lng).to be_a Float
+    expect(location.lng).to eq 3.0
+    expect(location.radius).to be_a Float
+    expect(location.radius).to eq 300.0
+    expect(location.speed).to be_a Float
+    expect(location.speed).to eq 2.0
+    expect(location.course).to be_a Float
+    expect(location.course).to eq 30.0
+  end
+    
+    source 'https://rubygems.org'
+    
+    class Devise::ConfirmationsController < DeviseController
+  # GET /resource/confirmation/new
+  def new
+    self.resource = resource_class.new
+  end
+    
+        def confirmation_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :confirmation_instructions, opts)
+    end
+    
+    group :docs do
+  gem 'typhoeus'
+  gem 'nokogiri'
+  gem 'html-pipeline'
+  gem 'image_optim'
+  gem 'image_optim_pack', platforms: :ruby
+  gem 'progress_bar', require: false
+  gem 'unix_utils', require: false
+  gem 'tty-pager', require: false
+end
+    
+      {
+    '/tips'                   => '/help',
+    '/css-data-types/'        => '/css-values-units/',
+    '/css-at-rules/'          => '/?q=css%20%40',
+    '/dom/window/setinterval' => '/dom/windoworworkerglobalscope/setinterval',
+    '/html/article'           => '/html/element/article',
+    '/html-html5/'            => 'html-elements/',
+    '/html-standard/'         => 'html-elements/',
+    '/http-status-codes/'     => '/http-status/',
+    '/ruby/bignum'            => '/ruby~2.3/bignum',
+    '/ruby/fixnum'            => '/ruby~2.3/fixnum',
+  }.each do |path, url|
+    class_eval <<-CODE, __FILE__, __LINE__ + 1
+      get '#{path}' do
+        redirect '#{url}', 301
+      end
+    CODE
+  end
+    
+      extend Instrumentable
+    
+        def version
+      context[:version]
+    end
+    
+        def assert_index(index)
+      i = index.is_a?(Integer) ? index : @filters.index(filter_const(index))
+      raise 'No such filter to insert: #{index}' unless i
+      i
+    end
+  end
+end
+
+    
+        def type=(value)
+      @type = value.try :strip
+    end
+    
+        def handle_response(response)
+      on_response.each do |callback|
+        result = callback.call(response)
+        result.each { |url| request(url) } if result.is_a? Array
       end
     end
   end
 end
 
     
-          mutable false
-    
-          private
-    
-          def y_day
-        @obj.date.strftime('%j')
-      end
-    
-        # --
-    # Check if an entry matches a specific pattern and return true,false.
-    # Returns true if path matches against any glob pattern.
-    # --
-    def glob_include?(enum, entry)
-      entry_path = Pathutil.new(site.in_source_dir).join(entry)
-      enum.any? do |exp|
-        # Users who send a Regexp knows what they want to
-        # exclude, so let them send a Regexp to exclude files,
-        # we will not bother caring if it works or not, it's
-        # on them at this point.
-    
-      def tumblr_consumer_secret
-    ENV['TUMBLR_OAUTH_SECRET']
-  end
-    
-    module SortableTable
-  extend ActiveSupport::Concern
-    
-      def index
-    if params[:agent_id]
-      @agent = current_user.agents.find(params[:agent_id])
-      @events = @agent.events.page(params[:page])
-    else
-      @events = current_user.events.preload(:agent).page(params[:page])
+        def process_response?(response)
+      response.body.present?
     end
     
-        respond_to do |format|
-      if @user_credential.update_attributes(user_credential_params)
-        format.html { redirect_to user_credentials_path, notice: 'Your credential was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user_credential.errors, status: :unprocessable_entity }
-      end
+    describe 'Kernel#srand' do
+  it 'needs to be reviewed for spec completeness'
+end
+
+    
+      before :each do
+    ENV['TEST_SH_EXPANSION'] = 'foo'
+    @shell_var = '$TEST_SH_EXPANSION'
+    platform_is :windows do
+      @shell_var = '%TEST_SH_EXPANSION%'
     end
   end
     
-        if resource.errors.empty?
-      set_flash_message! :notice, :unlocked
-      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
-    end
-  end
+            def find_order
+          @order = Spree::Order.find_by!(number: order_id)
+        end
     
-        def default_failure_app(options)
-      @failure_app = options[:failure_app] || Devise::FailureApp
-      if @failure_app.is_a?(String)
-        ref = Devise.ref(@failure_app)
-        @failure_app = lambda { |env| ref.get.call(env) }
-      end
-    end
+            def inventory_unit
+          @inventory_unit ||= InventoryUnit.accessible_by(current_ability, :read).find(params[:id])
+        end
     
-          expect(user.reload.current_sign_in_at).to be_within(1.0).of(prior)
-    end
+            def new; end
     
-      def create_issue(repo, title, body)
-    url = 'https://api.github.com/repos/#{repo}/issues'
-    data = { 'title' => title, 'body' => body }
-    scopes = GitHub::CREATE_ISSUE_FORK_OR_PR_SCOPES
-    GitHub.open_api(url, data: data, scopes: scopes)['html_url']
-  end
-    
-              # Don't use GitHub codeload URLs
-          codeload_gh_pattern = %r{https?://codeload\.github\.com/(.+)/(.+)/(?:tar\.gz|zip)/(.+)}
-          audit_urls(urls, codeload_gh_pattern) do |match, url|
-            problem <<~EOS
-              Use GitHub archive URLs:
-                https://github.com/#{match[1]}/#{match[2]}/archive/#{match[3]}.tar.gz
-              Rather than codeload:
-                #{url}
-            EOS
+            def create
+          authorize! :create, Spree::OptionValue
+          @option_value = scope.new(option_value_params)
+          if @option_value.save
+            render :show, status: 201
+          else
+            invalid_resource!(@option_value)
           end
+        end
+    
+                import_params = if @current_user_roles.include?('admin')
+                              params[:order].present? ? params[:order].permit! : {}
+                            else
+                              order_params
+                            end
+    
+            def payment_params
+          params.require(:payment).permit(permitted_payment_attributes)
+        end
+      end
+    end
+  end
+end
+
+    
+              @properties = if params[:ids]
+                          @properties.where(id: params[:ids].split(',').flatten)
+                        else
+                          @properties.ransack(params[:q]).result
+                        end
+    
+      it 'records when the config was read' do
+    expect(subject.read_at).to be <= Time.now
+  end
+    
+          puts 'Halting #{machines}'
+      LogStash::VagrantHelpers.halt(machines, options)
+    end
+  end
