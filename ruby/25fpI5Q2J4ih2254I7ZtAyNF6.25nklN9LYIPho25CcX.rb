9dde6f14ffa4cf4d1ab379cde79945df31f31b49
SUNsung <weatherius@gmail.com>
2018-09-02 21:55:36 +0300

    Rewrite [25fpI5Q2J4ih2254I7ZtAyNF6.25nklN9LYIPho25CcX.rb] File in: 2018-09-02 21:55:35

--- ruby/25fpI5Q2J4ih2254I7ZtAyNF6.25nklN9LYIPho25CcX.rb
+++ ruby/25fpI5Q2J4ih2254I7ZtAyNF6.25nklN9LYIPho25CcX.rb
@@ -1,161 +1,124 @@
 
         
-                if params[:default_keychain]
-          # if there is no default keychain - setting the original will fail - silent this error
-          begin
-            Actions.lane_context[Actions::SharedValues::ORIGINAL_DEFAULT_KEYCHAIN] = Fastlane::Actions.sh('security default-keychain', log: false).strip
-          rescue
-          end
-          commands << Fastlane::Actions.sh('security default-keychain -s #{keychain_path}', log: false)
-        end
-    
-          it 'get HG revision number' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          get_build_number_repository(
-            use_hg_revision_number: true
-          )
-        end').runner.execute(:test)
-    
-            expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::VERSION_NUMBER]).to match(/cd .* && agvtool new-marketing-version 1.0.1/)
-      end
-    
-      describe 'DATA_PTR' do
-    it 'returns the struct data' do
-      a = @s.typed_wrap_struct(1024)
-      @s.typed_get_struct_data_ptr(a).should == 1024
-    end
-  end
-end
-
-    
-      it 'adds a directory to $LOAD_PATH' do
-    dir = tmp('rubylib/incl')
-    ENV['RUBYLIB'] = @pre + dir
-    paths = ruby_exe('puts $LOAD_PATH').lines.map(&:chomp)
-    paths.should include(dir)
+          def self.all
+    Dir['#{root_path}/docs/scrapers/**/*.rb'].
+      map { |file| File.basename(file, '.rb') }.
+      map { |name| const_get(name.camelize) }.
+      sort { |a, b| a.name.casecmp(b.name) }.
+      reject(&:abstract)
   end
     
-        header = Gem::Package::TarHeader.new(:name => name, :mode => mode,
-                                         :size => 0, :typeflag => '2',
-                                         :linkname => target,
-                                         :prefix => prefix,
-                                         :mtime => ENV['SOURCE_DATE_EPOCH'] ? Time.at(ENV['SOURCE_DATE_EPOCH'].to_i).utc : Time.now).to_s
-    
-      def test_chdir
-    assert_in_out_err(%w(-C), '', [], /Can't chdir/)
-    
-            File.open(@path,'a').close unless File.exist?(@path)
-        reload
-      end
-    
-          when /^(\$[^.]*)$/
-        # global var
-        regmessage = Regexp.new(Regexp.quote($1))
-        candidates = global_variables.collect{|m| m.to_s}.grep(regmessage)
-    
-    def assert_normal_exit(testsrc, *rest, timeout: nil, **opt)
-  newtest
-  message, ignore_signals = rest
-  message ||= ''
-  show_progress(message) {
-    faildesc = nil
-    filename = make_srcfile(testsrc)
-    old_stderr = $stderr.dup
-    timeout_signaled = false
-    begin
-      $stderr.reopen('assert_normal_exit.log', 'w')
-      io = IO.popen('#{@ruby} -W0 #{filename}')
-      pid = io.pid
-      th = Thread.new {
-        io.read
-        io.close
-        $?
-      }
-      if !th.join(timeout)
-        Process.kill :KILL, pid
-        timeout_signaled = true
-      end
-      status = th.value
-    ensure
-      $stderr.reopen(old_stderr)
-      old_stderr.close
+        def current_url
+      context[:url]
     end
-    if status && status.signaled?
-      signo = status.termsig
-      signame = Signal.list.invert[signo]
-      unless ignore_signals and ignore_signals.include?(signame)
-        sigdesc = 'signal #{signo}'
-        if signame
-          sigdesc = 'SIG#{signame} (#{sigdesc})'
-        end
-        if timeout_signaled
-          sigdesc << ' (timeout)'
-        end
-        faildesc = pretty(testsrc, 'killed by #{sigdesc}', nil)
-        stderr_log = File.read('assert_normal_exit.log')
-        if !stderr_log.empty?
-          faildesc << '\n' if /\n\z/ !~ faildesc
-          stderr_log << '\n' if /\n\z/ !~ stderr_log
-          stderr_log.gsub!(/^.*\n/) { '| ' + $& }
-          faildesc << stderr_log
-        end
-      end
+    
+    module Docs
+  class PageDb
+    attr_reader :pages
+    
+        include Instrumentable
+    
+      it 'writes each array element on a line when passes an array' do
+    lambda {
+      $VERBOSE = true
+      warn(['line 1', 'line 2'])
+    }.should output(nil, 'line 1\nline 2\n')
+  end
+    
+    
+  # Returns a collection of found hidden inputs
+  #
+  # @return [Array<Hash>] An array, each element represents a form that contains a hash of found hidden inputs
+  #  * 'name' [String] The hidden input's original name. The value is the hidden input's original value.
+  # @example
+  #  res = send_request_cgi('uri'=>'/')
+  #  inputs = res.get_hidden_inputs
+  #  session_id = inputs[0]['sessionid'] # The first form's 'sessionid' hidden input
+  def get_hidden_inputs
+    forms = []
+    noko = get_html_document
+    noko.search('form').each_entry do |form|
+      found_inputs = {}
+      form.search('input').each_entry do |input|
+        input_type = input.attributes['type'] ? input.attributes['type'].value : ''
+        next if input_type !~ /hidden/i
+    
+        when nil
+      dprint('Invalid control packet: #{pkt.unpack('H*')[0]}')
     end
-    faildesc
+  end
+    
+    # Default timings
+IAX_DEFAULT_REG_REFRESH = 60
+IAX_DEFAULT_TIMEOUT     = 10
+    
+    
+  #
+  # Payload types were copied from xCAT-server source code (IPMI.pm)
+  #
+  RMCP_ERRORS = {
+    1 => 'Insufficient resources to create new session (wait for existing sessions to timeout)',
+    2 => 'Invalid Session ID', #this shouldn't occur...
+    3 => 'Invalid payload type',#shouldn't occur..
+    4 => 'Invalid authentication algorithm', #if this happens, we need to enhance our mechanism for detecting supported auth algorithms
+    5 => 'Invalid integrity algorithm', #same as above
+    6 => 'No matching authentication payload',
+    7 => 'No matching integrity payload',
+    8 => 'Inactive Session ID', #this suggests the session was timed out while trying to negotiate, shouldn't happen
+    9 => 'Invalid role',
+    0xa => 'Unauthorised role or privilege level requested',
+    0xb => 'Insufficient resources to create a session at the requested role',
+    0xc => 'Invalid username length',
+    0xd => 'Unauthorized name',
+    0xe => 'Unauthorized GUID',
+    0xf => 'Invalid integrity check value',
+    0x10 => 'Invalid confidentiality algorithm',
+    0x11 => 'No cipher suite match with proposed security algorithms',
+    0x12 => 'Illegal or unrecognized parameter', #have never observed this, would most likely mean a bug in xCAT or IPMI device
   }
-end
     
-      # Before we load the schema, define the timestamp_id function.
-  # Idiomatically, we might do this in a migration, but then it
-  # wouldn't end up in schema.rb, so we'd need to figure out a way to
-  # get it in before doing db:setup as well. This is simpler, and
-  # ensures it's always in place.
-  Rake::Task['db:schema:load'].enhance ['db:define_timestamp_id']
+        head + [data.length].pack('v') + data
+  end
     
-      has_one :icon, serializer: ActivityPub::ImageSerializer
-    
-        existence_maps.each do |group|
-      existing_one = group.key(true)
-    
-          def calculate_column_widths(rows)
-        num_columns = rows.map { |row| row.values.length }.max
-        Array.new(num_columns) do |col|
-          rows.map { |row| row.values[col].to_s.length }.max
+            # Sends a Kerberos Request over a tcp connection
+        #
+        # @param req [Rex::Proto::Kerberos::Model::KdcRequest] the request to send
+        # @return [Integer] the number of bytes sent
+        # @raise [RuntimeError] if the request can't be encoded
+        def send_request_tcp(req)
+          data = req.encode
+          length = [data.length].pack('N')
+          connection.put(length + data)
         end
-      end
     
-            on roles(target_roles) do
-          unless test '[ -f #{file.to_s.shellescape} ]'
-            info 'Uploading #{prerequisite_file} to #{file}'
-            upload! File.open(prerequisite_file), file
+              # Encodes the data
+          #
+          # @return [OpenSSL::ASN1::OctetString]
+          def encode_data(data)
+            OpenSSL::ASN1::OctetString.new(data)
           end
-        end
-      end
-    end
-    
-          it 'selects using the string when a host filter is present' do
-        dsl.set :filter, host: 'server.local'
-        SSHKit::Coordinator.expects(:new).with(['server.local']).returns(@coordinator)
-        dsl.on('server.local')
-      end
-    
-            def on_send(node)
-          return unless multiple_compare?(node)
-    
-            def parent_block_node(node)
-          node.each_ancestor(:block).first
         end
       end
     end
   end
 end
-
     
-            MSG = 'Do not freeze immutable objects, as freezing them has no ' \
-              'effect.'.freeze
+              # Rex::Proto::Kerberos::Model::Checksum decoding isn't supported
+          #
+          # @raise [NotImplementedError]
+          def decode(input)
+            raise ::NotImplementedError, 'Checksum decoding not supported'
+          end
     
-            inspect_source(expected_annotations.plain_source, file)
-        actual_annotations =
-          expected_annotations.with_offense_annotations(cop.offenses)
-        expect(actual_annotations.to_s).to eq(expected_annotations.to_s)
-      end
+              def self.decode(input)
+            elem = self.new
+            elem.decode(input)
+          end
+    
+              # Decodes the susec field
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Integer]
+          def decode_susec(input)
+            input.value[0].value.to_i
+          end
