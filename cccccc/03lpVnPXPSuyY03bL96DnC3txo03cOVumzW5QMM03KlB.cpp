e1a7c06a69eb0afebbac1ae6979992f575712f7a
SUNsung <weatherius@gmail.com>
2018-09-02 10:07:28 +0300

    Rewrite [03lpVnPXPSuyY03bL96DnC3txo03cOVumzW5QMM03KlB.cpp] File in: 2018-09-02 10:07:28

--- cccccc/03lpVnPXPSuyY03bL96DnC3txo03cOVumzW5QMM03KlB.cpp
+++ cccccc/03lpVnPXPSuyY03bL96DnC3txo03cOVumzW5QMM03KlB.cpp
@@ -1,85 +1,198 @@
 
         
-        
-    {}  // namespace guetzli
+        /*
+ * Call a function that produces a return value for each element of
+ * `inputs' in parallel, and collect the results.
+ *
+ * Requires: the type returned from the function call must be
+ * DefaultConstructible, and either MoveAssignable or Assignable.
+ *
+ * If `func' throws an exception, the results of the output vector
+ * will contain some default-constructed values.
+ */
+template<class Func, class Items>
+auto map(Items&& inputs, Func func) -> std::vector<decltype(func(inputs[0]))> {
+  std::vector<decltype(func(inputs[0]))> retVec(inputs.size());
+  auto const retMem = &retVec[0];
+    }
     
-    bool SetDepth(int p0, HuffmanTree *pool, uint8_t *depth, int max_depth) {
-  int stack[17];
-  int level = 0;
-  int p = p0;
-  assert(max_depth <= 16);
-  stack[0] = -1;
-  while (true) {
-    if (pool[p].index_left_ >= 0) {
-      level++;
-      if (level > max_depth) return false;
-      stack[level] = pool[p].index_right_or_value_;
-      p = pool[p].index_left_;
-      continue;
+      /*
+   * Patch a branch to the correct target.
+   *
+   * It decodes the branch @jmp to decide whether it's an absolute branch or an
+   * offset branch and patches it properly.
+   */
+  static void patchBranch(CodeAddress jmp, CodeAddress dest);
+  static void patchAbsolute(CodeAddress jmp, CodeAddress dest);
+    
+    #include <string>
+#include <utility>
+#include <vector>
+    
+    inline void* ExecutionContext::operator new(size_t s) {
+  // Can't use req::make_raw here because we want raw memory, not a constructed
+  // object. This gets called generically from ThreadLocal, so we can't just
+  // change the call-sites.
+  return req::malloc(s, type_scan::getIndexForMalloc<ExecutionContext>());
+}
+    
+    struct Options;
+struct FileMetaData;
+    
+    
+    {    fprintf(stderr,
+            'expected=%d..%d; got=%d; bad_keys=%d; bad_values=%d; missed=%d\n',
+            min_expected, max_expected, correct, bad_keys, bad_values, missed);
+    ASSERT_LE(min_expected, correct);
+    ASSERT_GE(max_expected, correct);
+  }
+    
+        Slice key = input->key();
+    if (compact->compaction->ShouldStopBefore(key) &&
+        compact->builder != nullptr) {
+      status = FinishCompactionOutputFile(compact, input);
+      if (!status.ok()) {
+        break;
+      }
+    }
+    
+    class DBImpl : public DB {
+ public:
+  DBImpl(const Options& options, const std::string& dbname);
+  virtual ~DBImpl();
+    }
+    
+    
+    {}  // namespace
+    
+    
+    {}  // namespace leveldb
+    
+    int main(int argc, char** argv) {
+  leveldb::Env* env = leveldb::Env::Default();
+  bool ok = true;
+  if (argc < 2) {
+    Usage();
+    ok = false;
+  } else {
+    std::string command = argv[1];
+    if (command == 'dump') {
+      ok = leveldb::HandleDumpCommand(env, argv+2, argc-2);
     } else {
-      depth[pool[p].index_right_or_value_] = static_cast<uint8_t>(level);
+      Usage();
+      ok = false;
     }
-    while (level >= 0 && stack[level] == -1) level--;
-    if (level < 0) return true;
-    p = stack[level];
-    stack[level] = -1;
   }
+  return (ok ? 0 : 1);
+}
+
+    
+    TEST(LogTest, MissingLastIsIgnored) {
+  Write(BigString('bar', kBlockSize));
+  // Remove the LAST block, including header.
+  ShrinkSize(14);
+  ASSERT_EQ('EOF', Read());
+  ASSERT_EQ('', ReportMessage());
+  ASSERT_EQ(0, DroppedBytes());
 }
     
-    // Computes the DCT (Discrete Cosine Transform) of the 8x8 array in 'block',
-// scaled up by a factor of 16. The values in 'block' are laid out row-by-row
-// and the result is written to the same memory area.
-void ComputeBlockDCT(coeff_t* block);
     
-    // Mimic libjpeg's heuristics to guess jpeg color space.
-// Requires that the jpg has 3 components.
-bool HasYCbCrColorSpace(const JPEGData& jpg) {
-  bool has_Adobe_marker = false;
-  uint8_t Adobe_transform = 0;
-  for (const std::string& app : jpg.app_data) {
-    if (static_cast<uint8_t>(app[0]) == 0xe0) {
-      return true;
-    } else if (static_cast<uint8_t>(app[0]) == 0xee && app.size() >= 15) {
-      has_Adobe_marker = true;
-      Adobe_transform = app[14];
+    {
+    {  void ArchiveFile(const std::string& fname) {
+    // Move into another directory.  E.g., for
+    //    dir/foo
+    // rename to
+    //    dir/lost/foo
+    const char* slash = strrchr(fname.c_str(), '/');
+    std::string new_dir;
+    if (slash != nullptr) {
+      new_dir.assign(fname.data(), slash - fname.data());
     }
+    new_dir.append('/lost');
+    env_->CreateDir(new_dir);  // Ignore error
+    std::string new_file = new_dir;
+    new_file.append('/');
+    new_file.append((slash == nullptr) ? fname.c_str() : slash + 1);
+    Status s = env_->RenameFile(fname, new_file);
+    Log(options_.info_log, 'Archiving %s: %s\n',
+        fname.c_str(), s.ToString().c_str());
   }
-  if (has_Adobe_marker) {
-    return (Adobe_transform != 0);
-  }
-  const int cid0 = jpg.components[0].id;
-  const int cid1 = jpg.components[1].id;
-  const int cid2 = jpg.components[2].id;
-  return (cid0 != 'R' || cid1 != 'G' || cid2 != 'B');
+};
+}  // namespace
+    
+    bool Action::isDone() const
+{
+    return true;
 }
     
-    namespace {
+        p = reverse->getControlPointAtIndex(reverse->count()-1);
+    reverse->removeControlPointAtIndex(reverse->count()-1);
+    
+    // implementation of ReuseGrid
+    
+    // FIXME: Passing 'const O *' instead of 'const O&' because HASH_FIND_IT requires the address of a pointer
+// and, it is not possible to get the address of a reference
+size_t ActionManager::getNumberOfRunningActionsInTargetByTag(const Node *target,
+                                                             int tag)
+{
+    CCASSERT(tag != Action::INVALID_TAG, 'Invalid tag value!');
     }
     
-      JpegHistogram() { Clear(); }
-  void Clear() {
-    memset(counts, 0, sizeof(counts));
-    counts[kSize - 1] = 1;
-  }
-  void Add(int symbol) {
-    counts[symbol] += 2;
-  }
-  void Add(int symbol, int weight) {
-    counts[symbol] += 2 * weight;
-  }
-  void AddHistogram(const JpegHistogram& other) {
-    for (int i = 0; i + 1 < kSize; ++i) {
-      counts[i] += other.counts[i];
-    }
-    counts[kSize - 1] = 1;
-  }
-  int NumSymbols() const {
-    int n = 0;
-    for (int i = 0; i + 1 < kSize; ++i) {
-      n += (counts[i] > 0 ? 1 : 0);
-    }
-    return n;
-  }
+        /** 
+     * @brief Initializes with a duration and destination percentage. 
+     * @param duration Specify the duration of the ProgressTo action. It's a value in seconds.
+     * @param percent Specify the destination percentage.
+     * @return If the creation success, return true; otherwise, return false.
+     */
+    bool initWithDuration(float duration, float percent);
     
+        if (_once == false)
+    {
+        for (i = 0; i < _gridSize.width; ++i)
+        {
+            for (j = 0; j < _gridSize.height; ++j)
+            {
+                Quad3 coords = getOriginalTile(Vec2(i ,j));
+                
+                // X
+                coords.bl.x += ( rand() % (_randrange*2) ) - _randrange;
+                coords.br.x += ( rand() % (_randrange*2) ) - _randrange;
+                coords.tl.x += ( rand() % (_randrange*2) ) - _randrange;
+                coords.tr.x += ( rand() % (_randrange*2) ) - _randrange;
+                
+                // Y
+                coords.bl.y += ( rand() % (_randrange*2) ) - _randrange;
+                coords.br.y += ( rand() % (_randrange*2) ) - _randrange;
+                coords.tl.y += ( rand() % (_randrange*2) ) - _randrange;
+                coords.tr.y += ( rand() % (_randrange*2) ) - _randrange;
+    }
+    }
+    }
     
-    {}  // namespace guetzli
+    CC_CONSTRUCTOR_ACCESS:
+    FadeOutTRTiles() {}
+    virtual ~FadeOutTRTiles() {}
+    
+    void ActionTween::startWithTarget(Node *target)
+{
+    CCASSERT(dynamic_cast<ActionTweenDelegate*>(target), 'target must implement ActionTweenDelegate');
+    ActionInterval::startWithTarget(target);
+    _delta = _to - _from;
+}
+    
+     ActionTween is an action that lets you update any property of an object.
+ For example, if you want to modify the 'width' property of a target from 200 to 300 in 2 seconds, then:
+    
+        /** Returns a Animation that was previously added.
+     * If the name is not found it will return nil.
+     * You should retain the returned copy if you are going to use it.
+     *
+     * @return A Animation that was previously added. If the name is not found it will return nil.
+     */
+    Animation* getAnimation(const std::string& name);
+    /**
+     * @deprecated. Use getAnimation() instead
+     * @js NA
+     * @lua NA
+     */
+    CC_DEPRECATED_ATTRIBUTE Animation* animationByName(const std::string& name){ return getAnimation(name); }
