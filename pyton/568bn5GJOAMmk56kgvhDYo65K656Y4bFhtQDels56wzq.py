82e58003dff5c761696bb5a0cad8dc4ba1c97d2b
SUNsung <weatherius@gmail.com>
2018-09-03 02:57:35 +0300

    Rewrite [568bn5GJOAMmk56kgvhDYo65K656Y4bFhtQDels56wzq.py] File in: 2018-09-03 02:57:34

--- pyton/568bn5GJOAMmk56kgvhDYo65K656Y4bFhtQDels56wzq.py
+++ pyton/568bn5GJOAMmk56kgvhDYo65K656Y4bFhtQDels56wzq.py
@@ -1,114 +1,17 @@
 
         
-        
-if __name__ == '__main__':
-    main()
-
-    
-      Args:
-    hps: The hyper parameters for the model.
-    kind: (optional) The kind of model to build.  Training vs inference require
-      different graphs.
-    datasets: The datasets structure (see top of lfads.py).
-    
-        # split into train and validation sets
-  train_inds, valid_inds = get_train_n_valid_inds(E, train_percentage,
-                                                  nreplications)
-    
-      @property
-  def eos(self):
-    return self._eos
-    
-        sys.stderr.write('Recovering checkpoint %s\n' % ckpt_file)
-    sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
-    sess.run('save/restore_all', {'save/Const:0': ckpt_file})
-    sess.run(t['states_init'])
-    
-    
-def ptb_iterator(raw_data, batch_size, num_steps, epoch_size_override=None):
-  '''Iterate on the raw PTB data.
-    
-        # Cumulative Discounted Returns.  The true value function V*(s).
-    cumulative_rewards = []
-    for t in xrange(FLAGS.sequence_length):
-      cum_value = tf.zeros(shape=[FLAGS.batch_size / 2])
-      for s in xrange(t, FLAGS.sequence_length):
-        cum_value += reward_missing_list[s] * np.power(gamma, (
-            s - t)) * rewards_list[s]
-      cumulative_rewards.append(cum_value)
-    cumulative_rewards = tf.stack(cumulative_rewards, axis=1)
-    
-        elif (FLAGS.discriminator_model == 'bidirectional_zaremba' or
-          FLAGS.discriminator_model == 'bidirectional_vd'):
-      dis_fwd_variable_maps = variable_mapping.dis_fwd_bidirectional(hparams)
-      dis_bwd_variable_maps = variable_mapping.dis_bwd_bidirectional(hparams)
-      # Savers for the forward/backward Discriminator components.
-      dis_fwd_init_saver = tf.train.Saver(var_list=dis_fwd_variable_maps)
-      dis_bwd_init_saver = tf.train.Saver(var_list=dis_bwd_variable_maps)
-      init_savers['dis_fwd_init_saver'] = dis_fwd_init_saver
-      init_savers['dis_bwd_init_saver'] = dis_bwd_init_saver
-    
-    # fill the picture with our saved filters
-for i in range(n):
-    for j in range(n):
-        img, loss = kept_filters[i * n + j]
-        stitched_filters[(img_width + margin) * i: (img_width + margin) * i + img_width,
-                         (img_height + margin) * j: (img_height + margin) * j + img_height, :] = img
-    
-    
-class L1L2(Regularizer):
-    '''Regularizer for L1 and L2 regularization.
-    
-        # Arguments
-        model: target model for the conversion.
-    '''
-    # Note: SeparableConvolution not included
-    # since only supported by TF.
-    conv_classes = {
-        'Conv1D',
-        'Conv2D',
-        'Conv3D',
-        'Conv2DTranspose',
-    }
-    to_assign = []
-    for layer in model.layers:
-        if layer.__class__.__name__ in conv_classes:
-            original_kernel = K.get_value(layer.kernel)
-            converted_kernel = convert_kernel(original_kernel)
-            to_assign.append((layer.kernel, converted_kernel))
-    K.batch_set_value(to_assign)
-    
-        def __init__(self):
-        Resource.__init__(self)
-        self.concurrent = 0
-        self.tail = deque(maxlen=100)
-        self._reset_stats()
-    
-        def __init__(self, *a, **kw):
-        super(QPSSpider, self).__init__(*a, **kw)
-        if self.qps is not None:
-            self.qps = float(self.qps)
-            self.download_delay = 1 / self.qps
-        elif self.download_delay is not None:
-            self.download_delay = float(self.download_delay)
-    
-    
-class _BenchServer(object):
-    
-    from scrapy.commands import ScrapyCommand
+        from scrapy.commands import ScrapyCommand
 from scrapy.contracts import ContractsManager
 from scrapy.utils.misc import load_object
 from scrapy.utils.conf import build_component_list
     
-        @property
-    def templates_dir(self):
-        _templates_base_dir = self.settings['TEMPLATES_DIR'] or \
-            join(scrapy.__path__[0], 'templates')
-        return join(_templates_base_dir, 'spiders')
-
-    
-                raise ContractFail('Returned %s %s, expected %s' % \
-                (occurrences, self.obj_name, expected))
+        def _print_response(self, response, opts):
+        if opts.headers:
+            self._print_headers(response.request.headers, b'>')
+            print('>')
+            self._print_headers(response.headers, b'<')
+        else:
+            self._print_bytes(response.body)
     
         def _get_slot(self, request, spider):
         key = self._get_slot_key(request, spider)
@@ -117,107 +20,10 @@ from scrapy.utils.conf import build_component_list
             conc, delay = _get_concurrency_delay(conc, spider, self.settings)
             self.slots[key] = Slot(conc, delay, self.randomize_delay)
     
-        def __init__(self, crawler):
-        self._crawler = crawler
-        self._schemes = {}  # stores acceptable schemes on instancing
-        self._handlers = {}  # stores instanced handlers for schemes
-        self._notconfigured = {}  # remembers failed handlers
-        handlers = without_none_values(
-            crawler.settings.getwithbase('DOWNLOAD_HANDLERS'))
-        for scheme, clspath in six.iteritems(handlers):
-            self._schemes[scheme] = clspath
-    
-        def set_appid_not_exist(self, appid):
-        self.logger.warn('APPID_manager, set_appid_not_exist %s', appid)
-        with self.lock:
-            if appid not in self.not_exist_appids:
-                self.not_exist_appids.append(appid)
-            try:
-                self.config.GAE_APPIDS.remove(appid)
-            except:
-                pass
-    
-    current_path = os.path.dirname(os.path.abspath(__file__))
-root_path = os.path.abspath( os.path.join(current_path, os.pardir, os.pardir))
-data_path = os.path.abspath(os.path.join(root_path, os.pardir, os.pardir, 'data'))
-module_data_path = os.path.join(data_path, 'gae_proxy')
-python_path = os.path.abspath( os.path.join(root_path, 'python27', '1.0'))
-    
-    
-    def setTokenTypeChannel(self, ttype, channel):
-        '''
-        A simple filter mechanism whereby you can tell this token stream
-        to force all tokens of type ttype to be on channel.  For example,
-        when interpreting, we cannot exec actions so we need to tell
-        the stream to force all WS and NEWLINE to be a different, ignored
-        channel.
-	'''
-        
-        self.channelOverrideMap[ttype] = channel
-    
-    
-def Main():
-  build_file = p.join( DIR_OF_THIS_SCRIPT, 'third_party', 'ycmd', 'build.py' )
-    
-    
-class DebugInfoRequest( BaseRequest ):
-  def __init__( self, extra_data = None ):
-    super( DebugInfoRequest, self ).__init__()
-    self._extra_data = extra_data
-    self._response = None
-    
-    from ycm.client.base_request import BaseRequest, BuildRequestData
-    
-    
-def SendShutdownRequest():
-  request = ShutdownRequest()
-  # This is a blocking call.
-  request.Start()
-
-    
-    
-class DiagnosticFilter( object ):
-  def __init__( self, config_or_filters ):
-    if isinstance( config_or_filters, list ):
-      self._filters = config_or_filters
-    
-    
-def _GetAllDescendentats( root_group ):
-  descendants = []
-  for child in root_group.children:
-    descendants.append( child )
-    descendants.extend( _GetAllDescendentats( child ) )
-  return descendants
-    
-    
-  def Extra_Info_No_Doc_String_test( self ):
-    self._Check( 0, {
-      'insertion_text':  'INSERTION TEXT',
-      'menu_text':       'MENU TEXT',
-      'extra_menu_info': 'EXTRA MENU INFO',
-      'kind':            'K',
-      'extra_data': {
-      },
-    }, {
-      'word'     : 'INSERTION TEXT',
-      'abbr'     : 'MENU TEXT',
-      'menu'     : 'EXTRA MENU INFO',
-      'kind'     : 'k',
-      'dup'      : 1,
-      'empty'    : 1,
-      'user_data': '0',
-    } )
-    
-    
-@YouCompleteMeInstance()
-def SendCompletionRequest_UnicodeWorkingDirectory_test( ycm ):
-  unicode_dir = PathToTestFile( 'uni¢𐍈d€' )
-  current_buffer = VimBuffer( PathToTestFile( 'uni¢𐍈d€', 'current_buffer' ) )
-    
-        try:
-      result = self.fn( *self.args, **self.kwargs )
-    except BaseException:
-      e = sys.exc_info()[ 1 ]
-      self.future.set_exception( e )
-    else:
-      self.future.set_result( result )
+        def download_request(self, request, spider):
+        scheme = urlparse_cached(request).scheme
+        handler = self._get_handler(scheme)
+        if not handler:
+            raise NotSupported('Unsupported URL scheme '%s': %s' %
+                               (scheme, self._notconfigured[scheme]))
+        return handler.download_request(request, spider)
