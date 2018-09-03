9299c8407fc0e2ce97c8d2729792d3d5ff802f35
SUNsung <weatherius@gmail.com>
2018-09-03 06:05:11 +0300

    Rewrite [27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp] File in: 2018-09-03 06:05:11

--- cccccc/27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp
+++ cccccc/27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp
@@ -1,462 +1,173 @@
 
         
-        void SILLayout::Profile(llvm::FoldingSetNodeID &id,
-                        CanGenericSignature Generics,
-                        ArrayRef<SILField> Fields) {
-  id.AddPointer(Generics.getPointer());
-  for (auto &field : Fields) {
-    id.AddPointer(field.getLoweredType().getPointer());
-    id.AddBoolean(field.isMutable());
-  }
+        Optional<PlatformKind> swift::platformFromString(StringRef Name) {
+  if (Name == '*')
+    return PlatformKind::none;
+  return llvm::StringSwitch<Optional<PlatformKind>>(Name)
+#define AVAILABILITY_PLATFORM(X, PrettyName) .Case(#X, PlatformKind::X)
+#include 'swift/AST/PlatformKinds.def'
+      .Case('macOS', PlatformKind::OSX)
+      .Case('macOSApplicationExtension', PlatformKind::OSXApplicationExtension)
+      .Default(Optional<PlatformKind>());
 }
+    
+    bool CacheImpl::getAndRetain(const void *Key, void **Value_out) {
+  int Ret = cache_get_and_retain(static_cast<cache_t*>(Impl),
+                                 const_cast<void*>(Key), Value_out);
+  return Ret == 0;
+}
+    
+      std::size_t capacity = End - Allocated;
+  std::size_t requiredCapacity = capacity + needed;
+  do {
+    capacity = 2 * capacity + 16;
+  } while (capacity < requiredCapacity);
+    
+    std::pair<bool, bool> LangOptions::setTarget(llvm::Triple triple) {
+  clearAllPlatformConditionValues();
+    }
+    
+    // Ternary predicate assertion macros.
+#define EXPECT_PRED_FORMAT3(pred_format, v1, v2, v3) \
+  GTEST_PRED_FORMAT3_(pred_format, v1, v2, v3, GTEST_NONFATAL_FAILURE_)
+#define EXPECT_PRED3(pred, v1, v2, v3) \
+  GTEST_PRED3_(pred, v1, v2, v3, GTEST_NONFATAL_FAILURE_)
+#define ASSERT_PRED_FORMAT3(pred_format, v1, v2, v3) \
+  GTEST_PRED_FORMAT3_(pred_format, v1, v2, v3, GTEST_FATAL_FAILURE_)
+#define ASSERT_PRED3(pred, v1, v2, v3) \
+  GTEST_PRED3_(pred, v1, v2, v3, GTEST_FATAL_FAILURE_)
+    
+    #if GTEST_HAS_DEATH_TEST
+    
+      template <typename T>
+  operator ParamGenerator<T>() const {
+    const T array[] = {static_cast<T>(v1_), static_cast<T>(v2_),
+        static_cast<T>(v3_), static_cast<T>(v4_), static_cast<T>(v5_),
+        static_cast<T>(v6_), static_cast<T>(v7_), static_cast<T>(v8_),
+        static_cast<T>(v9_), static_cast<T>(v10_), static_cast<T>(v11_),
+        static_cast<T>(v12_), static_cast<T>(v13_), static_cast<T>(v14_),
+        static_cast<T>(v15_), static_cast<T>(v16_), static_cast<T>(v17_),
+        static_cast<T>(v18_), static_cast<T>(v19_), static_cast<T>(v20_),
+        static_cast<T>(v21_), static_cast<T>(v22_), static_cast<T>(v23_)};
+    return ValuesIn(array);
+  }
+    
+    #ifndef GTEST_INCLUDE_GTEST_INTERNAL_GTEST_STRING_H_
+#define GTEST_INCLUDE_GTEST_INTERNAL_GTEST_STRING_H_
+    
+      GTEST_DECLARE_TUPLE_AS_FRIEND_
+    
+    // Tests the Set method.
+TEST(MyString, Set) {
+  MyString s;
+    }
+    
+    #include <folly/test/DeterministicSchedule.h>
+    
+    
+    {  dynamic d4 = dynamic::object('C', 'C++');
+  auto i4 = convertTo<std::pair<std::string, folly::fbstring>>(d4);
+  EXPECT_EQ(i4.first, 'C');
+  EXPECT_EQ(i4.second, 'C++');
+}
+    
+    // this test makes sure that the coroutine is destroyed properly
+TEST(Expected, CoroutineCleanedUp) {
+  int count_dest = 0;
+  auto r = [&]() -> Expected<int, Err> {
+    SCOPE_EXIT {
+      ++count_dest;
+    };
+    auto x = co_await Expected<int, Err>(makeUnexpected(Err::badder()));
+    ADD_FAILURE() << 'Should not be resuming';
+    co_return x;
+  }();
+  EXPECT_FALSE(r.hasValue());
+  EXPECT_EQ(1, count_dest);
+}
+    
+    #include <boost/random.hpp>
+    
+    void BENCHFUN(pushBack)(int iters, int initialSize) {
+  BenchmarkSuspender braces;
+  auto const obj = randomObject<VECTOR::value_type>();
+  VECTOR v(initialSize, obj);
+  braces.dismissing([&]() {
+    FOR_EACH_RANGE (i, 0, iters) { v.push_back(obj); }
+  });
+}
+    
+      {
+    Lock lock(f, std::defer_lock);
+    EXPECT_TRUE(lock.try_lock());
+  }
+    
+    struct Const {
+  void test() const;
+};
+    
+    TEST(CanClientFactoryTest, CreateCanClient) {
+  auto *can_factory = CanClientFactory::instance();
+  EXPECT_TRUE(can_factory != nullptr);
+    }
+    
+    /**
+ * @file
+ * @brief Defines the EsdCanClient class which inherits CanClient.
+ */
+    
+    #include <cstring>
+    
+      if (_card_port > MAX_CAN_PORT || _card_port < 0) {
+    AERROR << 'can port number [' << _card_port << '] is out of the range [0,'
+           << MAX_CAN_PORT << ']';
+    return ErrorCode::CAN_CLIENT_ERROR_BASE;
+  }
+  // open device
+  int32_t ret = bcan_open(_card_port, 0,
+                          5,  // 5ms for rx timeout
+                          5,  // 5ms for tx timeout
+                          &_dev_handler);
+    
+    
+    {
+    {
+    {
+    {}  // namespace can
+}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
 
     
-      TypeWalker &Walker;
-    
-    
-    {
-    {        // Register the memory buffer.
-        SML->registerMemoryBuffer(info.name, std::move(bitstream));
-        foundModules.push_back(info.name);
-      }
-    } else {
-      llvm::dbgs() << 'Unable to load module';
-      if (!info.name.empty())
-        llvm::dbgs() << ' '' << info.name << '\'';
-      llvm::dbgs() << '.\n';
-    }
-    
-    #ifndef PREPOSITION
-#  define PREPOSITION(Word)
-#endif
-    
-    
-    {      if (shortenedNameWord != newShortenedNameWord) {
-        matched();
-        unsigned targetSize = newShortenedNameWord.size();
-        while (nameWordRevIter.base().getPosition() > targetSize)
-          ++nameWordRevIter;
-        continue;
-      }
-    }
-    
-    
-    {    if (Finished) {
-      std::string Output = 'Output placeholder\n';
-      std::string Errors =
-          P.second->SeparateErrors ? 'Error placeholder\n' : '';
-      if (Finished(P.first, 0, Output, Errors, TaskProcessInformation(Pid),
-                   P.second->Context) == TaskFinishedResponse::StopExecution)
-        SubtaskFailed = true;
-    }
-  }
-    
-    /// The list of known CF types.  We use 'constexpr' to verify that this is
-/// emitted as a constant.  Note that this is expected to be sorted in
-/// quasi-lexicographic order.
-static constexpr const llvm::StringLiteral KnownCFTypes[] = {
-#define CF_TYPE(NAME) #NAME,
-#define NON_CF_TYPE(NAME)
-#include 'SortedCFDatabase.def'
-};
-const size_t NumKnownCFTypes = sizeof(KnownCFTypes) / sizeof(*KnownCFTypes);
-    
-        typedef ptrdiff_t  stride_t;
-    
-    #ifdef CAROTENE_NEON
-    // in this case we can use the following scheme:
-    // dst[p] = (src[p] + dst[p]) >> 1
-    // which is faster
-    if (alpha == 0.5f)
-    {
-        internal::vtransform(size,
-                             srcBase, srcStride,
-                             dstBase, dstStride,
-                             dstBase, dstStride,
-                             AccumulateWeightedHalf());
-    }
-    
-                    uint8x8x4_t vRes;
-                vRes.val[0] = vrshrn_n_u16(vSum_0_4, 8);
-                vRes.val[1] = vrshrn_n_u16(vSum_1_5, 8);
-                vRes.val[2] = vrshrn_n_u16(vSum_2_6, 8);
-                vRes.val[3] = vrshrn_n_u16(vSum_3_7, 8);
-    
-    inline void vnst(u8* dst, uint8x16_t v1, uint8x16_t v2) { vst1q_u8(dst, v1); vst1q_u8(dst+16, v2); }
-inline void vnst(u8* dst, uint16x8_t v1, uint16x8_t v2) { vst1q_u8(dst, vcombine_u8(vmovn_u16(v1), vmovn_u16(v2))); }
-inline void vnst(u8* dst, uint32x4_t v1, uint32x4_t v2) { vst1_u8(dst, vmovn_u16(vcombine_u16(vmovn_u32(v1), vmovn_u32(v2)))); }
-    
-    #ifndef GRPC_INTERNAL_CPP_CLIENT_CREATE_CHANNEL_INTERNAL_H
-#define GRPC_INTERNAL_CPP_CLIENT_CREATE_CHANNEL_INTERNAL_H
-    
-    const AuthProperty AuthPropertyIterator::operator*() {
-  return std::pair<grpc::string_ref, grpc::string_ref>(
-      property_->name,
-      grpc::string_ref(property_->value, property_->value_length));
-}
-    
-    #include <grpc/impl/codegen/grpc_types.h>
-#include <grpc/support/log.h>
-#include <grpcpp/grpcpp.h>
-#include <grpcpp/resource_quota.h>
-#include 'src/core/lib/channel/channel_args.h'
-#include 'src/core/lib/iomgr/exec_ctx.h'
-#include 'src/core/lib/iomgr/socket_mutator.h'
-    
-    #include <grpc/grpc.h>
-#include <grpc/support/log.h>
-#include <grpcpp/impl/grpc_library.h>
-#include <grpcpp/support/time.h>
-    
-    AuthPropertyIterator SecureAuthContext::begin() const {
-  if (ctx_) {
-    grpc_auth_property_iterator iter =
-        grpc_auth_context_property_iterator(ctx_);
-    const grpc_auth_property* property =
-        grpc_auth_property_iterator_next(&iter);
-    return AuthPropertyIterator(property, &iter);
-  } else {
-    return end();
-  }
-}
-    
-    // Deserialize the incoming SpanContext and generate a new server context based
-// on that. This new span will never be a root span. This should only be called
-// with a blank CensusContext as it overwrites it.
-void GenerateServerContext(absl::string_view tracing, absl::string_view stats,
-                           absl::string_view primary_role,
-                           absl::string_view method, CensusContext* context);
-    
-    
-    {	dealloc_ptr += (size >> 1) + sizeof(uint32_t);
-	return true;
-}
-    
-    	Map<String, String> locale_name_map;
-    
-    
-    {					r_err_str = 'Expected string or integer for keycode';
-					return ERR_PARSE_ERROR;
-				}
-    
-    #include 'rasterizer_storage_gles3.h'
-#include 'servers/visual/rasterizer.h'
-#include 'shaders/canvas_shadow.glsl.gen.h'
-    
-    ThreadPosix::ThreadPosix() {
-    }
-    
-    
-    {	return name;
-}
-    
-    
-    {	ReleaseSRWLockExclusive(&lock);
-}
-    
-    class RWLockWindows : public RWLock {
-    }
-    
-    	ThreadWindows *tr = memnew(ThreadWindows);
-	tr->callback = p_callback;
-	tr->user = p_user;
-	tr->handle = CreateThread(
-			NULL, // default security attributes
-			0, // use default stack size
-			thread_callback, // thread function name
-			tr, // argument to thread function
-			0, // use default creation flags
-			NULL); // returns the thread identifier
-    
-    size_t num_threads = 31;
-size_t work_chunk  = 120;
-    
-    using Peephole = ConcatPeephole;
-    
-    namespace HPHP { namespace HHBBC {
-    }
-    }
-    
-        if (!slash && !semi) {
-      raise_warning('rfc2397: invalid meta data');
-      return nullptr;
-    }
-    
-          if (pos_period != std::string::npos &&
-          pos_equals != std::string::npos &&
-          pos_period < pos_equals) {
-        section = line.substr(0, pos_period);
-      }
-    
-    #define ERROR_RAISE_WARNING(exp)        \
-  int ret = (exp);                      \
-  if (ret != 0) {                       \
-    raise_warning(                      \
-      '%s(): %s',                       \
-      __FUNCTION__,                     \
-      folly::errnoStr(errno).c_str()    \
-    );                                  \
-  }                                     \
-    
-    
-    {    if (isPHP == php) {
-      callback(spath + ename, false);
-    }
-  }
-    
-    
-    {private:
-   // this type is not copyable:
-   c_regex_traits(const c_regex_traits&);
-   c_regex_traits& operator=(const c_regex_traits&);
-};
-    
     
     {
     {
-} // namespace BOOST_REGEX_DETAIL_NS
-using boost::BOOST_REGEX_DETAIL_NS::directory_iterator;
-using boost::BOOST_REGEX_DETAIL_NS::file_iterator;
-using boost::BOOST_REGEX_DETAIL_NS::mapfile;
-} // namespace boost
+    {}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+
     
-    
-//
-// Unfortunately Rogue Waves standard library appears to have a bug
-// in std::basic_string::compare that results in eroneous answers
-// in some cases (tested with Borland C++ 5.1, Rogue Wave lib version
-// 0x020101) the test case was:
-// {39135,0} < {0xff,0}
-// which succeeds when it should not.
-//
-#ifndef _RWSTD_VER
-template <class C, class T, class A>
-inline int string_compare(const std::basic_string<C,T,A>& s, const C* p)
-{ 
-   if(0 == *p)
-   {
-      if(s.empty() || ((s.size() == 1) && (s[0] == 0)))
-         return 0;
-   }
-   return s.compare(p); 
-}
-#else
-template <class C, class T, class A>
-inline int string_compare(const std::basic_string<C,T,A>& s, const C* p)
-{ 
-   if(0 == *p)
-   {
-      if(s.empty() || ((s.size() == 1) && (s[0] == 0)))
-         return 0;
-   }
-   return s.compare(p); 
-}
-inline int string_compare(const std::string& s, const char* p)
-{ return std::strcmp(s.c_str(), p); }
-# ifndef BOOST_NO_WREGEX
-inline int string_compare(const std::wstring& s, const wchar_t* p)
-{ return std::wcscmp(s.c_str(), p); }
-#endif
-#endif
-template <class Seq, class C>
-inline int string_compare(const Seq& s, const C* p)
-{
-   std::size_t i = 0;
-   while((i < s.size()) && (p[i] == s[i]))
-   {
-      ++i;
-   }
-   return (i == s.size()) ? -p[i] : s[i] - p[i];
-}
-# define STR_COMP(s,p) string_compare(s,p)
-    
-    
-    {
-    {
-   } // namespace BOOST_REGEX_DETAIL_NS
-} // namespace boost
-    
-     /*
-  *   LOCATION:    see http://www.boost.org for most recent version.
-  *   FILE         regex_format.hpp
-  *   VERSION      see <boost/version.hpp>
-  *   DESCRIPTION: Provides formatting output routines for search and replace
-  *                operations.  Note this is an internal header file included
-  *                by regex.hpp, do not include on its own.
-  */
-    
-    template <class OutputIterator, class BidirectionalIterator, class traits, class charT, class Formatter>
-OutputIterator regex_replace(OutputIterator out,
-                         BidirectionalIterator first,
-                         BidirectionalIterator last,
-                         const basic_regex<charT, traits>& e, 
-                         Formatter fmt, 
-                         match_flag_type flags = match_default)
-{
-   regex_iterator<BidirectionalIterator, charT, traits> i(first, last, e, flags);
-   regex_iterator<BidirectionalIterator, charT, traits> j;
-   if(i == j)
-   {
-      if(!(flags & regex_constants::format_no_copy))
-         out = BOOST_REGEX_DETAIL_NS::copy(first, last, out);
-   }
-   else
-   {
-      BidirectionalIterator last_m(first);
-      while(i != j)
-      {
-         if(!(flags & regex_constants::format_no_copy))
-            out = BOOST_REGEX_DETAIL_NS::copy(i->prefix().first, i->prefix().second, out); 
-         out = i->format(out, fmt, flags, e);
-         last_m = (*i)[0].second;
-         if(flags & regex_constants::format_first_only)
-            break;
-         ++i;
-      }
-      if(!(flags & regex_constants::format_no_copy))
-         out = BOOST_REGEX_DETAIL_NS::copy(last_m, last, out);
-   }
-   return out;
-}
-    
-    #ifdef BOOST_MSVC
-#pragma warning(push)
-#pragma warning(disable: 4103)
-#endif
-#ifdef BOOST_HAS_ABI_HEADERS
-#  include BOOST_ABI_SUFFIX
-#endif
-#ifdef BOOST_MSVC
-#pragma warning(pop)
-#endif
-    
-    // https://codingforspeed.com/using-faster-exponential-approximation/
-inline Float8 Exp4096(Float8 x) {
-  x *= Float8(1.0f / 4096.0f);
-  x += Float8(1.0f);
-  x *= x;
-  x *= x;
-  x *= x;
-  x *= x;
-  x *= x;
-  x *= x;
-  x *= x;
-  x *= x;
-  x *= x;
-  x *= x;
-  x *= x;
-  x *= x;
-  return x;
-}
-    
-      inline void ParseStr(std::string *tok) {
-    while ((ch_buf_ = this->GetChar()) != EOF) {
-      switch (ch_buf_) {
-        case '\\': *tok += this->GetChar(); break;
-        case '\'': return;
-        case '\r':
-        case '\n': LOG(FATAL)<< 'ConfigReader: unterminated string';
-        default: *tok += ch_buf_;
-      }
+    namespace apollo {
+namespace drivers {
+namespace canbus {
     }
-    LOG(FATAL) << 'ConfigReader: unterminated string';
-  }
-  inline void ParseStrML(std::string *tok) {
-    while ((ch_buf_ = this->GetChar()) != EOF) {
-      switch (ch_buf_) {
-        case '\\': *tok += this->GetChar(); break;
-        case '\'': return;
-        default: *tok += ch_buf_;
-      }
     }
-    LOG(FATAL) << 'unterminated string';
-  }
-  // return newline
-  inline bool GetNextToken(std::string *tok) {
-    tok->clear();
-    bool new_line = false;
-    while (ch_buf_ != EOF) {
-      switch (ch_buf_) {
-        case '#' : SkipLine(); new_line = true; break;
-        case '\'':
-          if (tok->length() == 0) {
-            ParseStr(tok); ch_buf_ = this->GetChar(); return new_line;
-          } else {
-            LOG(FATAL) << 'ConfigReader: token followed directly by string';
-          }
-        case '\'':
-          if (tok->length() == 0) {
-            ParseStrML(tok); ch_buf_ = this->GetChar(); return new_line;
-          } else {
-            LOG(FATAL) << 'ConfigReader: token followed directly by string';
-          }
-        case '=':
-          if (tok->length() == 0) {
-            ch_buf_ = this->GetChar();
-            *tok = '=';
-          }
-          return new_line;
-        case '\r':
-        case '\n':
-          if (tok->length() == 0) new_line = true;
-        case '\t':
-        case ' ' :
-          ch_buf_ = this->GetChar();
-          if (tok->length() != 0) return new_line;
-          break;
-        default:
-          *tok += ch_buf_;
-          ch_buf_ = this->GetChar();
-          break;
-      }
     }
-    if (tok->length() == 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-};
-/*!
- * \brief an iterator use stream base, allows use all types of istream
- */
-class ConfigStreamReader: public ConfigReaderBase {
- public:
-  /*!
-   * \brief constructor
-   * \param fin istream input stream
+    
+    const char HEX[] = '0123456789ABCDEF';
+    
+      /**
+   * @brief Set the bit on a specified position to zero.
+   * @param pos The position of the bit to be set to zero.
    */
-  explicit ConfigStreamReader(std::istream &fin) : fin_(fin) {}
+  void set_bit_0(const int32_t pos);
     
-        struct sigaction sig_action;
-    sig_action.sa_handler = SIG_DFL;
-    sig_action.sa_flags = 0;
-    sigemptyset(&sig_action.sa_mask);
+     private:
+  void PublishSensorData();
+  void OnTimer(const ros::TimerEvent &event);
+  void DataTrigger();
+  common::Status OnError(const std::string &error_msg);
+  void RegisterCanClients();
     
-        // Call the diffing utility directly.
-    DiffResults expected = diff(previous_qd, result.second);
-    EXPECT_EQ(dr, expected);
-    
-      /// Paths to autoload extensions.
-  std::vector<std::string> extensions_paths_;
-    
-    void Initializer::platformSetup() {
-  // Initialize the COM libraries utilized by Windows WMI calls.
-  auto ret = ::CoInitializeEx(0, COINIT_MULTITHREADED);
-  if (ret != S_OK) {
-    ::CoUninitialize();
-  }
-}
-    
-        if (queries.IsObject()) {
-      for (const auto& query_entry : queries.GetObject()) {
-        if (!query_entry.name.IsString() || !query_entry.value.IsString()) {
-          return Status(1, 'Distributed discovery query is not a string');
-        }
-    }
-    }
-    
-      // Don't overload the normal `init` Subscription member.
-  sub->lateInit();
-  pub->configure();
-  EXPECT_EQ(pub->numSubscriptions(), 1U);
+    // System gflags
+DECLARE_string(sensor_node_name);
