167d0a2f6a08a8472f0b2b96bd7828b2b6b14a88
SUNsung <weatherius@gmail.com>
2018-09-03 16:48:18 +0300

    Rewrite [28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb] File in: 2018-09-03 16:48:17

--- ruby/28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb
+++ ruby/28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb
@@ -1,146 +1,131 @@
 
         
-                  private
-            def ensure_listener_running
-              @thread ||= Thread.new do
-                Thread.current.abort_on_exception = true
+              # Creates a file upload field. If you are using file uploads then you will also need
+      # to set the multipart option for the form tag:
+      #
+      #   <%= form_tag '/upload', multipart: true do %>
+      #     <label for='file'>File to Upload</label> <%= file_field_tag 'file' %>
+      #     <%= submit_tag %>
+      #   <% end %>
+      #
+      # The specified URL will then be passed a File object containing the selected file, or if the field
+      # was left blank, a StringIO object.
+      #
+      # ==== Options
+      # * Creates standard HTML attributes for the tag.
+      # * <tt>:disabled</tt> - If set to true, the user will not be able to use this input.
+      # * <tt>:multiple</tt> - If set to true, *in most updated browsers* the user will be allowed to select multiple files.
+      # * <tt>:accept</tt> - If set to one or multiple mime-types, the user will be suggested a filter when choosing a file. You still need to set up model validations.
+      #
+      # ==== Examples
+      #   file_field_tag 'attachment'
+      #   # => <input id='attachment' name='attachment' type='file' />
+      #
+      #   file_field_tag 'avatar', class: 'profile_input'
+      #   # => <input class='profile_input' id='avatar' name='avatar' type='file' />
+      #
+      #   file_field_tag 'picture', disabled: true
+      #   # => <input disabled='disabled' id='picture' name='picture' type='file' />
+      #
+      #   file_field_tag 'resume', value: '~/resume.doc'
+      #   # => <input id='resume' name='resume' type='file' value='~/resume.doc' />
+      #
+      #   file_field_tag 'user_pic', accept: 'image/png,image/gif,image/jpeg'
+      #   # => <input accept='image/png,image/gif,image/jpeg' id='user_pic' name='user_pic' type='file' />
+      #
+      #   file_field_tag 'file', accept: 'text/html', class: 'upload', value: 'index.html'
+      #   # => <input accept='text/html' class='upload' id='file' name='file' type='file' value='index.html' />
+      def file_field_tag(name, options = {})
+        text_field_tag(name, nil, convert_direct_upload_option_to_url(options.merge(type: :file)))
+      end
     
-      test 'invalid action on Channel' do
-    assert_logged('Unable to process ActionCable::Channel::BaseTest::ChatChannel#invalid_action') do
-      @channel.perform_action 'action' => :invalid_action
+    task :spec => :test
+require 'rake/testtask'
+Rake::TestTask.new(:test) do |test|
+  test.libs << 'lib' << 'test'
+  test.pattern = 'test/**/test_*.rb'
+  test.verbose = true
+end
+    
+    Benchmark.ips do |x|
+  x.report('no body include?') { CONTENT_NOT_CONTAINING.include?('<body') }
+  x.report('no body regexp')   { CONTENT_NOT_CONTAINING =~ /<\s*body/ }
+  x.compare!
+end
+    
+    def run_bundle(args)
+  run_in_shell('bundle', *args.strip.split(' '))
+end
+    
+        # The path used after sending reset password instructions
+    def after_sending_reset_password_instructions_path_for(resource_name)
+      new_session_path(resource_name) if is_navigational_format?
     end
-  end
     
-        assert subscriptions.verify
+      # POST /resource/unlock
+  def create
+    self.resource = resource_class.send_unlock_instructions(resource_params)
+    yield resource if block_given?
+    
+        def password_change(record, opts={})
+      devise_mail(record, :password_change, opts)
+    end
   end
 end
 
     
-    require 'test_helper'
-require 'minitest/mock'
-require 'stubs/test_connection'
-require 'stubs/room'
+          def stored_location_key_for(resource_or_scope)
+        scope = Devise::Mapping.find_scope!(resource_or_scope)
+        '#{scope}_return_to'
+      end
     
-            stdlibs = detect_stdlibs(ENV.compiler)
-        Tab.create(formula, ENV.compiler, stdlibs.first, formula.build, formula.source_modified_time).write
-    
-    module Homebrew
-  def build_env_keys(env)
-    %w[
-      CC CXX LD OBJC OBJCXX
-      HOMEBREW_CC HOMEBREW_CXX
-      CFLAGS CXXFLAGS CPPFLAGS LDFLAGS SDKROOT MAKEFLAGS
-      CMAKE_PREFIX_PATH CMAKE_INCLUDE_PATH CMAKE_LIBRARY_PATH CMAKE_FRAMEWORK_PATH
-      MACOSX_DEPLOYMENT_TARGET PKG_CONFIG_PATH PKG_CONFIG_LIBDIR
-      HOMEBREW_DEBUG HOMEBREW_MAKE_JOBS HOMEBREW_VERBOSE
-      HOMEBREW_SVN HOMEBREW_GIT
-      HOMEBREW_SDKROOT HOMEBREW_BUILD_FROM_SOURCE
-      MAKE GIT CPP
-      ACLOCAL_PATH PATH CPATH].select { |key| env.key?(key) }
-  end
-    
-      private
-    
-      # Removes any empty directories in the formula's prefix subtree
-  # Keeps any empty directions projected by skip_clean
-  # Removes any unresolved symlinks
-  def prune
-    dirs = []
-    symlinks = []
-    @f.prefix.find do |path|
-      if path == @f.libexec || @f.skip_clean?(path)
-        Find.prune
-      elsif path.symlink?
-        symlinks << path
-      elsif path.directory?
-        dirs << path
+          unless root?
+        raise Invalid, 'missing name' if !name || name.empty?
+        raise Invalid, 'missing path' if !path || path.empty?
+        raise Invalid, 'missing type' if !type || type.empty?
       end
     end
     
-      def self.canonical_name(name)
-    Formulary.canonical_name(name)
+        def parse_as_fragment
+      Nokogiri::HTML.fragment @content, 'UTF-8'
+    end
   end
+end
+
     
-                o.on('-f', '--force', 'Remove without confirmation.') do |f|
-              options[:force] = f
-            end
-    
-              # Repackage the box
-          output_name = @env.vagrantfile.config.package.name || 'package.box'
-          output_path = Pathname.new(File.expand_path(output_name, FileUtils.pwd))
-          box.repackage(output_path)
-    
-          def execute
-        options = {}
-        options[:check] = false
-    
-            def reference!(node)
-          @references << node
-          @referenced = true
-        end
-    
-            def on_percent_literal(node)
-          each_unnecessary_space_match(node) do |range|
-            add_offense(node, location: range)
-          end
-        end
-    
-            def_node_matcher :only_truthiness_matters?, <<-PATTERN
-          ^({if while until case while_post until_post} equal?(%0) ...)
-        PATTERN
-    
-    module RuboCop
-  module Cop
-    module Lint
-      # In math and Python, we can use `x < y < z` style comparison to compare
-      # multiple value. However, we can't use the comparison in Ruby. However,
-      # the comparison is not syntax error. This cop checks the bad usage of
-      # comparison operators.
-      #
-      # @example
-      #
-      #   # bad
-      #
-      #   x < y < z
-      #   10 <= x <= 20
-      #
-      # @example
-      #
-      #   # good
-      #
-      #   x < y && y < z
-      #   10 <= x && x <= 20
-      class MultipleCompare < Cop
-        MSG = 'Use the `&&` operator to compare multiple values.'.freeze
-    
-          # Chacks whether the `if` node has nested `if` nodes in any of its
-      # branches.
-      #
-      # @note This performs a shallow search.
-      #
-      # @return [Boolean] whether the `if` node contains nested conditionals
-      def nested_conditional?
-        node_parts[1..2].compact.each do |branch|
-          branch.each_node(:if) do |nested|
-            return true unless nested.elsif?
-          end
-        end
-    
-              def plugins
-            @plugins ||= find_plugins_gem_specs.map do |spec|
-              { :name => spec.name, :version => spec.version.to_s }
-            end.sort_by do |spec|
-              spec[:name]
-            end
-          end
-    
-            #target = $LOADED_FEATURES.grep(/#{path}/).first
-        #puts path
-        #puts caller.map { |c| '  #{c}' }.join('\n')
-        #fontsize = [10, duration * 48].max
-        puts '#{duration},#{path},#{source}'
-      end
-      #puts caller.map { |c| ' => #{c}' }.join('\n')
+        def error?
+      code == 0 || code != 404 && code != 403 && code >= 400 && code <= 599
     end
     
-      def process_downloads(files,target='')
+    module Docs
+  class Scraper < Doc
+    class << self
+      attr_accessor :base_url, :root_path, :initial_paths, :options, :html_filters, :text_filters, :stubs
+    
+            css('.api-profile-header-structure > li').each do |node|
+          node.inner_html = node.inner_html.remove('- ')
+        end
+    
+        def get_cache_file_for(gist, file)
+      bad_chars = /[^a-zA-Z0-9\-_.]/
+      gist      = gist.gsub bad_chars, ''
+      file      = file.gsub bad_chars, ''
+      md5       = Digest::MD5.hexdigest '#{gist}-#{file}'
+      File.join @cache_folder, '#{gist}-#{file}-#{md5}.cache'
+    end
+    
+        def initialize(tag_name, markup, tokens)
+      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
+      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
+      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
+      super
+    end
+    
+    class LogStash::PluginManager::Unpack < LogStash::PluginManager::PackCommand
+  option '--tgz', :flag, 'unpack a packaged tar.gz file', :default => !LogStash::Environment.windows?
+  option '--zip', :flag, 'unpack a packaged  zip file', :default => LogStash::Environment.windows?
+    
+    shared_examples 'logstash install' do |logstash|
+  before(:each) do
+    logstash.install({:version => LOGSTASH_VERSION})
+  end
