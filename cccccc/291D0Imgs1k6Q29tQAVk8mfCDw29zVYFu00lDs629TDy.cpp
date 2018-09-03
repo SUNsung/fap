4339e9f5033d51e19b4e5881c4de61815c10e4b2
SUNsung <weatherius@gmail.com>
2018-09-04 01:20:29 +0300

    Rewrite [291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp] File in: 2018-09-04 01:20:29

--- cccccc/291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp
+++ cccccc/291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp
@@ -1,273 +1,295 @@
 
         
-        #include <grpc/grpc_cronet.h>
-#include <grpcpp/channel.h>
-#include <grpcpp/support/channel_arguments.h>
-#include 'src/cpp/client/create_channel_internal.h'
-    
-    #include <grpcpp/support/channel_arguments.h>
-    
-    #ifndef GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CONTEXT_H
-#define GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CONTEXT_H
-    
-    
+            for (size_t i = 0; i < size.height; ++i)
     {
-    {			if (szArgList[i + 1] != NULL && szArgList[i + 1][0] != '/') {
-				cmderOptions.cmderCfgRoot = szArgList[i + 1];
-				i++;
-			}
-		}
-		else if (_wcsicmp(L'/start', szArgList[i]) == 0)
-		{
-			if (PathFileExists(szArgList[i + 1]))
-			{
-				cmderOptions.cmderStart = szArgList[i + 1];
-				i++;
-			}
-			else {
-				MessageBox(NULL, szArgList[i + 1], L'/START - Folder doses not exist!', MB_OK);
-			}
-		}
-		else if (_wcsicmp(L'/task', szArgList[i]) == 0)
-		{
-			cmderOptions.cmderTask = szArgList[i + 1];
-			i++;
-		}
-		else if (_wcsicmp(L'/single', szArgList[i]) == 0)
-		{
-			cmderOptions.cmderSingle = true;
-		}
-		else if (_wcsicmp(L'/register', szArgList[i]) == 0)
-		{
-			cmderOptions.registerApp = true;
-			cmderOptions.unRegisterApp = false;
-			if (szArgList[i + 1] != NULL)
-			{
-				if (_wcsicmp(L'all', szArgList[i + 1]) == 0 || _wcsicmp(L'user', szArgList[i + 1]) == 0)
-				{
-					cmderOptions.cmderRegScope = szArgList[i + 1];
-					i++;
-				}
-			}
-		}
-		else if (_wcsicmp(L'/unregister', szArgList[i]) == 0)
-		{
-			cmderOptions.unRegisterApp = true;
-			cmderOptions.registerApp = false;
-			if (szArgList[i + 1] != NULL)
-			{
-				if (_wcsicmp(L'all', szArgList[i + 1]) == 0 || _wcsicmp(L'user', szArgList[i + 1]) == 0)
-				{
-					cmderOptions.cmderRegScope = szArgList[i + 1];
-					i++;
-				}
-			}
-		}
-		else if (cmderOptions.cmderStart == L'' && PathFileExists(szArgList[i]))
-		{
-			cmderOptions.cmderStart = szArgList[i];
-		}
-		else {
-			MessageBox(NULL, L'Unrecognized parameter.\n\nValid options:\n\n    /c [CMDER User Root Path]\n\n    /task [ConEmu Task Name]\n\n    [/start [Start in Path] | [Start in Path]]\n\n    /single\n\nor\n\n    /register [USER | ALL]\n\nor\n\n    /unregister [USER | ALL]\n', MB_TITLE, MB_OK);
-			cmderOptions.error = true;
-		}
-	}
-    
-        // 1st element (which should be 0,0) should be here too
-    
-    p = pReverse->getControlPointAtIndex(pReverse->count()-1);
-    pReverse->removeControlPointAtIndex(pReverse->count()-1);
-    
-    p = -p;
-    pReverse->insertControlPoint(p, 0);
-    
-    for (ssize_t i = 1; i < pReverse->count(); ++i)
-    {
-        Vec2 current = pReverse->getControlPointAtIndex(i);
-        current = -current;
-        Vec2 abs = current + p;
-        pReverse->replaceControlPoint(abs, i);
-        
-        p = abs;
+        const u8* src = internal::getRowPtr(srcBase, srcStride, i);
+        s16* dst = internal::getRowPtr(dstBase, dstStride, i);
+        size_t j = 0;
     }
     
-        EaseRateAction *easeRateAction = new (std::nothrow) EaseRateAction();
-    if (easeRateAction && easeRateAction->initWithAction(action, rate))
-    {
-        easeRateAction->autorelease();
-        return easeRateAction;
-    }
-    
-        /** 
-    @brief Initializes an action with duration and grid size.
-    @param gridSize Specify the grid size of the FlipX3D action.
-    @param duration Specify the duration of the FlipX3D action. It's a value in seconds.
-    @return If the initialization success, return true; otherwise, return false.
-    */
-    virtual bool initWithSize(const Size& gridSize, float duration);
-    
-        /** Creates a Place action with a position.
-     *
-     * @param pos  A certain position.
-     * @return  An autoreleased Place object.
-     */
-    static Place * create(const Vec2& pos);
-    
-    
-    {
-    {
-    {            if ((action->getFlags() & flags) != 0 && action->getOriginalTarget() == target)
-            {
-                removeActionAtIndex(i, element);
-                --limit;
-            }
-            else
-            {
-                ++i;
-            }
-        }
-    }
+    #define IMPL_CMPOP(op, type)                              \
+void cmp##op(const Size2D &size,                          \
+             const type * src0Base, ptrdiff_t src0Stride, \
+             const type * src1Base, ptrdiff_t src1Stride, \
+                       u8 *dstBase, ptrdiff_t dstStride)  \
+{                                                         \
+    internal::assertSupportedConfiguration();             \
+    vcompare(size,                                        \
+             src0Base, src0Stride,                        \
+             src1Base, src1Stride,                        \
+             dstBase, dstStride,                          \
+             OpCmp##op<type>());                          \
 }
     
-    /** @class ActionManager
- @brief ActionManager is a singleton that manages all the actions.
- Normally you won't need to use this singleton directly. 99% of the cases you will use the Node interface,
- which uses this singleton.
- But there are some cases where you might need to use this singleton.
- Examples:
-    - When you want to run an action where the target is different from a Node. 
-    - When you want to pause / resume the actions.
- 
- @since v0.8
+    template <typename T>
+inline T *getRowPtr(T *base, ptrdiff_t stride, size_t row)
+{
+    char *baseRaw = const_cast<char *>(reinterpret_cast<const char *>(base));
+    return reinterpret_cast<T *>(baseRaw + ptrdiff_t(row) * stride);
+}
+    
+    template <typename T>
+inline void inRangeCheck(const Size2D &_size,
+                         const T * srcBase, ptrdiff_t srcStride,
+                         const T * rng1Base, ptrdiff_t rng1Stride,
+                         const T * rng2Base, ptrdiff_t rng2Stride,
+                         u8 * dstBase, ptrdiff_t dstStride)
+{
+    typedef typename internal::VecTraits<T>::vec128 vec128;
+    typedef typename internal::VecTraits<T>::unsign::vec128 uvec128;
+    }
+    
+    namespace grpc {
+class Channel;
+    }
+    
+    namespace grpc {
+    }
+    
+    namespace grpc {
+    }
+    
+    namespace grpc {
+    }
+    
+    void CensusClientCallData::StartTransportStreamOpBatch(
+    grpc_call_element* elem, TransportStreamOpBatch* op) {
+  if (op->send_initial_metadata() != nullptr) {
+    census_context* ctxt = op->get_census_context();
+    GenerateClientContext(
+        qualified_method_, &context_,
+        (ctxt == nullptr) ? nullptr : reinterpret_cast<CensusContext*>(ctxt));
+    size_t tracing_len = TraceContextSerialize(context_.Context(), tracing_buf_,
+                                               kMaxTraceContextLen);
+    if (tracing_len > 0) {
+      GRPC_LOG_IF_ERROR(
+          'census grpc_filter',
+          grpc_metadata_batch_add_tail(
+              op->send_initial_metadata()->batch(), &tracing_bin_,
+              grpc_mdelem_from_slices(
+                  GRPC_MDSTR_GRPC_TRACE_BIN,
+                  grpc_slice_from_copied_buffer(tracing_buf_, tracing_len))));
+    }
+    grpc_slice tags = grpc_empty_slice();
+    // TODO: Add in tagging serialization.
+    size_t encoded_tags_len = StatsContextSerialize(kMaxTagsLen, &tags);
+    if (encoded_tags_len > 0) {
+      GRPC_LOG_IF_ERROR(
+          'census grpc_filter',
+          grpc_metadata_batch_add_tail(
+              op->send_initial_metadata()->batch(), &stats_bin_,
+              grpc_mdelem_from_slices(GRPC_MDSTR_GRPC_TAGS_BIN, tags)));
+    }
+  }
+    }
+    
+    inline absl::string_view GetMethod(const grpc_slice* path) {
+  if (GRPC_SLICE_IS_EMPTY(*path)) {
+    return '';
+  }
+  // Check for leading '/' and trim it if present.
+  return absl::StripPrefix(absl::string_view(reinterpret_cast<const char*>(
+                                                 GRPC_SLICE_START_PTR(*path)),
+                                             GRPC_SLICE_LENGTH(*path)),
+                           '/');
+}
+    
+    MeasureDouble RpcServerReceivedBytesPerRpc() {
+  static const auto measure = MeasureDouble::Register(
+      kRpcServerReceivedBytesPerRpcMeasureName,
+      'Total bytes received across all messages per RPC', kUnitBytes);
+  return measure;
+}
+    
+    void StartCmder(std::wstring  path = L'', bool is_single_mode = false, std::wstring taskName = L'', std::wstring cfgRoot = L'')
+{
+#if USE_TASKBAR_API
+	wchar_t appId[MAX_PATH] = { 0 };
+#endif
+	wchar_t exeDir[MAX_PATH] = { 0 };
+	wchar_t icoPath[MAX_PATH] = { 0 };
+	wchar_t cfgPath[MAX_PATH] = { 0 };
+	wchar_t backupCfgPath[MAX_PATH] = { 0 };
+	wchar_t cpuCfgPath[MAX_PATH] = { 0 };
+	wchar_t userCfgPath[MAX_PATH] = { 0 };
+	wchar_t defaultCfgPath[MAX_PATH] = { 0 };
+	wchar_t conEmuPath[MAX_PATH] = { 0 };
+	wchar_t configDirPath[MAX_PATH] = { 0 };
+	wchar_t userConfigDirPath[MAX_PATH] = { 0 };
+	wchar_t userBinDirPath[MAX_PATH] = { 0 };
+	wchar_t userProfiledDirPath[MAX_PATH] = { 0 };
+	wchar_t userProfilePath[MAX_PATH] = { 0 };
+	wchar_t legacyUserProfilePath[MAX_PATH] = { 0 };
+	wchar_t args[MAX_PATH * 2 + 256] = { 0 };
+    }
+    
+    /** @class OrbitCamera
+ *
+ * @brief OrbitCamera action.
+ * Orbits the camera around the center of the screen using spherical coordinates.
+ * @ingroup Actions
  */
-class CC_DLL ActionManager : public Ref
+class CC_DLL OrbitCamera : public ActionCamera
 {
 public:
-    /**
-     * @js ctor
-     */
-    ActionManager();
-    }
-    
-    void ProgressTo::startWithTarget(Node *target)
-{
-    ActionInterval::startWithTarget(target);
-    _from = ((kProgressTimerCast)(target))->getPercentage();
-}
-    
-    The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-    
-    Animation::~Animation(void)
-{
-    CCLOGINFO('deallocing Animation: %p', this);
-}
-    
-        for (const auto& anim : animations)
-    {
-        const ValueMap& animationDict = anim.second.asValueMap();
-        const ValueVector& frameNames = animationDict.at('frames').asValueVector();
-        float delay = animationDict.at('delay').asFloat();
-        Animation* animation = nullptr;
-    }
-    
-        /** Returns a Animation that was previously added.
-     * If the name is not found it will return nil.
-     * You should retain the returned copy if you are going to use it.
+    /** Creates a OrbitCamera action with radius, delta-radius,  z, deltaZ, x, deltaX. 
      *
-     * @return A Animation that was previously added. If the name is not found it will return nil.
+     * @param t Duration in seconds.
+     * @param radius The start radius.
+     * @param deltaRadius The delta radius.
+     * @param angleZ The start angle in Z.
+     * @param deltaAngleZ The delta angle in Z.
+     * @param angleX The start angle in X.
+     * @param deltaAngleX The delta angle in X.
+     * @return An OrbitCamera.
      */
-    Animation* getAnimation(const std::string& name);
-    /**
-     * @deprecated. Use getAnimation() instead
-     * @js NA
-     * @lua NA
+    static OrbitCamera* create(float t, float radius, float deltaRadius, float angleZ, float deltaAngleZ, float angleX, float deltaAngleX);
+    
+    /** Positions the camera according to spherical coordinates. 
+     *
+     * @param r The spherical radius.
+     * @param zenith The spherical zenith.
+     * @param azimuth The spherical azimuth.
      */
-    CC_DEPRECATED_ATTRIBUTE Animation* animationByName(const std::string& name){ return getAnimation(name); }
-    
-    #include <vector>
-    
-      // Returns true if the argument of the last Compare() call (or the baseline
-  // image, if Compare() was not called yet) meets the image acceptance
-  // criteria. The target_mul modifies the acceptance criteria used in this call
-  // the following way:
-  //    = 1.0 : the original acceptance criteria is used,
-  //    < 1.0 : a more strict acceptance criteria is used,
-  //    > 1.0 : a less strict acceptance criteria is used.
-  virtual bool DistanceOK(double target_mul) const = 0;
-    
-    void PrintDebug(ProcessStats* stats, std::string s);
-    
-        size_t i = 0;      // Points to the next leaf node.
-    size_t j = n + 1;  // Points to the next non-leaf node.
-    for (size_t k = n - 1; k != 0; --k) {
-      size_t left, right;
-      if (tree[i].total_count_ <= tree[j].total_count_) {
-        left = i;
-        ++i;
-      } else {
-        left = j;
-        ++j;
-      }
-      if (tree[i].total_count_ <= tree[j].total_count_) {
-        right = i;
-        ++i;
-      } else {
-        right = j;
-        ++j;
-      }
+    void sphericalRadius(float *r, float *zenith, float *azimuth);
     }
     
-      if (fseek(f, 0, SEEK_END) == 0) {
-    buffer_size = std::max<off_t>(ftell(f), 1);
-    if (fseek(f, 0, SEEK_SET) != 0) {
-      perror('fseek');
-      exit(1);
+    ActionEase::~ActionEase(void)
+{
+    CC_SAFE_RELEASE(_inner);
+}
+    
+    protected:
+    unsigned int _jumps;
+    float _amplitude;
+    float _amplitudeRate;
+    
+    bool ActionTween::initWithDuration(float duration, const std::string& key, float from, float to)
+{
+    if (ActionInterval::initWithDuration(duration))
+    {
+        _key    = key;
+        _to       = to;
+        _from     = from;
+        return true;
     }
-  } else if (ferror(f)) {
-    perror('fseek');
-    exit(1);
-  }
-    
-      bool Is420() const;
-  bool Is444() const;
-    
-    // Decodes the parsed jpeg coefficients into an RGB image.
-// There can be only either 1 or 3 image components, in either case, an RGB
-// output image will be generated.
-// Only YUV420 and YUV444 sampling factors are supported.
-// Vector will be empty if a decoding error occurred.
-std::vector<uint8_t> DecodeJpegToRGB(const JPEGData& jpg);
-    
-    void BuildDCHistograms(const JPEGData& jpg, JpegHistogram* histo);
-void BuildACHistograms(const JPEGData& jpg, JpegHistogram* histo);
-size_t JpegHeaderSize(const JPEGData& jpg, bool strip_metadata);
-size_t EstimateJpegDataSize(const int num_components,
-                            const std::vector<JpegHistogram>& histograms);
-    
-    // Synchronous transmission of CAN messages
-ErrorCode EsdCanClient::Send(const std::vector<CanFrame> &frames,
-                             int32_t *const frame_num) {
-  CHECK_NOTNULL(frame_num);
-  CHECK_EQ(frames.size(), static_cast<size_t>(*frame_num));
     }
     
-    namespace apollo {
-namespace drivers {
-namespace canbus {
-namespace can {
-    }
-    }
-    }
-    }
+    
+    {    return true;
+}
     
     
     {
     {
-    {}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-
+    {                // Look for new watch:
+                for (int k = 2; k < c.size(); k++)
+                    if (value(c[k]) != l_False)
+                    {
+                        c[1] = c[k];
+                        c[k] = false_lit;
+                        watches[(~c[1]).x].emplace_back(w);
+                        goto NextClause;
+                    }
+                *j++ = w;
+                if (value(first) == l_False)     // conflict
+                {
+                    confl = cr;
+                    qhead = trail.size();
+                    while (i < end)
+                        *j++ = *i++;
+                }
+                else
+                    uncheckedEnqueue(first, cr);
+                NextClause :;
+            }
+            int size = i - j;
+            ws.erase(ws.end() - size, ws.end());
+        }
+        return confl;
+    }
     
-    const int32_t CANBUS_MESSAGE_LENGTH = 8;  // according to ISO-11891-1
-const int32_t MAX_CAN_PORT = 3;
+    using namespace std;
+    
+    
+    {	img_sine_arr 	= (int *)malloc(img_attrib_sine.width*img_attrib_sine.height*sizeof(int));
+	img_edges 		= cvCreateImage(img_attrib_dim, IPL_DEPTH_8U, 1);
+	img_sine		= cvCreateImage(img_attrib_sine, IPL_DEPTH_8U, 1);
+	cvZero(img_sine);
+}
+    
+    
+    {    return solutions;
+}
+    
+    // Usage:
+//   ./count_set_bits_lookup_table <amount of random numbers>, e.g:
+//   ./count_set_bits_lookup_table 2017
+//   -> Naive approach: 7962 in 107μs
+//   -> Lookup table: 7962 in 20μs
+//   -> GCC builtin: 7962 in 5μs
+    
+        websocket::async_echo_server s1{&std::cout, 1};
+    s1.set_option(read_message_max{64 * 1024 * 1024});
+    s1.set_option(auto_fragment{false});
+    s1.set_option(pmd);
+    s1.open(endpoint_type{
+        address_type::from_string('0.0.0.0'), 3000 }, ec);
+    
+    int callback(lws *wsi, lws_callback_reasons reason, void *user, void *in, size_t len)
+{
+    SocketExtension *ext = (SocketExtension *) user;
+    }
+    
+        auto startPoint = high_resolution_clock::now();
+    vector<thread *> threads;
+    for (int i = 0; i < THREADS; i++) {
+        threads.push_back(new thread([i] {
+            while(nextConnection(i));
+        }));
+    }
+    
+        void *getData() {
+        return data;
+    }
+    
+        Header getHeader(const char *key, size_t length) {
+        if (headers) {
+            for (Header *h = headers; *++h; ) {
+                if (h->keyLength == length && !strncmp(h->key, key, length)) {
+                    return *h;
+                }
+            }
+        }
+        return {nullptr, nullptr, 0, 0};
+    }
+    
+        void closeSocket(uv_os_sock_t fd) {
+#ifdef _WIN32
+        closesocket(fd);
+#else
+        close(fd);
+#endif
+    }
+    
+    #include 'WebSocket.h'
+#include <vector>
+    
+    
+    {    if (addr.ss_family == AF_INET) {
+        sockaddr_in *ipv4 = (sockaddr_in *) &addr;
+        inet_ntop(AF_INET, &ipv4->sin_addr, buf, sizeof(buf));
+        return {ntohs(ipv4->sin_port), buf, 'IPv4'};
+    } else {
+        sockaddr_in6 *ipv6 = (sockaddr_in6 *) &addr;
+        inet_ntop(AF_INET6, &ipv6->sin6_addr, buf, sizeof(buf));
+        return {ntohs(ipv6->sin6_port), buf, 'IPv6'};
+    }
+}
+    
+    
+    {    // if we are created in a group with sliding deflate window allocate it here
+    if (Group<isServer>::from(this)->extensionOptions & SLIDING_DEFLATE_WINDOW) {
+        slidingDeflateWindow = Hub::allocateDefaultCompressor(new z_stream{});
+    }
+}
