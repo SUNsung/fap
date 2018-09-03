f6b5488493c2561dedfc314e9d839a47372faed1
SUNsung <weatherius@gmail.com>
2018-09-03 19:35:49 +0300

    Rewrite [03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py] File in: 2018-09-03 19:35:49

--- pyton/03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py
+++ pyton/03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py
@@ -1,49 +1,189 @@
 
         
+        secret_msg = b'Secret message goes here'
+    
+    
+if len(sys.argv) <= 1:
+    print('Specify the version number as parameter')
+    sys.exit()
+version = sys.argv[1]
+    
+        template = template.replace('@SITES@', textwrap.indent('\n'.join(ie_htmls), '\t'))
+    
+        bug_text = re.search(
+        r'(?s)#\s*BUGS\s*[^\n]*\s*(.*?)#\s*COPYRIGHT', readme).group(1)
+    dev_text = re.search(
+        r'(?s)(#\s*DEVELOPER INSTRUCTIONS.*?)#\s*EMBEDDING YOUTUBE-DL',
+        readme).group(1)
+    
+    import io
+import optparse
+    
+        def gen_ies_md(ies):
+        for ie in ies:
+            ie_md = '**{0}**'.format(ie.IE_NAME)
+            ie_desc = getattr(ie, 'IE_DESC', None)
+            if ie_desc is False:
+                continue
+            if ie_desc is not None:
+                ie_md += ': {0}'.format(ie.IE_DESC)
+            if not ie.working():
+                ie_md += ' (Currently broken)'
+            yield ie_md
+    
+    # Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+]
+    
+            password = intlist_to_bytes(self.key).decode('utf-8')
+        encrypted = base64.b64encode(
+            intlist_to_bytes(self.iv[:8]) +
+            b'\x0b\xe6\xa4\xd9z\x0e\xb8\xb9\xd0\xd4i_\x85\x1d\x99\x98_\xe5\x80\xe7.\xbf\xa5\x83'
+        ).decode('utf-8')
+        decrypted = (aes_decrypt_text(encrypted, password, 32))
+        self.assertEqual(decrypted, self.secret_msg)
+    
+        # Instantiate the stack of residual units
+    for stage in range(3):
+        for res_block in range(num_res_blocks):
+            activation = 'relu'
+            batch_normalization = True
+            strides = 1
+            if stage == 0:
+                num_filters_out = num_filters_in * 4
+                if res_block == 0:  # first layer and first stage
+                    activation = None
+                    batch_normalization = False
+            else:
+                num_filters_out = num_filters_in * 2
+                if res_block == 0:  # first layer but not first stage
+                    strides = 2    # downsample
+    
+            if self.unit_forget_bias:
+            def bias_initializer(shape, *args, **kwargs):
+                return K.concatenate([
+                    self.bias_initializer((self.units * 5,), *args, **kwargs),
+                    initializers.Ones()((self.units,), *args, **kwargs),
+                    self.bias_initializer((self.units * 2,), *args, **kwargs),
+                ])
+        else:
+            bias_initializer = self.bias_initializer
+        self.bias = self.add_weight(shape=(self.units * 8,),
+                                    name='bias',
+                                    initializer=bias_initializer,
+                                    regularizer=self.bias_regularizer,
+                                    constraint=self.bias_constraint)
+    
+    
+def convert_all_kernels_in_model(model):
+    '''Converts all convolution kernels in a model from Theano to TensorFlow.
+    
+    
+def test_boston_housing():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = boston_housing.load_data()
+        assert len(x_train) == len(y_train)
+        assert len(x_test) == len(y_test)
+    
+    
+@keras_test
+def test_thresholded_relu():
+    layer_test(layers.ThresholdedReLU, kwargs={'theta': 0.5},
+               input_shape=(2, 3, 4))
+    
+            # remove old cert first
+        xlog.info('Removing old cert in database $HOME/.pki/nssdb')
+        cmd_line = 'certutil -L -d sql:$HOME/.pki/nssdb |grep 'GoAgent' && certutil -d sql:$HOME/.pki/nssdb -D -n '%s' ' % ( common_name)
+        os.system(cmd_line)
+    
+        if len(sys.argv) > 1:
+        ip = sys.argv[1]
+    else:
+        ip = '46.134.208.94'
+        ip = '2001:ee0:3203:a::12'
+        print('Usage: check_ip.py [ip] [top_domain] [wait_time=0]')
+    print('test ip:%s' % ip)
+    
+    
+    def __str__(self):
+        return 'FailedPredicateException('+self.ruleName+',{'+self.predicateText+'}?)'
+    __repr__ = __str__
+    
+    
+            This is ignored for lexers.
+        '''
         
-@pytest.fixture(autouse=True)
-def logs(mocker):
-    return mocker.patch('thefuck.entrypoints.not_configured.logs',
-                        new_callable=MagicMock)
+        return None
+    
+            Using setter/getter methods is deprecated. Use o.type instead.'''
     
     
-@pytest.mark.functional
-def test_with_confirmation(proc, TIMEOUT):
-    with_confirmation(proc, TIMEOUT)
-    history_changed(proc, TIMEOUT, u'echo test')
+# This class can be used to keep the ycmd server alive for the duration of the
+# life of the client. By default, ycmd shuts down if it doesn't see a request in
+# a while.
+class YcmdKeepalive( object ):
+  def __init__( self, ping_interval_seconds = 60 * 10 ):
+    self._keepalive_thread = Thread( target = self._ThreadMain )
+    self._keepalive_thread.daemon = True
+    self._ping_interval_seconds = ping_interval_seconds
     
-    CLI for apt.
-Basic commands:
- list - list packages based on package names
- search - search in package descriptions
- show - show package details
     
-        def _test_host(self, url):
+@patch( 'ycm.client.messages_request.PostVimMessage',
+        new_callable = ExtendedMock )
+def HandlePollResponse_MultipleMessagesAndDiagnostics_test( post_vim_message ):
+  diagnostics_handler = ExtendedMock()
+  messages = [
+    { 'filepath': 'foo', 'diagnostics': [ 'PLACEHOLDER1' ] },
+    { 'message': 'On the first day of Christmas, my VimScript gave to me' },
+    { 'filepath': 'bar', 'diagnostics': [ 'PLACEHOLDER2' ] },
+    { 'message': 'A test file in a Command-T' },
+    { 'filepath': 'baz', 'diagnostics': [ 'PLACEHOLDER3' ] },
+    { 'message': 'On the second day of Christmas, my VimScript gave to me' },
+    { 'filepath': 'foo', 'diagnostics': [ 'PLACEHOLDER4' ] },
+    { 'message': 'Two popup menus, and a test file in a Command-T' },
+  ]
+  assert_that( _HandlePollResponse( messages, diagnostics_handler ),
+               equal_to( True ) )
+  diagnostics_handler.UpdateWithNewDiagnosticsForFile.assert_has_exact_calls( [
+    call( 'foo', [ 'PLACEHOLDER1' ] ),
+    call( 'bar', [ 'PLACEHOLDER2' ] ),
+    call( 'baz', [ 'PLACEHOLDER3' ] ),
+    call( 'foo', [ 'PLACEHOLDER4' ] )
+  ] )
+    
+      eq_( request.Done(), True )
+    
+        def increment(self):
+        self.value += 1
+    
+        def detach(self, observer):
         try:
-            header = {
-                'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36',
-                'accept': 'application/json, text/javascript, */*; q=0.01',
-                'accept-encoding': 'gzip, deflate, sdch',
-                'accept-language': 'en-US,en;q=0.8,ja;q=0.6,zh-CN;q=0.4,zh;q=0.2',
-                'connection': 'keep-alive'
-                }
-            response = self.http_client.request('HEAD', url, header, '', read_payload=False)
-            if response:
-                return True
-        except Exception as e:
-            if __name__ == '__main__':
-                xlog.exception('test %s e:%r', url, e)
+            self._observers.remove(observer)
+        except ValueError:
+            pass
     
-    A Parser needs a TokenStream as input (which in turn is usually fed by a
-Lexer):
     
-            raise NotImplementedError
-        
+class B(Node):
+    pass
     
-    def LT(self, i):
-        '''
-        Get the ith character of lookahead.  This is the same usually as
-        LA(i).  This will be used for labels in the generated
-        lexer code.  I'd prefer to return a char here type-wise, but it's
-        probably better to be 32-bit clean and be consistent with LA.
-        '''
+    '''
+*What is this pattern about?
+This pattern is used when creating an object is costly (and they are
+created frequently) but only a few are used at a time. With a Pool we
+can manage those instances we have as of now by caching them. Now it
+is possible to skip the costly creation of an object if one is
+available in the pool.
+A pool allows to 'check out' an inactive object and then to return it.
+If none are available the pool creates one to provide without wait.
+    
+    '''
+Port of the Java example of 'Setter Injection' in
+'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
+(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
+http://xunitpatterns.com/Dependency%20Injection.html.
