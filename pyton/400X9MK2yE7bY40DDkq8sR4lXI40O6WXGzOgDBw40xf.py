a4490cafea92daa969acec7236d7efc06c7bd21c
SUNsung <weatherius@gmail.com>
2018-09-03 05:29:03 +0300

    Rewrite [400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py] File in: 2018-09-03 05:29:02

--- pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
+++ pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
@@ -1,130 +1,142 @@
 
         
-          def __init__(self):
-    self.spec = spec_pb2.MasterSpec()
-    self.hyperparams = spec_pb2.GridPoint()
-    self.lookup_component = {
-        'previous': MockComponent(self, spec_pb2.ComponentSpec())
-    }
-    
-      # All arguments must share the same type.
-  check.Same([
-      weights_arc.dtype.base_dtype, weights_source.dtype.base_dtype,
-      root.dtype.base_dtype, tokens.dtype.base_dtype
-  ], 'dtype mismatch')
-    
-    
-class DiffTest(tf.test.TestCase):
-    
-        # Running with batch size equal to 1 should be fine.
-    self.RunFullTrainingAndInference(
-        'tagger-parser',
-        master_spec=spec,
-        batch_size_limit=1,
-        component_weights=[0., 1., 1.],
-        unroll_using_oracle=[False, True, True],
-        expected_num_actions=9,
-        expected=_TAGGER_PARSER_EXPECTED_SENTENCES)
-    
-    from flask._compat import iteritems, text_type
-from flask.json import dumps, loads
-    
-    
-@pytest.mark.parametrize('data', (
-    {' t': (1, 2, 3)},
-    {' t__': b'a'},
-    {' di': ' di'},
-    {'x': (1, 2, 3), 'y': 4},
-    (1, 2, 3),
-    [(1, 2, 3)],
-    b'\xff',
-    Markup('<html>'),
-    uuid4(),
-    datetime.utcnow().replace(microsecond=0),
+        
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('', '', b'Username is required.'),
+    ('a', '', b'Password is required.'),
+    ('test', 'test', b'already registered'),
 ))
-def test_dump_load_unchanged(data):
-    s = TaggedJSONSerializer()
-    assert s.loads(s.dumps(data)) == data
-    
-        exec('def reraise(tp, value, tb=None):\n raise tp, value, tb')
-    
-        #: The canonical way to decorate class-based views is to decorate the
-    #: return value of as_view().  However since this moves parts of the
-    #: logic from the class declaration to the place where it's hooked
-    #: into the routing system.
-    #:
-    #: You can place one or more decorators in this list and whenever the
-    #: view function is created the result is automatically decorated.
-    #:
-    #: .. versionadded:: 0.8
-    decorators = ()
-    
-    
-def test_memory_consumption():
-    app = flask.Flask(__name__)
-    
-    
-@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
-@pytest.mark.parametrize('filename', filenames)
-def test_rst_file_syntax(filename):
-    p = subprocess.Popen(
-        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE
+def test_register_validate_input(client, username, password, message):
+    response = client.post(
+        '/auth/register',
+        data={'username': username, 'password': password}
     )
-    err = p.communicate()[1]
-    assert p.returncode == 0, err.decode('utf8')
+    assert message in response.data
+    
+        from inspect import getargspec
+    from cStringIO import StringIO
+    
+        def get_namespace(self, namespace, lowercase=True, trim_namespace=True):
+        '''Returns a dictionary containing a subset of configuration options
+        that match the specified namespace/prefix. Example usage::
+    
+        def _fail(self, *args, **kwargs):
+        raise RuntimeError('The session is unavailable because no secret '
+                           'key was set.  Set the secret_key on the '
+                           'application to something unique and secret.')
+    __setitem__ = __delitem__ = clear = pop = popitem = \
+        update = setdefault = _fail
+    del _fail
+    
+            if args:
+            if 'gui' in conf and conf['gui']:
+                # Enter GUI mode.
+                from .gui import gui_main
+                gui_main(*args, **conf)
+            else:
+                # Enter console mode.
+                from .console import console_main
+                console_main(*args, **conf)
+    
+        #decide sourceId to know which extractor to use
+    if 'sourceId' in info: sourceId = info['sourceId']
+    # danmakuId = info['danmakuId']
+    
+        #if dictify(e)['ckplayer']['info'][0]['title'][0]['_text'].strip():  #duration
+        #video_dict['title'] = dictify(e)['ckplayer']['info'][0]['title'][0]['_text'].strip()
+    
+    
+def cntv_download(url, **kwargs):
+    if re.match(r'http://tv\.cntv\.cn/video/(\w+)/(\w+)', url):
+        rid = match1(url, r'http://tv\.cntv\.cn/video/\w+/(\w+)')
+    elif re.match(r'http://tv\.cctv\.com/\d+/\d+/\d+/\w+.shtml', url):
+        rid = r1(r'var guid = '(\w+)'', get_content(url))
+    elif re.match(r'http://\w+\.cntv\.cn/(\w+/\w+/(classpage/video/)?)?\d+/\d+\.shtml', url) or \
+         re.match(r'http://\w+.cntv.cn/(\w+/)*VIDE\d+.shtml', url) or \
+         re.match(r'http://(\w+).cntv.cn/(\w+)/classpage/video/(\d+)/(\d+).shtml', url) or \
+         re.match(r'http://\w+.cctv.com/\d+/\d+/\d+/\w+.shtml', url) or \
+         re.match(r'http://\w+.cntv.cn/\d+/\d+/\d+/\w+.shtml', url): 
+        page = get_content(url)
+        rid = r1(r'videoCenterId','(\w+)'', page)
+        if rid is None:
+            guid = re.search(r'guid\s*=\s*'([0-9a-z]+)'', page).group(1)
+            rid = guid
+    elif re.match(r'http://xiyou.cntv.cn/v-[\w-]+\.html', url):
+        rid = r1(r'http://xiyou.cntv.cn/v-([\w-]+)\.html', url)
+    else:
+        raise NotImplementedError(url)
+    
+    def dailymotion_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    '''Downloads Dailymotion videos by URL.
+    '''
+    
+    #----------------------------------------------------------------------
+def fc2video_download_by_upid(upid, output_dir = '.', merge = True, info_only = False, **kwargs):
+    ''''''
+    fake_headers = {
+        'DNT': '1',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'en-CA,en;q=0.8,en-US;q=0.6,zh-CN;q=0.4,zh;q=0.2',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.58 Safari/537.36',
+        'Accept': '*/*',
+        'X-Requested-With': 'ShockwaveFlash/19.0.0.245',
+        'Connection': 'keep-alive',
+    }
+    api_base = 'http://video.fc2.com/ginfo.php?upid={upid}&mimi={mimi}'.format(upid = upid, mimi = makeMimi(upid))
+    html = get_content(api_base, headers=fake_headers)
+    
+    def get_orig_video_source(api_key, pid, secret):
+    parsed = json.loads(get_content_headered(tmpl_api_call_video_info % (api_key, pid, secret)))
+    for stream in parsed['streams']['stream']:
+        if stream['type'] == 'orig':
+            return stream['_content'].replace('\\', '')
+    return None
+    
+    expected_text_data_docstrings = tuple('\n     |      ' + s if s else ''
+                                      for s in expected_data_docstrings)
+    
+        def testIterator(self):
+        self.createTempFile()
+        with BZ2File(self.filename) as bz2f:
+            self.assertEqual(list(iter(bz2f)), self.TEXT_LINES)
+    
+    def handleSlideshowTitle(title):
+    print('<title>%s</title>' % getText(title.childNodes))
+    
+    # register the Foo class; make `f()` and `g()` accessible via proxy
+MyManager.register('Foo1', Foo)
+    
+            print('Unordered results using pool.imap_unordered():')
+        for x in imap_unordered_it:
+            print('\t', x)
+        print()
+    
+    con.close()
 
     
-    
-@mock.patch('httpie.core.get_response')
-def test_timeout(get_response):
-    def error(msg, *args, **kwargs):
-        global error_msg
-        error_msg = msg % args
-    
-    
-def test_follow_all_redirects_shown(httpbin):
-    r = http('--follow', '--all', httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 3
-    assert r.count('HTTP/1.1 302 FOUND', 2)
-    assert HTTP_OK in r
-    
-        def __init__(self, **kwargs):
-        '''
-        :param env: an class:`Environment` instance
-        :param kwargs: additional keyword argument that some
-                       processor might require.
-    
-            Referer:http://httpie.org  Cookie:foo=bar  User-Agent:bacon/1.0
-    
-    
-def test_POST_form_multiple_values(httpbin_both):
-    r = http('--form', 'POST', httpbin_both + '/post', 'foo=bar', 'foo=baz')
-    assert HTTP_OK in r
-    assert r.json['form'] == {'foo': ['bar', 'baz']}
-    
-        return out
-    
-    This module handles import compatibility issues between Python 2 and
-Python 3.
+    image = face_recognition.load_image_file('{}')
+face_locations = face_recognition.face_locations(image)
 '''
     
-        def json(self, **kwargs):
-        r'''Returns the json-encoded content of a response, if any.
+        # Apply some eyeliner
+    d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(0, 0, 0, 110), width=6)
+    d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(0, 0, 0, 110), width=6)
     
-    def _init():
-    for code, titles in _codes.items():
-        for title in titles:
-            setattr(codes, title, code)
-            if not title.startswith(('\\', '/')):
-                setattr(codes, title.upper(), code)
+    # Find all the faces in the image using a pre-trained convolutional neural network.
+# This method is more accurate than the default HOG model, but it's slower
+# unless you have an nvidia GPU and dlib compiled with CUDA extensions. But if you do,
+# this will use GPU acceleration and perform well.
+# See also: find_faces_in_picture.py
+face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model='cnn')
     
-    - BaseRecognizer: Base class with common recognizer functionality.
-- Lexer: Base class for lexers.
-- Parser: Base class for parsers.
-- tree.TreeParser: Base class for %tree parser.
     
-    A tree.RewriteCardinalityException is raised, when the parsers hits a
-cardinality mismatch during AST construction. Although this is basically a
-bug in your grammar, it can only be detected at runtime.
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+    
+    with open('README.rst') as readme_file:
+    readme = readme_file.read()
