5ba7e73881e255b77f49de48edbdacb3956d82f5
SUNsung <weatherius@gmail.com>
2018-09-04 03:59:31 +0300

    Rewrite [56.TkAHW1n1g56eRN4hURhejs562.bR3Jj5Tp.56sFH.rb] File in: 2018-09-04 03:59:30

--- ruby/56.TkAHW1n1g56eRN4hURhejs562.bR3Jj5Tp.56sFH.rb
+++ ruby/56.TkAHW1n1g56eRN4hURhejs562.bR3Jj5Tp.56sFH.rb
@@ -1,41 +1,141 @@
 
         
-          def dry_run!(event = nil)
-    @dry_run = true
+            it 'sends escape characters correctly to the backend' do
+      emitter.events << Event.new(payload: {data: 'Line 1\nLine 2\nLine 3'})
+      formatting_agent.sources << emitter
+      formatting_agent.options.merge!('instructions' => {'data' => '{{data | newline_to_br | strip_newlines | split: '<br />' | join: ','}}'})
+      formatting_agent.save!
     
-      # Optional
-  #   Override this method if you need to group multiple agents based on an API key,
-  #   or server they connect to.
-  #   Have a look at the TwitterStreamAgent for an example.
-  def self.setup_worker; end
+        it 'returns a FontAwesome icon element' do
+      icon = icon_tag('fa-copy', class: 'text-info')
+      expect(icon).to be_html_safe
+      expect(Nokogiri(icon).at('i.fa.fa-copy.text-info')).to be_a Nokogiri::XML::Element
+    end
+  end
     
-      def tumblr
-    Tumblr.configure do |config|
-      config.consumer_key = tumblr_consumer_key
-      config.consumer_secret = tumblr_consumer_secret
-      config.oauth_token = tumblr_oauth_token
-      config.oauth_token_secret = tumblr_oauth_token_secret
+        it 'should ignore strings which just contain a JSONPath' do
+      expect(LiquidMigrator.convert_string('$.data')).to eq('$.data')
+      expect(LiquidMigrator.convert_string('$first_title')).to eq('$first_title')
+      expect(LiquidMigrator.convert_string(' $.data', true)).to eq(' $.data')
+      expect(LiquidMigrator.convert_string('lorem $.data', true)).to eq('lorem $.data')
+    end
+    it 'should raise an exception when encountering complex JSONPaths' do
+      expect { LiquidMigrator.convert_string('$.data.test.*', true) }.
+        to raise_error('JSONPath '$.data.test.*' is too complex, please check your migration.')
+    end
+  end
+    
+      describe '#value_at' do
+    it 'returns the value at a JSON path' do
+      expect(Utils.value_at({ :foo => { :bar => :baz }}.to_json, 'foo.bar')).to eq('baz')
+      expect(Utils.value_at({ :foo => { :bar => { :bing => 2 } }}, 'foo.bar.bing')).to eq(2)
+      expect(Utils.value_at({ :foo => { :bar => { :bing => 2 } }}, 'foo.bar[?(@.bing == 2)].bing')).to eq(2)
     end
     
-    Tumblr::Client.new
+    # TODO:
+# group :mongoid do
+#   gem 'mongoid', '~> 4.0.0'
+# end
+
+    
+    class Devise::PasswordsController < DeviseController
+  prepend_before_action :require_no_authentication
+  # Render the #edit only if coming from a reset password email link
+  append_before_action :assert_reset_token_passed, only: :edit
+    
+      private
+    
+      # Store scopes mappings.
+  mattr_reader :mappings
+  @@mappings = {}
+    
+          generate_helpers!(Devise::URL_HELPERS)
+    
+        unless env['devise.skip_trackable']
+      warden.session(scope)['last_request_at'] = Time.now.utc.to_i
+    end
   end
 end
+
     
-      def destroy
-    @event.destroy
+    group :app do
+  gem 'rack'
+  gem 'sinatra'
+  gem 'sinatra-contrib'
+  gem 'thin'
+  gem 'sprockets'
+  gem 'sprockets-helpers'
+  gem 'erubi'
+  gem 'browser'
+  gem 'sass'
+  gem 'coffee-script'
+end
     
-      def update
-    @user_credential = current_user.user_credentials.find(params[:id])
+      def self.defaults
+    %w(css dom dom_events html http javascript).map(&method(:find))
+  end
     
-        def script_url_for(gist_id, filename)
-      url = 'https://gist.github.com/#{gist_id}.js'
-      url = '#{url}?file=#{filename}' unless filename.nil? or filename.empty?
-      url
+          def additional_options
+        if self.class.internal_urls
+          super.merge! \
+            only: self.class.internal_urls.to_set,
+            only_patterns: nil,
+            skip: nil,
+            skip_patterns: nil,
+            skip_links: nil,
+            fixed_internal_urls: true
+        else
+          super
+        end
+      end
+    
+        # This returns whether the guest is ready to work. If this returns
+    # `false`, then {#detect!} should be called in order to detect the
+    # guest OS.
+    #
+    # @return [Boolean]
+    def ready?
+      !!capability_host_chain
+    end
+  end
+end
+
+    
+            # Returns the instance variables as a hash of key-value pairs.
+        def instance_variables_hash
+          instance_variables.inject({}) do |acc, iv|
+            acc[iv.to_s[1..-1]] = instance_variable_get(iv)
+            acc
+          end
+        end
+    
+    Then(/the current symlink points to the previous release/) do
+  previous_release_path = @release_paths[-2]
+    
+        def configure_backend
+      backend.configure do |sshkit|
+        configure_sshkit_output(sshkit)
+        sshkit.output_verbosity = fetch(:log_level)
+        sshkit.default_env      = fetch(:default_env)
+        sshkit.backend          = fetch(:sshkit_backend, SSHKit::Backend::Netssh)
+        sshkit.backend.configure do |backend|
+          backend.pty                = fetch(:pty)
+          backend.connection_timeout = fetch(:connection_timeout)
+          backend.ssh_options        = (backend.ssh_options || {}).merge(fetch(:ssh_options, {}))
+        end
+      end
     end
     
-        def render(context)
-      code_dir = (context.registers[:site].config['code_dir'].sub(/^\//,'') || 'downloads/code')
-      code_path = (Pathname.new(context.registers[:site].source) + code_dir).expand_path
-      file = code_path + @file
+          def echo?
+        (options || {}).fetch(:echo, true)
+      end
+    end
+  end
+end
+
     
-    Liquid::Template.register_tag('video', Jekyll::VideoTag)
+            print_deprecation_warnings_if_applicable
+    
+          def has_role?(role)
+        roles.include? role.to_sym
+      end
