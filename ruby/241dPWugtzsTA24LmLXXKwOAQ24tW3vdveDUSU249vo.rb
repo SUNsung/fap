1e58880bb7004be0f921f218e40f2d815ba3ae35
SUNsung <weatherius@gmail.com>
2018-09-04 12:17:04 +0300

    Rewrite [241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb] File in: 2018-09-04 12:17:04

--- ruby/241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb
+++ ruby/241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb
@@ -1,97 +1,44 @@
 
         
-            # Whether the collection's documents ought to be written as individual
-    #   files in the output.
-    #
-    # Returns true if the 'write' metadata is true, false otherwise.
-    def write?
-      !!metadata.fetch('output', false)
+                def initialize(object_name, method_name, template_object, options = {})
+          @object_name, @method_name = object_name.to_s.dup, method_name.to_s.dup
+          @template_object = template_object
+    
+                options = options.dup
+            options[:field_name]           = @method_name
+            options[:include_position]     = true
+            options[:prefix]             ||= @object_name
+            options[:index]                = @auto_index if @auto_index && !options.has_key?(:index)
+    
+        def exists?(path, prefixes, *args)
+      find_all(path, prefixes, *args).any?
     end
     
-            msg = ' Please append `--trace` to the `#{cmd.name}` command '
-        dashes = '-' * msg.length
-        Jekyll.logger.error '', dashes
-        Jekyll.logger.error 'Jekyll #{Jekyll::VERSION} ', msg
-        Jekyll.logger.error '', ' for any additional information or backtrace. '
-        Jekyll.logger.abort_with '', dashes
-      end
-      # rubocop: enable RescueException
+    Given /^a user with username '([^\']*)' and password '([^\']*)'$/ do |username, password|
+  @me ||= FactoryGirl.create(:user, :username => username, :password => password,
+                  :password_confirmation => password, :getting_started => false)
+  @me.aspects.create(:name => 'Besties')
+  @me.aspects.create(:name => 'Unicorns')
+  @me.reload
+end
+    
+    require Rails.root.join('spec', 'helper_methods')
+require Rails.root.join('spec', 'support', 'inlined_jobs')
+require Rails.root.join('spec', 'support', 'user_methods')
+include HelperMethods
+    
+      def confirm_not_signed_up
+    confirm_on_page('the new user registration page')
+    confirm_form_validation_error('form#new_user')
+  end
+    
+        it 'generates a jasmine fixture', :fixture => true do
+      contact = alice.contact_for(bob.person)
+      aspect = alice.aspects.create(:name => 'people')
+      contact.aspects << aspect
+      contact.save
+      get :new, params: {person_id: bob.person.id}
+      save_fixture(html_for('body'), 'status_message_new')
     end
   end
 end
-
-    
-            # Build your jekyll site
-        # Continuously watch if `watch` is set to true in the config.
-        def process(options)
-          # Adjust verbosity quickly
-          Jekyll.logger.adjust_verbosity(options)
-    
-              theme.create!
-          Jekyll.logger.info 'Your new Jekyll theme, #{theme.name.cyan},' \
-                             ' is ready for you in #{theme.path.to_s.cyan}!'
-          Jekyll.logger.info 'For help getting started, read #{theme.path}/README.md.'
-        end
-        # rubocop:enable Metrics/AbcSize
-      end
-    end
-  end
-end
-
-    
-              enable_ssl(opts)
-          enable_logging(opts)
-          opts
-        end
-    
-          if ARGV.git?
-        system 'git', 'init'
-        system 'git', 'add', '-A'
-      end
-      if ARGV.interactive?
-        ohai 'Entering interactive mode'
-        puts 'Type `exit' to return and finalize the installation'
-        puts 'Install to this prefix: #{formula.prefix}'
-    
-    module BuildEnvironmentDSL
-  def env(*settings)
-    @env ||= BuildEnvironment.new
-    @env.merge(settings)
-  end
-end
-    
-      def option_defined?(name)
-    @options.include? name
-  end
-end
-
-    
-        def self.cleanup
-      cleanup_cellar
-      cleanup_cache
-      cleanup_logs
-      unless ARGV.dry_run?
-        cleanup_lockfiles
-        rm_DS_Store
-      end
-    end
-    
-            tests.each do |test|
-          target_module << '\n  target '#{test.name.gsub(/'/, '\\\\\'')}' do\n'
-          target_module << '    inherit! :search_paths\n'
-          target_module << template_contents(config.default_test_podfile_path, '    ', 'Pods for testing')
-          target_module << '\n  end\n'
-        end
-    
-      desc 'Install all spree gems'
-  task install: :build do
-    for_each_gem do |gem_path|
-      Bundler.with_clean_env do
-        sh 'gem install #{gem_path}'
-      end
-    end
-  end
-    
-            def find_order
-          @order = Spree::Order.find_by!(number: order_id)
-        end
