30455a27f54dc053250a7891e8008162bac7b852
SUNsung <weatherius@gmail.com>
2018-09-05 00:55:36 +0300

    Rewrite [25rLNtaPsW8JA25TjzopVRDqUw25vdWPczdqDnA25POB.py] File in: 2018-09-05 00:55:35

--- pyton/25rLNtaPsW8JA25TjzopVRDqUw25vdWPczdqDnA25POB.py
+++ pyton/25rLNtaPsW8JA25TjzopVRDqUw25vdWPczdqDnA25POB.py
@@ -1,189 +1,66 @@
 
         
-            ERROR_TIMEOUT = 2
-    ERROR_TOO_MANY_REDIRECTS = 6
+            def __init__(self, from_user_id, to_user_id, request_status, timestamp):
+        self.from_user_id = from_user_id
+        self.to_user_id = to_user_id
+        self.request_status = request_status
+        self.timestamp = timestamp
     
-        def _get_path(self):
-        return os.path.join(self.directory, self.name + '.json')
-    
-        @property
-    def encoding(self):
-        return 'utf8'
-    
-        def __init__(self, env, explicit_json=False,
-                 color_scheme=DEFAULT_STYLE, **kwargs):
-        super(ColorFormatter, self).__init__(**kwargs)
-        if not env.colors:
-            self.enabled = False
-            return
-    
-    
-# bdist_wheel
-extras_require = {
-    # http://wheel.readthedocs.io/en/latest/#defining-conditional-dependencies
-    'python_version == '3.0' or python_version == '3.1'': ['argparse>=1.2.1'],
-    ':sys_platform == 'win32'': ['colorama>=0.2.4'],
-}
-    
-    
-with codecs.open(JSON_FILE_PATH, encoding='utf8') as f:
-    JSON_FILE_CONTENT = f.read()
-    
-        exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR
-    assert error_msg == (
-        'ConnectionError: '
-        'Connection aborted while doing GET request to URL: '
-        'http://www.google.com')
-    
-        urls = []
-    
-    
-def _FormatYcmdDebugInfo( ycmd ):
-  python = ycmd[ 'python' ]
-  clang = ycmd[ 'clang' ]
-  message = ( 'Server Python interpreter: {0}\n'
-              'Server Python version: {1}\n'
-              'Server has Clang support compiled in: {2}\n'
-              'Clang version: {3}\n'.format( python[ 'executable' ],
-                                             python[ 'version' ],
-                                             clang[ 'has_support' ],
-                                             clang[ 'version' ] ) )
-  extra_conf = ycmd[ 'extra_conf' ]
-  extra_conf_path = extra_conf[ 'path' ]
-  if not extra_conf_path:
-    message += 'No extra configuration file found\n'
-  elif not extra_conf[ 'is_loaded' ]:
-    message += ( 'Extra configuration file found but not loaded\n'
-                 'Extra configuration path: {0}\n'.format( extra_conf_path ) )
-  else:
-    message += ( 'Extra configuration file found and loaded\n'
-                 'Extra configuration path: {0}\n'.format( extra_conf_path ) )
-  return message
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-      words = line.split()
-  if not words:
-    return []
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    
-@YouCompleteMeInstance()
-@patch( 'ycm.vimsupport.PostVimMessage', new_callable = ExtendedMock )
-def SendCompletionRequest_ResponseContainingError_test( ycm, post_vim_message ):
-  current_buffer = VimBuffer( 'buffer' )
-    
-    
-def EndsWithPython_Python2Paths_test():
-  python_paths = [
-    'python',
-    'python2',
-    '/usr/bin/python2.7',
-    '/home/user/.pyenv/shims/python2.7',
-    r'C:\Python27\python.exe',
-    '/Contents/MacOS/Python'
-  ]
-    
-        def get_allowed_warnings(self):
-        return [
-            # We can't set a non-heuristic freshness at the framework level,
-            # so just ignore this warning
-            rs.FRESHNESS_HEURISTIC,
-            # For our small test responses the Content-Encoding header
-            # wipes out any gains from compression
-            rs.CONNEG_GZIP_BAD,
-        ]
-    
-    
-class AuthLogoutHandler(BaseHandler, tornado.auth.FacebookGraphMixin):
-    def get(self):
-        self.clear_cookie('fbdemo_user')
-        self.redirect(self.get_argument('next', '/'))
-    
-        def on_message(self, message):
-        logging.info('got message %r', message)
-        parsed = tornado.escape.json_decode(message)
-        chat = {
-            'id': str(uuid.uuid4()),
-            'body': parsed['body'],
-        }
-        chat['html'] = tornado.escape.to_basestring(
-            self.render_string('message.html', message=chat))
-    
-        logging.warning('Starting fetch with simple client')
-    simple_client = SimpleAsyncHTTPClient()
-    simple_client.fetch('http://localhost:%d/' % options.port,
-                        callback=callback)
-    IOLoop.current().start()
-    
-    
-@gen.coroutine
-def c2():
-    pass
-    
-    tmpl = Template('''\
-<!doctype html>
-<html>
-  <head>
-    <title>{{ page_title }}</title>
-  </head>
-  <body>
-    <div class='header'>
-      <h1>{{ page_title }}</h1>
-    </div>
-    <ul class='navigation'>
-    {% for href, caption in [ \
-        ('index.html', 'Index'), \
-        ('downloads.html', 'Downloads'), \
-        ('products.html', 'Products') \
-      ] %}
-      <li><a href='{{ href }}'>{{ caption }}</a></li>
-    {% end %}
-    </ul>
-    <div class='table'>
-      <table>
-      {% for row in table %}
-        <tr>
-        {% for cell in row %}
-          <td>{{ cell }}</td>
-        {% end %}
-        </tr>
-      {% end %}
-      </table>
-    </div>
-  </body>
-</html>\
-''')
-    
-        url = options.url + '/updateReports?agent=%s' % options.name
-    update_ws = yield websocket_connect(url, None)
-    msg = yield update_ws.read_message()
-    assert msg is None
-    IOLoop.instance().stop()
-    
-    extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.coverage',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.viewcode',
-]
+        def __init__(self, categorizer):
+        self.categorizer = categorizer
+        ...
     
     
 if __name__ == '__main__':
-    IOLoop.instance().run_sync(main)
+    HitCounts.run()
+
+    
+        def insert_crawled_link(self, url, signature):
+        '''Add the given link to `crawled_links`.'''
+        pass
+    
+            if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+    
+        def logout(self):
+        return self._client.get('/auth/logout')
+    
+            :param variable_name: name of the environment variable
+        :param silent: set to ``True`` if you want silent failure for missing
+                       files.
+        :return: bool. ``True`` if able to load config, ``False`` otherwise.
+        '''
+        rv = os.environ.get(variable_name)
+        if not rv:
+            if silent:
+                return False
+            raise RuntimeError('The environment variable %r is not set '
+                               'and as such configuration could not be '
+                               'loaded.  Set this variable and make it '
+                               'point to a configuration file' %
+                               variable_name)
+        return self.from_pyfile(rv, silent=silent)
+    
+            from flask import jsonify
+    
+    
+ALL_SSL_OPTIONS_HASHES = [
+    '0f81093a1465e3d4eaa8b0c14e77b2a2e93568b0fc1351c2b87893a95f0de87c',
+    '9a7b32c49001fed4cff8ad24353329472a50e86ade1ef9b2b9e43566a619612e',
+    'a6d9f1c7d6b36749b52ba061fff1421f9a0a3d2cfdafbd63c05d06f65b990937',
+    '7f95624dd95cf5afc708b9f967ee83a24b8025dc7c8d9df2b556bbc64256b3ff',
+    '394732f2bbe3e5e637c3fb5c6e980a1f1b90b01e2e8d6b7cff41dde16e2a756d',
+    '4b16fec2bcbcd8a2f3296d886f17f9953ffdcc0af54582452ca1e52f5f776f16',
+]
+'''SHA256 hashes of the contents of all versions of MOD_SSL_CONF_SRC'''
+    
+        # TODO: decoder should check that nonce is in the protected header
+    
+            self.config.recovery_routine()
+        self.assertEqual(mock_load.call_count, 1)
+    
+        def test_basic_ifdefine(self):
+        self.assertEqual(len(self.parser.find_dir('VAR_DIRECTIVE')), 2)
+        self.assertEqual(len(self.parser.find_dir('INVALID_VAR_DIRECTIVE')), 0)
