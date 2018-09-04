b685b727b64d6b6c2b9fd6a970ecc997dd5912de
SUNsung <weatherius@gmail.com>
2018-09-04 19:31:21 +0300

    Rewrite [233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py] File in: 2018-09-04 19:31:21

--- pyton/233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py
+++ pyton/233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py
@@ -1,41 +1,68 @@
 
         
-            :copyright: © 2010 by the Pallets team.
-    :license: BSD, see LICENSE for more details.
-'''
+            def __init__(self):
+        self.name = 'pgcrypto'
     
-                version = match.group(1).strip()
+        def load(self):
+        try:
+            session_data = self._cache.get(self.cache_key)
+        except Exception:
+            # Some backends (e.g. memcache) raise an exception on invalid
+            # cache keys. If this happens, reset the session. See #17810.
+            session_data = None
+        if session_data is not None:
+            return session_data
+        self._session_key = None
+        return {}
     
-        with app.test_request_context(errors_stream=stream):
-        assert wsgi_errors_stream._get_current_object() is stream
+    KEY_PREFIX = 'django.contrib.sessions.cached_db'
     
-            # Force Python to track this dictionary at all times.
-        # This is necessary since Python only starts tracking
-        # dicts if they contain mutable objects.  It's a horrible,
-        # horrible hack but makes this kinda testable.
-        loc.__storage__['FOOO'] = [1, 2, 3]
+        objects = BaseSessionManager()
     
-        @app.route('/')
-    def index():
-        return None
+        class Meta(AbstractBaseSession.Meta):
+        db_table = 'django_session'
+
     
-        Models: `flatpages.flatpages`
-    Templates: Uses the template defined by the ``template_name`` field,
-        or :template:`flatpages/default.html` if template_name is not defined.
-    Context:
-        flatpage
-            `flatpages.flatpages` object
-    '''
-    if not url.startswith('/'):
-        url = '/' + url
-    site_id = get_current_site(request).id
-    try:
-        f = get_object_or_404(FlatPage, url=url, sites=site_id)
-    except Http404:
-        if not url.endswith('/') and settings.APPEND_SLASH:
-            url += '/'
-            f = get_object_or_404(FlatPage, url=url, sites=site_id)
-            return HttpResponsePermanentRedirect('%s/' % request.path)
+            if getattr(self, 'i18n', False):
+            urls = []
+            current_lang_code = translation.get_language()
+            for lang_code, lang_name in settings.LANGUAGES:
+                translation.activate(lang_code)
+                urls += self._urls(page, protocol, domain)
+            translation.activate(current_lang_code)
         else:
-            raise
-    return render_flatpage(request, f)
+            urls = self._urls(page, protocol, domain)
+    
+    from django.contrib.sites.shortcuts import get_current_site
+from django.core.paginator import EmptyPage, PageNotAnInteger
+from django.http import Http404
+from django.template.response import TemplateResponse
+from django.urls import reverse
+from django.utils.http import http_date
+    
+        def test_non_str_name(self):
+        # issue14638
+        # Treat illegal (non-str) name like no name
+        class A:
+            __name__ = 42
+        class B:
+            pass
+        adoc = pydoc.render_doc(A())
+        bdoc = pydoc.render_doc(B())
+        self.assertEqual(adoc.replace('A', 'B'), bdoc)
+    
+    ##
+    
+    def plus(a, b):
+    time.sleep(0.5*random.random())
+    return a + b
+    
+    # Register the adapter
+sqlite3.register_adapter(Point, adapt_point)
+    
+    DB_FILE = 'mydb'
+    
+        #Merging all the documents into a single PDF
+    for doc in group:
+        for p in doc.pages:
+            ap.append(p)
