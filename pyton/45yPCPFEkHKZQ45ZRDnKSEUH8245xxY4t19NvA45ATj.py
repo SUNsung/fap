1c22ca52d0fb8fc25fd036f7e94215f75faa4445
SUNsung <weatherius@gmail.com>
2018-09-04 15:12:27 +0300

    Rewrite [45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py] File in: 2018-09-04 15:12:26

--- pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
+++ pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
@@ -1,116 +1,219 @@
 
         
-          def testCanCreate(self):
-    '''Tests that create() works on a good spec.'''
-    with tf.Graph().as_default(), self.test_session():
-      master = MockMaster()
-      component = MockComponent(master, _make_biaffine_spec())
-    
-          # The cost corresponding to the three entries is, respectively,
-      # 0.7012, 0.7644, and 1.7269. Each of them is computed using the formula
-      # -prob_i * log(sigmoid(logit_i)) - (1-prob_i) * log(1-sigmoid(logit_i))
-      self.assertAlmostEqual(cost, 3.1924, 4)
-      self.assertEqual(correct, 1)
-      self.assertEqual(total, 3)
-      self.assertAllEqual(gold_labels, [0, 1, 2])
-    
-      NB: The format of the shortened resource paths should be considered an
-  implementation detail and may change.
-    
-        # The directory may already exist when running locally multiple times.
-    if not os.path.exists(lexicon_output_path):
-      os.mkdir(lexicon_output_path)
-    
-    model = Sequential()
-model.add(Embedding(max_features, embedding_size, input_length=maxlen))
-model.add(Dropout(0.25))
-model.add(Conv1D(filters,
-                 kernel_size,
-                 padding='valid',
-                 activation='relu',
-                 strides=1))
-model.add(MaxPooling1D(pool_size=pool_size))
-model.add(LSTM(lstm_output_size))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+            return g.db
     
     
-def get_example_array():
-    np.random.seed(3537)
-    example_array = np.random.random((100, 100)) * 100. - 50.
-    example_array[0, 0] = 0.  # 0 could possibly cause trouble
-    return example_array
-    
-        # Test with Sequential API
-    model = Sequential([
-        layers.Dense(16, input_shape=(x_train.shape[-1],), activation='relu'),
-        layers.Dense(8),
-        layers.Activation('relu'),
-        layers.Dense(num_classes, activation='softmax')
-    ])
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='rmsprop',
-                  metrics=['accuracy'])
-    model.summary()
-    history = model.fit(x_train, y_train, epochs=15, batch_size=16,
-                        validation_data=(x_test, y_test),
-                        verbose=0)
-    assert(history.history['val_acc'][-1] > 0.8)
-    config = model.get_config()
-    model = Sequential.from_config(config)
-    
-    model = Sequential()
-model.add(Embedding(max_features, 128, input_length=maxlen))
-model.add(Bidirectional(LSTM(64)))
-model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
-    
-        with custom_object_scope({'MSE_MAE_loss': MSE_MAE_loss}):
-        deserialized = losses.deserialize(serialized)
-    assert isinstance(deserialized, MSE_MAE_loss)
-    assert deserialized.mse_fraction == 0.3
-    
-        with gzip.open(paths[0], 'rb') as lbpath:
-        y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
-    
-    model_input = layers.Input(tensor=x_train_batch)
-model_output = cnn_layers(model_input)
-train_model = keras.models.Model(inputs=model_input, outputs=model_output)
-    
-        def _finish(self, request):
-        self.concurrent -= 1
-        if not request.finished and not request._disconnected:
-            request.finish()
-    
-    # Apply monkey patches to fix issues in external libraries
-from . import _monkeypatches
-del _monkeypatches
+def test_delete(client, auth, app):
+    auth.login()
+    response = client.post('/1/delete')
+    assert response.headers['Location'] == 'http://localhost/'
     
     
-class DownloadHandlers(object):
+# Certain versions of pypy have a bug where clearing the exception stack
+# breaks the __exit__ function in a very peculiar way.  The second level of
+# exception blocks is necessary because pypy seems to forget to check if an
+# exception happened until the next bytecode instruction?
+#
+# Relevant PyPy bugfix commit:
+# https://bitbucket.org/pypy/pypy/commits/77ecf91c635a287e88e60d8ddb0f4e9df4003301
+# According to ronan on #pypy IRC, it is released in PyPy2 2.3 and later
+# versions.
+#
+# Ubuntu 14.04 has PyPy 2.2.1, which does exhibit this bug.
+BROKEN_PYPY_CTXMGR_EXIT = False
+if hasattr(sys, 'pypy_version_info'):
+    class _Mgr(object):
+        def __enter__(self):
+            return self
+        def __exit__(self, *args):
+            if hasattr(sys, 'exc_clear'):
+                # Python 3 (PyPy3) doesn't have exc_clear
+                sys.exc_clear()
+    try:
+        try:
+            with _Mgr():
+                raise AssertionError()
+        except:
+            raise
+    except TypeError:
+        BROKEN_PYPY_CTXMGR_EXIT = True
+    except AssertionError:
+        pass
+
     
-        def __init__(self, headers):
-        self._headers = headers
     
-    class IgnoreRequest(Exception):
-    '''Indicates a decision was made not to process a request'''
+def attach_enctype_error_multidict(request):
+    '''Since Flask 0.8 we're monkeypatching the files object in case a
+    request is detected that does not use multipart form data but the files
+    object is accessed.
+    '''
+    oldcls = request.files.__class__
+    class newcls(oldcls):
+        def __getitem__(self, key):
+            try:
+                return oldcls.__getitem__(self, key)
+            except KeyError:
+                if key not in request.form:
+                    raise
+                raise DebugFilesKeyError(request, key)
+    newcls.__name__ = oldcls.__name__
+    newcls.__module__ = oldcls.__module__
+    request.files.__class__ = newcls
+    
+        #: Tag classes to bind when creating the serializer. Other tags can be
+    #: added later using :meth:`~register`.
+    default_tags = [
+        TagDict, PassDict, TagTuple, PassList, TagBytes, TagMarkup, TagUUID,
+        TagDateTime,
+    ]
+    
+        #: :meth:`make_null_session` will look here for the class that should
+    #: be created when a null session is requested.  Likewise the
+    #: :meth:`is_null_session` method will perform a typecheck against
+    #: this type.
+    null_session_class = NullSession
+    
+        def __call__(self, value):
+        keys = set(value)
+        missing_keys = self.keys - keys
+        if missing_keys:
+            raise ValidationError(
+                self.messages['missing_keys'],
+                code='missing_keys',
+                params={'keys': ', '.join(missing_keys)},
+            )
+        if self.strict:
+            extra_keys = keys - self.keys
+            if extra_keys:
+                raise ValidationError(
+                    self.messages['extra_keys'],
+                    code='extra_keys',
+                    params={'keys': ', '.join(extra_keys)},
+                )
+    
+        def process_response(self, request, response):
+        # No need to check for a redirect for non-404 responses.
+        if response.status_code != 404:
+            return response
+    
+        def save(self, session_key, session_dict, expire_date):
+        s = self.model(session_key, self.encode(session_dict), expire_date)
+        if session_dict:
+            s.save()
+        else:
+            s.delete()  # Clear sessions with no data.
+        return s
+    
+        def report_out_of_quota(self, appid):
+        self.logger.warn('report_out_of_quota:%s', appid)
+        with self.lock:
+            if appid not in self.out_of_quota_appids:
+                self.out_of_quota_appids.append(appid)
+            try:
+                self.working_appid_list.remove(appid)
+            except:
+                pass
+    
+                import win32elevate
+            try:
+                win32elevate.elevateAdminRun(os.path.abspath(__file__))
+            except Exception as e:
+                xlog.warning('CertUtil.import_windows_ca failed: %r', e)
+            return True
+        elif ret == 1:
+            CertUtil.win32_notify(msg=u'已经导入GoAgent证书，请重启浏览器.', title=u'Restart browser need.')
+    
+        def get_ip(self):
+        with self.lock:
+            while True:
+                line = self.in_fd.readline()
+                if not line:
+                    raise Exception()
+    
+        if not os.path.isdir(data_gae_proxy_path):
+        os.mkdir(data_gae_proxy_path)
+    
+        def get(self):
+        n = random.randint(2, 3)
+        ws = []
+        for i in range(0, n):
+            w = self.slice.get()
+            ws.append(w)
+    
+    - CommonTokenStream: A basic and most commonly used TokenStream
+  implementation.
+- TokenRewriteStream: A modification of CommonTokenStream that allows the
+  stream to be altered (by the Parser). See the 'tweak' example for a usecase.
     
     
-class TestData(unittest.TestCase):
-    
-        def unsubscribe(self, msg):
-        self.provider.unsubscribe(msg, self)
-    
-        def test_tc3_output(self):
-        self.tc3.run()
-        output = self.out.getvalue().strip()
-        self.assertEqual(output, self.average_result_tc3)
     
     
-# Alternative implementation for different levels of methods
-class CatalogInstance(object):
+    def substring(self, start, stop):
+        '''
+        For infinite streams, you don't need this; primarily I'm providing
+        a useful interface for action code.  Just make sure actions don't
+        use this on streams that don't support it.
+        '''
+    
+    # Additional templates that should be rendered to pages.
+html_additional_pages = {
+    'download': 'download.html',
+    'index': 'indexcontent.html',
+}
+    
+        def persistent_load(self, pid):
+        # This method is invoked whenever a persistent ID is encountered.
+        # Here, pid is the tuple returned by DBPickler.
+        cursor = self.connection.cursor()
+        type_tag, key_id = pid
+        if type_tag == 'MemoRecord':
+            # Fetch the referenced record from the database and return it.
+            cursor.execute('SELECT * FROM memos WHERE key=?', (str(key_id),))
+            key, task = cursor.fetchone()
+            return MemoRecord(key, task)
+        else:
+            # Always raises an error if you cannot return the correct object.
+            # Otherwise, the unpickler will think None is the object referenced
+            # by the persistent ID.
+            raise pickle.UnpicklingError('unsupported persistent object')
+    
+    # If we want to print a preview of the message content, we can extract whatever
+# the least formatted payload is and print the first three lines.  Of course,
+# if the message has no plain text part printing the first three lines of html
+# is probably useless, but this is just a conceptual example.
+simplest = msg.get_body(preferencelist=('plain', 'html'))
+print()
+print(''.join(simplest.get_content().splitlines(keepends=True)[:3]))
+    
+    dom = xml.dom.minidom.parseString(document)
+    
+    con = sqlite3.connect(DB_FILE)
+cur = con.cursor()
+cur.execute('''
+        create table people
+        (
+          name_last      varchar(20),
+          age            integer
+        )
+        ''')
+    
+            # Adding field 'ApiKey.scope_list'
+        db.add_column(
+            'sentry_apikey',
+            'scope_list',
+            self.gf('sentry.db.models.fields.array.ArrayField')(
+                of=('django.db.models.fields.TextField', [], {})
+            ),
+            keep_default=False
+        )
     
     
-class UserSpecification(CompositeSpecification):
-    
-        '''Base state. This is to share functionality'''
+class Migration(SchemaMigration):
+    def forwards(self, orm):
+        # Adding field 'CommitAuthor.external_id'
+        db.add_column(
+            'sentry_commitauthor',
+            'external_id',
+            self.gf('django.db.models.fields.CharField')(max_length=164, null=True),
+            keep_default=False
+        )
