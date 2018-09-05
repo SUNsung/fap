bae53c0ad114cb0ac1334003a42a7f50f46343b0
SUNsung <weatherius@gmail.com>
2018-09-05 14:43:00 +0300

    Rewrite [42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py] File in: 2018-09-05 14:42:59

--- pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
+++ pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
@@ -1,45 +1,68 @@
 
         
-            terminal_length = os.getenv('ANSIBLE_VYOS_TERMINAL_LENGTH', 10000)
-    
-    from ansible.module_utils._text import to_bytes
-from ansible.parsing.yaml.objects import AnsibleMapping, AnsibleSequence, AnsibleUnicode
-from ansible.parsing.yaml.objects import AnsibleVaultEncryptedUnicode
-from ansible.utils.unsafe_proxy import wrap_var
-from ansible.parsing.vault import VaultLib
-    
-    import os
-import sys
-import glob
-import re
-import markdown
-import argparse
-    
-        # sample_queue.put('sam')
-    # test_object(sample_queue)
-    # print('Outside func: {}'.format(sample_queue.get()))
-    
-        def test_shall_toggle_from_fm_to_am(self):
-        self.radio.toggle_amfm()
-        state = self.radio.state.name
-        expected_state_name = 'AM'
-        self.assertEqual(state, expected_state_name)
-
-    
-        def test_display_current_time_at_current_time(self):
-        '''
-        Just as justification for working example. (Will always pass.)
-        '''
-        production_code_time_provider = ProductionCodeTimeProvider()
-        class_under_test = TimeDisplay(production_code_time_provider)
-        current_time = datetime.datetime.now()
-        expected_time = '<span class=\'tinyBoldText\'>{}:{}</span>'.format(current_time.hour, current_time.minute)
-        self.assertEqual(class_under_test.get_current_time_as_html_fragment(), expected_time)
-    
+        
+@classmethod
+def get_args(cls, dist, header=None):
     '''
-http://code.activestate.com/recipes/131499-observer-pattern/
+    Yield write_script() argument tuples for a distribution's
+    console_scripts and gui_scripts entry points.
+    '''
+    if header is None:
+        header = cls.get_header()
+    spec = str(dist.as_requirement())
+    for type_ in 'console', 'gui':
+        group = type_ + '_scripts'
+        for name, ep in dist.get_entry_map(group).items():
+            # ensure_safe_name
+            if re.search(r'[\\/]', name):
+                raise ValueError('Path separators not allowed in script names')
+            script_text = TEMPLATE.format(
+                          ep.module_name, ep.attrs[0], '.'.join(ep.attrs),
+                          spec, group, name)
+            args = cls._get_script_args(type_, name, header, script_text)
+            for res in args:
+                yield res
     
-    *TL;DR80
-Implements state as a derived class of the state pattern interface.
-Implements state transitions by invoking methods from the pattern's superclass.
-'''
+    
+@pytest.fixture(params=[(python_3, False),
+                        (python_3, True),
+                        (python_2, False)])
+def proc(request, spawnu, TIMEOUT):
+    container, instant_mode = request.param
+    proc = spawnu(*container)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(init_zshrc.format(
+        u'--enable-experimental-instant-mode' if instant_mode else ''))
+    proc.sendline(u'zsh')
+    if instant_mode:
+        assert proc.expect([TIMEOUT, u'instant mode ready: True'])
+    return proc
+    
+        @staticmethod
+    def import_windows_ca(certfile):
+        xlog.debug('Begin to import Windows CA')
+        with open(certfile, 'rb') as fp:
+            certdata = fp.read()
+            if certdata.startswith(b'-----'):
+                begin = b'-----BEGIN CERTIFICATE-----'
+                end = b'-----END CERTIFICATE-----'
+                certdata = base64.b64decode(b''.join(certdata[certdata.find(begin)+len(begin):certdata.find(end)].strip().splitlines()))
+        try:
+            common_name = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_ASN1, certdata).get_subject().CN
+        except Exception as e:
+            logging.error('load_certificate(certfile=%r) 失败：%s', certfile, e)
+            return -1
+    
+    __file__ = os.path.abspath(__file__)
+if os.path.islink(__file__):
+    __file__ = getattr(os, 'readlink', lambda x: x)(__file__)
+work_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(work_path)
+    
+                else:
+                tokenName = self.tokenNames[e.expecting]
+    
+            Lines are numbered 1..n
+        
+        Using setter/getter methods is deprecated. Use o.line instead.'''
