c4ff6e02e76edbcbfa8dcfc8cef4c9e9d54f7a5b
SUNsung <weatherius@gmail.com>
2018-09-06 09:32:52 +0300

    Rewrite [56NkswV74Yxc.56H7nQ8xqafmg563JOApHelNZQ56yq0.cpp] File in: 2018-09-06 09:32:51

--- cccccc/56NkswV74Yxc.56H7nQ8xqafmg563JOApHelNZQ56yq0.cpp
+++ cccccc/56NkswV74Yxc.56H7nQ8xqafmg563JOApHelNZQ56yq0.cpp
@@ -1,220 +1,265 @@
 
         
-        bool IsUnresponsiveEventSuppressed() {
-  return g_suppress_level > 0;
-}
+        #endif  // TENSORFLOW_PYTHON_EAGER_PYWRAP_TENSOR_H_
+
     
-    namespace atom {
+    // Unlike test_ops.cc, these are built with the 'require_shapes' option in the
+// BUILD file.
+    
+      void Compute(OpKernelContext* context) override {
+    // Output a scalar string.
+    Tensor* output_tensor = nullptr;
+    OP_REQUIRES_OK(context,
+                   context->allocate_output(0, TensorShape(), &output_tensor));
+    auto output = output_tensor->scalar<string>();
     }
     
-      // A new extension RenderViewHost has been registered. The details are
-  // the RenderViewHost*.
-  NOTIFICATION_EXTENSION_VIEW_REGISTERED,
+    void ImportNumpy() {
+  import_array1();
+}
     
-     private:
-  // The following methods are implemented by platform-specific implementations
-  // of this class.
-  //
-  // Start/StopListening are called when transitioning between zero and nonzero
-  // registered accelerators. StartListening will be called after
-  // RegisterAcceleratorImpl and StopListening will be called after
-  // UnregisterAcceleratorImpl.
-  //
-  // For RegisterAcceleratorImpl, implementations return false if registration
-  // did not complete successfully.
-  virtual void StartListening() = 0;
-  virtual void StopListening() = 0;
-  virtual bool RegisterAcceleratorImpl(const ui::Accelerator& accelerator) = 0;
-  virtual void UnregisterAcceleratorImpl(
-      const ui::Accelerator& accelerator) = 0;
+    // Called by python code on initialization.
+//
+// 'trampoline' must represent a python function which has the
+// following signature:
+//   (string, list(ndarray)) | (string, list(EagerTensor)) ->
+//     ndarray | list(ndarray) | python scalar |
+//     EagerTensor | list(EagerTensor) | None
+//
+// The trampoline takes two arguments, the first is a string token
+// used by the python frontend's dispatching logic; the second is a
+// list of numpy ndarrays or EagerTensor objects. It can return a
+// single numpy ndarray, a list of numpy ndarrays, a python scalar, an
+// EagerTensor, a list of EagerTensors, or None.
+//
+// PyFunc requires inputs and outputs to be ndarrays. EagerPyFunc requires
+// inputs to be a list of EagerTensors and outputs to be an EagerTensor, a list
+// of EagerTensors, or None.
+//
+// The C++ runtime converts outputs back to Tensor objects.
+//
+// This function is called by script_ops.py during its module initialization.
+//
+// TODO(zhifengc): Support distributed runtime.
+void InitializePyTrampoline(PyObject* trampoline);
     
-    // An interface the PrintViewManager uses to notify an observer when the print
-// dialog is shown. Register the observer via PrintViewManager::set_observer.
-class PrintViewManagerObserver {
+    
+    {}  // namespace tensorflow
+    
+        QVariant value() const;
+    void setValue(const QVariant &value);
+    
+    #ifndef BITCOIN_RPC_MINING_H
+#define BITCOIN_RPC_MINING_H
+    
+    BOOST_AUTO_TEST_CASE(bip173_testvectors_valid)
+{
+    static const std::string CASES[] = {
+        'A12UEL5L',
+        'a12uel5l',
+        'an83characterlonghumanreadablepartthatcontainsthenumber1andtheexcludedcharactersbio1tt5tgs',
+        'abcdef1qpzry9x8gf2tvdw0s3jn54khce6mua7lmqqqxw',
+        '11qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqc8247j',
+        'split1checkupstagehandshakeupstreamerranterredcaperred2y9e3w',
+        '?1ezyfcl',
+    };
+    for (const std::string& str : CASES) {
+        auto ret = bech32::Decode(str);
+        BOOST_CHECK(!ret.first.empty());
+        std::string recode = bech32::Encode(ret.first, ret.second);
+        BOOST_CHECK(!recode.empty());
+        BOOST_CHECK(CaseInsensitiveEqual(str, recode));
+    }
+}
+    
+    BOOST_AUTO_TEST_CASE(get_difficulty_for_low_target)
+{
+    TestDifficulty(0x1ef88f6f, 0.000016);
+}
+    
+    int64_t UniValue::get_int64() const
+{
+    if (typ != VNUM)
+        throw std::runtime_error('JSON value is not an integer as expected');
+    int64_t retval;
+    if (!ParseInt64(getValStr(), &retval))
+        throw std::runtime_error('JSON integer out of range');
+    return retval;
+}
+    
+    void read_image(std::ifstream* image_file, std::ifstream* label_file,
+        uint32_t index, uint32_t rows, uint32_t cols,
+        char* pixels, char* label) {
+  image_file->seekg(index * rows * cols + 16);
+  image_file->read(pixels, rows * cols);
+  label_file->seekg(index + 8);
+  label_file->read(label, 1);
+}
+    
+      // Get a layer using a LayerParameter.
+  static shared_ptr<Layer<Dtype> > CreateLayer(const LayerParameter& param) {
+    if (Caffe::root_solver()) {
+      LOG(INFO) << 'Creating layer ' << param.name();
+    }
+    const string& type = param.type();
+    CreatorRegistry& registry = Registry();
+    CHECK_EQ(registry.count(type), 1) << 'Unknown layer type: ' << type
+        << ' (known types: ' << LayerTypeListString() << ')';
+    return registry[type](param);
+  }
+    
+    #endif  // CAFFE_BATCHNORM_LAYER_HPP_
+
+    
+    /**
+ * @brief Takes a Blob and crop it, to the shape specified by the second input
+ *  Blob, across all dimensions after the specified axis.
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+    
+    #ifdef USE_CUDNN
+/*
+ * @brief cuDNN implementation of ConvolutionLayer.
+ *        Fallback to ConvolutionLayer for CPU mode.
+ *
+ * cuDNN accelerates convolution through forward kernels for filtering and bias
+ * plus backward kernels for the gradient w.r.t. the filters, biases, and
+ * inputs. Caffe + cuDNN further speeds up the computation through forward
+ * parallelism across groups and backward parallelism across gradients.
+ *
+ * The CUDNN engine does not have memory overhead for matrix buffers. For many
+ * input and filter regimes the CUDNN engine is faster than the CAFFE engine,
+ * but for fully-convolutional models and large inputs the CAFFE engine can be
+ * faster as long as it fits in memory.
+*/
+template <typename Dtype>
+class CuDNNConvolutionLayer : public ConvolutionLayer<Dtype> {
  public:
-  // Notifies the observer that the print dialog was shown.
-  virtual void OnPrintDialogShown() = 0;
+  explicit CuDNNConvolutionLayer(const LayerParameter& param)
+      : ConvolutionLayer<Dtype>(param), handles_setup_(false) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNConvolutionLayer();
     }
     
-    // Parse hex color like '#FFF' or '#EFEFEF'
-SkColor ParseHexColor(const std::string& name);
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
     
-    void OutputImageComponent::ToFloatPixels(float* out, int stride) const {
-  assert(factor_x_ == 1);
-  assert(factor_y_ == 1);
-  for (int block_y = 0; block_y < height_in_blocks_; ++block_y) {
-    for (int block_x = 0; block_x < width_in_blocks_; ++block_x) {
-      coeff_t block[kDCTBlockSize];
-      GetCoeffBlock(block_x, block_y, block);
-      double blockd[kDCTBlockSize];
-      for (int k = 0; k < kDCTBlockSize; ++k) {
-        blockd[k] = block[k];
-      }
-      ComputeBlockIDCTDouble(blockd);
-      for (int iy = 0; iy < 8; ++iy) {
-        for (int ix = 0; ix < 8; ++ix) {
-          int y = block_y * 8 + iy;
-          int x = block_x * 8 + ix;
-          if (y >= height_ || x >= width_) continue;
-          out[(y * width_ + x) * stride] = static_cast<float>(blockd[8 * iy + ix] + 128.0);
-        }
-      }
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+            size_t version = 0;
+    
+                // Since scalar samples can be rank=1 with dim=1, we automatically pad the sequence data shape with a leading axis 
+            // of dim=1 if the sequence data shape's leading axis's dimensionality is not 1
+            if ((fullyDefinedSampleShape.Rank() == 1) && !fullyDefinedSampleShape.HasUnboundDimension() && (fullyDefinedSampleShape.TotalSize() == 1) && (currentSequenceDataShape.Rank() > 0) && (currentSequenceDataShape[0] != 1))
+                currentSequenceDataShape = NDShape(1, 1).AppendShape(currentSequenceDataShape);
+    
+        Variable Variable::Clone() const
+    {
+        Variable clonedVariable;
+        clonedVariable.m_dataFields = m_dataFields->Clone();
     }
+    
+    private:
+    // get the input nodes from the config
+    static vector<ComputationNodeBasePtr> GetInputNodes(const IConfigRecord& config)
+    {
+        return ScriptableObjects::ConfigArray::FlattenedVectorFrom<ComputationNodeBasePtr>(config[L'inputNodes']);
+    }
+    // create an array of parameter names for all inputs
+    // These names are never actually used, but required by the ConfigLambda constructor, and maybe useful for debugging.
+    static vector<wstring> CreateParamNames(const IConfigRecord& config)
+    {
+        let inputNodes = GetInputNodes(config);
+        vector<wstring> paramNames(inputNodes.size());
+        for (size_t i = 0; i < paramNames.size(); i++)
+            paramNames[i] = msra::strfun::wstrprintf(L'input_%d', (int)i);
+        return paramNames;
+    }
+    
+    public:
+    DeclareConstructorFromConfigWithNumInputs(SumColumnElementsNode);
+    SumColumnElementsNode(DEVICEID_TYPE deviceId, const wstring& name)
+        : Base(deviceId, name)
+    {
+    }
+    
+    
+    {        // change to other values so can support multiple sentences in each minibatch
+        ForwardCompute(alpha, backtrace, pos_scores, pair_scores, stt);
+        BackwardCompute(functionValues, backtrace, stp);
+    };
+    
+        // Initialize with bilinear interpolation coefficients (useful for deconvolution layer).
+    void InitBilinear(size_t kernelWidth, size_t kernelHeight)
+    {
+        InitBilinear(Value(), GetSampleLayout(), kernelWidth, kernelHeight, m_deviceId);
+    }
+    
+      /// Create an `inotify` handle descriptor.
+  Status setUp() override;
+    
+     public:
+  Err(Err const&) = default;
+  Err(Err&&) = default;
+  Err& operator=(Err const&) = default;
+  Err& operator=(Err&&) = default;
+    
+    template <class String>
+void randomString(String* toFill, size_t size = 1000) {
+  assert(toFill);
+  toFill->resize(size);
+  FOR_EACH (i, *toFill) {
+    *i = random('a', 'z');
   }
 }
     
-    inline void ColorTransformYCbCrToRGB(uint8_t* pixel) {
-  int y  = pixel[0];
-  int cb = pixel[1];
-  int cr = pixel[2];
-  pixel[0] = kRangeLimit[y + kCrToRedTable[cr]];
-  pixel[1] = kRangeLimit[y +
-                         ((kCrToGreenTable[cr] + kCbToGreenTable[cb]) >> 16)];
-  pixel[2] = kRangeLimit[y + kCbToBlueTable[cb]];
+    BENCHMARK_PARAM(BENCHFUN(insertFront), 16)
+BENCHMARK_PARAM(BENCHFUN(insertFront), 128)
+BENCHMARK_PARAM(BENCHFUN(insertFront), 1024)
+BENCHMARK_PARAM(BENCHFUN(insertFront), 10240)
+BENCHMARK_PARAM(BENCHFUN(insertFront), 102400)
+BENCHMARK_PARAM(BENCHFUN(insertFront), 1024000)
+    
+    TEST(FixedStringFindFirstNotOfTest, FindFirstNotOfChar) {
+  constexpr folly::FixedString<16> tmp{'This is a string'};
+  static_assert(tmp.find_first_not_of('T') == 1u, '');
+  static_assert(tmp.find_first_not_of('i') == 0u, '');
+  static_assert(tmp.find_first_not_of('x', 6u) == 6u, '');
+  static_assert(tmp.find_first_not_of('s', 6u) == 7u, '');
+  static_assert(FS('a').find_first_not_of('a') == tmp.npos, '');
+  static_assert(FS('').find_first_not_of('a') == tmp.npos, '');
 }
     
-        size_t i = 0;      // Points to the next leaf node.
-    size_t j = n + 1;  // Points to the next non-leaf node.
-    for (size_t k = n - 1; k != 0; --k) {
-      size_t left, right;
-      if (tree[i].total_count_ <= tree[j].total_count_) {
-        left = i;
-        ++i;
-      } else {
-        left = j;
-        ++j;
-      }
-      if (tree[i].total_count_ <= tree[j].total_count_) {
-        right = i;
-        ++i;
-      } else {
-        right = j;
-        ++j;
-      }
-    }
     
-    // Computes the DCT (Discrete Cosine Transform) of the 8x8 array in 'block',
-// scaled up by a factor of 16. The values in 'block' are laid out row-by-row
-// and the result is written to the same memory area.
-void ComputeBlockDCT(coeff_t* block);
+    {} // namespace
     
-    double ButteraugliScoreForQuality(double quality) {
-  if (quality < kLowestQuality) quality = kLowestQuality;
-  if (quality > kHighestQuality) quality = kHighestQuality;
-  int index = static_cast<int>(quality);
-  double mix = quality - index;
-  return kScoreForQuality[index - kLowestQuality] * (1 - mix) +
-      kScoreForQuality[index - kLowestQuality + 1] * mix;
+    
+    { private:
+  Function<void(FunctionRef<void(ValueType)>) const> const func_;
+};
+    
+      // Test decoding
+  EXPECT_EQ(size, GroupVarint64::encodedSize(start));
+    
+    
+    {  EXPECT_FALSE(LOG_VALUE((has_test<CV, void()>::value)));
+  EXPECT_FALSE(LOG_VALUE((has_test<CV, void() const>::value)));
+  EXPECT_FALSE(LOG_VALUE((
+          has_test<CV, void() /* nolint */ volatile>::value)));
+  EXPECT_TRUE(LOG_VALUE((
+          has_test<CV, void() const /* nolint */ volatile>::value)));
+  EXPECT_TRUE(LOG_VALUE((
+          has_test<CV, void() /* nolint */ volatile const>::value)));
 }
     
-      // Sync + corrupt => no change
-  ASSERT_OK(writable_file->Fsync());
-  ASSERT_OK(dynamic_cast<MockEnv*>(env_)->CorruptBuffer(kFileName));
-  result.clear();
-  ASSERT_OK(rand_file->Read(0, kGood.size(), &result, &(scratch[0])));
-  ASSERT_EQ(result.compare(kGood), 0);
-    
-    namespace rocksdb {
+    namespace folly {
     }
-    
-     private:
-  // A version of PartialMerge that actually performs 'partial merging'.
-  // Use this to simulate the exact behaviour of the StringAppendOperator.
-  bool _AssocPartialMergeMulti(const Slice& key,
-                               const std::deque<Slice>& operand_list,
-                               std::string* new_value, Logger* logger) const;
-    
-      bool StatisticsJni::HistEnabledForType(uint32_t type) const {
-    if (type >= HISTOGRAM_ENUM_MAX) {
-      return false;
-    }
-    
-    if (m_ignore_histograms.count(type) > 0) {
-        return false;
-    }
-    }
-    
-      class StatisticsJni : public StatisticsImpl {
-   public:
-     StatisticsJni(std::shared_ptr<Statistics> stats);
-     StatisticsJni(std::shared_ptr<Statistics> stats,
-         const std::set<uint32_t> ignore_histograms);
-     virtual bool HistEnabledForType(uint32_t type) const override;
-    }
-    
-      // 7. create wal_pool_ioctx
-  ret = _rados.ioctx_create(_wal_pool_name.c_str(), _wal_pool_ioctx);
-  if (ret < 0) {
-    std::cerr << 'couldn't set up ioctx! error ' << ret << std::endl;
-    ret = EXIT_FAILURE;
-    goto out;
-  }
-    
-    #include <jni.h>
-#include <memory>
-#include <string>
-#include 'rocksjni/jnicallback.h'
-#include 'port/port.h'
-#include 'rocksdb/env.h'
-    
-      void Seek(const Slice& target) override {
-    StopWatch seek_sw(env_, statistics_, BLOB_DB_SEEK_MICROS);
-    RecordTick(statistics_, BLOB_DB_NUM_SEEK);
-    iter_->Seek(target);
-    while (UpdateBlobValue()) {
-      iter_->Next();
-    }
-  }
-    
-    void InstrumentedCondVar::WaitInternal() {
-#ifndef NDEBUG
-  ThreadStatusUtil::TEST_StateDelay(ThreadStatus::STATE_MUTEX_WAIT);
-#endif
-  cond_.Wait();
-}
-    
-    // Register plugin
-REGISTER_CAMERA_TRANSFORMER(FlatCameraTransformer);
-    
-      // Make sure the points are in ccw order.
-  area_ = 0.0;
-  for (int i = 1; i < num_points_; ++i) {
-    area_ += CrossProd(points_[0], points_[i - 1], points_[i]);
-  }
-  if (area_ < 0) {
-    area_ = -area_;
-    std::reverse(points_.begin(), points_.end());
-  }
-  area_ /= 2.0;
-  CHECK_GT(area_, kMathEpsilon);
-    
-    PathObstacle *PathDecision::AddPathObstacle(const PathObstacle &path_obstacle) {
-  std::lock_guard<std::mutex> lock(obstacle_mutex_);
-  return path_obstacles_.Add(path_obstacle.Id(), path_obstacle);
-}
-    
-      /**
-   * @brief Override function Plan in parent class Planner.
-   * @param planning_init_point The trajectory point where planning starts.
-   * @param frame Current planning frame.
-   * @param reference_line_info The computed reference line.
-   * @return OK if planning succeeds; error otherwise.
-   */
-  apollo::common::Status PlanOnReferenceLine(
-      const common::TrajectoryPoint& planning_init_point, Frame* frame,
-      ReferenceLineInfo* reference_line_info) override;
-  /**
-   * @brief Read the recorded trajectory file.
-   * @param filename The name of the trajectory file.
-   */
-  void ReadTrajectoryFile(const std::string& filename);
-    
-      /**
-   * @brief Set lane feature vector
-   * @param Obstacle pointer
-   *        Lane sequence pointer
-   *        Feature container in a vector for receiving the feature values
-   */
-  void SetLaneFeatureValues(Obstacle* obstacle_ptr,
-                            LaneSequence* lane_sequence_ptr,
-                            std::vector<double>* feature_values);
