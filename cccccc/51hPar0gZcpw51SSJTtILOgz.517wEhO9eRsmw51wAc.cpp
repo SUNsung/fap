84b1c661c56a75601bbf928570bbd2e9f9500bc8
SUNsung <weatherius@gmail.com>
2018-09-06 04:33:32 +0300

    Rewrite [51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp] File in: 2018-09-06 04:33:32

--- cccccc/51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp
+++ cccccc/51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp
@@ -1,3 +1,39 @@
 
         
-        
+            std::unique_ptr<DHTPeerAnnounceStorage> peerAnnounceStorage;
+    
+    #endif // D_DHT_RESPONSE_MESSAGE_H
+
+    
+    void DHTRoutingTable::dropNode(const std::shared_ptr<DHTNode>& node)
+{
+  getBucketFor(node)->dropNode(node);
+}
+/*
+  void DHTRoutingTable::moveBucketHead(const std::shared_ptr<DHTNode>& node)
+  {
+  getBucketFor(node)->moveToHead(node);
+  }
+*/
+void DHTRoutingTable::moveBucketTail(const std::shared_ptr<DHTNode>& node)
+{
+  getBucketFor(node)->moveToTail(node);
+}
+    
+      void moveBucketHead(const std::shared_ptr<DHTNode>& node);
+    
+    #endif // D_DHT_TASK_EXECUTOR_H
+
+    
+    #include 'common.h'
+    
+    DHTTaskFactoryImpl::~DHTTaskFactoryImpl() = default;
+    
+      virtual ~DHTTaskQueueImpl();
+    
+    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
+                                           const std::string& ipaddr,
+                                           uint16_t port) const
+{
+  return generateToken(infoHash, ipaddr, port, secret_[0]);
+}
