e988a44aa6d3462c8e35ceb06a12a16ec88ff3a3
SUNsung <weatherius@gmail.com>
2018-09-06 21:45:01 +0300

    Rewrite [22SAvzSus8cGY22h94XY9Vy4UU22TJqz..NItbM227ZW.rb] File in: 2018-09-06 21:45:00

--- ruby/22SAvzSus8cGY22h94XY9Vy4UU22TJqz..NItbM227ZW.rb
+++ ruby/22SAvzSus8cGY22h94XY9Vy4UU22TJqz..NItbM227ZW.rb
@@ -1,163 +1,53 @@
 
         
-              # This method returns an HTML safe string similar to what <tt>Array#join</tt>
-      # would return. The array is flattened, and all items, including
-      # the supplied separator, are HTML escaped unless they are HTML
-      # safe, and the returned string is marked as HTML safe.
-      #
-      #   safe_join([raw('<p>foo</p>'), '<p>bar</p>'], '<br />')
-      #   # => '<p>foo</p>&lt;br /&gt;&lt;p&gt;bar&lt;/p&gt;'
-      #
-      #   safe_join([raw('<p>foo</p>'), raw('<p>bar</p>')], raw('<br />'))
-      #   # => '<p>foo</p><br /><p>bar</p>'
-      #
-      def safe_join(array, sep = $,)
-        sep = ERB::Util.unwrapped_html_escape(sep)
+        require 'action_view/helpers/tags/checkable'
     
-                # Prepend a hidden field to make sure something will be sent back to the
-            # server if all radio buttons are unchecked.
-            if options.fetch('include_hidden', true)
-              hidden_field + rendered_collection
-            else
-              rendered_collection
-            end
-          end
-    
-                case options[:default]
-            when nil
-              Time.current
-            when Date, Time
-              options[:default]
-            else
-              default = options[:default].dup
-    
-            def initialize(object_name, method_name, template_object, content_or_options = nil, options = nil)
-          options ||= {}
-    
-            private
-    
-          def install_phase(**options)
-        link(**options)
-      end
-    
-        def register_sprockets
-      Sprockets.append_path(stylesheets_path)
-      Sprockets.append_path(fonts_path)
-      Sprockets.append_path(javascripts_path)
+        def initialize_copy(other)
+      @paths = other.paths.dup
+      self
     end
+    
+        dump_formula_report :A, 'New Formulae'
+    dump_formula_report :M, 'Updated Formulae'
+    dump_formula_report :R, 'Renamed Formulae'
+    dump_formula_report :D, 'Deleted Formulae'
   end
-end
     
-      find_files = ->(path) {
-    Find.find(Pathname.new(path).relative_path_from(Pathname.new Dir.pwd).to_s).map do |path|
-      path if File.file?(path)
-    end.compact
-  }
-    
-        def str_to_byte_pos(pos)
-      @s.string.slice(0, pos).bytesize
-    end
-  end
-end
-    
-        def process_file(input, output)
-      input_path, output_path = path_for(input), path_for(output)
-      if input_path
-        @options[:from] ||=
-          case input_path
-          when /\.scss$/; :scss
-          when /\.sass$/; :sass
-          when /\.less$/; raise 'sass-convert no longer supports LessCSS.'
-          when /\.css$/; :css
-          end
-      elsif @options[:in_place]
-        raise 'Error: the --in-place option requires a filename.'
-      end
-    
-    if Encoding.default_external != Encoding::UTF_8
-    
-            def run
-          if @pod_name.nil?
-            # Note: at that point, @wipe_all is always true (thanks to `validate!`)
-            # Remove all
-            clear_cache
-          else
-            # Remove only cache for this pod
-            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
-            if cache_descriptors.nil?
-              UI.notice('No cache for pod named #{@pod_name} found')
-            elsif cache_descriptors.count > 1 && !@wipe_all
-              # Ask which to remove
-              choices = cache_descriptors.map { |c| '#{@pod_name} v#{c[:version]} (#{pod_type(c)})' }
-              index = UI.choose_from_array(choices, 'Which pod cache do you want to remove?')
-              remove_caches([cache_descriptors[index]])
-            else
-              # Remove all found cache of this pod
-              remove_caches(cache_descriptors)
-            end
-          end
-        end
-    
-            # Prints the list of specs & pod cache dirs for a single pod name.
-        #
-        # This output is valid YAML so it can be parsed with 3rd party tools
-        #
-        # @param [Array<Hash>] cache_descriptors
-        #        The various infos about a pod cache. Keys are
-        #        :spec_file, :version, :release and :slug
-        #
-        def print_pod_cache_infos(pod_name, cache_descriptors)
-          UI.puts '#{pod_name}:'
-          cache_descriptors.each do |desc|
-            if @short_output
-              [:spec_file, :slug].each { |k| desc[k] = desc[k].relative_path_from(@cache.root) }
-            end
-            UI.puts('  - Version: #{desc[:version]}')
-            UI.puts('    Type:    #{pod_type(desc)}')
-            UI.puts('    Spec:    #{desc[:spec_file]}')
-            UI.puts('    Pod:     #{desc[:slug]}')
-          end
-        end
-      end
-    end
+          export JAVA_HOME='$(/usr/libexec/java_home)'
+      export AWS_ACCESS_KEY='<Your AWS Access ID>'
+      export AWS_SECRET_KEY='<Your AWS Secret Key>'
+      export #{home_name}='#{home_value}'
+    EOS
   end
 end
 
     
-          # This method depends on the fact that we have guarded
-      # against implicit and empty literals.
-      def closing_brace_on_same_line?(node)
-        node.loc.end.line == children(node).last.last_line
-      end
+            it 'creates new Agents, even if one already exists with the given guid (so that we don't overwrite a user's work outside of the scenario)' do
+          agents(:bob_weather_agent).update_attribute :guid, 'a-weather-agent'
     
-      it 'ignores empty arrays' do
-    expect_no_offenses('[]')
-  end
+    require Rails.root.join('spec', 'helper_methods')
+require Rails.root.join('spec', 'support', 'inlined_jobs')
+require Rails.root.join('spec', 'support', 'user_methods')
+include HelperMethods
     
-            def on_for(node)
-          if style == :each
-            add_offense(node, message: PREFER_EACH) do
-              opposite_style_detected
-            end
-          else
-            correct_style_detected
-          end
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
+    
+      describe '#destroy' do
+    before do
+      @message = bob.post(:status_message, text: 'hey', to: @alices_aspect.id)
+      @like = alice.like!(@message)
+    end
+    
+            def add_arg_offense(arg, type)
+          add_offense(arg.parent, location: arg.source_range,
+                                  message: format(MSG,
+                                                  type: type.to_s.capitalize))
         end
     
-          # Checks whether the `for` node has a `do` keyword.
-      #
-      # @return [Boolean] whether the `for` node has a `do` keyword
-      def do?
-        loc.begin && loc.begin.is?('do')
-      end
-    
-          # Custom destructuring method. This is used to normalize the branches
-      # for `pair` and `kwsplat` nodes, to add duck typing to `hash` elements.
-      #
-      # @return [Array<KeywordSplatNode>] the different parts of the `kwsplat`
-      def node_parts
-        [self, self]
-      end
-    end
+      include_examples 'multiline literal brace layout trailing comma' do
+    let(:open) { '[' }
+    let(:close) { ']' }
   end
 end
