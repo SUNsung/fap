e56168cdf005bf5feb86c0ec8fe54ea4a9f1fd7e
SUNsung <weatherius@gmail.com>
2018-09-06 03:41:01 +0300

    Rewrite [153Kg81.5Z5wc156Yz0q.ZBvCY15ek5eiuSEXRY15UkL.py] File in: 2018-09-06 03:41:00

--- pyton/153Kg81.5Z5wc156Yz0q.ZBvCY15ek5eiuSEXRY15UkL.py
+++ pyton/153Kg81.5Z5wc156Yz0q.ZBvCY15ek5eiuSEXRY15UkL.py
@@ -1,3 +1,197 @@
 
         
-            point = readme_soup.find_all('h1')[1]
+            def get_expire_at_browser_close(self):
+        '''
+        Return ``True`` if the session is set to expire when the browser
+        closes, and ``False`` if there's an expiry date. Use
+        ``get_expiry_date()`` or ``get_expiry_age()`` to find the actual expiry
+        date/age, if there is one.
+        '''
+        if self.get('_session_expiry') is None:
+            return settings.SESSION_EXPIRE_AT_BROWSER_CLOSE
+        return self.get('_session_expiry') == 0
+    
+        def delete(self, session_key=None):
+        if session_key is None:
+            if self.session_key is None:
+                return
+            session_key = self.session_key
+        self._cache.delete(self.cache_key_prefix + session_key)
+    
+        def save(self, must_create=False):
+        super().save(must_create)
+        self._cache.set(self.cache_key, self._session, self.get_expiry_age())
+    
+        def exists(self, session_key):
+        return self.model.objects.filter(session_key=session_key).exists()
+    
+        The Django sessions framework is entirely cookie-based. It does
+    not fall back to putting session IDs in URLs. This is an intentional
+    design decision. Not only does that behavior make URLs ugly, it makes
+    your site vulnerable to session-ID theft via the 'Referer' header.
+    
+        # If protocol is None, the URLs in the sitemap will use the protocol
+    # with which the sitemap was requested.
+    protocol = None
+    
+    
+def get_web_acl_by_name(client, module, name):
+    acls = [d['WebACLId'] for d in list_web_acls(client, module) if d['Name'] == name]
+    if acls:
+        return acls[0]
+    else:
+        return acls
+    
+    import re
+    
+        try:
+        all_hooks = connection.describe_lifecycle_hooks(
+            AutoScalingGroupName=asg_name
+        )
+    except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
+        module.fail_json_aws(e, msg='Failed to get Lifecycle Hooks')
+    
+    
+def get_eni_info(interface):
+    
+        region, ec2_url, aws_connect_params = get_aws_connection_info(module, boto3=True)
+    
+            alm = MetricAlarm(
+            name=name,
+            metric=metric,
+            namespace=namespace,
+            statistic=statistic,
+            comparison=comparison,
+            threshold=threshold,
+            period=period,
+            evaluation_periods=evaluation_periods,
+            unit=unit,
+            description=description,
+            dimensions=dimensions,
+            alarm_actions=alarm_actions,
+            insufficient_data_actions=insufficient_data_actions,
+            ok_actions=ok_actions
+        )
+        try:
+            connection.create_alarm(alm)
+            changed = True
+            alarms = connection.describe_alarms(alarm_names=[name])
+        except BotoServerError as e:
+            module.fail_json(msg=str(e))
+    
+    
+def create_scaling_policy(connection, module):
+    sp_name = module.params.get('name')
+    adjustment_type = module.params.get('adjustment_type')
+    asg_name = module.params.get('asg_name')
+    scaling_adjustment = module.params.get('scaling_adjustment')
+    min_adjustment_step = module.params.get('min_adjustment_step')
+    cooldown = module.params.get('cooldown')
+    
+    import urllib
+    
+        for quality in ['1080','720','480','380','240','144','auto']:
+        try:
+            real_url = info[quality][1]['url']
+            if real_url:
+                break
+        except KeyError:
+            pass
+    
+            # here s the parser...
+        stream_types = dilidili_parser_data_to_stream_types(typ, vid, hd2, sign, tmsign, ulk)
+        
+        #get best
+        best_id = max([i['id'] for i in stream_types])
+        
+        parse_url = 'http://player.005.tv/parse.php?xmlurl=null&type={typ}&vid={vid}&hd={hd2}&sign={sign}&tmsign={tmsign}&userlink={ulk}'.format(typ = typ, vid = vid, hd2 = best_id, sign = sign, tmsign = tmsign, ulk = ulk)
+        
+        another_url = 'https://newplayer.jfrft.com/parse.php?xmlurl=null&type={typ}&vid={vid}&hd={hd2}&sign={sign}&tmsign={tmsign}&userlink={ulk}'.format(typ = typ, vid = vid, hd2 = hd2, sign = sign, tmsign = tmsign, ulk = ulk)
+    
+    # [
+# (
+#   regex_match_url,
+#   remote_api_method,
+#   additional_query_parameter_for_method,
+#   parser_for_additional_parameter,
+#   field_where_photourls_are_saved
+# )
+# ]
+url_patterns = [
+    # www.flickr.com/photos/{username|NSID}/sets|albums/{album-id}
+    (
+        pattern_url_photoset,
+        'photosets.getPhotos',
+        'photoset_id',
+        get_photoset_id,
+        'photoset'
+    ),
+    # www.flickr.com/photos/{username|NSID}/{pageN}?
+    (
+        pattern_url_photostream,
+        # according to flickr api documentation, this method needs to be
+        # authenticated in order to filter photo visible to the calling user
+        # but it seems works fine anonymously as well
+        'people.getPhotos',
+        'user_id',
+        get_NSID,
+        'photos'
+    ),
+    # www.flickr.com/photos/{username|NSID}/galleries/{gallery-id}
+    (
+        pattern_url_gallery,
+        'galleries.getPhotos',
+        'gallery_id',
+        get_gallery_id,
+        'photos'
+    ),
+    # www.flickr.com/groups/{groupname|groupNSID}/
+    (
+        pattern_url_group,
+        'groups.pools.getPhotos',
+        'group_id',
+        get_NSID,
+        'photos'
+    ),
+    # www.flickr.com/photos/{username|NSID}/favorites/*
+    (
+        pattern_url_favorite,
+        'favorites.getList',
+        'user_id',
+        get_NSID,
+        'photos'
+    )
+]
+    
+      CheckCall( [ sys.executable, build_file ] + sys.argv[ 1: ] )
+    
+    
+  def Response( self ):
+    return self._response
+    
+    
+# Not caching the result of this function; users shouldn't have to restart Vim
+# after running the install script or setting the
+# `g:ycm_server_python_interpreter` option.
+def PathToPythonInterpreter():
+  # Not calling the Python interpreter to check its version as it significantly
+  # impacts startup time.
+  from ycmd import utils
+    
+          if line[ 0 ] == ' ' or line[ 0 ] == '\t':
+        current_group.lines.append( line.strip() )
+    
+      _assert_rejects( f, { 'text' : 'This is an unimportant taco',
+                        'kind' : 'WARNING' } )
+  _assert_accepts( f, { 'text' : 'This taco will be shown',
+                        'kind' : 'ERROR' } )
+    
+    
+def YouCompleteMe_NotifyUserIfServerCrashed_UnexpectedCore_test():
+  message = ( 'The ycmd server SHUT DOWN \(restart with ':YcmRestartServer'\). '
+              'Unexpected error while loading the YCM core library. Type '
+              '':YcmToggleLogs ycmd_\d+_stderr_.+.log' to check the logs.' )
+  RunNotifyUserIfServerCrashed( {
+    'return_code': 3,
+    'expected_message': matches_regexp( message )
+  } )
