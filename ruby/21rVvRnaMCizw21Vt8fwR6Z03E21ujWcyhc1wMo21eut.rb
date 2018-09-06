a67c06b5a6942a4fa8a38ba17528abd91eeb563b
SUNsung <weatherius@gmail.com>
2018-09-06 10:00:18 +0300

    Rewrite [21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb] File in: 2018-09-06 10:00:17

--- ruby/21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb
+++ ruby/21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb
@@ -1,32 +1,244 @@
 
         
-              ::Sass.load_paths << stylesheets_path
+              #
     
-        # .visible-sm { @include responsive-visibility() }
-    # to:
-    # @include responsive-visibility('.visible-sm')
-    def apply_mixin_parent_selector(file, rule_sel)
-      log_transform rule_sel
-      replace_rules file, '\s*' + rule_sel, comments: false do |rule, rule_pos, css|
-        body = unwrap_rule_block(rule.dup).strip
-        next rule unless body =~ /^@include \w+/m || body =~ /^@media/ && body =~ /\{\s*@include/
-        rule =~ /(#{COMMENT_RE}*)([#{SELECTOR_CHAR}\s*]+?)#{RULE_OPEN_BRACE_RE}/
-        cmt, sel = $1, $2.strip
-        # take one up selector chain if this is an &. selector
-        if sel.start_with?('&')
-          parent_sel = selector_for_pos(css, rule_pos.begin)
-          sel        = parent_sel + sel[1..-1]
+    #
+    
+      s.add_runtime_dependency('addressable',           '~> 2.4')
+  s.add_runtime_dependency('colorator',             '~> 1.0')
+  s.add_runtime_dependency('em-websocket',          '~> 0.5')
+  s.add_runtime_dependency('i18n',                  '>= 0.9.5', '< 2')
+  s.add_runtime_dependency('jekyll-sass-converter', '~> 1.0')
+  s.add_runtime_dependency('jekyll-watch',          '~> 2.0')
+  s.add_runtime_dependency('kramdown',              '~> 1.14')
+  s.add_runtime_dependency('liquid',                '~> 4.0')
+  s.add_runtime_dependency('mercenary',             '~> 0.3.3')
+  s.add_runtime_dependency('pathutil',              '~> 0.9')
+  s.add_runtime_dependency('rouge',                 '~> 3.0')
+  s.add_runtime_dependency('safe_yaml',             '~> 1.0')
+end
+
+    
+            def create_sample_files(path)
+          FileUtils.cp_r site_template + '/.', path
+          FileUtils.chmod_R 'u+w', path
+          FileUtils.rm File.expand_path(scaffold_path, path)
         end
-        # unwrap, and replace @include
-        unindent unwrap_rule_block(rule).gsub(/(@include [\w-]+)\(?([\$\w\-,\s]*)\)?/) {
-          name, args = $1, $2
-          sel.gsub(/\s+/, ' ').split(/,\s*/ ).map { |sel_part|
-            '#{cmt}#{name}('#{sel_part}'#{', ' if args && !args.empty?}#{args})'
-          }.join(';\n')
-        }
+    
+        def self.cleanup_cache
+      return unless HOMEBREW_CACHE.directory?
+      HOMEBREW_CACHE.children.each do |path|
+        if path.to_s.end_with? '.incomplete'
+          cleanup_path(path) { path.unlink }
+          next
+        end
+        if path.basename.to_s == 'java_cache' && path.directory?
+          cleanup_path(path) { FileUtils.rm_rf path }
+          next
+        end
+        if prune?(path)
+          if path.file?
+            cleanup_path(path) { path.unlink }
+          elsif path.directory? && path.to_s.include?('--')
+            cleanup_path(path) { FileUtils.rm_rf path }
+          end
+          next
+        end
+    
+    class PrettyListing
+  def initialize(path)
+    Pathname.new(path).children.sort_by { |p| p.to_s.downcase }.each do |pn|
+      case pn.basename.to_s
+      when 'bin', 'sbin'
+        pn.find { |pnn| puts pnn unless pnn.directory? }
+      when 'lib'
+        print_dir pn do |pnn|
+          # dylibs have multiple symlinks and we don't care about them
+          (pnn.extname == '.dylib' || pnn.extname == '.pc') && !pnn.symlink?
+        end
+      else
+        if pn.directory?
+          if pn.symlink?
+            puts '#{pn} -> #{pn.readlink}'
+          else
+            print_dir pn
+          end
+        elsif Metafiles.list?(pn.basename.to_s)
+          puts pn
+        end
       end
     end
-    }
+  end
     
-      # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+      def updated?
+    initial_revision != current_revision
+  end
+    
+          GivenDailyLike.decrement_for(user.id)
+      expect(value_for(user.id, dt)).to eq(1)
+      expect(limit_reached_for(user.id, dt)).to eq(false)
+    
+    gem 'rails', '~> 5.2'
+gem 'omniauth', '~> 1.3'
+gem 'oauth2'
+gem 'omniauth-oauth2'
+gem 'rdoc'
+    
+        if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      if Devise.sign_in_after_reset_password
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message!(:notice, flash_message)
+        sign_in(resource_name, resource)
+      else
+        set_flash_message!(:notice, :updated_not_active)
+      end
+      respond_with resource, location: after_resetting_password_path_for(resource)
+    else
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+    
+      # GET /resource/unlock?unlock_token=abcdef
+  def show
+    self.resource = resource_class.unlock_access_by_token(params[:unlock_token])
+    yield resource if block_given?
+    
+      config.logger = Logger.new($stdout)
+  Rails.logger  = config.logger
+    
+        if record.timedout?(last_request_at) &&
+        !env['devise.skip_timeout'] &&
+        !proxy.remember_me_is_active?(record)
+      Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
+      throw :warden, scope: scope, message: :timeout
+    end
+    
+      def set_size
+    case params[:id]
+    when 'featured'
+      @account.pinned_statuses.count
+    else
+      raise ActiveRecord::NotFound
+    end
+  end
+    
+      def outbox_presenter
+    if page_requested?
+      ActivityPub::CollectionPresenter.new(
+        id: account_outbox_url(@account, page_params),
+        type: :ordered,
+        part_of: account_outbox_url(@account),
+        prev: prev_page,
+        next: next_page,
+        items: @statuses
+      )
+    else
+      ActivityPub::CollectionPresenter.new(
+        id: account_outbox_url(@account),
+        type: :ordered,
+        size: @account.statuses_count,
+        first: account_outbox_url(@account, page: true),
+        last: account_outbox_url(@account, page: true, min_id: 0)
+      )
+    end
+  end
+    
+        private
+    
+        def require_local_account!
+      redirect_to admin_account_path(@account.id) unless @account.local? && @account.user.present?
+    end
+    
+    module Admin
+  class EmailDomainBlocksController < BaseController
+    before_action :set_email_domain_block, only: [:show, :destroy]
+    
+        def set_report_note
+      @report_note = ReportNote.find(params[:id])
+    end
+  end
+end
+
+    
+      def update
+    if verify_payload?
+      process_salmon
+      head 202
+    elsif payload.present?
+      render plain: signature_verification_failure_reason, status: 401
+    else
+      head 400
+    end
+  end
+    
+        # /././././
+    if self.junk_self_referring_directories
+      str.gsub!(/\//) {
+        '/.' * (rand(3) + 1) + '/'
+      }
+    end
+    
+      #
+  # Initialize a new request instance.
+  #
+  def init_cli(server)
+    self.request   = Request.new
+    self.server    = server
+    self.keepalive = false
+  end
+    
+          when IAX_CTRL_RINGING
+        dprint('RINGING')
+        self.client.send_ack(self)
+    
+      #
+  # Payload types were identified from xCAT-server source code (IPMI.pm)
+  #
+  PAYLOAD_IPMI = 0
+  PAYLOAD_SOL  = 1
+  PAYLOAD_RMCPPLUSOPEN_REQ = 0x10
+  PAYLOAD_RMCPPLUSOPEN_REP = 0x11
+  PAYLOAD_RAKP1 = 0x12
+  PAYLOAD_RAKP2 = 0x13
+  PAYLOAD_RAKP3 = 0x14
+  PAYLOAD_RAKP4 = 0x15
+    
+              # Encodes a Rex::Proto::Kerberos::Model::EncryptionKey into an
+          # ASN.1 String
+          #
+          # @return [String]
+          def encode
+            elems = []
+            elems << OpenSSL::ASN1::ASN1Data.new([encode_type], 0, :CONTEXT_SPECIFIC)
+            elems << OpenSSL::ASN1::ASN1Data.new([encode_value], 1, :CONTEXT_SPECIFIC)
+            seq = OpenSSL::ASN1::Sequence.new(elems)
+    
+              # Decodes the realm field
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [String]
+          def decode_realm(input)
+            input.value[0].value
+          end
+    
+      def up
+    Photo.joins('INNER JOIN posts ON posts.guid = photos.status_message_guid')
+         .where(posts: {type: 'StatusMessage', public: true}).update_all(public: true)
+    
+    describe ConversationsController, :type => :controller do
+  describe '#index' do
+    before do
+      @person = alice.contacts.first.person
+      hash = {
+        :author => @person,
+        :participant_ids => [alice.person.id, @person.id],
+        :subject => 'not spam',
+        :messages_attributes => [ {:author => @person, :text => 'cool stuff'} ]
+      }
+      @conv1 = Conversation.create(hash)
+      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv1.id)
+             .increase_unread(alice)
+      Message.create(:author => @person, :created_at => Time.now + 200, :text => 'another message', :conversation_id => @conv1.id)
+             .increase_unread(alice)
