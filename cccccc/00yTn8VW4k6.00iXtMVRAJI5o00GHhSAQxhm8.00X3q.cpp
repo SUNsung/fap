fd89edaf8941e5e50310e71ca29f04e22c3d9acd
SUNsung <weatherius@gmail.com>
2018-09-07 08:25:03 +0300

    Rewrite [00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp] File in: 2018-09-07 08:25:02

--- cccccc/00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp
+++ cccccc/00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp
@@ -1,186 +1,122 @@
 
         
-        // Tell browser to delete a object.
-// function DeallocateObject(id);
-v8::Handle<v8::Value> DeallocateObject(int routing_id,
-                                       int object_id);
+        #ifndef GRPC_INTERNAL_CPP_CLIENT_CREATE_CHANNEL_INTERNAL_H
+#define GRPC_INTERNAL_CPP_CLIENT_CREATE_CHANNEL_INTERNAL_H
     
-       bool GetAcceleratorForCommandId(
-      int command_id,
-      ui::Accelerator* accelerator) const override;
-    
-    NwMenuModel::NwMenuModel(Delegate* delegate) : SimpleMenuModel(delegate) {
-}
-    
-    void MenuItem::SetChecked(bool checked) {
-  // Set active will cause 'activate' to be emitted, so block here
-  block_active_ = true;
-  gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(menu_item_), checked);
-  block_active_ = false;
-}
-    
-    void MenuItem::SetTooltip(const std::string& tooltip) {
-  is_modified_ = true;
-  tooltip_ = base::UTF8ToUTF16(tooltip);
-  if (menu_)
-    menu_->UpdateStates();
-}
-    
-    class NwAppClearAppCacheFunction : public NWSyncExtensionFunction {
- public:
-  NwAppClearAppCacheFunction();
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    namespace grpc {
     }
     
-    #include 'extensions/browser/extension_function.h'
+    #include <grpcpp/security/credentials.h>
     
-    NwObjCallObjectMethodSyncFunction::NwObjCallObjectMethodSyncFunction() {
+    
+    {}  // namespace grpc
+
+    
+    bool SecureAuthContext::SetPeerIdentityPropertyName(const grpc::string& name) {
+  if (!ctx_) return false;
+  return grpc_auth_context_set_peer_identity_property_name(ctx_,
+                                                           name.c_str()) != 0;
 }
     
-    #ifndef GTEST_INCLUDE_GTEST_INTERNAL_GTEST_FILEPATH_H_
-#define GTEST_INCLUDE_GTEST_INTERNAL_GTEST_FILEPATH_H_
+    void ChannelArguments::SetSslTargetNameOverride(const grpc::string& name) {
+  SetString(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG, name);
+}
     
-      // Returns true iff this number is at most kMaxUlps ULP's away from
-  // rhs.  In particular, this function:
-  //
-  //   - returns false if either number is (or both are) NAN.
-  //   - treats really large numbers as almost equal to infinity.
-  //   - thinks +0.0 and -0.0 are 0 DLP's apart.
-  bool AlmostEquals(const FloatingPoint& rhs) const {
-    // The IEEE standard says that any comparison operation involving
-    // a NAN must return false.
-    if (is_nan() || rhs.is_nan()) return false;
-    }
-    
-    // Protects copying of all linked_ptr objects.
-GTEST_API_ GTEST_DECLARE_STATIC_MUTEX_(g_linked_ptr_mutex);
-    
-      template <typename T>
-  operator ParamGenerator<T>() const {
-    const T array[] = {static_cast<T>(v1_), static_cast<T>(v2_),
-        static_cast<T>(v3_), static_cast<T>(v4_), static_cast<T>(v5_),
-        static_cast<T>(v6_), static_cast<T>(v7_), static_cast<T>(v8_),
-        static_cast<T>(v9_), static_cast<T>(v10_), static_cast<T>(v11_),
-        static_cast<T>(v12_), static_cast<T>(v13_), static_cast<T>(v14_),
-        static_cast<T>(v15_), static_cast<T>(v16_), static_cast<T>(v17_),
-        static_cast<T>(v18_), static_cast<T>(v19_), static_cast<T>(v20_),
-        static_cast<T>(v21_), static_cast<T>(v22_), static_cast<T>(v23_),
-        static_cast<T>(v24_), static_cast<T>(v25_), static_cast<T>(v26_),
-        static_cast<T>(v27_), static_cast<T>(v28_), static_cast<T>(v29_),
-        static_cast<T>(v30_), static_cast<T>(v31_), static_cast<T>(v32_),
-        static_cast<T>(v33_), static_cast<T>(v34_), static_cast<T>(v35_),
-        static_cast<T>(v36_), static_cast<T>(v37_), static_cast<T>(v38_),
-        static_cast<T>(v39_), static_cast<T>(v40_), static_cast<T>(v41_),
-        static_cast<T>(v42_), static_cast<T>(v43_), static_cast<T>(v44_),
-        static_cast<T>(v45_)};
-    return ValuesIn(array);
-  }
-    
-      // Clones a 0-terminated C string, allocating memory using new.  The
-  // caller is responsible for deleting the return value using
-  // delete[].  Returns the cloned string, or NULL if the input is
-  // NULL.
-  //
-  // This is different from strdup() in string.h, which allocates
-  // memory using malloc().
-  static const char* CloneCString(const char* c_str);
-    
-    template <GTEST_3_TYPENAMES_(T)>
-struct tuple_size<GTEST_3_TUPLE_(T) > {
-  static const int value = 3;
-};
-    
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12, typename T13, typename T14, typename T15,
-    typename T16, typename T17, typename T18, typename T19, typename T20,
-    typename T21, typename T22, typename T23, typename T24, typename T25,
-    typename T26, typename T27, typename T28, typename T29, typename T30,
-    typename T31, typename T32>
-struct Types32 {
-  typedef T1 Head;
-  typedef Types31<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15,
-      T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28, T29,
-      T30, T31, T32> Tail;
-};
-    
-    // We don't want to require the users to write TemplatesN<...> directly,
-// as that would require them to count the length.  Templates<...> is much
-// easier to write, but generates horrible messages when there is a
-// compiler error, as gcc insists on printing out each template
-// argument, even if it has the default value (this means Templates<list>
-// will appear as Templates<list, NoneT, NoneT, ..., NoneT> in the compiler
-// errors).
-//
-// Our solution is to combine the best part of the two approaches: a
-// user would write Templates<T1, ..., TN>, and Google Test will translate
-// that to TemplatesN<T1, ..., TN> internally to make error messages
-// readable.  The translation is done by the 'type' member of the
-// Templates template.
-    
-    #include <limits.h>
-#include 'sample1.h'
-#include 'gtest/gtest.h'
-    
-        void DeleteLocalRef(jobject localRef)
-    { functions->DeleteLocalRef(this, localRef); }
-    
-    #pragma once
-    
-      bool hasOnlyOneRef() const {
-    return m_refcount == 1;
-  }
-    
-    // This allows storing the assert message before the current process terminates due to a crash
-typedef void (*AssertHandler)(const char* message);
-void setAssertHandler(AssertHandler assertHandler);
-    
-    
-    {  BuildPlanningTarget(reference_line_info);
-  return Status::OK();
+    // Server
+MeasureDouble RpcServerSentBytesPerRpc() {
+  static const auto measure = MeasureDouble::Register(
+      kRpcServerSentBytesPerRpcMeasureName,
+      'Total bytes sent across all messages per RPC', kUnitBytes);
+  return measure;
 }
     
     
-    { private:
-  DISALLOW_COPY_AND_ASSIGN(BaseCameraTransformer);
-};
-    
-    #include <limits>
-#include <string>
-#include <vector>
-    
-    class Dreamview : public apollo::common::ApolloApp {
- public:
-  std::string Name() const override;
-  apollo::common::Status Init() override;
-  apollo::common::Status Start() override;
-  void Stop() override;
-  virtual ~Dreamview() = default;
-    }
-    
-    bool PathDecision::AddLateralDecision(const std::string &tag,
-                                      const std::string &object_id,
-                                      const ObjectDecisionType &decision) {
-  auto *path_obstacle = path_obstacles_.Find(object_id);
-  if (!path_obstacle) {
-    AERROR << 'failed to find obstacle';
-    return false;
+    {
+    {    grpc_slice_unref_internal(sml.tracing_slice);
+    grpc_slice_unref_internal(sml.census_proto);
+    grpc_slice_unref_internal(sml.path);
+    grpc_census_call_set_context(
+        calld->gc_, reinterpret_cast<census_context*>(&calld->context_));
   }
-  path_obstacle->AddLateralDecision(tag, decision);
-  return true;
+  GRPC_CLOSURE_RUN(calld->initial_on_done_recv_initial_metadata_,
+                   GRPC_ERROR_REF(error));
 }
     
     /**
- * @class RTKReplayPlanner
- * @brief RTKReplayPlanner is a derived class of Planner.
- *        It reads a recorded trajectory from a trajectory file and
- *        outputs proper segment of the trajectory according to vehicle
- * position.
+ * @file
+ * @brief Defines the CanFrame struct and CanClient interface.
  */
-class RTKReplayPlanner : public Planner {
- public:
-  /**
-   * @brief Constructor
+    
+      /**
+   * @brief Create a pointer to a specified brand of CAN client. The brand is
+   *        set in the parameter.
+   * @param parameter The parameter to create the CAN client.
+   * @return A pointer to the created CAN client.
    */
-  RTKReplayPlanner();
+  std::unique_ptr<CanClient> CreateCANClient(const CANCardParameter &parameter);
+    
+    DEFINE_bool(only_one_send, false, 'only send test.');
+DEFINE_string(can_client_conf_file_a,
+              'modules/canbus/conf/can_client_conf_a.pb.txt',
+              'can client conf for client a');
+DEFINE_string(can_client_conf_file_b,
+              'modules/canbus/conf/can_client_conf_b.pb.txt',
+              'can client conf for client b');
+DEFINE_int64(agent_mutual_send_frames, 1000, 'Every agent send frame num');
+    
+    
+    {  return std::string((const char *)(str_buf));
+}
+    
+    
+    {  EsdCanClient esd_can_client;
+  EXPECT_TRUE(esd_can_client.Init(param));
+  EXPECT_EQ(esd_can_client.Start(), ErrorCode::CAN_CLIENT_ERROR_BASE);
+  std::vector<CanFrame> frames;
+  int32_t num = 0;
+  EXPECT_EQ(esd_can_client.Send(frames, &num),
+            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
+  EXPECT_EQ(esd_can_client.Receive(&frames, &num),
+            ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED);
+  CanFrame can_frame;
+  frames.push_back(can_frame);
+  EXPECT_EQ(esd_can_client.SendSingleFrame(frames),
+            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
+  esd_can_client.Stop();
+}
+    
+    // buf size must be 8 bytes, every time, we receive only one frame
+ErrorCode SocketCanClientRaw::Receive(std::vector<CanFrame> *const frames,
+                                      int32_t *const frame_num) {
+  if (!is_started_) {
+    AERROR << 'Nvidia can client is not init! Please init first!';
+    return ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED;
+  }
     }
+    
+    #include 'modules/canbus/proto/chassis_detail.pb.h'
+#include 'modules/common/proto/error_code.pb.h'
+#include 'modules/drivers/canbus/can_client/fake/fake_can_client.h'
+#include 'modules/drivers/canbus/can_comm/protocol_data.h'
+    
+    template <typename SensorType>
+ErrorCode MessageManager<SensorType>::GetSensorData(
+    SensorType *const sensor_data) {
+  if (sensor_data == nullptr) {
+    AERROR << 'Failed to get sensor_data due to nullptr.';
+    return ErrorCode::CANBUS_ERROR;
+  }
+  std::lock_guard<std::mutex> lock(sensor_data_mutex_);
+  sensor_data->CopyFrom(sensor_data_);
+  return ErrorCode::OK;
+}
+    
+    TEST(ByteTest, GetValue) {
+  unsigned char byte_value = 0x1A;
+  Byte value(&byte_value);
+  EXPECT_EQ(0x05, value.get_byte(1, 3));
+  EXPECT_EQ(0x01, value.get_byte(1, 1));
+  EXPECT_EQ(0x00, value.get_byte(8, 1));
+  EXPECT_EQ(0x00, value.get_byte(-1, 1));
+  EXPECT_EQ(0x1A, value.get_byte(0, 10));
+}
