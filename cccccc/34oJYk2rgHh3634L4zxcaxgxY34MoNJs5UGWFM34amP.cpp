57840420c40edeac17e4c90335824391ab3f9103
SUNsung <weatherius@gmail.com>
2018-09-07 06:43:54 +0300

    Rewrite [34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp] File in: 2018-09-07 06:43:53

--- cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
+++ cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
@@ -1,124 +1,153 @@
 
         
-        using base::MessageLoop;
-using base::CommandLine;
-using content::BrowserThread;
-using content::Shell;
-using content::ShellBrowserContext;
-using content::RenderProcessHost;
-    
-    
-    {  base::ListValue result;
-  RenderThread::Get()->Send(new ShellViewHostMsg_Call_Object_Method_Sync(
-      routing_id,
-      object_id,
-      type,
-      method,
-      *static_cast<base::ListValue*>(value_args.get()),
-      &result));
-  return converter->ToV8Value(&result, isolate->GetCurrentContext());
-}
-    
-    #include 'content/nw/src/api/base/base.h'
-#include 'ui/gfx/display_observer.h'
-    
-    
-    {  ignore_result(menu_items_.empty());
-}
-    
-    class NwClipboardSetListSyncFunction : public NWSyncExtensionFunction {
- public:
-  NwClipboardSetListSyncFunction();
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    }
-    
-    #include 'chrome/browser/devtools/devtools_window.h'
-#include 'chrome/browser/extensions/devtools_util.h'
-#include 'chrome/browser/extensions/extension_service.h'
-#include 'content/nw/src/api/menuitem/menuitem.h'
-#include 'content/nw/src/api/object_manager.h'
-#include 'content/public/browser/render_view_host.h'
-#include 'content/public/browser/web_contents.h'
-#include 'extensions/browser/extension_system.h'
-#include 'extensions/common/error_utils.h'
-    
-    namespace tesseract {
-double DotProductAVX(const double* u, const double* v, int n) {
-  fprintf(stderr, 'DotProductAVX can't be used on Android\n');
-  abort();
-}
-}  // namespace tesseract
-    
-    namespace tesseract {
-    }
-    
-      // Rounds the input up to a multiple of the given factor.
-  static int Roundup(int input, int factor) {
-    return (input + factor - 1) / factor * factor;
+          /// Retrieve the array of replacement types, which line up with the
+  /// generic parameters.
+  ///
+  /// Note that the types may be null, for cases where the generic parameter
+  /// is concrete but hasn't been queried yet.
+  ArrayRef<Type> getReplacementTypes() const {
+    return llvm::makeArrayRef(getTrailingObjects<Type>(),
+                              getNumReplacementTypes());
   }
+    
+    bool swift::parseASTSection(SerializedModuleLoader *SML, StringRef buf,
+                            SmallVectorImpl<std::string> &foundModules) {
+  if (!serialization::isSerializedAST(buf))
+    return false;
+    }
+    
+    ClangDiagnosticConsumer::ClangDiagnosticConsumer(
+    ClangImporter::Implementation &impl,
+    clang::DiagnosticOptions &clangDiagOptions,
+    bool dumpToStderr)
+  : TextDiagnosticPrinter(llvm::errs(), &clangDiagOptions),
+    ImporterImpl(impl), DumpToStderr(dumpToStderr) {}
+    
+        struct Margin {
+        Margin() : left(0), right(0), top(0), bottom(0) {}
+        Margin(size_t left_, size_t right_, size_t top_, size_t bottom_)
+            : left(left_), right(right_), top(top_), bottom(bottom_) {}
+    }
+    
+    
+    {} //namespace CAROTENE_NS
+
+    
+    void bitwiseAnd(const Size2D &size,
+                const u8 *src0Base, ptrdiff_t src0Stride,
+                const u8 *src1Base, ptrdiff_t src1Stride,
+                u8 *dstBase, ptrdiff_t dstStride)
+{
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    internal::vtransform(size,
+                         src0Base, src0Stride,
+                         src1Base, src1Stride,
+                         dstBase, dstStride, BitwiseAnd());
+#else
+    (void)size;
+    (void)src0Base;
+    (void)src0Stride;
+    (void)src1Base;
+    (void)src1Stride;
+    (void)dstBase;
+    (void)dstStride;
+#endif
+}
+    
+            //left&right borders
+        if (borderType != BORDER_MODE_CONSTANT)
+            for (s32 k = 0; k < cn; ++k)
+            {
+                lanea[-cn+k] = lanea[idx_l + k];
+                lanea[colsn+k] = lanea[idx_r + k];
+                laneb[-cn+k] = laneb[idx_l + k];
+                laneb[colsn+k] = laneb[idx_r + k];
+            }
+    
+                    if (border == BORDER_MODE_CONSTANT && x4 < 0)
+                    prevx[0] = prevx[1] = prevx[2] = borderValue;
+                else
+                {
+                    prevx[0] = srow0 ? srow0[x4] : borderValue;
+                    prevx[1] =         srow1[x4]              ;
+                    prevx[2] = srow2 ? srow2[x4] : borderValue;
+                }
+    
+     *Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+    
+        if (flipMode == FLIP_VERTICAL_MODE)
+    {
+        for (size_t y = 0; y < size.height; ++y)
+        {
+            const u8 * src_row = internal::getRowPtr(srcBase, srcStride, y);
+            u8 * dst_row = internal::getRowPtr(dstBase, dstStride, size.height - y - 1);
+    }
+    }
+    
+    #include 'intsimdmatrixsse.h'
     
     #endif  // TESSERACT_CCMAIN_EQUATIONDETECT_H_
 
     
-    class TESS_API LTRResultIterator : public PageIterator {
-  friend class ChoiceIterator;
- public:
-  // page_res and tesseract come directly from the BaseAPI.
-  // The rectangle parameters are copied indirectly from the Thresholder,
-  // via the BaseAPI. They represent the coordinates of some rectangle in an
-  // original image (in top-left-origin coordinates) and therefore the top-left
-  // needs to be added to any output boxes in order to specify coordinates
-  // in the original image. See TessBaseAPI::SetRectangle.
-  // The scale and scaled_yres are in case the Thresholder scaled the image
-  // rectangle prior to thresholding. Any coordinates in tesseract's image
-  // must be divided by scale before adding (rect_left, rect_top).
-  // The scaled_yres indicates the effective resolution of the binary image
-  // that tesseract has been given by the Thresholder.
-  // After the constructor, Begin has already been called.
-  LTRResultIterator(PAGE_RES* page_res, Tesseract* tesseract,
-                    int scale, int scaled_yres,
-                    int rect_left, int rect_top,
-                    int rect_width, int rect_height);
-    }
+      bool has_leaders;   // does the line contain leader dots (.....)?
+  bool has_drop_cap;  // does the line have a drop cap?
+  int pix_ldistance;  // distance to the left pblock boundary in pixels
+  int pix_rdistance;  // distance to the right pblock boundary in pixels
+  float pix_xheight;  // guessed xheight for the line
+  int average_interword_space; // average space between words in pixels.
     
-    namespace tesseract {
-    }
+    #include 'osquery/config/parsers/prometheus_targets.h'
     
-    class ScriptDetector {
- public:
-  ScriptDetector(const GenericVector<int>* allowed_scripts,
-                 OSResults* osr, tesseract::Tesseract* tess);
-  void detect_blob(BLOB_CHOICE_LIST* scores);
-  bool must_stop(int orientation);
- private:
-  OSResults* osr_;
-  static const char* korean_script_;
-  static const char* japanese_script_;
-  static const char* fraktur_script_;
-  int korean_id_;
-  int japanese_id_;
-  int katakana_id_;
-  int hiragana_id_;
-  int han_id_;
-  int hangul_id_;
-  int latin_id_;
-  int fraktur_id_;
-  tesseract::Tesseract* tess_;
-  const GenericVector<int>* allowed_scripts_;
-};
+      // serialize the QueryLogItem and make sure decorations go top level
+  auto doc = JSON::newObject();
+  auto status = serializeQueryLogItem(item, doc);
+  std::string expected = 'test';
+  std::string result = doc.doc()['load_test'].GetString();
+  EXPECT_EQ(result, expected);
     
-      /**
-   * Returns a binary image of the current object at the given level.
-   * The position and size match the return from BoundingBoxInternal, and so
-   * this could be upscaled with respect to the original input image.
-   * Use pixDestroy to delete the image after use.
-   */
-  Pix* GetBinaryImage(PageIteratorLevel level) const;
+      ASSERT_TRUE(data.doc().HasMember('events'));
+  ASSERT_TRUE(data.doc()['events'].HasMember('environment_variables'));
+  ASSERT_TRUE(data.doc()['events']['environment_variables'].IsArray());
+  for (const auto& var :
+       data.doc()['events']['environment_variables'].GetArray()) {
+    std::string value = var.GetString();
+    EXPECT_TRUE(value == 'foo' || value == 'bar');
+  }
     
     
-    {  TBLOB* blob;
-  Tesseract* tesseract;
-  BLOB_CHOICE_LIST** choices;
-};
+    {/**
+ * @brief Compute a hash digest from the contents of a buffer.
+ *
+ * @param hash_type The osquery-supported hash algorithm.
+ * @param buffer A caller-controlled buffer (already allocated).
+ * @param size The length of buffer in bytes.
+ * @return A string (hex) representation of the hash digest.
+ */
+std::string hashFromBuffer(HashType hash_type, const void* buffer, size_t size);
+} // namespace osquery
+
     
-    	GetModuleFileName(NULL, exePath, sizeof(exePath));
+      std::shared_ptr<PlatformProcess> process;
+  int process_pid = ::fork();
+  if (process_pid == 0) {
+    // Start a Python script
+    ::execlp('sh', 'sh', '-c', argv.c_str(), nullptr);
+    ::exit(0);
+  } else if (process_pid > 0) {
+    process.reset(new PlatformProcess(process_pid));
+  }
+    
+    TEST_F(QueryTests, test_query_name_not_found_in_db) {
+  // Try to retrieve results from a query that has not executed.
+  QueryDataSet previous_qd;
+  auto query = getOsqueryScheduledQuery();
+  auto cf = Query('not_a_real_query', query);
+  auto status = cf.getPreviousQueryResults(previous_qd);
+  EXPECT_FALSE(status.ok());
+  EXPECT_TRUE(previous_qd.empty());
+}
+    
+      /// Paths to autoload extensions.
+  std::vector<std::string> extensions_paths_;
