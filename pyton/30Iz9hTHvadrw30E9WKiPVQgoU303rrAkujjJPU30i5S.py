2ecaa7c60f1d26551f6dcc30f2df4deb1fa1b851
SUNsung <weatherius@gmail.com>
2018-09-08 14:09:46 +0300

    Rewrite [30Iz9hTHvadrw30E9WKiPVQgoU303rrAkujjJPU30i5S.py] File in: 2018-09-08 14:09:46

--- pyton/30Iz9hTHvadrw30E9WKiPVQgoU303rrAkujjJPU30i5S.py
+++ pyton/30Iz9hTHvadrw30E9WKiPVQgoU303rrAkujjJPU30i5S.py
@@ -1,102 +1,45 @@
 
         
-                with self.assertRaises(Exception):
-          tf.test.compute_gradient_error(
-              scores, [3, 4, 4], log_partition_functions, [3], init_scores)
-    
-          self.assertAllEqual(predictions.eval(),
-                          [[0.0, 1.0, 0.0],
-                           [0.0, 1.0, 0.0],
-                           [0.0, 0.0, 0.0],
-                           [0.0, 1.0, 0.0],
-                           [0.0, 0.0, 1.0],
-                           [0.0, 0.0, 1.0]])  # pyformat: disable
-    
-    
-@pytest.mark.functional
-def test_without_confirmation(proc, TIMEOUT):
-    without_confirmation(proc, TIMEOUT)
-    
-                # train once so that the states change
-            model.train_on_batch(np.ones_like(inputs),
-                                 np.random.random(out1.shape))
-            out2 = model.predict(np.ones_like(inputs))
-    
-            for j in range(n):
-            # Initial position
-            xstart = np.random.randint(20, 60)
-            ystart = np.random.randint(20, 60)
-            # Direction of motion
-            directionx = np.random.randint(0, 3) - 1
-            directiony = np.random.randint(0, 3) - 1
-    
-    print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
-    
-    network2 = {
-    'n_dense': 6,
-    'dense_units': 16,
-    'activation': 'selu',
-    'dropout': AlphaDropout,
-    'dropout_rate': 0.1,
-    'kernel_initializer': 'lecun_normal',
-    'optimizer': 'sgd'
-}
-    
-        with custom_object_scope({'MSE_MAE_loss': MSE_MAE_loss}):
-        deserialized = losses.deserialize(serialized)
-    assert isinstance(deserialized, MSE_MAE_loss)
-    assert deserialized.mse_fraction == 0.3
-    
-        if os.path.exists(path):
-        return
-    
-        def on_open_shell(self):
+            def deal_card(self):
         try:
-            self._exec_cli_command('screen-length 0 temporary')
-        except AnsibleConnectionFailure:
-            raise AnsibleConnectionFailure('unable to set terminal parameters')
-
+            card = self.cards[self.deal_index]
+            card.is_available = False
+            self.deal_index += 1
+        except IndexError:
+            return None
+        return card
     
-    AnsibleConstructor.add_constructor(
-    u'tag:yaml.org,2002:str',
-    AnsibleConstructor.construct_yaml_str)
+            Emit key value pairs of the form:
     
-        data_train = fetch_20newsgroups_vectorized(subset='train')
-    data_test = fetch_20newsgroups_vectorized(subset='test')
-    X_train = check_array(data_train.data, dtype=np.float32,
-                          accept_sparse='csc')
-    X_test = check_array(data_test.data, dtype=np.float32, accept_sparse='csr')
-    y_train = data_train.target
-    y_test = data_test.target
+    from mrjob.job import MRJob
     
-        for i, kk in enumerate(krange):
-        print('k = %i (%i out of %i)' % (kk, i + 1, len(krange)))
-        for algorithm in algorithms:
-            nbrs = neighbors.NearestNeighbors(n_neighbors=kk,
-                                              algorithm=algorithm,
-                                              leaf_size=leaf_size)
-            t0 = time()
-            nbrs.fit(X)
-            t1 = time()
-            nbrs.kneighbors(X)
-            t2 = time()
+        def move_to_front(self, node):
+        ...
     
-        for n_samples in sample_sizes:
-        X = random_state.rand(n_samples, 300)
     
-    html_folder = u'html'
-text_folder = u'paragraphs'
-short_text_folder = u'short_paragraphs'
-n_words_per_short_text = 5
+class PagesDataStore(object):
     
-        if not os.path.exists(ARCHIVE_NAME):
-        print('Downloading dataset from %s (3 MB)' % URL)
-        opener = urlopen(URL)
-        with open(ARCHIVE_NAME, 'wb') as archive:
-            archive.write(opener.read())
+    # Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.linkcode',
+    'sphinx_autodoc_annotation',
+    'edit_on_github'
+]
     
-    plt.show()
+    
+class PasswordChangeRequired(CloudError):
+    '''Raised when a password change is required.'''
+    
+            for key, value in states.items():
+            if isinstance(value, (float, int)):
+                attribute = '{}.{}'.format(metric, key.replace(' ', '_'))
+                statsd.gauge(
+                    attribute, value, sample_rate=sample_rate, tags=tags)
+    
+    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_SCAN_DURATION, default=10): cv.positive_int,
+    vol.Optional(CONF_BLUETOOTH_DEVICE, default='hci0'): cv.string
+})
