292549dc670fb5a7efb8e48f73382fdb8a568105
SUNsung <weatherius@gmail.com>
2018-09-08 12:27:59 +0300

    Rewrite [49LWFspIchO7U49XHqs9LQ3gw249bkftXqhWEJM491h..rb] File in: 2018-09-08 12:27:58

--- ruby/49LWFspIchO7U49XHqs9LQ3gw249bkftXqhWEJM491h..rb
+++ ruby/49LWFspIchO7U49XHqs9LQ3gw249bkftXqhWEJM491h..rb
@@ -1,104 +1,48 @@
 
         
-        module ActionView
-  module Helpers
-    module Tags # :nodoc:
-      class TextField < Base # :nodoc:
-        include Placeholderable
-    
-        module ClassMethods
-      def inherited(klass) # :nodoc:
-        super
-        klass._write_layout_method
-      end
-    
-        def render_partial(event)
-      info do
-        message = '  Rendered #{from_rails_root(event.payload[:identifier])}'.dup
-        message << ' within #{from_rails_root(event.payload[:layout])}' if event.payload[:layout]
-        message << ' (#{event.duration.round(1)}ms)'
-        message << ' #{cache_message(event.payload)}' unless event.payload[:cache_hit].nil?
-        message
-      end
-    end
-    
-    def built_as_bottle?(f)
-  return false unless f.installed?
-  tab = Tab.for_keg(f.installed_prefix)
-  tab.built_as_bottle
+        def global_require
+  JSON.pretty_generate(DATA)
 end
     
-      # True if a {Formula} is being built with {Formula.devel} instead of {Formula.stable}.
-  # <pre>args << '--some-beta' if build.devel?</pre>
-  def devel?
-    include? 'devel'
+    When(%r!^I decide to build the theme gem$!) do
+  Dir.chdir(Paths.theme_gem_dir)
+  File.new('_includes/blank.html', 'w')
+  File.new('_sass/blank.scss', 'w')
+  File.new('assets/blank.scss', 'w')
+end
+    
+        # Private: The list of directories that need to be kept because they are
+    # parent directories of files specified in keep_files
+    #
+    # Returns a Set with the directory paths
+    def keep_dirs
+      site.keep_files.map { |file| parent_dirs(site.in_dest_dir(file)) }.flatten.to_set
+    end
+    
+    module Jekyll
+  module Commands
+    class Build < Command
+      class << self
+        # Create the Mercenary command for the Jekyll CLI for this Command
+        def init_with_program(prog)
+          prog.command(:build) do |c|
+            c.syntax      'build [options]'
+            c.description 'Build your site'
+            c.alias :b
+    
+        def translation_scope
+      'devise.confirmations'
+    end
+end
+
+    
+      def failure
+    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
+    redirect_to after_omniauth_failure_path_for(resource_name)
   end
     
-      def plist_caveats
-    s = []
-    if f.plist || (keg && keg.plist_installed?)
-      destination = if f.plist_startup
-        '/Library/LaunchDaemons'
-      else
-        '~/Library/LaunchAgents'
-      end
-    
-            begin
-          f = Formulary.from_rack(HOMEBREW_CELLAR/name)
-        rescue FormulaUnavailableError, TapFormulaAmbiguityError, TapFormulaWithOldnameAmbiguityError
-          next
-        end
-    
-      private
-    
-      def kernel
-    `uname -m`.chomp
+        get '/'
+    assert last_response.ok?
   end
     
-      # Use this method to generate standard caveats.
-  def standard_instructions(home_name, home_value = libexec)
-    <<-EOS.undent
-      Before you can use these tools you must export some variables to your $SHELL.
-    
-        def register_lotus
-      Lotus::Assets.sources << assets_path
-    end
-    
-      # Generate digests for assets URLs.
-  config.assets.digest = true
-    
-      config.active_support.test_order = :random
-    
-    When /^I (?:sign|log) in manually as '([^']*)' with password '([^']*)'( on the mobile website)?$/ \
-do |username, password, mobile|
-  @me = User.find_by_username(username)
-  @me.password ||= password
-  manual_login
-  confirm_login mobile
-end
-    
-    Then /^I should see an image in the publisher$/ do
-  photo_in_publisher.should be_present
-end
-    
-          @conv2 = Conversation.create(hash)
-      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv2.id)
-             .increase_unread(alice)
-    
-        it 'returns a 404 for a post not visible to the user' do
-      sign_in eve
-      expect {
-        get :index, params: {post_id: @message.id}
-      }.to raise_error(ActiveRecord::RecordNotFound)
-    end
-    
-        # Outputs a list of categories as comma-separated <a> links. This is used
-    # to output the category list for each post on a category page.
-    #
-    #  +categories+ is the list of categories to format.
-    #
-    # Returns string
-    #
-    def category_links(categories)
-      categories.sort.map { |c| category_link c }.join(', ')
-    end
+        proxy = Devise::Hooks::Proxy.new(warden)
