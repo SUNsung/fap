ea300dfb36ba6dce65efac4a6761fba1e10fd05e
SUNsung <weatherius@gmail.com>
2018-09-08 20:50:18 +0300

    Rewrite [34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb] File in: 2018-09-08 20:50:17

--- ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
+++ ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
@@ -1,285 +1,220 @@
 
         
-        def local_require
-  require 'json'
-  JSON.pretty_generate(DATA)
+              expected_options = FastlaneCore::Configuration.create(Deliver::Options.available_options, {
+        description: 'My description',
+        app_identifier: 'abcd',
+        metadata_path: 'metadata/path',
+        force: true
+      })
+    
+            cmd << ['-am #{message.shellescape}']
+        cmd << '--force' if options[:force]
+        cmd << '-s' if options[:sign]
+        cmd << ''#{tag}''
+        cmd << options[:commit].to_s if options[:commit]
+    
+          def self.run(params)
+        unless Helper.test?
+          UI.message('Install using `brew install homebrew/boneyard/appledoc`')
+          UI.user_error!('appledoc not installed') if `which appledoc`.length == 0
+        end
+    
+          def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :file,
+                                       env_name: 'FL_ARTIFACTORY_FILE',
+                                       description: 'File to be uploaded to artifactory',
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :repo,
+                                       env_name: 'FL_ARTIFACTORY_REPO',
+                                       description: 'Artifactory repo to put the file in',
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :repo_path,
+                                       env_name: 'FL_ARTIFACTORY_REPO_PATH',
+                                       description: 'Path to deploy within the repo, including filename',
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :endpoint,
+                                       env_name: 'FL_ARTIFACTORY_ENDPOINT',
+                                       description: 'Artifactory endpoint',
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :username,
+                                       env_name: 'FL_ARTIFACTORY_USERNAME',
+                                       description: 'Artifactory username',
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :password,
+                                       env_name: 'FL_ARTIFACTORY_PASSWORD',
+                                       description: 'Artifactory password',
+                                       sensitive: true,
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :properties,
+                                       env_name: 'FL_ARTIFACTORY_PROPERTIES',
+                                       description: 'Artifact properties hash',
+                                       is_string: false,
+                                       default_value: {},
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :ssl_pem_file,
+                                       env_name: 'FL_ARTIFACTORY_SSL_PEM_FILE',
+                                       description: 'Location of pem file to use for ssl verification',
+                                       default_value: nil,
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :ssl_verify,
+                                       env_name: 'FL_ARTIFACTORY_SSL_VERIFY',
+                                       description: 'Verify SSL',
+                                       is_string: false,
+                                       default_value: true,
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :proxy_username,
+                                       env_name: 'FL_ARTIFACTORY_PROXY_USERNAME',
+                                       description: 'Proxy username',
+                                       default_value: nil,
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :proxy_password,
+                                       env_name: 'FL_ARTIFACTORY_PROXY_PASSWORD',
+                                       description: 'Proxy password',
+                                       sensitive: true,
+                                       default_value: nil,
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :proxy_address,
+                                       env_name: 'FL_ARTIFACTORY_PROXY_ADDRESS',
+                                       description: 'Proxy address',
+                                       default_value: nil,
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :proxy_port,
+                                       env_name: 'FL_ARTIFACTORY_PROXY_PORT',
+                                       description: 'Proxy port',
+                                       default_value: nil,
+                                       optional: true)
+        ]
+      end
+    end
+  end
 end
+
     
-    CONTENT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+        case params
+    when EventFilter.push
+      events.where(action: Event::PUSHED)
+    when EventFilter.merged
+      events.where(action: Event::MERGED)
+    when EventFilter.comments
+      events.where(action: Event::COMMENTED)
+    when EventFilter.team
+      events.where(action: [Event::JOINED, Event::LEFT, Event::EXPIRED])
+    when EventFilter.issue
+      events.where(action: [Event::CREATED, Event::UPDATED, Event::CLOSED, Event::REOPENED])
+    end
+  end
     
-      p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
-  p.option 'destination', '-d', '--destination [DIR]',
-    'Destination directory (defaults to ./_site)'
-  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
-  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
-    'Plugins directory (defaults to ./_plugins)'
-  p.option 'layouts_dir', '--layouts DIR', String,
-    'Layouts directory (defaults to ./_layouts)'
-  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
+            def generate_temporarily_email(username)
+          'temp-email-for-oauth-#{username}@gitlab.localhost'
+        end
+      end
+    end
+  end
+end
+
     
-          #
+              <<-SQL.strip_heredoc
+            (CASE
+              WHEN (#{builds}) = (#{skipped}) AND (#{warnings}) THEN #{STATUSES[:success]}
+              WHEN (#{builds}) = (#{skipped}) THEN #{STATUSES[:skipped]}
+              WHEN (#{builds}) = (#{success}) THEN #{STATUSES[:success]}
+              WHEN (#{builds}) = (#{created}) THEN #{STATUSES[:created]}
+              WHEN (#{builds}) = (#{success}) + (#{skipped}) THEN #{STATUSES[:success]}
+              WHEN (#{builds}) = (#{success}) + (#{skipped}) + (#{canceled}) THEN #{STATUSES[:canceled]}
+              WHEN (#{builds}) = (#{created}) + (#{skipped}) + (#{pending}) THEN #{STATUSES[:pending]}
+              WHEN (#{running}) + (#{pending}) > 0 THEN #{STATUSES[:running]}
+              WHEN (#{manual}) > 0 THEN #{STATUSES[:manual]}
+              WHEN (#{created}) > 0 THEN #{STATUSES[:running]}
+              ELSE #{STATUSES[:failed]}
+            END)
+          SQL
+        end
+      end
     
-    # Helper method for Windows
-def dst_active?
-  config = Jekyll.configuration('quiet' => true)
-  ENV['TZ'] = config['timezone']
-  dst = Time.now.isdst
+            MergeRequest
+          .where(id: start_id..stop_id)
+          .where(latest_merge_request_diff_id: nil)
+          .each_batch(of: BATCH_SIZE) do |relation|
     
-        # All the entries in this collection.
+          def link_url
+        raise NotImplementedError
+      end
+    end
+  end
+end
+
+    
+            def entity
+          'pipeline'
+        end
+    
+        # The environment that this machine is a part of.
     #
-    # Returns an Array of file paths to the documents in this collection
-    #   relative to the collection's directory
-    def entries
-      return [] unless exists?
-      @entries ||=
-        Utils.safe_glob(collection_dir, ['**', '*'], File::FNM_DOTMATCH).map do |entry|
-          entry['#{collection_dir}/'] = ''
-          entry
+    # @return [Environment]
+    attr_reader :env
+    
+            # Upload a file to the remote machine.
+        #
+        # @param [String] from Path of the file locally to upload.
+        # @param [String] to Path of where to save the file on the remote
+        #   machine.
+        def upload(from, to)
         end
-    end
     
-          # Run Site#process and catch errors
-      #
-      # site - the Jekyll::Site object
-      #
-      # Returns nothing
-      def process_site(site)
-        site.process
-      rescue Jekyll::Errors::FatalException => e
-        Jekyll.logger.error 'ERROR:', 'YOUR SITE COULD NOT BE BUILT:'
-        Jekyll.logger.error '', '------------------------------------'
-        Jekyll.logger.error '', e.message
-        exit(1)
-      end
-    
-    module Jekyll
-  module Commands
-    class Build < Command
-      class << self
-        # Create the Mercenary command for the Jekyll CLI for this Command
-        def init_with_program(prog)
-          prog.command(:build) do |c|
-            c.syntax      'build [options]'
-            c.description 'Build your site'
-            c.alias :b
-    
-        def absolute_url_string?(str)
-      str =~ SCHEME_RGX
-    end
-    
-        def add(path, content)
-      @pages[path] = content
-    end
-    
-        def on_response(&block)
-      @on_response ||= []
-      @on_response << block if block
-      @on_response
-    end
-    
-        def error?
-      code == 0 || code != 404 && code != 403 && code >= 400 && code <= 599
-    end
-    
-          def process_response(response)
-        original_scheme = self.base_url.scheme
-        original_host = self.base_url.host
-        original_path = self.base_url.path
-    
-        def normalized_path
-      path == '' ? '/' : path
-    end
-    
-          private
-    
-          it 'raises KeyError when there is no matching key' do
-        -> () {
-          format('%{foo}', {})
-        }.should raise_error(KeyError)
-      end
-    
-    describe 'Kernel.sleep' do
-  it 'needs to be reviewed for spec completeness'
-end
-
-    
-      after :each do
-    ENV.delete('TEST_SH_EXPANSION')
-  end
-    
-      # Fetches cached values in persistent storage according to the type of data
-  # stored
-  #
-  # @abstract
-  def fetch_type(*)
-    raise NotImplementedError
-  end
-    
-          def self.uninstall_dsl_key
-        dsl_key.to_s.prepend('uninstall_').to_sym
-      end
-    
-        def graph_dependencies(cask = self.cask, acc = TopologicalHash.new)
-      return acc if acc.key?(cask)
-      deps = cask.depends_on.cask.map(&CaskLoader.public_method(:load))
-      acc[cask] = deps
-      deps.each do |dep|
-        graph_dependencies(dep, acc)
-      end
-      acc
-    end
-    
-            if tap_path = CaskLoader.tap_paths(token).first
-          CaskLoader::FromTapPathLoader.new(tap_path).load
-        elsif caskroom_path = Pathname.glob(path.join('.metadata/*/*/*/*.rb')).first
-          CaskLoader::FromPathLoader.new(caskroom_path).load
-        else
-          CaskLoader.load(token)
+              result
         end
-      end
-    end
+    
+      def collection_presenter
+    ActivityPub::CollectionPresenter.new(
+      id: account_collection_url(@account, params[:id]),
+      type: :ordered,
+      size: @size,
+      items: @statuses
+    )
   end
 end
 
     
-      failure_message_for_should do |actual|
-    'expected #{actual.inspect} to have path in #{expected.inspect} but was #{actual.current_path.inspect}'
-  end
-  failure_message_for_should_not do |actual|
-    'expected #{actual.inspect} to not have path in #{expected.inspect} but it had'
-  end
-end
-    
-    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
-    
-      describe '#new' do
-    before do
-      sign_in alice, scope: :user
+      def upgrade_account
+    if signed_request_account.ostatus?
+      signed_request_account.update(last_webfingered_at: nil)
+      ResolveAccountWorker.perform_async(signed_request_account.acct)
     end
     
-          def authorize_for_order
-        @order = Spree::Order.find_by(number: order_id)
-        authorize! :read, @order, order_token
-      end
+        def destroy
+      authorize @custom_emoji, :destroy?
+      @custom_emoji.destroy!
+      log_action :destroy, @custom_emoji
+      flash[:notice] = I18n.t('admin.custom_emojis.destroyed_msg')
+      redirect_to admin_custom_emojis_path(page: params[:page], **@filter_params)
     end
-  end
-end
-
     
-            def find_address
-          if @order.bill_address_id == params[:id].to_i
-            @order.bill_address
-          elsif @order.ship_address_id == params[:id].to_i
-            @order.ship_address
-          else
-            raise CanCan::AccessDenied
-          end
+              redirect_to admin_reports_path, notice: I18n.t('admin.reports.resolved_msg')
+          return
         end
-      end
-    end
+    
+      def status_finder
+    StatusFinder.new(params[:url])
+  end
+    
+      def hub_topic_uri
+    @_hub_topic_uri ||= Addressable::URI.parse(hub_topic).normalize
+  end
+    
+      def set_account
+    @account = Account.find(params[:id])
+  end
+    
+      def set_account
+    @account = Account.find(params[:id])
   end
 end
 
     
-            def log_state_changes
-          if @order.previous_changes[:state]
-            @order.log_state_changes(
-              state_name: 'order',
-              old_state: @order.previous_changes[:state].first,
-              new_state: @order.previous_changes[:state].last
-            )
-          end
-        end
-    
-            def update
-          @image = scope.images.accessible_by(current_ability, :update).find(params[:id])
-          if @image.update_attributes(image_params)
-            respond_with(@image, default_template: :show)
-          else
-            invalid_resource!(@image)
-          end
-        end
-    
-            def prepare_event
-          return unless @event = params[:fire]
-    
-              if @product_property.update_attributes(product_property_params)
-            respond_with(@product_property, status: 200, default_template: :show)
-          else
-            invalid_resource!(@product_property)
-          end
-        end
-    
-              params.require(:product).permit(
-            variants_key => [permitted_variant_attributes, :id]
-          ).delete(variants_key) || []
-        end
-    
-            def show
-          authorize! :admin, ReturnAuthorization
-          @return_authorization = order.return_authorizations.accessible_by(current_ability, :read).find(params[:id])
-          respond_with(@return_authorization)
-        end
-    
-              @line_item = Spree::Cart::AddItem.call(order: @order,
-                                                 variant: variant,
-                                                 quantity: quantity,
-                                                 options: { shipment: @shipment }).value
-    
-        def debug?
-      ENV['DEBUG']
-    end
-    
-      def validate_target_file
-    if File.exist?(target_file)
-      if  delete_target_file?
-        File.delete(target_file)
-      else
-        signal_error('Package creation cancelled, a previously generated package exist at location: #{target_file}, move this file to safe place and run the command again')
-      end
-    end
-  end
-    
-        def execute
-      raise PluginManager::FileNotFoundError, 'Can't file local file #{local_file}' unless ::File.exist?(local_file)
-      raise PluginManager::InvalidPackError, 'Invalid format, the pack must be in zip format' unless valid_format?(local_file)
-    
-    describe LogStash::Config::PipelineConfig do
-  let(:source) { LogStash::Config::Source::Local }
-  let(:pipeline_id) { :main }
-  let(:ordered_config_parts) do
-    [
-      org.logstash.common.SourceWithMetadata.new('file', '/tmp/1', 0, 0, 'input { generator1 }'),
-      org.logstash.common.SourceWithMetadata.new('file', '/tmp/2', 0, 0,  'input { generator2 }'),
-      org.logstash.common.SourceWithMetadata.new('file', '/tmp/3', 0, 0, 'input { generator3 }'),
-      org.logstash.common.SourceWithMetadata.new('file', '/tmp/4', 0, 0, 'input { generator4 }'),
-      org.logstash.common.SourceWithMetadata.new('file', '/tmp/5', 0, 0, 'input { generator5 }'),
-      org.logstash.common.SourceWithMetadata.new('file', '/tmp/6', 0, 0, 'input { generator6 }'),
-      org.logstash.common.SourceWithMetadata.new('string', 'config_string', 0, 0, 'input { generator1 }'),
-    ]
-  end
-    
-              it 'successfully install the plugin' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install #{gem_path_on_vagrant}')
-            expect(command).to install_successfully
-            expect(logstash).to have_installed?('logstash-filter-dns')
-          end
-        end
-    
-          it 'display a list of installed plugins' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list --installed')
-        expect(result.stdout.split('\n').size).to be > 1
-      end
+          DIRECTIVES = %i(base_uri child_src connect_src default_src
+                      font_src form_action frame_ancestors frame_src
+                      img_src manifest_src media_src object_src
+                      plugin_types referrer reflected_xss report_to
+                      report_uri require_sri_for sandbox script_src
+                      style_src worker_src).freeze
