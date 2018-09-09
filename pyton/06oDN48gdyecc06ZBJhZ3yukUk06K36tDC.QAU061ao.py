493448ea03fc3fa56ababdcdb7758802e2a95700
SUNsung <weatherius@gmail.com>
2018-09-09 03:25:36 +0300

    Rewrite [06oDN48gdyecc06ZBJhZ3yukUk06K36tDC.QAU061ao.py] File in: 2018-09-09 03:25:35

--- pyton/06oDN48gdyecc06ZBJhZ3yukUk06K36tDC.QAU061ao.py
+++ pyton/06oDN48gdyecc06ZBJhZ3yukUk06K36tDC.QAU061ao.py
@@ -1,147 +1,120 @@
 
         
         
-class BuildHTTPRequestHandler(compat_http_server.BaseHTTPRequestHandler):
-    actionDict = {'build': Builder, 'download': Builder}  # They're the same, no more caching.
+class HasKey(PostgresSimpleLookup):
+    lookup_name = 'has_key'
+    operator = '?'
+    prepare_rhs = False
     
-    filenames = {
-    'bin': 'youtube-dl',
-    'exe': 'youtube-dl.exe',
-    'tar': 'youtube-dl-%s.tar.gz' % version}
-build_dir = os.path.join('..', '..', 'build', version)
-for key, filename in filenames.items():
-    url = 'https://yt-dl.org/downloads/%s/%s' % (version, filename)
-    fn = os.path.join(build_dir, filename)
-    with open(fn, 'rb') as f:
-        data = f.read()
-    if not data:
-        raise ValueError('File %s is empty!' % fn)
-    sha256sum = hashlib.sha256(data).hexdigest()
-    new_version[key] = (url, sha256sum)
+        def load(self):
+        try:
+            session_data = self._cache.get(self.cache_key)
+        except Exception:
+            # Some backends (e.g. memcache) raise an exception on invalid
+            # cache keys. If this happens, reset the session. See #17810.
+            session_data = None
+        if session_data is not None:
+            return session_data
+        self._session_key = None
+        return {}
     
-    with io.open('update/releases.atom', 'w', encoding='utf-8') as atom_file:
-    atom_file.write(atom_template)
-
+        def exists(self, session_key):
+        return session_key and (self.cache_key_prefix + session_key) in self._cache or super().exists(session_key)
+    
+        req_protocol = request.scheme
+    req_site = get_current_site(request)
+    
+            print('Training %s ... ' % name, end='')
+        t0 = time()
+        clf.fit(X_train, y_train)
+        train_time[name] = time() - t0
+        t0 = time()
+        y_pred = clf.predict(X_test)
+        test_time[name] = time() - t0
+        accuracy[name] = accuracy_score(y_test, y_pred)
+        print('done')
+    
+        it = 0
+    results = defaultdict(lambda: [])
+    chunk = 100
+    
+        for i, kk in enumerate(krange):
+        print('k = %i (%i out of %i)' % (kk, i + 1, len(krange)))
+        for algorithm in algorithms:
+            nbrs = neighbors.NearestNeighbors(n_neighbors=kk,
+                                              algorithm=algorithm,
+                                              leaf_size=leaf_size)
+            t0 = time()
+            nbrs.fit(X)
+            t1 = time()
+            nbrs.kneighbors(X)
+            t2 = time()
+    
+    for i, n in enumerate(n_samples):
+    for j, p in enumerate(n_features):
+        X = np.random.normal(size=(n, p))
+        t0 = time.time()
+        ward.fit(X)
+        scikits_time[j, i] = time.time() - t0
+        t0 = time.time()
+        hierarchy.ward(X)
+        scipy_time[j, i] = time.time() - t0
+    
+    print('='*80 + '\n#' + '    Text vectorizers benchmark' + '\n' + '='*80 + '\n')
+print('Using a subset of the 20 newsrgoups dataset ({} documents).'
+      .format(len(text)))
+print('This benchmarks runs in ~20 min ...')
+    
+        def test_class(self):
+        class C: 'New-style class'
+        c = C()
+    
+        def persistent_id(self, obj):
+        # Instead of pickling MemoRecord as a regular class instance, we emit a
+        # persistent ID.
+        if isinstance(obj, MemoRecord):
+            # Here, our persistent ID is simply a tuple, containing a tag and a
+            # key, which refers to a specific record in the database.
+            return ('MemoRecord', obj.key)
+        else:
+            # If obj does not have a persistent ID, return None. This means obj
+            # needs to be pickled as usual.
+            return None
     
     
 def main():
-    with open('supportedsites.html.in', 'r', encoding='utf-8') as tmplf:
-        template = tmplf.read()
+    parser = ArgumentParser(description='''\
+Send the contents of a directory as a MIME message.
+Unless the -o option is given, the email is sent by forwarding to your local
+SMTP server, which then does the normal delivery process.  Your local machine
+must be running an SMTP server.
+''')
+    parser.add_argument('-d', '--directory',
+                        help='''Mail the contents of the specified directory,
+                        otherwise use the current directory.  Only the regular
+                        files in the directory are sent, and we don't recurse to
+                        subdirectories.''')
+    parser.add_argument('-o', '--output',
+                        metavar='FILE',
+                        help='''Print the composed message to FILE instead of
+                        sending the message to the SMTP server.''')
+    parser.add_argument('-s', '--sender', required=True,
+                        help='The value of the From: header (required)')
+    parser.add_argument('-r', '--recipient', required=True,
+                        action='append', metavar='RECIPIENT',
+                        default=[], dest='recipients',
+                        help='A To: header value (at least one required)')
+    args = parser.parse_args()
+    directory = args.directory
+    if not directory:
+        directory = '.'
+    # Create the message
+    msg = EmailMessage()
+    msg['Subject'] = 'Contents of directory %s' % os.path.abspath(directory)
+    msg['To'] = ', '.join(args.recipients)
+    msg['From'] = args.sender
+    msg.preamble = 'You will not see this in a MIME-aware mail reader.\n'
     
-    if isinstance(helptext, bytes):
-    helptext = helptext.decode('utf-8')
-    
-    
-total_bytes = 0
-    
-        def test_youtube_playlist_matching(self):
-        assertPlaylist = lambda url: self.assertMatch(url, ['youtube:playlist'])
-        assertPlaylist('ECUl4u3cNGP61MdtwGTqZA0MreSaDybji8')
-        assertPlaylist('UUBABnxM4Ar9ten8Mdjj1j0Q')  # 585
-        assertPlaylist('PL63F0C78739B09958')
-        assertPlaylist('https://www.youtube.com/playlist?list=UUBABnxM4Ar9ten8Mdjj1j0Q')
-        assertPlaylist('https://www.youtube.com/course?list=ECUl4u3cNGP61MdtwGTqZA0MreSaDybji8')
-        assertPlaylist('https://www.youtube.com/playlist?list=PLwP_SiAcdui0KVebT0mU9Apz359a4ubsC')
-        assertPlaylist('https://www.youtube.com/watch?v=AV6J6_AeFEQ&playnext=1&list=PL4023E734DA416012')  # 668
-        self.assertFalse('youtube:playlist' in self.matching_ies('PLtS2H6bU1M'))
-        # Top tracks
-        assertPlaylist('https://www.youtube.com/playlist?list=MCUS.20142101')
-    
-    
-def skip_internal(app, what, name, obj, skip, options):
-    docstring = inspect.getdoc(obj) or ''
-    
-        if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        error = None
-    
-        # test that the user was inserted into the database
-    with app.app_context():
-        assert get_db().execute(
-            'select * from user where username = 'a'',
-        ).fetchone() is not None
-    
-        with pytest.raises(sqlite3.ProgrammingError) as e:
-        db.execute('SELECT 1')
-    
-        def implements_to_string(cls):
-        cls.__unicode__ = cls.__str__
-        cls.__str__ = lambda x: x.__unicode__().encode('utf-8')
-        return cls
-    
-    
-def _dump_loader_info(loader):
-    yield 'class: %s.%s' % (type(loader).__module__, type(loader).__name__)
-    for key, value in sorted(loader.__dict__.items()):
-        if key.startswith('_'):
-            continue
-        if isinstance(value, (tuple, list)):
-            if not all(isinstance(x, (str, text_type)) for x in value):
-                continue
-            yield '%s:' % key
-            for item in value:
-                yield '  - %s' % item
-            continue
-        elif not isinstance(value, (str, text_type, int, float, bool)):
-            continue
-        yield '%s: %r' % (key, value)
-    
-            self.charPositionInLine = None
-    
-    
-class ANTLRStringStream(CharStream):
-    '''
-    @brief CharStream that pull data from a unicode string.
-    
-    A pretty quick CharStream that pulls all data from an array
-    directly.  Every method call counts in the lexer.
-    
-    
-    def getChannel(self):
-        return self.channel
-    
-    def setChannel(self, channel):
-        self.channel = channel
-    
-    
-    
-@asyncio.coroutine
-def async_setup(hass):
-    '''Set up the Hassbian config.'''
-    hass.http.register_view(CheckConfigView)
-    return True
-    
-            states = dict(state.attributes)
-        metric = '{}.{}'.format(prefix, state.domain)
-        tags = ['entity:{}'.format(state.entity_id)]
-    
-                    else:
-                    # If this light didn't happen to be turned on yet so
-                    # will all the following then, break.
-                    break
-    
-            devices = {}
-        for lease in leases_result:
-            match = _LEASES_REGEX.search(lease.decode('utf-8'))
-            if match is not None:
-                devices[match.group('ip')] = {
-                    'ip': match.group('ip'),
-                    'mac': match.group('mac').upper(),
-                    'timevalid': int(match.group('timevalid'))
-                    }
-        return devices
-
-    
-    REQUIREMENTS = ['pybbox==0.0.5-alpha']
-    
-        def random_see(dev_id, name):
-        '''Randomize a sighting.'''
-        see(
-            dev_id=dev_id,
-            host_name=name,
-            gps=(hass.config.latitude + offset(),
-                 hass.config.longitude + offset()),
-            gps_accuracy=random.randrange(50, 150),
-            battery=random.randrange(10, 90)
-        )
+    ans = input('View full message?')
+if ans.lower()[0] == 'n':
+    sys.exit()
