4d06773f41f3a33562ddac0a9a87b936a84102c5
SUNsung <weatherius@gmail.com>
2018-09-09 07:15:21 +0300

    Rewrite [35Lzod5DjJSng35q9qepcUwIo354mILYGBAktQ35TXP.py] File in: 2018-09-09 07:15:20

--- pyton/35Lzod5DjJSng35q9qepcUwIo354mILYGBAktQ35TXP.py
+++ pyton/35Lzod5DjJSng35q9qepcUwIo354mILYGBAktQ35TXP.py
@@ -1,50 +1,64 @@
 
         
-            with open('README.md', 'w+') as sorted_file:
-        # Then all of the blocks are sorted individually
-        blocks = [''.join(sorted(block, key=lambda s: s.lower())) for block in blocks]
-        # And the result is written back to README.md
-        sorted_file.write(''.join(blocks))
-    
-    import os
-import sys
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-    
-    versions_info['signature'] = signature
-with open('update/versions.json', 'w') as versionsf:
-    json.dump(versions_info, versionsf, indent=4, sort_keys=True)
+        
+def init_app(app):
+    '''Register database functions with the Flask app. This is called by
+    the application factory.
+    '''
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
 
     
+        def reraise(tp, value, tb=None):
+        if value.__traceback__ is not tb:
+            raise value.with_traceback(tb)
+        raise value
     
-def main():
-    with open('supportedsites.html.in', 'r', encoding='utf-8') as tmplf:
-        template = tmplf.read()
+            .. versionadded:: 0.11
+        '''
+        self.record_once(lambda s: s.app._register_error_handler(
+            self.name, code_or_exception, f))
+
     
-    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    This typically means that you attempted to use functionality that needed
+an active HTTP request.  Consult the documentation on testing for
+information about how to avoid this problem.\
+'''
+_app_ctx_err_msg = '''\
+Working outside of application context.
     
-        fileopts = []
-    for opt in opts_file:
-        if opt._short_opts:
-            fileopts.extend(opt._short_opts)
-        if opt._long_opts:
-            fileopts.extend(opt._long_opts)
+    # The namespace for code signals.  If you are not Flask code, do
+# not put signals in here.  Create your own namespace instead.
+_signals = Namespace()
     
-        proc.sendline(u'ehco test')
+            # Insert the import statement to setup.py if not present
+        with open(setup_path, 'a+') as setup:
+            setup.seek(0)
+            setup_content = setup.read()
+            if not 'import fastentrypoints' in setup_content:
+                setup.seek(0)
+                setup.truncate()
+                setup.write('import fastentrypoints\n' + setup_content)
     
     
-@pytest.fixture(params=containers)
-def proc(request, spawnu, TIMEOUT):
-    proc = spawnu(*request.param)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(u'thefuck --alias > ~/.config/fish/config.fish')
-    proc.sendline(u'fish')
-    return proc
+@pytest.fixture(autouse=True)
+def shell(mocker):
+    shell = mocker.patch('thefuck.entrypoints.not_configured.shell',
+                         new_callable=MagicMock)
+    shell.get_history.return_value = []
+    shell.how_to_configure.return_value = ShellConfiguration(
+        content='eval $(thefuck --alias)',
+        path='/tmp/.bashrc',
+        reload='bash',
+        can_configure_automatically=True)
+    return shell
+    
+        assert proc.expect([TIMEOUT, u'Aborted'])
     
     
 @pytest.mark.functional
 def test_how_to_configure_alias(proc, TIMEOUT):
-    proc.sendline(u'unfunction fuck')
+    proc.sendline('unset -f fuck')
     how_to_configure(proc, TIMEOUT)
 
     
@@ -57,121 +71,145 @@ def test_get_new_command(before, after):
     assert get_new_command(command) == after
 
     
-    no_such_subcommand = '''error: no such subcommand
     
-    ESTIMATORS = {
-    'dummy': DummyClassifier(),
-    'random_forest': RandomForestClassifier(n_estimators=100,
-                                            max_features='sqrt',
-                                            min_samples_split=10),
-    'extra_trees': ExtraTreesClassifier(n_estimators=100,
-                                        max_features='sqrt',
-                                        min_samples_split=10),
-    'logistic_regression': LogisticRegression(),
-    'naive_bayes': MultinomialNB(),
-    'adaboost': AdaBoostClassifier(n_estimators=10),
-}
+class CryptoExtension(CreateExtension):
     
     
-def bench_isotonic_regression(Y):
+class RedirectFallbackMiddleware(MiddlewareMixin):
+    # Defined as class-level attributes to be subclassing-friendly.
+    response_gone_class = HttpResponseGone
+    response_redirect_class = HttpResponsePermanentRedirect
+    
+    
+class SessionStore(SessionBase):
     '''
-    Runs a single iteration of isotonic regression on the input data,
-    and reports the total time taken (in seconds).
+    A cache-based session store.
     '''
-    gc.collect()
+    cache_key_prefix = KEY_PREFIX
     
-    from time import time
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.neighbors import LocalOutlierFactor
-from sklearn.metrics import roc_curve, auc
-from sklearn.datasets import fetch_kddcup99, fetch_covtype, fetch_mldata
-from sklearn.preprocessing import LabelBinarizer
+        def items(self):
+        return []
     
-    for i, n in enumerate(n_samples):
-    for j, p in enumerate(n_features):
-        X = np.random.normal(size=(n, p))
-        t0 = time.time()
-        ward.fit(X)
-        scikits_time[j, i] = time.time() - t0
-        t0 = time.time()
-        hierarchy.ward(X)
-        scipy_time[j, i] = time.time() - t0
+        if section is not None:
+        if section not in sitemaps:
+            raise Http404('No sitemap available for section: %r' % section)
+        maps = [sitemaps[section]]
+    else:
+        maps = sitemaps.values()
+    page = request.GET.get('p', 1)
     
-        url_fmt is along the lines of ('https://github.com/USER/PROJECT/'
-                                   'blob/{revision}/{package}/'
-                                   '{path}#L{lineno}')
-    '''
-    revision = _get_git_revision()
-    return partial(_linkcode_resolve, revision=revision, package=package,
-                   url_fmt=url_fmt)
+    # Check imported dependencies for compatibility.
+try:
+    check_compatibility(urllib3.__version__, chardet.__version__)
+except (AssertionError, ValueError):
+    warnings.warn('urllib3 ({0}) or chardet ({1}) doesn't match a supported '
+                  'version!'.format(urllib3.__version__, chardet.__version__),
+                  RequestsDependencyWarning)
+    
+        builtin_str = str
+    bytes = str
+    str = unicode
+    basestring = basestring
+    numeric_types = (int, long, float)
+    integer_types = (int, long)
+    
+        # OPENSSL_VERSION_NUMBER doesn't exist in the Python 2.6 ssl module.
+    system_ssl = getattr(ssl, 'OPENSSL_VERSION_NUMBER', None)
+    system_ssl_info = {
+        'version': '%x' % system_ssl if system_ssl is not None else ''
+    }
+    
+        def redirect_resp_handler(sock):
+        consume_socket_content(sock, timeout=0.5)
+        location = u'//{0}:{1}/{2}'.format(host, port, path)
+        sock.send(
+            b'HTTP/1.1 301 Moved Permanently\r\n'
+            b'Content-Length: 0\r\n'
+            b'Location: ' + location.encode('utf8') + b'\r\n'
+            b'\r\n'
+        )
+        redirect_request.append(consume_socket_content(sock, timeout=0.5))
+        sock.send(b'HTTP/1.1 200 OK\r\n\r\n')
+    
+    import pytest
+    
+        def __enter__(self):
+        self.start()
+        self.ready_event.wait(self.WAIT_EVENT_TIMEOUT)
+        return self.host, self.port
+    
+            :param request: The :class:`PreparedRequest <PreparedRequest>` being sent.
+        :param proxies: A dictionary of schemes or schemes and hosts to proxy URLs.
+        :rtype: str
+        '''
+        proxy = select_proxy(request.url, proxies)
+        scheme = urlparse(request.url).scheme
+    
+        result.pop('ResponseMetadata', None)
+    
+            if tags_to_add:
+            try:
+                connection.create_tags(Resources=[image_id], Tags=ansible_dict_to_boto3_tag_list(tags_to_add))
+                changed = True
+            except (botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:
+                module.fail_json_aws(e, msg='Error updating tags')
+    
+        module = AnsibleAWSModule(argument_spec=argument_spec,
+                              mutually_exclusive=[['customer_gateway_ids', 'filters']],
+                              supports_check_mode=True)
+    
+        raise EIPException('could not find instance' + device_id)
+    
+    
+if __name__ == '__main__':
+    main()
 
     
-    # A tool to help evaluate the teredo servers.
-# Thanks XndroidDev
-# Author: SeaHOH <seahoh@gmail.com>
-# Compatible: Python 2.7 & 3.4 & 3.5 & 3.6
-# References:
-#   https://tools.ietf.org/html/rfc4380 5.1 5.2
-#   https://tools.ietf.org/html/rfc4861 4.1 4.2
-#   https://tools.ietf.org/html/rfc2460 8.1
-#   https://github.com/XndroidDev/Xndroid/blob/master/fqrouter/manager/teredo.py
-    
-        def get(self):
-        n = random.randint(2, 3)
-        ws = []
-        for i in range(0, n):
-            w = self.slice.get()
-            ws.append(w)
+        except WaiterError as we:
+        module.fail_json(msg='An error occurred waiting for the snapshot to become available. (%s)' % str(we), exception=traceback.format_exc())
+    except ClientError as ce:
+        module.fail_json(msg=str(ce), exception=traceback.format_exc(), **camel_dict_to_snake_dict(ce.response))
     
     
-    def __str__(self):
-        exp = ', expected %s' % self.expecting
-        if self.expecting == INVALID_TOKEN_TYPE:
-            exp = ''
-    
-      return IsBlockInNameSpace(nesting_state, is_forward_declaration)
-    
-      text_after_cursor = vimsupport.TextAfterCursor()
-  if not text_after_cursor:
-    return candidates
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    import time
-from threading import Thread
-from ycm.client.base_request import BaseRequest
+def Main():
+  build_file = p.join( DIR_OF_THIS_SCRIPT, 'third_party', 'ycmd', 'build.py' )
     
     
-  def IsAllowed( self, diagnostic ):
-    # NOTE: a diagnostic IsAllowed() ONLY if NO filters match it
-    for filterMatches in self._filters:
-      if filterMatches( diagnostic ):
-        return False
+class ShutdownRequest( BaseRequest ):
+  def __init__( self ):
+    super( ShutdownRequest, self ).__init__()
+    
+      raise RuntimeError( 'Cannot find Python 2.7 or 3.4+. '
+                      'Set the 'g:ycm_server_python_interpreter' option '
+                      'to a Python interpreter path.' )
     
     
-  def Just_Detailed_Info_test( self ):
-    self._Check( 9999999999, {
-      'insertion_text':  'INSERTION TEXT',
-      'menu_text':       'MENU TEXT',
-      'extra_menu_info': 'EXTRA MENU INFO',
-      'kind':            'K',
-      'detailed_info':   'DETAILED INFO',
-    }, {
-      'word'     : 'INSERTION TEXT',
-      'abbr'     : 'MENU TEXT',
-      'menu'     : 'EXTRA MENU INFO',
-      'kind'     : 'k',
-      'info'     : 'DETAILED INFO',
-      'dup'      : 1,
-      'empty'    : 1,
-      'user_data': '9999999999',
-    } )
+def OverlapLength_EmptyInput_test():
+  eq_( 0, base.OverlapLength( '', 'goobar' ) )
+  eq_( 0, base.OverlapLength( 'foobar', '' ) )
+  eq_( 0, base.OverlapLength( '', '' ) )
     
-    from ycm.tests.test_utils import MockVimModule
-MockVimModule()
+    
+# This file provides an UnsafeThreadPoolExecutor, which operates exactly like
+# the upstream Python version of ThreadPoolExecutor with one exception: it
+# doesn't wait for worker threads to finish before shutting down the Python
+# interpreter.
+#
+# This is dangerous for many workloads, but fine for some (like when threads
+# only send network requests). The YCM workload is one of those workloads where
+# it's safe (the aforementioned network requests case).
+    
+    # The model was trained in a way that faces with a distance of 0.6 or less should be a match. But if you want to
+# be more strict, you can look for a smaller face distance. For example, using a 0.55 cutoff would reduce false
+# positive matches at the risk of more false negatives.
+    
+    face_detector = dlib.get_frontal_face_detector()
+    
+            if len(encodings) == 0:
+            click.echo('WARNING: No faces found in {}. Ignoring file.'.format(file))
+        else:
+            known_names.append(basename)
+            known_face_encodings.append(encodings[0])
+    
+    Tests for `face_recognition` module.
+'''
