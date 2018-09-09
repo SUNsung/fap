5ceb29637504705c6f8c10ffd1b3c247c4ef8808
SUNsung <weatherius@gmail.com>
2018-09-09 14:20:25 +0300

    Rewrite [51RLXBNTKJ74A51cpD16pBf3Os51LVgvWqTbjWQ51Zxb.cpp] File in: 2018-09-09 14:20:24

--- cccccc/51RLXBNTKJ74A51cpD16pBf3Os51LVgvWqTbjWQ51Zxb.cpp
+++ cccccc/51RLXBNTKJ74A51cpD16pBf3Os51LVgvWqTbjWQ51Zxb.cpp
@@ -1,205 +1,206 @@
 
         
-            // Define a new address (which should add to the address book successfully).
-    QString new_address;
+          size_t numTrailingObjects(OverloadToken<ProtocolConformanceRef>) const {
+    return numConformanceRequirements;
+  }
     
-    /**
- * Changelog:
- * - March 2013, Diederik Huys:    original version
- * - November 2014, Pieter Wuille: updated to use Peter Dettman's parallel multiplication algorithm
- * - December 2014, Pieter Wuille: converted from YASM to GCC inline assembly
+    
+    {  }
+  bool visitSILTokenType(SILTokenType *ty) { return false; }
+    
+        if (info.bytes == 0)
+      return false;
+    
+    DIRECTIONAL_PREPOSITION(above)
+DIRECTIONAL_PREPOSITION(after)
+DIRECTIONAL_PREPOSITION(along)
+DIRECTIONAL_PREPOSITION(alongside)
+DIRECTIONAL_PREPOSITION(as)
+DIRECTIONAL_PREPOSITION(at)
+DIRECTIONAL_PREPOSITION(before)
+DIRECTIONAL_PREPOSITION(below)
+DIRECTIONAL_PREPOSITION(by)
+DIRECTIONAL_PREPOSITION(following)
+DIRECTIONAL_PREPOSITION(for)
+DIRECTIONAL_PREPOSITION(from)
+DIRECTIONAL_PREPOSITION(given)
+DIRECTIONAL_PREPOSITION(in)
+DIRECTIONAL_PREPOSITION(including)
+DIRECTIONAL_PREPOSITION(inside)
+DIRECTIONAL_PREPOSITION(into)
+DIRECTIONAL_PREPOSITION(matching)
+DIRECTIONAL_PREPOSITION(of)
+DIRECTIONAL_PREPOSITION(on)
+DIRECTIONAL_PREPOSITION(passing)
+DIRECTIONAL_PREPOSITION(preceding)
+DIRECTIONAL_PREPOSITION(since)
+DIRECTIONAL_PREPOSITION(to)
+DIRECTIONAL_PREPOSITION(until)
+DIRECTIONAL_PREPOSITION(using)
+DIRECTIONAL_PREPOSITION(via)
+DIRECTIONAL_PREPOSITION(when)
+PREPOSITION(with)
+DIRECTIONAL_PREPOSITION(within)
+    
+    swift::unicode::GraphemeClusterBreakProperty
+swift::unicode::getGraphemeClusterBreakProperty(uint32_t C) {
+  // FIXME: replace linear search with a trie lookup.
+    }
+    
+    protected:
+    bool eventFilter(QObject *object, QEvent *event);
+    
+    /** Dialog showing transaction details. */
+class TransactionDescDialog : public QDialog
+{
+    Q_OBJECT
+    }
+    
+    #include <script/script.h>
+    
+    
+    {        secp256k1_sha256_initialize(&sha);
+        secp256k1_sha256_write(&sha, y, sizeof(y));
+        secp256k1_sha256_write(&sha, x, sizeof(x));
+        secp256k1_sha256_finalize(&sha, result);
+        ret = 1;
+    }
+    
+    int secp256k1_ecdsa_sign_recoverable(const secp256k1_context* ctx, secp256k1_ecdsa_recoverable_signature *signature, const unsigned char *msg32, const unsigned char *seckey, secp256k1_nonce_function noncefp, const void* noncedata) {
+    secp256k1_scalar r, s;
+    secp256k1_scalar sec, non, msg;
+    int recid;
+    int ret = 0;
+    int overflow = 0;
+    VERIFY_CHECK(ctx != NULL);
+    ARG_CHECK(secp256k1_ecmult_gen_context_is_built(&ctx->ecmult_gen_ctx));
+    ARG_CHECK(msg32 != NULL);
+    ARG_CHECK(signature != NULL);
+    ARG_CHECK(seckey != NULL);
+    if (noncefp == NULL) {
+        noncefp = secp256k1_nonce_function_default;
+    }
+    }
+    
+    BOOST_AUTO_TEST_SUITE_END()
+
+    
+    /* Equality between doubles is imprecise. Comparison should be done
+ * with a small threshold of tolerance, rather than exact equality.
  */
-    
-        /* Create random point */
-    random_scalar_order(&rand);
-    secp256k1_scalar_get_b32(s_rand, &rand);
-    CHECK(secp256k1_ec_pubkey_create(ctx, &point, s_rand) == 1);
-    
-        // Escapes
-    CheckParseTorReplyMapping(
-        'Foo=\'Bar\\ Baz\'', {
-            {'Foo', 'Bar Baz'},
-        });
-    CheckParseTorReplyMapping(
-        'Foo=\'Bar\\Baz\'', {
-            {'Foo', 'BarBaz'},
-        });
-    CheckParseTorReplyMapping(
-        'Foo=\'Bar\\@Baz\'', {
-            {'Foo', 'Bar@Baz'},
-        });
-    CheckParseTorReplyMapping(
-        'Foo=\'Bar\\\'Baz\' Spam=\'\\\'Eggs\\\'\'', {
-            {'Foo', 'Bar\'Baz'},
-            {'Spam', '\'Eggs\''},
-        });
-    CheckParseTorReplyMapping(
-        'Foo=\'Bar\\\\Baz\'', {
-            {'Foo', 'Bar\\Baz'},
-        });
-    
-      int timeout = -1;
-  std::vector<int> to_add;
-  std::vector<int> to_remove;
-  for (;;) {
-    int nevents;
-    if (client_sessions.size() == 0)
-      timeout = SHUTDOWN_TIMEOUT;
-    SYSCHECK(nevents = poll(pollfds.data(), pollfds.size(), timeout));
-    timeout = -1;
-    if (nevents == 0 && client_sessions.size() == 0)
-      break;
-    }
-    
-      template <typename T>
-  bool DoRunWithType() {
-    const auto& data = Input(0);
-    CAFFE_ENFORCE(data.ndim() == 1, 'data should be 1-D.');
-    }
-    
-    template <typename T, class Context>
-class FlexibleTopKGradientOp : public Operator<Context> {
- public:
-  USE_OPERATOR_CONTEXT_FUNCTIONS;
-    }
-    
-    workspace.FeedBlob('X', (np.random.uniform(-10, 10, (5,5))).astype(np.float32))
-print('X before running op:', workspace.FetchBlob('X'))
-workspace.RunOperatorOnce(op)
-print('X after running op:', workspace.FetchBlob('X'))
-    
-    
-    {} // namespace caffe2
-    
-    struct SrcLoc {
-  SrcLoc()
-    : start{0,0}
-    , past{0,0}
-  {}
-    }
-    
-    void Assembler::sth(const Reg64& rt, MemoryRef m) {
-  assertx(Reg64(-1) == m.r.index);  // doesn't support base+index
-  EmitDForm(44, rn(rt), rn(m.r.base), m.r.disp);
+static bool DoubleEquals(double a, double b, double epsilon)
+{
+    return std::abs(a - b) < epsilon;
 }
     
-       void EmitAForm(const uint8_t op,
-                  const RegNumber rt,
-                  const RegNumber ra,
-                  const RegNumber rb,
-                  const RegNumber bc,
-                  const uint16_t xop,
-                  const bool rc = 0) {
-    }
+    // Added for Bitcoin Core
+template<typename... Args>
+std::string format(const std::string &fmt, const Args&... args)
+{
+    std::ostringstream oss;
+    format(oss, fmt.c_str(), args...);
+    return oss.str();
+}
     
-    #include 'hphp/runtime/base/apc-object.h'
-#include 'hphp/runtime/base/apc-array.h'
-#include 'hphp/runtime/base/apc-stats.h'
-#include 'hphp/runtime/base/object-data.h'
-#include 'hphp/runtime/base/type-object.h'
-#include 'hphp/runtime/ext/apc/ext_apc.h'
-#include 'hphp/runtime/base/collections.h'
-#include 'hphp/runtime/ext/collections/ext_collections-map.h'
-#include 'hphp/runtime/ext/collections/ext_collections-set.h'
-#include 'hphp/runtime/ext/collections/ext_collections-vector.h'
-#include 'hphp/runtime/base/data-walker.h'
-    
-      /**
-   * Use the Iterate method for iterating over options that are stored as
-   * objects in runtime options (e.g. FilesMatch). This function iterates over
-   * the settings passed as ini/hdf, calls back to, generally, the constructor
-   * of the object in question.
-   *
-   * Note: For now, we are not `ini_get()` enabling these type of options as
-   * it is not trivial to come up with a non-hacky and workable way to store
-   * the data correctly. Also, as usual, Hdf takes priority.
-   */
-  static void Iterate(std::function<void (const IniSettingMap&,
-                                          const Hdf&,
-                                          const std::string&)> cb,
-                      const IniSettingMap &ini, const Hdf& config,
-                      const std::string &name, const bool prepend_hhvm = true);
-    
-    #include <string>
-#include <utility>
-#include <vector>
-    
-    
-    {}
-    
-    
-    {    if (memcmp(from_start, to_start, seg_len + 1)) {
-      break;
-    }
-    from_start += seg_len + 1;
-    to_start += seg_len + 1;
-  }
-    
-    #include <dmlc/registry.h>
-#include <functional>
-#include <vector>
-#include <utility>
-#include <string>
-#include './base.h'
-#include './data.h'
-#include './tree_model.h'
-#include '../../src/common/host_device_vector.h'
-    
-    /*!
- * \brief an iterator that iterates over a configure file and gets the configures
- */
-class ConfigIterator: public ConfigStreamReader {
- public:
-  /*!
-   * \brief constructor
-   * \param fname name of configure file
-   */
-  explicit ConfigIterator(const char *fname) : ConfigStreamReader(fi_) {
-    fi_.open(fname);
-    if (fi_.fail()) {
-      LOG(FATAL) << 'cannot open file ' << fname;
-    }
-    ConfigReaderBase::Init();
-  }
-  /*! \brief destructor */
-  ~ConfigIterator() {
-    fi_.close();
-  }
-    }
-    
-            static float f = 0.0f;
-        ImGui::Text('Hello, world!');
-        ImGui::SliderFloat('float', &f, 0.0f, 1.0f);
-        ImGui::Text('Application average %.3f ms/frame (%.1f FPS)', 1000.0f / io.Framerate, io.Framerate);
-        ImGui::ShowDemoWindow(NULL);
-    
-        void FreeTypeFont::SetPixelHeight(int pixel_height) 
+        enum FLIP_MODE
     {
-        // I'm not sure how to deal with font sizes properly.
-        // As far as I understand, currently ImGui assumes that the 'pixel_height' is a maximum height of an any given glyph,
-        // i.e. it's the sum of font's ascender and descender. Seems strange to me.
-        FT_Size_RequestRec req;
-        req.type = FT_SIZE_REQUEST_TYPE_REAL_DIM;
-        req.width = 0;
-        req.height = (uint32_t)pixel_height * 64;
-        req.horiResolution = 0;
-        req.vertResolution = 0;
-        FT_Request_Size(FreetypeFace, &req);
+        FLIP_HORIZONTAL_MODE = 1,
+        FLIP_VERTICAL_MODE = 2,
+        FLIP_BOTH_MODE = FLIP_HORIZONTAL_MODE | FLIP_VERTICAL_MODE
+    };
+    
+        for (size_t i = 0; i < size.height; ++i)
+    {
+        const u8 * src0 = internal::getRowPtr(src0Base, src0Stride, i);
+        const s16 * src1 = internal::getRowPtr(src1Base, src1Stride, i);
+        s16 * dst = internal::getRowPtr(dstBase, dstStride, i);
+        size_t j = 0;
     }
     
-                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
+            for (; j < roiw32; j += 32)
+        {
+            internal::prefetch(src + j);
+            uint8x16_t v_src0 = vld1q_u8(src + j), v_src1 = vld1q_u8(src + j + 16);
+            uint8x16_t v_dst0 = vmvnq_u8(v_src0), v_dst1 = vmvnq_u8(v_src1);
+            vst1q_u8(dst + j, v_dst0);
+            vst1q_u8(dst + j + 16, v_dst1);
+        }
+        for (; j < roiw8; j += 8)
+        {
+            uint8x8_t v_src = vld1_u8(src + j);
+            uint8x8_t v_dst = vmvn_u8(v_src);
+            vst1_u8(dst + j, v_dst);
+        }
     
-        if (!g_pIB || g_IndexBufferSize < draw_data->TotalIdxCount)
+            vline1_f32 = vmulq_f32(vline1_f32, vscale);
+        vline2_f32 = vmulq_f32(vline2_f32, vscale);
+        float32x4_t vline1Shifted_f32 = vaddq_f32(vline1_f32, vshift);
+        float32x4_t vline2Shifted_f32 = vaddq_f32(vline2_f32, vshift);
+        uint32x4_t vline1_u32 = vcvtq_u32_f32(vline1Shifted_f32);
+        uint32x4_t vline2_u32 = vcvtq_u32_f32(vline2Shifted_f32);
+        uint32x4_t vline1Mask = vbicq_u32(vmask, vreinterpretq_u32_f32(vline2_f32));
+        uint32x4_t vline2Mask = vbicq_u32(vmask, vreinterpretq_u32_f32(vline1Shifted_f32));
+        vline1Mask = vshrq_n_u32(vline1Mask, 16);
+        vline2Mask = vshrq_n_u32(vline2Mask, 16);
+        vline1_u32 = vqsubq_u32(vline1_u32, vline1Mask);
+        vline2_u32 = vqsubq_u32(vline2_u32, vline2Mask);
+        uint16x4_t vRes1 = vqrshrn_n_u32(vline1_u32, 16);
+        uint16x4_t vRes2 = vqrshrn_n_u32(vline2_u32, 16);
+        uint8x8_t vRes = vqmovn_u16(vcombine_u16(vRes1, vRes2));
+    
+    
+    {                v_dst1 = vmlal_n_s16(v_dst1, vget_high_s16(t0_16s), kernelBase[5]);
+                v_dst1 = vmlal_n_s16(v_dst1, vget_high_s16(t1_16s), kernelBase[4]);
+                v_dst1 = vmlal_n_s16(v_dst1, vget_high_s16(t2_16s), kernelBase[3]);
+            }
+    
+                for (; i <= lim; i+= 16)
+            {
+                internal::prefetch(src + i);
+                uint8x16_t vln = vld1q_u8(src + i);
+                uint8x16_t vnz = vminq_u8(vln, vc1);
+                vs = vaddq_u8(vs, vnz);
+            }
+    
+    
+    {            ws = vpadalq_u32(ws, s1);
+            ws = vpadalq_u32(ws, s2);
+        }
+    
+    
     {
-        if (g_pIB) { g_pIB->Release(); g_pIB = NULL; }
-        g_IndexBufferSize = draw_data->TotalIdxCount + 10000;
-        D3D10_BUFFER_DESC desc;
-        memset(&desc, 0, sizeof(D3D10_BUFFER_DESC));
-        desc.Usage = D3D10_USAGE_DYNAMIC;
-        desc.ByteWidth = g_IndexBufferSize * sizeof(ImDrawIdx);
-        desc.BindFlags = D3D10_BIND_INDEX_BUFFER;
-        desc.CPUAccessFlags = D3D10_CPU_ACCESS_WRITE;
-        if (ctx->CreateBuffer(&desc, NULL, &g_pIB) < 0)
-            return;
+    {}}
+    
+    //////////////////////////////////////////////////////////////////////
+    
+    #include <cstdint>
+#include <tuple>
+    
+    Variant PlainDirectory::read() {
+  struct dirent entry;
+  struct dirent *result;
+  int ret = readdir_r(m_dir, &entry, &result);
+  if (ret != 0 || !result) {
+    return false;
+  }
+  return String(entry.d_name, CopyString);
+}
+    
+      char** newargv_array = (char**)alloca(sizeof(char*) * (newargv.size() + 1));
+  for (unsigned i = 0; i < newargv.size(); i++) {
+    // printf('%s\n', newargv[i].data());
+    newargv_array[i] = (char *)newargv[i].data();
+  }
+  // NULL-terminate the argument array.
+  newargv_array[newargv.size()] = nullptr;
+    
+    //////////////////////////////////////////////////////////////////////
+    
+      while (true) {
+    int seg_len = 0;
+    char cur = from_start[0];
+    while (cur && !isDirSeparator(cur)) {
+      ++seg_len;
+      cur = from_start[seg_len];
+    }
     }
