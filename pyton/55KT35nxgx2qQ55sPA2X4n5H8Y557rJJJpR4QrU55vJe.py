093ff2d4768a9fb27c277b8dd497bc319988981b
SUNsung <weatherius@gmail.com>
2018-09-09 12:43:26 +0300

    Rewrite [55KT35nxgx2qQ55sPA2X4n5H8Y557rJJJpR4QrU55vJe.py] File in: 2018-09-09 12:43:25

--- pyton/55KT35nxgx2qQ55sPA2X4n5H8Y557rJJJpR4QrU55vJe.py
+++ pyton/55KT35nxgx2qQ55sPA2X4n5H8Y557rJJJpR4QrU55vJe.py
@@ -1,30 +1,142 @@
 
         
-        print('Counting to two...')
-for number in count_to_two():
-    print(number, end=' ')
+        # UNDERFITTING
+# If the primary task of LFADS is 'filtering' of data and not
+# generation, then it is possible that the KL penalty is too strong.
+# Empirically, we have found this to be the case.  So we add a
+# hyperparameter in front of the the two KL terms (one for the initial
+# conditions to the generator, the other for the controller outputs).
+# You should always think of the the default values as 1.0, and that
+# leads to a standard VAE formulation whereby the numbers that are
+# optimized are a lower-bound on the log-likelihood of the data. When
+# these 2 HPs deviate from 1.0, one cannot make any statement about
+# what those LL lower bounds mean anymore, and they cannot be compared
+# (AFAIK).
+flags.DEFINE_float('kl_ic_weight', KL_IC_WEIGHT,
+                   'Strength of KL weight on initial conditions KL penatly.')
+flags.DEFINE_float('kl_co_weight', KL_CO_WEIGHT,
+                   'Strength of KL weight on controller output KL penalty.')
     
-        @Transactional
-    def do_stuff(self):
-        self.value = '1111'  # <- invalid value
-        self.increment()  # <- will fail and rollback
+    
+def evaluate_ensemble(test_data_name, number_of_lms):
+  ensemble = EnsembleLM(test_data_name)
+  model_list = ['lm{:02d}'.format(i+1) for i in range(number_of_lms)]
+  for model_name in model_list:
+    ensemble.add_single_model(model_name)
+  accuracy = ensemble.evaluate()
+  print('Accuracy of {} LM(s) on {} = {}'.format(
+      number_of_lms, test_data_name, accuracy))
+    
+      # 0th token always present.
+  p = np.concatenate((np.ones((FLAGS.batch_size, 1)), p), axis=1)
+    
+    from models import bidirectional_zaremba
+from models import cnn
+from models import critic_vd
+from models import feedforward
+from models import rnn
+from models import rnn_nas
+from models import rnn_vd
+from models import rnn_zaremba
+from models import seq2seq
+from models import seq2seq_nas
+from models import seq2seq_vd
+from models import seq2seq_zaremba
+    
+      return variable_mapping
+    
+        kwargs = {
+        'stream': True,
+        'method': args.method.lower(),
+        'url': args.url,
+        'headers': headers,
+        'data': data,
+        'verify': {
+            'yes': True,
+            'true': True,
+            'no': False,
+            'false': False,
+        }.get(args.verify.lower(), args.verify),
+        'cert': cert,
+        'timeout': args.timeout,
+        'auth': args.auth,
+        'proxies': {p.key: p.value for p in args.proxy},
+        'files': args.files,
+        'allow_redirects': args.follow,
+        'params': args.params,
+    }
+    
+        def format_body(self, content, mime):
+        if is_valid_mime(mime):
+            for p in self.enabled_plugins:
+                content = p.format_body(content, mime)
+        return content
+
+    
+        if req:
+        output.append(Stream(
+            msg=HTTPRequest(request),
+            with_headers=req_h,
+            with_body=req_b))
+    
+        # Set to `False` to make it possible to invoke this auth
+    # plugin without requiring the user to specify credentials
+    # through `--auth, -a`.
+    auth_require = True
     
     
-class lazy_property(object):
+def load_json_preserve_order(s):
+    return json.loads(s, object_pairs_hook=OrderedDict)
     
-        def __init__(self):
-        self._objects = {}
+        plugin_manager.register(Plugin)
+    try:
+        r = http(
+            httpbin + BASIC_AUTH_URL,
+            '--auth-type',
+            Plugin.auth_type,
+            '--auth',
+            USERNAME + SEP_CREDENTIALS + PASSWORD,
+        )
+        assert HTTP_OK in r
+        assert r.json == AUTH_OK
+    finally:
+        plugin_manager.unregister(Plugin)
     
-        def now(self):
-        current_time = datetime.datetime.now()
-        current_time_formatted = '{}:{}'.format(current_time.hour, current_time.minute)
-        return current_time_formatted
+            get_filename_max_length.return_value = 10
     
-        def get_current_time_as_html_fragment(self):
-        current_time = self.time_provider.now()
-        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
-        return current_time_as_html_fragment
+    
+# Example usage...
+def main():
+    data1 = Data('Data 1')
+    data2 = Data('Data 2')
+    view1 = DecimalViewer()
+    view2 = HexViewer()
+    data1.attach(view1)
+    data1.attach(view2)
+    data2.attach(view2)
+    data2.attach(view1)
+    
+    ### OUTPUT ###
+# dog σκύλος
+# parrot parrot
+# cat γάτα
+# bear bear
+
     
     '''
-Reference: https://en.wikipedia.org/wiki/Delegation_pattern
-Author: https://github.com/IuryAlves
+Port of the Java example of 'Parameter Injection' in
+'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
+(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
+http://xunitpatterns.com/Dependency%20Injection.html.
+    
+            path.append(start)
+        if start == end:
+            return path
+        for node in self.graph.get(start, []):
+            if node not in path:
+                newpath = self.find_path(node, end, path[:])
+                if newpath:
+                    return newpath
+    
+        def __init__(self, HierachicalStateMachine):
+        self._hsm = HierachicalStateMachine
