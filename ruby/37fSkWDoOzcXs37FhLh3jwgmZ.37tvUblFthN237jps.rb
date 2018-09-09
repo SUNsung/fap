39a6c3a72ea8c8eb4c7988ca679b7adbe0ae7f0d
SUNsung <weatherius@gmail.com>
2018-09-09 17:10:26 +0300

    Rewrite [37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb] File in: 2018-09-09 17:10:25

--- ruby/37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb
+++ ruby/37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb
@@ -1,92 +1,70 @@
 
         
-                # Build your Jekyll site.
-        #
-        # site - the Jekyll::Site instance to build
-        # options - A Hash of options passed to the command
-        #
-        # Returns nothing.
-        def build(site, options)
-          t = Time.now
-          source      = options['source']
-          destination = options['destination']
-          incremental = options['incremental']
-          Jekyll.logger.info 'Source:', source
-          Jekyll.logger.info 'Destination:', destination
-          Jekyll.logger.info 'Incremental build:',
-                             (incremental ? 'enabled' : 'disabled. Enable with --incremental')
-          Jekyll.logger.info 'Generating...'
-          process_site(site)
-          Jekyll.logger.info '', 'done in #{(Time.now - t).round(3)} seconds.'
-        end
+          def upgrade_account
+    if signed_request_account.ostatus?
+      signed_request_account.update(last_webfingered_at: nil)
+      ResolveAccountWorker.perform_async(signed_request_account.acct)
+    end
     
-              if options['blank']
-            create_blank_site new_blog_path
+    module Admin
+  class AccountModerationNotesController < BaseController
+    before_action :set_account_moderation_note, only: [:destroy]
+    
+        def require_local_account!
+      redirect_to admin_account_path(@account.id) unless @account.local? && @account.user.present?
+    end
+    
+        def paginated_instances
+      filtered_instances.page(params[:page])
+    end
+    
+        # put the non-standard line terminations back to normal
+    # gah.  not having look behinds suck,
+    header.gsub!(/([^\r])\n/n,'\1' + '\r\n')
+    
+      # Puts a URI back together based on the URI parts
+  def uri
+    str = self.uri_parts['Resource'].dup || '/'
+    
+            # Sends a kerberos request through the connection
+        #
+        # @param req [Rex::Proto::Kerberos::Model::KdcRequest] the request to send
+        # @return [Integer] the number of bytes sent
+        # @raise [RuntimeError] if the transport protocol is unknown
+        # @raise [NotImplementedError] if the transport protocol isn't supported
+        def send_request(req)
+          connect
+    
+        # Returns a new {Gem::Version} based on the systems `git` version.
+    #
+    # @return [Gem::Version]
+    #
+    def self.git_version
+      raw_version = Executable.capture_command('git', ['--version']).first
+      unless match = raw_version.scan(/\d+\.\d+\.\d+/).first
+        raise 'Failed to extract git version from `git --version` (#{raw_version.inspect})'
+      end
+      Gem::Version.new(match)
+    end
+    
+            def run
+          if @pod_name.nil?
+            # Note: at that point, @wipe_all is always true (thanks to `validate!`)
+            # Remove all
+            clear_cache
           else
-            create_site new_blog_path
-          end
-    
-          private
-    
-          it 'renders HTML' do
-        expect(render(options, screenshots)).to match(/<html>/)
-      end
-    end
-    
-            expect(result).to eq('/usr/local/bin/cloc --exclude-dir=test1,test2,build --by-file --xml  --out=build/cloc.xml')
-      end
-    
-            # This returns all the registered communicators.
-        #
-        # @return [Hash]
-        def communicators
-          Registry.new.tap do |result|
-            @registered.each do |plugin|
-              result.merge!(plugin.communicator)
+            # Remove only cache for this pod
+            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
+            if cache_descriptors.nil?
+              UI.notice('No cache for pod named #{@pod_name} found')
+            elsif cache_descriptors.count > 1 && !@wipe_all
+              # Ask which to remove
+              choices = cache_descriptors.map { |c| '#{@pod_name} v#{c[:version]} (#{pod_type(c)})' }
+              index = UI.choose_from_array(choices, 'Which pod cache do you want to remove?')
+              remove_caches([cache_descriptors[index]])
+            else
+              # Remove all found cache of this pod
+              remove_caches(cache_descriptors)
             end
           end
         end
-    
-            @value << new_block
-      end
-    end
-  end
-end
-
-    
-              # Verify the box exists that we want to repackage
-          box = @env.boxes.find(box_name, box_provider, '= #{box_version}')
-          if !box
-            raise Vagrant::Errors::BoxNotFoundWithProviderAndVersion,
-              name: box_name,
-              provider: box_provider.to_s,
-              version: box_version
-          end
-    
-            def autocorrect(node)
-          # Regexp#match can take a second argument, but this cop doesn't
-          # register an offense in that case
-          return unless node.first_argument.regexp_type?
-    
-            def on_block(node)
-          on_body_of_reduce(node) do |body|
-            void_next = body.each_node(:next).find do |n|
-              n.children.empty? && parent_block_node(n) == node
-            end
-    
-        status data.respond_to?(:status_code) ? data.status_code : 200
-    
-              def logstash_plugin_gem_spec?(spec)
-            spec.metadata && spec.metadata['logstash_plugin'] == 'true'
-          end
-    
-            #target = $LOADED_FEATURES.grep(/#{path}/).first
-        #puts path
-        #puts caller.map { |c| '  #{c}' }.join('\n')
-        #fontsize = [10, duration * 48].max
-        puts '#{duration},#{path},#{source}'
-      end
-      #puts caller.map { |c| ' => #{c}' }.join('\n')
-    end
-    
-    end
