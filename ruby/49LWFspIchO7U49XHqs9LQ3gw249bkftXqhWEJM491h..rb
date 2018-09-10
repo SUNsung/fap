daa3be38f46db63482e828e1c09e10fa8d080f1a
SUNsung <weatherius@gmail.com>
2018-09-10 09:10:54 +0300

    Rewrite [49LWFspIchO7U49XHqs9LQ3gw249bkftXqhWEJM491h..rb] File in: 2018-09-10 09:10:54

--- ruby/49LWFspIchO7U49XHqs9LQ3gw249bkftXqhWEJM491h..rb
+++ ruby/49LWFspIchO7U49XHqs9LQ3gw249bkftXqhWEJM491h..rb
@@ -1,48 +1,91 @@
 
         
-        def global_require
-  JSON.pretty_generate(DATA)
-end
-    
-    When(%r!^I decide to build the theme gem$!) do
-  Dir.chdir(Paths.theme_gem_dir)
-  File.new('_includes/blank.html', 'w')
-  File.new('_sass/blank.scss', 'w')
-  File.new('assets/blank.scss', 'w')
-end
-    
-        # Private: The list of directories that need to be kept because they are
-    # parent directories of files specified in keep_files
-    #
-    # Returns a Set with the directory paths
-    def keep_dirs
-      site.keep_files.map { |file| parent_dirs(site.in_dest_dir(file)) }.flatten.to_set
+            def self.rm_DS_Store
+      paths = Queue.new
+      %w[Cellar Frameworks Library bin etc include lib opt sbin share var].
+        map { |p| HOMEBREW_PREFIX/p }.each { |p| paths << p if p.exist? }
+      workers = (0...Hardware::CPU.cores).map do
+        Thread.new do
+          begin
+            while p = paths.pop(true)
+              quiet_system 'find', p, '-name', '.DS_Store', '-delete'
+            end
+          rescue ThreadError # ignore empty queue error
+          end
+        end
+      end
+      workers.map(&:join)
     end
     
-    module Jekyll
-  module Commands
-    class Build < Command
-      class << self
-        # Create the Mercenary command for the Jekyll CLI for this Command
-        def init_with_program(prog)
-          prog.command(:build) do |c|
-            c.syntax      'build [options]'
-            c.description 'Build your site'
-            c.alias :b
+      # Use this method to generate standard caveats.
+  def standard_instructions(home_name, home_value = libexec)
+    <<-EOS.undent
+      Before you can use these tools you must export some variables to your $SHELL.
     
-        def translation_scope
-      'devise.confirmations'
+            it 'sets up screenshots folder in fastlane folder' do
+          expect(options[:screenshots_path]).to eq('./fastlane/screenshots')
+        end
+    
+            it 'review_user_needed is true' do
+          uploader.send('set_review_information', version, options)
+          expect(version).to have_received(:review_user_needed=).with(true)
+        end
+      end
+    
+          def self.details
+        'see adb --help for more details'
+      end
+    
+        s = StringIO.new
+    SystemConfig.dump_verbose_config s
+    # Dummy summary file, asciibetically first, to control display title of gist
+    files['# #{f.name} - #{timestamp}.txt'] = { content: brief_build_info(f) }
+    files['00.config.out'] = { content: s.string }
+    files['00.doctor.out'] = { content: Utils.popen_read('#{HOMEBREW_PREFIX}/bin/brew', 'doctor', err: :out) }
+    unless f.core_formula?
+      tap = <<~EOS
+        Formula: #{f.name}
+        Tap: #{f.tap}
+        Path: #{f.path}
+      EOS
+      files['00.tap.out'] = { content: tap }
     end
+    
+        # Exclude cache, logs, and repository, if they are located under the prefix.
+    [HOMEBREW_CACHE, HOMEBREW_LOGS, HOMEBREW_REPOSITORY].each do |dir|
+      dirs.delete dir.relative_path_from(HOMEBREW_PREFIX).to_s
+    end
+    dirs.delete 'etc'
+    dirs.delete 'var'
+    
+        Keg::PRUNEABLE_DIRECTORIES.each do |dir|
+      next unless dir.directory?
+      dir.find do |path|
+        path.extend(ObserverPathnameExtension)
+        if path.symlink?
+          unless path.resolved_path_exists?
+            if path.to_s =~ Keg::INFOFILE_RX
+              path.uninstall_info unless ARGV.dry_run?
+            end
+    
+    When /^I fill out change password section with my password and '([^']*)' and '([^']*)'$/ do |new_pass, confirm_pass|
+  fill_change_password_section(@me.password, new_pass, confirm_pass)
+end
+    
+    When /^(?:|I )click on '([^']*)' navbar title$/ do |title|
+  with_scope('.info-bar') do
+    find('h5', text: title).click
+  end
 end
 
     
-      def failure
-    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
-    redirect_to after_omniauth_failure_path_for(resource_name)
+      failure_message_for_should do |actual|
+    'expected #{actual.inspect} to have value #{expected.inspect} but was #{actual.value.inspect}'
   end
-    
-        get '/'
-    assert last_response.ok?
+  failure_message_for_should_not do |actual|
+    'expected #{actual.inspect} to not have value #{expected.inspect} but it had'
   end
+end
     
-        proxy = Devise::Hooks::Proxy.new(warden)
+          sign_in alice, scope: :user
+    end
