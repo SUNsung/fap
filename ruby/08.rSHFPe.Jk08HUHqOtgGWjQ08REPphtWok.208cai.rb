d0d9182efe1f962c07a2780df545c990b0d71029
SUNsung <weatherius@gmail.com>
2018-09-10 18:17:56 +0300

    Rewrite [08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb] File in: 2018-09-10 18:17:56

--- ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
+++ ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
@@ -1,96 +1,134 @@
 
         
-        HighLine.track_eof = false
+        describe GivenDailyLike do
     
-          describe 'running without fastlane' do
-        before do
-          allow(FastlaneCore::Helper).to receive(:fastlane_enabled?).and_return(false)
+        group.default_notification_level = 1
+    group.save
     
-            file_name = File.basename(file).gsub('.rb', '')
-    
-          def self.sample_return_value
-        []
+          (1..3).map do |i|
+        Notification.create!(read: false, user_id: user.id, topic_id: 2, post_number: i, data: '{}', notification_type: 1)
       end
+      Notification.create!(read: true, user_id: user.id, topic_id: 2, post_number: 4, data: '{}', notification_type: 1)
     
-              # DOCUMENTATION SET INFO
-          FastlaneCore::ConfigItem.new(key: :docset_bundle_id, env_name: 'FL_APPLEDOC_DOCSET_BUNDLE_ID', description: 'DocSet bundle identifier', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_bundle_name, env_name: 'FL_APPLEDOC_DOCSET_BUNDLE_NAME', description: 'DocSet bundle name', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_desc, env_name: 'FL_APPLEDOC_DOCSET_DESC', description: 'DocSet description', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_copyright, env_name: 'FL_APPLEDOC_DOCSET_COPYRIGHT', description: 'DocSet copyright message', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_feed_name, env_name: 'FL_APPLEDOC_DOCSET_FEED_NAME', description: 'DocSet feed name', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_feed_url, env_name: 'FL_APPLEDOC_DOCSET_FEED_URL', description: 'DocSet feed URL', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_feed_formats, env_name: 'FL_APPLEDOC_DOCSET_FEED_FORMATS', description: 'DocSet feed formats. Separated by a comma [atom,xml]', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_package_url, env_name: 'FL_APPLEDOC_DOCSET_PACKAGE_URL', description: 'DocSet package (.xar) URL', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_fallback_url, env_name: 'FL_APPLEDOC_DOCSET_FALLBACK_URL', description: 'DocSet fallback URL', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_publisher_id, env_name: 'FL_APPLEDOC_DOCSET_PUBLISHER_ID', description: 'DocSet publisher identifier', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_publisher_name, env_name: 'FL_APPLEDOC_DOCSET_PUBLISHER_NAME', description: 'DocSet publisher name', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_min_xcode_version, env_name: 'FL_APPLEDOC_DOCSET_MIN_XCODE_VERSION', description: 'DocSet min. Xcode version', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_platform_family, env_name: 'FL_APPLEDOC_DOCSET_PLATFORM_FAMILY', description: 'DocSet platform family', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_cert_issuer, env_name: 'FL_APPLEDOC_DOCSET_CERT_ISSUER', description: 'DocSet certificate issuer', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_cert_signer, env_name: 'FL_APPLEDOC_DOCSET_CERT_SIGNER', description: 'DocSet certificate signer', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_bundle_filename, env_name: 'FL_APPLEDOC_DOCSET_BUNDLE_FILENAME', description: 'DocSet bundle filename', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_atom_filename, env_name: 'FL_APPLEDOC_DOCSET_ATOM_FILENAME', description: 'DocSet atom feed filename', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_xml_filename, env_name: 'FL_APPLEDOC_DOCSET_XML_FILENAME', description: 'DocSet xml feed filename', is_string: true, optional: true),
-          FastlaneCore::ConfigItem.new(key: :docset_package_filename, env_name: 'FL_APPLEDOC_DOCSET_PACKAGE_FILENAME', description: 'DocSet package (.xar,.tgz) filename', is_string: true, optional: true),
-    
-      it 'accepts and uses a seed of 0' do
-    srand(0)
-    srand.should == 0
+      def self.defaults
+    %w(css dom dom_events html http javascript).map(&method(:find))
   end
     
-    # If you set this to false, any error raised from within your app will bubble
-# up to your step definition and out to cucumber unless you catch it somewhere
-# on the way. You can make Rails rescue errors and render error pages on a
-# per-scenario basis by tagging a scenario or feature with the @allow-rescue tag.
-#
-# If you set this to true, Rails will rescue all errors and render error
-# pages, more or less in the same way your application would behave in the
-# default production environment. It's not recommended to do this for all
-# of your scenarios, as this makes it hard to discover errors in your application.
-ActionController::Base.allow_rescue = false
     
-        it 'generates a jasmine fixture', :fixture => true do
-      contact = alice.contact_for(bob.person)
-      aspect = alice.aspects.create(:name => 'people')
-      contact.aspects << aspect
-      contact.save
-      get :new, params: {person_id: bob.person.id}
-      save_fixture(html_for('body'), 'status_message_new')
+    def initialize
+      raise NotImplementedError, '#{self.class} is an abstract class and cannot be instantiated.' if self.class.abstract
     end
+    
+        attr_accessor :name, :type, :path
+    
+              node['data-language'] = 'typescript' if node['path'].try(:ends_with?, '.ts')
+          node['data-language'] = 'html' if node['path'].try(:ends_with?, '.html')
+          node['data-language'] = 'css' if node['path'].try(:ends_with?, '.css')
+          node['data-language'] = 'js' if node['path'].try(:ends_with?, '.js')
+          node['data-language'] = 'json' if node['path'].try(:ends_with?, '.json')
+          node['data-language'] = node['language'].sub(/\Ats/, 'typescript').strip if node['language']
+          node['data-language'] ||= 'typescript' if node.content.start_with?('@')
+    
+    describe 'Kernel#sleep' do
+  it 'is a private method' do
+    Kernel.should have_private_instance_method(:sleep)
   end
+    
+      it 'creates a public method in script binding' do
+    eval @code, script_binding
+    Object.should have_method :boom
+  end
+    
+    module Rex
+  module Proto
+    module Kerberos
+      module Model
+        # This class provides a representation of a Kerberos Checksum definition.
+        class Checksum < Element
+    
+              # Decodes a Rex::Proto::Kerberos::Model::EncryptionKey
+          #
+          # @param input [String, OpenSSL::ASN1::Sequence] the input to decode from
+          # @return [self] if decoding succeeds
+          # @raise [RuntimeError] if decoding doesn't succeed
+          def decode(input)
+            case input
+            when String
+              decode_string(input)
+            when OpenSSL::ASN1::Sequence
+              decode_asn1(input)
+            else
+              raise ::RuntimeError, 'Failed to decode EncryptionKey, invalid input'
+            end
+    
+              # Decodes the ctime field
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Time]
+          def decode_ctime(input)
+            input.value[0].value
+          end
+    
+    When /^I submit the password reset form$/ do
+  submit_password_reset_form
+end
+    
+      aspect_id = @me.aspects.length == 1 ? @me.aspects.first.id : @me.aspects.where(:name => 'Besties').first.id
+  contacts.each do |contact|
+    aspect_memberships << AspectMembership.new(:contact_id => contact.id, :aspect_id => aspect_id)
+  end
+  AspectMembership.import(aspect_memberships)
+end
+    
+      # Reset overridden settings
+  AppConfig.reset_dynamic!
 end
 
     
-          def session(env)
-        return env[options[:session_key]] if session? env
-        fail 'you need to set up a session middleware *before* #{self.class}'
-      end
-    
-    desc 'Start an IRB session with all necessary files required.'
-task :shell do |t|
-  chdir File.dirname(__FILE__)
-  exec 'irb -I lib/ -I lib/paperclip -r rubygems -r active_record -r tempfile -r init'
-end
-    
-    When /^I configure the application to use '([^\']+)' from this project$/ do |name|
-  append_to_gemfile 'gem '#{name}', :path => '#{PROJECT_ROOT}''
-  steps %{And I successfully run `bundle install --local`}
-end
-    
-    class PaperclipGenerator < ActiveRecord::Generators::Base
-  desc 'Create a migration to add paperclip-specific fields to your model. ' +
-       'The NAME argument is the name of your model, and the following ' +
-       'arguments are the name of the attachments'
-    
-        # scale to the requested geometry and preserve the aspect ratio
-    def scale_to(new_geometry)
-      scale = [new_geometry.width.to_f / self.width.to_f , new_geometry.height.to_f / self.height.to_f].min
-      Paperclip::Geometry.new((self.width * scale).round, (self.height * scale).round)
-    end
+      def post_path_by_content(text)
+    p = Post.find_by_text(text)
+    post_path(p)
   end
-end
-
     
-        def register_new_attachment
-      Paperclip::AttachmentRegistry.register(@klass, @name, @options)
-    end
+            def find_order
+          @order = Spree::Order.find_by!(number: order_id)
+        end
+    
+            def create
+          authorize! :create, Spree::OptionType
+          @option_type = Spree::OptionType.new(option_type_params)
+          if @option_type.save
+            render :show, status: 201
+          else
+            invalid_resource!(@option_type)
+          end
+        end
+    
+            def destroy
+          @option_value = scope.accessible_by(current_ability, :destroy).find(params[:id])
+          @option_value.destroy
+          render plain: nil, status: 204
+        end
+    
+            def create
+          authorize! :create, ProductProperty
+          @product_property = @product.product_properties.new(product_property_params)
+          if @product_property.save
+            respond_with(@product_property, status: 201, default_template: :show)
+          else
+            invalid_resource!(@product_property)
+          end
+        end
+    
+            def variants_params
+          variants_key = if params[:product].key? :variants
+                           :variants
+                         else
+                           :variants_attributes
+          end
+    
+            def show
+          authorize! :admin, ReturnAuthorization
+          @return_authorization = order.return_authorizations.accessible_by(current_ability, :read).find(params[:id])
+          respond_with(@return_authorization)
+        end
