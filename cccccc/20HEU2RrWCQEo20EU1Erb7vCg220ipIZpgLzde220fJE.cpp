d3a59ffd5c7287b38265e2896da6e11022a27918
SUNsung <weatherius@gmail.com>
2018-09-11 02:25:01 +0300

    Rewrite [20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp] File in: 2018-09-11 02:25:01

--- cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
+++ cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
@@ -1,157 +1,101 @@
 
         
-        // Returns an EagerTensor of dimension [len(`tensors`)] containing
-// the `slice_dim`'th dimension of each tensor in `tensors`. In other words,
-// TFE_Py_TensorShapeSlice takes a slice of dimensions of tensors in
-// `tensors`. For example, if `tensors` contains tensors of with shapes
-// [1, 2, 3], [4, 5], [6, 7, 8, 9], TFE_Py_TensorShapeSlice called with
-// `slice_dim` equal to 1 will return [2, 5, 7].
-// On error, returns nullptr and sets python exception.
-// REQUIRES: `tensors` is a python list/tuple of EagerTensors
-// REQUIRES: `slice_dim` is non-negative and smaller than the rank of all
-//   tensors in `tensors`.
-PyObject* TFE_Py_TensorShapeSlice(PyObject* tensors, int slice_dim);
+        private:
+    MacDockIconHandler();
     
-    REGISTER_OP('TwoFloatOutputs')
-    .Output('a: float32')
-    .Output('b: float32')
-    .SetShapeFn(shape_inference::UnknownShape);
+        const QString &getAppName() const { return appName; }
+    const QIcon &getAppIcon() const { return appIcon; }
+    const QIcon &getTrayAndWindowIcon() const { return trayAndWindowIcon; }
+    const QString &getTitleAddText() const { return titleAddText; }
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+    private:
+    PlatformStyle(const QString &name, bool imagesOnButtons, bool colorizeIcons, bool useExtraSpacing);
     
-    REGISTER_OP('Invalid')
-    .Attr('invalid attr: int32')  // invalid since the name has a space.
-    .Doc(R'doc(
-An op to test that invalid ops do not successfully generate invalid python code.
-)doc');
+    namespace Ui {
+    class SignVerifyMessageDialog;
+}
     
-    // Must be included first.
-#include 'tensorflow/python/lib/core/numpy.h'
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    #include 'tensorflow/core/framework/graph.pb.h'
-#include 'tensorflow/core/platform/types.h'
-    
-    #ifndef BITCOIN_QT_PLATFORMSTYLE_H
-#define BITCOIN_QT_PLATFORMSTYLE_H
-    
-    namespace
+    /**
+ * An RAII-style reverse lock. Unlocks on construction and locks on destruction.
+ */
+template<typename Lock>
+class reverse_lock
 {
+public:
     }
     
-    #include <script/script.h>
     
-    static void secp256k1_hmac_sha256_initialize(secp256k1_hmac_sha256_t *hash, const unsigned char *key, size_t keylen) {
-    int n;
-    unsigned char rkey[64];
-    if (keylen <= 64) {
-        memcpy(rkey, key, keylen);
-        memset(rkey + keylen, 0, 64 - keylen);
-    } else {
-        secp256k1_sha256_t sha256;
-        secp256k1_sha256_initialize(&sha256);
-        secp256k1_sha256_write(&sha256, key, keylen);
-        secp256k1_sha256_finalize(&sha256, rkey);
-        memset(rkey + 32, 0, 32);
+    {    secp256k1_sha256_initialize(&hash->inner);
+    for (n = 0; n < 64; n++) {
+        rkey[n] ^= 0x5c ^ 0x36;
     }
-    }
+    secp256k1_sha256_write(&hash->inner, rkey, 64);
+    memset(rkey, 0, 64);
+}
     
-        s_one[31] = 1;
-    /* Check against pubkey creation when the basepoint is the generator */
-    for (i = 0; i < 100; ++i) {
-        secp256k1_sha256_t sha;
-        unsigned char s_b32[32];
-        unsigned char output_ecdh[32];
-        unsigned char output_ser[32];
-        unsigned char point_ser[33];
-        size_t point_ser_len = sizeof(point_ser);
-        secp256k1_scalar s;
-    }
+    BOOST_FIXTURE_TEST_SUITE(bech32_tests, BasicTestingSetup)
     
-        /* Check NULLs for conversion */
-    CHECK(secp256k1_ecdsa_sign(both, &normal_sig, message, privkey, NULL, NULL) == 1);
-    ecount = 0;
-    CHECK(secp256k1_ecdsa_recoverable_signature_convert(both, NULL, &recsig) == 0);
-    CHECK(ecount == 1);
-    CHECK(secp256k1_ecdsa_recoverable_signature_convert(both, &normal_sig, NULL) == 0);
-    CHECK(ecount == 2);
-    CHECK(secp256k1_ecdsa_recoverable_signature_convert(both, &normal_sig, &recsig) == 1);
-    
-    static bool CaseInsensitiveEqual(const std::string &s1, const std::string &s2)
+    inline void printfln(const char* fmt)
 {
-    if (s1.size() != s2.size()) return false;
-    for (size_t i = 0; i < s1.size(); ++i) {
-        char c1 = s1[i];
-        if (c1 >= 'A' && c1 <= 'Z') c1 -= ('A' - 'a');
-        char c2 = s2[i];
-        if (c2 >= 'A' && c2 <= 'Z') c2 -= ('A' - 'a');
-        if (c1 != c2) return false;
-    }
-    return true;
+    format(std::cout, fmt);
+    std::cout << '\n';
 }
     
-    BOOST_FIXTURE_TEST_SUITE(validation_block_tests, RegtestingSetup)
+    double UniValue::get_real() const
+{
+    if (typ != VNUM)
+        throw std::runtime_error('JSON value is not a number as expected');
+    double retval;
+    if (!ParseDouble(getValStr(), &retval))
+        throw std::runtime_error('JSON double out of range');
+    return retval;
+}
     
-    const UniValue& find_value( const UniValue& obj, const std::string& name);
-    
-      /**
-   * @brief Send messages
-   * @param frames The messages to send.
-   * @param frame_num The amount of messages to send.
-   * @return The status of the sending action which is defined by
-   *         apollo::common::ErrorCode.
-   */
-  virtual apollo::common::ErrorCode Send(const std::vector<CanFrame> &frames,
-                                         int32_t *const frame_num) = 0;
-    
-      /**
-   * @brief Stop the fake CAN client.
-   */
-  void Stop() override;
-    
-    
-    {
-    {    if (ret < 0) {
-      AERROR << 'receive message failed, error code: ' << ret;
-      return ErrorCode::CAN_CLIENT_ERROR_BASE;
-    }
-    if (recv_frames_[i].can_dlc != CANBUS_MESSAGE_LENGTH) {
-      AERROR << 'recv_frames_[' << i
-             << '].can_dlc = ' << recv_frames_[i].can_dlc
-             << ', which is not equal to can message data length ('
-             << CANBUS_MESSAGE_LENGTH << ').';
-      return ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED;
-    }
-    cf.id = recv_frames_[i].can_id;
-    cf.len = recv_frames_[i].can_dlc;
-    std::memcpy(cf.data, recv_frames_[i].data, recv_frames_[i].can_dlc);
-    frames->push_back(cf);
+      void IncInitialized() {
+    num_initialized_++;
   }
-  return ErrorCode::OK;
-}
     
-    template <typename SensorType>
-ErrorCode MessageManager<SensorType>::GetSensorData(
-    SensorType *const sensor_data) {
-  if (sensor_data == nullptr) {
-    AERROR << 'Failed to get sensor_data due to nullptr.';
-    return ErrorCode::CANBUS_ERROR;
+      virtual Status DisableFileDeletions() override {
+    return Status::NotSupported('Not supported in compacted db mode.');
   }
-  std::lock_guard<std::mutex> lock(sensor_data_mutex_);
-  sensor_data->CopyFrom(sensor_data_);
-  return ErrorCode::OK;
-}
+  virtual Status EnableFileDeletions(bool /*force*/) override {
+    return Status::NotSupported('Not supported in compacted db mode.');
+  }
+  virtual Status GetLiveFiles(std::vector<std::string>&,
+                              uint64_t* /*manifest_file_size*/,
+                              bool /*flush_memtable*/ = true) override {
+    return Status::NotSupported('Not supported in compacted db mode.');
+  }
+  using DBImpl::Flush;
+  virtual Status Flush(const FlushOptions& /*options*/,
+                       ColumnFamilyHandle* /*column_family*/) override {
+    return Status::NotSupported('Not supported in compacted db mode.');
+  }
+  using DB::IngestExternalFile;
+  virtual Status IngestExternalFile(
+      ColumnFamilyHandle* /*column_family*/,
+      const std::vector<std::string>& /*external_files*/,
+      const IngestExternalFileOptions& /*ingestion_options*/) override {
+    return Status::NotSupported('Not supported in compacted db mode.');
+  }
     
-    TEST(ByteTest, ByteToString) {
-  unsigned char value = 0x34;
-  EXPECT_EQ('34', Byte::byte_to_hex(value));
-  EXPECT_EQ('00110100', Byte::byte_to_binary(value));
-  uint32_t int_value = 0xE13A;
-  EXPECT_EQ('E13A', Byte::byte_to_hex(int_value));
+    
+    {  // return true if writes with this callback can be batched with other writes
+  virtual bool AllowWriteBatching() = 0;
+};
+    
+      // Use the returned alignment value to allocate
+  // aligned buffer for Direct I/O
+  virtual size_t GetRequiredBufferAlignment() const override { 
+    return file_->GetRequiredBufferAlignment(); 
+  }
+    
+    /**
+ * Get the size of valid data in the file. This will not match the
+ * size that is returned from the filesystem because we use mmap
+ * to extend file by map_size every time.
+ */
+uint64_t PosixMmapFile::GetFileSize() {
+  size_t used = dst_ - base_;
+  return file_offset_ + used;
 }
