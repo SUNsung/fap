5c172620a1765bc6398d9e8b28b7e97023a9ec94
SUNsung <weatherius@gmail.com>
2018-09-10 15:41:32 +0300

    Rewrite [26NrgZdbDZydw26bF3GTx0FdM2261BwP.aswsr226lzJ.rb] File in: 2018-09-10 15:41:32

--- ruby/26NrgZdbDZydw26bF3GTx0FdM2261BwP.aswsr226lzJ.rb
+++ ruby/26NrgZdbDZydw26bF3GTx0FdM2261BwP.aswsr226lzJ.rb
@@ -1,57 +1,99 @@
 
         
-            def get_json(url)
-      JSON.parse get_file(url)
+          # body
+  xml.tag!('body') do
+    xml.tag!('outline', text: TITLE, title: TITLE) do
+      blogs.each do |blog|
+        xml.tag!('outline', type: 'rss', text: blog.name, title: blog.name,
+          xmlUrl: blog.rss_url, htmlUrl: blog.web_url)
+      end
     end
   end
 end
-
     
-    require 'sass/logger'
-require 'sass/util'
+      # DBM stores ruby objects as a ruby `String`. Hence, when fetching the data,
+  # to convert the ruby string back into a ruby `Hash`, the string is converted
+  # into a JSON compatible string in `ruby_hash_to_json_string`, where it may
+  # later be parsed by `JSON.parse` in the `json_string_to_ruby_hash` method
+  #
+  # @param  [Hash] ruby `Hash` to be converted to `JSON` string
+  # @return [String]
+  def ruby_hash_to_json_string(hash)
+    hash.to_json
+  end
     
-      # An environment that can write to in-scope global variables, but doesn't
-  # create new variables in the global scope. Useful for top-level control
-  # directives.
-  class SemiGlobalEnvironment < Environment
-    def try_set_var(name, value)
-      @vars ||= {}
-      if @vars.include?(name)
-        @vars[name] = value
-        true
-      elsif @parent
-        @parent.try_set_var(name, value)
-      else
-        false
+        return unless download.file?
+    
+        # Exclude cache, logs, and repository, if they are located under the prefix.
+    [HOMEBREW_CACHE, HOMEBREW_LOGS, HOMEBREW_REPOSITORY].each do |dir|
+      dirs.delete dir.relative_path_from(HOMEBREW_PREFIX).to_s
+    end
+    dirs.delete 'etc'
+    dirs.delete 'var'
+    
+        def replace_vars(less)
+      less = less.dup
+      # skip header comment
+      less =~ %r(\A/\*(.*?)\*/)m
+      from           = $~ ? $~.to_s.length : 0
+      less[from..-1] = less[from..-1].
+          gsub(/(?!@mixin|@media|@page|@keyframes|@font-face|@-\w)@/, '$').
+          # variables that would be ignored by gsub above: e.g. @page-header-border-color
+          gsub(/@(page[\w-]+)/, '$\1')
+      less
+    end
+    
+      # Specifies the header that your server uses for sending files.
+  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+    
+    Cucumber::Rails::Database.autorun_database_cleaner = true
+Cucumber::Rails::World.use_transactional_tests = false
+    
+          @conv2 = Conversation.create(hash)
+      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv2.id)
+             .increase_unread(alice)
+    
+        it 'does not let a user destroy other likes' do
+      like2 = eve.like!(@message)
+      like_count = Like.count
+    
+    # Declares a dependency to the git repo of CocoaPods gem. This declaration is
+# compatible with the local git repos feature of Bundler.
+#
+def cp_gem(name, repo_name, branch = 'master', path: false)
+  return gem name if SKIP_UNRELEASED_VERSIONS
+  opts = if path
+           { :path => '../#{repo_name}' }
+         else
+           url = 'https://github.com/CocoaPods/#{repo_name}.git'
+           { :git => url, :branch => branch }
+         end
+  gem name, opts
+end
+    
+                target 'Static' do
+                use_frameworks!(false)
+            end
+            target 'Dynamic' do
+                use_frameworks!(true)
+            end
+        end
+    end
+end
+    
+        def self.verify_xcode_license_approved!
+      if `/usr/bin/xcrun clang 2>&1` =~ /license/ && !$?.success?
+        raise Informative, 'You have not agreed to the Xcode license, which ' \
+          'you must do to use CocoaPods. Agree to the license by running: ' \
+          '`xcodebuild -license`.'
       end
     end
   end
 end
 
     
-          ext = @options[:from]
-      Sass::Util.glob('#{@options[:input]}/**/*.#{ext}') do |f|
-        output =
-          if @options[:in_place]
-            f
-          elsif @options[:output]
-            output_name = f.gsub(/\.(c|sa|sc|le)ss$/, '.#{@options[:to]}')
-            output_name[0...@options[:input].size] = @options[:output]
-            output_name
-          else
-            f.gsub(/\.(c|sa|sc|le)ss$/, '.#{@options[:to]}')
-          end
-    
-          # Returns the time the given Sass file was last modified.
-      #
-      # If the given file has been deleted or the time can't be accessed
-      # for some other reason, this should return nil.
-      #
-      # @param uri [String] The URI of the file to check.
-      #   Comes from a `:filename` option set on an engine returned by this importer.
-      # @param options [{Symbol => Object}] Options for the Sass file
-      #   containing the `@import` currently being checked.
-      # @return [Time, nil]
-      def mtime(uri, options)
-        Sass::Util.abstract(self)
-      end
+        def ask(key, default=nil, options={})
+      question = Question.new(key, default, options)
+      set(key, question)
+    end
