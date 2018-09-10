10ceb4adeffe0872677a6a5f8d30d8867fd370f9
SUNsung <weatherius@gmail.com>
2018-09-10 14:40:52 +0300

    Rewrite [5615zmC7dFklk56JzLFdLLsXWA56cawq5yCLRuA56SkG.rb] File in: 2018-09-10 14:40:51

--- ruby/5615zmC7dFklk56JzLFdLLsXWA56cawq5yCLRuA56SkG.rb
+++ ruby/5615zmC7dFklk56JzLFdLLsXWA56cawq5yCLRuA56SkG.rb
@@ -1,53 +1,107 @@
 
         
-              if rest.start_with?('/window.')
-        return redirect '/dom#{rest.sub('window.', 'window/')}', 301
-      end
+        def version
+  Jekyll::VERSION
+end
     
-    module Docs
-  require 'docs/core/autoload_helper'
-  extend AutoloadHelper
+    Nullam luctus fermentum est id blandit. Phasellus consectetur ullamcorper
+ligula, at finibus eros laoreet id. Etiam sit amet est in libero efficitur.
+tristique. Ut nec magna augue. {{ author }} Quisque ut fringilla lacus
+Aliquam vel ornare mauris. Suspendisse ornare diam tempor nulla facilisis
+aliquet. Sed ultrices placerat ultricies.
+LIQUID
     
-        private
+    require 'benchmark/ips'
+require 'jekyll'
+require 'json'
     
-        def filter_const(name)
-      if name.is_a? Array
-        name.map &method(:filter_const)
-      else
-        Docs.const_get '#{name}_filter'.camelize
-      end
-    end
+    # -------------------------------------------------------------------
+# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
+# -------------------------------------------------------------------
     
-            css('pre.prettyprint').each do |node|
-          node.content = node.content.strip
-          node['data-language'] = 'dart' if node['class'].include?('dart')
-          node['data-language'] = 'html' if node.content.start_with?('<')
-          node.remove_attribute('class')
+    CONTENT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+    
+    #
+    
+    module Jekyll
+  module Commands
+    class Build < Command
+      class << self
+        # Create the Mercenary command for the Jekyll CLI for this Command
+        def init_with_program(prog)
+          prog.command(:build) do |c|
+            c.syntax      'build [options]'
+            c.description 'Build your site'
+            c.alias :b
+    
+      EmailToken.where(user_id: smoke_user.id).update_all(confirmed: true)
+end
+
+    
+            lounge.topic_id = post.topic.id
+        unless lounge.save
+          puts lounge.errors.full_messages
+          puts 'Failed to set the lounge description topic!'
         end
     
-            css('header').each do |node|
-          node.before(node.children).remove
-        end
+    require 'commands'
     
-      def set_statuses
-    @statuses = scope_for_collection
-    @statuses = cache_collection(@statuses, Status)
+      def find_internal_commands(directory)
+    Pathname.glob(directory/'*')
+            .select(&:file?)
+            .map { |f| f.basename.to_s.sub(/\.(?:rb|sh)$/, '') }
   end
+end
+
     
-        def create
-      authorize AccountModerationNote, :create?
-    
-    module Admin
-  class ChangeEmailsController < BaseController
-    before_action :set_account
-    before_action :require_local_account!
-    
-        def destroy
-      authorize @email_domain_block, :destroy?
-      @email_domain_block.destroy!
-      log_action :destroy, @email_domain_block
-      redirect_to admin_email_domain_blocks_path, notice: I18n.t('admin.email_domain_blocks.destroyed_msg')
+        if ARGV.include? '--list-checks'
+      puts checks.all.sort
+      exit
     end
     
-        head 200
+        # Description formatted to work well as page title when viewing gist
+    if f.core_formula?
+      descr = '#{f.name} on #{OS_VERSION} - Homebrew build logs'
+    else
+      descr = '#{f.name} (#{f.full_name}) on #{OS_VERSION} - Homebrew build logs'
+    end
+    url = create_gist(files, descr)
+    
+        if ObserverPathnameExtension.total.zero?
+      puts 'Nothing pruned' if ARGV.verbose?
+    else
+      n, d = ObserverPathnameExtension.counts
+      print 'Pruned #{n} symbolic links '
+      print 'and #{d} directories ' if d.positive?
+      puts 'from #{HOMEBREW_PREFIX}'
+    end
   end
+end
+
+    
+      class ImageTag < Liquid::Tag
+    @img = nil
+    
+          rtn = ''
+      (context.environments.first['site'][@array_name] || []).each do |file|
+        if file !~ /^[a-zA-Z0-9_\/\.-]+$/ || file =~ /\.\// || file =~ /\/\./
+          rtn = rtn + 'Include file '#{file}' contains invalid characters or sequences'
+        end
+    
+        def initialize(tag_name, markup, tokens)
+      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
+      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
+      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
+      super
+    end
