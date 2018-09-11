5f5241babf3f913c189dd9aee63edbfc39ad106b
SUNsung <weatherius@gmail.com>
2018-09-11 13:13:29 +0300

    Rewrite [13OgOfwHoZNqk13VGo4CRrhCVM131J4H5T.LUw13sQG.cpp] File in: 2018-09-11 13:13:28

--- cccccc/13OgOfwHoZNqk13VGo4CRrhCVM131J4H5T.LUw13sQG.cpp
+++ cccccc/13OgOfwHoZNqk13VGo4CRrhCVM131J4H5T.LUw13sQG.cpp
@@ -1,127 +1,259 @@
 
         
-        
-    {
-    {    C[0] = C[1];
-    TargetStart = C + 1;
-    GCBForC0 = GCBForC1;
+        int main(int argc, char** argv) {
+  if (argc != 4) {
+    printf('This script converts the MNIST dataset to the leveldb format used\n'
+           'by caffe to train a siamese network.\n'
+           'Usage:\n'
+           '    convert_mnist_data input_image_file input_label_file '
+           'output_db_file\n'
+           'The MNIST dataset could be downloaded at\n'
+           '    http://yann.lecun.com/exdb/mnist/\n'
+           'You should gunzip them after downloading.\n');
+  } else {
+    google::InitGoogleLogging(argv[0]);
+    convert_dataset(argv[1], argv[2], argv[3]);
   }
+  return 0;
 }
-    
-    #include 'swift/Runtime/HeapObject.h'
-#include <mutex>
-    
-    // On macOS and iOS, swift_once is implemented using GCD.
-// The compiler emits an inline check matching the barrier-free inline fast
-// path of dispatch_once(). See SwiftTargetInfo.OnceDonePredicateValue.
-    
-    
-    {  virtual void finish() {}
-};
-    
-      IndexTrieNode *getChild(unsigned Idx) {
-    assert(Idx != RootIdx);
-    }
-    
-      StringRef getRuntimeLibPath() const { return RuntimeLibPath; }
-    
-      grpc::testing::RunQPS();
-    
-    #include <chrono>
-#include <thread>
-#include <vector>
-    
-    template <class T, class F>
-double average(const T& container, F functor) {
-  return sum(container, functor) / container.size();
+#else
+int main(int argc, char** argv) {
+  LOG(FATAL) << 'This example requires LevelDB; compile with USE_LEVELDB.';
 }
-    
-    
-    {
-    {} // namespace asio
-} // namespace boost
-    
-    #endif // BOOST_ASIO_DETAIL_ATOMIC_COUNT_HPP
+#endif  // USE_LEVELDB
 
     
-      // Consume multiple bytes from the beginning of the buffer.
-  void consume(size_type count)
-  {
-    BOOST_ASIO_ASSERT(begin_offset_ + count <= end_offset_);
-    begin_offset_ += count;
-    if (empty())
-      clear();
+      /**
+   * @brief Applies the same transformation defined in the data layer's
+   * transform_param block to all the num images in a input_blob.
+   *
+   * @param input_blob
+   *    A Blob containing the data to be transformed. It applies the same
+   *    transformation to all the num images in the blob.
+   * @param transformed_blob
+   *    This is destination blob, it will contain as many images as the
+   *    input blob. It can be part of top blob's data.
+   */
+  void Transform(Blob<Dtype>* input_blob, Blob<Dtype>* transformed_blob);
+    
+    #include 'caffe/common.hpp'
+    
+    
+    {  /**
+   * @brief Computes the error gradient w.r.t. the absolute value inputs.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *      respect to the outputs
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
+   *      with respect to computed outputs @f$ y @f$
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length 2)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$; Backward fills their diff with
+   *      gradients @f$
+   *        \frac{\partial E}{\partial x} =
+   *            \mathrm{sign}(x) \frac{\partial E}{\partial y}
+   *      @f$ if propagate_down[0]
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+};
+    
+    namespace caffe {
+    }
+    
+    #ifndef CPU_ONLY
+  void forward_gpu_gemm(const Dtype* col_input, const Dtype* weights,
+      Dtype* output, bool skip_im2col = false);
+  void forward_gpu_bias(Dtype* output, const Dtype* bias);
+  void backward_gpu_gemm(const Dtype* input, const Dtype* weights,
+      Dtype* col_output);
+  void weight_gpu_gemm(const Dtype* col_input, const Dtype* output, Dtype*
+      weights);
+  void backward_gpu_bias(Dtype* bias, const Dtype* input);
+#endif
+    
+    /**
+ * @brief Normalizes the input to have 0-mean and/or unit (1) variance across
+ *        the batch.
+ *
+ * This layer computes Batch Normalization as described in [1]. For each channel
+ * in the data (i.e. axis 1), it subtracts the mean and divides by the variance,
+ * where both statistics are computed across both spatial dimensions and across
+ * the different examples in the batch.
+ *
+ * By default, during training time, the network is computing global
+ * mean/variance statistics via a running average, which is then used at test
+ * time to allow deterministic outputs for each input. You can manually toggle
+ * whether the network is accumulating or using the statistics via the
+ * use_global_stats option. For reference, these statistics are kept in the
+ * layer's three blobs: (0) mean, (1) variance, and (2) moving average factor.
+ *
+ * Note that the original paper also included a per-channel learned bias and
+ * scaling factor. To implement this in Caffe, define a `ScaleLayer` configured
+ * with `bias_term: true` after each `BatchNormLayer` to handle both the bias
+ * and scaling factor.
+ *
+ * [1] S. Ioffe and C. Szegedy, 'Batch Normalization: Accelerating Deep Network
+ *     Training by Reducing Internal Covariate Shift.' arXiv preprint
+ *     arXiv:1502.03167 (2015).
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+template <typename Dtype>
+class BatchNormLayer : public Layer<Dtype> {
+ public:
+  explicit BatchNormLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    }
+    
+    
+    {}  // namespace caffe
+    
+     protected:
+  /// @copydoc BNLLLayer
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    void Label::branchFar(Assembler& a,
+                  BranchConditions bc,
+                  LinkReg lr,
+                  ImmType immt,
+                  bool immMayChange) {
+  // Marking current address for patchAbsolute
+  addJump(&a);
+    }
+    
+        D_form_t d_formater {{
+                          static_cast<uint32_t>(imm),
+                          static_cast<uint32_t>(ra),
+                          static_cast<uint32_t>(rt),
+                          op
+                         }};
+    
+    #include 'hphp/runtime/base/apc-local-array.h'
+#include 'hphp/runtime/base/mixed-array-defs.h'
+#include 'hphp/runtime/base/memory-manager.h'
+    
+    
+    {///////////////////////////////////////////////////////////////////////////////
+}
+    
+      bool isRecoverable() const { return m_recoverable; }
+    
+    namespace HPHP { namespace FileUtil {
+    }
+    }
+    
+    #include 'hphp/runtime/base/type-string.h'
+    
+    TEST(ExecutorTest, KeepAliveConvert) {
+  KeepAliveTestExecutor exec;
+    }
+    
+    #include <folly/GLog.h>
+    
+    static uint32_t manyArgTypesTestFunc() {
+  uint32_t a = folly::Random::rand32();
+  uint32_t b = folly::Random::rand32();
+  bool bool_ = (a % 2) == (b % 2);
+  char char_ = a & 255;
+  short short_ = b & 32767;
+  long long_ = a;
+  float float_ = float(a) / float(b);
+  double double_ = double(a) / double(b);
+  FOLLY_SDT(
+      folly,
+      test_static_tracepoint_many_arg_types,
+      a,
+      b,
+      bool_,
+      char_,
+      short_,
+      long_,
+      float_,
+      double_);
+  FOLLY_SDT_WITH_SEMAPHORE(folly, test_semaphore_local, long_, short_);
+  return a + b;
+}
+    
+    /**
+ * Debug string for an exception: include type and what(), if
+ * defined.
+ */
+inline fbstring exceptionStr(const std::exception& e) {
+#ifdef FOLLY_HAS_RTTI
+  fbstring rv(demangle(typeid(e)));
+  rv += ': ';
+#else
+  fbstring rv('Exception (no RTTI available): ');
+#endif
+  rv += e.what();
+  return rv;
+}
+    
+    exception_wrapper exception_wrapper::from_exception_ptr(
+    std::exception_ptr const& ptr) noexcept {
+  if (!ptr) {
+    return exception_wrapper();
   }
+  try {
+    std::rethrow_exception(ptr);
+  } catch (std::exception& e) {
+    return exception_wrapper(std::current_exception(), e);
+  } catch (...) {
+    return exception_wrapper(std::current_exception());
+  }
+}
     
-    #ifndef BOOST_ASIO_DETAIL_DATE_TIME_FWD_HPP
-#define BOOST_ASIO_DETAIL_DATE_TIME_FWD_HPP
+    template <class String>
+String Uri::toString() const {
+  String str;
+  if (hasAuthority_) {
+    toAppend(scheme_, '://', &str);
+    if (!password_.empty()) {
+      toAppend(username_, ':', password_, '@', &str);
+    } else if (!username_.empty()) {
+      toAppend(username_, '@', &str);
+    }
+    toAppend(host_, &str);
+    if (port_ != 0) {
+      toAppend(':', port_, &str);
+    }
+  } else {
+    toAppend(scheme_, ':', &str);
+  }
+  toAppend(path_, &str);
+  if (!query_.empty()) {
+    toAppend('?', query_, &str);
+  }
+  if (!fragment_.empty()) {
+    toAppend('#', fragment_, &str);
+  }
+  return str;
+}
     
-    namespace boost {
-namespace asio {
-namespace detail {
-    }
-    }
-    }
-    
-    namespace boost {
-namespace asio {
-namespace detail {
-    }
-    }
-    }
-    
-      // Re-initialise the hash from the values already contained in the list.
-  void rehash(std::size_t num_buckets)
-  {
-    if (num_buckets == num_buckets_)
-      return;
-    num_buckets_ = num_buckets;
-    BOOST_ASIO_ASSERT(num_buckets_ != 0);
-    }
-    
-    // This function will create a Huffman tree.
-//
-// The catch here is that the tree cannot be arbitrarily deep.
-// Brotli specifies a maximum depth of 15 bits for 'code trees'
-// and 7 bits for 'code length code trees.'
-//
-// count_limit is the value that is to be faked as the minimum value
-// and this minimum value is raised until the tree matches the
-// maximum length requirement.
-//
-// This algorithm is not of excellent performance for very long data blocks,
-// especially when population counts are longer than 2**tree_limit, but
-// we are not planning to use this with extremely long blocks.
-//
-// See http://en.wikipedia.org/wiki/Huffman_coding
-void CreateHuffmanTree(const uint32_t *data,
-                       const size_t length,
-                       const int tree_limit,
-                       HuffmanTree* tree,
-                       uint8_t *depth) {
-  // For block sizes below 64 kB, we never need to do a second iteration
-  // of this loop. Probably all of our block sizes will be smaller than
-  // that, so this loop is mostly of academic interest. If we actually
-  // would need this, we would be better off with the Katajainen algorithm.
-  for (uint32_t count_limit = 1; ; count_limit *= 2) {
-    size_t n = 0;
-    for (size_t i = length; i != 0;) {
-      --i;
-      if (data[i]) {
-        const uint32_t count = std::max<uint32_t>(data[i], count_limit);
-        tree[n++] = HuffmanTree(count, -1, static_cast<int16_t>(i));
-      }
-    }
-    }
-    }
-    
-    namespace guetzli {
-    }
-    
-    // Definition of error codes for parsing jpeg files.
-    
-    namespace guetzli {
-    }
-    
-    namespace guetzli {
-    }
+    #include <folly/io/Cursor.h>
+#include <folly/io/IOBuf.h>
+#include <folly/lang/Bits.h>
