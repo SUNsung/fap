9cefdfde7987390eb7b5807aa3dde18c57fb2284
SUNsung <weatherius@gmail.com>
2018-09-11 06:49:00 +0300

    Rewrite [11Azw65mnIh..11zeETcQAm6Q11cU5.MlOQ52M11Rlz.cpp] File in: 2018-09-11 06:48:59

--- cccccc/11Azw65mnIh..11zeETcQAm6Q11cU5.MlOQ52M11Rlz.cpp
+++ cccccc/11Azw65mnIh..11zeETcQAm6Q11cU5.MlOQ52M11Rlz.cpp
@@ -1,226 +1,55 @@
 
         
-        namespace tensorflow {
-    }
-    
-    // Implementation of `tensorflow.MasterService`, based on the
-// definition in '//tensorflow/core/protobuf/master_service.proto',
-// and the gRPC generated stub and service classes.
-// See that file for the definition of methods and messages.
-class MasterService final {
- public:
-  class StubInterface {
-   public:
-    virtual ~StubInterface() {}
-    virtual ::grpc::Status CreateSession(::grpc::ClientContext* context,
-                                         const CreateSessionRequest& request,
-                                         CreateSessionResponse* response) = 0;
-    virtual ::grpc::Status ExtendSession(::grpc::ClientContext* context,
-                                         const ExtendSessionRequest& request,
-                                         ExtendSessionResponse* response) = 0;
-    virtual ::grpc::Status PartialRunSetup(
-        ::grpc::ClientContext* context, const PartialRunSetupRequest& request,
-        PartialRunSetupResponse* response) = 0;
-    virtual ::grpc::Status RunStep(::grpc::ClientContext* context,
-                                   const RunStepRequest& request,
-                                   RunStepResponse* response) = 0;
-    virtual ::grpc::Status CloseSession(::grpc::ClientContext* context,
-                                        const CloseSessionRequest& request,
-                                        CloseSessionResponse* response) = 0;
-    virtual ::grpc::Status ListDevices(::grpc::ClientContext* context,
-                                       const ListDevicesRequest& request,
-                                       ListDevicesResponse* response) = 0;
-    virtual ::grpc::Status Reset(::grpc::ClientContext* context,
-                                 const ResetRequest& request,
-                                 ResetResponse* response) = 0;
-    virtual ::grpc::Status MakeCallable(::grpc::ClientContext* context,
-                                        const MakeCallableRequest& request,
-                                        MakeCallableResponse* response) = 0;
-    virtual ::grpc::Status RunCallable(::grpc::ClientContext* context,
-                                       const RunCallableRequest& request,
-                                       RunCallableResponse* response) = 0;
-    virtual ::grpc::Status ReleaseCallable(
-        ::grpc::ClientContext* context, const ReleaseCallableRequest& request,
-        ReleaseCallableResponse* response) = 0;
-  };
-  class Stub final : public StubInterface {
-   public:
-    Stub(const std::shared_ptr< ::grpc::ChannelInterface>& channel);
-    ::grpc::Status CreateSession(::grpc::ClientContext* context,
-                                 const CreateSessionRequest& request,
-                                 CreateSessionResponse* response) override;
-    ::grpc::Status ExtendSession(::grpc::ClientContext* context,
-                                 const ExtendSessionRequest& request,
-                                 ExtendSessionResponse* response) override;
-    ::grpc::Status PartialRunSetup(::grpc::ClientContext* context,
-                                   const PartialRunSetupRequest& request,
-                                   PartialRunSetupResponse* response) override;
-    ::grpc::Status RunStep(::grpc::ClientContext* context,
-                           const RunStepRequest& request,
-                           RunStepResponse* response) override;
-    ::grpc::Status CloseSession(::grpc::ClientContext* context,
-                                const CloseSessionRequest& request,
-                                CloseSessionResponse* response) override;
-    ::grpc::Status ListDevices(::grpc::ClientContext* context,
-                               const ListDevicesRequest& request,
-                               ListDevicesResponse* response) override;
-    ::grpc::Status Reset(::grpc::ClientContext* context,
-                         const ResetRequest& request,
-                         ResetResponse* response) override;
-    ::grpc::Status MakeCallable(::grpc::ClientContext* context,
-                                const MakeCallableRequest& request,
-                                MakeCallableResponse* response) override;
-    ::grpc::Status RunCallable(::grpc::ClientContext* context,
-                               const RunCallableRequest& request,
-                               RunCallableResponse* response) override;
-    ::grpc::Status ReleaseCallable(::grpc::ClientContext* context,
-                                   const ReleaseCallableRequest& request,
-                                   ReleaseCallableResponse* response) override;
-    }
-    }
-    
-      Status ListDevices(CallOptions* call_options,
-                     const ListDevicesRequest* request,
-                     ListDevicesResponse* response) override {
-    ::grpc::ClientContext ctx;
-    return Call(&ctx, call_options, request, response,
-                &MasterServiceStub::ListDevices);
-  }
-    
-      const auto input_tensor_name = relu_input_op->inputs[0];
-  const auto output_tensor_name = add_op->outputs[0];
-    
-    REGISTER_OP('EncodeAudioV2')
-    .Input('sampled_audio: float')
-    .Input('file_format: string')
-    .Input('samples_per_second: int32')
-    .Input('bits_per_second: int32')
-    .Output('contents: string')
-    .SetShapeFn(shape_inference::ScalarShape)
-    .Doc(R'doc(
-Processes a `Tensor` containing sampled audio with the number of channels
-and length of the audio specified by the dimensions of the `Tensor`. The
-audio is converted into a string that, when saved to disk, will be equivalent
-to the audio in the specified audio format.
-    
-    namespace {
-    }
-    
-        D3D_FEATURE_LEVEL featureLevels[] =
-    {
-        D3D_FEATURE_LEVEL_11_0,
-        D3D_FEATURE_LEVEL_10_1,
-        D3D_FEATURE_LEVEL_10_0,
-    };
-    UINT numFeatureLevels = ARRAYSIZE(featureLevels);
-    
-    void compose_motion(InputArray _om1, InputArray _T1, InputArray _om2, InputArray _T2,
-                    Mat& om3, Mat& T3, Mat& dom3dom1, Mat& dom3dT1, Mat& dom3dom2,
-                    Mat& dom3dT2, Mat& dT3dom1, Mat& dT3dT1, Mat& dT3dom2, Mat& dT3dT2);
-    
-        virtual int runKernel( const CvMat*, const CvMat*, CvMat* );
-    virtual void computeReprojError( const CvMat*, const CvMat*,
-                                     const CvMat*, CvMat* );
-    
-    #undef cv_hal_Cholesky32f
-#define cv_hal_Cholesky32f lapack_Cholesky32f
-#undef cv_hal_Cholesky64f
-#define cv_hal_Cholesky64f lapack_Cholesky64f
-    
-    using namespace cv::ocl::runtime;
-    
-      // Gets the samples ready for training. Use after both
-  // ReadTrainingSamples+PostLoadCleanup or DeSerialize.
-  // Re-indexes the features and computes canonical and cloud features.
-  void PreTrainingSetup();
-    
-    
-    {
-    {      datadir = drive;
-      datadir += dir;
-      datadir += '/tessdata';
-    }
-#endif /* _WIN32 */
-#if defined(TESSDATA_PREFIX)
-  } else {
-/* Use tessdata prefix which was compiled in. */
-#define _STR(a) #a
-#define _XSTR(a) _STR(a)
-    datadir = _XSTR(TESSDATA_PREFIX) '/tessdata';
-#undef _XSTR
-#undef _STR
-#endif
-  }
-    
-        // Non-serialized cache data.
-    // Indexed features of the canonical sample.
-    GenericVector<int> canonical_features;
-    // The mapped features of all the samples.
-    BitVector cloud_features;
-    
-    #ifdef HAVE_CONFIG_H
-#include 'config_auto.h'
-#endif
-    
-        template <typename ElementType>
-    /*static*/ ValuePtr Value::CreateSequence(size_t dimension, const std::vector<size_t>& sequenceData, bool sequenceStartFlag, const DeviceDescriptor& device, bool readOnly/* = false*/)
-    {
-        //TODO: avoid data copy.
-        std::vector<std::vector<size_t>> input = { sequenceData };
-        return Create<ElementType>(dimension, input, {sequenceStartFlag}, device, readOnly);
-    }
-    
-        // operator += (vector)
-    // applied to each column
-    // This is a weird interface, as it makes also sense for a matrix. TODO: Fix this.
-    void operator+=(const ssematrixbase /*vector*/ &other)
-    {
-        auto &us = *this;
-        assert(other.cols() == 1);
-        foreach_coord (i, j, us)
-            us(i, j) += other[i];
-    }
-    
-        // prepare features
-    auto& featureNodes = m_net->FeatureNodes();
-    
-    using namespace std;
-using namespace Microsoft::MSR::ScriptableObjects;
-    
-        void getlattices(const std::wstring& key, std::shared_ptr<const latticepair>& L, size_t expectedframes) const
-    {
-        std::shared_ptr<latticepair> LP(new latticepair);
-        denlattices.getlattice(key, LP->second, expectedframes); // this loads the lattice from disk, using the existing L.second object
-        L = LP;
-    }
-    
-    
-    {    // CreateNetwork - create a network based on the network description
-    // networkDescription - network description
-    virtual void CreateNetwork(const std::string& networkDescription);
-    virtual void Init(const std::string& config);
-    virtual void Destroy();
-};
-    
-      virtual ~MemTableRep() { }
-    
-    const char* CompactionFilterFactoryJniCallback::Name() const {
-  return m_name.get();
+        std::shared_ptr<Channel> CreateInsecureChannelFromFd(const grpc::string& target,
+                                                     int fd) {
+  internal::GrpcLibrary init_lib;
+  init_lib.init();
+  return CreateChannelInternal(
+      '', grpc_insecure_channel_create_from_fd(target.c_str(), fd, nullptr));
 }
     
-    namespace rocksdb {
-BaseComparatorJniCallback::BaseComparatorJniCallback(
-    JNIEnv* env, jobject jComparator,
-    const ComparatorJniCallbackOptions* copt)
-    : JniCallback(env, jComparator),
-    mtx_compare(new port::Mutex(copt->use_adaptive_mutex)),
-    mtx_findShortestSeparator(new port::Mutex(copt->use_adaptive_mutex)) {
-    }
+    namespace grpc {
     }
     
-      // call once at the beginning of a test to setup the dependency between
-  // sync points and setup markers indicating the successor is only enabled
-  // when it is processed on the same thread as the predecessor.
-  // When adding a marker, it implicitly adds a dependency for the marker pair.
-  void LoadDependencyAndMarkers(const std::vector<SyncPointPair>& dependencies,
-                                const std::vector<SyncPointPair>& markers);
+    #include <grpcpp/support/channel_arguments.h>
+    
+    
+    {}  // namespace grpc
+    
+    constexpr size_t TraceContextEncoding::kGrpcTraceContextSize;
+constexpr size_t TraceContextEncoding::kEncodeDecodeFailure;
+constexpr size_t TraceContextEncoding::kVersionIdSize;
+constexpr size_t TraceContextEncoding::kFieldIdSize;
+constexpr size_t TraceContextEncoding::kVersionIdOffset;
+constexpr size_t TraceContextEncoding::kVersionId;
+    
+    
+    {    uint8_t version = buf[kVersionIdOffset];
+    uint32_t fieldID = buf[kServerElapsedTimeOffset];
+    if (version != kVersionId || fieldID != kServerElapsedTimeField) {
+      *time = 0;
+      return kEncodeDecodeFailure;
+    }
+    *time = absl::little_endian::Load64(
+        &buf[kServerElapsedTimeOffset + kFieldIdSize]);
+    return kRpcServerStatsSize;
+  }
+    
+    void CensusServerCallData::Destroy(grpc_call_element* elem,
+                                   const grpc_call_final_info* final_info,
+                                   grpc_closure* then_call_closure) {
+  const uint64_t request_size = GetOutgoingDataSize(final_info);
+  const uint64_t response_size = GetIncomingDataSize(final_info);
+  double elapsed_time_ms = absl::ToDoubleMilliseconds(elapsed_time_);
+  grpc_auth_context_release(auth_context_);
+  ::opencensus::stats::Record(
+      {{RpcServerSentBytesPerRpc(), static_cast<double>(response_size)},
+       {RpcServerReceivedBytesPerRpc(), static_cast<double>(request_size)},
+       {RpcServerServerLatency(), elapsed_time_ms},
+       {RpcServerSentMessagesPerRpc(), sent_message_count_},
+       {RpcServerReceivedMessagesPerRpc(), recv_message_count_}},
+      {{ServerMethodTagKey(), method_},
+       {ServerStatusTagKey(), StatusCodeToString(final_info->final_status)}});
+  grpc_slice_unref_internal(path_);
+  context_.EndSpan();
+}
