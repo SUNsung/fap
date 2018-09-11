20ab5a1ce126a8a0fec010e50d95febaca30f103
SUNsung <weatherius@gmail.com>
2018-09-11 22:02:30 +0300

    Rewrite [02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py] File in: 2018-09-11 22:02:29

--- pyton/02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py
+++ pyton/02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py
@@ -1,152 +1,184 @@
 
         
-            if skip or _internal_mark_re.search(docstring) is not None:
-        return True
+          # Print a classification report
+  print('%s classification report:' % set_name)
+  print(metrics.classification_report(labels, pred, target_names=classes))
     
-        # register the database commands
-    from flaskr import db
-    db.init_app(app)
+      def __mlp__(self):
+    '''Performs the MLP operations.
     
-        Checks that the id exists and optionally that the current user is
-    the author.
+        # Note the use of the plural in all of these quantities.  This is intended
+    # to mark that even though a sample z_t from the posterior is thought of a
+    # single sample of a multidimensional gaussian, the prior is actually
+    # thought of as U AR(1) processes, where U is the dimension of the inferred
+    # input.
+    size_bx1 = tf.stack([batch_size, 1])
+    size__xu = [None, z_size]
+    # process variance, the variance at time t over all instantiations of AR(1)
+    # with these parameters.
+    log_evar_inits_1xu = tf.expand_dims(tf.log(noise_variances), 0)
+    self.logevars_1xu = logevars_1xu = \
+        tf.Variable(log_evar_inits_1xu, name=name+'/logevars', dtype=tf.float32,
+                    trainable=do_train_prior_ar_nvar)
+    self.logevars_bxu = logevars_bxu = tf.tile(logevars_1xu, size_bx1)
+    logevars_bxu.set_shape(size__xu) # tile loses shape
     
-        def auto_pop(self, exc):
-        if self.request.environ.get('flask._preserve_context') or \
-           (exc is not None and self.app.preserve_context_on_exception):
-            self.preserved = True
-            self._preserved_exc = exc
-        else:
-            self.pop(exc)
+      if ext_input_bxtxi is not None:
+    input_title += ' External Input'
+    plot_time_series(ext_input_bxtxi, n_to_plot=n_to_plot, color='b',
+                     scale=scale, title=col_title + input_title)
     
-        :copyright: © 2010 by the Pallets team.
-    :license: BSD, see LICENSE for more details.
-'''
+      Args:
+    datasets: The dictionary of dataset structures.
+    npcs:  The number of pcs for each, basically like lfads factors.
+    nsamples (optional): Number of samples to take for each dataset.
+    ntime (optional): Number of time steps to take in each sample.
     
-            @app.route('/stream')
-        def streamed_response():
-            def generate():
-                yield 'Hello '
-                yield request.args['name']
-                yield '!'
-            return Response(stream_with_context(generate()))
+      Args:
+    values_t_bxn: The length T list of BxN numpy tensors.
     
-    from __future__ import absolute_import
-    
-    
-# bdist_wheel
-extras_require = {
-    # http://wheel.readthedocs.io/en/latest/#defining-conditional-dependencies
-    'python_version == '3.0' or python_version == '3.1'': ['argparse>=1.2.1'],
-    ':sys_platform == 'win32'': ['colorama>=0.2.4'],
-}
-    
-    
-def test_default_options_overwrite(httpbin):
-    env = MockEnvironment()
-    env.config['default_options'] = ['--form']
-    env.config.save()
-    r = http('--json', httpbin.url + '/post', 'foo=bar', env=env)
-    assert r.json['json'] == {'foo': 'bar'}
-    
-    
-def rst_filenames():
-    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
-        if '.tox' not in root:
-            for filename in fnmatch.filter(filenames, '*.rst'):
-                yield os.path.join(root, filename)
-    
-    from httpie.compat import urlopen
-from httpie.downloads import (
-    parse_content_range, filename_from_content_disposition, filename_from_url,
-    get_unique_filename, ContentRangeError, Downloader,
-)
-from utils import http, MockEnvironment
-    
-    
-@mock.patch('httpie.core.get_response')
-def test_error_traceback(get_response):
-    exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    with raises(ConnectionError):
-        main(['--ignore-stdin', '--traceback', 'www.google.com'])
-    
-    
-def test_json_input_preserve_order(httpbin_both):
-    r = http('PATCH', httpbin_both + '/patch',
-             'order:={'map':{'1':'first','2':'second'}}')
-    assert HTTP_OK in r
-    assert r.json['data'] == \
-        '{'order': {'map': {'1': 'first', '2': 'second'}}}'
+      @property
+  def vocab(self):
+    return self._vocab
 
     
-        # Arguments
-        input_shape (tensor): shape of input image tensor
-        depth (int): number of core convolutional layers
-        num_classes (int): number of classes (CIFAR10 has 10)
+      Args:
+    raw_data: one of the raw data outputs from imdb_raw_data.
+    batch_size: int, the batch size.
+    num_steps: int, the number of unrolls.
     
-        def __init__(self, min_value=0.0, max_value=1.0, rate=1.0, axis=0):
-        self.min_value = min_value
-        self.max_value = max_value
-        self.rate = rate
-        self.axis = axis
+      if not FLAGS.seq2seq_share_embedding:
+    variable_mapping = {
+        str(model_str) + '/embedding':
+            encoder_embedding,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/kernel':
+            encoder_lstm_w_0,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/bias':
+            encoder_lstm_b_0,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/kernel':
+            encoder_lstm_w_1,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/bias':
+            encoder_lstm_b_1
+    }
+  else:
+    variable_mapping = {
+        str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/kernel':
+            encoder_lstm_w_0,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/bias':
+            encoder_lstm_b_0,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/kernel':
+            encoder_lstm_w_1,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/bias':
+            encoder_lstm_b_1
+    }
+  return variable_mapping
     
-        dirname = 'cifar-100-python'
-    origin = 'https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz'
-    path = get_file(dirname, origin=origin, untar=True)
+            # attn_fun
+        scores = _attn_add_fun(score_v, keys, query)
+      elif attention_option == 'luong':
+        # reshape query: [batch_size, 1, num_units]
+        query = tf.reshape(query, [-1, 1, num_units])
     
-    
-def recurrent_args_preprocessor(args, kwargs):
-    converted = []
-    if 'forget_bias_init' in kwargs:
-        if kwargs['forget_bias_init'] == 'one':
-            kwargs.pop('forget_bias_init')
-            kwargs['unit_forget_bias'] = True
-            converted.append(('forget_bias_init', 'unit_forget_bias'))
-        else:
-            kwargs.pop('forget_bias_init')
-            warnings.warn('The `forget_bias_init` argument '
-                          'has been ignored. Use `unit_forget_bias=True` '
-                          'instead to initialize with ones.', stacklevel=3)
-    if 'input_dim' in kwargs:
-        input_length = kwargs.pop('input_length', None)
-        input_dim = kwargs.pop('input_dim')
-        input_shape = (input_length, input_dim)
-        kwargs['input_shape'] = input_shape
-        converted.append(('input_dim', 'input_shape'))
-        warnings.warn('The `input_dim` and `input_length` arguments '
-                      'in recurrent layers are deprecated. '
-                      'Use `input_shape` instead.', stacklevel=3)
-    return args, kwargs, converted
-    
-    
-@keras_test
-def test_vector_classification_functional():
-    (x_train, y_train), (x_test, y_test) = get_test_data(num_train=500,
-                                                         num_test=200,
-                                                         input_shape=(20,),
-                                                         classification=True,
-                                                         num_classes=num_classes)
-    # Test with functional API
-    inputs = layers.Input(shape=(x_train.shape[-1],))
-    x = layers.Dense(16, activation=keras.activations.relu)(inputs)
-    x = layers.Dense(8)(x)
-    x = layers.Activation('relu')(x)
-    outputs = layers.Dense(num_classes, activation='softmax')(x)
-    model = keras.models.Model(inputs, outputs)
-    model.compile(loss=keras.losses.sparse_categorical_crossentropy,
-                  optimizer=keras.optimizers.RMSprop(),
-                  metrics=['acc'])
-    history = model.fit(x_train, y_train, epochs=15, batch_size=16,
-                        validation_data=(x_test, y_test),
-                        verbose=0)
-    assert(history.history['val_acc'][-1] > 0.8)
+        def validate_invalidation_batch(self, invalidation_batch, caller_reference):
+        try:
+            if caller_reference is not None:
+                valid_caller_reference = caller_reference
+            else:
+                valid_caller_reference = datetime.datetime.now().isoformat()
+            valid_invalidation_batch = {
+                'paths': self.create_aws_list(invalidation_batch),
+                'caller_reference': valid_caller_reference
+            }
+            return valid_invalidation_batch
+        except (ClientError, BotoCoreError) as e:
+            self.module.fail_json_aws(e, msg='Error validating invalidation batch.')
     
     
-def test_selu():
-    x = K.placeholder(ndim=2)
-    f = K.function([x], [activations.selu(x)])
-    alpha = 1.6732632423543772848170429916717
-    scale = 1.0507009873554804934193349852946
+def wait_until_table_active(module, table, wait_timeout):
+    max_wait_time = time.time() + wait_timeout
+    while (max_wait_time > time.time()) and (table.describe()['Table']['TableStatus'] != 'ACTIVE'):
+        time.sleep(5)
+    if max_wait_time <= time.time():
+        # waiting took too long
+        module.fail_json(msg='timed out waiting for table to exist')
     
-            p = random.choice(self.plus)
+    - name: Gather facts about a filtered list of customer gateways, based on tags
+  ec2_customer_gateway_facts:
+    region: ap-southeast-2
+    filters:
+      'tag:Name': test-customer-gateway
+      'tag:AltName': test-customer-gateway-alt
+  register: cust_gw_facts
+    
+        if isinstance:
+        try:
+            reservations = ec2.get_all_reservations(instance_ids=[device_id])
+        except boto.exception.EC2ResponseError as e:
+            module.fail_json(msg=str(e))
+    
+        try:
+        key = ec2_client.import_key_pair(KeyName=name, PublicKeyMaterial=to_bytes(key_material))
+    except ClientError as err:
+        module.fail_json_aws(err, msg='error importing key')
+    return key
+    
+    
+import traceback
+from ansible.module_utils.ec2 import (get_aws_connection_info, ec2_argument_spec, ec2_connect, camel_dict_to_snake_dict, get_ec2_security_group_ids_from_names,
+                                      boto3_conn, snake_dict_to_camel_dict, HAS_BOTO3)
+from ansible.module_utils._text import to_text
+from ansible.module_utils.basic import AnsibleModule
+    
+        region, ec2_url, aws_connect_params = get_aws_connection_info(module, True)
+    
+        By default this will get the strings from the blns.txt file
+    
+        def test_decode_bad(self):
+        self.assertRaises(jose.DeserializationError, self.field.decode, 'y')
+    
+    
+def _guess_loader(filename, loader_pem, loader_der):
+    _, ext = os.path.splitext(filename)
+    if ext.lower() == '.pem':
+        return loader_pem
+    elif ext.lower() == '.der':
+        return loader_der
+    else:  # pragma: no cover
+        raise ValueError('Loader could not be recognized based on extension')
+    
+        :ivar aug: Augeas object
+    :type aug: :class:`augeas.Augeas`
+    
+        def test_filter_args_num(self):
+        '''Note: This may also fail do to Include conf-enabled/ syntax.'''
+        matches = self.parser.find_dir('TestArgsDirective')
+    
+            self.vhost1b = VirtualHost(
+            'filep', 'vh_path', set([self.addr1]), False, False, 'localhost')
+    
+        def _get_addrs(self, achall):
+        '''Return the Apache addresses needed for TLS-SNI-01.'''
+        # TODO: Checkout _default_ rules.
+        addrs = set()
+        default_addr = obj.Addr(('*', str(
+            self.configurator.config.tls_sni_01_port)))
+    
+    # Send the message via local SMTP server.
+with smtplib.SMTP('localhost') as s:
+    s.send_message(msg)
+
+    
+    def test():
+    manager = MyManager()
+    manager.start()
+    
+                if buffer.lstrip().upper().startswith('SELECT'):
+                print(cur.fetchall())
+        except sqlite3.Error as e:
+            print('An error occurred:', e.args[0])
+        buffer = ''
+    
+        headers=extension,extension,...
+      The allowed header extensions that cpplint will consider to be header files
+      (by default, only files with extensions %s
+      will be assumed to be headers)
