daff40313af0eacfbe218f859b1cfdfed08d2d94
SUNsung <weatherius@gmail.com>
2018-09-11 05:02:37 +0300

    Rewrite [34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp] File in: 2018-09-11 05:02:36

--- cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
+++ cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
@@ -1,153 +1,267 @@
 
         
-          /// Retrieve the array of replacement types, which line up with the
-  /// generic parameters.
-  ///
-  /// Note that the types may be null, for cases where the generic parameter
-  /// is concrete but hasn't been queried yet.
-  ArrayRef<Type> getReplacementTypes() const {
-    return llvm::makeArrayRef(getTrailingObjects<Type>(),
-                              getNumReplacementTypes());
-  }
+        #include 'stdafx.h'
+#include 'CNTKLibrary.h'
+#include 'Utils.h'
     
-    bool swift::parseASTSection(SerializedModuleLoader *SML, StringRef buf,
-                            SmallVectorImpl<std::string> &foundModules) {
-  if (!serialization::isSerializedAST(buf))
-    return false;
-    }
+            static bool IsUDF(const Dictionary& dict);
     
-    ClangDiagnosticConsumer::ClangDiagnosticConsumer(
-    ClangImporter::Implementation &impl,
-    clang::DiagnosticOptions &clangDiagOptions,
-    bool dumpToStderr)
-  : TextDiagnosticPrinter(llvm::errs(), &clangDiagOptions),
-    ImporterImpl(impl), DumpToStderr(dumpToStderr) {}
-    
-        struct Margin {
-        Margin() : left(0), right(0), top(0), bottom(0) {}
-        Margin(size_t left_, size_t right_, size_t top_, size_t bottom_)
-            : left(left_), right(right_), top(top_), bottom(bottom_) {}
-    }
-    
-    
-    {} //namespace CAROTENE_NS
-
-    
-    void bitwiseAnd(const Size2D &size,
-                const u8 *src0Base, ptrdiff_t src0Stride,
-                const u8 *src1Base, ptrdiff_t src1Stride,
-                u8 *dstBase, ptrdiff_t dstStride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    internal::vtransform(size,
-                         src0Base, src0Stride,
-                         src1Base, src1Stride,
-                         dstBase, dstStride, BitwiseAnd());
-#else
-    (void)size;
-    (void)src0Base;
-    (void)src0Stride;
-    (void)src1Base;
-    (void)src1Stride;
-    (void)dstBase;
-    (void)dstStride;
-#endif
-}
-    
-            //left&right borders
-        if (borderType != BORDER_MODE_CONSTANT)
-            for (s32 k = 0; k < cn; ++k)
+                // Validate that each of the dynamic axes are unique
+            std::unordered_set<Axis> uniqueDynamicAxis;
+            for (auto& currentDynamicAxis : dynamicAxes)
             {
-                lanea[-cn+k] = lanea[idx_l + k];
-                lanea[colsn+k] = lanea[idx_r + k];
-                laneb[-cn+k] = laneb[idx_l + k];
-                laneb[colsn+k] = laneb[idx_r + k];
+                auto retVal = uniqueDynamicAxis.insert(currentDynamicAxis);
+                if (!retVal.second)
+                    InvalidArgument('Dynamic axis named %S is specified more than once for Variable '%S'', currentDynamicAxis.Name().c_str(), AsString().c_str());
             }
     
-                    if (border == BORDER_MODE_CONSTANT && x4 < 0)
-                    prevx[0] = prevx[1] = prevx[2] = borderValue;
-                else
-                {
-                    prevx[0] = srow0 ? srow0[x4] : borderValue;
-                    prevx[1] =         srow1[x4]              ;
-                    prevx[2] = srow2 ? srow2[x4] : borderValue;
-                }
-    
-     *Redistributions of source code must retain the above copyright
-  notice, this list of conditions and the following disclaimer.
-    
-        if (flipMode == FLIP_VERTICAL_MODE)
-    {
-        for (size_t y = 0; y < size.height; ++y)
-        {
-            const u8 * src_row = internal::getRowPtr(srcBase, srcStride, y);
-            u8 * dst_row = internal::getRowPtr(dstBase, dstStride, size.height - y - 1);
-    }
-    }
-    
-    #include 'intsimdmatrixsse.h'
-    
-    #endif  // TESSERACT_CCMAIN_EQUATIONDETECT_H_
-
-    
-      bool has_leaders;   // does the line contain leader dots (.....)?
-  bool has_drop_cap;  // does the line have a drop cap?
-  int pix_ldistance;  // distance to the left pblock boundary in pixels
-  int pix_rdistance;  // distance to the right pblock boundary in pixels
-  float pix_xheight;  // guessed xheight for the line
-  int average_interword_space; // average space between words in pixels.
-    
-    #include 'osquery/config/parsers/prometheus_targets.h'
-    
-      // serialize the QueryLogItem and make sure decorations go top level
-  auto doc = JSON::newObject();
-  auto status = serializeQueryLogItem(item, doc);
-  std::string expected = 'test';
-  std::string result = doc.doc()['load_test'].GetString();
-  EXPECT_EQ(result, expected);
-    
-      ASSERT_TRUE(data.doc().HasMember('events'));
-  ASSERT_TRUE(data.doc()['events'].HasMember('environment_variables'));
-  ASSERT_TRUE(data.doc()['events']['environment_variables'].IsArray());
-  for (const auto& var :
-       data.doc()['events']['environment_variables'].GetArray()) {
-    std::string value = var.GetString();
-    EXPECT_TRUE(value == 'foo' || value == 'bar');
-  }
+       BOOST_ASSERT(rep->type == syntax_element_char_rep);
+   BOOST_ASSERT(rep->next.p != 0);
+   BOOST_ASSERT(rep->alt.p != 0);
+   BOOST_ASSERT(rep->next.p->type == syntax_element_literal);
+   BOOST_ASSERT(count < rep->max);
     
     
-    {/**
- * @brief Compute a hash digest from the contents of a buffer.
- *
- * @param hash_type The osquery-supported hash algorithm.
- * @param buffer A caller-controlled buffer (already allocated).
- * @param size The length of buffer in bytes.
- * @return A string (hex) representation of the hash digest.
- */
-std::string hashFromBuffer(HashType hash_type, const void* buffer, size_t size);
-} // namespace osquery
-
+enum{
+   sort_C,
+   sort_fixed,
+   sort_delim,
+   sort_unknown
+};
     
-      std::shared_ptr<PlatformProcess> process;
-  int process_pid = ::fork();
-  if (process_pid == 0) {
-    // Start a Python script
-    ::execlp('sh', 'sh', '-c', argv.c_str(), nullptr);
-    ::exit(0);
-  } else if (process_pid > 0) {
-    process.reset(new PlatformProcess(process_pid));
-  }
+    class BOOST_REGEX_DECL abstract_protected_call
+{
+public:
+   bool BOOST_REGEX_CALL execute()const;
+   // this stops gcc-4 from complaining:
+   virtual ~abstract_protected_call(){}
+private:
+   virtual bool call()const = 0;
+};
     
-    TEST_F(QueryTests, test_query_name_not_found_in_db) {
-  // Try to retrieve results from a query that has not executed.
-  QueryDataSet previous_qd;
-  auto query = getOsqueryScheduledQuery();
-  auto cf = Query('not_a_real_query', query);
-  auto status = cf.getPreviousQueryResults(previous_qd);
-  EXPECT_FALSE(status.ok());
-  EXPECT_TRUE(previous_qd.empty());
+    template <class OutputIterator, class Iterator, class traits, class charT>
+inline OutputIterator regex_merge(OutputIterator out,
+                         Iterator first,
+                         Iterator last,
+                         const basic_regex<charT, traits>& e, 
+                         const std::basic_string<charT>& fmt,
+                         match_flag_type flags = match_default)
+{
+   return regex_merge(out, first, last, e, fmt.c_str(), flags);
 }
     
-      /// Paths to autoload extensions.
-  std::vector<std::string> extensions_paths_;
+     /*
+  *   LOCATION:    see http://www.boost.org for most recent version.
+  *   FILE         regex_search.hpp
+  *   VERSION      see <boost/version.hpp>
+  *   DESCRIPTION: Provides regex_search implementation.
+  */
+    
+    template <class BidirectionalIterator,
+          class charT,
+          class traits>
+class regex_token_iterator_implementation 
+{
+   typedef basic_regex<charT, traits> regex_type;
+   typedef sub_match<BidirectionalIterator>      value_type;
+    }
+    
+    void Action::startWithTarget(Node *aTarget)
+{
+    _originalTarget = _target = aTarget;
+}
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+    
+    protected:
+    cocos2d::Size _sizeDelta;
+    cocos2d::Size _startSize;
+    cocos2d::Size _previousSize;
+    
+        // actions
+    
+    /** Adds an action with a target. 
+     If the target is already present, then the action will be added to the existing target.
+     If the target is not present, a new instance of this target will be created either paused or not, and the action will be added to the newly created target.
+     When the target is paused, the queued actions won't be 'ticked'.
+     *
+     * @param action    A certain action.
+     * @param target    The target which need to be added an action.
+     * @param paused    Is the target paused or not.
+     */
+    virtual void addAction(Action *action, Node *target, bool paused);
+    
+    // implementation of ShakyTiles3D
+    
+        for (auto& spriteFrame : frames)
+    {
+        auto animFrame = AnimationFrame::create(spriteFrame, 1, ValueMap());
+        _frames.pushBack(animFrame);
+        _totalDelayUnits++;
+    }
+    
+        /** Gets the total Delay units of the Animation. 
+     *
+     * @return The total Delay units of the Animation.
+     */
+    float getTotalDelayUnits() const { return _totalDelayUnits; };
+    
+    /** Sets the delay in seconds of the 'delay unit'.
+     *
+     * @param delayPerUnit The delay in seconds of the 'delay unit'.
+     */
+    void setDelayPerUnit(float delayPerUnit) { _delayPerUnit = delayPerUnit; };
+    
+    /** Gets the delay in seconds of the 'delay unit'.
+     * 
+     * @return The delay in seconds of the 'delay unit'.
+     */
+    float getDelayPerUnit() const { return _delayPerUnit; };
+    
+        /** Adds an animation from a plist file.
+     * Make sure that the frames were previously loaded in the SpriteFrameCache.
+     * @since v1.1
+     * @js addAnimations
+     * @lua addAnimations
+     * @param plist An animation from a plist file.
+     */
+    void addAnimationsWithFile(const std::string& plist);
+    
+      t7 = a[4].imag;
+  t4 = a[0].imag - t7;
+  t7 += a[0].imag;
+  a[0].imag = t7;
+    
+    namespace guetzli {
+    }
+    
+    #ifndef GUETZLI_FAST_LOG_H_
+#define GUETZLI_FAST_LOG_H_
+    
+    #endif  // GUETZLI_IDCT_H_
+
+    
+    #include 'guetzli/jpeg_data.h'
+    
+    // Quantization values for an 8x8 pixel block.
+struct JPEGQuantTable {
+  JPEGQuantTable() : values(kDCTBlockSize), precision(0),
+                     index(0), is_last(true) {}
+    }
+    
+    #include 'guetzli/jpeg_data_decoder.h'
+    
+      virtual void Run(State& state) = 0;
+    
+    // That gcc wants both of these prototypes seems mysterious. VC, for
+// its part, can't decide which to use (another mystery). Matching of
+// template overloads: the final frontier.
+#ifndef COMPILER_MSVC
+template <typename T, size_t N>
+char (&ArraySizeHelper(const T (&array)[N]))[N];
+#endif
+    
+    State::State(size_t max_iters, const std::vector<int>& ranges, int thread_i,
+             int n_threads, internal::ThreadTimer* timer,
+             internal::ThreadManager* manager)
+    : started_(false),
+      finished_(false),
+      total_iterations_(max_iters + 1),
+      range_(ranges),
+      bytes_processed_(0),
+      items_processed_(0),
+      complexity_n_(0),
+      error_occurred_(false),
+      counters(),
+      thread_index(thread_i),
+      threads(n_threads),
+      max_iterations(max_iters),
+      timer_(timer),
+      manager_(manager) {
+  CHECK(max_iterations != 0) << 'At least one iteration must be run';
+  CHECK(total_iterations_ != 0) << 'max iterations wrapped around';
+  CHECK_LT(thread_index, threads) << 'thread_index must be less than threads';
+}
+    
+    // Returns true if stdout appears to be a terminal that supports colored
+// output, false otherwise.
+bool IsColorTerminal();
+    
+    bool SameNames(UserCounters const& l, UserCounters const& r) {
+  if (&l == &r) return true;
+  if (l.size() != r.size()) {
+    return false;
+  }
+  for (auto const& c : l) {
+    if (r.find(c.first) == r.end()) {
+      return false;
+    }
+  }
+  return true;
+}
+    
+    
+    {
+    {
+    {  // Native Client does not provide any API to access cycle counter.
+  // Use clock_gettime(CLOCK_MONOTONIC, ...) instead of gettimeofday
+  // because is provides nanosecond resolution (which is noticable at
+  // least for PNaCl modules running on x86 Mac & Linux).
+  // Initialize to always return 0 if clock_gettime fails.
+  struct timespec ts = { 0, 0 };
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+  return static_cast<int64_t>(ts.tv_sec) * 1000000000 + ts.tv_nsec;
+#elif defined(__aarch64__)
+  // System timer of ARMv8 runs at a different frequency than the CPU's.
+  // The frequency is fixed, typically in the range 1-50MHz.  It can be
+  // read at CNTFRQ special register.  We assume the OS has set up
+  // the virtual timer properly.
+  int64_t virtual_timer_value;
+  asm volatile('mrs %0, cntvct_el0' : '=r'(virtual_timer_value));
+  return virtual_timer_value;
+#elif defined(__ARM_ARCH)
+  // V6 is the earliest arch that has a standard cyclecount
+  // Native Client validator doesn't allow MRC instructions.
+#if (__ARM_ARCH >= 6)
+  uint32_t pmccntr;
+  uint32_t pmuseren;
+  uint32_t pmcntenset;
+  // Read the user mode perf monitor counter access permissions.
+  asm volatile('mrc p15, 0, %0, c9, c14, 0' : '=r'(pmuseren));
+  if (pmuseren & 1) {  // Allows reading perfmon counters for user mode code.
+    asm volatile('mrc p15, 0, %0, c9, c12, 1' : '=r'(pmcntenset));
+    if (pmcntenset & 0x80000000ul) {  // Is it counting?
+      asm volatile('mrc p15, 0, %0, c9, c13, 0' : '=r'(pmccntr));
+      // The counter is set up to count every 64th cycle
+      return static_cast<int64_t>(pmccntr) * 64;  // Should optimize to << 6
+    }
+  }
+#endif
+  struct timeval tv;
+  gettimeofday(&tv, nullptr);
+  return static_cast<int64_t>(tv.tv_sec) * 1000000 + tv.tv_usec;
+#elif defined(__mips__)
+  // mips apparently only allows rdtsc for superusers, so we fall
+  // back to gettimeofday.  It's possible clock_gettime would be better.
+  struct timeval tv;
+  gettimeofday(&tv, nullptr);
+  return static_cast<int64_t>(tv.tv_sec) * 1000000 + tv.tv_usec;
+#else
+// The soft failover to a generic implementation is automatic only for ARM.
+// For other platforms the developer is expected to make an attempt to create
+// a fast implementation and use generic version if nothing better is available.
+#error You need to define CycleTimer for your OS and CPU
+#endif
+}
+}  // end namespace cycleclock
+}  // end namespace benchmark
+    
+          // regerror returns the number of bytes necessary to null terminate
+      // the string, so we move that when assigning to error.
+      CHECK_NE(needed, 0);
+      error->assign(errbuf, needed - 1);
