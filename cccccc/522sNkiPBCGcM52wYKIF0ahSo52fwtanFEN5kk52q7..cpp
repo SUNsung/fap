bb48af087e754b03952c621e1cac5a9ea3e566fa
SUNsung <weatherius@gmail.com>
2018-09-11 04:31:31 +0300

    Rewrite [522sNkiPBCGcM52wYKIF0ahSo52fwtanFEN5kk52q7..cpp] File in: 2018-09-11 04:31:31

--- cccccc/522sNkiPBCGcM52wYKIF0ahSo52fwtanFEN5kk52q7..cpp
+++ cccccc/522sNkiPBCGcM52wYKIF0ahSo52fwtanFEN5kk52q7..cpp
@@ -1,155 +1,199 @@
 
         
-        #ifdef __SSE4_1__
-// Computes part of matrix.vector v = Wu. Computes 1 result.
-static void PartialMatrixDotVector1(const int8_t* wi, const double* scales,
-                                    const int8_t* u, int num_in, int num_out,
-                                    double* v) {
-  int total = IntDotProductSSE(u, wi, num_in);
-  // Add in the bias and correct for integer values.
-  *v = (static_cast<double>(total) / INT8_MAX + wi[num_in]) * *scales;
-}
-#endif  // __SSE4_1__
+        namespace internal {
+    }
     
-    enum GARBAGE_LEVEL
-{
-  G_NEVER_CRUNCH,
-  G_OK,
-  G_DODGY,
-  G_TERRIBLE
+    // Prints the given number of bytes in the given object to the given
+// ostream.
+GTEST_API_ void PrintBytesInObjectTo(const unsigned char* obj_bytes,
+                                     size_t count,
+                                     ::std::ostream* os);
+    
+    // Helper function for *_STRNE on wide strings.
+//
+// INTERNAL IMPLEMENTATION - DO NOT USE IN A USER PROGRAM.
+GTEST_API_ AssertionResult CmpHelperSTRNE(const char* s1_expression,
+                                          const char* s2_expression,
+                                          const wchar_t* s1,
+                                          const wchar_t* s2);
+    
+    // Makes sure this header is not included before gtest.h.
+#ifndef GTEST_INCLUDE_GTEST_GTEST_H_
+# error Do not include gtest_pred_impl.h directly.  Include gtest.h instead.
+#endif  // GTEST_INCLUDE_GTEST_GTEST_H_
+    
+     private:
+  class Iterator : public ParamIteratorInterface<ParamType> {
+   public:
+    Iterator(const ParamGeneratorInterface<ParamType>* base,
+      const ParamGenerator<T1>& g1,
+      const typename ParamGenerator<T1>::iterator& current1,
+      const ParamGenerator<T2>& g2,
+      const typename ParamGenerator<T2>::iterator& current2,
+      const ParamGenerator<T3>& g3,
+      const typename ParamGenerator<T3>::iterator& current3,
+      const ParamGenerator<T4>& g4,
+      const typename ParamGenerator<T4>::iterator& current4,
+      const ParamGenerator<T5>& g5,
+      const typename ParamGenerator<T5>::iterator& current5)
+        : base_(base),
+          begin1_(g1.begin()), end1_(g1.end()), current1_(current1),
+          begin2_(g2.begin()), end2_(g2.end()), current2_(current2),
+          begin3_(g3.begin()), end3_(g3.end()), current3_(current3),
+          begin4_(g4.begin()), end4_(g4.end()), current4_(current4),
+          begin5_(g5.begin()), end5_(g5.end()), current5_(current5)    {
+      ComputeCurrentValue();
+    }
+    virtual ~Iterator() {}
+    }
+    
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9, typename T10,
+    typename T11, typename T12, typename T13, typename T14, typename T15,
+    typename T16, typename T17, typename T18, typename T19, typename T20,
+    typename T21, typename T22, typename T23, typename T24, typename T25,
+    typename T26>
+struct Types26 {
+  typedef T1 Head;
+  typedef Types25<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15,
+      T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26> Tail;
 };
     
-      // Before insert part back into part_grid_, we will need re-compute some
-  // of its attributes such as first_column_, last_column_. However, we still
-  // want to preserve its type.
-  BlobTextFlowType flow_type = part->flow();
-  PolyBlockType part_type = part->type();
-  BlobRegionType blob_type = part->blob_type();
-    
-      // Compute the super bounding box for all colpartitions inside part_grid_.
-  void ComputeCPsSuperBBox();
-    
-    const PageIterator& PageIterator::operator=(const PageIterator& src) {
-  page_res_ = src.page_res_;
-  tesseract_ = src.tesseract_;
-  include_upper_dots_ = src.include_upper_dots_;
-  include_lower_dots_ = src.include_lower_dots_;
-  scale_ = src.scale_;
-  scaled_yres_ = src.scaled_yres_;
-  rect_left_ = src.rect_left_;
-  rect_top_ = src.rect_top_;
-  rect_width_ = src.rect_width_;
-  rect_height_ = src.rect_height_;
-  delete it_;
-  it_ = new PAGE_RES_IT(*src.it_);
-  BeginWord(src.blob_index_);
-  return *this;
-}
-    
-    
-    {  bool rword_indicates_list_item;
-  bool rword_likely_starts_idea;
-  bool rword_likely_ends_idea;
+    template <GTEST_TEMPLATE_ T1>
+struct Templates1 {
+  typedef TemplateSel<T1> Head;
+  typedef Templates0 Tail;
 };
     
-      ~Carver();
+    // A sample program demonstrating using Google C++ testing framework.
+//
+// Author: wan@google.com (Zhanyong Wan)
     
-    #include <osquery/config.h>
-#include <osquery/logger.h>
-#include <osquery/registry_factory.h>
+    #include 'sample2.h'
     
-    /**
- * @brief Hash is a general utility class for hashing content
- *
- * @code{.cpp}
- *   Hash my_hash(HASH_TYPE_SHA256);
- *   my_hash.update(my_buffer, my_buffer_size);
- *   std::cout << my_hash.digest();
- * @endcode
- *
- */
-class Hash : private boost::noncopyable {
- public:
-  /**
-   * @brief Hash constructor
+      /*
+   * @brief the uri used to begin POSTing carve data
    *
-   * The hash class should be initialized with one of osquery::HashType as a
-   * constructor argument.
-   *
-   * @param algorithm The hashing algorithm which will be used to compute the
-   * hash
+   * This endpoint should negotiate the details of the carve, as well
+   * as give the client a session id used to continue POSTing the data.
    */
-  explicit Hash(HashType algorithm);
+  std::string startUri_;
+    
+    /// Root key to retrieve Kafka topic configurations.
+const std::string kKafkaTopicParserRootKey('kafka_topics');
+    
+    #pragma once
+    
+    TEST_F(DecoratorsConfigParserPluginTests, test_decorators_run_load_top_level) {
+  // Re-enable the decorators, then update the config.
+  // The 'load' decorator set should run every time the config is updated.
+  FLAGS_disable_decorators = false;
+  // enable top level decorations for the test
+  FLAGS_decorations_top_level = true;
+  Config::get().update(config_data_);
     }
     
-      auto constraint = Constraint(EQUALS);
-  constraint.expr = 'some';
-  cl.add(constraint);
     
-      void stop() override;
-    
-      // Test the matching capability.
-  {
-    auto sc = event_pub_->createSubscriptionContext();
-    sc->path = '/etc';
-    event_pub_->monitorSubscription(sc, false);
-    EXPECT_EQ(sc->path, '/etc/');
-    EXPECT_TRUE(event_pub_->isPathMonitored('/etc/'));
-    EXPECT_TRUE(event_pub_->isPathMonitored('/etc/passwd'));
-  }
-    
-      QueryData generate(QueryContext& request) {
-    QueryData results;
-    }
-    
-      static void clearData();
-    
-    public:
-  DHTReplaceNodeTask(const std::shared_ptr<DHTBucket>& bucket,
-                     const std::shared_ptr<DHTNode>& newNode);
-    
-    bool DHTRoutingTable::addNode(const std::shared_ptr<DHTNode>& node, bool good)
-{
-  A2_LOG_DEBUG(fmt('Trying to add node:%s', node->toString().c_str()));
-  if (*localNode_ == *node) {
-    A2_LOG_DEBUG('Adding node with the same ID with localnode is not allowed.');
-    return false;
-  }
-  auto treeNode = dht::findTreeNodeFor(root_.get(), node->getID());
-  while (1) {
-    const std::shared_ptr<DHTBucket>& bucket = treeNode->getBucket();
-    if (bucket->addNode(node)) {
-      A2_LOG_DEBUG('Added DHTNode.');
-      return true;
-    }
-    else if (bucket->splitAllowed()) {
-      A2_LOG_DEBUG(fmt('Splitting bucket. Range:%s-%s',
-                       util::toHex(bucket->getMinID(), DHT_ID_LENGTH).c_str(),
-                       util::toHex(bucket->getMaxID(), DHT_ID_LENGTH).c_str()));
-      treeNode->split();
-      ++numBucket_;
-      if (treeNode->getLeft()->isInRange(node->getID())) {
-        treeNode = treeNode->getLeft();
-      }
-      else {
-        treeNode = treeNode->getRight();
-      }
-    }
-    else {
-      if (good) {
-        bucket->cacheNode(node);
-        A2_LOG_DEBUG(fmt('Cached node=%s', node->toString().c_str()));
-      }
-      return false;
-    }
-  }
-  return false;
+    {
+    {  c.reset();
 }
-    
-    #endif // D_DHT_ROUTING_TABLE_DESERIALIZER_H
+}
 
     
-    namespace aria2 {
+      fpack.platform_ = 'bad_value';
+  EXPECT_FALSE(fpack.checkPlatform());
+    
+      free((char*)reply.message);
+    
+        // Use the basic 'force' flag to check implicit SQL usage.
+    auto flags =
+        SQL('select default_value from osquery_flags where name = 'force'');
+    if (flags.rows().size() > 0) {
+      r['flag_test'] = flags.rows().back().at('default_value');
     }
     
-    #include <cstring>
+      /**
+   * @brief Initialize the CAN client by specified CAN card parameters.
+   * @param parameter CAN card parameters to initialize the CAN client.
+   * @return If the initialization is successful.
+   */
+  virtual bool Init(const CANCardParameter &parameter) = 0;
+    
+    TEST(EsdCanClientTest, simple_test) {
+  CANCardParameter param;
+  param.set_brand(CANCardParameter::ESD_CAN);
+  param.set_channel_id(CANCardParameter::CHANNEL_ID_ZERO);
+    }
+    
+    /**
+ * @file
+ * @brief Defines the FakeCanClient class which inherites CanClient.
+ */
+    
+    // buf size must be 8 bytes, every time, we receive only one frame
+const int RX_TIMEOUT = -7;
+apollo::common::ErrorCode HermesCanClient::Receive(
+    std::vector<CanFrame> *const frames, int32_t *const frame_num) {
+  if (!_is_init) {
+    AERROR << 'Hermes can client is not init! Please init first!';
+    return ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED;
+  }
+  if (*frame_num > MAX_CAN_RECV_FRAME_LEN || *frame_num < 0) {
+    AERROR << 'recv can frame num not in range[0, ' << MAX_CAN_RECV_FRAME_LEN
+           << '], frame_num:' << *frame_num;
+    return ErrorCode::CAN_CLIENT_ERROR_FRAME_NUM;
+  }
+    }
+    
+      virtual void Stop();
+    
+      /**
+   * @brief Stop the ESD CAN client.
+   */
+  void Stop() override;
+    
+    #include 'modules/canbus/proto/chassis_detail.pb.h'
+#include 'modules/common/proto/error_code.pb.h'
+#include 'modules/drivers/canbus/can_client/fake/fake_can_client.h'
+#include 'modules/drivers/canbus/can_comm/protocol_data.h'
+    
+      /*
+   * @brief parse received data
+   * @param bytes a pointer to the input bytes
+   * @param length the length of the input bytes
+   * @param sensor_data the parsed sensor_data
+   */
+  virtual void Parse(const uint8_t *bytes, int32_t length,
+                     SensorType *sensor_data) const;
+    
+    std::string Byte::byte_to_hex(const uint8_t value) {
+  uint8_t high = value >> 4;
+  uint8_t low = value & 0x0F;
+  std::string result = '';
+  result += HEX[high];
+  result += HEX[low];
+  return result;
+}
+    
+      /**
+   * @brief Reset some consecutive bits starting from a specified position with
+   *        a certain length of another one-byte unsigned integer.
+   * @param value The one-byte unsigned integer whose certain bits are used
+   *        to set this Byte.
+   * @param start_pos The starting position (from the lowest) of the bits.
+   * @param length The length of the consecutive bits.
+   */
+  void set_value(const uint8_t value, const int32_t start_pos,
+                 const int32_t length);
+    
+    template <typename SensorType>
+class SensorCanbus : public apollo::common::ApolloApp {
+ public:
+  // TODO(lizh): check whether we need a new msg item, say
+  // MonitorMessageItem::SENSORCANBUS
+  SensorCanbus()
+      : monitor_logger_(apollo::common::monitor::MonitorMessageItem::CANBUS) {}
+    }
+    
+    // data file
+DECLARE_string(sensor_conf_file);
