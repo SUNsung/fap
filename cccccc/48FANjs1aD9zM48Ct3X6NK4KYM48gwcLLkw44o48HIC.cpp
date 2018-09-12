444310be6cfa653e517484b0e36c3a7a86d1d708
SUNsung <weatherius@gmail.com>
2018-09-12 19:46:57 +0300

    Rewrite [48FANjs1aD9zM48Ct3X6NK4KYM48gwcLLkw44o48HIC.cpp] File in: 2018-09-12 19:46:56

--- cccccc/48FANjs1aD9zM48Ct3X6NK4KYM48gwcLLkw44o48HIC.cpp
+++ cccccc/48FANjs1aD9zM48Ct3X6NK4KYM48gwcLLkw44o48HIC.cpp
@@ -1,64 +1,86 @@
 
         
-        @interface MutableDataVerifier : NSMutableData {
-    ObjectBehaviorVerifier *_verifier;
-    NSMutableData *_data;
-}
-@property (readonly) ObjectBehaviorVerifier *verifier;
-@end
-    
-    #include 'swift/Markup/Markup.h'
-#include 'llvm/ADT/Optional.h'
-    
-    
-    {  static bool classof(const MarkupASTNode *N) {
-    return N->getKind() >= ASTNodeKind::First_Private &&
-      N->getKind() <= ASTNodeKind::Last_Private;
-  }
-};
-    
-    #ifndef SWIFT_SYNTAX_FORMAT_H
-#define SWIFT_SYNTAX_FORMAT_H
+            typedef int8_t   s8;
+    typedef uint8_t  u8;
+    typedef int16_t  s16;
+    typedef uint16_t u16;
+    typedef int32_t  s32;
+    typedef uint32_t u32;
+    typedef float    f32;
+    typedef int64_t  s64;
+    typedef uint64_t u64;
+    typedef double   f64;
     
     
-    {} // end namespace swift
-    
-            size_t numSequences = oneHotSequences.size();
-        NDShape valueDataShape = sampleShape.AppendShape({ maxSequenceLength, numSequences });
-        size_t numCSCCols = valueDataShape.SubShape(1).TotalSize() + 1;
-        std::vector<SparseIndexType> colStarts(numCSCCols);
-        std::vector<ElementType> nonZeroValues;
-        std::vector<SparseIndexType> rowIndices;
-        for (size_t i = 0; i < numSequences; ++i)
+    {            vst1q_s16(dst + j, v_dst0);
+            vst1q_s16(dst + j + 8, v_dst1);
+        }
+        for (; j < roiw8; j += 8)
         {
-            size_t currentSequenceNumCols = oneHotSequences[i].size();
-            size_t j = 0;
-            for (; j < currentSequenceNumCols; ++j)
-            {
-                colStarts[(i * maxSequenceNumCols) + j] = (SparseIndexType)nonZeroValues.size();
-                size_t oneHotIdx = oneHotSequences[i][j];
-                if ((oneHotIdx & OneHotSkip) == OneHotSkip) // note that OneHotSkip used to be (size_t)-1, and later changed to (uint32_t)-1. Both are supported
-                {
-                    nonZeroValues.push_back(0);
-                    rowIndices.push_back(0);
-                }
-                else
-                {
-                    nonZeroValues.push_back(1);
-                    if (oneHotIdx >= dimension)
-                        InvalidArgument('Value::Create: one-hot index value (%zu) exceeds vocabulary size (%zu).', oneHotSequences[i][j], dimension);
-                    rowIndices.push_back((SparseIndexType)(oneHotSequences[i][j]));
-                }
-            }
-    }
+            int16x8_t v_src = vreinterpretq_s16_u16(vmovl_u8(vld1_u8(src + j)));
+            int16x8_t v_dst = vld1q_s16(dst + j);
+            int16x4_t v_srclo = vget_low_s16(v_src), v_srchi = vget_high_s16(v_src);
+            v_dst = vcombine_s16(vqmovn_s32(vaddw_s16(vshrq_n_s32(vmull_s16(v_srclo, v_srclo), shift), vget_low_s16(v_dst))),
+                                 vqmovn_s32(vaddw_s16(vshrq_n_s32(vmull_s16(v_srchi, v_srchi), shift), vget_high_s16(v_dst))));
+            vst1q_s16(dst + j, v_dst);
+        }
     
-        ComputationNetworkPtr net = make_shared<ComputationNetwork>(deviceID);
-    net->Load<ElemType>(modelPath);
     
-    #pragma once
+    {            vs1 = vqadd_s32(vs1, vnz1);
+            vs2 = vqadd_s32(vs2, vnz2);
+            vs3 = vqadd_s32(vs3, vnz3);
+            vs4 = vqadd_s32(vs4, vnz4);
+        }
     
-    public:
-    static bool GetTracingFlag()
+    void div(const Size2D &size,
+         const u8 * src0Base, ptrdiff_t src0Stride,
+         const u8 * src1Base, ptrdiff_t src1Stride,
+         u8 * dstBase, ptrdiff_t dstStride,
+         f32 scale,
+         CONVERT_POLICY cpolicy)
+{
+    div<u8>(size, src0Base, src0Stride, src1Base, src1Stride, dstBase, dstStride, scale, cpolicy);
+}
+    
+        for (size_t i = 0; i < size.height; ++i)
     {
-        return GetStaticInstance().m_tracingFlag;
+        s16* dst = internal::getRowPtr(dstBase, dstStride, i);
+        //vertical convolution
+        ptrdiff_t idx_rm2 = internal::borderInterpolate(i - 2, size.height, borderType, borderMargin.top, borderMargin.bottom);
+        ptrdiff_t idx_rm1 = internal::borderInterpolate(i - 1, size.height, borderType, borderMargin.top, borderMargin.bottom);
+        ptrdiff_t idx_rp1 = internal::borderInterpolate(i + 1, size.height, borderType, borderMargin.top, borderMargin.bottom);
+        ptrdiff_t idx_rp2 = internal::borderInterpolate(i + 2, size.height, borderType, borderMargin.top, borderMargin.bottom);
     }
+    
+                uint32x2_t el2l = vadd_u32(vget_low_u32(el8shr01l), vget_high_u32(el8shr01l));
+            uint32x2_t el2hl = vadd_u32(vget_low_u32(el4h), vget_high_u32(el8shr01l));
+            uint32x2_t el2hh = vadd_u32(vget_low_u32(el4h), vget_high_u32(el4h));
+    
+    #include 'src/cpp/client/create_channel_internal.h'
+    
+    
+    {}  // namespace grpc
+    
+    
+    {}  // namespace grpc
+    
+      // Must be called before any CompareBlock() calls can be called.
+  virtual void StartBlockComparisons() = 0;
+  // No more CompareBlock() calls can be called after this.
+  virtual void FinishBlockComparisons() = 0;
+    
+    // Creates a JPEG from the rgb pixel data. Returns true on success.
+bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
+                     JPEGData* jpg);
+    
+            // We don't care for comparison result here; the previous value will be stored into value anyway.
+        // Also we don't care for rbx and rcx values, they just have to be equal to rax and rdx before cmpxchg16b.
+        __asm__ __volatile__
+        (
+            'movq %%rbx, %%rax\n\t'
+            'movq %%rcx, %%rdx\n\t'
+            'lock; cmpxchg16b %[storage]\n\t'
+            : '=&A' (value)
+            : [storage] 'm' (storage)
+            : BOOST_ATOMIC_DETAIL_ASM_CLOBBER_CC_COMMA 'memory'
+        );
