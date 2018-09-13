c88a792dea9f08ad0b963787258f3814e1e7ff82
SUNsung <weatherius@gmail.com>
2018-09-13 13:08:41 +0300

    Rewrite [33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py] File in: 2018-09-13 13:08:41

--- pyton/33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py
+++ pyton/33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py
@@ -1,183 +1,236 @@
 
         
-        try:
-    # https://urllib3.readthedocs.io/en/latest/security.html
-    # noinspection PyPackageRequirements
-    import urllib3
-    urllib3.disable_warnings()
-except (ImportError, AttributeError):
-    # In some rare cases, the user may have an old version of the requests
-    # or urllib3, and there is no method called 'disable_warnings.' In these
-    # cases, we don't need to call the method.
-    # They may get some noisy output but execution shouldn't die. Move on.
-    pass
+        
+class Employee(metaclass=ABCMeta):
     
-            with open(self.path, 'w') as f:
-            json.dump(self, f, indent=4, sort_keys=True, ensure_ascii=True)
-            f.write('\n')
     
-        def __str__(self):
-        defaults = dict(type(self).__dict__)
-        actual = dict(defaults)
-        actual.update(self.__dict__)
-        actual['config'] = self.config
-        return repr_dict_nice(dict(
-            (key, value)
-            for key, value in actual.items()
-            if not key.startswith('_'))
-        )
+class Budget(object):
     
-            yield self.process_body(body)
+    
+if __name__ == '__main__':
+    HitCounts.run()
 
     
-            :param mime: E.g., 'application/atom+xml'.
-        :param content: The body content as text
+            (foo, p1), 2
+        (bar, p1), 2
+        (bar, p1), 1
+        (foo, p2), 3
+        (bar, p3), 10
+        (foo, p4), 1
+        '''
+        timestamp, product_id, category, quantity = line.split('\t')
+        if self.within_past_week(timestamp):
+            yield (category, product_id), quantity
     
-            See https://github.com/jakubroztocil/httpie/issues/212
+        def remove(self, key):
+        hash_index = self._hash_function(key)
+        for index, item in enumerate(self.table[hash_index]):
+            if item.key == key:
+                del self.table[hash_index][index]
+                return
+        raise KeyError('Key not found')
+
     
-    
-install_requires = [
-    'requests>=2.18.4',
-    'Pygments>=2.1.3'
-]
-    
-    
-FILE_PATH_ARG = patharg(FILE_PATH)
-BIN_FILE_PATH_ARG = patharg(BIN_FILE_PATH)
-JSON_FILE_PATH_ARG = patharg(JSON_FILE_PATH)
-    
-        @property
-    def bindata(self):
-        if not hasattr(self, '_bindata'):
-            self._bindata = urlopen(self.url).read()
-        return self._bindata
-    
-        def test_GET_with_data_auto_JSON_headers(self, httpbin):
-        # JSON headers should automatically be set also for GET with data.
-        r = http('POST', httpbin.url + '/post', 'a=b')
-        assert HTTP_OK in r
-        assert r.json['headers']['Accept'] == JSON_ACCEPT
-        assert r.json['headers']['Content-Type'] == 'application/json'
-    
-        if K.image_data_format() == 'channels_last':
-        x_train = x_train.transpose(0, 2, 3, 1)
-        x_test = x_test.transpose(0, 2, 3, 1)
-    
-        # Iterated over every node in the reference model, in depth order.
-    depth_keys = list(model._nodes_by_depth.keys())
-    depth_keys.sort(reverse=True)
-    for depth in depth_keys:
-        nodes = model._nodes_by_depth[depth]
-        for node in nodes:
-            # Recover the corresponding layer.
-            layer = node.outbound_layer
-    
-        # Test with Sequential API
-    model = Sequential([
-        layers.Dense(16, input_shape=(x_train.shape[-1],), activation='relu'),
-        layers.Dense(8),
-        layers.Activation('relu'),
-        layers.Dense(num_classes, activation='softmax')
-    ])
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='rmsprop',
-                  metrics=['accuracy'])
-    model.summary()
-    history = model.fit(x_train, y_train, epochs=15, batch_size=16,
-                        validation_data=(x_test, y_test),
-                        verbose=0)
-    assert(history.history['val_acc'][-1] > 0.8)
-    config = model.get_config()
-    model = Sequential.from_config(config)
-    
-        alpha = 0.01  # regularization parameter
+        def crawl(self):
+        while True:
+            page = self.data_store.extract_max_priority_page()
+            if page is None:
+                break
+            if self.data_store.crawled_similar(page.signature):
+                self.data_store.reduce_priority_link_to_crawl(page.url)
+            else:
+                self.crawl_page(page)
+            page = self.data_store.extract_max_priority_page()
+
     
     
-def plot_batch_errors(all_errors, n_features, all_batch_sizes, data):
-    plt.figure()
-    plot_results(all_batch_sizes, all_errors['pca'], label='PCA')
-    plot_results(all_batch_sizes, all_errors['ipca'], label='IncrementalPCA')
-    plt.legend(loc='lower left')
-    plt.suptitle('Algorithm error vs. batch_size for n_components %i\n \
-                 LFW, size %i x %i' % (
-                 n_features, data.shape[0], data.shape[1]))
-    plt.xlabel('Batch size')
-    plt.ylabel('Mean absolute error')
-    
-            # plot the actual surface
-        ax.plot_surface(X, Y, Z.T, cstride=1, rstride=1, color=c, alpha=0.8)
-    
-                gc.collect()
-            print('benchmarking orthogonal_mp (with Gram):', end='')
-            sys.stdout.flush()
-            tstart = time()
-            orthogonal_mp(X, y, precompute=True,
-                          n_nonzero_coefs=n_informative)
-            delta = time() - tstart
-            print('%0.3fs' % delta)
-            omp_gram[i_f, i_s] = delta
-    
-                gc.collect()
-            print('benchmarking scipy svd: ')
-            tstart = time()
-            svd(X, full_matrices=False)
-            results['scipy svd'].append(time() - tstart)
-    
-        op.add_option('--transformers',
-                  dest='selected_transformers',
-                  default='GaussianRandomProjection,SparseRandomProjection',
-                  type=str,
-                  help='Comma-separated list of transformer to benchmark. '
-                       'Default: %default. Available: '
-                       'GaussianRandomProjection,SparseRandomProjection')
+def create_app(test_config=None):
+    '''Create and configure an instance of the Flask application.'''
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        # a default secret that should be overridden by instance config
+        SECRET_KEY='dev',
+        # store the database in the instance folder
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
     
     
-def main():
-    command_stack = []
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('', '', b'Username is required.'),
+    ('a', '', b'Password is required.'),
+    ('test', 'test', b'already registered'),
+))
+def test_register_validate_input(client, username, password, message):
+    response = client.post(
+        '/auth/register',
+        data={'username': username, 'password': password}
+    )
+    assert message in response.data
     
-    print('Counting to five...')
-for number in count_to_five():
-    print(number, end=' ')
-    
-    
-class CompositeSpecification(Specification):
-    
-    
-def lazy_property2(fn):
-    attr = '_lazy__' + fn.__name__
-    
-    
-def main():
-    try:
-        import queue
-    except ImportError:  # python 2.x compatibility
-        import Queue as queue
-    
-        def now(self):
-        current_time = datetime.datetime.now()
-        current_time_formatted = '{}:{}'.format(current_time.hour,
-                                                current_time.minute)
-        return current_time_formatted
-    
-        @property
-    def is_eager_to_contribute(self):
-        return True
+        def match_request(self):
+        '''Can be overridden by a subclass to hook into the matching
+        of the request.
+        '''
+        try:
+            url_rule, self.request.view_args = \
+                self.url_adapter.match(return_rule=True)
+            self.request.url_rule = url_rule
+        except HTTPException as e:
+            self.request.routing_exception = e
     
     
-# ConcreteImplementor 2/2
-class DrawingAPI2(object):
+def _lookup_app_object(name):
+    top = _app_ctx_stack.top
+    if top is None:
+        raise RuntimeError(_app_ctx_err_msg)
+    return getattr(top, name)
     
-    print('I found {} face(s) in this photograph.'.format(len(face_locations)))
+        .. versionchanged:: 0.10
+       This function's return value is now always safe for HTML usage, even
+       if outside of script tags or if used in XHTML.  This rule does not
+       hold true when using this function in HTML attributes that are double
+       quoted.  Always single quote attributes if you use the ``|tojson``
+       filter.  Alternatively use ``|tojson|forceescape``.
+    '''
+    rv = dumps(obj, **kwargs) \
+        .replace(u'<', u'\\u003c') \
+        .replace(u'>', u'\\u003e') \
+        .replace(u'&', u'\\u0026') \
+        .replace(u''', u'\\u0027')
+    if not _slash_escape:
+        rv = rv.replace('\\/', '/')
+    return rv
     
-        # Quit when the input video file ends
-    if not ret:
-        break
+            def __init__(self, name, doc=None):
+            self.name = name
+            self.__doc__ = doc
+        def _fail(self, *args, **kwargs):
+            raise RuntimeError('signalling support is unavailable '
+                               'because the blinker library is '
+                               'not installed.')
+        send = lambda *a, **kw: None
+        connect = disconnect = has_receivers_for = receivers_for = \
+            temporarily_connected_to = connected_to = _fail
+        del _fail
     
-        # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
     
-        for file in image_files_in_folder(known_people_folder):
-        basename = os.path.splitext(os.path.basename(file))[0]
-        img = face_recognition.load_image_file(file)
-        encodings = face_recognition.face_encodings(img)
+python_3 = ('thefuck/python3-zsh',
+            u'''FROM python:3
+                RUN apt-get update
+                RUN apt-get install -yy zsh''',
+            u'sh')
+    
+        assert not match(command)
+    
+        assert get_new_command(Command('brew install aa',
+                                   brew_no_available_formula))\
+        != 'brew install aha'
+
+    
+        # Arguments
+        label_mode: one of 'fine', 'coarse'.
+    
+        # Arguments
+        model: Instance of `Model`.
+        input_tensors: optional list of input tensors
+            to build the model upon. If not provided,
+            placeholders will be created.
+    
+    
+def l1(l=0.01):
+    return L1L2(l1=l)
+    
+            # Arguments
+            layer: target layer.
+        '''
+        try:
+            output_shape = layer.output_shape
+        except AttributeError:
+            output_shape = 'multiple'
+        connections = []
+        for node in layer._inbound_nodes:
+            if relevant_nodes and node not in relevant_nodes:
+                # node is not part of the current network
+                continue
+            for i in range(len(node.inbound_layers)):
+                inbound_layer = node.inbound_layers[i].name
+                inbound_node_index = node.node_indices[i]
+                inbound_tensor_index = node.tensor_indices[i]
+                connections.append(inbound_layer +
+                                   '[' + str(inbound_node_index) + '][' +
+                                   str(inbound_tensor_index) + ']')
+    
+    
+class CheckNetwork(object):
+    def __init__(self, type='IPv4'):
+        self.type = type
+        self.urls = []
+        self._checking_lock = threading.Lock()
+        self._checking_num = 0
+        self.network_stat = 'unknown'
+        self.last_check_time = 0
+        self.continue_fail_count = 0
+    
+    
+    
+        def get(self):
+        n = random.randint(2, 3)
+        ws = []
+        for i in range(0, n):
+            w = self.slice.get()
+            ws.append(w)
+    
+    
+def CurrentIdentifierFinished():
+  line, current_column = vimsupport.CurrentLineContentsAndCodepointColumn()
+  previous_char_index = current_column - 1
+  if previous_char_index < 0:
+    return True
+  filetype = vimsupport.CurrentFiletypes()[ 0 ]
+  regex = identifier_utils.IdentifierRegexForFiletype( filetype )
+    
+    from ycm.client.base_request import BaseRequest, BuildRequestData
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    
+def AdjustCandidateInsertionText_MoreThanWordMatchingAfterCursor_test():
+  with MockTextAfterCursor( 'bar.h' ):
+    eq_( [ { 'abbr': 'foobar.h', 'word': 'foo' } ],
+         base.AdjustCandidateInsertionText( [ 'foobar.h' ] ) )
+    
+    
+  def Extra_Info_No_Doc_String_test( self ):
+    self._Check( 0, {
+      'insertion_text':  'INSERTION TEXT',
+      'menu_text':       'MENU TEXT',
+      'extra_menu_info': 'EXTRA MENU INFO',
+      'kind':            'K',
+      'extra_data': {
+      },
+    }, {
+      'word'     : 'INSERTION TEXT',
+      'abbr'     : 'MENU TEXT',
+      'menu'     : 'EXTRA MENU INFO',
+      'kind'     : 'k',
+      'dup'      : 1,
+      'empty'    : 1,
+      'user_data': '0',
+    } )
+    
+      # Other non-False responses mean the same thing
+  assert_that( _HandlePollResponse( '', None ), equal_to( True ) )
+  assert_that( _HandlePollResponse( 1, None ), equal_to( True ) )
+  assert_that( _HandlePollResponse( {}, None ), equal_to( True ) )
+    
+    class _WorkItem( object ):
+  def __init__( self, future, fn, args, kwargs ):
+    self.future = future
+    self.fn = fn
+    self.args = args
+    self.kwargs = kwargs
