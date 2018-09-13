751361059fb9e5497333d2ef151eb3eb7aefeae8
SUNsung <weatherius@gmail.com>
2018-09-13 04:53:04 +0300

    Rewrite [50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py] File in: 2018-09-13 04:53:04

--- pyton/50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py
+++ pyton/50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py
@@ -1,129 +1,161 @@
 
         
-        
-class Unaccent(Transform):
-    bilateral = True
-    lookup_name = 'unaccent'
-    function = 'UNACCENT'
+        from mrjob.job import MRJob
     
-        def exists(self, session_key):
-        '''
-        Return True if the given session_key already exists.
-        '''
-        raise NotImplementedError('subclasses of SessionBase must provide an exists() method')
+        def _hash_function(self, key):
+        return key % self.size
     
-        def flush(self):
-        '''
-        Remove the current session data from the database and regenerate the
-        key.
-        '''
-        self.clear()
-        self.delete(self.session_key)
-        self._session_key = None
+        def __init__(self, MAX_SIZE):
+        self.MAX_SIZE = MAX_SIZE
+        self.size = 0
+        self.lookup = {}
+        self.linked_list = LinkedList()
+    
+    from httpie.compat import str
+from httpie.context import Environment
+from httpie.models import HTTPRequest, HTTPResponse
+from httpie.input import (OUT_REQ_BODY, OUT_REQ_HEAD,
+                          OUT_RESP_HEAD, OUT_RESP_BODY)
+from httpie.output.processing import Formatting, Conversion
+    
+    
+class PyTest(TestCommand):
+    # `$ python setup.py test' simply installs minimal requirements
+    # and runs the tests with no fancy stuff like parallel execution.
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = [
+            '--doctest-modules', '--verbose',
+            './httpie', './tests'
+        ]
+        self.test_suite = True
+    
+    
+with open(BIN_FILE_PATH, 'rb') as f:
+    BIN_FILE_CONTENT = f.read()
+    
+    
+@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
+@pytest.mark.parametrize('filename', filenames)
+def test_rst_file_syntax(filename):
+    p = subprocess.Popen(
+        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
+    err = p.communicate()[1]
+    assert p.returncode == 0, err.decode('utf8')
 
     
     
-def _get_sitemap_full_url(sitemap_url):
-    if not django_apps.is_installed('django.contrib.sites'):
-        raise ImproperlyConfigured('ping_google requires django.contrib.sites, which isn't installed.')
+class Response(object):
+    # noinspection PyDefaultArgument
+    def __init__(self, url, headers={}, status_code=200):
+        self.url = url
+        self.headers = CaseInsensitiveDict(headers)
+        self.status_code = status_code
     
     
-class Antirectifier(layers.Layer):
-    '''This is the combination of a sample-wise
-    L2 normalization with the concatenation of the
-    positive part of the input with the negative part
-    of the input. The result is a tensor of samples that are
-    twice as large as the input samples.
+@mock.patch('httpie.core.get_response')
+def test_error_traceback(get_response):
+    exc = ConnectionError('Connection aborted')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    with raises(ConnectionError):
+        main(['--ignore-stdin', '--traceback', 'www.google.com'])
+    
+        r = http('GET', httpbin_both + '/headers', 'Accept;')
+    assert r.json['headers']['Accept'] == ''   # Accept has no value
     
     
-def top_k_categorical_accuracy(y_true, y_pred, k=5):
-    return K.mean(K.in_top_k(y_pred, K.argmax(y_true, axis=-1), k), axis=-1)
+@pytest.mark.parametrize('follow_flag', ['--follow', '-F'])
+def test_follow_without_all_redirects_hidden(httpbin, follow_flag):
+    r = http(follow_flag, httpbin.url + '/redirect/2')
+    assert r.count('HTTP/1.1') == 1
+    assert HTTP_OK in r
     
     
-def get(identifier):
-    if identifier is None:
-        return None
-    if isinstance(identifier, dict):
-        return deserialize(identifier)
-    elif isinstance(identifier, six.string_types):
-        config = {'class_name': str(identifier), 'config': {}}
-        return deserialize(config)
-    elif callable(identifier):
-        return identifier
-    else:
-        raise ValueError('Could not interpret regularizer identifier: ' +
-                         str(identifier))
+@pytest.mark.parametrize('command, packages', [
+    (Command('vim', 'vim: command not found'),
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('sudo vim', 'vim: command not found'),
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('vim', 'The program 'vim' is currently not installed. You can install it by typing: sudo apt install vim'),
+     [('vim', 'main'), ('vim-tiny', 'main')])])
+def test_match(mocker, command, packages):
+    mocker.patch('thefuck.rules.apt_get.which', return_value=None)
+    mocker.patch('thefuck.rules.apt_get._get_packages',
+                 create=True, return_value=packages)
+    
+    
+@pytest.mark.parametrize('script, output, help_text, result', [
+    ('apt-get isntall vim', invalid_operation('isntall'),
+     apt_get_help, 'apt-get install vim'),
+    ('apt saerch vim', invalid_operation('saerch'),
+     apt_help, 'apt search vim'),
+])
+def test_get_new_command(set_help, output, script, help_text, result):
+    set_help(help_text)
+    assert get_new_command(Command(script, output))[0] == result
 
     
-            return False
     
-    def message_html(title, banner, detail=''):
-    MESSAGE_TEMPLATE = '''
-    <html><head>
-    <meta http-equiv='content-type' content='text/html;charset=utf-8'>
-    <title>$title</title>
-    <style><!--
-    body {font-family: arial,sans-serif}
-    div.nav {margin-top: 1ex}
-    div.nav A {font-size: 10pt; font-family: arial,sans-serif}
-    span.nav {font-size: 10pt; font-family: arial,sans-serif; font-weight: bold}
-    div.nav A,span.big {font-size: 12pt; color: #0000cc}
-    div.nav A {font-size: 10pt; color: black}
-    A.l:link {color: #6f6f6f}
-    A.u:link {color: green}
-    //--></style>
-    </head>
-    <body text=#000000 bgcolor=#ffffff>
-    <table border=0 cellpadding=2 cellspacing=0 width=100%>
-    <tr><td bgcolor=#3366cc><font face=arial,sans-serif color=#ffffff><b>Message</b></td></tr>
-    <tr><td> </td></tr></table>
-    <blockquote>
-    <H1>$banner</H1>
-    $detail
-    <p>
-    </blockquote>
-    <table width=100% cellpadding=0 cellspacing=0><tr><td bgcolor=#3366cc><img alt='' width=1 height=4></td></tr></table>
-    </body></html>
-    '''
-    return string.Template(MESSAGE_TEMPLATE).substitute(title=title, banner=banner, detail=detail)
+class Overlap(PostgresSimpleLookup):
+    lookup_name = 'overlap'
+    operator = '&&'
+    
+            # No redirect was found. Return the response.
+        return response
+
+    
+        def clear(self):
+        # To avoid unnecessary persistent storage accesses, we set up the
+        # internals directly (loading data wastes time, since we are going to
+        # set it to an empty dict anyway).
+        self._session_cache = {}
+        self.accessed = True
+        self.modified = True
+    
+            # Determine domain
+        if site is None:
+            if django_apps.is_installed('django.contrib.sites'):
+                Site = django_apps.get_model('sites.Site')
+                try:
+                    site = Site.objects.get_current()
+                except Site.DoesNotExist:
+                    pass
+            if site is None:
+                raise ImproperlyConfigured(
+                    'To use sitemaps, either enable the sites framework or pass '
+                    'a Site/RequestSite object in your view.'
+                )
+        domain = site.domain
+    
+    try:
+    import simplejson as json
+except ImportError:
+    import json
     
     
-    def getText(self):
+class InvalidProxyURL(InvalidURL):
+    '''The proxy URL provided is invalid.'''
+    
+    
+def default_hooks():
+    return dict((event, []) for event in HOOKS)
+    
+        def __repr__(self):
+        return str(dict(self.items()))
+    
+            :param conn: The urllib3 connection object associated with the cert.
+        :param url: The requested URL.
+        :param verify: Either a boolean, in which case it controls whether we verify
+            the server's TLS certificate, or a string, in which case it must be a path
+            to a CA bundle to use
+        :param cert: The SSL certificate to verify.
         '''
-        Return the text matched so far for the current token or any
-        text override.
-        '''
-        if self._state.text is not None:
-            return self._state.text
-        
-        return self.input.substring(
-            self._state.tokenStartCharIndex,
-            self.getCharIndex()-1
-            )
+        if url.lower().startswith('https') and verify:
     
-        def test_iter_meta(self):
-        result = False
-        for k in self.dir.meta:
-            if k == 'terms_of_service':
-                result = self.dir.meta[k] == 'https://example.com/acme/terms'
-        self.assertTrue(result)
     
-        Takes in Augeas path and returns the file name
-    
-                # If equal and set is not empty... assume same server
-            if self.name is not None or self.aliases:
-                return True
-        # If we're looking for a generic vhost,
-        # don't return one with a ServerName
-        elif self.name:
-            return False
-    
-        @certbot_util.patch_get_utility()
-    def test_multiple_names(self, mock_util):
-        mock_util().menu.return_value = (display_util.OK, 5)
-    
-        _multiprocess_can_split_ = True
-    
-            self.assertTrue(self.addr_default.conflicts(self.addr))
-        self.assertTrue(self.addr_default.conflicts(self.addr1))
-        self.assertTrue(self.addr_default.conflicts(self.addr_defined))
+def patch(url, data=None, **kwargs):
+    r'''Sends a PATCH request.
