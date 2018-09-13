72337f507703b99c279a4c0b542d910e619f814a
SUNsung <weatherius@gmail.com>
2018-09-14 00:50:36 +0300

    Rewrite [29rRT311np8w.29VtKpXWPMBcQ29vw9X5TpJnOU29s55.py] File in: 2018-09-14 00:50:36

--- pyton/29rRT311np8w.29VtKpXWPMBcQ29vw9X5TpJnOU29s55.py
+++ pyton/29rRT311np8w.29VtKpXWPMBcQ29vw9X5TpJnOU29s55.py
@@ -1,72 +1,148 @@
 
         
-            print('Classifier Training')
-    print('===================')
-    accuracy, train_time, test_time = {}, {}, {}
-    for name in sorted(args['estimators']):
-        clf = ESTIMATORS[name]
+            with client:
+        auth.logout()
+        assert 'user_id' not in session
+
+    
+    
+class locked_cached_property(object):
+    '''A decorator that converts a function into a lazy property.  The
+    function wrapped is called the first time to retrieve the result
+    and then that calculated result is used the next time you access
+    the value.  Works like the one in Werkzeug but has a lock for
+    thread safety.
+    '''
+    
+    
+def htmlsafe_dumps(obj, **kwargs):
+    '''Works exactly like :func:`dumps` but is safe for use in ``<script>``
+    tags.  It accepts the same arguments and returns a JSON string.  Note that
+    this is available in templates through the ``|tojson`` filter which will
+    also mark the result as safe.  Due to how this function escapes certain
+    characters this is safe even if used outside of ``<script>`` tags.
+    
+    setup(name='thefuck',
+      version=VERSION,
+      description='Magnificent app which corrects your previous console command',
+      long_description=long_description,
+      author='Vladimir Iakovlev',
+      author_email='nvbn.rm@gmail.com',
+      url='https://github.com/nvbn/thefuck',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples',
+                                      'tests', 'tests.*', 'release']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      extras_require=extras_require,
+      entry_points={'console_scripts': [
+          'thefuck = thefuck.entrypoints.main:main',
+          'fuck = thefuck.entrypoints.not_configured:main']})
+
+    
+    
+@pytest.fixture(autouse=True)
+def functional(request):
+    if request.node.get_marker('functional') \
+            and not request.config.getoption('enable_functional'):
+        pytest.skip('functional tests are disabled')
+    
+    
+python_3 = (u'thefuck/python3-bash',
+            u'FROM python:3',
+            u'sh')
+    
+    
+@pytest.mark.parametrize('script, output, help_text, result', [
+    ('apt-get isntall vim', invalid_operation('isntall'),
+     apt_get_help, 'apt-get install vim'),
+    ('apt saerch vim', invalid_operation('saerch'),
+     apt_help, 'apt search vim'),
+])
+def test_get_new_command(set_help, output, script, help_text, result):
+    set_help(help_text)
+    assert get_new_command(Command(script, output))[0] == result
+
+    
+    
+no_such_subcommand_old = '''No such subcommand
+    
+    from django.contrib.sites.shortcuts import get_current_site
+from django.core.paginator import EmptyPage, PageNotAnInteger
+from django.http import Http404
+from django.template.response import TemplateResponse
+from django.urls import reverse
+from django.utils.http import http_date
+    
+            ax.set_xlabel('n_samples')
+        ax.set_ylabel('n_features')
+        ax.set_zlabel('Time (s)')
+        ax.set_zlim3d(0.0, max_time * 1.1)
+        ax.set_title(label)
+        # ax.legend()
+        i += 1
+    plt.show()
+
+    
+    The input data is mostly low rank but is a fat infinite tail.
+'''
+from __future__ import print_function
+    
+    PROJ_NAME = 'you-get'
+PACKAGE_NAME = 'you_get'
+    
+    _options = [
+    'help',
+    'version',
+    'gui',
+    'force',
+    'playlists',
+]
+_short_options = 'hVgfl'
+    
+    #----------------------------------------------------------------------
+def ckplayer_download(url, output_dir = '.', merge = False, info_only = False, is_xml = True, **kwargs):
+    if is_xml:  #URL is XML URL
         try:
-            clf.set_params(random_state=0)
-        except (TypeError, ValueError):
-            pass
+            title = kwargs['title']
+        except:
+            title = ''
+        try:
+            headers = kwargs['headers']  #headers provided
+            ckinfo = get_content(url, headers = headers)
+        except NameError:
+            ckinfo = get_content(url)
+        
+        ckplayer_download_by_xml(ckinfo, output_dir, merge, 
+                                info_only, title = title)
     
-        # start time
-    tstart = time()
-    clf = factory(alpha=alpha).fit(X, Y)
-    delta = (time() - tstart)
-    # stop time
+        sd_urls = list(set([
+        unicodize(str.replace(i, '\\/', '/'))
+        for i in re.findall(r'sd_src_no_ratelimit:'([^']*)'', html)
+    ]))
+    hd_urls = list(set([
+        unicodize(str.replace(i, '\\/', '/'))
+        for i in re.findall(r'hd_src_no_ratelimit:'([^']*)'', html)
+    ]))
+    urls = hd_urls if hd_urls else sd_urls
     
-            # If we're not plotting, dump the timing to stdout
-        if not args.show_plot:
-            print(n, np.mean(time_per_iteration))
+    # Find all facial features in all the faces in the image
+face_landmarks_list = face_recognition.face_landmarks(image)
     
-        for name in sorted(selected_transformers):
-        print_row(name,
-                  np.mean(time_fit[name]),
-                  np.mean(time_transform[name]))
+    for face_location in face_locations:
     
-        from sklearn.tree import DecisionTreeRegressor
+                # If a match was found in known_face_encodings, just use the first one.
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = known_face_names[first_match_index]
     
-    class default_prober_dict(dict):
+        # macOS will crash due to a bug in libdispatch if you don't use 'forkserver'
+    context = multiprocessing
+    if 'forkserver' in multiprocessing.get_all_start_methods():
+        context = multiprocessing.get_context('forkserver')
     
-    ##
-# imaginary tree navigation type; traverse 'get child' link
-DOWN = 2
-##
-#imaginary tree navigation type; finish with a child list
-UP = 3
-    
-    
-    {
-    {        tokens.insertAfter('pass1', t, 'text to put after t');}
-        tokens.insertAfter('pass2', u, 'text after u');}
-        System.out.println(tokens.toString('pass1'));
-        System.out.println(tokens.toString('pass2'));
-    
-            Using setter/getter methods is deprecated. Use o.text instead.
-        '''
-        raise NotImplementedError
-    
-    def setText(self, text):
-        '''@brief Set the text of the token.
-    
-          tok = tok.lstrip('(')
-      tok = tok.rstrip(')')
-      if not tok: continue
-      if Match(r'\d+', tok): continue
-      if Match(r'0[xX][0-9a-fA-F]+', tok): continue
-      if Match(r'k[A-Z0-9]\w*', tok): continue
-      if Match(r'(.+::)?k[A-Z0-9]\w*', tok): continue
-      if Match(r'(.+::)?[A-Z][A-Z0-9_]*', tok): continue
-      # A catch all for tricky sizeof cases, including 'sizeof expression',
-      # 'sizeof(*type)', 'sizeof(const type)', 'sizeof(struct StructName)'
-      # requires skipping the next token because we split on ' ' and '*'.
-      if tok.startswith('sizeof'):
-        skip_next = True
-        continue
-      is_const = False
-      break
-    if not is_const:
-      error(filename, linenum, 'runtime/arrays', 1,
-            'Do not use variable-length arrays.  Use an appropriately named '
-            '('k' followed by CamelCase) compile-time constant for the size.')
+        def test_raw_face_landmarks(self):
+        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
+        face_landmarks = api._raw_face_landmarks(img)
+        example_landmark = face_landmarks[0].parts()[10]
