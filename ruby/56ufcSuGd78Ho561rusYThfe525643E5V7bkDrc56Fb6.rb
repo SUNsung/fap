5422d30147f4efd29a759bed7ce868156a1faf47
SUNsung <weatherius@gmail.com>
2018-09-13 19:39:43 +0300

    Rewrite [56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb] File in: 2018-09-13 19:39:42

--- ruby/56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb
+++ ruby/56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb
@@ -1,203 +1,213 @@
 
         
-          def dump_build_env(env, f = $stdout)
-    keys = build_env_keys(env)
-    keys -= %w[CC CXX OBJC OBJCXX] if env['CC'] == env['HOMEBREW_CC']
+              it 'generates the correct git command with an array of paths and/or pathspecs' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          git_commit(path: ['./fastlane/*.md', './LICENSE'], message: 'message')
+        end').runner.execute(:test)
     
-      # Removes any empty directories in the formula's prefix subtree
-  # Keeps any empty directions projected by skip_clean
-  # Removes any unresolved symlinks
-  def prune
-    dirs = []
-    symlinks = []
-    @f.prefix.find do |path|
-      if path == @f.libexec || @f.skip_clean?(path)
-        Find.prune
-      elsif path.symlink?
-        symlinks << path
-      elsif path.directory?
-        dirs << path
-      end
-    end
-    
-      UNBREWED_EXCLUDE_FILES = %w[.DS_Store]
-  UNBREWED_EXCLUDE_PATHS = %w[
-    .github/*
-    bin/brew
-    lib/gdk-pixbuf-2.0/*
-    lib/gio/*
-    lib/node_modules/*
-    lib/python[23].[0-9]/*
-    lib/pypy/*
-    lib/pypy3/*
-    lib/ruby/gems/[12].*
-    lib/ruby/site_ruby/[12].*
-    lib/ruby/vendor_ruby/[12].*
-    share/pypy/*
-    share/pypy3/*
-    share/doc/homebrew/*
-    share/info/dir
-    share/man/man1/brew.1
-    share/man/whatis
-  ]
-    
-      # Puts a URI back together based on the URI parts
-  def uri
-    str = self.uri_parts['Resource'].dup || '/'
-    
-              # Encodes the Rex::Proto::Kerberos::Model::ApReq into an ASN.1 String
-          #
-          # @return [String]
-          def encode
-            elems = []
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_pvno], 0, :CONTEXT_SPECIFIC)
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_msg_type], 1, :CONTEXT_SPECIFIC)
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_options], 2, :CONTEXT_SPECIFIC)
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_ticket], 3, :CONTEXT_SPECIFIC)
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_authenticator], 4, :CONTEXT_SPECIFIC)
-            seq = OpenSSL::ASN1::Sequence.new(elems)
-    
-                int
-          end
-    
-              # Rex::Proto::Kerberos::Model::EncKdcResponse encoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def encode
-            raise ::NotImplementedError, 'EncKdcResponse encoding not supported'
-          end
-    
-              # Decodes the cname field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Rex::Proto::Kerberos::Model::PrincipalName]
-          def decode_cname(input)
-            Rex::Proto::Kerberos::Model::PrincipalName.decode(input.value[0])
-          end
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of a Kerberos KRB-ERROR (response error)
-        # message definition.
-        class KrbError < Element
-          # @!attribute pvno
-          #   @return [Integer] The protocol version number
-          attr_accessor :pvno
-          # @!attribute msg_type
-          #   @return [Integer] The type of a protocol message
-          attr_accessor :msg_type
-          # @!attribute ctime
-          #   @return [Time] The current time of the client's host
-          attr_accessor :ctime
-          # @!attribute cusec
-          #   @return [Integer] The microseconds part of the client timestamp
-          attr_accessor :cusec
-          # @!attribute stime
-          #   @return [Time] The current time of the server
-          attr_accessor :stime
-          # @!attribute susec
-          #   @return [Integer] The microseconds part of the server timestamp
-          attr_accessor :susec
-          # @!attribute error_code
-          #   @return [Integer] The error request returned by kerberos or the server when a request fails
-          attr_accessor :error_code
-          # @!attribute crealm
-          #   @return [String] The realm part of the client's principal identifier
-          attr_accessor :crealm
-          # @!attribute cname
-          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the client's principal identifier
-          attr_accessor :cname
-          # @!attribute realm
-          #   @return [String] The realm part of the server's principal identifier
-          attr_accessor :realm
-          # @!attribute sname
-          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the server's identity
-          attr_accessor :sname
-          # @!attribute e_data
-          #   @return [String] additional data about the error (ASN.1 encoded data)
-          attr_accessor :e_data
-    
-      def validate_target_file
-    if File.exist?(target_file)
-      if  delete_target_file?
-        File.delete(target_file)
-      else
-        signal_error('Package creation cancelled, a previously generated package exist at location: #{target_file}, move this file to safe place and run the command again')
-      end
-    end
-  end
-    
-        puts('Unpacking #{package_file}')
-    
-      private
-  def update_all?
-    plugins_arg.size == 0
-  end
-    
-    module RuboCop
-  module Cop
-    module Style
-      # This cop checks for braces around the last parameter in a method call
-      # if the last parameter is a hash.
-      # It supports `braces`, `no_braces` and `context_dependent` styles.
-      #
-      # @example EnforcedStyle: braces
-      #   # The `braces` style enforces braces around all method
-      #   # parameters that are hashes.
-      #
-      #   # bad
-      #   some_method(x, y, a: 1, b: 2)
-      #
-      #   # good
-      #   some_method(x, y, {a: 1, b: 2})
-      #
-      # @example EnforcedStyle: no_braces (default)
-      #   # The `no_braces` style checks that the last parameter doesn't
-      #   # have braces around it.
-      #
-      #   # bad
-      #   some_method(x, y, {a: 1, b: 2})
-      #
-      #   # good
-      #   some_method(x, y, a: 1, b: 2)
-      #
-      # @example EnforcedStyle: context_dependent
-      #   # The `context_dependent` style checks that the last parameter
-      #   # doesn't have braces around it, but requires braces if the
-      #   # second to last parameter is also a hash literal.
-      #
-      #   # bad
-      #   some_method(x, y, {a: 1, b: 2})
-      #   some_method(x, y, {a: 1, b: 2}, a: 1, b: 2)
-      #
-      #   # good
-      #   some_method(x, y, a: 1, b: 2)
-      #   some_method(x, y, {a: 1, b: 2}, {a: 1, b: 2})
-      class BracesAroundHashParameters < Cop
-        include ConfigurableEnforcedStyle
-        include RangeHelp
-    
-          # Checks whether this node body is a void context.
-      # Always `true` for `for`.
-      #
-      # @return [true] whether the `for` node body is a void context
-      def void_context?
-        true
-      end
-    
-          # Custom destructuring method. This is used to normalize the branches
-      # for `pair` and `kwsplat` nodes, to add duck typing to `hash` elements.
-      #
-      # @return [Array<KeywordSplatNode>] the different parts of the `kwsplat`
-      def node_parts
-        [self, self]
+            Fastlane::FastFile.new.parse('lane :test do
+          import_certificate ({
+            keychain_name: '#{keychain}',
+            certificate_path: '#{cert_name}',
+            certificate_password: '#{password}',
+            log_output: true
+          })
+        end').runner.execute(:test)
       end
     end
   end
 end
 
     
-              delta(first.loc.operator, second.loc.operator)
+            expect(FastlaneCore::CertChecker.install_wwdr_certificate).to be(true)
+      end
+    end
+  end
+end
+
+    
+              expect(config[:int_value]).to eq(10)
+        end
+    
+        it 'allows to delete a user' do
+      visit admin_users_path
+      find(:css, 'a[href='/admin/users/#{users(:bob).id}']').click
+      expect(page).to have_text('User 'bob' was deleted.')
+      expect(page).to have_no_text('bob@example.com')
+    end
+    
+        it 'opens the dry run modal even when clicking on the refresh icon' do
+      visit edit_agent_path(agent)
+      find('.agent-dry-run-button span.glyphicon').click
+      expect(page).to have_text('Event to send (Optional)')
+    end
+    
+        class Agents::DotBar < Agent
+      cannot_be_scheduled!
+    
+    describe JobsHelper do
+  let(:job) { Delayed::Job.new }
+    
+      describe '#recursively_interpolate_jsonpaths' do
+    it 'interpolates all string values in a structure' do
+      struct = {
+        :int => 5,
+        :string => 'this <escape $.works>',
+        :array => ['<works>', 'now', '<$.there.world>'],
+        :deep => {
+          :string => 'hello <there.world>',
+          :hello => :world
+        }
+      }
+      data = { :there => { :world => 'WORLD' }, :works => 'should work' }
+      expect(Utils.recursively_interpolate_jsonpaths(struct, data)).to eq({
+        :int => 5,
+        :string => 'this should+work',
+        :array => ['should work', 'now', 'WORLD'],
+        :deep => {
+          :string => 'hello WORLD',
+          :hello => :world
+        }
+      })
+    end
+  end
+    
+    describe AgentLog do
+  describe 'validations' do
+    before do
+      @log = AgentLog.new(:agent => agents(:jane_website_agent), :message => 'The agent did something', :level => 3)
+      expect(@log).to be_valid
+    end
+    
+          result
+    end
+    
+        # Deletes a machine by UUID.
+    #
+    # The machine being deleted with this UUID must either be locked
+    # by this index or must be unlocked.
+    #
+    # @param [Entry] entry The entry to delete.
+    # @return [Boolean] true if delete is successful
+    def delete(entry)
+      return true if !entry.id
+    
+            # Executes a command on the remote machine with administrative
+        # privileges. See {#execute} for documentation, as the API is the
+        # same.
+        #
+        # @see #execute
+        def sudo(command, opts=nil)
+        end
+    
+            # Initializes the system. Any subclasses MUST make sure this
+        # method is called on the parent. Therefore, if a subclass overrides
+        # `initialize`, then you must call `super`.
+        def initialize(vm)
+          @vm = vm
+        end
+    
+              @registered.each do |plugin|
+            plugin.config.each do |key, klass|
+              result[key] = klass
+            end
+          end
+    
+            # This is the method called to 'prepare' the provisioner. This is called
+        # before any actions are run by the action runner (see {Vagrant::Actions::Runner}).
+        # This can be used to setup shared folders, forward ports, etc. Whatever is
+        # necessary on a 'meta' level.
+        #
+        # No return value is expected.
+        def prepare
+        end
+    
+            # This is an internal initialize function that should never be
+        # overridden. It is used to initialize some common internal state
+        # that is used in a provider.
+        def _initialize(name, machine)
+          initialize_capabilities!(
+            name.to_sym,
+            { name.to_sym => [Class.new, nil] },
+            Vagrant.plugin('2').manager.provider_capabilities,
+            machine,
+          )
+        end
+      end
+    end
+  end
+end
+
+    
+      it 'has no effect on immediate values' do
+    [nil, true, false].each do |v|
+      v.taint
+      v.tainted?.should == false
+    end
+  end
+    
+    describe 'Kernel#warn' do
+  before :each do
+    @before_verbose = $VERBOSE
+    @before_separator = $/
+  end
+    
+    class Converter
+  extend Forwardable
+  include Network
+  include LessConversion
+  include JsConversion
+  include FontsConversion
+    
+        def pos=(i)
+      @s.pos = str_to_byte_pos i
+      i
+    end
+    
+          def order_token
+        request.headers['X-Spree-Order-Token'] || params[:order_token]
+      end
+    
+            def log_state_changes
+          if @order.previous_changes[:state]
+            @order.log_state_changes(
+              state_name: 'order',
+              old_state: @order.previous_changes[:state].first,
+              new_state: @order.previous_changes[:state].last
+            )
+          end
+        end
+    
+            def scope
+          if params[:product_id]
+            Spree::Product.friendly.find(params[:product_id])
+          elsif params[:variant_id]
+            Spree::Variant.find(params[:variant_id])
+          end
+        end
+      end
+    end
+  end
+end
+
+    
+            def fire
+          inventory_unit.send('#{@event}!') if @event
+        end
+    
+            def remove_coupon_code
+          find_order(true)
+          authorize! :update, @order, order_token
+          @handler = Spree::PromotionHandler::Coupon.new(@order).remove(params[:coupon_code])
+          status = @handler.successful? ? 200 : 404
+          render 'spree/api/v1/promotions/handler', status: status
+        end
+    
+            def index
+          @payments = @order.payments.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
+          respond_with(@payments)
+        end
+    
+            def find_product
+          super(params[:id])
         end
