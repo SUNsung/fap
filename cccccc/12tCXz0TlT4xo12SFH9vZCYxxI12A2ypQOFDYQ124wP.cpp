f263d19b3cdf74af8c0ad2346f936f36a03be5e6
SUNsung <weatherius@gmail.com>
2018-09-13 20:13:53 +0300

    Rewrite [12tCXz0TlT4xo12SFH9vZCYxxI12A2ypQOFDYQ124wP.cpp] File in: 2018-09-13 20:13:53

--- cccccc/12tCXz0TlT4xo12SFH9vZCYxxI12A2ypQOFDYQ124wP.cpp
+++ cccccc/12tCXz0TlT4xo12SFH9vZCYxxI12A2ypQOFDYQ124wP.cpp
@@ -1,248 +1,142 @@
 
         
-        
-void unregister_fd(int fd) {
-  pollfds.erase(
-    std::remove_if(pollfds.begin(), pollfds.end(),
-        [fd](const struct pollfd &pfd) { return pfd.fd == fd; }),
-    pollfds.end());
-  client_sessions.erase(fd);
+        #include <QDialog>
+    
+        /** Colorize an icon (given filename) with the text color */
+    QIcon TextColorIcon(const QString& filename) const;
+    
+    class SignVerifyMessageDialog : public QDialog
+{
+    Q_OBJECT
+    }
+    
+        // Add a preexisting 'receive' entry in the address book.
+    QString preexisting_r_address;
+    QString r_label('already here (r)');
+    
+    class ClientModel;
+    
+    
+    {    secp256k1_ecdsa_recoverable_signature_load(ctx, &r, &s, &recid, signature);
+    VERIFY_CHECK(recid >= 0 && recid < 4);  /* should have been caught in parse_compact */
+    secp256k1_scalar_set_b32(&m, msg32, NULL);
+    if (secp256k1_ecdsa_sig_recover(&ctx->ecmult_ctx, &r, &s, &q, &m, recid)) {
+        secp256k1_pubkey_save(pubkey, &q);
+        return 1;
+    } else {
+        memset(pubkey, 0, sizeof(*pubkey));
+        return 0;
+    }
 }
     
-    REGISTER_GRADIENT(Sub, GetSubGradient);
+    #include <boost/test/unit_test.hpp>
     
-    
-    {} // namespace caffe2
-
-    
-    <summary> <b>Example</b> </summary>
-    
-    
-    { private:
-  const int kNumTensorsPerInput = 5;
-};
-    
-    )DOC')
-    .Input(
-        0,
-        'X',
-        '*(type: Tensor)* Input Tensor of rank >= axis.')
-    .Output(
-        0,
-        'Y',
-        '*(type: Tensor)* A 2D tensor with the contents of the input tensor, '
-        'with input dimensions up to `axis` flattened to the outer dimension '
-        'of the output and the remaining input dimensions flattened into the '
-        'inner dimension of the output.')
-    .Arg(
-        'axis',
-        '*(type: int; default: 1)* Indicates up to which input dimensions '
-        '(exclusive) should be flattened to the outer dimension of the output.')
-    .InheritOnnxSchema('Flatten');
-    
-    #ifndef TCP_SERVER_POSIX_H
-#define TCP_SERVER_POSIX_H
-    
-    	ScriptServer::thread_exit();
-    
-    
-    {		if (error == WSAEWOULDBLOCK) {
-			// Expected when doing non-blocking sockets, retry later.
-		} else if (error == WSAECONNRESET) {
-			// If the remote target does not accept messages, this error may occur, but is harmless.
-			// Once the remote target gets available, this message will disappear for new messages.
-		} else {
-			close();
-			return FAILED;
-		}
-	}
-    
-    	ThreadWindows *t = reinterpret_cast<ThreadWindows *>(userdata);
-    
-      a[4].real = t3 - t6;
-  t3 += t6;
-  a[6].real = t3;
-    
-    static const uint8_t kRangeLimitLut[4 * 256] = {
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-  0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,  15,
-  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,
-  32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,
-  48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,
-  64,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,
-  80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  94,  95,
-  96,  97,  98,  99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111,
- 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127,
- 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143,
- 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159,
- 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175,
- 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191,
- 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207,
- 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223,
- 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239,
- 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
- 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-};
-    
-    // This function will create a Huffman tree.
-//
-// The (data,length) contains the population counts.
-// The tree_limit is the maximum bit depth of the Huffman codes.
-//
-// The depth contains the tree, i.e., how many bits are used for
-// the symbol.
-//
-// The actual Huffman tree is constructed in the tree[] array, which has to
-// be at least 2 * length + 1 long.
-//
-// See http://en.wikipedia.org/wiki/Huffman_coding
-void CreateHuffmanTree(const uint32_t *data,
-                       const size_t length,
-                       const int tree_limit,
-                       HuffmanTree* tree,
-                       uint8_t *depth);
-    
-    
-    {  // odd part
-  const int C1 = table[0];
-  const int C3 = table[2];
-  const int C5 = table[4];
-  const int C7 = table[6];
-  in[1] = DESCALE(C1 * b0 + C3 * b1 + C5 * b2 + C7 * b3);
-  in[3] = DESCALE(C3 * b0 - C7 * b1 - C1 * b2 - C5 * b3);
-  in[5] = DESCALE(C5 * b0 - C1 * b1 + C7 * b2 + C3 * b3);
-  in[7] = DESCALE(C7 * b0 - C5 * b1 + C3 * b2 - C1 * b3);
+    BOOST_AUTO_TEST_CASE(get_difficulty_for_mid_target)
+{
+    TestDifficulty(0x1df88f6f, 0.004023);
 }
-#undef DESCALE
-#undef LOAD_CST
-#undef LOAD
-#undef MULT
-#undef ADD
-#undef SUB
-#undef LSHIFT
-#undef STORE16
-#undef CORRECT_LSB
-#undef kTan1
-#undef kTan2
-#undef kTan3m1
-#undef k2Sqrt2
-#undef BUTTERFLY
-#undef COLUMN_DCT8
     
-      png_read_png(png_ptr, info_ptr, png_transforms, nullptr);
     
-      void WriteBits(int nbits, uint64_t bits) {
-    put_bits -= nbits;
-    put_buffer |= (bits << put_bits);
-    if (put_bits <= 16) {
-      // At this point we are ready to emit the most significant 6 bytes of
-      // put_buffer_ to the output.
-      // The JPEG format requires that after every 0xff byte in the entropy
-      // coded section, there is a zero byte, therefore we first check if any of
-      // the 6 most significant bytes of put_buffer_ is 0xff.
-      if (HasZeroByte(~put_buffer | 0xffff)) {
-        // We have a 0xff byte somewhere, examine each byte and append a zero
-        // byte if necessary.
-        EmitByte((put_buffer >> 56) & 0xff);
-        EmitByte((put_buffer >> 48) & 0xff);
-        EmitByte((put_buffer >> 40) & 0xff);
-        EmitByte((put_buffer >> 32) & 0xff);
-        EmitByte((put_buffer >> 24) & 0xff);
-        EmitByte((put_buffer >> 16) & 0xff);
-      } else if (pos + 6 < len) {
-        // We don't have any 0xff bytes, output all 6 bytes without checking.
-        data[pos] = (put_buffer >> 56) & 0xff;
-        data[pos + 1] = (put_buffer >> 48) & 0xff;
-        data[pos + 2] = (put_buffer >> 40) & 0xff;
-        data[pos + 3] = (put_buffer >> 32) & 0xff;
-        data[pos + 4] = (put_buffer >> 24) & 0xff;
-        data[pos + 5] = (put_buffer >> 16) & 0xff;
-        pos += 6;
-      } else {
-        overflow = true;
-      }
-      put_buffer <<= 48;
-      put_bits += 48;
+    {  for (size_t i = 0; i < windows.size(); ++i) {
+    // Only send close event to browser windows, since devtools windows will
+    // be automatically closed.
+    if (!windows[i]->is_devtools()) {
+      // If there is no js object bound to the window, then just close.
+      if (force || windows[i]->ShouldCloseWindow(quit))
+        // we used to delete the Shell object here
+        // but it should be deleted on native window destruction
+        windows[i]->window()->Close();
     }
   }
-    
-    #include 'guetzli/jpeg_data.h'
-    
-    // Mimic libjpeg's heuristics to guess jpeg color space.
-// Requires that the jpg has 3 components.
-bool HasYCbCrColorSpace(const JPEGData& jpg);
-    
-    /*
- * Encode a single unicode code point into a UTF-8 byte sequence.
- *
- * Return value is undefined if `cp' is an invalid code point.
- */
-std::string codePointToUtf8(char32_t cp);
-    
-    
-    {} // namespace std
-
-    
-        // Test the upper boundary of conversion to int64_t microseconds
-    using usec_i64 = std::chrono::duration<int64_t, std::micro>;
-    ts.tv_sec = 9223372036854LL;
-    ts.tv_nsec = 775807000;
-    EXPECT_EQ(std::numeric_limits<int64_t>::max(), to<usec_i64>(ts).count());
-    
-    /**
- * Helper functions for compression codecs.
- */
-namespace folly {
-namespace io {
-namespace compression {
-namespace detail {
+  if (force) {
+    // in a special force close case, since we're going to exit the
+    // main loop soon, we should delete the shell object asap so the
+    // render widget can be closed on the renderer side
+    windows = Shell::windows();
+    for (size_t i = 0; i < windows.size(); ++i) {
+      if (!windows[i]->is_devtools())
+        delete windows[i];
     }
+  }
+}
+    
+    void Base::CallSync(const std::string& method,
+                    const base::ListValue& arguments,
+                    base::ListValue* result) {
+  NOTREACHED() << 'Uncatched callAsync in Base'
+               << ' method:' << method
+               << ' arguments:' << arguments;
+}
+    
+    #include <map>
+    
+    
+    {  DISALLOW_COPY_AND_ASSIGN(MenuDelegate);
+};
+    
+    void MenuItem::SetIcon(const std::string& icon) {
+  if (icon.empty()) {
+    gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(menu_item_), NULL); 
+  } else {
+    gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(menu_item_),
+                                  gtk_image_new_from_file(icon.c_str()));
+    gtk_image_menu_item_set_always_show_image(GTK_IMAGE_MENU_ITEM(menu_item_),
+                                              TRUE);
+  }
+}
+    
+     protected:
+  ~NwAppSetProxyConfigFunction() override;
+    
+    #include 'extensions/browser/extension_function.h'
+    
+    class NwMenuGetNSStringWithFixupFunction : public NWSyncExtensionFunction {
+ public:
+  NwMenuGetNSStringWithFixupFunction(){}
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    
+ protected:
+  ~NwMenuGetNSStringWithFixupFunction() override {}
+    
+  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringWithFixup', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringWithFixupFunction);
+};
+    
+    #include 'godot_collision_dispatcher.h'
+    
+    	virtual void processCollision(const btCollisionObjectWrapper *body0Wrap, const btCollisionObjectWrapper *body1Wrap, const btDispatcherInfo &dispatchInfo, btManifoldResult *resultOut);
+	virtual btScalar calculateTimeOfImpact(btCollisionObject *body0, btCollisionObject *body1, const btDispatcherInfo &dispatchInfo, btManifoldResult *resultOut);
+    
+        NDMaskPtr NDMask::DeepClone(const DeviceDescriptor& device) const
+    {
+        NDMaskPtr newMask = MakeSharedObject<NDMask>(this->Shape(), device);
+        newMask->CopyFrom(*this);
     }
+    
+            void Erase() override
+        {
+            if (IsPacked())
+            {
+                m_packedData = nullptr;
+                m_packedDataLayout = nullptr;
+                m_isPacked = false;
+            }
+            else
+                Value::Erase();
+        }
+    
+    // some older code uses this namespace
+namespace DebugUtil
+{
+    void PrintCallStack(size_t skipLevels = 0, bool makeFunctionNamesStandOut = false);
     }
+    
+        virtual bool OutputUsedInComputingInputNodesGradients() const override
+    {
+        return false;
+    }
+    virtual bool InputUsedInComputingInputNodesGradients(size_t /*childIndex*/) const override
+    {
+        return false;
     }
