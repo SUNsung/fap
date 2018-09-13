9c5cf536dae9cc24982cdd950eae2d216f1733d0
SUNsung <weatherius@gmail.com>
2018-09-13 23:32:53 +0300

    Rewrite [11Azw65mnIh..11zeETcQAm6Q11cU5.MlOQ52M11Rlz.cpp] File in: 2018-09-13 23:32:52

--- cccccc/11Azw65mnIh..11zeETcQAm6Q11cU5.MlOQ52M11Rlz.cpp
+++ cccccc/11Azw65mnIh..11zeETcQAm6Q11cU5.MlOQ52M11Rlz.cpp
@@ -1,55 +1,318 @@
 
         
-        std::shared_ptr<Channel> CreateInsecureChannelFromFd(const grpc::string& target,
-                                                     int fd) {
-  internal::GrpcLibrary init_lib;
-  init_lib.init();
-  return CreateChannelInternal(
-      '', grpc_insecure_channel_create_from_fd(target.c_str(), fd, nullptr));
+        #include 'atom/app/command_line_args.h'
+    
+    // static
+void UvTaskRunner::OnClose(uv_handle_t* handle) {
+  delete reinterpret_cast<uv_timer_t*>(handle);
 }
     
-    namespace grpc {
+    #include <map>
+    
+      // add two new params for better error handling
+  errorObject->Set(mate::StringToV8(isolate(), 'code'),
+                   v8::Integer::New(isolate(), code));
+  errorObject->Set(mate::StringToV8(isolate(), 'domain'),
+                   mate::StringToV8(isolate(), domain));
+    
+    std::string DownloadItem::GetFilename() const {
+  return base::UTF16ToUTF8(
+      net::GenerateFileName(GetURL(), GetContentDisposition(), std::string(),
+                            download_item_->GetSuggestedFilename(),
+                            GetMimeType(), 'download')
+          .LossyDisplayName());
+}
+    
+    void GlobalShortcut::OnKeyPressed(const ui::Accelerator& accelerator) {
+  if (accelerator_callback_map_.find(accelerator) ==
+      accelerator_callback_map_.end()) {
+    // This should never occur, because if it does, GlobalGlobalShortcutListener
+    // notifes us with wrong accelerator.
+    NOTREACHED();
+    return;
+  }
+  accelerator_callback_map_[accelerator].Run();
+}
+    
+    namespace atom {
     }
     
-    #include <grpcpp/support/channel_arguments.h>
+    void Initialize(v8::Local<v8::Object> exports,
+                v8::Local<v8::Value> unused,
+                v8::Local<v8::Context> context,
+                void* priv) {
+#if defined(OS_MACOSX)
+  v8::Isolate* isolate = context->GetIsolate();
+  mate::Dictionary dict(isolate, exports);
+  dict.Set('inAppPurchase', InAppPurchase::Create(isolate));
+  dict.Set('InAppPurchase',
+           InAppPurchase::GetConstructor(isolate)->GetFunction());
+#endif
+}
     
-    
-    {}  // namespace grpc
-    
-    constexpr size_t TraceContextEncoding::kGrpcTraceContextSize;
-constexpr size_t TraceContextEncoding::kEncodeDecodeFailure;
-constexpr size_t TraceContextEncoding::kVersionIdSize;
-constexpr size_t TraceContextEncoding::kFieldIdSize;
-constexpr size_t TraceContextEncoding::kVersionIdOffset;
-constexpr size_t TraceContextEncoding::kVersionId;
-    
-    
-    {    uint8_t version = buf[kVersionIdOffset];
-    uint32_t fieldID = buf[kServerElapsedTimeOffset];
-    if (version != kVersionId || fieldID != kServerElapsedTimeField) {
-      *time = 0;
-      return kEncodeDecodeFailure;
+    class InAppPurchase : public mate::EventEmitter<InAppPurchase>,
+                      public in_app_purchase::TransactionObserver {
+ public:
+  static mate::Handle<InAppPurchase> Create(v8::Isolate* isolate);
     }
-    *time = absl::little_endian::Load64(
-        &buf[kServerElapsedTimeOffset + kFieldIdSize]);
-    return kRpcServerStatsSize;
+    
+      /// Retrieve the array of replacement types, which line up with the
+  /// generic parameters.
+  ///
+  /// Note that the types may be null, for cases where the generic parameter
+  /// is concrete but hasn't been queried yet.
+  ArrayRef<Type> getReplacementTypes() const {
+    return llvm::makeArrayRef(getTrailingObjects<Type>(),
+                              getNumReplacementTypes());
   }
     
-    void CensusServerCallData::Destroy(grpc_call_element* elem,
-                                   const grpc_call_final_info* final_info,
-                                   grpc_closure* then_call_closure) {
-  const uint64_t request_size = GetOutgoingDataSize(final_info);
-  const uint64_t response_size = GetIncomingDataSize(final_info);
-  double elapsed_time_ms = absl::ToDoubleMilliseconds(elapsed_time_);
-  grpc_auth_context_release(auth_context_);
-  ::opencensus::stats::Record(
-      {{RpcServerSentBytesPerRpc(), static_cast<double>(response_size)},
-       {RpcServerReceivedBytesPerRpc(), static_cast<double>(request_size)},
-       {RpcServerServerLatency(), elapsed_time_ms},
-       {RpcServerSentMessagesPerRpc(), sent_message_count_},
-       {RpcServerReceivedMessagesPerRpc(), recv_message_count_}},
-      {{ServerMethodTagKey(), method_},
-       {ServerStatusTagKey(), StatusCodeToString(final_info->final_status)}});
-  grpc_slice_unref_internal(path_);
-  context_.EndSpan();
+    DIRECTIONAL_PREPOSITION(above)
+DIRECTIONAL_PREPOSITION(after)
+DIRECTIONAL_PREPOSITION(along)
+DIRECTIONAL_PREPOSITION(alongside)
+DIRECTIONAL_PREPOSITION(as)
+DIRECTIONAL_PREPOSITION(at)
+DIRECTIONAL_PREPOSITION(before)
+DIRECTIONAL_PREPOSITION(below)
+DIRECTIONAL_PREPOSITION(by)
+DIRECTIONAL_PREPOSITION(following)
+DIRECTIONAL_PREPOSITION(for)
+DIRECTIONAL_PREPOSITION(from)
+DIRECTIONAL_PREPOSITION(given)
+DIRECTIONAL_PREPOSITION(in)
+DIRECTIONAL_PREPOSITION(including)
+DIRECTIONAL_PREPOSITION(inside)
+DIRECTIONAL_PREPOSITION(into)
+DIRECTIONAL_PREPOSITION(matching)
+DIRECTIONAL_PREPOSITION(of)
+DIRECTIONAL_PREPOSITION(on)
+DIRECTIONAL_PREPOSITION(passing)
+DIRECTIONAL_PREPOSITION(preceding)
+DIRECTIONAL_PREPOSITION(since)
+DIRECTIONAL_PREPOSITION(to)
+DIRECTIONAL_PREPOSITION(until)
+DIRECTIONAL_PREPOSITION(using)
+DIRECTIONAL_PREPOSITION(via)
+DIRECTIONAL_PREPOSITION(when)
+PREPOSITION(with)
+DIRECTIONAL_PREPOSITION(within)
+    
+      while (!RawText.empty()) {
+    size_t Pos = RawText.find_first_of('\n\r');
+    if (Pos == StringRef::npos)
+      Pos = RawText.size();
+    }
+    
+    /// Identify certain vacuous names to which we do not want to reduce any name.
+static bool isVacuousName(StringRef name) {
+  return camel_case::sameWordIgnoreFirstCase(name, 'get') ||
+         camel_case::sameWordIgnoreFirstCase(name, 'for') ||
+         camel_case::sameWordIgnoreFirstCase(name, 'set') ||
+         camel_case::sameWordIgnoreFirstCase(name, 'using') ||
+         camel_case::sameWordIgnoreFirstCase(name, 'with');
 }
+    
+    void DummyTaskQueue::addTask(const char *ExecPath, ArrayRef<const char *> Args,
+                             ArrayRef<const char *> Env, void *Context,
+                             bool SeparateErrors) {
+  QueuedTasks.emplace(std::unique_ptr<DummyTask>(
+      new DummyTask(ExecPath, Args, Env, Context, SeparateErrors)));
+}
+    
+    void swift::UUID::toString(llvm::SmallVectorImpl<char> &out) const {
+  out.resize(UUID::StringBufferSize);
+#if defined(_WIN32)
+  ::UUID uuid;
+  memcpy(&uuid, Value, Size);
+    }
+    
+    const uint16_t swift::unicode::ExtendedGraphemeClusterNoBoundaryRulesMatrix[] = {
+% for row in get_extended_grapheme_cluster_rules_matrix(break_table):
+  ${row},
+% end
+};
+    
+      bool isVoid() const {
+    assert(isValid());
+    return Decl.isNull();
+  }
+    
+    #include 'ClangDiagnosticConsumer.h'
+#include 'ImporterImpl.h'
+#include 'swift/AST/ASTContext.h'
+#include 'swift/AST/DiagnosticEngine.h'
+#include 'swift/AST/DiagnosticsClangImporter.h'
+#include 'clang/AST/ASTContext.h'
+#include 'clang/Frontend/DiagnosticRenderer.h'
+#include 'clang/Frontend/FrontendDiagnostic.h'
+#include 'clang/Lex/LexDiagnostic.h'
+#include 'llvm/ADT/STLExtras.h'
+    
+    IPC_MESSAGE_ROUTED3(ShellViewHostMsg_Allocate_Object,
+                    int /* object id */,
+                    std::string /* type name */,
+                    base::DictionaryValue /* option */)
+    
+    
+void Base::Call(const std::string& method, const base::ListValue& arguments,
+                content::RenderFrameHost* rvh) {
+  NOTREACHED() << 'Uncatched call in Base'
+               << ' method:' << method
+               << ' arguments:' << arguments;
+}
+    
+    namespace remote {
+    }
+    
+       void Call(const std::string& method,
+                    const base::ListValue& arguments) override;
+   void CallSync(const std::string& method,
+                        const base::ListValue& arguments,
+                        base::ListValue* result) override;
+    
+    
+    {}  // namespace nw
+
+    
+    
+bool MenuDelegate::GetAcceleratorForCommandId(
+      int command_id,
+      ui::Accelerator* accelerator) const {
+  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
+  if (!item)
+    return false;
+    }
+    
+    void Menu::UpdateKeys(GtkAccelGroup *gtk_accel_group){
+  this->gtk_accel_group = gtk_accel_group;
+  if (!GTK_IS_ACCEL_GROUP(gtk_accel_group)){
+    return ;
+  } else {
+    std::vector<MenuItem*>::iterator menu_item_iterator = menu_items.begin();
+    std::vector<MenuItem*>::iterator menu_item_end = menu_items.end();
+    while (menu_item_iterator != menu_item_end){
+      MenuItem *menu_item = *menu_item_iterator;
+      if (menu_item!=NULL && GTK_IS_MENU_ITEM(menu_item->menu_item_)){
+        menu_item->UpdateKeys(gtk_accel_group);
+      }
+      ++menu_item_iterator;
+    }
+  }
+}
+    
+    
+    {
+  DECLARE_EXTENSION_FUNCTION('nw.App.setProxyConfig', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwAppSetProxyConfigFunction);
+};
+    
+    #include 'extensions/browser/extension_function.h'
+    
+    #endif // DEBUG_METHODS_ENABLED
+    
+    #include 'core/math/matrix3.h'
+#include 'core/math/transform.h'
+#include 'core/math/vector3.h'
+#include 'core/typedefs.h'
+    
+    	const btCollisionObjectWrapper *other_co_wrapper;
+    
+    Follow* Follow::create(Node *followedNode, const Rect& rect/* = Rect::ZERO*/)
+{
+    return createWithOffset(followedNode, 0.0, 0.0,rect);
+}
+    
+    void ActionEase::update(float time)
+{
+    _inner->update(time);
+}
+    
+    // implementation AccelDeccelAmplitude
+    
+        if (action && action->initWithDuration(duration))
+    {
+        action->autorelease();
+        return action;
+    }
+    
+    //
+// FlipX
+//
+    
+        if (_seed != (unsigned int)-1)
+    {
+        std::srand(_seed);
+    }
+    
+        /** whether or not it shall restore the original frame when the animation finishes. */
+    bool _restoreOriginalFrame;
+    
+    /**
+ * @addtogroup _2d
+ * @{
+ */
+    }
+    
+    Status ColumnFamilyData::RangesOverlapWithMemtables(
+    const autovector<Range>& ranges, SuperVersion* super_version,
+    bool* overlap) {
+  assert(overlap != nullptr);
+  *overlap = false;
+  // Create an InternalIterator over all unflushed memtables
+  Arena arena;
+  ReadOptions read_opts;
+  read_opts.total_order_seek = true;
+  MergeIteratorBuilder merge_iter_builder(&internal_comparator_, &arena);
+  merge_iter_builder.AddIterator(
+      super_version->mem->NewIterator(read_opts, &arena));
+  super_version->imm->AddIterators(read_opts, &merge_iter_builder);
+  ScopedArenaIterator memtable_iter(merge_iter_builder.Finish());
+    }
+    
+      using DBImpl::Put;
+  virtual Status Put(const WriteOptions& /*options*/,
+                     ColumnFamilyHandle* /*column_family*/,
+                     const Slice& /*key*/, const Slice& /*value*/) override {
+    return Status::NotSupported('Not supported in compacted db mode.');
+  }
+  using DBImpl::Merge;
+  virtual Status Merge(const WriteOptions& /*options*/,
+                       ColumnFamilyHandle* /*column_family*/,
+                       const Slice& /*key*/, const Slice& /*value*/) override {
+    return Status::NotSupported('Not supported in compacted db mode.');
+  }
+  using DBImpl::Delete;
+  virtual Status Delete(const WriteOptions& /*options*/,
+                        ColumnFamilyHandle* /*column_family*/,
+                        const Slice& /*key*/) override {
+    return Status::NotSupported('Not supported in compacted db mode.');
+  }
+  virtual Status Write(const WriteOptions& /*options*/,
+                       WriteBatch* /*updates*/) override {
+    return Status::NotSupported('Not supported in compacted db mode.');
+  }
+  using DBImpl::CompactRange;
+  virtual Status CompactRange(const CompactRangeOptions& /*options*/,
+                              ColumnFamilyHandle* /*column_family*/,
+                              const Slice* /*begin*/,
+                              const Slice* /*end*/) override {
+    return Status::NotSupported('Not supported in compacted db mode.');
+  }
+    
+          options.compression_per_level.resize(3);
+      options.compression_per_level[0] = kNoCompression;
+      options.compression_per_level[1] = kLZ4Compression;
+      options.compression_per_level[2] = kSnappyCompression;
+      options.env = env_;
+    
+    
+    {}  // namespace rocksdb
+
+    
+      // if background compaction is not working, write will stall
+  // because of options.level0_stop_writes_trigger
+  for (int i = 1000; i < 99999; ++i) {
+    db->Put(WriteOptions(), std::to_string(i),
+                            std::string(500, 'a' + (i % 26)));
+  }
