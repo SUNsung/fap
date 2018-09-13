661fd97c32e593413b6de3d33899fedd22eaca9d
SUNsung <weatherius@gmail.com>
2018-09-13 08:31:04 +0300

    Rewrite [37ee9WDclnXGs37gjZAgT2rmxE373j7KDepwWwE37ND9.cpp] File in: 2018-09-13 08:31:03

--- cccccc/37ee9WDclnXGs37gjZAgT2rmxE373j7KDepwWwE37ND9.cpp
+++ cccccc/37ee9WDclnXGs37gjZAgT2rmxE373j7KDepwWwE37ND9.cpp
@@ -1,201 +1,100 @@
 
         
-            accumulateSquareConstFunc funcs[16] =
+        #include <QWidget>
+#include <QQueue>
+    
+        Round(a, b, c, d, e, f, g, h, 0x27b70a85, w0 += sigma1(w14) + w9 + sigma0(w1));
+    Round(h, a, b, c, d, e, f, g, 0x2e1b2138, w1 += sigma1(w15) + w10 + sigma0(w2));
+    Round(g, h, a, b, c, d, e, f, 0x4d2c6dfc, w2 += sigma1(w0) + w11 + sigma0(w3));
+    Round(f, g, h, a, b, c, d, e, 0x53380d13, w3 += sigma1(w1) + w12 + sigma0(w4));
+    Round(e, f, g, h, a, b, c, d, 0x650a7354, w4 += sigma1(w2) + w13 + sigma0(w5));
+    Round(d, e, f, g, h, a, b, c, 0x766a0abb, w5 += sigma1(w3) + w14 + sigma0(w6));
+    Round(c, d, e, f, g, h, a, b, 0x81c2c92e, w6 += sigma1(w4) + w15 + sigma0(w7));
+    Round(b, c, d, e, f, g, h, a, 0x92722c85, w7 += sigma1(w5) + w0 + sigma0(w8));
+    Round(a, b, c, d, e, f, g, h, 0xa2bfe8a1, w8 += sigma1(w6) + w1 + sigma0(w9));
+    Round(h, a, b, c, d, e, f, g, 0xa81a664b, w9 += sigma1(w7) + w2 + sigma0(w10));
+    Round(g, h, a, b, c, d, e, f, 0xc24b8b70, w10 += sigma1(w8) + w3 + sigma0(w11));
+    Round(f, g, h, a, b, c, d, e, 0xc76c51a3, w11 += sigma1(w9) + w4 + sigma0(w12));
+    Round(e, f, g, h, a, b, c, d, 0xd192e819, w12 += sigma1(w10) + w5 + sigma0(w13));
+    Round(d, e, f, g, h, a, b, c, 0xd6990624, w13 += sigma1(w11) + w6 + sigma0(w14));
+    Round(c, d, e, f, g, h, a, b, 0xf40e3585, w14 += sigma1(w12) + w7 + sigma0(w15));
+    Round(b, c, d, e, f, g, h, a, 0x106aa070, w15 += sigma1(w13) + w8 + sigma0(w0));
+    
+    
+    {    // Inputs that are effectively invalid under the target grammar.
+    // PROTOCOLINFO accepts an OtherLine that is just an OptArguments, which
+    // would make these inputs valid. However,
+    // - This parser is never used in that situation, because the
+    //   SplitTorReplyLine parser enables OtherLine to be skipped.
+    // - Even if these were valid, an OptArguments contains no semantic data,
+    //   so there is no point in parsing it.
+    CheckParseTorReplyMapping('ARGS', {});
+    CheckParseTorReplyMapping('MORE ARGS', {});
+    CheckParseTorReplyMapping('MORE  ARGS', {});
+    CheckParseTorReplyMapping('EVEN more=ARGS', {});
+    CheckParseTorReplyMapping('EVEN+more ARGS', {});
+}
+    
+    void PrintDebug(ProcessStats* stats, std::string s);
+    
+    
     {
-        accumulateSquareConst<0>,
-        accumulateSquareConst<1>,
-        accumulateSquareConst<2>,
-        accumulateSquareConst<3>,
-        accumulateSquareConst<4>,
-        accumulateSquareConst<5>,
-        accumulateSquareConst<6>,
-        accumulateSquareConst<7>,
-        accumulateSquareConst<8>,
-        accumulateSquareConst<9>,
-        accumulateSquareConst<10>,
-        accumulateSquareConst<11>,
-        accumulateSquareConst<12>,
-        accumulateSquareConst<13>,
-        accumulateSquareConst<14>,
-        accumulateSquareConst<15>
-    }, func = funcs[shift];
-    
-    #include 'common.hpp'
-#include 'vtransform.hpp'
-    
-                float32x4_t lane0c = vld1q_f32(lanea + x - cn);
-            float32x4_t lane2c = vld1q_f32(lanea + x + cn);
-            float32x4_t lane1c = vld1q_f32(lanea + x);
-    
-    
     {
-    {        _x = internal::getRowPtr(dxBase, dxStride, i-1);
-        _y = internal::getRowPtr(dyBase, dyStride, i-1);
+    {      // Add back the last sentinel node.
+      tree[j_end + 1] = sentinel;
     }
+    if (SetDepth(static_cast<int>(2 * n - 1), &tree[0], depth, tree_limit)) {
+      /* We need to pack the Huffman tree in tree_limit bits. If this was not
+         successful, add fake entities to the lowest values and retry. */
+      break;
+    }
+  }
+}
+    
+    // Computes the DCT (Discrete Cosine Transform) of the 8x8 array in 'block',
+// scaled up by a factor of 16. The values in 'block' are laid out row-by-row
+// and the result is written to the same memory area.
+void ComputeBlockDCT(coeff_t* block);
+    
+    
+    {  FILE* f = write_to_stdout ? stdout : fopen(filename, 'wb');
+  if (!f) {
+    perror('Can't open output file for writing');
+    exit(1);
+  }
+  if (fwrite(contents.data(), 1, contents.size(), f) != contents.size()) {
+    perror('fwrite');
+    exit(1);
+  }
+  if (fclose(f) < 0) {
+    perror('fclose');
+    exit(1);
+  }
+}
+    
+    #include 'guetzli/jpeg_data.h'
+    
+    
+    {}  // namespace guetzli
+    
+    // Parses the jpeg stream contained in data[*pos ... len) and fills in *jpg with
+// the parsed information.
+// If mode is JPEG_READ_HEADER, it fills in only the image dimensions in *jpg.
+// Returns false if the data is not valid jpeg, or if it contains an unsupported
+// jpeg feature.
+bool ReadJpeg(const uint8_t* data, const size_t len, JpegReadMode mode,
+              JPEGData* jpg);
+// string variant
+bool ReadJpeg(const std::string& data, JpegReadMode mode,
+              JPEGData* jpg);
+    
+    // Output callback function with associated data.
+struct JPEGOutput {
+  JPEGOutput(JPEGOutputHook cb, void* data) : cb(cb), data(data) {}
+  bool Write(const uint8_t* buf, size_t len) const {
+    return (len == 0) || (cb(data, buf, len) == len);
+  }
+ private:
+  JPEGOutputHook cb;
+  void* data;
 };
-    
-    /*!
- *  Aligns pointer by the certain number of bytes
- *
- *  This small inline function aligns the pointer by the certain number of bytes by shifting
- *  it forward by 0 or a positive offset.
- */
-template<typename T> inline T* alignPtr(T* ptr, size_t n=sizeof(T))
-{
-    return (T*)(((size_t)ptr + n-1) & -n);
-}
-    
-    typedef void (* rshiftConstFunc)(const Size2D &size,
-                                const s16 * srcBase, ptrdiff_t srcStride,
-                                u8 * dstBase, ptrdiff_t dstStride,
-                                CONVERT_POLICY cpolicy);
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 7 && !defined(__clang__)
-CVTS_FUNC(f32, u8, 8,
-    register float32x4_t vscale asm ('q0') = vdupq_n_f32((f32)((1 << 16)*alpha));
-    register float32x4_t vshift asm ('q1') = vdupq_n_f32((f32)((1 << 16)*beta));
-    register uint32x4_t  vmask  asm ('q2') = vdupq_n_u32(1<<16);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        __asm__ (
-            'vld1.32 {d6-d7}, [%[src1]]                              \n\t'
-            'vld1.32 {d8-d9}, [%[src2]]                              \n\t'
-            'vmul.f32 q5, q3, q0                                     \n\t'
-            'vmul.f32 q6, q4, q0                                     \n\t'
-            'vadd.f32 q7, q5, q1                                     \n\t'
-            'vadd.f32 q8, q6, q1                                     \n\t'
-            'vcvt.u32.f32 q9, q7                                     \n\t'
-            'vcvt.u32.f32 q10, q8                                    \n\t'
-            'vbic q11, q2, q6                                        \n\t'
-            'vbic q12, q2, q7                                        \n\t'
-            'vshr.u32 q13, q11, #16                                  \n\t'
-            'vshr.u32 q14, q12, #16                                  \n\t'
-            'vqsub.u32 q7, q9, q13                                   \n\t'
-            'vqsub.u32 q8, q10, q14                                  \n\t'
-            'vqrshrn.u32 d22, q7, #16                                \n\t'
-            'vqrshrn.u32 d23, q8, #16                                \n\t'
-            'vqmovn.u16 d30, q11                                     \n\t'
-            'vst1.8 {d30}, [%[dst]]                                  \n\t'
-            : /*no output*/
-            : [src1] 'r' (_src + i + 0),
-              [src2] 'r' (_src + i + 4),
-              [dst] 'r' (_dst + i),
-              'w' (vscale), 'w' (vshift), 'w' (vmask)
-            : 'd6','d7','d8','d9','d10','d11','d12','d13','d14','d15','d16','d17','d18','d19','d20','d21','d22','d23','d24','d25','d26','d27','d28','d29','d30'
-        );
-    }
-})
-#else
-CVTS_FUNC(f32, u8, 8,
-    float32x4_t vscale = vdupq_n_f32((f32)((1 << 16)*alpha));
-    float32x4_t vshift = vdupq_n_f32((f32)((1 << 16)*beta));
-    uint32x4_t  vmask  = vdupq_n_u32(1<<16);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        float32x4_t vline1_f32 = vld1q_f32(_src + i + 0);
-        float32x4_t vline2_f32 = vld1q_f32(_src + i + 4);
-    }
-    }
-    
-    #endif
-    
-        for(size_t j = 0; j < size.height; ++j)
-    {
-        const T *  src = internal::getRowPtr( srcBase,  srcStride, j);
-        const T * rng1 = internal::getRowPtr(rng1Base, rng1Stride, j);
-        const T * rng2 = internal::getRowPtr(rng2Base, rng2Stride, j);
-             u8 *  dst = internal::getRowPtr( dstBase,  dstStride, j);
-        size_t i = 0;
-        for( ; i < width; i += 32/sizeof(T) )
-        {
-            internal::prefetch(src + i);
-            internal::prefetch(rng1 + i);
-            internal::prefetch(rng2 + i);
-    }
-    }
-    
-    void integral(const Size2D &size,
-              const u8 * srcBase, ptrdiff_t srcStride,
-              u32 * sumBase, ptrdiff_t sumStride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    uint32x4_t v_zero = vmovq_n_u32(0u);
-    }
-    
-        template <typename ElementType, typename V1ElemType>
-    ElementType NDArrayView::_AsScalar() const
-    {
-        auto scalarData = this->shared_from_this();
-        if (scalarData->Shape().TotalSize() != 1)
-            LogicError('NDArrayView::AsScalar: The NDArrayView shaped '%S' is not a scalar.', scalarData->Shape().AsString().c_str());
-    }
-    
-                // Validate that each of the dynamic axes are unique
-            std::unordered_set<Axis> uniqueDynamicAxis;
-            for (auto& currentDynamicAxis : dynamicAxes)
-            {
-                auto retVal = uniqueDynamicAxis.insert(currentDynamicAxis);
-                if (!retVal.second)
-                    InvalidArgument('Dynamic axis named %S is specified more than once for Variable '%S'', currentDynamicAxis.Name().c_str(), AsString().c_str());
-            }
-    
-        virtual void /*ComputationNodeBase::*/ Validate(bool isFinalValidationPass) override
-    {
-        Base::Validate(isFinalValidationPass);
-        InferMBLayoutFromInputsForStandardCase(isFinalValidationPass);
-    }
-    
-    int main(int argc, char** argv) {
-  return leveldb::test::RunAllTests();
-}
-
-    
-    // Build a Table file from the contents of *iter.  The generated file
-// will be named according to meta->number.  On success, the rest of
-// *meta will be filled with metadata about the generated table.
-// If no data is present in *iter, meta->file_size will be set to
-// zero, and no Table file will be produced.
-Status BuildTable(const std::string& dbname,
-                  Env* env,
-                  const Options& options,
-                  TableCache* table_cache,
-                  Iterator* iter,
-                  FileMetaData* meta);
-    
-      Corrupt(kTableFile, 100, 1);
-  Check(5, 9);
-    
-    #include 'db/db_iter.h'
-    
-    #endif  // STORAGE_LEVELDB_DB_DBFORMAT_H_
-
-    
-    
-    {  delete iter;
-  delete table;
-  delete file;
-  return Status::OK();
-}
-    
-    // Return the legacy file name for an sstable with the specified number
-// in the db named by 'dbname'. The result will be prefixed with
-// 'dbname'.
-std::string SSTTableFileName(const std::string& dbname, uint64_t number);
-    
-      // Offset at which to start looking for the first record to return
-  uint64_t const initial_offset_;
-    
-    
-    {    virtual Status Close() { return Status::OK(); }
-    virtual Status Flush() { return Status::OK(); }
-    virtual Status Sync() { return Status::OK(); }
-    virtual Status Append(const Slice& slice) {
-      contents_.append(slice.data(), slice.size());
-      return Status::OK();
-    }
-  };
