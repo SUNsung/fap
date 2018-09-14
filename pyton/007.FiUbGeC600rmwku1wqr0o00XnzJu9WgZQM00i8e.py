f0676f55024145dc55f0b7acfa9879ae81a5f25e
SUNsung <weatherius@gmail.com>
2018-09-14 22:00:52 +0300

    Rewrite [007.FiUbGeC600rmwku1wqr0o00XnzJu9WgZQM00i8e.py] File in: 2018-09-14 22:00:51

--- pyton/007.FiUbGeC600rmwku1wqr0o00XnzJu9WgZQM00i8e.py
+++ pyton/007.FiUbGeC600rmwku1wqr0o00XnzJu9WgZQM00i8e.py
@@ -1,61 +1,53 @@
 
         
-        
-class Fixed(jose.Field):
-    '''Fixed field.'''
+            def __init__(self, operators, supervisors, directors):
+        self.operators = operators
+        self.supervisors = supervisors
+        self.directors = directors
+        self.queued_calls = deque()
     
-        def test_default_decoder_raises_deserialization_error(self):
-        from acme.fields import RFC3339Field
-        self.assertRaises(
-            jose.DeserializationError, RFC3339Field.default_decoder, '')
+        def reducer(self, key, values):
+        total = sum(values)
+        if total == 1:
+            yield key, total
     
-        def setUp(self):
-        self.privkey = KEY
-        self.pubkey = self.privkey.public_key()
-        self.nonce = jose.b64encode(b'Nonce')
-        self.url = 'hi'
-        self.kid = 'baaaaa'
+        def __init__(self, key, value):
+        self.key = key
+        self.value = value
     
+        def crawl_page(self, page):
+        for url in page.child_urls:
+            self.data_store.add_link_to_crawl(url)
+        self.reverse_index_queue.generate(page)
+        self.doc_index_queue.generate(page)
+        self.data_store.remove_link_to_crawl(page.url)
+        self.data_store.insert_crawled_link(page.url, page.signature)
     
-def skip_unless(condition, reason):  # pragma: no cover
-    '''Skip tests unless a condition holds.
-    
-        @certbot_util.patch_get_utility()
-    def test_select_cancel(self, mock_util):
-        mock_util().checklist.return_value = (display_util.CANCEL, 'whatever')
-        vhs = select_vhost_multiple([self.vhosts[2], self.vhosts[3]])
-        self.assertFalse(vhs)
-    
-        def execute(self):
-        if not self._bProblem:
-            print('Executing the test')
-            time.sleep(0.1)
+        def setdefault(self, key, value):
+        if key in self._session:
+            return self._session[key]
         else:
-            print('Problem in setup. Test not executed.')
+            self.modified = True
+            self._session[key] = value
+            return value
     
-        REGISTRY = {}
+        def save(self, session_key, session_dict, expire_date):
+        s = self.model(session_key, self.encode(session_dict), expire_date)
+        if session_dict:
+            s.save()
+        else:
+            s.delete()  # Clear sessions with no data.
+        return s
+    
+        For complete documentation on using Sessions in your code, consult
+    the sessions documentation that is shipped with Django (also available
+    on the Django Web site).
+    '''
+    objects = SessionManager()
     
     
-a = A()
-b = B()
-c = C()
-visitor = Visitor()
-visitor.visit(a)
-visitor.visit(b)
-visitor.visit(c)
-    
-        def build_floor(self):
-        raise NotImplementedError
-    
-        def __init__(self, blackboard):
-        self.blackboard = blackboard
-    
-    ''
-    
-        def __get__(self, obj, klas):
-        print('(Fetching from Data Store)')
-        return {'products': self.products}
-    
-        # high-level i.e. Abstraction specific
-    def scale(self, pct):
-        self._radius *= pct
+def is_code(line, indent_depth = 4):
+    '''returns the indent depth, 0 means not code in markup'''
+    if line.startswith(' ' * indent_depth):
+        return len(line) - len(line.lstrip(' '))
+    return 0
