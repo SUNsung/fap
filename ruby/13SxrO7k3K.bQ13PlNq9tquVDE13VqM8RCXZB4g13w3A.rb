e6a318b15d61fbe68591a3cc0452dd068e4d8b57
SUNsung <weatherius@gmail.com>
2018-09-14 11:46:41 +0300

    Rewrite [13SxrO7k3K.bQ13PlNq9tquVDE13VqM8RCXZB4g13w3A.rb] File in: 2018-09-14 11:46:41

--- ruby/13SxrO7k3K.bQ13PlNq9tquVDE13VqM8RCXZB4g13w3A.rb
+++ ruby/13SxrO7k3K.bQ13PlNq9tquVDE13VqM8RCXZB4g13w3A.rb
@@ -1,133 +1,183 @@
 
         
-          it 'imports a scenario that does not exist yet' do
-    visit new_scenario_imports_path
-    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
-    click_on 'Start Import'
-    expect(page).to have_text('This scenario has a few agents to get you started. Feel free to change them or delete them as you see fit!')
-    expect(page).not_to have_text('This Scenario already exists in your system.')
-    check('I confirm that I want to import these Agents.')
-    click_on 'Finish Import'
-    expect(page).to have_text('Import successful!')
-  end
+            Category.transaction do
+      staff.group_names = ['staff']
+      unless staff.save
+        puts staff.errors.full_messages
+        raise 'Failed to set permissions on the Staff category!'
+      end
     
-        it 'works for queued jobs' do
-      expect(status(job)).to eq('<span class='label label-warning'>queued</span>')
-    end
-  end
-    
-        it 'is turned off for existing instances of Huginn' do
-      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
-      stub.proxy(ENV).[](anything)
-      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { nil }
-      DefaultScenarioImporter.import(user)
+        it 'understands hl=-' do
+      stub(params).[](:hl) { '-' }
+      expect((1..10).select { |i| highlighted?(i) }).to eq [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     end
     
-                scenario_import.merges = {
-              '0' => {
-                'name' => 'a new name',
-                'schedule' => '6pm',
-                'keep_events_for' => 2.days.to_i.to_s,
-                'disabled' => 'true',
-                'options' => weather_agent_options.merge('api_key' => 'foo').to_json
-              }
-            }
+    describe DotHelper do
+  describe 'with example Agents' do
+    class Agents::DotFoo < Agent
+      default_schedule '2pm'
     
-            agent_runner.stop
+      describe '#status' do
+    it 'works for failed jobs' do
+      job.failed_at = Time.now
+      expect(status(job)).to eq('<span class='label label-danger'>failed</span>')
+    end
+    
+      describe '#scenario_label' do
+    it 'creates a scenario label with the scenario name' do
+      expect(scenario_label(scenario)).to eq(
+        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Scene</span>'
+      )
+    end
+    
+    describe ScenarioImport do
+  let(:user) { users(:bob) }
+  let(:guid) { 'somescenarioguid' }
+  let(:tag_fg_color) { '#ffffff' }
+  let(:tag_bg_color) { '#000000' }
+  let(:icon) { 'Star' }
+  let(:description) { 'This is a cool Huginn Scenario that does something useful!' }
+  let(:name) { 'A useful Scenario' }
+  let(:source_url) { 'http://example.com/scenarios/2/export.json' }
+  let(:weather_agent_options) {
+    {
+      'api_key' => 'some-api-key',
+      'location' => '12345'
+    }
+  }
+  let(:trigger_agent_options) {
+    {
+      'expected_receive_period_in_days' => 2,
+      'rules' => [{
+                    'type' => 'regex',
+                    'value' => 'rain|storm',
+                    'path' => 'conditions',
+                  }],
+      'message' => 'Looks like rain!'
+    }
+  }
+  let(:valid_parsed_weather_agent_data) do
+    {
+      :type => 'Agents::WeatherAgent',
+      :name => 'a weather agent',
+      :schedule => '5pm',
+      :keep_events_for => 14.days,
+      :disabled => true,
+      :guid => 'a-weather-agent',
+      :options => weather_agent_options
+    }
+  end
+  let(:valid_parsed_trigger_agent_data) do
+    {
+      :type => 'Agents::TriggerAgent',
+      :name => 'listen for weather',
+      :keep_events_for => 0,
+      :propagate_immediately => true,
+      :disabled => false,
+      :guid => 'a-trigger-agent',
+      :options => trigger_agent_options
+    }
+  end
+  let(:valid_parsed_basecamp_agent_data) do
+    {
+      :type => 'Agents::BasecampAgent',
+      :name => 'Basecamp test',
+      :schedule => 'every_2m',
+      :keep_events_for => 0,
+      :propagate_immediately => true,
+      :disabled => false,
+      :guid => 'a-basecamp-agent',
+      :options => {project_id: 12345}
+    }
+  end
+  let(:valid_parsed_data) do
+    {
+      schema_version: 1,
+      name: name,
+      description: description,
+      guid: guid,
+      tag_fg_color: tag_fg_color,
+      tag_bg_color: tag_bg_color,
+      icon: icon,
+      source_url: source_url,
+      exported_at: 2.days.ago.utc.iso8601,
+      agents: [
+        valid_parsed_weather_agent_data,
+        valid_parsed_trigger_agent_data
+      ],
+      links: [
+        { :source => 0, :receiver => 1 }
+      ],
+      control_links: []
+    }
+  end
+  let(:valid_data) { valid_parsed_data.to_json }
+  let(:invalid_data) { { :name => 'some scenario missing a guid' }.to_json }
+    
+        it 'should work with nested arrays' do
+      @agent.options['array'] = ['one', '$.two']
+      LiquidMigrator.convert_all_agent_options(@agent)
+      expect(@agent.reload.options).to eq({'auth_token' => 'token', 'color' => 'yellow', 'array' => ['one', '{{two}}'], 'notify' => false, 'room_name' => 'test', 'username' => '{{username}}', 'message' => '{{message}}'})
+    end
+    
+      it 'replaces invalid byte sequences in a message' do
+    log = AgentLog.new(:agent => agents(:jane_website_agent), level: 3)
+    log.message = '\u{3042}\xffA\x95'
+    expect { log.save! }.not_to raise_error
+    expect(log.message).to eq('\u{3042}<ff>A\<95>')
+  end
+    
+    SPREE_GEMS = %w(core api cmd backend frontend sample).freeze
+    
+            def find_address
+          if @order.bill_address_id == params[:id].to_i
+            @order.bill_address
+          elsif @order.ship_address_id == params[:id].to_i
+            @order.ship_address
+          else
+            raise CanCan::AccessDenied
+          end
+        end
       end
     end
-    
-      def guid_order(agent_list, agent_name)
-    agent_list.map{|a|a.guid}.sort.find_index(agents(agent_name).guid)
   end
 end
 
     
-        it 'work with set FAILED_JOBS_TO_KEEP env variable' do
-      expect { @scheduler.send(:cleanup_failed_jobs!) }.to change(Delayed::Job, :count).by(-1)
-      expect { @scheduler.send(:cleanup_failed_jobs!) }.to change(Delayed::Job, :count).by(0)
-      expect(@keep.id).to eq(Delayed::Job.order(:failed_at)[0].id)
-    end
-    
-          @log.level = -1
-      expect(@log).not_to be_valid
-      expect(@log).to have(1).error_on(:level)
-    
-        def log_transform(*args, from: caller[1][/`.*'/][1..-2].sub(/^block in /, ''))
-      puts '    #{cyan from}#{cyan ': #{args * ', '}' unless args.empty?}'
-    end
-    
-      # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-    
-      not_found do
-    send_file(File.join(File.dirname(__FILE__), 'public', '404.html'), {:status => 404})
-  end
-    
-            Dir.chdir(includes_dir) do
-          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
-          if choices.include?(file)
-            source = File.read(file)
-            partial = Liquid::Template.parse(source)
-            context.stack do
-              rtn = rtn + partial.render(context)
-            end
-          else
-            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
+            def scope
+          if params[:product_id]
+            Spree::Product.friendly.find(params[:product_id])
+          elsif params[:variant_id]
+            Spree::Variant.find(params[:variant_id])
           end
         end
       end
-      rtn
-    end
-  end
-    
-        def sizes
-      attrs = 'width='#{@sizes[0]}'' if @sizes[0]
-      attrs += ' height='#{@sizes[1]}'' if @sizes[1]
-      attrs
     end
   end
 end
+
     
-          # users should be able to set price when importing orders via api
-      def permitted_line_item_attributes
-        if @current_user_roles.include?('admin')
-          super + [:price, :variant_id, :sku]
-        else
-          super
-        end
-      end
+              can_event = 'can_#{@event}?'
     
-            def order
-          @order ||= Spree::Order.find_by!(number: order_id)
-          authorize! :read, @order
-        end
-    
-              state = @states.last
-          respond_with(@states) if stale?(state)
+            def create
+          authorize! :create, Spree::OptionType
+          @option_type = Spree::OptionType.new(option_type_params)
+          if @option_type.save
+            render :show, status: 201
+          else
+            invalid_resource!(@option_type)
+          end
         end
     
-            def stock_location
-          render 'spree/api/v1/shared/stock_location_required', status: 422 and return unless params[:stock_location_id]
-          @stock_location ||= StockLocation.accessible_by(current_ability, :read).find(params[:stock_location_id])
+            def remove_coupon_code
+          find_order(true)
+          authorize! :update, @order, order_token
+          @handler = Spree::PromotionHandler::Coupon.new(@order).remove(params[:coupon_code])
+          status = @handler.successful? ? 200 : 404
+          render 'spree/api/v1/promotions/handler', status: status
         end
     
-        def raise_if_blank_file
-      if path.blank?
-        raise Errors::NotIdentifiedByImageMagickError.new('Cannot find the geometry of a file with a blank name')
-      end
-    end
-    
-        def add_active_record_callbacks
-      name = @name
-      @klass.send(:after_save) { send(name).send(:save) }
-      @klass.send(:before_destroy) { send(name).send(:queue_all_for_delete) }
-      if @klass.respond_to?(:after_commit)
-        @klass.send(:after_commit, on: :destroy) do
-          send(name).send(:flush_deletes)
-        end
-      else
-        @klass.send(:after_destroy) { send(name).send(:flush_deletes) }
-      end
-    end
+              if params[:stock_item].key?(:backorderable)
+            @stock_item.backorderable = params[:stock_item][:backorderable]
+            @stock_item.save
+          end
