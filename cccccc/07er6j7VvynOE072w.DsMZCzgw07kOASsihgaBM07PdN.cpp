aff7b624868623a5a2aeaf4e37fb64734e6affd5
SUNsung <weatherius@gmail.com>
2018-09-15 01:19:28 +0300

    Rewrite [07er6j7VvynOE072w.DsMZCzgw07kOASsihgaBM07PdN.cpp] File in: 2018-09-15 01:19:27

--- cccccc/07er6j7VvynOE072w.DsMZCzgw07kOASsihgaBM07PdN.cpp
+++ cccccc/07er6j7VvynOE072w.DsMZCzgw07kOASsihgaBM07PdN.cpp
@@ -1,320 +1,203 @@
- protected:
-  // content::ContentMainDelegate:
-  bool BasicStartupComplete(int* exit_code) override;
-  void PreSandboxStartup() override;
-  content::ContentBrowserClient* CreateContentBrowserClient() override;
-  content::ContentRendererClient* CreateContentRendererClient() override;
-  content::ContentUtilityClient* CreateContentUtilityClient() override;
-  int RunProcess(
-      const std::string& process_type,
-      const content::MainFunctionParams& main_function_params) override;
-#if defined(OS_MACOSX)
-  bool ShouldSendMachPort(const std::string& process_type) override;
-  bool DelaySandboxInitialization(const std::string& process_type) override;
-#endif
-    
-    using atom::api::BoxLayout;
-    
-    using atom::api::Button;
+
+        
+          AutoCompactTest() {
+    dbname_ = test::TmpDir() + '/autocompact_test';
+    tiny_cache_ = NewLRUCache(100);
+    options_.block_cache = tiny_cache_;
+    DestroyDB(dbname_, options_);
+    options_.create_if_missing = true;
+    options_.compression = kNoCompression;
+    ASSERT_OK(DB::Open(options_, dbname_, &db_));
+  }
     
     
-    { private:
-  DISALLOW_COPY_AND_ASSIGN(Button);
+    {  std::string tmp1, tmp2;
+  ASSERT_OK(db_->Put(WriteOptions(), Key(1000, &tmp1), Value(1000, &tmp2)));
+  std::string v;
+  ASSERT_OK(db_->Get(ReadOptions(), Key(1000, &tmp1), &v));
+  ASSERT_EQ(Value(1000, &tmp2).ToString(), v);
+  dbi->TEST_CompactMemTable();
+  ASSERT_OK(db_->Get(ReadOptions(), Key(1000, &tmp1), &v));
+  ASSERT_EQ(Value(1000, &tmp2).ToString(), v);
+}
+    
+    std::string DescriptorFileName(const std::string& dbname, uint64_t number) {
+  assert(number > 0);
+  char buf[100];
+  snprintf(buf, sizeof(buf), '/MANIFEST-%06llu',
+           static_cast<unsigned long long>(number));
+  return dbname + buf;
+}
+    
+    
+    {}  // namespace leveldb
+    
+        if (!status.ok()) {
+      env_->DeleteFile(tmp);
+    } else {
+      // Discard older manifests
+      for (size_t i = 0; i < manifests_.size(); i++) {
+        ArchiveFile(dbname_ + '/' + manifests_[i]);
+      }
+    }
+    
+      virtual Status DisableFileDeletions() override {
+    return Status::NotSupported('Not supported in compacted db mode.');
+  }
+  virtual Status EnableFileDeletions(bool /*force*/) override {
+    return Status::NotSupported('Not supported in compacted db mode.');
+  }
+  virtual Status GetLiveFiles(std::vector<std::string>&,
+                              uint64_t* /*manifest_file_size*/,
+                              bool /*flush_memtable*/ = true) override {
+    return Status::NotSupported('Not supported in compacted db mode.');
+  }
+  using DBImpl::Flush;
+  virtual Status Flush(const FlushOptions& /*options*/,
+                       ColumnFamilyHandle* /*column_family*/) override {
+    return Status::NotSupported('Not supported in compacted db mode.');
+  }
+  using DB::IngestExternalFile;
+  virtual Status IngestExternalFile(
+      ColumnFamilyHandle* /*column_family*/,
+      const std::vector<std::string>& /*external_files*/,
+      const IngestExternalFileOptions& /*ingestion_options*/) override {
+    return Status::NotSupported('Not supported in compacted db mode.');
+  }
+    
+    struct CompactionIterationStats {
+  // Compaction statistics
+    }
+    
+      // this will produce empty file (delete compaction filter)
+  ASSERT_OK(db_->CompactRange(CompactRangeOptions(), nullptr, nullptr));
+  ASSERT_EQ(0U, CountLiveFiles());
+    
+      auto defaultEnv = Env::Default();
+  int hits = 0;
+  for (auto it = fileNames.begin() ; it != fileNames.end(); ++it) {
+    if ((*it == '..') || (*it == '.')) {
+      continue;
+    }
+    auto filePath = dbname_ + '/' + *it;
+    unique_ptr<SequentialFile> seqFile;
+    auto envOptions = EnvOptions(CurrentOptions());
+    status = defaultEnv->NewSequentialFile(filePath, &seqFile, envOptions);
+    ASSERT_OK(status);
+    }
+    
+      // these three metods are querying the state of the WriteController
+  bool IsStopped() const;
+  bool NeedsDelay() const { return total_delayed_.load() > 0; }
+  bool NeedSpeedupCompaction() const {
+    return IsStopped() || NeedsDelay() || total_compaction_pressure_ > 0;
+  }
+  // return how many microseconds the caller needs to sleep after the call
+  // num_bytes: how many number of bytes to put into the DB.
+  // Prerequisite: DB mutex held.
+  uint64_t GetDelay(Env* env, uint64_t num_bytes);
+  void set_delayed_write_rate(uint64_t write_rate) {
+    // avoid divide 0
+    if (write_rate == 0) {
+      write_rate = 1u;
+    } else if (write_rate > max_delayed_write_rate()) {
+      write_rate = max_delayed_write_rate();
+    }
+    delayed_write_rate_ = write_rate;
+  }
+    
+          // Truncate to available space if necessary
+      if (p >= limit) {
+        if (iter == 0) {
+          continue;       // Try again with larger buffer
+        } else {
+          p = limit - 1;
+        }
+      }
+    
+    namespace aria2 {
+    }
+    
+      std::vector<std::shared_ptr<DHTNode>> nodes;
+  // nodes
+  const int compactlen = bittorrent::getCompactLength(family_);
+  for (size_t i = 0; i < numNodes; ++i) {
+    // 1byte compact peer info length
+    uint8_t peerInfoLen;
+    READ_CHECK(fp, &peerInfoLen, sizeof(peerInfoLen));
+    if (peerInfoLen != compactlen) {
+      // skip this entry
+      readBytes(fp, buf, buf.size(), 7 + 48);
+      continue;
+    }
+    // 7bytes reserved
+    readBytes(fp, buf, buf.size(), 7);
+    // compactlen bytes compact peer info
+    readBytes(fp, buf, buf.size(), compactlen);
+    if (memcmp(zero, buf, compactlen) == 0) {
+      // skip this entry
+      readBytes(fp, buf, buf.size(), 48 - compactlen);
+      continue;
+    }
+    std::pair<std::string, uint16_t> peer =
+        bittorrent::unpackcompact(buf, family_);
+    if (peer.first.empty()) {
+      // skip this entry
+      readBytes(fp, buf, buf.size(), 48 - compactlen);
+      continue;
+    }
+    // 24-compactlen bytes reserved
+    readBytes(fp, buf, buf.size(), 24 - compactlen);
+    // node ID
+    readBytes(fp, buf, buf.size(), DHT_ID_LENGTH);
+    }
+    
+    std::shared_ptr<DHTTask> DHTTaskFactoryImpl::createReplaceNodeTask(
+    const std::shared_ptr<DHTBucket>& bucket,
+    const std::shared_ptr<DHTNode>& newNode)
+{
+  auto task = std::make_shared<DHTReplaceNodeTask>(bucket, newNode);
+  task->setTimeout(timeout_);
+  setCommonProperty(task);
+  return task;
+}
+    
+      virtual void addPeriodicTask2(const std::shared_ptr<DHTTask>& task) = 0;
+    
+    
+    {  virtual void
+  addImmediateTask(const std::shared_ptr<DHTTask>& task) CXX11_OVERRIDE;
 };
     
-    Debugger::Debugger(v8::Isolate* isolate, content::WebContents* web_contents)
-    : web_contents_(web_contents) {
-  Init(isolate);
-}
+      // TODO handle exception thrown by this function.
+  std::string generateToken(const unsigned char* infoHash,
+                            const std::string& ipaddr, uint16_t port) const;
     
-    #include 'atom/browser/api/trackable_object.h'
-#include 'base/callback.h'
-#include 'base/values.h'
-#include 'content/public/browser/devtools_agent_host_client.h'
-#include 'native_mate/handle.h'
+    #include <memory>
     
-    void DownloadItem::SetSavePath(const base::FilePath& path) {
-  save_path_ = path;
-}
-    
-    // static
-void GlobalShortcut::BuildPrototype(v8::Isolate* isolate,
-                                    v8::Local<v8::FunctionTemplate> prototype) {
-  prototype->SetClassName(mate::StringToV8(isolate, 'GlobalShortcut'));
-  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
-      .SetMethod('register', &GlobalShortcut::Register)
-      .SetMethod('isRegistered', &GlobalShortcut::IsRegistered)
-      .SetMethod('unregister', &GlobalShortcut::Unregister)
-      .SetMethod('unregisterAll', &GlobalShortcut::UnregisterAll);
-}
-    
-        Trainer::Trainer(const FunctionPtr& model, const FunctionPtr& lossFunction, const FunctionPtr& evaluationFunction,
-                     const std::vector<LearnerPtr>& parameterLearners,
-                    const std::vector<ProgressWriterPtr>& progressWriters) 
-        : Evaluator(evaluationFunction, progressWriters, false),
-          m_model(model),
-          m_lossFunction(lossFunction),
-          m_parameterLearners(std::make_shared<Learners>(parameterLearners)),
-          m_prevMinibatchNumSamples(0),
-          m_distributed(false),
-          m_aggregatedTrainingLossValue(std::make_shared<Accumulator>()),
-          m_aggregatedTrainingEvalCriterionValue(),
-          m_prevDistributedTotalNumSamples(0)
-    {
-        std::vector<Variable> combinedFunctionArgs;
-        if (m_model) // model is optional, since it may not be adding any information on top of lossFunction
-            combinedFunctionArgs = m_model->Outputs();
-    }
-    
-        ParameterInitializer HeNormalInitializer(double scale, int outputRank, int filterRank, unsigned long seed)
-    {
-        return CreateInitializer(Microsoft::MSR::CNTK::HeNormalInitializerTypeName, scale, outputRank, filterRank, seed);
-    }
-    
-    bool DataReader::SetNetOutput(
-    const std::vector<std::vector<std::pair<wstring, size_t>>>& uttInfo,
-    const MatrixBase& outputs,
-    const MBLayoutPtr pMBLayout)
+    DHTUnknownMessage::DHTUnknownMessage(const std::shared_ptr<DHTNode>& localNode,
+                                     const unsigned char* data, size_t length,
+                                     const std::string& ipaddr, uint16_t port)
+    : DHTMessage(localNode, std::shared_ptr<DHTNode>()),
+      length_(length),
+      ipaddr_(ipaddr),
+      port_(port)
 {
-    bool ans = false;
-    for (size_t i = 0; i < m_ioNames.size(); i++)
-        ans = (m_dataReaders[m_ioNames[i]]->SetNetOutput(uttInfo, outputs, pMBLayout) || ans);
-    return ans;
-}
-    
-    class Timer
-{
-public:
-    Timer()
-        : m_start(0), m_end(0)
-    {
-    }
-    }
-    
-        virtual void /*ComputationNode::*/ ForwardProp(const FrameRange& fr) override
-    {
-        auto sliceInputValue  = InputRef(0).ValueFor(fr);
-        auto sliceOutputValue =           ValueFor(fr); // row vector
-    }
-    
-    template <class charT, class traits>
-bool basic_regex_parser<charT, traits>::parse_repeat(std::size_t low, std::size_t high)
-{
-   bool greedy = true;
-   bool pocessive = false;
-   std::size_t insert_point;
-   // 
-   // when we get to here we may have a non-greedy ? mark still to come:
-   //
-   if((m_position != m_end) 
-      && (
-            (0 == (this->flags() & (regbase::main_option_type | regbase::no_perl_ex)))
-            || ((regbase::basic_syntax_group|regbase::emacs_ex) == (this->flags() & (regbase::main_option_type | regbase::emacs_ex)))
-         )
-      )
-   {
-      // OK we have a perl or emacs regex, check for a '?':
-      if(this->m_traits.syntax_type(*m_position) == regex_constants::syntax_question)
-      {
-         greedy = false;
-         ++m_position;
-      }
-      // for perl regexes only check for pocessive ++ repeats.
-      if((m_position != m_end)
-         && (0 == (this->flags() & regbase::main_option_type)) 
-         && (this->m_traits.syntax_type(*m_position) == regex_constants::syntax_plus))
-      {
-         pocessive = true;
-         ++m_position;
-      }
-   }
-   if(0 == this->m_last_state)
-   {
-      fail(regex_constants::error_badrepeat, ::boost::BOOST_REGEX_DETAIL_NS::distance(m_base, m_position), 'Nothing to repeat.');
-      return false;
-   }
-   if(this->m_last_state->type == syntax_element_endmark)
-   {
-      // insert a repeat before the '(' matching the last ')':
-      insert_point = this->m_paren_start;
-   }
-   else if((this->m_last_state->type == syntax_element_literal) && (static_cast<re_literal*>(this->m_last_state)->length > 1))
-   {
-      // the last state was a literal with more than one character, split it in two:
-      re_literal* lit = static_cast<re_literal*>(this->m_last_state);
-      charT c = (static_cast<charT*>(static_cast<void*>(lit+1)))[lit->length - 1];
-      lit->length -= 1;
-      // now append new state:
-      lit = static_cast<re_literal*>(this->append_state(syntax_element_literal, sizeof(re_literal) + sizeof(charT)));
-      lit->length = 1;
-      (static_cast<charT*>(static_cast<void*>(lit+1)))[0] = c;
-      insert_point = this->getoffset(this->m_last_state);
-   }
-   else
-   {
-      // repeat the last state whatever it was, need to add some error checking here:
-      switch(this->m_last_state->type)
-      {
-      case syntax_element_start_line:
-      case syntax_element_end_line:
-      case syntax_element_word_boundary:
-      case syntax_element_within_word:
-      case syntax_element_word_start:
-      case syntax_element_word_end:
-      case syntax_element_buffer_start:
-      case syntax_element_buffer_end:
-      case syntax_element_alt:
-      case syntax_element_soft_buffer_end:
-      case syntax_element_restart_continue:
-      case syntax_element_jump:
-      case syntax_element_startmark:
-      case syntax_element_backstep:
-         // can't legally repeat any of the above:
-         fail(regex_constants::error_badrepeat, m_position - m_base);
-         return false;
-      default:
-         // do nothing...
-         break;
-      }
-      insert_point = this->getoffset(this->m_last_state);
-   }
-   //
-   // OK we now know what to repeat, so insert the repeat around it:
-   //
-   re_repeat* rep = static_cast<re_repeat*>(this->insert_state(insert_point, syntax_element_rep, re_repeater_size));
-   rep->min = low;
-   rep->max = high;
-   rep->greedy = greedy;
-   rep->leading = false;
-   // store our repeater position for later:
-   std::ptrdiff_t rep_off = this->getoffset(rep);
-   // and append a back jump to the repeat:
-   re_jump* jmp = static_cast<re_jump*>(this->append_state(syntax_element_jump, sizeof(re_jump)));
-   jmp->alt.i = rep_off - this->getoffset(jmp);
-   this->m_pdata->m_data.align();
-   // now fill in the alt jump for the repeat:
-   rep = static_cast<re_repeat*>(this->getaddress(rep_off));
-   rep->alt.i = this->m_pdata->m_data.size() - rep_off;
-   //
-   // If the repeat is pocessive then bracket the repeat with a (?>...)
-   // independent sub-expression construct:
-   //
-   if(pocessive)
-   {
-      if(m_position != m_end)
-      {
-         //
-         // Check for illegal following quantifier, we have to do this here, because
-         // the extra states we insert below circumvents our usual error checking :-(
-         //
-         switch(this->m_traits.syntax_type(*m_position))
-         {
-         case regex_constants::syntax_star:
-         case regex_constants::syntax_plus:
-         case regex_constants::syntax_question:
-         case regex_constants::syntax_open_brace:
-            fail(regex_constants::error_badrepeat, m_position - m_base);
-            return false;
-         }
-      }
-      re_brace* pb = static_cast<re_brace*>(this->insert_state(insert_point, syntax_element_startmark, sizeof(re_brace)));
-      pb->index = -3;
-      pb->icase = this->flags() & regbase::icase;
-      jmp = static_cast<re_jump*>(this->insert_state(insert_point + sizeof(re_brace), syntax_element_jump, sizeof(re_jump)));
-      this->m_pdata->m_data.align();
-      jmp->alt.i = this->m_pdata->m_data.size() - this->getoffset(jmp);
-      pb = static_cast<re_brace*>(this->append_state(syntax_element_endmark, sizeof(re_brace)));
-      pb->index = -3;
-      pb->icase = this->flags() & regbase::icase;
-   }
-   return true;
-}
-    
-       file_iterator();
-   file_iterator(const char* wild);
-   ~file_iterator();
-   file_iterator(const file_iterator&);
-   file_iterator& operator=(const file_iterator&);
-   const char* root()const { return _root; }
-   const char* path()const { return _path; }
-   const char* name()const { return ptr; }
-   _fi_find_data* data() { return &(ref->_data); }
-   void next();
-   file_iterator& operator++() { next(); return *this; }
-   file_iterator operator++(int);
-   const char* operator*() { return path(); }
-    
-    
-template <class traits, class charT>
-unsigned find_sort_syntax(const traits* pt, charT* delim)
-{
-   //
-   // compare 'a' with 'A' to see how similar they are,
-   // should really use a-accute but we can't portably do that,
-   //
-   typedef typename traits::string_type string_type;
-   typedef typename traits::char_type char_type;
-    }
-    
-     /*
-  *   LOCATION:    see http://www.boost.org for most recent version.
-  *   FILE         basic_regex_creator.cpp
-  *   VERSION      see <boost/version.hpp>
-  *   DESCRIPTION: Declares template class basic_regex_creator which fills in
-  *                the data members of a regex_data object.
-  */
-    
-    
-    
-       match_results<BidirectionalIterator> what;  // current match
-   BidirectionalIterator                base;  // start of sequence
-   BidirectionalIterator                end;   // end of sequence
-   const regex_type                     re;   // the expression
-   match_flag_type                      flags; // flags for matching
-    
-    
-namespace boost{
-    }
-    
-    bool DHTResponseMessage::isReply() const { return true; }
-    
-    void DHTRoutingTable::dropNode(const std::shared_ptr<DHTNode>& node)
-{
-  getBucketFor(node)->dropNode(node);
-}
-/*
-  void DHTRoutingTable::moveBucketHead(const std::shared_ptr<DHTNode>& node)
-  {
-  getBucketFor(node)->moveToHead(node);
+  if (length_ == 0) {
+    data_ = nullptr;
   }
-*/
-void DHTRoutingTable::moveBucketTail(const std::shared_ptr<DHTNode>& node)
-{
-  getBucketFor(node)->moveToTail(node);
+  else {
+    data_ = new unsigned char[length];
+    memcpy(data_, data, length);
+  }
 }
     
-      // localnode
-  // 8bytes reserved
-  readBytes(fp, buf, buf.size(), 8);
-  // localnode ID
-  readBytes(fp, buf, buf.size(), DHT_ID_LENGTH);
-  auto localNode = std::make_shared<DHTNode>(buf);
-  // 4bytes reserved
-  readBytes(fp, buf, buf.size(), 4);
-    
-    class DHTRoutingTableDeserializer {
-private:
-  int family_;
-    }
-    
-    namespace aria2 {
-    }
-    
-    class DHTTask;
-    
-      DHTTaskExecutor immediateTaskQueue_;
-    
-    namespace aria2 {
-    }
-    
-    #endif // D_DNS_CACHE_H
+    void DNSCache::markBad(const std::string& hostname, const std::string& ipaddr,
+                       uint16_t port)
+{
+  auto target = std::make_shared<CacheEntry>(hostname, port);
+  auto i = entries_.find(target);
+  if (i != entries_.end()) {
+    (*i)->markBad(ipaddr);
+  }
+}
