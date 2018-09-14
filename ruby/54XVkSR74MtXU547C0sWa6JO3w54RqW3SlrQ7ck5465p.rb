78fa02fddc423dd08d8caa3af600c4e39b17ebd0
SUNsung <weatherius@gmail.com>
2018-09-14 21:08:57 +0300

    Rewrite [54XVkSR74MtXU547C0sWa6JO3w54RqW3SlrQ7ck5465p.rb] File in: 2018-09-14 21:08:56

--- ruby/54XVkSR74MtXU547C0sWa6JO3w54RqW3SlrQ7ck5465p.rb
+++ ruby/54XVkSR74MtXU547C0sWa6JO3w54RqW3SlrQ7ck5465p.rb
@@ -1,68 +1,162 @@
 
         
-            find_union(segments, Project).includes(:namespace).order_id_desc
-  end
+                      if block_given?
+                @template_object.capture(builder, &block)
+              else
+                render_component(builder)
+              end
+            end
     
-    #
-# Railties
-#
+              add_default_name_and_id_for_value(tag_value, name_and_id)
+          options.delete('index')
+          options.delete('namespace')
+          options['for'] = name_and_id['id'] unless options.key?('for')
     
-    msfenv_real_pathname = Pathname.new(__FILE__).realpath
-root = msfenv_real_pathname.parent.parent
-    
-      def parse(pkt)
-    # We want to return immediatly if we do not have a packet which is handled by us
-    return unless pkt.is_tcp?
-    return if (pkt.tcp_sport != 21 and pkt.tcp_dport != 21)
-    s = find_session((pkt.tcp_sport == 21) ? get_session_src(pkt) : get_session_dst(pkt))
-    s[:sname] ||= 'ftp'
-    
-    require 'openssl'
-require 'rex'
-    
-    classNames = [ 'HelloWorld1', 'HelloWorld2' ]
-    
-        def javascripts_path
-      File.join assets_path, 'javascripts'
+          _normalize_layout(value)
     end
     
-      def process_bootstrap
-    log_status 'Convert Bootstrap LESS to Sass'
-    puts ' repo   : #@repo_url'
-    puts ' branch : #@branch_sha #@repo_url/tree/#@branch'
-    puts ' save to: #{@save_to.to_json}'
-    puts ' twbs cache: #{@cache_path}'
-    puts '-' * 60
-    
-      # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-    
-          # Override `Kernel#puts` to prepend four spaces to each line.
-      def puts(string=nil)
-        $stdout.puts(string.to_s.gsub(/^/, '    '))
+        def cache_message(payload) # :doc:
+      case payload[:cache_hit]
+      when :hit
+        '[cache hit]'
+      when :miss
+        '[cache miss]'
       end
-    
-          def stage_definitions
-        stage_config_path.join('*.rb')
-      end
-    
-      def command_line(*options)
-    options.each { |opt| ARGV << opt }
-    subject.define_singleton_method(:exit) do |*_args|
-      throw(:system_exit, :exit)
     end
-    subject.run
-    subject.options
-  end
     
-      SPREE_GEMS.each do |gem_name|
-    rm_f  '#{gem_name}/Gemfile.lock'
-    rm_rf '#{gem_name}/pkg'
-    rm_rf '#{gem_name}/spec/dummy'
-  end
+    group :app do
+  gem 'rack'
+  gem 'sinatra'
+  gem 'sinatra-contrib'
+  gem 'thin'
+  gem 'sprockets'
+  gem 'sprockets-helpers'
+  gem 'erubi'
+  gem 'browser'
+  gem 'sass'
+  gem 'coffee-script'
 end
     
-            def stock_location
-          render 'spree/api/v1/shared/stock_location_required', status: 422 and return unless params[:stock_location_id]
-          @stock_location ||= StockLocation.accessible_by(current_ability, :read).find(params[:stock_location_id])
+        use BetterErrors::Middleware
+    BetterErrors.application_root = File.expand_path('..', __FILE__)
+    BetterErrors.editor = :sublime
+    
+        def to_a
+      @filters.dup
+    end
+    
+        def base_url
+      @base_url ||= URL.parse self.class.base_url
+    end
+    
+        def file_path_for(url)
+      File.join self.class.dir, url.remove(base_url.to_s)
+    end
+    
+            def with_redirections
+          @redirections = new.fetch_redirections
+          yield
+        ensure
+          @redirections = nil
         end
+      end
+    
+        def format_url(url)
+      url.to_s.remove %r{\Ahttps?://}
+    end
+    
+            css('.filetree').each do |node|
+          node.content = node.css('.file').map(&:inner_html).join('\n')
+          node.name = 'pre'
+          node.remove_attribute('class')
+        end
+    
+        FUNCTION_RE = /^@function\s*(#{Sass::SCSS::RX::IDENT})(.*)$/
+    def parse_function_directive(parent, line, root, value, offset)
+      name, arg_string = line.text.scan(FUNCTION_RE).first
+      raise SyntaxError.new('Invalid function definition \'#{line.text}\'.') if name.nil?
+    
+        # The content passed to this environment. This is naturally only set
+    # for mixin body environments with content passed in.
+    #
+    # @return {[Array<Sass::Tree::Node>, Environment]?} The content nodes and
+    #   the lexical environment of the content block.
+    def content
+      @content || (@parent && @parent.content)
+    end
+    
+        # Parses the command-line arguments and runs the executable.
+    # This does not handle exceptions or exit the program.
+    #
+    # @see #parse!
+    def parse
+      @opts = OptionParser.new(&method(:set_opts))
+      @opts.parse!(@args)
+    
+          opts.on('-s', '--stdin', :NONE,
+              'Read input from standard input instead of an input file.',
+              'This is the default if no input file is specified. Requires --from.') do
+        @options[:input] = $stdin
+      end
+    
+        def common_options(opts)
+      opts.separator ''
+      opts.separator 'Common Options:'
+    
+    platforms :jruby do
+  gem 'json'
+end
+
+    
+        if run? && ARGV.any?
+      require 'optparse'
+      OptionParser.new { |op|
+        op.on('-p port',   'set the port (default is 4567)')                { |val| set :port, Integer(val) }
+        op.on('-o addr',   'set the host (default is #{bind})')             { |val| set :bind, val }
+        op.on('-e env',    'set the environment (default is development)')  { |val| set :environment, val.to_sym }
+        op.on('-s server', 'specify rack server/handler (default is thin)') { |val| set :server, val }
+        op.on('-q',        'turn on quiet mode (default is off)')           {       set :quiet, true }
+        op.on('-x',        'turn on the mutex lock (default is off)')       {       set :lock, true }
+      }.parse!(ARGV.dup)
+    end
+  end
+    
+        <div id='backtrace' class='condensed'>
+      <h3>BACKTRACE</h3>
+      <p><a href='#' id='expando'
+            onclick='toggleBacktrace(); return false'>(expand)</a></p>
+      <p id='nav'><strong>JUMP TO:</strong>
+         <% unless bad_request?(exception) %>
+            <a href='#get-info'>GET</a>
+            <a href='#post-info'>POST</a>
+         <% end %>
+         <a href='#cookie-info'>COOKIES</a>
+         <a href='#env-info'>ENV</a>
+      </p>
+      <div class='clear'></div>
+    
+          NO_ARG_DIRECTIVES = %i(block_all_mixed_content disown_opener
+                             upgrade_insecure_requests).freeze
+    
+          def escape_string(str)
+        str = @escaper.escape_url(str)        if @url
+        str = @escaper.escape_html(str)       if @html
+        str = @escaper.escape_javascript(str) if @javascript
+        str
+      end
+    end
+  end
+end
+
+    
+    require './plugins/pygments_code'
+require './plugins/raw'
+require 'pathname'
+    
+      # Replaces relative urls with full urls
+  def expand_urls(input, url='')
+    url ||= '/'
+    input.gsub /(\s+(href|src|poster)\s*=\s*['|']{1})(\/[^\/>]{1}[^\''>]*)/ do
+      $1+url+$3
+    end
+  end
