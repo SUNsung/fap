4660e5d01b31996c1a2b34f1a64c823d55322166
SUNsung <weatherius@gmail.com>
2018-09-14 05:10:55 +0300

    Rewrite [59B7HLznbiIrQ59sNWrlHHECCs5990Cbv8BtQGc59NPk.rb] File in: 2018-09-14 05:10:54

--- ruby/59B7HLznbiIrQ59sNWrlHHECCs5990Cbv8BtQGc59NPk.rb
+++ ruby/59B7HLznbiIrQ59sNWrlHHECCs5990Cbv8BtQGc59NPk.rb
@@ -1,60 +1,151 @@
 
         
-              # Overwrite this factory method for Redis connections if you want to use a different Redis library than the redis gem.
-      # This is needed, for example, when using Makara proxies for distributed Redis.
-      cattr_accessor :redis_connector, default: ->(config) do
-        ::Redis.new(config.slice(:url, :host, :port, :db, :password))
+              def initialize(request)
+        @request = request
       end
     
-      after_save :after_save_for_transaction
-  after_create :after_create_for_transaction
+          def route_authentication_setting
+        return {} unless respond_to?(:route_setting)
     
-    class ActionCable::Channel::BroadcastingTest < ActionCable::TestCase
-  class ChatChannel < ActionCable::Channel::Base
-  end
+            def value_text
+          STATUS_RENAME[@status.to_s] || @status.to_s
+        end
     
-            assert_nil connection.last_transmission
-    
-      def test_disappearing_client
-    with_puma_server do |port|
-      c = websocket_client(port)
-      assert_equal({ 'type' => 'welcome' }, c.read_message)  # pop the first welcome message off the stack
-      c.send_message command: 'subscribe', identifier: JSON.generate(channel: 'ClientTest::EchoChannel')
-      assert_equal({ 'identifier' => '{\'channel\':\'ClientTest::EchoChannel\'}', 'type' => 'confirm_subscription' }, c.read_message)
-      c.send_message command: 'message', identifier: JSON.generate(channel: 'ClientTest::EchoChannel'), data: JSON.generate(action: 'delay', message: 'hello')
-      c.close # disappear before write
-    
-          Connection.new(@server, env).tap do |connection|
-        connection.process
-        connection.send :handle_open
-        assert connection.connected
-      end
+      #
+  # More advanced [] that does downcase comparison.
+  #
+  def [](key)
+    begin
+      rv = self.fetch(key)
+    rescue IndexError
+      rv = nil
     end
-end
-
-    
-        REDIRECT_RGX = /http-equiv='refresh'/i
-    NOT_FOUND_RGX = /<title>Not Found<\/title>/
-    
-        def add_entry(entry)
-      if @index.add?(entry.as_json.to_s)
-        @entries << entry.dup
-        @types[entry.type].count += 1 if entry.type
+    if (rv == nil)
+      begin
+        rv = self.dcase_hash[key.downcase]
+      rescue IndexError
+        rv = nil
       end
     end
     
-        File.write(dest, Oj.dump(map))
-    puts 'Wrote emojo to destination! (#{dest})'
+      #
+  # A hash that associated a file extension with a mime type for use as the
+  # content type of responses.
+  #
+  ExtensionMimeTypes =
+    {
+      'rhtml' => 'text/html',
+      'html'  => 'text/html',
+      'htm'   => 'text/htm',
+      'jpg'   => 'image/jpeg',
+      'jpeg'  => 'image/jpeg',
+      'gif'   => 'image/gif',
+      'png'   => 'image/png',
+      'bmp'   => 'image/bmp',
+      'txt'   => 'text/plain',
+      'css'   => 'text/css',
+      'ico'   => 'image/x-icon',
+    }
+    
+    require 'rex/proto/ipmi/utils'
+    
+    
+  # open rmcpplus_request with cipherzero
+  def self.create_ipmi_session_open_cipher_zero_request(console_session_id)
+    head = [
+      0x06, 0x00, 0xff, 0x07,   # RMCP Header
+      0x06,                     # RMCP+ Authentication Type
+      PAYLOAD_RMCPPLUSOPEN_REQ, # Payload Type
+      0x00, 0x00, 0x00, 0x00,   # Session ID
+      0x00, 0x00, 0x00, 0x00    # Sequence Number
+    ].pack('C*')
+    
+              # Encodes the renew_time field
+          #
+          # @return [String]
+          def encode_renew_time
+            [renew_till].pack('N')
+          end
+    
+              # Encodes the type field
+          #
+          # @return [OpenSSL::ASN1::Integer]
+          def encode_type
+            bn = OpenSSL::BN.new(type.to_s)
+            int = OpenSSL::ASN1::Integer.new(bn)
+    
+              # Decodes the srealm field
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [String]
+          def decode_srealm(input)
+            input.value[0].value
+          end
+    
+              # Decodes a Rex::Proto::Kerberos::Model::EncryptionKey from an
+          # OpenSSL::ASN1::Sequence
+          #
+          # @param input [OpenSSL::ASN1::Sequence] the input to decode from
+          def decode_asn1(input)
+            seq_values = input.value
+            self.type = decode_type(seq_values[0])
+            self.value = decode_value(seq_values[1])
+          end
+    
+    require 'bundler/cli'
+require 'bundler/friendly_errors'
+    
+        # @return [String] major.minor ruby version, ex 1.9
+    def ruby_abi_version
+      RUBY_VERSION[/(\d+\.\d+)(\.\d+)*/, 1]
+    end
+    
+            if specs.size > 0
+          specs
+        else
+          raise LogStash::PluginManager::PluginNotFoundError, 'Cannot find plugins matching: `#{plugin_pattern}`'
+        end
+      end.flatten
+    end
+    
+        context 'without a username / password' do
+      let(:scheme) { 'myscheme' }
+      let(:user) { nil }
+      let(:password) { nil }
+      let(:hostname) { 'myhostname' }
+      let(:path) { '/my/path' }
+      let(:uri_str) { '#{scheme}://#{hostname}#{path}' }
+      let(:uri_hidden) { '#{scheme}://#{hostname}#{path}' }
+    
+          it 'list the plugin with his version' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list --verbose #{plugin_name}')
+        expect(result).to run_successfully_and_output(/^#{plugin_name} \(\d+\.\d+.\d+\)/)
+      end
+    end
   end
 end
 
     
-    desc 'Dumps output to a CSS file for testing'
-task :debug do
-  require 'sass'
-  path = Bootstrap.stylesheets_path
-  %w(bootstrap).each do |file|
-    engine = Sass::Engine.for_file('#{path}/#{file}.scss', syntax: :scss, load_paths: [path])
-    File.open('./#{file}.css', 'w') { |f| f.write(engine.render) }
-  end
-end
+      let(:cop_config) { { 'EnforcedStyle' => 'symmetrical' } }
+    
+        context 'opening brace on same line as first element' do
+      it 'allows closing brace on different line from last element' do
+        expect_no_offenses(construct(false, true))
+      end
+    
+          # Returns the iteration variable of the `for` loop.
+      #
+      # @return [Node] The iteration variable of the `for` loop
+      def variable
+        node_parts[0]
+      end
+    
+          # Checks whether any argument of the node is a splat
+      # argument, i.e. `*splat`.
+      #
+      # @return [Boolean] whether the node is a splat argument
+      def splat_argument?
+        arguments? &&
+          (arguments.any?(&:splat_type?) || arguments.any?(&:restarg_type?))
+      end
+      alias rest_argument? splat_argument?
