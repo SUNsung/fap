bfede878d6c07cf5183249a2b3582aec0623f1a0
SUNsung <weatherius@gmail.com>
2018-09-15 19:11:04 +0300

    Rewrite [55m.9i.FurH4o55bSeC03sbEvU55.ADtsVjUXIM55Ofr.py] File in: 2018-09-15 19:11:04

--- pyton/55m.9i.FurH4o55bSeC03sbEvU55.ADtsVjUXIM55Ofr.py
+++ pyton/55m.9i.FurH4o55bSeC03sbEvU55.ADtsVjUXIM55Ofr.py
@@ -1,72 +1,165 @@
 
         
-        intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'werkzeug': ('http://werkzeug.pocoo.org/docs/', None),
-    'click': ('http://click.pocoo.org/', None),
-    'jinja': ('http://jinja.pocoo.org/docs/', None),
-    'itsdangerous': ('https://pythonhosted.org/itsdangerous', None),
-    'sqlalchemy': ('https://docs.sqlalchemy.org/en/latest/', None),
-    'wtforms': ('https://wtforms.readthedocs.io/en/latest/', None),
-    'blinker': ('https://pythonhosted.org/blinker/', None),
-}
+            def __init__(self, level, row, spot_number, spot_size, vehicle_size):
+        self.level = level
+        self.row = row
+        self.spot_number = spot_number
+        self.spot_size = spot_size
+        self.vehicle_size = vehicle_size
+        self.vehicle = None
     
-    bp = Blueprint('auth', __name__, url_prefix='/auth')
-    
-    import click
-from flask import current_app, g
-from flask.cli import with_appcontext
-    
-        # test that the user was inserted into the database
-    with app.app_context():
-        assert get_db().execute(
-            'select * from user where username = 'a'',
-        ).fetchone() is not None
-    
-        with app.app_context():
-        db = get_db()
-        post = db.execute('SELECT * FROM post WHERE id = 1').fetchone()
-        assert post['title'] == 'updated'
-    
-            :param obj: an import name or object
+            (2016-01, shopping), 25
+        (2016-01, shopping), 100
+        (2016-01, gas), 50
         '''
-        if isinstance(obj, string_types):
-            obj = import_string(obj)
-        for key in dir(obj):
-            if key.isupper():
-                self[key] = getattr(obj, key)
+        timestamp, category, amount = line.split('\t')
+        period = self. extract_year_month(timestamp)
+        if period == self.current_year_month():
+            yield (period, category), amount
     
-        for idx, (loader, srcobj, triple) in enumerate(attempts):
-        if isinstance(srcobj, Flask):
-            src_info = 'application '%s'' % srcobj.import_name
-        elif isinstance(srcobj, Blueprint):
-            src_info = 'blueprint '%s' (%s)' % (srcobj.name,
-                                                srcobj.import_name)
-        else:
-            src_info = repr(srcobj)
+        def categorize(self, transaction):
+        if transaction.seller in self.seller_category_map:
+            return self.seller_category_map[transaction.seller]
+        if transaction.seller in self.seller_category_overrides_map:
+            seller_category_map[transaction.seller] = \
+                self.manual_overrides[transaction.seller].peek_min()
+            return self.seller_category_map[transaction.seller]
+        return None
     
-    
-class CreateExtension(Operation):
-    reversible = True
-    
-        def exists(self, session_key):
-        return session_key and (self.cache_key_prefix + session_key) in self._cache or super().exists(session_key)
-    
-        def exists(self, session_key=None):
-        '''
-        This method makes sense when you're talking to a shared resource, but
-        it doesn't matter when you're storing the information in the client's
-        cookie.
-        '''
-        return False
+    from ansible.module_utils.aws.core import AnsibleAWSModule
+from ansible.module_utils.ec2 import boto3_conn, get_aws_connection_info, ec2_argument_spec
+from ansible.module_utils.ec2 import camel_dict_to_snake_dict, AWSRetry, compare_policies
+from ansible.module_utils.aws.waf import run_func_with_change_token_backoff, MATCH_LOOKUP
+from ansible.module_utils.aws.waf import get_rule_with_backoff, list_rules_with_backoff
     
     
-@x_robots_tag
-def sitemap(request, sitemaps, section=None,
-            template_name='sitemap.xml', content_type='application/xml'):
+class CloudFrontOriginAccessIdentityValidationManager(object):
+    '''
+    Manages Cloudfront Origin Access Identities
+    '''
     
-        parser = argparse.ArgumentParser(description = 'Download all the PDF/HTML links into README.md')
-    parser.add_argument('-d', action='store', dest='directory')
-    parser.add_argument('--no-html', action='store_true', dest='nohtml', default = False)
-    parser.add_argument('--overwrite', action='store_true', default = False)    
-    results = parser.parse_args()
+    
+if __name__ == '__main__':
+    main()
+
+    
+    
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['stableinterface'],
+                    'supported_by': 'certified'}
+    
+    from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.ec2 import ansible_dict_to_boto3_filter_list, boto3_conn
+from ansible.module_utils.ec2 import boto3_tag_list_to_ansible_dict, camel_dict_to_snake_dict
+from ansible.module_utils.ec2 import ec2_argument_spec, get_aws_connection_info
+    
+    
+def main():
+    argument_spec = ec2_argument_spec()
+    argument_spec.update(
+        dict(
+            name=dict(required=False, default=[], type='list'),
+            sort=dict(required=False, default=None,
+                      choices=['launch_configuration_name', 'image_id', 'created_time', 'instance_type', 'kernel_id', 'ramdisk_id', 'key_name']),
+            sort_order=dict(required=False, default='ascending',
+                            choices=['ascending', 'descending']),
+            sort_start=dict(required=False),
+            sort_end=dict(required=False),
+        )
+    )
+    
+                results.append(data)
+    
+    from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.ec2 import (AnsibleAWSError, HAS_BOTO, connect_to_aws, ec2_argument_spec,
+                                      get_aws_connection_info)
+    
+        def test_encoding(self):
+        # Test non-default encoding.
+        text = self.TEXT.decode('ascii')
+        text_native_eol = text.replace('\n', os.linesep)
+        with self.open(self.filename, 'wt', encoding='utf-16-le') as f:
+            f.write(text)
+        with open(self.filename, 'rb') as f:
+            file_data = ext_decompress(f.read()).decode('utf-16-le')
+            self.assertEqual(file_data, text_native_eol)
+        with self.open(self.filename, 'rt', encoding='utf-16-le') as f:
+            self.assertEqual(f.read(), text)
+    
+    # If we want to print a preview of the message content, we can extract whatever
+# the least formatted payload is and print the first three lines.  Of course,
+# if the message has no plain text part printing the first three lines of html
+# is probably useless, but this is just a conceptual example.
+simplest = msg.get_body(preferencelist=('plain', 'html'))
+print()
+print(''.join(simplest.get_content().splitlines(keepends=True)[:3]))
+    
+            print('Testing IMapIterator.next() with timeout:', end=' ')
+        it = pool.imap(calculatestar, TASKS)
+        while 1:
+            sys.stdout.flush()
+            try:
+                sys.stdout.write('\n\t%s' % it.next(0.02))
+            except StopIteration:
+                break
+            except multiprocessing.TimeoutError:
+                sys.stdout.write('.')
+        print()
+        print()
+    
+    
+def main():
+    parse_command_line()
+    app = tornado.web.Application(
+        [
+            (r'/', MainHandler),
+            (r'/a/message/new', MessageNewHandler),
+            (r'/a/message/updates', MessageUpdatesHandler),
+        ],
+        cookie_secret='__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__',
+        template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+        static_path=os.path.join(os.path.dirname(__file__), 'static'),
+        xsrf_cookies=True,
+        debug=options.debug,
+    )
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+    
+    define('port', default=8888, help='port to listen on')
+define('config_file', default='secrets.cfg',
+       help='filename for additional configuration')
+    
+    import logging
+import tornado.escape
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.websocket
+import os.path
+import uuid
+    
+            Parameter:
+            - segment : The segments that the word is expected to be cut into,
+                        If the word should be treated as a whole, use a str.
+            - tune : If True, tune the word frequency.
+    
+    seg_list = jieba.cut('我来到北京清华大学', cut_all=True)
+print('Full Mode: ' + '/ '.join(seg_list))  # 全模式
+    
+    USAGE = 'usage:    python extract_tags_stop_words.py [file name] -k [top k]'
+    
+        def testCutForSearch_NOHMM(self):
+        for content in test_contents:
+            result = jieba.cut_for_search(content,HMM=False)
+            assert isinstance(result, types.GeneratorType), 'Test CutForSearch Generator error'
+            result = list(result)
+            assert isinstance(result, list), 'Test CutForSearch error on content: %s' % content
+            print(' , '.join(result), file=sys.stderr)
+        print('testCutForSearch_NOHMM', file=sys.stderr)
+    
+    default_encoding='utf-8'
+    
+    
+if len(args) <1:
+    print(USAGE)
+    sys.exit(1)
