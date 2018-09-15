fe390b5bcc6f18632214ed0dd38eec9b2577d942
SUNsung <weatherius@gmail.com>
2018-09-15 03:46:29 +0300

    Rewrite [00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py] File in: 2018-09-15 03:46:29

--- pyton/00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py
+++ pyton/00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py
@@ -1,60 +1,103 @@
 
         
         
-def setup(app):
-    app.add_crossref_type(
-        directivename = 'setting',
-        rolename      = 'setting',
-        indextemplate = 'pair: %s; setting',
-    )
-    app.add_crossref_type(
-        directivename = 'signal',
-        rolename      = 'signal',
-        indextemplate = 'pair: %s; signal',
-    )
-    app.add_crossref_type(
-        directivename = 'command',
-        rolename      = 'command',
-        indextemplate = 'pair: %s; command',
-    )
-    app.add_crossref_type(
-        directivename = 'reqmeta',
-        rolename      = 'reqmeta',
-        indextemplate = 'pair: %s; reqmeta',
-    )
-    app.add_role('source', source_role)
-    app.add_role('commit', commit_role)
-    app.add_role('issue', issue_role)
-    app.add_role('rev', rev_role)
+def check_alphabetical(lines):
+    '''
+    checks if all entries per section are in alphabetical order based in entry title
+    '''
+    sections = {}
+    section_line_num = {}
+    for line_num, line in enumerate(lines):
+        if line.startswith(anchor):
+            category = line.split(anchor)[1].strip()
+            sections[category] = []
+            section_line_num[category] = line_num
+            continue
+        if not line.startswith('|') or line.startswith('|---'):
+            continue
+        raw_title = [x.strip() for x in line.split('|')[1:-1]][0]
+        title_re_match = link_re.match(raw_title)
+        if title_re_match:
+            sections[category].append(title_re_match.group(1).upper())
     
-    def _get_commands_from_entry_points(inproject, group='scrapy.commands'):
-    cmds = {}
-    for entry_point in pkg_resources.iter_entry_points(group):
-        obj = entry_point.load()
-        if inspect.isclass(obj):
-            cmds[entry_point.name] = obj()
-        else:
-            raise Exception('Invalid entry point %s' % entry_point.name)
-    return cmds
+            final_headers[name] = value
+    return final_headers
     
-        def syntax(self):
-        return '[-v]'
+        def __repr__(self):
+        return '<{0} {1}>'.format(type(self).__name__, str(self))
+
     
-        return result
     
-    # Load a sample picture and learn how to recognize it.
-print('Loading known face image(s)')
-obama_image = face_recognition.load_image_file('obama_small.jpg')
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
+FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
+JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
+BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
     
-        def test_face_landmarks_small_model(self):
-        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
-        face_landmarks = api.face_landmarks(img, model='small')
     
-    requirements = [
-    'face_recognition_models>=0.3.0',
-    'Click>=6.0',
-    'dlib>=19.7',
-    'numpy',
-    'Pillow'
-]
+@mock.patch('httpie.input.AuthCredentials._getpass',
+            new=lambda self, prompt: 'UNEXPECTED_PROMPT_RESPONSE')
+def test_auth_plugin_prompt_password_false(httpbin):
+    
+        def test_binary_file_path(self, httpbin):
+        env = MockEnvironment(stdin_isatty=True, stdout_isatty=False)
+        r = http('--print=B', 'POST', httpbin.url + '/post',
+                 '@' + BIN_FILE_PATH_ARG, env=env, )
+        assert r == BIN_FILE_CONTENT
+    
+        def test_POST_explicit_JSON_auto_JSON_accept(self, httpbin):
+        r = http('--json', 'POST', httpbin.url + '/post')
+        assert HTTP_OK in r
+        assert r.json['headers']['Accept'] == JSON_ACCEPT
+        # Make sure Content-Type gets set even with no data.
+        # https://github.com/jakubroztocil/httpie/issues/137
+        assert 'application/json' in r.json['headers']['Content-Type']
+    
+        def test_get_lexer_not_found(self):
+        assert get_lexer('xxx/yyy') is None
+    
+        def init_per_thread_state(self):
+        # Ensure state is initialized just once per-thread
+        if not hasattr(self._thread_local, 'init'):
+            self._thread_local.init = True
+            self._thread_local.last_nonce = ''
+            self._thread_local.nonce_count = 0
+            self._thread_local.chal = {}
+            self._thread_local.pos = None
+            self._thread_local.num_401_calls = None
+    
+    
+def default_hooks():
+    return dict((event, []) for event in HOOKS)
+    
+    
+@pytest.mark.skipif(sys.version_info < (2,7), reason='Only run on Python 2.7+')
+def test_system_ssl():
+    '''Verify we're actually setting system_ssl when it should be available.'''
+    assert info()['system_ssl']['version'] != ''
+    
+        @pytest.fixture(autouse=True)
+    def setup(self):
+        '''LookupDict instance with 'bad_gateway' attribute.'''
+        self.lookup_dict = LookupDict('test')
+        self.lookup_dict.bad_gateway = 502
+    
+                sock.close()
+    
+        def run(self):
+        try:
+            self.server_sock = self._create_socket_and_bind()
+            # in case self.port = 0
+            self.port = self.server_sock.getsockname()[1]
+            self.ready_event.set()
+            self._handle_requests()
+    
+    
+pteredor_is_running = False
+    
+            p = random.choice(self.plus)
+    
+    Here is a little overview over the most commonly used classes provided by
+this runtime:
+    
+            You don't have to implement but it's nice to know where a Token
+        comes from if you have include files etc... on the input.'''
