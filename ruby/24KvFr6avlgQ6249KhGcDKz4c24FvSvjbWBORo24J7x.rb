f0dc786d776fdac1273f8d82e5eb89f53bb05c50
SUNsung <weatherius@gmail.com>
2018-09-15 16:55:27 +0300

    Rewrite [24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb] File in: 2018-09-15 16:55:26

--- ruby/24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb
+++ ruby/24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb
@@ -1,49 +1,60 @@
 
         
-        module Jekyll
-  binding.pry
-end
-
+        WITH_LIQUID = <<-LIQUID.freeze
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor libero at
+pharetra tempus. {{ author }} et metus fermentum, eu cursus lorem
+mattis. Curabitur vel dui et lacus rutrum suscipit et eget neque.
     
-    unless File.exist?(PROF_OUTPUT_FILE)
-  StackProf.run(
-    mode: MODE.to_sym,
-    interval: 100,
-    raw: true,
-    out: PROF_OUTPUT_FILE
-  ) do
-    puts 'GC Stats:', JSON.pretty_generate(GC.stat)
-    GC.disable
+                c.option 'force', '--force', 'Force creation even if PATH already exists'
+            c.option 'blank', '--blank', 'Creates scaffolding but with empty files'
+            c.option 'skip-bundle', '--skip-bundle', 'Skip 'bundle install''
     
-    # Create an array of vendored mimetype => [extensions]
-mimes = {}
-json = open('https://raw.githubusercontent.com/jshttp/mime-db/master/db.json').read
-data = JSON.parse(json)
-data.reject! { |mime, meta| meta['extensions'].nil? || meta['extensions'].empty? }
-data.each do |mime, meta|
-  # Normalize extensions and mime-types
-  mime = mime.downcase.strip
-  extensions = meta['extensions'].map { |e| e.downcase.strip }.compact
+    module Jekyll
+  module Commands
+    class NewTheme < Jekyll::Command
+      class << self
+        def init_with_program(prog)
+          prog.command(:'new-theme') do |c|
+            c.syntax 'new-theme NAME'
+            c.description 'Creates a new Jekyll theme scaffold'
+            c.option 'code_of_conduct', \
+                     '-c', '--code-of-conduct', \
+                     'Include a Code of Conduct. (defaults to false)'
     
-        def self.require_from_bundler
-      if !ENV['JEKYLL_NO_BUNDLER_REQUIRE'] && File.file?('Gemfile')
-        require 'bundler'
+                Jekyll.logger.debug 'LiveReload:', 'Reloading #{p.url}'
+            Jekyll.logger.debug '', json_message
+            @websockets.each { |ws| ws.send(json_message) }
+          end
+        end
     
-            end
+    source 'https://rubygems.org'
+    
+    if Encoding.default_external != Encoding::UTF_8
+    
+          def plugins_string
+        UI::ErrorReport.plugins_string
+      end
+    
+          it 'corrects one hash parameter without braces with one hash value' do
+        corrected = autocorrect_source('where(x: { 'y' => 'z' })')
+        expect(corrected).to eq('where({x: { 'y' => 'z' }})')
       end
     end
   end
 end
 
     
-      module Condition0
-    def _1
-      elements[0]
-    end
+          # The body of the method definition.
+      #
+      # @note this can be either a `begin` node, if the method body contains
+      #       multiple expressions, or any other node, if it contains a single
+      #       expression.
+      #
+      # @return [Node] the body of the method definition
+      def body
+        node_parts[0]
+      end
     
-          if download =~ /.tar.gz/
-        prefix = download.gsub('.tar.gz', '').gsub('#{target}/', '')
-        untar(download) do |entry|
-          next unless out = eval_file(entry, file['files'], prefix)
-          File.join(target, out)
+            each_child_node(:pair) do |pair|
+          yield(*pair)
         end
