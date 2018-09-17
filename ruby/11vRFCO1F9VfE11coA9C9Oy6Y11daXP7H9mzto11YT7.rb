72ec3fe6e57274d9c16a3cfa62e75e8e3578c5c4
SUNsung <weatherius@gmail.com>
2018-09-17 14:25:55 +0300

    Rewrite [11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb] File in: 2018-09-17 14:25:55

--- ruby/11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb
+++ ruby/11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb
@@ -1,101 +1,91 @@
 
         
-                # see http://www.w3.org/TR/html4/types.html#type-name
-        def sanitize_to_id(name)
-          name.to_s.delete(']').tr('^-a-zA-Z0-9:.', '_')
-        end
-    
-    module ActionView #:nodoc:
-  # = Action View Raw Output Helper
-  module Helpers #:nodoc:
-    module OutputSafetyHelper
-      # This method outputs without escaping a string. Since escaping tags is
-      # now default, this can be used when you don't want Rails to automatically
-      # escape tags. This is not recommended if the data is coming from the user's
-      # input.
-      #
-      # For example:
-      #
-      #  raw @user.name
-      #  # => 'Jimmy <alert>Tables</alert>'
-      def raw(stringish)
-        stringish.to_s.html_safe
-      end
-    
-              def hidden_field_for_checkbox(options)
-            @unchecked_value ? tag('input', options.slice('name', 'disabled', 'form').merge!('type' => 'hidden', 'value' => @unchecked_value)) : ''.html_safe
-          end
+        def check_link(uri)
+  HTTParty.head(uri, :verify => false).code.to_i.tap do |status|
+    if (400..422).include?(status)
+      if status != 403 && !uri.exclude?('udemy.com')
+        raise 'Request had status #{status}'
+      else
+        putc('S')
       end
     end
   end
 end
-
     
-            class RadioButtonBuilder < Builder # :nodoc:
-          def radio_button(extra_html_options = {})
-            html_options = extra_html_options.merge(@input_html_options)
-            html_options[:skip_default_ids] = false
-            @template_object.radio_button(@object_name, @method_name, @value, html_options)
-          end
+            def self.normalize_dn(dn)
+          ::Gitlab::Auth::LDAP::DN.new(dn).to_normalized_s
+        rescue ::Gitlab::Auth::LDAP::DN::FormatError => e
+          Rails.logger.info('Returning original DN \'#{dn}\' due to error during normalization attempt: #{e.message}')
+    
+          def valid_access_token?(scopes: [])
+        validate_access_token!(scopes: scopes)
+    
+          def image_url
+        raise NotImplementedError
+      end
+    
+            def metadata
+          @metadata ||= Pipeline::Metadata.new(self)
         end
     
-            # If no layout is supplied, look for a template named the return
-        # value of this method.
-        #
-        # ==== Returns
-        # * <tt>String</tt> - A template name
-        def _implied_layout_name
-          controller_path
-        end
-    end
+    group :test do
+  gem 'minitest'
+  gem 'rr', require: false
+  gem 'rack-test', require: false
+end
     
-            def self.find_by_email(email, adapter)
-          email_fields = adapter.config.attributes['email']
-    
-            def value_color
-          case @status
-          when 95..100 then STATUS_COLOR[:good]
-          when 90..95 then STATUS_COLOR[:acceptable]
-          when 75..90 then STATUS_COLOR[:medium]
-          when 0..75 then STATUS_COLOR[:low]
-          else
-            STATUS_COLOR[:unknown]
-          end
-        end
+          def store_meta(store)
+        json = as_json
+        json[:mtime] = Time.now.to_i
+        json[:db_size] = store.size(DB_FILENAME)
+        store.write(META_FILENAME, json.to_json)
       end
     end
-  end
-end
-
     
-          spec['version'] = Bootstrap::VERSION
-    
-    # If you set this to false, any error raised from within your app will bubble
-# up to your step definition and out to cucumber unless you catch it somewhere
-# on the way. You can make Rails rescue errors and render error pages on a
-# per-scenario basis by tagging a scenario or feature with the @allow-rescue tag.
-#
-# If you set this to true, Rails will rescue all errors and render error
-# pages, more or less in the same way your application would behave in the
-# default production environment. It's not recommended to do this for all
-# of your scenarios, as this makes it hard to discover errors in your application.
-ActionController::Base.allow_rescue = false
-    
-      class PostToService < Base
-    def perform(*_args)
-      # don't post to services in cucumber
+        def absolute_url_string?(str)
+      str =~ SCHEME_RGX
     end
-  end
     
-      # fill the password reset form
-  def fill_password_reset_form(new_pass, confirm_pass)
-    fill_in 'user_password', :with => new_pass
-    fill_in 'user_password_confirmation', :with => confirm_pass
-  end
-    
-        it 'returns a 404 for a post not visible to the user' do
-      sign_in eve
-      expect {
-        get :index, params: {post_id: @message.id}
-      }.to raise_error(ActiveRecord::RecordNotFound)
+        def mime_type
+      headers['Content-Type'] || 'text/plain'
     end
+    
+        class << self
+      attr_accessor :dir
+    
+            css('img[style]').each do |node|
+          node['align'] ||= node['style'][/float:\s*(left|right)/, 1]
+          node['style'] = node['style'].split(';').map(&:strip).select { |s| s =~ /\Awidth|height/ }.join(';')
+        end
+    
+          def get_type
+        if slug.start_with?('guide/')
+          'Guide'
+        elsif slug.start_with?('cookbook/')
+          'Cookbook'
+        elsif slug == 'glossary'
+          'Guide'
+        else
+          type = at_css('.nav-title.is-selected').content.strip
+          type.remove! ' Reference'
+          type << ': #{mod}' if mod
+          type
+        end
+      end
+    
+      def internal_developer_commands
+    find_internal_commands HOMEBREW_LIBRARY_PATH/'dev-cmd'
+  end
+    
+        first_warning = true
+    methods.each do |method|
+      $stderr.puts 'Checking #{method}' if ARGV.debug?
+      unless checks.respond_to?(method)
+        Homebrew.failed = true
+        puts 'No check available by the name: #{method}'
+        next
+      end
+    
+      def framework_version
+    @framework_version ||= `rails -v`[/^Rails (.+)$/, 1]
+  end
