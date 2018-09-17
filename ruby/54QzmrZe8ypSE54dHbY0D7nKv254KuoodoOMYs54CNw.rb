cde3f90a54e6a36ecc06d1289c8b1993a5d07ff1
SUNsung <weatherius@gmail.com>
2018-09-17 16:51:18 +0300

    Rewrite [54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb] File in: 2018-09-17 16:51:17

--- ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
+++ ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
@@ -1,39 +1,223 @@
 
         
-        STDOUT.sync = true
-    
-          Utils.safe_glob(site.in_dest_dir, ['**', '*'], File::FNM_DOTMATCH).each do |file|
-        next if file =~ HIDDEN_FILE_REGEX || file =~ regex || dirs.include?(file)
-        files << file
+                if block_given?
+          content_tag :button, options, &block
+        else
+          content_tag :button, content_or_options || 'Button', options
+        end
       end
     
-          # Add common options to a command for building configuration
+        # The category for users with trust level 3 has been created.
+    # Add initial permissions and description. They can be changed later.
+    
+          check_value =
+        case check_value
+        when Integer
+          check_value
+        when Symbol
+          record.public_send(check_value) # rubocop:disable GitlabSecurity/PublicSend
+        end
+    
+            def initialize(entry, provider)
+          Rails.logger.debug { 'Instantiating #{self.class.name} with LDIF:\n#{entry.to_ldif}' }
+          @entry = entry
+          @provider = provider
+        end
+    
+      # PRIVATE CONFIGURATION
+    
+          # Return true if it's a devise_controller. false to all controllers unless
+      # the controllers defined inside devise. Useful if you want to apply a before
+      # filter to all controllers, except the ones in devise:
       #
-      # cmd - the Jekyll::Command to add these options to
-      #
-      # Returns nothing
-      # rubocop:disable Metrics/MethodLength
-      def add_build_options(cmd)
-        cmd.option 'config', '--config CONFIG_FILE[,CONFIG_FILE2,...]',
-                   Array, 'Custom configuration file'
-        cmd.option 'destination', '-d', '--destination DESTINATION',
-                   'The current folder will be generated into DESTINATION'
-        cmd.option 'source', '-s', '--source SOURCE', 'Custom source directory'
-        cmd.option 'future', '--future', 'Publishes posts with a future date'
-        cmd.option 'limit_posts', '--limit_posts MAX_POSTS', Integer,
-                   'Limits the number of posts to parse and publish'
-        cmd.option 'watch', '-w', '--[no-]watch', 'Watch for changes and rebuild'
-        cmd.option 'baseurl', '-b', '--baseurl URL',
-                   'Serve the website from the given base URL'
-        cmd.option 'force_polling', '--force_polling', 'Force watch to use polling'
-        cmd.option 'lsi', '--lsi', 'Use LSI for improved related posts'
-        cmd.option 'show_drafts', '-D', '--drafts', 'Render posts in the _drafts folder'
-        cmd.option 'unpublished', '--unpublished',
-                   'Render posts that were marked as unpublished'
-        cmd.option 'quiet', '-q', '--quiet', 'Silence output.'
-        cmd.option 'verbose', '-V', '--verbose', 'Print verbose output.'
-        cmd.option 'incremental', '-I', '--incremental', 'Enable incremental rebuild.'
-        cmd.option 'strict_front_matter', '--strict_front_matter',
-                   'Fail if errors are present in front matter'
+      #   before_action :my_filter, unless: :devise_controller?
+      def devise_controller?
+        is_a?(::DeviseController)
       end
-      # rubocop:enable Metrics/MethodLength
+    
+          def remember_key(resource, scope)
+        resource.rememberable_options.fetch(:key, 'remember_#{scope}_token')
+      end
+    end
+  end
+end
+
+    
+          def self.generate_helpers!(routes=nil)
+        routes ||= begin
+          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
+          Devise::URL_HELPERS.slice(*mappings)
+        end
+    
+    gem 'rake'
+gem 'thor'
+gem 'pry', '~> 0.11.0'
+gem 'activesupport', '~> 5.2', require: false
+gem 'yajl-ruby', require: false
+    
+        SPLIT_INTS = /(?<=\d)\.(?=[\s\d])/.freeze
+    
+        def initialize(name = nil, path = nil, type = nil)
+      self.name = name
+      self.path = path
+      self.type = type
+    
+        def build_and_queue_request(url, options, &block)
+      request = Request.new(url, request_options.merge(options))
+      request.on_complete(&block) if block
+      queue(request)
+      request
+    end
+    
+        def effective_url
+      @effective_url ||= URL.parse super
+    end
+    
+            css('header').each do |node|
+          node.before(node.children).remove
+        end
+    
+      #
+  # HTTP 404/File not found response class wrapper.
+  #
+  class E404 < Response
+    def initialize(message = 'File not found', proto = DefaultProtocol)
+      super(404, message, proto)
+    end
+  end
+    
+      #
+  # Listens on the defined port and host and starts monitoring for clients.
+  #
+  def start
+    
+        self.codec = res[2][IAX_IE_DESIRED_CODEC].unpack('N')[0]
+    self.state = :ringing
+    self.ring_start = ::Time.now.to_i
+    self.client.send_ack(self)
+    true
+  end
+    
+    # Default timings
+IAX_DEFAULT_REG_REFRESH = 60
+IAX_DEFAULT_TIMEOUT     = 10
+    
+      def self.create_ipmi_getchannel_probe
+    [   # Get Channel Authentication Capabilities
+      0x06, 0x00, 0xff, 0x07, # RMCP Header
+      0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x20, 0x18,
+      0xc8, 0x81, 0x00, 0x38, 0x8e, 0x04, 0xb5
+    ].pack('C*')
+  end
+    
+            # Sends a kerberos request, and reads the response through the connection
+        #
+        # @param req [Rex::Proto::Kerberos::Model::KdcRequest] the request to send
+        # @return [<Rex::Proto::Kerberos::Model::KrbError, Rex::Proto::Kerberos::Model::KdcResponse>] The kerberos message
+        # @raise [RuntimeError] if the transport protocol is unknown or the response can't be parsed.
+        # @raise [NotImplementedError] if the transport protocol isn't supported
+        def send_recv(req)
+          send_request(req)
+          res = recv_response
+    
+    module Rex
+  module Proto
+    module Kerberos
+      module CredentialCache
+        # This class provides a representation of a Principal stored in the Kerberos Credential Cache.
+        class Principal < Element
+          # @!attribute name_type
+          #   @return [Integer]
+          attr_accessor :name_type
+          # @!attribute realm
+          #   @return [String]
+          attr_accessor :realm
+          # @!attribute components
+          #   @return [Array<String>]
+          attr_accessor :components
+    
+                res = ''
+            case etype
+            when RC4_HMAC
+              res = encrypt_rc4_hmac(data, key, 5)
+            else
+              raise ::NotImplementedError, 'EncryptedData schema is not supported'
+            end
+    
+    module Rex
+  module Proto
+    module Kerberos
+      module Model
+        # This class provides a representation of a principal, an asset (e.g., a
+        # workstation user or a network server) on a network.
+        class Element
+    
+                res = ''
+            case etype
+            when RC4_HMAC
+              res = decrypt_rc4_hmac(cipher, key, msg_type)
+              raise ::RuntimeError, 'EncryptedData failed to decrypt' if res.length < 8
+              res = res[8, res.length - 1]
+            else
+              raise ::NotImplementedError, 'EncryptedData schema is not supported'
+            end
+    
+        def initialize(tag_name, markup, tokens)
+      @by = nil
+      @source = nil
+      @title = nil
+      if markup =~ FullCiteWithTitle
+        @by = $1
+        @source = $2 + $3
+        @title = $4.titlecase.strip
+      elsif markup =~ FullCite
+        @by = $1
+        @source = $2 + $3
+      elsif markup =~ AuthorTitle
+        @by = $1
+        @title = $2.titlecase.strip
+      elsif markup =~ Author
+        @by = $1
+      end
+      super
+    end
+    
+        def render(context)
+      if @img
+        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
+      else
+        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
+      end
+    end
+  end
+end
+    
+            Dir.chdir(includes_dir) do
+          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
+          if choices.include?(file)
+            source = File.read(file)
+            partial = Liquid::Template.parse(source)
+            context.stack do
+              rtn = rtn + partial.render(context)
+            end
+          else
+            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
+          end
+        end
+      end
+      rtn
+    end
+  end
+    
+        def render(context)
+      code_dir = (context.registers[:site].config['code_dir'].sub(/^\//,'') || 'downloads/code')
+      code_path = (Pathname.new(context.registers[:site].source) + code_dir).expand_path
+      file = code_path + @file
+    
+        def initialize(tag_name, markup, tokens)
+      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
+      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
+      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
+      super
+    end
