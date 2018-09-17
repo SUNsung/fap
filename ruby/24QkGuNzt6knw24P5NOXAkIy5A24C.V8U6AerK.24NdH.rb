cf661f1978f337b193eef17579fb64b3e16fa5cc
SUNsung <weatherius@gmail.com>
2018-09-17 23:10:05 +0300

    Rewrite [24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb] File in: 2018-09-17 23:10:04

--- ruby/24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb
+++ ruby/24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb
@@ -1,148 +1,35 @@
 
         
-                # Find and link metafiles
-        formula.prefix.install_metafiles formula.buildpath
-        formula.prefix.install_metafiles formula.libexec if formula.libexec.exist?
-      end
-    end
-  end
+                before_action :find_order, except: [:create, :mine, :current, :index, :update, :remove_coupon_code]
     
-        def self.cleanup_lockfiles
-      return unless HOMEBREW_CACHE_FORMULA.directory?
-      candidates = HOMEBREW_CACHE_FORMULA.children
-      lockfiles  = candidates.select { |f| f.file? && f.extname == '.brewing' }
-      lockfiles.each do |file|
-        next unless file.readable?
-        file.open.flock(File::LOCK_EX | File::LOCK_NB) && file.unlink
-      end
-    end
+            def index
+          authorize! :admin, ReturnAuthorization
+          @return_authorizations = order.return_authorizations.accessible_by(current_ability, :read).
+                                   ransack(params[:q]).result.
+                                   page(params[:page]).per(params[:per_page])
+          respond_with(@return_authorizations)
+        end
     
-      def migrate_tap_migration
-    report[:D].each do |full_name|
-      name = full_name.split('/').last
-      next unless (dir = HOMEBREW_CELLAR/name).exist? # skip if formula is not installed.
-      next unless new_tap_name = tap.tap_migrations[name] # skip if formula is not in tap_migrations list.
-      tabs = dir.subdirs.map { |d| Tab.for_keg(Keg.new(d)) }
-      next unless tabs.first.tap == tap # skip if installed formula is not from this tap.
-      new_tap = Tap.fetch(new_tap_name)
-      new_tap.install unless new_tap.installed?
-      # update tap for each Tab
-      tabs.each { |tab| tab.tap = new_tap }
-      tabs.each(&:write)
-    end
-  end
-    
-    # This formula serves as the base class for several very similar
-# formulae for Amazon Web Services related tools.
-class AmazonWebServicesFormula < Formula
-  # Use this method to peform a standard install for Java-based tools,
-  # keeping the .jars out of HOMEBREW_PREFIX/lib
-  def install
-    rm Dir['bin/*.cmd'] # Remove Windows versions
-    libexec.install Dir['*']
-    bin.install_symlink Dir['#{libexec}/bin/*'] - ['#{libexec}/bin/service']
-  end
-  alias_method :standard_install, :install
-    
-    TEMPLATE = ERB.new <<-HTML # :nodoc:
-<!DOCTYPE html>
-<html>
-<head>
-  <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
-  <title><%=h exception.class %> at <%=h path %></title>
-    
-    desc 'generate gemspec'
-task 'rack-protection.gemspec' do
-  require 'rack/protection/version'
-  content = File.binread 'rack-protection.gemspec'
-    
-          def session_key
-        @session_key ||= options[:session_key]
+            def scope
+          if params[:country_id]
+            @country = Country.accessible_by(current_ability, :read).find(params[:country_id])
+            @country.states.accessible_by(current_ability, :read).order('name ASC')
+          else
+            State.accessible_by(current_ability, :read).order('name ASC')
+          end
+        end
       end
     end
   end
 end
 
     
-            if has_vector?(request, headers)
-          warn env, 'attack prevented by #{self.class}'
-    
-      it 'ignores single-line arrays' do
-    expect_no_offenses('[a, b, c]')
-  end
-    
-      let(:prefix) { '' } # A prefix before the opening brace.
-  let(:suffix) { '' } # A suffix for the line after the closing brace.
-  let(:open) { nil } # The opening brace.
-  let(:close) { nil } # The closing brace.
-  let(:a) { 'a' } # The first element.
-  let(:b) { 'b' } # The second element.
-  let(:b_comment) { '' } # Comment after the second element.
-  let(:multi_prefix) { '' } # Prefix multi and heredoc with this.
-  let(:multi) do # A viable multi-line element.
-    <<-RUBY.strip_indent.chomp
-      {
-      foo: bar
-      }
-    RUBY
-  end
-  # This heredoc is unsafe to edit around because it ends on the same line as
-  # the node itself.
-  let(:heredoc) do
-    <<-RUBY.strip_indent.chomp
-      <<-EOM
-      baz
-      EOM
-    RUBY
-  end
-  # This heredoc is safe to edit around because it ends on a line before the
-  # last line of the node.
-  let(:safe_heredoc) do
-    <<-RUBY.strip_indent.chomp
-      {
-      a: <<-EOM
-      baz
-      EOM
-      }
-    RUBY
-  end
-    
-      context 'when arguments to a method' do
-    let(:prefix) { 'bar(' }
-    let(:suffix) { ')' }
-    let(:source) { construct(false, true) }
-    
-        it 'registers an offense for opposite + correct style' do
-      expect_offense(<<-RUBY.strip_indent)
-        def func
-          for n in [1, 2, 3] do
-          ^^^^^^^^^^^^^^^^^^^^^ Prefer `each` over `for`.
-            puts n
-          end
-          [1, 2, 3].each do |n|
-            puts n
+            def create
+          authorize! :create, StockLocation
+          @stock_location = StockLocation.new(stock_location_params)
+          if @stock_location.save
+            respond_with(@stock_location, status: 201, default_template: :show)
+          else
+            invalid_resource!(@stock_location)
           end
         end
-      RUBY
-    end
-    
-            def on_block(node)
-          return if target_ruby_version < 2.5
-    
-          # An array containing the arguments of the method definition.
-      #
-      # @return [Array<Node>] the arguments of the method definition
-      def arguments
-        node_parts[1]
-      end
-    
-          # Returns the branch of the `if` node that gets evaluated when its
-      # condition is falsey.
-      #
-      # @note This is normalized for `unless` nodes.
-      #
-      # @return [Node] the falsey branch node of the `if` node
-      # @return [nil] when there is no else branch
-      def else_branch
-        node_parts[2]
-      end
