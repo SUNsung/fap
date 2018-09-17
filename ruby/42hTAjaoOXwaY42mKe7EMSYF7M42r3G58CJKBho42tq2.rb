f1a257542050db559cce264bbed787050d5a3616
SUNsung <weatherius@gmail.com>
2018-09-17 07:48:56 +0300

    Rewrite [42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb] File in: 2018-09-17 07:48:56

--- ruby/42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb
+++ ruby/42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb
@@ -1,35 +1,68 @@
 
         
-            if superenv?
-      ENV.keg_only_deps = keg_only_deps
-      ENV.deps = formula_deps
-      ENV.x11 = reqs.any? { |rq| rq.is_a?(X11Requirement) }
-      ENV.setup_build_environment(formula)
-      post_superenv_hacks
-      reqs.each(&:modify_build_environment)
-      deps.each(&:modify_build_environment)
-    else
-      ENV.setup_build_environment(formula)
-      reqs.each(&:modify_build_environment)
-      deps.each(&:modify_build_environment)
+                # Execute a command on the remote machine. The exact semantics
+        # of this method are up to the implementor, but in general the
+        # users of this class will expect this to be a shell.
+        #
+        # This method gives you no way to write data back to the remote
+        # machine, so only execute commands that don't expect input.
+        #
+        # @param [String] command Command to execute.
+        # @yield [type, data] Realtime output of the command being executed.
+        # @yieldparam [String] type Type of the output. This can be
+        #   `:stdout`, `:stderr`, etc. The exact types are up to the
+        #   implementor.
+        # @yieldparam [String] data Data for the given output.
+        # @return [Integer] Exit code of the command.
+        def execute(command, opts=nil)
+        end
     
-        if RUBY_PATH.to_s !~ %r{^/System/Library/Frameworks/Ruby.framework/Versions/[12]\.[089]/usr/bin/ruby}
-      s << ' => #{RUBY_PATH}'
-    end
-    s
-  end
+            # Converts this configuration object to JSON.
+        def to_json(*a)
+          instance_variables_hash.to_json(*a)
+        end
     
-      # Use this method to generate standard caveats.
-  def standard_instructions(home_name, home_value = libexec)
-    <<-EOS.undent
-      Before you can use these tools you must export some variables to your $SHELL.
+            def initialize(env, config)
+          @env    = env
+          @config = config
+        end
     
-        find_union(segments, Project).includes(:namespace).order_id_desc
-  end
+            # Initializes the communicator with the machine that we will be
+        # communicating with. This base method does nothing (it doesn't
+        # even store the machine in an instance variable for you), so you're
+        # expected to override this and do something with the machine if
+        # you care about it.
+        #
+        # @param [Machine] machine The machine this instance is expected to
+        #   communicate with.
+        def initialize(machine)
+        end
     
-    signer._invoke('JarSignerMSF','[Ljava.lang.String;',jarsignerOpts)
+              # Return the dummy object so that anything else works
+          ::Vagrant::Config::V2::DummyConfig.new
+        end
     
-          spec['main'] =
-          find_files.(File.join(Bootstrap.stylesheets_path, '_bootstrap.scss')) +
-          find_files.(Bootstrap.fonts_path) +
-          %w(assets/javascripts/bootstrap.js)
+            # This returns all synced folder implementations.
+        #
+        # @return [Registry]
+        def synced_folders
+          Registry.new.tap do |result|
+            @registered.each do |plugin|
+              result.merge!(plugin.components.synced_folders)
+            end
+          end
+        end
+    
+          def requires_authentication?
+        Spree::Api::Config[:requires_authentication]
+      end
+    
+            def new; end
+    
+            def destroy
+          authorize! :destroy, @product_property
+          @product_property.destroy
+          respond_with(@product_property, status: 204)
+        end
+    
+            private
