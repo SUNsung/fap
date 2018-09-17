b0d4f27b71a449376da670a8b9d64e302550631a
SUNsung <weatherius@gmail.com>
2018-09-17 10:43:33 +0300

    Rewrite [00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py] File in: 2018-09-17 10:43:32

--- pyton/00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py
+++ pyton/00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py
@@ -1,103 +1,111 @@
 
         
         
-def check_alphabetical(lines):
+def CheckCall( args, **kwargs ):
+  try:
+    subprocess.check_call( args, **kwargs )
+  except subprocess.CalledProcessError as error:
+    sys.exit( error.returncode )
+    
+      AddToGroupMap( 'Function', identifier_group )
+    
+    import contextlib
+import functools
+import os
+import requests
+import time
+import warnings
+    
+      results = request.RawResponse()[ 'completions' ]
+    
+    
+def EndsWithPython_BadPaths_test():
+  not_python_paths = [
+    None,
+    '',
+    '/opt/local/bin/vim',
+    r'C:\Program Files\Vim\vim74\gvim.exe',
+    '/usr/bin/python2.5',
+    '/home/user/.pyenv/shims/python3.2',
+  ]
+    
+    from ycm.tests.test_utils import MockVimModule
+MockVimModule()
+    
+        requests *= 10000
+    client1_delegate = timeit(client1.delegate)
+    client2_delegate = timeit(client2.delegate)
+    with suppress_stdout():
+        client1_delegate(requests)
+        client2_delegate(requests)
+    # lets check which is faster
+    print(client1_delegate._time, client2_delegate._time)
+    
+    print('Counting to two...')
+for number in count_to_two():
+    print(number, end=' ')
+    
+        def tearDown(self):
+        if not self._bProblem:
+            print('Tearing down')
+            time.sleep(0.1)
+            self._tm.publishReport()
+        else:
+            print('Test not executed. No tear down required.')
+    
+    
+# Makes templates
+def make_template(skeleton, getter, action):
+    '''Instantiate a template method with getter and action'''
+    def template():
+        skeleton(getter, action)
+    return template
+    
+    
+class Dog(object):
+    
+        def __get__(self, obj, type_):
+        if obj is None:
+            return self
+        val = self.function(obj)
+        obj.__dict__[self.function.__name__] = val
+        return val
+    
+    
+class MidnightTimeProvider(object):
     '''
-    checks if all entries per section are in alphabetical order based in entry title
+    Class implemented as hard-coded stub (in contrast to configurable stub).
     '''
-    sections = {}
-    section_line_num = {}
-    for line_num, line in enumerate(lines):
-        if line.startswith(anchor):
-            category = line.split(anchor)[1].strip()
-            sections[category] = []
-            section_line_num[category] = line_num
-            continue
-        if not line.startswith('|') or line.startswith('|---'):
-            continue
-        raw_title = [x.strip() for x in line.split('|')[1:-1]][0]
-        title_re_match = link_re.match(raw_title)
-        if title_re_match:
-            sections[category].append(title_re_match.group(1).upper())
     
-            final_headers[name] = value
-    return final_headers
+        def __getattr__(self, name):
+        def wrapper(*args, **kwargs):
+            if hasattr(self.delegate, name):
+                attr = getattr(self.delegate, name)
+                if callable(attr):
+                    return attr(*args, **kwargs)
+        return wrapper
     
-        def __repr__(self):
-        return '<{0} {1}>'.format(type(self).__name__, str(self))
-
+        for obj in objects:
+        print('A {0} goes {1}'.format(obj.name, obj.make_noise()))
     
+    # Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named 'default.css' will overwrite the builtin 'default.css'.
+html_static_path = ['_static']
     
-FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
-FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
-JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
-BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
+        hass.services.register(DOMAIN, SERVICE_BROWSE_URL,
+                           lambda service:
+                           webbrowser.open(service.data[ATTR_URL]),
+                           schema=SERVICE_BROWSE_URL_SCHEMA)
     
+        def __init__(self, test_mode):
+        '''Initialize suite view.'''
+        self._test_mode = test_mode
     
-@mock.patch('httpie.input.AuthCredentials._getpass',
-            new=lambda self, prompt: 'UNEXPECTED_PROMPT_RESPONSE')
-def test_auth_plugin_prompt_password_false(httpbin):
-    
-        def test_binary_file_path(self, httpbin):
-        env = MockEnvironment(stdin_isatty=True, stdout_isatty=False)
-        r = http('--print=B', 'POST', httpbin.url + '/post',
-                 '@' + BIN_FILE_PATH_ARG, env=env, )
-        assert r == BIN_FILE_CONTENT
-    
-        def test_POST_explicit_JSON_auto_JSON_accept(self, httpbin):
-        r = http('--json', 'POST', httpbin.url + '/post')
-        assert HTTP_OK in r
-        assert r.json['headers']['Accept'] == JSON_ACCEPT
-        # Make sure Content-Type gets set even with no data.
-        # https://github.com/jakubroztocil/httpie/issues/137
-        assert 'application/json' in r.json['headers']['Content-Type']
-    
-        def test_get_lexer_not_found(self):
-        assert get_lexer('xxx/yyy') is None
-    
-        def init_per_thread_state(self):
-        # Ensure state is initialized just once per-thread
-        if not hasattr(self._thread_local, 'init'):
-            self._thread_local.init = True
-            self._thread_local.last_nonce = ''
-            self._thread_local.nonce_count = 0
-            self._thread_local.chal = {}
-            self._thread_local.pos = None
-            self._thread_local.num_401_calls = None
-    
-    
-def default_hooks():
-    return dict((event, []) for event in HOOKS)
-    
-    
-@pytest.mark.skipif(sys.version_info < (2,7), reason='Only run on Python 2.7+')
-def test_system_ssl():
-    '''Verify we're actually setting system_ssl when it should be available.'''
-    assert info()['system_ssl']['version'] != ''
-    
-        @pytest.fixture(autouse=True)
-    def setup(self):
-        '''LookupDict instance with 'bad_gateway' attribute.'''
-        self.lookup_dict = LookupDict('test')
-        self.lookup_dict.bad_gateway = 502
-    
-                sock.close()
-    
-        def run(self):
-        try:
-            self.server_sock = self._create_socket_and_bind()
-            # in case self.port = 0
-            self.port = self.server_sock.getsockname()[1]
-            self.ready_event.set()
-            self._handle_requests()
-    
-    
-pteredor_is_running = False
-    
-            p = random.choice(self.plus)
-    
-    Here is a little overview over the most commonly used classes provided by
-this runtime:
-    
-            You don't have to implement but it's nice to know where a Token
-        comes from if you have include files etc... on the input.'''
+        see(
+        dev_id='demo_home_boy',
+        host_name='Home Boy',
+        gps=[hass.config.latitude - 0.00002, hass.config.longitude + 0.00002],
+        gps_accuracy=20,
+        battery=53
+    )
