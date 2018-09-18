e9eb1097c430f8896767abc69a19b773baf642f5
SUNsung <weatherius@gmail.com>
2018-09-18 22:45:38 +0300

    Rewrite [25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py] File in: 2018-09-18 22:45:37

--- pyton/25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py
+++ pyton/25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py
@@ -1,152 +1,52 @@
 
         
-            Validates that the username is not already taken. Hashes the
-    password for security.
-    '''
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
-    
-            :param obj: an import name or object
-        '''
-        if isinstance(obj, string_types):
-            obj = import_string(obj)
-        for key in dir(obj):
-            if key.isupper():
-                self[key] = getattr(obj, key)
-    
-    This typically means that you attempted to use functionality that needed
-an active HTTP request.  Consult the documentation on testing for
-information about how to avoid this problem.\
-'''
-_app_ctx_err_msg = '''\
-Working outside of application context.
-    
-            from flask import jsonify
-    
-        tag = to_json
-    
-        def open_session(self, app, request):
-        s = self.get_signing_serializer(app)
-        if s is None:
-            return None
-        val = request.cookies.get(app.session_cookie_name)
-        if not val:
-            return self.session_class()
-        max_age = total_seconds(app.permanent_session_lifetime)
+            @staticmethod
+    def import_windows_ca(certfile):
+        xlog.debug('Begin to import Windows CA')
+        with open(certfile, 'rb') as fp:
+            certdata = fp.read()
+            if certdata.startswith(b'-----'):
+                begin = b'-----BEGIN CERTIFICATE-----'
+                end = b'-----END CERTIFICATE-----'
+                certdata = base64.b64decode(b''.join(certdata[certdata.find(begin)+len(begin):certdata.find(end)].strip().splitlines()))
         try:
-            data = s.loads(val, max_age=max_age)
-            return self.session_class(data)
-        except BadSignature:
-            return self.session_class()
+            common_name = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_ASN1, certdata).get_subject().CN
+        except Exception as e:
+            logging.error('load_certificate(certfile=%r) 失败：%s', certfile, e)
+            return -1
     
+        if probe_nat:
+        pteredor_is_running = False
+        log = Log()
+        log.write('qualified: %s\nNAT type: %s' % (prober.qualified, prober.nat_type))
+        log.close()
+    return prober
     
-if len(sys.argv) == 2 and sys.argv[1] == '--list':
-    inv_json = get_guests()
-    print(json.dumps(inv_json, sort_keys=True))
-elif len(sys.argv) == 3 and sys.argv[1] == '--host':
-    print(json.dumps({}))
-else:
-    print('Need an argument, either --list or --host <host>')
-
+        def eval_servers(self):
+        if self.nat_type is 'null':
+            self.nat_type = self.nat_type_probe()
+        elif self.nat_type is 'probing':
+            print('Is probing NAT type now, pleace wait...')
+        while self.nat_type is 'probing':
+            time.sleep(0.1)
+        if not self.qualified:
+            print('This device can not use teredo tunnel, the NAT type is %s!' % prober.nat_type)
+            return []
+        print('Starting evaluate servers...')
+        self.clear()
+        eval_list = []
+        queue_obj = queue.Queue()
+        for server_ip in self.server_ip_list:
+            thread.start_new_thread(self._eval_servers, (server_ip, queue_obj))
+        for _ in self.server_ip_list:
+            eval_list.append(queue_obj.get())
+        return eval_list
     
-        allvars = {}
-    output = {}
-    cli_list = []
-    cli_bin_name_list = []
+    A tree.RewriteCardinalityException is raised, when the parsers hits a
+cardinality mismatch during AST construction. Although this is basically a
+bug in your grammar, it can only be detected at runtime.
     
-    #__requires__ = ['ansible']
-try:
-    import pkg_resources
-except Exception:
-    # Use pkg_resources to find the correct versions of libraries and set
-    # sys.path appropriately when there are multiversion installs.  But we
-    # have code that better expresses the errors in the places where the code
-    # is actually used (the deps are optional for many code paths) so we don't
-    # want to fail here.
-    pass
+    # Predefined token types
+EOR_TOKEN_TYPE = 1
     
-            # Actions
-        action_group = optparse.OptionGroup(self.parser, 'Actions', 'One of following must be used on invocation, ONLY ONE!')
-        action_group.add_option('--list', action='store_true', default=False, dest='list', help='Output all hosts info, works as inventory script')
-        action_group.add_option('--host', action='store', default=None, dest='host', help='Output specific host info, works as inventory script')
-        action_group.add_option('--graph', action='store_true', default=False, dest='graph',
-                                help='create inventory graph, if supplying pattern it must be a valid group name')
-        self.parser.add_option_group(action_group)
-    
-                tested_methods = conman.tested_methods_from_spidercls(spidercls)
-            if opts.list:
-                for method in tested_methods:
-                    contract_reqs[spidercls.name].append(method)
-            elif tested_methods:
-                self.crawler_process.crawl(spidercls)
-    
-        def print_results(self, opts):
-        colour = not opts.nocolour
-    
-            if not assertion:
-            if self.min_bound == self.max_bound:
-                expected = self.min_bound
-            else:
-                expected = '%s..%s' % (self.min_bound, self.max_bound)
-    
-    from setuptools import setup, find_packages
-setup(
-    name = proj_info['name'],
-    version = VERSION,
-    
-    
-    def extract(self, **kwargs):
-        for i in self.streams:
-            s = self.streams[i]
-            _, s['container'], s['size'] = url_info(s['url'])
-            s['src'] = [s['url']]
-        if 'stream_id' in kwargs and kwargs['stream_id']:
-            # Extract the stream
-            stream_id = kwargs['stream_id']
-    
-    from xml.etree import cElementTree as ET
-from copy import copy
-from ..common import *
-#----------------------------------------------------------------------
-def ckplayer_get_info_by_xml(ckinfo):
-    '''str->dict
-    Information for CKPlayer API content.'''
-    e = ET.XML(ckinfo)
-    video_dict = {'title': '',
-                  #'duration': 0,
-                  'links': [],
-                  'size': 0,
-                  'flashvars': '',}
-    dictified = dictify(e)['ckplayer']
-    if 'info' in dictified:
-        if '_text' in dictified['info'][0]['title'][0]:  #title
-            video_dict['title'] = dictified['info'][0]['title'][0]['_text'].strip()
-    
-        def prepare(self, **kwargs):
-        self.api_data = json.loads(get_content(self.__class__.ep.format(self.vid)))
-        self.title = self.api_data['title']
-        for s in self.api_data['video']:
-            for st in self.__class__.stream_types:
-                if st['map_to'] == s:
-                    urls = self.api_data['video'][s]
-                    src = [u['url'] for u in urls]
-                    stream_data = dict(src=src, size=0, container='mp4', video_profile=st['video_profile'])
-                    self.streams[st['id']] = stream_data
-    
-            #type_, ext, size = url_info(url)
-        #print_info(site_info, title, type_, size)
-        #if not info_only:
-            #download_urls([url], title, ext, total_size=None, output_dir=output_dir, merge=merge)
-    
-        sd_urls = list(set([
-        unicodize(str.replace(i, '\\/', '/'))
-        for i in re.findall(r'sd_src_no_ratelimit:'([^']*)'', html)
-    ]))
-    hd_urls = list(set([
-        unicodize(str.replace(i, '\\/', '/'))
-        for i in re.findall(r'hd_src_no_ratelimit:'([^']*)'', html)
-    ]))
-    urls = hd_urls if hd_urls else sd_urls
+            return [token.text for token in tokens]
