e269ea52228f308eb52d53ccda33106fe1e5a3c5
SUNsung <weatherius@gmail.com>
2018-09-18 18:37:31 +0300

    Rewrite [24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb] File in: 2018-09-18 18:37:30

--- ruby/24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb
+++ ruby/24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb
@@ -1,60 +1,38 @@
 
         
-        WITH_LIQUID = <<-LIQUID.freeze
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor libero at
-pharetra tempus. {{ author }} et metus fermentum, eu cursus lorem
-mattis. Curabitur vel dui et lacus rutrum suscipit et eget neque.
-    
-                c.option 'force', '--force', 'Force creation even if PATH already exists'
-            c.option 'blank', '--blank', 'Creates scaffolding but with empty files'
-            c.option 'skip-bundle', '--skip-bundle', 'Skip 'bundle install''
-    
-    module Jekyll
-  module Commands
-    class NewTheme < Jekyll::Command
-      class << self
-        def init_with_program(prog)
-          prog.command(:'new-theme') do |c|
-            c.syntax 'new-theme NAME'
-            c.description 'Creates a new Jekyll theme scaffold'
-            c.option 'code_of_conduct', \
-                     '-c', '--code-of-conduct', \
-                     'Include a Code of Conduct. (defaults to false)'
-    
-                Jekyll.logger.debug 'LiveReload:', 'Reloading #{p.url}'
-            Jekyll.logger.debug '', json_message
-            @websockets.each { |ws| ws.send(json_message) }
-          end
-        end
-    
-    source 'https://rubygems.org'
-    
-    if Encoding.default_external != Encoding::UTF_8
-    
-          def plugins_string
-        UI::ErrorReport.plugins_string
-      end
-    
-          it 'corrects one hash parameter without braces with one hash value' do
-        corrected = autocorrect_source('where(x: { 'y' => 'z' })')
-        expect(corrected).to eq('where({x: { 'y' => 'z' }})')
-      end
+            def initialize_copy(other)
+      @paths = other.paths.dup
+      self
     end
+    
+            stdlibs = detect_stdlibs(ENV.compiler)
+        Tab.create(formula, ENV.compiler, stdlibs.first, formula.build, formula.source_modified_time).write
+    
+      # True if a {Formula} is being built as a bottle (i.e. binary package).
+  def bottle?
+    include? 'build-bottle'
+  end
+    
+            if Pathname::BOTTLE_EXTNAME_RX === file.to_s
+          version = bottle_resolve_version(file) rescue file.version
+        else
+          version = file.version
+        end
+        next unless version
+        next unless (name = file.basename.to_s[/(.*)-(?:#{Regexp.escape(version)})/, 1])
+    
+      private
+    
+      def gcc_40
+    @gcc_40 ||= MacOS.gcc_40_build_version if MacOS.has_apple_developer_tools?
+  end
+    
+    # See browser for an example
+class GithubGistFormula < ScriptFileFormula
+  def self.url(val)
+    super
+    version File.basename(File.dirname(val))[0, 6]
   end
 end
-
     
-          # The body of the method definition.
-      #
-      # @note this can be either a `begin` node, if the method body contains
-      #       multiple expressions, or any other node, if it contains a single
-      #       expression.
-      #
-      # @return [Node] the body of the method definition
-      def body
-        node_parts[0]
-      end
-    
-            each_child_node(:pair) do |pair|
-          yield(*pair)
-        end
+    source 'https://rubygems.org'
