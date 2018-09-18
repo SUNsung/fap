c879d181f62c1b0a30fb35c18ba816583bebca77
SUNsung <weatherius@gmail.com>
2018-09-18 21:30:06 +0300

    Rewrite [06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py] File in: 2018-09-18 21:30:05

--- pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
+++ pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
@@ -1,11 +1,3 @@
 
         
-            def extract(self, **kwargs):
-        for i in self.streams:
-            s = self.streams[i]
-            _, s['container'], s['size'] = url_info(s['url'])
-            s['src'] = [s['url']]
-    
-    site_info = 'CBS.com'
-download = cbs_download
-download_playlist = playlist_not_supported('cbs')
+        
