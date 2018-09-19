45ddeffa6aeabad416500b9912e4f9b2b6a46824
SUNsung <weatherius@gmail.com>
2018-09-19 09:38:19 +0300

    Rewrite [291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp] File in: 2018-09-19 09:38:18

--- cccccc/291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp
+++ cccccc/291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp
@@ -1,217 +1,278 @@
 
         
-        #ifndef BITCOIN_QT_MACDOCKICONHANDLER_H
-#define BITCOIN_QT_MACDOCKICONHANDLER_H
-    
-    protected:
-    void paintEvent(QPaintEvent *);
-    
-    /* Equality between doubles is imprecise. Comparison should be done
- * with a small threshold of tolerance, rather than exact equality.
- */
-static bool DoubleEquals(double a, double b, double epsilon)
+        /// This class implements a simple type recursive traverser which queries a
+/// user-provided walker class on every node in a type.
+class Traversal : public TypeVisitor<Traversal, bool>
 {
-    return std::abs(a - b) < epsilon;
+  using Base = TypeVisitor;
+  friend Base;
+    }
+    
+      // Check whether the current end of the vector is a clean multiple
+  // of the chunk size.
+  auto offset = LengthInBits % ChunkSizeInBits;
+  ChunkType *nextChunk = &getChunksPtr()[LengthInBits / ChunkSizeInBits];
+    
+    StringRef
+camel_case::toLowercaseInitialisms(StringRef string,
+                                   SmallVectorImpl<char> &scratch) {
+    }
+    
+    // Include the correct TaskQueue implementation.
+#if LLVM_ON_UNIX && !defined(__CYGWIN__) && !defined(__HAIKU__)
+#include 'Unix/TaskQueue.inc'
+#else
+#include 'Default/TaskQueue.inc'
+#endif
+    
+    
+    {}%
+    
+    
+    {    return name;
+  }
+    
+    class MetadataCredentialsPluginWrapper final : private GrpcLibraryCodegen {
+ public:
+  static void Destroy(void* wrapper);
+  static int GetMetadata(
+      void* wrapper, grpc_auth_metadata_context context,
+      grpc_credentials_plugin_metadata_cb cb, void* user_data,
+      grpc_metadata creds_md[GRPC_METADATA_CREDENTIALS_PLUGIN_SYNC_MAX],
+      size_t* num_creds_md, grpc_status_code* status,
+      const char** error_details);
+    }
+    
+    namespace grpc {
+    }
+    
+    void CensusClientCallData::Destroy(grpc_call_element* elem,
+                                   const grpc_call_final_info* final_info,
+                                   grpc_closure* then_call_closure) {
+  const uint64_t request_size = GetOutgoingDataSize(final_info);
+  const uint64_t response_size = GetIncomingDataSize(final_info);
+  double latency_ms = absl::ToDoubleMilliseconds(absl::Now() - start_time_);
+  ::opencensus::stats::Record(
+      {{RpcClientSentBytesPerRpc(), static_cast<double>(request_size)},
+       {RpcClientReceivedBytesPerRpc(), static_cast<double>(response_size)},
+       {RpcClientRoundtripLatency(), latency_ms},
+       {RpcClientServerLatency(),
+        ToDoubleMilliseconds(absl::Nanoseconds(elapsed_time_))},
+       {RpcClientSentMessagesPerRpc(), sent_message_count_},
+       {RpcClientReceivedMessagesPerRpc(), recv_message_count_}},
+      {{ClientMethodTagKey(), method_},
+       {ClientStatusTagKey(), StatusCodeToString(final_info->final_status)}});
+  grpc_slice_unref_internal(path_);
+  context_.EndSpan();
 }
     
     
-    {    // These inputs are valid because PROTOCOLINFO accepts an OtherLine that is
-    // just an OptArguments, which enables multiple spaces to be present
-    // between the command and arguments.
-    CheckSplitTorReplyLine('COMMAND  ARGS', 'COMMAND', ' ARGS');
-    CheckSplitTorReplyLine('COMMAND   EVEN+more  ARGS', 'COMMAND', '  EVEN+more  ARGS');
+    {}  // namespace
+    
+    constexpr size_t TraceContextEncoding::kGrpcTraceContextSize;
+constexpr size_t TraceContextEncoding::kEncodeDecodeFailure;
+constexpr size_t TraceContextEncoding::kVersionIdSize;
+constexpr size_t TraceContextEncoding::kFieldIdSize;
+constexpr size_t TraceContextEncoding::kVersionIdOffset;
+constexpr size_t TraceContextEncoding::kVersionId;
+    
+    // Fix user-supplied options to be reasonable
+template <class T, class V>
+static void ClipToRange(T* ptr, V minvalue, V maxvalue) {
+  if (static_cast<V>(*ptr) > maxvalue) *ptr = maxvalue;
+  if (static_cast<V>(*ptr) < minvalue) *ptr = minvalue;
+}
+Options SanitizeOptions(const std::string& dbname,
+                        const InternalKeyComparator* icmp,
+                        const InternalFilterPolicy* ipolicy,
+                        const Options& src) {
+  Options result = src;
+  result.comparator = icmp;
+  result.filter_policy = (src.filter_policy != nullptr) ? ipolicy : nullptr;
+  ClipToRange(&result.max_open_files,    64 + kNumNonTableCacheFiles, 50000);
+  ClipToRange(&result.write_buffer_size, 64<<10,                      1<<30);
+  ClipToRange(&result.max_file_size,     1<<20,                       1<<30);
+  ClipToRange(&result.block_size,        1<<10,                       4<<20);
+  if (result.info_log == nullptr) {
+    // Open a log file in the same directory as the db
+    src.env->CreateDir(dbname);  // In case it does not exist
+    src.env->RenameFile(InfoLogFileName(dbname), OldInfoLogFileName(dbname));
+    Status s = src.env->NewLogger(InfoLogFileName(dbname), &result.info_log);
+    if (!s.ok()) {
+      // No place suitable for logging
+      result.info_log = nullptr;
+    }
+  }
+  if (result.block_cache == nullptr) {
+    result.block_cache = NewLRUCache(8 << 20);
+  }
+  return result;
 }
     
+    namespace leveldb {
+    }
     
-//------------------------------------------------------------------------------
-// Implementation details.
-#include <algorithm>
-#include <cassert>
-#include <iostream>
-#include <sstream>
-#include <stdexcept>
-    
-        void __pushKV(const std::string& key, const UniValue& val);
-    bool pushKV(const std::string& key, const UniValue& val);
-    bool pushKV(const std::string& key, const std::string& val_) {
-        UniValue tmpVal(VSTR, val_);
-        return pushKV(key, tmpVal);
+    TEST(DBTest, MultiThreaded) {
+  do {
+    // Initialize state
+    MTState mt;
+    mt.test = this;
+    mt.stop.Release_Store(0);
+    for (int id = 0; id < kNumThreads; id++) {
+      mt.counter[id].Release_Store(0);
+      mt.thread_done[id].Release_Store(0);
     }
-    bool pushKV(const std::string& key, const char *val_) {
-        std::string _val(val_);
-        return pushKV(key, _val);
     }
-    bool pushKV(const std::string& key, int64_t val_) {
-        UniValue tmpVal(val_);
-        return pushKV(key, tmpVal);
     }
-    bool pushKV(const std::string& key, uint64_t val_) {
-        UniValue tmpVal(val_);
-        return pushKV(key, tmpVal);
-    }
-    bool pushKV(const std::string& key, bool val_) {
-        UniValue tmpVal((bool)val_);
-        return pushKV(key, tmpVal);
-    }
-    bool pushKV(const std::string& key, int val_) {
-        UniValue tmpVal((int64_t)val_);
-        return pushKV(key, tmpVal);
-    }
-    bool pushKV(const std::string& key, double val_) {
-        UniValue tmpVal(val_);
-        return pushKV(key, tmpVal);
-    }
-    bool pushKVs(const UniValue& obj);
-    
-    # define TYPED_TEST_P(CaseName, TestName) \
-  namespace GTEST_CASE_NAMESPACE_(CaseName) { \
-  template <typename gtest_TypeParam_> \
-  class TestName : public CaseName<gtest_TypeParam_> { \
-   private: \
-    typedef CaseName<gtest_TypeParam_> TestFixture; \
-    typedef gtest_TypeParam_ TypeParam; \
-    virtual void TestBody(); \
-  }; \
-  static bool gtest_##TestName##_defined_ GTEST_ATTRIBUTE_UNUSED_ = \
-      GTEST_TYPED_TEST_CASE_P_STATE_(CaseName).AddTestName(\
-          __FILE__, __LINE__, #CaseName, #TestName); \
-  } \
-  template <typename gtest_TypeParam_> \
-  void GTEST_CASE_NAMESPACE_(CaseName)::TestName<gtest_TypeParam_>::TestBody()
-    
-    // A unique type used as the default value for the arguments of class
-// template Types.  This allows us to simulate variadic templates
-// (e.g. Types<int>, Type<int, double>, and etc), which C++ doesn't
-// support directly.
-struct None {};
     
     
-    {  // <TechnicalDetails>
-  //
-  // EXPECT_EQ(expected, actual) is the same as
-  //
-  //   EXPECT_TRUE((expected) == (actual))
-  //
-  // except that it will print both the expected value and the actual
-  // value when the assertion fails.  This is very helpful for
-  // debugging.  Therefore in this case EXPECT_EQ is preferred.
-  //
-  // On the other hand, EXPECT_TRUE accepts any Boolean expression,
-  // and is thus more general.
-  //
-  // </TechnicalDetails>
+    {  ASSERT_TRUE(!ParseInternalKey(Slice('bar'), &decoded));
 }
     
-    
-    {  // Sets the 0-terminated C string this MyString object represents.
-  void Set(const char* c_string);
+    // Called on every item found in a WriteBatch.
+class WriteBatchItemPrinter : public WriteBatch::Handler {
+ public:
+  WritableFile* dst_;
+  virtual void Put(const Slice& key, const Slice& value) {
+    std::string r = '  put '';
+    AppendEscapedStringTo(&r, key);
+    r += '' '';
+    AppendEscapedStringTo(&r, value);
+    r += ''\n';
+    dst_->Append(r);
+  }
+  virtual void Delete(const Slice& key) {
+    std::string r = '  del '';
+    AppendEscapedStringTo(&r, key);
+    r += ''\n';
+    dst_->Append(r);
+  }
 };
     
-    void EaseBezierAction::setBezierParamer( float p0, float p1, float p2, float p3)
-{
-    _p0 = p0;
-    _p1 = p1;
-    _p2 = p2;
-    _p3 = p3;
-}
+      // Reports dropped bytes to the reporter.
+  // buffer_ must be updated to remove the dropped bytes prior to invocation.
+  void ReportCorruption(uint64_t bytes, const char* reason);
+  void ReportDrop(uint64_t bytes, const Status& reason);
     
-        //
-    // Overrides
-    //
-    /**
-     * @param time In seconds.
-     */
-    virtual void update(float time) override;
-    virtual FlipY* reverse() const override;
-    virtual FlipY* clone() const override;
+      // Backward iteration test
+  {
+    SkipList<Key, Comparator>::Iterator iter(&list);
+    iter.SeekToLast();
+    }
     
-CC_CONSTRUCTOR_ACCESS:
-    FlipY() :_flipY(false) {}
-    virtual ~FlipY() {}
     
-    #endif // __ACTION_CCACTION_MANAGER_H__
-
+    {    Executor::KeepAlive<Executor> ka2 = std::move(ka); // conversion
+    EXPECT_FALSE(ka);
+    EXPECT_TRUE(ka2);
+    EXPECT_EQ(&exec, ka2.get());
+    EXPECT_EQ(1, exec.refCount);
+  }
+    
+    // ============================================================================
+// folly/test/GLogBenchmark.cpp                    relative  time/iter  iters/s
+// ============================================================================
+// skip_overhead                                               36.37ns   27.49M
+// dev_null_log_overhead                                        2.61us  382.57K
+// ============================================================================
+    
+    // clang-format off
+#include <cstddef>
+    
+    
+    {    static constexpr result_type max() {
+      return std::numeric_limits<result_type>::max();
+    }
+  };
     
     /*
- * Update each tick
- * Time is the percentage of the way through the duration
+ * Decode a single unicode code point from UTF-8 byte sequence.
  */
-void PageTurn3D::update(float time)
-{
-    float tt = MAX(0, time - 0.25f);
-    float deltaAy = (tt * tt * 500);
-    float ay = -100 - deltaAy;
+char32_t utf8ToCodePoint(
+    const unsigned char*& p,
+    const unsigned char* const e,
+    bool skipOnError);
     
-    float deltaTheta = sqrtf(time);
-    float theta = deltaTheta > 0.5f ? (float)M_PI_2*deltaTheta : (float)M_PI_2*(1-deltaTheta);
+    #include <folly/Conv.h>
+#include <folly/Exception.h>
+#include <folly/FileUtil.h>
+#include <folly/Format.h>
+#include <folly/ScopeGuard.h>
     
-    float rotateByYAxis = (2-time)* M_PI;
-    
-    float sinTheta = sinf(theta);
-    float cosTheta = cosf(theta);
-    
-    for (int i = 0; i <= _gridSize.width; ++i)
-    {
-        for (int j = 0; j <= _gridSize.height; ++j)
-        {
-            // Get original vertex
-            Vec3 p = getOriginalVertex(Vec2(i ,j));
-            
-            p.x -= getGridRect().origin.x;
-            float R = sqrtf((p.x * p.x) + ((p.y - ay) * (p.y - ay)));
-            float r = R * sinTheta;
-            float alpha = asinf( p.x / R );
-            float beta = alpha / sinTheta;
-            float cosBeta = cosf( beta );
-            
-            // If beta > PI then we've wrapped around the cone
-            // Reduce the radius to stop these points interfering with others
-            if (beta <= M_PI)
-            {
-                p.x = ( r * sinf(beta));
-            }
-            else
-            {
-                // Force X = 0 to stop wrapped
-                // points
-                p.x = 0;
-            }
-    }
-    }
-    }
-    
-        for (const auto& anim : animations)
-    {
-        std::string name = anim.first;
-        ValueMap& animationDict = const_cast<ValueMap&>(anim.second.asValueMap());
-    }
-    
-    #include '2d/CCAtlasNode.h'
-#include 'renderer/CCTextureAtlas.h'
-#include 'base/CCDirector.h'
-#include 'renderer/CCTextureCache.h'
-#include 'renderer/CCRenderer.h'
-#include 'renderer/CCGLProgram.h'
-    
-        
-    // Overrides
-    virtual void draw(Renderer *renderer, const Mat4 &transform, uint32_t flags) override;
-    virtual Texture2D* getTexture() const override;
-    virtual void setTexture(Texture2D *texture) override;
-    virtual bool isOpacityModifyRGB() const override;
-    virtual void setOpacityModifyRGB(bool isOpacityModifyRGB) override;
-    virtual const Color3B& getColor(void) const override;
-    virtual void setColor(const Color3B& color) override;
-    virtual void setOpacity(GLubyte opacity) override;
     /**
-    * @code
-    * When this function bound into js or lua,the parameter will be changed
-    * In js: var setBlendFunc(var src, var dst)
-    * @endcode
-    * @lua NA
-    */
-    virtual void setBlendFunc(const BlendFunc& blendFunc) override;
-    /**
-    * @lua NA
-    */
-    virtual const BlendFunc& getBlendFunc() const override;
+ * @class CanFrame
+ * @brief The class which defines the information to send and receive.
+ */
+struct CanFrame {
+  /// Message id
+  uint32_t id;
+  /// Message length
+  uint8_t len;
+  /// Message content
+  uint8_t data[8];
+  /// Time stamp
+  struct timeval timestamp;
+    }
+    
+      /**
+   * @brief Create a pointer to a specified brand of CAN client. The brand is
+   *        set in the parameter.
+   * @param parameter The parameter to create the CAN client.
+   * @return A pointer to the created CAN client.
+   */
+  std::unique_ptr<CanClient> CreateCANClient(const CANCardParameter &parameter);
+    
+    // buf size must be 8 bytes, every time, we receive only one frame
+ErrorCode EsdCanClient::Receive(std::vector<CanFrame> *const frames,
+                                int32_t *const frame_num) {
+  if (!is_started_) {
+    AERROR << 'Esd can client is not init! Please init first!';
+    return ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED;
+  }
+    }
+    
+    #ifndef MODULES_DRIVERS_CANBUS_CAN_CLIENT_CLIENT_ESD_CAN_CLIENT_H_
+#define MODULES_DRIVERS_CANBUS_CAN_CLIENT_CLIENT_ESD_CAN_CLIENT_H_
+    
+    
+    {
+    {
+    {
+    {}  // namespace can
+}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+
+    
+      if (_card_port > MAX_CAN_PORT || _card_port < 0) {
+    AERROR << 'can port number [' << _card_port << '] is out of the range [0,'
+           << MAX_CAN_PORT << ']';
+    return ErrorCode::CAN_CLIENT_ERROR_BASE;
+  }
+  // open device
+  int32_t ret = bcan_open(_card_port, 0,
+                          5,  // 5ms for rx timeout
+                          5,  // 5ms for tx timeout
+                          &_dev_handler);
+    
+    /*
+TEST(HermesCanClient, send) {
+  CANCardParameter param;
+  param.set_brand(CANCardParameter::HERMES_CAN);
+  param.set_channel_id(CANCardParameter::CHANNEL_ID_ZERO);
+  HermesCanClient hermes_can;
+  EXPECT_TRUE(hermes_can.Init(param));
+    }
+    
+    #include <linux/can.h>
+#include <linux/can/raw.h>
+    
+    #include 'gtest/gtest.h'
+    
+    TEST(MessageManagerTest, GetMutableProtocolDataById) {
+  uint8_t mock_data = 1;
+  MockMessageManager manager;
+  manager.Parse(MockProtocolData::ID, &mock_data, 8);
+  manager.ResetSendMessages();
+  EXPECT_TRUE(manager.GetMutableProtocolDataById(MockProtocolData::ID) !=
+              nullptr);
+    }
