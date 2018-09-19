2260f6f31176366f5f170c6e72a774cb18757e7c
SUNsung <weatherius@gmail.com>
2018-09-19 12:10:34 +0300

    Rewrite [59B7HLznbiIrQ59sNWrlHHECCs5990Cbv8BtQGc59NPk.rb] File in: 2018-09-19 12:10:34

--- ruby/59B7HLznbiIrQ59sNWrlHHECCs5990Cbv8BtQGc59NPk.rb
+++ ruby/59B7HLznbiIrQ59sNWrlHHECCs5990Cbv8BtQGc59NPk.rb
@@ -1,84 +1,114 @@
 
         
-        describe GroupUser do
+          def set_account
+    @account = Account.find_local!(params[:account_username])
+  end
     
-    # def du_upload_video_preview_success
-#  stub_request(:post, 'https://du-itc.itunes.apple.com/upload/app-screenshot-image').
-#      with(body: 'trailer preview...',
-#           headers: {'Accept' => 'application/json, text/plain, */*', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Connection' => 'keep-alive', 'Content-Length' => '12345', 'Content-Type' => 'image/joeg', 'Referrer' => 'https://appstoreconnect.apple.com/WebObjects/iTunesConnect.woa/ra/ng/app/898536088',
-#                     'User-Agent' => 'spaceship', 'X-Apple-Jingle-Correlation-Key' => 'iOS App:AdamId=898536088:Version=0.9.13', 'X-Apple-Upload-Appleid' => '898536088', 'X-Apple-Upload-Contentproviderid' => '1234567', 'X-Apple-Upload-Itctoken' => 'the_sso_token_for_image',
-#                     'X-Apple-Upload-Referrer' => 'https://appstoreconnect.apple.com/WebObjects/iTunesConnect.woa/ra/ng/app/898536088', 'X-Original-Filename' => 'ftl_FAKEMD5_trailer-en-US_preview.jpg'}).
-#      to_return(status: 201, body: du_read_upload_trailer_preview_response_success, headers: {'Content-Type' => 'application/json'})
-# end
+      before_action :set_account
+  before_action :set_statuses
+    
+        def resource_params
+      params.require(:account_moderation_note).permit(
+        :content,
+        :target_account_id
+      )
+    end
+    
+        def check_confirmation
+      if @user.confirmed?
+        flash[:error] = I18n.t('admin.accounts.resend_confirmation.already_confirmed')
+        redirect_to admin_accounts_path
+      end
+    end
+  end
+end
 
     
-          #   # First, stub a failing request
-      #   stub_request(:get, 'https://appstoreconnect.apple.com/testflight/v2/providers/1234/apps/898536088/platforms/ios/trains').
-      #     # to_return(status: 200, body: TunesStubbing.itc_read_fixture_file('build_trains_operation_failed.json'), headers: { 'Content-Type' => 'application/json' }).times(2).
-      #     to_return(status: 200, body: TunesStubbing.itc_read_fixture_file('build_trains.json'), headers: { 'Content-Type' => 'application/json' })
+      def encoded_challenge
+    HTMLEntities.new.encode(params['hub.challenge'])
+  end
     
-              zip_build_products_path = Scan.cache[:zip_build_products_path]
-          Actions.lane_context[SharedValues::SCAN_ZIP_BUILD_PRODUCTS_PATH] = zip_build_products_path if zip_build_products_path
-    
-            mock_test_result_parser = Object.new
-        allow(Scan::TestResultParser).to receive(:new).and_return(mock_test_result_parser)
-        allow(mock_test_result_parser).to receive(:parse_result).and_return({ tests: 100, failures: 0 })
-    
-          def self.fetch_plugins(cache_path)
-        page = 1
-        plugins = []
-        loop do
-          url = 'https://rubygems.org/api/v1/search.json?query=fastlane-plugin-&page=#{page}'
-          puts('RubyGems API Request: #{url}')
-          results = JSON.parse(open(url).read)
-          break if results.count == 0
-    
-        def run
-      program :name, 'cert'
-      program :version, Fastlane::VERSION
-      program :description, 'CLI for \'cert\' - Create new iOS code signing certificates'
-      program :help, 'Author', 'Felix Krause <cert@krausefx.com>'
-      program :help, 'Website', 'https://fastlane.tools'
-      program :help, 'Documentation', 'https://docs.fastlane.tools/actions/cert/'
-      program :help_formatter, :compact
-    
-    (allow process-exec
-  (literal
-    '<%= pod_bin %>'
-    '<%= ruby_bin %>'
-  )
-  (regex
-<% prefixes.each do |prefix| %>
-    #'^<%= prefix %>/*'
-<% end %>
-  )
-)
-    
-    module Pod
-  require 'pathname'
-  require 'tmpdir'
-    
-          def stack
-        UI::ErrorReport.stack
+              # Encodes the authenticator field
+          #
+          # @return [String]
+          def encode_authenticator
+            authenticator.encode
+          end
+        end
       end
-    
-          def roles_array
-        roles.to_a
-      end
-    
-            value_to_evaluate = block || value
-    
-    team = ['Ryan Tomayko', 'Blake Mizerany', 'Simon Rozet', 'Konstantin Haase', 'Zachary Scott']
-desc 'list of contributors'
-task :thanks, ['release:all', :backports] do |t, a|
-  a.with_defaults :release => '#{prev_version}..HEAD',
-    :backports => '#{prev_feature}.0..#{prev_feature}.x'
-  included = `git log --format=format:'%aN\t%s' #{a.release}`.lines.map { |l| l.force_encoding('binary') }
-  excluded = `git log --format=format:'%aN\t%s' #{a.backports}`.lines.map { |l| l.force_encoding('binary') }
-  commits  = (included - excluded).group_by { |c| c[/^[^\t]+/] }
-  authors  = commits.keys.sort_by { |n| - commits[n].size } - team
-  puts authors[0..-2].join(', ') << ' and ' << authors.last,
-    '(based on commits included in #{a.release}, but not in #{a.backports})'
+    end
+  end
 end
     
-          private
+              # Encodes the data
+          #
+          # @return [OpenSSL::ASN1::OctetString]
+          def encode_data(data)
+            OpenSSL::ASN1::OctetString.new(data)
+          end
+        end
+      end
+    end
+  end
+end
+    
+                decode_asn1(asn1)
+          end
+    
+              # Encodes the type field
+          #
+          # @return [OpenSSL::ASN1::Integer]
+          def encode_type
+            bn = OpenSSL::BN.new(type.to_s)
+            int = OpenSSL::ASN1::Integer.new(bn)
+    
+              # Encodes the sname
+          #
+          # @return [String]
+          def encode_sname
+            sname.encode
+          end
+    
+      gem 'bacon'
+  gem 'mocha'
+  gem 'mocha-on-bacon'
+  gem 'prettybacon'
+  gem 'webmock'
+    
+    (allow file-ioctl)
+(allow sysctl-read)
+(allow mach-lookup)
+(allow ipc-posix-shm)
+(allow process-fork)
+(allow system-socket)
+    
+        def self.options
+      [
+        ['--silent', 'Show nothing'],
+      ].concat(super)
+    end
+    
+            # Prints the list of specs & pod cache dirs for a single pod name.
+        #
+        # This output is valid YAML so it can be parsed with 3rd party tools
+        #
+        # @param [Array<Hash>] cache_descriptors
+        #        The various infos about a pod cache. Keys are
+        #        :spec_file, :version, :release and :slug
+        #
+        def print_pod_cache_infos(pod_name, cache_descriptors)
+          UI.puts '#{pod_name}:'
+          cache_descriptors.each do |desc|
+            if @short_output
+              [:spec_file, :slug].each { |k| desc[k] = desc[k].relative_path_from(@cache.root) }
+            end
+            UI.puts('  - Version: #{desc[:version]}')
+            UI.puts('    Type:    #{pod_type(desc)}')
+            UI.puts('    Spec:    #{desc[:spec_file]}')
+            UI.puts('    Pod:     #{desc[:slug]}')
+          end
+        end
+      end
+    end
+  end
+end
