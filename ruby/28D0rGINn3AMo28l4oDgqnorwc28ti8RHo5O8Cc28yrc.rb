fc08087116f17fa5a6658f53ce67adc1b29bfb5b
SUNsung <weatherius@gmail.com>
2018-09-19 04:57:59 +0300

    Rewrite [28D0rGINn3AMo28l4oDgqnorwc28ti8RHo5O8Cc28yrc.rb] File in: 2018-09-19 04:57:58

--- ruby/28D0rGINn3AMo28l4oDgqnorwc28ti8RHo5O8Cc28yrc.rb
+++ ruby/28D0rGINn3AMo28l4oDgqnorwc28ti8RHo5O8Cc28yrc.rb
@@ -1,66 +1,69 @@
 
         
-          class FeatureTopicUsers < Jobs::Base
-    
-        def form_configurable_attributes
-      form_configurable_fields.keys
+            if DOC_REDIRECTS.key?(doc)
+      return redirect '/#{DOC_REDIRECTS[doc]}#{type}#{rest}', 301
     end
-  end
-end
-
     
-      def tumblr_oauth_token_secret
-    service.secret
-  end
+      extend Instrumentable
     
-      it 'evaluates elsif-body in containing scope' do
-    a = 123
-    if false
-      b = a+1
-    elsif false
-      b = a+2
-    elsif true
-      b = a+3
-    else
-      b = a+4
+        def initialize(options = {})
+      @request_options = options.extract!(:request_options)[:request_options].try(:dup) || {}
+      options[:max_concurrency] ||= 20
+      options[:pipelining] = false
+      super
     end
-    b.should == 126
-  end
-end
-
     
-      it 'returns a Time with a UTC offset specified as -HH:MM' do
-    t = Time.gm(2007, 1, 9, 12, 0, 0)
-    t.localtime('-01:00')
-    t.should == Time.new(2007, 1, 9, 11, 0, 0, -3600)
-    t.utc_offset.should == -3600
-  end
+    require 'active_support/subscriber'
     
-        self
-  end
+        def subpath_to(url, options = nil)
+      url = self.class.parse(url)
+      return unless origin == url.origin
     
-        Tempfile.create(name.to_s) {|f|
-      f.sync = true
-      expected = ''
-      result = nil
-      bytes[0...-1].each do |x|
-        f.write x
-        f.write ' '
-        f.pos -= 1
-        expected << x
-        assert_nothing_raised(bug6487) {result = File.read(f.path, mode: 'rb:bom|utf-8')}
-        assert_equal('#{expected} '.force_encoding('utf-8'), result)
+              node['data-language'] = 'typescript' if node['path'].try(:ends_with?, '.ts')
+          node['data-language'] = 'html' if node['path'].try(:ends_with?, '.html')
+          node['data-language'] = 'css' if node['path'].try(:ends_with?, '.css')
+          node['data-language'] = 'js' if node['path'].try(:ends_with?, '.js')
+          node['data-language'] = 'json' if node['path'].try(:ends_with?, '.json')
+          node['data-language'] = node['language'].sub(/\Ats/, 'typescript').strip if node['language']
+          node['data-language'] ||= 'typescript' if node.content.start_with?('@')
+    
+    require 'sass/version'
+    
+    module Sass
+  module CacheStores
+    # A backend for the Sass cache using the filesystem.
+    class Filesystem < Base
+      # The directory where the cached files will be stored.
+      #
+      # @return [String]
+      attr_accessor :cache_location
+    
+        # The selector for the current CSS rule, or nil if there is no
+    # current CSS rule.
+    #
+    # @return [Selector::CommaSequence?] The current selector, with any
+    #   nesting fully resolved.
+    def selector
+      @selector || (@caller && @caller.selector) || (@parent && @parent.selector)
+    end
+    
+          # A string representation of the importer.
+      # Should be overridden by subclasses.
+      #
+      # This is used to help debugging,
+      # and should usually just show the load path encapsulated by this importer.
+      #
+      # @return [String]
+      def to_s
+        Sass::Util.abstract(self)
       end
-      f.write bytes[-1]
-      assert_nothing_raised(bug6487) {result = File.read(f.path, mode: 'rb:bom|utf-8')}
-      assert_equal '', result, 'valid bom'
-    }
-  end
     
-      it 'decodes the remaining doubles when passed the '*' modifier' do
-    array = '@\x07333333?\xf6ffffff@\x20ffffff'.unpack(unpack_format('*'))
-    array.should == [2.9, 1.4, 8.2]
-  end
-    
-    fileOutJar 	= clsFile.new_with_sig('Ljava.lang.String;', 'output.jar')
-filesIn		= Array.new
+          # Creates a new filesystem importer that imports files relative to a given path.
+      #
+      # @param root [String] The root path.
+      #   This importer will import files relative to this path.
+      def initialize(root)
+        @root = File.expand_path(root)
+        @real_root = Sass::Util.realpath(@root).to_s
+        @same_name_warnings = Set.new
+      end
