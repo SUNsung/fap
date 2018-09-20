c4dae96ebd056872d35d2941ed19ae41a7262da3
SUNsung <weatherius@gmail.com>
2018-09-20 12:42:26 +0300

    Rewrite [28ai8SC50Sy8Q28VrVmKTnhL9s28AXG7TGkxooY28cxu.rb] File in: 2018-09-20 12:42:25

--- ruby/28ai8SC50Sy8Q28VrVmKTnhL9s28AXG7TGkxooY28cxu.rb
+++ ruby/28ai8SC50Sy8Q28VrVmKTnhL9s28AXG7TGkxooY28cxu.rb
@@ -1,15 +1,162 @@
 
         
-        $z = $x.keys.sort.join(':')
-test_ok($z == '1:2:3')
+            def initialize
+      @root = nil
+      super
+    end
     
-        def recheck
-      pod = Pod.find(params[:pod_id])
-      pod.test_connection!
-    
-          # Inspired by https://github.com/nov/openid_connect_sample/blob/master/app/controllers/connect/clients_controller.rb#L24
-      def create
-        registrar = OpenIDConnect::Client::Registrar.new(request.url, params)
-        client = Api::OpenidConnect::OAuthApplication.register! registrar
-        render json: client.as_json(root: false)
+          if ARGV.git?
+        system 'git', 'init'
+        system 'git', 'add', '-A'
       end
+      if ARGV.interactive?
+        ohai 'Entering interactive mode'
+        puts 'Type `exit' to return and finalize the installation'
+        puts 'Install to this prefix: #{formula.prefix}'
+    
+        keys.each do |key|
+      value = env[key]
+      s = '#{key}: #{value}'
+      case key
+      when 'CC', 'CXX', 'LD'
+        s << ' => #{Pathname.new(value).realpath}' if File.symlink?(value)
+      end
+      f.puts s
+    end
+  end
+end
+
+    
+      def caveats
+    caveats = []
+    begin
+      build, f.build = f.build, Tab.for_formula(f)
+      s = f.caveats.to_s
+      caveats << s.chomp + '\n' if s.length > 0
+    ensure
+      f.build = build
+    end
+    caveats << keg_only_text
+    caveats << bash_completion_caveats
+    caveats << zsh_completion_caveats
+    caveats << fish_completion_caveats
+    caveats << plist_caveats
+    caveats << python_caveats
+    caveats << app_caveats
+    caveats << elisp_caveats
+    caveats.compact.join('\n')
+  end
+    
+        if $stdout.tty?
+      metacharacters = %w[\\ | ( ) [ ] { } ^ $ * + ? .]
+      bad_regex = metacharacters.any? do |char|
+        ARGV.any? do |arg|
+          arg.include?(char) && !arg.start_with?('/')
+        end
+      end
+      if ARGV.any? && bad_regex
+        ohai 'Did you mean to perform a regular expression search?'
+        ohai 'Surround your query with /slashes/ to search by regex.'
+      end
+    end
+    
+      def python(_options = {}, &block)
+    opoo 'Formula#python is deprecated and will go away shortly.'
+    block.call if block_given?
+    PythonRequirement.new
+  end
+  alias_method :python2, :python
+  alias_method :python3, :python
+end
+
+    
+      # Use this method to generate standard caveats.
+  def standard_instructions(home_name, home_value = libexec)
+    <<-EOS.undent
+      Before you can use these tools you must export some variables to your $SHELL.
+    
+              it 'cuts fraction part to have only 6 digits at all' do
+            format('%#{f}', 1.1234567).should == '1.12346'
+            format('%#{f}', 12.1234567).should == '12.1235'
+            format('%#{f}', 123.1234567).should == '123.123'
+            format('%#{f}', 1234.1234567).should == '1234.12'
+            format('%#{f}', 12345.1234567).should == '12345.1'
+            format('%#{f}', 123456.1234567).should == '123456'
+          end
+        end
+    
+    describe 'Kernel#srand' do
+  it 'needs to be reviewed for spec completeness'
+end
+
+    
+      desc 'Updates the last know version of CocoaPods in the specs repo'
+  task :post_release do
+    title 'Updating last known version in Specs repo'
+    specs_branch = 'master'
+    Dir.chdir('../Specs') do
+      puts Dir.pwd
+      sh 'git checkout #{specs_branch}'
+      sh 'git pull'
+    
+      def developer_prefix
+    `xcode-select --print-path`.strip
+  end
+    
+    abstract_target 'Abstract Target' do
+    use_modular_headers!
+    
+            def initialize(argv)
+          @pod_name = argv.shift_argument
+          @wipe_all = argv.flag?('all')
+          super
+        end
+    
+            private
+    
+          def ask_question
+        $stdout.print question
+        $stdout.flush
+      end
+    
+          def load_built_in_scm
+        require 'capistrano/scm/#{scm_name}'
+        scm_class = Object.const_get(built_in_scm_plugin_class_name)
+        # We use :load_immediately because we are initializing the SCM plugin
+        # late in the load process and therefore can't use the standard
+        # load:defaults technique.
+        install_plugin(scm_class, load_immediately: true)
+      end
+    
+    ::Bundler.with_friendly_errors do
+  ::Bundler::CLI.start(ARGV, :debug => true)
+end
+
+    
+      require ARGV.shift
+  exit_status = LogStash::Runner.run('bin/logstash', ARGV)
+  exit(exit_status || 0)
+end
+
+    
+        # Add plugins/gems to the current gemfile
+    puts('Installing' + (install_list.empty? ? '...' : ' ' + install_list.collect(&:first).join(', ')))
+    install_list.each do |plugin, version, options|
+      if preserve?
+        plugin_gem = gemfile.find(plugin)
+        puts('Preserving Gemfile gem options for plugin #{plugin}') if plugin_gem && !plugin_gem.options.empty?
+        gemfile.update(plugin, version, options)
+      else
+        gemfile.overwrite(plugin, version, options)
+      end
+    end
+    
+      gem.add_runtime_dependency 'logstash-core', LOGSTASH_CORE_VERSION.gsub('-', '.')
+    
+          it 'list the plugin with his version' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list --verbose #{plugin_name}')
+        expect(result).to run_successfully_and_output(/^#{plugin_name} \(\d+\.\d+.\d+\)/)
+      end
+    end
+  end
+end
