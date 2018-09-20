58a3cacb712ea2996c22a3a47bdea14ac69ea9f9
SUNsung <weatherius@gmail.com>
2018-09-20 06:41:21 +0300

    Rewrite [39UChgc.keD5c39I7q888hxspQ39hOgycteb6yE396I1.cpp] File in: 2018-09-20 06:41:21

--- cccccc/39UChgc.keD5c39I7q888hxspQ39hOgycteb6yE396I1.cpp
+++ cccccc/39UChgc.keD5c39I7q888hxspQ39hOgycteb6yE396I1.cpp
@@ -1,189 +1,180 @@
 
         
-        template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10>
-internal::ValueArray10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> Values(T1 v1,
-    T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10) {
-  return internal::ValueArray10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(v1,
-      v2, v3, v4, v5, v6, v7, v8, v9, v10);
-}
-    
-    #endif  // GTEST_INCLUDE_GTEST_GTEST_PARAM_TEST_H_
+        #endif  // ATOM_APP_COMMAND_LINE_ARGS_H_
 
     
-      template <GTEST_9_TYPENAMES_(U)>
-  tuple& operator=(const GTEST_9_TUPLE_(U)& t) {
-    return CopyFrom(t);
+    UvTaskRunner::~UvTaskRunner() {
+  for (auto& iter : tasks_) {
+    uv_unref(reinterpret_cast<uv_handle_t*>(iter.first));
+    delete iter.first;
   }
-    
-    
-    {    return true;
-  }
-    
-    // A sample program demonstrating using Google C++ testing framework.
-//
-// Author: wan@google.com (Zhanyong Wan)
-    
-      bool ApplyToCall(grpc_call* call) override;
-  SecureCallCredentials* AsSecureCredentials() override { return this; }
-    
-    void ChannelArguments::SetString(const grpc::string& key,
-                                 const grpc::string& value) {
-  grpc_arg arg;
-  arg.type = GRPC_ARG_STRING;
-  strings_.push_back(key);
-  arg.key = const_cast<char*>(strings_.back().c_str());
-  strings_.push_back(value);
-  arg.value.string = const_cast<char*>(strings_.back().c_str());
-    }
-    
-    grpc::string ChannelArguments::GetSslTargetNameOverride() const {
-  for (unsigned int i = 0; i < args_.size(); i++) {
-    if (grpc::string(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG) == args_[i].key) {
-      return args_[i].value.string;
-    }
-  }
-  return '';
 }
     
     
-    {}  // namespace grpc
-
+    {}  // namespace atom
     
-      static void OnDoneRecvMessageCb(void* user_data, grpc_error* error);
+    #include 'atom/browser/native_window_views.h'
     
-    // Server
-MeasureDouble RpcServerSentBytesPerRpc() {
-  static const auto measure = MeasureDouble::Register(
-      kRpcServerSentBytesPerRpcMeasureName,
-      'Total bytes sent across all messages per RPC', kUnitBytes);
-  return measure;
+    void Initialize(v8::Local<v8::Object> exports,
+                v8::Local<v8::Value> unused,
+                v8::Local<v8::Context> context,
+                void* priv) {
+  auto controller = base::Unretained(TracingController::GetInstance());
+  mate::Dictionary dict(context->GetIsolate(), exports);
+  dict.SetMethod('getCategories',
+                 base::Bind(&TracingController::GetCategories, controller));
+  dict.SetMethod('startRecording',
+                 base::Bind(&TracingController::StartTracing, controller));
+  dict.SetMethod('stopRecording', &StopRecording);
+  dict.SetMethod(
+      'getTraceBufferUsage',
+      base::Bind(&TracingController::GetTraceBufferUsage, controller));
 }
     
+    void Initialize(v8::Local<v8::Object> exports,
+                v8::Local<v8::Value> unused,
+                v8::Local<v8::Context> context,
+                void* priv) {
+  v8::Isolate* isolate = context->GetIsolate();
+  mate::Dictionary(isolate, exports)
+      .Set('DownloadItem',
+           atom::api::DownloadItem::GetConstructor(isolate)->GetFunction());
+}
     
-    {}  // namespace grpc
-
+    std::unique_ptr<views::LayoutManager> LayoutManager::TakeOver() {
+  if (!managed_by_us_)  // already taken over.
+    return nullptr;
+  managed_by_us_ = false;
+  return std::unique_ptr<views::LayoutManager>(layout_manager_);
+}
     
-      CensusServerCallData()
-      : gc_(nullptr),
-        auth_context_(nullptr),
-        recv_initial_metadata_(nullptr),
-        initial_on_done_recv_initial_metadata_(nullptr),
-        initial_on_done_recv_message_(nullptr),
-        recv_message_(nullptr),
-        recv_message_count_(0),
-        sent_message_count_(0) {
-    memset(&census_bin_, 0, sizeof(grpc_linked_mdelem));
-    memset(&path_, 0, sizeof(grpc_slice));
-    memset(&on_done_recv_initial_metadata_, 0, sizeof(grpc_closure));
-    memset(&on_done_recv_message_, 0, sizeof(grpc_closure));
-  }
-    
-    namespace HPHP { namespace HHBBC {
-    }
-    }
-    
-    //////////////////////////////////////////////////////////////////////
-    
-      // Parse and process a .hdf string (e.g., -v)
-  static void ParseHdfString(const std::string &hdfStr, Hdf &hdf);
-    
-    namespace HPHP {
-///////////////////////////////////////////////////////////////////////////////n
+    void Initialize(v8::Local<v8::Object> exports,
+                v8::Local<v8::Value> unused,
+                v8::Local<v8::Context> context,
+                void* priv) {
+  v8::Isolate* isolate = context->GetIsolate();
     }
     
-        if ((se.st_mode & S_IFMT) == S_IFDIR) {
-      find(root, spath + ename, php, callback);
-      continue;
-    }
-    
-            if (!learnerParametersNotPartOfModel.empty())
-            InvalidArgument('Trainer ctor: %d of the learner parameters '%S' are not part of the model specified', 
-                            (int)learnerParametersNotPartOfModel.size(), NamedListString(learnerParametersNotPartOfModel).c_str());
-    
-    #include 'stdafx.h'
-#include 'CNTKLibrary.h'
-#include 'Sequences.h'
-#include 'TensorView.h'
-#include 'Utils.h'
-    
-        NDArrayViewPtr Variable::Value() const
+        /*virtual*/ void LearnerSGD::Update(const Parameter& parameter, const NDArrayViewPtr& gradientValue, 
+                                        const NDArrayViewPtr& smoothedGradientValue, size_t trainingSampleCount) /*override*/
     {
-        if (!IsConstant() && !IsParameter())
-            LogicError('Variable '%S' Value(): Only Variables of kind Parameter and Constant have a Value.', AsString().c_str());
+        DISPATCH_TO_TYPED_UPDATE_FUNCTION;
     }
     
-                // Validate that each of the dynamic axes are unique
-            std::unordered_set<Axis> uniqueDynamicAxis;
-            for (auto& currentDynamicAxis : dynamicAxes)
-            {
-                auto retVal = uniqueDynamicAxis.insert(currentDynamicAxis);
-                if (!retVal.second)
-                    InvalidArgument('Dynamic axis named %S is specified more than once for Variable '%S'', currentDynamicAxis.Name().c_str(), AsString().c_str());
-            }
+        private:
+        std::unordered_set<StreamInformation> m_streamInfos;
+        bool m_epochEndReached;
+        size_t m_numWorkers;
+        size_t m_workerRank;
+        size_t m_prevMinibatchSize;
+        size_t m_maxNumSamplesToRead;
+        size_t m_maxNumSweepsToRead;
+        size_t m_truncationLength;
+        size_t m_maxErrors;
+        std::unordered_map<StreamInformation, MinibatchData> m_minibatchData;
     
-    // GetMinibatch4SE - Get the next minibatch for SE training, including lattice, labels and phone boundary
-// latticeinput - lattice for each utterances in this minibatch
-// uids - lables stored in size_t vector instead of ElemType matrix
-// boundary - phone boundaries
-// returns - true if there are more minibatches, false if no more minibatches remain
-bool DataReader::GetMinibatch4SE(std::vector<shared_ptr<const msra::dbn::latticepair>>& latticeinput, vector<size_t>& uids, vector<size_t>& boundaries, vector<size_t>& extrauttmap)
+            m_aggregatedTrainingLossValue->Update(loss, computeDevice);
+     
+        if (m_aggregatedTrainingEvalCriterionValue)
+        {
+            m_aggregatedTrainingEvalCriterionValue->Update(evalCriterion, computeDevice);
+        }
+    
+        // Acquires the mutex. If 'wait' is true and mutex is acquired by someone else then
+    // function waits until mutex is released
+    // Returns false if !wait and lock cannot be acquired, or in case of a system error that prevents us from acquiring the lock.
+    bool Acquire(bool wait)
+    {
+        assert(m_handle == NULL);
+        m_handle = ::CreateMutexA(NULL /*security attr*/, FALSE /*bInitialOwner*/, m_name.c_str());
+        if (m_handle == NULL)
+        {
+            if (!wait)
+                return false;   // can't lock due to access permissions: lock already exists, consider not available
+            else
+                RuntimeError('Acquire: Failed to create named mutex %s: %d.', m_name.c_str(), GetLastError());
+        }
+    }
+    
+    // some older code uses this namespace
+namespace DebugUtil
 {
-    bool bRet = true;
-    for (size_t i = 0; i < m_ioNames.size(); i++)
-        bRet &= m_dataReaders[m_ioNames[i]]->GetMinibatch4SE(latticeinput, uids, boundaries, extrauttmap);
-    return bRet;
-}
-    
-            // take the chance to validate inputNodes
-        let allInputsSet = set<ComputationNodeBasePtr>(allInputs.begin(), allInputs.end());
-        for (let& input : inputNodes)
-            if (allInputsSet.find(input) == allInputsSet.end())
-                InvalidArgument('CloneFunction: No specified output depends on the specified input %ls.', input->NodeDescription().c_str());
-        // TODO: Is this really always an error? Are there valid cases where one would over-specify possible input nodes, even if they are not used/needed?
-    
-      /*
-   * @brief the uri used to begin POSTing carve data
-   *
-   * This endpoint should negotiate the details of the carve, as well
-   * as give the client a session id used to continue POSTing the data.
-   */
-  std::string startUri_;
-    
-    namespace osquery {
+    void PrintCallStack(size_t skipLevels = 0, bool makeFunctionNamesStandOut = false);
     }
     
-    #include <osquery/config.h>
-#include <osquery/registry.h>
+    
+    {        Matrix<ElemType>::VectorSum(sliceInputValue, sliceOutputValue, true);
+    }
     
     
-    {  // Any views left are views that don't exist in the new configuration file
-  // so we tear them down and remove them from the database.
-  for (const auto& old_view : erase_views) {
-    osquery::query('DROP VIEW ' + old_view, r);
-    deleteDatabaseValue(kQueries, kConfigViews + old_view);
+    {
+    {
+    {} } }
+
+    
+    
+    {        SetDims(sampleLayout, HasMBLayout()); // also called when reloading a file. Then we have an MBLayout, otherwise not yet
+        UpdateFunctionValuesSize();           // we must allocate the matrix so that the readers get objects with valid row dimensions (some readers expect that)
+        SetLearningRateMultiplier(learningRateMultiplier);
+        m_dynamicAxisNodeName = axisName;
+    }
+    
+    #include <errno.h>
+#include <signal.h>
+    
+      std::set<std::string> queries_to_run;
+  // Check for and run discovery queries first
+  if (doc.doc().HasMember('discovery')) {
+    const auto& queries = doc.doc()['discovery'];
+    assert(queries.IsObject());
+    }
+    
+        // Use the basic 'force' flag to check implicit SQL usage.
+    auto flags =
+        SQL('select default_value from osquery_flags where name = 'force'');
+    if (flags.rows().size() > 0) {
+      r['flag_test'] = flags.rows().back().at('default_value');
+    }
+    
+    DHTReplaceNodeTask::DHTReplaceNodeTask(const std::shared_ptr<DHTBucket>& bucket,
+                                       const std::shared_ptr<DHTNode>& newNode)
+    : bucket_(bucket),
+      newNode_(newNode),
+      numRetry_(0),
+      timeout_(DHT_MESSAGE_TIMEOUT)
+{
+}
+    
+    
+    {} // namespace aria2
+    
+    namespace aria2 {
+    }
+    
+    class DownloadContext;
+class PeerStorage;
+class DHTTask;
+class DHTNode;
+class DHTBucket;
+    
+    
+    {} // namespace aria2
+    
+    
+    {} // namespace aria2
+    
+      std::string generateToken(const unsigned char* infoHash,
+                            const std::string& ipaddr, uint16_t port,
+                            const unsigned char* secret) const;
+    
+    DNSCache::CacheEntry& DNSCache::CacheEntry::operator=(const CacheEntry& c)
+{
+  if (this != &c) {
+    hostname_ = c.hostname_;
+    port_ = c.port_;
+    addrEntries_ = c.addrEntries_;
   }
-  return Status(0, 'OK');
+  return *this;
 }
     
-        // Create a basic path trigger, this is a file path.
-    real_test_path = kTestWorkingDirectory + 'inotify-trigger' +
-                     std::to_string(rand() % 10000 + 10000);
-    // Create a similar directory for embedded paths and directories.
-    real_test_dir = kTestWorkingDirectory + 'inotify-triggers' +
-                    std::to_string(rand() % 10000 + 10000);
-    
-    namespace benchmark {
-namespace internal {
-// The arraysize(arr) macro returns the # of elements in an array arr.
-// The expression is a compile-time constant, and therefore can be
-// used in defining new arrays, for example.  If you use arraysize on
-// a pointer by mistake, you will get a compile-time error.
-//
-    }
-    }
-    
-    #include <cstdint>
-#include <string>
-    
-    #include 'check.h'
+    #endif // defined(BOOST_ATOMIC_DETAIL_X86_HAS_CMPXCHG16B)
