b877d7ea66ef4347b5d5459b1e1e327af786ac6f
SUNsung <weatherius@gmail.com>
2018-09-20 13:49:17 +0300

    Rewrite [57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py] File in: 2018-09-20 13:49:16

--- pyton/57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py
+++ pyton/57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py
@@ -1,89 +1,69 @@
 
         
-        
-class MockMaster(object):
+            def __init__(self, seller_category_map, seller_category_overrides_map):
+        self.seller_category_map = seller_category_map
+        self.seller_category_overrides_map = seller_category_overrides_map
+    
+    from mrjob.job import MRJob
+    
+        def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.friend_ids = []
+    
+        def get(self, key):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                return item.value
+        raise KeyError('Key not found')
+    
+            def get_auth(self, username=None, password=None):
+            assert self.raw_auth == USERNAME + SEP_CREDENTIALS + PASSWORD
+            assert username == USERNAME
+            assert password == PASSWORD
+            return basic_auth()
+    
+        def test_binary_included_and_correct_when_suitable(self):
+        env = MockEnvironment(stdin_isatty=True, stdout_isatty=False)
+        r = http('GET', self.url, env=env)
+        assert r == self.bindata
+
+    
+    logging.getLogger(__name__).addHandler(NullHandler())
+    
+    Available hooks:
     
     
-class DragnnModelSaverLibTest(test_util.TensorFlowTestCase):
+@pytest.mark.skipif(sys.version_info < (2,7), reason='Only run on Python 2.7+')
+def test_system_ssl():
+    '''Verify we're actually setting system_ssl when it should be available.'''
+    assert info()['system_ssl']['version'] != ''
     
-        try:
-      with open(FLAGS.expected_file) as expected:
-        content_expected = expected.read()
-    except IOError as e:
-      self.fail('Error opening '%s': %s' % (FLAGS.expected_file, e.strerror))
-    
-        This constructs a separately managed pipeline for a given target:
-    it has its own ComputeSession, InputSpec placeholder, etc. The ops
-    are given standardized names to allow access from the C++ API. It
-    passes the values in target_config to build_training() above.
-    
-            with Server.basic_response_server(wait_to_close_event=block_server) as (host, port):
-            sock = socket.socket()
-            sock.connect((host, port))
-            sock.sendall(b'send something')
-            time.sleep(2.5)
-            sock.sendall(b'still alive')
-            block_server.set()  # release server block
-    
-    
-def test_idna_without_version_attribute(mocker):
-    '''Older versions of IDNA don't provide a __version__ attribute, verify
-    that if we have such a package, we don't blow up.
-    '''
-    mocker.patch('requests.help.idna', new=None)
-    assert info()['idna'] == {'version': ''}
-    
-    
-class UnrewindableBodyError(RequestException):
-    '''Requests encountered an error when trying to rewind a body'''
-    
-    import idna
-import urllib3
-import chardet
-    
-        def iteritems(self):
-        '''Dict-like iteritems() that returns an iterator of name-value tuples
-        from the jar.
-    
-    # Declare top-level shortcuts
-from scrapy.spiders import Spider
-from scrapy.http import Request, FormRequest
-from scrapy.selector import Selector
-from scrapy.item import Item, Field
-    
-            # Delay queue processing if a download_delay is configured
-        now = time()
-        delay = slot.download_delay()
-        if delay:
-            penalty = delay - now + slot.lastseen
-            if penalty > 0:
-                slot.latercall = reactor.callLater(penalty, self._process_queue, spider, slot)
-                return
-    
-    
-def _get_boto_connection():
-    from boto.s3.connection import S3Connection
-    
-            @defer.inlineCallbacks
-        def process_response(response):
-            assert response is not None, 'Received None in process_response'
-            if isinstance(response, Request):
-                defer.returnValue(response)
-    
-        def gotHeaders(self, headers):
-        self.headers_time = time()
-        self.response_headers = headers
-    
-        def process_response(self, request, response, spider):
-    
-        @classmethod
-    def from_crawler(cls, crawler):
-        o = cls(crawler.settings['USER_AGENT'])
-        crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
-        return o
-    
-    class ScrapyDeprecationWarning(Warning):
-    '''Warning category for deprecated features, since the default
-    DeprecationWarning is silenced on Python 2.7+
-    '''
-    pass
+            :param proxy: The proxy to return a urllib3 ProxyManager for.
+        :param proxy_kwargs: Extra keyword arguments used to configure the Proxy Manager.
+        :returns: ProxyManager
+        :rtype: urllib3.ProxyManager
+        '''
+        if proxy in self.proxy_manager:
+            manager = self.proxy_manager[proxy]
+        elif proxy.lower().startswith('socks'):
+            username, password = get_auth_from_url(proxy)
+            manager = self.proxy_manager[proxy] = SOCKSProxyManager(
+                proxy,
+                username=username,
+                password=password,
+                num_pools=self._pool_connections,
+                maxsize=self._pool_maxsize,
+                block=self._pool_block,
+                **proxy_kwargs
+            )
+        else:
+            proxy_headers = self.proxy_headers(proxy)
+            manager = self.proxy_manager[proxy] = proxy_from_url(
+                proxy,
+                proxy_headers=proxy_headers,
+                num_pools=self._pool_connections,
+                maxsize=self._pool_maxsize,
+                block=self._pool_block,
+                **proxy_kwargs)
