3d5d85fe0d693e250611a15687a53f1c82c3efb0
SUNsung <weatherius@gmail.com>
2018-09-20 17:44:37 +0300

    Rewrite [43VTf97iRGnnE43tGxfPSzt5vw43dXrAdlGrWT243eFx.cpp] File in: 2018-09-20 17:44:37

--- cccccc/43VTf97iRGnnE43tGxfPSzt5vw43dXrAdlGrWT243eFx.cpp
+++ cccccc/43VTf97iRGnnE43tGxfPSzt5vw43dXrAdlGrWT243eFx.cpp
@@ -1,153 +1,241 @@
 
         
-            enum BORDER_MODE
-    {
-        BORDER_MODE_UNDEFINED,
-        BORDER_MODE_CONSTANT,
-        BORDER_MODE_REPLICATE,
-        BORDER_MODE_REFLECT,
-        BORDER_MODE_REFLECT101,
-        BORDER_MODE_WRAP
-    };
+        #endif  // GTEST_INCLUDE_GTEST_GTEST_MESSAGE_H_
+
     
     
-    {} // namespace
-    
-    template<typename T>
-struct OpCmpGT
-{
-    typedef T type;
-    }
-    
-            int16x8_t buv = (int16x8_t)vmlsl_u8(convertYUV420Internals.vc17672, raw_uv.val[1-vIdx], convertYUV420Internals.vc129); //(17672-129*u)
-        int16x8_t guv = (int16x8_t)vmlsl_u8(gu,      raw_uv.val[vIdx], convertYUV420Internals.vc52);  //((8696-25*u)-52*v))
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 7 && !defined(__clang__)
-CVTS_FUNC(f32, s32, 8,
-    register float32x4_t vscale asm ('q0') = vdupq_n_f32((f32)alpha);
-    register float32x4_t vshift asm ('q1') = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        __asm__ (
-            'vld1.32 {d4-d5}, [%[src1]]                              \n\t'
-            'vld1.32 {d6-d7}, [%[src2]]                              \n\t'
-            'vmul.f32 q4, q2, q0                                     \n\t'
-            'vmul.f32 q5, q3, q0                                     \n\t'
-            'vadd.f32 q6, q4, q1                                     \n\t'
-            'vadd.f32 q7, q5, q1                                     \n\t'
-            'vcvt.s32.f32 q4, q6                                     \n\t'
-            'vcvt.s32.f32 q5, q7                                     \n\t'
-            'vst1.32 {d8-d9},   [%[dst1]]                            \n\t'
-            'vst1.32 {d10-d11}, [%[dst2]]                            \n\t'
-            : //no output
-            : [src1] 'r' (_src + i),
-              [src2] 'r' (_src + i + 4),
-              [dst1] 'r' (_dst + i),
-              [dst2] 'r' (_dst + i + 4),
-              'w' (vscale), 'w' (vshift)
-            : 'd4','d5','d6','d7','d8','d9','d10','d11','d12','d13','d14','d15'
-        );
-    }
-})
-#else
-CVTS_FUNC(f32, s32, 8,
-    float32x4_t vscale = vdupq_n_f32((f32)alpha);
-    float32x4_t vshift = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        float32x4_t vline1_f32 = vld1q_f32(_src + i + 0);
-        float32x4_t vline2_f32 = vld1q_f32(_src + i + 4);
-        vline1_f32 = vmulq_f32(vline1_f32, vscale);
-        vline2_f32 = vmulq_f32(vline2_f32, vscale);
-        vline1_f32 = vaddq_f32(vline1_f32, vshift);
-        vline2_f32 = vaddq_f32(vline2_f32, vshift);
-        int32x4_t vline1_s32 = vcvtq_s32_f32(vline1_f32);
-        int32x4_t vline2_s32 = vcvtq_s32_f32(vline2_f32);
-        vst1q_s32(_dst + i + 0, vline1_s32);
-        vst1q_s32(_dst + i + 4, vline2_s32);
-    }
-})
-#endif
-    
-                    vec128 v_mask = vtstq(v_src1,v_src1);
-                internal::vst1q(dst + j, internal::vandq(v_mask, recipWrapQ(v_src1, scale)));
-            }
-            for (; j < roiw64; j += step64)
-            {
-                vec64 v_src1 = internal::vld1(src1 + j);
-    
-            if(i + 2 <= size.width)
-        {
-            float32x2_t vres = vmul_f32(vld1_f32(src0 + i), vld1_f32(src1 + i));
-            result += vget_lane_f32(vres, 0) + vget_lane_f32(vres, 1);
-            i += 2;
-        }
-    
-                    int8x16_t x0 = vreinterpretq_s8_u8(vsubq_u8(vld1q_u8(ptr + pixel[0]), delta));
-                int8x16_t x1 = vreinterpretq_s8_u8(vsubq_u8(vld1q_u8(ptr + pixel[4]), delta));
-                int8x16_t x2 = vreinterpretq_s8_u8(vsubq_u8(vld1q_u8(ptr + pixel[8]), delta));
-                int8x16_t x3 = vreinterpretq_s8_u8(vsubq_u8(vld1q_u8(ptr + pixel[12]), delta));
-    
-    inline void vnst(u8* dst, uint8x16_t v1, uint8x16_t v2) { vst1q_u8(dst, v1); vst1q_u8(dst+16, v2); }
-inline void vnst(u8* dst, uint16x8_t v1, uint16x8_t v2) { vst1q_u8(dst, vcombine_u8(vmovn_u16(v1), vmovn_u16(v2))); }
-inline void vnst(u8* dst, uint32x4_t v1, uint32x4_t v2) { vst1_u8(dst, vmovn_u16(vcombine_u16(vmovn_u32(v1), vmovn_u32(v2)))); }
-    
-    
-    {            prev = vaddw_u16(prev, vdup_lane_u16(el4h, 3));
-        }
-    
-    template <>
-template <typename T>
-bool EnforceFiniteOp<CPUContext>::DoRunWithType() {
-  EnforceOnCPU<T>(Input(0));
-  return true;
-}
-    
-    workspace.FeedBlob('data', np.zeros((1,1,100,100)).astype(np.float32))
-print('data.shape:', workspace.FetchBlob('data').shape)
-    
-    #ifndef CAFFE2_OPERATORS_FLEXIBLE_TOP_K_H_
-#define CAFFE2_OPERATORS_FLEXIBLE_TOP_K_H_
-    
-    		GLuint polygon_buffer;
-		GLuint polygon_buffer_quad_arrays[4];
-		GLuint polygon_buffer_pointer_array;
-		GLuint polygon_index_buffer;
-    
-    #ifndef TCP_SERVER_POSIX_H
-#define TCP_SERVER_POSIX_H
-    
-    #ifdef WINDOWS_USE_MUTEX
-	CloseHandle(mutex);
-#else
-    
-    #ifdef WINDOWS_USE_MUTEX
-	HANDLE mutex;
-#else
-	CRITICAL_SECTION mutex;
-#endif
-    
-    
-    {	return OK;
+    {  virtual void ReportTestPartResult(const TestPartResult& result) = 0;
 };
     
-    #ifdef WINDOWS_ENABLED
+    // Adds reference to a type if it is not a reference type,
+// otherwise leaves it unchanged.  This is the same as
+// tr1::add_reference, which is not widely available yet.
+template <typename T>
+struct AddReference { typedef T& type; };  // NOLINT
+template <typename T>
+struct AddReference<T&> { typedef T& type; };  // NOLINT
     
-    DHTRoutingTableSerializer::DHTRoutingTableSerializer(int family)
-    : family_(family)
-{
+    #endif  // GTEST_INCLUDE_GTEST_INTERNAL_GTEST_STRING_H_
+
+    
+    
+// Step 2. Use the TEST macro to define your tests.
+//
+// TEST has two parameters: the test case name and the test name.
+// After using the macro, you should define your test logic between a
+// pair of braces.  You can use a bunch of macros to indicate the
+// success or failure of a test.  EXPECT_TRUE and EXPECT_EQ are
+// examples of such macros.  For a complete list, see gtest.h.
+//
+// <TechnicalDetails>
+//
+// In Google Test, tests are grouped into test cases.  This is how we
+// keep test code organized.  You should put logically related tests
+// into the same test case.
+//
+// The test case name and the test name should both be valid C++
+// identifiers.  And you should not use underscore (_) in the names.
+//
+// Google Test guarantees that each test you define is run exactly
+// once, but it makes no guarantee on the order the tests are
+// executed.  Therefore, you should write your tests in such a way
+// that their results don't depend on their order.
+//
+// </TechnicalDetails>
+    
+    // Sets the 0-terminated C string this MyString object
+// represents.
+void MyString::Set(const char* a_c_string) {
+  // Makes sure this works when c_string == c_string_
+  const char* const temp = MyString::CloneCString(a_c_string);
+  delete[] c_string_;
+  c_string_ = temp;
 }
+
+    
+      // Gets the first element of the queue, or NULL if the queue is empty.
+  QueueNode<E>* Head() { return head_; }
+  const QueueNode<E>* Head() const { return head_; }
+    
+    #include <emmintrin.h>
+#include <smmintrin.h>
+#include <cstdint>
+#include 'dotproductsse.h'
+    
+    // Computes a reshaped copy of the weight matrix w. If there are no
+// partial_funcs_, it does nothing.
+void IntSimdMatrix::Init(const GENERIC_2D_ARRAY<int8_t>& w) {
+  if (partial_funcs_.empty()) return;
+  int num_out = w.dim1();
+  int num_in = w.dim2() - 1;
+  // The rounded-up sizes of the reshaped weight matrix, excluding biases.
+  int rounded_num_in = Roundup(num_in, num_inputs_per_group_);
+  int rounded_num_out = RoundOutputs(num_out);
+  // Add the bias and compute the required size.
+  shaped_w_.resize((rounded_num_in + 1) * rounded_num_out, 0);
+  int shaped_index = 0;
+  int output = 0;
+  // Each number of registers needs a different format! Iterates over the
+  // different numbers of registers (each a power of 2).
+  for (int num_registers = max_output_registers_; num_registers >= 1;
+       num_registers /= 2) {
+    // The number of outputs that we will generate with this many registers.
+    int num_outputs_per_register_set =
+        num_registers * num_outputs_per_register_;
+    // Use the max number of registers until we have to go fewer.
+    while (output + num_outputs_per_register_set <= rounded_num_out) {
+      // Accumulating outputs in registers saves iterating over the inputs, so
+      // we only have to do it once per output register set.
+      for (int input = 0; input < num_in; input += num_inputs_per_group_) {
+        // Iterate over the number of outputs in a register set.
+        for (int j = 0; j < num_outputs_per_register_set; ++j) {
+          // Inner-most loop corresponds to the number of inputs in an input
+          // group.
+          for (int i = 0; i < num_inputs_per_group_; ++i) {
+            int8_t weight = 0;
+            if (output + j < num_out && input + i < num_in)
+              weight = w(output + j, input + i);
+            shaped_w_[shaped_index++] = weight;
+          }
+        }
+      }
+      // Append the bias weights for the register set.
+      for (int j = 0; j < num_outputs_per_register_set; ++j) {
+        int8_t weight = 0;
+        if (output + j < num_out) weight = w(output + j, num_in);
+        shaped_w_[shaped_index++] = weight;
+      }
+      output += num_outputs_per_register_set;
+    }
+  }
+}
+    
+    #include <cstdint>  // for int16_t
+    
+      // Split the part (horizontally), and save the split result into
+  // parts_splitted. Note that it is caller's responsibility to release the
+  // memory owns by parts_splitted. On the other hand, the part is unchanged
+  // during this process and still owns the blobs, so do NOT call DeleteBoxes
+  // when freeing the colpartitions in parts_splitted.
+  void SplitCPHor(ColPartition* part,
+                  GenericVector<ColPartition*>* parts_splitted);
+    
+      bool has_leaders;   // does the line contain leader dots (.....)?
+  bool has_drop_cap;  // does the line have a drop cap?
+  int pix_ldistance;  // distance to the left pblock boundary in pixels
+  int pix_rdistance;  // distance to the right pblock boundary in pixels
+  float pix_xheight;  // guessed xheight for the line
+  int average_interword_space; // average space between words in pixels.
+    
+    
+    {
+    {    if (amount[tag.string()] == 1) {
+      other->AddChild(vc->GetName(), vc->GetId(), vc->GetValue().string(),
+                      vc->GetDescription());
+    } else {  // More than one would use this submenu -> create submenu.
+      SVMenuNode* sv = mr->AddChild(tag.string());
+      if ((amount[tag.string()] <= MAX_ITEMS_IN_SUBMENU) ||
+          (amount[tag2.string()] <= 1)) {
+        sv->AddChild(vc->GetName(), vc->GetId(),
+                     vc->GetValue().string(), vc->GetDescription());
+      } else {  // Make subsubmenus.
+        SVMenuNode* sv2 = sv->AddChild(tag2.string());
+        sv2->AddChild(vc->GetName(), vc->GetId(),
+                      vc->GetValue().string(), vc->GetDescription());
+      }
+    }
+  }
+  return mr;
+}
+    
+      // Gets the first n words (split by _) and puts them in t.
+  // For example, tesseract_foo_bar with N=2 will yield tesseract_foo_.
+  void GetFirstWords(const char *s,  // source string
+                     int n,          // number of words
+                     char *t);       // target string
+    
+    #include <grpc/support/port_platform.h>
+    
+    namespace grpc {
+    }
+    
+    MeasureDouble RpcClientServerLatency() {
+  static const auto measure = MeasureDouble::Register(
+      kRpcClientServerLatencyMeasureName,
+      'Time between first byte of request received to last byte of response '
+      'sent, or terminal error (propagated from the server)',
+      kUnitMilliseconds);
+  return measure;
+}
+    
+    constexpr size_t TraceContextEncoding::kGrpcTraceContextSize;
+constexpr size_t TraceContextEncoding::kEncodeDecodeFailure;
+constexpr size_t TraceContextEncoding::kVersionIdSize;
+constexpr size_t TraceContextEncoding::kFieldIdSize;
+constexpr size_t TraceContextEncoding::kVersionIdOffset;
+constexpr size_t TraceContextEncoding::kVersionId;
+    
+    
+    {  std::atomic<int> refCount{0};
+};
+    
+    template <class UIntType, size_t w, size_t s, size_t r>
+struct StateSize<std::subtract_with_carry_engine<UIntType, w, s, r>> {
+  // [rand.eng.sub]: r * ceil(w / 32)
+  using type = std::integral_constant<size_t, r*((w + 31) / 32)>;
+};
+    
+      /**
+   * Returns a secure random uint64_t
+   */
+  static uint64_t secureRand64() {
+    return secureRandom<uint64_t>();
+  }
+    
+    namespace folly {
+    }
+    
+    #endif // D_DHT_REGISTRY_H
+
+    
+    public:
+  DHTRoutingTable(const std::shared_ptr<DHTNode>& localNode);
+    
+    
+    {} // namespace aria2
+
+    
+      std::shared_ptr<DHTNode> localNode_;
+    
+        taskFactory->setLocalNode(localNode);
+    taskFactory->setRoutingTable(routingTable.get());
+    taskFactory->setMessageDispatcher(dispatcher.get());
+    taskFactory->setMessageFactory(factory.get());
+    taskFactory->setTaskQueue(taskQueue.get());
+    taskFactory->setTimeout(std::chrono::seconds(messageTimeout));
+    
+    #endif // D_DHT_SETUP_H
+
     
     
     {  virtual bool finished() = 0;
 };
     
-    #include <memory>
+    #include <algorithm>
+    
+    class DHTTaskExecutor {
+private:
+  int numConcurrent_;
+  std::vector<std::shared_ptr<DHTTask>> execTasks_;
+  std::deque<std::shared_ptr<DHTTask>> queue_;
+    }
     
     
-    {  static const std::string UNKNOWN;
-};
+    {} // namespace aria2
+    
+    void DHTTaskFactoryImpl::setMessageFactory(DHTMessageFactory* factory)
+{
+  factory_ = factory;
+}
+    
+      void setTaskQueue(DHTTaskQueue* taskQueue);
