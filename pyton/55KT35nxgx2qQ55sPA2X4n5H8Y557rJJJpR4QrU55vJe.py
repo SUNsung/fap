fea5c104bad012377f1b9796d53afb67a26c27a1
SUNsung <weatherius@gmail.com>
2018-09-20 03:11:04 +0300

    Rewrite [55KT35nxgx2qQ55sPA2X4n5H8Y557rJJJpR4QrU55vJe.py] File in: 2018-09-20 03:11:03

--- pyton/55KT35nxgx2qQ55sPA2X4n5H8Y557rJJJpR4QrU55vJe.py
+++ pyton/55KT35nxgx2qQ55sPA2X4n5H8Y557rJJJpR4QrU55vJe.py
@@ -1,142 +1,135 @@
 
         
-        # UNDERFITTING
-# If the primary task of LFADS is 'filtering' of data and not
-# generation, then it is possible that the KL penalty is too strong.
-# Empirically, we have found this to be the case.  So we add a
-# hyperparameter in front of the the two KL terms (one for the initial
-# conditions to the generator, the other for the controller outputs).
-# You should always think of the the default values as 1.0, and that
-# leads to a standard VAE formulation whereby the numbers that are
-# optimized are a lower-bound on the log-likelihood of the data. When
-# these 2 HPs deviate from 1.0, one cannot make any statement about
-# what those LL lower bounds mean anymore, and they cannot be compared
-# (AFAIK).
-flags.DEFINE_float('kl_ic_weight', KL_IC_WEIGHT,
-                   'Strength of KL weight on initial conditions KL penatly.')
-flags.DEFINE_float('kl_co_weight', KL_CO_WEIGHT,
-                   'Strength of KL weight on controller output KL penalty.')
+            def load(self):
+        '''
+        Load the data from the key itself instead of fetching from some
+        external data store. Opposite of _get_session_key(), raise BadSignature
+        if signature fails.
+        '''
+        try:
+            return signing.loads(
+                self.session_key,
+                serializer=self.serializer,
+                # This doesn't handle non-default expiry dates, see #19201
+                max_age=settings.SESSION_COOKIE_AGE,
+                salt='django.contrib.sessions.backends.signed_cookies',
+            )
+        except Exception:
+            # BadSignature, ValueError, or unpickling exceptions. If any of
+            # these happen, reset the session.
+            self.create()
+        return {}
     
     
-def evaluate_ensemble(test_data_name, number_of_lms):
-  ensemble = EnsembleLM(test_data_name)
-  model_list = ['lm{:02d}'.format(i+1) for i in range(number_of_lms)]
-  for model_name in model_list:
-    ensemble.add_single_model(model_name)
-  accuracy = ensemble.evaluate()
-  print('Accuracy of {} LM(s) on {} = {}'.format(
-      number_of_lms, test_data_name, accuracy))
-    
-      # 0th token always present.
-  p = np.concatenate((np.ones((FLAGS.batch_size, 1)), p), axis=1)
-    
-    from models import bidirectional_zaremba
-from models import cnn
-from models import critic_vd
-from models import feedforward
-from models import rnn
-from models import rnn_nas
-from models import rnn_vd
-from models import rnn_zaremba
-from models import seq2seq
-from models import seq2seq_nas
-from models import seq2seq_vd
-from models import seq2seq_zaremba
-    
-      return variable_mapping
-    
-        kwargs = {
-        'stream': True,
-        'method': args.method.lower(),
-        'url': args.url,
-        'headers': headers,
-        'data': data,
-        'verify': {
-            'yes': True,
-            'true': True,
-            'no': False,
-            'false': False,
-        }.get(args.verify.lower(), args.verify),
-        'cert': cert,
-        'timeout': args.timeout,
-        'auth': args.auth,
-        'proxies': {p.key: p.value for p in args.proxy},
-        'files': args.files,
-        'allow_redirects': args.follow,
-        'params': args.params,
-    }
-    
-        def format_body(self, content, mime):
-        if is_valid_mime(mime):
-            for p in self.enabled_plugins:
-                content = p.format_body(content, mime)
-        return content
+default_app_config = 'django.contrib.sitemaps.apps.SiteMapsConfig'
 
     
-        if req:
-        output.append(Stream(
-            msg=HTTPRequest(request),
-            with_headers=req_h,
-            with_body=req_b))
+        def __init__(self, *a, **kw):
+        super(QPSSpider, self).__init__(*a, **kw)
+        if self.qps is not None:
+            self.qps = float(self.qps)
+            self.download_delay = 1 / self.qps
+        elif self.download_delay is not None:
+            self.download_delay = float(self.download_delay)
     
-        # Set to `False` to make it possible to invoke this auth
-    # plugin without requiring the user to specify credentials
-    # through `--auth, -a`.
-    auth_require = True
+            for spidername in args or spider_loader.list():
+            spidercls = spider_loader.load(spidername)
+            spidercls.start_requests = lambda s: conman.from_spider(s, result)
+    
+        def run(self, args, opts):
+        if len(args) < 1:
+            raise UsageError()
+        elif len(args) > 1:
+            raise UsageError('running 'scrapy crawl' with more than one spider is no longer supported')
+        spname = args[0]
+    
+        name = 'scrapes'
+    
+    from scrapy import twisted_version
+    
+        def _build_response(self, result, request, protocol):
+        self.result = result
+        respcls = responsetypes.from_args(url=request.url)
+        protocol.close()
+        body = protocol.filename or protocol.body.read()
+        headers = {'local filename': protocol.filename or '', 'size': protocol.size}
+        return respcls(url=request.url, status=200, body=to_bytes(body), headers=headers)
+    
+        strings = []
+    with open(filepath, 'r') as f:
+    
+        def test_decode_good(self):
+        self.assertEqual('x', self.field.decode('x'))
     
     
-def load_json_preserve_order(s):
-    return json.loads(s, object_pairs_hook=OrderedDict)
-    
-        plugin_manager.register(Plugin)
-    try:
-        r = http(
-            httpbin + BASIC_AUTH_URL,
-            '--auth-type',
-            Plugin.auth_type,
-            '--auth',
-            USERNAME + SEP_CREDENTIALS + PASSWORD,
-        )
-        assert HTTP_OK in r
-        assert r.json == AUTH_OK
-    finally:
-        plugin_manager.unregister(Plugin)
-    
-            get_filename_max_length.return_value = 10
-    
-    
-# Example usage...
-def main():
-    data1 = Data('Data 1')
-    data2 = Data('Data 2')
-    view1 = DecimalViewer()
-    view2 = HexViewer()
-    data1.attach(view1)
-    data1.attach(view2)
-    data2.attach(view2)
-    data2.attach(view1)
-    
-    ### OUTPUT ###
-# dog σκύλος
-# parrot parrot
-# cat γάτα
-# bear bear
-
-    
+class Header(jose.Header):
+    '''ACME-specific JOSE Header. Implements nonce, kid, and url.
     '''
-Port of the Java example of 'Parameter Injection' in
-'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
-(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
-http://xunitpatterns.com/Dependency%20Injection.html.
+    nonce = jose.Field('nonce', omitempty=True, encoder=jose.encode_b64jose)
+    kid = jose.Field('kid', omitempty=True)
+    url = jose.Field('url', omitempty=True)
     
-            path.append(start)
-        if start == end:
-            return path
-        for node in self.graph.get(start, []):
-            if node not in path:
-                newpath = self.find_path(node, end, path[:])
-                if newpath:
-                    return newpath
+            self.jobj_to = {
+            'contact': contact,
+            'agreement': agreement,
+            'key': key,
+        }
+        self.jobj_from = self.jobj_to.copy()
+        self.jobj_from['key'] = key.to_json()
     
-        def __init__(self, HierachicalStateMachine):
-        self._hsm = HierachicalStateMachine
+        def test_basic_ifdefine(self):
+        self.assertEqual(len(self.parser.find_dir('VAR_DIRECTIVE')), 2)
+        self.assertEqual(len(self.parser.find_dir('INVALID_VAR_DIRECTIVE')), 0)
+    
+    
+class SelectVhostMultiTest(unittest.TestCase):
+    '''Tests for certbot_apache.display_ops.select_vhost_multiple.'''
+    
+    import mock
+    
+        def test_repr(self):
+        self.assertEqual(repr(self.addr2), 'certbot_apache.obj.Addr(('127.0.0.1', '443'))')
+    
+            self.assertEqual(mock_setup_cert.call_count, 2)
+    
+    [1] http://www.yummly.com/recipe/Roasted-Asparagus-Epicurious-203718
+    
+        for filename in os.listdir(directory):
+        path = os.path.join(directory, filename)
+        if not os.path.isfile(path):
+            continue
+        # Guess the content type based on the file's extension.  Encoding
+        # will be ignored, although we should check for simple things like
+        # gzip'd or compressed files.
+        ctype, encoding = mimetypes.guess_type(path)
+        if ctype is None or encoding is not None:
+            # No guess could be made, or the file is encoded (compressed), so
+            # use a generic bag-of-bits type.
+            ctype = 'application/octet-stream'
+        maintype, subtype = ctype.split('/', 1)
+        with open(path, 'rb') as fp:
+            msg.add_attachment(fp.read(),
+                               maintype=maintype,
+                               subtype=subtype,
+                               filename=filename)
+    # Now send or store the message
+    if args.output:
+        with open(args.output, 'wb') as fp:
+            fp.write(msg.as_bytes(policy=SMTP))
+    else:
+        with smtplib.SMTP('localhost') as s:
+            s.send_message(msg)
+    
+        print('-' * 20)
+    
+            print('Ordered results using pool.map() --- will block till complete:')
+        for x in pool.map(calculatestar, TASKS):
+            print('\t', x)
+        print()
+    
+    con = sqlite3.connect(':memory:')
+con.isolation_level = None
+cur = con.cursor()
+    
+    if os.path.exists(DB_FILE):
+    os.remove(DB_FILE)
