af405fe18876aae67bf890a010bf31d4fd74e592
SUNsung <weatherius@gmail.com>
2018-09-20 15:24:20 +0300

    Rewrite [40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp] File in: 2018-09-20 15:24:19

--- cccccc/40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp
+++ cccccc/40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp
@@ -1,158 +1,211 @@
 
         
-        #include 'content/nw/src/api/base/base.h'
-    
-      int id() const { return id_; }
-  std::string extension_id_;
-  ObjectManager* object_manager() const { return object_manager_.get(); }
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(Clipboard);
-};
-    
-    bool MenuDelegate::GetIconForCommandId(int command_id,
-                                       gfx::Image* icon) const {
-  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
-  if (!item)
-    return false;
-  if (item->icon_.IsEmpty())
-    return false;
+        namespace atom {
     }
     
-       bool HasIcon(int command_id) override;
+    
+    {}  // namespace api
     
     
-    {  gtk_widget_show(menu_);
-  g_object_ref_sink(G_OBJECT(menu_));
-}
-    
-        bool WriteRTF(ClipboardData& data) {
-      DCHECK(data.type == TYPE_RTF);
-      scw_->WriteRTF(*(data.data));
-      return true;
-    }
-    
-    class NwClipboardReadAvailableTypesFunction : public NWSyncExtensionFunction {
- public:
-  NwClipboardReadAvailableTypesFunction();
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    }
-    
-        // Check reverse iteration results are the reverse of forward results
-    size_t matched = 0;
-    for (iter->SeekToLast(); iter->Valid(); iter->Prev()) {
-      ASSERT_LT(matched, forward.size());
-      ASSERT_EQ(IterStatus(iter), forward[forward.size() - matched - 1]);
-      matched++;
-    }
-    ASSERT_EQ(matched, forward.size());
-    
-    // Soft limit on number of level-0 files.  We slow down writes at this point.
-static const int kL0_SlowdownWritesTrigger = 8;
-    
-    bool GuessType(const std::string& fname, FileType* type) {
-  size_t pos = fname.rfind('/');
-  std::string basename;
-  if (pos == std::string::npos) {
-    basename = fname;
-  } else {
-    basename = std::string(fname.data() + pos + 1, fname.size() - pos - 1);
-  }
-  uint64_t ignored;
-  return ParseFileName(basename, &ignored, type);
-}
+    {}  // namespace atom
     
     
-    {  // Errors
-  static const char* errors[] = {
-    '',
-    'foo',
-    'foo-dx-100.log',
-    '.log',
-    '',
-    'manifest',
-    'CURREN',
-    'CURRENTX',
-    'MANIFES',
-    'MANIFEST',
-    'MANIFEST-',
-    'XMANIFEST-3',
-    'MANIFEST-3x',
-    'LOC',
-    'LOCKx',
-    'LO',
-    'LOGx',
-    '18446744073709551616.log',
-    '184467440737095516150.log',
-    '100',
-    '100.',
-    '100.lop'
-  };
-  for (int i = 0; i < sizeof(errors) / sizeof(errors[0]); i++) {
-    std::string f = errors[i];
-    ASSERT_TRUE(!ParseFileName(f, &number, &type)) << f;
-  }
-}
+    {}  // namespace api
     
-      // Read the next record into *record.  Returns true if read
-  // successfully, false if we hit end of the input.  May use
-  // '*scratch' as temporary storage.  The contents filled in *record
-  // will only be valid until the next mutating operation on this
-  // reader or the next mutation to *scratch.
-  bool ReadRecord(Slice* record, std::string* scratch);
+      friend class SubstitutionMap;
     
-    TEST(LogTest, ReadFourthFirstBlockTrailer) {
-  CheckInitialOffsetRecord(log::kBlockSize - 4, 3);
-}
+      char *oldAllocation = Allocated;
+  char *oldBegin = Begin;
+  std::size_t oldSize = (std::size_t) (End - oldBegin);
     
-    #ifndef BOOST_NO_TEMPLATED_ITERATOR_CONSTRUCTORS
-   //
-   // Augment error message with the regular expression text:
-   //
-   if(start_pos == position)
-      start_pos = (std::max)(static_cast<std::ptrdiff_t>(0), position - static_cast<std::ptrdiff_t>(10));
-   std::ptrdiff_t end_pos = (std::min)(position + static_cast<std::ptrdiff_t>(10), static_cast<std::ptrdiff_t>(m_end - m_base));
-   if(error_code != regex_constants::error_empty)
-   {
-      if((start_pos != 0) || (end_pos != (m_end - m_base)))
-         message += '  The error occurred while parsing the regular expression fragment: '';
-      else
-         message += '  The error occurred while parsing the regular expression: '';
-      if(start_pos != end_pos)
-      {
-         message += std::string(m_base + start_pos, m_base + position);
-         message += '>>>HERE>>>';
-         message += std::string(m_base + position, m_base + end_pos);
+        {
+      Out << Indent;
+      if (childKind == ChildKind::Root) {
+        Out << '+- ';
+      } else if (childKind == ChildKind::Left) {
+        Out << '/- ';
+      } else if (childKind == ChildKind::Right) {
+        Out << '\\- ';
+      } else if (childKind == ChildKind::Further) {
+        Out << '\\-> ';
       }
-      message += ''.';
-   }
-#endif
+      PrintNodeData(Out, node);
+      Out << '\n';
+    }
     
-    #if defined(__SUNPRO_CC)
-using std::list;
-#endif
+    void
+swift::trimLeadingWhitespaceFromLines(StringRef RawText,
+                                      unsigned WhitespaceToTrim,
+                                      SmallVectorImpl<StringRef> &OutLines) {
+  SmallVector<StringRef, 8> Lines;
+    }
     
-    #endif
+      bool SubtaskFailed = false;
     
-    #ifndef BOOST_REGEX_MATCHER_HPP
-#define BOOST_REGEX_MATCHER_HPP
+      PyObject* as_pyobject() const { return reinterpret_cast<PyObject*>(ptr_); }
     
-    template <class T>
-class concrete_protected_call
-   : public abstract_protected_call
-{
-public:
-   typedef bool (T::*proc_type)();
-   concrete_protected_call(T* o, proc_type p)
-      : obj(o), proc(p) {}
-private:
-   virtual bool call()const;
-   T* obj;
-   proc_type proc;
+    AnyMetadata::AnyMetadata(UrlType* type_url, ValueType* value)
+    : type_url_(type_url), value_(value) {
+}
+    
+    bool RunProtoCompiler(const string& filename,
+                      const string& plugin_specific_args,
+                      CommandLineInterface* cli, FileDescriptorProto* file) {
+  cli->SetInputsAreProtoPathRelative(true);
+    }
+    
+    
+    {
+    {
+    {}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
+
+    
+    void MessageOneofFieldGenerator::GenerateParsingCode(io::Printer* printer) {
+  // TODO(jonskeet): We may be able to do better than this
+  printer->Print(
+    variables_,
+    '$type_name$ subBuilder = new $type_name$();\n'
+    'if ($has_property_check$) {\n'
+    '  subBuilder.MergeFrom($property_name$);\n'
+    '}\n'
+    'input.ReadMessage(subBuilder);\n' // No support of TYPE_GROUP
+    '$property_name$ = subBuilder;\n');
+}
+    
+    #include <google/protobuf/stubs/common.h>
+namespace google {
+namespace protobuf {
+namespace compiler {
+namespace csharp {
+    }
+    }
+    }
+    }
+    
+    void PrimitiveOneofFieldGenerator::WriteToString(io::Printer* printer) {
+  printer->Print(variables_,
+    'PrintField(\'$descriptor_name$\', $has_property_check$, $oneof_name$_, writer);\n');
+}
+    
+    #include <google/protobuf/compiler/code_generator.h>
+#include <google/protobuf/compiler/plugin.h>
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/descriptor.pb.h>
+#include <google/protobuf/io/printer.h>
+#include <google/protobuf/io/zero_copy_stream.h>
+#include <google/protobuf/stubs/strutil.h>
+    
+    #include <mxnet/op_attr_types.h>
+#include <mxnet/graph_attr_types.h>
+#include <mxnet/c_api.h>
+#include <nnvm/symbolic.h>
+#include <nnvm/op.h>
+#include <nnvm/graph.h>
+#include <vector>
+#include <atomic>
+#include <utility>
+#include <string>
+#include <unordered_map>
+    
+    struct CaffeDataParam : public dmlc::Parameter<CaffeDataParam> {
+  /*! \brief protobuf text */
+  ::caffe::LayerParameter prototxt;
+  /*! \brief number of iterations per epoch */
+  int num_examples;
+  /*! \brief data mode */
+  bool flat;
+    }
+    
+    
+    {
+    {/*! \brief typedef the factory function of data iterator */
+typedef std::function<ImageAugmenter *()> ImageAugmenterFactory;
+/*!
+ * \brief Registry entry for DataIterator factory functions.
+ */
+struct ImageAugmenterReg
+    : public dmlc::FunctionRegEntryBase<ImageAugmenterReg,
+                                        ImageAugmenterFactory> {
 };
+//--------------------------------------------------------------
+// The following part are API Registration of Iterators
+//--------------------------------------------------------------
+/*!
+ * \brief Macro to register image augmenter
+ *
+ * \code
+ * // example of registering a mnist iterator
+ * REGISTER_IMAGE_AUGMENTER(aug_default)
+ * .describe('default augmenter')
+ * .set_body([]() {
+ *     return new DefaultAugmenter();
+ *   });
+ * \endcode
+ */
+#define MXNET_REGISTER_IMAGE_AUGMENTER(name)                            \
+  DMLC_REGISTRY_REGISTER(::mxnet::io::ImageAugmenterReg, ImageAugmenterReg, name)
+}  // namespace io
+}  // namespace mxnet
+#endif  // MXNET_USE_OPENCV
     
-    #ifndef BOOST_REGEX_V4_REGEX_SEARCH_HPP
-#define BOOST_REGEX_V4_REGEX_SEARCH_HPP
+    bool get_png_size(const uint8_t* data, uint32_t data_size, int64_t *width, int64_t *height) {
+  if (data[0] == 0x89 && data[1] == 0x50 && data[2] ==0x4E && data[3] == 0x47) {
+    uint8_t const* p = data + 16;
+    *width = ((p[0]*256 + p[1])*256 + p[2])*256 + p[3];
+    p += 4;
+    *height = ((p[0]*256 + p[1])*256 + p[2])*256 + p[3];
+    return true;
+  } else {
+    return false;
+  }
+}
+    
+        LearnerPtr MomentumSGDLearner(const vector<Parameter>& parameters,
+                                  const LearningRateSchedule& learningRateSchedule,
+                                  const MomentumSchedule& momentumSchedule,
+                                  bool unitGain,
+                                  AdditionalLearningOptions additionalOptions /*= AdditionalLearningOptions()*/)
+    {
+        return MakeSharedObject<LearnerMomentumSGD>(parameters, learningRateSchedule, momentumSchedule, unitGain, additionalOptions, 1);
+    }
+    
+            if (sectionShape.Rank() > m_maskShape.Rank())
+            LogicError('NDMask::MaskSection: The section shape '%S' rank (%d) must be <= rank (%d) of 'this' mask.', sectionShape.AsString().c_str(), (int)sectionShape.Rank(), (int)m_maskShape.Rank());
+    
+        void ProgressWriter::UpdateTraining(size_t samples, const ValuePtr& accumulatedLoss,
+                                        const ValuePtr& accumulatedMetric)
+    {
+        m_training->Update(samples, accumulatedLoss, accumulatedMetric,
+            [this](const std::pair<size_t, size_t> samples, std::pair<size_t, size_t> updates,
+                   const std::pair<double, double> aggregateLoss, std::pair<double, double> aggregateMetric)
+            {
+                OnWriteTrainingUpdate(samples, updates, aggregateLoss, aggregateMetric);
+            });
+        OnTrainingUpdateEnd();
+    }
+    
+    
+    {                unpackedShape = unpackedShape.AppendShape({ packedDataLayout->GetNumSequences() });
+            }
+            else if (!sampleDynamicAxes.empty())
+                LogicError('A PackedValue object that does not have a layout cannot have any dynamic axes.');
+    
+    // some older code uses this namespace
+namespace DebugUtil
+{
+    void PrintCallStack(size_t skipLevels = 0, bool makeFunctionNamesStandOut = false);
+    }
+    
+    #endif // BOOST_NO_WREGEX
+    
+    namespace boost{
+   namespace BOOST_REGEX_DETAIL_NS{
+    }
+    }
+    
+    
     
     #ifdef BOOST_MSVC
 #pragma warning(push)
@@ -165,108 +218,98 @@ private:
 #pragma warning(pop)
 #endif
     
+       format_perl = 0,                                  /* perl style replacement */
+   format_default = 0,                               /* ditto. */
+   format_sed = match_max << 1,                      /* sed style replacement. */
+   format_all = format_sed << 1,                     /* enable all extentions to sytax. */
+   format_no_copy = format_all << 1,                 /* don't copy non-matching segments. */
+   format_first_only = format_no_copy << 1,          /* Only replace first occurance. */
+   format_is_if = format_first_only << 1,            /* internal use only. */
+   format_literal = format_is_if << 1                /* treat string as a literal */
     
+    template <class BidiIterator, class Allocator, class traits>
+bool perl_matcher<BidiIterator, Allocator, traits>::unwind_recursion_stopper(bool)
+{
+   boost::BOOST_REGEX_DETAIL_NS::inplace_destroy(m_backup_state++);
+   pstate = 0;   // nothing left to search
+   return false; // end of stack nothing more to search
+}
+    
+    template <class BidiIterator, class Allocator, class traits>
+bool perl_matcher<BidiIterator, Allocator, traits>::backtrack_till_match(std::size_t count)
+{
+#ifdef BOOST_MSVC
+#pragma warning(push)
+#pragma warning(disable:4127)
+#endif
+   if(!m_can_backtrack)
+      return false;
+   if((m_match_flags & match_partial) && (position == last))
+      m_has_partial_match = true;
+    }
+    
+    template <class ST, class SA, class Allocator, class charT, class traits>
+inline bool regex_match(const std::basic_string<charT, ST, SA>& s, 
+                 match_results<typename std::basic_string<charT, ST, SA>::const_iterator, Allocator>& m, 
+                 const basic_regex<charT, traits>& e, 
+                 match_flag_type flags = match_default)
+{
+   return regex_match(s.begin(), s.end(), m, e, flags);
+}
+template <class charT, class traits>
+inline bool regex_match(const charT* str, 
+                        const basic_regex<charT, traits>& e, 
+                        match_flag_type flags = match_default)
+{
+   match_results<const charT*> m;
+   return regex_match(str, str + traits::length(str), m, e, flags | regex_constants::match_any);
+}
+    
+    
+
+    
+        // Cleanup
+    ImGui_ImplOpenGL2_Shutdown();
+    ImGui_ImplFreeGLUT_Shutdown();
+    ImGui::DestroyContext();
+    
+        // Create window with graphics context
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_DisplayMode current;
+    SDL_GetCurrentDisplayMode(0, &current);
+    SDL_Window* window = SDL_CreateWindow('Dear ImGui SDL2+OpenGL3 example', SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    SDL_GL_SetSwapInterval(1); // Enable vsync
+    
+        if (!g_pIB || g_IndexBufferSize < draw_data->TotalIdxCount)
     {
-    {      // Record end time
-      uint64_t end_time = env->NowMicros();
-      double elapsed = static_cast<double>(end_time - start_time) * 1e-6;
-      uint32_t qps = static_cast<uint32_t>(
-          static_cast<double>(FLAGS_threads * FLAGS_ops_per_thread) / elapsed);
-      fprintf(stdout, 'Complete in %.3f s; QPS = %u\n', elapsed, qps);
-    }
-    return true;
-  }
-    
-    // REQUIRES: DB mutex held
-Env::WriteLifeTimeHint ColumnFamilyData::CalculateSSTWriteHint(int level) {
-  if (initial_cf_options_.compaction_style != kCompactionStyleLevel) {
-    return Env::WLTH_NOT_SET;
-  }
-  if (level == 0) {
-    return Env::WLTH_MEDIUM;
-  }
-  int base_level = current_->storage_info()->base_level();
+        if (g_pIB) { g_pIB->Release(); g_pIB = NULL; }
+        g_IndexBufferSize = draw_data->TotalIdxCount + 10000;
+        D3D10_BUFFER_DESC desc;
+        memset(&desc, 0, sizeof(D3D10_BUFFER_DESC));
+        desc.Usage = D3D10_USAGE_DYNAMIC;
+        desc.ByteWidth = g_IndexBufferSize * sizeof(ImDrawIdx);
+        desc.BindFlags = D3D10_BIND_INDEX_BUFFER;
+        desc.CPUAccessFlags = D3D10_CPU_ACCESS_WRITE;
+        if (ctx->CreateBuffer(&desc, NULL, &g_pIB) < 0)
+            return;
     }
     
-      enum class WriteStallCause {
-    kNone,
-    kMemtableLimit,
-    kL0FileCountLimit,
-    kPendingCompactionBytes,
-  };
-  static std::pair<WriteStallCondition, WriteStallCause>
-  GetWriteStallConditionAndCause(int num_unflushed_memtables, int num_l0_files,
-                                 uint64_t num_compaction_needed_bytes,
-                                 const MutableCFOptions& mutable_cf_options);
-    
-    #include 'db/compaction.h'
-#include 'db/compaction_iteration_stats.h'
-#include 'db/merge_helper.h'
-#include 'db/pinned_iterators_manager.h'
-#include 'db/range_del_aggregator.h'
-#include 'db/snapshot_checker.h'
-#include 'options/cf_options.h'
-#include 'rocksdb/compaction_filter.h'
-    
-        uint64_t fileSize;
-    status = defaultEnv->GetFileSize(filePath, &fileSize);
-    ASSERT_OK(status);
-    
-      env.now_micros_ += 1999900u;  // sleep debt 1000
-    
-      // Skip 'n' bytes from the file. This is guaranteed to be no
-  // slower that reading the same data, but may be faster.
-  //
-  // If end of file is reached, skipping will stop at the end of the
-  // file, and Skip will return OK.
-  //
-  // REQUIRES: External synchronization
-  virtual Status Skip(uint64_t n) override {
-    auto status = file_->Skip(n);
-    if (!status.ok()) {
-      return status;
+    bool    ImGui_ImplDX11_Init(ID3D11Device* device, ID3D11DeviceContext* device_context)
+{
+    // Get factory from device
+    IDXGIDevice* pDXGIDevice = NULL;
+    IDXGIAdapter* pDXGIAdapter = NULL;
+    IDXGIFactory* pFactory = NULL;
     }
-    offset_ += n;
-    return status;
-  }
     
-          // Truncate to available space if necessary
-      if (p >= limit) {
-        if (iter == 0) {
-          continue;       // Try again with larger buffer
-        } else {
-          p = limit - 1;
-        }
-      }
-    
-     public:
-  PosixLogger(FILE* f, uint64_t (*gettid)(), Env* env,
-              const InfoLogLevel log_level = InfoLogLevel::ERROR_LEVEL)
-      : Logger(log_level),
-        file_(f),
-        gettid_(gettid),
-        log_size_(0),
-        fd_(fileno(f)),
-        last_flush_micros_(0),
-        env_(env),
-        flush_pending_(false) {}
-  virtual ~PosixLogger() {
-    if (!closed_) {
-      closed_ = true;
-      PosixCloseHelper();
+    // Render function.
+// (this used to be set in io.RenderDrawListsFn and called by ImGui::Render(), but you can now call this directly from your main loop)
+void ImGui_ImplDX9_RenderDrawData(ImDrawData* draw_data)
+{
+    // Avoid rendering when minimized
+    if (draw_data->DisplaySize.x <= 0.0f || draw_data->DisplaySize.y <= 0.0f)
+        return;
     }
-  }
-  virtual void Flush() override {
-    TEST_SYNC_POINT('PosixLogger::Flush:Begin1');
-    TEST_SYNC_POINT('PosixLogger::Flush:Begin2');
-    if (flush_pending_) {
-      flush_pending_ = false;
-      fflush(file_);
-    }
-    last_flush_micros_ = env_->NowMicros();
-  }
-    
-    
-    {  mutable int count_ = 0;
-  mutable int merge_count_ = 0;
-};
