77f7855e36824f6b1cceff197af5a62539b8657c
SUNsung <weatherius@gmail.com>
2018-09-21 00:28:54 +0300

    Rewrite [48hrIFD4uv9lg48zgtdHKWFb.484KtOdM0Z8648U0a.rb] File in: 2018-09-21 00:28:54

--- ruby/48hrIFD4uv9lg48zgtdHKWFb.484KtOdM0Z8648U0a.rb
+++ ruby/48hrIFD4uv9lg48zgtdHKWFb.484KtOdM0Z8648U0a.rb
@@ -1,84 +1,120 @@
 
         
-        def normalize_bullets(markdown)
-  markdown.gsub(%r!\n\s{2}\*{1}!, '\n-')
+                  def sanitized_object_name
+            @sanitized_object_name ||= @object_name.gsub(/\]\[|[^-a-zA-Z0-9:.]/, '_').sub(/_$/, '')
+          end
+    
+              def hidden_field_for_checkbox(options)
+            @unchecked_value ? tag('input', options.slice('name', 'disabled', 'form').merge!('type' => 'hidden', 'value' => @unchecked_value)) : ''.html_safe
+          end
+      end
+    end
+  end
+end
+
+    
+                  Time.utc(
+                default[:year], default[:month], default[:day],
+                default[:hour], default[:min], default[:sec]
+              )
+            end
+          end
+      end
+    end
+  end
+end
+
+    
+              add_default_name_and_id_for_value(tag_value, name_and_id)
+          options.delete('index')
+          options.delete('namespace')
+          options['for'] = name_and_id['id'] unless options.key?('for')
+    
+    require 'action_view/helpers/tags/placeholderable'
+    
+                if only = conditions[:only]
+              only.include?(action_name)
+            elsif except = conditions[:except]
+              !except.include?(action_name)
+            else
+              true
+            end
+          end
+      end
+    
+        def cache_message(payload) # :doc:
+      case payload[:cache_hit]
+      when :hit
+        '[cache hit]'
+      when :miss
+        '[cache miss]'
+      end
+    end
+    
+            def #{name}=(value)
+          value = value.present? ? Array(value) : default_#{name}
+          _set_detail(:#{name}, value) if value != @details[:#{name}]
+        end
+      METHOD
+    end
+    
+    def docs_name
+  '#{name}-docs'
 end
     
     # For this pull request, which changes Page#dir
 # https://github.com/jekyll/jekyll/pull/4403
     
-      all_files       = `git ls-files -z`.split('\x0')
-  s.files         = all_files.grep(%r!^(exe|lib|rubocop)/|^.rubocop.yml$!)
-  s.executables   = all_files.grep(%r!^exe/!) { |f| File.basename(f) }
-  s.bindir        = 'exe'
-  s.require_paths = ['lib']
+    def graceful_require
+  Jekyll::External.require_with_graceful_fail('json')
+  JSON.pretty_generate(DATA)
+end
     
-            def create_sample_files(path)
-          FileUtils.cp_r site_template + '/.', path
-          FileUtils.chmod_R 'u+w', path
-          FileUtils.rm File.expand_path(scaffold_path, path)
-        end
+    #
     
-    # TODO:
-# group :mongoid do
-#   gem 'mongoid', '~> 4.0.0'
-# end
-
+      def self.status_file; test_dir.join('jekyll_status.txt'); end
     
-      def after_omniauth_failure_path_for(scope)
-    new_session_path(scope)
-  end
+            # rubocop:disable Metrics/AbcSize
+        def process(args, opts)
+          if !args || args.empty?
+            raise Jekyll::Errors::InvalidThemeName, 'You must specify a theme name.'
+          end
     
-        # Check if a reset_password_token is provided in the request
-    def assert_reset_token_passed
-      if params[:reset_password_token].blank?
-        set_flash_message(:alert, :no_token)
-        redirect_to new_session_path(resource_name)
-      end
-    end
+    require 'cucumber/rails'
     
-        # The path used after sending unlock password instructions
-    def after_sending_unlock_instructions_path_for(resource)
-      new_session_path(resource) if is_navigational_format?
-    end
-    
-        def unlock_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :unlock_instructions, opts)
-    end
-    
-          # Remembers the given resource by setting up a cookie
-      def remember_me(resource)
-        return if request.env['devise.skip_storage']
-        scope = Devise::Mapping.find_scope!(resource)
-        resource.remember_me!
-        cookies.signed[remember_key(resource, scope)] = remember_cookie_values(resource)
-      end
-    
-    require 'uri'
-    
-    # Each time a record is set we check whether its session has already timed out
-# or not, based on last request time. If so, the record is logged out and
-# redirected to the sign in page. Also, each time the request comes and the
-# record is set, we set the last request time inside its scoped session to
-# verify timeout in the following request.
-Warden::Manager.after_set_user do |record, warden, options|
-  scope = options[:scope]
-  env   = warden.request.env
-    
-    def attach_attachment(name, definition = nil)
-  snippet = 'has_attached_file :#{name}'
-  if definition
-    snippet += ', \n'
-    snippet += definition
-  end
-  snippet += '\ndo_not_validate_attachment_file_type :#{name}\n'
-  cd('.') do
-    transform_file('app/models/user.rb') do |content|
-      content.sub(/end\Z/, '#{snippet}\nend')
+      def confirm_on_page(page_name)
+    if page_name == 'my profile page'
+      expect(page).to have_path_in([person_path(@me.person), user_profile_path(@me.username)])
+    else
+      expect(page).to have_path(path_to(page_name))
     end
   end
 end
     
-      argument :attachment_names, :required => true, :type => :array, :desc => 'The names of the attachment(s) to add.',
-           :banner => 'attachment_one attachment_two attachment_three ...'
+        it 'generates the aspects_manage_contacts_json fixture', fixture: true do
+      # adds one not mutual contact
+      bob.share_with(FactoryGirl.create(:person), @aspect)
+    
+        it 'generates a jasmine fixture', fixture: true do
+      session[:mobile_view] = true
+      get :new, format: :mobile
+      save_fixture(html_for('body'), 'conversations_new_mobile')
+    end
+  end
+end
+
+    
+      def migration_name
+    'add_attachment_#{attachment_names.join('_')}_to_#{name.underscore.pluralize}'
+  end
+    
+        def self.register(klass, attachment_name, attachment_options)
+      instance.register(klass, attachment_name, attachment_options)
+    end
+    
+        def type_from_file_contents
+      type_from_mime_magic || type_from_file_command
+    rescue Errno::ENOENT => e
+      Paperclip.log('Error while determining content type: #{e}')
+      SENSIBLE_DEFAULT
+    end
