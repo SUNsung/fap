8b84b42290d5116179898e5ce4e6fceebcb4b25e
SUNsung <weatherius@gmail.com>
2018-09-20 16:17:51 +0300

    Rewrite [21NUTFkmOKF.215Okfg0X71Js21JAvEXS8uvsw213w..py] File in: 2018-09-20 16:17:50

--- pyton/21NUTFkmOKF.215Okfg0X71Js21JAvEXS8uvsw213w..py
+++ pyton/21NUTFkmOKF.215Okfg0X71Js21JAvEXS8uvsw213w..py
@@ -1,195 +1,203 @@
 
         
-        
-if __name__ == '__main__':
-    main()
+        SVCNAME = 'youtubedl_builder'
+    
+    with io.open(README_FILE, 'w', encoding='utf-8') as f:
+    f.write(header)
+    f.write(options)
+    f.write(footer)
 
     
-    new_version = {}
+        def gen_ies_md(ies):
+        for ie in ies:
+            ie_md = '**{0}**'.format(ie.IE_NAME)
+            ie_desc = getattr(ie, 'IE_DESC', None)
+            if ie_desc is False:
+                continue
+            if ie_desc is not None:
+                ie_md += ': {0}'.format(ie.IE_DESC)
+            if not ie.working():
+                ie_md += ' (Currently broken)'
+            yield ie_md
     
-    import rsa
-import json
-from binascii import hexlify
+        readme = filter_options(readme)
     
-        ies = sorted(youtube_dl.gen_extractors(), key=lambda i: i.IE_NAME.lower())
-    out = '# Supported sites\n' + ''.join(
-        ' - ' + md + '\n'
-        for md in gen_ies_md(ies))
+        fileopts = []
+    for opt in opts_file:
+        if opt._short_opts:
+            fileopts.extend(opt._short_opts)
+        if opt._long_opts:
+            fileopts.extend(opt._long_opts)
     
-    if len(sys.argv) >= 2 and sys.argv[1] == 'py2exe':
-    params = py2exe_params
-else:
-    files_spec = [
-        ('etc/bash_completion.d', ['youtube-dl.bash-completion']),
-        ('etc/fish/completions', ['youtube-dl.fish']),
-        ('share/doc/youtube_dl', ['README.txt']),
-        ('share/man/man1', ['youtube-dl.1'])
-    ]
-    root = os.path.dirname(os.path.abspath(__file__))
-    data_files = []
-    for dirname, files in files_spec:
-        resfiles = []
-        for fn in files:
-            if not os.path.exists(fn):
-                warnings.warn('Skipping file %s since it is not present. Type  make  to build all automatically generated files.' % fn)
-            else:
-                resfiles.append(fn)
-        data_files.append((dirname, resfiles))
+        def test_html_search_meta(self):
+        ie = self.ie
+        html = '''
+            <meta name='a' content='1' />
+            <meta name='b' content='2'>
+            <meta name='c' content='3'>
+            <meta name=d content='4'>
+            <meta property='e' content='5' >
+            <meta content='6' name='f'>
+        '''
     
-    
-def assertGreaterEqual(self, got, expected, msg=None):
-    if not (got >= expected):
-        if msg is None:
-            msg = '%r not greater than or equal to %r' % (got, expected)
-        self.assertTrue(got >= expected, msg)
-    
-            for mpd_file, mpd_url, expected_formats in _TEST_CASES:
-            with io.open('./test/testdata/mpd/%s.mpd' % mpd_file,
-                         mode='r', encoding='utf-8') as f:
-                formats = self.ie._parse_mpd_formats(
-                    compat_etree_fromstring(f.read().encode('utf-8')),
-                    mpd_url=mpd_url)
-                self.ie._sort_formats(formats)
-                expect_value(self, formats, expected_formats, None)
-    
-        def test_youtube_extract(self):
-        assertExtractId = lambda url, id: self.assertEqual(YoutubeIE.extract_id(url), id)
-        assertExtractId('http://www.youtube.com/watch?&v=BaW_jenozKc', 'BaW_jenozKc')
-        assertExtractId('https://www.youtube.com/watch?&v=BaW_jenozKc', 'BaW_jenozKc')
-        assertExtractId('https://www.youtube.com/watch?feature=player_embedded&v=BaW_jenozKc', 'BaW_jenozKc')
-        assertExtractId('https://www.youtube.com/watch_popup?v=BaW_jenozKc', 'BaW_jenozKc')
-        assertExtractId('http://www.youtube.com/watch?v=BaW_jenozKcsharePLED17F32AD9753930', 'BaW_jenozKc')
-        assertExtractId('BaW_jenozKc', 'BaW_jenozKc')
+        # Path-based component
+    if self.hparams.input in ['path', 'integrated', 'integrated-nc']:
     
         Args:
-      session: The current TensorFlow session,
-      instances: The instances for which to load the labels.
+      data_name: The name of the data dict, to select which in/out matrices
+        to use.
+      data_bxtxd:  Numpy array training data with shape:
+        batch_size x # time steps x # dimensions
+      ext_input_bxtxi: Numpy array training external input with shape:
+        batch_size x # time steps x # external input dims
+      do_eval_cost (optional): If true, the IWAE (Importance Weighted
+         Autoencoder) log likeihood bound, instead of the VAE version.
+      do_average_batch (optional): average over the batch, useful for getting
+      good IWAE costs, and model outputs for a single data point.
     
-        # Convert to full tensors, not lists of tensors in time dim.
-    gen_states = list_t_bxn_to_tensor_bxtxn(gen_states)
-    factors = list_t_bxn_to_tensor_bxtxn(factors)
-    out_dist_params = list_t_bxn_to_tensor_bxtxn(out_dist_params)
-    if self.hps.ic_dim > 0:
-      # select first time point
-      prior_g0_mean = prior_g0_mean[0]
-      prior_g0_logvar = prior_g0_logvar[0]
-      post_g0_mean = post_g0_mean[0]
-      post_g0_logvar = post_g0_logvar[0]
-    if self.hps.co_dim > 0:
-      controller_outputs = list_t_bxn_to_tensor_bxtxn(controller_outputs)
+    # generate trials for both RNNs
+rates_a, x0s_a, _ = generate_data(rnn_a, T=T, E=E, x0s=x0s, P_sxn=P_nxn,
+                                  input_magnitude=0.0, input_times=None)
+spikes_a = spikify_data(rates_a, rng, rnn_a['dt'], rnn_a['max_firing_rate'])
     
+        us = np.zeros([1, ntime_steps])
+    for t in range(ntime_steps):
+      x_t = alpha*x_tm1 + np.dot(W,r_tm1) + b
+      if input_time is not None and t == input_time:
+        us[0,t] = input_magnitude
+        x_t += Bin * us[0,t] # DCS is this what was used?
+      r_t = np.tanh(x_t)
+      x_tm1 = x_t
+      r_tm1 = r_t
+      rs[:,t] = r_t
+    return rs, us
     
-def generate_data(rnn, T, E, x0s=None, P_sxn=None, input_magnitude=0.0,
-                  input_times=None):
-  ''' Generates data from an randomly initialized RNN.
-  Args:
-    rnn: the rnn
-    T: Time in seconds to run (divided by rnn['dt'] to get steps, rounded down.
-    E: total number of examples
-    S: number of samples (subsampling N)
-  Returns:
-    A list of length E of NxT tensors of the network being run.
+      Args:
+    data_fname: The filename of teh file in which to write the data.
+    data_dict:  The dictionary of data to write. The keys are strings
+      and the values are numpy arrays.
+    use_json (optional): human readable format for simple items
+    compression (optional): The compression to use for h5py (disabled by
+      default because the library borks on scalars, otherwise try 'gzip').
   '''
-  N = rnn['N']
-  def run_rnn(rnn, x0, ntime_steps, input_time=None):
-    rs = np.zeros([N,ntime_steps])
-    x_tm1 = x0
-    r_tm1 = np.tanh(x0)
-    tau = rnn['tau']
-    dt = rnn['dt']
-    alpha = (1.0-dt/tau)
-    W = dt/tau*rnn['W']*rnn['g']
-    Bin = dt/tau*rnn['Bin']
-    Bin2 = dt/tau*rnn['Bin2']
-    b = dt/tau*rnn['b']
     
-      Returns:
-    The length B list of TxN numpy tensors.
-  '''
-  T = len(values_t_bxn)
-  B, N = values_t_bxn[0].shape
-  values_b_txn = []
-  for b in range(B):
-    values_pb_txn = np.zeros([T,N])
-    for t in range(T):
-      values_pb_txn[t,:] = values_t_bxn[t][b,:]
-    values_b_txn.append(values_pb_txn)
+      # print('Number of batches per epoch: %d' % epoch_size)
+  for i in range(epoch_size):
+    x = data[:, i * num_steps:(i + 1) * num_steps]
+    y = data[:, i * num_steps + 1:(i + 1) * num_steps + 1]
+    w = np.ones_like(x)
+    yield (x, y, w)
+
     
-        self.bos_char = free_ids[0]  # <begin sentence>
-    self.eos_char = free_ids[1]  # <end sentence>
-    self.bow_char = free_ids[2]  # <begin word>
-    self.eow_char = free_ids[3]  # <end word>
-    self.pad_char = free_ids[4]  # <padding>
+      elif FLAGS.mask_strategy == 'contiguous':
+    masked_length = int((1 - FLAGS.is_present_rate) * FLAGS.sequence_length) - 1
+    # Determine location to start masking.
+    start_mask = np.random.randint(
+        1, FLAGS.sequence_length - masked_length + 1, size=FLAGS.batch_size)
+    p = np.full([FLAGS.batch_size, FLAGS.sequence_length], True, dtype=bool)
+    
+            # attn_fun
+        scores = _attn_add_fun(score_v, keys, query)
+      elif attention_option == 'luong':
+        # reshape query: [batch_size, 1, num_units]
+        query = tf.reshape(query, [-1, 1, num_units])
+    
+            request_line = '{method} {path}{query} HTTP/1.1'.format(
+            method=self._orig.method,
+            path=url.path or '/',
+            query='?' + url.query if url.query else ''
+        )
+    
+        # The URL prefix the adapter should be mount to.
+    prefix = None
     
     
-def _convert_to_partial(scoring1, scoring2):
-  '''Convert full scoring into partial scoring.'''
-  mask1, mask2 = _substitution_mask(
-      scoring1['sentence'], scoring2['sentence'])
+def test_basic_auth(httpbin_both):
+    r = http('--auth=user:password',
+             'GET', httpbin_both + '/basic-auth/user/password')
+    assert HTTP_OK in r
+    assert r.json == {'authenticated': True, 'user': 'user'}
     
-      '''
-  zero_tensor = tf.constant(0., dtype=tf.float32, shape=[])
+        config['implicit_content_type'] = 'form'
+    config.save()
+    config.load()
+    assert 'implicit_content_type' not in config
+    assert config['default_options'] == ['--form']
     
-      for t in ngrams_list:
-    key = hash_function(t)
-    if key in counts:
-      counts[key] += 1
-    else:
-      counts[key] = 1
-  return counts
+        def test_format_option(self, httpbin):
+        env = MockEnvironment(colors=256)
+        r = http('--print=B', '--pretty=format',
+                 'GET', httpbin.url + '/get', 'a=b',
+                 env=env)
+        # Tests that the JSON data is formatted.
+        assert r.strip().count('\n') == 2
+        assert COLOR not in r
     
-      ## Encoder forward variables.
-  encoder_lstm_w_0 = [
-      v for v in tf.trainable_variables() if v.op.name ==
-      'dis/encoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/kernel'
-  ][0]
-  encoder_lstm_b_0 = [
-      v for v in tf.trainable_variables() if v.op.name ==
-      'dis/encoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/bias'
-  ][0]
-  encoder_lstm_w_1 = [
-      v for v in tf.trainable_variables() if v.op.name ==
-      'dis/encoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/kernel'
-  ][0]
-  encoder_lstm_b_1 = [
-      v for v in tf.trainable_variables() if v.op.name ==
-      'dis/encoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/bias'
-  ][0]
     
-        old_update_wrapper = functools.update_wrapper
-    def update_wrapper(wrapper, wrapped, *a, **kw):
-        rv = old_update_wrapper(wrapper, wrapped, *a, **kw)
-        rv._original_function = wrapped
-        return rv
-    functools.update_wrapper = update_wrapper
+def test_follow_all_output_options_used_for_redirects(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 3
+    assert HTTP_OK not in r
     
-        if db is not None:
-        db.close()
     
-        assert 'closed' in str(e)
+easy_install.ScriptWriter.get_args = get_args
     
-    This typically means that you attempted to use functionality that needed
-an active HTTP request.  Consult the documentation on testing for
-information about how to avoid this problem.\
+    match_output = '''
+Hit:1 http://us.archive.ubuntu.com/ubuntu zesty InRelease
+Hit:2 http://us.archive.ubuntu.com/ubuntu zesty-updates InRelease
+Get:3 http://us.archive.ubuntu.com/ubuntu zesty-backports InRelease [89.2 kB]
+Hit:4 http://security.ubuntu.com/ubuntu zesty-security InRelease
+Hit:5 http://ppa.launchpad.net/ubuntu-mozilla-daily/ppa/ubuntu zesty InRelease
+Hit:6 https://download.docker.com/linux/ubuntu zesty InRelease
+Hit:7 https://cli-assets.heroku.com/branches/stable/apt ./ InRelease
+Fetched 89.2 kB in 0s (122 kB/s)
+Reading package lists... Done
+Building dependency tree
+Reading state information... Done
+8 packages can be upgraded. Run 'apt list --upgradable' to see them.
 '''
-_app_ctx_err_msg = '''\
-Working outside of application context.
     
-            @app.route('/stream')
-        def streamed_response():
-            def generate():
-                yield 'Hello '
-                yield request.args['name']
-                yield '!'
-            return Response(stream_with_context(generate()))
     
-    containers = (('thefuck/python3-tcsh',
-               u'''FROM python:3
-                   RUN apt-get update
-                   RUN apt-get install -yy tcsh''',
-               u'tcsh'),
-              ('thefuck/python2-tcsh',
-               u'''FROM python:2
-                   RUN apt-get update
-                   RUN apt-get install -yy tcsh''',
-               u'tcsh'))
+@pytest.mark.parametrize('script, output', [
+    ('brew link sshfs', output),
+    ('cat output', output),
+    ('brew install sshfs', '')])
+def test_not_match(script, output):
+    command = Command(script, output)
+    assert not match(command)
+    
+    
+@pytest.mark.skipif(_is_not_okay_to_test(),
+                    reason='No need to run if there\'s no formula')
+def test_match(brew_no_available_formula, brew_already_installed,
+               brew_install_no_argument):
+    assert match(Command('brew install elsticsearch',
+                         brew_no_available_formula))
+    assert not match(Command('brew install git',
+                             brew_already_installed))
+    assert not match(Command('brew install', brew_install_no_argument))
+    
+            return urls
+    
+            complex_vh = VirtualHost(
+            'fp', 'vhp',
+            set([Addr.fromstring('*:443'), Addr.fromstring('1.2.3.4:443')]),
+            False, False)
+        self.assertTrue(complex_vh.conflicts([self.addr1]))
+        self.assertTrue(complex_vh.conflicts([self.addr2]))
+        self.assertFalse(complex_vh.conflicts([self.addr_default]))
+    
+        def test_perform0(self):
+        resp = self.sni.perform()
+        self.assertEqual(len(resp), 0)
+    
+    def get_marker(line):
+    matchlist = TAG_REGEX.findall(line)
+    if matchlist:
+        namematch = NAMED_A_TAG_REGEX.match(line)
+        if namematch:
+            return namematch.group(1) # group 0 is full match
