285f39d25d87d0ef91737111a2e1a60397be154e
SUNsung <weatherius@gmail.com>
2018-09-21 14:18:39 +0300

    Rewrite [26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py] File in: 2018-09-21 14:18:39

--- pyton/26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py
+++ pyton/26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py
@@ -1,50 +1,47 @@
 
         
-          Parameters:
-    forest: If true, solve for a spanning forest instead of a spanning tree.
-    loss: The loss function for training.  Select from
-      softmax: Default unstructured softmax (prediction is still structured).
-      m3n: Max-Margin Markov Networks loss.
-    crf_max_dynamic_range: Max dynamic range for the log partition function.
+            def report_out_of_quota(self, appid):
+        self.logger.warn('report_out_of_quota:%s', appid)
+        with self.lock:
+            if appid not in self.out_of_quota_appids:
+                self.out_of_quota_appids.append(appid)
+            try:
+                self.working_appid_list.remove(appid)
+            except:
+                pass
     
-          # For the first tree, the gold and scores agree on one arc (that index 1
-      # is a root), and for the second tree, the gold and scores agree on none
-      # of the arcs.  Therefore, we expect +1 and +3 for the first and second
-      # trees in the M3N loss.
-      gold = tf.constant([0, 1, -1, 0, 0, 1], tf.int32)
-      first_gold_score = 0.5 + 2.0
-      second_gold_score = 2.5 + 2.5 + 2.5
-      first_tree_correct = 1
-      second_tree_correct = 0
-      first_tree_loss = 2 * 2.0 + 2 - first_tree_correct - first_gold_score
-      second_tree_loss = 3 * 4.0 + 3 - second_tree_correct - second_gold_score
+        @property
+    def type_restricted(self):
+        if not hasattr(self, '_type_restricted'):
+            self._type_restricted = self.creat_rs_packet()
+        return self._type_restricted
     
-      Args:
-    function: Function to apply using captured variables.  The function should
-        take one argument, its enclosing variable scope.
-    scope_name: Variable scope in which the |function| is evaluated.  Must match
-        the scope passed to capture_variables().
-    component: Component from which to extract captured variables.
+        if __password__ and __password__ != kwargs.get('password', ''):
+        start_response('403 Forbidden', [('Content-Type', 'text/html')])
+        yield message_html('403 Wrong password', 'Wrong password(%r)' % kwargs.get('password', ''), 'GoAgent proxy.ini password is wrong!')
+        raise StopIteration
     
+    EOF = -1
     
-if __name__ == '__main__':
-  tf.app.run()
-
-    
-      # Convert to char-based corpora, if requested.
-  if config['convert_to_char_corpora']:
-    # NB: Do not convert the |gold_tune_corpus|, which should remain word-based
-    # for segmentation evaluation purposes.
-    train_corpus = _convert_to_char_corpus(train_corpus)
-    tune_corpus = _convert_to_char_corpus(tune_corpus)
-    
-    from syntaxnet.ops import gen_parser_ops
-from syntaxnet.util import check
-    
-        parts[-1] += 1
-    return '.'.join(map(str, parts))
-    
-    
-def test_add_template_filter(app):
-    def my_reverse(s):
-        return s[::-1]
+                    if c >= self.min[s] and c <= self.max[s]:
+                    # move to next state
+                    snext = self.transition[s][c-self.min[s]]
+                    #print 'in range, next state = %d' % snext
+                    
+                    if snext < 0:
+                        #print 'not a normal transition'
+                        # was in range but not a normal transition
+                        # must check EOT, which is like the else clause.
+                        # eot[s]>=0 indicates that an EOT edge goes to another
+                        # state.
+                        if self.eot[s] >= 0: # EOT Transition to accept state?
+                            #print 'EOT trans to accept state %d' % self.eot[s]
+                            
+                            s = self.eot[s]
+                            input.consume()
+                            # TODO: I had this as return accept[eot[s]]
+                            # which assumed here that the EOT edge always
+                            # went to an accept...faster to do this, but
+                            # what about predicated edges coming from EOT
+                            # target?
+                            continue
