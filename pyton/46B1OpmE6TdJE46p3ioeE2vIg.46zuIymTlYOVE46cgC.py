613dfd0f580d2e2ba5855632fd0961c96dfbe8c2
SUNsung <weatherius@gmail.com>
2018-09-22 00:02:03 +0300

    Rewrite [46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py] File in: 2018-09-22 00:02:02

--- pyton/46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py
+++ pyton/46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py
@@ -1,103 +1,137 @@
 
         
         
-def has_docutils():
-    try:
-        # noinspection PyUnresolvedReferences
-        import docutils
-        return True
-    except ImportError:
-        return False
+    {    # Options that need a file parameter
+    'download-archive': ['--require-parameter'],
+    'cookies': ['--require-parameter'],
+    'load-info': ['--require-parameter'],
+    'batch-file': ['--require-parameter'],
+}
     
-        exc = Timeout('Request timed out')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR_TIMEOUT
-    assert error_msg == 'Request timed out (30s).'
+    
+def hex_str(int_list):
+    return codecs.encode(intlist_to_bytes(int_list), 'hex')
+    
+    py2exe_params = {
+    'console': py2exe_console,
+    'options': {'py2exe': py2exe_options},
+    'zipfile': None
+}
+    
+    from youtube_dl.aes import aes_decrypt, aes_encrypt, aes_cbc_decrypt, aes_cbc_encrypt, aes_decrypt_text
+from youtube_dl.utils import bytes_to_intlist, intlist_to_bytes
+import base64
+    
+        with gzip.open(paths[2], 'rb') as lbpath:
+        y_test = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+    
+    
+def test_regularization_shared_layer_in_different_models():
+    shared_dense = Dense(num_classes,
+                         kernel_regularizer=regularizers.l1(),
+                         activity_regularizer=regularizers.l1())
+    models = []
+    for _ in range(2):
+        input_tensor = Input(shape=(data_dim,))
+        unshared_dense = Dense(num_classes, kernel_regularizer=regularizers.l1())
+        out = unshared_dense(shared_dense(input_tensor))
+        models.append(Model(input_tensor, out))
+    
+                kwargs['Updates'].append({'Action': 'INSERT', self.conditiontuple: condition_insert})
+    
+    ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+    
+        state = module.params.get('state')
+    caller_reference = module.params.get('caller_reference')
+    
+            try:
+            response = self.client.delete_rule(Name=self.name)
+        except (botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:
+            self.module.fail_json_aws(e, msg='Could not delete rule %s' % self.name)
+        self.changed = True
+        return response
+    
+    __metaclass__ = type
+    
+        # See if there is already a pipeline with the same unique_id
+    unique_id = build_unique_id(module)
+    try:
+        dp_id = pipeline_id(client, dp_name)
+        dp_unique_id = to_text(pipeline_field(client, dp_id, field='uniqueId'))
+        if dp_unique_id != unique_id:
+            # A change is expected but not determined. Updated to a bool in create_pipeline().
+            changed = 'NEW_VERSION'
+            create_dp = True
+        # Unique ids are the same - check if pipeline needs modification
+        else:
+            dp_objects = client.get_pipeline_definition(pipelineId=dp_id)['pipelineObjects']
+            # Definition needs to be updated
+            if dp_objects != objects:
+                changed, msg = define_pipeline(client, module, objects, dp_id)
+            # No changes
+            else:
+                msg = 'Data Pipeline {0} is present'.format(dp_name)
+            data_pipeline = get_result(client, dp_id)
+            result = {'data_pipeline': data_pipeline,
+                      'msg': msg}
+    except DataPipelineNotFound:
+        create_dp = True
+    
+        return removed_indexes, added_indexes, index_throughput_changes
+    
+        if changed:
+        try:
+            connection.put_lifecycle_hook(**lch_params)
+        except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
+            module.fail_json_aws(e, msg='Failed to create LifecycleHook')
+    
+    EXAMPLES = '''
+# Note: These examples do not set authentication details or the AWS region,
+# see the AWS Guide for details.
+    
+        connection = boto3_conn(module, conn_type='client', resource='ec2', region=region, endpoint=ec2_url, **aws_connect_params)
+    
+    try:
+    import boto3
+    from botocore.exceptions import ClientError, WaiterError
+    HAS_BOTO3 = True
+except ImportError:
+    HAS_BOTO3 = False
+    
+    def cbs_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    '''Downloads CBS videos by URL.
+    '''
+    
+    
+def write_loop_file(records_number, loop_file_path, file_name):
+    with open(loop_file_path, 'a') as file:
+        for i in range(records_number):
+            file.write('file '{}'\n'.format(file_name))
+    
+    from ..common import *
+import urllib.parse
+    
+    site_info = 'ehow.com'
+download = ehow_download
+download_playlist = playlist_not_supported('ehow')
 
     
-        # Used only when requested with --check-status:
-    ERROR_HTTP_3XX = 3
-    ERROR_HTTP_4XX = 4
-    ERROR_HTTP_5XX = 5
     
+def fantasy_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+    if 'fantasy.tv' not in url:
+        raise Exception('Wrong place!')
     
-def test_default_options(httpbin):
-    env = MockEnvironment()
-    env.config['default_options'] = ['--form']
-    env.config.save()
-    r = http(httpbin.url + '/post', 'foo=bar', env=env)
-    assert r.json['form'] == {'foo': 'bar'}
-    
-    
-class TestDownloads:
-    # TODO: more tests
-    
-    
-def test_headers_unset(httpbin_both):
-    r = http('GET', httpbin_both + '/headers')
-    assert 'Accept' in r.json['headers']  # default Accept present
-    
-    \note Please be warned that the line numbers in the API documentation do not
-match the real locations in the source code of the package. This is an
-unintended artifact of doxygen, which I could only convince to use the
-correct module names by concatenating all files from the package into a single
-module file...
-    
-            self.expecting = expecting
-        
-    
-                t = self.tokenSource.nextToken()
-       
-        # leave p pointing at first token on channel
-        self.p = 0
-        self.p = self.skipOffTokenChannels(self.p)
-    
-        '''
-    
-    def __init__(self, type=None, channel=DEFAULT_CHANNEL, text=None,
-                 input=None, start=None, stop=None, oldToken=None):
-        Token.__init__(self)
-        
-        if oldToken is not None:
-            self.type = oldToken.type
-            self.line = oldToken.line
-            self.charPositionInLine = oldToken.charPositionInLine
-            self.channel = oldToken.channel
-            self.index = oldToken.index
-            self._text = oldToken._text
-            if isinstance(oldToken, CommonToken):
-                self.input = oldToken.input
-                self.start = oldToken.start
-                self.stop = oldToken.stop
-            
-        else:
-            self.type = type
-            self.input = input
-            self.charPositionInLine = -1 # set to invalid position
-            self.line = 0
-            self.channel = channel
-            
-	    #What token number is this from 0..n-1 tokens; < 0 implies invalid index
-            self.index = -1
-            
-            # We need to be able to change the text once in a while.  If
-            # this is non-null, then getText should return this.  Note that
-            # start/stop are not affected by changing this.
-            self._text = text
-    
-    - CommonTokenStream: A basic and most commonly used TokenStream
-  implementation.
-- TokenRewriteStream: A modification of CommonTokenStream that allows the
-  stream to be altered (by the Parser). See the 'tweak' example for a usecase.
-    
-    	# Track the line at which the error occurred in case this is
-	# generated from a lexer.  We need to track this since the
-        # unexpected char doesn't carry the line info.
-        self.line = None
-    
-    def is_inside_code(line, indent_depth):
-    return is_code(line, indent_depth) > 0 or line.strip() == ''
-    
-      cppvar = GetHeaderGuardCPPVariable(filename)
+    def get_single_photo_url(url):
+    page = get_html(url)
+    pid = get_photo_id(url, page)
+    title = match1(page, pattern_inline_title)
+    if match1(page, pattern_inline_video_mark):
+        api_key = get_api_key(page)
+        reply = get_content(tmpl_api_call_photo_info % (api_key, get_photo_id(url, page)))
+        secret = json.loads(reply)['photo']['secret']
+        return get_orig_video_source(api_key, pid, secret), title
+    #last match always has the best resolution
+    match = match1(page, pattern_inline_img_url)
+    return 'https:' + match.replace('\\', ''), title
