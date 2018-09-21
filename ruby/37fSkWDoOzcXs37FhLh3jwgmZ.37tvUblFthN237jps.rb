049ffd8dc060cde5e5c423c419d2ddee6672eb4c
SUNsung <weatherius@gmail.com>
2018-09-22 00:47:27 +0300

    Rewrite [37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb] File in: 2018-09-22 00:47:26

--- ruby/37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb
+++ ruby/37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb
@@ -1,70 +1,97 @@
 
         
-          def upgrade_account
-    if signed_request_account.ostatus?
-      signed_request_account.update(last_webfingered_at: nil)
-      ResolveAccountWorker.perform_async(signed_request_account.acct)
+          at_exit do
+    if ENV['KEEP_RUNNING']
+      puts 'Vagrant vm will be left up because KEEP_RUNNING is set.'
+      puts 'Rerun without KEEP_RUNNING set to cleanup the vm.'
+    else
+      vagrant_cli_command('destroy -f')
     end
+  end
     
-    module Admin
-  class AccountModerationNotesController < BaseController
-    before_action :set_account_moderation_note, only: [:destroy]
-    
-        def require_local_account!
-      redirect_to admin_account_path(@account.id) unless @account.local? && @account.user.present?
-    end
-    
-        def paginated_instances
-      filtered_instances.page(params[:page])
-    end
-    
-        # put the non-standard line terminations back to normal
-    # gah.  not having look behinds suck,
-    header.gsub!(/([^\r])\n/n,'\1' + '\r\n')
-    
-      # Puts a URI back together based on the URI parts
-  def uri
-    str = self.uri_parts['Resource'].dup || '/'
-    
-            # Sends a kerberos request through the connection
-        #
-        # @param req [Rex::Proto::Kerberos::Model::KdcRequest] the request to send
-        # @return [Integer] the number of bytes sent
-        # @raise [RuntimeError] if the transport protocol is unknown
-        # @raise [NotImplementedError] if the transport protocol isn't supported
-        def send_request(req)
-          connect
-    
-        # Returns a new {Gem::Version} based on the systems `git` version.
-    #
-    # @return [Gem::Version]
-    #
-    def self.git_version
-      raw_version = Executable.capture_command('git', ['--version']).first
-      unless match = raw_version.scan(/\d+\.\d+\.\d+/).first
-        raise 'Failed to extract git version from `git --version` (#{raw_version.inspect})'
+        def display_error_message(ex)
+      unless options.backtrace
+        Rake.application.options.suppress_backtrace_pattern = backtrace_pattern if backtrace_pattern
+        trace '(Backtrace restricted to imported tasks)'
       end
-      Gem::Version.new(match)
+    
+          def add_roles(roles)
+        Array(roles).each { |role| add_role(role) }
+        self
+      end
+      alias roles= add_roles
+    
+    module Sinatra
+  class Application < Base
+    
+          TEMPLATE.result(binding)
     end
     
-            def run
-          if @pod_name.nil?
-            # Note: at that point, @wipe_all is always true (thanks to `validate!`)
-            # Remove all
-            clear_cache
+    desc 'generate documentation'
+task :doc => 'doc:all'
+    
+          DIRECTIVES = %i(base_uri child_src connect_src default_src
+                      font_src form_action frame_ancestors frame_src
+                      img_src manifest_src media_src object_src
+                      plugin_types referrer reflected_xss report_to
+                      report_uri require_sri_for sandbox script_src
+                      style_src worker_src).freeze
+    
+      def plugins_args_human
+    plugins_arg.join(', ')
+  end
+end # class Logstash::PluginManager
+
+    
+      def filtered_specs
+    @filtered_specs ||= begin
+                          # start with all locally installed plugin gems regardless of the Gemfile content
+                          specs = LogStash::PluginManager.find_plugins_gem_specs
+    
+        desc 'Generate a valid ssh-config'
+    task :ssh_config do
+      require 'json'
+      # Loop until the Vagrant box finishes SSH bootstrap
+      raw_ssh_config = Stud.try(50.times, LogStash::CommandExecutor::CommandError) do
+          LogStash::VagrantHelpers.fetch_config.stdout.split('\n');
+      end
+      parsed_ssh_config = LogStash::VagrantHelpers.parse(raw_ssh_config)
+      File.write('.vm_ssh_config', parsed_ssh_config.to_json)
+    end
+    
+            def load_order(lock = false)
+          @order = Spree::Order.lock(lock).find_by!(number: params[:id])
+          raise_insufficient_quantity and return if @order.insufficient_stock_lines.present?
+          @order.state = params[:state] if params[:state]
+          state_callback(:before)
+        end
+    
+              can_event = 'can_#{@event}?'
+    
+            def create
+          authorize! :create, Property
+          @property = Spree::Property.new(property_params)
+          if @property.save
+            respond_with(@property, status: 201, default_template: :show)
           else
-            # Remove only cache for this pod
-            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
-            if cache_descriptors.nil?
-              UI.notice('No cache for pod named #{@pod_name} found')
-            elsif cache_descriptors.count > 1 && !@wipe_all
-              # Ask which to remove
-              choices = cache_descriptors.map { |c| '#{@pod_name} v#{c[:version]} (#{pod_type(c)})' }
-              index = UI.choose_from_array(choices, 'Which pod cache do you want to remove?')
-              remove_caches([cache_descriptors[index]])
-            else
-              # Remove all found cache of this pod
-              remove_caches(cache_descriptors)
-            end
+            invalid_resource!(@property)
+          end
+        end
+    
+            def destroy
+          @stock_item = StockItem.accessible_by(current_ability, :destroy).find(params[:id])
+          @stock_item.destroy
+          respond_with(@stock_item, status: 204)
+        end
+    
+            private
+    
+            def create
+          authorize! :create, StockMovement
+          @stock_movement = scope.new(stock_movement_params)
+          if @stock_movement.save
+            respond_with(@stock_movement, status: 201, default_template: :show)
+          else
+            invalid_resource!(@stock_movement)
           end
         end
