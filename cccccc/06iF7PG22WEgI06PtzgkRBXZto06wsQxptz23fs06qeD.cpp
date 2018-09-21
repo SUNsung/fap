f7b43f8a7918ffcd59934d8ad85acf255e6a8cf7
SUNsung <weatherius@gmail.com>
2018-09-21 13:11:58 +0300

    Rewrite [06iF7PG22WEgI06PtzgkRBXZto06wsQxptz23fs06qeD.cpp] File in: 2018-09-21 13:11:57

--- cccccc/06iF7PG22WEgI06PtzgkRBXZto06wsQxptz23fs06qeD.cpp
+++ cccccc/06iF7PG22WEgI06PtzgkRBXZto06wsQxptz23fs06qeD.cpp
@@ -1,212 +1,275 @@
 
         
-        namespace {
-    }
-    
-    // TaskRunner implementation that posts tasks into libuv's default loop.
-class UvTaskRunner : public base::SingleThreadTaskRunner {
- public:
-  explicit UvTaskRunner(uv_loop_t* loop);
-    }
-    
-    class AutoUpdater : public mate::EventEmitter<AutoUpdater>,
-                    public auto_updater::Delegate,
-                    public WindowListObserver {
- public:
-  static mate::Handle<AutoUpdater> Create(v8::Isolate* isolate);
-    }
-    
-    
-    {}  // namespace atom
-    
-    
-    {  delegate.Get('isCommandIdChecked', &is_checked_);
-  delegate.Get('isCommandIdEnabled', &is_enabled_);
-  delegate.Get('isCommandIdVisible', &is_visible_);
-  delegate.Get('getAcceleratorForCommandId', &get_accelerator_);
-  delegate.Get('executeCommand', &execute_command_);
-  delegate.Get('menuWillShow', &menu_will_show_);
-}
-    
-    // static
-v8::Local<v8::Value> Net::Create(v8::Isolate* isolate) {
-  return mate::CreateHandle(isolate, new Net(isolate)).ToV8();
-}
-    
-    struct TemporaryFile {
-  std::string path;
-    }
-    
-    bool object_exists(const char *name) {
-  int fd = shm_open(name, O_RDONLY, 0);
-  if (fd >= 0) {
-    close(fd);
-    return true;
-  } else {
-    return false;
+        void NwCurrentWindowInternalCapturePageInternalFunction::OnCaptureFailure(FailureReason reason) {
+  const char* reason_description = 'internal error';
+  switch (reason) {
+    case FAILURE_REASON_UNKNOWN:
+      reason_description = 'unknown error';
+      break;
+    case FAILURE_REASON_ENCODING_FAILED:
+      reason_description = 'encoding failed';
+      break;
+    case FAILURE_REASON_VIEW_INVISIBLE:
+      reason_description = 'view is invisible';
+      break;
   }
+  error_ = ErrorUtils::FormatErrorMessage('Failed to capture tab: *',
+                                          reason_description);
+  Respond(Error(error_));
 }
     
-    // FreeOp frees the content of the output blob. We allow it to take in input
-// blobs purely for the reason that it can 'wait' on the input blobs to be
-// produced by some of the earlier operators before a free is called.
-template <class Context>
-class FreeOp : public Operator<Context> {
- public:
-  FreeOp(const OperatorDef& def, Workspace* ws) : Operator<Context>(def, ws) {}
-    }
-    
-    #define TEMPL_MAKE_ENUM_TYPE_INFO(m_enum, m_impl)                                                                                                                                 \
-	template <>                                                                                                                                                                   \
-	struct GetTypeInfo<m_impl> {                                                                                                                                                  \
-		static const Variant::Type VARIANT_TYPE = Variant::INT;                                                                                                                   \
-		static inline PropertyInfo get_class_info() {                                                                                                                             \
-			return PropertyInfo(Variant::INT, String(), PROPERTY_HINT_NONE, String(), PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_CLASS_IS_ENUM, String(#m_enum).replace('::', '.')); \
-		}                                                                                                                                                                         \
-	};
-    
-    #include <BulletDynamics/Dynamics/btDiscreteDynamicsWorld.h>
-    
-    	real_t getSoftnessDirLin() const;
-	real_t getRestitutionDirLin() const;
-	real_t getDampingDirLin() const;
-	real_t getSoftnessDirAng() const;
-	real_t getRestitutionDirAng() const;
-	real_t getDampingDirAng() const;
-	real_t getSoftnessLimLin() const;
-	real_t getRestitutionLimLin() const;
-	real_t getDampingLimLin() const;
-	real_t getSoftnessLimAng() const;
-	real_t getRestitutionLimAng() const;
-	real_t getDampingLimAng() const;
-	real_t getSoftnessOrthoLin() const;
-	real_t getRestitutionOrthoLin() const;
-	real_t getDampingOrthoLin() const;
-	real_t getSoftnessOrthoAng() const;
-	real_t getRestitutionOrthoAng() const;
-	real_t getDampingOrthoAng() const;
-	void setSoftnessDirLin(real_t softnessDirLin);
-	void setRestitutionDirLin(real_t restitutionDirLin);
-	void setDampingDirLin(real_t dampingDirLin);
-	void setSoftnessDirAng(real_t softnessDirAng);
-	void setRestitutionDirAng(real_t restitutionDirAng);
-	void setDampingDirAng(real_t dampingDirAng);
-	void setSoftnessLimLin(real_t softnessLimLin);
-	void setRestitutionLimLin(real_t restitutionLimLin);
-	void setDampingLimLin(real_t dampingLimLin);
-	void setSoftnessLimAng(real_t softnessLimAng);
-	void setRestitutionLimAng(real_t restitutionLimAng);
-	void setDampingLimAng(real_t dampingLimAng);
-	void setSoftnessOrthoLin(real_t softnessOrthoLin);
-	void setRestitutionOrthoLin(real_t restitutionOrthoLin);
-	void setDampingOrthoLin(real_t dampingOrthoLin);
-	void setSoftnessOrthoAng(real_t softnessOrthoAng);
-	void setRestitutionOrthoAng(real_t restitutionOrthoAng);
-	void setDampingOrthoAng(real_t dampingOrthoAng);
-	void setPoweredLinMotor(bool onOff);
-	bool getPoweredLinMotor();
-	void setTargetLinMotorVelocity(real_t targetLinMotorVelocity);
-	real_t getTargetLinMotorVelocity();
-	void setMaxLinMotorForce(real_t maxLinMotorForce);
-	real_t getMaxLinMotorForce();
-	void setPoweredAngMotor(bool onOff);
-	bool getPoweredAngMotor();
-	void setTargetAngMotorVelocity(real_t targetAngMotorVelocity);
-	real_t getTargetAngMotorVelocity();
-	void setMaxAngMotorForce(real_t maxAngMotorForce);
-	real_t getMaxAngMotorForce();
-	real_t getLinearPos();
-    
-    	ClassDB::register_virtual_class<CSGShape>();
-	ClassDB::register_virtual_class<CSGPrimitive>();
-	ClassDB::register_class<CSGMesh>();
-	ClassDB::register_class<CSGSphere>();
-	ClassDB::register_class<CSGBox>();
-	ClassDB::register_class<CSGCylinder>();
-	ClassDB::register_class<CSGTorus>();
-	ClassDB::register_class<CSGPolygon>();
-	ClassDB::register_class<CSGCombiner>();
-    
-    static ResourceFormatDDS *resource_loader_dds = NULL;
-    
-    PoolAllocator *MemoryPool::memory_pool = NULL;
-uint8_t *MemoryPool::pool_memory = NULL;
-size_t *MemoryPool::pool_size = NULL;
-    
-    void FuncRef::_bind_methods() {
-    }
-    
-    #include <string>
-#include <vector>
-    
-    #include <string>
-#include <vector>
+    // Generate param traits read methods.
+#include 'ipc/param_traits_read_macros.h'
+namespace IPC {
+#include 'content/nw/src/common/common_message_generator.h'
+}  // namespace IPC
     
     
-    { private:
-  static const std::string kLoggerKey;
-};
-    
-    
-    {  // This should work.
-  ASSERT_TRUE(doc.HasMember('custom_fake'));
-  EXPECT_TRUE(doc['custom_fake'].IsNumber());
-  EXPECT_EQ(1U, doc['custom_fake'].GetUint());
-  EXPECT_FALSE(Flag::getValue('custom_fake').empty());
-}
-    
-    #include 'osquery/core/conversions.h'
-    
-    Status Distributed::pullUpdates() {
-  auto distributed_plugin = RegistryFactory::get().getActive('distributed');
-  if (!RegistryFactory::get().exists('distributed', distributed_plugin)) {
-    return Status(1, 'Missing distributed plugin: ' + distributed_plugin);
+    {    result->AppendBoolean(success);
+    return;
+  } else if (method == 'UnregisterGlobalHotKey') {
+    int object_id = -1;
+    arguments.GetInteger(0, &object_id);
+    Shortcut* shortcut =
+        static_cast<Shortcut*>(DispatcherHost::GetApiObject(object_id));
+    GlobalShortcutListener::GetInstance()->UnregisterAccelerator(
+        shortcut->GetAccelerator(), shortcut);
+    return;
+  } else if (method == 'SetProxyConfig') {
+    std::string proxy_config;
+    arguments.GetString(0, &proxy_config);
+    SetProxyConfig(GetRenderProcessHost(), proxy_config);
+    return;
+  } else if (method == 'DoneMenuShow') {
+    dispatcher_host->quit_run_loop();
+    return;
   }
-    }
-    
-      // Make sure monitors are empty.
-  EXPECT_EQ(pub->numDescriptors(), 0U);
-    
-    The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-    
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
     
     
-    {// end of actions group
-/// @}
     
-        /** Returns the numbers of actions that are running in a certain target. 
-     * Composable actions are counted as 1 action. Example:
-     * - If you are running 1 Sequence of 7 actions, it will return 1.
-     * - If you are running 7 Sequences of 2 actions, it will return 7.
-     *
-     * @param target    A certain target.
-     * @return  The numbers of actions that are running in a certain target.
-     * @js NA
-     */
-    virtual ssize_t getNumberOfRunningActionsInTarget(const Node *target) const;
+    Menu::Menu(int id,
+           const base::WeakPtr<ObjectManager>& object_manager,
+           const base::DictionaryValue& option,
+           const std::string& extension_id)
+  : Base(id, object_manager, option, extension_id), enable_show_event_(false)  {
+  Create(option);
+}
     
-    /** Returns the numbers of actions that are running in all targets.
-     * @return  The numbers of actions that are running in all target.
-     * @js NA
-     */
-    virtual ssize_t getNumberOfRunningActions() const;
+       void ExecuteCommand(int command_id, int event_flags) override;
     
-    The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-    
-    NS_CC_END
+    MenuItem::MenuItem(int id,
+                   const base::WeakPtr<ObjectManager>& object_manager,
+                   const base::DictionaryValue& option,
+                   const std::string& extension_id)
+  : Base(id, object_manager, option, extension_id) {
+  Create(option);
+}
     
     
     {
-    {        AnimationCache::getInstance()->addAnimation(animation, name);
-    }
+    {    std::string error_;
+    std::unique_ptr<ui::ScopedClipboardWriter> scw_;
+  };
 }
+    
+    
+    {
+} // namespace extensions
+#endif
+
+    
+    #include 'extensions/browser/extension_function.h'
+    
+     protected:
+  ~NwObjAllocateIdFunction() override;
+    
+    OPERATOR_SCHEMA(ExpandDims)
+    .NumInputs(1)
+    .NumOutputs(1)
+    .AllowInplace({{0, 0}})
+    .TensorInferenceFunction([](const OperatorDef& def,
+                                const vector<TensorShape>& in) {
+      ArgumentHelper helper(def);
+      auto dims = helper.template GetRepeatedArgument<int>('dims');
+      auto originalSize = dims.size();
+      CAFFE_ENFORCE(originalSize > 0, 'Parameter `dims` must be provided.');
+    }
+    
+    #include 'caffe2/core/context.h'
+#include 'caffe2/core/operator.h'
+#include 'caffe2/core/tensor.h'
+    
+    **Result**
+    
+    #endif // CAFFE2_OPERATORS_FLOOR_OP_H_
+
+    
+    /// Database domain where we store carve table entries
+const std::string kCarveDbDomain = 'carves';
+    
+    
+    {
+    {  Status update(const std::string& source, const ParserConfig& config) override;
+};
+} // namespace osquery
+
+    
+      // We use a restricted scope below to change the data structure from
+  // an array to a set. This lets us do deletes much more efficiently
+  std::vector<std::string> created_views;
+  std::set<std::string> erase_views;
+  {
+    std::vector<std::string> old_views_vec;
+    scanDatabaseKeys(kQueries, old_views_vec, kConfigViews);
+    for (const auto& view : old_views_vec) {
+      erase_views.insert(view.substr(kConfigViews.size()));
+    }
+  }
+    
+      /**
+   * @brief Update the internal context buffer with additional content
+   *
+   * This method allows you to chunk up large content so that it doesn't all
+   * have to be loaded into memory at the same time
+   *
+   * @param buffer The buffer to be hashed
+   * @param size The size of the buffer to be hashed
+   */
+  void update(const void* buffer, size_t size);
+    
+      // Now test inclusive bounds.
+  struct ConstraintList cl3;
+  constraint = Constraint(LESS_THAN_OR_EQUALS);
+  constraint.expr = '1000';
+  cl3.add(constraint);
+  constraint = Constraint(GREATER_THAN_OR_EQUALS);
+  constraint.expr = '1';
+  cl3.add(constraint);
+    
+        // Main loop
+    bool done = false;
+    while (!done)
+    {
+        // Poll and handle events (inputs, window resize, etc.)
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT)
+                done = true;
+        }
+    }
+    
+        UINT64 fenceValue = frameCtxt->FenceValue;
+    if (fenceValue == 0)
+        return; // No fence was signaled
+    
+    
+    {  // Reorder to the correct output order.
+  // TODO(szabadka): Modify the above computation so that this is not needed.
+  Complex tmp = a[2];
+  a[2] = a[3];
+  a[3] = a[5];
+  a[5] = a[7];
+  a[7] = a[4];
+  a[4] = a[1];
+  a[1] = a[6];
+  a[6] = tmp;
+}
+    
+      // Returns true if the argument of the last Compare() call (or the baseline
+  // image, if Compare() was not called yet) meets the image acceptance
+  // criteria. The target_mul modifies the acceptance criteria used in this call
+  // the following way:
+  //    = 1.0 : the original acceptance criteria is used,
+  //    < 1.0 : a more strict acceptance criteria is used,
+  //    > 1.0 : a less strict acceptance criteria is used.
+  virtual bool DistanceOK(double target_mul) const = 0;
+    
+    #define GUETZLI_LOG(stats, ...)                                    \
+  do {                                                             \
+    char debug_string[1024];                                       \
+    int res = snprintf(debug_string, sizeof(debug_string),         \
+                       __VA_ARGS__);                               \
+    assert(res > 0 && 'expected successful printing');             \
+    (void)res;                                                     \
+    debug_string[sizeof(debug_string) - 1] = '\0';                 \
+    ::guetzli::PrintDebug(                      \
+         stats, std::string(debug_string));        \
+  } while (0)
+#define GUETZLI_LOG_QUANT(stats, q)                    \
+  for (int y = 0; y < 8; ++y) {                        \
+    for (int c = 0; c < 3; ++c) {                      \
+      for (int x = 0; x < 8; ++x)                      \
+        GUETZLI_LOG(stats, ' %2d', (q)[c][8 * y + x]); \
+      GUETZLI_LOG(stats, '   ');                       \
+    }                                                  \
+    GUETZLI_LOG(stats, '\n');                          \
+  }
+    
+        // The nodes are:
+    // [0, n): the sorted leaf nodes that we start with.
+    // [n]: we add a sentinel here.
+    // [n + 1, 2n): new parent nodes are added here, starting from
+    //              (n+1). These are naturally in ascending order.
+    // [2n]: we add a sentinel at the end as well.
+    // There will be (2n+1) elements at the end.
+    const HuffmanTree sentinel(~static_cast<uint32_t>(0), -1, -1);
+    tree[n] = sentinel;
+    tree[n + 1] = sentinel;
+    
+    inline int Log2FloorNonZero(uint32_t n) {
+#ifdef __GNUC__
+  return 31 ^ __builtin_clz(n);
+#else
+  unsigned int result = 0;
+  while (n >>= 1) result++;
+  return result;
+#endif
+}
+    
+    // An upper estimate of memory usage of Guetzli. The bound is
+// max(kLowerMemusaeMB * 1<<20, pixel_count * kBytesPerPixel)
+constexpr int kBytesPerPixel = 350;
+constexpr int kLowestMemusageMB = 100; // in MB
+    
+      tmp0 = in[stride];
+  tmp1 = kIDCTMatrix[ 1] * tmp0;
+  tmp2 = kIDCTMatrix[ 9] * tmp0;
+  tmp3 = kIDCTMatrix[17] * tmp0;
+  tmp4 = kIDCTMatrix[25] * tmp0;
+  out[0] += tmp1;
+  out[1] += tmp2;
+  out[2] += tmp3;
+  out[3] += tmp4;
+  out[4] -= tmp4;
+  out[5] -= tmp3;
+  out[6] -= tmp2;
+  out[7] -= tmp1;
+    
+      // Read sampling factors and quant table index for each component.
+  std::vector<bool> ids_seen(256, false);
+  for (size_t i = 0; i < jpg->components.size(); ++i) {
+    const int id = ReadUint8(data, pos);
+    if (ids_seen[id]) {   // (cf. section B.2.2, syntax of Ci)
+      fprintf(stderr, 'Duplicate ID %d in SOF.\n', id);
+      jpg->error = JPEG_DUPLICATE_COMPONENT_ID;
+      return false;
+    }
+    ids_seen[id] = true;
+    jpg->components[i].id = id;
+    int factor = ReadUint8(data, pos);
+    int h_samp_factor = factor >> 4;
+    int v_samp_factor = factor & 0xf;
+    VERIFY_INPUT(h_samp_factor, 1, 15, SAMP_FACTOR);
+    VERIFY_INPUT(v_samp_factor, 1, 15, SAMP_FACTOR);
+    jpg->components[i].h_samp_factor = h_samp_factor;
+    jpg->components[i].v_samp_factor = v_samp_factor;
+    jpg->components[i].quant_idx = ReadUint8(data, pos);
+    jpg->max_h_samp_factor = std::max(jpg->max_h_samp_factor, h_samp_factor);
+    jpg->max_v_samp_factor = std::max(jpg->max_v_samp_factor, v_samp_factor);
+  }
