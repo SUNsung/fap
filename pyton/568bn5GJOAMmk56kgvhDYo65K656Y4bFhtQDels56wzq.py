523638e149767201098c0587b0f302c2c36e6286
SUNsung <weatherius@gmail.com>
2018-09-21 12:39:37 +0300

    Rewrite [568bn5GJOAMmk56kgvhDYo65K656Y4bFhtQDels56wzq.py] File in: 2018-09-21 12:39:36

--- pyton/568bn5GJOAMmk56kgvhDYo65K656Y4bFhtQDels56wzq.py
+++ pyton/568bn5GJOAMmk56kgvhDYo65K656Y4bFhtQDels56wzq.py
@@ -1,99 +1,57 @@
 
         
-                        pCertCtx = crypt32.CertFindCertificateInStore(store_handle, X509_ASN_ENCODING, 0, CERT_FIND_SUBJECT_STR, common_name, None)
-                while pCertCtx:
-                    certCtx = CERT_CONTEXT.from_address(pCertCtx)
-                    _certdata = ctypes.string_at(certCtx.pbCertEncoded, certCtx.cbCertEncoded)
-                    if _certdata == certdata:
-                        ca_exists = True
-                        xlog.debug('XX-Net CA already exists')
-                    else:
-                        cert =  OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_ASN1, _certdata)
-                        if hasattr(cert, 'get_subject'):
-                             cert = cert.get_subject()
-                        cert_name = next((v for k, v in cert.get_components() if k == 'CN'), '')
-                        if cert_name == common_name:
-                            ret = crypt32.CertDeleteCertificateFromStore(crypt32.CertDuplicateCertificateContext(pCertCtx))
-                            if ret == 1:
-                                xlog.debug('Invalid Windows CA %r has been removed', common_name)
-                            elif ret == 0 and store == CERT_SYSTEM_STORE_LOCAL_MACHINE:
-                                # to elevate
-                                break
-                    pCertCtx = crypt32.CertFindCertificateInStore(store_handle, X509_ASN_ENCODING, 0, CERT_FIND_SUBJECT_STR, common_name, pCertCtx)
-    
-        for qualified, server, _, _ in server_list:
-        if qualified:
-            best_server = server[0]
-            break
-    log = Log()
-    if best_server:
-        log.write('best server is: %s.' % best_server)
-    else:
-        xlog.warning('no server detected, return default: teredo.remlab.net.')
-        log.write('no server detected, return default: teredo.remlab.net.')
-        best_server = 'teredo.remlab.net'
-    log.close()
-    return best_server
-    
-        server_list = ['teredo.remlab.net','win1710.ipv6.microsoft.com']
-    prober = teredo_prober(server_list, probe_nat=False)
-    prober.timeout = 4
-    server_ip_list = prober.server_ip_list.copy()
-    server_ip = server_ip_list.pop()
-    for _ in range(2):
-        print(prober.qualify_loop(server_ip))
-        prober.rs_cone_flag = prober.rs_cone_flag ^ 1
-    server_ip = server_ip_list.pop()
-    for _ in range(2):
-        print(prober.qualify_loop(server_ip))
-        prober.rs_cone_flag = prober.rs_cone_flag ^ 1
-#    prober.close()
+            def __call__(self, r):
+        # Initialize per-thread state, if needed
+        self.init_per_thread_state()
+        # If we have a saved nonce, skip the 401
+        if self._thread_local.last_nonce:
+            r.headers['Authorization'] = self.build_digest_header(r.method, r.url)
+        try:
+            self._thread_local.pos = r.body.tell()
+        except AttributeError:
+            # In the case of HTTPDigestAuth being reused and the body of
+            # the previous request was a file-like object, pos has the
+            # file position of the previous body. Ensure it's set to
+            # None.
+            self._thread_local.pos = None
+        r.register_hook('response', self.handle_401)
+        r.register_hook('response', self.handle_redirect)
+        self._thread_local.num_401_calls = 1
     
     
-from xlog import getLogger
-xlog = getLogger('gae_proxy')
-xlog.set_buffer(1000)
+class RequestsWarning(Warning):
+    '''Base warning for Requests.'''
+    pass
     
     
-class SniManager(object):
-    plus = ['-', '', '.']
-    end = ['com', 'net', 'ml', 'org', 'us']
+def default_hooks():
+    return dict((event, []) for event in HOOKS)
     
-    - CommonTokenStream: A basic and most commonly used TokenStream
-  implementation.
-- TokenRewriteStream: A modification of CommonTokenStream that allows the
-  stream to be altered (by the Parser). See the 'tweak' example for a usecase.
+        def __repr__(self):
+        return str(dict(self.items()))
     
+        return inner
     
+        def test_basic_waiting_server(self):
+        '''the server waits for the block_server event to be set before closing'''
+        block_server = threading.Event()
     
-        ANTLR generates code that throws exceptions upon recognition error and
-    also generates code to catch these exceptions in each rule.  If you
-    want to quit upon first error, you can turn off the automatic error
-    handling mechanism using rulecatch action, but you still need to
-    override methods mismatch and recoverFromMismatchSet.
+        def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is None:
+            self.stop_event.wait(self.WAIT_EVENT_TIMEOUT)
+        else:
+            if self.wait_to_close_event:
+                # avoid server from waiting for event timeouts
+                # if an exception is found in the main thread
+                self.wait_to_close_event.set()
     
-    In general, the recognition exceptions can track where in a grammar a
-    problem occurred and/or what was the expected input.  While the parser
-    knows its state (such as current input symbol and line info) that
-    state can change before the exception is reported so current token index
-    is computed and stored at exception time.  From this info, you can
-    perhaps print an entire line of input not just a single token, for example.
-    Better to just say the recognizer had a problem and then let the parser
-    figure out a fancy report.
+        return request('delete', url, **kwargs)
+
     
-    '''
+                # Recompute Content-Length
+            self.prepare_content_length(self.body)
     
-    
-    def skip(self):
-        '''
-        Instruct the lexer to skip creating a token for current lexer rule
-        and look for another token.  nextToken() knows to keep looking when
-        a lexer rule finishes with token set to SKIP_TOKEN.  Recall that
-        if token==null at end of any token rule, it creates one for you
-        and emits it.
-        '''
-        
-        self._state.token = SKIP_TOKEN
-    
-                with open('htmlout.html', 'w') as out:
-                out.write(header)
+        def __getitem__(self, name):
+        '''Dict-like __getitem__() for compatibility with client code. Throws
+        exception if there are more than one cookie with name. In that case,
+        use the more explicit get() method instead.
