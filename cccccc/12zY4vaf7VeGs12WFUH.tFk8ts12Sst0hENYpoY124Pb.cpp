9f101a9b7ff97f1cc25cdde05b49f120d6aa40ab
SUNsung <weatherius@gmail.com>
2018-09-21 08:03:04 +0300

    Rewrite [12zY4vaf7VeGs12WFUH.tFk8ts12Sst0hENYpoY124Pb.cpp] File in: 2018-09-21 08:03:03

--- cccccc/12zY4vaf7VeGs12WFUH.tFk8ts12Sst0hENYpoY124Pb.cpp
+++ cccccc/12zY4vaf7VeGs12WFUH.tFk8ts12Sst0hENYpoY124Pb.cpp
@@ -1,273 +1,257 @@
 
         
-        static const char* kModuleName = '_api_implementation';
-static const char kModuleDocstring[] =
-'_api_implementation is a module that exposes compile-time constants that\n'
-'determine the default API implementation to use for Python proto2.\n'
-'\n'
-'It complements api_implementation.py by setting defaults using compile-time\n'
-'constants defined in C, such that one can set defaults at compilation\n'
-'(e.g. with blaze flag --copt=-DPYTHON_PROTO2_CPP_IMPL_V2).';
+        #else  // !defined(__SSE4_1__)
+// Non-Android code here
     
-    const Message* GetCProtoInsidePyProto(PyObject* msg) {
-  return GetCProtoInsidePyProtoPtr(msg);
-}
-Message* MutableCProtoInsidePyProto(PyObject* msg) {
-  return MutableCProtoInsidePyProtoPtr(msg);
-}
-    
-      auto* message2_on_arena =
-      Arena::CreateMessage<protobuf_unittest::TestAllTypes>(&arena);
-    
-    void WriteEnumDocComment(io::Printer* printer, const EnumDescriptor* enumDescriptor) {
-    WriteDocCommentBody(printer, enumDescriptor);
-}
-void WriteEnumValueDocComment(io::Printer* printer, const EnumValueDescriptor* value) {
-    WriteDocCommentBody(printer, value);
-}
-    
-    // Returns the Pascal-cased last part of the proto file. For example,
-// input of 'google/protobuf/foo_bar.proto' would result in 'FooBar'.
-std::string GetFileNameBase(const FileDescriptor* descriptor) {
-    std::string proto_file = descriptor->name();
-    int lastslash = proto_file.find_last_of('/');
-    std::string base = proto_file.substr(lastslash + 1);
-    return UnderscoresToPascalCase(StripDotProto(base));
-}
-    
-    // Generator options (used by csharp_generator.cc):
-struct Options {
-  Options() :
-      file_extension('.cs'),
-      base_namespace(''),
-      base_namespace_specified(false),
-      internal_access(false) {
+    // Computes a reshaped copy of the weight matrix w. If there are no
+// partial_funcs_, it does nothing.
+void IntSimdMatrix::Init(const GENERIC_2D_ARRAY<int8_t>& w) {
+  if (partial_funcs_.empty()) return;
+  int num_out = w.dim1();
+  int num_in = w.dim2() - 1;
+  // The rounded-up sizes of the reshaped weight matrix, excluding biases.
+  int rounded_num_in = Roundup(num_in, num_inputs_per_group_);
+  int rounded_num_out = RoundOutputs(num_out);
+  // Add the bias and compute the required size.
+  shaped_w_.resize((rounded_num_in + 1) * rounded_num_out, 0);
+  int shaped_index = 0;
+  int output = 0;
+  // Each number of registers needs a different format! Iterates over the
+  // different numbers of registers (each a power of 2).
+  for (int num_registers = max_output_registers_; num_registers >= 1;
+       num_registers /= 2) {
+    // The number of outputs that we will generate with this many registers.
+    int num_outputs_per_register_set =
+        num_registers * num_outputs_per_register_;
+    // Use the max number of registers until we have to go fewer.
+    while (output + num_outputs_per_register_set <= rounded_num_out) {
+      // Accumulating outputs in registers saves iterating over the inputs, so
+      // we only have to do it once per output register set.
+      for (int input = 0; input < num_in; input += num_inputs_per_group_) {
+        // Iterate over the number of outputs in a register set.
+        for (int j = 0; j < num_outputs_per_register_set; ++j) {
+          // Inner-most loop corresponds to the number of inputs in an input
+          // group.
+          for (int i = 0; i < num_inputs_per_group_; ++i) {
+            int8_t weight = 0;
+            if (output + j < num_out && input + i < num_in)
+              weight = w(output + j, input + i);
+            shaped_w_[shaped_index++] = weight;
+          }
+        }
+      }
+      // Append the bias weights for the register set.
+      for (int j = 0; j < num_outputs_per_register_set; ++j) {
+        int8_t weight = 0;
+        if (output + j < num_out) weight = w(output + j, num_in);
+        shaped_w_[shaped_index++] = weight;
+      }
+      output += num_outputs_per_register_set;
+    }
   }
-  // Extension of the generated file. Defaults to '.cs'
-  string file_extension;
-  // Base namespace to use to create directory hierarchy. Defaults to ''.
-  // This option allows the simple creation of a conventional C# file layout,
-  // where directories are created relative to a project-specific base
-  // namespace. For example, in a project with a base namespace of PetShop, a
-  // proto of user.proto with a C# namespace of PetShop.Model.Shared would
-  // generate Model/Shared/User.cs underneath the specified --csharp_out
-  // directory.
-  //
-  // If no base namespace is specified, all files are generated in the
-  // --csharp_out directory, with no subdirectories created automatically.
-  string base_namespace;
-  // Whether the base namespace has been explicitly specified by the user.
-  // This is required as the base namespace can be explicitly set to the empty
-  // string, meaning 'create a full directory hierarchy, starting from the first
-  // segment of the namespace.'
-  bool base_namespace_specified;
-  // Whether the generated classes should have accessibility level of 'internal'.
-  // Defaults to false that generates 'public' classes.
-  bool internal_access;
-};
-    
-    namespace google {
-namespace protobuf {
-namespace compiler {
-namespace csharp {
-    }
-    }
-    }
-    }
-    
-    
-    { protected:
-  /*! \brief internal base score of the model */
-  bst_float base_score_;
-  /*! \brief objective function */
-  std::unique_ptr<ObjFunction> obj_;
-  /*! \brief The gradient booster used by the model*/
-  std::unique_ptr<GradientBooster> gbm_;
-  /*! \brief The evaluation metrics used to evaluate the model. */
-  std::vector<std::unique_ptr<Metric> > metrics_;
-};
-    
-    template<typename IndexType>
-class DensifyParser : public dmlc::Parser<IndexType> {
- public:
-  DensifyParser(dmlc::Parser<IndexType>* parser, uint32_t num_col)
-      : parser_(parser), num_col_(num_col) {
-  }
-    }
-    
-    // Store 8 gradient pairs given vectors containing gradient and Hessian
-inline void StoreGpair(xgboost::GradientPair* dst, const Float8& grad,
-                       const Float8& hess) {
-  float* ptr = reinterpret_cast<float*>(dst);
-  __m256 gpair_low = _mm256_unpacklo_ps(grad.x, hess.x);
-  __m256 gpair_high = _mm256_unpackhi_ps(grad.x, hess.x);
-  _mm256_storeu_ps(ptr, _mm256_permute2f128_ps(gpair_low, gpair_high, 0x20));
-  _mm256_storeu_ps(ptr + 8,
-                   _mm256_permute2f128_ps(gpair_low, gpair_high, 0x31));
-}
-}  // namespace avx
-#else
-namespace avx {
-/**
- * \struct  Float8
- *
- * \brief Fallback implementation not using AVX.
- */
-    
-    // comparator functions for sorting pairs in descending order
-inline static bool CmpFirst(const std::pair<float, unsigned> &a,
-                            const std::pair<float, unsigned> &b) {
-  return a.first > b.first;
-}
-inline static bool CmpSecond(const std::pair<float, unsigned> &a,
-                             const std::pair<float, unsigned> &b) {
-  return a.second > b.second;
 }
     
-    #include <xgboost/data.h>
-#include <algorithm>
-#include <vector>
-    
-    #ifndef __STDC_FORMAT_MACROS
-#define __STDC_FORMAT_MACROS
-#endif
-#ifndef GFLAGS
-#include <cstdio>
-int main() {
-  fprintf(stderr, 'Please install gflags to run rocksdb tools\n');
-  return 1;
-}
-#else
-    
-      std::atomic<int> total_stopped_;
-  std::atomic<int> total_delayed_;
-  std::atomic<int> total_compaction_pressure_;
-  uint64_t bytes_left_;
-  uint64_t last_refill_time_;
-  // write rate set when initialization or by `DBImpl::SetDBOptions`
-  uint64_t max_delayed_write_rate_;
-  // current write rate
-  uint64_t delayed_write_rate_;
-    
-    Status PosixMmapFile::Close() {
-  Status s;
-  size_t unused = limit_ - dst_;
-    }
-    
-      // close db
-  for (auto handle : handles) {
-    delete handle;
-  }
-  delete db;
-    
-      // Do some reads and writes to key 'y'
-  s = txn->GetForUpdate(read_options, 'y', &value);
-  txn->Put('y', 'y');
-    
-    void DHTRoutingTableDeserializer::deserialize(const std::string& filename)
+    enum GARBAGE_LEVEL
 {
-  A2_LOG_INFO(fmt('Loading DHT routing table from %s.', filename.c_str()));
-  BufferedFile fp(filename.c_str(), BufferedFile::READ);
-  if (!fp) {
-    throw DL_ABORT_EX(
-        fmt('Failed to load DHT routing table from %s', filename.c_str()));
+  G_NEVER_CRUNCH,
+  G_OK,
+  G_DODGY,
+  G_TERRIBLE
+};
+    
+    ELISTIZE(ParamContent)
+    
+      // Constructors for the various ParamTypes.
+  ParamContent() = default;
+  explicit ParamContent(tesseract::StringParam* it);
+  explicit ParamContent(tesseract::IntParam* it);
+  explicit ParamContent(tesseract::BoolParam* it);
+  explicit ParamContent(tesseract::DoubleParam* it);
+    
+      const UNICHARSET &unicharset = *word->uch_set;
+  word->ClearResults();
+  word2->ClearResults();
+  word->chopped_word = chopped;
+  word2->chopped_word = chopped2;
+  word->SetupBasicsFromChoppedWord(unicharset);
+  word2->SetupBasicsFromChoppedWord(unicharset);
+    
+    // Deserializing an array could give back a different ArrayKind than we need,
+// so we have to go with the slow case of calling a collection constructor.
+NEVER_INLINE
+Object createFromSerialized(CollectionType colType, APCHandle* handle) {
+  auto const col = Object::attach(collections::alloc(colType));
+  auto const arr = handle->toLocal();
+  switch (colType) {
+  case CollectionType::ImmVector:
+  case CollectionType::Vector:
+    static_cast<BaseVector*>(col.get())->init(arr);
+    break;
+  case CollectionType::ImmSet:
+  case CollectionType::Set:
+    static_cast<BaseSet*>(col.get())->init(arr);
+    break;
+  case CollectionType::ImmMap:
+  case CollectionType::Map:
+    static_cast<BaseMap*>(col.get())->init(arr);
+    break;
+  case CollectionType::Pair:
+    not_reached();
+    break;
   }
-  char header[8];
-  memset(header, 0, sizeof(header));
-  // magic
-  header[0] = 0xa1u;
-  header[1] = 0xa2u;
-  // format ID
-  header[2] = 0x02u;
-  // version
-  header[6] = 0;
-  header[7] = 0x03u;
+  return col;
+}
+    
+    #ifndef HPHP_DATA_STREAM_WRAPPER_H
+#define HPHP_DATA_STREAM_WRAPPER_H
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    
+    
+    {}
+    
+    
+    {  while (1) {
+    char buf[8192];
+    bool err = false;
+    ssize_t rbytes = read(srcFd, buf, sizeof(buf));
+    ssize_t wbytes;
+    if (rbytes == 0) break;
+    if (rbytes == -1) {
+      err = true;
+      Logger::Error('read failed: %s', folly::errnoStr(errno).c_str());
+    } else if ((wbytes = write(dstFd, buf, rbytes)) != rbytes) {
+      err = true;
+      Logger::Error('write failed: %zd, %s', wbytes,
+                    folly::errnoStr(errno).c_str());
     }
+    if (err) {
+      close(srcFd);
+      close(dstFd);
+      return -1;
+    }
+  }
+  close(srcFd);
+  close(dstFd);
+  return 0;
+}
+    
+        private:
+        static Microsoft::MSR::CNTK::InputStreamDescription GetInputStreamDescription(const StreamInformation& s, const DeviceDescriptor& device)
+        {
+            assert(s.m_storageFormat == StorageFormat::Dense || s.m_storageFormat == StorageFormat::SparseCSC);
+            auto CNTKdeviceId = AsCNTKImplDeviceId(device);
+            auto CNTKMatrixType = s.m_storageFormat == StorageFormat::Dense ? Microsoft::MSR::CNTK::MatrixType::DENSE : Microsoft::MSR::CNTK::MatrixType::SPARSE;
+            auto CNTKMatrixFormat = AsCNTKImplMatrixFormat(s.m_storageFormat);
+            return Microsoft::MSR::CNTK::InputStreamDescription(s.m_name, CNTKdeviceId, CNTKMatrixType, CNTKMatrixFormat);
+        }
+    
+    
+    {
+    {    private:
+        // Disallow copy and move construction and assignment
+        VariableFields(const VariableFields&) = delete; VariableFields& operator=(const VariableFields& other) = delete; VariableFields(VariableFields&&) = delete; VariableFields& operator=(VariableFields&&) = delete;
+    };
+}
+
+    
+    #include 'stdafx.h'
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS // 'secure' CRT not available on all platforms  --add this at the top of all CPP files that give 'function or variable may be unsafe' warnings
+#endif
+    
+    class Timer
+{
+public:
+    Timer()
+        : m_start(0), m_end(0)
+    {
+    }
+    }
+    
+    public:
+    DeclareConstructorFromConfigWithNumInputs(PerDimMeanVarNormalizationNode);
+    PerDimMeanVarNormalizationNode(DEVICEID_TYPE deviceId, const wstring& name)
+        : Base(deviceId, name)
+    {
+    }
+    
+    
+    {    /** Reverse the current control point array inline, without generating a new one.
+     * @js NA
+     */
+    void reverseInline();
+    /**
+     * @js NA
+     * @lua NA
+     */
+    virtual PointArray* clone() const;
+    /**
+     * @js NA
+     */
+    const std::vector<Vec2>& getControlPoints() const;
+    /**
+     * @js NA
+     */
+    void setControlPoints(std::vector<Vec2> controlPoints);
+private:
+    /** Array that contains the control points. */
+    std::vector<Vec2> _controlPoints;
+};
+    
+    // implementation of Waves
+    
+    /**
+@brief Progress from a percentage to another percentage.
+@since v0.99.1
+*/
+class CC_DLL ProgressFromTo : public ActionInterval
+{
+public:
+    /** 
+     * @brief Create and initializes the action with a duration, a 'from' percentage and a 'to' percentage.
+     * @param duration Specify the duration of the ProgressFromTo action. It's a value in seconds.
+     * @param fromPercentage Specify the source percentage.
+     * @param toPercentage Specify the destination percentage.
+     * @return If the creation success, return a pointer of ProgressFromTo action; otherwise, return nil.
+     */
+    static ProgressFromTo* create(float duration, float fromPercentage, float toPercentage);
+    }
+    
+            if ( frames.empty() )
+        {
+            CCLOG('cocos2d: AnimationCache: None of the frames for animation '%s' were found in the SpriteFrameCache. Animation is not being added to the Animation Cache.', anim.first.c_str());
+            continue;
+        }
+        else if ( frames.size() != frameNameSize )
+        {
+            CCLOG('cocos2d: AnimationCache: An animation in your dictionary refers to a frame which is not in the SpriteFrameCache. Some or all of the frames for the animation '%s' may be missing.', anim.first.c_str());
+        }
+    
+    public:
+  DHTReplaceNodeTask(const std::shared_ptr<DHTBucket>& bucket,
+                     const std::shared_ptr<DHTNode>& newNode);
+    
+      // number of nodes
+  READ_CHECK(fp, &temp32, sizeof(temp32));
+  uint32_t numNodes = ntohl(temp32);
+  // 4bytes reserved
+  readBytes(fp, buf, buf.size(), 4);
+    
+      size_t getExecutingTaskSize() const { return execTasks_.size(); }
     
     
     {} // namespace aria2
+
     
-    void DHTTaskFactoryImpl::setTaskQueue(DHTTaskQueue* taskQueue)
+    DHTTokenUpdateCommand::DHTTokenUpdateCommand(cuid_t cuid, DownloadEngine* e,
+                                             std::chrono::seconds interval)
+    : TimeBasedCommand{cuid, e, std::move(interval)}, tokenTracker_{nullptr}
 {
-  taskQueue_ = taskQueue;
 }
     
-      std::chrono::seconds timeout_;
-    
-      virtual void executeTask() CXX11_OVERRIDE;
-    
-    template< bool Signed >
-struct operations< 1u, Signed > :
-    public msvc_x86_operations< typename make_storage_type< 1u, Signed >::type, operations< 1u, Signed > >
-{
-    typedef msvc_x86_operations< typename make_storage_type< 1u, Signed >::type, operations< 1u, Signed > > base_type;
-    typedef typename base_type::storage_type storage_type;
-    typedef typename make_storage_type< 1u, Signed >::aligned aligned_storage_type;
-    }
-    
-      /**
-   * @brief Constructor
-   */
-  CanFrame() : id(0), len(0), timestamp{0} {
-    std::memset(data, 0, sizeof(data));
-  }
-    
-      void WaitForFinish() {
-    if (thread_send_ != nullptr && thread_send_->joinable()) {
-      thread_send_->join();
-      thread_send_.reset();
-      AINFO << 'Send thread stopped. conf:'
-            << param_ptr_->conf.ShortDebugString();
-    }
-    if (thread_recv_ != nullptr && thread_recv_->joinable()) {
-      thread_recv_->join();
-      thread_recv_.reset();
-      AINFO << 'Recv thread stopped. conf:'
-            << param_ptr_->conf.ShortDebugString();
-    }
-  }
+    class DHTTokenTracker;
     
     
-    { private:
-  NTCAN_HANDLE dev_handler_;
-  CANCardParameter::CANChannelId port_;
-  CMSG send_frames_[MAX_CAN_SEND_FRAME_LEN];
-  CMSG recv_frames_[MAX_CAN_RECV_FRAME_LEN];
+    {  static const std::string UNKNOWN;
 };
-    
-      /**
-   * @brief Get the error string.
-   * @param status The status to get the error string.
-   */
-  std::string GetErrorString(const int32_t status) override;
-    
-    using Clock = common::time::Clock;
-using micros = common::time::micros;
-using apollo::common::ErrorCode;
-    
-    TEST(SocketCanClientRawTest, simple_test) {
-  CANCardParameter param;
-  param.set_brand(CANCardParameter::SOCKET_CAN_RAW);
-  param.set_channel_id(CANCardParameter::CHANNEL_ID_ZERO);
-    }
-    
-    #include 'modules/drivers/canbus/can_comm/message_manager.h'
-    
-    /**
- * @namespace apollo::drivers::canbus
- * @brief apollo::drivers::canbus
- */
-namespace apollo {
-namespace drivers {
-namespace canbus {
-    }
-    }
-    }
-    
-    #include 'modules/drivers/canbus/common/byte.h'
-    
-    #ifndef MODULES_DRIVERS_CANBUS_COMMON_CANBUS_CONSTS_H_
-#define MODULES_DRIVERS_CANBUS_COMMON_CANBUS_CONSTS_H_
-    
-    // data file
-DECLARE_string(sensor_conf_file);
