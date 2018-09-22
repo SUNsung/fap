7b48655e2d1a266384836296902a3771ce914b31
SUNsung <weatherius@gmail.com>
2018-09-22 22:11:28 +0300

    Rewrite [56i9GW1Ho5kvU561l2AVYjN4eg56p2q9bbBQcQ.566Vf.rb] File in: 2018-09-22 22:11:28

--- ruby/56i9GW1Ho5kvU561l2AVYjN4eg56p2q9bbBQcQ.566Vf.rb
+++ ruby/56i9GW1Ho5kvU561l2AVYjN4eg56p2q9bbBQcQ.566Vf.rb
@@ -1,138 +1,120 @@
 
         
-              # Creates a check box form input tag.
-      #
-      # ==== Options
-      # * <tt>:disabled</tt> - If set to true, the user will not be able to use this input.
-      # * Any other key creates standard HTML options for the tag.
-      #
-      # ==== Examples
-      #   check_box_tag 'accept'
-      #   # => <input id='accept' name='accept' type='checkbox' value='1' />
-      #
-      #   check_box_tag 'rock', 'rock music'
-      #   # => <input id='rock' name='rock' type='checkbox' value='rock music' />
-      #
-      #   check_box_tag 'receive_email', 'yes', true
-      #   # => <input checked='checked' id='receive_email' name='receive_email' type='checkbox' value='yes' />
-      #
-      #   check_box_tag 'tos', 'yes', false, class: 'accept_tos'
-      #   # => <input class='accept_tos' id='tos' name='tos' type='checkbox' value='yes' />
-      #
-      #   check_box_tag 'eula', 'accepted', false, disabled: true
-      #   # => <input disabled='disabled' id='eula' name='eula' type='checkbox' value='accepted' />
-      def check_box_tag(name, value = '1', checked = false, options = {})
-        html_options = { 'type' => 'checkbox', 'name' => name, 'id' => sanitize_to_id(name), 'value' => value }.update(options.stringify_keys)
-        html_options['checked'] = 'checked' if checked
-        tag :input, html_options
+            Category.transaction do
+      lounge.group_names = ['trust_level_3']
+      unless lounge.save
+        puts lounge.errors.full_messages
+        raise 'Failed to set permissions on trust level 3 lounge category!'
       end
     
-              def select_type
-            self.class.select_type
+            unless post && post.id
+          puts post.errors.full_messages if post
+          puts creator.errors.inspect
+          raise 'Failed to create description for Staff category!'
+        end
+    
+        # Accesses a machine by UUID and returns a {MachineIndex::Entry}
+    #
+    # The entry returned is locked and can't be read again or updated by
+    # this process or any other. To unlock the machine, call {#release}
+    # with the entry.
+    #
+    # You can only {#set} an entry (update) when the lock is held.
+    #
+    # @param [String] uuid UUID for the machine to access.
+    # @return [MachineIndex::Entry]
+    def get(uuid)
+      entry = nil
+    
+            # Initializes the system. Any subclasses MUST make sure this
+        # method is called on the parent. Therefore, if a subclass overrides
+        # `initialize`, then you must call `super`.
+        def initialize(vm)
+          @vm = vm
+        end
+    
+              @registered.each do |plugin|
+            plugin.components.provider_capabilities.each do |provider, caps|
+              results[provider].merge!(caps)
+            end
           end
     
-              def field_type
-            self.class.field_type
+      it 'raises a TypeError when passed nil' do
+    lambda { sleep(nil)   }.should raise_error(TypeError)
+  end
+    
+    describe 'Kernel.test' do
+  it 'needs to be reviewed for spec completeness'
+end
+
+    
+          # Creates a masked version of the authenticity token that varies
+      # on each request. The masking is used to mitigate SSL attacks
+      # like BREACH.
+      def mask_token(token)
+        token = decode_token(token)
+        one_time_pad = SecureRandom.random_bytes(token.length)
+        encrypted_token = xor_byte_strings(one_time_pad, token)
+        masked_token = one_time_pad + encrypted_token
+        encode_token(masked_token)
+      end
+    
+        it 'sets a session authenticity token if one does not exist' do
+      session = {}
+      allow(Rack::Protection::AuthenticityToken).to receive(:random_token).and_return(token)
+      allow_any_instance_of(Rack::Protection::AuthenticityToken).to receive(:mask_token).and_return(masked_token)
+      Rack::Protection::AuthenticityToken.token(session)
+      expect(session[:csrf]).to eq(token)
+    end
+  end
+    
+      describe '#referrer' do
+    it 'Reads referrer from Referer header' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
+      expect(subject.referrer(env)).to eq('bar.com')
+    end
+    
+            def find_address
+          if @order.bill_address_id == params[:id].to_i
+            @order.bill_address
+          elsif @order.ship_address_id == params[:id].to_i
+            @order.ship_address
+          else
+            raise CanCan::AccessDenied
           end
+        end
       end
     end
   end
 end
 
     
-    require 'active_support/log_subscriber'
+            def next
+          authorize! :update, @order, order_token
+          @order.next!
+          respond_with(@order, default_template: 'spree/api/v1/orders/show', status: 200)
+        rescue StateMachines::InvalidTransition
+          respond_with(@order, default_template: 'spree/api/v1/orders/could_not_transition', status: 422)
+        end
     
-        # Register callbacks
-    self.listener.on_client_connect_proc = Proc.new { |cli|
-      on_client_connect(cli)
-    }
-    self.listener.on_client_data_proc = Proc.new { |cli|
-      on_client_data(cli)
-    }
+              @line_item = Spree::Cart::AddItem.call(order: order,
+                                                 variant: variant,
+                                                 quantity: params[:line_item][:quantity],
+                                                 options: line_item_params[:options]).value
+          if @line_item.errors.empty?
+            respond_with(@line_item, status: 201, default_template: :show)
+          else
+            invalid_resource!(@line_item)
+          end
+        end
     
-    # Supported
-IAX_SUPPORTED_CODECS  = IAX_CODEC_G711_MULAW | IAX_CODEC_G711_ALAW | IAX_CODEC_LINEAR_PCM
-    
-              # Encodes the realm field
-          #
-          # @return [String]
-          def encode_realm
-            encoded = ''
-            encoded << [realm.length].pack('N')
-            encoded << realm
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module CredentialCache
-        # This class provides a representation of credential times stored in the Kerberos Credential Cache.
-        class Time < Element
-          # @!attribute auth_time
-          #   @return [Integer]
-          attr_accessor :auth_time
-          # @!attribute start_time
-          #   @return [Integer]
-          attr_accessor :start_time
-          # @!attribute end_time
-          #   @return [Integer]
-          attr_accessor :end_time
-          # @!attribute renew_till
-          #   @return [Integer]
-          attr_accessor :renew_till
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Crypto
-        module Rc4Hmac
-          # Decrypts the cipher using RC4-HMAC schema
-          #
-          # @param cipher [String] the data to decrypt
-          # @param key [String] the key to decrypt
-          # @param msg_type [Integer] the message type
-          # @return [String] the decrypted cipher
-          # @raise [RuntimeError] if decryption doesn't succeed
-          def decrypt_rc4_hmac(cipher, key, msg_type)
-            unless cipher && cipher.length > 16
-              raise ::RuntimeError, 'RC4-HMAC decryption failed'
-            end
-    
-              # Decodes the last_req from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Array<Rex::Proto::Kerberos::Model::LastRequest>]
-          def decode_last_req(input)
-            last_requests = []
-            input.value[0].value.each do |last_request|
-              last_requests << Rex::Proto::Kerberos::Model::LastRequest.decode(last_request)
-            end
-    
-              # Decodes the Rex::Proto::Kerberos::Model::KdcResponse from an input
-          #
-          # @param input [String, OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [self] if decoding succeeds
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode(input)
-            case input
-            when String
-              decode_string(input)
-            when OpenSSL::ASN1::ASN1Data
-              decode_asn1(input)
+                respond_with(@order, default_template: :show, status: 201)
+          else
+            @order = Spree::Order.create!(user: current_api_user, store: current_store)
+            if Cart::Update.call(order: @order, params: order_params).success?
+              respond_with(@order, default_template: :show, status: 201)
             else
-              raise ::RuntimeError, 'Failed to decode KdcResponse, invalid input'
+              invalid_resource!(@order)
             end
-    
-        # Outputs the post.date as formatted html, with hooks for CSS styling.
-    #
-    #  +date+ is the date object to format as HTML.
-    #
-    # Returns string
-    def date_to_html_string(date)
-      result = '<span class='month'>' + date.strftime('%b').upcase + '</span> '
-      result << date.strftime('<span class='day'>%d</span> ')
-      result << date.strftime('<span class='year'>%Y</span> ')
-      result
-    end
-    
-        def render(context)
-      includes_dir = File.join(context.registers[:site].source, '_includes')
+          end
+        end
