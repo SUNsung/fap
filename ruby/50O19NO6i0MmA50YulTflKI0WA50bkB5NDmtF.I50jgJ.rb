4f64c8fffe4c96b07a264b192242cfe59ec762f9
SUNsung <weatherius@gmail.com>
2018-09-22 15:19:05 +0300

    Rewrite [50O19NO6i0MmA50YulTflKI0WA50bkB5NDmtF.I50jgJ.rb] File in: 2018-09-22 15:19:04

--- ruby/50O19NO6i0MmA50YulTflKI0WA50bkB5NDmtF.I50jgJ.rb
+++ ruby/50O19NO6i0MmA50YulTflKI0WA50bkB5NDmtF.I50jgJ.rb
@@ -1,154 +1,61 @@
 
         
-                  @coverage.to_f.round(2)
-        end
-    
-            def key_text
-          @entity.to_s
-        end
-    
-            # The logger for this class.
-        LOGGER = Log4r::Logger.new('vagrant::plugin::v1::plugin')
-    
-            def initialize(env, config)
-          @env    = env
-          @config = config
-        end
-    
-            # This returns all the registered provider capabilities.
+                # Sends a Kerberos Request over a tcp connection
         #
-        # @return [Hash]
-        def provider_capabilities
-          results = Hash.new { |h, k| h[k] = Registry.new }
-    
-    @@ layout
-<html>
-  <head>
-    <title>Super Simple Chat with Sinatra</title>
-    <meta charset='utf-8' />
-    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
-  </head>
-  <body><%= yield %></body>
-</html>
-    
-        <% unless bad_request?(exception) %>
-      <div id='get'>
-        <h3 id='get-info'>GET</h3>
-        <% if req.GET and not req.GET.empty? %>
-          <table class='req'>
-            <tr>
-              <th>Variable</th>
-              <th>Value</th>
-            </tr>
-             <% req.GET.sort_by { |k, v| k.to_s }.each { |key, val| %>
-            <tr>
-              <td><%=h key %></td>
-              <td class='code'><div><%=h val.inspect %></div></td>
-            </tr>
-            <% } %>
-          </table>
-        <% else %>
-          <p class='no-data'>No GET data.</p>
-        <% end %>
-        <div class='clear'></div>
-      </div> <!-- /GET -->
-    
-          def compare_with_real_token(token, session)
-        secure_compare(token, real_token(session))
-      end
-    
-          def accepts?(env)
-        cookie_header = env['HTTP_COOKIE']
-        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
-        cookies.each do |k, v|
-          if k == session_key && Array(v).size > 1
-            bad_cookies << k
-          elsif k != session_key && Rack::Utils.unescape(k) == session_key
-            bad_cookies << k
-          end
-        end
-        bad_cookies.empty?
-      end
-    
-      desc 'Install all spree gems'
-  task install: :build do
-    for_each_gem do |gem_path|
-      Bundler.with_clean_env do
-        sh 'gem install #{gem_path}'
-      end
-    end
-  end
-    
-              if Spree::Cart::Update.call(order: @order, params: line_items_attributes).success?
-            @line_item.reload
-            respond_with(@line_item, default_template: :show)
-          else
-            invalid_resource!(@line_item)
-          end
+        # @param req [Rex::Proto::Kerberos::Model::KdcRequest] the request to send
+        # @return [Integer] the number of bytes sent
+        # @raise [RuntimeError] if the request can't be encoded
+        def send_request_tcp(req)
+          data = req.encode
+          length = [data.length].pack('N')
+          connection.put(length + data)
         end
     
-            def new; end
+              # Encodes a Rex::Proto::Kerberos::Model::Checksum into an ASN.1 String
+          #
+          # @return [String]
+          def encode
+            elems = []
+            elems << OpenSSL::ASN1::ASN1Data.new([encode_type], 0, :CONTEXT_SPECIFIC)
+            elems << OpenSSL::ASN1::ASN1Data.new([encode_checksum], 1, :CONTEXT_SPECIFIC)
     
-              if @product_property.update_attributes(product_property_params)
-            respond_with(@product_property, status: 200, default_template: :show)
-          else
-            invalid_resource!(@product_property)
-          end
-        end
+        BUNDLE_DIR = ::File.join(LOGSTASH_HOME, 'vendor', 'bundle')
+    GEMFILE_PATH = ::File.join(LOGSTASH_HOME, 'Gemfile')
+    LOCAL_GEM_PATH = ::File.join(LOGSTASH_HOME, 'vendor', 'local_gems')
+    CACHE_PATH = ::File.join(LOGSTASH_HOME, 'vendor', 'cache')
+    LOCKFILE = Pathname.new(::File.join(LOGSTASH_HOME, 'Gemfile.lock'))
+    GEMFILE = Pathname.new(::File.join(LOGSTASH_HOME, 'Gemfile'))
     
-            def show
-          authorize! :admin, ReturnAuthorization
-          @return_authorization = order.return_authorizations.accessible_by(current_ability, :read).find(params[:id])
-          respond_with(@return_authorization)
-        end
-    
-        output = LogStash::Bundler.invoke!(bundler_options)
-    
-        signal_error('No plugins found') if filtered_specs.empty?
-    
-            PluginManager.ui.debug('Looking if package named: #{plugin_name} exists at #{uri}')
-    
-        FileUtils.rm_rf(LogStash::Environment::CACHE_PATH)
-    validate_cache_location
-    archive_manager.extract(package_file, LogStash::Environment::CACHE_PATH)
-    puts('Unpacked at #{LogStash::Environment::CACHE_PATH}')
-    puts('The unpacked plugins can now be installed in local-only mode using bin/logstash-plugin install --local [plugin name]')
+      option '--installed', :flag, 'List only explicitly installed plugins using bin/logstash-plugin install ...', :default => false
+  option '--verbose', :flag, 'Also show plugin version number', :default => false
+  option '--group', 'NAME', 'Filter plugins per group: input, output, filter or codec' do |arg|
+    raise(ArgumentError, 'should be one of: input, output, filter or codec') unless ['input', 'output', 'filter', 'codec', 'pack'].include?(arg)
+    arg
   end
     
-      # create list of plugins to update
-  def plugins_to_update(previous_gem_specs_map)
-    if update_all?
-      previous_gem_specs_map.values.map{|spec| spec.name}
-    else
-      # If the plugins isn't available in the gemspec or in 
-      # the gemfile defined with a local path, we assume the plugins is not
-      # installed.
-      not_installed = plugins_arg.select{|plugin| !previous_gem_specs_map.has_key?(plugin.downcase) && !gemfile.find(plugin) }
-      signal_error('Plugin #{not_installed.join(', ')} is not installed so it cannot be updated, aborting') unless not_installed.empty?
-      plugins_arg
-    end
-  end
+    require 'clamp'
+require 'pluginmanager/util'
+require 'pluginmanager/gemfile'
+require 'pluginmanager/install'
+require 'pluginmanager/remove'
+require 'pluginmanager/list'
+require 'pluginmanager/update'
+require 'pluginmanager/pack'
+require 'pluginmanager/unpack'
+require 'pluginmanager/generate'
+require 'pluginmanager/prepare_offline_pack'
+require 'pluginmanager/proxy_support'
+configure_proxy
     
-    project_versions_yaml_path = File.expand_path('../versions.yml', File.dirname(__FILE__))
-if File.exist?(project_versions_yaml_path)
-  # we need to copy the project level versions.yml into the gem root
-  # to be able to package it into the gems file structure
-  # as the require 'logstash-core-plugin-api/version' loads the yaml file from within the gem root.
-  #
-  # we ignore the copy in git and we overwrite an existing file
-  # each time we build the logstash-core gem
-  original_lines = IO.readlines(project_versions_yaml_path)
-  original_lines << ''
-  original_lines << '# This is a copy the project level versions.yml into this gem's root and it is created when the gemspec is evaluated.'
-  gem_versions_yaml_path = File.expand_path('./versions-gem-copy.yml', File.dirname(__FILE__))
-  File.open(gem_versions_yaml_path, 'w') do |new_file|
-    # create or overwrite
-    new_file.puts(original_lines)
-  end
-end
+      class OfflinePluginPackager
+    LOGSTASH_DIR = 'logstash'
+    DEPENDENCIES_DIR = ::File.join(LOGSTASH_DIR, 'dependencies')
     
-              it 'successfully install the plugin when verification is disabled' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify logstash-filter-qatest')
+        puts('Packaging plugins for offline usage')
+    
+              it 'successfully install the plugin' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install #{gem_path_on_vagrant}')
             expect(command).to install_successfully
-            expect(logstash).to have_installed?('logstash-filter-qatest')
+            expect(logstash).to have_installed?('logstash-filter-dns')
           end
+        end
