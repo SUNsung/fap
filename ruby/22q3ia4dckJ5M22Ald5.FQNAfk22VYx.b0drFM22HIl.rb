17c1ebd1b4e52907d3344de5e514c66294203399
SUNsung <weatherius@gmail.com>
2018-09-28 01:38:20 +0300

    Rewrite [22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb] File in: 2018-09-28 01:38:19

--- ruby/22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb
+++ ruby/22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb
@@ -1,121 +1,180 @@
 
         
-              # Converts the array to a comma-separated sentence where the last element is
-      # joined by the connector word. This is the html_safe-aware version of
-      # ActiveSupport's {Array#to_sentence}[http://api.rubyonrails.org/classes/Array.html#method-i-to_sentence].
-      #
-      def to_sentence(array, options = {})
-        options.assert_valid_keys(:words_connector, :two_words_connector, :last_word_connector, :locale)
+        Benchmark.ips do |x|
+  x.report('local-require') { local_require }
+  x.report('global-require') { global_require }
+  x.report('graceful-require') { graceful_require }
+  x.compare!
+end
+
     
-            class RadioButtonBuilder < Builder # :nodoc:
-          def radio_button(extra_html_options = {})
-            html_options = extra_html_options.merge(@input_html_options)
-            html_options[:skip_default_ids] = false
-            @template_object.radio_button(@object_name, @method_name, @value, html_options)
+    # For this pull request, which changes Page#dir
+# https://github.com/jekyll/jekyll/pull/4403
+    
+      [before || '.',
+    after || '.',]
+end
+    
+      all_files       = `git ls-files -z`.split('\x0')
+  s.files         = all_files.grep(%r!^(exe|lib|rubocop)/|^.rubocop.yml$!)
+  s.executables   = all_files.grep(%r!^exe/!) { |f| File.basename(f) }
+  s.bindir        = 'exe'
+  s.require_paths = ['lib']
+    
+        s = nil
+    homebrew_site_packages = Language::Python.homebrew_site_packages
+    user_site_packages = Language::Python.user_site_packages 'python'
+    pth_file = user_site_packages/'homebrew.pth'
+    instructions = <<-EOS.undent.gsub(/^/, '  ')
+      mkdir -p #{user_site_packages}
+      echo 'import site; site.addsitedir('#{homebrew_site_packages}')' >> #{pth_file}
+    EOS
+    
+        def self.rm_DS_Store
+      paths = Queue.new
+      %w[Cellar Frameworks Library bin etc include lib opt sbin share var].
+        map { |p| HOMEBREW_PREFIX/p }.each { |p| paths << p if p.exist? }
+      workers = (0...Hardware::CPU.cores).map do
+        Thread.new do
+          begin
+            while p = paths.pop(true)
+              quiet_system 'find', p, '-name', '.DS_Store', '-delete'
+            end
+          rescue ThreadError # ignore empty queue error
           end
         end
-    
-                options = options.dup
-            options[:field_name]           = @method_name
-            options[:include_position]     = true
-            options[:prefix]             ||= @object_name
-            options[:index]                = @auto_index if @auto_index && !options.has_key?(:index)
-    
-          def exists?(name, prefixes = [], partial = false, keys = [], **options)
-        @view_paths.exists?(*args_for_lookup(name, prefixes, partial, keys, options))
       end
-      alias :template_exists? :exists?
+      workers.map(&:join)
+    end
     
-          def typecast(paths)
-        paths.map do |path|
-          case path
-          when Pathname, String
-            OptimizedFileSystemResolver.new path.to_s
-          else
-            path
-          end
+          out = checks.send(method)
+      unless out.nil? || out.empty?
+        if first_warning
+          $stderr.puts <<-EOS.undent
+            #{Tty.white}Please note that these warnings are just used to help the Homebrew maintainers
+            with debugging if you file an issue. If everything you use Homebrew for is
+            working fine: please don't worry and just ignore them. Thanks!#{Tty.reset}
+          EOS
         end
+    
+        results.map do |name|
+      begin
+        formula = Formulary.factory(name)
+        canonical_name = formula.name
+        canonical_full_name = formula.full_name
+      rescue
+        canonical_name = canonical_full_name = name
       end
+      # Ignore aliases from results when the full name was also found
+      if aliases.include?(name) && results.include?(canonical_full_name)
+        next
+      elsif (HOMEBREW_CELLAR/canonical_name).directory?
+        pretty_installed(name)
+      else
+        name
+      end
+    end.compact
   end
 end
 
     
-          def flag(name, description: nil, required_for: nil, depends_on: nil)
-        if name.end_with? '='
-          required = OptionParser::REQUIRED_ARGUMENT
-          name.chomp! '='
-        else
-          required = OptionParser::OPTIONAL_ARGUMENT
-        end
-        description = option_to_description(name) if description.nil?
-        @parser.on(name, description, required) do |option_value|
-          Homebrew.args[option_to_name(name)] = option_value
-        end
-    
-      def external_commands
-    cmd_paths = PATH.new(ENV['PATH']).append(Tap.cmd_directories)
-    cmd_paths.each_with_object([]) do |path, cmds|
-      Dir['#{path}/brew-*'].each do |file|
-        next unless File.executable?(file)
-        cmd = File.basename(file, '.rb')[5..-1]
-        next if cmd.include?('.')
-        cmds << cmd
+      def dump_formula_report(key, title)
+    formulae = select_formula(key).sort.map do |name, new_name|
+      # Format list items of renamed formulae
+      if key == :R
+        name = pretty_installed(name) if installed?(name)
+        new_name = pretty_installed(new_name) if installed?(new_name)
+        '#{name} -> #{new_name}'
+      else
+        installed?(name) ? pretty_installed(name) : name
       end
-    end.sort
-  end
-    
-    # Undocumented options:
-#     `-D` activates debugging and profiling of the audit methods (not the same as `--debug`)
-#     `--list-checks` lists all audit methods
-    
-        if GitHub.api_credentials_type == :none
-      puts <<~EOS
-        You can create a new personal access token:
-         #{GitHub::ALL_SCOPES_URL}
-        and then set the new HOMEBREW_GITHUB_API_TOKEN as the authentication method.
-    
-        print_remaining_files remaining_root_files, root, other
-  end
-    
-        return if ARGV.dry_run?
-    
-          if rails?
-        register_rails_engine
-      elsif lotus?
-        register_lotus
-      elsif sprockets?
-        register_sprockets
-      end
-    
-        # convert recursively evaluated selector $list to @for loop
-    def mixin_all_grid_columns(css, selector: raise('pass class'), from: 1, to: raise('pass to'))
-      mxn_def = css.each_line.first.strip
-      # inject local variables as default arguments
-      # this is to avoid overwriting outer variables with the same name with Sass <= 3.3
-      # see also: https://github.com/twbs/bootstrap-sass/issues/636
-      locals = <<-SASS.strip
-        $i: #{from}, $list: '#{selector}'
-      SASS
-      mxn_def.sub!(/(\(?)(\)\s*\{)/) {  '#{$1}#{', ' if $1.empty?}#{locals}#{$2}' }
-      step_body = (css =~ /\$list \{\n(.*?)\n[ ]*\}/m) && $1
-<<-SASS
-// [converter] This is defined recursively in LESS, but Sass supports real loops
-#{mxn_def}
-  @for $i from (#{from} + 1) through #{to} {
-    $list: '\#{$list}, #{selector}';
-  }
-  \#{$list} {
-#{unindent step_body, 2}
-  }
-}
-SASS
     end
     
-              context 'and a comment after the last element' do
-            let(:b_comment) { ' # comment b' }
+          expect(value_for(user.id, dt)).to eq(0)
+      expect(limit_reached_for(user.id, dt)).to eq(false)
     
-            when_branches.each do |condition|
-          yield condition
-        end
+    describe GroupUser do
     
-            private
+        def action_completed(action_name, status: nil)
+      completion_context = FastlaneCore::ActionCompletionContext.context_for_action_name(action_name,
+                                                                                         args: ARGV,
+                                                                                         status: status)
+      FastlaneCore.session.action_completed(completion_context: completion_context)
+    end
+  end
+end
+
+    
+        # Some actions have special handling in fast_file.rb, that means we can't directly call the action
+    # but we have to use the same logic that is in fast_file.rb instead.
+    # That's where this switch statement comes into play
+    def run_action_requiring_special_handling(command: nil, parameter_map: nil, action_return_type: nil)
+      action_return = nil
+      closure_argument_value = nil # only used if the action uses it
+    
+          context 'as array with spaces in name and directory' do
+        let(:path) { ['my file.txt', 'some dir/your file.txt'] }
+    
+          it 'generates the correct git command with a shell-escaped message' do
+        message = 'message with 'quotes' (and parens)'
+        result = Fastlane::FastFile.new.parse('lane :test do
+          git_commit(path: './fastlane/README.md', message: \'#{message}\')
+        end').runner.execute(:test)
+        expect(result).to eq('git commit -m #{message.shellescape} ./fastlane/README.md')
+      end
+    end
+  end
+end
+
+    
+          it 'works with multiple ignore patterns' do
+        pattern1 = 'Pods/*'
+        pattern2 = '../**/*/Xcode*'
+        result = Fastlane::FastFile.new.parse('lane :test do
+          slather({
+            ignore: ['#{pattern1}', '#{pattern2}'],
+            proj: 'foo.xcodeproj'
+          })
+        end').runner.execute(:test)
+    
+        def help_default_value
+      return '#{self.default_value} *'.strip if self.default_value_dynamic
+      return '' if self.default_value.nil?
+      return '''' if self.default_value.instance_of?(String) && self.default_value.empty?
+      return ':#{self.default_value}' if self.default_value.instance_of?(Symbol)
+    
+          it 'should shell escape keychain names when checking for installation' do
+        expect(FastlaneCore::CertChecker).to receive(:wwdr_keychain).and_return(keychain_name)
+        expect(FastlaneCore::Helper).to receive(:backticks).with(name_regex, anything).and_return('')
+    
+    Then /^'([^']*)' should be post (\d+)$/ do |post_text, position|
+  stream_element_numbers_content(position).should have_content(post_text)
+end
+    
+    RSpec::Matchers.define :have_value do |expected|
+  match do |actual|
+    await_condition { actual.value && actual.value.include?(expected) }
+  end
+    
+      def post_path_by_content(text)
+    p = Post.find_by_text(text)
+    post_path(p)
+  end
+    
+      # fill change password section on the user edit page
+  def fill_change_password_section(cur_pass, new_pass, confirm_pass)
+    fill_in 'user_current_password', :with => cur_pass
+    fill_in 'user_password', :with => new_pass
+    fill_in 'user_password_confirmation', :with => confirm_pass
+  end
+    
+        it 'generates a jasmine fixture', :fixture => true do
+      contact = alice.contact_for(bob.person)
+      aspect = alice.aspects.create(:name => 'people')
+      contact.aspects << aspect
+      contact.save
+      get :new, params: {person_id: bob.person.id}
+      save_fixture(html_for('body'), 'status_message_new')
+    end
+  end
+end
