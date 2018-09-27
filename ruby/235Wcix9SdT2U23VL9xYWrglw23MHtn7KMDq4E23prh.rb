9c65aae4ceac75a204e9530e61f42b0285994ac8
SUNsung <weatherius@gmail.com>
2018-09-27 15:30:33 +0300

    Rewrite [235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb] File in: 2018-09-27 15:30:32

--- ruby/235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb
+++ ruby/235Wcix9SdT2U23VL9xYWrglw23MHtn7KMDq4E23prh.rb
@@ -1,34 +1,6 @@
 
         
-          def self.repository(tap = nil)
-    if tap.nil? || tap.core_tap?
-      'bottles'
-    else
-      'bottles-#{tap.repo}'
-    end
-  end
-end
-    
-    class BuildEnvironment
-  def initialize(*settings)
-    @settings = Set.new(*settings)
-  end
-    
-      # True if a {Formula} is being built as a bottle (i.e. binary package).
-  def bottle?
-    include? 'build-bottle'
-  end
-    
-      def app_caveats
-    if keg && keg.app_installed?
-      <<-EOS.undent
-        .app bundles were installed.
-        Run `brew linkapps #{keg.name}` to symlink these to /Applications.
-      EOS
-    end
-  end
-    
-        def self.cleanup_lockfiles
+            def self.cleanup_lockfiles
       return unless HOMEBREW_CACHE_FORMULA.directory?
       candidates = HOMEBREW_CACHE_FORMULA.children
       lockfiles  = candidates.select { |f| f.file? && f.extname == '.brewing' }
@@ -38,42 +10,22 @@ end
       end
     end
     
-    puts '\nUnable to find an RSS feed for the following blogs:'
-puts '==================================================='
-unavailable.each do |b|
-  puts '#{b.name} | #{b.web_url}'
+      def find_internal_commands(directory)
+    directory.children.reduce([]) do |cmds, f|
+      cmds << f.basename.to_s.sub(/\.(?:rb|sh)$/, '') if f.file?
+      cmds
+    end
+  end
 end
-puts '==================================================='
 
     
-          opts.on('--trace', :NONE, 'Show a full Ruby stack trace on error') do
-        @options[:trace] = true
-      end
-    end
+      def std_cmake_parameters
+    '-DCMAKE_INSTALL_PREFIX='#{prefix}' -DCMAKE_BUILD_TYPE=None -DCMAKE_FIND_FRAMEWORK=LAST -Wno-dev'
+  end
     
-          Sass::Plugin.on_template_modified do |template|
-        puts '>>> Change detected to: #{template}'
-        STDOUT.flush
-      end
-      Sass::Plugin.on_template_created do |template|
-        puts '>>> New template detected: #{template}'
-        STDOUT.flush
-      end
-      Sass::Plugin.on_template_deleted do |template|
-        puts '>>> Deleted template detected: #{template}'
-        STDOUT.flush
-      end
-    
-          # Returns the time the given Sass file was last modified.
-      #
-      # If the given file has been deleted or the time can't be accessed
-      # for some other reason, this should return nil.
-      #
-      # @param uri [String] The URI of the file to check.
-      #   Comes from a `:filename` option set on an engine returned by this importer.
-      # @param options [{Symbol => Object}] Options for the Sass file
-      #   containing the `@import` currently being checked.
-      # @return [Time, nil]
-      def mtime(uri, options)
-        Sass::Util.abstract(self)
-      end
+    # See chcase for an example
+class ScriptFileFormula < Formula
+  def install
+    bin.install Dir['*']
+  end
+end
