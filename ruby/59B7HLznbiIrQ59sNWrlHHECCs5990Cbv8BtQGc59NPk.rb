edc825df40a132bb0008e0cb154149dda5facd0d
SUNsung <weatherius@gmail.com>
2018-09-28 00:50:28 +0300

    Rewrite [59B7HLznbiIrQ59sNWrlHHECCs5990Cbv8BtQGc59NPk.rb] File in: 2018-09-28 00:50:27

--- ruby/59B7HLznbiIrQ59sNWrlHHECCs5990Cbv8BtQGc59NPk.rb
+++ ruby/59B7HLznbiIrQ59sNWrlHHECCs5990Cbv8BtQGc59NPk.rb
@@ -1,114 +1,97 @@
 
         
-          def set_account
-    @account = Account.find_local!(params[:account_username])
-  end
-    
-      before_action :set_account
-  before_action :set_statuses
-    
-        def resource_params
-      params.require(:account_moderation_note).permit(
-        :content,
-        :target_account_id
-      )
-    end
-    
-        def check_confirmation
-      if @user.confirmed?
-        flash[:error] = I18n.t('admin.accounts.resend_confirmation.already_confirmed')
-        redirect_to admin_accounts_path
-      end
-    end
-  end
+        desc 'Open an irb session preloaded with this library'
+task :console do
+  sh 'irb -rubygems -r ./lib/#{name}.rb'
 end
 
     
-      def encoded_challenge
-    HTMLEntities.new.encode(params['hub.challenge'])
+    def local_require
+  require 'json'
+  JSON.pretty_generate(DATA)
+end
+    
+                add_build_options(c)
+    
+            def scaffold_path
+          '_posts/0000-00-00-welcome-to-jekyll.markdown.erb'
+        end
+    
+                  EM.start_server(
+                opts['host'],
+                opts['livereload_port'],
+                HttpAwareConnection,
+                opts
+              ) do |ws|
+                handle_websockets_event(ws)
+              end
+    
+      def devise_i18n_options(options)
+    options
   end
     
-              # Encodes the authenticator field
-          #
-          # @return [String]
-          def encode_authenticator
-            authenticator.encode
-          end
+                bypass_sign_in(user)
+          DEPRECATION
+          warden.session_serializer.store(resource, scope)
+        elsif warden.user(scope) == resource && !options.delete(:force)
+          # Do nothing. User already signed in and we are not forcing it.
+          true
+        else
+          warden.set_user(resource, options.merge!(scope: scope))
+        end
+      end
+    
+            routes.each do |module_name, actions|
+          [:path, :url].each do |path_or_url|
+            actions.each do |action|
+              action = action ? '#{action}_' : ''
+              method = :'#{action}#{module_name}_#{path_or_url}'
+    
+            it 'does not use two's complement form for negative numbers for formats bBoxX' do
+          format('%+b', -10).should == '-1010'
+          format('%+B', -10).should == '-1010'
+          format('%+o', -87).should == '-127'
+          format('%+x', -196).should == '-c4'
+          format('%+X', -196).should == '-C4'
         end
       end
     end
-  end
-end
     
-              # Encodes the data
-          #
-          # @return [OpenSSL::ASN1::OctetString]
-          def encode_data(data)
-            OpenSSL::ASN1::OctetString.new(data)
-          end
-        end
+    describe 'Kernel.system' do
+  it_behaves_like :kernel_system, :system, Kernel
+end
+
+    
+      it 'returns true when passed ?R if the argument is readable by the real uid' do
+    Kernel.test(?R, @file).should be_true
+  end
+    
+            -> { w.f4(0.1) }.should output(nil, %r|classes.rb:#{w.warn_call_lineno}:|)
+        -> { w.f4(Rational(1, 2)) }.should output(nil, %r|classes.rb:#{w.warn_call_lineno}:|)
       end
-    end
-  end
-end
     
-                decode_asn1(asn1)
-          end
+          def report
+        <<-EOS
     
-              # Encodes the type field
-          #
-          # @return [OpenSSL::ASN1::Integer]
-          def encode_type
-            bn = OpenSSL::BN.new(type.to_s)
-            int = OpenSSL::ASN1::Integer.new(bn)
+    desc 'Generate RDoc under doc/api'
+task 'doc'     => ['doc:api']
+task('doc:api') { sh 'yardoc -o doc/api' }
+CLEAN.include 'doc/api'
     
-              # Encodes the sname
-          #
-          # @return [String]
-          def encode_sname
-            sname.encode
-          end
+    @@ chat
+<pre id='chat'></pre>
+<form>
+  <input id='msg' placeholder='type message here...' />
+</form>
     
-      gem 'bacon'
-  gem 'mocha'
-  gem 'mocha-on-bacon'
-  gem 'prettybacon'
-  gem 'webmock'
     
-    (allow file-ioctl)
-(allow sysctl-read)
-(allow mach-lookup)
-(allow ipc-posix-shm)
-(allow process-fork)
-(allow system-socket)
-    
-        def self.options
-      [
-        ['--silent', 'Show nothing'],
-      ].concat(super)
-    end
-    
-            # Prints the list of specs & pod cache dirs for a single pod name.
-        #
-        # This output is valid YAML so it can be parsed with 3rd party tools
-        #
-        # @param [Array<Hash>] cache_descriptors
-        #        The various infos about a pod cache. Keys are
-        #        :spec_file, :version, :release and :slug
-        #
-        def print_pod_cache_infos(pod_name, cache_descriptors)
-          UI.puts '#{pod_name}:'
-          cache_descriptors.each do |desc|
-            if @short_output
-              [:spec_file, :slug].each { |k| desc[k] = desc[k].relative_path_from(@cache.root) }
-            end
-            UI.puts('  - Version: #{desc[:version]}')
-            UI.puts('    Type:    #{pod_type(desc)}')
-            UI.puts('    Spec:    #{desc[:spec_file]}')
-            UI.puts('    Pod:     #{desc[:slug]}')
-          end
-        end
-      end
-    end
-  end
-end
+    {    if (pre.style.display == 'block') {
+      pre.style.display = 'none';
+      post.style.display = 'none';
+      context.style.background = 'none';
+    } else {
+      pre.style.display = 'block';
+      post.style.display = 'block';
+      context.style.background = '#fffed9';
+    }
+  }
