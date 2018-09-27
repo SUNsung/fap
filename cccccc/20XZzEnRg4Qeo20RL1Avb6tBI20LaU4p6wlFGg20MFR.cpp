aa1608d36a5b5c0bfc9ef982d5644e5a0dc810da
SUNsung <weatherius@gmail.com>
2018-09-28 00:22:01 +0300

    Rewrite [20XZzEnRg4Qeo20RL1Avb6tBI20LaU4p6wlFGg20MFR.cpp] File in: 2018-09-28 00:22:01

--- cccccc/20XZzEnRg4Qeo20RL1Avb6tBI20LaU4p6wlFGg20MFR.cpp
+++ cccccc/20XZzEnRg4Qeo20RL1Avb6tBI20LaU4p6wlFGg20MFR.cpp
@@ -1,53 +1,105 @@
 
         
-        void InternalKeyComparator::FindShortSuccessor(std::string* key) const {
-  Slice user_key = ExtractUserKey(*key);
-  std::string tmp(user_key.data(), user_key.size());
-  user_comparator_->FindShortSuccessor(&tmp);
-  if (tmp.size() < user_key.size() &&
-      user_comparator_->Compare(user_key, tmp) < 0) {
-    // User key has become shorter physically, but larger logically.
-    // Tack on the earliest possible number to the shortened user key.
-    PutFixed64(&tmp, PackSequenceAndType(kMaxSequenceNumber,kValueTypeForSeek));
-    assert(this->Compare(*key, tmp) < 0);
-    key->swap(tmp);
-  }
+        IPC_MESSAGE_ROUTED3(ShellViewHostMsg_Allocate_Object,
+                    int /* object id */,
+                    std::string /* type name */,
+                    base::DictionaryValue /* option */)
+    
+    
+    {  base::ListValue result;
+  RenderThread::Get()->Send(new ShellViewHostMsg_Call_Object_Method_Sync(
+      routing_id,
+      object_id,
+      type,
+      method,
+      *static_cast<base::ListValue*>(value_args.get()),
+      &result));
+  return converter->ToV8Value(&result, isolate->GetCurrentContext());
 }
     
-    // Maximum level to which a new compacted memtable is pushed if it
-// does not create overlap.  We try to push to level 2 to avoid the
-// relatively expensive level 0=>1 compactions and to avoid some
-// expensive manifest file operations.  We do not push all the way to
-// the largest level since that can generate a lot of wasted disk
-// space if the same key space is being repeatedly overwritten.
-static const int kMaxMemCompactLevel = 2;
     
-    // Tag numbers for serialized VersionEdit.  These numbers are written to
-// disk and should not be changed.
-enum Tag {
-  kComparator           = 1,
-  kLogNumber            = 2,
-  kNextFileNumber       = 3,
-  kLastSequence         = 4,
-  kCompactPointer       = 5,
-  kDeletedFile          = 6,
-  kNewFile              = 7,
-  // 8 was used for large value refs
-  kPrevLogNumber        = 9
+    {  focus_manager_ = NULL;
+}
+    
+    bool NwAppClearCacheFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  content::BrowsingDataRemover* remover = content::BrowserContext::GetBrowsingDataRemover(
+      Profile::FromBrowserContext(context_));
+    }
+    
+    class NwMenuGetNSStringFWithFixupFunction : public NWSyncExtensionFunction {
+ public:
+  NwMenuGetNSStringFWithFixupFunction() {}
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    
+ protected:
+  ~NwMenuGetNSStringFWithFixupFunction() override {}
+    
+  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringFWithFixup', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringFWithFixupFunction);
 };
     
-    		// Settings
-		real_t min_distance;
-		int max_iterations;
+    NwObjDestroyFunction::~NwObjDestroyFunction() {
+}
     
-      // stdio FILE stream class.
-  class jpeg_decoder_file_stream : public jpeg_decoder_stream
-  {
-    jpeg_decoder_file_stream(const jpeg_decoder_file_stream &);
-    jpeg_decoder_file_stream &operator =(const jpeg_decoder_file_stream &);
+    
+    {  DECLARE_EXTENSION_FUNCTION('nw.Obj.destroy', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwObjDestroyFunction);
+};
+    
+    void ShowErrorAndExit(DWORD ec, const wchar_t * func, int line)
+{
+	wchar_t * buffer;
+	if (FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL, ec, 0, (LPWSTR)&buffer, 0, NULL) == 0)
+	{
+		buffer = L'Unknown error. FormatMessage failed.';
+	}
     }
     
-        FILE* stream = popen('logcat -d -v time -s 'DEBUG:I'', 'r');    // 将“ls －l”命令的输出 通过管道读取（“r”参数）到FILE* stream
+    TEST(AutoCompactTest, ReadHalf) {
+  DoReads(kCount/2);
+}
     
-    namespace design_patterns {
+    TEST(CorruptionTest, TableFileRepair) {
+  options_.block_size = 2 * kValueSize;  // Limit scope of corruption
+  options_.paranoid_checks = true;
+  Reopen();
+  Build(100);
+  DBImpl* dbi = reinterpret_cast<DBImpl*>(db_);
+  dbi->TEST_CompactMemTable();
+  dbi->TEST_CompactRange(0, nullptr, nullptr);
+  dbi->TEST_CompactRange(1, nullptr, nullptr);
     }
+    
+      // Constant after construction
+  Env* const env_;
+  const InternalKeyComparator internal_comparator_;
+  const InternalFilterPolicy internal_filter_policy_;
+  const Options options_;  // options_.comparator == &internal_comparator_
+  const bool owns_info_log_;
+  const bool owns_cache_;
+  const std::string dbname_;
+    
+    
+    {}  // namespace leveldb
+
+    
+      void FixChecksum(int header_offset, int len) {
+    // Compute crc of type/len/data
+    uint32_t crc = crc32c::Value(&dest_.contents_[header_offset+6], 1 + len);
+    crc = crc32c::Mask(crc);
+    EncodeFixed32(&dest_.contents_[header_offset], crc);
+  }
+    
+      std::string Get(const std::string& k, const Snapshot* snapshot = nullptr) {
+    std::string result;
+    Status s = db_->Get(ReadOptions(), k, &result);
+    if (s.IsNotFound()) {
+      result = 'NOT_FOUND';
+    } else if (!s.ok()) {
+      result = s.ToString();
+    }
+    return result;
+  }
