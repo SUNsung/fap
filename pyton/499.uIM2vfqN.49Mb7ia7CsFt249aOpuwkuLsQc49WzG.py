5caffd3eb61dc1f5674bfd26ac5927f59040f70f
SUNsung <weatherius@gmail.com>
2018-09-27 22:42:58 +0300

    Rewrite [499.uIM2vfqN.49Mb7ia7CsFt249aOpuwkuLsQc49WzG.py] File in: 2018-09-27 22:42:57

--- pyton/499.uIM2vfqN.49Mb7ia7CsFt249aOpuwkuLsQc49WzG.py
+++ pyton/499.uIM2vfqN.49Mb7ia7CsFt249aOpuwkuLsQc49WzG.py
@@ -1,162 +1,20 @@
 
         
-            with open(empty_input_path, 'w'):
-      pass
+                # Placeholder for augeas
+        self.aug = None
     
-      def create(self,
-             fixed_embeddings,
-             linked_embeddings,
-             context_tensor_arrays,
-             attention_tensor,
-             during_training,
-             stride=None):
-    '''Forwards the lengths and scores.'''
-    check.NotNone(stride, 'MstSolverNetwork requires stride')
+    def print_title(title, pattern = '-'):
+    print('\n'.join(('', title, pattern * len(title)))) 
     
+    for image in TEST_IMAGES:
+    size = image.split('-')[1].split('.')[0]
+    print('Timings at {}:'.format(size))
     
-def apply_feature_id_dropout(ids, weights, channel):
-  '''Randomly perturbs a vector of feature IDs.
+            # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
     
-          # Should be equal to the hidden dimension (128) times 3.
-      self.assertEqual(ico_bias.shape, (384,))
-    
-      # TODO(googleuser): If the extra row is removed from the variable itself, remove
-  # the tf.slice() and point the hook directly at the variable.
-  _add_hook_node(matrix, _get_hook_name(component, var_name, '/trimmed'))
-    
-        if transition_spec.registered_name in ('shift-only', 'tagger', 'morpher',
-                                           'lm-transitions', 'dependency-label',
-                                           'category'):
-      if 'left_to_right' in transition_spec.parameters:
-        if transition_spec.parameters['left_to_right'] == 'false':
-          return 'reverse-token'
-      return 'identity'
-    
-          tf.logging.info('Processed %d documents in %.2f seconds.',
-                      len(char_corpus), time.time() - start_time)
-    
-    from tensorflow.python.platform import gfile
-from tensorflow.python.platform import tf_logging as logging
-    
-    cc_test(
-    name = 'registry_test_with_duplicate',
-    srcs = ['registry_test.cc'],
-    defines = ['DRAGNN_REGISTRY_TEST_WITH_DUPLICATE'],
-    deps = [
-        ':base',
-        ':registry',
-        ':test_main',
-        '//dragnn/core/test:generic',
-        '@org_tensorflow//tensorflow/core:test',
-    ],
-)
-    
-      def PathScores(self, iterations, beam_size, max_steps, batch_size):
-    with self.test_session(graph=tf.Graph()) as sess:
-      t = self.MakeGraph(beam_size=beam_size, max_steps=max_steps,
-                         batch_size=batch_size).training
-      sess.run(t['inits'])
-      all_path_scores = []
-      beam_path_scores = []
-      for i in range(iterations):
-        logging.info('run %d', i)
-        tensors = (
-            sess.run(
-                [t['alive_steps'], t['concat_scores'],
-                 t['all_path_scores'], t['beam_path_scores'],
-                 t['indices'], t['path_ids']]))
-    
-        def _finish(self, request):
-        self.concurrent -= 1
-        if not request.finished and not request._disconnected:
-            request.finish()
-    
-            # If no credentials could be found anywhere,
-        # consider this an anonymous connection request by default;
-        # unless 'anon' was set explicitly (True/False).
-        anon = kw.get('anon')
-        if anon is None and not aws_access_key_id and not aws_secret_access_key:
-            kw['anon'] = True
-        self.anon = kw.get('anon')
-    
-        def spider_opened(self, spider):
-        usr = getattr(spider, 'http_user', '')
-        pwd = getattr(spider, 'http_pass', '')
-        if usr or pwd:
-            self.auth = basic_auth_header(usr, pwd)
-    
-        def __init__(self, reason='cancelled'):
-        super(CloseSpider, self).__init__()
-        self.reason = reason
-    
-        def item_scraped(self, item, spider):
-        self.counter['itemcount'] += 1
-        if self.counter['itemcount'] == self.close_on['itemcount']:
-            self.crawler.engine.close_spider(spider, 'closespider_itemcount')
-    
-         
-    def getUnexpectedType(self):
-        '''Return the token type or char of the unexpected input element'''
-    
-                t = self.tokenSource.nextToken()
-       
-        # leave p pointing at first token on channel
-        self.p = 0
-        self.p = self.skipOffTokenChannels(self.p)
-    
-    if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        zones = TZInfo.zonelist()
-        for z in zones:
-            print(z)
-        sys.exit()
-    filepath = sys.argv[1]
-    if not filepath.startswith('/'):
-        filepath = os.path.join('/usr/share/zoneinfo', filepath)
-    with open(filepath, 'rb') as fileobj:
-        tzi = TZInfo.fromfile(fileobj)
-    tzi.dump(sys.stdout)
-
-    
-                lines = []
-            if file is not None:
-                # XXX lines = None instead?
-                terminators = (b'.' + _CRLF, b'.\n')
-                while 1:
-                    line = self._getline(False)
-                    if line in terminators:
-                        break
-                    if line.startswith(b'..'):
-                        line = line[1:]
-                    file.write(line)
-            else:
-                terminator = b'.'
-                while 1:
-                    line = self._getline()
-                    if line == terminator:
-                        break
-                    if line.startswith(b'..'):
-                        line = line[1:]
-                    lines.append(line)
-        finally:
-            # If this method created the file, then it must close it
-            if openedFile:
-                openedFile.close()
-    
-    
-def _normalize_path(path) -> str:
-    '''Normalize a path by ensuring it is a string.
-    
-        The 'size' argument is normally passed to the decorated test method as an
-    extra argument. If 'dry_run' is true, the value passed to the test method
-    may be less than the requested value. If 'dry_run' is false, it means the
-    test doesn't support dummy runs when -M is not specified.
-    '''
-    def decorator(f):
-        def wrapper(self):
-            size = wrapper.size
-            memuse = wrapper.memuse
-            if not real_max_memuse:
-                maxsize = 5147
-            else:
-                maxsize = size
+    # Create a PIL imagedraw object so we can draw on the picture
+pil_image = Image.fromarray(image)
+d = ImageDraw.Draw(pil_image)
