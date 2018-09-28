542c38d7e86bd61536359ce30259b61d8ab83647
SUNsung <weatherius@gmail.com>
2018-09-28 14:53:16 +0300

    Rewrite [16E5eCalx28H216jG9Djsfv9616m48.3PNNQe.16OO..rb] File in: 2018-09-28 14:53:16

--- ruby/16E5eCalx28H216jG9Djsfv9616m48.3PNNQe.16OO..rb
+++ ruby/16E5eCalx28H216jG9Djsfv9616m48.3PNNQe.16OO..rb
@@ -1,138 +1,103 @@
 
         
-        def custom_release_header_anchors(markdown)
-  header_regexp = %r!^(\d{1,2})\.(\d{1,2})\.(\d{1,2}) \/ \d{4}-\d{2}-\d{2}!
-  section_regexp = %r!^### \w+ \w+$!
-  markdown.split(%r!^##\s!).map do |release_notes|
-    _, major, minor, patch = *release_notes.match(header_regexp)
-    release_notes
-      .gsub(header_regexp, '\\0\n{: #v\\1-\\2-\\3}')
-      .gsub(section_regexp) { |section| '#{section}\n{: ##{slugify(section)}-v#{major}-#{minor}-#{patch}}' }
-  end.join('\n## ')
-end
-    
-    require 'benchmark/ips'
-require 'jekyll'
-require 'json'
-    
-    Benchmark.ips do |x|
-  x.report('no body include?') { CONTENT_NOT_CONTAINING.include?('<body') }
-  x.report('no body regexp')   { CONTENT_NOT_CONTAINING =~ /<\s*body/ }
-  x.compare!
-end
-    
-    Mercenary.program(:jekyll) do |p|
-  p.version Jekyll::VERSION
-  p.description 'Jekyll is a blog-aware, static site generator in Ruby'
-  p.syntax 'jekyll <subcommand> [options]'
-    
-              if options.fetch('skip_initial_build', false)
-            Jekyll.logger.warn 'Build Warning:', 'Skipping the initial build.' \
-                               ' This may result in an out-of-date site.'
-          else
-            build(site, options)
-          end
-    
-    puts '\nDone.'
-
-    
-      def up_down(change)
-    change.up do
-      Mention.update_all(mentions_container_type: 'Post')
-      change_column :mentions, :mentions_container_type, :string, null: false
-      Notification.where(type: 'Notifications::Mentioned').update_all(type: 'Notifications::MentionedInPost')
-    end
-    
-    When /^I (?:sign|log) in with password '([^']*)'( on the mobile website)?$/ do |password, mobile|
-  @me.password = password
-  automatic_login
-  confirm_login mobile
-end
-    
-    describe ContactsController, :type => :controller do
-  describe '#index' do
-    before do
-      AppConfig.chat.enabled = true
-      @aspect = bob.aspects.create(:name => 'another aspect')
-      bob.share_with alice.person, @aspect
-      bob.share_with eve.person, @aspect
-      sign_in bob, scope: :user
-    end
-    
-      end
-    
-          doc.css('#filecontents').css('h1, h2, h3, h4, h5, h6').each do |h|
-        next if h.inner_text.empty?
-        h['id'] =
-          case h.inner_text
-          when 'Referencing Parent Selectors: &'; 'parent-selector'
-          when /^Comments:/; 'comments'
-          when 'Strings'; 'sass-script-strings'
-          when 'Division and /'; 'division-and-slash'
-          when /^Subtraction,/; 'subtraction'
-          when '& in SassScript'; 'parent-script'
-          when '@-Rules and Directives'; 'directives'
-          when '@extend-Only Selectors'; 'placeholders'
-          when '@extend-Only Selectors'; 'placeholders'
-          when '@each'; 'each-directive'
-          when 'Multiple Assignment'; 'each-multi-assign'
-          when 'Mixin Directives'; 'mixins'
-          when /^Defining a Mixin:/; 'defining_a_mixin'
-          when /^Including a Mixin:/; 'including_a_mixin'
-          when 'Arguments'; 'mixin-arguments'
-          when 'Passing Content Blocks to a Mixin'; 'mixin-content'
-          else
-            h.inner_text.downcase.gsub(/[^a-z _-]/, '').gsub(' ', '_')
-          end
-      end
-    
-          # Returns the path to a file for the given key.
-      #
-      # @param key [String]
-      # @return [String] The path to the cache file.
-      def path_to(key)
-        key = key.gsub(/[<>:\\|?*%]/) {|c| '%%%03d' % c.ord}
-        File.join(cache_location, key)
-      end
-    end
-  end
-end
-
-    
-        # The content passed to this environment. If the content's environment isn't already
-    # read-only, it's made read-only.
-    #
-    # @see BaseEnvironment#content
-    #
-    # @return {[Array<Sass::Tree::Node>, ReadOnlyEnvironment]?} The content nodes and
-    #   the lexical environment of the content block.
-    #   Returns `nil` when there is no content in this environment.
-    def content
-      # Return the cached content from a previous invocation if any
-      return @content if @content_cached
-      # get the content with a read-write environment from the superclass
-      read_write_content = super
-      if read_write_content
-        tree, env = read_write_content
-        # make the content's environment read-only
-        if env && !env.is_a?(ReadOnlyEnvironment)
-          env = ReadOnlyEnvironment.new(env, env.options)
+                def self.find_by_dn(dn, adapter)
+          adapter.user('dn', dn)
         end
-        @content_cached = true
-        @content = [tree, env]
-      else
-        @content_cached = true
-        @content = nil
+    
+          def failed?
+        error_message.present?
+      end
+    
+          def perform(start_id, stop_id)
+        status_sql = Build
+          .where('ci_builds.commit_id = ci_stages.pipeline_id')
+          .where('ci_builds.stage = ci_stages.name')
+          .status_sql
+    
+              # Make sure we're only working with one VM if single target
+          if options[:single_target] && vms.length != 1
+            vm = @env.primary_vm
+            raise Errors::MultiVMTargetRequired if !vm
+            vms = [vm]
+          end
+    
+            # Checks if the target machine is ready for communication. If this
+        # returns true, then all the other methods for communicating with
+        # the machine are expected to be functional.
+        #
+        # @return [Boolean]
+        def ready?
+          false
+        end
+    
+            # This is the method called to when the system is being destroyed
+        # and allows the provisioners to engage in any cleanup tasks necessary.
+        def cleanup
+        end
       end
     end
   end
-    
-    desc 'Start an IRB session with all necessary files required.'
-task :shell do |t|
-  chdir File.dirname(__FILE__)
-  exec 'irb -I lib/ -I lib/paperclip -r rubygems -r active_record -r tempfile -r init'
 end
+
     
-    When /^(?:|I )follow '([^']*)'$/ do |link|
-  click_link(link)
+        describe 'A' do
+      it 'converts floating point argument as [-]0xh.hhhhp[+-]dd and use uppercase X and P' do
+        format('%A', 196).should == '0X1.88P+7'
+        format('%A', -196).should == '-0X1.88P+7'
+        format('%A', 196.1).should == '0X1.8833333333333P+7'
+        format('%A', 0.01).should == '0X1.47AE147AE147BP-7'
+        format('%A', -0.01).should == '-0X1.47AE147AE147BP-7'
+      end
+    
+      it 'accepts a Rational' do
+    sleep(Rational(1, 9)).should be_close(0, 2)
+  end
+    
+      it 'returns true when passed ?d if the argument is a directory' do
+    Kernel.test(?d, @dir).should == true
+  end
+    
+      it 'sets the return value of the catch block to nil by default' do
+    res = catch :blah do
+      throw :blah
+    end
+    res.should == nil
+  end
+    
+    def config_tag(config, key, tag=nil, classname=nil)
+  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
+  tag       ||= 'div'
+  classname ||= key.sub(/_/, '-').sub(/\./, '-')
+  output      = '<#{tag} class='#{classname}''
+    
+    require './plugins/pygments_code'
+require './plugins/raw'
+require 'pathname'
+    
+    ::Bundler.with_friendly_errors do
+  ::Bundler::CLI.start(ARGV, :debug => true)
 end
+
+    
+    module LogStash
+  module PluginManager
+    class Error < StandardError; end
+    
+    module LogStash module PluginManager module PackFetchStrategy
+  class Repository
+    DEFAULT_PACK_URL = 'https://artifacts.elastic.co/downloads/logstash-plugins'
+    PACK_EXTENSION = 'zip'
+    
+        platforms.types.each do |type|
+      desc 'Run acceptance test in #{type} machines'
+      task type do
+        ENV['LS_TEST_PLATFORM']=type
+        exit(RSpec::Core::Runner.run([Rake::FileList['acceptance/spec/lib/*_spec.rb']]))
+      end
+    end
+    
+              it 'successfully install the plugin when verification is disabled' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify logstash-filter-qatest')
+            expect(command).to install_successfully
+            expect(logstash).to have_installed?('logstash-filter-qatest')
+          end
