1cfafaa897188fa29224e6d0b7b00a26086d05df
SUNsung <weatherius@gmail.com>
2018-09-29 07:02:50 +0300

    Rewrite [20XZzEnRg4Qeo20RL1Avb6tBI20LaU4p6wlFGg20MFR.cpp] File in: 2018-09-29 07:02:49

--- cccccc/20XZzEnRg4Qeo20RL1Avb6tBI20LaU4p6wlFGg20MFR.cpp
+++ cccccc/20XZzEnRg4Qeo20RL1Avb6tBI20LaU4p6wlFGg20MFR.cpp
@@ -1,105 +1,457 @@
 
         
-        IPC_MESSAGE_ROUTED3(ShellViewHostMsg_Allocate_Object,
-                    int /* object id */,
-                    std::string /* type name */,
-                    base::DictionaryValue /* option */)
-    
-    
-    {  base::ListValue result;
-  RenderThread::Get()->Send(new ShellViewHostMsg_Call_Object_Method_Sync(
-      routing_id,
-      object_id,
-      type,
-      method,
-      *static_cast<base::ListValue*>(value_args.get()),
-      &result));
-  return converter->ToV8Value(&result, isolate->GetCurrentContext());
-}
-    
-    
-    {  focus_manager_ = NULL;
-}
-    
-    bool NwAppClearCacheFunction::RunNWSync(base::ListValue* response, std::string* error) {
-  content::BrowsingDataRemover* remover = content::BrowserContext::GetBrowsingDataRemover(
-      Profile::FromBrowserContext(context_));
+          // Add info about requested inputs.
+  CppShapeInferenceInputsNeeded needed;
+  for (int i = 0; i < c.num_inputs(); ++i) {
+    if (c.requested_input_tensor(i)) {
+      needed.add_input_tensors_needed(i);
     }
-    
-    class NwMenuGetNSStringFWithFixupFunction : public NWSyncExtensionFunction {
- public:
-  NwMenuGetNSStringFWithFixupFunction() {}
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    
- protected:
-  ~NwMenuGetNSStringFWithFixupFunction() override {}
-    
-  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringFWithFixup', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringFWithFixupFunction);
-};
-    
-    NwObjDestroyFunction::~NwObjDestroyFunction() {
-}
-    
-    
-    {  DECLARE_EXTENSION_FUNCTION('nw.Obj.destroy', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwObjDestroyFunction);
-};
-    
-    void ShowErrorAndExit(DWORD ec, const wchar_t * func, int line)
-{
-	wchar_t * buffer;
-	if (FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL, ec, 0, (LPWSTR)&buffer, 0, NULL) == 0)
-	{
-		buffer = L'Unknown error. FormatMessage failed.';
-	}
+    if (c.requested_input_tensor_as_partial_shape(i)) {
+      needed.add_input_tensors_as_shapes_needed(i);
     }
-    
-    TEST(AutoCompactTest, ReadHalf) {
-  DoReads(kCount/2);
-}
-    
-    TEST(CorruptionTest, TableFileRepair) {
-  options_.block_size = 2 * kValueSize;  // Limit scope of corruption
-  options_.paranoid_checks = true;
-  Reopen();
-  Build(100);
-  DBImpl* dbi = reinterpret_cast<DBImpl*>(db_);
-  dbi->TEST_CompactMemTable();
-  dbi->TEST_CompactRange(0, nullptr, nullptr);
-  dbi->TEST_CompactRange(1, nullptr, nullptr);
-    }
-    
-      // Constant after construction
-  Env* const env_;
-  const InternalKeyComparator internal_comparator_;
-  const InternalFilterPolicy internal_filter_policy_;
-  const Options options_;  // options_.comparator == &internal_comparator_
-  const bool owns_info_log_;
-  const bool owns_cache_;
-  const std::string dbname_;
+  }
+  *input_tensors_needed_out = needed.SerializeAsString();
     
     
-    {}  // namespace leveldb
+    {}  // namespace tensorflow
 
     
-      void FixChecksum(int header_offset, int len) {
-    // Compute crc of type/len/data
-    uint32_t crc = crc32c::Value(&dest_.contents_[header_offset+6], 1 + len);
-    crc = crc32c::Mask(crc);
-    EncodeFixed32(&dest_.contents_[header_offset], crc);
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    #include <Python.h>
+    
+    
+    {}  // namespace tensorflow
+
+    
+    
+    {  uint64 offset_;
+  RandomAccessFile* file_;    // Owned
+  io::RecordReader* reader_;  // Owned
+  string record_;
+  TF_DISALLOW_COPY_AND_ASSIGN(PyRecordReader);
+};
+    
+      const tensorflow::OpRegistrationData* op_reg_data;
+  auto status =
+      tensorflow::OpRegistry::Global()->LookUp(node_def.op(), &op_reg_data);
+  if (!status.ok()) {
+    LOG(WARNING) << 'Op ' << node_def.op() << ' not found: ' << status;
+    return '';
+  }
+  AddDefaultsToNodeDef(op_reg_data->op_def, &node_def);
+    
+    // Functions for getting information about kernels registered in the binary.
+#ifndef TENSORFLOW_PYTHON_UTIL_KERNEL_REGISTRY_H_
+#define TENSORFLOW_PYTHON_UTIL_KERNEL_REGISTRY_H_
+    
+    
+    {
+    {}  // namespace blas
+}  // namespace stream_executor
+
+    
+        EditAddressAndSubmit(
+        &editAddressDialog, QString('uhoh, different'), preexisting_s_address,
+        QString(
+            'The entered address \'%1\' is already in the address book with '
+            'label \'%2\'.'
+            ).arg(preexisting_s_address).arg(s_label));
+    
+    #undef BE32
+#undef Round
+#undef sigma1
+#undef sigma0
+#undef Sigma1
+#undef Sigma0
+#undef Maj
+#undef Ch
+    
+    int secp256k1_ecdh(const secp256k1_context* ctx, unsigned char *result, const secp256k1_pubkey *point, const unsigned char *scalar) {
+    int ret = 0;
+    int overflow = 0;
+    secp256k1_gej res;
+    secp256k1_ge pt;
+    secp256k1_scalar s;
+    VERIFY_CHECK(ctx != NULL);
+    ARG_CHECK(result != NULL);
+    ARG_CHECK(point != NULL);
+    ARG_CHECK(scalar != NULL);
+    }
+    
+    static int secp256k1_ecdsa_sig_recover(const secp256k1_ecmult_context *ctx, const secp256k1_scalar *sigr, const secp256k1_scalar* sigs, secp256k1_ge *pubkey, const secp256k1_scalar *message, int recid) {
+    unsigned char brx[32];
+    secp256k1_fe fx;
+    secp256k1_ge x;
+    secp256k1_gej xj;
+    secp256k1_scalar rn, u1, u2;
+    secp256k1_gej qj;
+    int r;
+    }
+    
+    BOOST_AUTO_TEST_CASE(bip173_testvectors_invalid)
+{
+    static const std::string CASES[] = {
+        ' 1nwldj5',
+        '\x7f''1axkwrx',
+        '\x80''1eym55h',
+        'an84characterslonghumanreadablepartthatcontainsthenumber1andtheexcludedcharactersbio1569pvx',
+        'pzry9x0s0muk',
+        '1pzry9x0s0muk',
+        'x1b4n0q5v',
+        'li1dgmt3',
+        'de1lg7wt\xff',
+        'A1G7SGD8',
+        '10a06t8',
+        '1qzzfhee',
+        'a12UEL5L',
+        'A12uEL5L',
+    };
+    for (const std::string& str : CASES) {
+        auto ret = bech32::Decode(str);
+        BOOST_CHECK(ret.first.empty());
+    }
+}
+    
+    /* Given a BlockIndex with the provided nbits,
+ * verify that the expected difficulty results.
+ */
+static void TestDifficulty(uint32_t nbits, double expected_difficulty)
+{
+    CBlockIndex* block_index = CreateBlockIndexWithNbits(nbits);
+    double difficulty = GetDifficulty(block_index);
+    delete block_index;
+    }
+    
+        // C escapes
+    CheckParseTorReplyMapping(
+        'Foo=\'Bar\\nBaz\\t\' Spam=\'\\rEggs\' Octals=\'\\1a\\11\\17\\18\\81\\377\\378\\400\\2222\' Final=Check', {
+            {'Foo', 'Bar\nBaz\t'},
+            {'Spam', '\rEggs'},
+            {'Octals', '\1a\11\17\1' '881\377\37' '8\40' '0\222' '2'},
+            {'Final', 'Check'},
+        });
+    CheckParseTorReplyMapping(
+        'Valid=Mapping Escaped=\'Escape\\\\\'', {
+            {'Valid', 'Mapping'},
+            {'Escaped', 'Escape\\'},
+        });
+    CheckParseTorReplyMapping(
+        'Valid=Mapping Bare=\'Escape\\\'', {});
+    CheckParseTorReplyMapping(
+        'OneOctal=\'OneEnd\\1\' TwoOctal=\'TwoEnd\\11\'', {
+            {'OneOctal', 'OneEnd\1'},
+            {'TwoOctal', 'TwoEnd\11'},
+        });
+    
+    int UniValue::get_int() const
+{
+    if (typ != VNUM)
+        throw std::runtime_error('JSON value is not an integer as expected');
+    int32_t retval;
+    if (!ParseInt32(getValStr(), &retval))
+        throw std::runtime_error('JSON integer out of range');
+    return retval;
+}
+    
+    
+    {
+    {
+    {
+    {          std::transform(
+            begin(inputs) + start, begin(inputs) + stop,
+            retMem + start,
+            func
+          );
+        }
+      } catch (const std::runtime_error& e) {
+        std::fprintf(stderr,
+          'worker thread exited with exception: %s\n', e.what());
+        failed = true;
+      }
+    }));
   }
     
-      std::string Get(const std::string& k, const Snapshot* snapshot = nullptr) {
-    std::string result;
-    Status s = db_->Get(ReadOptions(), k, &result);
-    if (s.IsNotFound()) {
-      result = 'NOT_FOUND';
-    } else if (!s.ok()) {
-      result = s.ToString();
-    }
-    return result;
+    private:
+  APCCollection();
+  ~APCCollection();
+  static APCHandle::Pair WrapArray(APCHandle::Pair, CollectionType);
+    
+    std::string Config::IniName(const std::string& config,
+                            bool prepend_hhvm /* = true */) {
+  std::string out = '';
+  if (prepend_hhvm) {
+    out += 'hhvm.';
   }
+  size_t idx = 0;
+  for (auto& c : config) {
+    // This is the first or last character
+    if (idx == 0 || idx == config.length() - 1) {
+      out += tolower(c);
+    } else if (!isalpha(c)) {
+      // Any . or _ or numeral is just output with no special behavior
+      out += c;
+    } else {
+      if (isupper(c) && isupper(config[idx - 1 ]) && islower(config[idx + 1])) {
+      // Handle something like 'SSLPort', and c = 'P', which will then put
+      // the underscore between the 'L' and 'P'
+        out += '_';
+        out += tolower(c);
+      } else if (islower(c) && isupper(config[idx + 1])) {
+      // Handle something like 'PathDebug', and c = 'h', which will then put
+      // the underscore between the 'h' and 'D'
+        out += tolower(c);
+        out += '_';
+      } else {
+      // Otherwise we just output as lower
+        out += tolower(c);
+      }
+    }
+    idx++;
+  }
+    }
+    
+      char** newargv_array = (char**)alloca(sizeof(char*) * (newargv.size() + 1));
+  for (unsigned i = 0; i < newargv.size(); i++) {
+    // printf('%s\n', newargv[i].data());
+    newargv_array[i] = (char *)newargv[i].data();
+  }
+  // NULL-terminate the argument array.
+  newargv_array[newargv.size()] = nullptr;
+    
+    /**
+ * Search for PHP or non-PHP files under a directory.
+ */
+void find(std::vector<std::string> &out,
+          const std::string &root, const std::string& path, bool php,
+          const std::set<std::string> *excludeDirs = nullptr,
+          const std::set<std::string> *excludeFiles = nullptr);
+    
+     public:
+  RandomGenerator() {
+    // We use a limited amount of data over and over again and ensure
+    // that it is larger than the compression window (32KB), and also
+    // large enough to serve all typical value sizes we want to write.
+    Random rnd(301);
+    std::string piece;
+    while (data_.size() < 1048576) {
+      // Add a short fragment that is as compressible as specified
+      // by FLAGS_compression_ratio.
+      test::CompressibleString(&rnd, FLAGS_compression_ratio, 100, &piece);
+      data_.append(piece);
+    }
+    pos_ = 0;
+  }
+    
+    // Return the name of the descriptor file for the db named by
+// 'dbname' and the specified incarnation number.  The result will be
+// prefixed with 'dbname'.
+std::string DescriptorFileName(const std::string& dbname, uint64_t number);
+    
+    int main(int argc, char** argv) {
+  leveldb::Env* env = leveldb::Env::Default();
+  bool ok = true;
+  if (argc < 2) {
+    Usage();
+    ok = false;
+  } else {
+    std::string command = argv[1];
+    if (command == 'dump') {
+      ok = leveldb::HandleDumpCommand(env, argv+2, argc-2);
+    } else {
+      Usage();
+      ok = false;
+    }
+  }
+  return (ok ? 0 : 1);
+}
+
+    
+    namespace leveldb {
+namespace log {
+    }
+    }
+    
+    void Reader::ReportDrop(uint64_t bytes, const Status& reason) {
+  if (reporter_ != nullptr &&
+      end_of_buffer_offset_ - buffer_.size() - bytes >= initial_offset_) {
+    reporter_->Corruption(static_cast<size_t>(bytes), reason);
+  }
+}
+    
+      void CheckOffsetPastEndReturnsNoRecords(uint64_t offset_past_end) {
+    WriteInitialOffsetLog();
+    reading_ = true;
+    source_.contents_ = Slice(dest_.contents_);
+    Reader* offset_reader = new Reader(&source_, &report_, true/*checksum*/,
+                                       WrittenBytes() + offset_past_end);
+    Slice record;
+    std::string scratch;
+    ASSERT_TRUE(!offset_reader->ReadRecord(&record, &scratch));
+    delete offset_reader;
+  }
+    
+    class MemTableIterator: public Iterator {
+ public:
+  explicit MemTableIterator(MemTable::Table* table) : iter_(table) { }
+    }
+    
+    
+    {  // Force creation of multiple memtables by reducing the write buffer size.
+  Options opt;
+  opt.reuse_logs = true;
+  opt.write_buffer_size = (kNum*100) / 2;
+  Open(&opt);
+  ASSERT_LE(2, NumTables());
+  ASSERT_EQ(1, NumLogs());
+  ASSERT_NE(old_log_file, FirstLogFile()) << 'must not reuse log';
+  for (int i = 0; i < kNum; i++) {
+    char buf[100];
+    snprintf(buf, sizeof(buf), '%050d', i);
+    ASSERT_EQ(buf, Get(buf));
+  }
+}
+    
+      Status WriteDescriptor() {
+    std::string tmp = TempFileName(dbname_, 1);
+    WritableFile* file;
+    Status status = env_->NewWritableFile(tmp, &file);
+    if (!status.ok()) {
+      return status;
+    }
+    }
+    
+    #ifndef __CCCAMERA_ACTION_H__
+#define __CCCAMERA_ACTION_H__
+    
+    
+    {    if (ActionInterval::initWithDuration(duration))
+    {
+        this->setPoints(points);
+        this->_tension = tension;
+        
+        return true;
+    }
+    
+    return false;
+}
+    
+        /** Initializes a Catmull Rom config with a capacity hint.
+     *
+     * @js NA
+     * @param capacity The size of the array.
+     * @return True.
+     */
+    bool initWithCapacity(ssize_t capacity);
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+    
+    bool Waves3D::initWithDuration(float duration, const Size& gridSize, unsigned int waves, float amplitude)
+{
+    if (Grid3DAction::initWithDuration(duration, gridSize))
+    {
+        _waves = waves;
+        _amplitude = amplitude;
+        _amplitudeRate = 1.0f;
+    }
+    }
+    
+    /** @class BezierBy
+ * @brief An action that moves the target with a cubic Bezier curve by a certain distance.
+ */
+class CC_DLL BezierBy : public ActionInterval
+{
+public:
+    /** Creates the action with a duration and a bezier configuration.
+     * @param t Duration time, in seconds.
+     * @param c Bezier config.
+     * @return An autoreleased BezierBy object.
+     * @code
+     * When this function bound to js or lua,the input params are changed.
+     * in js: var create(var t,var table)
+     * in lua: local create(local t, local table)
+     * @endcode
+     */
+    static BezierBy* create(float t, const ccBezierConfig& c);
+    }
+    
+                // We scale z here to avoid the animation being
+            // too much bigger than the screen due to perspective transform
+    
+    Animation* Animation::createWithSpriteFrames(const Vector<SpriteFrame*>& frames, float delay/* = 0.0f*/, unsigned int loops/* = 1*/)
+{
+    Animation *animation = new (std::nothrow) Animation();
+    animation->initWithSpriteFrames(frames, delay, loops);
+    animation->autorelease();
+    }
+    
+        /** Adds an animation from an NSDictionary.
+     * Make sure that the frames were previously loaded in the SpriteFrameCache.
+     * @param dictionary An NSDictionary.
+     * @param plist The path of the relative file,it use to find the plist path for load SpriteFrames.
+     * @since v1.1
+	 @js NA
+     */
+    void addAnimationsWithDictionary(const ValueMap& dictionary,const std::string& plist);
+    
+    class TextureAtlas;
+    
+    // Sort the root nodes, least popular first.
+static inline bool SortHuffmanTree(const HuffmanTree& v0,
+                                   const HuffmanTree& v1) {
+  if (v0.total_count_ != v1.total_count_) {
+    return v0.total_count_ < v1.total_count_;
+  }
+  return v0.index_right_or_value_ > v1.index_right_or_value_;
+}
+    
+    
+    {}  // namespace guetzli
+    
+    void RowDct(coeff_t* in, const coeff_t* table) {
+  // The Fourier transform is an unitary operator, so we're basically
+  // doing the transpose of RowIdct()
+  const int a0 = in[0] + in[7];
+  const int b0 = in[0] - in[7];
+  const int a1 = in[1] + in[6];
+  const int b1 = in[1] - in[6];
+  const int a2 = in[2] + in[5];
+  const int b2 = in[2] - in[5];
+  const int a3 = in[3] + in[4];
+  const int b3 = in[3] - in[4];
+    }
+    
+      tmp0 = in[3 * stride];
+  tmp1 = kIDCTMatrix[ 3] * tmp0;
+  tmp2 = kIDCTMatrix[11] * tmp0;
+  tmp3 = kIDCTMatrix[19] * tmp0;
+  tmp4 = kIDCTMatrix[27] * tmp0;
+  out[0] += tmp1;
+  out[1] += tmp2;
+  out[2] += tmp3;
+  out[3] += tmp4;
+  out[4] -= tmp4;
+  out[5] -= tmp3;
+  out[6] -= tmp2;
+  out[7] -= tmp1;
+    
+    namespace guetzli {
+    }
+    
+    // Mimic libjpeg's heuristics to guess jpeg color space.
+// Requires that the jpg has 3 components.
+bool HasYCbCrColorSpace(const JPEGData& jpg);
+    
+    #ifndef GUETZLI_JPEG_DATA_ENCODER_H_
+#define GUETZLI_JPEG_DATA_ENCODER_H_
+    
+    // Writes a string using the out callback.
+inline bool JPEGWrite(JPEGOutput out, const std::string& s) {
+  const uint8_t* data = reinterpret_cast<const uint8_t*>(&s[0]);
+  return JPEGWrite(out, data, s.size());
+}
