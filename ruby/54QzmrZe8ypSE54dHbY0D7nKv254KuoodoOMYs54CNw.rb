1ef56c0fa91372d8cd21c5239a264f644c055ca1
SUNsung <weatherius@gmail.com>
2018-09-29 17:14:14 +0300

    Rewrite [54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb] File in: 2018-09-29 17:14:14

--- ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
+++ ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
@@ -1,223 +1,112 @@
 
         
-                if block_given?
-          content_tag :button, options, &block
-        else
-          content_tag :button, content_or_options || 'Button', options
+          it 'returns the previous seed value' do
+    srand(10)
+    srand(20).should == 10
+  end
+    
+        reset
+  end
+    
+            end
+      end
+    end
+  end
+end
+    
+                if OpenSSL::HMAC.digest('MD5', k1, decrypted) != checksum
+              raise ::RuntimeError, 'RC4-HMAC decryption failed, incorrect checksum verification'
+            end
+    
+              include Rex::Proto::Kerberos::Crypto
+          include Rex::Proto::Kerberos::Model
+    
+              # Decodes a Rex::Proto::Kerberos::Model::EncKdcResponse from an String
+          #
+          # @param input [String] the input to decode from
+          def decode_string(input)
+            asn1 = OpenSSL::ASN1.decode(input)
+    
+              # Encodes the value field
+          #
+          # @return [OpenSSL::ASN1::OctetString]
+          def encode_value
+            OpenSSL::ASN1::OctetString.new(value)
+          end
         end
       end
+    end
+  end
+end
     
-        # The category for users with trust level 3 has been created.
-    # Add initial permissions and description. They can be changed later.
+        def pos=(i)
+      @s.pos = str_to_byte_pos i
+      i
+    end
     
-          check_value =
-        case check_value
-        when Integer
-          check_value
-        when Symbol
-          record.public_send(check_value) # rubocop:disable GitlabSecurity/PublicSend
+        # These mixins will get vararg definitions in SCSS (not supported by LESS):
+    VARARG_MIXINS               = %w(
+      scale transition transition-duration transition-property transition-transform box-shadow
+    )
+    
+        def log_processing(name)
+      puts yellow '  #{File.basename(name)}'
+    end
+    
+        def read_cached_files(path, files)
+      full_path = '#@cache_path/#@branch_sha/#{path}'
+      contents  = {}
+      if File.directory?(full_path)
+        files.each do |name|
+          path = '#{full_path}/#{name}'
+          contents[name] = File.read(path, mode: 'rb') if File.exists?(path)
         end
-    
-            def initialize(entry, provider)
-          Rails.logger.debug { 'Instantiating #{self.class.name} with LDIF:\n#{entry.to_ldif}' }
-          @entry = entry
-          @provider = provider
-        end
-    
-      # PRIVATE CONFIGURATION
-    
-          # Return true if it's a devise_controller. false to all controllers unless
-      # the controllers defined inside devise. Useful if you want to apply a before
-      # filter to all controllers, except the ones in devise:
-      #
-      #   before_action :my_filter, unless: :devise_controller?
-      def devise_controller?
-        is_a?(::DeviseController)
       end
+      contents
+    end
     
-          def remember_key(resource, scope)
-        resource.rememberable_options.fetch(:key, 'remember_#{scope}_token')
-      end
+        remove_duplicates
+    remove_index :share_visibilities, name: :shareable_and_user_id
+    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id, unique: true
+    
+    Then /^'([^']*)' should be post (\d+)$/ do |post_text, position|
+  stream_element_numbers_content(position).should have_content(post_text)
+end
+    
+    When /^I view '([^\']*)''s first post$/ do |email|
+  user = User.find_by_email(email)
+  post = user.posts.first
+  visit post_path(post)
+end
+    
+          delete :destroy, params: {post_id: @message.id, id: like2.id}, format: :json
+      expect(response.status).to eq(404)
+      expect(response.body).to eq(I18n.t('likes.destroy.error'))
+      expect(Like.count).to eq(like_count)
     end
   end
 end
 
     
-          def self.generate_helpers!(routes=nil)
-        routes ||= begin
-          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
-          Devise::URL_HELPERS.slice(*mappings)
-        end
-    
-    gem 'rake'
-gem 'thor'
-gem 'pry', '~> 0.11.0'
-gem 'activesupport', '~> 5.2', require: false
-gem 'yajl-ruby', require: false
-    
-        SPLIT_INTS = /(?<=\d)\.(?=[\s\d])/.freeze
-    
-        def initialize(name = nil, path = nil, type = nil)
-      self.name = name
-      self.path = path
-      self.type = type
-    
-        def build_and_queue_request(url, options, &block)
-      request = Request.new(url, request_options.merge(options))
-      request.on_complete(&block) if block
-      queue(request)
-      request
-    end
-    
-        def effective_url
-      @effective_url ||= URL.parse super
-    end
-    
-            css('header').each do |node|
-          node.before(node.children).remove
-        end
-    
-      #
-  # HTTP 404/File not found response class wrapper.
-  #
-  class E404 < Response
-    def initialize(message = 'File not found', proto = DefaultProtocol)
-      super(404, message, proto)
-    end
-  end
-    
-      #
-  # Listens on the defined port and host and starts monitoring for clients.
-  #
-  def start
-    
-        self.codec = res[2][IAX_IE_DESIRED_CODEC].unpack('N')[0]
-    self.state = :ringing
-    self.ring_start = ::Time.now.to_i
-    self.client.send_ack(self)
-    true
-  end
-    
-    # Default timings
-IAX_DEFAULT_REG_REFRESH = 60
-IAX_DEFAULT_TIMEOUT     = 10
-    
-      def self.create_ipmi_getchannel_probe
-    [   # Get Channel Authentication Capabilities
-      0x06, 0x00, 0xff, 0x07, # RMCP Header
-      0x00, 0x00, 0x00, 0x00,
-      0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x20, 0x18,
-      0xc8, 0x81, 0x00, 0x38, 0x8e, 0x04, 0xb5
-    ].pack('C*')
-  end
-    
-            # Sends a kerberos request, and reads the response through the connection
-        #
-        # @param req [Rex::Proto::Kerberos::Model::KdcRequest] the request to send
-        # @return [<Rex::Proto::Kerberos::Model::KrbError, Rex::Proto::Kerberos::Model::KdcResponse>] The kerberos message
-        # @raise [RuntimeError] if the transport protocol is unknown or the response can't be parsed.
-        # @raise [NotImplementedError] if the transport protocol isn't supported
-        def send_recv(req)
-          send_request(req)
-          res = recv_response
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module CredentialCache
-        # This class provides a representation of a Principal stored in the Kerberos Credential Cache.
-        class Principal < Element
-          # @!attribute name_type
-          #   @return [Integer]
-          attr_accessor :name_type
-          # @!attribute realm
-          #   @return [String]
-          attr_accessor :realm
-          # @!attribute components
-          #   @return [Array<String>]
-          attr_accessor :components
-    
-                res = ''
-            case etype
-            when RC4_HMAC
-              res = encrypt_rc4_hmac(data, key, 5)
-            else
-              raise ::NotImplementedError, 'EncryptedData schema is not supported'
-            end
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of a principal, an asset (e.g., a
-        # workstation user or a network server) on a network.
-        class Element
-    
-                res = ''
-            case etype
-            when RC4_HMAC
-              res = decrypt_rc4_hmac(cipher, key, msg_type)
-              raise ::RuntimeError, 'EncryptedData failed to decrypt' if res.length < 8
-              res = res[8, res.length - 1]
-            else
-              raise ::NotImplementedError, 'EncryptedData schema is not supported'
-            end
-    
-        def initialize(tag_name, markup, tokens)
-      @by = nil
-      @source = nil
-      @title = nil
-      if markup =~ FullCiteWithTitle
-        @by = $1
-        @source = $2 + $3
-        @title = $4.titlecase.strip
-      elsif markup =~ FullCite
-        @by = $1
-        @source = $2 + $3
-      elsif markup =~ AuthorTitle
-        @by = $1
-        @title = $2.titlecase.strip
-      elsif markup =~ Author
-        @by = $1
-      end
-      super
-    end
-    
-        def render(context)
-      if @img
-        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
-      else
-        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
-      end
-    end
-  end
+      gem 'rb-fsevent'
+  gem 'kicker'
+  gem 'awesome_print'
+  gem 'ruby-prof', :platforms => [:ruby]
 end
+
     
-            Dir.chdir(includes_dir) do
-          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
-          if choices.include?(file)
-            source = File.read(file)
-            partial = Liquid::Template.parse(source)
-            context.stack do
-              rtn = rtn + partial.render(context)
-            end
-          else
-            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
-          end
-        end
-      end
-      rtn
-    end
-  end
+        pod 'Alamofire', path: '../Alamofire Example/Alamofire'
     
-        def render(context)
-      code_dir = (context.registers[:site].config['code_dir'].sub(/^\//,'') || 'downloads/code')
-      code_path = (Pathname.new(context.registers[:site].source) + code_dir).expand_path
-      file = code_path + @file
-    
-        def initialize(tag_name, markup, tokens)
-      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
-      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
-      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
-      super
-    end
+      autoload :AggregateTarget,           'cocoapods/target/aggregate_target'
+  autoload :Command,                   'cocoapods/command'
+  autoload :Deintegrator,              'cocoapods_deintegrate'
+  autoload :Executable,                'cocoapods/executable'
+  autoload :ExternalSources,           'cocoapods/external_sources'
+  autoload :Installer,                 'cocoapods/installer'
+  autoload :HooksManager,              'cocoapods/hooks_manager'
+  autoload :PodTarget,                 'cocoapods/target/pod_target'
+  autoload :Project,                   'cocoapods/project'
+  autoload :Resolver,                  'cocoapods/resolver'
+  autoload :Sandbox,                   'cocoapods/sandbox'
+  autoload :Target,                    'cocoapods/target'
+  autoload :Validator,                 'cocoapods/validator'
