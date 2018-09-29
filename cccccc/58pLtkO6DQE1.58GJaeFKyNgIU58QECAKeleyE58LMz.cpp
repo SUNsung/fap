0ed11f99958a92e9c5940f4e5aa74ac7a3c5f444
SUNsung <weatherius@gmail.com>
2018-09-29 11:03:31 +0300

    Rewrite [58pLtkO6DQE1.58GJaeFKyNgIU58QECAKeleyE58LMz.cpp] File in: 2018-09-29 11:03:30

--- cccccc/58pLtkO6DQE1.58GJaeFKyNgIU58QECAKeleyE58LMz.cpp
+++ cccccc/58pLtkO6DQE1.58GJaeFKyNgIU58QECAKeleyE58LMz.cpp
@@ -1,291 +1,40 @@
 
-    {    int n_aux = this->ListAuxiliaryStates().size();
-    aux_type->clear();
-    for (int i = 0; i < n_aux; ++i ) aux_type->push_back(mshadow::default_type_flag);
-    return true;
-  }
-  /*!
-   * \brief Copy this OperatorProperty.
-   * \return a pointer to the copied OperatorProperty
-   */
-  virtual OperatorProperty* Copy() const = 0;
-  /*!
-   * \brief Create a Operator on specific context
-   */
-  virtual Operator* CreateOperator(Context ctx) const = 0;
-  /*!
-   * \brief Create a Operator on specific context and input shape/type
-   * \param ctx context of this operator
-   * \param in_shape shape of the input ndarrays
-   * \param in_type dtype of the input ndarrays
-   * \return the created operator
-   */
-  virtual Operator* CreateOperatorEx(Context ctx, std::vector<TShape> *in_shape,
-                                     std::vector<int> *in_type) const {
-    std::vector<int> out_type, aux_type;
-    std::vector<TShape> out_shape, aux_shape;
-    out_type.resize(this->ListOutputs().size());
-    out_shape.resize(this->ListOutputs().size());
-    aux_type.resize(this->ListAuxiliaryStates().size());
-    aux_shape.resize(this->ListAuxiliaryStates().size());
-    CHECK(InferType(in_type, &out_type, &aux_type));
-    CHECK(InferShape(in_shape, &out_shape, &aux_shape));
-    return CreateOperator(ctx);
-  }
-  /*!
-   * \brief return the type string of the Operator
-   *  subclasses override this function.
-   * \return The type string.
-   */
-  virtual std::string TypeString() const = 0;
-  //--------------------------------------------------------
-  // All the below functions are optional to override.
-  //--------------------------------------------------------
-  /*!
-   * \brief Declare additional resource required in forward pass.
-   *  These additional resources will be presented in OpContext.requested
-   *  in the same order of the returned Resource.
-   * \param in_shape The input shape to the operator, corresponds to shapes of in_data.
-   * \return Additional resource request
-   */
-  virtual std::vector<ResourceRequest> ForwardResource(
-      const std::vector<TShape> &in_shape) const {
-    return std::vector<ResourceRequest>();
-  }
-  /*!
-   * \brief Declare additional resource required in backward pass.
-   *  These additional resources will be presented in OpContext.requested
-   *  in the same order of the returned Resource.
-   * \param in_shape The input shape to the operator, corresponds to shapes of in_data.
-   * \return Additional resource request
-   */
-  virtual std::vector<ResourceRequest> BackwardResource(
-      const std::vector<TShape> &in_shape) const {
-    return std::vector<ResourceRequest>();
-  }
-  /*!
-   * \brief Declare the input requirement of Backward pass.
-   *
-   *  Only the returned list of variables will be used in Backward.
-   *  This function is used for memory optimization.
-   *  It is advised to override and only return what is actually needed.
-   *  If this function is not overriden, all the variables will be valid in Backward.
-   *
-   * \code
-   *  // The following code declares Backward need out_grad[0], in_data[0],in_data[1]
-   *  vector<int> BackwardInputs(const vector<int> &out_grad,
-   *                             const vector<int> &in_data,
-   *                             const vector<int> &out_data) const {
-   *    return {out_grad[0], in_data[0], in_data[1]};
-   *  }
-   * \endcode
-   * \param out_grad gradient of outputs in backward pass.
-   * \param in_data the input data in forward pass.
-   * \param out_data the output data in forward pass.
-   * \return an integer vector indicating the input requirments
-   * \sa BackwardInputs
-   */
-  virtual std::vector<int> DeclareBackwardDependency(
-      const std::vector<int> &out_grad,
-      const std::vector<int> &in_data,
-      const std::vector<int> &out_data) const {
-    // By default requires to see all the things.
-    // remember to override this function to get a better performance.
-    std::vector<int> ret = out_grad;
-    ret.insert(ret.end(), in_data.begin(), in_data.end());
-    ret.insert(ret.end(), out_data.begin(), out_data.end());
+        
+        /**
+ * Changelog:
+ * - March 2013, Diederik Huys:    original version
+ * - November 2014, Pieter Wuille: updated to use Peter Dettman's parallel multiplication algorithm
+ * - December 2014, Pieter Wuille: converted from YASM to GCC inline assembly
+ */
+    
+    #include 'hash.h'
+    
+    
+    {    secp256k1_scalar_clear(&s);
     return ret;
-  }
-  /*!
-   * \brief Get possible forward inplace options.
-   *  This function enables optimization to reuse memory of inputs in output.
-   *  Only override when necessary, by default in-place is disabled.
-   *
-   *  The reason for void* type in the out_data is to distinguish the order
-   *  of mappings between the two, compiler will report error when
-   *  in_data and out_data's order in the pair get reversed.
-   *
-   * \code
-   *  // The following code says out_data[0] can share data with in_data[0]
-   *  vector<pair<int, void*> > ForwardInplaceOption(const vector<int> &in_data,
-   *                                                 const vector<void*> &out_data) const {
-   *    return {{in_data[0], out_data[0]}};
-   *  }
-   * \endcode
-   * \param in_data The input data in forward pass.
-   * \param out_data The output data in forward pass.
-   * \return list of pair of that maps input->output,
-   *   indicating possible in place operations.
-   */
-  virtual std::vector<std::pair<int, void*> > ForwardInplaceOption(
-      const std::vector<int> &in_data,
-      const std::vector<void*> &out_data) const {
-    return std::vector<std::pair<int, void*> >();
-  }
-  /*!
-   * \brief Get possible backward inplace options.
-   *  This function enables optimization to reuse memory of inputs in output.
-   *  Only override when necessary, by default in-place is disabled.
-   *
-   *  The reason for void* type in the in_grad is to distinguish the order
-   *  of mappings between the two, compiler will report error when
-   *  in_data and out_data's order in the pair get reversed.
-   *
-   * \code
-   *  // The following code says in_grad[0] can share data with in_data[0]
-   *  vector<pair<int,int> > BackwardInplaceOption(
-   *                 const std::vector<int> &out_grad,
-   *                 const std::vector<int> &in_data,
-   *                 const std::vector<int> &out_data,
-   *                 const std::vector<int> &in_grad) const {
-   *    return {in_data[0], in_grad[0]}};
-   *  }
-   * \endcode
-   * \param in_data The input data in forward pass.
-   * \param out_data The output data in forward pass.
-   * \param in_grad Gradient of inputs in backward pass.
-   * \param out_grad Gradient of outputs in backward pass.
-   * \return list of pair of that maps input->output,
-   *   indicating possible in place operations.
-   */
-  virtual std::vector<std::pair<int, void*> > BackwardInplaceOption(
-      const std::vector<int> &out_grad,
-      const std::vector<int> &in_data,
-      const std::vector<int> &out_data,
-      const std::vector<void*> &in_grad) const {
-    return std::vector<std::pair<int, void*> >();
-  }
-  /*!
-   * \brief Get Backward Input Dependency for generic types of data.
-   *  Normally T can be pointer of Symbol::DataEntry, or NDArray.
-   *  This function will select the result list of T according to DeclareBackwardDependency.
-   *
-   * \param in_data the input data in forward pass.
-   * \param out_data the output data in forward pass.
-   * \param out_grad gradient of outputs in backward pass.
-   * \tparam T the generic type parameter.
-   * \return vector of inputs the Backward Operation depends on.
-   * \sa DeclareBackwardDependency
-   */
-  template<typename T>
-  inline std::vector<T> BackwardInputs(const std::vector<T> &out_grad,
-                                       const std::vector<T> &in_data,
-                                       const std::vector<T> &out_data) const {
-    int counter = 0;
-    std::vector<int> out_grad_index(out_grad.size());
-    std::vector<int> in_data_index(in_data.size());
-    std::vector<int> out_data_index(out_data.size());
-    for (size_t i = 0; i < out_grad_index.size(); ++i) {
-      out_grad_index[i] = counter++;
+}
+    
+        s_one[31] = 1;
+    /* Check against pubkey creation when the basepoint is the generator */
+    for (i = 0; i < 100; ++i) {
+        secp256k1_sha256_t sha;
+        unsigned char s_b32[32];
+        unsigned char output_ecdh[32];
+        unsigned char output_ser[32];
+        unsigned char point_ser[33];
+        size_t point_ser_len = sizeof(point_ser);
+        secp256k1_scalar s;
     }
-    for (size_t i = 0; i < in_data_index.size(); ++i) {
-      in_data_index[i] = counter++;
-    }
-    for (size_t i = 0; i < out_data_index.size(); ++i) {
-      out_data_index[i] = counter++;
-    }
-    std::vector<T> all_data;
-    all_data.insert(all_data.end(), out_grad.begin(), out_grad.end());
-    all_data.insert(all_data.end(), in_data.begin(), in_data.end());
-    all_data.insert(all_data.end(), out_data.begin(), out_data.end());
-    
-        caffe::TBlob2CaffeBlob<xpu, Dtype>(caffe::Grad,
-                                      bot_.begin(),
-                                      in_grad.begin(),
-                                      param_.num_data);
-    // Pass grad scale to caffe blob
-    MXCAFFEBLOB(top_[0], Dtype)->set_cpu_diff(&grad_scale_);
-    
-      void Start() override {
-    streams_.reset(new StreamManager<kMaxNumGpus, kNumStreamsPerGpu>());
-    task_queue_.reset(new dmlc::ConcurrentBlockingQueue<OprBlock*>());
-    io_task_queue_.reset(new dmlc::ConcurrentBlockingQueue<OprBlock*>());
-    thread_pool_.reset(new ThreadPool(kNumWorkingThreads,
-                                      [this](std::shared_ptr<dmlc::ManualEvent> ready_event) {
-                                        ThreadWorker(task_queue_, ready_event); },
-                                      true));
-    io_thread_pool_.reset(new ThreadPool(1,
-                                         [this](std::shared_ptr<dmlc::ManualEvent> ready_event) {
-                                           ThreadWorker(io_task_queue_, ready_event); },
-                                         true));
-  }
     
     
-    {
-    {/*! \brief typedef the factory function of data iterator */
-typedef std::function<ImageAugmenter *()> ImageAugmenterFactory;
-/*!
- * \brief Registry entry for DataIterator factory functions.
- */
-struct ImageAugmenterReg
-    : public dmlc::FunctionRegEntryBase<ImageAugmenterReg,
-                                        ImageAugmenterFactory> {
-};
-//--------------------------------------------------------------
-// The following part are API Registration of Iterators
-//--------------------------------------------------------------
-/*!
- * \brief Macro to register image augmenter
- *
- * \code
- * // example of registering a mnist iterator
- * REGISTER_IMAGE_AUGMENTER(aug_default)
- * .describe('default augmenter')
- * .set_body([]() {
- *     return new DefaultAugmenter();
- *   });
- * \endcode
- */
-#define MXNET_REGISTER_IMAGE_AUGMENTER(name)                            \
-  DMLC_REGISTRY_REGISTER(::mxnet::io::ImageAugmenterReg, ImageAugmenterReg, name)
-}  // namespace io
-}  // namespace mxnet
-#endif  // MXNET_USE_OPENCV
+    {    secp256k1_ecdsa_recoverable_signature_load(ctx, &r, &s, &recid, sigin);
+    secp256k1_ecdsa_signature_save(sig, &r, &s);
+    return 1;
+}
     
-    IMGUI_IMPL_API bool     ImGui_ImplGlfw_InitForOpenGL(GLFWwindow* window, bool install_callbacks);
-IMGUI_IMPL_API bool     ImGui_ImplGlfw_InitForVulkan(GLFWwindow* window, bool install_callbacks);
-IMGUI_IMPL_API void     ImGui_ImplGlfw_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplGlfw_NewFrame();
-    
-    #include <stdint.h>
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_primitives.h>
-#include 'imgui.h'
-#include 'imgui_impl_allegro5.h'
-    
-                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
-            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox('Another Window', &show_another_window);
-    
-    extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+    const std::string& UniValue::get_str() const
 {
-    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
-        return true;
-    }
-    
-            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-    
-        io.SetClipboardTextFn = ImGui_ImplGlfw_SetClipboardText;
-    io.GetClipboardTextFn = ImGui_ImplGlfw_GetClipboardText;
-    io.ClipboardUserData = g_Window;
-#if defined(_WIN32)
-    io.ImeWindowHandle = (void*)glfwGetWin32Window(g_Window);
-#endif
-    
-            // Create the input layout
-        D3D11_INPUT_ELEMENT_DESC local_layout[] = 
-        {
-            { 'POSITION', 0, DXGI_FORMAT_R32G32_FLOAT,   0, (size_t)(&((ImDrawVert*)0)->pos), D3D11_INPUT_PER_VERTEX_DATA, 0 },
-            { 'TEXCOORD', 0, DXGI_FORMAT_R32G32_FLOAT,   0, (size_t)(&((ImDrawVert*)0)->uv),  D3D11_INPUT_PER_VERTEX_DATA, 0 },
-            { 'COLOR',    0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, (size_t)(&((ImDrawVert*)0)->col), D3D11_INPUT_PER_VERTEX_DATA, 0 },
-        };
-        if (g_pd3dDevice->CreateInputLayout(local_layout, 3, g_pVertexShaderBlob->GetBufferPointer(), g_pVertexShaderBlob->GetBufferSize(), &g_pInputLayout) != S_OK)
-            return false;
-    
-    struct VERTEX_CONSTANT_BUFFER
-{
-    float   mvp[4][4];
-};
+    if (typ != VSTR)
+        throw std::runtime_error('JSON value is not a string as expected');
+    return getValStr();
+}
